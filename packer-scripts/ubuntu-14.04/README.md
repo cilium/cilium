03df78e5ac5df6ef7ad23fa3796e30404f4fcdee
Madhu Challa <madhu@cilium.io>
2015-12-29 12:03:08 -0800

    change partition to be a single partition to conserve disk space and add support for public and private keys by default.

---
+++ packer-scripts/ubuntu-14.04/README.md
@@ -0,0 +1,2 @@
+Before running make replace public key in scripts/vagrant.sh with your
+public key and private key in Vagrantfile.sample with your private key path.
--- packer-scripts/ubuntu-14.04/scripts/vagrant.sh
+++ packer-scripts/ubuntu-14.04/scripts/vagrant.sh
@@ -18,10 +18,12 @@ usermod -a -G sudo vagrant
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
-#cat <<EOF > authorized_keys
-#ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZTTnFNK09hRwUjTsT33KsZbCb0U8oUrj/VkP+uwfY1ygiNZki/nupU1YZhxWw4kADlU9xt1U1Iy0abfCqih9Scd5YyucA9iaFmO+PzSRlvtrn3B5T9Ss7pT6lNemL9yqpR7M6dvyDa+Hchrdfv/9M3DJRHKGj82lIJy8QfpWpnU9C7K8J8qiC2t63ugjQBK2VvrIpb66beaDFCUPU3Wst0OZuzWrjsRtc8MwhgnHD8zED9vim1Z6h/e2tW2wvAuJEWfUGB4EMIOigRJnaispPsHr8hXQCQy2LD9yo8g6os+xYVJ3OSsTNiCgaQ951i4lzaOL2YqQ4t2kC4D/d5PID root@noiro-ucs08
-#EOF
 
-wget --no-check-certificate 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+# Replace this key with your public key
+cat <<EOF > authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZTTnFNK09hRwUjTsT33KsZbCb0U8oUrj/VkP+uwfY1ygiNZki/nupU1YZhxWw4kADlU9xt1U1Iy0abfCqih9Scd5YyucA9iaFmO+PzSRlvtrn3B5T9Ss7pT6lNemL9yqpR7M6dvyDa+Hchrdfv/9M3DJRHKGj82lIJy8QfpWpnU9C7K8J8qiC2t63ugjQBK2VvrIpb66beaDFCUPU3Wst0OZuzWrjsRtc8MwhgnHD8zED9vim1Z6h/e2tW2wvAuJEWfUGB4EMIOigRJnaispPsHr8hXQCQy2LD9yo8g6os+xYVJ3OSsTNiCgaQ951i4lzaOL2YqQ4t2kC4D/d5PID root@noiro-ucs08
+EOF
+
+#wget --no-check-certificate 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
