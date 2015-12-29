51fadf3e834e16e2c239fe1f76d88e8313aa2b03
Madhu Challa <challa@noironetworks.com>
2015-12-29 15:33:00 -0800

    Replace standard kernel with net-next build produces about 350MB vagrant box.
    root@noiro-ucs08:~/cilium-net/packer-scripts/ubuntu-14.04# ls -l *.box
    -rw-r--r-- 1 root root 364980917 Dec 29 15:31 ubuntu-1404-libvirt.box
    
    vagrant@ubuntu:~$ uname -a
    Linux ubuntu 4.1.0-rc4-custom #1 SMP Tue Dec 29 15:26:42 PST 2015 x86_64 x86_64 x86_64 GNU/Linux
    vagrant@ubuntu:~$

--- packer-scripts/ubuntu-14.04/README.md
+++ packer-scripts/ubuntu-14.04/README.md
@@ -1,2 +1,5 @@
 Before running make replace public key in scripts/vagrant.sh with your
 public key and private key in Vagrantfile.sample with your private key path.
+
+Also replace number of cores and memory in ubuntu-1404.json and
+Vagrantfile.sample to whatever your system can support.
--- packer-scripts/ubuntu-14.04/scripts/net-next.sh
+++ packer-scripts/ubuntu-14.04/scripts/net-next.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-apt-get -y install git build-essential kernel-package fakeroot libncurses5-dev libssl-dev ccache bc
+apt-get update
+apt-get -y install git
 cd $HOME
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/davem/net-next.git
+git clone git://git.breakpoint.cc/dborkman/net-next.git
 cd net-next
+rm -Rf .git
 cp /boot/config-`uname -r` .config
 yes '' | make oldconfig
-./scripts/config --enable CONFIG_NET_ACT_BPF
+./scripts/config --disable CONFIG_DEBUG_INFO
+./scripts/config --disable CONFIG_DEBUG_KERNEL
+./scripts/config --enable CONFIG_BPF
+./scripts/config --enable CONFIG_BPF_SYSCALL
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_BPF
+./scripts/config --module CONFIG_NET_CLS_BPF
+./scripts/config --module CONFIG_NET_ACT_BPF
+./scripts/config --enable CONFIG_BPF_JIT
+./scripts/config --enable CONFIG_HAVE_BPF_JIT
+./scripts/config --enable CONFIG_BPF_EVENTS
+./scripts/config --module CONFIG_TEST_BPF
 ./scripts/config --disable CONFIG_LUSTRE_FS
+
+# make and install latest kernel
 make -j `getconf _NPROCESSORS_ONLN` LOCALVERSION=-custom
+
+# clean all old kernels
+rm -Rf /lib/modules/*
+rm /boot/*
+
 make modules_install
 make install
-shutdown -r now
-sleep 60
+
+# cleanup
+apt-get -y remove git build-essential bc
+apt-get -y autoremove
+apt-get clean
+
+# delete kernel sources
+rm -Rf $HOME/net-next
