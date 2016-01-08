a8c04e70beb3c37826f20b8882079cc3b2c5397d
Thomas Graf <tgraf@suug.ch>
2016-01-09 00:42:12 +0100

    Adapt to new addressing model, 16 bits for containers
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- common/addressing.go
+++ common/addressing.go
@@ -7,9 +7,9 @@ import (
 // Default addressing schema
 //
 // cluster:		beef:beef:beef:beef::/64
-// rack:		beef:beef:beef:beef:<rack>::/80
-// node:		beef:beef:beef:beef:<rack>:<node>::/96
-// lxc:			beef:beef:beef:beef:<rack>:<node>:<lxc>:<lxc>/128
+// loadbalancer:	beef:beef:beef:beef:<lb>::/80
+// node:		beef:beef:beef:beef:<lb>:<node>::/112
+// lxc:			beef:beef:beef:beef:<lb>:<node>:<node>:<lxc>/128
 
 func ValidEndpointAddress(addr net.IP) bool {
 	switch len(addr) {
@@ -18,12 +18,12 @@ func ValidEndpointAddress(addr net.IP) bool {
 		return false
 	case net.IPv6len:
 		// node id may not be 0
-		if addr[8] == 0 && addr[9] == 0 && addr[10] == 0 && addr[11] == 0 {
+		if addr[10] == 0 && addr[11] == 0 && addr[12] == 0 && addr[13] == 0 {
 			return false
 		}
 
 		// endpoint id may not be 0
-		if addr[12] == 0 && addr[13] == 0 && addr[14] == 0 && addr[15] == 0 {
+		if addr[14] == 0 && addr[15] == 0 {
 			return false
 		}
 	}
@@ -38,12 +38,12 @@ func ValidNodeAddress(addr net.IP) bool {
 		return false
 	case net.IPv6len:
 		// node id may not be 0
-		if addr[8] == 0 && addr[9] == 0 && addr[10] == 0 && addr[11] == 0 {
+		if addr[10] == 0 && addr[11] == 0 && addr[12] == 0 && addr[13] == 0 {
 			return false
 		}
 
 		// node address must contain 0 suffix
-		if addr[12] != 0 || addr[13] != 0 || addr[14] != 0 || addr[15] != 0 {
+		if addr[14] != 0 || addr[15] != 0 {
 			return false
 		}
 	}
@@ -56,9 +56,8 @@ func MapEndpointToNode(epAddr net.IP) net.IP {
 	case net.IPv4len:
 		// Not supported yet
 	case net.IPv6len:
-		for i := 12; i < 16; i++ {
-			epAddr[i] = 0
-		}
+		epAddr[14] = 0
+		epAddr[15] = 0
 		return epAddr
 	}
 
