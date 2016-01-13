6785dd603166a024c8a735272748c7d2489f0bf0
André Martins <andre@cilium.io>
2016-01-13 22:42:02 +0000

    Added new TDD framework go-check
    Signed-off-by: André Martins <andre@cilium.io>

--- common/addressing_test.go
+++ common/addressing_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"net"
 	"testing"
+
+	. "github.com/noironetworks/cilium-net/common/Godeps/_workspace/src/gopkg.in/check.v1"
 )
 
 var (
@@ -11,31 +13,32 @@ var (
 	NodeAddr = net.IP{0xbe, 0xef, 0xbe, 0xef, 0xbe, 0xef, 0xbe, 0xef, 0xaa, 0xaa, 0xaa, 0xaa, 0x11, 0x11, 0, 0}
 )
 
-func TestEpAddrEndpointAddress(t *testing.T) {
-	if !ValidEndpointAddress(EpAddr) {
-		t.Fatalf("unexpected invalid EP address %s", EpAddr.String())
-	}
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
 
-	if ValidEndpointAddress(NodeAddr) {
-		t.Fatalf("unexpected valid node address %s", NodeAddr.String())
-	}
+type CommonSuite struct{}
+
+var _ = Suite(&CommonSuite{})
+
+func (s *CommonSuite) TestEpAddrEndpointAddress(c *C) {
+	c.Assert(ValidEndpointAddress(EpAddr), Equals, true,
+		Commentf("unexpected invalid EP address %s", EpAddr.String()))
+
+	c.Assert(ValidEndpointAddress(NodeAddr), Equals, false,
+		Commentf("unexpected valid node address %s", NodeAddr.String()))
 }
 
-func TestNodeAddrEndpointAddress(t *testing.T) {
-	if ValidNodeAddress(EpAddr) {
-		t.Fatalf("unexpected valid EP address %s", EpAddr.String())
-	}
+func (s *CommonSuite) TestNodeAddrEndpointAddress(c *C) {
+	c.Assert(ValidNodeAddress(EpAddr), Equals, false,
+		Commentf("unexpected valid EP address %s", EpAddr.String()))
 
-	if !ValidNodeAddress(NodeAddr) {
-		t.Fatalf("unexpected invalid node address %s", NodeAddr.String())
-	}
+	c.Assert(ValidNodeAddress(NodeAddr), Equals, true,
+		Commentf("unexpected invalid node address %s", NodeAddr.String()))
 }
 
-func TestMapEndpointToNode(t *testing.T) {
-
+func (s *CommonSuite) TestMapEndpointToNode(c *C) {
 	node := MapEndpointToNode(EpAddr)
 
-	if bytes.Compare(node, NodeAddr) != 0 {
-		t.Fatalf("MapEndpointToNode failed: %s != %s", node.String(), NodeAddr.String())
-	}
+	c.Assert(bytes.Compare(node, NodeAddr) != 0, Equals, false,
+		Commentf("MapEndpointToNode failed: %s != %s", node.String(), NodeAddr.String()))
 }
