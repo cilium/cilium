b7a5789e9ff64ba2f522e282d5f8ab5aab4e38f9
André Martins <andre@cilium.io>
2016-01-19 01:27:42 +0000

    Fixing BuildEndpointAddress with unit tests
    Signed-off-by: André Martins <andre@cilium.io>

--- common/const.go
+++ common/const.go
@@ -12,8 +12,14 @@ const (
 )
 
 var (
+	// Default addressing schema
+	//
+	// cluster:		    beef:beef:beef:beef::/64
+	// loadbalancer:	beef:beef:beef:beef:<lb>::/80
+	// node:		    beef:beef:beef:beef:<lb>:<node>::/96
+	// lxc:			    beef:beef:beef:beef:<lb>:<node>:<lxc>:<lxc>/128
 	ClusterIPv6Mask      = net.CIDRMask(64, 128)
 	LoadBalancerIPv6Mask = net.CIDRMask(80, 128)
-	NodeIPv6Mask         = net.CIDRMask(112, 128)
+	NodeIPv6Mask         = net.CIDRMask(96, 128)
 	ContainerIPv6Mask    = net.CIDRMask(128, 128)
 )
