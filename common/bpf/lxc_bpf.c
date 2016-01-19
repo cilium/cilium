c66855683f9f45156c553bcb986a8c5520aae65f
Thomas Graf <tgraf@suug.ch>
2016-01-19 21:46:33 +0100

    Source mac address validation
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- common/bpf/lxc_bpf.c
+++ common/bpf/lxc_bpf.c
@@ -1,11 +1,27 @@
 #include <iproute2/bpf_api.h>
 #include <sys/socket.h>
 #include <stdint.h>
+#include <string.h>
 #include "common.h"
 #include "lib/ipv6.h"
 #include "lib/eth.h"
+#include <lxc_config.h>
 
 __BPF_MAP(cilium_lxc, BPF_MAP_TYPE_HASH, 0, sizeof(__u16), sizeof(struct lxc_info), PIN_GLOBAL_NS, 1024);
+
+#ifndef DISABLE_SMAC_VERIFICATION
+static inline int verify_src_mac(struct __sk_buff *skb)
+{
+	union macaddr src, valid = LXC_MAC;
+	load_eth_saddr(skb, &src, 0);
+	return compare_eth_addr(&src, &valid);
+}
+#else
+static inline int verify_src_mac(struct __sk_buff *skb)
+{
+	return 0;
+}
+#endif
 
 static inline int do_redirect6(struct __sk_buff *skb, int nh_off)
 {
@@ -16,7 +32,8 @@ static inline int do_redirect6(struct __sk_buff *skb, int nh_off)
         char fmt[] = "skb %p len %d\n";
         char fmt2[] = "%x %x\n";
 
-	/* FIXME: Validate source MAC and source IP */
+	if (verify_src_mac(skb))
+		return -1;
 
 	/* FIXME: Validate destination node ID and perform encap */
 
