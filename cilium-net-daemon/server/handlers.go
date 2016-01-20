4030271b5da4353665d72e59c0fd3b7c51fd0e0c
André Martins <aanm90@gmail.com>
2016-01-20 17:22:58 +0000

    completed server implementation
    Signed-off-by: André Martins <aanm90@gmail.com>

--- cilium-net-daemon/server/handlers.go
+++ cilium-net-daemon/server/handlers.go
@@ -1,33 +1,49 @@
 package server
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+
+	"github.com/noironetworks/cilium-net/common/types"
 
 	"github.com/noironetworks/cilium-net/Godeps/_workspace/src/github.com/gorilla/mux"
 )
 
 func (router *Router) ping(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	str, _ := router.daemon.Ping()
-	fmt.Fprint(w, str)
+	if str, err := router.daemon.Ping(); err != nil {
+		processServerError(w, r, err)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, str)
+	}
 }
 
 func (router *Router) endpointCreate(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	//compile eBPF program
-	log.Debug("vars[\"uuid\"]=", vars["uuid"])
-	w.WriteHeader(http.StatusNotImplemented)
+	d := json.NewDecoder(r.Body)
+	var ep types.Endpoint
+	if err := d.Decode(&ep); err != nil {
+		processServerError(w, r, err)
+		return
+	}
+	if err := router.daemon.EndpointJoin(ep); err != nil {
+		processServerError(w, r, err)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
 
 func (router *Router) endpointDelete(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	log.Debug("vars[\"uuid\"]=", vars["uuid"])
-	w.WriteHeader(http.StatusNotImplemented)
-}
-
-func (router *Router) endpointGet(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	log.Debug("vars[\"uuid\"]=", vars["uuid"])
-	w.WriteHeader(http.StatusNotImplemented)
+	if val, ok := vars["uuid"]; !ok {
+		processServerError(w, r, errors.New("server received empty uuid"))
+		return
+	} else {
+		if err := router.daemon.EndpointLeave(val); err != nil {
+			processServerError(w, r, err)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
