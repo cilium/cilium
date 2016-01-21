b3e26b3ab80c7aaeb61aa9f228a24d6add1ee5e5
André Martins <aanm90@gmail.com>
2016-01-21 15:45:09 +0000

    Added Test to goArray2C
    Signed-off-by: André Martins <aanm90@gmail.com>

--- cilium-net-daemon/daemon/endpoint_test.go
+++ cilium-net-daemon/daemon/endpoint_test.go
@@ -26,3 +26,11 @@ func (s *DaemonSuite) TestIsValidID(c *C) {
 	c.Assert(isValidID("0x12"), Equals, false)
 	c.Assert(isValidID("./../../../etc"), Equals, false)
 }
+
+func (s *DaemonSuite) TestGoArray2C(c *C) {
+	c.Assert(goArray2C([]byte{0, 0x01, 0x02, 0x03}), Equals, "{ 0x0, 0x1, 0x2, 0x3 }")
+	c.Assert(goArray2C([]byte{0, 0xFF, 0xFF, 0xFF}), Equals, "{ 0x0, 0xff, 0xff, 0xff }")
+	c.Assert(goArray2C([]byte{0xa, 0xbc, 0xde, 0xf1}), Equals, "{ 0xa, 0xbc, 0xde, 0xf1 }")
+	c.Assert(goArray2C([]byte{0}), Equals, "{ 0x0 }")
+	c.Assert(goArray2C([]byte{}), Equals, "{  }")
+}
