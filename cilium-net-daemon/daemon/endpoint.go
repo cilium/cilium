e1536dc6a6d32e06956dc0ba1940d75e53f2236f
Thomas Graf <thomas@cilium.io>
2016-01-21 15:44:06 +0100

    Generate lxc_config.h in daemon
    The generation of the header in the shell script has become
    ridiculous. Generate it from golang.
    
    An example header will look like this:
     /*
      * Container ID: 33468
      * MAC: aa:bb:cc:dd:ee:ff
      * IP: beef::1:165:82bc
      * Router MAC: de:ad:be:ef:c0:de
      */
    
     #define DEBUG
     #define NODE_ID 1
     #define LXC_MAC { .addr = { aa, bb, cc, dd, ee, ff } }
     #define LXC_IP { .addr = { be, ef, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 65, 82, bc } }
     #define ROUTER_MAC { .addr = { de, ad, be, ef, c0, de } }
    
    Also create a working container directory based on the container
    ID which persists for the runtime of the container.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- cilium-net-daemon/daemon/endpoint.go
+++ cilium-net-daemon/daemon/endpoint.go
@@ -2,12 +2,68 @@ package daemon
 
 import (
 	"fmt"
+	"net"
+	"os"
 	"os/exec"
 
 	"github.com/noironetworks/cilium-net/common/types"
 )
 
+func goArray2C(array []byte) string {
+	ret := "{ "
+
+	for i, e := range array {
+		if i == 0 {
+			ret = ret + fmt.Sprintf("%x", e)
+		} else {
+			ret = ret + fmt.Sprintf(", %x", e)
+		}
+	}
+
+	return ret + " }"
+}
+
+func fmtDefineAddress(name string, addr []byte) string {
+	return fmt.Sprintf("#define %s { .addr = %s }\n", name, goArray2C(addr))
+}
+
 func (d Daemon) EndpointJoin(ep types.Endpoint) error {
+
+	lxcDir := "./" + ep.ID
+
+	if err := os.MkdirAll(lxcDir, 0777); err != nil {
+		log.Warningf("Failed to create container temporary directory: %s", err)
+		return fmt.Errorf("Failed to create temporary directory: \"%s\"", err)
+	}
+
+	f, err := os.Create(lxcDir + "/lxc_config.h")
+	if err != nil {
+		os.RemoveAll(lxcDir)
+		log.Warningf("Failed to create container headerfile: %s", err)
+		return fmt.Errorf("Failed to create temporary directory: \"%s\"", err)
+
+	}
+
+	routerMac, _ := net.ParseMAC("de:ad:be:ef:c0:de")
+
+	fmt.Fprintf(f, ""+
+		"/*\n"+
+		" * Container ID: %s\n"+
+		" * MAC: %s\n"+
+		" * IP: %s\n"+
+		" * Router MAC: %s\n"+
+		" */\n\n",
+		ep.ID, ep.LxcMAC.String(), ep.LxcIP.String(),
+		routerMac.String())
+
+	f.WriteString("#define DEBUG\n")
+	f.WriteString("#define NODE_ID 1\n")
+	f.WriteString(fmtDefineAddress("LXC_MAC", ep.LxcMAC))
+	f.WriteString(fmtDefineAddress("LXC_IP", ep.LxcIP))
+	f.WriteString(fmtDefineAddress("ROUTER_MAC", routerMac))
+
+	f.Close()
+
 	args := []string{ep.ID, ep.Ifname, ep.LxcMAC.String(), ep.LxcIP.String()}
 	out, err := exec.Command("../common/bpf/join_ep.sh", args...).CombinedOutput()
 	if err != nil {
@@ -21,6 +77,9 @@ func (d Daemon) EndpointJoin(ep types.Endpoint) error {
 }
 
 func (d Daemon) EndpointLeave(epID string) error {
+	lxcDir := "./" + epID
+	os.RemoveAll(lxcDir)
+
 	args := []string{epID}
 	out, err := exec.Command("../common/bpf/leave_ep.sh", args...).CombinedOutput()
 	if err != nil {
