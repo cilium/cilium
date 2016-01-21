de9fb5ee1267d942c9cc573df3c7dde1fcf03fef
Thomas Graf <thomas@cilium.io>
2016-01-21 15:31:57 +0100

    Only need ID when leaving
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- common/bpf/leave_ep.sh
+++ common/bpf/leave_ep.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 ID=$1
-IFNAME=$2
-MAC=$3
-IP=$4
 
-echo "Leave EP id=$ID ifname=$IFNAME mac=$MAC ip=$IP"
+echo "Leave EP id=$ID"
 
 cd ../common/bpf
 ./map_ctrl delete "/sys/fs/bpf/tc/globals/cilium_lxc" $ID
-tc qdisc delete dev $IFNAME ingress 2> /dev/null
