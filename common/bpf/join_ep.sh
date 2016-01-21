1a66a177d9150dd9fad570cae02060d441474675
Thomas Graf <tgraf@suug.ch>
2016-01-21 15:44:06 +0100

    Generate lxc_config.h in daemon
    The generation of the header in the shell script has become
    ridiculous. Generate it from golang.
    
    An example header will look like this:
     /*
      * Container ID: 33468
      * MAC: aa:bb:cc:dd:ee:ff
      * IP: beef::1:165:82bc
      * Router MAC: de:ad:be:ef:c0:de
      */
    
     #define DEBUG
     #define NODE_ID 1
     #define LXC_MAC { .addr = { aa, bb, cc, dd, ee, ff } }
     #define LXC_IP { .addr = { be, ef, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 65, 82, bc } }
     #define ROUTER_MAC { .addr = { de, ad, be, ef, c0, de } }
    
    Also create a working container directory based on the container
    ID which persists for the runtime of the container.
    
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- common/bpf/join_ep.sh
+++ common/bpf/join_ep.sh
@@ -7,40 +7,18 @@ IFNAME=$2
 MAC=$3
 IP=$4
 
-# FIXME FIXME FIXME
-NODE_ID=1
-ROUTER_MAC="ba:ad:ba:ad:ba:ad"
-
 IFINDEX=$(cat /sys/class/net/$IFNAME/ifindex)
 
 echo "Join EP id=$ID ifname=$IFNAME mac=$MAC ip=$IP ifindex=$IFINDEX"
+
+# This directory was created by the daemon and contains the per container header file
+DIR="$PWD/$ID"
 
 cd ../common/bpf
 ./map_ctrl update "/sys/fs/bpf/tc/globals/cilium_lxc" $ID $IFINDEX $MAC $IP
 
 # Temporary fix until clang is properly installed and available in default PATH
 export PATH="/usr/local/clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04/bin/:$PATH"
-
-DIR=`mktemp -d -p ./`
-
-function mac2array()
-{
-        echo "{ 0x${1//:/, 0x} }"
-}
-
-function addr2array()
-{
-	# FIXME
-	echo "{ .p1 = 0xdead, .p2 = 0xbeef, .p3 = 0xbaad, .p4 = 0xc0de }"
-}
-
-cat <<EOF > $DIR/lxc_config.h
-#define DEBUG
-#define LXC_MAC { .addr = $(mac2array $MAC) }
-#define ROUTER_MAC { .addr = $(mac2array $ROUTER_MAC) }
-#define LXC_IP $(addr2array $IP)
-#define NODE_ID $NODE_ID
-EOF
 
 clang -O2 -emit-llvm -c lxc_bpf.c -I$DIR -o - | llc -march=bpf -filetype=obj -o $DIR/bpf.o
 
@@ -50,5 +28,3 @@ tc qdisc add dev $IFNAME ingress
 
 #tc filter add dev dummy1 parent eeee: bpf da obj /tmp/bpf.o sec dummy1-egress
 tc filter add dev $IFNAME parent ffff: bpf da obj $DIR/bpf.o sec from-container
-
-rm -r $DIR
