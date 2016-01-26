6c22b90439a6c20b325936a591eb1ee49c9dda5e
Daniel Borkmann <daniel@cilium.io>
2016-01-26 14:54:13 +0100

    bpf-net: update scripts to use cl-egress with clone-less redirect
    Signed-off-by: Daniel Borkmann <daniel@cilium.io>

--- bpf-net/netns.sh
+++ bpf-net/netns.sh
@@ -37,6 +37,12 @@ ip netns exec ns2 ip link add dummy2 type dummy
 ip netns exec ns1 ip link set dev dummy1 up
 ip netns exec ns2 ip link set dev dummy2 up
 
+ip netns exec ns1 ip link set dev vxlan1 mtu 1400
+ip netns exec ns1 ip link set dev dummy1 mtu 1400
+
+ip netns exec ns2 ip link set dev vxlan2 mtu 1400
+ip netns exec ns2 ip link set dev dummy2 mtu 1400
+
 ip netns exec ns1 ip addr add 10.0.1.1/24 dev dummy1
 ip netns exec ns2 ip addr add 10.0.1.2/24 dev dummy2
 
@@ -56,79 +62,86 @@ ip netns exec ns2 ip neigh add 10.0.1.1 dev dummy2 lladdr $NS1_DP
 
 cat <<EOF > /tmp/bpf.c
 #include <iproute2/bpf_api.h>
-#include <sys/socket.h>
 
-#define TX_XMIT	0
-#define TX_FRWD	1
+#include <sys/socket.h>
 
 __section("vxlan1-ingress")
 int cls_entry_vx1i(struct __sk_buff *skb)
 {
+	char fmt[] = "cls_entry_vx1i tos:%u\n";
 	struct bpf_tunnel_key key = {};
+	int ret;
 
-	skb_get_tunnel_key(skb, &key, sizeof(key), 0);
-	if (key.tunnel_id != 42)
+	ret = skb_get_tunnel_key(skb, &key, sizeof(key), 0);
+	if (ret < 0 || key.tunnel_id != 42)
 		return TC_ACT_SHOT;
 
-	return redirect($NS1_DU, TX_FRWD);
+	trace_printk(fmt, sizeof(fmt), key.tunnel_tos);
+	return redirect($NS1_DU, BPF_F_INGRESS);
 }
 
 __section("dummy1-egress")
 int cls_entry_vx1e(struct __sk_buff *skb)
 {
 	struct bpf_tunnel_key key = {};
+	int ret;
 
 	key.tunnel_id = 42;
+	key.tunnel_tos = 32;
 	key.remote_ipv4 = 0x0a000202; /* 10.0.2.2 */
-	key.tunnel_af = AF_INET;
 
-	skb_set_tunnel_key(skb, &key, sizeof(key), 0);
+	ret = skb_set_tunnel_key(skb, &key, sizeof(key), 0);
+	if (unlikely(ret < 0))
+		return TC_ACT_SHOT;
 
-	clone_redirect(skb, $NS1_VX, TX_XMIT);
-	return TC_ACT_STOLEN;
+	return redirect($NS1_VX, 0);
 }
 
 __section("vxlan2-ingress")
 int cls_entry_vx2i(struct __sk_buff *skb)
 {
+	char fmt[] = "cls_entry_vx2i tos:%u\n";
 	struct bpf_tunnel_key key = {};
+	int ret;
 
-	skb_get_tunnel_key(skb, &key, sizeof(key), 0);
-	if (key.tunnel_id != 42)
+	ret = skb_get_tunnel_key(skb, &key, sizeof(key), 0);
+	if (ret < 0 || key.tunnel_id != 42)
 		return TC_ACT_SHOT;
 
-	return redirect($NS2_DU, TX_FRWD);
+	trace_printk(fmt, sizeof(fmt), key.tunnel_tos);
+	return redirect($NS2_DU, BPF_F_INGRESS);
 }
 
 __section("dummy2-egress")
 int cls_entry_vx2e(struct __sk_buff *skb)
 {
 	struct bpf_tunnel_key key = {};
+	int ret;
 
 	key.tunnel_id = 42;
+	key.tunnel_tos = 52;
 	key.remote_ipv4 = 0x0a000201; /* 10.0.2.1 */
-	key.tunnel_af = AF_INET;
 
-	skb_set_tunnel_key(skb, &key, sizeof(key), 0);
+	ret = skb_set_tunnel_key(skb, &key, sizeof(key), 0);
+	if (unlikely(ret < 0))
+		return TC_ACT_SHOT;
 
-	clone_redirect(skb, $NS2_VX, TX_XMIT);
-	return TC_ACT_STOLEN;
+	return redirect($NS2_VX, 0);
 }
 
 BPF_LICENSE("GPL");
 EOF
 
-clang -O2 -emit-llvm -c /tmp/bpf.c -o - | llc -march=bpf -filetype=obj -o /tmp/bpf.o
+clang -O2 -emit-llvm -I./include/ -c /tmp/bpf.c -o - | llc -march=bpf -filetype=obj -o /tmp/bpf.o
 
-# Still need this prio bandaid as we don't have prequeue yet, can become a bottleneck due to locking
-ip netns exec ns1 tc qdisc add dev dummy1 root handle eeee: prio bands 3
-ip netns exec ns1 tc qdisc add dev vxlan1 ingress
+ip netns exec ns1 tc qdisc add dev dummy1 clsonly
+ip netns exec ns1 tc qdisc add dev vxlan1 clsonly
 
-ip netns exec ns2 tc qdisc add dev dummy2 root handle eeee: prio bands 3
-ip netns exec ns2 tc qdisc add dev vxlan2 ingress
+ip netns exec ns2 tc qdisc add dev dummy2 clsonly
+ip netns exec ns2 tc qdisc add dev vxlan2 clsonly
 
-ip netns exec ns1 tc filter add dev dummy1 parent eeee: bpf da obj /tmp/bpf.o sec dummy1-egress
-ip netns exec ns1 tc filter add dev vxlan1 parent ffff: bpf da obj /tmp/bpf.o sec vxlan1-ingress
+ip netns exec ns1 tc filter add dev dummy1 cl-egress  bpf da obj /tmp/bpf.o sec dummy1-egress
+ip netns exec ns1 tc filter add dev vxlan1 cl-ingress bpf da obj /tmp/bpf.o sec vxlan1-ingress
 
-ip netns exec ns2 tc filter add dev dummy2 parent eeee: bpf da obj /tmp/bpf.o sec dummy2-egress
-ip netns exec ns2 tc filter add dev vxlan2 parent ffff: bpf da obj /tmp/bpf.o sec vxlan2-ingress
+ip netns exec ns2 tc filter add dev dummy2 cl-egress  bpf da obj /tmp/bpf.o sec dummy2-egress
+ip netns exec ns2 tc filter add dev vxlan2 cl-ingress bpf da obj /tmp/bpf.o sec vxlan2-ingress
--- bpf-net/netns6.sh
+++ bpf-net/netns6.sh
@@ -37,6 +37,12 @@ ip netns exec ns2 ip link add dummy2 type dummy
 ip netns exec ns1 ip link set dev dummy1 up
 ip netns exec ns2 ip link set dev dummy2 up
 
+ip netns exec ns1 ip link set dev vxlan1 mtu 1400
+ip netns exec ns1 ip link set dev dummy1 mtu 1400
+
+ip netns exec ns2 ip link set dev vxlan2 mtu 1400
+ip netns exec ns2 ip link set dev dummy2 mtu 1400
+
 ip netns exec ns1 ip -6 addr add 2607::1:0:1/120 dev dummy1
 ip netns exec ns2 ip -6 addr add 2607::1:0:2/120 dev dummy2
 
@@ -56,22 +62,26 @@ ip netns exec ns2 ip -6 neigh add 2607::1:0:1 dev dummy2 lladdr $NS1_DP
 
 cat <<EOF > /tmp/bpf.c
 #include <iproute2/bpf_api.h>
-#include <sys/socket.h>
-#include <stdint.h>
 
-#define TX_XMIT	0
-#define TX_FRWD	1
+#include <stdint.h>
+#include <sys/socket.h>
 
 __section("vxlan1-ingress")
 int cls_entry_vx1i(struct __sk_buff *skb)
 {
+	char fmt[] = "cls_entry_vx1i tos:%u\n";
 	struct bpf_tunnel_key key = {};
+	uint8_t *addr = (uint8_t *)key.remote_ipv6;
+	int ret;
 
-	skb_get_tunnel_key(skb, &key, sizeof(key), 0);
-	if (key.tunnel_id != 42)
+	ret = skb_get_tunnel_key(skb, &key, sizeof(key),
+				 BPF_F_TUNINFO_IPV6);
+	if (ret < 0 || key.tunnel_id != 42 ||
+	    addr[0] != 0x26 || addr[1] != 0x07 || addr[11] != 0 || addr[15] != 2)
 		return TC_ACT_SHOT;
 
-	return redirect($NS1_DU, TX_FRWD);
+	trace_printk(fmt, sizeof(fmt), key.tunnel_tos);
+	return redirect($NS1_DU, BPF_F_INGRESS);
 }
 
 __section("dummy1-egress")
@@ -79,30 +89,39 @@ int cls_entry_vx1e(struct __sk_buff *skb)
 {
 	struct bpf_tunnel_key key = {};
 	uint8_t *addr = (uint8_t *)key.remote_ipv6;
+	int ret;
 
 	key.tunnel_id = 42;
-	key.tunnel_af = AF_INET6; // -> 2607::2:0:2
+	key.tunnel_tos = 63;
 	addr[0]  = 0x26;
 	addr[1]  = 0x07;
 	addr[11] = 2;
 	addr[15] = 2;
 
-	skb_set_tunnel_key(skb, &key, sizeof(key), 0);
+	ret = skb_set_tunnel_key(skb, &key, sizeof(key),
+				 BPF_F_TUNINFO_IPV6);
+	if (unlikely(ret < 0))
+		return TC_ACT_SHOT;
 
-	clone_redirect(skb, $NS1_VX, TX_XMIT);
-	return TC_ACT_STOLEN;
+	return redirect($NS1_VX, 0);
 }
 
 __section("vxlan2-ingress")
 int cls_entry_vx2i(struct __sk_buff *skb)
 {
+	char fmt[] = "cls_entry_vx2i tos:%u\n";
 	struct bpf_tunnel_key key = {};
+	uint8_t *addr = (uint8_t *)key.remote_ipv6;
+	int ret;
 
-	skb_get_tunnel_key(skb, &key, sizeof(key), 0);
-	if (key.tunnel_id != 42)
+	ret = skb_get_tunnel_key(skb, &key, sizeof(key),
+				 BPF_F_TUNINFO_IPV6);
+	if (ret < 0 || key.tunnel_id != 42 ||
+	    addr[0] != 0x26 || addr[1] != 0x07 || addr[11] != 0 || addr[15] != 1)
 		return TC_ACT_SHOT;
 
-	return redirect($NS2_DU, TX_FRWD);
+	trace_printk(fmt, sizeof(fmt), key.tunnel_tos);
+	return redirect($NS2_DU, BPF_F_INGRESS);
 }
 
 __section("dummy2-egress")
@@ -110,34 +129,36 @@ int cls_entry_vx2e(struct __sk_buff *skb)
 {
 	struct bpf_tunnel_key key = {};
 	uint8_t *addr = (uint8_t *)key.remote_ipv6;
+	int ret;
 
 	key.tunnel_id = 42;
-	key.tunnel_af = AF_INET6; // -> 2607::2:0:1
+	key.tunnel_tos = 52;
 	addr[0]  = 0x26;
 	addr[1]  = 0x07;
 	addr[11] = 2;
 	addr[15] = 1;
 
-	skb_set_tunnel_key(skb, &key, sizeof(key), 0);
+	ret = skb_set_tunnel_key(skb, &key, sizeof(key),
+				 BPF_F_TUNINFO_IPV6);
+	if (unlikely(ret < 0))
+		return TC_ACT_SHOT;
 
-	clone_redirect(skb, $NS2_VX, TX_XMIT);
-	return TC_ACT_STOLEN;
+	return redirect($NS2_VX, 0);
 }
 
 BPF_LICENSE("GPL");
 EOF
 
-clang -O2 -emit-llvm -c /tmp/bpf.c -o - | llc -march=bpf -filetype=obj -o /tmp/bpf.o
+clang -O2 -emit-llvm -I./include/ -c /tmp/bpf.c -o - | llc -march=bpf -filetype=obj -o /tmp/bpf.o
 
-# Still need this prio bandaid as we don't have prequeue yet, can become a bottleneck due to locking
-ip netns exec ns1 tc qdisc add dev dummy1 root handle eeee: prio bands 3
-ip netns exec ns1 tc qdisc add dev vxlan1 ingress
+ip netns exec ns1 tc qdisc add dev dummy1 clsonly
+ip netns exec ns1 tc qdisc add dev vxlan1 clsonly
 
-ip netns exec ns2 tc qdisc add dev dummy2 root handle eeee: prio bands 3
-ip netns exec ns2 tc qdisc add dev vxlan2 ingress
+ip netns exec ns2 tc qdisc add dev dummy2 clsonly
+ip netns exec ns2 tc qdisc add dev vxlan2 clsonly
 
-ip netns exec ns1 tc filter add dev dummy1 parent eeee: bpf da obj /tmp/bpf.o sec dummy1-egress
-ip netns exec ns1 tc filter add dev vxlan1 parent ffff: bpf da obj /tmp/bpf.o sec vxlan1-ingress
+ip netns exec ns1 tc filter add dev dummy1 cl-egress  bpf da obj /tmp/bpf.o sec dummy1-egress
+ip netns exec ns1 tc filter add dev vxlan1 cl-ingress bpf da obj /tmp/bpf.o sec vxlan1-ingress
 
-ip netns exec ns2 tc filter add dev dummy2 parent eeee: bpf da obj /tmp/bpf.o sec dummy2-egress
-ip netns exec ns2 tc filter add dev vxlan2 parent ffff: bpf da obj /tmp/bpf.o sec vxlan2-ingress
+ip netns exec ns2 tc filter add dev dummy2 cl-egress  bpf da obj /tmp/bpf.o sec dummy2-egress
+ip netns exec ns2 tc filter add dev vxlan2 cl-ingress bpf da obj /tmp/bpf.o sec vxlan2-ingress
