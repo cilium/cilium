ba111f6676534c1a7157ac48ad83aa05c5a972e4
Thomas Graf <tgraf@suug.ch>
2016-01-26 14:34:53 +0100

    Automatically derive first global IPv4 address as node id
    Running the docker-plugin via Makefile will now automatically
    derive the node-id from the first global IPv4 address configured.
    This allows deriving the encap endpoint from the node-id section
    of the container directly.
    
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

---
+++ docker-plugin/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+V4=$(ip -4 a show scope global | \
+	grep -oEm 1 '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | \
+	head -1)
+
+HEX=$(printf '%02X' ${V4//./ })
+
+V6="beef::${HEX:0:4}:${HEX:4}:0:0"
+echo "Running ./cilium-docker -d -n $V6"
+
+./cilium-docker -d -n $V6
