e918cb89d1bb454b48314b603e42cd133ec74fe6
Thomas Graf <tgraf@suug.ch>
2016-01-26 20:06:11 +0100

    Initial support for RX encapsulation
    Not fully functional yet without manual modificiation
    of NODE_ID. Requires automatic generation of a per node
    configuration header file.
    
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- common/bpf/init.sh
+++ common/bpf/init.sh
@@ -7,11 +7,15 @@ MAP="/sys/fs/bpf/tc/globals/cilium_lxc"
 # Enable JIT
 echo 1 > /proc/sys/net/core/bpf_jit_enable
 
+cd ../common/bpf
+
 ip link del cilium_vxlan 2> /dev/null || true
 ip link add cilium_vxlan type vxlan external
 ip link set cilium_vxlan up
 
-cd ../common/bpf
+tc qdisc add dev cilium_vxlan ingress
+tc filter add dev cilium_vxlan parent ffff: bpf da obj lxc_bpf.o sec from-overlay
+
 mount bpffs /sys/fs/bpf/ -t bpf || true
 
 mkdir -p $(dirname $MAP)
