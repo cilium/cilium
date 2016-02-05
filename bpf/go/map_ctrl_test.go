2e7b20ac896e5b0f0b52421229ebe685186ae252
Thomas Graf <thomas@cilium.io>
2016-02-05 12:32:40 +0100

    Provide MAC address in correct byte order
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- bpf/go/map_ctrl_test.go
+++ bpf/go/map_ctrl_test.go
@@ -17,10 +17,10 @@ var _ = Suite(&BPFMapSuite{})
 func (s *BPFMapSuite) TestIsValidID(c *C) {
 	m, err := ParseMAC("01:23:45:67:89:ab")
 	c.Assert(err, Equals, nil)
-	c.Assert(m, Equals, mac(0x0123456789ab))
+	c.Assert(m, Equals, mac(0xAB8967452301))
 	c.Assert(m.String(), Equals, "01:23:45:67:89:AB")
 	m, err = ParseMAC("FE:DC:BA:98:76:54")
 	c.Assert(err, Equals, nil)
-	c.Assert(m, Equals, mac(0xFEDCBA987654))
+	c.Assert(m, Equals, mac(0x547698BADCFE))
 	c.Assert(m.String(), Equals, "FE:DC:BA:98:76:54")
 }
