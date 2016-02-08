3d7104a9bd4e730df72616fe845349f92d38ad0d
André Martins <aanm90@gmail.com>
2016-02-08 02:54:05 +0000

    Replaced CNI IPAM with daemon's IPAM
    Signed-off-by: André Martins <aanm90@gmail.com>

--- cni/host-local-src/dummy-file.go
+++
@@ -1,8 +0,0 @@
-// This file only exists to forcely have the host-local dependency under GoDeps
-package main
-
-import . "github.com/noironetworks/cilium-net/Godeps/_workspace/src/github.com/appc/cni/plugins/ipam/host-local"
-
-func main() {
-
-}
