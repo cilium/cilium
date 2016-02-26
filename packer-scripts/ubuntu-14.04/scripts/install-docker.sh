ffe05453f8e75c4287cd710067baf45340f43c7f
Thomas Graf <tgraf@suug.ch>
2016-02-26 18:58:01 +0100

    Bump version to include latest Docker build
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- packer-scripts/ubuntu-14.04/scripts/install-docker.sh
+++ packer-scripts/ubuntu-14.04/scripts/install-docker.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p install
 cd install
 
-wget -r -np -nd http://www.infradead.org/~tgr/cilium-docker-build/
+wget --quiet -r -np -nd http://www.infradead.org/~tgr/cilium-docker-build/
 
 for pkg in *.deb; do
 	dpkg -i $pkg
