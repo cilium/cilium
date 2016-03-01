01e436212f0a6b21efeaaa60aa6dbb32cbdf8bb3
Thomas Graf <tgraf@suug.ch>
2016-03-01 12:46:32 +0100

    Policy WIP

--- cilium-net-daemon/daemon/policy.go
+++ cilium-net-daemon/daemon/policy.go
@@ -14,17 +14,6 @@ var (
 	tree types.PolicyTree
 )
 
-// Return child of node with given name
-func findChild(name string, node *types.PolicyNode) *types.PolicyNode {
-	for _, leaf := range node.Childs {
-		if leaf.Name == name {
-			return &leaf
-		}
-	}
-
-	return nil
-}
-
 // Returns node and its parent or an error
 func findNode(path string) (*types.PolicyNode, *types.PolicyNode, error) {
 	var parent *types.PolicyNode
@@ -38,11 +27,11 @@ func findNode(path string) (*types.PolicyNode, *types.PolicyNode, error) {
 	parent = nil
 
 	for _, nodeName := range strings.Split(path, ".") {
-		if child := findChild(nodeName, current); child == nil {
-			return nil, nil, fmt.Errorf("Unable to find node %s in path %s", nodeName, path)
-		} else {
+		if child, ok := current.Children[nodeName]; ok {
 			parent = current
 			current = child
+		} else {
+			return nil, nil, fmt.Errorf("Unable to find node %s in path %s", nodeName, path)
 		}
 	}
 
@@ -50,42 +39,43 @@ func findNode(path string) (*types.PolicyNode, *types.PolicyNode, error) {
 }
 
 func (d Daemon) PolicyAdd(path string, node types.PolicyNode) error {
-	if path == "" {
-		tree.Root = node
-		return nil
-	}
+	log.Debugf("Policy Add Request: %+v", &node)
 
-	if node, _, err := findNode(path); err != nil {
+	if node, parent, err := findNode(path); err != nil {
 		return err
 	} else {
-		*node = types.PolicyNode{}
-
+		if parent == nil {
+			log.Debugf("Replacing root")
+			tree.Root = *node
+		} else {
+			node.Children[node.Name] = node
+		}
 	}
 
 	return nil
 }
 
 func (d Daemon) PolicyDelete(path string) error {
-	if path == "" {
-		// delete entire tree
-		tree.Root = types.PolicyNode{}
-		return nil
-	}
+	log.Debugf("Policy Delete Request: %s", path)
 
-	if node, _, err := findNode(path); err != nil {
+	if node, parent, err := findNode(path); err != nil {
 		return err
 	} else {
-		*node = types.PolicyNode{}
+		if parent == nil {
+			tree.Root = types.PolicyNode{}
+		} else {
+			delete(parent.Children, node.Name)
+		}
 	}
 
 	return nil
 }
 
 func (d Daemon) PolicyGet(path string) (*types.PolicyNode, error) {
-	if path == "" {
-		return &tree.Root, nil
-	} else {
-		node, _, err := findNode(path)
-		return node, err
-	}
+	log.Debugf("Policy Get Request: %s", path)
+	node, _, err := findNode(path)
+
+	log.Debugf("Found node: %+v", node)
+
+	return node, err
 }
--- policy.sh
+++ policy.sh
@@ -2,36 +2,130 @@
 
 set -x
 
+# // doing this as andrema
+# kubectl pod create -f ngnix.yaml
+# -> tying to environment: Lizards with uid andrema
+#    LABELS: io.cilium.Lizards
+#            io.cilium.Lizards.user=andrema
+#            io.cilium.Lizards.ngnix
+#
+#   cilium-daemon:
+#     JoinEndpoint()
+#          bpf_lxc
+#          bpf_policy [empty]
+#
+#   Notification of new local container: ngnix
+#     1. cilium_endpoint := daemon.endpoints[dockerID]
+#     2. Get Docker Labels
+#     3. Get k8s labels [metadata]
+#     4. Check valid prefixes based valid_labels.json
+#     5. endpoint.SecLabel = GetLabelsID("[...]") -> ???
+#
+#  cilium-daemon:
+#      AddPolicy() <- policy.JSON
+#
+#      -> Triggers generation of SecLabel and Consumers:
+#
+#     endpoint.SecLabel = GetLabelsID(["io.cilium.Lizards", "io.cilium.Lizards.user=andrema", ...]) -> 50
+#
+#     foreach endpoint {
+#       foreach seclabel {
+#         if canConsume(seclabel, endpoint) {
+#            endpoint.Consumers = append(endpoint.Consumers, label)
+#         updateBPFMAP(endpoint.Conumsers)
+#       }
+#     }
+#
+#     func canConsume(seclabel, endpoint) {
+#        src_labels := GetLabels(seclabel)
+#        dst_labels := GetLabels(endpoint)
+#
+#        // walk through policy
+#        return true or false
+#     }
+# SecLabel
+#     SRC:                        DST:
+# 11  # io.cilium.Lizards.Web  => io.cilium.Lizards.DB    33
+#     # io.cilium.Lizards.QA   => io.cilium.Lizards.Prod
+#
+# 22  # io.cilium.Lizards.Web  => io.cilium.Lizards.DB    33
+#     # io.cilium.Lizards.Prod => io.cilium.Lizards.Prod
+#
+#     33: [22]
+#     22: [33]
+#
+#     TODO: Thomas: Ask John about conntrack in BPF
+
+LABELS=$(cat <<EOF
+[{
+	"Name": "io.cilium",
+	"Type": "cilium",
+},{
+	"Name": "io.kuberentes.pod.uid",
+	"Type": "kubernetes"
+}
+EOF
+
 POLICY=$(cat <<EOF
 {
         "Name": "io.cilium",
-        "Rules": null,
-        "Childs": [{
-                "Name": "Lizards",
-		"Rules": [{
-			"Coverage": "QA",
-			"Requires": "QA"
-		}],
-                "Childs": [{
-                        "Name": "Web"
-                },{
-                        "Name": "DB",
-                        "Rules": [{
-				"Allow": "Web"
+        "Children": {
+		"Lizards": {
+			"Rules": [{
+				"Coverage": ["QA"],
+				"Requires": ["QA"]
+			},{
+				"Coverage": ["Prod"],
+				"Requires": ["Prod"]
+			}],
+			"Children": {
+				"Web": { },
+				"DB": {
+					"Rules": [{
+						"Allow": ["Web"]
+						"Allow": [{"com.coke.flavour", "blah"}, "Web", {"io.kubernetes.pod.uid", "3224234"}],
+					}]
+				},
+				"Ngnix": {
+					"Rules": [{
+						"Allow": ["World"]
+					}]
+				}
+			}
+		},
+		"Birds": {
+			"Name": "Birds",
+			"Children": {
+				"DB": { }
+			}
+		},
+		"k8s": {
+			"Name": "k8s",
+			"Rules": [{
+				"Coverage": ["PodSelector", "PodSelector2"],
+				"Allow": [{"type": "pod", "label": "tier=database"}, {"type": "namespace", ...}],
+				"Ports": { "to": [{"tcp", 80},{"udp", 50}], "from": null },
+				"Drop-Privileges": "Ports",
 			}]
-                }]
-        },{
-                "Name": "Birds",
-                "Rules": null,
-                "Childs": [{
-                        "Name": "DB",
-                        "Rules": null
-                }]
-        }]
+		}
+
+	}
 }
 EOF
 )
 
-curl $FLAGS -XPOST http://localhost:9000/policy/foo -d "$POLICY"
+curl $FLAGS -XPOST http://localhost:9000/policy/io.cilium -d "$POLICY"
 curl $FLAGS -XGET http://localhost:9000/policy/io.cilium.Lizards
 
+
+POLICY=$(cat <<EOF
+{
+        "Name": "DB",
+	"Rules": [{
+		"Allow": ["Web"]
+	}]
+}
+EOF
+)
+
+curl $FLAGS -XPOST http://localhost:9000/policy/io.cilium.Lizards -d "$POLICY"
