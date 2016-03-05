4d9a421a6126b9b5fc1a419eca5b4c487fdd1bc4
André Martins <aanm90@gmail.com>
2016-03-05 02:23:12 +0000

    Fix iptables docker error
    Signed-off-by: André Martins <aanm90@gmail.com>

--- packer-scripts/ubuntu-14.04/scripts/install-docker.sh
+++ packer-scripts/ubuntu-14.04/scripts/install-docker.sh
@@ -14,7 +14,7 @@ for pkg in *.deb; do
 done
 
 usermod -aG docker vagrant
-echo 'DOCKER_OPTS="--storage-driver=overlay"' >> /etc/default/docker
+echo 'DOCKER_OPTS="--storage-driver=overlay --iptables=false"' >> /etc/default/docker
 
 cd ..
 rm -rf $HOME/install
