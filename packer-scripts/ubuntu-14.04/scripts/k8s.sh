5dd473dcf37af7e8226e2710fd50fec10edb7976
Thomas Graf <thomas@cilium.io>
2016-03-11 14:40:46 +0000

    Add kubernetes build to packer scripts
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ packer-scripts/ubuntu-14.04/scripts/k8s.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+cd $HOME
+
+curl -L  https://github.com/coreos/etcd/releases/download/v2.2.4/etcd-v2.2.4-linux-amd64.tar.gz -o etcd-v2.2.4-linux-amd64.tar.gz
+tar xzvf etcd-v2.2.4-linux-amd64.tar.gz
+
+export PATH=$PATH:/home/vagrant/etcd-v2.2.4-linux-amd64
+echo 'export PATH=$PATH:/home/vagrant/etcd-v2.2.4-linux-amd64' >> $HOME/.profile
+
+sudo sudo chmod -R 775  /usr/local/go/pkg/
+sudo sudo chgrp vagrant /usr/local/go/pkg/
+
+git clone -b v1.2.0-beta.0 https://github.com/kubernetes/kubernetes.git
+cd kubernetes
+patch -p1 < ../go/src/github.com/noironetworks/cilium-net/cni/kubernetes.patch
