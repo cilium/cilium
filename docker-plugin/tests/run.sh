a7c63e487b0d4e82815427ae5d5e720cb799fcc5
Thomas Graf <tgraf@suug.ch>
2016-04-06 11:22:24 +0200

    Add policy for runtime tests
    Assigns labels io.cilium.test.server and io.cilium.test.client to the
    test workloads.
    
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- docker-plugin/tests/run.sh
+++ docker-plugin/tests/run.sh
@@ -24,38 +24,42 @@ function abort {
 	exit 1
 }
 
+SERVER_LABEL="io.cilium.test.server"
+CLIENT_LABEL="io.cilium.test.client"
+
+sudo cilium-net-policy -d import -p ./tests/policy/
 
 docker network inspect $TEST_NET || {
 	docker network create --ipam-driver cilium --driver cilium $TEST_NET
 }
 
 reset_trace
-docker run -dt --net=$TEST_NET --name server $NETPERF_IMAGE
+docker run -dt --net=$TEST_NET --name server -l $SERVER_LABEL $NETPERF_IMAGE
 
 SERVER_IP=$(docker inspect --format '{{ .NetworkSettings.Networks.cilium.GlobalIPv6Address }}' server)
 
 reset_trace
-docker run --rm -i --net=$TEST_NET --name client noironetworks/nettools sh -c "sleep 5s && ping6 -c 5 $SERVER_IP" || {
+docker run --rm -i --net=$TEST_NET --name client -l $CLIENT_LABEL noironetworks/nettools sh -c "sleep 5s && ping6 -c 5 $SERVER_IP" || {
 	abort "Error: Could not ping server container"
 }
 
 reset_trace
-docker run --rm -i --net=$TEST_NET --name client noironetworks/nettools sh -c "sleep 5s && tracepath6 $SERVER_IP" || {
+docker run --rm -i --net=$TEST_NET --name client -l $CLIENT_LABEL noironetworks/nettools sh -c "sleep 5s && tracepath6 $SERVER_IP" || {
 	abort "Error: Could not traceroute to server"
 }
 
 reset_trace
-docker run --rm -i --net=$TEST_NET --name netperf $NETPERF_IMAGE sh -c "sleep 5s && netperf -c -C -H $SERVER_IP" || {
+docker run --rm -i --net=$TEST_NET --name netperf -l $CLIENT_LABEL $NETPERF_IMAGE sh -c "sleep 5s && netperf -c -C -H $SERVER_IP" || {
 	abort "Error: Could not netperf to server"
 }
 
 reset_trace
-docker run --rm -i --net=$TEST_NET --name netperf $NETPERF_IMAGE sh -c "sleep 5s && netperf -c -C -t TCP_SENDFILE -H $SERVER_IP" || {
+docker run --rm -i --net=$TEST_NET --name netperf -l $CLIENT_LABEL $NETPERF_IMAGE sh -c "sleep 5s && netperf -c -C -t TCP_SENDFILE -H $SERVER_IP" || {
 	abort "Error: Could not netperf to server"
 }
 
 reset_trace
-docker run --rm -i --net=$TEST_NET --name netperf $NETPERF_IMAGE sh -c "sleep 5s && super_netperf 10 -c -C -t TCP_SENDFILE -H $SERVER_IP" || {
+docker run --rm -i --net=$TEST_NET --name netperf -l $CLIENT_LABEL $NETPERF_IMAGE sh -c "sleep 5s && super_netperf 10 -c -C -t TCP_SENDFILE -H $SERVER_IP" || {
 	abort "Error: Could not netperf to server"
 }
 
