3cafd6fd3b00ba3cbf546cfac9c344f37438b625
Thomas Graf <thomas@cilium.io>
2016-04-13 15:55:35 +0200

    Fix Vagrantfile
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- packer-scripts/ubuntu-14.04/scripts/go.sh
+++ packer-scripts/ubuntu-14.04/scripts/go.sh
@@ -15,14 +15,14 @@ tar -C /usr/local -xJf clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 rm clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 
 export GOROOT=/usr/local/go
-echo "export GOROOT=$GOROOT" >> /etc/bash.bashrc
+echo "export GOROOT=$GOROOT" >> /home/vagrant/.profile
 
 export GOPATH=/home/vagrant/go
-echo "export GOPATH=$GOPATH" >> /etc/bash.bashrc
+echo "export GOPATH=$GOPATH" >> /home/vagrant/.profile
 
 NEWPATH="$GOROOT/bin:$GOPATH/bin:/usr/local/clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04/bin"
 export PATH="$NEWPATH:$PATH"
-echo "export PATH=$NEWPATH:\$PATH" >>  /etc/bash.bashrc
+echo "export PATH=$NEWPATH:\$PATH" >>  /home/vagrant/.profile
 
 rm -rf /home/vagrant/go
 mkdir -p /home/vagrant/go/src
