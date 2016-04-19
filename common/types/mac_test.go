ca2adabcb67e48e1f43b7acf6b44e0b1d14f7030
André Martins <andre@cilium.io>
2016-04-19 13:30:52 +0100

    Implemented JSON Marshal and Unmarshal to our MAC type
    Signed-off-by: André Martins <andre@cilium.io>

--- common/types/mac_test.go
+++ common/types/mac_test.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"encoding/json"
+
 	. "github.com/noironetworks/cilium-net/Godeps/_workspace/src/gopkg.in/check.v1"
 )
 
@@ -14,4 +16,18 @@ func (s *MACSuite) TestUint64(c *C) {
 	v, err := m.Uint64()
 	c.Assert(err, Equals, nil)
 	c.Assert(v, Equals, uint64(0x564534231211))
+}
+
+func (s *MACSuite) TestUnmarshalJSON(c *C) {
+	m := MAC([]byte{0x11, 0x12, 0x23, 0x34, 0x45, 0x56})
+	w := MAC([]byte{0x11, 0x12, 0x23, 0x34, 0x45, 0xAB})
+	d, err := json.Marshal(m)
+	c.Assert(err, Equals, nil)
+	c.Assert(d, DeepEquals, []byte(`"11:12:23:34:45:56"`))
+	var t MAC
+	err = json.Unmarshal([]byte(`"11:12:23:34:45:AB"`), &t)
+	c.Assert(err, Equals, nil)
+	c.Assert(t, DeepEquals, w)
+	err = json.Unmarshal([]byte(`"11:12:23:34:45:A"`), &t)
+	c.Assert(err, NotNil)
 }
