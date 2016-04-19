b9eba60b5d037e740c526440c1603cbe72187567
André Martins <aanm90@gmail.com>
2016-04-19 13:31:52 +0100

    Added String method to ServerError type
    Signed-off-by: André Martins <aanm90@gmail.com>

--- common/types/server.go
+++ common/types/server.go
@@ -1,8 +1,15 @@
 package types
 
+import "fmt"
+
 // ServerError is the type of message used when the daemon returns any error messages
 // in case of failure.
 type ServerError struct {
 	Code int    `json:"code"`
 	Text string `json:"text"`
+}
+
+// String returns string format of the given ServerError.
+func (se ServerError) String() string {
+	return fmt.Sprintf("%d: %s", se.Code, se.Text)
 }
