21e94a3cd213b15106c84f1c54af9fb65fec9326
Thomas Graf <thomas@cilium.io>
2016-05-10 13:06:03 +0200

    bpf: Report all packet drops
    Send a drop notification whenever we return TC_ACT_SHOT and provide
    the reason for the drop.
    
    e.g.
    Packet dropped 132 (Invalid source ip) 110 bytes ifindex=13
    00000000  33 33 00 00 00 16 62 2e  d3 b4 9d a1 86 dd 60 00  |33....b.......`.|
    00000010  00 00 00 38 00 01 fe 80  00 00 00 00 00 00 60 2e  |...8..........`.|
    00000020  d3 ff fe b4 9d a1 ff 02  00 00 00 00 00 00 00 00  |................|
    00000030  00 00 00 00 00 16 3a 00  05 02 00 00 01 00 8f 00  |......:.........|
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- bpf/lib/lxc.h
+++ bpf/lib/lxc.h
@@ -7,62 +7,60 @@
 #include "dbg.h"
 
 #ifndef DISABLE_SMAC_VERIFICATION
-static inline int verify_src_mac(struct __sk_buff *skb)
+static inline int invalid_src_mac(struct __sk_buff *skb)
 {
 	union macaddr src, valid = LXC_MAC;
 	int ret;
 
-	eth_load_saddr(skb, src.addr, 0);
-	ret = eth_addrcmp(&src, &valid);
-#ifdef DEBUG_DROP
-	if (unlikely(ret))
-		printk("Invalid source MAC address, dropping...\n");
-#endif
-
-	return ret;
+	ret = eth_load_saddr(skb, src.addr, 0);
+	if (likely(ret == 0))
+		return eth_addrcmp(&src, &valid);
+	else
+		return ret;
 }
 #else
-static inline int verify_src_mac(struct __sk_buff *skb)
+static inline int invalid_src_mac(struct __sk_buff *skb)
 {
 	return 0;
 }
 #endif
 
 #ifndef DISABLE_SIP_VERIFICATION
-static inline int verify_src_ip(struct __sk_buff *skb, int off)
+static inline int invalid_src_ip(struct __sk_buff *skb, int off)
 {
 	union v6addr src, valid = LXC_IP;
 	int ret;
 
-	ipv6_load_saddr(skb, off, &src);
-	ret = ipv6_addrcmp(&src, &valid);
-#ifdef DEBUG_DROP
-	if (unlikely(ret))
-		printk("Invalid source IP address, dropping...\n");
-#endif
-
-	return ret;
+	ret = ipv6_load_saddr(skb, off, &src);
+	if (likely(ret == 0))
+		return ipv6_addrcmp(&src, &valid);
+	else
+		return ret;
 }
 #else
-static inline int verify_src_ip(struct __sk_buff *skb, int off)
+static inline int invalid_src_ip(struct __sk_buff *skb, int off)
 {
 	return 0;
 }
 #endif
 
-static inline int verify_dst_mac(struct __sk_buff *skb)
+#ifndef DISABLE_DMAC_VERIFICATION
+static inline int invalid_dst_mac(struct __sk_buff *skb)
 {
 	union macaddr dst, valid = NODE_MAC;
 	int ret;
 
-	eth_load_daddr(skb, dst.addr, 0);
-	ret = eth_addrcmp(&dst, &valid);
-#ifdef DEBUG_DROP
-	if (unlikely(ret))
-		printk("Invalid destination MAC address, dropping...\n");
-#endif
-
-	return ret;
+	ret = eth_load_daddr(skb, dst.addr, 0);
+	if (likely(ret == 0))
+		return eth_addrcmp(&dst, &valid);
+	else
+		return ret;
 }
+#else
+static inline int invalid_dst_mac(struct __sk_buff *skb)
+{
+	return 0;
+}
+#endif
 
 #endif
