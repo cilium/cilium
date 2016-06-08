a88c5936d8cab1bc65d2378404193d461984b529
Thomas Graf <tgraf@suug.ch>
2016-06-08 08:49:01 +0200

    bpf: Fix policy statistics
    Always do a policy lookup even if policy is already known to
    perform accounting properly.
    
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- bpf/lib/policy.h
+++ bpf/lib/policy.h
@@ -9,25 +9,25 @@ static inline int policy_can_access(void *map, struct __sk_buff *skb, __u32 src_
 {
 	struct policy_entry *policy;
 
-	if (skb->cb[CB_POLICY] == POLICY_SKIP)
-		goto allow;
-
 	policy = map_lookup_elem(map, &src_label);
 	if (likely(policy)) {
 		/* FIXME: Use per cpu counters */
 		__sync_fetch_and_add(&policy->packets, 1);
 		__sync_fetch_and_add(&policy->bytes, skb->len);
-allow:
 		return TC_ACT_OK;
 	}
+
+	if (skb->cb[CB_POLICY] == POLICY_SKIP)
+		goto allow;
 
 	cilium_trace(skb, DBG_POLICY_DENIED, src_label, SECLABEL);
 
 #ifndef IGNORE_DROP
 	return TC_ACT_SHOT;
-#else
+#endif
+
+allow:
 	return TC_ACT_OK;
-#endif /* IGNORE_DROP */
 }
 
 static inline void policy_mark_skip(struct __sk_buff *skb)
