3168fdcc69d71b6bd8af2728ba676a01d2e61245
André Martins <andre@cilium.io>
2016-06-10 05:30:45 +0100

    K8s-watcher: reading all network policies when starting watcher
    Signed-off-by: André Martins <andre@cilium.io>

--- cilium-net-daemon/daemon/k8s_watcher.go
+++ cilium-net-daemon/daemon/k8s_watcher.go
@@ -18,17 +18,18 @@ type networkPolicyWatchEvent struct {
 
 func (d *Daemon) EnableK8sWatcher(maxSeconds time.Duration) error {
 	curSeconds := 2 * time.Second
-
-	u := d.k8sClient.Get().RequestURI("apis/extensions/v1beta1").
-		Namespace("default").Resource("networkpolicies").Param("watch", "true").URL()
+	uNPs := d.k8sClient.Get().RequestURI("apis/extensions/v1beta1").
+		Resource("networkpolicies").URL().String()
+	uWatcher := d.k8sClient.Get().RequestURI("apis/extensions/v1beta1").
+		Namespace("default").Resource("networkpolicies").Param("watch", "true").URL().String()
 	go func() {
 		reportError := true
-		makeRequest := func() *http.Response {
+		makeRequest := func(url string) *http.Response {
 			for {
-				resp, err := http.Get(u.String())
+				resp, err := http.Get(url)
 				if err != nil {
 					if reportError {
-						log.Warningf("Unable to intall k8s watcher for URL %s: %s", u, err)
+						log.Warningf("Unable to intall k8s watcher for URL %s: %s", url, err)
 						reportError = false
 					}
 				} else if resp.StatusCode == http.StatusOK {
@@ -42,44 +43,64 @@ func (d *Daemon) EnableK8sWatcher(maxSeconds time.Duration) error {
 				}
 			}
 		}
-		resp := makeRequest()
-		curSeconds = time.Second
-		log.Info("Now listening for kubernetes network policies changes")
 		for {
-			npwe := networkPolicyWatchEvent{}
-			err := json.NewDecoder(resp.Body).Decode(&npwe)
+			resp := makeRequest(uNPs)
+			curSeconds = time.Second
+			log.Info("Receiving all policies stored in kubernetes")
+			npList := v1beta1.NetworkPolicyList{}
+			err := json.NewDecoder(resp.Body).Decode(&npList)
 			if err != nil {
 				log.Errorf("Error while receiving data %s", err)
 				resp.Body.Close()
-				resp = makeRequest()
-				curSeconds = time.Second
 				continue
 			}
-			log.Debugf("Received kubernetes network policy %+v\n", npwe)
-			go d.processNPE(npwe)
+			log.Debugf("Received kubernetes network policies %+v\n", npList)
+			for _, np := range npList.Items {
+				go d.processNPE(networkPolicyWatchEvent{watch.Added, np})
+			}
+			resp.Body.Close()
+
+			resp = makeRequest(uWatcher)
+			log.Info("Listening for kubernetes network policies events")
+			for {
+				npwe := networkPolicyWatchEvent{}
+				err := json.NewDecoder(resp.Body).Decode(&npwe)
+				if err != nil {
+					log.Errorf("Error while receiving data %s", err)
+					resp.Body.Close()
+					break
+				}
+				log.Debugf("Received kubernetes network policy %+v\n", npwe)
+				go d.processNPE(npwe)
+			}
 		}
 	}()
 	return nil
 }
 
 func (d *Daemon) processNPE(npwe networkPolicyWatchEvent) {
-	nodePath, pn, err := types.K8sNP2CP(npwe.Object)
-	if err != nil {
-		log.Errorf("Error while parsing kubernetes network policy %+v: %s", npwe.Object, err)
-		return
-	}
 	switch npwe.Type {
 	case watch.Added, watch.Modified:
+		nodePath, pn, err := types.K8sNP2CP(npwe.Object)
+		if err != nil {
+			log.Errorf("Error while parsing kubernetes network policy %+v: %s", npwe.Object, err)
+			return
+		}
 		if err := d.PolicyAdd(nodePath, pn); err != nil {
 			log.Errorf("Error while adding kubernetes network policy %+v: %s", pn, err)
 			return
 		}
-		log.Info("Kubernetes network policy successfully add")
+		log.Infof("Kubernetes network policy successfully add %+v", npwe.Object)
 	case watch.Deleted:
+		nodePath, pn, err := types.K8sNP2CP(npwe.Object)
+		if err != nil {
+			log.Errorf("Error while parsing kubernetes network policy %+v: %s", npwe.Object, err)
+			return
+		}
 		if err := d.PolicyDelete(nodePath); err != nil {
 			log.Errorf("Error while deleting kubernetes network policy %+v: %s", pn, err)
 			return
 		}
-		log.Info("Kubernetes network policy successfully removed")
+		log.Infof("Kubernetes network policy successfully removed %+v", npwe.Object)
 	}
 }
