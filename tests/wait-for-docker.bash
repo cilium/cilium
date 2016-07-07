aba2b6e83486cec0796e4f6c3b91883426924e51
Thomas Graf <thomas@cilium.io>
2016-07-07 19:24:21 +0200

    runtime-tests: Fixup wait-for-docker.bash
    Acked-by: André Martins <andre@cilium.io>
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- tests/wait-for-docker.bash
+++ tests/wait-for-docker.bash
@@ -3,27 +3,28 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 set -e
 if [ -z $1 ]; then
-    return "Empty name"
+	echo "Empty name"
+	exit 1
 else
-    name=$1
+	name=$1
 fi
 
 if [ -z $2 ]; then
-    tries=10
+	tries=10
 else
-    tries=$2
+	tries=$2
 fi
 
 #Wait for container ${name} to be ready
 i=1
 while [[ -z "$contID" && ${i} -le ${tries} ]] ; do
-    echo "Waiting for container ${name}. Attempt ${i}/${tries}..."
-    contID=$(docker ps -aq --filter=name="${name}")
-    sleep 2
-    i=$(( $i + 1 ))
+	echo "Waiting for container ${name}. Attempt ${i}/${tries}..."
+	contID=$(docker ps -aq --filter=name="${name}")
+	sleep 2
+	i=$(( $i + 1 ))
 done
 
 if [ ${i} -ge ${tries} ]; then
-    echo "Unable to find container ${name}, please try again in a couple moments"
-    exit
+	echo "Unable to find container ${name}, please try again in a couple moments"
+	exit 1
 fi
