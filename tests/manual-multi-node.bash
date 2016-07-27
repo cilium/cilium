9569b4a903209f6d7f13b10d5d95a4e9d25ea116
Thomas Graf <thomas@cilium.io>
2016-07-27 13:55:58 +0200

    tests: Extend multi node test to multiple configurations
    Reconfigures the nodes so both native routing and VXLAN encap are being
    tested
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- tests/manual-multi-node.bash
+++ tests/manual-multi-node.bash
@@ -9,24 +9,59 @@ function node_run {
 	vagrant ssh $NODE -- -t "$*"
 }
 
-vagrant up node1
-vagrant up node2
+function cleanup {
+	node_run node1 'docker rm -f server 2> /dev/null' || true
+	node_run node2 'docker rm -f client 2> /dev/null' || true
+}
 
-node_run node1 'docker rm -f server 2> /dev/null' || true
-node_run node2 'docker rm -f client 2> /dev/null' || true
+trap cleanup EXIT
 
-SERVER=$(node_run node1 'docker run -d --net cilium -l io.cilium.server --name server noironetworks/netperf')
-CLIENT=$(node_run node2 'docker run -d --net cilium -l io.cilium.client --name client noironetworks/netperf')
-sleep 5s
+function init {
+	vagrant up node1
+	vagrant up node2
 
-SERVER_IP=$(node_run node1 "docker inspect --format '{{ .NetworkSettings.Networks.cilium.GlobalIPv6Address }}' server" | tr -d '\r')
-echo "Server IPv6: $SERVER_IP"
+	cleanup
 
-node_run node2 "docker exec -i client ping6 -c 5 $SERVER_IP"
+	node_run node1 'docker network create --driver cilium --ipam-driver cilium cilium 2> /dev/null' || true
+	node_run node2 'docker network create --driver cilium --ipam-driver cilium cilium 2> /dev/null' || true
+}
 
-SERVER_IP4=$(node_run node1 "docker inspect --format '{{ .NetworkSettings.Networks.cilium.IPAddress }}' server" | tr -d '\r')
-echo "Server IPv4: $SERVER_IP4"
-node_run node2 "docker exec -i client ping -c 5 $SERVER_IP4"
+function test_run {
+	SERVER=$(node_run node1 'docker run -d --net cilium -l io.cilium.server --name server noironetworks/netperf')
+	CLIENT=$(node_run node2 'docker run -d --net cilium -l io.cilium.client --name client noironetworks/netperf')
+	sleep 5s
 
-node_run node1 'docker rm -f server'
-node_run node2 'docker rm -f client'
+	SERVER_IP=$(node_run node1 "docker inspect --format '{{ .NetworkSettings.Networks.cilium.GlobalIPv6Address }}' server" | tr -d '\r')
+	echo "Server IPv6: $SERVER_IP"
+
+	node_run node2 "docker exec -i client ping6 -c 5 $SERVER_IP"
+	node_run node2 "docker exec -i client netperf -t TCP_STREAM -H $SERVER_IP"
+
+	SERVER_IP4=$(node_run node1 "docker inspect --format '{{ .NetworkSettings.Networks.cilium.IPAddress }}' server" | tr -d '\r')
+	echo "Server IPv4: $SERVER_IP4"
+	node_run node2 "docker exec -i client ping -c 5 $SERVER_IP4"
+	node_run node2 "docker exec -i client netperf -t TCP_STREAM -H $SERVER_IP4"
+
+	cleanup
+}
+
+function test_nodes {
+	OPTS=$*
+
+	echo "Setting up nodes with options: $OPTS"
+
+	node_run node1 "echo \"exec cilium -D daemon run -n f00d::c0a8:210b:0 --ipv4-range 10.1.0.1 $OPTS\" > tmp; sudo cp tmp /etc/init/cilium-net-daemon.conf; sudo service cilium-net-daemon restart"
+	node_run node2 "echo \"exec cilium -D daemon run -n f00d::c0a8:210c:0 --ipv4-range 10.2.0.1 $OPTS -c 192.168.33.11:8500\" > tmp; sudo cp tmp /etc/init/cilium-net-daemon.conf; sudo service cilium-net-daemon restart"
+
+	echo "Waiting for daemon to start up..."
+	sleep 5s
+
+	node_run node1 'cilium policy import ~/go/src/github.com/noironetworks/cilium-net/examples/policy/default/'
+	node_run node2 'cilium policy import ~/go/src/github.com/noironetworks/cilium-net/examples/policy/default/'
+
+	test_run
+}
+
+init
+test_nodes "-t vxlan"
+test_nodes "-d eth1"
