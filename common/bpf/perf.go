cilium/cilium

commit 4b47250f6184d37a4c0853098425c9fc23116e80

Drop notifications
Monitoring:
cilium monitor [-d]

BPF:

Signed-off-by: Thomas Graf <thomas@cilium.io>

common/bpf/perf.go
@@ -1,0 +1,471 @@
+package bpf
+
+/*
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <linux/unistd.h>
+#include <linux/bpf.h>
+#include <linux/perf_event.h>
+#include <sys/resource.h>
+
+void create_perf_event_attr(int type, int config, int sample_type,
+			    int wakeup_events, void *attr)
+{
+	struct perf_event_attr *ptr = (struct perf_event_attr *) attr;
+
+	memset(ptr, 0, sizeof(*ptr));
+
+	ptr->type = type;
+	ptr->config = config;
+	ptr->sample_type = sample_type;
+	ptr->wakeup_events = wakeup_events;
+
+}
+
+static void dump_data(uint8_t *data, size_t size, int cpu)
+{
+	int i;
+
+	printf("event on cpu%d: ", cpu);
+	for (i = 0; i < size; i++)
+		printf("%02x ", data[i]);
+	printf("\n");
+}
+
+struct event_sample {
+	struct perf_event_header header;
+	uint32_t size;
+	uint8_t data[];
+};
+
+struct read_state {
+	size_t raw_size;
+	void *base, *begin, *end;
+};
+
+int perf_event_read_init(int page_count, int page_size, void *_header, void *_state)
+{
+	volatile struct perf_event_mmap_page *header = _header;
+	struct read_state *state = _state;
+	uint64_t data_tail = header->data_tail;
+	uint64_t data_head = header->data_head;
+
+	__sync_synchronize();
+	if (data_head == data_tail)
+		return 0;
+
+	state->raw_size = page_count * page_size;
+	state->base  = ((uint8_t *)header) + page_size;
+	state->begin = state->base + data_tail % state->raw_size;
+	state->end   = state->base + data_head % state->raw_size;
+
+	return state->begin != state->end;
+}
+
+int perf_event_read(void *_state, void *buf, void *_msg)
+{
+	void **msg = (void **) _msg;
+	struct read_state *state = _state;
+	struct event_sample *e = state->begin;
+
+	if (state->begin == state->end)
+		return 0;
+
+	if (state->begin + e->header.size > state->base + state->raw_size) {
+		uint64_t len = state->base + state->raw_size - state->begin;
+
+		memcpy(buf, state->begin, len);
+		memcpy((char *) buf + len, state->base, e->header.size - len);
+
+		e = buf;
+		state->begin = state->base + e->header.size - len;
+	} else if (state->begin + e->header.size == state->base + state->raw_size) {
+		state->begin = state->base;
+	} else {
+		state->begin += e->header.size;
+	}
+
+	*msg = e;
+
+	return 1;
+}
+
+void perf_event_read_finish(void *_header)
+{
+	volatile struct perf_event_mmap_page *header = _header;
+
+	__sync_synchronize();
+	header->data_tail = header->data_head;
+}
+
+void cast(void *ptr, void *_dst)
+{
+	void **dst = (void **) _dst;
+	*dst = ptr;
+}
+
+*/
+import "C"
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+const (
+	MAX_POLL_EVENTS = 32
+)
+
+type PerfEventConfig struct {
+	NumCpus      int
+	NumPages     int
+	MapPath      string
+	Type         int
+	Config       int
+	SampleType   int
+	WakeupEvents int
+}
+
+type PerfEvent struct {
+	cpu      int
+	Fd       int
+	pagesize int
+	npages   int
+	lost     uint64
+	unknown  uint64
+	data     []byte
+}
+
+// Matching 'struct perf_event_header in <linux/perf_event.h>
+type PerfEventHeader struct {
+	Type      uint32
+	Misc      uint16
+	TotalSize uint16
+}
+
+// Matching 'struct perf_event_sample in kernel sources
+type PerfEventSample struct {
+	PerfEventHeader
+	size uint32
+	data byte // Size bytes of data
+}
+
+// Matching 'struct perf_event_lost in kernel sources
+type PerfEventLost struct {
+	PerfEventHeader
+	Id   uint64
+	Lost uint64
+}
+
+func (e *PerfEventSample) DataDirect() []byte {
+	// http://stackoverflow.com/questions/27532523/how-to-convert-1024c-char-to-1024byte
+	size := e.size - 4
+	return (*[1 << 30]byte)(unsafe.Pointer(&e.data))[:int(size):int(size)]
+}
+
+func (e *PerfEventSample) DataCopy() []byte {
+	return C.GoBytes(unsafe.Pointer(&e.data), C.int(e.size))
+}
+
+type ReceiveFunc func(msg *PerfEventSample)
+
+func PerfEventOpen(config *PerfEventConfig, pid int, cpu int, groupFD int, flags int) (*PerfEvent, error) {
+	attr := C.struct_perf_event_attr{}
+
+	C.create_perf_event_attr(
+		C.int(config.Type),
+		C.int(config.Config),
+		C.int(config.SampleType),
+		C.int(config.WakeupEvents),
+		unsafe.Pointer(&attr),
+	)
+
+	ret, _, err := syscall.Syscall6(
+		C.__NR_perf_event_open,
+		uintptr(unsafe.Pointer(&attr)),
+		uintptr(pid),
+		uintptr(cpu),
+		uintptr(groupFD),
+		uintptr(flags), 0)
+
+	if int(ret) > 0 && err == 0 {
+		return &PerfEvent{
+			cpu: cpu,
+			Fd:  int(ret),
+		}, nil
+	}
+	return nil, fmt.Errorf("Unable to open perf event: %s", err)
+}
+
+func (e *PerfEvent) Mmap(pagesize int, npages int) error {
+	size := pagesize * (npages + 1)
+	data, err := syscall.Mmap(e.Fd,
+		0,
+		size,
+		syscall.PROT_READ|syscall.PROT_WRITE,
+		syscall.MAP_SHARED)
+
+	if err != nil {
+		return fmt.Errorf("Unable to mmap perf event: %s", err)
+	}
+
+	e.pagesize = pagesize
+	e.npages = npages
+	e.data = data
+
+	return nil
+}
+
+func (e *PerfEvent) Enable() error {
+	_, _, err := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		uintptr(e.Fd),
+		C.PERF_EVENT_IOC_ENABLE,
+		0)
+
+	if err != 0 {
+		return fmt.Errorf("Unable to enable perf event: %s", err)
+	}
+
+	return nil
+}
+
+func (e *PerfEvent) Disable() error {
+	if e == nil {
+		return nil
+	}
+
+	_, _, err := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		uintptr(e.Fd),
+		C.PERF_EVENT_IOC_DISABLE,
+		0)
+
+	if err != 0 {
+		return fmt.Errorf("Unable to disable perf event: %s", err)
+	}
+
+	return nil
+}
+
+func (e *PerfEvent) Read(receive ReceiveFunc) error {
+	buf := make([]byte, 256)
+	state := C.struct_read_state{}
+
+	// Prepare for reading and check if events are available
+	available := C.perf_event_read_init(C.int(e.npages), C.int(e.pagesize),
+		unsafe.Pointer(&e.data[0]), unsafe.Pointer(&state))
+
+	// Poll false positive
+	if available == 0 {
+		return nil
+	}
+
+	for {
+		var msg *PerfEventHeader
+
+		if ok := C.perf_event_read(unsafe.Pointer(&state),
+			unsafe.Pointer(&buf[0]), unsafe.Pointer(&msg)); ok == 0 {
+			break
+		}
+
+		if msg.Type == C.PERF_RECORD_SAMPLE {
+			var sample *PerfEventSample
+			C.cast(unsafe.Pointer(msg), unsafe.Pointer(&sample))
+			receive(sample)
+		} else if msg.Type == C.PERF_RECORD_LOST {
+			var lost *PerfEventLost
+			C.cast(unsafe.Pointer(msg), unsafe.Pointer(&lost))
+			e.lost += lost.Lost
+		} else {
+			e.unknown++
+		}
+	}
+
+	// Move ring buffer tail pointer
+	C.perf_event_read_finish(unsafe.Pointer(&e.data[0]))
+
+	return nil
+}
+
+func (e *PerfEvent) Close() {
+	if e == nil {
+		return
+	}
+
+	syscall.Close(e.Fd)
+}
+
+type EPoll struct {
+	fd     int
+	nfds   int
+	events [MAX_POLL_EVENTS]syscall.EpollEvent
+}
+
+func (ep *EPoll) AddFD(fd int, events uint32) error {
+	ev := syscall.EpollEvent{
+		Events: events,
+		Fd:     int32(fd),
+	}
+
+	return syscall.EpollCtl(ep.fd, syscall.EPOLL_CTL_ADD, fd, &ev)
+}
+
+func (ep *EPoll) Poll() (int, error) {
+	nfds, err := syscall.EpollWait(ep.fd, ep.events[0:], -1)
+	if err != nil {
+		return 0, err
+	}
+
+	ep.nfds = nfds
+
+	return nfds, nil
+}
+
+func (ep *EPoll) Close() {
+	if ep.fd > 0 {
+		syscall.Close(ep.fd)
+	}
+}
+
+type EventMap struct {
+	fd int
+}
+
+func openMap(path string) (*EventMap, error) {
+	fd, err := ObjGet(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &EventMap{
+		fd: fd,
+	}, nil
+}
+
+func (e *EventMap) Update(ev *PerfEvent) error {
+	return UpdateElement(e.fd, unsafe.Pointer(&ev.cpu), unsafe.Pointer(&ev.Fd), 0)
+}
+
+func (e *EventMap) Close() {
+	if e == nil {
+		return
+	}
+
+	syscall.Close(e.fd)
+}
+
+type PerCpuEvents struct {
+	cpus     int
+	npages   int
+	pagesize int
+	eventMap *EventMap
+	event    map[int]*PerfEvent
+	poll     EPoll
+}
+
+func NewPerCpuEvents(config *PerfEventConfig) (*PerCpuEvents, error) {
+	var err error
+
+	e := &PerCpuEvents{
+		cpus:     config.NumCpus,
+		npages:   config.NumPages,
+		pagesize: os.Getpagesize(),
+		event:    make(map[int]*PerfEvent),
+	}
+
+	defer func() {
+		if err != nil {
+			e.CloseAll()
+		}
+	}()
+
+	e.poll.fd, err = syscall.EpollCreate1(0)
+	if err != nil {
+		return nil, err
+	}
+
+	e.eventMap, err = openMap(config.MapPath)
+	if err != nil {
+		return nil, err
+	}
+
+	for cpu := int(0); cpu < config.NumCpus; cpu++ {
+		event, err := PerfEventOpen(config, -1, cpu, -1, 0)
+		if err != nil {
+			return nil, err
+		} else {
+			e.event[event.Fd] = event
+		}
+
+		if err := e.poll.AddFD(event.Fd, syscall.EPOLLIN); err != nil {
+			return nil, err
+		}
+
+		if err = event.Mmap(e.pagesize, e.npages); err != nil {
+			return nil, err
+		}
+
+		if err = event.Enable(); err != nil {
+			return nil, err
+		}
+	}
+
+	for _, event := range e.event {
+		// FIXME: Not sure what to do here, the map has alreayd been updated and we can't
+		// fully restore it.
+		if err := e.eventMap.Update(event); err != nil {
+			return nil, err
+		}
+	}
+
+	return e, nil
+}
+
+func (e *PerCpuEvents) Poll() (int, error) {
+	return e.poll.Poll()
+}
+
+func (e *PerCpuEvents) ReadAll(receive ReceiveFunc) error {
+	for i := 0; i < e.poll.nfds; i++ {
+		fd := int(e.poll.events[i].Fd)
+		if event, ok := e.event[fd]; ok {
+			if err := event.Read(receive); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (e *PerCpuEvents) Stats() (uint64, uint64) {
+	var lost, unknown uint64
+
+	for _, event := range e.event {
+		lost += event.lost
+		unknown += event.unknown
+	}
+
+	return lost, unknown
+}
+
+func (e *PerCpuEvents) CloseAll() error {
+	var retErr error
+
+	e.eventMap.Close()
+	e.poll.Close()
+
+	for _, event := range e.event {
+		if err := event.Disable(); err != nil {
+			retErr = err
+		}
+
+		event.Close()
+	}
+
+	return retErr
+}

Makefile
@@ -1,7 +1,7 @@
 include Makefile.defs
 
-SUBDIRS = docker-plugin cilium cni bpf
-SUBDIRSLIB = common cilium-net-daemon policy-repo integration
+SUBDIRS = docker-plugin cilium cni bpf common
+SUBDIRSLIB = cilium-net-daemon policy-repo integration
 
 all: $(SUBDIRS)
 

bpf/Makefile
@@ -1,6 +1,6 @@
 include ../Makefile.defs
 
-CLANG_FLAGS := -I. -Wall -Werror
+CLANG_FLAGS := -D__NR_CPUS__=$(shell nproc) -O2 -target bpf -I. -Wall -Werror
 
 BPF = bpf_lxc.o bpf_netdev.o bpf_overlay.o
 SCRIPTS = init.sh join_ep.sh leave_ep.sh
@@ -10,16 +10,16 @@ all: nop.o $(BPF)
 	$(MAKE) -C go
 
 nop.o:
-	clang -O2 -target bpf -c nop.c ${CLANG_FLAGS} -o $@
+	clang ${CLANG_FLAGS} -c nop.c -o $@
 
 bpf_lxc.o:
-	clang -O2 -target bpf -c bpf_lxc.c ${CLANG_FLAGS} -o $@
+	clang ${CLANG_FLAGS} -c bpf_lxc.c -o $@
 
 bpf_netdev.o:
-	clang -O2 -target bpf -c bpf_netdev.c ${CLANG_FLAGS} -o $@
+	clang ${CLANG_FLAGS} -c bpf_netdev.c -o $@
 
 bpf_overlay.o:
-	clang -O2 -target bpf -c bpf_overlay.c ${CLANG_FLAGS} -o $@
+	clang ${CLANG_FLAGS} -c bpf_overlay.c -o $@
 
 clean:
 	rm -fr *.o

bpf/bpf_lxc.c
@@ -18,6 +18,10 @@
 #include "lib/lxc.h"
 #include "lib/nat46.h"
 
+#ifdef DROP_NOTIFY
+#include "lib/drop.h"
+#endif
+
 #ifndef DISABLE_PORT_MAP
 static inline void map_lxc_out(struct __sk_buff *skb, int off)
 {
@@ -172,7 +176,9 @@ __section_tail(CILIUM_MAP_JMP, SECLABEL) int handle_policy(struct __sk_buff *skb
 
 	policy = map_lookup_elem(&POLICY_MAP, &src_label);
 	if (unlikely(!policy)) {
-		// FIXME: Notify drop in perf ring buffer
+#ifdef DROP_NOTIFY
+		send_drop_notify(skb, src_label, SECLABEL, LXC_ID, ifindex);
+#endif
 		printk("Denied by policy! (%u->%u)\n", src_label, SECLABEL);
 		return TC_ACT_SHOT;
 	}

bpf/bpf_netdev.c
@@ -19,6 +19,10 @@
 #include "lib/nat46.h"
 #include "lib/arp.h"
 
+#ifdef DROP_NOTIFY
+#include "lib/drop.h"
+#endif
+
 static inline int is_node_subnet(const union v6addr *dst, const union v6addr *node_ip)
 {
 	int tmp;
@@ -172,6 +176,9 @@ __section_tail(CILIUM_MAP_JMP, SECLABEL) int handle_policy(struct __sk_buff *skb
 
 	policy = map_lookup_elem(&POLICY_MAP, &src_label);
 	if (!policy) {
+#ifdef DROP_NOTIFY
+		send_drop_notify(skb, src_label, SECLABEL, 0, ifindex);
+#endif
 		printk("Denied by policy!\n");
 		return TC_ACT_SHOT;
 	}

bpf/init.sh
@@ -20,6 +20,7 @@ function mac2array()
 
 # This directory was created by the daemon and contains the per container header file
 DIR="$PWD/globals"
+CLANG_OPTS="-D__NR_CPUS__=$(nproc) -O2 -target bpf -I$DIR -I."
 
 # Temporary fix until clang is properly installed and available in default PATH
 export PATH="/usr/local/clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04/bin/:$PATH"
@@ -56,8 +57,8 @@ echo "#define HOST_IFINDEX $HOST_IDX" >> /var/run/cilium/globals/node_config.h
 echo "#define HOST_IFINDEX_MAC { .addr = ${HOST_MAC}}" >> /var/run/cilium/globals/node_config.h
 
 ID=$(cilium policy get-id $HOST_ID 2> /dev/null)
-OPTS="-DHANDLE_NS -DFIXED_SRC_SECCTX=${ID} -DSECLABEL=${ID} -DPOLICY_MAP=cilium_policy_reserved_${ID}"
-clang -O2 $OPTS -target bpf -c $LIB/bpf_netdev.c -I$DIR -I. -o bpf_netdev_ns.o
+OPTS="$CLANG_OPTS -DHANDLE_NS -DFIXED_SRC_SECCTX=${ID} -DSECLABEL=${ID} -DPOLICY_MAP=cilium_policy_reserved_${ID}"
+clang $OPTS -c $LIB/bpf_netdev.c -o bpf_netdev_ns.o
 
 tc qdisc del dev $HOST_DEV2 clsact 2> /dev/null || true
 tc qdisc add dev $HOST_DEV2 clsact
@@ -82,7 +83,7 @@ if [ "$MODE" = "vxlan" -o "$MODE" = "geneve" ]; then
 	sed '/ENCAP_IFINDEX/d' /var/run/cilium/globals/node_config.h
 	echo "#define ENCAP_IFINDEX $ENCAP_IDX" >> /var/run/cilium/globals/node_config.h
 
-	clang -O2 -target bpf -c $LIB/bpf_overlay.c -I$DIR -I. -o bpf_overlay.o
+	clang $CLANG_OPTS -c $LIB/bpf_overlay.c -o bpf_overlay.o
 
 	tc qdisc del dev $ENCAP_DEV clsact 2> /dev/null || true
 	tc qdisc add dev $ENCAP_DEV clsact
@@ -98,7 +99,7 @@ elif [ "$MODE" = "direct" ]; then
 		tc qdisc del dev $DEV clsact 2> /dev/null || true
 		tc qdisc add dev $DEV clsact
 
-		clang -O2 -target bpf -c $LIB/bpf_netdev.c -I$DIR -I. -o bpf_netdev.o
+		clang $CLANG_OPTS -c $LIB/bpf_netdev.c -o bpf_netdev.o
 
 		tc filter add dev $DEV ingress bpf da obj bpf_netdev.o sec from-netdev
 	fi

bpf/join_ep.sh
@@ -10,11 +10,12 @@ echo "Join EP id=$ID ifname=$IFNAME"
 
 # This directory was created by the daemon and contains the per container header file
 DIR="$PWD/$ID"
+CLANG_OPTS="-D__NR_CPUS__=$(nproc) -O2 -target bpf -I/var/run/cilium/globals -I$DIR"
 
 # Temporary fix until clang is properly installed and available in default PATH
 export PATH="/usr/local/clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04/bin/:$PATH"
 
-clang -O2 -target bpf -c $LIB/bpf_lxc.c -I/var/run/cilium/globals -I$DIR -o $DIR/bpf_lxc.o
+clang $CLANG_OPTS -c $LIB/bpf_lxc.c -o $DIR/bpf_lxc.o
 
 tc qdisc replace dev $IFNAME clsact || true
 tc filter replace dev $IFNAME ingress bpf da obj $DIR/bpf_lxc.o sec from-container

bpf/lib/common.h
@@ -49,6 +49,28 @@ struct policy_entry {
 	__u64		bytes;
 };
 
+#define DROP_SAMPLE_LEN 64
+
+enum {
+	CILIUM_NOTIFY_UNSPEC,
+	CILIUM_NOTIFY_DROP,
+};
+
+#define COMMON_HDR \
+	__u8		type; \
+	__u8		subtype; \
+	__u16		flags; \
+
+struct drop_notify {
+	COMMON_HDR
+	__u32		len;
+	__u32		src_label;
+	__u32		dst_label;
+	__u32		dst_id;
+	__u32		dst_ifindex;
+	char		data[DROP_SAMPLE_LEN];
+};
+
 #ifndef BPF_F_PSEUDO_HDR
 # define BPF_F_PSEUDO_HDR                (1ULL << 4)
 #endif

commit 92bc34e5a2b4331f30c390029eb07af96d5e8a32

perf: Fix update of tail pointer
Tail pointer must be updated with the cache head pointer, otherwise
messages will get lost.

Fixes: 4b47250f6184d37a4c0853098425c9fc23116e80
Suggested-by: Daniel Borkmann <daniel@cilium.io>
Signed-off-by: Thomas Graf <thomas@cilium.io>

common/bpf/perf.go
@@ -42,7 +42,7 @@ struct event_sample {
 
 struct read_state {
 	size_t raw_size;
-	void *base, *begin, *end;
+	void *base, *begin, *end, *head;
 };
 
 int perf_event_read_init(int page_count, int page_size, void *_header, void *_state)
@@ -56,6 +56,7 @@ int perf_event_read_init(int page_count, int page_size, void *_header, void *_st
 	if (data_head == data_tail)
 		return 0;
 
+	state->head = data_head;
 	state->raw_size = page_count * page_size;
 	state->base  = ((uint8_t *)header) + page_size;
 	state->begin = state->base + data_tail % state->raw_size;
@@ -92,12 +93,13 @@ int perf_event_read(void *_state, void *buf, void *_msg)
 	return 1;
 }
 
-void perf_event_read_finish(void *_header)
+void perf_event_read_finish(void *_header, void *_state)
 {
 	volatile struct perf_event_mmap_page *header = _header;
+	struct read_state *state = _state;
 
 	__sync_synchronize();
-	header->data_tail = header->data_head;
+	header->data_tail = state->head;
 }
 
 void cast(void *ptr, void *_dst)