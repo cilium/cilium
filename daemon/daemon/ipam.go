cilium/cilium

commit 33aac404331b2b4ba1881fd024a16dcf543e63bd

Addressing: Preventing release of reserved addresses
Acked-by: Thomas Graf <thomas@cilium.io>
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon/ipam.go
@@ -164,6 +164,10 @@ func (d *Daemon) AllocateIP(ipamType ipam.IPAMType, options ipam.IPAMReq) (*ipam
 
 // ReleaseIP releases an IP address in use by the specific IPAM type.
 func (d *Daemon) ReleaseIP(ipamType ipam.IPAMType, options ipam.IPAMReq) error {
+	if options.IP != nil && d.isReservedAddress(*options.IP) {
+		fmt.Errorf("refusing to release reserved IP address: %s", options.IP)
+	}
+
 	switch ipamType {
 	case ipam.CNIIPAMType:
 		return releaseIPCNI(options, d.ipamConf)
@@ -223,4 +227,8 @@ func (d *Daemon) GetIPAMConf(ipamType ipam.IPAMType, options ipam.IPAMReq) (*ipa
 		return getIPAMConfLibnetwork(options, d.ipamConf)
 	}
 	return nil, fmt.Errorf("unknown IPAM Type %s", ipamType)
+}
+
+func (d *Daemon) isReservedAddress(ip net.IP) bool {
+	return d.conf.IPv4Enabled && d.conf.NodeAddress.IPv4Address.IP().Equal(ip)
 }

common/addressing/ip.go
@@ -97,6 +97,15 @@ func (ip CiliumIPv6) NodeIP() net.IP {
 	return nodeAddr
 }
 
+// Returns the host address from the node ID
+func (ip CiliumIPv6) HostIP() net.IP {
+	nodeAddr := make(net.IP, len(ip))
+	copy(nodeAddr, ip)
+	nodeAddr[14] = 0xff
+	nodeAddr[15] = 0xff
+	return nodeAddr
+}
+
 func (ip CiliumIPv6) IPNet(ones int) *net.IPNet {
 	return &net.IPNet{
 		IP:   ip.IP(),

daemon/daemon/daemon.go
@@ -184,9 +184,7 @@ func (d *Daemon) init() error {
 	}
 	fw := bufio.NewWriter(f)
 
-	hostIP := common.DupIP(d.conf.NodeAddress.IPv6Address.IP())
-	hostIP[14] = 0xff
-	hostIP[15] = 0xff
+	hostIP := d.conf.NodeAddress.IPv6Address.HostIP()
 
 	fmt.Fprintf(fw, ""+
 		"/*\n"+
@@ -249,9 +247,10 @@ func NewDaemon(c *Config) (*Daemon, error) {
 		return nil, fmt.Errorf("Configuration is nil")
 	}
 
-	ones, bits := addressing.StateIPv6Mask.Size()
-	maskPerIPAMType := ones + 1
-	ipamSubnets := net.IPNet{IP: c.NodeAddress.IPv6Address.IP(), Mask: net.CIDRMask(maskPerIPAMType, bits)}
+	ipamSubnets := net.IPNet{
+		IP:   c.NodeAddress.IPv6Address.IP(),
+		Mask: addressing.StateIPv6Mask,
+	}
 
 	ipamConf := &ipam.IPAMConfig{
 		IPAMConfig: hb.IPAMConfig{

commit caf62b77de65b17e898692bdcffd9047efcfcef4

daemon: Add missing return statement when handling error
daemon/daemon/ipam.go:168: result of fmt.Errorf call not used

Fixes: 33aac40433 ("Addressing: Preventing release of reserved addresses")
Acked-by: André Martins <andre@cilium.io>
Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/daemon/ipam.go
@@ -165,7 +165,7 @@ func (d *Daemon) AllocateIP(ipamType ipam.IPAMType, options ipam.IPAMReq) (*ipam
 // ReleaseIP releases an IP address in use by the specific IPAM type.
 func (d *Daemon) ReleaseIP(ipamType ipam.IPAMType, options ipam.IPAMReq) error {
 	if options.IP != nil && d.isReservedAddress(*options.IP) {
-		fmt.Errorf("refusing to release reserved IP address: %s", options.IP)
+		return fmt.Errorf("refusing to release reserved IP address: %s", options.IP)
 	}
 
 	switch ipamType {