cilium/cilium

commit 555cffd8bb4fb9fb771efd80afa1f8c29e312fc4

LB: Add k8s integration with our loadbalancer mechanism
This commit is still lacking the BPF maps integration which will be made
in the future along with some performance improvements.

Acked-by: Thomas Graf <thomas@cilium.io>
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon/k8s_watcher.go
@@ -17,12 +17,15 @@ package daemon
 
 import (
 	"encoding/json"
+	"net"
 	"net/http"
 	"time"
 
 	"github.com/cilium/cilium/common/types"
 
+	k8sAPI "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/apis/extensions/v1beta1"
+	k8sProxyConfig "k8s.io/kubernetes/pkg/proxy/config"
 	"k8s.io/kubernetes/pkg/watch"
 )
 
@@ -90,6 +93,23 @@ func (d *Daemon) EnableK8sWatcher(maxSeconds time.Duration) error {
 			}
 		}
 	}()
+
+	go func() {
+		time.Sleep(30 * time.Second)
+		serviceConfig := k8sProxyConfig.NewServiceConfig()
+		serviceConfig.RegisterHandler(d)
+
+		endpointsConfig := k8sProxyConfig.NewEndpointsConfig()
+		endpointsConfig.RegisterHandler(d)
+
+		k8sProxyConfig.NewSourceAPI(
+			d.k8sClient,
+			15*time.Minute,
+			serviceConfig.Channel("api"),
+			endpointsConfig.Channel("api"),
+		)
+	}()
+
 	return nil
 }
 
@@ -117,5 +137,173 @@ func (d *Daemon) processNPE(npwe networkPolicyWatchEvent) {
 			return
 		}
 		log.Infof("Kubernetes network policy successfully removed %+v", npwe.Object)
+	}
+}
+
+func (d *Daemon) OnServiceUpdate(allServices []k8sAPI.Service) {
+	log.Debugf("All Services %+v", allServices)
+	d.loadBalancer.ServicesMU.Lock()
+	defer d.loadBalancer.ServicesMU.Unlock()
+
+	newServices := map[types.ServiceNamespace]bool{}
+	for _, svc := range allServices {
+		if svc.Spec.Type != k8sAPI.ServiceTypeClusterIP {
+			log.Infof("Ignoring service %s/%s since its type is %s", svc.Namespace, svc.Name, svc.Spec.Type)
+			continue
+		}
+
+		svcns := types.ServiceNamespace{
+			Service:   svc.Name,
+			Namespace: svc.Namespace,
+		}
+		newServices[svcns] = true
+
+		si, ok := d.loadBalancer.Services[svcns]
+		clusterIP := net.ParseIP(svc.Spec.ClusterIP)
+		if !ok {
+			si = types.NewServiceInfo(clusterIP)
+			d.loadBalancer.Services[svcns] = si
+		} else {
+			si.IP = clusterIP
+		}
+
+		for _, port := range svc.Spec.Ports {
+			p, err := types.NewLBSvcPort(types.L4Type(port.Protocol), uint16(port.Port))
+			if err != nil {
+				log.Errorf("Unable to add service port %v: %s", port, err)
+			}
+			if _, ok := si.Ports[types.LBPortName(port.Name)]; !ok {
+				si.Ports[types.LBPortName(port.Name)] = p
+			}
+		}
+		log.Debugf("Got service %+v", si)
+	}
+
+	// Cleaning old services
+	for svc := range d.loadBalancer.Services {
+		if !newServices[svc] {
+			delete(d.loadBalancer.Services, svc)
+		}
+	}
+
+	d.syncLB()
+}
+
+func (d *Daemon) OnEndpointsUpdate(endpoints []k8sAPI.Endpoints) {
+	log.Debugf("All Endpoints %+v", endpoints)
+	d.loadBalancer.ServicesMU.Lock()
+	defer d.loadBalancer.ServicesMU.Unlock()
+
+	newEndpoints := map[types.ServiceNamespace]bool{}
+	for _, ep := range endpoints {
+		svcns := types.ServiceNamespace{
+			Service:   ep.Name,
+			Namespace: ep.Namespace,
+		}
+		newEndpoints[svcns] = true
+
+		svcEp, ok := d.loadBalancer.Endpoints[svcns]
+		if !ok {
+			svcEp = types.NewServiceEndpoint()
+			d.loadBalancer.Endpoints[svcns] = svcEp
+		}
+
+		newIPs := map[string]bool{}
+		newLBPorts := map[types.LBPortName]bool{}
+		for _, sub := range ep.Subsets {
+			for _, addr := range sub.Addresses {
+				svcEp.IPs[addr.IP] = true
+				newIPs[addr.IP] = true
+			}
+			for _, port := range sub.Ports {
+				lbPort, err := types.NewLBPort(types.L4Type(port.Protocol), uint16(port.Port))
+				if err != nil {
+					log.Errorf("Error while creating a new LB Port: %s", err)
+					continue
+				}
+				svcEp.Ports[types.LBPortName(port.Name)] = lbPort
+				newLBPorts[types.LBPortName(port.Name)] = true
+			}
+		}
+
+		// Cleaning old IPs
+		for ip := range svcEp.IPs {
+			if !newIPs[ip] {
+				delete(svcEp.IPs, ip)
+			}
+		}
+
+		// Cleaning old service ports
+		for portName := range svcEp.Ports {
+			if !newLBPorts[portName] {
+				delete(svcEp.Ports, portName)
+			}
+		}
+
+	}
+
+	// Cleaning old endpoints
+	for ep := range d.loadBalancer.Endpoints {
+		if !newEndpoints[ep] {
+			delete(d.loadBalancer.Endpoints, ep)
+		}
+	}
+
+	d.syncLB()
+}
+
+func (d *Daemon) syncLB() {
+	for svc, svcInfo := range d.loadBalancer.Services {
+		se, ok := d.loadBalancer.Endpoints[svc]
+		if !ok {
+			continue
+		}
+
+		for svcPortName, svcPort := range svcInfo.Ports {
+			epPort, ok := se.Ports[svcPortName]
+			if !ok {
+				continue
+			}
+			if svcPort.ServiceID == 0 {
+				svcl4 := types.ServiceL4{
+					IP:   svcInfo.IP,
+					Port: svcPort.Port,
+				}
+				svcl4ID, err := d.PutServiceL4(svcl4)
+				if err != nil {
+					log.Errorf("Error while getting a new service ID: %s. Ignoring service %v...", err, svcl4)
+					continue
+				}
+				svcPort.ServiceID = svcl4ID.ServiceID
+			}
+			isServerPresent := false
+
+			serverIndex := 0
+
+			if len(se.IPs) != 0 {
+				log.Debugf("# cilium lb update-service %s %d %d %d %d %s %d",
+					svcInfo.IP, svcPort.Port, serverIndex, len(svcInfo.Ports), svcPort.ServiceID,
+					"::", 0)
+			}
+			for epIP := range se.IPs {
+				serverIndex++
+				log.Debugf("# cilium lb update-service %s %d %d %d %d %s %d",
+					svcInfo.IP, svcPort.Port, serverIndex, len(svcInfo.Ports), svcPort.ServiceID,
+					epIP, epPort.Port)
+
+				if !isServerPresent {
+					d.ipamConf.AllocatorMutex.RLock()
+					if d.ipamConf.IPv6Allocator.Has(net.ParseIP(epIP)) ||
+						d.ipamConf.IPv4Allocator.Has(net.ParseIP(epIP)) {
+						isServerPresent = true
+					}
+					d.ipamConf.AllocatorMutex.RUnlock()
+				}
+			}
+			if isServerPresent {
+				log.Debugf("# cilium lb update-state %d %s %d",
+					svcPort.ServiceID, svcInfo.IP, svcPort.Port)
+			}
+		}
 	}
 }

common/const.go
@@ -55,6 +55,16 @@ const (
 	MaxSetOfLabels = uint32(0xFFFF)
 	// FirstFreeLabelID is the first ID for which the labels should be assigned.
 	FirstFreeLabelID = uint32(256)
+	// LastFreeServiceIDKeyPath is the path where the Last free UUID is stored in consul.
+	LastFreeServiceIDKeyPath = OperationalPath + "/Services/LastUUID"
+	// ServiceKeyPath is the base path where services are stored in consul.
+	ServicesKeyPath = OperationalPath + "/Services/SHA256SUMServices"
+	// ServiceIDKeyPath is the base path where the IDs are stored in consul.
+	ServiceIDKeyPath = OperationalPath + "/Services/IDs"
+	// MaxSetOfServiceID is maximum number of set of service IDs that can be stored in consul.
+	MaxSetOfServiceID = uint32(0xFFFF)
+	// FirstFreeServiceID is the first ID for which the services should be assigned.
+	FirstFreeServiceID = uint32(1)
 
 	// Miscellaneous dedicated constants
 

common/ipam/ipam.go
@@ -48,7 +48,7 @@ type IPAMConfig struct {
 	IPAMConfig     hb.IPAMConfig
 	IPv6Allocator  *ipallocator.Range
 	IPv4Allocator  *ipallocator.Range
-	AllocatorMutex sync.Mutex
+	AllocatorMutex sync.RWMutex
 }
 
 // IPAMReq is used for IPAM request operation.

common/types/loadbalancer.go
@@ -1,0 +1,114 @@
+//
+// Copyright 2016 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package types
+
+import (
+	"crypto/sha512"
+	"encoding/json"
+	"fmt"
+	"net"
+	"sync"
+)
+
+const (
+	TCP = L4Type("TCP")
+	UDP = L4Type("UDP")
+)
+
+type L4Type string
+
+type LBPortName string
+
+type ServiceID uint16
+
+type LoadBalancer struct {
+	ServicesMU sync.RWMutex
+	Services   map[ServiceNamespace]*ServiceInfo
+	Endpoints  map[ServiceNamespace]*ServiceEndpoint
+}
+
+type ServiceNamespace struct {
+	Service   string
+	Namespace string
+}
+
+type ServiceInfo struct {
+	IP    net.IP
+	Ports map[LBPortName]*LBSvcPort
+}
+
+func NewServiceInfo(ip net.IP) *ServiceInfo {
+	return &ServiceInfo{
+		IP:    ip,
+		Ports: map[LBPortName]*LBSvcPort{},
+	}
+}
+
+type ServiceEndpoint struct {
+	// TODO: Replace bool for time.Time so we know last time the service endpoint was seen?
+	IPs   map[string]bool
+	Ports map[LBPortName]*LBPort
+}
+
+func NewServiceEndpoint() *ServiceEndpoint {
+	return &ServiceEndpoint{
+		IPs:   map[string]bool{},
+		Ports: map[LBPortName]*LBPort{},
+	}
+}
+
+type LBPort struct {
+	Protocol L4Type
+	Port     uint16
+}
+
+func NewLBPort(protocol L4Type, number uint16) (*LBPort, error) {
+	switch protocol {
+	case TCP, UDP:
+	default:
+		return nil, fmt.Errorf("unknown protocol type %s", protocol)
+	}
+	return &LBPort{Protocol: protocol, Port: number}, nil
+}
+
+type LBSvcPort struct {
+	*LBPort
+	ServiceID ServiceID
+}
+
+func NewLBSvcPort(protocol L4Type, number uint16) (*LBSvcPort, error) {
+	lbport, err := NewLBPort(protocol, number)
+	return &LBSvcPort{LBPort: lbport}, err
+}
+
+type ServiceL4 struct {
+	IP   net.IP
+	Port uint16
+}
+
+type ServiceL4ID struct {
+	ServiceL4
+	ServiceID
+}
+
+// SHA256Sum calculates ServiceL4's internal SHA256Sum.
+func (sl4 ServiceL4) SHA256Sum() (string, error) {
+	sha := sha512.New512_256()
+	if err := json.NewEncoder(sha).Encode(sl4); err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", sha.Sum(nil)), nil
+}

daemon/daemon/daemon.go
@@ -62,6 +62,7 @@ type Daemon struct {
 	endpointsLearningMU       sync.RWMutex
 	endpointsLearningRegister chan types.LearningLabel
 	dockerClient              *dClient.Client
+	loadBalancer              *types.LoadBalancer
 	k8sClient                 *k8sClient.Client
 	conf                      *Config
 	policyTree                types.PolicyTree
@@ -296,6 +297,11 @@ func NewDaemon(c *Config) (*Daemon, error) {
 
 	rootNode.Root.Path()
 
+	lb := &types.LoadBalancer{
+		Services:  map[types.ServiceNamespace]*types.ServiceInfo{},
+		Endpoints: map[types.ServiceNamespace]*types.ServiceEndpoint{},
+	}
+
 	d := Daemon{
 		conf:                      c,
 		ipamConf:                  ipamConf,
@@ -308,6 +314,7 @@ func NewDaemon(c *Config) (*Daemon, error) {
 		endpointsDockerEP:         make(map[string]*types.Endpoint),
 		endpointsLearning:         make(map[uint16]types.LearningLabel),
 		endpointsLearningRegister: make(chan types.LearningLabel, 1),
+		loadBalancer:              lb,
 		cacheIteration:            1,
 		reservedConsumables:       make([]*types.Consumable, 0),
 		policyTree:                rootNode,

daemon/daemon/services.go
@@ -1,0 +1,143 @@
+//
+// Copyright 2016 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package daemon
+
+import (
+	"encoding/json"
+	"path"
+	"strconv"
+
+	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/common/types"
+)
+
+func (d *Daemon) updateServiceL4IDRef(svcl4ID types.ServiceL4ID) error {
+	key := path.Join(common.ServiceIDKeyPath, strconv.FormatUint(uint64(svcl4ID.ServiceID), 10))
+	return d.kvClient.SetValue(key, svcl4ID)
+}
+
+// gasNewServiceL4ID gets and sets a New ServiceL4 ID.
+func (d *Daemon) gasNewServiceL4ID(svcl4 *types.ServiceL4ID) error {
+	baseID, err := d.GetMaxServiceID()
+	if err != nil {
+		return err
+	}
+
+	return d.kvClient.GASNewServiceL4ID(common.ServiceIDKeyPath, baseID, svcl4)
+}
+
+// PutServiceL4 stores the given service in the kvstore and returns the ServiceL4ID
+// created for the given servicel4.
+func (d *Daemon) PutServiceL4(svcl4 types.ServiceL4) (*types.ServiceL4ID, error) {
+	log.Debugf("Resolving service %+v of %s", svcl4)
+
+	// Retrieve unique SHA256Sum for service
+	sha256Sum, err := svcl4.SHA256Sum()
+	if err != nil {
+		return nil, err
+	}
+	svcPath := path.Join(common.ServicesKeyPath, sha256Sum)
+
+	// Lock that sha256Sum
+	lockKey, err := d.kvClient.LockPath(svcPath)
+	if err != nil {
+		return nil, err
+	}
+	defer lockKey.Unlock()
+
+	// After lock complete, get svc's path
+	rmsg, err := d.kvClient.GetValue(svcPath)
+	if err != nil {
+		return nil, err
+	}
+
+	sl4KV := types.ServiceL4ID{}
+	if rmsg == nil {
+		sl4KV.ServiceL4 = svcl4
+		if err := d.gasNewServiceL4ID(&sl4KV); err != nil {
+			return nil, err
+		}
+		err = d.kvClient.SetValue(svcPath, sl4KV)
+	} else {
+		err = json.Unmarshal(rmsg, &sl4KV)
+	}
+
+	return &sl4KV, err
+}
+
+func (d *Daemon) getServiceL4ID(keyPath string) (*types.ServiceL4ID, error) {
+	rmsg, err := d.kvClient.GetValue(keyPath)
+	if err != nil {
+		return nil, err
+	}
+	if rmsg == nil {
+		return nil, nil
+	}
+
+	var svcl4ID types.ServiceL4ID
+	err = json.Unmarshal(rmsg, &svcl4ID)
+	return &svcl4ID, err
+}
+
+// GetServiceL4ID returns the ServiceL4ID that belongs to the given id.
+func (d *Daemon) GetServiceL4ID(id uint32) (*types.ServiceL4ID, error) {
+	strID := strconv.FormatUint(uint64(id), 10)
+	return d.getServiceL4ID(path.Join(common.ServiceIDKeyPath, strID))
+}
+
+// GetServiceL4IDBySHA256 returns the ServiceL4ID that have the given SHA256SUM.
+func (d *Daemon) GetServiceL4IDBySHA256(sha256sum string) (*types.ServiceL4ID, error) {
+	return d.getServiceL4ID(path.Join(common.ServicesKeyPath, sha256sum))
+}
+
+// DeleteServiceL4IDByUUID deletes the ServiceL4ID belonging to the given id.
+func (d *Daemon) DeleteServiceL4IDByUUID(id uint32) error {
+	svcl4ID, err := d.GetServiceL4ID(id)
+	if err != nil {
+		return err
+	}
+	if svcl4ID == nil {
+		return nil
+	}
+	sha256sum, err := svcl4ID.SHA256Sum()
+	if err != nil {
+		return err
+	}
+
+	return d.DeleteServiceL4IDBySHA256(sha256sum)
+}
+
+// DeleteServiceL4IDBySHA256 deletes the ServiceL4ID that belong to the serviceL4ID'
+// sha256Sum.
+func (d *Daemon) DeleteServiceL4IDBySHA256(sha256Sum string) error {
+	if sha256Sum == "" {
+		return nil
+	}
+	svcPath := path.Join(common.ServicesKeyPath, sha256Sum)
+	// Lock that sha256Sum
+	lockKey, err := d.kvClient.LockPath(svcPath)
+	if err != nil {
+		return err
+	}
+	defer lockKey.Unlock()
+
+	return d.kvClient.SetValue(svcPath, types.ServiceL4ID{ServiceID: 0})
+}
+
+// GetMaxServiceID returns the maximum possible free UUID stored in the kvstore.
+func (d *Daemon) GetMaxServiceID() (uint32, error) {
+	return d.kvClient.GetMaxID(common.LastFreeServiceIDKeyPath, common.FirstFreeServiceID)
+}

examples/kubernetes/kubernetes-v1.3.0.patch
@@ -12,7 +12,7 @@ index 6be0ceb..41b7a72 100755
        return 0
      fi
 diff --git a/hack/local-up-cluster.sh b/hack/local-up-cluster.sh
-index 21c8ce0..b386256 100755
+index 21c8ce0..d2a3fde 100755
 --- a/hack/local-up-cluster.sh
 +++ b/hack/local-up-cluster.sh
 @@ -106,6 +106,7 @@ set +e
@@ -32,7 +32,7 @@ index 21c8ce0..b386256 100755
      if [ ! $? -eq 0 ]; then
          echo "API SERVER port is free, proceeding..."
      else
-@@ -276,9 +277,10 @@ function start_apiserver {
+@@ -276,11 +277,12 @@ function start_apiserver {
        --service-account-key-file="${SERVICE_ACCOUNT_KEY}" \
        --service-account-lookup="${SERVICE_ACCOUNT_LOOKUP}" \
        --admission-control="${ADMISSION_CONTROL}" \
@@ -43,8 +43,11 @@ index 21c8ce0..b386256 100755
 -      --advertise-address="${API_HOST}" \
 +      --advertise-address="${API_HOST_IP}" \
        --etcd-servers="http://${ETCD_HOST}:${ETCD_PORT}" \
-       --service-cluster-ip-range="10.0.0.0/24" \
+-      --service-cluster-ip-range="10.0.0.0/24" \
++      --service-cluster-ip-range="f00d:1::/112" \
        --cloud-provider="${CLOUD_PROVIDER}" \
+       --cors-allowed-origins="${API_CORS_ALLOWED_ORIGINS}" >"${APISERVER_LOG}" 2>&1 &
+     APISERVER_PID=$!
 @@ -327,7 +329,7 @@ function start_kubelet {
           fi
        fi
@@ -54,3 +57,20 @@ index 21c8ce0..b386256 100755
           dns_args="--cluster-dns=${DNS_SERVER_IP} --cluster-domain=${DNS_DOMAIN}"
        else
           # To start a private DNS server set ENABLE_CLUSTER_DNS and
+@@ -391,11 +393,11 @@ function start_kubelet {
+ 
+ function start_kubeproxy {
+     PROXY_LOG=/tmp/kube-proxy.log
+-    sudo -E "${GO_OUT}/hyperkube" proxy \
+-      --v=${LOG_LEVEL} \
+-      --hostname-override="${HOSTNAME_OVERRIDE}" \
+-      --master="http://${API_HOST}:${API_PORT}" >"${PROXY_LOG}" 2>&1 &
+-    PROXY_PID=$!
++    #sudo -E "${GO_OUT}/hyperkube" proxy \
++    #  --v=${LOG_LEVEL} \
++    #  --hostname-override="${HOSTNAME_OVERRIDE}" \
++    #  --master="http://${API_HOST}:${API_PORT}" >"${PROXY_LOG}" 2>&1 &
++    #PROXY_PID=$!
+ 
+     SCHEDULER_LOG=/tmp/kube-scheduler.log
+     sudo -E "${GO_OUT}/hyperkube" scheduler \

pkg/kvstore/consul.go
@@ -276,6 +276,68 @@ func (c *ConsulClient) GASNewSecLabelID(basePath string, baseID uint32, secCtxLa
 	}
 }
 
+func (c *ConsulClient) setMaxServiceL4ID(maxID uint32) error {
+	return c.SetMaxID(common.LastFreeServiceIDKeyPath, common.FirstFreeServiceID, maxID)
+}
+
+func (c *ConsulClient) GASNewServiceL4ID(basePath string, baseID uint32, sl4 *types.ServiceL4ID) error {
+
+	setID2ServiceL4 := func(lockPair *consulAPI.KVPair) error {
+		defer c.KV().Release(lockPair, nil)
+		sl4.ServiceID = types.ServiceID(baseID)
+		keyPath := path.Join(basePath, strconv.FormatUint(uint64(sl4.ServiceID), 10))
+		if err := c.SetValue(keyPath, sl4); err != nil {
+			return err
+		}
+		return c.setMaxServiceL4ID(baseID + 1)
+	}
+
+	session, _, err := c.Session().CreateNoChecks(nil, nil)
+	if err != nil {
+		return err
+	}
+
+	beginning := baseID
+	for {
+		log.Debugf("Trying to aquire a new free ID %d", baseID)
+		keyPath := path.Join(basePath, strconv.FormatUint(uint64(baseID), 10))
+
+		lockPair := &consulAPI.KVPair{Key: GetLockPath(keyPath), Session: session}
+		acq, _, err := c.KV().Acquire(lockPair, nil)
+		if err != nil {
+			return err
+		}
+
+		if acq {
+			svcKey, _, err := c.KV().Get(keyPath, nil)
+			if err != nil {
+				c.KV().Release(lockPair, nil)
+				return err
+			}
+			if svcKey == nil {
+				return setID2ServiceL4(lockPair)
+			}
+			var consulServiceL4ID types.ServiceL4ID
+			if err := json.Unmarshal(svcKey.Value, &consulServiceL4ID); err != nil {
+				c.KV().Release(lockPair, nil)
+				return err
+			}
+			if consulServiceL4ID.ServiceID == 0 {
+				log.Infof("Recycling Service ID %d", baseID)
+				return setID2ServiceL4(lockPair)
+			}
+			c.KV().Release(lockPair, nil)
+		}
+		baseID++
+		if baseID > common.MaxSetOfServiceID {
+			baseID = common.FirstFreeServiceID
+		}
+		if beginning == baseID {
+			return fmt.Errorf("reached maximum set of serviceIDs available.")
+		}
+	}
+}
+
 // GetWatcher watches for kvstore changes in the given key. Triggers the returned channel
 // every time the key path is changed.
 func (c *ConsulClient) GetWatcher(key string, timeSleep time.Duration) <-chan []uint32 {

commit 57e084c0dc1d8b98f0f0f68577e3206f635cabeb

k8s_watcher: fixed nil pointer when using ipv4
Fixes: 555cffd8bb ("LB: Add k8s integration with our loadbalancer mechanism")
Acked-by: Thomas Graf <thomas@cilium.io>
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon/k8s_watcher.go
@@ -99,7 +99,6 @@ func (d *Daemon) EnableK8sWatcher(maxSeconds time.Duration) error {
 	}()
 
 	go func() {
-		time.Sleep(30 * time.Second)
 		serviceConfig := k8sProxyConfig.NewServiceConfig()
 		serviceConfig.RegisterHandler(d)
 
@@ -296,12 +295,15 @@ func (d *Daemon) syncLB() {
 					epIP, epPort.Port)
 
 				if !isServerPresent {
+					d.conf.OptsMU.RLock()
 					d.ipamConf.AllocatorMutex.RLock()
 					if d.ipamConf.IPv6Allocator.Has(net.ParseIP(epIP)) ||
-						d.ipamConf.IPv4Allocator.Has(net.ParseIP(epIP)) {
+						(d.conf.IPv4Enabled &&
+							d.ipamConf.IPv4Allocator.Has(net.ParseIP(epIP))) {
 						isServerPresent = true
 					}
 					d.ipamConf.AllocatorMutex.RUnlock()
+					d.conf.OptsMU.RUnlock()
 				}
 			}
 			if isServerPresent {