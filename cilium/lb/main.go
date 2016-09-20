cilium/cilium

commit d4fc3e2d677fc57bbfcc39f802037c383fb72032

bpf: lb: Add reverse NAT translation via CT state
After an outgoing connection has been loadbalanced its reverse
NAT index is stored in the connection tracking table to be
looked up for reply and related packets.

Also makes the loadbalancer configurable via LB_L3 and LB_L4
defines to limit key extraction and logic to either L3, L4 or
both.

Acked-by: Andre Martins <andre@cilium.io>
Acked-by: Madhu Challa <madhu@cilium.io>
Signed-off-by: Thomas Graf <thomas@cilium.io>

cilium/lb/main.go
@@ -22,7 +22,6 @@ import (
 	"strconv"
 
 	"github.com/cilium/cilium/bpf/lbmap"
-	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/common/bpf"
 
 	"github.com/codegangsta/cli"
@@ -254,25 +253,13 @@ func parseServiceValue(ctx *cli.Context, ipv6 bool, firstArg int) lbmap.ServiceV
 	}
 
 	if ipv6 {
-		iv6, err := addressing.NewCiliumIPv6(address)
-		if err != nil {
+		if target.To4() != nil {
 			fmt.Fprintf(os.Stderr, "Expecting an IPv6 address, got: %s\n", address)
 			os.Exit(1)
 		}
 
-		revNat := parseUint16(ctx, firstArg+1)
-
-		if revNat != 0 {
-			if iv6.State() != 0 {
-				fmt.Fprintf(os.Stderr, "Error: Address has non-zero state bits.\n")
-				os.Exit(1)
-			}
-
-			iv6.SetState(revNat)
-		}
-
-		return lbmap.NewService6Value(parseUint16(ctx, firstArg), iv6.IP(),
-			parseUint16(ctx, firstArg+3))
+		return lbmap.NewService6Value(parseUint16(ctx, firstArg), target,
+			parseUint16(ctx, firstArg+3), parseUint16(ctx, firstArg+1))
 	} else {
 		if target.To4() == nil {
 			fmt.Fprintf(os.Stderr, "Expecting an IPv4 address, got: %s\n", address)

bpf/Makefile
@@ -18,8 +18,15 @@ bpf_netdev.o:
 bpf_overlay.o:
 	clang ${CLANG_FLAGS} -c bpf_overlay.c -o $@
 
+LB_OPTIONS = \
+	 \
+	-DLB_L3 \
+	-DLB_L4 \
+	-DLB_L3 -DLB_L4
+
 bpf_lb.o:
-	clang ${CLANG_FLAGS} -c bpf_lb.c -o $@
+	$(foreach OPTS,$(LB_OPTIONS), \
+		clang ${OPTS} ${CLANG_FLAGS} -c bpf_lb.c -o $@;)
 
 clean:
 	rm -fr *.o

bpf/bpf_lb.c
@@ -30,6 +30,7 @@
  *  - LB_DISABLE_IPV4 - Ignore IPv4 packets
  *  - LB_DISABLE_IPV6 - Ignore IPv6 packets
  *  - LB_REDIRECT     - Redirect to an ifindex
+ *  - LB_L4           - Enable L4 matching and mapping
  */
 
 #include <netdev_config.h>
@@ -73,8 +74,10 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 	ipv6_addr_copy(&key.address, dst);
 	l3_off = ETH_HLEN;
 	l4_off = ETH_HLEN + ipv6_hdrlen(skb, ETH_HLEN, &nexthdr);
+	csum_l4_offset_and_flags(nexthdr, &csum_off);
 
-	ret = extract_l4_port(skb, nexthdr, l4_off, &csum_off, &key.dport);
+#ifdef LB_L4
+	ret = extract_l4_port(skb, nexthdr, l4_off, &key.dport);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_UNKNOWN_L4) {
 			/* Pass unknown L4 to stack */
@@ -82,6 +85,7 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 		} else
 			return ret;
 	}
+#endif
 
 	svc = lb6_lookup_service(skb, &key);
 	if (svc == NULL) {
@@ -94,6 +98,9 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 		return DROP_NO_SERVICE;
 
 	ipv6_addr_copy(&new_dst, &svc->target);
+	if (svc->rev_nat_index)
+		new_dst.p4 |= svc->rev_nat_index;
+
 	ret = lb6_xlate(skb, &new_dst, nexthdr, l3_off, l4_off, &csum_off, &key, svc);
 	if (IS_ERR(ret))
 		return ret;
@@ -125,8 +132,10 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	key.address = ip->daddr;
 	l3_off = ETH_HLEN;
 	l4_off = ETH_HLEN + ipv4_hdrlen(ip);
+	csum_l4_offset_and_flags(nexthdr, &csum_off);
 
-	ret = extract_l4_port(skb, nexthdr, l4_off, &csum_off, &key.dport);
+#ifdef LB_L4
+	ret = extract_l4_port(skb, nexthdr, l4_off, &key.dport);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_UNKNOWN_L4) {
 			/* Pass unknown L4 to stack */
@@ -134,6 +143,7 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 		} else
 			return ret;
 	}
+#endif
 
 	svc = lb4_lookup_service(skb, &key);
 	if (svc == NULL) {

bpf/bpf_lxc.c
@@ -106,11 +106,11 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 {
 	union macaddr router_mac = NODE_MAC;
 	union v6addr host_ip = HOST_IP;
-	int do_nat46 = 0, ret, l4_off, skip_service = 0;
+	int do_nat46 = 0, ret, l4_off;
 	struct csum_offset csum_off = {};
 	struct lb6_service *svc;
 	struct lb6_key key = {};
-	__u16 rev_nat_index = 0;
+	__u16 rev_nat_index = 0, nat_index = 0;
 
 	if (unlikely(!valid_src_mac(eth)))
 		return DROP_INVALID_SMAC;
@@ -138,11 +138,26 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	ret = lb6_extract_key(skb, tuple, l4_off, &key, &csum_off);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_UNKNOWN_L4)
-			skip_service = 1;
+			goto skip_service_lookup;
 		else
 			return ret;
 	}
 
+	/*
+	 * Check if the destination address is among the address that should be
+	 * load balanced. This operation is performed before we go through the
+	 * connection tracker to allow storing the reverse nat index in the CT
+	 * entry for destination endpoints where we can't encode the state in the
+	 * address.
+	 */
+	if ((svc = lb6_lookup_service(skb, &key)) != NULL) {
+		ret = lb6_local(skb, l3_off, l4_off, &csum_off, &key, tuple, svc,
+				&nat_index);
+		if (IS_ERR(ret))
+			return ret;
+	}
+
+skip_service_lookup:
 	/* Port reverse mapping can never happen when we balanced to a service */
 	ret = map_lxc_out(skb, l4_off, tuple->nexthdr);
 	if (IS_ERR(ret))
@@ -159,7 +174,12 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 
 	switch (ret) {
 	case CT_NEW:
-		ret = ct_create6(&CT_MAP6, tuple, skb, 0, 0);
+		/* New connection implies that rev_nat_index remains untouched
+		 * to the index provided by the loadbalancer (if it applied).
+		 * Create a CT entry which allows to track replies and to
+		 * reverse NAT.
+		 */
+		ret = ct_create6(&CT_MAP6, tuple, skb, 0, nat_index);
 		if (IS_ERR(ret))
 			return ret;
 		break;
@@ -170,24 +190,20 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	case CT_RELATED:
 	case CT_REPLY:
 		skb->cb[CB_POLICY] = POLICY_SKIP;
+
+		if (rev_nat_index) {
+			ret = lb6_rev_nat(skb, l4_off, &csum_off, rev_nat_index, tuple, 0);
+			if (IS_ERR(ret))
+				return ret;
+
+			/* A reverse translate packet is always allowed except for delivery
+			 * on the local node in which case this marking is cleared again. */
+			policy_mark_skip(skb);
+		}
 		break;
 
 	default:
 		return DROP_POLICY;
-	}
-
-	if (!skip_service && (svc = lb6_lookup_service(skb, &key)) != NULL) {
-		ret = lb6_local(skb, l3_off, l4_off, &csum_off, &key, tuple, svc);
-		if (IS_ERR(ret))
-			return ret;
-	} else if (rev_nat_index) {
-		ret = lb6_dsr_snat(skb, l4_off, &csum_off, rev_nat_index, tuple);
-		if (IS_ERR(ret))
-			return ret;
-
-		/* A reverse translate packet is always allowed except for delivery
-		 * on the local node in which case this marking is cleared again. */
-		policy_mark_skip(skb);
 	}
 
 	/* Check if destination is within our cluster prefix */
@@ -336,11 +352,10 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	struct iphdr *ip4 = data + ETH_HLEN;
 	struct ethhdr *eth = data;
 	int ret, l3_off = ETH_HLEN, l4_off;
-	int skip_service = 0;
 	struct csum_offset csum_off = {};
 	struct lb4_service *svc;
 	struct lb4_key key = {};
-	__u16 rev_nat_index = 0;
+	__u16 rev_nat_index = 0, nat_index = 0;
 
 	if (data + sizeof(*ip4) + ETH_HLEN > data_end)
 		return DROP_INVALID;
@@ -372,10 +387,19 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	ret = lb4_extract_key(skb, &tuple, l4_off, &key, &csum_off);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_UNKNOWN_L4)
-			skip_service = 1;
+			goto skip_service_lookup;
 		else
 			return ret;
 	}
+
+	if ((svc = lb4_lookup_service(skb, &key)) != NULL) {
+		ret = lb4_local(skb, l3_off, l4_off, &csum_off,
+				&key, &tuple, svc, &nat_index);
+		if (IS_ERR(ret))
+			return ret;
+	}
+
+skip_service_lookup:
 	ret = map_lxc_out(skb, l4_off, tuple.nexthdr);
 	if (IS_ERR(ret))
 		return ret;
@@ -392,7 +416,12 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 
 	switch (ret) {
 	case CT_NEW:
-		ret = ct_create4(&CT_MAP4, &tuple, skb, 0, 0);
+		/* New connection implies that rev_nat_index remains untouched
+		 * to the index provided by the loadbalancer (if it applied).
+		 * Create a CT entry which allows to track replies and to
+		 * reverse NAT.
+		 */
+		ret = ct_create4(&CT_MAP4, &tuple, skb, 0, nat_index);
 		if (IS_ERR(ret))
 			return ret;
 		break;
@@ -403,25 +432,21 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	case CT_RELATED:
 	case CT_REPLY:
 		skb->cb[CB_POLICY] = POLICY_SKIP;
+
+		if (rev_nat_index) {
+			ret = lb4_rev_nat(skb, l3_off, l4_off, &csum_off,
+					  rev_nat_index, &tuple, 0);
+			if (IS_ERR(ret))
+				return ret;
+
+			/* A reverse translate packet is always allowed except for delivery
+			 * on the local node in which case this marking is cleared again. */
+			policy_mark_skip(skb);
+		}
 		break;
 
 	default:
 		return DROP_POLICY;
-	}
-
-	if (!skip_service && (svc = lb4_lookup_service(skb, &key)) != NULL) {
-		ret = lb4_local(skb, l3_off, l4_off, &csum_off,
-				&key, &tuple, svc);
-		if (IS_ERR(ret))
-			return ret;
-	} else if (rev_nat_index) {
-		ret = lb4_dsr_snat(skb, l3_off, l4_off, &csum_off, rev_nat_index, &tuple);
-		if (IS_ERR(ret))
-			return ret;
-
-		/* A reverse translate packet is always allowed except for delivery
-		 * on the local node in which case this marking is cleared again. */
-		policy_mark_skip(skb);
 	}
 
 	/* After L4 write in port mapping: revalidate for direct packet access */
@@ -581,8 +606,10 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	void *data = (void *) (long) skb->data;
 	void *data_end = (void *) (long) skb->data_end;
 	struct ipv6hdr *ip6 = data + ETH_HLEN;
+	struct csum_offset off = {};
 	int ret, l4_off, verdict;
 	__u32 rev_nat_index;
+	__u16 src_rev_nat = 0;
 
 	if (data + sizeof(struct ipv6hdr) + ETH_HLEN > data_end)
 		return DROP_INVALID;
@@ -591,11 +618,11 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	tuple.nexthdr = ip6->nexthdr;
 	ipv6_addr_copy(&tuple.addr, (union v6addr *) &ip6->saddr);
 	l4_off = ETH_HLEN + ipv6_hdrlen(skb, ETH_HLEN, &tuple.nexthdr);
+	csum_l4_offset_and_flags(tuple.nexthdr, &off);
 
 	/* derive reverse NAT index and zero it. */
 	rev_nat_index = ip6->daddr.s6_addr32[3] & 0xFFFF;
 	if (rev_nat_index) {
-		struct csum_offset off = {};
 		union v6addr dip;
 
 		ipv6_addr_copy(&dip, (union v6addr *) &ip6->daddr);
@@ -604,7 +631,6 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 		if (IS_ERR(ret))
 			return DROP_WRITE_ERROR;
 
-		csum_l4_offset_and_flags(tuple.nexthdr, &off);
 		if (off.offset) {
 			__u32 zero_nat = 0;
 			__be32 sum = csum_diff(&rev_nat_index, 4, &zero_nat, 4, 0);
@@ -613,9 +639,17 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 		}
 	}
 
-	ret = ct_lookup6(&CT_MAP6, &tuple, skb, l4_off, SECLABEL, 1, NULL);
+	ret = ct_lookup6(&CT_MAP6, &tuple, skb, l4_off, SECLABEL, 1, &src_rev_nat);
 	if (ret < 0)
 		return ret;
+
+	if (unlikely(src_rev_nat)) {
+		int ret2;
+
+		ret2 = lb6_rev_nat(skb, l4_off, &off, src_rev_nat, &tuple, 0);
+		if (IS_ERR(ret2))
+			return ret2;
+	}
 
 	/* Policy lookup is done on every packet to account for packets that
 	 * passed through the allowed consumer. */
@@ -638,7 +672,9 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 	void *data = (void *) (long) skb->data;
 	void *data_end = (void *) (long) skb->data_end;
 	struct iphdr *ip4 = data + ETH_HLEN;
+	struct csum_offset off = {};
 	int ret, verdict, l4_off;
+	__u16 src_rev_nat = 0;
 
 	if (data + sizeof(*ip4) + ETH_HLEN > data_end)
 		return DROP_INVALID;
@@ -648,20 +684,31 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 	tuple.addr = ip4->saddr;
 
 	l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
-	ret = ct_lookup4(&CT_MAP4, &tuple, skb, l4_off, SECLABEL, 1, NULL);
+	csum_l4_offset_and_flags(tuple.nexthdr, &off);
+
+	ret = ct_lookup4(&CT_MAP4, &tuple, skb, l4_off, SECLABEL, 1, &src_rev_nat);
 	if (ret < 0)
 		return ret;
+
+	if (unlikely(src_rev_nat)) {
+		int ret2;
+
+		ret2 = lb4_rev_nat(skb, ETH_HLEN, l4_off, &off, src_rev_nat, &tuple,
+				  REV_NAT_F_TUPLE_SADDR);
+		if (IS_ERR(ret2))
+			return ret2;
+
+	}
 
 	/* Policy lookup is done on every packet to account for packets that
 	 * passed through the allowed consumer. */
 	verdict = policy_can_access(&POLICY_MAP, skb, src_label);
 	if (unlikely(ret == CT_NEW)) {
-		volatile int rev_nat;
+		//volatile int rev_nat;
 		if (verdict != TC_ACT_OK)
 			return DROP_POLICY;
 
-		rev_nat = skb->cb[CB_REVERSE_NAT];
-		ret = ct_create4(&CT_MAP4, &tuple, skb, 1, rev_nat & 0xFFFF);
+		ret = ct_create4(&CT_MAP4, &tuple, skb, 1, 0);
 		if (IS_ERR(ret))
 			return ret;
 	}

bpf/join_ep.sh
@@ -27,6 +27,7 @@ echo "Join EP id=$ID ifname=$IFNAME"
 DIR="$PWD/$ID"
 CLANG_OPTS="-D__NR_CPUS__=$(nproc) -O2 -target bpf -I$RUNDIR/globals -I$DIR -I$LIB/include"
 
+clang $CLANG_OPTS -c $LIB/bpf_lxc.c -S -o $DIR/bpf_lxc.asm
 clang $CLANG_OPTS -c $LIB/bpf_lxc.c -o $DIR/bpf_lxc.o
 
 tc qdisc replace dev $IFNAME clsact || true

bpf/lbmap/ipv6.go
@@ -71,12 +71,14 @@ type Service6Value struct {
 	Address types.IPv6
 	Port    uint16
 	Count   uint16
+	RevNat  uint16
 }
 
-func NewService6Value(count uint16, target net.IP, port uint16) *Service6Value {
+func NewService6Value(count uint16, target net.IP, port uint16, revNat uint16) *Service6Value {
 	svc := Service6Value{
-		Count: count,
-		Port:  common.Swab16(port),
+		Count:  count,
+		Port:   common.Swab16(port),
+		RevNat: common.Swab16(revNat),
 	}
 
 	copy(svc.Address[:], target.To16())

bpf/lib/common.h
@@ -143,10 +143,6 @@ enum {
 	CB_SRC_LABEL,
 	CB_IFINDEX,
 	CB_POLICY,
-
-	/* Used to carry reverse NAT index for load balanced IPv4 packets.
-	 * Index is set in lb4_local(). */
-	CB_REVERSE_NAT,
 };
 
 /* Flag values for CB_POLICY */
@@ -195,6 +191,7 @@ struct lb6_service {
 	union v6addr target;
 	__u16 port;
 	__u16 count;
+	__u16 rev_nat_index;
 } __attribute__((packed));
 
 struct lb6_reverse_nat {

bpf/lib/lb.h
@@ -15,6 +15,18 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
+/**
+ * Configuration:
+ * LB_L4: Include L4 matching and rewriting capabilities
+ * LB_L3: Enable fallback to L3 LB entries
+ *
+ * Either LB_L4, LB_L3, or both need to be set to enable forward
+ * translation. Reverse translation will awlays occur regardless
+ * of the settings.
+ */
+
+
 #ifndef __LB_H_
 #define __LB_H_
 
@@ -39,6 +51,8 @@ __BPF_MAP(cilium_lb4_services, BPF_MAP_TYPE_HASH, 0,
 	  sizeof(struct lb4_key), sizeof(struct lb4_service),
 	  PIN_GLOBAL_NS, CILIUM_LB_MAP_MAX_ENTRIES);
 
+#define REV_NAT_F_TUPLE_SADDR 1
+
 static inline int lb_select_slave(struct __sk_buff *skb, __u16 count)
 {
 	__be16 hash = get_hash_recalc(skb);
@@ -51,8 +65,8 @@ static inline int lb_select_slave(struct __sk_buff *skb, __u16 count)
 	return slave;
 }
 
-static inline int extract_l4_port(struct __sk_buff *skb, __u8 nexthdr, int l4_off,
-				  struct csum_offset *csum_off, __u16 *port)
+static inline int __inline__ extract_l4_port(struct __sk_buff *skb, __u8 nexthdr,
+					     int l4_off, __u16 *port)
 {
 	int ret;
 
@@ -73,8 +87,6 @@ static inline int extract_l4_port(struct __sk_buff *skb, __u8 nexthdr, int l4_of
 		/* Pass unknown L4 to stack */
 		return DROP_UNKNOWN_L4;
 	}
-
-	csum_l4_offset_and_flags(nexthdr, csum_off);
 
 	return 0;
 }
@@ -115,28 +127,16 @@ static inline int __inline__ reverse_map_l4_port(struct __sk_buff *skb, __u8 nex
 	return 0;
 }
 
-/** Perform IPv6 DSR SNAT based on reverse NAT index
- * @arg skb		packet
- * @arg l4_off		offset to L4
- * @arg csum_off	offset to L4 checksum field
- * @arg csum_flags	checksum flags
- * @arg index		reverse NAT index
- * @arg tuple		tuple
- */
-static inline int lb6_dsr_snat(struct __sk_buff *skb, int l4_off,
-			       struct csum_offset *csum_off, __u16 index,
-			       struct ipv6_ct_tuple *tuple)
+static inline int __inline__ __lb6_rev_nat(struct __sk_buff *skb, int l4_off,
+					 struct csum_offset *csum_off,
+					 struct ipv6_ct_tuple *tuple, int flags,
+					 struct lb6_reverse_nat *nat)
 {
-	union v6addr tmp, sip;
-	struct lb6_reverse_nat *nat;
+	union v6addr old_saddr;
+	union v6addr tmp;
+	__u8 *new_saddr;
 	__be32 sum;
 	int ret;
-
-	cilium_trace(skb, DBG_LB6_REVERSE_NAT_LOOKUP, index, 0);
-	nat = map_lookup_elem(&cilium_lb6_reverse_nat, &index);
-	if (nat == NULL) {
-		return 0;
-	}
 
 	cilium_trace(skb, DBG_LB6_REVERSE_NAT, nat->address.p4, nat->port);
 
@@ -146,19 +146,50 @@ static inline int lb6_dsr_snat(struct __sk_buff *skb, int l4_off,
 			return ret;
 	}
 
-	if (ipv6_load_saddr(skb, ETH_HLEN, &sip) < 0)
-		return DROP_INVALID;
+	if (flags & REV_NAT_F_TUPLE_SADDR) {
+		ipv6_addr_copy(&old_saddr, &tuple->addr);
+		ipv6_addr_copy(&tuple->addr, &nat->address);
+		new_saddr = tuple->addr.addr;
+	} else {
+		if (ipv6_load_saddr(skb, ETH_HLEN, &old_saddr) < 0)
+			return DROP_INVALID;
 
-	ipv6_addr_copy(&tmp, &nat->address);
-	ret = ipv6_store_saddr(skb, tmp.addr, ETH_HLEN);
+		ipv6_addr_copy(&tmp, &nat->address);
+		new_saddr = tmp.addr;
+	}
+
+	ret = ipv6_store_saddr(skb, new_saddr, ETH_HLEN);
 	if (IS_ERR(ret))
 		return DROP_WRITE_ERROR;
 
-	sum = csum_diff(sip.addr, 16, tmp.addr, 16, 0);
+	sum = csum_diff(old_saddr.addr, 16, new_saddr, 16, 0);
 	if (csum_l4_replace(skb, l4_off, csum_off, 0, sum, BPF_F_PSEUDO_HDR) < 0)
 		return DROP_CSUM_L4;
 
 	return 0;
+}
+
+/** Perform IPv6 reverse NAT based on reverse NAT index
+ * @arg skb		packet
+ * @arg l4_off		offset to L4
+ * @arg csum_off	offset to L4 checksum field
+ * @arg csum_flags	checksum flags
+ * @arg index		reverse NAT index
+ * @arg tuple		tuple
+ * @arg saddr_tuple	If set, tuple address will be updated with new source address
+ */
+static inline int __inline__ lb6_rev_nat(struct __sk_buff *skb, int l4_off,
+					 struct csum_offset *csum_off, __u16 index,
+					 struct ipv6_ct_tuple *tuple, int flags)
+{
+	struct lb6_reverse_nat *nat;
+
+	cilium_trace(skb, DBG_LB6_REVERSE_NAT_LOOKUP, index, 0);
+	nat = map_lookup_elem(&cilium_lb6_reverse_nat, &index);
+	if (nat == NULL)
+		return 0;
+
+	return __lb6_rev_nat(skb, l4_off, csum_off, tuple, flags, nat);
 }
 
 /** Extract IPv6 LB key from packet
@@ -181,26 +212,41 @@ static inline int __inline__ lb6_extract_key(struct __sk_buff *skb, struct ipv6_
 					     struct csum_offset *csum_off)
 {
 	ipv6_addr_copy(&key->address, &tuple->addr);
-	return extract_l4_port(skb, tuple->nexthdr, l4_off, csum_off, &key->dport);
+	csum_l4_offset_and_flags(tuple->nexthdr, csum_off);
+
+#ifdef LB_L4
+	return extract_l4_port(skb, tuple->nexthdr, l4_off, &key->dport);
+#else
+	return 0;
+#endif
 }
 
 static inline struct lb6_service *lb6_lookup_service(struct __sk_buff *skb,
 						    struct lb6_key *key)
 {
-	struct lb6_service *svc;
+#ifdef LB_L4
+	if (key->dport) {
+		struct lb6_service *svc;
 
-	cilium_trace(skb, DBG_LB6_LOOKUP_MASTER, key->address.p4, key->dport);
-	svc = map_lookup_elem(&cilium_lb6_services, key);
-	if (svc && svc->count != 0)
-		return svc;
+		cilium_trace(skb, DBG_LB6_LOOKUP_MASTER, key->address.p4, key->dport);
+		svc = map_lookup_elem(&cilium_lb6_services, key);
+		if (svc && svc->count != 0)
+			return svc;
 
-	if (key->dport != 0) {
 		key->dport = 0;
+	}
+#endif
+
+#ifdef LB_L3
+	if (1) {
+		struct lb6_service *svc;
+
 		cilium_trace(skb, DBG_LB6_LOOKUP_MASTER, key->address.p4, key->dport);
 		svc = map_lookup_elem(&cilium_lb6_services, key);
 		if (svc && svc->count != 0)
 			return svc;
 	}
+#endif
 
 	cilium_trace(skb, DBG_LB6_LOOKUP_MASTER_FAIL, key->address.p2, key->address.p3);
 	return NULL;
@@ -226,8 +272,6 @@ static inline int __inline__ lb6_xlate(struct __sk_buff *skb, union v6addr *new_
 				       int l3_off, int l4_off, struct csum_offset *csum_off,
 				       struct lb6_key *key, struct lb6_service *svc)
 {
-	int ret;
-
 	ipv6_store_daddr(skb, new_dst->addr, l3_off);
 
 	if (csum_off) {
@@ -236,21 +280,26 @@ static inline int __inline__ lb6_xlate(struct __sk_buff *skb, union v6addr *new_
 			return DROP_CSUM_L4;
 	}
 
+#ifdef LB_L4
 	if (svc->port && key->dport != svc->port &&
 	    (nexthdr == IPPROTO_TCP || nexthdr == IPPROTO_UDP)) {
 		__u16 tmp = svc->port;
+		int ret;
+
 		/* Port offsets for UDP and TCP are the same */
 		ret = l4_modify_port(skb, l4_off, TCP_DPORT_OFF, csum_off, tmp, key->dport);
 		if (IS_ERR(ret))
 			return ret;
 	}
+#endif
 
 	return TC_ACT_OK;
 }
 
 static inline int __inline__ lb6_local(struct __sk_buff *skb, int l3_off, int l4_off,
 				       struct csum_offset *csum_off, struct lb6_key *key,
-				       struct ipv6_ct_tuple *tuple, struct lb6_service *svc)
+				       struct ipv6_ct_tuple *tuple, struct lb6_service *svc,
+				       __u16 *rev_nat_index)
 {
 	__u16 slave;
 
@@ -260,34 +309,20 @@ static inline int __inline__ lb6_local(struct __sk_buff *skb, int l3_off, int l4
 
 	ipv6_addr_copy(&tuple->addr, &svc->target);
 
+	if (rev_nat_index)
+		*rev_nat_index = svc->rev_nat_index;
+
 	return lb6_xlate(skb, &tuple->addr, tuple->nexthdr, l3_off, l4_off,
 			 csum_off, key, svc);
 }
 
-
-/** Perform IPv4 DSR SNAT based on reverse NAT index
- * @arg skb		packet
- * @arg l3_off		offset to L3
- * @arg l4_off		offset to L4
- * @arg csum_off	offset to L4 checksum field
- * @arg csum_flags	checksum flags
- * @arg index		reverse NAT index
- * @arg tuple		tuple
- */
-static inline int lb4_dsr_snat(struct __sk_buff *skb, int l3_off, int l4_off,
-			       struct csum_offset *csum_off, __u16 index,
-			       struct ipv4_ct_tuple *tuple)
+static inline int __inline__ __lb4_rev_nat(struct __sk_buff *skb, int l3_off, int l4_off,
+					 struct csum_offset *csum_off,
+					 struct ipv4_ct_tuple *tuple, int flags,
+					 struct lb4_reverse_nat *nat)
 {
-	__be32 old_sip, new_sip;
-	struct lb4_reverse_nat *nat;
-	__be32 sum;
+	__be32 old_sip, new_sip, sum;
 	int ret;
-
-	cilium_trace(skb, DBG_LB4_REVERSE_NAT_LOOKUP, index, 0);
-	nat = map_lookup_elem(&cilium_lb4_reverse_nat, &index);
-	if (nat == NULL) {
-		return 0;
-	}
 
 	cilium_trace(skb, DBG_LB4_REVERSE_NAT, nat->address, nat->port);
 
@@ -297,11 +332,17 @@ static inline int lb4_dsr_snat(struct __sk_buff *skb, int l3_off, int l4_off,
 			return ret;
 	}
 
-        ret = skb_load_bytes(skb, l3_off + offsetof(struct iphdr, saddr), &old_sip, 4);
-	if (IS_ERR(ret))
-		return ret;
+	if (flags & REV_NAT_F_TUPLE_SADDR) {
+		old_sip = tuple->addr;
+		tuple->addr = new_sip = nat->address;
+	} else {
+		ret = skb_load_bytes(skb, l3_off + offsetof(struct iphdr, saddr), &old_sip, 4);
+		if (IS_ERR(ret))
+			return ret;
 
-	new_sip = nat->address;
+		new_sip = nat->address;
+	}
+
         ret = skb_store_bytes(skb, l3_off + offsetof(struct iphdr, saddr), &new_sip, 4, 0);
 	if (IS_ERR(ret))
 		return DROP_WRITE_ERROR;
@@ -315,6 +356,30 @@ static inline int lb4_dsr_snat(struct __sk_buff *skb, int l3_off, int l4_off,
 		return DROP_CSUM_L4;
 
 	return 0;
+}
+
+
+/** Perform IPv4 reverse NAT based on reverse NAT index
+ * @arg skb		packet
+ * @arg l3_off		offset to L3
+ * @arg l4_off		offset to L4
+ * @arg csum_off	offset to L4 checksum field
+ * @arg csum_flags	checksum flags
+ * @arg index		reverse NAT index
+ * @arg tuple		tuple
+ */
+static inline int __inline__ lb4_rev_nat(struct __sk_buff *skb, int l3_off, int l4_off,
+					 struct csum_offset *csum_off, __u16 index,
+					 struct ipv4_ct_tuple *tuple, int flags)
+{
+	struct lb4_reverse_nat *nat;
+
+	cilium_trace(skb, DBG_LB4_REVERSE_NAT_LOOKUP, index, 0);
+	nat = map_lookup_elem(&cilium_lb4_reverse_nat, &index);
+	if (nat == NULL)
+		return 0;
+
+	return __lb4_rev_nat(skb, l3_off, l4_off, csum_off, tuple, flags, nat);
 }
 
 /** Extract IPv4 LB key from packet
@@ -334,26 +399,43 @@ static inline int __inline__ lb4_extract_key(struct __sk_buff *skb, struct ipv4_
 					     struct csum_offset *csum_off)
 {
 	key->address = tuple->addr;
-	return extract_l4_port(skb, tuple->nexthdr, l4_off, csum_off, &key->dport);
+	csum_l4_offset_and_flags(tuple->nexthdr, csum_off);
+
+#ifdef LB_L4
+	return extract_l4_port(skb, tuple->nexthdr, l4_off, &key->dport);
+#else
+	return 0;
+#endif
 }
 
 static inline struct lb4_service *lb4_lookup_service(struct __sk_buff *skb,
 						     struct lb4_key *key)
 {
-	struct lb4_service *svc;
+#ifdef LB_L4
+	if (key->dport) {
+		struct lb4_service *svc;
 
-	cilium_trace(skb, DBG_LB4_LOOKUP_MASTER, key->address, key->dport);
-	svc = map_lookup_elem(&cilium_lb4_services, key);
-	if (svc && svc->count != 0)
-		return svc;
+		/* FIXME: The verifier barks on these calls right now for some reason */
+		/* cilium_trace(skb, DBG_LB4_LOOKUP_MASTER, key->address, key->dport); */
+		svc = map_lookup_elem(&cilium_lb4_services, key);
+		if (svc && svc->count != 0)
+			return svc;
 
-	if (key->dport != 0) {
 		key->dport = 0;
-		cilium_trace(skb, DBG_LB4_LOOKUP_MASTER, key->address, key->dport);
+	}
+#endif
+
+#ifdef LB_L3
+	if (1) {
+		struct lb4_service *svc;
+
+		/* FIXME: The verifier barks on these calls right now for some reason */
+		/* cilium_trace(skb, DBG_LB4_LOOKUP_MASTER, key->address, key->dport); */
 		svc = map_lookup_elem(&cilium_lb4_services, key);
 		if (svc && svc->count != 0)
 			return svc;
 	}
+#endif
 
 	cilium_trace(skb, DBG_LB4_LOOKUP_MASTER_FAIL, 0, 0);
 	return NULL;
@@ -395,6 +477,7 @@ static inline int __inline__ lb4_xlate(struct __sk_buff *skb, __be32 *new_addr, 
 			return DROP_CSUM_L4;
 	}
 
+#ifdef LB_L4
 	if (svc->port && key->dport != svc->port &&
 	    (nexthdr == IPPROTO_TCP || nexthdr == IPPROTO_UDP)) {
 		__u16 tmp = svc->port;
@@ -403,13 +486,15 @@ static inline int __inline__ lb4_xlate(struct __sk_buff *skb, __be32 *new_addr, 
 		if (IS_ERR(ret))
 			return ret;
 	}
+#endif
 
 	return TC_ACT_OK;
 }
 
 static inline int __inline__ lb4_local(struct __sk_buff *skb, int l3_off, int l4_off,
 				       struct csum_offset *csum_off, struct lb4_key *key,
-				       struct ipv4_ct_tuple *tuple, struct lb4_service *svc)
+				       struct ipv4_ct_tuple *tuple, struct lb4_service *svc,
+				       __u16 *rev_nat_index)
 {
 	__u16 slave;
 
@@ -418,7 +503,9 @@ static inline int __inline__ lb4_local(struct __sk_buff *skb, int l3_off, int l4
 		return DROP_NO_SERVICE;
 
 	tuple->addr = svc->target;
-	skb->cb[CB_REVERSE_NAT] = svc->rev_nat_index;
+
+	if (rev_nat_index)
+		*rev_nat_index = svc->rev_nat_index;
 
 	return lb4_xlate(skb, &tuple->addr, tuple->nexthdr, l3_off, l4_off,
 			 csum_off, key, svc);

commit 2566c95b6f1b0152cf6d4f9eb5bd17edb0fedddf

lb: Print reverse nat index when dumping IPv6 services
Fixes: d4fc3e2d67 ("bpf: lb: Add reverse NAT translation via CT state")
Signed-off-by: Thomas Graf <thomas@cilium.io>

cilium/lb/main.go
@@ -167,7 +167,7 @@ func dumpService6(key bpf.MapKey, value bpf.MapValue) {
 	if svcKey.Slave == 0 {
 		fmt.Printf("%d\n", svcVal.Count)
 	} else {
-		fmt.Printf("%v %d\n", svcVal.Address, svcVal.Port)
+		fmt.Printf("%v %d %d\n", svcVal.Address, svcVal.RevNat, svcVal.Port)
 	}
 }
 