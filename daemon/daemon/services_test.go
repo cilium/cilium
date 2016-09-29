12178e6b7ac723ac947bfd60545910b407c58fda
André Martins <andre@cilium.io>
2016-09-29 02:26:13 +0100

    daemon: add services unit-tests
    Acked-by: Thomas Graf <thomas@cilium.io>
    Signed-off-by: André Martins <andre@cilium.io>

---
+++ daemon/daemon/services_test.go
@@ -0,0 +1,128 @@
+//
+// Copyright 2016 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package daemon
+
+import (
+	"net"
+
+	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/common/types"
+
+	. "gopkg.in/check.v1"
+)
+
+var (
+	svc1 = types.ServiceL4{
+		IP:   net.IPv6loopback,
+		Port: 0,
+	}
+	svc2 = types.ServiceL4{
+		IP:   net.IPv6loopback,
+		Port: 1,
+	}
+	wantSvcL4ID = &types.ServiceL4ID{
+		ServiceID: 123,
+		ServiceL4: svc2,
+	}
+)
+
+func (ds *DaemonSuite) TestServices(c *C) {
+	var nilSvcL4ID *types.ServiceL4ID
+	// Set up last free ID with zero
+	id, err := ds.d.GetMaxServiceID()
+	c.Assert(err, Equals, nil)
+	c.Assert(id, Equals, common.FirstFreeServiceID)
+
+	ffsIDu16 := types.ServiceID(uint16(common.FirstFreeServiceID))
+
+	svcL4ID, err := ds.d.PutServiceL4(svc1)
+	c.Assert(err, Equals, nil)
+	c.Assert(svcL4ID.ServiceID, Equals, ffsIDu16)
+
+	svcL4ID, err = ds.d.PutServiceL4(svc1)
+	c.Assert(err, Equals, nil)
+	c.Assert(svcL4ID.ServiceID, Equals, ffsIDu16)
+
+	svcL4ID, err = ds.d.PutServiceL4(svc2)
+	c.Assert(err, Equals, nil)
+	c.Assert(svcL4ID.ServiceID, Equals, ffsIDu16+1)
+
+	gotSvcL4ID, err := ds.d.GetServiceL4ID(common.FirstFreeServiceID)
+	c.Assert(err, Equals, nil)
+	wantSvcL4ID.ServiceID = ffsIDu16
+	wantSvcL4ID.ServiceL4 = svc1
+	c.Assert(gotSvcL4ID, DeepEquals, wantSvcL4ID)
+
+	err = ds.d.DeleteServiceL4IDByUUID(common.FirstFreeServiceID)
+	c.Assert(err, Equals, nil)
+	gotSvcL4ID, err = ds.d.GetServiceL4ID(common.FirstFreeServiceID)
+	c.Assert(err, Equals, nil)
+	c.Assert(gotSvcL4ID, Equals, nilSvcL4ID)
+
+	gotSvcL4ID, err = ds.d.GetServiceL4ID(common.FirstFreeServiceID + 1)
+	c.Assert(err, Equals, nil)
+	wantSvcL4ID.ServiceID = types.ServiceID(common.FirstFreeServiceID + 1)
+	wantSvcL4ID.ServiceL4 = svc2
+	c.Assert(gotSvcL4ID, DeepEquals, wantSvcL4ID)
+
+	err = ds.d.DeleteServiceL4IDByUUID(common.FirstFreeServiceID)
+	c.Assert(err, Equals, nil)
+
+	err = ds.d.kvClient.SetMaxID(common.LastFreeServiceIDKeyPath, common.FirstFreeServiceID, common.FirstFreeServiceID)
+	c.Assert(err, Equals, nil)
+
+	err = ds.d.DeleteServiceL4IDByUUID(common.FirstFreeServiceID)
+	c.Assert(err, Equals, nil)
+	gotSvcL4ID, err = ds.d.GetServiceL4ID(common.FirstFreeServiceID)
+	c.Assert(err, Equals, nil)
+	c.Assert(gotSvcL4ID, Equals, nilSvcL4ID)
+
+	gotSvcL4ID, err = ds.d.PutServiceL4(svc2)
+	c.Assert(err, Equals, nil)
+	c.Assert(gotSvcL4ID.ServiceID, Equals, types.ServiceID(common.FirstFreeServiceID+1))
+
+	sha256sum, err := svc2.SHA256Sum()
+	c.Assert(err, Equals, nil)
+
+	gotSvcL4ID, err = ds.d.GetServiceL4IDBySHA256(sha256sum)
+	c.Assert(err, Equals, nil)
+	c.Assert(gotSvcL4ID, DeepEquals, wantSvcL4ID)
+
+	err = ds.d.DeleteServiceL4IDBySHA256(sha256sum)
+	c.Assert(err, Equals, nil)
+	err = ds.d.DeleteServiceL4IDByUUID(common.FirstFreeServiceID + 1)
+	c.Assert(err, Equals, nil)
+	err = ds.d.DeleteServiceL4IDByUUID(common.FirstFreeServiceID + 1)
+	c.Assert(err, Equals, nil)
+
+	gotSvcL4ID, err = ds.d.PutServiceL4(svc2)
+	c.Assert(err, Equals, nil)
+	c.Assert(gotSvcL4ID.ServiceID, Equals, ffsIDu16)
+
+	gotSvcL4ID, err = ds.d.PutServiceL4(svc1)
+	c.Assert(err, Equals, nil)
+	c.Assert(gotSvcL4ID.ServiceID, Equals, types.ServiceID(common.FirstFreeServiceID+1))
+}
+
+func (ds *DaemonSuite) TestGetMaxServiceID(c *C) {
+	lastID := uint32(common.MaxSetOfServiceID - 1)
+	err := ds.d.kvClient.SetValue(common.LastFreeServiceIDKeyPath, lastID)
+	c.Assert(err, Equals, nil)
+
+	id, err := ds.d.GetMaxServiceID()
+	c.Assert(err, Equals, nil)
+	c.Assert(id, Equals, (common.MaxSetOfServiceID - 1))
+}
