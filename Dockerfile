cilium/cilium

commit ab9a8e420a53e8032fbb7c4454dbe79373663a66

Dockerfile: add option to install cni on host
Signed-off-by: André Martins <andre@cilium.io>

Dockerfile
@@ -50,7 +50,9 @@ make PKG_BUILD=1 install && \
 #
 apt-get purge --auto-remove -y gcc make bison flex git curl xz-utils && \
 apt-get clean && \
-rm -fr /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/local/go
+rm -fr /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/local/go && \
+echo -e '#!/usr/bin/env bash\ncp /opt/cni/bin/cilium-cni /tmp/cni/bin && /usr/bin/cilium $@' > /home/with-cni.sh && \
+chmod +x /home/with-cni.sh
 
 ENV PATH="/usr/local/clang+llvm/bin:$PATH"
 

commit dd47cd98d0a0e712c1b3fbfef1e332488ba78314

Dockerfile: removed unecessary echo's -e flag
Fixes: ab9a8e420a ("Dockerfile: add option to install cni on host")
Signed-off-by: André Martins <andre@cilium.io>

Dockerfile
@@ -51,7 +51,7 @@ make PKG_BUILD=1 install && \
 apt-get purge --auto-remove -y gcc make bison flex git curl xz-utils && \
 apt-get clean && \
 rm -fr /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/local/go && \
-echo -e '#!/usr/bin/env bash\ncp /opt/cni/bin/cilium-cni /tmp/cni/bin && /usr/bin/cilium $@' > /home/with-cni.sh && \
+echo '#!/usr/bin/env bash\ncp /opt/cni/bin/cilium-cni /tmp/cni/bin && /usr/bin/cilium $@' > /home/with-cni.sh && \
 chmod +x /home/with-cni.sh
 
 ENV PATH="/usr/local/clang+llvm/bin:$PATH"