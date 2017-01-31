0fae2adca203c8163ea9a266ddab348665d3b0ae
André Martins <andre@cilium.io>
2017-01-31 04:17:28 +0000

    tests: changing k8s runtime test to use k8s binaries
    Signed-off-by: André Martins <andre@cilium.io>

--- tests/04-k8s.sh
+++ tests/04-k8s.sh
@@ -9,47 +9,22 @@ if [ -z $K8S ]; then
 	exit 0
 fi
 
-source "${dir}/../examples/kubernetes/env-kube.sh"
-
-TEST_NET="cilium"
-NETPERF_IMAGE="noironetworks/netperf"
-K8S_PATH="/home/vagrant/kubernetes"
-
-function start_k8s {
-    if [ ! -d "${K8S_PATH}" ]; then
-        exit 1
-    fi
-    echo "Starting kubernetes..."
-    cd ${K8S_PATH}
-    "./hack/local-up-cluster.sh" &
-    sleep 5s
-}
-
 function cleanup {
     echo "Cleaning up"
     sleep 3s
-    sudo killall -9 etcd || true
-    sudo killall -9 kubelet || true
-    sudo killall -9 hyperkube || true
-    sudo killall -9 kube-scheduler || true
-    sudo killall -9 kube-controller-manager || true
-    sudo killall -9 kube-proxy || true
-    sudo killall -9 kube-apiserver || true
-    docker rm -f `docker ps -aq --filter=name=k8s` 2> /dev/null || true
     monitor_stop
-    sudo cilium -D policy delete io.cilium
+    "${dir}/../examples/kubernetes/scripts/11-cleanup.sh"
 }
 
 trap cleanup EXIT
-
-start_k8s
 
 monitor_start
 
 set -x
 
-"${dir}/../examples/kubernetes/0-policy.sh" 300
-"${dir}/../examples/kubernetes/1-guestbook.sh" 300
+"${dir}/../examples/kubernetes/scripts/08-cilium.sh"
+"${dir}/../examples/kubernetes/scripts/09-dns-addon.sh"
+SOCAT_OFF=1 "${dir}/../examples/kubernetes/scripts/10-1-smoke-test.sh"
 "${dir}/wait-for-docker.bash" k8s_guestbook 100
 "${dir}/wait-for-docker.bash" k8s_redis-slave 100
 "${dir}/wait-for-docker.bash" k8s_redis-master 100
