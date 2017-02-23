8d9e85d1f2bc9f0459262924ad1cd19ca08bbf23
Thomas Graf <thomas@cilium.io>
2017-02-23 16:27:34 +0100

    Add automatically generated code based on API specification
    This adds the code which is auto-generated by go-swagger. It includes:
     - Models for all data structures defined by the spec [api/v1/models/]
     - Client side code to use the API [api/v1/client]
     - Server side handling code to use the API [api/v1/server]
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ api/v1/server/configure_cilium.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	errors "github.com/go-openapi/errors"
+	runtime "github.com/go-openapi/runtime"
+	graceful "github.com/tylerb/graceful"
+
+	"github.com/cilium/cilium/api/v1/server/restapi"
+)
+
+// This file is safe to edit. Once it exists it will not be overwritten
+
+//go:generate swagger generate server --target ../api/v1 --name  --spec ../api/v1/openapi.json --api-package restapi --server-package server
+
+func configureFlags(api *restapi.CiliumAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *restapi.CiliumAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// s.api.Logger = log.Printf
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix"
+func configureServer(s *graceful.Server, scheme string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
---
+++ api/v1/server/doc.go
@@ -0,0 +1,22 @@
+/*Package server Cilium API
+
+Cilium
+
+
+    Schemes:
+      http
+    Host: localhost
+    BasePath: /v1beta
+    Version: v1beta
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package server
