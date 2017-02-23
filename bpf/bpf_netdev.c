92dcd5700890f4ddc96a13f5d4e6858d13e5dedc
Thomas Graf <thomas@cilium.io>
2017-02-24 00:25:13 +0100

    Update copyright headers
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- bpf/bpf_netdev.c
+++ bpf/bpf_netdev.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016 Authors of Cilium
+ *  Copyright (C) 2016-2017 Authors of Cilium
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- pkg/client/service.go
+++ pkg/client/service.go
@@ -1,4 +1,3 @@
-//
 // Copyright 2016-2017 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
 package client
 
 import (
