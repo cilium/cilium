996c88371dc9da57441f09435a7bfdf041fdf1b1
Thomas Graf <thomas@cilium.io>
2017-03-13 00:47:45 +0100

    policy: Continue evaluating policy on single consumer eval failure
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/endpoint/policy.go
+++ pkg/endpoint/policy.go
@@ -136,7 +136,7 @@ func (e *Endpoint) regenerateConsumable(owner Owner) (bool, error) {
 		if err := e.evaluateConsumerSource(owner, &ctx, id.ID); err != nil {
 			// This should never really happen
 			// FIXME: clear policy because it is inconsistent
-			break
+			log.Debugf("Received error while evaluating policy: %s", err)
 		}
 	}
 
@@ -146,7 +146,7 @@ func (e *Endpoint) regenerateConsumable(owner Owner) (bool, error) {
 	for idx < maxID {
 		if err := e.evaluateConsumerSource(owner, &ctx, idx); err != nil {
 			// FIXME: clear policy because it is inconsistent
-			break
+			log.Debugf("Received error while evaluating policy: %s", err)
 		}
 		idx++
 	}
