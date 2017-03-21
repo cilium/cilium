993b983634620d0b3bf417437ffd2b8876a2f005
Daniel Borkmann <daniel@cilium.io>
2017-03-21 17:23:37 +0100

    bpf: move v4/v6 ct tables to lru map
    Add a probe whether BPF LRU map is available and if so then use the
    LRU-based hash table map instead of the traditional hash table. It
    uses a global lru list, not the strictly per-cpu one to save memory
    cost. As a side-effect, when using lru, we disable the gc in the go
    daemon and can just reuse stale entries. Also, update bpf/bpf_features.h
    so we can build test locally.
    
    Signed-off-by: Daniel Borkmann <daniel@cilium.io>

--- bpf/bpf_features.h
+++ bpf/bpf_features.h
@@ -1,25 +1,12 @@
-/*
- *  Copyright (C) 2016-2017 Authors of Cilium
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-/* Dummy header for test compilation, add all capabilities here */
-
 #ifndef BPF_FEATURES_H_
 #define BPF_FEATURES_H_
 
 #define HAVE_SKB_CHANGE_TAIL
 
-#endif
+#define HAVE_LRU_MAP_TYPE
+
+#define HAVE_MAP_VAL_ADJ
+
+#define HAVE_MARK_MAP_VALS
+
+#endif /* BPF_FEATURES_H_ */
