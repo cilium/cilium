ec25f762641434c80e131f49c0595de66a6372af
Thomas Graf <thomas@cilium.io>
2017-03-24 21:26:26 +0100

    doc: Use readthedocs.io sphinx theme
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -83,7 +83,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
