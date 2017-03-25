cilium/cilium

commit 770ad9ccb2de7cb4ee025846af325c0b9666c4b8

proxy: Initial transparent proxy support
Transparent redirection of packets into a proxy in either user or kernel
space. The configuration occurs through the L4 policy which can now
redirect to an L4 port.

An initial POC implementation of a proxy runs a separate go subroutine
for each endpoint which requires redirection. It can receive metadata
from the datapath via BPF maps.

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/lib/lxc.h
@@ -23,6 +23,8 @@
 #include "ipv4.h"
 #include "eth.h"
 #include "dbg.h"
+#include "csum.h"
+#include "l4.h"
 
 #ifndef DISABLE_SMAC_VERIFICATION
 static inline int valid_src_mac(struct ethhdr *eth)
@@ -80,5 +82,48 @@ static inline int valid_dst_mac(struct ethhdr *eth)
 	return 1;
 }
 #endif
+
+static inline int __inline__
+ipv4_redirect_to_host_port(struct __sk_buff *skb, struct csum_offset *csum,
+			  int l4_off, __u16 new_port, __u16 old_port, __be32 old_ip,
+			  struct ipv4_ct_tuple *tuple)
+{
+	__be32 host_ip = IPV4_GATEWAY;
+	struct proxy4_tbl_key key = {
+		.saddr = __constant_htonl(LXC_IPV4),
+		.sport = tuple->sport,
+		.dport = new_port,
+		.nexthdr = tuple->nexthdr,
+	};
+	struct proxy4_tbl_value value = {
+		.orig_daddr = old_ip,
+		.orig_dport = old_port,
+		.lifetime = 360,
+	};
+
+	cilium_trace_capture(skb, DBG_CAPTURE_PROXY_PRE, old_port);
+
+	if (l4_modify_port(skb, l4_off, TCP_DPORT_OFF, csum,
+			   new_port, old_port) < 0)
+		return DROP_WRITE_ERROR;
+
+	if (skb_store_bytes(skb, ETH_HLEN + offsetof(struct iphdr, daddr), &host_ip, 4, 0) < 0)
+		return DROP_WRITE_ERROR;
+
+	if (l3_csum_replace(skb, ETH_HLEN + offsetof(struct iphdr, check), old_ip, host_ip, 4) < 0)
+		return DROP_CSUM_L3;
+
+	if (csum->offset &&
+	    csum_l4_replace(skb, l4_off, csum, old_ip, host_ip, 4 | BPF_F_PSEUDO_HDR) < 0)
+		return DROP_CSUM_L4;
+
+	cilium_trace_capture(skb, DBG_CAPTURE_PROXY_POST, new_port);
+
+	cilium_trace(skb, DBG_REV_PROXY_UPDATE, key.sport << 16 | key.dport, key.saddr);
+	if (map_update_elem(&cilium_proxy4, &key, &value, 0) < 0)
+		return DROP_CT_CREATE_FAILED;
+
+	return 0;
+}
 
 #endif

bpf/bpf_lxc.c
@@ -42,6 +42,7 @@
 #include "lib/drop.h"
 #include "lib/dbg.h"
 #include "lib/csum.h"
+#include "lib/conntrack.h"
 
 #define POLICY_ID ((LXC_ID << 16) | SECLABEL)
 
@@ -371,6 +372,8 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	struct lb4_key key = {};
 	struct ct_state ct_state_new = {};
 	struct ct_state ct_state_reply = {};
+	__be32 orig_dip;
+	__u16 dport;
 
 	if (data + sizeof(*ip4) + ETH_HLEN > data_end)
 		return DROP_INVALID;
@@ -408,6 +411,7 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	}
 
 	ct_state_new.orig_dport = key.dport;
+	orig_dip = tuple.addr;
 
 	if ((svc = lb4_lookup_service(skb, &key)) != NULL) {
 		ret = lb4_local(skb, l3_off, l4_off, &csum_off,
@@ -432,6 +436,8 @@ skip_service_lookup:
 	if (ret < 0)
 		return ret;
 
+	dport = tuple.dport;
+
 	switch (ret) {
 	case CT_NEW:
 		/* New connection implies that rev_nat_index remains untouched
@@ -442,6 +448,8 @@ skip_service_lookup:
 		ret = ct_create4(&CT_MAP4, &tuple, skb, 0, &ct_state_new);
 		if (IS_ERR(ret))
 			return ret;
+
+		ct_state_reply.proxy_port = ct_state_new.proxy_port;
 		break;
 
 	case CT_ESTABLISHED:
@@ -461,6 +469,33 @@ skip_service_lookup:
 
 	default:
 		return DROP_POLICY;
+	}
+
+	if (ct_state_reply.proxy_port) {
+		union macaddr host_mac = HOST_IFINDEX_MAC;
+		int ret;
+
+		ret = ipv4_redirect_to_host_port(skb, &csum_off, l4_off,
+						 ct_state_reply.proxy_port, dport,
+						 orig_dip, &tuple);
+		if (IS_ERR(ret))
+			return ret;
+
+		/* After L4 write in port mapping: revalidate for direct packet access */
+		data = (void *) (long) skb->data;
+		data_end = (void *) (long) skb->data_end;
+		ip4 = data + ETH_HLEN;
+		if (data + sizeof(*ip4) + ETH_HLEN > data_end)
+			return DROP_INVALID;
+
+		cilium_trace(skb, DBG_TO_HOST, skb->cb[CB_POLICY], 0);
+
+		ret = ipv4_l3(skb, l3_off, (__u8 *) &router_mac.addr, (__u8 *) &host_mac.addr, ip4);
+		if (ret != TC_ACT_OK)
+			return ret;
+
+		cilium_trace_capture(skb, DBG_CAPTURE_DELIVERY, HOST_IFINDEX);
+		return redirect(HOST_IFINDEX, 0);
 	}
 
 	/* After L4 write in port mapping: revalidate for direct packet access */
@@ -627,10 +662,11 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	void *data = (void *) (long) skb->data;
 	void *data_end = (void *) (long) skb->data_end;
 	struct ipv6hdr *ip6 = data + ETH_HLEN;
-	struct csum_offset off = {};
+	struct csum_offset csum_off = {};
 	int ret, l4_off, verdict;
 	struct ct_state ct_state_reply = {};
 	struct ct_state ct_state_new = {};
+	__u16 dport;
 
 	if (data + sizeof(struct ipv6hdr) + ETH_HLEN > data_end)
 		return DROP_INVALID;
@@ -639,7 +675,7 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	tuple.nexthdr = ip6->nexthdr;
 	ipv6_addr_copy(&tuple.addr, (union v6addr *) &ip6->saddr);
 	l4_off = ETH_HLEN + ipv6_hdrlen(skb, ETH_HLEN, &tuple.nexthdr);
-	csum_l4_offset_and_flags(tuple.nexthdr, &off);
+	csum_l4_offset_and_flags(tuple.nexthdr, &csum_off);
 
 	/* derive reverse NAT index and zero it. */
 	ct_state_new.rev_nat_index = ip6->daddr.s6_addr32[3] & 0xFFFF;
@@ -652,10 +688,10 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 		if (IS_ERR(ret))
 			return DROP_WRITE_ERROR;
 
-		if (off.offset) {
+		if (csum_off.offset) {
 			__u32 zero_nat = 0;
 			__be32 sum = csum_diff(&ct_state_new.rev_nat_index, 4, &zero_nat, 4, 0);
-			if (csum_l4_replace(skb, l4_off, &off, 0, sum, BPF_F_PSEUDO_HDR) < 0)
+			if (csum_l4_replace(skb, l4_off, &csum_off, 0, sum, BPF_F_PSEUDO_HDR) < 0)
 				return DROP_CSUM_L4;
 		}
 	}
@@ -664,10 +700,12 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	if (ret < 0)
 		return ret;
 
+	dport = tuple.dport;
+
 	if (unlikely(ct_state_reply.rev_nat_index)) {
 		int ret2;
 
-		ret2 = lb6_rev_nat(skb, l4_off, &off,
+		ret2 = lb6_rev_nat(skb, l4_off, &csum_off,
 				   ct_state_reply.rev_nat_index, &tuple, 0);
 		if (IS_ERR(ret2))
 			return ret2;
@@ -675,6 +713,8 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 
 	/* Policy lookup is done on every packet to account for packets that
 	 * passed through the allowed consumer. */
+	/* FIXME: Add option to disable policy accounting and avoid policy
+	 * lookup if policy accounting is disabled */
 	verdict = policy_can_access(&POLICY_MAP, skb, src_label);
 	if (unlikely(ret == CT_NEW)) {
 		if (verdict != TC_ACT_OK)
@@ -684,6 +724,32 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 		ret = ct_create6(&CT_MAP6, &tuple, skb, 1, &ct_state_new);
 		if (IS_ERR(ret))
 			return ret;
+
+		ct_state_reply.proxy_port = ct_state_new.proxy_port;
+	}
+
+	if (ct_state_reply.proxy_port && (ret == CT_NEW || ret == CT_ESTABLISHED)) {
+		//union v6addr host_ip = HOST_IP;
+		//union v6addr lxc_ip = LXC_IP;
+		//__be32 sum;
+
+		if (l4_modify_port(skb, l4_off, TCP_DPORT_OFF, &csum_off,
+				   ct_state_reply.proxy_port, dport) < 0)
+			return DROP_WRITE_ERROR;
+
+#if 0
+		if (ipv6_store_daddr(skb, host_ip.addr, ETH_HLEN) < 0)
+			return DROP_WRITE_ERROR;
+		/* FIXME: Calculate the checksum fix in user space */
+		sum = csum_diff(lxc_ip.addr, 16, host_ip.addr, 16, 0);
+		if (csum_l4_replace(skb, l4_off, &csum_off, 0, sum, BPF_F_PSEUDO_HDR) < 0)
+			return DROP_CSUM_L4;
+#endif
+		/* Mark packet with PACKET_HOST and pass to host */
+		if (skb_change_type(skb, 0) < 0)
+			return DROP_WRITE_ERROR;
+
+		skb->cb[CB_IFINDEX] = 0;
 	}
 
 	return 0;
@@ -695,10 +761,11 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 	void *data = (void *) (long) skb->data;
 	void *data_end = (void *) (long) skb->data_end;
 	struct iphdr *ip4 = data + ETH_HLEN;
-	struct csum_offset off = {};
+	struct csum_offset csum_off = {};
 	int ret, verdict, l4_off;
 	struct ct_state ct_state_reply = {};
 	struct ct_state ct_state_new = {};
+	__u16 dport;
 
 	if (data + sizeof(*ip4) + ETH_HLEN > data_end)
 		return DROP_INVALID;
@@ -708,11 +775,14 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 	tuple.addr = ip4->saddr;
 
 	l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
-	csum_l4_offset_and_flags(tuple.nexthdr, &off);
+	csum_l4_offset_and_flags(tuple.nexthdr, &csum_off);
 
 	ret = ct_lookup4(&CT_MAP4, &tuple, skb, l4_off, SECLABEL, 1, &ct_state_reply);
 	if (ret < 0)
 		return ret;
+
+	/* store dport because ct_create4() will invalidate it */
+	dport = tuple.dport;
 
 #ifdef LXC_NAT46
 	if (skb->cb[CB_NAT46_STATE] == NAT46) {
@@ -725,7 +795,7 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 		     !ct_state_reply.loopback)) {
 		int ret2;
 
-		ret2 = lb4_rev_nat(skb, ETH_HLEN, l4_off, &off,
+		ret2 = lb4_rev_nat(skb, ETH_HLEN, l4_off, &csum_off,
 				   &ct_state_reply, &tuple,
 				   REV_NAT_F_TUPLE_SADDR);
 		if (IS_ERR(ret2))
@@ -744,6 +814,26 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 		ret = ct_create4(&CT_MAP4, &tuple, skb, 1, &ct_state_new);
 		if (IS_ERR(ret))
 			return ret;
+
+		/* NOTE: tuple has been invalidated after this */
+
+		ct_state_reply.proxy_port = ct_state_new.proxy_port;
+	}
+
+	if (ct_state_reply.proxy_port && (ret == CT_NEW || ret == CT_ESTABLISHED)) {
+		__be32 orig_dip = LXC_IPV4;
+
+		ret = ipv4_redirect_to_host_port(skb, &csum_off, l4_off,
+						 ct_state_reply.proxy_port, dport,
+						 orig_dip, &tuple);
+		if (IS_ERR(ret))
+			return ret;
+
+		/* Mark packet with PACKET_HOST and redirect to host */
+		if (skb_change_type(skb, 0) < 0)
+			return DROP_WRITE_ERROR;
+
+		skb->cb[CB_IFINDEX] = 0;
 	}
 
 	return 0;
@@ -776,8 +866,14 @@ __section_tail(CILIUM_MAP_POLICY, LXC_ID) int handle_policy(struct __sk_buff *sk
 			return send_drop_notify_error(skb, ret, TC_ACT_SHOT);
 	}
 
+	ifindex = skb->cb[CB_IFINDEX];
+
 	cilium_trace_capture(skb, DBG_CAPTURE_DELIVERY, ifindex);
-	return redirect(ifindex, 0);
+
+	if (ifindex)
+		return redirect(ifindex, 0);
+	else
+		return TC_ACT_OK;
 }
 
 #ifdef LXC_NAT46

bpf/bpf_netdev.c
@@ -37,6 +37,7 @@
 #include "lib/eth.h"
 #include "lib/dbg.h"
 #include "lib/l3.h"
+#include "lib/l4.h"
 #include "lib/policy.h"
 #include "lib/drop.h"
 
@@ -104,6 +105,64 @@ static inline __u32 derive_ipv4_sec_ctx(struct __sk_buff *skb, struct iphdr *ip4
 #endif
 }
 
+static inline int __inline__
+reverse_proxy(struct __sk_buff *skb, int l4_off, struct iphdr *ip4,
+	      struct ipv4_ct_tuple *tuple)
+{
+	struct proxy4_tbl_value *val;
+	struct proxy4_tbl_key key = {
+		.saddr = ip4->daddr,
+		.nexthdr = tuple->nexthdr,
+	};
+	__be32 new_saddr, old_saddr = ip4->saddr;
+	__be16 new_sport, old_sport;
+	struct csum_offset csum;
+
+	switch (tuple->nexthdr) {
+	case IPPROTO_TCP:
+	case IPPROTO_UDP:
+		/* load sport + dport in reverse order, sport=dport, dport=sport */
+		if (skb_load_bytes(skb, l4_off, &key.dport, 4) < 0)
+			return DROP_CT_INVALID_HDR;
+		break;
+	default:
+		/* ignore */
+		return 0;
+	}
+
+	csum_l4_offset_and_flags(tuple->nexthdr, &csum);
+
+	cilium_trace(skb, DBG_REV_PROXY_LOOKUP, key.sport << 16 | key.dport, key.saddr);
+
+	val = map_lookup_elem(&cilium_proxy4, &key);
+	if (!val)
+		return 0;
+
+	new_saddr = val->orig_daddr;
+	new_sport = val->orig_dport;
+	old_sport = key.dport;
+
+	cilium_trace(skb, DBG_REV_PROXY_FOUND, new_saddr, ntohs(new_sport));
+	cilium_trace_capture(skb, DBG_CAPTURE_PROXY_PRE, 0);
+
+	if (l4_modify_port(skb, l4_off, TCP_SPORT_OFF, &csum, new_sport, old_sport) < 0)
+		return DROP_WRITE_ERROR;
+
+	if (skb_store_bytes(skb, ETH_HLEN + offsetof(struct iphdr, saddr), &new_saddr, 4, 0) < 0)
+		return DROP_WRITE_ERROR;
+
+	if (l3_csum_replace(skb, ETH_HLEN + offsetof(struct iphdr, check), old_saddr, new_saddr, 4) < 0)
+		return DROP_CSUM_L3;
+
+	if (csum.offset &&
+	    csum_l4_replace(skb, l4_off, &csum, old_saddr, new_saddr, 4 | BPF_F_PSEUDO_HDR) < 0)
+		return DROP_CSUM_L4;
+
+	cilium_trace_capture(skb, DBG_CAPTURE_PROXY_POST, 0);
+
+	return 0;
+}
+
 static inline int handle_ipv4(struct __sk_buff *skb)
 {
 	void *data = (void *) (long) skb->data;
@@ -123,6 +182,18 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 		l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
 		secctx = derive_ipv4_sec_ctx(skb, ip4);
 		tuple.nexthdr = ip4->protocol;
+
+		ret = reverse_proxy(skb, l4_off, ip4, &tuple);
+		/* DIRECT PACKET READ INVALID */
+		if (IS_ERR(ret))
+			return ret;
+
+		data = (void *) (long) skb->data;
+		data_end = (void *) (long) skb->data_end;
+		ip4 = data + ETH_HLEN;
+		if (data + sizeof(*ip4) + ETH_HLEN > data_end)
+			return DROP_INVALID;
+
 		ret = ipv4_local_delivery(skb, ETH_HLEN, l4_off, secctx, ip4);
 		if (ret != DROP_NO_LXC)
 			return ret;

bpf/lib/common.h
@@ -189,6 +189,7 @@ struct ct_entry {
 	      lb_loopback:1,
 	      reserve:12;
 	__u16 rev_nat_index;
+	__u16 proxy_port;
 };
 
 struct lb6_key {
@@ -240,7 +241,21 @@ struct ct_state {
 	__u16 loopback:1,
 	      reserved:15;
 	__u16 orig_dport;
+	__u16 proxy_port;
 	__be32 addr;
 };
+
+struct proxy4_tbl_key {
+	__be32 saddr;
+	__u16 dport; /* dport must be in front of sport, loaded with 4 bytes read */
+	__u16 sport;
+	__u8 nexthdr;
+} __attribute__((packed));
+
+struct proxy4_tbl_value {
+	__be32 orig_daddr;
+	__u16 orig_dport;
+	__u16 lifetime;
+} __attribute__((packed));
 
 #endif

commit 9412c06c258e750d7e4e9329ab93c59bf124a440

bpf: Protect ipv4_redirect_to_host_port() with LXC_IPV4
The caller is protected by LXC_IPV4 as well. Fixes the following
error:
In file included from /var/lib/cilium/bpf/bpf_lxc.c:38:
  /var/lib/cilium/bpf/lib/lxc.h:93:29: error: use of undeclared identifier 'LXC_IPV4'
  .saddr = __constant_htonl(LXC_IPV4),

Fixes: 770ad9ccb2de7cb ("proxy: Initial transparent proxy support")
Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/lib/lxc.h
@@ -83,6 +83,7 @@ static inline int valid_dst_mac(struct ethhdr *eth)
 }
 #endif
 
+#ifdef LXC_IPV4
 static inline int __inline__
 ipv4_redirect_to_host_port(struct __sk_buff *skb, struct csum_offset *csum,
 			  int l4_off, __u16 new_port, __u16 old_port, __be32 old_ip,
@@ -125,5 +126,6 @@ ipv4_redirect_to_host_port(struct __sk_buff *skb, struct csum_offset *csum,
 
 	return 0;
 }
+#endif /* LXC_IPV4 */
 
 #endif

daemon/bindata.go
@@ -983,7 +983,7 @@ func bpfLibLbH() (*asset, error) {
 	return a, nil
 }
 
-var _bpfLibLxcH = []byte("\x1f\x8b\x08\x00\x00\x09\x6e\x88\x00\xff\xa4\x56\x5d\x73\xe2\x3a\x12\x7d\xc6\xbf\xa2\xef\x4d\x55\x16\x58\x27\x81\x84\x65\x1f\x98\xa4\xca\x01\x93\xb8\x8a\x80\xcb\x98\xc9\xcd\x93\xca\xd8\xed\x58\x15\x23\xb9\x24\x99\x5c\x6a\x66\xfe\xfb\x96\x64\xf3\xb5\xc9\xcc\x64\xe6\x3e\xe4\x43\x2d\xf5\x39\x47\xa7\xbb\x05\x17\x6d\x0b\xda\x00\x43\x5e\x6c\x04\x7d\xce\x14\x34\x87\x2d\xb8\xec\x74\xfb\x67\x97\x9d\xee\x7f\xc1\x29\x55\xc6\x85\x04\x9e\xc2\x90\xe6\xb4\x5c\x59\x50\x25\x84\x19\x95\x50\x08\xfe\x2c\xa2\x15\x50\x09\xa9\x40\x04\xc9\x53\xf5\x1a\x09\x1c\xc0\x86\x97\x10\x47\x0c\x04\x26\x54\x2a\x41\x97\xa5\x42\xa0\x0a\x22\x96\x5c\x70\x01\x2b\x9e\xd0\x74\x63\x80\xa8\x82\x92\x25\x28\x40\x65\x08\x0a\xc5\xca\x90\xe9\xc5\xdd\x74\x01\x77\xc8\x50\x44\x39\xf8\xe5\x32\xa7\x31\x4c\x68\x8c\x4c\x22\x44\x12\x0a\x1d\x91\x19\x26\xb0\xac\x80\x74\xca\x58\xab\x98\xd7\x2a\x60\xcc\x4b\x96\x44\x8a\x72\x36\x00\xa4\x2a\x43\x01\x6b\x14\x92\x72\x06\x97\x5b\x92\x1a\xd1\x06\x2e\x0c\x4a\x33\x52\x5a\xbc\x00\x5e\xe8\xc4\x16\x44\x6c\x03\x79\xa4\xf6\xb9\xe7\xdf\xb3\x60\x7f\xd3\x04\x28\x33\xe8\x19\x2f\x10\x54\x16\x29\x7d\xcd\x57\x9a\xe7\xb0\x44\x28\x25\xa6\x65\x6e\x1b\x8c\x65\xa9\xe0\xd1\x0b\xef\x67\x8b\x10\x9c\xe9\x13\x3c\x3a\x41\xe0\x4c\xc3\xa7\x01\xbc\x52\x95\xf1\x52\x01\xae\xb1\xc2\xa2\xab\x22\xa7\x98\xc0\x6b\x24\x44\xc4\xd4\x06\x78\x6a\x20\x1e\xdc\x60\x78\xef\x4c\x43\xe7\xd6\x9b\x78\xe1\x13\x70\x01\x63\x2f\x9c\xba\xf3\x39\x8c\x67\x01\x38\xe0\x3b\x41\xe8\x0d\x17\x13\x27\x00\x7f\x11\xf8\xb3\xb9\x7b\x0e\x30\x47\x2d\x0c\x0d\xc2\x0f\x8c\x4e\x4d\xb1\x04\x42\x82\x2a\xa2\xb9\xdc\x5d\xfe\x89\x97\x20\x33\x5e\xe6\x09\x64\xd1\x1a\x41\x60\x8c\x74\x8d\x09\x44\x10\xf3\x62\xf3\xf3\x1a\x1a\x94\x28\xe7\xec\xd9\x5c\x15\xd4\x81\x9b\x03\xa0\x29\x30\xae\x6c\x78\x15\x54\x21\x28\xfe\xb6\xba\x26\x7f\x5f\x61\x1b\x3c\x16\x9f\xdb\xf0\x9f\x2e\x8c\x45\xc4\x5e\x72\xca\x60\xae\x6c\x18\xd3\x54\x65\x30\xce\x39\x17\x36\xdc\x72\xa9\xf4\xd1\x07\x07\xa0\x73\xd9\xed\x76\xce\xba\x57\x9d\x2e\xc0\x62\xee\x58\xd0\xbe\xb0\x4e\x68\xca\x12\x4c\x81\x90\x89\x77\x4b\x26\x7f\x0d\xc9\x3d\xb1\x4e\x12\x4c\x29\xc3\xe3\xa0\x75\x42\x59\x9c\x97\x09\xc2\x9f\x31\x5f\xad\x38\x3b\xcf\xfe\x3c\x88\xd1\x62\xdd\x7f\x13\xe9\x1d\x47\x50\x65\xc7\x81\x64\xf9\x7c\x1c\x88\x65\xb9\x3a\x8e\xe4\x06\x63\xa7\x73\xe4\xcd\x9d\xdb\x89\x4b\xe6\x0f\xce\x90\x7c\x76\x03\x6f\xec\x0d\x9d\xd0\x9b\x4d\x2d\xa9\x22\x45\x63\xa0\x2c\xd7\xd2\x29\x53\xb0\x8e\x72\x9a\x10\x29\x62\xb2\x8a\xe2\xa6\x54\xa2\x8c\x15\xa0\xca\xb2\x44\x40\x1b\x55\xd6\xb2\xbe\x58\x8d\x92\xe9\xe1\x58\x45\x71\x94\x24\xa2\x4a\x81\x6b\xd0\x77\x7e\x70\x86\x03\xcb\x6a\x08\x54\xa5\x60\xf0\x07\xaa\x8c\xe8\x33\xf1\xaa\x68\x9e\x9a\x73\x36\x34\x8f\xb3\xdb\x2d\x38\x45\x95\x9d\xdd\x64\x44\xf2\x52\xc4\xd8\x1a\x58\xdf\xac\x13\xcc\x25\xfe\xae\xbc\x9a\xbd\x5b\x01\xb1\x84\xa6\xef\x78\xe1\xf9\x1f\xb7\x82\x16\x5b\x2a\x5d\x31\xc3\x45\x8b\xfe\x81\x15\xeb\xfe\x1b\x27\x3c\xff\xd0\x08\x9d\xb7\x73\xa2\x79\x94\xa4\x0d\xa0\x45\xff\xec\x46\xea\xa5\x0d\x95\x4f\xc6\x85\x9f\x88\x5a\xf7\xf6\xb2\x6a\x51\x3d\x2d\xea\x84\xa6\xfa\xaa\x95\x8a\xcf\xbd\x9d\x0a\x5a\xf4\x6a\x16\xf8\xe3\x7a\xb7\x3d\xa8\xcd\x6e\x5c\xb4\x61\x18\xb1\x7f\x29\x90\xc8\x12\xf0\xfc\x75\xaf\x9a\xaf\xea\x5f\x9d\x86\x52\xea\x17\x2c\xe6\x2c\xa5\xcf\xa5\xc0\x44\x4f\xc3\x16\xbd\x33\xd8\x9a\xfd\x91\xf2\xfd\xc0\xd2\xc3\xf2\xfd\xa6\x03\x1f\x6a\x81\xd1\x87\xc7\x21\x91\xea\xd7\xc7\x61\x3a\x1b\xb9\xff\x6c\x1e\x12\x94\xea\x03\xd3\xf0\x13\x75\xef\x59\xf1\x16\x8a\x90\x6a\x41\x88\xa5\x6d\x25\xfa\x13\x59\x60\xac\x88\xe2\x24\xe3\x52\x91\x82\x0b\xb5\xa5\x20\x44\xbe\x90\x65\x99\xa6\xd0\x96\x2f\x4b\x1b\xea\xb0\x7e\x88\x08\x4f\x53\x89\x0a\xda\x7a\x61\x5b\x8d\x46\x03\x0c\x7e\xde\xd3\x3b\x36\x10\x52\x76\xfb\xc0\xf0\xd5\x00\x6e\xd7\x3c\x4f\x76\xeb\x25\x5e\x5d\x9a\x00\x2d\xea\xfc\x7d\xa3\xf4\x88\x56\x54\x16\x39\x42\xdb\xfc\x31\x37\xac\x73\x8c\x4c\x5a\xc0\x35\xe8\xb6\x26\x77\x4e\xe8\x3e\x3a\x4f\x03\xab\x51\xe7\x17\x82\xff\xbd\xe9\x11\xb5\xcc\xc9\x0b\x6e\x40\xff\x5c\xc3\x17\xab\xd1\x38\xaf\x66\xe2\x1a\x08\x89\x39\x93\x2a\x62\x8a\x64\x8a\xb3\xbc\xb9\x9d\x91\x96\x6d\x8e\x69\x89\x70\x0d\x86\xf9\xec\xc6\x2c\xcd\x46\x52\x6f\xec\xae\xa5\x83\x0c\xff\x56\x99\x81\xad\xcf\xd7\x01\xdb\x6a\x7c\x7b\x57\xd4\x3a\xca\x4b\x84\xea\x77\x2d\x8c\x0b\xfa\x4c\x92\x5a\xdd\xde\x93\x3a\x5e\xb3\xee\xcc\xd3\x3b\x39\x4d\x51\xd1\x95\x46\xb8\xea\x77\x2a\x2e\xab\x11\x9b\x2f\x65\x44\x89\x28\x46\x12\x47\x85\x2a\x05\x36\x4d\xe9\x46\xb7\x77\x64\xe8\xf8\xe1\x22\x70\x89\x1f\xcc\xfe\x7a\x22\x7e\xe0\xda\x3b\xcc\x96\xce\xa6\x29\x34\xf3\x1e\xa9\xbe\x8d\xd5\x8d\xa0\x73\xb7\x35\x0d\x87\x3e\x19\xf9\xb3\x20\x24\xb3\xf1\xd8\x86\x83\xca\x1f\x14\x7a\x87\x08\x9f\xa0\xd3\xb2\x1a\xdb\xbe\x1c\x05\x33\x9f\x3c\x06\x5e\xe8\x12\x37\x08\x66\xc1\x96\x50\xbe\x2c\x89\x54\x5c\x20\x59\x6e\x14\xca\x8a\xd1\x0d\xef\xc9\xfd\xc4\x9d\xc2\xbf\xa1\xea\x33\x9e\x1e\x3d\x00\x36\x18\xb3\x5a\x36\x9c\xd6\xdd\x60\x43\xcf\x86\xce\x47\x49\xf3\x2b\x62\x9a\x58\x60\x91\x47\x31\x7e\x94\x34\xce\x30\x7e\x69\xd9\xdb\x0a\xc1\x9e\xfb\x5d\xe2\xe1\x7c\xf1\x40\x26\x57\x5b\x52\xcd\x78\x76\x53\xcf\xcd\xe9\xa9\xa5\x7d\xab\x66\x29\xef\x1d\x2b\xd9\x1a\x6e\x1c\x7e\x87\x0d\xbe\xc2\xad\x3f\x26\x63\xe2\xcf\xdd\xc5\x68\x46\xee\x47\xc1\x0f\x04\xf4\x7e\xb9\x33\x66\xf3\xd0\xde\x95\xb4\xf5\xff\xe9\xfb\xb4\xc0\xfd\x5c\xa7\x2c\xfc\x91\x13\xba\xb6\x1e\xb6\x7a\x7c\x3e\x7d\x82\x6e\x1f\xbe\x9a\x48\x52\xb5\x86\xd9\x34\x75\x1b\x54\x8e\xac\xa2\x82\x94\x45\x12\x29\x24\x98\xe3\xaa\x79\x5a\xd3\x54\xc3\x62\xc3\xe9\x0b\x6e\xaa\x0f\xca\x12\xbf\x57\xdd\x61\x48\x86\x81\xeb\x84\x2e\x19\x3b\xde\xc4\x1d\x1d\x3c\xc3\x1d\xf3\xc9\x52\xbf\x85\xff\x0b\x00\x00\xff\xff\x01\x59\xc9\x72\xda\x0c\x00\x00")
+var _bpfLibLxcH = []byte("\x1f\x8b\x08\x00\x00\x09\x6e\x88\x00\xff\xa4\x56\x5d\x73\xe2\x3a\x12\x7d\xc6\xbf\xa2\xef\x4d\x55\x16\x58\x27\x81\x84\x65\x1f\x98\xa4\xca\x01\x93\xb8\x8a\x80\xcb\x98\xc9\xcd\x93\xca\xd8\xed\x58\x15\x23\xb9\x24\x99\x5c\x6a\x66\xfe\xfb\x96\x64\xf3\xb5\xc9\xcc\x64\xe6\x3e\xe4\x43\x2d\xf5\x39\x47\xa7\xbb\x05\x17\x6d\x0b\xda\x00\x43\x5e\x6c\x04\x7d\xce\x14\x34\x87\x2d\xb8\xec\x74\xfb\x67\x97\x9d\xee\x7f\xc1\x29\x55\xc6\x85\x04\x9e\xc2\x90\xe6\xb4\x5c\x59\x50\x25\x84\x19\x95\x50\x08\xfe\x2c\xa2\x15\x50\x09\xa9\x40\x04\xc9\x53\xf5\x1a\x09\x1c\xc0\x86\x97\x10\x47\x0c\x04\x26\x54\x2a\x41\x97\xa5\x42\xa0\x0a\x22\x96\x5c\x70\x01\x2b\x9e\xd0\x74\x63\x80\xa8\x82\x92\x25\x28\x40\x65\x08\x0a\xc5\xca\x90\xe9\xc5\xdd\x74\x01\x77\xc8\x50\x44\x39\xf8\xe5\x32\xa7\x31\x4c\x68\x8c\x4c\x22\x44\x12\x0a\x1d\x91\x19\x26\xb0\xac\x80\x74\xca\x58\xab\x98\xd7\x2a\x60\xcc\x4b\x96\x44\x8a\x72\x36\x00\xa4\x2a\x43\x01\x6b\x14\x92\x72\x06\x97\x5b\x92\x1a\xd1\x06\x2e\x0c\x4a\x33\x52\x5a\xbc\x00\x5e\xe8\xc4\x16\x44\x6c\x03\x79\xa4\xf6\xb9\xe7\xdf\xb3\x60\x7f\xd3\x04\x28\x33\xe8\x19\x2f\x10\x54\x16\x29\x7d\xcd\x57\x9a\xe7\xb0\x44\x28\x25\xa6\x65\x6e\x1b\x8c\x65\xa9\xe0\xd1\x0b\xef\x67\x8b\x10\x9c\xe9\x13\x3c\x3a\x41\xe0\x4c\xc3\xa7\x01\xbc\x52\x95\xf1\x52\x01\xae\xb1\xc2\xa2\xab\x22\xa7\x98\xc0\x6b\x24\x44\xc4\xd4\x06\x78\x6a\x20\x1e\xdc\x60\x78\xef\x4c\x43\xe7\xd6\x9b\x78\xe1\x13\x70\x01\x63\x2f\x9c\xba\xf3\x39\x8c\x67\x01\x38\xe0\x3b\x41\xe8\x0d\x17\x13\x27\x00\x7f\x11\xf8\xb3\xb9\x7b\x0e\x30\x47\x2d\x0c\x0d\xc2\x0f\x8c\x4e\x4d\xb1\x04\x42\x82\x2a\xa2\xb9\xdc\x5d\xfe\x89\x97\x20\x33\x5e\xe6\x09\x64\xd1\x1a\x41\x60\x8c\x74\x8d\x09\x44\x10\xf3\x62\xf3\xf3\x1a\x1a\x94\x28\xe7\xec\xd9\x5c\x15\xd4\x81\x9b\x03\xa0\x29\x30\xae\x6c\x78\x15\x54\x21\x28\xfe\xb6\xba\x26\x7f\x5f\x61\x1b\x3c\x16\x9f\xdb\xf0\x9f\x2e\x8c\x45\xc4\x5e\x72\xca\x60\xae\x6c\x18\xd3\x54\x65\x30\xce\x39\x17\x36\xdc\x72\xa9\xf4\xd1\x07\x07\xa0\x73\xd9\xed\x76\xce\xba\x57\x9d\x2e\xc0\x62\xee\x58\xd0\xbe\xb0\x4e\x68\xca\x12\x4c\x81\x90\x89\x77\x4b\x26\x7f\x0d\xc9\x3d\xb1\x4e\x12\x4c\x29\xc3\xe3\xa0\x75\x42\x59\x9c\x97\x09\xc2\x9f\x31\x5f\xad\x38\x3b\xcf\xfe\x3c\x88\xd1\x62\xdd\x7f\x13\xe9\x1d\x47\x50\x65\xc7\x81\x64\xf9\x7c\x1c\x88\x65\xb9\x3a\x8e\xe4\x06\x63\xa7\x73\xe4\xcd\x9d\xdb\x89\x4b\xe6\x0f\xce\x90\x7c\x76\x03\x6f\xec\x0d\x9d\xd0\x9b\x4d\x2d\xa9\x22\x45\x63\xa0\x2c\xd7\xd2\x29\x53\xb0\x8e\x72\x9a\x10\x29\x62\xb2\x8a\xe2\xa6\x54\xa2\x8c\x15\xa0\xca\xb2\x44\x40\x1b\x55\xd6\xb2\xbe\x58\x8d\x92\xe9\xe1\x58\x45\x71\x94\x24\xa2\x4a\x81\x6b\xd0\x77\x7e\x70\x86\x03\xcb\x6a\x08\x54\xa5\x60\xf0\x07\xaa\x8c\xe8\x33\xf1\xaa\x68\x9e\x9a\x73\x36\x34\x8f\xb3\xdb\x2d\x38\x45\x95\x9d\xdd\x64\x44\xf2\x52\xc4\xd8\x1a\x58\xdf\xac\x13\xcc\x25\xfe\xae\xbc\x9a\xbd\x5b\x01\xb1\x84\xa6\xef\x78\xe1\xf9\x1f\xb7\x82\x16\x5b\x2a\x5d\x31\xc3\x45\x8b\xfe\x81\x15\xeb\xfe\x1b\x27\x3c\xff\xd0\x08\x9d\xb7\x73\xa2\x79\x94\xa4\x0d\xa0\x45\xff\xec\x46\xea\xa5\x0d\x95\x4f\xc6\x85\x9f\x88\x5a\xf7\xf6\xb2\x6a\x51\x3d\x2d\xea\x84\xa6\xfa\xaa\x95\x8a\xcf\xbd\x9d\x0a\x5a\xf4\x6a\x16\xf8\xe3\x7a\xb7\x3d\xa8\xcd\x6e\x5c\xb4\x61\x18\xb1\x7f\x29\x90\xc8\x12\xf0\xfc\x75\xaf\x9a\xaf\xea\x5f\x9d\x86\x52\xea\x17\x2c\xe6\x2c\xa5\xcf\xa5\xc0\x44\x4f\xc3\x16\xbd\x33\xd8\x9a\xfd\x91\xf2\xfd\xc0\xd2\xc3\xf2\xfd\xa6\x03\x1f\x6a\x81\xd1\x87\xc7\x21\x91\xea\xd7\xc7\x61\x3a\x1b\xb9\xff\x6c\x1e\x12\x94\xea\x03\xd3\xf0\x13\x75\xdf\xb1\xe2\xa8\x43\xde\x42\x13\x52\x2d\x08\xb1\xb4\xcd\x44\x7f\x42\x0b\x8c\x15\x51\x9c\x64\x5c\x2a\x52\x70\xa1\xb6\x94\x84\xc8\x17\xb2\x2c\xd3\x14\xda\xf2\x65\x69\x43\x1d\xd6\x0f\x13\xe1\x69\x2a\x51\x41\x5b\x2f\x6c\xab\xd1\x68\x80\xc1\xcf\x7b\x7a\xc7\x06\x42\xca\x6e\x1f\x18\xbe\x1a\xc0\xed\x9a\xe7\xc9\x6e\xbd\xc4\xab\x4b\x13\xa0\x45\x9d\xbf\x6f\x9c\x1e\xd1\x8a\xca\x22\x47\x68\x9b\x3f\xe6\xc6\x75\x8e\x91\x49\x0b\xb8\x06\x7d\x47\x72\xe7\x84\xee\xa3\xf3\x34\xb0\x1a\x75\x7e\x21\xf8\xdf\x9b\x1e\x51\xcb\x9c\xbc\xe0\x06\xf4\xcf\x35\x7c\xb1\x1a\x8d\xf3\x6a\x46\xae\x81\x90\x98\x33\xa9\x22\xa6\x48\xa6\x38\xcb\x9b\x5b\xc3\x5a\xb6\x39\xa6\x25\xc2\x35\x18\xe6\xb3\x1b\xb3\x34\x1b\x49\xbd\xb1\xbb\x96\x0e\x32\xfc\x5b\x65\x06\xb6\x3e\x5f\x07\x6c\xab\xf1\xed\x5d\x51\xeb\x28\x2f\x11\xaa\xdf\xb5\x30\x2e\xe8\x33\x49\x6a\x75\x7b\x4f\xea\x78\xcd\xba\x33\x4f\xef\xe4\x34\x45\x45\x57\x1a\xe1\xaa\xdf\xa9\xb8\xac\x46\x6c\xbe\xa4\x11\x25\xa2\x18\x49\x1c\x15\xaa\x14\xd8\x34\xa5\x1b\xdd\xde\x91\xa1\xe3\x87\x8b\xc0\x25\x7e\x30\xfb\xeb\x89\xf8\x81\x6b\xef\x30\x5b\x3a\x9b\xa6\xd0\xcc\x7b\xa4\xfa\x76\x56\x37\x82\xce\xdd\xd6\x34\x1c\xfa\x64\xe4\xcf\x82\x90\xcc\xc6\x63\x1b\x0e\x2a\x7f\x50\xe8\x1d\x22\x7c\x82\x4e\xcb\x6a\x6c\xfb\x74\x14\xcc\x7c\xf2\x18\x78\xa1\x4b\xdc\x20\x98\x05\x5b\x42\xf9\xb2\x24\x52\x71\x81\x64\xb9\x51\x28\x2b\x46\x37\xbc\x27\xf7\x13\x77\x0a\xff\x86\xaa\xcf\x78\x7a\xf4\x20\xd8\x60\xcc\x6a\xd9\x70\x5a\x77\x83\x0d\x3d\x1b\x3a\x1f\x25\xcd\xaf\x88\x69\x62\x81\x45\x1e\xc5\xf8\x51\xd2\x38\xc3\xf8\xa5\x65\x6f\x2b\x04\x7b\xee\x77\x89\x87\xf3\xc5\x03\x99\x5c\x6d\x49\x35\xe3\xd9\x4d\x3d\x37\xa7\xa7\x96\xf6\xad\x9a\xa5\xbc\x77\xac\x64\x6b\xb8\x71\xf8\x1d\x36\xf8\x0a\xb7\xfe\x98\x8c\x89\x3f\x77\x17\xa3\x19\xb9\x1f\x05\x3f\x10\xd0\xfb\xe5\xce\x98\xcd\x43\x7b\x57\xd2\xd6\xff\xa7\xef\xd3\x02\xf7\x73\x9d\xb2\xf0\x47\x4e\xe8\xda\x7a\xd8\xea\xf1\xf9\xf4\x09\xba\x7d\xf8\x6a\x22\x49\xd5\x1a\x66\xd3\xd4\x6d\x50\x39\xb2\x8a\x0a\x52\x16\x49\xa4\x90\x60\x8e\xab\xe6\x69\x4d\x53\x0d\x8b\x0d\xa7\x2f\xb8\xa9\x3e\x38\x4b\xfc\x5e\x75\x87\x21\x19\x06\xae\x13\xba\x64\xec\x78\x13\x77\x74\xf0\x2c\x77\xf6\x4f\x23\x5c\xb4\x77\xaf\xa2\xfe\x70\xdb\xbe\x98\xff\x0b\x00\x00\xff\xff\xbf\xba\x51\xac\x00\x0d\x00\x00")
 
 func bpfLibLxcHBytes() ([]byte, error) {
 	return bindataRead(
@@ -998,7 +998,7 @@ func bpfLibLxcH() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "bpf/lib/lxc.h", size: 3290, mode: os.FileMode(416), modTime: time.Unix(1450269211, 0)}
+	info := bindataFileInfo{name: "bpf/lib/lxc.h", size: 3328, mode: os.FileMode(416), modTime: time.Unix(1450269211, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }