af71e52dabcea121bb39140186814e7a55c87306
André Martins <andre@cilium.io>
2017-04-03 20:14:31 +0100

    option: fixed broken option_test.go
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/option/option_test.go
+++ pkg/option/option_test.go
@@ -15,8 +15,15 @@
 package option
 
 import (
+	"testing"
+
 	. "gopkg.in/check.v1"
 )
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
 
 type OptionSuite struct{}
 
