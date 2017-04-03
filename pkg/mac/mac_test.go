6ef6f1059b5f9040d612a72200e6fe5fc0662c58
André Martins <andre@cilium.io>
2017-04-03 20:14:16 +0100

    mac: fixed broken mac_test.go
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/mac/mac_test.go
+++ pkg/mac/mac_test.go
@@ -16,11 +16,16 @@ package mac
 
 import (
 	"encoding/json"
+	"testing"
 
 	. "gopkg.in/check.v1"
 )
 
 // Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
 type MACSuite struct{}
 
 var _ = Suite(&MACSuite{})
