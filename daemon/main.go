cilium/cilium

commit 880f9cc4159198206678ee628254f2e9579108ca

daemon: Separate state directory inside runtime directory
Moves all endpoint state into the state/ subdirectory inside the
existing runtime directory. This allows to establish different
permissions for the default runtime directory which can be mounted
into container images and the actual state which must be protected.

Allows moving cilium.sock back into the runtime directory for easy
sharing in and out of containers.

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/main.go
@@ -183,15 +183,16 @@ func checkMinRequirements() {
 	log.Info("linking environment: OK!")
 
 	// Checking for bpf_features
-	globalsDir := filepath.Join(config.RunDir, "globals")
-	if err := os.MkdirAll(globalsDir, defaults.RuntimePathRights); err != nil {
+	globalsDir := filepath.Join(config.StateDir, "globals")
+	if err := os.MkdirAll(globalsDir, defaults.StateDirRights); err != nil {
 		log.Fatalf("Could not create runtime directory %q: %s", globalsDir, err)
 	}
 	if err := os.Chdir(config.LibDir); err != nil {
 		log.Fatalf("Could not change to runtime directory %q: %s",
 			config.LibDir, err)
 	}
-	if err := exec.Command("./bpf/run_probes.sh", "./bpf", config.RunDir).Run(); err != nil {
+	probeScript := filepath.Join(config.BpfDir, "run_probes.sh")
+	if err := exec.Command(probeScript, config.BpfDir, config.StateDir).Run(); err != nil {
 		log.Fatalf("BPF Verifier: NOT OK. Unable to run checker for bpf_features: %s", err)
 	}
 	if _, err := os.Stat(filepath.Join(globalsDir, "bpf_features.h")); os.IsNotExist(err) {
@@ -322,8 +323,14 @@ func initConfig() {
 	if err := os.MkdirAll(config.RunDir, defaults.RuntimePathRights); err != nil {
 		log.Fatalf("Could not create runtime directory %q: %s", config.RunDir, err)
 	}
+
+	config.StateDir = filepath.Join(config.RunDir, defaults.StateDir)
+	if err := os.MkdirAll(config.StateDir, defaults.StateDirRights); err != nil {
+		log.Fatalf("Could not create state directory %q: %s", config.StateDir, err)
+	}
+
 	if err := os.MkdirAll(config.LibDir, defaults.RuntimePathRights); err != nil {
-		log.Fatalf("Could not create library directory %q: %s", config.RunDir, err)
+		log.Fatalf("Could not create library directory %q: %s", config.LibDir, err)
 	}
 	if !config.KeepTemplates {
 		if err := RestoreAssets(config.LibDir, defaults.BpfDir); err != nil {

daemon/config.go
@@ -62,6 +62,9 @@ type Config struct {
 	KeepConfig    bool // Keep configuration of existing endpoints when starting up.
 	KeepTemplates bool // Do not overwrite the template files
 
+	// StateDir is the directory where runtime state of endpoints is stored
+	StateDir string
+
 	// Options changeable at runtime
 	Opts   *option.BoolOptions
 	OptsMU sync.RWMutex

daemon/daemon.go
@@ -93,8 +93,9 @@ func (d *Daemon) WriteEndpoint(e *endpoint.Endpoint) error {
 	return nil
 }
 
-func (d *Daemon) GetRuntimeDir() string {
-	return d.conf.RunDir
+// GetStateDir returns the path to the state directory
+func (d *Daemon) GetStateDir() string {
+	return d.conf.StateDir
 }
 
 func (d *Daemon) GetBpfDir() string {
@@ -233,13 +234,13 @@ func (d *Daemon) compileBase() error {
 			mode = "direct"
 		}
 
-		args = []string{d.conf.BpfDir, d.conf.RunDir, d.conf.NodeAddress.String(), d.conf.NodeAddress.IPv4Address.String(), mode, d.conf.Device}
+		args = []string{d.conf.BpfDir, d.conf.StateDir, d.conf.NodeAddress.String(), d.conf.NodeAddress.IPv4Address.String(), mode, d.conf.Device}
 	} else {
 		if d.conf.IsLBEnabled() {
 			//FIXME: allow LBMode in tunnel
 			return fmt.Errorf("Unable to run LB mode with tunnel mode")
 		}
-		args = []string{d.conf.BpfDir, d.conf.RunDir, d.conf.NodeAddress.String(), d.conf.NodeAddress.IPv4Address.String(), d.conf.Tunnel}
+		args = []string{d.conf.BpfDir, d.conf.StateDir, d.conf.NodeAddress.String(), d.conf.NodeAddress.IPv4Address.String(), d.conf.Tunnel}
 	}
 
 	out, err := exec.Command(filepath.Join(d.conf.BpfDir, "init.sh"), args...).CombinedOutput()
@@ -289,14 +290,14 @@ func (d *Daemon) useK8sNodeCIDR(nodeName string) error {
 }
 
 func (d *Daemon) init() error {
-	globalsDir := filepath.Join(d.conf.RunDir, "globals")
+	globalsDir := filepath.Join(d.conf.StateDir, "globals")
 	if err := os.MkdirAll(globalsDir, defaults.RuntimePathRights); err != nil {
 		log.Fatalf("Could not create runtime directory %s: %s", globalsDir, err)
 	}
 
-	if err := os.Chdir(d.conf.RunDir); err != nil {
+	if err := os.Chdir(d.conf.StateDir); err != nil {
 		log.Fatalf("Could not change to runtime directory %s: \"%s\"",
-			d.conf.RunDir, err)
+			d.conf.StateDir, err)
 	}
 
 	f, err := os.Create("./globals/node_config.h")
@@ -535,7 +536,7 @@ func NewDaemon(c *Config) (*Daemon, error) {
 	d.l7Proxy = proxy.NewProxy(10000, 20000)
 
 	if c.RestoreState {
-		if err := d.SyncState(d.conf.RunDir, true); err != nil {
+		if err := d.SyncState(d.conf.StateDir, true); err != nil {
 			log.Warningf("Error while recovering endpoints: %s\n", err)
 		}
 		if err := d.SyncLBMap(); err != nil {

daemon/defaults/defaults.go
@@ -19,16 +19,22 @@ const (
 	RuntimePath = "/var/run/cilium"
 
 	// RuntimePathRights are the default access rights of the RuntimePath directory
-	RuntimePathRights = 0770
+	RuntimePathRights = 0775
 
-	// BpfDir is the default path for template files relative to RuntimePath
+	// StateDirRights are the default access rights of the state directory
+	StateDirRights = 0770
+
+	//StateDir is the default path for the state directory relative to RuntimePath
+	StateDir = "state"
+
+	// BpfDir is the default path for template files relative to LibDir
 	BpfDir = "bpf"
 
 	// LibraryPath is the default path to the cilium libraries directory
 	LibraryPath = "/var/lib/cilium"
 
 	// SockPath is the path to the UNIX domain socket exposing the API to clients locally
-	SockPath = "/var/run/cilium.sock"
+	SockPath = RuntimePath + "/cilium.sock"
 
 	// SockPathEnv is the environment variable to overwrite SockPath
 	SockPathEnv = "CILIUM_SOCK"

daemon/labels_test.go
@@ -72,6 +72,7 @@ func (ds *DaemonSuite) SetUpTest(c *C) {
 		Opts:    option.NewBoolOptions(&options.Library),
 	}
 	daemonConf.RunDir = tempRunDir
+	daemonConf.StateDir = tempRunDir
 	daemonConf.LXCMap = nil
 	daemonConf.NodeAddress = nodeAddress
 	daemonConf.DockerEndpoint = "tcp://127.0.0.1"

pkg/endpoint/bpf.go
@@ -220,7 +220,7 @@ func writeGeneve(prefix string, e *Endpoint) ([]byte, error) {
 
 func (e *Endpoint) runInit(owner Owner, prefix string) error {
 	libdir := owner.GetBpfDir()
-	rundir := owner.GetRuntimeDir()
+	rundir := owner.GetStateDir()
 	args := []string{libdir, rundir, prefix, e.IfName}
 
 	out, err := exec.Command(filepath.Join(libdir, "join_ep.sh"), args...).CombinedOutput()

pkg/endpoint/owner.go
@@ -49,8 +49,8 @@ type Owner interface {
 	// Must synchronize endpoint object with datapath
 	WriteEndpoint(ep *Endpoint) error
 
-	// Must return path to runtime directory
-	GetRuntimeDir() string
+	// GetStateDir must return path to the state directory
+	GetStateDir() string
 
 	// Must return path to BPF template files directory
 	GetBpfDir() string

commit 8e9236c66b352579e99ac800fab825690dc05b49

daemon: Read bpf_features.log from state directory
Also fixes 06-lb.sh to use correct state directory

Fixes: 880f9cc41 ("daemon: Separate state directory inside runtime directory")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/main.go
@@ -198,7 +198,7 @@ func checkMinRequirements() {
 	if _, err := os.Stat(filepath.Join(globalsDir, "bpf_features.h")); os.IsNotExist(err) {
 		log.Fatalf("BPF Verifier: NOT OK. Unable to read bpf_features.h: %s", err)
 	}
-	bpfLogPath := filepath.Join(config.RunDir, "bpf_features.log")
+	bpfLogPath := filepath.Join(config.StateDir, "bpf_features.log")
 	if _, err := os.Stat(bpfLogPath); os.IsNotExist(err) {
 		log.Infof("BPF Verifier: OK!")
 	} else if err == nil {

tests/06-lb.sh
@@ -213,7 +213,7 @@ ip route add 2.2.2.2/32 via 3.3.3.3 src $SRC
 
 ip link set lbtest2 up
 LIB=/var/lib/cilium/bpf
-RUN=/var/run/cilium
+RUN=/var/run/cilium/state
 NH_IFINDEX=$(cat /sys/class/net/cilium_host/ifindex)
 NH_MAC=$(ip link show cilium_host | grep ether | awk '{print $2}')
 NH_MAC="{.addr=$(mac2array $NH_MAC)}"