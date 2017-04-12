9d420ac940bb18ef6c3ec32d3b1e5094f74774c9
Thomas Graf <thomas@cilium.io>
2017-04-12 23:06:48 +0200

    policy: Fix policy Lookup() function
    A bug in the tree.Lookup() function caused path dependencies to
    not be correctly created. Fixes this bug and adds several additional
    unit tests to cover this better.
    
    Backports: 3697d305e3a055c6ff858086d197ca04a92b39f8
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/policy/tree_test.go
+++ pkg/policy/tree_test.go
@@ -15,6 +15,10 @@
 package policy
 
 import (
+	"encoding/json"
+
+	"github.com/cilium/cilium/pkg/labels"
+
 	. "gopkg.in/check.v1"
 )
 
@@ -28,22 +32,22 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 
-	root := Node{}
+	root := NewNode(RootNodeName, nil)
 
 	// adding a root node should succeed
-	added, err := tree.Add(RootNodeName, &root)
+	added, err := tree.Add(RootNodeName, root)
 	c.Assert(added, Equals, true)
 	c.Assert(err, IsNil)
 
 	// lookup of root node should succeed now
 	n, p = tree.Lookup(RootNodeName)
-	c.Assert(n, Equals, &root)
+	c.Assert(n, Equals, root)
 	c.Assert(n.Name, Equals, RootNodeName)
 	c.Assert(p, IsNil)
 
 	// lookup of empty path should return root node
 	n, p = tree.Lookup("")
-	c.Assert(n, Equals, &root)
+	c.Assert(n, Equals, root)
 	c.Assert(n.Name, Equals, RootNodeName)
 	c.Assert(p, IsNil)
 
@@ -55,7 +59,7 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(p, IsNil)
 
 	// Added a child if no root node exist must add parents of that node
-	foo := &Node{Name: "foo"}
+	foo := NewNode("foo", nil)
 	added, err = tree.Add(RootNodeName+".bar", foo)
 	c.Assert(added, Equals, true)
 	c.Assert(err, IsNil)
@@ -71,16 +75,16 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(n, Not(IsNil))
 	c.Assert(p, IsNil)
 
-	fooNode := Node{}
-	root = Node{
+	fooNode := NewNode("foo", nil)
+	root = &Node{
 		Children: map[string]*Node{
-			"foo": &fooNode,
+			"foo": fooNode,
 			"bar": {},
 		},
 	}
 
 	// Add root node with children, should succeed
-	added, err = tree.Add(RootNodeName, &root)
+	added, err = tree.Add(RootNodeName, root)
 	c.Assert(added, Equals, true)
 	c.Assert(err, IsNil)
 
@@ -90,14 +94,14 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	// make it the same with this hack
 	root.Children["bar"] = pBar
 	root.resolved = true
-	c.Assert(n, DeepEquals, &root)
+	c.Assert(n, DeepEquals, root)
 	c.Assert(n.Name, Equals, RootNodeName)
 	c.Assert(p, IsNil)
 
 	// lookup of child foo should succeed
 	n, p = tree.Lookup("root.foo")
-	c.Assert(n, Equals, &fooNode)
-	c.Assert(p, DeepEquals, &root)
+	c.Assert(n, Equals, fooNode)
+	c.Assert(p, DeepEquals, root)
 
 	// delete root node
 	deleted = tree.Delete("root", "")
@@ -112,4 +116,244 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	n, p = tree.Lookup("root.foo")
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
+}
+
+func (ds *PolicyTestSuite) TestLookup(c *C) {
+	//var nullPtr *Node
+
+	tree := Tree{}
+	foo := NewNode("foo", nil)
+
+	// adding foo to root
+	added, err := tree.Add(RootNodeName, foo)
+	c.Assert(added, Equals, true)
+	c.Assert(err, IsNil)
+
+	// search for foo.bar, should return nil, foo
+	n, p := tree.Lookup("foo.bar")
+	c.Assert(n, IsNil)
+	c.Assert(p, Equals, foo)
+
+	// adding bar to foo
+	bar := NewNode("bar", nil)
+	added, err = tree.Add("root.foo", bar)
+	c.Assert(added, Equals, true)
+	c.Assert(err, IsNil)
+
+	// lookup of foo
+	n, p = tree.Lookup("foo")
+	c.Assert(n, Equals, foo)
+	c.Assert(n.path, Equals, "root.foo")
+	c.Assert(n.Name, Equals, "foo")
+	c.Assert(p.Name, Equals, "root")
+
+	// lookup of bar should fail
+	n, p = tree.Lookup("bar")
+	c.Assert(n, IsNil)
+	c.Assert(p, IsNil)
+
+	// lookup of foo.bar should suceed
+	n, p = tree.Lookup("foo.bar")
+	c.Assert(n, Equals, bar)
+	c.Assert(n.path, Equals, "root.foo.bar")
+	c.Assert(n.Name, Equals, "bar")
+	c.Assert(p, Equals, foo)
+
+	// lookup of foo.bar.baz should return nil, bar
+	n, p = tree.Lookup("foo.bar.baz")
+	c.Assert(n, IsNil)
+	c.Assert(p, Equals, bar)
+
+	// adding bar to foo
+	deep := Node{Name: "deep"}
+	added, err = tree.Add("foo.bar.1.2.3", &deep)
+	c.Assert(added, Equals, true)
+	c.Assert(err, IsNil)
+
+	// lookup of foo.bar.1
+	n, p = tree.Lookup("foo.bar.1")
+	c.Assert(n.Name, Equals, "1")
+	c.Assert(p, Equals, bar)
+
+	// lookup of foo.bar.1.2
+	n, p = tree.Lookup("foo.bar.1.2")
+	c.Assert(n.Name, Equals, "2")
+	c.Assert(p.Name, Equals, "1")
+
+	// lookup of foo.bar.1.2.3.deep
+	n, p = tree.Lookup("foo.bar.1.2.3.deep")
+	c.Assert(n.Name, Equals, "deep")
+	c.Assert(p.Name, Equals, "3")
+}
+
+func (ds *PolicyTestSuite) TestAddDelete2(c *C) {
+	//var nullPtr *Node
+
+	tree := Tree{}
+	root := NewNode("io.cilium", nil)
+
+	// adding "io.cilium" to root node should succeed
+	added, err := tree.Add(RootNodeName, root)
+	c.Assert(added, Equals, true)
+	c.Assert(err, IsNil)
+
+	// Lookup io and io.cilium nodes created
+	n, p := tree.Lookup("io")
+	c.Assert(n.Name, Equals, "io")
+	c.Assert(p.Name, Equals, "root")
+	n, p = tree.Lookup("io.cilium")
+	c.Assert(n.Name, Equals, "cilium")
+	c.Assert(p.Name, Equals, "io")
+
+	// adding "k8s-app" to io.cilium.k8s node should succeed
+	k8s := NewNode("k8s-app", nil)
+	added, err = tree.Add("io.cilium.k8s", k8s)
+	c.Assert(added, Equals, true)
+	c.Assert(err, IsNil)
+
+	// Lookup io.cilium.k8s and io.cilium.k8s.k8s-app nodes created
+	n, p = tree.Lookup("io.cilium.k8s")
+	c.Assert(n, Not(IsNil))
+	c.Assert(n.Name, Equals, "k8s")
+	c.Assert(p.Name, Equals, "cilium")
+	c.Assert(p.path, Equals, "root.io.cilium")
+	n, p = tree.Lookup("io.cilium.k8s.k8s-app")
+	c.Assert(n, Equals, k8s)
+	c.Assert(p.Name, Equals, "k8s")
+	c.Assert(p.path, Equals, "root.io.cilium.k8s")
+
+	deleted := tree.Delete(RootNodeName, "")
+	c.Assert(deleted, Equals, true)
+
+	n, p = tree.Lookup(RootNodeName)
+	c.Assert(n, IsNil)
+	c.Assert(p, IsNil)
+}
+
+var defaultCtx = &SearchContext{
+	Trace: TRACE_ENABLED,
+	From: []labels.Label{
+		{Key: "id.foo", Source: "cilium"},
+	},
+	To: []labels.Label{
+		{Key: "id.bar", Source: "cilium"},
+	},
+}
+
+func (ds *PolicyTestSuite) TestAlwaysAllow(c *C) {
+	// always-accept foo of root must overwrite deny foo of child
+	policyText := `
+{
+	"name": "root",
+	"rules": [{
+		"coverage": ["id.bar"],
+		"allow": [{
+			"action": "always-accept",
+			"label": {
+				"key": "id.foo",
+				"source": "cilium"
+			}
+		}]
+	}],
+	"children": {
+		"id": {
+			"rules": [{
+				"coverage": ["bar"],
+				"allow": ["!foo"]
+			}]
+		}
+	}
+}
+`
+	node := Node{}
+	err := json.Unmarshal([]byte(policyText), &node)
+	c.Assert(err, IsNil)
+
+	tree := Tree{}
+	added, err := tree.Add("root", &node)
+	c.Assert(added, Equals, true)
+	c.Assert(err, IsNil)
+
+	decision := tree.Allows(defaultCtx)
+	c.Assert(decision, Equals, ACCEPT)
+}
+
+func (ds *PolicyTestSuite) TestDenyOverwrite(c *C) {
+	// deny foo of child must overwrite allow foo of root
+	policyText := `
+{
+	"name": "root",
+	"rules": [{
+		"coverage": ["id.bar"],
+		"allow": ["id.foo"]
+	}],
+	"children": {
+		"id": {
+			"rules": [{
+				"coverage": ["bar"],
+				"allow": ["!foo"]
+			}]
+		}
+	}
+}
+`
+	node := Node{}
+	err := json.Unmarshal([]byte(policyText), &node)
+	c.Assert(err, IsNil)
+
+	tree := Tree{}
+	added, err := tree.Add("root", &node)
+	c.Assert(added, Equals, true)
+	c.Assert(err, IsNil)
+
+	decision := tree.Allows(defaultCtx)
+	c.Assert(decision, Equals, DENY)
+}
+
+func (ds *PolicyTestSuite) TestRulePrecedence(c *C) {
+	// !id.foo rule must overwrite id.foo rule
+	policyText := `
+{
+	"name": "root",
+	"rules": [{
+		"coverage": ["id.bar"],
+		"allow": ["id.foo", "!id.foo"]
+	}]
+}
+`
+	node := Node{}
+	err := json.Unmarshal([]byte(policyText), &node)
+	c.Assert(err, IsNil)
+
+	tree := Tree{}
+	added, err := tree.Add("root", &node)
+	c.Assert(added, Equals, true)
+	c.Assert(err, IsNil)
+
+	decision := tree.Allows(defaultCtx)
+	c.Assert(decision, Equals, DENY)
+}
+
+func (ds *PolicyTestSuite) TestOutsideCoverage(c *C) {
+	// coverage of not_id rules it outside of the node path
+	policyText := `
+{
+	"name": "root",
+	"rules": [{
+		"coverage": ["id.bar"],
+		"allow": ["id.foo"]
+	}],
+	"children": {
+		"not_id": {
+			"rules": [{
+				"coverage": ["root.id.bar"],
+				"allow": ["!root.id.foo"]
+			}]
+		}
+	}
+}
+`
+	node := Node{}
+	err := json.Unmarshal([]byte(policyText), &node)
+	c.Assert(err, Not(IsNil))
 }
