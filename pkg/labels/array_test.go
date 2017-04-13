3eda71962afa4b9d5b00f16009ac1682ffca2715
Thomas Graf <thomas@cilium.io>
2017-04-13 16:42:44 +0200

    labels: New LabelArray type
    New LabelArray type with Contains() function to implement functionality
    to require multiple labels as a set.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ pkg/labels/array_test.go
@@ -0,0 +1,40 @@
+// Copyright 2016-2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package labels
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+var _ = Suite(&LabelsSuite{})
+
+func (s *LabelsSuite) TestMatches(c *C) {
+	a := LabelArray{
+		NewLabel("1", "1", "1"),
+		NewLabel("2", "2", "1"),
+		NewLabel("3", "3", "1"),
+	}
+	b := LabelArray{
+		NewLabel("1", "1", "1"),
+		NewLabel("2", "2", "1"),
+	}
+	empty := LabelArray{}
+
+	c.Assert(a.Contains(b), Equals, true)      // b is in a
+	c.Assert(b.Contains(a), Equals, false)     // a is NOT in b
+	c.Assert(a.Contains(empty), Equals, true)  // empty is in a
+	c.Assert(b.Contains(empty), Equals, true)  // empty is in b
+	c.Assert(empty.Contains(a), Equals, false) // a is NOT in empty
+}
