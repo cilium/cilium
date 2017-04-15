cilium/cilium

commit e7c1e0294d172e34a8e4fe3886c547b0545d2a12

Introducing endpoints parallelization build
With this commit it is introduced a parallelization for the endpoints
regeneration. The daemon will have worker threads as the same number of
CPUs available on the system. Each of those worker threads will
signalize the endpoints turn to build itself.

Thus, the endpoints creation/regeneration will be asynchronous where the
user should verify if the endpoint had a successful regeneration with
`cilium endpoint list`.

Due the parallelization created around the endpoints builder there was a
need to create a mutex on various structures. Those mutex should be used
whenever that same structure's values are read or written.
All private methods of those structures will assume the structure locks
are locked, all public methods will assmue those structures locks are
unlocked, except when stated otherwise such as "MethodLocked", where
the public method needs to be called with the lock locked.

Signed-off-by: André Martins <andre@cilium.io>

daemon/policy.go
@@ -65,9 +65,8 @@ func (d *Daemon) invalidateCache() {
 	}
 }
 
-func (d *Daemon) triggerPolicyUpdates(added []policy.NumericIdentity) {
-	d.endpointsMU.Lock()
-	defer d.endpointsMU.Unlock()
+// TriggerPolicyUpdates triggers policy updates for every daemon's endpoint.
+func (d *Daemon) TriggerPolicyUpdates(added []policy.NumericIdentity) {
 
 	if len(added) == 0 {
 		log.Debugf("Full policy recalculation triggered")
@@ -78,21 +77,26 @@ func (d *Daemon) triggerPolicyUpdates(added []policy.NumericIdentity) {
 		d.invalidateCache()
 	}
 
-	log.Debugf("Iterating over endpoints...")
-
-	for _, ep := range d.endpoints {
-		log.Debugf("Triggering policy update for ep %s", ep.StringID())
-		err := ep.TriggerPolicyUpdates(d)
-		if err != nil {
-			log.Warningf("Error while handling policy updates for endpoint %s: %s\n",
-				ep.StringID(), err)
-			ep.LogStatus(endpoint.Policy, endpoint.Failure, err.Error())
-		} else {
-			ep.LogStatusOK(endpoint.Policy, "Policy regenerated")
-		}
+	d.endpointsMU.RLock()
+	for k := range d.endpoints {
+		go func(ep *endpoint.Endpoint) {
+			ep.Mutex.RLock()
+			epID := ep.StringIDLocked()
+			ep.Mutex.RUnlock()
+			policyChanges, err := ep.TriggerPolicyUpdates(d)
+			if err != nil {
+				log.Warningf("Error while handling policy updates for endpoint %s: %s\n",
+					epID, err)
+				ep.LogStatus(endpoint.Policy, endpoint.Failure, err.Error())
+			} else {
+				ep.LogStatusOK(endpoint.Policy, "Policy regenerated")
+			}
+			if policyChanges {
+				ep.Regenerate(d)
+			}
+		}(d.endpoints[k])
 	}
-
-	log.Debugf("End")
+	d.endpointsMU.RUnlock()
 }
 
 // PolicyCanConsume calculates if the ctx allows the consumer to be consumed. This public
@@ -105,7 +109,7 @@ func (d *Daemon) PolicyCanConsume(ctx *policy.SearchContext) (*policy.SearchCont
 	}
 	scr := policy.SearchContextReply{}
 	d.policy.Mutex.RLock()
-	scr.Decision = d.policy.Allows(ctx)
+	scr.Decision = d.policy.AllowsRLocked(ctx)
 	d.policy.Mutex.RUnlock()
 
 	if ctx.Trace != policy.TRACE_DISABLED {
@@ -134,7 +138,7 @@ func (h *getPolicyResolve) Handle(params GetPolicyResolveParams) middleware.Resp
 	}
 
 	d.policy.Mutex.RLock()
-	verdict := d.policy.Allows(&search)
+	verdict := d.policy.AllowsRLocked(&search)
 	d.policy.Mutex.RUnlock()
 
 	result := models.PolicyTraceResult{
@@ -148,16 +152,18 @@ func (h *getPolicyResolve) Handle(params GetPolicyResolveParams) middleware.Resp
 func (d *Daemon) enablePolicyEnforcement() {
 	d.conf.Opts.Set(endpoint.OptionPolicy, true)
 
-	d.endpointsMU.Lock()
-	defer d.endpointsMU.Unlock()
-
 	enablePolicy := map[string]string{endpoint.OptionPolicy: "enabled"}
 
+	d.endpointsMU.RLock()
 	for _, ep := range d.endpoints {
-		if ep.ApplyOpts(enablePolicy) {
+		ep.Mutex.Lock()
+		optionsChanged := ep.ApplyOptsLocked(enablePolicy)
+		ep.Mutex.Unlock()
+		if optionsChanged {
 			ep.RegenerateIfReady(d)
 		}
 	}
+	d.endpointsMU.RUnlock()
 }
 
 func (d *Daemon) PolicyAdd(path string, node *policy.Node) *apierror.APIError {
@@ -172,7 +178,7 @@ func (d *Daemon) PolicyAdd(path string, node *policy.Node) *apierror.APIError {
 		return apierror.Error(PutPolicyPathFailureCode, err)
 	} else if policyModified {
 		log.Info("New policy imported, regenerating...")
-		d.triggerPolicyUpdates([]policy.NumericIdentity{})
+		d.TriggerPolicyUpdates([]policy.NumericIdentity{})
 	}
 
 	return nil
@@ -194,7 +200,7 @@ func (d *Daemon) PolicyDelete(path, cover256Sum string) *apierror.APIError {
 		return apierror.New(DeletePolicyPathNotFoundCode, "policy not found")
 	}
 
-	d.triggerPolicyUpdates([]policy.NumericIdentity{})
+	d.TriggerPolicyUpdates([]policy.NumericIdentity{})
 	return nil
 }
 

pkg/policy/node.go
@@ -44,6 +44,25 @@ func NewNode(name string, parent *Node) *Node {
 	}
 }
 
+// DeepCopy returns a deep copy of the given node plus a deep copy of its
+// children and rules.
+func (n *Node) DeepCopy() *Node {
+	cpy := NewNode(n.Name, nil)
+	if n.Parent != nil {
+		cpy.Parent = n.Parent.DeepCopy()
+	}
+	cpy.Rules = []PolicyRule{}
+	for _, rule := range n.Rules {
+		cpy.Rules = append(cpy.Rules, rule)
+	}
+	for k, v := range n.Children {
+		cpy.Children[k] = v.DeepCopy()
+	}
+	cpy.mergeable = n.mergeable
+	cpy.resolved = n.resolved
+	return cpy
+}
+
 func (n *Node) Path() string {
 	if n.path == "" {
 		n.buildPath()

pkg/policy/tree.go
@@ -78,7 +78,9 @@ func canConsume(root *Node, ctx *SearchContext) ConsumableDecision {
 	return decision
 }
 
-func (t *Tree) Allows(ctx *SearchContext) ConsumableDecision {
+// AllowsRLocked checks the ConsumableDecision of the given `SearchContext`.
+// Must be called with the Mutex's tree at least RLocked.
+func (t *Tree) AllowsRLocked(ctx *SearchContext) ConsumableDecision {
 	policyTrace(ctx, "NEW TRACE >> %s\n", ctx.String())
 
 	var decision, subDecision ConsumableDecision
@@ -132,8 +134,23 @@ func (t *Tree) ResolveL4Policy(ctx *SearchContext) *L4Policy {
 	return result
 }
 
-// Lookup returns a policy node and its parent for a given path
+// Lookup returns a policy node and its parent for a given path and returns
+// a deepcopy of the nodes found.
 func (t *Tree) Lookup(path string) (node, parent *Node) {
+	t.Mutex.RLock()
+	defer t.Mutex.RUnlock()
+	n, p := t.lookup(path)
+	if n != nil {
+		node = n.DeepCopy()
+	}
+	if p != nil {
+		parent = p.DeepCopy()
+	}
+	return
+}
+
+// Lookup returns a policy node and its parent for a given path
+func (t *Tree) lookup(path string) (node, parent *Node) {
 	// Empty tree
 	if t.Root == nil {
 		return nil, nil
@@ -209,7 +226,7 @@ func (t *Tree) add(parentPath string, node *Node) (bool, error) {
 		}
 	} else {
 		absPath := JoinPath(parentPath, node.Name)
-		_, parent := t.Lookup(absPath)
+		_, parent := t.lookup(absPath)
 		if parent == nil {
 			grandParentPath, parentName := SplitNodePath(parentPath)
 			parent = NewNode(parentName, nil)
@@ -243,7 +260,7 @@ func (t *Tree) Delete(path string, coverage string) bool {
 	t.Mutex.Lock()
 	defer t.Mutex.Unlock()
 
-	node, parent := t.Lookup(path)
+	node, parent := t.lookup(path)
 	if node == nil {
 		return false
 	}

pkg/policy/tree_test.go
@@ -28,7 +28,7 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	tree := Tree{}
 
 	// Empty tree should return empty result
-	n, p := tree.Lookup("")
+	n, p := tree.lookup("")
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 
@@ -40,13 +40,13 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(err, IsNil)
 
 	// lookup of root node should succeed now
-	n, p = tree.Lookup(RootNodeName)
+	n, p = tree.lookup(RootNodeName)
 	c.Assert(n, Equals, root)
 	c.Assert(n.Name, Equals, RootNodeName)
 	c.Assert(p, IsNil)
 
 	// lookup of empty path should return root node
-	n, p = tree.Lookup("")
+	n, p = tree.lookup("")
 	c.Assert(n, Equals, root)
 	c.Assert(n.Name, Equals, RootNodeName)
 	c.Assert(p, IsNil)
@@ -54,7 +54,7 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	deleted := tree.Delete(RootNodeName, "")
 	c.Assert(deleted, Equals, true)
 
-	n, p = tree.Lookup(RootNodeName)
+	n, p = tree.lookup(RootNodeName)
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 
@@ -65,13 +65,13 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(err, IsNil)
 
 	// The node should exist afterwards
-	n, pBar := tree.Lookup(RootNodeName + ".bar.foo")
+	n, pBar := tree.lookup(RootNodeName + ".bar.foo")
 	c.Assert(n, Equals, foo)
 	c.Assert(pBar.Name, Equals, "bar")
 	c.Assert(pBar.path, Equals, RootNodeName+".bar")
 
 	// The root node should have been added
-	n, p = tree.Lookup(RootNodeName)
+	n, p = tree.lookup(RootNodeName)
 	c.Assert(n, Not(IsNil))
 	c.Assert(p, IsNil)
 
@@ -89,7 +89,7 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(err, IsNil)
 
 	// lookup of root node should succeed now
-	n, p = tree.Lookup(RootNodeName)
+	n, p = tree.lookup(RootNodeName)
 	// The "root" node was merged into the tree's root, therefore we need to
 	// make it the same with this hack
 	root.Children["bar"] = pBar
@@ -99,7 +99,7 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(p, IsNil)
 
 	// lookup of child foo should succeed
-	n, p = tree.Lookup("root.foo")
+	n, p = tree.lookup("root.foo")
 	c.Assert(n, Equals, fooNode)
 	c.Assert(p, DeepEquals, root)
 
@@ -108,12 +108,12 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(deleted, Equals, true)
 
 	// lookup of root node should fail now
-	n, p = tree.Lookup(RootNodeName)
+	n, p = tree.lookup(RootNodeName)
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 
 	// lookup of child foo should fail now
-	n, p = tree.Lookup("root.foo")
+	n, p = tree.lookup("root.foo")
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 }
@@ -274,7 +274,7 @@ func (ds *PolicyTestSuite) TestAlwaysAllow(c *C) {
 	c.Assert(added, Equals, true)
 	c.Assert(err, IsNil)
 
-	decision := tree.Allows(defaultCtx)
+	decision := tree.AllowsRLocked(defaultCtx)
 	c.Assert(decision, Equals, ACCEPT)
 }
 
@@ -306,7 +306,7 @@ func (ds *PolicyTestSuite) TestDenyOverwrite(c *C) {
 	c.Assert(added, Equals, true)
 	c.Assert(err, IsNil)
 
-	decision := tree.Allows(defaultCtx)
+	decision := tree.AllowsRLocked(defaultCtx)
 	c.Assert(decision, Equals, DENY)
 }
 
@@ -330,7 +330,7 @@ func (ds *PolicyTestSuite) TestRulePrecedence(c *C) {
 	c.Assert(added, Equals, true)
 	c.Assert(err, IsNil)
 
-	decision := tree.Allows(defaultCtx)
+	decision := tree.AllowsRLocked(defaultCtx)
 	c.Assert(decision, Equals, DENY)
 }
 

pkg/endpoint/policy.go
@@ -40,7 +40,7 @@ func (e *Endpoint) checkEgressAccess(owner Owner, opts models.ConfigurationMap, 
 		return
 	}
 
-	switch owner.GetPolicyTree().Allows(&ctx) {
+	switch owner.GetPolicyTree().AllowsRLocked(&ctx) {
 	case policy.ACCEPT, policy.ALWAYS_ACCEPT:
 		opts[opt] = "enabled"
 	case policy.DENY:
@@ -73,7 +73,7 @@ func (e *Endpoint) evaluateConsumerSource(owner Owner, ctx *policy.SearchContext
 
 	log.Debugf("Evaluating policy for %+v", ctx)
 
-	decision := owner.GetPolicyTree().Allows(ctx)
+	decision := owner.GetPolicyTree().AllowsRLocked(ctx)
 	if decision == policy.ACCEPT {
 		e.allowConsumer(owner, srcID)
 	}
@@ -81,7 +81,7 @@ func (e *Endpoint) evaluateConsumerSource(owner Owner, ctx *policy.SearchContext
 	return nil
 }
 
-func (e *Endpoint) InvalidatePolicy() {
+func (e *Endpoint) invalidatePolicy() {
 	if e.Consumable != nil {
 		// Resetting to 0 will trigger a regeneration on the next update
 		log.Debugf("Invalidated policy for endpoint %d", e.ID)
@@ -147,8 +147,6 @@ func (e *Endpoint) regenerateConsumable(owner Owner) (bool, error) {
 		return false, nil
 	}
 
-	tree := owner.GetPolicyTree()
-	tree.Mutex.RLock()
 	cache := owner.GetConsumableCache()
 
 	// Skip if policy for this consumable is already valid
@@ -157,7 +155,6 @@ func (e *Endpoint) regenerateConsumable(owner Owner) (bool, error) {
 	//	log.Debugf("Reusing cached policy for identity %d", c.ID)
 	//	return false, nil
 	//}
-	tree.Mutex.RUnlock()
 
 	maxID, err := owner.GetCachedMaxLabelID()
 	if err != nil {
@@ -172,13 +169,15 @@ func (e *Endpoint) regenerateConsumable(owner Owner) (bool, error) {
 		ctx.Trace = policy.TRACE_ENABLED
 	}
 
+	tree := owner.GetPolicyTree()
+	tree.Mutex.Lock()
+	newL4policy := tree.ResolveL4Policy(&ctx)
+	defer tree.Mutex.Unlock()
+
 	// Mark all entries unused by denying them
 	for k := range c.Consumers {
 		c.Consumers[k].DeletionMark = true
 	}
-
-	tree.Mutex.RLock()
-	newL4policy := tree.ResolveL4Policy(&ctx)
 
 	if c.L4Policy != nil {
 		e.cleanUnusedRedirects(owner, c.L4Policy.Ingress, newL4policy.Ingress)
@@ -211,7 +210,6 @@ func (e *Endpoint) regenerateConsumable(owner Owner) (bool, error) {
 		}
 		idx++
 	}
-	tree.Mutex.RUnlock()
 
 	// Garbage collect all unused entries
 	for _, val := range c.Consumers {
@@ -244,7 +242,7 @@ func (e *Endpoint) regeneratePolicy(owner Owner) (bool, error) {
 		opts[OptionConntrack] = "enabled"
 	}
 
-	optsChanged := e.ApplyOpts(opts)
+	optsChanged := e.ApplyOptsLocked(opts)
 
 	if !e.PolicyCalculated {
 		e.PolicyCalculated = true
@@ -257,7 +255,7 @@ func (e *Endpoint) regeneratePolicy(owner Owner) (bool, error) {
 }
 
 func (e *Endpoint) regenerate(owner Owner) error {
-	origDir := filepath.Join(".", e.StringID())
+	origDir := filepath.Join(".", e.StringIDLocked())
 
 	// This is the temporary directory to store the generated headers,
 	// the original existing directory is not overwritten until the
@@ -311,41 +309,61 @@ func (e *Endpoint) regenerate(owner Owner) error {
 	return nil
 }
 
-// Force regeneration of endpoint programs & policy
-func (e *Endpoint) regenerateLocked(owner Owner) error {
-	err := e.regenerate(owner)
-	if err != nil {
-		e.LogStatus(BPF, Failure, err.Error())
-	} else {
-		e.LogStatusOK(BPF, "Successfully regenerated endpoint program")
+// Regenerate forces the regeneration of endpoint programs & policy
+func (e *Endpoint) Regenerate(owner Owner) <-chan bool {
+	newReq := &Request{
+		ID:           uint64(e.ID),
+		MyTurn:       make(chan bool),
+		Done:         make(chan bool),
+		ExternalDone: make(chan bool),
 	}
-
-	return err
-}
-
-// Regenerate forces the regeneration of endpoint programs & policy.
-func (e *Endpoint) Regenerate(owner Owner) error {
-	return e.regenerateLocked(owner)
+	owner.QueueEndpointBuild(newReq)
+	go func(e *Endpoint, myTurn <-chan bool, finish chan<- bool, externalFinish chan<- bool) {
+		buildSuccess := true
+		e.Mutex.Lock()
+		e.State = StateRegenerating
+		eID := e.ID
+		e.Mutex.Unlock()
+		isMyTurn, isMyTurnChanOK := <-myTurn
+		if isMyTurnChanOK && isMyTurn {
+			log.Debugf("Finally, is my turn to regenerate myself [%d]", eID)
+			e.Mutex.Lock()
+			err := e.regenerate(owner)
+			e.State = StateReady
+			e.Mutex.Unlock()
+			if err != nil {
+				buildSuccess = false
+				e.LogStatus(BPF, Failure, err.Error())
+			} else {
+				buildSuccess = true
+				e.LogStatusOK(BPF, "Successfully regenerated endpoint program")
+			}
+			finish <- buildSuccess
+		} else {
+			buildSuccess = false
+			log.Debugf("My request was canceled because I'm already in line [%d]", eID)
+		}
+		// The external listener can ignore the channel so we need to
+		// make sure we don't block
+		select {
+		case externalFinish <- buildSuccess:
+		default:
+		}
+		close(externalFinish)
+	}(e, newReq.MyTurn, newReq.Done, newReq.ExternalDone)
+	return newReq.ExternalDone
 }
 
 // TriggerPolicyUpdates indicates that a policy change is likely to
 // affect this endpoint. Will update all required endpoint configuration and
 // state to reflect new policy and regenerate programs if required.
-func (e *Endpoint) TriggerPolicyUpdates(owner Owner) error {
+func (e *Endpoint) TriggerPolicyUpdates(owner Owner) (bool, error) {
+	e.Mutex.Lock()
+	defer e.Mutex.Unlock()
 	if e.Consumable == nil {
-		return nil
+		return false, nil
 	}
-
-	optionChanges, err := e.regeneratePolicy(owner)
-	if err != nil {
-		return err
-	}
-
-	if optionChanges {
-		return e.regenerateLocked(owner)
-	}
-
-	return nil
+	return e.regeneratePolicy(owner)
 }
 
 func (e *Endpoint) SetIdentity(owner Owner, id *policy.Identity) {
@@ -354,6 +372,8 @@ func (e *Endpoint) SetIdentity(owner Owner, id *policy.Identity) {
 	defer tree.Mutex.Unlock()
 	cache := owner.GetConsumableCache()
 
+	e.Mutex.Lock()
+	defer e.Mutex.Unlock()
 	if e.Consumable != nil {
 		if e.SecLabel != nil && id.ID == e.Consumable.ID {
 			e.SecLabel = id

api/v1/models/endpoint_state.go
@@ -25,6 +25,8 @@ const (
 	EndpointStateWaitingForIdentity EndpointState = "waiting-for-identity"
 	// EndpointStateNotReady captures enum value "not-ready"
 	EndpointStateNotReady EndpointState = "not-ready"
+	// EndpointStateRegenerating captures enum value "regenerating"
+	EndpointStateRegenerating EndpointState = "regenerating"
 	// EndpointStateReady captures enum value "ready"
 	EndpointStateReady EndpointState = "ready"
 )
@@ -34,7 +36,7 @@ var endpointStateEnum []interface{}
 
 func init() {
 	var res []EndpointState
-	if err := json.Unmarshal([]byte(`["creating","disconnected","waiting-for-identity","not-ready","ready"]`), &res); err != nil {
+	if err := json.Unmarshal([]byte(`["creating","disconnected","waiting-for-identity","not-ready","regenerating","ready"]`), &res); err != nil {
 		panic(err)
 	}
 	for _, v := range res {

api/v1/openapi.yaml
@@ -725,6 +725,7 @@ definitions:
       - disconnected
       - waiting-for-identity
       - not-ready
+      - regenerating
       - ready
   EndpointStatusChange:
     description: Indication of a change of status

api/v1/server/embedded_spec.go
@@ -1046,6 +1046,7 @@ func init() {
         "disconnected",
         "waiting-for-identity",
         "not-ready",
+        "regenerating",
         "ready"
       ]
     },

commit 00cbd7d727ea5e629b1337dc07cecb09e11f28b1

policy: removed DeepCopy node
Fixes: e7c1e0294d ("Introducing endpoints parallelization build")

Signed-off-by: André Martins <andre@cilium.io>

daemon/policy.go
@@ -274,7 +274,7 @@ func (h *getPolicyPath) Handle(params GetPolicyPathParams) middleware.Responder 
 	d.policy.Mutex.RLock()
 	defer d.policy.Mutex.RUnlock()
 
-	node, _ := d.policy.Lookup(params.Path)
+	node, _ := d.policy.LookupLocked(params.Path)
 	if node == nil {
 		return NewGetPolicyPathNotFound()
 	}

pkg/policy/node.go
@@ -44,25 +44,6 @@ func NewNode(name string, parent *Node) *Node {
 	}
 }
 
-// DeepCopy returns a deep copy of the given node plus a deep copy of its
-// children and rules.
-func (n *Node) DeepCopy() *Node {
-	cpy := NewNode(n.Name, nil)
-	if n.Parent != nil {
-		cpy.Parent = n.Parent.DeepCopy()
-	}
-	cpy.Rules = []PolicyRule{}
-	for _, rule := range n.Rules {
-		cpy.Rules = append(cpy.Rules, rule)
-	}
-	for k, v := range n.Children {
-		cpy.Children[k] = v.DeepCopy()
-	}
-	cpy.mergeable = n.mergeable
-	cpy.resolved = n.resolved
-	return cpy
-}
-
 func (n *Node) Path() string {
 	if n.path == "" {
 		n.buildPath()

pkg/policy/tree.go
@@ -135,23 +135,8 @@ func (t *Tree) ResolveL4Policy(ctx *SearchContext) *L4Policy {
 	return result
 }
 
-// Lookup returns a policy node and its parent for a given path and returns
-// a deepcopy of the nodes found.
-func (t *Tree) Lookup(path string) (node, parent *Node) {
-	t.Mutex.RLock()
-	defer t.Mutex.RUnlock()
-	n, p := t.lookup(path)
-	if n != nil {
-		node = n.DeepCopy()
-	}
-	if p != nil {
-		parent = p.DeepCopy()
-	}
-	return
-}
-
-// Lookup returns a policy node and its parent for a given path
-func (t *Tree) lookup(path string) (node, parent *Node) {
+// LookupLocked returns a policy node and its parent for a given path
+func (t *Tree) LookupLocked(path string) (node, parent *Node) {
 	// Empty tree
 	if t.Root == nil {
 		return nil, nil
@@ -227,7 +212,7 @@ func (t *Tree) add(parentPath string, node *Node) (bool, error) {
 		}
 	} else {
 		absPath := JoinPath(parentPath, node.Name)
-		_, parent := t.lookup(absPath)
+		_, parent := t.LookupLocked(absPath)
 		if parent == nil {
 			grandParentPath, parentName := SplitNodePath(parentPath)
 			parent = NewNode(parentName, nil)
@@ -261,7 +246,7 @@ func (t *Tree) Delete(path string, coverage string) bool {
 	t.Mutex.Lock()
 	defer t.Mutex.Unlock()
 
-	node, parent := t.lookup(path)
+	node, parent := t.LookupLocked(path)
 	if node == nil {
 		return false
 	}

pkg/policy/tree_test.go
@@ -28,7 +28,7 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	tree := Tree{}
 
 	// Empty tree should return empty result
-	n, p := tree.lookup("")
+	n, p := tree.LookupLocked("")
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 
@@ -40,13 +40,13 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(err, IsNil)
 
 	// lookup of root node should succeed now
-	n, p = tree.lookup(RootNodeName)
+	n, p = tree.LookupLocked(RootNodeName)
 	c.Assert(n, Equals, root)
 	c.Assert(n.Name, Equals, RootNodeName)
 	c.Assert(p, IsNil)
 
 	// lookup of empty path should return root node
-	n, p = tree.lookup("")
+	n, p = tree.LookupLocked("")
 	c.Assert(n, Equals, root)
 	c.Assert(n.Name, Equals, RootNodeName)
 	c.Assert(p, IsNil)
@@ -54,7 +54,7 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	deleted := tree.Delete(RootNodeName, "")
 	c.Assert(deleted, Equals, true)
 
-	n, p = tree.lookup(RootNodeName)
+	n, p = tree.LookupLocked(RootNodeName)
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 
@@ -65,13 +65,13 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(err, IsNil)
 
 	// The node should exist afterwards
-	n, pBar := tree.lookup(RootNodeName + ".bar.foo")
+	n, pBar := tree.LookupLocked(RootNodeName + ".bar.foo")
 	c.Assert(n, Equals, foo)
 	c.Assert(pBar.Name, Equals, "bar")
 	c.Assert(pBar.path, Equals, RootNodeName+".bar")
 
 	// The root node should have been added
-	n, p = tree.lookup(RootNodeName)
+	n, p = tree.LookupLocked(RootNodeName)
 	c.Assert(n, Not(IsNil))
 	c.Assert(p, IsNil)
 
@@ -89,7 +89,7 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(err, IsNil)
 
 	// lookup of root node should succeed now
-	n, p = tree.lookup(RootNodeName)
+	n, p = tree.LookupLocked(RootNodeName)
 	// The "root" node was merged into the tree's root, therefore we need to
 	// make it the same with this hack
 	root.Children["bar"] = pBar
@@ -99,7 +99,7 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(p, IsNil)
 
 	// lookup of child foo should succeed
-	n, p = tree.lookup("root.foo")
+	n, p = tree.LookupLocked("root.foo")
 	c.Assert(n, Equals, fooNode)
 	c.Assert(p, DeepEquals, root)
 
@@ -108,12 +108,12 @@ func (ds *PolicyTestSuite) TestAddDelete(c *C) {
 	c.Assert(deleted, Equals, true)
 
 	// lookup of root node should fail now
-	n, p = tree.lookup(RootNodeName)
+	n, p = tree.LookupLocked(RootNodeName)
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 
 	// lookup of child foo should fail now
-	n, p = tree.lookup("root.foo")
+	n, p = tree.LookupLocked("root.foo")
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 }
@@ -130,7 +130,7 @@ func (ds *PolicyTestSuite) TestLookup(c *C) {
 	c.Assert(err, IsNil)
 
 	// search for foo.bar, should return nil, foo
-	n, p := tree.Lookup("foo.bar")
+	n, p := tree.LookupLocked("foo.bar")
 	c.Assert(n, IsNil)
 	c.Assert(p, Equals, foo)
 
@@ -141,26 +141,26 @@ func (ds *PolicyTestSuite) TestLookup(c *C) {
 	c.Assert(err, IsNil)
 
 	// lookup of foo
-	n, p = tree.Lookup("foo")
+	n, p = tree.LookupLocked("foo")
 	c.Assert(n, Equals, foo)
 	c.Assert(n.path, Equals, "root.foo")
 	c.Assert(n.Name, Equals, "foo")
 	c.Assert(p.Name, Equals, "root")
 
 	// lookup of bar should fail
-	n, p = tree.Lookup("bar")
+	n, p = tree.LookupLocked("bar")
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 
 	// lookup of foo.bar should suceed
-	n, p = tree.Lookup("foo.bar")
+	n, p = tree.LookupLocked("foo.bar")
 	c.Assert(n, Equals, bar)
 	c.Assert(n.path, Equals, "root.foo.bar")
 	c.Assert(n.Name, Equals, "bar")
 	c.Assert(p, Equals, foo)
 
 	// lookup of foo.bar.baz should return nil, bar
-	n, p = tree.Lookup("foo.bar.baz")
+	n, p = tree.LookupLocked("foo.bar.baz")
 	c.Assert(n, IsNil)
 	c.Assert(p, Equals, bar)
 
@@ -171,17 +171,17 @@ func (ds *PolicyTestSuite) TestLookup(c *C) {
 	c.Assert(err, IsNil)
 
 	// lookup of foo.bar.1
-	n, p = tree.Lookup("foo.bar.1")
+	n, p = tree.LookupLocked("foo.bar.1")
 	c.Assert(n.Name, Equals, "1")
 	c.Assert(p, Equals, bar)
 
 	// lookup of foo.bar.1.2
-	n, p = tree.Lookup("foo.bar.1.2")
+	n, p = tree.LookupLocked("foo.bar.1.2")
 	c.Assert(n.Name, Equals, "2")
 	c.Assert(p.Name, Equals, "1")
 
 	// lookup of foo.bar.1.2.3.deep
-	n, p = tree.Lookup("foo.bar.1.2.3.deep")
+	n, p = tree.LookupLocked("foo.bar.1.2.3.deep")
 	c.Assert(n.Name, Equals, "deep")
 	c.Assert(p.Name, Equals, "3")
 }
@@ -198,10 +198,10 @@ func (ds *PolicyTestSuite) TestAddDelete2(c *C) {
 	c.Assert(err, IsNil)
 
 	// Lookup io and io.cilium nodes created
-	n, p := tree.Lookup("io")
+	n, p := tree.LookupLocked("io")
 	c.Assert(n.Name, Equals, "io")
 	c.Assert(p.Name, Equals, "root")
-	n, p = tree.Lookup("io.cilium")
+	n, p = tree.LookupLocked("io.cilium")
 	c.Assert(n.Name, Equals, "cilium")
 	c.Assert(p.Name, Equals, "io")
 
@@ -212,12 +212,12 @@ func (ds *PolicyTestSuite) TestAddDelete2(c *C) {
 	c.Assert(err, IsNil)
 
 	// Lookup io.cilium.k8s and io.cilium.k8s.k8s-app nodes created
-	n, p = tree.Lookup("io.cilium.k8s")
+	n, p = tree.LookupLocked("io.cilium.k8s")
 	c.Assert(n, Not(IsNil))
 	c.Assert(n.Name, Equals, "k8s")
 	c.Assert(p.Name, Equals, "cilium")
 	c.Assert(p.path, Equals, "root.io.cilium")
-	n, p = tree.Lookup("io.cilium.k8s.k8s-app")
+	n, p = tree.LookupLocked("io.cilium.k8s.k8s-app")
 	c.Assert(n, Equals, k8s)
 	c.Assert(p.Name, Equals, "k8s")
 	c.Assert(p.path, Equals, "root.io.cilium.k8s")
@@ -225,7 +225,7 @@ func (ds *PolicyTestSuite) TestAddDelete2(c *C) {
 	deleted := tree.Delete(RootNodeName, "")
 	c.Assert(deleted, Equals, true)
 
-	n, p = tree.Lookup(RootNodeName)
+	n, p = tree.LookupLocked(RootNodeName)
 	c.Assert(n, IsNil)
 	c.Assert(p, IsNil)
 }