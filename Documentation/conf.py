a1439d4a1795031c826b30077a235b0644997de6
Alexander Alemayhu <alexander@alemayhu.com>
2017-04-19 21:29:18 +0200

    Documentation: remove empty page from latexpdf target
    When printing out this really adds no value and seeing TOC earlier is
    more interesting.
    
    Reference: https://github.com/sphinx-doc/sphinx/issues/2622
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -123,6 +123,7 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
+    'extraclassoptions': 'openany',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
