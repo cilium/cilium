e08b95b1d5015bbb7e4383658c0a88f8125214aa
André Martins <andre@cilium.io>
2017-04-28 16:43:40 +0200

    examples: update kubernetes example
    Changed kubernetes example for the new functionalities introduced.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- examples/kubernetes-ingress/scripts/09-dns-addon.sh
+++ examples/kubernetes-ingress/scripts/09-dns-addon.sh
@@ -5,12 +5,32 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "${dir}/helpers.bash"
 
 set -e
+export 'NWORKERS'=${NWORKERS:-0}
 
 svc_file="${dir}/../deployments/kubedns-svc.yaml"
 rc_file="${dir}/../deployments/kubedns-rc.yaml"
+guestbook_dir="${dir}/../deployments/guestbook"
 
 sed "s/\$cluster-ip/${cluster_dns_ip}/" "${svc_file}.sed" > "${svc_file}"
-sed "s/\$kube-master/${kubernetes_master}/" "${rc_file}.sed" > "${rc_file}"
+
+if [ "${NWORKERS}" -gt "0" ]; then
+    node_selector="cilium${K8STAG}-node-2"
+else
+    node_selector="cilium${K8STAG}-master"
+fi
+
+sed -e "s/\$kube-master/${kubernetes_master}/" \
+    -e "s/\$kube-node-selector/${node_selector}/" \
+    "${rc_file}.sed" > "${rc_file}"
+
+sed "s/\$kube-node-selector/${node_selector}/" \
+    "${guestbook_dir}/1-redis-master-controller.json.sed" > "${guestbook_dir}/1-redis-master-controller.json"
+
+sed "s/\$kube-node-selector/${node_selector}/" \
+    "${guestbook_dir}/3-redis-slave-controller.json.sed" > "${guestbook_dir}/3-redis-slave-controller.json"
+
+sed "s/\$kube-node-selector/${node_selector}/" \
+    "${guestbook_dir}/5-guestbook-controller.json.sed" > "${guestbook_dir}/5-guestbook-controller.json"
 
 kubectl create -f "${svc_file}" || true
 
