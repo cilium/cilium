7b80e895f4f28c7cb8df632e8a94b40ff49b34e0
Madhu Challa <madhu@cilium.io>
2017-05-05 22:16:28 +0200

    bpf: fix wrong ipv4 ttl calculation
    The offset for l3_csum_replace() is wrong, but it also never worked
    since as size we pass 1, which is invalid from kernel side. But also
    the skb_store_bytes() used the old ttl value.
    
    Fix the csum offset and use 2 as size, which is fine since it just
    zero extends. Then, store new_ttl into the packet.
    
    Signed-off-by: Madhu Challa <madhu@cilium.io>
    Signed-off-by: Daniel Borkmann <daniel@cilium.io>

--- bpf/lib/ipv4.h
+++ bpf/lib/ipv4.h
@@ -27,8 +27,6 @@ static inline int ipv4_load_daddr(struct __sk_buff *skb, int off, __u32 *dst)
 	return skb_load_bytes(skb, off + offsetof(struct iphdr, daddr), dst, 4);
 }
 
-#define TTL_OFF(off) (off + offsetof(struct iphdr, ttl))
-
 static inline int ipv4_dec_ttl(struct __sk_buff *skb, int off, struct iphdr *ip4)
 {
 	__u8 new_ttl, ttl = ip4->ttl;
@@ -37,8 +35,9 @@ static inline int ipv4_dec_ttl(struct __sk_buff *skb, int off, struct iphdr *ip4
 		return 1;
 
 	new_ttl = ttl - 1;
-	l3_csum_replace(skb, TTL_OFF(off), ttl, new_ttl, 1);
-	skb_store_bytes(skb, off + offsetof(struct iphdr, ttl), &ttl, sizeof(ttl), 0);
+	/* l3_csum_replace() takes at min 2 bytes, zero extended. */
+	l3_csum_replace(skb, off + offsetof(struct iphdr, check), ttl, new_ttl, 2);
+	skb_store_bytes(skb, off + offsetof(struct iphdr, ttl), &new_ttl, sizeof(new_ttl), 0);
 
 	return 0;
 }
