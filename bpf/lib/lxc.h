22af6b5c8c098bc34e8493b27330adc2431aeddb
Daniel Borkmann <daniel@cilium.io>
2017-05-17 09:42:39 +0200

    bpf: fix ipv4 src address validation
    When auditing other LXC_IPV4 occurences, we noticed that
    valid_src_ipv4() is incorrect. We also need a bpf_htonl()
    in this case, since LXC_IPV4 is provided via header file
    in host endianess, but also the test itself is wrong,
    meaning valid_src_ipv4() should check whether the source
    IP is the one from the LXC, and not any other IP.
    
    valid_src_ip() got this right. ipv6_addrcmp() returns 0
    if both addresses are the same. And negation of it then
    returns 1 for valid_src_ip() match. This never triggered
    due to endianess being wrong and thus old function always
    returning 1 due to IP mismatch. Fix it to what we actually
    want to test against and also make all these validation
    helper names easier to understand.
    
    Signed-off-by: Daniel Borkmann <daniel@cilium.io>
    Signed-off-by: John Fastabend <john@covalent.io>

--- bpf/lib/lxc.h
+++ bpf/lib/lxc.h
@@ -27,57 +27,57 @@
 #include "l4.h"
 
 #ifndef DISABLE_SMAC_VERIFICATION
-static inline int valid_src_mac(struct ethhdr *eth)
+static inline int is_valid_lxc_src_mac(struct ethhdr *eth)
 {
 	union macaddr valid = LXC_MAC;
 
 	return !eth_addrcmp(&valid, (union macaddr *) &eth->h_source);
 }
 #else
-static inline int valid_src_mac(struct ethhdr *eth)
+static inline int is_valid_lxc_src_mac(struct ethhdr *eth)
 {
 	return 1;
 }
 #endif
 
 #ifndef DISABLE_SIP_VERIFICATION
-static inline int valid_src_ip(struct ipv6hdr *ip6)
+static inline int is_valid_lxc_src_ip(struct ipv6hdr *ip6)
 {
 	union v6addr valid = LXC_IP;
 
 	return !ipv6_addrcmp((union v6addr *) &ip6->saddr, &valid);
 }
 
-static inline int valid_src_ipv4(struct iphdr *ip4)
+static inline int is_valid_lxc_src_ipv4(struct iphdr *ip4)
 {
 #ifdef LXC_IPV4
-	return ip4->saddr != LXC_IPV4;
+	return ip4->saddr == bpf_htonl(LXC_IPV4);
 #else
 	/* Can't send IPv4 if no IPv4 address is configured */
 	return 0;
 #endif
 }
 #else
-static inline int valid_src_ip(struct ipv6hdr *ip6)
+static inline int is_valid_lxc_src_ip(struct ipv6hdr *ip6)
 {
 	return 1;
 }
 
-static inline int valid_src_ipv4(struct iphdr *ip4)
+static inline int is_valid_lxc_src_ipv4(struct iphdr *ip4)
 {
 	return 1;
 }
 #endif
 
 #ifndef DISABLE_DMAC_VERIFICATION
-static inline int valid_dst_mac(struct ethhdr *eth)
+static inline int is_valid_gw_dst_mac(struct ethhdr *eth)
 {
 	union macaddr valid = NODE_MAC;
 
 	return !eth_addrcmp(&valid, (union macaddr *) &eth->h_dest);
 }
 #else
-static inline int valid_dst_mac(struct ethhdr *eth)
+static inline int is_valid_gw_dst_mac(struct ethhdr *eth)
 {
 	return 1;
 }
