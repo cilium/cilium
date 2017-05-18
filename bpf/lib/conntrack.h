06751f2adeb17d1136e990d7ffdb513194879d17
Daniel Borkmann <daniel@cilium.io>
2017-05-19 01:34:08 +0200

    bpf: fix lb6 load balancing wrt ct_lookup6's ct_state
    Commit bc703f5cb873 ("bpf: __ct_lookup() in forward direction must
    pass in state ptr") tried to fix commit 3a8be7ebb16b ("bpf: don't
    update ct_state on second v4 __ct_lookup") but added ct_state to
    ct_lookup6() too much. The 2nd lookup had a NULL ct_state all the
    time. Adding it broke the lb v6 parts, and bisect pointed to the
    commit bc703f5cb873.
    
    Signed-off-by: Daniel Borkmann <daniel@cilium.io>

--- bpf/lib/conntrack.h
+++ bpf/lib/conntrack.h
@@ -263,7 +263,7 @@ static inline int __inline__ ct_lookup6(void *map, struct ipv6_ct_tuple *tuple,
 	ipv6_ct_tuple_reverse(tuple);
 	cilium_trace(skb, DBG_CT_LOOKUP, (bpf_ntohs(tuple->sport) << 16) |
 		     bpf_ntohs(tuple->dport), (tuple->nexthdr << 8) | tuple->flags);
-	ret = __ct_lookup(map, skb, tuple, action, dir, ct_state);
+	ret = __ct_lookup(map, skb, tuple, action, dir, NULL);
 
 #ifdef LXC_NAT46
 	skb->cb[CB_NAT46_STATE] = NAT46_CLEAR;
