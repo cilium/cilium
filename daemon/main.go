affbebe9a6ab8f37bcb1e018b067e0a83cbea1d3
peiqi <uestc.shi@gmail.com>
2017-05-20 11:45:36 +0800

    message fix
    fix log message, which is misleading when we use ubuntu system.
    
    Signed-off-by: peiqi <uestc.shi@gmail.com>

--- daemon/main.go
+++ daemon/main.go
@@ -181,10 +181,10 @@ func checkMinRequirements() {
 	// both files exist on both systems but cdefs.h already exists in fedora
 	// without 'glibc-devel.i686' so we check for 'stubs-32.h first.
 	if _, err := os.Stat("/usr/include/gnu/stubs-32.h"); os.IsNotExist(err) {
-		log.Fatal("linking environment: NOT OK, please make sure you have 'glibc-devel.i686' in your system")
+		log.Fatal("linking environment: NOT OK, please make sure you have 'glibc-devel.i686' if you use fedora system or 'libc6-dev-i386' if you use ubuntu system")
 	}
 	if _, err := os.Stat("/usr/include/sys/cdefs.h"); os.IsNotExist(err) {
-		log.Fatal("linking environment: NOT OK, please make sure you have 'libc6-dev-i386' in your system")
+		log.Fatal("linking environment: NOT OK, please make sure you have 'libc6-dev-i386' in your ubuntu system")
 	}
 	log.Info("linking environment: OK!")
 
