d782db81d249ad1a3cc220fc3e7e150e89430a51
Thomas Graf <thomas@cilium.io>
2017-05-21 22:56:05 +0200

    Revert "add loopback binary in cni_install"
    The `loopback` binary is not present and causes errors. Need
    to revert this for now.
    
    This reverts commit 2d2cd407506fed93b570ff121a4e59d5f31bfe93.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- plugins/cilium-cni/cni-install.sh
+++ plugins/cilium-cni/cni-install.sh
@@ -6,13 +6,12 @@ HOST_PREFIX=${HOST_PREFIX:-/host}
 CNI_CONF_NAME=${CNI_CONF_NAME:-10-cilium.conf}
 MTU=${MTU:-1450}
 
-CILIUM_CNI=${CILIUM_CNI:-${HOST_PREFIX}/opt/cni/bin/}
+CILIUM_CNI=${CILIUM_CNI:-${HOST_PREFIX}/opt/cni/bin/cilium-cni}
 CILIUM_CNI_CONF=${CILIUM_CNI_CONF:-${HOST_PREFIX}/etc/cni/net.d/${CNI_CONF_NAME}}
 
-# Install cilium-cni and loopback binary to host
+# Install cilium-cni binary tohost
 echo "Installing $CILIUM_CNI ..."
 cp /opt/cni/bin/cilium-cni ${CILIUM_CNI}
-cp /opt/cni/bin/loopback ${CILIUM_CNI}
 
 cat > ${CNI_CONF_NAME} <<EOF
 {
