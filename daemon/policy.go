f06ac26592f30fad2b772f3ca27698ba3f88469e
Thomas Graf <thomas@cilium.io>
2017-05-24 19:48:20 +0200

    policy: Simplify locking when selecting endpoint policy selection
    No need to unlock and relock in this case. owner.UpdatePolicyEnforcement
    can be called within the section of the locks already being held.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- daemon/policy.go
+++ daemon/policy.go
@@ -100,7 +100,8 @@ func (d *Daemon) TriggerPolicyUpdates(added []policy.NumericIdentity) {
 
 // UpdatePolicyEnforcement returns whether policy enforcement needs to be
 // enabled for the specified endpoint.
-// Endpoint needs to be called with RLock set.
+//
+// Must be called with e.Consumable.Mutex and d.GetPolicyRepositor().Mutex held
 func (d *Daemon) UpdatePolicyEnforcement(e *endpoint.Endpoint) bool {
 	if d.conf.EnablePolicy == endpoint.AlwaysEnforce {
 		return true
@@ -114,13 +115,11 @@ func (d *Daemon) UpdatePolicyEnforcement(e *endpoint.Endpoint) bool {
 			return false
 		}
 	} else if d.conf.EnablePolicy == endpoint.DefaultEnforcement && d.conf.IsK8sEnabled() {
-		e.Consumable.Mutex.RLock()
 		// Convert to LabelArray so we can pass to Matches function later.
 		var endpointLabels labels.LabelArray
 		for _, lbl := range e.Consumable.LabelList {
 			endpointLabels = append(endpointLabels, lbl)
 		}
-		e.Consumable.Mutex.RUnlock()
 		// Check if rules match the labels for this endpoint.
 		// If so, enable policy enforcement.
 		return d.GetPolicyRepository().GetRulesMatching(endpointLabels)
