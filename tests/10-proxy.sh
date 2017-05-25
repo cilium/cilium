d145dee8cd3cb415030d2453c6c28ec508bb8ef6
John Fastabend <john.fastabend@gmail.com>
2017-05-25 11:49:45 -0700

    cilium: add contrack local tests for egress proxy
    Proxy tests for conntrack local case.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- tests/10-proxy.sh
+++ tests/10-proxy.sh
@@ -16,26 +16,29 @@ CLIENT_LABEL="id.client"
 cleanup
 logs_clear
 
-docker network inspect $TEST_NET 2> /dev/null || {
-	docker network create --ipv6 --subnet ::1/112 --ipam-driver cilium --driver cilium $TEST_NET
-}
+function proxy_init {
+	docker network inspect $TEST_NET 2> /dev/null || {
+		docker network create --ipv6 --subnet ::1/112 --ipam-driver cilium --driver cilium $TEST_NET
+	}
 
-docker run -dt --net=$TEST_NET --name server -l $SERVER_LABEL cilium/demo-httpd
-docker run -dt --net=cilium --name client -l id.client tgraf/netperf
 
-SERVER_IP=$(docker inspect --format '{{ .NetworkSettings.Networks.cilium.GlobalIPv6Address }}' server)
-SERVER_IP4=$(docker inspect --format '{{ .NetworkSettings.Networks.cilium.IPAddress }}' server)
-SERVER_ID=$(cilium endpoint list | grep $SERVER_LABEL | awk '{ print $1}')
+	docker run -dt --net=$TEST_NET --name server -l $SERVER_LABEL cilium/demo-httpd
+	docker run -dt --net=cilium --name client -l id.client tgraf/netperf
 
-echo -n "Sleeping 3 seconds..."
-sleep 3
-echo " done."
-set -x
 
-cilium endpoint list
+	SERVER_IP=$(docker inspect --format '{{ .NetworkSettings.Networks.cilium.GlobalIPv6Address }}' server)
+	SERVER_IP4=$(docker inspect --format '{{ .NetworkSettings.Networks.cilium.IPAddress }}' server)
+	SERVER_ID=$(cilium endpoint list | grep $SERVER_LABEL | awk '{ print $1}')
 
-cilium policy delete --all
-cat <<EOF | cilium -D policy import -
+	echo -n "Sleeping 3 seconds..."
+	sleep 3
+	echo " done."
+	set -x
+
+	cilium endpoint list
+
+	cilium policy delete --all
+	cat <<EOF | cilium -D policy import -
 [{
     "endpointSelector": {"matchLabels":{"id.server":""}},
     "ingress": [{
@@ -59,9 +62,10 @@ cat <<EOF | cilium -D policy import -
     }]
 }]
 EOF
+}
 
-sleep 2
 
+function proxy_test {
 until [ "$(cilium endpoint list | grep cilium -c)" -eq 3 ]; do
 	    echo "Waiting for all endpoints to be ready"
 	        sleep 4s
@@ -86,5 +90,16 @@ RETURN=$(docker exec -i client bash -c "curl -s --output /dev/stderr -w '%{http_
 if [[ "${RETURN//$'\n'}" != "403" ]]; then
 	abort "GET /private, unexpected return"
 fi
+}
 
-cilium policy delete --all
+for state in "false" "true"; do
+	echo "Testing with Conntrack=$state"
+	cilium config ConntrackLocal=$state
+	sleep 2
+	proxy_init
+	sleep 2
+	proxy_test
+	cilium policy delete --all 2> /dev/null || true
+	docker rm -f server client 2> /dev/null || true
+	sleep 2
+done
