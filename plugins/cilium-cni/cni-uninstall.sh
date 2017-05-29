7a610b87f2acbbd9cc2b012f4cacaf116ca76e23
Thomas Graf <thomas@cilium.io>
2017-05-29 23:19:39 +0200

    cni: Install cni loopback driver if not installed already
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- plugins/cilium-cni/cni-uninstall.sh
+++ plugins/cilium-cni/cni-uninstall.sh
@@ -5,11 +5,11 @@ set -eu
 HOST_PREFIX=${HOST_PREFIX:-/host}
 CNI_CONF_NAME=${CNI_CONF_NAME:-10-cilium.conf}
 
-CILIUM_CNI=${CILIUM_CNI:-${HOST_PREFIX}/opt/cni/bin/cilium-cni}
+CNI_DIR=${CNI_DIR:-${HOST_PREFIX}/opt/cni}
 CILIUM_CNI_CONF=${CILIUM_CNI_CONF:-${HOST_PREFIX}/etc/cni/net.d/${CNI_CONF_NAME}}
 
-echo "Removing ${CILIUM_CNI} ..."
-rm -f ${CILIUM_CNI}
+echo "Removing ${CNI_DIR}/bin/cilium-cni..."
+rm -f ${CNI_DIR}/bin/cilium-cni
 
 echo "Removing ${CILIUM_CNI_CONF} ..."
 rm -f ${CILIUM_CNI_CONF}
