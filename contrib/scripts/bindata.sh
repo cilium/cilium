e7ab146467943864a2ef235672ce90e030406505
Alexander Alemayhu <alexander@alemayhu.com>
2017-06-15 12:12:46 +0200

    daemon: track go version used for bindata updates
    This might be useful for debugging.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- contrib/scripts/bindata.sh
+++ contrib/scripts/bindata.sh
@@ -6,7 +6,9 @@ BINDATA_FILE=bindata.go
 
 if [[ "$arg1" == "apply" ]]; then
   NEW_SHA1SUM=`sha1sum ${BINDATA_FILE} | awk '{ print $1}'`
+  GO_VERSION_USED=`go version | awk '{ print $3 }'`
   sed -i "s/GO_BINDATA_SHA1SUM=.*/GO_BINDATA_SHA1SUM=${NEW_SHA1SUM}/g" Makefile
+  sed -i "s/GO_VERSION_USED=.*/GO_VERSION_USED=${GO_VERSION_USED}/g" Makefile
   git commit -s -m 'cilium: bindata update due to changes in bpf' Makefile
   exit 0
 fi
