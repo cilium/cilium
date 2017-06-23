8ba88fe57dc59667daa3e0c6575c95fd8ef74efd
André Martins <andre@cilium.io>
2017-06-23 02:53:32 -0700

    retrieving K8s PodCIDR from k8s-apiserver
    Add integration with kubernetes to retrive all nodes addresses and, if
    set, their pod CIDR.
    
    This will allow cilium to route endpoints's network traffic
    automatically to the destination without the user manually inserting the
    necessary routes.
    
    Signed-off-by: André Martins <andre@cilium.io>

---
+++ pkg/node/node.go
@@ -0,0 +1,75 @@
+// Copyright 2016-2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package node
+
+import (
+	"net"
+
+	"k8s.io/client-go/pkg/api/v1"
+)
+
+// Identity represents the node identity of a node.
+type Identity struct {
+	Name string
+}
+
+// String returns the string representation on NodeIdentity.
+func (nn Identity) String() string {
+	return nn.Name
+}
+
+// Node contains the nodes name, the list of addresses to this address
+type Node struct {
+	Name          string
+	IPAddresses   []Address
+	EndpointsCIDR *net.IPNet
+}
+
+// Address is a node address which contains an IP and the address type.
+type Address struct {
+	AddressType v1.NodeAddressType
+	IP          net.IP
+}
+
+// GetNodeIP returns one of the node's IP addresses available with the
+// following priority:
+// - NodeInternalIP
+// - NodeExternalIP
+// - other IP address type
+func (n *Node) GetNodeIP(ipv6 bool) net.IP {
+	var backupIP net.IP
+	for _, addr := range n.IPAddresses {
+		if (ipv6 && addr.IP.To4() != nil) ||
+			(!ipv6 && addr.IP.To4() == nil) {
+			continue
+		}
+		switch addr.AddressType {
+		// Always prefer a cluster internal IP
+		case v1.NodeInternalIP:
+			return addr.IP
+		case v1.NodeExternalIP:
+			// Fall back to external Node IP
+			// if no internal IP could be found
+			backupIP = addr.IP
+		default:
+			// As a last resort, if no internal or external
+			// IP was found, use any node address available
+			if backupIP == nil {
+				backupIP = addr.IP
+			}
+		}
+	}
+	return backupIP
+}
---
+++ pkg/node/node_test.go
@@ -0,0 +1,76 @@
+// Copyright 2016-2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package node
+
+import (
+	"net"
+	"testing"
+
+	. "gopkg.in/check.v1"
+	"k8s.io/client-go/pkg/api/v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type NodeSuite struct{}
+
+var _ = Suite(&NodeSuite{})
+
+func (s *NodeSuite) TestGetNodeIP(c *C) {
+	n := Node{
+		Name: "node-1",
+		IPAddresses: []Address{
+			{IP: net.ParseIP("203.0.113.1"), AddressType: v1.NodeLegacyHostIP},
+		},
+		EndpointsCIDR: nil,
+	}
+	ip := n.GetNodeIP(false)
+	// Return the only IP present
+	c.Assert(ip.Equal(net.ParseIP("203.0.113.1")), Equals, true)
+
+	n.IPAddresses = append(n.IPAddresses, Address{IP: net.ParseIP("192.0.2.3"), AddressType: v1.NodeExternalIP})
+	ip = n.GetNodeIP(false)
+	// The next priority should be NodeExternalIP
+	c.Assert(ip.Equal(net.ParseIP("192.0.2.3")), Equals, true)
+
+	n.IPAddresses = append(n.IPAddresses, Address{IP: net.ParseIP("198.51.100.2"), AddressType: v1.NodeInternalIP})
+	ip = n.GetNodeIP(false)
+	// The next priority should be NodeInternalIP
+	c.Assert(ip.Equal(net.ParseIP("198.51.100.2")), Equals, true)
+
+	n.IPAddresses = append(n.IPAddresses, Address{IP: net.ParseIP("2001:DB8::1"), AddressType: v1.NodeExternalIP})
+	ip = n.GetNodeIP(true)
+	// The next priority should be NodeExternalIP and IPv6
+	c.Assert(ip.Equal(net.ParseIP("2001:DB8::1")), Equals, true)
+
+	n.IPAddresses = append(n.IPAddresses, Address{IP: net.ParseIP("2001:DB8::3"), AddressType: v1.NodeLegacyHostIP})
+	ip = n.GetNodeIP(true)
+	// Should still return the NodeExternalIP and IPv6
+	c.Assert(ip.Equal(net.ParseIP("2001:DB8::1")), Equals, true)
+
+	n.IPAddresses = append(n.IPAddresses, Address{IP: net.ParseIP("2001:DB8::2"), AddressType: v1.NodeInternalIP})
+	ip = n.GetNodeIP(true)
+	// The next priority should be NodeInternalIP and IPv6
+	c.Assert(ip.Equal(net.ParseIP("2001:DB8::2")), Equals, true)
+
+	n.IPAddresses = append(n.IPAddresses, Address{IP: net.ParseIP("198.51.100.2"), AddressType: v1.NodeInternalIP})
+	ip = n.GetNodeIP(false)
+	// Should still return NodeInternalIP and IPv4
+	c.Assert(ip.Equal(net.ParseIP("198.51.100.2")), Equals, true)
+
+}
