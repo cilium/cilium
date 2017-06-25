5165c7f22202da006cd7cefc0b069fe46c0d146b
Thomas Graf <thomas@cilium.io>
2017-06-25 08:51:16 +0200

    bindata: Remove auto commit on apply
    Automatic commit makes fixing up the bindata SHA cumbersome while
    rebasing.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- contrib/scripts/bindata.sh
+++ contrib/scripts/bindata.sh
@@ -9,7 +9,6 @@ if [[ "$arg1" == "apply" ]]; then
   GO_VERSION_USED=`go version | awk '{ print $3 }'`
   sed -i "s/GO_BINDATA_SHA1SUM=.*/GO_BINDATA_SHA1SUM=${NEW_SHA1SUM}/g" Makefile
   sed -i "s/GO_VERSION_USED=.*/GO_VERSION_USED=${GO_VERSION_USED}/g" Makefile
-  git commit -s -m 'cilium: bindata update due to changes in bpf' Makefile
   exit 0
 fi
 
@@ -24,11 +23,11 @@ if echo "$GO_BINDATA_SHA1SUM bindata.go" | sha1sum -c; then
   exit 0
 fi
 
-echo "##################################################################"
+echo "########################################################################"
 echo ""
-echo "  ERROR: bindata.go is out of date."
+echo "                  ERROR: bindata.go is out of date."
 echo ""
-echo "  Use 'make -C daemon apply-bindata' to commit the changes."
-echo "##################################################################"
+echo "  Use 'make -C daemon apply-bindata' to update the SHA in the Makefile."
+echo "########################################################################"
 
 exit 1
