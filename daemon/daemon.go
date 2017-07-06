8ee6d5c9d92419b39e97b93edb94f6c7bb5dbc40
Thomas Graf <thomas@cilium.io>
2017-07-06 06:07:28 +0200

    agent: Print IPv4/IPv6 node IPs when starting up
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -669,6 +669,8 @@ func NewDaemon(c *Config) (*Daemon, error) {
 	}
 
 	log.Infof("Local node-name: %s", nodeaddress.GetName())
+	log.Infof("Node-IPv6: %s", nodeaddress.GetIPv6())
+	log.Infof("Node-IPv4: %s", nodeaddress.GetIPv4())
 	log.Infof("Cluster IPv6 prefix: %s", nodeaddress.GetIPv6ClusterRange())
 	log.Infof("Cluster IPv4 prefix: %s", nodeaddress.GetIPv4ClusterRange())
 	log.Infof("IPv6 node prefix: %s", nodeaddress.GetIPv6NodeRange())
