845a82a0d1264b3dbf981b8867c8773f4f43589e
Thomas Graf <thomas@cilium.io>
2017-07-11 00:01:02 +0200

    find-llvm.sh: Add fast track for common location
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- contrib/scripts/find-llvm.sh
+++ contrib/scripts/find-llvm.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+
+if [ -x /usr/local/clang/bin/clang ]; then
+	echo "/usr/local/clang"
+	exit 0
+fi
+
 find / -iname libLLVMBPFCodeGen.a | head -1 | xargs dirname | xargs dirname
