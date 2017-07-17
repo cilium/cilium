45a6b708123c74f3ec12b58d277267292354c000
André Martins <andre@cilium.io>
2017-07-17 20:55:15 +0200

    tests: adding k8s GSG to multi node test
    Signed-off-by: André Martins <andre@cilium.io>

--- tests/k8s/multi-node/run-tests.bash
+++ tests/k8s/multi-node/run-tests.bash
@@ -4,7 +4,15 @@ set -ex
 
 vm_suffix="${BUILD_NUMBER:+-build-$BUILD_NUMBER}"
 
+node1="k8s1${vm_suffix}"
 node2="k8s2${vm_suffix}"
+
+function reinstall_ipv4(){
+    vm="${1}"
+    vagrant ssh ${vm} -- -t '/home/vagrant/go/src/github.com/cilium/cilium/tests/k8s/multi-node/cluster/cluster-manager.bash reinstall --yes-delete-all-etcd-data'
+    vagrant ssh ${vm} -- -t 'sudo cp -R /root/.kube /home/vagrant'
+    vagrant ssh ${vm} -- -t 'sudo chown vagrant.vagrant -R /home/vagrant/.kube'
+}
 
 function reinstall_ipv6(){
     vm="${1}"
@@ -23,6 +31,11 @@ function deploy_cilium_lb(){
 
 echo "================== Running in IPv4 mode =================="
 
+# Run the GSG first and then restart the cluster to run the remaining tests
+vagrant ssh ${node1} -- -t '/home/vagrant/go/src/github.com/cilium/cilium/tests/k8s/multi-node/tests/00-gsg-test.bash'
+
+reinstall_ipv4 ${node1}
+reinstall_ipv4 ${node2}
 # Set up cilium-lb-ds and cilium-ds
 deploy_cilium
 
@@ -31,14 +44,19 @@ vagrant ssh ${node2} -- -t 'for test in /home/vagrant/go/src/github.com/cilium/c
 # Run ipv4 tests
 vagrant ssh ${node2} -- -t 'for test in /home/vagrant/go/src/github.com/cilium/cilium/tests/k8s/multi-node/tests/ipv4/*.sh; do $test; done'
 
+# Run IPv6 tests
+
 # Reinstall everything with IPv6 addresses
-# Kubeadm doesn't quite support IPv6 yet
-#reinstall_ipv6 ${master}
+# FIXME Kubeadm doesn't quite support IPv6 yet
+#reinstall_ipv6 ${node1}
 #reinstall_ipv6 ${node2}
 
 echo "================== Running in IPv6 mode =================="
 
 echo "IPv6 tests are currently disabled"
+# Run the GSG first and then restart the cluster to run the remaining tests
+#vagrant ssh ${node1} -- -t '/home/vagrant/go/src/github.com/cilium/cilium/tests/k8s/multi-node/tests/00-gsg-test.bash'
+#
 # Set up cilium-lb-ds and cilium-ds
 #deploy_cilium
 
---
+++ tests/k8s/multi-node/tests/00-gsg-test.bash
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+
+# This test:
+# - K8s GSG in our multi node environment
+#
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "${dir}/../helpers.bash"
+# dir might have been overwritten by helpers.bash
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "${dir}/../cluster/env.bash"
+
+set -ex
+
+NAMESPACE="kube-system"
+GOPATH="/home/vagrant/go"
+
+MINIKUBE="${dir}/../../../../examples/minikube"
+K8SDIR="${dir}/../../../../examples/kubernetes"
+GSGDIR="${dir}/deployments/gsg"
+
+function cleanup {
+	kubectl delete -f "${MINIKUBE}/l3_l4_l7_policy.yaml" 2> /dev/null || true
+	kubectl delete -f "${MINIKUBE}/l3_l4_policy.yaml" 2> /dev/null || true
+	kubectl delete -f "${GSGDIR}/demo.yaml" 2> /dev/null || true
+	kubectl delete -f "${K8SDIR}/cilium-ds-gsg.yaml" 2> /dev/null || true
+	kubectl delete -f "${K8SDIR}/rbac.yaml" 2> /dev/null || true
+}
+
+function gather_logs {
+	mkdir -p ./cilium-files/logs
+	kubectl logs -n kube-system ${CILIUM_POD_1} > ./cilium-files/logs/cilium-logs-1 || true
+	kubectl logs -n kube-system ${CILIUM_POD_2} > ./cilium-files/logs/cilium-logs-2 || true
+	kubectl logs -n kube-system kube-apiserver-k8s-1 > ./cilium-files/logs/kube-apiserver-k8s-1-logs || true
+	kubectl logs -n kube-system kube-controller-manager-k8s-1 > ./cilium-files/logs/kube-controller-manager-k8s-1-logs || true
+	journalctl -au kubelet > ./cilium-files/logs/kubelet-k8s-1-logs || true
+}
+
+function finish_test {
+	gather_files k8s-gsg-test ${TEST_SUITE}
+	gather_logs
+	cleanup
+}
+
+trap finish_test exit
+
+cleanup
+
+wait_for_healthy_k8s_cluster 2
+
+echo "----- adding RBAC for Cilium -----"
+kubectl create -f "${K8SDIR}/rbac.yaml"
+
+echo "----- deploying Cilium Daemon Set onto cluster -----"
+cp "${K8SDIR}/cilium-ds-gsg.yaml" ./cilium-ds.yaml
+sed -i s+/var/lib/kubelet/kubeconfig+/etc/kubernetes/kubelet.conf+g cilium-ds.yaml
+sed -i s+/cilium/cilium:stable+localhost:5000/cilium:${DOCKER_IMAGE_TAG}+g cilium-ds.yaml
+kubectl create -f cilium-ds.yaml
+
+wait_for_daemon_set_ready ${NAMESPACE} cilium 2
+
+CILIUM_POD_1=$(kubectl -n ${NAMESPACE} get pods -l k8s-app=cilium | awk 'NR==2{ print $1 }')
+wait_for_kubectl_cilium_status ${NAMESPACE} ${CILIUM_POD_1}
+
+CILIUM_POD_2=$(kubectl -n ${NAMESPACE} get pods -l k8s-app=cilium | awk 'NR==3{ print $1 }')
+wait_for_kubectl_cilium_status ${NAMESPACE} ${CILIUM_POD_2}
+
+echo "----- deploying demo application onto cluster -----"
+
+# Since the GSG guide is intended to be used on a single cluster we need
+# to add the nodeSelector to a single node so we can properly test the GSG
+cp "${MINIKUBE}/demo.yaml" "${GSGDIR}/demo.yaml"
+patch -p0 "${GSGDIR}/demo.yaml" "${GSGDIR}/minikube-gsg-l7-fix.diff"
+
+kubectl create -f "${GSGDIR}/demo.yaml"
+
+wait_for_n_running_pods 4
+
+echo "----- adding L3 L4 policy  -----"
+kubectl create -f "${MINIKUBE}/l3_l4_policy.yaml"
+
+wait_all_k8s_regenerated 5
+
+echo "----- testing L3/L4 policy -----"
+APP2_POD=$(kubectl get pods -l id=app2 -o jsonpath='{.items[0].metadata.name}')
+SVC_IP=$(kubectl get svc app1-service -o jsonpath='{.spec.clusterIP}' )
+
+echo "----- testing app2 can reach app1 (expected behavior: can reach) -----"
+RETURN=$(kubectl $ID exec $APP2_POD -- curl -s --output /dev/stderr -w '%{http_code}' --connect-timeout 10 -XGET $SVC_IP || true)
+if [[ "${RETURN//$'\n'}" != "200" ]]; then
+	abort "Error: could not reach pod allowed by L3 L4 policy"
+fi
+
+echo "----- testing that app3 cannot reach app 1 (expected behavior: cannot reach)"
+APP3_POD=$(kubectl get pods -l id=app3 -o jsonpath='{.items[0].metadata.name}')
+RETURN=$(kubectl exec $APP3_POD -- curl -s --output /dev/stderr -w '%{http_code}' --connect-timeout 10 -XGET $SVC_IP || true)
+if [[ "${RETURN//$'\n'}" != "000" ]]; then
+	abort "Error: unexpectedly reached pod allowed by L3 L4 Policy, received return code ${RETURN}"
+fi
+
+echo "------ performing HTTP GET on ${SVC_IP}/public from service2 ------"
+RETURN=$(kubectl exec $APP2_POD -- curl -s --output /dev/stderr -w '%{http_code}' --connect-timeout 10 http://${SVC_IP}/public || true)
+if [[ "${RETURN//$'\n'}" != "200" ]]; then
+	abort "Error: Could not reach ${SVC_IP}/public on port 80"
+fi
+
+echo "------ performing HTTP GET on ${SVC_IP}/private from service2 ------"
+RETURN=$(kubectl exec $APP2_POD -- curl -s --output /dev/stderr -w '%{http_code}' --connect-timeout 10 http://${SVC_IP}/private || true)
+if [[ "${RETURN//$'\n'}" != "200" ]]; then
+	abort "Error: Could not reach ${SVC_IP}/private on port 80"
+fi
+
+echo "----- creating L7-aware policy -----"
+kubectl create -f "${MINIKUBE}/l3_l4_l7_policy.yaml"
+
+echo "----- Waiting for endpoints to get into 'ready' state -----"
+wait_all_k8s_regenerated 5
+
+echo "------ performing HTTP GET on ${SVC_IP}/public from service2 ------"
+RETURN=$(kubectl exec $APP2_POD -- curl -s --output /dev/stderr -w '%{http_code}' --connect-timeout 10 http://${SVC_IP}/public || true)
+if [[ "${RETURN//$'\n'}" != "200" ]]; then
+	abort "Error: Could not reach ${SVC_IP}/public on port 80"
+fi
+
+echo "------ performing HTTP GET on ${SVC_IP}/private from service2 ------"
+RETURN=$(kubectl exec $APP2_POD -- curl -s --output /dev/stderr -w '%{http_code}' --connect-timeout 10 http://${SVC_IP}/private || true)
+if [[ "${RETURN//$'\n'}" != "403" ]]; then
+	abort "Error: Unexpected success reaching  ${SVC_IP}/private on port 80"
+fi
+
+echo "------ L7 policy success ! ------"
