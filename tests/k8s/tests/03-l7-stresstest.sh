3e1e02b02ae5fc9675de807f7278cd2f2435ce79
André Martins <andre@cilium.io>
2017-07-18 22:38:11 +0200

    tests: removed L7 workaround
    Since L7 works on a multinode environment we can remove the workaround
    created.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- tests/k8s/tests/03-l7-stresstest.sh
+++ tests/k8s/tests/03-l7-stresstest.sh
@@ -31,8 +31,7 @@ sed "s/\$kube_node_selector/${node_selector}/" \
     "${l7_stresstest_dir}/1-frontend.json.sed" > "${l7_stresstest_dir}/1-frontend.json"
 
 # Set backend on k8s-2 to force inter-node communication
-# FIXME FIXME FIXME replace with k8s-2 when the l3 + l7 policies are working
-node_selector="k8s-1"
+node_selector="k8s-2"
 
 sed "s/\$kube_node_selector/${node_selector}/" \
     "${l7_stresstest_dir}/2-backend-server.json.sed" > "${l7_stresstest_dir}/2-backend-server.json"
@@ -43,14 +42,15 @@ kubectl create namespace development
 
 kubectl create -f "${l7_stresstest_dir}"
 
-kubectl get pods -n qa -o wide
-kubectl get pods -n development -o wide
-
 wait_for_running_pod frontend qa
 wait_for_running_pod backend development
 
 wait_for_service_endpoints_ready development backend 80
 # frontend doesn't have any endpoints
+
+kubectl get pods -n qa -o wide
+kubectl get pods -n development -o wide
+kubectl describe svc -n development backend
 
 frontend_pod=$(kubectl get pods -n qa | grep frontend | awk '{print $1}')
 backend_pod=$(kubectl get pods -n development | grep backend | awk '{print $1}')
@@ -58,34 +58,34 @@ backend_pod=$(kubectl get pods -n development | grep backend | awk '{print $1}')
 backend_svc_ip=$(kubectl get svc -n development | awk 'NR==2{print $2}')
 
 echo "Running tests WITHOUT Policy / Proxy loaded"
-kubectl exec -n qa -i ${frontend_pod} -- wrk -t20 -c1000 -d60 "http://${backend_svc_ip}:80/"
-kubectl exec -n qa -i ${frontend_pod} -- ab -r -n 1000000 -c 200 -s 60 -v 1 "http://${backend_svc_ip}:80/"
 
 code=$(kubectl exec -n qa -i ${frontend_pod} -- curl -s -o /dev/null -w "%{http_code}" http://${backend_svc_ip}:80/)
 
-if [ ${code} -ne 200 ]; then abort "Error: unable to connect between frontend and backend" ; fi
+if [ ${code} -ne 200 ]; then abort "Error: unable to connect between frontend and backend:80/" ; fi
+
+kubectl exec -n qa -i ${frontend_pod} -- wrk -t20 -c1000 -d60 "http://${backend_svc_ip}:80/"
+kubectl exec -n qa -i ${frontend_pod} -- ab -r -n 1000000 -c 200 -s 60 -v 1 "http://${backend_svc_ip}:80/"
 
 kubectl create -f "${l7_stresstest_dir}/policies"
 
 wait_all_k8s_regenerated
 
 echo "Running tests WITH Policy / Proxy loaded"
-kubectl exec -n qa -i ${frontend_pod} -- wrk -t20 -c1000 -d60 "http://${backend_svc_ip}:80/"
 
+code=$(kubectl exec -n qa -i ${frontend_pod} -- curl --connect-timeout 10 -s -o /dev/null -w "%{http_code}" http://${backend_svc_ip}:80/)
+
+if [ ${code} -ne 200 ]; then abort "Error: unable to connect between frontend and backend" ; fi
+
+code=$(kubectl exec -n qa -i ${frontend_pod} -- curl --connect-timeout 10 -s -o /dev/null -w "%{http_code}" http://${backend_svc_ip}:80/health)
+
+if [ ${code} -ne 403 ]; then abort "Error: unexpected connection between frontend and backend. wanted HTTP 403, got: HTTP ${code}" ; fi
+
+kubectl exec -n qa -i ${frontend_pod} -- wrk -t20 -c1000 -d60 "http://${backend_svc_ip}:80/"
 # FIXME: Due proxy constrains (memory?) it's impossible to execute the test
 # with 1000000 requests and 200 parallel connections. It was tested with
 # 1 request and 1 parallel connection with no success.
 #
 #kubectl exec -n qa -i ${frontend_pod} -- ab -r -n 1000000 -c 200 -s 60 -v 1 "http://${backend_svc_ip}:80/"
-
-code=$(kubectl exec -n qa -i ${frontend_pod} -- curl -s -o /dev/null -w "%{http_code}" http://${backend_svc_ip}:80/)
-
-if [ ${code} -ne 200 ]; then abort "Error: unable to connect between frontend and backend" ; fi
-
-code=$(kubectl exec -n qa -i ${frontend_pod} -- curl -s -o /dev/null -w "%{http_code}" http://${backend_svc_ip}:80/health)
-
-if [ ${code} -ne 403 ]; then abort "Error: unexpected connection between frontend and backend. wanted HTTP 403, got: HTTP ${code}" ; fi
-
 
 echo "SUCCESS!"
 
