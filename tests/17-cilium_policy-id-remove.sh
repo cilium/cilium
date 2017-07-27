e9aa4d2ce439bb3c4e2ccc42422ebd3ec15db041
Alexander Alemayhu <alexander@alemayhu.com>
2017-07-27 17:02:05 +0200

    tests: check if cilium_policy_$ep are removed
    Closes: #738 (tests: Add test case for cilium_policy id removal)
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ tests/17-cilium_policy-id-remove.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+source ./helpers.bash
+
+TEST_NET="cilium"
+
+function cleanup {
+	gather_files 17-cilium_policy-id-remove ${TEST_SUITE}
+	docker rm -f a b 2> /dev/null || true
+	docker network rm $TEST_NET > /dev/null 2>&1
+}
+
+trap cleanup EXIT
+
+cleanup
+logs_clear
+
+docker network inspect $TEST_NET 2> /dev/null || {
+	docker network create --ipv6 --subnet ::1/112 --ipam-driver cilium --driver cilium $TEST_NET
+}
+
+docker run -dt --net=$TEST_NET --name a -l id.a tgraf/netperf
+docker run -dt --net=$TEST_NET --name b -l id.b tgraf/netperf
+
+known_endpoints=`cilium endpoint list|awk 'NR>2 { print $1 }'`
+
+# Sanity check
+for ep in $known_endpoints; do
+  ep_policy_map="/sys/fs/bpf/tc/globals/cilium_policy_$ep"
+  if [ ! -f $ep_policy_map ]; then
+    abort "No such file $ep_policy_map"
+  fi
+done
+
+docker rm -f a b
+
+# There should only be one cilium_policy file after the containers are gone.
+# Ignoring the reserved files.
+actual=`find /sys/fs/bpf/tc/globals/cilium_policy*|grep -v reserved`
+expected="/sys/fs/bpf/tc/globals/cilium_policy"
+if [ "$actual" != "$expected" ]; then
+  abort "want $expected got $actual"
+fi
