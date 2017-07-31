3d293ac3c333d200e6db84d6350923094a91e213
Ian Vernon <ian@covalent.io>
2017-07-31 14:58:58 -0700

    install correct docker version in k8s vm

--- tests/k8s/cluster/cluster-manager.bash
+++ tests/k8s/cluster/cluster-manager.bash
@@ -189,15 +189,18 @@ EOF
 }
 
 function install_kubeadm_dependencies(){
-    sudo apt-get update && sudo apt-get install -y apt-transport-https
+    sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo touch /etc/apt/sources.list.d/kubernetes.list
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg  | sudo apt-key add -
     sudo bash -c "cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 "
-
-    sudo apt-get update && sudo apt-get install -y docker-engine
+    # Remove older versions of Docker so we can install Docker 17.06
+    sudo apt-get remove docker docker-engine docker.io
+    sudo apt-get update && sudo apt-get install -y docker-ce=17.06
     sudo usermod -aG docker vagrant
 }
 
