29c508345e33f3541684705387b5880b99b20ae4
Ian Vernon <ian@covalent.io>
2017-07-31 16:28:03 -0700

    add logs to cluster-manager.bash for kubeadm-dependencies installation

--- tests/k8s/cluster/cluster-manager.bash
+++ tests/k8s/cluster/cluster-manager.bash
@@ -189,6 +189,7 @@ EOF
 }
 
 function install_kubeadm_dependencies(){
+    echo "============== installing kubeadm dependencies =============="
     sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -199,9 +200,10 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 "
     # Remove older versions of Docker so we can install Docker 17.06
-    sudo apt-get remove docker docker-engine docker.io
+    sudo apt-get remove docker docker-engine docker.io 
     sudo apt-get update && sudo apt-get install -y docker-ce=17.06
     sudo usermod -aG docker vagrant
+    echo "echo "============== done installing kubeadm dependencies =============="
 }
 
 function install_kubeadm() {
