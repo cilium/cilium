6ffaa9116ff7b75cf516ce9208fa161d47da2af2
Ian Vernon <ian@covalent.io>
2017-08-11 17:25:07 -0700

    finish test cases that comprise entire state machine for policy enforcement

--- tests/k8s/tests/test-001-policy-enforcement.sh
+++ tests/k8s/tests/test-001-policy-enforcement.sh
@@ -104,7 +104,7 @@ service cilium restart
 # Default behavior is to have policy enforcement disabled for all endpoints that have
 # no rules applying to them. Since no policies have been imported, all endpoints should have 
 # policy enforcement disabled.
-echo "---- default mode: test configuration with no policy imported ----"
+echo "---- Test 1: default mode: test configuration with no policy imported ----"
 kubectl delete -f ${MINIKUBE}/demo.yaml
 kubectl create -f ${MINIKUBE}/demo.yaml
 wait_for_n_running_pods 4
@@ -119,7 +119,7 @@ check_endpoints_policy_disabled 5
 # Import the following policy, which only applies to app3. 
 # Since policy enforcement is in 'default' mode for the daemon, policy enforcement 
 # should be enabled for only one endpoint (app3), and should be disabled for all other endpoints.
-echo "---- default mode: test with policy imported  ----"
+echo "---- Test 2: default mode: test with policy imported  ----"
 
 echo "---- Importing L3 CIDR Policy ----"
 cat << EOF | policy_import_and_wait -
@@ -140,20 +140,123 @@ check_endpoints_policy_disabled 4
 # Test 3: Delete policy and check that all endpoints have policy enforcement disabled.
 # Delete the aforementioned policy. Since the policy repository is now empty, we expect
 # that all endpoints should have policy enforcement disabled.
-echo "---- default mode: check that policy enforcement for each endpoint is disabled after all policies are removed ----"
+echo "---- Test 3: default mode: check that policy enforcement for each endpoint is disabled after all policies are removed ----"
 policy_delete_and_wait "--all"
 check_endpoints_policy_disabled 5
 
 # Test 4: Change daemon's policy enforcement configuration from 'default' to 'true' with no policy imported. 
 #We expect that all endpoints should have policy enforcement enabled after this configuration is applied.
-echo "---- enabled mode: check that each endpoint has policy enforcement enabled with no policy imported ----"
+echo "---- Test 4: enabled mode: check that each endpoint has policy enforcement enabled with no policy imported ----"
 cilium config PolicyEnforcement=always
 wait_for_endpoints 5
 check_endpoints_policy_enabled 5
 
-# Test 5: Change daemon's policy enforcement configuration from 'true' to false'.
+# Test 5: Change daemon's policy enforcement configuration from 'true' to 'false'.
 # We expect that all endpoints should have policy enforcement disabled after this configuration is applied.
-echo "---- disabled mode: check that each endpoint has policy enforcement disabled with no policy imported ----"
+echo "---- Test 5: disabled mode: check that each endpoint has policy enforcement disabled with no policy imported ----"
 cilium config PolicyEnforcement=never
+wait_for_endpoints 5
+check_endpoints_policy_disabled 5
+
+# Test 6: Import a policy while policy enforcement is disabled.
+# Policy enforcement should be disabled for all endpoints.
+echo "---- Test 6: disabled mode: check that each endpoint has policy enforcement disabled with policy imported ----"
+echo "---- Importing L3 CIDR Policy ----"
+cat << EOF | policy_import_and_wait -
+[{
+     "endpointSelector": {"matchLabels":{"k8s:id":"app3"}},
+     "egress": [{
+         "toCIDR": [ { "ip": "9.9.9.9/32" } ]
+     }]
+ }]
+EOF
+
+echo "---- Policies in cilium ----"
+cilium policy get
+
+check_endpoints_policy_disabled 5
+
+# Test 7: Change daemon's policy enforcement configuration from 'false' to 'true' with a policy imported.
+# Policy enforcement should be enabled for all endpoints.
+echo "---- Test 7 ----"
+cilium config PolicyEnforcement=always
+wait_for_endpoints 5
+check_endpoints_policy_enabled 5
+
+# Test 8: Change daemon's policy enforcement configuration from 'true' to 'default' with a policy imported.
+# Policy enforcement should be enabled for only one endpoint.
+echo "---- Test 8 ----"
+cilium config PolicyEnforcement=default
+wait_for_endpoints 5
+check_endpoints_policy_enabled 1
+
+# Test 9: Change daemon's policy enforcement configuration from 'default' to 'true' with a policy imported.
+# Policy enforcement should be enabled for all endpoints.
+echo "---- Test 9 ----"
+cilium config PolicyEnforcement=always
+wait_for_endpoints 5
+check_endpoints_policy_enabled 5
+
+# Test 10: Delete policy while policy enforcement is set to 'true'. 
+# Policy enforcement should be 'true' for all endpoints.
+echo "---- Test 10 ----"
+policy_delete_and_wait "--all"
+wait_for_endpoints 5
+check_endpoints_policy_enabled 5
+
+# Test 11: Import a policy while policy enforcement is set to 'true'. 
+# All endpoints should have policy enforcement enabled.
+echo "---- Test 11 ----"
+echo "---- Importing L3 CIDR Policy ----"
+cat << EOF | policy_import_and_wait -
+[{
+     "endpointSelector": {"matchLabels":{"k8s:id":"app3"}},
+     "egress": [{
+         "toCIDR": [ { "ip": "9.9.9.9/32" } ]
+     }]
+ }]
+EOF
+wait_for_endpoints 5
+check_endpoints_policy_enabled 5
+
+# Test 12: Set policy enforcement 'true' --> 'false' while a policy is imported.
+# All endpoints should have policy enforcement disabled. 
+echo "---- Test 12 ----"
+cilium config PolicyEnforcement=never
+wait_for_endpoints 5
+check_endpoints_policy_disabled 5
+
+# Test 13: Delete a policy while policy enforcement is set to 'false.
+# All endpoints should have policy enforcement disabled.
+echo "---- Test 13 ----"
+policy_delete_and_wait "--all"
+wait_for_endpoints 5
+check_endpoints_policy_disabled 5
+
+# Test 14: Set policy enforcement 'false' --> 'true' with no policy imported.
+# All endpoints should have policy enforcement enabled.
+echo "---- Test 14 ----"
+cilium config PolicyEnforcement=always
+wait_for_endpoints 5
+check_endpoints_policy_enabled 5
+
+# Test 15: Set policy enforcement 'true' --> 'default' with no policy imported.
+# All endpoints should have policy enforcement disabled.
+echo "---- Test 15 ----"
+cilium config PolicyEnforcement=default
+wait_for_endpoints 5
+check_endpoints_policy_disabled 5
+
+# Test 16: Set policy enforcement 'default' --> 'false' with no policy imported.
+# All endpoints should have policy enforcement disabled.
+echo "---- Test 16 ----"
+cilium config PolicyEnforcement=never
+wait_for_endpoints 5
+check_endpoints_policy_disabled 5
+
+# Test 17: Set policy enforcement 'false' --> 'default' with no policy imported.
+# All endpoints should have policy enforcement disabled.
+echo "---- Test 17 ----"
+cilium config PolicyEnforcement=default
 wait_for_endpoints 5
 check_endpoints_policy_disabled 5
