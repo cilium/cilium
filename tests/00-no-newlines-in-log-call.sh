c224ba554143767d563c8d9edab4fe53109f20c2
Michi Mutsuzaki <michi@covalent.io>
2017-08-31 16:03:05 -0700

    test: Exclude logBuf from the newline test
    Signed-off-by: Michi Mutsuzaki <michi@covalent.io>

--- tests/00-no-newlines-in-log-call.sh
+++ tests/00-no-newlines-in-log-call.sh
@@ -5,6 +5,7 @@ source ./helpers.bash
 if grep --include \*.go -r log\. ../ | grep -v vendor \
   | grep -v contrib \
   | grep -v logging.go \
+  | grep -v logBuf \
   | grep -F "\n"; then
   abort "found newline(s) in log call(s), please remove ending \n"
 fi
