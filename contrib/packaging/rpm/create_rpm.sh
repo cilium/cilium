60971e926d09aee666edd723808679ec75734f0e
Tony Lambiris <tony@criticalstack.com>
2017-09-17 13:32:52 -0400

    Fix directory paths

--- contrib/packaging/rpm/create_rpm.sh
+++ contrib/packaging/rpm/create_rpm.sh
@@ -7,10 +7,5 @@ source /opt/cilium/env
 
 envsubst < /opt/cilium/cilium.spec.envsubst > /opt/cilium/cilium.spec
 fedpkg --release f26 local
-set +x
 
 find /opt/cilium -type f -name 'cilium*.rpm' -exec mv -f "{}" /output/ \;
-
-echo
-echo "Cilium version ${VERSION} packages can be found in output/ directory"
-echo
