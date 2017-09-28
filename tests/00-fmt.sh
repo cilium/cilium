0f08df7805f4f59cbde20eed60a37578fd7361c7
Joe Stringer <joe@covalent.io>
2017-09-28 14:21:02 -0700

    tests: Skip contrib/ for go vet
    The contrib/ directory sometimes interferes with the formatting checks
    that are invoked by running "make tests", remove them from the
    formatting .go file locator.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- tests/00-fmt.sh
+++ tests/00-fmt.sh
@@ -2,7 +2,12 @@
 
 set -ex
 
-diff="$(find . ! \( -path './vendor' -prune \) ! \( -path './.git' -prune \) ! -samefile ./daemon/bindata.go -type f -name '*.go' -print0 | xargs -0 gofmt -d -l -s )"
+diff="$(find . ! \( -path './contrib' -prune \) \
+        ! \( -path './vendor' -prune \) \
+        ! \( -path './.git' -prune \) \
+        ! -samefile ./daemon/bindata.go \
+        -type f -name '*.go' -print0 \
+                | xargs -0 gofmt -d -l -s )"
 
 if [ -n "$diff" ]; then
 	echo "Unformatted Go source code:"
