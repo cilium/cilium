cilium/cilium

commit 9bd565261ac733f2e07d5288851fd2683f7f49d3

bpf: manually handle the perf read state memory allocation
When the Cilium monitor is under heavy load we see the following crash

	panic: runtime error: cgo argument has Go pointer to Go pointer

The crash is occurring near the call to `C.perf_event_read(...)`.  At
some point in time the state struct instance contains a Go pointer to Go
pointer.  According to the [Rules for passing pointers between Go and
C][0] this is not allowed.

>Go code may pass a Go pointer to C provided that the Go memory to which
it points does not contain any Go pointers.

I have not studied the Go source well enough to back up any suspicions
that something is wrong in the runtime code, but looking at workarounds
we could bypass the crasher by disabling the cgo runtime checks, but
that doesn't really make sense considering that it requires a user to
make a change. The other approach, which this patch does is instead of
relying on `struct_read_state` to do the right thing, we fallback to
malloc / free so we can reduce the chance of mismatch between Go and C
memory.

After this patch I am not able to reproduce the crasher.

[0]: https://github.com/golang/proposal/blob/master/design/12416-cgo-pointers.md
Closes: #409 (Crash in cilium monitor (ring buffer overflow?))
Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

pkg/bpf/perf.go
@@ -23,6 +23,7 @@ package bpf
 #include <linux/bpf.h>
 #include <linux/perf_event.h>
 #include <sys/resource.h>
+#include <stdlib.h>
 
 void create_perf_event_attr(int type, int config, int sample_type,
 			    int wakeup_events, void *attr)
@@ -303,11 +304,11 @@ func (e *PerfEvent) Disable() error {
 
 func (e *PerfEvent) Read(receive ReceiveFunc, lostFn LostFunc) error {
 	buf := make([]byte, 256)
-	state := C.struct_read_state{}
+	state := C.malloc(C.size_t(unsafe.Sizeof(C.struct_read_state{})))
 
 	// Prepare for reading and check if events are available
 	available := C.perf_event_read_init(C.int(e.npages), C.int(e.pagesize),
-		unsafe.Pointer(&e.data[0]), unsafe.Pointer(&state))
+		unsafe.Pointer(&e.data[0]), unsafe.Pointer(state))
 
 	// Poll false positive
 	if available == 0 {
@@ -317,7 +318,7 @@ func (e *PerfEvent) Read(receive ReceiveFunc, lostFn LostFunc) error {
 	for {
 		var msg *PerfEventHeader
 
-		if ok := C.perf_event_read(unsafe.Pointer(&state),
+		if ok := C.perf_event_read(unsafe.Pointer(state),
 			unsafe.Pointer(&buf[0]), unsafe.Pointer(&msg)); ok == 0 {
 			break
 		}
@@ -339,7 +340,8 @@ func (e *PerfEvent) Read(receive ReceiveFunc, lostFn LostFunc) error {
 	}
 
 	// Move ring buffer tail pointer
-	C.perf_event_read_finish(unsafe.Pointer(&e.data[0]), unsafe.Pointer(&state))
+	C.perf_event_read_finish(unsafe.Pointer(&e.data[0]), unsafe.Pointer(state))
+	C.free(state)
 
 	return nil
 }

commit 8bf34a91e40e35ec6308fc6ce9436f53d7c6f313

pkg/bpf: free up allocated memory for state on poll false positives
Have not measured how often this happens, but we should still free.

Related-to: #1807 (cilium leaks memory when running inside a container)
Fixes: 9bd565261ac7 (bpf: manually handle the perf read state memory allocation, 2017-08-12)
Backport: 31021efa1b209cff60cdbb67c9126e5b4dd9d012
Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

pkg/bpf/perf.go
@@ -305,6 +305,7 @@ func (e *PerfEvent) Disable() error {
 func (e *PerfEvent) Read(receive ReceiveFunc, lostFn LostFunc) {
 	buf := make([]byte, 256)
 	state := C.malloc(C.size_t(unsafe.Sizeof(C.struct_read_state{})))
+	defer C.free(state)
 
 	// Prepare for reading and check if events are available
 	available := C.perf_event_read_init(C.int(e.npages), C.int(e.pagesize),
@@ -341,7 +342,6 @@ func (e *PerfEvent) Read(receive ReceiveFunc, lostFn LostFunc) {
 
 	// Move ring buffer tail pointer
 	C.perf_event_read_finish(unsafe.Pointer(&e.data[0]), unsafe.Pointer(state))
-	C.free(state)
 }
 
 func (e *PerfEvent) Close() {