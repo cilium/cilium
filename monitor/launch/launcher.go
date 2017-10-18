7a882fbe6e6208566daadcb72c6385f2142a9ed1
Ray Bejjani <ray@covalent.io>
2017-10-18 17:56:22 +0200

    pkg/monitor: consistently use logrus fields
    Signed-off-by: Ray Bejjani <ray@covalent.io>

--- monitor/launch/launcher.go
+++ monitor/launch/launcher.go
@@ -42,7 +42,7 @@ func (nm *NodeMonitor) Run() {
 		cmd := exec.Command(targetName, nm.GetArg())
 		stdout, _ := cmd.StdoutPipe()
 		if err := cmd.Start(); err != nil {
-			log.Errorf("cmd.Start(): %s", err)
+			log.WithError(err).Error("cmd.Start()")
 		}
 
 		nm.setProcess(cmd.Process)
@@ -69,7 +69,7 @@ func (nm *NodeMonitor) Restart(arg string) {
 		return
 	}
 	if err := nm.process.Kill(); err != nil {
-		log.Errorf("process.Kill(): %s", err)
+		log.WithError(err).Error("process.Kill()")
 	}
 	nm.process = nil
 }
