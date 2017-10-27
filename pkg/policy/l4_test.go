cilium/cilium

commit 13e4f3983502522856d1a280c2bdaf6121a65401

l4: Add L3 dependency to L4 policies
This commit extends the L4Filter to include a list of EndpointSelectors,
which enables a future commit to narrow down which endpoints the L4
policy will ultimately apply to. For now, the existing paths will follow
the paths with the WildcardEndpointSelector, which allows an L4Filter to
apply to all traffic (as the L4-only policies already do today).

Signed-off-by: Amre Shakimov <amre@covalent.io>
Co-authored-by: Joe Stringer <joe@covalent.io>
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/policy/l4.go
@@ -22,7 +22,16 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/policy/api"
+	log "github.com/sirupsen/logrus"
 )
+
+const (
+	// WildcardEndpointSelector is a special hash value for the wildcard endpoint, i.e., applies to all
+	WildcardEndpointSelector = iota
+)
+
+// L7DataMap contains a map of L7 rules per endpoint where key is a hash of EndpointSelector
+type L7DataMap map[uint64]api.L7Rules
 
 // L7ParserType is the type used to indicate what L7 parser to use and
 // defines all supported types of L7 parsers
@@ -40,18 +49,54 @@ type L4Filter struct {
 	Port int
 	// Protocol is the L4 protocol to allow or NONE
 	Protocol string
+	// FromEndpoints limit the source labels for allowing traffic. If
+	// FromEndpoints is empty, then it selects all endpoints.
+	FromEndpoints []api.EndpointSelector `json:"-"`
 	// L7Parser specifies the L7 protocol parser (optional)
 	L7Parser L7ParserType
 	// L7RedirectPort is the L7 proxy port to redirect to (optional)
 	L7RedirectPort int
-	// L7Rules is a list of L7 rules which are passed to the L7 proxy (optional)
-	L7Rules api.L7Rules
+	// L7RulesPerEp is a list of L7 rules per endpoint passed to the L7 proxy (optional)
+	L7RulesPerEp L7DataMap
 	// Ingress is true if filter applies at ingress
 	Ingress bool
 }
 
-// CreateL4Filter creates an L4Filter based on an api.PortRule and api.PortProtocol
-func CreateL4Filter(rule api.PortRule, port api.PortProtocol, direction string, protocol string) L4Filter {
+func (dm L7DataMap) addRulesForEndpoints(rules api.L7Rules,
+	fromEndpoints []api.EndpointSelector) error {
+
+	if rules.Len() == 0 {
+		return nil
+	}
+	if len(fromEndpoints) > 0 {
+		for _, ep := range fromEndpoints {
+			hash, err := ep.Hash()
+			if err != nil || hash == 0 {
+				return fmt.Errorf("Could not hash (%d) endpoint %e", hash, err)
+			}
+			dm[hash] = api.L7Rules{
+				HTTP:  append(dm[hash].HTTP, rules.HTTP...),
+				Kafka: append(dm[hash].Kafka, rules.Kafka...),
+			}
+		}
+	} else {
+		// If there are no explicit fromEps, have a 'special' wildcard endpoint.
+		dm[WildcardEndpointSelector] = api.L7Rules{
+			HTTP:  append(dm[WildcardEndpointSelector].HTTP, rules.HTTP...),
+			Kafka: append(dm[WildcardEndpointSelector].Kafka, rules.Kafka...),
+		}
+	}
+
+	return nil
+}
+
+// CreateL4Filter creates an L4Filter for the specified api.PortProtocol in
+// the direction ("ingress"/"egress") for a particular protocol.
+// This L4Filter will only apply to endpoints covered by `fromEndpoints`.
+// `rule` allows a series of L7 rules to be associated with this L4Filter.
+func CreateL4Filter(fromEndpoints []api.EndpointSelector, rule api.PortRule, port api.PortProtocol,
+	direction string, protocol string) L4Filter {
+
 	// already validated via PortRule.Validate()
 	p, _ := strconv.ParseUint(port.Port, 0, 16)
 
@@ -59,6 +104,8 @@ func CreateL4Filter(rule api.PortRule, port api.PortProtocol, direction string, 
 		Port:           int(p),
 		Protocol:       protocol,
 		L7RedirectPort: rule.RedirectPort,
+		L7RulesPerEp:   make(map[uint64]api.L7Rules),
+		FromEndpoints:  fromEndpoints,
 	}
 
 	if strings.ToLower(direction) == "ingress" {
@@ -72,7 +119,12 @@ func CreateL4Filter(rule api.PortRule, port api.PortProtocol, direction string, 
 		case len(rule.Rules.Kafka) > 0:
 			l4.L7Parser = ParserTypeKafka
 		}
-		l4.L7Rules = *rule.Rules
+		if err := l4.L7RulesPerEp.addRulesForEndpoints(*rule.Rules,
+			fromEndpoints); err != nil {
+
+			log.Errorf("%s", err)
+			return L4Filter{}
+		}
 	}
 
 	return l4

pkg/endpoint/policy.go
@@ -117,7 +117,7 @@ func (e *Endpoint) cleanUnusedRedirects(owner Owner, oldMap policy.L4PolicyMap, 
 			}
 		}
 
-		if v.L7Parser != "" {
+		if v.IsRedirect() {
 			if err := owner.RemoveProxyRedirect(e, &v); err != nil {
 				log.Warningf("error while removing proxy: %s", err)
 			}

pkg/k8s/network_policy_test.go
@@ -16,6 +16,7 @@ package k8s
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/cilium/cilium/api/v1/models"
@@ -77,12 +78,14 @@ func (s *K8sSuite) TestParseNetworkPolicy(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(len(rules), Equals, 1)
 
+	fromEndpoints := labels.LabelArray{
+		labels.NewLabel(PodNamespaceLabel, v1.NamespaceDefault, labels.LabelSourceK8s),
+		labels.NewLabel("foo3", "bar3", labels.LabelSourceK8s),
+		labels.NewLabel("foo4", "bar4", labels.LabelSourceK8s),
+	}
+
 	ctx := policy.SearchContext{
-		From: labels.LabelArray{
-			labels.NewLabel(PodNamespaceLabel, v1.NamespaceDefault, labels.LabelSourceK8s),
-			labels.NewLabel("foo3", "bar3", labels.LabelSourceK8s),
-			labels.NewLabel("foo4", "bar4", labels.LabelSourceK8s),
-		},
+		From: fromEndpoints,
 		To: labels.LabelArray{
 			labels.NewLabel(PodNamespaceLabel, v1.NamespaceDefault, labels.LabelSourceK8s),
 			labels.NewLabel("foo1", "bar1", labels.LabelSourceK8s),
@@ -95,14 +98,27 @@ func (s *K8sSuite) TestParseNetworkPolicy(c *C) {
 	repo.AddList(rules)
 	c.Assert(repo.CanReachRLocked(&ctx), Equals, api.Allowed)
 
+	matchLabels := make(map[string]string)
+	for _, v := range fromEndpoints {
+		matchLabels[fmt.Sprintf("%s.%s", v.Source, v.Key)] = v.Value
+	}
+	lblSelector := metav1.LabelSelector{
+		MatchLabels: matchLabels,
+	}
+	epSelector := api.EndpointSelector{
+		LabelSelector: &lblSelector,
+	}
+
 	result, err := repo.ResolveL4Policy(&ctx)
 	c.Assert(result, Not(IsNil))
 	c.Assert(err, IsNil)
 	c.Assert(result, DeepEquals, &policy.L4Policy{
 		Ingress: policy.L4PolicyMap{
 			"80/tcp": policy.L4Filter{
-				Port: 80, Protocol: "tcp", L7Parser: "",
-				L7RedirectPort: 0, L7Rules: api.L7Rules{},
+				Port: 80, Protocol: "tcp",
+				FromEndpoints:  []api.EndpointSelector{epSelector},
+				L7Parser:       "",
+				L7RedirectPort: 0, L7RulesPerEp: policy.L7DataMap{},
 				Ingress: true,
 			},
 		},

pkg/k8s/network_policy_v1beta_test.go
@@ -18,6 +18,7 @@ package k8s
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/labels"
@@ -69,12 +70,13 @@ func (s *K8sSuite) TestParseNetworkPolicyDeprecated(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(len(rules), Equals, 1)
 
+	fromEndpoints := labels.LabelArray{
+		labels.NewLabel(PodNamespaceLabel, v1.NamespaceDefault, labels.LabelSourceK8s),
+		labels.NewLabel("foo3", "bar3", labels.LabelSourceK8s),
+		labels.NewLabel("foo4", "bar4", labels.LabelSourceK8s),
+	}
 	ctx := policy.SearchContext{
-		From: labels.LabelArray{
-			labels.NewLabel(PodNamespaceLabel, v1.NamespaceDefault, labels.LabelSourceK8s),
-			labels.NewLabel("foo3", "bar3", labels.LabelSourceK8s),
-			labels.NewLabel("foo4", "bar4", labels.LabelSourceK8s),
-		},
+		From: fromEndpoints,
 		To: labels.LabelArray{
 			labels.NewLabel(PodNamespaceLabel, v1.NamespaceDefault, labels.LabelSourceK8s),
 			labels.NewLabel("foo1", "bar1", labels.LabelSourceK8s),
@@ -87,14 +89,27 @@ func (s *K8sSuite) TestParseNetworkPolicyDeprecated(c *C) {
 	repo.AddList(rules)
 	c.Assert(repo.CanReachRLocked(&ctx), Equals, api.Allowed)
 
+	matchLabels := make(map[string]string)
+	for _, v := range fromEndpoints {
+		matchLabels[fmt.Sprintf("%s.%s", v.Source, v.Key)] = v.Value
+	}
+	lblSelector := metav1.LabelSelector{
+		MatchLabels: matchLabels,
+	}
+	epSelector := api.EndpointSelector{
+		LabelSelector: &lblSelector,
+	}
+
 	result, err := repo.ResolveL4Policy(&ctx)
 	c.Assert(result, Not(IsNil))
 	c.Assert(err, IsNil)
 	c.Assert(result, DeepEquals, &policy.L4Policy{
 		Ingress: policy.L4PolicyMap{
 			"80/tcp": policy.L4Filter{
-				Port: 80, Protocol: "tcp", L7Parser: "",
-				L7RedirectPort: 0, L7Rules: api.L7Rules{},
+				Port: 80, Protocol: "tcp",
+				FromEndpoints:  []api.EndpointSelector{epSelector},
+				L7Parser:       "",
+				L7RedirectPort: 0, L7RulesPerEp: policy.L7DataMap{},
 				Ingress: true,
 			},
 		},

pkg/policy/api/rule.go
@@ -94,9 +94,11 @@ var EntitySelectorMapping = map[Entity]EndpointSelector{
 //
 // - All members of this structure are optional. If omitted or empty, the
 //   member will have no effect on the rule.
-// - All members of this structure are evaluated independently, i.e. L4 ports
-//   allowed with ToPorts do not depend on a match of the FromEndpoints in the
-//   same IngressRule.
+//
+// - If multiple members are set, all of them need to match in order for
+//   the rule to take effect. The exception to this rule is FromRequires field;
+//   the effects of any Requires field in any rule will apply to all other
+//   rules as well.
 type IngressRule struct {
 	// FromEndpoints is a list of endpoints identified by an
 	// EndpointSelector which are allowed to communicate with the endpoint
@@ -252,6 +254,11 @@ type L7Rules struct {
 	//
 	// +optional
 	Kafka []PortRuleKafka `json:"kafka,omitempty"`
+}
+
+// Len returns the total number of rules inside `L7Rules`.
+func (rules *L7Rules) Len() int {
+	return len(rules.HTTP) + len(rules.Kafka)
 }
 
 // PortRuleHTTP is a list of HTTP protocol constraints. All fields are

pkg/policy/api/selector.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/labels"
 
+	"github.com/mitchellh/hashstructure"
 	"github.com/op/go-logging"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sLbls "k8s.io/apimachinery/pkg/labels"
@@ -38,6 +39,11 @@ type EndpointSelector struct {
 func (n EndpointSelector) String() string {
 	j, _ := n.MarshalJSON()
 	return string(j)
+}
+
+// Hash return hash of the internal json structure that represents the endpoint selector
+func (n *EndpointSelector) Hash() (uint64, error) {
+	return hashstructure.Hash(n.LabelSelector, nil)
 }
 
 // UnmarshalJSON unmarshals the endpoint selector from the byte array.

pkg/policy/repository.go
@@ -156,10 +156,10 @@ func (p *Repository) allowsL4Egress(ctx *SearchContext) api.Decision {
 	ctx.EgressL4Only = true
 
 	ctx.PolicyTrace("\n")
-	policy := p.ResolveL4Policy(ctx)
+	policy, err := p.ResolveL4Policy(ctx)
 	verdict := api.Undecided
 	if err == nil {
-		verdict := policy.EgressCoversDPorts(ctx.DPorts)
+		verdict = policy.EgressCoversDPorts(ctx.DPorts)
 	}
 
 	if len(ctx.DPorts) == 0 {
@@ -176,10 +176,10 @@ func (p *Repository) allowsL4Ingress(ctx *SearchContext) api.Decision {
 	ctx.IngressL4Only = true
 
 	ctx.PolicyTrace("\n")
-	policy := p.ResolveL4Policy(ctx)
+	policy, err := p.ResolveL4Policy(ctx)
 	verdict := api.Undecided
 	if err == nil {
-		verdict := policy.IngressCoversDPorts(ctx.DPorts)
+		verdict = policy.IngressCoversDPorts(ctx.DPorts)
 	}
 
 	if len(ctx.DPorts) == 0 {

pkg/policy/rule.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 
 	"github.com/cilium/cilium/pkg/policy/api"
+	log "github.com/sirupsen/logrus"
 )
 
 type rule struct {
@@ -74,14 +75,31 @@ func (r *rule) validate() error {
 	return nil
 }
 
-func mergeL4Port(ctx *SearchContext, r api.PortRule, p api.PortProtocol, dir string, proto string, resMap L4PolicyMap) (int, error) {
+func adjustL4PolicyIfNeeded(fromEndpoints []api.EndpointSelector, policy *L4Filter) bool {
+
+	if len(policy.FromEndpoints) == 0 && len(fromEndpoints) > 0 {
+		log.Debugf("skipping L4 filter %s as the endpoints %s are already covered.", policy, fromEndpoints)
+		return true
+	}
+
+	if len(policy.FromEndpoints) > 0 && len(fromEndpoints) == 0 {
+		// new policy is more permissive than the existing policy
+		// use a more permissive one
+		policy.FromEndpoints = nil
+	}
+	return false
+}
+
+func mergeL4Port(ctx *SearchContext, fromEndpoints []api.EndpointSelector, r api.PortRule, p api.PortProtocol,
+	dir string, proto string, resMap L4PolicyMap) (int, error) {
+
 	key := p.Port + "/" + proto
 	v, ok := resMap[key]
 	if !ok {
-		resMap[key] = CreateL4Filter(r, p, dir, proto)
+		resMap[key] = CreateL4Filter(fromEndpoints, r, p, dir, proto)
 		return 1, nil
 	}
-	l4Filter := CreateL4Filter(r, p, dir, proto)
+	l4Filter := CreateL4Filter(fromEndpoints, r, p, dir, proto)
 	if l4Filter.L7Parser != "" && v.L7Parser == "" {
 		v.L7Parser = l4Filter.L7Parser
 	} else if l4Filter.L7Parser != v.L7Parser {
@@ -94,27 +112,54 @@ func mergeL4Port(ctx *SearchContext, r api.PortRule, p api.PortProtocol, dir str
 		ctx.PolicyTrace("   Merge conflict: mismatching redirect ports %d/%d\n", l4Filter.L7RedirectPort, v.L7RedirectPort)
 		return 0, fmt.Errorf("Cannot merge conflicting redirect ports (%d/%d)", l4Filter.L7RedirectPort, v.L7RedirectPort)
 	}
-	switch {
-	case len(l4Filter.L7Rules.HTTP) > 0:
-		if len(v.L7Rules.Kafka) > 0 {
-			ctx.PolicyTrace("   Merge conflict: mismatching L7 rule types.\n")
-			return 0, fmt.Errorf("Cannot merge conflicting L7 rule types")
-		}
-		v.L7Rules.HTTP = append(v.L7Rules.HTTP, l4Filter.L7Rules.HTTP...)
-	case len(l4Filter.L7Rules.Kafka) > 0:
-		if len(v.L7Rules.HTTP) > 0 {
-			ctx.PolicyTrace("   Merge conflict: mismatching L7 rule types.\n")
-			return 0, fmt.Errorf("Cannot merge conflicting L7 rule types")
-		}
-		v.L7Rules.Kafka = append(v.L7Rules.Kafka, l4Filter.L7Rules.Kafka...)
-	default:
-		ctx.PolicyTrace("   No L7 rules to merge.\n")
+
+	if adjustL4PolicyIfNeeded(fromEndpoints, &v) && len(r.Rules.HTTP) == 0 {
+		// skip this policy as it is already covered and it does not contain L7 rules
+		return 1, nil
 	}
+
+	// if (1) the existing rule did not have a wildcard endpoint
+	// AND (2) the new rule does not have explicit fromEndpoints
+	// THEN we need to copy all existing L7 rules to the wildcard endpoint
+	if _, ok := v.L7RulesPerEp[WildcardEndpointSelector]; !ok && len(fromEndpoints) == 0 {
+		wildcardEp := api.L7Rules{}
+		for _, existingL7Rules := range v.L7RulesPerEp {
+			wildcardEp.HTTP = append(wildcardEp.HTTP, existingL7Rules.HTTP...)
+			wildcardEp.Kafka = append(wildcardEp.Kafka, existingL7Rules.Kafka...)
+		}
+		v.L7RulesPerEp[WildcardEndpointSelector] = wildcardEp
+	}
+
+	for hash, newL7Rules := range l4Filter.L7RulesPerEp {
+		if ep, ok := v.L7RulesPerEp[hash]; ok {
+			switch {
+			case len(l4Filter.L7RulesPerEp[hash].HTTP) > 0:
+				if len(v.L7RulesPerEp[hash].Kafka) > 0 {
+					ctx.PolicyTrace("   Merge conflict: mismatching L7 rule types.\n")
+					return 0, fmt.Errorf("Cannot merge conflicting L7 rule types")
+				}
+				ep.HTTP = append(ep.HTTP, newL7Rules.HTTP...)
+			case len(l4Filter.L7RulesPerEp[hash].Kafka) > 0:
+				if len(v.L7RulesPerEp[hash].HTTP) > 0 {
+					ctx.PolicyTrace("   Merge conflict: mismatching L7 rule types.\n")
+					return 0, fmt.Errorf("Cannot merge conflicting L7 rule types")
+				}
+				ep.Kafka = append(ep.Kafka, newL7Rules.Kafka...)
+			default:
+				ctx.PolicyTrace("   No L7 rules to merge.\n")
+			}
+		} else {
+			v.L7RulesPerEp[hash] = newL7Rules
+		}
+	}
+
 	resMap[key] = v
 	return 1, nil
 }
 
-func mergeL4(ctx *SearchContext, dir string, portRules []api.PortRule, resMap L4PolicyMap) (int, error) {
+func mergeL4(ctx *SearchContext, dir string, fromEndpoints []api.EndpointSelector, portRules []api.PortRule,
+	resMap L4PolicyMap) (int, error) {
+
 	found := 0
 	var err error
 
@@ -134,19 +179,19 @@ func mergeL4(ctx *SearchContext, dir string, portRules []api.PortRule, resMap L4
 		for _, p := range r.Ports {
 			var cnt int
 			if p.Protocol != "" {
-				cnt, err = mergeL4Port(ctx, r, p, dir, p.Protocol, resMap)
+				cnt, err = mergeL4Port(ctx, fromEndpoints, r, p, dir, p.Protocol, resMap)
 				if err != nil {
 					return found, err
 				}
 				found += cnt
 			} else {
-				cnt, err = mergeL4Port(ctx, r, p, dir, "tcp", resMap)
+				cnt, err = mergeL4Port(ctx, fromEndpoints, r, p, dir, "tcp", resMap)
 				if err != nil {
 					return found, err
 				}
 				found += cnt
 
-				cnt, err = mergeL4Port(ctx, r, p, dir, "udp", resMap)
+				cnt, err = mergeL4Port(ctx, fromEndpoints, r, p, dir, "udp", resMap)
 				if err != nil {
 					return found, err
 				}
@@ -170,7 +215,7 @@ func (r *rule) resolveL4Policy(ctx *SearchContext, state *traceState, result *L4
 
 	if !ctx.EgressL4Only {
 		for _, r := range r.Ingress {
-			cnt, err := mergeL4(ctx, "Ingress", r.ToPorts, result.Ingress)
+			cnt, err := mergeL4(ctx, "Ingress", r.FromEndpoints, r.ToPorts, result.Ingress)
 			if err != nil {
 				return nil, err
 			}
@@ -180,7 +225,7 @@ func (r *rule) resolveL4Policy(ctx *SearchContext, state *traceState, result *L4
 
 	if !ctx.IngressL4Only {
 		for _, r := range r.Egress {
-			cnt, err := mergeL4(ctx, "Egress", r.ToPorts, result.Egress)
+			cnt, err := mergeL4(ctx, "Egress", nil, r.ToPorts, result.Egress)
 			if err != nil {
 				return nil, err
 			}

commit 67f9195c757dce1cfc7ca3cb7930e6b3cb73c7cd

l4: Fix JSON marshalling of models.L4Policy
Since L7DataMap was introduced, the `cilium endpoint get` representation
of the L4Policy model was being returned as a JSON error, rather than
having a reasonable representation, ie:

    [json: unsupported type: policy.L7DataMap]

rather than

    [{
      "port": 8080,
      "protocol": "TCP"
    }]

This commit fixes it up.

Reported-by: André Martins <andre@cilium.io>
Fixes: 13e4f3983502 ("l4: Add L3 dependency to L4 policies")
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/policy/l4.go
@@ -33,6 +33,25 @@ var (
 // L7DataMap contains a map of L7 rules per endpoint where key is a hash of EndpointSelector
 type L7DataMap map[api.EndpointSelector]api.L7Rules
 
+func (l7 L7DataMap) MarshalJSON() ([]byte, error) {
+	var err error
+	rules := []byte{}
+
+	if len(l7) == 0 {
+		return []byte("{}"), nil
+	}
+
+	for _, v := range l7 {
+		b, err := json.MarshalIndent(v, "", "  ")
+		if err != nil {
+			b = []byte("\"L7DataMap error: " + err.Error() + "\"")
+		}
+		rules = append(rules, b...)
+	}
+
+	return rules, err
+}
+
 // L7ParserType is the type used to indicate what L7 parser to use and
 // defines all supported types of L7 parsers
 type L7ParserType string
@@ -154,7 +173,7 @@ func (l4 *L4Filter) IsRedirect() bool {
 func (l4 *L4Filter) MarshalIndent() string {
 	b, err := json.MarshalIndent(l4, "", "  ")
 	if err != nil {
-		return err.Error()
+		b = []byte("\"L4Filter error: " + err.Error() + "\"")
 	}
 	return string(b)
 }

pkg/policy/l4_test.go
@@ -16,8 +16,10 @@ package policy
 
 import (
 	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/comparator"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/kr/pretty"
 
 	. "gopkg.in/check.v1"
 )
@@ -115,4 +117,75 @@ func (s *PolicyTestSuite) TestCreateL4Filter(c *C) {
 			c.Assert(len(filter.L7RulesPerEp), Equals, 1)
 		}
 	}
+}
+
+func (s *PolicyTestSuite) TestJSONMarshal(c *C) {
+	selector := api.NewESFromLabels(labels.ParseSelectLabel("foo"))
+
+	policy := L4Policy{
+		Egress: L4PolicyMap{
+			"8080/TCP": {
+				Port:     8080,
+				Protocol: api.ProtoTCP,
+				Ingress:  false,
+			},
+		},
+		Ingress: L4PolicyMap{
+			"80/TCP": {
+				Port: 80, Protocol: api.ProtoTCP,
+				FromEndpoints: []api.EndpointSelector{selector},
+				L7Parser:      "http",
+				L7RulesPerEp: L7DataMap{
+					selector: api.L7Rules{
+						HTTP: []api.PortRuleHTTP{{Path: "/", Method: "GET"}},
+					},
+				},
+				Ingress: true,
+			},
+			"8080/TCP": {
+				Port: 8080, Protocol: api.ProtoTCP,
+				FromEndpoints: []api.EndpointSelector{selector},
+				L7Parser:      "http",
+				L7RulesPerEp: L7DataMap{
+					selector: api.L7Rules{
+						HTTP: []api.PortRuleHTTP{{Path: "/", Method: "GET"}},
+					},
+				},
+				Ingress: true,
+			},
+		},
+	}
+
+	model := policy.GetModel()
+	c.Assert(model, NotNil)
+
+	expected := `[{
+  "port": 8080,
+  "protocol": "TCP"
+}]`
+	c.Assert(pretty.Sprintf("%+ v", model.Egress), comparator.DeepEquals, expected)
+	expected = `[{
+  "port": 80,
+  "protocol": "TCP",
+  "l7-rules": {
+    "http": [
+      {
+        "path": "/",
+        "method": "GET"
+      }
+    ]
+  }
+} {
+  "port": 8080,
+  "protocol": "TCP",
+  "l7-rules": {
+    "http": [
+      {
+        "path": "/",
+        "method": "GET"
+      }
+    ]
+  }
+}]`
+	c.Assert(pretty.Sprintf("%+ v", model.Ingress), comparator.DeepEquals, expected)
 }