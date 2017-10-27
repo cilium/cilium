fcc3e8ad40c0d42c9768dd3190e2ef6dcd0e7d4d
Ian Vernon <ian@covalent.io>
2017-10-27 16:14:50 -0700

    daemon: parse CNP rules before checking equality
    An issue was observed where creation and deletion of CiliumNetworkPolicy objects
    kept occurring in the case where a CiliumNetworkPolicy with a Kafka rule was
    imported via Kubernetes. This kept occurring because when a CiliumNetworkPolicy
    object is updated in Kubernetes, calls to
    `daemon/k8s_watcher.go:updateCiliumNetworkPolicy` checked if the provided old /
    new CiliumNetworkPolicy objects were equal. The old and new objects were not
    equal due to the fact that parsing is performed upon CiliumNetworkPolicy
    pointers. The rules bieng pointed do are modified when they are parsed. The
    pointers to the rules are cached locally by the Kubernetes watcher. Thus the
    rules that are parsed into Kubernetes which are provided into
    `daemon/k8s_watcher.go:updateCiliumNetworkPolicy` upon future updates are the
    modified copies of the rules that were parsed previously. The provided old rule
    was modified due to being parsed, while the new rule was not. These modified
    copies were not truly equal, and thus did not cause
    `daemon/k8s_watcher.go:updateCiliumNetworkPolicy` to return:
    
    ```
    // Since we are updating the status map from all nodes we need to prevent
    // deletion and addition of all rules in cilium.
    if oldRule.SpecEquals(newRules) {
    	return
    }
    
    d.deleteCiliumNetworkPolicy(oldObj)
    d.addCiliumNetworkPolicy(newObj)
    ```
    
    and instead kept triggering deletions and updates of CiliumNetworkPolicies.
    This commit simply parses the rules before checking whether they are equal to
    normalize them. This is a workaround fix. The correct fix is to DeepCopy the
    old / new objects before they are modified, which #1885 covers.
    
    Backports: cda7d47f75ed15e0881232bbd603966b28470e9b
    Signed-off by: Ian Vernon <ian@cilium.io>

--- daemon/k8s_watcher.go
+++ daemon/k8s_watcher.go
@@ -997,6 +997,21 @@ func (d *Daemon) updateCiliumNetworkPolicy(oldObj interface{}, newObj interface{
 		log.Warningf("Received unknown object %+v, expected a CiliumNetworkPolicy object", newObj)
 		return
 	}
+
+	// Parse rules before checking whether they are equal. When rules are parsed,
+	// fields are sanitized and changed. Since Parse() is called on a pointer
+	// to a rule, the oldRule, which is cached locally, is modified, while the
+	// newRule is not modified yet. See GH-1885.
+	_, err := oldRule.Parse()
+	if err != nil {
+		log.WithError(err).Warn("Error parsing old CiliumNetworkPolicy rule")
+	}
+
+	_, err = newRules.Parse()
+	if err != nil {
+		log.WithError(err).Warn("Error parsing new CiliumNetworkPolicy rule")
+	}
+
 	// Since we are updating the status map from all nodes we need to prevent
 	// deletion and addition of all rules in cilium.
 	if oldRule.SpecEquals(newRules) {
