89524d177ac0aaa3f873ad0ab0f1a5f24cf7f474
Ian Vernon <ian@covalent.io>
2017-11-10 10:18:18 -0800

    test/helpers: add constants for use throughout tests
    Add a variety of constants for use in tests to significantly reduce the amount
    of hardcoding.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- test/helpers/cons.go
+++ test/helpers/cons.go
@@ -20,9 +20,102 @@ import (
 )
 
 var (
-	timeout     time.Duration = 300 //WithTimeout helper translate it to seconds
-	basePath                  = "/vagrant/"
-	networkName               = "cilium-net"
+	timeout time.Duration = 300
+
+	// K8s1VMName is the name of the Kubernetes master node when running
+	// Kubernetes tests.
+	K8s1VMName = fmt.Sprintf("k8s1-%s", GetCurrentK8SEnv())
+
+	// K8s2VMName is the name of the Kubernetes worker node when running
+	// Kubernetes tests.
+	K8s2VMName = fmt.Sprintf("k8s2-%s", GetCurrentK8SEnv())
+)
+
+const (
+	basePath = "/vagrant/"
+
+	// VM / Test suite constants.
+	K8s     = "k8s"
+	K8s1    = "k8s1"
+	K8s2    = "k8s2"
+	Runtime = "runtime"
+
+	Enabled  = "enabled"
+	Disabled = "disabled"
+	Total    = "total"
+
+	// PolicyEnforcement represents the PolicyEnforcement configuration option
+	// for the Cilium agent.
+	PolicyEnforcement = "PolicyEnforcement"
+
+	// PolicyEnforcementDefault represents the default PolicyEnforcement mode
+	// for Cilium.
+	PolicyEnforcementDefault = "default"
+
+	// PolicyEnforcementAlways represents the PolicyEnforcement mode
+	// for Cilium in which traffic is denied by default even when no policy
+	// is imported.
+	PolicyEnforcementAlways = "always"
+
+	// PolicyEnforcementNever represents the PolicyEnforcement mode
+	// for Cilium in which traffic is always allowed even if there is a policy
+	// selecting endpoints.
+	PolicyEnforcementNever = "never"
+
+	// Docker Image names
+
+	// CiliumDockerNetwork is the name of the Docker network which Cilium manages.
+	CiliumDockerNetwork = "cilium-net"
+
+	// NetperfImage is the Docker image used for performance testing
+	NetperfImage = "tgraf/netperf"
+
+	// HttpdImage is the image used for starting an HTTP server.
+	HttpdImage = "cilium/demo-httpd"
+
+	// Names of commonly used containers in tests.
+
+	Httpd1 = "httpd1"
+	Httpd2 = "httpd2"
+	Httpd3 = "httpd3"
+	App1   = "app1"
+	App2   = "app2"
+	App3   = "app3"
+	Client = "client"
+	Server = "server"
+	Host   = "host"
+
+	// Container lifecycle actions.
+	Create = "create"
+	Delete = "delete"
+
+	// IP Address families.
+	IPv4 = "IPv4"
+	IPv6 = "IPv6"
+
+	// Configuration options for endpoints. Copied from endpoint/endpoint.go
+	// TODO: these should be converted into types for use in configuration
+	// functions instead of using basic strings.
+
+	OptionAllowToHost         = "AllowToHost"
+	OptionAllowToWorld        = "AllowToWorld"
+	OptionConntrackAccounting = "ConntrackAccounting"
+	OptionConntrackLocal      = "ConntrackLocal"
+	OptionConntrack           = "Conntrack"
+	OptionDebug               = "Debug"
+	OptionDropNotify          = "DropNotification"
+	OptionTraceNotify         = "TraceNotification"
+	OptionNAT46               = "NAT46"
+	OptionPolicy              = "Policy"
+
+	OptionDisabled = "Disabled"
+	OptionEnabled  = "Enabled"
+
+	PingCount          = 5
+	CurlConnectTimeout = 5
+
+	DefaultNamespace    = "default"
+	KubeSystemNamespace = "kube-system"
 )
 
 //GetFilePath returns the absolute path of the provided filename
