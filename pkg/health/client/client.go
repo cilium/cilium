4fb43db94676735611d9cfa98f0f2e486d4d5a65
Joe Stringer <joe@covalent.io>
2017-11-15 15:54:09 -0800

    cilium-health: Add new health status agent
    cilium-health is a new client and daemon which allows users to probe the
    status of the local node, and also of the connectivity between the local
    node and remote nodes in the cluster.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

---
+++ pkg/health/client/client.go
@@ -0,0 +1,104 @@
+// Copyright 2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	clientapi "github.com/cilium/cilium/api/v1/health/client"
+	"github.com/cilium/cilium/pkg/health/defaults"
+
+	runtime_client "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+)
+
+// Client is a client for cilium health
+type Client struct {
+	clientapi.CiliumHealth
+}
+
+func configureTransport(tr *http.Transport, proto, addr string) *http.Transport {
+	if tr == nil {
+		tr = &http.Transport{}
+	}
+
+	if proto == "unix" {
+		// No need for compression in local communications.
+		tr.DisableCompression = true
+		tr.Dial = func(_, _ string) (net.Conn, error) {
+			return net.Dial(proto, addr)
+		}
+	} else {
+		tr.Proxy = http.ProxyFromEnvironment
+		tr.Dial = (&net.Dialer{}).Dial
+	}
+
+	return tr
+}
+
+// NewDefaultClient creates a client with default parameters connecting to UNIX domain socket.
+func NewDefaultClient() (*Client, error) {
+	return NewClient("")
+}
+
+// NewClient creates a client for the given `host`.
+func NewClient(host string) (*Client, error) {
+	if host == "" {
+		// Check if environment variable points to socket
+		e := os.Getenv(defaults.SockPathEnv)
+		if e == "" {
+			// If unset, fall back to default value
+			e = defaults.SockPath
+		}
+		host = "unix://" + e
+	}
+	tmp := strings.SplitN(host, "://", 2)
+	if len(tmp) != 2 {
+		return nil, fmt.Errorf("invalid host format '%s'", host)
+	}
+
+	switch tmp[0] {
+	case "tcp":
+		if _, err := url.Parse("tcp://" + tmp[1]); err != nil {
+			return nil, err
+		}
+		host = "http://" + tmp[1]
+	case "unix":
+		host = tmp[1]
+	}
+
+	transport := configureTransport(nil, tmp[0], host)
+	httpClient := &http.Client{Transport: transport}
+	clientTrans := runtime_client.NewWithClient(tmp[1], clientapi.DefaultBasePath,
+		clientapi.DefaultSchemes, httpClient)
+	return &Client{*clientapi.New(clientTrans, strfmt.Default)}, nil
+}
+
+// Hint tries to improve the error message displayed to the user.
+func Hint(err error) error {
+	if err == nil {
+		return err
+	}
+	e, _ := url.PathUnescape(err.Error())
+	if strings.Contains(err.Error(), defaults.SockPath) {
+		return fmt.Errorf("%s\nIs the agent running?", e)
+	}
+	return fmt.Errorf("%s", e)
+}
---
+++ pkg/health/server/health.go
@@ -0,0 +1,78 @@
+// Copyright 2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"fmt"
+	"strconv"
+
+	healthModels "github.com/cilium/cilium/api/v1/health/models"
+	. "github.com/cilium/cilium/api/v1/health/server/restapi"
+	ciliumModels "github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/apierror"
+	"github.com/cilium/cilium/pkg/client"
+
+	"github.com/c9s/goprocinfo/linux"
+	"github.com/go-openapi/runtime/middleware"
+)
+
+func dumpLoad() (*healthModels.LoadResponse, error) {
+	load, err := linux.ReadLoadAvg("/proc/loadavg")
+	if err != nil {
+		return nil, fmt.Errorf("Failure getting /stat: %s", err.Error())
+	}
+	return &healthModels.LoadResponse{
+		Last1min:  strconv.FormatFloat(load.Last1Min, 'f', 2, 64),
+		Last5min:  strconv.FormatFloat(load.Last5Min, 'f', 2, 64),
+		Last15min: strconv.FormatFloat(load.Last15Min, 'f', 2, 64),
+	}, nil
+}
+
+type getHealthz struct {
+	*Server
+}
+
+// NewGetHealthzHandler handles health requests.
+func NewGetHealthzHandler(s *Server) GetHealthzHandler {
+	return &getHealthz{Server: s}
+}
+
+func (h *getHealthz) getCiliumStatus() (*ciliumModels.StatusResponse, error) {
+	resp, err := h.Daemon.GetHealthz(nil)
+	if err != nil {
+		return nil, client.Hint(err)
+	}
+	return resp.Payload, nil
+}
+
+// Handle handles GET requests for /healthz .
+func (h *getHealthz) Handle(params GetHealthzParams) middleware.Responder {
+	log.Debug("Handling request for /healthz")
+
+	ciliumStatus, err := h.getCiliumStatus()
+	if err != nil {
+		return apierror.Error(GetHealthzFailedCode, err)
+	}
+	load, err := dumpLoad()
+	if err != nil {
+		return apierror.Error(GetHealthzFailedCode, err)
+	}
+
+	sr := healthModels.HealthResponse{}
+	sr.Cilium = ciliumStatus
+	sr.Uptime = h.Server.DumpUptime()
+	sr.SystemLoad = load
+	return NewGetHealthzOK().WithPayload(&sr)
+}
