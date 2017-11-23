f4e46efaa96ba87772bf6506b992796e4d81bdca
Alexander Alemayhu <alexander@alemayhu.com>
2017-11-23 12:38:58 +0100

    tests: perform validation on YAML examples
    This is just on a syntax level, _not_ a proper policy runtime test.
    Also making the script make sure that when a policy exist there should
    be a equivalent of the counter part. So if a JSON example is missing it
    fails and the same for YAML.
    
    To get the test passing added missing YAML files for the getting-started
    guide.
    
    Closes: #1845 (Validate example policies in Cilium documentation)
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- tests/00-check-examples.sh
+++ tests/00-check-examples.sh
@@ -11,8 +11,45 @@ redirect_debug_logs ${LOGS_DIR}
 
 set -ex
 
-for p in ../examples/policies/*.json ../examples/demo/*.json ; do
- if ! cilium policy validate $p; then
-  abort "$p is not valid"
- fi
+code=0
+
+function check_coverage() {
+  policy=$1
+  if [ ! -f $policy.json ]; then
+    log "Missing $policy.json"
+    code=1
+  fi
+  if [ ! -f $policy.yaml ]; then
+    log "Missing $policy.yaml"
+    code=1
+  fi
+}
+
+set +x
+
+log "Validating demos"
+for p in $dir/../examples/demo/*.json ; do
+  if ! cilium policy validate $p; then
+    log "$p is not valid"
+    code=1
+  fi
 done
+
+log "Validate JSON examples"
+for p in `find $dir/../examples/policies/ -name '*.json'`; do
+  if ! cilium policy validate $p; then
+    log "$p is not valid"
+    code=1
+  fi
+  check_coverage "${p%.*}"
+done
+
+log "Validate YAML examples"
+for p in `find $dir/../examples/policies/ -name '*.yaml'`; do
+  if ! yamllint -c $dir/yaml.config $p; then
+    log "$p is not valid"
+    code=1
+  fi
+done
+
+exit $code
