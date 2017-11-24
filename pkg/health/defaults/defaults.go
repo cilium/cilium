6ea245763b91339e2d55e34867e3990cc27e046c
Joe Stringer <joe@covalent.io>
2017-11-24 15:57:53 -0800

    cilium-health: Add support for HTTP ping to nodes
    Add support to the cilium-health client and server to host a new /hello
    API call over TCP socket 4240, and be able to make requests to it.
    
    Pinging this port on all known cluster nodes is done periodically, just
    like how regular ICMP ping polling is done.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- pkg/health/defaults/defaults.go
+++ pkg/health/defaults/defaults.go
@@ -24,4 +24,16 @@ const (
 
 	// SockPathEnv is the environment variable to overwrite SockPath
 	SockPathEnv = "CILIUM_HEALTH_SOCK"
+
+	// HTTPPathPort is used for probing base HTTP path connectivity
+	HTTPPathPort = 4240
+
+	// L7PathPort is used for probing L7 path connectivity
+	L7PathPort = 4241
+
+	// ServicePathPort is used for probing service redirect path connectivity
+	ServicePathPort = 4242
+
+	// ServiceL7PathPort is used for probing service redirect path connectivity with L7
+	ServiceL7PathPort = 4243
 )
