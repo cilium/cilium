c7a63484d9cc28a7e0fff8ffd836e4d233fb85ff
Joe Stringer <joe@covalent.io>
2017-11-27 12:45:33 -0800

    pkg: Refactor monitor launcher common code
    A generic launcher will be useful for an upcoming commit where the
    Cilium daemon also launches the cilium-health daemon.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

---
+++ pkg/launcher/launcher.go
@@ -0,0 +1,126 @@
+// Copyright 2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package launcher
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+var log = common.DefaultLogger
+
+// Launcher is used to wrap the node executable binary.
+type Launcher struct {
+	Mutex   lock.RWMutex
+	target  string
+	arg     string
+	process *os.Process
+	stdout  io.ReadCloser
+}
+
+// Run starts the daemon.
+func (launcher *Launcher) Run() {
+	targetName := launcher.GetTarget()
+	cmd := exec.Command(targetName, launcher.GetArg())
+	cmd.Stderr = os.Stderr
+	stdout, _ := cmd.StdoutPipe()
+	if err := cmd.Start(); err != nil {
+		cmdStr := fmt.Sprintf("%s %s", targetName, launcher.GetArg())
+		log.WithError(err).WithField("cmd", cmdStr).Error("cmd.Start()")
+	}
+
+	launcher.setProcess(cmd.Process)
+	launcher.setStdout(stdout)
+}
+
+// Restart stops the launcher which will trigger a rerun.
+func (launcher *Launcher) Restart(arg string) {
+	launcher.Mutex.Lock()
+	defer launcher.Mutex.Unlock()
+	launcher.arg = arg
+
+	if launcher.process == nil {
+		return
+	}
+	if err := launcher.process.Kill(); err != nil {
+		log.WithError(err).WithField("pid", launcher.process.Pid).Error("process.Kill()")
+	}
+	launcher.process = nil
+}
+
+// SetTarget sets the Launcher target.
+func (launcher *Launcher) SetTarget(target string) {
+	launcher.Mutex.Lock()
+	launcher.target = target
+	launcher.Mutex.Unlock()
+}
+
+// GetTarget returns the Launcher target.
+func (launcher *Launcher) GetTarget() string {
+	launcher.Mutex.RLock()
+	arg := launcher.target
+	launcher.Mutex.RUnlock()
+	return arg
+}
+
+// SetArg sets the Launcher arg.
+func (launcher *Launcher) SetArg(arg string) {
+	launcher.Mutex.Lock()
+	launcher.arg = arg
+	launcher.Mutex.Unlock()
+}
+
+// GetArg returns the Launcher arg.
+func (launcher *Launcher) GetArg() string {
+	launcher.Mutex.RLock()
+	arg := launcher.arg
+	launcher.Mutex.RUnlock()
+	return arg
+}
+
+// setProcess sets the internal process with the given process.
+func (launcher *Launcher) setProcess(proc *os.Process) {
+	launcher.Mutex.Lock()
+	launcher.process = proc
+	launcher.Mutex.Unlock()
+}
+
+// GetProcess returns the internal process.
+func (launcher *Launcher) GetProcess() *os.Process {
+	launcher.Mutex.RLock()
+	proc := launcher.process
+	launcher.Mutex.RUnlock()
+	return proc
+}
+
+// setStdout sets the stdout pipe.
+func (launcher *Launcher) setStdout(stdout io.ReadCloser) {
+	launcher.Mutex.Lock()
+	launcher.stdout = stdout
+	launcher.Mutex.Unlock()
+}
+
+// GetStdout gets the stdout pipe.
+func (launcher *Launcher) GetStdout() io.ReadCloser {
+	launcher.Mutex.RLock()
+	stdout := launcher.stdout
+	launcher.Mutex.RUnlock()
+	return stdout
+}
