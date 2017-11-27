1022893e3360c714857309fa430352f7cc6d9336
Joe Stringer <joe@covalent.io>
2017-11-27 12:45:33 -0800

    pkg: Refactor monitor launcher common code
    A generic launcher will be useful for an upcoming commit where the
    Cilium daemon also launches the cilium-health daemon.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- monitor/launch/launcher.go
+++ monitor/launch/launcher.go
@@ -17,42 +17,28 @@ package launch
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
-	"os"
-	"os/exec"
 
 	"github.com/cilium/cilium/api/v1/models"
-	"github.com/cilium/cilium/common"
-	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/launcher"
 )
-
-var log = common.DefaultLogger
 
 const targetName = "cilium-node-monitor"
 
 // NodeMonitor is used to wrap the node executable binary.
 type NodeMonitor struct {
-	mutex   lock.RWMutex
-	arg     string
-	process *os.Process
-	state   *models.MonitorStatus
+	launcher.Launcher
+
+	state *models.MonitorStatus
 }
 
 // Run starts the node monitor.
 func (nm *NodeMonitor) Run() {
+	nm.SetTarget(targetName)
 	for {
-		cmd := exec.Command(targetName, nm.GetArg())
-		cmd.Stderr = os.Stderr
-		stdout, _ := cmd.StdoutPipe()
-		if err := cmd.Start(); err != nil {
-			cmdStr := fmt.Sprintf("%s %s", targetName, nm.GetArg())
-			log.WithError(err).WithField("cmd", cmdStr).Error("cmd.Start()")
-		}
+		nm.Launcher.Run()
 
-		nm.setProcess(cmd.Process)
-
-		r := bufio.NewReader(stdout)
-		for nm.getProcess() != nil {
+		r := bufio.NewReader(nm.GetStdout())
+		for nm.GetProcess() != nil {
 			l, _ := r.ReadBytes('\n')
 			var tmp *models.MonitorStatus
 			if err := json.Unmarshal(l, &tmp); err != nil {
@@ -63,55 +49,17 @@ func (nm *NodeMonitor) Run() {
 	}
 }
 
-// Restart stops the node monitor which will trigger a rerun.
-func (nm *NodeMonitor) Restart(arg string) {
-	nm.mutex.Lock()
-	defer nm.mutex.Unlock()
-	nm.arg = arg
-
-	if nm.process == nil {
-		return
-	}
-	if err := nm.process.Kill(); err != nil {
-		log.WithError(err).WithField("pid", nm.process.Pid).Error("process.Kill()")
-	}
-	nm.process = nil
-}
-
 // State returns the monitor status.
 func (nm *NodeMonitor) State() *models.MonitorStatus {
-	nm.mutex.RLock()
+	nm.Mutex.RLock()
 	state := nm.state
-	nm.mutex.RUnlock()
+	nm.Mutex.RUnlock()
 	return state
 }
 
-// GetArg returns the NodeMonitor arg.
-func (nm *NodeMonitor) GetArg() string {
-	nm.mutex.RLock()
-	arg := nm.arg
-	nm.mutex.RUnlock()
-	return arg
-}
-
-// setProcess sets the internal node monitor process with the given process.
-func (nm *NodeMonitor) setProcess(proc *os.Process) {
-	nm.mutex.Lock()
-	nm.process = proc
-	nm.mutex.Unlock()
-}
-
-// getProcess returns the NodeMonitor internal process.
-func (nm *NodeMonitor) getProcess() *os.Process {
-	nm.mutex.RLock()
-	proc := nm.process
-	nm.mutex.RUnlock()
-	return proc
-}
-
-// setProcess sets the internal state monitor with the given state.
+// setState sets the internal state monitor with the given state.
 func (nm *NodeMonitor) setState(state *models.MonitorStatus) {
-	nm.mutex.Lock()
+	nm.Mutex.Lock()
 	nm.state = state
-	nm.mutex.Unlock()
+	nm.Mutex.Unlock()
 }
