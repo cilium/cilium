361ad2dc270e662a59dad225ed066ea2aaa0842c
André Martins <andre@cilium.io>
2017-11-29 00:33:51 +0100

    pkg/endpoint: RLock endpoint in ResolveIdentity
    Endpoint.Consumable is protected by this mutex and so this variable
    needs to be protected.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/endpoint/endpoint.go
+++ pkg/endpoint/endpoint.go
@@ -782,6 +782,8 @@ func (e *Endpoint) GetIdentity() policy.NumericIdentity {
 
 // ResolveIdentity fetches Consumable from consumable cache, using security identity as key.
 func (e *Endpoint) ResolveIdentity(srcIdentity policy.NumericIdentity) *policy.Identity {
+	e.Mutex.RLock()
+	defer e.Mutex.RUnlock()
 	return e.Consumable.ResolveIdentityFromCache(srcIdentity)
 }
 
