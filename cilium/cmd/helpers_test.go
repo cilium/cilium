93581df2caae7babf584f24bed4ce028f0fa5213
Joe Stringer <joe@covalent.io>
2017-12-04 15:49:02 -0800

    cmd: Test nested JSON parsing/decode
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- cilium/cmd/helpers_test.go
+++ cilium/cmd/helpers_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bytes"
 	"testing"
 
 	. "gopkg.in/check.v1"
@@ -33,4 +34,32 @@ func (s *CMDHelpersSuite) TestDumpJSON(c *C) {
 	if err == nil {
 		c.Fatalf("Dumpjson jsonpath no error with invalid path '%s'", err)
 	}
+}
+
+func (s *CMDHelpersSuite) TestExpandNestedJSON(c *C) {
+	buf := bytes.NewBufferString("not json at all")
+	_, err := expandNestedJSON(*buf)
+	c.Assert(err, IsNil)
+
+	buf = bytes.NewBufferString(`{\n\"escapedJson\": \"foo\"}`)
+	_, err = expandNestedJSON(*buf)
+	c.Assert(err, IsNil)
+
+	buf = bytes.NewBufferString(`nonjson={\n\"escapedJson\": \"foo\"}`)
+	_, err = expandNestedJSON(*buf)
+	c.Assert(err, IsNil)
+
+	buf = bytes.NewBufferString(`nonjson:morenonjson={\n\"escapedJson\": \"foo\"}`)
+	_, err = expandNestedJSON(*buf)
+	c.Assert(err, IsNil)
+
+	buf = bytes.NewBufferString(`{"foo": ["{\n  \"port\": 8080,\n  \"protocol\": \"TCP\"\n}"]}`)
+	_, err = expandNestedJSON(*buf)
+	c.Assert(err, IsNil)
+
+	buf = bytes.NewBufferString(`"foo": [
+  "bar:baz/alice={\"bob\":{\"charlie\":4}}\n"
+]`)
+	_, err = expandNestedJSON(*buf)
+	c.Assert(err, IsNil)
 }
