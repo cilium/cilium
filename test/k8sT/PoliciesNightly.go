661d16cf34510f0dd192f0b3993ae63e220f18b0
Eloy Coto <eloy.coto@gmail.com>
2017-12-05 10:44:45 +0100

    Ginkgo: Added PolicyGeneration on Nightly tests
    - Added a new policy generation tool under helpers/policygen folder.
    - Added exitCode on CMDRes
    - Added Kubectl.GetServiceHostPort method.
    - Added NightlyK8sPolicies test
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

---
+++ test/k8sT/PoliciesNightly.go
@@ -0,0 +1,78 @@
+// Copyright 2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package k8sTest
+
+import (
+	"fmt"
+
+	"github.com/cilium/cilium/test/helpers"
+	"github.com/cilium/cilium/test/helpers/policygen"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+)
+
+var _ = Describe("NightlyK8sPolicies", func() {
+
+	var kubectl *helpers.Kubectl
+	var logger *logrus.Entry
+	var initialized bool
+
+	ciliumPath := fmt.Sprintf("%s/cilium_ds.yaml", kubectl.ManifestsPath())
+
+	initialize := func() {
+		if initialized == true {
+			return
+		}
+
+		logger = log.WithFields(logrus.Fields{"testName": "NightlyK8sPolicies"})
+		logger.Info("Starting")
+
+		kubectl = helpers.CreateKubectl(helpers.K8s1VMName(), logger)
+		kubectl.Apply(ciliumPath)
+		_, err := kubectl.WaitforPods(helpers.KubeSystemNamespace, "-l k8s-app=cilium", 600)
+		Expect(err).Should(BeNil())
+		initialized = true
+	}
+
+	BeforeEach(func() {
+		initialize()
+	})
+
+	AfterEach(func() {
+		if CurrentGinkgoTestDescription().Failed {
+			ciliumPod, _ := kubectl.GetCiliumPodOnNode(helpers.KubeSystemNamespace, "k8s1")
+			kubectl.CiliumReport("kube-system", ciliumPod, []string{
+				"cilium policy get",
+				"cilium endpoint list",
+				"cilium service list"})
+		}
+	})
+
+	Context("PolicyEnforcement default", func() {
+		createTests := func() {
+			testSpecs := policygen.GeneratedTestSpec()
+			for _, test := range testSpecs {
+				func(testSpec policygen.TestSpec) {
+					It(fmt.Sprintf("%s", testSpec), func() {
+						testSpec.RunTest(kubectl)
+					})
+				}(test)
+			}
+		}
+		createTests()
+	})
+})
