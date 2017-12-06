97b32e27a59e6a1ac02cbd899735e1f644e7baa8
Joe Stringer <joe@covalent.io>
2017-12-06 15:33:05 -0800

    cmd: Add "--json" support to "bpf policy list"
    Signed-off-by: Joe Stringer <joestringernz@gmail.com>

--- cilium/cmd/helpers.go
+++ cilium/cmd/helpers.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cilium/cilium/pkg/policy"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"k8s.io/client-go/util/jsonpath"
 )
 
@@ -119,7 +120,7 @@ func OutputPrinter(data interface{}) error {
 func dumpJSON(data interface{}, jsonPath string) error {
 
 	if len(jsonPath) == 0 {
-		result, err := json.Marshal(data)
+		result, err := json.MarshalIndent(data, "", "  ")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Couldn't marshal to json: '%s'\n", err)
 			return err
@@ -142,6 +143,21 @@ func dumpJSON(data interface{}, jsonPath string) error {
 	}
 	fmt.Println(buf.String())
 	return nil
+}
+
+// handleJSON checks if the "--json" flag was specified, and formats the object
+// provided to the console (or fails if this cannot be done).
+//
+// Returns true if the output has been printed, false otherwise.
+func handleJSON(data interface{}) bool {
+	if viper.GetBool("json") {
+		if err := dumpJSON(data, ""); err != nil {
+			os.Exit(1)
+		} else {
+			return true
+		}
+	}
+	return false
 }
 
 // Search 'result' for strings with escaped JSON inside, and expand the JSON.
