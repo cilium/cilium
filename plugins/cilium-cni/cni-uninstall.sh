b5dd2382809286cfa9016a449ed0533644e2eb9d
Thomas Graf <thomas@cilium.io>
2017-12-14 20:01:22 +0100

    cni: Allow installation of cni plugin when binary is in use
    Reported-by: Jon Davies
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- plugins/cilium-cni/cni-uninstall.sh
+++ plugins/cilium-cni/cni-uninstall.sh
@@ -5,11 +5,13 @@ set -eu
 HOST_PREFIX=${HOST_PREFIX:-/host}
 CNI_CONF_NAME=${CNI_CONF_NAME:-10-cilium.conf}
 
+BIN_NAME=cilium-cni
 CNI_DIR=${CNI_DIR:-${HOST_PREFIX}/opt/cni}
 CILIUM_CNI_CONF=${CILIUM_CNI_CONF:-${HOST_PREFIX}/etc/cni/net.d/${CNI_CONF_NAME}}
 
 echo "Removing ${CNI_DIR}/bin/cilium-cni..."
-rm -f ${CNI_DIR}/bin/cilium-cni
+rm -f ${CNI_DIR}/bin/${BIN_NAME}
+rm -f ${CNI_DIR}/bin/${BIN_NAME}.old
 
 echo "Removing ${CILIUM_CNI_CONF} ..."
 rm -f ${CILIUM_CNI_CONF}
