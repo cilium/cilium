3c3e611e7ad445ce368abdba673429f7b5211b0a
Thomas Graf <thomas@cilium.io>
2017-12-14 20:01:22 +0100

    cni: Allow installation of cni plugin when binary is in use
    Reported-by: Jon Davies
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- plugins/cilium-cni/cni-install.sh
+++ plugins/cilium-cni/cni-install.sh
@@ -6,6 +6,7 @@ HOST_PREFIX=${HOST_PREFIX:-/host}
 CNI_CONF_NAME=${CNI_CONF_NAME:-10-cilium.conf}
 MTU=${MTU:-1450}
 
+BIN_NAME=cilium-cni
 CNI_DIR=${CNI_DIR:-${HOST_PREFIX}/opt/cni}
 CILIUM_CNI_CONF=${CILIUM_CNI_CONF:-${HOST_PREFIX}/etc/cni/net.d/${CNI_CONF_NAME}}
 
@@ -17,8 +18,16 @@ if [ ! -f ${CNI_DIR}/bin/loopback ]; then
 	cp /cni/loopback ${CNI_DIR}/bin/
 fi
 
-echo "Installing cilium-cni to ${CNI_DIR}/bin/ ..."
-cp /opt/cni/bin/cilium-cni ${CNI_DIR}/bin/
+echo "Installing ${BIN_NAME} to ${CNI_DIR}/bin/ ..."
+
+# Move an eventual old existing binary out of the way, we can't delete it
+# as it might be in use right now.
+if [ -f "${CNI_DIR}/bin/${BIN_NAME}" ]; then
+        rm -f ${CNI_DIR}/bin/${BIN_NAME}.old
+        mv ${CNI_DIR}/bin/${BIN_NAME} ${CNI_DIR}/bin/${BIN_NAME}.old
+fi
+
+cp /opt/cni/bin/${BIN_NAME} ${CNI_DIR}/bin/
 
 if [ -f "${CILIUM_CNI_CONF}" ]; then
 	echo "Using existing ${CILIUM_CNI_CONF}..."
