cilium/cilium

commit 945d84b35d03898fefbcf4dc9a51ac65e49711f9

Ginkgo: ensuring docker containers are removed on jenkins.
Add a `clean-ginkgo-tests` target on Makefile to remove the docker
containers in any situation.

Fix #2009

Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

ginkgo.Jenkinsfile
@@ -28,6 +28,9 @@ pipeline {
             steps {
                 sh "cd ${TESTDIR}; make tests-ginkgo"
             }
+            post {
+                sh "cd ${TESTDIR}; make clean-ginkgo-tests || true"
+            }
         }
         stage('BDD-Test') {
             environment {
@@ -53,7 +56,7 @@ pipeline {
                     junit 'test/*.xml'
                     // Temporary workaround to test cleanup
                     // rm -rf ${GOPATH}/src/github.com/cilium/cilium
-                    sh 'cd test/; ./post_build_agent.sh || true'                    
+                    sh 'cd test/; ./post_build_agent.sh || true'
                     sh 'cd test/; vagrant destroy -f'
                     sh 'cd test/; K8S_VERSION=1.6 vagrant destroy -f'
                 }

Makefile
@@ -29,6 +29,10 @@ tests-common-ginkgo: force
 	# Remove the networks
 	docker-compose -f test/docker-compose.yml -p $$JOB_BASE_NAME-$$BUILD_NUMBER down
 
+clean-ginkgo-tests:
+	docker-compose -f test/docker-compose.yml -p $$JOB_BASE_NAME-$$BUILD_NUMBER down
+	docker-compose -f test/docker-compose.yml -p $$JOB_BASE_NAME-$$BUILD_NUMBER rm
+
 tests-common: force
 	tests/00-fmt.sh
 	go vet $(GOFILES)

commit 0a9688be2b61d9ff6f53dc9704643940d8f8525f

Ginkgo: Fix issue with Jenkinsfile
Issue introduced by commit `945d84b35d03898fefbcf4dc9a51ac65e49711f9`

Current error message:

```
Obtained ginkgo.Jenkinsfile from git https://github.com/cilium/cilium.git
org.codehaus.groovy.control.MultipleCompilationErrorsException: startup failed:
WorkflowScript: 32: The post section can only contain build condition names with code blocks. Valid condition names are [always, changed, aborted, failure, success, unstable, notBuilt] @ line 32, column 17.
                   sh "cd ${TESTDIR}; make clean-ginkgo-tests || true"
                   ^

WorkflowScript: 31: post can not be empty @ line 31, column 13.
               post {
```

Build ID: https://jenkins.cilium.io/job/Ginkgo-CI-Tests-Pipeline/556/console

Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

ginkgo.Jenkinsfile
@@ -29,7 +29,9 @@ pipeline {
                 sh "cd ${TESTDIR}; make tests-ginkgo"
             }
             post {
-                sh "cd ${TESTDIR}; make clean-ginkgo-tests || true"
+                always {
+                    sh "cd ${TESTDIR}; make clean-ginkgo-tests || true"
+                }
             }
         }
         stage('BDD-Test') {
@@ -61,7 +63,6 @@ pipeline {
                     sh 'cd test/; K8S_VERSION=1.6 vagrant destroy -f'
                     sh 'cd test/; ./archive_test_results.sh || true'
                     archiveArtifacts artifacts: "test_results_${JOB_BASE_NAME}_${BUILD_NUMBER}.tar", allowEmptyArchive: true
-                    
                 }
             }
         }