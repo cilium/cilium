ea56d2efb0e44ee5a8d1ae3b2d8258c2e4d88111
Alexander Alemayhu <alexander@alemayhu.com>
2017-12-30 16:57:51 +0100

    Documentation: override the font and frame for parsed literal
    This makes the snippets using a variable URL look the same
    as regular snippets.
    
    Closes: #1784 (docs: make snippets / examples have a consistent look)
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -113,8 +113,7 @@ html_theme_path = ["_themes", ]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['images']
-
+html_static_path = ['images', '_static']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
@@ -177,3 +176,6 @@ http_strict_mode = False
 
 # Try as hard as possible to find references
 default_role = 'any'
+
+def setup(app):
+    app.add_stylesheet('parsed-literal.css')
