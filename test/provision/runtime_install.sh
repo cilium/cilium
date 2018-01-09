d24faaf26e8f33f7030d32be3a353ab55cb3630e
Ian Vernon <ian@cilium.io>
2018-01-09 11:20:30 -0800

    test/provision: add retry logic for download cmds
    Sometimes, commands to download resources fail. Add logic to retry so that
    provisioning does not fail immediately.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- test/provision/runtime_install.sh
+++ test/provision/runtime_install.sh
@@ -3,11 +3,17 @@ set -e
 
 HOST=$(hostname)
 PROVISIONSRC="/tmp/provision/"
+GOPATH=/go/
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source ${PROVISIONSRC}/helpers.bash
+
 
 $PROVISIONSRC/dns.sh
-sudo adduser vagrant docker
 
-export GOPATH=/go/
-go get -u github.com/jteeuwen/go-bindata/...
+sudo adduser vagrant docker
+retry_function "go get -u github.com/jteeuwen/go-bindata/..."
+
 ln -sf /go/bin/* /usr/local/bin/
 $PROVISIONSRC/compile.sh
