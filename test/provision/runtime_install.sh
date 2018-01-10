2d4d0ab256d8704b13ea9a40be4e0228774b7244
Ian Vernon <ian@cilium.io>
2018-01-10 15:18:06 -0800

    test/provision: address review comments
    * Remove trailing slash from PROVISIONSRC variable
    * Quote uses of PROVISIONSRC to avoid issues with spaces in path
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- test/provision/runtime_install.sh
+++ test/provision/runtime_install.sh
@@ -2,18 +2,18 @@
 set -e
 
 HOST=$(hostname)
-PROVISIONSRC="/tmp/provision/"
+PROVISIONSRC="/tmp/provision"
 GOPATH=/go/
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-source ${PROVISIONSRC}/helpers.bash
+source "${PROVISIONSRC}/helpers.bash"
 
 
-$PROVISIONSRC/dns.sh
+"${PROVISIONSRC}"/dns.sh
 
 sudo adduser vagrant docker
 retry_function "go get -u github.com/jteeuwen/go-bindata/..."
 
 ln -sf /go/bin/* /usr/local/bin/
-$PROVISIONSRC/compile.sh
+"${PROVISIONSRC}"/compile.sh
