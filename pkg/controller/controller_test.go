d3a59ef500357cac2fe02e7dbd7d398b61b2528c
Thomas Graf <thomas@cilium.io>
2018-01-15 05:28:00 +0100

    controller: New generic controller pattern for async operations
    Controller is a simple pattern that allows to perform the following
    tasks:
      - Run an operation in the background and retry until it succeeds
      - Perform a regular sync operation in the background
    
    A controller has cofigurable retry intervals and will collect statistics
    on number of successful runs, number of failures, last error message,
    and last error timestamp.
    
    Controllers have a name and are tied to a ControllerManager. The manager
    is typically bound to higher level objects such as endpoint. These
    higher level objects can then run multiple controllers to perform async
    tasks such as:
     - Annotating k8s resources with values
     - Synchronizing an object with the kvstore
     - Any other async operation to may fail and require retries
    
    Embedding the ControllerManager into higher level resources allows to
    bind controllers to the lifetime of that object. Controllers also have a
    UUID to allow correlating all log messages of a controller instance.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ pkg/controller/controller_test.go
@@ -0,0 +1,91 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controller
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type ControllerSuite struct{}
+
+var _ = Suite(&ControllerSuite{})
+
+func (b *ControllerSuite) TestUpdateRemoveController(c *C) {
+	mngr := Manager{}
+	mngr.UpdateController("test", ControllerParams{})
+	c.Assert(mngr.RemoveController("test"), IsNil)
+	c.Assert(mngr.RemoveController("not-exist"), Not(IsNil))
+}
+
+func (b *ControllerSuite) TestRemoveAll(c *C) {
+	mngr := Manager{}
+	// create
+	mngr.UpdateController("test1", ControllerParams{})
+	mngr.UpdateController("test2", ControllerParams{})
+	mngr.UpdateController("test3", ControllerParams{})
+	// update
+	mngr.UpdateController("test1", ControllerParams{})
+	mngr.UpdateController("test2", ControllerParams{})
+	mngr.UpdateController("test3", ControllerParams{})
+	mngr.RemoveAll()
+}
+
+type testObj struct {
+	cnt int
+}
+
+func (b *ControllerSuite) TestRunController(c *C) {
+	mngr := Manager{}
+	o := &testObj{}
+
+	ctrl := mngr.UpdateController("test", ControllerParams{
+		DoFunc: func() error {
+			// after two failed attempts, start succeeding
+			if o.cnt >= 2 {
+				return nil
+			}
+
+			o.cnt++
+			return fmt.Errorf("temporary error")
+		},
+		RunInterval:            time.Duration(1) * time.Millisecond, // short interval
+		ErrorRetryBaseDuration: time.Duration(1) * time.Millisecond, // short error retry
+	})
+
+	for n := 0; ctrl.GetSuccessCount() < 2; n++ {
+		if n > 100 {
+			c.Fatalf("time out while waiting for controller to succeed, last error: %s", ctrl.GetLastError())
+		}
+
+		time.Sleep(time.Duration(100) * time.Millisecond)
+	}
+
+	c.Assert(ctrl.GetSuccessCount(), Not(Equals), 0)
+	c.Assert(ctrl.GetFailureCount(), Equals, 2)
+
+	// last error is still stored
+	c.Assert(ctrl.GetLastError(), ErrorMatches, "temporary error")
+
+	c.Assert(mngr.RemoveController("test"), IsNil)
+}
