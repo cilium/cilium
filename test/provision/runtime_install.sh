34255812968d9c4d99d19db5133161675682bd48
Eloy Coto <eloy.coto@gmail.com>
2018-01-22 12:54:29 +0100

    Ginkgo: Added Envoy on ginkgo test
    - Added Envoy options on the PR tests
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- test/provision/runtime_install.sh
+++ test/provision/runtime_install.sh
@@ -9,11 +9,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source "${PROVISIONSRC}/helpers.bash"
 
+sudo adduser vagrant docker
 
 "${PROVISIONSRC}"/dns.sh
-
-sudo adduser vagrant docker
-retry_function "go get -u github.com/jteeuwen/go-bindata/..."
-
-ln -sf /go/bin/* /usr/local/bin/
 "${PROVISIONSRC}"/compile.sh
