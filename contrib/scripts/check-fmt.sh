ecb1890d84de952f096de5e784006ca567b55910
Joe Stringer <joe@covalent.io>
2018-01-25 15:01:17 -0800

    contrib: Remove debug output from checkers
    These checkers used to be part of the testsuite, but are now part of the
    regular build. They haven't failed in a long time and are unlikely to
    require debugging. Quieten the standard cilium build output by removing
    these unnecessary bash options.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- contrib/scripts/check-fmt.sh
+++ contrib/scripts/check-fmt.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env sh
 
-set -x
-
 diff="$(find . ! \( -path './contrib' -prune \) \
         ! \( -path './vendor' -prune \) \
         ! \( -path './.git' -prune \) \
         ! -samefile ./daemon/bindata.go \
         -type f -name '*.go' -print0 \
                 | xargs -0 gofmt -d -l -s )"
-set -e
 
 if [ -n "$diff" ]; then
 	echo "Unformatted Go source code:"
