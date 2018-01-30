95872dc1aa27e8ccd10579ef03ac96842290c0bf
Thomas Graf <thomas@cilium.io>
2018-01-29 18:37:00 -0800

    tests: Enable --debug-verbose flow in all CI runs
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- contrib/vagrant/start.sh
+++ contrib/vagrant/start.sh
@@ -303,7 +303,7 @@ function write_cilium_cfg() {
     ipv6_addr="${3}"
     filename="${4}"
 
-    cilium_options="--auto-ipv6-node-routes"
+    cilium_options="--auto-ipv6-node-routes --debug-verbose flow"
 
     if [[ "${IPV4}" -eq "1" ]]; then
         if [[ -z "${K8S}" ]]; then
