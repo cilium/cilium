41f8a6f92ae5edbe531d90eb22e68e6767d4fb30
Ian Vernon <ian@cilium.io>
2018-02-05 09:10:21 -0800

    pkg/endpoint: check label-based egress in endpoint regeneration
    Check label-based policy for egress when endpoint policy is
    being regenerated.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- pkg/endpoint/policy.go
+++ pkg/endpoint/policy.go
@@ -411,25 +411,54 @@ func (e *Endpoint) regenerateConsumable(owner Owner, labelsMap *identityPkg.Iden
 		}
 	}
 
-	ctx := policy.SearchContext{
+	ingressCtx := policy.SearchContext{
 		To: c.LabelArray,
 	}
-	if owner.TracingEnabled() {
-		ctx.Trace = policy.TRACE_ENABLED
+	egressCtx := policy.SearchContext{
+		From: c.LabelArray,
 	}
 
-	for srcID, srcLabels := range *labelsMap {
-		ctx.From = srcLabels
+	if owner.TracingEnabled() {
+		ingressCtx.Trace = policy.TRACE_ENABLED
+		egressCtx.Trace = policy.TRACE_ENABLED
+	}
+
+	// Only L3 (label-based) policy apply.
+	// Complexity increases linearly by the number of identities in the map.
+	for identity, labels := range *labelsMap {
+		ingressCtx.From = labels
+		egressCtx.To = labels
+
 		e.getLogger().WithFields(logrus.Fields{
-			logfields.PolicyID: srcID,
-			"ctx":              ctx,
-		}).Debug("Evaluating context for source PolicyID")
+			logfields.PolicyID: identity,
+			"ingress_context":  ingressCtx,
+		}).Debug("Evaluating ingress context for source PolicyID")
 
-
-		if repo.AllowsIngressLabelAccess(&ctx) == api.Allowed {
-			if e.allowIngressIdentity(owner, srcID) {
+		ingressAccess := repo.AllowsIngressLabelAccess(&ingressCtx)
+		if ingressAccess == api.Allowed {
+			if e.allowIngressIdentity(owner, identity) {
 				changed = true
 			}
+		}
+
+		e.getLogger().WithFields(logrus.Fields{
+			logfields.PolicyID: identity,
+			"egress_context":   egressCtx,
+		}).Debug("Evaluating egress context for source PolicyID")
+
+		egressAccess := repo.AllowsEgressLabelAccess(&egressCtx)
+
+		log.WithFields(logrus.Fields{
+			logfields.PolicyID:   identity,
+			logfields.EndpointID: e.ID,
+			"labels":             labels,
+		}).Debugf("egress verdict: %v", egressAccess)
+
+		// TODO (ianvernon) plumb egress verdict into endpoint structure
+		if egressAccess == api.Allowed {
+			e.getLogger().WithFields(logrus.Fields{
+				logfields.PolicyID: identity,
+				"ctx":              ingressCtx}).Debug("egress allowed")
 		}
 	}
 
