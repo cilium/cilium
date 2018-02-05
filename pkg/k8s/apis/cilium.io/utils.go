8ef8745854d2ea1a4417625b06e7b429db67d9a6
Ian Vernon <ian@cilium.io>
2018-02-05 12:05:41 -0800

    pkg/k8s/apis/cilium.io: update ParseToCiliumRule to account for egress
    Signed-off by: Ian Vernon <ian@cilium.io>

--- pkg/k8s/apis/cilium.io/utils.go
+++ pkg/k8s/apis/cilium.io/utils.go
@@ -57,34 +57,10 @@ func GetPolicyLabels(ns, name string) labels.LabelArray {
 	)
 }
 
-// ParseToCiliumRule returns an api.Rule with all the labels parsed into cilium
-// labels.
-func ParseToCiliumRule(namespace, name string, r *api.Rule) *api.Rule {
-	retRule := &api.Rule{}
-	if r.EndpointSelector.LabelSelector != nil {
-		retRule.EndpointSelector = api.NewESFromK8sLabelSelector("", r.EndpointSelector.LabelSelector)
-		// The PodSelector should only reflect to the same namespace
-		// the policy is being stored, thus we add the namespace to
-		// the MatchLabels map.
-		if retRule.EndpointSelector.LabelSelector.MatchLabels == nil {
-			retRule.EndpointSelector.LabelSelector.MatchLabels = map[string]string{}
-		}
-
-		userNamespace, ok := retRule.EndpointSelector.LabelSelector.MatchLabels[podPrefixLbl]
-		if ok && userNamespace != namespace {
-			log.WithFields(logrus.Fields{
-				logfields.K8sNamespace:              namespace,
-				logfields.CiliumNetworkPolicyName:   name,
-				logfields.K8sNamespace + ".illegal": userNamespace,
-			}).Warn("CiliumNetworkPolicy contains illegal namespace match in EndpointSelector." +
-				" EndpointSelector always applies in namespace of the policy resource, removing illegal namespace match'.")
-		}
-		retRule.EndpointSelector.LabelSelector.MatchLabels[podPrefixLbl] = namespace
-	}
-
-	if r.Ingress != nil {
-		retRule.Ingress = make([]api.IngressRule, len(r.Ingress))
-		for i, ing := range r.Ingress {
+func parseToCiliumIngressRule(namespace string, inRule, retRule *api.Rule) {
+	if inRule.Ingress != nil {
+		retRule.Ingress = make([]api.IngressRule, len(inRule.Ingress))
+		for i, ing := range inRule.Ingress {
 			if ing.FromEndpoints != nil {
 				retRule.Ingress[i].FromEndpoints = make([]api.EndpointSelector, len(ing.FromEndpoints))
 				for j, ep := range ing.FromEndpoints {
@@ -137,11 +113,98 @@ func ParseToCiliumRule(namespace, name string, r *api.Rule) *api.Rule {
 			}
 		}
 	}
+}
 
-	if r.Egress != nil {
-		retRule.Egress = make([]api.EgressRule, len(r.Egress))
-		copy(retRule.Egress, r.Egress)
+func parseToCiliumEgressRule(namespace string, inRule, retRule *api.Rule) {
+	if inRule.Egress != nil {
+		retRule.Egress = make([]api.EgressRule, len(inRule.Egress))
+
+		for i, egr := range inRule.Egress {
+			if egr.ToEndpoints != nil {
+				retRule.Egress[i].ToEndpoints = make([]api.EndpointSelector, len(egr.ToEndpoints))
+				for j, ep := range egr.ToEndpoints {
+					retRule.Egress[i].ToEndpoints[j] = api.NewESFromK8sLabelSelector("", ep.LabelSelector)
+					if retRule.Egress[i].ToEndpoints[j].MatchLabels == nil {
+						retRule.Egress[i].ToEndpoints[j].MatchLabels = map[string]string{}
+					}
+					// There's no need to add K8s prefix for reserved labels
+					if retRule.Egress[i].ToEndpoints[j].HasKeyPrefix(labels.LabelSourceReservedKeyPrefix) {
+						continue
+					}
+					// The user can explicitly specify the namespace in the
+					// ToEndpoints selector. If omitted, we limit the
+					// scope to the namespace the policy lives in.
+					if !retRule.Egress[i].ToEndpoints[j].HasKey(podPrefixLbl) {
+						retRule.Egress[i].ToEndpoints[j].MatchLabels[podPrefixLbl] = namespace
+					}
+				}
+			}
+
+			if egr.ToPorts != nil {
+				retRule.Egress[i].ToPorts = make([]api.PortRule, len(egr.ToPorts))
+				copy(retRule.Egress[i].ToPorts, egr.ToPorts)
+			}
+			if egr.ToCIDR != nil {
+				retRule.Egress[i].ToCIDR = make([]api.CIDR, len(egr.ToCIDR))
+				copy(retRule.Egress[i].ToCIDR, egr.ToCIDR)
+			}
+
+			if egr.ToCIDRSet != nil {
+				retRule.Egress[i].ToCIDRSet = make([]api.CIDRRule, len(egr.ToCIDRSet))
+				copy(retRule.Egress[i].ToCIDRSet, egr.ToCIDRSet)
+			}
+
+			if egr.ToRequires != nil {
+				retRule.Egress[i].ToRequires = make([]api.EndpointSelector, len(egr.ToRequires))
+				for j, ep := range egr.ToRequires {
+					retRule.Egress[i].ToRequires[j] = api.NewESFromK8sLabelSelector("", ep.LabelSelector)
+					if retRule.Egress[i].ToRequires[j].MatchLabels == nil {
+						retRule.Egress[i].ToRequires[j].MatchLabels = map[string]string{}
+					}
+					// The user can explicitly specify the namespace in the
+					// ToEndpoints selector. If omitted, we limit the
+					// scope to the namespace the policy lives in.
+					if _, ok := retRule.Egress[i].ToRequires[j].MatchLabels[podPrefixLbl]; !ok {
+						retRule.Egress[i].ToRequires[j].MatchLabels[podPrefixLbl] = namespace
+					}
+				}
+			}
+
+			if egr.ToServices != nil {
+				retRule.Egress[i].ToServices = make([]api.Service, len(egr.ToServices))
+				copy(retRule.Egress[i].ToServices, egr.ToServices)
+			}
+		}
 	}
+}
+
+// ParseToCiliumRule returns an api.Rule with all the labels parsed into cilium
+// labels.
+func ParseToCiliumRule(namespace, name string, r *api.Rule) *api.Rule {
+	retRule := &api.Rule{}
+	if r.EndpointSelector.LabelSelector != nil {
+		retRule.EndpointSelector = api.NewESFromK8sLabelSelector("", r.EndpointSelector.LabelSelector)
+		// The PodSelector should only reflect to the same namespace
+		// the policy is being stored, thus we add the namespace to
+		// the MatchLabels map.
+		if retRule.EndpointSelector.LabelSelector.MatchLabels == nil {
+			retRule.EndpointSelector.LabelSelector.MatchLabels = map[string]string{}
+		}
+
+		userNamespace, ok := retRule.EndpointSelector.LabelSelector.MatchLabels[podPrefixLbl]
+		if ok && userNamespace != namespace {
+			log.WithFields(logrus.Fields{
+				logfields.K8sNamespace:              namespace,
+				logfields.CiliumNetworkPolicyName:   name,
+				logfields.K8sNamespace + ".illegal": userNamespace,
+			}).Warn("CiliumNetworkPolicy contains illegal namespace match in EndpointSelector." +
+				" EndpointSelector always applies in namespace of the policy resource, removing illegal namespace match'.")
+		}
+		retRule.EndpointSelector.LabelSelector.MatchLabels[podPrefixLbl] = namespace
+	}
+
+	parseToCiliumIngressRule(namespace, r, retRule)
+	parseToCiliumEgressRule(namespace, r, retRule)
 
 	policyLbls := GetPolicyLabels(namespace, name)
 	if retRule.Labels == nil {
