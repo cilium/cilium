1da84f18f98c2a25e6feaeb0c63ae56abbf1453c
Ian Vernon <ian@cilium.io>
2018-02-05 16:10:23 -0800

    tests/k8s/tests: deprecate GSG test
    Now that tests/k8sT/Policies.go has been marked as validated, deprecate
    corresponding bash test.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- tests/k8s/tests/00-gsg-test.sh
+++ tests/k8s/tests/00-gsg-test.sh
@@ -17,6 +17,9 @@ redirect_debug_logs ${LOGS_DIR}
 
 set -ex
 
+log "${TEST_NAME} has been deprecated and replaced by test/k8sT/Policies.go:checks all kind of kubernetes policies"
+exit 0
+
 NAMESPACE="kube-system"
 GOPATH="/home/vagrant/go"
 DENIED="Final verdict: DENIED"
