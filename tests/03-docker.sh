db49090eb8ba216fafc7cee3681fddb90bfc115b
Ian Vernon <ian@cilium.io>
2018-02-09 13:57:04 -0800

    tests: deprecate 03-docker.sh
    Signed-off by: Ian Vernon <ian@cilium.io>

--- tests/03-docker.sh
+++ tests/03-docker.sh
@@ -11,6 +11,9 @@ redirect_debug_logs ${LOGS_DIR}
 
 set -ex
 
+log "${TEST_NAME} has been deprecated and replaced by test/runtime/connectivity.go: Test connectivity between containers with / without policy imported"
+exit 0
+
 NETPERF_IMAGE="tgraf/netperf"
 
 function cleanup {
