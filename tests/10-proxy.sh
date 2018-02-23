cc79dad63649b8c7baebc9bc5621fae1b3604619
Ian Vernon <ian@cilium.io>
2018-02-22 17:51:50 -0800

    tests: deprecate 10-proxy.sh
    Signed-off by: Ian Vernon <ian@cilium.io>

--- tests/10-proxy.sh
+++ tests/10-proxy.sh
@@ -11,6 +11,9 @@ redirect_debug_logs ${LOGS_DIR}
 
 set -ex
 
+log "${TEST_NAME} has been deprecated and replaced by test/runtime/lb.go:Services Policies"
+exit 0
+
 function cleanup {
   monitor_stop
   cilium service delete --all 2> /dev/null || true
