274646ff476ec4a505e49a5444bf7e7aa1ce3610
Ian Vernon <ian@cilium.io>
2018-02-23 14:35:59 -0800

    contrib/scripts: move newline in log checker script
    This puts it with the gofmt checker script, cmdref checker script, etc. so all
    checker scripts are in the same directory.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

---
+++ contrib/scripts/no-newlines-in-log-call.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+if grep --include \*.go -r 'log\.' ../ | grep -v vendor \
+  | grep -v envoy \
+  | grep -v contrib \
+  | grep -v logging.go \
+  | grep -F "\n"; then
+  echo "found newline(s) in log call(s), please remove ending \n"
+  exit 1
+fi
