5e8b0d07d86b9e36671a6907a13e85fe6d8b4298
Ian Vernon <ian@cilium.io>
2018-02-22 21:44:10 -0800

    test/helpers: add "-W 2" to ping wrapper functions
    Signed-off by: Ian Vernon <ian@cilium.io>

--- test/helpers/wrappers.go
+++ test/helpers/wrappers.go
@@ -32,8 +32,7 @@ const (
 // Ping returns the string representing the ping command to ping the specified
 // endpoint.
 func Ping(endpoint string) string {
-	// TODO: add W -2 ?
-	return fmt.Sprintf("ping -c %d %s", PingCount, endpoint)
+	return fmt.Sprintf("ping -W 2 -c %d %s", PingCount, endpoint)
 }
 
 // Ping6 returns the string representing the ping6 command to ping6 the
