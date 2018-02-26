3dd2e9c61f5a00b3f8412c81604086e5262ae650
Thomas Graf <thomas@cilium.io>
2018-02-26 15:43:25 -0800

    contrib: Make k8s-monitor.sh more generic
    Allow executing any command inside all cilium pods
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- contrib/k8s/k8s-monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-trap 'kill $(jobs -p)' EXIT
-
-while read -r p; do
-	kubectl -n kube-system exec -ti $p -- cilium monitor $*&
-done <<< "$(kubectl -n kube-system get pods -l k8s-app=cilium | awk '{print $1}' | grep cilium)"
-
-wait
