960316e239cbaac889879f3eda58ebe844a09570
Ian Vernon <ian@cilium.io>
2018-02-26 13:27:09 -0800

    tests: remove bash tests and associated scripts
    * Also move helpers.bash and 98-documentation-link-check to `contrib/scripts`
    directory.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- tests/k8s/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-cd "${dir}"
-VAGRANT_DEFAULT_PROVIDER=virtualbox vagrant provision
-make k8s-multi-node-tests
--- tests/k8s/tests/ipv6/ipv6.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-set -ex
-
-source "${dir}/../../helpers.bash"
-
