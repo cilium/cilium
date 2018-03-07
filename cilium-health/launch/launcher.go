295681df1d0d3b30b6524051a79e23f8ba78cd21
Joe Stringer <joe@covalent.io>
2018-03-07 11:33:37 -0800

    cilium-health: Serve API over HTTP by default
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- cilium-health/launch/launcher.go
+++ cilium-health/launch/launcher.go
@@ -45,7 +45,7 @@ const targetName = "cilium-health"
 // Run launches the cilium-health daemon.
 func (ch *CiliumHealth) Run() {
 	ch.SetTarget(targetName)
-	ch.SetArgs([]string{"-d"})
+	ch.SetArgs([]string{"-d", "--admin=any"})
 
 	// Wait until Cilium API is available
 	for {
