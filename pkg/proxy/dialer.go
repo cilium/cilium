0ab3b55e8d1ea1bab93e1ebfd3225b8eb12fa6a0
Joe Stringer <joe@covalent.io>
2018-03-17 17:00:01 -0700

    proxy: Add comment to reference proxy entry timer
    Refactor the TCP keep-alive setting to a variable and add a comment to
    reference it against the datapath. This value should always be strictly
    less than the PROXY_DEFAULT_LIFETIME.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- pkg/proxy/dialer.go
+++ pkg/proxy/dialer.go
@@ -22,13 +22,17 @@ import (
 	"time"
 )
 
+// ProxyKeepAlivePeriod is the time used for sending periodic keepalives on
+// proxy connections. Cross-reference with datapath PROXY_DEFAULT_LIFETIME.
+const ProxyKeepAlivePeriod = time.Duration(5) * time.Minute
+
 func setKeepAlive(c net.Conn) error {
 	if tcp, ok := c.(*net.TCPConn); ok {
 		if err := tcp.SetKeepAlive(true); err != nil {
 			return fmt.Errorf("unable to enable keepalive: %s", err)
 		}
 
-		if err := tcp.SetKeepAlivePeriod(time.Duration(5) * time.Minute); err != nil {
+		if err := tcp.SetKeepAlivePeriod(ProxyKeepAlivePeriod); err != nil {
 			return fmt.Errorf("unable to set keepalive period: %s", err)
 		}
 	}
