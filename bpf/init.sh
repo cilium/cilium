f15487c6b0cdc11656841250f246cd1a6c4f2bd8
Joe Stringer <joe@covalent.io>
2018-03-19 17:35:12 -0700

    bpf: Set cilium_host device MTU to 1450
    We observed an issue where requests to external services would often
    stall or fail from pods inside a minikube 0.24/0.25 environment (Linux
    4.9.x). As soon as an outgoing request attempted to fetch data beyond
    the MTU of the endpoint veth device MTU, the command would stall. This
    was tested by running a curl to external service such as google. Running
    `cilium monitor` showed large responses being redirected into the veth
    device, however the packets were not observed on the device within the
    pod making the request (observing via tcpdump). Upon closer inspection,
    the container device MTU was being set to 1450 however the devices on
    the cilium node used for connectivity to the outside world had MTU 1500.
    Lowering the cilium_host device MTU fixed the issue.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- bpf/init.sh
+++ bpf/init.sh
@@ -244,6 +244,9 @@ setup_veth_pair $HOST_DEV1 $HOST_DEV2
 ip link set $HOST_DEV1 arp off
 ip link set $HOST_DEV2 arp off
 
+ip link set $HOST_DEV1 mtu 1450
+ip link set $HOST_DEV2 mtu 1450
+
 sed -i '/^#.*CILIUM_NET_MAC.*$/d' $RUNDIR/globals/node_config.h
 CILIUM_NET_MAC=$(ip link show $HOST_DEV2 | grep ether | awk '{print $2}')
 CILIUM_NET_MAC=$(mac2array $CILIUM_NET_MAC)
