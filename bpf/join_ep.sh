cilium/cilium

commit 850615cc41acc6b3d643dbf0f5529911a5d18b06

bpf: Move calls map to temporary location and remove after filter replace
Removing a BPF map cause it to lose all state immediately. Current code is
removing the tail call map just before replacing the filter which could lead to
a short race window where tail calls would fail in the old program just before
it is replaced with the new program.

Fixes: 95a2c8aea ("bpf: Remove old calls map when inserting endpoint programs")

Signed-off-by: Joe Stringer <joe@covalent.io>
Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/join_ep.sh
@@ -58,9 +58,15 @@ fi
 
 bpf_compile bpf_lxc.c bpf_lxc.o obj
 
-# Unlink the old calls map so we can upgrade it
-CALLS_MAP=cilium_calls_$EPID
-rm "/sys/fs/bpf/tc/globals/$CALLS_MAP" 2> /dev/null || true
-
 tc qdisc replace dev $IFNAME clsact || true
-tc filter replace dev $IFNAME ingress prio 1 handle 1 bpf da obj $EPDIR/bpf_lxc.o sec from-container
+
+# Move the old calls map to a temporary location so it can be removed after the
+# program has been replaced
+CALLS_MAP="/sys/fs/bpf/tc/globals/cilium_calls_${EPID}"
+mv "$CALLS_MAP" "${CALLS_MAP}.old" 2> /dev/null || true
+tc filter replace dev $IFNAME ingress prio 1 handle 1 bpf da obj $EPDIR/bpf_lxc.o sec from-container || {
+	mv "${CALLS_MAP}.old" "${CALLS_MAP}"
+	exit 1
+}
+
+rm "${CALLS_MAP}.old" 2> /dev/null || true

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=22f6870d19de9643645c6b2b4a1409ca817d081d
+GO_BINDATA_SHA1SUM=5262dd9e320936e6a4d463d59fb80fd563048b77
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/geneve.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

commit ff06ac8fe135044730661882516bf50b85073061

bpf: actually move calls map to temporary location and remove it later
Commit 850615cc41ac ("bpf: Move calls map to temporary location and
remove after filter replace") is actually buggy and doesn't work since
in BPF fs dentry lookups on files with '.' are reserved names and will
return 'Permission Denied'. So the mv + rm combination never worked
and we always hit the || true case.

The 'mv "${CALLS_MAP}.old" "${CALLS_MAP}"' noticeably failed for Adrian
when the lxc device from the EP was gone. Fix it by not using a reserved
name. Reported via Slack.

Fixes: 850615cc41ac ("bpf: Move calls map to temporary location and remove after filter replace")
Reported-by: Adrian Wawrzak <adiq94@gmail.com>
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/join_ep.sh
@@ -63,10 +63,10 @@ tc qdisc replace dev $IFNAME clsact || true
 # Move the old calls map to a temporary location so it can be removed after the
 # program has been replaced
 CALLS_MAP="/sys/fs/bpf/tc/globals/cilium_calls_${EPID}"
-mv "$CALLS_MAP" "${CALLS_MAP}.old" 2> /dev/null || true
+mv "$CALLS_MAP" "${CALLS_MAP}:old" 2> /dev/null || true
 tc filter replace dev $IFNAME ingress prio 1 handle 1 bpf da obj $EPDIR/bpf_lxc.o sec from-container || {
-	mv "${CALLS_MAP}.old" "${CALLS_MAP}"
+	mv "${CALLS_MAP}:old" "${CALLS_MAP}"
 	exit 1
 }
 
-rm "${CALLS_MAP}.old" 2> /dev/null || true
+rm "${CALLS_MAP}:old" 2> /dev/null || true

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=ee2b196e5dfc0ade39bf813ebf613ac13aa7aa6d
+GO_BINDATA_SHA1SUM=5405a4b3b5d90e603025a036d6f670637ad6ceb9
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/geneve.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 