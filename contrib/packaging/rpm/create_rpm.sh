df40e26a9acefa1df04f1afafc0f2f806b0d5745
Tony Lambiris <tony@criticalstack.com>
2018-03-29 07:17:31 -0400

    Update rpm packaging files

--- contrib/packaging/rpm/create_rpm.sh
+++ contrib/packaging/rpm/create_rpm.sh
@@ -6,6 +6,9 @@ set -x
 source /opt/cilium/env
 
 envsubst < /opt/cilium/cilium.spec.envsubst > /opt/cilium/cilium.spec
-fedpkg --release f26 local
+# Remove dash (in case of a version like 1.0.0-rc8)
+sed -i -re '/^Version/s/-//g' /opt/cilium/cilium.spec
+
+fedpkg --release f27 local
 
 find /opt/cilium -type f -name 'cilium*.rpm' -exec mv -f "{}" /output/ \;
