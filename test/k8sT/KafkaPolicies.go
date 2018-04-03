0ed2b33dcc847ad018d050b91b7c7ecf4bcccfa8
Romain Lenglet <romain@covalent.io>
2018-04-02 17:28:16 -0700

    test: Re-enable Kafka tests
    Fixes: #3472
    
    Signed-off-by: Romain Lenglet <romain@covalent.io>

--- test/k8sT/KafkaPolicies.go
+++ test/k8sT/KafkaPolicies.go
@@ -115,9 +115,6 @@ var _ = Describe("K8sValidatedKafkaPolicyTest", func() {
 	})
 
 	It("KafkaPolicies", func() {
-		// GH-3440 Re-enable when stable
-		return
-
 		clusterIP, err := kubectl.Get(helpers.DefaultNamespace, "svc").Filter(
 			"{.items[?(@.metadata.name == \"kafka-service\")].spec.clusterIP}")
 		logger.Infof("KafkaPolicyRulesTest: cluster service ip '%s'", clusterIP)
