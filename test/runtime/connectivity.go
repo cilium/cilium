ec58423fb4112476943b9aa10ff8923045010cd8
Manali Bhutiyani <manali@covalent.io>
2018-04-09 12:53:30 -0700

    CI: Temporarily add retry 3 times logic in connectivity.go Fixes: #3596 Related to: #3393 Related to: #3595 Related to: #3558
    Signed-off-By: Manali Bhutiyani <manali@covalent.io>

--- test/runtime/connectivity.go
+++ test/runtime/connectivity.go
@@ -292,6 +292,7 @@ var _ = Describe("RuntimeValidatedConntrackTest", func() {
 
 	var curl1ContainerName = "curl"
 	var curl2ContainerName = "curl2"
+	var maxTestTrials = 3
 	var CTPolicyConntrackLocalDisabled = "ct-test-policy-conntrack-local-disabled.json"
 
 	type conntestCases struct {
@@ -436,11 +437,21 @@ var _ = Describe("RuntimeValidatedConntrackTest", func() {
 			},
 		}
 
+		// This retry logic of maxTestTrials per test case is temporary till
+		// GH #3393 issue to support egress CT cleanup is in place.
 		for _, test := range testCases {
 			By(fmt.Sprintf("Container %q test connectivity to %q", test.from, test.destination))
-			res = vm.ContainerExec(test.from, test.to)
-			ExpectWithOffset(1, res.WasSuccessful()).To(test.assert(),
-				"The result of %q from container %q to %s does not match", test.to, test.from, test.destination)
+			for testTryCount := 0; testTryCount <= maxTestTrials; testTryCount++ {
+				res = vm.ContainerExec(test.from, test.to)
+				if res.WasSuccessful() == false {
+					if testTryCount == maxTestTrials {
+						ExpectWithOffset(1, res.WasSuccessful()).To(test.assert(),
+							"The result of %q from container %q to %s does not match", test.to, test.from, test.destination)
+					}
+				} else {
+					break // break from inner loop and try next test-case.
+				}
+			}
 		}
 
 		By("Testing bidirectional connectivity from client to server")
