2a75e480996c1d1bea3daa4d66bacc7469dfc184
Ian Vernon <ian@cilium.io>
2018-04-11 21:03:39 -0700

    pkg/endpoint: move RunK8sCiliumEndpointSyncGC to k8s.go
    Signed-off by: Ian Vernon <ian@cilium.io>

--- pkg/endpoint/k8s.go
+++ pkg/endpoint/k8s.go
@@ -20,7 +20,16 @@ import (
 	cilium_client_v2 "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/typed/cilium.io/v2"
 	"k8s.io/client-go/rest"
 
+	"fmt"
+	"github.com/cilium/cilium/pkg/controller"
 	clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/node"
+	"github.com/sirupsen/logrus"
+	"math/rand"
+	"time"
+
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // getCiliumClient builds and returns a k8s auto-generated client for cilium
@@ -58,4 +67,110 @@ func getCiliumClient() (ciliumClient cilium_client_v2.CiliumV2Interface, err err
 	}
 
 	return ciliumEndpointSyncControllerK8sClient.CiliumV2(), nil
+}
+
+// RunK8sCiliumEndpointSyncGC starts the node-singleton sweeper for
+// CiliumEndpoint objects where the managing node is no longer running. These
+// objects are created by the sync-to-k8s-ciliumendpoint controller on each
+// Endpoint.
+// The general steps are:
+//   - get list of nodes
+//   - only run with probability 1/nodes
+//   - get list of CEPs
+//   - for each CEP
+//       delete CEP if the corresponding pod does not exist
+// CiliumEndpoint objects have the same name as the pod they represent
+// RunK8sCiliumEndpointSyncGC starts the node-singleton sweeper for
+// CiliumEndpoint objects where the managing node is no longer running. These
+// objects are created by the sync-to-k8s-ciliumendpoint controller on each
+// Endpoint.
+// The general steps are:
+//   - get list of nodes
+//   - only run with probability 1/nodes
+//   - get list of CEPs
+//   - for each CEP
+//       delete CEP if the corresponding pod does not exist
+// CiliumEndpoint objects have the same name as the pod they represent
+func RunK8sCiliumEndpointSyncGC() {
+	var (
+		controllerName = fmt.Sprintf("sync-to-k8s-ciliumendpoint-gc (%v)", node.GetName())
+		scopedLog      = log.WithField("controller", controllerName)
+
+		// random source to throttle how often this controller runs cluster-wide
+		runThrottler = rand.New(rand.NewSource(time.Now().UnixNano()))
+	)
+
+	// this is a sanity check
+	if !k8s.IsEnabled() {
+		scopedLog.WithField("name", controllerName).Warn("Not running controller because k8s is disabled")
+		return
+	}
+	sv, err := k8s.GetServerVersion()
+	if err != nil {
+		scopedLog.WithError(err).Error("unable to retrieve kubernetes serverversion")
+		return
+	}
+	if !ciliumEPControllerLimit.Check(sv) {
+		scopedLog.WithFields(logrus.Fields{
+			"expected": sv,
+			"found":    ciliumEPControllerLimit,
+		}).Warn("cannot run with this k8s version")
+		return
+	}
+
+	ciliumClient, err := getCiliumClient()
+	if err != nil {
+		scopedLog.WithError(err).Error("Not starting controller because unable to get cilium k8s client")
+		return
+	}
+	k8sClient := k8s.Client()
+
+	// this dummy manager is needed only to add this controller to the global list
+	controller.NewManager().UpdateController(controllerName,
+		controller.ControllerParams{
+			RunInterval: 1 * time.Minute,
+			DoFunc: func() error {
+				// Don't run if there are no other known nodes
+				// Only run with a probability of 1/(number of nodes in cluster). This
+				// is because this controller runs on every node on the same interval
+				// but only one is neede to run.
+				nodes := node.GetNodes()
+				if len(nodes) <= 1 || runThrottler.Int63n(int64(len(nodes))) != 0 {
+					return nil
+				}
+
+				clusterPodSet := map[string]bool{}
+				clusterPods, err := k8sClient.CoreV1().Pods("").List(meta_v1.ListOptions{})
+				if err != nil {
+					return err
+				}
+				for _, pod := range clusterPods.Items {
+					podFullName := pod.Name + ":" + pod.Namespace
+					clusterPodSet[podFullName] = true
+				}
+
+				// "" is all-namespaces
+				ceps, err := ciliumClient.CiliumEndpoints(meta_v1.NamespaceAll).List(meta_v1.ListOptions{})
+				if err != nil {
+					scopedLog.WithError(err).Debug("Cannot list CEPs")
+					return err
+				}
+				for _, cep := range ceps.Items {
+					cepFullName := cep.Name + ":" + cep.Namespace
+					if _, found := clusterPodSet[cepFullName]; !found {
+						// delete
+						scopedLog = scopedLog.WithFields(logrus.Fields{
+							logfields.EndpointID: cep.Status.ID,
+							logfields.K8sPodName: cepFullName,
+						})
+						scopedLog.Debug("Orphaned CiliumEndpoint is being garbage collected")
+						if err := ciliumClient.CiliumEndpoints(cep.Namespace).Delete(cep.Name, &meta_v1.DeleteOptions{}); err != nil {
+							scopedLog.WithError(err).Debug("Unable to delete CEP")
+							return err
+						}
+					}
+				}
+				return nil
+			},
+		})
 }
