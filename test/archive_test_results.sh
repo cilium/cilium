ac6b5a5a0a0216c67f2dd297875d0441817460f4
Eloy Coto <eloy.coto@gmail.com>
2018-04-12 10:16:35 +0200

    Nightly: Fix log gathering for Kubernetes stages
    Fix a typo in the ENV variable that it does not allow Jenkins to
    retrieve kubernetes logs correctly.
    
    Fix #3656
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- test/archive_test_results.sh
+++ test/archive_test_results.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
- 
+
 cd ${WORKSPACE}/test
 
 TEST_RESULTS="test_results"
 ARCHIVE_NAME="${TEST_RESULTS}_${JOB_BASE_NAME}_${BUILD_NUMBER}.zip"
+if [ -n "$1" ]
+then
+    ARCHIVE_NAME="${1}_${TEST_RESULTS}_${JOB_BASE_NAME}_${BUILD_NUMBER}.zip"
+    echo $ARCHIVE_NAME
+fi
 
 zip -r ${ARCHIVE_NAME} ${TEST_RESULTS}
 mv ${ARCHIVE_NAME} ../
