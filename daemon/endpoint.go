cilium/cilium

commit 41c08396ce4b7934c72906ff87b91f0d6da1c1b8

daemon: Only regenerate in PATCH from valid state
Previously, we would attempt to regenerate even if we were not in the
state where we are waiting to regenerate. The function being called
expects that it must be in that state already, so don't regenerate if
not already in the right state.

Related: #3268

Signed-off-by: Joe Stringer <joe@covalent.io>

daemon/endpoint.go
@@ -315,7 +315,7 @@ func (h *patchEndpointID) Handle(params PatchEndpointIDParams) middleware.Respon
 	}
 	ep.Mutex.Unlock()
 
-	if changed {
+	if ep.GetStateLocked() == endpoint.StateWaitingToRegenerate {
 		if err := ep.RegenerateWait(h.d, "Waiting on endpoint regeneration because identity is known while handling API PATCH"); err != nil {
 			return apierror.Error(PatchEndpointIDFailedCode, err)
 		}

commit 36a738a521c8da6e3c55f2ac2f8b49a3013d5c17

daemon: Regenerate endpoint in PATCH handler also when endpoint is in waiting-for-identity state.
commit 41c08396ce ("daemon: Only regenerate in PATCH from valid
state") intended to limit endpoint regeneration calls from the API
PATCH endpoint handler to valid endpoint states, but inadvertently
limited the allowed states only to waiting-to-regenerate, while the
endpoint should also be built while in the waiting-for-identity state.

The symptom was that endpoints created via docker never built the
initial drop-all bpf program while waiting for identity.

This commit allows endpoint regeneration via the PATCH endpoint API
also when the endpoint is in wairing-for-identity state.

Fixes: 41c08396ce ("daemon: Only regenerate in PATCH from valid state")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

daemon/endpoint.go
@@ -318,23 +318,30 @@ func (h *patchEndpointID) Handle(params PatchEndpointIDParams) middleware.Respon
 		changed = true
 	}
 
-	wait := false
+	reason := ""
 	if changed {
 		// Force policy regeneration as endpoint's configuration was changed.
 		// Other endpoints need not be regenerated as no labels were changed.
+		// Note that we still need to (eventually) regenerate the endpoint for
+		// the changes to take effect.
 		ep.ForcePolicyCompute()
+
 		// Transition to waiting-to-regenerate if ready.
 		if ep.GetStateLocked() == endpoint.StateReady {
 			ep.SetStateLocked(endpoint.StateWaitingToRegenerate, "Forcing endpoint regeneration because identity is known while handling API PATCH")
 		}
-		if ep.GetStateLocked() == endpoint.StateWaitingToRegenerate {
-			wait = true
+
+		switch ep.GetStateLocked() {
+		case endpoint.StateWaitingToRegenerate:
+			reason = "Waiting on endpoint regeneration because identity is known while handling API PATCH"
+		case endpoint.StateWaitingForIdentity:
+			reason = "Waiting on endpoint initial program regeneration while handling API PATCH"
 		}
 	}
 	ep.Mutex.Unlock()
 
-	if wait {
-		if err := ep.RegenerateWait(h.d, "Waiting on endpoint regeneration because identity is known while handling API PATCH"); err != nil {
+	if reason != "" {
+		if err := ep.RegenerateWait(h.d, reason); err != nil {
 			return apierror.Error(PatchEndpointIDFailedCode, err)
 		}
 		// FIXME: Special return code to indicate regeneration happened?

pkg/endpoint/policy.go
@@ -968,7 +968,8 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 }
 
 // Regenerate forces the regeneration of endpoint programs & policy
-// Should only be called with e.state == StateWaitingToRegenerate
+// Should only be called with e.state == StateWaitingToRegenerate or with
+// e.state == StateWaitingForIdentity
 func (e *Endpoint) Regenerate(owner Owner, reason string) <-chan bool {
 	newReq := &Request{
 		ID:           uint64(e.ID),