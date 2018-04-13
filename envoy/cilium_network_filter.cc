dd376fb9948372ce50e7c189a63ce2059d9ad858
Jarno Rajahalme <jarno@covalent.io>
2018-04-13 13:13:56 -0700

    envoy: Remove assert, reduce logging.
    [ upstream commit 1e1fbbd899c625f809ab12d794e8c5c2b32f46e7 ]
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- envoy/cilium_network_filter.cc
+++ envoy/cilium_network_filter.cc
@@ -62,7 +62,6 @@ Network::FilterStatus Instance::onNewConnection() {
       if (option) {
 	if (option->maps_) {
 	  // Insert connection callback to delete the proxymap entry once the connection is closed.
-	  ASSERT(!maps_);
 	  maps_ = option->maps_;
 	  proxy_port_ = option->proxy_port_;
 	  if (proxy_port_ != 0) {
@@ -71,7 +70,7 @@ Network::FilterStatus Instance::onNewConnection() {
 	  }
 	  break;
 	} else {
-	  ENVOY_CONN_LOG(warn, "Cilium Network: No proxymap", conn);
+	  ENVOY_CONN_LOG(debug, "Cilium Network: No proxymap", conn);
 	}
       }
     }
@@ -89,9 +88,11 @@ void Instance::onEvent(Network::ConnectionEvent event) {
   if (event == Network::ConnectionEvent::RemoteClose ||
       event == Network::ConnectionEvent::LocalClose) {
     auto& conn = callbacks_->connection();
-    bool ok = maps_->removeBpfMetadata(conn, proxy_port_);
-    ENVOY_CONN_LOG(warn, "Cilium Network: Connection Closed, proxymap cleanup {}", conn,
-	           ok ? "succeeded" : "failed");
+    if (maps_) {
+      bool ok = maps_->removeBpfMetadata(conn, proxy_port_);
+      ENVOY_CONN_LOG(debug, "Cilium Network: Connection Closed, proxymap cleanup {}", conn,
+		     ok ? "succeeded" : "failed");
+    }
   }
 }
 
