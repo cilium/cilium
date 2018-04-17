cilium/cilium

commit 7448e41aa0476cf6a01032fde9331160f56e4392

endpoint: sync endpoint IP-SecID map to kvstore
Whenever an endpoint is assigned a new security identity, update the key-value
store with a mapping of the endpoint's IP(s) to its newly assigned identity.
Add a new cache which caches this information from the key-value store locally.

Signed-off by: Ian Vernon <ian@cilium.io>

daemon/daemon.go
@@ -44,6 +44,7 @@ import (
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipam"
+	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
@@ -1050,6 +1051,9 @@ func NewDaemon(c *Config) (*Daemon, error) {
 	// This needs to be done after the node addressing has been configured
 	// as the node address is required as sufix
 	identity.InitIdentityAllocator(&d)
+
+	// Start watcher for endpoint IP --> identity mappings in key-value store.
+	ipcache.InitIPIdentityWatcher(&d)
 
 	if !d.conf.IPv4Disabled {
 		// Allocate IPv4 service loopback IP

common/addressing/ip.go
@@ -32,6 +32,7 @@ type CiliumIP interface {
 	IP() net.IP
 	String() string
 	IsIPv6() bool
+	GetFamilyString() string
 }
 
 type CiliumIPv6 []byte
@@ -278,4 +279,14 @@ func (ip CiliumIPv4) NodeIP() net.IP {
 	nodeIP[3] = 1
 
 	return nodeIP
+}
+
+// GetFamilyString returns the address family of ip as a string.
+func (ip CiliumIPv4) GetFamilyString() string {
+	return "IPv4"
+}
+
+// GetFamilyString returns the address family of ip as a string.
+func (ip CiliumIPv6) GetFamilyString() string {
+	return "IPv6"
 }

daemon/endpoint.go
@@ -679,3 +679,9 @@ func (h *putEndpointIDLabels) Handle(params PutEndpointIDLabelsParams) middlewar
 	}
 	return NewPutEndpointIDLabelsOK()
 }
+
+// OnIPIdentityCacheChange is called whenever there is a change of state in the
+// IPCache (pkg/ipcache). Currently does nothing as of now.
+func (d *Daemon) OnIPIdentityCacheChange() {
+
+}

pkg/endpoint/policy.go
@@ -15,23 +15,31 @@
 package endpoint
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
+	"path"
 	"path/filepath"
 	"reflect"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/pkg/controller"
 	identityPkg "github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/k8s"
+	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/monitor"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/policy/api"
 
@@ -923,6 +931,74 @@ func (e *Endpoint) runIdentityToK8sPodSync() {
 	)
 }
 
+// FormatGlobalEndpointID returns the global ID of endpoint in the format
+// / <global ID Prefix>:<cluster name>:<node name>:<endpoint ID> as a string.
+func (e *Endpoint) FormatGlobalEndpointID() string {
+	nodeIdentity, _ := node.GetLocalNode()
+	metadata := []string{CiliumGlobalIdPrefix, ipcache.AddressSpace, nodeIdentity.Name, strconv.Itoa(int(e.ID))}
+	return strings.Join(metadata, ":")
+}
+
+// This synchronizes the key-value store with a mapping of the endpoint's IP
+// with the numerical ID representing its security identity.
+func (e *Endpoint) runIPIdentitySync(endpointIP addressing.CiliumIP) {
+
+	if endpointIP == nil {
+		return
+	}
+
+	addressFamily := endpointIP.GetFamilyString()
+	ipKey := path.Join(ipcache.IPIdentitiesPath, ipcache.AddressSpace, endpointIP.String())
+
+	e.controllers.UpdateController(fmt.Sprintf("sync-%s-identity-mapping (%d)", addressFamily, e.ID),
+		controller.ControllerParams{
+			DoFunc: func() error {
+
+				e.Mutex.RLock()
+
+				if e.state == StateDisconnected || e.state == StateDisconnecting {
+					log.WithFields(logrus.Fields{logfields.EndpointState: e.state}).
+						Debugf("not synchronizing endpoint IP with kvstore due to endpoint state")
+					e.Mutex.RUnlock()
+					return nil
+				}
+
+				if e.SecurityIdentity == nil {
+					e.Mutex.RUnlock()
+					return nil
+				}
+				//identityValue := e.SecurityIdentity.ID.StringID()
+				ipIDPair := identityPkg.IPIdentityPair{
+					IP:       endpointIP.IP(),
+					ID:       e.SecurityIdentity.ID,
+					Metadata: e.FormatGlobalEndpointID(),
+				}
+
+				// Release lock as we do not want to have long-lasting key-value
+				// store operations resulting in lock being held for a long time.
+				e.Mutex.RUnlock()
+
+				marshaledIPIDPair, err := json.Marshal(ipIDPair)
+				if err != nil {
+					return err
+				}
+
+				if err := kvstore.Update(ipKey, marshaledIPIDPair, true); err != nil {
+					return fmt.Errorf("unable to add endpoint IP '%s' to identity '%s': %s", ipKey, marshaledIPIDPair, err)
+				}
+				return nil
+			},
+			StopFunc: func() error {
+				if err := kvstore.Delete(ipKey); err != nil {
+					return fmt.Errorf("unable to delete endpoint IP '%s': %s", ipKey, err)
+				}
+				return nil
+			},
+			RunInterval: 5 * time.Minute,
+		},
+	)
+}
+
 // SetIdentity resets endpoint's policy identity to 'id'.
 // Caller triggers policy regeneration if needed.
 // Called with e.Mutex Locked
@@ -956,6 +1032,11 @@ func (e *Endpoint) SetIdentity(owner Owner, identity *identityPkg.Identity) {
 	}
 
 	e.runIdentityToK8sPodSync()
+
+	// Whenever the identity is updated, propagate change to key-value store
+	// of IP to identity mapping.
+	e.runIPIdentitySync(e.IPv4)
+	e.runIPIdentitySync(e.IPv6)
 
 	e.Consumable.Mutex.RLock()
 	e.getLogger().WithFields(logrus.Fields{

pkg/identity/identity.go
@@ -15,6 +15,8 @@
 package identity
 
 import (
+	"net"
+
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/labels"
 )
@@ -28,6 +30,18 @@ type Identity struct {
 	Labels labels.Labels `json:"labels"`
 	// SHA256 of labels.
 	LabelsSHA256 string `json:"labelsSHA256"`
+}
+
+// IPIdentityPair is a pairing of an IP and the security identity to which that
+// IP corresponds.
+//
+// WARNING - STABLE API
+// This structure is written as JSON to the key-value store. Do NOT modify this
+// structure in ways which are not JSON forward compatible.
+type IPIdentityPair struct {
+	IP       net.IP          `json:"IP"`
+	ID       NumericIdentity `json:"ID"`
+	Metadata string          `json:"Metadata"`
 }
 
 func NewIdentityFromModel(base *models.Identity) *Identity {

pkg/ipcache/config.go
@@ -1,0 +1,21 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ipcache
+
+import "github.com/cilium/cilium/pkg/logging"
+
+var (
+	log = logging.DefaultLogger
+)

pkg/ipcache/doc.go
@@ -1,0 +1,17 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package ipcache provides a local cache of the mapping of IPs of endpoints
+// managed by Cilium to their corresponding security identities.
+package ipcache

pkg/ipcache/ipcache.go
@@ -1,0 +1,232 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ipcache
+
+import (
+	"encoding/json"
+	"path"
+	"sort"
+	"sync"
+
+	"github.com/cilium/cilium/pkg/envoy"
+	envoyAPI "github.com/cilium/cilium/pkg/envoy/cilium"
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/kvstore"
+	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	// DefaultAddressSpace is the address space used if none is provided.
+	// TODO - once pkg/node adds this to clusterConfiguration, remove.
+	DefaultAddressSpace = "default"
+)
+
+var (
+	// IPIdentitiesPath is the path to where endpoint IPs are stored in the key-value
+	//store.
+	IPIdentitiesPath = path.Join(kvstore.BaseKeyPrefix, "state", "ip", "v1")
+
+	// IPIdentityCache caches the mapping of endpoint IPs to their corresponding
+	// security identities across the entire cluster in which this instance of
+	// Cilium is running.
+	IPIdentityCache = NewIPCache()
+
+	// AddressSpace is the address space (cluster, etc.) in which policy is
+	// computed. It is determined by the orchestration system / runtime.
+	AddressSpace = DefaultAddressSpace
+
+	setupIPIdentityWatcher sync.Once
+)
+
+// IPCache is a caching of endpoint IP to security identity (and vice-versa) for
+// all endpoints which are part of the same cluster.
+type IPCache struct {
+	mutex             lock.RWMutex
+	ipToIdentityCache map[string]identity.NumericIdentity
+	identityToIPCache map[identity.NumericIdentity]map[string]struct{}
+}
+
+// NewIPCache returns a new IPCache with the mappings of endpoint IP to security
+// identity (and vice-versa) initialized.
+func NewIPCache() *IPCache {
+	return &IPCache{
+		ipToIdentityCache: map[string]identity.NumericIdentity{},
+		identityToIPCache: map[identity.NumericIdentity]map[string]struct{}{},
+	}
+}
+
+// upsert adds / updates the provided IP and identity into both caches contained
+// within ipc.
+func (ipc *IPCache) upsert(endpointIP string, identity identity.NumericIdentity) {
+	ipc.mutex.Lock()
+	defer ipc.mutex.Unlock()
+
+	// An update is treated as a deletion and then an insert.
+	ipc.deleteLocked(endpointIP)
+
+	// Update both maps.
+	ipc.ipToIdentityCache[endpointIP] = identity
+
+	_, found := ipc.identityToIPCache[identity]
+	if !found {
+		ipc.identityToIPCache[identity] = map[string]struct{}{}
+	}
+	ipc.identityToIPCache[identity][endpointIP] = struct{}{}
+}
+
+// deleteLocked removes removes the provided IP-to-security-identity mapping
+// from both caches within ipc with the assumption that ipc's mutex is held.
+func (ipc *IPCache) deleteLocked(endpointIP string) {
+
+	identity, found := ipc.ipToIdentityCache[endpointIP]
+	if found {
+		delete(ipc.ipToIdentityCache, endpointIP)
+		delete(ipc.identityToIPCache[identity], endpointIP)
+		if len(ipc.identityToIPCache[identity]) == 0 {
+			delete(ipc.identityToIPCache, identity)
+		}
+	}
+}
+
+// delete removes the provided IP-to-security-identity mapping from both caches
+// within ipc.
+func (ipc *IPCache) delete(endpointIP string) {
+	ipc.mutex.Lock()
+	defer ipc.mutex.Unlock()
+	ipc.deleteLocked(endpointIP)
+}
+
+// LookupByIP returns the corresponding security identity that endpoint IP maps
+// to within the provided IPCache, as well as if the corresponding entry exists
+// in the IPCache.
+func (ipc *IPCache) LookupByIP(endpointIP string) (identity.NumericIdentity, bool) {
+	ipc.mutex.RLock()
+	defer ipc.mutex.RUnlock()
+	identity, exists := ipc.ipToIdentityCache[endpointIP]
+	return identity, exists
+}
+
+// LookupByIdentity returns the set of endpoint IPs that have security identity
+// ID, as well as if the corresponding entry exists in the IPCache.
+func (ipc *IPCache) LookupByIdentity(id identity.NumericIdentity) (map[string]struct{}, bool) {
+	ipc.mutex.RLock()
+	defer ipc.mutex.RUnlock()
+	ips, exists := ipc.identityToIPCache[id]
+	return ips, exists
+}
+
+// IPIdentityMappingOwner is the interface the owner of an identity allocator
+// must implement
+type IPIdentityMappingOwner interface {
+	// OnIPIdentityCacheChange will be called whenever there the state of the
+	// IPCache has changed.
+	OnIPIdentityCacheChange()
+}
+
+// GetIPIdentityMapModel returns all known endpoint IP to security identity mappings
+// stored in the key-value store.
+func GetIPIdentityMapModel() {
+	// TODO (ianvernon) return model of ip to identity mapping. For use in CLI.
+	// see GH-2555
+}
+
+func ipIdentityWatcher(owner IPIdentityMappingOwner) {
+
+	for {
+		watcher := kvstore.ListAndWatch("endpointIPWatcher", IPIdentitiesPath, 512)
+
+		// Get events from channel as they come in.
+		for event := range watcher.Events {
+
+			var (
+				cacheChanged bool
+				ipIDPair     identity.IPIdentityPair
+			)
+
+			err := json.Unmarshal(event.Value, &ipIDPair)
+			if err != nil {
+				log.WithFields(logrus.Fields{"value": event.Value}).WithError(err).Errorf("not adding entry to ip cache; error unmarshaling data from key-value store")
+				continue
+			}
+
+			// Synchronize local caching of endpoint IP to ipIDPair mapping with
+			// operation key-value store has informed us about.
+
+			ipStr := ipIDPair.IP.String()
+
+			cachedIdentity, exists := IPIdentityCache.LookupByIP(ipStr)
+
+			switch event.Typ {
+			case kvstore.EventTypeCreate, kvstore.EventTypeModify:
+				if !exists || cachedIdentity != ipIDPair.ID {
+					IPIdentityCache.upsert(ipStr, ipIDPair.ID)
+					cacheChanged = true
+
+					endpointIPs, _ := IPIdentityCache.LookupByIdentity(ipIDPair.ID)
+
+					// Update XDS Cache as well.
+					ipStrings := make([]string, 0, len(endpointIPs))
+					for endpointIP := range endpointIPs {
+						ipStrings = append(ipStrings, endpointIP)
+					}
+					sort.Strings(ipStrings)
+					envoy.NetworkPolicyHostsCache.Upsert(envoy.NetworkPolicyHostsTypeURL, ipIDPair.ID.StringID(), &envoyAPI.NetworkPolicyHosts{Policy: uint64(ipIDPair.ID), HostAddresses: ipStrings}, false)
+				}
+			case kvstore.EventTypeDelete:
+				if exists {
+					IPIdentityCache.delete(ipStr)
+					cacheChanged = true
+
+					endpointIPs, exists := IPIdentityCache.LookupByIdentity(ipIDPair.ID)
+					if !exists {
+						// Delete from XDS Cache as well.
+						envoy.NetworkPolicyHostsCache.Delete(envoy.NetworkPolicyHostsTypeURL, cachedIdentity.StringID(), false)
+					} else {
+
+						// TODO (factor this out into a helper function).
+						ipStrings := make([]string, 0, len(endpointIPs))
+						for endpointIP := range endpointIPs {
+							ipStrings = append(ipStrings, endpointIP)
+						}
+						sort.Strings(ipStrings)
+						envoy.NetworkPolicyHostsCache.Upsert(envoy.NetworkPolicyHostsTypeURL, ipIDPair.ID.StringID(), &envoyAPI.NetworkPolicyHosts{Policy: uint64(ipIDPair.ID), HostAddresses: ipStrings}, false)
+					}
+				}
+			}
+
+			if cacheChanged {
+				log.WithFields(logrus.Fields{
+					"endpoint-ip":      ipIDPair.IP,
+					"cached-identity":  cachedIdentity,
+					logfields.Identity: ipIDPair.ID,
+				}).Debugf("endpoint IP cache changed state")
+				owner.OnIPIdentityCacheChange()
+			}
+		}
+
+		log.Debugf("%s closed, restarting watch", watcher.String())
+	}
+}
+
+// InitIPIdentityWatcher initializes the watcher for ip-identity mapping events
+// in the key-value store.
+func InitIPIdentityWatcher(owner IPIdentityMappingOwner) {
+	setupIPIdentityWatcher.Do(func() {
+		go ipIdentityWatcher(owner)
+	})
+}

commit 09ec80f512f161165e85e02864b7fd713910c128

daemon: Push reserved IP->Identity mappings to XDS
Previously we were only handling the BPF case and missing these IPs in
XDS, so presumably we would not apply L3/L4 policies correctly for the
XDS (eg Envoy sidecar) case.

CC: Ian Vernon <ian@cilium.io>
Fixes: 7448e41aa047 ("endpoint: sync endpoint IP-SecID map to kvstore")
Signed-off-by: Joe Stringer <joe@covalent.io>

daemon/daemon.go
@@ -845,7 +845,13 @@ func (d *Daemon) init() error {
 
 			log.WithField(logfields.IPAddr, ip).Debug("Adding local ip to ipcache")
 			ipcache.IPIdentityCache.Upsert(ip.String(), identity.ReservedIdentityHost)
-			d.OnIPIdentityCacheChange(ipcache.Upsert, identity.IPIdentityPair{IP: ip, ID: identity.ReservedIdentityHost})
+
+			// The ipcache hasn't been bootstrapped yet, and we don't actually
+			// want to propagate reserved:host->ID mappings across the cluster.
+			// Manually push it into each IPIdentityMappingListener.
+			localIPIDPair := identity.IPIdentityPair{IP: ip, ID: identity.ReservedIdentityHost}
+			d.OnIPIdentityCacheChange(ipcache.Upsert, localIPIDPair)
+			envoy.NetworkPolicyHostsCache.OnIPIdentityCacheChange(ipcache.Upsert, localIPIDPair)
 		}
 
 		if _, err := lbmap.Service6Map.OpenOrCreate(); err != nil {