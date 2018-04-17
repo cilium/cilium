cb149ee85d1ce10b3fad28aff063491f4d3e8e8e
Jarno Rajahalme <jarno@covalent.io>
2018-04-17 12:49:49 -0700

    envoy: Use network byte-order addresses in host map.
    This is more efficient and is a step into making the keys maskable for
    CIDR lookup.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- envoy/cilium_host_map.h
+++ envoy/cilium_host_map.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <arpa/inet.h>
+
 #include "envoy/local_info/local_info.h"
 #include "envoy/upstream/cluster_manager.h"
 #include "envoy/event/dispatcher.h"
@@ -11,8 +13,22 @@
 
 #include "cilium/nphds.pb.h"
 
+#include "absl/numeric/int128.h"
+
+namespace std {
+  template <> struct hash<absl::uint128>
+  {
+    size_t operator()(const absl::uint128& x) const
+    {
+      return hash<uint64_t>{}(absl::Uint128Low64(x)) ^ (hash<uint64_t>{}(absl::Uint128High64(x)) << 1);
+    }
+  };
+}
+
 namespace Envoy {
 namespace Cilium {
+
+enum ID : uint64_t { UNKNOWN = 0, WORLD = 2 };
 
 class PolicyHostMap : public Singleton::Instance,
                       Config::SubscriptionCallbacks<cilium::NetworkPolicyHosts>,
@@ -32,20 +48,50 @@ public:
       const auto& hosts = proto.host_addresses();
 
       for (const auto& host: hosts) {
-	auto pair = ip_to_policy_.emplace(std::make_pair(host, policy));
-	if (pair.second == false) {
-	  throw EnvoyException(fmt::format("NetworkPolicyHosts: Duplicate host entry {}:{}", host, policy));
+	uint32_t addr4;
+	int rc = inet_pton(AF_INET, host.c_str(), &addr4);
+	if (rc == 1) {
+	  auto pair = ipv4_to_policy_.emplace(std::make_pair(addr4, policy));
+	  if (pair.second == false) {
+	    throw EnvoyException(fmt::format("NetworkPolicyHosts: Duplicate host entry {}:{}", host, policy));
+	  }
+	  continue;
 	}
+	absl::uint128 addr6;
+	rc = inet_pton(AF_INET6, host.c_str(), &addr6);
+	if (rc == 1) {
+	  auto pair = ipv6_to_policy_.emplace(std::make_pair(addr6, policy));
+	  if (pair.second == false) {
+	    throw EnvoyException(fmt::format("NetworkPolicyHosts: Duplicate host entry {}:{}", host, policy));
+	  }
+	  continue;
+	}
+	throw EnvoyException(fmt::format("NetworkPolicyHosts: Invalid host entry {}:{}", host, policy));
       }
     }
 
-    uint64_t resolve(const std::string& addr) const {
-      auto it = ip_to_policy_.find(addr);
-      return it == ip_to_policy_.end() ? 2 /* WORLD */ : it->second;
+    uint64_t resolve(const Network::Address::Ip* addr) const {
+      auto* ipv4 = addr->ipv4();
+      if (ipv4) {
+	auto it = ipv4_to_policy_.find(ipv4->address());
+	if (it != ipv4_to_policy_.end()) {
+	  return it->second;
+	}
+      } else {
+	auto* ipv6 = addr->ipv6();
+	if (ipv6) {
+	  auto it = ipv6_to_policy_.find(ipv6->address());
+	  if (it != ipv6_to_policy_.end()) {
+	    return it->second;
+	  }
+	}
+      }
+      return ID::WORLD;
     }
 
   private:
-    std::unordered_map<std::string, uint64_t> ip_to_policy_;
+    std::unordered_map<uint32_t, uint64_t> ipv4_to_policy_;
+    std::unordered_map<absl::uint128, uint64_t> ipv6_to_policy_;
   };
   typedef std::shared_ptr<ThreadLocalHostMap> ThreadLocalHostMapSharedPtr;
 
@@ -53,9 +99,9 @@ public:
     return &tls_->getTyped<ThreadLocalHostMap>();
   }
 
-  uint64_t resolve(const std::string& addr) const {
+  uint64_t resolve(const Network::Address::Ip* addr) const {
     const ThreadLocalHostMap* hostmap = getHostMap();
-    return (hostmap != nullptr) ? hostmap->resolve(addr) : 0;
+    return (hostmap != nullptr) ? hostmap->resolve(addr) : ID::UNKNOWN;
   }
 
   // Config::SubscriptionCallbacks
