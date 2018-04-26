f3a357568712291224dc1a5f8e7f4134c1734c8f
Eloy Coto <eloy.coto@gmail.com>
2018-04-26 10:54:57 +0200

    Test: Archive in quiet mode
    The zip command verbose is a bit high on Nighly test and there is no value added.
    Added a -q option will show an error if happens but avoid to list all
    the files that are in the zip file.
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- test/archive_test_results.sh
+++ test/archive_test_results.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
- 
+
 cd ${WORKSPACE}/test
 
 TEST_RESULTS="test_results"
 ARCHIVE_NAME="${TEST_RESULTS}_${JOB_BASE_NAME}_${BUILD_NUMBER}.zip"
 
-zip -r ${ARCHIVE_NAME} ${TEST_RESULTS}
+zip -qr ${ARCHIVE_NAME} ${TEST_RESULTS}
 mv ${ARCHIVE_NAME} ../
 rm -rf ./${TEST_RESULTS}
