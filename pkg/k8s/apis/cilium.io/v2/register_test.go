2eeb203021a005fbf5b68fd460c3776f304a4212
Joe Stringer <joe@covalent.io>
2018-05-03 17:53:15 -0700

    k8s: Add CRD IP address validation unit tests
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- pkg/k8s/apis/cilium.io/v2/register_test.go
+++ pkg/k8s/apis/cilium.io/v2/register_test.go
@@ -15,8 +15,9 @@
 package v2
 
 import (
-	. "gopkg.in/check.v1"
+	"regexp"
 
+	. "gopkg.in/check.v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -76,4 +77,75 @@ func (s *CiliumV2RegisterSuite) TestNeedsUpdateCorruptedVersion(c *C) {
 	crd.Labels[CustomResourceDefinitionSchemaVersionKey] = "totally-not-semver"
 
 	c.Assert(needsUpdate(crd), Equals, true)
+}
+
+func (s *CiliumV2RegisterSuite) TestCIDRRegex(c *C) {
+	goodCIDRs := []string{
+		"192.0.2.3/32",
+		"192.0.2.0/24",
+		"0.0.0.0/0",
+		"::/0",
+		"::cafe/128",
+		"::f00d:cafe/128",
+		"0:0:0:0:0:0:0:cafe/128",
+		"cafe:cafe:cafe:cafe:cafe:cafe:cafe:cafe/128",
+		"bad:f00d:cafe:0:0:0:0:add/64",
+		"bad:f00d:cafe::bad/64",
+		"f00d::/64",
+		"f00d::0:0/120",
+		"f00d:cafe::1:2/120",
+	}
+
+continueTest:
+	for _, input := range goodCIDRs {
+		for _, prop := range CIDR.OneOf {
+			matched, err := regexp.MatchString(prop.Pattern, input)
+			c.Assert(err, IsNil)
+			if matched {
+				continue continueTest
+			}
+		}
+		// The below is always false, valid CIDR prefixes should
+		// always skip this by continuing in the above loop.
+		c.Assert(input, Equals, "failed to match CIDR.OneOf[*].Pattern")
+	}
+
+	badCIDRs := []string{
+		"192.0.2.3",
+		"192.0.2.3/",
+		"abcdef",
+		"0.0.0.0/0/0",
+		"::",
+		":",
+		":/",
+		"0:0",
+		"::cafe/128/12",
+		"abc:def",
+		"abc:def/64",
+		"f00d::/",
+		"f00d::0:0",
+		"bad.f00d.cafe.0.0.0.0.add/20",
+		"::192.0.2.3/128",
+		"::ffff:192.0.2.3/128",
+		"abcd:192.0.2.3/128",
+		":abcd:192.0.2.3/128",
+		"abcd::192.0.2.3/128",
+		":abcd::192.0.2.3/128",
+		"bad::f00d::cafe/1",
+		":bad::f00d::cafe/1",
+		"::bad::f00d::cafe/1",
+		"::bad::food::cafe/1",
+	}
+
+	for _, input := range badCIDRs {
+		for _, prop := range CIDR.OneOf {
+			matched, err := regexp.MatchString(prop.Pattern, input)
+			c.Assert(err, IsNil)
+			if matched {
+				// The below is always false, invalid CIDR
+				// prefixes are not supposed to match the regex.
+				c.Assert(input, Equals, "unexpectedly matched CIDR.OneOf[*].Pattern")
+			}
+		}
+	}
 }
