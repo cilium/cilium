a978b9752c2233aa7bd1a91449b15115242a3dc0
Ian Vernon <ian@cilium.io>
2018-05-09 15:05:18 -0700

    vagrant: configure journald to allow for large amounts of logs
    We have observed large gaps in time in Cilium logs in runtime tests. Configure
    journald rate limit interval and rate limit burst to allow for more logs so that
    if we hit an issue in Cilium, logs are not lost.
    
    Signed-off by: Ian Vernon <ian@cilium.io.

--- test/provision/runtime_install.sh
+++ test/provision/runtime_install.sh
@@ -8,6 +8,15 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source "${PROVISIONSRC}/helpers.bash"
 
+echo "editing journald configuration"
+sudo bash -c "echo RateLimitIntervalSec=1s >> /etc/systemd/journald.conf"
+sudo bash -c "echo RateLimitBurst=1000 >> /etc/systemd/journald.conf"
+echo "restarting systemd-journald"
+sudo systemctl restart systemd-journald
+echo "getting status of systemd-journald"
+sudo service systemd-journald status
+echo "done configuring journald"
+
 # Delete this section when the new server is ready
 # https://github.com/cilium/cilium/pull/3023/files
 export GOPATH="/home/vagrant/go"
