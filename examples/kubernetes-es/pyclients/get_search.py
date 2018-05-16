565732c681f5dc4d51324ba487fa1cea6acb27fa
Arvind Soni <arvindsoni@gmail.com>
2018-05-15 22:25:20 -0700

    es gsg
    Please enter the commit message for your changes. Lines starting

--- examples/kubernetes-es/pyclients/get_search.py
+++ examples/kubernetes-es/pyclients/get_search.py
@@ -1,26 +1,18 @@
 from elasticsearch import Elasticsearch
-es = Elasticsearch(['192.168.99.101:31608'])
+es = Elasticsearch(['elasticsearch.default.svc.cluster.local:9200'])
 
 book1 = { 'author': 'sidious', 'title': 'Convert Jedi to Dark Side: 101' }
 
 book2 = { 'author': 'sidious', 'title': 'Welcome to the Dark Side' }
 
-
-res = es.index(index="sidious", doc_type="tome", id=1, body=book1)
-
-print(res['result'])
-
-
-res = es.index(index="sidious", doc_type="tome", id=2, body=book2)
-
-print(res['result'])
-
+print("Searching for Books by Darth Sidious")
 res = es.search(index="sidious", body={"query": {"match_all": {}}})
 
 print("Got %d Hits:" % res['hits']['total'])
 for hit in res['hits']['hits']:
     print(hit)
 
+print("Get Book 1 by Darth Sidious")
 res = es.get(index="sidious", doc_type="tome", id=1)
 print(res['_source'])
 
