3497dfc5e7a68316c86b4cf827227f20bc58d6a9
Arvind Soni <arvindsoni@gmail.com>
2018-05-15 22:25:20 -0700

    es gsg
    Please enter the commit message for your changes. Lines starting

--- examples/kubernetes-es/pyclients/update.py
+++ examples/kubernetes-es/pyclients/update.py
@@ -1,27 +1,16 @@
 from elasticsearch import Elasticsearch
-es = Elasticsearch(['192.168.99.101:31608'])
+es = Elasticsearch(['elasticsearch.default.svc.cluster.local:9200'])
 
-book1 = { 'author': 'sidious', 'title': 'Convert Jedi to Dark Side: 101' }
+book1 = { 'author': 'sidious', 'title': 'Why Convert a Jedi!' }
 
-book2 = { 'author': 'sidious', 'title': 'Welcome to the Dark Side' }
+book2 = { 'author': 'sidious', 'title': 'Force is Same for Dark Side and Jedi' }
 
+print("Creating/Updating Books")
 
 res = es.index(index="sidious", doc_type="tome", id=1, body=book1)
 
-print(res['result'])
-
+print(res['result'], ": ", res)
 
 res = es.index(index="sidious", doc_type="tome", id=2, body=book2)
 
-print(res['result'])
-
-res = es.search(index="sidious", body={"query": {"match_all": {}}})
-
-print("Got %d Hits:" % res['hits']['total'])
-for hit in res['hits']['hits']:
-    print(hit)
-
-res = es.get(index="sidious", doc_type="tome", id=1)
-print(res['_source'])
-
-
+print(res['result'], ": ", res)
