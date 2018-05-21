eb1e94b7e6e00356a59b0bedc9f74c8869713619
André Martins <andre@cilium.io>
2018-05-22 00:22:19 +0200

    test: update k8s versions to 1.7.15, 1.8.13, 1.9.8, 1.10.3 and 1.11.0-beta.0
    Signed-off-by: André Martins <andre@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -86,22 +86,22 @@ case $K8S_VERSION in
         ;;
     "1.8")
         KUBERNETES_CNI_VERSION="0.5.1-00"
-        K8S_FULL_VERSION="1.8.11"
+        K8S_FULL_VERSION="1.8.13"
         ;;
     "1.9")
         KUBERNETES_CNI_VERSION="0.6.0-00"
-        K8S_FULL_VERSION="1.9.7"
+        K8S_FULL_VERSION="1.9.8"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         ;;
     "1.10")
         KUBERNETES_CNI_VERSION="0.6.0-00"
-        K8S_FULL_VERSION="1.10.1"
+        K8S_FULL_VERSION="1.10.3"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri"
         ;;
     "1.11")
         KUBERNETES_CNI_VERSION="v0.6.0"
-        K8S_FULL_VERSION="1.11.0-alpha.1"
+        K8S_FULL_VERSION="1.11.0-beta.0"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri"
         ;;
