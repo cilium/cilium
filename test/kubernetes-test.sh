a90bbe355b82dea6655624f7c0a0c6b21e9502ab
André Martins <andre@cilium.io>
2018-05-23 12:39:16 +0200

    test: download exact k8s version of k8s upstream e2e
    Signed-off-by: André Martins <andre@cilium.io>

--- test/kubernetes-test.sh
+++ test/kubernetes-test.sh
@@ -13,14 +13,14 @@ while true; do
     sleep 1
 done
 
-KUBERNETES_VERSION=$(kubectl version -o json | jq -r '.serverVersion | .major + "." +.minor')
+KUBERNETES_VERSION=$(kubectl version -o json | jq -r '.serverVersion | .gitVersion')
 
 set -e
 echo "Installing kubernetes"
 
 mkdir -p $HOME/go/src/github.com/kubernetes/
 cd $HOME/go/src/github.com/kubernetes/
-git clone https://github.com/kubernetes/kubernetes.git -b release-${KUBERNETES_VERSION} --depth 1
+git clone https://github.com/kubernetes/kubernetes.git -b ${KUBERNETES_VERSION} --depth 1
 cd kubernetes
 make ginkgo
 make WHAT='test/e2e/e2e.test'
