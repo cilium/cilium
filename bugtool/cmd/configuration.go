bfdefc657058662346bfc69aee5f87bbeae2b388
Steven Ceuppens <steven.ceuppens@icloud.com>
2018-05-24 16:10:21 +0200

    Add "cilium identity list" output to bugtool
    Fixes: #4236
    Signed-Off-By: Steven Ceuppens <steven.ceuppens@icloud.com>

--- bugtool/cmd/configuration.go
+++ bugtool/cmd/configuration.go
@@ -217,6 +217,7 @@ func copyCiliumInfoCommands(cmdDir string, k8sPods []string) []string {
 		"cilium bpf proxy list",
 		"cilium bpf ipcache list",
 		"cilium status --verbose",
+		"cilium identity list",
 		"cilium-health status",
 	}
 	var commands []string
