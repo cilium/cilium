cilium/cilium

commit 7e4436b9bc44f5fdf99450078964343991433959

agent: Add --ipv6-cluster-alloc-cidr option to specify IPv6 cluster prefix
Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/defaults/defaults.go
@@ -19,6 +19,12 @@ import (
 )
 
 const (
+	// IPv6ClusterAllocCIDR is the default value for option.IPv6ClusterAllocCIDR
+	IPv6ClusterAllocCIDR = IPv6ClusterAllocCIDRBase + "/64"
+
+	// IPv6ClusterAllocCIDRBase is the default base for IPv6ClusterAllocCIDR
+	IPv6ClusterAllocCIDRBase = "fdfd::"
+
 	// RuntimePath is the default path to the runtime directory
 	RuntimePath = "/var/run/cilium"
 

pkg/node/defaults.go
@@ -19,9 +19,6 @@ import (
 )
 
 const (
-	// DefaultIPv6Prefix is the prefix for all the IPv6 addresses.
-	DefaultIPv6Prefix = "f00d::"
-
 	// IPv6NodeAllocPrefixLen is the length of the prefix used for allocation per node
 	IPv6NodeAllocPrefixLen = 112
 

daemon/daemon.go
@@ -1017,6 +1017,11 @@ func (d *Daemon) syncLXCMap() error {
 
 // NewDaemon creates and returns a new Daemon with the parameters set in c.
 func NewDaemon() (*Daemon, error) {
+	// Validate the daemon specific global options
+	if err := option.Config.Validate(); err != nil {
+		return nil, fmt.Errorf("invalid daemon configuration: %s", err)
+	}
+
 	if err := workloads.Setup(option.Config.Workloads, map[string]string{}); err != nil {
 		return nil, fmt.Errorf("unable to setup workload: %s", err)
 	}

daemon/main.go
@@ -348,6 +348,8 @@ func init() {
 		"ipv4-range", AutoCIDR, "Per-node IPv4 endpoint prefix, e.g. 10.16.0.0/16")
 	flags.StringVar(&v6Prefix,
 		"ipv6-range", AutoCIDR, "Per-node IPv6 endpoint prefix, must be /96, e.g. fd02:1:1::/96")
+	flags.StringVar(&option.Config.IPv6ClusterAllocCIDR,
+		option.IPv6ClusterAllocCIDRName, defaults.IPv6ClusterAllocCIDR, "IPv6 /64 CIDR used to allocate per node endpoint /96 CIDR")
 	flags.StringVar(&v4ServicePrefix,
 		"ipv4-service-range", AutoCIDR, "Kubernetes IPv4 services CIDR if not inside cluster prefix")
 	flags.StringVar(&v6ServicePrefix,

pkg/node/node_address.go
@@ -163,7 +163,7 @@ func InitDefaultPrefix(device string) {
 		// The IPv6 allocation should be derived from the IPv4 allocation.
 		ip = ipv4AllocRange.IP
 		v6range := fmt.Sprintf("%s%02x%02x:%02x%02x:0:0/%d",
-			DefaultIPv6Prefix, ip[0], ip[1], ip[2], ip[3],
+			option.Config.IPv6ClusterAllocCIDRBase, ip[0], ip[1], ip[2], ip[3],
 			IPv6NodePrefixLen)
 
 		_, ip6net, err := net.ParseCIDR(v6range)

pkg/option/config.go
@@ -15,6 +15,7 @@
 package option
 
 import (
+	"fmt"
 	"net"
 	"os"
 	"path/filepath"
@@ -22,6 +23,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/lock"
 )
 
@@ -44,6 +46,9 @@ const (
 
 	// ModePreFilterGeneric for loading progs with xdpgeneric
 	ModePreFilterGeneric = "generic"
+
+	// IPv6ClusterAllocCIDRName is the name of the IPv6ClusterAllocCIDR option
+	IPv6ClusterAllocCIDRName = "ipv6-cluster-alloc-cidr"
 )
 
 // daemonConfig is the configuration used by Daemon.
@@ -89,12 +94,25 @@ type daemonConfig struct {
 
 	// AgentLabels contains additional labels to identify this agent in monitor events.
 	AgentLabels []string
+
+	// IPv6ClusterAllocCIDR is the base CIDR used to allocate IPv6 node
+	// CIDRs if allocation is not performed by an orchestration system
+	IPv6ClusterAllocCIDR string
+
+	// IPv6ClusterAllocCIDRBase is derived from IPv6ClusterAllocCIDR and
+	// contains the CIDR without the mask, e.g. "fdfd::1/64" -> "fdfd::"
+	//
+	// This variable should never be written to, it is initialized via
+	// daemonConfig.Validate()
+	IPv6ClusterAllocCIDRBase string
 }
 
 var (
 	Config = &daemonConfig{
-		Opts:    NewBoolOptions(&daemonLibrary),
-		Monitor: &models.MonitorStatus{Cpus: int64(runtime.NumCPU()), Npages: 64, Pagesize: int64(os.Getpagesize()), Lost: 0, Unknown: 0},
+		Opts:                     NewBoolOptions(&daemonLibrary),
+		Monitor:                  &models.MonitorStatus{Cpus: int64(runtime.NumCPU()), Npages: 64, Pagesize: int64(os.Getpagesize()), Lost: 0, Unknown: 0},
+		IPv6ClusterAllocCIDR:     defaults.IPv6ClusterAllocCIDR,
+		IPv6ClusterAllocCIDRBase: defaults.IPv6ClusterAllocCIDRBase,
 	}
 )
 
@@ -129,4 +147,33 @@ func (c *daemonConfig) AlwaysAllowLocalhost() bool {
 // specific set of labels) is enabled.
 func (c *daemonConfig) TracingEnabled() bool {
 	return c.Opts.IsEnabled(PolicyTracing)
+}
+
+func (c *daemonConfig) validateIPv6ClusterAllocCIDR() error {
+	ip, cidr, err := net.ParseCIDR(c.IPv6ClusterAllocCIDR)
+	if err != nil {
+		return err
+	}
+
+	if cidr == nil {
+		return fmt.Errorf("ParseCIDR returned nil")
+	}
+
+	if ones, _ := cidr.Mask.Size(); ones != 64 {
+		return fmt.Errorf("CIDR length must be /64")
+	}
+
+	c.IPv6ClusterAllocCIDRBase = ip.Mask(cidr.Mask).String()
+
+	return nil
+}
+
+// Validate validates the daemon configuration
+func (c *daemonConfig) Validate() error {
+	if err := c.validateIPv6ClusterAllocCIDR(); err != nil {
+		return fmt.Errorf("unable to parse CIDR value '%s' of option --%s: %s",
+			c.IPv6ClusterAllocCIDR, IPv6ClusterAllocCIDRName, err)
+	}
+
+	return nil
 }

pkg/option/config_test.go
@@ -1,0 +1,41 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package option
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+func (s *OptionSuite) TestValidateIPv6ClusterAllocCIDR(c *C) {
+	valid1 := &daemonConfig{IPv6ClusterAllocCIDR: "fdfd::/64"}
+	c.Assert(valid1.validateIPv6ClusterAllocCIDR(), IsNil)
+	c.Assert(valid1.IPv6ClusterAllocCIDRBase, Equals, "fdfd::")
+
+	valid2 := &daemonConfig{IPv6ClusterAllocCIDR: "fdfd:fdfd:fdfd:fdfd:aaaa::/64"}
+	c.Assert(valid2.validateIPv6ClusterAllocCIDR(), IsNil)
+	c.Assert(valid2.IPv6ClusterAllocCIDRBase, Equals, "fdfd:fdfd:fdfd:fdfd::")
+
+	invalid1 := &daemonConfig{IPv6ClusterAllocCIDR: "foo"}
+	c.Assert(invalid1.validateIPv6ClusterAllocCIDR(), Not(IsNil))
+
+	invalid2 := &daemonConfig{IPv6ClusterAllocCIDR: "fdfd"}
+	c.Assert(invalid2.validateIPv6ClusterAllocCIDR(), Not(IsNil))
+
+	invalid3 := &daemonConfig{IPv6ClusterAllocCIDR: "fdfd::/32"}
+	c.Assert(invalid3.validateIPv6ClusterAllocCIDR(), Not(IsNil))
+
+	invalid4 := &daemonConfig{}
+	c.Assert(invalid4.validateIPv6ClusterAllocCIDR(), Not(IsNil))
+}

commit df08a6f1f092e85d9ec4939594c4b8dba796fd13

node: Undo default IPv6 prefix change
Comit 7e4436b9bc44f ("agent: Add --ipv6-cluster-alloc-cidr option to specify IPv6 cluster prefix")
changed the default IPv6 prefix by accident. Undo this.

Fixes: 7e4436b9bc44f ("agent: Add --ipv6-cluster-alloc-cidr option to specify IPv6 cluster prefix")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/defaults/defaults.go
@@ -23,7 +23,7 @@ const (
 	IPv6ClusterAllocCIDR = IPv6ClusterAllocCIDRBase + "/64"
 
 	// IPv6ClusterAllocCIDRBase is the default base for IPv6ClusterAllocCIDR
-	IPv6ClusterAllocCIDRBase = "fdfd::"
+	IPv6ClusterAllocCIDRBase = "f00d::"
 
 	// RuntimePath is the default path to the runtime directory
 	RuntimePath = "/var/run/cilium"