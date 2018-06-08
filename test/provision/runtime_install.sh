99b13b739b55979fdbd75377dedbbc747ea3c1fd
Joe Stringer <joe@covalent.io>
2018-06-08 15:50:44 -0700

    test: Bump journald log ratelimit to 10000
    In common test environments, it is easy for LDS + NPDS configuration for
    ~10 endpoints to generate more than 1000 lines of log messages within a
    second. To prevent losing logs in test environments, bump this ratelimit
    to 10000.
    
    Related: #4417
    Related: #4425
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- test/provision/runtime_install.sh
+++ test/provision/runtime_install.sh
@@ -8,5 +8,13 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source "${PROVISIONSRC}/helpers.bash"
 
+echo "editing journald configuration"
+sudo bash -c "sed -i 's/RateLimitBurst=1000/RateLimitBurst=10000/' /etc/systemd/journald.conf"
+echo "restarting systemd-journald"
+sudo systemctl restart systemd-journald
+echo "getting status of systemd-journald"
+sudo service systemd-journald status
+echo "done configuring journald"
+
 "${PROVISIONSRC}"/dns.sh
 "${PROVISIONSRC}"/compile.sh
