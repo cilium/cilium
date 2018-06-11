5398b3006f81170c473a79e8660cbd4c875e9c0b
Joe Stringer <joe@covalent.io>
2018-06-11 14:46:34 -0700

    ipcache: Don't push reserved identities to kvstore
    Reserved identities such as the one for world shouldn't be pushed to the
    kvstore, as they will always be handled locally as the daemon inserts
    such IP->identity mappings into the local ipcache on startup.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- pkg/identity/identity.go
+++ pkg/identity/identity.go
@@ -105,6 +105,12 @@ func (id *Identity) GetModel() *models.Identity {
 	return ret
 }
 
+// IsReserved returns whether the identity represents a reserved identity
+// (true), or not (false).
+func (id *Identity) IsReserved() bool {
+	return reservedIdentityCache[id.ID] != nil
+}
+
 // NewIdentity creates a new identity
 func NewIdentity(id NumericIdentity, lbls labels.Labels) *Identity {
 	var lblArray labels.LabelArray
