c33f0e8629c8ef4e21e726223bb59e86ce128636
Romain Lenglet <romain@covalent.io>
2018-06-11 18:28:23 -0700

    k8s: Add label to endpoints injected with Istio + Cilium sidecar
    Signed-off-by: Romain Lenglet <romain@covalent.io>

--- pkg/workloads/kubernetes.go
+++ pkg/workloads/kubernetes.go
@@ -15,18 +15,37 @@
 package workloads
 
 import (
+	"strings"
+
 	"github.com/cilium/cilium/pkg/k8s"
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/policy"
 
 	"github.com/sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sDockerLbls "k8s.io/kubernetes/pkg/kubelet/types"
 )
 
+const (
+	// AnnotationIstioSidecarStatus is the annotation added by Istio into a pod
+	// when it is injected with a sidecar proxy.
+	// Since Istio 0.5.0, the value of this annotation is a serialized JSON object
+	// with the following structure ("imagePullSecrets" was added in Istio 0.8.0):
+	//
+	//     {
+	//         "version": "0213afe1274259d2f23feb4820ad2f8eb8609b84a5538e5f51f711545b6bde88",
+	//         "initContainers": ["sleep", "istio-init"],
+	//         "containers": ["istio-proxy"],
+	//         "volumes": ["cilium-unix-sock-dir", "istio-envoy", "istio-certs"],
+	//         "imagePullSecrets": null
+	//     }
+	AnnotationIstioSidecarStatus = "sidecar.istio.io/status"
+)
+
 // fetchK8sLabels returns the kubernetes labels from the given container labels
-func fetchK8sLabels(containerLbls map[string]string) (map[string]string, error) {
+func fetchK8sLabels(containerID string, containerLbls map[string]string) (map[string]string, error) {
 	if !k8s.IsEnabled() {
 		return nil, nil
 	}
@@ -69,5 +88,55 @@ func fetchK8sLabels(containerLbls map[string]string) (map[string]string, error) 
 		delete(k8sLabels, k8sConst.PolicyLabelServiceAccount)
 	}
 
+	// If the pod has been injected with an Istio sidecar proxy compatible with
+	// Cilium, add a label to notify that.
+	// If the pod already contains that label to explicitly enable or disable
+	// the sidecar proxy mode, keep it as is.
+	if _, ok := k8sLabels[k8sConst.PolicyLabelIstioSidecarProxy]; !ok &&
+		isInjectedWithIstioSidecarProxy(containerID, result) {
+		k8sLabels[k8sConst.PolicyLabelIstioSidecarProxy] = "true"
+	}
+
 	return k8sLabels, nil
+}
+
+// isInjectedWithIstioSidecarProxy returns whether the given pod has been
+// injected by Istio with a sidecar proxy that is compatible with Cilium.
+func isInjectedWithIstioSidecarProxy(containerID string, pod *corev1.Pod) bool {
+	scopedLog := log.WithField(logfields.ContainerID, shortContainerID(containerID))
+	istioStatusString, ok := pod.GetAnnotations()[AnnotationIstioSidecarStatus]
+	if !ok {
+		// Istio's injection annotation was not found.
+		scopedLog.Debugf("No %s annotation", AnnotationIstioSidecarStatus)
+		return false
+	}
+
+	scopedLog.Debugf("Found %s annotation with value: %s",
+		AnnotationIstioSidecarStatus, istioStatusString)
+
+	// Check that there's an "istio-proxy" container that uses an image
+	// compatible with Cilium.
+	for _, container := range pod.Spec.Containers {
+		if container.Name != "istio-proxy" {
+			continue
+		}
+		scopedLog.Debug("Found istio-proxy container in pod")
+
+		if !strings.Contains(container.Image, "cilium/istio_proxy") {
+			continue
+		}
+		scopedLog.Debugf("istio-proxy container runs Cilium-compatible image: %s", container.Image)
+
+		for _, mount := range container.VolumeMounts {
+			if mount.MountPath != "/var/run/cilium" {
+				continue
+			}
+			scopedLog.Debug("istio-proxy container has volume mounted into /var/run/cilium")
+
+			return true
+		}
+	}
+
+	scopedLog.Debug("No Cilium-compatible istio-proxy container found")
+	return false
 }
