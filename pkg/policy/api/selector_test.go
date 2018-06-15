802f5fca9549c8a9aeb9c0fad5925c06be84dcb4
Joe Stringer <joe@covalent.io>
2018-06-14 18:14:01 -0700

    api: Cache Selector in EndpointSelector on create
    Previously, the EndpointSelector would create a new k8sLabels.Selector
    every time a match occurs, which would involve validating the selector,
    running regexes, DNS FQDN checking, taking global locks and so on. This
    is not acceptable from a performance perspective: All policy computation
    is serialized. Observed in a setup where several goroutines were
    blocking on a mutex while constructing the Selector here.
    
    Instead, when creating the EndpointSelector, generate the set of
    requirements at that point in time so that the Matches() can be a set of
    simple match operations.
    
    Using microbenchmarks, before:
        $ go test -bench=Parallel -benchtime=10s
        OK: 24 passed
        goos: linux
        goarch: amd64
        pkg: github.com/cilium/cilium/pkg/policy/api
        BenchmarkMatchesValid1000Parallel-4          100         176576315 ns/op
        PASS
        ok      github.com/cilium/cilium/pkg/policy/api 26.933s
    
    After:
        $ go test -bench=Parallel -benchtime=10s
        OK: 24 passed
        goos: linux
        goarch: amd64
        pkg: github.com/cilium/cilium/pkg/policy/api
        BenchmarkMatchesValid1000Parallel-4      2000000              8036 ns/op
        PASS
        ok      github.com/cilium/cilium/pkg/policy/api 24.149s
    
    Related: #4526
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- pkg/policy/api/selector_test.go
+++ pkg/policy/api/selector_test.go
@@ -18,9 +18,13 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/cilium/cilium/pkg/comparator"
 	"github.com/cilium/cilium/pkg/labels"
 
 	. "gopkg.in/check.v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sLbls "k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
 )
 
 var _ = Suite(&PolicyAPITestSuite{})
@@ -40,6 +44,35 @@ func (s *PolicyAPITestSuite) TestSelectsAllEndpoints(c *C) {
 
 	selectorSlice = EndpointSelectorSlice{NewESFromLabels(labels.ParseSelectLabel("bar")), NewESFromLabels(labels.ParseSelectLabel("foo"))}
 	c.Assert(selectorSlice.SelectsAllEndpoints(), Equals, false)
+}
+
+func (s *PolicyAPITestSuite) TestLabelSelectorToRequirements(c *C) {
+	labelSelector := &metav1.LabelSelector{
+		MatchLabels: map[string]string{
+			"any.foo": "bar",
+			"k8s.baz": "alice",
+		},
+		MatchExpressions: []metav1.LabelSelectorRequirement{
+			{
+				Key:      "any.foo",
+				Operator: "NotIn",
+				Values:   []string{"default"},
+			},
+		},
+	}
+
+	expRequirements := k8sLbls.Requirements{}
+	req, err := k8sLbls.NewRequirement("any.foo", selection.Equals, []string{"bar"})
+	c.Assert(err, IsNil)
+	expRequirements = append(expRequirements, *req)
+	req, err = k8sLbls.NewRequirement("any.foo", selection.NotIn, []string{"default"})
+	c.Assert(err, IsNil)
+	expRequirements = append(expRequirements, *req)
+	req, err = k8sLbls.NewRequirement("k8s.baz", selection.Equals, []string{"alice"})
+	c.Assert(err, IsNil)
+	expRequirements = append(expRequirements, *req)
+
+	c.Assert(labelSelectorToRequirements(labelSelector), comparator.DeepEquals, &expRequirements)
 }
 
 func benchmarkMatchesSetup(match string, count int) (EndpointSelector, labels.LabelArray) {
