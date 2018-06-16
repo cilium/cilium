e820226cbd357ebd16087d5d8914733475c1626b
André Martins <andre@cilium.io>
2018-06-16 19:16:11 +0200

    test: update k8s to 1.8.14, 1.10.4 and 1.11.0-rc.1
    Signed-off-by: André Martins <andre@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -91,7 +91,7 @@ case $K8S_VERSION in
         ;;
     "1.8")
         KUBERNETES_CNI_VERSION="0.5.1-00"
-        K8S_FULL_VERSION="1.8.13"
+        K8S_FULL_VERSION="1.8.14"
         ;;
     "1.9")
         KUBERNETES_CNI_VERSION="0.6.0-00"
@@ -101,12 +101,12 @@ case $K8S_VERSION in
         ;;
     "1.10")
         KUBERNETES_CNI_VERSION="0.6.0-00"
-        K8S_FULL_VERSION="1.10.3"
+        K8S_FULL_VERSION="1.10.4"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri"
         ;;
     "1.11")
         KUBERNETES_CNI_VERSION="v0.6.0"
-        K8S_FULL_VERSION="1.11.0-beta.0"
+        K8S_FULL_VERSION="1.11.0-rc.1"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri"
         INSTALL_KUBEDNS=0
