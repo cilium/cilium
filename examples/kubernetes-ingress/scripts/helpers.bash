c925de4905a4dd0292742796e9bce15ac9622a98
André Martins <andre@cilium.io>
2018-06-18 15:52:41 +0200

    examples/kubernetes-ingress: update kubernetes options to 1.11.0
    Signed-off-by: André Martins <andre@cilium.io>

--- examples/kubernetes-ingress/scripts/helpers.bash
+++ examples/kubernetes-ingress/scripts/helpers.bash
@@ -75,7 +75,7 @@ cluster_api_server_ip=${K8S_CLUSTER_API_SERVER_IP:-"172.20.0.1"}
 #cluster_dns_ip=${K8S_CLUSTER_DNS_IP:-"FD03::A"}
 #cluster_api_server_ip=${K8S_CLUSTER_API_SERVER_IP:-"FD03::1"}
 
-k8s_version="v1.10.2"
+k8s_version="v1.11.0"
 etcd_version="v3.2.7"
 
 function restore_flag {
