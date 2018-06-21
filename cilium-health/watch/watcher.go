9a239c6ad995d448387f53f50d8fa0cac86d982b
Michal Rostecki <mrostecki@suse.com>
2018-06-21 16:28:07 +0200

    metrics: Add cilium-health metrics about latency
    This change adds the Prometheus metrics server for cilium-health
    daemon.
    
    Currently supported metric is a histogram with information about
    latency to nodes and endpoints.
    
    Fixes #4267
    
    Signed-off-by: Michal Rostecki <mrostecki@suse.com>

--- cilium-health/watch/watcher.go
+++ cilium-health/watch/watcher.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2017-2018 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,15 +22,16 @@ import (
 	ciliumPkg "github.com/cilium/cilium/pkg/client"
 	healthPkg "github.com/cilium/cilium/pkg/health/client"
 	"github.com/cilium/cilium/pkg/health/defaults"
-	"github.com/cilium/cilium/pkg/launcher"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
-// CiliumHealth is used to wrap the node executable binary.
+// CiliumHealth is used to watch the cilium-health deamon by communication
+// through client.
 type CiliumHealth struct {
-	launcher.Launcher
 	client *healthPkg.Client
+	mutex  lock.RWMutex
 	status *models.Status
 }
 
@@ -41,13 +42,8 @@ var (
 	statusProbeInterval  = 5 * time.Second
 )
 
-const targetName = "cilium-health"
-
-// Run launches the cilium-health daemon.
-func (ch *CiliumHealth) Run() {
-	ch.SetTarget(targetName)
-	ch.SetArgs([]string{"-d"})
-
+// Watch watches the cilium-health daemon.
+func (ch *CiliumHealth) Watch() {
 	// Wait until Cilium API is available
 	for {
 		cli, err := ciliumPkg.NewDefaultClient()
@@ -64,10 +60,9 @@ func (ch *CiliumHealth) Run() {
 		var err error
 
 		os.Remove(defaults.SockPath)
-		ch.Launcher.Run()
 		ch.client, err = healthPkg.NewDefaultClient()
 		if err != nil {
-			log.WithError(err).Infof("Cannot establish connection to local %s instance", targetName)
+			log.WithError(err).Infof("Cannot establish connection to local cilium-health instance")
 			time.Sleep(connectRetryInterval)
 			continue
 		}
@@ -88,15 +83,15 @@ func (ch *CiliumHealth) Run() {
 
 // GetStatus returns the status of the cilium-health daemon.
 func (ch *CiliumHealth) GetStatus() *models.Status {
-	ch.Mutex.RLock()
+	ch.mutex.RLock()
 	status := ch.status
-	ch.Mutex.RUnlock()
+	ch.mutex.RUnlock()
 	return status
 }
 
 // setStatus updates the status of the cilium-health daemon.
 func (ch *CiliumHealth) setStatus(status *models.Status) {
-	ch.Mutex.Lock()
+	ch.mutex.Lock()
 	ch.status = status
-	ch.Mutex.Unlock()
+	ch.mutex.Unlock()
 }
