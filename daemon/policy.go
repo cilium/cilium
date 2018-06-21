cilium/cilium

commit 4692732a789f38f222b1d3affc661f85eeb8be4c

daemon: Allocate identities for CIDRs
For each unique CIDR that is configured via policy, we allocate an
Identity. These CIDR -> Identity mappings are then stored in the ipcache
in the kvstore so that we can perform LPM CIDR -> Identity mapping.

With these things in place, the core of the Cilium userspace and
datapath code can use simple IP (CIDR) -> Identity mapping, then
Identity-based policy for allowing or denying traffic to/from those
CIDRs. This greatly simplifies the handling of CIDRs throughout the
codebase.

Signed-off-by: Joe Stringer <joe@covalent.io>

daemon/policy.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Authors of Cilium
+// Copyright 2016-2018 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,6 +25,8 @@ import (
 	"github.com/cilium/cilium/pkg/apierror"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
@@ -33,6 +35,7 @@ import (
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/op/go-logging"
+	"github.com/sirupsen/logrus"
 )
 
 // TriggerPolicyUpdates triggers policy updates for every daemon's endpoint.
@@ -222,8 +225,28 @@ func (d *Daemon) policyAdd(rules api.Rules, opts *AddOptions) (uint64, error) {
 func (d *Daemon) PolicyAdd(rules api.Rules, opts *AddOptions) (uint64, error) {
 	log.WithField(logfields.CiliumNetworkPolicy, logfields.Repr(rules)).Debug("Policy Add Request")
 
+	prefixes := policy.GetCIDRPrefixes(rules)
+	log.WithField("prefixes", prefixes).Debug("Policy imported via API, found CIDR prefixes...")
+
+	prefixIdentities, err := identity.AllocateCIDRIdentities(prefixes)
+	if err != nil {
+		metrics.PolicyImportErrors.Inc()
+		return d.policy.GetRevision(), err
+	}
+
+	if err = ipcache.UpsertIPNetsToKVStore(prefixes, prefixIdentities); err != nil {
+		metrics.PolicyImportErrors.Inc()
+
+		// Failed to update Prefix->ID mappings; don't leak identities
+		identity.ReleaseSlice(prefixIdentities)
+		return d.policy.GetRevision(), err
+	}
+
 	rev, err := d.policyAdd(rules, opts)
 	if err != nil {
+		// Don't leak identities allocated above.
+		identity.ReleaseSlice(prefixIdentities)
+
 		return 0, apierror.Error(PutPolicyFailureCode, err)
 	}
 
@@ -243,12 +266,49 @@ func (d *Daemon) PolicyAdd(rules api.Rules, opts *AddOptions) (uint64, error) {
 func (d *Daemon) PolicyDelete(labels labels.LabelArray) (uint64, error) {
 	log.WithField(logfields.IdentityLabels, logfields.Repr(labels)).Debug("Policy Delete Request")
 
-	// An error is only returned if a label filter was provided and then
-	// not found. A deletion request for all policy entries should not fail
-	// if no policies are loaded.
-	rev, deleted := d.policy.DeleteByLabels(labels)
-	if deleted == 0 && len(labels) != 0 {
+	d.policy.Mutex.Lock()
+
+	// First, find rules by the label. We'll use this set of rules to
+	// determine which CIDR identities that we need to release.
+	rules := d.policy.SearchRLocked(labels)
+
+	// Return an error if a label filter was provided and there are no
+	// rules matching it. A deletion request for all policy entries should
+	// not fail if no policies are loaded.
+	if len(rules) == 0 && len(labels) != 0 {
+		rev := d.policy.GetRevision()
+		d.policy.Mutex.Unlock()
 		return rev, apierror.New(DeletePolicyNotFoundCode, "policy not found")
+	}
+	rev, _ := d.policy.DeleteByLabelsLocked(labels)
+	d.policy.Mutex.Unlock()
+
+	// Now that the policies are deleted, we can also attempt to remove
+	// all CIDR identities referenced by the deleted rules.
+	//
+	// We don't treat failures to clean up identities as API failures,
+	// because the policy can still successfully be updated. We're just
+	// not appropriately performing garbage collection.
+	prefixes := policy.GetCIDRPrefixes(rules)
+	log.WithField("prefixes", prefixes).Debug("Policy deleted via API, found prefixes...")
+
+	prefixIdentities, err := identity.LookupCIDRIdentities(prefixes)
+	if err == nil {
+		if err = identity.ReleaseSlice(prefixIdentities); err != nil {
+			log.WithError(err).WithFields(logrus.Fields{
+				logfields.Labels: labels,
+			}).Debug("Cannot delete identities for CIDR prefixes by labels")
+		}
+
+		if err = ipcache.DeleteIPNetsFromKVStore(prefixes); err != nil {
+			log.WithError(err).WithFields(logrus.Fields{
+				logfields.Labels: labels,
+			}).Debug("Could not delete prefix->Identity mappings during policy delete")
+		}
+	} else {
+		log.WithError(err).WithFields(logrus.Fields{
+			logfields.Labels: labels,
+		}).Debug("Cannot find identities for CIDR prefixes by labels")
 	}
 
 	d.TriggerPolicyUpdates(false)

pkg/ipcache/ipcache.go
@@ -98,6 +98,22 @@ func (ipc *IPCache) RUnlock() {
 	ipc.mutex.RUnlock()
 }
 
+func upsertToKVStore(ipKey string, ipIDPair identity.IPIdentityPair) error {
+	marshaledIPIDPair, err := json.Marshal(ipIDPair)
+	if err != nil {
+		return err
+	}
+
+	log.WithFields(logrus.Fields{
+		logfields.IPAddr:       ipIDPair.IP,
+		logfields.IPMask:       ipIDPair.Mask,
+		logfields.Identity:     ipIDPair.ID,
+		logfields.Modification: Upsert,
+	}).Debug("upserting IP->ID mapping to kvstore")
+
+	return kvstore.Update(ipKey, marshaledIPIDPair, true)
+}
+
 // UpsertIPToKVStore updates / inserts the provided IP->Identity mapping into the
 // kvstore, which will subsequently trigger an event in ipIdentityWatcher().
 func UpsertIPToKVStore(IP net.IP, ID identity.NumericIdentity, metadata string) error {
@@ -108,18 +124,50 @@ func UpsertIPToKVStore(IP net.IP, ID identity.NumericIdentity, metadata string) 
 		Metadata: metadata,
 	}
 
-	marshaledIPIDPair, err := json.Marshal(ipIDPair)
-	if err != nil {
-		return err
+	return upsertToKVStore(ipKey, ipIDPair)
+}
+
+// UpsertIPNetToKVStore updates / inserts the provided CIDR->Identity mapping
+// into the kvstore, which will subsequently trigger an event in
+// ipIdentityWatcher().
+func UpsertIPNetToKVStore(prefix *net.IPNet, ID *identity.Identity) error {
+	ipKey := path.Join(IPIdentitiesPath, AddressSpace, prefix.String())
+	ipIDPair := identity.IPIdentityPair{
+		IP:       prefix.IP,
+		Mask:     prefix.Mask,
+		ID:       ID.ID,
+		Metadata: AddressSpace, // XXX: Should we associate more metadata?
 	}
 
-	log.WithFields(logrus.Fields{
-		logfields.IPAddr:       ipIDPair.IP,
-		logfields.Identity:     ipIDPair.ID,
-		logfields.Modification: Upsert,
-	}).Debug("upserting IP->ID mapping to kvstore")
+	return upsertToKVStore(ipKey, ipIDPair)
+}
 
-	return kvstore.Update(ipKey, marshaledIPIDPair, true)
+// UpsertIPNetsToKVStore inserts a CIDR->Identity mapping into the kvstore
+// ipcache for each of the specified prefixes and identities. That is to say,
+// prefixes[0] is mapped to identities[0].
+//
+// If any Prefix->Identity mapping cannot be created, it will not create any
+// of the mappings and returns an error.
+func UpsertIPNetsToKVStore(prefixes []*net.IPNet, identities []*identity.Identity) (err error) {
+	if len(prefixes) != len(identities) {
+		return fmt.Errorf("Invalid []Prefix->[]Identity ipcache mapping requested: prefixes=%d identities=%d", len(prefixes), len(identities))
+	}
+	for i, prefix := range prefixes {
+		id := identities[i]
+		err = UpsertIPNetToKVStore(prefix, id)
+		if err != nil {
+			for j := 0; j < i; j++ {
+				err2 := DeleteIPFromKVStore(prefix.String())
+				if err2 != nil {
+					log.WithFields(logrus.Fields{
+						"prefix": prefix.String(),
+					}).Error("Failed to clean up CIDR->ID mappings")
+				}
+			}
+		}
+	}
+
+	return err
 }
 
 // DeleteIPFromKVStore removes the IP->Identity mapping for the specified ip from the
@@ -127,6 +175,22 @@ func UpsertIPToKVStore(IP net.IP, ID identity.NumericIdentity, metadata string) 
 func DeleteIPFromKVStore(ip string) error {
 	ipKey := path.Join(IPIdentitiesPath, AddressSpace, ip)
 	return kvstore.Delete(ipKey)
+}
+
+// DeleteIPNetsFromKVStore removes the Prefix->Identity mappings for the
+// specified slice of prefixes from the kvstore, which will subsequently
+// trigger an event in ipIdentityWatcher().
+func DeleteIPNetsFromKVStore(prefixes []*net.IPNet) (err error) {
+	for _, prefix := range prefixes {
+		if err2 := DeleteIPFromKVStore(prefix.String()); err2 != nil {
+			err = err2
+			log.WithFields(logrus.Fields{
+				"prefix": prefix.String(),
+			}).Error("Failed to delete CIDR->ID mappings")
+		}
+	}
+
+	return
 }
 
 // refPrefixLength adds one reference to the prefix length in the map.

pkg/identity/cidr.go
@@ -1,0 +1,92 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package identity
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/labels/cidr"
+)
+
+// AllocateCIDRIdentities allocates identities for each of the specified CIDR
+// prefixes so they will be available later during policy resolution.
+//
+// On success, the returned slice will have a 1-to-1 correspondence with the
+// CIDR in the provided prefixes slice, eg the identity for prefixes[0] will
+// be held in identities[0].
+// On failure, attempts to clean up after itself and returns the error.
+func AllocateCIDRIdentities(prefixes []*net.IPNet) (res []*Identity, err error) {
+	if len(prefixes) == 0 {
+		return nil, nil
+	}
+
+	log.Debugf("Attempting to allocate identities for %d prefixes", len(prefixes))
+
+	// Allocate identities for each CIDR.
+	var lbls labels.Labels
+	res = make([]*Identity, 0, len(prefixes))
+	for _, prefix := range prefixes {
+		if prefix == nil {
+			continue
+		}
+		lbls := cidr.GetCIDRLabels(prefix)
+		id, _, err := AllocateIdentity(lbls)
+		if err != nil {
+			// If any identity allocation failed, release existing identities
+			// and log the error.
+			if err2 := ReleaseSlice(res); err2 != nil {
+				log.WithError(err2).Error("Could not recover from error during CIDR identity allocation")
+			}
+			res = nil
+			break
+		}
+		res = append(res, id)
+	}
+
+	if err == nil {
+		log.Debugf("Allocated identities for %d prefixes", len(res))
+	} else {
+		log.WithError(err).Warningf("Failed to allocate identities for %d prefixes", len(res))
+	}
+
+	if err != nil {
+		err = fmt.Errorf("Failed to allocate identity for %s: %s", lbls.String(), err)
+	}
+	return
+}
+
+// LookupCIDRIdentities finds identities corresponding to each of the specified
+// prefixes. It expects to be able to find all identities.
+// On success, returns a slice of identities with a 1-to-1 correspondence to
+// the specified slice of prefixes, and nil.
+// On error, returns all identities that can be resolved and an error.
+func LookupCIDRIdentities(prefixes []*net.IPNet) (res []*Identity, err error) {
+	res = make([]*Identity, 0, len(prefixes))
+
+	for _, prefix := range prefixes {
+		labels := cidr.GetCIDRLabels(prefix)
+		id, err2 := identityAllocator.Get(globalIdentity{labels})
+		if err2 == nil {
+			res = append(res, NewIdentity(NumericIdentity(id), labels))
+		} else {
+			err = err2
+			log.Infof("Cannot locate identity for CIDR %s", prefix.String())
+		}
+	}
+
+	return res, err
+}

pkg/identity/allocator.go
@@ -120,3 +120,19 @@ func AllocateIdentity(lbls labels.Labels) (*Identity, bool, error) {
 func (id *Identity) Release() error {
 	return identityAllocator.Release(globalIdentity{id.Labels})
 }
+
+// ReleaseSlice attempts to release a set of identities. It is a helper
+// function that may be useful for cleaning up multiple identities in paths
+// where several identities may be allocated and another error means that they
+// should all be released.
+func ReleaseSlice(identities []*Identity) error {
+	var err error
+	for _, id := range identities {
+		if err = id.Release(); err != nil {
+			log.WithFields(logrus.Fields{
+				logfields.Identity: id,
+			}).Error("Failed to release identity")
+		}
+	}
+	return err
+}

commit 37ad35f208f5208419ef8a558b4d16277c8476de

daemon: Release ipcache entries on policy add failure
If adding a policy failed, then we previously would release the
identities that were allocated, but leave the ipcache mappings in the
kvstore for those CIDR->ID pairs. Release the ipcache mappings as well.

If some of the identities cannot be located in the kvstore, then none of
the identities or ipcache entries would be deleted when removing a
policy. Fix this up to instead attempt to locate as many of the entries
as possible, then free as many as can be located.

Fixes: 4692732a789f ("daemon: Allocate identities for CIDRs")

Signed-off-by: Joe Stringer <joe@covalent.io>

daemon/policy.go
@@ -25,7 +25,6 @@ import (
 	"github.com/cilium/cilium/pkg/apierror"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
-	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -38,7 +37,6 @@ import (
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/op/go-logging"
-	"github.com/sirupsen/logrus"
 )
 
 // TriggerPolicyUpdates triggers policy updates for every daemon's endpoint.
@@ -237,28 +235,20 @@ func (d *Daemon) PolicyAdd(rules api.Rules, opts *AddOptions) (uint64, error) {
 	prefixes := policy.GetCIDRPrefixes(rules)
 	log.WithField("prefixes", prefixes).Debug("Policy imported via API, found CIDR prefixes...")
 
-	prefixIdentities, err := identity.AllocateCIDRIdentities(prefixes)
-	if err != nil {
+	if err := ipcache.AllocateCIDRs(bpfIPCache.IPCache, prefixes); err != nil {
 		metrics.PolicyImportErrors.Inc()
-		return d.policy.GetRevision(), err
-	}
-
-	if err = ipcache.CheckPrefixes(bpfIPCache.IPCache, prefixes); err == nil {
-		err = ipcache.UpsertIPNetsToKVStore(prefixes, prefixIdentities)
-	}
-	if err != nil {
-		metrics.PolicyImportErrors.Inc()
-
-		// Failed to update Prefix->ID mappings; don't leak identities
-		identity.ReleaseSlice(prefixIdentities)
+		log.WithError(err).WithField("prefixes", prefixes).Warning(
+			"Failed to allocate identities for CIDRs during policy add")
 		return d.policy.GetRevision(), err
 	}
 
 	rev, err := d.policyAdd(rules, opts)
 	if err != nil {
 		// Don't leak identities allocated above.
-		identity.ReleaseSlice(prefixIdentities)
-
+		if err2 := ipcache.ReleaseCIDRs(prefixes); err2 != nil {
+			log.WithError(err2).WithField("prefixes", prefixes).Debug(
+				"Failed to release CIDRs during policy import failure")
+		}
 		return 0, apierror.Error(PutPolicyFailureCode, err)
 	}
 
@@ -310,24 +300,9 @@ func (d *Daemon) PolicyDelete(labels labels.LabelArray) (uint64, error) {
 	// not appropriately performing garbage collection.
 	prefixes := policy.GetCIDRPrefixes(rules)
 	log.WithField("prefixes", prefixes).Debug("Policy deleted via API, found prefixes...")
-
-	prefixIdentities, err := identity.LookupCIDRIdentities(prefixes)
-	if err == nil {
-		if err = identity.ReleaseSlice(prefixIdentities); err != nil {
-			log.WithError(err).WithFields(logrus.Fields{
-				logfields.Labels: labels,
-			}).Debug("Cannot delete identities for CIDR prefixes by labels")
-		}
-
-		if err = ipcache.DeleteIPNetsFromKVStore(prefixes); err != nil {
-			log.WithError(err).WithFields(logrus.Fields{
-				logfields.Labels: labels,
-			}).Debug("Could not delete prefix->Identity mappings during policy delete")
-		}
-	} else {
-		log.WithError(err).WithFields(logrus.Fields{
-			logfields.Labels: labels,
-		}).Debug("Cannot find identities for CIDR prefixes by labels")
+	if err := ipcache.ReleaseCIDRs(prefixes); err != nil {
+		log.WithError(err).WithField("prefixes", prefixes).Warning(
+			"Failed to release CIDRs during policy delete")
 	}
 
 	d.TriggerPolicyUpdates(false)

pkg/ipcache/cidr.go
@@ -1,0 +1,86 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ipcache
+
+import (
+	"net"
+
+	"github.com/cilium/cilium/pkg/identity"
+
+	"github.com/sirupsen/logrus"
+)
+
+// AllocateCIDRs attempts to allocate identities and IP<->Identity mappings for
+// the specified CIDR prefixes. If any allocation fails, all allocations are
+// rolled back and the error is returned. Returns nil on success.
+func AllocateCIDRs(impl Implementation, prefixes []*net.IPNet) error {
+	// First, if the implementation will complain, exit early.
+	if err := checkPrefixes(impl, prefixes); err != nil {
+		return err
+	}
+
+	// Next, allocate labels -> ID mappings in KVstore (for policy)
+	prefixIdentities, err := identity.AllocateCIDRIdentities(prefixes)
+	if err != nil {
+		return err
+	}
+
+	// Finally, allocate CIDR -> ID mappings in KVstore (for ipcache)
+	err = upsertIPNetsToKVStore(prefixes, prefixIdentities)
+	if err != nil {
+		if err2 := identity.ReleaseSlice(prefixIdentities); err2 != nil {
+			log.WithError(err2).WithFields(logrus.Fields{
+				"identities": prefixIdentities,
+			}).Debug("Failed during CIDR allocation")
+		}
+	}
+
+	return err
+}
+
+// ReleaseCIDRs attempts to release identities and IP<->Identity mappings for
+// the specified CIDR prefixes. If any release fails, all remaining prefixes
+// will be attempted to be rolled back and an error is returned representing
+// the most recent error. Returns nil if no errors occur.
+func ReleaseCIDRs(prefixes []*net.IPNet) (err error) {
+	scopedLog := log.WithField("prefixes", prefixes)
+	if prefixes != nil {
+		if err = deleteIPNetsFromKVStore(prefixes); err != nil {
+			scopedLog.WithError(err).Debug(
+				"Failed to release CIDR->Identity mappings")
+		}
+	}
+
+	prefixIdentities, err2 := identity.LookupCIDRIdentities(prefixes)
+	if err2 != nil {
+		if err == nil {
+			err = err2
+		}
+		scopedLog.WithError(err2).Debug(
+			"Could not locate identities for CIDRs during release")
+	}
+	if prefixIdentities != nil {
+		if err2 = identity.ReleaseSlice(prefixIdentities); err2 != nil {
+			if err == nil {
+				err = err2
+			}
+			log.WithError(err2).WithFields(logrus.Fields{
+				"identities": prefixIdentities,
+			}).Debug("Failed to release Identities for CIDRs")
+		}
+	}
+
+	return err
+}

pkg/ipcache/ipcache.go
@@ -133,10 +133,10 @@ func UpsertIPToKVStore(IP net.IP, ID identity.NumericIdentity, metadata string) 
 	return upsertToKVStore(ipKey, ipIDPair)
 }
 
-// UpsertIPNetToKVStore updates / inserts the provided CIDR->Identity mapping
+// upsertIPNetToKVStore updates / inserts the provided CIDR->Identity mapping
 // into the kvstore, which will subsequently trigger an event in
 // ipIdentityWatcher().
-func UpsertIPNetToKVStore(prefix *net.IPNet, ID *identity.Identity) error {
+func upsertIPNetToKVStore(prefix *net.IPNet, ID *identity.Identity) error {
 	// Reserved identities are handled locally, don't push them to kvstore.
 	if ID.IsReserved() {
 		return nil
@@ -176,10 +176,10 @@ func checkPrefixLengthsAgainstMap(impl Implementation, prefixes []*net.IPNet, ex
 	return nil
 }
 
-// CheckPrefixes ensures that we will reject rules if the import of those
+// checkPrefixes ensures that we will reject rules if the import of those
 // rules would cause the underlying implementation of the ipcache to exceed
 // the maximum number of supported CIDR prefix lengths.
-func CheckPrefixes(impl Implementation, prefixes []*net.IPNet) (err error) {
+func checkPrefixes(impl Implementation, prefixes []*net.IPNet) (err error) {
 	IPIdentityCache.RLock()
 	defer IPIdentityCache.RUnlock()
 
@@ -189,7 +189,7 @@ func CheckPrefixes(impl Implementation, prefixes []*net.IPNet) (err error) {
 	return checkPrefixLengthsAgainstMap(impl, prefixes, IPIdentityCache.v6PrefixLengths)
 }
 
-// UpsertIPNetsToKVStore inserts a CIDR->Identity mapping into the kvstore
+// upsertIPNetsToKVStore inserts a CIDR->Identity mapping into the kvstore
 // ipcache for each of the specified prefixes and identities. That is to say,
 // prefixes[0] is mapped to identities[0].
 //
@@ -198,13 +198,13 @@ func CheckPrefixes(impl Implementation, prefixes []*net.IPNet) (err error) {
 //
 // The caller should check the prefix lengths against the underlying IPCache
 // implementation using CheckPrefixLengths prior to upserting to the kvstore.
-func UpsertIPNetsToKVStore(prefixes []*net.IPNet, identities []*identity.Identity) (err error) {
+func upsertIPNetsToKVStore(prefixes []*net.IPNet, identities []*identity.Identity) (err error) {
 	if len(prefixes) != len(identities) {
 		return fmt.Errorf("Invalid []Prefix->[]Identity ipcache mapping requested: prefixes=%d identities=%d", len(prefixes), len(identities))
 	}
 	for i, prefix := range prefixes {
 		id := identities[i]
-		err = UpsertIPNetToKVStore(prefix, id)
+		err = upsertIPNetToKVStore(prefix, id)
 		if err != nil {
 			for j := 0; j < i; j++ {
 				err2 := DeleteIPFromKVStore(prefix.String())
@@ -227,10 +227,10 @@ func DeleteIPFromKVStore(ip string) error {
 	return kvstore.Delete(ipKey)
 }
 
-// DeleteIPNetsFromKVStore removes the Prefix->Identity mappings for the
+// deleteIPNetsFromKVStore removes the Prefix->Identity mappings for the
 // specified slice of prefixes from the kvstore, which will subsequently
 // trigger an event in ipIdentityWatcher().
-func DeleteIPNetsFromKVStore(prefixes []*net.IPNet) (err error) {
+func deleteIPNetsFromKVStore(prefixes []*net.IPNet) (err error) {
 	for _, prefix := range prefixes {
 		if err2 := DeleteIPFromKVStore(prefix.String()); err2 != nil {
 			err = err2