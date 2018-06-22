d23c39f0f7ab91cbbc7f23b927fe062f2f31fd36
Joe Stringer <joe@covalent.io>
2018-06-21 21:56:51 -0700

    daemon: Refactor ipcache CIDR allocations to ipcache
    [ upstream commit 9a3037232fed19fc165985410e37fadf544c8586 ]
    
    Signed-off-by: Joe Stringer <joe@covalent.io>
    Signed-off-by: Ray Bejjani <ray@covalent.io>

--- daemon/policy.go
+++ daemon/policy.go
@@ -249,9 +249,6 @@ func (d *Daemon) PolicyAdd(rules api.Rules, opts *AddOptions) (uint64, error) {
 			log.WithError(err2).WithField("prefixes", prefixes).Warn(
 				"Failed to release CIDRs during policy import failure")
 		}
-		ipcache.DeleteIPNetsFromKVStore(prefixes)
-		identity.ReleaseSlice(prefixIdentities)
-
 		return 0, apierror.Error(PutPolicyFailureCode, err)
 	}
 
@@ -303,25 +300,9 @@ func (d *Daemon) PolicyDelete(labels labels.LabelArray) (uint64, error) {
 	// not appropriately performing garbage collection.
 	prefixes := policy.GetCIDRPrefixes(rules)
 	log.WithField("prefixes", prefixes).Debug("Policy deleted via API, found prefixes...")
-	if prefixes != nil {
-		if err := ipcache.DeleteIPNetsFromKVStore(prefixes); err != nil {
-			log.WithError(err).WithFields(logrus.Fields{
-				logfields.Labels: labels,
-			}).Debug("Could not delete prefix->Identity mappings during policy delete")
-		}
-	}
-
-	prefixIdentities, err := cidr.LookupCIDRIdentities(prefixes)
-	if err != nil {
-		log.WithError(err).Debug(
-			"Could not find identities for CIDRs during release")
-	}
-	if prefixIdentities != nil {
-		if err = identity.ReleaseSlice(prefixIdentities); err != nil {
-			log.WithError(err).WithFields(logrus.Fields{
-				logfields.Labels: labels,
-			}).Debug("Cannot delete identities for CIDR prefixes by labels")
-		}
+	if err := ipcache.ReleaseCIDRs(prefixes); err != nil {
+		log.WithError(err).WithField("prefixes", prefixes).Warn(
+			"Failed to release CIDRs during policy delete")
 	}
 
 	d.TriggerPolicyUpdates(false)
