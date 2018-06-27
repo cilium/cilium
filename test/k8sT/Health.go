a5f9c9782e9adba94af2479f5eefad6b775ec057
Eloy Coto <eloy.coto@gmail.com>
2018-06-27 11:52:26 +0200

    Test: Refactor health test
    On `healthTest` the use of `jq` command didn't show the full json output,
    and it was hard to know the status. Move the execution to use
    `cmdRes.Filter` so the health data will be dumped into `test-output.log`
    
    The current output/log that we have is the following:
    
    ```
    /home/jenkins/workspace/Ginkgo-CI-Tests-Pipeline/src/github.com/cilium/cilium/test/ginkgo-ext/scopes.go:376
    Expected
        <string>: "Connection timed out"
    
    to contain substring
        <string>: null
    /home/jenkins/workspace/Ginkgo-CI-Tests-Pipeline/src/github.com/cilium/cilium/test/k8sT/Health.go:109
    ```
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- test/k8sT/Health.go
+++ test/k8sT/Health.go
@@ -97,17 +97,19 @@ var _ = Describe(testName, func() {
 		apiPaths := []string{
 			"endpoint.icmp",
 			"endpoint.http",
-			"host.\"primary-address\".icmp",
-			"host.\"primary-address\".http",
+			"host.primary-address.icmp",
+			"host.primary-address.http",
 		}
 		for node := 0; node <= 1; node++ {
+			healthCmd := "cilium-health status -o json"
+			status := kubectl.CiliumExec(cilium1, healthCmd)
+			status.ExpectSuccess("Cannot retrieve health status")
 			for _, path := range apiPaths {
-				jqArg := fmt.Sprintf(".nodes[%d].%s.status", node, path)
-				By("checking API response for '%s'", jqArg)
-				healthCmd := fmt.Sprintf("cilium-health status -o json | jq '%s'", jqArg)
-				status := kubectl.CiliumExec(cilium1, healthCmd)
-				Expect(status.Output().String()).Should(ContainSubstring("null"))
-				status.ExpectSuccess()
+				filter := fmt.Sprintf("{.nodes[%d].%s.status}", node, path)
+				By("checking API response for %q", filter)
+				data, err := status.Filter(filter)
+				Expect(err).To(BeNil(), "cannot retrieve filter %q from health output", filter)
+				Expect(data.String()).Should(BeEmpty())
 			}
 		}
 	}, 30)
