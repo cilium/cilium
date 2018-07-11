b723e3691215ccf5967571fef03e99a2d36bde3d
André Martins <andre@cilium.io>
2018-07-11 22:39:39 +0200

    pkg/endpointmanager: remove useless endpoint RLock
    Since e.Opts field is never changed and it already contains a lock for
    itself there is no need to lock the endpoint to read from e.Opts.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/endpointmanager/manager.go
+++ pkg/endpointmanager/manager.go
@@ -301,10 +301,7 @@ func TriggerPolicyUpdates(owner endpoint.Owner, force bool) *sync.WaitGroup {
 func HasGlobalCT() bool {
 	eps := GetEndpoints()
 	for _, e := range eps {
-		e.RLock()
-		globalCT := !e.Options.IsEnabled(option.ConntrackLocal)
-		e.RUnlock()
-		if globalCT {
+		if !e.Options.IsEnabled(option.ConntrackLocal) {
 			return true
 		}
 	}
