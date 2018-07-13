cilium/cilium

commit 7ccf87701fc9daba1cba018a98da0b4001813da3

kvstore: New kvstore abstraction API
 - Basic lockless operations:
    - Get, GetPrefix, ListPrefix, Set, Delete, DeletePrefix, CreateOnly
    - ListAndWatch, CreateLease, KeepAlive
 - Advanced operations (etcd: lockless, consul: locking required)
    - CreateIfExists
 - Unit tests automatically test etcd and consul backends in single run,
   no need for special make targets anymore
 - Abstracted encoding of keys to allow for binary keys

 - New generic allocator:
   - Maps keys to identifiers using lockless operations in the fast path
   - Uses TTLs to protect reference counting keys in case the node
     disappears
   - Utilizes garbage collector which uses distribtued locks to release
     unused identities
   - Local cache of all identities and keys for fast allocation and
     retrieval
   - Local reference counting if key->id mapping is used multiple times
     from a single node
   - Prepared to support lockless operations with etcd 3.3

Fixes: #915
Fixes: #916
Fixes: #2629

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/endpoint/endpoint.go
@@ -32,6 +32,7 @@ import (
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
 	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/labels"
 	pkgLabels "github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -200,9 +201,6 @@ const (
 	PolicyGlobalMapName = "cilium_policy"
 )
 
-// LabelsMap holds mapping from numeric policy identity to labels
-type LabelsMap map[policy.NumericIdentity]pkgLabels.LabelArray
-
 // Endpoint represents a container or similar which can be individually
 // addresses on L3 with its own IP addresses. This structured is managed by the
 // endpoint manager in pkg/endpointmanager.
@@ -246,6 +244,10 @@ type Endpoint struct {
 	// FIXME: Rename this field to Labels
 	OpLabels pkgLabels.OpLabels
 
+	// identityRevision is incremented each time the identity label
+	// information of the endpoint has changed
+	identityRevision int
+
 	// LXCMAC is the MAC address of the endpoint
 	//
 	// FIXME: Rename this field to MAC
@@ -269,7 +271,7 @@ type Endpoint struct {
 	LabelsHash string
 
 	// LabelsMap is the Set of all security labels used in the last policy computation
-	LabelsMap *LabelsMap
+	LabelsMap *policy.IdentityCache
 
 	// PortMap is port mapping configuration of the endpoint
 	PortMap []PortMap // Port mapping used for this endpoint.
@@ -679,9 +681,7 @@ func (e *Endpoint) GetLabelsSHA() string {
 		return ""
 	}
 
-	e.SecLabel.LabelsSHA256 = e.SecLabel.Labels.SHA256Sum()
-
-	return e.SecLabel.LabelsSHA256
+	return e.SecLabel.GetLabelsSHA256()
 }
 
 // GetIPv4Address returns the IPv4 address of the endpoint
@@ -1256,9 +1256,7 @@ func (e *Endpoint) HasLabels(l pkgLabels.Labels) bool {
 	return true
 }
 
-// UpdateOrchInformationLabels updates orchestration labels for the endpoint which
-// are not used in determining the security identity for the endpoint.
-func (e *Endpoint) UpdateOrchInformationLabels(l pkgLabels.Labels) {
+func (e *Endpoint) replaceInformationLabels(l pkgLabels.Labels) {
 	e.Mutex.Lock()
 	for k, v := range l {
 		tmp := v.DeepCopy()
@@ -1268,11 +1266,10 @@ func (e *Endpoint) UpdateOrchInformationLabels(l pkgLabels.Labels) {
 	e.Mutex.Unlock()
 }
 
-// UpdateOrchIdentityLabels updates orchestration labels for the endpoint which
-// are used in determining the security identity for the endpoint.
-//
-// Note: Must be called with endpoint.Mutex held!
-func (e *Endpoint) UpdateOrchIdentityLabels(l pkgLabels.Labels) bool {
+// replaceIdentityLabels replaces the identity labels of an endpoint. If a net
+// changed occurred, the identityRevision is bumped and return, otherwise 0 is
+// returned.
+func (e *Endpoint) replaceIdentityLabels(l pkgLabels.Labels) int {
 	e.Mutex.Lock()
 	changed := false
 
@@ -1298,14 +1295,23 @@ func (e *Endpoint) UpdateOrchIdentityLabels(l pkgLabels.Labels) bool {
 	if e.OpLabels.OrchestrationIdentity.DeleteMarked() || e.OpLabels.Disabled.DeleteMarked() {
 		changed = true
 	}
+
+	rev := 0
+	if changed {
+		e.identityRevision++
+		rev = e.identityRevision
+	}
+
 	e.Mutex.Unlock()
 
-	return changed
+	return rev
 }
 
 // LeaveLocked removes the endpoint's directory from the system. Must be called
 // with Endpoint's mutex AND BuildMutex locked.
-func (e *Endpoint) LeaveLocked(owner Owner) {
+func (e *Endpoint) LeaveLocked(owner Owner) int {
+	errors := 0
+
 	owner.RemoveFromEndpointQueue(uint64(e.ID))
 	if c := e.Consumable; c != nil {
 		c.Mutex.RLock()
@@ -1321,7 +1327,18 @@ func (e *Endpoint) LeaveLocked(owner Owner) {
 	if e.PolicyMap != nil {
 		if err := e.PolicyMap.Close(); err != nil {
 			e.getLogger().WithError(err).WithField(logfields.Path, e.PolicyMapPathLocked()).Warn("Unable to close policy map")
+			errors++
 		}
+	}
+
+	if e.SecLabel != nil {
+		if err := e.SecLabel.Release(); err != nil {
+			log.WithError(err).WithField(logfields.Identity, e.SecLabel.ID).
+				Error("Unable to release identity of endpoint")
+			errors++
+		}
+
+		e.SecLabel = nil
 	}
 
 	e.L3Maps.Close()
@@ -1329,6 +1346,8 @@ func (e *Endpoint) LeaveLocked(owner Owner) {
 	e.controllers.RemoveAll()
 
 	e.SetStateLocked(StateDisconnected, "Endpoint removed")
+
+	return errors
 }
 
 func (e *Endpoint) removeDirectory() {
@@ -1627,4 +1646,197 @@ func (e *Endpoint) getIDandLabels() string {
 	}
 
 	return fmt.Sprintf("%d (%s)", e.ID, labels)
+}
+
+// ModifyIdentityLabels changes the identity relevant labels of an endpoint.
+// labels can be added or deleted. If a net label changed is performed, the
+// endpoint will receive a new identity and will be regenerated. Both of these
+// operations will happen in the background.
+func (e *Endpoint) ModifyIdentityLabels(owner Owner, addLabels, delLabels labels.Labels) error {
+	e.Mutex.Lock()
+	defer e.Mutex.Unlock()
+
+	newLabels := e.OpLabels.DeepCopy()
+
+	if len(delLabels) > 0 {
+		for k := range delLabels {
+			// The change request is accepted if the label is on
+			// any of the lists. If the label is already disabled,
+			// we will simply ignore that change.
+			if newLabels.OrchestrationIdentity[k] != nil ||
+				newLabels.Custom[k] != nil ||
+				newLabels.Disabled[k] != nil {
+				break
+			}
+
+			return fmt.Errorf("label %s not found", k)
+		}
+	}
+
+	if len(delLabels) > 0 {
+		for k, v := range delLabels {
+			if newLabels.OrchestrationIdentity[k] != nil {
+				delete(newLabels.OrchestrationIdentity, k)
+				newLabels.Disabled[k] = v
+			}
+
+			if newLabels.Custom[k] != nil {
+				delete(newLabels.Custom, k)
+			}
+		}
+	}
+
+	if len(addLabels) > 0 {
+		for k, v := range addLabels {
+			if newLabels.Disabled[k] != nil {
+				delete(newLabels.Disabled, k)
+				newLabels.OrchestrationIdentity[k] = v
+			} else if newLabels.OrchestrationIdentity[k] == nil {
+				newLabels.Custom[k] = v
+			}
+		}
+	}
+
+	e.OpLabels = *newLabels
+
+	// Mark with StateWaitingForIdentity, it will be set to
+	// StateWaitingToRegenerate after the identity resolution has been
+	// completed
+	e.SetStateLocked(StateWaitingForIdentity, "Triggering identity resolution due to updated security labels")
+
+	e.identityRevision++
+	rev := e.identityRevision
+
+	e.runLabelsResolver(owner, rev)
+
+	return nil
+}
+
+// UpdateLabels is called to update the labels of an endpoint. Calls to this
+// function do not necessarily mean that the labels actually changed. The
+// container runtime layer will periodically synchronize labels.
+//
+// If a net label changed was performed, the endpoint will receive a new
+// identity and will be regenerated. Both of these operations will happen in
+// the background.
+func (e *Endpoint) UpdateLabels(owner Owner, identityLabels, infoLabels labels.Labels) {
+	log.WithFields(logrus.Fields{
+		logfields.ContainerID:    e.GetShortContainerID(),
+		logfields.EndpointID:     e.StringID(),
+		logfields.IdentityLabels: identityLabels.String(),
+		logfields.InfoLabels:     infoLabels.String(),
+	}).Debug("Refreshing labels of endpoint")
+
+	e.replaceInformationLabels(infoLabels)
+
+	// replace identity labels and update the identity if labels have changed
+	if rev := e.replaceIdentityLabels(identityLabels); rev != 0 {
+		e.runLabelsResolver(owner, rev)
+	}
+}
+
+func (e *Endpoint) identityResolutionIsObsolete(myChangeRev int) bool {
+	// If in disconnected state, skip as well as this operation is no
+	// longer required.
+	if e.state == StateDisconnected {
+		return true
+	}
+
+	// Check if the endpoint has since received a new identity revision, if
+	// so, abort as a new resolution routine will have been started.
+	if myChangeRev != e.identityRevision {
+		return true
+	}
+
+	return false
+}
+
+func (e *Endpoint) runLabelsResolver(owner Owner, myChangeRev int) {
+	ctrlName := fmt.Sprintf("resolve-identity-%d", e.ID)
+	e.controllers.UpdateController(ctrlName,
+		controller.ControllerParams{
+			DoFunc: func() error {
+				return e.identityLabelsChanged(owner, myChangeRev)
+			},
+			RunInterval: time.Duration(5) * time.Minute,
+		},
+	)
+}
+
+func (e *Endpoint) identityLabelsChanged(owner Owner, myChangeRev int) error {
+	e.Mutex.RLock()
+	newLabels := e.OpLabels.IdentityLabels()
+	elog := log.WithFields(logrus.Fields{
+		logfields.EndpointID:     e.ID,
+		logfields.IdentityLabels: newLabels,
+	})
+
+	// Since we unlocked the endpoint and re-locked, the label update may already be obsolete
+	if e.identityResolutionIsObsolete(myChangeRev) {
+		e.Mutex.RUnlock()
+		elog.Debug("Endpoint identity has changed, aborting resolution routine in favour of new one")
+		return nil
+	}
+
+	if e.SecLabel != nil && string(e.SecLabel.Labels.SortedList()) != string(newLabels.SortedList()) {
+		e.Mutex.RUnlock()
+		elog.Debug("Endpoint labels unchanged, skipping resolution of identity")
+		return nil
+	}
+
+	// Unlock the endpoint mutex for the possibly long lasting kvstore operation
+	e.Mutex.RUnlock()
+	elog.Debug("Resolving identity for labels")
+
+	identity, _, err := policy.AllocateIdentity(newLabels)
+	if err != nil {
+		err = fmt.Errorf("unable to resolve identity: %s", err)
+		e.LogStatus(Other, Warning, fmt.Sprintf("%s (will retry)", err.Error()))
+		return err
+	}
+
+	e.Mutex.Lock()
+
+	// Since we unlocked the endpoint and re-locked, the label update may already be obsolete
+	if e.identityResolutionIsObsolete(myChangeRev) {
+		e.Mutex.Unlock()
+
+		if err := identity.Release(); err != nil {
+			// non fatal error as keys will expire after lease expires but log it
+			elog.WithFields(logrus.Fields{logfields.Identity: identity.ID}).
+				WithError(err).Warn("Unable to release newly allocated identity again")
+		}
+
+		return nil
+	}
+
+	// If endpoint has an old identity, defer release of it to the end of
+	// the function after the endpoint structured has been unlocked again
+	if e.SecLabel != nil {
+		oldIdentity := e.SecLabel
+		defer func() {
+			if err := oldIdentity.Release(); err != nil {
+				elog.WithFields(logrus.Fields{logfields.Identity: oldIdentity.ID}).
+					WithError(err).Warn("BUG: Unable to release old endpoint identity")
+			}
+		}()
+	}
+
+	elog.WithFields(logrus.Fields{logfields.Identity: identity.StringID()}).
+		Debug("Assigned new identity to endpoint")
+
+	e.SetIdentity(owner, identity)
+
+	ready := e.SetStateLocked(StateWaitingToRegenerate, "Triggering regeneration due to new identity")
+	if ready {
+		e.ForcePolicyCompute()
+	}
+
+	e.Mutex.Unlock()
+
+	if ready {
+		e.Regenerate(owner, "updated security labels")
+	}
+
+	return nil
 }

cilium-health/launch/endpoint.go
@@ -184,8 +184,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 	}
 
 	// Give the endpoint a security identity
-	_, errLabelsAdd := owner.UpdateSecLabels(info.ContainerID, lbl, labels.Labels{})
-	if errLabelsAdd != nil {
+	if errLabelsAdd := ep.ModifyIdentityLabels(owner, lbl, labels.Labels{}); errLabelsAdd != nil {
 		log.WithError(errLabelsAdd).Fatal("Failed to set labels on cilium-health endpoint")
 	}
 

daemon/endpoint.go
@@ -155,7 +155,7 @@ func (d *Daemon) createEndpoint(epTemplate *models.EndpointChangeRequest, id str
 	add := labels.NewLabelsFromModel(lbls)
 
 	if len(add) > 0 {
-		code, errLabelsAdd := d.UpdateSecLabels(id, add, labels.Labels{})
+		code, errLabelsAdd := d.updateEndpointLabels(id, add, labels.Labels{})
 		if errLabelsAdd != nil {
 			// XXX: Why should the endpoint remain in this case?
 			log.WithFields(logrus.Fields{
@@ -328,14 +328,6 @@ func (d *Daemon) deleteEndpoint(ep *endpoint.Endpoint) int {
 	}
 	ep.SetStateLocked(endpoint.StateDisconnecting, "Deleting endpoint")
 
-	sha256sum := ep.OpLabels.IdentityLabels().SHA256Sum()
-	if err := d.DeleteIdentityBySHA256(sha256sum, ep.StringID()); err != nil {
-		log.WithError(err).WithFields(logrus.Fields{
-			logfields.SHA:            sha256sum,
-			logfields.IdentityLabels: ep.OpLabels.IdentityLabels(),
-		}).Error("Error while deleting labels")
-	}
-
 	// Remove the endpoint before we clean up. This ensures it is no longer
 	// listed or queued for rebuilds.
 	endpointmanager.Remove(ep)
@@ -393,7 +385,7 @@ func (d *Daemon) deleteEndpoint(ep *endpoint.Endpoint) int {
 		errors++
 	}
 
-	ep.LeaveLocked(d)
+	errors += ep.LeaveLocked(d)
 	ep.Mutex.Unlock()
 
 	err := ep.WaitForProxyCompletions()
@@ -599,92 +591,14 @@ func checkLabels(add, del labels.Labels) (addLabels, delLabels labels.Labels, ok
 	return addLabels, delLabels, true
 }
 
-func (d *Daemon) updateSecLabels(ep *endpoint.Endpoint, add, del labels.Labels) (int, error) {
-	// This is safe only if no other goroutine may change the labels in parallel
-	ep.Mutex.RLock()
-	oldLabels := ep.OpLabels.DeepCopy()
-	ep.Mutex.RUnlock()
-
-	if len(del) > 0 {
-		for k := range del {
-			// The change request is accepted if the label is on
-			// any of the lists. If the label is already disabled,
-			// we will simply ignore that change.
-			if oldLabels.OrchestrationIdentity[k] != nil ||
-				oldLabels.Custom[k] != nil ||
-				oldLabels.Disabled[k] != nil {
-				break
-			}
-
-			return PutEndpointIDLabelsLabelNotFoundCode, fmt.Errorf("label %s not found", k)
-		}
-	}
-
-	if len(del) > 0 {
-		for k, v := range del {
-			if oldLabels.OrchestrationIdentity[k] != nil {
-				delete(oldLabels.OrchestrationIdentity, k)
-				oldLabels.Disabled[k] = v
-			}
-
-			if oldLabels.Custom[k] != nil {
-				delete(oldLabels.Custom, k)
-			}
-		}
-	}
-
-	if len(add) > 0 {
-		for k, v := range add {
-			if oldLabels.Disabled[k] != nil {
-				delete(oldLabels.Disabled, k)
-				oldLabels.OrchestrationIdentity[k] = v
-			} else if oldLabels.OrchestrationIdentity[k] == nil {
-				oldLabels.Custom[k] = v
-			}
-		}
-	}
-
-	identity, newHash, err2 := d.updateEndpointIdentity(ep.StringID(), ep.LabelsHash, oldLabels)
-	if err2 != nil {
-		return PutEndpointIDLabelsUpdateFailedCode, err2
-	}
-
-	ep.Mutex.Lock()
-	if ep.GetStateLocked() == endpoint.StateDisconnected {
-		ep.Mutex.Unlock()
-		if err := d.DeleteIdentity(identity.ID, ep.StringID()); err != nil {
-			log.WithFields(logrus.Fields{
-				logfields.EndpointID: ep.StringID(),
-				logfields.Identity:   identity.ID,
-			}).WithError(err).Warn("Unable to release temporary identity")
-		}
-		return PutEndpointIDLabelsNotFoundCode, fmt.Errorf("No endpoint with ID %s found", ep.StringID())
-	}
-
-	ep.LabelsHash = newHash
-	ep.OpLabels = *oldLabels
-	ep.SetIdentity(d, identity)
-	ready := ep.SetStateLocked(endpoint.StateWaitingToRegenerate, "Triggering regeneration due to updated security labels")
-	if ready {
-		ep.ForcePolicyCompute()
-	}
-	ep.Mutex.Unlock()
-
-	if ready {
-		ep.Regenerate(d, "updated security labels")
-	}
-
-	return PutEndpointIDLabelsOKCode, nil
-}
-
-// UpdateSecLabels add and deletes the given labels on given endpoint ID.
+// updateEndpointLabels add and deletes the given labels on given endpoint ID.
 // The received `add` and `del` labels will be filtered with the valid label
 // prefixes.
 // The `add` labels take precedence over `del` labels, this means if the same
 // label is set on both `add` and `del`, that specific label will exist in the
 // endpoint's labels.
 // Returns an HTTP response code and an error msg (or nil on success).
-func (d *Daemon) UpdateSecLabels(id string, add, del labels.Labels) (int, error) {
+func (d *Daemon) updateEndpointLabels(id string, add, del labels.Labels) (int, error) {
 	addLabels, delLabels, ok := checkLabels(add, del)
 	if !ok {
 		return 0, nil
@@ -698,12 +612,16 @@ func (d *Daemon) UpdateSecLabels(id string, add, del labels.Labels) (int, error)
 		return PutEndpointIDLabelsNotFoundCode, fmt.Errorf("Endpoint ID %s not found", id)
 	}
 
-	return d.updateSecLabels(ep, addLabels, delLabels)
+	if err := ep.ModifyIdentityLabels(d, addLabels, delLabels); err != nil {
+		return PutEndpointIDLabelsNotFoundCode, err
+	}
+
+	return PutEndpointIDLabelsOKCode, nil
 }
 
-// updateSecLabelsFromAPI is the same as UpdateSecLabels(), but also performs
-// checks for whether the endpoint may be modified by an API call.
-func (d *Daemon) updateSecLabelsFromAPI(id string, add, del labels.Labels) (int, error) {
+// updateEndpointLabelsFromAPI is the same as updateEndpointLabels(), but also
+// performs checks for whether the endpoint may be modified by an API call.
+func (d *Daemon) updateEndpointLabelsFromAPI(id string, add, del labels.Labels) (int, error) {
 	addLabels, delLabels, ok := checkLabels(add, del)
 	if !ok {
 		return 0, nil
@@ -725,7 +643,11 @@ func (d *Daemon) updateSecLabelsFromAPI(id string, add, del labels.Labels) (int,
 		return PutEndpointIDInvalidCode, err
 	}
 
-	return d.updateSecLabels(ep, addLabels, delLabels)
+	if err := ep.ModifyIdentityLabels(d, addLabels, delLabels); err != nil {
+		return PutEndpointIDLabelsNotFoundCode, err
+	}
+
+	return PutEndpointIDLabelsOKCode, nil
 }
 
 type putEndpointIDLabels struct {
@@ -744,68 +666,9 @@ func (h *putEndpointIDLabels) Handle(params PutEndpointIDLabelsParams) middlewar
 	add := labels.NewLabelsFromModel(mod.Add)
 	del := labels.NewLabelsFromModel(mod.Delete)
 
-	code, errMsg := d.updateSecLabelsFromAPI(params.ID, add, del)
-	if errMsg != nil {
-		return apierror.Error(code, errMsg)
+	code, err := d.updateEndpointLabelsFromAPI(params.ID, add, del)
+	if err != nil {
+		return apierror.Error(code, err)
 	}
 	return NewPutEndpointIDLabelsOK()
-}
-
-// EndpointLabelsUpdate is called periodically to sync the labels of an
-// endpoint. Calls to this function do not necessarily mean that the labels
-// actually changed. The container runtime layer will periodically synchronize
-// labels
-// The responsibility of this function is to:
-//  - resolve the identity and update the endpoint
-//  - trigger endpoint regeneration if required
-//  - trigger policy regeneration if required
-func (d *Daemon) EndpointLabelsUpdate(ep *endpoint.Endpoint, identityLabels, infoLabels labels.Labels) error {
-	log.WithFields(logrus.Fields{
-		logfields.ContainerID:    ep.GetShortContainerID(),
-		logfields.EndpointID:     ep.StringID(),
-		logfields.IdentityLabels: identityLabels.String(),
-		"infoLabels":             infoLabels.String(),
-	}).Debug("Updating labels of endpoint")
-
-	ep.UpdateOrchInformationLabels(infoLabels)
-	ep.UpdateOrchIdentityLabels(identityLabels)
-
-	// It's mandatory to update the endpoint identity in the KVStore.  This
-	// way we keep the RefCount refreshed and the SecurityLabelID will not
-	// be considered unused.
-	identity, newHash, err := d.updateEndpointIdentity(ep.StringID(), ep.LabelsHash, &ep.OpLabels)
-	if err != nil {
-		return fmt.Errorf("Unable to update identity of endpoint")
-	}
-
-	// Set identity labels and identity associating while holding endpoint
-	// lock never have a disconnect between labels and identity.
-	ep.Mutex.Lock()
-
-	// Endpoint might have transitioned to disconnected state. If
-	// disconnected, do not associate the identity with the endpoint and
-	// release it again
-	if ep.GetStateLocked() == endpoint.StateDisconnected {
-		ep.Mutex.Unlock()
-		if err := d.DeleteIdentity(identity.ID, ep.StringID()); err != nil {
-			log.WithFields(logrus.Fields{
-				logfields.EndpointID: ep.StringID(),
-				logfields.Identity:   identity.ID,
-			}).WithError(err).Warn("Unable to release temporary identity")
-		}
-
-		return fmt.Errorf("Endpoint is disconnected, aborting label update handler")
-	}
-
-	ep.LabelsHash = newHash
-	oldIdentity := ep.GetIdentity()
-	ep.SetIdentity(d, identity)
-	ep.Mutex.Unlock()
-
-	// Skip building endpoint if identity is invalid or unchanged
-	if identity.ID != oldIdentity {
-		// Triggers policy updates on all endpoints
-		d.TriggerPolicyUpdates(true)
-	}
-	return nil
 }

Makefile
@@ -21,11 +21,8 @@ build: $(SUBDIRS)
 $(SUBDIRS): force
 	@ $(MAKE) -C $@ all
 
-tests: tests-common tests-consul tests-envoy
-
-tests-ginkgo: tests-common-ginkgo
-
-tests-common-ginkgo: force
+# invoked from ginkgo Jenkinsfile
+tests-ginkgo: force
 	go vet $(GOFILES)
 	# Make the bindata to run the unittest
 	make -C daemon go-bindata
@@ -37,14 +34,25 @@ clean-ginkgo-tests:
 	docker-compose -f test/docker-compose.yml -p $$JOB_BASE_NAME-$$BUILD_NUMBER down
 	docker-compose -f test/docker-compose.yml -p $$JOB_BASE_NAME-$$BUILD_NUMBER rm
 
-tests-common: force
-	go vet $(GOFILES)
-	make -C test/ build
+TEST_LDFLAGS=-ldflags "-X github.com/cilium/cilium/pkg/kvstore.consulDummyAddress=consul:8500 -X github.com/cilium/cilium/pkg/kvstore.etcdDummyAddress=etcd:4002"
+
+# invoked from ginkgo compose file after starting kvstore backends
+tests-ginkgo-real:
+	echo "mode: count" > coverage-all.out
+	echo "mode: count" > coverage.out
+	$(foreach pkg,$(TESTPKGS),\
+	go test $(TEST_LDFLAGS) \
+            -timeout 360s -coverprofile=coverage.out -covermode=count $(pkg) $(GOTEST_OPTS) || exit 1;\
+            tail -n +2 coverage.out >> coverage-all.out;)
+	go tool cover -html=coverage-all.out -o=coverage-all.html
+	rm coverage-all.out
+	rm coverage.out
+	@rmdir ./daemon/1 ./daemon/1_backup 2> /dev/null || true
 
 tests-envoy:
 	@ $(MAKE) -C envoy tests
 
-tests-etcd:
+start-kvstores:
 	@docker rm -f "cilium-etcd-test-container" 2> /dev/null || true
 	-docker run -d \
 	    --name "cilium-etcd-test-container" \
@@ -55,34 +63,6 @@ tests-etcd:
         -listen-client-urls http://0.0.0.0:4001 \
         -initial-cluster-token etcd-cluster-1 \
         -initial-cluster-state new
-	echo "mode: count" > coverage-all.out
-	echo "mode: count" > coverage.out
-	$(foreach pkg,$(TESTPKGS),\
-	go test \
-            -ldflags "-X "github.com/cilium/cilium/pkg/kvstore".backend=etcd" \
-            -timeout 30s -coverprofile=coverage.out -covermode=count $(pkg) $(GOTEST_OPTS) || exit 1;\
-            tail -n +2 coverage.out >> coverage-all.out;)
-	go tool cover -html=coverage-all.out -o=coverage-all.html
-	rm coverage-all.out
-	rm coverage.out
-	@rmdir ./daemon/1 ./daemon/1_backup 2> /dev/null || true
-	docker rm -f "cilium-etcd-test-container"
-
-tests-consul-ginkgo:
-	echo "mode: count" > coverage-all.out
-	echo "mode: count" > coverage.out
-	$(foreach pkg,$(TESTPKGS),\
-	go test \
-            -ldflags "-X github.com/cilium/cilium/pkg/kvstore.backend=consul -X github.com/cilium/cilium/pkg/kvstore.consulAddress=consul:8500" \
-            -timeout 30s -coverprofile=coverage.out -covermode=count $(pkg) $(GOTEST_OPTS) || exit 1;\
-            tail -n +2 coverage.out >> coverage-all.out;)
-	go tool cover -html=coverage-all.out -o=coverage-all.html
-	rm coverage-all.out
-	rm coverage.out
-	@rmdir ./daemon/1 ./daemon/1_backup 2> /dev/null || true
-
-
-tests-consul:
 	@docker rm -f "cilium-consul-test-container" 2> /dev/null || true
 	-docker run -d \
            --name "cilium-consul-test-container" \
@@ -90,17 +70,20 @@ tests-consul:
            -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true, "disable_update_check": true}' \
            consul:0.8.3 \
            agent -client=0.0.0.0 -server -bootstrap-expect 1
+
+tests: start-kvstores tests-envoy
+	go vet $(GOFILES)
 	echo "mode: count" > coverage-all.out
 	echo "mode: count" > coverage.out
 	$(foreach pkg,$(TESTPKGS),\
 	go test \
-            -ldflags "-X "github.com/cilium/cilium/pkg/kvstore".backend=consul" \
-            -timeout 30s -coverprofile=coverage.out -covermode=count $(pkg) $(GOTEST_OPTS) || exit 1;\
+            -timeout 360s -coverprofile=coverage.out -covermode=count $(pkg) $(GOTEST_OPTS) || exit 1;\
             tail -n +2 coverage.out >> coverage-all.out;)
 	go tool cover -html=coverage-all.out -o=coverage-all.html
 	rm coverage-all.out
 	rm coverage.out
 	@rmdir ./daemon/1 ./daemon/1_backup 2> /dev/null || true
+	docker rm -f "cilium-etcd-test-container"
 	docker rm -f "cilium-consul-test-container"
 
 clean-tags:

daemon/daemon.go
@@ -100,9 +100,6 @@ type Daemon struct {
 	policy            *policy.Repository
 	preFilter         *policy.PreFilter
 
-	maxCachedLabelIDMU lock.RWMutex
-	maxCachedLabelID   policy.NumericIdentity
-
 	uniqueIDMU lock.Mutex
 	uniqueID   map[uint64]bool
 
@@ -211,6 +208,11 @@ func (d *Daemon) StartEndpointBuilders(nRoutines int) {
 			}
 		}()
 	}
+}
+
+// GetTunnelMode returns the path to the state directory
+func (d *Daemon) GetTunnelMode() string {
+	return d.conf.Tunnel
 }
 
 // GetStateDir returns the path to the state directory
@@ -1013,6 +1015,10 @@ func NewDaemon(c *Config) (*Daemon, error) {
 	ni, n := node.GetLocalNode()
 	node.UpdateNode(ni, n, node.TunnelRoute, nil)
 
+	// This needs to be done after the node addressing has been configured
+	// as the node address is required as sufix
+	policy.InitIdentityAllocator(&d)
+
 	if !d.conf.IPv4Disabled {
 		// Allocate IPv4 service loopback IP
 		loopbackIPv4, _, err := ipam.AllocateNext("ipv4")
@@ -1412,4 +1418,15 @@ func (d *Daemon) sendEvent(data []byte) error {
 	}
 
 	return nil
+}
+
+// GetNodeSuffix returns the suffix to be appended to kvstore keys of this
+// agent
+func (d *Daemon) GetNodeSuffix() string {
+	if ip := node.GetExternalIPv4(); ip != nil {
+		return ip.String()
+	}
+
+	log.Fatal("Node IP not available yet")
+	return "<nil>"
 }

commit f360c46ffb626561861c4557ed5c9512198fb4eb

pkg/endpoint: set state ready if endpoint labels are the same
Fixes: 7ccf87701fc ("kvstore: New kvstore abstraction API")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/endpoint.go
@@ -2328,6 +2328,10 @@ func (e *Endpoint) identityLabelsChanged(owner Owner, myChangeRev int) error {
 	}
 
 	if e.SecurityIdentity != nil && e.SecurityIdentity.Labels.Equals(newLabels) {
+		// Sets endpoint state to ready if was waiting for identity
+		if e.GetStateLocked() == StateWaitingForIdentity {
+			e.SetStateLocked(StateReady, "Set identity for this endpoint")
+		}
 		e.Mutex.RUnlock()
 		elog.Debug("Endpoint labels unchanged, skipping resolution of identity")
 		return nil