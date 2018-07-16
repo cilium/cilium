38e98f5ae4e99ecc3d48b0c6b8bdc70bb90ed703
Thomas Graf <thomas@cilium.io>
2018-07-17 00:21:30 +0200

    bugtool: Include cilium map list --verbose
    Lists all open maps in addition to their cache contents.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- bugtool/cmd/configuration.go
+++ bugtool/cmd/configuration.go
@@ -219,6 +219,7 @@ func copyCiliumInfoCommands(cmdDir string, k8sPods []string) []string {
 		"cilium bpf ct list global",
 		"cilium bpf proxy list",
 		"cilium bpf ipcache list",
+		"cilium map list --verbose",
 		"cilium status --verbose",
 		"cilium identity list",
 		"cilium-health status",
