c8e083dcb7f46a02e5ca6b90fbc26638147e7f27
Eloy Coto <eloy.coto@gmail.com>
2018-07-16 23:30:57 +0200

    Test: Add SSHMetaError type
    On helpers functions is a common struggle that developers do not have
    all the information that we can give to them. With this change a new
    helper error is added, so helpers that return an error can have a custom
    callback function that return some useful data needed to the developer
    to triage the issue.
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

---
+++ test/helpers/errors.go
@@ -0,0 +1,32 @@
+package helpers
+
+import "fmt"
+
+// NewSSHMetaError returns a new SSHMetaError with the given string and a
+// callback function
+func NewSSHMetaError(errorString string, callback func() string) *SSHMetaError {
+	if callback == nil {
+		callback = func() string { return "Invalid callback" }
+	}
+	return &SSHMetaError{
+		errorString:   errorString,
+		errorCallBack: callback,
+	}
+}
+
+// SSHMetaError is a custom error that executes a callback function when its
+// Error() function is invoked
+type SSHMetaError struct {
+	errorString   string
+	errorCallBack func() string
+}
+
+func (e *SSHMetaError) String() string {
+	return fmt.Sprintf(
+		"Error: %s\nExtended info: %s\n",
+		e.errorString, e.errorCallBack())
+}
+
+func (e *SSHMetaError) Error() string {
+	return e.String()
+}
