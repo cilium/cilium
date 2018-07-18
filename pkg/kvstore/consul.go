b9711087ba2572dbd6fcf11c7f362b9ae2f79e0e
André Martins <andre@cilium.io>
2018-07-18 22:23:52 +0200

    pkg/kvstore: fix high-cpu usage when Cilium loses Consul connectivity
    Fixes: 85469b099dac ("kvstore: New kvstore abstraction API")
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/kvstore/consul.go
+++ pkg/kvstore/consul.go
@@ -436,7 +436,7 @@ func (c *consulClient) Watch(w *Watcher) {
 
 		// timeout while watching for changes, re-schedule
 		if qo.WaitIndex != 0 && (q == nil || q.LastIndex == qo.WaitIndex) {
-			continue
+			goto wait
 		}
 
 		for _, newPair := range pairs {
@@ -487,6 +487,7 @@ func (c *consulClient) Watch(w *Watcher) {
 			w.Events <- KeyValueEvent{Typ: EventTypeListDone}
 		}
 
+	wait:
 		select {
 		case <-time.After(sleepTime):
 		case <-w.stopWatch:
