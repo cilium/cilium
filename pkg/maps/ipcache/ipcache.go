cilium/cilium

commit deb11d9901671ba66d7824300c5f29e49ff00095

bpf: Support LPM for ipcache on newer kernels
The keys are already in the right format for LPM map type, the code just
needs to be extended to pass the appropriate parameters to the lookup
functions. On kernels 4.11 and newer, this will save a few instructions
and allow us to support more than 16 unique prefix lengths in CIDR
rules. As such, we relax this restriction in this case.

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/maps/ipcache/ipcache.go
@@ -139,11 +139,11 @@ func NewMap() *Map {
 	return &Map{
 		Map: *bpf.NewMap(
 			"cilium_ipcache",
-			bpf.BPF_MAP_TYPE_HASH,
+			bpf.BPF_MAP_TYPE_LPM_TRIE,
 			int(unsafe.Sizeof(Key{})),
 			int(unsafe.Sizeof(RemoteEndpointInfo{})),
 			MaxEntries,
-			0,
+			bpf.BPF_F_NO_PREALLOC,
 			func(key []byte, value []byte) (bpf.MapKey, bpf.MapValue, error) {
 				k, v := Key{}, RemoteEndpointInfo{}
 
@@ -159,6 +159,9 @@ func NewMap() *Map {
 // GetMaxPrefixLengths determines how many unique prefix lengths are supported
 // simultaneously based on the underlying BPF map type in use.
 func (m *Map) GetMaxPrefixLengths() (count int) {
+	if IPCache.MapType == bpf.BPF_MAP_TYPE_LPM_TRIE {
+		return net.IPv6len * 8
+	}
 	return maxPrefixLengths
 }
 

bpf/lib/eps.h
@@ -76,6 +76,7 @@ ipcache_lookup4(struct bpf_elf_map *map, __be32 addr, __u32 prefix)
 	return map_lookup_elem(map, &key);
 }
 
+#ifndef HAVE_LPM_MAP_TYPE
 /* Define a function with the following NAME which iterates through PREFIXES
  * (a list of integers ordered from high to low representing prefix length),
  * performing a lookup in MAP using LOOKUP_FN to find a provided IP of type
@@ -102,6 +103,14 @@ LPM_LOOKUP_FN(lookup_ip6_remote_endpoint, union v6addr *, IPCACHE6_PREFIXES,
 LPM_LOOKUP_FN(lookup_ip4_remote_endpoint, __be32, IPCACHE4_PREFIXES,
 	      cilium_ipcache, ipcache_lookup4)
 #undef LPM_LOOKUP_FN
+#else /* HAVE_LPM_MAP_TYPE */
+#define V6_CACHE_KEY_LEN IPCACHE_PREFIX_LEN(sizeof(union v6addr)*8)
+#define lookup_ip6_remote_endpoint(addr) \
+	ipcache_lookup6(&cilium_ipcache, addr, V6_CACHE_KEY_LEN)
+#define V4_CACHE_KEY_LEN IPCACHE_PREFIX_LEN(sizeof(__u32)*8)
+#define lookup_ip4_remote_endpoint(addr) \
+	ipcache_lookup4(&cilium_ipcache, addr, V4_CACHE_KEY_LEN)
+#endif /* HAVE_LPM_MAP_TYPE */
 
 #endif /* POLICY_EGRESS */
 

bpf/lib/maps.h
@@ -93,6 +93,12 @@ struct bpf_elf_map __section_maps cilium_tunnel_map = {
 
 #endif
 
+#ifdef HAVE_LPM_MAP_TYPE
+#define LPM_MAP_TYPE BPF_MAP_TYPE_LPM_TRIE
+#else
+#define LPM_MAP_TYPE BPF_MAP_TYPE_HASH
+#endif
+
 #if defined POLICY_INGRESS || defined POLICY_EGRESS
 
 #ifndef LPM_MAP_SIZE
@@ -101,12 +107,6 @@ struct bpf_elf_map __section_maps cilium_tunnel_map = {
 
 #ifndef LPM_MAP_VALUE_SIZE
 #define LPM_MAP_VALUE_SIZE 1
-#endif
-
-#ifdef HAVE_LPM_MAP_TYPE
-#define LPM_MAP_TYPE BPF_MAP_TYPE_LPM_TRIE
-#else
-#define LPM_MAP_TYPE BPF_MAP_TYPE_HASH
 #endif
 
 struct bpf_lpm_trie_key6 {
@@ -292,11 +292,12 @@ struct ipcache_key {
 
 /* Global IP -> Identity map for applying egress label-based policy */
 struct bpf_elf_map __section_maps cilium_ipcache = {
-	.type		= BPF_MAP_TYPE_HASH,
+	.type		= LPM_MAP_TYPE,
 	.size_key	= sizeof(struct ipcache_key),
 	.size_value	= sizeof(struct remote_endpoint_info),
 	.pinning	= PIN_GLOBAL_NS,
 	.max_elem	= IPCACHE_MAP_SIZE,
+	.flags		= BPF_F_NO_PREALLOC,
 };
 
 #ifndef SKIP_CALLS_MAP

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=0eaad66759eeb1f7120a494771425b121c1f6f3c
+GO_BINDATA_SHA1SUM=6dc14e02d34faca61199babdcf7c2a66d0fa8fd7
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

pkg/bpf/map.go
@@ -272,7 +272,17 @@ func (m *Map) OpenOrCreate() (bool, error) {
 		os.Remove(m.path)
 	}
 
+retry:
 	fd, isNew, err := OpenOrCreateMap(m.path, int(m.MapType), m.KeySize, m.ValueSize, m.MaxEntries, m.Flags)
+	if err != nil && m.MapType == BPF_MAP_TYPE_LPM_TRIE {
+		// If the map type is an LPM, then we can typically fall back
+		// to a hash map. Note that this requires datapath support,
+		// such as an unrolled loop performing repeated lookups with
+		// a defined set of prefixes.
+		log.WithError(err).Debugf("Kernel does not support LPM maps, creating hash table for %s instead.", m.name)
+		m.MapType = BPF_MAP_TYPE_HASH
+		goto retry
+	}
 	if err != nil {
 		return false, err
 	}

commit 60f216acd9df801c3b0fd3bd4b8f27399b6fcd1e

maps/ipcache: Fix max limit for LPM prefix lengths
It's possible to configure each mask from 0...128 inclusive, which
results in 129 total masks. Fix up this limit (although it's unlikely
anyone will hit it any time soon).

Fixes: deb11d990167 ("bpf: Support LPM for ipcache on newer kernels")
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/maps/ipcache/ipcache.go
@@ -174,7 +174,7 @@ func Delete(k bpf.MapKey) error {
 // simultaneously based on the underlying BPF map type in use.
 func (m *Map) GetMaxPrefixLengths() (count int) {
 	if IPCache.MapType == bpf.BPF_MAP_TYPE_LPM_TRIE {
-		return net.IPv6len * 8
+		return net.IPv6len*8 + 1
 	}
 	return maxPrefixLengths
 }