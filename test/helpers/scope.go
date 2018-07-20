38a90992f102d773eea651e1988bf6ce6578a878
Eloy Coto <eloy.coto@gmail.com>
2018-07-20 21:46:22 +0200

    Test: Add the cilium.TestScope option
    On the test suite the scope (k8s, Nightly or Runtime) was defined by the
    ginkgo.focus option. Over the time that was a bad approach because some
    devs runs only the last test name, not all the test name and vagrant VM
    didn't provision correctly.
    
    With this change developers can choose the scope that they want.
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- test/helpers/scope.go
+++ test/helpers/scope.go
@@ -1,32 +1,41 @@
 package helpers
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
 	ginkgoconfig "github.com/onsi/ginkgo/config"
 )
 
+// PreDefinedScope in case that the test scope is defined by the user instead
+// of the focus string.
+var PreDefinedScope string
+
 // GetScope returns the scope for the currently running test.
-func GetScope() string {
+func GetScope() (string, error) {
+	if PreDefinedScope != "" {
+		return PreDefinedScope, nil
+	}
+
 	focusString := strings.TrimSpace(strings.ToLower(ginkgoconfig.GinkgoConfig.FocusString))
 	switch {
 	case strings.HasPrefix(focusString, "run"):
-		return Runtime
+		return Runtime, nil
 	case strings.HasPrefix(focusString, K8s):
-		return K8s
+		return K8s, nil
 	case strings.Contains(focusString, "nightly"):
 		// Nightly tests run in a Kubernetes environment.
-		return K8s
+		return K8s, nil
 	default:
-		return Runtime
+		return "", errors.New("Scope cannot be set")
 	}
 }
 
 // GetScopeWithVersion returns the scope of the running test. If the scope is
 // k8s, then it returns k8s scope along with the version of k8s that is running.
 func GetScopeWithVersion() string {
-	result := GetScope()
+	result, _ := GetScope()
 	if result != K8s {
 		return result
 	}
