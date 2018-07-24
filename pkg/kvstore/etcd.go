a56daf803f2d350fa832f43f5b9250abc386fcd2
Thomas Graf <thomas@cilium.io>
2018-07-23 23:08:29 -0700

    etcd: Fix and relax during recreate watcher loop
    [ upstream commit 734745ec7c801e54758ebbad61dc5f2d24dbef2c ]
    
    The revision to watch should not be incremented when Watch() returns an already
    closed channel.
    
    Also fix a cosmetic problem where lastRev in the for loop was shadowing the
    variable declared outside of the loop.
    
    We have observed occasions where etcd returns a closed channel on calling
    Watch(). The problem typically resolves itself quickly but we should sleep for
    a while to relax the CPU.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>
    Signed-off-by: Maciej Kwiek <maciej.iai@gmail.com>

--- pkg/kvstore/etcd.go
+++ pkg/kvstore/etcd.go
@@ -488,7 +488,6 @@ func (e *etcdClient) DeletePrefix(path string) error {
 
 // Watch starts watching for changes in a prefix
 func (e *etcdClient) Watch(w *Watcher) {
-	lastRev := int64(0)
 	localCache := watcherCache{}
 	listSignalSent := false
 
@@ -500,15 +499,13 @@ func (e *etcdClient) Watch(w *Watcher) {
 reList:
 	for {
 		res, err := e.client.Get(ctx.Background(), w.prefix, client.WithPrefix(),
-			client.WithRev(lastRev), client.WithSerializable())
+			client.WithSerializable())
 		if err != nil {
-			scopedLog.WithField(fieldRev, lastRev).WithError(err).Warn("Unable to list keys before starting watcher")
+			scopedLog.WithError(err).Warn("Unable to list keys before starting watcher")
 			continue
 		}
 
-		lastRev := res.Header.Revision
-
-		scopedLog = scopedLog.WithField(fieldRev, lastRev)
+		nextRev := res.Header.Revision + 1
 		scopedLog.Debugf("List response from etcd len=%d: %+v", res.Count, res)
 
 		if res.Count > 0 {
@@ -554,11 +551,9 @@ reList:
 		}
 
 	recreateWatcher:
-		lastRev++
-
-		scopedLog.WithField(fieldRev, lastRev).Debug("Starting to watch a prefix")
+		scopedLog.WithField(fieldRev, nextRev).Debug("Starting to watch a prefix")
 		etcdWatch := e.client.Watch(ctx.Background(), w.prefix,
-			client.WithPrefix(), client.WithRev(lastRev))
+			client.WithPrefix(), client.WithRev(nextRev))
 		for {
 			select {
 			case <-w.stopWatch:
@@ -567,6 +562,7 @@ reList:
 
 			case r, ok := <-etcdWatch:
 				if !ok {
+					time.Sleep(50 * time.Millisecond)
 					goto recreateWatcher
 				}
 
@@ -581,9 +577,6 @@ reList:
 						scopedLog.WithError(err).Debug("Tried watching on compacted revision")
 					}
 
-					// Retrieve latest revision
-					lastRev = 0
-
 					// mark all local keys in state for
 					// deletion unless the upcoming GET
 					// marks them alive
@@ -592,7 +585,7 @@ reList:
 					continue reList
 				}
 
-				lastRev = r.Header.Revision
+				nextRev = r.Header.Revision + 1
 				scopedLog.Debugf("Received event from etcd: %+v", r)
 
 				for _, ev := range r.Events {
