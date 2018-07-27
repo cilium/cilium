cilium/cilium

commit 1fd4f57c1ab38affbf281403f12c4ac82f4262b5

adding CRD cilium network policy policy status
When using kubernetes to manage CRD CNP, it was difficult to know on
which nodes was the policy being placed. With this commit it will be
possible to check with `kubectl get ciliumnetworkpolicies <name> -o
json` or with `kubectl describe ciliumnetworkpolicies <name>` when was
the policy received by which node.

Signed-off-by: André Martins <andre@cilium.io>

daemon/k8s_watcher.go
@@ -53,6 +53,9 @@ var (
 
 	// cnpClient is the interface for CRD and TPR
 	cnpClient k8s.CNPCliInterface
+
+	// ciliumRulesStore is the local cache for the CNP
+	ciliumRulesStore cache.Store
 )
 
 func init() {
@@ -227,7 +230,8 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 	)
 	go ingressController.Run(wait.NeverStop)
 
-	_, ciliumRulesController := cache.NewInformer(
+	var ciliumRulesController cache.Controller
+	ciliumRulesStore, ciliumRulesController = cache.NewInformer(
 		cnpClient.NewListWatch(),
 		&k8s.CiliumNetworkPolicy{},
 		reSyncPeriod,
@@ -932,11 +936,24 @@ func (d *Daemon) addCiliumNetworkPolicy(obj interface{}) {
 		}
 	}
 
+	var cnpns k8s.CiliumNetworkPolicyNodeStatus
 	if err != nil {
-		log.Warningf("Unable to add CiliumNetworkPolicy %s: %s", rule.Metadata.Name, err)
+		cnpns = k8s.CiliumNetworkPolicyNodeStatus{
+			OK:          false,
+			Error:       fmt.Sprintf("%s", err),
+			LastUpdated: time.Now(),
+		}
+		log.Warningf("Unable to add CiliumNetworkPolicy %s: err: '%s'. err != nil: '%t'. a nil object: '%s'", rule.Metadata.Name, err, err != nil, nil)
 	} else {
+		cnpns = k8s.CiliumNetworkPolicyNodeStatus{
+			OK:          true,
+			Error:       "OK",
+			LastUpdated: time.Now(),
+		}
 		log.Infof("Imported CiliumNetworkPolicy %s", rule.Metadata.Name)
 	}
+
+	go k8s.UpdateCNPStatus(cnpClient, k8s.BackOffLoopTimeout, ciliumRulesStore, rule, cnpns)
 }
 
 func (d *Daemon) deleteCiliumNetworkPolicy(obj interface{}) {
@@ -963,7 +980,22 @@ func (d *Daemon) deleteCiliumNetworkPolicy(obj interface{}) {
 }
 
 func (d *Daemon) updateCiliumNetworkPolicy(oldObj interface{}, newObj interface{}) {
-	// FIXME
+	oldRule, ok := oldObj.(*k8s.CiliumNetworkPolicy)
+	if !ok {
+		log.Warningf("Received unknown object %+v, expected a CiliumNetworkPolicy object", oldObj)
+		return
+	}
+	newRules, ok := newObj.(*k8s.CiliumNetworkPolicy)
+	if !ok {
+		log.Warningf("Received unknown object %+v, expected a CiliumNetworkPolicy object", newObj)
+		return
+	}
+	// Since we are updating the status map from all nodes we need to prevent
+	// deletion and addition of all rules in cilium.
+	if oldRule.SpecEquals(newRules) {
+		return
+	}
+
 	d.deleteCiliumNetworkPolicy(oldObj)
 	d.addCiliumNetworkPolicy(newObj)
 }

pkg/k8s/client.go
@@ -20,6 +20,8 @@ import (
 	"net"
 	"time"
 
+	"github.com/cilium/cilium/pkg/nodeaddress"
+
 	log "github.com/Sirupsen/logrus"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
@@ -327,4 +329,59 @@ func AnnotateNodeCIDR(c kubernetes.Interface, nodeName string, v4CIDR, v6CIDR *n
 		}(c, k8sNode, v4CIDR, v6CIDR, err)
 	}
 	return nil
+}
+
+// UpdateCNPStatus updates the status into the given CNP. This function retries
+// to do successful update into the kube-apiserver until it reaches the given
+// timeout.
+func UpdateCNPStatus(cnpClient CNPCliInterface, timeout time.Duration,
+	ciliumRulesStore cache.Store, rule *CiliumNetworkPolicy, cnpns CiliumNetworkPolicyNodeStatus) {
+
+	rule.SetPolicyStatus(nodeaddress.GetName(), cnpns)
+	_, err := cnpClient.Update(rule)
+	if err != nil {
+		ns := ExtractNamespace(&rule.Metadata)
+		name := rule.Metadata.GetObjectMeta().GetName()
+		log.Warningf("k8s: unable to update CNP %s/%s with status: %s, retrying...", ns, name, err)
+		t := time.NewTimer(timeout)
+		defer t.Stop()
+		loopTimer := time.NewTimer(time.Second)
+		defer loopTimer.Stop()
+		for n := 0; ; n++ {
+			serverRuleStore, exists, err := ciliumRulesStore.Get(rule)
+			if !exists {
+				break
+			}
+			if err != nil {
+				log.Warningf("k8s: unable to get k8s CNP %s/%s from local cache: %s", ns, name, err)
+				break
+			}
+			serverRule, ok := serverRuleStore.(*CiliumNetworkPolicy)
+			if !ok {
+				log.Warningf("Received unknown object %+v, expected a CiliumNetworkPolicy object", serverRuleStore)
+				return
+			}
+			if serverRule.Metadata.UID != rule.Metadata.UID &&
+				serverRule.SpecEquals(rule) {
+				// Although the policy was found this means it was deleted,
+				// and re-added with the same name.
+				log.Debugf("k8s: rule %s/%s changed while updating node status, stopping retry", ns, name)
+				break
+			}
+			serverRule.SetPolicyStatus(nodeaddress.GetName(), cnpns)
+			_, err = cnpClient.Update(serverRule)
+			if err == nil {
+				log.Debugf("k8s: successfully updated %s/%s with status: %s", ns, name, serverRule.Status)
+				break
+			}
+			loopTimer.Reset(time.Duration(n) * time.Second)
+			select {
+			case <-t.C:
+				log.Errorf("k8s: unable to update CNP %s/%s with status: %s", ns, name, err)
+				break
+			case <-loopTimer.C:
+			}
+			log.Warningf("k8s: unable to update CNP %s/%s with status: %s, retrying...", ns, name, err)
+		}
+	}
 }

pkg/k8s/const.go
@@ -16,6 +16,7 @@ package k8s
 
 import (
 	"k8s.io/kubernetes/pkg/kubelet/types"
+	"time"
 )
 
 const (
@@ -44,4 +45,8 @@ const (
 	// PodNamespaceMetaLabels is the label used to store the labels of the
 	// kubernetes namespace's labels.
 	PodNamespaceMetaLabels = "ns-labels"
+
+	// BackOffLoopTimeout is the default duration when trying to reach the
+	// kube-apiserver.
+	BackOffLoopTimeout = 2 * time.Minute
 )

pkg/k8s/third_party.go
@@ -16,6 +16,8 @@ package k8s
 
 import (
 	"fmt"
+	"reflect"
+	"time"
 
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/policy/api"
@@ -38,6 +40,45 @@ type CiliumNetworkPolicy struct {
 
 	// Specs is a list of desired Cilium specific rule specification.
 	Specs api.Rules `json:"specs,omitempty"`
+
+	// Status is the status of the Cilium policy rule
+	Status CiliumNetworkPolicyStatus `json:"status"`
+}
+
+// CiliumNetworkPolicyStatus is the status of a Cilium policy rule
+type CiliumNetworkPolicyStatus struct {
+	// Nodes is the Cilium policy status for each node
+	Nodes map[string]CiliumNetworkPolicyNodeStatus `json:"nodes,omitempty"`
+}
+
+// CiliumNetworkPolicyNodeStatus is the status of a Cilium policy rule for a
+// specific node
+type CiliumNetworkPolicyNodeStatus struct {
+	// OK is true when the policy has been installed successfully
+	OK bool `json:"ok,omitempty"`
+
+	// Error describes the error condition if OK is false
+	Error string `json:"error,omitempty"`
+
+	// LastUpdated contains the last time this status was updated
+	LastUpdated time.Time `json:"lastUpdated,omitempty"`
+}
+
+// SetPolicyStatus sets the given policy status for the given nodes' map
+func (r *CiliumNetworkPolicy) SetPolicyStatus(nodeName string, cnpns CiliumNetworkPolicyNodeStatus) {
+	if r.Status.Nodes == nil {
+		r.Status.Nodes = map[string]CiliumNetworkPolicyNodeStatus{}
+	}
+	r.Status.Nodes[nodeName] = cnpns
+}
+
+// SpecEquals returns true if the spec and specs metadata is the sa
+func (r *CiliumNetworkPolicy) SpecEquals(o *CiliumNetworkPolicy) bool {
+	if o == nil {
+		return r == nil
+	}
+	return reflect.DeepEqual(r.Spec, o.Spec) &&
+		reflect.DeepEqual(r.Specs, o.Specs)
 }
 
 // GetObjectKind returns the kind of the object

commit 19fe9d3488a9890f8e9359020390b1e4e9734db6

daemon: always re-add CNP when receiving an update from Kubernetes
Fixes: 1fd4f57c1ab3 ("adding CRD cilium network policy policy status")
Signed-off-by: André Martins <andre@cilium.io>

daemon/k8s_watcher.go
@@ -1681,7 +1681,6 @@ func (d *Daemon) updateCiliumNetworkPolicyV2(ciliumV2Store cache.Store,
 		"annotations":                              newRuleCpy.ObjectMeta.Annotations,
 	}).Debug("Modified CiliumNetworkPolicy")
 
-	d.deleteCiliumNetworkPolicyV2(oldRuleCpy)
 	d.addCiliumNetworkPolicyV2(ciliumV2Store, newRuleCpy)
 }
 