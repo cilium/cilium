cilium/cilium

commit deb2de2ce563740be063537afc009f0deca4ba01

completion: Refactor proxy completion logic in a new package
Move the completion into its own package:
github.com/cilium/cilium/pkg/completion.
Rename CompletionContainer into WaitGroup to reflect the similarity
with sync.WaitGroup.
Refactor Completion and WaitGroup to take a Context and handle
context cancellation.
Rename Completion.Completed into Complete to make it a verb.
Added Completion.Completed method to return a channel, to make it
easier to use in unit tests.

Revert the (de)serialization of the ProxyCompletions field to/from
JSON within the Endpoint struct, and rename ProxyCompletions into
ProxyWaitGroup.

Signed-off-by: Romain Lenglet <romain@covalent.io>

pkg/endpoint/endpoint.go
@@ -17,6 +17,7 @@ package endpoint
 import (
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -24,13 +25,13 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/labels"
 	pkgLabels "github.com/cilium/cilium/pkg/labels"
@@ -337,53 +338,21 @@ type Endpoint struct {
 	// proxy redirects to remove later during the build
 	proxiesToRemove map[string]bool
 
-	// Pending completions, protected by the BuildMutex!
-	ProxyCompletions *Completions
-}
-
-// Completions maintains the state needed to register and wait for asynchronous completions.
-type Completions struct {
-	wg     sync.WaitGroup
-	lock   lock.Mutex
-	errors int
-	done   bool
-}
-
-// Completed is called when an asynchronous event is completed.
-// May be called from any goroutine without holding any locks
-func (c *Completions) Completed(success bool) {
-	log.Debug("completions.Completed: ", success)
-	// 'done' is used to catch late callbacks (after Wait()), non-locked access on purpose.
-	if c.done {
-		log.Fatal("Completed called after Wait!")
-	}
-	if !success {
-		c.lock.Lock()
-		c.errors++
-		c.lock.Unlock()
-	}
-	c.wg.Done()
-}
-
-// AddCompletion increases the count of 'Completed()' callbacks we need by 1.
-// Called with BuildMutex held
-func (e *Endpoint) AddCompletion() (policy.Completion, time.Duration) {
-	e.ProxyCompletions.wg.Add(1)
-	return e.ProxyCompletions, time.Duration(10) * time.Second
+	// ProxyWaitGroup waits for pending proxy changes to complete.
+	// You must hold Endpoint.BuildMutex to read or write it.
+	ProxyWaitGroup *completion.WaitGroup `json:"-"`
 }
 
 // WaitForProxyCompletions blocks until all proxy changes have been completed.
-// Called with BuildMutex held
+// Called with BuildMutex held.
 func (e *Endpoint) WaitForProxyCompletions() error {
 	start := time.Now()
-	log.Debug("Waiting for proxy updates to complete...")
-	e.ProxyCompletions.wg.Wait()
-	// Wait is done, no parallel access any more
-	e.ProxyCompletions.done = true
-	if e.ProxyCompletions.errors > 0 {
-		return fmt.Errorf("%d proxy state changes failed", e.ProxyCompletions.errors)
+	e.getLogger().Debug("Waiting for proxy updates to complete...")
+	err := e.ProxyWaitGroup.Wait()
+	if err != nil {
+		return errors.New("proxy state changes failed")
 	}
-	log.Debug("Wait time for proxy updates: ", time.Since(start))
+	e.getLogger().Debug("Wait time for proxy updates: ", time.Since(start))
 	return nil
 }
 

daemon/endpoint.go
@@ -16,13 +16,16 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
 	"github.com/cilium/cilium/pkg/apierror"
+	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/ipam"
@@ -314,7 +317,9 @@ func (d *Daemon) deleteEndpoint(ep *endpoint.Endpoint) int {
 
 	// Wait for existing builds to complete and prevent further builds
 	ep.BuildMutex.Lock()
-	ep.ProxyCompletions = &endpoint.Completions{}
+	completionCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	ep.ProxyWaitGroup = completion.NewWaitGroup(completionCtx)
 
 	// Lock out any other writers to the endpoint
 	ep.Mutex.Lock()
@@ -392,7 +397,7 @@ func (d *Daemon) deleteEndpoint(ep *endpoint.Endpoint) int {
 	if err != nil {
 		scopedLog.WithError(err).Warn("Error removing proxy redirects.")
 	}
-	ep.ProxyCompletions = nil
+	ep.ProxyWaitGroup = nil
 	ep.BuildMutex.Unlock()
 
 	return errors

CODEOWNERS
@@ -64,6 +64,7 @@ monitor/payload @cilium/api
 pkg/apierror/ @cilium/api
 pkg/apisocket/ @cilium/api
 pkg/bpf/ @cilium/bpf
+pkg/completion/ @cilium/proxy
 pkg/endpoint/ @cilium/endpoint
 pkg/envoy/ @cilium/proxy
 pkg/health/ @cilium/health

daemon/daemon.go
@@ -124,7 +124,7 @@ func (d *Daemon) UpdateProxyRedirect(e *endpoint.Endpoint, l4 *policy.L4Filter) 
 		return 0, fmt.Errorf("can't redirect, proxy disabled")
 	}
 
-	r, err := d.l7Proxy.CreateOrUpdateRedirect(l4, e.ProxyID(l4), e, d, e)
+	r, err := d.l7Proxy.CreateOrUpdateRedirect(l4, e.ProxyID(l4), e, d, e.ProxyWaitGroup)
 	if err != nil {
 		return 0, err
 	}
@@ -143,7 +143,7 @@ func (d *Daemon) RemoveProxyRedirect(e *endpoint.Endpoint, id string) error {
 		logfields.EndpointID: e.ID,
 		logfields.L4PolicyID: id,
 	}).Debug("Removing redirect to endpoint")
-	return d.l7Proxy.RemoveRedirect(id, e)
+	return d.l7Proxy.RemoveRedirect(id, e.ProxyWaitGroup)
 }
 
 // QueueEndpointBuild puts the given request in the endpoints queue for

pkg/completion/completion.go
@@ -1,0 +1,118 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package completion
+
+import (
+	"context"
+
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+// WaitGroup waits for a collection of Completions to complete.
+type WaitGroup struct {
+	// ctx is the context of all the Completions in the wait group.
+	ctx context.Context
+
+	// counterLocker locks all calls to AddCompletion and Wait, which must not
+	// be called concurrently.
+	counterLocker lock.Mutex
+
+	// pendingCompletions is the list of Completions returned by
+	// AddCompletion() which have not yet been completed.
+	pendingCompletions []*Completion
+}
+
+// NewWaitGroup returns a new WaitGroup using the given context.
+func NewWaitGroup(ctx context.Context) *WaitGroup {
+	return &WaitGroup{ctx: ctx}
+}
+
+// Context returns the context of all the Completions in the wait group.
+func (wg *WaitGroup) Context() context.Context {
+	return wg.ctx
+}
+
+// AddCompletion creates a new completion, adds it into the wait group, and
+// returns it.
+func (wg *WaitGroup) AddCompletion() *Completion {
+	wg.counterLocker.Lock()
+	defer wg.counterLocker.Unlock()
+	c := &Completion{
+		ctx:       wg.ctx,
+		completed: make(chan struct{}),
+	}
+	wg.pendingCompletions = append(wg.pendingCompletions, c)
+	return c
+}
+
+// Wait blocks until all completions added by calling AddCompletion are
+// completed, or the context is canceled, whichever happens first.
+// Returns the context's error if it is cancelled, nil otherwise.
+func (wg *WaitGroup) Wait() error {
+	wg.counterLocker.Lock()
+	defer wg.counterLocker.Unlock()
+
+Loop:
+	for i, comp := range wg.pendingCompletions {
+		select {
+		case <-comp.Completed():
+			continue Loop
+		case <-wg.ctx.Done():
+			// Complete the remaining completions to make sure their completed
+			// channels are closed.
+			for _, comp := range wg.pendingCompletions[i:] {
+				comp.Complete()
+			}
+			break Loop
+		}
+	}
+	wg.pendingCompletions = nil
+	return wg.ctx.Err()
+}
+
+// Completion provides the Complete callback to be called when an asynchronous
+// computation is completed.
+type Completion struct {
+	// ctx is the context of the wait group.
+	ctx context.Context
+
+	// completed is the channel to be closed when Complete is called the first
+	// time.
+	completed chan struct{}
+}
+
+// Context returns the context of the asynchronous computation.
+// If the context is cancelled, e.g. if it times out, the computation must be
+// cancelled.
+func (c *Completion) Context() context.Context {
+	return c.ctx
+}
+
+// Complete notifies of the completion of the asynchronous computation.
+// Idempotent.
+func (c *Completion) Complete() {
+	select {
+	case <-c.completed:
+	default:
+		close(c.completed)
+	}
+}
+
+// Completed returns a channel that's closed when the completion is completed,
+// i.e. when Complete is called the first time, or when the call to the parent
+// WaitGroup's Wait terminated because the context was canceled.
+func (c *Completion) Completed() <-chan struct{} {
+	return c.completed
+}

pkg/completion/completion_test.go
@@ -1,0 +1,165 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package completion
+
+import (
+	"testing"
+	"time"
+
+	"context"
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	//logging.ToggleDebugLogs(true)
+	TestingT(t)
+}
+
+type CompletionSuite struct{}
+
+var _ = Suite(&CompletionSuite{})
+
+const (
+	TestTimeout      = 10 * time.Second
+	WaitGroupTimeout = 250 * time.Millisecond
+	CompletionDelay  = 250 * time.Millisecond
+)
+
+func (s *CompletionSuite) TestNoCompletion(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	wg := NewWaitGroup(ctx)
+
+	// Wait should return immediately, since there are no completions.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}
+
+func (s *CompletionSuite) TestCompletionBeforeWait(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	wg := NewWaitGroup(ctx)
+
+	comp := wg.AddCompletion()
+	c.Assert(comp.Context(), Equals, ctx)
+
+	comp.Complete()
+
+	// Wait should return immediately, since the only completion is already completed.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}
+
+func (s *CompletionSuite) TestCompletionAfterWait(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	wg := NewWaitGroup(ctx)
+
+	comp := wg.AddCompletion()
+	c.Assert(comp.Context(), Equals, ctx)
+
+	go func() {
+		time.Sleep(CompletionDelay)
+		comp.Complete()
+	}()
+
+	// Wait should block until comp.Complete is called, then return nil.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}
+
+func (s *CompletionSuite) TestCompletionBeforeAndAfterWait(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	wg := NewWaitGroup(ctx)
+
+	comp1 := wg.AddCompletion()
+	c.Assert(comp1.Context(), Equals, ctx)
+
+	comp2 := wg.AddCompletion()
+	c.Assert(comp2.Context(), Equals, ctx)
+
+	comp1.Complete()
+
+	go func() {
+		time.Sleep(CompletionDelay)
+		comp2.Complete()
+	}()
+
+	// Wait should block until comp2.Complete is called, then return nil.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}
+
+func (s *CompletionSuite) TestCompletionTimeout(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	// Set a shorter timeout to shorten the test duration.
+	wgCtx, cancel := context.WithTimeout(ctx, WaitGroupTimeout)
+	defer cancel()
+	wg := NewWaitGroup(wgCtx)
+
+	comp := wg.AddCompletion()
+	c.Assert(comp.Context(), Equals, wgCtx)
+
+	// comp never completes.
+
+	// Wait should block until wgCtx expires.
+	err = wg.Wait()
+	c.Assert(err, Not(IsNil))
+	c.Assert(err, Equals, wgCtx.Err())
+
+	// Complete is idempotent and harmless, and can be called after the
+	// context is canceled.
+	comp.Complete()
+}
+
+func (s *CompletionSuite) TestCompletionMultipleCallbacks(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	// Set a shorter timeout to shorten the test duration.
+	wg := NewWaitGroup(ctx)
+
+	comp := wg.AddCompletion()
+	c.Assert(comp.Context(), Equals, ctx)
+
+	// Complete is idempotent.
+	comp.Complete()
+	comp.Complete()
+	comp.Complete()
+
+	// Wait should return immediately, since the only completion is already completed.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}

pkg/completion/doc.go
@@ -1,0 +1,58 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package completion implements a variant of sync.WaitGroup that is associated
+// with a context.Context.
+//
+// A WaitGroup is created by calling NewWaitGroup with the context:
+//
+//    wg := completion.NewWaitGroup(ctx)
+//
+// For each concurrent computation to wait for, a Completion is created by
+// calling AddCompletion and then passing it to the concurrent computation:
+//
+//    comp1 := wg.AddCompletion()
+//    DoSomethingConcurrently(..., comp1)
+//    comp2 := wg.AddCompletion()
+//    DoSomethingElse(..., comp2)
+//
+// The Completion type provides the WaitGroup's context and the Complete
+// method:
+//
+//    func (c *Completion) Context() context.Context
+//    func (c *Completion) Complete()
+//    func (c *Completion) Completed() <-chan struct{}
+//
+// The Complete method must be called when the concurrent computation is
+// completed, for instance:
+//
+//    func DoSomethingConcurrently(..., comp Completion) {
+//        ...
+//        go func() {
+//            ...
+//            // Computation is completed successfully.
+//            comp.Complete()
+//        }()
+//        ...
+//    }
+//
+// Once all Completions are created, one can wait for the completion of all
+// of the Completions by calling Wait:
+//
+//    err := wg.Wait()
+//
+// Wait blocks until either all Completions are completed, or the context is
+// canceled (e.g. times out), whichever happens first. The returned error is
+// non-nil in the case the context is canceled, nil otherwise.
+package completion

pkg/endpoint/policy.go
@@ -15,6 +15,7 @@
 package endpoint
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
@@ -25,6 +26,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
@@ -729,9 +731,11 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	}()
 
 	e.BuildMutex.Lock()
-	e.ProxyCompletions = &Completions{}
+	completionCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	e.ProxyWaitGroup = completion.NewWaitGroup(completionCtx)
 	defer func() {
-		e.ProxyCompletions = nil
+		cancel()
+		e.ProxyWaitGroup = nil
 		e.BuildMutex.Unlock()
 	}()
 

commit 76765ec6984d65346459a307052f9413d0d70072

endpoint: Hold endpoint lock when calling getLogger()
Fixes: deb2de2ce563 ("completion: Refactor proxy completion logic in a new package")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/endpoint/endpoint.go
@@ -482,12 +482,19 @@ func (e *Endpoint) WaitForProxyCompletions(proxyWaitGroup *completion.WaitGroup)
 	}
 
 	start := time.Now()
-	e.getLogger().Debug("Waiting for proxy updates to complete...")
+
+	e.Mutex.RLock()
+	logger := getLogger()
+	e.Mutex.RUnlock()
+
+	logger.Debug("Waiting for proxy updates to complete...")
+
 	err := proxyWaitGroup.Wait()
 	if err != nil {
 		return fmt.Errorf("proxy state changes failed: %s", err)
 	}
-	e.getLogger().Debug("Wait time for proxy updates: ", time.Since(start))
+	logger.Debug("Wait time for proxy updates: ", time.Since(start))
+
 	return nil
 }
 