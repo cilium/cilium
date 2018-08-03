a3c0cf2aa3e3742bcd8a2eaf84bc4602a7327718
Thomas Graf <thomas@cilium.io>
2018-08-03 22:56:17 +0200

    endpoint: Fix locking while calling endpoint.getLogger()
    Fixes several occasions of endpoint.Mutex not being held at all when calling
    endpoint.getLogger().
    
    Fixes several additional occasions of endpoint.Mutex only being held for
    writing which did not protect the e.logger field sufficiently when the cached
    logger was to be updated. Instead of fixing all callers, introduce a new
    dedicated mutex for reading and writing e.logger.
    
    Given the new mutex for the logger, some of the write locking of endpoint.Mutex
    can be relaxed to read locking.
    
    Fixes: deb2de2ce563 ("completion: Refactor proxy completion logic in a new package")
    Fixes: 1cd2b3092d6c ("k8s: Add CiliumEndpoint sync GC controller")
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/endpoint/policy.go
+++ pkg/endpoint/policy.go
@@ -385,6 +385,7 @@ func (e *Endpoint) IngressOrEgressIsEnforced() bool {
 		e.Options.IsEnabled(option.EgressPolicy)
 }
 
+// must be called with endpoint.Mutex held for reading
 func (e *Endpoint) updateNetworkPolicy(owner Owner, proxyWaitGroup *completion.WaitGroup) error {
 	// Skip updating the NetworkPolicy if no policy has been calculated.
 	// This breaks a circular dependency between configuring NetworkPolicies in
@@ -689,9 +690,10 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	e.BuildMutex.Lock()
 	defer e.BuildMutex.Unlock()
 
-	e.Mutex.Lock()
-	e.getLogger().Debug("Regenerating endpoint...")
-	e.Mutex.Unlock()
+	e.Mutex.RLock()
+	scopedLog := e.getLogger()
+	e.Mutex.RUnlock()
+	scopedLog.Debug("Regenerating endpoint...")
 
 	origDir := filepath.Join(owner.GetStateDir(), e.StringID())
 
@@ -722,7 +724,7 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	failDir := e.failedDirectoryPath()
 	os.RemoveAll(failDir) // Most likely will not exist; ignore failure.
 	if err != nil {
-		e.getLogger().WithFields(logrus.Fields{
+		scopedLog.WithFields(logrus.Fields{
 			logfields.Path: failDir,
 		}).Warn("Generating BPF for endpoint failed, keeping stale directory.")
 		os.Rename(tmpDir, failDir)
@@ -741,7 +743,7 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 		os.RemoveAll(tmpDir)
 
 		if err2 := os.Rename(backupDir, origDir); err2 != nil {
-			e.getLogger().WithFields(logrus.Fields{
+			scopedLog.WithFields(logrus.Fields{
 				logfields.Path: backupDir,
 			}).Warn("Restoring directory for endpoint failed, endpoint " +
 				"is in inconsistent state. Keeping stale directory.")
@@ -774,7 +776,7 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	// compiled for
 	e.bumpPolicyRevision(revision)
 
-	e.getLogger().Info("Endpoint policy recalculated")
+	scopedLog.Info("Endpoint policy recalculated")
 
 	return nil
 }
@@ -793,10 +795,10 @@ func (e *Endpoint) Regenerate(owner Owner, reason string) <-chan bool {
 	go func(owner Owner, req *Request, e *Endpoint) {
 		buildSuccess := true
 
-		e.Mutex.Lock()
+		e.Mutex.RLock()
 		// This must be accessed in a locked section, so we grab it here.
 		scopedLog := e.getLogger()
-		e.Mutex.Unlock()
+		e.Mutex.RUnlock()
 
 		// We should only queue the request after we use all the endpoint's
 		// lock/unlock. Otherwise this can get a deadlock if the endpoint is
