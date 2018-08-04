cilium/cilium

commit deb2de2ce563740be063537afc009f0deca4ba01

completion: Refactor proxy completion logic in a new package
Move the completion into its own package:
github.com/cilium/cilium/pkg/completion.
Rename CompletionContainer into WaitGroup to reflect the similarity
with sync.WaitGroup.
Refactor Completion and WaitGroup to take a Context and handle
context cancellation.
Rename Completion.Completed into Complete to make it a verb.
Added Completion.Completed method to return a channel, to make it
easier to use in unit tests.

Revert the (de)serialization of the ProxyCompletions field to/from
JSON within the Endpoint struct, and rename ProxyCompletions into
ProxyWaitGroup.

Signed-off-by: Romain Lenglet <romain@covalent.io>

pkg/endpoint/endpoint.go
@@ -17,6 +17,7 @@ package endpoint
 import (
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -24,13 +25,13 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/labels"
 	pkgLabels "github.com/cilium/cilium/pkg/labels"
@@ -337,53 +338,21 @@ type Endpoint struct {
 	// proxy redirects to remove later during the build
 	proxiesToRemove map[string]bool
 
-	// Pending completions, protected by the BuildMutex!
-	ProxyCompletions *Completions
-}
-
-// Completions maintains the state needed to register and wait for asynchronous completions.
-type Completions struct {
-	wg     sync.WaitGroup
-	lock   lock.Mutex
-	errors int
-	done   bool
-}
-
-// Completed is called when an asynchronous event is completed.
-// May be called from any goroutine without holding any locks
-func (c *Completions) Completed(success bool) {
-	log.Debug("completions.Completed: ", success)
-	// 'done' is used to catch late callbacks (after Wait()), non-locked access on purpose.
-	if c.done {
-		log.Fatal("Completed called after Wait!")
-	}
-	if !success {
-		c.lock.Lock()
-		c.errors++
-		c.lock.Unlock()
-	}
-	c.wg.Done()
-}
-
-// AddCompletion increases the count of 'Completed()' callbacks we need by 1.
-// Called with BuildMutex held
-func (e *Endpoint) AddCompletion() (policy.Completion, time.Duration) {
-	e.ProxyCompletions.wg.Add(1)
-	return e.ProxyCompletions, time.Duration(10) * time.Second
+	// ProxyWaitGroup waits for pending proxy changes to complete.
+	// You must hold Endpoint.BuildMutex to read or write it.
+	ProxyWaitGroup *completion.WaitGroup `json:"-"`
 }
 
 // WaitForProxyCompletions blocks until all proxy changes have been completed.
-// Called with BuildMutex held
+// Called with BuildMutex held.
 func (e *Endpoint) WaitForProxyCompletions() error {
 	start := time.Now()
-	log.Debug("Waiting for proxy updates to complete...")
-	e.ProxyCompletions.wg.Wait()
-	// Wait is done, no parallel access any more
-	e.ProxyCompletions.done = true
-	if e.ProxyCompletions.errors > 0 {
-		return fmt.Errorf("%d proxy state changes failed", e.ProxyCompletions.errors)
+	e.getLogger().Debug("Waiting for proxy updates to complete...")
+	err := e.ProxyWaitGroup.Wait()
+	if err != nil {
+		return errors.New("proxy state changes failed")
 	}
-	log.Debug("Wait time for proxy updates: ", time.Since(start))
+	e.getLogger().Debug("Wait time for proxy updates: ", time.Since(start))
 	return nil
 }
 

pkg/endpoint/policy.go
@@ -15,6 +15,7 @@
 package endpoint
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
@@ -25,6 +26,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
@@ -729,9 +731,11 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	}()
 
 	e.BuildMutex.Lock()
-	e.ProxyCompletions = &Completions{}
+	completionCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	e.ProxyWaitGroup = completion.NewWaitGroup(completionCtx)
 	defer func() {
-		e.ProxyCompletions = nil
+		cancel()
+		e.ProxyWaitGroup = nil
 		e.BuildMutex.Unlock()
 	}()
 

daemon/endpoint.go
@@ -16,13 +16,16 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
 	"github.com/cilium/cilium/pkg/apierror"
+	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/ipam"
@@ -314,7 +317,9 @@ func (d *Daemon) deleteEndpoint(ep *endpoint.Endpoint) int {
 
 	// Wait for existing builds to complete and prevent further builds
 	ep.BuildMutex.Lock()
-	ep.ProxyCompletions = &endpoint.Completions{}
+	completionCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	ep.ProxyWaitGroup = completion.NewWaitGroup(completionCtx)
 
 	// Lock out any other writers to the endpoint
 	ep.Mutex.Lock()
@@ -392,7 +397,7 @@ func (d *Daemon) deleteEndpoint(ep *endpoint.Endpoint) int {
 	if err != nil {
 		scopedLog.WithError(err).Warn("Error removing proxy redirects.")
 	}
-	ep.ProxyCompletions = nil
+	ep.ProxyWaitGroup = nil
 	ep.BuildMutex.Unlock()
 
 	return errors

pkg/policy/policy.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/labels"
@@ -26,18 +25,6 @@ import (
 
 	"github.com/op/go-logging"
 )
-
-// Completion is an interface for an individual asynchronous success/failure
-// notification.
-type Completion interface {
-	Completed(bool)
-}
-
-// CompletionContainer is an interface for pooling asynchronous success/failure
-// notifications.
-type CompletionContainer interface {
-	AddCompletion() (Completion, time.Duration)
-}
 
 type Tracing int
 

CODEOWNERS
@@ -64,6 +64,7 @@ monitor/payload @cilium/api
 pkg/apierror/ @cilium/api
 pkg/apisocket/ @cilium/api
 pkg/bpf/ @cilium/bpf
+pkg/completion/ @cilium/proxy
 pkg/endpoint/ @cilium/endpoint
 pkg/envoy/ @cilium/proxy
 pkg/health/ @cilium/health

daemon/daemon.go
@@ -124,7 +124,7 @@ func (d *Daemon) UpdateProxyRedirect(e *endpoint.Endpoint, l4 *policy.L4Filter) 
 		return 0, fmt.Errorf("can't redirect, proxy disabled")
 	}
 
-	r, err := d.l7Proxy.CreateOrUpdateRedirect(l4, e.ProxyID(l4), e, d, e)
+	r, err := d.l7Proxy.CreateOrUpdateRedirect(l4, e.ProxyID(l4), e, d, e.ProxyWaitGroup)
 	if err != nil {
 		return 0, err
 	}
@@ -143,7 +143,7 @@ func (d *Daemon) RemoveProxyRedirect(e *endpoint.Endpoint, id string) error {
 		logfields.EndpointID: e.ID,
 		logfields.L4PolicyID: id,
 	}).Debug("Removing redirect to endpoint")
-	return d.l7Proxy.RemoveRedirect(id, e)
+	return d.l7Proxy.RemoveRedirect(id, e.ProxyWaitGroup)
 }
 
 // QueueEndpointBuild puts the given request in the endpoints queue for

pkg/completion/completion.go
@@ -1,0 +1,118 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package completion
+
+import (
+	"context"
+
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+// WaitGroup waits for a collection of Completions to complete.
+type WaitGroup struct {
+	// ctx is the context of all the Completions in the wait group.
+	ctx context.Context
+
+	// counterLocker locks all calls to AddCompletion and Wait, which must not
+	// be called concurrently.
+	counterLocker lock.Mutex
+
+	// pendingCompletions is the list of Completions returned by
+	// AddCompletion() which have not yet been completed.
+	pendingCompletions []*Completion
+}
+
+// NewWaitGroup returns a new WaitGroup using the given context.
+func NewWaitGroup(ctx context.Context) *WaitGroup {
+	return &WaitGroup{ctx: ctx}
+}
+
+// Context returns the context of all the Completions in the wait group.
+func (wg *WaitGroup) Context() context.Context {
+	return wg.ctx
+}
+
+// AddCompletion creates a new completion, adds it into the wait group, and
+// returns it.
+func (wg *WaitGroup) AddCompletion() *Completion {
+	wg.counterLocker.Lock()
+	defer wg.counterLocker.Unlock()
+	c := &Completion{
+		ctx:       wg.ctx,
+		completed: make(chan struct{}),
+	}
+	wg.pendingCompletions = append(wg.pendingCompletions, c)
+	return c
+}
+
+// Wait blocks until all completions added by calling AddCompletion are
+// completed, or the context is canceled, whichever happens first.
+// Returns the context's error if it is cancelled, nil otherwise.
+func (wg *WaitGroup) Wait() error {
+	wg.counterLocker.Lock()
+	defer wg.counterLocker.Unlock()
+
+Loop:
+	for i, comp := range wg.pendingCompletions {
+		select {
+		case <-comp.Completed():
+			continue Loop
+		case <-wg.ctx.Done():
+			// Complete the remaining completions to make sure their completed
+			// channels are closed.
+			for _, comp := range wg.pendingCompletions[i:] {
+				comp.Complete()
+			}
+			break Loop
+		}
+	}
+	wg.pendingCompletions = nil
+	return wg.ctx.Err()
+}
+
+// Completion provides the Complete callback to be called when an asynchronous
+// computation is completed.
+type Completion struct {
+	// ctx is the context of the wait group.
+	ctx context.Context
+
+	// completed is the channel to be closed when Complete is called the first
+	// time.
+	completed chan struct{}
+}
+
+// Context returns the context of the asynchronous computation.
+// If the context is cancelled, e.g. if it times out, the computation must be
+// cancelled.
+func (c *Completion) Context() context.Context {
+	return c.ctx
+}
+
+// Complete notifies of the completion of the asynchronous computation.
+// Idempotent.
+func (c *Completion) Complete() {
+	select {
+	case <-c.completed:
+	default:
+		close(c.completed)
+	}
+}
+
+// Completed returns a channel that's closed when the completion is completed,
+// i.e. when Complete is called the first time, or when the call to the parent
+// WaitGroup's Wait terminated because the context was canceled.
+func (c *Completion) Completed() <-chan struct{} {
+	return c.completed
+}

pkg/completion/completion_test.go
@@ -1,0 +1,165 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package completion
+
+import (
+	"testing"
+	"time"
+
+	"context"
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	//logging.ToggleDebugLogs(true)
+	TestingT(t)
+}
+
+type CompletionSuite struct{}
+
+var _ = Suite(&CompletionSuite{})
+
+const (
+	TestTimeout      = 10 * time.Second
+	WaitGroupTimeout = 250 * time.Millisecond
+	CompletionDelay  = 250 * time.Millisecond
+)
+
+func (s *CompletionSuite) TestNoCompletion(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	wg := NewWaitGroup(ctx)
+
+	// Wait should return immediately, since there are no completions.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}
+
+func (s *CompletionSuite) TestCompletionBeforeWait(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	wg := NewWaitGroup(ctx)
+
+	comp := wg.AddCompletion()
+	c.Assert(comp.Context(), Equals, ctx)
+
+	comp.Complete()
+
+	// Wait should return immediately, since the only completion is already completed.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}
+
+func (s *CompletionSuite) TestCompletionAfterWait(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	wg := NewWaitGroup(ctx)
+
+	comp := wg.AddCompletion()
+	c.Assert(comp.Context(), Equals, ctx)
+
+	go func() {
+		time.Sleep(CompletionDelay)
+		comp.Complete()
+	}()
+
+	// Wait should block until comp.Complete is called, then return nil.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}
+
+func (s *CompletionSuite) TestCompletionBeforeAndAfterWait(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	wg := NewWaitGroup(ctx)
+
+	comp1 := wg.AddCompletion()
+	c.Assert(comp1.Context(), Equals, ctx)
+
+	comp2 := wg.AddCompletion()
+	c.Assert(comp2.Context(), Equals, ctx)
+
+	comp1.Complete()
+
+	go func() {
+		time.Sleep(CompletionDelay)
+		comp2.Complete()
+	}()
+
+	// Wait should block until comp2.Complete is called, then return nil.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}
+
+func (s *CompletionSuite) TestCompletionTimeout(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	// Set a shorter timeout to shorten the test duration.
+	wgCtx, cancel := context.WithTimeout(ctx, WaitGroupTimeout)
+	defer cancel()
+	wg := NewWaitGroup(wgCtx)
+
+	comp := wg.AddCompletion()
+	c.Assert(comp.Context(), Equals, wgCtx)
+
+	// comp never completes.
+
+	// Wait should block until wgCtx expires.
+	err = wg.Wait()
+	c.Assert(err, Not(IsNil))
+	c.Assert(err, Equals, wgCtx.Err())
+
+	// Complete is idempotent and harmless, and can be called after the
+	// context is canceled.
+	comp.Complete()
+}
+
+func (s *CompletionSuite) TestCompletionMultipleCallbacks(c *C) {
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), TestTimeout)
+	defer cancel()
+
+	// Set a shorter timeout to shorten the test duration.
+	wg := NewWaitGroup(ctx)
+
+	comp := wg.AddCompletion()
+	c.Assert(comp.Context(), Equals, ctx)
+
+	// Complete is idempotent.
+	comp.Complete()
+	comp.Complete()
+	comp.Complete()
+
+	// Wait should return immediately, since the only completion is already completed.
+	err = wg.Wait()
+	c.Assert(err, IsNil)
+}

commit 3cff5d42e772d1d5403e644aac4b434e8f07aba2

endpoint: Fix locking while calling endpoint.getLogger()
Fixes several occasions of endpoint.Mutex not being held at all when calling
endpoint.getLogger().

Fixes several additional occasions of endpoint.Mutex only being held for
writing which did not protect the e.logger field sufficiently when the cached
logger was to be updated. Instead of fixing all callers, introduce a new
dedicated mutex for reading and writing e.logger.

Given the new mutex for the logger, some of the write locking of endpoint.Mutex
can be relaxed to read locking.

Fixes: deb2de2ce563 ("completion: Refactor proxy completion logic in a new package")
Fixes: 1cd2b3092d6c ("k8s: Add CiliumEndpoint sync GC controller")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/endpoint/endpoint.go
@@ -292,7 +292,8 @@ type Endpoint struct {
 	// ID of the endpoint, unique in the scope of the node
 	ID uint16
 
-	// Mutex protects write operations to this endpoint structure
+	// Mutex protects write operations to this endpoint structure except
+	// for the logger field which has its own mutex
 	Mutex lock.RWMutex `json:"-"`
 
 	// ContainerName is the name given to the endpoint by the container runtime
@@ -439,6 +440,12 @@ type Endpoint struct {
 	// You must hold Endpoint.Mutex to read or write it (but not to log with it).
 	logger *logrus.Entry
 
+	// loggerMutex protects write operations to the logger field.
+	//
+	// NOTE: If both endpoint.Mutex and endpoint.loggerMutex must be held,
+	// then endpoint.Mutex must *always* be acquired first.
+	loggerMutex lock.RWMutex
+
 	// controllers is the list of async controllers syncing the endpoint to
 	// other resources
 	controllers controller.Manager
@@ -482,18 +489,28 @@ func (e *Endpoint) WaitForProxyCompletions(proxyWaitGroup *completion.WaitGroup)
 	}
 
 	start := time.Now()
-	e.getLogger().Debug("Waiting for proxy updates to complete...")
+
+	e.Mutex.RLock()
+	logger := e.getLogger()
+	e.Mutex.RUnlock()
+
+	logger.Debug("Waiting for proxy updates to complete...")
+
 	err := proxyWaitGroup.Wait()
 	if err != nil {
 		return fmt.Errorf("proxy state changes failed: %s", err)
 	}
-	e.getLogger().Debug("Wait time for proxy updates: ", time.Since(start))
+	logger.Debug("Wait time for proxy updates: ", time.Since(start))
+
 	return nil
 }
 
 // RunK8sCiliumEndpointSync starts a controller that syncronizes the endpoint
 // to the corresponding k8s CiliumEndpoint CRD
 // CiliumEndpoint objects have the same name as the pod they represent
+//
+// Endpoint mutex must be held for now. This is guaranteed via
+// endpointmanager.Insert() but is really not ideal.
 func (e *Endpoint) RunK8sCiliumEndpointSync() {
 	var (
 		endpointID     = e.ID
@@ -1536,6 +1553,8 @@ func (e *Endpoint) LogStatusOKLocked(typ StatusType, msg string) {
 	e.logStatusLocked(typ, OK, msg)
 }
 
+// logStatusLocked logs a status message
+// must be called with endpoint.Mutex held
 func (e *Endpoint) logStatusLocked(typ StatusType, code StatusCode, msg string) {
 	e.Status.indexMU.Lock()
 	defer e.Status.indexMU.Unlock()
@@ -1583,9 +1602,9 @@ func (e UpdateStateChangeError) Error() string { return e.msg }
 // if there was an issue triggering policy updates for the given endpoint,
 // or if endpoint regeneration was unable to be triggered.
 func (e *Endpoint) Update(owner Owner, cfg *models.EndpointConfigurationSpec) error {
+	e.Mutex.Lock()
 	e.getLogger().WithField("configuration-options", cfg).Debug("updating endpoint configuration options")
 
-	e.Mutex.Lock()
 	if err := e.Options.Validate(cfg.Options); err != nil {
 		e.Mutex.Unlock()
 		return UpdateValidationError{err.Error()}
@@ -1686,9 +1705,11 @@ func (e *Endpoint) replaceInformationLabels(l pkgLabels.Labels) {
 	}
 	e.OpLabels.OrchestrationInfo.MarkAllForDeletion()
 
+	scopedLog := e.getLogger()
+
 	for _, v := range l {
 		if e.OpLabels.OrchestrationInfo.UpsertLabel(v) {
-			e.getLogger().WithField(logfields.Labels, logfields.Repr(v)).Debug("Assigning information label")
+			scopedLog.WithField(logfields.Labels, logfields.Repr(v)).Debug("Assigning information label")
 		}
 	}
 	e.OpLabels.OrchestrationInfo.DeleteMarked()
@@ -1710,12 +1731,14 @@ func (e *Endpoint) replaceIdentityLabels(l pkgLabels.Labels) int {
 	e.OpLabels.OrchestrationIdentity.MarkAllForDeletion()
 	e.OpLabels.Disabled.MarkAllForDeletion()
 
+	scopedLog := e.getLogger()
+
 	for k, v := range l {
 		// A disabled identity label stays disabled without value updates
 		if e.OpLabels.Disabled[k] != nil {
 			e.OpLabels.Disabled[k].ClearDeletionMark()
 		} else if e.OpLabels.OrchestrationIdentity.UpsertLabel(v) {
-			e.getLogger().WithField(logfields.Labels, logfields.Repr(v)).Debug("Assigning security relevant label")
+			scopedLog.WithField(logfields.Labels, logfields.Repr(v)).Debug("Assigning security relevant label")
 			changed = true
 		}
 	}

pkg/endpoint/log.go
@@ -25,15 +25,20 @@ var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "endpoint")
 
 // logger returns a logrus object with EndpointID, ContainerID and the Endpoint
 // revision fields.
-// Note: You must hold Endpoint.Mutex
+// Note: You must hold Endpoint.Mutex for reading
 func (e *Endpoint) getLogger() *logrus.Entry {
 	e.updateLogger()
-	return e.logger
+
+	e.loggerMutex.RLock()
+	logger := e.logger
+	e.loggerMutex.RUnlock()
+
+	return logger
 }
 
 // updateLogger creates a logger instance specific to this endpoint. It will
 // create a custom Debug logger for this endpoint when the option on it is set.
-// Note: You must hold Endpoint.Mutex
+// Note: You must hold Endpoint.Mutex for reading
 func (e *Endpoint) updateLogger() {
 	containerID := e.getShortContainerID()
 
@@ -70,8 +75,8 @@ func (e *Endpoint) updateLogger() {
 		baseLogger.SetLevel(logrus.DebugLevel)
 	}
 
-	// update the logger object.
-	// Note: endpoint.Mutex protects the reference but not the logger objects. We
+	e.loggerMutex.Lock()
+	// Note: endpoint.loggerMutex protects the reference but not the logger objects. We
 	// cannot update the old object directly as that could be racey.
 	e.logger = baseLogger.WithFields(logrus.Fields{
 		logfields.EndpointID:     e.ID,
@@ -81,4 +86,5 @@ func (e *Endpoint) updateLogger() {
 		logfields.IPv6:           e.IPv6.String(),
 		logfields.K8sPodName:     podName,
 	})
+	e.loggerMutex.Unlock()
 }

pkg/endpoint/policy.go
@@ -385,6 +385,7 @@ func (e *Endpoint) IngressOrEgressIsEnforced() bool {
 		e.Options.IsEnabled(option.EgressPolicy)
 }
 
+// must be called with endpoint.Mutex held for reading
 func (e *Endpoint) updateNetworkPolicy(owner Owner, proxyWaitGroup *completion.WaitGroup) error {
 	// Skip updating the NetworkPolicy if no policy has been calculated.
 	// This breaks a circular dependency between configuring NetworkPolicies in
@@ -689,9 +690,10 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	e.BuildMutex.Lock()
 	defer e.BuildMutex.Unlock()
 
-	e.Mutex.Lock()
-	e.getLogger().Debug("Regenerating endpoint...")
-	e.Mutex.Unlock()
+	e.Mutex.RLock()
+	scopedLog := e.getLogger()
+	e.Mutex.RUnlock()
+	scopedLog.Debug("Regenerating endpoint...")
 
 	origDir := filepath.Join(owner.GetStateDir(), e.StringID())
 
@@ -722,7 +724,7 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	failDir := e.failedDirectoryPath()
 	os.RemoveAll(failDir) // Most likely will not exist; ignore failure.
 	if err != nil {
-		e.getLogger().WithFields(logrus.Fields{
+		scopedLog.WithFields(logrus.Fields{
 			logfields.Path: failDir,
 		}).Warn("Generating BPF for endpoint failed, keeping stale directory.")
 		os.Rename(tmpDir, failDir)
@@ -741,7 +743,7 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 		os.RemoveAll(tmpDir)
 
 		if err2 := os.Rename(backupDir, origDir); err2 != nil {
-			e.getLogger().WithFields(logrus.Fields{
+			scopedLog.WithFields(logrus.Fields{
 				logfields.Path: backupDir,
 			}).Warn("Restoring directory for endpoint failed, endpoint " +
 				"is in inconsistent state. Keeping stale directory.")
@@ -774,7 +776,7 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	// compiled for
 	e.bumpPolicyRevision(revision)
 
-	e.getLogger().Info("Endpoint policy recalculated")
+	scopedLog.Info("Endpoint policy recalculated")
 
 	return nil
 }
@@ -793,10 +795,10 @@ func (e *Endpoint) Regenerate(owner Owner, reason string) <-chan bool {
 	go func(owner Owner, req *Request, e *Endpoint) {
 		buildSuccess := true
 
-		e.Mutex.Lock()
+		e.Mutex.RLock()
 		// This must be accessed in a locked section, so we grab it here.
 		scopedLog := e.getLogger()
-		e.Mutex.Unlock()
+		e.Mutex.RUnlock()
 
 		// We should only queue the request after we use all the endpoint's
 		// lock/unlock. Otherwise this can get a deadlock if the endpoint is