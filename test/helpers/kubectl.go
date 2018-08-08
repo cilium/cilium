99fe57b1d185d16f76f07051c6a1659837d75c9d
Ian Vernon <ian@cilium.io>
2018-08-07 22:47:43 -0700

    test: add additional WaitForKubeDNSEntry checks
    If a service with the same name is installed, deleted, and installed again, it
    is possible for DNS to cache the old service IP due to long TTL. As a result,
    the lookup for an IP for DNS would succeed in `WaitForKubeDNSEntry`, because
    the old entry was cached. This would result in the function returning and
    indicating success to the caller, but tests using the service name would fail
    because the IP that was returned was not the actual service IP. Add logic to
    detect if the IP returned by looking up service names via DNS is the same as the
    IP stored for the given service in Kubernetes, and use this as a condition for
    blocking the return of `WaitForKubeDNSEntry`.
    
    If the service is headless, then do not check for the service IP stored in
    Kubernetes to match the IP returned for the service from kube-dns. This is
    because the IP for headless services in Kubernetes is 'None', which will never
    match the IP returned by kube-dns.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- test/helpers/kubectl.go
+++ test/helpers/kubectl.go
@@ -636,7 +636,7 @@ func (kub *Kubectl) WaitForKubeDNSEntry(serviceName, serviceNamespace string) er
 
 	serviceNameWithNamespace := fmt.Sprintf("%s.%s", serviceName, serviceNamespace)
 	if !strings.HasSuffix(serviceNameWithNamespace, svcSuffix) {
-		serviceNameWithNamespace = fmt.Sprintf("%s.%s", svcSuffix, svcSuffix)
+		serviceNameWithNamespace = fmt.Sprintf("%s.%s", serviceNameWithNamespace, svcSuffix)
 	}
 	// https://bugs.launchpad.net/ubuntu/+source/bind9/+bug/854705
 	digCMD := "dig +short %s @%s | grep -v -e '^;'"
@@ -645,18 +645,49 @@ func (kub *Kubectl) WaitForKubeDNSEntry(serviceName, serviceNamespace string) er
 	// established or DNS does not exist.
 	digCMDFallback := "dig +tcp %s @%s"
 
-	host, _, err := kub.GetServiceHostPort(KubeSystemNamespace, "kube-dns")
+	dnsClusterIP, _, err := kub.GetServiceHostPort(KubeSystemNamespace, "kube-dns")
 	if err != nil {
 		logger.WithError(err).Error("cannot get kube-dns service IP")
 		return err
 	}
 
 	body := func() bool {
-		res := kub.Exec(fmt.Sprintf(digCMD, serviceNameWithNamespace, host))
-		if !res.WasSuccessful() {
-			_ = kub.Exec(fmt.Sprintf(digCMDFallback, serviceNameWithNamespace, host))
+		serviceIP, _, err := kub.GetServiceHostPort(serviceNamespace, serviceName)
+		if err != nil {
+			log.WithError(err).Errorf("cannot get service IP for service %s", serviceNameWithNamespace)
+			return false
 		}
-		return res.WasSuccessful()
+
+		// ClusterIPNone denotes that this service is headless; there is no
+		// service IP for this service, and thus the IP returned by `dig` is
+		// an IP of the pod itself, not ClusterIPNone, which is what Kubernetes
+		// shows as the IP for the service for headless services.
+		if serviceIP == v1.ClusterIPNone {
+			res := kub.Exec(fmt.Sprintf(digCMD, serviceNameWithNamespace, dnsClusterIP))
+			_ = kub.Exec(fmt.Sprintf(digCMDFallback, serviceNameWithNamespace, dnsClusterIP))
+			return res.WasSuccessful()
+		}
+		log.Debugf("service is not headless; checking whether IP retrieved from DNS matches the IP for the service stored in Kubernetes")
+		res := kub.Exec(fmt.Sprintf(digCMD, serviceNameWithNamespace, dnsClusterIP))
+		serviceIPFromDNS := res.SingleOut()
+		if !govalidator.IsIP(serviceIPFromDNS) {
+			logger.Debugf("output of dig (%s) did not return an IP")
+			return false
+		}
+
+		// Due to lag between new IPs for the same service being synced between
+		// kube-apiserver and DNS, check if the IP for the service that is
+		// stored in K8s matches the IP of the service cached in DNS. These
+		// can be different, because some tests use the same service names.
+		// Wait accordingly for services to match, and for resolving the service
+		// name to resolve via DNS.
+		if !strings.Contains(serviceIPFromDNS, serviceIP) {
+			logger.Debugf("service IP retrieved from DNS (%s) does not match the IP for the service stored in Kubernetes (%s)", serviceIPFromDNS, serviceIP)
+			_ = kub.Exec(fmt.Sprintf(digCMDFallback, serviceNameWithNamespace, dnsClusterIP))
+			return false
+		}
+		logger.Debugf("service IP retrieved from DNS (%s) matches the IP for the service stored in Kubernetes (%s)", serviceIPFromDNS, serviceIP)
+		return true
 	}
 
 	return WithTimeout(
