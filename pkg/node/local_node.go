cilium/cilium

commit 4a7d484af44b6e68f4e16dd9587704e449d9648c

Daemon: Update Health endpoints IP on localNode.
On commit `db248247854f7a161bd9009e01fa39cdbc2bfcd4` the GetLocalNode
changed and the values of the Health endpoints were now no updated
correctly into the localnode.

This commit updates HealthIps for localnode.

Fix #5111

Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

pkg/node/local_node.go
@@ -17,8 +17,9 @@ package node
 import (
 	"github.com/cilium/cilium/pkg/option"
 
-	"k8s.io/api/core/v1"
 	"time"
+
+	"k8s.io/api/core/v1"
 )
 
 var localNode Node
@@ -67,4 +68,10 @@ func ConfigureLocalNode() error {
 		}
 	}()
 	return nil
+}
+
+// NotifyLocalNodeUpdated Update local node information in the key-value
+// storage
+func NotifyLocalNodeUpdated() error {
+	return nodeStore.UpdateLocalKeySync(GetLocalNode())
 }

daemon/daemon.go
@@ -1351,10 +1351,20 @@ func NewDaemon() (*Daemon, error) {
 	if err != nil {
 		log.WithError(err).Fatal("Error while allocating cilium-health IP")
 	}
-	node.SetIPv4HealthIP(health4)
-	node.SetIPv6HealthIP(health6)
+
+	err = node.SetIPv4HealthIP(health4)
+	if err != nil {
+		log.WithError(err).Fatal("Error while set health IPv4 ip on the local node.")
+	}
+
+	err = node.SetIPv6HealthIP(health6)
+	if err != nil {
+		log.WithError(err).Fatal("Error while set health IPv6 ip on the local node.")
+	}
+
 	log.Debugf("IPv4 health endpoint address: %s", node.GetIPv4HealthIP())
 	log.Debugf("IPv6 health endpoint address: %s", node.GetIPv6HealthIP())
+	node.NotifyLocalNodeUpdated()
 
 	d.startStatusCollector()
 	d.dnsPoller = fqdn.NewDNSPoller(fqdn.DNSPollerConfig{

pkg/node/node_address.go
@@ -356,8 +356,15 @@ func GetIPv4HealthIP() net.IP {
 }
 
 // SetIPv4HealthIP sets the IPv4 address of the local cilium-health endpoint
-func SetIPv4HealthIP(ip net.IP) {
+func SetIPv4HealthIP(ip net.IP) error {
 	ipv4HealthAddress = ip
+
+	localNode := GetLocalNode()
+	if localNode == nil {
+		return fmt.Errorf("cannot get local node configuration")
+	}
+	localNode.IPv4HealthIP = ip
+	return nil
 }
 
 // GetIPv6HealthIP returns the IPv6 address of the local cilium-health endpoint
@@ -366,8 +373,15 @@ func GetIPv6HealthIP() net.IP {
 }
 
 // SetIPv6HealthIP sets the IPv6 address of the local cilium-health endpoint
-func SetIPv6HealthIP(ip net.IP) {
+func SetIPv6HealthIP(ip net.IP) error {
 	ipv6HealthAddress = ip
+
+	localNode := GetLocalNode()
+	if localNode == nil {
+		return fmt.Errorf("cannot get local node configuration")
+	}
+	localNode.IPv6HealthIP = ip
+	return nil
 }
 
 // GetIPv6NodeRoute returns a route pointing to the IPv6 node address

commit 4125df0d0fb68b4d63716d48649329e7e4fe4fa4

node: Fix panic when node store is not initialized yet
Because node store registration happens in the background, the nodeStore
pointer can still be nil at the time the local node is updated first.

Fixes: 4a7d484af44b ("Daemon: Update Health endpoints IP on localNode.")
Fixes: #5137

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/node/local_node.go
@@ -15,14 +15,17 @@
 package node
 
 import (
-	"github.com/cilium/cilium/pkg/option"
-
 	"time"
+
+	"github.com/cilium/cilium/pkg/option"
 
 	"k8s.io/api/core/v1"
 )
 
-var localNode Node
+var (
+	localNode      Node
+	nodeRegistered = make(chan struct{})
+)
 
 // GetLocalNode returns the identity and node spec for the local node
 func GetLocalNode() *Node {
@@ -53,7 +56,6 @@ func ConfigureLocalNode() error {
 
 	UpdateNode(&localNode, TunnelRoute, nil)
 
-	nodeRegistered := make(chan struct{})
 	go func() {
 		if err := registerNode(); err != nil {
 			log.WithError(err).Fatal("Unable to initialize local node")
@@ -73,5 +75,12 @@ func ConfigureLocalNode() error {
 // NotifyLocalNodeUpdated Update local node information in the key-value
 // storage
 func NotifyLocalNodeUpdated() error {
-	return nodeStore.UpdateLocalKeySync(GetLocalNode())
+	go func() {
+		<-nodeRegistered
+		if err := nodeStore.UpdateLocalKeySync(GetLocalNode()); err != nil {
+			log.WithError(err).Error("Unable to propagate local node change to kvstore")
+		}
+	}()
+
+	return nil
 }