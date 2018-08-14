27a2fae1bb87960601548db152f430db089e3a72
Joe Stringer <joe@covalent.io>
2018-08-14 16:30:55 +0200

    docs: Fix theme paths so RTD picks up in-tree theme
    [ upstream commit 8e7979e5b3c4b1745ad7753c9b0a86ff49d5e0b7 ]
    
    Signed-off-by: Joe Stringer <joe@covalent.io>
    Signed-off-by: Maciej Kwiek <maciej.iai@gmail.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -118,7 +118,7 @@ html_context = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['images', '_static']
+html_static_path = ['images', '_static', '_themes/sphinx_rtd_theme/sphinx_rtd_theme']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
