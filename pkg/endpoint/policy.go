cilium/cilium

commit 70b82a69f914347938788d73e674edcb2ad0a0c2

metrics: Expose endpoint and policy computation time metrics
This commit adds changes to export endpoint and policy computation time
metrics to prometheus. To have an idea of the distribution, assuming a normal form, we need all 3:

1. count (policy or endpoint)
2. sum of time taken to regenerate
3. sum of squares of time taken to regenerate

Policy and Endpoint count are already exposed as follows:
1. EndpointRegenerationCount gives endpoint regenerations that have completed.
2. PolicyCount and PolicyImportErrors gives total successful policies imported.

This commit adds sum of time taken to regenerate and sum of squares of time taken to regenerate
for policy generation/computation and endpoint regeneration.

Fixes: #4396
Signed-Off-By: Manali Bhutiyani <manali@covalent.io>

pkg/endpoint/policy.go
@@ -16,6 +16,7 @@ package endpoint
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -451,7 +452,8 @@ func (e *Endpoint) updateNetworkPolicy(owner Owner, proxyWaitGroup *completion.W
 //  - changed: true if the policy was changed for this endpoint;
 //  - err: error in case of an error.
 // Must be called with endpoint mutex held.
-func (e *Endpoint) regeneratePolicy(owner Owner, opts models.ConfigurationMap) (bool, error) {
+func (e *Endpoint) regeneratePolicy(owner Owner, opts models.ConfigurationMap) (isPolicyComp bool, err error) {
+	var labelsMap *identityPkg.IdentityCache
 	// Dry mode does not regenerate policy via bpf regeneration, so we let it pass
 	// through. Some bpf/redirect updates are skipped in that case.
 	//
@@ -477,11 +479,26 @@ func (e *Endpoint) regeneratePolicy(owner Owner, opts models.ConfigurationMap) (
 	// GH-1128 should allow optimizing this away, but currently we can't
 	// reliably know if the KV-store has changed or not, so we must scan
 	// through it each time.
-	labelsMap, err := getLabelsMap()
+	labelsMap, err = getLabelsMap()
 	if err != nil {
 		e.getLogger().WithError(err).Debug("Received error while evaluating policy")
 		return false, err
 	}
+
+	regenerateStart := time.Now()
+	// Capture successful regeneration time
+	defer func() {
+		if err == nil && isPolicyComp {
+			regenerateTimeNs := time.Since(regenerateStart)
+			regenerateTimeSec := float64(regenerateTimeNs) / float64(time.Second)
+			e.getLogger().WithField(logfields.PolicyRegenerationTime, time.Since(regenerateStart).String()).
+				Info("Regeneration of policy has completed")
+			metrics.PolicyRegenerationCount.Inc()
+			metrics.PolicyRegenerationTime.Add(regenerateTimeSec)
+			metrics.PolicyRegenerationTimeSquare.Add(math.Pow(regenerateTimeSec, 2))
+		}
+	}()
+
 	// Use the old labelsMap instance if the new one is still the same.
 	// Later we can compare the pointers to figure out if labels have changed or not.
 	if reflect.DeepEqual(e.LabelsMap, labelsMap) {
@@ -625,12 +642,24 @@ func (e *Endpoint) updateAndOverrideEndpointOptions(owner Owner, opts models.Con
 
 // Called with e.Mutex UNlocked
 func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
+	var revision uint64
+	var compilationExecuted bool
+	var err error
+
 	metrics.EndpointCountRegenerating.Inc()
+	regenerateStart := time.Now()
 	defer func() {
 		metrics.EndpointCountRegenerating.Dec()
-		if retErr == nil {
+		if retErr == nil && compilationExecuted {
 			metrics.EndpointRegenerationCount.
 				WithLabelValues(metrics.LabelValueOutcomeSuccess).Inc()
+
+			// Capture successful endpoint generation time
+			regenerateTimeNs := time.Since(regenerateStart)
+			regenerateTimeSec := float64(regenerateTimeNs) / float64(time.Second)
+			e.getLogger().WithField(logfields.EndpointRegenerationTime, time.Since(regenerateStart).String()).Info("Regeneration of endpoint has completed")
+			metrics.EndpointRegenerationTime.Add(regenerateTimeSec)
+			metrics.EndpointRegenerationTimeSquare.Add(math.Pow(regenerateTimeSec, 2))
 		} else {
 			metrics.EndpointRegenerationCount.
 				WithLabelValues(metrics.LabelValueOutcomeFail).Inc()
@@ -666,7 +695,7 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 		e.Mutex.Unlock()
 	}()
 
-	revision, compilationExecuted, err := e.regenerateBPF(owner, tmpDir, reason)
+	revision, compilationExecuted, err = e.regenerateBPF(owner, tmpDir, reason)
 
 	// If generation fails, keep the directory around. If it ever succeeds
 	// again, clean up the XXX_next_fail copy.

pkg/logging/logfields/logfields.go
@@ -84,6 +84,12 @@ const (
 	// BuildDuration is the time elapsed to build a BPF program
 	BuildDuration = "buildDuration"
 
+	// PolicyRegenerationTime is the time elapsed to generate a policy
+	PolicyRegenerationTime = "policyRegenerationTime"
+
+	// EndpointRegenerationTime is the time elapsed to generate an endpoint
+	EndpointRegenerationTime = "endpointRegenerationTime"
+
 	// StartTime is the start time of an event
 	StartTime = "startTime"
 

pkg/metrics/metrics.go
@@ -92,6 +92,21 @@ var (
 	},
 		[]string{"outcome"})
 
+	// EndpointRegenerationTime is the total time taken to regenerate endpoint
+	EndpointRegenerationTime = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Name:      "endpoint_regeneration_seconds_total",
+		Help:      "Total sum of successful endpoint regeneration times",
+	})
+
+	// EndpointRegenerationTimeSquare is the sum of squares of total time taken
+	// to regenerate endpoint.
+	EndpointRegenerationTimeSquare = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Name:      "endpoint_regeneration_square_seconds_total",
+		Help:      "Total sum of squares of successful endpoint regeneration times",
+	})
+
 	// Policy
 
 	// PolicyCount is the number of policies loaded into the agent
@@ -99,6 +114,29 @@ var (
 		Namespace: Namespace,
 		Name:      "policy_count",
 		Help:      "Number of policies currently loaded",
+	})
+
+	// PolicyRegenerationCount is the total number of successful policy
+	// regenerations.
+	PolicyRegenerationCount = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Name:      "policy_regeneration_total",
+		Help:      "Total number of successful policy regenerations",
+	})
+
+	// PolicyRegenerationTime is the total time taken to generate policies
+	PolicyRegenerationTime = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Name:      "policy_regeneration_seconds_total",
+		Help:      "Total sum of successful policy regeneration times",
+	})
+
+	// PolicyRegenerationTimeSquare is the sum of squares of total time taken
+	// to generate policies
+	PolicyRegenerationTimeSquare = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: Namespace,
+		Name:      "policy_regeneration_square_seconds_total",
+		Help:      "Total sum of squares of successful policy regeneration times",
 	})
 
 	// PolicyRevision is the current policy revision number for this agent
@@ -215,8 +253,13 @@ func init() {
 
 	MustRegister(EndpointCountRegenerating)
 	MustRegister(EndpointRegenerationCount)
+	MustRegister(EndpointRegenerationTime)
+	MustRegister(EndpointRegenerationTimeSquare)
 
 	MustRegister(PolicyCount)
+	MustRegister(PolicyRegenerationCount)
+	MustRegister(PolicyRegenerationTime)
+	MustRegister(PolicyRegenerationTimeSquare)
 	MustRegister(PolicyRevision)
 	MustRegister(PolicyImportErrors)
 

commit f6d651fbf61b40578471e3dff325d9579524cdec

endpoint: Fix endpoint regeneration failure metric
The endpoint regeneration metric was being considered "failed" if the
BPF compilation was not executed, even if the overall regeneration was
successful. Fix this by not taking into account whether BPF compilation
occurred when incrementing the metric.

We could in theory split this information out into a separate metric,
however I think that with the EndpointRegenerationTimeSquare we should
be able to tell which cases involved compilation and which did not, as
we should see a bimodal distribution of endpoint regeneration times.

Fixes: 70b82a69f914 ("metrics: Expose endpoint and policy computation time metrics")
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/endpoint/policy.go
@@ -674,7 +674,7 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	regenerateStart := time.Now()
 	defer func() {
 		metrics.EndpointCountRegenerating.Dec()
-		if retErr == nil && compilationExecuted {
+		if retErr == nil {
 			metrics.EndpointRegenerationCount.
 				WithLabelValues(metrics.LabelValueOutcomeSuccess).Inc()
 