cilium/cilium

commit a6d3a5daa4c2d8e1a68cbd5c4ec5c2e87b1d4cd2

identity: Wait for initial set of security identities before restoring endpoints
Fixes: #5290

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/main.go
@@ -829,6 +829,7 @@ func runDaemon() {
 	go func() {
 		log.Info("Waiting until all pre-existing resources related to policy have been received")
 		d.k8sResourceSyncWaitGroup.Wait()
+		identity.WaitForInitialIdentities()
 		cachesSynced <- struct{}{}
 	}()
 

pkg/identity/allocator.go
@@ -96,6 +96,12 @@ func InitIdentityAllocator(owner IdentityAllocatorOwner) {
 	})
 }
 
+// WaitForInitialIdentities waits for the initial set of security identities to
+// have been received and populated into the allocator cache
+func WaitForInitialIdentities() {
+	identityAllocator.WaitForInitialSync()
+}
+
 // IdentityAllocationIsLocal returns true if a call to AllocateIdentity with
 // the given labels would not require accessing the KV store to allocate the
 // identity.

pkg/kvstore/allocator/allocator.go
@@ -196,6 +196,10 @@ type Allocator struct {
 	// stopGC is the channel used to stop the garbage collector
 	stopGC chan struct{}
 
+	// initialListDone is a channel that is closed when the initial
+	// synchronization has completed
+	initialListDone waitChan
+
 	// idPool maintains a pool of available ids for allocation.
 	idPool *idPool
 }
@@ -269,11 +273,13 @@ func NewAllocator(basePath string, typ AllocatorKey, opts ...AllocatorOption) (*
 
 	a.idPool = newIDPool(a.min, a.max)
 
+	a.initialListDone = a.mainCache.start(a)
 	go func() {
-		if err := a.mainCache.startAndWait(a); err != nil {
-			log.WithError(err).Fatalf("Unable to watch allocation prefix")
+		select {
+		case <-a.initialListDone:
+		case <-time.After(listTimeout):
+			log.Fatalf("Timeout while waiting for initial allocator state")
 		}
-
 		a.startGC()
 	}()
 
@@ -321,6 +327,11 @@ func (a *Allocator) Delete() {
 	if a.events != nil {
 		close(a.events)
 	}
+}
+
+// WaitForInitialSync waits until the initial sync is complete
+func (a *Allocator) WaitForInitialSync() {
+	<-a.initialListDone
 }
 
 // lockPath locks a key in the scope of an allocator

commit 2f825bb30a810dcfef3963b080f3c21205e16de3

pkg/identity: Wait For Initial Identities for endpoints without fixed identity
By waiting for the initial set of identities without creating the daemon
socket, Cilium would remain unavailable for kubelet to create endpoints,
breaking the etcd-operator integration.

Fixes: a6d3a5daa4c2 ("identity: Wait for initial set of security identities before restoring endpoints")
Signed-off-by: André Martins <andre@cilium.io>

daemon/main.go
@@ -836,7 +836,6 @@ func runDaemon() {
 	go func() {
 		log.Info("Waiting until all pre-existing resources related to policy have been received")
 		d.k8sResourceSyncWaitGroup.Wait()
-		identity.WaitForInitialIdentities()
 		cachesSynced <- struct{}{}
 	}()
 

daemon/state.go
@@ -175,6 +175,12 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 				scopedLog.WithError(err).Warn("Unable to restore endpoint")
 				epRegenerated <- false
 			}
+			// Wait for initial identities from the kvstore before
+			// doing any policy calculation for endpoints that don't have
+			// a fixed identity.
+			if !identity.IsFixed() {
+				identityPkg.WaitForInitialIdentities()
+			}
 
 			if err := ep.LockAlive(); err != nil {
 				scopedLog.Warn("Endpoint to restore has been deleted")

pkg/identity/identity.go
@@ -112,6 +112,12 @@ func (id *Identity) IsReserved() bool {
 	return LookupReservedIdentity(id.ID) != nil
 }
 
+// IsFixed returns whether the identity represents a fixed identity
+// (true), or not (false).
+func (id *Identity) IsFixed() bool {
+	return LookupReservedIdentity(id.ID) != nil && IsUserReservedIdentity(id.ID)
+}
+
 // NewIdentity creates a new identity
 func NewIdentity(id NumericIdentity, lbls labels.Labels) *Identity {
 	var lblArray labels.LabelArray