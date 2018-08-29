bcb8b9cb50925a8fb68d60c48a65b8f6244f400d
John Fastabend <john.fastabend@gmail.com>
2018-08-29 12:20:31 -0700

    cilium: add bpf cgroup2 sockops program
    Add initial sock_ops BPF program which will run on the cgroupv2
    BPF mount and monitor socket operations. This will be used to
    add sockets to a cilium sockmap.
    
    The following criteria must be met to add a socket to the sockmap
    which will later use bpf_redir.c to do redirect.
    
     - Must not be a service dst. This is just a limitation in
       the code at the moment we can add support after initial
       patches.
     - Must be allowed by the policy, policy_sk_egress()
     - Must be a local dst endpoint either (a) proxy or (b) endpoint
    
    IPv4 supported in this patch set IPv6 will follow shortly.
    
    I put this code into its own directory 'sockops' to make it
    clear where sockops/sk_msg code exists and where traditional
    skb based BPF code lives.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

---
+++ bpf/sockops/bpf_sockops.c
@@ -0,0 +1,136 @@
+/*
+ *  Copyright (C) 2018 Authors of Cilium
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#define SOCKMAP 1
+#define LB_L3
+
+#include <node_config.h>
+#include <bpf/api.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <sys/socket.h>
+
+#include "../lib/utils.h"
+#include "../lib/common.h"
+#include "../lib/maps.h"
+#include "../lib/lb.h"
+#include "../lib/eps.h"
+#include "../lib/events.h"
+#include "../lib/policy.h"
+
+#include "bpf_sockops.h"
+
+static inline void bpf_sock_ops_ipv4(struct bpf_sock_ops *skops)
+{
+	__u32 dip4, dport, dstID = 0;
+	struct endpoint_info *exists;
+	struct lb4_key lb4_key = {};
+	struct sock_key key = {};
+	struct lb4_service *svc;
+	int verdict;
+
+	sk_extract4_key(skops, &key);
+	sk_lb4_key(&lb4_key, &key);
+
+	/* If endpoint a service use L4/L3 stack for now. These can be
+	 * pulled in as needed.
+	 */
+	svc = __lb4_lookup_service(&lb4_key);
+	if (svc)
+		return;
+
+	/* Policy lookup required to learn proxy port */
+	if (1) {
+		struct remote_endpoint_info *info;
+
+		info = lookup_ip4_remote_endpoint(key.dip4);
+		if (info != NULL && info->sec_label)
+			dstID = info->sec_label;
+		else
+			dstID = WORLD_ID;
+	}
+
+	verdict = policy_sk_egress(dstID, key.sip4, key.dport);
+	if (redirect_to_proxy(verdict)) {
+		__be32 host_ip = IPV4_GATEWAY;
+
+		key.dip4 = key.sip4;
+		key.dport = key.sport;
+		key.sip4 = host_ip;
+		key.sport = verdict;
+
+		sock_hash_update(skops, &SOCK_OPS_MAP, &key, BPF_ANY);
+		return;
+	}
+
+	/* Lookup IPv4 address, this will return a match if:
+	 * - The destination IP address belongs to the local endpoint manage
+	 *   by Cilium.
+	 * - The destination IP address is an IP address associated with the
+	 *   host itself.
+	 * Then because these are local IPs that have passed LB/Policy/NAT
+	 * blocks redirect directly to socket.
+	 */
+	exists = __lookup_ip4_endpoint(key.dip4);
+	if (!exists)
+		return;
+
+	dip4 = key.dip4;
+	dport = key.dport;
+	key.dip4 = key.sip4;
+	key.dport = key.sport;
+	key.sip4 = dip4;
+	key.sport = dport;
+
+	sock_hash_update(skops, &SOCK_OPS_MAP, &key, BPF_ANY);
+}
+
+static inline void bpf_sock_ops_ipv6(struct bpf_sock_ops *skops)
+{
+	if (skops->remote_ip4)
+		bpf_sock_ops_ipv4(skops);
+}
+
+__section("sockops")
+int bpf_sockmap(struct bpf_sock_ops *skops)
+{
+	__u32 family, op;
+
+	family = skops->family;
+	op = skops->op;
+
+	switch (op) {
+	case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
+	case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
+		if (family == AF_INET6)
+			bpf_sock_ops_ipv6(skops);
+		else if (family == AF_INET)
+			bpf_sock_ops_ipv4(skops);
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
+
+BPF_LICENSE("GPL");
+int _version __section("version") = 1;
