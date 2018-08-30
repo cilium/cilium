582b36c0e61e538e05b1c1f8b42182680f97443b
Ian Vernon <ian@cilium.io>
2018-08-30 19:47:08 -0400

    daemon: move map-related code to map.go
    Navigating daemon.go is confusing; there is a lot of code in various places that
    could be separated out into smaller files that more logically indicate the
    function of the code contained inside of them. Move map-related code to its own
    file accordingly.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -25,7 +25,6 @@ import (
 	"path/filepath"
 	"reflect"
 	"runtime"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -57,7 +56,6 @@ import (
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/maps/ctmap"
 	ipcachemap "github.com/cilium/cilium/pkg/maps/ipcache"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
 	"github.com/cilium/cilium/pkg/maps/lxcmap"
@@ -73,7 +71,6 @@ import (
 	policyApi "github.com/cilium/cilium/pkg/policy/api"
 	"github.com/cilium/cilium/pkg/proxy"
 	"github.com/cilium/cilium/pkg/proxy/logger"
-	"github.com/cilium/cilium/pkg/u8proto"
 	"github.com/cilium/cilium/pkg/workloads"
 
 	"github.com/go-openapi/runtime/middleware"
@@ -1337,111 +1334,6 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 	fqdn.StartDNSPoller(d.dnsPoller)
 
 	return &d, restoredEndpoints, nil
-}
-
-func (d *Daemon) validateExistingMaps() error {
-	walker := func(path string, _ os.FileInfo, _ error) error {
-		return mapValidateWalker(path)
-	}
-
-	return filepath.Walk(bpf.MapPrefixPath(), walker)
-}
-
-func (d *Daemon) collectStaleMapGarbage() {
-	if option.Config.DryMode {
-		return
-	}
-	walker := func(path string, _ os.FileInfo, _ error) error {
-		return d.staleMapWalker(path)
-	}
-
-	if err := filepath.Walk(bpf.MapPrefixPath(), walker); err != nil {
-		log.WithError(err).Warn("Error while scanning for stale maps")
-	}
-}
-
-func (d *Daemon) removeStaleMap(path string) {
-	if err := os.RemoveAll(path); err != nil {
-		log.WithError(err).WithField(logfields.Path, path).Warn("Error while deleting stale map file")
-	} else {
-		log.WithField(logfields.Path, path).Info("Removed stale bpf map")
-	}
-}
-
-func (d *Daemon) removeStaleIDFromPolicyMap(id uint32) {
-	gpm, err := policymap.OpenGlobalMap(bpf.MapPath(endpoint.PolicyGlobalMapName))
-	if err == nil {
-		gpm.Delete(id, policymap.AllPorts, u8proto.All, policymap.Ingress)
-		gpm.Delete(id, policymap.AllPorts, u8proto.All, policymap.Egress)
-		gpm.Close()
-	}
-}
-
-func (d *Daemon) checkStaleMap(path string, filename string, id string) {
-	if tmp, err := strconv.ParseUint(id, 0, 16); err == nil {
-		if ep := endpointmanager.LookupCiliumID(uint16(tmp)); ep == nil {
-			d.removeStaleIDFromPolicyMap(uint32(tmp))
-			d.removeStaleMap(path)
-		}
-	}
-}
-
-func (d *Daemon) checkStaleGlobalMap(path string, filename string) {
-	globalCTinUse := endpointmanager.HasGlobalCT()
-
-	if !globalCTinUse &&
-		(filename == ctmap.MapName6Global ||
-			filename == ctmap.MapName4Global) {
-		d.removeStaleMap(path)
-	}
-}
-
-func (d *Daemon) staleMapWalker(path string) error {
-	filename := filepath.Base(path)
-
-	mapPrefix := []string{
-		policymap.MapName,
-		ctmap.MapName6,
-		ctmap.MapName4,
-		endpoint.CallsMapName,
-	}
-
-	d.checkStaleGlobalMap(path, filename)
-
-	for _, m := range mapPrefix {
-		if strings.HasPrefix(filename, m) {
-			if id := strings.TrimPrefix(filename, m); id != filename {
-				d.checkStaleMap(path, filename, id)
-			}
-		}
-	}
-
-	return nil
-}
-
-func mapValidateWalker(path string) error {
-	prefixToValidator := map[string]bpf.MapValidator{
-		policymap.MapName: policymap.Validate,
-	}
-
-	filename := filepath.Base(path)
-	for m, validate := range prefixToValidator {
-		if strings.HasPrefix(filename, m) {
-			valid, err := validate(path)
-			switch {
-			case err != nil:
-				return err
-			case !valid:
-				log.WithField(logfields.Path, filename).Info("Outdated non-persistent BPF map found, removing...")
-
-				if err := os.Remove(path); err != nil {
-					return err
-				}
-			}
-		}
-	}
-
-	return nil
 }
 
 // TriggerReloadWithoutCompile causes all BPF programs and maps to be reloaded,
--- daemon/map.go
+++ daemon/map.go
@@ -15,9 +15,21 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
 	"github.com/cilium/cilium/api/v1/models"
 	restapi "github.com/cilium/cilium/api/v1/server/restapi/daemon"
 	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/endpoint"
+	"github.com/cilium/cilium/pkg/endpointmanager"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/maps/ctmap"
+	"github.com/cilium/cilium/pkg/maps/policymap"
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/u8proto"
 
 	"github.com/go-openapi/runtime/middleware"
 )
@@ -53,4 +65,109 @@ func (h *getMap) Handle(params restapi.GetMapParams) middleware.Responder {
 	}
 
 	return restapi.NewGetMapOK().WithPayload(mapList)
+}
+
+func (d *Daemon) validateExistingMaps() error {
+	walker := func(path string, _ os.FileInfo, _ error) error {
+		return mapValidateWalker(path)
+	}
+
+	return filepath.Walk(bpf.MapPrefixPath(), walker)
+}
+
+func (d *Daemon) collectStaleMapGarbage() {
+	if option.Config.DryMode {
+		return
+	}
+	walker := func(path string, _ os.FileInfo, _ error) error {
+		return d.staleMapWalker(path)
+	}
+
+	if err := filepath.Walk(bpf.MapPrefixPath(), walker); err != nil {
+		log.WithError(err).Warn("Error while scanning for stale maps")
+	}
+}
+
+func (d *Daemon) removeStaleMap(path string) {
+	if err := os.RemoveAll(path); err != nil {
+		log.WithError(err).WithField(logfields.Path, path).Warn("Error while deleting stale map file")
+	} else {
+		log.WithField(logfields.Path, path).Info("Removed stale bpf map")
+	}
+}
+
+func (d *Daemon) removeStaleIDFromPolicyMap(id uint32) {
+	gpm, err := policymap.OpenGlobalMap(bpf.MapPath(endpoint.PolicyGlobalMapName))
+	if err == nil {
+		gpm.Delete(id, policymap.AllPorts, u8proto.All, policymap.Ingress)
+		gpm.Delete(id, policymap.AllPorts, u8proto.All, policymap.Egress)
+		gpm.Close()
+	}
+}
+
+func (d *Daemon) checkStaleMap(path string, filename string, id string) {
+	if tmp, err := strconv.ParseUint(id, 0, 16); err == nil {
+		if ep := endpointmanager.LookupCiliumID(uint16(tmp)); ep == nil {
+			d.removeStaleIDFromPolicyMap(uint32(tmp))
+			d.removeStaleMap(path)
+		}
+	}
+}
+
+func (d *Daemon) checkStaleGlobalMap(path string, filename string) {
+	globalCTinUse := endpointmanager.HasGlobalCT()
+
+	if !globalCTinUse &&
+		(filename == ctmap.MapName6Global ||
+			filename == ctmap.MapName4Global) {
+		d.removeStaleMap(path)
+	}
+}
+
+func (d *Daemon) staleMapWalker(path string) error {
+	filename := filepath.Base(path)
+
+	mapPrefix := []string{
+		policymap.MapName,
+		ctmap.MapName6,
+		ctmap.MapName4,
+		endpoint.CallsMapName,
+	}
+
+	d.checkStaleGlobalMap(path, filename)
+
+	for _, m := range mapPrefix {
+		if strings.HasPrefix(filename, m) {
+			if id := strings.TrimPrefix(filename, m); id != filename {
+				d.checkStaleMap(path, filename, id)
+			}
+		}
+	}
+
+	return nil
+}
+
+func mapValidateWalker(path string) error {
+	prefixToValidator := map[string]bpf.MapValidator{
+		policymap.MapName: policymap.Validate,
+	}
+
+	filename := filepath.Base(path)
+	for m, validate := range prefixToValidator {
+		if strings.HasPrefix(filename, m) {
+			valid, err := validate(path)
+			switch {
+			case err != nil:
+				return err
+			case !valid:
+				log.WithField(logfields.Path, filename).Info("Outdated non-persistent BPF map found, removing...")
+
+				if err := os.Remove(path); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
 }
