b2dceb956693de06b0e5939a26e155cf4812c9e1
Romain Lenglet <romain@covalent.io>
2018-08-31 18:36:27 -0700

    xds: Return a revert function on every cache update
    [ upstream commit 6eac6855a0ad028201e609405bc30fb56b4a7505 ]
    
    Signed-off-by: Romain Lenglet <romain@covalent.io>
    Signed-off-by: Ian Vernon <ian@cilium.io>

--- pkg/envoy/xds/set.go
+++ pkg/envoy/xds/set.go
@@ -61,6 +61,11 @@ type VersionedResources struct {
 	Canary bool
 }
 
+// ResourceMutatorRevertFunc is a function which reverts the effects of an update on a
+// ResourceMutator.
+// The returned version value is the set's version after update.
+type ResourceMutatorRevertFunc func(force bool) (version uint64, updated bool)
+
 // ResourceMutator provides write access to a versioned set of resources.
 // A single version is associated to all the contained resources.
 // The version is monotonically increased for any change to the set.
@@ -72,7 +77,9 @@ type ResourceMutator interface {
 	// Otherwise, the version number is not modified and the returned updated
 	// value is false.
 	// The returned version value is the set's version after update.
-	Upsert(typeURL string, resourceName string, resource proto.Message, force bool) (version uint64, updated bool)
+	// A call to the returned revert function reverts the effects of this
+	// method call.
+	Upsert(typeURL string, resourceName string, resource proto.Message, force bool) (version uint64, updated bool, revert ResourceMutatorRevertFunc)
 
 	// Delete deletes a resource from this set by name.
 	// If force is true and/or the set is actually modified (the resource is
@@ -81,7 +88,9 @@ type ResourceMutator interface {
 	// Otherwise, the version number is not modified and the returned updated
 	// value is false.
 	// The returned version value is the set's version after update.
-	Delete(typeURL string, resourceName string, force bool) (version uint64, updated bool)
+	// A call to the returned revert function reverts the effects of this
+	// method call.
+	Delete(typeURL string, resourceName string, force bool) (version uint64, updated bool, revert ResourceMutatorRevertFunc)
 
 	// Clear deletes all the resources of the given type from this set.
 	// If force is true and/or the set is actually modified (at least one
@@ -90,6 +99,7 @@ type ResourceMutator interface {
 	// Otherwise, the version number is not modified and the returned updated
 	// value is false.
 	// The returned version value is the set's version after update.
+	// This method call cannot be reverted.
 	Clear(typeURL string, force bool) (version uint64, updated bool)
 }
 
