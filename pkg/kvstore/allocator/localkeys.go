d317bc5ef786bd575a1b2a39c2257a73e367bf52
Thomas Graf <thomas@cilium.io>
2018-09-02 09:38:58 -0700

    allocator: Only re-create verified local keys
    [ upstream commit 7e5dd1052490134e0d14081736cba67b7181b9cf ]
    
    The re-creation logic recreated all local keys, even keys that were not
    verified yet. Unverified keys are keys that have been locally allocated but not
    yet synchronized with the kvstore. Upon re-creating such keys, the later
    initial creation of the key in the regular code path would no longer regard
    such keys as created and unit tests would fail. This happened when the background
    sync routine ran in between local allocation and kvstore synchronization.
    
    Fixes: #5437
    Fixes: 10e50bef7b6a ("allocator: Periodically re-create master keys for local allocations")
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/kvstore/allocator/localkeys.go
+++ pkg/kvstore/allocator/localkeys.go
@@ -135,11 +135,13 @@ func (lk *localKeys) release(key string) (lastUse bool, err error) {
 	return false, fmt.Errorf("unable to find key in local cache")
 }
 
-func (lk *localKeys) getIDs() map[ID]string {
+func (lk *localKeys) getVerifiedIDs() map[ID]string {
 	ids := map[ID]string{}
 	lk.RLock()
 	for id, localKey := range lk.ids {
-		ids[id] = localKey.key
+		if localKey.verified {
+			ids[id] = localKey.key
+		}
 	}
 	lk.RUnlock()
 
