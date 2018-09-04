7e1d88126064b366d0013327fc37ada002cd5766
Thomas Graf <thomas@cilium.io>
2018-09-03 18:39:22 -0700

    endpoint: Add metric to cover endpoint waiting in buildqueue
    ```
    msg="Completed endpoint regeneration" bpfCompilation=951.714899ms buildDuration=955.93512ms containerID=5203510c17 datapathPolicyRevision=2 desiredPolicyRevision=2 endpointID=26036 ipv4=10.11.134.178 ipv6="f00d::a0f:0:0:65b4" k8sPodName=/ mapSync="285.687µs" policyCalculation="132.505µs" prepareBuild="885.71µs" proxyConfiguration="1.004µs" proxyPolicyCalculation=1.065864ms proxyWaitForAck="136.099µs" queueWait=3.315218896s reason="restoring endpoint" totalTime=2562047h47m16.854775807s uuid=f7d7c71d-adff-11e8-83c2-0800278b5835 waitingForCTClean=557ns waitingForLock="1.218µs"
    ```
    
    Suggested by: Eloy Coto <eloy@cilium.io>
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/endpoint/builder.go
+++ pkg/endpoint/builder.go
@@ -49,6 +49,8 @@ type buildStatus struct {
 }
 
 func (e *Endpoint) newEndpointBuild(owner Owner, context *RegenerationContext) *endpointBuild {
+	context.Stats.totalTime.Start()
+	context.Stats.queueWait.Start()
 	return &endpointBuild{
 		endpoint: e,
 		owner:    owner,
@@ -78,6 +80,8 @@ func (b *endpointBuild) BuildsDequeued(nbuilds int, cancelled bool) {
 
 func (b *endpointBuild) Build() error {
 	e := b.endpoint
+
+	b.context.Stats.queueWait.End()
 
 	err := e.regenerate(b.owner, b.context)
 
