cilium/cilium

commit d440059231eaffa639a7982264b38ab2b19c4089

daemon: block until initial policy list
Previously, when Cilium was bootstrapped, endpoints were restored before the
Kubernetes watcher was started. Since Cilium does not maintain state on disk
about the policy which applied to the endpoints which are being restored, the
restore operation would regenerate endpoints without any policy rules present in
the policy repository. Soon after, the Kubernetes watcher would be started, and
policy would get sent to the Cilium policy repository; eventually, endpoints
would have the correct policy. However, in the window between when the endpoints
were restored and when the watcher was started, the lack of rules in the
repository would result in the computed policy being a default-allow for said
endpoints. This patch fixes that, by using the prior commit (restoring endpoints
after the watchers are started),  to block restoration of endpoints until the
initial list of all CiliumNetworkPolicy and NetworkPolicy resources is completed
from Kubernetes. This ensures that the restored endpoints will always have their
policy computed against the set of rules that are in Kubernetes at all times
when Cilium is ran with Kubernetes. It also ensures that no endpoints can be
created in Cilium until the initial list of policy provided to Kubernetes is
received. If the initial list does not succeed after a specified timeout of 3
minutes, the cilium-agent crashes. This ensures that the agent does not
wait infinitely if there is an issue getting the initial list of policies.

Signed-off by: Ian Vernon <ian@cilium.io>

daemon/k8s_watcher.go
@@ -74,6 +74,7 @@ const (
 	k8sAPIGroupNetworkingV1Core = "networking.k8s.io/v1::NetworkPolicy"
 	k8sAPIGroupIngressV1Beta1   = "extensions/v1beta1::Ingress"
 	k8sAPIGroupCiliumV2         = "cilium/v2::CiliumNetworkPolicy"
+	cacheSyncTimeout            = time.Duration(3 * time.Minute)
 )
 
 var (
@@ -289,7 +290,19 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 				},
 			},
 		)
+		d.k8sResourceSyncWaitGroup.Add(1)
 		go policyController.Run(wait.NeverStop)
+		go func() {
+			completed := make(<-chan struct{})
+			log.Debug("waiting for cache to synchronize for NetworkPolicies")
+			if ok := cache.WaitForCacheSync(completed, policyController.HasSynced); !ok {
+				// If we can't get NetworkPolicies for K8s, fatally exit.
+				log.Fatalf("failed to wait for cache to sync for NetworkPolicies")
+			}
+			log.Debug("caches synced for NetworkPolicies")
+			d.k8sResourceSyncWaitGroup.Done()
+		}()
+
 		d.k8sAPIGroups.addAPI(k8sAPIGroupNetworkingV1Core)
 	}
 
@@ -452,6 +465,18 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 				}
 			},
 		})
+
+		d.k8sResourceSyncWaitGroup.Add(1)
+		go func() {
+			completed := make(<-chan struct{})
+			log.Debug("waiting for cache to synchronize for CiliumNetworkPolicies")
+			if ok := cache.WaitForCacheSync(completed, ciliumV2Controller.HasSynced); !ok {
+				// If we can't get CiliumNetworkPolicies for K8s, fatally exit.
+				log.Fatalf("failed to wait for cache to sync for CiliumNetworkPolicies")
+			}
+			log.Debug("caches synced for CiliumNetworkPolicies")
+			d.k8sResourceSyncWaitGroup.Done()
+		}()
 	}
 
 	si.Start(wait.NeverStop)

daemon/daemon.go
@@ -142,6 +142,14 @@ type Daemon struct {
 	prefixLengths *counter.PrefixLengthCounter
 
 	clustermesh *clustermesh.ClusterMesh
+
+	// k8sResourceSyncWaitGroup is used to block the starting of the daemon,
+	// including regenerating restored endpoints (if specified) until all
+	// policies stored in Kubernetes are plumbed into the local Cilium
+	// repository.
+	// This prevents regeneration of endpoints before all policy rules in
+	// Kubernetes are consumed by Cilium. See GH-5038.
+	k8sResourceSyncWaitGroup sync.WaitGroup
 }
 
 // UpdateProxyRedirect updates the redirect rules in the proxy for a particular

daemon/main.go
@@ -820,10 +820,26 @@ func runDaemon() {
 		log.WithError(err).Fatal("Unable to establish connection to Kubernetes apiserver")
 	}
 
+	cachesSynced := make(chan struct{})
+
+	go func() {
+		log.Info("Waiting until all pre-existing policies have been received")
+		d.k8sResourceSyncWaitGroup.Wait()
+		cachesSynced <- struct{}{}
+	}()
+
+	select {
+	case <-cachesSynced:
+		log.Info("All pre-existing policies have been received; continuing")
+	case <-time.After(cacheSyncTimeout):
+		log.Fatalf("Timed out waiting for pre-existing policies to be received; exiting")
+	}
+
 	if option.Config.RestoreState {
-
+		// When we regenerate restored endpoints, it is guaranteed tha we have
+		// received the full list of policies present at the time the daemon
+		// is bootstrapped.
 		d.regenerateRestoredEndpoints(restoredEndpoints)
-
 		go func() {
 			if err := d.SyncLBMap(); err != nil {
 				log.WithError(err).Warn("Error while recovering endpoints")

commit 36aa22c649d7a4d84bd10bf368ccbb3c14b8289e

daemon: block endpoints addition until all policies are processed from k8s
For a large number of policies it is possible Cilium signalizes that caches
were synced with Kubernetes although they might not have been processed
and pushed down the local policy repository.

With the following diff is easy to replicate the bug:

```
diff --git a/daemon/k8s_watcher.go b/daemon/k8s_watcher.go
index da3fcba1e..3e6ddef6c 100644
--- a/daemon/k8s_watcher.go
+++ b/daemon/k8s_watcher.go
@@ -571,10 +571,13 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
                                metrics.EventTSK8s.SetToCurrentTime()
                                if cnp := copyObjToV2CNP(obj); cnp != nil {
                                        serCNPs.Enqueue(func() error {
+                                               time.Sleep(5 * time.Second)
+                                               log.Debugf("adding cilium network policy after sleeping")
                                                d.addCiliumNetworkPolicyV2(cnpStore, cnp)
                                                return nil
                                        }, serializer.NoRetry)
                                }
+                               log.Debugf("adding cilium network policy in AddFunc")
                        },
                        UpdateFunc: func(oldObj, newObj interface{}) {
                                metrics.EventTSK8s.SetToCurrentTime()
```
would give the following logs:

```
msg="cache synced" kubernetesResource=NetworkPolicy subsys=daemon
msg="cache synced" kubernetesResource=Service subsys=daemon
msg="cache synced" kubernetesResource=Endpoint subsys=daemon
msg="adding cilium network policy in AddFunc" subsys=daemon
msg="cache synced" kubernetesResource=CiliumNetworkPolicy subsys=daemon
msg="adding cilium network policy after sleeping" subsys=daemon
```

With this commit we make sure all policies are received and processed by
Cilium before signalizing the cache was synced with Kubernetes.

As we can see in the following logs, the policies were processed and
added to the policy repository before signalizing that caches were
synced.

```
msg="adding cilium network policy in AddFunc" subsys=daemon
msg="cache synced" kubernetesResource=NetworkPolicy subsys=daemon
msg="cache synced" kubernetesResource=Service subsys=daemon
msg="cache synced" kubernetesResource=Endpoint subsys=daemon
msg="adding cilium network policy after sleeping" subsys=daemon
msg="cache synced" kubernetesResource=CiliumNetworkPolicy subsys=daemon
```

Fixes: d440059231ea ("daemon: block until initial policy list")

daemon/k8s_watcher.go
@@ -317,19 +317,24 @@ func k8sErrorHandler(e error) {
 
 // blockWaitGroupToSyncResources ensures that anything which waits on waitGroup
 // waits until all objects of the specified resource stored in Kubernetes are
-// received by informer. Fatally exits if syncing these initial objects fails.
-func blockWaitGroupToSyncResources(waitGroup *sync.WaitGroup, informer cache.Controller, resourceName string) {
+// received by the informer and processed by the given Function Queue.
+// Fatally exits if syncing these initial objects fails.
+func blockWaitGroupToSyncResources(waitGroup *sync.WaitGroup, informer cache.Controller,
+	resourceName string, queue *serializer.FunctionQueue) {
+
 	waitGroup.Add(1)
 	go func() {
-		completed := make(<-chan struct{})
 		scopedLog := log.WithField("kubernetesResource", resourceName)
 		scopedLog.Debug("waiting for cache to synchronize")
-		if ok := cache.WaitForCacheSync(completed, informer.HasSynced); !ok {
+		if ok := cache.WaitForCacheSync(wait.NeverStop, informer.HasSynced); !ok {
 			// Fatally exit it resource fails to sync
 			scopedLog.Fatalf("failed to wait for cache to sync")
 		}
-		scopedLog.Debug("cache synced")
-		waitGroup.Done()
+		queue.Enqueue(func() error {
+			scopedLog.Debug("cache synced")
+			waitGroup.Done()
+			return nil
+		}, serializer.NoRetry)
 	}()
 }
 
@@ -422,7 +427,7 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 				},
 			},
 		)
-		blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, policyController, "NetworkPolicy")
+		blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, policyController, "NetworkPolicy", serKNPs)
 		go policyController.Run(wait.NeverStop)
 
 		d.k8sAPIGroups.addAPI(k8sAPIGroupNetworkingV1Core)
@@ -465,7 +470,7 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 			},
 		},
 	)
-	blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, svcController, "Service")
+	blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, svcController, "Service", serSvcs)
 	go svcController.Run(wait.NeverStop)
 	d.k8sAPIGroups.addAPI(k8sAPIGroupServiceV1Core)
 
@@ -508,7 +513,7 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 			},
 		},
 	)
-	blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, endpointController, "Endpoint")
+	blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, endpointController, "Endpoint", serEps)
 	go endpointController.Run(wait.NeverStop)
 	d.k8sAPIGroups.addAPI(k8sAPIGroupEndpointV1Core)
 
@@ -550,7 +555,7 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 				},
 			},
 		)
-		blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, ingressController, "Ingress")
+		blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, ingressController, "Ingress", serEps)
 		go ingressController.Run(wait.NeverStop)
 		d.k8sAPIGroups.addAPI(k8sAPIGroupIngressV1Beta1)
 	}
@@ -592,7 +597,7 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 				}
 			},
 		})
-		blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, ciliumV2Controller, "CiliumNetworkPolicy")
+		blockWaitGroupToSyncResources(&d.k8sResourceSyncWaitGroup, ciliumV2Controller, "CiliumNetworkPolicy", serCNPs)
 	}
 
 	si.Start(wait.NeverStop)