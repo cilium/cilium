4c04296961cdc1e04d35578208d4887a47e613aa
Romain Lenglet <romain@covalent.io>
2018-09-06 13:31:44 -0700

    proxy: Remove port binding check on redirect creation
    [ upstream commit 02e737224008e21f04cf7872eaf1e568a0325c3f ]
    
    On redirect creation, we were checking that the allocated port
    could be bound, by opening and closing a listen socket on that
    port.
    However, Linux gives no guarantees about the delay after closing
    when the port can be bound again (by Envoy, in this case). Due to
    recent performance improvements, the delay between the test and the
    creation of the listener in Envoy was shortened, which increased
    the probability of Envoy not being able to bind the port for a new
    listener.
    Remove this check altogether.
    
    Signed-off-by: Romain Lenglet <romain@covalent.io>
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -123,16 +123,6 @@ var (
 	portRandomizerMutex lock.Mutex
 )
 
-func isPortBindable(port uint16) bool {
-	socket, err := listenSocket(fmt.Sprintf(":%d", port), 0)
-	if err != nil {
-		log.WithError(err).Infof("Skipping port %d already in use", port)
-		return false
-	}
-	socket.Close()
-	return true
-}
-
 func (p *Proxy) allocatePort() (uint16, error) {
 	portRandomizerMutex.Lock()
 	defer portRandomizerMutex.Unlock()
@@ -141,9 +131,7 @@ func (p *Proxy) allocatePort() (uint16, error) {
 		resPort := uint16(r) + p.rangeMin
 
 		if _, ok := p.allocatedPorts[resPort]; !ok {
-			if isPortBindable(resPort) {
-				return resPort, nil
-			}
+			return resPort, nil
 		}
 
 	}
