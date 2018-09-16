0a94a5d6a4fc81bfac7af631634ad1e1f87f02c4
Jarno Rajahalme <jarno@covalent.io>
2018-09-16 08:30:38 -0700

    envoy: Add config to Cilium network filter
    Currently empty.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- envoy/cilium_network_filter.h
+++ envoy/cilium_network_filter.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include "envoy/json/json_object.h"
 #include "envoy/network/connection.h"
 #include "envoy/network/filter.h"
+#include "envoy/server/filter_config.h"
+
 #include "common/common/logger.h"
 
+#include "cilium/cilium_network_filter.pb.h"
 #include "proxymap.h"
 
 namespace Envoy {
@@ -11,15 +15,27 @@ namespace Filter {
 namespace CiliumL3 {
 
 /**
+ * Shared configuration for Cilium network filter worker Instances.
+ */
+class Config : Logger::Loggable<Logger::Id::config> {
+public:
+  Config(const ::cilium::NetworkFilter& config, Server::Configuration::FactoryContext& context);
+  Config(const Json::Object& config, Server::Configuration::FactoryContext& context);
+  virtual ~Config() {}
+};
+
+typedef std::shared_ptr<Config> ConfigSharedPtr;
+
+/**
  * Implementation of a Cilium network filter.
  */
 class Instance : public Network::ReadFilter, public Network::ConnectionCallbacks,
                  Logger::Loggable<Logger::Id::filter> {
 public:
+  Instance(const ConfigSharedPtr& config) : config_(config) {}
+
   // Network::ReadFilter
-  Network::FilterStatus onData(Buffer::Instance&, bool) override {
-    return Network::FilterStatus::Continue;
-  }
+  Network::FilterStatus onData(Buffer::Instance&, bool) override;
   Network::FilterStatus onNewConnection() override;
   void initializeReadFilterCallbacks(Network::ReadFilterCallbacks& callbacks) override {
     callbacks_ = &callbacks;
@@ -31,6 +47,7 @@ public:
   void onBelowWriteBufferLowWatermark() override {}
 
 private:
+  const ConfigSharedPtr config_;
   Network::ReadFilterCallbacks* callbacks_ = nullptr;
   Cilium::ProxyMapSharedPtr maps_{};
   uint16_t proxy_port_ = 0;
