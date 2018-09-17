c2ff5146cf9ced1d0d5a0742bf12e0324e326312
Joe Stringer <joe@covalent.io>
2018-09-17 12:09:03 -0700

    test/bpf: Set pipefail to fail verification checks
    Previously, if the verifier-test.sh script failed then the
    check-complexity.sh script would not fail, even though an error
    occurred. This was misleading, fix it by setting pipefail on the whole
    script.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- test/bpf/check-complexity.sh
+++ test/bpf/check-complexity.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set -eo pipefail
 
 TESTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BPFDIR="$TESTDIR/../../bpf/"
