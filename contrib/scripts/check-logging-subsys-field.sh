cilium/cilium

commit 629151d4feaa1f6c0ebbce75f99285d040646e83

make: Check LogSubsys field in logging entry instances
All logging entry instances have to contain LogSubsys field. This
is required for proper labeling of error/warning Prometheus metric
and helpful for debugging.

Signed-off-by: Michal Rostecki <mrostecki@suse.de>

contrib/scripts/check-logging-subsys-field.sh
@@ -1,0 +1,35 @@
+#!/bin/bash
+
+# check-logging-subsys-field.sh checks whether all logging entry instancs
+# created from DefaultLogger contain the LogSubsys field. This is required for
+# proper labeling of error/warning Prometheus metric and helpful for debugging.
+# If any entry which writes any message doesn't contaion the 'subsys' field,
+# Prometheus metric logging hook (`pkg/metrics/logging_hook.go`) is going to
+# fail.
+
+# Directories:
+# - pkg/debugdetection
+# - test/
+# - vendor/
+# are excluded, because instances of DefaultLogger in those modules have their
+# specific usage which doesn't break the Prometheus logging hook.
+
+set -eu
+
+if grep -IPRns '(?!.*LogSubsys)log[ ]*= logging\.DefaultLogger.*' \
+        --exclude-dir=test/ \
+        --exclude-dir=pkg/debugdetection/ \
+        --exclude-dir=vendor/ \
+        --include=*.go .; then
+    echo "Logging entry instances have to contain the LogSubsys field. Example of"
+    echo "properly configured entry instance:"
+    echo
+    echo -e "\timport ("
+    echo -e "\t\t\"github.com/cilium/cilium/pkg/logging\""
+    echo -e "\t\t\"github.com/cilium/cilium/pkg/logging/logfields\""
+    echo -e "\t)"
+    echo
+    echo -e "\tvar log = logging.DefaultLogger.WithField(logfields.LogSubsys, \"my-subsystem\")"
+    echo
+    exit 1
+fi

Makefile
@@ -222,7 +222,11 @@ ineffassign:
 	@$(ECHO_CHECK) ineffassign
 	$(QUIET) ineffassign .
 
-precheck: govet ineffassign
+logging-subsys-field:
+	@$(ECHO_CHECK) contrib/scripts/check-logging-subsys-field.sh
+	$(QUIET) contrib/scripts/check-logging-subsys-field.sh
+
+precheck: govet ineffassign logging-subsys-field
 	@$(ECHO_CHECK) contrib/scripts/check-fmt.sh
 	$(QUIET) contrib/scripts/check-fmt.sh
 	@$(ECHO_CHECK) contrib/scripts/check-log-newlines.sh

commit 1417a463e3056c47c7a91b4eb9b12bd6b27ede69

cilium: check-looging-subsys-field.sh remove trailing '/'
Resolve log fields errors. My environment has an issue with the script
check-logging-subsys-fields where excluding ./test dir fails due to
trailing '/'. Remove '/' slashes to resolve the error below.

contrib/scripts/check-logging-subsys-field.sh
 ./test/k8sT/log.go:22:var log = logging.DefaultLogger
 ./test/runtime/log.go:22:var log = logging.DefaultLogger
 ./test/helpers/cilium.go:36:var log = logging.DefaultLogger
 ./test/test_suite_test.go:42:   log             = logging.DefaultLogger
 Logging entry instances have to contain the LogSubsys field. Example of
 properly configured entry instance:

         import (
                "github.com/cilium/cilium/pkg/logging"
                "github.com/cilium/cilium/pkg/logging/logfields"
         )

         var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "my-subsystem")

make: *** [logging-subsys-field] Error 1

Fixes: 629151d4feaa "make: Check LogSubsys field in logging entry instances"
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

contrib/scripts/check-logging-subsys-field.sh
@@ -17,9 +17,9 @@
 set -eu
 
 if grep -IPRns '(?!.*LogSubsys)log[ ]*= logging\.DefaultLogger.*' \
-        --exclude-dir=test/ \
-        --exclude-dir=pkg/debugdetection/ \
-        --exclude-dir=vendor/ \
+        --exclude-dir=test \
+        --exclude-dir=pkg/debugdetection \
+        --exclude-dir=vendor \
         --include=*.go .; then
     echo "Logging entry instances have to contain the LogSubsys field. Example of"
     echo "properly configured entry instance:"