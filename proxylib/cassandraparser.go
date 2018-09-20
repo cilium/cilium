8505133aa3b01013e8c31e1d99d3012825d8cbb5
Dan Wendlandt <dan@covalent.io>
2018-09-19 23:35:59 -0700

    Add stateful awareness of prepared queries, allowing proper filterin with policy changes

--- proxylib/cassandraparser.go
+++ proxylib/cassandraparser.go
@@ -29,8 +29,8 @@ import (
 
 //
 // Cassandra v3/v4 Parser
-// 
-// Spec: https://github.com/apache/cassandra/blob/trunk/doc/native_protocol_v4.spec 
+//
+// Spec: https://github.com/apache/cassandra/blob/trunk/doc/native_protocol_v4.spec
 //
 
 // Current Cassandra parser supports filtering on 'opcode' and if the opcode is 'query-like'
@@ -46,9 +46,8 @@ import (
 // Batch requests are logged as invidual queries, but an entire batch request will be allowed
 // only if all requests are allowed.
 
-// There are known changes in protocol v2 that are not compatible with this parser, see the 
+// There are known changes in protocol v2 that are not compatible with this parser, see the
 // the "Changes from v2" in https://github.com/apache/cassandra/blob/trunk/doc/native_protocol_v3.spec
-
 
 type CassandraRule struct {
 	opcode_exact         string
@@ -130,12 +129,26 @@ func init() {
 type CassandraParser struct {
 	connection *Connection
 	inserted   bool
-	keyspace   string
+	keyspace   string // stores current keyspace name from 'use' command
+
+	// stores prepared query string while
+	// waiting for 'prepared' reply from server
+	// with a prepared id.
+	// replies associated via stream-id
+	preparedQueryPathByStreamId map[uint16]string
+
+	// allowing us to enforce policy on query
+	// at the time of the execute command.
+	preparedQueryPathByPreparedId map[string]string // stores query string based on prepared-id,
 }
 
-func (p *CassandraParserFactory) Create(connection *Connection) Parser {
+func (pf *CassandraParserFactory) Create(connection *Connection) Parser {
 	log.Infof("CassandraParserFactory: Create: %v", connection)
-	return &CassandraParser{connection: connection}
+
+	p := CassandraParser{connection: connection}
+	p.preparedQueryPathByStreamId = make(map[uint16]string)
+	p.preparedQueryPathByPreparedId = make(map[string]string)
+	return &p
 }
 
 func (p *CassandraParser) OnData(reply, endStream bool, data_arr [][]byte, offset int) (OpType, int) {
@@ -169,13 +182,15 @@ func (p *CassandraParser) OnData(reply, endStream bool, data_arr [][]byte, offse
 		if len(data) == 0 {
 			log.Infof("ignoring zero length reply call to onData")
 			return NOP, 0
-		} else {
-			log.Infof("reply, passing %d bytes", uint32(len(data)))
-			return PASS, len(data)
+
 		}
+		cassandra_parse_reply(p, data[0:(9+request_len)])
+
+		log.Infof("reply, passing %d bytes", uint32(len(data)))
+		return PASS, len(data)
 	}
 
-	err, paths := cassandra_parse(p, data[0:(9+request_len)])
+	err, paths := cassandra_parse_request(p, data[0:(9+request_len)])
 	if err != 0 {
 		log.Infof("print parsing error %d", err)
 		return ERROR, 0
@@ -240,10 +255,10 @@ func (p *CassandraParser) OnData(reply, endStream bool, data_arr [][]byte, offse
 
 }
 
-// generate a full response (header + body) to be used as an
+// A full response (header + body) to be used as an
 // "unauthorized" error to be sent to cassandra client as part of policy
-// deny.   Original message is used to ensure that reply has matching
-// protocol version, and matching stream-id.
+// deny.   Array must be updated to ensure that reply has
+// protocol version and stream-id that matches the request.
 
 var unauth_msg_base = []byte{
 	0x0,      // version (uint8) - must be set before injection
@@ -254,6 +269,20 @@ var unauth_msg_base = []byte{
 	0x0, 0x0, 0x21, 0x00, // 'unauthorized error code' 0x2100 (uint32)
 	0x0, 0x14, // length of error msg (uint16)  - update if text changes
 	'R', 'e', 'q', 'u', 'e', 's', 't', ' ', 'U', 'n', 'a', 'u', 't', 'h', 'o', 'r', 'i', 'z', 'e', 'd',
+}
+
+// A full response (header + body) to be used as a
+// "unprepared" error to be sent to cassandra client if proxy
+// does not have the path for this prepare-query-id cached
+
+var unprepared_msg_base = []byte{
+	0x0,      // version (uint8) - must be set before injection
+	0x0,      // flags, (uint8)
+	0x0, 0x0, // stream-id (uint16) - must be set before injection
+	0x0,                 // opcode error (uint8)
+	0x0, 0x0, 0x0, 0x1a, // request length (uint32) - update if text changes
+	0x0, 0x0, 0x25, 0x00, // 'unauthorized error code' 0x2100 (uint32)
+	// must append [short bytes] array of prepared query id.
 }
 
 var opcodeMap = map[byte]string{
@@ -280,7 +309,7 @@ var reg_insert = regexp.MustCompile("(?i)^insert ")
 var reg_update = regexp.MustCompile("(?i)^update ")
 var reg_delete = regexp.MustCompile("(?i)^delete ")
 var reg_use = regexp.MustCompile("(?i)^use ")
-var reg_from = regexp.MustCompile("(?i)from")
+var reg_from = regexp.MustCompile("(?i)^from$")
 
 func parse_query(p *CassandraParser, query string) (string, string) {
 	var action string
@@ -292,6 +321,9 @@ func parse_query(p *CassandraParser, query string) (string, string) {
 			if reg_from.MatchString(fields[i]) {
 				table = strings.ToLower(fields[i+1])
 			}
+		}
+		if len(table) == 0 {
+			log.Warnf("Unable to parse table name from select query '%s'", query)
 		}
 	} else if reg_insert.MatchString(query) {
 		action = "insert"
@@ -309,10 +341,14 @@ func parse_query(p *CassandraParser, query string) (string, string) {
 				table = strings.ToLower(fields[i+1])
 			}
 		}
+		if len(table) == 0 {
+			log.Warnf("Unable to parse table name from delete query '%s'", query)
+		}
 	} else if reg_use.MatchString(query) {
 		action = "use"
 		p.keyspace = strings.Trim(strings.Fields(query)[1], "\"\\'")
 		log.Infof("Saving keyspace '%s'", p.keyspace)
+		table = "*"
 	} else {
 		fields := strings.Fields(strings.ToLower(query))
 		f0 := fields[0]
@@ -325,23 +361,23 @@ func parse_query(p *CassandraParser, query string) (string, string) {
 			action = f0
 		}
 	}
-	if !strings.Contains(table, ".") {
+	if len(table) > 0 && !strings.Contains(table, ".") {
 		table = p.keyspace + "." + table
 	}
 	return action, table
 }
 
-func cassandra_parse(p *CassandraParser, data []byte) (OpError, []string) {
+func cassandra_parse_request(p *CassandraParser, data []byte) (OpError, []string) {
 
 	direction := data[0] & 0x80 // top bit
 	if direction != 0 {
-		log.Infof("Direction bit is 'reply', but cassandra parser only parses requests")
+		log.Errorf("Direction bit is 'reply', but we are trying to parse a request")
 		return ERROR_INVALID_FRAME_TYPE, nil
 	}
 
 	compression_flag := data[1] & 0x01
 	if compression_flag == 1 {
-		log.Infof("Compression flag set, unable to parse beyond the header")
+		log.Errorf("Compression flag set, unable to parse beyond the header")
 		return ERROR_INVALID_FRAME_TYPE, nil
 	}
 
@@ -362,10 +398,21 @@ func cassandra_parse(p *CassandraParser, data []byte) (OpError, []string) {
 		end_index := 13 + query_len
 		query := string(data[13:end_index])
 		action, table := parse_query(p, query)
+
 		path = "/" + path + "/" + action + "/" + table
+		if opcode == 0x09 {
+			// stash 'path' for this prepared query based on stream id
+			// rewrite 'opcode' portion of the path to be 'execute' rather than 'prepare'
+			streamId := binary.BigEndian.Uint16(data[2:4])
+			log.Infof("Prepare query path '%s' with stream-id %d", path, streamId)
+			p.preparedQueryPathByStreamId[streamId] = strings.Replace(path, "prepare", "execute", 1)
+		}
 		return 0, []string{path}
 	} else if opcode == 0x0d {
 		// batch
+
+		// TODO: need to handle already prepared query in batch requests
+
 		num_queries := binary.BigEndian.Uint16(data[10:11])
 		paths := make([]string, num_queries)
 		log.Infof("batch query count = %d", num_queries)
@@ -381,9 +428,84 @@ func cassandra_parse(p *CassandraParser, data []byte) (OpError, []string) {
 			offset = query_end_offset
 		}
 		return 0, paths
+	} else if opcode == 0x0a {
+		// execute
+
+		// parse out prepared query id, and then look up our
+		// cached query path for policy evaluation.
+		idLen := binary.BigEndian.Uint16(data[9:11])
+		preparedId := string(data[11:(11 + idLen)])
+		log.Infof("Execute with prepared-id = '%s'", preparedId)
+		path := p.preparedQueryPathByPreparedId[preparedId]
+		if len(path) > 0 {
+			return 0, []string{path}
+		} else {
+			log.Warnf("No cached entry for prepared-id = '%s'", preparedId)
+			//TODO: return error with error code 'unprepared' with code 0x2500
+			// followed by a [short bytes] indicating the unknown ID
+			// must set stream-id of the response to match the request
+
+			unprepared_msg := make([]byte, len(unprepared_msg_base))
+			copy(unprepared_msg, unprepared_msg_base)
+			// We want to use the same protocol and stream ID
+			// as the incoming request.
+			// update the protocol to match the request
+			unprepared_msg[0] = 0x80 | (data[0] & 0x07)
+			// update the stream ID to match the request
+			unprepared_msg[2] = data[2]
+			unprepared_msg[3] = data[3]
+			p.connection.Inject(true, unprepared_msg)
+			// finish error message with a copy of the prepared-query-id
+			// in [short bytes] format
+			p.connection.Inject(true, data[9:11+idLen])
+
+			return ERROR_INVALID_FRAME_TYPE, nil
+		}
 	} else {
-		// other opcode
+		// other opcode, just return type of opcode
+
 		return 0, []string{"/" + path}
 	}
 
+}
+
+// reply parsing is very basic, just focusing on parsing RESULT messages that
+// contain prepared query IDs so that we can later enforce policy on "execute" requests.
+func cassandra_parse_reply(p *CassandraParser, data []byte) {
+
+	direction := data[0] & 0x80 // top bit
+	if direction != 0x80 {
+		log.Errorf("Direction bit is 'request', but we are trying to parse a reply")
+		return
+	}
+
+	compression_flag := data[1] & 0x01
+	if compression_flag == 1 {
+		log.Errorf("Compression flag set, unable to parse beyond the header")
+		return
+	}
+
+	streamId := binary.BigEndian.Uint16(data[2:4])
+	log.Infof("Reply with opcode %d and stream-id %d", data[4], streamId)
+	// if this is an opcode == RESULT message of type 'prepared', associate the prepared
+	// statement id with the full query string that was included in the
+	// associated PREPARE request.  The stream-id in this reply allows us to
+	// find the associated prepare query string.
+	if data[4] == 0x08 {
+		resultKind := binary.BigEndian.Uint32(data[9:13])
+		log.Infof("resultKind = %d", resultKind)
+		if resultKind == 0x0004 {
+			idLen := binary.BigEndian.Uint16(data[13:15])
+			preparedId := string(data[15 : 15+idLen])
+			log.Infof("Result with prepared-id = '%s' for stream-id %d", preparedId, streamId)
+			path := p.preparedQueryPathByStreamId[streamId]
+			if len(path) > 0 {
+				// found cached query path to associate with this preparedId
+				p.preparedQueryPathByPreparedId[preparedId] = path
+				log.Infof("Associating query path '%s' with prepared-id %d as part of stream-id %d", path, preparedId, streamId)
+			} else {
+				log.Warnf("Unable to find prepared query path associated with stream-id %d", streamId)
+			}
+		}
+	}
 }
