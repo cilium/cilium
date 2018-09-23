cilium/cilium

commit eda792688371130f0476b1ec8375d208950eae01

bpf: Do not delete and re-add ip rules and routes in the proxy table
This can otherwise lead to a brief interruption of traffic from the proxy to
local endpoints when the agent is being restarted.

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/init.sh
@@ -58,21 +58,6 @@ echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 # This directory was created by the daemon and contains the per container header file
 DIR="$PWD/globals"
 
-function delete_old_ip_rules_af()
-{
-	IP=$1
-	TBL=$2
-	for i in $($IP rule list | grep "lookup $TBL" | awk -F: '{print $1}'); do
-		$IP rule del pref $i;
-	done
-}
-
-function delete_old_ip_rules()
-{
-	delete_old_ip_rules_af "ip -4" $1
-	delete_old_ip_rules_af "ip -6" $1
-}
-
 function setup_veth()
 {
 	local -r NAME=$1
@@ -140,19 +125,15 @@ function move_local_rules()
 
 function setup_proxy_rules()
 {
-	# delete old ip rules and flush table
-	delete_old_ip_rules $PROXY_RT_TABLE
+	# Any packet from a local process uses a separate routing table
+	rulespec="fwmark 0xA00/0xF00 pref 10 lookup $PROXY_RT_TABLE"
 
-	if [ -n "$(ip -4 rule list)" ]; then
-		ip -4 route flush table $PROXY_RT_TABLE
-		# Any packet from a proxy uses a separate routing table
-		ip -4 rule add fwmark 0xA00/0xF00 pref 10 lookup $PROXY_RT_TABLE
+	if [ -z "$(ip -4 rule list $rulespec)" ]; then
+		ip -4 rule add $rulespec
 	fi
 
-	if [ -n "$(ip -6 rule list)" ]; then
-		ip -6 route flush table $PROXY_RT_TABLE
-		# Any packet from a proxy uses a separate routing table
-		ip -6 rule add fwmark 0xA00/0xF00 pref 10 lookup $PROXY_RT_TABLE
+	if [ -z "$(ip -6 rule list $rulespec)" ]; then
+		ip -6 rule add $rulespec
 	fi
 
 	if [ -n "$IP4_HOST" ]; then

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=87929b5009d2af06eb4548e180a9bed476576eb9
+GO_BINDATA_SHA1SUM=b8a75895e52cf0dbf428cd5abf728dfe6e2efd51
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

commit e219b59bb4d13858ef8232b1f67060afabbb02dc

bpf: Only attempt to install ip rules if address family is available
Fixes: eda792688371 ("bpf: Do not delete and re-add ip rules and routes in the proxy table")

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/init.sh
@@ -128,17 +128,21 @@ function setup_proxy_rules()
 	# Any packet from a local process uses a separate routing table
 	rulespec="fwmark 0xA00/0xF00 pref 10 lookup $PROXY_RT_TABLE"
 
-	if [ -z "$(ip -4 rule list $rulespec)" ]; then
-		ip -4 rule add $rulespec
-	fi
-
-	if [ -z "$(ip -6 rule list $rulespec)" ]; then
-		ip -6 rule add $rulespec
+	if [ -n "$(ip -4 rule list)" ]; then
+		if [ -z "$(ip -4 rule list $rulespec)" ]; then
+			ip -4 rule add $rulespec
+		fi
 	fi
 
 	if [ -n "$IP4_HOST" ]; then
 		ip route replace table $PROXY_RT_TABLE $IP4_HOST/32 dev $HOST_DEV1
 		ip route replace table $PROXY_RT_TABLE default via $IP4_HOST
+	fi
+
+	if [ -n "$(ip -6 rule list)" ]; then
+		if [-z "$(ip -6 rule list $rulespec)" ]; then
+			ip -6 rule add $rulespec
+		fi
 	fi
 
 	IP6_LLADDR=$(ip -6 addr show dev $HOST_DEV2 | grep inet6 | head -1 | awk '{print $2}' | awk -F'/' '{print $1}')

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=b8a75895e52cf0dbf428cd5abf728dfe6e2efd51
+GO_BINDATA_SHA1SUM=e02d2ad9836b2d80e2c36a331c8edb1e364f5ac4
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 