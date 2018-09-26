2acc53f5f42b220c988d21b80c4a69677ae464c4
Joe Stringer <joe@covalent.io>
2018-09-26 15:13:18 -0700

    bpf: Add basic endpointKey.ToIP() test
    [ upstream commit 6e7c551ec8970960c95f065f663f1758de3e90c8 ]
    
    Add a test to ensure that endpointKey.ToIP() converts IPv4 and IPv6
    addresses into strings properly.
    
    Signed-off-by: Joe Stringer <joe@covalent.io>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

---
+++ pkg/bpf/endpoint_test.go
@@ -0,0 +1,48 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bpf
+
+import (
+	"net"
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+type BPFTestSuite struct{}
+
+var _ = Suite(&BPFTestSuite{})
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+func (s *BPFTestSuite) TestEndpointKeyToString(c *C) {
+	tests := []struct {
+		ip string
+	}{
+		{"0.0.0.0"},
+		{"192.0.2.3"},
+		{"::"},
+		{"fdff::ff"},
+	}
+
+	for _, tt := range tests {
+		ip := net.ParseIP(tt.ip)
+		k := NewEndpointKey(ip)
+		c.Assert(k.ToIP().String(), Equals, tt.ip)
+	}
+}
