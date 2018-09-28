d5b1b60620dc16c26a0081b6915a16893b428d0d
Maciej Kwiek <maciej.iai@gmail.com>
2018-09-28 10:42:11 +0200

    Unified memcache parser e2e tests
    Signed-off-by: Maciej Kwiek <maciej@covalent.io>

--- test/runtime/memcache.go
+++ test/runtime/memcache.go
@@ -22,7 +22,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("RuntimeMemcacheBinary", func() {
+var _ = Describe("RuntimeMemcache", func() {
 
 	var (
 		vm         *helpers.SSHMeta
@@ -32,11 +32,13 @@ var _ = Describe("RuntimeMemcacheBinary", func() {
 	containers := func(mode string) {
 
 		images := map[string]string{
-			"memcache":        "memcached",
-			"memcache-client": "nebril/python-binary-memcached",
+			"memcache":             "memcached",
+			"memcache-bin-client":  "nebril/python-binary-memcached",
+			"memcache-text-client": "alpine",
 		}
 		cmds := map[string][]string{
-			"memcache-client": {"sleep", "10000"},
+			"memcache-bin-client":  {"sleep", "10000"},
+			"memcache-text-client": {"sleep", "10000"},
 		}
 
 		switch mode {
@@ -45,7 +47,8 @@ var _ = Describe("RuntimeMemcacheBinary", func() {
 				cmd, ok := cmds[k]
 				var res *helpers.CmdRes
 				if ok {
-					res = vm.ContainerCreate(k, v, helpers.CiliumDockerNetwork, fmt.Sprintf("-l id.%s", k), cmd...)
+					// client containers
+					res = vm.ContainerCreate(k, v, helpers.CiliumDockerNetwork, "-l memcache-client", cmd...)
 				} else {
 					res = vm.ContainerCreate(k, v, helpers.CiliumDockerNetwork, fmt.Sprintf("-l id.%s", k))
 				}
@@ -62,24 +65,32 @@ var _ = Describe("RuntimeMemcacheBinary", func() {
 		}
 	}
 
-	setKey := func(key, value string) *helpers.CmdRes {
+	setKeyBinary := func(key, value string) *helpers.CmdRes {
 		logger.Infof("Setting %s as %s", key, value)
-		res := vm.ContainerExec("memcache-client", fmt.Sprintf(
+		res := vm.ContainerExec("memcache-bin-client", fmt.Sprintf(
 			`python -c 'import bmemcached; client = bmemcached.Client(("%s:11211", )); print(client.set("%s", "%s"))'`, memcacheIP, key, value))
 		return res
 	}
 
-	getKey := func(key string) *helpers.CmdRes {
+	getKeyBinary := func(key string) *helpers.CmdRes {
 		logger.Infof("Getting %s", key)
-		res := vm.ContainerExec("memcache-client", fmt.Sprintf(
+		res := vm.ContainerExec("memcache-bin-client", fmt.Sprintf(
 			`python -c 'import bmemcached; client = bmemcached.Client(("%s:11211", )); print(client.get("%s"))'`, memcacheIP, key))
 		return res
 	}
 
-	runWithClient := func(code string) *helpers.CmdRes {
+	runWithClientBinary := func(code string) *helpers.CmdRes {
 		logger.Infof("Executing %s", code)
-		res := vm.ContainerExec("memcache-client", fmt.Sprintf(
+		res := vm.ContainerExec("memcache-bin-client", fmt.Sprintf(
 			`python -c 'import bmemcached; client = bmemcached.Client(("%s:11211", )); %s'`, memcacheIP, code))
+		return res
+	}
+
+	flush := func() *helpers.CmdRes {
+		logger.Infof("flushing memcached")
+		cmd := fmt.Sprintf(
+			`sh -c 'echo -en "flush_all\r\nquit\r\n" | nc %s 11211'`, memcacheIP)
+		res := vm.ContainerExec("memcache-text-client", cmd)
 		return res
 	}
 
@@ -95,6 +106,7 @@ var _ = Describe("RuntimeMemcacheBinary", func() {
 
 	AfterEach(func() {
 		vm.PolicyDelAll()
+		flush().ExpectContains("OK", "Unable to flush memcache")
 	})
 
 	AfterAll(func() {
@@ -106,99 +118,200 @@ var _ = Describe("RuntimeMemcacheBinary", func() {
 	})
 
 	AfterFailed(func() {
+		containers("delete")
 		vm.ReportFailed("cilium policy get")
 	})
 
-	It("Tests basic memcache operation", func() {
-		key := "test1"
-		value := "value1"
-		By("Setting memcache value")
-		r := setKey(key, value)
+	Context("Testing binary memcache", func() {
+		It("Tests basic memcache operation", func() {
+			key := "test1"
+			value := "value1"
+			By("Setting memcache value")
+			r := setKeyBinary(key, value)
 
-		r.ExpectSuccess("Unable to set key", key)
+			r.ExpectSuccess("Unable to set key %s", key)
 
-		By("getting memcache value")
-		r = getKey(key)
-		r.ExpectSuccess("Unable to get key", key)
-		r.ExpectContains(value, "Value mismatch on get")
+			By("getting memcache value")
+			r = getKeyBinary(key)
+			r.ExpectSuccess("Unable to get key %s", key)
+			r.ExpectContains(value, "Value mismatch on get")
+		})
+
+		It("Tests policy allowing all actions", func() {
+			_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-allow.json"), 300)
+			Expect(err).Should(BeNil(), "Failed to import policy")
+
+			key := "test2"
+			value := "value2"
+			By("Setting memcache value")
+			r := setKeyBinary(key, value)
+			r.ExpectSuccess("Unable to set key %s", key)
+
+			By("getting memcache value")
+			r = getKeyBinary(key)
+			r.ExpectSuccess("Unable to get key %s", key)
+			r.ExpectContains(value, "Value mismatch for key %s", key)
+		})
+
+		It("Tests policy disallowing set action", func() {
+			By("Setting key before disallowing set")
+			keyBeforePolicy := "before"
+			valBeforePolicy := "beforeval"
+			r := setKeyBinary(keyBeforePolicy, valBeforePolicy)
+			r.ExpectSuccess("Unable to set key %q without policy loaded", keyBeforePolicy)
+
+			By("Importing policy disallowing set")
+			_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-disallow-set.json"), 300)
+			Expect(err).Should(BeNil(), "Failed to import policy")
+
+			By("Trying to set new key")
+			key := "keyAfterPolicy"
+			value := "valAfterPolicy"
+
+			By("Setting memcache value")
+			r = setKeyBinary(key, value)
+			r.ExpectFail("Set key should be prohibited by policy")
+
+			By("getting memcache value")
+			r = getKeyBinary(keyBeforePolicy)
+			r.ExpectSuccess("Unable to get key set before applying policy")
+			r.ExpectContains(valBeforePolicy, "Value mismatch on get")
+		})
+
+		It("Tests policy allowing actions only for key", func() {
+			By("Importing key-specific policy")
+			_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-allow-key.json"), 300)
+			Expect(err).Should(BeNil(), "Failed to import policy")
+
+			key := "allowed"
+			value := "value"
+			By("Setting memcache allowed key")
+			r := setKeyBinary(key, value)
+			r.ExpectSuccess("Unable to set allowed key")
+			By("Getting memcache allowed key")
+			r = getKeyBinary(key)
+			r.ExpectSuccess("Unable to get allowed key")
+
+			key = "disallowed"
+			By("Setting memcache disallowed key")
+			r = setKeyBinary(key, value)
+			r.ExpectFail("Able to set disallowed key")
+		})
+
+		It("Tests multi-get from a disallowed and allowed keys set", func() {
+			By("Setting value to both keys")
+			r := setKeyBinary("allowed", "value")
+			r.ExpectSuccess("Unable to set allowed value")
+			r = setKeyBinary("disallowed", "value")
+			r.ExpectSuccess("Unable to set disallowed value")
+
+			By("Importing key-specific policy")
+			_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-allow-key-get.json"), 300)
+			Expect(err).Should(BeNil(), "Failed to import policy")
+
+			By("Getting multiple keys")
+			code := "print(client.get_multi([\"allowed\", \"disallowed\"]))"
+			r = runWithClientBinary(code)
+
+			r.ExpectFail("Able to get multiple keys with disallowed")
+		})
 	})
 
-	It("Tests policy allowing all actions", func() {
-		_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-binary-allow.json"), 300)
-		Expect(err).Should(BeNil(), "Failed to import policy")
+	setKeyText := func(key, value string) *helpers.CmdRes {
+		logger.Infof("Setting %s as %s", key, value)
+		cmd := fmt.Sprintf(
+			`sh -c 'echo -en "set %s 0 500 %d\r\n%s\r\nquit\r\n" | nc %s 11211'`, key, len(value), value, memcacheIP)
+		res := vm.ContainerExec("memcache-text-client", cmd)
+		return res
+	}
 
-		key := "test2"
-		value := "value2"
-		By("Setting memcache value")
-		r := setKey(key, value)
-		r.ExpectSuccess("Unable to set key", key)
+	getKeyText := func(key string) *helpers.CmdRes {
+		logger.Infof("Getting %s", key)
+		res := vm.ContainerExec("memcache-text-client", fmt.Sprintf(
+			`sh -c 'echo -en "get %s\r\nquit\r\n" | nc %s 11211'`, key, memcacheIP))
+		return res
+	}
 
-		By("getting memcache value")
-		r = getKey(key)
-		r.ExpectSuccess("Unable to get key", key)
-		r.ExpectContains(value)
-	})
+	Context("Testing text memcache", func() {
+		It("Tests basic memcache operation", func() {
+			key := "test1"
+			value := "value1"
+			By("Setting memcache value")
+			r := setKeyText(key, value)
+			r.ExpectSuccess("Unable to set key %s", key)
+			r.ExpectContains("STORED", "value not stored")
 
-	It("Tests policy disallowing set action", func() {
-		By("Setting key before disallowing set")
-		keyBeforePolicy := "before"
-		valBeforePolicy := "beforeval"
-		r := setKey(keyBeforePolicy, valBeforePolicy)
-		r.ExpectSuccess("Unable to set key %q without policy loaded", keyBeforePolicy)
+			By("getting memcache value")
+			r = getKeyText(key)
+			r.ExpectSuccess("Unable to get key %s", key)
+			r.ExpectContains(value, "Value mismatch on get")
+			r.ExpectContains("VALUE", "Did not have text VALUE header")
+		})
 
-		By("Importing policy disallowing set")
-		_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-binary-disallow-set.json"), 300)
-		Expect(err).Should(BeNil(), "Failed to import policy")
+		It("Tests policy allowing all actions", func() {
+			_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-allow.json"), 300)
+			Expect(err).Should(BeNil(), "Failed to import policy")
 
-		By("Trying to set new key")
-		key := "keyAfterPolicy"
-		value := "valAfterPolicy"
+			key := "test2"
+			value := "value2"
+			By("Setting memcache value")
+			r := setKeyText(key, value)
+			r.ExpectSuccess("Unable to set key, %s", key)
+			r.ExpectContains("STORED", "value not stored")
 
-		By("Setting memcache value")
-		r = setKey(key, value)
-		r.ExpectFail("Set key should be prohibited by policy")
+			By("getting memcache value")
+			r = getKeyText(key)
+			r.ExpectSuccess("Unable to get key, %s", key)
+			r.ExpectContains(value, "memcache key %s value mismatch", key)
+			r.ExpectContains("VALUE", "Did not have text VALUE header")
+		})
 
-		By("getting memcache value")
-		r = getKey(keyBeforePolicy)
-		r.ExpectSuccess("Unable to get key set before applying policy")
-		r.ExpectContains(valBeforePolicy, "Value mismatch on get")
-	})
+		It("Tests policy disallowing set action", func() {
+			By("Setting key before disallowing set")
+			keyBeforePolicy := "before"
+			valBeforePolicy := "beforeval"
+			r := setKeyText(keyBeforePolicy, valBeforePolicy)
+			r.ExpectSuccess("Unable to set key %q without policy loaded", keyBeforePolicy)
+			r.ExpectContains("STORED", "value not stored")
 
-	It("Tests policy allowing actions only for key", func() {
-		By("Importing key-specific policy")
-		_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-binary-allow-key.json"), 300)
-		Expect(err).Should(BeNil(), "Failed to import policy")
+			By("Importing policy disallowing set")
+			_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-disallow-set.json"), 300)
+			Expect(err).Should(BeNil(), "Failed to import policy")
 
-		key := "allowed"
-		value := "value"
-		By("Setting memcache allowed key")
-		r := setKey(key, value)
-		r.ExpectSuccess("Unable to set allowed key")
-		By("Getting memcache allowed key")
-		r = getKey(key)
-		r.ExpectSuccess("Unable to get allowed key")
+			By("Trying to set new key")
+			key := "keyAfterPolicy"
+			value := "valAfterPolicy"
 
-		key = "disallowed"
-		By("Setting memcache disallowed key")
-		r = setKey(key, value)
-		r.ExpectFail("Able to set disallowed key")
-	})
+			By("Setting memcache value")
+			r = setKeyText(key, value)
+			r.ExpectSuccess("Unable to set key %s", key)
+			r.ExpectContains("access denied", "did not get access denied message")
 
-	It("Tests if you are able to multi-get from a disallowed and allowed keys", func() {
-		By("Setting value to both keys")
-		r := setKey("allowed", "value")
-		r.ExpectSuccess("Unable to set allowed value")
-		r = setKey("disallowed", "value")
-		r.ExpectSuccess("Unable to set disallowed value")
+			By("getting memcache value")
+			r = getKeyText(keyBeforePolicy)
+			r.ExpectSuccess("Unable to get key set before applying policy")
+			r.ExpectContains(valBeforePolicy, "Value mismatch on get")
+		})
 
-		By("Importing key-specific policy")
-		_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-binary-allow-key-get.json"), 300)
-		Expect(err).Should(BeNil(), "Failed to import policy")
+		It("Tests policy allowing actions only for allowed key", func() {
+			By("Importing key-specific policy")
+			_, err := vm.PolicyImportAndWait(vm.GetFullPath("Policies-memcache-allow-key.json"), 300)
+			Expect(err).Should(BeNil(), "Failed to import policy")
 
-		By("Getting multiple keys")
-		code := "print(client.get_multi([\"allowed\", \"disallowed\"]))"
-		r = runWithClient(code)
+			key := "allowed"
+			value := "value"
+			By("Setting memcache allowed key")
+			r := setKeyText(key, value)
+			r.ExpectSuccess("Unable to set allowed key")
+			By("Getting memcache allowed key")
+			r = getKeyText(key)
+			r.ExpectSuccess("Unable to get allowed key")
 
-		r.ExpectFail("Unable to get multiple keys")
+			key = "disallowed"
+			By("Setting memcache disallowed key")
+			r = setKeyText(key, value)
+			r.ExpectSuccess("Unable to set key %s", key)
+			r.ExpectContains("access denied", "did not get access denied message")
+		})
 	})
 })
