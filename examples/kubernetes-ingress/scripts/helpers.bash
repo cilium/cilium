f6318babb359f6fcabce6213ede8baa4347f8430
André Martins <andre@cilium.io>
2018-10-01 10:28:09 +0200

    dev: bump default k8s development version to v1.12.0
    Signed-off-by: André Martins <andre@cilium.io>

--- examples/kubernetes-ingress/scripts/helpers.bash
+++ examples/kubernetes-ingress/scripts/helpers.bash
@@ -75,8 +75,8 @@ cluster_api_server_ip=${K8S_CLUSTER_API_SERVER_IP:-"172.20.0.1"}
 #cluster_dns_ip=${K8S_CLUSTER_DNS_IP:-"FD03::A"}
 #cluster_api_server_ip=${K8S_CLUSTER_API_SERVER_IP:-"FD03::1"}
 
-k8s_version="v1.11.3"
-etcd_version="v3.2.7"
+k8s_version="v1.12.0"
+etcd_version="v3.2.24"
 
 function restore_flag {
   check_num_params "$#" "2"
