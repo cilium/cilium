2209c5ee15635af6ca5e17ce66021734a9667163
André Martins <andre@cilium.io>
2018-10-02 19:40:17 +0200

    docs: add warning in docs for older versions
    Signed-off-by: André Martins <andre@cilium.io>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -37,7 +37,8 @@ extensions = ['sphinx.ext.ifconfig',
     'sphinx.ext.extlinks',
     'sphinxcontrib.openapi',
     'sphinx_tabs.tabs',
-    'sphinxcontrib.spelling' ]
+    'sphinxcontrib.spelling',
+    'versionwarning.extension' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -65,6 +66,9 @@ author = u'Cilium Authors'
 #
 # The short X.Y version.
 release = open("../VERSION", "r").read().strip()
+# Used by version warning
+versionwarning_project_version = open("../VERSION", "r").read().strip()
+versionwarning_body_selector = "div.document"
 
 # Fetch the docs version from an environment variable.
 # Map latest -> master.
