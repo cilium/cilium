7c0b966f85f6a37ffd46e6b06d782878f9680a8d
Thomas Graf <thomas@cilium.io>
2018-10-12 11:34:18 +0200

    k8s: Avoid TriggerPolicyUpdates when no ToServices rules are loaded
    The call to regenerate policy for all endpoints due to an Endpoints change on
    an external service is only required when the policy repository contains
    ToServices rules. This can avoid a lot of unnecessary calls to
    TriggerPolicyUpdates().
    
    Fixes: #5871
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- daemon/k8s_watcher.go
+++ daemon/k8s_watcher.go
@@ -975,12 +975,13 @@ func (d *Daemon) addK8sEndpointV1(ep *v1.Endpoints) error {
 		// select said service.
 		if !cacheOK || (cacheOK && serviceImportMeta.ruleTranslationError != nil) {
 			translator := k8s.NewK8sTranslator(svcns, *newSvcEP, false, svc.Labels, bpfIPCache.IPCache)
-			err := d.policy.TranslateRules(translator)
+			result, err := d.policy.TranslateRules(translator)
 			endpointMetadataCache.upsert(ep, err)
 			if err != nil {
 				log.Errorf("Unable to repopulate egress policies from ToService rules: %v", err)
 				return err
-			} else {
+			} else if result.NumToServicesRules > 0 {
+				// Only trigger policy updates if ToServices rules are in effect
 				scopedLog.Info("Kubernetes service endpoint added")
 				d.TriggerPolicyUpdates(true, "Kubernetes service endpoint added")
 			}
@@ -1037,10 +1038,11 @@ func (d *Daemon) deleteK8sEndpointV1(ep *v1.Endpoints) error {
 		svc, ok := d.loadBalancer.K8sServices[svcns]
 		if ok && svc.IsExternal() {
 			translator := k8s.NewK8sTranslator(svcns, *endpoint, true, svc.Labels, bpfIPCache.IPCache)
-			err := d.policy.TranslateRules(translator)
+			result, err := d.policy.TranslateRules(translator)
 			if err != nil {
 				log.Errorf("Unable to depopulate egress policies from ToService rules: %v", err)
-			} else {
+			} else if result.NumToServicesRules > 0 {
+				// Only trigger policy updates if ToServices rules are in effect
 				d.TriggerPolicyUpdates(true, "Kubernetes service endpoint deleted")
 			}
 		}
