cilium/cilium

commit cb49db51afaac8aede386f0fdacd38b015b6ee3b

endpoint: Clear conntrack on initial endpoint build
On regular endpoint lifecycle management, the connection tracking table should
automatically be cleared from entries of endpoints that have been removed.

Similar, if the agent crashed, the cleaning during the bootstrapping should
remove all entries that cannot be related to a restored endpoint.

As a last safety net, this commit removes all eventual connection tracking
entries related to an endpoint IP that is being re-used. The cleaning is done
for the first regeneration of the endpoint in parallel to the compilation.

Fixes: #5239

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/endpoint/endpoint.go
@@ -467,6 +467,10 @@ type Endpoint struct {
 	// All fields within the PolicyKey and the proxy port must be in host byte-order.
 	desiredMapState PolicyMapState
 
+	// ctCleaned indicates whether the conntrack table has already been
+	// cleaned when this endpoint was first created
+	ctCleaned bool
+
 	///////////////////////
 	// DEPRECATED FIELDS //
 	///////////////////////
@@ -1786,12 +1790,7 @@ func (e *Endpoint) LeaveLocked(owner Owner, proxyWaitGroup *completion.WaitGroup
 	e.controllers.RemoveAll()
 	e.cleanPolicySignals()
 
-	e.GarbageCollectConntrack(&ctmap.GCFilter{
-		MatchIPs: map[string]struct{}{
-			e.IPv4.String(): {},
-			e.IPv6.String(): {},
-		},
-	})
+	e.scrubIPsInConntrackTableLocked()
 
 	e.SetStateLocked(StateDisconnected, "Endpoint removed")
 
@@ -2704,9 +2703,9 @@ func (e *Endpoint) LogDisconnectedMutexAction(err error, context string) {
 	e.mutex.Unlock()
 }
 
-// garbageCollectConntrack is usd by GarbageCollectConntrack and should not be
+// doGarbageCollectConntrack is usd by garbageCollectConntrack and should not be
 // called directly.
-func (e *Endpoint) garbageCollectConntrack(isIPv6 bool, filter *ctmap.GCFilter) {
+func (e *Endpoint) doGarbageCollectConntrack(isIPv6 bool, filter *ctmap.GCFilter) {
 	var file, mapType string
 
 	if e.Options != nil && e.Options.IsEnabled(option.ConntrackLocal) {
@@ -2725,14 +2724,29 @@ func (e *Endpoint) garbageCollectConntrack(isIPv6 bool, filter *ctmap.GCFilter) 
 	ctmap.GC(m, mapType, filter)
 }
 
-// GarbageCollectConntrack will run the ctmap.GC() on either the endpoint's
+// garbageCollectConntrack will run the ctmap.GC() on either the endpoint's
 // local conntrack table or the global conntrack table.
 //
 // The endponit lock must be held
-func (e *Endpoint) GarbageCollectConntrack(filter *ctmap.GCFilter) {
+func (e *Endpoint) garbageCollectConntrack(filter *ctmap.GCFilter) {
 	if !option.Config.IPv4Disabled {
-		e.garbageCollectConntrack(false, filter)
+		e.doGarbageCollectConntrack(false, filter)
 	}
 
-	e.garbageCollectConntrack(true, filter)
+	e.doGarbageCollectConntrack(true, filter)
+}
+
+func (e *Endpoint) scrubIPsInConntrackTableLocked() {
+	e.garbageCollectConntrack(&ctmap.GCFilter{
+		MatchIPs: map[string]struct{}{
+			e.IPv4.String(): {},
+			e.IPv6.String(): {},
+		},
+	})
+}
+
+func (e *Endpoint) scrubIPsInConntrackTable() {
+	e.UnconditionalLock()
+	e.scrubIPsInConntrackTableLocked()
+	e.Unlock()
 }

pkg/endpoint/bpf.go
@@ -464,6 +464,8 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (revnum uint
 
 	buildStart := time.Now()
 
+	ctCleaned := make(chan struct{})
+
 	if err = e.LockAlive(); err != nil {
 		return 0, compilationExecuted, err
 	}
@@ -474,6 +476,18 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (revnum uint
 		logger.WithField(logfields.BuildDuration, time.Since(buildStart).String()).
 			Info("Regeneration of BPF program has completed")
 	}()
+
+	// In the first ever regeneration of the endpoint, the conntrack table
+	// is cleaned from the new endpoint IPs as it is guaranteed that any
+	// pre-existing connections using that IP are now invalid.
+	if !e.ctCleaned {
+		go func() {
+			e.scrubIPsInConntrackTable()
+			close(ctCleaned)
+		}()
+	} else {
+		close(ctCleaned)
+	}
 
 	// If endpoint was marked as disconnected then
 	// it won't be regenerated.
@@ -669,10 +683,15 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (revnum uint
 		return 0, compilationExecuted, fmt.Errorf("Error while configuring proxy redirects: %s", err)
 	}
 
+	// Wait for connection tracking cleaning to be complete
+	<-ctCleaned
+
 	if err = e.LockAlive(); err != nil {
 		return 0, compilationExecuted, err
 	}
 	defer e.Unlock()
+
+	e.ctCleaned = true
 
 	// Synchronously try to update PolicyMap for this endpoint. If any
 	// part of updating the PolicyMap fails, bail out and do not generate

commit 67e347d389c6d47287a13a014c40f341ca7149d7

endpoint: Skip conntrack clean on endpoint restore
The commit cb49db51afa introduced conntrack cleaning on the initial endpoint
build to clean eventual state from a previous endpoint build. This is correct
in principle but the commit missed to exclude endpoint builds triggered by
restored endpoints.

Fixes: cb49db51afa ("endpoint: Clear conntrack on initial endpoint build")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/state.go
@@ -130,6 +130,8 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 
 		ep.Unlock()
 
+		ep.SkipStateClean()
+
 		state.restored = append(state.restored, ep)
 
 		delete(existingEndpoints, ep.IPv4.String())

pkg/endpoint/endpoint.go
@@ -2759,3 +2759,12 @@ func (e *Endpoint) scrubIPsInConntrackTable() {
 	e.scrubIPsInConntrackTableLocked()
 	e.Unlock()
 }
+
+// SkipStateClean can be called on a endpoint before its first build to skip
+// the cleaning of state such as the conntrack table. This is useful when an
+// endpoint is being restored from state and the datapath state should not be
+// claned.
+func (e *Endpoint) SkipStateClean() {
+	// Mark conntrack as already cleaned
+	e.ctCleaned = true
+}