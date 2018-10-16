cilium/cilium

commit 99a73a2fbd802786a864df7869f7cf5364c35556

envoy: Update revision upon acks from NPDS
When the NetworkPolicyDiscoveryService acknowledges oustanding
NetworkPolicyUpdates for an endpoint, push the corresponding policy
revision number into the endpoint.

Fixes: #3144

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/envoy/server.go
@@ -41,6 +41,7 @@ import (
 	"github.com/golang/protobuf/ptypes/duration"
 	"github.com/golang/protobuf/ptypes/struct"
 	"github.com/golang/protobuf/ptypes/wrappers"
+	"github.com/spf13/viper"
 )
 
 // allowAction is a "Pass" route action to use in route rules. Immutable.
@@ -90,7 +91,7 @@ func createXDSServer(path, accessLogPath string) *XDSServer {
 
 	npdsConfig := &xds.ResourceTypeConfiguration{
 		Source:      NetworkPolicyCache,
-		AckObserver: nil, // We don't wait for ACKs for those resources.
+		AckObserver: AckingNetworkPolicyMutator,
 	}
 
 	nphdsConfig := &xds.ResourceTypeConfiguration{
@@ -509,7 +510,7 @@ func getNetworkPolicy(name string, id identity.NumericIdentity, policy *policy.L
 // When the proxy acknowledges the network policy update, it will result in
 // a subsequent call to the endpoint's OnProxyPolicyAcknowledge() function.
 func UpdateNetworkPolicy(ep NetworkPolicyEndpoint, policy *policy.L4Policy,
-	labelsMap identity.IdentityCache, deniedIngressIdentities, deniedEgressIdentities map[identity.NumericIdentity]bool) error {
+	labelsMap identity.IdentityCache, deniedIngressIdentities, deniedEgressIdentities map[identity.NumericIdentity]bool, wg *completion.WaitGroup) error {
 
 	// First, validate all policies
 	ips := []string{
@@ -531,7 +532,26 @@ func UpdateNetworkPolicy(ep NetworkPolicyEndpoint, policy *policy.L4Policy,
 
 	// When successful, push them into the cache.
 	for _, p := range policies {
-		NetworkPolicyCache.Upsert(NetworkPolicyTypeURL, p.Name, p, false)
+		policyRevision := policy.Revision
+		callback := func() {
+			ep.OnProxyPolicyUpdate(policyRevision)
+		}
+		var c *completion.Completion
+		if wg == nil {
+			c = completion.NewCallback(context.Background(), callback)
+		} else {
+			c = wg.AddCompletionWithCallback(callback)
+		}
+		nodeIDs := make([]string, 0, 1)
+		if viper.GetBool("sidecar-http-proxy") {
+			if ep.GetIPv4Address() == "" {
+				log.Fatal("envoy: sidecar proxy has no IPv4 address")
+			}
+			nodeIDs = append(nodeIDs, ep.GetIPv4Address())
+		} else {
+			nodeIDs = append(nodeIDs, "127.0.0.1")
+		}
+		AckingNetworkPolicyMutator.Upsert(NetworkPolicyTypeURL, p.Name, p, nodeIDs, c)
 	}
 	return nil
 }

daemon/daemon.go
@@ -153,7 +153,8 @@ func (d *Daemon) UpdateNetworkPolicy(e *endpoint.Endpoint, policy *policy.L4Poli
 	if d.l7Proxy == nil {
 		return fmt.Errorf("can't update network policy, proxy disabled")
 	}
-	return d.l7Proxy.UpdateNetworkPolicy(e, policy, labelsMap, deniedIngressIdentities, deniedEgressIdentities)
+	// TODO(jrajahalme): Pass e.ProxyWaitGroup down
+	return d.l7Proxy.UpdateNetworkPolicy(e, policy, labelsMap, deniedIngressIdentities, deniedEgressIdentities, nil)
 }
 
 // RemoveNetworkPolicy removes a network policy from the set published to

pkg/envoy/endpoint.go
@@ -24,4 +24,8 @@ type NetworkPolicyEndpoint interface {
 	GetIPv4Address() string
 	GetIPv6Address() string
 	GetIdentity() identity.NumericIdentity
+
+	// OnProxyPolicyUpdate is called when the proxy acknowledges that it
+	// has applied a policy.
+	OnProxyPolicyUpdate(policyRevision uint64)
 }

pkg/envoy/resources.go
@@ -36,6 +36,10 @@ var (
 	// NetworkPolicyTypeURL type URL.
 	NetworkPolicyCache = xds.NewCache()
 
+	// AckingNetworkPolicyMutator handles acknowledgements of NetworkPolicy
+	// resource updates.
+	AckingNetworkPolicyMutator = xds.NewAckingResourceMutatorWrapper(NetworkPolicyCache, xds.IstioNodeToIP)
+
 	// NetworkPolicyHostsCache is the global cache of resources of type
 	// NetworkPolicyHosts. Resources in this cache must have the
 	// NetworkPolicyHostsTypeURL type URL.

pkg/proxy/proxy.go
@@ -562,8 +562,8 @@ func (p *Proxy) RemoveRedirect(id string, wg *completion.WaitGroup) error {
 // UpdateNetworkPolicy adds or updates a network policy in the set
 // published to L7 proxies.
 func (p *Proxy) UpdateNetworkPolicy(ep envoy.NetworkPolicyEndpoint, policy *policy.L4Policy,
-	labelsMap identityPkg.IdentityCache, deniedIngressIdentities, deniedEgressIdentities map[identityPkg.NumericIdentity]bool) error {
-	return envoy.UpdateNetworkPolicy(ep, policy, labelsMap, deniedIngressIdentities, deniedEgressIdentities)
+	labelsMap identityPkg.IdentityCache, deniedIngressIdentities, deniedEgressIdentities map[identityPkg.NumericIdentity]bool, wg *completion.WaitGroup) error {
+	return envoy.UpdateNetworkPolicy(ep, policy, labelsMap, deniedIngressIdentities, deniedEgressIdentities, wg)
 }
 
 // RemoveNetworkPolicy removes a network policy from the set published to

commit 9acccc4c7f0bba45343577565ca21927d62e6301

envoy: Pass nil completion if Acks are not expected.
Completions are cleaned up when an ack or nack for the resource type
is received. This never happens if there are no configured L7 proxies.

Prevent indefinite collection of completions in this case by passing
nil completion to Upsert().

Fixes: 99a73a2fbd ("envoy: Update revision upon acks from NPDS")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/envoy/server.go
@@ -703,10 +703,7 @@ func (s *XDSServer) UpdateNetworkPolicy(ep logger.EndpointUpdater, policy *polic
 			}
 		}
 		var c *completion.Completion
-		if wg == nil {
-			// Passing a nil CancelFunc
-			c = completion.NewCompletion(nil, callback)
-		} else {
+		if wg != nil {
 			c = wg.AddCompletionWithCallback(callback)
 		}
 		nodeIDs := make([]string, 0, 1)

pkg/envoy/xds/ack.go
@@ -139,14 +139,16 @@ func (m *AckingResourceMutatorWrapper) Upsert(typeURL string, resourceName strin
 	m.locker.Lock()
 	defer m.locker.Unlock()
 
-	if c == nil {
-		log.WithFields(logrus.Fields{
-			logfields.XDSTypeURL:      typeURL,
-			logfields.XDSResourceName: resourceName,
-		}).Fatal("no completion given to upsert xDS resource")
-	}
-
 	version, _, revert := m.mutator.Upsert(typeURL, resourceName, resource, true)
+
+	if c == nil {
+		// No completion in upsert, revert also without completion
+		return func(_ *completion.Completion) {
+			m.locker.Lock()
+			defer m.locker.Unlock()
+			revert(true)
+		}
+	}
 
 	if _, found := m.pendingCompletions[c]; found {
 		log.WithFields(logrus.Fields{
@@ -194,10 +196,12 @@ func (m *AckingResourceMutatorWrapper) Delete(typeURL string, resourceName strin
 	version, _, revert := m.mutator.Delete(typeURL, resourceName, true)
 
 	if c == nil {
-		log.WithFields(logrus.Fields{
-			logfields.XDSTypeURL:      typeURL,
-			logfields.XDSResourceName: resourceName,
-		}).Fatal("no completion given to delete xDS resource")
+		// No completion in upsert, revert also without completion
+		return func(_ *completion.Completion) {
+			m.locker.Lock()
+			defer m.locker.Unlock()
+			revert(true)
+		}
 	}
 
 	if _, found := m.pendingCompletions[c]; found {

pkg/envoy/xds/ack_test.go
@@ -267,14 +267,10 @@ func (s *AckSuite) TestRevertInsert(c *C) {
 
 	// Create version 1 with resource 0.
 	// Insert.
-	comp := wg.AddCompletion()
-	defer comp.Complete(nil)
-	revert := acker.Upsert(typeURL, resources[0].Name, resources[0], []string{node0}, comp)
+	revert := acker.Upsert(typeURL, resources[0].Name, resources[0], []string{node0}, nil)
 
 	// Insert another resource.
-	comp = wg.AddCompletion()
-	defer comp.Complete(nil)
-	_ = acker.Upsert(typeURL, resources[2].Name, resources[2], []string{node0}, comp)
+	_ = acker.Upsert(typeURL, resources[2].Name, resources[2], []string{node0}, nil)
 
 	res, err := cache.Lookup(typeURL, resources[0].Name)
 	c.Assert(err, IsNil)
@@ -284,7 +280,7 @@ func (s *AckSuite) TestRevertInsert(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(res, Equals, resources[2])
 
-	comp = wg.AddCompletion()
+	comp := wg.AddCompletion()
 	defer comp.Complete(nil)
 	revert(comp)
 
@@ -308,14 +304,10 @@ func (s *AckSuite) TestRevertUpdate(c *C) {
 
 	// Create version 1 with resource 0.
 	// Insert.
-	comp := wg.AddCompletion()
-	defer comp.Complete(nil)
-	acker.Upsert(typeURL, resources[0].Name, resources[0], []string{node0}, comp)
+	acker.Upsert(typeURL, resources[0].Name, resources[0], []string{node0}, nil)
 
 	// Insert another resource.
-	comp = wg.AddCompletion()
-	defer comp.Complete(nil)
-	_ = acker.Upsert(typeURL, resources[2].Name, resources[2], []string{node0}, comp)
+	_ = acker.Upsert(typeURL, resources[2].Name, resources[2], []string{node0}, nil)
 
 	res, err := cache.Lookup(typeURL, resources[0].Name)
 	c.Assert(err, IsNil)
@@ -326,15 +318,13 @@ func (s *AckSuite) TestRevertUpdate(c *C) {
 	c.Assert(res, Equals, resources[2])
 
 	// Update.
-	comp = wg.AddCompletion()
-	defer comp.Complete(nil)
-	revert := acker.Upsert(typeURL, resources[0].Name, resources[1], []string{node0}, comp)
+	revert := acker.Upsert(typeURL, resources[0].Name, resources[1], []string{node0}, nil)
 
 	res, err = cache.Lookup(typeURL, resources[0].Name)
 	c.Assert(err, IsNil)
 	c.Assert(res, Equals, resources[1])
 
-	comp = wg.AddCompletion()
+	comp := wg.AddCompletion()
 	defer comp.Complete(nil)
 	revert(comp)
 
@@ -358,14 +348,10 @@ func (s *AckSuite) TestRevertDelete(c *C) {
 
 	// Create version 1 with resource 0.
 	// Insert.
-	comp := wg.AddCompletion()
-	defer comp.Complete(nil)
-	acker.Upsert(typeURL, resources[0].Name, resources[0], []string{node0}, comp)
+	acker.Upsert(typeURL, resources[0].Name, resources[0], []string{node0}, nil)
 
 	// Insert another resource.
-	comp = wg.AddCompletion()
-	defer comp.Complete(nil)
-	_ = acker.Upsert(typeURL, resources[2].Name, resources[2], []string{node0}, comp)
+	_ = acker.Upsert(typeURL, resources[2].Name, resources[2], []string{node0}, nil)
 
 	res, err := cache.Lookup(typeURL, resources[0].Name)
 	c.Assert(err, IsNil)
@@ -376,9 +362,7 @@ func (s *AckSuite) TestRevertDelete(c *C) {
 	c.Assert(res, Equals, resources[2])
 
 	// Delete.
-	comp = wg.AddCompletion()
-	defer comp.Complete(nil)
-	revert := acker.Delete(typeURL, resources[0].Name, []string{node0}, comp)
+	revert := acker.Delete(typeURL, resources[0].Name, []string{node0}, nil)
 
 	res, err = cache.Lookup(typeURL, resources[0].Name)
 	c.Assert(err, IsNil)
@@ -388,7 +372,7 @@ func (s *AckSuite) TestRevertDelete(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(res, Equals, resources[2])
 
-	comp = wg.AddCompletion()
+	comp := wg.AddCompletion()
 	defer comp.Complete(nil)
 	revert(comp)
 