aa505adc14df1d4ec1ab153e916680d7c1905b13
Joe Stringer <joe@covalent.io>
2018-10-16 10:58:22 -0700

    pidfile: Add 'Remove' to provide pidfile deletion
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- pkg/logging/logfields/logfields.go
+++ pkg/logging/logfields/logfields.go
@@ -302,4 +302,10 @@ const (
 
 	// Debug is a boolean value for whether debug is set or not.
 	Debug = "debug"
+
+	// PID is an integer value for the process identifier of a process.
+	PID = "pid"
+
+	// PIDFile is a string value for the path to a file containing a PID.
+	PIDFile = "pidfile"
 )
