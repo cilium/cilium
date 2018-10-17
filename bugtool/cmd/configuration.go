44e323eb21021797d6f55f23203a73dae9057867
Ian Vernon <ian@cilium.io>
2018-10-17 11:15:54 -0700

    bugtool: dump all policymap state from bpffs
    Add `cilium bpf policy get --all` to the bugtool.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- bugtool/cmd/configuration.go
+++ bugtool/cmd/configuration.go
@@ -222,6 +222,7 @@ func copyCiliumInfoCommands(cmdDir string, k8sPods []string) []string {
 		"cilium bpf ct list global",
 		"cilium bpf proxy list",
 		"cilium bpf ipcache list",
+		"cilium bpf policy get --all",
 		"cilium map list --verbose",
 		"cilium status --verbose",
 		"cilium identity list",
