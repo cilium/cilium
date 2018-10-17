cilium/cilium

commit a8a81b32a771e64a1827651f9ba565e087498382

pkg/endpoint: add UpdateStatus functionality for CEP
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/endpoint.go
@@ -82,6 +82,12 @@ var (
 	// controller via getCiliumClient and the sync.Once is used to avoid race.
 	ciliumEndpointSyncControllerOnce      sync.Once
 	ciliumEndpointSyncControllerK8sClient clientset.Interface
+
+	// ciliumUpdateStatusVerConstr is the minimal version supported for
+	// to perform a CRD UpdateStatus.
+	ciliumUpdateStatusVerConstr, _ = go_version.NewConstraint(">= 1.11.0")
+
+	k8sServerVer *go_version.Version
 )
 
 // getCiliumClient builds and returns a k8s auto-generated client for cilium
@@ -482,20 +488,21 @@ func (e *Endpoint) RunK8sCiliumEndpointSync() {
 		endpointID     = e.ID
 		controllerName = fmt.Sprintf("sync-to-k8s-ciliumendpoint (%v)", endpointID)
 		scopedLog      = e.getLogger().WithField("controller", controllerName)
+		err            error
 	)
 
 	if !k8s.IsEnabled() {
 		scopedLog.Debug("Not starting controller because k8s is disabled")
 		return
 	}
-	sv, err := k8s.GetServerVersion()
+	k8sServerVer, err = k8s.GetServerVersion()
 	if err != nil {
 		scopedLog.WithError(err).Error("unable to retrieve kubernetes serverversion")
 		return
 	}
-	if !ciliumEPControllerLimit.Check(sv) {
+	if !ciliumEPControllerLimit.Check(k8sServerVer) {
 		scopedLog.WithFields(logrus.Fields{
-			"expected": sv,
+			"expected": k8sServerVer,
 			"found":    ciliumEPControllerLimit,
 		}).Warn("cannot run with this k8s version")
 		return
@@ -583,10 +590,16 @@ func (e *Endpoint) RunK8sCiliumEndpointSync() {
 				case err == nil:
 					// Update the copy of the cep
 					k8sMdl.DeepCopyInto(&cep.Status)
-
-					if _, err = ciliumClient.CiliumEndpoints(namespace).Update(cep); err != nil {
-						scopedLog.WithError(err).Error("Cannot update CEP")
-						return err
+					var err2 error
+					switch {
+					case ciliumUpdateStatusVerConstr.Check(k8sServerVer):
+						_, err2 = ciliumClient.CiliumEndpoints(namespace).UpdateStatus(cep)
+					default:
+						_, err2 = ciliumClient.CiliumEndpoints(namespace).Update(cep)
+					}
+					if err2 != nil {
+						scopedLog.WithError(err2).Error("Cannot update CEP")
+						return err2
 					}
 
 					lastMdl = mdl

pkg/k8s/apis/cilium.io/v2/types.go
@@ -184,7 +184,6 @@ type CiliumNetworkPolicyList struct {
 }
 
 // +genclient
-// +genclient:noStatus
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // CiliumEndpoint is the status of a Cilium policy rule

pkg/k8s/client/clientset/versioned/typed/cilium.io/v2/ciliumendpoint.go
@@ -35,6 +35,7 @@ type CiliumEndpointsGetter interface {
 type CiliumEndpointInterface interface {
 	Create(*v2.CiliumEndpoint) (*v2.CiliumEndpoint, error)
 	Update(*v2.CiliumEndpoint) (*v2.CiliumEndpoint, error)
+	UpdateStatus(*v2.CiliumEndpoint) (*v2.CiliumEndpoint, error)
 	Delete(name string, options *v1.DeleteOptions) error
 	DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error
 	Get(name string, options v1.GetOptions) (*v2.CiliumEndpoint, error)
@@ -112,6 +113,22 @@ func (c *ciliumEndpoints) Update(ciliumEndpoint *v2.CiliumEndpoint) (result *v2.
 		Namespace(c.ns).
 		Resource("ciliumendpoints").
 		Name(ciliumEndpoint.Name).
+		Body(ciliumEndpoint).
+		Do().
+		Into(result)
+	return
+}
+
+// UpdateStatus was generated because the type contains a Status member.
+// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
+
+func (c *ciliumEndpoints) UpdateStatus(ciliumEndpoint *v2.CiliumEndpoint) (result *v2.CiliumEndpoint, err error) {
+	result = &v2.CiliumEndpoint{}
+	err = c.client.Put().
+		Namespace(c.ns).
+		Resource("ciliumendpoints").
+		Name(ciliumEndpoint.Name).
+		SubResource("status").
 		Body(ciliumEndpoint).
 		Do().
 		Into(result)

pkg/k8s/client/clientset/versioned/typed/cilium.io/v2/fake/fake_ciliumendpoint.go
@@ -98,6 +98,18 @@ func (c *FakeCiliumEndpoints) Update(ciliumEndpoint *v2.CiliumEndpoint) (result 
 	return obj.(*v2.CiliumEndpoint), err
 }
 
+// UpdateStatus was generated because the type contains a Status member.
+// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
+func (c *FakeCiliumEndpoints) UpdateStatus(ciliumEndpoint *v2.CiliumEndpoint) (*v2.CiliumEndpoint, error) {
+	obj, err := c.Fake.
+		Invokes(testing.NewUpdateSubresourceAction(ciliumendpointsResource, "status", c.ns, ciliumEndpoint), &v2.CiliumEndpoint{})
+
+	if obj == nil {
+		return nil, err
+	}
+	return obj.(*v2.CiliumEndpoint), err
+}
+
 // Delete takes name of the ciliumEndpoint and deletes it. Returns an error if one occurs.
 func (c *FakeCiliumEndpoints) Delete(name string, options *v1.DeleteOptions) error {
 	_, err := c.Fake.

commit 336ef4767d278b1dcb0652f4912cfa6cd7e3a7db

pkg/endpoint: fix global k8sServerVer variable assignment
As the variable is being globally assigned for all endpoints, if one
endpoints re-sets it to nil it can cause a nil pointer exception during
a controller execution causing the cilium-agent to crash.

Fixes: a8a81b32a771 ("pkg/endpoint: add UpdateStatus functionality for CEP")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/endpoint.go
@@ -61,7 +61,6 @@ import (
 	"github.com/cilium/cilium/pkg/u8proto"
 	"github.com/cilium/cilium/pkg/versioncheck"
 
-	go_version "github.com/hashicorp/go-version"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
@@ -89,8 +88,6 @@ var (
 	// ciliumUpdateStatusVerConstr is the minimal version supported for
 	// to perform a CRD UpdateStatus.
 	ciliumUpdateStatusVerConstr = versioncheck.MustCompile(">= 1.11.0")
-
-	k8sServerVer *go_version.Version
 )
 
 // getCiliumClient builds and returns a k8s auto-generated client for cilium
@@ -554,7 +551,7 @@ func (e *Endpoint) RunK8sCiliumEndpointSync() {
 		scopedLog.Debug("Not starting controller because k8s is disabled")
 		return
 	}
-	k8sServerVer, err = k8s.GetServerVersion()
+	k8sServerVer, err := k8s.GetServerVersion()
 	if err != nil {
 		scopedLog.WithError(err).Error("unable to retrieve kubernetes serverversion")
 		return