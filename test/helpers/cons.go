267fbbbfd39120ca00e0cb7f04fe35d0f16946c2
Thomas Graf <thomas@cilium.io>
2018-10-19 18:36:18 +0200

    test: Use cilium-etcd-operator
    The cilium-etcd-operator takes care of secret key creation and will
    re-bootstrap the etcd cluster after it has scaled down to a point where the
    etcd operator can't recover.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- test/helpers/cons.go
+++ test/helpers/cons.go
@@ -255,15 +255,7 @@ var ciliumKubCLICommands = map[string]string{
 	"cilium kvstore get cilium --recursive": "kvstore_get.txt",
 }
 
-// etcdDeploymentFiles are the list of files used to deploy etcd operator.
-var etcdDeploymentFiles = []string{
-	"00-crd-etcd.yaml",
-	"cilium-etcd-sa.yaml",
-	"cluster-role-binding-template.yaml",
-	"cluster-role-template.yaml",
-	"deployment.yaml",
-	"cilium-etcd-cluster.yaml",
-}
+const ciliumEtcdOperator = "https://raw.githubusercontent.com/cilium/cilium-etcd-operator/1.2/cilium-etcd-operator.yaml"
 
 //GetFilePath returns the absolute path of the provided filename
 func GetFilePath(filename string) string {
