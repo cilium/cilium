1b7d945b9623a91075c57ffa6a32f433e53f4469
Cynthia Thomas <cynthia@covalent.io>
2018-10-22 12:17:53 -0700

    Documentation: memcached gsg + link fix
    [Upstream commit bd5acfd4760e7ff067528cac3efb1ac3a9d2eb04]
    
    Signed-off-by: Cynthia Thomas <cynthia@covalent.io>
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ examples/kubernetes-memcached/memcd-env.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+#define ENV variables for memcached demo
+MEMCD_SERVER_POD=$(kubectl get pods -l app=memcd-server -o jsonpath='{.items[0].metadata.name}')
+AWING_POD=$(kubectl get pods -l app=a-wing -o jsonpath='{.items[0].metadata.name}')
+XWING_POD=$(kubectl get pods -l app=x-wing -o jsonpath='{.items[0].metadata.name}')
+TRACKER_POD=$(kubectl get pods -l name=fleet-tracker -o jsonpath='{.items[0].metadata.name}')
+SETXC="set xwing-coord 0 2400 16\r\n8893.34,234.3290\r\nquit\r\n"
+GETXC="get xwing-coord \r\nquit\r\n"
+GETAC="get awing-coord \r\nquit\r\n"
+
+echo "MEMCD_SERVER_POD=$(kubectl get pods -l app=memcd-server -o jsonpath='{.items[0].metadata.name}')"
+echo "AWING_POD=$(kubectl get pods -l app=a-wing -o jsonpath='{.items[0].metadata.name}')"
+echo "XWING_POD=$(kubectl get pods -l app=x-wing -o jsonpath='{.items[0].metadata.name}')"
+echo "TRACKER_POD=$(kubectl get pods -l name=fleet-tracker -o jsonpath='{.items[0].metadata.name}')"
+echo "SETXC=\"set xwing-coord 0 2400 16\\r\\n8893.34,234.3290\\r\\nquit\\r\\n\""
+echo "GETXC=\"get xwing-coord \\r\\nquit\\r\\n\""
+echo "GETAC=\"get awing-coord \\r\\nquit\\r\\n\""
