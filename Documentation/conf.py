73426a3b0d21e0df07701592b8d39543038906c7
André Martins <andre@cilium.io>
2018-10-22 16:02:41 +0200

    wip
    Signed-off-by: André Martins <andre@cilium.io>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -109,9 +109,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes/sphinx_rtd_theme", ]
-html_style = "static/css/theme.css"
+html_theme = "alabaster"
+# html_theme_path = ["_themes/sphinx_rtd_theme", ]
+# html_style = "static/css/theme.css"
 html_context = {
         'release': release
 }
