1d13fd25800d4a49161e91b0079eacf8fd971563
André Martins <andre@cilium.io>
2018-10-30 15:42:26 +0100

    Revert "build: Remove travis integration"
    This reverts commit b7483ba03d590d87d9b4b26736cc15d776eef404.

---
+++ .travis/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CLANG_DIR="clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-14.04"
+CLANG_FILE="${CLANG_DIR}.tar.xz"
+CLANG_URL="http://releases.llvm.org/3.8.1/$CLANG_FILE"
+
+wget -nv $CLANG_URL
+sudo rm -rf /usr/local/clang
+sudo mkdir -p /usr/local
+sudo tar -C /usr/local -xJf $CLANG_FILE
+sudo ln -s /usr/local/$CLANG_DIR /usr/local/clang
+rm $CLANG_FILE
+
+NEWPATH="/usr/local/clang/bin"
+export PATH="$NEWPATH:$PATH"
