ae6566cf27c06dab18b9694a99c65acb03de52d9
André Martins <andre@cilium.io>
2018-10-30 15:50:14 +0100

    travis: change target to run unit-tests
    Signed-off-by: André Martins <andre@cilium.io>

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -13,3 +13,11 @@ rm $CLANG_FILE
 
 NEWPATH="/usr/local/clang/bin"
 export PATH="$NEWPATH:$PATH"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+go get -u github.com/jteeuwen/go-bindata/...
+go get golang.org/x/tools/cmd/cover
+go get github.com/mattn/goveralls
