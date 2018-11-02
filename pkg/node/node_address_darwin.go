be4e2aeb2335662fd879db5f0c199ca39de49c06
Ian Vernon <ian@cilium.io>
2018-11-02 09:20:34 -0700

    route, node: create stub files for Darwin builds
    Running unit tests on Mac OS environments for many packages in Cilium has not
    been possible due to the fact that there are dependencies which depend on
    variables that are defined only for Linux architectures, e.g. netlink.FAMILY_V6.
    
    Refactor the node and route packages to have Linux-specific and Darwin-specific
    files, by using build constraints. For Darwin builds, stubs are created, while
    the implementation used by the cilium-agent is factored out into files built
    for non-Darwin builds. This allows for running of unit tests on the Mac for
    `pkg/policy`, for instance.
    
    No functional change is intended by this.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

---
+++ pkg/node/node_address_darwin.go
@@ -0,0 +1,43 @@
+// Copyright 2016-2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// +build darwin
+
+package node
+
+import (
+	"net"
+
+	"github.com/vishvananda/netlink"
+)
+
+func firstGlobalV4Addr(intf string) (net.IP, error) {
+	return net.IP{}, nil
+}
+
+func findIPv6NodeAddr() net.IP {
+	return net.IP{}
+}
+
+// getCiliumHostIPsFromNetDev returns the first IPv4 link local and returns
+// it
+func getCiliumHostIPsFromNetDev(devName string) (ipv4GW, ipv6Router net.IP) {
+	return net.IP{}, net.IP{}
+}
+
+// firstLinkWithv6 returns the first network interface that contains the given
+// IPv6 address.
+func firstLinkWithv6(ip net.IP) (netlink.Link, error) {
+	return nil, nil
+}
