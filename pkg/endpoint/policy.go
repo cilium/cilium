cilium/cilium

commit 20a49da060fdd7cd4503e76f6ab05e4e4c44067a

Consistently check for liveness of endpoint when re-locking (#5116)
Consistently check for liveness of endpoint when re-locking

Signed-off-by: Maciej Kwiek <maciej@covalent.io>

pkg/endpoint/policy.go
@@ -690,9 +690,11 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	e.BuildMutex.Lock()
 	defer e.BuildMutex.Unlock()
 
-	e.Mutex.RLock()
+	if err = e.RLockAlive(); err != nil {
+		return err
+	}
 	scopedLog := e.getLogger()
-	e.Mutex.RUnlock()
+	e.RUnlock()
 	scopedLog.Debug("Regenerating endpoint...")
 
 	origDir := filepath.Join(owner.GetStateDir(), e.StringID())
@@ -708,13 +710,20 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	}
 
 	defer func() {
+		if err := e.LockAlive(); err != nil {
+			if retErr == nil {
+				retErr = err
+			} else {
+				e.LogDisconnectedMutexAction(err, "after regenerate")
+			}
+			return
+		}
 		// Set to Ready, but only if no other changes are pending.
 		// State will remain as waiting-to-regenerate if further
 		// changes are needed. There should be an another regenerate
 		// queued for taking care of it.
-		e.Mutex.Lock()
 		e.BuilderSetStateLocked(StateReady, "Completed endpoint regeneration with no pending regeneration requests")
-		e.Mutex.Unlock()
+		e.Unlock()
 	}()
 
 	revision, compilationExecuted, err = e.regenerateBPF(owner, tmpDir, reason)
@@ -768,13 +777,14 @@ func (e *Endpoint) regenerate(owner Owner, reason string) (retErr error) {
 	// Update desired policy for endpoint because policy has now been realized
 	// in the datapath. PolicyMap state is not updated here, because that is
 	// performed in endpoint.syncPolicyMap().
-	e.Mutex.Lock()
+	if err = e.LockAlive(); err != nil {
+		return err
+	}
 	e.RealizedL4Policy = e.DesiredL4Policy
-	e.Mutex.Unlock()
-
 	// Mark the endpoint to be running the policy revision it was
 	// compiled for
-	e.bumpPolicyRevision(revision)
+	e.bumpPolicyRevisionLocked(revision)
+	e.Unlock()
 
 	scopedLog.Info("Endpoint policy recalculated")
 
@@ -795,10 +805,16 @@ func (e *Endpoint) Regenerate(owner Owner, reason string) <-chan bool {
 	go func(owner Owner, req *Request, e *Endpoint) {
 		buildSuccess := true
 
-		e.Mutex.RLock()
+		err := e.RLockAlive()
+		if err != nil {
+			e.LogDisconnectedMutexAction(err, "before regeneration")
+			req.ExternalDone <- false
+			close(req.ExternalDone)
+			return
+		}
 		// This must be accessed in a locked section, so we grab it here.
 		scopedLog := e.getLogger()
-		e.Mutex.RUnlock()
+		e.RUnlock()
 
 		// We should only queue the request after we use all the endpoint's
 		// lock/unlock. Otherwise this can get a deadlock if the endpoint is
@@ -886,11 +902,13 @@ func (e *Endpoint) runIdentityToK8sPodSync() {
 			DoFunc: func() error {
 				id := ""
 
-				e.Mutex.RLock()
+				if err := e.RLockAlive(); err != nil {
+					return err
+				}
 				if e.SecurityIdentity != nil {
 					id = e.SecurityIdentity.ID.StringID()
 				}
-				e.Mutex.RUnlock()
+				e.RUnlock()
 
 				if id != "" && e.GetK8sNamespace() != "" && e.GetK8sPodName() != "" {
 					return k8s.AnnotatePod(e, k8sConst.CiliumIdentityAnnotation, id)
@@ -925,17 +943,19 @@ func (e *Endpoint) runIPIdentitySync(endpointIP addressing.CiliumIP) {
 		controller.ControllerParams{
 			DoFunc: func() error {
 
-				e.Mutex.RLock()
+				// NOTE: this Lock is Unconditional because this controller
+				// handles disconnecting endpoint state properly
+				e.UnconditionalRLock()
 
 				if e.state == StateDisconnected || e.state == StateDisconnecting {
 					log.WithFields(logrus.Fields{logfields.EndpointState: e.state}).
 						Debugf("not synchronizing endpoint IP with kvstore due to endpoint state")
-					e.Mutex.RUnlock()
+					e.RUnlock()
 					return nil
 				}
 
 				if e.SecurityIdentity == nil {
-					e.Mutex.RUnlock()
+					e.RUnlock()
 					return nil
 				}
 
@@ -946,7 +966,7 @@ func (e *Endpoint) runIPIdentitySync(endpointIP addressing.CiliumIP) {
 
 				// Release lock as we do not want to have long-lasting key-value
 				// store operations resulting in lock being held for a long time.
-				e.Mutex.RUnlock()
+				e.RUnlock()
 
 				if err := ipcache.UpsertIPToKVStore(IP, hostIP, ID, metadata); err != nil {
 					return fmt.Errorf("unable to add endpoint IP mapping '%s'->'%d': %s", IP.String(), ID, err)

daemon/endpoint.go
@@ -257,10 +257,12 @@ func (h *putEndpointID) Handle(params PutEndpointIDParams) middleware.Responder 
 		for {
 			select {
 			case <-ticker.C:
-				e.Mutex.RLock()
+				if err := e.RLockAlive(); err != nil {
+					return api.Error(PutEndpointIDFailedCode, fmt.Errorf("error locking endpoint: %s", err.Error()))
+				}
 				epState := e.GetStateLocked()
 				hasSidecarProxy := e.HasSidecarProxy()
-				e.Mutex.RUnlock()
+				e.RUnlock()
 
 				if epState == endpoint.StateReady {
 					return NewPutEndpointIDCreated()
@@ -341,13 +343,7 @@ func (h *patchEndpointID) Handle(params PatchEndpointIDParams) middleware.Respon
 	//
 	//  Support arbitrary changes? Support only if unset?
 
-	ep.Mutex.Lock()
-
-	// The endpoint may have just been deleted since the lookup, so return
-	// that it can't be found.
-	if ep.GetStateLocked() == endpoint.StateDisconnecting ||
-		ep.GetStateLocked() == endpoint.StateDisconnected {
-		ep.Mutex.Unlock()
+	if err := ep.LockAlive(); err != nil {
 		return NewPatchEndpointIDNotFound()
 	}
 
@@ -429,7 +425,8 @@ func (h *patchEndpointID) Handle(params PatchEndpointIDParams) middleware.Respon
 			reason = "Waiting on endpoint initial program regeneration while handling API PATCH"
 		}
 	}
-	ep.Mutex.Unlock()
+
+	ep.Unlock()
 
 	if reason != "" {
 		if err := ep.RegenerateWait(h.d, reason); err != nil {
@@ -473,12 +470,12 @@ func (d *Daemon) deleteEndpointQuiet(ep *endpoint.Endpoint, releaseIP bool) []er
 	ep.BuildMutex.Lock()
 
 	// Lock out any other writers to the endpoint
-	ep.Mutex.Lock()
+	ep.UnconditionalLock()
 
 	// In case multiple delete requests have been enqueued, have all of them
 	// except the first return here.
 	if ep.GetStateLocked() == endpoint.StateDisconnecting {
-		ep.Mutex.Unlock()
+		ep.Unlock()
 		ep.BuildMutex.Unlock()
 		return []error{}
 	}
@@ -535,7 +532,7 @@ func (d *Daemon) deleteEndpointQuiet(ep *endpoint.Endpoint, releaseIP bool) []er
 	proxyWaitGroup := completion.NewWaitGroup(completionCtx)
 
 	errors = append(errors, ep.LeaveLocked(d, proxyWaitGroup)...)
-	ep.Mutex.Unlock()
+	ep.Unlock()
 
 	err := ep.WaitForProxyCompletions(proxyWaitGroup)
 	if err != nil {
@@ -603,9 +600,11 @@ func (d *Daemon) EndpointUpdate(id string, cfg *models.EndpointConfigurationSpec
 				return api.Error(PatchEndpointIDConfigFailedCode, err)
 			}
 		}
-		ep.Mutex.RLock()
+		if err := ep.RLockAlive(); err != nil {
+			return api.Error(PatchEndpointIDNotFoundCode, err)
+		}
 		endpointmanager.UpdateReferences(ep)
-		ep.Mutex.RUnlock()
+		ep.RUnlock()
 	} else {
 		return api.New(PatchEndpointIDConfigNotFoundCode, "endpoint %s not found", id)
 	}
@@ -685,7 +684,9 @@ func (h *getEndpointIDLabels) Handle(params GetEndpointIDLabelsParams) middlewar
 		return NewGetEndpointIDLabelsNotFound()
 	}
 
-	ep.Mutex.RLock()
+	if err := ep.RLockAlive(); err != nil {
+		return api.Error(GetEndpointIDInvalidCode, err)
+	}
 	spec := &models.LabelConfigurationSpec{
 		User: ep.OpLabels.Custom.GetModel(),
 	}
@@ -699,7 +700,7 @@ func (h *getEndpointIDLabels) Handle(params GetEndpointIDLabelsParams) middlewar
 			Disabled:         ep.OpLabels.Disabled.GetModel(),
 		},
 	}
-	ep.Mutex.RUnlock()
+	ep.RUnlock()
 
 	return NewGetEndpointIDLabelsOK().WithPayload(&cfg)
 }
@@ -817,9 +818,11 @@ func (h *putEndpointIDLabels) Handle(params PatchEndpointIDLabelsParams) middlew
 		return NewPatchEndpointIDLabelsNotFound()
 	}
 
-	ep.Mutex.RLock()
+	if err := ep.RLockAlive(); err != nil {
+		return api.Error(PutEndpointIDInvalidCode, err)
+	}
 	currentLbls := ep.OpLabels.DeepCopy()
-	ep.Mutex.RUnlock()
+	ep.RUnlock()
 
 	for _, lbl := range lbls {
 		if currentLbls.Custom[lbl.Key] == nil {

daemon/endpoint_test.go
@@ -114,9 +114,9 @@ Loop:
 		case <-timeout.C:
 			break Loop
 		case <-tick.C:
-			ep.Mutex.RLock()
+			ep.UnconditionalRLock()
 			secID = ep.SecurityIdentity
-			ep.Mutex.RUnlock()
+			ep.RUnlock()
 			if secID != nil {
 				break Loop
 			}

daemon/logstash.go
@@ -71,14 +71,16 @@ func EnableLogstash(LogstashAddr string, refreshTime int) {
 			allPes := map[uint16][]policymap.PolicyEntryDump{}
 			eps := endpointmanager.GetEndpoints()
 			for _, ep := range eps {
-				ep.Mutex.RLock()
+				// NOTE: UnconditionalRLock is used here because endpoint state doesn't matter here
+				// all endpoint info is continuosly dumped anyway
+				ep.UnconditionalRLock()
 				pes, err := ep.PolicyMap.DumpToSlice()
 				if err != nil {
-					ep.Mutex.RUnlock()
+					ep.RUnlock()
 					continue
 				}
 				allPes[ep.ID] = pes
-				ep.Mutex.RUnlock()
+				ep.RUnlock()
 			}
 			lss := processStats(allPes)
 			for _, ls := range lss {

daemon/policy_test.go
@@ -154,9 +154,9 @@ func (ds *DaemonSuite) TestUpdateConsumerMap(c *C) {
 		os.RemoveAll("1_backup")
 	}()
 	e.SetIdentity(qaBarSecLblsCtx)
-	e.Mutex.Lock()
+	e.UnconditionalLock()
 	ready := e.SetStateLocked(endpoint.StateWaitingToRegenerate, "test")
-	e.Mutex.Unlock()
+	e.Unlock()
 	c.Assert(ready, Equals, true)
 	buildSuccess := <-e.Regenerate(ds.d, "test")
 	c.Assert(buildSuccess, Equals, true)
@@ -172,9 +172,9 @@ func (ds *DaemonSuite) TestUpdateConsumerMap(c *C) {
 	e.LXCMAC = ProdHardAddr
 	e.NodeMAC = ProdHardAddr
 	e.SetIdentity(prodBarSecLblsCtx)
-	e.Mutex.Lock()
+	e.UnconditionalLock()
 	ready = e.SetStateLocked(endpoint.StateWaitingToRegenerate, "test")
-	e.Mutex.Unlock()
+	e.Unlock()
 	c.Assert(ready, Equals, true)
 	buildSuccess = <-e.Regenerate(ds.d, "test")
 	c.Assert(buildSuccess, Equals, true)
@@ -438,9 +438,9 @@ func (ds *DaemonSuite) TestRemovePolicy(c *C) {
 		os.RemoveAll("1_backup")
 	}()
 	e.SetIdentity(qaBarSecLblsCtx)
-	e.Mutex.Lock()
+	e.UnconditionalLock()
 	ready := e.SetStateLocked(endpoint.StateWaitingToRegenerate, "test")
-	e.Mutex.Unlock()
+	e.Unlock()
 	c.Assert(ready, Equals, true)
 	buildSuccess := <-e.Regenerate(ds.d, "test")
 	c.Assert(buildSuccess, Equals, true)
@@ -453,9 +453,9 @@ func (ds *DaemonSuite) TestRemovePolicy(c *C) {
 	c.Assert(qaBarNetworkPolicy, Not(IsNil))
 
 	// Delete the endpoint.
-	e.Mutex.Lock()
+	e.UnconditionalLock()
 	e.LeaveLocked(ds.d, nil)
-	e.Mutex.Unlock()
+	e.Unlock()
 
 	// Check that the policy has been removed from the xDS cache.
 	networkPolicies = ds.getXDSNetworkPolicies(c, nil)

daemon/state.go
@@ -97,12 +97,12 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 			continue
 		}
 
-		ep.Mutex.Lock()
+		ep.UnconditionalLock()
 		scopedLog.Debug("Restoring endpoint")
 		ep.LogStatusOKLocked(endpoint.Other, "Restoring endpoint from previous cilium instance")
 
 		if err := d.allocateIPsLocked(ep); err != nil {
-			ep.Mutex.Unlock()
+			ep.Unlock()
 			scopedLog.WithError(err).Error("Failed to re-allocate IP of endpoint. Not restoring endpoint.")
 			state.toClean = append(state.toClean, ep)
 			continue
@@ -117,7 +117,7 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 			ep.Options.SetBool(option.EgressPolicy, alwaysEnforce)
 		}
 
-		ep.Mutex.Unlock()
+		ep.Unlock()
 
 		state.restored = append(state.restored, ep)
 	}
@@ -144,28 +144,29 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 		// not in a goroutine) because regenerateRestoredEndpoints must guarantee
 		// upon returning that endpoints are exposed to other subsystems via
 		// endpointmanager.
-		ep.Mutex.RLock()
+
+		ep.UnconditionalRLock()
 		endpointmanager.Insert(ep)
-		ep.Mutex.RUnlock()
+		ep.RUnlock()
 
 		go func(ep *endpoint.Endpoint, epRegenerated chan<- bool) {
-			ep.Mutex.RLock()
+			if err := ep.RLockAlive(); err != nil {
+				ep.LogDisconnectedMutexAction(err, "before filtering labels during regenerating restored endpoint")
+				return
+			}
 			scopedLog := log.WithField(logfields.EndpointID, ep.ID)
 			// Filter the restored labels with the new daemon's filter
 			l, _ := labels.FilterLabels(ep.OpLabels.IdentityLabels())
-			ep.Mutex.RUnlock()
+			ep.RUnlock()
 
 			identity, _, err := identityPkg.AllocateIdentity(l)
 			if err != nil {
 				scopedLog.WithError(err).Warn("Unable to restore endpoint")
 				epRegenerated <- false
 			}
-			ep.Mutex.Lock()
 
-			state := ep.GetStateLocked()
-			if state == endpoint.StateDisconnected || state == endpoint.StateDisconnecting {
+			if err := ep.LockAlive(); err != nil {
 				scopedLog.Warn("Endpoint to restore has been deleted")
-				ep.Mutex.Unlock()
 				return
 			}
 
@@ -183,7 +184,7 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 			ep.SetIdentity(identity)
 
 			ready := ep.SetStateLocked(endpoint.StateWaitingToRegenerate, "Triggering synchronous endpoint regeneration while syncing state to host")
-			ep.Mutex.Unlock()
+			ep.Unlock()
 
 			if !ready {
 				scopedLog.WithField(logfields.EndpointState, ep.GetState()).Warn("Endpoint in inconsistent state")
@@ -196,9 +197,10 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 				return
 			}
 
-			ep.Mutex.RLock()
+			// NOTE: UnconditionalRLock is used here because it's used only for logging an already restored endpoint
+			ep.UnconditionalRLock()
 			scopedLog.WithField(logfields.IPAddr, []string{ep.IPv4.String(), ep.IPv6.String()}).Info("Restored endpoint")
-			ep.Mutex.RUnlock()
+			ep.RUnlock()
 			epRegenerated <- true
 		}(ep, epRegenerated)
 	}

daemon/state_test.go
@@ -141,9 +141,10 @@ func (ds *DaemonSuite) generateEPs(baseDir string, epsWanted []*e.Endpoint, epsM
 	epsNames := []string{}
 	for _, ep := range epsWanted {
 		os.MkdirAll(filepath.Join(baseDir, ep.StringID()), 777)
-		ep.Mutex.Lock()
+		ep.UnconditionalLock()
+
 		ready := ep.SetStateLocked(e.StateWaitingToRegenerate, "test")
-		ep.Mutex.Unlock()
+		ep.Unlock()
 		if ready {
 			<-ep.Regenerate(ds, "test")
 		}

pkg/endpoint/bpf.go
@@ -270,9 +270,11 @@ func (e *Endpoint) runInit(libdir, rundir, epdir, ifName, debug string) error {
 	args := []string{libdir, rundir, epdir, ifName, debug, e.StringID()}
 	prog := filepath.Join(libdir, "join_ep.sh")
 
-	e.Mutex.RLock()
+	if err := e.RLockAlive(); err != nil {
+		return err
+	}
 	scopedLog := e.getLogger() // must be called with e.Mutex held
-	e.Mutex.RUnlock()
+	e.RUnlock()
 
 	ctx, cancel := context.WithTimeout(context.Background(), ExecTimeout)
 	defer cancel()
@@ -449,7 +451,7 @@ func (e *Endpoint) removeOldRedirects(owner Owner, desiredRedirects map[string]b
 // Must be called with endpoint.Mutex not held and endpoint.BuildMutex held.
 // Returns the policy revision number when the regeneration has called, a
 // boolean if the BPF compilation was executed and an error in case of an error.
-func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, bool, error) {
+func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (revnum uint64, compiled bool, reterr error) {
 	var (
 		err                 error
 		compilationExecuted bool
@@ -462,14 +464,15 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 
 	buildStart := time.Now()
 
-	e.Mutex.Lock()
+	if err = e.LockAlive(); err != nil {
+		return 0, compilationExecuted, err
+	}
+
 	logger := e.getLogger()
 	logger.WithField(logfields.StartTime, time.Now()).Info("Regenerating BPF program")
 	defer func() {
-		e.Mutex.RLock()
 		logger.WithField(logfields.BuildDuration, time.Since(buildStart).String()).
 			Info("Regeneration of BPF program has completed")
-		e.Mutex.RUnlock()
 	}()
 
 	// If endpoint was marked as disconnected then
@@ -480,13 +483,14 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 		!e.BuilderSetStateLocked(StateRegenerating, "Regenerating Endpoint BPF: "+reason) {
 
 		logger.WithField(logfields.EndpointState, e.state).Debug("Skipping build due to invalid state")
-		e.Mutex.Unlock()
+		e.Unlock()
+
 		return 0, compilationExecuted, fmt.Errorf("Skipping build due to invalid state: %s", e.state)
 	}
 
 	// If dry mode is enabled, no further changes to BPF maps are performed
 	if owner.DryModeEnabled() {
-		defer e.Mutex.Unlock()
+		defer e.Unlock()
 
 		// Regenerate policy and apply any options resulting in the
 		// policy change.
@@ -514,8 +518,8 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 	createdPolicyMap := false
 
 	defer func() {
-		if err != nil {
-			e.Mutex.Lock()
+		if reterr != nil {
+			e.UnconditionalLock()
 			epLogger := e.getLogger()
 			epLogger.WithError(err).Error("destroying BPF maps due to" +
 				" errors during regeneration")
@@ -524,21 +528,21 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 				os.RemoveAll(e.PolicyMapPathLocked())
 				e.PolicyMap = nil
 			}
-			e.Mutex.Unlock()
+			e.Unlock()
 		}
 	}()
 
 	if e.PolicyMap == nil {
 		e.PolicyMap, createdPolicyMap, err = policymap.OpenMap(e.PolicyMapPathLocked())
 		if err != nil {
-			e.Mutex.Unlock()
+			e.Unlock()
 			return 0, compilationExecuted, err
 		}
 		// Clean up map contents
 		logger.Debug("flushing old PolicyMap")
 		err = e.PolicyMap.Flush()
 		if err != nil {
-			e.Mutex.Unlock()
+			e.Unlock()
 			return 0, compilationExecuted, err
 		}
 	}
@@ -557,7 +561,7 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 		// This also populates e.PolicyMap.
 		_, err = e.regeneratePolicy(owner, nil)
 		if err != nil {
-			e.Mutex.Unlock()
+			e.Unlock()
 			return 0, compilationExecuted, fmt.Errorf("unable to regenerate policy for '%s': %s", e.PolicyMap.String(), err)
 		}
 
@@ -569,13 +573,13 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 		// with the old one.
 		err := e.syncPolicyMap()
 		if err != nil {
-			e.Mutex.Unlock()
+			e.Unlock()
 			return 0, compilationExecuted, fmt.Errorf("unable to regenerate policy because PolicyMap synchronization failed: %s", err)
 		}
 
 		// Configure the new network policy with the proxies.
 		if err = e.updateNetworkPolicy(owner, proxyWaitGroup); err != nil {
-			e.Mutex.Unlock()
+			e.Unlock()
 			return 0, compilationExecuted, err
 		}
 	}
@@ -583,7 +587,7 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 	// Generate header file specific to this endpoint for use in compiling
 	// BPF programs for this endpoint.
 	if err = e.writeHeaderfile(epdir, owner); err != nil {
-		e.Mutex.Unlock()
+		e.Unlock()
 		return 0, compilationExecuted, fmt.Errorf("unable to write header file: %s", err)
 	}
 
@@ -605,7 +609,7 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 	// TODO (ianvernon): why do we need to do this?
 	epInfoCache := e.createEpInfoCache()
 	if epInfoCache == nil {
-		e.Mutex.Unlock()
+		e.Unlock()
 		err = fmt.Errorf("Unable to cache endpoint information")
 		return 0, compilationExecuted, err
 	}
@@ -616,7 +620,7 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 	os.RemoveAll(e.IPv6IngressMapPathLocked())
 	os.RemoveAll(e.IPv4IngressMapPathLocked())
 
-	e.Mutex.Unlock()
+	e.Unlock()
 	logger.WithField("bpfHeaderfilesChanged", bpfHeaderfilesChanged).Debug("Preparing to compile BPF")
 	libdir := owner.GetBpfDir()
 	rundir := owner.GetStateDir()
@@ -635,20 +639,22 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 		compilationExecuted = true
 		e.bpfHeaderfileHash = bpfHeaderfilesHash
 	} else {
-		e.Mutex.RLock()
+		e.UnconditionalRLock()
 		logger.WithField(logfields.BPFHeaderfileHash, bpfHeaderfilesHash).
 			Debug("BPF header file unchanged, skipping BPF compilation and installation")
-		e.Mutex.RUnlock()
+		e.RUnlock()
 	}
 
-	e.Mutex.Lock()
+	if err = e.LockAlive(); err != nil {
+		return 0, compilationExecuted, err
+	}
 	// Walk the L4Policy to add new redirects and update the desired policy map
 	// state to set the newly allocated proxy ports.
 	var desiredRedirects map[string]bool
 	if e.DesiredL4Policy != nil {
 		desiredRedirects, err = e.addNewRedirects(owner, e.DesiredL4Policy, proxyWaitGroup)
 		if err != nil {
-			e.Mutex.Unlock()
+			e.Unlock()
 			return 0, compilationExecuted, err
 		}
 	}
@@ -656,18 +662,17 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 	// now-obsolete redirects, since we synced the updated policy map above.
 	// It's now safe to remove the redirects from the proxy's configuration.
 	e.removeOldRedirects(owner, desiredRedirects, proxyWaitGroup)
-	e.Mutex.Unlock()
+	e.Unlock()
+
 	err = e.WaitForProxyCompletions(proxyWaitGroup)
 	if err != nil {
 		return 0, compilationExecuted, fmt.Errorf("Error while configuring proxy redirects: %s", err)
 	}
 
-	e.Mutex.Lock()
-	defer e.Mutex.Unlock()
-
-	if e.IsDisconnecting() {
-		return 0, compilationExecuted, fmt.Errorf("endpoint was removed while waiting for proxy to be configured")
+	if err = e.LockAlive(); err != nil {
+		return 0, compilationExecuted, err
 	}
+	defer e.Unlock()
 
 	// Synchronously try to update PolicyMap for this endpoint. If any
 	// part of updating the PolicyMap fails, bail out and do not generate

commit 313b9b7aad511b170ff576185d1b051bb4a1b341

pkg/endpoint: do not leak go routines if endpoint is disconnected
As the external listner can ignore the value written in the channel we
need to make sure the go routine doesn't block until the channel is
read.

Fixes: 20a49da060fd ("Consistently check for liveness of endpoint when re-locking (#5116)")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/policy.go
@@ -821,12 +821,19 @@ func (e *Endpoint) Regenerate(owner Owner, context *RegenerationContext) <-chan 
 
 	go func(owner Owner, req *Request, e *Endpoint) {
 		var buildSuccess bool
+		defer func() {
+			// The external listener can ignore the channel so we need to
+			// make sure we don't block
+			select {
+			case req.ExternalDone <- buildSuccess:
+			default:
+			}
+			close(req.ExternalDone)
+		}()
 
 		err := e.RLockAlive()
 		if err != nil {
 			e.LogDisconnectedMutexAction(err, "before regeneration")
-			req.ExternalDone <- false
-			close(req.ExternalDone)
 			return
 		}
 		e.RUnlock()
@@ -862,16 +869,8 @@ func (e *Endpoint) Regenerate(owner Owner, context *RegenerationContext) <-chan 
 			req.Done <- buildSuccess
 		} else {
 			buildSuccess = false
-
 			scopedLog.Debug("My request was cancelled because I'm already in line")
 		}
-		// The external listener can ignore the channel so we need to
-		// make sure we don't block
-		select {
-		case req.ExternalDone <- buildSuccess:
-		default:
-		}
-		close(req.ExternalDone)
 	}(owner, newReq, e)
 	return newReq.ExternalDone
 }