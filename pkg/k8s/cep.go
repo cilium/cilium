e26781f4690338a538a11f9f486275811d23a5d1
Ray Bejjani <ray@covalent.io>
2018-11-08 20:55:54 +0100

    k8s: CEP controller retries k8s version checks
    When requests for the k8s version are flaky, newly created CEP
    controllers will not create the controller and never try again. The
    check is now repeated in the controller until it succeeds, and the
    result cached.
    
    Signed-off-by: Ray Bejjani <ray@covalent.io>

--- pkg/k8s/cep.go
+++ pkg/k8s/cep.go
@@ -32,6 +32,7 @@ import (
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/versioncheck"
+	go_version "github.com/hashicorp/go-version"
 
 	"github.com/sirupsen/logrus"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
@@ -225,18 +226,6 @@ func (epSync *EndpointSynchronizer) RunK8sCiliumEndpointSync(e *endpoint.Endpoin
 		scopedLog.Debug("Not starting controller because k8s is disabled")
 		return
 	}
-	k8sServerVer, err := GetServerVersion()
-	if err != nil {
-		scopedLog.WithError(err).Error("unable to retrieve kubernetes serverversion")
-		return
-	}
-	if !ciliumEPControllerLimit.Check(k8sServerVer) {
-		scopedLog.WithFields(logrus.Fields{
-			"expected": k8sServerVer,
-			"found":    ciliumEPControllerLimit,
-		}).Warn("cannot run with this k8s version")
-		return
-	}
 
 	ciliumClient, err := getCiliumClient()
 	if err != nil {
@@ -252,8 +241,9 @@ func (epSync *EndpointSynchronizer) RunK8sCiliumEndpointSync(e *endpoint.Endpoin
 	}
 
 	var (
-		lastMdl  *models.Endpoint
-		firstRun = true
+		lastMdl      *models.Endpoint
+		firstRun     = true
+		k8sServerVer *go_version.Version // CEPs are not supported with certain versions
 	)
 
 	// NOTE: The controller functions do NOT hold the endpoint locks
@@ -264,6 +254,27 @@ func (epSync *EndpointSynchronizer) RunK8sCiliumEndpointSync(e *endpoint.Endpoin
 				// Update logger as scopeLog might not have the podName when it
 				// was created.
 				scopedLog = e.Logger(subsysK8s).WithField("controller", controllerName)
+
+				// This lookup can fail but once we do it once, we no longer want to try again.
+				if k8sServerVer == nil {
+					var err error
+					k8sServerVer, err = GetServerVersion()
+					switch {
+					case err != nil:
+						scopedLog.WithError(err).Error("Unable to retrieve kubernetes server version")
+						return err
+
+					case !ciliumEPControllerLimit.Check(k8sServerVer):
+						scopedLog.WithFields(logrus.Fields{
+							"found":    k8sServerVer,
+							"expected": ciliumEPControllerLimit,
+						}).Warn("Cannot run with this k8s version")
+						return nil
+					}
+				}
+				if k8sServerVer == nil || !ciliumEPControllerLimit.Check(k8sServerVer) {
+					return nil // silently return when k8s is incompatible
+				}
 
 				podName := e.GetK8sPodName()
 				if podName == "" {
