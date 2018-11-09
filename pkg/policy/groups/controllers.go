0636068eb00c529eb733111538748145c6130fd7
Eloy Coto <eloy.coto@gmail.com>
2018-11-09 16:12:38 +0100

    ToGroups: Added initial implementation
    This commit adds the initial implementation of the actions that need to
    run when a new policy is added and is received by a Kubernetes watcher.
    
    The main structure is the following:
    
    - log: will only have the log initialiser.
    - actions: the functions that are going to be triggered by the k8sevents
    - helpers: some function that are used by multiple actions or
    controllers.
    - controllers definition of the given actions.
    - Cache: is the number of policies that are now CNP that has any child
    rule
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

---
+++ pkg/policy/groups/controllers.go
@@ -0,0 +1,44 @@
+// Copyright 2017-2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package groups
+
+import (
+	"time"
+
+	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+)
+
+const (
+	maxConcurrentUpdates = 4
+)
+
+func init() {
+	go UpgradeCNPInformation()
+}
+
+func UpgradeCNPInformation() {
+	for {
+		cnpToUpdate := ToGroupsCNPCache.GetAllCNP()
+		sem := make(chan bool, maxConcurrentUpdates)
+		for _, cnp := range cnpToUpdate {
+			sem <- true
+			go func(cnp *cilium_v2.CiliumNetworkPolicy) {
+				defer func() { <-sem }()
+				addDerivativeCNP(cnp)
+			}(cnp)
+		}
+		time.Sleep(10 * time.Second)
+	}
+}
