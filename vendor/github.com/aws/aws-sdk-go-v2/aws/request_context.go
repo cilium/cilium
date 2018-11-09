b5d757138d212734be597b8178171c8ae3eb4e79
Eloy Coto <eloy.coto@gmail.com>
2018-11-09 17:30:32 +0100

    Deps: Update AWS dependencies
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

---
+++ vendor/github.com/aws/aws-sdk-go-v2/aws/request_context.go
@@ -0,0 +1,12 @@
+// +build go1.7
+
+package aws
+
+// setContext updates the Request to use the passed in context for cancellation.
+// Context will also be used for request retry delay.
+//
+// Creates shallow copy of the http.Request with the WithContext method.
+func setRequestContext(r *Request, ctx Context) {
+	r.context = ctx
+	r.HTTPRequest = r.HTTPRequest.WithContext(ctx)
+}
