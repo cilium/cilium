74b384d17589f7e93b35adf01a00d73f24ff9872
Eloy Coto <eloy.coto@gmail.com>
2018-11-12 09:32:16 +0100

    ToGroups: Add controller in operator CNP events.
    Add a controller in operator to update IPs information after five
    minutes to upgrade AWS ips.
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- operator/cnp_event.go
+++ operator/cnp_event.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
@@ -79,6 +80,15 @@ func enableCNPWatcher() error {
 	ciliumV2Controller := si.Cilium().V2().CiliumNetworkPolicies().Informer()
 	ciliumV2Controller.AddEventHandler(watcher)
 	si.Start(wait.NeverStop)
+
+	controller.NewManager().UpdateController("cnp-to-groups-controller",
+		controller.ControllerParams{
+			DoFunc: func() error {
+				groups.UpdateCNPInformation()
+				return nil
+			},
+			RunInterval: 5 * time.Minute,
+		})
 
 	return nil
 }
