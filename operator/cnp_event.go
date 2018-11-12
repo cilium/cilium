644c9556e5bf88c73c3d275cb53b596cf98d7a41
Eloy Coto <eloy.coto@gmail.com>
2018-11-12 09:06:16 +0100

    Operator: Add CNP watcher.
    To enable toGroups child policies enable watcher on the operator to add
    actions when a new policy is added, deleted or updated.
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

---
+++ operator/cnp_event.go
@@ -0,0 +1,84 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/cilium/cilium/pkg/k8s"
+	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
+	informer "github.com/cilium/cilium/pkg/k8s/client/informers/externalversions"
+	k8sUtils "github.com/cilium/cilium/pkg/k8s/utils"
+	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/policy/groups"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+const (
+	reSyncPeriod = 5 * time.Minute
+)
+
+func enableCNPWatcher() error {
+	restConfig, err := k8s.CreateConfig()
+	if err != nil {
+		return fmt.Errorf("Unable to create rest configuration: %s", err)
+	}
+
+	ciliumNPClient, err := clientset.NewForConfig(restConfig)
+	if err != nil {
+		return fmt.Errorf("Unable to create cilium network policy client: %s", err)
+	}
+
+	watcher := k8sUtils.ResourceEventHandlerFactory(
+		func(i interface{}) func() error {
+			return func() error {
+				cnp := i.(*cilium_v2.CiliumNetworkPolicy)
+				groups.AddDerivativeCNPIfNeeded(cnp)
+				return nil
+			}
+		},
+		func(i interface{}) func() error {
+			return func() error {
+				// The derivative policy will be deleted by the parent but need
+				// to delete the cnp from the pooling.
+				groups.DeleteDerivativeFromCache(i.(*cilium_v2.CiliumNetworkPolicy))
+				return nil
+			}
+		},
+		func(old, new interface{}) func() error {
+			return func() error {
+				newCNP := new.(*cilium_v2.CiliumNetworkPolicy)
+				oldCNP := old.(*cilium_v2.CiliumNetworkPolicy)
+				groups.UpdateDerivativeCNPIfNeeded(newCNP, oldCNP)
+				return nil
+			}
+		},
+		nil,
+		&cilium_v2.CiliumNetworkPolicy{},
+		ciliumNPClient,
+		reSyncPeriod,
+		metrics.EventTSK8s,
+	)
+
+	si := informer.NewSharedInformerFactory(ciliumNPClient, reSyncPeriod)
+	ciliumV2Controller := si.Cilium().V2().CiliumNetworkPolicies().Informer()
+	ciliumV2Controller.AddEventHandler(watcher)
+	si.Start(wait.NeverStop)
+
+	return nil
+}
