ebab50b6edb1d233ca19c4c4afaf406a3ac7f85c
Ian Vernon <ian@cilium.io>
2018-11-14 13:30:11 -0800

    endpoint: move RegenerationContext to separate file
    Signed-off by: Ian Vernon <ian@cilium.io>

--- pkg/endpoint/regenerationcontext.go
+++ pkg/endpoint/regenerationcontext.go
@@ -14,6 +14,13 @@
 
 package endpoint
 
+func (e *ExternalRegenerationMetadata) toRegenerationContext() *regenerationContext {
+	return &regenerationContext{
+		Reason:         e.Reason,
+		ReloadDatapath: e.ReloadDatapath,
+	}
+}
+
 // ExternalRegenerationMetadata contains any information about a regeneration that
 // the endpoint subsystem should be made aware of for a given endpoint.
 type ExternalRegenerationMetadata struct {
@@ -26,9 +33,23 @@ type ExternalRegenerationMetadata struct {
 	ReloadDatapath bool
 }
 
-func (e *ExternalRegenerationMetadata) toRegenerationContext() *regenerationContext {
-	return &regenerationContext{
-		Reason:         e.Reason,
-		ReloadDatapath: e.ReloadDatapath,
-	}
+// RegenerationContext provides context to regenerate() calls to determine
+// the caller, and which specific aspects to regeneration are necessary to
+// update the datapath to implement the new behavior.
+type regenerationContext struct {
+	// Reason provides context to source for the regeneration, which is
+	// used to generate useful log messages.
+	Reason string
+
+	// ReloadDatapath forces the datapath programs to be reloaded. It does
+	// not guarantee recompilation of the programs.
+	ReloadDatapath bool
+
+	// Stats are collected during the endpoint regeneration and provided
+	// back to the caller
+	Stats regenerationStatistics
+
+	// DoneFunc must be called when the most resource intensive portion of
+	// the regeneration is done
+	DoneFunc func()
 }
