8cf6d1b8906a8b48e0f6c6ff3b4c0abb23b22aa0
Martynas Pumputis <martynas@covalent.io>
2018-11-15 18:12:21 +0100

    safetime: add wrapper for time.Since
    The wrapper "TimeSinceSafe" is used to work around the case when CLOCK_MONOTONIC
    returns a non-monotonic time (observed on a burstable EC2 instance), which
    results in "time.Since" returning a negative duration.
    
    Passing a negative value to some Prometheus metrics methods (e.g. "Counter.Add")
    can cause a panic. Thus, for the sake of safety, the wrapper should be used
    instead "time.Since" when calling the methods.
    
    Fixes #6222
    
    Signed-off-by: Martynas Pumputis <martynas@covalent.io>

---
+++ pkg/safetime/safetime.go
@@ -0,0 +1,38 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package safetime
+
+import (
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// TimeSinceSafe returns the duration since t. If the duration is negative,
+// returns false to indicate the fact.
+//
+// Used to workaround a malfunctioning monotonic clock.
+func TimeSinceSafe(t time.Time, logger *log.Entry) (bool, time.Duration) {
+	n := time.Now()
+	d := n.Sub(t)
+
+	if d < 0 {
+		logger.WithField("then", t).WithField("now", n).WithField("duration", d).
+			Warn("BUG: negative duration")
+		return false, time.Duration(0)
+	}
+
+	return true, d
+}
