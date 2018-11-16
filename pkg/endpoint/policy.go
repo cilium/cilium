16c5f60e66194f5cd8d182f2c7bb4650326fd303
Martynas Pumputis <martynas@covalent.io>
2018-11-16 11:10:19 +0100

    endpoint: refactor negative duration check
    Replace "time.Since" with "safetime.TimeSinceSafe" which is guaranteed
    to return a non-negative duration.
    
    Signed-off-by: Martynas Pumputis <martynas@covalent.io>

--- pkg/endpoint/policy.go
+++ pkg/endpoint/policy.go
@@ -43,6 +43,7 @@ import (
 	"github.com/cilium/cilium/pkg/policy/api"
 	"github.com/cilium/cilium/pkg/policy/trafficdirection"
 	"github.com/cilium/cilium/pkg/revert"
+	"github.com/cilium/cilium/pkg/safetime"
 	"github.com/cilium/cilium/pkg/uuid"
 
 	"github.com/sirupsen/logrus"
@@ -573,24 +574,19 @@ func (e *Endpoint) regeneratePolicy(owner Owner) error {
 	// the regeneration of the endpoint to complete.
 	policyChanged := l3PolicyChanged || l4PolicyChanged
 
-	totalRegeneration := time.Since(regenerateStart)
+	logger := e.getLogger().WithFields(logrus.Fields{
+		"policyChanged":      policyChanged,
+		"forcedRegeneration": forceRegeneration,
+	})
 
-	e.getLogger().WithFields(logrus.Fields{
-		"policyChanged":                  policyChanged,
-		"forcedRegeneration":             forceRegeneration,
-		logfields.PolicyRegenerationTime: totalRegeneration.String(),
-	}).Info("Completed endpoint policy recalculation")
+	ok, totalRegeneration := safetime.TimeSinceSafe(regenerateStart, logger)
 
-	regenerateTimeSec := totalRegeneration.Seconds()
+	logger.WithField(logfields.PolicyRegenerationTime, totalRegeneration.String()).
+		Info("Completed endpoint policy recalculation")
+
 	metrics.PolicyRegenerationCount.Inc()
-	if regenerateTimeSec < 0 {
-		e.getLogger().WithFields(logrus.Fields{
-			"policyChanged":                  policyChanged,
-			"forcedRegeneration":             forceRegeneration,
-			logfields.PolicyRegenerationTime: totalRegeneration.String(),
-			"regenerateTimeSec":              regenerateTimeSec,
-		}).Warn("BUG: regenerateTimeSec is less than 0")
-	} else {
+	if ok {
+		regenerateTimeSec := totalRegeneration.Seconds()
 		metrics.PolicyRegenerationTime.Add(regenerateTimeSec)
 		metrics.PolicyRegenerationTimeSquare.Add(math.Pow(regenerateTimeSec, 2))
 	}
