0a996185c905e20eb1b95430c6471168d58b8a2c
André Martins <andre@cilium.io>
2018-11-20 21:19:22 +0100

    plugins/cni: add installation script for policy enforcement mode
    When POLICY_ENFORCEMENT_INTERFACE is set, the cni configuration that
    chains flannel with Cilium will be installed and uninstalled.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- plugins/cilium-cni/cni-uninstall.sh
+++ plugins/cilium-cni/cni-uninstall.sh
@@ -3,7 +3,11 @@
 set -eu
 
 HOST_PREFIX=${HOST_PREFIX:-/host}
-CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conf}
+if [ -z "${CILIUM_POLICY_ENFORCEMENT_INTERFACE}" ]; then
+    CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conf}
+else
+    CNI_CONF_NAME=${CNI_CONF_NAME:-04-cilium.conflist}
+fi
 
 BIN_NAME=cilium-cni
 CNI_DIR=${CNI_DIR:-${HOST_PREFIX}/opt/cni}
@@ -15,3 +19,10 @@ rm -f ${CNI_DIR}/bin/${BIN_NAME}.old
 
 echo "Removing ${CILIUM_CNI_CONF} ..."
 rm -f ${CILIUM_CNI_CONF}
+
+if [ -z "${CILIUM_POLICY_ENFORCEMENT_CLEAN_UP}" ]; then
+    echo "Removing BPF programs from all containers and from ${CILIUM_POLICY_ENFORCEMENT_INTERFACE}"
+    tc filter delete dev cni0 egress pref 1 handle 1 bpf
+    # TODO create script to detect all interfaces that have bpf programs
+    # installed
+fi
