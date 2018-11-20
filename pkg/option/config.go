62aca0a0b02c7be09721aa0581f50f21a327302d
André Martins <andre@cilium.io>
2018-11-20 19:07:55 +0100

    clean bpf programs when running with policy enforcement mode
    There will be 2 main paths when a user will deploy Cilium on top of
    other CNI plugins.
    
    a) The user will install a CNI plugin and will deploy Cilium on top, for
    each it will not set the flag `policy-enforcement-clean-up`. This user
    has the intention of running Cilium as long as the cluster lives.
    
    b) The user as a cluster with a CNI plugin already installed. The user
    wants to test out Cilium to see the security advantages it could give.
    For this time of use case, `policy-enforcement-clean-up` should be set
    as true which will leave the cluster as it was before installing Cilium,
    meaning all policy enforcement will no longer occur once Cilium is
    uninstalled.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -218,6 +218,10 @@ const (
 	// flannel, this value should be set with 'cni0'. [EXPERIMENTAL]")
 	PolicyEnforcementInterface = "policy-enforcement-interface"
 
+	// PolicyEnforcementCleanUP should be used along the policy-enforcement-interface flag,
+	// it cleans up all BPF programs installed when Cilium agent is terminated.
+	PolicyEnforcementCleanUP = "policy-enforcement-clean-up"
+
 	// PProf enables serving the pprof debugging API
 	PProf = "pprof"
 
@@ -846,6 +850,7 @@ func (c *DaemonConfig) Populate() {
 	c.MTU = viper.GetInt(MTUName)
 	c.NAT46Range = viper.GetString(NAT46Range)
 	c.PolicyEnforcementInterface = viper.GetString(PolicyEnforcementInterface)
+	c.PolicyEnforcementCleanUp = viper.GetBool(PolicyEnforcementCleanUP)
 	c.PProf = viper.GetBool(PProf)
 	c.PrependIptablesChains = viper.GetBool(PrependIptablesChainsName)
 	c.PrometheusServeAddr = getPrometheusServerAddr()
