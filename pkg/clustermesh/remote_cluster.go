e81be85e58c831ac8666562639b7606277c0ce3f
Thomas Graf <thomas@cilium.io>
2018-11-26 14:06:00 +0100

    clustermesh: Wait for controllers to be shutdown when closing
    Don't defer shutting down of controllers until after the Close() function has
    returned to avoid having parallel watchers process kvstore events in parallel.
    
    This was mainly a problem for unit tests as in practice, the clustermesh is
    only brought up and down once in the agent lifecycle.
    
    Fixes: 076b0188b98d ("Inter cluster connectivity (ClusterMesh)")
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/clustermesh/remote_cluster.go
+++ pkg/clustermesh/remote_cluster.go
@@ -194,7 +194,7 @@ func (rc *remoteCluster) onInsert() {
 }
 
 func (rc *remoteCluster) onRemove() {
-	rc.controllers.RemoveAll()
+	rc.controllers.RemoveAllAndWait()
 	close(rc.changed)
 
 	rc.getLogger().Info("Remote cluster disconnected")
