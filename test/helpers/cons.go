92302ca5b2b0dc22c868cedb2f2018c2655c247e
André Martins <andre@cilium.io>
2018-11-26 15:30:57 +0100

    tests: add version constraints for k8s 1.13 and cilium 1.3
    Signed-off-by: André Martins <andre@cilium.io>

--- test/helpers/cons.go
+++ test/helpers/cons.go
@@ -220,7 +220,8 @@ var (
 	CiliumV1_0 = versioncheck.MustCompile(">=v1.0,<v1.1")
 	CiliumV1_1 = versioncheck.MustCompile(">=v1.1,<v1.2")
 	CiliumV1_2 = versioncheck.MustCompile(">=v1.2,<v1.3")
-	CiliumV1_3 = versioncheck.MustCompile(">=v1.2,<v1.4")
+	CiliumV1_3 = versioncheck.MustCompile(">=v1.2.90,<v1.4")
+	CiliumV1_4 = versioncheck.MustCompile(">=v1.3.90,<v1.5")
 )
 
 // CiliumDefaultDSPatch is the default Cilium DaemonSet patch to be used in all tests.
