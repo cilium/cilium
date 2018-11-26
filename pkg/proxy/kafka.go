544d0a30c62b5a77ff13be9cbfb5a8d89e7ce8c1
Ray Bejjani <ray@covalent.io>
2018-11-26 13:26:30 +0100

    proxy: Add RedirectImplementation.UpdateRules
    Envoy maintains the desired state  in the xDS cache, and kafka uses the
    rules in the redirect object directly during processing. The DNS proxy
    needs an event when rules are updated, and UpdateRules is the callback
    for that. It is a no-op in this commit to simplify it.
    
    Signed-off-by: Ray Bejjani <ray@covalent.io>

--- pkg/proxy/kafka.go
+++ pkg/proxy/kafka.go
@@ -441,6 +441,12 @@ func (k *kafkaRedirect) handleResponseConnection(pair *connectionPair, correlati
 		}, remoteAddr, remoteIdentity, origDstAddr)
 }
 
+// UpdateRules is a no-op for kafka redirects, as rules are read directly
+// during request processing.
+func (k *kafkaRedirect) UpdateRules(wg *completion.WaitGroup, l4 *policy.L4Filter) (error, revert.RevertFunc) {
+	return nil, func() error { return nil }
+}
+
 // Close the redirect.
 func (k *kafkaRedirect) Close(wg *completion.WaitGroup) (revert.FinalizeFunc, revert.RevertFunc) {
 	return k.socket.Close, nil
