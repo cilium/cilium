3c010e8327a9d282538210f22b6180b25b695906
Thomas Graf <thomas@cilium.io>
2018-11-27 14:03:18 +0100

    cni: Synchroneous pod label retrieval on CNI add
    [ upstream commit 65fe98c4f391b82e47c47f63e556d9a835cee99c ]
    
    Leverage the CNI_ARGS to retrieve the pod name and namespace and issue a call
    to the apiserver to retrieve the pod labels. This allows to retrieve the pod
    labels and allocate the identity synchronously on the endpoint creation API
    call.
    
    This makes the CNI endpoint creation code divert from the CMM plugin. The CMM
    plugin continues to rely on the workloads API to retrieve the metadata.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- pkg/endpoint/endpoint.go
+++ pkg/endpoint/endpoint.go
@@ -702,6 +702,8 @@ func NewEndpointFromChangeModel(base *models.EndpointChangeRequest) (*Endpoint, 
 		DockerNetworkID:  base.DockerNetworkID,
 		DockerEndpointID: base.DockerEndpointID,
 		IfName:           base.InterfaceName,
+		k8sPodName:       base.K8sPodName,
+		k8sNamespace:     base.K8sNamespace,
 		IfIndex:          int(base.InterfaceIndex),
 		OpLabels: pkgLabels.OpLabels{
 			Custom:                pkgLabels.Labels{},
@@ -712,6 +714,7 @@ func NewEndpointFromChangeModel(base *models.EndpointChangeRequest) (*Endpoint, 
 		state:  "",
 		Status: NewEndpointStatus(),
 	}
+
 	ep.UpdateLogger(nil)
 
 	ep.SetStateLocked(string(base.State), "Endpoint creation")
