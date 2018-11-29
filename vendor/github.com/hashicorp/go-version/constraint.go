cilium/cilium

commit 24ed166d72a0869fdd882f197d61d5b45c29f873

vendor: update hashicorp/go-version to latest stable
Signed-off-by: André Martins <andre@cilium.io>

Gopkg.toml
@@ -229,7 +229,7 @@ required = [
 
 [[constraint]]
   name = "github.com/hashicorp/go-version"
-  revision = "03c5bf6be031b6dd45afec16b1cf94fc8938bc77"
+  revision = "v1.0.0"
 
   # main-usage = "multiple packages"
   # on-revision = ""

vendor/github.com/hashicorp/go-version/constraint.go
@@ -2,6 +2,7 @@ package version
 
 import (
 	"fmt"
+	"reflect"
 	"regexp"
 	"strings"
 )
@@ -113,6 +114,26 @@ func parseSingle(v string) (*Constraint, error) {
 	}, nil
 }
 
+func prereleaseCheck(v, c *Version) bool {
+	switch vPre, cPre := v.Prerelease() != "", c.Prerelease() != ""; {
+	case cPre && vPre:
+		// A constraint with a pre-release can only match a pre-release version
+		// with the same base segments.
+		return reflect.DeepEqual(c.Segments64(), v.Segments64())
+
+	case !cPre && vPre:
+		// A constraint without a pre-release can only match a version without a
+		// pre-release.
+		return false
+
+	case cPre && !vPre:
+		// OK, except with the pessimistic operator
+	case !cPre && !vPre:
+		// OK
+	}
+	return true
+}
+
 //-------------------------------------------------------------------
 // Constraint functions
 //-------------------------------------------------------------------
@@ -126,22 +147,27 @@ func constraintNotEqual(v, c *Version) bool {
 }
 
 func constraintGreaterThan(v, c *Version) bool {
-	return v.Compare(c) == 1
+	return prereleaseCheck(v, c) && v.Compare(c) == 1
 }
 
 func constraintLessThan(v, c *Version) bool {
-	return v.Compare(c) == -1
+	return prereleaseCheck(v, c) && v.Compare(c) == -1
 }
 
 func constraintGreaterThanEqual(v, c *Version) bool {
-	return v.Compare(c) >= 0
+	return prereleaseCheck(v, c) && v.Compare(c) >= 0
 }
 
 func constraintLessThanEqual(v, c *Version) bool {
-	return v.Compare(c) <= 0
+	return prereleaseCheck(v, c) && v.Compare(c) <= 0
 }
 
 func constraintPessimistic(v, c *Version) bool {
+	// Using a pessimistic constraint with a pre-release, restricts versions to pre-releases
+	if !prereleaseCheck(v, c) || (c.Prerelease() != "" && v.Prerelease() == "") {
+		return false
+	}
+
 	// If the version being checked is naturally less than the constraint, then there
 	// is no way for the version to be valid against the constraint
 	if v.LessThan(c) {

vendor/github.com/hashicorp/go-version/version.go
@@ -15,8 +15,8 @@ var versionRegexp *regexp.Regexp
 // The raw regular expression string used for testing the validity
 // of a version.
 const VersionRegexpRaw string = `v?([0-9]+(\.[0-9]+)*?)` +
-	`(-?([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?` +
-	`(\+([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?` +
+	`(-([0-9]+[0-9A-Za-z\-~]*(\.[0-9A-Za-z\-~]+)*)|(-?([A-Za-z\-~]+[0-9A-Za-z\-~]*(\.[0-9A-Za-z\-~]+)*)))?` +
+	`(\+([0-9A-Za-z\-~]+(\.[0-9A-Za-z\-~]+)*))?` +
 	`?`
 
 // Version represents a single version.
@@ -25,6 +25,7 @@ type Version struct {
 	pre      string
 	segments []int64
 	si       int
+	original string
 }
 
 func init() {
@@ -59,11 +60,17 @@ func NewVersion(v string) (*Version, error) {
 		segments = append(segments, 0)
 	}
 
+	pre := matches[7]
+	if pre == "" {
+		pre = matches[4]
+	}
+
 	return &Version{
-		metadata: matches[7],
-		pre:      matches[4],
+		metadata: matches[10],
+		pre:      pre,
 		segments: segments,
 		si:       si,
+		original: v,
 	}, nil
 }
 
@@ -166,14 +173,16 @@ func comparePart(preSelf string, preOther string) int {
 		return 0
 	}
 
+	var selfInt int64
 	selfNumeric := true
-	_, err := strconv.ParseInt(preSelf, 10, 64)
+	selfInt, err := strconv.ParseInt(preSelf, 10, 64)
 	if err != nil {
 		selfNumeric = false
 	}
 
+	var otherInt int64
 	otherNumeric := true
-	_, err = strconv.ParseInt(preOther, 10, 64)
+	otherInt, err = strconv.ParseInt(preOther, 10, 64)
 	if err != nil {
 		otherNumeric = false
 	}
@@ -197,7 +206,9 @@ func comparePart(preSelf string, preOther string) int {
 		return -1
 	} else if !selfNumeric && otherNumeric {
 		return 1
-	} else if preSelf > preOther {
+	} else if !selfNumeric && !otherNumeric && preSelf > preOther {
+		return 1
+	} else if selfInt > otherInt {
 		return 1
 	}
 
@@ -297,11 +308,19 @@ func (v *Version) Segments() []int {
 // for a version "1.2.3-beta", segments will return a slice of
 // 1, 2, 3.
 func (v *Version) Segments64() []int64 {
-	return v.segments
+	result := make([]int64, len(v.segments))
+	copy(result, v.segments)
+	return result
 }
 
 // String returns the full version string included pre-release
 // and metadata information.
+//
+// This value is rebuilt according to the parsed segments and other
+// information. Therefore, ambiguities in the version string such as
+// prefixed zeroes (1.04.0 => 1.4.0), `v` prefix (v1.0.0 => 1.0.0), and
+// missing parts (1.0 => 1.0.0) will be made into a canonicalized form
+// as shown in the parenthesized examples.
 func (v *Version) String() string {
 	var buf bytes.Buffer
 	fmtParts := make([]string, len(v.segments))
@@ -319,4 +338,10 @@ func (v *Version) String() string {
 	}
 
 	return buf.String()
+}
+
+// Original returns the original parsed version as-is, including any
+// potential whitespace, `v` prefix, etc.
+func (v *Version) Original() string {
+	return v.original
 }

commit 7a4a8b67ff8289b2aca4ce6fbd0211b2f2e608df

vendor: downgrade hashicorp/go-version
As hashicorp/go-version introduced an unwanted behavior in the
go-version library we marked the revision number to be the previous
revision where the unwanted commit was introduced.

https://github.com/hashicorp/go-version/commit/fbe76cfa08cd0fa2695a1e8feea4687221ed2b32

Fixes: 24ed166d72a0 ("vendor: update hashicorp/go-version to latest stable"
Signed-off-by: André Martins <andre@cilium.io>

Gopkg.toml
@@ -229,10 +229,12 @@ required = [
 
 [[constraint]]
   name = "github.com/hashicorp/go-version"
-  revision = "v1.0.0"
+  revision = "4fe82ae3040f80a03d04d2cccb5606a626b8e1ee"
 
   # main-usage = "multiple packages"
-  # on-revision = ""
+  # on-revision = "this revision is the parent revision of the commit removes"
+  #               "comparision between constrains that are not pre-releases and
+  #               "versions that are pre-releases"
 
 [[constraint]]
   name = "github.com/jessevdk/go-flags"

vendor/github.com/hashicorp/consul/website/source/docs/commands/license.html.markdown.erb
@@ -71,12 +71,12 @@ Expires At: 2019-05-22 03:59:59.999 +0000 UTC
 Datacenter: *
 Package: premium
 Licensed Features:
-	Automated Backups
-	Automated Upgrades
-	Enhanced Read Scalability
-	Network Segments
-	Redundancy Zone
-	Advanced Network Federation
+        Automated Backups
+        Automated Upgrades
+        Enhanced Read Scalability
+        Network Segments
+        Redundancy Zone
+        Advanced Network Federation
 ```
 
 ## get
@@ -100,10 +100,10 @@ Expires At: 2019-05-22 03:59:59.999 +0000 UTC
 Datacenter: *
 Package: premium
 Licensed Features:
-	Automated Backups
-	Automated Upgrades
-	Enhanced Read Scalability
-	Network Segments
-	Redundancy Zone
-	Advanced Network Federation
+        Automated Backups
+        Automated Upgrades
+        Enhanced Read Scalability
+        Network Segments
+        Redundancy Zone
+        Advanced Network Federation
 ```

vendor/github.com/hashicorp/go-rootcerts/LICENSE
@@ -27,11 +27,11 @@ Mozilla Public License, version 2.0
      means
 
      a. that the initial Contributor has attached the notice described in
-	Exhibit B to the Covered Software; or
+        Exhibit B to the Covered Software; or
 
      b. that the Covered Software was made available under the terms of
-	version 1.1 or earlier of the License, but not also under the terms of
-	a Secondary License.
+        version 1.1 or earlier of the License, but not also under the terms of
+        a Secondary License.
 
 1.6. "Executable Form"
 
@@ -57,7 +57,7 @@ Mozilla Public License, version 2.0
      means any of the following:
 
      a. any file in Source Code Form that results from an addition to,
-	deletion from, or modification of the contents of Covered Software; or
+        deletion from, or modification of the contents of Covered Software; or
 
      b. any new file in Source Code Form that contains any Covered Software.
 
@@ -98,14 +98,14 @@ Mozilla Public License, version 2.0
      non-exclusive license:
 
      a. under intellectual property rights (other than patent or trademark)
-	Licensable by such Contributor to use, reproduce, make available,
-	modify, display, perform, distribute, and otherwise exploit its
-	Contributions, either on an unmodified basis, with Modifications, or
-	as part of a Larger Work; and
+        Licensable by such Contributor to use, reproduce, make available,
+        modify, display, perform, distribute, and otherwise exploit its
+        Contributions, either on an unmodified basis, with Modifications, or
+        as part of a Larger Work; and
 
      b. under Patent Claims of such Contributor to make, use, sell, offer for
-	sale, have made, import, and otherwise transfer either its
-	Contributions or its Contributor Version.
+        sale, have made, import, and otherwise transfer either its
+        Contributions or its Contributor Version.
 
 2.2. Effective Date
 
@@ -124,12 +124,12 @@ Mozilla Public License, version 2.0
      a. for any code that a Contributor has removed from Covered Software; or
 
      b. for infringements caused by: (i) Your and any other third party's
-	modifications of Covered Software, or (ii) the combination of its
-	Contributions with other software (except as part of its Contributor
-	Version); or
+        modifications of Covered Software, or (ii) the combination of its
+        Contributions with other software (except as part of its Contributor
+        Version); or
 
      c. under Patent Claims infringed by Covered Software in the absence of
-	its Contributions.
+        its Contributions.
 
      This License does not grant any rights in the trademarks, service marks,
      or logos of any Contributor (except as may be necessary to comply with
@@ -177,15 +177,15 @@ Mozilla Public License, version 2.0
      If You distribute Covered Software in Executable Form then:
 
      a. such Covered Software must also be made available in Source Code Form,
-	as described in Section 3.1, and You must inform recipients of the
-	Executable Form how they can obtain a copy of such Source Code Form by
-	reasonable means in a timely manner, at a charge no more than the cost
-	of distribution to the recipient; and
+        as described in Section 3.1, and You must inform recipients of the
+        Executable Form how they can obtain a copy of such Source Code Form by
+        reasonable means in a timely manner, at a charge no more than the cost
+        of distribution to the recipient; and
 
      b. You may distribute such Executable Form under the terms of this
-	License, or sublicense it under different terms, provided that the
-	license for the Executable Form does not attempt to limit or alter the
-	recipients' rights in the Source Code Form under this License.
+        License, or sublicense it under different terms, provided that the
+        license for the Executable Form does not attempt to limit or alter the
+        recipients' rights in the Source Code Form under this License.
 
 3.3. Distribution of a Larger Work
 
@@ -360,3 +360,4 @@ Exhibit B - "Incompatible With Secondary Licenses" Notice
       This Source Code Form is "Incompatible
       With Secondary Licenses", as defined by
       the Mozilla Public License, v. 2.0.
+

vendor/github.com/hashicorp/go-version/constraint.go
@@ -2,7 +2,6 @@ package version
 
 import (
 	"fmt"
-	"reflect"
 	"regexp"
 	"strings"
 )
@@ -114,26 +113,6 @@ func parseSingle(v string) (*Constraint, error) {
 	}, nil
 }
 
-func prereleaseCheck(v, c *Version) bool {
-	switch vPre, cPre := v.Prerelease() != "", c.Prerelease() != ""; {
-	case cPre && vPre:
-		// A constraint with a pre-release can only match a pre-release version
-		// with the same base segments.
-		return reflect.DeepEqual(c.Segments64(), v.Segments64())
-
-	case !cPre && vPre:
-		// A constraint without a pre-release can only match a version without a
-		// pre-release.
-		return false
-
-	case cPre && !vPre:
-		// OK, except with the pessimistic operator
-	case !cPre && !vPre:
-		// OK
-	}
-	return true
-}
-
 //-------------------------------------------------------------------
 // Constraint functions
 //-------------------------------------------------------------------
@@ -147,27 +126,22 @@ func constraintNotEqual(v, c *Version) bool {
 }
 
 func constraintGreaterThan(v, c *Version) bool {
-	return prereleaseCheck(v, c) && v.Compare(c) == 1
+	return v.Compare(c) == 1
 }
 
 func constraintLessThan(v, c *Version) bool {
-	return prereleaseCheck(v, c) && v.Compare(c) == -1
+	return v.Compare(c) == -1
 }
 
 func constraintGreaterThanEqual(v, c *Version) bool {
-	return prereleaseCheck(v, c) && v.Compare(c) >= 0
+	return v.Compare(c) >= 0
 }
 
 func constraintLessThanEqual(v, c *Version) bool {
-	return prereleaseCheck(v, c) && v.Compare(c) <= 0
+	return v.Compare(c) <= 0
 }
 
 func constraintPessimistic(v, c *Version) bool {
-	// Using a pessimistic constraint with a pre-release, restricts versions to pre-releases
-	if !prereleaseCheck(v, c) || (c.Prerelease() != "" && v.Prerelease() == "") {
-		return false
-	}
-
 	// If the version being checked is naturally less than the constraint, then there
 	// is no way for the version to be valid against the constraint
 	if v.LessThan(c) {

vendor/github.com/hashicorp/go-version/version.go
@@ -15,7 +15,7 @@ var versionRegexp *regexp.Regexp
 // The raw regular expression string used for testing the validity
 // of a version.
 const VersionRegexpRaw string = `v?([0-9]+(\.[0-9]+)*?)` +
-	`(-([0-9]+[0-9A-Za-z\-~]*(\.[0-9A-Za-z\-~]+)*)|(-?([A-Za-z\-~]+[0-9A-Za-z\-~]*(\.[0-9A-Za-z\-~]+)*)))?` +
+	`(-?([0-9A-Za-z\-~]+(\.[0-9A-Za-z\-~]+)*))?` +
 	`(\+([0-9A-Za-z\-~]+(\.[0-9A-Za-z\-~]+)*))?` +
 	`?`
 
@@ -25,7 +25,6 @@ type Version struct {
 	pre      string
 	segments []int64
 	si       int
-	original string
 }
 
 func init() {
@@ -60,17 +59,11 @@ func NewVersion(v string) (*Version, error) {
 		segments = append(segments, 0)
 	}
 
-	pre := matches[7]
-	if pre == "" {
-		pre = matches[4]
-	}
-
 	return &Version{
-		metadata: matches[10],
-		pre:      pre,
+		metadata: matches[7],
+		pre:      matches[4],
 		segments: segments,
 		si:       si,
-		original: v,
 	}, nil
 }
 
@@ -308,19 +301,11 @@ func (v *Version) Segments() []int {
 // for a version "1.2.3-beta", segments will return a slice of
 // 1, 2, 3.
 func (v *Version) Segments64() []int64 {
-	result := make([]int64, len(v.segments))
-	copy(result, v.segments)
-	return result
+	return v.segments
 }
 
 // String returns the full version string included pre-release
 // and metadata information.
-//
-// This value is rebuilt according to the parsed segments and other
-// information. Therefore, ambiguities in the version string such as
-// prefixed zeroes (1.04.0 => 1.4.0), `v` prefix (v1.0.0 => 1.0.0), and
-// missing parts (1.0 => 1.0.0) will be made into a canonicalized form
-// as shown in the parenthesized examples.
 func (v *Version) String() string {
 	var buf bytes.Buffer
 	fmtParts := make([]string, len(v.segments))
@@ -338,10 +323,4 @@ func (v *Version) String() string {
 	}
 
 	return buf.String()
-}
-
-// Original returns the original parsed version as-is, including any
-// potential whitespace, `v` prefix, etc.
-func (v *Version) Original() string {
-	return v.original
 }