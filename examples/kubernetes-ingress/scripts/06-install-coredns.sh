d10b3ccaa445ea254d1f04c755bf140399477f5a
André Martins <andre@cilium.io>
2018-11-29 12:29:30 +0100

    examples/kubernetes-ingress: re-install kube-dns pods without nohup
    It seems nohup was not executing all commands, this fix might prevent
    the bug from happening again.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- examples/kubernetes-ingress/scripts/06-install-coredns.sh
+++ examples/kubernetes-ingress/scripts/06-install-coredns.sh
@@ -23,13 +23,13 @@ sed "s/\$DNS_SERVER_IP/${cluster_dns_ip}/" "${svc_file}.sed" > "${svc_file}"
 sed -e "s/\$DNS_DOMAIN/cluster.local/" \
     "${cm_file}.sed" > "${cm_file}"
 
-nohup kubectl delete -f "${controller_file}" 2>/dev/null || true && \
-kubectl delete -f "${svc_file}" 2>/dev/null || true && \
-kubectl delete -f "${sa_file}" 2>/dev/null || true && \
-kubectl delete -f "${cm_file}" 2>/dev/null || true && \
-kubectl create -f "${cm_file}" || true && \
-kubectl create -f "${sa_file}" || true && \
-kubectl create -f "${svc_file}" || true && \
+kubectl delete --grace-period=5 -f "${controller_file}" 2>/dev/null || true
+kubectl delete --grace-period=5 -f "${svc_file}" 2>/dev/null || true
+kubectl delete --grace-period=5 -f "${sa_file}" 2>/dev/null || true
+kubectl delete --grace-period=5 -f "${cm_file}" 2>/dev/null || true
+kubectl create -f "${cm_file}" || true
+kubectl create -f "${sa_file}" || true
+kubectl create -f "${svc_file}" || true
 kubectl create -f "${controller_file}" || true &
 
 kubectl --namespace=kube-system get svc
