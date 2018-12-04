a0eab1fbbb5b64d5edd7490a58674088c600a597
Thomas Graf <thomas@cilium.io>
2018-12-04 14:00:11 -0800

    k8s: Provide ServiceCache contents via /debuginfo
    The library used to pretty print the structure has already been vendored via
    another dependency so it doesn't add to the binary size.
    
    Example:
    ```
    (*k8s.ServiceCache)(0xc00098ed30)({
     mutex: (lock.RWMutex) {
      internalRWMutex: (lock.internalRWMutex) {
       RWMutex: (sync.RWMutex) {
        w: (sync.Mutex) {
         state: (int32) 0,
         sema: (uint32) 0
        },
        writerSem: (uint32) 0,
        readerSem: (uint32) 0,
        readerCount: (int32) 1,
        readerWait: (int32) 0
       }
      }
     },
     services: (map[k8s.ServiceID]*k8s.Service) (len=3) {
      (k8s.ServiceID) default/echo: (*k8s.Service)(0xc002e1ed80)(frontend:172.20.0.214/ports=[]/selector=map[name:echo]),
      (k8s.ServiceID) default/kubernetes: (*k8s.Service)(0xc002e1ee00)(frontend:172.20.0.1/ports=[https]/selector=map[]),
      (k8s.ServiceID) kube-system/kube-dns: (*k8s.Service)(0xc002e1ee40)(frontend:172.20.0.10/ports=[dns dns-tcp]/selector=map[k8s-app:kube-dns])
     },
     endpoints: (map[k8s.ServiceID]*k8s.Endpoints) (len=5) {
      (k8s.ServiceID) default/echo: (*k8s.Endpoints)(0xc002ed9480)(10.16.105.205:80/TCP,10.16.150.104:80/TCP,10.16.179.87:80/TCP,10.16.198.212:80/TCP,10.16.223.106:80/TCP),
      (k8s.ServiceID) default/kubernetes: (*k8s.Endpoints)(0xc002ed9490)(192.168.33.11:6443/TCP),
      (k8s.ServiceID) kube-system/kube-controller-manager: (*k8s.Endpoints)(0xc002ed94a0)(),
      (k8s.ServiceID) kube-system/kube-dns: (*k8s.Endpoints)(0xc002ed9460)(10.16.226.224:53/TCP,10.16.226.224:53/UDP),
      (k8s.ServiceID) kube-system/kube-scheduler: (*k8s.Endpoints)(0xc002ed9470)()
     },
     ingresses: (map[k8s.ServiceID]*k8s.Service) {
     },
     externalEndpoints: (map[k8s.ServiceID]k8s.externalEndpoints) {
     },
     Events: (chan k8s.ServiceEvent) (cap=128) 0xc0002b0960
    })
    ```
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- daemon/debuginfo.go
+++ daemon/debuginfo.go
@@ -22,6 +22,7 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	restapi "github.com/cilium/cilium/api/v1/server/restapi/daemon"
 	"github.com/cilium/cilium/api/v1/server/restapi/endpoint"
+	"github.com/cilium/cilium/pkg/debug"
 	"github.com/cilium/cilium/pkg/version"
 
 	"github.com/go-openapi/runtime/middleware"
@@ -55,7 +56,7 @@ func (h *getDebugInfo) Handle(params restapi.GetDebuginfoParams) middleware.Resp
 
 	dr.EndpointList = getEndpointList(p)
 	dr.Policy = d.policy.GetRulesList()
-
+	dr.Subsystem = debug.CollectSubsystemStatus()
 	dr.CiliumMemoryMap = memoryMap(os.Getpid())
 
 	if d.nodeMonitor.State() != nil {
