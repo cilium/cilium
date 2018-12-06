22fab37c44ea8acdf846a0e571c9a9e78978ae73
André Martins <andre@cilium.io>
2018-12-06 16:49:55 +0100

    test: do not run cilium health precheck in cni integration mode
    Signed-off-by: André Martins <andre@cilium.io>

--- test/helpers/kubectl.go
+++ test/helpers/kubectl.go
@@ -1748,9 +1748,14 @@ func (kub *Kubectl) CiliumPreFlightCheck() error {
 		if err != nil {
 			return false
 		}
-		err = kub.CiliumHealthPreFlightCheck()
-		if err != nil {
-			return false
+
+		switch GetCurrentIntegration() {
+		case CIIntegrationFlannel:
+		default:
+			err = kub.CiliumHealthPreFlightCheck()
+			if err != nil {
+				return false
+			}
 		}
 		err = kub.fillServiceCache()
 		if err != nil {
