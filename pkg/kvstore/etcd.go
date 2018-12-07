a9a3019514498cc02afa022cbeb9d461400827c4
Thomas Graf <thomas@cilium.io>
2018-12-06 17:36:26 -0800

    etcd: Remove useless sync.Once around status checker
    The sync.Once variable is allocated inside of the same function so does nothing
    and is only confusing.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/kvstore/etcd.go
+++ pkg/kvstore/etcd.go
@@ -17,7 +17,6 @@ package kvstore
 import (
 	"fmt"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/cilium/cilium/pkg/controller"
@@ -150,9 +149,8 @@ type etcdClient struct {
 	// is set up in the etcd Client.
 	firstSession chan struct{}
 
-	client               *client.Client
-	controllers          *controller.Manager
-	statusCheckerStarted sync.Once
+	client      *client.Client
+	controllers *controller.Manager
 
 	// config and configPath are initialized once and never written to again, they can be accessed without locking
 	config     *client.Config
@@ -298,10 +296,7 @@ func newEtcdClient(config *client.Config, cfgPath string) (BackendOperations, er
 		close(ec.firstSession)
 	}()
 
-	ec.statusCheckerStarted.Do(func() {
-		go ec.statusChecker()
-	})
-
+	go ec.statusChecker()
 	go ec.checkMinVersion()
 
 	ec.controllers.UpdateController("kvstore-etcd-session-renew",
