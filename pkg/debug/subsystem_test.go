8152d02f1adff21ba72971494b75adb340bc68dc
Michal Rostecki <mrostecki@suse.de>
2018-12-13 14:29:16 +0100

    debug: Use checker.DeepEquals in unit tests
    Signed-off-by: Michal Rostecki <mrostecki@suse.de>

--- pkg/debug/subsystem_test.go
+++ pkg/debug/subsystem_test.go
@@ -19,6 +19,8 @@ package debug
 import (
 	"testing"
 
+	"github.com/cilium/cilium/pkg/checker"
+
 	. "gopkg.in/check.v1"
 )
 
@@ -39,28 +41,28 @@ func (d *debugObj) DebugStatus() string {
 
 func (s *DebugTestSuite) TestSubsystem(c *C) {
 	sf := newStatusFunctions()
-	c.Assert(sf.collectStatus(), DeepEquals, StatusMap{})
+	c.Assert(sf.collectStatus(), checker.DeepEquals, StatusMap{})
 
 	sf = newStatusFunctions()
 	sf.register("foo", func() string { return "test1" })
-	c.Assert(sf.collectStatus(), DeepEquals, StatusMap{
+	c.Assert(sf.collectStatus(), checker.DeepEquals, StatusMap{
 		"foo": "test1",
 	})
 
 	sf.register("bar", func() string { return "test2" })
-	c.Assert(sf.collectStatus(), DeepEquals, StatusMap{
+	c.Assert(sf.collectStatus(), checker.DeepEquals, StatusMap{
 		"foo": "test1",
 		"bar": "test2",
 	})
 
 	sf.register("bar", func() string { return "test2" })
-	c.Assert(sf.collectStatus(), DeepEquals, StatusMap{
+	c.Assert(sf.collectStatus(), checker.DeepEquals, StatusMap{
 		"foo": "test1",
 		"bar": "test2",
 	})
 
 	sf.registerStatusObject("baz", &debugObj{})
-	c.Assert(sf.collectStatus(), DeepEquals, StatusMap{
+	c.Assert(sf.collectStatus(), checker.DeepEquals, StatusMap{
 		"foo": "test1",
 		"bar": "test2",
 		"baz": "test3",
