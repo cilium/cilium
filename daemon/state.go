cilium/cilium

commit f5e51bc3cf3c7530d327bfd26d702727424e9415

endpoint: Allocate endpoint ID independent of IP address
This removes the dependency for an API user to specify a unique endpoint ID
when creating an endpoint and instead allocates the endpoint ID when inserting
the endpoint into the manager.

The endpoint ID is allocated when an endpoint is inserted into the list of
endpoints. When the endpoint ID in the endpoint structure is already set, it is
attempted to be reused to support endpoint restore. For this reason, requests
from plugins creating endpoints with the endpoint ID set are rejected going
forward.

Due to the endpoint ID being allocated on endpoint list insertion, the endpoint
has to be inserted into the list of endpoints before the initial build. This is
behavior change but provides better visibility into the system.

Both the CNI and CMM plugin are adjusted to no longer specify the endpoint ID
in endpoint creation API calls.

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/state.go
@@ -174,7 +174,7 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 	// account for the new identity during the grace period. For this
 	// purpose, all endpoints being restored must already be in the
 	// endpoint list.
-	for _, ep := range state.restored {
+	for i, ep := range state.restored {
 		// If the endpoint has local conntrack option enabled, then
 		// check whether the CT map needs upgrading (and do so).
 		if ep.Options.IsEnabled(option.ConntrackLocal) {
@@ -187,7 +187,11 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 		// upon returning that endpoints are exposed to other subsystems via
 		// endpointmanager.
 
-		endpointmanager.Insert(ep)
+		if err := endpointmanager.Insert(ep); err != nil {
+			log.WithError(err).Warning("Unable to restore endpoint")
+			// remove endpoint from slice of endpoints to restore
+			state.restored = append(state.restored[:i], state.restored[i+1:]...)
+		}
 	}
 
 	for _, ep := range state.restored {

cilium-health/launch/endpoint.go
@@ -25,11 +25,9 @@ import (
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
-	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/pkg/datapath/route"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpoint/connector"
-	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	healthPkg "github.com/cilium/cilium/pkg/health/client"
 	"github.com/cilium/cilium/pkg/health/defaults"
@@ -195,10 +193,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 	cmd := launcher.Launcher{}
 
 	// Prepare the endpoint change request
-	id := int64(addressing.CiliumIPv6(ip6).EndpointID())
 	info := &models.EndpointChangeRequest{
-		ID:            id,
-		ContainerID:   endpointid.NewCiliumID(id),
 		ContainerName: ciliumHealth,
 		State:         models.EndpointStateWaitingForIdentity,
 		Addressing: &models.AddressPair{

daemon/endpoint.go
@@ -156,7 +156,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 
 	ep.SetDefaultOpts(option.Config.Opts)
 
-	checkIDs := []string{ep.ID}
+	checkIDs := []string{}
 
 	if ep.IPv4.IsSet() {
 		checkIDs = append(checkIDs, endpointid.NewID(endpointid.IPv4Prefix, ep.IPv4.String()))
@@ -294,20 +294,9 @@ func (h *putEndpointID) Handle(params PutEndpointIDParams) middleware.Responder 
 	epTemplate := params.Endpoint
 
 	logger := log.WithFields(logrus.Fields{
-		logfields.EndpointID:  epTemplate.ID,
 		logfields.ContainerID: epTemplate.ContainerID,
 		logfields.EventUUID:   uuid.NewUUID(),
 	})
-
-	if n, err := endpointid.ParseCiliumID(params.ID); err != nil {
-		return api.Error(PutEndpointIDInvalidCode, err)
-	} else if n != epTemplate.ID {
-		return api.New(PutEndpointIDInvalidCode,
-			"ID parameter does not match ID in endpoint parameter")
-	} else if epTemplate.ID == 0 {
-		return api.New(PutEndpointIDInvalidCode,
-			"endpoint ID cannot be 0")
-	}
 
 	code, err := h.d.createEndpoint(params.HTTPRequest.Context(), epTemplate)
 	if err != nil {

daemon/endpoint_test.go
@@ -18,12 +18,11 @@ package main
 
 import (
 	"context"
-	"strconv"
+	"net"
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	apiEndpoint "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
-	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/pkg/checker"
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/endpointmanager"
@@ -42,10 +41,7 @@ func getEPTemplate(c *C) *models.EndpointChangeRequest {
 	c.Assert(ip4, Not(IsNil))
 	c.Assert(ip6, Not(IsNil))
 
-	id := int64(addressing.CiliumIPv6(ip6).EndpointID())
 	return &models.EndpointChangeRequest{
-		ID:            id,
-		ContainerID:   endpointid.NewCiliumID(id),
 		ContainerName: "foo",
 		State:         models.EndpointStateWaitingForIdentity,
 		Addressing: &models.AddressPair{
@@ -78,12 +74,12 @@ func (ds *DaemonSuite) TestEndpointAddNoLabels(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(code, Equals, apiEndpoint.PutEndpointIDCreatedCode)
 
-	// Check that the endpoint has the reserved:init label.
-	ep, err := endpointmanager.Lookup(strconv.FormatInt(epTemplate.ID, 10))
-	c.Assert(err, IsNil)
 	expectedLabels := labels.Labels{
 		labels.IDNameInit: labels.NewLabel(labels.IDNameInit, "", labels.LabelSourceReserved),
 	}
+	// Check that the endpoint has the reserved:init label.
+	ep, err := endpointmanager.Lookup(endpointid.NewIPPrefixID(net.ParseIP(epTemplate.Addressing.IPV4)))
+	c.Assert(err, IsNil)
 	c.Assert(ep.OpLabels.IdentityLabels(), checker.DeepEquals, expectedLabels)
 
 	// If the mode is "default", check that the policy is always enforced for

daemon/k8s_watcher_test.go
@@ -291,7 +291,9 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 			name: "ipcache is missing a pod",
 			setupArgs: func() args {
 				endpointmanager.RemoveAll()
-				endpointmanager.Insert(endpointCreator(123, identity.NumericIdentity(1000)))
+				if err := endpointmanager.Insert(endpointCreator(123, identity.NumericIdentity(1000))); err != nil {
+					panic(err)
+				}
 				m := versioned.NewMap()
 				m.Add("", versioned.Object{
 					Data: &core_v1.Pod{
@@ -330,7 +332,9 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 			name: "ipcache contains the pod but endpointmanager doesn't contain any endpoint that manages the pod. Should be no-op",
 			setupArgs: func() args {
 				endpointmanager.RemoveAll()
-				endpointmanager.Insert(endpointCreator(123, identity.NumericIdentity(1000)))
+				if err := endpointmanager.Insert(endpointCreator(123, identity.NumericIdentity(1000))); err != nil {
+					panic(err)
+				}
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
 					ID:     identity.ReservedIdentityInit,
@@ -366,7 +370,9 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 				ep := endpointCreator(123, identity.NumericIdentity(1000))
 				ep.SetK8sPodName("foo")
 				ep.SetK8sNamespace("bar")
-				endpointmanager.Insert(ep)
+				if err := endpointmanager.Insert(ep); err != nil {
+					panic(err)
+				}
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
 					ID:     identity.ReservedIdentityInit,
@@ -422,7 +428,9 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 				ep.OpLabels.OrchestrationIdentity = labels.Map2Labels(map[string]string{"foo": "bar"}, labels.LabelSourceK8s)
 				ep.SetK8sPodName("foo")
 				ep.SetK8sNamespace("bar")
-				endpointmanager.Insert(ep)
+				if err := endpointmanager.Insert(ep); err != nil {
+					panic(err)
+				}
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
 					ID:     identity.ReservedIdentityInit,
@@ -462,7 +470,9 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 				ep.OpLabels.OrchestrationIdentity = labels.Map2Labels(map[string]string{"foo": "bar"}, labels.LabelSourceK8s)
 				ep.SetK8sPodName("foo")
 				ep.SetK8sNamespace("bar")
-				endpointmanager.Insert(ep)
+				if err := endpointmanager.Insert(ep); err != nil {
+					panic(err)
+				}
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
 					ID:     identity.ReservedIdentityInit,
@@ -713,6 +723,7 @@ func (ds *DaemonSuite) Test_missingK8sNamespaceV1(c *C) {
 	type args struct {
 		m versioned.Map
 	}
+
 	tests := []struct {
 		name        string
 		setupArgs   func() args
@@ -732,8 +743,11 @@ func (ds *DaemonSuite) Test_missingK8sNamespaceV1(c *C) {
 		{
 			name: "endpointmanager doesn't contain any endpoint that is part of that namespace. Should be no-op",
 			setupArgs: func() args {
+				endpointmanager.RemoveAll()
 				ep := endpointCreator(123, identity.NumericIdentity(1000))
-				endpointmanager.Insert(ep)
+				if err := endpointmanager.Insert(ep); err != nil {
+					panic(err)
+				}
 				m := versioned.NewMap()
 				m.Add("", versioned.Object{
 					Data: &core_v1.Namespace{
@@ -754,9 +768,12 @@ func (ds *DaemonSuite) Test_missingK8sNamespaceV1(c *C) {
 		{
 			name: "endpointmanager contains the endpoint that is part of that namespace but ep doesn't have all labels",
 			setupArgs: func() args {
-				ep := endpointCreator(123, identity.NumericIdentity(1000))
+				endpointmanager.RemoveAll()
+				ep := endpointCreator(124, identity.NumericIdentity(1000))
 				ep.SetK8sNamespace("foo")
-				endpointmanager.Insert(ep)
+				if err := endpointmanager.Insert(ep); err != nil {
+					panic(err)
+				}
 				m := versioned.NewMap()
 				m.Add("", versioned.Object{
 					Data: &core_v1.Namespace{
@@ -791,12 +808,15 @@ func (ds *DaemonSuite) Test_missingK8sNamespaceV1(c *C) {
 		{
 			name: "endpointmanager contains the endpoint that is part of that namespace and have all labels",
 			setupArgs: func() args {
-				ep := endpointCreator(123, identity.NumericIdentity(1000))
+				endpointmanager.RemoveAll()
+				ep := endpointCreator(125, identity.NumericIdentity(1000))
 				ep.OpLabels.OrchestrationIdentity = labels.Map2Labels(
 					map[string]string{policy.JoinPath(k8sConst.PodNamespaceMetaLabels, "id.foo"): "bar"},
 					labels.LabelSourceK8s)
 				ep.SetK8sNamespace("foo")
-				endpointmanager.Insert(ep)
+				if err := endpointmanager.Insert(ep); err != nil {
+					panic(err)
+				}
 				m := versioned.NewMap()
 				m.Add("", versioned.Object{
 					Data: &core_v1.Namespace{

pkg/endpoint/endpoint.go
@@ -2064,7 +2064,19 @@ func (e *Endpoint) identityLabelsChanged(owner Owner, myChangeRev int) error {
 
 	e.SetIdentity(identity)
 
-	readyToRegenerate := e.SetStateLocked(StateWaitingToRegenerate, "Triggering regeneration due to new identity")
+	readyToRegenerate := false
+
+	// Regeneration is olny triggered once the endpoint ID has been
+	// assigned. This ensures that on the initial creation, the endpoint is
+	// not generated until the endpoint ID has been assigned. If the
+	// identity is resolved before the endpoint ID is assigned, the
+	// regeneration is deferred into endpointmanager.AddEndpoint(). If the
+	// identity is not allocated yet when endpointmanager.AddEndpoint() is
+	// called, the controller calling identityLabelsChanged() will trigger
+	// the regeneration as soon as the identity is known.
+	if e.ID != 0 {
+		readyToRegenerate = e.SetStateLocked(StateWaitingToRegenerate, "Triggering regeneration due to new identity")
+	}
 
 	// Unconditionally force policy recomputation after a new identity has been
 	// assigned.

pkg/endpointmanager/manager.go
@@ -69,7 +69,20 @@ func init() {
 }
 
 // Insert inserts the endpoint into the global maps.
-func Insert(ep *endpoint.Endpoint) {
+func Insert(ep *endpoint.Endpoint) error {
+	if ep.ID != 0 {
+		if err := endpointid.Reuse(ep.ID); err != nil {
+			return fmt.Errorf("unable to reuse endpoint ID: %s", err)
+		}
+	} else {
+		id := endpointid.Allocate()
+		if id == uint16(0) {
+			return fmt.Errorf("no more endpoint IDs available")
+		}
+
+		ep.ID = id
+	}
+
 	// No need to check liveness as an endpoint can only be deleted via the
 	// API after it has been inserted into the manager.
 	ep.UnconditionalRLock()
@@ -84,6 +97,8 @@ func Insert(ep *endpoint.Endpoint) {
 	if EndpointSynchronizer != nil {
 		EndpointSynchronizer.RunK8sCiliumEndpointSync(ep)
 	}
+
+	return nil
 }
 
 // Lookup looks up the endpoint by prefix id
@@ -178,6 +193,15 @@ func Remove(ep *endpoint.Endpoint) {
 	defer mutex.Unlock()
 	delete(endpoints, ep.ID)
 
+	if err := endpointid.Release(ep.ID); err != nil {
+		// While restoring, endpoint IDs may not have been reused yet.
+		// Failure to release means that the endpoint ID was not reused
+		// yet. Avoid irritating warning messages.
+		if ep.GetStateLocked() != endpoint.StateRestoring {
+			log.WithError(err).Warning("Unable to release endpoint ID")
+		}
+	}
+
 	if ep.ContainerID != "" {
 		delete(endpointsAux, endpointid.NewID(endpointid.ContainerIdPrefix, ep.ContainerID))
 	}
@@ -207,6 +231,7 @@ func Remove(ep *endpoint.Endpoint) {
 func RemoveAll() {
 	mutex.Lock()
 	defer mutex.Unlock()
+	endpointid.ReallocatePool()
 	endpoints = map[uint16]*endpoint.Endpoint{}
 	endpointsAux = map[string]*endpoint.Endpoint{}
 }
@@ -353,6 +378,10 @@ func AddEndpoint(owner endpoint.Owner, ep *endpoint.Endpoint, reason string) (er
 	ep.SetIngressPolicyEnabled(alwaysEnforce)
 	ep.SetEgressPolicyEnabled(alwaysEnforce)
 
+	if ep.ID != 0 {
+		return fmt.Errorf("Endpoint ID is already set to %d", ep.ID)
+	}
+
 	// Regenerate immediately if ready or waiting for identity
 	if err := ep.LockAlive(); err != nil {
 		return err
@@ -366,15 +395,20 @@ func AddEndpoint(owner endpoint.Owner, ep *endpoint.Endpoint, reason string) (er
 		ep.SetStateLocked(endpoint.StateWaitingToRegenerate, reason)
 		build = true
 	}
+
 	ep.Unlock()
+
+	if err := Insert(ep); err != nil {
+		return err
+	}
 
 	if build {
 		if err := ep.RegenerateWait(owner, reason); err != nil {
+			Remove(ep)
 			return err
 		}
 	}
 
-	Insert(ep)
 	ep.InsertEvent()
 
 	return nil

plugins/cilium-cni/cilium-cni.go
@@ -418,7 +418,6 @@ func cmdAdd(args *skel.CmdArgs) error {
 		if err != nil {
 			return err
 		}
-		ep.ID = int64(state.IP6.EndpointID())
 		res.IPs = append(res.IPs, ipConfig)
 		res.Routes = append(res.Routes, routes...)
 	} else {
@@ -457,13 +456,11 @@ func cmdAdd(args *skel.CmdArgs) error {
 	ep.SyncBuildEndpoint = true
 	if err = c.EndpointCreate(ep); err != nil {
 		logger.WithError(err).WithFields(logrus.Fields{
-			logfields.EndpointID:  ep.ID,
 			logfields.ContainerID: ep.ContainerID}).Warn("Unable to create endpoint")
 		return fmt.Errorf("Unable to create endpoint: %s", err)
 	}
 
 	logger.WithFields(logrus.Fields{
-		logfields.EndpointID:  ep.ID,
 		logfields.ContainerID: ep.ContainerID}).Debug("Endpoint successfully created")
 	return cniTypes.PrintResult(res, cniVer)
 }

commit e73633543f0386d2d729129511e52808de2ffbbc

agent: Avoid panic when more than one endpoint fails to restore
Fixes: f5e51bc3cf3 ("endpoint: Allocate endpoint ID independent of IP address")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/state.go
@@ -174,7 +174,8 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 	// account for the new identity during the grace period. For this
 	// purpose, all endpoints being restored must already be in the
 	// endpoint list.
-	for i, ep := range state.restored {
+	for i := len(state.restored) - 1; i >= 0; i-- {
+		ep := state.restored[i]
 		// If the endpoint has local conntrack option enabled, then
 		// check whether the CT map needs upgrading (and do so).
 		if ep.Options.IsEnabled(option.ConntrackLocal) {