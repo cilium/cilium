cilium/cilium

commit 84f82e7ccd057813129a73aca486218a2b7378ac

agent: Make IPv6 optional
Datapath:
Use a consistent ENABLE_IPV4 and ENABLE_IPV6 define to steer enabling IPv4 and
IPv6 code paths. Make all IPv6 datapath code dependent on ENABLE_IPV6.

Agent:
Make installation of routes, creation of maps, insertion into ipcache, etc.
dependent on option.Config.EnableIPv6.

Also fixes a problem in bpf/Makefile to properly test a combination of defines
when performing test compilation.

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/Makefile
@@ -23,7 +23,11 @@ LB_OPTIONS = \
 	-DSKIP_DEBUG \
 	-DLB_L3 \
 	-DLB_L4 \
-	-DLB_L3 -DLB_L4
+	"-DLB_L3 -DLB_L4" \
+	"-DENABLE_IPV4 -DLB_L3 -DLB_L4" \
+	"-DENABLE_IPV6 -DLB_L3 -DLB_L4" \
+	"-DENABLE_IPV4 -DENABLE_IPV6 -DLB_L3 -DLB_L4"
+
 
 bpf_lb.ll: bpf_lb.c $(LIB)
 	$(QUIET) set -e; \
@@ -41,7 +45,11 @@ bpf_lb.o: bpf_lb.ll
 LXC_OPTIONS = \
 	 -DSKIP_DEBUG \
 	 -DHAVE_LPM_MAP_TYPE \
-	 -DHAVE_LRU_MAP_TYPE
+	 -DHAVE_LRU_MAP_TYPE \
+	 -DENABLE_IPV4 \
+	 -DENABLE_IPV6 \
+	 "-DENABLE_IPV6 -DENABLE_IPV4" \
+	 "-DENABLE_IPV4 -DENABLE_NAT46"
 
 bpf_lxc.ll: bpf_lxc.c $(LIB)
 	$(QUIET) set -e; \

bpf/lib/common.h
@@ -26,6 +26,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#if defined LXC_NAT46 && defined LXC_IP
+#define ENABLE_NAT46
+#endif
+
 // FIXME: GH-3239 LRU logic is not handling timeouts gracefully enough
 // #ifndef HAVE_LRU_MAP_TYPE
 // #define NEEDS_TIMEOUT 1

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=82bba54432f3d6ce632ac08333d2ca7b93ea92c2
+GO_BINDATA_SHA1SUM=38dc757e85be72ae7981a6259de5d947a826fe25
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

bpf/bpf_lb.c
@@ -27,8 +27,6 @@
  *              then passed back to the stack.
  *
  * Configuration:
- *  - LB_DISABLE_IPV4 - Ignore IPv4 packets
- *  - LB_DISABLE_IPV6 - Ignore IPv6 packets
  *  - LB_REDIRECT     - Redirect to an ifindex
  *  - LB_L4           - Enable L4 matching and mapping
  */
@@ -54,7 +52,7 @@
 #include "lib/drop.h"
 #include "lib/lb.h"
 
-#ifndef LB_DISABLE_IPV6
+#ifdef ENABLE_IPV6
 static inline int handle_ipv6(struct __sk_buff *skb)
 {
 	void *data, *data_end;
@@ -113,9 +111,9 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 
 	return TC_ACT_REDIRECT;
 }
-#endif
+#endif /* ENABLE_IPV6 */
 
-#ifndef LB_DISABLE_IPV4
+#ifdef ENABLE_IPV4
 static inline int handle_ipv4(struct __sk_buff *skb)
 {
 	void *data;
@@ -168,7 +166,7 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 
 	return TC_ACT_REDIRECT;
 }
-#endif
+#endif /* ENABLE_IPV4 */
 
 __section("from-netdev")
 int from_netdev(struct __sk_buff *skb)
@@ -178,13 +176,13 @@ int from_netdev(struct __sk_buff *skb)
 	bpf_clear_cb(skb);
 
 	switch (skb->protocol) {
-#ifndef LB_DISABLE_IPV6
+#ifdef ENABLE_IPV6
 	case bpf_htons(ETH_P_IPV6):
 		ret = handle_ipv6(skb);
 		break;
 #endif
 
-#ifndef LB_DISABLE_IPV4
+#ifdef ENABLE_IPV4
 	case bpf_htons(ETH_P_IP):
 		ret = handle_ipv4(skb);
 		break;

bpf/bpf_lxc.c
@@ -56,6 +56,7 @@
 #define CT_MAP_TYPE BPF_MAP_TYPE_HASH
 #endif
 
+#ifdef ENABLE_IPV6
 struct bpf_elf_map __section_maps CT_MAP_TCP6 = {
 	.type		= CT_MAP_TYPE,
 	.size_key	= sizeof(struct ipv6_ct_tuple),
@@ -72,6 +73,17 @@ struct bpf_elf_map __section_maps CT_MAP_ANY6 = {
 	.max_elem	= CT_MAP_SIZE_ANY,
 };
 
+static inline struct bpf_elf_map *
+get_ct_map6(struct ipv6_ct_tuple *tuple)
+{
+	if (tuple->nexthdr == IPPROTO_TCP) {
+		return &CT_MAP_TCP6;
+	}
+	return &CT_MAP_ANY6;
+}
+#endif
+
+#ifdef ENABLE_IPV4
 struct bpf_elf_map __section_maps CT_MAP_TCP4 = {
 	.type		= CT_MAP_TYPE,
 	.size_key	= sizeof(struct ipv4_ct_tuple),
@@ -89,15 +101,6 @@ struct bpf_elf_map __section_maps CT_MAP_ANY4 = {
 };
 
 static inline struct bpf_elf_map *
-get_ct_map6(struct ipv6_ct_tuple *tuple)
-{
-	if (tuple->nexthdr == IPPROTO_TCP) {
-		return &CT_MAP_TCP6;
-	}
-	return &CT_MAP_ANY6;
-}
-
-static inline struct bpf_elf_map *
 get_ct_map4(struct ipv4_ct_tuple *tuple)
 {
 	if (tuple->nexthdr == IPPROTO_TCP) {
@@ -105,12 +108,16 @@ get_ct_map4(struct ipv4_ct_tuple *tuple)
 	}
 	return &CT_MAP_ANY4;
 }
+#endif
 
+#if defined ENABLE_IPV4 || defined ENABLE_IPV6
 static inline bool redirect_to_proxy(int verdict, int dir)
 {
 	return verdict > 0 && (dir == CT_NEW || dir == CT_ESTABLISHED);
 }
+#endif
 
+#ifdef ENABLE_IPV6
 static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 				   struct ipv6_ct_tuple *tuple, int l3_off,
 				   struct ipv6hdr *ip6, __u32 *dstID)
@@ -339,7 +346,7 @@ skip_service_lookup:
 	}
 #endif
 
-#ifdef LXC_NAT46
+#ifdef ENABLE_NAT46
 	if (unlikely(ipv6_addr_is_mapped(daddr))) {
 		ep_tail_call(skb, CILIUM_CALL_NAT64);
 		return DROP_MISSED_TAIL_CALL;
@@ -423,9 +430,9 @@ __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV6_FROM_LXC) int tail_handle_ipv6
 
 	return ret;
 }
+#endif /* ENABLE_IPV6 */
 
-#ifdef LXC_IPV4
-
+#ifdef ENABLE_IPV4
 static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 {
 	struct ipv4_ct_tuple tuple = {};
@@ -465,14 +472,13 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 	}
 
 	ct_state_new.orig_dport = key.dport;
-#ifdef ENABLE_IPV4
 	if ((svc = lb4_lookup_service(skb, &key)) != NULL) {
 		ret = lb4_local(get_ct_map4(&tuple), skb, l3_off, l4_off, &csum_off,
 				&key, &tuple, svc, &ct_state_new, ip4->saddr);
 		if (IS_ERR(ret))
 			return ret;
 	}
-#endif
+
 skip_service_lookup:
 	/* The verifier wants to see this assignment here in case the above goto
 	 * skip_service_lookup is hit. However, in the case the packet
@@ -684,8 +690,6 @@ __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV4_FROM_LXC) int tail_handle_ipv4
 	return ret;
 }
 
-#endif
-
 /*
  * ARP responder for ARP requests from container
  * Respond to IPV4_GATEWAY with NODE_MAC
@@ -695,6 +699,7 @@ __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_ARP) int tail_handle_arp(struct __s
 	union macaddr mac = NODE_MAC;
 	return arp_respond(skb, &mac);
 }
+#endif /* ENABLE_IPV4 */
 
 __section("from-container")
 int handle_ingress(struct __sk_buff *skb)
@@ -707,11 +712,14 @@ int handle_ingress(struct __sk_buff *skb)
 			  TRACE_PAYLOAD_LEN);
 
 	switch (skb->protocol) {
+#ifdef ENABLE_IPV6
 	case bpf_htons(ETH_P_IPV6):
 		ep_tail_call(skb, CILIUM_CALL_IPV6_FROM_LXC);
 		ret = DROP_MISSED_TAIL_CALL;
 		break;
+#endif
 
+#ifdef ENABLE_IPV4
 	case bpf_htons(ETH_P_IP):
 		ep_tail_call(skb, CILIUM_CALL_IPV4_FROM_LXC);
 		ret = DROP_MISSED_TAIL_CALL;
@@ -721,6 +729,7 @@ int handle_ingress(struct __sk_buff *skb)
 		ep_tail_call(skb, CILIUM_CALL_ARP);
 		ret = DROP_MISSED_TAIL_CALL;
 		break;
+#endif
 
 	default:
 		ret = DROP_UNKNOWN_L3;
@@ -732,6 +741,7 @@ int handle_ingress(struct __sk_buff *skb)
 	return ret;
 }
 
+#ifdef ENABLE_IPV6
 static inline int __inline__
 ipv6_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding_reason, struct ep_config *cfg)
 {
@@ -884,8 +894,9 @@ __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV6_TO_LXC) int tail_ipv6_policy(s
 
 	return ret;
 }
+#endif /* ENABLE_IPV6 */
 
-#ifdef LXC_IPV4
+#ifdef ENABLE_IPV4
 static inline int __inline__
 ipv4_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding_reason, struct ep_config *cfg)
 {
@@ -927,7 +938,7 @@ ipv4_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 
 	*forwarding_reason = ret;
 
-#ifdef LXC_NAT46
+#ifdef ENABLE_NAT46
 	if (skb->cb[CB_NAT46_STATE] == NAT46) {
 		ep_tail_call(skb, CILIUM_CALL_NAT46);
 		return DROP_MISSED_TAIL_CALL;
@@ -1025,8 +1036,7 @@ __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV4_TO_LXC) int tail_ipv4_policy(s
 
 	return ret;
 }
-
-#endif
+#endif /* ENABLE_IPV4 */
 
 /* Handle policy decisions as the packet makes its way towards the endpoint.
  * Previously, the packet may have come from another local endpoint, another
@@ -1041,17 +1051,19 @@ __section_tail(CILIUM_MAP_POLICY, LXC_ID) int handle_policy(struct __sk_buff *sk
 	__u32 src_label = skb->cb[CB_SRC_LABEL];
 
 	switch (skb->protocol) {
+#ifdef ENABLE_IPV6
 	case bpf_htons(ETH_P_IPV6):
 		ep_tail_call(skb, CILIUM_CALL_IPV6_TO_LXC);
 		ret = DROP_MISSED_TAIL_CALL;
 		break;
+#endif /* ENABLE_IPV6 */
 
-#ifdef LXC_IPV4
+#ifdef ENABLE_IPV4
 	case bpf_htons(ETH_P_IP):
 		ep_tail_call(skb, CILIUM_CALL_IPV4_TO_LXC);
 		ret = DROP_MISSED_TAIL_CALL;
 		break;
-#endif
+#endif /* ENABLE_IPV4 */
 
 	default:
 		ret = DROP_UNKNOWN_L3;
@@ -1065,7 +1077,7 @@ __section_tail(CILIUM_MAP_POLICY, LXC_ID) int handle_policy(struct __sk_buff *sk
 	return ret;
 }
 
-#ifdef LXC_NAT46
+#ifdef ENABLE_NAT46
 __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_NAT64) int tail_ipv6_to_ipv4(struct __sk_buff *skb)
 {
 	int ret = ipv6_to_ipv4(skb, 14, LXC_IPV4);

bpf/bpf_netdev.c
@@ -47,6 +47,25 @@
 #include "lib/drop.h"
 #include "lib/encap.h"
 
+
+#if defined FROM_HOST && (defined ENABLE_IPV4 || defined ENABLE_IPV6)
+static inline int rewrite_dmac_to_host(struct __sk_buff *skb)
+{
+	/* When attached to cilium_host, we rewrite the DMAC to the mac of
+	 * cilium_host (peer) to ensure the packet is being considered to be
+	 * addressed to the host (PACKET_HOST) */
+	union macaddr cilium_net_mac = CILIUM_NET_MAC;
+
+	/* Rewrite to destination MAC of cilium_net (remote peer) */
+	if (eth_store_daddr(skb, (__u8 *) &cilium_net_mac.addr, 0) < 0)
+		return send_drop_notify_error(skb, DROP_WRITE_ERROR, TC_ACT_OK, METRIC_INGRESS);
+
+	return TC_ACT_OK;
+}
+#endif
+
+
+#ifdef ENABLE_IPV6
 static inline __u32 derive_sec_ctx(struct __sk_buff *skb, const union v6addr *node_ip,
 				   struct ipv6hdr *ip6)
 {
@@ -121,51 +140,6 @@ reverse_proxy6(struct __sk_buff *skb, int l4_off, struct ipv6hdr *ip6, __u8 nh)
 	skb->tc_index |= TC_INDEX_F_SKIP_PROXY;
 
 	return 0;
-}
-#endif
-
-#ifdef FROM_HOST
-static inline bool __inline__ handle_identity_from_host(struct __sk_buff *skb, __u32 *identity)
-{
-	__u32 magic = skb->mark & MARK_MAGIC_HOST_MASK;
-	bool from_proxy = false;
-
-	/* Packets from the ingress proxy must skip the proxy when the
-	 * destination endpoint evaluates the policy. As the packet
-	 * would loop otherwise. */
-	if (magic == MARK_MAGIC_PROXY_INGRESS) {
-		*identity = get_identity_via_proxy(skb);
-		skb->tc_index |= TC_INDEX_F_SKIP_PROXY;
-		from_proxy = true;
-	} else if (magic == MARK_MAGIC_PROXY_EGRESS) {
-		*identity = get_identity_via_proxy(skb);
-		from_proxy = true;
-	} else if (magic == MARK_MAGIC_HOST) {
-		*identity = HOST_ID;
-	} else {
-		*identity = WORLD_ID;
-	}
-
-	/* Reset packet mark to avoid hitting routing rules again */
-	skb->mark = 0;
-
-	return from_proxy;
-}
-#endif
-
-#ifdef FROM_HOST
-static inline int rewrite_dmac_to_host(struct __sk_buff *skb)
-{
-	/* When attached to cilium_host, we rewrite the DMAC to the mac of
-	 * cilium_host (peer) to ensure the packet is being considered to be
-	 * addressed to the host (PACKET_HOST) */
-	union macaddr cilium_net_mac = CILIUM_NET_MAC;
-
-	/* Rewrite to destination MAC of cilium_net (remote peer) */
-	if (eth_store_daddr(skb, (__u8 *) &cilium_net_mac.addr, 0) < 0)
-		return send_drop_notify_error(skb, DROP_WRITE_ERROR, TC_ACT_OK, METRIC_INGRESS);
-
-	return TC_ACT_OK;
 }
 #endif
 
@@ -282,6 +256,7 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 #endif
 	return TC_ACT_OK;
 }
+#endif /* ENABLE_IPV6 */
 
 #ifdef ENABLE_IPV4
 static inline __u32 derive_ipv4_sec_ctx(struct __sk_buff *skb, struct iphdr *ip4)
@@ -473,6 +448,35 @@ __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV4_FROM_LXC) int tail_handle_ipv4
 	return ret;
 }
 
+#endif /* ENABLE_IPV4 */
+
+#ifdef FROM_HOST
+static inline bool __inline__ handle_identity_from_host(struct __sk_buff *skb, __u32 *identity)
+{
+	__u32 magic = skb->mark & MARK_MAGIC_HOST_MASK;
+	bool from_proxy = false;
+
+	/* Packets from the ingress proxy must skip the proxy when the
+	 * destination endpoint evaluates the policy. As the packet
+	 * would loop otherwise. */
+	if (magic == MARK_MAGIC_PROXY_INGRESS) {
+		*identity = get_identity_via_proxy(skb);
+		skb->tc_index |= TC_INDEX_F_SKIP_PROXY;
+		from_proxy = true;
+	} else if (magic == MARK_MAGIC_PROXY_EGRESS) {
+		*identity = get_identity_via_proxy(skb);
+		from_proxy = true;
+	} else if (magic == MARK_MAGIC_HOST) {
+		*identity = HOST_ID;
+	} else {
+		*identity = WORLD_ID;
+	}
+
+	/* Reset packet mark to avoid hitting routing rules again */
+	skb->mark = 0;
+
+	return from_proxy;
+}
 #endif
 
 __section("from-netdev")
@@ -500,6 +504,7 @@ int from_netdev(struct __sk_buff *skb)
 #endif
 
 	switch (skb->protocol) {
+#ifdef ENABLE_IPV6
 	case bpf_htons(ETH_P_IPV6):
 		/* This is considered the fast path, no tail call */
 		ret = handle_ipv6(skb, identity);
@@ -509,6 +514,7 @@ int from_netdev(struct __sk_buff *skb)
 		if (IS_ERR(ret))
 			return send_drop_notify_error(skb, ret, TC_ACT_SHOT, METRIC_INGRESS);
 		break;
+#endif
 
 #ifdef ENABLE_IPV4
 	case bpf_htons(ETH_P_IP):

bpf/init.sh
@@ -52,10 +52,12 @@ echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
 # Disable unprivileged BPF
 echo 1 > /proc/sys/kernel/unprivileged_bpf_disabled || true
 
+if [ "$IP6_HOST" != "<nil>" ]; then
 # Docker <17.05 has an issue which causes IPv6 to be disabled in the initns for all
 # interface (https://github.com/docker/libnetwork/issues/1720)
 # Enable IPv6 for now
-echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+	echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6 || true
+fi
 
 # This directory was created by the daemon and contains the per container header file
 DIR="$PWD/globals"
@@ -65,11 +67,17 @@ function setup_veth()
 	local -r NAME=$1
 
 	ip link set $NAME up
-	echo 1 > /proc/sys/net/ipv4/conf/${NAME}/forwarding
-	echo 1 > /proc/sys/net/ipv6/conf/${NAME}/forwarding
-	echo 0 > /proc/sys/net/ipv4/conf/${NAME}/rp_filter
-	echo 1 > /proc/sys/net/ipv4/conf/${NAME}/accept_local
-	echo 0 > /proc/sys/net/ipv4/conf/${NAME}/send_redirects
+
+	if [ "$IP6_HOST" != "<nil>" ]; then
+		echo 1 > /proc/sys/net/ipv6/conf/${NAME}/forwarding
+	fi
+
+	if [ "$IP4_HOST" != "<nil>" ]; then
+		echo 1 > /proc/sys/net/ipv4/conf/${NAME}/forwarding
+		echo 0 > /proc/sys/net/ipv4/conf/${NAME}/rp_filter
+		echo 1 > /proc/sys/net/ipv4/conf/${NAME}/accept_local
+		echo 0 > /proc/sys/net/ipv4/conf/${NAME}/send_redirects
+	fi
 }
 
 function setup_veth_pair()
@@ -121,8 +129,13 @@ function move_local_rules_af()
 
 function move_local_rules()
 {
-	move_local_rules_af "ip -4"
-	move_local_rules_af "ip -6"
+	if [ "$IP4_HOST" != "<nil>" ]; then
+		move_local_rules_af "ip -4"
+	fi
+
+	if [ "$IP6_HOST" != "<nil>" ]; then 
+		move_local_rules_af "ip -6"
+	fi
 }
 
 function setup_proxy_rules()
@@ -130,27 +143,33 @@ function setup_proxy_rules()
 	# Any packet from a local process uses a separate routing table
 	rulespec="fwmark 0xA00/0xF00 pref 10 lookup $PROXY_RT_TABLE"
 
-	if [ -n "$(ip -4 rule list)" ]; then
-		if [ -z "$(ip -4 rule list $rulespec)" ]; then
-			ip -4 rule add $rulespec
+	if [ "$IP4_HOST" != "<nil>" ]; then
+		if [ -n "$(ip -4 rule list)" ]; then
+			if [ -z "$(ip -4 rule list $rulespec)" ]; then
+				ip -4 rule add $rulespec
+			fi
 		fi
-	fi
 
-	if [ -n "$IP4_HOST" ]; then
 		ip route replace table $PROXY_RT_TABLE $IP4_HOST/32 dev $HOST_DEV1
 		ip route replace table $PROXY_RT_TABLE default via $IP4_HOST
+	else
+		ip -4 rule del $rulespec 2> /dev/null || true
 	fi
 
-	if [ -n "$(ip -6 rule list)" ]; then
-		if [ -z "$(ip -6 rule list $rulespec)" ]; then
-			ip -6 rule add $rulespec
+	if [ "$IP6_HOST" != "<nil>" ]; then
+		if [ -n "$(ip -6 rule list)" ]; then
+			if [ -z "$(ip -6 rule list $rulespec)" ]; then
+				ip -6 rule add $rulespec
+			fi
 		fi
-	fi
 
-	IP6_LLADDR=$(ip -6 addr show dev $HOST_DEV2 | grep inet6 | head -1 | awk '{print $2}' | awk -F'/' '{print $1}')
-	if [ -n "$IP6_LLADDR" ]; then
-		ip -6 route replace table $PROXY_RT_TABLE ${IP6_LLADDR}/128 dev $HOST_DEV1
-		ip -6 route replace table $PROXY_RT_TABLE default via $IP6_LLADDR dev $HOST_DEV1
+		IP6_LLADDR=$(ip -6 addr show dev $HOST_DEV2 | grep inet6 | head -1 | awk '{print $2}' | awk -F'/' '{print $1}')
+		if [ -n "$IP6_LLADDR" ]; then
+			ip -6 route replace table $PROXY_RT_TABLE ${IP6_LLADDR}/128 dev $HOST_DEV1
+			ip -6 route replace table $PROXY_RT_TABLE default via $IP6_LLADDR dev $HOST_DEV1
+		fi
+	else
+		ip -6 rule del $rulespec 2> /dev/null || true
 	fi
 }
 
@@ -270,14 +289,12 @@ echo "#define HOST_IFINDEX_MAC { .addr = ${HOST_MAC}}" >> $RUNDIR/globals/node_c
 
 # If the host does not have an IPv6 address assigned, assign our generated host
 # IP to make the host accessible to endpoints
-[ -n "$(ip -6 addr show to $IP6_HOST)" ] || {
-	ip -6 addr add $IP6_HOST dev $HOST_DEV1
-}
+if [ "$IP6_HOST" != "<nil>" ]; then
+	[ -n "$(ip -6 addr show to $IP6_HOST)" ] || ip -6 addr add $IP6_HOST dev $HOST_DEV1
+fi
 
-if [[ "$IP4_HOST" != "<nil>" ]]; then
-	[ -n "$(ip -4 addr show to $IP4_HOST)" ] || {
-		ip -4 addr add $IP4_HOST dev $HOST_DEV1 scope link
-	}
+if [ "$IP4_HOST" != "<nil>" ]; then
+	[ -n "$(ip -4 addr show to $IP4_HOST)" ] || ip -4 addr add $IP4_HOST dev $HOST_DEV1 scope link
 fi
 
 # Decrease priority of the rule to identify local addresses
@@ -320,7 +337,9 @@ if [ "$MODE" = "direct" ]; then
 	if [ -z "$NATIVE_DEV" ]; then
 		echo "No device specified for $MODE mode, ignoring..."
 	else
-		echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+		if [ "$IP6_HOST" != "<nil>" ]; then
+			echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+		fi
 
 		CALLS_MAP=cilium_calls_netdev_${ID_WORLD}
 		POLICY_MAP="cilium_policy_reserved_${ID_WORLD}"
@@ -333,7 +352,9 @@ elif [ "$MODE" = "lb" ]; then
 	if [ -z "$NATIVE_DEV" ]; then
 		echo "No device specified for $MODE mode, ignoring..."
 	else
-		echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+		if [ "$IP6_HOST" != "<nil>" ]; then
+			echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+		fi
 
 		CALLS_MAP="cilium_calls_lb"
 		OPTS="-DLB_L3 -DLB_L4"

bpf/lib/lb.h
@@ -35,6 +35,7 @@
 
 #define CILIUM_LB_MAP_MAX_FE		256
 
+#ifdef ENABLE_IPV6
 struct bpf_elf_map __section_maps cilium_lb6_reverse_nat = {
 	.type		= BPF_MAP_TYPE_HASH,
 	.size_key	= sizeof(__u16),
@@ -58,7 +59,9 @@ struct bpf_elf_map __section_maps cilium_lb6_rr_seq = {
 	.pinning        = PIN_GLOBAL_NS,
 	.max_elem       = CILIUM_LB_MAP_MAX_FE,
 };
+#endif /* ENABLE_IPV6 */
 
+#ifdef ENABLE_IPV4
 struct bpf_elf_map __section_maps cilium_lb4_reverse_nat = {
 	.type		= BPF_MAP_TYPE_HASH,
 	.size_key	= sizeof(__u16),
@@ -82,6 +85,9 @@ struct bpf_elf_map __section_maps cilium_lb4_rr_seq = {
 	.pinning        = PIN_GLOBAL_NS,
 	.max_elem       = CILIUM_LB_MAP_MAX_FE,
 };
+#endif /* ENABLE_IPV4 */
+
+
 #define REV_NAT_F_TUPLE_SADDR 1
 #ifdef LB_DEBUG
 #define cilium_dbg_lb cilium_dbg
@@ -251,6 +257,7 @@ static inline int __inline__ reverse_map_l4_port(struct __sk_buff *skb, __u8 nex
 	return 0;
 }
 
+#ifdef ENABLE_IPV6
 static inline int __inline__ __lb6_rev_nat(struct __sk_buff *skb, int l4_off,
 					 struct csum_offset *csum_off,
 					 struct ipv6_ct_tuple *tuple, int flags,
@@ -481,7 +488,9 @@ static inline int __inline__ lb6_local(void *map, struct __sk_buff *skb, int l3_
 	return lb6_xlate(skb, addr, tuple->nexthdr, l3_off, l4_off,
 			 csum_off, key, svc);
 }
+#endif /* ENABLE_IPV6 */
 
+#ifdef ENABLE_IPV4
 static inline int __inline__ __lb4_rev_nat(struct __sk_buff *skb, int l3_off, int l4_off,
 					 struct csum_offset *csum_off,
 					 struct ipv4_ct_tuple *tuple, int flags,
@@ -703,7 +712,6 @@ lb4_xlate(struct __sk_buff *skb, __be32 *new_daddr, __be32 *new_saddr,
 	return TC_ACT_OK;
 }
 
-#ifdef ENABLE_IPV4
 static inline int __inline__ lb4_local(void *map, struct __sk_buff *skb,
 				       int l3_off, int l4_off,
 				       struct csum_offset *csum_off, struct lb4_key *key,
@@ -780,6 +788,6 @@ static inline int __inline__ lb4_local(void *map, struct __sk_buff *skb,
 			 tuple->nexthdr, l3_off, l4_off, csum_off, key,
 			 svc);
 }
-#endif
+#endif /* ENABLE_IPV4 */
 
 #endif /* __LB_H_ */

commit 567bc867213c0a00204ff76940cf94c75a5be814

bpf: Fix default configuration of bpf/
The default configuration of binaries compiled in the bpf/ directory was
broken in commit 84f82e7ccd05 ("agent: Make IPv6 optional"). This causes
the complexity checker scripts to severely underestimate the datapath
complexity, breaking the verifier complexity tests.

Rearrange the logic so that the default configuration will enable all
possible code, then specific parts may be conditionally disabled.

Signed-off-by: Joe Stringer <joe@cilium.io>

bpf/Makefile
@@ -24,9 +24,9 @@ LB_OPTIONS = \
 	-DLB_L3 \
 	-DLB_L4 \
 	"-DLB_L3 -DLB_L4" \
-	"-DENABLE_IPV4 -DLB_L3 -DLB_L4" \
-	"-DENABLE_IPV6 -DLB_L3 -DLB_L4" \
-	"-DENABLE_IPV4 -DENABLE_IPV6 -DLB_L3 -DLB_L4"
+	"-DDISABLE_IPV4 -DLB_L3 -DLB_L4" \
+	"-DDISABLE_IPV6 -DLB_L3 -DLB_L4" \
+	"-DDISABLE_IPV4 -DDISABLE_IPV6 -DLB_L3 -DLB_L4"
 
 
 bpf_lb.ll: bpf_lb.c $(LIB)
@@ -46,10 +46,10 @@ LXC_OPTIONS = \
 	 -DSKIP_DEBUG \
 	 -DHAVE_LPM_MAP_TYPE \
 	 -DHAVE_LRU_MAP_TYPE \
-	 -DENABLE_IPV4 \
-	 -DENABLE_IPV6 \
-	 "-DENABLE_IPV6 -DENABLE_IPV4" \
-	 "-DENABLE_IPV4 -DENABLE_NAT46"
+	 -DDISABLE_IPV4 \
+	 -DDISABLE_IPV6 \
+	 "-DDISABLE_IPV6 -DDISABLE_IPV4" \
+	 "-DDISABLE_IPV4 -DENABLE_NAT46"
 
 bpf_lxc.ll: bpf_lxc.c $(LIB)
 	$(QUIET) set -e; \

bpf/lib/common.h
@@ -30,6 +30,18 @@
 #define ENABLE_NAT46
 #endif
 
+#if defined DISABLE_IPV4
+#undef ENABLE_IPV4
+#elif !defined ENABLE_IPV4
+#define ENABLE_IPV4
+#endif
+
+#if defined DISABLE_IPV6
+#undef ENABLE_IPV6
+#elif !defined ENABLE_IPV6
+#define ENABLE_IPV6
+#endif
+
 // FIXME: GH-3239 LRU logic is not handling timeouts gracefully enough
 // #ifndef HAVE_LRU_MAP_TYPE
 // #define NEEDS_TIMEOUT 1

bpf/netdev_config.h
@@ -23,7 +23,6 @@
 #ifndef SKIP_DEBUG
 #define DEBUG
 #endif
-#define ENABLE_IPV4
 #define HANDLE_NS
 #define FROM_HOST
 #define ENCAP_IFINDEX 1

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=962c2a960a696d1b20917ce396bb077cb2eee47b
+GO_BINDATA_SHA1SUM=9cde9fc4bb9283008c8cd8ec7035d45946834563
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 