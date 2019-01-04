cilium/cilium

commit d37e16129d499232d31180839c06f625ec45c5d9

clustermesh: Do not error out of agent when remote cluster is unreachable
The etcd client code contained a log.Fatalf() error message when the session
could not be established after a certain timeout. This would error out of the
agent immediately. This is desired behavior for the main etcd connection but
not for connection to remote clusters.

Return a channel containing the error when session establishment fails to give
the caller a chance to decide how to react to connectivity problems.

Fixes: #6376

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/kvstore/client.go
@@ -22,13 +22,17 @@ var (
 )
 
 func initClient(module backendModule) error {
-	c, err := module.newClient()
-	if err != nil {
-		return err
-	}
+	c, errChan := module.newClient()
+	go func() {
+		err, isErr := <-errChan
+		if isErr {
+			log.WithError(err).Fatalf("Unable to connect to kvstore")
+		}
+
+		deleteLegacyPrefixes()
+	}()
 
 	defaultClient = c
-	go deleteLegacyPrefixes()
 
 	return nil
 }
@@ -39,20 +43,22 @@ func Client() BackendOperations {
 }
 
 // NewClient returns a new kvstore client based on the configuration
-func NewClient(selectedBackend string, opts map[string]string) (BackendOperations, error) {
+func NewClient(selectedBackend string, opts map[string]string) (BackendOperations, chan error) {
+	// Channel used to report immediate errors, module.newClient will
+	// create and return a different channel, caller doesn't need to know
+	errChan := make(chan error, 1)
+	defer close(errChan)
+
 	module := getBackend(selectedBackend)
 	if module == nil {
-		return nil, fmt.Errorf("unknown key-value store type %q. See cilium.link/err-kvstore for details", selectedBackend)
+		errChan <- fmt.Errorf("unknown key-value store type %q. See cilium.link/err-kvstore for details", selectedBackend)
+		return nil, errChan
 	}
 
 	if err := module.setConfig(opts); err != nil {
-		return nil, err
+		errChan <- err
+		return nil, errChan
 	}
 
-	c, err := module.newClient()
-	if err != nil {
-		return nil, err
-	}
-
-	return c, nil
+	return module.newClient()
 }

pkg/clustermesh/remote_cluster.go
@@ -138,11 +138,21 @@ func (rc *remoteCluster) restartRemoteConnection() {
 				}
 				rc.mutex.Unlock()
 
-				backend, err := kvstore.NewClient(kvstore.EtcdBackendName,
+				backend, errChan := kvstore.NewClient(kvstore.EtcdBackendName,
 					map[string]string{
 						kvstore.EtcdOptionConfig: rc.configPath,
 					})
-				if err != nil {
+
+				// Block until either an error is returned or
+				// the channel is closed due to success of the
+				// connection
+				rc.getLogger().Debugf("Waiting for connection to be established")
+				err, isErr := <-errChan
+				if isErr {
+					if backend != nil {
+						backend.Close()
+					}
+					rc.getLogger().WithError(err).Warning("Unable to establish etcd connection to remote cluser")
 					return err
 				}
 

pkg/kvstore/backend.go
@@ -50,7 +50,7 @@ type backendModule interface {
 
 	// newClient must initializes the backend and create a new kvstore
 	// client which implements the BackendOperations interface
-	newClient() (BackendOperations, error)
+	newClient() (BackendOperations, chan error)
 
 	// createInstance creates a new instance of the module
 	createInstance() backendModule

pkg/kvstore/consul.go
@@ -104,7 +104,17 @@ func (c *consulModule) getConfig() map[string]string {
 	return getOpts(c.opts)
 }
 
-func (c *consulModule) newClient() (BackendOperations, error) {
+func (c *consulModule) newClient() (BackendOperations, chan error) {
+	errChan := make(chan error, 1)
+	backend, err := c.connectConsulClient()
+	if err != nil {
+		errChan <- err
+	}
+	close(errChan)
+	return backend, errChan
+}
+
+func (c *consulModule) connectConsulClient() (BackendOperations, error) {
 	if c.config == nil {
 		consulAddr, consulAddrSet := c.opts[optAddress]
 		configPathOpt, configPathOptSet := c.opts[consulOptionConfig]

pkg/kvstore/etcd.go
@@ -45,11 +45,6 @@ type etcdModule struct {
 }
 
 var (
-	// etcdVersionMismatchFatal defines whether the agent should die
-	// immediately if an etcd not matching the minimal version is
-	// encountered.
-	etcdVersionMismatchFatal = true
-
 	// versionCheckTimeout is the time we wait trying to verify the version
 	// of an etcd endpoint. The timeout can be encountered on network
 	// connectivity problems.
@@ -109,20 +104,25 @@ func (e *etcdModule) getConfig() map[string]string {
 	return getOpts(e.opts)
 }
 
-func (e *etcdModule) newClient() (BackendOperations, error) {
+func (e *etcdModule) newClient() (BackendOperations, chan error) {
+	errChan := make(chan error, 10)
+
 	endpointsOpt, endpointsSet := e.opts[addrOption]
 	configPathOpt, configSet := e.opts[EtcdOptionConfig]
 	configPath := ""
 
 	if e.config == nil {
 		if !endpointsSet && !configSet {
-			return nil, fmt.Errorf("invalid etcd configuration, %s or %s must be specified",
-				EtcdOptionConfig, addrOption)
+			errChan <- fmt.Errorf("invalid etcd configuration, %s or %s must be specified", EtcdOptionConfig, addrOption)
+			close(errChan)
+			return nil, errChan
 		}
 
 		if endpointsOpt.value == "" && configPathOpt.value == "" {
-			return nil, fmt.Errorf("invalid etcd configuration, %s or %s must be specified",
+			errChan <- fmt.Errorf("invalid etcd configuration, %s or %s must be specified",
 				EtcdOptionConfig, addrOption)
+			close(errChan)
+			return nil, errChan
 		}
 
 		e.config = &client.Config{}
@@ -136,7 +136,15 @@ func (e *etcdModule) newClient() (BackendOperations, error) {
 		}
 	}
 
-	return newEtcdClient(e.config, configPath)
+	// connectEtcdClient will close errChan when the connection attempt has
+	// been successful
+	backend, err := connectEtcdClient(e.config, configPath, errChan)
+	if err != nil {
+		errChan <- err
+		close(errChan)
+	}
+
+	return backend, errChan
 }
 
 func init() {
@@ -227,7 +235,7 @@ func (e *etcdClient) renewSession() error {
 	return nil
 }
 
-func newEtcdClient(config *client.Config, cfgPath string) (BackendOperations, error) {
+func connectEtcdClient(config *client.Config, cfgPath string, errChan chan error) (BackendOperations, error) {
 	if cfgPath != "" {
 		cfg, err := clientyaml.NewConfig(cfgPath)
 		if err != nil {
@@ -248,7 +256,7 @@ func newEtcdClient(config *client.Config, cfgPath string) (BackendOperations, er
 	log.WithFields(logrus.Fields{
 		"endpoints": config.Endpoints,
 		"config":    cfgPath,
-	}).Info("Waiting for etcd client to be ready")
+	}).Info("Connecting to etcd server...")
 
 	var s concurrency.Session
 	firstSession := make(chan struct{})
@@ -277,6 +285,8 @@ func newEtcdClient(config *client.Config, cfgPath string) (BackendOperations, er
 
 	// wait for session to be created also in parallel
 	go func() {
+		defer close(errChan)
+
 		var session concurrency.Session
 		select {
 		case err = <-errorChan:
@@ -287,7 +297,7 @@ func newEtcdClient(config *client.Config, cfgPath string) (BackendOperations, er
 			err = fmt.Errorf("timed out while waiting for etcd session. Ensure that etcd is running on %s", config.Endpoints)
 		}
 		if err != nil {
-			log.Fatal(err)
+			errChan <- err
 			return
 		}
 
@@ -295,14 +305,13 @@ func newEtcdClient(config *client.Config, cfgPath string) (BackendOperations, er
 		s = session
 		if err != nil {
 			c.Close()
-			err = fmt.Errorf("unable to create default lease: %s", err)
-			log.Fatal(err)
+			errChan <- fmt.Errorf("unable to create default lease: %s", err)
 			return
 		}
 		close(ec.firstSession)
 
 		if err := ec.checkMinVersion(); err != nil {
-			log.Fatalf("Unable to validate etcd version: %s", err)
+			errChan <- fmt.Errorf("Unable to validate etcd version: %s", err)
 		}
 	}()
 

commit 3318724393737c386d7c24531ffabb20db3683c5

pkg/kvstore: set defaultClient before calling deleteLegacyPrefixes
As deleteLegacyPrefixes depends on the defaultClient to not be nil, the
go routine should be scheduled after defaultClient has a client assign
to it.

Fixes: d37e16129d49 ("clustermesh: Do not error out of agent when remote cluster is unreachable")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/client.go
@@ -23,6 +23,9 @@ var (
 
 func initClient(module backendModule) error {
 	c, errChan := module.newClient()
+
+	defaultClient = c
+
 	go func() {
 		err, isErr := <-errChan
 		if isErr {
@@ -31,8 +34,6 @@ func initClient(module backendModule) error {
 
 		deleteLegacyPrefixes()
 	}()
-
-	defaultClient = c
 
 	return nil
 }