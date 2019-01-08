cilium/cilium

commit d95ad2edb5eb4f5748ec85e3d856bab3796a656f

cilium-health: Report ping status to other nodes
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/health/server/server.go
@@ -17,14 +17,18 @@ package server
 import (
 	"time"
 
+	healthModels "github.com/cilium/cilium/api/v1/health/models"
 	healthApi "github.com/cilium/cilium/api/v1/health/server"
 	"github.com/cilium/cilium/api/v1/health/server/restapi"
+	ciliumModels "github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common"
 	ciliumPkg "github.com/cilium/cilium/pkg/client"
 	"github.com/cilium/cilium/pkg/health/defaults"
+	"github.com/cilium/cilium/pkg/lock"
 
 	"github.com/go-openapi/loads"
 	flags "github.com/jessevdk/go-flags"
+	"github.com/sirupsen/logrus"
 )
 
 var (
@@ -33,9 +37,18 @@ var (
 
 // Config stores the configuration data for a cilium-health server.
 type Config struct {
-	Debug     bool
-	CiliumURI string
+	Debug         bool
+	CiliumURI     string
+	ProbeInterval time.Duration
+	ProbeDeadline time.Duration
 }
+
+// ipString is an IP address used as a more descriptive type name in maps.
+type ipString string
+
+// nodeMap maps IP addresses to NodeElements for convenient access to node
+// information.
+type nodeMap map[ipString]*ciliumModels.NodeElement
 
 // Server is the cilium-health daemon that is in charge of performing health
 // and connectivity checks periodically, and serving the cilium-health API.
@@ -45,6 +58,15 @@ type Server struct {
 	Config
 
 	startTime time.Time
+
+	// The lock protects against read and write access to the IP->Node map,
+	// the list of statuses as most recently seen, and the last time a
+	// probe was conducted.
+	lock.RWMutex
+	nodes        nodeMap
+	connectivity []*healthModels.NodeStatus
+	lastProbe    time.Time
+	localStatus  *healthModels.SelfStatus
 }
 
 // DumpUptime returns the time that this server has been running.
@@ -52,11 +74,171 @@ func (s *Server) DumpUptime() string {
 	return time.Since(s.startTime).String()
 }
 
+// getNodes fetches the latest set of nodes in the cluster from the Cilium
+// daemon, and updates the Server's 'nodes' map.
+func (s *Server) getNodes() (nodeMap, error) {
+	scopedLog := logrus.NewEntry(log)
+	if s.CiliumURI != "" {
+		scopedLog = log.WithField("URI", s.CiliumURI)
+	}
+	scopedLog.Debug("Sending request for /healthz ...")
+
+	resp, err := s.Daemon.GetHealthz(nil)
+	if err != nil {
+		log.WithError(err).Warn("Failed to retrieve Cilium /healthz")
+		return nil, err
+	}
+	log.Debug("Got cilium /healthz")
+
+	if resp.Payload.Cluster.Self != "" {
+		s.localStatus = &healthModels.SelfStatus{
+			Name: resp.Payload.Cluster.Self,
+		}
+	}
+
+	nodes := make(nodeMap)
+	for _, n := range resp.Payload.Cluster.Nodes {
+		if n.PrimaryAddress.IPV4 != nil {
+			nodes[ipString(n.PrimaryAddress.IPV4.IP)] = n
+		}
+		if n.PrimaryAddress.IPV6 != nil {
+			nodes[ipString(n.PrimaryAddress.IPV6.IP)] = n
+		}
+		for _, addr := range n.SecondaryAddresses {
+			nodes[ipString(addr.IP)] = n
+		}
+	}
+	return nodes, nil
+}
+
+func (s *Server) updateCluster(nodes nodeMap, connectivity []*healthModels.NodeStatus) {
+	s.Lock()
+	defer s.Unlock()
+
+	s.nodes = nodes
+	s.lastProbe = time.Now()
+	s.connectivity = connectivity
+}
+
+// GetStatusResponse returns the most recent cluster connectivity status.
+func (s *Server) GetStatusResponse() *healthModels.HealthStatusResponse {
+	s.RLock()
+	defer s.RUnlock()
+	return &healthModels.HealthStatusResponse{
+		Local:     s.localStatus,
+		Nodes:     s.connectivity,
+		Timestamp: s.lastProbe.Format(time.RFC3339),
+	}
+}
+
+// FetchStatusResponse updates the cluster with the latest set of nodes,
+// runs a synchronous probe across the cluster, updates the connectivity cache
+// and returns the results.
+func (s *Server) FetchStatusResponse() (*healthModels.HealthStatusResponse, error) {
+	nodes, err := s.getNodes()
+	if err != nil {
+		return nil, err
+	}
+
+	prober := newProber(s, nodes)
+	if err := prober.Run(); err != nil {
+		log.WithError(err).Info("Failed to run ping")
+		return nil, err
+	}
+	log.Debug("Run complete")
+	s.updateCluster(nodes, prober.getResults())
+
+	return s.GetStatusResponse(), nil
+}
+
+// Serve spins up two parallel goroutines:
+// * Prober: Periodically run pings across the cluster at a configured interval
+//   and update the server's connectivity status cache
+// * API Server: Handle API requests.
+func (s *Server) Serve() error {
+	var prober *prober
+
+	// Run it once at the start so we get some initial status
+	s.FetchStatusResponse()
+
+	go func() {
+		for {
+			nodes, err := s.getNodes()
+			if err != nil {
+				time.Sleep(time.Second)
+				continue
+			}
+
+			prober = newProber(s, nodes)
+			prober.MaxRTT = s.ProbeInterval
+			prober.OnIdle = func() {
+				s.updateCluster(prober.getNodes(), prober.getResults())
+				if nodes, err := s.getNodes(); err == nil {
+					prober.setNodes(nodes)
+				}
+			}
+			prober.RunLoop()
+
+			if <-prober.Done() {
+				if err := prober.Err(); err != nil {
+					log.WithError(err).Debug("Restarting prober")
+				}
+			}
+		}
+
+	}()
+
+	err := s.Server.Serve()
+	if prober != nil {
+		prober.Stop()
+	}
+	return err
+}
+
+// newServer instantiates a new instance of the API that serves the health
+// API on the specified port. If tcpPort is 0, then a unix socket is opened
+// which serves the entire API. If a tcpPort is specified, then it returns
+// a server which only answers get requests for the root URL "/".
+func (s *Server) newServer(spec *loads.Document, tcpPort int) *healthApi.Server {
+	api := restapi.NewCiliumHealthAPI(spec)
+	api.Logger = log.Printf
+
+	if tcpPort == 0 {
+		// /healthz/
+		api.GetHealthzHandler = NewGetHealthzHandler(s)
+
+		// /status/
+		// /status/probe/
+		api.ConnectivityGetStatusHandler = NewGetStatusHandler(s)
+		api.ConnectivityPutStatusProbeHandler = NewPutStatusProbeHandler(s)
+	}
+
+	srv := healthApi.NewServer(api)
+	if tcpPort == 0 {
+		srv.EnabledListeners = []string{"unix"}
+		srv.SocketPath = flags.Filename(defaults.SockPath)
+	} else {
+		srv.EnabledListeners = []string{"http"}
+		srv.Port = tcpPort
+		srv.Host = "" // FIXME: Allow binding to specific IPs
+	}
+	srv.ConfigureAPI()
+
+	return srv
+}
+
 // NewServer creates a server to handle health requests.
 func NewServer(config Config) (*Server, error) {
 	server := &Server{
-		startTime: time.Now(),
-		Config:    config,
+		startTime:    time.Now(),
+		Config:       config,
+		nodes:        make(nodeMap),
+		connectivity: []*healthModels.NodeStatus{},
+	}
+
+	cl, err := ciliumPkg.NewClient(config.CiliumURI)
+	if err != nil {
+		return nil, err
 	}
 
 	swaggerSpec, err := loads.Analyzed(healthApi.SwaggerJSON, "")
@@ -64,29 +246,8 @@ func NewServer(config Config) (*Server, error) {
 		return nil, err
 	}
 
-	if cl, err := ciliumPkg.NewClient(config.CiliumURI); err != nil {
-		return nil, err
-	} else {
-		server.Client = cl
-	}
-
-	api := restapi.NewCiliumHealthAPI(swaggerSpec)
-	api.Logger = log.Printf
-
-	// /healthz/
-	api.GetHealthzHandler = NewGetHealthzHandler(server)
-
-	// /status/
-	// /status/probe/
-	// FIXME: Implement /status
-
-	srv := healthApi.NewServer(api)
-	srv.EnabledListeners = []string{"unix"}
-	srv.SocketPath = flags.Filename(defaults.SockPath)
-	// FIXME: Initialize httpServerL
-	// FIXME: Listen on ports 4240+ for Connectivity probes
-	srv.ConfigureAPI()
-	server.Server = *srv
+	server.Client = cl
+	server.Server = *server.newServer(swaggerSpec, 0)
 
 	return server, nil
 }

cilium-health/cmd/root.go
@@ -72,6 +72,10 @@ func init() {
 	flags.BoolP("daemon", "d", false, "Run as a daemon")
 	flags.StringP("host", "H", "", "URI to cilum-health server API")
 	flags.StringP("cilium", "c", "", "URI to Cilium server API")
+	flags.IntP("interval", "i", 60, "Interval (in seconds) for periodic connectivity probes")
+	flags.BoolP("json", "j", false, "Format as JSON")
+	// TODO GH #2083 Hide until all commands support JSON output
+	flags.MarkHidden("json")
 	viper.BindPFlags(flags)
 }
 
@@ -90,8 +94,10 @@ func initConfig() {
 
 	if viper.GetBool("daemon") {
 		config := serverPkg.Config{
-			CiliumURI: viper.GetString("cilium"),
-			Debug:     viper.GetBool("debug"),
+			CiliumURI:     viper.GetString("cilium"),
+			Debug:         viper.GetBool("debug"),
+			ProbeInterval: time.Duration(viper.GetInt("interval")) * time.Second,
+			ProbeDeadline: time.Second,
 		}
 		if srv, err := serverPkg.NewServer(config); err != nil {
 			Fatalf("Error while creating server: %s\n", err)

cilium-health/cmd/status.go
@@ -1,0 +1,126 @@
+// Copyright 2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"text/tabwriter"
+	"time"
+
+	"github.com/cilium/cilium/api/v1/health/models"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	probe   bool
+	verbose bool
+)
+
+func formatConnectivityStatus(w io.Writer, cs *models.ConnectivityStatus, path, indent string) {
+	status := cs.Status
+	if status == "" {
+		latency := time.Duration(cs.Latency)
+		status = fmt.Sprintf("OK, RTT=%s", latency)
+	}
+	fmt.Fprintf(w, "%s%s:\t%s\n", indent, path, status)
+}
+
+func formatPathStatus(w io.Writer, name string, cp *models.PathStatus, indent string) {
+	if cp == nil {
+		if verbose {
+			fmt.Fprintf(w, "%s%s connectivity:\tnil\n", indent, name)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%s%s connectivity to %s:\n", indent, name, cp.IP)
+	indent = fmt.Sprintf("%s  ", indent)
+
+	statuses := map[string]*models.ConnectivityStatus{
+		"ICMP":                      cp.Icmp,
+		"HTTP via L3":               cp.HTTP,
+		"HTTP via L7":               cp.HTTPViaL7,
+		"HTTP via Service Redirect": cp.HTTPViaService,
+		"HTTP via Service with L7":  cp.HTTPViaServiceL7,
+	}
+	for name, status := range statuses {
+		if status != nil {
+			formatConnectivityStatus(w, status, name, indent)
+		}
+	}
+}
+
+// statusGetCmd represents the status command
+var statusGetCmd = &cobra.Command{
+	Use:     "status",
+	Aliases: []string{"connectivity"},
+	Short:   "Display cilium connectivity to other nodes",
+	Run: func(cmd *cobra.Command, args []string) {
+		var sr *models.HealthStatusResponse
+
+		if probe {
+			result, err := client.Connectivity.PutStatusProbe(nil)
+			if err != nil {
+				Fatalf("Cannot get status/probe: %s\n", err)
+			}
+			sr = result.Payload
+		} else {
+			result, err := client.Connectivity.GetStatus(nil)
+			if err != nil {
+				Fatalf("Cannot get status: %s\n", err)
+			}
+			sr = result.Payload
+		}
+
+		if msg, err := json.MarshalIndent(sr, "", "  "); err != nil {
+			Fatalf("Cannot marshal response %s", err.Error())
+		} else if viper.GetBool("json") {
+			w := tabwriter.NewWriter(os.Stdout, 2, 0, 3, ' ', 0)
+			fmt.Fprintf(w, "%s\n", msg)
+			w.Flush()
+		} else {
+			w := tabwriter.NewWriter(os.Stdout, 2, 0, 3, ' ', 0)
+			fmt.Fprintf(w, "Probe time:\t%s\n", sr.Timestamp)
+			fmt.Fprintf(w, "Nodes:\n")
+			for _, node := range sr.Nodes {
+				localStr := ""
+				if sr.Local != nil && node.Name == sr.Local.Name {
+					localStr = " (localhost)"
+				}
+				fmt.Fprintf(w, "  %s%s:\n", node.Name, localStr)
+				formatPathStatus(w, "Host", node.Host.PrimaryAddress, "    ")
+				if verbose && len(node.Host.SecondaryAddresses) > 0 {
+					for _, addr := range node.Host.SecondaryAddresses {
+						formatPathStatus(w, "Secondary", addr, "      ")
+					}
+				}
+				formatPathStatus(w, "Endpoint", node.Endpoint, "    ")
+			}
+			w.Flush()
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusGetCmd)
+	statusGetCmd.Flags().BoolVarP(&probe, "probe", "", false,
+		"Synchronously probe connectivity status")
+	statusGetCmd.Flags().BoolVarP(&verbose, "verbose", "", false,
+		"Print the result verbosely")
+}

pkg/health/server/prober.go
@@ -1,0 +1,254 @@
+// Copyright 2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"net"
+	"strings"
+	"time"
+
+	"github.com/cilium/cilium/api/v1/health/models"
+	ciliumModels "github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logfields"
+
+	"github.com/servak/go-fastping"
+	"github.com/sirupsen/logrus"
+)
+
+type prober struct {
+	*fastping.Pinger
+	server *Server
+
+	// 'stop' is closed upon a call to prober.Stop(). When the stopping is
+	// finished, then prober.Done() will be notified.
+	stop chan bool
+	done chan bool
+
+	// The lock protects multiple requests attempting to update the status
+	// at the same time - ie, serialize updates between the periodic prober
+	// and probes initiated via "GET /status/probe". It is also used to
+	// co-ordinate updates of the ICMP responses and the HTTP responses.
+	lock.RWMutex
+	results map[ipString]*models.PathStatus
+	nodes   nodeMap
+
+	// TODO: If nodes leave the cluster, we will never clear out their
+	//       entries in the 'results' map.
+}
+
+// copyResultRLocked makes a copy of the path status for the specified IP.
+func (p *prober) copyResultRLocked(ip string) *models.PathStatus {
+	status := p.results[ipString(ip)]
+	if status == nil {
+		return nil
+	}
+
+	result := &models.PathStatus{
+		IP: ip,
+	}
+	paths := map[**models.ConnectivityStatus]*models.ConnectivityStatus{
+		&result.Icmp:             status.Icmp,
+		&result.HTTP:             status.HTTP,
+		&result.HTTPViaL7:        status.HTTPViaL7,
+		&result.HTTPViaService:   status.HTTPViaService,
+		&result.HTTPViaServiceL7: status.HTTPViaServiceL7,
+	}
+	for res, value := range paths {
+		if value != nil {
+			*res = &*value
+		}
+	}
+	return result
+}
+
+func getPrimaryIP(node *ciliumModels.NodeElement) string {
+	if node.PrimaryAddress.IPV4.Enabled {
+		return node.PrimaryAddress.IPV4.IP
+	}
+	return node.PrimaryAddress.IPV6.IP
+}
+
+// getResults gathers a copy of all of the results for nodes currently in the
+// cluster.
+func (p *prober) getResults() []*models.NodeStatus {
+	p.RLock()
+	defer p.RUnlock()
+
+	// De-duplicate IPs in 'p.nodes' by building a map based on node.Name.
+	resultMap := map[string]*models.NodeStatus{}
+	for _, node := range p.nodes {
+		if resultMap[node.Name] != nil {
+			continue
+		}
+		primaryIP := getPrimaryIP(node)
+		status := &models.NodeStatus{
+			Name: node.Name,
+			Host: &models.HostStatus{
+				PrimaryAddress: p.copyResultRLocked(primaryIP),
+			},
+			// TODO: Endpoint: &models.PathStatus{},
+		}
+		secondaryResults := []*models.PathStatus{}
+		for _, addr := range node.SecondaryAddresses {
+			if addr.Enabled {
+				secondaryStatus := p.copyResultRLocked(addr.IP)
+				secondaryResults = append(secondaryResults, secondaryStatus)
+			}
+		}
+		status.Host.SecondaryAddresses = secondaryResults
+		resultMap[node.Name] = status
+	}
+
+	result := []*models.NodeStatus{}
+	for _, res := range resultMap {
+		result = append(result, res)
+	}
+	return result
+}
+
+func (p *prober) getNodes() nodeMap {
+	p.RLock()
+	defer p.RUnlock()
+	return p.nodes
+}
+
+func isIPv4(ip string) bool {
+	netIP := net.ParseIP(ip)
+	return netIP != nil && !strings.Contains(ip, ":")
+}
+
+func skipAddress(elem *ciliumModels.NodeAddressingElement) bool {
+	return elem == nil || !elem.Enabled || elem.IP == "<nil>"
+}
+
+// getAddresses returns a map of the node's addresses -> "primary" bool
+func getNodeAddresses(node *ciliumModels.NodeElement) map[*ciliumModels.NodeAddressingElement]bool {
+	addresses := map[*ciliumModels.NodeAddressingElement]bool{
+		node.PrimaryAddress.IPV4: node.PrimaryAddress.IPV4.Enabled,
+		node.PrimaryAddress.IPV6: node.PrimaryAddress.IPV6.Enabled,
+	}
+	for _, elem := range node.SecondaryAddresses {
+		addresses[elem] = false
+	}
+	return addresses
+}
+
+// setNodes sets the list of nodes for the prober, and updates the pinger to
+// start sending pings to all of the nodes.
+func (p *prober) setNodes(nodes nodeMap) {
+	p.Lock()
+	defer p.Unlock()
+	p.nodes = nodes
+
+	for _, n := range nodes {
+		for elem, primary := range getNodeAddresses(n) {
+			if skipAddress(elem) {
+				continue
+			}
+
+			network := "ip6:icmp"
+			if isIPv4(elem.IP) {
+				network = "ip4:icmp"
+			}
+			scopedLog := log.WithFields(logrus.Fields{
+				logfields.NodeName: n.Name,
+				logfields.IPAddr:   elem.IP,
+				"primary":          primary,
+			})
+
+			result := &models.ConnectivityStatus{}
+			ra, err := net.ResolveIPAddr(network, elem.IP)
+			if err == nil {
+				scopedLog.Debug("Probing for connectivity to node")
+				result.Status = "Connection timed out"
+				p.AddIPAddr(ra)
+			} else {
+				scopedLog.Debug("Skipping probe for node")
+				result.Status = "Failed to resolve IP"
+			}
+
+			ip := ipString(elem.IP)
+			if p.results[ip] == nil {
+				p.results[ip] = &models.PathStatus{
+					IP: elem.IP,
+				}
+			}
+			p.results[ip].Icmp = result
+		}
+	}
+}
+
+// Run sends a single probes out to all of the other cilium nodes to gather
+// connectivity status for the cluster.
+func (p *prober) Run() error {
+	// TODO: Launch goroutines to probe HTTP ports on other nodes
+
+	err := p.Pinger.Run()
+	// TODO: For each other probe, fetch error and return errors in
+	//       priority of p.Pinger(), then connect(), etc.?
+	return err
+}
+
+// RunLoop periodically sends probes out to all of the other cilium nodes to
+// gather connectivity status for the cluster.
+//
+// This is a non-blocking method so it immediately returns. If you want to
+// stop sending packets, call Done().
+func (p *prober) RunLoop() {
+	// FIXME: Spread the probes out across the probing interval
+	p.Pinger.RunLoop()
+}
+
+// newPinger prepares a prober. The caller may invoke one the Run* methods of
+// the prober to populate its 'results' map.
+func newProber(s *Server, nodes nodeMap) *prober {
+	prober := &prober{
+		Pinger:  fastping.NewPinger(),
+		server:  s,
+		results: make(map[ipString]*models.PathStatus),
+		nodes:   nodes,
+	}
+	prober.MaxRTT = s.ProbeDeadline
+
+	prober.setNodes(nodes)
+	prober.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		prober.RLock()
+		node := prober.nodes[ipString(addr.String())]
+		prober.RUnlock()
+
+		scopedLog := log.WithFields(logrus.Fields{
+			logfields.IPAddr: addr,
+			"rtt":            rtt,
+		})
+		if node == nil {
+			scopedLog.Debugf("Node disappeared, skip result")
+			return
+		}
+
+		prober.Lock()
+		prober.results[ipString(addr.String())].Icmp = &models.ConnectivityStatus{
+			Latency: rtt.Nanoseconds(),
+			Status:  "",
+		}
+		prober.Unlock()
+
+		scopedLog.WithFields(logrus.Fields{
+			logfields.NodeName: node.Name,
+		}).Debugf("Probe successful")
+	}
+
+	return prober
+}

pkg/health/server/status.go
@@ -1,0 +1,58 @@
+// Copyright 2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	. "github.com/cilium/cilium/api/v1/health/server/restapi/connectivity"
+	"github.com/cilium/cilium/pkg/apierror"
+
+	"github.com/go-openapi/runtime/middleware"
+)
+
+type getStatusCache struct {
+	*Server
+}
+
+type putStatusProbe struct {
+	*Server
+}
+
+// NewGetStatusHandler handles requests for cached connectivity status.
+func NewGetStatusHandler(s *Server) GetStatusHandler {
+	return &getStatusCache{Server: s}
+}
+
+// NewPutStatusProbeHandler handles requests for connectivity probes.
+func NewPutStatusProbeHandler(s *Server) PutStatusProbeHandler {
+	return &putStatusProbe{Server: s}
+}
+
+// Handle handles GET requests for /status .
+func (h *getStatusCache) Handle(params GetStatusParams) middleware.Responder {
+	log.Debug("Handling request for /status")
+
+	return NewGetStatusOK().WithPayload(h.GetStatusResponse())
+}
+
+// Handle handles GET requests for /status/probe .
+func (h *putStatusProbe) Handle(params PutStatusProbeParams) middleware.Responder {
+	log.Debug("Handling request for /status/probe")
+
+	status, err := h.FetchStatusResponse()
+	if err != nil {
+		return apierror.Error(PutStatusProbeFailedCode, err)
+	}
+	return NewPutStatusProbeOK().WithPayload(status)
+}

commit 634c5fc3034823f713125ced927d373f31958147

pkg/health: protect local variable against concurrent writes
```
==================
WARNING: DATA RACE
Write at 0x00c0004c8590 by goroutine 26:
  github.com/cilium/cilium/pkg/health/server.(*Server).getNodes()
      /go/src/github.com/cilium/cilium/pkg/health/server/server.go:122 +0xa18
  github.com/cilium/cilium/pkg/health/server.(*Server).FetchStatusResponse()
      /go/src/github.com/cilium/cilium/pkg/health/server/server.go:189 +0x3f
  github.com/cilium/cilium/pkg/health/server.(*putStatusProbe).Handle()
      /go/src/github.com/cilium/cilium/pkg/health/server/status.go:53 +0xab
  github.com/cilium/cilium/api/v1/health/server/restapi/connectivity.(*PutStatusProbe).ServeHTTP()
      /go/src/github.com/cilium/cilium/api/v1/health/server/restapi/connectivity/put_status_probe.go:59 +0x2b1
  github.com/cilium/cilium/vendor/github.com/go-openapi/runtime/middleware.NewOperationExecutor.func1()
      /go/src/github.com/cilium/cilium/vendor/github.com/go-openapi/runtime/middleware/operation.go:28 +0xb3
  net/http.HandlerFunc.ServeHTTP()
      /usr/local/go/src/net/http/server.go:1964 +0x51
  github.com/cilium/cilium/vendor/github.com/go-openapi/runtime/middleware.NewRouter.func1()
      /go/src/github.com/cilium/cilium/vendor/github.com/go-openapi/runtime/middleware/router.go:73 +0x440
  net/http.HandlerFunc.ServeHTTP()
      /usr/local/go/src/net/http/server.go:1964 +0x51
  github.com/cilium/cilium/vendor/github.com/go-openapi/runtime/middleware.Redoc.func1()
      /go/src/github.com/cilium/cilium/vendor/github.com/go-openapi/runtime/middleware/redoc.go:72 +0xff
  net/http.HandlerFunc.ServeHTTP()
      /usr/local/go/src/net/http/server.go:1964 +0x51
  github.com/cilium/cilium/vendor/github.com/go-openapi/runtime/middleware.Spec.func1()
      /go/src/github.com/cilium/cilium/vendor/github.com/go-openapi/runtime/middleware/spec.go:45 +0xee
  net/http.HandlerFunc.ServeHTTP()
      /usr/local/go/src/net/http/server.go:1964 +0x51
  net/http.serverHandler.ServeHTTP()
      /usr/local/go/src/net/http/server.go:2741 +0xc4
  net/http.(*conn).serve()
      /usr/local/go/src/net/http/server.go:1847 +0x80a

Previous write at 0x00c0004c8590 by goroutine 58:
  github.com/cilium/cilium/pkg/health/server.(*Server).getNodes()
      /go/src/github.com/cilium/cilium/pkg/health/server/server.go:122 +0xa18
  github.com/cilium/cilium/pkg/health/server.(*Server).runActiveServices.func1()
      /go/src/github.com/cilium/cilium/pkg/health/server/server.go:220 +0x6f
  github.com/cilium/cilium/vendor/github.com/servak/go-fastping.(*Pinger).run()
      /go/src/github.com/cilium/cilium/vendor/github.com/servak/go-fastping/fastping.go:454 +0xa33

Goroutine 26 (running) created at:
  net/http.(*Server).Serve()
      /usr/local/go/src/net/http/server.go:2851 +0x4c5
  github.com/cilium/cilium/vendor/github.com/tylerb/graceful.(*Server).Serve()
      /go/src/github.com/cilium/cilium/vendor/github.com/tylerb/graceful/graceful.go:307 +0x464
  github.com/cilium/cilium/api/v1/health/server.(*Server).Serve.func1()
      /go/src/github.com/cilium/cilium/api/v1/health/server/server.go:177 +0x80

Goroutine 58 (running) created at:
  github.com/cilium/cilium/vendor/github.com/servak/go-fastping.(*Pinger).RunLoop()
      /go/src/github.com/cilium/cilium/vendor/github.com/servak/go-fastping/fastping.go:362 +0x199
  github.com/cilium/cilium/pkg/health/server.(*prober).RunLoop()
      /go/src/github.com/cilium/cilium/pkg/health/server/prober.go:354 +0x4b
  github.com/cilium/cilium/pkg/health/server.(*Server).runActiveServices()
      /go/src/github.com/cilium/cilium/pkg/health/server/server.go:224 +0x1ab
  github.com/cilium/cilium/pkg/health/server.(*Server).Serve.func2()
      /go/src/github.com/cilium/cilium/pkg/health/server/server.go:251 +0x38
==================
```

Fixes: d95ad2edb5eb ("cilium-health: Report ping status to other nodes")

pkg/health/server/server.go
@@ -119,9 +119,11 @@ func (s *Server) getNodes() (nodeMap, error) {
 	}
 
 	if resp.Payload.Cluster.Self != "" {
+		s.RWMutex.Lock()
 		s.localStatus = &healthModels.SelfStatus{
 			Name: resp.Payload.Cluster.Self,
 		}
+		s.RWMutex.Unlock()
 	}
 
 	nodes := make(nodeMap)