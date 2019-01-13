cilium/cilium

commit 4ecf111c35f047f490f5cc8858d23c15e342541f

agent: Fix temporary corruption of BPF endpoint map on restart
Due to deleting and repopulating the BPF endpoint map on startup. Cilium agent
restarts would introduce a race window of a couple of seconds [0] in which
local endpoints would not be reachable. This would result in packet drops or
depending on the routing path, a routing loop which eventually leads to a drop
followed by a ICMP TTL exceeded.

[0] The race window for host IPs was in the microseconds range. The race
window for endpoints depended on the number of local endpoints and etcd
latency as the endpoint would reappear as it was regenerated on restore.

Fixes: #5651

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/state.go
@@ -17,6 +17,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net"
 	"path/filepath"
 	"strings"
 
@@ -28,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
+	"github.com/cilium/cilium/pkg/maps/lxcmap"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/workloads"
@@ -62,6 +64,11 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 	}
 
 	log.Info("Restoring endpoints from former life...")
+
+	existingEndpoints, err := lxcmap.DumpToMap()
+	if err != nil {
+		return nil, err
+	}
 
 	dirFiles, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -122,12 +129,25 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 		ep.Unlock()
 
 		state.restored = append(state.restored, ep)
+
+		delete(existingEndpoints, ep.IPv4.String())
+		delete(existingEndpoints, ep.IPv6.String())
 	}
 
 	log.WithFields(logrus.Fields{
 		"count.restored": len(state.restored),
 		"count.total":    len(possibleEPs),
 	}).Info("Endpoints restored")
+
+	for hostIP, info := range existingEndpoints {
+		if ip := net.ParseIP(hostIP); !info.IsHost() && ip != nil {
+			if err := lxcmap.DeleteEntry(ip); err != nil {
+				log.WithError(err).Warn("Unable to delete obsolete endpoint from BPF map")
+			} else {
+				log.Debugf("Removed outdated endpoint %d from endpoint map", info.LxcID)
+			}
+		}
+	}
 
 	return state, nil
 }

pkg/maps/lxcmap/lxcmap.go
@@ -136,6 +136,11 @@ func NewEndpointKey(ip net.IP) *EndpointKey {
 	}
 }
 
+// IsHost returns true if the EndpointInfo represents a host IP
+func (v *EndpointInfo) IsHost() bool {
+	return v.Flags&EndpointFlagHost != 0
+}
+
 // String returns the human readable representation of an EndpointInfo
 func (v *EndpointInfo) String() string {
 	if v.Flags&EndpointFlagHost != 0 {
@@ -205,4 +210,22 @@ func DeleteElement(f EndpointFrontend) []error {
 	}
 
 	return errors
+}
+
+// DumpToMap dumps the contents of the lxcmap into a map and returns it
+func DumpToMap() (map[string]*EndpointInfo, error) {
+	m := map[string]*EndpointInfo{}
+	callback := func(key bpf.MapKey, value bpf.MapValue) {
+		if info, ok := value.(*EndpointInfo); ok {
+			if endpointKey, ok := key.(*EndpointKey); ok {
+				m[endpointKey.IP.String()] = info
+			}
+		}
+	}
+
+	if err := LXCMap.DumpWithCallback(callback); err != nil {
+		return m, fmt.Errorf("unable to read BPF endpoint list: %s", err)
+	}
+
+	return m, nil
 }

daemon/daemon.go
@@ -798,11 +798,6 @@ func (d *Daemon) init() error {
 			return err
 		}
 
-		// Clean all endpoint entries
-		if err := lxcmap.LXCMap.DeleteAll(); err != nil {
-			return err
-		}
-
 		// Set up the list of IPCache listeners in the daemon, to be
 		// used by syncLXCMap().
 		ipcache.IPIdentityCache.SetListeners([]ipcache.IPIdentityMappingListener{
@@ -879,6 +874,10 @@ func (d *Daemon) init() error {
 					return err
 				}
 			}
+
+			// If we are not restoring state, all endpoints can be
+			// deleted. Entries will be re-populated.
+			lxcmap.LXCMap.DeleteAll()
 		}
 	}
 
@@ -969,7 +968,6 @@ func createNodeConfigHeaderfile() error {
 func (d *Daemon) syncLXCMap() error {
 	// TODO: Update addresses first, in case node addressing has changed.
 	// TODO: Once these start changing on runtime, figure out the locking strategy.
-	// TODO: Delete stale host entries from the lxcmap.
 	specialIdentities := []identity.IPIdentityPair{
 		{
 			IP: node.GetInternalIPv4(),
@@ -1009,6 +1007,11 @@ func (d *Daemon) syncLXCMap() error {
 		},
 	}
 
+	existingEndpoints, err := lxcmap.DumpToMap()
+	if err != nil {
+		return err
+	}
+
 	for _, ipIDPair := range specialIdentities {
 		isHost := ipIDPair.ID == identity.ReservedIdentityHost
 		if isHost {
@@ -1020,6 +1023,9 @@ func (d *Daemon) syncLXCMap() error {
 				log.WithField(logfields.IPAddr, ipIDPair.IP).Debugf("Added local ip to endpoint map")
 			}
 		}
+
+		delete(existingEndpoints, ipIDPair.IP.String())
+
 		// Upsert will not propagate (reserved:foo->ID) mappings across the cluster,
 		// and we specifically don't want to do so.
 		ipcache.IPIdentityCache.Upsert(ipIDPair.PrefixString(), nil, ipcache.Identity{
@@ -1027,6 +1033,17 @@ func (d *Daemon) syncLXCMap() error {
 			Source: ipcache.FromAgentLocal,
 		})
 	}
+
+	for hostIP, info := range existingEndpoints {
+		if ip := net.ParseIP(hostIP); info.IsHost() && ip != nil {
+			if err := lxcmap.DeleteEntry(ip); err != nil {
+				log.WithError(err).Warn("Unable to delete obsolete host IP from BPF map")
+			} else {
+				log.Debugf("Removed outdated host ip %s from endpoint map", hostIP)
+			}
+		}
+	}
+
 	return nil
 }
 

commit 197bcf79dff0c4093ea68da124ecfbc984d125e9

agent: Fix endpoint restore with unmounted BPF filesystem
The function Daemon.restoreOldEndpoints() had errored out when the endpoint BPF
map could not be opened. This would prevent restoring of endpoints when the BPF
filesystem was not mounted. The error does not matter if we can't open the map
as the map will be initialized from scratch and no cleaning up of old
potentially stale entries is necessary.

Fixes: 4ecf111c35f ("agent: Fix temporary corruption of BPF endpoint map on restart")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/state.go
@@ -72,7 +72,7 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 
 	existingEndpoints, err := lxcmap.DumpToMap()
 	if err != nil {
-		return state, err
+		log.WithError(err).Warning("Unable to open endpoint map while restoring. Skipping cleanup of endpoint map on startup")
 	}
 
 	dirFiles, err := ioutil.ReadDir(dir)
@@ -144,8 +144,10 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 
 		state.restored = append(state.restored, ep)
 
-		delete(existingEndpoints, ep.IPv4.String())
-		delete(existingEndpoints, ep.IPv6.String())
+		if existingEndpoints != nil {
+			delete(existingEndpoints, ep.IPv4.String())
+			delete(existingEndpoints, ep.IPv6.String())
+		}
 	}
 
 	log.WithFields(logrus.Fields{
@@ -153,12 +155,14 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 		"count.total":    len(possibleEPs),
 	}).Info("Endpoints restored")
 
-	for hostIP, info := range existingEndpoints {
-		if ip := net.ParseIP(hostIP); !info.IsHost() && ip != nil {
-			if err := lxcmap.DeleteEntry(ip); err != nil {
-				log.WithError(err).Warn("Unable to delete obsolete endpoint from BPF map")
-			} else {
-				log.Debugf("Removed outdated endpoint %d from endpoint map", info.LxcID)
+	if existingEndpoints != nil {
+		for hostIP, info := range existingEndpoints {
+			if ip := net.ParseIP(hostIP); !info.IsHost() && ip != nil {
+				if err := lxcmap.DeleteEntry(ip); err != nil {
+					log.WithError(err).Warn("Unable to delete obsolete endpoint from BPF map")
+				} else {
+					log.Debugf("Removed outdated endpoint %d from endpoint map", info.LxcID)
+				}
 			}
 		}
 	}

pkg/maps/lxcmap/lxcmap.go
@@ -220,7 +220,7 @@ func DumpToMap() (map[string]*EndpointInfo, error) {
 	}
 
 	if err := LXCMap.DumpWithCallback(callback); err != nil {
-		return m, fmt.Errorf("unable to read BPF endpoint list: %s", err)
+		return nil, fmt.Errorf("unable to read BPF endpoint list: %s", err)
 	}
 
 	return m, nil