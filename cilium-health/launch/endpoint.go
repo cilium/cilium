cilium/cilium

commit 74d2ce2f5d8111ebf30fe8948b56bba64e885254

agent: Fix endpoint removal when createEndpoint() fails
This fixes two scenarios where a failure in createEndpoint() can lead to the
controller created in endpoint.UpdateLabels() to be leaked due to lack of
invoking `d.deleteEndpoint()`. This had resulted in an endpoint to exist,
invisible in the endpoint list that would be continuously rebuilt via the
identity controller.

1. Endpoint build triggered via AddEndpoint() fails
2. Client context timeout causes exit after UpdateLabels()

Fixes: 186b5e93a84 ("daemon: do not add endpoint if client connection closes during add operation")

Signed-off-by: Thomas Graf <thomas@cilium.io>

cilium-health/launch/endpoint.go
@@ -262,9 +262,21 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 		return fmt.Errorf("Error while configuring routes: %s", err)
 	}
 
-	// Add the endpoint
 	if err := endpointmanager.AddEndpoint(owner, ep, "Create cilium-health endpoint"); err != nil {
 		return fmt.Errorf("Error while adding endpoint: %s", err)
+	}
+
+	if !ep.SetStateLocked(endpoint.StateWaitingToRegenerate, "initial build of health endpoint") {
+		endpointmanager.Remove(ep)
+		return fmt.Errorf("unable to transition health endpoint to WaitingToRegenerate state")
+	}
+
+	buildSuccessful := <-ep.Regenerate(owner, &endpoint.ExternalRegenerationMetadata{
+		Reason: "health daemon bootstrap",
+	})
+	if !buildSuccessful {
+		endpointmanager.Remove(ep)
+		return fmt.Errorf("unable to build health endpoint")
 	}
 
 	// Propagate health IPs to all other nodes via annotations

daemon/endpoint.go
@@ -211,19 +211,37 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 		}
 	}
 
+	if err := endpointmanager.AddEndpoint(d, ep, "Create endpoint from API PUT"); err != nil {
+		log.WithError(err).Warn("Aborting endpoint join")
+		return PutEndpointIDFailedCode, err
+	}
+
 	ep.UpdateLabels(d, addLabels, infoLabels, true)
 
-	// Do not add endpoint to the endpoint manager if client abort connection
 	select {
 	case <-ctx.Done():
 		log.WithError(ctx.Err()).Warn("Aborting endpoint join due client connection closed")
+		d.deleteEndpoint(ep)
 		return PutEndpointIDFailedCode, fmt.Errorf("aborting endpoint %d join due client connection closed", ep.ID)
 	default:
 	}
 
-	if err := endpointmanager.AddEndpoint(d, ep, "Create endpoint from API PUT"); err != nil {
-		log.WithError(err).Warn("Aborting endpoint join")
-		return PutEndpointIDFailedCode, err
+	if err := ep.LockAlive(); err != nil {
+		d.deleteEndpoint(ep)
+		return PutEndpointIDFailedCode, fmt.Errorf("endpoint was deleted while waiting for the identity")
+	}
+
+	build := ep.GetStateLocked() == endpoint.StateReady
+	if build {
+		ep.SetStateLocked(endpoint.StateWaitingToRegenerate, "Identity is known at endpoint creation time")
+	}
+	ep.Unlock()
+
+	if build {
+		if err := ep.RegenerateWait(d, "Initial build on endpoint creation"); err != nil {
+			d.deleteEndpoint(ep)
+			return PutEndpointIDFailedCode, fmt.Errorf("failed to build the endpoint: %s", err)
+		}
 	}
 
 	// Only used for CRI-O since it does not support events.

pkg/endpointmanager/manager.go
@@ -98,6 +98,8 @@ func Insert(ep *endpoint.Endpoint) error {
 		EndpointSynchronizer.RunK8sCiliumEndpointSync(ep)
 	}
 
+	ep.InsertEvent()
+
 	return nil
 }
 
@@ -378,36 +380,7 @@ func AddEndpoint(owner endpoint.Owner, ep *endpoint.Endpoint, reason string) (er
 		return fmt.Errorf("Endpoint ID is already set to %d", ep.ID)
 	}
 
-	// Regenerate immediately if ready or waiting for identity
-	if err := ep.LockAlive(); err != nil {
-		return err
-	}
-	build := false
-	state := ep.GetStateLocked()
-
-	// We can only trigger regeneration of endpoints if the endpoint is in a
-	// state where it can regenerate. See endpoint.SetStateLocked().
-	if state == endpoint.StateReady {
-		ep.SetStateLocked(endpoint.StateWaitingToRegenerate, reason)
-		build = true
-	}
-
-	ep.Unlock()
-
-	if err := Insert(ep); err != nil {
-		return err
-	}
-
-	if build {
-		if err := ep.RegenerateWait(owner, reason); err != nil {
-			Remove(ep)
-			return err
-		}
-	}
-
-	ep.InsertEvent()
-
-	return nil
+	return Insert(ep)
 }
 
 // WaitForEndpointsAtPolicyRev waits for all endpoints which existed at the time

commit 70075ff309ccd377691378ce974806e247f1d769

health: acquire lock to set state to WaitingToRegenerate in LaunchAsEndpoint
\`SetStateLocked\` requires the endpoint mutex be held, so acquire it when
bootstrapping the health endpoint.

Fixes: 74d2ce2 ("agent: Fix endpoint removal when createEndpoint() fails")

Signed-off by: Ian Vernon <ian@cilium.io>

cilium-health/launch/endpoint.go
@@ -266,10 +266,15 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 		return fmt.Errorf("Error while adding endpoint: %s", err)
 	}
 
+	if err := ep.LockAlive(); err != nil {
+		return err
+	}
 	if !ep.SetStateLocked(endpoint.StateWaitingToRegenerate, "initial build of health endpoint") {
 		endpointmanager.Remove(ep)
+		ep.Unlock()
 		return fmt.Errorf("unable to transition health endpoint to WaitingToRegenerate state")
 	}
+	ep.Unlock()
 
 	buildSuccessful := <-ep.Regenerate(owner, &endpoint.ExternalRegenerationMetadata{
 		Reason: "health daemon bootstrap",