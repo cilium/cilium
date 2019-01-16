fa662ebe281e57aae2df7d3319c517961f99f8ae
André Martins <andre@cilium.io>
2019-01-16 18:03:40 +0100

    add helper to wait for cilium to be scheduled and then start flannel
    Signed-off-by: André Martins <andre@cilium.io>

--- test/helpers/kubectl.go
+++ test/helpers/kubectl.go
@@ -583,6 +583,47 @@ func (kub *Kubectl) WaitforPods(namespace string, filter string, timeout time.Du
 	return kub.WaitforNPods(namespace, filter, 0, timeout)
 }
 
+// WaitForPodsRunning waits for all pods of that particular namespace with the
+// given filter to be ready on all nodes.
+func (kub *Kubectl) WaitForPodsRunning(namespace, filter string, minPodsScheduled int, timeout time.Duration) error {
+	body := func() bool {
+
+		podList := &v1.PodList{}
+		err := kub.GetPods("kube-system", "-l k8s-app=cilium").Unmarshal(podList)
+		if err != nil {
+			kub.logger.Infof("Error while getting PodList: %s", err)
+			return false
+		}
+		if len(podList.Items) == 0 {
+			return false
+		}
+		currScheduled := 0
+		for _, pod := range podList.Items {
+			if pod.Status.Phase == v1.PodRunning {
+				currScheduled++
+			}
+		}
+
+		if currScheduled >= minPodsScheduled {
+			return true
+		}
+
+		kub.logger.WithFields(logrus.Fields{
+			"namespace":     namespace,
+			"filter":        filter,
+			"data":          podList,
+			"currScheduled": currScheduled,
+			"minRequired":   minPodsScheduled,
+		}).Info("WaitForPodsRunning: pods are not ready")
+
+		return false
+	}
+	return WithTimeout(
+		body,
+		fmt.Sprintf("timed out waiting for pods of daemon set with filter %q to be scheduled", filter),
+		&TimeoutConfig{Timeout: timeout})
+}
+
 // WaitforNPods waits up until timeout seconds have elapsed for at least
 // minRequired pods in the specified namespace that match the provided JSONPath
 // filter to have their containterStatuses equal to "ready". Returns true if all
@@ -1746,9 +1787,13 @@ func (kub *Kubectl) CiliumPreFlightCheck() error {
 	// nodes cannot be show up yet, and the cilium-health can fail as a false positive.
 	var err error
 	body := func() bool {
-		err = kub.CiliumStatusPreFlightCheck()
-		if err != nil {
-			return false
+		switch GetCurrentIntegration() {
+		case CIIntegrationFlannel:
+		default:
+			err = kub.CiliumStatusPreFlightCheck()
+			if err != nil {
+				return false
+			}
 		}
 		err = kub.CiliumControllersPreFlightCheck()
 		if err != nil {
