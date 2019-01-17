d346caa53f68017ab5f4c6b0ba64fb8c0ca217f7
John Fastabend <john.fastabend@gmail.com>
2019-01-17 18:59:44 +0000

    cilium: ipsec, add go API to configure xfrm (IPSec)
    [ upstream commit b2b901fb191634fe971bdec4f24f1a145f62b923 ]
    
    This adds a new datapath directory ipsec to configure ipsec for Cilium.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

---
+++ pkg/datapath/linux/linux_defaults/linux_defaults.go
@@ -0,0 +1,32 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package linux_defaults
+
+// Linux specific constants used in Linux datapath
+const (
+	// RouteTableIPSec is the default table ID to use for IPSec routing rules
+	RouteTableIPSec = 200
+
+	// RouteMarkDecrypt is the default route mark to use to indicate datapath
+	// needs to decrypt a packet.
+	RouteMarkDecrypt = 0x0D00
+
+	// RouteMarkEncrypt is the default route mark to use to indicate datapath
+	// needs to encrypt a packet.
+	RouteMarkEncrypt = 0x0E00
+
+	// RouteMarkMask is the mask required for the route mark value
+	RouteMarkMask = 0xF00
+)
