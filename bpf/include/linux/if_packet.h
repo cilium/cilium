99f10bd1de1e342d5fc75a52266bc255772b7ead
John Fastabend <john.fastabend@gmail.com>
2019-01-18 16:50:00 +0000

    cilium: ipsec, bpf add receive side ipsec
    If we did nothing here cilium_vxlan will pop the vxlan header then
    the outer header will route to cilium_host (assuming tunnel mode)
    this route would hit the ipsec decrypt path so cilium host would
    get decrypted packet. These would process using normal host logic
    and be locally redirected to lxc endpoint via BPF program. However,
    when mixed with non-ipsec traffic some flows were getting
    encrypted/decrypted unexpectedly.
    
    So instead of hopping through multiple BPF programs this changes the
    flow as follows. Set the mark decrypt bit and pass to stack. The
    routing stack decrypts the packet and routes it back to cilium
    vxlan after decryption is complete. After this cilium_vxlan "sees"
    the decrypted bit is set and does redirect to lxc. We keep the
    ipsec traffic localized this way. IMO its easier to trace the
    flow.
    
    We had to pull in a portion of if_packet to get magic numbers
    for PACKET_* values. Only pulled in required defines for now.
    
    Finally, in the next set of patches we drop the tunnel mode flag
    and this becomes even more relavent to avoid (re)routing.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

---
+++ bpf/include/linux/if_packet.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef __LINUX_IF_PACKET_H
+#define __LINUX_IF_PACKET_H
+
+#define PACKET_HOST		0		/* To us		*/
+#define PACKET_BROADCAST	1		/* To all		*/
+#define PACKET_MULTICAST	2		/* To group		*/
+#define PACKET_OTHERHOST	3		/* To someone else 	*/
+#define PACKET_OUTGOING		4		/* Outgoing of any type */
+#define PACKET_LOOPBACK		5		/* MC/BRD frame looped back */
+#define PACKET_USER		6		/* To user space	*/
+#define PACKET_KERNEL		7		/* To kernel space	*/
+/* Unused, PACKET_FASTROUTE and PACKET_LOOPBACK are invisible to user space */
+#define PACKET_FASTROUTE	6		/* Fastrouted frame	*/
+
+#endif // __LINUX_IF_PACKET_H
