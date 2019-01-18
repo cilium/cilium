cilium/cilium

commit a5b011fb4891d7380c1f639717b3fa017fffa002

plugins/cni: add installation script for policy enforcement mode
When POLICY_ENFORCEMENT_INTERFACE is set, the cni configuration that
chains flannel with Cilium will be installed and uninstalled.

Signed-off-by: André Martins <andre@cilium.io>

plugins/cilium-cni/cni-install.sh
@@ -3,7 +3,15 @@
 set -eu
 
 HOST_PREFIX=${HOST_PREFIX:-/host}
-CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conf}
+if [ -z "${CILIUM_FLANNEL_MASTER_DEVICE}" ]; then
+	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conf}
+else
+	until ip link show "${CILIUM_FLANNEL_MASTER_DEVICE}" &>/dev/null ; do
+		echo "Waiting for ${CILIUM_FLANNEL_MASTER_DEVICE} to be initialized"
+		sleep 1s
+	done
+	CNI_CONF_NAME=${CNI_CONF_NAME:-04-flannel-cilium-cni.conflist}
+fi
 MTU=${MTU:-1500}
 
 BIN_NAME=cilium-cni
@@ -27,8 +35,8 @@ echo "Installing ${BIN_NAME} to ${CNI_DIR}/bin/ ..."
 # Move an eventual old existing binary out of the way, we can't delete it
 # as it might be in use right now.
 if [ -f "${CNI_DIR}/bin/${BIN_NAME}" ]; then
-        rm -f ${CNI_DIR}/bin/${BIN_NAME}.old || true
-        mv ${CNI_DIR}/bin/${BIN_NAME} ${CNI_DIR}/bin/${BIN_NAME}.old
+	rm -f ${CNI_DIR}/bin/${BIN_NAME}.old || true
+	mv ${CNI_DIR}/bin/${BIN_NAME} ${CNI_DIR}/bin/${BIN_NAME}.old
 fi
 
 cp /opt/cni/bin/${BIN_NAME} ${CNI_DIR}/bin/
@@ -37,12 +45,40 @@ if [ -f "${CILIUM_CNI_CONF}" ]; then
 	echo "Using existing ${CILIUM_CNI_CONF}..."
 else
 	echo "Installing new ${CILIUM_CNI_CONF}..."
-	cat > ${CNI_CONF_NAME} <<EOF
+	if [ -z "${CILIUM_FLANNEL_MASTER_DEVICE}" ]; then
+		cat > ${CNI_CONF_NAME} <<EOF
 {
     "name": "cilium",
     "type": "cilium-cni"
 }
 EOF
+	else
+		cat > ${CNI_CONF_NAME} <<EOF
+{
+  "cniVersion": "0.3.1",
+  "name": "flannel-cilium",
+  "plugins": [
+    {
+      "type": "flannel",
+      "delegate": {
+         "hairpinMode": true,
+         "isDefaultGateway": true
+      }
+    },
+    {
+      "type": "portmap",
+      "capabilities": {
+        "portMappings": true
+      }
+    },
+    {
+       "name": "cilium",
+       "type": "cilium-cni"
+    }
+  ]
+}
+EOF
+	fi
 	if [ ! -d $(dirname $CILIUM_CNI_CONF) ]; then
 		mkdir -p $(dirname $CILIUM_CNI_CONF)
 	fi

plugins/cilium-cni/cni-uninstall.sh
@@ -3,7 +3,11 @@
 set -eu
 
 HOST_PREFIX=${HOST_PREFIX:-/host}
-CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conf}
+if [ -z "${CILIUM_FLANNEL_MASTER_DEVICE}" ]; then
+    CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conf}
+else
+    CNI_CONF_NAME=${CNI_CONF_NAME:-04-flannel-cilium-cni.conflist}
+fi
 
 BIN_NAME=cilium-cni
 CNI_DIR=${CNI_DIR:-${HOST_PREFIX}/opt/cni}
@@ -15,3 +19,10 @@ rm -f ${CNI_DIR}/bin/${BIN_NAME}.old
 
 echo "Removing ${CILIUM_CNI_CONF} ..."
 rm -f ${CILIUM_CNI_CONF}
+
+if [ -z "${CILIUM_FLANNEL_UNINSTALL_ON_EXIT}" ]; then
+	echo "Removing BPF programs from all containers and from ${CILIUM_FLANNEL_MASTER_DEVICE}"
+	tc filter delete dev cni0 egress pref 1 handle 1 bpf || true
+	# TODO create script to detect all interfaces that have bpf programs
+	# installed
+fi

plugins/cilium-cni/04-flannel-cilium-cni.conflist
@@ -1,0 +1,23 @@
+{
+  "cniVersion": "0.3.1",
+  "name": "flannel-cilium",
+  "plugins": [
+    {
+      "type": "flannel",
+      "delegate": {
+         "hairpinMode": true,
+         "isDefaultGateway": true
+      }
+    },
+    {
+      "type": "portmap",
+      "capabilities": {
+        "portMappings": true
+      }
+    },
+    {
+       "name": "cilium",
+       "type": "cilium-cni"
+    }
+  ]
+}

commit 246a4214000cb598ff2df0f2124590c7cbb68083

plugins/cilium-cni: do not error if variable unset
As some users will not have the CILIUM_FLANNEL_MASTER_DEVICE nor
CILIUM_FLANNEL_UNINSTALL_ON_EXIT environment labels set in their
environments, the cni installation scripts will fail to run preventing
cilium from starting.

Fixes: a5b011fb4891 ("plugins/cni: add installation script for policy enforcement mode")
Signed-off-by: André Martins <andre@cilium.io>

plugins/cilium-cni/cni-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eu
+set -e
 
 HOST_PREFIX=${HOST_PREFIX:-/host}
 if [ -z "${CILIUM_FLANNEL_MASTER_DEVICE}" ]; then

plugins/cilium-cni/cni-uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eu
+set -e
 
 HOST_PREFIX=${HOST_PREFIX:-/host}
 if [ -z "${CILIUM_FLANNEL_MASTER_DEVICE}" ]; then