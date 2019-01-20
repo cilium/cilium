f0cad26da0f4ef7ad69c3191d8729034a48ba67f
Thomas Graf <thomas@cilium.io>
2019-01-20 19:19:32 +0100

    datapath: Introduce datapath interface abstraction
    pkg/datapath is extended to contain the requierments for a datapath specific
    implementation. This includes handling of node events, changes to the local
    node configuration and platform specific node addressing.
    
    These new types of interfaces should be used to abstract all datapath specific
    requirements. This commit introduces the interfaces for node event handling but
    the same concept can be applied to all datapath interactions and thus allow to
    provide comprehensive isolated unit tests for the datapath interactions.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ pkg/datapath/node.go
@@ -0,0 +1,118 @@
+// Copyright 2018-2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datapath
+
+import (
+	"github.com/cilium/cilium/pkg/cidr"
+	"github.com/cilium/cilium/pkg/mtu"
+	"github.com/cilium/cilium/pkg/node"
+)
+
+// LocalNodeConfiguration represents the configuration of the local node
+type LocalNodeConfiguration struct {
+	// MtuConfig is the MTU configuration of the node.
+	//
+	// This field is immutable at runtime. The value will not change in
+	// subsequent calls to NodeConfigurationChanged().
+	MtuConfig mtu.Configuration
+
+	// AuxiliaryPrefixes is the list of auxiliary prefixes that should be
+	// configured in addition to the node PodCIDR
+	//
+	// This field is mutable. The implementation of
+	// NodeConfigurationChanged() must adjust the routes accordingly.
+	AuxiliaryPrefixes []*cidr.CIDR
+
+	// EnableIPv4 enables use of IPv4. Routing to the IPv4 allocation CIDR
+	// of other nodes must be enabled.
+	//
+	// This field is immutable at runtime. The value will not change in
+	// subsequent calls to NodeConfigurationChanged().
+	EnableIPv4 bool
+
+	// EnableIPv6 enables use of IPv6. Routing to the IPv6 allocation CIDR
+	// of other nodes must be enabled.
+	//
+	// This field is immutable at runtime. The value will not change in
+	// subsequent calls to NodeConfigurationChanged().
+	EnableIPv6 bool
+
+	// UseSingleClusterRoute enables the use of a single cluster-wide route
+	// to direct traffic from the host into the Cilium datapath.  This
+	// avoids the requirement to install a separate route for each node
+	// CIDR and can thus improve the overhead when operating large clusters
+	// with significant node event churn due to auto-scaling.
+	//
+	// Use of UseSingleClusterRoute must be compatible with
+	// EnableAutoDirectRouting. When both are enabled, any direct node
+	// route must take precedence over the cluster-wide route as per LPM
+	// routing definition.
+	//
+	// This field is mutable. The implementation of
+	// NodeConfigurationChanged() must adjust the routes accordingly.
+	UseSingleClusterRoute bool
+
+	// EnableEncapsulation enables use of encapsulation in communication
+	// between nodes.
+	//
+	// This field is immutable at runtime. The value will not change in
+	// subsequent calls to NodeConfigurationChanged().
+	EnableEncapsulation bool
+
+	// EnableAutoDirectRouting enables the use of direct routes for
+	// communication between nodes if two nodes have direct L2
+	// connectivity.
+	//
+	// EnableAutoDirectRouting must be compatible with EnableEncapsulation
+	// and must provide a fallback to use encapsulation if direct routing
+	// is not feasible and encapsulation is enabled.
+	//
+	// This field is immutable at runtime. The value will not change in
+	// subsequent calls to NodeConfigurationChanged().
+	EnableAutoDirectRouting bool
+
+	// EnableLocalNodeRoute enables installation of the route which points
+	// the allocation prefix of the local node. Disabling this option is
+	// useful when another component is responsible for the routing of the
+	// allocation CIDR IPs into Cilium endpoints.
+	EnableLocalNodeRoute bool
+}
+
+// NodeHandler handles node related events such as addition, update or deletion
+// of nodes or changes to the local node configuration.
+//
+// Node events apply to the local node as well as to remote nodes. The
+// implementation can differ between the own local node and remote nodes by
+// calling node.IsLocal().
+type NodeHandler interface {
+	// NodeAdd is called when a node is discovered for the first time.
+	NodeAdd(newNode node.Node) error
+
+	// NodeUpdate is called when a node definition changes. Both the old
+	// and new node definition is provided. NodeUpdate() is never called
+	// before NodeAdd() is called for a particular node.
+	NodeUpdate(oldNode, newNode node.Node) error
+
+	// NodeDelete is called after a node has been deleted
+	NodeDelete(node node.Node) error
+
+	// NodeValidateImplementation is called to validate the implementation
+	// of the node in the datapath
+	NodeValidateImplementation(node node.Node) error
+
+	// NodeConfigurationChanged is called when the local node configuration
+	// has changed
+	NodeConfigurationChanged(config LocalNodeConfiguration) error
+}
