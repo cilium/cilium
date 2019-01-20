fb317d4c99e306c9265a53150d20b272b1a861e0
Thomas Graf <thomas@cilium.io>
2019-01-20 19:19:32 +0100

    datapath/fake: Add fake datapath implementation
    [ upstream commit bca02228fca9143d04e4bb0c8fb6cc4db457072d ]
    
    fake implementation of the datapath for testing
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

---
+++ pkg/datapath/fake/node.go
@@ -0,0 +1,47 @@
+// Copyright 2018-2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fake
+
+import (
+	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/node"
+)
+
+type fakeNodeHandler struct{}
+
+// NewNodeHandler returns a fake NodeHandler that performs no actions
+func NewNodeHandler() datapath.NodeHandler {
+	return &fakeNodeHandler{}
+}
+
+func (n *fakeNodeHandler) NodeAdd(newNode node.Node) error {
+	return nil
+}
+
+func (n *fakeNodeHandler) NodeUpdate(oldNode, newNode node.Node) error {
+	return nil
+}
+
+func (n *fakeNodeHandler) NodeDelete(node node.Node) error {
+	return nil
+}
+
+func (n *fakeNodeHandler) NodeValidateImplementation(node node.Node) error {
+	return nil
+}
+
+func (n *fakeNodeHandler) NodeConfigurationChanged(config datapath.LocalNodeConfiguration) error {
+	return nil
+}
