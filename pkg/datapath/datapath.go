b4c790a9b31fde9b397229b9f42276c8f9d6bb74
Thomas Graf <thomas@cilium.io>
2019-01-20 19:19:32 +0100

    datapath: Introduce datapath interface abstraction
    [ upstream commit 00a3be781920e0d59b84b6fa7940feff7ecfbcc4 ]
    
    pkg/datapath is extended to contain the requierments for a datapath specific
    implementation. This includes handling of node events, changes to the local
    node configuration and platform specific node addressing.
    
    These new types of interfaces should be used to abstract all datapath specific
    requirements. This commit introduces the interfaces for node event handling but
    the same concept can be applied to all datapath interactions and thus allow to
    provide comprehensive isolated unit tests for the datapath interactions.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

---
+++ pkg/datapath/datapath.go
@@ -0,0 +1,27 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datapath
+
+// Datapath is the interface to abstract all datapath interactions. The
+// abstraction allows to implement the datapath requirements with multiple
+// implementations
+type Datapath interface {
+	// Node must return the handler for node events
+	Node() NodeHandler
+
+	// LocalNodeAddressing must return the node addressing implementation
+	// of the local node
+	LocalNodeAddressing() NodeAddressing
+}
---
+++ pkg/datapath/node_addressing.go
@@ -0,0 +1,44 @@
+// Copyright 2018-2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datapath
+
+import (
+	"net"
+
+	"github.com/cilium/cilium/pkg/cidr"
+)
+
+// NodeAddressingFamily is the node addressing information for a particular
+// address family
+type NodeAddressingFamily interface {
+	// Router is the address that will act as the router on each node where
+	// an agent is running on. Endpoints have a default route that points
+	// to this address.
+	Router() net.IP
+
+	// PrimaryExternal is the primary external address of the node. Nodes
+	// must be able to reach each other via this address.
+	PrimaryExternal() net.IP
+
+	// AllocationCIDR is the CIDR used for IP allocation of all endpoints
+	// on the node
+	AllocationCIDR() *cidr.CIDR
+}
+
+// NodeAddressing implements addressing of a node
+type NodeAddressing interface {
+	IPv6() NodeAddressingFamily
+	IPv4() NodeAddressingFamily
+}
