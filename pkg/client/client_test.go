6e749548e1d9d497f4a340b1f904fe6586ba1cc1
Michal Rostecki <mrostecki@suse.de>
2019-01-21 16:07:34 +0100

    client: Extend DeadlineExceeded error
    [ upstream commit 005f3fd3d4049ac3951210d3a424f45c84873918 ]
    
    Error message `context deadline exceeded` is not descriptive and does
    not show what component has a connectivity issue. This change extends
    DeadlineExceeded errors with information that it comes from Cilium API
    client.
    
    Fixes: #6458
    
    Signed-off-by: Michal Rostecki <mrostecki@suse.de>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

---
+++ pkg/client/client_test.go
@@ -0,0 +1,62 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package client
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"golang.org/x/net/context"
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+type ClientTestSuite struct{}
+
+var _ = Suite(&ClientTestSuite{})
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+func (cs *ClientTestSuite) TestHint(c *C) {
+	var err error
+
+	err = nil
+	c.Assert(Hint(err), IsNil)
+
+	err = errors.New("foo bar")
+	c.Assert(Hint(err), ErrorMatches, "foo bar")
+
+	err = fmt.Errorf("ayy lmao")
+	c.Assert(Hint(err), ErrorMatches, "ayy lmao")
+
+	err = context.DeadlineExceeded
+	c.Assert(Hint(err), ErrorMatches, "Cilium API client timeout exceeded")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
+	defer cancel()
+
+	select {
+	case <-ctx.Done():
+		err = ctx.Err()
+	}
+
+	c.Assert(Hint(err), ErrorMatches, "Cilium API client timeout exceeded")
+}
