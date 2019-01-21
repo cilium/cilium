c1ad54374452d18399cbe49923b6655b77adf462
John Fastabend <john.fastabend@gmail.com>
2019-01-21 22:24:59 +0000

    cilium: ipsec, add ipsec unit test

---
+++ pkg/datapath/linux/ipsec/ipsec_linux_test.go
@@ -0,0 +1,133 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build privileged_tests
+
+package ipsec
+
+import (
+	"bytes"
+	"net"
+	"os"
+	"testing"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/vishvananda/netlink"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type IPSecSuitePrivileged struct{}
+
+var _ = Suite(&IPSecSuitePrivileged{})
+
+var (
+	path           = "ipsec_keys_test"
+	keysDat        = []byte("hmac(sha256) abcdefghijklmnopqrstuvwzyzABCDEF cbc(aes) abcdefghijklmnopqrstuvwzyzABCDEF\nhmac(sha256) abcdefghijklmnopqrstuvwzyzABCDEF cbc(aes) abcdefghijklmnopqrstuvwzyzABCDEF foobar\n")
+	invalidKeysDat = []byte("test abcdefghijklmnopqrstuvwzyzABCDEF test abcdefghijklmnopqrstuvwzyzABCDEF\n")
+)
+
+func (p *IPSecSuitePrivileged) TestLoadKeysNoFile(c *C) {
+	err := LoadIPSecKeysFile(path)
+	c.Assert(os.IsNotExist(err), Equals, true)
+}
+
+func (p *IPSecSuitePrivileged) TestInvalidLoadKeys(c *C) {
+	keys := bytes.NewReader(invalidKeysDat)
+	err := loadIPSecKeys(keys)
+	c.Assert(err, IsNil)
+
+	_, local, err := net.ParseCIDR("1.1.3.4/16")
+	c.Assert(err, IsNil)
+	_, remote, err := net.ParseCIDR("1.2.3.4/16")
+	c.Assert(err, IsNil)
+	err = UpsertIPSecEndpoint(local, remote)
+	c.Assert(err, NotNil)
+}
+
+func (p *IPSecSuitePrivileged) TestLoadKeys(c *C) {
+	keys := bytes.NewReader(keysDat)
+	err := loadIPSecKeys(keys)
+	c.Assert(err, IsNil)
+}
+
+func (p *IPSecSuitePrivileged) TestUpsertIPSecEquals(c *C) {
+	_, local, err := net.ParseCIDR("1.2.3.4/16")
+	c.Assert(err, IsNil)
+	_, remote, err := net.ParseCIDR("1.2.3.4/16")
+	c.Assert(err, IsNil)
+
+	key := &ipSecKey{
+		Spi:   1,
+		ReqID: 1,
+		Auth:  &netlink.XfrmStateAlgo{Name: "hmac(sha256)", Key: []byte("abcdefghijklmnopqrstuvwzyzABCDEF")},
+		Crypt: &netlink.XfrmStateAlgo{Name: "cbc(aes)", Key: []byte("abcdefghijklmnopqrstuvwzyzABCDEF")},
+	}
+
+	ipSecKeysGlobal["1.2.3.4"] = key
+	ipSecKeysGlobal[""] = key
+
+	err = UpsertIPSecEndpoint(local, remote)
+	c.Assert(err, IsNil)
+
+	err = DeleteIPSecEndpoint(remote.IP, local.IP)
+	c.Assert(err, IsNil)
+
+	ipSecKeysGlobal["1.2.3.4"] = nil
+	ipSecKeysGlobal[""] = nil
+
+}
+
+func (p *IPSecSuitePrivileged) TestUpsertIPSecEndpoint(c *C) {
+	_, local, err := net.ParseCIDR("1.1.3.4/16")
+	c.Assert(err, IsNil)
+	_, remote, err := net.ParseCIDR("1.2.3.4/16")
+	c.Assert(err, IsNil)
+
+	key := &ipSecKey{
+		Spi:   1,
+		ReqID: 1,
+		Auth:  &netlink.XfrmStateAlgo{Name: "hmac(sha256)", Key: []byte("abcdefghijklmnopqrstuvwzyzABCDEF")},
+		Crypt: &netlink.XfrmStateAlgo{Name: "cbc(aes)", Key: []byte("abcdefghijklmnopqrstuvwzyzABCDEF")},
+	}
+
+	ipSecKeysGlobal["1.1.3.4"] = key
+	ipSecKeysGlobal["1.2.3.4"] = key
+	ipSecKeysGlobal[""] = key
+
+	err = UpsertIPSecEndpoint(local, remote)
+	c.Assert(err, IsNil)
+
+	err = DeleteIPSecEndpoint(remote.IP, local.IP)
+	c.Assert(err, IsNil)
+
+	ipSecKeysGlobal["1.1.3.4"] = nil
+	ipSecKeysGlobal["1.2.3.4"] = nil
+	ipSecKeysGlobal[""] = nil
+}
+
+func (p *IPSecSuitePrivileged) TestUpsertIPSecKeyMissing(c *C) {
+	_, local, err := net.ParseCIDR("1.1.3.4/16")
+	c.Assert(err, IsNil)
+	_, remote, err := net.ParseCIDR("1.2.3.4/16")
+	c.Assert(err, IsNil)
+
+	err = UpsertIPSecEndpoint(local, remote)
+	c.Assert(err, ErrorMatches, "IPSec key missing")
+
+	err = DeleteIPSecEndpoint(remote.IP, local.IP)
+	c.Assert(err, IsNil)
+}
