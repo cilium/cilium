9d9cef4c442dabc8e80eddaeb1791703ef67be85
Joe Stringer <joe@cilium.io>
2019-01-22 20:29:42 -0800

    bpf: Replace data definitions with static variables
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/datapath/linux/utils_test.go
+++ pkg/datapath/linux/utils_test.go
@@ -61,20 +61,48 @@ func (s *DatapathSuite) TestGoArray2C(c *C) {
 }
 
 func (s *DatapathSuite) TestdefineIPv6(c *C) {
-	input := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
-	expOut := "#define foo 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10\n"
-	c.Assert(defineIPv6("foo", input), Equals, expOut)
+	tests := []formatTestCase{
+		{
+			input:  nil,
+			output: "/* BUG: bad ip define foo  */\n",
+		},
+		{
+			input:  []byte{},
+			output: "/* BUG: bad ip define foo  */\n",
+		},
+		{
+			input:  []byte{1, 2, 3},
+			output: "/* BUG: bad ip define foo 0x1, 0x2, 0x3 */\n",
+		},
+		{
+			input:  []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+			output: "DEFINE_IPV6(foo, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10);\n",
+		},
+	}
+
+	for _, test := range tests {
+		c.Assert(defineIPv6("foo", test.input), Equals, test.output)
+	}
 }
 
 func (s *DatapathSuite) TestdefineMAC(c *C) {
 	tests := []formatTestCase{
 		{
-			input:  []byte{1, 2, 3},
-			output: "#define foo { .addr = { 0x1, 0x2, 0x3 } }\n",
+			input:  nil,
+			output: "/* BUG: bad mac define foo  */\n",
 		},
 		{
 			input:  []byte{},
-			output: "#define foo { .addr = {  } }\n",
+			output: "/* BUG: bad mac define foo  */\n",
+		},
+		{
+			input:  []byte{1, 2, 3},
+			output: "/* BUG: bad mac define foo 0x1, 0x2, 0x3 */\n",
+		},
+		{
+			input: []byte{1, 2, 3, 4, 5, 6},
+			output: "DEFINE_MAC(foo, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6);\n" +
+				"#define foo fetch_mac(foo)\n",
 		},
 	}
 	for _, test := range tests {
