dfc1fd1417409f7195f9dd7dcf40cc8c550c15b7
Joe Stringer <joe@cilium.io>
2019-01-22 20:29:42 -0800

    bpf: Replace data definitions with static variables
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- bpf/lib/utils.h
+++ bpf/lib/utils.h
@@ -99,6 +99,7 @@ static inline __u32 bpf_ktime_get_sec(void)
 #define fetch_u32(x) __fetch(x)
 #define fetch_u32_i(x, i) __fetch(x ## _ ## i)
 #define fetch_ipv6(x) fetch_u32_i(x, 1), fetch_u32_i(x, 2), fetch_u32_i(x, 3), fetch_u32_i(x, 4)
+#define fetch_mac(x) { { fetch_u32_i(x, 1), (__u16)fetch_u32_i(x, 2) } }
 
 /* DEFINE_* macros help to declare static data. */
 #define DEFINE_U32(NAME, value) uint32_t NAME = value
@@ -108,5 +109,9 @@ DEFINE_U32_I(NAME, 1) = bpf_htonl( (a1) << 24 |  (a2) << 16 |  (a3) << 8 |  (a4)
 DEFINE_U32_I(NAME, 2) = bpf_htonl( (a5) << 24 |  (a6) << 16 |  (a7) << 8 |  (a8));			\
 DEFINE_U32_I(NAME, 3) = bpf_htonl( (a9) << 24 | (a10) << 16 | (a11) << 8 | (a12));			\
 DEFINE_U32_I(NAME, 4) = bpf_htonl((a13) << 24 | (a14) << 16 | (a15) << 8 | (a16))
+
+#define DEFINE_MAC(NAME, a1, a2, a3, a4, a5, a6)			\
+DEFINE_U32_I(NAME, 1) = (a1) << 24 | (a2) << 16 |  (a3) << 8 | (a4);	\
+DEFINE_U32_I(NAME, 2) =                            (a5) << 8 | (a6)
 
 #endif
