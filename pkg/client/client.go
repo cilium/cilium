cilium/cilium

commit c6518190de3038d55184c7283ddda22832b9ff52

client: Add NewDefaultClientWithTimeout function
When NewDefaultClient function is used and cilium-agent is not up and
available, it immediately returns an error. This change introduces a new
function, NewDefaultClientWithTimeout, which waits 30 seconds for
cilium-agent availability before returning an error.

Signed-off-by: Michal Rostecki <mrostecki@suse.de>

pkg/client/client.go
@@ -76,6 +76,28 @@ func NewDefaultClient() (*Client, error) {
 	return NewClient("")
 }
 
+// NewDefaultClientWithTimeout creates a client with default parameters connecting to UNIX
+// domain socket and waits for cilium-agent availability.
+func NewDefaultClientWithTimeout(timeout time.Duration) (*Client, error) {
+	timeoutAfter := time.After(timeout)
+	var c *Client
+	var err error
+	for {
+		select {
+		case <-timeoutAfter:
+			return c, fmt.Errorf("Failed to create cilium agent client after %f seconds timeout: %s", timeout.Seconds(), err)
+		default:
+		}
+
+		c, err = NewDefaultClient()
+		if err == nil {
+			break
+		}
+	}
+
+	return c, nil
+}
+
 // NewClient creates a client for the given `host`.
 // If host is nil then use SockPath provided by CILIUM_SOCK
 // or the cilium default SockPath

commit 027b0f8892dc5fceca9fb6ed177b898b8524efe1

pkg/client: wait until the client has connectivity with daemon
Fixes: c6518190de30 ("client: Add NewDefaultClientWithTimeout function")
Signed-off-by: André Martins <andre@cilium.io>

pkg/client/client.go
@@ -85,14 +85,18 @@ func NewDefaultClientWithTimeout(timeout time.Duration) (*Client, error) {
 	for {
 		select {
 		case <-timeoutAfter:
-			return c, fmt.Errorf("Failed to create cilium agent client after %f seconds timeout: %s", timeout.Seconds(), err)
+			return nil, fmt.Errorf("Failed to create cilium agent client after %f seconds timeout: %s", timeout.Seconds(), err)
 		default:
 		}
 
 		c, err = NewDefaultClient()
 		if err == nil {
-			break
+			_, err := c.Daemon.GetConfig(nil)
+			if err == nil {
+				break
+			}
 		}
+		time.Sleep(500 * time.Millisecond)
 	}
 
 	return c, nil