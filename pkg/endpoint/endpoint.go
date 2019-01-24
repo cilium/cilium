cfb8e55b7361b63c552cc11fc1a3d70aa021bea8
Ian Vernon <ian@cilium.io>
2019-01-24 10:32:46 -0800

    endpoint: do not return error if endpoint RLock fails due to endpoint being removed
    [ upstream commit 73aafb818b564b673116af008497c28121d1794e ]
    
    The Cilium CI has failed recently on multiple occasions due to errors appearing
    for controllers for endpoints which resolve the identity for said endpoints.
    The errors were "rlock failed: endpoint is in the process of being removed".
    Handle this case specially in both cases where \`identityLabelsChanged\` is
    called for endpoints by adding a new error variable which is returned if
    \`RLockAlive\` fails, and handle this error being returned in callers to
    \`identityLabelsChanged\`. In the case where \`identityLabelsChanged\` is
    called within the \`resolve-identity\` controller for an endpoint, do not
    return an error in the controller if \`RLockAlive\` fails because the endpoint
    is being deleted, so there is no purpose in allocating a new identity for it.
    
    Fixes: #6721
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- pkg/endpoint/endpoint.go
+++ pkg/endpoint/endpoint.go
@@ -2446,8 +2446,14 @@ func (e *Endpoint) runLabelsResolver(owner Owner, myChangeRev int, blocking bool
 	if blocking || identityPkg.IdentityAllocationIsLocal(newLabels) {
 		scopedLog.Debug("Endpoint has reserved identity, changing synchronously")
 		err := e.identityLabelsChanged(owner, myChangeRev)
-		if err != nil {
-			scopedLog.WithError(err).Warn("Error changing endpoint identity")
+		switch err {
+		case ErrNotAlive:
+			scopedLog.Debug("not changing endpoint identity because endpoint is in process of being removed")
+			return
+		default:
+			if err != nil {
+				scopedLog.WithError(err).Warn("Error changing endpoint identity")
+			}
 		}
 	}
 
@@ -2455,7 +2461,14 @@ func (e *Endpoint) runLabelsResolver(owner Owner, myChangeRev int, blocking bool
 	e.controllers.UpdateController(ctrlName,
 		controller.ControllerParams{
 			DoFunc: func() error {
-				return e.identityLabelsChanged(owner, myChangeRev)
+				err := e.identityLabelsChanged(owner, myChangeRev)
+				switch err {
+				case ErrNotAlive:
+					e.getLogger().Debug("not changing endpoint identity because endpoint is in process of being removed")
+					return nil
+				default:
+					return err
+				}
 			},
 			RunInterval: 5 * time.Minute,
 		},
@@ -2464,7 +2477,7 @@ func (e *Endpoint) runLabelsResolver(owner Owner, myChangeRev int, blocking bool
 
 func (e *Endpoint) identityLabelsChanged(owner Owner, myChangeRev int) error {
 	if err := e.RLockAlive(); err != nil {
-		return err
+		return ErrNotAlive
 	}
 	newLabels := e.OpLabels.IdentityLabels()
 	elog := e.getLogger().WithFields(logrus.Fields{
