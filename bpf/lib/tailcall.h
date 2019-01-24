aa70feb926e1eb7ee737b1f3bae44e7c48662e88
Daniel Borkmann <daniel@iogearbox.net>
2019-01-24 13:43:55 +0000

    cilium, bpf: config depdendent tail call emission in fast path
    Tail calls are expensive due to being an indirect call and thus subject
    to speculative execution which is mitigated these days via retpoline
    in JIT. Lets add some macros that can be used with is_defined(...) to
    switch between tail call and more performant direct inlined call for
    fast-path. Given verifier complexity limits we need to be selective when
    to enable. For now, go for avoiding tail calls when either v4 or v6 is
    set but not both as it has highest complexity. x86-64 JIT retpoline
    code-gen output:
    
      https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=a493a87f38cfa48caaa95c9347be2d914c6fdf29
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

---
+++ bpf/lib/tailcall.h
@@ -0,0 +1,100 @@
+#ifndef TAILCALL_H
+#define TAILCALL_H
+
+#include "config.h"
+
+#define __eval(x, ...) x ## __VA_ARGS__
+
+#define __and_00 0
+#define __and_01 0
+#define __and_10 0
+#define __and_11 1
+#define __and_0(y)  __eval(__and_0, y)
+#define __and_1(y)  __eval(__and_1, y)
+#define __and(x, y) __eval(__and_, x)(y)
+
+#define __or_00 0
+#define __or_01 1
+#define __or_10 1
+#define __or_11 1
+#define __or_0(y)  __eval(__or_0, y)
+#define __or_1(y)  __eval(__or_1, y)
+#define __or(x, y) __eval(__or_, x)(y)
+
+/* declare_tailcall_if() and invoke_tailcall_if() is a pair
+ * of helpers which based on COND either selects to emit a
+ * tail call for the underlying function when true or emits
+ * it as inlined when false. COND can be selected by one or
+ * multiple compile time flags.
+ *
+ * Usage example:
+ *
+ * 1) Declaration:
+ *
+ * declare_tailcall_if(__and(is_defined(ENABLE_IPV4), is_defined(ENABLE_IPV6)),
+ *                     CILIUM_CALL_FOO)
+ * int foo_fn(struct __sk_buff *skb)
+ * {
+ *    [...]
+ * }
+ *
+ * 2) Call-site:
+ *
+ * [...]
+ * invoke_tailcall_if(__and(is_defined(ENABLE_IPV4), is_defined(ENABLE_IPV6)),
+ *                    CILIUM_CALL_FOO, foo_fn);
+ * [...]
+ *
+ * 3) Compilation result:
+ *
+ * When compiled with -DENABLE_IPV4 and -DENABLE_IPV6 both
+ * set, then above emits a tail call as follows:
+ *
+ * __attribute__((section("2" "/" "10"), used))
+ * int foo_fn(struct __sk_buff *skb)
+ * {
+ *    [...]
+ * }
+ *
+ * [...]
+ * do { ep_tail_call(skb, 10); ret = -140; } while (0);
+ * [...]
+ *
+ * The fall-through side sets DROP_MISSED_TAIL_CALL as ret.
+ *
+ * When only one of them is set in the above example or none
+ * of them, then the code emission looks like:
+ *
+ * static __inline __attribute__ ((__always_inline__))
+ * int foo_fn(struct __sk_buff *skb)
+ * {
+ *    [...]
+ * }
+ *
+ * [...]
+ * return foo_fn(skb);
+ * [...]
+ *
+ * Selectors can be single is_defined(), or multiple ones
+ * combined with __and() or __or() macros. COND must be
+ * the same expression for declare_tailcall_if() and the
+ * invoke_tailcall_if() part.
+ */
+#define __declare_tailcall_if_0(NAME)         \
+	static __always_inline
+#define __declare_tailcall_if_1(NAME)         \
+	__section_tail(CILIUM_MAP_CALLS, NAME)
+#define declare_tailcall_if(COND, NAME)       \
+	__eval(__declare_tailcall_if_, COND)(NAME)
+
+#define __invoke_tailcall_if_0(NAME, FUNC)    \
+	return FUNC(skb)
+#define __invoke_tailcall_if_1(NAME, FUNC)    \
+	do {                                  \
+		ep_tail_call(skb, NAME);      \
+		ret = DROP_MISSED_TAIL_CALL;  \
+	} while (0)
+#define invoke_tailcall_if(COND, NAME, FUNC)  \
+	__eval(__invoke_tailcall_if_, COND)(NAME, FUNC)
+
+#endif /* TAILCALL_H */
