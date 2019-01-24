cilium/cilium

commit be7d5cb4b919e46cb12aa9fbd2ded6b186d17f75

node: Use new routing datapath for node events
This commit achieves separation of node definition, node management and
datapath implementation based on node events.

pkg/node contains the definition of a node, its addressing, logic to determine
the primary external address, determination of local hostname, and management
of the addressing for the local node. The addressing should be moved out of
this package in a future PR but was outside of thes scope of this commit.

pkg/node/manager manages the list of nodes and implements the logic to handle
node events from multiple sources. Sources include:

    * local node definition
    * local agent information
    * kubernetes
    * kvstore

Two new metrics are introduced:

   * gauge cilium_nodes_all_num
   * counter cilium_nodes_all_events_received_total {"all", "update", "delete"}
   * counter cilium_nodes_all_datapath_validations_total

This allows to visualize the current cluster size as well as the rate of node
updates received per minute.

The manager periodically requests the datapath to validate the implementation
of a node. As the cost of validation grows with the cluster size, the interval
in which the validation is performed is made dependent on the cluster size
using a logarithmic function.

The local node management is moved into daemon/node_discovery.go and simplified
by allocating health IPs before starting node discovery. This avoids requiring
to annodate the k8s node multiple times.

Signed-off-by: Thomas Graf <thomas@cilium.io>

cilium-health/launch/endpoint.go
@@ -25,6 +25,7 @@ import (
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/endpoint"
@@ -37,7 +38,6 @@ import (
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/mtu"
-	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/pidfile"
 
@@ -60,10 +60,6 @@ var (
 
 	// client is used to ping the cilium-health endpoint as a health check.
 	client *healthPkg.Client
-
-	// NodeEpAnnotator is used to annotate nodes and pods in a cluster with
-	// information about this cilium-health instance.
-	NodeEpAnnotator Annotator
 )
 
 func logFromCommand(cmd *exec.Cmd, netns string) error {
@@ -183,7 +179,7 @@ func CleanupEndpoint() {
 // Annotator is an interface which describes anything which annotates a node
 // with cilium-health metadata.
 type Annotator interface {
-	AnnotateNode(nodeName string, v4CIDR, v6CIDR *net.IPNet, v4HealthIP, v6HealthIP, v4CiliumHostIP net.IP) error
+	AnnotateNode(nodeName string, v4CIDR, v6CIDR *cidr.CIDR, v4HealthIP, v6HealthIP, v4CiliumHostIP net.IP) error
 	AnnotatePod(k8sNamespace, k8sPodName, annotationKey, annotationValue string) error
 }
 
@@ -193,7 +189,7 @@ type Annotator interface {
 //
 // CleanupEndpoint() must be called before calling LaunchAsEndpoint() to ensure
 // cleanup of prior cilium-health endpoint instances.
-func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressing, mtuConfig mtu.Configuration) error {
+func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressing, mtuConfig mtu.Configuration, ip4health, ip6health net.IP) error {
 	var (
 		cmd  = launcher.Launcher{}
 		info = &models.EndpointChangeRequest{
@@ -201,21 +197,19 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 			State:         models.EndpointStateWaitingForIdentity,
 			Addressing:    &models.AddressPair{},
 		}
-		ip4, ip6               net.IP
+		ip4                    net.IP
 		ip4Address, ip6Address string
 	)
 
 	if option.Config.EnableIPv4 {
-		ip4 = node.GetIPv4HealthIP()
-		info.Addressing.IPV4 = ip4.String()
-		ip4WithMask := net.IPNet{IP: ip4, Mask: defaults.ContainerIPv4Mask}
+		info.Addressing.IPV4 = ip4health.String()
+		ip4WithMask := net.IPNet{IP: ip4health, Mask: defaults.ContainerIPv4Mask}
 		ip4Address = ip4WithMask.String()
 	}
 
 	if option.Config.EnableIPv6 {
-		ip6 = node.GetIPv6HealthIP()
-		info.Addressing.IPV6 = ip6.String()
-		ip6WithMask := net.IPNet{IP: ip6, Mask: defaults.ContainerIPv6Mask}
+		info.Addressing.IPV6 = ip6health.String()
+		ip6WithMask := net.IPNet{IP: ip6health, Mask: defaults.ContainerIPv6Mask}
 		ip6Address = ip6WithMask.String()
 	}
 
@@ -282,14 +276,6 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 	if !buildSuccessful {
 		endpointmanager.Remove(ep)
 		return fmt.Errorf("unable to build health endpoint")
-	}
-
-	// Propagate health IPs to all other nodes via annotations
-	if NodeEpAnnotator != nil {
-		err = NodeEpAnnotator.AnnotateNode(node.GetName(), nil, nil, ip4, ip6, nil)
-		if err != nil {
-			return fmt.Errorf("Cannot annotate node CIDR range data: %s", err)
-		}
 	}
 
 	// Initialize the health client to talk to this instance. This is why

daemon/health.go
@@ -15,13 +15,13 @@
 package main
 
 import (
+	"net"
 	"path/filepath"
 	"time"
 
 	health "github.com/cilium/cilium/cilium-health/launch"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpointmanager"
-	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
@@ -38,24 +38,6 @@ func (d *Daemon) initHealth() {
 
 	// Allocate health endpoint IPs after restoring state
 	log.Info("Building health endpoint")
-	health4, health6, err := ipam.AllocateNext("")
-	if err != nil {
-		log.WithError(err).Fatal("IPAM allocation failed. For more detail, see https://cilium.link/ipam-range-full")
-	}
-
-	err = node.SetIPv4HealthIP(health4)
-	if err != nil {
-		log.WithError(err).Fatal("Error while set health IPv4 ip on the local node.")
-	}
-
-	err = node.SetIPv6HealthIP(health6)
-	if err != nil {
-		log.WithError(err).Fatal("Error while set health IPv6 ip on the local node.")
-	}
-
-	log.Debugf("IPv4 health endpoint address: %s", node.GetIPv4HealthIP())
-	log.Debugf("IPv6 health endpoint address: %s", node.GetIPv6HealthIP())
-	node.NotifyLocalNodeUpdated()
 
 	// Launch cilium-health in the same namespace as cilium.
 	log.Info("Launching Cilium health daemon")
@@ -69,29 +51,27 @@ func (d *Daemon) initHealth() {
 		// running inside a new PID namespace which means that existing
 		// PIDfiles are referring to PIDs that may be reused. Clean up.
 		pidfile.Remove(filepath.Join(option.Config.StateDir, health.PidfilePath))
-
-		// Inject K8s dependency into packages which need to annotate K8s resources.
-		health.NodeEpAnnotator = k8s.Client()
 	}
 	controller.NewManager().UpdateController("cilium-health-ep",
 		controller.ControllerParams{
 			DoFunc: func() error {
-				return d.runCiliumHealthEndpoint()
+				return d.runCiliumHealthEndpoint(d.nodeDiscovery.localNode.IPv4HealthIP, d.nodeDiscovery.localNode.IPv6HealthIP)
 			},
 			StopFunc: func() error {
 				err := health.PingEndpoint()
-				d.cleanupHealthEndpoint()
+				d.cleanupHealthEndpoint(d.nodeDiscovery.localNode.IPv4HealthIP)
+
 				return err
 			},
 			RunInterval: 30 * time.Second,
 		})
 }
 
-func (d *Daemon) cleanupHealthEndpoint() {
+func (d *Daemon) cleanupHealthEndpoint(healthIPv4 net.IP) {
 	// Delete the process
 	health.KillEndpoint()
 	// Clean up agent resources
-	ep := endpointmanager.LookupIPv4(node.GetIPv4HealthIP().String())
+	ep := endpointmanager.LookupIPv4(healthIPv4.String())
 	if ep == nil {
 		log.Debug("Didn't find existing cilium-health endpoint to delete")
 	} else {
@@ -106,12 +86,12 @@ func (d *Daemon) cleanupHealthEndpoint() {
 
 // runCiliumHealthEndpoint attempts to contact the cilium-health endpoint, and
 // if it cannot be reached, restarts it.
-func (d *Daemon) runCiliumHealthEndpoint() error {
+func (d *Daemon) runCiliumHealthEndpoint(healthIPv4, healthIPv6 net.IP) error {
 	// PingEndpoint will always fail the first time (initialization).
 	if err := health.PingEndpoint(); err != nil {
-		d.cleanupHealthEndpoint()
+		d.cleanupHealthEndpoint(healthIPv4)
 		addressing := node.GetNodeAddressing()
-		return health.LaunchAsEndpoint(d, addressing, d.mtuConfig)
+		return health.LaunchAsEndpoint(d, addressing, d.mtuConfig, healthIPv4, healthIPv6)
 	}
 	return nil
 }

Makefile
@@ -209,6 +209,12 @@ generate-k8s-api:
 	cd "./vendor/k8s.io/code-generator" && \
 	./generate-groups.sh deepcopy \
 	    github.com/cilium/cilium/pkg/k8s/client \
+	    github.com/cilium/cilium \
+	    "pkg:node" \
+	    --go-header-file "$(PWD)/hack/custom-boilerplate.go.txt"
+	cd "./vendor/k8s.io/code-generator" && \
+	./generate-groups.sh deepcopy \
+	    github.com/cilium/cilium/pkg/k8s/client \
 	    github.com/cilium/cilium/api \
 	    "v1:models" \
 	    --go-header-file "$(PWD)/hack/custom-boilerplate.go.txt"

daemon/daemon.go
@@ -35,11 +35,13 @@ import (
 	"github.com/cilium/cilium/pkg/api"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/clustermesh"
 	"github.com/cilium/cilium/pkg/command/exec"
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/counter"
+	"github.com/cilium/cilium/pkg/datapath"
 	bpfIPCache "github.com/cilium/cilium/pkg/datapath/ipcache"
 	"github.com/cilium/cilium/pkg/datapath/iptables"
 	"github.com/cilium/cilium/pkg/datapath/prefilter"
@@ -73,6 +75,7 @@ import (
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 	"github.com/cilium/cilium/pkg/mtu"
 	"github.com/cilium/cilium/pkg/node"
+	nodemanager "github.com/cilium/cilium/pkg/node/manager"
 	nodeStore "github.com/cilium/cilium/pkg/node/store"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
@@ -166,6 +169,13 @@ type Daemon struct {
 
 	mtuConfig     mtu.Configuration
 	policyTrigger *trigger.Trigger
+
+	// datapath is the underlying datapath implementation to use to
+	// implement all aspects of an agent
+	datapath datapath.Datapath
+
+	// nodeDiscovery defines the node discovery logic of the agent
+	nodeDiscovery *nodeDiscovery
 }
 
 // UpdateProxyRedirect updates the redirect rules in the proxy for a particular
@@ -514,7 +524,10 @@ func (d *Daemon) compileBase() error {
 
 	if !option.Config.IsFlannelMasterDeviceSet() {
 		ipam.ReserveLocalRoutes()
-		node.InstallHostRoutes(d.mtuConfig)
+	}
+
+	if err := d.datapath.Node().NodeConfigurationChanged(d.nodeDiscovery.localConfig); err != nil {
+		return err
 	}
 
 	if option.Config.EnableIPv4 {
@@ -863,7 +876,7 @@ func createPrefixLengthCounter() *counter.PrefixLengthCounter {
 }
 
 // NewDaemon creates and returns a new Daemon with the parameters set in c.
-func NewDaemon() (*Daemon, *endpointRestoreState, error) {
+func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 	// Validate the daemon-specific global options.
 	if err := option.Config.Validate(); err != nil {
 		return nil, nil, fmt.Errorf("invalid daemon configuration: %s", err)
@@ -873,6 +886,13 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 
 	if err := workloads.Setup(option.Config.Workloads, map[string]string{}); err != nil {
 		return nil, nil, fmt.Errorf("unable to setup workload: %s", err)
+	}
+
+	mtuConfig := mtu.NewConfiguration(option.Config.Tunnel != option.TunnelDisabled, option.Config.MTU)
+
+	nodeMngr, err := nodemanager.NewManager("all", dp.Node())
+	if err != nil {
+		return nil, nil, err
 	}
 
 	d := Daemon{
@@ -885,7 +905,9 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 
 		buildEndpointSem: semaphore.NewWeighted(int64(numWorkerThreads())),
 		compilationMutex: new(lock.RWMutex),
-		mtuConfig:        mtu.NewConfiguration(option.Config.Tunnel != option.TunnelDisabled, option.Config.MTU),
+		mtuConfig:        mtuConfig,
+		datapath:         dp,
+		nodeDiscovery:    newNodeDiscovery(nodeMngr, mtuConfig),
 	}
 
 	// Open or create BPF maps.
@@ -956,6 +978,9 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 		} else {
 			option.Config.HostAllowsWorld = false
 		}
+
+		// Inject K8s dependency into packages which need to annotate K8s resources.
+		endpoint.EpAnnotator = k8s.Client()
 	}
 
 	// If the device has been specified, the IPv4AllocPrefix and the
@@ -972,10 +997,6 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 	// or IPv4 alloc prefix, respectively, retrieved by k8s node annotations.
 	log.Info("Initializing node addressing")
 
-	// Inject BPF dependency, kvstore dependency into node package.
-	node.TunnelDatapath = tunnel.TunnelMap
-	node.NodeReg = &nodeStore.NodeRegistrar{}
-
 	if err := node.AutoComplete(); err != nil {
 		log.WithError(err).Fatal("Cannot autocomplete node addresses")
 	}
@@ -983,20 +1004,11 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 	node.SetIPv4ClusterCidrMaskSize(option.Config.IPv4ClusterCIDRMaskSize)
 
 	if option.Config.IPv4Range != AutoCIDR {
-		_, net, err := net.ParseCIDR(option.Config.IPv4Range)
+		allocCIDR, err := cidr.ParseCIDR(option.Config.IPv4Range)
 		if err != nil {
 			log.WithError(err).WithField(logfields.V4Prefix, option.Config.IPv4Range).Fatal("Invalid IPv4 allocation prefix")
 		}
-		node.SetIPv4AllocRange(net)
-	}
-
-	if option.Config.IPv4ServiceRange != AutoCIDR {
-		_, ipnet, err := net.ParseCIDR(option.Config.IPv4ServiceRange)
-		if err != nil {
-			log.WithError(err).WithField(logfields.V4Prefix, option.Config.IPv4ServiceRange).Fatal("Invalid IPv4 service prefix")
-		}
-
-		node.AddAuxPrefix(ipnet)
+		node.SetIPv4AllocRange(allocCIDR)
 	}
 
 	if option.Config.IPv6Range != AutoCIDR {
@@ -1008,15 +1020,6 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 		if err := node.SetIPv6NodeRange(net); err != nil {
 			log.WithError(err).WithField(logfields.V6Prefix, net).Fatal("Invalid per node IPv6 allocation prefix")
 		}
-	}
-
-	if option.Config.IPv6ServiceRange != AutoCIDR {
-		_, ipnet, err := net.ParseCIDR(option.Config.IPv6ServiceRange)
-		if err != nil {
-			log.WithError(err).WithField(logfields.V6Prefix, option.Config.IPv6ServiceRange).Fatal("Invalid IPv6 service prefix")
-		}
-
-		node.AddAuxPrefix(ipnet)
 	}
 
 	// Set up ipam conf after init() because we might be running d.conf.KVStoreIPv4Registration
@@ -1096,9 +1099,20 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 		log.Infof("  Loopback IPv4: %s", node.GetIPv4Loopback().String())
 	}
 
-	if err := node.ConfigureLocalNode(); err != nil {
-		log.WithError(err).Fatal("Unable to initialize local node")
+	if option.Config.EnableHealthChecking {
+		health4, health6, err := ipam.AllocateNext("")
+		if err != nil {
+			return nil, restoredEndpoints, fmt.Errorf("unable to allocate health IPs: %s,see https://cilium.link/ipam-range-full", err)
+		}
+
+		d.nodeDiscovery.localNode.IPv4HealthIP = health4
+		log.Debugf("IPv4 health endpoint address: %s", health4)
+
+		d.nodeDiscovery.localNode.IPv6HealthIP = health6
+		log.Debugf("IPv6 health endpoint address: %s", health6)
 	}
+
+	d.nodeDiscovery.startDiscovery()
 
 	// This needs to be done after the node addressing has been configured
 	// as the node address is required as suffix.
@@ -1154,6 +1168,7 @@ func (d *Daemon) Close() {
 	if d.policyTrigger != nil {
 		d.policyTrigger.Shutdown()
 	}
+	d.nodeDiscovery.Close()
 }
 
 func (d *Daemon) attachExistingInfraContainers() {

daemon/daemon_main.go
@@ -35,10 +35,10 @@ import (
 	"github.com/cilium/cilium/pkg/cleanup"
 	"github.com/cilium/cilium/pkg/components"
 	"github.com/cilium/cilium/pkg/controller"
+	linuxdatapath "github.com/cilium/cilium/pkg/datapath/linux"
 	"github.com/cilium/cilium/pkg/datapath/loader"
 	"github.com/cilium/cilium/pkg/datapath/maps"
 	"github.com/cilium/cilium/pkg/defaults"
-	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/envoy"
 	"github.com/cilium/cilium/pkg/flowdebug"
@@ -330,8 +330,9 @@ func init() {
 	flags.String(option.AllowLocalhost, option.AllowLocalhostAuto, "Policy when to allow local stack to reach local endpoints { auto | always | policy }")
 	option.BindEnv(option.AllowLocalhost)
 
-	flags.Bool(option.AutoIPv6NodeRoutesName, false, "Automatically adds IPv6 L3 routes to reach other nodes for non-overlay mode (--device) (BETA)")
-	option.BindEnv(option.AutoIPv6NodeRoutesName)
+	flags.Bool(option.LegacyAutoIPv6NodeRoutesName, false, "Deprecatd, use --auto-direct-node-routes")
+	option.BindEnv(option.LegacyAutoIPv6NodeRoutesName)
+	flags.MarkDeprecated(option.LegacyAutoIPv6NodeRoutesName, fmt.Sprintf("please use %s", option.EnableAutoDirectRoutingName))
 
 	flags.String(option.BPFRoot, "", "Path to BPF filesystem")
 	option.BindEnv(option.BPFRoot)
@@ -403,6 +404,9 @@ func init() {
 
 	flags.StringP(option.Docker, "e", workloads.GetRuntimeDefaultOpt(workloads.Docker, "endpoint"), "Path to docker runtime socket (DEPRECATED: use container-runtime-endpoint instead)")
 	option.BindEnv(option.Docker)
+
+	flags.Bool(option.EnableAutoDirectRoutingName, defaults.EnableAutoDirectRouting, "Enable automatic L2 routing between nodes")
+	option.BindEnv(option.EnableAutoDirectRoutingName)
 
 	flags.String(option.EnablePolicy, option.DefaultEnforcement, "Enable policy enforcement")
 	option.BindEnv(option.EnablePolicy)
@@ -931,6 +935,10 @@ func initEnv(cmd *cobra.Command) {
 		if option.Config.InstallIptRules {
 			option.Config.Ipvlan.OperationMode = option.OperationModeL3S
 		}
+
+		// Currently, cilium-health cannot run in ipvlan mode as it
+		// tries to connect to network in veth mode, thus we disable it
+		option.Config.EnableHealthChecking = false
 	default:
 		log.WithField(logfields.DatapathMode, option.Config.DatapathMode).Fatal("Invalid datapath mode")
 	}
@@ -1011,6 +1019,10 @@ func waitForHostDeviceWhenReady(ifaceName string) {
 }
 
 func runDaemon() {
+	datapathConfig := linuxdatapath.DatapathConfiguration{
+		HostDevice: option.Config.HostDevice,
+	}
+
 	log.Info("Initializing daemon")
 
 	// Since flannel doesn't create the cni0 interface until the first container
@@ -1021,7 +1033,7 @@ func runDaemon() {
 		waitForHostDeviceWhenReady(option.Config.FlannelMasterDevice)
 	}
 
-	d, restoredEndpoints, err := NewDaemon()
+	d, restoredEndpoints, err := NewDaemon(linuxdatapath.NewDatapath(datapathConfig))
 	if err != nil {
 		log.WithError(err).Fatal("Error while creating daemon")
 		return
@@ -1083,6 +1095,9 @@ func runDaemon() {
 	}
 
 	if option.Config.IsFlannelMasterDeviceSet() {
+		// health checking is not supported by flannel
+		option.Config.EnableHealthChecking = false
+
 		err := node.SetInternalIPv4From(option.Config.FlannelMasterDevice)
 		if err != nil {
 			log.WithError(err).WithField("device", option.Config.FlannelMasterDevice).Fatal("Unable to set internal IPv4")
@@ -1104,14 +1119,7 @@ func runDaemon() {
 		d.workloadsEventsCh = eventsCh
 	}
 
-	if k8s.IsEnabled() {
-		// Inject K8s dependency into packages which need to annotate K8s resources.
-		endpoint.EpAnnotator = k8s.Client()
-	}
-
-	// Currently, cilium-health cannot run in ipvlan mode as it tries to connect
-	// to network in veth mode, thus we disable it
-	if option.Config.DatapathMode != option.DatapathModeIpvlan {
+	if option.Config.EnableHealthChecking {
 		d.initHealth()
 	}
 

daemon/daemon_test.go
@@ -17,8 +17,6 @@
 package main
 
 import (
-	"github.com/cilium/cilium/pkg/identity/cache"
-	"github.com/cilium/cilium/pkg/revert"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -28,9 +26,11 @@ import (
 
 	"github.com/cilium/cilium/common"
 	"github.com/cilium/cilium/pkg/completion"
+	fakedatapath "github.com/cilium/cilium/pkg/datapath/fake"
 	e "github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
@@ -38,6 +38,7 @@ import (
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/proxy/accesslog"
+	"github.com/cilium/cilium/pkg/revert"
 
 	. "gopkg.in/check.v1"
 )
@@ -102,7 +103,7 @@ func (ds *DaemonSuite) SetUpTest(c *C) {
 	// state left on disk.
 	option.Config.EnableHostIPRestore = false
 
-	d, _, err := NewDaemon()
+	d, _, err := NewDaemon(fakedatapath.NewDatapath())
 	c.Assert(err, IsNil)
 	ds.d = d
 

daemon/k8s_watcher.go
@@ -1784,17 +1784,7 @@ func (d *Daemon) updateK8sNodeTunneling(k8sNodeOld, k8sNodeNew *v1.Node) error {
 		return fmt.Errorf("ipcache entry owned by kvstore or agent")
 	}
 
-	routeTypes := node.TunnelRoute
-
-	// Add IPv6 routing only in non encap. With encap we do it with bpf tunnel
-	// FIXME create a function to know on which mode is the daemon running on
-	var ownAddr net.IP
-	if option.Config.AutoIPv6NodeRoutes && option.Config.Device != "undefined" {
-		ownAddr = node.GetIPv6()
-		routeTypes |= node.DirectRoute
-	}
-
-	node.UpdateNode(nodeNew, routeTypes, ownAddr)
+	d.nodeDiscovery.manager.NodeUpdated(*nodeNew)
 
 	return nil
 }
@@ -1816,6 +1806,12 @@ func (d *Daemon) updateK8sNodeV1(k8sNodeOld, k8sNodeNew *v1.Node) error {
 }
 
 func (d *Daemon) deleteK8sNodeV1(k8sNode *v1.Node) error {
+	oldNode := k8s.ParseNode(k8sNode, node.FromKubernetes)
+	// Ignore own node
+	if oldNode.Name == node.GetName() {
+		return nil
+	}
+
 	ip := k8sNode.GetAnnotations()[annotation.CiliumHostIP]
 
 	logger := log.WithFields(logrus.Fields{
@@ -1823,12 +1819,7 @@ func (d *Daemon) deleteK8sNodeV1(k8sNode *v1.Node) error {
 		logfields.IPAddr: ip,
 	})
 
-	ni := node.Identity{
-		Name:    k8sNode.ObjectMeta.Name,
-		Cluster: option.Config.ClusterName,
-	}
-
-	node.DeleteNode(ni, node.TunnelRoute|node.DirectRoute)
+	d.nodeDiscovery.manager.NodeDeleted(*oldNode)
 
 	id, exists := ipcache.IPIdentityCache.LookupByIP(ip)
 	if !exists {
@@ -1870,7 +1861,7 @@ func updateK8sEventMetric(scope string, action string, status bool) {
 // associated with the right node.
 func (d *Daemon) missingK8sNodeV1(m versioned.Map) versioned.Map {
 	missing := versioned.NewMap()
-	nodes := node.GetNodes()
+	nodes := d.nodeDiscovery.manager.GetNodes()
 	for k, v := range m {
 		n := v.Data.(*v1.Node)
 		ciliumHostIPStr := n.GetAnnotations()[annotation.CiliumHostIP]

daemon/k8s_watcher_test.go
@@ -526,7 +526,7 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 }
 
 func (ds *DaemonSuite) Test_missingK8sNodeV1(c *C) {
-	defer node.DeleteAllNodes()
+	defer ds.d.nodeDiscovery.manager.DeleteAllNodes()
 	prevClusterName := option.Config.ClusterName
 	option.Config.ClusterName = "default"
 	defer func() {
@@ -591,22 +591,23 @@ func (ds *DaemonSuite) Test_missingK8sNodeV1(c *C) {
 		{
 			name: "ipcache and the node package contains the node. Should be no-op",
 			setupArgs: func() args {
-				node.DeleteAllNodes()
+				ds.d.nodeDiscovery.manager.DeleteAllNodes()
 				cache := ipcache.NewIPCache()
 				cache.Upsert("172.20.0.1", net.ParseIP("172.20.0.2"), ipcache.Identity{
 					ID:     identity.ReservedIdentityInit,
 					Source: ipcache.FromKubernetes,
 				})
-				node.UpdateNode(&node.Node{
+				ds.d.nodeDiscovery.manager.NodeUpdated(node.Node{
 					Name:    "foo",
 					Cluster: "default",
+					Source:  node.FromKubernetes,
 					IPAddresses: []node.Address{
 						{
 							Type: nodeAddressing.NodeInternalIP,
 							IP:   net.ParseIP("172.20.0.1"),
 						},
 					},
-				}, 0, net.ParseIP("172.20.0.2"))
+				})
 
 				m := versioned.NewMap()
 				m.Add("", versioned.Object{
@@ -632,7 +633,7 @@ func (ds *DaemonSuite) Test_missingK8sNodeV1(c *C) {
 		{
 			name: "node doesn't contain any cilium host IP. should be no-op",
 			setupArgs: func() args {
-				node.DeleteAllNodes()
+				ds.d.nodeDiscovery.manager.DeleteAllNodes()
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
 					ID:     identity.ReservedIdentityInit,
@@ -662,14 +663,15 @@ func (ds *DaemonSuite) Test_missingK8sNodeV1(c *C) {
 		{
 			name: "ipcache contains the node but the CiliumHostIP is not the right one for the given nodeIP",
 			setupArgs: func() args {
-				node.DeleteAllNodes()
+				ds.d.nodeDiscovery.manager.DeleteAllNodes()
 				cache := ipcache.NewIPCache()
 				cache.Upsert("172.20.9.1", net.ParseIP("172.20.1.1"), ipcache.Identity{
 					ID:     identity.ReservedIdentityInit,
 					Source: ipcache.FromKubernetes,
 				})
-				node.UpdateNode(&node.Node{
+				ds.d.nodeDiscovery.manager.NodeUpdated(node.Node{
 					Name:    "bar",
+					Source:  node.FromAgentLocal,
 					Cluster: "default",
 					IPAddresses: []node.Address{
 						{
@@ -677,7 +679,7 @@ func (ds *DaemonSuite) Test_missingK8sNodeV1(c *C) {
 							IP:   net.ParseIP("172.20.0.1"),
 						},
 					},
-				}, 0, net.ParseIP("172.20.2.1"))
+				})
 				m := versioned.NewMap()
 				m.Add("", versioned.Object{
 					Data: &core_v1.Node{

commit aa9fd7975990e3af8e102589bb2942d4b83952dc

health: Fix health check to local health agent
Fixes: #6728
Fixes: be7d5cb4b919 ("node: Use new routing datapath for node events")

Signed-off-by: Thomas Graf <thomas@cilium.io>

cilium-health/launch/endpoint.go
@@ -197,20 +197,22 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 			State:         models.EndpointStateWaitingForIdentity,
 			Addressing:    &models.AddressPair{},
 		}
-		ip4                    net.IP
 		ip4Address, ip6Address string
+		healthIP               net.IP
 	)
 
 	if option.Config.EnableIPv4 {
 		info.Addressing.IPV4 = ip4health.String()
 		ip4WithMask := net.IPNet{IP: ip4health, Mask: defaults.ContainerIPv4Mask}
 		ip4Address = ip4WithMask.String()
+		healthIP = ip4health
 	}
 
 	if option.Config.EnableIPv6 {
 		info.Addressing.IPV6 = ip6health.String()
 		ip6WithMask := net.IPNet{IP: ip6health, Mask: defaults.ContainerIPv6Mask}
 		ip6Address = ip6WithMask.String()
+		healthIP = ip6health
 	}
 
 	if _, _, err := connector.SetupVethWithNames(vethName, vethPeerName, mtuConfig.GetDeviceMTU(), info); err != nil {
@@ -224,6 +226,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 	prog := filepath.Join(option.Config.BpfDir, "spawn_netns.sh")
 	cmd.SetTarget(prog)
 	cmd.SetArgs(args)
+	log.Infof("Spawning health endpoint with arguments %#v", args)
 	if err := cmd.Run(); err != nil {
 		return err
 	}
@@ -280,7 +283,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 
 	// Initialize the health client to talk to this instance. This is why
 	// the caller must limit usage of this package to a single goroutine.
-	client, err = healthPkg.NewClient(fmt.Sprintf("tcp://%s:%d", ip4, healthDefaults.HTTPPathPort))
+	client, err = healthPkg.NewClient("tcp://" + net.JoinHostPort(healthIP.String(), fmt.Sprintf("%d", healthDefaults.HTTPPathPort)))
 	if err != nil {
 		return fmt.Errorf("Cannot establish connection to health endpoint: %s", err)
 	}

daemon/health.go
@@ -58,6 +58,7 @@ func (d *Daemon) initHealth() {
 				return d.runCiliumHealthEndpoint(d.nodeDiscovery.localNode.IPv4HealthIP, d.nodeDiscovery.localNode.IPv6HealthIP)
 			},
 			StopFunc: func() error {
+				log.Info("Stopping health endpoint")
 				err := health.PingEndpoint()
 				d.cleanupHealthEndpoint(d.nodeDiscovery.localNode.IPv4HealthIP)
 
@@ -89,6 +90,7 @@ func (d *Daemon) cleanupHealthEndpoint(healthIPv4 net.IP) {
 func (d *Daemon) runCiliumHealthEndpoint(healthIPv4, healthIPv6 net.IP) error {
 	// PingEndpoint will always fail the first time (initialization).
 	if err := health.PingEndpoint(); err != nil {
+		log.WithError(err).Warning("health endpoint is unreachable, restarting health endpoint")
 		d.cleanupHealthEndpoint(healthIPv4)
 		addressing := node.GetNodeAddressing()
 		return health.LaunchAsEndpoint(d, addressing, d.mtuConfig, healthIPv4, healthIPv6)