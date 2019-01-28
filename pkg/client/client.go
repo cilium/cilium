cilium/cilium

commit d48af6ab5c286fd91b751eed90a7d1b715a55231

pkg/client: wait until the client has connectivity with daemon
Fixes: c6518190de30 ("client: Add NewDefaultClientWithTimeout function")
Signed-off-by: André Martins <andre@cilium.io>

pkg/client/client.go
@@ -86,14 +86,18 @@ func NewDefaultClientWithTimeout(timeout time.Duration) (*Client, error) {
 	for {
 		select {
 		case <-timeoutAfter:
-			return c, fmt.Errorf("Failed to create cilium agent client after %f seconds timeout: %s", timeout.Seconds(), err)
+			return nil, fmt.Errorf("Failed to create cilium agent client after %f seconds timeout: %s", timeout.Seconds(), err)
 		default:
 		}
 
 		c, err = NewDefaultClient()
 		if err == nil {
-			break
+			_, err := c.Daemon.GetConfig(nil)
+			if err == nil {
+				break
+			}
 		}
+		time.Sleep(500 * time.Millisecond)
 	}
 
 	return c, nil

commit 9a90e67cb2639da2144468fe32468d5b43fafe1f

client: Use one err variable in client with timeout
Before this change, `GetConfig` call in `NewDefaultClientWithTimeout`
was using its own `err` variable, which was never returned and nil error
was returned instead.

Fixes: d48af6ab5c28 ("pkg/client: wait until the client has connectivity
with daemon")
Fixes: #6801

Signed-off-by: Michal Rostecki <mrostecki@suse.de>

pkg/client/client.go
@@ -92,7 +92,7 @@ func NewDefaultClientWithTimeout(timeout time.Duration) (*Client, error) {
 
 		c, err = NewDefaultClient()
 		if err == nil {
-			_, err := c.Daemon.GetConfig(nil)
+			_, err = c.Daemon.GetConfig(nil)
 			if err == nil {
 				break
 			}