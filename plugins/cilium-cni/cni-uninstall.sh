e59ce0054813e419ec7e4fec6c07902127846963
André Martins <andre@cilium.io>
2019-02-01 13:30:35 +0100

    plugins/cilium-cni: remove bpf programs installed by Cilium if user opt-in
    Signed-off-by: André Martins <andre@cilium.io>

--- plugins/cilium-cni/cni-uninstall.sh
+++ plugins/cilium-cni/cni-uninstall.sh
@@ -1,9 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+function uninstall_cilium_from_flannel_master() {
+    CNI_MASTER_DEVICE="${1}"
+    # Those are the settings set by default from flannel
+    [[ "$(tc filter show dev "${CNI_MASTER_DEVICE}" egress)" != "" ]] && \
+        tc filter delete dev "${CNI_MASTER_DEVICE}" egress pref 1 handle 1 bpf || \
+        echo 1 > "/proc/sys/net/ipv4/conf/${CNI_MASTER_DEVICE}/forwarding" && \
+        echo 1 > "/proc/sys/net/ipv4/conf/${CNI_MASTER_DEVICE}/rp_filter" && \
+        echo 0 > "/proc/sys/net/ipv4/conf/${CNI_MASTER_DEVICE}/accept_local" && \
+        echo 1 > "/proc/sys/net/ipv4/conf/${CNI_MASTER_DEVICE}/send_redirects" && \
+        echo 1 > "/proc/sys/net/ipv4/conf/all/rp_filter"
+}
+
+function uninstall_cilium_from_pod() {
+    POD_VETH_SIDE_PAIR="${1}"
+    [[ "$(tc filter show dev "${POD_VETH_SIDE_PAIR}" ingress)" != "" ]] && \
+        tc filter delete dev "${POD_VETH_SIDE_PAIR}" ingress pref 1 handle 1 bpf || \
+        echo 1 > "/proc/sys/net/ipv4/conf/${POD_VETH_SIDE_PAIR}/rp_filter"
+}
+
+function get_list_of_veth_from_bridge() {
+    CNI_MASTER_DEVICE="${1}"
+    echo "$(ip -o link show master "${CNI_MASTER_DEVICE}" type veth | awk '{print $2}' | sed 's/@.*//g')"
+}
+
 HOST_PREFIX=${HOST_PREFIX:-/host}
-if [ -z "${CILIUM_FLANNEL_MASTER_DEVICE}" ]; then
+if [[ -z "${CILIUM_FLANNEL_MASTER_DEVICE}" ]]; then
     CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conf}
 else
     CNI_CONF_NAME=${CNI_CONF_NAME:-04-flannel-cilium-cni.conflist}
@@ -18,15 +42,19 @@ rm -f ${CNI_DIR}/bin/${BIN_NAME}
 rm -f ${CNI_DIR}/bin/${BIN_NAME}.old
 
 
-if [ -z "${CILIUM_FLANNEL_MASTER_DEVICE}" ]; then
+if [[ -z "${CILIUM_FLANNEL_MASTER_DEVICE}" ]]; then
     echo "Removing ${CILIUM_CNI_CONF} ..."
     rm -f ${CILIUM_CNI_CONF}
-elif [ -z "${CILIUM_FLANNEL_UNINSTALL_ON_EXIT}" ]; then
+elif [[ "${CILIUM_FLANNEL_UNINSTALL_ON_EXIT}" == "true" ]]; then
     echo "Removing ${CILIUM_CNI_CONF} ..."
     rm -f ${CILIUM_CNI_CONF}
 
     echo "Removing BPF programs from all containers and from ${CILIUM_FLANNEL_MASTER_DEVICE}"
-    tc filter delete dev cni0 egress pref 1 handle 1 bpf || true
-    # TODO create script to detect all interfaces that have bpf programs
-    # installed
+    echo "Uninstalling cilium from ${CILIUM_FLANNEL_MASTER_DEVICE}"
+    uninstall_cilium_from_flannel_master "${CILIUM_FLANNEL_MASTER_DEVICE}"
+
+    for iDev in $(get_list_of_veth_from_bridge "${CILIUM_FLANNEL_MASTER_DEVICE}"); do
+        echo "Uninstalling cilium from ${iDev}"
+        uninstall_cilium_from_pod ${iDev}
+    done
 fi
