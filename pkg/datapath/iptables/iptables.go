cilium/cilium

commit c6888d48780e957a475038b421c64ef3888a6881

iptables: Ensure NAT for locally-sourced packets
If a local application attempts to access a nodeport on the local node
via localhost/127.0.0.1, then both the source and destination of the
packet will be 127.0.0.1. The destination will be NATed to the
underlying pod's address, however if the source address is not NATed
then the traffic will arrive into the pod with the 127.0.0.1 source
address, which inside the pod will be considered the local network
namespace's loopback IP. As such, the responses to such requests will be
looped within the pod rather than being routed back out to the host.

Fix this by not clearing the k8s masquerade mark if the source address
is 127.0.0.1.

Fixes: #6381

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/datapath/iptables/iptables.go
@@ -209,6 +209,7 @@ func InstallRules() error {
 		"-t", "mangle",
 		"-A", ciliumPostMangleChain,
 		"-o", defaults.HostDevice,
+		"!", "-s", "127.0.0.1",
 		"-m", "comment", "--comment", "cilium: clear masq bit for pkts to " + defaults.HostDevice,
 		"-j", "MARK", "--set-xmark", clearMasqBit}, false); err != nil {
 		return err

commit 8bdfa2b4552954754c21ca99ec1d130c5cec42ac

iptables: Fix 127.0.0.1:NodePort to remote pods
Commit c6888d48780 fixed 127.0.0.1:NodePort redirection by adding an additional
clause to the kube-proxy masquerade bit clearing rule to ensure that kube-proxy
masquerades all traffic with a source IP of 127.0.0.1. This fixes NodePort
redirection to local pods but it can cause problem when running in
encapsulation mode depending on the rp_filter settings of nodes because the
source IP of the traffic will have a source IP equal to any of the node IPs
which are not routable on the overlay. This causes reply traffic to be routed
back via the underlay which the reverse path filter can potentially drop.

Fixes: c6888d48780 ("iptables: Ensure NAT for locally-sourced packets")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/datapath/iptables/iptables.go
@@ -213,7 +213,6 @@ func InstallRules(ifName string) error {
 		"-o", ifName,
 		"-m", "mark", "!", "--mark", matchFromIPSecDecrypt, // Don't match ipsec traffic
 		"-m", "mark", "!", "--mark", matchFromIPSecEncrypt, // Don't match ipsec traffic
-		"!", "-s", "127.0.0.1",
 		"-m", "comment", "--comment", "cilium: clear masq bit for pkts to " + ifName,
 		"-j", "MARK", "--set-xmark", clearMasqBit}, false); err != nil {
 		return err
@@ -302,6 +301,24 @@ func InstallRules(ifName string) error {
 			"-m", "mark", "!", "--mark", matchFromIPSecEncrypt, // Don't match ipsec traffic
 			"-o", ifName,
 			"-m", "comment", "--comment", "cilium host->cluster masquerade",
+			"-j", "SNAT", "--to-source", node.GetHostMasqueradeIPv4().String()}, false); err != nil {
+			return err
+		}
+
+		// Masquerade all traffic from the host into the ifName
+		// interface if the source is 127.0.0.1
+		//
+		// The following conditions must be met:
+		// * Must be targeted for the ifName interface
+		// * Must be from 127.0.0.1
+		if err := runProg("iptables", []string{
+			"-t", "nat",
+			"-A", ciliumPostNatChain,
+			"-s", "127.0.0.1",
+			"-m", "mark", "!", "--mark", matchFromIPSecDecrypt, // Don't match ipsec traffic
+			"-m", "mark", "!", "--mark", matchFromIPSecEncrypt, // Don't match ipsec traffic
+			"-o", ifName,
+			"-m", "comment", "--comment", "cilium host->cluster from 127.0.0.1 masquerade",
 			"-j", "SNAT", "--to-source", node.GetHostMasqueradeIPv4().String()}, false); err != nil {
 			return err
 		}