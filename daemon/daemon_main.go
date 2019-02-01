cilium/cilium

commit 894365fc6b0ee00437d633462cd5d7a1da7c2c55

daemon: Handle listen/serve errors with channels
Metrics server was always running in goroutine without error
channel. Daemon module expected an error to be returned, which
was never happening.

After this change, both metrics server and Cilium daemon server
are running in goroutines with error channels and then errors
are handled by `select` function.

Signed-off-by: Michal Rostecki <mrostecki@suse.de>

daemon/daemon_main.go
@@ -884,7 +884,7 @@ func runDaemon() {
 
 	d.initHealth()
 
-	initMetrics()
+	metricsErrs := initMetrics()
 
 	api := d.instantiateAPI()
 
@@ -907,7 +907,16 @@ func runDaemon() {
 	log.WithField("bootstrapTime", time.Since(bootstrapTimestamp)).
 		Info("Daemon initialization completed")
 
-	if err := server.Serve(); err != nil {
+	errs := make(chan error, 1)
+
+	go func() {
+		errs <- server.Serve()
+	}()
+
+	select {
+	case err := <-metricsErrs:
+		log.WithError(err).Fatal("Cannot start metrics server")
+	case err := <-errs:
 		log.WithError(err).Fatal("Error returned from non-returning Serve() call")
 	}
 }

daemon/metrics.go
@@ -45,15 +45,17 @@ func (h *getMetrics) Handle(params restapi.GetMetricsParams) middleware.Responde
 	return restapi.NewGetMetricsOK().WithPayload(metrics)
 }
 
-func initMetrics() {
+func initMetrics() <-chan error {
+	var errs <-chan error
+
 	promAddr := viper.GetString("prometheus-serve-addr")
 	if promAddr == "" {
 		promAddr = viper.GetString("prometheus-serve-addr-deprecated")
 	}
 	if promAddr != "" {
 		log.Infof("Serving prometheus metrics on %s", promAddr)
-		if err := metrics.Enable(promAddr); err != nil {
-			log.WithError(err).Fatal("Error while starting metrics")
-		}
+		errs = metrics.Enable(promAddr)
 	}
+
+	return errs
 }

pkg/metrics/metrics.go
@@ -30,7 +30,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	dto "github.com/prometheus/client_model/go"
-	log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -509,15 +508,17 @@ func MustRegister(c prometheus.Collector) {
 
 // Enable begins serving prometheus metrics on the address passed in. Addresses
 // of the form ":8080" will bind the port on all interfaces.
-func Enable(addr string) error {
+func Enable(addr string) <-chan error {
+	errs := make(chan error, 1)
+
 	go func() {
 		// The Handler function provides a default handler to expose metrics
 		// via an HTTP server. "/metrics" is the usual endpoint for that.
 		http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
-		log.WithError(http.ListenAndServe(addr, nil)).Warnf("Cannot start metrics server on %s", addr)
+		errs <- http.ListenAndServe(addr, nil)
 	}()
 
-	return nil
+	return errs
 }
 
 // GetCounterValue returns the current value

commit 9617cf67bb60f460ad5b9944a788b03bb74181c2

daemon: only fatal if err != nil
Fixes: 894365fc6b0e ("daemon: Handle listen/serve errors with channels")
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon_main.go
@@ -1191,9 +1191,13 @@ func runDaemon() {
 
 	select {
 	case err := <-metricsErrs:
-		log.WithError(err).Fatal("Cannot start metrics server")
+		if err != nil {
+			log.WithError(err).Fatal("Cannot start metrics server")
+		}
 	case err := <-errs:
-		log.WithError(err).Fatal("Error returned from non-returning Serve() call")
+		if err != nil {
+			log.WithError(err).Fatal("Error returned from non-returning Serve() call")
+		}
 	}
 }
 