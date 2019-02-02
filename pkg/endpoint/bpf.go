cilium/cilium

commit 8eb1698b2f2ac8980b2d8a4a58bf581f1de70279

endpoint: Avoid harmless controller failures when endpoint disappears
When the endpoint is no longer alive during a controller run, stop the
controller gracefully and wait for it to be deleted.

Avoids a spam of controller failures when pods are restarted or endpoints
disappear for another reason.

Example:
```
msg="Controller run failed" consecutiveErrors=1 error="rlock failed: endpoint is in the process of being removed" name="sync-identity-to-k8s-pod (3558)" subsys=controller uuid=0ea5b9e4-25d6-11e9-8745-080027d2d952
msg="Controller run failed" consecutiveErrors=2 error="rlock failed: endpoint is in the process of being removed" name="sync-identity-to-k8s-pod (3558)" subsys=controller uuid=0ea5b9e4-25d6-11e9-8745-080027d2d952
msg="Controller run failed" consecutiveErrors=3 error="rlock failed: endpoint is in the process of being removed" name="sync-identity-to-k8s-pod (3558)" subsys=controller uuid=0ea5b9e4-25d6-11e9-8745-080027d2d952
msg="Controller run failed" consecutiveErrors=4 error="rlock failed: endpoint is in the process of being removed" name="sync-identity-to-k8s-pod (3558)" subsys=controller uuid=0ea5b9e4-25d6-11e9-8745-080027d2d952
msg="Controller run failed" consecutiveErrors=1 error="rlock failed: endpoint is in the process of being removed" name="sync-identity-to-k8s-pod (1507)" subsys=controller uuid=0ec539f5-25d6-11e9-8745-080027d2d952
msg="Controller run failed" consecutiveErrors=2 error="rlock failed: endpoint is in the process of being removed" name="sync-identity-to-k8s-pod (1507)" subsys=controller uuid=0ec539f5-25d6-11e9-8745-080027d2d952
msg="Controller run failed" consecutiveErrors=3 error="rlock failed: endpoint is in the process of being removed" name="sync-identity-to-k8s-pod (1507)" subsys=controller uuid=0ec539f5-25d6-11e9-8745-080027d2d952

pkg/endpoint/bpf.go
@@ -1158,8 +1158,8 @@ func (e *Endpoint) syncPolicyMapController() {
 				// Failure to lock is not an error, it means
 				// that the endpoint was disconnected and we
 				// should exit gracefully.
-				if err := e.LockAlive(); err != nil {
-					return nil
+				if err := e.RLockAlive(); err != nil {
+					return controller.NewExitReason("Endpoint disappeared")
 				}
 				defer e.Unlock()
 				return e.syncPolicyMap()

pkg/controller/controller.go
@@ -15,6 +15,7 @@
 package controller
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -39,11 +40,16 @@ type ControllerFunc func() error
 // ExitReason is a returnable type from DoFunc that causes the
 // controller to exit. This reason is recorded in the controller's status. The
 // controller is not removed from any manager.
-// Construct one with ExitReason{errors.New("a reason")}
+// Construct one with NewExitReason("a reason")
 type ExitReason struct {
 	// This is constucted in this odd way because the type assertion in
 	// runController didn't work otherwise.
 	error
+}
+
+// NewExitReason returns a new ExitReason
+func NewExitReason(reason string) ExitReason {
+	return ExitReason{errors.New(reason)}
 }
 
 // ControllerParams contains all parameters of a controller

pkg/controller/controller_test.go
@@ -17,7 +17,6 @@
 package controller
 
 import (
-	"errors"
 	"fmt"
 	"sync/atomic"
 	"testing"
@@ -76,7 +75,7 @@ func (b *ControllerSuite) TestSelfExit(c *C) {
 		RunInterval: 100 * time.Millisecond,
 		DoFunc: func() error {
 			atomic.AddUint32(&iterations, 1)
-			return ExitReason{errors.New("test exit")}
+			return NewExitReason("test exit")
 		},
 		StopFunc: func() error {
 			close(waitChan)

pkg/endpoint/endpoint.go
@@ -2011,7 +2011,7 @@ func (e *Endpoint) runLabelsResolver(owner Owner, myChangeRev int, blocking bool
 				switch err {
 				case ErrNotAlive:
 					e.getLogger().Debug("not changing endpoint identity because endpoint is in process of being removed")
-					return nil
+					return controller.NewExitReason("Endpoint disappeared")
 				default:
 					return err
 				}

pkg/endpoint/policy.go
@@ -466,7 +466,7 @@ func (e *Endpoint) runIdentityToK8sPodSync() {
 				id := ""
 
 				if err := e.RLockAlive(); err != nil {
-					return err
+					return controller.NewExitReason("Endpoint disappeared")
 				}
 				if e.SecurityIdentity != nil {
 					id = e.SecurityIdentity.ID.StringID()
@@ -520,16 +520,8 @@ func (e *Endpoint) runIPIdentitySync(endpointIP addressing.CiliumIP) {
 	e.controllers.UpdateController(fmt.Sprintf("sync-%s-identity-mapping (%d)", addressFamily, e.ID),
 		controller.ControllerParams{
 			DoFunc: func() error {
-
-				// NOTE: this Lock is Unconditional because this controller
-				// handles disconnecting endpoint state properly
-				e.UnconditionalRLock()
-
-				if e.state == StateDisconnected || e.state == StateDisconnecting {
-					log.WithFields(logrus.Fields{logfields.EndpointState: e.state}).
-						Debugf("not synchronizing endpoint IP with kvstore due to endpoint state")
-					e.RUnlock()
-					return nil
+				if err := e.RLockAlive(); err != nil {
+					return controller.NewExitReason("Endpoint disappeared")
 				}
 
 				if e.SecurityIdentity == nil {

commit 6c76304b583722d6449d184abd593b5210b03cc8

endpoint: fix incorrect RLock usage in controller
The below commit accidentally replaced a call to `LockAlive` with `RLockAlive`.
This resulted in the endpoint mutex having `Unlock` called when the endpoint
mutex was `RLocked`, which caused the following error:

```
fatal error: sync: Unlock of unlocked RWMutex

goroutine 996 [running]:
runtime.throw(0x2d25cea, 0x20)
    /usr/local/go/src/runtime/panic.go:608 +0x72 fp=0xc000cdcd30 sp=0xc000cdcd00 pc=0x108fd72
sync.throw(0x2d25cea, 0x20)
    /usr/local/go/src/runtime/panic.go:594 +0x35 fp=0xc000cdcd50 sp=0xc000cdcd30 pc=0x108fcf5
sync.(*RWMutex).Unlock(0xc000f62004)
    /usr/local/go/src/sync/rwmutex.go:124 +0xa1 fp=0xc000cdcd80 sp=0xc000cdcd50 pc=0x10c98e1
github.com/cilium/cilium/pkg/endpoint.(*Endpoint).Unlock(0xc000f62000)
```

Fixes: 8eb1698b2 ("endpoint: Avoid harmless controller failures when endpoint disappears")

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/endpoint/bpf.go
@@ -1158,7 +1158,7 @@ func (e *Endpoint) syncPolicyMapController() {
 				// Failure to lock is not an error, it means
 				// that the endpoint was disconnected and we
 				// should exit gracefully.
-				if err := e.RLockAlive(); err != nil {
+				if err := e.LockAlive(); err != nil {
 					return controller.NewExitReason("Endpoint disappeared")
 				}
 				defer e.Unlock()