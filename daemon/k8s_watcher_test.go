8d463ba529e8518eea8f59087c9d62a05cc88735
Eloy Coto <eloy.coto@gmail.com>
2019-02-04 11:17:02 +0100

    Operator: fix crash on invalid policy.
    If operator got a invalid policy there was a crash, with this change
    operator will log the error.
    
    Sample policy
    ```
    apiVersion: cilium.io/v2
    kind: CiliumNetworkPolicy
    metadata:
      name: allow-egress-ingress-cluster
    spec:
      egress:
      - toEndpoints:
        - matchLabels:
            'k8s:io.kubernetes.pod.namespace': {}
      endpointSelector:
        matchLabels: {}
      ingress:
      - fromEndpoints:
        - matchLabels:
            'k8s:io.kubernetes.pod.namespace': {}
    ```
    
    Evidence:
    ```
    NAMESPACE     NAME                                       READY   STATUS    RESTARTS   AGE
    kube-system   pod/cilium-etcd-8hmkpwk8xs                 1/1     Running   0          36m
    kube-system   pod/cilium-etcd-l8h9rlspc5                 1/1     Running   0          35m
    kube-system   pod/cilium-etcd-lkh4wfvj8z                 1/1     Running   0          37m
    kube-system   pod/cilium-etcd-operator-d6cb86868-pg8wf   1/1     Running   0          37m
    kube-system   pod/cilium-operator-cb9fb7b5-lqnvn         1/1     Running   1          22m
    kube-system   pod/cilium-tqdrf                           1/1     Running   1          37m
    kube-system   pod/coredns-fff89c9b9-cb8dt                1/1     Running   0          45m
    kube-system   pod/etcd-k8s1                              1/1     Running   0          44m
    kube-system   pod/etcd-operator-5cf67779fd-r7kjm         1/1     Running   0          37m
    kube-system   pod/kube-apiserver-k8s1                    1/1     Running   0          44m
    kube-system   pod/kube-controller-manager-k8s1           1/1     Running   0          44m
    kube-system   pod/kube-proxy-ljr7q                       1/1     Running   0          45m
    kube-system   pod/kube-scheduler-k8s1                    1/1     Running   0          44m
    
    NAMESPACE   NAME                                                         AGE
    default     ciliumnetworkpolicy.cilium.io/allow-egress-ingress-cluster   21m
    ```
    
    Fix #6910
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- daemon/k8s_watcher_test.go
+++ daemon/k8s_watcher_test.go
@@ -18,7 +18,6 @@ package main
 
 import (
 	"net"
-	"time"
 
 	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/checker"
@@ -43,27 +42,6 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/cache"
 )
-
-func (ds *DaemonSuite) TestK8sErrorLogTimeout(c *C) {
-	errstr := "I am an error string"
-
-	// ensure k8sErrMsg is empty for tests that use it
-	k8sErrMsgMU.Lock()
-	k8sErrMsg = map[string]time.Time{}
-	k8sErrMsgMU.Unlock()
-
-	// Returns true because it's the first time we see this message
-	startTime := time.Now()
-	c.Assert(k8sErrorUpdateCheckUnmuteTime(errstr, startTime), Equals, true)
-
-	// Returns false because <= k8sErrLogTimeout time has passed
-	noLogTime := startTime.Add(k8sErrLogTimeout)
-	c.Assert(k8sErrorUpdateCheckUnmuteTime(errstr, noLogTime), Equals, false)
-
-	// Returns true because k8sErrLogTimeout has passed
-	shouldLogTime := startTime.Add(k8sErrLogTimeout).Add(time.Nanosecond)
-	c.Assert(k8sErrorUpdateCheckUnmuteTime(errstr, shouldLogTime), Equals, true)
-}
 
 func (ds *DaemonSuite) Test_missingK8sNetworkPolicyV1(c *C) {
 	type args struct {
