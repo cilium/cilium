12fcd23b4b2af5b0b3f8e042795475fc2b999424
André Martins <andre@cilium.io>
2019-02-04 17:06:45 +0100

    test: update k8s versions to v1.11.7 and v1.13.3
    Signed-off-by: André Martins <andre@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -166,7 +166,7 @@ case $K8S_VERSION in
         ;;
     "1.11")
         KUBERNETES_CNI_VERSION="0.6.0"
-        K8S_FULL_VERSION="1.11.6"
+        K8S_FULL_VERSION="1.11.7"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri,FileExisting-crictl,SystemVerification"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,FileExisting-crictl,SystemVerification"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
@@ -181,7 +181,7 @@ case $K8S_VERSION in
         ;;
     "1.13")
         KUBERNETES_CNI_VERSION="0.6.0"
-        K8S_FULL_VERSION="1.13.2"
+        K8S_FULL_VERSION="1.13.3"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
