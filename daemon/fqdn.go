92fae0343fc805f4f202e1a42b277fc678a3234d
Maciej Kwiek <maciej@covalent.io>
2019-02-04 16:35:59 +0100

    Add DNS message info to access log
    Signed-off-by: Maciej Kwiek <maciej@covalent.io>

--- daemon/fqdn.go
+++ daemon/fqdn.go
@@ -230,7 +230,7 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState) (err err
 				return err
 			}
 
-			qname, responseIPs, TTL, CNAMEs, err := dnsproxy.ExtractMsgDetails(msg)
+			qname, responseIPs, TTL, CNAMEs, rcode, recordTypes, qTypes, err := dnsproxy.ExtractMsgDetails(msg)
 			if err != nil {
 				// This error is ok because all these values are used for reporting, or filling in the cache.
 				log.WithError(err).Error("cannot extract DNS message details")
@@ -251,6 +251,9 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState) (err err
 					TTL:               TTL,
 					CNAMEs:            CNAMEs,
 					ObservationSource: accesslog.DNSSourceProxy,
+					RCode:             rcode,
+					QTypes:            qTypes,
+					AnswerTypes:       recordTypes,
 				}),
 			)
 			record.Log()
--- pkg/proxy/accesslog/record.go
+++ pkg/proxy/accesslog/record.go
@@ -282,6 +282,21 @@ type LogRecordDNS struct {
 	// Empty or undefined may indicate older cilium versions, as it is expected
 	// to be filled in.
 	ObservationSource DNSDataSource `json:"ObservationSource,omitempty"`
+
+	// RCode is the response code
+	// defined as per https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-6
+	// Use 	github.com/miekg/dns.RcodeToString map to retrieve string representation
+	RCode int
+
+	// QTypes are question types in DNS message
+	// https://www.ietf.org/rfc/rfc1035.txt
+	// Use github.com/miekg/dns.TypeToString map to retrieve string representation
+	QTypes []uint16
+
+	// AnswerTypes are record types in the answer section
+	// https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-4
+	// Use github.com/miekg/dns.TypeToString map to retrieve string representation
+	AnswerTypes []uint16
 }
 
 // LogRecordL7 contains the generic L7 portion of a log record
