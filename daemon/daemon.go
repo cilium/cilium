cilium/cilium

commit d49267940840686050707903f82b1ad5dbf06509

workloads: Setup options in single function
Previously, workloads runtime options can be set from two different
functions, namely `ParseConfigEndpoint` and `Setup`, which made code
difficult to follow. One possible result of it was that logging
of runtime options was happening at the wrong place.

This commit gets rid of the former function by moving all workloads
init steps to the latter function.

To make unit testing of `Setup` possible to some certain extent without
mocking runtime clients, we introduce `bypassStatusCheck` flag which
should be set to `true` only when testing.

Note, that I removed the containerd runtime from the tests, as it
internally tries to call its endpoint during initialization.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/daemon.go
@@ -1136,15 +1136,35 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 		EnableIPv6: option.Config.EnableIPv6,
 	})
 
-	// Workloads must be initialized after IPAM has started as it requires
-	// to allocate IPs.
-	wOpts := map[workloads.WorkloadRuntimeType]map[string]string{
-		workloads.Docker:     {workloads.DatapathModeOpt: option.Config.DatapathMode},
-		workloads.ContainerD: make(map[string]string),
-		workloads.CRIO:       make(map[string]string),
-	}
-	if err := workloads.Setup(d.ipam, option.Config.Workloads, wOpts); err != nil {
-		return nil, nil, fmt.Errorf("unable to setup workload: %s", err)
+	if option.Config.WorkloadsEnabled() {
+		// workaround for to use the values of the deprecated dockerEndpoint
+		// variable if it is set with a different value than defaults.
+		defaultDockerEndpoint := workloads.GetRuntimeDefaultOpt(workloads.Docker, "endpoint")
+		if defaultDockerEndpoint != option.Config.DockerEndpoint {
+			option.Config.ContainerRuntimeEndpoint[string(workloads.Docker)] = option.Config.DockerEndpoint
+			log.Warn(`"docker" flag is deprecated.` +
+				`Please use "--container-runtime-endpoint=docker=` + defaultDockerEndpoint + `" instead`)
+		}
+
+		opts := make(map[workloads.WorkloadRuntimeType]map[string]string)
+		for _, rt := range option.Config.Workloads {
+			opts[workloads.WorkloadRuntimeType(rt)] = make(map[string]string)
+		}
+		for rt, ep := range option.Config.ContainerRuntimeEndpoint {
+			opts[workloads.WorkloadRuntimeType(rt)][workloads.EpOpt] = ep
+		}
+		if opts[workloads.Docker] == nil {
+			opts[workloads.Docker] = make(map[string]string)
+		}
+		opts[workloads.Docker][workloads.DatapathModeOpt] = option.Config.DatapathMode
+
+		// Workloads must be initialized after IPAM has started as it requires
+		// to allocate IPs.
+		if err := workloads.Setup(d.ipam, option.Config.Workloads, opts); err != nil {
+			return nil, nil, fmt.Errorf("unable to setup workload: %s", err)
+		}
+
+		log.Infof("Container runtime options set: %s", workloads.GetRuntimeOptions())
 	}
 	log.Infof("Container runtime options set: %s", workloads.GetRuntimeOptions())
 

daemon/daemon_main.go
@@ -989,23 +989,6 @@ func initEnv(cmd *cobra.Command) {
 		}
 	}
 
-	if option.Config.WorkloadsEnabled() {
-		// workaround for to use the values of the deprecated dockerEndpoint
-		// variable if it is set with a different value than defaults.
-		defaultDockerEndpoint := workloads.GetRuntimeDefaultOpt(workloads.Docker, "endpoint")
-		if defaultDockerEndpoint != option.Config.DockerEndpoint {
-			option.Config.ContainerRuntimeEndpoint[string(workloads.Docker)] = option.Config.DockerEndpoint
-			log.Warn(`"docker" flag is deprecated.` +
-				`Please use "--container-runtime-endpoint=docker=` + defaultDockerEndpoint + `" instead`)
-		}
-
-		err = workloads.ParseConfigEndpoint(option.Config.Workloads, option.Config.ContainerRuntimeEndpoint)
-		if err != nil {
-			log.WithError(err).Fatal("Unable to initialize policy container runtimes")
-			return
-		}
-	}
-
 	if option.Config.SidecarHTTPProxy {
 		log.Warn(`"sidecar-http-proxy" flag is deprecated and has no effect`)
 	}

pkg/workloads/config.go
@@ -78,7 +78,7 @@ var (
 	allocator allocatorInterface
 )
 
-func setup(workloadRuntime WorkloadRuntimeType, opts map[string]string) error {
+func setupWorkload(workloadRuntime WorkloadRuntimeType, opts map[string]string) error {
 	workloadMod := getWorkload(workloadRuntime)
 	if workloadMod == nil {
 		return fmt.Errorf("unknown workloadRuntime runtime type %s", workloadRuntime)
@@ -98,7 +98,18 @@ type allocatorInterface interface {
 // Setup sets up the workload runtime specified in workloadRuntime and configures it
 // with the options provided in opts
 func Setup(a allocatorInterface, workloadRuntimes []string, opts map[WorkloadRuntimeType]map[string]string) error {
-	var err error
+	return setup(a, workloadRuntimes, opts, false)
+}
+
+func setup(a allocatorInterface, workloadRuntimes []string, opts map[WorkloadRuntimeType]map[string]string, bypassStatusCheck bool) error {
+	var (
+		st  *models.Status
+		err error
+	)
+
+	if bypassStatusCheck {
+		st = &models.Status{State: models.StatusStateOk}
+	}
 
 	setupOnce.Do(
 		func() {
@@ -112,6 +123,7 @@ func Setup(a allocatorInterface, workloadRuntimes []string, opts map[WorkloadRun
 				}
 				switch crt {
 				case None:
+					unregisterWorkloads()
 					err = nil
 					return
 				}
@@ -124,25 +136,20 @@ func Setup(a allocatorInterface, workloadRuntimes []string, opts map[WorkloadRun
 				}
 				switch crt {
 				case Auto:
-					err1 := setup(Docker, opts[Docker])
-					st := Status()
-					if err1 == nil && st.State == models.StatusStateOk {
-						return
+					var setupErr error
+					for _, rt := range []WorkloadRuntimeType{Docker, ContainerD, CRIO} {
+						setupErr = setupWorkload(rt, opts[rt])
+						if !bypassStatusCheck {
+							st = Status()
+						}
+						if setupErr == nil && st.State == models.StatusStateOk {
+							return
+						}
 					}
-					err2 := setup(ContainerD, opts[ContainerD])
-					st = Status()
-					if err2 == nil && st.State == models.StatusStateOk {
-						return
-					}
-					err3 := setup(CRIO, opts[CRIO])
-					st = Status()
-					if err3 == nil && st.State == models.StatusStateOk {
-						return
-					}
-					err = err1
+					err = setupErr
 					return
 				default:
-					err = setup(crt, opts[crt])
+					err = setupWorkload(crt, opts[crt])
 					return
 				}
 			}

pkg/workloads/containerd.go
@@ -54,7 +54,7 @@ const (
 var (
 	containerDInstance = &containerDModule{
 		opts: workloadRuntimeOpts{
-			epOpt: &workloadRuntimeOpt{
+			EpOpt: &workloadRuntimeOpt{
 				description: "Address of containerD endpoint",
 				value:       containerDEndpoint,
 			},
@@ -95,7 +95,7 @@ type containerDClient struct {
 }
 
 func newContainerDClient(opts workloadRuntimeOpts) (WorkloadRuntime, error) {
-	ep := string(opts[epOpt].value)
+	ep := string(opts[EpOpt].value)
 	c, err := containerd.New(ep)
 	if err != nil {
 		return nil, err
@@ -107,7 +107,7 @@ func newContainerDClient(opts workloadRuntimeOpts) (WorkloadRuntime, error) {
 	if p.Scheme == "" {
 		ep = "unix://" + ep
 	}
-	rsc, err := newCRIClient(context.WithValue(context.Background(), epOpt, ep))
+	rsc, err := newCRIClient(context.WithValue(context.Background(), EpOpt, ep))
 	return &containerDClient{c, rsc}, err
 }
 

pkg/workloads/cri.go
@@ -40,7 +40,7 @@ import (
 )
 
 func getGRPCCLient(ctx context.Context) (*grpc.ClientConn, error) {
-	ep, ok := ctx.Value(epOpt).(string)
+	ep, ok := ctx.Value(EpOpt).(string)
 	if !ok {
 		return nil, fmt.Errorf("unknown runtime endpoint")
 	}

pkg/workloads/crio.go
@@ -33,7 +33,7 @@ const (
 var (
 	criOInstance = &criOModule{
 		opts: workloadRuntimeOpts{
-			epOpt: &workloadRuntimeOpt{
+			EpOpt: &workloadRuntimeOpt{
 				description: "Address of cri-o endpoint",
 				value:       criOEndpoint,
 			},
@@ -73,7 +73,7 @@ type criOClient struct {
 }
 
 func newCRIOClient(opts workloadRuntimeOpts) (WorkloadRuntime, error) {
-	ep := string(opts[epOpt].value)
+	ep := string(opts[EpOpt].value)
 	p, err := url.Parse(ep)
 	if err != nil {
 		return nil, err
@@ -81,7 +81,7 @@ func newCRIOClient(opts workloadRuntimeOpts) (WorkloadRuntime, error) {
 	if p.Scheme == "" {
 		ep = "unix://" + ep
 	}
-	rsc, err := newCRIClient(context.WithValue(context.Background(), epOpt, ep))
+	rsc, err := newCRIClient(context.WithValue(context.Background(), EpOpt, ep))
 	return &criOClient{rsc}, err
 }
 

pkg/workloads/docker.go
@@ -62,7 +62,7 @@ const (
 var (
 	dockerInstance = &dockerModule{
 		opts: workloadRuntimeOpts{
-			epOpt: &workloadRuntimeOpt{
+			EpOpt: &workloadRuntimeOpt{
 				description: "Addresses of docker endpoint",
 				value:       "unix:///var/run/docker.sock",
 			},
@@ -108,7 +108,7 @@ type dockerClient struct {
 
 func newDockerClient(opts workloadRuntimeOpts) (WorkloadRuntime, error) {
 	defaultHeaders := map[string]string{"User-Agent": "cilium"}
-	ep := opts[epOpt]
+	ep := opts[EpOpt]
 	c, err := client.NewClient(ep.value, "v1.21", nil, defaultHeaders)
 	if err != nil {
 		return nil, err

pkg/workloads/runtimes.go
@@ -52,7 +52,8 @@ const (
 )
 
 const (
-	epOpt = "endpoint"
+	// EpOpt is the option name for path to endpoint
+	EpOpt = "endpoint"
 	// DatapathModeOpt is the option name for datapath mode
 	DatapathModeOpt = "datapath-mode"
 )
@@ -102,39 +103,6 @@ var (
 
 func unregisterWorkloads() {
 	registeredWorkloads = map[WorkloadRuntimeType]workloadModule{}
-}
-
-// ParseConfigEndpoint parses the containerRuntimes and the containerRuntime
-// options and adds them to the internal containerRuntime maps.
-// In case any containeRuntime does not exist an error is returned.
-// If `auto` is set in containerRuntimes, the default options of each container
-// runtime will be set. The user defined options of each particular runtime
-// will overwrite defaults.
-// If `none` is set, all containerRuntimes will be ignored.
-func ParseConfigEndpoint(containerRuntimes []string, containerRuntimesEPOpts map[string]string) error {
-	for _, runtime := range containerRuntimes {
-		crt, err := parseRuntimeType(runtime)
-		if err != nil {
-			return err
-		}
-		switch crt {
-		case None:
-			unregisterWorkloads()
-			return nil
-		}
-	}
-
-	for runtime, epOpts := range containerRuntimesEPOpts {
-		crt, _ := parseRuntimeType(runtime)
-		switch crt {
-		case None, Auto:
-		default:
-			registeredWorkloads[crt].setConfig(map[string]string{
-				epOpt: epOpts,
-			})
-		}
-	}
-	return nil
 }
 
 func parseRuntimeType(str string) (WorkloadRuntimeType, error) {
@@ -217,7 +185,7 @@ func GetDefaultEPOptsStringWithPrefix(prefix string) string {
 
 	for _, cr := range crs {
 		v := registeredWorkloads[WorkloadRuntimeType(cr)].getConfig()
-		strs = append(strs, fmt.Sprintf("%s%s=%s", prefix, cr, v[epOpt]))
+		strs = append(strs, fmt.Sprintf("%s%s=%s", prefix, cr, v[EpOpt]))
 	}
 
 	return strings.Join(strs, ", ")

commit 26c30dafc32a7d84e4f163e6e532b8027b5dd67b

daemon: initialize workloads map based on option.Config.ContainerRuntimeEndpoint
This avoids nil pointer exception when setting up Cilium with
containerD.

Fixes: d49267940840 ("workloads: Setup options in single function")
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon.go
@@ -1147,7 +1147,7 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 		}
 
 		opts := make(map[workloads.WorkloadRuntimeType]map[string]string)
-		for _, rt := range option.Config.Workloads {
+		for rt := range option.Config.ContainerRuntimeEndpoint {
 			opts[workloads.WorkloadRuntimeType(rt)] = make(map[string]string)
 		}
 		for rt, ep := range option.Config.ContainerRuntimeEndpoint {