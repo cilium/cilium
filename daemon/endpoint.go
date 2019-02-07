cilium/cilium

commit b277ab4d565d4750a9ee1d67e93917ac0806a0cd

endpoint: Refactor ipvlan related method names
The previously used names were too generic, so the following renames:

- MustGraft() -> MustGraftDatapathMap().
- MapPin() -> PinDatapathMap().

Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/endpoint.go
@@ -257,7 +257,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 
 	// Now that we have ep.ID we can pin the map from this point. This
 	// also has to happen before the first build took place.
-	if err = ep.MapPinLocked(); err != nil {
+	if err = ep.PinDatapathMap(); err != nil {
 		d.deleteEndpoint(ep)
 		log.WithError(err).Warn("Aborting endpoint tail call map pin")
 		return nil, err

cilium-health/launch/endpoint.go
@@ -303,7 +303,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, n *node.Node, mtuConfig mtu.Configur
 		ep.Unlock()
 		return nil, fmt.Errorf("unable to transition health endpoint to WaitingToRegenerate state")
 	}
-	ep.MapPinLocked()
+	ep.PinDatapathMap()
 	ep.Unlock()
 
 	buildSuccessful := <-ep.Regenerate(owner, &endpoint.ExternalRegenerationMetadata{

pkg/endpoint/endpoint.go
@@ -337,8 +337,9 @@ func (e *Endpoint) HasIpvlanDataPath() bool {
 	return false
 }
 
-// MustGraft returns whether we need full replacement or grafting of object file.
-func (e *Endpoint) MustGraft() bool {
+// MustGraftDatapathMap returns whether we need full replacement or grafting of
+// object file.
+func (e *Endpoint) MustGraftDatapathMap() bool {
 	return e.HasIpvlanDataPath()
 }
 
@@ -1599,7 +1600,7 @@ func (e *Endpoint) GetDockerNetworkID() string {
 // SetDatapathMapIDAndPinMapLocked modifies the endpoint's datapath map ID
 func (e *Endpoint) SetDatapathMapIDAndPinMapLocked(id int) error {
 	e.DataPathMapID = id
-	return e.MapPinLocked()
+	return e.PinDatapathMap()
 }
 
 // IsDatapathMapPinnedLocked returns whether the endpoint's datapath map has been pinned
@@ -2254,9 +2255,9 @@ func (e *Endpoint) IsDisconnecting() bool {
 	return e.state == StateDisconnected || e.state == StateDisconnecting
 }
 
-// MapPinLocked retrieves a file descriptor from the map ID from the API call
+// PinDatapathMap retrieves a file descriptor from the map ID from the API call
 // and pins the corresponding map into the BPF file system.
-func (e *Endpoint) MapPinLocked() error {
+func (e *Endpoint) PinDatapathMap() error {
 	if e.DataPathMapID == 0 {
 		return nil
 	}

pkg/datapath/loader/loader.go
@@ -41,7 +41,7 @@ type endpoint interface {
 	Logger(subsystem string) *logrus.Entry
 	StateDir() string
 	MapPath() string
-	MustGraft() bool
+	MustGraftDatapathMap() bool
 }
 
 // compileDatapath invokes the compiler and linker to create all state files for
@@ -85,7 +85,7 @@ func compileDatapath(ctx context.Context, ep endpoint, dirs *directoryInfo, debu
 func reloadDatapath(ctx context.Context, ep endpoint, dirs *directoryInfo) error {
 	// Replace the current program
 	objPath := path.Join(dirs.Output, endpointObj)
-	if ep.MustGraft() {
+	if ep.MustGraftDatapathMap() {
 		if err := graftDatapath(ctx, ep.MapPath(), objPath, symbolFromEndpoint); err != nil {
 			scopedLog := ep.Logger(Subsystem).WithFields(logrus.Fields{
 				logfields.Path: objPath,

pkg/datapath/loader/loader_test.go
@@ -120,7 +120,7 @@ func (ep *testEP) MapPath() string {
 	return "map_path"
 }
 
-func (ep *testEP) MustGraft() bool {
+func (ep *testEP) MustGraftDatapathMap() bool {
 	return false
 }
 

pkg/endpoint/cache.go
@@ -58,7 +58,7 @@ func (e *Endpoint) createEpInfoCache(epdir string) *epInfoCache {
 		id:          e.StringID(),
 		ifName:      e.IfName,
 		keys:        e.GetBPFKeys(),
-		graftToLoad: e.MustGraft(),
+		graftToLoad: e.MustGraftDatapathMap(),
 	}
 
 	var err error
@@ -91,8 +91,8 @@ func (ep *epInfoCache) Logger(subsystem string) *logrus.Entry {
 	return ep.endpoint.Logger(subsystem)
 }
 
-// MustGraft returns whether we need full replacement or grafting of object file.
-func (ep *epInfoCache) MustGraft() bool {
+// MustGraftDatapathMap returns whether we need full replacement or grafting of object file.
+func (ep *epInfoCache) MustGraftDatapathMap() bool {
 	return ep.graftToLoad
 }
 

commit 8f3c608879985b82da86fcb3953db3899ea33054

daemon: release endpoint lock if PinDatapathMap fails
\`daemon.deleteEndpoint\` assumes the endpoint lock is not held when
it is called.

Fixes: b277ab4d56 ("endpoint: Refactor ipvlan related method names")

Signed-off by: Ian Vernon <ian@cilium.io>

daemon/endpoint.go
@@ -258,6 +258,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 	// Now that we have ep.ID we can pin the map from this point. This
 	// also has to happen before the first build took place.
 	if err = ep.PinDatapathMap(); err != nil {
+		ep.Unlock()
 		d.deleteEndpoint(ep)
 		log.WithError(err).Warn("Aborting endpoint tail call map pin")
 		return nil, err