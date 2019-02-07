901b0203adb6f66e1e101ea3a01ff391ab03ffbf
Eloy Coto <eloy.coto@gmail.com>
2019-02-07 16:37:23 +0100

    FQDN: Endpoint sync header file on new DNS cache update.
    In this commit if a DNS response provide a new IP it'll trigger the
    `SyncEndpointHeaderFile` function to make sure that on restore the IP is
    not missed and connectivity keeps working as normal.
    
    Fix #6957
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- daemon/fqdn.go
+++ daemon/fqdn.go
@@ -328,7 +328,11 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState) (err err
 				if effectiveTTL < option.Config.ToFQDNsMinTTL {
 					effectiveTTL = option.Config.ToFQDNsMinTTL
 				}
-				ep.DNSHistory.Update(lookupTime, qname, responseIPs, effectiveTTL)
+
+				if ep.DNSHistory.Update(lookupTime, qname, responseIPs, effectiveTTL) {
+					ep.SyncEndpointHeaderFile()
+				}
+
 				log.Debug("Updating DNS name in cache from response to to query")
 				err = d.dnsRuleGen.UpdateGenerateDNS(lookupTime, map[string]*fqdn.DNSIPRecords{
 					qname: {
