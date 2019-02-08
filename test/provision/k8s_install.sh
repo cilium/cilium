382590446705bed400be3696280eca7d1483bf4b
Eloy Coto <eloy.coto@gmail.com>
2019-02-08 08:55:05 +0100

    Test: Add Kuberentes 1.14-rc.1 to the build system.
    Added kubernetes 1.14 to the build
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -190,6 +190,14 @@ case $K8S_VERSION in
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
         KUBEADM_CONFIG="${KUBEADM_CONFIG_ALPHA3}"
         ;;
+    "1.14")
+        KUBERNETES_CNI_VERSION="0.6.0"
+        K8S_FULL_VERSION="1.14.0-rc.1"
+        KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
+        KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification"
+        sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
+        KUBEADM_CONFIG="${KUBEADM_CONFIG_ALPHA3}"
+        ;;
 esac
 
 #Install kubernetes
@@ -201,9 +209,9 @@ case $K8S_VERSION in
             kubeadm=${K8S_FULL_VERSION}* \
             kubectl=${K8S_FULL_VERSION}*
         ;;
-#    "1.13")
-#        install_k8s_using_binary "v${K8S_FULL_VERSION}" "v${KUBERNETES_CNI_VERSION}"
-#        ;;
+   "1.14")
+       install_k8s_using_binary "v${K8S_FULL_VERSION}" "v${KUBERNETES_CNI_VERSION}"
+       ;;
 esac
 
 case $CONTAINER_RUNTIME in
