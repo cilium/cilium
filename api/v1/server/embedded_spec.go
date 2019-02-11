cilium/cilium

commit 48abb62084acc9a1f27f6c8927429ed8903b30cd

api: add pod / container name, labels for GET ep
* add new fields to Endpoint (pod-name, container-name)
* GET /endpoint now takes an optional array of labels to return a list of endpoints whose labels match the provided array.

Signed-off by: Ian Vernon <ian@covalent.io>

api/v1/openapi.yaml
@@ -169,11 +169,13 @@ paths:
           description: Endpoint not found
   "/endpoint":
     get:
-      summary: Get list of all endpoints
+      summary: Retrieves a list of endpoints that have metadata matching the provided parameters.
       description: |
-        Returns an array of all local endpoints.
+        Retrieves a list of endpoints that have metadata matching the provided parameters, or all endpoints if no parameters provided.
       tags:
       - endpoint
+      parameters:
+      - "$ref": "#/parameters/labels"
       responses:
         '200':
           description: Success
@@ -181,6 +183,8 @@ paths:
             type: array
             items:
               "$ref": "#/definitions/Endpoint"
+        '404':
+          description: Endpoints with provided parameters not found
   "/endpoint/{id}/config":
     get:
       summary: Retrieve endpoint configuration
@@ -443,7 +447,7 @@ paths:
       parameters:
       - name: labels
         in: body
-        required: true
+        required: false
         schema:
           "$ref": "#/definitions/Labels"
       responses:
@@ -557,6 +561,8 @@ parameters:
         - cilium-local: Local Cilium endpoint UUID, e.g. cilium-local:3389595
         - cilium-global: Global Cilium endpoint UUID, e.g. cilium-global:cluster1:nodeX:452343
         - container-id: Container runtime ID, e.g. container-id:22222
+        - container-name: Container name, e.g. container-name:foobar
+        - pod-name: pod name for this container if K8s is enabled, e.g. pod-name:default:foobar
         - docker-net-endpoint: Docker libnetwork endpoint ID, e.g. docker-net-endpoint:4444
     in: path
     required: true
@@ -574,6 +580,14 @@ parameters:
     in: path
     required: true
     type: string
+  labels:
+    name: labels
+    description: |
+      List of labels
+    in: body
+    required: true
+    schema:
+      "$ref": "#/definitions/Labels"
   policy-rules:
     name: policy
     description: Policy rules
@@ -581,6 +595,13 @@ parameters:
     in: body
     schema:
       type: string
+  pod-name:
+    name: pod
+    description: |
+      K8s pod name
+    required: true
+    in: path
+    type: string
   identity-context:
     name: identity-context
     description: Context to provide policy evaluation on
@@ -632,6 +653,9 @@ definitions:
       container-id:
         description: ID assigned by container runtime
         type: string
+      container-name:
+        description: Name assigned to container
+        type: string
       docker-endpoint-id:
         description: Docker endpoint ID
         type: string
@@ -666,6 +690,9 @@ definitions:
       identity:
         description: Security identity
         "$ref": "#/definitions/Identity"
+      pod-name:
+        description: K8s pod for this endpoint
+        type: string
       policy:
         description: Policy information of endpoint
         "$ref": "#/definitions/EndpointPolicy"
@@ -684,6 +711,9 @@ definitions:
         type: integer
       container-id:
         description: ID assigned by container runtime
+        type: string
+      container-name:
+        description: Name assigned to container
         type: string
       docker-endpoint-id:
         description: Docker endpoint ID
@@ -815,8 +845,11 @@ definitions:
     description: Label configuration of an endpoint
     type: object
     properties:
-      orchestration-system:
-        description: "Labels derived from orchestration system"
+      orchestration-identity:
+        description: "Labels derived from orchestration system that are used in computing a security identity"
+        "$ref": "#/definitions/Labels"
+      orchestration-info:
+        description: "Labels derived from orchestration system that are not used in computing a security identity"
         "$ref": "#/definitions/Labels"
       custom:
         description: "Custom labels in addition to orchestration system labels."

api/v1/server/restapi/endpoint/get_endpoint_parameters.go
@@ -4,10 +4,14 @@ package endpoint
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/cilium/cilium/api/v1/models"
 )
 
 // NewGetEndpointParams creates a new GetEndpointParams object
@@ -25,6 +29,13 @@ type GetEndpointParams struct {
 
 	// HTTP Request Object
 	HTTPRequest *http.Request
+
+	/*List of labels
+
+	  Required: true
+	  In: body
+	*/
+	Labels models.Labels
 }
 
 // BindRequest both binds and validates a request, it assumes that complex things implement a Validatable(strfmt.Registry) error interface
@@ -32,6 +43,27 @@ type GetEndpointParams struct {
 func (o *GetEndpointParams) BindRequest(r *http.Request, route *middleware.MatchedRoute) error {
 	var res []error
 	o.HTTPRequest = r
+
+	if runtime.HasBody(r) {
+		defer r.Body.Close()
+		var body models.Labels
+		if err := route.Consumer.Consume(r.Body, &body); err != nil {
+			if err == io.EOF {
+				res = append(res, errors.Required("labels", "body"))
+			} else {
+				res = append(res, errors.NewParseError("labels", "body", "", err))
+			}
+
+		} else {
+
+			if len(res) == 0 {
+				o.Labels = body
+			}
+		}
+
+	} else {
+		res = append(res, errors.Required("labels", "body"))
+	}
 
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)

api/v1/server/embedded_spec.go
@@ -80,11 +80,16 @@ func init() {
     },
     "/endpoint": {
       "get": {
-        "description": "Returns an array of all local endpoints.\n",
+        "description": "Retrieves a list of endpoints that have metadata matching the provided parameters, or all endpoints if no parameters provided.\n",
         "tags": [
           "endpoint"
         ],
-        "summary": "Get list of all endpoints",
+        "summary": "Retrieves a list of endpoints that have metadata matching the provided parameters.",
+        "parameters": [
+          {
+            "$ref": "#/parameters/labels"
+          }
+        ],
         "responses": {
           "200": {
             "description": "Success",
@@ -94,6 +99,9 @@ func init() {
                 "$ref": "#/definitions/Endpoint"
               }
             }
+          },
+          "404": {
+            "description": "Endpoints with provided parameters not found"
           }
         }
       }
@@ -646,7 +654,6 @@ func init() {
           {
             "name": "labels",
             "in": "body",
-            "required": true,
             "schema": {
               "$ref": "#/definitions/Labels"
             }
@@ -888,6 +895,10 @@ func init() {
           "description": "ID assigned by container runtime",
           "type": "string"
         },
+        "container-name": {
+          "description": "Name assigned to container",
+          "type": "string"
+        },
         "docker-endpoint-id": {
           "description": "Docker endpoint ID",
           "type": "string"
@@ -916,8 +927,16 @@ func init() {
           "description": "Name of network device",
           "type": "string"
         },
+        "labels": {
+          "description": "Labels describing the identity",
+          "$ref": "#/definitions/Labels"
+        },
         "mac": {
           "description": "MAC address",
+          "type": "string"
+        },
+        "pod-name": {
+          "description": "K8s pod for this endpoint",
           "type": "string"
         },
         "policy": {
@@ -967,6 +986,10 @@ func init() {
         },
         "container-id": {
           "description": "ID assigned by container runtime",
+          "type": "string"
+        },
+        "container-name": {
+          "description": "Name assigned to container",
           "type": "string"
         },
         "docker-endpoint-id": {
@@ -1198,8 +1221,12 @@ func init() {
           "description": "Labels derived from orchestration system which have been disabled.",
           "$ref": "#/definitions/Labels"
         },
-        "orchestration-system": {
-          "description": "Labels derived from orchestration system",
+        "orchestration-identity": {
+          "description": "Labels derived from orchestration system that are used in computing a security identity",
+          "$ref": "#/definitions/Labels"
+        },
+        "orchestration-info": {
+          "description": "Labels derived from orchestration system that are not used in computing a security identity",
           "$ref": "#/definitions/Labels"
         }
       }
@@ -1397,7 +1424,7 @@ func init() {
     },
     "endpoint-id": {
       "type": "string",
-      "description": "String describing an endpoint with the format ` + "`" + `[prefix:]id` + "`" + `. If no prefix\nis specified, a prefix of ` + "`" + `cilium-local:` + "`" + ` is assumed. Not all endpoints\nwill be addressable by all endpoint ID prefixes with the exception of the\nlocal Cilium UUID which is assigned to all endpoints.\n\nSupported endpoint id prefixes:\n  - cilium-local: Local Cilium endpoint UUID, e.g. cilium-local:3389595\n  - cilium-global: Global Cilium endpoint UUID, e.g. cilium-global:cluster1:nodeX:452343\n  - container-id: Container runtime ID, e.g. container-id:22222\n  - docker-net-endpoint: Docker libnetwork endpoint ID, e.g. docker-net-endpoint:4444\n",
+      "description": "String describing an endpoint with the format ` + "`" + `[prefix:]id` + "`" + `. If no prefix\nis specified, a prefix of ` + "`" + `cilium-local:` + "`" + ` is assumed. Not all endpoints\nwill be addressable by all endpoint ID prefixes with the exception of the\nlocal Cilium UUID which is assigned to all endpoints.\n\nSupported endpoint id prefixes:\n  - cilium-local: Local Cilium endpoint UUID, e.g. cilium-local:3389595\n  - cilium-global: Global Cilium endpoint UUID, e.g. cilium-global:cluster1:nodeX:452343\n  - container-id: Container runtime ID, e.g. container-id:22222\n  - container-name: Container name, e.g. container-name:foobar\n  - pod-name: pod name for this container if K8s is enabled, e.g. pod-name:default:foobar\n  - docker-net-endpoint: Docker libnetwork endpoint ID, e.g. docker-net-endpoint:4444\n",
       "name": "id",
       "in": "path",
       "required": true
@@ -1430,6 +1457,22 @@ func init() {
       "type": "string",
       "description": "IP address",
       "name": "ip",
+      "in": "path",
+      "required": true
+    },
+    "labels": {
+      "description": "List of labels\n",
+      "name": "labels",
+      "in": "body",
+      "required": true,
+      "schema": {
+        "$ref": "#/definitions/Labels"
+      }
+    },
+    "pod-name": {
+      "type": "string",
+      "description": "K8s pod name\n",
+      "name": "pod",
       "in": "path",
       "required": true
     },

api/v1/client/endpoint/get_endpoint_parameters.go
@@ -14,12 +14,14 @@ import (
 	cr "github.com/go-openapi/runtime/client"
 
 	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/cilium/cilium/api/v1/models"
 )
 
 // NewGetEndpointParams creates a new GetEndpointParams object
 // with the default values initialized.
 func NewGetEndpointParams() *GetEndpointParams {
-
+	var ()
 	return &GetEndpointParams{
 
 		timeout: cr.DefaultTimeout,
@@ -29,7 +31,7 @@ func NewGetEndpointParams() *GetEndpointParams {
 // NewGetEndpointParamsWithTimeout creates a new GetEndpointParams object
 // with the default values initialized, and the ability to set a timeout on a request
 func NewGetEndpointParamsWithTimeout(timeout time.Duration) *GetEndpointParams {
-
+	var ()
 	return &GetEndpointParams{
 
 		timeout: timeout,
@@ -39,7 +41,7 @@ func NewGetEndpointParamsWithTimeout(timeout time.Duration) *GetEndpointParams {
 // NewGetEndpointParamsWithContext creates a new GetEndpointParams object
 // with the default values initialized, and the ability to set a context for a request
 func NewGetEndpointParamsWithContext(ctx context.Context) *GetEndpointParams {
-
+	var ()
 	return &GetEndpointParams{
 
 		Context: ctx,
@@ -49,7 +51,7 @@ func NewGetEndpointParamsWithContext(ctx context.Context) *GetEndpointParams {
 // NewGetEndpointParamsWithHTTPClient creates a new GetEndpointParams object
 // with the default values initialized, and the ability to set a custom HTTPClient for a request
 func NewGetEndpointParamsWithHTTPClient(client *http.Client) *GetEndpointParams {
-
+	var ()
 	return &GetEndpointParams{
 		HTTPClient: client,
 	}
@@ -59,6 +61,14 @@ func NewGetEndpointParamsWithHTTPClient(client *http.Client) *GetEndpointParams 
 for the get endpoint operation typically these are written to a http.Request
 */
 type GetEndpointParams struct {
+
+	/*Labels
+	  List of labels
+
+
+	*/
+	Labels models.Labels
+
 	timeout    time.Duration
 	Context    context.Context
 	HTTPClient *http.Client
@@ -97,6 +107,17 @@ func (o *GetEndpointParams) SetHTTPClient(client *http.Client) {
 	o.HTTPClient = client
 }
 
+// WithLabels adds the labels to the get endpoint params
+func (o *GetEndpointParams) WithLabels(labels models.Labels) *GetEndpointParams {
+	o.SetLabels(labels)
+	return o
+}
+
+// SetLabels adds the labels to the get endpoint params
+func (o *GetEndpointParams) SetLabels(labels models.Labels) {
+	o.Labels = labels
+}
+
 // WriteToRequest writes these params to a swagger request
 func (o *GetEndpointParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Registry) error {
 
@@ -104,6 +125,10 @@ func (o *GetEndpointParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.R
 		return err
 	}
 	var res []error
+
+	if err := r.SetBodyParam(o.Labels); err != nil {
+		return err
+	}
 
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)

api/v1/client/endpoint/delete_endpoint_id_parameters.go
@@ -70,6 +70,8 @@ type DeleteEndpointIDParams struct {
 	  - cilium-local: Local Cilium endpoint UUID, e.g. cilium-local:3389595
 	  - cilium-global: Global Cilium endpoint UUID, e.g. cilium-global:cluster1:nodeX:452343
 	  - container-id: Container runtime ID, e.g. container-id:22222
+	  - container-name: Container name, e.g. container-name:foobar
+	  - pod-name: pod name for this container if K8s is enabled, e.g. pod-name:default:foobar
 	  - docker-net-endpoint: Docker libnetwork endpoint ID, e.g. docker-net-endpoint:4444
 
 

api/v1/client/endpoint/endpoint_client.go
@@ -68,9 +68,9 @@ func (a *Client) DeleteEndpointID(params *DeleteEndpointIDParams) (*DeleteEndpoi
 }
 
 /*
-GetEndpoint gets list of all endpoints
+GetEndpoint retrieves a list of endpoints that have metadata matching the provided parameters
 
-Returns an array of all local endpoints.
+Retrieves a list of endpoints that have metadata matching the provided parameters, or all endpoints if no parameters provided.
 
 */
 func (a *Client) GetEndpoint(params *GetEndpointParams) (*GetEndpointOK, error) {

api/v1/client/endpoint/get_endpoint_id_config_parameters.go
@@ -70,6 +70,8 @@ type GetEndpointIDConfigParams struct {
 	  - cilium-local: Local Cilium endpoint UUID, e.g. cilium-local:3389595
 	  - cilium-global: Global Cilium endpoint UUID, e.g. cilium-global:cluster1:nodeX:452343
 	  - container-id: Container runtime ID, e.g. container-id:22222
+	  - container-name: Container name, e.g. container-name:foobar
+	  - pod-name: pod name for this container if K8s is enabled, e.g. pod-name:default:foobar
 	  - docker-net-endpoint: Docker libnetwork endpoint ID, e.g. docker-net-endpoint:4444
 
 

api/v1/client/endpoint/get_endpoint_id_labels_parameters.go
@@ -70,6 +70,8 @@ type GetEndpointIDLabelsParams struct {
 	  - cilium-local: Local Cilium endpoint UUID, e.g. cilium-local:3389595
 	  - cilium-global: Global Cilium endpoint UUID, e.g. cilium-global:cluster1:nodeX:452343
 	  - container-id: Container runtime ID, e.g. container-id:22222
+	  - container-name: Container name, e.g. container-name:foobar
+	  - pod-name: pod name for this container if K8s is enabled, e.g. pod-name:default:foobar
 	  - docker-net-endpoint: Docker libnetwork endpoint ID, e.g. docker-net-endpoint:4444
 
 

commit 269acc9213bfe7bf35d358bf160ef931b485d27f

api/v1: remove requirements of labels in endpoints API
Labels are not really required in this API endpoint and so they should
be marked as optional in `GET: v1/endpoint`

Fixes: 48abb62084ac ("api: add pod / container name, labels for GET ep")
Signed-off-by: André Martins <andre@cilium.io>

api/v1/openapi.yaml
@@ -176,7 +176,13 @@ paths:
       tags:
       - endpoint
       parameters:
-      - "$ref": "#/parameters/labels"
+      - name: labels
+        description: |
+          List of labels
+        in: body
+        required: false
+        schema:
+          "$ref": "#/definitions/Labels"
       responses:
         '200':
           description: Success

api/v1/server/restapi/endpoint/get_endpoint_parameters.go
@@ -6,7 +6,6 @@ package endpoint
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
-	"io"
 	"net/http"
 
 	"github.com/go-openapi/errors"
@@ -34,7 +33,6 @@ type GetEndpointParams struct {
 
 	/*List of labels
 
-	  Required: true
 	  In: body
 	*/
 	Labels models.Labels
@@ -50,12 +48,7 @@ func (o *GetEndpointParams) BindRequest(r *http.Request, route *middleware.Match
 		defer r.Body.Close()
 		var body models.Labels
 		if err := route.Consumer.Consume(r.Body, &body); err != nil {
-			if err == io.EOF {
-				res = append(res, errors.Required("labels", "body"))
-			} else {
-				res = append(res, errors.NewParseError("labels", "body", "", err))
-			}
-
+			res = append(res, errors.NewParseError("labels", "body", "", err))
 		} else {
 
 			if len(res) == 0 {
@@ -63,8 +56,6 @@ func (o *GetEndpointParams) BindRequest(r *http.Request, route *middleware.Match
 			}
 		}
 
-	} else {
-		res = append(res, errors.Required("labels", "body"))
 	}
 
 	if len(res) > 0 {

api/v1/server/embedded_spec.go
@@ -112,7 +112,12 @@ func init() {
         "summary": "Retrieves a list of endpoints that have metadata matching the provided parameters.",
         "parameters": [
           {
-            "$ref": "#/parameters/labels"
+            "description": "List of labels\n",
+            "name": "labels",
+            "in": "body",
+            "schema": {
+              "$ref": "#/definitions/Labels"
+            }
           }
         ],
         "responses": {