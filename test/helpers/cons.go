b0b06d79e7b7fabd203ee1b686641ec26eec580f
Ian Vernon <ian@cilium.io>
2019-02-11 13:22:49 -0800

    test: fail tests if container creation pulls image that is not cached in VM
    Keep the set of all container images which are invoked via `docker {run,create}`
    in the Cilium CI, and check that any image which is utilized in any test which
    calls `ContainerCreate` uses an image that is in this set. If an image is not in
    this set, the test will fail.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- test/helpers/cons.go
+++ test/helpers/cons.go
@@ -299,6 +299,23 @@ var ciliumKubCLICommands = map[string]string{
 	"cilium kvstore get cilium --recursive": "kvstore_get.txt",
 }
 
+// AllImages is the set of all container images which are ran directly via
+// `docker run` in the Cilium CI. It is used to provide a central location in
+// the code of all images that are used.
+var AllImages = map[string]struct{}{
+	NetperfImage:         {},
+	HttpdImage:           {},
+	DNSSECContainerImage: {},
+	BindContainerImage:   {},
+	KafkaClientImage:     {},
+	ZookeeperImage:       {},
+	BusyboxImage:         {},
+	AlpineCurlImage:      {},
+	MemcacheDImage:       {},
+	MemcacheBinClient:    {},
+	AlpineImage:          {},
+}
+
 const (
 	ciliumEtcdOperatorSA   = "cilium-etcd-operator-sa.yaml"
 	ciliumEtcdOperatorRBAC = "cilium-etcd-operator-rbac.yaml"
