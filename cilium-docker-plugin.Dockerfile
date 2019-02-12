cilium/cilium

commit 18eb98aaac164ec15124d84e738e8a37a93caf83

add cilium-docker-plugin Dockerfile
This allows to provide the cilium docker plugin in its own docker image

Signed-off-by: André Martins <andre@cilium.io>

cilium-docker-plugin.Dockerfile
@@ -1,0 +1,14 @@
+FROM docker.io/library/golang:1.11.1 as builder
+LABEL maintainer="maintainer@cilium.io"
+ADD . /go/src/github.com/cilium/cilium
+WORKDIR /go/src/github.com/cilium/cilium/plugins/cilium-docker
+ARG LOCKDEBUG
+ARG V
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o cilium-docker
+RUN strip cilium-docker
+
+FROM scratch
+LABEL maintainer="maintainer@cilium.io"
+COPY --from=builder /go/src/github.com/cilium/cilium/plugins/cilium-docker /usr/bin/cilium-docker
+WORKDIR /
+CMD ["/usr/bin/cilium-etcd-operator"]

plugins/cilium-docker/Makefile
@@ -8,7 +8,7 @@ SOURCES := $(shell find ../../api/v1 ../../common ../../pkg/client ../../pkg/end
 
 $(TARGET): $(SOURCES)
 	@$(ECHO_GO)
-	$(QUIET)$(GO) build $(GOBUILD) -o $(TARGET)
+	$(QUIET) CGO_ENABLED=0 $(GO) build $(GOBUILD) -o $(TARGET)
 
 run:
 	./cilium-docker -d

commit be4ab9b91236284c7e43977ee80942d5a0103044

cilium-docker-plugin: set default CMD to /usr/bin/cilium-docker
Fixes: 18eb98aaac16 ("add cilium-docker-plugin Dockerfile")
Reported-by: Chanwit Kaewkasi <chanwit@gmail.com>
Signed-off-by: André Martins <andre@cilium.io>

cilium-docker-plugin.Dockerfile
@@ -9,6 +9,6 @@ RUN strip cilium-docker
 
 FROM scratch
 LABEL maintainer="maintainer@cilium.io"
-COPY --from=builder /go/src/github.com/cilium/cilium/plugins/cilium-docker /usr/bin/cilium-docker
+COPY --from=builder /go/src/github.com/cilium/cilium/plugins/cilium-docker/cilium-docker /usr/bin/cilium-docker
 WORKDIR /
-CMD ["/usr/bin/cilium-etcd-operator"]
+CMD ["/usr/bin/cilium-docker"]