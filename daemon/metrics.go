f93bf8147c024556f285a5b682391a54abd073dd
Ray Bejjani <ray@covalent.io>
2019-02-19 11:24:59 +0100

    daemon: Track policy implementation delay by source
    We see high variance in the time from a .PolicyAdd in the daemon to when
    a revision is finally in the datapath. Quantifying this delay is
    valuable when diagnosing certain classes of problems, particularly
    transient drops to IPs seen by the DNS Proxy and inserted into node's
    policy.
    The options to .PolicyAdd now includes a source field that reflects the
    source of that specific invocation. It is expected to be one of api,
    fqdn or k8s. This information is passed onto a new goroutine that waits
    on each revision to be deployed and records the time since the
    .PolicyAdd invocation into a new policy_implementation_delay histogram.
    
    Signed-off-by: Ray Bejjani <ray@covalent.io>

--- daemon/metrics.go
+++ daemon/metrics.go
@@ -15,11 +15,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	restapi "github.com/cilium/cilium/api/v1/server/restapi/metrics"
 	"github.com/cilium/cilium/pkg/api"
+	"github.com/cilium/cilium/pkg/endpointmanager"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/spanstat"
@@ -133,4 +136,118 @@ func init() {
 	if err := metrics.Register(metricBootstrapTimes); err != nil {
 		log.WithError(err).Fatal("unable to register prometheus metric")
 	}
+}
+
+// policyRevChange is an internal type used to coordinate between PolicyAdd and
+// the watcher goroutine spawned in startImplementationDelayWatcher. It holds
+// the timestamp when that revision was added in PolicyAdd and the source for
+// that particular call.
+type policyRevChange struct {
+	rev       uint64
+	timestamp time.Time
+	source    string
+}
+
+// recordPolicyDelayMetric enqueues this revision for tracking later. The
+// information is later used by the goroutine in
+// startImplementationDelayWatcher when recording the time taken since the
+// policy revision was reached and when it was deployed to the datapath.
+func (d *Daemon) recordPolicyDelayMetric(policyRev uint64, start time.Time, source string) {
+	select {
+	case d.pendingMetricRevisions <- policyRevChange{rev: policyRev, timestamp: start, source: source}:
+		// Success. Do nothing.
+	default:
+		// The channel is full. Write a warning.
+		log.WithField(logfields.PolicyRevision, policyRev).Warn("policy_implementation_delay metric collection queue is full. No metric will be recorded for this revision.")
+	}
+}
+
+// startImplementationDelayWatcher runs a goroutine to record the time taken
+// for a policy to be implemented in the datapath. It makes some critical
+// assumptions:
+//   - A revision is only seen once, from a single source. Note that actual
+//     regenerations may be rolled up into one by a trigger.Trigger.
+//   - The revision only ever increments and we can never see an older revision
+//     once we have successfully waited on it.
+// The buffered pendingMetricRevisions channel is used as the input to this
+// goroutine and holds an entry for every PolicyAdd call that resulted in a new
+// revision. It is expected to remain fairly empty and only fill up when
+// multiple revision changes queue up during a slower regeneration. Closing the
+// channel causes the goroutine to exit.
+func startImplementationDelayWatcher() (pendingMetricRevisions chan policyRevChange) {
+	// pendingMetricRevisions is a buffered channel of revisions for which we
+	// want the implementation delay.
+	// It is returned after the goroutine is started below and used by
+	// recordPolicyDelayMetric.
+	pendingMetricRevisions = make(chan policyRevChange, 16384)
+
+	// Start a goroutine to wait on revision changes sequentially, then report
+	// the PolicyImplementationDelay metric for each entry seen in
+	// pendingMetricRevisions
+	go func() {
+		log.Debug("startImplementationDelayWatcher goroutine started")
+		defer log.Debug("startImplementationDelayWatcher goroutine exited")
+
+		var (
+			// starts is a map of revision -> metadata for revisions in
+			// pendingMetricRevisions that were in the future when returning from
+			// WaitForEndpointsAtPolicyRevision. Once we get up to that revision the
+			// entry is handled and deleted
+			starts = make(map[uint64]policyRevChange)
+
+			// currentRevision is what we are currently operating on. It is only
+			// incremented once we succcessfully wait on that revision number and
+			// record metrics for it.
+			currentRevision = uint64(0)
+		)
+
+		for {
+			// backoff if there are no endpoints. This is required to avoid
+			// spin-looping and incrementing the revision
+			// (WaitForEndpointsAtPolicyRevision returns when there are no
+			// endpoints).
+			if len(endpointmanager.GetEndpoints()) == 0 {
+				time.Sleep(10 * time.Second)
+				continue
+			}
+
+			// Wait for currentRevision and, once it returns, for each entry in pendingMetricRevisions:
+			//  - If pendingMetricRevisions is closed, exit the goroutine
+			//  - If the desired revision is in the past, record the metric immediately
+			//  - If the revision is in the future, store it for when that revision is seen
+			// Break out when pendingMetricRevisions is empty
+			endpointmanager.WaitForEndpointsAtPolicyRev(context.Background(), currentRevision)
+			log.WithField(logfields.PolicyRevision, currentRevision).Debug("startImplementationDelayWatcher finished waiting for a revision")
+			now := time.Now()
+		pendingRevisions:
+			for {
+				select {
+				case start, stillOpen := <-pendingMetricRevisions:
+					switch {
+					case !stillOpen:
+						return
+					case start.rev <= currentRevision:
+						duration := now.Sub(start.timestamp)
+						metrics.PolicyImplementationDelay.WithLabelValues(start.source).Observe(duration.Seconds())
+					default:
+						starts[start.rev] = start
+					}
+				default:
+					break pendingRevisions
+				}
+			}
+
+			// Check for a revision stored previously and record the metric
+			if start, exists := starts[currentRevision]; exists {
+				duration := now.Sub(start.timestamp)
+				metrics.PolicyImplementationDelay.WithLabelValues(start.source).Observe(duration.Seconds())
+				delete(starts, currentRevision)
+			}
+
+			// Work on the next revision
+			currentRevision++
+		}
+	}()
+
+	return pendingMetricRevisions
 }
