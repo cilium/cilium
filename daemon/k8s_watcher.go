0194860f55eb73d98cf6c71a84c0c6a7a817bb30
Ray Bejjani <ray@covalent.io>
2019-02-19 11:24:59 +0100

    daemon: Track policy implementation delay by source
    We see high variance in the time from a .PolicyAdd in the daemon to when
    a revision is finally in the datapath. Quantifying this delay is
    valuable when diagnosing certain classes of problems, particularly
    transient drops to IPs seen by the DNS Proxy and inserted into node's
    policy.
    The options to .PolicyAdd now includes a source field that reflects the
    source of that specific invocation. It is expected to be one of api,
    fqdn or k8s. This information is passed onto a callback that will be
    called when each endpoint reaches the revision in the .PolicyAdd that
    created the revision. This information is recorded into a new
    policy_implementation_delay histogram metric for each endpoint.
    
    Signed-off-by: Ray Bejjani <ray@covalent.io>

--- daemon/k8s_watcher.go
+++ daemon/k8s_watcher.go
@@ -667,7 +667,7 @@ func (d *Daemon) addK8sNetworkPolicyV1(k8sNP *networkingv1.NetworkPolicy) error 
 	}
 	scopedLog = scopedLog.WithField(logfields.K8sNetworkPolicyName, k8sNP.ObjectMeta.Name)
 
-	opts := AddOptions{Replace: true}
+	opts := AddOptions{Replace: true, Source: metrics.LabelEventSourceK8s}
 	if _, err := d.PolicyAdd(rules, &opts); err != nil {
 		scopedLog.WithError(err).WithFields(logrus.Fields{
 			logfields.CiliumNetworkPolicy: logfields.Repr(rules),
@@ -1166,6 +1166,7 @@ func (d *Daemon) addCiliumNetworkPolicyV2(ciliumV2Store cache.Store, cnp *cilium
 		// resourceTypeCiliumNetworkPolicy
 		rev, policyImportErr = d.PolicyAdd(rules, &AddOptions{
 			ReplaceWithLabels: cnp.GetIdentityLabels(),
+			Source:            metrics.LabelEventSourceK8s,
 		})
 	}
 
