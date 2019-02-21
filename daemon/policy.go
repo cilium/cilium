1e249728cd1ea89e4265567ed77d7cc1b07829fb
Eloy Coto <eloy.coto@gmail.com>
2019-02-21 08:46:31 +0100

    Daemon: policy add rules to DNS when is locked.
    On PolicyAdd rules are copied to dns manage. It can be a race where the
    same rule can be pushed to the DNS without lock and overwrite old ones.
    
    With this change the StartdNS will happens under the lock, and race will
    be skipped.
    
    Fix #7105
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- daemon/policy.go
+++ daemon/policy.go
@@ -244,6 +244,14 @@ func (d *Daemon) PolicyAdd(rules policyAPI.Rules, opts *AddOptions) (uint64, err
 		}
 	}
 	rev := d.policy.AddListLocked(rules)
+
+	// The rules are added, we can begin ToFQDN DNS polling for them
+	// Note: api.FQDNSelector.sanitize checks that the matchName entries are
+	// valid. This error should never happen (of course).
+	if err := d.dnsRuleGen.StartManageDNSName(rules); err != nil {
+		logger.WithError(err).Warn("Error trying to manage rules during PolicyAdd")
+	}
+
 	d.policy.Mutex.Unlock()
 
 	// remove prefixes of replaced rules above. This potentially blocks on the
@@ -257,15 +265,7 @@ func (d *Daemon) PolicyAdd(rules policyAPI.Rules, opts *AddOptions) (uint64, err
 		d.prefixLengths.Delete(removedPrefixes)
 	}
 
-	// The rules are added, we can begin ToFQDN DNS polling for them
-	// Note: api.FQDNSelector.sanitize checks that the matchName entries are
-	// valid. This error should never happen (of course).
-	if err := d.dnsRuleGen.StartManageDNSName(rules); err != nil {
-		logger.WithError(err).Warn("Error trying to manage rules during PolicyAdd")
-	}
-
 	logger.WithField(logfields.PolicyRevision, rev).Info("Policy imported via API, recalculating...")
-
 	d.TriggerPolicyUpdates(false, "policy rules added")
 
 	labels := make([]string, 0, len(rules))
