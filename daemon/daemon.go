348c45085f1a34dcf97dbfe8da4e89412afd6b49
Thomas Graf <thomas@cilium.io>
2019-02-22 22:26:17 +0100

    k8s: Print addresses used to annotate the node
    Related: #6776
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1216,7 +1216,15 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 	// PodCIDR range and allocation of the health IPs.
 	if k8s.IsEnabled() {
 		bootstrapStats.k8sInit.Start()
-		log.Info("Annotating k8s node with CIDR ranges")
+		log.WithFields(logrus.Fields{
+			logfields.V4Prefix:       node.GetIPv4AllocRange(),
+			logfields.V6Prefix:       node.GetIPv6NodeRange(),
+			logfields.V4HealthIP:     d.nodeDiscovery.LocalNode.IPv4HealthIP,
+			logfields.V6HealthIP:     d.nodeDiscovery.LocalNode.IPv6HealthIP,
+			logfields.V4CiliumHostIP: node.GetInternalIPv4(),
+			logfields.V6CiliumHostIP: node.GetIPv6Router(),
+		}).Info("Annotating k8s node")
+
 		err := k8s.Client().AnnotateNode(node.GetName(),
 			node.GetIPv4AllocRange(), node.GetIPv6NodeRange(),
 			d.nodeDiscovery.LocalNode.IPv4HealthIP, d.nodeDiscovery.LocalNode.IPv6HealthIP,
