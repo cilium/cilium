cilium/cilium

commit 96895f9d35cf2f1d6c3f885ddeb7a883ba55f0f7

kvstore: Support creation of multiple clients
Requires moving lease management into client specific code. New function
NewClient() and .Close() to create and close clients.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/kvstore/consul.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cilium/cilium/common"
 	"github.com/cilium/cilium/common/types"
 	"github.com/cilium/cilium/pkg/backoff"
+	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 
 	consulAPI "github.com/hashicorp/consul/api"
@@ -65,6 +66,11 @@ var (
 func init() {
 	// register consul module for use
 	registerBackend(consulName, module)
+}
+
+func (c *consulModule) createInstance() backendModule {
+	cpy := *module
+	return &cpy
 }
 
 func (c *consulModule) getName() string {
@@ -117,6 +123,8 @@ var (
 
 type consulClient struct {
 	*consulAPI.Client
+	lease       string
+	controllers *controller.Manager
 }
 
 func newConsulClient(config *consulAPI.Config) (BackendOperations, error) {
@@ -156,7 +164,33 @@ func newConsulClient(config *consulAPI.Config) (BackendOperations, error) {
 		log.WithError(err).Fatal("Unable to contact consul server")
 	}
 
-	return &consulClient{c}, nil
+	entry := &consulAPI.SessionEntry{
+		TTL:      fmt.Sprintf("%ds", int(LeaseTTL.Seconds())),
+		Behavior: consulAPI.SessionBehaviorDelete,
+	}
+
+	lease, _, err := c.Session().Create(entry, nil)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create default lease: %s", err)
+	}
+
+	client := &consulClient{
+		Client:      c,
+		lease:       lease,
+		controllers: controller.NewManager(),
+	}
+
+	client.controllers.UpdateController(fmt.Sprintf("consul-lease-keepalive-%p", c),
+		controller.ControllerParams{
+			DoFunc: func() error {
+				_, _, err := c.Session().Renew(lease, nil)
+				return err
+			},
+			RunInterval: KeepAliveInterval,
+		},
+	)
+
+	return client, nil
 }
 
 func (c *consulClient) LockPath(path string) (kvLocker, error) {
@@ -515,12 +549,7 @@ func (c *consulClient) Update(key string, value []byte, lease bool) error {
 	k := &consulAPI.KVPair{Key: key, Value: value}
 
 	if lease {
-		id, ok := leaseInstance.(string)
-		if !ok {
-			return fmt.Errorf("argument not a LeaseID")
-		}
-
-		k.Session = id
+		k.Session = c.lease
 	}
 
 	_, err := c.KV().Put(k, nil)
@@ -536,12 +565,7 @@ func (c *consulClient) CreateOnly(key string, value []byte, lease bool) error {
 	}
 
 	if lease {
-		id, ok := leaseInstance.(string)
-		if !ok {
-			return fmt.Errorf("argument not a LeaseID")
-		}
-
-		k.Session = id
+		k.Session = c.lease
 	}
 
 	success, _, err := c.KV().CAS(k, nil)
@@ -600,40 +624,14 @@ func (c *consulClient) ListPrefix(prefix string) (KeyValuePairs, error) {
 	return p, nil
 }
 
-// CreateLease creates a new lease with the given ttl
-func (c *consulClient) CreateLease(ttl time.Duration) (interface{}, error) {
-	entry := &consulAPI.SessionEntry{
-		TTL:      fmt.Sprintf("%ds", int(ttl.Seconds())),
-		Behavior: consulAPI.SessionBehaviorDelete,
+// Close closes the consul session
+func (c *consulClient) Close() {
+	if c.controllers != nil {
+		c.controllers.RemoveAll()
 	}
-
-	id, _, err := c.Session().Create(entry, nil)
-	return id, err
-}
-
-// KeepAlive keeps a lease created with CreateLease alive
-func (c *consulClient) KeepAlive(lease interface{}) error {
-	id, ok := lease.(string)
-	if !ok {
-		return fmt.Errorf("argument not a LeaseID")
+	if c.lease != "" {
+		c.Session().Destroy(c.lease, nil)
 	}
-
-	_, _, err := c.Session().Renew(id, nil)
-	return err
-}
-
-// DeleteLease deletes a lease
-func (c *consulClient) DeleteLease(lease interface{}) error {
-	id, ok := lease.(string)
-	if !ok {
-		return fmt.Errorf("argument not a LeaseID")
-	}
-
-	_, err := c.Session().Destroy(id, nil)
-	return err
-}
-
-func (c *consulClient) closeClient() {
 }
 
 // GetCapabilities returns the capabilities of the backend

pkg/kvstore/etcd.go
@@ -87,6 +87,11 @@ var (
 	statusCheckerStarted sync.Once
 )
 
+func (e *etcdModule) createInstance() backendModule {
+	cpy := *etcdInstance
+	return &cpy
+}
+
 func (e *etcdModule) getName() string {
 	return etcdName
 }
@@ -139,6 +144,8 @@ type etcdClient struct {
 
 	client      *client.Client
 	session     *concurrency.Session
+	lease       *client.LeaseGrantResponse
+	controllers *controller.Manager
 	lockPathsMU lock.Mutex
 	lockPaths   map[string]*lock.Mutex
 }
@@ -167,10 +174,33 @@ func (e *etcdClient) renewSession() error {
 
 	go e.checkMinVersion()
 
-	if err := renewDefaultLease(); err != nil {
-		e.client.Close()
-		return err
+	e.renewLease()
+
+	return nil
+}
+
+func (e *etcdClient) renewLease() error {
+	if e.lease != nil {
+		e.client.Revoke(ctx.TODO(), e.lease.ID)
 	}
+
+	lease, err := e.client.Grant(ctx.TODO(), int64(LeaseTTL.Seconds()))
+	if err != nil {
+		e.client.Close()
+		return fmt.Errorf("unable to create default lease: %s", err)
+	}
+
+	e.lease = lease
+
+	e.controllers.UpdateController(fmt.Sprintf("etcd-lease-keepalive-%p", e),
+		controller.ControllerParams{
+			DoFunc: func() error {
+				_, err := e.client.KeepAliveOnce(ctx.TODO(), lease.ID)
+				return err
+			},
+			RunInterval: KeepAliveInterval,
+		},
+	)
 
 	return nil
 }
@@ -220,10 +250,17 @@ func newEtcdClient(config *client.Config, cfgPath string) (BackendOperations, er
 	if err != nil {
 		return nil, fmt.Errorf("Unable to contact etcd: %s", err)
 	}
+
 	ec := &etcdClient{
-		client:    c,
-		session:   s,
-		lockPaths: map[string]*lock.Mutex{},
+		client:      c,
+		session:     s,
+		lockPaths:   map[string]*lock.Mutex{},
+		controllers: controller.NewManager(),
+	}
+
+	if err := ec.renewLease(); err != nil {
+		c.Close()
+		return nil, fmt.Errorf("unable to create default lease: %s", err)
 	}
 
 	statusCheckerStarted.Do(func() {
@@ -232,7 +269,7 @@ func newEtcdClient(config *client.Config, cfgPath string) (BackendOperations, er
 
 	go ec.checkMinVersion()
 
-	kvstoreControllers.UpdateController("kvstore-etcd-session-renew",
+	ec.controllers.UpdateController("kvstore-etcd-session-renew",
 		controller.ControllerParams{
 			DoFunc: func() error {
 				return ec.renewSession()
@@ -728,28 +765,20 @@ func (e *etcdClient) Delete(key string) error {
 	return err
 }
 
-func createOpPut(key string, value []byte, lease bool) (*client.Op, error) {
+func (e *etcdClient) createOpPut(key string, value []byte, lease bool) *client.Op {
 	if lease {
-		r, ok := leaseInstance.(*client.LeaseGrantResponse)
-		if !ok {
-			return nil, fmt.Errorf("argument not a LeaseID")
-		}
-		op := client.OpPut(key, string(value), client.WithLease(r.ID))
-		return &op, nil
+		op := client.OpPut(key, string(value), client.WithLease(e.lease.ID))
+		return &op
 	}
 
 	op := client.OpPut(key, string(value))
-	return &op, nil
+	return &op
 }
 
 // Update creates or updates a key
 func (e *etcdClient) Update(key string, value []byte, lease bool) error {
 	if lease {
-		r, ok := leaseInstance.(*client.LeaseGrantResponse)
-		if !ok {
-			return fmt.Errorf("argument not a LeaseID")
-		}
-		_, err := e.client.Put(ctx.Background(), key, string(value), client.WithLease(r.ID))
+		_, err := e.client.Put(ctx.Background(), key, string(value), client.WithLease(e.lease.ID))
 		return err
 	}
 
@@ -759,11 +788,7 @@ func (e *etcdClient) Update(key string, value []byte, lease bool) error {
 
 // CreateOnly creates a key with the value and will fail if the key already exists
 func (e *etcdClient) CreateOnly(key string, value []byte, lease bool) error {
-	req, err := createOpPut(key, value, lease)
-	if err != nil {
-		return err
-	}
-
+	req := e.createOpPut(key, value, lease)
 	cond := client.Compare(client.Version(key), "=", 0)
 	txnresp, err := e.client.Txn(ctx.TODO()).If(cond).Then(*req).Commit()
 	if err != nil {
@@ -779,11 +804,7 @@ func (e *etcdClient) CreateOnly(key string, value []byte, lease bool) error {
 
 // CreateIfExists creates a key with the value only if key condKey exists
 func (e *etcdClient) CreateIfExists(condKey, key string, value []byte, lease bool) error {
-	req, err := createOpPut(key, value, lease)
-	if err != nil {
-		return err
-	}
-
+	req := e.createOpPut(key, value, lease)
 	cond := client.Compare(client.Version(condKey), "!=", 0)
 	txnresp, err := e.client.Txn(ctx.TODO()).If(cond).Then(*req).Commit()
 	if err != nil {
@@ -832,34 +853,14 @@ func (e *etcdClient) ListPrefix(prefix string) (KeyValuePairs, error) {
 	return pairs, nil
 }
 
-// CreateLease creates a new lease with the given ttl
-func (e *etcdClient) CreateLease(ttl time.Duration) (interface{}, error) {
-	return e.client.Grant(ctx.TODO(), int64(ttl.Seconds()))
-}
-
-// KeepAlive keeps a lease created with CreateLease alive
-func (e *etcdClient) KeepAlive(lease interface{}) error {
-	r, ok := lease.(*client.LeaseGrantResponse)
-	if !ok {
-		return fmt.Errorf("argument not a LeaseID")
+// Close closes the etcd session
+func (e *etcdClient) Close() {
+	if e.controllers != nil {
+		e.controllers.RemoveAll()
 	}
-
-	_, err := e.client.KeepAliveOnce(ctx.TODO(), r.ID)
-	return err
-}
-
-// DeleteLease deletes a lease
-func (e *etcdClient) DeleteLease(lease interface{}) error {
-	r, ok := lease.(*client.LeaseGrantResponse)
-	if !ok {
-		return fmt.Errorf("argument not a LeaseID")
+	if e.lease != nil {
+		e.client.Revoke(ctx.TODO(), e.lease.ID)
 	}
-
-	_, err := e.client.Revoke(ctx.TODO(), r.ID)
-	return err
-}
-
-func (e *etcdClient) closeClient() {
 	e.client.Close()
 }
 

pkg/kvstore/backend.go
@@ -16,7 +16,6 @@ package kvstore
 
 import (
 	"encoding/json"
-	"time"
 
 	"github.com/cilium/cilium/common/types"
 )
@@ -58,6 +57,9 @@ type backendModule interface {
 	// newClient must initializes the backend and create a new kvstore
 	// client which implements the BackendOperations interface
 	newClient() (BackendOperations, error)
+
+	// createInstance creates a new instance of the module
+	createInstance() backendModule
 }
 
 var (
@@ -78,7 +80,7 @@ func registerBackend(name string, module backendModule) {
 // getBackend finds a registered backend by name
 func getBackend(name string) backendModule {
 	if backend, ok := registeredBackends[name]; ok {
-		return backend
+		return backend.createInstance()
 	}
 
 	return nil
@@ -139,18 +141,8 @@ type BackendOperations interface {
 	// keys first.
 	Watch(w *Watcher)
 
-	// CreateLease creates a lease with the specified ttl
-	CreateLease(ttl time.Duration) (interface{}, error)
-
-	// KeepAlive keeps a lease previously created with CreateLease alive
-	// for the duration specified at CreateLease time
-	KeepAlive(lease interface{}) error
-
-	// DeleteLease deletes a lease
-	DeleteLease(interface{}) error
-
-	// close closes the kvstore client
-	closeClient()
+	// Close closes the kvstore client
+	Close()
 
 	// GetCapabilities returns the capabilities of the backend
 	GetCapabilities() Capabilities

pkg/kvstore/client.go
@@ -15,20 +15,12 @@
 package kvstore
 
 import (
-	"github.com/cilium/cilium/pkg/controller"
-	"github.com/cilium/cilium/pkg/lock"
+	"fmt"
 )
 
 var (
 	// defaultClient is the default client initialized by initClient
 	defaultClient BackendOperations
-
-	// leaseInstance is the backend specific lease object. The lease is
-	// created by initClient()
-	leaseInstance interface{}
-	leaseMutex    lock.RWMutex
-
-	kvstoreControllers = controller.NewManager()
 )
 
 func initClient(module backendModule) error {
@@ -38,12 +30,7 @@ func initClient(module backendModule) error {
 	}
 
 	defaultClient = c
-
 	deleteLegacyPrefixes()
-	if err := renewDefaultLease(); err != nil {
-		defaultClient = nil
-		return err
-	}
 
 	return nil
 }
@@ -51,4 +38,23 @@ func initClient(module backendModule) error {
 // Client returns the global kvstore client or nil if the client is not configured yet
 func Client() BackendOperations {
 	return defaultClient
+}
+
+// NewClient returns a new kvstore client based on the configuration
+func NewClient(selectedBackend string, opts map[string]string) (BackendOperations, error) {
+	module := getBackend(selectedBackend)
+	if module == nil {
+		return nil, fmt.Errorf("unknown key-value store type %q. See cilium.link/err-kvstore for details", selectedBackend)
+	}
+
+	if err := module.setConfig(opts); err != nil {
+		return nil, err
+	}
+
+	c, err := module.newClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
 }

pkg/kvstore/consul_test.go
@@ -48,6 +48,13 @@ func TestMain(m *testing.M) {
 		handler(w, r)
 	})
 
+	mux.HandleFunc("/v1/session/create", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "{ \"ID\": \"adf4238a-882b-9ddc-4a9d-5b6758e4159e\"}")
+	})
+
+	// /v1/session/renew/{uuid} does not need to be handled for the basic
+	// test to succeed
+
 	srv := &http.Server{
 		Addr:    ":8000",
 		Handler: mux,

pkg/kvstore/keepalive.go
@@ -15,12 +15,7 @@
 package kvstore
 
 import (
-	"fmt"
 	"time"
-
-	"github.com/cilium/cilium/pkg/controller"
-
-	"github.com/sirupsen/logrus"
 )
 
 var (
@@ -37,46 +32,3 @@ var (
 	// the KVstore.
 	RetryInterval = 1 * time.Minute
 )
-
-// CreateLease creates a new lease with the given ttl
-func CreateLease(ttl time.Duration) (interface{}, error) {
-	lease, err := Client().CreateLease(ttl)
-	Trace("CreateLease", err, logrus.Fields{fieldTTL: ttl, fieldLease: lease})
-	return lease, err
-}
-
-// KeepAlive keeps a lease created with CreateLease alive
-func KeepAlive(lease interface{}) error {
-	err := Client().KeepAlive(lease)
-	Trace("KeepAlive", err, logrus.Fields{fieldLease: lease})
-	return err
-}
-
-func renewDefaultLease() error {
-	l, err := CreateLease(LeaseTTL)
-	if err != nil {
-		return fmt.Errorf("Unable to create lease: %s", err)
-	}
-
-	leaseMutex.Lock()
-	if leaseInstance != nil {
-		defaultClient.DeleteLease(leaseInstance)
-	}
-	leaseInstance = l
-	leaseMutex.Unlock()
-
-	// keep default lease alive
-	kvstoreControllers.UpdateController("kvstore-lease-keepalive",
-		controller.ControllerParams{
-			DoFunc: func() error {
-				leaseMutex.RLock()
-				defer leaseMutex.RUnlock()
-
-				return KeepAlive(leaseInstance)
-			},
-			RunInterval: KeepAliveInterval,
-		},
-	)
-
-	return nil
-}

pkg/kvstore/kvstore.go
@@ -119,14 +119,5 @@ func Decode(in string) ([]byte, error) {
 
 // Close closes the kvstore client
 func Close() {
-	kvstoreControllers.RemoveAll()
-
-	leaseMutex.Lock()
-	if leaseInstance != nil {
-		defaultClient.DeleteLease(leaseInstance)
-	}
-	leaseInstance = nil
-	leaseMutex.Unlock()
-
-	defaultClient.closeClient()
+	defaultClient.Close()
 }

commit 38b35c71ed3389e5b889a435f7befcdd1604bd95

pkg/kvstore: do not use default instance to create new instance module
By returning the same instance module for every client created this
can create race conditions if 2 or more clients are being setup at the
same time. For this reason we should really create a new instance
instead of using a copy of an existing instance.

Fixes: 96895f9d35cf ("kvstore: Support creation of multiple clients")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/consul.go
@@ -54,7 +54,16 @@ var (
 	//consulDummyConfigFile can be overwritten from test invokers using ldflags
 	consulDummyConfigFile = "/tmp/cilium-consul-certs/cilium-consul.yaml"
 
-	module = &consulModule{
+	module = newConsulModule()
+)
+
+func init() {
+	// register consul module for use
+	registerBackend(consulName, module)
+}
+
+func newConsulModule() backendModule {
+	return &consulModule{
 		opts: backendOptions{
 			optAddress: &backendOption{
 				description: "Addresses of consul cluster",
@@ -64,16 +73,10 @@ var (
 			},
 		},
 	}
-)
-
-func init() {
-	// register consul module for use
-	registerBackend(consulName, module)
 }
 
 func (c *consulModule) createInstance() backendModule {
-	cpy := *module
-	return &cpy
+	return newConsulModule()
 }
 
 func (c *consulModule) getName() string {

pkg/kvstore/etcd.go
@@ -68,7 +68,15 @@ var (
 	// etcdDummyAddress can be overwritten from test invokers using ldflags
 	etcdDummyAddress = "http://127.0.0.1:4002"
 
-	etcdInstance = &etcdModule{
+	etcdInstance = newEtcdModule()
+)
+
+func EtcdDummyAddress() string {
+	return etcdDummyAddress
+}
+
+func newEtcdModule() backendModule {
+	return &etcdModule{
 		opts: backendOptions{
 			addrOption: &backendOption{
 				description: "Addresses of etcd cluster",
@@ -78,15 +86,10 @@ var (
 			},
 		},
 	}
-)
-
-func EtcdDummyAddress() string {
-	return etcdDummyAddress
 }
 
 func (e *etcdModule) createInstance() backendModule {
-	cpy := *etcdInstance
-	return &cpy
+	return newEtcdModule()
 }
 
 func (e *etcdModule) getName() string {