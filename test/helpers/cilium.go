b77716cd445f917ba68da5f2e9147790be44a8f8
Martynas Pumputis <m@lambda.lt>
2019-02-25 12:34:28 +0100

    test: Get rid of JoinEP flakes
    [ upstream commit bf21355a3dc53d950c1a94531f71ac1323025a1b ]
    
    Previously, if a log of a test run contained any "bad" message from the
    predefined list, such test was marked as failing one. This introduced
    many flakes for the `JoinEP:` "bad" message, as due to terminating
    cilium-agent any subprocess of loader was stopped with either
    SIGTERM or SIGKILL which resulted in a "bad" message with `JoinEP:` being
    logged.
    
    This commit relaxes the "bad" messages check by introducing a sublist
    containing a list of values which indicate whether the "bad" message
    has to be ignored if the "bad" log message contains any of the values.
    
    The relaxed check is more computationally intense due to the string
    splitting and the iteration of each log message. However, when measured,
    the time (sum of all test cases per CI build run) it takes to perform
    the check is not really noticeable:
    
        67.443 ms (old check) vs  112.486 ms (relaxed check)
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- test/helpers/cilium.go
+++ test/helpers/cilium.go
@@ -656,12 +656,7 @@ func (s *SSHMeta) ValidateNoErrorsOnLogs(duration time.Duration) {
 		}
 	}()
 
-	for _, message := range checkLogsMessages {
-		if strings.Contains(logs, message) {
-			fmt.Fprintf(CheckLogs, "⚠️  Found a %q in logs\n", message)
-			ginkgoext.Fail(fmt.Sprintf("Found a %q in Cilium Logs", message))
-		}
-	}
+	failIfContainsBadLogMsg(logs)
 
 	// Count part
 	for _, message := range countLogsMessages {
