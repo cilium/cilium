df4201d735e0ccfb1ef2aa71a902bfe4318affed
John Fastabend <john.fastabend@gmail.com>
2019-02-27 15:06:29 -0800

    cilium: push decryption up so we can decrypt even if not endpoint
    [ upstream commit 1466d7fe7636c1cea644c80a25a41f5717ce6c72 ]
    
    Currently we check if there is a corresponding endpoint for the ip4
    header before decrypting it. But, if we are encrypting the traffic
    to the tunnel for an unmanaged pod we mey not have an endpoint in
    the table. If this is the case move ESP check out of if/else and
    decrypt using IPsec stack.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- bpf/bpf_overlay.c
+++ bpf/bpf_overlay.c
@@ -57,31 +57,32 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 
 	cilium_dbg(skb, DBG_DECAP, key.tunnel_id, key.tunnel_label);
 
-	/* Lookup IPv6 address in list of local endpoints */
-	if ((ep = lookup_ip6_endpoint(ip6)) != NULL) {
 #ifdef ENABLE_IPSEC
-		if (!decrypted) {
-			/* IPSec is not currently enforce (feature coming soon)
-			 * so for now just handle normally
-			 */
-			if (ip6->nexthdr != IPPROTO_ESP)
-				goto not_esp;
-			skb->mark = MARK_MAGIC_DECRYPT;
-			set_identity(skb, key.tunnel_id);
-			/* To IPSec stack on cilium_vxlan we are going to pass
-			 * this up the stack but eth_type_trans has already labeled
-			 * this as an OTHERHOST type packet. To avoid being dropped
-			 * by IP stack before IPSec can be processed mark as a HOST
-			 * packet.
-			 */
-			skb_change_type(skb, PACKET_HOST);
-			return TC_ACT_OK;
-		} else {
-			key.tunnel_id = get_identity(skb);
-			skb->mark = 0;
-		}
+	if (!decrypted) {
+		/* IPSec is not currently enforce (feature coming soon)
+		 * so for now just handle normally
+		 */
+		if (ip6->nexthdr != IPPROTO_ESP)
+			goto not_esp;
+		skb->mark = MARK_MAGIC_DECRYPT;
+		set_identity(skb, key.tunnel_id);
+		/* To IPSec stack on cilium_vxlan we are going to pass
+		 * this up the stack but eth_type_trans has already labeled
+		 * this as an OTHERHOST type packet. To avoid being dropped
+		 * by IP stack before IPSec can be processed mark as a HOST
+		 * packet.
+		 */
+		skb_change_type(skb, PACKET_HOST);
+		return TC_ACT_OK;
+	} else {
+		key.tunnel_id = get_identity(skb);
+		skb->mark = 0;
+	}
 not_esp:
 #endif
+
+	/* Lookup IPv6 address in list of local endpoints */
+	if ((ep = lookup_ip6_endpoint(ip6)) != NULL) {
 		/* Let through packets to the node-ip so they are
 		 * processed by the local ip stack */
 		if (ep->flags & ENDPOINT_F_HOST)
@@ -140,32 +141,32 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	}
 
 	l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
+#ifdef ENABLE_IPSEC
+	if (!decrypted) {
+		/* IPSec is not currently enforce (feature coming soon)
+		 * so for now just handle normally
+		 */
+		if (ip4->protocol != IPPROTO_ESP)
+			goto not_esp;
+		skb->mark = MARK_MAGIC_DECRYPT;
+		set_identity(skb, key.tunnel_id);
+		/* To IPSec stack on cilium_vxlan we are going to pass
+		 * this up the stack but eth_type_trans has already labeled
+		 * this as an OTHERHOST type packet. To avoid being dropped
+		 * by IP stack before IPSec can be processed mark as a HOST
+		 * packet.
+		 */
+		skb_change_type(skb, PACKET_HOST);
+		return TC_ACT_OK;
+	} else {
+		key.tunnel_id = get_identity(skb);
+		skb->mark = 0;
+	}
+not_esp:
+#endif
 
 	/* Lookup IPv4 address in list of local endpoints */
 	if ((ep = lookup_ip4_endpoint(ip4)) != NULL) {
-#ifdef ENABLE_IPSEC
-		if (!decrypted) {
-			/* IPSec is not currently enforce (feature coming soon)
-			 * so for now just handle normally
-			 */
-			if (ip4->protocol != IPPROTO_ESP)
-				goto not_esp;
-			skb->mark = MARK_MAGIC_DECRYPT;
-			set_identity(skb, key.tunnel_id);
-			/* To IPSec stack on cilium_vxlan we are going to pass
-			 * this up the stack but eth_type_trans has already labeled
-			 * this as an OTHERHOST type packet. To avoid being dropped
-			 * by IP stack before IPSec can be processed mark as a HOST
-			 * packet.
-			 */
-			skb_change_type(skb, PACKET_HOST);
-			return TC_ACT_OK;
-		} else {
-			key.tunnel_id = get_identity(skb);
-			skb->mark = 0;
-		}
-not_esp:
-#endif
 		/* Let through packets to the node-ip so they are
 		 * processed by the local ip stack */
 		if (ep->flags & ENDPOINT_F_HOST)
