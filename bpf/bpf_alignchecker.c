23713379efd8c30b575343be9b81535fffccc609
Joe Stringer <joe@cilium.io>
2019-02-28 14:22:37 -0800

    bpf: Adjust alignchecker to avoid stack limits
    When adding more fields to align-check, I hit this error:
    
      error: bpf_alignchecker.c:80:25: in function main i32 (): Looks like the
      BPF stack limit of 512 bytes is exceeded. Please move large on stack
      variables into BPF per-cpu array map.
    
    Avoid this by nesting the structure declarations. While we're at it, use
    a simple iterator to reduce boilerplate when adding new structs.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- bpf/bpf_alignchecker.c
+++ bpf/bpf_alignchecker.c
@@ -22,50 +22,42 @@
 #include "lib/maps.h"
 #include "sockops/bpf_sockops.h"
 
+// DECLARE_STRUCT declares a unique usage of the struct 'x' on the stack.
+//
+// To prevent compiler from optimizing away the var, we pass a reference
+// to the var to a BPF helper function which accepts a reference as
+// an argument.
+#define DECLARE_STRUCT(x, iter)			\
+{						\
+	struct x s ## iter = {};		\
+	trace_printk("%p", 1, &s ## iter);	\
+	iter++;					\
+}
+
 // This function is a placeholder for C struct definitions shared with Go, and
 // it's never being executed.
 int main() {
-    struct ipv4_ct_tuple s0 = {};
-    // To prevent compiler from optimizing away the var, we pass a reference
-    // to the var to a BPF helper function which accepts a reference as
-    // an argument.
-    trace_printk("%p", 1, &s0);
-    struct ipv6_ct_tuple s1 = {};
-    trace_printk("%p", 1, &s1);
-    struct ct_entry s2 = {};
-    trace_printk("%p", 1, &s2);
-    struct ipcache_key s3 = {};
-    trace_printk("%p", 1, &s3);
-    struct remote_endpoint_info s4 = {};
-    trace_printk("%p", 1, &s4);
-    struct lb4_key s5 = {};
-    trace_printk("%p", 1, &s5);
-    struct lb4_service s6 = {};
-    trace_printk("%p", 1, &s6);
-    struct lb6_key s7 = {};
-    trace_printk("%p", 1, &s7);
-    struct lb6_service s8 = {};
-    trace_printk("%p", 1, &s8);
-    struct endpoint_key s9 = {};
-    trace_printk("%p", 1, &s9);
-    struct endpoint_info s10 = {};
-    trace_printk("%p", 1, &s10);
-    struct metrics_key s11 = {};
-    trace_printk("%p", 1, &s11);
-    struct metrics_value s12 = {};
-    trace_printk("%p", 1, &s12);
-    struct proxy4_tbl_key s13 = {};
-    trace_printk("%p", 1, &s13);
-    struct proxy4_tbl_value s14 = {};
-    trace_printk("%p", 1, &s14);
-    struct proxy6_tbl_key s15 = {};
-    trace_printk("%p", 1, &s15);
-    struct proxy6_tbl_value s16 = {};
-    trace_printk("%p", 1, &s16);
-    struct sock_key s17 = {};
-    trace_printk("%p", 1, &s17);
-    struct ep_config s18 = {};
-    trace_printk("%p", 1, &s18);
+    int iter = 0;
+
+    DECLARE_STRUCT(ipv4_ct_tuple, iter);
+    DECLARE_STRUCT(ipv6_ct_tuple, iter);
+    DECLARE_STRUCT(ct_entry, iter);
+    DECLARE_STRUCT(ipcache_key, iter);
+    DECLARE_STRUCT(remote_endpoint_info, iter);
+    DECLARE_STRUCT(lb4_key, iter);
+    DECLARE_STRUCT(lb4_service, iter);
+    DECLARE_STRUCT(lb6_key, iter);
+    DECLARE_STRUCT(lb6_service, iter);
+    DECLARE_STRUCT(endpoint_key, iter);
+    DECLARE_STRUCT(endpoint_info, iter);
+    DECLARE_STRUCT(metrics_key, iter);
+    DECLARE_STRUCT(metrics_value, iter);
+    DECLARE_STRUCT(proxy4_tbl_key, iter)
+    DECLARE_STRUCT(proxy4_tbl_value, iter);
+    DECLARE_STRUCT(proxy6_tbl_key, iter);
+    DECLARE_STRUCT(proxy6_tbl_value, iter);
+    DECLARE_STRUCT(sock_key, iter);
+    DECLARE_STRUCT(ep_config, iter);
 
     return 0;
 }
