efca10b1f88b8b7808aeaa5c3a1449b23e670e7b
Joe Stringer <joe@cilium.io>
2019-02-28 12:44:49 -0800

    test: Wait for cilium to start in runtime provision
    Refactor the code used to ensure that the dev VM's cilium has
    successfully started into a script and reuse it from the ginkgo
    provision scripts. This should ensure that if Cilium crashes shortly
    after startup, the CI provision step will fail with a useful error
    message in the logs.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- contrib/vagrant/start.sh
+++ contrib/vagrant/start.sh
@@ -356,26 +356,7 @@ if [ -n "\${K8S}" ]; then
 fi
 
 service cilium restart
-
-cilium_started=false
-
-for ((i = 0 ; i < 24; i++)); do
-    if cilium status > /dev/null 2>&1; then
-        cilium_started=true
-        break
-    fi
-    sleep 5s
-    echo "Waiting for Cilium daemon to come up..."
-done
-
-if [ "\$cilium_started" = true ] ; then
-    echo 'Cilium successfully started!'
-else
-    >&2 echo 'Timeout waiting for Cilium to start...'
-    journalctl -u cilium.service --since \$(systemctl show -p ActiveEnterTimestamp cilium.service | awk '{print \$2 \$3}')
-    >&2 echo 'Cilium failed to start'
-    exit 1
-fi
+/home/vagrant/go/src/github.com/cilium/cilium/test/provision/wait-cilium.sh
 EOF
 }
 
