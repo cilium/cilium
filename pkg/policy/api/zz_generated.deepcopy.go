39cd2c2be87a8ad110e3e69175195bb39df13b2b
Ian Vernon <ian@cilium.io>
2019-02-28 18:26:58 -0800

    update DeepCopy functions for rule types to include aggregatedSelectors
    Signed-off by: Ian Vernon <ian@cilium.io>

--- pkg/policy/api/zz_generated.deepcopy.go
+++ pkg/policy/api/zz_generated.deepcopy.go
@@ -179,6 +179,13 @@ func (in *EgressRule) DeepCopyInto(out *EgressRule) {
 			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
+	if in.aggregatedSelectors != nil {
+		in, out := &in.aggregatedSelectors, &out.aggregatedSelectors
+		*out = make(EndpointSelectorSlice, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
 	return
 }
 
@@ -342,6 +349,13 @@ func (in *IngressRule) DeepCopyInto(out *IngressRule) {
 		in, out := &in.FromEntities, &out.FromEntities
 		*out = make(EntitySlice, len(*in))
 		copy(*out, *in)
+	}
+	if in.aggregatedSelectors != nil {
+		in, out := &in.aggregatedSelectors, &out.aggregatedSelectors
+		*out = make(EndpointSelectorSlice, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
 	}
 	return
 }
