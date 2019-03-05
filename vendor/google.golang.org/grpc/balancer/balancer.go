32fad4ea94bf410017fb4177394fffcf0e9add98
André Martins <andre@cilium.io>
2019-03-05 14:44:55 +0100

    vendor: downgrade grpc to v1.17.0
    Since grpc v1.18.0 introduces a regressions that prevents cri client to
    connect to crio daemon, we have to downgrade to v1.17.0 once the issue
    is fixed upstream.
    
    Fixes: 00b26af59c9b ("vendor: update google.golang.org/grpc to v1.18.0")
    Signed-off-by: André Martins <andre@cilium.io>

--- vendor/google.golang.org/grpc/balancer/balancer.go
+++ vendor/google.golang.org/grpc/balancer/balancer.go
@@ -28,7 +28,6 @@ import (
 
 	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/internal"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
 )
@@ -46,18 +45,6 @@ var (
 // registered with the same name, the one registered last will take effect.
 func Register(b Builder) {
 	m[strings.ToLower(b.Name())] = b
-}
-
-// unregisterForTesting deletes the balancer with the given name from the
-// balancer map.
-//
-// This function is not thread-safe.
-func unregisterForTesting(name string) {
-	delete(m, name)
-}
-
-func init() {
-	internal.BalancerUnregister = unregisterForTesting
 }
 
 // Get returns the resolver builder registered with the given name.
