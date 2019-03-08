139a8c6681905654a35b41e8b6abff98e6269d99
Martynas Pumputis <m@lambda.lt>
2019-03-08 14:38:43 +0100

    WIP
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -398,8 +398,8 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 	int ret, verdict, l3_off = ETH_HLEN, l4_off, forwarding_reason;
 	struct csum_offset csum_off = {};
 	struct endpoint_info *ep;
-	struct lb4_service *svc;
-	struct lb4_key key = {};
+	struct lb4_service_v2 *svc;
+	struct lb4_key_v2 key = {};
 	struct ct_state ct_state_new = {};
 	struct ct_state ct_state = {};
 	__be32 orig_dip;
@@ -420,7 +420,7 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 
 	l4_off = l3_off + ipv4_hdrlen(ip4);
 
-	ret = lb4_extract_key(skb, &tuple, l4_off, &key, &csum_off, CT_EGRESS);
+	ret = lb4_extract_key_v2(skb, &tuple, l4_off, &key, &csum_off, CT_EGRESS);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_UNKNOWN_L4)
 			goto skip_service_lookup;
@@ -429,7 +429,7 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 	}
 
 	ct_state_new.orig_dport = key.dport;
-	if ((svc = lb4_lookup_service(skb, &key)) != NULL) {
+	if ((svc = lb4_lookup_service_v2(skb, &key)) != NULL) {
 		ret = lb4_local(get_ct_map4(&tuple), skb, l3_off, l4_off, &csum_off,
 				&key, &tuple, svc, &ct_state_new, ip4->saddr);
 		if (IS_ERR(ret))
