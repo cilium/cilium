cilium/cilium

commit d192b4c1f1b00ea0979e2f9282ab42d82d2f4e81

loader: Introduce datapath templates
Introduce template wrappers around an endpoint. This passes through all
datapath configuration parameters that must be applied to implement the
current configuration of the endpoint, but substitutes all static data
for dummy values (typically 0xFFFF).

Add tests for the other side of the logic which substitutes the template
values back into the endpoint's actual static data. This information
will be passed to the ELF library by upcoming loader logic for
substitution in ELF.Write().

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/datapath/loader/template.go
@@ -1,0 +1,194 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package loader
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/cilium/cilium/common/addressing"
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/mac"
+	bpfconfig "github.com/cilium/cilium/pkg/maps/configmap"
+	"github.com/cilium/cilium/pkg/maps/policymap"
+)
+
+var (
+	TemplateSecurityID = identity.ReservedIdentityWorld
+	TemplateLxcID      = uint16(65535)
+	TemplateMAC        = mac.MAC([]byte{0x02, 0x00, 0x60, 0x0D, 0xF0, 0x0D})
+	TemplateIPv4       = []byte{192, 0, 2, 3}
+	TemplateIPv6       = []byte{0x20, 0x01, 0xdb, 0x8, 0x0b, 0xad, 0xca, 0xfe, 0x60, 0x0d, 0xbe, 0xe2, 0x0b, 0xad, 0xca, 0xfe}
+
+	CallsMapName   = "cilium_calls_"
+	elfMapPrefixes = []string{
+		policymap.MapName,
+		CallsMapName,
+		bpfconfig.MapNamePrefix,
+	}
+)
+
+// templateCfg wraps a real configuration from an endpoint to pass through its
+// configuration of conditional branches in the datapath, but to mock out dummy
+// values for static data.
+//
+// Note that the static data dummy values must be non-zero in every 32-bit
+// section of the data to ensure that during compilation, the compiler reserves
+// space in the .data section of the ELF for the value of the data, rather than
+// generating a reference to the .bss section (which is what it will typically
+// do if a static integer is initialized to zero).
+//
+// Ideally we also statically configure the values used in the template in such
+// a way that if ever someone managed to inadvertently attach the template
+// program directly to a device, that there are no unintended consequences such
+// as allowing traffic to leak out with routable addresses.
+type templateCfg struct {
+	datapath.EndpointConfiguration
+}
+
+// GetID returns a uint64, but in practice on the datapath side it is
+// guaranteed to be 16-bit; it is used to generate map names, so we need to
+// ensure that the template generates map names that are as long as the longest
+// possible name, which would be guaranteed with a 5-digit output.
+//
+// In practice, attempts to load an endpoint with the ID 65535 will fail which
+// means that the calling code must approprately substitute the ID in the ELF
+// prior to load, or it will fail with a relatively obvious error.
+func (t *templateCfg) GetID() uint64 {
+	return uint64(TemplateLxcID)
+}
+
+// StringID returns the string form of the ID returned by GetID().
+func (t *templateCfg) StringID() string {
+	return fmt.Sprintf("%d", t.GetID())
+}
+
+// GetIdentity treats the template program as part of the world, so if there's
+// ever some weird bug that causes the template value here to be used, it will
+// be in the least privileged security context.
+func (t *templateCfg) GetIdentity() identity.NumericIdentity {
+	return TemplateSecurityID
+}
+
+// GetNodeMAC returns a well-known dummy MAC address which may be later
+// substituted in the ELF.
+func (t *templateCfg) GetNodeMAC() mac.MAC {
+	return TemplateMAC
+}
+
+// IPv4Address always returns an IP in the documentation prefix (RFC5737) as
+// a nonsense address that should typically not be routable.
+func (t *templateCfg) IPv4Address() addressing.CiliumIPv4 {
+	return addressing.CiliumIPv4(TemplateIPv4)
+}
+
+// IPv6Address returns an IP in the documentation prefix (RFC3849) to ensure
+// that each 32-bit segment of the address is non-zero as per the requirements
+// described in the structure definition. This can't be guaranteed while using
+// a more appropriate prefix such as the discard prefix (RFC6666).
+func (t *templateCfg) IPv6Address() addressing.CiliumIPv6 {
+	return addressing.CiliumIPv6(TemplateIPv6)
+}
+
+// wrap takes an endpoint configuration and optional stats tracker and wraps
+// it inside a templateCfg which hides static data from callers that wish to
+// generate header files based on the configuration, substituting it for
+// template data.
+func wrap(cfg datapath.EndpointConfiguration) *templateCfg {
+	return &templateCfg{
+		EndpointConfiguration: cfg,
+	}
+}
+
+// elfMapSubstitutions returns the set of map substitutions that must occur in
+// an ELF template object file to update map references for the specified
+// endpoint.
+func elfMapSubstitutions(ep endpoint) map[string]string {
+	result := make(map[string]string)
+
+	epID := uint16(ep.GetID())
+	for _, name := range elfMapPrefixes {
+		templateStr := bpf.LocalMapName(name, TemplateLxcID)
+		desiredStr := bpf.LocalMapName(name, epID)
+		result[templateStr] = desiredStr
+	}
+	result[policymap.CallString(TemplateLxcID)] = policymap.CallString(epID)
+	return result
+}
+
+// sliceToU16 converts the input slice of two bytes to a uint16.
+func sliceToU16(input []byte) uint16 {
+	result := uint16(input[0]) << 8
+	result |= uint16(input[1])
+	return result
+}
+
+// sliceToBe16 converts the input slice of two bytes to a big-endian uint16.
+func sliceToBe16(input []byte) uint16 {
+	return byteorder.HostToNetwork(sliceToU16(input)).(uint16)
+}
+
+// sliceToU32 converts the input slice of four bytes to a uint32.
+func sliceToU32(input []byte) uint32 {
+	result := uint32(input[0]) << 24
+	result |= uint32(input[1]) << 16
+	result |= uint32(input[2]) << 8
+	result |= uint32(input[3])
+	return result
+}
+
+// sliceToBe32 converts the input slice of four bytes to a big-endian uint32.
+func sliceToBe32(input []byte) uint32 {
+	return byteorder.HostToNetwork(sliceToU32(input)).(uint32)
+}
+
+// elfVariableSubstitutions returns the set of data substitutions that must
+// occur in an ELF template object file to update static data for the specified
+// endpoint.
+func elfVariableSubstitutions(ep endpoint) map[string]uint32 {
+	result := make(map[string]uint32)
+
+	if ipv6 := ep.IPv6Address(); ipv6 != nil {
+		// Corresponds to DEFINE_IPV6() in bpf/lib/utils.h
+		result["LXC_IP_1"] = sliceToBe32(ipv6[0:4])
+		result["LXC_IP_2"] = sliceToBe32(ipv6[4:8])
+		result["LXC_IP_3"] = sliceToBe32(ipv6[8:12])
+		result["LXC_IP_4"] = sliceToBe32(ipv6[12:16])
+	}
+	if ipv4 := ep.IPv4Address(); ipv4 != nil {
+		result["LXC_IPV4"] = byteorder.HostSliceToNetwork(ipv4, reflect.Uint32).(uint32)
+	}
+
+	mac := ep.GetNodeMAC()
+	result["NODE_MAC_1"] = sliceToBe32(mac[0:4])
+	result["NODE_MAC_2"] = uint32(sliceToBe16(mac[4:6]))
+	result["LXC_ID"] = uint32(ep.GetID())
+	identity := ep.GetIdentity().Uint32()
+	result["SECLABEL"] = identity
+	result["SECLABEL_NB"] = byteorder.HostToNetwork(identity).(uint32)
+
+	return result
+
+}
+
+// ELFSubstitutions fetches the set of variable and map substitutions that
+// must be implemented against an ELF template to configure the datapath for
+// the specified endpoint.
+func ELFSubstitutions(ep endpoint) (map[string]uint32, map[string]string) {
+	return elfVariableSubstitutions(ep), elfMapSubstitutions(ep)
+}

pkg/datapath/datapath.go
@@ -39,7 +39,11 @@ type Datapath interface {
 	// endpoints may have equivalent configurable options.
 	WriteNetdevConfig(io.Writer, DeviceConfiguration) error
 
+	// WriteTemplateConfig writes the implementation-specific configuration
+	// of configurable options for BPF templates to the specified writer.
+	WriteTemplateConfig(w io.Writer, cfg EndpointConfiguration) error
+
 	// WriteEndpointConfig writes the implementation-specific configuration
-	// of configurable options to the specified writer.
+	// of configurable options for the endpoint to the specified writer.
 	WriteEndpointConfig(w io.Writer, cfg EndpointConfiguration) error
 }

pkg/datapath/fake/datapath.go
@@ -54,6 +54,11 @@ func (f *fakeDatapath) WriteNetdevConfig(io.Writer, datapath.DeviceConfiguration
 	return nil
 }
 
+// WriteTemplateConfig pretends to write the endpoint configuration to a writer.
+func (f *fakeDatapath) WriteTemplateConfig(io.Writer, datapath.EndpointConfiguration) error {
+	return nil
+}
+
 // WriteEndpointConfig pretends to write the endpoint configuration to a writer.
 func (f *fakeDatapath) WriteEndpointConfig(io.Writer, datapath.EndpointConfiguration) error {
 	return nil

pkg/datapath/linux/config.go
@@ -194,6 +194,10 @@ func (l *linuxDatapath) WriteEndpointConfig(w io.Writer, e datapath.EndpointConf
 	writeIncludes(w)
 	l.writeStaticData(fw, e)
 
+	return l.writeTemplateConfig(fw, e)
+}
+
+func (l *linuxDatapath) writeTemplateConfig(fw *bufio.Writer, e datapath.EndpointConfiguration) error {
 	if !e.HasIpvlanDataPath() {
 		fmt.Fprint(fw, "#define ENABLE_ARP_RESPONDER 1\n")
 		fmt.Fprint(fw, "#define ENABLE_HOST_REDIRECT 1\n")
@@ -218,4 +222,10 @@ func (l *linuxDatapath) WriteEndpointConfig(w io.Writer, e datapath.EndpointConf
 	l.writeNetdevConfig(fw, e)
 
 	return fw.Flush()
+}
+
+// WriteEndpointConfig writes the BPF configuration for the template to a writer.
+func (l *linuxDatapath) WriteTemplateConfig(w io.Writer, e datapath.EndpointConfiguration) error {
+	fw := bufio.NewWriter(w)
+	return l.writeTemplateConfig(fw, e)
 }

pkg/datapath/linux/config_test.go
@@ -20,8 +20,10 @@ import (
 	"bytes"
 	"errors"
 	"io"
+	"strings"
 
 	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/datapath/loader"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/testutils"
 
@@ -98,4 +100,35 @@ func (s *DatapathSuite) TestWriteEndpointConfig(c *C) {
 	writeConfig(c, "endpoint", func(w io.Writer, dp datapath.Datapath) error {
 		return dp.WriteEndpointConfig(w, &dummyEPCfg)
 	})
+}
+
+func (s *DatapathSuite) TestWriteStaticData(c *C) {
+	dp := NewDatapath(DatapathConfiguration{}).(*linuxDatapath)
+	ep := &dummyEPCfg
+
+	varSub, stringSub := loader.ELFSubstitutions(ep)
+
+	var buf bytes.Buffer
+	dp.writeStaticData(&buf, ep)
+	b := buf.Bytes()
+	for k := range varSub {
+		for _, suffix := range []string{"_1", "_2", "_3", "_4"} {
+			// Variables with these suffixes are implemented via
+			// multiple 32-bit values. The header define doesn't
+			// include these numbers though, so strip them.
+			if strings.HasSuffix(k, suffix) {
+				k = strings.TrimSuffix(k, suffix)
+				break
+			}
+		}
+		c.Assert(bytes.Contains(b, []byte(k)), Equals, true)
+	}
+	for _, v := range stringSub {
+		c.Logf("Ensuring config has %s", v)
+		if strings.HasPrefix(v, "1/0x") {
+			// Skip tail call map name replacement
+			continue
+		}
+		c.Assert(bytes.Contains(b, []byte(v)), Equals, true)
+	}
 }

pkg/datapath/loader/doc_test.go
@@ -1,0 +1,40 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package loader
+
+import (
+	"testing"
+
+	"github.com/cilium/cilium/pkg/node"
+
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+type LoaderTestSuite struct{}
+
+var _ = Suite(&LoaderTestSuite{})
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+func (s *LoaderTestSuite) SetUpTest(c *C) {
+	node.InitDefaultPrefix("")
+	node.SetInternalIPv4(TemplateIPv4)
+	node.SetIPv4Loopback(TemplateIPv4)
+}

pkg/datapath/loader/loader.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"path"
 
+	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
@@ -37,11 +38,11 @@ const (
 // endpoint provides access to endpoint information that is necessary to
 // compile and load the datapath.
 type endpoint interface {
+	datapath.EndpointConfiguration
 	InterfaceName() string
 	Logger(subsystem string) *logrus.Entry
 	StateDir() string
 	MapPath() string
-	HasIpvlanDataPath() bool
 }
 
 func reloadDatapath(ctx context.Context, ep endpoint, dirs *directoryInfo) error {

pkg/datapath/loader/template_test.go
@@ -1,0 +1,57 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package loader
+
+import (
+	"bytes"
+
+	"github.com/cilium/cilium/pkg/checker"
+	"github.com/cilium/cilium/pkg/datapath/linux"
+	"github.com/cilium/cilium/pkg/testutils"
+
+	. "gopkg.in/check.v1"
+)
+
+func (s *LoaderTestSuite) TestWrap(c *C) {
+	var (
+		realEPBuffer   bytes.Buffer
+		templateBuffer bytes.Buffer
+	)
+
+	realEP := testutils.NewTestEndpoint()
+	template := wrap(&realEP)
+	dp := linux.NewDatapath(linux.DatapathConfiguration{})
+
+	// Write the configuration that should be the same, and verify it is.
+	err := dp.WriteTemplateConfig(&realEPBuffer, &realEP)
+	c.Assert(err, IsNil)
+	err = dp.WriteTemplateConfig(&templateBuffer, template)
+	c.Assert(err, IsNil)
+	c.Assert(realEPBuffer.String(), checker.DeepEquals, templateBuffer.String())
+
+	// Write with the static data, and verify that the buffers differ.
+	// Note this isn't an overly strong test because it only takes one
+	// character to change for this test to pass, but we would ideally
+	// define every bit of static data differently in the templates.
+	realEPBuffer.Reset()
+	templateBuffer.Reset()
+	err = dp.WriteEndpointConfig(&realEPBuffer, &realEP)
+	c.Assert(err, IsNil)
+	err = dp.WriteEndpointConfig(&templateBuffer, template)
+	c.Assert(err, IsNil)
+	c.Assert(realEPBuffer.String(), Not(Equals), templateBuffer.String())
+}

commit b7cc6055d707b55fdc8331a4c21190d30043ad64

datapath: Add template substitutions for local conntrack maps.
When endpoint is configured with local conntrack maps, the dummy map
names in the template need to be substituted.

Fixes: d192b4c1f1b00ea0979e2f9282ab42d82d2f4e81
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

cilium/cmd/bpf_ct_flush.go
@@ -17,6 +17,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/cilium/cilium/common"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
@@ -40,11 +41,11 @@ func init() {
 }
 
 type dummyEndpoint struct {
-	ID string
+	ID int
 }
 
-func (d dummyEndpoint) StringID() string {
-	return d.ID
+func (d dummyEndpoint) GetID() uint64 {
+	return uint64(d.ID)
 }
 
 func flushCt(eID string) {
@@ -52,7 +53,8 @@ func flushCt(eID string) {
 	if eID == "global" {
 		maps = ctmap.GlobalMaps(true, true)
 	} else {
-		maps = ctmap.LocalMaps(&dummyEndpoint{ID: eID}, true, true)
+		id, _ := strconv.Atoi(eID)
+		maps = ctmap.LocalMaps(&dummyEndpoint{ID: id}, true, true)
 	}
 	for _, m := range maps {
 		path, err := m.Path()

cilium/cmd/bpf_ct_list.go
@@ -17,6 +17,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/cilium/cilium/common"
 	"github.com/cilium/cilium/pkg/command"
@@ -47,7 +48,8 @@ func dumpCt(eID string) {
 	if eID == "global" {
 		maps = ctmap.GlobalMaps(true, true)
 	} else {
-		maps = ctmap.LocalMaps(&dummyEndpoint{ID: eID}, true, true)
+		id, _ := strconv.Atoi(eID)
+		maps = ctmap.LocalMaps(&dummyEndpoint{ID: id}, true, true)
 	}
 
 	for _, m := range maps {

pkg/datapath/loader/template.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/mac"
 	bpfconfig "github.com/cilium/cilium/pkg/maps/configmap"
+	"github.com/cilium/cilium/pkg/maps/ctmap"
 	"github.com/cilium/cilium/pkg/maps/policymap"
 )
 
@@ -40,6 +41,12 @@ var (
 		policymap.MapName,
 		CallsMapName,
 		bpfconfig.MapNamePrefix,
+	}
+	elfCtMapPrefixes = []string{
+		ctmap.MapNameTCP4,
+		ctmap.MapNameAny4,
+		ctmap.MapNameTCP6,
+		ctmap.MapNameAny6,
 	}
 )
 
@@ -132,6 +139,14 @@ func elfMapSubstitutions(ep endpoint) map[string]string {
 		desiredStr := bpf.LocalMapName(name, epID)
 		result[templateStr] = desiredStr
 	}
+	if ep.ConntrackLocalLocked() {
+		for _, name := range elfCtMapPrefixes {
+			templateStr := bpf.LocalMapName(name, TemplateLxcID)
+			desiredStr := bpf.LocalMapName(name, epID)
+			result[templateStr] = desiredStr
+		}
+	}
+
 	result[policymap.CallString(TemplateLxcID)] = policymap.CallString(epID)
 	return result
 }

pkg/maps/ctmap/ctmap.go
@@ -133,7 +133,7 @@ func init() {
 // CtEndpoint represents an endpoint for the functions required to manage
 // conntrack maps for the endpoint.
 type CtEndpoint interface {
-	StringID() string
+	GetID() uint64
 }
 
 // Map represents an instance of a BPF connection tracking map.
@@ -413,12 +413,16 @@ func maps(e CtEndpoint, ipv4, ipv6 bool) []*Map {
 		}
 	} else {
 		if ipv4 {
-			result = append(result, NewMap(MapNameTCP4+e.StringID(), MapTypeIPv4TCPLocal))
-			result = append(result, NewMap(MapNameAny4+e.StringID(), MapTypeIPv4AnyLocal))
+			result = append(result, NewMap(bpf.LocalMapName(MapNameTCP4, uint16(e.GetID())),
+				MapTypeIPv4TCPLocal))
+			result = append(result, NewMap(bpf.LocalMapName(MapNameAny4, uint16(e.GetID())),
+				MapTypeIPv4AnyLocal))
 		}
 		if ipv6 {
-			result = append(result, NewMap(MapNameTCP6+e.StringID(), MapTypeIPv6TCPLocal))
-			result = append(result, NewMap(MapNameAny6+e.StringID(), MapTypeIPv6AnyLocal))
+			result = append(result, NewMap(bpf.LocalMapName(MapNameTCP6, uint16(e.GetID())),
+				MapTypeIPv6TCPLocal))
+			result = append(result, NewMap(bpf.LocalMapName(MapNameAny6, uint16(e.GetID())),
+				MapTypeIPv6AnyLocal))
 		}
 	}
 	return result