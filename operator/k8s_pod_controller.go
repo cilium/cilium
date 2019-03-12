cilium/cilium

commit da2318f22b2a0c295085c2de82a1bde52d418bac

do not make any more pod annotations
Cilium pod annotations are no longer necessary as they can be deprecated
with the usage of Cilium Endpoints. With this change it also makes sure
we don't need to make unnecessary calls to kube-apiserver.

Signed-off-by: André Martins <andre@cilium.io>

operator/k8s_pod_controller.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018 Authors of Cilium
+// Copyright 2016-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,8 +20,8 @@ import (
 
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
-	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -55,11 +55,15 @@ func enableUnmanagedKubeDNSController() {
 				}
 
 				for _, pod := range pods.Items {
-					id, podHasIdentity := pod.Annotations[ciliumio.CiliumK8sAnnotationPrefix+"identity"]
+					if !pod.Spec.HostNetwork {
+						continue
+					}
+					cep, err := ciliumK8sClient.CiliumV2().CiliumEndpoints(pod.Namespace).Get(pod.Name, metav1.GetOptions{})
 					podID := fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
-					if podHasIdentity {
-						log.Debugf("Found kube-dns pod %s with identity %s", podID, id)
-					} else if !pod.Spec.HostNetwork {
+					switch {
+					case err == nil:
+						log.Debugf("Found kube-dns pod %s with identity %d", podID, cep.Status.ID)
+					case errors.IsNotFound(err):
 						log.Debugf("Found unmanaged kube-dns pod %s", podID)
 						if startTime := pod.Status.StartTime; startTime != nil {
 							if age := time.Since((*startTime).Time); age > unmanagedKubeDnsMinimalAge {
@@ -79,6 +83,8 @@ func enableUnmanagedKubeDNSController() {
 
 							}
 						}
+					default:
+						return err
 					}
 				}
 

cilium/cmd/policy_trace.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2017-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import (
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/k8s"
-	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
 	"github.com/cilium/cilium/pkg/policy/trace"
 
 	"github.com/spf13/cobra"
@@ -52,7 +52,8 @@ var policyTraceCmd = &cobra.Command{
 destination. Source / destination can be provided as endpoint ID, security ID, Kubernetes Pod, YAML file, set of LABELs. LABEL is represented as
 SOURCE:KEY[=VALUE].
 dports can be can be for example: 80/tcp, 53 or 23/udp.
-If multiple sources and / or destinations are provided, each source is tested whether there is a policy allowing traffic between it and each destination`,
+If multiple sources and / or destinations are provided, each source is tested whether there is a policy allowing traffic between it and each destination.
+--src-k8s-pod and --dst-k8s-pod requires cilium-agent to be running with disable-endpoint-crd option set to "false".`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		srcSlices := [][]string{}
@@ -256,22 +257,22 @@ func getSecIDFromK8s(podName string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("unable to create rest configuration: %s", err)
 	}
-	k8sClient, err := k8s.CreateClient(restConfig)
+	ciliumK8sClient, err := clientset.NewForConfig(restConfig)
 	if err != nil {
 		return "", fmt.Errorf("unable to create k8s client: %s", err)
 	}
 
-	p, err := k8sClient.CoreV1().Pods(namespace).Get(pod, meta_v1.GetOptions{})
+	ep, err := ciliumK8sClient.CiliumV2().CiliumEndpoints(namespace).Get(pod, meta_v1.GetOptions{})
 	if err != nil {
 		return "", fmt.Errorf("unable to get pod %s in namespace %s", pod, namespace)
 	}
 
-	secID := p.GetAnnotations()[k8sConst.CiliumIdentityAnnotation]
-	if secID == "" {
-		return "", fmt.Errorf("cilium-identity annotation not set for pod %s in namespace %s", pod, namespace)
+	if ep.Status.Identity == nil {
+		return "", fmt.Errorf("cilium security identity"+
+			" not set for pod %s in namespace %s", pod, namespace)
 	}
 
-	return secID, nil
+	return strconv.Itoa(int(ep.Status.Identity.ID)), nil
 }
 
 // parseL4PortsSlice parses a given `slice` of strings. Each string should be in

pkg/endpoint/policy.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018 Authors of Cilium
+// Copyright 2016-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -41,7 +41,6 @@ import (
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/revert"
 	"github.com/cilium/cilium/pkg/safetime"
-	"github.com/cilium/cilium/pkg/uuid"
 
 	"github.com/sirupsen/logrus"
 )
@@ -459,46 +458,6 @@ func (e *Endpoint) Regenerate(owner Owner, regenMetadata *ExternalRegenerationMe
 	return done
 }
 
-func (e *Endpoint) runIdentityToK8sPodSync() {
-	e.controllers.UpdateController(fmt.Sprintf("sync-identity-to-k8s-pod (%d)", e.ID),
-		controller.ControllerParams{
-			DoFunc: func() error {
-				id := ""
-
-				if err := e.RLockAlive(); err != nil {
-					return controller.NewExitReason("Endpoint disappeared")
-				}
-				if e.SecurityIdentity != nil {
-					id = e.SecurityIdentity.ID.StringID()
-				}
-				e.RUnlock()
-
-				// This is equivalent to checking if K8s is enabled, but by
-				// checking endpoint state instead.
-				if id != "" && e.GetK8sNamespace() != "" && e.GetK8sPodName() != "" {
-					if EpAnnotator != nil {
-						err := EpAnnotator.AnnotatePod(e.GetK8sNamespace(), e.GetK8sPodName(), k8sConst.CiliumIdentityAnnotation, id)
-						if err == nil {
-							log.WithFields(logrus.Fields{
-								logfields.EndpointID:            e.StringID(),
-								logfields.K8sNamespace:          e.GetK8sNamespace(),
-								logfields.K8sPodName:            e.GetK8sPodName(),
-								logfields.K8sIdentityAnnotation: k8sConst.CiliumIdentityAnnotation,
-								logfields.RetryUUID:             uuid.NewUUID(),
-							}).Debugf("Successfully annotated endpoint with %s=%s", logfields.K8sIdentityAnnotation, id)
-						}
-						return err
-					}
-					e.getLogger().Warningf("unable to annotate corresponding pod due to nil annotator")
-				}
-
-				return nil
-			},
-			RunInterval: 1 * time.Minute,
-		},
-	)
-}
-
 // FormatGlobalEndpointID returns the global ID of endpoint in the format
 // / <global ID Prefix>:<cluster name>:<node name>:<endpoint ID> as a string.
 func (e *Endpoint) FormatGlobalEndpointID() string {
@@ -578,8 +537,6 @@ func (e *Endpoint) SetIdentity(identity *identityPkg.Identity) {
 	if e.GetStateLocked() == StateWaitingForIdentity {
 		e.SetStateLocked(StateReady, "Set identity for this endpoint")
 	}
-
-	e.runIdentityToK8sPodSync()
 
 	// Whenever the identity is updated, propagate change to key-value store
 	// of IP to identity mapping.

pkg/k8s/apis/cilium.io/const.go
@@ -56,9 +56,6 @@ const (
 
 	// CiliumK8sAnnotationPrefix is the prefix key for the annotations used in kubernetes.
 	CiliumK8sAnnotationPrefix = "cilium.io/"
-
-	// CiliumIdentityAnnotation is the annotation key used to map to an endpoint's security identity.
-	CiliumIdentityAnnotation = CiliumK8sAnnotationPrefix + "identity"
 	// CiliumIdentityAnnotationDeprecated is the previous annotation key used to map to an endpoint's security identity.
 	CiliumIdentityAnnotationDeprecated = "cilium-identity"
 )

test/helpers/kubectl.go
@@ -1098,6 +1098,24 @@ func (kub *Kubectl) CiliumEndpointWaitReady() error {
 	return NewSSHMetaError(err.Error(), callback)
 }
 
+// WaitForCEPIdentity waits for a particular CEP to have an identity present.
+func (kub *Kubectl) WaitForCEPIdentity(ns, podName string) error {
+	body := func(ctx context.Context) (bool, error) {
+		ep := kub.CepGet(ns, podName)
+		if ep == nil {
+			return false, nil
+		}
+		if ep.Identity == nil {
+			return false, nil
+		}
+		return ep.Identity.ID != 0, nil
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), HelperTimeout)
+	defer cancel()
+	return WithContext(ctx, body, 1*time.Second)
+}
+
 // CiliumExecContext runs cmd in the specified Cilium pod with the given context.
 func (kub *Kubectl) CiliumExecContext(ctx context.Context, pod string, cmd string) *CmdRes {
 	limitTimes := 5

test/k8sT/Policies.go
@@ -17,7 +17,6 @@ package k8sTest
 import (
 	"context"
 	"fmt"
-
 	. "github.com/cilium/cilium/test/ginkgo-ext"
 	"github.com/cilium/cilium/test/helpers"
 	. "github.com/onsi/gomega"
@@ -194,6 +193,11 @@ var _ = Describe("K8sPolicyTest", func() {
 				helpers.KubeSystemNamespace, l3Policy, helpers.KubectlApply, helpers.HelperTimeout)
 			Expect(err).Should(BeNil())
 
+			for _, appName := range []string{helpers.App1, helpers.App2, helpers.App3} {
+				err = kubectl.WaitForCEPIdentity(helpers.DefaultNamespace, appPods[appName])
+				Expect(err).Should(BeNil())
+			}
+
 			trace := kubectl.CiliumExec(ciliumPod, fmt.Sprintf(
 				"cilium policy trace --src-k8s-pod default:%s --dst-k8s-pod default:%s --dport 80",
 				appPods[helpers.App2], appPods[helpers.App1]))
@@ -258,6 +262,11 @@ var _ = Describe("K8sPolicyTest", func() {
 			_, err := kubectl.CiliumPolicyAction(
 				helpers.KubeSystemNamespace, serviceAccountPolicy, helpers.KubectlApply, helpers.HelperTimeout)
 			Expect(err).Should(BeNil())
+
+			for _, appName := range []string{helpers.App1, helpers.App2, helpers.App3} {
+				err = kubectl.WaitForCEPIdentity(helpers.DefaultNamespace, appPods[appName])
+				Expect(err).Should(BeNil())
+			}
 
 			trace := kubectl.CiliumExec(ciliumPod, fmt.Sprintf(
 				"cilium policy trace --src-k8s-pod default:%s --dst-k8s-pod default:%s --dport 80",

commit d4c18abc1d3d8608633bce1a2f8296e2c87062ee

operator: do not restart unmanaged hostNetwork pods
Fixes: da2318f22b2a ("do not make any more pod annotations")
Reported-by: Rahul Prabhu <rahuldprabhu@gmail.com>
Signed-off-by: André Martins <andre@cilium.dev>

operator/k8s_pod_controller.go
@@ -56,7 +56,7 @@ func enableUnmanagedKubeDNSController() {
 				}
 
 				for _, pod := range pods.Items {
-					if !pod.Spec.HostNetwork {
+					if pod.Spec.HostNetwork {
 						continue
 					}
 					cep, err := ciliumK8sClient.CiliumV2().CiliumEndpoints(pod.Namespace).Get(pod.Name, metav1.GetOptions{})