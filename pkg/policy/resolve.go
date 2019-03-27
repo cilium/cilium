65435eab653f5ae61453e5dbafa3b9adfd554555
Joe Stringer <joe@cilium.io>
2019-03-27 15:51:29 -0700

    policy: Split SelectorPolicy from EndpointPolicy
    * SelectorPolicy represents the resolved policy for an Identity, still
      in terms of the label selectors and so on, but with rules merged into a
      common representation. These are oblivious to concrete known identities.
    
    * EndpointPolicy represents the SelectorPolicy which has been
      "distilled" down to the sets of datapath fields that are required to
      implement the policy. This is generated from a set of identities that
      are currently known by Cilium.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/policy/resolve.go
+++ pkg/policy/resolve.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Authors of Cilium
+// Copyright 2018-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,14 +18,17 @@ import (
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy/api"
 	"github.com/cilium/cilium/pkg/policy/trafficdirection"
+
 	"github.com/sirupsen/logrus"
 )
 
-// EndpointPolicy is a structure which contains the resolved policy across all layers
-// (L3, L4, and L7).
-type EndpointPolicy struct {
+// SelectorPolicy is a structure which contains the resolved policy for a
+// particular Identity across all layers (L3, L4, and L7), with the policy
+// still determined in terms of EndpointSelectors.
+type SelectorPolicy struct {
 	// ID is the node-local identifier of this EndpointPolicy.
 	ID uint16
 
@@ -42,6 +45,19 @@ type EndpointPolicy struct {
 	// EgressPolicyEnabled specifies whether this policy contains any policy
 	// at egress.
 	EgressPolicyEnabled bool
+
+	// matchingRules is the set of rules in the repository that were
+	// matched during policy resolution. These refer directly into the
+	// policy repository and must not be modified.
+	//
+	// GH-7516 tracks removal of this field.
+	matchingRules ruleSlice
+}
+
+// EndpointPolicy is a structure which contains the resolved policy across all
+// layers (L3, L4, and L7), distilled against a set of identities.
+type EndpointPolicy struct {
+	*SelectorPolicy
 
 	// PolicyMapState contains the state of this policy as it relates to the
 	// datapath. In the future, this will be factored out of this object to
@@ -69,6 +85,7 @@ type EndpointPolicy struct {
 // PolicyOwner is anything which consumes a EndpointPolicy.
 type PolicyOwner interface {
 	LookupRedirectPort(l4 *L4Filter) uint16
+	GetSecurityIdentity() *identity.Identity
 }
 
 func getSecurityIdentities(labelsMap cache.IdentityCache, selector *api.EndpointSelector) []identity.NumericIdentity {
@@ -84,6 +101,84 @@ func getSecurityIdentities(labelsMap cache.IdentityCache, selector *api.Endpoint
 	}
 
 	return identities
+}
+
+// DistillPolicy filters down the specified SelectorPolicy (which acts upon
+// selectors) into a set of concrete map entries based on the specified
+// identityCache. These can subsequently be plumbed into the datapath.
+//
+// Must be performed while holding the Repository lock.
+func (p *SelectorPolicy) DistillPolicy(policyOwner PolicyOwner, identityCache cache.IdentityCache) *EndpointPolicy {
+
+	calculatedPolicy := &EndpointPolicy{
+		SelectorPolicy:          p,
+		PolicyMapState:          make(MapState),
+		PolicyOwner:             policyOwner,
+		DeniedIngressIdentities: cache.IdentityCache{},
+		DeniedEgressIdentities:  cache.IdentityCache{},
+	}
+
+	labels := policyOwner.GetSecurityIdentity().LabelArray
+	ingressCtx := SearchContext{
+		To:                            labels,
+		rulesSelect:                   true,
+		skipL4RequirementsAggregation: true,
+	}
+
+	egressCtx := SearchContext{
+		From:                          labels,
+		rulesSelect:                   true,
+		skipL4RequirementsAggregation: true,
+	}
+
+	if option.Config.TracingEnabled() {
+		ingressCtx.Trace = TRACE_ENABLED
+		egressCtx.Trace = TRACE_ENABLED
+	}
+
+	if p.IngressPolicyEnabled {
+		for identity, labels := range identityCache {
+			ingressCtx.From = labels
+			egressCtx.To = labels
+
+			ingressAccess := p.matchingRules.canReachIngressRLocked(&ingressCtx)
+			if ingressAccess == api.Allowed {
+				keyToAdd := Key{
+					Identity:         identity.Uint32(),
+					TrafficDirection: trafficdirection.Ingress.Uint8(),
+				}
+				calculatedPolicy.PolicyMapState[keyToAdd] = MapStateEntry{}
+			} else if ingressAccess == api.Denied {
+				calculatedPolicy.DeniedIngressIdentities[identity] = labels
+			}
+		}
+	} else {
+		calculatedPolicy.PolicyMapState.AllowAllIdentities(identityCache, trafficdirection.Ingress)
+	}
+
+	if p.EgressPolicyEnabled {
+		for identity, labels := range identityCache {
+			egressCtx.To = labels
+
+			egressAccess := p.matchingRules.canReachEgressRLocked(&egressCtx)
+			if egressAccess == api.Allowed {
+				keyToAdd := Key{
+					Identity:         identity.Uint32(),
+					TrafficDirection: trafficdirection.Egress.Uint8(),
+				}
+				calculatedPolicy.PolicyMapState[keyToAdd] = MapStateEntry{}
+			} else if egressAccess == api.Denied {
+				calculatedPolicy.DeniedEgressIdentities[identity] = labels
+			}
+		}
+	} else {
+		calculatedPolicy.PolicyMapState.AllowAllIdentities(identityCache, trafficdirection.Egress)
+	}
+
+	calculatedPolicy.computeDesiredL4PolicyMapEntries(identityCache)
+	calculatedPolicy.PolicyMapState.DetermineAllowLocalhost(p.L4Policy)
+
+	return calculatedPolicy
 }
 
 // computeDesiredL4PolicyMapEntries transforms the EndpointPolicy.L4Policy into
@@ -120,17 +215,33 @@ func (p *EndpointPolicy) computeDirectionL4PolicyMapEntries(identityCache cache.
 	}
 }
 
+// NewEndpointPolicy returns an empty EndpointPolicy stub.
+func NewEndpointPolicy() *EndpointPolicy {
+	return &EndpointPolicy{
+		SelectorPolicy: &SelectorPolicy{},
+	}
+}
+
 // Realizes copies the fields from desired into p. It assumes that the fields in
 // desired are not modified after this function is called.
-func (p *EndpointPolicy) Realizes(desired *EndpointPolicy) {
+func (p *SelectorPolicy) Realizes(desired *SelectorPolicy) {
 	if p == nil {
-		p = &EndpointPolicy{}
+		p = &SelectorPolicy{}
 	}
-
 	p.IngressPolicyEnabled = desired.IngressPolicyEnabled
 	p.EgressPolicyEnabled = desired.EgressPolicyEnabled
 	p.L4Policy = desired.L4Policy
 	p.CIDRPolicy = desired.CIDRPolicy
+}
+
+// Realizes copies the fields from desired into p. It assumes that the fields in
+// desired are not modified after this function is called.
+func (p *EndpointPolicy) Realizes(desired *EndpointPolicy) {
+	if p == nil {
+		p = NewEndpointPolicy()
+	}
+
+	p.SelectorPolicy.Realizes(desired.SelectorPolicy)
 	p.DeniedEgressIdentities = desired.DeniedEgressIdentities
 	p.DeniedIngressIdentities = desired.DeniedIngressIdentities
 }
