3337e692dc4d1e8f0cd0e986f53cc0b3b99d6cc0
Martynas Pumputis <m@lambda.lt>
2019-04-02 16:47:21 +0200

    daemon,lbmap: Restore and migrate v2 services
    This commit makes restoration and migration of v2 services possible
    by doing the following:
    
    - Introducing dumping routines of svcv2-related maps. Also, one
      noticeable change that any dump routine does not include
      master service entry in the returned maps.
    - Adding a helper function to restore backend IDs before doing
      the restoration of the service entries.
    - Creating v2 from legacy.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- pkg/maps/lbmap/bpfservice.go
+++ pkg/maps/lbmap/bpfservice.go
@@ -182,16 +182,18 @@ func createBackendsMap(backends []ServiceValue) serviceValueMap {
 	return m
 }
 
-func (l *lbmapCache) restoreService(svc loadbalancer.LBSVC) error {
+// restoreService restores service cache of the given legacy svc. If v2Exists
+// is set, the cache for the respective svc v2 is restored as well.
+func (l *lbmapCache) restoreService(svc loadbalancer.LBSVC, v2Exists bool) error {
 	l.mutex.Lock()
 	defer l.mutex.Unlock()
 
-	frontendID := svc.FE.String()
-
-	serviceKey, serviceValues, err := LBSVC2ServiceKeynValue(svc)
+	serviceKey, serviceValues, err := LBSVC2ServiceKeynValue(&svc)
 	if err != nil {
 		return err
 	}
+
+	frontendID := serviceKey.String()
 
 	bpfSvc, ok := l.entries[frontendID]
 	if !ok {
@@ -201,16 +203,24 @@ func (l *lbmapCache) restoreService(svc loadbalancer.LBSVC) error {
 
 	for index, backend := range serviceValues {
 		b := &bpfBackend{
-			id:       backend.String(),
+			id:       backend.BackendAddrID(),
 			bpfValue: backend,
 		}
-		if _, ok := bpfSvc.uniqueBackends[backend.String()]; ok {
+		if _, ok := bpfSvc.uniqueBackends[backend.BackendAddrID()]; ok {
 			b.isHole = true
 		} else {
-			bpfSvc.uniqueBackends[backend.String()] = backend
+			bpfSvc.uniqueBackends[backend.BackendAddrID()] = backend
+			bpfSvc.slaveSlotByBackendAddrID[backend.BackendAddrID()] = index + 1
 		}
 
 		bpfSvc.backendsByMapIndex[index+1] = b
+	}
+
+	if v2Exists {
+		for addrID, backend := range bpfSvc.uniqueBackends {
+			bpfSvc.backendsV2[addrID] = backend
+			l.addBackendV2Locked(addrID)
+		}
 	}
 
 	return nil
