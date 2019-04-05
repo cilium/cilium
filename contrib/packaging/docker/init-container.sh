4d4c61858e7798aee88ae8def838f4e42629552f
Maciej Kwiek <maciej@covalent.io>
2019-04-05 15:09:01 +0200

    Add bpf mount check to Cilium init container
    Signed-off-by: Maciej Kwiek <maciej@covalent.io>

--- contrib/packaging/docker/init-container.sh
+++ contrib/packaging/docker/init-container.sh
@@ -10,3 +10,6 @@ if [ "${CLEAN_CILIUM_STATE}" = "true" ] \
     rm -rf /sys/fs/bpf/tc/globals/cilium_* \
            /var/run/cilium/bpffs/tc/globals/cilium_*;
 fi
+if [ "${CILIUM_WAIT_BPF_MOUNT}" = "true" ]; then
+	until mount | grep bpf; do echo "BPF filesystem is not mounted yet"; sleep 1; done
+fi;
