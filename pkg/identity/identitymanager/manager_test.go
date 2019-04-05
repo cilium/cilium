907c5a0b90d839a71180b396ac1fc5c5fcc8b481
Ian Vernon <ian@cilium.io>
2019-04-05 14:14:21 -0700

    add identitymanager package
    This package will be used to track what global identities are being consumed
    by the currently running cilium-agent, i.e. what identities are being used
    by endpoints that are currently running. When no endpoint references an
    identity in the identitymanager any more, it is removed from the
    identitymanager.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

---
+++ pkg/identity/identitymanager/manager_test.go
@@ -0,0 +1,90 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package identitymanager
+
+import (
+	"testing"
+
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/labels"
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type IdentityManagerTestSuite struct{}
+
+var _ = Suite(&IdentityManagerTestSuite{})
+
+func (s *IdentityManagerTestSuite) TestIdentityManagerLifecycle(c *C) {
+
+	idFooSelectLabelArray := labels.ParseSelectLabelArray("id=foo")
+	idFooSelectLabels := labels.Labels{}
+	for _, lbl := range idFooSelectLabelArray {
+		idFooSelectLabels[lbl.Key] = lbl
+	}
+
+	idBarSelectLabelArray := labels.ParseSelectLabelArray("id=bar")
+	idBarSelectLabels := labels.Labels{}
+	for _, lbl := range idBarSelectLabelArray {
+		idBarSelectLabels[lbl.Key] = lbl
+	}
+
+	fooIdentity := identity.NewIdentity(identity.NumericIdentity(12345), idFooSelectLabels)
+	barIdentity := identity.NewIdentity(identity.NumericIdentity(54321), idBarSelectLabels)
+
+	idm := NewIdentityManager()
+	c.Assert(idm.identities, Not(IsNil))
+
+	_, exists := idm.identities[fooIdentity.ID]
+	c.Assert(exists, Equals, false)
+
+	idm.Add(fooIdentity)
+	c.Assert(idm.identities[fooIdentity.ID].refCount, Equals, uint(1))
+
+	idm.Add(fooIdentity)
+	c.Assert(idm.identities[fooIdentity.ID].refCount, Equals, uint(2))
+
+	idm.Add(barIdentity)
+	c.Assert(idm.identities[barIdentity.ID].refCount, Equals, uint(1))
+
+	idm.Remove(fooIdentity)
+	c.Assert(idm.identities[fooIdentity.ID].refCount, Equals, uint(1))
+
+	idm.Remove(fooIdentity)
+	_, exists = idm.identities[fooIdentity.ID]
+	c.Assert(exists, Equals, false)
+
+	_, exists = idm.identities[barIdentity.ID]
+	c.Assert(exists, Equals, true)
+
+	idm.Remove(barIdentity)
+	_, exists = idm.identities[barIdentity.ID]
+	c.Assert(exists, Equals, false)
+
+	idm.Add(fooIdentity)
+	_, exists = idm.identities[fooIdentity.ID]
+	c.Assert(exists, Equals, true)
+	idm.RemoveOldAddNew(fooIdentity, barIdentity)
+	_, exists = idm.identities[fooIdentity.ID]
+	c.Assert(exists, Equals, false)
+	_, exists = idm.identities[barIdentity.ID]
+	c.Assert(exists, Equals, true)
+}
