cilium/cilium

commit df8582d16e51792e62ef1a512a1c13a4fd6a8057

datapath: Optimize connection-tracking GC interval
The original default for the connection-tracking GC interval was set to 1
minute. This is very aggressive and leads to a significant amount of system
calls. They are incredibly cheap but it still adds up.

Improve the situation with the following changes:

 * Change the default to 15 minutes for old kernels and 12 hours for kernels
   with LRU maps. LRU maps will automatically obsolete the least recently used
   entries when the table fills ups. Scanning the table will taint all entries
   and corrupt proper LRU behavior.

 * Deprecate the option --conntrack-garbage-collector-interval in favour
   of --conntrack-gc-interval which parses a duration instead of the number
   seconds.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/option/config.go
@@ -78,9 +78,12 @@ const (
 	// represents the value of that option.
 	ConfigDir = "config-dir"
 
-	// ConntrackGarbageCollectorInterval is the garbage collection interval for
-	// the connection tracking table (in seconds)
-	ConntrackGarbageCollectorInterval = "conntrack-garbage-collector-interval"
+	// ConntrackGarbageCollectorIntervalDeprecated is the deprecated option
+	// name to set the conntrack gc interval
+	ConntrackGarbageCollectorIntervalDeprecated = "conntrack-garbage-collector-interval"
+
+	// ConntrackGCInterval is the name of the ConntrackGCInterval option
+	ConntrackGCInterval = "conntrack-gc-interval"
 
 	// ContainerRuntime sets the container runtime(s) used by Cilium
 	// { containerd | crio | docker | none | auto } ( "auto" uses the container
@@ -726,42 +729,41 @@ type DaemonConfig struct {
 
 	// CLI options
 
-	BPFRoot                           string
-	CGroupRoot                        string
-	BPFCompileDebug                   string
-	ConfigFile                        string
-	ConfigDir                         string
-	ConntrackGarbageCollectorInterval int
-	ContainerRuntimeEndpoint          map[string]string
-	Debug                             bool
-	DebugVerbose                      []string
-	DisableConntrack                  bool
-	DisableK8sServices                bool
-	DockerEndpoint                    string
-	EnablePolicy                      string
-	EnableTracing                     bool
-	EnvoyLog                          string
-	DisableEnvoyVersionCheck          bool
-	FixedIdentityMapping              map[string]string
-	FixedIdentityMappingValidator     func(val string) (string, error)
-	IPv4ClusterCIDRMaskSize           int
-	IPv4Range                         string
-	IPv6Range                         string
-	IPv4ServiceRange                  string
-	IPv6ServiceRange                  string
-	K8sAPIServer                      string
-	K8sKubeConfigPath                 string
-	K8sLegacyHostAllowsWorld          string
-	K8sWatcherEndpointSelector        string
-	KVStore                           string
-	KVStoreOpt                        map[string]string
-	LabelPrefixFile                   string
-	Labels                            []string
-	LogDriver                         []string
-	LogOpt                            map[string]string
-	Logstash                          bool
-	LogSystemLoadConfig               bool
-	NAT46Range                        string
+	BPFRoot                       string
+	CGroupRoot                    string
+	BPFCompileDebug               string
+	ConfigFile                    string
+	ConfigDir                     string
+	ContainerRuntimeEndpoint      map[string]string
+	Debug                         bool
+	DebugVerbose                  []string
+	DisableConntrack              bool
+	DisableK8sServices            bool
+	DockerEndpoint                string
+	EnablePolicy                  string
+	EnableTracing                 bool
+	EnvoyLog                      string
+	DisableEnvoyVersionCheck      bool
+	FixedIdentityMapping          map[string]string
+	FixedIdentityMappingValidator func(val string) (string, error)
+	IPv4ClusterCIDRMaskSize       int
+	IPv4Range                     string
+	IPv6Range                     string
+	IPv4ServiceRange              string
+	IPv6ServiceRange              string
+	K8sAPIServer                  string
+	K8sKubeConfigPath             string
+	K8sLegacyHostAllowsWorld      string
+	K8sWatcherEndpointSelector    string
+	KVStore                       string
+	KVStoreOpt                    map[string]string
+	LabelPrefixFile               string
+	Labels                        []string
+	LogDriver                     []string
+	LogOpt                        map[string]string
+	Logstash                      bool
+	LogSystemLoadConfig           bool
+	NAT46Range                    string
 
 	// Masquerade specifies whether or not to masquerade packets from endpoints
 	// leaving the host.
@@ -855,6 +857,10 @@ type DaemonConfig struct {
 	// are regenerated upon every policy change regardless of the scope of the
 	// policy change.
 	SelectiveRegeneration bool
+
+	// ConntrackGCInterval is the connection tracking garbage collection
+	// interval
+	ConntrackGCInterval time.Duration
 }
 
 var (
@@ -1085,7 +1091,6 @@ func (c *DaemonConfig) Populate() {
 	c.ClusterID = viper.GetInt(ClusterIDName)
 	c.ClusterName = viper.GetString(ClusterName)
 	c.ClusterMeshConfig = viper.GetString(ClusterMeshConfigName)
-	c.ConntrackGarbageCollectorInterval = viper.GetInt(ConntrackGarbageCollectorInterval)
 	c.DatapathMode = viper.GetString(DatapathMode)
 	c.Debug = viper.GetBool(DebugArg)
 	c.DebugVerbose = viper.GetStringSlice(DebugVerbose)
@@ -1198,6 +1203,13 @@ func (c *DaemonConfig) Populate() {
 
 	if m := viper.GetStringMapString(LogOpt); len(m) != 0 {
 		c.LogOpt = m
+	}
+
+	if viper.IsSet(ConntrackGarbageCollectorIntervalDeprecated) {
+		val := time.Duration(viper.GetInt(ConntrackGarbageCollectorIntervalDeprecated))
+		c.ConntrackGCInterval = val * time.Second
+	} else {
+		c.ConntrackGCInterval = viper.GetDuration(ConntrackGCInterval)
 	}
 
 	// Hidden options

daemon/daemon_main.go
@@ -376,8 +376,12 @@ func init() {
 	flags.String(option.ConfigDir, "", `Configuration directory that contains a file for each option`)
 	option.BindEnv(option.ConfigDir)
 
-	flags.Uint(option.ConntrackGarbageCollectorInterval, 60, "Garbage collection interval for the connection tracking table (in seconds)")
-	option.BindEnv(option.ConntrackGarbageCollectorInterval)
+	flags.Uint(option.ConntrackGarbageCollectorIntervalDeprecated, 0, "Garbage collection interval for the connection tracking table (in seconds)")
+	flags.MarkDeprecated(option.ConntrackGarbageCollectorIntervalDeprecated, fmt.Sprintf("please use --%s", option.ConntrackGCInterval))
+	option.BindEnv(option.ConntrackGarbageCollectorIntervalDeprecated)
+
+	flags.Duration(option.ConntrackGCInterval, time.Duration(0), "Overwrite the connection-tracking garbage collection interval")
+	option.BindEnv(option.ConntrackGCInterval)
 
 	flags.StringSlice(option.ContainerRuntime, []string{"auto"}, `Sets the container runtime(s) used by Cilium { containerd | crio | docker | none | auto } ( "auto" uses the container runtime found in the order: "docker", "containerd", "crio" )`)
 	option.BindEnv(option.ContainerRuntime)
@@ -1153,7 +1157,6 @@ func runDaemon() {
 	bootstrapStats.enableConntrack.Start()
 	log.Info("Starting connection tracking garbage collector")
 	endpointmanager.EnableConntrackGC(option.Config.EnableIPv4, option.Config.EnableIPv6,
-		option.Config.ConntrackGarbageCollectorInterval,
 		restoredEndpoints.restored)
 	bootstrapStats.enableConntrack.End(true)
 

pkg/defaults/defaults.go
@@ -181,4 +181,12 @@ const (
 	// K8sWatcherEndpointSelector specifies the k8s endpoints that Cilium
 	// should watch for.
 	K8sWatcherEndpointSelector = "metadata.name!=kube-scheduler,metadata.name!=kube-controller-manager,metadata.name!=etcd-operator,metadata.name!=gcp-controller-manager"
+
+	// ConntrackGCIntervalLRU is the default connection tracking interval
+	// when using LRU maps
+	ConntrackGCIntervalLRU = 12 * time.Hour
+
+	// ConntrackGCIntervalNonLRU is the default connection tracking
+	// interval when using non-LRU maps
+	ConntrackGCIntervalNonLRU = 15 * time.Minute
 )

pkg/endpointmanager/conntrack.go
@@ -19,16 +19,14 @@ import (
 	"os"
 	"time"
 
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
+	"github.com/cilium/cilium/pkg/option"
 
 	"github.com/sirupsen/logrus"
-)
-
-const (
-	// MinGcInterval is the minimum garbage collection interval.
-	MinGcInterval int = 5
 )
 
 // runGC run CT's garbage collector for the given endpoint. `isLocal` refers if
@@ -39,7 +37,7 @@ const (
 // The provided endpoint is optional; if it is provided, then its map will be
 // garbage collected and any failures will be logged to the endpoint log.
 // Otherwise it will garbage-collect the global map and use the global log.
-func runGC(e *endpoint.Endpoint, ipv4, ipv6 bool, filter *ctmap.GCFilter) {
+func runGC(e *endpoint.Endpoint, ipv4, ipv6 bool, filter *ctmap.GCFilter) (mapType bpf.MapType) {
 	var maps []*ctmap.Map
 
 	if e == nil {
@@ -67,6 +65,8 @@ func runGC(e *endpoint.Endpoint, ipv4, ipv6 bool, filter *ctmap.GCFilter) {
 		}
 		defer m.Close()
 
+		mapType = m.MapInfo.MapType
+
 		deleted := ctmap.GC(m, filter)
 
 		if deleted > 0 {
@@ -76,6 +76,8 @@ func runGC(e *endpoint.Endpoint, ipv4, ipv6 bool, filter *ctmap.GCFilter) {
 			}).Debug("Deleted filtered entries from map")
 		}
 	}
+
+	return
 }
 
 func createGCFilter(initialScan bool, restoredEndpoints []*endpoint.Endpoint) *ctmap.GCFilter {
@@ -98,21 +100,35 @@ func createGCFilter(initialScan bool, restoredEndpoints []*endpoint.Endpoint) *c
 	return filter
 }
 
+func calculateInterval(mapType bpf.MapType) time.Duration {
+	if val := option.Config.ConntrackGCInterval; val != time.Duration(0) {
+		return val
+	}
+
+	bpfNAT := !option.Config.InstallIptRules && option.Config.Masquerade
+
+	//  Fall-back to short interval when BPF NAT is enabled for now as the
+	//  NAT entry expiration relies on a frequent cleanup
+	if mapType == bpf.MapTypeLRUHash && !bpfNAT {
+		return defaults.ConntrackGCIntervalLRU
+	}
+
+	return defaults.ConntrackGCIntervalNonLRU
+}
+
 // EnableConntrackGC enables the connection tracking garbage collection.
-func EnableConntrackGC(ipv4, ipv6 bool, gcinterval int, restoredEndpoints []*endpoint.Endpoint) {
-	initialScan := true
-	initialScanComplete := make(chan struct{})
+func EnableConntrackGC(ipv4, ipv6 bool, restoredEndpoints []*endpoint.Endpoint) {
+	var (
+		initialScan         = true
+		initialScanComplete = make(chan struct{})
+		mapType             bpf.MapType
+	)
 
 	go func() {
-		if gcinterval < MinGcInterval {
-			gcinterval = MinGcInterval
-			log.Warnf("Setting conntrack garbage collector interval to its minimum value(%d seconds)", gcinterval)
-		}
-		sleepTime := time.Duration(gcinterval) * time.Second
 		for {
 			eps := GetEndpoints()
 			if len(eps) > 0 || initialScan {
-				runGC(nil, ipv4, ipv6, createGCFilter(initialScan, restoredEndpoints))
+				mapType = runGC(nil, ipv4, ipv6, createGCFilter(initialScan, restoredEndpoints))
 			}
 			for _, e := range eps {
 				if !e.ConntrackLocal() {
@@ -127,7 +143,7 @@ func EnableConntrackGC(ipv4, ipv6 bool, gcinterval int, restoredEndpoints []*end
 				initialScan = false
 			}
 
-			time.Sleep(sleepTime)
+			time.Sleep(calculateInterval(mapType))
 		}
 	}()
 

pkg/maps/ctmap/ctmap.go
@@ -367,11 +367,6 @@ func doGC(m *Map, filter *GCFilter) int {
 // It returns how many items were deleted from m.
 func GC(m *Map, filter *GCFilter) int {
 	if filter.RemoveExpired {
-		// If LRUHashtable, no need to garbage collect as LRUHashtable cleans itself up.
-		// FIXME: GH-3239 LRU logic is not handling timeouts gracefully enough
-		// if m.MapInfo.MapType == bpf.MapTypeLRUHash {
-		// 	return 0
-		// }
 		t, _ := bpf.GetMtime()
 		tsec := t / 1000000000
 		filter.Time = uint32(tsec)

commit cb5ec00680ad22c2b2478c7f47282f106b096153

agent: Fix --contrack-gc-interval option
The option was always ignored

Fixes: #7649
Fixes: df8582d16e5 ("datapath: Optimize connection-tracking GC interval")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/option/config.go
@@ -1205,9 +1205,8 @@ func (c *DaemonConfig) Populate() {
 		c.LogOpt = m
 	}
 
-	if viper.IsSet(ConntrackGarbageCollectorIntervalDeprecated) {
-		val := time.Duration(viper.GetInt(ConntrackGarbageCollectorIntervalDeprecated))
-		c.ConntrackGCInterval = val * time.Second
+	if val := viper.GetInt(ConntrackGarbageCollectorIntervalDeprecated); val != 0 {
+		c.ConntrackGCInterval = time.Duration(val) * time.Second
 	} else {
 		c.ConntrackGCInterval = viper.GetDuration(ConntrackGCInterval)
 	}