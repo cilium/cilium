10e956f5c1db79d43425911c317edf6736609b12
Martynas Pumputis <m@lambda.lt>
2019-04-10 14:11:20 +0200

    cli: Use svc v2 maps when listing
    This commit changes the source maps of the `cilium bpf lb list`
    cmd - instead of the legacy svc BPF maps, the v2 BPF are used.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- cilium/cmd/bpf_lb_list.go
+++ cilium/cmd/bpf_lb_list.go
@@ -15,8 +15,12 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/command"
+	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
 
 	"github.com/spf13/cobra"
@@ -49,11 +53,55 @@ var bpfLBListCmd = &cobra.Command{
 				Fatalf("Unable to dump IPv6 reverse NAT table: %s", err)
 			}
 		} else {
+			// It's safe to use the same map for both IPv4 and IPv6, as backend
+			// IDs are allocated from the same pool regardless the protocol
+			backendMap := make(map[loadbalancer.BackendID]lbmap.BackendValue)
+
+			parseBackendEntry := func(key bpf.MapKey, value bpf.MapValue) {
+				id := key.(lbmap.BackendKey).GetID()
+				backendMap[id] = value.(lbmap.BackendValue)
+			}
+			if err := lbmap.Backend4Map.DumpWithCallbackIfExists(parseBackendEntry); err != nil {
+				Fatalf("Unable to dump IPv4 backends table: %s", err)
+			}
+			if err := lbmap.Backend6Map.DumpWithCallbackIfExists(parseBackendEntry); err != nil {
+				Fatalf("Unable to dump IPv6 backends table: %s", err)
+			}
+
+			parseSVCEntry := func(key bpf.MapKey, value bpf.MapValue) {
+				var entry string
+
+				svcKey := key.(lbmap.ServiceKeyV2)
+				svcVal := value.(lbmap.ServiceValueV2)
+				svc := svcKey.String()
+				revNATID := svcVal.GetRevNat()
+				backendID := svcVal.GetBackendID()
+
+				if backendID == 0 {
+					ip := "0.0.0.0"
+					if svcKey.IsIPv6() {
+						ip = "[::]"
+					}
+					entry = fmt.Sprintf("%s:%d (%d)", ip, 0, revNATID)
+				} else if backend, found := backendMap[backendID]; !found {
+					entry = fmt.Sprintf("backend %d not found", backendID)
+				} else {
+					fmtStr := "%s:%d (%d)"
+					if svcKey.IsIPv6() {
+						fmtStr = "[%s]:%d (%d)"
+					}
+					entry = fmt.Sprintf(fmtStr, backend.GetAddress(),
+						backend.GetPort(), revNATID)
+				}
+
+				serviceList[svc] = append(serviceList[svc], entry)
+			}
+
 			firstTitle = serviceAddressTitle
-			if err := lbmap.Service4Map.DumpIfExists(serviceList); err != nil {
+			if err := lbmap.Service4MapV2.DumpWithCallback(parseSVCEntry); err != nil {
 				Fatalf("Unable to dump IPv4 services table: %s", err)
 			}
-			if err := lbmap.Service6Map.DumpIfExists(serviceList); err != nil {
+			if err := lbmap.Service6MapV2.DumpWithCallback(parseSVCEntry); err != nil {
 				Fatalf("Unable to dump IPv6 services table: %s", err)
 			}
 		}
