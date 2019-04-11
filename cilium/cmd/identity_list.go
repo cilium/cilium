00143cf0369be8621cffe85e4b73642048c2959e
Ian Vernon <ian@cilium.io>
2019-04-11 09:03:54 -0700

    cmd: add `cilium identity list --endpoints` command
    This command allows for introspecting about the current state of the
    GlobalIdentityManager in cilium.
    
    Example:
    
    ```
    $ cilium identity list --endpoints
    getting identities endpoints
    ID      LABELS                                                 REFCOUNT
    4       reserved:health                                        1
    104     k8s:io.cilium.k8s.policy.cluster=default               1
            k8s:io.cilium.k8s.policy.serviceaccount=coredns
            k8s:io.kubernetes.pod.namespace=kube-system
            k8s:k8s-app=kube-dns
    24857   k8s:appSecond=true                                     2
            k8s:id=app2
            k8s:io.cilium.k8s.policy.cluster=default
            k8s:io.cilium.k8s.policy.serviceaccount=app2-account
            k8s:io.kubernetes.pod.namespace=default
            k8s:zgroup=testapp
    33585   k8s:id=app1                                            4
            k8s:io.cilium.k8s.policy.cluster=default
            k8s:io.cilium.k8s.policy.serviceaccount=app1-account
            k8s:io.kubernetes.pod.namespace=default
            k8s:zgroup=testapp
    58550   k8s:id=app3                                            2
            k8s:io.cilium.k8s.policy.cluster=default
            k8s:io.cilium.k8s.policy.serviceaccount=default
            k8s:io.kubernetes.pod.namespace=default
            k8s:zgroup=testapp
    ```
    
    This shows that for identity 33585, 4 endpoints are consuming this identity.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- cilium/cmd/identity_list.go
+++ cilium/cmd/identity_list.go
@@ -15,13 +15,20 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
 	"sort"
+	"text/tabwriter"
 
 	identityApi "github.com/cilium/cilium/api/v1/client/policy"
+	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/api"
 	pkg "github.com/cilium/cilium/pkg/client"
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/identity/identitymanager"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/spf13/viper"
 
 	"github.com/spf13/cobra"
 )
@@ -39,25 +46,65 @@ var identityListCmd = &cobra.Command{
 func init() {
 	identityCmd.AddCommand(identityListCmd)
 	command.AddJSONOutput(identityListCmd)
+	flags := identityListCmd.Flags()
+	flags.Bool("endpoints", false, "list identities of locally managed endpoints")
+	viper.BindPFlags(flags)
 }
 
 func listIdentities(args []string) {
-	params := identityApi.NewGetIdentityParams().WithTimeout(api.ClientTimeout)
-	if len(args) != 0 {
-		params = params.WithLabels(args)
-	}
-
-	identities, err := client.Policy.GetIdentity(params)
-	if err != nil {
-		if params != nil {
-			Fatalf("Cannot get identities for given labels %v. err: %s\n", params.Labels, err.Error())
-		} else {
+	switch {
+	case viper.GetBool("endpoints"):
+		params := identityApi.NewGetIdentityEndpointsParams().WithTimeout(api.ClientTimeout)
+		identities, err := client.Policy.GetIdentityEndpoints(params)
+		if err != nil {
 			Fatalf("Cannot get identities. err: %s", pkg.Hint(err))
 		}
+		// sort identities by ID
+		im := identitymanager.IdentitiesModel(identities.Payload)
+		sort.Slice(im, im.Less)
+		printIdentitesEndpoints(identities.Payload)
+	default:
+		params := identityApi.NewGetIdentityParams().WithTimeout(api.ClientTimeout)
+		if len(args) != 0 {
+			params = params.WithLabels(args)
+		}
+		identities, err := client.Policy.GetIdentity(params)
+		if err != nil {
+			if params != nil {
+				Fatalf("Cannot get identities for given labels %v. err: %s\n", params.Labels, err.Error())
+			} else {
+				Fatalf("Cannot get identities. err: %s", pkg.Hint(err))
+			}
+		}
+		// sort identities by ID
+		im := cache.IdentitiesModel(identities.Payload)
+		sort.Slice(im, im.Less)
+		printIdentities(identities.Payload)
 	}
+}
 
-	// sort identities by ID
-	im := cache.IdentitiesModel(identities.Payload)
-	sort.Slice(im, im.Less)
-	printIdentities(identities.Payload)
+func printIdentitesEndpoints(identities []*models.IdentityEndpoints) {
+	if command.OutputJSON() {
+		if err := command.PrintOutput(identities); err != nil {
+			Fatalf("Unable to provide JSON output: %s", err)
+		}
+	} else {
+		w := tabwriter.NewWriter(os.Stdout, 5, 0, 3, ' ', 0)
+
+		fmt.Fprintf(w, "ID\tLABELS\tREFCOUNT\n")
+		for _, identity := range identities {
+			lbls := labels.NewLabelsFromModel(identity.Identity.Labels)
+			first := true
+			for _, lbl := range lbls.GetPrintableModel() {
+				if first {
+					fmt.Fprintf(w, "%d\t%s\t%d\t\n", identity.Identity.ID, lbl, identity.RefCount)
+					first = false
+				} else {
+					fmt.Fprintf(w, "\t%s\t\n", lbl)
+				}
+			}
+		}
+
+		w.Flush()
+	}
 }
