9f8a80bc92d63a27a73fb620dd5a64844f299570
André Martins <andre@cilium.io>
2019-04-12 20:43:41 +0200

    operator: GC nodes from existing CNPs
    [ upstream commit 0cd1be0e64dc848ea9db83a90e6e2564a4a22f9f ]
    
    As nodes can be removed from the cluster their status in the CNP can be
    left behind. The operator will be checking if nodes are still running
    and if not, they will be removed from the CNP Node Status.
    
    Signed-off-by: André Martins <andre@cilium.io>
    Signed-off-by: Ian Vernon <ian@cilium.io>

--- pkg/k8s/json_patch.go
+++ pkg/k8s/json_patch.go
@@ -14,6 +14,12 @@
 
 package k8s
 
+const(
+	// maximum number of operations a single json patch may contain.
+	// See https://github.com/kubernetes/kubernetes/pull/74000
+	MaxJSONPatchOperations = 10000
+)
+
 // JSONPatch structure based on the RFC 6902
 type JSONPatch struct {
 	OP    string      `json:"op,omitempty"`
