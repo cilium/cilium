f7d43a1e2fb48ed2da2e069b0e5850a5644d9059
Martynas Pumputis <m@lambda.lt>
2019-04-12 11:52:26 +0200

    lbmap: Fix BackendAddrID of IPv6 backend
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- pkg/maps/lbmap/lbmap_test.go
+++ pkg/maps/lbmap/lbmap_test.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/checker"
 	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/u8proto"
 
 	. "gopkg.in/check.v1"
 )
@@ -214,4 +215,17 @@ func (b *LBMapTestSuite) TestGetBackends(c *C) {
 	c.Assert(len(backends), Equals, 2)
 	c.Assert(backends[0], checker.DeepEquals, b1)
 	c.Assert(backends[1], checker.DeepEquals, b2)
+}
+
+func (b *LBMapTestSuite) TestBackendAddrID(c *C) {
+	b4, err := NewBackend4Value(net.ParseIP("1.1.1.1"), 80, u8proto.All)
+	c.Assert(err, IsNil)
+	v4 := NewService4Value(0, net.ParseIP("1.1.1.1"), 80, 0, 0)
+	c.Assert(b4.BackendAddrID(), Equals, v4.BackendAddrID())
+
+	b6, err := NewBackend6Value(net.ParseIP("f00d::0:0"), 80, u8proto.All)
+	c.Assert(err, IsNil)
+	v6 := NewService6Value(0, net.ParseIP("f00d::0:0"), 80, 0, 0)
+	c.Assert(b6.BackendAddrID(), Equals, v6.BackendAddrID())
+
 }
