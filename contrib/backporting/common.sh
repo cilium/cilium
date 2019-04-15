e733dc12ae3a3f997257eff89b7491e20e18b7b3
Martynas Pumputis <m@lambda.lt>
2019-04-15 21:06:49 +0200

    contrib: Exit early if no git remote is found
    [ upstream commit 65b001f4377904768be03384b68a3324f00a072e ]
    
    Previously, if no git remote pointing to git@github.com:cilium/cilium.git
    or https://github.com/cilium/cilium was found, the `check-stable` and
    `cherry-pick` scripts continued to execute nevertheless which lead to
    cryptic errors.
    
    This commit makes both scripts to exit early and also to print an
    error to stderr that the git remote could not have been found.
    
    Also, factor out the common code into the `get_remote` function which
    can be shared by sourcing common.sh.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

---
+++ contrib/backporting/common.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright 2019 Authors of Cilium
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+get_remote () {
+  local remote
+  remote=$(git remote -v | \
+    grep "git@github.com:cilium/cilium.git\|https://github.com/cilium/cilium" | \
+    head -n1 | cut -f1)
+  if [ -z "$remote" ]; then
+      echo "No remote git@github:cilium/cilium.git or https://github.com/cilium/cilium found" 1>&2
+      return 1
+  fi
+  echo "$remote"
+}
