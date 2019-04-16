5635e6f0f417f5d5541fe3667e2236c5fe0b9364
Joe Stringer <joe@cilium.io>
2019-04-16 16:04:02 -0700

    identitymanager: Support subscribing to events
    Add a simple callback mechanism to allow observers to be notified when
    an identity becomes used or no longer used by endpoints that are local
    to the node.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

---
+++ pkg/identity/identitymanager/observer.go
@@ -0,0 +1,27 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package identitymanager
+
+import (
+	"github.com/cilium/cilium/pkg/identity"
+)
+
+// Observer can sign up to receive events whenever local identities are removed.
+type Observer interface {
+	// LocalEndpointIdentityRemoved is called when an identity is no longer
+	// in use on the node. Implementations must ensure that the callback
+	// returns within a reasonable period.
+	LocalEndpointIdentityRemoved(*identity.Identity)
+}
