dccb8ba2a7186adc2ac835c1a075016fd8195974
Ray Bejjani <ray@covalent.io>
2019-04-18 17:32:52 +0200

    Don't match nil SecurityIdentity
    Signed-off-by: Ray Bejjani <ray@covalent.io>

--- pkg/policy/rule.go
+++ pkg/policy/rule.go
@@ -500,6 +500,10 @@ func (r *rule) meetsRequirementsIngress(ctx *SearchContext, state *traceState) a
 }
 
 func (r *rule) matches(id uint16, securityIdentity *identity.Identity) bool {
+	if securityIdentity == nil {
+		return false
+	}
+
 	r.metadata.Mutex.Lock()
 	defer r.metadata.Mutex.Unlock()
 	var ruleMatches bool
