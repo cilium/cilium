25046f42f9ea824f8c3cb878a27919f311b7a7c8
Jarno Rajahalme <jarno@covalent.io>
2019-04-22 07:55:02 -0700

    istio: Update to release 1.1.3
    [ upstream commit 08d1cfd851070e22a2b9d3c52a192653a06cba34 ]
    
    Update the CI and the getting started guide to Istio 1.1.3.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>
    Signed-off-by: Joe Stringer <joe@wand.net.nz>

--- vagrant_box_defaults.rb
+++ vagrant_box_defaults.rb
@@ -3,6 +3,6 @@
 Vagrant.require_version ">= 2.2.0"
 
 $SERVER_BOX = "cilium/ubuntu-dev"
-$SERVER_VERSION= "126"
+$SERVER_VERSION= "147"
 $NETNEXT_SERVER_BOX= "cilium/ubuntu-next"
-$NETNEXT_SERVER_VERSION= "6"
+$NETNEXT_SERVER_VERSION= "21"
