cilium/cilium

commit 7321663ff855ee6a0e696eb271e6829ebb84c80b

policy: store L3-only policy in L4Filter
This allow for all policy at L3 to be stored in a single construct, and
means that all resolved policy is now specified in terms of selectors.

Resolution to identities is now done in the translation to PolicyMap keys
and in the xDS layer.

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/policy/rule.go
@@ -165,32 +165,29 @@ func mergeL4Port(ctx *SearchContext, endpoints []api.EndpointSelector, existingF
 // into L7 wildcards (ie, traffic will be forwarded to the proxy for endpoints
 // matching those labels, but the proxy will allow all such traffic).
 func mergeL4IngressPort(ctx *SearchContext, endpoints []api.EndpointSelector, endpointsWithL3Override []api.EndpointSelector, r api.PortRule, p api.PortProtocol,
-	proto api.L4Proto, ruleLabels labels.LabelArray, resMap L4PolicyMap) (int, error) {
+	proto api.L4Proto, ruleLabels labels.LabelArray, resMap L4PolicyMap, isL3Only bool) (int, error) {
 
 	key := p.Port + "/" + string(proto)
 	existingFilter, ok := resMap[key]
 	if !ok {
-		resMap[key] = CreateL4IngressFilter(endpoints, endpointsWithL3Override, r, p, proto, ruleLabels)
+		resMap[key] = CreateL4IngressFilter(endpoints, endpointsWithL3Override, r, p, proto, ruleLabels, isL3Only)
 		return 1, nil
 	}
 
 	// Create a new L4Filter based off of the arguments provided to this function
 	// for merging with the filter which is already in the policy map.
-	filterToMerge := CreateL4IngressFilter(endpoints, endpointsWithL3Override, r, p, proto, ruleLabels)
+	filterToMerge := CreateL4IngressFilter(endpoints, endpointsWithL3Override, r, p, proto, ruleLabels, isL3Only)
 
 	if err := mergeL4Port(ctx, endpoints, &existingFilter, &filterToMerge); err != nil {
 		return 0, err
 	}
 	existingFilter.DerivedFromRules = append(existingFilter.DerivedFromRules, ruleLabels)
 	resMap[key] = existingFilter
+
 	return 1, nil
 }
 
 func mergeL4Ingress(ctx *SearchContext, rule api.IngressRule, ruleLabels labels.LabelArray, resMap L4PolicyMap) (int, error) {
-	if len(rule.ToPorts) == 0 {
-		ctx.PolicyTrace("    No L4 %s rules\n", trafficdirection.Ingress)
-		return 0, nil
-	}
 
 	fromEndpoints := rule.GetSourceEndpointSelectors()
 	found := 0
@@ -214,6 +211,21 @@ func mergeL4Ingress(ctx *SearchContext, rule api.IngressRule, ruleLabels labels.
 		endpointsWithL3Override = append(endpointsWithL3Override, api.ReservedEndpointSelectors[labels.IDNameHost])
 	}
 
+	var (
+		cnt int
+		err error
+	)
+
+	// L3-only rule without requirements.
+	if len(rule.ToPorts) == 0 && len(rule.FromRequires) == 0 {
+		cnt, err = mergeL4IngressPort(ctx, fromEndpoints, endpointsWithL3Override, api.PortRule{}, api.PortProtocol{Port: "0", Protocol: api.ProtoAny}, api.ProtoAny, ruleLabels, resMap, true)
+		if err != nil {
+			return found, err
+		}
+	}
+
+	found += cnt
+
 	for _, r := range rule.ToPorts {
 		ctx.PolicyTrace("    Allows %s port %v from endpoints %v\n", trafficdirection.Ingress, r.Ports, fromEndpoints)
 		if r.Rules != nil && r.Rules.L7Proto != "" {
@@ -233,19 +245,19 @@ func mergeL4Ingress(ctx *SearchContext, rule api.IngressRule, ruleLabels labels.
 
 		for _, p := range r.Ports {
 			if p.Protocol != api.ProtoAny {
-				cnt, err := mergeL4IngressPort(ctx, fromEndpoints, endpointsWithL3Override, r, p, p.Protocol, ruleLabels, resMap)
+				cnt, err := mergeL4IngressPort(ctx, fromEndpoints, endpointsWithL3Override, r, p, p.Protocol, ruleLabels, resMap, false)
 				if err != nil {
 					return found, err
 				}
 				found += cnt
 			} else {
-				cnt, err := mergeL4IngressPort(ctx, fromEndpoints, endpointsWithL3Override, r, p, api.ProtoTCP, ruleLabels, resMap)
+				cnt, err := mergeL4IngressPort(ctx, fromEndpoints, endpointsWithL3Override, r, p, api.ProtoTCP, ruleLabels, resMap, false)
 				if err != nil {
 					return found, err
 				}
 				found += cnt
 
-				cnt, err = mergeL4IngressPort(ctx, fromEndpoints, endpointsWithL3Override, r, p, api.ProtoUDP, ruleLabels, resMap)
+				cnt, err = mergeL4IngressPort(ctx, fromEndpoints, endpointsWithL3Override, r, p, api.ProtoUDP, ruleLabels, resMap, false)
 				if err != nil {
 					return found, err
 				}
@@ -301,7 +313,6 @@ func (r *rule) resolveL4IngressPolicy(ctx *SearchContext, state *traceState, res
 			}
 			ruleCopy.SetAggregatedSelectors()
 		}
-
 		cnt, err := mergeL4Ingress(ctx, ruleCopy, r.Rule.Labels.DeepCopy(), result.Ingress)
 		if err != nil {
 			return nil, err
@@ -529,13 +540,24 @@ func (r *rule) canReachEgress(ctx *SearchContext, state *traceState) api.Decisio
 }
 
 func mergeL4Egress(ctx *SearchContext, rule api.EgressRule, ruleLabels labels.LabelArray, resMap L4PolicyMap) (int, error) {
-	if len(rule.ToPorts) == 0 {
-		ctx.PolicyTrace("    No L4 %s rules\n", trafficdirection.Egress)
-		return 0, nil
-	}
 
 	toEndpoints := rule.GetDestinationEndpointSelectors()
 	found := 0
+
+	var (
+		cnt int
+		err error
+	)
+
+	// L3-only rule
+	if len(rule.ToPorts) == 0 && len(rule.ToRequires) == 0 {
+		cnt, err = mergeL4EgressPort(ctx, toEndpoints, api.PortRule{}, api.PortProtocol{Port: "0", Protocol: api.ProtoAny}, api.ProtoAny, ruleLabels, resMap, true)
+		if err != nil {
+			return found, err
+		}
+	}
+
+	found += cnt
 
 	for _, r := range rule.ToPorts {
 		ctx.PolicyTrace("    Allows %s port %v to endpoints %v\n", trafficdirection.Egress, r.Ports, toEndpoints)
@@ -556,19 +578,19 @@ func mergeL4Egress(ctx *SearchContext, rule api.EgressRule, ruleLabels labels.La
 
 		for _, p := range r.Ports {
 			if p.Protocol != api.ProtoAny {
-				cnt, err := mergeL4EgressPort(ctx, toEndpoints, r, p, p.Protocol, ruleLabels, resMap)
+				cnt, err := mergeL4EgressPort(ctx, toEndpoints, r, p, p.Protocol, ruleLabels, resMap, false)
 				if err != nil {
 					return found, err
 				}
 				found += cnt
 			} else {
-				cnt, err := mergeL4EgressPort(ctx, toEndpoints, r, p, api.ProtoTCP, ruleLabels, resMap)
+				cnt, err := mergeL4EgressPort(ctx, toEndpoints, r, p, api.ProtoTCP, ruleLabels, resMap, false)
 				if err != nil {
 					return found, err
 				}
 				found += cnt
 
-				cnt, err = mergeL4EgressPort(ctx, toEndpoints, r, p, api.ProtoUDP, ruleLabels, resMap)
+				cnt, err = mergeL4EgressPort(ctx, toEndpoints, r, p, api.ProtoUDP, ruleLabels, resMap, false)
 				if err != nil {
 					return found, err
 				}
@@ -586,18 +608,18 @@ func mergeL4Egress(ctx *SearchContext, rule api.EgressRule, ruleLabels labels.La
 // being merged has conflicting L7 rules with those already in the provided
 // L4PolicyMap for the specified port-protocol tuple, it returns an error.
 func mergeL4EgressPort(ctx *SearchContext, endpoints []api.EndpointSelector, r api.PortRule, p api.PortProtocol,
-	proto api.L4Proto, ruleLabels labels.LabelArray, resMap L4PolicyMap) (int, error) {
+	proto api.L4Proto, ruleLabels labels.LabelArray, resMap L4PolicyMap, isL3Only bool) (int, error) {
 
 	key := p.Port + "/" + string(proto)
 	existingFilter, ok := resMap[key]
 	if !ok {
-		resMap[key] = CreateL4EgressFilter(endpoints, r, p, proto, ruleLabels)
+		resMap[key] = CreateL4EgressFilter(endpoints, r, p, proto, ruleLabels, isL3Only)
 		return 1, nil
 	}
 
 	// Create a new L4Filter based off of the arguments provided to this function
 	// for merging with the filter which is already in the policy map.
-	filterToMerge := CreateL4EgressFilter(endpoints, r, p, proto, ruleLabels)
+	filterToMerge := CreateL4EgressFilter(endpoints, r, p, proto, ruleLabels, isL3Only)
 
 	if err := mergeL4Port(ctx, endpoints, &existingFilter, &filterToMerge); err != nil {
 		return 0, err

pkg/policy/repository_test.go
@@ -786,6 +786,15 @@ func (ds *PolicyTestSuite) TestWildcardL3RulesIngress(c *C) {
 	c.Assert(err, IsNil)
 
 	expectedPolicy := L4PolicyMap{
+		"0/ANY": {
+			Port:             0,
+			Protocol:         api.ProtoAny,
+			U8Proto:          0x0,
+			Endpoints:        []api.EndpointSelector{selBar1},
+			L7RulesPerEp:     L7DataMap{},
+			Ingress:          true,
+			DerivedFromRules: labels.LabelArrayList{labelsL3},
+		},
 		"9092/TCP": {
 			Port:      9092,
 			Protocol:  api.ProtoTCP,
@@ -1223,6 +1232,17 @@ func (ds *PolicyTestSuite) TestWildcardL3RulesEgress(c *C) {
 			},
 			DerivedFromRules: labels.LabelArrayList{labelsHTTP, labelsL4},
 		},
+		"0/ANY": {
+			Port:             0,
+			Protocol:         "ANY",
+			U8Proto:          0x0,
+			allowsAllAtL3:    false,
+			Endpoints:        api.EndpointSelectorSlice{selBar1},
+			L7Parser:         "",
+			L7RulesPerEp:     L7DataMap{},
+			Ingress:          false,
+			DerivedFromRules: labels.LabelArrayList{labelsL4},
+		},
 	}
 	c.Assert((*policy), checker.DeepEquals, expectedPolicy)
 }
@@ -1445,12 +1465,23 @@ func (ds *PolicyTestSuite) TestWildcardL3RulesIngressFromEntities(c *C) {
 
 	policy, err := repo.ResolveL4IngressPolicy(ctx)
 	c.Assert(err, IsNil)
-	c.Assert(len(*policy), Equals, 2)
+	c.Assert(len(*policy), Equals, 3)
 	c.Assert(len((*policy)["80/TCP"].Endpoints), Equals, 2)
 	selWorld := (*policy)["80/TCP"].Endpoints[1]
 	c.Assert(api.EndpointSelectorSlice{selWorld}, checker.DeepEquals, api.EntitySelectorMapping[api.EntityWorld])
 
 	expectedPolicy := L4PolicyMap{
+		"0/ANY": {
+			Port:             0,
+			Protocol:         "ANY",
+			U8Proto:          0x0,
+			allowsAllAtL3:    false,
+			Endpoints:        []api.EndpointSelector{selWorld},
+			L7Parser:         "",
+			L7RulesPerEp:     L7DataMap{},
+			Ingress:          true,
+			DerivedFromRules: labels.LabelArrayList{labelsL3},
+		},
 		"9092/TCP": {
 			Port:      9092,
 			Protocol:  api.ProtoTCP,
@@ -1567,12 +1598,23 @@ func (ds *PolicyTestSuite) TestWildcardL3RulesEgressToEntities(c *C) {
 
 	policy, err := repo.ResolveL4EgressPolicy(ctx)
 	c.Assert(err, IsNil)
-	c.Assert(len(*policy), Equals, 2)
+	c.Assert(len(*policy), Equals, 3)
 	c.Assert(len((*policy)["80/TCP"].Endpoints), Equals, 2)
 	selWorld := (*policy)["80/TCP"].Endpoints[1]
 	c.Assert(api.EndpointSelectorSlice{selWorld}, checker.DeepEquals, api.EntitySelectorMapping[api.EntityWorld])
 
 	expectedPolicy := L4PolicyMap{
+		"0/ANY": {
+			Port:             0,
+			Protocol:         "ANY",
+			U8Proto:          0x0,
+			allowsAllAtL3:    false,
+			Endpoints:        api.EndpointSelectorSlice{selWorld},
+			L7Parser:         "",
+			L7RulesPerEp:     L7DataMap{},
+			Ingress:          false,
+			DerivedFromRules: labels.LabelArrayList{labelsL3},
+		},
 		"9092/TCP": {
 			Port:      9092,
 			Protocol:  api.ProtoTCP,
@@ -1879,7 +1921,7 @@ Label verdict: undecided
 
 Resolving ingress port policy for [any:bar]
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    No L4 Ingress rules
+    Labels [any:baz] not found
 * Rule {"matchLabels":{"any:bar":""}}: selected
     Found all required labels
     Allows Ingress port [{80 ANY}] from endpoints [{"matchLabels":{"reserved:host":""}} {"matchLabels":{"any:baz":""}}]
@@ -1908,7 +1950,7 @@ Label verdict: undecided
 
 Resolving ingress port policy for [any:bar]
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    No L4 Ingress rules
+    Labels [any:bar] not found
 * Rule {"matchLabels":{"any:bar":""}}: selected
     Labels [any:bar] not found
 2/2 rules selected

pkg/policy/api/selector.go
@@ -362,11 +362,17 @@ func (s EndpointSelectorSlice) Matches(ctx labels.LabelArray) bool {
 }
 
 // SelectsAllEndpoints returns whether the EndpointSelectorSlice selects all
-// endpoints (if it contains the WildcardEndpointSelector, or if it is empty).
-func (s EndpointSelectorSlice) SelectsAllEndpoints() bool {
+// endpoints. Depending on what "layer" of policy is being computed for this
+// EndpointSelectorSlice (L3, or L4), an empty slice represents different
+// behavior: at L3, an empty slice does not allow traffic, while at L4, it does.
+// If a slice contains the WildcardEndpointSelector, it of course selects all
+// endpoints.
+func (s EndpointSelectorSlice) SelectsAllEndpoints(isL3Only bool) bool {
 
 	if len(s) == 0 {
-		return true
+		if !isL3Only {
+			return true
+		}
 	}
 
 	for _, selector := range s {

pkg/policy/api/selector_test.go
@@ -35,17 +35,17 @@ func (s *PolicyAPITestSuite) TestSelectsAllEndpoints(c *C) {
 
 	// Empty endpoint selector slice equates to a wildcard.
 	selectorSlice := EndpointSelectorSlice{}
-	c.Assert(selectorSlice.SelectsAllEndpoints(), Equals, true)
+	c.Assert(selectorSlice.SelectsAllEndpoints(false), Equals, true)
 
 	selectorSlice = EndpointSelectorSlice{WildcardEndpointSelector}
-	c.Assert(selectorSlice.SelectsAllEndpoints(), Equals, true)
+	c.Assert(selectorSlice.SelectsAllEndpoints(false), Equals, true)
 
 	// Slice that contains wildcard and other selectors still selects all endpoints.
 	selectorSlice = EndpointSelectorSlice{WildcardEndpointSelector, NewESFromLabels(labels.ParseSelectLabel("bar"))}
-	c.Assert(selectorSlice.SelectsAllEndpoints(), Equals, true)
+	c.Assert(selectorSlice.SelectsAllEndpoints(false), Equals, true)
 
 	selectorSlice = EndpointSelectorSlice{NewESFromLabels(labels.ParseSelectLabel("bar")), NewESFromLabels(labels.ParseSelectLabel("foo"))}
-	c.Assert(selectorSlice.SelectsAllEndpoints(), Equals, false)
+	c.Assert(selectorSlice.SelectsAllEndpoints(false), Equals, false)
 }
 
 func (s *PolicyAPITestSuite) TestLabelSelectorToRequirements(c *C) {

pkg/policy/distillery_test.go
@@ -39,6 +39,12 @@ var (
 	selectFoo_  = api.NewESFromLabels(labels.ParseSelectLabel("foo"))
 	allowFooL3_ = selectFoo_
 
+	// Identity, labels, selectors for an endpoint named "bar"
+	identityBar = uint32(200)
+	labelsBar   = labels.ParseSelectLabelArray("bar", "blue")
+	selectBar_  = api.NewESFromLabels(labels.ParseSelectLabel("bar"))
+	allowBarL3_ = selectBar_
+
 	// API rule sections for composability
 	// L4 rule sections
 	allowAllL4_ []api.PortRule
@@ -82,12 +88,23 @@ var (
 			ToPorts: combineL4L7(allowPort80, allowHTTPRoot),
 		}})
 
+	rule__L3AllowFoo = api.NewRule().
+				WithIngressRules([]api.IngressRule{{
+			FromEndpoints: []api.EndpointSelector{allowFooL3_},
+		}})
+
+	rule__L3AllowBar = api.NewRule().
+				WithIngressRules([]api.IngressRule{{
+			FromEndpoints: []api.EndpointSelector{allowBarL3_},
+		}})
+
 	// Misc other bpf key fields for convenience / readability.
 	l7RedirectNone_ = uint16(0)
 	l7RedirectProxy = uint16(1)
 	dirIngress      = trafficdirection.Ingress.Uint8()
 	// Desired map keys for L3, L3-dependent L4, L4
 	mapKeyAllowFoo__ = Key{identityFoo, 0, 0, dirIngress}
+	mapKeyAllowBar__ = Key{identityBar, 0, 0, dirIngress}
 	mapKeyAllowFooL4 = Key{identityFoo, 80, 6, dirIngress}
 	mapKeyAllow___L4 = Key{0, 80, 6, dirIngress}
 	// Desired map entries for no L7 redirect / redirect to Proxy
@@ -186,7 +203,7 @@ func (d *policyDistillery) distillPolicy(epLabels labels.LabelArray) (MapState, 
 	// Handle L4 ingress from each identity in the cache to the endpoint.
 	io.WriteString(d.log, "[distill] Producing L4 filter keys\n")
 	for _, l4 := range *l4IngressPolicy {
-		io.WriteString(d.log, fmt.Sprintf("[distill] Processing L4Filter (l7: %+v)\n", l4.L7RulesPerEp))
+		io.WriteString(d.log, fmt.Sprintf("[distill] Processing L4Filter (l3: %+v), (l4: %d/%s), (l7: %+v)\n", l4.Endpoints, l4.Port, l4.Protocol, l4.L7RulesPerEp))
 		for _, key := range l4.ToKeys(0, d.identityCache, deniedPeers) {
 			io.WriteString(d.log, fmt.Sprintf("[distill] L4 ingress allow %+v (parser=%s, redirect=%t)\n", key, l4.L7Parser, l4.IsRedirect()))
 			if l4.IsRedirect() {
@@ -232,6 +249,45 @@ func (d *policyDistillery) distillPolicy(epLabels labels.LabelArray) (MapState, 
 	}
 
 	return result, nil
+}
+
+func Test_MergeL3(t *testing.T) {
+	identityCache := cache.IdentityCache{
+		identity.NumericIdentity(identityFoo): labelsFoo,
+		identity.NumericIdentity(identityBar): labelsBar,
+	}
+
+	tests := []struct {
+		test   int
+		rules  api.Rules
+		result MapState
+	}{
+		{0, api.Rules{rule__L3AllowFoo, rule__L3AllowBar}, MapState{mapKeyAllowFoo__: mapEntryL7None_, mapKeyAllowBar__: mapEntryL7None_}},
+		{1, api.Rules{rule__L3AllowFoo, ruleL3L4__Allow}, MapState{mapKeyAllowFoo__: mapEntryL7None_, mapKeyAllowFooL4: mapEntryL7None_}},
+	}
+
+	for _, tt := range tests {
+		repo := newPolicyDistillery(identityCache)
+		for _, r := range tt.rules {
+			if r != nil {
+				rule := r.WithEndpointSelector(selectFoo_)
+				_, _ = repo.AddList(api.Rules{rule})
+			}
+		}
+		t.Run(fmt.Sprintf("permutation_%d", tt.test), func(t *testing.T) {
+			logBuffer := new(bytes.Buffer)
+			repo = repo.WithLogBuffer(logBuffer)
+			mapstate, err := repo.distillPolicy(labelsFoo)
+			if err != nil {
+				t.Errorf("Policy resolution failure: %s", err)
+			}
+			if equal, err := checker.DeepEqual(mapstate, tt.result); !equal {
+				t.Logf("Rules:\n%s\n\n", tt.rules.String())
+				t.Logf("Policy Trace: \n%s\n", logBuffer.String())
+				t.Errorf("Policy obtained didn't match expected for endpoint %s:\n%s", labelsFoo, err)
+			}
+		})
+	}
 }
 
 func Test_MergeRules(t *testing.T) {

pkg/policy/l4.go
@@ -174,6 +174,7 @@ func (l4 *L4Filter) ToKeys(direction trafficdirection.TrafficDirection, identity
 			}
 		}
 	}
+
 	return keysToAdd
 }
 
@@ -226,7 +227,7 @@ func (l7 L7DataMap) addRulesForEndpoints(rules api.L7Rules, endpoints []api.Endp
 // filter is derived from. This filter may be associated with a series of L7
 // rules via the `rule` parameter.
 func CreateL4Filter(peerEndpoints api.EndpointSelectorSlice, rule api.PortRule, port api.PortProtocol,
-	protocol api.L4Proto, ruleLabels labels.LabelArray, ingress bool) L4Filter {
+	protocol api.L4Proto, ruleLabels labels.LabelArray, ingress bool, l3Only bool) L4Filter {
 
 	// already validated via PortRule.Validate()
 	p, _ := strconv.ParseUint(port.Port, 0, 16)
@@ -235,7 +236,7 @@ func CreateL4Filter(peerEndpoints api.EndpointSelectorSlice, rule api.PortRule, 
 
 	filterEndpoints := peerEndpoints
 	allowsAllL3 := false
-	if peerEndpoints.SelectsAllEndpoints() {
+	if peerEndpoints.SelectsAllEndpoints(l3Only) {
 		filterEndpoints = api.EndpointSelectorSlice{api.WildcardEndpointSelector}
 		allowsAllL3 = true
 	}
@@ -282,9 +283,9 @@ func CreateL4Filter(peerEndpoints api.EndpointSelectorSlice, rule api.PortRule, 
 // endpointsWithL3Override determines selectors for which L7 rules should be
 // wildcarded (eg, host / world in the relevant daemon modes).
 func CreateL4IngressFilter(fromEndpoints api.EndpointSelectorSlice, endpointsWithL3Override []api.EndpointSelector, rule api.PortRule, port api.PortProtocol,
-	protocol api.L4Proto, ruleLabels labels.LabelArray) L4Filter {
+	protocol api.L4Proto, ruleLabels labels.LabelArray, isL3Only bool) L4Filter {
 
-	filter := CreateL4Filter(fromEndpoints, rule, port, protocol, ruleLabels, true)
+	filter := CreateL4Filter(fromEndpoints, rule, port, protocol, ruleLabels, true, isL3Only)
 
 	// If the filter would apply L7 rules for endpointsWithL3Override,
 	// then wildcard those specific endpoints at L7.
@@ -302,9 +303,9 @@ func CreateL4IngressFilter(fromEndpoints api.EndpointSelectorSlice, endpointsWit
 // to the original rules that the filter is derived from. This filter may be
 // associated with a series of L7 rules via the `rule` parameter.
 func CreateL4EgressFilter(toEndpoints api.EndpointSelectorSlice, rule api.PortRule, port api.PortProtocol,
-	protocol api.L4Proto, ruleLabels labels.LabelArray) L4Filter {
+	protocol api.L4Proto, ruleLabels labels.LabelArray, isL3Only bool) L4Filter {
 
-	return CreateL4Filter(toEndpoints, rule, port, protocol, ruleLabels, false)
+	return CreateL4Filter(toEndpoints, rule, port, protocol, ruleLabels, false, isL3Only)
 }
 
 // IsRedirect returns true if the L4 filter contains a port redirection

pkg/policy/l4_filter_test.go
@@ -106,7 +106,7 @@ func (ds *PolicyTestSuite) TestMergeAllowAllL3AndAllowAllL7(c *C) {
 	c.Assert(filter.Port, Equals, 80)
 	c.Assert(filter.Ingress, Equals, true)
 
-	c.Assert(filter.Endpoints.SelectsAllEndpoints(), Equals, true)
+	c.Assert(filter.Endpoints.SelectsAllEndpoints(false), Equals, true)
 
 	c.Assert(filter.L7Parser, Equals, ParserTypeNone)
 	c.Assert(len(filter.L7RulesPerEp), Equals, 0)
@@ -148,7 +148,7 @@ func (ds *PolicyTestSuite) TestMergeAllowAllL3AndAllowAllL7(c *C) {
 	c.Assert(filter.Port, Equals, 80)
 	c.Assert(filter.Ingress, Equals, true)
 
-	c.Assert(filter.Endpoints.SelectsAllEndpoints(), Equals, true)
+	c.Assert(filter.Endpoints.SelectsAllEndpoints(false), Equals, true)
 
 	c.Assert(filter.L7Parser, Equals, ParserTypeNone)
 	c.Assert(len(filter.L7RulesPerEp), Equals, 0)
@@ -267,7 +267,7 @@ func (ds *PolicyTestSuite) TestMergeAllowAllL3AndShadowedL7(c *C) {
 	c.Assert(filter.Port, Equals, 80)
 	c.Assert(filter.Ingress, Equals, true)
 
-	c.Assert(filter.Endpoints.SelectsAllEndpoints(), Equals, true)
+	c.Assert(filter.Endpoints.SelectsAllEndpoints(false), Equals, true)
 
 	c.Assert(filter.L7Parser, Equals, ParserTypeHTTP)
 	c.Assert(len(filter.L7RulesPerEp), Equals, 1)

pkg/policy/l4_test.go
@@ -75,12 +75,12 @@ func (s *PolicyTestSuite) TestCreateL4Filter(c *C) {
 	for _, selector := range selectors {
 		eps := []api.EndpointSelector{selector}
 		// Regardless of ingress/egress, we should end up with
-		// a single L7 rule whether the selector is wildcarded
+		// a single L7 rule whether the selector is wildcardeds
 		// or if it is based on specific labels.
-		filter := CreateL4IngressFilter(eps, nil, portrule, tuple, tuple.Protocol, nil)
+		filter := CreateL4IngressFilter(eps, nil, portrule, tuple, tuple.Protocol, nil, false)
 		c.Assert(len(filter.L7RulesPerEp), Equals, 1)
 
-		filter = CreateL4EgressFilter(eps, portrule, tuple, tuple.Protocol, nil)
+		filter = CreateL4EgressFilter(eps, portrule, tuple, tuple.Protocol, nil, false)
 		c.Assert(len(filter.L7RulesPerEp), Equals, 1)
 	}
 }

commit 61238203ebe97fa6d3fbef7ef84aa1a1c9b97b25

policy: Generate L3-only filter also for rules with requirements.
FromRequires can be placed into the same rule as FromEndpoints, and
an L3-only filter needs to be generated also in that case.

Basically, as [From|To]Requires affects all rules selected for a given
identity, the processing of individual rules should not depend on the
(non)precense of [From|To]Requires in the same rule.

Fixes: 7321663ff85 ("policy: store L3-only policy in L4Filter")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/policy/rule.go
@@ -256,8 +256,8 @@ func mergeIngress(ctx *SearchContext, rule api.IngressRule, ruleLabels labels.La
 		err error
 	)
 
-	// L3-only rule without requirements.
-	if len(rule.ToPorts) == 0 && len(rule.FromRequires) == 0 {
+	// L3-only rule (with requirements folded into fromEndpoints).
+	if len(rule.ToPorts) == 0 && len(fromEndpoints) > 0 {
 		cnt, err = mergeIngressPortProto(ctx, fromEndpoints, endpointsWithL3Override, api.PortRule{}, api.PortProtocol{Port: "0", Protocol: api.ProtoAny}, api.ProtoAny, ruleLabels, resMap)
 		if err != nil {
 			return found, err
@@ -356,7 +356,7 @@ func (r *rule) resolveIngressPolicy(ctx *SearchContext, state *traceState, resul
 		// from rules which select the labels in ctx.To. This ensures that
 		// FromRequires is taken into account even if it isn't part of the current
 		// rule over which we are iterating.
-		if len(requirements) > 0 {
+		if len(requirements) > 0 && len(ingressRule.FromEndpoints) > 0 {
 			// Create a deep copy of the rule, as we are going to modify FromEndpoints
 			// with requirementsSelector. We don't want to modify the rule itself
 			// in the policy repository.
@@ -635,8 +635,8 @@ func mergeEgress(ctx *SearchContext, rule api.EgressRule, ruleLabels labels.Labe
 		err error
 	)
 
-	// L3-only rule
-	if len(rule.ToPorts) == 0 && len(rule.ToRequires) == 0 {
+	// L3-only rule (with requirements folded into toEndpoints).
+	if len(rule.ToPorts) == 0 && len(toEndpoints) > 0 {
 		cnt, err = mergeEgressPortProto(ctx, toEndpoints, api.PortRule{}, api.PortProtocol{Port: "0", Protocol: api.ProtoAny}, api.ProtoAny, ruleLabels, resMap)
 		if err != nil {
 			return found, err
@@ -742,7 +742,7 @@ func (r *rule) resolveEgressPolicy(ctx *SearchContext, state *traceState, result
 		// from rules which select the labels in ctx.From. This ensures that
 		// ToRequires is taken into account even if it isn't part of the current
 		// rule over which we are iterating.
-		if len(requirements) > 0 {
+		if len(requirements) > 0 && len(egressRule.ToEndpoints) > 0 {
 			// Create a deep copy of the rule, as we are going to modify
 			// ToEndpoints with requirements; we don't want to modify the rule
 			// in the repository.

pkg/policy/repository_test.go
@@ -1022,6 +1022,9 @@ func (ds *PolicyTestSuite) TestL3DependentL4EgressFromRequires(c *C) {
 				}},
 			},
 			{
+				ToEndpoints: []api.EndpointSelector{
+					api.WildcardEndpointSelector,
+				},
 				ToRequires: []api.EndpointSelector{selBar2},
 			},
 		},
@@ -1048,8 +1051,26 @@ func (ds *PolicyTestSuite) TestL3DependentL4EgressFromRequires(c *C) {
 			Values:   []string{"bar2"},
 		},
 	})
+	expectedSelector2 := api.NewESFromMatchRequirements(map[string]string{}, []v1.LabelSelectorRequirement{
+		{
+			Key:      "any.id",
+			Operator: v1.LabelSelectorOpIn,
+			Values:   []string{"bar2"},
+		},
+	})
 
 	expectedPolicy := L4PolicyMap{
+		"0/ANY": L4Filter{
+			Port:          0,
+			Protocol:      "ANY",
+			U8Proto:       0x0,
+			allowsAllAtL3: false,
+			Endpoints: api.EndpointSelectorSlice{
+				expectedSelector2,
+			},
+			L7RulesPerEp:     L7DataMap{},
+			DerivedFromRules: labels.LabelArrayList{nil},
+		},
 		"80/TCP": L4Filter{
 			Port:     80,
 			Protocol: api.ProtoTCP,
@@ -1902,8 +1923,7 @@ Resolving ingress policy for [any:bar]
     Allows from labels {"matchLabels":{"any:baz":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
       No label match for [any:foo]
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
-      No label match for [any:foo]
+      Found all required labels
 3/3 rules selected
 Found no allow rule
 Ingress verdict: denied
@@ -1927,7 +1947,6 @@ Resolving ingress policy for [any:bar]
       Allows port [{80 ANY}]
         No port match found
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
       Found all required labels
 3/3 rules selected
 Found no allow rule