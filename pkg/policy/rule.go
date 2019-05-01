cilium/cilium

commit b4f41cbe7a99c3412cf705209ecba14c798de1bc

policy: Rework ingress policy trace to L4PolicyMap
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/policy/rule.go
@@ -15,6 +15,7 @@
 package policy
 
 import (
+	"bytes"
 	"fmt"
 
 	"github.com/cilium/cilium/pkg/identity"
@@ -179,19 +180,67 @@ func mergeIngressPortProto(ctx *SearchContext, endpoints []api.EndpointSelector,
 	return 1, nil
 }
 
+func traceL3(ctx *SearchContext, peerEndpoints api.EndpointSelectorSlice, direction string) {
+	var result bytes.Buffer
+
+	// Requirements will be cloned into every selector, only trace them once.
+	if len(peerEndpoints[0].MatchExpressions) > 0 {
+		sel := peerEndpoints[0]
+		result.WriteString("    Enforcing requirements ")
+		result.WriteString(fmt.Sprintf("%+v", sel.MatchExpressions))
+		result.WriteString("\n")
+	}
+	// EndpointSelector
+	for _, sel := range peerEndpoints {
+		if len(sel.MatchLabels) > 0 {
+			result.WriteString("    Allows ")
+			result.WriteString(direction)
+			result.WriteString(" labels ")
+			result.WriteString(sel.String())
+			result.WriteString("\n")
+		}
+	}
+	ctx.PolicyTrace(result.String())
+}
+
+// portRulesCoverContext determines whether L4 portions of rules cover the
+// specified port models.
+//
+// Returns true if the list of ports is 0, or the rules match the ports.
+func rulePortsCoverSearchContext(ports []api.PortProtocol, ctx *SearchContext) bool {
+	if len(ctx.DPorts) == 0 {
+		return true
+	}
+	for _, p := range ports {
+		for _, dp := range ctx.DPorts {
+			tracePort := api.PortProtocol{
+				Port:     fmt.Sprintf("%d", dp.Port),
+				Protocol: api.L4Proto(dp.Protocol),
+			}
+			if p.Covers(tracePort) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func mergeIngress(ctx *SearchContext, rule api.IngressRule, ruleLabels labels.LabelArray, resMap L4PolicyMap) (int, error) {
 
 	fromEndpoints := rule.GetSourceEndpointSelectors()
 	found := 0
 
 	if ctx.From != nil && len(fromEndpoints) > 0 {
+		if ctx.TraceEnabled() {
+			traceL3(ctx, fromEndpoints, "from")
+		}
 		if !fromEndpoints.Matches(ctx.From) {
-			ctx.PolicyTrace("    Labels %s not found", ctx.From)
+			ctx.PolicyTrace("      No label match for %s", ctx.From)
 			return 0, nil
 		}
 	}
 
-	ctx.PolicyTrace("    Found all required labels")
+	ctx.PolicyTrace("      Found all required labels")
 
 	// Daemon options may induce L3 allows for host/world. In this case, if
 	// we find any L7 rules matching host/world then we need to turn any L7
@@ -225,19 +274,23 @@ func mergeIngress(ctx *SearchContext, rule api.IngressRule, ruleLabels labels.La
 		if len(fromEndpoints) == 0 {
 			fromEndpoints = api.EndpointSelectorSlice{api.WildcardEndpointSelector}
 		}
-		ctx.PolicyTrace("    Allows %s port %v from endpoints %v\n", trafficdirection.Ingress, r.Ports, fromEndpoints)
+		ctx.PolicyTrace("      Allows port %v\n", r.Ports)
+		if !rulePortsCoverSearchContext(r.Ports, ctx) {
+			ctx.PolicyTrace("        No port match found\n")
+			continue
+		}
 		if r.Rules != nil && r.Rules.L7Proto != "" {
-			ctx.PolicyTrace("      l7proto: \"%s\"\n", r.Rules.L7Proto)
+			ctx.PolicyTrace("        l7proto: \"%s\"\n", r.Rules.L7Proto)
 		}
 		if !r.Rules.IsEmpty() {
 			for _, l7 := range r.Rules.HTTP {
-				ctx.PolicyTrace("        %+v\n", l7)
+				ctx.PolicyTrace("          %+v\n", l7)
 			}
 			for _, l7 := range r.Rules.Kafka {
-				ctx.PolicyTrace("        %+v\n", l7)
+				ctx.PolicyTrace("          %+v\n", l7)
 			}
 			for _, l7 := range r.Rules.L7 {
-				ctx.PolicyTrace("        %+v\n", l7)
+				ctx.PolicyTrace("          %+v\n", l7)
 			}
 		}
 
@@ -294,7 +347,7 @@ func (r *rule) resolveIngressPolicy(ctx *SearchContext, state *traceState, resul
 	found := 0
 
 	if len(r.Ingress) == 0 {
-		ctx.PolicyTrace("    No L4 ingress rules\n")
+		ctx.PolicyTrace("    No ingress rules\n")
 	}
 	for _, ingressRule := range r.Ingress {
 		ruleCopy := ingressRule
@@ -309,6 +362,12 @@ func (r *rule) resolveIngressPolicy(ctx *SearchContext, state *traceState, resul
 			// with requirementsSelector. We don't want to modify the rule itself
 			// in the policy repository.
 			ruleCopy = *ingressRule.DeepCopy()
+			// If the rule only consists of a FromRequires statement,
+			// provide an empty selector so that it will be updated
+			// in the next step.
+			if ctx.TraceEnabled() && len(ruleCopy.FromEndpoints) == 0 {
+				ruleCopy.FromEndpoints = []api.EndpointSelector{api.NewESFromLabels()}
+			}
 			// Update each EndpointSelector in FromEndpoints to contain requirements.
 			for idx := range ruleCopy.FromEndpoints {
 				ruleCopy.FromEndpoints[idx].MatchExpressions = append(ruleCopy.FromEndpoints[idx].MatchExpressions, requirements...)
@@ -434,7 +493,10 @@ func (r *rule) canReachFromEndpoints(ctx *SearchContext, state *traceState) api.
 	// assume requirements have already been analyzed.
 	for _, r := range r.Ingress {
 		for _, sel := range r.GetSourceEndpointSelectors() {
-			ctx.PolicyTrace("    Allows from labels %+v", sel)
+			if len(sel.MatchExpressions) > 0 {
+				ctx.PolicyTrace("    Requires %+v", sel.MatchExpressions)
+			}
+			ctx.PolicyTrace("    Allows from labels %+v", sel.MatchLabels)
 			if sel.Matches(ctx.From) {
 				ctx.PolicyTrace("      Found all required labels")
 				if len(r.ToPorts) == 0 {

pkg/policy/repository_test.go
@@ -436,7 +436,7 @@ func (ds *PolicyTestSuite) TestContainsAllRLocked(c *C) {
 	c.Assert(repoEmpty.ContainsAllRLocked(a), Equals, false)    // a is NOT in empty
 }
 
-func (ds *PolicyTestSuite) TestCanReachIngress(c *C) {
+func (ds *PolicyTestSuite) TestAllowsIngress(c *C) {
 	repo := NewPolicyRepository()
 
 	fooToBar := &SearchContext{
@@ -445,8 +445,6 @@ func (ds *PolicyTestSuite) TestCanReachIngress(c *C) {
 	}
 
 	repo.Mutex.RLock()
-	// no rules loaded: CanReach => undecided
-	c.Assert(repo.CanReachIngressRLocked(fooToBar), Equals, api.Undecided)
 	// no rules loaded: Allows() => denied
 	c.Assert(repo.AllowsIngressRLocked(fooToBar), Equals, api.Denied)
 	repo.Mutex.RUnlock()
@@ -1615,13 +1613,9 @@ func (ds *PolicyTestSuite) TestMinikubeGettingStarted(c *C) {
 	}
 
 	repo.Mutex.RLock()
-	// no rules loaded: CanReach => undecided
-	c.Assert(repo.CanReachIngressRLocked(fromApp2), Equals, api.Undecided)
-	c.Assert(repo.CanReachIngressRLocked(fromApp3), Equals, api.Undecided)
-
 	// no rules loaded: Allows() => denied
-	c.Assert(repo.AllowsIngressLabelAccess(fromApp2), Equals, api.Denied)
-	c.Assert(repo.AllowsIngressLabelAccess(fromApp3), Equals, api.Denied)
+	c.Assert(repo.AllowsIngressRLocked(fromApp2), Equals, api.Denied)
+	c.Assert(repo.AllowsIngressRLocked(fromApp3), Equals, api.Denied)
 	repo.Mutex.RUnlock()
 
 	selFromApp2 := api.NewESFromLabels(
@@ -1744,10 +1738,7 @@ func (ds *PolicyTestSuite) TestMinikubeGettingStarted(c *C) {
 }
 
 func buildSearchCtx(from, to string, port uint16) *SearchContext {
-	var ports []*models.Port
-	if port != 0 {
-		ports = []*models.Port{{Port: port}}
-	}
+	ports := []*models.Port{{Port: port, Protocol: string(api.ProtoAny)}}
 	return &SearchContext{
 		From:   labels.ParseSelectLabelArray(from),
 		To:     labels.ParseSelectLabelArray(to),
@@ -1790,10 +1781,10 @@ func (repo *Repository) checkTrace(c *C, ctx *SearchContext, trace string,
 	repo.Mutex.RLock()
 	verdict := repo.AllowsIngressRLocked(ctx)
 	repo.Mutex.RUnlock()
-	c.Assert(verdict, Equals, expectedVerdict)
 
-	expectedOut := "Tracing " + ctx.String() + trace
+	expectedOut := "Tracing " + ctx.String() + "\n" + trace
 	c.Assert(buffer.String(), checker.DeepEquals, expectedOut)
+	c.Assert(verdict, Equals, expectedVerdict)
 }
 
 func (ds *PolicyTestSuite) TestPolicyTrace(c *C) {
@@ -1806,16 +1797,14 @@ func (ds *PolicyTestSuite) TestPolicyTrace(c *C) {
 
 	// foo=>bar is OK
 	expectedOut := `
+Resolving ingress policy for [any:bar]
 * Rule {"matchLabels":{"any:bar":""}}: selected
     Allows from labels {"matchLabels":{"reserved:host":""}}
-      Labels [any:foo] not found
     Allows from labels {"matchLabels":{"any:foo":""}}
       Found all required labels
-+       No L4 restrictions
 1/1 rules selected
 Found allow rule
-Label verdict: allowed
-L4 ingress policies skipped
+Ingress verdict: allowed
 `
 	ctx := buildSearchCtx("foo", "bar", 0)
 	repo.checkTrace(c, ctx, expectedOut, api.Allowed)
@@ -1827,20 +1816,15 @@ L4 ingress policies skipped
 	// bar=>foo is Denied
 	ctx = buildSearchCtx("bar", "foo", 0)
 	expectedOut = `
+Resolving ingress policy for [any:foo]
 0/1 rules selected
 Found no allow rule
-Label verdict: undecided
+Ingress verdict: denied
 `
 	repo.checkTrace(c, ctx, expectedOut, api.Denied)
 
-	// bar=>foo:80 is Denied, also checks L4 policy
+	// bar=>foo:80 is also Denied by the same logic
 	ctx = buildSearchCtx("bar", "foo", 80)
-	expectedOut += `
-Resolving ingress port policy for [any:foo]
-0/1 rules selected
-Found no allow rule
-L4 ingress verdict: undecided
-`
 	repo.checkTrace(c, ctx, expectedOut, api.Denied)
 
 	// Now, add extra rules to allow specifically baz=>bar on port 80
@@ -1851,58 +1835,37 @@ L4 ingress verdict: undecided
 	// baz=>bar:80 is OK
 	ctx = buildSearchCtx("baz", "bar", 80)
 	expectedOut = `
+Resolving ingress policy for [any:bar]
 * Rule {"matchLabels":{"any:bar":""}}: selected
     Allows from labels {"matchLabels":{"reserved:host":""}}
-      Labels [any:baz] not found
     Allows from labels {"matchLabels":{"any:foo":""}}
-      Labels [any:baz] not found
+      No label match for [any:baz]
 * Rule {"matchLabels":{"any:bar":""}}: selected
     Allows from labels {"matchLabels":{"reserved:host":""}}
-      Labels [any:baz] not found
     Allows from labels {"matchLabels":{"any:baz":""}}
       Found all required labels
-        Rule restricts traffic to specific L4 destinations; deferring policy decision to L4 policy stage
-2/2 rules selected
-Found no allow rule
-Label verdict: undecided
-
-Resolving ingress port policy for [any:bar]
-* Rule {"matchLabels":{"any:bar":""}}: selected
-    Labels [any:baz] not found
-* Rule {"matchLabels":{"any:bar":""}}: selected
-    Found all required labels
-    Allows Ingress port [{80 ANY}] from endpoints [{"matchLabels":{"reserved:host":""}} {"matchLabels":{"any:baz":""}}]
+      Allows port [{80 ANY}]
 2/2 rules selected
 Found allow rule
-L4 ingress verdict: allowed
+Ingress verdict: allowed
 `
 	repo.checkTrace(c, ctx, expectedOut, api.Allowed)
 
 	// bar=>bar:80 is Denied
 	ctx = buildSearchCtx("bar", "bar", 80)
 	expectedOut = `
+Resolving ingress policy for [any:bar]
 * Rule {"matchLabels":{"any:bar":""}}: selected
     Allows from labels {"matchLabels":{"reserved:host":""}}
-      Labels [any:bar] not found
     Allows from labels {"matchLabels":{"any:foo":""}}
-      Labels [any:bar] not found
+      No label match for [any:bar]
 * Rule {"matchLabels":{"any:bar":""}}: selected
     Allows from labels {"matchLabels":{"reserved:host":""}}
-      Labels [any:bar] not found
     Allows from labels {"matchLabels":{"any:baz":""}}
-      Labels [any:bar] not found
+      No label match for [any:bar]
 2/2 rules selected
 Found no allow rule
-Label verdict: undecided
-
-Resolving ingress port policy for [any:bar]
-* Rule {"matchLabels":{"any:bar":""}}: selected
-    Labels [any:bar] not found
-* Rule {"matchLabels":{"any:bar":""}}: selected
-    Labels [any:bar] not found
-2/2 rules selected
-Found no allow rule
-L4 ingress verdict: undecided
+Ingress verdict: denied
 `
 	repo.checkTrace(c, ctx, expectedOut, api.Denied)
 
@@ -1921,46 +1884,48 @@ L4 ingress verdict: undecided
 	// foo=>bar is now denied due to the FromRequires
 	ctx = buildSearchCtx("foo", "bar", 0)
 	expectedOut = `
+Resolving ingress policy for [any:bar]
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Allows from labels {"matchLabels":{"reserved:host":""}}
-      Labels [any:foo] not found
-    Allows from labels {"matchLabels":{"any:foo":""}}
-      Found all required labels
-+       No L4 restrictions
+    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
+    Allows from labels {"matchLabels":{"reserved:host":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
+    Allows from labels {"matchLabels":{"any:foo":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
+      No label match for [any:foo]
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Allows from labels {"matchLabels":{"reserved:host":""}}
-      Labels [any:foo] not found
-    Allows from labels {"matchLabels":{"any:baz":""}}
-      Labels [any:foo] not found
+    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
+    Allows from labels {"matchLabels":{"reserved:host":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
+    Allows from labels {"matchLabels":{"any:baz":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
+      No label match for [any:foo]
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Requires from labels {"matchLabels":{"any:baz":""}}
--     Labels [any:foo] not found
+    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
+      No label match for [any:foo]
 3/3 rules selected
-Found unsatisfied FromRequires constraint
-Label verdict: denied
+Found no allow rule
+Ingress verdict: denied
 `
 	repo.checkTrace(c, ctx, expectedOut, api.Denied)
 
 	// baz=>bar is only denied because of the L4 policy
 	ctx = buildSearchCtx("baz", "bar", 0)
 	expectedOut = `
+Resolving ingress policy for [any:bar]
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Allows from labels {"matchLabels":{"reserved:host":""}}
-      Labels [any:baz] not found
-    Allows from labels {"matchLabels":{"any:foo":""}}
-      Labels [any:baz] not found
+    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
+    Allows from labels {"matchLabels":{"reserved:host":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
+    Allows from labels {"matchLabels":{"any:foo":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
+      No label match for [any:baz]
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Allows from labels {"matchLabels":{"reserved:host":""}}
-      Labels [any:baz] not found
-    Allows from labels {"matchLabels":{"any:baz":""}}
+    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
+    Allows from labels {"matchLabels":{"reserved:host":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
+    Allows from labels {"matchLabels":{"any:baz":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
       Found all required labels
-        Rule restricts traffic to specific L4 destinations; deferring policy decision to L4 policy stage
+      Allows port [{80 ANY}]
+        No port match found
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Requires from labels {"matchLabels":{"any:baz":""}}
-+     Found all required labels
+    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
+      Found all required labels
 3/3 rules selected
 Found no allow rule
-Label verdict: undecided
+Ingress verdict: denied
 `
 	repo.checkTrace(c, ctx, expectedOut, api.Denied)
 

pkg/k8s/network_policy_test.go
@@ -190,7 +190,7 @@ func (s *K8sSuite) TestParseNetworkPolicyIngress(c *C) {
 	}
 
 	// ctx.To needs to have all labels from the policy in order to be accepted
-	c.Assert(repo.CanReachIngressRLocked(&ctx), Not(Equals), api.Allowed)
+	c.Assert(repo.AllowsIngressRLocked(&ctx), Not(Equals), api.Allowed)
 
 	ctx = policy.SearchContext{
 		From: labels.LabelArray{
@@ -203,7 +203,7 @@ func (s *K8sSuite) TestParseNetworkPolicyIngress(c *C) {
 		Trace: policy.TRACE_VERBOSE,
 	}
 	// ctx.From also needs to have all labels from the policy in order to be accepted
-	c.Assert(repo.CanReachIngressRLocked(&ctx), Not(Equals), api.Allowed)
+	c.Assert(repo.AllowsIngressRLocked(&ctx), Not(Equals), api.Allowed)
 }
 
 func (s *K8sSuite) TestParseNetworkPolicyNoSelectors(c *C) {
@@ -552,7 +552,7 @@ func (s *K8sSuite) TestParseNetworkPolicyEmptyFrom(c *C) {
 
 	repo := policy.NewPolicyRepository()
 	repo.AddList(rules)
-	c.Assert(repo.CanReachIngressRLocked(&ctx), Equals, api.Allowed)
+	c.Assert(repo.AllowsIngressRLocked(&ctx), Equals, api.Allowed)
 
 	// Empty From rules, all sources should be allowed
 	netPolicy2 := &networkingv1.NetworkPolicy{
@@ -576,7 +576,7 @@ func (s *K8sSuite) TestParseNetworkPolicyEmptyFrom(c *C) {
 	c.Assert(len(rules), Equals, 1)
 	repo = policy.NewPolicyRepository()
 	repo.AddList(rules)
-	c.Assert(repo.CanReachIngressRLocked(&ctx), Equals, api.Allowed)
+	c.Assert(repo.AllowsIngressRLocked(&ctx), Equals, api.Allowed)
 }
 
 func (s *K8sSuite) TestParseNetworkPolicyDenyAll(c *C) {

pkg/policy/repository.go
@@ -94,34 +94,6 @@ func (state *traceState) trace(rules ruleSlice, ctx *SearchContext) {
 	}
 }
 
-// CanReachIngressRLocked evaluates the policy repository for the provided search
-// context and returns the verdict or api.Undecided if no rule matches for
-// ingress. The policy repository mutex must be held.
-func (p *Repository) CanReachIngressRLocked(ctx *SearchContext) api.Decision {
-	return p.rules.canReachIngressRLocked(ctx)
-}
-
-// AllowsIngressLabelAccess evaluates the policy repository for the provided search
-// context and returns the verdict for ingress policy. If no matching policy
-// allows for the  connection, the request will be denied. The policy repository
-// mutex must be held.
-func (p *Repository) AllowsIngressLabelAccess(ctx *SearchContext) api.Decision {
-	ctx.PolicyTrace("Tracing %s\n", ctx.String())
-	decision := api.Denied
-
-	if len(p.rules) == 0 {
-		ctx.PolicyTrace("  No rules found\n")
-	} else {
-		if p.CanReachIngressRLocked(ctx) == api.Allowed {
-			decision = api.Allowed
-		}
-	}
-
-	ctx.PolicyTrace("Label verdict: %s", decision.String())
-
-	return decision
-}
-
 func wildcardL3L4Rule(proto api.L4Proto, port int, endpoints api.EndpointSelectorSlice,
 	ruleLabels labels.LabelArray, l4Policy L4PolicyMap) {
 	for k, filter := range l4Policy {
@@ -238,48 +210,34 @@ func (p *Repository) allowsL4Egress(ctx *SearchContext) api.Decision {
 	return verdict
 }
 
-func (p *Repository) allowsL4Ingress(ctx *SearchContext) api.Decision {
-	ingressPolicy, err := p.ResolveL4IngressPolicy(ctx)
-	if err != nil {
-		log.WithError(err).Warn("Evaluation error while resolving L4 ingress policy")
-	}
-	verdict := api.Undecided
-	if err == nil && len(*ingressPolicy) > 0 {
-		verdict = ingressPolicy.IngressCoversContext(ctx)
-	}
-
-	if len(ctx.DPorts) == 0 {
-		ctx.PolicyTrace("L4 ingress verdict: [no port context specified]")
-	} else {
-		ctx.PolicyTrace("L4 ingress verdict: %s", verdict.String())
-	}
-
-	return verdict
-}
-
 // AllowsIngressRLocked evaluates the policy repository for the provided search
 // context and returns the verdict for ingress. If no matching policy allows for
 // the  connection, the request will be denied. The policy repository mutex must
 // be held.
 func (p *Repository) AllowsIngressRLocked(ctx *SearchContext) api.Decision {
-	ctx.PolicyTrace("Tracing %s\n", ctx.String())
-	decision := p.CanReachIngressRLocked(ctx)
-	ctx.PolicyTrace("Label verdict: %s", decision.String())
-	if decision == api.Allowed {
-		ctx.PolicyTrace("L4 ingress policies skipped")
-		return decision
+	// Lack of DPorts in the SearchContext means L3-only search
+	if len(ctx.DPorts) == 0 {
+		newCtx := *ctx
+		newCtx.DPorts = []*models.Port{{
+			Port:     0,
+			Protocol: models.PortProtocolANY,
+		}}
+		ctx = &newCtx
 	}
 
-	// We only report the overall decision as L4 inclusive if a port has
-	// been specified
-	if len(ctx.DPorts) != 0 {
-		decision = p.allowsL4Ingress(ctx)
+	ctx.PolicyTrace("Tracing %s", ctx.String())
+	ingressPolicy, err := p.ResolveL4IngressPolicy(ctx)
+	if err != nil {
+		log.WithError(err).Warn("Evaluation error while resolving L4 ingress policy")
 	}
 
-	if decision != api.Allowed {
-		decision = api.Denied
+	verdict := api.Denied
+	if err == nil && len(*ingressPolicy) > 0 {
+		verdict = ingressPolicy.IngressCoversContext(ctx)
 	}
-	return decision
+
+	ctx.PolicyTrace("Ingress verdict: %s", verdict.String())
+	return verdict
 }
 
 // AllowsEgressRLocked evaluates the policy repository for the provided search

pkg/policy/rules.go
@@ -186,7 +186,7 @@ func (rules ruleSlice) resolveL4IngressPolicy(ctx *SearchContext, revision uint6
 	result := NewL4Policy()
 
 	ctx.PolicyTrace("\n")
-	ctx.PolicyTrace("Resolving ingress port policy for %+v\n", ctx.To)
+	ctx.PolicyTrace("Resolving ingress policy for %+v\n", ctx.To)
 
 	state := traceState{}
 	var requirements []v1.LabelSelectorRequirement

commit 1aad3070cc861344315377297a712c767208bf04

policy: Report 'found all labels' only when 'Matches()' succeeds.
Remove adding requirements to rules without remote endpoint selectors
when tracing, as this would allow traffic based on the requirement
alone, which is against the policy language specification.

Move the trace print-out of "Found all required labels" to take place
only if an endpoint selector matches. This was already the case in
most places.

Fixes: b4f41cb ("policy: Rework ingress policy trace to L4PolicyMap")
Fixes: a41d8a0 ("policy: Rework egress policy trace to L4PolicyMap")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/policy/rule.go
@@ -237,9 +237,8 @@ func mergeIngress(ctx *SearchContext, rule api.IngressRule, ruleLabels labels.La
 			ctx.PolicyTrace("      No label match for %s", ctx.From)
 			return 0, nil
 		}
+		ctx.PolicyTrace("      Found all required labels")
 	}
-
-	ctx.PolicyTrace("      Found all required labels")
 
 	// Daemon options may induce L3 allows for host/world. In this case, if
 	// we find any L7 rules matching host/world then we need to turn any L7
@@ -356,17 +355,11 @@ func (r *rule) resolveIngressPolicy(ctx *SearchContext, state *traceState, resul
 		// from rules which select the labels in ctx.To. This ensures that
 		// FromRequires is taken into account even if it isn't part of the current
 		// rule over which we are iterating.
-		if len(requirements) > 0 {
+		if len(requirements) > 0 && len(ingressRule.FromEndpoints) > 0 {
 			// Create a deep copy of the rule, as we are going to modify FromEndpoints
 			// with requirementsSelector. We don't want to modify the rule itself
 			// in the policy repository.
 			ruleCopy = *ingressRule.DeepCopy()
-			// If the rule only consists of a FromRequires statement,
-			// provide an empty selector so that it will be updated
-			// in the next step.
-			if ctx.TraceEnabled() && len(ruleCopy.FromEndpoints) == 0 {
-				ruleCopy.FromEndpoints = []api.EndpointSelector{api.NewESFromLabels()}
-			}
 			// Update each EndpointSelector in FromEndpoints to contain requirements.
 			for idx := range ruleCopy.FromEndpoints {
 				ruleCopy.FromEndpoints[idx].MatchExpressions = append(ruleCopy.FromEndpoints[idx].MatchExpressions, requirements...)
@@ -626,9 +619,8 @@ func mergeEgress(ctx *SearchContext, rule api.EgressRule, ruleLabels labels.Labe
 			ctx.PolicyTrace("      No label match for %s", ctx.To)
 			return 0, nil
 		}
+		ctx.PolicyTrace("      Found all required labels")
 	}
-
-	ctx.PolicyTrace("      Found all required labels")
 
 	var (
 		cnt int
@@ -742,17 +734,11 @@ func (r *rule) resolveEgressPolicy(ctx *SearchContext, state *traceState, result
 		// from rules which select the labels in ctx.From. This ensures that
 		// ToRequires is taken into account even if it isn't part of the current
 		// rule over which we are iterating.
-		if len(requirements) > 0 {
+		if len(requirements) > 0 && len(egressRule.ToEndpoints) > 0 {
 			// Create a deep copy of the rule, as we are going to modify
 			// ToEndpoints with requirements; we don't want to modify the rule
 			// in the repository.
 			ruleCopy = *egressRule.DeepCopy()
-			// If the rule only consists of a ToRequires statement,
-			// provide an empty selector so that it will be updated
-			// in the next step.
-			if ctx.TraceEnabled() && len(ruleCopy.ToEndpoints) == 0 {
-				ruleCopy.ToEndpoints = []api.EndpointSelector{api.NewESFromLabels()}
-			}
 			for idx := range ruleCopy.ToEndpoints {
 				// Update each EndpointSelector in ToEndpoints to contain
 				// requirements.

pkg/policy/repository_test.go
@@ -1902,8 +1902,6 @@ Resolving ingress policy for [any:bar]
     Allows from labels {"matchLabels":{"any:baz":""},"matchExpressions":[{"key":"any:baz","operator":"In","values":[""]}]}
       No label match for [any:foo]
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
-      No label match for [any:foo]
 3/3 rules selected
 Found no allow rule
 Ingress verdict: denied
@@ -1927,8 +1925,6 @@ Resolving ingress policy for [any:bar]
       Allows port [{80 ANY}]
         No port match found
 * Rule {"matchLabels":{"any:bar":""}}: selected
-    Enforcing requirements [{Key:any.baz Operator:In Values:[]}]
-      Found all required labels
 3/3 rules selected
 Found no allow rule
 Ingress verdict: denied