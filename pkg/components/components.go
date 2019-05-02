cilium/cilium

commit b9064c5e8b76363a6d1d29fe68825c6a06541007

cmd: Detect BPF map root properly
Cilium CLI should detect the map root on its own, using the
mountinfo package, because there is no way to get the information
from daemon process.

Fixes #4521

Signed-off-by: Michal Rostecki <mrostecki@suse.com>

pkg/components/components.go
@@ -1,0 +1,33 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package components
+
+import (
+	"os"
+	"strings"
+)
+
+const (
+	// CiliumAgentName is the name of cilium-agent (daemon) process name.
+	CiliumAgentName = "cilium-agent"
+	// CiliumDaemonTestName is the name of test binary for daemon package.
+	CiliumDaemonTestName = "daemon.test"
+)
+
+// IsCiliumAgent checks whether the current process is cilium-agent (daemon).
+func IsCiliumAgent() bool {
+	binaryName := os.Args[0]
+	return binaryName == CiliumAgentName || strings.Contains(binaryName, CiliumDaemonTestName)
+}

pkg/bpf/bpffs.go
@@ -21,6 +21,7 @@ import (
 	"sync"
 	"syscall"
 
+	"github.com/cilium/cilium/pkg/components"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -72,9 +73,29 @@ func MapPrefixPath() string {
 	return filepath.Join(mapRoot, mapPrefix)
 }
 
+func mapPathFromMountInfo(name string) string {
+	mountInfos, err := mountinfo.GetMountInfo()
+	if err != nil {
+		log.WithError(err).Fatal("Could not get mount info for map root lookup")
+	}
+
+	for _, mountInfo := range mountInfos {
+		if mountInfo.FilesystemType == mountinfo.FilesystemTypeBPFFS {
+			return filepath.Join(mountInfo.MountPoint, mapPrefix, name)
+		}
+	}
+
+	log.Fatal("Could not find BPF map root")
+	return ""
+}
+
+// MapPath returns a path for a BPF map with a given name.
 func MapPath(name string) string {
-	once.Do(lockDown)
-	return filepath.Join(mapRoot, mapPrefix, name)
+	if components.IsCiliumAgent() {
+		once.Do(lockDown)
+		return filepath.Join(mapRoot, mapPrefix, name)
+	}
+	return mapPathFromMountInfo(name)
 }
 
 // Environment returns a list of environment variables which are needed to make

commit 4d33bc6b021ac23c0ff861bf585729263e31f8a1

components: Fix cilium-agent process detection
Previously, the `components.IsCiliumAgent()` function was not able
to detect when cilium-agent was started from not the same dir as
the binary, e.g. `../daemon/cilium-agent`.

Fixes: b9064c5e8 ("cmd: Detect BPF map root properly")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/components/components.go
@@ -29,5 +29,6 @@ const (
 // IsCiliumAgent checks whether the current process is cilium-agent (daemon).
 func IsCiliumAgent() bool {
 	binaryName := os.Args[0]
-	return binaryName == CiliumAgentName || strings.Contains(binaryName, CiliumDaemonTestName)
+	return strings.HasSuffix(binaryName, CiliumAgentName) ||
+		strings.HasSuffix(binaryName, CiliumDaemonTestName)
 }