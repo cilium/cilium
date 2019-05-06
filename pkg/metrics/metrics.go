cilium/cilium

commit 5a68e1f11dc3215c7e96456cec0d75e56a3775d9

FQDN: Add metrics for Garbage collector cleanup.
This commit add the option to get the number of names that has been
cleaned on a GC job.

Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

pkg/metrics/metrics.go
@@ -510,6 +510,13 @@ var (
 		Name:      "operations_duration_seconds",
 		Help:      "Duration in seconds of kvstore operations",
 	}, []string{LabelScope, LabelKind, LabelAction, LabelOutcome})
+
+	// FQDNGarbageCollectorCleanedTotal is the number of domains cleaned by the
+	// GC job.
+	FQDNGarbageCollectorCleanedTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "fqdn_gc_deletions_total",
+		Help: "Number of FQDNs that have been cleaned on FQDN Garbage collector job",
+	})
 )
 
 func init() {
@@ -574,6 +581,8 @@ func init() {
 
 	MustRegister(KVStoreOperationsTotal)
 	MustRegister(KVStoreOperationsDuration)
+
+	MustRegister(FQDNGarbageCollectorCleanedTotal)
 }
 
 // MustRegister adds the collector to the registry, exposing this metric to

daemon/fqdn.go
@@ -150,6 +150,8 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState, preCache
 			for _, ep := range endpoints {
 				cfg.Cache.UpdateFromCache(ep.DNSHistory, namesToClean)
 			}
+
+			metrics.FQDNGarbageCollectorCleanedTotal.Add(float64(len(namesToClean)))
 			log.WithField(logfields.Controller, dnsGCJobName).Infof(
 				"FQDN garbage collector work deleted %d name entries", len(namesToClean))
 			return d.dnsRuleGen.ForceGenerateDNS(namesToClean)

commit fa5a0fcef34877903cc08bf31326c271d138e486

pkg/metrics: add namespace to fqdn_gc_deletions_total
The fqdn_gc_deletions_total misses the Cilium namespace and it's
currently exporter without it. This commit fixes that omission.

Fixes: 5a68e1f11dc3 ("FQDN: Add metrics for Garbage collector cleanup.")
Signed-off-by: André Martins <andre@cilium.io>

pkg/metrics/metrics.go
@@ -551,8 +551,9 @@ var (
 	// FQDNGarbageCollectorCleanedTotal is the number of domains cleaned by the
 	// GC job.
 	FQDNGarbageCollectorCleanedTotal = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "fqdn_gc_deletions_total",
-		Help: "Number of FQDNs that have been cleaned on FQDN Garbage collector job",
+		Namespace: Namespace,
+		Name:      "fqdn_gc_deletions_total",
+		Help:      "Number of FQDNs that have been cleaned on FQDN Garbage collector job",
 	})
 )
 