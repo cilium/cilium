e03b20029520d67d402fa9cd04d199e18a2591bf
André Martins <andre@cilium.io>
2019-05-06 13:29:51 +0200

    pkg/metrics: add CounterVec and GaugeVec interfaces
    As prometheus golang_library does not offer those interfaces we need to
    add our own specification of those interfaces. This will allow to have
    no-op implementations of those interfaces where they will be used when a
    metric is disabled.
    
    Signed-off-by: André Martins <andre@cilium.io>

---
+++ pkg/metrics/interfaces.go
@@ -0,0 +1,31 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type CounterVec interface {
+	WithLabelValues(lvls ...string) prometheus.Counter
+	GetMetricWithLabelValues(lvs ...string) (prometheus.Counter, error)
+	With(labels prometheus.Labels) prometheus.Counter
+	prometheus.Collector
+}
+
+type GaugeVec interface {
+	WithLabelValues(lvls ...string) prometheus.Gauge
+	prometheus.Collector
+}
