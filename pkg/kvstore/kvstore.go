9a9baaa92c1069816bf058495804b857125cfeac
Thomas Graf <thomas@cilium.io>
2019-05-07 12:09:46 +0200

    kvstore: Wait for kvstore to reach quorum
    [ upstream commit 680b2ee4b96c335c8873301b869100a0614fc00c ]
    
    So far, the channel returned by the Connected() function of the kvstore client
    was closed when the kvstore could be connected to. This did not guarantee that
    the kvstore actually had quorum and operations could still block and timeout
    afterwards. Wait for a distributed lock to be acquired to identify the moment
    quorum has been reached.
    
    Also indicate the quorum status in the kvstore status of `cilium status` for
    etcd.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/kvstore/kvstore.go
+++ pkg/kvstore/kvstore.go
@@ -42,6 +42,9 @@ const (
 
 	// BaseKeyPrefix is the base prefix that should be used for all keys
 	BaseKeyPrefix = "cilium"
+
+	// InitLockPath is the path to the init lock to test quorum
+	InitLockPath = BaseKeyPrefix + "/.initlock"
 )
 
 // Get returns value of key
