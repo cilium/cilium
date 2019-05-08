74cacf7a2bb10fe49adcb30f344b93432673aecf
Daniel Borkmann <daniel@iogearbox.net>
2019-05-08 10:56:55 +0000

    bpf: implement unconnected udp based host lb
    This gets nslookup, dig and friends working for UDP over IPv4/IPv6.
    
    Idea is to have a LRU map as a cache only in kernel space where
    requests are tracked on a per socket basis, aka socket cookie as
    part of the lookup key. It's large enough that this should be
    sufficient for current connections, old entries are purged as
    per LRU. Stale service entries are detected and automatically
    removed in the LRU if there is a revnat mismatch or reverse lookup
    fails.
    
    One acceptable limitation is that if a socket does a direct request
    to a backend instead of using the service IP (and the service IP
    still exists in the service lookup table), then it may get reverse
    translated wrongly due to the cached entry.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/init.sh
+++ bpf/init.sh
@@ -519,9 +519,13 @@ if [ "$HOSTLB" = "true" ]; then
 	COPTS="-DLB_L3 -DLB_L4"
 	if [ "$IP6_HOST" != "<nil>" ]; then
 		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr connect6 from-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr sendmsg6 snd-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr recvmsg6 rcv-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 	fi
 	if [ "$IP4_HOST" != "<nil>" ]; then
 		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr connect4 from-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr sendmsg4 snd-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr recvmsg4 rcv-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 	fi
 fi
 
