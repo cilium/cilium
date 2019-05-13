053a2cdec261d4bdd50dc72dbfb269d91cf2d215
Thomas Graf <thomas@cilium.io>
2019-05-14 00:51:31 +0200

    ctmap: Introduce variable conntrack gc interval
    Commit df8582d16e5 ("datapath: Optimize connection-tracking GC interval") has
    introduced a regression by changing the default interval for LRU map based
    conntrack to 12h on the basis that the tables will automatically expire the
    oldest entries when filling up.
    
    Unfortunately, walking the table to clean the conntrack stale podIPs on pod
    removal taints all entries. This causes to random conntrack entries being
    evicted while the table is full.
    
    Resolve this by adjusting the conntrack gc interval automatically based on the
    number of entries deleted in the table with the most deletions.
    
    Fixes: #7997
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/maps/ctmap/ctmap_test.go
+++ pkg/maps/ctmap/ctmap_test.go
@@ -19,8 +19,11 @@ package ctmap
 import (
 	"strings"
 	"testing"
+	"time"
 	"unsafe"
 
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/tuple"
 
@@ -64,4 +67,24 @@ func (t *CTMapTestSuite) TestInit(c *C) {
 			}
 		}
 	}
+}
+
+func (t *CTMapTestSuite) TestCalculateInterval(c *C) {
+	c.Assert(calculateInterval(bpf.MapTypeLRUHash, time.Minute, 0.1), Equals, time.Minute)  // no change
+	c.Assert(calculateInterval(bpf.MapTypeLRUHash, time.Minute, 0.2), Equals, time.Minute)  // no change
+	c.Assert(calculateInterval(bpf.MapTypeLRUHash, time.Minute, 0.25), Equals, time.Minute) // no change
+
+	c.Assert(calculateInterval(bpf.MapTypeLRUHash, time.Minute, 0.40), Equals, 36*time.Second)
+	c.Assert(calculateInterval(bpf.MapTypeLRUHash, time.Minute, 0.60), Equals, 24*time.Second)
+
+	d, err := time.ParseDuration("1m40s")
+	c.Assert(err, IsNil)
+	c.Assert(calculateInterval(bpf.MapTypeLRUHash, 10*time.Second, 0.01), Equals, d)
+	c.Assert(calculateInterval(bpf.MapTypeLRUHash, 10*time.Second, 0.04), Equals, 25*time.Second)
+
+	c.Assert(calculateInterval(bpf.MapTypeLRUHash, 1*time.Second, 0.01), Equals, defaults.ConntrackGCMinInterval)
+	c.Assert(calculateInterval(bpf.MapTypeHash, 1*time.Second, 0.01), Equals, defaults.ConntrackGCMinInterval)
+
+	c.Assert(calculateInterval(bpf.MapTypeLRUHash, 24*time.Hour, 0.01), Equals, defaults.ConntrackGCMaxLRUInterval)
+	c.Assert(calculateInterval(bpf.MapTypeHash, 24*time.Hour, 0.01), Equals, defaults.ConntrackGCMaxInterval)
 }
