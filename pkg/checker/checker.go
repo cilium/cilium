e1a8a7209e3540751de6522bc3134f2a783a0733
Jarno Rajahalme <jarno@covalent.io>
2019-05-14 14:43:11 +0300

    checker: Add support for using google/go-cmp
    reflect.DeepEquals does not support cyclical data structures. While it
    is good to avoid them, sometimes they are useful. Add support for
    using google/go-cmp, which allows data types to define their own
    Equal() function, which is used for comparisons. When such a function
    omits comparing the cyclical internals, cyclical data structures can
    be compared.
    
    One example of a cyclical data structure is a cache that tracks the
    users of a cached entity. The cached entity itself is the same
    regardless of how many users it has, so an Equal() function can omit
    comparing the tracked users.
    
    This is used by the forthcoming Selector Cache.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- pkg/checker/checker.go
+++ pkg/checker/checker.go
@@ -19,6 +19,8 @@ import (
 
 	"github.com/cilium/cilium/pkg/comparator"
 
+	"github.com/google/go-cmp/cmp"
+
 	"gopkg.in/check.v1"
 )
 
@@ -57,4 +59,83 @@ func (checker *diffChecker) Check(params []interface{}, names []string) (result 
 // includes a json representation of the difference between the parameters.
 func DeepEqual(params ...interface{}) (bool, string) {
 	return DeepEquals.Check(params, defaultParams)
+}
+
+type cmpChecker struct {
+	*check.CheckerInfo
+}
+
+// Equals is a GoCheck checker that does a diff between two objects and
+// pretty-prints any difference between the two. It can act as a substitute
+// for DeepEquals.
+var (
+	cmpParams               = []string{"obtained", "expected"}
+	Equals    check.Checker = &cmpChecker{
+		&check.CheckerInfo{Name: "Equals", Params: cmpParams},
+	}
+)
+
+// Check performs a diff between two objects provided as parameters, and
+// returns either true if the objects are identical, or false otherwise. If
+// it returns false, it also returns the unified diff between the expected
+// and obtained output.
+func (checker *cmpChecker) Check(params []interface{}, _ []string) (result bool, error string) {
+	if len(params) < 2 {
+		return false, "Parameter missing"
+	}
+
+	// Diff expects to receive parameters in order ("expected",
+	// "obtained"), but our convention is to pass them as
+	// ("obtained", "expected"), so reverse them here.
+	diff := cmp.Diff(params[1], params[0], DeepAllowUnexported(params[1], params[0]))
+
+	return diff == "", diff
+}
+
+// Equal tests whether two parameters are deeply equal, and returns true if
+// they are. If the objects are not deeply equal, then the second return value
+// includes a json representation of the difference between the parameters.
+func Equal(params ...interface{}) (bool, string) {
+	return Equals.Check(params, cmpParams)
+}
+
+func DeepAllowUnexported(vs ...interface{}) cmp.Option {
+	m := make(map[reflect.Type]struct{})
+	for _, v := range vs {
+		structTypes(reflect.ValueOf(v), m)
+	}
+	var typs []interface{}
+	for t := range m {
+		typs = append(typs, reflect.New(t).Elem().Interface())
+	}
+	return cmp.AllowUnexported(typs...)
+}
+
+func structTypes(v reflect.Value, m map[reflect.Type]struct{}) {
+	if !v.IsValid() {
+		return
+	}
+	switch v.Kind() {
+	case reflect.Ptr:
+		if !v.IsNil() {
+			structTypes(v.Elem(), m)
+		}
+	case reflect.Interface:
+		if !v.IsNil() {
+			structTypes(v.Elem(), m)
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			structTypes(v.Index(i), m)
+		}
+	case reflect.Map:
+		for _, k := range v.MapKeys() {
+			structTypes(v.MapIndex(k), m)
+		}
+	case reflect.Struct:
+		m[v.Type()] = struct{}{}
+		for i := 0; i < v.NumField(); i++ {
+			structTypes(v.Field(i), m)
+		}
+	}
 }
