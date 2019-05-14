cilium/cilium

commit 076b0188b98dcc5eea3100626b45adc0073bd908

Inter cluster connectivity (ClusterMesh)
This commit introduces connectivity between endpoints in multiple clusters.
The feature requires basic connectivity between all cluster nodes of all
clusters.

Two new options are added to unique identify clusters:

--cluster-name=NAME

   Human-readable, unique name to identify the cluster.

--cluster-id=ID

   Unique identifier for the cluster. Must be in the range of 1..255. The
   unique cluster ID is utilized to make per cluster security identities unique
   across multiple clusters without requiring co-ordination when allocating
   security identities.

CLUSTERMESH_CONFIG_PATH

  Path to a directory with individual configuration files pointing to remote
  clusters. Cilium monitors the directory using fsnotify to detect file
  changes.  Each file is interpreted as an etcd configuration and a connection
  is attempted. Each configuration represents a remote cluster. If the
  connection is successful, the nodes and ipache will be synchronized to the
  local instance, thus making the cluster reachable from the individual agent.

  This option is set to a default value in the DaemonSet file and populated by
  an Kubernetes secret. If present, the files stores in the secret are
  automatically mounted into the Cilium pod.

Upon successful connection to a remote etcd. All nodes in all remote clusters
are discovered to either build a tunnel mesh between all clusters or to
establish direct routing.

The IP cache of all clusters are synchronized to establish egress policy
enforcement. Ingress policy enforcement is provided by relying on the identity
based mechanism with a fall back to IP based security.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/node/node.go
@@ -65,6 +65,9 @@ type Node struct {
 	// cilium-health endpoint located on the node.
 	IPv6HealthIP net.IP
 
+	// ClusterID is the unique identifier of the cluster
+	ClusterID int
+
 	// cluster membership
 	cluster *clusterConfiguation
 }

daemon/daemon.go
@@ -38,6 +38,7 @@ import (
 	"github.com/cilium/cilium/pkg/api"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/clustermesh"
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/counter"
@@ -138,6 +139,8 @@ type Daemon struct {
 	// prefixLengths tracks a mapping from CIDR prefix length to the count
 	// of rules that refer to that prefix length.
 	prefixLengths *counter.PrefixLengthCounter
+
+	clustermesh *clustermesh.ClusterMesh
 }
 
 // UpdateProxyRedirect updates the redirect rules in the proxy for a particular
@@ -1261,6 +1264,8 @@ func NewDaemon() (*Daemon, error) {
 		log.WithError(err).Fatal("postinit failed")
 	}
 	log.Info("Addressing information:")
+	log.Infof("  Cluster-Name: %s", option.Config.ClusterName)
+	log.Infof("  Cluster-ID: %d", option.Config.ClusterID)
 	log.Infof("  Local node-name: %s", node.GetName())
 	log.Infof("  Node-IPv6: %s", node.GetIPv6())
 	log.Infof("  External-Node IPv4: %s", node.GetExternalIPv4())
@@ -1284,6 +1289,24 @@ func NewDaemon() (*Daemon, error) {
 
 	if err := node.ConfigureLocalNode(); err != nil {
 		log.WithError(err).Fatal("Unable to initialize local node")
+	}
+
+	if path := option.Config.ClusterMeshConfig; path != "" {
+		if option.Config.ClusterID == 0 {
+			log.Info("Cluster-ID is not specified, skipping ClusterMesh initialization")
+		} else {
+			log.WithField("path", path).Info("Initializing ClusterMesh routing")
+			clustermesh, err := clustermesh.NewClusterMesh(clustermesh.Configuration{
+				Name:            "clustermesh",
+				ConfigDirectory: path,
+				NodeKeyCreator:  node.KeyCreator,
+			})
+			if err != nil {
+				log.WithError(err).Fatal("Unable to initialize ClusterMesh")
+			}
+
+			d.clustermesh = clustermesh
+		}
 	}
 
 	// This needs to be done after the node addressing has been configured

daemon/main.go
@@ -341,8 +341,12 @@ func init() {
 		option.AutoIPv6NodeRoutesName, false, "Automatically adds IPv6 L3 routes to reach other nodes for non-overlay mode (--device) (BETA)")
 	flags.StringVar(&bpfRoot,
 		"bpf-root", "", "Path to BPF filesystem")
+	flags.Int(option.ClusterIDName, 0, "Unique identifier of the cluster")
+	viper.BindEnv(option.ClusterIDName, option.ClusterIDEnv)
 	flags.String(option.ClusterName, defaults.ClusterName, "Name of the cluster")
 	viper.BindEnv(option.ClusterName, option.ClusterNameEnv)
+	flags.String(option.ClusterMeshConfigName, "", "Path to the ClusterMesh configuration directory")
+	viper.BindEnv(option.ClusterMeshConfigName, option.ClusterMeshConfigNameEnv)
 	flags.StringVar(&cfgFile,
 		"config", "", `Configuration file (default "$HOME/ciliumd.yaml")`)
 	flags.Uint("conntrack-garbage-collector-interval", 60, "Garbage collection interval for the connection tracking table (in seconds)")

pkg/clustermesh/clustermesh.go
@@ -1,0 +1,164 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clustermesh
+
+import (
+	"fmt"
+
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/kvstore/store"
+	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/option"
+)
+
+const (
+	// configNotificationsChannelSize is the size of the channel used to
+	// notify a clustermesh of configuration changes
+	configNotificationsChannelSize = 512
+)
+
+// Configuration is the configuration that must be provided to
+// NewClusterMesh()
+type Configuration struct {
+	// Name is the name of the remote cluster cache. This is for logging
+	// purposes only
+	Name string
+
+	// ConfigDirectory is the path to the directory that will be watched for etcd
+	// configuration files to appear
+	ConfigDirectory string
+
+	// NodeKeyCreator is the function used to create node instances as
+	// nodes are being discovered in remote clusters
+	NodeKeyCreator store.KeyCreator
+}
+
+// ClusterMesh is a cache of multiple remote clusters
+type ClusterMesh struct {
+	// conf is the configuration, it is immutable after NewClusterMesh()
+	conf Configuration
+
+	mutex         lock.RWMutex
+	clusters      map[string]*remoteCluster
+	controllers   *controller.Manager
+	configWatcher *configDirectoryWatcher
+}
+
+// NewClusterMesh creates a new remote cluster cache based on the
+// provided configuration
+func NewClusterMesh(c Configuration) (*ClusterMesh, error) {
+	cm := &ClusterMesh{
+		conf:        c,
+		clusters:    map[string]*remoteCluster{},
+		controllers: controller.NewManager(),
+	}
+
+	w, err := createConfigDirectoryWatcher(c.ConfigDirectory, cm)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create config directory watcher: %s", err)
+	}
+
+	cm.configWatcher = w
+
+	controllerName := fmt.Sprintf("clustermesh-%s-config-fsnotify", c.Name)
+	cm.controllers.UpdateController(controllerName,
+		controller.ControllerParams{
+			DoFunc: func() error { return cm.configWatcher.watch() },
+		},
+	)
+
+	return cm, nil
+}
+
+// Close stops watching for remote cluster configuration files to appear and
+// will close all connections to remote clusters
+func (cm *ClusterMesh) Close() {
+	cm.mutex.Lock()
+	defer cm.mutex.Unlock()
+
+	if cm.configWatcher != nil {
+		cm.configWatcher.close()
+	}
+
+	for name, cluster := range cm.clusters {
+		cluster.onRemove()
+		delete(cm.clusters, name)
+	}
+
+	cm.controllers.RemoveAll()
+}
+
+func (cm *ClusterMesh) newRemoteCluster(name, path string) *remoteCluster {
+	return &remoteCluster{
+		name:        name,
+		configPath:  path,
+		mesh:        cm,
+		changed:     make(chan bool, configNotificationsChannelSize),
+		controllers: controller.NewManager(),
+	}
+}
+
+func (cm *ClusterMesh) add(name, path string) {
+	if name == option.Config.ClusterName {
+		log.WithField(fieldClusterName, name).Debug("Ignoring configuration for own cluster")
+		return
+	}
+
+	inserted := false
+	cm.mutex.Lock()
+	cluster, ok := cm.clusters[name]
+	if !ok {
+		cluster = cm.newRemoteCluster(name, path)
+		cm.clusters[name] = cluster
+		inserted = true
+	}
+	cm.mutex.Unlock()
+
+	log.WithField(fieldClusterName, name).Debug("Remote cluster configuration added")
+
+	if inserted {
+		cluster.onInsert()
+	} else {
+		// signal a change in configuration
+		cluster.changed <- true
+	}
+}
+
+func (cm *ClusterMesh) remove(name string) {
+	cm.mutex.Lock()
+	if cluster, ok := cm.clusters[name]; ok {
+		cluster.onRemove()
+		delete(cm.clusters, name)
+	}
+	cm.mutex.Unlock()
+
+	log.WithField(fieldClusterName, name).Debug("Remote cluster configuration removed")
+}
+
+// NumReadyClusters returns the number of remote clusters to which a connection
+// has been established
+func (cm *ClusterMesh) NumReadyClusters() int {
+	cm.mutex.RLock()
+	defer cm.mutex.RUnlock()
+
+	nready := 0
+	for _, cm := range cm.clusters {
+		if cm.isReady() {
+			nready++
+		}
+	}
+
+	return nready
+}

pkg/clustermesh/clustermesh_test.go
@@ -1,0 +1,182 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clustermesh
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/kvstore"
+	"github.com/cilium/cilium/pkg/kvstore/store"
+	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/testutils"
+
+	"github.com/sirupsen/logrus"
+	. "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type ClusterMeshTestSuite struct{}
+
+var _ = Suite(&ClusterMeshTestSuite{})
+
+var (
+	nodes      = map[string]*testNode{}
+	nodesMutex lock.RWMutex
+)
+
+type testNode struct {
+	// Name is the name of the node. This is typically the hostname of the node.
+	Name string
+
+	// Cluster is the name of the cluster the node is associated with
+	Cluster string
+}
+
+func (n *testNode) GetKeyName() string {
+	return path.Join(n.Name, n.Cluster)
+}
+
+func (n *testNode) OnUpdate() {
+	nodesMutex.Lock()
+	nodes[n.GetKeyName()] = n
+	nodesMutex.Unlock()
+}
+
+func (n *testNode) OnDelete() {
+	nodesMutex.Lock()
+	delete(nodes, n.GetKeyName())
+	nodesMutex.Unlock()
+}
+
+func (n *testNode) Marshal() ([]byte, error) {
+	return json.Marshal(n)
+}
+
+func (n *testNode) Unmarshal(data []byte) error {
+	return json.Unmarshal(data, n)
+}
+
+var testNodeCreator = func() store.Key {
+	n := testNode{}
+	return &n
+}
+
+type identityAllocatorOwnerMock struct{}
+
+func (i *identityAllocatorOwnerMock) TriggerPolicyUpdates(force bool) *sync.WaitGroup {
+	return nil
+}
+
+func (i *identityAllocatorOwnerMock) GetNodeSuffix() string {
+	return "foo"
+}
+
+func (s *ClusterMeshTestSuite) TestClusterMesh(c *C) {
+	kvstore.SetupDummy("etcd")
+	defer kvstore.Close()
+
+	logging.DefaultLogger.SetLevel(logrus.DebugLevel)
+
+	identity.InitIdentityAllocator(&identityAllocatorOwnerMock{})
+
+	dir, err := ioutil.TempDir("", "multicluster")
+	c.Assert(err, IsNil)
+	defer os.RemoveAll(dir)
+
+	etcdConfig := []byte(fmt.Sprintf("endpoints:\n- %s\n", kvstore.EtcdDummyAddress()))
+
+	config1 := path.Join(dir, "cluster1")
+	err = ioutil.WriteFile(config1, etcdConfig, 0644)
+	c.Assert(err, IsNil)
+
+	config2 := path.Join(dir, "cluster2")
+	err = ioutil.WriteFile(config2, etcdConfig, 0644)
+	c.Assert(err, IsNil)
+
+	cm, err := NewClusterMesh(Configuration{
+		Name:            "test2",
+		ConfigDirectory: dir,
+		NodeKeyCreator:  testNodeCreator,
+	})
+	c.Assert(err, IsNil)
+	c.Assert(cm, Not(IsNil))
+
+	nodeNames := []string{"foo", "bar", "baz"}
+
+	// wait for both clusters to appear in the list of cm clusters
+	c.Assert(testutils.WaitUntil(func() bool {
+		return cm.NumReadyClusters() == 2
+	}, 10*time.Second), IsNil)
+
+	cm.mutex.RLock()
+	for _, rc := range cm.clusters {
+		rc.mutex.RLock()
+		for _, name := range nodeNames {
+			err = rc.remoteNodes.UpdateLocalKeySync(&testNode{Name: name, Cluster: rc.name})
+			c.Assert(err, IsNil)
+		}
+		rc.mutex.RUnlock()
+	}
+	cm.mutex.RUnlock()
+
+	// wait for all cm nodes in both clusters to appear in the node list
+	c.Assert(testutils.WaitUntil(func() bool {
+		nodesMutex.RLock()
+		defer nodesMutex.RUnlock()
+		return len(nodes) == 2*len(nodeNames)
+	}, 10*time.Second), IsNil)
+
+	os.RemoveAll(config2)
+
+	// wait for the removed cluster to disappear
+	c.Assert(testutils.WaitUntil(func() bool {
+		return cm.NumReadyClusters() == 1
+	}, 5*time.Second), IsNil)
+
+	// wait for the nodes of the removed cluster to disappear
+	c.Assert(testutils.WaitUntil(func() bool {
+		nodesMutex.RLock()
+		defer nodesMutex.RUnlock()
+		return len(nodes) == len(nodeNames)
+	}, 10*time.Second), IsNil)
+
+	os.RemoveAll(config1)
+
+	// wait for the removed cluster to disappear
+	c.Assert(testutils.WaitUntil(func() bool {
+		return cm.NumReadyClusters() == 0
+	}, 5*time.Second), IsNil)
+
+	// wait for the nodes of the removed cluster to disappear
+	c.Assert(testutils.WaitUntil(func() bool {
+		nodesMutex.RLock()
+		defer nodesMutex.RUnlock()
+		return len(nodes) == 0
+	}, 10*time.Second), IsNil)
+
+	cm.Close()
+}

pkg/clustermesh/config.go
@@ -1,0 +1,105 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clustermesh
+
+import (
+	"io/ioutil"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+// clusterLifecycle is the interface to implement in order to receive cluster
+// configuration lifecycle events. This is implemented by the ClusterMesh.
+type clusterLifecycle interface {
+	add(clusterName, clusterConfigPath string)
+	remove(clusterName string)
+}
+
+type configDirectoryWatcher struct {
+	watcher   *fsnotify.Watcher
+	lifecycle clusterLifecycle
+	path      string
+	stop      chan struct{}
+}
+
+func createConfigDirectoryWatcher(path string, lifecycle clusterLifecycle) (*configDirectoryWatcher, error) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := watcher.Add(path); err != nil {
+		watcher.Close()
+		return nil, err
+	}
+
+	return &configDirectoryWatcher{
+		watcher:   watcher,
+		path:      path,
+		lifecycle: lifecycle,
+		stop:      make(chan struct{}, 0),
+	}, nil
+}
+
+func (cdw *configDirectoryWatcher) watch() error {
+	log.WithField(fieldConfig, cdw.path).Debug("Starting config directory watcher")
+
+	files, err := ioutil.ReadDir(cdw.path)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range files {
+		// A typical directory will look like this:
+		// lrwxrwxrwx. 1 root root 12 Jul 21 16:32 test5 -> ..data/test5
+		// lrwxrwxrwx. 1 root root 12 Jul 21 16:32 test7 -> ..data/test7
+		//
+		// Ignore all backing files and only read the symlinks
+		if strings.HasPrefix(f.Name(), "..") {
+			continue
+		}
+
+		log.WithField(fieldClusterName, f.Name()).WithField("mode", f.Mode()).Debugf("Found configuration in initial scan")
+		cdw.lifecycle.add(f.Name(), path.Join(cdw.path, f.Name()))
+	}
+
+	for {
+		select {
+		case event := <-cdw.watcher.Events:
+			name := filepath.Base(event.Name)
+			log.WithField(fieldClusterName, name).Debugf("Received fsnotify event: %+v", event)
+			switch event.Op {
+			case fsnotify.Create, fsnotify.Write, fsnotify.Chmod:
+				cdw.lifecycle.add(name, event.Name)
+			case fsnotify.Remove, fsnotify.Rename:
+				cdw.lifecycle.remove(name)
+			}
+
+		case err := <-cdw.watcher.Errors:
+			return err
+
+		case <-cdw.stop:
+			return nil
+		}
+	}
+}
+
+func (cdw *configDirectoryWatcher) close() {
+	close(cdw.stop)
+	cdw.watcher.Close()
+}

pkg/clustermesh/config_test.go
@@ -1,0 +1,125 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clustermesh
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"time"
+
+	"github.com/cilium/cilium/pkg/testutils"
+
+	. "gopkg.in/check.v1"
+)
+
+func createFile(c *C, name string) {
+	f, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE, 0666)
+	c.Assert(err, IsNil)
+	f.Sync()
+	f.Close()
+}
+
+func expectExists(c *C, cm *ClusterMesh, name string) {
+	c.Assert(cm.clusters[name], Not(IsNil))
+}
+
+func expectChange(c *C, cm *ClusterMesh, name string) {
+	cluster := cm.clusters[name]
+	c.Assert(cluster, Not(IsNil))
+
+	select {
+	case <-cluster.changed:
+	case <-time.After(time.Second):
+		c.Fatal("timeout while waiting for changed event")
+	}
+}
+
+func expectNotExist(c *C, cm *ClusterMesh, name string) {
+	c.Assert(cm.clusters[name], IsNil)
+}
+
+func (s *ClusterMeshTestSuite) TestWatchConfigDirectory(c *C) {
+	skipKvstoreConnection = true
+
+	dir, err := ioutil.TempDir("", "multicluster")
+	c.Assert(err, IsNil)
+	defer os.RemoveAll(dir)
+
+	file1 := path.Join(dir, "cluster1")
+	file2 := path.Join(dir, "cluster2")
+	file3 := path.Join(dir, "cluster3")
+
+	createFile(c, file1)
+	createFile(c, file2)
+
+	cm, err := NewClusterMesh(Configuration{
+		Name:            "test1",
+		ConfigDirectory: dir,
+		NodeKeyCreator:  testNodeCreator,
+	})
+	c.Assert(err, IsNil)
+	c.Assert(cm, Not(IsNil))
+	defer cm.Close()
+
+	// wait for cluster1 and cluster2 to appear
+	c.Assert(testutils.WaitUntil(func() bool { return len(cm.clusters) == 2 }, time.Second), IsNil)
+	expectExists(c, cm, "cluster1")
+	expectExists(c, cm, "cluster2")
+	expectNotExist(c, cm, "cluster3")
+
+	err = os.RemoveAll(file1)
+	c.Assert(err, IsNil)
+
+	// wait for cluster1 to disappear
+	c.Assert(testutils.WaitUntil(func() bool { return len(cm.clusters) == 1 }, time.Second), IsNil)
+
+	createFile(c, file3)
+
+	// wait for cluster3 to appear
+	c.Assert(testutils.WaitUntil(func() bool { return len(cm.clusters) == 2 }, time.Second), IsNil)
+	expectNotExist(c, cm, "cluster1")
+	expectExists(c, cm, "cluster2")
+	expectExists(c, cm, "cluster3")
+
+	// Test renaming of file from cluster3 to cluster1
+	err = os.Rename(file3, file1)
+	c.Assert(err, IsNil)
+
+	// wait for cluster1 to appear
+	c.Assert(testutils.WaitUntil(func() bool { return cm.clusters["cluster1"] != nil }, time.Second), IsNil)
+	expectExists(c, cm, "cluster2")
+	expectNotExist(c, cm, "cluster3")
+
+	// touch file
+	err = os.Chtimes(file1, time.Now(), time.Now())
+	c.Assert(err, IsNil)
+
+	// give time for events to be processed
+	time.Sleep(100 * time.Millisecond)
+	expectChange(c, cm, "cluster1")
+
+	err = os.RemoveAll(file1)
+	c.Assert(err, IsNil)
+	err = os.RemoveAll(file2)
+	c.Assert(err, IsNil)
+
+	// wait for all clusters to disappear
+	c.Assert(testutils.WaitUntil(func() bool { return len(cm.clusters) == 0 }, time.Second), IsNil)
+	expectNotExist(c, cm, "cluster1")
+	expectNotExist(c, cm, "cluster2")
+	expectNotExist(c, cm, "cluster3")
+
+}

pkg/clustermesh/logfields.go
@@ -1,0 +1,30 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clustermesh
+
+import (
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
+
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "clustermesh")
+
+const (
+	fieldName          = "name"
+	fieldClusterName   = "clusterName"
+	fieldConfig        = "config"
+	fieldKVStoreStatus = "kvstoreStatus"
+	fieldKVStoreErr    = "kvstoreErr"
+)

commit b20d69c1767c6003ba3630d531e0affaab5aa2c3

cilium: IsLocal() needs to compare both Name and Cluster
The cluster name should be used to determine if a node is local to
avoid an error if two nodes in different clusters have the same
name.

A unique string identifier is Cluster + Name.

Fixes: 076b0188b98dc ("Inter cluster connectivity (ClusterMesh)")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/node/node.go
@@ -257,10 +257,14 @@ func (n *Node) Identity() Identity {
 	}
 }
 
+func getCluster() string {
+	return option.Config.ClusterName
+}
+
 // IsLocal returns true if this is the node on which the agent itself is
 // running on
 func (n *Node) IsLocal() bool {
-	return n != nil && n.Name == GetName()
+	return n != nil && n.Name == GetName() && n.Cluster == getCluster()
 }
 
 // PublicAttrEquals returns true only if the public attributes of both nodes