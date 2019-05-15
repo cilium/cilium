8c798166c82de0cc9196e6cc7ceb2bd55718d25f
André Martins <andre@cilium.io>
2019-05-15 16:35:44 +0200

    pkg/maps: create CtKeyGlobal structures
    As Ct{4,6}Key were being passed to the initialization of the BPF Maps
    cilium was not correctly dumping the content of the ct global maps as
    the Dump method used of for local CT maps.
    
    Fixes: 79c247a955f8 ("pkg/{bpf,datapath,maps}: use same MapKey and MapValue in map iterations")
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/maps/ctmap/lookup.go
+++ pkg/maps/ctmap/lookup.go
@@ -26,6 +26,8 @@ import (
 )
 
 // CtKey4 is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
 type CtKey4 struct {
 	tuple.TupleKey4
 }
@@ -41,7 +43,27 @@ func (k *CtKey4) ToNetwork() tuple.TupleKey {
 	return &n
 }
 
+// CtKey4Global is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type CtKey4Global struct {
+	tuple.TupleKey4Global
+}
+
+// NewValue creates a new bpf.MapValue.
+func (k *CtKey4Global) NewValue() bpf.MapValue { return &CtEntry{} }
+
+// ToNetwork converts CtKey4Global ports to network byte order.
+func (k *CtKey4Global) ToNetwork() tuple.TupleKey {
+	n := *k
+	n.SourcePort = byteorder.HostToNetwork(n.SourcePort).(uint16)
+	n.DestPort = byteorder.HostToNetwork(n.DestPort).(uint16)
+	return &n
+}
+
 // CtKey6 is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
 type CtKey6 struct {
 	tuple.TupleKey6
 }
@@ -57,40 +79,78 @@ func (k *CtKey6) ToNetwork() tuple.TupleKey {
 	return &n
 }
 
-func createTupleKey(remoteAddr, localAddr string, proto u8proto.U8proto, ingress bool) (tuple.TupleKey, error) {
+// CtKey6Global is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type CtKey6Global struct {
+	tuple.TupleKey6Global
+}
+
+// NewValue creates a new bpf.MapValue.
+func (k *CtKey6Global) NewValue() bpf.MapValue { return &CtEntry{} }
+
+// ToNetwork converts CtKey6Global ports to network byte order.
+func (k *CtKey6Global) ToNetwork() tuple.TupleKey {
+	n := *k
+	n.SourcePort = byteorder.HostToNetwork(n.SourcePort).(uint16)
+	n.DestPort = byteorder.HostToNetwork(n.DestPort).(uint16)
+	return &n
+}
+
+func createTupleKey(isGlobal bool, remoteAddr, localAddr string, proto u8proto.U8proto, ingress bool) (bpf.MapKey, bool, error) {
 	ip, port, err := net.SplitHostPort(remoteAddr)
 	if err != nil {
-		return nil, fmt.Errorf("invalid remote address '%s': %s", remoteAddr, err)
+		return nil, false, fmt.Errorf("invalid remote address '%s': %s", remoteAddr, err)
 	}
 
 	sIP := net.ParseIP(ip)
 	if sIP == nil {
-		return nil, fmt.Errorf("unable to parse IP %s", ip)
+		return nil, false, fmt.Errorf("unable to parse IP %s", ip)
 	}
 
 	sport, err := strconv.ParseUint(port, 10, 16)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse port string: %s", err)
+		return nil, false, fmt.Errorf("unable to parse port string: %s", err)
 	}
 
 	localIp, localPort, err := net.SplitHostPort(localAddr)
 	if err != nil {
-		return nil, fmt.Errorf("invalid local address '%s': %s", localAddr, err)
+		return nil, false, fmt.Errorf("invalid local address '%s': %s", localAddr, err)
 	}
 
 	dIP := net.ParseIP(localIp)
 	if dIP == nil {
-		return nil, fmt.Errorf("unable to parse IP %s", localIp)
+		return nil, false, fmt.Errorf("unable to parse IP %s", localIp)
 	}
 
 	dport, err := strconv.ParseUint(localPort, 10, 16)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse port string: %s", err)
+		return nil, false, fmt.Errorf("unable to parse port string: %s", err)
 	}
 
 	if sIP.To4() != nil {
+		if isGlobal {
+			key := &CtKey4Global{
+				TupleKey4Global: tuple.TupleKey4Global{
+					TupleKey4: tuple.TupleKey4{
+						SourcePort: uint16(sport),
+						DestPort:   uint16(dport),
+						NextHeader: proto,
+						Flags:      TUPLE_F_OUT,
+					},
+				},
+			}
+			// CTmap has the addresses in the reverse order w.r.t. the original direction
+			copy(key.SourceAddr[:], dIP.To4())
+			copy(key.DestAddr[:], sIP.To4())
+			if ingress {
+				key.Flags = TUPLE_F_IN
+			}
+			return key.ToNetwork(), true, nil
+		}
+
 		key := &CtKey4{
-			tuple.TupleKey4{
+			TupleKey4: tuple.TupleKey4{
 				SourcePort: uint16(sport),
 				DestPort:   uint16(dport),
 				NextHeader: proto,
@@ -103,11 +163,31 @@ func createTupleKey(remoteAddr, localAddr string, proto u8proto.U8proto, ingress
 		if ingress {
 			key.Flags = TUPLE_F_IN
 		}
-		return key.ToNetwork(), nil
+		return key.ToNetwork(), true, nil
+	}
+
+	if isGlobal {
+		key := &CtKey6Global{
+			TupleKey6Global: tuple.TupleKey6Global{
+				TupleKey6: tuple.TupleKey6{
+					SourcePort: uint16(sport),
+					DestPort:   uint16(dport),
+					NextHeader: proto,
+					Flags:      TUPLE_F_OUT,
+				},
+			},
+		}
+		// CTmap has the addresses in the reverse order w.r.t. the original direction
+		copy(key.SourceAddr[:], dIP.To16())
+		copy(key.DestAddr[:], sIP.To16())
+		if ingress {
+			key.Flags = TUPLE_F_IN
+		}
+		return key.ToNetwork(), false, nil
 	}
 
 	key := &CtKey6{
-		tuple.TupleKey6{
+		TupleKey6: tuple.TupleKey6{
 			SourcePort: uint16(sport),
 			DestPort:   uint16(dport),
 			NextHeader: proto,
@@ -120,7 +200,7 @@ func createTupleKey(remoteAddr, localAddr string, proto u8proto.U8proto, ingress
 	if ingress {
 		key.Flags = TUPLE_F_IN
 	}
-	return key.ToNetwork(), nil
+	return key.ToNetwork(), false, nil
 }
 
 func getMapName(mapname string, ipv4 bool, proto u8proto.U8proto) string {
@@ -145,12 +225,12 @@ func getMapName(mapname string, ipv4 bool, proto u8proto.U8proto) string {
 // 'epname' is a 5-digit represenation of the endpoint ID if local maps
 // are to be used, or "global" if global maps should be used.
 func Lookup(epname string, remoteAddr, localAddr string, proto u8proto.U8proto, ingress bool) (*CtEntry, error) {
-	key, err := createTupleKey(remoteAddr, localAddr, proto, ingress)
+	isGlobal := epname == "global"
+
+	key, ipv4, err := createTupleKey(isGlobal, remoteAddr, localAddr, proto, ingress)
 	if err != nil {
 		return nil, err
 	}
-
-	_, ipv4 := key.(*CtKey4)
 
 	mapname := getMapName(epname, ipv4, proto)
 
@@ -161,10 +241,18 @@ func Lookup(epname string, remoteAddr, localAddr string, proto u8proto.U8proto, 
 		if err != nil {
 			return nil, fmt.Errorf("Can not open CT map %s: %s", mapname, err)
 		}
-		if ipv4 {
-			m.MapKey = &CtKey4{}
+		if isGlobal {
+			if ipv4 {
+				m.MapKey = &CtKey4Global{}
+			} else {
+				m.MapKey = &CtKey6Global{}
+			}
 		} else {
-			m.MapKey = &CtKey6{}
+			if ipv4 {
+				m.MapKey = &CtKey4{}
+			} else {
+				m.MapKey = &CtKey6{}
+			}
 		}
 		m.MapValue = &CtEntry{}
 	}
