d7d2403ce1d16a1c11ff486895bd82d0a135ce7f
Thomas Graf <thomas@cilium.io>
2019-05-17 01:52:28 +0200

    cni: Add generic veth chaining plugin
    This generic plugin identifies a veth peer in the container namespace and
    extracts all relevant information from it, creates a Cilium endpoint and
    instructs Cilium to install ingress and egress programs.
    
    It can be used to build chaining with any veth-based CNI plugin.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ plugins/cilium-cni/chaining/generic-veth/generic-veth.go
@@ -0,0 +1,225 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package genericveth
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/client"
+	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	chainingapi "github.com/cilium/cilium/plugins/cilium-cni/chaining/api"
+
+	cniTypesVer "github.com/containernetworking/cni/pkg/types/current"
+	cniVersion "github.com/containernetworking/cni/pkg/version"
+	"github.com/containernetworking/plugins/pkg/ns"
+	"github.com/sirupsen/logrus"
+	"github.com/vishvananda/netlink"
+)
+
+var (
+	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "generic-veth")
+)
+
+type genericVethChainer struct{}
+
+func (f *genericVethChainer) ImplementsAdd() bool {
+	return true
+}
+
+func (f *genericVethChainer) Add(ctx context.Context, pluginCtx chainingapi.PluginContext) (res *cniTypesVer.Result, err error) {
+	err = cniVersion.ParsePrevResult(&pluginCtx.NetConf.NetConf)
+	if err != nil {
+		err = fmt.Errorf("unable to understand network config: %s", err)
+		return
+	}
+
+	var prevRes *cniTypesVer.Result
+	prevRes, err = cniTypesVer.NewResultFromResult(pluginCtx.NetConf.PrevResult)
+	if err != nil {
+		err = fmt.Errorf("unable to get previous network result: %s", err)
+		return
+	}
+
+	defer func() {
+		if err != nil {
+			pluginCtx.Logger.WithError(err).
+				WithFields(logrus.Fields{"cni-pre-result": pluginCtx.NetConf.PrevResult.String()}).
+				Errorf("Unable to create endpoint")
+		}
+	}()
+	var (
+		hostMac, vethHostName, vethLXCMac, vethIP string
+		vethHostIdx, peerIndex                    int
+		peer                                      netlink.Link
+		netNs                                     ns.NetNS
+	)
+
+	netNs, err = ns.GetNS(pluginCtx.Args.Netns)
+	if err != nil {
+		err = fmt.Errorf("failed to open netns %q: %s", pluginCtx.Args.Netns, err)
+		return
+	}
+	defer netNs.Close()
+
+	if err = netNs.Do(func(_ ns.NetNS) error {
+		links, err := netlink.LinkList()
+		if err != nil {
+			return err
+		}
+
+		for _, link := range links {
+			log.Debugf("Found interface in container %+v", link.Attrs())
+
+			if link.Type() != "veth" {
+				continue
+			}
+
+			vethLXCMac = link.Attrs().HardwareAddr.String()
+
+			veth, ok := link.(*netlink.Veth)
+			if !ok {
+				return fmt.Errorf("link %s is not a veth interface", vethHostName)
+			}
+
+			peerIndex, err = netlink.VethPeerIndex(veth)
+			if err != nil {
+				return fmt.Errorf("unable to retrieve index of veth peer %s: %s", vethHostName, err)
+			}
+
+			addrs, err := netlink.AddrList(link, netlink.FAMILY_V4)
+			if err != nil {
+				return fmt.Errorf("unable to list addresses for link %s: %s", link.Attrs().Name, err)
+			}
+
+			if len(addrs) < 1 {
+				return fmt.Errorf("no address configured inside container")
+			}
+
+			vethIP = addrs[0].IPNet.IP.String()
+			return nil
+		}
+
+		return fmt.Errorf("no link found inside container")
+	}); err != nil {
+		return
+	}
+
+	peer, err = netlink.LinkByIndex(peerIndex)
+	if err != nil {
+		err = fmt.Errorf("unable to lookup link %d: %s", peerIndex, err)
+		return
+	}
+
+	hostMac = peer.Attrs().HardwareAddr.String()
+	vethHostName = peer.Attrs().Name
+	vethHostIdx = peer.Attrs().Index
+
+	switch {
+	case vethHostName == "":
+		err = errors.New("unable to determine name of veth pair on the host side")
+		return
+	case vethLXCMac == "":
+		err = errors.New("unable to determine MAC address of veth pair on the container side")
+		return
+	case vethIP == "":
+		err = errors.New("unable to determine IP address of the container")
+		return
+	case vethHostIdx == 0:
+		err = errors.New("unable to determine index interface of veth pair on the host side")
+		return
+	}
+
+	var enabled = true
+	ep := &models.EndpointChangeRequest{
+		Addressing: &models.AddressPair{
+			IPV4: vethIP,
+		},
+		ContainerID:       pluginCtx.Args.ContainerID,
+		State:             models.EndpointStateWaitingForIdentity,
+		HostMac:           hostMac,
+		InterfaceIndex:    int64(vethHostIdx),
+		Mac:               vethLXCMac,
+		InterfaceName:     vethHostName,
+		K8sPodName:        string(pluginCtx.CniArgs.K8S_POD_NAME),
+		K8sNamespace:      string(pluginCtx.CniArgs.K8S_POD_NAMESPACE),
+		SyncBuildEndpoint: true,
+		DatapathConfiguration: &models.EndpointDatapathConfiguration{
+			// aws-cni requires ARP passthrough between Linux and
+			// the pod
+			RequireArpPassthrough: true,
+
+			// The route is pointing directly into the veth of the
+			// pod, install a host-facing egress program to
+			// implement ingress policy and to provide reverse NAT
+			RequireEgressProg: true,
+
+			// The IP is managed by the aws-cni plugin, no need for
+			// Cilium to manage any aspect of addressing
+			ExternalIPAM: true,
+
+			// All routing is performed by the Linux stack
+			RequireRouting: &enabled,
+		},
+	}
+
+	err = pluginCtx.Client.EndpointCreate(ep)
+	if err != nil {
+		pluginCtx.Logger.WithError(err).WithFields(logrus.Fields{
+			logfields.ContainerID: ep.ContainerID}).Warn("Unable to create endpoint")
+		err = fmt.Errorf("unable to create endpoint: %s", err)
+		return
+	}
+
+	pluginCtx.Logger.WithFields(logrus.Fields{
+		logfields.ContainerID: ep.ContainerID}).Debug("Endpoint successfully created")
+
+	res = prevRes
+
+	return
+}
+
+func (f *genericVethChainer) ImplementsDelete() bool {
+	return true
+}
+
+func (f *genericVethChainer) Delete(ctx context.Context, pluginCtx chainingapi.PluginContext) (err error) {
+	id := endpointid.NewID(endpointid.ContainerIdPrefix, pluginCtx.Args.ContainerID)
+	if err := pluginCtx.Client.EndpointDelete(id); err != nil {
+		// EndpointDelete returns an error in the following scenarios:
+		// DeleteEndpointIDInvalid: Invalid delete parameters, no need to retry
+		// DeleteEndpointIDNotFound: No need to retry
+		// DeleteEndpointIDErrors: Errors encountered while deleting,
+		//                         the endpoint is always deleted though, no
+		//                         need to retry
+		// ClientError: Various reasons, type will be ClientError and
+		//              Recoverable() will return true if error can be
+		//              retried
+		if clientError, ok := err.(client.ClientError); ok {
+			if clientError.Recoverable() {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func init() {
+	chainingapi.Register("generic-veth", &genericVethChainer{})
+}
