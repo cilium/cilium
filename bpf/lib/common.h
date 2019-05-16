cilium/cilium

commit be323d8ca2ca12f18f6380b3683c60a18cbdeedf

bpf,lbmap: Change backend ID to uint32
This commit changes the underlying backend ID type from uint16 to
uint36, so that more than 65556 unique service endpoints can be
supported.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/lib/common.h
@@ -486,10 +486,11 @@ struct lb6_key_v2 {
 
 /* See lb4_service_v2 comments */
 struct lb6_service_v2 {
+	__u32 backend_id;
 	__u16 count;
-	__u16 backend_id;
 	__u16 rev_nat_index;
 	__u16 weight;
+	__u16 pad;
 };
 
 /* See lb4_backend comments */
@@ -528,15 +529,16 @@ struct lb4_key_v2 {
 };
 
 struct lb4_service_v2 {
+	__u32 backend_id;	/* Backend ID in lb4_backends */
 	/* For the master service, count denotes number of service endpoints,
 	 * while for any service endpoint, count contains a slave slot number
 	 * in a corresponding legacy service which points to the same backend
 	 * (used for the backward compatibility)
 	 */
 	__u16 count;
-	__u16 backend_id;	/* Backend ID in lb4_backends */
 	__u16 rev_nat_index;	/* Reverse NAT ID in lb4_reverse_nat */
 	__u16 weight;		/* Currently not used */
+	__u16 pad;
 };
 
 struct lb4_backend {

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=f9678534dd09b4a9a5f01b87c13abfdc4f6514fa
+GO_BINDATA_SHA1SUM=0f63fd108482b0a22fa049e7d989bdb45556052b
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

pkg/maps/lbmap/ipv4.go
@@ -344,15 +344,16 @@ func (k *Service4KeyV2) ToNetwork() ServiceKeyV2 {
 
 // Service4ValueV2 must match 'struct lb4_service_v2' in "bpf/lib/common.h".
 type Service4ValueV2 struct {
-	Count     uint16                 `align:"count"`
-	BackendID loadbalancer.BackendID `align:"backend_id"`
-	RevNat    uint16                 `align:"rev_nat_index"`
-	Weight    uint16                 `align:"weight"`
+	BackendID uint32 `align:"backend_id"`
+	Count     uint16 `align:"count"`
+	RevNat    uint16 `align:"rev_nat_index"`
+	Weight    uint16 `align:"weight"`
+	Pad       uint16
 }
 
 func NewService4ValueV2(count uint16, backendID loadbalancer.BackendID, revNat uint16, weight uint16) *Service4ValueV2 {
 	svc := Service4ValueV2{
-		BackendID: backendID,
+		BackendID: uint32(backendID),
 		Count:     count,
 		RevNat:    revNat,
 		Weight:    weight,
@@ -367,15 +368,20 @@ func (s *Service4ValueV2) String() string {
 
 func (s *Service4ValueV2) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(s) }
 
-func (s *Service4ValueV2) SetCount(count int)                     { s.Count = uint16(count) }
-func (s *Service4ValueV2) GetCount() int                          { return int(s.Count) }
-func (s *Service4ValueV2) SetRevNat(id int)                       { s.RevNat = uint16(id) }
-func (s *Service4ValueV2) GetRevNat() int                         { return int(s.RevNat) }
-func (s *Service4ValueV2) SetWeight(weight uint16)                { s.Weight = weight }
-func (s *Service4ValueV2) GetWeight() uint16                      { return s.Weight }
-func (s *Service4ValueV2) SetBackendID(id loadbalancer.BackendID) { s.BackendID = id }
-func (s *Service4ValueV2) GetBackendID() loadbalancer.BackendID   { return s.BackendID }
-func (s *Service4ValueV2) RevNatKey() RevNatKey                   { return &RevNat4Key{s.RevNat} }
+func (s *Service4ValueV2) SetCount(count int)      { s.Count = uint16(count) }
+func (s *Service4ValueV2) GetCount() int           { return int(s.Count) }
+func (s *Service4ValueV2) SetRevNat(id int)        { s.RevNat = uint16(id) }
+func (s *Service4ValueV2) GetRevNat() int          { return int(s.RevNat) }
+func (s *Service4ValueV2) SetWeight(weight uint16) { s.Weight = weight }
+func (s *Service4ValueV2) GetWeight() uint16       { return s.Weight }
+func (s *Service4ValueV2) RevNatKey() RevNatKey    { return &RevNat4Key{s.RevNat} }
+
+func (s *Service4ValueV2) SetBackendID(id loadbalancer.BackendID) {
+	s.BackendID = uint32(id)
+}
+func (s *Service4ValueV2) GetBackendID() loadbalancer.BackendID {
+	return loadbalancer.BackendID(s.BackendID)
+}
 
 func (s *Service4ValueV2) ToNetwork() ServiceValueV2 {
 	n := *s

pkg/maps/lbmap/ipv6.go
@@ -338,16 +338,17 @@ func (k *Service6KeyV2) ToNetwork() ServiceKeyV2 {
 
 // Service6ValueV2 must match 'struct lb6_service_v2' in "bpf/lib/common.h".
 type Service6ValueV2 struct {
-	Count     uint16                 `align:"count"`
-	BackendID loadbalancer.BackendID `align:"backend_id"`
-	RevNat    uint16                 `align:"rev_nat_index"`
-	Weight    uint16                 `align:"weight"`
+	BackendID uint32 `align:"backend_id"`
+	Count     uint16 `align:"count"`
+	RevNat    uint16 `align:"rev_nat_index"`
+	Weight    uint16 `align:"weight"`
+	Pad       uint16
 }
 
 func NewService6ValueV2(count uint16, backendID loadbalancer.BackendID, revNat uint16, weight uint16) *Service6ValueV2 {
 	svc := Service6ValueV2{
 		Count:     count,
-		BackendID: backendID,
+		BackendID: uint32(backendID),
 		RevNat:    revNat,
 		Weight:    weight,
 	}
@@ -361,15 +362,20 @@ func (s *Service6ValueV2) String() string {
 
 func (s *Service6ValueV2) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(s) }
 
-func (s *Service6ValueV2) SetCount(count int)                     { s.Count = uint16(count) }
-func (s *Service6ValueV2) GetCount() int                          { return int(s.Count) }
-func (s *Service6ValueV2) SetRevNat(id int)                       { s.RevNat = uint16(id) }
-func (s *Service6ValueV2) GetRevNat() int                         { return int(s.RevNat) }
-func (s *Service6ValueV2) SetWeight(weight uint16)                { s.Weight = weight }
-func (s *Service6ValueV2) GetWeight() uint16                      { return s.Weight }
-func (s *Service6ValueV2) SetBackendID(id loadbalancer.BackendID) { s.BackendID = id }
-func (s *Service6ValueV2) GetBackendID() loadbalancer.BackendID   { return s.BackendID }
-func (s *Service6ValueV2) RevNatKey() RevNatKey                   { return &RevNat6Key{s.RevNat} }
+func (s *Service6ValueV2) SetCount(count int)      { s.Count = uint16(count) }
+func (s *Service6ValueV2) GetCount() int           { return int(s.Count) }
+func (s *Service6ValueV2) SetRevNat(id int)        { s.RevNat = uint16(id) }
+func (s *Service6ValueV2) GetRevNat() int          { return int(s.RevNat) }
+func (s *Service6ValueV2) SetWeight(weight uint16) { s.Weight = weight }
+func (s *Service6ValueV2) GetWeight() uint16       { return s.Weight }
+func (s *Service6ValueV2) RevNatKey() RevNatKey    { return &RevNat6Key{s.RevNat} }
+
+func (s *Service6ValueV2) SetBackendID(id loadbalancer.BackendID) {
+	s.BackendID = uint32(id)
+}
+func (s *Service6ValueV2) GetBackendID() loadbalancer.BackendID {
+	return loadbalancer.BackendID(s.BackendID)
+}
 
 func (s *Service6ValueV2) ToNetwork() ServiceValueV2 {
 	n := *s

commit 15c1b3c335e7415f0b45a3abd83fb494e58ca626

datapath: Fix backend_id type (uint32)
For some reasons, the be323d8ca commit totally missed the fact that
`loadbalancer.BackendID` is of the uin16 type and re-used the type
for `backend_id` in the LB BPF maps. So, `backend_id` was uint16.

Meanwhile, the backend ID allocator allocated internally uint32, but
the API call `AcquireBackendID` casteds IDs to
`loadbalancer.BackendID`(=uint16). So technically, it could returned
the same IDs for different backends.

Fixes: be323d8ca ("bpf,lbmap: Change backend ID to uint32")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/lib/common.h
@@ -559,7 +559,7 @@ struct ct_state {
 	__be32 svc_addr;
 	__u32 src_sec_id;
 	__u16 slave;		/* Slave slot number in a legacy service */
-	__u16 backend_id;	/* Backend ID in lb4_backends */
+	__u32 backend_id;	/* Points to cilium_lb{4,6}_backends */
 };
 
 /* ep_config corresponds to the EndpointConfig object in pkg/maps/configmap. */

bpf/lib/lb.h
@@ -65,7 +65,7 @@ struct bpf_elf_map __section_maps LB6_RR_SEQ_MAP_V2 = {
 
 struct bpf_elf_map __section_maps LB6_BACKEND_MAP = {
 	.type           = BPF_MAP_TYPE_HASH,
-	.size_key       = sizeof(__u16),
+	.size_key       = sizeof(__u32),
 	.size_value     = sizeof(struct lb6_backend),
 	.pinning        = PIN_GLOBAL_NS,
 	.max_elem       = CILIUM_LB_MAP_MAX_ENTRIES,
@@ -124,7 +124,7 @@ struct bpf_elf_map __section_maps LB4_RR_SEQ_MAP_V2 = {
 
 struct bpf_elf_map __section_maps LB4_BACKEND_MAP = {
 	.type           = BPF_MAP_TYPE_HASH,
-	.size_key       = sizeof(__u16),
+	.size_key       = sizeof(__u32),
 	.size_value     = sizeof(struct lb4_backend),
 	.pinning        = PIN_GLOBAL_NS,
 	.max_elem       = CILIUM_LB_MAP_MAX_ENTRIES,
@@ -503,13 +503,13 @@ struct lb6_service_v2 *lb6_lookup_service_v2(struct __sk_buff *skb,
 	return svc;
 }
 
-static inline struct lb6_backend *__lb6_lookup_backend(__u16 backend_id)
+static inline struct lb6_backend *__lb6_lookup_backend(__u32 backend_id)
 {
 	return map_lookup_elem(&LB6_BACKEND_MAP, &backend_id);
 }
 
 static inline struct lb6_backend *lb6_lookup_backend(struct __sk_buff *skb,
-						     __u16 backend_id)
+						     __u32 backend_id)
 {
 	struct lb6_backend *backend;
 
@@ -935,13 +935,13 @@ struct lb4_service_v2 *lb4_lookup_service_v2(struct __sk_buff *skb,
 	return svc;
 }
 
-static inline struct lb4_backend *__lb4_lookup_backend(__u16 backend_id)
+static inline struct lb4_backend *__lb4_lookup_backend(__u32 backend_id)
 {
 	return map_lookup_elem(&LB4_BACKEND_MAP, &backend_id);
 }
 
 static inline struct lb4_backend *lb4_lookup_backend(struct __sk_buff *skb,
-						     __u16 backend_id)
+						     __u32 backend_id)
 {
 	struct lb4_backend *backend;
 

cilium/cmd/bpf_lb_list.go
@@ -20,7 +20,6 @@ import (
 	"github.com/cilium/cilium/common"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/command"
-	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
 
 	"github.com/spf13/cobra"
@@ -49,7 +48,7 @@ func dumpRevNat(serviceList map[string][]string) {
 func dumpSVC(serviceList map[string][]string) {
 	// It's safe to use the same map for both IPv4 and IPv6, as backend
 	// IDs are allocated from the same pool regardless the protocol
-	backendMap := make(map[loadbalancer.BackendID]lbmap.BackendValue)
+	backendMap := make(map[uint32]lbmap.BackendValue)
 
 	parseBackendEntry := func(key bpf.MapKey, value bpf.MapValue) {
 		id := key.(lbmap.BackendKey).GetID()

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=7ebc17568b23485111ee20954f74149e0547feca
+GO_BINDATA_SHA1SUM=936ecd31fc2c0cbf5e0682199d90340dccc758fa
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

pkg/loadbalancer/loadbalancer.go
@@ -61,7 +61,7 @@ type FEPortName string
 type ServiceID uint16
 
 // BackendID is the backend's ID.
-type BackendID uint16
+type BackendID uint32
 
 // ID is the ID of L3n4Addr endpoint (either service or backend).
 type ID uint32

pkg/maps/lbmap/ipv4.go
@@ -22,7 +22,6 @@ import (
 	"github.com/cilium/cilium/common/types"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
-	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/u8proto"
 )
 
@@ -380,9 +379,9 @@ type Service4ValueV2 struct {
 	Pad       uint16
 }
 
-func NewService4ValueV2(count uint16, backendID loadbalancer.BackendID, revNat uint16, weight uint16) *Service4ValueV2 {
+func NewService4ValueV2(count uint16, backendID uint32, revNat uint16, weight uint16) *Service4ValueV2 {
 	svc := Service4ValueV2{
-		BackendID: uint32(backendID),
+		BackendID: backendID,
 		Count:     count,
 		RevNat:    revNat,
 		Weight:    weight,
@@ -405,11 +404,11 @@ func (s *Service4ValueV2) SetWeight(weight uint16) { s.Weight = weight }
 func (s *Service4ValueV2) GetWeight() uint16       { return s.Weight }
 func (s *Service4ValueV2) RevNatKey() RevNatKey    { return &RevNat4Key{s.RevNat} }
 
-func (s *Service4ValueV2) SetBackendID(id loadbalancer.BackendID) {
-	s.BackendID = uint32(id)
+func (s *Service4ValueV2) SetBackendID(id uint32) {
+	s.BackendID = id
 }
-func (s *Service4ValueV2) GetBackendID() loadbalancer.BackendID {
-	return loadbalancer.BackendID(s.BackendID)
+func (s *Service4ValueV2) GetBackendID() uint32 {
+	return s.BackendID
 }
 
 func (s *Service4ValueV2) ToNetwork() ServiceValueV2 {
@@ -422,19 +421,19 @@ func (s *Service4ValueV2) ToNetwork() ServiceValueV2 {
 // +k8s:deepcopy-gen=true
 // +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
 type Backend4Key struct {
-	ID loadbalancer.BackendID
+	ID uint32
 }
 
-func NewBackend4Key(id loadbalancer.BackendID) *Backend4Key {
+func NewBackend4Key(id uint32) *Backend4Key {
 	return &Backend4Key{ID: id}
 }
 
-func (k *Backend4Key) String() string                  { return fmt.Sprintf("%d", k.ID) }
-func (k *Backend4Key) GetKeyPtr() unsafe.Pointer       { return unsafe.Pointer(k) }
-func (k *Backend4Key) NewValue() bpf.MapValue          { return &Backend4Value{} }
-func (k *Backend4Key) Map() *bpf.Map                   { return Backend4Map }
-func (k *Backend4Key) SetID(id loadbalancer.BackendID) { k.ID = id }
-func (k *Backend4Key) GetID() loadbalancer.BackendID   { return k.ID }
+func (k *Backend4Key) String() string            { return fmt.Sprintf("%d", k.ID) }
+func (k *Backend4Key) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+func (k *Backend4Key) NewValue() bpf.MapValue    { return &Backend4Value{} }
+func (k *Backend4Key) Map() *bpf.Map             { return Backend4Map }
+func (k *Backend4Key) SetID(id uint32)           { k.ID = id }
+func (k *Backend4Key) GetID() uint32             { return k.ID }
 
 // Backend4Value must match 'struct lb4_backend' in "bpf/lib/common.h".
 // +k8s:deepcopy-gen=true
@@ -485,7 +484,7 @@ type Backend4 struct {
 	Value *Backend4Value
 }
 
-func NewBackend4(id loadbalancer.BackendID, ip net.IP, port uint16, proto u8proto.U8proto) (*Backend4, error) {
+func NewBackend4(id uint32, ip net.IP, port uint16, proto u8proto.U8proto) (*Backend4, error) {
 	val, err := NewBackend4Value(ip, port, proto)
 	if err != nil {
 		return nil, err
@@ -497,8 +496,8 @@ func NewBackend4(id loadbalancer.BackendID, ip net.IP, port uint16, proto u8prot
 	}, nil
 }
 
-func (b *Backend4) IsIPv6() bool                  { return false }
-func (b *Backend4) Map() *bpf.Map                 { return Backend4Map }
-func (b *Backend4) GetID() loadbalancer.BackendID { return b.Key.GetID() }
-func (b *Backend4) GetKey() bpf.MapKey            { return b.Key }
-func (b *Backend4) GetValue() BackendValue        { return b.Value }
+func (b *Backend4) IsIPv6() bool           { return false }
+func (b *Backend4) Map() *bpf.Map          { return Backend4Map }
+func (b *Backend4) GetID() uint32          { return b.Key.GetID() }
+func (b *Backend4) GetKey() bpf.MapKey     { return b.Key }
+func (b *Backend4) GetValue() BackendValue { return b.Value }

pkg/maps/lbmap/ipv6.go
@@ -22,7 +22,6 @@ import (
 	"github.com/cilium/cilium/common/types"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
-	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/u8proto"
 )
 
@@ -366,7 +365,7 @@ type Service6ValueV2 struct {
 	Pad       uint16
 }
 
-func NewService6ValueV2(count uint16, backendID loadbalancer.BackendID, revNat uint16, weight uint16) *Service6ValueV2 {
+func NewService6ValueV2(count uint16, backendID uint32, revNat uint16, weight uint16) *Service6ValueV2 {
 	svc := Service6ValueV2{
 		Count:     count,
 		BackendID: uint32(backendID),
@@ -391,11 +390,11 @@ func (s *Service6ValueV2) SetWeight(weight uint16) { s.Weight = weight }
 func (s *Service6ValueV2) GetWeight() uint16       { return s.Weight }
 func (s *Service6ValueV2) RevNatKey() RevNatKey    { return &RevNat6Key{s.RevNat} }
 
-func (s *Service6ValueV2) SetBackendID(id loadbalancer.BackendID) {
+func (s *Service6ValueV2) SetBackendID(id uint32) {
 	s.BackendID = uint32(id)
 }
-func (s *Service6ValueV2) GetBackendID() loadbalancer.BackendID {
-	return loadbalancer.BackendID(s.BackendID)
+func (s *Service6ValueV2) GetBackendID() uint32 {
+	return s.BackendID
 }
 
 func (s *Service6ValueV2) ToNetwork() ServiceValueV2 {
@@ -408,19 +407,19 @@ func (s *Service6ValueV2) ToNetwork() ServiceValueV2 {
 // +k8s:deepcopy-gen=true
 // +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
 type Backend6Key struct {
-	ID loadbalancer.BackendID
+	ID uint32
 }
 
-func NewBackend6Key(id loadbalancer.BackendID) *Backend6Key {
+func NewBackend6Key(id uint32) *Backend6Key {
 	return &Backend6Key{ID: id}
 }
 
-func (k *Backend6Key) String() string                  { return fmt.Sprintf("%d", k.ID) }
-func (k *Backend6Key) GetKeyPtr() unsafe.Pointer       { return unsafe.Pointer(k) }
-func (k *Backend6Key) NewValue() bpf.MapValue          { return &Backend6Value{} }
-func (k *Backend6Key) Map() *bpf.Map                   { return Backend6Map }
-func (k *Backend6Key) SetID(id loadbalancer.BackendID) { k.ID = id }
-func (k *Backend6Key) GetID() loadbalancer.BackendID   { return k.ID }
+func (k *Backend6Key) String() string            { return fmt.Sprintf("%d", k.ID) }
+func (k *Backend6Key) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+func (k *Backend6Key) NewValue() bpf.MapValue    { return &Backend6Value{} }
+func (k *Backend6Key) Map() *bpf.Map             { return Backend6Map }
+func (k *Backend6Key) SetID(id uint32)           { k.ID = id }
+func (k *Backend6Key) GetID() uint32             { return k.ID }
 
 // Backend6Value must match 'struct lb6_backend' in "bpf/lib/common.h".
 // +k8s:deepcopy-gen=true
@@ -471,7 +470,7 @@ type Backend6 struct {
 	Value *Backend6Value
 }
 
-func NewBackend6(id loadbalancer.BackendID, ip net.IP, port uint16, proto u8proto.U8proto) (*Backend6, error) {
+func NewBackend6(id uint32, ip net.IP, port uint16, proto u8proto.U8proto) (*Backend6, error) {
 	val, err := NewBackend6Value(ip, port, proto)
 	if err != nil {
 		return nil, err
@@ -483,8 +482,8 @@ func NewBackend6(id loadbalancer.BackendID, ip net.IP, port uint16, proto u8prot
 	}, nil
 }
 
-func (b *Backend6) IsIPv6() bool                  { return true }
-func (b *Backend6) Map() *bpf.Map                 { return Backend6Map }
-func (b *Backend6) GetID() loadbalancer.BackendID { return b.Key.GetID() }
-func (b *Backend6) GetKey() bpf.MapKey            { return b.Key }
-func (b *Backend6) GetValue() BackendValue        { return b.Value }
+func (b *Backend6) IsIPv6() bool           { return true }
+func (b *Backend6) Map() *bpf.Map          { return Backend6Map }
+func (b *Backend6) GetID() uint32          { return b.Key.GetID() }
+func (b *Backend6) GetKey() bpf.MapKey     { return b.Key }
+func (b *Backend6) GetValue() BackendValue { return b.Value }

pkg/maps/lbmap/lbmap.go
@@ -357,10 +357,10 @@ func updateBackendsLocked(addedBackends map[loadbalancer.BackendID]ServiceValue)
 
 		if svcVal.IsIPv6() {
 			svc6Val := svcVal.(*Service6Value)
-			b, err = NewBackend6(backendID, svc6Val.Address.IP(), svc6Val.Port, u8proto.All)
+			b, err = NewBackend6(uint32(backendID), svc6Val.Address.IP(), svc6Val.Port, u8proto.All)
 		} else {
 			svc4Val := svcVal.(*Service4Value)
-			b, err = NewBackend4(backendID, svc4Val.Address.IP(), svc4Val.Port, u8proto.All)
+			b, err = NewBackend4(uint32(backendID), svc4Val.Address.IP(), svc4Val.Port, u8proto.All)
 		}
 		if err != nil {
 			return err
@@ -458,7 +458,7 @@ func updateServiceV2Locked(fe ServiceKey, backends map[BackendAddrID]ServiceValu
 			svcValV2.SetCount(legacySlaveSlot) // For the backward-compatibility
 		}
 		backendID := cache.getBackendIDByAddrID(addrID)
-		svcValV2.SetBackendID(backendID)
+		svcValV2.SetBackendID(uint32(backendID))
 		svcValV2.SetRevNat(revNATID)
 		svcValV2.SetWeight(svcVal.GetWeight())
 		svcKeyV2.SetSlave(slot)
@@ -526,7 +526,7 @@ func removeBackendsLocked(removedBackendIDs []loadbalancer.BackendID, isIPv6 boo
 	}
 
 	for _, backendID := range removedBackendIDs {
-		backendKey.SetID(backendID)
+		backendKey.SetID(uint32(backendID))
 		if err := deleteBackendLocked(backendKey); err != nil {
 			return fmt.Errorf("Unable to delete backend with ID %d: %s", backendID, err)
 		}
@@ -632,7 +632,8 @@ func DumpServiceMapsToUserspaceV2() (loadbalancer.SVCMap, []*loadbalancer.LBSVC,
 	parseBackendEntries := func(key bpf.MapKey, value bpf.MapValue) {
 		backendKey := key.(BackendKey)
 		backendValue := value.DeepCopyMapValue().(BackendValue)
-		backendValueMap[backendKey.GetID()] = backendValue
+		backendValueMap[loadbalancer.BackendID(backendKey.GetID())] =
+			backendValue
 	}
 
 	parseSVCEntries := func(key bpf.MapKey, value bpf.MapValue) {
@@ -644,7 +645,7 @@ func DumpServiceMapsToUserspaceV2() (loadbalancer.SVCMap, []*loadbalancer.LBSVC,
 			return
 		}
 
-		backendID := svcValue.GetBackendID()
+		backendID := loadbalancer.BackendID(svcValue.GetBackendID())
 
 		scopedLog := log.WithFields(logrus.Fields{
 			logfields.BPFMapKey:   svcKey,
@@ -725,7 +726,8 @@ func DumpBackendMapsToUserspace() (map[BackendAddrID]*loadbalancer.LBBackEnd, er
 		// is a value.
 		backendKey := key.(BackendKey)
 		backendValue := value.DeepCopyMapValue().(BackendValue)
-		backendValueMap[backendKey.GetID()] = backendValue
+		backendValueMap[loadbalancer.BackendID(backendKey.GetID())] =
+			backendValue
 	}
 
 	if option.Config.EnableIPv4 {
@@ -941,7 +943,7 @@ func DeleteServiceV2(svc loadbalancer.L3n4AddrID, releaseBackendID func(loadbala
 	}
 
 	for _, id := range backendsToRemove {
-		backendKey.SetID(id)
+		backendKey.SetID(uint32(id))
 		if err := deleteBackendLocked(backendKey); err != nil {
 			return fmt.Errorf("Unable to delete backend with ID %d: %s", id, err)
 		}
@@ -1049,9 +1051,9 @@ func DeleteOrphanBackends(releaseBackendID func(loadbalancer.BackendID)) []error
 	for addrID, id := range toRemove {
 		log.WithField(logfields.BackendID, id).Debug("Removing orphan backend")
 		if addrID.IsIPv6() {
-			key = NewBackend6Key(id)
+			key = NewBackend6Key(uint32(id))
 		} else {
-			key = NewBackend4Key(id)
+			key = NewBackend4Key(uint32(id))
 		}
 		if err := deleteBackendLocked(key); err != nil {
 			errors = append(errors,

pkg/maps/lbmap/types.go
@@ -171,10 +171,10 @@ type ServiceValueV2 interface {
 	GetWeight() uint16
 
 	// Set backend identifier
-	SetBackendID(id loadbalancer.BackendID)
+	SetBackendID(id uint32)
 
 	// Get backend identifier
-	GetBackendID() loadbalancer.BackendID
+	GetBackendID() uint32
 
 	// Returns a RevNatKey matching a ServiceValue
 	RevNatKey() RevNatKey
@@ -191,10 +191,10 @@ type BackendKey interface {
 	Map() *bpf.Map
 
 	// Set backend identifier
-	SetID(loadbalancer.BackendID)
+	SetID(uint32)
 
 	// Get backend identifier
-	GetID() loadbalancer.BackendID
+	GetID() uint32
 }
 
 // BackendValue is the interface describing protocol independent backend value.
@@ -223,7 +223,7 @@ type Backend interface {
 	Map() *bpf.Map
 
 	// Get backend identifier
-	GetID() loadbalancer.BackendID
+	GetID() uint32
 
 	// Get key of the backend entry
 	GetKey() bpf.MapKey
@@ -460,7 +460,7 @@ func LBSVC2ServiceKeynValuenBackendV2(svc *loadbalancer.LBSVC) (ServiceKeyV2, []
 
 		svcValue.SetRevNat(int(svc.FE.ID))
 		svcValue.SetWeight(be.Weight)
-		svcValue.SetBackendID(loadbalancer.BackendID(be.ID))
+		svcValue.SetBackendID(uint32(be.ID))
 
 		backends = append(backends, backend)
 		svcValues = append(svcValues, svcValue)
@@ -529,8 +529,8 @@ func l3n4Addr2ServiceKeyV2(l3n4Addr loadbalancer.L3n4AddrID) ServiceKeyV2 {
 
 func lbBackEnd2Backend(be loadbalancer.LBBackEnd) (Backend, error) {
 	if be.IsIPv6() {
-		return NewBackend6(loadbalancer.BackendID(be.ID), be.IP, be.Port, u8proto.All)
+		return NewBackend6(uint32(be.ID), be.IP, be.Port, u8proto.All)
 	}
 
-	return NewBackend4(loadbalancer.BackendID(be.ID), be.IP, be.Port, u8proto.All)
+	return NewBackend4(uint32(be.ID), be.IP, be.Port, u8proto.All)
 }