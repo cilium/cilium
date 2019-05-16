3c868d0035c96ffdaf35072a1f0e210a76843f1c
André Martins <andre@cilium.io>
2019-05-16 22:55:51 +0200

    Gopkg: update cilium/proxy
    [ upstream commit f27130849a88ec74f1397705c659481de63c0509 ]
    
    The dependencies are now vendored in the cilium/proxy repository. Cilium
    will import the right dependencies for the cilium/proxy based in the
    Gopkg.toml of that directory.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- vendor/github.com/cilium/proxy/go/cilium/api/npds.pb.go
+++ vendor/github.com/cilium/proxy/go/cilium/api/npds.pb.go
@@ -13,6 +13,8 @@ import (
 	_ "github.com/lyft/protoc-gen-validate/validate"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -751,6 +753,17 @@ func (c *networkPolicyDiscoveryServiceClient) FetchNetworkPolicies(ctx context.C
 type NetworkPolicyDiscoveryServiceServer interface {
 	StreamNetworkPolicies(NetworkPolicyDiscoveryService_StreamNetworkPoliciesServer) error
 	FetchNetworkPolicies(context.Context, *v2.DiscoveryRequest) (*v2.DiscoveryResponse, error)
+}
+
+// UnimplementedNetworkPolicyDiscoveryServiceServer can be embedded to have forward compatible implementations.
+type UnimplementedNetworkPolicyDiscoveryServiceServer struct {
+}
+
+func (*UnimplementedNetworkPolicyDiscoveryServiceServer) StreamNetworkPolicies(srv NetworkPolicyDiscoveryService_StreamNetworkPoliciesServer) error {
+	return status.Errorf(codes.Unimplemented, "method StreamNetworkPolicies not implemented")
+}
+func (*UnimplementedNetworkPolicyDiscoveryServiceServer) FetchNetworkPolicies(ctx context.Context, req *v2.DiscoveryRequest) (*v2.DiscoveryResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FetchNetworkPolicies not implemented")
 }
 
 func RegisterNetworkPolicyDiscoveryServiceServer(s *grpc.Server, srv NetworkPolicyDiscoveryServiceServer) {
