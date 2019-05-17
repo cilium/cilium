e1b80d812667f1fcab25ba49b5c963fb4c95d7c7
Thomas Graf <thomas@cilium.io>
2019-05-17 02:31:49 +0200

    bpf: Remove several debug messages
    Remove several unneeded debug messages to reduce the number of instructions
    with debugging enabled. This serves as a workaround until instruction size can
    be reduced otherwise.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -222,8 +222,6 @@ skip_service_lookup:
 		if (IS_ERR(ret))
 			return ret;
 
-		cilium_dbg(skb, DBG_TO_HOST, skb->cb[CB_POLICY], 0);
-
 		ret = ipv6_l3(skb, l3_off, (__u8 *) &router_mac.addr, (__u8 *) &host_mac.addr, METRIC_EGRESS);
 		if (ret != TC_ACT_OK)
 			return ret;
@@ -298,8 +296,6 @@ to_host:
 	if (is_defined(ENABLE_HOST_REDIRECT)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 
-		cilium_dbg(skb, DBG_TO_HOST, is_policy_skip(skb), 0);
-
 		ret = ipv6_l3(skb, l3_off, (__u8 *) &router_mac.addr, (__u8 *) &host_mac.addr, METRIC_EGRESS);
 		if (ret != TC_ACT_OK)
 			return ret;
@@ -312,8 +308,6 @@ to_host:
 	}
 
 pass_to_stack:
-	cilium_dbg(skb, DBG_TO_STACK, 0, 0);
-
 	ret = ipv6_l3(skb, l3_off, NULL, (__u8 *) &router_mac.addr, METRIC_EGRESS);
 	if (unlikely(ret != TC_ACT_OK))
 		return ret;
@@ -533,8 +527,6 @@ skip_service_lookup:
 		if (!revalidate_data(skb, &data, &data_end, &ip4))
 			return DROP_INVALID;
 
-		cilium_dbg(skb, DBG_TO_HOST, skb->cb[CB_POLICY], 0);
-
 		ret = ipv4_l3(skb, l3_off, (__u8 *) &router_mac.addr, (__u8 *) &host_mac.addr, ip4);
 		if (ret != TC_ACT_OK)
 			return ret;
@@ -601,8 +593,6 @@ to_host:
 	if (is_defined(ENABLE_HOST_REDIRECT)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 
-		cilium_dbg(skb, DBG_TO_HOST, is_policy_skip(skb), 0);
-
 		ret = ipv4_l3(skb, l3_off, (__u8 *) &router_mac.addr, (__u8 *) &host_mac.addr, ip4);
 		if (ret != TC_ACT_OK)
 			return ret;
@@ -619,8 +609,6 @@ to_host:
 	}
 
 pass_to_stack:
-	cilium_dbg(skb, DBG_TO_STACK, 0, 0);
-
 	ret = ipv4_l3(skb, l3_off, NULL, (__u8 *) &router_mac.addr, ip4);
 	if (unlikely(ret != TC_ACT_OK))
 		return ret;
@@ -975,8 +963,6 @@ ipv4_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 						 *forwarding_reason, monitor);
 		if (IS_ERR(ret))
 			return ret;
-
-		cilium_dbg(skb, DBG_TO_HOST, is_policy_skip(skb), 0);
 
 		if (eth_store_saddr(skb, (__u8 *) &router_mac.addr, 0) < 0)
 			return DROP_WRITE_ERROR;
