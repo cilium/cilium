cilium/cilium

commit 680b2ee4b96c335c8873301b869100a0614fc00c

kvstore: Wait for kvstore to reach quorum
So far, the channel returned by the Connected() function of the kvstore client
was closed when the kvstore could be connected to. This did not guarantee that
the kvstore actually had quorum and operations could still block and timeout
afterwards. Wait for a distributed lock to be acquired to identify the moment
quorum has been reached.

Also indicate the quorum status in the kvstore status of `cilium status` for
etcd.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/kvstore/etcd.go
@@ -289,34 +289,72 @@ func (e *etcdClient) closeSession(leaseID client.LeaseID) {
 	e.RWMutex.RUnlock()
 }
 
+func (e *etcdClient) waitForInitLock(ctx context.Context) <-chan bool {
+	initLockSucceeded := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				initLockSucceeded <- false
+				close(initLockSucceeded)
+				return
+			default:
+			}
+
+			locker, err := e.LockPath(ctx, InitLockPath)
+			if err == nil {
+				initLockSucceeded <- true
+				close(initLockSucceeded)
+				locker.Unlock()
+				e.getLogger().Info("Distributed lock successful, etcd has quorum")
+				return
+			}
+
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+	return initLockSucceeded
+}
+
+func (e *etcdClient) isConnectedAndHasQuorum() bool {
+	ctxTimeout, cancel := ctx.WithTimeout(ctx.TODO(), statusCheckTimeout)
+	defer cancel()
+
+	select {
+	// Wait for the the initial connection to be established
+	case <-e.firstSession:
+	// Timeout while waiting for initial connection, no success
+	case <-ctxTimeout.Done():
+		return false
+	}
+
+	e.RLock()
+	ch := e.session.Done()
+	e.RUnlock()
+
+	initLockSucceeded := e.waitForInitLock(ctxTimeout)
+	select {
+	// Catch disconnect event, no success
+	case <-ch:
+		return false
+	// wait for initial lock to succeed
+	case success := <-initLockSucceeded:
+		return success
+	}
+}
+
 // Connected closes the returned channel when the etcd client is connected.
 func (e *etcdClient) Connected() <-chan struct{} {
-	select {
-	case <-e.firstSession:
-		out := make(chan struct{})
-		go func() {
-		getSession:
-			e.RLock()
-			ch := e.session.Done()
-			e.RUnlock()
-			select {
-			case <-ch:
-				// this case means we are disconnected as the e.session.Done() was
-				// closed. We will polling until we have a new etcd session and
-				// we are connected to the kvstore.
-				time.Sleep(100 * time.Millisecond)
-				goto getSession
-			default:
-				close(out)
-			}
-		}()
-		return out
-	default:
-		// We assume when the e.firstSession is closed that we have connectivity
-		// with the kvstore as this channel is closed when we have the first
-		// session.
-		return e.firstSession
-	}
+	out := make(chan struct{})
+	go func() {
+		for !e.isConnectedAndHasQuorum() {
+			time.Sleep(100 * time.Millisecond)
+		}
+		close(out)
+	}()
+	return out
 }
 
 // Disconnected closes the returned channel when the etcd client is
@@ -694,6 +732,8 @@ func (e *etcdClient) statusChecker() {
 		newStatus := []string{}
 		ok := 0
 
+		hasQuorum := e.isConnectedAndHasQuorum()
+
 		endpoints := e.client.Endpoints()
 		for _, ep := range endpoints {
 			st, err := e.determineEndpointStatus(ep)
@@ -707,7 +747,7 @@ func (e *etcdClient) statusChecker() {
 		allConnected := len(endpoints) == ok
 
 		e.statusLock.Lock()
-		e.latestStatusSnapshot = fmt.Sprintf("etcd: %d/%d connected: %s", ok, len(endpoints), strings.Join(newStatus, "; "))
+		e.latestStatusSnapshot = fmt.Sprintf("etcd: %d/%d connected, has-quorum=%t: %s", ok, len(endpoints), hasQuorum, strings.Join(newStatus, "; "))
 
 		// Only mark the etcd health as unstable if no etcd endpoints can be reached
 		if len(endpoints) > 0 && ok == 0 {

pkg/kvstore/consul.go
@@ -373,6 +373,26 @@ func (c *consulClient) Watch(w *Watcher) {
 	}
 }
 
+func (c *consulClient) waitForInitLock() <-chan struct{} {
+	initLockSucceeded := make(chan struct{})
+
+	go func() {
+		for {
+			locker, err := c.LockPath(context.TODO(), InitLockPath)
+			if err == nil {
+				close(initLockSucceeded)
+				locker.Unlock()
+				log.Info("Distributed lock successful, consul has quorum")
+				return
+			}
+
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+	return initLockSucceeded
+}
+
 // Connected closes the returned channel when the etcd client is connected.
 func (c *consulClient) Connected() <-chan struct{} {
 	ch := make(chan struct{})
@@ -385,6 +405,7 @@ func (c *consulClient) Connected() <-chan struct{} {
 			}
 			time.Sleep(100 * time.Millisecond)
 		}
+		<-c.waitForInitLock()
 		close(ch)
 	}()
 	return ch

pkg/kvstore/kvstore.go
@@ -35,6 +35,9 @@ const (
 
 	// BaseKeyPrefix is the base prefix that should be used for all keys
 	BaseKeyPrefix = "cilium"
+
+	// InitLockPath is the path to the init lock to test quorum
+	InitLockPath = BaseKeyPrefix + "/.initlock"
 )
 
 // Get returns value of key

commit 7b8d24c6d0726c9e4a07da7f81e7d37f7395c996

pkg/kvstore: acquire a random initlock
If all cilium agents are trying to acquire the same lock they can return
a wrong perception that etcd does not have quorum but in reality it can
be the fact that one client got killed while it was holding the lock and
its lease hasn't expire yet. Making all agents to acquire a random path
will make it more unlikely for 2 agents to acquire the same path.

Fixes: 680b2ee4b96c ("kvstore: Wait for kvstore to reach quorum")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/etcd.go
@@ -18,6 +18,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"math/rand"
 	"net/url"
 	"os"
 	"strconv"
@@ -49,6 +50,10 @@ const (
 	// EtcdRateLimitOption specifies maximum kv operations per second
 	EtcdRateLimitOption = "etcd.qps"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 type etcdModule struct {
 	opts   backendOptions
@@ -302,12 +307,15 @@ func (e *etcdClient) waitForInitLock(ctx context.Context) <-chan bool {
 			default:
 			}
 
-			locker, err := e.LockPath(ctx, InitLockPath)
+			// Generate a random number so that we can acquire a lock even
+			// if other agents are killed while locking this path.
+			randNumber := strconv.FormatUint(rand.Uint64(), 16)
+			locker, err := e.LockPath(ctx, InitLockPath+"/"+randNumber)
 			if err == nil {
 				initLockSucceeded <- true
 				close(initLockSucceeded)
 				locker.Unlock()
-				e.getLogger().Info("Distributed lock successful, etcd has quorum")
+				e.getLogger().Debug("Distributed lock successful, etcd has quorum")
 				return
 			}
 