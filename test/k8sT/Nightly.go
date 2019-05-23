4ffe9b1f4815f36e0ec51bb9396a15c3fc865120
Ray Bejjani <ray@covalent.io>
2019-05-23 17:19:19 +0200

    CI: Add WaitForDaemonSetReady & ExpectDaemonSetReady
    WaitForDaemonSetReady uses the k8s expected pods to determine that pods
    are ready. This sidesteps passing a count to WaitforNPods or passing 0
    to cause it to determine the number of pods from those currently
    running.
    
    Utility functions changed:
    - ExpectCiliumPreFlightInstallReady
    - ExpectCiliumOperatorReady
    - ExpectCiliumRunning
    - ExpectETCDOperatorReady
    
    Tests changed:
    - K8sChaosTest
    - NightlyEpsMeasurement
    - K8sServicesTest
    
    Signed-off-by: Ray Bejjani <ray@covalent.io>

--- test/k8sT/Nightly.go
+++ test/k8sT/Nightly.go
@@ -295,6 +295,7 @@ var _ = Describe("NightlyEpsMeasurement", func() {
 			kubectl.ValidateNoErrorsInLogs(CurrentGinkgoTestDescription().Duration)
 			manifest := helpers.ManifestGet(netcatDsManifest)
 			kubectl.Apply(manifest).ExpectSuccess("Cannot apply netcat ds")
+			ExpectDaemonSetReady(kubectl, helpers.DefaultNamespace, "netcatds", helpers.HelperTimeout)
 			defer kubectl.Delete(manifest)
 			testConnectivity()
 		})
@@ -302,6 +303,7 @@ var _ = Describe("NightlyEpsMeasurement", func() {
 		It("Test TCP Keepalive without L7 Policy", func() {
 			manifest := helpers.ManifestGet(netcatDsManifest)
 			kubectl.Apply(manifest).ExpectSuccess("Cannot apply netcat ds")
+			ExpectDaemonSetReady(kubectl, helpers.DefaultNamespace, "netcatds", helpers.HelperTimeout)
 			defer kubectl.Delete(manifest)
 			kubectl.Exec(fmt.Sprintf(
 				"%s delete --all cnp -n %s", helpers.KubectlCmd, helpers.DefaultNamespace))
