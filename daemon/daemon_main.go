cilium/cilium

commit e99bee54869bb078b200322c62cddfe36954b027

agent: Support reading CNI configuration from agent to set per node settings
This will be used for the upcoming ENI integration to allow specifying per node
configuration via the CNI configuration which is already templated in a lot of
environments. For now, allow setting the MTU as this field was already defined
although unused so far.

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/daemon_main.go
@@ -65,6 +65,7 @@ import (
 	"github.com/cilium/cilium/pkg/version"
 	"github.com/cilium/cilium/pkg/versioncheck"
 	"github.com/cilium/cilium/pkg/workloads"
+	cnitypes "github.com/cilium/cilium/plugins/cilium-cni/types"
 
 	"github.com/go-openapi/loads"
 	gops "github.com/google/gops/agent"
@@ -642,6 +643,9 @@ func init() {
 	flags.String(option.IPv4NodeAddr, "auto", "IPv4 address of node")
 	option.BindEnv(option.IPv4NodeAddr)
 
+	flags.String(option.ReadCNIConfiguration, "", "Read to the CNI configuration at specified path to extract per node configuration")
+	option.BindEnv(option.ReadCNIConfiguration)
+
 	flags.Bool(option.Restore, true, "Restores state, if possible, from previous daemon")
 	option.BindEnv(option.Restore)
 
@@ -891,6 +895,17 @@ func initEnv(cmd *cobra.Command) {
 
 	if option.Config.PProf {
 		pprof.Enable()
+	}
+
+	if option.Config.ReadCNIConfiguration != "" {
+		netConf, _, err := cnitypes.ReadNetConf(option.Config.ReadCNIConfiguration)
+		if err != nil {
+			log.WithError(err).Fatal("Unable to read CNI configuration")
+		}
+
+		if netConf.MTU != 0 {
+			option.Config.MTU = netConf.MTU
+		}
 	}
 
 	if option.Config.PreAllocateMaps {

plugins/cilium-cni/types/types.go
@@ -17,6 +17,7 @@ package types
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net"
 
 	cniTypes "github.com/containernetworking/cni/pkg/types"
@@ -29,6 +30,17 @@ type NetConf struct {
 	cniTypes.NetConf
 	MTU  int  `json:"mtu"`
 	Args Args `json:"args"`
+}
+
+// ReadNetConf reads a CNI configuration file and returns the corresponding
+// NetConf structure
+func ReadNetConf(path string) (*NetConf, string, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, "", fmt.Errorf("Unable to read CNI configuration '%s': %s", path, err)
+	}
+
+	return LoadNetConf(b)
 }
 
 // LoadNetConf unmarshals a Cilium network configuration from JSON and returns

plugins/cilium-cni/types/types_test.go
@@ -1,0 +1,109 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package types
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/cilium/cilium/pkg/checker"
+
+	cnitypes "github.com/containernetworking/cni/pkg/types"
+	"gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+type CNITypesSuite struct{}
+
+var _ = check.Suite(&CNITypesSuite{})
+
+func testConfRead(c *check.C, confContent string, netconf *NetConf) {
+	dir, err := ioutil.TempDir("", "cilium-cnitype-testsuite")
+	c.Assert(err, check.IsNil)
+	defer os.RemoveAll(dir)
+
+	p := path.Join(dir, "conf1")
+	err = ioutil.WriteFile(p, []byte(confContent), 0644)
+	c.Assert(err, check.IsNil)
+
+	netConf, _, err := ReadNetConf(p)
+	c.Assert(err, check.IsNil)
+
+	c.Assert(netConf, checker.DeepEquals, netconf)
+}
+
+func (t *CNITypesSuite) TestReadCNIConf(c *check.C) {
+	confFile1 := `
+{
+  "name": "cilium",
+  "type": "cilium-cni"
+}
+`
+
+	netConf1 := NetConf{
+		NetConf: cnitypes.NetConf{
+			Name: "cilium",
+			Type: "cilium-cni",
+		},
+	}
+	testConfRead(c, confFile1, &netConf1)
+
+	confFile2 := `
+{
+  "name": "cilium",
+  "type": "cilium-cni",
+  "mtu": 9000
+}
+`
+
+	netConf2 := NetConf{
+		NetConf: cnitypes.NetConf{
+			Name: "cilium",
+			Type: "cilium-cni",
+		},
+		MTU: 9000,
+	}
+	testConfRead(c, confFile2, &netConf2)
+}
+
+func (t *CNITypesSuite) TestReadCNIConfError(c *check.C) {
+	// Try to read errorneous CNI configuration file with MTU provided as
+	// string instead of int
+	errorConf := `
+{
+  "name": "cilium",
+  "type": "cilium-cni",
+  "mtu": "9000"
+}
+`
+
+	dir, err := ioutil.TempDir("", "cilium-cnitype-testsuite")
+	c.Assert(err, check.IsNil)
+	defer os.RemoveAll(dir)
+
+	p := path.Join(dir, "errorconf")
+	err = ioutil.WriteFile(p, []byte(errorConf), 0644)
+	c.Assert(err, check.IsNil)
+
+	_, _, err = ReadNetConf(p)
+	c.Assert(err, check.Not(check.IsNil))
+}

pkg/option/config.go
@@ -540,6 +540,11 @@ const (
 	// ProxyConnectTimeout specifies the time in seconds after which a TCP connection attempt
 	// is considered timed out
 	ProxyConnectTimeout = "proxy-connect-timeout"
+
+	// ReadCNIConfiguration reads the CNI configuration file and extracts
+	// Cilium relevant information. This can be used to pass per node
+	// configuration to Cilium.
+	ReadCNIConfiguration = "read-cni-conf"
 )
 
 // GetTunnelModes returns the list of all tunnel modes
@@ -961,6 +966,11 @@ type DaemonConfig struct {
 
 	// RunMonitorAgent indicates whether to run the monitor agent
 	RunMonitorAgent bool
+
+	// ReadCNIConfiguration reads the CNI configuration file and extracts
+	// Cilium relevant information. This can be used to pass per node
+	// configuration to Cilium.
+	ReadCNIConfiguration string
 }
 
 var (
@@ -1282,6 +1292,7 @@ func (c *DaemonConfig) Populate() {
 	c.PrometheusServeAddr = getPrometheusServerAddr()
 	c.ProxyConnectTimeout = viper.GetInt(ProxyConnectTimeout)
 	c.BlacklistConflictingRoutes = viper.GetBool(BlacklistConflictingRoutes)
+	c.ReadCNIConfiguration = viper.GetString(ReadCNIConfiguration)
 	c.RestoreState = viper.GetBool(Restore)
 	c.RunDir = viper.GetString(StateDir)
 	c.SidecarIstioProxyImage = viper.GetString(SidecarIstioProxyImage)

commit dac7b3f7391bd2b5a93d6a1a67f717a87fa03a6a

cni: Fix incorrect merge of e99bee54 and 43e0c4e2a
When merging 43e0c4e2a and e99bee54, a merge conflict should have been detected
but it was not so both PRs got merged, resulting in code revision that cannot
be compiled.

Fixes: e99bee54869b ("agent: Support reading CNI configuration from agent to set per node settings")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/daemon_main.go
@@ -898,7 +898,7 @@ func initEnv(cmd *cobra.Command) {
 	}
 
 	if option.Config.ReadCNIConfiguration != "" {
-		netConf, _, err := cnitypes.ReadNetConf(option.Config.ReadCNIConfiguration)
+		netConf, err := cnitypes.ReadNetConf(option.Config.ReadCNIConfiguration)
 		if err != nil {
 			log.WithError(err).Fatal("Unable to read CNI configuration")
 		}

plugins/cilium-cni/types/types.go
@@ -34,10 +34,10 @@ type NetConf struct {
 
 // ReadNetConf reads a CNI configuration file and returns the corresponding
 // NetConf structure
-func ReadNetConf(path string) (*NetConf, string, error) {
+func ReadNetConf(path string) (*NetConf, error) {
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, "", fmt.Errorf("Unable to read CNI configuration '%s': %s", path, err)
+		return nil, fmt.Errorf("Unable to read CNI configuration '%s': %s", path, err)
 	}
 
 	return LoadNetConf(b)

plugins/cilium-cni/types/types_test.go
@@ -45,7 +45,7 @@ func testConfRead(c *check.C, confContent string, netconf *NetConf) {
 	err = ioutil.WriteFile(p, []byte(confContent), 0644)
 	c.Assert(err, check.IsNil)
 
-	netConf, _, err := ReadNetConf(p)
+	netConf, err := ReadNetConf(p)
 	c.Assert(err, check.IsNil)
 
 	c.Assert(netConf, checker.DeepEquals, netconf)
@@ -104,6 +104,6 @@ func (t *CNITypesSuite) TestReadCNIConfError(c *check.C) {
 	err = ioutil.WriteFile(p, []byte(errorConf), 0644)
 	c.Assert(err, check.IsNil)
 
-	_, _, err = ReadNetConf(p)
+	_, err = ReadNetConf(p)
 	c.Assert(err, check.Not(check.IsNil))
 }