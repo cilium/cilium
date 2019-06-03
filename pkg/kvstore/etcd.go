cilium/cilium

commit 0259ad0568fdda09895ec3c89715550bc6dddecd

pkg/kvstore: perform update if value or lease are different
When UpdateIfDifferent was introduced, the lease information of the key
was not taken into account. When a cilium-agent was restarted, its lease
would also expire as well, making its keys being deleted after the
lease TTL (roughly 15 minutes) expired. As UpdateIfDifferent was not build
accounting for the fact that leases would expire, those leased keys
would never be updated with the new lease as the only attribute being
compared if it was equal or not was the value of the key and it should
have been the value *and* the lease information of that same key.

Fixes: ccd02fb04898 ("kvstore: Add UpdateIfDifferent()")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/etcd.go
@@ -873,11 +873,18 @@ func (e *etcdClient) UpdateIfDifferent(ctx context.Context, key string, value []
 		return false, fmt.Errorf("update cancelled via context: %s", ctx.Err())
 	}
 
-	existingValue, err := e.Get(key)
+	duration := spanstat.Start()
+	e.limiter.Wait(ctx)
+	getR, err := e.client.Get(ctx, key)
+	increaseMetric(key, metricRead, "Get", duration.EndError(err).Total(), err)
 	// On error, attempt update blindly
-	if err == nil {
-		// Value already exists, do not update
-		if bytes.Equal(existingValue, value) {
+	if err == nil && getR.Count != 0 {
+		e.RWMutex.RLock()
+		leaseID := e.session.Lease()
+		e.RWMutex.RUnlock()
+		// if lease is different and value is not equal then update.
+		if getR.Kvs[0].Lease == int64(leaseID) &&
+			bytes.Equal(getR.Kvs[0].Value, value) {
 			return false, nil
 		}
 	}

pkg/kvstore/consul.go
@@ -496,11 +496,14 @@ func (c *consulClient) Update(ctx context.Context, key string, value []byte, lea
 }
 
 func (c *consulClient) UpdateIfDifferent(ctx context.Context, key string, value []byte, lease bool) (bool, error) {
-	existingValue, err := c.Get(key)
+	duration := spanstat.Start()
+	getR, _, err := c.KV().Get(key, nil)
+	increaseMetric(key, metricRead, "Get", duration.EndError(err).Total(), err)
 	// On error, attempt update blindly
-	if err == nil {
-		// Value already exists, do not update
-		if bytes.Equal(existingValue, value) {
+	if err == nil && getR != nil {
+		// if lease is different and value is not equal then update.
+		if getR.Session == c.lease &&
+			bytes.Equal(getR.Value, value) {
 			return false, nil
 		}
 	}

commit ebb6f02909bb11f13f0cc79467edd948015ba72a

pkg/kvstore: do not always UpdateIfDifferent with and without lease
We should only update the value of the given key if the lease is
different only if we are trying to update it lease as well.

Fixes: 0259ad0568fd ("pkg/kvstore: perform update if value or lease are different")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/etcd.go
@@ -878,18 +878,23 @@ func (e *etcdClient) UpdateIfDifferent(ctx context.Context, key string, value []
 	getR, err := e.client.Get(ctx, key)
 	increaseMetric(key, metricRead, "Get", duration.EndError(err).Total(), err)
 	// On error, attempt update blindly
-	if err == nil && getR.Count != 0 {
+	if err != nil || getR.Count == 0 {
+		return true, e.Update(ctx, key, value, lease)
+	}
+	if lease {
 		e.RWMutex.RLock()
 		leaseID := e.session.Lease()
 		e.RWMutex.RUnlock()
-		// if lease is different and value is not equal then update.
-		if getR.Kvs[0].Lease == int64(leaseID) &&
-			bytes.Equal(getR.Kvs[0].Value, value) {
-			return false, nil
+		if getR.Kvs[0].Lease != int64(leaseID) {
+			return true, e.Update(ctx, key, value, lease)
 		}
 	}
+	// if value is not equal then update.
+	if !bytes.Equal(getR.Kvs[0].Value, value) {
+		return true, e.Update(ctx, key, value, lease)
+	}
 
-	return true, e.Update(ctx, key, value, lease)
+	return false, nil
 }
 
 // CreateOnly creates a key with the value and will fail if the key already exists