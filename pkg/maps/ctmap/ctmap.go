cilium/cilium

commit eacec0bffcf03295b3b15517443188f1b264e461

pkg/maps: create CtKeyGlobal structures
As Ct{4,6}Key were being passed to the initialization of the BPF Maps
cilium was not correctly dumping the content of the ct global maps as
the Dump method used of for local CT maps.

Fixes: 79c247a955f8 ("pkg/{bpf,datapath,maps}: use same MapKey and MapValue in map iterations")
Signed-off-by: André Martins <andre@cilium.io>

pkg/maps/ctmap/ctmap.go
@@ -94,19 +94,24 @@ const (
 type mapAttributes struct {
 	mapKey     bpf.MapKey
 	keySize    int
+	mapValue   bpf.MapValue
+	valueSize  int
 	maxEntries int
 	parser     bpf.DumpParser
 	bpfDefine  string
 	natMap     *nat.Map
 }
 
-func setupMapInfo(mapType MapType, define string, mapKey bpf.MapKey, keySize, maxEntries int, parser bpf.DumpParser, nat *nat.Map) {
+func setupMapInfo(mapType MapType, define string, mapKey bpf.MapKey, keySize int, maxEntries int, nat *nat.Map) {
 	mapInfo[mapType] = mapAttributes{
-		bpfDefine:  define,
-		mapKey:     mapKey,
-		keySize:    keySize,
+		bpfDefine: define,
+		mapKey:    mapKey,
+		keySize:   keySize,
+		// the value type is CtEntry for all CT maps
+		mapValue:   &CtEntry{},
+		valueSize:  int(unsafe.Sizeof(CtEntry{})),
 		maxEntries: maxEntries,
-		parser:     parser,
+		parser:     bpf.ConvertKeyValue,
 		natMap:     nat,
 	}
 }
@@ -120,31 +125,37 @@ func InitMapInfo(tcpMaxEntries, anyMaxEntries int) {
 	natV4 := natMaps[0]
 	natV6 := natMaps[1]
 
-	mapType := MapTypeIPv4TCPLocal
-	for _, maxEntries := range []int{MapNumEntriesLocal, tcpMaxEntries} {
-		setupMapInfo(MapType(mapType), "CT_MAP_TCP4",
-			&CtKey4{},
-			int(unsafe.Sizeof(CtKey4{})), maxEntries,
-			bpf.ConvertKeyValue, natV4)
-		mapType++
-		setupMapInfo(MapType(mapType), "CT_MAP_TCP6",
-			&CtKey6{},
-			int(unsafe.Sizeof(CtKey6{})), maxEntries,
-			bpf.ConvertKeyValue, natV6)
-		mapType++
-	}
-	for _, maxEntries := range []int{MapNumEntriesLocal, anyMaxEntries} {
-		setupMapInfo(MapType(mapType), "CT_MAP_ANY4",
-			&CtKey4{},
-			int(unsafe.Sizeof(tuple.TupleKey4{})), maxEntries,
-			bpf.ConvertKeyValue, natV4)
-		mapType++
-		setupMapInfo(MapType(mapType), "CT_MAP_ANY6",
-			&CtKey6{},
-			int(unsafe.Sizeof(CtKey6{})), maxEntries,
-			bpf.ConvertKeyValue, natV6)
-		mapType++
-	}
+	setupMapInfo(MapType(MapTypeIPv4TCPLocal), "CT_MAP_TCP4",
+		&CtKey4{}, int(unsafe.Sizeof(CtKey4{})),
+		MapNumEntriesLocal, natV4)
+
+	setupMapInfo(MapType(MapTypeIPv6TCPLocal), "CT_MAP_TCP6",
+		&CtKey6{}, int(unsafe.Sizeof(CtKey6{})),
+		MapNumEntriesLocal, natV6)
+
+	setupMapInfo(MapType(MapTypeIPv4TCPGlobal), "CT_MAP_TCP4",
+		&CtKey4Global{}, int(unsafe.Sizeof(CtKey4Global{})),
+		tcpMaxEntries, natV4)
+
+	setupMapInfo(MapType(MapTypeIPv6TCPGlobal), "CT_MAP_TCP6",
+		&CtKey6Global{}, int(unsafe.Sizeof(CtKey6Global{})),
+		tcpMaxEntries, natV6)
+
+	setupMapInfo(MapType(MapTypeIPv4AnyLocal), "CT_MAP_ANY4",
+		&CtKey4{}, int(unsafe.Sizeof(CtKey4{})),
+		MapNumEntriesLocal, natV4)
+
+	setupMapInfo(MapType(MapTypeIPv6AnyLocal), "CT_MAP_ANY6",
+		&CtKey6{}, int(unsafe.Sizeof(CtKey6{})),
+		MapNumEntriesLocal, natV6)
+
+	setupMapInfo(MapType(MapTypeIPv4AnyGlobal), "CT_MAP_ANY4",
+		&CtKey4Global{}, int(unsafe.Sizeof(CtKey4Global{})),
+		anyMaxEntries, natV4)
+
+	setupMapInfo(MapType(MapTypeIPv6AnyGlobal), "CT_MAP_ANY6",
+		&CtKey6Global{}, int(unsafe.Sizeof(CtKey6Global{})),
+		anyMaxEntries, natV6)
 }
 
 func init() {
@@ -214,8 +225,8 @@ func NewMap(mapName string, mapType MapType) *Map {
 			bpf.MapTypeLRUHash,
 			mapInfo[mapType].mapKey,
 			mapInfo[mapType].keySize,
-			&CtEntry{},
-			int(unsafe.Sizeof(CtEntry{})),
+			mapInfo[mapType].mapValue,
+			mapInfo[mapType].valueSize,
 			mapInfo[mapType].maxEntries,
 			0, 0,
 			mapInfo[mapType].parser,

pkg/maps/ctmap/lookup.go
@@ -26,6 +26,8 @@ import (
 )
 
 // CtKey4 is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
 type CtKey4 struct {
 	tuple.TupleKey4
 }
@@ -41,7 +43,27 @@ func (k *CtKey4) ToNetwork() tuple.TupleKey {
 	return &n
 }
 
+// CtKey4Global is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type CtKey4Global struct {
+	tuple.TupleKey4Global
+}
+
+// NewValue creates a new bpf.MapValue.
+func (k *CtKey4Global) NewValue() bpf.MapValue { return &CtEntry{} }
+
+// ToNetwork converts CtKey4Global ports to network byte order.
+func (k *CtKey4Global) ToNetwork() tuple.TupleKey {
+	n := *k
+	n.SourcePort = byteorder.HostToNetwork(n.SourcePort).(uint16)
+	n.DestPort = byteorder.HostToNetwork(n.DestPort).(uint16)
+	return &n
+}
+
 // CtKey6 is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
 type CtKey6 struct {
 	tuple.TupleKey6
 }
@@ -57,40 +79,78 @@ func (k *CtKey6) ToNetwork() tuple.TupleKey {
 	return &n
 }
 
-func createTupleKey(remoteAddr, localAddr string, proto u8proto.U8proto, ingress bool) (tuple.TupleKey, error) {
+// CtKey6Global is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type CtKey6Global struct {
+	tuple.TupleKey6Global
+}
+
+// NewValue creates a new bpf.MapValue.
+func (k *CtKey6Global) NewValue() bpf.MapValue { return &CtEntry{} }
+
+// ToNetwork converts CtKey6Global ports to network byte order.
+func (k *CtKey6Global) ToNetwork() tuple.TupleKey {
+	n := *k
+	n.SourcePort = byteorder.HostToNetwork(n.SourcePort).(uint16)
+	n.DestPort = byteorder.HostToNetwork(n.DestPort).(uint16)
+	return &n
+}
+
+func createTupleKey(isGlobal bool, remoteAddr, localAddr string, proto u8proto.U8proto, ingress bool) (bpf.MapKey, bool, error) {
 	ip, port, err := net.SplitHostPort(remoteAddr)
 	if err != nil {
-		return nil, fmt.Errorf("invalid remote address '%s': %s", remoteAddr, err)
+		return nil, false, fmt.Errorf("invalid remote address '%s': %s", remoteAddr, err)
 	}
 
 	sIP := net.ParseIP(ip)
 	if sIP == nil {
-		return nil, fmt.Errorf("unable to parse IP %s", ip)
+		return nil, false, fmt.Errorf("unable to parse IP %s", ip)
 	}
 
 	sport, err := strconv.ParseUint(port, 10, 16)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse port string: %s", err)
+		return nil, false, fmt.Errorf("unable to parse port string: %s", err)
 	}
 
 	localIp, localPort, err := net.SplitHostPort(localAddr)
 	if err != nil {
-		return nil, fmt.Errorf("invalid local address '%s': %s", localAddr, err)
+		return nil, false, fmt.Errorf("invalid local address '%s': %s", localAddr, err)
 	}
 
 	dIP := net.ParseIP(localIp)
 	if dIP == nil {
-		return nil, fmt.Errorf("unable to parse IP %s", localIp)
+		return nil, false, fmt.Errorf("unable to parse IP %s", localIp)
 	}
 
 	dport, err := strconv.ParseUint(localPort, 10, 16)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse port string: %s", err)
+		return nil, false, fmt.Errorf("unable to parse port string: %s", err)
 	}
 
 	if sIP.To4() != nil {
+		if isGlobal {
+			key := &CtKey4Global{
+				TupleKey4Global: tuple.TupleKey4Global{
+					TupleKey4: tuple.TupleKey4{
+						SourcePort: uint16(sport),
+						DestPort:   uint16(dport),
+						NextHeader: proto,
+						Flags:      TUPLE_F_OUT,
+					},
+				},
+			}
+			// CTmap has the addresses in the reverse order w.r.t. the original direction
+			copy(key.SourceAddr[:], dIP.To4())
+			copy(key.DestAddr[:], sIP.To4())
+			if ingress {
+				key.Flags = TUPLE_F_IN
+			}
+			return key.ToNetwork(), true, nil
+		}
+
 		key := &CtKey4{
-			tuple.TupleKey4{
+			TupleKey4: tuple.TupleKey4{
 				SourcePort: uint16(sport),
 				DestPort:   uint16(dport),
 				NextHeader: proto,
@@ -103,11 +163,31 @@ func createTupleKey(remoteAddr, localAddr string, proto u8proto.U8proto, ingress
 		if ingress {
 			key.Flags = TUPLE_F_IN
 		}
-		return key.ToNetwork(), nil
+		return key.ToNetwork(), true, nil
+	}
+
+	if isGlobal {
+		key := &CtKey6Global{
+			TupleKey6Global: tuple.TupleKey6Global{
+				TupleKey6: tuple.TupleKey6{
+					SourcePort: uint16(sport),
+					DestPort:   uint16(dport),
+					NextHeader: proto,
+					Flags:      TUPLE_F_OUT,
+				},
+			},
+		}
+		// CTmap has the addresses in the reverse order w.r.t. the original direction
+		copy(key.SourceAddr[:], dIP.To16())
+		copy(key.DestAddr[:], sIP.To16())
+		if ingress {
+			key.Flags = TUPLE_F_IN
+		}
+		return key.ToNetwork(), false, nil
 	}
 
 	key := &CtKey6{
-		tuple.TupleKey6{
+		TupleKey6: tuple.TupleKey6{
 			SourcePort: uint16(sport),
 			DestPort:   uint16(dport),
 			NextHeader: proto,
@@ -120,7 +200,7 @@ func createTupleKey(remoteAddr, localAddr string, proto u8proto.U8proto, ingress
 	if ingress {
 		key.Flags = TUPLE_F_IN
 	}
-	return key.ToNetwork(), nil
+	return key.ToNetwork(), false, nil
 }
 
 func getMapName(mapname string, ipv4 bool, proto u8proto.U8proto) string {
@@ -145,12 +225,12 @@ func getMapName(mapname string, ipv4 bool, proto u8proto.U8proto) string {
 // 'epname' is a 5-digit represenation of the endpoint ID if local maps
 // are to be used, or "global" if global maps should be used.
 func Lookup(epname string, remoteAddr, localAddr string, proto u8proto.U8proto, ingress bool) (*CtEntry, error) {
-	key, err := createTupleKey(remoteAddr, localAddr, proto, ingress)
+	isGlobal := epname == "global"
+
+	key, ipv4, err := createTupleKey(isGlobal, remoteAddr, localAddr, proto, ingress)
 	if err != nil {
 		return nil, err
 	}
-
-	_, ipv4 := key.(*CtKey4)
 
 	mapname := getMapName(epname, ipv4, proto)
 
@@ -161,10 +241,18 @@ func Lookup(epname string, remoteAddr, localAddr string, proto u8proto.U8proto, 
 		if err != nil {
 			return nil, fmt.Errorf("Can not open CT map %s: %s", mapname, err)
 		}
-		if ipv4 {
-			m.MapKey = &CtKey4{}
+		if isGlobal {
+			if ipv4 {
+				m.MapKey = &CtKey4Global{}
+			} else {
+				m.MapKey = &CtKey6Global{}
+			}
 		} else {
-			m.MapKey = &CtKey6{}
+			if ipv4 {
+				m.MapKey = &CtKey4{}
+			} else {
+				m.MapKey = &CtKey6{}
+			}
 		}
 		m.MapValue = &CtEntry{}
 	}

pkg/maps/ctmap/zz_generated.deepcopy.go
@@ -45,3 +45,103 @@ func (in *CtEntry) DeepCopyMapValue() bpf.MapValue {
 	}
 	return nil
 }
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CtKey4) DeepCopyInto(out *CtKey4) {
+	*out = *in
+	in.TupleKey4.DeepCopyInto(&out.TupleKey4)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CtKey4.
+func (in *CtKey4) DeepCopy() *CtKey4 {
+	if in == nil {
+		return nil
+	}
+	out := new(CtKey4)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *CtKey4) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CtKey4Global) DeepCopyInto(out *CtKey4Global) {
+	*out = *in
+	in.TupleKey4Global.DeepCopyInto(&out.TupleKey4Global)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CtKey4Global.
+func (in *CtKey4Global) DeepCopy() *CtKey4Global {
+	if in == nil {
+		return nil
+	}
+	out := new(CtKey4Global)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *CtKey4Global) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CtKey6) DeepCopyInto(out *CtKey6) {
+	*out = *in
+	in.TupleKey6.DeepCopyInto(&out.TupleKey6)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CtKey6.
+func (in *CtKey6) DeepCopy() *CtKey6 {
+	if in == nil {
+		return nil
+	}
+	out := new(CtKey6)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *CtKey6) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CtKey6Global) DeepCopyInto(out *CtKey6Global) {
+	*out = *in
+	in.TupleKey6Global.DeepCopyInto(&out.TupleKey6Global)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CtKey6Global.
+func (in *CtKey6Global) DeepCopy() *CtKey6Global {
+	if in == nil {
+		return nil
+	}
+	out := new(CtKey6Global)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *CtKey6Global) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}

commit 616c218b5195d2833fdefa090aab229eb6c5e3e7

ctmap: Fix conntrack map filtering
The type was incorrect causing all filters to be ignored. Add a warning
statement to catch future mismatches.

Fixes: eacec0bffcf ("pkg/maps: create CtKeyGlobal structures")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/maps/ctmap/ctmap.go
@@ -21,6 +21,7 @@ import (
 	"math"
 	"net"
 	"os"
+	"reflect"
 	"time"
 	"unsafe"
 
@@ -263,7 +264,7 @@ func doGC6(m *Map, filter *GCFilter) gcStats {
 		entry := value.(*CtEntry)
 
 		switch obj := key.(type) {
-		case *tuple.TupleKey6Global:
+		case *CtKey6Global:
 			currentKey6Global := obj
 			// In CT entries, the source address of the conntrack entry (`SourceAddr`) is
 			// the destination of the packet received, therefore it's the packet's
@@ -282,7 +283,7 @@ func doGC6(m *Map, filter *GCFilter) gcStats {
 			default:
 				stats.aliveEntries++
 			}
-		case *tuple.TupleKey6:
+		case *CtKey6:
 			currentKey6 := obj
 			// In CT entries, the source address of the conntrack entry (`SourceAddr`) is
 			// the destination of the packet received, therefore it's the packet's
@@ -301,6 +302,8 @@ func doGC6(m *Map, filter *GCFilter) gcStats {
 			default:
 				stats.aliveEntries++
 			}
+		default:
+			log.Warningf("Encountered unknown type while scanning conntrack table: %v", reflect.TypeOf(key))
 		}
 	}
 	stats.dumpError = m.DumpReliablyWithCallback(filterCallback, stats.DumpStats)
@@ -334,7 +337,7 @@ func doGC4(m *Map, filter *GCFilter) gcStats {
 		entry := value.(*CtEntry)
 
 		switch obj := key.(type) {
-		case *tuple.TupleKey4Global:
+		case *CtKey4Global:
 			currentKey4Global := obj
 			// In CT entries, the source address of the conntrack entry (`SourceAddr`) is
 			// the destination of the packet received, therefore it's the packet's
@@ -353,7 +356,7 @@ func doGC4(m *Map, filter *GCFilter) gcStats {
 			default:
 				stats.aliveEntries++
 			}
-		case *tuple.TupleKey4:
+		case *CtKey4:
 			currentKey4 := obj
 			// In CT entries, the source address of the conntrack entry (`SourceAddr`) is
 			// the destination of the packet received, therefore it's the packet's
@@ -372,6 +375,8 @@ func doGC4(m *Map, filter *GCFilter) gcStats {
 			default:
 				stats.aliveEntries++
 			}
+		default:
+			log.Warningf("Encountered unknown type while scanning conntrack table: %v", reflect.TypeOf(key))
 		}
 	}
 	stats.dumpError = m.DumpReliablyWithCallback(filterCallback, stats.DumpStats)