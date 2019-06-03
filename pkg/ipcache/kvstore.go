cilium/cilium

commit b76bb7f504a3db0072406e466821e1ed767f495e

ipcache: Fix refcounting of endpoint IPCache entries
The reference counter in the ipcache introduced by commit 6f9ab9a9808a
("ipcache: Count references to ipcache mappings") in PR #4611 was
introduced to count references to CIDR IPCache entries to ensure that if
multiple rules referred to the same CIDR, and one was deleted, Cilium
would keep the CIDR in the kv-store when one rule is deleted.

However, in the process it also introduced reference counting on
something that doesn't inherently need reference counting: the
synchronization of endpoint IPs to the kv-store. As a result, every time
the identity sync controller for an endpoint attempted to push the
endpoint's IP into the kvstore, it would accumulate a reference to the
ipcache entry. However, when the endpoint is deleted and the controller
removed, it would only remove a single reference from the in-memory map
and fail to remove the endpoint IP from the actual kv-store.

Fix this by removing the reference counting from the endpoint IP
identity synchronization paths.

Fixes: #5887

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/ipcache/kvstore.go
@@ -125,7 +125,7 @@ func (r *kvReferenceCounter) upsert(ipKey string, ipIDPair identity.IPIdentityPa
 		logfields.IPMask:       ipIDPair.Mask,
 		logfields.Identity:     ipIDPair.ID,
 		logfields.Modification: Upsert,
-	}).Debug("upserting IP->ID mapping to kvstore")
+	}).Debug("upserting CIDR->ID mapping to kvstore")
 
 	r.Lock()
 	defer r.Unlock()
@@ -174,7 +174,18 @@ func UpsertIPToKVStore(IP, hostIP net.IP, ID identity.NumericIdentity, metadata 
 		HostIP:   hostIP,
 	}
 
-	return globalMap.upsert(ipKey, ipIDPair)
+	marshaledIPIDPair, err := json.Marshal(ipIDPair)
+	if err != nil {
+		return err
+	}
+
+	log.WithFields(logrus.Fields{
+		logfields.IPAddr:       ipIDPair.IP,
+		logfields.Identity:     ipIDPair.ID,
+		logfields.Modification: Upsert,
+	}).Debug("upserting IP->ID mapping to kvstore")
+
+	return globalMap.store.upsert(ipKey, marshaledIPIDPair, true)
 }
 
 // upsertIPNetToKVStore updates / inserts the provided CIDR->Identity mapping
@@ -273,7 +284,7 @@ func upsertIPNetsToKVStore(prefixes []*net.IPNet, identities []*identity.Identit
 // NewIPIdentityWatcher().
 func DeleteIPFromKVStore(ip string) error {
 	ipKey := path.Join(IPIdentitiesPath, AddressSpace, ip)
-	return globalMap.release(ipKey)
+	return globalMap.store.release(ipKey)
 }
 
 // deleteIPNetsFromKVStore removes the Prefix->Identity mappings for the

commit c7585d6306d15c164d6a7387a22babb610a59e76

ipcache: Fix automatic recovery of deleted ipcache entries
The existing delete recovery logic was relying on upsert() to be called on
kvReferenceCounter. This was never done though so when the delete handler
verified for local existance, no entry was ever found.

Remove the obsolete upsert() handler of kvReferenceCounter as no reference
counting is needed and insert an entry into marshaledIPIDPairs from
UpsertIPToKVStore() directly.

Fixes: b76bb7f504a ("ipcache: Fix refcounting of endpoint IPCache entries")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/ipcache/kvstore.go
@@ -47,8 +47,8 @@ var (
 	// computed. It is determined by the orchestration system / runtime.
 	AddressSpace = DefaultAddressSpace
 
-	// globalMap wraps the kvstore and provides reference-tracking for keys
-	// that are upserted or released from the kvstore.
+	// globalMap wraps the kvstore and provides a cache of all entries
+	// which are owned by a local user
 	globalMap = newKVReferenceCounter(kvstoreImplementation{})
 
 	setupIPIdentityWatcher sync.Once
@@ -81,21 +81,10 @@ func (k kvstoreImplementation) release(ctx context.Context, key string) error {
 }
 
 // kvReferenceCounter provides a thin wrapper around the kvstore which adds
-// reference tracking for all entries being updated. When the first key is
-// updated, it adds a reference to the kvstore and tracks the reference
-// internally. Subsequent updates also update the kvstore, and add a reference.
-// Deletes from the kvReferenceCounter are only propagated to the kvstore when
-// the final reference is released.
-//
-// This has some small overlap with the pkg/kvstore/allocator but this is only
-// a map from key to reference count rather than also tracking values.
+// reference tracking for all entries which are used by a local user.
 type kvReferenceCounter struct {
 	lock.Mutex
 	store
-
-	// keys is a map from key to reference count for locally-referenced
-	// keys in the global kvstore.
-	keys map[string]uint64
 
 	// marshaledIPIDPair is map indexed by the key that contains the
 	// marshaled IPIdentityPair
@@ -107,62 +96,8 @@ type kvReferenceCounter struct {
 func newKVReferenceCounter(s store) *kvReferenceCounter {
 	return &kvReferenceCounter{
 		store:              s,
-		keys:               map[string]uint64{},
 		marshaledIPIDPairs: map[string][]byte{},
 	}
-}
-
-// upsert attempts to insert the specified {key, ipIDPair} into the kvstore. If
-// the key has previously been upserted, increments a reference on the key.
-// Always updates the underlying store with this {key, ipIDPair} tuple.
-// Only adds a reference to the key if the upsert is successful.
-func (r *kvReferenceCounter) upsert(ctx context.Context, ipKey string, ipIDPair identity.IPIdentityPair) error {
-	marshaledIPIDPair, err := json.Marshal(ipIDPair)
-	if err != nil {
-		return err
-	}
-
-	log.WithFields(logrus.Fields{
-		logfields.IPAddr:       ipIDPair.IP,
-		logfields.IPMask:       ipIDPair.Mask,
-		logfields.Identity:     ipIDPair.ID,
-		logfields.Modification: Upsert,
-	}).Debug("upserting CIDR->ID mapping to kvstore")
-
-	r.Lock()
-	defer r.Unlock()
-	refcnt := r.keys[ipKey] // 0 if not found
-	refcnt++
-	err = r.store.upsert(ctx, ipKey, marshaledIPIDPair, true)
-	if err == nil {
-		r.keys[ipKey] = refcnt
-		r.marshaledIPIDPairs[ipKey] = marshaledIPIDPair
-	}
-	return err
-}
-
-// release removes a reference to the specified key. If the number of
-// references reaches 0, the key is removed from the underlying kvstore.
-func (r *kvReferenceCounter) release(ctx context.Context, key string) (err error) {
-	r.Lock()
-	defer r.Unlock()
-
-	refcnt, ok := r.keys[key] // 0 if not found
-	// avoid underflow and report bug
-	if !ok || refcnt == 0 {
-		log.WithField("key", key).Error("BUG: attempt to release ipcache entry while refcnt == 0")
-		return nil
-	}
-
-	refcnt--
-	if refcnt == 0 {
-		delete(r.keys, key)
-		delete(r.marshaledIPIDPairs, key)
-		err = r.store.release(ctx, key)
-	} else {
-		r.keys[key] = refcnt
-	}
-	return err
 }
 
 // UpsertIPToKVStore updates / inserts the provided IP->Identity mapping into the
@@ -187,9 +122,15 @@ func UpsertIPToKVStore(ctx context.Context, IP, hostIP net.IP, ID identity.Numer
 		logfields.Identity:     ipIDPair.ID,
 		logfields.Key:          ipIDPair.Key,
 		logfields.Modification: Upsert,
-	}).Debug("upserting IP->ID mapping to kvstore")
+	}).Debug("Upserting IP->ID mapping to kvstore")
 
-	return globalMap.store.upsert(ctx, ipKey, marshaledIPIDPair, true)
+	err = globalMap.store.upsert(ctx, ipKey, marshaledIPIDPair, true)
+	if err == nil {
+		globalMap.Lock()
+		globalMap.marshaledIPIDPairs[ipKey] = marshaledIPIDPair
+		globalMap.Unlock()
+	}
+	return err
 }
 
 // keyToIPNet returns the IPNet describing the key, whether it is a host, and
@@ -231,6 +172,9 @@ func keyToIPNet(key string) (parsedPrefix *net.IPNet, host bool, err error) {
 // NewIPIdentityWatcher().
 func DeleteIPFromKVStore(ctx context.Context, ip string) error {
 	ipKey := path.Join(IPIdentitiesPath, AddressSpace, ip)
+	globalMap.Lock()
+	delete(globalMap.marshaledIPIDPairs, ipKey)
+	globalMap.Unlock()
 	return globalMap.store.release(ctx, ipKey)
 }
 
@@ -343,9 +287,6 @@ restart:
 					if err != nil {
 						log.WithError(err).WithField("ip", ip).Warning("Unable to re-create alive ipcache entry")
 					}
-					globalMap.Unlock()
-				} else if _, ok := globalMap.keys[path.Join(IPIdentitiesPath, AddressSpace, ip)]; ok {
-					log.WithField("ip", ip).Warning("Received kvstore delete notification with mismatching key but alive IP. Ignoring")
 					globalMap.Unlock()
 				} else {
 					globalMap.Unlock()

pkg/ipcache/kvstore_test.go
@@ -1,88 +1,0 @@
-// Copyright 2018 Authors of Cilium
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// +build !privileged_tests
-
-package ipcache
-
-import (
-	"context"
-	"fmt"
-
-	"github.com/cilium/cilium/pkg/identity"
-
-	. "gopkg.in/check.v1"
-)
-
-type testStore map[string]int
-
-func (ts testStore) upsert(ctx context.Context, key string, value []byte, lease bool) error {
-	refcnt, ok := ts[key]
-	if ok {
-		ts[key] = refcnt + 1
-	} else {
-		ts[key] = 1
-	}
-	return nil
-}
-
-func (ts testStore) release(ctx context.Context, key string) error {
-	_, ok := ts[key]
-	if !ok {
-		return fmt.Errorf("Unexpected delete from underlying store")
-	}
-	delete(ts, key)
-	return nil
-}
-
-func (s *IPCacheTestSuite) TestKVReferenceCounter(c *C) {
-	ts := testStore{}
-	refcnt := newKVReferenceCounter(ts)
-
-	// Add two references to "foo"; we should see two updates.
-	key1 := "foo"
-	err := refcnt.upsert(context.Background(), key1, identity.IPIdentityPair{})
-	c.Assert(err, IsNil)
-	c.Assert(ts[key1], Equals, 1)
-	err = refcnt.upsert(context.Background(), key1, identity.IPIdentityPair{})
-	c.Assert(err, IsNil)
-	c.Assert(ts[key1], Equals, 2)
-
-	// Remove one reference, "foo" should still map to 2
-	err = refcnt.release(context.Background(), key1)
-	c.Assert(err, IsNil)
-	c.Assert(ts[key1], Equals, 2)
-	// Remove the second reference, "foo" should be deleted from the store.
-	err = refcnt.release(context.Background(), key1)
-	c.Assert(err, IsNil)
-	_, ok := ts[key1]
-	c.Assert(ok, Equals, false)
-
-	// Create two keys at once
-	key2 := "bar"
-	err = refcnt.upsert(context.Background(), key1, identity.IPIdentityPair{})
-	c.Assert(err, IsNil)
-	c.Assert(ts[key1], Equals, 1)
-	err = refcnt.upsert(context.Background(), key2, identity.IPIdentityPair{})
-	c.Assert(err, IsNil)
-	c.Assert(ts[key2], Equals, 1)
-
-	// Remove one of the keys. The other remains.
-	err = refcnt.release(context.Background(), key1)
-	c.Assert(err, IsNil)
-	_, ok = ts[key1]
-	c.Assert(ok, Equals, false)
-	_, ok = ts[key2]
-	c.Assert(ok, Equals, true)
-}