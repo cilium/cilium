6e1893e394d96c79f39e6377ca63e945e25598dc
Joe Stringer <joe@cilium.io>
2019-06-05 12:58:58 -0700

    endpoint: Add tests,benchmarks for headerfile write
    Add tests and benchmarks that assist in assessing the cost of writing
    endpoint information to a headerfile.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

---
+++ pkg/endpoint/bpf_test.go
@@ -0,0 +1,80 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package endpoint
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"testing"
+
+	"github.com/cilium/cilium/pkg/datapath/linux"
+	"github.com/cilium/cilium/pkg/policy"
+
+	. "gopkg.in/check.v1"
+)
+
+func (s *EndpointSuite) TestWriteInformationalComments(c *C) {
+	e := NewEndpointWithState(s.repo, 100, StateCreating)
+
+	var f bytes.Buffer
+	err := e.writeInformationalComments(&f)
+	c.Assert(err, IsNil)
+}
+
+type writeFunc func(io.Writer) error
+
+func BenchmarkWriteHeaderfile(b *testing.B) {
+	repo := policy.NewPolicyRepository()
+	e := NewEndpointWithState(repo, 100, StateCreating)
+	dp := linux.NewDatapath(linux.DatapathConfiguration{})
+
+	targetComments := func(w io.Writer) error {
+		return e.writeInformationalComments(w)
+	}
+	targetConfig := func(w io.Writer) error {
+		return dp.WriteEndpointConfig(w, e)
+	}
+
+	var buf bytes.Buffer
+	file, err := ioutil.TempFile("", "cilium_ep_bench_")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer file.Close()
+
+	benchmarks := []struct {
+		name   string
+		output io.Writer
+		write  writeFunc
+	}{
+		{"in-memory-info", &buf, targetComments},
+		{"in-memory-cfg", &buf, targetConfig},
+		{"to-disk-info", file, targetComments},
+		{"to-disk-cfg", file, targetConfig},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				if err := bm.write(bm.output); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
