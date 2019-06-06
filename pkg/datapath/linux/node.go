cilium/cilium

commit 1e5d74882a5d0e9fb035f6dbe125d974113d3275

cilium: adds option to pull node traffic into Cilium for encryption
This patch adds an option to encrypt traffic associated with the node
IP even if it is not otherwise routed via cilium host.

To accomplish this add a route entry to route the node IP to cilium
host interface. Then node traffic will fallthrough bpf_netdev program
and because we have also added the peer nodes to the ip cache there
will be a key in the entry that can be used to encrypt the
traffic. Note we do not add any hooks to policy layer here so if
policy would drop the flow here it will be dropped. The assumption
is users enforcing policy will have a rule to allow node traffic when
this is enabled. If folks would prefer to not have this requirement
we can add a flag relatively easily to not enfoce policy on node IPs.

Picture of egress flow,

        +-------+
        |  App  |
        +-------+
           |
           | route to cilium host
           |
     +------------+
     | bpf_netdev |
     +------------+
           |
           |  skb->cb = (key | encrypt_cmd)
           |  skb plaintext here
           |
     +------------+
     | bpf_ipsec  |
     +------------+
           |
           |  skb->cb = 0
           |  skb->mark = (key | encrypt_cmd)
           |  skb plaintext here
           |
     +------------+
     |   xfrm     |
     +------------+
           |
           |  skb->mark = (key | encrypt_cmd)
           |  skb encrypted here
           |  route back to bpf_netdev
           |
     +------------+
     | bpf_netdev |
     +------------+
           |
           |  skb->mark = 0
           |  skb encrypted here
           |  redirect to encrypt-interface
           v
     network stack

On first inspection we might be tempted to bypass bpf_netdev
altogether and do encryption from xfrm directly e.g.

     App -> xfrm -> egress network stack

However, this is problematic because we use bpf_netdev to select
the key to use and with rolling key updates this is critical to

pkg/datapath/linux/node.go
@@ -474,6 +474,25 @@ func (n *linuxNodeHandler) enableIPsec(newNode *node.Node) {
 		}
 	}
 
+	if n.nodeConfig.EnableIPv4 && n.nodeConfig.EncryptNode {
+		internalIPv4 := n.nodeAddressing.IPv4().PrimaryExternal()
+		exactMask := net.IPv4Mask(255, 255, 255, 255)
+		ipsecLocal := &net.IPNet{IP: internalIPv4, Mask: exactMask}
+		if newNode.IsLocal() {
+			ipsecIPv4Wildcard := &net.IPNet{IP: net.ParseIP(wildcardIPv4), Mask: net.IPv4Mask(0, 0, 0, 0)}
+			n.replaceNodeIPSecInRoute(ipsecLocal)
+			spi, err = ipsec.UpsertIPsecEndpoint(ipsecLocal, ipsecIPv4Wildcard, ipsec.IPSecDirIn)
+			upsertIPsecLog(err, "local IPv4", ipsecLocal, ipsecIPv4Wildcard, spi)
+		} else {
+			if remoteIPv4 := newNode.GetNodeIP(false); remoteIPv4 != nil {
+				ipsecRemote := &net.IPNet{IP: remoteIPv4, Mask: exactMask}
+				n.replaceNodeExternalIPSecOutRoute(ipsecRemote)
+				spi, err = ipsec.UpsertIPsecEndpoint(ipsecLocal, ipsecRemote, ipsec.IPSecDirOut)
+				upsertIPsecLog(err, "IPv4", ipsecLocal, ipsecRemote, spi)
+			}
+		}
+	}
+
 	if n.nodeConfig.EnableIPv6 && newNode.IPv6AllocCIDR != nil {
 		new6Net := &net.IPNet{IP: newNode.IPv6AllocCIDR.IP, Mask: newNode.IPv6AllocCIDR.Mask}
 		if newNode.IsLocal() {
@@ -496,6 +515,26 @@ func (n *linuxNodeHandler) enableIPsec(newNode *node.Node) {
 			}
 		}
 	}
+
+	if n.nodeConfig.EnableIPv6 && n.nodeConfig.EncryptNode {
+		internalIPv6 := n.nodeAddressing.IPv6().PrimaryExternal()
+		internalMask := net.CIDRMask(64, 128)
+		ipsecLocal := &net.IPNet{IP: internalIPv6, Mask: internalMask}
+		if newNode.IsLocal() {
+			ipsecIPv6Wildcard := &net.IPNet{IP: net.ParseIP(wildcardIPv6), Mask: net.CIDRMask(0, 0)}
+			n.replaceNodeIPSecInRoute(ipsecLocal)
+			spi, err = ipsec.UpsertIPsecEndpoint(ipsecLocal, ipsecIPv6Wildcard, ipsec.IPSecDirIn)
+			upsertIPsecLog(err, "local IPv6", ipsecLocal, ipsecIPv6Wildcard, spi)
+		} else {
+			if remoteIPv6 := newNode.GetNodeIP(false); remoteIPv6 != nil {
+				ipsecRemote := &net.IPNet{IP: remoteIPv6, Mask: internalMask}
+				n.replaceNodeExternalIPSecOutRoute(ipsecRemote)
+				spi, err = ipsec.UpsertIPsecEndpoint(ipsecLocal, ipsecRemote, ipsec.IPSecDirOut)
+				upsertIPsecLog(err, "IPv6", ipsecLocal, ipsecRemote, spi)
+			}
+		}
+	}
+
 }
 
 func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *node.Node, firstAddition bool) error {
@@ -705,6 +744,32 @@ func (n *linuxNodeHandler) createNodeIPSecOutRoute(ip *net.IPNet) route.Route {
 	}
 }
 
+func (n *linuxNodeHandler) createNodeExternalIPSecOutRoute(ip *net.IPNet, dflt bool) route.Route {
+	var nexthop net.IP
+	var tbl int
+	var dev string
+
+	if ip.IP.To4() != nil {
+		nexthop = n.nodeAddressing.IPv4().PrimaryExternal()
+	} else {
+		nexthop = n.nodeAddressing.IPv6().PrimaryExternal()
+	}
+
+	if dflt {
+		dev = n.datapathConfig.HostDevice
+	} else {
+		tbl = linux_defaults.RouteTableIPSec
+		dev = n.datapathConfig.HostDevice //n.datapathConfig.EncryptInterface
+	}
+
+	return route.Route{
+		Nexthop: &nexthop,
+		Device:  dev,
+		Prefix:  *ip,
+		Table:   tbl,
+	}
+}
+
 // replaceNodeIPSecOutRoute replace the out IPSec route in the host routing table
 // with the new route. If no route exists the route is installed on the host.
 func (n *linuxNodeHandler) replaceNodeIPSecOutRoute(ip *net.IPNet) {
@@ -723,6 +788,33 @@ func (n *linuxNodeHandler) replaceNodeIPSecOutRoute(ip *net.IPNet) {
 	}
 
 	_, err := route.Upsert(n.createNodeIPSecOutRoute(ip), &n.nodeConfig.MtuConfig)
+	if err != nil {
+		log.WithError(err).Error("Unable to replace the IPSec route OUT the host routing table")
+	}
+}
+
+// replaceNodeExternalIPSecOutRoute replace the out IPSec route in the host routing table
+// with the new route. If no route exists the route is installed on the host.
+func (n *linuxNodeHandler) replaceNodeExternalIPSecOutRoute(ip *net.IPNet) {
+	if ip == nil {
+		return
+	}
+
+	if ip.IP.To4() != nil {
+		if !n.nodeConfig.EnableIPv4 {
+			return
+		}
+	} else {
+		if !n.nodeConfig.EnableIPv6 {
+			return
+		}
+	}
+
+	_, err := route.Upsert(n.createNodeExternalIPSecOutRoute(ip, true), &n.nodeConfig.MtuConfig)
+	if err != nil {
+		log.WithError(err).Error("Unable to replace the IPSec route OUT the default routing table")
+	}
+	_, err = route.Upsert(n.createNodeExternalIPSecOutRoute(ip, false), &n.nodeConfig.MtuConfig)
 	if err != nil {
 		log.WithError(err).Error("Unable to replace the IPSec route OUT the host routing table")
 	}

pkg/datapath/node.go
@@ -91,6 +91,9 @@ type LocalNodeConfiguration struct {
 
 	// EnableIPSec enables IPSec routes
 	EnableIPSec bool
+
+	// EncryptNode enables encrypting NodeIP traffic requries EnableIPSec
+	EncryptNode bool
 }
 
 // NodeHandler handles node related events such as addition, update or deletion

bpf/bpf_netdev.c
@@ -402,8 +402,11 @@ static __always_inline int do_netdev(struct __sk_buff *skb, __u16 proto)
 
 #ifdef ENCAP_IFINDEX
 			return __encap_and_redirect_with_nodeid(skb, tunnel_endpoint, seclabel, TRACE_PAYLOAD_LEN);
-#endif
+#elif ENCRYPT_NODE
+			return redirect(ENCRYPT_IFACE, 0);
+#else
 			return TC_ACT_OK;
+#endif
 		}
 	}
 #endif

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=a815a3c044cb12cbb728ff73ecf603b4e65cd534
+GO_BINDATA_SHA1SUM=8253d5caf8e9744cd543b6ad6600c71387dc7eec
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

daemon/daemon_main.go
@@ -448,6 +448,9 @@ func init() {
 	flags.String(option.EncryptInterface, "", "Transparent encryption interface")
 	option.BindEnv(option.EncryptInterface)
 
+	flags.Bool(option.EncryptNode, defaults.EncryptNode, "Enables encrypting traffic from non-Cilium pods and host networking")
+	option.BindEnv(option.EncryptNode)
+
 	flags.String(option.EndpointInterfaceNamePrefix, defaults.EndpointInterfaceNamePrefix, "Prefix of interface name shared by all endpoints")
 	option.BindEnv(option.EndpointInterfaceNamePrefix)
 

pkg/datapath/linux/config.go
@@ -40,6 +40,8 @@ import (
 	"github.com/cilium/cilium/pkg/maps/tunnel"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
+
+	"github.com/vishvananda/netlink"
 )
 
 func writeIncludes(w io.Writer) (int, error) {
@@ -146,6 +148,15 @@ func (l *linuxDatapath) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeConf
 	}
 	if option.Config.EnableIPSec {
 		fmt.Fprintf(fw, "#define ENABLE_IPSEC 1\n")
+	}
+	if option.Config.EncryptNode {
+		fmt.Fprintf(fw, "#define ENCRYPT_NODE 1\n")
+	}
+	if option.Config.EncryptInterface != "" {
+		link, err := netlink.LinkByName(option.Config.EncryptInterface)
+		if err == nil {
+			fmt.Fprintf(fw, "#define ENCRYPT_IFACE %d\n", link.Attrs().Index)
+		}
 	}
 	if !option.Config.InstallIptRules && option.Config.Masquerade {
 		fmt.Fprintf(fw, "#define ENABLE_MASQUERADE 1\n")

pkg/defaults/defaults.go
@@ -136,6 +136,10 @@ const (
 	// EnableIPSec is the default value for IPSec enablement
 	EnableIPSec = false
 
+	// EncryptNode enables encrypting traffic from host networking applications
+	// which are not part of Cilium manged pods.
+	EncryptNode = false
+
 	// MonitorQueueSizePerCPU is the default value for the monitor queue
 	// size per CPU
 	MonitorQueueSizePerCPU = 1024

pkg/node/store/store.go
@@ -22,6 +22,7 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 var (
@@ -65,6 +66,18 @@ func (o *NodeObserver) OnUpdate(k store.Key) {
 				Source: ipcache.FromKVStore,
 			})
 		}
+
+		if option.Config.EncryptNode {
+			hostIP := nodeCopy.GetNodeIP(false)
+			if hostIP != nil {
+				hostKey := node.GetIPsecKeyIdentity()
+				ipcache.IPIdentityCache.Upsert(hostIP.String(), hostIP, hostKey, ipcache.Identity{
+					ID:     identity.ReservedIdentityHost,
+					Source: ipcache.FromKVStore,
+				})
+			}
+		}
+
 		ciliumIPv6 := nodeCopy.GetCiliumInternalIP(true)
 		if ciliumIPv6 != nil {
 			hostIP := nodeCopy.GetNodeIP(true)

commit fed58f33ece2467ef7aa3278d30aa656d4cbe9c4

cilium: encrypt-node option adds incorrect route
On at least some kernels (specifically the GKE Ubuntu 4.15 kernels)
Cilium with encrypt-node configured is creating a route to the
cilium_host interface with dst IP matching the encrypt-interface
IP. This results in a routing loop and any packets that should be
routed to the host networking via that IP are lost.

The root cause of this is a bit mysterious. It appears that building
a route with a Prefix and Nexthop is causing the lower layers either
netlink API or kernel itself(?!) to swap the Prefix and Nexthop the
result is the above looping address.

The easiest fix at the moment is to simply drop the Nexthop. Its
actually not needed and should be dropped anyways. So while we
root cause the above apply this fix which is also optimal.

Fixes: 1e5d74882a5d0 ("cilium: adds option to pull node traffic into Cilium for encryption")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/linux/node.go
@@ -745,15 +745,8 @@ func (n *linuxNodeHandler) createNodeIPSecOutRoute(ip *net.IPNet) route.Route {
 }
 
 func (n *linuxNodeHandler) createNodeExternalIPSecOutRoute(ip *net.IPNet, dflt bool) route.Route {
-	var nexthop net.IP
 	var tbl int
 	var dev string
-
-	if ip.IP.To4() != nil {
-		nexthop = n.nodeAddressing.IPv4().PrimaryExternal()
-	} else {
-		nexthop = n.nodeAddressing.IPv6().PrimaryExternal()
-	}
 
 	if dflt {
 		dev = n.datapathConfig.HostDevice
@@ -763,10 +756,9 @@ func (n *linuxNodeHandler) createNodeExternalIPSecOutRoute(ip *net.IPNet, dflt b
 	}
 
 	return route.Route{
-		Nexthop: &nexthop,
-		Device:  dev,
-		Prefix:  *ip,
-		Table:   tbl,
+		Device: dev,
+		Prefix: *ip,
+		Table:  tbl,
 	}
 }
 