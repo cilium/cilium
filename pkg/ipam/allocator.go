cilium/cilium

commit 58579f18280c5e3581982cbdfbb2842cd5008ac9

ipam: Use Blacklist() to reserve IP in allocation range
The existing strategy to use AllocateIP() to reserve existing IPs which could
potentially conflict with future allocations required the allocation range to
be known beforehand. This will no longer be true in the future.

Introduce a Blacklist() function to keep track of IPs to be blacklisted. This
allows to keep track of them separately and allocate them when they actually
conflict.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/ipam/allocator.go
@@ -49,6 +49,11 @@ var (
 func (ipam *IPAM) AllocateIP(ip net.IP, owner string) error {
 	ipam.allocatorMutex.Lock()
 	defer ipam.allocatorMutex.Unlock()
+
+	if owner, ok := ipam.blacklist[ip.String()]; ok {
+		return fmt.Errorf("IP %s is blacklisted, owned by %s", ip.String(), owner)
+	}
+
 	family := familyIPv4
 	if ip.To4() != nil {
 		if ipam.IPv4Allocator == nil {
@@ -94,17 +99,27 @@ func (ipam *IPAM) allocateNextFamily(family Family, allocator *ipallocator.Range
 		return nil, fmt.Errorf("%s allocator not available", family)
 	}
 
-	ip, err = allocator.AllocateNext()
-	if err == nil {
-		log.WithFields(logrus.Fields{
-			"ip":    ip.String(),
-			"owner": owner,
-		}).Debugf("Allocated random IP")
-		ipam.owner[ip.String()] = owner
-		metrics.IpamEvent.WithLabelValues(metricAllocate, string(family)).Inc()
-	}
+	for {
+		ip, err = allocator.AllocateNext()
+		if err != nil {
+			return
+		}
 
-	return
+		if owner, ok := ipam.blacklist[ip.String()]; !ok {
+			log.WithFields(logrus.Fields{
+				"ip":    ip.String(),
+				"owner": owner,
+			}).Debugf("Allocated random IP")
+			ipam.owner[ip.String()] = owner
+			metrics.IpamEvent.WithLabelValues(metricAllocate, string(family)).Inc()
+			return
+		}
+
+		// The allocated IP is blacklisted, do not use it. The
+		// blacklisted IP is now allocated so it won't be allocated in
+		// the next iteration.
+		ipam.owner[ip.String()] = fmt.Sprintf("%s (blacklisted)", owner)
+	}
 }
 
 // AllocateNextFamily allocates the next IP of the requested address family

daemon/daemon.go
@@ -902,19 +902,10 @@ func deleteHostDevice() {
 }
 
 func (d *Daemon) prepareAllocationCIDR(family datapath.NodeAddressingFamily) (routerIP net.IP, err error) {
-	// Reserve the IPv4 external node IP within the allocation range if
-	// required.
-	allocRange := family.AllocationCIDR()
-	nodeIP := family.PrimaryExternal()
-	if allocRange.Contains(nodeIP) {
-		err = d.ipam.AllocateIP(nodeIP, "node")
-		if err != nil {
-			err = fmt.Errorf("Unable to allocate external IPv4 node IP %s from allocation range %s: %s",
-				nodeIP, allocRange, err)
-			return
-		}
-	}
+	// Blacklist allocation of the external IP
+	d.ipam.Blacklist(family.PrimaryExternal(), "node-ip")
 
+	allocRange := family.AllocationCIDR()
 	routerIP = family.Router()
 	if routerIP != nil && !allocRange.Contains(routerIP) {
 		log.Warningf("Detected allocation CIDR change to %s, previous router IP %s", allocRange, routerIP)

pkg/ipam/ipam.go
@@ -22,7 +22,6 @@ import (
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 
-	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
 	"k8s.io/kubernetes/pkg/registry/core/service/ipallocator"
 )
@@ -54,6 +53,7 @@ func NewIPAM(nodeAddressing datapath.NodeAddressing, c Configuration) *IPAM {
 		nodeAddressing: nodeAddressing,
 		config:         c,
 		owner:          map[string]string{},
+		blacklist:      map[string]string{},
 	}
 
 	if c.EnableIPv6 {
@@ -91,8 +91,6 @@ func (ipam *IPAM) reserveLocalRoutes() {
 		return
 	}
 
-	allocRange := ipam.nodeAddressing.IPv4().AllocationCIDR()
-
 	for _, r := range routes {
 		// ignore routes which point to defaults.HostDevice
 		if r.LinkIndex == link.Attrs().Index {
@@ -111,17 +109,9 @@ func (ipam *IPAM) reserveLocalRoutes() {
 			continue
 		}
 
-		log.WithField("route", logfields.Repr(r)).Debug("Considering route")
-
-		if allocRange.Contains(r.Dst.IP) {
-			log.WithFields(logrus.Fields{
-				"route":            r.Dst,
-				logfields.V4Prefix: allocRange,
-			}).Info("Marking local route as no-alloc in node allocation prefix")
-
-			for ip := r.Dst.IP.Mask(r.Dst.Mask); r.Dst.Contains(ip); nextIP(ip) {
-				ipam.AllocateIP(ip, r.Dst.String())
-			}
+		log.WithField("route", r.Dst).Info("Blacklisting local route as no-alloc")
+		for ip := r.Dst.IP.Mask(r.Dst.Mask); r.Dst.Contains(ip); nextIP(ip) {
+			ipam.Blacklist(ip, "local route: "+r.Dst.String())
 		}
 	}
 }
@@ -132,4 +122,13 @@ func (ipam *IPAM) ReserveLocalRoutes() {
 	if ipam.IPv4Allocator != nil {
 		ipam.reserveLocalRoutes()
 	}
+}
+
+// Blacklist ensures that a certain IP is never allocated. It is preferred to
+// use Blacklist() instead of allocating the IP as the allocation block can
+// change and suddenly cover the IP to be blacklisted.
+func (ipam *IPAM) Blacklist(ip net.IP, owner string) {
+	ipam.allocatorMutex.Lock()
+	ipam.blacklist[ip.String()] = owner
+	ipam.allocatorMutex.Unlock()
 }

pkg/ipam/ipam_test.go
@@ -17,6 +17,7 @@
 package ipam
 
 import (
+	"net"
 	"testing"
 
 	"github.com/cilium/cilium/common/addressing"
@@ -62,4 +63,27 @@ func (s *IPAMSuite) TestLock(c *C) {
 
 	err = ipam.IPv4Allocator.Release(epipv4.IP())
 	c.Assert(err, IsNil)
+}
+
+func (s *IPAMSuite) TestBlackList(c *C) {
+	fakeAddressing := fake.NewNodeAddressing()
+	ipam := NewIPAM(fakeAddressing, Configuration{EnableIPv4: true, EnableIPv6: true})
+
+	// force copy of first possible IP in allocation range
+	ipv4 := net.ParseIP(fakeAddressing.IPv4().AllocationCIDR().IP.String())
+	nextIP(ipv4)
+
+	ipam.Blacklist(ipv4, "test")
+	err := ipam.AllocateIP(ipv4, "test")
+	c.Assert(err, Not(IsNil))
+	ipam.ReleaseIP(ipv4)
+
+	// force copy of first possible IP in allocation range
+	ipv6 := net.ParseIP(fakeAddressing.IPv6().AllocationCIDR().IP.String())
+	nextIP(ipv6)
+
+	ipam.Blacklist(ipv6, "test")
+	err = ipam.AllocateIP(ipv6, "test")
+	c.Assert(err, Not(IsNil))
+	ipam.ReleaseIP(ipv6)
 }

pkg/ipam/types.go
@@ -34,4 +34,6 @@ type IPAM struct {
 
 	// mutex covers access to all members of this struct
 	allocatorMutex lock.RWMutex
+
+	blacklist map[string]string
 }

pkg/workloads/config.go
@@ -92,7 +92,7 @@ func setupWorkload(workloadRuntime WorkloadRuntimeType, opts map[string]string) 
 }
 
 type allocatorInterface interface {
-	AllocateIP(ip net.IP, owner string) error
+	Blacklist(ip net.IP, owner string)
 }
 
 // Setup sets up the workload runtime specified in workloadRuntime and configures it

pkg/workloads/cri.go
@@ -297,9 +297,7 @@ func (c *criClient) IgnoreRunningWorkloads() {
 		if cIP == nil {
 			continue
 		}
-		if err := allocator.AllocateIP(cIP.IP(), "ignored workload"); err != nil {
-			continue
-		}
+		allocator.Blacklist(cIP.IP(), "ignored container: "+pod.GetId())
 		//TODO Release this address when the ignored container leaves
 		scopedLog.WithFields(logrus.Fields{
 			logfields.IPAddr: cIP.IP(),

pkg/workloads/docker.go
@@ -513,9 +513,7 @@ func (d *dockerClient) IgnoreRunningWorkloads() {
 		if cIP == nil {
 			continue
 		}
-		if err := allocator.AllocateIP(cIP.IP(), "ignored workload"); err != nil {
-			continue
-		}
+		allocator.Blacklist(cIP.IP(), "ignored docker container: "+cont.ID)
 		// TODO Release this address when the ignored container leaves
 		scopedLog.WithFields(logrus.Fields{
 			logfields.IPAddr: cIP.IP(),

commit 7b5c54c7fffdba227be47d3d2f28c644f3bde65e

pkg/ipam: protect map against concurrent access
Fixes: 58579f18280c ("ipam: Use Blacklist() to reserve IP in allocation range")
Signed-off-by: André Martins <andre@cilium.io>

pkg/ipam/allocator.go
@@ -122,6 +122,8 @@ func (ipam *IPAM) allocateNextFamily(family Family, allocator Allocator, owner s
 
 // AllocateNextFamily allocates the next IP of the requested address family
 func (ipam *IPAM) AllocateNextFamily(family Family, owner string) (ip net.IP, err error) {
+	ipam.allocatorMutex.Lock()
+	defer ipam.allocatorMutex.Unlock()
 	switch family {
 	case IPv6:
 		ip, err = ipam.allocateNextFamily(family, ipam.IPv6Allocator, owner)