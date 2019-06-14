cilium/cilium

commit a47befb438fa37904b446389c11d1187c4a5c8ee

agent: Add --enable-endpoint-routes option
Add a flag --enable-endpoint-routes which enables use of per endpoint routes
for all new endpoints. The existing cilium_host interface is still created and
required for L7 proxy redirection at ingress. Ingress policy of a local
endpoint continues to be evaluated at egress of the sending pod unless
`--force-local-policy-eval-at-source=false` is passed to the agent.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/option/config.go
@@ -470,6 +470,9 @@ const (
 	// SkipCRDCreation specifies whether the CustomResourceDefinition will be
 	// created by the daemon
 	SkipCRDCreation = "skip-crd-creation"
+
+	// EnableEndpointRoutes enables use of per endpoint routes
+	EnableEndpointRoutes = "enable-endpoint-routes"
 )
 
 // FQDNS variables
@@ -945,6 +948,9 @@ type DaemonConfig struct {
 	// SkipCRDCreation disables creation of the CustomResourceDefinition
 	// on daemon startup
 	SkipCRDCreation bool
+
+	// EnableEndpointRoutes enables use of per endpoint routes
+	EnableEndpointRoutes bool
 }
 
 var (
@@ -970,6 +976,7 @@ var (
 		EndpointInterfaceNamePrefix:  defaults.EndpointInterfaceNamePrefix,
 		BlacklistConflictingRoutes:   defaults.BlacklistConflictingRoutes,
 		ForceLocalPolicyEvalAtSource: defaults.ForceLocalPolicyEvalAtSource,
+		EnableEndpointRoutes:         defaults.EnableEndpointRoutes,
 	}
 )
 

daemon/daemon_main.go
@@ -429,6 +429,9 @@ func init() {
 	flags.MarkHidden(option.LegacyDisableIPv4Name)
 	option.BindEnv(option.LegacyDisableIPv4Name)
 
+	flags.Bool(option.EnableEndpointRoutes, defaults.EnableEndpointRoutes, "Use per endpoint routes instead of routing via cilium_host")
+	option.BindEnv(option.EnableEndpointRoutes)
+
 	flags.Bool(option.EnableHealthChecking, defaults.EnableHealthChecking, "Enable connectivity health checking")
 	option.BindEnv(option.EnableHealthChecking)
 

daemon/endpoint.go
@@ -159,6 +159,21 @@ func (d *Daemon) errorDuringCreation(ep *endpoint.Endpoint, err error) (*endpoin
 // createEndpoint attempts to create the endpoint corresponding to the change
 // request that was specified.
 func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.EndpointChangeRequest) (*endpoint.Endpoint, int, error) {
+	if option.Config.EnableEndpointRoutes {
+		if epTemplate.DatapathConfiguration == nil {
+			epTemplate.DatapathConfiguration = &models.EndpointDatapathConfiguration{}
+		}
+
+		// Indicate to insert a per endpoint route instead of routing
+		// via cilium_host interface
+		epTemplate.DatapathConfiguration.InstallEndpointRoute = true
+
+		// Since routing occurs via endpoint interface directly, BPF
+		// program is needed on that device at egress as BPF program on
+		// cilium_host interface is bypassed
+		epTemplate.DatapathConfiguration.RequireEgressProg = true
+	}
+
 	ep, err := endpoint.NewEndpointFromChangeModel(d.policy, epTemplate)
 	if err != nil {
 		return invalidDataError(ep, fmt.Errorf("unable to parse endpoint parameters: %s", err))

pkg/defaults/defaults.go
@@ -221,4 +221,8 @@ const (
 	// option.ForceLocalPolicyEvalAtSource. It is enabled by default to
 	// provide backwards compatibility, it can be disabled via an option
 	ForceLocalPolicyEvalAtSource = true
+
+	// EnableEndpointRoutes is the value for option.EnableEndpointRoutes.
+	// It is disabled by default for backwards compatibility.
+	EnableEndpointRoutes = false
 )

commit a6237401234c7d7cfe8114833c87d7177e6ce4bb

option: Fix --enable-endpoint-routes option
Fixes: a47befb438f ("agent: Add --enable-endpoint-routes option")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/option/config.go
@@ -1293,6 +1293,7 @@ func (c *DaemonConfig) Populate() {
 	c.EnableHostReachableServices = viper.GetBool(EnableHostReachableServices)
 	c.DockerEndpoint = viper.GetString(Docker)
 	c.EnableAutoDirectRouting = viper.GetBool(EnableAutoDirectRoutingName)
+	c.EnableEndpointRoutes = viper.GetBool(EnableEndpointRoutes)
 	c.EnableHealthChecking = viper.GetBool(EnableHealthChecking)
 	c.EnablePolicy = strings.ToLower(viper.GetString(EnablePolicy))
 	c.EnableTracing = viper.GetBool(EnableTracing)