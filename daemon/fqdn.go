cilium/cilium

commit 2935f589aba999bb1de8c7ccf7930b20a9aff5be

FQDN: Do not lock twice the endpoint Manager.
In DNS proxy all the information about the endpoint is already gathered
from the endpoint manager in the ServeDNS function, this function called
a few times to NotifyDNSMsg, and this function also lock the endpoint
manager to retrieve endpoint.

This commit send the endpointID and the endpointAddress in the callback,
so the number of locks in the EndpointManager decreases and make the
dns-proxy a bit faster. Also the lookups are using the endpointManager
map instead of the IP, so are a bit faster.

Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

daemon/fqdn.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -202,13 +203,13 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState, preCache
 	// StartProxySupport
 	proxy.DefaultDNSProxy, err = dnsproxy.StartDNSProxy("", uint16(option.Config.ToFQDNsProxyPort),
 		// LookupEPByIP
-		func(endpointIP net.IP) (endpointID string, err error) {
+		func(endpointIP net.IP) (endpoint *endpoint.Endpoint, err error) {
 			e := endpointmanager.LookupIPv4(endpointIP.String())
 			if e == nil {
-				return "", fmt.Errorf("Cannot find endpoint with IP %s", endpointIP.String())
+				return nil, fmt.Errorf("Cannot find endpoint with IP %s", endpointIP.String())
 			}
 
-			return e.StringID(), nil
+			return e, nil
 		},
 		// NotifyOnDNSMsg handles DNS data in the daemon by emitting monitor
 		// events, proxy metrics and storing DNS data in the DNS cache. This may
@@ -221,9 +222,8 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState, preCache
 		//   can lookup the endpoint related to it
 		// epAddr and serverAddr should match the original request, where epAddr is
 		// the source for egress (the only case current).
-		func(lookupTime time.Time, epAddr, serverAddr string, msg *dns.Msg, protocol string, allowed bool, stat dnsproxy.ProxyRequestContext) error {
+		func(lookupTime time.Time, ep *endpoint.Endpoint, serverAddr string, msg *dns.Msg, protocol string, allowed bool, stat dnsproxy.ProxyRequestContext) error {
 			var protoID = u8proto.ProtoIDs[strings.ToLower(protocol)]
-
 			var verdict accesslog.FlowVerdict
 			var reason string
 			metricError := metricErrorAllow
@@ -273,29 +273,17 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState, preCache
 					log.WithError(err).WithField(logfields.Port, serverPortStr).Error("cannot parse destination port")
 				}
 			}
-
-			var ep *endpoint.Endpoint
-			epIP, _, err := net.SplitHostPort(epAddr)
-			if err != nil {
-				log.WithError(err).Error("cannot extract endpoint IP from DNS request")
-				// We are always egress
-				ep.UpdateProxyStatistics("dns", uint16(serverPort), false, !msg.Response, accesslog.VerdictError)
-				endMetric()
-				return err
-			}
-			ep = endpointmanager.LookupIPv4(epIP)
 			if ep == nil {
 				// This is a hard fail. We cannot proceed because record.Log requires a
 				// non-nil ep, and we also don't want to insert this data into the
 				// cache if we don't know that an endpoint asked for it (this is
 				// asserted via ep != nil here and msg.Response && msg.Rcode ==
 				// dns.RcodeSuccess below).
-				err := fmt.Errorf("Cannot find matching endpoint for IP %s", epAddr)
+				err := errors.New("DNS request cannot be associated with an existing endpoint")
 				log.WithError(err).Error("cannot find matching endpoint")
 				endMetric()
 				return err
 			}
-
 			qname, responseIPs, TTL, CNAMEs, rcode, recordTypes, qTypes, err := dnsproxy.ExtractMsgDetails(msg)
 			if err != nil {
 				// This error is ok because all these values are used for reporting, or filling in the cache.
@@ -307,7 +295,7 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState, preCache
 				func(lr *logger.LogRecord) { lr.LogRecord.TransportProtocol = accesslog.TransportProtocol(protoID) },
 				logger.LogTags.Verdict(verdict, reason),
 				logger.LogTags.Addressing(logger.AddressingInfo{
-					SrcIPPort:   epAddr,
+					SrcIPPort:   ep.String(),
 					DstIPPort:   serverAddr,
 					SrcIdentity: ep.GetIdentity().Uint32(),
 				}),

pkg/fqdn/dnsproxy/proxy.go
@@ -23,6 +23,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/fqdn/regexpmap"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -116,11 +117,11 @@ type DNSProxy struct {
 
 // LookupEndpointIDByIPFunc wraps logic to lookup an endpoint with any backend.
 // See DNSProxy.LookupEndpointIDByIP for usage.
-type LookupEndpointIDByIPFunc func(ip net.IP) (endpointID string, err error)
+type LookupEndpointIDByIPFunc func(ip net.IP) (endpoint *endpoint.Endpoint, err error)
 
 // NotifyOnDNSMsgFunc handles propagating DNS response data
 // See DNSProxy.LookupEndpointIDByIP for usage.
-type NotifyOnDNSMsgFunc func(lookupTime time.Time, epAddr, serverAddr string, msg *dns.Msg, protocol string, allowed bool, stat ProxyRequestContext) error
+type NotifyOnDNSMsgFunc func(lookupTime time.Time, ep *endpoint.Endpoint, serverAddr string, msg *dns.Msg, protocol string, allowed bool, stat ProxyRequestContext) error
 
 // ProxyRequestContext proxy dns request context struct to send in the callback
 type ProxyRequestContext struct {
@@ -275,40 +276,39 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 	requestID := request.Id // keep the original request ID
 	qname := string(request.Question[0].Name)
 	protocol := w.LocalAddr().Network()
-	endpointAddr := w.RemoteAddr().String()
 	scopedLog := log.WithFields(logrus.Fields{
 		logfields.DNSName:      qname,
 		logfields.IPAddr:       w.RemoteAddr(),
 		logfields.DNSRequestID: request.Id})
-
 	scopedLog.Debug("Handling DNS query from endpoint")
 
-	endpointIPStr, _, err := net.SplitHostPort(w.RemoteAddr().String())
+	addr, _, err := net.SplitHostPort(w.RemoteAddr().String())
 	if err != nil {
 		scopedLog.WithError(err).Error("cannot extract endpoint IP from DNS request")
 		stat.Err = fmt.Errorf("Cannot extract endpoint IP from DNS request: %s", err)
 		stat.ProcessingTime.End(false)
-		p.NotifyOnDNSMsg(time.Now(), endpointAddr, "", request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), nil, "", request, protocol, false, stat)
 		p.sendRefused(scopedLog, w, request)
 		return
 	}
-	endpointID, err := p.LookupEndpointIDByIP(net.ParseIP(endpointIPStr))
+	ep, err := p.LookupEndpointIDByIP(net.ParseIP(addr))
 	if err != nil {
 		scopedLog.WithError(err).Error("cannot extract endpoint ID from DNS request")
 		stat.Err = fmt.Errorf("Cannot extract endpoint ID from DNS request: %s", err)
 		stat.ProcessingTime.End(false)
-		p.NotifyOnDNSMsg(time.Now(), endpointAddr, "", request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), nil, "", request, protocol, false, stat)
 		p.sendRefused(scopedLog, w, request)
 		return
 	}
-	scopedLog = scopedLog.WithField(logfields.EndpointID, endpointID)
+
+	scopedLog = scopedLog.WithField(logfields.EndpointID, ep.StringID())
 
 	targetServerAddr, err := p.lookupTargetDNSServer(w)
 	if err != nil {
 		scopedLog.WithError(err).Error("Cannot extract target server address to forward DNS request to")
 		stat.Err = fmt.Errorf("Cannot extract target server address to forward DNS request to: %s", err)
 		stat.ProcessingTime.End(false)
-		p.NotifyOnDNSMsg(time.Now(), endpointAddr, targetServerAddr, request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, false, stat)
 		p.sendRefused(scopedLog, w, request)
 		return
 	}
@@ -319,16 +319,16 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 	// Note: The cache doesn't know about the source of the DNS data (yet) and so
 	// it won't enforce any separation between results from different endpoints.
 	// This isn't ideal but we are trusting the DNS responses anyway.
-	if !p.CheckAllowed(qname, endpointID) {
+	if !p.CheckAllowed(qname, ep.StringID()) {
 		scopedLog.Debug("Rejecting DNS query from endpoint due to policy")
 		stat.Err = p.sendRefused(scopedLog, w, request)
 		stat.ProcessingTime.End(true)
-		p.NotifyOnDNSMsg(time.Now(), endpointAddr, targetServerAddr, request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, false, stat)
 		return
 	}
 
 	scopedLog.Debug("Forwarding DNS request for a name that is allowed")
-	p.NotifyOnDNSMsg(time.Now(), endpointAddr, targetServerAddr, request, protocol, true, stat)
+	p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, true, stat)
 
 	// Keep the same L4 protocol. This handles DNS re-requests over TCP, for
 	// requests that were too large for UDP.
@@ -342,7 +342,7 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 		scopedLog.Error("Cannot parse DNS proxy client network to select forward client")
 		stat.Err = fmt.Errorf("Cannot parse DNS proxy client network to select forward client: %s", err)
 		stat.ProcessingTime.End(true)
-		p.NotifyOnDNSMsg(time.Now(), endpointAddr, targetServerAddr, request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, false, stat)
 		p.sendRefused(scopedLog, w, request)
 		return
 	}
@@ -361,7 +361,7 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 			p.sendRefused(scopedLog, w, request)
 			stat.Err = fmt.Errorf("Cannot forward proxied DNS lookup: %s", err)
 		}
-		p.NotifyOnDNSMsg(time.Now(), endpointAddr, targetServerAddr, request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, false, stat)
 		return
 	}
 
@@ -375,13 +375,13 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 	if err != nil {
 		scopedLog.WithError(err).Error("Cannot forward proxied DNS response")
 		stat.Err = fmt.Errorf("Cannot forward proxied DNS response: %s", err)
-		p.NotifyOnDNSMsg(time.Now(), endpointAddr, targetServerAddr, response, protocol, true, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, response, protocol, true, stat)
 	}
 
 	// Note: This may block and it is safer to do it after we have written out
 	// the DNS response to the pod.
 	scopedLog.Debug("Notifying with DNS response to original DNS query")
-	p.NotifyOnDNSMsg(time.Now(), endpointAddr, targetServerAddr, response, protocol, true, stat)
+	p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, response, protocol, true, stat)
 }
 
 // sendRefused creates and sends a REFUSED response for request to w

pkg/fqdn/dnsproxy/proxy_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/fqdn/regexpmap"
 	"github.com/cilium/cilium/pkg/option"
 
@@ -81,10 +82,11 @@ func (s *DNSProxyTestSuite) SetUpSuite(c *C) {
 	c.Assert(s.dnsServer, Not(IsNil), Commentf("unable to setup DNS server"))
 
 	proxy, err := StartDNSProxy("", 0,
-		func(ip net.IP) (endpointID string, err error) {
-			return "endpoint1", nil
+		func(ip net.IP) (*endpoint.Endpoint, error) {
+			ep := endpoint.NewEndpointWithState(123, endpoint.StateReady)
+			return ep, nil
 		},
-		func(lookupTime time.Time, srcAddr, dstAddr string, msg *dns.Msg, protocol string, allowed bool, stat ProxyRequestContext) error {
+		func(lookupTime time.Time, ep *endpoint.Endpoint, dstAddr string, msg *dns.Msg, protocol string, allowed bool, stat ProxyRequestContext) error {
 			return nil
 		})
 	c.Assert(err, IsNil, Commentf("error starting DNS Proxy"))
@@ -169,7 +171,7 @@ func (s *DNSProxyTestSuite) TestRejectNonMatchingNoDomainResponse(c *C) {
 func (s *DNSProxyTestSuite) TestRespondViaCorrectProtocol(c *C) {
 	// respond with an actual answer for the query. This also tests that the
 	// connection was forwarded via the correct protocol (tcp/udp)
-	s.proxy.AddAllowed("c[il]{3,3}um[.]io[.]", "endpoint1")
+	s.proxy.AddAllowed("c[il]{3,3}um[.]io[.]", "123")
 	request := new(dns.Msg)
 	request.SetQuestion("cilium.io.", dns.TypeA)
 	response, _, err := s.dnsTCPClient.Exchange(request, s.proxy.TCPServer.Listener.Addr().String())
@@ -179,22 +181,22 @@ func (s *DNSProxyTestSuite) TestRespondViaCorrectProtocol(c *C) {
 }
 
 func (s *DNSProxyTestSuite) TestRespondMixedCaseInRequest(c *C) {
-	s.proxy.AddAllowed("c[il]{3,3}um[.]io[.]", "endpoint1")
+	s.proxy.AddAllowed("c[il]{3,3}um[.]io[.]", "123")
 	request := new(dns.Msg)
 	request.SetQuestion("CILIUM.io.", dns.TypeA)
 	response, _, err := s.dnsTCPClient.Exchange(request, s.proxy.TCPServer.Listener.Addr().String())
 	c.Assert(err, IsNil, Commentf("DNS request from test client failed when it should succeed"))
-	c.Assert(len(response.Answer), Equals, 1, Commentf("Proxy returned incorrect number of answer RRs", response.Answer))
+	c.Assert(len(response.Answer), Equals, 1, Commentf("Proxy returned incorrect number of answer RRs: %+v", response.Answer))
 	c.Assert(response.Answer[0].String(), Equals, "CILIUM.io.\t60\tIN\tA\t1.1.1.1", Commentf("Proxy returned incorrect RRs"))
 }
 
 func (s *DNSProxyTestSuite) TestRespondMixedCaseInResponse(c *C) {
-	s.proxy.AddAllowed("c[IL]{3,3}um[.]io[.]", "endpoint1")
+	s.proxy.AddAllowed("c[IL]{3,3}um[.]io[.]", "123")
 	request := new(dns.Msg)
 	request.SetQuestion("ciliuM.io.", dns.TypeA)
 	response, _, err := s.dnsTCPClient.Exchange(request, s.proxy.TCPServer.Listener.Addr().String())
 	c.Assert(err, IsNil, Commentf("DNS request from test client failed when it should succeed"))
-	c.Assert(len(response.Answer), Equals, 1, Commentf("Proxy returned incorrect number of answer RRs", response.Answer))
+	c.Assert(len(response.Answer), Equals, 1, Commentf("Proxy returned incorrect number of answer RRs %+v", response.Answer))
 	c.Assert(response.Answer[0].String(), Equals, "ciliuM.io.\t60\tIN\tA\t1.1.1.1", Commentf("Proxy returned incorrect RRs"))
 }
 

commit e5162c04318109b9432d0c42680be84764dd28d8

fqdn: correctly populate Source IP and Port in `notifyOnDNSMsg`
Before, we were calling `endpoint.String()` for each DNS request going through
the DNS proxy and providing that as the value for `SrcIPPort` for the
`AddressingInfo` portion of the corresponding `LogRecord` for said DNS request.
This in and of itself was a bug (the endpoint string is not a valid hostname /
port tuple). On top of this, this was a performance bottleneck, because of the
amount of CPU being used to marshal the endpoint into JSON and convert it into a
string, per the output of running `gops pprof-cpu <cilium-agent PID>` and
running `list notifyOnDNSMsg`:

```
ROUTINE ======================== main.(*Daemon).notifyOnDNSMsg in /home/vagrant/go/src/github.com/cilium/cilium/daemon/fqdn.go
      10ms      2.75s (flat, cum) 47.33% of Total
         .          .    399:		err := errors.New("DNS request cannot be associated with an existing endpoint")
         .          .    400:		log.WithError(err).Error("cannot find matching endpoint")
         .          .    401:		endMetric()
         .          .    402:		return err
         .          .    403:	}
         .       10ms    404:	qname, responseIPs, TTL, CNAMEs, rcode, recordTypes, qTypes, err := dnsproxy.ExtractMsgDetails(msg)
         .          .    405:	if err != nil {
         .          .    406:		// This error is ok because all these values are used for reporting, or filling in the cache.
         .          .    407:		log.WithError(err).Error("cannot extract DNS message details")
         .          .    408:	}
         .          .    409:
         .       60ms    410:	ep.UpdateProxyStatistics("dns", uint16(serverPort), false, !msg.Response, verdict)
         .      240ms    411:	record := logger.NewLogRecord(proxy.DefaultEndpointInfoRegistry, ep, flowType, false,
         .          .    412:		func(lr *logger.LogRecord) { lr.LogRecord.TransportProtocol = accesslog.TransportProtocol(protoID) },
         .          .    413:		logger.LogTags.Verdict(verdict, reason),
         .          .    414:		logger.LogTags.Addressing(logger.AddressingInfo{
         .      1.81s    415:			SrcIPPort:   ep.String(),
         .          .    416:			DstIPPort:   serverAddr,
         .          .    417:			SrcIdentity: ep.GetIdentity().Uint32(),
         .          .    418:		}),
```

Instead, provide the IP/Port tuple from the initial request into all
`NotifyOnDNSMsg` invocations for the `DNSProxy`. This correctly populates the
`SrcIPPort` field, and improves performance due to not having to marshal the
endpoint. When running the aforementioned pprof commands again, the performance
is clearly much better when running cilium against the same set of pods
making DNS requests at the same cadence:

```
ROUTINE ======================== main.(*Daemon).notifyOnDNSMsg in /home/vagrant/go/src/github.com/cilium/cilium/daemon/fqdn.go
         0      460ms (flat, cum) 17.83% of Total
         .          .    405:	if err != nil {
         .          .    406:		// This error is ok because all these values are used for reporting, or filling in the cache.
         .          .    407:		log.WithError(err).Error("cannot extract DNS message details")
         .          .    408:	}
         .          .    409:
         .       30ms    410:	ep.UpdateProxyStatistics("dns", uint16(serverPort), false, !msg.Response, verdict)
         .       70ms    411:	record := logger.NewLogRecord(proxy.DefaultEndpointInfoRegistry, ep, flowType, false,
         .          .    412:		func(lr *logger.LogRecord) { lr.LogRecord.TransportProtocol = accesslog.TransportProtocol(protoID) },
         .          .    413:		logger.LogTags.Verdict(verdict, reason),
         .          .    414:		logger.LogTags.Addressing(logger.AddressingInfo{
         .          .    415:			SrcIPPort:   epIPPort,
         .          .    416:			DstIPPort:   serverAddr,
         .          .    417:			SrcIdentity: ep.GetIdentity().Uint32(),
         .          .    418:		}),

```

Fixes: 2935f589ab

daemon/fqdn.go
@@ -337,9 +337,9 @@ func (d *Daemon) lookupEPByIP(endpointIP net.IP) (endpoint *endpoint.Endpoint, e
 // - Report the verdict in a monitor event and emit proxy metrics
 // - Insert the DNS data into the cache when msg is a DNS response and we
 //   can lookup the endpoint related to it
-// epAddr and serverAddr should match the original request, where epAddr is
+// epIPPort and serverAddr should match the original request, where epAddr is
 // the source for egress (the only case current).
-func (d *Daemon) notifyOnDNSMsg(lookupTime time.Time, ep *endpoint.Endpoint, serverAddr string, msg *dns.Msg, protocol string, allowed bool, stat dnsproxy.ProxyRequestContext) error {
+func (d *Daemon) notifyOnDNSMsg(lookupTime time.Time, ep *endpoint.Endpoint, epIPPort string, serverAddr string, msg *dns.Msg, protocol string, allowed bool, stat dnsproxy.ProxyRequestContext) error {
 	var protoID = u8proto.ProtoIDs[strings.ToLower(protocol)]
 	var verdict accesslog.FlowVerdict
 	var reason string
@@ -412,7 +412,7 @@ func (d *Daemon) notifyOnDNSMsg(lookupTime time.Time, ep *endpoint.Endpoint, ser
 		func(lr *logger.LogRecord) { lr.LogRecord.TransportProtocol = accesslog.TransportProtocol(protoID) },
 		logger.LogTags.Verdict(verdict, reason),
 		logger.LogTags.Addressing(logger.AddressingInfo{
-			SrcIPPort:   ep.String(),
+			SrcIPPort:   epIPPort,
 			DstIPPort:   serverAddr,
 			SrcIdentity: ep.GetIdentity().Uint32(),
 		}),

pkg/fqdn/dnsproxy/proxy.go
@@ -121,7 +121,7 @@ type LookupEndpointIDByIPFunc func(ip net.IP) (endpoint *endpoint.Endpoint, err 
 
 // NotifyOnDNSMsgFunc handles propagating DNS response data
 // See DNSProxy.LookupEndpointIDByIP for usage.
-type NotifyOnDNSMsgFunc func(lookupTime time.Time, ep *endpoint.Endpoint, serverAddr string, msg *dns.Msg, protocol string, allowed bool, stat ProxyRequestContext) error
+type NotifyOnDNSMsgFunc func(lookupTime time.Time, ep *endpoint.Endpoint, epIPPort string, serverAddr string, msg *dns.Msg, protocol string, allowed bool, stat ProxyRequestContext) error
 
 // ProxyRequestContext proxy dns request context struct to send in the callback
 type ProxyRequestContext struct {
@@ -288,12 +288,13 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 		logfields.DNSRequestID: request.Id})
 	scopedLog.Debug("Handling DNS query from endpoint")
 
-	addr, _, err := net.SplitHostPort(w.RemoteAddr().String())
+	epIPPort := w.RemoteAddr().String()
+	addr, _, err := net.SplitHostPort(epIPPort)
 	if err != nil {
 		scopedLog.WithError(err).Error("cannot extract endpoint IP from DNS request")
 		stat.Err = fmt.Errorf("Cannot extract endpoint IP from DNS request: %s", err)
 		stat.ProcessingTime.End(false)
-		p.NotifyOnDNSMsg(time.Now(), nil, "", request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), nil, epIPPort, "", request, protocol, false, stat)
 		p.sendRefused(scopedLog, w, request)
 		return
 	}
@@ -302,7 +303,7 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 		scopedLog.WithError(err).Error("cannot extract endpoint ID from DNS request")
 		stat.Err = fmt.Errorf("Cannot extract endpoint ID from DNS request: %s", err)
 		stat.ProcessingTime.End(false)
-		p.NotifyOnDNSMsg(time.Now(), nil, "", request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), nil, epIPPort, "", request, protocol, false, stat)
 		p.sendRefused(scopedLog, w, request)
 		return
 	}
@@ -314,7 +315,7 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 		scopedLog.WithError(err).Error("Cannot extract target server address to forward DNS request to")
 		stat.Err = fmt.Errorf("Cannot extract target server address to forward DNS request to: %s", err)
 		stat.ProcessingTime.End(false)
-		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, epIPPort, targetServerAddr, request, protocol, false, stat)
 		p.sendRefused(scopedLog, w, request)
 		return
 	}
@@ -329,12 +330,12 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 		scopedLog.Debug("Rejecting DNS query from endpoint due to policy")
 		stat.Err = p.sendRefused(scopedLog, w, request)
 		stat.ProcessingTime.End(true)
-		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, epIPPort, targetServerAddr, request, protocol, false, stat)
 		return
 	}
 
 	scopedLog.Debug("Forwarding DNS request for a name that is allowed")
-	p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, true, stat)
+	p.NotifyOnDNSMsg(time.Now(), ep, epIPPort, targetServerAddr, request, protocol, true, stat)
 
 	// Keep the same L4 protocol. This handles DNS re-requests over TCP, for
 	// requests that were too large for UDP.
@@ -348,7 +349,7 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 		scopedLog.Error("Cannot parse DNS proxy client network to select forward client")
 		stat.Err = fmt.Errorf("Cannot parse DNS proxy client network to select forward client: %s", err)
 		stat.ProcessingTime.End(true)
-		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, epIPPort, targetServerAddr, request, protocol, false, stat)
 		p.sendRefused(scopedLog, w, request)
 		return
 	}
@@ -367,7 +368,7 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 			p.sendRefused(scopedLog, w, request)
 			stat.Err = fmt.Errorf("Cannot forward proxied DNS lookup: %s", err)
 		}
-		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, request, protocol, false, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, epIPPort, targetServerAddr, request, protocol, false, stat)
 		return
 	}
 
@@ -381,13 +382,13 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 	if err != nil {
 		scopedLog.WithError(err).Error("Cannot forward proxied DNS response")
 		stat.Err = fmt.Errorf("Cannot forward proxied DNS response: %s", err)
-		p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, response, protocol, true, stat)
+		p.NotifyOnDNSMsg(time.Now(), ep, epIPPort, targetServerAddr, response, protocol, true, stat)
 	}
 
 	// Note: This may block and it is safer to do it after we have written out
 	// the DNS response to the pod.
 	scopedLog.Debug("Notifying with DNS response to original DNS query")
-	p.NotifyOnDNSMsg(time.Now(), ep, targetServerAddr, response, protocol, true, stat)
+	p.NotifyOnDNSMsg(time.Now(), ep, epIPPort, targetServerAddr, response, protocol, true, stat)
 }
 
 // sendRefused creates and sends a REFUSED response for request to w

pkg/fqdn/dnsproxy/proxy_test.go
@@ -89,7 +89,7 @@ func (s *DNSProxyTestSuite) SetUpSuite(c *C) {
 			ep := endpoint.NewEndpointWithState(s.repo, 123, endpoint.StateReady)
 			return ep, nil
 		},
-		func(lookupTime time.Time, ep *endpoint.Endpoint, dstAddr string, msg *dns.Msg, protocol string, allowed bool, stat ProxyRequestContext) error {
+		func(lookupTime time.Time, ep *endpoint.Endpoint, epIPPort string, dstAddr string, msg *dns.Msg, protocol string, allowed bool, stat ProxyRequestContext) error {
 			return nil
 		})
 	c.Assert(err, IsNil, Commentf("error starting DNS Proxy"))