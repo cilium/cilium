cb232659e7a7105d977c8843a1f65e491d811152
Thomas Graf <thomas@cilium.io>
2019-06-22 09:57:37 -0700

    ipam: Add CRD-backed allocator
    This adds an initial, minimal version of a CiliumNode CRD-backed IP allocator.
    It is enabled by setting --ipam=crd and will hand out IPs based on the IPs made
    available via the custom resource.
    
    An outside operator or user can provide IPs via a map spec.ipam.avaiable. The
    agent will use those IPs for allocation as they become available an update the
    map status.ipam.used to list all used IPs.
    
    This is a minimal version targeted for use by the operator for ENI support.
    Further versions of this could become more sophisticated to also target manual
    configuration use cases or integration into other IPAM systems.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/k8s/const.go
+++ pkg/k8s/const.go
@@ -26,4 +26,7 @@ const (
 	// EnvNodeNameSpec is the environment label used by Kubernetes to
 	// specify the node's name.
 	EnvNodeNameSpec = "K8S_NODE_NAME"
+
+	// DefaultNamespace is the name of the Kubernetes default namespace
+	DefaultNamespace = "default"
 )
