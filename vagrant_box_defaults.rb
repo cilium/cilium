e78f798d48f96e6d7f2d724eabde0b27431eafb8
Daniel Borkmann <daniel@iogearbox.net>
2019-06-24 23:03:34 +0000

    cilium: update to developer vm to image 157
    Same process to get in latest bpftool and iproute2 changes.
    For net-next, the build 28 went fine, but CI is hitting the
    docker bug in https://github.com/moby/moby/issues/34672,
    leave it out for now.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- vagrant_box_defaults.rb
+++ vagrant_box_defaults.rb
@@ -3,6 +3,6 @@
 Vagrant.require_version ">= 2.2.0"
 
 $SERVER_BOX = "cilium/ubuntu-dev"
-$SERVER_VERSION= "151"
+$SERVER_VERSION= "157"
 $NETNEXT_SERVER_BOX= "cilium/ubuntu-next"
 $NETNEXT_SERVER_VERSION= "23"
