8c2a33d5fcc05d567d08cc5c8094ac474fd4717a
André Martins <andre@cilium.io>
2019-06-25 22:08:34 +0200

    test: set k8s 1.15 as default k8s version
    Fixes: 4794445b0e02 ("test: test against 1.15.0")
    Signed-off-by: André Martins <andre@cilium.io>

--- test/test_suite_test.go
+++ test/test_suite_test.go
@@ -39,7 +39,7 @@ import (
 var (
 	log             = logging.DefaultLogger
 	DefaultSettings = map[string]string{
-		"K8S_VERSION": "1.14",
+		"K8S_VERSION": "1.15",
 	}
 	k8sNodesEnv         = "K8S_NODES"
 	commandsLogFileName = "cmds.log"
