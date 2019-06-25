cilium/cilium

commit f71d87a71c9955863b71904d67531a2fc88c2b2a

endpointmanager: signal when work is done
Provide new ways of signaling when various operations are completed by the
endpointmanager for the following cases:

* Removal of endpoint: when an endpoint is removed, all references to it must
also be removed from the policy repository in the Daemon as well. Block on
releasing the endpoint ID until it is removed from all caches in the policy
repository, but do so in an asynchronous manner so that `endpointmanager.Remove`
does not get stuck for a long period of time waiting for said work to complete.
* Provide new functions to indicate when regenerations for a set of endpoints
have been enqueued for each endpoint. This is useful for ensuring that a given
regeneration is queued up before some other event, e.g., incrementing the policy
revision for the endpoint for a later revision than the regeneration which is
being queued.

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/endpointmanager/manager.go
@@ -200,13 +200,7 @@ func UpdateReferences(ep *endpoint.Endpoint) {
 	updateReferences(ep)
 }
 
-// Remove removes the endpoint from the global maps.
-// Must be called with ep.Mutex.RLock held.
-func Remove(ep *endpoint.Endpoint) {
-	mutex.Lock()
-	defer mutex.Unlock()
-	delete(endpoints, ep.ID)
-
+func releaseID(ep *endpoint.Endpoint) {
 	if err := endpointid.Release(ep.ID); err != nil {
 		// While restoring, endpoint IDs may not have been reused yet.
 		// Failure to release means that the endpoint ID was not reused
@@ -220,6 +214,63 @@ func Remove(ep *endpoint.Endpoint) {
 			log.WithError(err).WithField("state", state).Warning("Unable to release endpoint ID")
 		}
 	}
+}
+
+// RuleCacheOwner is an interface representing anything which needs to be
+// notified upon endpoint removal from the endpointmanager.
+type RuleCacheOwner interface {
+	// ClearPolicyConsumers removes references to the specified id from the
+	// policy rules managed by RuleCacheOwner.
+	ClearPolicyConsumers(id uint16) *sync.WaitGroup
+}
+
+// WaitEndpointRemoved waits until all operations associated with Remove of
+// the endpoint have been completed.
+func WaitEndpointRemoved(ep *endpoint.Endpoint, owner RuleCacheOwner) {
+	select {
+	case <-Remove(ep, owner):
+		return
+	}
+}
+
+// Remove removes the endpoint from the global maps and releases the node-local
+// ID allocated for the endpoint.
+// Must be called with ep.Mutex.RLock held. Releasing of the ID of the endpoint
+// is done asynchronously. Once the ID of the endpoint is released, the returned
+// channel is closed.
+func Remove(ep *endpoint.Endpoint, owner RuleCacheOwner) <-chan struct{} {
+
+	epRemoved := make(chan struct{})
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	// This must be done before the ID is released for the endpoint!
+	delete(endpoints, ep.ID)
+
+	go func(ep *endpoint.Endpoint) {
+
+		// The endpoint's EventQueue may not be stopped yet (depending on whether
+		// the caller of the EventQueue has stopped it or not). Call it here
+		// to be safe so that ep.IsDrained() does not hang forever.
+		ep.EventQueue.Stop()
+
+		// Wait for no more events (primarily regenerations) to be occurring for
+		// this endpoint.
+		<-ep.EventQueue.IsDrained()
+
+		// Now that queue is drained, no more regenerations are occurring for
+		// this endpoint. Safe to remove references to it from the policy
+		// repository
+		wg := owner.ClearPolicyConsumers(ep.ID)
+
+		// Need to wait for cleanup for consumers of endpoint ID to be done
+		// before endpoint ID is released so that there aren't stale references
+		// laying around if another endpoint with the same ID is created.
+		wg.Wait()
+		releaseID(ep)
+		close(epRemoved)
+	}(ep)
 
 	if ep.ContainerID != "" {
 		delete(endpointsAux, endpointid.NewID(endpointid.ContainerIdPrefix, ep.ContainerID))
@@ -244,6 +295,7 @@ func Remove(ep *endpoint.Endpoint) {
 	if podName := ep.GetK8sNamespaceAndPodNameLocked(); podName != "" {
 		delete(endpointsAux, endpointid.NewID(endpointid.PodNamePrefix, podName))
 	}
+	return epRemoved
 }
 
 // RemoveAll removes all endpoints from the global maps.
@@ -346,23 +398,54 @@ func RegenerateAllEndpoints(owner endpoint.Owner, regenMetadata *endpoint.Extern
 
 	log.Infof("regenerating all endpoints due to %s", regenMetadata.Reason)
 	for _, ep := range eps {
-		go func(ep *endpoint.Endpoint, wg *sync.WaitGroup) {
-			if err := ep.LockAlive(); err != nil {
-				log.WithError(err).Warnf("Endpoint disappeared while queued to be regenerated: %s", regenMetadata.Reason)
-				ep.LogStatus(endpoint.Policy, endpoint.Failure, "Error while handling policy updates for endpoint: "+err.Error())
-			} else {
-				regen := ep.SetStateLocked(endpoint.StateWaitingToRegenerate, fmt.Sprintf("Triggering endpoint regeneration due to %s", regenMetadata.Reason))
-				ep.Unlock()
-				if regen {
-					// Regenerate logs status according to the build success/failure
-					<-ep.Regenerate(owner, regenMetadata)
-				}
-			}
-			wg.Done()
-		}(ep, &wg)
+		go regenerateEndpointBlocking(owner, ep, regenMetadata, &wg)
 	}
 
 	return &wg
+}
+
+func regenerateEndpointBlocking(owner endpoint.Owner, ep *endpoint.Endpoint, regenMetadata *endpoint.ExternalRegenerationMetadata, wg *sync.WaitGroup) {
+	if err := ep.LockAlive(); err != nil {
+		log.WithError(err).Warnf("Endpoint disappeared while queued to be regenerated: %s", regenMetadata.Reason)
+		ep.LogStatus(endpoint.Policy, endpoint.Failure, "Error while handling policy updates for endpoint: "+err.Error())
+	} else {
+		regen := ep.SetStateLocked(endpoint.StateWaitingToRegenerate, fmt.Sprintf("Triggering endpoint regeneration due to %s", regenMetadata.Reason))
+		ep.Unlock()
+		if regen {
+			// Regenerate logs status according to the build success/failure
+			<-ep.Regenerate(owner, regenMetadata)
+		}
+	}
+	wg.Done()
+}
+
+func regenerateEndpointNonBlocking(owner endpoint.Owner, ep *endpoint.Endpoint, regenMetadata *endpoint.ExternalRegenerationMetadata) {
+	if err := ep.LockAlive(); err != nil {
+		log.WithError(err).Warnf("Endpoint disappeared while queued to be regenerated: %s", regenMetadata.Reason)
+		ep.LogStatus(endpoint.Policy, endpoint.Failure, "Error while handling policy updates for endpoint: "+err.Error())
+	} else {
+		regen := ep.SetStateLocked(endpoint.StateWaitingToRegenerate, fmt.Sprintf("Triggering endpoint regeneration due to %s", regenMetadata.Reason))
+		ep.Unlock()
+		if regen {
+			// Regenerate logs status according to the build success/failure
+			ep.Regenerate(owner, regenMetadata)
+		}
+	}
+}
+
+// RegenerateEndpointSetSignalWhenEnqueued regenerates the endpoints represented
+// by endpointIDs. It signals to the provided WaitGroup when all of the endpoints
+// in said set have had regenerations queued up.
+func RegenerateEndpointSetSignalWhenEnqueued(owner endpoint.Owner, regenMetadata *endpoint.ExternalRegenerationMetadata, endpointIDs map[uint16]struct{}, wg *sync.WaitGroup) {
+	wg.Add(len(endpointIDs))
+
+	for endpointID := range endpointIDs {
+		ep := endpoints[endpointID]
+		go func() {
+			regenerateEndpointNonBlocking(owner, ep, regenMetadata)
+			wg.Done()
+		}()
+	}
 }
 
 // HasGlobalCT returns true if the endpoints have a global CT, false otherwise.

cilium-health/launch/endpoint.go
@@ -268,7 +268,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, n *node.Node, mtuConfig mtu.Configur
 		return nil, err
 	}
 	if !ep.SetStateLocked(endpoint.StateWaitingToRegenerate, "initial build of health endpoint") {
-		endpointmanager.Remove(ep)
+		endpointmanager.Remove(ep, owner)
 		ep.Unlock()
 		return nil, fmt.Errorf("unable to transition health endpoint to WaitingToRegenerate state")
 	}
@@ -279,7 +279,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, n *node.Node, mtuConfig mtu.Configur
 		Reason: "health daemon bootstrap",
 	})
 	if !buildSuccessful {
-		endpointmanager.Remove(ep)
+		endpointmanager.Remove(ep, owner)
 		return nil, fmt.Errorf("unable to build health endpoint")
 	}
 

daemon/endpoint.go
@@ -591,7 +591,7 @@ func (d *Daemon) deleteEndpointQuiet(ep *endpoint.Endpoint, releaseIP bool) []er
 
 	// Remove the endpoint before we clean up. This ensures it is no longer
 	// listed or queued for rebuilds.
-	endpointmanager.Remove(ep)
+	endpointmanager.Remove(ep, d)
 
 	// If dry mode is enabled, no changes to BPF maps are performed
 	if !option.Config.DryMode {

pkg/endpointmanager/manager_test.go
@@ -18,6 +18,7 @@ package endpointmanager
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -37,8 +38,16 @@ type EndpointManagerSuite struct{}
 
 var _ = Suite(&EndpointManagerSuite{})
 
+type DummyRuleCacheOwner struct{}
+
+func (d *DummyRuleCacheOwner) ClearPolicyConsumers(id uint16) *sync.WaitGroup {
+	return &sync.WaitGroup{}
+}
+
+var ruleCacheOwner *DummyRuleCacheOwner
+
 func (s *EndpointManagerSuite) TestLookup(c *C) {
-	ep := &endpoint.Endpoint{}
+	ep := endpoint.NewEndpointWithState(10, endpoint.StateReady)
 	ep.UpdateLogger(nil)
 	type args struct {
 		id string
@@ -91,7 +100,7 @@ func (s *EndpointManagerSuite) TestLookup(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.ID = 0
 			},
 		},
@@ -113,7 +122,7 @@ func (s *EndpointManagerSuite) TestLookup(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.ID = 0
 			},
 		},
@@ -136,7 +145,7 @@ func (s *EndpointManagerSuite) TestLookup(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.ContainerID = ""
 			},
 		},
@@ -159,7 +168,7 @@ func (s *EndpointManagerSuite) TestLookup(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.DockerEndpointID = ""
 			},
 		},
@@ -182,7 +191,7 @@ func (s *EndpointManagerSuite) TestLookup(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.ContainerName = ""
 			},
 		},
@@ -206,7 +215,7 @@ func (s *EndpointManagerSuite) TestLookup(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.SetK8sPodName("")
 			},
 		},
@@ -231,7 +240,7 @@ func (s *EndpointManagerSuite) TestLookup(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.IPv4 = nil
 			},
 		},
@@ -284,7 +293,7 @@ func (s *EndpointManagerSuite) TestLookup(c *C) {
 }
 
 func (s *EndpointManagerSuite) TestLookupCiliumID(c *C) {
-	ep := &endpoint.Endpoint{}
+	ep := endpoint.NewEndpointWithState(2, endpoint.StateReady)
 	ep.UpdateLogger(nil)
 	type args struct {
 		id uint16
@@ -318,7 +327,7 @@ func (s *EndpointManagerSuite) TestLookupCiliumID(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.ID = 0
 			},
 		},
@@ -351,7 +360,7 @@ func (s *EndpointManagerSuite) TestLookupCiliumID(c *C) {
 }
 
 func (s *EndpointManagerSuite) TestLookupContainerID(c *C) {
-	ep := &endpoint.Endpoint{}
+	ep := endpoint.NewEndpointWithState(3, endpoint.StateReady)
 	ep.UpdateLogger(nil)
 	type args struct {
 		id string
@@ -385,7 +394,7 @@ func (s *EndpointManagerSuite) TestLookupContainerID(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.SetContainerID("")
 			},
 		},
@@ -418,7 +427,7 @@ func (s *EndpointManagerSuite) TestLookupContainerID(c *C) {
 }
 
 func (s *EndpointManagerSuite) TestLookupIPv4(c *C) {
-	ep := &endpoint.Endpoint{}
+	ep := endpoint.NewEndpointWithState(4, endpoint.StateReady)
 	ep.UpdateLogger(nil)
 	type args struct {
 		ip string
@@ -454,7 +463,7 @@ func (s *EndpointManagerSuite) TestLookupIPv4(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.IPv4 = nil
 			},
 		},
@@ -487,7 +496,7 @@ func (s *EndpointManagerSuite) TestLookupIPv4(c *C) {
 }
 
 func (s *EndpointManagerSuite) TestLookupPodName(c *C) {
-	ep := &endpoint.Endpoint{}
+	ep := endpoint.NewEndpointWithState(5, endpoint.StateReady)
 	ep.UpdateLogger(nil)
 	type args struct {
 		podName string
@@ -522,7 +531,7 @@ func (s *EndpointManagerSuite) TestLookupPodName(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.IPv4 = nil
 			},
 		},
@@ -555,7 +564,7 @@ func (s *EndpointManagerSuite) TestLookupPodName(c *C) {
 }
 
 func (s *EndpointManagerSuite) TestUpdateReferences(c *C) {
-	ep := &endpoint.Endpoint{}
+	ep := endpoint.NewEndpointWithState(6, endpoint.StateReady)
 	ep.UpdateLogger(nil)
 	type args struct {
 		ep *endpoint.Endpoint
@@ -598,7 +607,7 @@ func (s *EndpointManagerSuite) TestUpdateReferences(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.SetK8sNamespace("")
 				ep.SetK8sPodName("")
 				ep.SetContainerID("")
@@ -635,7 +644,7 @@ func (s *EndpointManagerSuite) TestUpdateReferences(c *C) {
 }
 
 func (s *EndpointManagerSuite) TestRemove(c *C) {
-	ep := &endpoint.Endpoint{}
+	ep := endpoint.NewEndpointWithState(7, endpoint.StateReady)
 	ep.UpdateLogger(nil)
 	type args struct {
 	}
@@ -675,7 +684,7 @@ func (s *EndpointManagerSuite) TestRemove(c *C) {
 }
 
 func (s *EndpointManagerSuite) TestHasGlobalCT(c *C) {
-	ep := &endpoint.Endpoint{}
+	ep := endpoint.NewEndpointWithState(1, endpoint.StateReady)
 	ep.UpdateLogger(nil)
 	type args struct {
 		ep *endpoint.Endpoint
@@ -703,7 +712,7 @@ func (s *EndpointManagerSuite) TestHasGlobalCT(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.ID = 0
 				ep.Options = nil
 			},
@@ -722,7 +731,7 @@ func (s *EndpointManagerSuite) TestHasGlobalCT(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
+				WaitEndpointRemoved(ep, ruleCacheOwner)
 				ep.ID = 0
 				ep.Options = nil
 			},
@@ -738,7 +747,7 @@ func (s *EndpointManagerSuite) TestHasGlobalCT(c *C) {
 }
 
 func (s *EndpointManagerSuite) TestWaitForEndpointsAtPolicyRev(c *C) {
-	ep := &endpoint.Endpoint{}
+	ep := endpoint.NewEndpointWithState(1, endpoint.StateReady)
 	ep.UpdateLogger(nil)
 	type args struct {
 		ctx    context.Context
@@ -776,8 +785,8 @@ func (s *EndpointManagerSuite) TestWaitForEndpointsAtPolicyRev(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
-				ep = &endpoint.Endpoint{}
+				WaitEndpointRemoved(ep, ruleCacheOwner)
+				ep = endpoint.NewEndpointWithState(1, endpoint.StateReady)
 			},
 		},
 		{
@@ -802,8 +811,8 @@ func (s *EndpointManagerSuite) TestWaitForEndpointsAtPolicyRev(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
-				ep = &endpoint.Endpoint{}
+				WaitEndpointRemoved(ep, ruleCacheOwner)
+				ep = endpoint.NewEndpointWithState(1, endpoint.StateReady)
 			},
 		},
 		{
@@ -828,8 +837,8 @@ func (s *EndpointManagerSuite) TestWaitForEndpointsAtPolicyRev(c *C) {
 				}
 			},
 			postTestRun: func() {
-				Remove(ep)
-				ep = &endpoint.Endpoint{}
+				WaitEndpointRemoved(ep, ruleCacheOwner)
+				ep = endpoint.NewEndpointWithState(1, endpoint.StateReady)
 			},
 		},
 	}

commit 8bf10863c8d2fc21441c683b3fd25d11f028f69e

pkg/endpointmanager: protecting endpoints against concurrent access
Fixes: f71d87a71c99 ("endpointmanager: signal when work is done")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpointmanager/manager.go
@@ -451,6 +451,8 @@ func regenerateEndpointNonBlocking(owner endpoint.Owner, ep *endpoint.Endpoint, 
 // in said set have had regenerations queued up.
 func RegenerateEndpointSetSignalWhenEnqueued(owner endpoint.Owner, regenMetadata *endpoint.ExternalRegenerationMetadata, endpointIDs map[uint16]struct{}, wg *sync.WaitGroup) {
 
+	mutex.RLock()
+	defer mutex.RUnlock()
 	for endpointID := range endpointIDs {
 		ep := endpoints[endpointID]
 		if ep == nil {