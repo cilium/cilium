ba8411393037e1de219d60f17497b7d0d702b7c3
Daniel Borkmann <daniel@iogearbox.net>
2019-06-27 14:33:34 +0200

    iptables: fix direct routing regression
    After rebasing our BPF nodeport work, it was not working anymore
    in combination with direct routing. Doing a git bisection with
    our multi-host setup pointed reliably to b08cf3fa1df6 ("iptables:
    Remove legacy workaround for kube-proxy of k8s < 1.8"). Reverting
    this commit gets direct routing working again in master; rework
    the revert as we cannot use allocation CIDR for all use cases. The
    second rule needs to use 'lxc+' instead of localDeliveryInterface
    since the latter is cilium_host in non-ENI case whereas we need
    to allow forwarding for lxc interfaces.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -256,10 +256,6 @@ sudo systemctl enable $MOUNT_SYSTEMD
 sudo systemctl restart $MOUNT_SYSTEMD
 sudo rm -rfv /var/lib/kubelet
 
-# Allow iptables forwarding so kube-dns can function.
-sudo iptables -P FORWARD ACCEPT
-sudo ip6tables -P FORWARD ACCEPT
-
 #check hostname to know if is kubernetes or runtime test
 if [[ "${HOST}" == "k8s1" ]]; then
 
