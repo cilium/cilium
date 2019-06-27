cilium/cilium

commit b9d2a0a9dcbd4ff3b041ad382bbd8a97a5f4dead

maps/nat: Add NatKey{4,6} types
Signed-off-by: André Martins <andre@cilium.io>

pkg/maps/nat/nat.go
@@ -85,13 +85,13 @@ func NewMap(name string, v4 bool) *Map {
 	var mapValue bpf.MapValue
 
 	if v4 {
-		mapKey = &tuple.TupleKey4Global{}
-		sizeKey = int(unsafe.Sizeof(tuple.TupleKey4Global{}))
+		mapKey = &NatKey4{}
+		sizeKey = int(unsafe.Sizeof(NatKey4{}))
 		mapValue = &NatEntry4{}
 		sizeVal = int(unsafe.Sizeof(NatEntry4{}))
 	} else {
-		mapKey = &tuple.TupleKey6Global{}
-		sizeKey = int(unsafe.Sizeof(tuple.TupleKey6Global{}))
+		mapKey = &NatKey6{}
+		sizeKey = int(unsafe.Sizeof(NatKey6{}))
 		mapValue = &NatEntry6{}
 		sizeVal = int(unsafe.Sizeof(NatEntry6{}))
 	}
@@ -184,7 +184,9 @@ func (m *Map) Flush() int {
 }
 
 func deleteMapping4(m *Map, ctKey *tuple.TupleKey4Global) error {
-	key := *ctKey
+	key := NatKey4{
+		TupleKey4Global: *ctKey,
+	}
 	// Workaround #5848.
 	addr := key.SourceAddr
 	key.SourceAddr = key.DestAddr
@@ -206,7 +208,9 @@ func deleteMapping4(m *Map, ctKey *tuple.TupleKey4Global) error {
 }
 
 func deleteMapping6(m *Map, ctKey *tuple.TupleKey6Global) error {
-	key := *ctKey
+	key := NatKey6{
+		TupleKey6Global: *ctKey,
+	}
 	// Workaround #5848.
 	addr := key.SourceAddr
 	key.SourceAddr = key.DestAddr

pkg/maps/nat/types.go
@@ -1,0 +1,109 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package nat
+
+import (
+	"bytes"
+	"unsafe"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/tuple"
+)
+
+type NatKey interface {
+	bpf.MapKey
+
+	// ToNetwork converts fields to network byte order.
+	ToNetwork() NatKey
+
+	// ToHost converts fields to host byte order.
+	ToHost() NatKey
+
+	// Dump contents of key to buffer. Returns true if successful.
+	Dump(buffer *bytes.Buffer, reverse bool) bool
+
+	// GetFlags flags containing the direction of the TupleKey.
+	GetFlags() uint8
+}
+
+// NatKey4 is needed to provide NatEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type NatKey4 struct {
+	tuple.TupleKey4Global
+}
+
+// NewValue creates a new bpf.MapValue.
+func (k *NatKey4) NewValue() bpf.MapValue { return &NatEntry4{} }
+
+// ToNetwork converts ports to network byte order.
+//
+// This is necessary to prevent callers from implicitly converting
+// the NatKey4 type here into a local key type in the nested
+// TupleKey4Global field.
+func (k *NatKey4) ToNetwork() NatKey {
+	return &NatKey4{
+		TupleKey4Global: *k.TupleKey4Global.ToNetwork().(*tuple.TupleKey4Global),
+	}
+}
+
+// ToHost converts ports to host byte order.
+//
+// This is necessary to prevent callers from implicitly converting
+// the NatKey4 type here into a local key type in the nested
+// TupleKey4Global field.
+func (k *NatKey4) ToHost() NatKey {
+	return &NatKey4{
+		TupleKey4Global: *k.TupleKey4Global.ToHost().(*tuple.TupleKey4Global),
+	}
+}
+
+// GetKeyPtr returns the unsafe.Pointer for k.
+func (k *NatKey4) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+
+// NatKey6 is needed to provide NatEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type NatKey6 struct {
+	tuple.TupleKey6Global
+}
+
+// NewValue creates a new bpf.MapValue.
+func (k *NatKey6) NewValue() bpf.MapValue { return &NatEntry4{} }
+
+// ToNetwork converts ports to network byte order.
+//
+// This is necessary to prevent callers from implicitly converting
+// the NatKey6 type here into a local key type in the nested
+// TupleKey6Global field.
+func (k *NatKey6) ToNetwork() NatKey {
+	return &NatKey6{
+		TupleKey6Global: *k.TupleKey6Global.ToNetwork().(*tuple.TupleKey6Global),
+	}
+}
+
+// ToHost converts ports to host byte order.
+//
+// This is necessary to prevent callers from implicitly converting
+// the NatKey6 type here into a local key type in the nested
+// TupleKey6Global field.
+func (k *NatKey6) ToHost() NatKey {
+	return &NatKey6{
+		TupleKey6Global: *k.TupleKey6Global.ToHost().(*tuple.TupleKey6Global),
+	}
+}
+
+// GetKeyPtr returns the unsafe.Pointer for k.
+func (k *NatKey6) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }

pkg/maps/nat/zz_generated.deepcopy.go
@@ -71,3 +71,53 @@ func (in *NatEntry6) DeepCopyMapValue() bpf.MapValue {
 	}
 	return nil
 }
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NatKey4) DeepCopyInto(out *NatKey4) {
+	*out = *in
+	in.TupleKey4Global.DeepCopyInto(&out.TupleKey4Global)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NatKey4.
+func (in *NatKey4) DeepCopy() *NatKey4 {
+	if in == nil {
+		return nil
+	}
+	out := new(NatKey4)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *NatKey4) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NatKey6) DeepCopyInto(out *NatKey6) {
+	*out = *in
+	in.TupleKey6Global.DeepCopyInto(&out.TupleKey6Global)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NatKey6.
+func (in *NatKey6) DeepCopy() *NatKey6 {
+	if in == nil {
+		return nil
+	}
+	out := new(NatKey6)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *NatKey6) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}

commit bc0f000d6ea0754775b5678d71ce8b3484d86611

pkg/maps: fix panic while accessing nat maps
Fixes: b9d2a0a9dcbd ("maps/nat: Add NatKey{4,6} types")
Signed-off-by: André Martins <andre@cilium.io>

pkg/maps/nat/ipv4.go
@@ -48,7 +48,7 @@ func (n *NatEntry4) String() string {
 }
 
 // Dump dumps NAT entry to string.
-func (n *NatEntry4) Dump(key tuple.TupleKey, start uint64) string {
+func (n *NatEntry4) Dump(key NatKey, start uint64) string {
 	var which string
 
 	if key.GetFlags()&tuple.TUPLE_F_IN != 0 {

pkg/maps/nat/ipv6.go
@@ -48,7 +48,7 @@ func (n *NatEntry6) String() string {
 }
 
 // Dump dumps NAT entry to string.
-func (n *NatEntry6) Dump(key tuple.TupleKey, start uint64) string {
+func (n *NatEntry6) Dump(key NatKey, start uint64) string {
 	var which string
 
 	if key.GetFlags()&tuple.TUPLE_F_IN != 0 {

pkg/maps/nat/nat.go
@@ -67,7 +67,7 @@ type NatEntry interface {
 	ToHost() NatEntry
 
 	// Dumps the Nat entry as string.
-	Dump(key tuple.TupleKey, start uint64) string
+	Dump(key NatKey, start uint64) string
 }
 
 // NatDumpCreated returns time in seconds when NAT entry was created.
@@ -118,7 +118,7 @@ func (m *Map) DumpEntries() (string, error) {
 
 	nsecStart, _ := bpf.GetMtime()
 	cb := func(k bpf.MapKey, v bpf.MapValue) {
-		key := k.(tuple.TupleKey)
+		key := k.(NatKey)
 		if !key.ToHost().Dump(&buffer, false) {
 			return
 		}
@@ -148,10 +148,9 @@ func statStartGc(m *Map) gcStats {
 func doFlush4(m *Map) gcStats {
 	stats := statStartGc(m)
 	filterCallback := func(key bpf.MapKey, _ bpf.MapValue) {
-		currentKey := key.(*tuple.TupleKey4Global)
-		err := m.Delete(currentKey)
+		err := m.Delete(key)
 		if err != nil {
-			log.WithError(err).WithField(logfields.Key, currentKey.String()).Error("Unable to delete CT entry")
+			log.WithError(err).WithField(logfields.Key, key.String()).Error("Unable to delete CT entry")
 		} else {
 			stats.deleted++
 		}
@@ -163,10 +162,9 @@ func doFlush4(m *Map) gcStats {
 func doFlush6(m *Map) gcStats {
 	stats := statStartGc(m)
 	filterCallback := func(key bpf.MapKey, _ bpf.MapValue) {
-		currentKey := key.(*tuple.TupleKey6Global)
-		err := m.Delete(currentKey)
+		err := m.Delete(key)
 		if err != nil {
-			log.WithError(err).WithField(logfields.Key, currentKey.String()).Error("Unable to delete CT entry")
+			log.WithError(err).WithField(logfields.Key, key.String()).Error("Unable to delete CT entry")
 		} else {
 			stats.deleted++
 		}