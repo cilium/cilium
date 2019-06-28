6b9d213a0772e2b1a39c2b03cbc9c120b7a67ceb
Daniel Borkmann <daniel@iogearbox.net>
2019-06-28 17:05:12 +0200

    daemon: mark host reachable services as beta
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- daemon/daemon_main.go
+++ daemon/daemon_main.go
@@ -473,7 +473,7 @@ func init() {
 	flags.String(option.EgressMasqueradeInterfaces, "", "Limit egress masquerading to interface selector")
 	option.BindEnv(option.EgressMasqueradeInterfaces)
 
-	flags.Bool(option.EnableHostReachableServices, false, "Enable reachability of services for host applications")
+	flags.Bool(option.EnableHostReachableServices, false, "Enable reachability of services for host applications (beta)")
 	option.BindEnv(option.EnableHostReachableServices)
 
 	flags.Bool(option.EnableLegacyServices, true, "Enable legacy (prior-v1.5) services")
