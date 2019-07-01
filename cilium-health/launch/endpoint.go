cilium/cilium

commit 9b8cf2aeaf2ee15eda58d01c3fdb22aa5d38fa42

health: Remove spawn_netns.sh
This commit removes the `spawn_netns.sh` script, which is used to
set up a network namespace for the endpoint connectivity test, and
replaces it with custom code written in Go. We however still rely
on iproute2 to spawn the child process in the new network namespace,
as doing this in pure Go tends to be error-prone.

The logic of the network namespace configuration is overall kept
the same. There are however a few changes with regards to cleanup:

  - Instead of moving out the veth peer into the host namespace and
    deleting the device in the host namespace, we just delete the
    namespace. The kernel will remove any virtual interfaces in that
    namespace and therefore also remove the veth pair. We already rely
    on this for ipvlan slaves since e5b9eb32.

  - In a similar fashion, we also do not manually remove ip addresses on
    the interfaces, we rely on the kernel to remove them with the
    interface. This further simplifies the cleanup code.

  - The shell script was responsible to clean up the namespace when it
    received a termination signal (i.e. due to cilium being shut down).
    With this change, it is now the cilium-agent which listens for a
    termination signal and performs the cleanup.
    The cleanup functionality is also still called by the
    `cilium-health-ep` controller if the child exits unexpectedly.

By removing the bash process previously required for cleanup, this change
saves roughly 3.9MB of memory.

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

cilium-health/launch/endpoint.go
@@ -42,6 +42,7 @@ import (
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/pidfile"
 
+	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
 )
@@ -107,6 +108,42 @@ func configureHealthRouting(netns, dev string, addressing *models.NodeAddressing
 	return err
 }
 
+func configureHealthInterface(netNS ns.NetNS, ifName string, ip4Addr, ip6Addr *net.IPNet) error {
+	return netNS.Do(func(_ ns.NetNS) error {
+		link, err := netlink.LinkByName(ifName)
+		if err != nil {
+			return err
+		}
+
+		if ip6Addr != nil {
+			if err = netlink.AddrAdd(link, &netlink.Addr{IPNet: ip6Addr}); err != nil {
+				return err
+			}
+		}
+
+		if ip4Addr != nil {
+			if err = netlink.AddrAdd(link, &netlink.Addr{IPNet: ip4Addr}); err != nil {
+				return err
+			}
+		}
+
+		if err = netlink.LinkSetUp(link); err != nil {
+			return err
+		}
+
+		lo, err := netlink.LinkByName("lo")
+		if err != nil {
+			return err
+		}
+
+		if err = netlink.LinkSetUp(lo); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
 // Client wraps a client to a specific cilium-health endpoint instance, to
 // provide convenience methods such as PingEndpoint().
 type Client struct {
@@ -142,18 +179,10 @@ func KillEndpoint() {
 // This is expected to be called after the process is killed and the endpoint
 // is removed from the endpointmanager.
 func CleanupEndpoint() {
-	// In the case of ipvlan, the ipvlan slave device is removed by removal
-	// of the endpoint netns in "cleanup" of spawn_netns.sh
-	if option.Config.DatapathMode == option.DatapathModeVeth {
-		scopedLog := log.WithField(logfields.Veth, vethName)
-		if link, err := netlink.LinkByName(vethName); err == nil {
-			err = netlink.LinkDel(link)
-			if err != nil {
-				scopedLog.WithError(err).Warning("Couldn't delete cilium-health device")
-			}
-		} else {
-			scopedLog.WithError(err).Debug("Didn't find existing device")
-		}
+	// By removing the network namespace, we also remove any associated
+	// veth pairs and ipvlan slaves
+	if err := netns.RemoveNetNSWithName(netNSName); err != nil {
+		log.WithError(err).Debug("Unable to remove cilium-health namespace")
 	}
 }
 
@@ -171,38 +200,39 @@ func LaunchAsEndpoint(baseCtx context.Context, owner endpoint.Owner, n *node.Nod
 			State:         models.EndpointStateWaitingForIdentity,
 			Addressing:    &models.AddressPair{},
 		}
-		ip4Address, ip6Address string
 		healthIP               net.IP
-		hostIfaceName          string
+		ip4Address, ip6Address *net.IPNet
 	)
 
 	if n.IPv4HealthIP != nil {
 		healthIP = n.IPv4HealthIP
 		info.Addressing.IPV4 = healthIP.String()
-		ip4WithMask := net.IPNet{IP: healthIP, Mask: defaults.ContainerIPv4Mask}
-		ip4Address = ip4WithMask.String()
+		ip4Address = &net.IPNet{IP: healthIP, Mask: defaults.ContainerIPv4Mask}
 	}
 
 	if n.IPv6HealthIP != nil {
 		healthIP = n.IPv6HealthIP
 		info.Addressing.IPV6 = healthIP.String()
-		ip6WithMask := net.IPNet{IP: healthIP, Mask: defaults.ContainerIPv6Mask}
-		ip6Address = ip6WithMask.String()
+		ip6Address = &net.IPNet{IP: healthIP, Mask: defaults.ContainerIPv6Mask}
+	}
+
+	netNS, err := netns.ReplaceNetNSWithName(netNSName)
+	if err != nil {
+		return nil, err
 	}
 
 	switch option.Config.DatapathMode {
 	case option.DatapathModeVeth:
-		if _, _, err := connector.SetupVethWithNames(vethName, epIfaceName, mtuConfig.GetDeviceMTU(), info); err != nil {
+		_, epLink, err := connector.SetupVethWithNames(vethName, epIfaceName, mtuConfig.GetDeviceMTU(), info)
+		if err != nil {
 			return nil, fmt.Errorf("Error while creating veth: %s", err)
 		}
-		hostIfaceName = vethName
 
-	case option.DatapathModeIpvlan:
-		netNS, err := netns.ReplaceNetNSWithName(netNSName)
-		if err != nil {
-			return nil, err
+		if err = netlink.LinkSetNsFd(*epLink, int(netNS.Fd())); err != nil {
+			return nil, fmt.Errorf("failed to move device %q to health namespace: %s", epIfaceName, err)
 		}
 
+	case option.DatapathModeIpvlan:
 		mapFD, err := connector.CreateAndSetupIpvlanSlave("",
 			epIfaceName, netNS, mtuConfig.GetDeviceMTU(),
 			option.Config.Ipvlan.MasterDeviceIndex,
@@ -216,22 +246,18 @@ func LaunchAsEndpoint(baseCtx context.Context, owner endpoint.Owner, n *node.Nod
 		}
 		defer unix.Close(mapFD)
 
-		hostIfaceName = option.Config.Device
+	}
+
+	if err = configureHealthInterface(netNS, epIfaceName, ip4Address, ip6Address); err != nil {
+		return nil, fmt.Errorf("failed configure health interface %q: %s", epIfaceName, err)
 	}
 
 	pidfile := filepath.Join(option.Config.StateDir, PidfilePath)
-	healthArgs := fmt.Sprintf("-d --admin=unix --passive --pidfile %s", pidfile)
-	args := []string{info.ContainerName, hostIfaceName, epIfaceName,
-		ip6Address, ip4Address, ciliumHealth, healthArgs}
-	if option.Config.DatapathMode == option.DatapathModeIpvlan {
-		// Do not initialize netns (i.e. create and move the slave to it), as
-		// otherwise qdisc of the slave will get reset
-		args = append(args, "--skip-netns-init")
-	}
-	prog := filepath.Join(option.Config.BpfDir, "spawn_netns.sh")
+	prog := "ip"
+	args := []string{"netns", "exec", netNSName, ciliumHealth, "-d", "--admin=unix", "--passive", "--pidfile", pidfile}
 	cmd.SetTarget(prog)
 	cmd.SetArgs(args)
-	log.Infof("Spawning health endpoint with arguments %#v", args)
+	log.Infof("Spawning health endpoint with command %q %q", prog, args)
 	if err := cmd.Run(); err != nil {
 		return nil, err
 	}

daemon/health.go
@@ -20,6 +20,7 @@ import (
 	"time"
 
 	health "github.com/cilium/cilium/cilium-health/launch"
+	"github.com/cilium/cilium/pkg/cleanup"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
@@ -78,6 +79,11 @@ func (d *Daemon) initHealth() {
 			RunInterval: 30 * time.Second,
 		},
 	)
+
+	// Make sure to clean up the endpoint namespace when cilium-agent terminates
+	cleanup.DeferTerminationCleanupFunction(cleanUPWg, cleanUPSig, func() {
+		health.CleanupEndpoint()
+	})
 }
 
 func (d *Daemon) cleanupHealthEndpoint() {

bpf/Makefile
@@ -6,7 +6,6 @@ SUBDIRS = sockops
 
 BPF_SIMPLE = bpf_netdev.o bpf_overlay.o bpf_xdp.o bpf_ipsec.o bpf_network.o bpf_alignchecker.o bpf_hostdev_ingress.o
 BPF = bpf_lxc.o bpf_lb.o bpf_sock.o $(BPF_SIMPLE)
-SCRIPTS = init.sh join_ep.sh run_probes.sh spawn_netns.sh
 
 TARGET=cilium-map-migrate
 

bpf/spawn_netns.sh
@@ -1,126 +1,0 @@
-#!/bin/bash
-#
-# Copyright 2016-2017 Authors of Cilium
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This script creates a network namespace, configures the network connectivity
-# for it, then runs a command within the namespace and waits for the command
-# to exit. Cleans up the namespace and device configuration on termination.
-# Must be run as root.
-#
-# Example: $ spawn_netns.sh mynetns mynetns-dev host-dev "fc00::1/64" \
-#            "192.168.0.100/24" ping "8.8.8.8"
-
-NETNS=$1
-HOSTDEV=$2
-NETNSDEV=$3
-IP6=$4
-IP4=$5
-TARGET=$6
-TARGET_ARGS=$7
-# If set, assumes that $NETNS has been already created and $NETNSDEV has been
-# moved to it.
-[ "$8" = "--skip-netns-init" ] && SKIP_NETNS_INIT=1
-
-configure_netns()
-{
-    if [ -z "${SKIP_NETNS_INIT}" ]; then
-	    ip netns add ${NETNS}
-	    ip li set dev ${HOSTDEV} up
-	    ip li set dev ${NETNSDEV} netns ${NETNS}
-    fi
-	if [ "${IP6}" != "" ]; then
-		ip netns exec ${NETNS} ip addr add dev ${NETNSDEV} ${IP6}
-	fi
-	if [ "${IP4}" != "" ]; then
-		ip netns exec ${NETNS} ip addr add dev ${NETNSDEV} ${IP4}
-	fi
-	ip netns exec ${NETNS} ip li set dev ${NETNSDEV} up
-	ip netns exec ${NETNS} ip li set dev lo up
-}
-
-cleanup()
-{
-	ip netns exec ${NETNS} ip li set dev ${NETNSDEV} down
-	if [ "${IP6}" != "" ]; then
-		ip netns exec ${NETNS} ip addr del dev ${NETNSDEV} ${IP6}
-	fi
-	if [ "${IP4}" != "" ]; then
-		ip netns exec ${NETNS} ip addr del dev ${NETNSDEV} ${IP4}
-	fi
-    if [ -z "${SKIP_NETNS_INIT}" ]; then
-        ip netns exec ${NETNS} ip li set dev ${NETNSDEV} netns 1
-    fi
-    ip netns del ${NETNS}
-}
-
-netns_exists()
-{
-	ip netns list | grep -q ${NETNS}
-}
-
-run_target()
-{
-	ip netns exec ${NETNS} ${TARGET} ${TARGET_ARGS}
-}
-
-invalid_dev()
-{
-    if [ -z "$2" ]; then
-	    ! ip link show $1 2>&1 >/dev/null
-    else
-        ! ip netns exec $2 ip link show $1 2>&1 >/dev/null
-    fi
-}
-
-validate_args()
-{
-	if [ $# -lt 6 ]; then
-		echo "Usage: $0 <netns> <host-iface> <netns-iface> <ip6/cidr> <ip4/cidr> <target> [<target-args>] [--skip-netns-init]" >&2
-		exit 1
-	fi
-
-	if [ -z "$SKIP_NETNS_INIT" ] && ip netns | grep "${NETNS}"; then
-		ip netns del ${NETNS}
-	fi
-
-	if invalid_dev "${HOSTDEV}";  then
-		echo "Cannot find interface ${HOSTDEV}" >&2
-		exit 1
-	fi
-
-    [ -n "$SKIP_NETNS_INIT" ] && local NS="${NETNS}"
-    if invalid_dev "${NETNSDEV}" "$NS"; then
-		echo "Cannot find interface ${NETNSDEV}" >&2
-    fi
-
-	if ! which ${TARGET} 2>&1 >/dev/null ; then
-		echo "Cannot locate ${TARGET}" >&2
-		exit 1
-	fi
-}
-
-main()
-{
-	validate_args "$@"
-	configure_netns
-	while true; do
-		if ! netns_exists || ! run_target; then
-			break
-		fi
-	done
-}
-
-trap cleanup SIGINT SIGKILL
-main "$@"

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=158ed9e6414958e65438033ddbfc12e31378c9da
-BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 
+GO_BINDATA_SHA1SUM=5113923e067161ee2a57d2fa9015d9c67fcae4ee
+BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

commit ad1b8c16431887d958d9710cf5b82ae29c2527c0

health: Re-introduce deletion of endpoint interfaces upon termination
This re-introduces the manual removal of the virtual interfaces in the
health endpoint cleanup. The existing logic relied on the kernel to
remove these interfaces as part of namespace deletion. However, the deletion
by the kernel happens asynchronously, and can be blocked when there is still
a process in that namespace.

This caused a race where the old veth pair for the health endpoint was
not deleted when cilium would try to create a new one, causing the launch
of the enpoint responder to fail. To fix this, we manually remove the
interfaces during cleanup, exactly as it was done in previous versions of
cilium.

In addition, we now also explicitly kill the endpoint child process
when the parent agent receives a termination signal.

Fixes: 9b8cf2aeaf2e ("health: Remove spawn_netns.sh")
Fixes: #8400

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

cilium-health/launch/endpoint.go
@@ -184,8 +184,29 @@ func KillEndpoint() {
 // This is expected to be called after the process is killed and the endpoint
 // is removed from the endpointmanager.
 func CleanupEndpoint() {
-	// By removing the network namespace, we also remove any associated
-	// veth pairs and ipvlan slaves
+	// Removes the interfaces used for the endpoint process, followed by the
+	// deletion of the health namespace itself. The removal of the interfaces
+	// is needed, because network namespace removal does not always trigger the
+	// deletion of associated interfaces immediately (e.g. when a process in the
+	// namespace marked for deletion has not yet been terminated).
+	switch option.Config.DatapathMode {
+	case option.DatapathModeVeth:
+		scopedLog := log.WithField(logfields.Veth, vethName)
+		if link, err := netlink.LinkByName(vethName); err == nil {
+			err = netlink.LinkDel(link)
+			if err != nil {
+				scopedLog.WithError(err).Info("Couldn't delete cilium-health veth device")
+			}
+		} else {
+			scopedLog.WithError(err).Debug("Didn't find existing device")
+		}
+	case option.DatapathModeIpvlan:
+		if err := netns.RemoveIfFromNetNSWithNameIfBothExist(netNSName, epIfaceName); err != nil {
+			log.WithError(err).WithField(logfields.Ipvlan, epIfaceName).
+				Info("Couldn't delete cilium-health ipvlan slave device")
+		}
+	}
+
 	if err := netns.RemoveNetNSWithName(netNSName); err != nil {
 		log.WithError(err).Debug("Unable to remove cilium-health namespace")
 	}

daemon/health.go
@@ -88,6 +88,7 @@ func (d *Daemon) initHealth() {
 
 	// Make sure to clean up the endpoint namespace when cilium-agent terminates
 	cleanup.DeferTerminationCleanupFunction(cleanUPWg, cleanUPSig, func() {
+		health.KillEndpoint()
 		health.CleanupEndpoint()
 	})
 }

pkg/netns/netns.go
@@ -42,6 +42,22 @@ func RemoveIfFromNetNSIfExists(netNS ns.NetNS, ifName string) error {
 	})
 }
 
+// RemoveIfFromNetNSWithNameIfBothExist removes the given interface from
+// the given network namespace identified by its name.
+//
+// If either the interface or the namespace do not exist, no error will
+// be returned.
+func RemoveIfFromNetNSWithNameIfBothExist(netNSName, ifName string) error {
+	netNS, err := ns.GetNS(netNSPath(netNSName))
+	if err != nil {
+		if _, ok := err.(ns.NSPathNotExistErr); ok {
+			return nil
+		}
+		return err
+	}
+	return RemoveIfFromNetNSIfExists(netNS, ifName)
+}
+
 // ReplaceNetNSWithName creates a network namespace with the given name.
 // If such netns already exists from a previous run, it will be removed
 // and then re-created to avoid any previous state of the netns.