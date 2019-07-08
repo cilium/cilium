cilium/cilium

commit a9ed3577bcb23e7be38a0c6bf4e1ec03497a0dbc

operator: do not depend on cluster DNS to connect to etcd
As the operator is can be running in hostNetwork, its schedule is not
dependent in the initialization of a cluster DNS nor Cilium to be up and
ready. For this reason the operator should try, whenever possible, to
perform the etcd name translation into the service IP of etcd removing
the dependency of a cluster DNS to be running.

Fixes: 767cdb24a402 ("operator: Run operator in host networking mode")
Signed-off-by: André Martins <andre@cilium.io>

operator/main.go
@@ -17,6 +17,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -36,6 +37,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 	"k8s.io/klog"
 )
 
@@ -196,12 +198,53 @@ func runOperator(cmd *cobra.Command) {
 	}
 
 	if requiresKVstore() {
+		var goopts *kvstore.ExtraOptions
 		scopedLog := log.WithFields(logrus.Fields{
 			"kvstore": kvStore,
 			"address": kvStoreOpts[fmt.Sprintf("%s.address", kvStore)],
 		})
+		if synchronizeServices {
+			// If K8s is enabled we can do the service translation automagically by
+			// looking at services from k8s and retrieve the service IP from that.
+			// This makes cilium to not depend on kube dns to interact with etcd
+			if k8s.IsEnabled() && kvstore.IsEtcdOperator(option.Config.KVStore, option.Config.KVStoreOpt, option.Config.K8sNamespace) {
+				// Wait services and endpoints cache are synced with k8s before setting
+				// up etcd so we can perform the name resolution for etcd-operator
+				// to the service IP as well perform the service -> backend IPs for
+				// that service IP.
+
+				scopedLog.Info("cilium-operator running with service synchronization: automatic etcd service translation enabled")
+				log := log.WithField(logfields.LogSubsys, "etcd")
+				scopedLog.Info("Waiting for all services to be synced with kubernetes before connecting to etcd")
+				<-k8sSvcCacheSynced
+				scopedLog.Info("Kubernetes services synced")
+				goopts = &kvstore.ExtraOptions{
+					DialOption: []grpc.DialOption{
+						grpc.WithDialer(func(s string, duration time.Duration) (conn net.Conn, e error) {
+							// If the service is available, do the service translation to
+							// the service IP. Otherwise dial with the original service
+							// name `s`.
+							svc := k8s.ParseServiceIDFrom(s)
+							if svc != nil {
+								backendIP := k8sSvcCache.GetRandomBackendIP(*svc)
+								if backendIP != nil {
+									s = backendIP.String()
+								}
+							} else {
+								log.Debug("Service not found")
+							}
+							log.Debugf("custom dialer based on k8s service backend is dialing to %q", s)
+							return net.Dial("tcp", s)
+						},
+						),
+					},
+				}
+			}
+		} else {
+			scopedLog.Info("cilium-operator running without service synchronization: automatic etcd service translation disabled")
+		}
 		scopedLog.Info("Connecting to kvstore...")
-		if err := kvstore.Setup(kvStore, kvStoreOpts, nil); err != nil {
+		if err := kvstore.Setup(kvStore, kvStoreOpts, goopts); err != nil {
 			scopedLog.WithError(err).Fatal("Unable to setup kvstore")
 		}
 	}

operator/k8s_service_sync.go
@@ -33,8 +33,11 @@ import (
 )
 
 var (
-	k8sSvcCache   = k8s.NewServiceCache()
-	servicesStore *store.SharedStore
+	k8sSvcCache = k8s.NewServiceCache()
+	// k8sSvcCacheSynced is used do signalize when all services are synced with
+	// k8s.
+	k8sSvcCacheSynced = make(chan struct{})
+	servicesStore     *store.SharedStore
 )
 
 func k8sServiceHandler() {
@@ -196,6 +199,13 @@ func startSynchronizingServices() {
 	)
 
 	go endpointController.Run(wait.NeverStop)
+
+	go func() {
+		cache.WaitForCacheSync(wait.NeverStop, svcController.HasSynced)
+		cache.WaitForCacheSync(wait.NeverStop, endpointController.HasSynced)
+		close(k8sSvcCacheSynced)
+	}()
+
 	go func() {
 		<-readyChan
 		log.Info("Starting to synchronize Kubernetes services to kvstore")

commit cf90ff3335e6c4685be3307306bd3625b9e6991b

operator: startSynchronizingServices before kvstore
As kvstore with etcd-operator requires the services to be synchronized
first the setup of those services need to happen before the kvstore
setup is done.

Fixes: a9ed3577bcb2 ("operator: do not depend on cluster DNS to connect to etcd")
Signed-off-by: André Martins <andre@cilium.io>

operator/main.go
@@ -209,6 +209,10 @@ func runOperator(cmd *cobra.Command) {
 		startSynchronizingCiliumNodes()
 	}
 
+	if synchronizeServices {
+		startSynchronizingServices()
+	}
+
 	if requiresKVstore() {
 		var goopts *kvstore.ExtraOptions
 		scopedLog := log.WithFields(logrus.Fields{
@@ -259,10 +263,6 @@ func runOperator(cmd *cobra.Command) {
 		if err := kvstore.Setup(kvStore, kvStoreOpts, goopts); err != nil {
 			scopedLog.WithError(err).Fatal("Unable to setup kvstore")
 		}
-	}
-
-	if synchronizeServices {
-		startSynchronizingServices()
 	}
 
 	if enableCepGC {