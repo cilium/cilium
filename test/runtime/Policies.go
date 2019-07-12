98f5eb2be8ebaf27922ebdc0f3f438b7d8c4b57c
Ian Vernon <ian@cilium.io>
2019-07-12 14:46:12 -0700

    test: misc. runtime policy test fixes
    * Ensure that Cilium is started with the set of configuration options
    set in `SetUpCilium`, specifically of excluding of local addresses
    that are used to simulate external IPs for CIDR policy testing.
    * Fix a log error message to contain a string that was expected.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- test/runtime/Policies.go
+++ test/runtime/Policies.go
@@ -65,8 +65,11 @@ var _ = Describe("RuntimePolicies", func() {
 
 	BeforeAll(func() {
 		vm = helpers.InitRuntimeHelper(helpers.Runtime, logger)
+		// Make sure that Cilium is started with appropriate CLI options
+		// (specifically to exclude the local addresses that are populated for
+		// CIDR policy tests).
+		Expect(vm.SetUpCilium()).To(BeNil())
 		ExpectCiliumReady(vm)
-
 		vm.SampleContainersActions(helpers.Create, helpers.CiliumDockerNetwork)
 		vm.PolicyDelAll()
 
@@ -1334,7 +1337,7 @@ var _ = Describe("RuntimePolicies", func() {
 					curlFailures++
 				}
 			}
-			Expect(curlFailures).To(BeNumerically("<=", 1), "Curl to %q have failed more than once")
+			Expect(curlFailures).To(BeNumerically("<=", 1), "Curl to %q have failed more than once", cloudFlare)
 
 			By("Pinging %q from %q (should not work)", api.EntityHost, helpers.App1)
 			res = vm.ContainerExec(helpers.App1, helpers.Ping(cloudFlare))
