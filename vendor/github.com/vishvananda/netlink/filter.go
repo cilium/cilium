5b74a0c0c5ce361cdefc54832101490699f45008
Daniel Borkmann <daniel@iogearbox.net>
2019-07-15 22:23:32 +0200

    vendor: update netlink with golang fix
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- vendor/github.com/vishvananda/netlink/filter.go
+++ vendor/github.com/vishvananda/netlink/filter.go
@@ -240,6 +240,8 @@ type BpfFilter struct {
 	Fd           int
 	Name         string
 	DirectAction bool
+	Id           int
+	Tag          string
 }
 
 func (filter *BpfFilter) Type() string {
--- vendor/github.com/vishvananda/netlink/route_linux.go
+++ vendor/github.com/vishvananda/netlink/route_linux.go
@@ -1037,12 +1037,18 @@ func routeSubscribeAt(newNs, curNs netns.NsHandle, ch chan<- RouteUpdate, done <
 	go func() {
 		defer close(ch)
 		for {
-			msgs, err := s.Receive()
+			msgs, from, err := s.Receive()
 			if err != nil {
 				if cberr != nil {
 					cberr(err)
 				}
 				return
+			}
+			if from.Pid != nl.PidKernel {
+				if cberr != nil {
+					cberr(fmt.Errorf("Wrong sender portid %d, expected %d", from.Pid, nl.PidKernel))
+				}
+				continue
 			}
 			for _, m := range msgs {
 				if m.Header.Type == unix.NLMSG_DONE {
