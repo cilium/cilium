bb71621ac090adc3b12acf18488d40144759266a
Jarno Rajahalme <jarno@covalent.io>
2019-07-16 09:00:54 -0700

    proxy: Do not error out if reading of open ports fails.
    [ upstream commit 15f5e2687d58f47f1f92847e104f2530e521560a ]
    
    Checking the random port number against known open ports is a
    precaution to avoid trying to re-use an open port. We should not error
    out if the reading of the open ports fails, however, as there is a
    high likelihood that the port selection will result in an usable port
    anyway.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>
    Signed-off-by: Ian Vernon <ian@cilium.io>

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -91,7 +91,7 @@ func StartProxySupport(minPort uint16, maxPort uint16, stateDir string,
 
 	if accessLogFile != "" {
 		if err := logger.OpenLogfile(accessLogFile); err != nil {
-			log.WithError(err).WithField(logger.FieldFilePath, accessLogFile).
+			log.WithError(err).WithField(logfields.Path, accessLogFile).
 				Warn("Cannot open L7 access log")
 		}
 	}
@@ -123,10 +123,7 @@ var (
 
 func (p *Proxy) allocatePort() (uint16, error) {
 	// Get a snapshot of the TCP ports already open locally.
-	openLocalPorts, err := readOpenLocalPorts(procNetTCPFiles)
-	if err != nil {
-		return 0, fmt.Errorf("couldn't read local ports from /proc: %s", err)
-	}
+	openLocalPorts := readOpenLocalPorts(append(procNetTCPFiles, procNetUDPFiles...))
 
 	portRandomizerMutex.Lock()
 	defer portRandomizerMutex.Unlock()
