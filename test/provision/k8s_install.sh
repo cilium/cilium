931f688ce6169a8e19f67e21e504738680b5abaf
André Martins <andre@cilium.io>
2019-07-23 13:41:56 +0200

    test: update k8s test version to v1.15.1
    Signed-off-by: André Martins <andre@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -210,7 +210,7 @@ case $K8S_VERSION in
         ;;
     "1.15")
         KUBERNETES_CNI_VERSION="0.7.5"
-        K8S_FULL_VERSION="1.15.0"
+        K8S_FULL_VERSION="1.15.1"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
