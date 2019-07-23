cilium/cilium

commit 59315dcdcf799bc9b79e1b3f8a7e4c1dc26d365f

daemon: Remove svc from cache in syncLBMapsWithK8s
Previously, the syncLBMapsWithK8s method did not remove a service
from the d.loadBalancer, only from the BPF svc maps. This allowed
for the following race condition to happen:

                                              d.delK8sSVCs(svcA)
                                                    |
      d.syncLBMapsWithK8s()                         |
                |                                   |
   k8sSvcCache.UniqueFrontends()=(no svcA)          |
                |                                   |
    d.loadBalancer.BPFMapMU.Lock()                  |
                |                                   |
   lbmap.dumpServiceMapsToUserspaceV2()=(svcA)      |
                |                                   |
         delete svcA from BPF maps                  |
                |                                   |
    d.loadBalancer.BPFMapMU.Unlock()                |
                                                    |
                                        d.loadBalancer.BPFMapMU.Lock()
                                                    |
                                        svcA exists in d.loadBalancer?
                                                    |
                                          delete svcA from BPF maps

This race could have led to removing the same backend twice which
could explain the "backend $ID not found" error observed in the wild.

Also, move the locking in syncLBMapsWithK8s above the retrieval of
k8s services from the cache to avoid any other not yet discovered race
condition.

Fixes: cc4be8e3 ("daemon: sync BPF maps with in-memory K8s service maps")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/loadbalancer.go
@@ -636,11 +636,11 @@ func (d *Daemon) syncLBMapsWithK8s() error {
 	// Maps service IDs to whether they are IPv6 (true) or IPv4 (false).
 	k8sDeletedRevNATS := make(map[loadbalancer.ServiceID]bool)
 
-	// Set of L3n4Addrs in string form for storage as a key in map.
-	k8sServicesFrontendAddresses := d.k8sSvcCache.UniqueServiceFrontends()
-
 	d.loadBalancer.BPFMapMU.Lock()
 	defer d.loadBalancer.BPFMapMU.Unlock()
+
+	// Set of L3n4Addrs in string form for storage as a key in map.
+	k8sServicesFrontendAddresses := d.k8sSvcCache.UniqueServiceFrontends()
 
 	log.Debugf("dumping BPF service maps to userspace")
 	// At this point the creation of the v2 svc from the corresponding legacy
@@ -708,7 +708,7 @@ func (d *Daemon) syncLBMapsWithK8s() error {
 	for _, svc := range k8sDeletedServices {
 		svcLogger := log.WithField(logfields.Object, logfields.Repr(svc))
 		svcLogger.Debug("removing service because it was not synced from Kubernetes")
-		if err := d.svcDeleteBPF(svc); err != nil {
+		if err := d.svcDeleteByFrontendLocked(&svc); err != nil {
 			bpfDeleteErrors = append(bpfDeleteErrors, err)
 		}
 	}

commit d35d85ef386a33e08ebf32fd6b2748e13df291b8

daemon: Fix removal of non-existing SVCs in syncLBMapsWithK8s
59315dcdcf missed the fact that population of "daemon.loadBalancer"
happens only via k8s_watcher's "UpdateService". So, in the case of
a non-existing SVC, "daemon.loadBalancer" doesn't have an entry for
the svc.

Revert the change, but keep the locking at the top of the function.

Fixes: 59315dcdcf ("daemon: Remove svc from cache in syncLBMapsWithK8s")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/loadbalancer.go
@@ -708,7 +708,7 @@ func (d *Daemon) syncLBMapsWithK8s() error {
 	for _, svc := range k8sDeletedServices {
 		svcLogger := log.WithField(logfields.Object, logfields.Repr(svc))
 		svcLogger.Debug("removing service because it was not synced from Kubernetes")
-		if err := d.svcDeleteByFrontendLocked(&svc); err != nil {
+		if err := d.svcDeleteBPF(svc); err != nil {
 			bpfDeleteErrors = append(bpfDeleteErrors, err)
 		}
 	}