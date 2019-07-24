e1f2740ee07494ebe00f105c56af9cb88ab3804c
Michal Rostecki <mrostecki@opensuse.org>
2019-07-24 16:14:44 +0200

    init-container: Look for a concrete BPFFS mount in /sys/fs/bpf
    [ upstream commit 76a60eb27f2464d2b428cda721de1237dc4f8b17 ]
    
    Before this change, init container was looking for any mount which
    contains the `bpf` string, which was always true, even if there was no
    real BPFFS mount. When BPFFS is not mounted on host, init container sees
    the mount from host directory of sysfs type:
    
    ```
    sysfs on /sys/fs/bpf type sysfs (rw,relatime)
    ```
    
    To check whether BPFFS is mounted properly, we need to look explicitly
    for a mount entry like:
    
    ```
    bpf on /sys/fs/bpf type bpf (rw,nosuid,nodev,noexec,relatime,mode=700)
    ```
    
    Fixes: b264343377fe ("Add bpf mount check to Cilium init container")
    Fixes: #8646
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>
    Signed-off-by: Ian Vernon <ian@cilium.io>

--- contrib/packaging/docker/init-container.sh
+++ contrib/packaging/docker/init-container.sh
@@ -15,5 +15,5 @@ if [ "${CILIUM_ALL_STATE}" = "true" ] \
 fi
 
 if [ "${CILIUM_WAIT_BPF_MOUNT}" = "true" ]; then
-	until mount | grep bpf; do echo "BPF filesystem is not mounted yet"; sleep 1; done
+	until mount | grep "/sys/fs/bpf type bpf"; do echo "BPF filesystem is not mounted yet"; sleep 1; done
 fi;
