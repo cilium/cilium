cilium/cilium

commit b264343377fe3e46cdf2568c2636c47de527657a

Add bpf mount check to Cilium init container
Signed-off-by: Maciej Kwiek <maciej@covalent.io>

contrib/packaging/docker/init-container.sh
@@ -10,3 +10,6 @@ if [ "${CLEAN_CILIUM_STATE}" = "true" ] \
     rm -rf /sys/fs/bpf/tc/globals/cilium_* \
            /var/run/cilium/bpffs/tc/globals/cilium_*;
 fi
+if [ "${CILIUM_WAIT_BPF_MOUNT}" = "true" ]; then
+	until mount | grep bpf; do echo "BPF filesystem is not mounted yet"; sleep 1; done
+fi;

examples/kubernetes/Makefile
@@ -4,7 +4,7 @@ ifeq ($(CILIUM_VERSION),)
     CILIUM_VERSION = "v$(shell cat ../../VERSION)"
 endif
 
-CILIUM_INIT_VERSION = "2018-10-16"
+CILIUM_INIT_VERSION = "2019-04-05"
 CILIUM_ETCD_OPERATOR_VERSION = "v2.0.6"
 K8S_VERSIONS = 1.10 1.11 1.12 1.13 1.14
 
@@ -121,7 +121,8 @@ cilium-with-node-init.yaml:
 	    sed -i 's+namespace: kube-system+namespace: cilium+' cilium-with-node-init.yaml; \
 	    sed -i '/priorityClassName: system-node-critical/d' cilium-with-node-init.yaml; \
 	    sed -i 's+path: /opt/cni/bin+path: /home/kubernetes/bin+' cilium-with-node-init.yaml; \
-	    sed -i 's+https://cilium-etcd-client.kube-system.svc:2379+https://cilium-etcd-client.cilium.svc:2379+' cilium-with-node-init.yaml); \
+	    sed -i 's+https://cilium-etcd-client.kube-system.svc:2379+https://cilium-etcd-client.cilium.svc:2379+' cilium-with-node-init.yaml; \
+        sed -i 's+wait-bpf-mount: "false"+wait-bpf-mount: "true"+' cilium-with-node-init.yaml); \
 	done
 
 cilium-external-etcd.yaml:

examples/kubernetes/templates/v1/cilium-cm.yaml
@@ -148,3 +148,6 @@ data:
   # [0] http://docs.cilium.io/en/stable/policy/language/#dns-based
   # [1] http://docs.cilium.io/en/stable/install/upgrade/#changes-that-may-require-action
   tofqdns-enable-poller: "false"
+
+  # wait-bpf-mount makes init container wait until bpf filesystem is mounted
+  wait-bpf-mount: "false"

examples/kubernetes/templates/v1/cilium-ds.yaml.sed
@@ -166,6 +166,12 @@ spec:
               key: clean-cilium-bpf-state
               name: cilium-config
               optional: true
+        - name: CILIUM_WAIT_BPF_MOUNT
+          valueFrom:
+            configMapKeyRef:
+              key: wait-bpf-mount
+              name: cilium-config
+              optional: true
         image: docker.io/cilium/cilium-init:__CILIUM_INIT_VERSION__
         imagePullPolicy: IfNotPresent
         name: clean-cilium-state

commit a7e351c928038c89d7baf952d7d9625110f6b321

init-container: Look for a concrete BPFFS mount in /sys/fs/bpf
Before this change, init container was looking for any mount which
contains the `bpf` string, which was always true, even if there was no
real BPFFS mount. When BPFFS is not mounted on host, init container sees
the mount from host directory of sysfs type:

```
sysfs on /sys/fs/bpf type sysfs (rw,relatime)
```

To check whether BPFFS is mounted properly, we need to look explicitly
for a mount entry like:

```
bpf on /sys/fs/bpf type bpf (rw,nosuid,nodev,noexec,relatime,mode=700)
```

Fixes: b264343377fe ("Add bpf mount check to Cilium init container")
Fixes: #8646
Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

contrib/packaging/docker/init-container.sh
@@ -15,5 +15,5 @@ if [ "${CILIUM_ALL_STATE}" = "true" ] \
 fi
 
 if [ "${CILIUM_WAIT_BPF_MOUNT}" = "true" ]; then
-	until mount | grep bpf; do echo "BPF filesystem is not mounted yet"; sleep 1; done
+	until mount | grep "/sys/fs/bpf type bpf"; do echo "BPF filesystem is not mounted yet"; sleep 1; done
 fi;