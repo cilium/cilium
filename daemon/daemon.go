ad247126626174dd14876fd9128d2316cbb9cc51
bob <bob.bouteillier@datadoghq.com>
2019-07-25 19:36:44 +0200

    [daemon] - Change MTU source for cilium_host (Use the Route one)
    [ upstream commit 86f926e816dc871bdd985c82f556644aae58c6d9 ]
    
    * for pods in hostNetwork the mtu was the same a the default interface.
      Packets were thus dropped once the stream was encrypted.
    Fixes: 1ce278f ("daemon: move IPSec bootstrap into separate function")
    
    Signed-off-by: Ray Bejjani <ray@isovalent.com>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -469,7 +469,7 @@ func (d *Daemon) compileBase() error {
 		args[initArgIPv6NodeIP] = "<nil>"
 	}
 
-	args[initArgMTU] = fmt.Sprintf("%d", d.mtuConfig.GetDeviceMTU())
+	args[initArgMTU] = fmt.Sprintf("%d", d.mtuConfig.GetRouteMTU())
 
 	if option.Config.EnableIPSec {
 		args[initArgIPSec] = "true"
