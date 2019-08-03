cilium/cilium

commit cb0d869b9d603ec72e0fc2321a4da3cc38ef0404

nodediscovery: Create CiliumNode from the nodediscovery package
This prepare extending the CiliumNode with additional nodediscovery relevant
fields.

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/daemon.go
@@ -863,7 +863,7 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 		log.Debug("Annotate k8s node is disabled.")
 	}
 
-	d.nodeDiscovery.StartDiscovery(node.GetName())
+	d.nodeDiscovery.StartDiscovery(node.GetName(), &d)
 
 	// This needs to be done after the node addressing has been configured
 	// as the node address is required as suffix.

pkg/ipam/crd.go
@@ -21,9 +21,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/cilium/cilium/pkg/aws/metadata"
 	"github.com/cilium/cilium/pkg/cidr"
-	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/k8s"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/informer"
@@ -35,7 +33,6 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/cache"
@@ -85,76 +82,6 @@ func newNodeStore(nodeName string, owner Owner) *nodeStore {
 		allocationPoolSize: map[Family]int{},
 	}
 	ciliumClient := k8s.CiliumClient()
-
-	if option.Config.AutoCreateCiliumNodeResource {
-		nodeResource := &ciliumv2.CiliumNode{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: node.GetName(),
-			},
-		}
-
-		// Tie the CiliumNode custom resource lifecycle to the
-		// lifecycle of the Kubernetes node
-		if k8sNode, err := k8s.GetNode(k8s.Client(), node.GetName()); err != nil {
-			log.Warning("Kubernetes node resource representing own node is not available, cannot set OwnerReference")
-		} else {
-			nodeResource.ObjectMeta.OwnerReferences = []metav1.OwnerReference{{
-				APIVersion: "v1",
-				Kind:       "Node",
-				Name:       node.GetName(),
-				UID:        k8sNode.UID,
-			}}
-		}
-
-		if option.Config.IPAM == option.IPAMENI {
-			instanceID, instanceType, availabilityZone, vpcID, err := metadata.GetInstanceMetadata()
-			if err != nil {
-				log.WithError(err).Fatal("Unable to retrieve InstanceID of own EC2 instance")
-			}
-
-			nodeResource.Spec.ENI.VpcID = vpcID
-			nodeResource.Spec.ENI.FirstInterfaceIndex = 1
-			nodeResource.Spec.ENI.DeleteOnTermination = true
-			nodeResource.Spec.ENI.PreAllocate = defaults.ENIPreAllocation
-
-			if c := owner.GetNetConf(); c != nil {
-				if c.ENI.MinAllocate != 0 {
-					nodeResource.Spec.ENI.MinAllocate = c.ENI.MinAllocate
-				}
-
-				if c.ENI.PreAllocate != 0 {
-					nodeResource.Spec.ENI.PreAllocate = c.ENI.PreAllocate
-				}
-
-				if c.ENI.FirstInterfaceIndex != 0 {
-					nodeResource.Spec.ENI.FirstInterfaceIndex = c.ENI.FirstInterfaceIndex
-				}
-
-				if len(c.ENI.SecurityGroups) > 0 {
-					nodeResource.Spec.ENI.SecurityGroups = c.ENI.SecurityGroups
-				}
-
-				if len(c.ENI.SubnetTags) > 0 {
-					nodeResource.Spec.ENI.SubnetTags = c.ENI.SubnetTags
-				}
-
-				if c.ENI.VpcID != "" {
-					nodeResource.Spec.ENI.VpcID = c.ENI.VpcID
-				}
-
-				nodeResource.Spec.ENI.DeleteOnTermination = c.ENI.DeleteOnTermination
-			}
-
-			nodeResource.Spec.ENI.InstanceID = instanceID
-			nodeResource.Spec.ENI.InstanceType = instanceType
-			nodeResource.Spec.ENI.AvailabilityZone = availabilityZone
-		}
-
-		_, err := ciliumClient.CiliumV2().CiliumNodes().Create(nodeResource)
-		if err != nil {
-			log.WithError(err).Error("Unable to create CiliumNode resource")
-		}
-	}
 
 	t, err := trigger.NewTrigger(trigger.Parameters{
 		Name:        "crd-allocator-node-refresher",

pkg/ipam/ipam.go
@@ -23,7 +23,6 @@ import (
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
-	cnitypes "github.com/cilium/cilium/plugins/cilium-cni/types"
 
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
@@ -60,10 +59,6 @@ type Configuration struct {
 
 // Owner is the interface the owner of an IPAM allocator has to implement
 type Owner interface {
-	// GetNetConf must return the CNI configuration as passed in by the
-	// user
-	GetNetConf() *cnitypes.NetConf
-
 	// K8sEventReceived is called to do metrics accounting for received
 	// Kubernetes events
 	K8sEventReceived(scope string, action string, valid, equal bool)

pkg/nodediscovery/nodediscovery.go
@@ -18,10 +18,13 @@ import (
 	"context"
 	"time"
 
+	"github.com/cilium/cilium/pkg/aws/metadata"
 	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/k8s"
+	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/mtu"
@@ -31,6 +34,9 @@ import (
 	nodestore "github.com/cilium/cilium/pkg/node/store"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/source"
+
+	cnitypes "github.com/cilium/cilium/plugins/cilium-cni/types"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -100,10 +106,18 @@ func NewNodeDiscovery(manager *nodemanager.Manager, mtuConfig mtu.Configuration)
 	}
 }
 
+// Configuration is the configuration interface that must be implemented in
+// order to manage node discovery
+type Configuration interface {
+	// GetNetConf must return the CNI configuration as passed in by the
+	// user
+	GetNetConf() *cnitypes.NetConf
+}
+
 // start configures the local node and starts node discovery. This is called on
 // agent startup to configure the local node based on the configuration options
 // passed to the agent. nodeName is the name to be used in the local agent.
-func (n *NodeDiscovery) StartDiscovery(nodeName string) {
+func (n *NodeDiscovery) StartDiscovery(nodeName string, conf Configuration) {
 	n.LocalNode.Name = nodeName
 	n.LocalNode.Cluster = option.Config.ClusterName
 	n.LocalNode.IPAddresses = []node.Address{}
@@ -163,6 +177,12 @@ func (n *NodeDiscovery) StartDiscovery(nodeName string) {
 		}
 	}()
 
+	if k8s.IsEnabled() {
+		// Creation of the CiliumNode can be done in the background,
+		// nothing depends on the completion of this.
+		go n.createCiliumNodeResource(conf)
+	}
+
 	go func() {
 		<-n.Registered
 		controller.NewManager().UpdateController("propagating local node change to kv-store",
@@ -181,4 +201,80 @@ func (n *NodeDiscovery) StartDiscovery(nodeName string) {
 // Close shuts down the node discovery engine
 func (n *NodeDiscovery) Close() {
 	n.Manager.Close()
+}
+
+func (n *NodeDiscovery) createCiliumNodeResource(conf Configuration) {
+	if !option.Config.AutoCreateCiliumNodeResource {
+		return
+	}
+
+	ciliumClient := k8s.CiliumClient()
+
+	nodeResource := &ciliumv2.CiliumNode{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: node.GetName(),
+		},
+	}
+
+	// Tie the CiliumNode custom resource lifecycle to the lifecycle of the
+	// Kubernetes node
+	if k8sNode, err := k8s.GetNode(k8s.Client(), node.GetName()); err != nil {
+		log.Warning("Kubernetes node resource representing own node is not available, cannot set OwnerReference")
+	} else {
+		nodeResource.ObjectMeta.OwnerReferences = []metav1.OwnerReference{{
+			APIVersion: "v1",
+			Kind:       "Node",
+			Name:       node.GetName(),
+			UID:        k8sNode.UID,
+		}}
+	}
+
+	if option.Config.IPAM == option.IPAMENI {
+		instanceID, instanceType, availabilityZone, vpcID, err := metadata.GetInstanceMetadata()
+		if err != nil {
+			log.WithError(err).Fatal("Unable to retrieve InstanceID of own EC2 instance")
+		}
+
+		nodeResource.Spec.ENI.VpcID = vpcID
+		nodeResource.Spec.ENI.FirstInterfaceIndex = 1
+		nodeResource.Spec.ENI.DeleteOnTermination = true
+		nodeResource.Spec.ENI.PreAllocate = defaults.ENIPreAllocation
+
+		if c := conf.GetNetConf(); c != nil {
+			if c.ENI.MinAllocate != 0 {
+				nodeResource.Spec.ENI.MinAllocate = c.ENI.MinAllocate
+			}
+
+			if c.ENI.PreAllocate != 0 {
+				nodeResource.Spec.ENI.PreAllocate = c.ENI.PreAllocate
+			}
+
+			if c.ENI.FirstInterfaceIndex != 0 {
+				nodeResource.Spec.ENI.FirstInterfaceIndex = c.ENI.FirstInterfaceIndex
+			}
+
+			if len(c.ENI.SecurityGroups) > 0 {
+				nodeResource.Spec.ENI.SecurityGroups = c.ENI.SecurityGroups
+			}
+
+			if len(c.ENI.SubnetTags) > 0 {
+				nodeResource.Spec.ENI.SubnetTags = c.ENI.SubnetTags
+			}
+
+			if c.ENI.VpcID != "" {
+				nodeResource.Spec.ENI.VpcID = c.ENI.VpcID
+			}
+
+			nodeResource.Spec.ENI.DeleteOnTermination = c.ENI.DeleteOnTermination
+		}
+
+		nodeResource.Spec.ENI.InstanceID = instanceID
+		nodeResource.Spec.ENI.InstanceType = instanceType
+		nodeResource.Spec.ENI.AvailabilityZone = availabilityZone
+	}
+
+	_, err := ciliumClient.CiliumV2().CiliumNodes().Create(nodeResource)
+	if err != nil {
+		log.WithError(err).Fatal("Unable to create CiliumNode resource")
+	}
 }

pkg/ipam/allocator_test.go
@@ -21,16 +21,11 @@ import (
 
 	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/pkg/datapath/fake"
-	cnitypes "github.com/cilium/cilium/plugins/cilium-cni/types"
 
 	. "gopkg.in/check.v1"
 )
 
 type ownerMock struct{}
-
-func (o *ownerMock) GetNetConf() *cnitypes.NetConf {
-	return nil
-}
 
 func (o *ownerMock) K8sEventReceived(scope string, action string, valid, equal bool) {}
 func (o *ownerMock) K8sEventProcessed(scope string, action string, status bool)      {}

commit ef2008371ce9f6af59c3eb69ef24edc6dec0774a

ipam: eni: Resolve bootstrap misorder to create CiliumNode CR for ENI
Commit cb0d869b9d6 has moved the creation of the CiliumNode CR into the
nodediscovery package to allow inclusion of health and node IP addresses. This
created a deadlock on bootstrap as nodediscovery is only performed after IPAM
init. IPAM init for CRD depends on the creation of the CR in the ENI scenario.

Export the update call and create the CR in the ENI IPAM init code and then
update it in the nodediscovery case.

Fixes: cb0d869b9d6 ("nodediscovery: Create CiliumNode from the nodediscovery package")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/daemon.go
@@ -1123,3 +1123,9 @@ func (d *Daemon) GetNodeSuffix() string {
 func (d *Daemon) GetNetConf() *cnitypes.NetConf {
 	return d.netConf
 }
+
+// UpdateCiliumNodeResource implements nodediscovery.Owner to create/update the
+// CiliumNode resource
+func (d *Daemon) UpdateCiliumNodeResource() {
+	d.nodeDiscovery.UpdateCiliumNodeResource(d)
+}

pkg/ipam/crd.go
@@ -93,6 +93,10 @@ func newNodeStore(nodeName string, owner Owner) *nodeStore {
 	}
 	store.refreshTrigger = t
 
+	// Create the CiliumNode custom resource. This call will block until
+	// the custom resource has been created
+	owner.UpdateCiliumNodeResource()
+
 	ciliumNodeSelector := fields.ParseSelectorOrDie("metadata.name=" + nodeName)
 	ciliumNodeStore := cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
 	ciliumNodeInformer := informer.NewInformerWithStore(

pkg/ipam/ipam.go
@@ -66,6 +66,11 @@ type Owner interface {
 	// K8sEventProcessed is called to do metrics accounting for each processed
 	// Kubernetes event
 	K8sEventProcessed(scope string, action string, status bool)
+
+	// UpdateCiliumNodeResource is called to create/update the CiliumNode
+	// resource. The function must block until the custom resource has been
+	// created.
+	UpdateCiliumNodeResource()
 }
 
 // NewIPAM returns a new IP address manager

pkg/nodediscovery/nodediscovery.go
@@ -178,9 +178,9 @@ func (n *NodeDiscovery) StartDiscovery(nodeName string, conf Configuration) {
 	}()
 
 	if k8s.IsEnabled() {
-		// Creation of the CiliumNode can be done in the background,
-		// nothing depends on the completion of this.
-		go n.createCiliumNodeResource(conf)
+		// Creation or update of the CiliumNode can be done in the
+		// background, nothing depends on the completion of this.
+		go n.UpdateCiliumNodeResource(conf)
 	}
 
 	if option.Config.KVStore != "" {
@@ -205,7 +205,9 @@ func (n *NodeDiscovery) Close() {
 	n.Manager.Close()
 }
 
-func (n *NodeDiscovery) createCiliumNodeResource(conf Configuration) {
+// UpdateCiliumNodeResource updates the CiliumNode resource representing the
+// local node
+func (n *NodeDiscovery) UpdateCiliumNodeResource(conf Configuration) {
 	if !option.Config.AutoCreateCiliumNodeResource {
 		return
 	}

pkg/ipam/allocator_test.go
@@ -29,6 +29,7 @@ type ownerMock struct{}
 
 func (o *ownerMock) K8sEventReceived(scope string, action string, valid, equal bool) {}
 func (o *ownerMock) K8sEventProcessed(scope string, action string, status bool)      {}
+func (o *ownerMock) UpdateCiliumNodeResource()                                       {}
 
 func (s *IPAMSuite) TestAllocatedIPDump(c *C) {
 	fakeAddressing := fake.NewNodeAddressing()