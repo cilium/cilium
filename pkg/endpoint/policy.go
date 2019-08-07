65acd9d925430381753dbfd7214034cfd894669b
Ian Vernon <ian@cilium.io>
2019-08-07 14:33:21 -0700

    eventqueue: return error if Enqueue fails
    [ upstream commit bb115ad8face1af7d90eb044612411e69b68c35c ]
    
    Given that an Enqueue may not occur due to the queue not being initialized
    correctly / the Event being nil, or the Event has already been Enqueued before,
    return an error which follows Golang conventions instead of encoding error state
    in the returned value.
    
    Signed-off by: Ian Vernon <ian@cilium.io>
    
    Signed-off-by: Ian Vernon <ian@cilium.io>

--- pkg/endpoint/policy.go
+++ pkg/endpoint/policy.go
@@ -449,7 +449,13 @@ func (e *Endpoint) Regenerate(owner Owner, regenMetadata *ExternalRegenerationMe
 	// This may block if the Endpoint's EventQueue is full. This has to be done
 	// synchronously as some callers depend on the fact that the event is
 	// synchronously enqueued.
-	resChan := e.EventQueue.Enqueue(epEvent)
+	resChan, err := e.EventQueue.Enqueue(epEvent)
+	if err != nil {
+		e.getLogger().Errorf("enqueue of EndpointRegenerationEvent failed: %s", err)
+		done <- false
+		close(done)
+		return done
+	}
 
 	go func() {
 
