cilium/cilium

commit cc66f0a90324de7ef2085359babf57da57c7e792

operator: Start health API earlier
This avoids getting the operator killed while waiting on k8s apiserver connectivity

Signed-off-by: Thomas Graf <thomas@cilium.io>

operator/main.go
@@ -146,6 +146,7 @@ func runOperator(cmd *cobra.Command) {
 	logging.SetupLogging([]string{}, map[string]string{}, "cilium-operator", viper.GetBool("debug"))
 
 	log.Infof("Cilium Operator %s", version.Version)
+	go StartServer(fmt.Sprintf(":%d", apiServerPort), shutdownSignal)
 
 	if err := kvstore.Setup(kvStore, kvStoreOpts, nil); err != nil {
 		log.WithError(err).WithFields(logrus.Fields{
@@ -193,8 +194,6 @@ func runOperator(cmd *cobra.Command) {
 		log.WithError(err).WithField("subsys", "CNPWatcher").Fatal(
 			"Cannot connect to Kubernetes apiserver ")
 	}
-
-	go StartServer(fmt.Sprintf(":%d", apiServerPort), shutdownSignal)
 
 	<-shutdownSignal
 	// graceful exit

commit 127cc95e42c21b0137d98d5e95a075e5db002dee

operator: start health check handler after initializing k8s client
As the health checks performed in depend in the initialization of k8s
client we should start the health check handler after initializing the
k8s client.

Fixes: cc66f0a90324 ("operator: Start health API earlier")
Signed-off-by: André Martins <andre@cilium.io>

operator/api.go
@@ -24,10 +24,18 @@ import (
 )
 
 // startServer starts an api server listening on the given address.
-func startServer(addr string, shutdownSignal <-chan struct{}) {
+func startServer(addr string, shutdownSignal <-chan struct{}, allSystemsGo <-chan struct{}) {
 	log.Infof("Starting apiserver on address %s", addr)
 
-	http.HandleFunc("/healthz", healthHandler)
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		select {
+		// only start serving the real health check once all systems all up and running
+		case <-allSystemsGo:
+			healthHandler(w, r)
+		default:
+			healthHandlerOK(w, r)
+		}
+	})
 
 	srv := &http.Server{Addr: addr}
 
@@ -39,6 +47,13 @@ func startServer(addr string, shutdownSignal <-chan struct{}) {
 	}()
 
 	log.Fatalf("Unable to start status api: %s", srv.ListenAndServe())
+}
+
+func healthHandlerOK(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write([]byte("ok")); err != nil {
+		log.WithError(err).Error("Failed to write liveness-probe response")
+	}
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {

operator/main.go
@@ -184,7 +184,8 @@ func runOperator(cmd *cobra.Command) {
 	logging.SetupLogging([]string{}, map[string]string{}, "cilium-operator", viper.GetBool("debug"))
 
 	log.Infof("Cilium Operator %s", version.Version)
-	go startServer(fmt.Sprintf(":%d", apiServerPort), shutdownSignal)
+	k8sInitDone := make(chan struct{})
+	go startServer(fmt.Sprintf(":%d", apiServerPort), shutdownSignal, k8sInitDone)
 
 	if enableMetrics {
 		registerMetrics()
@@ -197,6 +198,7 @@ func runOperator(cmd *cobra.Command) {
 	if err := k8s.Init(); err != nil {
 		log.WithError(err).Fatal("Unable to connect to Kubernetes apiserver")
 	}
+	close(k8sInitDone)
 
 	ciliumK8sClient = k8s.CiliumClient()
 	k8sversion.Update(k8s.Client())