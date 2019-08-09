8a503e2ff3c77c0f5668e6a7d5fbf81e08bdeb1b
Ian Vernon <ian@cilium.io>
2019-08-09 08:09:48 -0700

    datapath: factor out configuration-writing functions into interface
    A `Datapath` is composed of a `ConfigWriter`, so no functional changes are
    present in this commit. Future commits depend on this factoring out so that
    there are not import cycles.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- pkg/datapath/datapath.go
+++ pkg/datapath/datapath.go
@@ -14,38 +14,18 @@
 
 package datapath
 
-import (
-	"io"
-)
-
 // Datapath is the interface to abstract all datapath interactions. The
 // abstraction allows to implement the datapath requirements with multiple
 // implementations
 type Datapath interface {
+	ConfigWriter
+
 	// Node must return the handler for node events
 	Node() NodeHandler
 
 	// LocalNodeAddressing must return the node addressing implementation
 	// of the local node
 	LocalNodeAddressing() NodeAddressing
-
-	// WriteNodeConfig writes the implementation-specific configuration of
-	// node-wide options into the specified writer.
-	WriteNodeConfig(io.Writer, *LocalNodeConfiguration) error
-
-	// WriteNetdevConfig writes the implementation-specific configuration
-	// of configurable options to the specified writer. Options specified
-	// here will apply to base programs and not to endpoints, though
-	// endpoints may have equivalent configurable options.
-	WriteNetdevConfig(io.Writer, DeviceConfiguration) error
-
-	// WriteTemplateConfig writes the implementation-specific configuration
-	// of configurable options for BPF templates to the specified writer.
-	WriteTemplateConfig(w io.Writer, cfg EndpointConfiguration) error
-
-	// WriteEndpointConfig writes the implementation-specific configuration
-	// of configurable options for the endpoint to the specified writer.
-	WriteEndpointConfig(w io.Writer, cfg EndpointConfiguration) error
 
 	// InstallProxyRules creates the necessary datapath config (e.g., iptables
 	// rules for redirecting host proxy traffic on a specific ProxyPort)
