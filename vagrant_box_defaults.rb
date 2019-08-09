843056a966f10ccd499c67f52f618cc630500470
Maciej Kwiek <maciej@isovalent.com>
2019-08-09 15:34:08 +0200

    Bump vagrant box versions
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

--- vagrant_box_defaults.rb
+++ vagrant_box_defaults.rb
@@ -3,6 +3,6 @@
 Vagrant.require_version ">= 2.2.0"
 
 $SERVER_BOX = "cilium/ubuntu-dev"
-$SERVER_VERSION= "157"
+$SERVER_VERSION= "158"
 $NETNEXT_SERVER_BOX= "cilium/ubuntu-next"
-$NETNEXT_SERVER_VERSION= "31"
+$NETNEXT_SERVER_VERSION= "32"
