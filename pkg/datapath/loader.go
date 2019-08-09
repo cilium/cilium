5695d9daed745243202fd8d25a912d67824b56b6
Ian Vernon <ian@cilium.io>
2019-08-09 10:01:01 -0700

    datapath: add Loader interface
    This is currently only implemented by `pkg/loader:Loader`.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- pkg/datapath/loader.go
+++ pkg/datapath/loader.go
@@ -1,1 +1,33 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package datapath
+
+import (
+	"context"
+
+	"github.com/cilium/cilium/pkg/datapath/loader/metrics"
+)
+
+// Loader is an interface to abstract out loading of datapath programs.
+type Loader interface {
+	CallsMapPath(id uint16) string
+	CompileAndLoad(ctx context.Context, ep Endpoint, stats *metrics.SpanStat) error
+	CompileOrLoad(ctx context.Context, ep Endpoint, stats *metrics.SpanStat) error
+	ReloadDatapath(ctx context.Context, ep Endpoint, stats *metrics.SpanStat) error
+	EndpointHash(cfg EndpointConfiguration) (string, error)
+	DeleteDatapath(ctx context.Context, ifName, direction string) error
+	Unload(ep Endpoint)
+	Init(d ConfigWriter, nodeCfg *LocalNodeConfiguration)
+}
