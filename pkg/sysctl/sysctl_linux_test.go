3679bdfeb9161a99dcac919c995dd86b46680cab
Michal Rostecki <mrostecki@opensuse.org>
2019-08-13 15:17:53 +0200

    sysctl: Add package for managing kernel parameters
    [ upstream commit 759ae501d9dff7a1ef5ddbbc4139ca233db4ec22 ]
    
    The new sysctl package allows to disable and enable the runtime kernel
    parameters. It expects the options to be provided in format of variable
    names like i.e. "net.ipv4.ip_forward".
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

---
+++ pkg/sysctl/sysctl_linux_test.go
@@ -0,0 +1,60 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build linux
+
+package sysctl
+
+import (
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type SysctlLinuxTestSuite struct{}
+
+var _ = Suite(&SysctlLinuxTestSuite{})
+
+func (s *SysctlLinuxTestSuite) TestFullPath(c *C) {
+	testCases := []struct {
+		name     string
+		expected string
+	}{
+		{
+			name:     "net.ipv4.ip_forward",
+			expected: "/proc/sys/net/ipv4/ip_forward",
+		},
+		{
+			name:     "net.ipv4.conf.all.forwarding",
+			expected: "/proc/sys/net/ipv4/conf/all/forwarding",
+		},
+		{
+			name:     "net.ipv6.conf.all.forwarding",
+			expected: "/proc/sys/net/ipv6/conf/all/forwarding",
+		},
+		{
+			name:     "foo.bar",
+			expected: "/proc/sys/foo/bar",
+		},
+	}
+
+	for _, tc := range testCases {
+		c.Assert(fullPath(tc.name), Equals, tc.expected)
+	}
+}
