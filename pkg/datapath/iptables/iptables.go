cilium/cilium

commit 431bd82d39c743c20d2a9e49944998508fd810e4

cilium: fix up source address selection for cluster ip
[ upstream commit 2bdca067a90833a2d03d0d8a986f821a469121e0 ]

The CI test 'K8sServicesTest Checks ClusterIP Connectivity Checks service
on same node' failed recently due to a buggy L7 policy removal from prior
test; fixed via d8ff01841646 ("test: fix incorrect deletion statement for
policy"). This incorrect deletion uncovered another bug however triggered
by curl failing to connect to the endpoint backing the service:

  # curl --path-as-is -s -D /dev/stderr --fail --connect-timeout 5 --max-time 8 http://10.110.178.169/ -w "time-> DNS: '%{time_namelookup}(%{remote_ip})', Connect: '%{time_connect}',Transfer '%{time_starttransfer}', total '%{time_total}'"
time-> DNS: '0.000038()', Connect: '0.000000',Transfer '0.000000', total '3.203689'

  (Note, DNS has nothing to do with the failure.)

  # docker exec -ti 6d5465170bf4 bash
  # cilium bpf lb list
  SERVICE ADDRESS      BACKEND ADDRESS
  10.110.178.169:80    0.0.0.0:0 (6)        <--- service from above curl
                       10.10.0.183:80 (6)
                       10.10.0.62:80 (6)
  10.101.55.250:2379   10.10.1.67:2379 (3)
                       0.0.0.0:0 (3)
                       10.10.1.162:2379 (3)
                       10.10.0.24:2379 (3)
  10.96.0.10:53        0.0.0.0:0 (2)
                       10.10.1.185:53 (2)
  10.96.0.1:443        192.168.36.11:6443 (1)
                       0.0.0.0:0 (1)
  # exit

  # ip route get 10.110.178.169
  10.110.178.169 via 10.0.2.2 dev enp0s3 src 10.0.2.15 uid 0
      cache
  # ip route get 10.10.0.183
  10.10.0.183 via 10.10.0.160 dev cilium_host src 10.10.0.160 uid 0
      cache
  # ip route get 10.10.0.62
  10.10.0.62 via 10.10.0.160 dev cilium_host src 10.10.0.160 uid 0
      cache
  # ip r
  default via 10.0.2.2 dev enp0s3 proto dhcp src 10.0.2.15 metric 100
  10.0.2.0/24 dev enp0s3 proto kernel scope link src 10.0.2.15
  10.0.2.2 dev enp0s3 proto dhcp scope link src 10.0.2.15 metric 100

pkg/datapath/iptables/iptables.go
@@ -33,23 +33,25 @@ import (
 )
 
 const (
-	ciliumOutputChain     = "CILIUM_OUTPUT"
-	ciliumOutputRawChain  = "CILIUM_OUTPUT_raw"
-	ciliumPostNatChain    = "CILIUM_POST"
-	ciliumPostMangleChain = "CILIUM_POST_mangle"
-	ciliumPreMangleChain  = "CILIUM_PRE_mangle"
-	ciliumPreRawChain     = "CILIUM_PRE_raw"
-	ciliumForwardChain    = "CILIUM_FORWARD"
-	feederDescription     = "cilium-feeder:"
-	xfrmDescription       = "cilium-xfrm-notrack:"
+	ciliumOutputChain      = "CILIUM_OUTPUT"
+	ciliumOutputRawChain   = "CILIUM_OUTPUT_raw"
+	ciliumPostNatChain     = "CILIUM_POST"
+	ciliumPostNatKubeChain = "CILIUM_POST_KUBE"
+	ciliumPostMangleChain  = "CILIUM_POST_mangle"
+	ciliumPreMangleChain   = "CILIUM_PRE_mangle"
+	ciliumPreRawChain      = "CILIUM_PRE_raw"
+	ciliumForwardChain     = "CILIUM_FORWARD"
+	feederDescription      = "cilium-feeder:"
+	xfrmDescription        = "cilium-xfrm-notrack:"
 )
 
 type customChain struct {
-	name       string
-	table      string
-	hook       string
-	feederArgs []string
-	ipv6       bool // ip6tables chain in addition to iptables chain
+	name        string
+	table       string
+	hook        string
+	feederArgs  []string
+	ipv6        bool // ip6tables chain in addition to iptables chain
+	appendFixed bool
 }
 
 func runProg(prog string, args []string, quiet bool) error {
@@ -155,7 +157,7 @@ func (c *customChain) remove() {
 
 func (c *customChain) installFeeder() error {
 	installMode := "-A"
-	if option.Config.PrependIptablesChains {
+	if option.Config.PrependIptablesChains && !c.appendFixed {
 		installMode = "-I"
 	}
 
@@ -194,6 +196,14 @@ var ciliumChains = []customChain{
 		table:      "nat",
 		hook:       "POSTROUTING",
 		feederArgs: []string{""},
+	},
+	{
+		name:        ciliumPostNatKubeChain,
+		table:       "nat",
+		hook:        "POSTROUTING",
+		feederArgs:  []string{""},
+		ipv6:        true,
+		appendFixed: true,
 	},
 	{
 		name:       ciliumPostMangleChain,
@@ -258,7 +268,7 @@ func (m *IptablesManager) RemoveRules() {
 
 	// Set of tables that have had ip6tables rules in any Cilium version
 	if m.ip6tables {
-		tables6 := []string{"mangle", "raw"}
+		tables6 := []string{"nat", "mangle", "raw"}
 		for _, t := range tables6 {
 			removeCiliumRules(t, "ip6tables")
 		}
@@ -325,6 +335,39 @@ func (m *IptablesManager) InstallRules(ifName string) error {
 			"-m", "comment", "--comment", "cilium: cluster->any forward accept",
 			"-j", "ACCEPT"}, false); err != nil {
 			return err
+		}
+
+		// For communication from host to local services via k8s cluster IP
+		// we need to fix up wrong source address selection. Linux routing
+		// will initially select a source address based on the service IP and
+		// after Kubernetes iptables rules selected a local backend, we still
+		// retain the original source address (and not the one on cilium_host).
+		// As a result, ipcache will assign a WORLD identity (via catch-all 0/0)
+		// as opposed to a HOST identity and therefore policy is dropping the
+		// skb. As there is no fixup by iptables, we need to SNAT for these
+		// cases. This rule here must come after all Kubernetes post-routing
+		// chains, so we can match on our endpoint allocation range.
+		if option.Config.EnableIPv4 {
+			if err := runProg("iptables", []string{
+				"-t", "nat",
+				"-A", ciliumPostNatKubeChain,
+				"-d", node.GetIPv4AllocRange().String(),
+				"-o", ifName,
+				"-m", "comment", "--comment", "cilium: host->service(cluster ip)->local-endpoint on " + ifName + " src address fix",
+				"-j", "SNAT", "--to-source", node.GetHostMasqueradeIPv4().String()}, false); err != nil {
+				return err
+			}
+		}
+		if option.Config.EnableIPv6 {
+			if err := runProg("ip6tables", []string{
+				"-t", "nat",
+				"-A", ciliumPostNatKubeChain,
+				"-d", node.GetIPv6AllocRange().String(),
+				"-o", ifName,
+				"-m", "comment", "--comment", "cilium: host->service(cluster ip)->local-endpoint on " + ifName + " src address fix",
+				"-j", "SNAT", "--to-source", node.GetIPv6().String()}, false); err != nil {
+				return err
+			}
 		}
 
 		// Mark all packets sourced from processes running on the host with a

commit 7b0d9ebcc0d07e6dcc71c0383b6da6cffc821d1a

datapath: Limit host->service IP SNAT to local traffic
The existing rule applies to all traffic via cilium_host which also applies to
traffic that is routed into cilium_host from an external interface. This breaks
"externaltrafficpolicy: local".

Fixes: 431bd82d39c ("cilium: fix up source address selection for cluster ip# Please enter the commit message for your changes. Lines starting")

Reported-by: Ole Markus With
Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/datapath/iptables/iptables.go
@@ -475,6 +475,7 @@ func (m *IptablesManager) InstallRules(ifName string) error {
 				"-A", ciliumPostNatKubeChain,
 				"-d", node.GetIPv4AllocRange().String(),
 				"-o", ifName,
+				"-m", "addrtype", "--src-type", "local",
 				"-m", "comment", "--comment", "cilium: host->service(cluster ip)->local-endpoint on "+ifName+" src address fix",
 				"-j", "SNAT", "--to-source", node.GetHostMasqueradeIPv4().String()), false); err != nil {
 				return err
@@ -487,6 +488,7 @@ func (m *IptablesManager) InstallRules(ifName string) error {
 				"-A", ciliumPostNatKubeChain,
 				"-d", node.GetIPv6AllocRange().String(),
 				"-o", ifName,
+				"-m", "addrtype", "--src-type", "local",
 				"-m", "comment", "--comment", "cilium: host->service(cluster ip)->local-endpoint on "+ifName+" src address fix",
 				"-j", "SNAT", "--to-source", node.GetIPv6().String()), false); err != nil {
 				return err