0dde139cecf54ec89286bb60697e058a07f41080
Joe Stringer <joe@cilium.io>
2019-08-21 11:29:35 -0700

    workloads: Fix disabled status reflection in API
    Previously in Cilium status, because the status (and not the message)
    was set to disabled, the primary cilium status would complain in a way
    that hints that something is wrong with the container runtime plugin:
    
        $ cilium status | grep -i runtime
        ContainerRuntime:       Disabled
        Cilium:                 Disabled   Container runtime is not ready
    
    However, other status like kvstore when disabled show up like this:
    
        KVStore:                Ok         Disabled
    
    This commit fixes the container runtime to follow the kvstore style,
    which means that the "Cilium" status line will no longer format a
    message stating that the container runtime isn't ready.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/workloads/crio.go
+++ pkg/workloads/crio.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Authors of Cilium
+// Copyright 2018-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
 package workloads
 
 import (
@@ -94,7 +94,7 @@ func (c *criOClient) IsRunning(ep *endpoint.Endpoint) bool {
 // Status returns the status of the workload runtime
 func (c *criOClient) Status() *models.Status {
 	if c == nil {
-		return &models.Status{State: models.StatusStateDisabled}
+		return workloadStatusDisabled
 	}
 
 	criStatus := c.cri.Status()
