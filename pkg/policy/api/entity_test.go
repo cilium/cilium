85ccb85227735dd88ca441a943e1a52e2e8fc07e
Jarno Rajahalme <jarno@covalent.io>
2019-08-23 10:48:14 -0700

    policy/api: remove Entity matching functions
    Entity.Matches() was only used in unit testing. Instead of defining it
    directly, always map to EndpointSelectors for matching purposes. This
    way we are testing what we are using.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- pkg/policy/api/entity_test.go
+++ pkg/policy/api/entity_test.go
@@ -25,46 +25,56 @@ import (
 	. "gopkg.in/check.v1"
 )
 
+// matches returns true if the entity matches the labels
+func (e Entity) matches(ctx labels.LabelArray) bool {
+	return EntitySlice{e}.matches(ctx)
+}
+
+// matches returns true if any of the entities in the slice match the labels
+func (s EntitySlice) matches(ctx labels.LabelArray) bool {
+	return s.GetAsEndpointSelectors().Matches(ctx)
+}
+
 func (s *PolicyAPITestSuite) TestEntityMatches(c *C) {
 	InitEntities("cluster1")
 
-	c.Assert(EntityHost.Matches(labels.ParseLabelArray("reserved:host")), Equals, true)
-	c.Assert(EntityHost.Matches(labels.ParseLabelArray("reserved:host", "id:foo")), Equals, true)
-	c.Assert(EntityHost.Matches(labels.ParseLabelArray("reserved:world")), Equals, false)
-	c.Assert(EntityHost.Matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, false)
-	c.Assert(EntityHost.Matches(labels.ParseLabelArray("reserved:none")), Equals, false)
-	c.Assert(EntityHost.Matches(labels.ParseLabelArray("id=foo")), Equals, false)
+	c.Assert(EntityHost.matches(labels.ParseLabelArray("reserved:host")), Equals, true)
+	c.Assert(EntityHost.matches(labels.ParseLabelArray("reserved:host", "id:foo")), Equals, true)
+	c.Assert(EntityHost.matches(labels.ParseLabelArray("reserved:world")), Equals, false)
+	c.Assert(EntityHost.matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, false)
+	c.Assert(EntityHost.matches(labels.ParseLabelArray("reserved:none")), Equals, false)
+	c.Assert(EntityHost.matches(labels.ParseLabelArray("id=foo")), Equals, false)
 
-	c.Assert(EntityAll.Matches(labels.ParseLabelArray("reserved:host")), Equals, true)
-	c.Assert(EntityAll.Matches(labels.ParseLabelArray("reserved:world")), Equals, true)
-	c.Assert(EntityAll.Matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, true)
-	c.Assert(EntityAll.Matches(labels.ParseLabelArray("reserved:none")), Equals, true) // in a white-list model, All trumps None
-	c.Assert(EntityAll.Matches(labels.ParseLabelArray("id=foo")), Equals, true)
+	c.Assert(EntityAll.matches(labels.ParseLabelArray("reserved:host")), Equals, true)
+	c.Assert(EntityAll.matches(labels.ParseLabelArray("reserved:world")), Equals, true)
+	c.Assert(EntityAll.matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, true)
+	c.Assert(EntityAll.matches(labels.ParseLabelArray("reserved:none")), Equals, true) // in a white-list model, All trumps None
+	c.Assert(EntityAll.matches(labels.ParseLabelArray("id=foo")), Equals, true)
 
-	c.Assert(EntityCluster.Matches(labels.ParseLabelArray("reserved:host")), Equals, true)
-	c.Assert(EntityCluster.Matches(labels.ParseLabelArray("reserved:init")), Equals, true)
-	c.Assert(EntityCluster.Matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, true)
-	c.Assert(EntityCluster.Matches(labels.ParseLabelArray("reserved:world")), Equals, false)
-	c.Assert(EntityCluster.Matches(labels.ParseLabelArray("reserved:none")), Equals, false)
+	c.Assert(EntityCluster.matches(labels.ParseLabelArray("reserved:host")), Equals, true)
+	c.Assert(EntityCluster.matches(labels.ParseLabelArray("reserved:init")), Equals, true)
+	c.Assert(EntityCluster.matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, true)
+	c.Assert(EntityCluster.matches(labels.ParseLabelArray("reserved:world")), Equals, false)
+	c.Assert(EntityCluster.matches(labels.ParseLabelArray("reserved:none")), Equals, false)
 
 	clusterLabel := fmt.Sprintf("k8s:%s=%s", k8sapi.PolicyLabelCluster, "cluster1")
-	c.Assert(EntityCluster.Matches(labels.ParseLabelArray(clusterLabel, "id=foo")), Equals, true)
-	c.Assert(EntityCluster.Matches(labels.ParseLabelArray(clusterLabel, "id=foo", "id=bar")), Equals, true)
-	c.Assert(EntityCluster.Matches(labels.ParseLabelArray("id=foo")), Equals, false)
+	c.Assert(EntityCluster.matches(labels.ParseLabelArray(clusterLabel, "id=foo")), Equals, true)
+	c.Assert(EntityCluster.matches(labels.ParseLabelArray(clusterLabel, "id=foo", "id=bar")), Equals, true)
+	c.Assert(EntityCluster.matches(labels.ParseLabelArray("id=foo")), Equals, false)
 
-	c.Assert(EntityWorld.Matches(labels.ParseLabelArray("reserved:host")), Equals, false)
-	c.Assert(EntityWorld.Matches(labels.ParseLabelArray("reserved:world")), Equals, true)
-	c.Assert(EntityWorld.Matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, false)
-	c.Assert(EntityWorld.Matches(labels.ParseLabelArray("reserved:none")), Equals, false)
-	c.Assert(EntityWorld.Matches(labels.ParseLabelArray("id=foo")), Equals, false)
-	c.Assert(EntityWorld.Matches(labels.ParseLabelArray("id=foo", "id=bar")), Equals, false)
+	c.Assert(EntityWorld.matches(labels.ParseLabelArray("reserved:host")), Equals, false)
+	c.Assert(EntityWorld.matches(labels.ParseLabelArray("reserved:world")), Equals, true)
+	c.Assert(EntityWorld.matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, false)
+	c.Assert(EntityWorld.matches(labels.ParseLabelArray("reserved:none")), Equals, false)
+	c.Assert(EntityWorld.matches(labels.ParseLabelArray("id=foo")), Equals, false)
+	c.Assert(EntityWorld.matches(labels.ParseLabelArray("id=foo", "id=bar")), Equals, false)
 
-	c.Assert(EntityNone.Matches(labels.ParseLabelArray("reserved:host")), Equals, false)
-	c.Assert(EntityNone.Matches(labels.ParseLabelArray("reserved:world")), Equals, false)
-	c.Assert(EntityNone.Matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, false)
-	c.Assert(EntityNone.Matches(labels.ParseLabelArray("reserved:init")), Equals, false)
-	c.Assert(EntityNone.Matches(labels.ParseLabelArray("id=foo")), Equals, false)
-	c.Assert(EntityNone.Matches(labels.ParseLabelArray(clusterLabel, "id=foo", "id=bar")), Equals, false)
+	c.Assert(EntityNone.matches(labels.ParseLabelArray("reserved:host")), Equals, false)
+	c.Assert(EntityNone.matches(labels.ParseLabelArray("reserved:world")), Equals, false)
+	c.Assert(EntityNone.matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, false)
+	c.Assert(EntityNone.matches(labels.ParseLabelArray("reserved:init")), Equals, false)
+	c.Assert(EntityNone.matches(labels.ParseLabelArray("id=foo")), Equals, false)
+	c.Assert(EntityNone.matches(labels.ParseLabelArray(clusterLabel, "id=foo", "id=bar")), Equals, false)
 
 }
 
@@ -72,17 +82,9 @@ func (s *PolicyAPITestSuite) TestEntitySliceMatches(c *C) {
 	InitEntities("cluster1")
 
 	slice := EntitySlice{EntityHost, EntityWorld}
-	c.Assert(slice.Matches(labels.ParseLabelArray("reserved:host")), Equals, true)
-	c.Assert(slice.Matches(labels.ParseLabelArray("reserved:world")), Equals, true)
-	c.Assert(slice.Matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, false)
-	c.Assert(slice.Matches(labels.ParseLabelArray("reserved:none")), Equals, false)
-	c.Assert(slice.Matches(labels.ParseLabelArray("id=foo")), Equals, false)
-
-	// result must be identical if matched via endpoint selector
-	selector := slice.GetAsEndpointSelectors()
-	c.Assert(selector.Matches(labels.ParseLabelArray("reserved:host")), Equals, true)
-	c.Assert(selector.Matches(labels.ParseLabelArray("reserved:world")), Equals, true)
-	c.Assert(selector.Matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, false)
-	c.Assert(selector.Matches(labels.ParseLabelArray("reserved:none")), Equals, false)
-	c.Assert(selector.Matches(labels.ParseLabelArray("id=foo")), Equals, false)
+	c.Assert(slice.matches(labels.ParseLabelArray("reserved:host")), Equals, true)
+	c.Assert(slice.matches(labels.ParseLabelArray("reserved:world")), Equals, true)
+	c.Assert(slice.matches(labels.ParseLabelArray("reserved:unmanaged")), Equals, false)
+	c.Assert(slice.matches(labels.ParseLabelArray("reserved:none")), Equals, false)
+	c.Assert(slice.matches(labels.ParseLabelArray("id=foo")), Equals, false)
 }
