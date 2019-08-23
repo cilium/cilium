202957ec83d33491a45df99386ebc72858368192
Joe Stringer <joe@cilium.io>
2019-08-23 14:13:47 -0700

    packet: Format script with functions
    This makes the subsections clearer and easier to reason about.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- test/packet/scripts/install.sh
+++ test/packet/scripts/install.sh
@@ -10,68 +10,86 @@ VAGRANT_VERSION="2.2.4"
 PACKER_VERSION="1.3.5"
 VIRTUALBOX_VERSION="6.0"
 
-#repositories
+install_vagrant() {
+    echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" > /etc/apt/sources.list.d/virtualbox.list
 
-echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" > /etc/apt/sources.list.d/virtualbox.list
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88
+    wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc -O- | sudo apt-key add -
+    wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+    sudo --preserve-env=DEBIAN_FRONTEND apt-get update
+    sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y \
+        curl jq apt-transport-https htop bmon zip \
+        linux-tools-common linux-tools-generic \
+        ca-certificates software-properties-common \
+        git openjdk-8-jdk gcc make perl unzip awscli \
+        linux-headers-`uname -r` \
+        virtualbox-${VIRTUALBOX_VERSION} docker-ce
 
-sudo --preserve-env=DEBIAN_FRONTEND apt-get update
-sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y \
-    curl jq apt-transport-https htop bmon zip \
-    linux-tools-common linux-tools-generic \
-    ca-certificates software-properties-common \
-    git openjdk-8-jdk gcc make perl unzip awscli \
-    linux-headers-`uname -r` \
-    virtualbox-${VIRTUALBOX_VERSION} docker-ce
+    cd /tmp/
+    wget https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.deb
+    dpkg -i vagrant_*.deb
+}
 
-cd /tmp/
-wget https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.deb
-dpkg -i vagrant_*.deb
+preload_vagrant() {
+    # this block will attempt to preload required vagrant boxes from the vagrant cache server
+    # (it's configuration is in vagrant-cache directory in root of this repo).
+    # vagrant cache server is a separate packet box which vagrant-cache.ci.cilium.io points to
+    cp /provision/add_vagrant_box /usr/local/bin/
+    chmod 755 /usr/local/bin/add_vagrant_box
 
-# this block will attempt to preload required vagrant boxes from the vagrant cache server
-# (it's configuration is in vagrant-cache directory in root of this repo).
-# vagrant cache server is a separate packet box which vagrant-cache.ci.cilium.io points to
-cp /provision/add_vagrant_box /usr/local/bin/
-chmod 755 /usr/local/bin/add_vagrant_box
+    curl -s https://raw.githubusercontent.com/cilium/cilium/master/vagrant_box_defaults.rb > defaults.rb
+    /usr/local/bin/add_vagrant_box defaults.rb
+}
 
-curl -s https://raw.githubusercontent.com/cilium/cilium/master/vagrant_box_defaults.rb > defaults.rb
-/usr/local/bin/add_vagrant_box defaults.rb
+install_packer() {
+    wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+    unzip packer_${PACKER_VERSION}_linux_amd64.zip
+    mv packer /usr/local/bin/
+}
 
-wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
-unzip packer_${PACKER_VERSION}_linux_amd64.zip
-mv packer /usr/local/bin/
+configure_kernel() {
+    export CPU=$(($(nproc)-1))
+    for i in $(seq 0 $CPU);
+    do
+       echo performance > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+    done
+}
 
-# Kernel parameters
-export CPU=$(($(nproc)-1))
-for i in $(seq 0 $CPU);
-do
-   echo performance > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
-done
+install_golang() {
+    cd /tmp/
+    sudo curl -Sslk -o go.tar.gz "https://storage.googleapis.com/golang/go${GOLANG_VERSION}.linux-amd64.tar.gz"
+    sudo tar -C /usr/local -xzf go.tar.gz
+    sudo rm go.tar.gz
+    sudo ln -s /usr/local/go/bin/* /usr/local/bin/
+    go version
+    sudo mkdir /go/
+    export GOPATH=/go/
+    go get -u github.com/cilium/go-bindata/...
+    go get -u github.com/google/gops
+    go get -u github.com/onsi/ginkgo/ginkgo
+    go get -u github.com/onsi/gomega/...
+    sudo ln -sf /go/bin/* /usr/local/bin/
+}
 
-#Install Golang
-cd /tmp/
-sudo curl -Sslk -o go.tar.gz "https://storage.googleapis.com/golang/go${GOLANG_VERSION}.linux-amd64.tar.gz"
-sudo tar -C /usr/local -xzf go.tar.gz
-sudo rm go.tar.gz
-sudo ln -s /usr/local/go/bin/* /usr/local/bin/
-go version
-sudo mkdir /go/
-export GOPATH=/go/
-go get -u github.com/cilium/go-bindata/...
-go get -u github.com/google/gops
-go get -u github.com/onsi/ginkgo/ginkgo
-go get -u github.com/onsi/gomega/...
-sudo ln -sf /go/bin/* /usr/local/bin/
+install_docker() {
+    sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+}
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+main() {
+    install_vagrant
+    preload_vagrant
+    install_packer
+    configure_kernel
+    install_golang
+    install_docker
+    echo 'cd /root/go/src/github.com/cilium/cilium' >> /root/.bashrc
+}
 
-echo 'cd /root/go/src/github.com/cilium/cilium' >> /root/.bashrc
+main "$@"
