213a463fb5ac17ab63eea2df0cd213d82f9135e4
John Fastabend <john.fastabend@gmail.com>
2019-08-23 17:33:56 -0700

    cilium: encryption, if encryptNode is disable release routes
    If encryptNode is disabled but encryption in general is still enabled
    we currently do not remote the routes so all host networking traffic
    will continue to be routed via cilium_host which is unexpected and
    depending on other routing rules may create a routing loop.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -958,6 +958,10 @@ func NewDaemon(dp datapath.Datapath, iptablesManager rulesManager) (*Daemon, *en
 }
 
 func setupIPSec() (int, error) {
+	if option.Config.EncryptNode == false {
+		ipsec.DeleteIPsecEncryptRoute()
+	}
+
 	if !option.Config.EnableIPSec {
 		return 0, nil
 	}
@@ -967,10 +971,6 @@ func setupIPSec() (int, error) {
 		return 0, err
 	}
 	node.SetIPsecKeyIdentity(spi)
-	if option.Config.EncryptNode == false {
-		ipsec.DeleteIPsecEncryptRoute()
-	}
-
 	return authKeySize, nil
 }
 
