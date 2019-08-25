cilium/cilium

commit 294cf66a9c9c8e9564f63c348f090d0a3928a8a1

daemon: Enable IP forwarding on start
Cilium does not work properly when sysctl parameters about IP forwarding
are not enables. This change ensures that they all are enabled on the
daemon start.

Fixes: #8476
Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

daemon/sysctl_linux.go
@@ -1,0 +1,32 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/cilium/cilium/pkg/sysctl"
+)
+
+func enableIPForwarding() error {
+	if err := sysctl.Enable("net.ipv4.ip_forward"); err != nil {
+		return err
+	}
+	if err := sysctl.Enable("net.ipv4.conf.all.forwarding"); err != nil {
+		return err
+	}
+	if err := sysctl.Enable("net.ipv6.conf.all.forwarding"); err != nil {
+		return err
+	}
+	return nil
+}

daemon/daemon_main.go
@@ -1368,6 +1368,10 @@ func runDaemon() {
 
 	option.Config.RunMonitorAgent = true
 
+	if err := enableIPForwarding(); err != nil {
+		log.WithError(err).Fatal("Error when enabling sysctl parameters")
+	}
+
 	iptablesManager := &iptables.IptablesManager{}
 	iptablesManager.Init()
 

daemon/sysctl_darwin.go
@@ -1,0 +1,21 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+// enableIPForwarding on OS X and Darwin is not doing anything. It just exists
+// to make compilation possible.
+func enableIPForwarding() error {
+	return nil
+}

daemon/sysctl_linux_test.go
@@ -1,0 +1,37 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build linux,privileged_tests
+
+package main
+
+import (
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type DaemonPrivilegedSuite struct{}
+
+var _ = Suite(&DaemonPrivilegedSuite{})
+
+func (s *DaemonPrivilegedSuite) TestInitSysctlParams(c *C) {
+	err := initSysctlParams()
+	c.Assert(err, IsNil)
+}

commit 72757d5d6c818ab2c9ef70a848c025b788a7379f

cilium: only enable IPv6 forwarding if IPv6 is enabled
When running with a kernel that has IPv6 compiled out there is no
/proc/net/ipv6/ directory so trying to enable IPv6 fails and the
daemon can not start.

To resolve this only enable IPv6 forwarding when daemon is running with
IPv6 enabled.

Error seen with IPv6 compiled out and IPv6 disabled in config map is

level=fatal msg="Error when enabling sysctl parameters" error="could not open the sysctl file /proc/sys/net/ipv6/conf/all/forwarding: open /proc/sys/net/ipv6/conf/all/forwarding: no such file or directory" subsys=daemon

Fixes: 294cf66a9c9c8 ("daemon: Enable IP forwarding on start")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

daemon/sysctl_linux.go
@@ -15,6 +15,7 @@
 package main
 
 import (
+	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/sysctl"
 )
 
@@ -25,8 +26,10 @@ func enableIPForwarding() error {
 	if err := sysctl.Enable("net.ipv4.conf.all.forwarding"); err != nil {
 		return err
 	}
-	if err := sysctl.Enable("net.ipv6.conf.all.forwarding"); err != nil {
-		return err
+	if option.Config.EnableIPv6 {
+		if err := sysctl.Enable("net.ipv6.conf.all.forwarding"); err != nil {
+			return err
+		}
 	}
 	return nil
 }