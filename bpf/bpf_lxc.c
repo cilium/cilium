cilium/cilium

commit 661062fec8a1cc20d464b56ab8d7af3b135f858f

bpf: Remove cluster identity
This commit removes all logic that involves associating the cluster range with
the cluster identity. The cluster range cannot be detected automatically in a
reliable manner which led to undefined behavior with several side effects:

 * If the cluster range was assumed to be too broad, IPs outside of the cluster
   were mistakenly associated with the identity reserved:cluster.
 * The cluster identity was not a superset of all pods and worker node IPs which
   lead to the inability to accurately define the behavior allowing communication
   to or from the cluster identity.
 * CIDR rules apply to IP addresses that were not associated with the cluster
   identity. If the detection of the cluster range was not accurate, CIDR
   rules would in turn be incorrect as well.

In order to resolve the above side effects, the following changes are
performed:

1. Egress traffic that cannot be associated with a particular identity is no
   longer associated with the identity cluster even if the IP is within the
   assumed cluster range. Any IP address that is not associated with an
   endpoint or a host via the ipcache is now classified as world.

2. The detection of mapped IPv6 addresses no longer depends on the cluster
   identity. This was merely an optimization and is unnecessary.

3. For packets received via cilium_host, the node prefix based lookup to find
   the encapsulation target is now performed for all destinations. Regardless
   of whether the destination IP is within the cluster prefix or not. This was
   already the slow path as the majority of traffic via the cilium_host
   interface is targeted to local endpoints. Even for non-local traffic, it
   is an exception if the destination IP cannot be found in the ipcache to
   derive the encapsulation target. The small number of additional lookups is
   an extreme corner case.

4. All pod IP to hostIP mappings as learned via the Kubernetes pod

bpf/bpf_lxc.c
@@ -117,7 +117,6 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 				   __u32 *dstID)
 {
 	union macaddr router_mac = NODE_MAC;
-	union v6addr router_ip = {};
 	int ret, verdict, l4_off, forwarding_reason, hdrlen;
 	struct csum_offset csum_off = {};
 	struct endpoint_info *ep;
@@ -198,11 +197,8 @@ skip_service_lookup:
 
 	if (!revalidate_data(skb, &data, &data_end, &ip6))
 		return DROP_INVALID;
-	daddr = (union v6addr *)&ip6->daddr;
 
 	/* Determine the destination category for policy fallback. */
-	BPF_V6(router_ip, ROUTER_IP);
-
 	if (1) {
 		struct remote_endpoint_info *info;
 
@@ -210,8 +206,6 @@ skip_service_lookup:
 		if (info != NULL && info->sec_label) {
 			*dstID = info->sec_label;
 			tunnel_endpoint = info->tunnel_endpoint;
-		} else if (ipv6_match_prefix_64(daddr, &router_ip)){
-			*dstID = CLUSTER_ID;
 		} else {
 			*dstID = WORLD_ID;
 		}
@@ -351,11 +345,9 @@ skip_service_lookup:
 #endif
 
 #ifdef LXC_NAT46
-	if (*dstID != CLUSTER_ID) {
-		if (unlikely(ipv6_addr_is_mapped(daddr))) {
-			ep_tail_call(skb, CILIUM_CALL_NAT64);
-			return DROP_MISSED_TAIL_CALL;
-                }
+	if (unlikely(ipv6_addr_is_mapped(daddr))) {
+		ep_tail_call(skb, CILIUM_CALL_NAT64);
+		return DROP_MISSED_TAIL_CALL;
 	}
 #endif
 	goto pass_to_stack;
@@ -521,8 +513,6 @@ skip_service_lookup:
 		if (info != NULL && info->sec_label) {
 			*dstID = info->sec_label;
 			tunnel_endpoint = info->tunnel_endpoint;
-		} else if ((orig_dip & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE) {
-			*dstID = CLUSTER_ID;
 		} else {
 			*dstID = WORLD_ID;
 		}

bpf/bpf_netdev.c
@@ -205,7 +205,7 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 		info = ipcache_lookup6(&cilium_ipcache, src, V6_CACHE_KEY_LEN);
 		if (info != NULL) {
 			__u32 sec_label = info->sec_label;
-			if (sec_label && sec_label != CLUSTER_ID)
+			if (sec_label)
 				src_identity = info->sec_label;
 		}
 		cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
@@ -280,12 +280,7 @@ static inline __u32 derive_ipv4_sec_ctx(struct __sk_buff *skb, struct iphdr *ip4
 #ifdef FIXED_SRC_SECCTX
 	return FIXED_SRC_SECCTX;
 #else
-	__u32 secctx = WORLD_ID;
-
-	if ((ip4->saddr & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE) {
-		/* FIXME: Derive */
-	}
-	return secctx;
+	return WORLD_ID;
 #endif
 }
 
@@ -384,12 +379,8 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 				 * (passed into this function) reports the src
 				 * as the host. So we can ignore the ipcache
 				 * if it reports the source as HOST_ID.
-				 *
-				 * For compatibility with older versions of
-				 * Cilium, we also ignore CLUSTER_ID.
 				 */
-				if (sec_label != CLUSTER_ID &&
-				    sec_label != HOST_ID)
+				if (sec_label != HOST_ID)
 					src_identity = sec_label;
 			}
 		}
@@ -434,7 +425,7 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 	if (info != NULL && info->tunnel_endpoint != 0) {
 		return encap_and_redirect_with_nodeid(skb, info->tunnel_endpoint,
 						      secctx, true);
-	} else if ((ip4->daddr & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE) {
+	} else {
 		/* IPv4 lookup key: daddr & IPV4_MASK */
 		struct endpoint_key key = {};
 		int ret;

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=c9e6468686e58b0f758e69c8a6914ae90c530708
+GO_BINDATA_SHA1SUM=0ed624185eb258011966eec7fd6f9922bcee3b8e
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

bpf/lib/policy.h
@@ -30,7 +30,6 @@
  * - IdentityUnknown		(0)
  * - ReservedIdentityHost	(1)
  * - ReservedIdentityWorld	(2)
- * - ReservedIdentityCluster	(3)
  *
  * The following identities are given to endpoints so return false for these:
  * - ReservedIdentityHealth	(4)

bpf/node_config.h
@@ -33,14 +33,11 @@
 #define HOST_IP 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xa, 0x0, 0x2, 0xf, 0xff, 0xff
 #define HOST_ID 1
 #define WORLD_ID 2
-#define CLUSTER_ID 3
 #define HEALTH_ID 4
 #define INIT_ID 5
 #define HOST_IFINDEX_MAC { .addr = { 0xce, 0x72, 0xa7, 0x03, 0x88, 0x56 } }
 #define NAT46_PREFIX { .addr = { 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xa, 0x0, 0x0, 0x0, 0x0, 0x0 } }
 #define IPV4_MASK 0xffff
-#define IPV4_CLUSTER_MASK 0xff0000
-#define IPV4_CLUSTER_RANGE 0x100000
 #define IPV4_GATEWAY 0xfffff50a
 #define IPV4_LOOPBACK 0x1ffff50a
 #define ENCAP_GENEVE 1

daemon/daemon.go
@@ -926,10 +926,6 @@ func createNodeConfigHeaderfile() error {
 	ipv4Range := node.GetIPv4AllocRange()
 	fmt.Fprintf(fw, "#define IPV4_MASK %#x\n", byteorder.HostSliceToNetwork(ipv4Range.Mask, reflect.Uint32).(uint32))
 
-	ipv4ClusterRange := node.GetIPv4ClusterRange()
-	fmt.Fprintf(fw, "#define IPV4_CLUSTER_RANGE %#x\n", byteorder.HostSliceToNetwork(ipv4ClusterRange.IP, reflect.Uint32).(uint32))
-	fmt.Fprintf(fw, "#define IPV4_CLUSTER_MASK %#x\n", byteorder.HostSliceToNetwork(ipv4ClusterRange.Mask, reflect.Uint32).(uint32))
-
 	if nat46Range := option.Config.NAT46Prefix; nat46Range != nil {
 		fw.WriteString(common.FmtDefineAddress("NAT46_PREFIX", nat46Range.IP))
 	}
@@ -937,7 +933,6 @@ func createNodeConfigHeaderfile() error {
 	fw.WriteString(common.FmtDefineComma("HOST_IP", hostIP))
 	fmt.Fprintf(fw, "#define HOST_ID %d\n", identity.GetReservedID(labels.IDNameHost))
 	fmt.Fprintf(fw, "#define WORLD_ID %d\n", identity.GetReservedID(labels.IDNameWorld))
-	fmt.Fprintf(fw, "#define CLUSTER_ID %d\n", identity.GetReservedID(labels.IDNameCluster))
 	fmt.Fprintf(fw, "#define HEALTH_ID %d\n", identity.GetReservedID(labels.IDNameHealth))
 	fmt.Fprintf(fw, "#define INIT_ID %d\n", identity.GetReservedID(labels.IDNameInit))
 	fmt.Fprintf(fw, "#define LB_RR_MAX_SEQ %d\n", lbmap.MaxSeq)
@@ -981,16 +976,6 @@ func (d *Daemon) syncLXCMap() error {
 		{
 			IP: node.GetIPv6Router(),
 			ID: identity.ReservedIdentityHost,
-		},
-		{
-			IP:   node.GetIPv6ClusterRange().IP,
-			Mask: node.GetIPv6ClusterRange().Mask,
-			ID:   identity.ReservedIdentityCluster,
-		},
-		{
-			IP:   node.GetIPv4ClusterRange().IP,
-			Mask: node.GetIPv4ClusterRange().Mask,
-			ID:   identity.ReservedIdentityCluster,
 		},
 		{
 			IP:   net.IPv4zero,
@@ -1063,13 +1048,11 @@ func createPrefixLengthCounter() *counter.PrefixLengthCounter {
 	// without parsing strings, etc.
 	defaultPrefixes := []*net.IPNet{
 		// IPv4
-		createIPNet(0, net.IPv4len*8),                     // world
-		createIPNet(v4ClusterCidrMaskSize, net.IPv4len*8), // cluster
-		createIPNet(net.IPv4len*8, net.IPv4len*8),         // hosts
+		createIPNet(0, net.IPv4len*8),             // world
+		createIPNet(net.IPv4len*8, net.IPv4len*8), // hosts
 
 		// IPv6
-		createIPNet(0, net.IPv6len*8), // world
-		createIPNet(defaults.DefaultIPv6ClusterPrefixLen, net.IPv6len*8),
+		createIPNet(0, net.IPv6len*8),             // world
 		createIPNet(net.IPv6len*8, net.IPv6len*8), // hosts
 	}
 	_, err := counter.Add(defaultPrefixes)
@@ -1261,7 +1244,6 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 	log.Infof("  Node-IPv6: %s", node.GetIPv6())
 	log.Infof("  External-Node IPv4: %s", node.GetExternalIPv4())
 	log.Infof("  Internal-Node IPv4: %s", node.GetInternalIPv4())
-	log.Infof("  Cluster IPv6 prefix: %s", node.GetIPv6ClusterRange())
 	log.Infof("  Cluster IPv4 prefix: %s", node.GetIPv4ClusterRange())
 	log.Infof("  IPv6 node prefix: %s", node.GetIPv6NodeRange())
 	log.Infof("  IPv6 allocation prefix: %s", node.GetIPv6AllocRange())

daemon/k8s_watcher.go
@@ -1992,7 +1992,7 @@ func (d *Daemon) updatePodHostIP(pod *v1.Pod) (bool, error) {
 	}
 
 	selfOwned := ipcache.IPIdentityCache.Upsert(pod.Status.PodIP, hostIP, ipcache.Identity{
-		ID:     identity.ReservedIdentityCluster,
+		ID:     identity.ReservedIdentityInit,
 		Source: ipcache.FromKubernetes,
 	})
 	if !selfOwned {

daemon/k8s_watcher_test.go
@@ -817,7 +817,7 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 				endpointmanager.Insert(endpointCreator(123, identity.NumericIdentity(1000)))
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
-					ID:     identity.ReservedIdentityCluster,
+					ID:     identity.ReservedIdentityInit,
 					Source: ipcache.FromKubernetes,
 				})
 				m := versioned.NewMap()
@@ -853,7 +853,7 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 				endpointmanager.Insert(ep)
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
-					ID:     identity.ReservedIdentityCluster,
+					ID:     identity.ReservedIdentityInit,
 					Source: ipcache.FromKubernetes,
 				})
 				m := versioned.NewMap()
@@ -909,7 +909,7 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 				endpointmanager.Insert(ep)
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
-					ID:     identity.ReservedIdentityCluster,
+					ID:     identity.ReservedIdentityInit,
 					Source: ipcache.FromKubernetes,
 				})
 				m := versioned.NewMap()
@@ -949,7 +949,7 @@ func (ds *DaemonSuite) Test_missingK8sPodV1(c *C) {
 				endpointmanager.Insert(ep)
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
-					ID:     identity.ReservedIdentityCluster,
+					ID:     identity.ReservedIdentityInit,
 					Source: ipcache.FromKubernetes,
 				})
 				m := versioned.NewMap()
@@ -1067,7 +1067,7 @@ func (ds *DaemonSuite) Test_missingK8sNodeV1(c *C) {
 				node.DeleteAllNodes()
 				cache := ipcache.NewIPCache()
 				cache.Upsert("172.20.0.1", net.ParseIP("172.20.0.2"), ipcache.Identity{
-					ID:     identity.ReservedIdentityCluster,
+					ID:     identity.ReservedIdentityInit,
 					Source: ipcache.FromKubernetes,
 				})
 				node.UpdateNode(&node.Node{
@@ -1108,7 +1108,7 @@ func (ds *DaemonSuite) Test_missingK8sNodeV1(c *C) {
 				node.DeleteAllNodes()
 				cache := ipcache.NewIPCache()
 				cache.Upsert("127.0.0.1", net.ParseIP("127.0.0.2"), ipcache.Identity{
-					ID:     identity.ReservedIdentityCluster,
+					ID:     identity.ReservedIdentityInit,
 					Source: ipcache.FromKubernetes,
 				})
 				m := versioned.NewMap()
@@ -1138,7 +1138,7 @@ func (ds *DaemonSuite) Test_missingK8sNodeV1(c *C) {
 				node.DeleteAllNodes()
 				cache := ipcache.NewIPCache()
 				cache.Upsert("172.20.9.1", net.ParseIP("172.20.1.1"), ipcache.Identity{
-					ID:     identity.ReservedIdentityCluster,
+					ID:     identity.ReservedIdentityInit,
 					Source: ipcache.FromKubernetes,
 				})
 				node.UpdateNode(&node.Node{

commit ef44ed7efc172a44965ed4acb3cdf9b76ef4625a

bpf: fix asymmetric routing and cilium_host connectivity in v6+tunnel
The current v6 + tunnel behavior is broken in multiple ways:

1) When one host tries to connect to a remote pod, then we find the
   destination address of the pod via ipcache lookup and given the
   info->tunnel_endpoint is set, we route the request via tunnel.

   BUT: with a cilium_host source IP which is a public address. This
   means that the reply of the pod is routed asymmetrically via public
   interface but not via tunnel!

   Some more context and background: for v6 (direct routing or tunnel
   mode), the cilium_host device has a globally scoped v6 address which
   matches the one of the public interface. This is due to the fact that
   we cannot add a private address with link scope like in v4 case where
   kernel can ensure that no traffic is leaked to the public:

   # ip a
   [...]
   2: eno1: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
    inet6 2a02:120b:2c12:c120:ec4:7aff:fa13:f90f/64 scope global dynamic mngtmpaddr noprefixroute
       valid_lft 262sec preferred_lft 22sec
   [...]
   12: cilium_host@cilium_net: <BROADCAST,MULTICAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP group default qlen 1000
    inet6 2a02:120b:2c12:c120:ec4:7aff:fa13:f90f/128 scope global
       valid_lft forever preferred_lft forever
   [...]

   # ip -6 r
   [...]
   2001:db1:3::d899 dev cilium_host metric 1024 pref medium
   2001:db1:3::/112 via 2001:db1:3::d899 dev cilium_host src 2a02:120b:2c12:c120:ec4:7aff:fa13:f90f metric 1024 mtu 1450 pref medium
   2001:db8:5::/112 via 2001:db1:3::d899 dev cilium_host src 2a02:120b:2c12:c120:ec4:7aff:fa13:f90f metric 1024 mtu 1450 pref medium
   [...]

   This means that cilium_host effectively has two v6 addresses: HOST_IP
   which above is 2a02:120b:2c12:c120:ec4:7aff:fa13:f90f and ROUTER_IP
   from our v6 allocation pool which is 2001:db1:3::d899.

2) When one host tries to connect to a cidr address that is routable
   via tunnel, but either not part of the ipcache or info->tunnel_endpoint
   is not set, then we have no connectivity to such address. The v6 behavior
   is slightly different compared to v4 here in the sense that we only do
   a tunnel map lookup in case /96 prefix matches for the address. Looking
   at the history, the cluster prefix match had been removed for v4 back
   in 661062fec8a1 ("bpf: Remove cluster identity") but w/o corresponding
   v6 removal. Meaning, we never attempt to push the packet through the
   tunnel, and therefore we have no connectivity.

In order to fix both issues, we 1) remove the ipv6_match_prefix_96() in
order to get the same tunnel lookup behavior as with v4 and therefore
allow reachablility for such addresses, and 2) we NAT cilium_host addresses
to the ROUTER_IP when pushed through the tunnel. The ROUTER_IP already comes
from a Cilium node's allocation pool, but adding it to the cilium_host
interface is not an option since it's going to be scoped globally and is
therefore public.

Given the node's ROUTER_IP is already a reserved address, we simply NAT
the cilium_host source IP whenever we redirect to the tunnel /and/ whenever
we receive traffic via tunnel to our own node's ROUTER_IP. For the latter
we then reverse NAT back into HOST_IP. This mechanism ensures that traffic
will stay within the tunnel. Any requests to a remote Cilium node's public
address won't be NATed.

Fixes: #8654
Fixes: 661062fec8a1 ("bpf: Remove cluster identity")

bpf/bpf_lxc.c
@@ -291,6 +291,8 @@ ct_recreate6:
 		 * (c) packet was redirected to tunnel device so return.
 		 */
 		ret = encap_and_redirect_lxc(skb, tunnel_endpoint, encrypt_key, &key, SECLABEL, monitor);
+		if (!revalidate_data(skb, &data, &data_end, &ip6))
+			return DROP_INVALID;
 		if (ret == IPSEC_ENDPOINT)
 			goto pass_to_stack;
 		else if (ret != DROP_NO_TUNNEL_ENDPOINT)
@@ -299,6 +301,7 @@ ct_recreate6:
 #endif
 
 #ifdef ENABLE_NAT46
+	daddr = (union v6addr *)&ip6->daddr;
 	if (unlikely(ipv6_addr_is_mapped(daddr))) {
 		ep_tail_call(skb, CILIUM_CALL_NAT64);
 		return DROP_MISSED_TAIL_CALL;
@@ -597,6 +600,8 @@ ct_recreate4:
 		key.family = ENDPOINT_KEY_IPV4;
 
 		ret = encap_and_redirect_lxc(skb, tunnel_endpoint, encrypt_key, &key, SECLABEL, monitor);
+		if (!revalidate_data(skb, &data, &data_end, &ip4))
+			return DROP_INVALID;
 		if (ret == DROP_NO_TUNNEL_ENDPOINT)
 			goto pass_to_stack;
 		/* If not redirected noteably due to IPSEC then pass up to stack

bpf/bpf_netdev.c
@@ -205,7 +205,7 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 			return TC_ACT_OK;
 		else
 			return ret;
-	} else if (likely(ipv6_match_prefix_96(dst, &node_ip))) {
+	} else {
 		struct endpoint_key key = {};
 		int ret;
 
@@ -218,6 +218,8 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 		key.family = ENDPOINT_KEY_IPV6;
 
 		ret = encap_and_redirect_netdev(skb, &key, secctx, TRACE_PAYLOAD_LEN);
+		if (!revalidate_data(skb, &data, &data_end, &ip6))
+			return DROP_INVALID;
 		if (ret == IPSEC_ENDPOINT)
 			return TC_ACT_OK;
 		else if (ret != DROP_NO_TUNNEL_ENDPOINT)
@@ -388,6 +390,8 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 
 		cilium_dbg(skb, DBG_NETDEV_ENCAP4, key.ip4, secctx);
 		ret = encap_and_redirect_netdev(skb, &key, secctx, TRACE_PAYLOAD_LEN);
+		if (!revalidate_data(skb, &data, &data_end, &ip4))
+			return DROP_INVALID;
 		if (ret == IPSEC_ENDPOINT)
 			return TC_ACT_OK;
 		else if (ret != DROP_NO_TUNNEL_ENDPOINT)

bpf/bpf_overlay.c
@@ -57,6 +57,13 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 *identity)
 			return ret;
 	}
 #endif
+#if ENABLE_ENCAP_HOST_REMAP
+	{
+		int ret = encap_remap_v6_host_address(skb, false);
+		if (unlikely(ret < 0))
+			return ret;
+	}
+#endif
 	if (!revalidate_data(skb, &data, &data_end, &ip6))
 		return DROP_INVALID;
 

bpf/lib/common.h
@@ -54,6 +54,13 @@
 #define CONDITIONAL_PREALLOC BPF_F_NO_PREALLOC
 #endif
 
+/* TODO: ipsec still needs separate fixing */
+#ifndef ENABLE_IPSEC
+# ifdef ENABLE_IPV6
+#  define ENABLE_ENCAP_HOST_REMAP 1
+# endif
+#endif
+
 #define __inline__ __attribute__((always_inline))
 #ifndef __always_inline
 #define __always_inline inline __inline__

bpf/lib/encap.h
@@ -64,6 +64,63 @@ encap_and_redirect_ipsec(struct __sk_buff *skb, __u32 tunnel_endpoint, __u8 key,
 }
 #endif
 
+#ifdef ENABLE_ENCAP_HOST_REMAP
+static inline int __inline__
+encap_remap_v6_host_address(struct __sk_buff *skb, const bool egress)
+{
+	struct csum_offset csum = {};
+	union v6addr host_ip;
+	void *data, *data_end;
+	struct ipv6hdr *ip6;
+	union v6addr *which;
+	__u32 off, noff;
+	__u8 nexthdr;
+	__u16 proto;
+	__be32 sum;
+	int ret;
+
+	validate_ethertype(skb, &proto);
+	if (proto != bpf_htons(ETH_P_IPV6))
+		return 0;
+	if (!revalidate_data(skb, &data, &data_end, &ip6))
+		return DROP_INVALID;
+	/* For requests routed via tunnel with external v6 node IP
+	 * we need to remap their source address to the router address
+	 * as otherwise replies are not routed via tunnel but public
+	 * address instead.
+	 */
+	if (egress) {
+		BPF_V6(host_ip, HOST_IP);
+		which = (union v6addr *)&ip6->saddr;
+	} else {
+		BPF_V6(host_ip, ROUTER_IP);
+		which = (union v6addr *)&ip6->daddr;
+	}
+	if (ipv6_addrcmp(which, &host_ip))
+		return 0;
+	nexthdr = ip6->nexthdr;
+	ret = ipv6_hdrlen(skb, ETH_HLEN, &nexthdr);
+	if (ret < 0)
+		return ret;
+	off = ((void *)ip6 - data) + ret;
+	if (egress) {
+		BPF_V6(host_ip, ROUTER_IP);
+		noff = ETH_HLEN + offsetof(struct ipv6hdr, saddr);
+	} else {
+		BPF_V6(host_ip, HOST_IP);
+		noff = ETH_HLEN + offsetof(struct ipv6hdr, daddr);
+	}
+	sum = csum_diff(which, 16, &host_ip, 16, 0);
+	csum_l4_offset_and_flags(nexthdr, &csum);
+	if (skb_store_bytes(skb, noff, &host_ip, 16, 0) < 0)
+		return DROP_WRITE_ERROR;
+	if (csum.offset &&
+	    csum_l4_replace(skb, off, &csum, 0, sum, BPF_F_PSEUDO_HDR) < 0)
+		return DROP_CSUM_L4;
+	return 0;
+}
+#endif /* ENABLE_ENCAP_HOST_REMAP */
+
 static inline int __inline__
 __encap_with_nodeid(struct __sk_buff *skb, __u32 tunnel_endpoint,
 		    __u32 seclabel, __u32 monitor)
@@ -81,7 +138,11 @@ __encap_with_nodeid(struct __sk_buff *skb, __u32 tunnel_endpoint,
 	ret = skb_set_tunnel_key(skb, &key, sizeof(key), BPF_F_ZERO_CSUM_TX);
 	if (unlikely(ret < 0))
 		return DROP_WRITE_ERROR;
-
+#ifdef ENABLE_ENCAP_HOST_REMAP
+	ret = encap_remap_v6_host_address(skb, true);
+	if (unlikely(ret < 0))
+		return ret;
+#endif
 	send_trace_notify(skb, TRACE_TO_OVERLAY, seclabel, 0, 0, ENCAP_IFINDEX,
 			  0, monitor);
 	return 0;

bpf/lib/ipv6.h
@@ -140,20 +140,6 @@ static inline void ipv6_addr_clear_suffix(union v6addr *addr, int prefix)
 	prefix -= 32;
 }
 
-static inline int ipv6_match_prefix_96(const union v6addr *addr, const union v6addr *prefix)
-{
-	int tmp;
-
-	tmp = addr->p1 - prefix->p1;
-	if (!tmp) {
-		tmp = addr->p2 - prefix->p2;
-		if (!tmp)
-			tmp = addr->p3 - prefix->p3;
-	}
-
-	return !tmp;
-}
-
 static inline int ipv6_match_prefix_64(const union v6addr *addr, const union v6addr *prefix)
 {
 	int tmp;

bpf/lib/nodeport.h
@@ -148,7 +148,7 @@ int tail_nodeport_nat_ipv6(struct __sk_buff *skb)
 			if (ret)
 				return ret;
 
-			BPF_V6(target.addr, HOST_IP);
+			BPF_V6(target.addr, ROUTER_IP);
 			ifindex = ENCAP_IFINDEX;
 
 			/* fib lookup not necessary when going over tunnel. */
@@ -835,7 +835,7 @@ static __always_inline int nodeport_nat_fwd(struct __sk_buff *skb,
 		union v6addr addr;
 #ifdef ENCAP_IFINDEX
 		if (encap)
-			BPF_V6(addr, HOST_IP);
+			BPF_V6(addr, ROUTER_IP);
 		else
 #endif
 			BPF_V6(addr, IPV6_NODEPORT);
@@ -873,7 +873,7 @@ static __always_inline int nodeport_nat_rev(struct __sk_buff *skb,
 		union v6addr addr;
 #ifdef ENCAP_IFINDEX
 		if (encap)
-			BPF_V6(addr, HOST_IP);
+			BPF_V6(addr, ROUTER_IP);
 		else
 #endif
 			BPF_V6(addr, IPV6_NODEPORT);

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=c9f12e1ce6f20b8b52ba2a0687937b2e3be6fc9c
+GO_BINDATA_SHA1SUM=8ab46a949f8cb1ba2529ea9981661f2bebfd4f60
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 