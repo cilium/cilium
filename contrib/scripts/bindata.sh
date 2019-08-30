be23304856156abe5d5a32a4550c9abe03a8b576
Michal Rostecki <mrostecki@opensuse.org>
2019-08-30 16:26:22 +0200

    scripts: Fix required Go version in bindata script
    Use the information from GO_VERSION file.
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- contrib/scripts/bindata.sh
+++ contrib/scripts/bindata.sh
@@ -3,13 +3,13 @@
 arg1=$1
 
 BINDATA_FILE=bindata.go
+REQUIRED_GO_VERSION=$(cat ../GO_VERSION)
 
 if [[ "$arg1" == "apply" ]]; then
   NEW_SHA1SUM=`sha1sum ${BINDATA_FILE} | awk '{ print $1}'`
   GO_VERSION_USED=`go version | awk '{ print $3 }'`
   BPF_FILES=`git ls-files ../bpf/ | grep -v .gitignore | tr "\n" ' '`
   sed -i "s/GO_BINDATA_SHA1SUM=.*/GO_BINDATA_SHA1SUM=${NEW_SHA1SUM}/g" bpf.sha
-  sed -i "s/GO_VERSION_USED=.*/GO_VERSION_USED=${GO_VERSION_USED}/g" bpf.sha
   sed -i "s#BPF_FILES=.*#BPF_FILES=${BPF_FILES}#g" bpf.sha
   exit 0
 fi
@@ -31,7 +31,7 @@ echo "                  ERROR: bindata.go is out of date."
 echo ""
 echo " This can happen for two reasons:"
 echo " 1. You are using a go-bindata binary compiled with a different version"
-echo "    of golang (not 1.9). If so, please up/downgrade."
+echo "    of golang (not ${REQUIRED_GO_VERSION}). If so, please up/downgrade."
 echo ""
 echo " 2. You have made changes to the bpf/ directory. Please run the"
 echo "    following command to update the SHA in daemon/bpf.sha:"
