24c3e75a1c2d812f7289d6f283851cd692d29cb8
Joe Stringer <joe@cilium.io>
2019-09-10 13:22:19 +0100

    bpf: Make monitor aggregation tcp flags configurable
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/datapath/linux/config/config.go
+++ pkg/datapath/linux/config/config.go
@@ -137,6 +137,7 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 	cDefinesMap["CT_SYN_TIMEOUT"] = fmt.Sprintf("%d", int64(option.Config.CTMapEntriesTimeoutSYN.Seconds()))
 	cDefinesMap["CT_CLOSE_TIMEOUT"] = fmt.Sprintf("%d", int64(option.Config.CTMapEntriesTimeoutFIN.Seconds()))
 	cDefinesMap["CT_REPORT_INTERVAL"] = fmt.Sprintf("%d", int64(option.Config.MonitorAggregationInterval.Seconds()))
+	cDefinesMap["CT_REPORT_FLAGS"] = fmt.Sprintf("%#04x", int64(option.Config.MonitorAggregationFlags))
 
 	if option.Config.DatapathMode == option.DatapathModeIpvlan {
 		cDefinesMap["ENABLE_SECCTX_FROM_IPCACHE"] = "1"
