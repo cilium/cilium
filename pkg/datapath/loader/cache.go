cilium/cilium

commit 561100e14170d50e943b7cf907b3b33ce6e4e3ab

loader: Cache datapath objects
* When attempting to CompileAndLoad progs for an endpoint, first
  hash the endpoint header config and see if there's a template.
* If there's no such template, generate a new template header and
  objectfile, then cache this.
* Otherwise if there's a template, use it!

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/datapath/loader/cache.go
@@ -15,10 +15,21 @@
 package loader
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 	"sync"
 
+	"github.com/cilium/cilium/common"
 	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/elf"
 	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/serializer"
+
+	"github.com/sirupsen/logrus"
 )
 
 var (
@@ -26,6 +37,22 @@ var (
 
 	// templateCache is the cache of pre-compiled datapaths.
 	templateCache *objectCache
+
+	ignoredELFPrefixes = []string{
+		"2/",             // Calls within the endpoint
+		"HOST_IP",        // Global
+		"ROUTER_IP",      // Global
+		"cilium_ct",      // All CT maps, including local
+		"cilium_events",  // Global
+		"cilium_ipcache", // Global
+		"cilium_lb",      // Global
+		"cilium_lxc",     // Global
+		"cilium_metrics", // Global
+		"cilium_proxy",   // Global
+		"cilium_tunnel",  // Global
+		"cilium_policy",  // Global
+		"from-container", // Prog name
+	}
 )
 
 // Init initializes the datapath cache with base program hashes derived from
@@ -33,6 +60,7 @@ var (
 func Init(dp datapath.Datapath, nodeCfg *datapath.LocalNodeConfiguration) {
 	once.Do(func() {
 		templateCache = NewObjectCache(dp, nodeCfg)
+		elf.IgnoreSymbolPrefixes(ignoredELFPrefixes)
 	})
 	templateCache.Update(nodeCfg)
 }
@@ -45,12 +73,21 @@ type objectCache struct {
 
 	workingDirectory string
 	baseHash         *datapathHash
+
+	// toPath maps a hash to the filesystem path of the corresponding object.
+	toPath map[string]string
+
+	// compileQueue maps a hash to a queue which ensures that only one
+	// attempt is made concurrently to compile the corresponding template.
+	compileQueue map[string]*serializer.FunctionQueue
 }
 
 func newObjectCache(dp datapath.Datapath, nodeCfg *datapath.LocalNodeConfiguration, workingDir string) *objectCache {
 	oc := &objectCache{
 		Datapath:         dp,
 		workingDirectory: workingDir,
+		toPath:           make(map[string]string),
+		compileQueue:     make(map[string]*serializer.FunctionQueue),
 	}
 	oc.Update(nodeCfg)
 	return oc
@@ -70,6 +107,113 @@ func (o *objectCache) Update(nodeCfg *datapath.LocalNodeConfiguration) {
 	o.Lock()
 	defer o.Unlock()
 	o.baseHash = newHash
+}
+
+// serialize finds the channel that serializes builds against the same hash.
+// Returns the channel and whether or not the caller needs to compile the
+// datapath for this hash.
+func (o *objectCache) serialize(hash string) (fq *serializer.FunctionQueue, found bool) {
+	o.Lock()
+	defer o.Unlock()
+
+	fq, compiled := o.compileQueue[hash]
+	if !compiled {
+		fq = serializer.NewFunctionQueue(1)
+		o.compileQueue[hash] = fq
+	}
+	return fq, compiled
+}
+
+func (o *objectCache) lookup(hash string) (string, bool) {
+	o.Lock()
+	defer o.Unlock()
+	path, exists := o.toPath[hash]
+	return path, exists
+}
+
+func (o *objectCache) insert(hash, objectPath string) {
+	o.Lock()
+	defer o.Unlock()
+	o.toPath[hash] = objectPath
+}
+
+// build attempts to compile and cache a datapath template object file
+// corresponding to the specified endpoint configuration.
+func (o *objectCache) build(ctx context.Context, cfg *templateCfg, hash string) error {
+	templatePath := filepath.Join(o.workingDirectory, defaults.TemplatesDir, hash)
+	headerPath := filepath.Join(templatePath, common.CHeaderFileName)
+	objectPath := filepath.Join(templatePath, endpointObj)
+
+	if err := os.MkdirAll(templatePath, defaults.StateDirRights); err != nil {
+		return err
+	}
+
+	f, err := os.Create(headerPath)
+	if err != nil {
+		return &os.PathError{
+			Op:   "failed to open template header for writing",
+			Path: headerPath,
+			Err:  err,
+		}
+	}
+
+	if err = o.Datapath.WriteEndpointConfig(f, cfg); err != nil {
+		return &os.PathError{
+			Op:   "failed to write template header",
+			Path: headerPath,
+			Err:  err,
+		}
+	}
+
+	err = compileTemplate(ctx, templatePath)
+	if err != nil {
+		return err
+	}
+
+	log.WithFields(logrus.Fields{
+		logfields.Path: objectPath,
+	}).Info("Compiled new BPF template")
+
+	o.insert(hash, objectPath)
+	return nil
+}
+
+// fetchOrCompile attempts to fetch the path to the datapath object
+// corresponding to the provided endpoint configuration, or if this
+// configuration is not yet compiled, compiles it. It will block if multiple
+// threads attempt to concurrently fetchOrCompile a template binary for the
+// same set of EndpointConfiguration.
+//
+// Returns the path to the compiled template datapath object and whether the
+// object was compiled, or an error.
+func (o *objectCache) fetchOrCompile(ctx context.Context, cfg datapath.EndpointConfiguration) (string, bool, error) {
+	hash, err := o.baseHash.sumEndpoint(o, cfg, false)
+	if err != nil {
+		return "", false, err
+	}
+
+	// Serializes attempts to compile this cfg.
+	fq, compiled := o.serialize(hash)
+	if !compiled {
+		fq.Enqueue(func() error {
+			defer fq.Stop()
+			templateCfg := wrap(cfg)
+			return o.build(ctx, templateCfg, hash)
+		}, serializer.NoRetry)
+	}
+
+	// Wait until the build completes.
+	if err = fq.Wait(ctx); err != nil {
+		return "", false, fmt.Errorf("context cancelled while waiting for template compilation: %s", err)
+	}
+
+	// Fetch the result of the compilation.
+	path, ok := o.lookup(hash)
+	if !ok {
+		return "", false, fmt.Errorf("peer compilation for this template failed")
+	}
+
+	return path, !compiled, nil
 }
 
 // EndpointHash hashes the specified endpoint configuration with the current

pkg/datapath/loader/cache_test.go
@@ -1,0 +1,152 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build privileged_tests
+
+package loader
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/cilium/cilium/pkg/datapath/linux"
+	"github.com/cilium/cilium/pkg/testutils"
+
+	. "gopkg.in/check.v1"
+)
+
+func (s *LoaderTestSuite) TestobjectCache(c *C) {
+	tmpDir, err := ioutil.TempDir("", "cilium_test")
+	c.Assert(err, IsNil)
+	defer os.RemoveAll(tmpDir)
+
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+
+	cache := newObjectCache(linux.NewDatapath(linux.DatapathConfiguration{}), nil, tmpDir)
+	realEP := testutils.NewTestEndpoint()
+
+	// First run should compile and generate the object.
+	_, isNew, err := cache.fetchOrCompile(ctx, &realEP)
+	c.Assert(err, IsNil)
+	c.Assert(isNew, Equals, true)
+
+	// Same EP should not be compiled twice.
+	_, isNew, err = cache.fetchOrCompile(ctx, &realEP)
+	c.Assert(err, IsNil)
+	c.Assert(isNew, Equals, false)
+
+	// Changing the ID should not generate a new object.
+	realEP.Id++
+	_, isNew, err = cache.fetchOrCompile(ctx, &realEP)
+	c.Assert(err, IsNil)
+	c.Assert(isNew, Equals, false)
+
+	// Changing a setting on the EP should generate a new object.
+	realEP.Opts.SetBool("foo", true)
+	_, isNew, err = cache.fetchOrCompile(ctx, &realEP)
+	c.Assert(err, IsNil)
+	c.Assert(isNew, Equals, true)
+}
+
+type buildResult struct {
+	goroutine int
+	path      string
+	compiled  bool
+	err       error
+}
+
+func receiveResult(c *C, results chan buildResult) (*buildResult, error) {
+	select {
+	case result := <-results:
+		if result.err != nil {
+			return nil, result.err
+		}
+		return &result, nil
+	case <-time.After(contextTimeout):
+		return nil, fmt.Errorf("Timed out waiting for goroutines to return")
+	}
+}
+
+func (s *LoaderTestSuite) TestobjectCacheParallel(c *C) {
+	tmpDir, err := ioutil.TempDir("", "cilium_test")
+	c.Assert(err, IsNil)
+	defer os.RemoveAll(tmpDir)
+
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+
+	tests := []struct {
+		description string
+		builds      int
+		divisor     int
+	}{
+		{
+			description: "One build, multiple blocking goroutines",
+			builds:      8,
+			divisor:     8,
+		},
+		{
+			description: "Eight builds, half compile, half block",
+			builds:      8,
+			divisor:     2,
+		},
+		{
+			description: "Eight unique builds",
+			builds:      8,
+			divisor:     1,
+		},
+	}
+
+	for _, t := range tests {
+		c.Logf("  %s", t.description)
+
+		results := make(chan buildResult, t.builds)
+		cache := newObjectCache(linux.NewDatapath(linux.DatapathConfiguration{}), nil, tmpDir)
+		for i := 0; i < t.builds; i++ {
+			go func(i int) {
+				ep := testutils.NewTestEndpoint()
+				opt := fmt.Sprintf("OPT%d", i/t.divisor)
+				ep.Opts.SetBool(opt, true)
+				path, isNew, err := cache.fetchOrCompile(ctx, &ep)
+				results <- buildResult{
+					goroutine: i,
+					path:      path,
+					compiled:  isNew,
+					err:       err,
+				}
+			}(i)
+		}
+
+		// First result will always be a compilation for the new set of options
+		compiled := make(map[int]string, t.builds)
+		for i := 0; i < t.builds; i++ {
+			result, err := receiveResult(c, results)
+			c.Assert(err, IsNil)
+
+			opt := result.goroutine / t.divisor
+			basePath, exists := compiled[opt]
+			if exists {
+				c.Assert(result.compiled, Equals, false)
+				c.Assert(result.path, Equals, basePath)
+			} else {
+				c.Assert(result.compiled, Equals, true)
+				compiled[opt] = result.path
+			}
+		}
+	}
+}

pkg/datapath/loader/compile.go
@@ -303,3 +303,14 @@ func Compile(ctx context.Context, src string, out string) error {
 	}
 	return compile(ctx, &prog, &dirs, debug)
 }
+
+// compileTemplate compiles a BPF program generating a template object file.
+func compileTemplate(ctx context.Context, out string) error {
+	dirs := directoryInfo{
+		Library: option.Config.BpfDir,
+		Runtime: option.Config.StateDir,
+		Output:  out,
+		State:   out,
+	}
+	return compile(ctx, datapathProg, &dirs, option.Config.BPFCompilationDebug)
+}

pkg/datapath/loader/loader.go
@@ -19,6 +19,7 @@ import (
 	"path"
 
 	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/elf"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
@@ -95,6 +96,40 @@ func CompileAndLoad(ctx context.Context, ep endpoint) error {
 		Output:  ep.StateDir(),
 	}
 	return compileAndLoad(ctx, ep, &dirs)
+}
+
+// CompileOrLoad loads the BPF datapath programs for the specified endpoint.
+//
+// In contrast with CompileAndLoad(), it attempts to find a pre-compiled
+// template datapath object to use, to avoid a costly compile operation.
+// Only if there is no existing template that has the same configuration
+// parameters as the specified endpoint, this function will compile a new
+// template for this configuration.
+//
+// This function will block if the cache does not contain an entry for the
+// same EndpointConfiguration and multiple goroutines attempt to concurrently
+// CompileOrLoad with the same configuration parameters. When the first
+// goroutine completes compilation of the template, all other CompileOrLoad
+// invocations will be released.
+func CompileOrLoad(ctx context.Context, ep endpoint) error {
+	templatePath, _, err := templateCache.fetchOrCompile(ctx, ep)
+	if err != nil {
+		return err
+	}
+
+	template, err := elf.Open(templatePath)
+	if err != nil {
+		return err
+	}
+	defer template.Close()
+
+	dstPath := path.Join(ep.StateDir(), endpointObj)
+	opts, strings := ELFSubstitutions(ep)
+	if err = template.Write(dstPath, opts, strings); err != nil {
+		return err
+	}
+
+	return ReloadDatapath(ctx, ep)
 }
 
 func ReloadDatapath(ctx context.Context, ep endpoint) error {

pkg/datapath/loader/loader_test.go
@@ -53,6 +53,22 @@ func Test(t *testing.T) {
 	TestingT(t)
 }
 
+func (s *LoaderTestSuite) SetUpSuite(c *C) {
+	SetTestIncludes([]string{
+		fmt.Sprintf("-I%s", bpfDir),
+		fmt.Sprintf("-I%s", filepath.Join(bpfDir, "include")),
+	})
+
+	sourceFile := filepath.Join(bpfDir, endpointProg)
+	err := os.Symlink(sourceFile, endpointProg)
+	c.Assert(err, IsNil)
+}
+
+func (s *LoaderTestSuite) TearDownSuite(c *C) {
+	SetTestIncludes(nil)
+	os.RemoveAll(endpointProg)
+}
+
 func (s *LoaderTestSuite) TearDownTest(c *C) {
 	// Old map names as created by older versions of these tests
 	//

pkg/datapath/maps/map.go
@@ -22,6 +22,7 @@ import (
 	"strings"
 
 	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/datapath/loader"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/logging"
@@ -129,7 +130,7 @@ func (ms *mapSweeper) walk(path string, _ os.FileInfo, _ error) error {
 		ctmap.MapNameTCP4,
 		ctmap.MapNameAny6,
 		ctmap.MapNameAny4,
-		endpoint.CallsMapName,
+		loader.CallsMapName,
 		bpfconfig.MapNamePrefix,
 		endpoint.IpvlanMapName,
 	}

pkg/defaults/defaults.go
@@ -37,6 +37,9 @@ const (
 	//StateDir is the default path for the state directory relative to RuntimePath
 	StateDir = "state"
 
+	// TemplatesDir is the default path for the compiled template objects relative to StateDir
+	TemplatesDir = "templates"
+
 	// BpfDir is the default path for template files relative to LibDir
 	BpfDir = "bpf"
 

pkg/endpoint/bpf.go
@@ -58,7 +58,7 @@ func (e *Endpoint) PolicyMapPathLocked() string {
 
 // CallsMapPathLocked returns the path to cilium tail calls map of an endpoint.
 func (e *Endpoint) CallsMapPathLocked() string {
-	return bpf.LocalMapPath(CallsMapName, e.ID)
+	return bpf.LocalMapPath(loader.CallsMapName, e.ID)
 }
 
 // BPFConfigMapPath returns the path to the BPF config map of endpoint.
@@ -486,11 +486,9 @@ func (e *Endpoint) realizeBPFState(regenContext *regenerationContext) (compilati
 		// Compile and install BPF programs for this endpoint
 		if datapathRegenCtxt.regenerationLevel == RegenerateWithDatapathRebuild {
 			stats.bpfCompilation.Start()
-			err = loader.CompileAndLoad(datapathRegenCtxt.completionCtx, datapathRegenCtxt.epInfoCache)
+			err = loader.CompileOrLoad(datapathRegenCtxt.completionCtx, datapathRegenCtxt.epInfoCache)
 			stats.bpfCompilation.End(err == nil)
-			e.getLogger().WithError(err).
-				WithField(logfields.BPFCompilationTime, stats.bpfCompilation.Total().String()).
-				Info("Recompiled endpoint BPF program")
+			e.getLogger().WithError(err).Info("Rewrote endpoint BPF program")
 			compilationExecuted = true
 		} else { // RegenerateWithDatapathLoad
 			err = loader.ReloadDatapath(datapathRegenCtxt.completionCtx, datapathRegenCtxt.epInfoCache)

commit 17f83561ad4e0559a58c5fe1ad86bcbedc5b0ff5

loader: remove hash from compileQueue if build fails
Otherwise, the loader erroneously thinks that the template has been built,
while it actually needs to be rebuilt again. Note that if the template cannot
be built, we do not have any logic to signal to other waiters on this queue that
the build failed and that said waiters should try to rebuild the program; all
waiters will fail as well. We also do not have retry logic if endpoints are not
regenerated, so all endpoints which were waiting on a given template to be
compiled will not be rebuilt. The retry logic is out of the scope of this
commit.

Fixes: 561100e141

Related-to: #8817

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/datapath/loader/cache.go
@@ -268,6 +268,9 @@ func (o *objectCache) fetchOrCompile(ctx context.Context, cfg datapath.EndpointC
 			err := o.build(ctx, templateCfg, hash)
 			if err != nil {
 				scopedLog.WithError(err).Error("BPF template object creation failed")
+				o.Lock()
+				delete(o.compileQueue, hash)
+				o.Unlock()
 			}
 			return err
 		}, serializer.NoRetry)