cilium/cilium

commit a3f76386742b8f5b619413d0c12243e42defcf58

deps: update etcd to v3.4.0
- Changed library path for the new etcd path
- Changed service translation logic since etcd v3.4 provides the
protocol scheme in the URL as well.

Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon_main.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
+	"net/url"
 	"os"
 	"path"
 	"path/filepath"
@@ -1111,16 +1112,21 @@ func (d *Daemon) initKVStore() {
 				// If the service is available, do the service translation to
 				// the service IP. Otherwise dial with the original service
 				// name `s`.
-				svc := k8s.ParseServiceIDFrom(s)
-				if svc != nil {
-					backendIP := d.k8sSvcCache.GetRandomBackendIP(*svc)
-					if backendIP != nil {
-						s = backendIP.String()
+				u, err := url.Parse(s)
+				if err == nil {
+					svc := k8s.ParseServiceIDFrom(u.Host)
+					if svc != nil {
+						backendIP := d.k8sSvcCache.GetRandomBackendIP(*svc)
+						if backendIP != nil {
+							s = backendIP.String()
+						}
+					} else {
+						log.Debug("Service not found")
 					}
+					log.Debugf("custom dialer based on k8s service backend is dialing to 2 %q", s)
 				} else {
-					log.Debug("Service not found")
+					log.Errorf("Unable to parse etcd service URL %s", err)
 				}
-				log.Debugf("custom dialer based on k8s service backend is dialing to %q", s)
 				return net.Dial("tcp", s)
 			},
 			),

commit 5b1fb062f59bdbed6d7dd15c34a91ba8c112d3e4

use common custom dialer to connect to etcd
Make use of custom dialer to avoid code duplication across the repo.

Fixes: a3f76386742b ("deps: update etcd to v3.4.0")
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon_main.go
@@ -19,7 +19,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
-	"net/url"
 	"os"
 	"path"
 	"path/filepath"
@@ -1126,28 +1125,7 @@ func (d *Daemon) initKVStore() {
 		d.waitForCacheSync(k8sAPIGroupServiceV1Core, k8sAPIGroupEndpointV1Core)
 		log := log.WithField(logfields.LogSubsys, "etcd")
 		goopts.DialOption = []grpc.DialOption{
-			grpc.WithDialer(func(s string, duration time.Duration) (conn net.Conn, e error) {
-				// If the service is available, do the service translation to
-				// the service IP. Otherwise dial with the original service
-				// name `s`.
-				u, err := url.Parse(s)
-				if err == nil {
-					svc := k8s.ParseServiceIDFrom(u.Host)
-					if svc != nil {
-						backendIP := d.k8sSvcCache.GetRandomBackendIP(*svc)
-						if backendIP != nil {
-							s = backendIP.String()
-						}
-					} else {
-						log.Debug("Service not found")
-					}
-					log.Debugf("custom dialer based on k8s service backend is dialing to 2 %q", s)
-				} else {
-					log.Errorf("Unable to parse etcd service URL %s", err)
-				}
-				return net.Dial("tcp", s)
-			},
-			),
+			grpc.WithDialer(k8s.CreateCustomDialer(&d.k8sSvcCache, log)),
 		}
 	}
 

operator/main.go
@@ -17,7 +17,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -32,11 +31,11 @@ import (
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/version"
-	"github.com/spf13/cobra/doc"
 
 	gops "github.com/google/gops/agent"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"k8s.io/klog"
@@ -279,23 +278,7 @@ func runOperator(cmd *cobra.Command) {
 				scopedLog.Info("Kubernetes services synced")
 				goopts = &kvstore.ExtraOptions{
 					DialOption: []grpc.DialOption{
-						grpc.WithDialer(func(s string, duration time.Duration) (conn net.Conn, e error) {
-							// If the service is available, do the service translation to
-							// the service IP. Otherwise dial with the original service
-							// name `s`.
-							svc := k8s.ParseServiceIDFrom(s)
-							if svc != nil {
-								backendIP := k8sSvcCache.GetRandomBackendIP(*svc)
-								if backendIP != nil {
-									s = backendIP.String()
-								}
-							} else {
-								log.Debug("Service not found")
-							}
-							log.Debugf("custom dialer based on k8s service backend is dialing to %q", s)
-							return net.Dial("tcp", s)
-						},
-						),
+						grpc.WithDialer(k8s.CreateCustomDialer(&k8sSvcCache, log)),
 					},
 				}
 			}