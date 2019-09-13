d2b9e932534163f66a1831aa60b0d2f24c2dd70c
Ian Vernon <ian@cilium.io>
2019-09-13 09:27:08 -0700

    endpoint: use endpoint ID for error message
    [ upstream commit 23a87cd1abfb10633a6dfbf4adb7615cd942b686 ]
    
    We usually refer to an Endpoint by its ID, not by its PolicyMap ID when an
    operation performed on it fails; change error message if policy calculation
    fails accordingly.
    
    Signed-off by: Ian Vernon <ian@cilium.io>
    
    Signed-off-by: Ray Bejjani <ray@isovalent.com>

--- pkg/endpoint/bpf.go
+++ pkg/endpoint/bpf.go
@@ -618,7 +618,7 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 		err = e.regeneratePolicy()
 		stats.policyCalculation.End(err == nil)
 		if err != nil {
-			return fmt.Errorf("unable to regenerate policy for '%s': %s", e.PolicyMap.String(), err)
+			return fmt.Errorf("unable to regenerate policy for '%s': %s", e.StringID(), err)
 		}
 
 		_ = e.updateAndOverrideEndpointOptions(nil)
