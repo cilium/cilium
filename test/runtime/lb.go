31a91f2cec0e600d737d01d6cffe817dd4c6bc2f
Martynas Pumputis <m@lambda.lt>
2019-09-16 14:36:17 +0200

    test: Remove bpf_lb.c based runtime test cases
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/runtime/lb.go
+++ test/runtime/lb.go
@@ -16,7 +16,6 @@ package RuntimeTest
 
 import (
 	"fmt"
-	"os"
 
 	. "github.com/cilium/cilium/test/ginkgo-ext"
 	"github.com/cilium/cilium/test/helpers"
@@ -56,10 +55,6 @@ var _ = Describe("RuntimeLB", func() {
 			"cilium bpf lb list",
 			"cilium policy get")
 	})
-
-	AfterEach(func() {
-		cleanupLBDevice(vm)
-	}, 500)
 
 	images := map[string]string{
 		helpers.Httpd1: constants.HttpdImage,
@@ -154,127 +149,6 @@ var _ = Describe("RuntimeLB", func() {
 		AfterAll(func() {
 			deleteContainers()
 		})
-
-		It("validates that services work for L3 (IP) loadbalancing", func() {
-			err := createLBDevice(vm)
-			if err != nil {
-				log.Errorf("error creating interface: %s", err)
-			}
-			Expect(err).Should(BeNil())
-
-			httpd1, err := vm.ContainerInspectNet(helpers.Httpd1)
-			Expect(err).Should(BeNil())
-			httpd2, err := vm.ContainerInspectNet(helpers.Httpd2)
-			Expect(err).Should(BeNil())
-
-			By("Creating services")
-
-			services := map[string][]string{
-				"2.2.2.2:0": {
-					fmt.Sprintf("%s:0", httpd1[helpers.IPv4]),
-					fmt.Sprintf("%s:0", httpd2[helpers.IPv4]),
-				},
-				"[f00d::1:1]:0": {
-					fmt.Sprintf("[%s]:0", httpd1[helpers.IPv6]),
-					fmt.Sprintf("[%s]:0", httpd2[helpers.IPv6]),
-				},
-				"3.3.3.3:0": {
-					fmt.Sprintf("%s:0", "10.0.2.15"),
-				},
-				"[f00d::1:2]:0": {
-					fmt.Sprintf("[%s]:0", "fd02:1:1:1:1:1:1:1"),
-				},
-			}
-			svc := 1
-			for fe, be := range services {
-				status := vm.ServiceAdd(svc, fe, be)
-				status.ExpectSuccess(fmt.Sprintf("failed to create service %s=>%v", fe, be))
-				svc++
-			}
-
-			By("Pinging host => bpf_lb => container")
-
-			status := vm.Exec(helpers.Ping("2.2.2.2"))
-			status.ExpectSuccess("failed to ping service IP from host")
-			// FIXME GH-2889: createLBDevice() doesn't configure host IPv6
-			//status = vm.Exec(helpers.Ping6("f00d::1:1"))
-			//status.ExpectSuccess("failed to ping service IP from host")
-
-			By("Pinging container => bpf_lb => container")
-
-			status = vm.ContainerExec(helpers.Client, helpers.Ping("2.2.2.2"))
-			status.ExpectSuccess("failed to ping service IP 2.2.2.2")
-			status = vm.ContainerExec(helpers.Client, helpers.Ping6("f00d::1:1"))
-			status.ExpectSuccess("failed to ping service IP f00d::1:1")
-
-			By("Pinging container => bpf_lb => host")
-
-			status = vm.ContainerExec(helpers.Client, helpers.Ping("3.3.3.3"))
-			status.ExpectSuccess("failed to ping service IP 3.3.3.3")
-			status = vm.ContainerExec(helpers.Client, helpers.Ping("f00d::1:2"))
-			status.ExpectSuccess("failed to ping service IP f00d::1:2")
-
-			By("Configuring services to point to own IP via service")
-
-			vm.ServiceDelAll().ExpectSuccess()
-			loopbackServices := map[string]string{
-				"2.2.2.2:0":     fmt.Sprintf("%s:0", httpd1[helpers.IPv4]),
-				"[f00d::1:1]:0": fmt.Sprintf("[%s]:0", httpd1[helpers.IPv6]),
-			}
-			svc = 1
-			for fe, be := range loopbackServices {
-				status := vm.ServiceAdd(svc, fe, []string{be})
-				status.ExpectSuccess(fmt.Sprintf("failed to create service %s=>%v", fe, be))
-				svc++
-			}
-
-			By("Pinging from server1 to its own service IP")
-
-			status = vm.ContainerExec(helpers.Httpd1, helpers.Ping("2.2.2.2"))
-			status.ExpectSuccess("failed to ping service IP 2.2.2.2")
-			status = vm.ContainerExec(helpers.Httpd1, helpers.Ping6("f00d::1:1"))
-			status.ExpectSuccess("failed to ping service IP f00d::1:1")
-		}, 500)
-
-		It("validates that services work for L4 (IP+Port) loadbalancing", func() {
-			err := createLBDevice(vm)
-			if err != nil {
-				log.Errorf("error creating interface: %s", err)
-			}
-			Expect(err).Should(BeNil())
-
-			httpd1, err := vm.ContainerInspectNet(helpers.Httpd1)
-			Expect(err).Should(BeNil())
-			httpd2, err := vm.ContainerInspectNet(helpers.Httpd2)
-			Expect(err).Should(BeNil())
-
-			By("Creating services")
-
-			services := map[string][]string{
-				"2.2.2.2:80": {
-					fmt.Sprintf("%s:80", httpd1[helpers.IPv4]),
-					fmt.Sprintf("%s:80", httpd2[helpers.IPv4]),
-				},
-				"[f00d::1:1]:80": {
-					fmt.Sprintf("[%s]:80", httpd1[helpers.IPv6]),
-					fmt.Sprintf("[%s]:80", httpd2[helpers.IPv6]),
-				},
-			}
-			svc := 1
-			for fe, be := range services {
-				status := vm.ServiceAdd(svc, fe, be)
-				status.ExpectSuccess("failed to create service %s=>%v", fe, be)
-				svc++
-			}
-
-			By("Making HTTP requests from container => bpf_lb => container")
-
-			for ip := range services {
-				url := fmt.Sprintf("http://%s/public", ip)
-				status := vm.ContainerExec(helpers.Client, helpers.CurlFail(url))
-				status.ExpectSuccess(fmt.Sprintf("failed to fetch via URL %s", url))
-			}
-		}, 500)
 
 		It("validates service recovery on restart", func() {
 			service := "2.2.2.2:80"
@@ -449,91 +323,3 @@ var _ = Describe("RuntimeLB", func() {
 		})
 	})
 })
-
-// createLBDevice instantiates a device with the bpf_lb program to handle
-// loadbalancing as though it were attached to a physical device on the system.
-// This is implemented through a veth pair with two ends, lbtest1 and lbtest2.
-// bpf_lb is attached to ingress at lbtest2, so when traffic is sent through
-// lbtest1 it is forwarded through the veth pair into lbtest2 where the BPF
-// program executes the services functionality.
-//
-// The following traffic is routed to lbtest1 (so it goes to the LB BPF prog):
-// * 3.3.3.3/32
-// * 2.2.2.2/32
-// * f00d:1:1/128
-// * fbfb::10:10/128
-//
-// Additionally, the following IPs are associated with the cilium_host device,
-// so they may be used as backends for services and they will receive a
-// response from the host:
-// * 10.0.2.15 (inherited from virtualbox VM configuration)
-// * fd02:1:1:1:1:1:1:1 (explicitly configured below)
-func createLBDevice(node *helpers.SSHMeta) error {
-	script := `#!/bin/bash
-function mac2array() {
-    echo "{0x${1//:/,0x}}"
-}
-
-ip link add lbtest1 type veth peer name lbtest2
-ip link set lbtest1 up
-
-# Route f00d::1:1 IPv6 packets to a fantasy router ("fbfb::10:10") behind lbtest1
-ip -6 route add fbfb::10:10/128 dev lbtest1
-MAC=$(ip link show lbtest1 | grep ether | awk '{print $2}')
-ip neigh add fbfb::10:10 lladdr $MAC dev lbtest1
-ip -6 route add f00d::1:1/128 via fbfb::10:10
-
-# Route 2.2.2.2 IPv4 packets to a fantasy router ("3.3.3.3") behind lbtest1
-ip route add 3.3.3.3/32 dev lbtest1
-MAC=$(ip link show lbtest1 | grep ether | awk '{print $2}')
-ip neigh add 3.3.3.3 lladdr $MAC dev lbtest1
-ip route add 2.2.2.2/32 via 3.3.3.3
-
-ip link set lbtest2 up
-
-LIB=/var/lib/cilium/bpf
-RUN=/var/run/cilium/state
-NH_IFINDEX=$(cat /sys/class/net/cilium_host/ifindex)
-NH_MAC=$(ip link show cilium_host | grep ether | awk '{print $2}')
-NH_MAC="{.addr=$(mac2array $NH_MAC)}"
-CLANG_OPTS="-D__NR_CPUS__=$(nproc) -DLB_L3 -DLB_REDIRECT=$NH_IFINDEX -DLB_DSTMAC=$NH_MAC -DCALLS_MAP=lbtest -O2 -target bpf -I. -I$LIB -I$LIB/include -I$RUN/globals -DDEBUG -Wno-address-of-packed-member -Wno-unknown-warning-option"
-touch netdev_config.h
-clang $CLANG_OPTS -c $LIB/bpf_lb.c -o tmp_lb.o
-
-tc qdisc del dev lbtest2 clsact 2> /dev/null || true
-tc qdisc add dev lbtest2 clsact
-tc filter add dev lbtest2 ingress bpf da obj tmp_lb.o sec from-netdev
-`
-	By("Creating LB device to handle service requests")
-	scriptName := "create_veth_interface"
-	log.Infof("generating veth script: %s", scriptName)
-	err := helpers.RenderTemplateToFile(scriptName, script, os.ModePerm)
-	if err != nil {
-		return err
-	}
-
-	// filesystem is mounted at path /vagrant on VM
-	scriptPath := node.GetFilePath(scriptName)
-
-	ipAddrCmd := "sudo ip addr add fd02:1:1:1:1:1:1:1 dev cilium_host"
-	res := node.Exec(ipAddrCmd)
-	log.Infof("output of %q: %s", ipAddrCmd, res.CombineOutput())
-
-	log.Infof("running script %s", scriptPath)
-	runScriptCmd := fmt.Sprintf("sudo %s", scriptPath)
-	res = node.Exec(runScriptCmd)
-	log.Infof("output of %q: %s", runScriptCmd, res.CombineOutput())
-	log.Infof("removing file %q", scriptName)
-	err = os.Remove(scriptName)
-	return err
-}
-
-func cleanupLBDevice(node *helpers.SSHMeta) {
-	ipAddrCmd := "sudo ip addr del fd02:1:1:1:1:1:1:1/128 dev cilium_host"
-	res := node.Exec(ipAddrCmd)
-	log.Infof("output of %q: %s", ipAddrCmd, res.CombineOutput())
-
-	ipLinkCmd := "sudo ip link del dev lbtest1"
-	res = node.Exec(ipLinkCmd)
-	log.Infof("output of %q: %s", ipLinkCmd, res.CombineOutput())
-}
