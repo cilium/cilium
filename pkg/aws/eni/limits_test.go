70c2f9f684ce0bbebf24c32ac04181deb411c80e
Jaff Cheng <jaff.cheng.sh@gmail.com>
2019-09-20 10:01:28 +0000

    eni: Allow overwriting AWS instance limit via agent configuration
    Allow adding or overwriting AWS instance limit via the cilium-agent option
    `--aws-instance-limit-mapping`.
    
    Fixes: #9052
    
    Signed-off-by: Jaff Cheng <jaff.cheng.sh@gmail.com>

--- pkg/aws/eni/limits_test.go
+++ pkg/aws/eni/limits_test.go
@@ -17,15 +17,63 @@
 package eni
 
 import (
+	"github.com/cilium/cilium/pkg/option"
+
 	"gopkg.in/check.v1"
 )
 
 func (e *ENISuite) TestGetLimits(c *check.C) {
-	_, ok := GetLimits("unknown")
+	option.Config.AwsInstanceLimitMapping = map[string]string{"a2.custom2": "4,5,6"}
+
+	_, ok, err := GetLimits("unknown")
+	c.Assert(err, check.Equals, nil)
 	c.Assert(ok, check.Equals, false)
 
-	l, ok := GetLimits("m3.large")
+	l, ok, err := GetLimits("m3.large")
+	c.Assert(err, check.Equals, nil)
 	c.Assert(ok, check.Equals, true)
 	c.Assert(l.Adapters, check.Not(check.Equals), 0)
 	c.Assert(l.IPv4, check.Not(check.Equals), 0)
+
+	l, ok, err = GetLimits("a2.custom2")
+	c.Assert(err, check.Equals, nil)
+	c.Assert(ok, check.Equals, true)
+	c.Assert(l.Adapters, check.Equals, 4)
+	c.Assert(l.IPv4, check.Equals, 5)
+	c.Assert(l.IPv6, check.Equals, 6)
+
+}
+
+func (e *ENISuite) TestParseLimitString(c *check.C) {
+	limitString1 := "4,5 ,6"
+	limitString2 := "4,5,a"
+	limitString3 := "4,5"
+	limitString4 := "45"
+	limitString5 := ","
+	limitString6 := ""
+
+	limit, err := parseLimitString(limitString1)
+	c.Assert(err, check.Equals, nil)
+	c.Assert(limit.Adapters, check.Equals, 4)
+	c.Assert(limit.IPv4, check.Equals, 5)
+	c.Assert(limit.IPv6, check.Equals, 6)
+
+	limit, err = parseLimitString(limitString2)
+	c.Assert(err, check.Not(check.Equals), nil)
+
+	limit, err = parseLimitString(limitString3)
+	c.Assert(err.Error(), check.Equals, "invalid limit value")
+	c.Assert(limit.Adapters, check.Not(check.Equals), 4)
+	c.Assert(limit.IPv4, check.Not(check.Equals), 5)
+	c.Assert(limit.IPv6, check.Equals, 0)
+
+	limit, err = parseLimitString(limitString4)
+	c.Assert(err.Error(), check.Equals, "invalid limit value")
+
+	limit, err = parseLimitString(limitString5)
+	c.Assert(err.Error(), check.Equals, "invalid limit value")
+
+	limit, err = parseLimitString(limitString6)
+	c.Assert(err.Error(), check.Equals, "invalid limit value")
+
 }
