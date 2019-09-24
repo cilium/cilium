adb24053f0afd2229fcea8ea0220de13e8b45445
Martynas Pumputis <m@lambda.lt>
2019-09-24 07:17:11 +0200

    lbmap: Remove RRSeq map definitions
    They used to be required for the maps removal. However, we can remove
    maps with bpf.UnpinMapIfExists() which doesn't depend on map
    definitions.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- pkg/maps/lbmap/ipv4.go
+++ pkg/maps/lbmap/ipv4.go
@@ -95,23 +95,6 @@ var (
 
 			return revKey.ToNetwork(), revNat.ToNetwork(), nil
 		}).WithCache()
-	RRSeq4MapV2 = bpf.NewMap("cilium_lb4_rr_seq_v2",
-		bpf.MapTypeHash,
-		&Service4KeyV2{},
-		int(unsafe.Sizeof(Service4KeyV2{})),
-		&RRSeqValue{},
-		int(unsafe.Sizeof(RRSeqValue{})),
-		maxFrontEnds,
-		0, 0,
-		func(key []byte, value []byte, mapKey bpf.MapKey, mapValue bpf.MapValue) (bpf.MapKey, bpf.MapValue, error) {
-			svcKey := mapKey.(*Service4KeyV2)
-
-			if _, _, err := bpf.ConvertKeyValue(key, value, svcKey, mapValue); err != nil {
-				return nil, nil, err
-			}
-
-			return svcKey.ToNetwork(), mapValue, nil
-		}).WithCache()
 )
 
 // Service4Key must match 'struct lb4_key' in "bpf/lib/common.h".
