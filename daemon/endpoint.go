d631282f6638bdebe21b137376c1f895384d49cb
Joe Stringer <joe@cilium.io>
2019-09-23 17:14:30 -0700

    daemon: Start controller when pod labels resolution fails
    [ upstream commit eb8df612c551cf0385eece4d625caf7f44c03344 ]
    
      Backporter's notes: Replaced GetK8sNamespaceAndPodName() call with
      GetK8sNamespaceAndPodNameLocked(), since the former wasn't available
      on branch v1.6. The latter is still safe because the endpoint isn't
      yet shared.
    
    When a pod's labels are unable to be resolved in kube-apiserver, start
    up a controller which will regularly attempt resolving the pod labels.
    Eventually the apiserver should become aware of the pod and the labels
    will be resolved, at which point this new code will update the
    labels/identity and stop the controller.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- daemon/endpoint.go
+++ daemon/endpoint.go
@@ -25,11 +25,13 @@ import (
 	. "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
 	"github.com/cilium/cilium/pkg/api"
 	"github.com/cilium/cilium/pkg/completion"
+	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint"
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/k8s"
+	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/lxcmap"
@@ -249,6 +251,42 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 		// get its actual identity.
 		addLabels = labels.Labels{
 			labels.IDNameInit: labels.NewLabel(labels.IDNameInit, "", labels.LabelSourceReserved),
+		}
+	}
+
+	// Static pods (mirror pods) might be configured before the apiserver
+	// is available or has received the notification that includes the
+	// static pod's labels. In this case, start a controller to attempt to
+	// resolve the labels.
+	if ep.K8sNamespaceAndPodNameIsSet() && k8s.IsEnabled() {
+		// If there are labels, but no pod namespace then it's
+		// likely that there are no k8s labels at all. Resolve.
+		if _, k8sLabelsConfigured := addLabels[k8sConst.PodNamespaceLabel]; !k8sLabelsConfigured {
+			done := make(chan struct{})
+
+			// 'ep' is not yet shared; safe to GetK8sNamespaceAndPodNameLocked() without lock.
+			controllerName := fmt.Sprintf("resolve-labels-%s", ep.GetK8sNamespaceAndPodNameLocked())
+			mgr := controller.NewManager()
+			mgr.UpdateController(controllerName,
+				controller.ControllerParams{
+					DoFunc: func(ctx context.Context) error {
+						identityLabels, info, err := fetchK8sLabels(ep)
+						if err != nil {
+							ep.Logger(controllerName).WithError(err).Warning("Unable to fetch kubernetes labels")
+							return err
+						}
+
+						ep.UpdateLabels(ctx, identityLabels, info, true)
+						close(done)
+						return nil
+					},
+					RunInterval: 30 * time.Second,
+				},
+			)
+			go func() {
+				<-done
+				mgr.RemoveController(controllerName)
+			}()
 		}
 	}
 
