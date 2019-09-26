cilium/cilium

commit c89f3a272533752d4ee78357758f2e2ed8ef6546

lbmap: Get rid of LB weights
Previously LB backend weights were not used, so just get rid of them.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/maps/lbmap/ipv6.go
@@ -198,15 +198,13 @@ type Service6Value struct {
 	Port    uint16     `align:"port"`
 	Count   uint16     `align:"count"`
 	RevNat  uint16     `align:"rev_nat_index"`
-	Weight  uint16     `align:"weight"`
 }
 
-func NewService6Value(count uint16, target net.IP, port uint16, revNat uint16, weight uint16) *Service6Value {
+func NewService6Value(count uint16, target net.IP, port uint16, revNat uint16) *Service6Value {
 	svc := Service6Value{
 		Count:  count,
 		Port:   port,
 		RevNat: revNat,
-		Weight: weight,
 	}
 
 	copy(svc.Address[:], target.To16())
@@ -221,8 +219,6 @@ func (s *Service6Value) SetCount(count int)          { s.Count = uint16(count) }
 func (s *Service6Value) GetCount() int               { return int(s.Count) }
 func (s *Service6Value) SetRevNat(id int)            { s.RevNat = uint16(id) }
 func (s *Service6Value) RevNatKey() RevNatKey        { return &RevNat6Key{s.RevNat} }
-func (s *Service6Value) SetWeight(weight uint16)     { s.Weight = weight }
-func (s *Service6Value) GetWeight() uint16           { return s.Weight }
 func (s *Service6Value) IsIPv6() bool                { return true }
 
 func (s *Service6Value) SetAddress(ip net.IP) error {
@@ -243,7 +239,6 @@ func (s *Service6Value) ToNetwork() ServiceValue {
 	n := *s
 	n.RevNat = byteorder.HostToNetwork(n.RevNat).(uint16)
 	n.Port = byteorder.HostToNetwork(n.Port).(uint16)
-	n.Weight = byteorder.HostToNetwork(n.Weight).(uint16)
 	return &n
 }
 
@@ -252,7 +247,6 @@ func (s *Service6Value) ToHost() ServiceValue {
 	n := *s
 	n.RevNat = byteorder.NetworkToHost(n.RevNat).(uint16)
 	n.Port = byteorder.NetworkToHost(n.Port).(uint16)
-	n.Weight = byteorder.NetworkToHost(n.Weight).(uint16)
 	return &n
 }
 
@@ -366,16 +360,14 @@ type Service6ValueV2 struct {
 	BackendID uint32 `align:"backend_id"`
 	Count     uint16 `align:"count"`
 	RevNat    uint16 `align:"rev_nat_index"`
-	Weight    uint16 `align:"weight"`
 	Pad       uint16
 }
 
-func NewService6ValueV2(count uint16, backendID loadbalancer.BackendID, revNat uint16, weight uint16) *Service6ValueV2 {
+func NewService6ValueV2(count uint16, backendID loadbalancer.BackendID, revNat uint16) *Service6ValueV2 {
 	svc := Service6ValueV2{
 		Count:     count,
 		BackendID: uint32(backendID),
 		RevNat:    revNat,
-		Weight:    weight,
 	}
 
 	return &svc
@@ -387,13 +379,11 @@ func (s *Service6ValueV2) String() string {
 
 func (s *Service6ValueV2) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(s) }
 
-func (s *Service6ValueV2) SetCount(count int)      { s.Count = uint16(count) }
-func (s *Service6ValueV2) GetCount() int           { return int(s.Count) }
-func (s *Service6ValueV2) SetRevNat(id int)        { s.RevNat = uint16(id) }
-func (s *Service6ValueV2) GetRevNat() int          { return int(s.RevNat) }
-func (s *Service6ValueV2) SetWeight(weight uint16) { s.Weight = weight }
-func (s *Service6ValueV2) GetWeight() uint16       { return s.Weight }
-func (s *Service6ValueV2) RevNatKey() RevNatKey    { return &RevNat6Key{s.RevNat} }
+func (s *Service6ValueV2) SetCount(count int)   { s.Count = uint16(count) }
+func (s *Service6ValueV2) GetCount() int        { return int(s.Count) }
+func (s *Service6ValueV2) SetRevNat(id int)     { s.RevNat = uint16(id) }
+func (s *Service6ValueV2) GetRevNat() int       { return int(s.RevNat) }
+func (s *Service6ValueV2) RevNatKey() RevNatKey { return &RevNat6Key{s.RevNat} }
 
 func (s *Service6ValueV2) SetBackendID(id loadbalancer.BackendID) {
 	s.BackendID = uint32(id)
@@ -405,7 +395,6 @@ func (s *Service6ValueV2) GetBackendID() loadbalancer.BackendID {
 func (s *Service6ValueV2) ToNetwork() ServiceValueV2 {
 	n := *s
 	n.RevNat = byteorder.HostToNetwork(n.RevNat).(uint16)
-	n.Weight = byteorder.HostToNetwork(n.Weight).(uint16)
 	return &n
 }
 

bpf/lib/common.h
@@ -546,8 +546,7 @@ struct lb6_service_v2 {
 	__u32 backend_id;
 	__u16 count;
 	__u16 rev_nat_index;
-	__u16 weight;
-	__u16 pad;
+	__u32 pad;
 };
 
 /* See lb4_backend comments */
@@ -578,8 +577,7 @@ struct lb4_service_v2 {
 	 */
 	__u16 count;
 	__u16 rev_nat_index;	/* Reverse NAT ID in lb4_reverse_nat */
-	__u16 weight;		/* Currently not used */
-	__u16 pad;
+	__u32 pad;
 };
 
 struct lb4_backend {

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=28d63962f004fe907754b64a133e72204a6a925f
+GO_BINDATA_SHA1SUM=3ad3deb153b3cbf28a579c57747b3b8c49ac992e
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/conntrack_test.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

pkg/maps/lbmap/ipv4.go
@@ -198,15 +198,13 @@ type Service4Value struct {
 	Port    uint16     `align:"port"`
 	Count   uint16     `align:"count"`
 	RevNat  uint16     `align:"rev_nat_index"`
-	Weight  uint16     `align:"weight"`
 }
 
-func NewService4Value(count uint16, target net.IP, port uint16, revNat uint16, weight uint16) *Service4Value {
+func NewService4Value(count uint16, target net.IP, port uint16, revNat uint16) *Service4Value {
 	svc := Service4Value{
 		Count:  count,
 		RevNat: revNat,
 		Port:   port,
-		Weight: weight,
 	}
 
 	copy(svc.Address[:], target.To4())
@@ -220,8 +218,6 @@ func (s *Service4Value) GetPort() uint16             { return s.Port }
 func (s *Service4Value) SetCount(count int)          { s.Count = uint16(count) }
 func (s *Service4Value) GetCount() int               { return int(s.Count) }
 func (s *Service4Value) SetRevNat(id int)            { s.RevNat = uint16(id) }
-func (s *Service4Value) SetWeight(weight uint16)     { s.Weight = weight }
-func (s *Service4Value) GetWeight() uint16           { return s.Weight }
 func (s *Service4Value) IsIPv6() bool                { return false }
 
 func (s *Service4Value) SetAddress(ip net.IP) error {
@@ -242,7 +238,6 @@ func (s *Service4Value) ToNetwork() ServiceValue {
 	n := *s
 	n.RevNat = byteorder.HostToNetwork(n.RevNat).(uint16)
 	n.Port = byteorder.HostToNetwork(n.Port).(uint16)
-	n.Weight = byteorder.HostToNetwork(n.Weight).(uint16)
 	return &n
 }
 
@@ -251,7 +246,6 @@ func (s *Service4Value) ToHost() ServiceValue {
 	n := *s
 	n.RevNat = byteorder.NetworkToHost(n.RevNat).(uint16)
 	n.Port = byteorder.NetworkToHost(n.Port).(uint16)
-	n.Weight = byteorder.NetworkToHost(n.Weight).(uint16)
 	return &n
 }
 
@@ -380,16 +374,14 @@ type Service4ValueV2 struct {
 	BackendID uint32 `align:"backend_id"`
 	Count     uint16 `align:"count"`
 	RevNat    uint16 `align:"rev_nat_index"`
-	Weight    uint16 `align:"weight"`
-	Pad       uint16
+	Pad       uint32
 }
 
-func NewService4ValueV2(count uint16, backendID loadbalancer.BackendID, revNat uint16, weight uint16) *Service4ValueV2 {
+func NewService4ValueV2(count uint16, backendID loadbalancer.BackendID, revNat uint16) *Service4ValueV2 {
 	svc := Service4ValueV2{
 		BackendID: uint32(backendID),
 		Count:     count,
 		RevNat:    revNat,
-		Weight:    weight,
 	}
 
 	return &svc
@@ -401,13 +393,11 @@ func (s *Service4ValueV2) String() string {
 
 func (s *Service4ValueV2) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(s) }
 
-func (s *Service4ValueV2) SetCount(count int)      { s.Count = uint16(count) }
-func (s *Service4ValueV2) GetCount() int           { return int(s.Count) }
-func (s *Service4ValueV2) SetRevNat(id int)        { s.RevNat = uint16(id) }
-func (s *Service4ValueV2) GetRevNat() int          { return int(s.RevNat) }
-func (s *Service4ValueV2) SetWeight(weight uint16) { s.Weight = weight }
-func (s *Service4ValueV2) GetWeight() uint16       { return s.Weight }
-func (s *Service4ValueV2) RevNatKey() RevNatKey    { return &RevNat4Key{s.RevNat} }
+func (s *Service4ValueV2) SetCount(count int)   { s.Count = uint16(count) }
+func (s *Service4ValueV2) GetCount() int        { return int(s.Count) }
+func (s *Service4ValueV2) SetRevNat(id int)     { s.RevNat = uint16(id) }
+func (s *Service4ValueV2) GetRevNat() int       { return int(s.RevNat) }
+func (s *Service4ValueV2) RevNatKey() RevNatKey { return &RevNat4Key{s.RevNat} }
 
 func (s *Service4ValueV2) SetBackendID(id loadbalancer.BackendID) {
 	s.BackendID = uint32(id)
@@ -419,7 +409,6 @@ func (s *Service4ValueV2) GetBackendID() loadbalancer.BackendID {
 func (s *Service4ValueV2) ToNetwork() ServiceValueV2 {
 	n := *s
 	n.RevNat = byteorder.HostToNetwork(n.RevNat).(uint16)
-	n.Weight = byteorder.HostToNetwork(n.Weight).(uint16)
 	return &n
 }
 

pkg/maps/lbmap/lbmap.go
@@ -72,66 +72,6 @@ func deleteRevNatLocked(key RevNatKey) error {
 	return key.Map().Delete(key.ToNetwork())
 }
 
-// gcd computes the gcd of two numbers.
-func gcd(x, y uint16) uint16 {
-	for y != 0 {
-		x, y = y, x%y
-	}
-	return x
-}
-
-// generateWrrSeq generates a wrr sequence based on provided weights.
-func generateWrrSeq(weights []uint16) (*RRSeqValue, error) {
-	svcRRSeq := RRSeqValue{}
-
-	n := len(weights)
-	if n < 2 {
-		return nil, fmt.Errorf("needs at least 2 weights")
-	}
-
-	g := uint16(0)
-	for i := 0; i < n; i++ {
-		if weights[i] != 0 {
-			g = gcd(g, weights[i])
-		}
-	}
-
-	// This means all the weights are 0.
-	if g == 0 {
-		return nil, fmt.Errorf("all specified weights are 0")
-	}
-
-	sum := uint16(0)
-	for i := range weights {
-		// Normalize the weights.
-		weights[i] = weights[i] / g
-		sum += weights[i]
-	}
-
-	// Check if Generated seq fits in our array.
-	if int(sum) > len(svcRRSeq.Idx) {
-		return nil, fmt.Errorf("sum of normalized weights exceeds %d", len(svcRRSeq.Idx))
-	}
-
-	// Generate the Sequence.
-	i := uint16(0)
-	k := uint16(0)
-	for {
-		j := uint16(0)
-		for j < weights[k] {
-			svcRRSeq.Idx[i] = k
-			i++
-			j++
-		}
-		if i >= sum {
-			break
-		}
-		k++
-	}
-	svcRRSeq.Count = sum
-	return &svcRRSeq, nil
-}
-
 // UpdateService adds or updates the given service in the bpf maps.
 func UpdateService(fe ServiceKey, backends []ServiceValue, revNATID int,
 	acquireBackendID func(loadbalancer.L3n4Addr) (loadbalancer.BackendID, error),
@@ -144,11 +84,6 @@ func UpdateService(fe ServiceKey, backends []ServiceValue, revNATID int,
 
 	mutex.Lock()
 	defer mutex.Unlock()
-
-	var (
-		weights         []uint16
-		nNonZeroWeights uint16
-	)
 
 	// Find out which backends are new (i.e. the ones which do not exist yet and
 	// will be created in this function) and acquire IDs for them
@@ -166,15 +101,6 @@ func UpdateService(fe ServiceKey, backends []ServiceValue, revNATID int,
 		return err
 	}
 
-	// FIXME(brb) Uncomment the following code after we have enabled weights
-	// in the BPF datapath code.
-	//for _, be := range besValues {
-	//	weights = append(weights, be.GetWeight())
-	//	if be.GetWeight() != 0 {
-	//		nNonZeroWeights++
-	//	}
-	//}
-
 	besValuesV2 := svc.getBackendsV2()
 
 	scopedLog.Debug("Updating BPF representation of service")
@@ -185,7 +111,7 @@ func UpdateService(fe ServiceKey, backends []ServiceValue, revNATID int,
 	}
 
 	// Update the v2 service BPF maps
-	if err := updateServiceV2Locked(fe, besValuesV2, svc, revNATID, weights, nNonZeroWeights); err != nil {
+	if err := updateServiceV2Locked(fe, besValuesV2, svc, revNATID); err != nil {
 		return err
 	}
 
@@ -253,8 +179,7 @@ func updateBackendsLocked(addedBackends map[loadbalancer.BackendID]ServiceValue)
 }
 
 func updateServiceV2Locked(fe ServiceKey, backends serviceValueMap,
-	svc *bpfService, revNATID int,
-	weights []uint16, nNonZeroWeights uint16) error {
+	svc *bpfService, revNATID int) error {
 
 	var (
 		existingCount int
@@ -276,11 +201,10 @@ func updateServiceV2Locked(fe ServiceKey, backends serviceValueMap,
 
 	svcValV2 = svcKeyV2.NewValue().(ServiceValueV2)
 	slot := 1
-	for addrID, svcVal := range backends {
+	for addrID := range backends {
 		backendKey := cache.getBackendKey(addrID)
 		svcValV2.SetBackendID(backendKey.GetID())
 		svcValV2.SetRevNat(revNATID)
-		svcValV2.SetWeight(svcVal.GetWeight())
 		svcKeyV2.SetSlave(slot)
 		if err := updateServiceEndpointV2(svcKeyV2, svcValV2); err != nil {
 			return fmt.Errorf("Unable to update service %+v with the value %+v: %s",
@@ -308,14 +232,9 @@ func updateServiceV2Locked(fe ServiceKey, backends serviceValueMap,
 		}
 	}()
 
-	err = updateMasterServiceV2(svcKeyV2, len(svc.backendsV2), nNonZeroWeights, revNATID)
+	err = updateMasterServiceV2(svcKeyV2, len(svc.backendsV2), revNATID)
 	if err != nil {
 		return fmt.Errorf("unable to update service %+v: %s", svcKeyV2, err)
-	}
-
-	err = updateWrrSeqV2(svcKeyV2, weights)
-	if err != nil {
-		return fmt.Errorf("unable to update service weights for %s with value %+v: %s", svcKeyV2.String(), weights, err)
 	}
 
 	for i := slot; i <= existingCount; i++ {
@@ -474,9 +393,8 @@ func DumpBackendMapsToUserspace() (map[BackendAddrID]*loadbalancer.LBBackEnd, er
 	for backendID, backendVal := range backendValueMap {
 		ip := backendVal.GetAddress()
 		port := backendVal.GetPort()
-		weight := uint16(0) // FIXME(brb): set weight when we support it
 		proto := loadbalancer.NONE
-		lbBackend := loadbalancer.NewLBBackEnd(backendID, proto, ip, port, weight)
+		lbBackend := loadbalancer.NewLBBackEnd(backendID, proto, ip, port, 0)
 		lbBackends[backendVal.BackendAddrID()] = lbBackend
 	}
 
@@ -503,58 +421,17 @@ func lookupServiceV2(key ServiceKeyV2) (ServiceValueV2, error) {
 	return svc.ToNetwork(), nil
 }
 
-func updateMasterServiceV2(fe ServiceKeyV2, nbackends int, nonZeroWeights uint16, revNATID int) error {
+func updateMasterServiceV2(fe ServiceKeyV2, nbackends int, revNATID int) error {
 	fe.SetSlave(0)
 	zeroValue := fe.NewValue().(ServiceValueV2)
 	zeroValue.SetCount(nbackends)
-	zeroValue.SetWeight(nonZeroWeights)
 	zeroValue.SetRevNat(revNATID)
 
 	return updateServiceEndpointV2(fe, zeroValue)
 }
 
-// updateWrrSeq updates bpf map with the generated wrr sequence.
-func updateWrrSeqV2(fe ServiceKeyV2, weights []uint16) error {
-	sum := uint16(0)
-	for _, v := range weights {
-		sum += v
-	}
-	if sum == 0 {
-		return nil
-	}
-	svcRRSeq, err := generateWrrSeq(weights)
-	if err != nil {
-		return fmt.Errorf("unable to generate weighted round robin seq for %s with value %+v: %s", fe.String(), weights, err)
-	}
-	return updateServiceWeightsV2(fe, svcRRSeq)
-}
-
-// updateServiceWeightsV2 updates cilium_lb6_rr_seq_v2 or cilium_lb4_rr_seq_v2 bpf maps.
-func updateServiceWeightsV2(key ServiceKeyV2, value *RRSeqValue) error {
-	if _, err := key.RRMap().OpenOrCreate(); err != nil {
-		return err
-	}
-
-	return key.RRMap().Update(key.ToNetwork(), value)
-}
-
 func deleteServiceLockedV2(key ServiceKeyV2) error {
-	err := key.Map().Delete(key.ToNetwork())
-	if err != nil {
-		return err
-	}
-	return lookupAndDeleteServiceWeightsV2(key)
-}
-
-// lookupAndDeleteServiceWeightsV2 deletes entry from cilium_lb6_rr_seq or cilium_lb4_rr_seq
-func lookupAndDeleteServiceWeightsV2(key ServiceKeyV2) error {
-	_, err := key.RRMap().Lookup(key.ToNetwork())
-	if err != nil {
-		// Ignore if entry is not found.
-		return nil
-	}
-
-	return key.RRMap().Delete(key.ToNetwork())
+	return key.Map().Delete(key.ToNetwork())
 }
 
 func updateBackend(backend Backend) error {

pkg/maps/lbmap/lbmap_test.go
@@ -37,12 +37,12 @@ var _ = Suite(&LBMapTestSuite{})
 func (b *LBMapTestSuite) TestBackendAddrID(c *C) {
 	b4, err := NewBackend4Value(net.ParseIP("1.1.1.1"), 80, u8proto.ANY)
 	c.Assert(err, IsNil)
-	v4 := NewService4Value(0, net.ParseIP("1.1.1.1"), 80, 0, 0)
+	v4 := NewService4Value(0, net.ParseIP("1.1.1.1"), 80, 0)
 	c.Assert(b4.BackendAddrID(), Equals, v4.BackendAddrID())
 
 	b6, err := NewBackend6Value(net.ParseIP("f00d::0:0"), 80, u8proto.ANY)
 	c.Assert(err, IsNil)
-	v6 := NewService6Value(0, net.ParseIP("f00d::0:0"), 80, 0, 0)
+	v6 := NewService6Value(0, net.ParseIP("f00d::0:0"), 80, 0)
 	c.Assert(b6.BackendAddrID(), Equals, v6.BackendAddrID())
 
 }

pkg/maps/lbmap/types.go
@@ -41,9 +41,6 @@ type ServiceKey interface {
 	// Returns the BPF map matching the key type
 	Map() *bpf.Map
 
-	// Returns the BPF Weighted Round Robin map matching the key type
-	RRMap() *bpf.Map
-
 	// Returns a RevNatValue matching a ServiceKey
 	RevNatValue() RevNatValue
 
@@ -91,12 +88,6 @@ type ServiceValue interface {
 	// Set reverse NAT identifier
 	SetRevNat(int)
 
-	// Set Weight
-	SetWeight(uint16)
-
-	// Get Weight
-	GetWeight() uint16
-
 	// ToNetwork converts fields to network byte order.
 	ToNetwork() ServiceValue
 
@@ -121,9 +112,6 @@ type ServiceKeyV2 interface {
 
 	// Return the BPF map matching the key type
 	Map() *bpf.Map
-
-	// Return the BPF Weighted Round Robin map matching the key type
-	RRMap() *bpf.Map
 
 	// Set slave slot for the key
 	SetSlave(slave int)
@@ -159,12 +147,6 @@ type ServiceValueV2 interface {
 
 	// Get reverse NAT identifier
 	GetRevNat() int
-
-	// Set weight
-	SetWeight(uint16)
-
-	// Get weight
-	GetWeight() uint16
 
 	// Set backend identifier
 	SetBackendID(id loadbalancer.BackendID)
@@ -303,7 +285,6 @@ func LBSVC2ServiceKeynValue(svc loadbalancer.LBSVC) (ServiceKey, []ServiceValue,
 		}
 		beValue.SetPort(be.Port)
 		beValue.SetRevNat(int(svc.FE.ID))
-		beValue.SetWeight(be.Weight)
 
 		besValues = append(besValues, beValue)
 		log.WithFields(logrus.Fields{
@@ -363,7 +344,6 @@ func LBSVC2ServiceKeynValuenBackendV2(svc *loadbalancer.LBSVC) (ServiceKeyV2, []
 		}
 
 		svcValue.SetRevNat(int(svc.FE.ID))
-		svcValue.SetWeight(be.Weight)
 		svcValue.SetBackendID(loadbalancer.BackendID(be.ID))
 
 		backends = append(backends, backend)
@@ -412,9 +392,8 @@ func serviceKeynValuenBackendValue2FEnBE(svcKey ServiceKeyV2, svcValue ServiceVa
 	if backendID != 0 {
 		beIP := backend.GetAddress()
 		bePort := backend.GetPort()
-		beWeight := svcValue.GetWeight()
 		beProto := loadbalancer.NONE
-		beLBBackEnd = loadbalancer.NewLBBackEnd(backendID, beProto, beIP, bePort, beWeight)
+		beLBBackEnd = loadbalancer.NewLBBackEnd(backendID, beProto, beIP, bePort, 0)
 	}
 
 	return feL3n4AddrID, beLBBackEnd

commit 34987d7e6b2eb05917f0d026870405b0bcb5b102

lbmap: Fix padding of Service6ValueV2
The actual padding of the struct is u32 instead of u16. The mismatch
is probably harmless, as Go compiled should have added an u16 unexported
field. Otherwise, the alignchecker would have complained.

Fixes: c89f3a2725 ("lbmap: Get rid of LB weights")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/maps/lbmap/ipv6.go
@@ -189,7 +189,7 @@ type Service6ValueV2 struct {
 	BackendID uint32 `align:"backend_id"`
 	Count     uint16 `align:"count"`
 	RevNat    uint16 `align:"rev_nat_index"`
-	Pad       uint16
+	Pad       uint32
 }
 
 func NewService6ValueV2(count uint16, backendID loadbalancer.BackendID, revNat uint16) *Service6ValueV2 {