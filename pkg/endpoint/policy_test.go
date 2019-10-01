45f6426695f081a14aba7d4b670d8a5e2e935205
Ian Vernon <ian@cilium.io>
2019-10-01 08:54:19 -0700

    endpoint: add `VisibilityPolicy` field and plumbing logic
    When a `VisibilityPolicy` exists for an endpoint, and policy enforcement is
    disabled for the traffic direction of the `VisibilityPolicy`, an entry is added
    to the `PolicyMap` for the endpoint which will redirect the traffic to the L7
    proxy. If policy enforcement is enabled for a given traffic direction, then
    `VisibilityPolicy` currently is ignored.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

---
+++ pkg/endpoint/policy_test.go
@@ -0,0 +1,46 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package endpoint
+
+import (
+	"github.com/cilium/cilium/pkg/annotation"
+	"github.com/cilium/cilium/pkg/policy"
+	"github.com/cilium/cilium/pkg/u8proto"
+	"gopkg.in/check.v1"
+)
+
+func (s *EndpointSuite) TestUpdateVisibilityPolicy(c *check.C) {
+	ep := NewEndpointWithState(&DummyOwner{repo: policy.NewPolicyRepository()}, nil, 12345, StateReady)
+	ep.UpdateVisibilityPolicy(nil)
+	c.Assert(ep.visibilityPolicy, check.IsNil)
+
+	ep.UpdateVisibilityPolicy(map[string]string{
+		annotation.ProxyVisibility: "<Ingress/80/TCP/HTTP>",
+	})
+
+	c.Assert(ep.visibilityPolicy, check.Not(check.Equals), nil)
+	c.Assert(ep.visibilityPolicy.Ingress["80/TCP"], check.DeepEquals, &policy.VisibilityMetadata{
+		Parser:  policy.ParserTypeHTTP,
+		Port:    uint16(80),
+		Proto:   u8proto.TCP,
+		Ingress: true,
+	})
+
+	// Check that updating after previously having value works.
+	ep.UpdateVisibilityPolicy(nil)
+	c.Assert(ep.visibilityPolicy, check.IsNil)
+}
