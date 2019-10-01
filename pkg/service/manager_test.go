d872612cf9d3295f8c8feeb7348f02eafc5b5918
Martynas Pumputis <m@lambda.lt>
2019-10-01 15:53:40 +0200

    service: Add unit test for restoring services
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- pkg/service/manager_test.go
+++ pkg/service/manager_test.go
@@ -17,6 +17,7 @@
 package service
 
 import (
+	"fmt"
 	"net"
 
 	lb "github.com/cilium/cilium/pkg/loadbalancer"
@@ -46,6 +47,19 @@ func (e *ManagerTestSuite) TearDownTest(c *C) {
 	backendIDAlloc.resetLocalID()
 }
 
+var (
+	frontend1 = *lb.NewL3n4AddrID(lb.TCP, net.ParseIP("1.1.1.1"), 80, 0)
+	frontend2 = *lb.NewL3n4AddrID(lb.TCP, net.ParseIP("1.1.1.2"), 80, 0)
+	backends1 = []lb.LBBackEnd{
+		*lb.NewLBBackEnd(0, lb.TCP, net.ParseIP("10.0.0.1"), 8080),
+		*lb.NewLBBackEnd(0, lb.TCP, net.ParseIP("10.0.0.2"), 8080),
+	}
+	backends2 = []lb.LBBackEnd{
+		*lb.NewLBBackEnd(0, lb.TCP, net.ParseIP("10.0.0.2"), 8080),
+		*lb.NewLBBackEnd(0, lb.TCP, net.ParseIP("10.0.0.3"), 8080),
+	}
+)
+
 func (m *ManagerTestSuite) TestUpsertAndDeleteService(c *C) {
 	frontend1 := *lb.NewL3n4AddrID(lb.TCP, net.ParseIP("1.1.1.1"), 80, 0)
 	frontend2 := *lb.NewL3n4AddrID(lb.TCP, net.ParseIP("1.1.1.2"), 80, 0)
@@ -59,7 +73,7 @@ func (m *ManagerTestSuite) TestUpsertAndDeleteService(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(created, Equals, true)
 	c.Assert(id1, Equals, lb.ID(1))
-	c.Assert(len(m.lbmap.ServiceBackendsByID[uint16(id1)]), Equals, 2)
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id1)].BES), Equals, 2)
 	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
 
 	// Should update nothing
@@ -67,15 +81,16 @@ func (m *ManagerTestSuite) TestUpsertAndDeleteService(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(created, Equals, false)
 	c.Assert(id1, Equals, lb.ID(1))
-	c.Assert(len(m.lbmap.ServiceBackendsByID[uint16(id1)]), Equals, 2)
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id1)].BES), Equals, 2)
 	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
+	// TODO(brb) test that backends are the same
 
 	// Should remove one backend
 	created, id1, err = m.svc.UpsertService(frontend1, backends1[0:1], TypeNodePort)
 	c.Assert(err, IsNil)
 	c.Assert(created, Equals, false)
 	c.Assert(id1, Equals, lb.ID(1))
-	c.Assert(len(m.lbmap.ServiceBackendsByID[uint16(id1)]), Equals, 1)
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id1)].BES), Equals, 1)
 	c.Assert(len(m.lbmap.BackendByID), Equals, 1)
 
 	// Should add another service
@@ -83,7 +98,7 @@ func (m *ManagerTestSuite) TestUpsertAndDeleteService(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(created, Equals, true)
 	c.Assert(id2, Equals, lb.ID(2))
-	c.Assert(len(m.lbmap.ServiceBackendsByID[uint16(id2)]), Equals, 2)
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id2)].BES), Equals, 2)
 	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
 
 	// Should remove the service and the backend, but keep another service and
@@ -91,7 +106,7 @@ func (m *ManagerTestSuite) TestUpsertAndDeleteService(c *C) {
 	found, err := m.svc.DeleteServiceByID(lb.ServiceID(id1))
 	c.Assert(err, IsNil)
 	c.Assert(found, Equals, true)
-	c.Assert(len(m.lbmap.ServiceBackendsByID), Equals, 1)
+	c.Assert(len(m.lbmap.ServiceByID), Equals, 1)
 	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
 
 	// Should delete both backends of service
@@ -99,13 +114,41 @@ func (m *ManagerTestSuite) TestUpsertAndDeleteService(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(created, Equals, false)
 	c.Assert(id2, Equals, lb.ID(2))
-	c.Assert(len(m.lbmap.ServiceBackendsByID[uint16(id2)]), Equals, 0)
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id2)].BES), Equals, 0)
 	c.Assert(len(m.lbmap.BackendByID), Equals, 0)
 
 	// Should delete the remaining service
 	found, err = m.svc.DeleteServiceByID(lb.ServiceID(id2))
 	c.Assert(err, IsNil)
 	c.Assert(found, Equals, true)
-	c.Assert(len(m.lbmap.ServiceBackendsByID), Equals, 0)
+	c.Assert(len(m.lbmap.ServiceByID), Equals, 0)
 	c.Assert(len(m.lbmap.BackendByID), Equals, 0)
+}
+
+func (m *ManagerTestSuite) TestRestoreServices(c *C) {
+	_, id1, err := m.svc.UpsertService(frontend1, backends1, TypeNodePort)
+	c.Assert(err, IsNil)
+	_, id2, err := m.svc.UpsertService(frontend2, backends2, TypeClusterIP)
+	c.Assert(err, IsNil)
+	fmt.Println(id1, id2)
+
+	// Restart service, but keep the lbmap to restore services from
+	lbmap := m.svc.lbmap.(*lbmap.LBMockMap)
+	m.svc = NewService()
+	m.svc.lbmap = lbmap
+	err = m.svc.RestoreServices()
+	c.Assert(err, IsNil)
+
+	// Backends have been restored
+	c.Assert(len(m.svc.backendByHash), Equals, 3)
+	backends := append(backends1, backends2...)
+	for _, b := range backends {
+		_, found := m.svc.backendByHash[b.SHA256Sum()]
+		c.Assert(found, Equals, true)
+	}
+
+	// Services have been restored too
+	c.Assert(len(m.svc.svcByID), Equals, 2)
+	c.Assert(m.svc.svcByID[id1], Equals, lbmap.ServiceByID[uint16(id1)])
+	c.Assert(m.svc.svcByID[id2], Equals, lbmap.ServiceByID[uint16(id2)])
 }
