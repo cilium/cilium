cilium/cilium

commit 4234ed773d5db0f646fbba069e94101afc400232

cli: Remove --rev param of cilium service update
The param used to control whether the rev-NAT xlation should be
enabled for a service. No service can properly function without it,
so just enable it by default.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

cilium/cmd/service_update.go
@@ -29,7 +29,6 @@ import (
 )
 
 var (
-	addRev   bool
 	idU      uint64
 	frontend string
 	backends []string
@@ -46,7 +45,6 @@ var serviceUpdateCmd = &cobra.Command{
 
 func init() {
 	serviceCmd.AddCommand(serviceUpdateCmd)
-	serviceUpdateCmd.Flags().BoolVarP(&addRev, "rev", "", true, "Add reverse translation")
 	serviceUpdateCmd.Flags().Uint64VarP(&idU, "id", "", 0, "Identifier")
 	serviceUpdateCmd.Flags().StringVarP(&frontend, "frontend", "", "", "Frontend address")
 	serviceUpdateCmd.Flags().StringSliceVarP(&backends, "backends", "", []string{}, "Backend address or addresses followed by optional weight (<IP:Port>[/weight])")
@@ -92,7 +90,6 @@ func updateService(cmd *cobra.Command, args []string) {
 	}
 
 	spec.FrontendAddress = fa
-	spec.Flags.DirectServerReturn = addRev
 
 	if len(backends) == 0 {
 		fmt.Printf("Reading backend list from stdin...\n")

daemon/loadbalancer.go
@@ -190,16 +190,11 @@ func (h *putServiceID) Handle(params PutServiceIDParams) middleware.Responder {
 		backends = append(backends, *b)
 	}
 
-	revnat := false
-	if params.Config.Flags != nil {
-		revnat = params.Config.Flags.DirectServerReturn
-	}
-
 	// FIXME
 	// Add flag to indicate whether service should be registered in
 	// global key value store
 
-	if created, err := h.d.SVCAdd(frontend, backends, revnat); err != nil {
+	if created, err := h.d.SVCAdd(frontend, backends, true); err != nil {
 		return api.Error(PutServiceIDFailureCode, err)
 	} else if created {
 		return NewPutServiceIDCreated()

test/helpers/cilium.go
@@ -756,7 +756,7 @@ func (s *SSHMeta) GatherLogs() {
 // backends. Returns the result of creating said service.
 func (s *SSHMeta) ServiceAdd(id int, frontend string, backends []string) *CmdRes {
 	cmd := fmt.Sprintf(
-		"service update --frontend '%s' --backends '%s' --id '%d' --rev",
+		"service update --frontend '%s' --backends '%s' --id '%d'",
 		frontend, strings.Join(backends, ","), id)
 	return s.ExecCilium(cmd)
 }

commit 23bbf51ffc99a13ad44e5ac005fd85c46710d6ef

cli: Warn if --rev flag is used with cilium service update
Instead of returning an error, warn a user that the flag is deprecated
and inactive. This will prevent from breaking user scripts which used
to set the param.

Reported-by: Jarno Rajahalme
Fixes: 4234ed773 ("cli: Remove --rev param of cilium service update")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

cilium/cmd/service_update.go
@@ -27,9 +27,10 @@ import (
 )
 
 var (
-	idU      uint64
-	frontend string
-	backends []string
+	deprecatedAddRev bool // TODO(v1.8+): remove it
+	idU              uint64
+	frontend         string
+	backends         []string
 )
 
 // serviceUpdateCmd represents the service_update command
@@ -44,6 +45,8 @@ var serviceUpdateCmd = &cobra.Command{
 func init() {
 	serviceCmd.AddCommand(serviceUpdateCmd)
 	serviceUpdateCmd.Flags().Uint64VarP(&idU, "id", "", 0, "Identifier")
+	serviceUpdateCmd.Flags().BoolVarP(&deprecatedAddRev, "rev", "", false, "Add reverse translation")
+	serviceUpdateCmd.Flags().MarkDeprecated("rev", "and it is inactive")
 	serviceUpdateCmd.Flags().StringVarP(&frontend, "frontend", "", "", "Frontend address")
 	serviceUpdateCmd.Flags().StringSliceVarP(&backends, "backends", "", []string{}, "Backend address or addresses (<IP:Port>)")
 }