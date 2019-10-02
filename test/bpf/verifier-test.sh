f4f36718c79f36a4efdeacb79eb630189ceb6920
Joe Stringer <joe@cilium.io>
2019-10-01 19:51:55 -0700

    test: bpf: Add support for verifying cgroup progs
    Add support to verifier-test.sh to test the loading / verification of
    the BPF programs that operate at cgroups/sockops.
    
    Fixes: #8424
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- test/bpf/verifier-test.sh
+++ test/bpf/verifier-test.sh
@@ -18,6 +18,7 @@ set -eo pipefail
 
 DEV="cilium-probe"
 DIR=$(dirname $0)/../../bpf
+MAPTOOL=$(dirname $0)/../../tools/maptool/maptool
 ALL_TC_PROGS="bpf_hostdev_ingress bpf_ipsec bpf_lxc bpf_netdev bpf_network bpf_overlay"
 TC_PROGS=${TC_PROGS:-$ALL_TC_PROGS}
 ALL_CG_PROGS="bpf_sock sockops/bpf_sockops sockops/bpf_redir"
@@ -29,8 +30,22 @@ ALL_PROGS="${IGNORED_PROGS} ${ALL_CG_PROGS} ${ALL_TC_PROGS} ${ALL_XDP_PROGS}"
 VERBOSE=false
 
 BPFFS=${BPFFS:-"/sys/fs/bpf"}
+TESTMOUNT=$BPFFS/test
+BPFTOOL=${BPFTOOL:-"bpftool"}
 TC=${TC:-"tc"}
 IPROUTE2=${IPROUTE2:-"ip"}
+
+FEATURES=""
+
+# The following arrays are used for loading cgroups programs, and are
+# initialized in cg_prog_type_init().
+#
+# prog_types is keyed by a section in a bpf/*{,/*}.c source file, with
+# the value as the TC prog_type argument.
+declare -A prog_types
+# attach_types is keyed by a section in a bpf/*{,/*}.c source file,
+# with the value as the tc or bpftool attach_type argument.
+declare -A attach_types
 
 function clean_maps {
 	for f in $BPFFS/tc/globals/test_*; do
@@ -41,6 +56,7 @@ function clean_maps {
 function cleanup {
 	$IPROUTE2 link del ${DEV} 2>/dev/null || true
 	clean_maps
+	rm -rf ${TESTMOUNT}
 }
 
 function get_section {
@@ -79,9 +95,136 @@ function load_tc {
 	done
 }
 
+# $1 - Partial macro to check
+function check_macro {
+	# $FEATURES here is deliberately unquoted to ensure the correct
+	# grepping behaviour, otherwise we get issues with exit code 141
+	echo $FEATURES | grep -q " HAVE_[^ ]*$1"
+}
+
+function cg_prog_type_init {
+	FEATURES="$($BPFTOOL feature probe macros)"
+
+	mkdir -p ${TESTMOUNT}
+
+	# These show up in sockops progs; skip them quietly.
+	prog_types["int"]="SKIP"
+	prog_types["_version"]="SKIP"
+	prog_types["version"]="SKIP"
+	if $BPFTOOL help 2>&1 | grep -q feature; then
+		if check_macro SOCK_OPS; then
+			prog_types["sockops"]="sock_ops"
+			attach_types["sockops"]="sock_ops"
+		fi
+		if check_macro SK_MSG; then
+			prog_types["sk_msg"]="sock_ops"
+			attach_types["sk_msg"]="msg_verdict"
+		fi
+		if check_macro CGROUP_INET4_CONNECT; then
+			prog_types["from-sock4"]="sockaddr"
+			attach_types["from-sock4"]="connect4"
+		fi
+		if check_macro CGROUP_INET6_CONNECT; then
+			prog_types["from-sock6"]="sockaddr"
+			attach_types["from-sock6"]="connect6"
+		fi
+		if check_macro UDP4_RECVMSG; then
+			prog_types["snd-sock4"]="sockaddr"
+			prog_types["rcv-sock4"]="sockaddr"
+			attach_types["snd-sock4"]="sendmsg4"
+			attach_types["rcv-sock4"]="recvmsg4"
+		fi
+		if check_macro UDP6_RECVMSG; then
+			prog_types["snd-sock6"]="sockaddr"
+			prog_types["rcv-sock6"]="sockaddr"
+			attach_types["snd-sock6"]="sendmsg6"
+			attach_types["rcv-sock6"]="recvmsg6"
+		fi
+	fi
+
+	# Hack: Get the eppolicymap and sockmap pinned
+	#
+	# Basically none of the loaders support arbitrarily creating a map
+	# of type 'hash_of_maps' right now, so we have a little tool in the
+	# repo that allows creation of this map type from the commandline.
+	#
+	# Only set it up if we determined kernel support above!
+	if [ "${#attach_types[@]}" -gt 1 ]; then
+		$MAPTOOL eppolicymap "test_cilium_ep_to_policy" 2>/dev/null
+		$MAPTOOL sockmap "test_sock_ops_map" 2>/dev/null
+	fi
+}
+
+function load_sock_prog {
+	prog=$1
+	pinpath=$2
+	prog_type=$3
+	attach_type=$4
+	section=$5
+
+	local args="pin $pinpath obj ${prog}.o type $prog_type \
+		    attach_type $attach_type sec $section"
+	if $VERBOSE; then
+		$TC exec bpf $args verbose 2>&1
+	else
+		$TC exec bpf $args 2>/dev/null \
+		|| $TC exec bpf $args verbose
+	fi
+}
+
+function load_sockops_prog {
+	prog="$1"
+	pinpath="$2"
+
+	ALL_MAPS="cilium_ipcache cilium_ep_to_policy cilium_lxc sock_ops_map	\
+		cilium_metrics cilium_tunnel_map cilium_encrypt_state		\
+		cilium_lb6_reverse_nat cilium_lb6_services cilium_lb6_backends	\
+		cilium_lb4_reverse_nat cilium_lb4_services cilium_lb4_backends	\
+		cilium_events"
+
+	map_args=""
+	for map in $ALL_MAPS; do
+		map_args="$map_args map name test_$map pinned /sys/fs/bpf/tc/globals/test_$map"
+	done
+
+	echo "=> Loading ${p}.c:${section}..."
+	if $VERBOSE; then
+		$BPFTOOL -m prog load "$prog.o" "$pinpath" $map_args 2>&1
+	else
+		$BPFTOOL -m prog load "$prog.o" "$pinpath" $map_args 2>/dev/null \
+		|| $BPFTOOL -m prog load "$prog.o" "$pinpath" $map_args
+	fi
+}
+
 function load_cg {
+	cg_prog_type_init
+
+	mkdir -p $TESTMOUNT/sockops
 	for p in ${CG_PROGS}; do
-		echo "=> Skipping ${DIR}/${p}.c."
+		ELF_SECTIONS="$(readelf -S $DIR/${p}.o)"
+		for section in $(get_section ${p}.c); do
+			if [ "${prog_types[$section]}" == "" ]; then
+				echo "=> Skipping ${p}.c:$section"
+				continue
+			elif [ "${prog_types[$section]}" == "SKIP" ]; then
+				continue
+			elif ! echo $ELF_SECTIONS | grep -q $section; then
+				echo "=> Skipping ${p}.c:$section (not found in ELF)"
+				continue
+			fi
+
+			local prog_type=${prog_types[$section]}
+			local attach_type=${attach_types[$section]}
+			if [ "$prog_type" = "sockaddr" ]; then
+				local args="exec bpf pin $TESTMOUNT/$p obj $DIR/${p}.o \
+					type $prog_type \
+					attach_type $attach_type sec $section"
+				load_prog "$TC" "$args" "${p}.o:$section"
+			else
+				load_sockops_prog "$DIR/$p" "$TESTMOUNT/$p"
+			fi
+			rm -f $TESTMOUNT/$p
+		done
 	done
 }
 
