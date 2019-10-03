b70eaed9232dc9fd1edfbc24520e38973a786a7a
Joe Stringer <joe@cilium.io>
2019-10-03 13:44:59 -0700

    bpf: Fix sockops compile on newer LLVM
    Older versions of Clang/LLVM (7.0 and earlier) compile this line into a
    32-bit read followed by a shift:
    
      r8 = *(u32 *)(r6 + 64)
      r8 >>= 16
    
    Newer versions of Clang/LLVM will compile this instead into a single
    16-bit read into an offset from the context like this:
    
      r8 = *(u16 *)(r6 + 66)
    
    Unfortunately, the kernel verifier (at least as of v5.0, possibly newer)
    does not currently support reads into 'struct bpf_sock_ops' at such
    offsets, which leads to verifier errors when attempting to load the
    program when compiled on newer compilers.
    
    Fix it by forcing a 32-bit read and shifting it.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- bpf/sockops/bpf_sockops.h
+++ bpf/sockops/bpf_sockops.h
@@ -61,7 +61,7 @@ static __always_inline void sk_extract4_key(struct bpf_sock_ops *ops,
 	key->family = ENDPOINT_KEY_IPV4;
 
 	key->sport = (bpf_ntohl(ops->local_port) >> 16);
-	key->dport = ops->remote_port >> 16;
+	key->dport = READ_ONCE(ops->remote_port) >> 16;
 }
 
 static __always_inline void sk_msg_extract4_key(struct sk_msg_md *msg,
