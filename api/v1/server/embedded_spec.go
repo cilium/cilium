56dd0a0c4e564684d49038940a6e51a65da54246
Daniel Borkmann <daniel@iogearbox.net>
2019-10-09 23:55:31 +0200

    cilium: add prefilter delete method to openapi
    [ upstream commit d1198f58dbca360825a355a2a5815606e05db013 ]
    
    Rest is auto-generated:
    
      $ make generate-api
      $ make generate-k8s-api
    
    Prep work to fix a regression introduced via #3478.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- api/v1/server/embedded_spec.go
+++ api/v1/server/embedded_spec.go
@@ -1045,6 +1045,39 @@ func init() {
           }
         }
       },
+      "delete": {
+        "tags": [
+          "prefilter"
+        ],
+        "summary": "Delete list of CIDRs",
+        "parameters": [
+          {
+            "$ref": "#/parameters/prefilter-spec"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "Deleted",
+            "schema": {
+              "$ref": "#/definitions/Prefilter"
+            }
+          },
+          "461": {
+            "description": "Invalid CIDR prefix",
+            "schema": {
+              "$ref": "#/definitions/Error"
+            },
+            "x-go-name": "InvalidCIDR"
+          },
+          "500": {
+            "description": "Prefilter delete failed",
+            "schema": {
+              "$ref": "#/definitions/Error"
+            },
+            "x-go-name": "Failure"
+          }
+        }
+      },
       "patch": {
         "tags": [
           "prefilter"
@@ -4095,6 +4128,45 @@ func init() {
           },
           "500": {
             "description": "Prefilter get failed",
+            "schema": {
+              "$ref": "#/definitions/Error"
+            },
+            "x-go-name": "Failure"
+          }
+        }
+      },
+      "delete": {
+        "tags": [
+          "prefilter"
+        ],
+        "summary": "Delete list of CIDRs",
+        "parameters": [
+          {
+            "description": "List of CIDR ranges for filter table",
+            "name": "prefilter-spec",
+            "in": "body",
+            "required": true,
+            "schema": {
+              "$ref": "#/definitions/PrefilterSpec"
+            }
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "Deleted",
+            "schema": {
+              "$ref": "#/definitions/Prefilter"
+            }
+          },
+          "461": {
+            "description": "Invalid CIDR prefix",
+            "schema": {
+              "$ref": "#/definitions/Error"
+            },
+            "x-go-name": "InvalidCIDR"
+          },
+          "500": {
+            "description": "Prefilter delete failed",
             "schema": {
               "$ref": "#/definitions/Error"
             },
