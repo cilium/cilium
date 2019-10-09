cilium/cilium

commit c0c3c213cba91f60f82e71071207836583f55f23

api: Refactor /prefilter API for 1.0
https://github.com/cilium/cilium/issues/3280 We want the API to
be more declarative and to reflect the difference between the
desired configuration and the one in effect.

Signed-off-by: Ray Bejjani <ray@covalent.io>

cilium/cmd/prefilter_delete.go
@@ -38,9 +38,9 @@ func init() {
 }
 
 func deleteFilters(cmd *cobra.Command, args []string) {
-	cl := &models.CIDRList{
+	spec := &models.PrefilterSpec{
 		Revision: int64(revision),
-		List:     cidrs,
+		Deny:     cidrs,
 	}
 	for _, cidr := range cidrs {
 		_, _, err := net.ParseCIDR(cidr)
@@ -48,9 +48,9 @@ func deleteFilters(cmd *cobra.Command, args []string) {
 			Fatalf("Cannot parse CIDR \"%s\": %s", cidr, err)
 		}
 	}
-	if err := client.DeletePrefilter(cl); err != nil {
+	if err := client.DeletePrefilter(spec); err != nil {
 		Fatalf("Cannot delete prefilter: %s", err)
 	} else {
-		fmt.Printf("Deleted %d prefilter entries\n", len(cl.List))
+		fmt.Printf("Deleted %d prefilter entries\n", len(spec.Deny))
 	}
 }

daemon/prefilter.go
@@ -41,71 +41,46 @@ func (h *getPrefilter) Handle(params GetPrefilterParams) middleware.Responder {
 		return apierror.Error(GetPrefilterFailureCode, msg)
 	}
 	list, revision = h.d.preFilter.Dump(list)
-	cl := &models.CIDRList{
+	spec := &models.PrefilterSpec{
 		Revision: revision,
-		List:     list,
+		Deny:     list,
 	}
-	return NewGetPrefilterOK().WithPayload(cl)
+	status := &models.Prefilter{
+		Spec: spec,
+		Status: &models.PrefilterStatus{
+			Realized: spec,
+		},
+	}
+	return NewGetPrefilterOK().WithPayload(status)
 }
 
-type putPrefilter struct {
+type patchPrefilter struct {
 	d *Daemon
 }
 
-// NewPutPrefilterHandler returns new put handler for api
-func NewPutPrefilterHandler(d *Daemon) PutPrefilterHandler {
-	return &putPrefilter{d: d}
+// NewPatchPrefilterHandler returns new patch handler for api
+func NewPatchPrefilterHandler(d *Daemon) PatchPrefilterHandler {
+	return &patchPrefilter{d: d}
 }
 
-func (h *putPrefilter) Handle(params PutPrefilterParams) middleware.Responder {
+func (h *patchPrefilter) Handle(params PatchPrefilterParams) middleware.Responder {
 	var list []net.IPNet
-	cl := params.CidrList
+	spec := params.PrefilterSpec
 	if h.d.preFilter == nil {
 		msg := fmt.Errorf("Prefilter is not enabled in daemon")
-		return apierror.Error(PutPrefilterFailureCode, msg)
+		return apierror.Error(PatchPrefilterFailureCode, msg)
 	}
-	for _, cidrStr := range cl.List {
+	for _, cidrStr := range spec.Deny {
 		_, cidr, err := net.ParseCIDR(cidrStr)
 		if err != nil {
 			msg := fmt.Errorf("Invalid CIDR string %s", cidrStr)
-			return apierror.Error(PutPrefilterInvalidCIDRCode, msg)
+			return apierror.Error(PatchPrefilterInvalidCIDRCode, msg)
 		}
 		list = append(list, *cidr)
 	}
-	err := h.d.preFilter.Insert(cl.Revision, list)
+	err := h.d.preFilter.Insert(spec.Revision, list)
 	if err != nil {
-		return apierror.Error(PutPrefilterFailureCode, err)
+		return apierror.Error(PatchPrefilterFailureCode, err)
 	}
-	return NewPutPrefilterOK()
-}
-
-type deletePrefilter struct {
-	d *Daemon
-}
-
-// NewDeletePrefilterHandler returns new delete handler for api
-func NewDeletePrefilterHandler(d *Daemon) DeletePrefilterHandler {
-	return &deletePrefilter{d: d}
-}
-
-func (h *deletePrefilter) Handle(params DeletePrefilterParams) middleware.Responder {
-	var list []net.IPNet
-	cl := params.CidrList
-	if h.d.preFilter == nil {
-		msg := fmt.Errorf("Prefilter is not enabled in daemon")
-		return apierror.Error(DeletePrefilterFailureCode, msg)
-	}
-	for _, cidrStr := range cl.List {
-		_, cidr, err := net.ParseCIDR(cidrStr)
-		if err != nil {
-			msg := fmt.Errorf("Invalid CIDR string %s", cidrStr)
-			return apierror.Error(DeletePrefilterInvalidCIDRCode, msg)
-		}
-		list = append(list, *cidr)
-	}
-	err := h.d.preFilter.Delete(cl.Revision, list)
-	if err != nil {
-		return apierror.Error(DeletePrefilterFailureCode, err)
-	}
-	return NewDeletePrefilterOK()
+	return NewPatchPrefilterOK()
 }

pkg/client/prefilter.go
@@ -20,7 +20,7 @@ import (
 )
 
 // GetPrefilter returns a list of all CIDR prefixes
-func (c *Client) GetPrefilter() (*models.CIDRList, error) {
+func (c *Client) GetPrefilter() (*models.Prefilter, error) {
 	resp, err := c.Prefilter.GetPrefilter(nil)
 	if err != nil {
 		return nil, Hint(err)
@@ -28,16 +28,39 @@ func (c *Client) GetPrefilter() (*models.CIDRList, error) {
 	return resp.Payload, nil
 }
 
-// PutPrefilter adds a list of CIDR prefixes
-func (c *Client) PutPrefilter(cl *models.CIDRList) error {
-	params := prefilter.NewPutPrefilterParams().WithCidrList(cl)
-	_, err := c.Prefilter.PutPrefilter(params)
-	return Hint(err)
+// PatchPrefilter sets a list of CIDR prefixes
+func (c *Client) PatchPrefilter(spec *models.PrefilterSpec) (*models.Prefilter, error) {
+	params := prefilter.NewPatchPrefilterParams().WithPrefilterSpec(spec)
+	resp, err := c.Prefilter.PatchPrefilter(params)
+	if err != nil {
+		return nil, Hint(err)
+	}
+	return resp.Payload, nil
 }
 
 // DeletePrefilter deletes a list of CIDR prefixes
-func (c *Client) DeletePrefilter(cl *models.CIDRList) error {
-	params := prefilter.NewDeletePrefilterParams().WithCidrList(cl)
-	_, err := c.Prefilter.DeletePrefilter(params)
+func (c *Client) DeletePrefilter(spec *models.PrefilterSpec) error {
+	current, err := c.GetPrefilter()
+	if err != nil {
+		return Hint(err)
+	}
+
+	deleteSet := map[string]bool{}
+	keepList := []string{}
+	for _, delCIDR := range spec.Deny {
+		deleteSet[delCIDR] = true
+	}
+
+	if current.Status != nil && current.Status.Realized != nil {
+		for _, keepCIDR := range current.Status.Realized.Deny {
+			if !deleteSet[keepCIDR] {
+				keepList = append(keepList, keepCIDR)
+			}
+		}
+	}
+
+	update := current.Status.Realized
+	update.Deny = keepList
+	_, err = c.PatchPrefilter(update)
 	return Hint(err)
 }

api/v1/models/prefilter.go
@@ -1,0 +1,105 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package models
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/swag"
+)
+
+// Prefilter Collection of endpoints to be served
+// swagger:model Prefilter
+
+type Prefilter struct {
+
+	// spec
+	Spec *PrefilterSpec `json:"spec,omitempty"`
+
+	// status
+	Status *PrefilterStatus `json:"status,omitempty"`
+}
+
+/* polymorph Prefilter spec false */
+
+/* polymorph Prefilter status false */
+
+// Validate validates this prefilter
+func (m *Prefilter) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.validateSpec(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
+	if err := m.validateStatus(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
+
+func (m *Prefilter) validateSpec(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Spec) { // not required
+		return nil
+	}
+
+	if m.Spec != nil {
+
+		if err := m.Spec.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("spec")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *Prefilter) validateStatus(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Status) { // not required
+		return nil
+	}
+
+	if m.Status != nil {
+
+		if err := m.Status.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("status")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *Prefilter) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *Prefilter) UnmarshalBinary(b []byte) error {
+	var res Prefilter
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}

api/v1/client/prefilter/delete_prefilter_parameters.go
@@ -1,142 +1,0 @@
-// Code generated by go-swagger; DO NOT EDIT.
-
-package prefilter
-
-// This file was generated by the swagger tool.
-// Editing this file might prove futile when you re-run the swagger generate command
-
-import (
-	"net/http"
-	"time"
-
-	"golang.org/x/net/context"
-
-	"github.com/go-openapi/errors"
-	"github.com/go-openapi/runtime"
-	cr "github.com/go-openapi/runtime/client"
-
-	strfmt "github.com/go-openapi/strfmt"
-
-	"github.com/cilium/cilium/api/v1/models"
-)
-
-// NewDeletePrefilterParams creates a new DeletePrefilterParams object
-// with the default values initialized.
-func NewDeletePrefilterParams() *DeletePrefilterParams {
-	var ()
-	return &DeletePrefilterParams{
-
-		timeout: cr.DefaultTimeout,
-	}
-}
-
-// NewDeletePrefilterParamsWithTimeout creates a new DeletePrefilterParams object
-// with the default values initialized, and the ability to set a timeout on a request
-func NewDeletePrefilterParamsWithTimeout(timeout time.Duration) *DeletePrefilterParams {
-	var ()
-	return &DeletePrefilterParams{
-
-		timeout: timeout,
-	}
-}
-
-// NewDeletePrefilterParamsWithContext creates a new DeletePrefilterParams object
-// with the default values initialized, and the ability to set a context for a request
-func NewDeletePrefilterParamsWithContext(ctx context.Context) *DeletePrefilterParams {
-	var ()
-	return &DeletePrefilterParams{
-
-		Context: ctx,
-	}
-}
-
-// NewDeletePrefilterParamsWithHTTPClient creates a new DeletePrefilterParams object
-// with the default values initialized, and the ability to set a custom HTTPClient for a request
-func NewDeletePrefilterParamsWithHTTPClient(client *http.Client) *DeletePrefilterParams {
-	var ()
-	return &DeletePrefilterParams{
-		HTTPClient: client,
-	}
-}
-
-/*DeletePrefilterParams contains all the parameters to send to the API endpoint
-for the delete prefilter operation typically these are written to a http.Request
-*/
-type DeletePrefilterParams struct {
-
-	/*CidrList
-	  List of CIDRs for filter table
-
-	*/
-	CidrList *models.CIDRList
-
-	timeout    time.Duration
-	Context    context.Context
-	HTTPClient *http.Client
-}
-
-// WithTimeout adds the timeout to the delete prefilter params
-func (o *DeletePrefilterParams) WithTimeout(timeout time.Duration) *DeletePrefilterParams {
-	o.SetTimeout(timeout)
-	return o
-}
-
-// SetTimeout adds the timeout to the delete prefilter params
-func (o *DeletePrefilterParams) SetTimeout(timeout time.Duration) {
-	o.timeout = timeout
-}
-
-// WithContext adds the context to the delete prefilter params
-func (o *DeletePrefilterParams) WithContext(ctx context.Context) *DeletePrefilterParams {
-	o.SetContext(ctx)
-	return o
-}
-
-// SetContext adds the context to the delete prefilter params
-func (o *DeletePrefilterParams) SetContext(ctx context.Context) {
-	o.Context = ctx
-}
-
-// WithHTTPClient adds the HTTPClient to the delete prefilter params
-func (o *DeletePrefilterParams) WithHTTPClient(client *http.Client) *DeletePrefilterParams {
-	o.SetHTTPClient(client)
-	return o
-}
-
-// SetHTTPClient adds the HTTPClient to the delete prefilter params
-func (o *DeletePrefilterParams) SetHTTPClient(client *http.Client) {
-	o.HTTPClient = client
-}
-
-// WithCidrList adds the cidrList to the delete prefilter params
-func (o *DeletePrefilterParams) WithCidrList(cidrList *models.CIDRList) *DeletePrefilterParams {
-	o.SetCidrList(cidrList)
-	return o
-}
-
-// SetCidrList adds the cidrList to the delete prefilter params
-func (o *DeletePrefilterParams) SetCidrList(cidrList *models.CIDRList) {
-	o.CidrList = cidrList
-}
-
-// WriteToRequest writes these params to a swagger request
-func (o *DeletePrefilterParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Registry) error {
-
-	if err := r.SetTimeout(o.timeout); err != nil {
-		return err
-	}
-	var res []error
-
-	if o.CidrList == nil {
-		o.CidrList = new(models.CIDRList)
-	}
-
-	if err := r.SetBodyParam(o.CidrList); err != nil {
-		return err
-	}
-
-	if len(res) > 0 {
-		return errors.CompositeValidationError(res...)
-	}
-	return nil
-}

api/v1/client/prefilter/delete_prefilter_responses.go
@@ -1,127 +1,0 @@
-// Code generated by go-swagger; DO NOT EDIT.
-
-package prefilter
-
-// This file was generated by the swagger tool.
-// Editing this file might prove futile when you re-run the swagger generate command
-
-import (
-	"fmt"
-	"io"
-
-	"github.com/go-openapi/runtime"
-
-	strfmt "github.com/go-openapi/strfmt"
-
-	"github.com/cilium/cilium/api/v1/models"
-)
-
-// DeletePrefilterReader is a Reader for the DeletePrefilter structure.
-type DeletePrefilterReader struct {
-	formats strfmt.Registry
-}
-
-// ReadResponse reads a server response into the received o.
-func (o *DeletePrefilterReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
-	switch response.Code() {
-
-	case 200:
-		result := NewDeletePrefilterOK()
-		if err := result.readResponse(response, consumer, o.formats); err != nil {
-			return nil, err
-		}
-		return result, nil
-
-	case 461:
-		result := NewDeletePrefilterInvalidCIDR()
-		if err := result.readResponse(response, consumer, o.formats); err != nil {
-			return nil, err
-		}
-		return nil, result
-
-	case 500:
-		result := NewDeletePrefilterFailure()
-		if err := result.readResponse(response, consumer, o.formats); err != nil {
-			return nil, err
-		}
-		return nil, result
-
-	default:
-		return nil, runtime.NewAPIError("unknown error", response, response.Code())
-	}
-}
-
-// NewDeletePrefilterOK creates a DeletePrefilterOK with default headers values
-func NewDeletePrefilterOK() *DeletePrefilterOK {
-	return &DeletePrefilterOK{}
-}
-
-/*DeletePrefilterOK handles this case with default header values.
-
-Deleted
-*/
-type DeletePrefilterOK struct {
-}
-
-func (o *DeletePrefilterOK) Error() string {
-	return fmt.Sprintf("[DELETE /prefilter][%d] deletePrefilterOK ", 200)
-}
-
-func (o *DeletePrefilterOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
-
-	return nil
-}
-
-// NewDeletePrefilterInvalidCIDR creates a DeletePrefilterInvalidCIDR with default headers values
-func NewDeletePrefilterInvalidCIDR() *DeletePrefilterInvalidCIDR {
-	return &DeletePrefilterInvalidCIDR{}
-}
-
-/*DeletePrefilterInvalidCIDR handles this case with default header values.
-
-Invalid CIDR prefix
-*/
-type DeletePrefilterInvalidCIDR struct {
-	Payload models.Error
-}
-
-func (o *DeletePrefilterInvalidCIDR) Error() string {
-	return fmt.Sprintf("[DELETE /prefilter][%d] deletePrefilterInvalidCIdR  %+v", 461, o.Payload)
-}
-
-func (o *DeletePrefilterInvalidCIDR) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
-
-	// response payload
-	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
-		return err
-	}
-
-	return nil
-}
-
-// NewDeletePrefilterFailure creates a DeletePrefilterFailure with default headers values
-func NewDeletePrefilterFailure() *DeletePrefilterFailure {
-	return &DeletePrefilterFailure{}
-}
-
-/*DeletePrefilterFailure handles this case with default header values.
-
-CIDR deletion failed
-*/
-type DeletePrefilterFailure struct {
-	Payload models.Error
-}
-
-func (o *DeletePrefilterFailure) Error() string {
-	return fmt.Sprintf("[DELETE /prefilter][%d] deletePrefilterFailure  %+v", 500, o.Payload)
-}
-
-func (o *DeletePrefilterFailure) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
-
-	// response payload
-	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
-		return err
-	}
-
-	return nil
-}

api/v1/client/prefilter/get_prefilter_responses.go
@@ -54,7 +54,7 @@ func NewGetPrefilterOK() *GetPrefilterOK {
 Success
 */
 type GetPrefilterOK struct {
-	Payload *models.CIDRList
+	Payload *models.Prefilter
 }
 
 func (o *GetPrefilterOK) Error() string {
@@ -63,7 +63,7 @@ func (o *GetPrefilterOK) Error() string {
 
 func (o *GetPrefilterOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
 
-	o.Payload = new(models.CIDRList)
+	o.Payload = new(models.Prefilter)
 
 	// response payload
 	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
@@ -80,7 +80,7 @@ func NewGetPrefilterFailure() *GetPrefilterFailure {
 
 /*GetPrefilterFailure handles this case with default header values.
 
-CIDR list get failed
+Prefilter get failed
 */
 type GetPrefilterFailure struct {
 	Payload models.Error

api/v1/client/prefilter/patch_prefilter_parameters.go
@@ -1,0 +1,142 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package prefilter
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"net/http"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	cr "github.com/go-openapi/runtime/client"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/cilium/cilium/api/v1/models"
+)
+
+// NewPatchPrefilterParams creates a new PatchPrefilterParams object
+// with the default values initialized.
+func NewPatchPrefilterParams() *PatchPrefilterParams {
+	var ()
+	return &PatchPrefilterParams{
+
+		timeout: cr.DefaultTimeout,
+	}
+}
+
+// NewPatchPrefilterParamsWithTimeout creates a new PatchPrefilterParams object
+// with the default values initialized, and the ability to set a timeout on a request
+func NewPatchPrefilterParamsWithTimeout(timeout time.Duration) *PatchPrefilterParams {
+	var ()
+	return &PatchPrefilterParams{
+
+		timeout: timeout,
+	}
+}
+
+// NewPatchPrefilterParamsWithContext creates a new PatchPrefilterParams object
+// with the default values initialized, and the ability to set a context for a request
+func NewPatchPrefilterParamsWithContext(ctx context.Context) *PatchPrefilterParams {
+	var ()
+	return &PatchPrefilterParams{
+
+		Context: ctx,
+	}
+}
+
+// NewPatchPrefilterParamsWithHTTPClient creates a new PatchPrefilterParams object
+// with the default values initialized, and the ability to set a custom HTTPClient for a request
+func NewPatchPrefilterParamsWithHTTPClient(client *http.Client) *PatchPrefilterParams {
+	var ()
+	return &PatchPrefilterParams{
+		HTTPClient: client,
+	}
+}
+
+/*PatchPrefilterParams contains all the parameters to send to the API endpoint
+for the patch prefilter operation typically these are written to a http.Request
+*/
+type PatchPrefilterParams struct {
+
+	/*PrefilterSpec
+	  List of CIDR ranges for filter table
+
+	*/
+	PrefilterSpec *models.PrefilterSpec
+
+	timeout    time.Duration
+	Context    context.Context
+	HTTPClient *http.Client
+}
+
+// WithTimeout adds the timeout to the patch prefilter params
+func (o *PatchPrefilterParams) WithTimeout(timeout time.Duration) *PatchPrefilterParams {
+	o.SetTimeout(timeout)
+	return o
+}
+
+// SetTimeout adds the timeout to the patch prefilter params
+func (o *PatchPrefilterParams) SetTimeout(timeout time.Duration) {
+	o.timeout = timeout
+}
+
+// WithContext adds the context to the patch prefilter params
+func (o *PatchPrefilterParams) WithContext(ctx context.Context) *PatchPrefilterParams {
+	o.SetContext(ctx)
+	return o
+}
+
+// SetContext adds the context to the patch prefilter params
+func (o *PatchPrefilterParams) SetContext(ctx context.Context) {
+	o.Context = ctx
+}
+
+// WithHTTPClient adds the HTTPClient to the patch prefilter params
+func (o *PatchPrefilterParams) WithHTTPClient(client *http.Client) *PatchPrefilterParams {
+	o.SetHTTPClient(client)
+	return o
+}
+
+// SetHTTPClient adds the HTTPClient to the patch prefilter params
+func (o *PatchPrefilterParams) SetHTTPClient(client *http.Client) {
+	o.HTTPClient = client
+}
+
+// WithPrefilterSpec adds the prefilterSpec to the patch prefilter params
+func (o *PatchPrefilterParams) WithPrefilterSpec(prefilterSpec *models.PrefilterSpec) *PatchPrefilterParams {
+	o.SetPrefilterSpec(prefilterSpec)
+	return o
+}
+
+// SetPrefilterSpec adds the prefilterSpec to the patch prefilter params
+func (o *PatchPrefilterParams) SetPrefilterSpec(prefilterSpec *models.PrefilterSpec) {
+	o.PrefilterSpec = prefilterSpec
+}
+
+// WriteToRequest writes these params to a swagger request
+func (o *PatchPrefilterParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Registry) error {
+
+	if err := r.SetTimeout(o.timeout); err != nil {
+		return err
+	}
+	var res []error
+
+	if o.PrefilterSpec == nil {
+		o.PrefilterSpec = new(models.PrefilterSpec)
+	}
+
+	if err := r.SetBodyParam(o.PrefilterSpec); err != nil {
+		return err
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}

commit 4d0865d45414e0a23ba05c4603f0cec7f2a3e9b5

cilium: re-implement broken delete handler for prefilter
While testing prefilter I noticed that deletions don't work at all,
and they in-fact call the prefilter Insert() handler which is completely
wrong. Turns out that c0c3c213cba9 ("api: Refactor /prefilter API for
1.0") had some wrong assumptions about how prefilter CIDRs are updated.
E.g. pkg/client/prefilter.go would first get the list and diff a keepList
which is then pushed into Insert() assuming the other entries would get
evicted. Undo all this, and properly implement delete handler.

After this fix:

  # ./cilium/cilium prefilter list
  Revision: 2
  4.4.4.4/32
  2.2.2.2/32
  # ./cilium/cilium prefilter delete --cidr="4.4.4.4/32,2.2.2.2/32"
  Deleted 2 prefilter entries
  # ./cilium/cilium prefilter list
  Revision: 3
  #

Fixes: #9382
Related: #3478

Fixes: c0c3c213cba9 ("api: Refactor /prefilter API for 1.0")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

cilium/cmd/prefilter_delete.go
@@ -48,7 +48,7 @@ func deleteFilters(cmd *cobra.Command, args []string) {
 			Fatalf("Cannot parse CIDR \"%s\": %s", cidr, err)
 		}
 	}
-	if err := client.DeletePrefilter(spec); err != nil {
+	if _, err := client.DeletePrefilter(spec); err != nil {
 		Fatalf("Cannot delete prefilter: %s", err)
 	} else {
 		fmt.Printf("Deleted %d prefilter entries\n", len(spec.Deny))

daemon/daemon_main.go
@@ -1405,6 +1405,7 @@ func (d *Daemon) instantiateAPI() *restapi.CiliumAPI {
 
 	// /prefilter/
 	api.PrefilterGetPrefilterHandler = NewGetPrefilterHandler(d)
+	api.PrefilterDeletePrefilterHandler = NewDeletePrefilterHandler(d)
 	api.PrefilterPatchPrefilterHandler = NewPatchPrefilterHandler(d)
 
 	// /ipam/{ip}/

daemon/prefilter.go
@@ -84,3 +84,34 @@ func (h *patchPrefilter) Handle(params PatchPrefilterParams) middleware.Responde
 	}
 	return NewPatchPrefilterOK()
 }
+
+type deletePrefilter struct {
+	d *Daemon
+}
+
+// NewDeletePrefilterHandler returns new patch handler for api
+func NewDeletePrefilterHandler(d *Daemon) DeletePrefilterHandler {
+	return &deletePrefilter{d: d}
+}
+
+func (h *deletePrefilter) Handle(params DeletePrefilterParams) middleware.Responder {
+	var list []net.IPNet
+	spec := params.PrefilterSpec
+	if h.d.preFilter == nil {
+		msg := fmt.Errorf("Prefilter is not enabled in daemon")
+		return api.Error(DeletePrefilterFailureCode, msg)
+	}
+	for _, cidrStr := range spec.Deny {
+		_, cidr, err := net.ParseCIDR(cidrStr)
+		if err != nil {
+			msg := fmt.Errorf("Invalid CIDR string %s", cidrStr)
+			return api.Error(DeletePrefilterInvalidCIDRCode, msg)
+		}
+		list = append(list, *cidr)
+	}
+	err := h.d.preFilter.Delete(spec.Revision, list)
+	if err != nil {
+		return api.Error(DeletePrefilterFailureCode, err)
+	}
+	return NewDeletePrefilterOK()
+}

pkg/client/prefilter.go
@@ -40,28 +40,11 @@ func (c *Client) PatchPrefilter(spec *models.PrefilterSpec) (*models.Prefilter, 
 }
 
 // DeletePrefilter deletes a list of CIDR prefixes
-func (c *Client) DeletePrefilter(spec *models.PrefilterSpec) error {
-	current, err := c.GetPrefilter()
+func (c *Client) DeletePrefilter(spec *models.PrefilterSpec) (*models.Prefilter, error) {
+	params := prefilter.NewDeletePrefilterParams().WithPrefilterSpec(spec).WithTimeout(api.ClientTimeout)
+	resp, err := c.Prefilter.DeletePrefilter(params)
 	if err != nil {
-		return Hint(err)
+		return nil, Hint(err)
 	}
-
-	deleteSet := map[string]bool{}
-	keepList := []string{}
-	for _, delCIDR := range spec.Deny {
-		deleteSet[delCIDR] = true
-	}
-
-	if current.Status != nil && current.Status.Realized != nil {
-		for _, keepCIDR := range current.Status.Realized.Deny {
-			if !deleteSet[keepCIDR] {
-				keepList = append(keepList, keepCIDR)
-			}
-		}
-	}
-
-	update := current.Status.Realized
-	update.Deny = keepList
-	_, err = c.PatchPrefilter(update)
-	return Hint(err)
+	return resp.Payload, nil
 }