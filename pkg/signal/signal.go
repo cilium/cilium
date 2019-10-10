1a19a8bb09f7cb354ad6e1b3a9569ff387aa3682
Daniel Borkmann <daniel@iogearbox.net>
2019-10-10 14:58:48 +0200

    cilium: add signal package for handling BPF datapath signals
    Instead of reworking CT GC timing heuristics as in PR attempt #9366,
    we can properly address the issue of needing to trigger the GC by
    having the BPF datapath tell us of a potential fill up of the NAT
    table.
    
    This is a much much cleaner approach as we get the feedback right
    where we need it instead of periodically polling which might happen
    too late or uselessly often. Just let the BPF datapath wake up the
    CT GC instead! Add a signal package to receive events from BPF and
    plug a channel into the GC's select for custom wakeups along with
    meta data to know which of the maps (v4 or v6) we need to clean.
    
    Fixes: #8740
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

---
+++ pkg/signal/signal.go
@@ -0,0 +1,153 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package signal
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"runtime"
+	"sync"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
+
+const (
+	// SignalMapName is the BPF map name
+	SignalMapName = "cilium_signals"
+)
+
+const (
+	// SignalNatFillUp denotes potential congestion on the NAT table
+	SignalNatFillUp = iota
+	SignalTypeMax
+)
+
+const (
+	// SignalNatV4 denotes NAT IPv4 table
+	SignalNatV4 = iota
+	// SignalNatV6 denotes NAT IPv6 table
+	SignalNatV6
+)
+
+var (
+	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "signal")
+
+	config = bpf.PerfEventConfig{
+		MapName:      SignalMapName,
+		Type:         bpf.PERF_TYPE_SOFTWARE,
+		Config:       bpf.PERF_COUNT_SW_BPF_OUTPUT,
+		SampleType:   bpf.PERF_SAMPLE_RAW,
+		NumPages:     2,
+		WakeupEvents: 1,
+	}
+
+	channels [SignalTypeMax]chan<- int
+)
+
+// SignalMsg is the message we receive from BPF datapath
+type SignalMsg struct {
+	Which uint32
+	Data  uint32
+}
+
+func signalReceive(msg *bpf.PerfEventSample, cpu int) {
+	sig := SignalMsg{}
+	if err := binary.Read(bytes.NewReader(msg.DataDirect()), byteorder.Native, &sig); err != nil {
+		log.WithError(err).Warningf("Cannot parse signal from BPF datapath")
+		return
+	}
+	if channels[sig.Which] != nil {
+		channels[sig.Which] <- int(sig.Data)
+	}
+}
+
+func signalLost(lost *bpf.PerfEventLost, cpu int) {
+}
+
+func signalError(err *bpf.PerfEvent) {
+}
+
+// MuteChannel tells to not send any new events to a particular channel
+// for a given signal.
+func MuteChannel(signal int) error {
+	if signal != SignalNatFillUp {
+		return fmt.Errorf("Signal number not supported: %d", signal)
+	}
+	if events != nil {
+		events.Mute()
+	}
+	return nil
+}
+
+// UnmuteChannel tells to allow sending new events to a particular channel
+// for a given signal.
+func UnmuteChannel(signal int) error {
+	if signal != SignalNatFillUp {
+		return fmt.Errorf("Signal number not supported: %d", signal)
+	}
+	if events != nil {
+		events.Unmute()
+	}
+	return nil
+}
+
+// RegisterChannel registers a go channel for a given signal.
+func RegisterChannel(signal int, ch chan<- int) error {
+	if signal >= SignalTypeMax {
+		return fmt.Errorf("Signal number not supported: %d", signal)
+	}
+	if channels[signal] != nil {
+		return fmt.Errorf("Channel for signal number already registered: %d", signal)
+	}
+
+	channels[signal] = ch
+	return nil
+}
+
+var once sync.Once
+var events *bpf.PerCpuEvents
+
+// SetupSignalListener bootstraps signal listener infrastructure.
+func SetupSignalListener() {
+	once.Do(func() {
+		var err error
+		config.NumCpus = runtime.NumCPU()
+		events, err = bpf.NewPerCpuEvents(&config)
+		if err != nil {
+			log.WithError(err).Warningf("Cannot open %s map! Ignoring signals!",
+				SignalMapName)
+			return
+		}
+
+		go func() {
+			log.Info("Datapath signal listener running")
+			for {
+				todo, err := events.Poll(-1)
+				if err != nil {
+					log.WithError(err).Warningf("%s poll error!",
+						SignalMapName)
+					continue
+				}
+				if todo > 0 {
+					events.ReadAll(signalReceive, signalLost, signalError)
+				}
+			}
+		}()
+	})
+}
