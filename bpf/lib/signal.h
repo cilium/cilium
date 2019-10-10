04b6bbef971c787b9293338bac11c6916891bdf9
Daniel Borkmann <daniel@iogearbox.net>
2019-10-10 12:16:43 +0200

    bpf: let nat signal potential congestion to cilium agent
    [ upstream commit 969ee00ca818165ff2e86262fa26eaac647bbf1c ]
    
    If we end up being under pressure finding a free NAT mapping let
    user space know and trigger a GC cleanup run in the hope to free
    up timed out CT entries with associated mappings.
    
    During the GC runs, Cilium agent can temporarily turn off new perf
    RB notifications by just removing itself from the map. Meaning,
    __bpf_perf_event_output() in the kernel which is invoked by the
    helper checks array->ptrs[index] and if nothing present it drops
    the notification without writing to the ring buffer.
    
    Also much cleaner than an extra single entry map which holds state
    for muting/unmuting the perf RB notifications.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

---
+++ bpf/lib/signal.h
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (C) 2019 Authors of Cilium
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef __LIB_SIGNAL_H_
+#define __LIB_SIGNAL_H_
+
+#include <bpf/api.h>
+
+struct bpf_elf_map __section_maps SIGNAL_MAP = {
+	.type		= BPF_MAP_TYPE_PERF_EVENT_ARRAY,
+	.size_key	= sizeof(__u32),
+	.size_value	= sizeof(__u32),
+	.pinning	= PIN_GLOBAL_NS,
+	.max_elem	= __NR_CPUS__,
+};
+
+enum {
+	SIGNAL_NAT_FILL_UP = 0,
+};
+
+enum {
+	SIGNAL_NAT_PROTO_V4 = 0,
+	SIGNAL_NAT_PROTO_V6,
+};
+
+struct signal_msg {
+	__u32 signal_nr;
+	union {
+		struct {
+			__u32 proto;
+		};
+	};
+};
+
+static inline void send_signal(struct __sk_buff *skb, struct signal_msg *msg)
+{
+	skb_event_output(skb, &SIGNAL_MAP, BPF_F_CURRENT_CPU,
+			 msg, sizeof(*msg));
+}
+
+static inline void send_signal_nat_fill_up(struct __sk_buff *skb, __u32 proto)
+{
+	struct signal_msg msg = {
+		.signal_nr	= SIGNAL_NAT_FILL_UP,
+		.proto		= proto,
+	};
+
+	send_signal(skb, &msg);
+}
+
+#endif /* __LIB_SIGNAL_H_ */
