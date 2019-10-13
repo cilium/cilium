7e7595a2d5bb9cfe7cfefb685063b9bf04064fe0
Deepesh Pathak <deepshpathak@gmail.com>
2019-10-13 19:05:00 +0530

    policy: add k8s controller to daemon for clusterwide policies
    * Update k8s_watcher.go to include controller to watch for
    CiliumClusterwideNetworkPolicy CRD.
    * Update ExtractNamespace function in k8s utils to account for the fact
    that any object with empty namespace is a cluster scoped object and we
    should not automatically add default namespace to such objects.
    
    Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

--- pkg/k8s/utils/utils.go
+++ pkg/k8s/utils/utils.go
@@ -21,17 +21,35 @@ import (
 )
 
 // ExtractNamespace extracts the namespace of ObjectMeta.
+// For cluster scoped objects the Namespace field is empty and this function
+// assumes that the object is returned from kubernetes itself implying that
+// the namespace is empty only and only when the Object is cluster scoped
+// and thus returns empty namespace for such objects.
 func ExtractNamespace(np metav1.Object) string {
+	return np.GetNamespace()
+}
+
+// ExtractNamespaceOrDefault extracts the namespace of ObjectMeta, it returns default
+// namespace if the namespace field in the ObjectMeta is empty.
+func ExtractNamespaceOrDefault(np metav1.Object) string {
 	ns := np.GetNamespace()
 	if ns == "" {
 		return v1.NamespaceDefault
 	}
+
 	return ns
 }
 
 // GetObjNamespaceName returns the object's namespace and name.
+// If the object is cluster scoped then the function returns only the object name
+// without any namespace prefix.
 func GetObjNamespaceName(obj metav1.Object) string {
-	return ExtractNamespace(obj) + "/" + obj.GetName()
+	ns := ExtractNamespace(obj)
+	if ns == "" {
+		return obj.GetName()
+	}
+
+	return ns + "/" + obj.GetName()
 }
 
 // GetObjUID returns the object's namespace and name.
