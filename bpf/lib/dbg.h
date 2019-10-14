5b1caf0aee300549b0812f03d941bbed1ae95fc1
Joe Stringer <joe@cilium.io>
2019-10-14 16:38:57 -0700

    bpf: Repurpose upper bits of cap_len for version
    The cap_len in practice was previously always TRACE_PAYLOAD_LEN if a
    notification message is emitted, which by default is 128 and was not
    known to exceed 65535 bytes. By repurposing the upper 16 bits of this
    field, we can now inject a version of the message which allows clients
    to support extensions of the trace notify header to accommodate new
    metadata.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- bpf/lib/dbg.h
+++ bpf/lib/dbg.h
@@ -208,6 +208,7 @@ static inline void cilium_dbg_capture2(struct __sk_buff *skb, __u8 type, __u32 a
 		.hash = hash,
 		.len_orig = skb_len,
 		.len_cap = cap_len,
+		.version = NOTIFY_CAPTURE_VER,
 		.arg1 = arg1,
 		.arg2 = arg2,
 	};
