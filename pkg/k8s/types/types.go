14c85d4e5cf8e3ad4c6cdb1aec1fc118103a47a7
André Martins <andre@cilium.io>
2019-10-16 12:52:59 +0200

    pkg/k8s: consider node taints as part of node equalness
    [ upstream commit 9b22d4e4f1d92db4e7feaffbb5631f99fb1ba898 ]
    
    As kube-controller-manager can change node taints to notify if the node
    is or is not reachable, Cilium should also take into account those
    taints. In the eventuality of a node to be down for more than 15 minutes
    it can be removed from the kvstore due lease expiration. That same node
    won't be re-added because Cilium is only checking for the equalness of
    annotations and node name. Checking for Taints equalness will make sure
    the node will be re-added to the kvstore.
    
    Signed-off-by: André Martins <andre@cilium.io>
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- pkg/k8s/types/types.go
+++ pkg/k8s/types/types.go
@@ -64,6 +64,7 @@ type Node struct {
 	Type            v1.NodeAddressType
 	StatusAddresses []v1.NodeAddress
 	SpecPodCIDR     string
+	SpecTaints      []v1.Taint
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
