1368913f27ec3190d077153f472a6e4d1a39d4aa
Deepesh Pathak <deepshpathak@gmail.com>
2019-10-30 21:14:11 +0530

    policy: fix clusterwide policy status update issue for k8s<1.13
    Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

--- pkg/k8s/apis/cilium.io/v2/types.go
+++ pkg/k8s/apis/cilium.io/v2/types.go
@@ -295,7 +295,14 @@ type CiliumNetworkPolicyList struct {
 // CiliumClusterwideNetworkPolicy is a Kubernetes third-party resource with an modified version
 // of CiliumNetworkPolicy which is cluster scoped rather than namespace scoped.
 type CiliumClusterwideNetworkPolicy struct {
-	CiliumNetworkPolicy
+	*CiliumNetworkPolicy
+
+	// Status is the status of the Cilium policy rule
+	// +optional
+	// The reason this field exists in this structure is due a bug in the k8s code-generator
+	// that doesn't create a `UpdateStatus` method because the field does not exist in
+	// the structure.
+	Status CiliumNetworkPolicyStatus `json:"status"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
