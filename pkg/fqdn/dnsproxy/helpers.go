1121202121f72f26bfbbd6a9a356084383a43a00
Ray Bejjani <ray@isovalent.com>
2019-11-09 16:46:10 +0100

    fqdn: L3-aware L7 DNS policy enforcement
    The DNS proxy now accounts for the target L3 selector and destination
    port, only allowing requests whilelisted specifically for that L3/L4
    pair. ipcache lookups iterate over in-use prefix lengths, which will
    likely be bounded in most scenarios.
    Regexpmap is also removed as the proxy was the final user.
    
    Signed-off-by: Ray Bejjani <ray@isovalent.com>

--- pkg/fqdn/dnsproxy/helpers.go
+++ pkg/fqdn/dnsproxy/helpers.go
@@ -15,6 +15,8 @@
 package dnsproxy
 
 import (
+	"fmt"
+	"net"
 	"strings"
 
 	"github.com/miekg/dns"
@@ -43,6 +45,13 @@ func prepareNameMatch(name string) string {
 // lookupTargetDNSServer finds the intended DNS target server for a specific
 // request (passed in via ServeDNS). The IP:port combination is
 // returned.
-func lookupTargetDNSServer(w dns.ResponseWriter) (server string, err error) {
-	return w.LocalAddr().String(), nil
+func lookupTargetDNSServer(w dns.ResponseWriter) (serverIP net.IP, serverPort uint16, addrStr string, err error) {
+	switch addr := (w.LocalAddr()).(type) {
+	case *net.UDPAddr:
+		return addr.IP, uint16(addr.Port), addr.String(), nil
+	case *net.TCPAddr:
+		return addr.IP, uint16(addr.Port), addr.String(), nil
+	default:
+		return nil, 0, addr.String(), fmt.Errorf("Cannot extract address information for type %T: %+v", addr, addr)
+	}
 }
