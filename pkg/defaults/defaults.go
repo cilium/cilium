f5a59fe5a8c5cc7effd8dc5d676f11677cb03d34
Ray Bejjani <ray@isovalent.com>
2019-11-23 14:54:02 +0100

    daemon: Reduce default toFQDNs min TTL values
    These previously also had to account for connection lifetimes to the IPs
    in the DNS responses. We now defer deleting IPs until CT GC marks them
    unused so these times can be reduced considerably.
    
    Signed-off-by: Ray Bejjani <ray@isovalent.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -90,12 +90,12 @@ const (
 
 	// ToFQDNsMinTTL is the default lower bound for TTLs used with ToFQDNs rules.
 	// This or ToFQDNsMinTTLPoller is used in DaemonConfig.Populate
-	ToFQDNsMinTTL = 604800 // 1 week in seconds
+	ToFQDNsMinTTL = 3600 // 1 hour in seconds
 
 	// ToFQDNsMinTTLPoller is the default lower bound for TTLs used with ToFQDNs
 	// rules when the poller is enabled.
 	// This or ToFQDNsMinTTL is used in DaemonConfig.Populate
-	ToFQDNsMinTTLPoller = 3600 // 1 hour in seconds
+	ToFQDNsMinTTLPoller = 600 // 10 minutes in seconds
 
 	// ToFQDNsMaxIPsPerHost defines the maximum number of IPs to maintain
 	// for each FQDN name in an endpoint's FQDN cache
