3578324b0a2a30a9320cf77abdf8d7143062dbb2
Martynas Pumputis <m@lambda.lt>
2019-12-17 13:21:58 +0100

    WIP: remove /112 ipv6 contraint
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- pkg/defaults/node.go
+++ pkg/defaults/node.go
@@ -19,9 +19,6 @@ import (
 )
 
 const (
-	// IPv6NodeAllocPrefixLen is the length of the prefix used for allocation per node
-	IPv6NodeAllocPrefixLen = 112
-
 	// IPv6NodePrefixLen is the length used to allocate container IPv6 addresses from.
 	IPv6NodePrefixLen = 96
 
