fc1b8bd7053eabe3cc91784f2a46d33c51b02d15
Martynas Pumputis <m@lambda.lt>
2019-12-23 19:36:11 +0200

    test: Schedule test-nodeport-local pod on k8s2
    For the DSR test case, we need to schedule the test-k8s2 (prev.
    test-k8s1) pod on k8s2. Otherwise, a request from the
    client-from-outside Docker container running on k8s1 to the pod
    via k8s2 (sending via k8s1 does not test the DSR) would be dropped
    by the kernel due to a routing loop detection mechanism:
    
    1) k8s2 recv: client-from-outside (192.168.10.10) @ k8s1 -> k8s2:NodePort
    2) k8s2 fwd to k8s1: client-from-outside (192.168.10.10) @ k8s1 -> Pod @ k8s1
    3) k8s1 recv the packet on enp0s8, and has a route
       "192.168.10.0/24 dev $DOCKER_BRIDGE" <- kernel detects a potential loop.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/k8sT/Services.go
+++ test/k8sT/Services.go
@@ -332,8 +332,8 @@ var _ = Describe("K8sServicesTest", func() {
 			url := getURL(k8s1IP, data.Spec.Ports[0].NodePort)
 			doRequestsFromOutsideClient(url, count, true)
 
-			// Checks that requests to k8s1 succeed, while requests to k8s2 are dropped
-			err = kubectl.Get(helpers.DefaultNamespace, "service test-nodeport-local-k8s1").Unmarshal(&data)
+			// Checks that requests to k8s2 succeed, while requests to k8s1 are dropped
+			err = kubectl.Get(helpers.DefaultNamespace, "service test-nodeport-local-k8s2").Unmarshal(&data)
 			Expect(err).Should(BeNil(), "Can not retrieve service")
 
 			url = getURL(k8s1IP, data.Spec.Ports[0].NodePort)
@@ -481,8 +481,6 @@ var _ = Describe("K8sServicesTest", func() {
 				err := kubectl.Get(helpers.DefaultNamespace, "service test-nodeport").Unmarshal(&data)
 				Expect(err).Should(BeNil(), "Can not retrieve service")
 				url := getURL(helpers.K8s1Ip, data.Spec.Ports[0].NodePort)
-				doRequestsFromOutsideClient(url, 10, true)
-				url = getURL(helpers.K8s2Ip, data.Spec.Ports[0].NodePort)
 				doRequestsFromOutsideClient(url, 10, true)
 			})
 		})
