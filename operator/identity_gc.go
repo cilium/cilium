8c2247dbcff9c9439d3fc591e672e5c9381a5d0b
Thomas Graf <thomas@cilium.io>
2019-12-27 16:14:34 +0100

    operator: Improve identity GC logging
    No log line indicated whether the CRD identity GC has been started or not.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- operator/identity_gc.go
+++ operator/identity_gc.go
@@ -33,7 +33,7 @@ var (
 )
 
 func startIdentityGC() {
-	log.Infof("Starting security identity garbage collector with %s interval...", identityGCInterval)
+	log.Infof("Starting kvstore identity garbage collector with %s interval...", identityGCInterval)
 	backend, err := kvstoreallocator.NewKVStoreBackend(cache.IdentitiesPath, "", nil)
 	if err != nil {
 		log.WithError(err).Fatal("Unable to initialize kvstore backend for identity allocation")
