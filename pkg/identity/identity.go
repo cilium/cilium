cilium/cilium

commit f3bbcd8e886d3ddcaa36731fcaaf3d1941758b0e

identity: Use local identities to represent CIDR
This removes the need to allocate identities to represent CIDR via the kvstore.
This restricts policy calculation propogation to local nodes when CIDR
notations change.

The local identity space is also much bigger (24 bits) than the global identity
space (16 bits) and thus allows to represent a lot more unique CIDR
representations.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/identity/identity.go
@@ -35,6 +35,10 @@ type Identity struct {
 	// LabelArray contains the same labels as Labels in a form of a list, used
 	// for faster lookup.
 	LabelArray labels.LabelArray `json:"-"`
+
+	// ReferenceCount counts the number of references pointing to this
+	// identity. This field is used by the owning cache of the identity.
+	ReferenceCount int
 }
 
 // IPIdentityPair is a pairing of an IP and the security identity to which that
@@ -159,4 +163,18 @@ func (pair *IPIdentityPair) PrefixString() string {
 		suffix = fmt.Sprintf("/%d", ones)
 	}
 	return fmt.Sprintf("%s%s", pair.IP.String(), suffix)
+}
+
+// RequiresGlobalIdentity returns true if the label combination requires a
+// global identity
+func RequiresGlobalIdentity(lbls labels.Labels) bool {
+	for _, label := range lbls {
+		switch label.Source {
+		case labels.LabelSourceCIDR, labels.LabelSourceReserved:
+		default:
+			return true
+		}
+	}
+
+	return false
 }

pkg/identity/cidr/identity.go
@@ -1,99 +1,0 @@
-// Copyright 2018 Authors of Cilium
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package cidr
-
-import (
-	"fmt"
-	"net"
-
-	"github.com/cilium/cilium/pkg/identity"
-	"github.com/cilium/cilium/pkg/identity/cache"
-	"github.com/cilium/cilium/pkg/labels"
-	"github.com/cilium/cilium/pkg/labels/cidr"
-	"github.com/cilium/cilium/pkg/logging"
-	"github.com/cilium/cilium/pkg/logging/logfields"
-)
-
-var (
-	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "identity-cidr")
-)
-
-// AllocateCIDRIdentities allocates identities for each of the specified CIDR
-// prefixes so they will be available later during policy resolution.
-//
-// On success, the returned slice will have a 1-to-1 correspondence with the
-// CIDR in the provided prefixes slice, eg the identity for prefixes[0] will
-// be held in identities[0].
-// On failure, attempts to clean up after itself and returns the error.
-func AllocateCIDRIdentities(prefixes []*net.IPNet) (res []*identity.Identity, err error) {
-	if len(prefixes) == 0 {
-		return nil, nil
-	}
-
-	log.Debugf("Attempting to allocate identities for %d prefixes", len(prefixes))
-
-	// Allocate identities for each CIDR.
-	var lbls labels.Labels
-	res = make([]*identity.Identity, len(prefixes))
-	for i, prefix := range prefixes {
-		if prefix == nil {
-			continue
-		}
-		lbls := cidr.GetCIDRLabels(prefix)
-		id, _, err := cache.AllocateIdentity(lbls)
-		if err != nil {
-			// If any identity allocation failed, release existing identities
-			// and log the error.
-			if err2 := cache.ReleaseSlice(res); err2 != nil {
-				log.WithError(err2).Error("Could not recover from error during CIDR identity allocation")
-			}
-			res = nil
-			break
-		}
-		res[i] = id
-	}
-
-	if err == nil {
-		log.Debugf("Allocated identities for %d prefixes", len(res))
-	} else {
-		log.WithError(err).Warningf("Failed to allocate identities for %d prefixes", len(res))
-	}
-
-	if err != nil {
-		err = fmt.Errorf("Failed to allocate identity for %s: %s", lbls.String(), err)
-	}
-	return
-}
-
-// LookupCIDRIdentities finds identities corresponding to each of the specified
-// prefixes. It expects to be able to find all identities.
-// On success, returns a slice of identities with a 1-to-1 correspondence to
-// the specified slice of prefixes, and nil.
-// On error, returns all identities that can be resolved and an error.
-func LookupCIDRIdentities(prefixes []*net.IPNet) (res []*identity.Identity, err error) {
-	res = make([]*identity.Identity, len(prefixes))
-
-	for i, prefix := range prefixes {
-		labels := cidr.GetCIDRLabels(prefix)
-		if id := cache.LookupIdentity(labels); id != nil {
-			res[i] = id
-		} else {
-			err = fmt.Errorf("Unable to find CIDR identity for labels %s", labels)
-			log.Debugf("Cannot locate identity for CIDR %s", prefix.String())
-		}
-	}
-
-	return res, err
-}

daemon/policy.go
@@ -246,12 +246,8 @@ func (d *Daemon) PolicyAdd(rules policyAPI.Rules, opts *AddOptions) (uint64, err
 	// used.
 	if len(removedPrefixes) > 0 {
 		log.WithField("prefixes", removedPrefixes).Debug("Decrementing replaced CIDR refcounts when adding rules")
-		if err := ipcache.ReleaseCIDRs(removedPrefixes); err != nil {
-			log.WithError(err).WithField("prefixes", removedPrefixes).Error(
-				"Failed to release CIDRs in replaced rules during policy add with replace. These may never be released.")
-		} else {
-			_ = d.prefixLengths.Delete(removedPrefixes)
-		}
+		ipcache.ReleaseCIDRs(removedPrefixes)
+		d.prefixLengths.Delete(removedPrefixes)
 	}
 
 	// The rules are added, we can begin ToFQDN DNS polling for them
@@ -313,10 +309,7 @@ func (d *Daemon) PolicyDelete(labels labels.LabelArray) (uint64, error) {
 	// not appropriately performing garbage collection.
 	prefixes := policy.GetCIDRPrefixes(rules)
 	log.WithField("prefixes", prefixes).Debug("Policy deleted via API, found prefixes...")
-	if err := ipcache.ReleaseCIDRs(prefixes); err != nil {
-		log.WithError(err).WithField("prefixes", prefixes).Warn(
-			"Failed to release CIDRs during policy delete")
-	}
+	ipcache.ReleaseCIDRs(prefixes)
 
 	prefixesChanged := d.prefixLengths.Delete(prefixes)
 	if !bpfIPCache.BackedByLPM() && prefixesChanged {

pkg/endpoint/endpoint.go
@@ -1414,7 +1414,7 @@ func (e *Endpoint) LeaveLocked(owner Owner, proxyWaitGroup *completion.WaitGroup
 	}
 
 	if e.SecurityIdentity != nil {
-		err := cache.Release(e.SecurityIdentity)
+		_, err := cache.Release(e.SecurityIdentity)
 		if err != nil {
 			errors = append(errors, fmt.Errorf("unable to release identity: %s", err))
 		}
@@ -1996,7 +1996,7 @@ func (e *Endpoint) identityLabelsChanged(owner Owner, myChangeRev int) error {
 	}
 
 	releaseNewlyAllocatedIdentity := func() {
-		err := cache.Release(identity)
+		_, err := cache.Release(identity)
 		if err != nil {
 			// non fatal error as keys will expire after lease expires but log it
 			elog.WithFields(logrus.Fields{logfields.Identity: identity.ID}).
@@ -2023,7 +2023,7 @@ func (e *Endpoint) identityLabelsChanged(owner Owner, myChangeRev int) error {
 	if e.SecurityIdentity != nil {
 		oldIdentity := e.SecurityIdentity
 		defer func() {
-			err := cache.Release(oldIdentity)
+			_, err := cache.Release(oldIdentity)
 			if err != nil {
 				elog.WithFields(logrus.Fields{logfields.Identity: oldIdentity.ID}).
 					WithError(err).Warn("BUG: Unable to release old endpoint identity")

pkg/identity/cache/allocation_test.go
@@ -134,16 +134,19 @@ func (ias *IdentityAllocatorSuite) TestAllocator(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(isNew, Equals, true)
 
+	// reuse the same identity
 	id1b, isNew, err := AllocateIdentity(lbls1)
 	c.Assert(id1b, Not(IsNil))
 	c.Assert(isNew, Equals, false)
 	c.Assert(err, IsNil)
 	c.Assert(id1a.ID, Equals, id1b.ID)
 
-	err = Release(id1a)
+	released, err := Release(id1a)
 	c.Assert(err, IsNil)
-	err = Release(id1b)
+	c.Assert(released, Equals, false)
+	released, err = Release(id1b)
 	c.Assert(err, IsNil)
+	c.Assert(released, Equals, true)
 
 	id1b, isNew, err = AllocateIdentity(lbls1)
 	c.Assert(id1b, Not(IsNil))
@@ -170,10 +173,56 @@ func (ias *IdentityAllocatorSuite) TestAllocator(c *C) {
 	c.Assert(id1a.ID, Not(Equals), id3.ID)
 	c.Assert(id2.ID, Not(Equals), id3.ID)
 
-	err = Release(id1b)
+	released, err = Release(id1b)
 	c.Assert(err, IsNil)
-	err = Release(id2)
+	c.Assert(released, Equals, true)
+	released, err = Release(id2)
 	c.Assert(err, IsNil)
-	err = Release(id3)
+	c.Assert(released, Equals, true)
+	released, err = Release(id3)
 	c.Assert(err, IsNil)
+	c.Assert(released, Equals, true)
+}
+
+func (ias *IdentityAllocatorSuite) TestLocalAllocationr(c *C) {
+	lbls1 := labels.NewLabelsFromSortedList("cidr:192.0.2.3/32")
+
+	InitIdentityAllocator(dummyOwner{})
+	defer Close()
+	defer IdentityAllocator.DeleteAllKeys()
+
+	id, isNew, err := AllocateIdentity(lbls1)
+	c.Assert(id, Not(IsNil))
+	c.Assert(err, IsNil)
+	c.Assert(isNew, Equals, true)
+	c.Assert(id.ID.HasLocalScope(), Equals, true)
+
+	// reuse the same identity
+	id, isNew, err = AllocateIdentity(lbls1)
+	c.Assert(id, Not(IsNil))
+	c.Assert(err, IsNil)
+	c.Assert(isNew, Equals, false)
+
+	cache := GetIdentityCache()
+	c.Assert(cache[id.ID], Not(IsNil))
+
+	released, err := Release(id)
+	c.Assert(err, IsNil)
+	c.Assert(released, Equals, false)
+	released, err = Release(id)
+	c.Assert(err, IsNil)
+	c.Assert(released, Equals, true)
+
+	cache = GetIdentityCache()
+	c.Assert(cache[id.ID], IsNil)
+
+	id, isNew, err = AllocateIdentity(lbls1)
+	c.Assert(id, Not(IsNil))
+	c.Assert(err, IsNil)
+	c.Assert(isNew, Equals, true)
+	c.Assert(id.ID.HasLocalScope(), Equals, true)
+
+	released, err = Release(id)
+	c.Assert(err, IsNil)
+	c.Assert(released, Equals, true)
 }

pkg/identity/cache/allocator.go
@@ -56,6 +56,8 @@ var (
 	// kvstore.
 	IdentityAllocator *allocator.Allocator
 
+	localIdentities *localIdentityCache
+
 	// IdentitiesPath is the path to where identities are stored in the key-value
 	// store.
 	IdentitiesPath = path.Join(kvstore.BaseKeyPrefix, "state", "identities", "v1")
@@ -111,6 +113,8 @@ func InitIdentityAllocator(owner IdentityAllocatorOwner) {
 	}
 
 	IdentityAllocator = a
+	localIdentities = newLocalIdentityCache(1, 0xFFFFFF, events)
+
 }
 
 // Close closes the identity allocator and allows to call
@@ -126,6 +130,7 @@ func Close() {
 	IdentityAllocator.Delete()
 	watcher.stop()
 	IdentityAllocator = nil
+	localIdentities = nil
 }
 
 // WaitForInitialIdentities waits for the initial set of security identities to
@@ -166,6 +171,10 @@ func AllocateIdentity(lbls labels.Labels) (*identity.Identity, bool, error) {
 		return reservedIdentity, false, nil
 	}
 
+	if !identity.RequiresGlobalIdentity(lbls) && localIdentities != nil {
+		return localIdentities.lookupOrCreate(lbls)
+	}
+
 	if IdentityAllocator == nil {
 		return nil, false, fmt.Errorf("allocator not initialized")
 	}
@@ -187,14 +196,19 @@ func AllocateIdentity(lbls labels.Labels) (*identity.Identity, bool, error) {
 // Release is the reverse operation of AllocateIdentity() and releases the
 // identity again. This function may result in kvstore operations.
 // After the last user has released the ID, the returned lastUse value is true.
-func Release(id *identity.Identity) error {
+func Release(id *identity.Identity) (bool, error) {
+	if id.IsReserved() {
+		return false, nil
+	}
+
 	// Ignore reserved identities.
-	if identity.LookupReservedIdentity(id.ID) != nil {
-		return nil
+	if !identity.RequiresGlobalIdentity(id.Labels) && localIdentities != nil {
+		released := localIdentities.release(id)
+		return released, nil
 	}
 
 	if IdentityAllocator == nil {
-		return fmt.Errorf("allocator not initialized")
+		return false, fmt.Errorf("allocator not initialized")
 	}
 
 	return IdentityAllocator.Release(globalIdentity{id.Labels})
@@ -210,7 +224,7 @@ func ReleaseSlice(identities []*identity.Identity) error {
 		if id == nil {
 			continue
 		}
-		if err2 := Release(id); err2 != nil {
+		if _, err2 := Release(id); err2 != nil {
 			log.WithError(err2).WithFields(logrus.Fields{
 				logfields.Identity: id,
 			}).Error("Failed to release identity")

pkg/identity/cache/cache.go
@@ -65,6 +65,10 @@ func GetIdentityCache() IdentityCache {
 		cache[key] = identity.Labels.LabelArray()
 	}
 
+	for _, identity := range localIdentities.GetIdentities() {
+		cache[identity.ID] = identity.Labels.LabelArray()
+	}
+
 	return cache
 }
 
@@ -81,6 +85,10 @@ func GetIdentities() IdentitiesModel {
 	})
 	// append user reserved identities
 	for _, v := range identity.ReservedIdentityCache {
+		identities = append(identities, v.GetModel())
+	}
+
+	for _, v := range localIdentities.GetIdentities() {
 		identities = append(identities, v.GetModel())
 	}
 
@@ -137,6 +145,10 @@ func (w *identityWatcher) stop() {
 func LookupIdentity(lbls labels.Labels) *identity.Identity {
 	if reservedIdentity := LookupReservedIdentityByLabels(lbls); reservedIdentity != nil {
 		return reservedIdentity
+	}
+
+	if identity := localIdentities.lookup(lbls); identity != nil {
+		return identity
 	}
 
 	if IdentityAllocator == nil {
@@ -206,6 +218,10 @@ func LookupIdentityByID(id identity.NumericIdentity) *identity.Identity {
 
 	if IdentityAllocator == nil {
 		return nil
+	}
+
+	if identity := localIdentities.lookupByID(id); identity != nil {
+		return identity
 	}
 
 	allocatorKey, err := IdentityAllocator.GetByID(idpool.ID(id))

pkg/identity/cache/local.go
@@ -1,0 +1,188 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cache
+
+import (
+	"fmt"
+
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/kvstore"
+	"github.com/cilium/cilium/pkg/kvstore/allocator"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+type localIdentityCache struct {
+	mutex               lock.RWMutex
+	identitiesByID      map[identity.NumericIdentity]*identity.Identity
+	identitiesByLabels  map[string]*identity.Identity
+	nextNumericIdentity identity.NumericIdentity
+	minID               identity.NumericIdentity
+	maxID               identity.NumericIdentity
+	events              allocator.AllocatorEventChan
+}
+
+func newLocalIdentityCache(minID, maxID identity.NumericIdentity, events allocator.AllocatorEventChan) *localIdentityCache {
+	return &localIdentityCache{
+		identitiesByID:      map[identity.NumericIdentity]*identity.Identity{},
+		identitiesByLabels:  map[string]*identity.Identity{},
+		nextNumericIdentity: minID,
+		minID:               minID,
+		maxID:               maxID,
+		events:              events,
+	}
+}
+
+func (l *localIdentityCache) bumpNextNumericIdentity() {
+	if l.nextNumericIdentity == l.maxID {
+		l.nextNumericIdentity = l.minID
+	} else {
+		l.nextNumericIdentity++
+	}
+}
+
+// getNextFreeNumericIdentity returns the next available numeric identity or an error
+// The l.mutex must be held
+func (l *localIdentityCache) getNextFreeNumericIdentity() (identity.NumericIdentity, error) {
+	firstID := l.nextNumericIdentity
+	for {
+		idCandidate := l.nextNumericIdentity | identity.LocalIdentityFlag
+		if _, ok := l.identitiesByID[idCandidate]; !ok {
+			l.bumpNextNumericIdentity()
+			return idCandidate, nil
+		}
+
+		l.bumpNextNumericIdentity()
+		if l.nextNumericIdentity == firstID {
+			return 0, fmt.Errorf("out of local identity space")
+		}
+	}
+}
+
+// lookupOrCreate searches for the existence of a local identity with the given
+// labels. If it exists, the reference count is incremented and the identity is
+// returned. If it does not exist, a new identity is created with a unique
+// numeric identity. All identities returned by lookupOrCreate() must be
+// released again via localIdentityCache.release().
+func (l *localIdentityCache) lookupOrCreate(lbls labels.Labels) (*identity.Identity, bool, error) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	stringRepresentation := string(lbls.SortedList())
+	if id, ok := l.identitiesByLabels[stringRepresentation]; ok {
+		id.ReferenceCount++
+		return id, false, nil
+	}
+
+	numericIdentity, err := l.getNextFreeNumericIdentity()
+	if err != nil {
+		return nil, false, err
+	}
+
+	id := &identity.Identity{
+		ID:             numericIdentity,
+		Labels:         lbls,
+		LabelArray:     lbls.LabelArray(),
+		ReferenceCount: 1,
+	}
+
+	l.identitiesByLabels[stringRepresentation] = id
+	l.identitiesByID[numericIdentity] = id
+
+	if l.events != nil {
+		l.events <- allocator.AllocatorEvent{
+			Typ: kvstore.EventTypeCreate,
+		}
+	}
+
+	return id, true, nil
+}
+
+// release releases a local identity from the cache. true is returned when the
+// last use of the identity has been released and the identity has been
+// forgotten.
+func (l *localIdentityCache) release(id *identity.Identity) bool {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	stringRepresentation := string(id.Labels.SortedList())
+	if id, ok := l.identitiesByLabels[stringRepresentation]; ok {
+		switch {
+		case id.ReferenceCount > 1:
+			id.ReferenceCount--
+			return false
+
+		case id.ReferenceCount == 1:
+			// Release is only attempted once, when the reference count is
+			// hitting the last use
+			stringRepresentation := string(id.Labels.SortedList())
+			delete(l.identitiesByLabels, stringRepresentation)
+			delete(l.identitiesByID, id.ID)
+
+			if l.events != nil {
+				l.events <- allocator.AllocatorEvent{
+					Typ: kvstore.EventTypeDelete,
+				}
+			}
+
+			return true
+		}
+	}
+
+	return false
+}
+
+// lookup searches for a local identity matching the given labels and returns
+// it. If found, the reference count is NOT incremented and thus release must
+// NOT be called.
+func (l *localIdentityCache) lookup(lbls labels.Labels) *identity.Identity {
+	l.mutex.RLock()
+	defer l.mutex.RUnlock()
+
+	stringRepresentation := string(lbls.SortedList())
+	if id, ok := l.identitiesByLabels[stringRepresentation]; ok {
+		return id
+	}
+
+	return nil
+}
+
+// lookupByID searches for a local identity matching the given ID and returns
+// it. If found, the reference count is NOT incremented and thus release must
+// NOT be called.
+func (l *localIdentityCache) lookupByID(id identity.NumericIdentity) *identity.Identity {
+	l.mutex.RLock()
+	defer l.mutex.RUnlock()
+
+	if id, ok := l.identitiesByID[id]; ok {
+		return id
+	}
+
+	return nil
+}
+
+// GetIdentities returns all local identities
+func (l *localIdentityCache) GetIdentities() map[identity.NumericIdentity]*identity.Identity {
+	cache := map[identity.NumericIdentity]*identity.Identity{}
+
+	l.mutex.RLock()
+	defer l.mutex.RUnlock()
+
+	for key, id := range l.identitiesByID {
+		cache[key] = id
+	}
+
+	return cache
+}

commit 568a77e1cd64ef58a1b8445eba5bc673ac40edef

identity: require global identity for empty labels
On recent master, I noticed that there's a lack of connectivity when going
over vxlan tunnel. Git bisection pointed me to commit 28dc941544f1 ("bpf:
Map HOST_ID to REMOTE_NODE_ID when encapsulating"). After a closer look,
it turns out however that 28dc941544f1 itself does nothing wrong, and for
tunnel ID the reserved REMOTE_NODE_ID was properly set for traffic going
out of the node via curl from hostns. However, the packets got dropped due
to the identity == HOST_ID check on replies even though they are originating
from a remote container. Turns out those containers had identity of 16777217
which is 0b100000000000000000000001 and therefore get truncated in case
of vxlan tunnel id since it's 24 bits width. Moreover, packets with such
0dab1b63b87c ("identity: Introduce local scope for numeric identity")
identity are only for local scope and not destined to leave the node.
Turns out for empty labels f3bbcd8e886d ("identity: Use local identities
to represent CIDR") RequiresGlobalIdentity() returns false which is not
correct; fix gets connectivity working again.

Fixes: f3bbcd8e886d ("identity: Use local identities to represent CIDR")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/identity/identity.go
@@ -202,15 +202,18 @@ func (pair *IPIdentityPair) PrefixString() string {
 // RequiresGlobalIdentity returns true if the label combination requires a
 // global identity
 func RequiresGlobalIdentity(lbls labels.Labels) bool {
+	needsGlobal := true
+
 	for _, label := range lbls {
 		switch label.Source {
 		case labels.LabelSourceCIDR, labels.LabelSourceReserved:
+			needsGlobal = false
 		default:
 			return true
 		}
 	}
 
-	return false
+	return needsGlobal
 }
 
 // AddUserDefinedNumericIdentitySet adds all key-value pairs from the given map