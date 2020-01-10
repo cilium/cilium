cilium/cilium

commit 2bf35ed2f2a0cd4d9aa25b5c844f261d00440b9c

add correct implementation of k8s externalIPs in datapath
If the user wants to check if the backend address is of externalIP type
he can check the flags field which will be set with `ExternalIPs` if the
service is type of external IP.

```
sudo cilium bpf lb list
SERVICE ADDRESS       BACKEND ADDRESS
192.0.2.233:82        10.16.56.85:80 (22)
                      0.0.0.0:0 (22) [FLAGS: ExternalIPs]
                      10.16.92.10:80 (22)
```

Fixes: 46ae5ef2be6b ("pkg/k8s: add k8s external IPs support")
Signed-off-by: André Martins <andre@cilium.io>

bpf/bpf_sock.c
@@ -210,10 +210,47 @@ int sock4_xlate(struct bpf_sock_addr *ctx)
 	if (!sock_proto_enabled(ctx))
 		return CONNECT_PROCEED;
 
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+	/* We need to check if a possible external IP address exists in the
+	 * service map. This is because kubernetes allows to set host IPs as
+	 * externalIPs.
+	 */
+	key.address = ctx->user_ip4;
+	svc = __lb4_lookup_service(&key);
+
+	if (!svc) {
+		key.address = 0;
+		key.dport = ctx_get_port(ctx);
+		sock4_handle_node_port(ctx, &key);
+		/* We already performed a lookup where key.address is
+		 * ctx->user_ip4. If it was not found then it is not going to
+		 * be found again.
+		 */
+		if (key.address != ctx->user_ip4) {
+			svc = __lb4_lookup_service(&key);
+		}
+	}
+#else
 	sock4_handle_node_port(ctx, &key);
 
 	svc = __lb4_lookup_service(&key);
+#endif  /* ENABLE_K8S_EXTERNAL_IP */
+
 	if (svc) {
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+		/* Do not perform service translation for external IPs that
+		 * are not a local address because we don't want a k8s service
+		 * to easily do MITM attacks for a public IP address. But do the
+		 * service translation if the IP is from the host.
+		 */
+		if (svc->k8s_external) {
+			struct remote_endpoint_info *info;
+			info = ipcache_lookup4(&IPCACHE_MAP, key.address, V4_CACHE_KEY_LEN);
+			if (info == NULL || info->sec_label != HOST_ID)
+				return CONNECT_PROCEED;
+		}
+#endif /* ENABLE_K8S_EXTERNAL_IP */
+
 		key.slave = (sock_local_cookie(ctx) % svc->count) + 1;
 
 		slave_svc = __lb4_lookup_slave(&key);
@@ -253,10 +290,47 @@ int sock4_xlate_snd(struct bpf_sock_addr *ctx)
 	struct lb4_service *svc;
 	struct lb4_service *slave_svc;
 
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+	/* We need to check if a possible external IP address exists in the
+	 * service map. This is because kubernetes allows to set host IPs as
+	 * externalIPs.
+	 */
+	lkey.address = ctx->user_ip4;
+	svc = __lb4_lookup_service(&lkey);
+
+	if (!svc) {
+		lkey.address = 0;
+		lkey.dport = ctx_get_port(ctx);
+		sock4_handle_node_port(ctx, &lkey);
+		/* We already performed a lookup where lkey.address is
+		 * ctx->user_ip4. If it was not found then it is not going to
+		 * be found again.
+		 */
+		if (lkey.address != ctx->user_ip4) {
+			svc = __lb4_lookup_service(&lkey);
+		}
+	}
+#else
 	sock4_handle_node_port(ctx, &lkey);
 
 	svc = __lb4_lookup_service(&lkey);
+#endif  /* ENABLE_K8S_EXTERNAL_IP */
+
 	if (svc) {
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+		/* Do not perform service translation for external IPs that
+		 * are not a local address because we don't want a k8s service
+		 * to easily do MITM attacks for a public IP address. But do the
+		 * service translation if the IP is from the host.
+		 */
+		if (svc->k8s_external) {
+			struct remote_endpoint_info *info;
+			info = ipcache_lookup4(&IPCACHE_MAP, lkey.address, V4_CACHE_KEY_LEN);
+			if (info == NULL || info->sec_label != HOST_ID)
+				return CONNECT_PROCEED;
+		}
+#endif /* ENABLE_K8S_EXTERNAL_IP */
+
 		lkey.slave = (sock_local_cookie(ctx) % svc->count) + 1;
 
 		slave_svc = __lb4_lookup_slave(&lkey);
@@ -432,13 +506,48 @@ int sock6_xlate(struct bpf_sock_addr *ctx)
 	};
 	struct lb6_service *slave_svc;
 
-	if (!sock_proto_enabled(ctx))
-		return CONNECT_PROCEED;
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+	/* We need to check if a possible external IP address exists in the
+	 * service map. This is because kubernetes allows to set host IPs as
+	 * externalIPs.
+	 */
+	ctx_get_v6_address(ctx, &key.address);
+	svc = __lb6_lookup_service(&key);
 
+	if (!svc) {
+		__builtin_memset(&key.address, 0, sizeof(union v6addr));
+		key.dport = ctx_get_port(ctx);
+		sock6_handle_node_port(ctx, &key);
+		/* We already performed a lookup where key.address is
+		 * ctx->user_ip6. If it was not found then it is not going to
+		 * be found again.
+		 */
+		union v6addr user_ip6;
+		ctx_get_v6_address(ctx, &user_ip6);
+		if (ipv6_addrcmp(&key.address, &user_ip6) != 0 ) {
+			svc = __lb6_lookup_service(&key);
+		}
+	}
+#else
 	sock6_handle_node_port(ctx, &key);
 
 	svc = __lb6_lookup_service(&key);
+#endif  /* ENABLE_K8S_EXTERNAL_IP */
+
 	if (svc) {
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+		/* Do not perform service translation for external IPs that
+		 * are not a local address because we don't want a k8s service
+		 * to easily do MITM attacks for a public IP address. But do the
+		 * service translation if the IP is from the host.
+		 */
+		if (svc->k8s_external) {
+			struct remote_endpoint_info *info;
+			info = ipcache_lookup6(&IPCACHE_MAP, &key.address, V6_CACHE_KEY_LEN);
+			if (info == NULL || info->sec_label != HOST_ID)
+				return CONNECT_PROCEED;
+		}
+#endif /* ENABLE_K8S_EXTERNAL_IP */
 		key.slave = (sock_local_cookie(ctx) % svc->count) + 1;
 
 		slave_svc = __lb6_lookup_slave(&key);
@@ -478,10 +587,48 @@ int sock6_xlate_snd(struct bpf_sock_addr *ctx)
 	};
 	struct lb6_service *slave_svc;
 
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+	/* We need to check if a possible external IP address exists in the
+	 * service map. This is because kubernetes allows to set host IPs as
+	 * externalIPs.
+	 */
+	ctx_get_v6_address(ctx, &lkey.address);
+	svc = __lb6_lookup_service(&lkey);
+
+	if (!svc) {
+		__builtin_memset(&lkey.address, 0, sizeof(union v6addr));
+		lkey.dport = ctx_get_port(ctx);
+		sock6_handle_node_port(ctx, &lkey);
+		/* We already performed a lookup where lkey.address is
+		 * ctx->user_ip6. If it was not found then it is not going to
+		 * be found again.
+		 */
+		union v6addr user_ip6;
+		ctx_get_v6_address(ctx, &user_ip6);
+		if (ipv6_addrcmp(&lkey.address, &user_ip6) != 0 ) {
+			svc = __lb6_lookup_service(&lkey);
+		}
+	}
+#else
 	sock6_handle_node_port(ctx, &lkey);
 
 	svc = __lb6_lookup_service(&lkey);
+#endif  /* ENABLE_K8S_EXTERNAL_IP */
+
 	if (svc) {
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+		/* Do not perform service translation for external IPs that
+		 * are not a local address because we don't want a k8s service
+		 * to easily do MITM attacks for a public IP address. But do the
+		 * service translation if the IP is from the host.
+		 */
+		if (svc->k8s_external) {
+			struct remote_endpoint_info *info;
+			info = ipcache_lookup6(&IPCACHE_MAP, &lkey.address, V6_CACHE_KEY_LEN);
+			if (info == NULL || info->sec_label != HOST_ID)
+				return CONNECT_PROCEED;
+		}
+#endif /* ENABLE_K8S_EXTERNAL_IP */
 		lkey.slave = (sock_local_cookie(ctx) % svc->count) + 1;
 
 		slave_svc = __lb6_lookup_slave(&lkey);

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=db3e8a432d908439af689bee5153782e7704cfdb
+GO_BINDATA_SHA1SUM=fa34a8c1f7f12aef60f684acc1c0acd6c5b73e06
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/conntrack_test.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/ipv6_test.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/signal.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_max_insn.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

bpf/Makefile
@@ -45,12 +45,14 @@ LB_OPTIONS = \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_NODEPORT \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP \
-	-DENABLE_IPV4:-DENABLE_IPV6:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT
+	-DENABLE_IPV4:-DENABLE_IPV6:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT \
+	-DENABLE_IPV4:-DENABLE_IPV6:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_K8S_EXTERNAL_IP
 
 # These options are intended to max out the BPF program complexity. it is load
 # tested as well.
 MAX_LB_OPTIONS = -DENABLE_IPV4 -DENABLE_IPV6 -DLB_L3 -DLB_L4 \
-	-DENABLE_HOST_SERVICES_TCP -DENABLE_HOST_SERVICES_TCP -DENABLE_NODEPORT
+	-DENABLE_HOST_SERVICES_TCP -DENABLE_HOST_SERVICES_TCP -DENABLE_NODEPORT \
+	-DENABLE_K8S_EXTERNAL_IP
 
 bpf_sock.ll: bpf_sock.c $(LIB)
 	$(QUIET) set -e; \

bpf/lib/common.h
@@ -561,7 +561,9 @@ struct lb6_service {
 	__u32 backend_id;
 	__u16 count;
 	__u16 rev_nat_index;
-	__u32 pad;
+	__u8 k8s_external:1, /* K8s External IPs */
+	     reserved:7;
+	__u8 pad[3];
 };
 
 /* See lb4_backend comments */
@@ -592,7 +594,9 @@ struct lb4_service {
 	 */
 	__u16 count;
 	__u16 rev_nat_index;	/* Reverse NAT ID in lb4_reverse_nat */
-	__u32 pad;
+	__u8 k8s_external:1, /* K8s External IPs */
+	     reserved:7;
+	__u8 pad[3];
 };
 
 struct lb4_backend {

bpf/lib/nodeport.h
@@ -259,7 +259,25 @@ static inline int nodeport_lb6(struct __sk_buff *skb, __u32 src_identity)
 		else
 			return ret;
 	}
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+	if ((svc = lb6_lookup_service(skb, &key)) != NULL) {
+		ret = lb6_local(get_ct_map6(&tuple), skb, l3_off, l4_off,
+				&csum_off, &key, &tuple, svc, &ct_state_new);
+		if (IS_ERR(ret))
+			return ret;
+	}
 
+	if (svc == NULL || !svc->k8s_external) {
+		service_port = bpf_ntohs(key.dport);
+		if (service_port < NODEPORT_PORT_MIN ||
+		    service_port > NODEPORT_PORT_MAX) {
+                        skb->cb[CB_NAT] = NAT_DIR_INGRESS;
+                        skb->cb[CB_SRC_IDENTITY] = src_identity;
+                        ep_tail_call(skb, CILIUM_CALL_IPV6_NODEPORT_NAT);
+                        return DROP_MISSED_TAIL_CALL;
+		}
+	}
+#else
 	service_port = bpf_ntohs(key.dport);
 	if (service_port < NODEPORT_PORT_MIN ||
 	    service_port > NODEPORT_PORT_MAX) {
@@ -279,6 +297,7 @@ static inline int nodeport_lb6(struct __sk_buff *skb, __u32 src_identity)
 	} else {
 		return TC_ACT_OK;
 	}
+#endif  /* ENABLE_K8S_EXTERNAL_IP */
 
 	ret = ct_lookup6(get_ct_map6(&tuple), &tuple, skb, l4_off, CT_EGRESS,
 			 &ct_state, &monitor);
@@ -631,6 +650,25 @@ static inline int nodeport_lb4(struct __sk_buff *skb, __u32 src_identity)
 			return ret;
 	}
 
+#ifdef ENABLE_K8S_EXTERNAL_IP  /* ENABLE_K8S_EXTERNAL_IP */
+	if ((svc = lb4_lookup_service(skb, &key)) != NULL) {
+		ret = lb4_local(get_ct_map4(&tuple), skb, l3_off, l4_off, &csum_off,
+				&key, &tuple, svc, &ct_state_new, ip4->saddr);
+		if (IS_ERR(ret))
+			return ret;
+	}
+
+	if (svc == NULL || !svc->k8s_external) {
+		service_port = bpf_ntohs(key.dport);
+		if (service_port < NODEPORT_PORT_MIN ||
+		    service_port > NODEPORT_PORT_MAX) {
+                        skb->cb[CB_NAT] = NAT_DIR_INGRESS;
+                        skb->cb[CB_SRC_IDENTITY] = src_identity;
+                        ep_tail_call(skb, CILIUM_CALL_IPV4_NODEPORT_NAT);
+                        return DROP_MISSED_TAIL_CALL;
+		}
+	}
+#else
 	service_port = bpf_ntohs(key.dport);
 	if (service_port < NODEPORT_PORT_MIN ||
 	    service_port > NODEPORT_PORT_MAX) {
@@ -644,12 +682,13 @@ static inline int nodeport_lb4(struct __sk_buff *skb, __u32 src_identity)
 
 	if ((svc = lb4_lookup_service(skb, &key)) != NULL) {
 		ret = lb4_local(get_ct_map4(&tuple), skb, l3_off, l4_off, &csum_off,
-				&key, &tuple, svc, &ct_state_new, ip4->saddr);
+		                &key, &tuple, svc, &ct_state_new, ip4->saddr);
 		if (IS_ERR(ret))
 			return ret;
 	} else {
 		return TC_ACT_OK;
 	}
+#endif  /* ENABLE_K8S_EXTERNAL_IP */
 
 	ret = ct_lookup4(get_ct_map4(&tuple), &tuple, skb, l4_off, CT_EGRESS,
 			 &ct_state, &monitor);

pkg/maps/lbmap/ipv4.go
@@ -198,7 +198,8 @@ type Service4Value struct {
 	BackendID uint32 `align:"backend_id"`
 	Count     uint16 `align:"count"`
 	RevNat    uint16 `align:"rev_nat_index"`
-	Pad       uint32 `align:"pad"`
+	Flags     uint8
+	Pad       pad3uint8 `align:"pad"`
 }
 
 func NewService4Value(count uint16, backendID loadbalancer.BackendID, revNat uint16) *Service4Value {
@@ -212,7 +213,7 @@ func NewService4Value(count uint16, backendID loadbalancer.BackendID, revNat uin
 }
 
 func (s *Service4Value) String() string {
-	return fmt.Sprintf("%d (%d)", s.BackendID, s.RevNat)
+	return fmt.Sprintf("%d (%d) [FLAGS: 0x%x]", s.BackendID, s.RevNat, s.Flags)
 }
 
 func (s *Service4Value) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(s) }
@@ -222,6 +223,8 @@ func (s *Service4Value) GetCount() int        { return int(s.Count) }
 func (s *Service4Value) SetRevNat(id int)     { s.RevNat = uint16(id) }
 func (s *Service4Value) GetRevNat() int       { return int(s.RevNat) }
 func (s *Service4Value) RevNatKey() RevNatKey { return &RevNat4Key{s.RevNat} }
+func (s *Service4Value) SetFlags(flags uint8) { s.Flags = flags }
+func (s *Service4Value) GetFlags() uint8      { return s.Flags }
 
 func (s *Service4Value) SetBackendID(id loadbalancer.BackendID) {
 	s.BackendID = uint32(id)

pkg/maps/lbmap/ipv6.go
@@ -188,7 +188,8 @@ type Service6Value struct {
 	BackendID uint32 `align:"backend_id"`
 	Count     uint16 `align:"count"`
 	RevNat    uint16 `align:"rev_nat_index"`
-	Pad       uint32 `align:"pad"`
+	Flags     uint8
+	Pad       pad3uint8 `align:"pad"`
 }
 
 func NewService6Value(count uint16, backendID loadbalancer.BackendID, revNat uint16) *Service6Value {
@@ -202,7 +203,7 @@ func NewService6Value(count uint16, backendID loadbalancer.BackendID, revNat uin
 }
 
 func (s *Service6Value) String() string {
-	return fmt.Sprintf("%d (%d)", s.BackendID, s.RevNat)
+	return fmt.Sprintf("%d (%d) [FLAGS: 0x%x]", s.BackendID, s.RevNat, s.Flags)
 }
 
 func (s *Service6Value) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(s) }
@@ -212,6 +213,8 @@ func (s *Service6Value) GetCount() int        { return int(s.Count) }
 func (s *Service6Value) SetRevNat(id int)     { s.RevNat = uint16(id) }
 func (s *Service6Value) GetRevNat() int       { return int(s.RevNat) }
 func (s *Service6Value) RevNatKey() RevNatKey { return &RevNat6Key{s.RevNat} }
+func (s *Service6Value) SetFlags(flags uint8) { s.Flags = flags }
+func (s *Service6Value) GetFlags() uint8      { return s.Flags }
 
 func (s *Service6Value) SetBackendID(id loadbalancer.BackendID) {
 	s.BackendID = uint32(id)

pkg/maps/lbmap/types.go
@@ -69,6 +69,12 @@ type ServiceValue interface {
 	// Get reverse NAT identifier
 	GetRevNat() int
 
+	// Set flags
+	SetFlags(uint8)
+
+	// Get flags
+	GetFlags() uint8
+
 	// Set backend identifier
 	SetBackendID(id loadbalancer.BackendID)
 

commit 9c15eda89dd9b9db1f039b39d55a4e0eb9a414d8

bpf: fix sock6_xlate when not all host service protocols are enabled
We have the case in v4, and the v6 one got accidentally removed in
commit 2bf35ed2f2a0 ("add correct implementation of k8s externalIPs
in datapath"). Restore it back.

Fixes: 2bf35ed2f2a0 ("add correct implementation of k8s externalIPs in datapath")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/bpf_sock.c
@@ -499,6 +499,9 @@ int sock6_xlate(struct bpf_sock_addr *ctx)
 	};
 	struct lb6_service *slave_svc;
 
+	if (!sock_proto_enabled(ctx))
+		return SYS_PROCEED;
+
 	ctx_get_v6_address(ctx, &key.address);
 
 	svc = __lb6_lookup_service(&key);

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=f471e2903c645c93efc540568b9e81012375f91f
+GO_BINDATA_SHA1SUM=153de6bc36b9f1b7e4a43560dde22755e1a4494b
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/conntrack_test.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/ipv6_test.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/signal.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_max_insn.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 