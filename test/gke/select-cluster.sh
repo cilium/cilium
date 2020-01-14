bd05148479e965a56225694a7bfaff2e9cef0a12
Maciej Kwiek <maciej@isovalent.com>
2020-01-14 15:36:29 +0100

    [CI] Add GKE jenkinsfile
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

---
+++ test/gke/select-cluster.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+locked=1
+
+zone=europe-west4-a
+export KUBECONFIG=gke-kubeconfig
+
+while [ $locked -ne 0 ]; do
+    rm gke-kubeconfig || true
+    echo "selecting random cluster"
+    cluster=$(gcloud container clusters list --zone europe-west4-a | grep cilium-ci | sort -R | head -n 1 | awk '{print $1}')
+
+    echo "getting kubeconfig for $cluster"
+    gcloud container clusters get-credentials --zone $zone $cluster
+
+    echo "aquiring cluster lock"
+    set +e
+    kubectl create -f lock.yaml
+
+    kubectl annotate deployment lock lock=1
+    locked=$?
+    echo $locked
+    set -e
+done
+
+echo "lock acquired on cluster $cluster"
+echo $cluster > cluster-name
+
+echo "creating cilium ns"
+kubectl create ns cilium || true
+
+echo "scaling $cluster to 2"
+yes | gcloud container clusters resize $cluster --node-pool default-pool --num-nodes 2 --zone $zone
+
+echo "labeling nodes"
+index=1
+for node in $(kubectl get nodes --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}');
+do
+    kubectl label node $node cilium.io/ci-node=k8s$index
+    index=$((index+1))
+done
+
+echo "adding node registry as trusted"
+helm template ../k8sT/manifests/registry-adder-gke --set IP="$(../print-node-ip.sh)" > registry-adder.yaml
+kubectl apply -f registry-adder.yaml
