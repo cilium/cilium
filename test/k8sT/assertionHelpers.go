2f9f80fbe0c08dfda66f7130a9a5d9848131a038
Martynas Pumputis <m@lambda.lt>
2020-01-16 17:55:59 +0100

    test: Introduce NO_CILIUM_ON_NODE env var
    The env var prevents scheduling cilium-agent and any pods running in
    non-host netns on that host.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/k8sT/assertionHelpers.go
+++ test/k8sT/assertionHelpers.go
@@ -61,7 +61,7 @@ func ExpectCiliumOperatorReady(vm *helpers.Kubectl) {
 // ExpectCiliumRunning is a wrapper around helpers/WaitForNPods. It
 // asserts the cilium pods are running on all nodes (but not yet ready!).
 func ExpectCiliumRunning(vm *helpers.Kubectl) {
-	err := vm.WaitforNPodsRunning(helpers.KubeSystemNamespace, "-l k8s-app=cilium", vm.GetNumNodes(), longTimeout)
+	err := vm.WaitforNPodsRunning(helpers.KubeSystemNamespace, "-l k8s-app=cilium", vm.GetNumCiliumNodes(), longTimeout)
 	ExpectWithOffset(1, err).Should(BeNil(), "cilium was not able to get into ready state")
 
 }
