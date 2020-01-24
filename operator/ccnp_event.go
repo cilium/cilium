cilium/cilium

commit 25ef8ab702409b783ded40d419875eea52b6584f

operator: add ccnp event watcher to cilium operator
* Add kvstore status watcher for CiliumClusterwideNetworkPolicies
similar to that of CiliumNetworkPolicies.

Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

operator/ccnp_event.go
@@ -1,0 +1,140 @@
+// Copyright 2018-2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/k8s"
+	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/informer"
+	k8sversion "github.com/cilium/cilium/pkg/k8s/version"
+	"github.com/cilium/cilium/pkg/kvstore/store"
+	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/policy/groups"
+
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+)
+
+var (
+	// ccnpStatusUpdateInterval is the amount of time between status updates
+	// being sent to the K8s apiserver for a given CCNP.
+	ccnpStatusUpdateInterval time.Duration
+)
+
+// enableCCNPWatcher is similar to enableCNPWatcher but handles the watch events for
+// clusterwide policies. Since, internally Clusterwide policies are implemented
+// using CiliumNetworkPolicy itself, the entire implementation uses the methods
+// associcated with CiliumNetworkPolicy.
+func enableCCNPWatcher() error {
+	log.Info("Starting to garbage collect stale CiliumClusterwideNetworkPolicy status field entries...")
+
+	var (
+		ccnpConverterFunc informer.ConvertFunc
+	)
+	ccnpStore := cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
+
+	switch {
+	case k8sversion.Capabilities().Patch:
+		// k8s >= 1.13 does not require a store to update CNP status so
+		// we don't even need to keep the status of a CNP with us.
+		ccnpConverterFunc = k8s.ConvertToCCNP
+	default:
+		ccnpConverterFunc = k8s.ConvertToCCNPWithStatus
+	}
+
+	ccnpSharedStore, err := store.JoinSharedStore(store.Configuration{
+		Prefix: k8s.CCNPStatusesPath,
+		KeyCreator: func() store.Key {
+			return &k8s.CNPNSWithMeta{}
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	ccnpStatusMgr := k8s.NewCCNPStatusEventHandler(ccnpSharedStore, ccnpStore, ccnpStatusUpdateInterval)
+
+	if kvstoreEnabled() {
+		go ccnpStatusMgr.WatchForCCNPStatusEvents()
+	}
+
+	ciliumV2Controller := informer.NewInformerWithStore(
+		cache.NewListWatchFromClient(k8s.CiliumClient().CiliumV2().RESTClient(),
+			"ciliumclusterwidenetworkpolicies", v1.NamespaceAll, fields.Everything()),
+		&cilium_v2.CiliumClusterwideNetworkPolicy{},
+		0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				metrics.EventTSK8s.SetToCurrentTime()
+				if cnp := k8s.CopyObjToV2CNP(obj); cnp != nil {
+					groups.AddDerivativeCNPIfNeeded(cnp.CiliumNetworkPolicy)
+					ccnpStatusMgr.StartStatusHandler(cnp)
+				}
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				metrics.EventTSK8s.SetToCurrentTime()
+				if oldCNP := k8s.CopyObjToV2CNP(oldObj); oldCNP != nil {
+					if newCNP := k8s.CopyObjToV2CNP(newObj); newCNP != nil {
+						if k8s.EqualV2CNP(oldCNP, newCNP) {
+							return
+						}
+						groups.UpdateDerivativeCNPIfNeeded(newCNP.CiliumNetworkPolicy, oldCNP.CiliumNetworkPolicy)
+					}
+				}
+			},
+			DeleteFunc: func(obj interface{}) {
+				metrics.EventTSK8s.SetToCurrentTime()
+				cnp := k8s.CopyObjToV2CNP(obj)
+				if cnp == nil {
+					deletedObj, ok := obj.(cache.DeletedFinalStateUnknown)
+					if !ok {
+						return
+					}
+					// Delete was not observed by the watcher but is
+					// removed from kube-apiserver. This is the last
+					// known state and the object no longer exists.
+					cnp = k8s.CopyObjToV2CNP(deletedObj.Obj)
+					if cnp == nil {
+						return
+					}
+				}
+				// The derivative policy will be deleted by the parent but need
+				// to delete the cnp from the pooling.
+				groups.DeleteDerivativeFromCache(cnp.CiliumNetworkPolicy)
+				ccnpStatusMgr.StopStatusHandler(cnp)
+			},
+		},
+		ccnpConverterFunc,
+		ccnpStore,
+	)
+	go ciliumV2Controller.Run(wait.NeverStop)
+
+	controller.NewManager().UpdateController("ccnp-to-groups",
+		controller.ControllerParams{
+			DoFunc: func(ctx context.Context) error {
+				groups.UpdateCNPInformation()
+				return nil
+			},
+			RunInterval: 5 * time.Minute,
+		})
+
+	return nil
+}

Makefile
@@ -355,7 +355,7 @@ LOCAL_IMAGE_TAG=local
 LOCAL_IMAGE=localhost:32000/cilium/cilium:$(LOCAL_IMAGE_TAG)
 microk8s: check-microk8s
 	$(QUIET)$(MAKE) dev-docker-image DOCKER_IMAGE_TAG=$(LOCAL_IMAGE_TAG)
-	@echo "  DPLOY image to microk8s ($(LOCAL_IMAGE))"
+	@echo "  DEPLOY image to microk8s ($(LOCAL_IMAGE))"
 	$(CONTAINER_ENGINE_FULL) tag cilium/cilium-dev:$(LOCAL_IMAGE_TAG) $(LOCAL_IMAGE)
 	$(CONTAINER_ENGINE_FULL) push $(LOCAL_IMAGE)
 	$(QUIET)kubectl apply -f contrib/k8s/microk8s-prepull.yaml

operator/main.go
@@ -334,6 +334,12 @@ func runOperator(cmd *cobra.Command) {
 			"Cannot connect to Kubernetes apiserver ")
 	}
 
+	err = enableCCNPWatcher()
+	if err != nil {
+		log.WithError(err).WithField("subsys", "CCNPWatcher").Fatal(
+			"Cannot connect to Kubernetes apiserver ")
+	}
+
 	log.Info("Initialization complete")
 
 	<-shutdownSignal

pkg/k8s/ccnpstatus.go
@@ -1,0 +1,68 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package k8s
+
+import (
+	"context"
+	"path"
+	"time"
+
+	"github.com/cilium/cilium/pkg/k8s/types"
+	"github.com/cilium/cilium/pkg/kvstore"
+	"github.com/cilium/cilium/pkg/kvstore/store"
+
+	"k8s.io/client-go/tools/cache"
+)
+
+var CCNPStatusesPath = path.Join(kvstore.BaseKeyPrefix, "state", "ccnpstatuses", "v2")
+
+// CCNPStatusEventHandler handles status updates events for all the CCNPs
+// in the cluster. Upon creation of Clusterwide policies, it will start a
+// controller for that CNP which handles sending of updates for that CCNP to
+// the kubernetes API server. Upon receiving eventes from the key-value store
+// it will send the update for the CCNP corresponding to the status update to
+// the controller for that CCNP.
+type CCNPStatusEventHandler struct {
+	*CNPStatusEventHandler
+}
+
+// NewCCNPStatusEventHandler returns a new CCNPStatusEventHandler.
+// which is more or less a wrapper around the CNPStatusEventHandler itself.
+func NewCCNPStatusEventHandler(cnpStore *store.SharedStore, k8sStore cache.Store, updateInterval time.Duration) *CCNPStatusEventHandler {
+	return &CCNPStatusEventHandler{
+		CNPStatusEventHandler: NewCNPStatusEventHandler(cnpStore, k8sStore, updateInterval),
+	}
+}
+
+// WatchForCCNPStatusEvents starts a watcher for all the Clusterwide policy
+// updates from the key-value store.
+func (c *CCNPStatusEventHandler) WatchForCCNPStatusEvents() {
+	watcher := kvstore.Client().ListAndWatch(context.TODO(), "ccnpStatusWatcher", CCNPStatusesPath, 512)
+
+	// Loop and block for this network policy watch event.
+	for {
+		c.watchForCNPStatusEvents(watcher)
+	}
+}
+
+// StopStatusHandler signals that we need to stop managing the sending of
+// status updates to the Kubernetes APIServer for the given CCNP. It also cleans
+// up all status updates from the key-value store for this CCNP.
+func (c *CCNPStatusEventHandler) StopStatusHandler(cnp *types.SlimCNP) {
+	cnpKey := getKeyFromObjectMeta(cnp.ObjectMeta)
+	prefix := path.Join(CCNPStatusesPath, cnpKey)
+
+	c.stopStatusHandler(cnp, cnpKey, prefix)
+}

pkg/k8s/cnp.go
@@ -288,8 +288,16 @@ func generateCNPKey(uid, namespace, name string) string {
 	return path.Join(uid, namespace, name)
 }
 
+// formatKeyForKvstore formats the key to be used for kvstore, it takes into consideration
+// the namespaced nature of the resource, so if the namespace provided is empty
+// then it assumes that the resource corresponsing to the key is a clusterwide
+// resource.
 func formatKeyForKvstore(uid k8sTypes.UID, namespace, name, nodeName string) string {
-	return path.Join(CNPStatusesPath, string(uid), namespace, name, nodeName)
+	if namespace != "" {
+		return path.Join(CNPStatusesPath, string(uid), namespace, name, nodeName)
+	}
+
+	return path.Join(CCNPStatusesPath, string(uid), name, nodeName)
 }
 
 func (c *CNPStatusUpdateContext) updateViaAPIServer(cnp *types.SlimCNP, enforcing, ok bool, cnpError error, rev uint64, cnpAnnotations map[string]string) error {
@@ -390,6 +398,9 @@ func (c *CNPStatusUpdateContext) updateViaKVStore(ctx context.Context, cnp *type
 		return err
 	}
 
+	// If the namespace is empty it means that the policy is clusterwide policy.
+	// This is then taken care of internally when we try to join the path using
+	// golangs `path.Join`
 	key := formatKeyForKvstore(cnp.UID, cnp.Namespace, cnp.Name, node.GetName())
 	log.WithFields(logrus.Fields{
 		"key":   key,

pkg/k8s/cnpstatus.go
@@ -116,19 +116,27 @@ type NodeStatusUpdate struct {
 	*cilium_v2.CiliumNetworkPolicyNodeStatus
 }
 
-// WatchForCNPStatusEvents starts a watcher for all CNP status updates from
-// the key-value store.
+// WatchForCNPStatusEvents starts a watcher for all the CNP update from the
+// key-value store.
 func (c *CNPStatusEventHandler) WatchForCNPStatusEvents() {
-
-restart:
 	watcher := kvstore.Client().ListAndWatch(context.TODO(), "cnpStatusWatcher", CNPStatusesPath, 512)
+
+	// Loop and block for the watcher
+	for {
+		c.watchForCNPStatusEvents(watcher)
+	}
+}
+
+// watchForCNPStatusEvents starts responds to the events from the watcher of
+// the key-value store.
+func (c *CNPStatusEventHandler) watchForCNPStatusEvents(watcher *kvstore.Watcher) {
 	for {
 		select {
 		case event, ok := <-watcher.Events:
 			if !ok {
 				log.Debugf("%s closed, restarting watch", watcher.String())
 				time.Sleep(500 * time.Millisecond)
-				goto restart
+				return
 			}
 
 			switch event.Typ {
@@ -153,6 +161,8 @@ restart:
 
 				// Send the update to the corresponding controller for the
 				// CNP which sends all status updates to the K8s apiserver.
+				// If the namespace is empty for the status update then the cnpKey
+				// will correspond to the ccnpKey.
 				cnpKey := generateCNPKey(string(cnpStatusUpdate.UID), cnpStatusUpdate.Namespace, cnpStatusUpdate.Name)
 				updater, ok := c.eventMap.lookup(cnpKey)
 				if !ok {
@@ -189,18 +199,22 @@ restart:
 	}
 }
 
+func (c *CNPStatusEventHandler) stopStatusHandler(cnp *types.SlimCNP, cnpKey, prefix string) {
+	err := kvstore.DeletePrefix(context.TODO(), prefix)
+	if err != nil {
+		log.WithError(err).WithField("prefix", prefix).Warning("error deleting prefix from kvstore")
+	}
+	c.eventMap.delete(cnpKey)
+}
+
 // StopStatusHandler signals that we need to stop managing the sending of
 // status updates to the Kubernetes APIServer for the given CNP. It also cleans
 // up all status updates from the key-value store for this CNP.
 func (c *CNPStatusEventHandler) StopStatusHandler(cnp *types.SlimCNP) {
 	cnpKey := getKeyFromObjectMeta(cnp.ObjectMeta)
 	prefix := path.Join(CNPStatusesPath, cnpKey)
-	err := kvstore.DeletePrefix(context.TODO(), prefix)
-	if err != nil {
-		log.WithError(err).WithField("prefix", prefix).Warning("error deleting prefix from kvstore")
-	}
-	c.eventMap.delete(cnpKey)
 
+	c.stopStatusHandler(cnp, cnpKey, prefix)
 }
 
 func (c *CNPStatusEventHandler) runStatusHandler(cnpKey string, cnp *types.SlimCNP, nodeStatusUpdater *NodeStatusUpdater) {
@@ -285,7 +299,7 @@ func (c *CNPStatusEventHandler) runStatusHandler(cnpKey string, cnp *types.SlimC
 		// needing the actual CNP object itself.
 		case k8sversion.Capabilities().Patch:
 		default:
-			cnp, err = getUpdatedCNPFromStore(c.k8sStore, fmt.Sprintf("%s/%s", namespace, name))
+			cnp, err = getUpdatedCNPFromStore(c.k8sStore, namespace, name)
 			if err != nil {
 				scopedLog.WithError(err).Error("error getting updated cnp from store")
 			}
@@ -313,10 +327,19 @@ func (c *CNPStatusEventHandler) StartStatusHandler(cnp *types.SlimCNP) {
 }
 
 func getKeyFromObjectMeta(t metaV1.ObjectMeta) string {
-	return path.Join(string(t.UID), t.Namespace, t.Name)
+	if t.Namespace != "" {
+		return path.Join(string(t.UID), t.Namespace, t.Name)
+	}
+
+	return path.Join(string(t.UID), t.Name)
 }
 
-func getUpdatedCNPFromStore(ciliumStore cache.Store, nameNamespace string) (*types.SlimCNP, error) {
+func getUpdatedCNPFromStore(ciliumStore cache.Store, namespace, name string) (*types.SlimCNP, error) {
+	nameNamespace := name
+	if namespace != "" {
+		nameNamespace = fmt.Sprintf("%s/%s", namespace, name)
+	}
+
 	serverRuleStore, exists, err := ciliumStore.GetByKey(nameNamespace)
 	if err != nil {
 		return nil, fmt.Errorf("unable to find v2.CiliumNetworkPolicy in local cache: %s", err)

commit ddb7af536867288c9d6e5dfcde7bdf2b62958c55

operator: only enable kvstore watcher if kvstore is enabled
When cilium-agent is running with `--enable-k8s-event-handover` it sends
its CNP Node Status updates to the kvstore and `cilium-operator` is
watching for kvstore events and once it has all status for all the nodes
in the cluster, or every 1 second until all the nodes have updated its
status, which ever comes first, it will update the full CNP Node Status
into to the kube-apiserver. However, if `cilium-operator` does not have
KVStore enabled it can't perform this operation and therefore it should
not spend any resources to implement the `k8s-event-handover`
functionality.

Fixes: 25ef8ab70240 ("operator: add ccnp event watcher to cilium operator")
Fixes: 7393c2c302f4 ("operator: send CNPNodeStatus updates to k8s")
Signed-off-by: André Martins <andre@cilium.io>

operator/ccnp_event.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Authors of Cilium
+// Copyright 2018-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -48,6 +48,7 @@ func enableCCNPWatcher() error {
 
 	var (
 		ccnpConverterFunc informer.ConvertFunc
+		ccnpStatusMgr     *k8s.CCNPStatusEventHandler
 	)
 	ccnpStore := cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
 
@@ -60,19 +61,19 @@ func enableCCNPWatcher() error {
 		ccnpConverterFunc = k8s.ConvertToCCNPWithStatus
 	}
 
-	ccnpSharedStore, err := store.JoinSharedStore(store.Configuration{
-		Prefix: k8s.CCNPStatusesPath,
-		KeyCreator: func() store.Key {
-			return &k8s.CNPNSWithMeta{}
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	ccnpStatusMgr := k8s.NewCCNPStatusEventHandler(ccnpSharedStore, ccnpStore, ccnpStatusUpdateInterval)
-
 	if kvstoreEnabled() {
+		ccnpSharedStore, err := store.JoinSharedStore(store.Configuration{
+			Prefix: k8s.CCNPStatusesPath,
+			KeyCreator: func() store.Key {
+				return &k8s.CNPNSWithMeta{}
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		ccnpStatusMgr = k8s.NewCCNPStatusEventHandler(ccnpSharedStore, ccnpStore, ccnpStatusUpdateInterval)
+
 		go ccnpStatusMgr.WatchForCCNPStatusEvents()
 	}
 
@@ -86,7 +87,9 @@ func enableCCNPWatcher() error {
 				metrics.EventTSK8s.SetToCurrentTime()
 				if cnp := k8s.CopyObjToV2CNP(obj); cnp != nil {
 					groups.AddDerivativeCNPIfNeeded(cnp.CiliumNetworkPolicy)
-					ccnpStatusMgr.StartStatusHandler(cnp)
+					if kvstoreEnabled() {
+						ccnpStatusMgr.StartStatusHandler(cnp)
+					}
 				}
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
@@ -119,7 +122,9 @@ func enableCCNPWatcher() error {
 				// The derivative policy will be deleted by the parent but need
 				// to delete the cnp from the pooling.
 				groups.DeleteDerivativeFromCache(cnp.CiliumNetworkPolicy)
-				ccnpStatusMgr.StopStatusHandler(cnp)
+				if kvstoreEnabled() {
+					ccnpStatusMgr.StopStatusHandler(cnp)
+				}
 			},
 		},
 		ccnpConverterFunc,

operator/cnp_event.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Authors of Cilium
+// Copyright 2018-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -51,6 +51,7 @@ func enableCNPWatcher() error {
 
 	var (
 		cnpConverterFunc informer.ConvertFunc
+		cnpStatusMgr     *k8s.CNPStatusEventHandler
 	)
 	cnpStore := cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
 
@@ -63,19 +64,19 @@ func enableCNPWatcher() error {
 		cnpConverterFunc = k8s.ConvertToCNPWithStatus
 	}
 
-	cnpSharedStore, err := store.JoinSharedStore(store.Configuration{
-		Prefix: k8s.CNPStatusesPath,
-		KeyCreator: func() store.Key {
-			return &k8s.CNPNSWithMeta{}
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	cnpStatusMgr := k8s.NewCNPStatusEventHandler(cnpSharedStore, cnpStore, cnpStatusUpdateInterval)
-
 	if kvstoreEnabled() {
+		cnpSharedStore, err := store.JoinSharedStore(store.Configuration{
+			Prefix: k8s.CNPStatusesPath,
+			KeyCreator: func() store.Key {
+				return &k8s.CNPNSWithMeta{}
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		cnpStatusMgr = k8s.NewCNPStatusEventHandler(cnpSharedStore, cnpStore, cnpStatusUpdateInterval)
+
 		go cnpStatusMgr.WatchForCNPStatusEvents()
 	}
 
@@ -89,7 +90,9 @@ func enableCNPWatcher() error {
 				metrics.EventTSK8s.SetToCurrentTime()
 				if cnp := k8s.CopyObjToV2CNP(obj); cnp != nil {
 					groups.AddDerivativeCNPIfNeeded(cnp.CiliumNetworkPolicy)
-					cnpStatusMgr.StartStatusHandler(cnp)
+					if kvstoreEnabled() {
+						cnpStatusMgr.StartStatusHandler(cnp)
+					}
 				}
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
@@ -122,7 +125,9 @@ func enableCNPWatcher() error {
 				// The derivative policy will be deleted by the parent but need
 				// to delete the cnp from the pooling.
 				groups.DeleteDerivativeFromCache(cnp.CiliumNetworkPolicy)
-				cnpStatusMgr.StopStatusHandler(cnp)
+				if kvstoreEnabled() {
+					cnpStatusMgr.StopStatusHandler(cnp)
+				}
 			},
 		},
 		cnpConverterFunc,