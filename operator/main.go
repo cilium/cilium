57f08c1f18be4a8e910f160c80c0f97ce0be12ed
André Martins <andre@cilium.io>
2020-01-28 15:42:32 +0100

    garbage collect stale distributed locks
    [ upstream commit 440539b7604aa50557d02b13e01a3027b6209b03 ]
    
    Under special conditions [1], cilium-agent could leak distributed locks
    into the KVStore making all cilium-agents to deadlock. To avoid this
    cilium-operator will garbage collect stale distributed locks. If a lock
    is being held for more than 2x default value of cilium's lock lease TTL,
    cilium-operator will delete that lock from the KVStore. On a first sight
    this might not be reasonable but cilium-agents only use the distributed
    locks to coordinated across each other which one should pick the
    security identity for a new set of labels. Even if 2 cilium-agents pick
    2 different security identities for the same set of labels, only the
    first one will be able to publish that identity into the KVStore. The
    2nd agent will fail to publish the security identity that it picked for
    the set of labels:
    
    [1]
    1) connect to a etcd member (https://node-A:2379) to create a lock with
       a 25 second lease (the default for cilium)
    2) drop all peer packets, not client packets, to and from that etcd
       member (https://node-A:2380).
    3) try releasing that lock immediately. The unlock will fail with the
       error level=error msg="Unable to unlock kvstore lock"
       error="etcdserver: request timed out".
    4) As soon the unlock fails, resume connectivity dropped in 2), this
       re-connectivity needs to happen before the lease expires, which means
       the Unlock on step 3) needs to fail in less than 25 seconds, this is
       what happen for my case.
    5) since the client resumed connectivity with the server in less than 25
       seconds, the lease didn't expire which means the server still thinks
       the client is still holding the lock but the client assumes the lock was
       unlocked.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- operator/main.go
+++ operator/main.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Authors of Cilium
+// Copyright 2018-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -298,6 +298,8 @@ func runOperator(cmd *cobra.Command) {
 				log.WithError(err).Error("Unable to setup node watcher")
 			}
 		}
+
+		startKvstoreWatchdog()
 	}
 
 	if identityAllocationMode == option.IdentityAllocationModeCRD {
--- pkg/kvstore/etcd.go
+++ pkg/kvstore/etcd.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -50,7 +50,7 @@ const (
 	// EtcdBackendName is the backend name for etcd
 	EtcdBackendName = "etcd"
 
-	addrOption           = "etcd.address"
+	EtcdAddrOption       = "etcd.address"
 	isEtcdOperatorOption = "etcd.operator"
 	EtcdOptionConfig     = "etcd.config"
 
@@ -105,7 +105,7 @@ func newEtcdModule() backendModule {
 			isEtcdOperatorOption: &backendOption{
 				description: "if the configuration is setting up an etcd-operator",
 			},
-			addrOption: &backendOption{
+			EtcdAddrOption: &backendOption{
 				description: "Addresses of etcd cluster",
 			},
 			EtcdOptionConfig: &backendOption{
@@ -154,7 +154,7 @@ func (e *etcdModule) getConfig() map[string]string {
 func (e *etcdModule) newClient(opts *ExtraOptions) (BackendOperations, chan error) {
 	errChan := make(chan error, 10)
 
-	endpointsOpt, endpointsSet := e.opts[addrOption]
+	endpointsOpt, endpointsSet := e.opts[EtcdAddrOption]
 	configPathOpt, configSet := e.opts[EtcdOptionConfig]
 
 	rateLimitOpt, rateLimitSet := e.opts[EtcdRateLimitOption]
@@ -171,14 +171,14 @@ func (e *etcdModule) newClient(opts *ExtraOptions) (BackendOperations, chan erro
 	}
 	if e.config == nil {
 		if !endpointsSet && !configSet {
-			errChan <- fmt.Errorf("invalid etcd configuration, %s or %s must be specified", EtcdOptionConfig, addrOption)
+			errChan <- fmt.Errorf("invalid etcd configuration, %s or %s must be specified", EtcdOptionConfig, EtcdAddrOption)
 			close(errChan)
 			return nil, errChan
 		}
 
 		if endpointsOpt.value == "" && configPath == "" {
 			errChan <- fmt.Errorf("invalid etcd configuration, %s or %s must be specified",
-				EtcdOptionConfig, addrOption)
+				EtcdOptionConfig, EtcdAddrOption)
 			close(errChan)
 			return nil, errChan
 		}
@@ -1293,6 +1293,7 @@ func (e *etcdClient) ListPrefix(prefix string) (KeyValuePairs, error) {
 		pairs[string(getR.Kvs[i].Key)] = Value{
 			Data:        getR.Kvs[i].Value,
 			ModRevision: uint64(getR.Kvs[i].ModRevision),
+			LeaseID:     getR.Kvs[i].Lease,
 		}
 
 	}
@@ -1364,7 +1365,7 @@ func IsEtcdOperator(selectedBackend string, opts map[string]string, k8sNamespace
 			names[1] == k8sNamespace
 	}
 
-	fqdn := opts[addrOption]
+	fqdn := opts[EtcdAddrOption]
 	if len(fqdn) != 0 {
 		return fqdnIsEtcdOperator(fqdn)
 	}
