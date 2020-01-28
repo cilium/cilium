cilium/cilium

commit d3ad5c7265ce3746eb9ab925984fc5c7b0ea05ba

kvstore: plumb context
etcd and consul both take in `context.Context` as inputs, but we did not always
pass down said type into invocations of their APIs. Update `pkg/kvstore` to take
in `context.Context`, and pass in `context.TODO()` into packages that use
functions that were updated to take in `context.Context` in the Cilium codebase.

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/allocator/allocator.go
@@ -443,7 +443,7 @@ func (a *Allocator) lockedAllocate(ctx context.Context, key AllocatorKey) (idpoo
 		return 0, false, err
 	}
 
-	defer lock.Unlock()
+	defer lock.Unlock(ctx)
 
 	// fetch first key that matches /value/<key> while ignoring the
 	// node suffix

pkg/kvstore/allocator/allocator.go
@@ -101,6 +101,8 @@ type kvstoreBackend struct {
 	deleteInvalidPrefixes bool
 
 	keyType allocator.AllocatorKey
+
+	ctx context.Context
 }
 
 func locklessCapability() bool {
@@ -140,7 +142,8 @@ func (k *kvstoreBackend) lockPath(ctx context.Context, key string) (*kvstore.Loc
 
 // DeleteAllKeys will delete all keys
 func (k *kvstoreBackend) DeleteAllKeys() {
-	kvstore.DeletePrefix(k.basePrefix)
+	kvstore.DeletePrefix(context.TODO(), k.basePrefix)
+	kvstore.DeletePrefix(context.TODO(), k.basePrefix)
 }
 
 // AllocateID allocates a key->ID mapping in the kvstore.
@@ -217,7 +220,7 @@ func (k *kvstoreBackend) Get(ctx context.Context, key allocator.AllocatorKey) (i
 	//
 	// Only key1 should match
 	prefix := path.Join(k.valuePrefix, kvstore.Encode(key.GetKey()))
-	pairs, err := kvstore.ListPrefix(prefix)
+	pairs, err := kvstore.ListPrefix(ctx, prefix)
 	kvstore.Trace("ListPrefix", err, logrus.Fields{fieldPrefix: prefix, "entries": len(pairs)})
 	if err != nil {
 		return 0, err
@@ -254,7 +257,7 @@ func (k *kvstoreBackend) GetIfLocked(ctx context.Context, key allocator.Allocato
 	//
 	// Only key1 should match
 	prefix := path.Join(k.valuePrefix, kvstore.Encode(key.GetKey()))
-	pairs, err := kvstore.ListPrefixIfLocked(prefix, lock)
+	pairs, err := kvstore.ListPrefixIfLocked(ctx, prefix, lock)
 	kvstore.Trace("ListPrefixLocked", err, logrus.Fields{fieldPrefix: prefix, "entries": len(pairs)})
 	if err != nil {
 		return 0, err
@@ -275,7 +278,7 @@ func (k *kvstoreBackend) GetIfLocked(ctx context.Context, key allocator.Allocato
 // GetByID returns the key associated with an ID. Returns nil if no key is
 // associated with the ID.
 func (k *kvstoreBackend) GetByID(id idpool.ID) (allocator.AllocatorKey, error) {
-	v, err := kvstore.Get(path.Join(k.idPrefix, id.String()))
+	v, err := kvstore.Get(context.TODO(), path.Join(k.idPrefix, id.String()))
 	if err != nil {
 		return nil, err
 	}
@@ -381,7 +384,7 @@ func (k *kvstoreBackend) Release(ctx context.Context, key allocator.AllocatorKey
 
 	// does not need to be deleted with a lock as its protected by the
 	// Allocator.slaveKeysMutex
-	if err := kvstore.Delete(valueKey); err != nil {
+	if err := kvstore.Delete(ctx, valueKey); err != nil {
 		log.WithError(err).WithFields(logrus.Fields{fieldKey: key}).Warning("Ignoring node specific ID")
 		return err
 	}
@@ -398,7 +401,7 @@ func (k *kvstoreBackend) Release(ctx context.Context, key allocator.AllocatorKey
 // RunGC scans the kvstore for unused master keys and removes them
 func (k *kvstoreBackend) RunGC(staleKeysPrevRound map[string]uint64) (map[string]uint64, error) {
 	// fetch list of all /id/ keys
-	allocated, err := kvstore.ListPrefix(k.idPrefix)
+	allocated, err := kvstore.ListPrefix(context.TODO(), k.idPrefix)
 	if err != nil {
 		return nil, fmt.Errorf("list failed: %s", err)
 	}
@@ -411,7 +414,7 @@ func (k *kvstoreBackend) RunGC(staleKeysPrevRound map[string]uint64) (map[string
 		// FIXME: Add DeleteOnZeroCount support
 		// }
 
-		lock, err := k.lockPath(context.Background(), key)
+		lock, err := k.lockPath(context.TODO(), key)
 		if err != nil {
 			log.WithError(err).WithField(fieldKey, key).Warning("allocator garbage collector was unable to lock key")
 			continue
@@ -419,10 +422,10 @@ func (k *kvstoreBackend) RunGC(staleKeysPrevRound map[string]uint64) (map[string
 
 		// fetch list of all /value/<key> keys
 		valueKeyPrefix := path.Join(k.valuePrefix, string(v.Data))
-		pairs, err := kvstore.ListPrefixIfLocked(valueKeyPrefix, lock)
+		pairs, err := kvstore.ListPrefixIfLocked(context.TODO(), valueKeyPrefix, lock)
 		if err != nil {
 			log.WithError(err).WithField(fieldPrefix, valueKeyPrefix).Warning("allocator garbage collector was unable to list keys")
-			lock.Unlock()
+			lock.Unlock(context.TODO())
 			continue
 		}
 
@@ -442,7 +445,7 @@ func (k *kvstoreBackend) RunGC(staleKeysPrevRound map[string]uint64) (map[string
 			})
 			// Only delete if this key was previously marked as to be deleted
 			if modRev, ok := staleKeysPrevRound[key]; ok && modRev == v.ModRevision {
-				if err := kvstore.DeleteIfLocked(key, lock); err != nil {
+				if err := kvstore.DeleteIfLocked(context.TODO(), key, lock); err != nil {
 					scopedLog.WithError(err).Warning("Unable to delete unused allocator master key")
 				} else {
 					scopedLog.Info("Deleted unused allocator master key")
@@ -453,7 +456,7 @@ func (k *kvstoreBackend) RunGC(staleKeysPrevRound map[string]uint64) (map[string
 			}
 		}
 
-		lock.Unlock()
+		lock.Unlock(context.TODO())
 	}
 
 	return staleKeys, nil
@@ -478,7 +481,7 @@ func (k *kvstoreBackend) keyToID(key string) (id idpool.ID, err error) {
 }
 
 func (k *kvstoreBackend) ListAndWatch(handler allocator.CacheMutations, stopChan chan struct{}) {
-	watcher := kvstore.ListAndWatch(k.idPrefix, k.idPrefix, 512)
+	watcher := kvstore.ListAndWatch(context.TODO(), k.idPrefix, k.idPrefix, 512)
 
 	for {
 		select {
@@ -497,7 +500,7 @@ func (k *kvstoreBackend) ListAndWatch(handler allocator.CacheMutations, stopChan
 				log.WithError(err).WithField(fieldKey, event.Key).Warning("Invalid key")
 
 				if k.deleteInvalidPrefixes {
-					kvstore.Delete(event.Key)
+					kvstore.Delete(context.TODO(), event.Key)
 				}
 
 			case id != idpool.NoID:

pkg/kvstore/consul.go
@@ -116,9 +116,9 @@ func (c *consulModule) getConfig() map[string]string {
 	return getOpts(c.opts)
 }
 
-func (c *consulModule) newClient(opts *ExtraOptions) (BackendOperations, chan error) {
+func (c *consulModule) newClient(ctx context.Context, opts *ExtraOptions) (BackendOperations, chan error) {
 	errChan := make(chan error, 1)
-	backend, err := c.connectConsulClient(opts)
+	backend, err := c.connectConsulClient(ctx, opts)
 	if err != nil {
 		errChan <- err
 	}
@@ -126,7 +126,7 @@ func (c *consulModule) newClient(opts *ExtraOptions) (BackendOperations, chan er
 	return backend, errChan
 }
 
-func (c *consulModule) connectConsulClient(opts *ExtraOptions) (BackendOperations, error) {
+func (c *consulModule) connectConsulClient(ctx context.Context, opts *ExtraOptions) (BackendOperations, error) {
 	if c.config == nil {
 		consulAddr, consulAddrSet := c.opts[optAddress]
 		configPathOpt, configPathOptSet := c.opts[consulOptionConfig]
@@ -156,7 +156,7 @@ func (c *consulModule) connectConsulClient(opts *ExtraOptions) (BackendOperation
 		c.config.Address = addr
 
 	}
-	client, err := newConsulClient(c.config, opts)
+	client, err := newConsulClient(ctx, c.config, opts)
 	if err != nil {
 		return nil, err
 	}
@@ -177,7 +177,7 @@ type consulClient struct {
 	disconnected   chan struct{}
 }
 
-func newConsulClient(config *consulAPI.Config, opts *ExtraOptions) (BackendOperations, error) {
+func newConsulClient(ctx context.Context, config *consulAPI.Config, opts *ExtraOptions) (BackendOperations, error) {
 	var (
 		c   *consulAPI.Client
 		err error
@@ -206,7 +206,7 @@ func newConsulClient(config *consulAPI.Config, opts *ExtraOptions) (BackendOpera
 				err = errors.New("timeout while waiting for leader to be elected")
 			}
 		}
-		boff.Wait(context.TODO())
+		boff.Wait(ctx)
 	}
 
 	if err != nil {
@@ -218,7 +218,8 @@ func newConsulClient(config *consulAPI.Config, opts *ExtraOptions) (BackendOpera
 		Behavior: consulAPI.SessionBehaviorDelete,
 	}
 
-	lease, _, err := c.Session().Create(entry, nil)
+	wo := &consulAPI.WriteOptions{}
+	lease, _, err := c.Session().Create(entry, wo.WithContext(ctx))
 	if err != nil {
 		return nil, fmt.Errorf("unable to create default lease: %s", err)
 	}
@@ -234,7 +235,8 @@ func newConsulClient(config *consulAPI.Config, opts *ExtraOptions) (BackendOpera
 	client.controllers.UpdateController(fmt.Sprintf("consul-lease-keepalive-%p", c),
 		controller.ControllerParams{
 			DoFunc: func(ctx context.Context) error {
-				_, _, err := c.Session().Renew(lease, nil)
+				wo := &consulAPI.WriteOptions{}
+				_, _, err := c.Session().Renew(lease, wo.WithContext(ctx))
 				if err != nil {
 					// consider disconnected!
 					client.disconnectedMu.Lock()
@@ -253,6 +255,10 @@ func newConsulClient(config *consulAPI.Config, opts *ExtraOptions) (BackendOpera
 
 type ConsulLocker struct {
 	*consulAPI.Lock
+}
+
+func (cl *ConsulLocker) Unlock(ctx context.Context) error {
+	return cl.Lock.Unlock()
 }
 
 func (cl *ConsulLocker) Comparator() interface{} {
@@ -287,14 +293,16 @@ func (c *consulClient) LockPath(ctx context.Context, path string) (KVLocker, err
 }
 
 // Watch starts watching for changes in a prefix
-func (c *consulClient) Watch(w *Watcher) {
+func (c *consulClient) Watch(ctx context.Context, w *Watcher) {
 	// Last known state of all KVPairs matching the prefix
 	localState := map[string]consulAPI.KVPair{}
 	nextIndex := uint64(0)
 
-	qo := consulAPI.QueryOptions{
+	q := &consulAPI.QueryOptions{
 		WaitTime: time.Second,
 	}
+
+	qo := q.WithContext(ctx)
 
 	for {
 		// Initialize sleep time to a millisecond as we don't
@@ -302,7 +310,7 @@ func (c *consulClient) Watch(w *Watcher) {
 		sleepTime := 1 * time.Millisecond
 
 		qo.WaitIndex = nextIndex
-		pairs, q, err := c.KV().List(w.prefix, &qo)
+		pairs, q, err := c.KV().List(w.prefix, qo)
 		if err != nil {
 			sleepTime = 5 * time.Second
 			Trace("List of Watch failed", err, logrus.Fields{fieldPrefix: w.prefix, fieldWatcher: w.name})
@@ -382,15 +390,15 @@ func (c *consulClient) Watch(w *Watcher) {
 	}
 }
 
-func (c *consulClient) waitForInitLock() <-chan struct{} {
+func (c *consulClient) waitForInitLock(ctx context.Context) <-chan struct{} {
 	initLockSucceeded := make(chan struct{})
 
 	go func() {
 		for {
-			locker, err := c.LockPath(context.TODO(), InitLockPath)
+			locker, err := c.LockPath(ctx, InitLockPath)
 			if err == nil {
 				close(initLockSucceeded)
-				locker.Unlock()
+				locker.Unlock(ctx)
 				log.Info("Distributed lock successful, consul has quorum")
 				return
 			}
@@ -402,19 +410,20 @@ func (c *consulClient) waitForInitLock() <-chan struct{} {
 	return initLockSucceeded
 }
 
-// Connected closes the returned channel when the etcd client is connected.
-func (c *consulClient) Connected() <-chan struct{} {
+// Connected closes the returned channel when the consul client is connected.
+func (c *consulClient) Connected(ctx context.Context) <-chan struct{} {
 	ch := make(chan struct{})
 	go func() {
 		for {
+			qo := &consulAPI.QueryOptions{}
 			// TODO find out if there's a better way to do this for consul
-			_, _, err := c.Session().Info(c.lease, &consulAPI.QueryOptions{})
+			_, _, err := c.Session().Info(c.lease, qo.WithContext(ctx))
 			if err == nil {
 				break
 			}
 			time.Sleep(100 * time.Millisecond)
 		}
-		<-c.waitForInitLock()
+		<-c.waitForInitLock(ctx)
 		close(ch)
 	}()
 	return ch
@@ -434,43 +443,47 @@ func (c *consulClient) Status() (string, error) {
 	return "Consul: " + leader, err
 }
 
-func (c *consulClient) DeletePrefix(path string) error {
+func (c *consulClient) DeletePrefix(ctx context.Context, path string) error {
 	duration := spanstat.Start()
-	_, err := c.Client.KV().DeleteTree(path, nil)
+	wo := &consulAPI.WriteOptions{}
+	_, err := c.Client.KV().DeleteTree(path, wo.WithContext(ctx))
 	increaseMetric(path, metricDelete, "DeletePrefix", duration.EndError(err).Total(), err)
 	return err
 }
 
 // Set sets value of key
-func (c *consulClient) Set(key string, value []byte) error {
+func (c *consulClient) Set(ctx context.Context, key string, value []byte) error {
 	duration := spanstat.Start()
-	_, err := c.KV().Put(&consulAPI.KVPair{Key: key, Value: value}, nil)
+	wo := &consulAPI.WriteOptions{}
+	_, err := c.KV().Put(&consulAPI.KVPair{Key: key, Value: value}, wo.WithContext(ctx))
 	increaseMetric(key, metricSet, "Set", duration.EndError(err).Total(), err)
 	return err
 }
 
 // DeleteIfLocked deletes a key if the client is still holding the given lock.
-func (c *consulClient) DeleteIfLocked(key string, lock KVLocker) error {
-	return c.Delete(key)
+func (c *consulClient) DeleteIfLocked(ctx context.Context, key string, lock KVLocker) error {
+	return c.Delete(ctx, key)
 }
 
 // Delete deletes a key
-func (c *consulClient) Delete(key string) error {
+func (c *consulClient) Delete(ctx context.Context, key string) error {
 	duration := spanstat.Start()
-	_, err := c.KV().Delete(key, nil)
+	wo := &consulAPI.WriteOptions{}
+	_, err := c.KV().Delete(key, wo.WithContext(ctx))
 	increaseMetric(key, metricDelete, "Delete", duration.EndError(err).Total(), err)
 	return err
 }
 
 // GetIfLocked returns value of key if the client is still holding the given lock.
-func (c *consulClient) GetIfLocked(key string, lock KVLocker) ([]byte, error) {
-	return c.Get(key)
+func (c *consulClient) GetIfLocked(ctx context.Context, key string, lock KVLocker) ([]byte, error) {
+	return c.Get(ctx, key)
 }
 
 // Get returns value of key
-func (c *consulClient) Get(key string) ([]byte, error) {
+func (c *consulClient) Get(ctx context.Context, key string) ([]byte, error) {
 	duration := spanstat.Start()
-	pair, _, err := c.KV().Get(key, nil)
+	qo := &consulAPI.QueryOptions{}
+	pair, _, err := c.KV().Get(key, qo.WithContext(ctx))
 	increaseMetric(key, metricRead, "Get", duration.EndError(err).Total(), err)
 	if err != nil {
 		return nil, err
@@ -532,7 +545,8 @@ func (c *consulClient) UpdateIfDifferentIfLocked(ctx context.Context, key string
 // UpdateIfDifferent updates a key if the value is different
 func (c *consulClient) UpdateIfDifferent(ctx context.Context, key string, value []byte, lease bool) (bool, error) {
 	duration := spanstat.Start()
-	getR, _, err := c.KV().Get(key, nil)
+	qo := &consulAPI.QueryOptions{}
+	getR, _, err := c.KV().Get(key, qo.WithContext(ctx))
 	increaseMetric(key, metricRead, "Get", duration.EndError(err).Total(), err)
 	// On error, attempt update blindly
 	if err != nil || getR == nil {
@@ -579,30 +593,30 @@ func (c *consulClient) CreateOnly(ctx context.Context, key string, value []byte,
 }
 
 // createIfExists creates a key with the value only if key condKey exists
-func (c *consulClient) createIfExists(condKey, key string, value []byte, lease bool) error {
+func (c *consulClient) createIfExists(ctx context.Context, condKey, key string, value []byte, lease bool) error {
 	// Consul does not support transactions which would allow to check for
 	// the presence of a conditional key if the key is not the key being
 	// manipulated
 	//
 	// Lock the conditional key to serialize all CreateIfExists() calls
 
-	l, err := LockPath(context.Background(), condKey)
+	l, err := LockPath(ctx, condKey)
 	if err != nil {
 		return fmt.Errorf("unable to lock condKey for CreateIfExists: %s", err)
 	}
 
-	defer l.Unlock()
+	defer l.Unlock(ctx)
 
 	// Create the key if it does not exist
-	if _, err := c.CreateOnly(context.TODO(), key, value, lease); err != nil {
+	if _, err := c.CreateOnly(ctx, key, value, lease); err != nil {
 		return err
 	}
 
 	// Consul does not support transactions which would allow to check for
 	// the presence of another key
-	masterKey, err := c.Get(condKey)
+	masterKey, err := c.Get(ctx, condKey)
 	if err != nil || masterKey == nil {
-		c.Delete(key)
+		c.Delete(ctx, key)
 		return fmt.Errorf("conditional key not present")
 	}
 
@@ -610,22 +624,23 @@ func (c *consulClient) createIfExists(condKey, key string, value []byte, lease b
 }
 
 // CreateIfExists creates a key with the value only if key condKey exists
-func (c *consulClient) CreateIfExists(condKey, key string, value []byte, lease bool) error {
+func (c *consulClient) CreateIfExists(ctx context.Context, condKey, key string, value []byte, lease bool) error {
 	duration := spanstat.Start()
-	err := c.createIfExists(condKey, key, value, lease)
+	err := c.createIfExists(ctx, condKey, key, value, lease)
 	increaseMetric(key, metricSet, "CreateIfExists", duration.EndError(err).Total(), err)
 	return err
 }
 
 // ListPrefixIfLocked returns a list of keys matching the prefix only if the client is still holding the given lock.
-func (c *consulClient) ListPrefixIfLocked(prefix string, lock KVLocker) (KeyValuePairs, error) {
-	return c.ListPrefix(prefix)
+func (c *consulClient) ListPrefixIfLocked(ctx context.Context, prefix string, lock KVLocker) (KeyValuePairs, error) {
+	return c.ListPrefix(ctx, prefix)
 }
 
 // ListPrefix returns a map of matching keys
-func (c *consulClient) ListPrefix(prefix string) (KeyValuePairs, error) {
+func (c *consulClient) ListPrefix(ctx context.Context, prefix string) (KeyValuePairs, error) {
 	duration := spanstat.Start()
-	pairs, _, err := c.KV().List(prefix, nil)
+	qo := &consulAPI.QueryOptions{}
+	pairs, _, err := c.KV().List(prefix, qo.WithContext(ctx))
 	increaseMetric(prefix, metricRead, "ListPrefix", duration.EndError(err).Total(), err)
 	if err != nil {
 		return nil, err
@@ -668,12 +683,12 @@ func (c *consulClient) Decode(in string) ([]byte, error) {
 }
 
 // ListAndWatch implements the BackendOperations.ListAndWatch using consul
-func (c *consulClient) ListAndWatch(name, prefix string, chanSize int) *Watcher {
+func (c *consulClient) ListAndWatch(ctx context.Context, name, prefix string, chanSize int) *Watcher {
 	w := newWatcher(name, prefix, chanSize)
 
 	log.WithField(fieldWatcher, w).Debug("Starting watcher...")
 
-	go c.Watch(w)
+	go c.Watch(ctx, w)
 
 	return w
 }

commit 3e2b12df255524b7fc3685164b89f8bba061f762

pkg/kvstore: use different context for lock and unlock
Using the same context used to lock a key is not the right functionality
that we want as it can cause distributed locking issues. For example, in
case a client gets a distributed key and the context is canceled while
that lock is being held, the Unlock() functions will not execute since
that same context was canceled leaving the distributed lock locked until
the client disconnects from the kvstore.

Fixes: d3ad5c7265ce ("kvstore: plumb context")
Fixes: 157089d341f2 ("allocator: add context to `RunGC`")
Signed-off-by: André Martins <andre@cilium.io>

pkg/allocator/allocator.go
@@ -443,7 +443,7 @@ func (a *Allocator) lockedAllocate(ctx context.Context, key AllocatorKey) (idpoo
 		return 0, false, err
 	}
 
-	defer lock.Unlock(ctx)
+	defer lock.Unlock(context.Background())
 
 	// fetch first key that matches /value/<key> while ignoring the
 	// node suffix

pkg/kvstore/allocator/allocator.go
@@ -423,7 +423,7 @@ func (k *kvstoreBackend) RunGC(ctx context.Context, staleKeysPrevRound map[strin
 		pairs, err := kvstore.ListPrefixIfLocked(ctx, valueKeyPrefix, lock)
 		if err != nil {
 			log.WithError(err).WithField(fieldPrefix, valueKeyPrefix).Warning("allocator garbage collector was unable to list keys")
-			lock.Unlock(ctx)
+			lock.Unlock(context.Background())
 			continue
 		}
 
@@ -454,7 +454,7 @@ func (k *kvstoreBackend) RunGC(ctx context.Context, staleKeysPrevRound map[strin
 			}
 		}
 
-		lock.Unlock(ctx)
+		lock.Unlock(context.Background())
 	}
 
 	return staleKeys, nil

pkg/kvstore/consul.go
@@ -398,7 +398,7 @@ func (c *consulClient) waitForInitLock(ctx context.Context) <-chan struct{} {
 			locker, err := c.LockPath(ctx, InitLockPath)
 			if err == nil {
 				close(initLockSucceeded)
-				locker.Unlock(ctx)
+				locker.Unlock(context.Background())
 				log.Info("Distributed lock successful, consul has quorum")
 				return
 			}
@@ -605,7 +605,7 @@ func (c *consulClient) createIfExists(ctx context.Context, condKey, key string, 
 		return fmt.Errorf("unable to lock condKey for CreateIfExists: %s", err)
 	}
 
-	defer l.Unlock(ctx)
+	defer l.Unlock(context.Background())
 
 	// Create the key if it does not exist
 	if _, err := c.CreateOnly(ctx, key, value, lease); err != nil {

pkg/kvstore/etcd.go
@@ -367,7 +367,7 @@ func (e *etcdClient) waitForInitLock(ctx context.Context) <-chan bool {
 			if err == nil {
 				initLockSucceeded <- true
 				close(initLockSucceeded)
-				locker.Unlock(ctx)
+				locker.Unlock(context.Background())
 				e.getLogger().Debug("Distributed lock successful, etcd has quorum")
 				return
 			}