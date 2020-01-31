3f03c2bff7388c059f926b9f20081753d7641862
André Martins <andre@cilium.io>
2020-01-31 16:25:41 +0100

    operator: retrieve etcd service spec upon initialization
    [ upstream commit fccd76643895d492405ea674bd056b79cc09f805 ]
    
    Contrary to cilium-agent, cilium-operator might require connectivity
    with the kvstore to synchronize k8s services with it, for example with
    the clustermesh functionality. Since the operator waits for all caches
    to be synchronized with k8s before connecting to the kvstore it would
    simply deadlock in case there would be more than 128 services in
    kubernetes and none of those services were etcd.
    
    To fix this deadlock, the operator will try to retrieve the service
    spec of etcd from kubernetes upon initialization.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/logging/logfields/logfields.go
+++ pkg/logging/logfields/logfields.go
@@ -177,6 +177,9 @@ const (
 	// ServiceName is the orchestration framework name for a service
 	ServiceName = "serviceName"
 
+	// ServiceNamespace is the orchestration framework name for a service namespace.
+	ServiceNamespace = "serviceNamespace"
+
 	// ClusterName is the name of the cluster
 	ClusterName = "clusterName"
 
