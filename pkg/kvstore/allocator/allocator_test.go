b20a88da066b5fc110bc9f90a2f16efd1361e328
André Martins <andre@cilium.io>
2020-01-31 13:47:08 +0100

    kvstore/allocator: test for stale locks before acquiring lock
    [ upstream commit 9ba79ef2517ede0ece6c1d1a7798c57d33d24f77 ]
    
    Checking for stale locks after acquiring the 1st lock might lead into
    test flakes so we should test for them before acquiring the 1st lock.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/kvstore/allocator/allocator_test.go
+++ pkg/kvstore/allocator/allocator_test.go
@@ -121,10 +121,15 @@ func (s *AllocatorSuite) TestRunLocksGC(c *C) {
 	// FIXME: Did this previousy use allocatorName := randomTestName() ? so TestAllocatorKey(randomeTestName())
 	backend1, err := NewKVStoreBackend(allocatorName, "a", TestAllocatorKey(""))
 	c.Assert(err, IsNil)
-	c.Assert(err, IsNil)
 	allocator, err := allocator.NewAllocator(TestAllocatorKey(""), backend1, allocator.WithMax(maxID), allocator.WithoutGC())
 	c.Assert(err, IsNil)
 	shortKey := TestAllocatorKey("1;")
+
+	staleLocks := map[string]kvstore.Value{}
+	staleLocks, err = allocator.RunLocksGC(staleLocks)
+	c.Assert(err, IsNil)
+	c.Assert(len(staleLocks), Equals, 0)
+
 	var (
 		lock1, lock2 kvstore.KVLocker
 		gotLock1     = make(chan struct{})
@@ -161,10 +166,6 @@ func (s *AllocatorSuite) TestRunLocksGC(c *C) {
 		c.Assert(err, IsNil)
 		close(gotLock2)
 	}()
-	staleLocks := map[string]kvstore.Value{}
-	staleLocks, err = allocator.RunLocksGC(staleLocks)
-	c.Assert(err, IsNil)
-	c.Assert(len(staleLocks), Equals, 0)
 
 	// Wait until lock1 is gotten.
 	c.Assert(testutils.WaitUntil(func() bool {
