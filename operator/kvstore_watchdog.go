cilium/cilium

commit 440539b7604aa50557d02b13e01a3027b6209b03

garbage collect stale distributed locks
Under special conditions [1], cilium-agent could leak distributed locks
into the KVStore making all cilium-agents to deadlock. To avoid this
cilium-operator will garbage collect stale distributed locks. If a lock
is being held for more than 2x default value of cilium's lock lease TTL,
cilium-operator will delete that lock from the KVStore. On a first sight
this might not be reasonable but cilium-agents only use the distributed
locks to coordinated across each other which one should pick the
security identity for a new set of labels. Even if 2 cilium-agents pick
2 different security identities for the same set of labels, only the
first one will be able to publish that identity into the KVStore. The
2nd agent will fail to publish the security identity that it picked for
the set of labels:

[1]
1) connect to a etcd member (https://node-A:2379) to create a lock with
   a 25 second lease (the default for cilium)
2) drop all peer packets, not client packets, to and from that etcd
   member (https://node-A:2380).
3) try releasing that lock immediately. The unlock will fail with the
   error level=error msg="Unable to unlock kvstore lock"
   error="etcdserver: request timed out".
4) As soon the unlock fails, resume connectivity dropped in 2), this
   re-connectivity needs to happen before the lease expires, which means
   the Unlock on step 3) needs to fail in less than 25 seconds, this is
   what happen for my case.
5) since the client resumed connectivity with the server in less than 25
   seconds, the lease didn't expire which means the server still thinks
   the client is still holding the lock but the client assumes the lock was
   unlocked.

Signed-off-by: André Martins <andre@cilium.io>

operator/kvstore_watchdog.go
@@ -1,0 +1,80 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/cilium/cilium/pkg/allocator"
+	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/kvstore"
+	kvstoreallocator "github.com/cilium/cilium/pkg/kvstore/allocator"
+)
+
+// keyPathFromLockPath returns the path of the given key that contains a lease
+// prefixed to its path.
+func keyPathFromLockPath(k string) string {
+	// vendor/go.etcd.io/etcd/clientv3/concurrency/mutex.go:L46
+	i := strings.LastIndexByte(k, '/')
+	if i >= 0 {
+		return k[:i]
+	}
+	return k
+}
+
+// getOldestLeases returns the value that has the smaller revision for each
+// 'path'. A 'path' shares the same common prefix for different locks.
+func getOldestLeases(m map[string]kvstore.Value) map[string]kvstore.Value {
+	oldestLeases := map[string]kvstore.Value{}
+	oldestPaths := map[string]kvstore.Value{}
+	for k, v := range m {
+		commonPath := keyPathFromLockPath(k)
+		oldestLease, ok := oldestPaths[commonPath]
+		if !ok || v.ModRevision < oldestLease.ModRevision {
+			oldestPaths[commonPath] = v
+			oldestLeases[k] = v
+		}
+	}
+	return oldestLeases
+}
+
+func startKvstoreWatchdog() {
+	log.Infof("Starting kvstore watchdog with %s interval...", defaults.LockLeaseTTL)
+	backend, err := kvstoreallocator.NewKVStoreBackend(cache.IdentitiesPath, "", nil)
+	if err != nil {
+		log.WithError(err).Fatal("Unable to initialize kvstore backend for identity garbage collection")
+	}
+	a := allocator.NewAllocatorForGC(backend)
+
+	keysToDelete := map[string]kvstore.Value{}
+	go func() {
+		for {
+			keysToDelete = getOldestLeases(keysToDelete)
+			ctx, cancel := context.WithTimeout(context.Background(), defaults.LockLeaseTTL)
+			keysToDelete2, err := a.RunLocksGC(ctx, keysToDelete)
+			if err != nil {
+				log.WithError(err).Warning("Unable to run security identity garbage collector")
+			} else {
+				keysToDelete = keysToDelete2
+			}
+			cancel()
+
+			<-time.After(defaults.LockLeaseTTL)
+		}
+	}()
+}

operator/kvstore_watchdog_test.go
@@ -1,0 +1,136 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/cilium/cilium/pkg/kvstore"
+)
+
+func Test_getOldestLeases(t *testing.T) {
+	type args struct {
+		m map[string]kvstore.Value
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]kvstore.Value
+	}{
+		{
+			name: "test-1",
+			args: args{
+				m: map[string]kvstore.Value{},
+			},
+			want: map[string]kvstore.Value{},
+		},
+		{
+			name: "test-2",
+			args: args{
+				m: map[string]kvstore.Value{
+					"foo/bar/1": {
+						Data:        nil,
+						ModRevision: 1,
+						LeaseID:     1,
+					},
+					"foo/bar/2": {
+						Data:        nil,
+						ModRevision: 2,
+						LeaseID:     2,
+					},
+					"foo/bar/3": {
+						Data:        nil,
+						ModRevision: 3,
+						LeaseID:     3,
+					},
+					"foo/baz/4": {
+						Data:        nil,
+						ModRevision: 4,
+						LeaseID:     4,
+					},
+				},
+			},
+			want: map[string]kvstore.Value{
+				"foo/bar/1": {
+					Data:        nil,
+					ModRevision: 1,
+					LeaseID:     1,
+				},
+				"foo/baz/4": {
+					Data:        nil,
+					ModRevision: 4,
+					LeaseID:     4,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getOldestLeases(tt.args.m); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getOldestLeases() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getPath(t *testing.T) {
+	type args struct {
+		k string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test-1",
+			args: args{
+				k: "cilium/state/identities/v1/locks/" +
+					"k8s:io.cilium.k8s.policy.cluster=default;" +
+					"k8s:io.cilium.k8s.policy.serviceaccount=default;" +
+					"k8s:io.kubernetes.pod.namespace=default;k8s:k8s-app.guestbook=redis;" +
+					"k8s:role=master;" +
+					"/29c66fd840fa06f7",
+			},
+			want: "cilium/state/identities/v1/locks/" +
+				"k8s:io.cilium.k8s.policy.cluster=default;" +
+				"k8s:io.cilium.k8s.policy.serviceaccount=default;" +
+				"k8s:io.kubernetes.pod.namespace=default;k8s:k8s-app.guestbook=redis;" +
+				"k8s:role=master;",
+		},
+		{
+			name: "test-2",
+			args: args{
+				k: "cilium/state/identities/v1/locks/" +
+					"k8s:io.cilium.k8s.policy.cluster=default;" +
+					"k8s:role=master/////;" +
+					"/29c66fd840fa06f7",
+			},
+			want: "cilium/state/identities/v1/locks/" +
+				"k8s:io.cilium.k8s.policy.cluster=default;" +
+				"k8s:role=master/////;",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := keyPathFromLockPath(tt.args.k); got != tt.want {
+				t.Errorf("keyPathFromLockPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

operator/main.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Authors of Cilium
+// Copyright 2018-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -320,6 +320,8 @@ func runOperator(cmd *cobra.Command) {
 				log.WithError(err).Error("Unable to setup node watcher")
 			}
 		}
+
+		startKvstoreWatchdog()
 	}
 
 	switch identityAllocationMode {

pkg/allocator/allocator.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -243,6 +243,14 @@ type Backend interface {
 	// Note: not all Backend implemenations rely on this, such as the kvstore
 	// backends, and may use leases to expire keys.
 	RunGC(ctx context.Context, staleKeysPrevRound map[string]uint64) (map[string]uint64, error)
+
+	// RunLocksGC reaps stale or unused locks within the Backend. It is used by
+	// the cilium-operator and is not invoked by cilium-agent. Returns
+	// a map of locks currently being held in the KVStore including the ones
+	// that failed to be GCed.
+	// Note: not all Backend implementations rely on this, such as the kvstore
+	// backends, and may use leases to expire keys.
+	RunLocksGC(ctx context.Context, staleKeysPrevRound map[string]kvstore.Value) (map[string]kvstore.Value, error)
 
 	// Status returns a human-readable status of the Backend.
 	Status() (string, error)
@@ -698,6 +706,11 @@ func (a *Allocator) Release(ctx context.Context, key AllocatorKey) (lastUse bool
 // RunGC scans the kvstore for unused master keys and removes them
 func (a *Allocator) RunGC(staleKeysPrevRound map[string]uint64) (map[string]uint64, error) {
 	return a.backend.RunGC(context.TODO(), staleKeysPrevRound)
+}
+
+// RunLocksGC scans the kvstore for stale locks and removes them
+func (a *Allocator) RunLocksGC(ctx context.Context, staleLocksPrevRound map[string]kvstore.Value) (map[string]kvstore.Value, error) {
+	return a.backend.RunLocksGC(ctx, staleLocksPrevRound)
 }
 
 // DeleteAllKeys will delete all keys. It is expected to be used in tests.

pkg/allocator/allocator_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -172,6 +172,10 @@ func (d *dummyBackend) ListAndWatch(ctx context.Context, handler CacheMutations,
 	d.mutex.Unlock()
 	d.handler.OnListDone()
 	<-stopChan
+}
+
+func (d *dummyBackend) RunLocksGC(_ context.Context, _ map[string]kvstore.Value) (map[string]kvstore.Value, error) {
+	return nil, nil
 }
 
 func (d *dummyBackend) RunGC(context.Context, map[string]uint64) (map[string]uint64, error) {

pkg/k8s/identitybackend/identity.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Authors of Cilium
+// Copyright 2019-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -202,6 +202,10 @@ func (c *crdBackend) AcquireReference(ctx context.Context, id idpool.ID, key all
 
 	_, err = identityOps.Update(identityCopy.CiliumIdentity)
 	return err
+}
+
+func (c *crdBackend) RunLocksGC(_ context.Context, _ map[string]kvstore.Value) (map[string]kvstore.Value, error) {
+	return nil, nil
 }
 
 func (c *crdBackend) RunGC(ctx context.Context, staleKeysPrevRound map[string]uint64) (map[string]uint64, error) {

pkg/kvstore/allocator/allocator.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -394,6 +394,51 @@ func (k *kvstoreBackend) Release(ctx context.Context, key allocator.AllocatorKey
 	// }
 
 	return nil
+}
+
+// RunLocksGC scans the kvstore for unused locks and removes them. Returns
+// a map of locks that are currently being held, including the ones that have
+// failed to be GCed.
+func (k *kvstoreBackend) RunLocksGC(ctx context.Context, staleKeysPrevRound map[string]kvstore.Value) (map[string]kvstore.Value, error) {
+	// fetch list of all /../locks keys
+	allocated, err := kvstore.ListPrefix(ctx, k.lockPrefix)
+	if err != nil {
+		return nil, fmt.Errorf("list failed: %s", err)
+	}
+
+	staleKeys := map[string]kvstore.Value{}
+
+	// iterate over /../locks
+	for key, v := range allocated {
+		scopedLog := log.WithFields(logrus.Fields{
+			fieldKey:     key,
+			fieldLeaseID: fmt.Sprintf("%x", v.LeaseID),
+		})
+		// Only delete if this key was previously marked as to be deleted
+		if modRev, ok := staleKeysPrevRound[key]; ok &&
+			// comparing ModRevision ensures the same client is still holding
+			// this lock since the last GC was called.
+			modRev.ModRevision == v.ModRevision &&
+			modRev.LeaseID == v.LeaseID &&
+			modRev.SessionID == v.SessionID {
+			if err := kvstore.Delete(ctx, key); err == nil {
+				scopedLog.Warning("Forcefully removed distributed lock due to client staleness." +
+					" Please check the connectivity between the KVStore and the client with that lease ID.")
+				continue
+			}
+			scopedLog.WithError(err).
+				Warning("Unable to remove distributed lock due to client staleness." +
+					" Please check the connectivity between the KVStore and the client with that lease ID.")
+		}
+		// If the key was not found mark it to be delete in the next RunGC
+		staleKeys[key] = kvstore.Value{
+			ModRevision: v.ModRevision,
+			LeaseID:     v.LeaseID,
+			SessionID:   v.SessionID,
+		}
+	}
+
+	return staleKeys, nil
 }
 
 // RunGC scans the kvstore for unused master keys and removes them

pkg/kvstore/allocator/allocator_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -39,7 +39,9 @@ func Test(t *testing.T) {
 	TestingT(t)
 }
 
-type AllocatorSuite struct{}
+type AllocatorSuite struct {
+	backend string
+}
 
 type AllocatorEtcdSuite struct {
 	AllocatorSuite
@@ -48,6 +50,7 @@ type AllocatorEtcdSuite struct {
 var _ = Suite(&AllocatorEtcdSuite{})
 
 func (e *AllocatorEtcdSuite) SetUpTest(c *C) {
+	e.backend = "etcd"
 	kvstore.SetupDummy("etcd")
 }
 
@@ -63,6 +66,7 @@ type AllocatorConsulSuite struct {
 var _ = Suite(&AllocatorConsulSuite{})
 
 func (e *AllocatorConsulSuite) SetUpTest(c *C) {
+	e.backend = "consul"
 	kvstore.SetupDummy("consul")
 }
 
@@ -109,6 +113,121 @@ func (s *AllocatorSuite) BenchmarkAllocate(c *C) {
 	}
 	c.StopTimer()
 
+}
+
+func (s *AllocatorSuite) TestRunLocksGC(c *C) {
+	allocatorName := randomTestName()
+	maxID := idpool.ID(256 + c.N)
+	// FIXME: Did this previousy use allocatorName := randomTestName() ? so TestAllocatorKey(randomeTestName())
+	backend1, err := NewKVStoreBackend(allocatorName, "a", TestAllocatorKey(""))
+	c.Assert(err, IsNil)
+	c.Assert(err, IsNil)
+	allocator, err := allocator.NewAllocator(TestAllocatorKey(""), backend1, allocator.WithMax(maxID), allocator.WithoutGC())
+	c.Assert(err, IsNil)
+	shortKey := TestAllocatorKey("1;")
+	var (
+		lock1, lock2 kvstore.KVLocker
+		gotLock1     = make(chan struct{})
+		gotLock2     = make(chan struct{})
+	)
+	go func() {
+		var (
+			err error
+		)
+		lock1, err = backend1.Lock(context.Background(), shortKey)
+		c.Assert(err, IsNil)
+		close(gotLock1)
+		var client kvstore.BackendOperations
+		switch s.backend {
+		case "etcd":
+			client, _ = kvstore.NewClient(context.Background(),
+				s.backend,
+				map[string]string{
+					kvstore.EtcdAddrOption: kvstore.EtcdDummyAddress(),
+				},
+				nil,
+			)
+		case "consul":
+			client, _ = kvstore.NewClient(context.Background(),
+				s.backend,
+				map[string]string{
+					kvstore.ConsulAddrOption:   kvstore.ConsulDummyAddress(),
+					kvstore.ConsulOptionConfig: kvstore.ConsulDummyConfigFile(),
+				},
+				nil,
+			)
+		}
+		lock2, err = client.LockPath(context.Background(), allocatorName+"/locks/"+shortKey.GetKey())
+		c.Assert(err, IsNil)
+		close(gotLock2)
+	}()
+	staleLocks := map[string]kvstore.Value{}
+	staleLocks, err = allocator.RunLocksGC(context.Background(), staleLocks)
+	c.Assert(err, IsNil)
+	c.Assert(len(staleLocks), Equals, 0)
+
+	// Wait until lock1 is gotten.
+	c.Assert(testutils.WaitUntil(func() bool {
+		select {
+		case <-gotLock1:
+			return true
+		default:
+			return false
+		}
+	}, 5*time.Second), IsNil)
+
+	// wait until client2, in line 160, tries to grab the lock.
+	// We can't detect when that actually happen so we have to assume it will
+	// happen within one second.
+	time.Sleep(time.Second)
+
+	// Check which locks are stale, it should be lock1 and lock2
+	staleLocks, err = allocator.RunLocksGC(context.Background(), staleLocks)
+	c.Assert(err, IsNil)
+	c.Assert(len(staleLocks), Equals, 2)
+
+	var (
+		oldestRev     uint64
+		oldestLeaseID int64
+		sessionID     string
+	)
+	// Stale locks contains 2 locks, which is expected but we only want to GC
+	// the oldest one so we can unlock all the remaining clients waiting to hold
+	// the lock.
+	for _, v := range staleLocks {
+		if v.ModRevision < oldestRev {
+			oldestRev = v.ModRevision
+			oldestLeaseID = v.LeaseID
+			sessionID = v.SessionID
+		}
+	}
+	staleLocks[allocatorName+"/locks/"+shortKey.GetKey()] = kvstore.Value{
+		ModRevision: oldestRev,
+		LeaseID:     oldestLeaseID,
+		SessionID:   sessionID,
+	}
+
+	// GC lock1 because it's the oldest lock being held.
+	staleLocks, err = allocator.RunLocksGC(context.Background(), staleLocks)
+	c.Assert(err, IsNil)
+	c.Assert(len(staleLocks), Equals, 0)
+
+	// Wait until lock2 is gotten as it should have happen since we have
+	// GC lock1.
+	c.Assert(testutils.WaitUntil(func() bool {
+		select {
+		case <-gotLock2:
+			return true
+		default:
+			return false
+		}
+	}, 10*time.Second), IsNil)
+
+	// Unlock lock1 because we still hold the local locks.
+	err = lock1.Unlock(context.Background())
+	c.Assert(err, IsNil)
+	err = lock2.Unlock(context.Background())
+	c.Assert(err, IsNil)
 }
 
 func (s *AllocatorSuite) TestGC(c *C) {

commit 8474525106a40a8c269f53b5352a7200186b9be1

operator: fix getOldestLeases logic
getOldestLeases was wrongly returning all the key paths found that were
older than the oldest path found. For example, having 2 lock paths that
shared the same key path would make getOldestLeases to return both lock
paths in case the first lock path found was considered younger than the
second lock path.

Fixes: 440539b7604a ("garbage collect stale distributed locks")
Signed-off-by: André Martins <andre@cilium.io>

operator/kvstore_watchdog.go
@@ -39,16 +39,27 @@ func keyPathFromLockPath(k string) string {
 
 // getOldestLeases returns the value that has the smaller revision for each
 // 'path'. A 'path' shares the same common prefix for different locks.
-func getOldestLeases(m map[string]kvstore.Value) map[string]kvstore.Value {
-	oldestLeases := map[string]kvstore.Value{}
-	oldestPaths := map[string]kvstore.Value{}
-	for k, v := range m {
-		commonPath := keyPathFromLockPath(k)
-		oldestLease, ok := oldestPaths[commonPath]
-		if !ok || v.ModRevision < oldestLease.ModRevision {
-			oldestPaths[commonPath] = v
-			oldestLeases[k] = v
+func getOldestLeases(lockPaths map[string]kvstore.Value) map[string]kvstore.Value {
+	type LockValue struct {
+		kvstore.Value
+		keyPath string
+	}
+	oldestPaths := map[string]LockValue{}
+	for lockPath, v := range lockPaths {
+		keyPath := keyPathFromLockPath(lockPath)
+		oldestKeyPath, ok := oldestPaths[keyPath]
+		if !ok || v.ModRevision < oldestKeyPath.ModRevision {
+			// Store the oldest common path
+			oldestPaths[keyPath] = LockValue{
+				keyPath: lockPath,
+				Value:   v,
+			}
 		}
+	}
+	oldestLeases := map[string]kvstore.Value{}
+	for _, v := range oldestPaths {
+		// Retrieve the oldest lock path
+		oldestLeases[v.keyPath] = v.Value
 	}
 	return oldestLeases
 }

operator/kvstore_watchdog_test.go
@@ -58,10 +58,25 @@ func Test_getOldestLeases(t *testing.T) {
 						ModRevision: 3,
 						LeaseID:     3,
 					},
-					"foo/baz/4": {
+					"foo/bar/4": {
 						Data:        nil,
 						ModRevision: 4,
 						LeaseID:     4,
+					},
+					"foo/bar/5": {
+						Data:        nil,
+						ModRevision: 5,
+						LeaseID:     5,
+					},
+					"foo/baz/6": {
+						Data:        nil,
+						ModRevision: 6,
+						LeaseID:     6,
+					},
+					"foo/bbz/7": {
+						Data:        nil,
+						ModRevision: 3,
+						LeaseID:     3,
 					},
 				},
 			},
@@ -71,10 +86,15 @@ func Test_getOldestLeases(t *testing.T) {
 					ModRevision: 1,
 					LeaseID:     1,
 				},
-				"foo/baz/4": {
+				"foo/baz/6": {
 					Data:        nil,
-					ModRevision: 4,
-					LeaseID:     4,
+					ModRevision: 6,
+					LeaseID:     6,
+				},
+				"foo/bbz/7": {
+					Data:        nil,
+					ModRevision: 3,
+					LeaseID:     3,
 				},
 			},
 		},