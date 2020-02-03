e692401fd5f0a0a03e79e1f93b56874959902a2b
Ray Bejjani <ray@isovalent.com>
2020-02-03 11:37:27 +0100

    CI: Determine and set K8s version in GKE pipeline
    GKE supports multiple versions of k8s. We need to set K8S_VERSION so our
    tests can match k8s. Since a cluster may arbitrarily change version
    (from the CI's perspective) we do this after we claim a cluster.
    
    Signed-off-by: Ray Bejjani <ray@isovalent.com>

---
+++ test/gke/get-cluster-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# get-cluster-version.sh prints the version of the selected cluster. It expects
+# the file 'cluster-name' to be populated. That file is created in
+# select-cluster.sh
+
+set -e
+
+zone=europe-west4-a
+
+
+CLUSTER_NAME=$(cat ./gke/cluster-name)
+K8S_VERSION=$(gcloud container clusters list --filter "name:${CLUSTER_NAME}" | awk '{print $3}' | grep -v MASTER_VERSION | sed -E 's/([0-9]+\.[0-9]+)\..*/\1/')
+echo ${K8S_VERSION}
