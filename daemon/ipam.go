6c07241f83e8d6a02ea544f839068aeeb40e1368
Thomas Graf <thomas@cilium.io>
2020-02-05 16:34:55 +0100

    ipam: Protect release from releasing alive IP
    It has been observed that kubelet calls CNI DELETE multiple times with
    potentially stale CNI result information. This can lead to a race condition
    where the initial CNI DELETE properly releases the IP in use which then gets
    reused by a different pod. Any subsequent CNI DELETE with the stale IP will
    then cause the IP of the live pod to be released. While the pod will continue
    to function, the next scheduled pod will attempt to use that IP and
    continuously fail to be scheduled due to a IP in use error.
    
    This is a regression of commit ab61853 which introduced the ability for CNI
    DELETE to release an IP even if the endpoint deletion fails which is required
    to fix the race condition when the CNI binary gets killed in between allocating
    an IP and creating the endpoint.
    
    Fixes: ab61853ca3 ("cni: Release IP even when endpoint deletion fails")
    Fixes: #10065
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- daemon/ipam.go
+++ daemon/ipam.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cilium/cilium/pkg/api"
 	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
@@ -110,6 +111,14 @@ func NewDeleteIPAMIPHandler(d *Daemon) ipamapi.DeleteIPAMIPHandler {
 }
 
 func (h *deleteIPAMIP) Handle(params ipamapi.DeleteIPAMIPParams) middleware.Responder {
+	// Release of an IP that is in use is not allowed
+	if ep := endpointmanager.LookupIPv4(params.IP); ep != nil {
+		return api.Error(ipamapi.DeleteIPAMIPFailureCode, fmt.Errorf("IP is in use by endpoint %d", ep.ID))
+	}
+	if ep := endpointmanager.LookupIPv6(params.IP); ep != nil {
+		return api.Error(ipamapi.DeleteIPAMIPFailureCode, fmt.Errorf("IP is in use by endpoint %d", ep.ID))
+	}
+
 	if err := h.daemon.ipam.ReleaseIPString(params.IP); err != nil {
 		return api.Error(ipamapi.DeleteIPAMIPFailureCode, err)
 	}
