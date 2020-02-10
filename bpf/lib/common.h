61f84a85da155133c7ce9026b2dff4ebeaa04b28
Thomas Graf <thomas@cilium.io>
2020-02-10 13:59:47 +0100

    bpf: Fix proxy redirection for egress programs
    [ upstream commit c58d749816d943c4f9426f67738013f4e8778463 ]
    
     * Handling of the proxy redirection was missing entirely in the to-container
       section for IPv6. Add it.
    
     * The to-container section case was assuming that any proxy redirection is
       indicated by ipv{46}_policy() returning a non-zero proxy port. This is no
       longer true since commit 830adba. Fix this by using a separate return code
       to indicate proxy redirection and treating the proxy port as optional.
    
    The above deficits lead to proxy redirection being ineffective when the setting
    EnableEndpointRoutes was set.
    
    Fixes: 830adba1c02 ("bpf: Support proxy using original source address and port.")
    Fixes: 25a80dfdd5e ("bpf: Add to-container section to bpf_lxc")
    Fixes: #10105
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

--- bpf/lib/common.h
+++ bpf/lib/common.h
@@ -274,6 +274,9 @@ enum {
  */
 #define IPSEC_ENDPOINT TC_ACT_OK
 
+/* Return value to indicate that proxy redirection is required */
+#define POLICY_ACT_PROXY_REDIRECT (1 << 16)
+
 /* Cilium error codes, must NOT overlap with TC return codes.
  * These also serve as drop reasons for metrics,
  * where reason > 0 corresponds to -(DROP_*)
