7fe5304748d7ee7b1ab6eda776363946f9af17a5
Martynas Pumputis <m@lambda.lt>
2020-02-11 15:10:37 +0100

    test: Remove runtime ipvlan tests
    https://github.com/cilium/cilium/pull/9447 removed the container runtime
    support which was required to set up properly ipvlan when running with
    libnetwork. This made tests obsolete.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/helpers/cilium.go
+++ test/helpers/cilium.go
@@ -905,11 +905,6 @@ func (s *SSHMeta) SetUpCiliumWithSockops() error {
 	return s.SetUpCiliumWithOptions("--sockops-enable --tofqdns-enable-poller=true")
 }
 
-// SetUpCiliumInIpvlanMode starts cilium-agent in the ipvlan mode
-func (s *SSHMeta) SetUpCiliumInIpvlanMode(ipvlanMasterDevice string) error {
-	return s.SetUpCiliumWithOptions("--tunnel=disabled --datapath-mode=ipvlan --ipvlan-master-device=" + ipvlanMasterDevice)
-}
-
 // WaitUntilReady waits until the output of `cilium status` returns with code
 // zero. Returns an error if the output of `cilium status` returns a nonzero
 // return code after the specified timeout duration has elapsed.
