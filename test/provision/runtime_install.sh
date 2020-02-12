cd943b8e8fec469987bd6150c4e1accdd5871022
Michal Rostecki <mrostecki@opensuse.org>
2020-02-12 17:40:44 +0100

    ci: Install bpftool from Cilium fork of the kernel
    [ upstream commit c4b6095d69fca4b9875d2c222bba434e818632d6 ]
    
    This should be done in packer-ci-build, but to get the fix faster, we
    install patched bpftool here as a temporary hack...
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- test/provision/runtime_install.sh
+++ test/provision/runtime_install.sh
@@ -12,5 +12,9 @@ sudo bash -c "echo MaxSessions 200 >> /etc/ssh/sshd_config"
 sudo systemctl restart ssh
 
 "${PROVISIONSRC}"/dns.sh
+# NOTE(mrostecki): Temporary hack until either:
+# 1) https://github.com/cilium/packer-ci-build/pull/187 gets merged.
+# 2) `bpftool feature filter_in/filter_out` gets upstreamed.
+"${PROVISIONSRC}"/bpftool.sh
 "${PROVISIONSRC}"/compile.sh
 "${PROVISIONSRC}"/wait-cilium.sh
