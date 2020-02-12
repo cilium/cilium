eba72ce05545cc80b8930b37e2b070e9a7cc9a88
Ray Bejjani <ray@isovalent.com>
2020-02-12 23:37:03 +0100

    kvstore/allocator: Keep internal reference to backend
    We previously only used single pkg/kvstore/allocator.kvstoreBackend
    instance system-wide. This also used a single, global, etcd/consul
    backend instance (in this case a backend to allocator.kvstoreBackend,
    itself the kvstore backend for the identity allocator).
    With clustermesh, cilium-agent needs to sync with remote etcd instances.
    This means that multiple etcd clients can exist, and that the identity
    allocator may use different ones. This is now possible by keeping
    an internal reference instead of using the global one.
    
    Signed-off-by: Ray Bejjani <ray@isovalent.com>

--- operator/kvstore_watchdog.go
+++ operator/kvstore_watchdog.go
@@ -66,7 +66,7 @@ func getOldestLeases(lockPaths map[string]kvstore.Value) map[string]kvstore.Valu
 
 func startKvstoreWatchdog() {
 	log.Infof("Starting kvstore watchdog with %s interval...", defaults.LockLeaseTTL)
-	backend, err := kvstoreallocator.NewKVStoreBackend(cache.IdentitiesPath, "", nil)
+	backend, err := kvstoreallocator.NewKVStoreBackend(cache.IdentitiesPath, "", nil, kvstore.Client())
 	if err != nil {
 		log.WithError(err).Fatal("Unable to initialize kvstore backend for identity garbage collection")
 	}
