fa66793215920987791cd4444fe19189ef8d8476
André Martins <andre@cilium.io>
2020-02-13 13:30:54 +0100

    golang: update to 1.13.8
    [ upstream commit 459925361e4a05f83c4366df93830c017e9a0425 ]
    
    Signed-off-by: André Martins <andre@cilium.io>
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

--- test/kubernetes-test.sh
+++ test/kubernetes-test.sh
@@ -53,6 +53,7 @@ git clone https://github.com/kubernetes/kubernetes.git -b ${KUBERNETES_VERSION} 
 cd kubernetes
 
 # Kubernetes is only compiling with golang 1.13.4 for versions >=1.17
+GO_VERSION="1.13.8"
 sudo rm -fr /usr/local/go
 curl https://dl.google.com/go/go1.13.7.linux-amd64.tar.gz > go1.13.7.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.13.7.linux-amd64.tar.gz
