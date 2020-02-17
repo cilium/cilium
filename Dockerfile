cilium/cilium

commit 459925361e4a05f83c4366df93830c017e9a0425

golang: update to 1.13.8
Signed-off-by: André Martins <andre@cilium.io>

Dockerfile
@@ -13,7 +13,7 @@ FROM quay.io/cilium/cilium-envoy:c31482c3e49670980c05cafc914320f7949b266f as cil
 # versions to be built while allowing the new versions to make changes
 # that are not backwards compatible.
 #
-FROM quay.io/cilium/cilium-builder:2020-01-29 as builder
+FROM quay.io/cilium/cilium-builder:2020-02-13 as builder
 LABEL maintainer="maintainer@cilium.io"
 WORKDIR /go/src/github.com/cilium/cilium
 COPY . ./

.travis.yml
@@ -4,7 +4,7 @@ dist: trusty
 sudo: required
 
 go:
- - 1.13.7
+ - 1.13.8
 
 if: branch = master OR type = pull_request
 
@@ -17,6 +17,6 @@ before_install: ./.travis/prepare.sh
 
 before_script:
   - export PATH=/usr/local/clang/bin:$PATH
-  - export GO=/home/travis/.gimme/versions/go1.13.7.linux.amd64/bin/go
+  - export GO=/home/travis/.gimme/versions/go1.13.8.linux.amd64/bin/go
 
 script: ./.travis/build.sh

Dockerfile.builder
@@ -14,7 +14,7 @@ WORKDIR /go/src/github.com/cilium/cilium
 ENV GOROOT /usr/local/go
 ENV GOPATH /go
 ENV PATH "$GOROOT/bin:$GOPATH/bin:$PATH"
-ENV GO_VERSION 1.13.7
+ENV GO_VERSION 1.13.8
 
 #
 # Build dependencies

GO_VERSION
@@ -1,1 +1,1 @@
-1.13.7
+1.13.8

cilium-docker-plugin.Dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/library/golang:1.13.7 as builder
+FROM docker.io/library/golang:1.13.8 as builder
 LABEL maintainer="maintainer@cilium.io"
 ADD . /go/src/github.com/cilium/cilium
 WORKDIR /go/src/github.com/cilium/cilium/plugins/cilium-docker

cilium-operator.Dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/library/golang:1.13.7 as builder
+FROM docker.io/library/golang:1.13.8 as builder
 LABEL maintainer="maintainer@cilium.io"
 ADD . /go/src/github.com/cilium/cilium
 WORKDIR /go/src/github.com/cilium/cilium/operator

contrib/packaging/docker/Dockerfile.runtime
@@ -81,7 +81,7 @@ apt-get clean
 #
 # Go-based tools we need at runtime
 #
-FROM docker.io/library/golang:1.13.7 as runtime-gobuild
+FROM docker.io/library/golang:1.13.8 as runtime-gobuild
 WORKDIR /tmp
 RUN go get -d github.com/google/gops && \
 cd /go/src/github.com/google/gops && \

test/docker-compose.yml
@@ -15,7 +15,7 @@ services:
     command: "etcd -name etcd0 -advertise-client-urls http://0.0.0.0:4002 -listen-client-urls http://0.0.0.0:4002 -initial-cluster-token etcd-cluster-1 -initial-cluster-state new"
     privileged: true
   base_image:
-    image: "quay.io/cilium/cilium-builder:2020-01-29"
+    image: "quay.io/cilium/cilium-builder:2020-02-13"
     volumes:
       - "./../:/go/src/github.com/cilium/cilium/"
     privileged: true

commit d6fd967f78a6ae240390682b0210eecde74a3066

update cilium-runtime with golang 1.13.8
Fixes: 459925361e4a ("golang: update to 1.13.8")
Signed-off-by: André Martins <andre@cilium.io>

Dockerfile
@@ -37,7 +37,7 @@ RUN make LOCKDEBUG=$LOCKDEBUG PKG_BUILD=1 V=$V LIBNETWORK_PLUGIN=$LIBNETWORK_PLU
 # built while allowing the new versions to make changes that are not
 # backwards compatible.
 #
-FROM quay.io/cilium/cilium-runtime:2020-02-12
+FROM quay.io/cilium/cilium-runtime:2020-02-17
 LABEL maintainer="maintainer@cilium.io"
 COPY --from=builder /tmp/install /
 COPY --from=cilium-envoy / /