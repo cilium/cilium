70c4ca3aaa4e41153b1087b6e15d718bf88ae9bc
Thomas Graf <thomas@cilium.io>
2020-02-17 13:20:20 +0100

    api: Add ability to return and specify address expiration
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- api/v1/client/ipam/post_ipam_parameters.go
+++ api/v1/client/ipam/post_ipam_parameters.go
@@ -13,6 +13,7 @@ import (
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
 	cr "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/swag"
 
 	strfmt "github.com/go-openapi/strfmt"
 )
@@ -61,6 +62,8 @@ for the post ipam operation typically these are written to a http.Request
 */
 type PostIpamParams struct {
 
+	/*Expiration*/
+	Expiration *bool
 	/*Family*/
 	Family *string
 	/*Owner*/
@@ -104,6 +107,17 @@ func (o *PostIpamParams) SetHTTPClient(client *http.Client) {
 	o.HTTPClient = client
 }
 
+// WithExpiration adds the expiration to the post ipam params
+func (o *PostIpamParams) WithExpiration(expiration *bool) *PostIpamParams {
+	o.SetExpiration(expiration)
+	return o
+}
+
+// SetExpiration adds the expiration to the post ipam params
+func (o *PostIpamParams) SetExpiration(expiration *bool) {
+	o.Expiration = expiration
+}
+
 // WithFamily adds the family to the post ipam params
 func (o *PostIpamParams) WithFamily(family *string) *PostIpamParams {
 	o.SetFamily(family)
@@ -133,6 +147,15 @@ func (o *PostIpamParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Regi
 		return err
 	}
 	var res []error
+
+	if o.Expiration != nil {
+
+		// header param expiration
+		if err := r.SetHeaderParam("expiration", swag.FormatBool(*o.Expiration)); err != nil {
+			return err
+		}
+
+	}
 
 	if o.Family != nil {
 
--- api/v1/server/restapi/ipam/post_ipam_parameters.go
+++ api/v1/server/restapi/ipam/post_ipam_parameters.go
@@ -11,6 +11,7 @@ import (
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/swag"
 	"github.com/go-openapi/validate"
 
 	strfmt "github.com/go-openapi/strfmt"
@@ -33,6 +34,10 @@ type PostIpamParams struct {
 	HTTPRequest *http.Request `json:"-"`
 
 	/*
+	  In: header
+	*/
+	Expiration *bool
+	/*
 	  In: query
 	*/
 	Family *string
@@ -53,6 +58,10 @@ func (o *PostIpamParams) BindRequest(r *http.Request, route *middleware.MatchedR
 
 	qs := runtime.Values(r.URL.Query())
 
+	if err := o.bindExpiration(r.Header[http.CanonicalHeaderKey("expiration")], true, route.Formats); err != nil {
+		res = append(res, err)
+	}
+
 	qFamily, qhkFamily, _ := qs.GetOK("family")
 	if err := o.bindFamily(qFamily, qhkFamily, route.Formats); err != nil {
 		res = append(res, err)
@@ -66,6 +75,28 @@ func (o *PostIpamParams) BindRequest(r *http.Request, route *middleware.MatchedR
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
+	return nil
+}
+
+// bindExpiration binds and validates parameter Expiration from header.
+func (o *PostIpamParams) bindExpiration(rawData []string, hasKey bool, formats strfmt.Registry) error {
+	var raw string
+	if len(rawData) > 0 {
+		raw = rawData[len(rawData)-1]
+	}
+
+	// Required: false
+
+	if raw == "" { // empty values pass all other validations
+		return nil
+	}
+
+	value, err := swag.ConvertBool(raw)
+	if err != nil {
+		return errors.InvalidType("expiration", "header", "bool", raw)
+	}
+	o.Expiration = &value
+
 	return nil
 }
 
