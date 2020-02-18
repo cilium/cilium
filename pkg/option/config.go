38188de407a18a99f63a1e8d0a8e534c251f0c3b
Michi Mutsuzaki <michi@isovalent.com>
2020-02-18 10:11:08 -0800

    daemon: Add options to launch Hubble
    Add options to launch Hubble with the following configuration parameters:
    
    - hubble-listen-addresses:
        List of IP addresses for Hubble server to listen to. Hubble is disabled
        if this list is empty. It also limits the addresses to be Unix domain
        socket for now.
    - hubble-flow-buffer-size:
        Maximum number of flows in Hubble's buffer.
    - hubble-metrics-server:
        Address to serve Hubble metrics on. The metrics server is disabled if
        this string is empty.
    - hubble-metrics:
        List of Hubble metrics to enable.
    
    Closes #9925
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -672,6 +672,18 @@ const (
 
 	// EnableRemoteNodeIdentity enables use of the remote-node identity
 	EnableRemoteNodeIdentity = "enable-remote-node-identity"
+
+	// HubbleListenAddresses specifies addresses for Hubble server to listen to.
+	HubbleListenAddresses = "hubble-listen-addresses"
+
+	// HubbleFlowBufferSize specifies the maximum number of flows in Hubble's buffer.
+	HubbleFlowBufferSize = "hubble-flow-buffer-size"
+
+	// HubbleMetricsServer specifies the addresses to serve Hubble metrics on.
+	HubbleMetricsServer = "hubble-metrics-server"
+
+	// HubbleMetrics specifies enabled metrics and their configuration options.
+	HubbleMetrics = "hubble-metrics"
 )
 
 // Default string arguments
@@ -1359,6 +1371,18 @@ type DaemonConfig struct {
 
 	// EnableRemoteNodeIdentity enables use of the remote-node identity
 	EnableRemoteNodeIdentity bool
+
+	// HubbleListenAddresses specifies addresses for Hubble to listen to.
+	HubbleListenAddresses []string
+
+	// HubbleFlowBufferSize specifies the maximum number of flows in Hubble's buffer.
+	HubbleFlowBufferSize int
+
+	// HubbleMetricsServer specifies the addresses to serve Hubble metrics on.
+	HubbleMetricsServer string
+
+	// HubbleMetrics specifies enabled metrics and their configuration options.
+	HubbleMetrics []string
 }
 
 var (
@@ -1943,6 +1967,12 @@ func (c *DaemonConfig) Populate() {
 			c.DisableCiliumEndpointCRD = false
 		}
 	}
+
+	// Hubble options.
+	c.HubbleListenAddresses = viper.GetStringSlice(HubbleListenAddresses)
+	c.HubbleFlowBufferSize = viper.GetInt(HubbleFlowBufferSize)
+	c.HubbleMetricsServer = viper.GetString(HubbleMetricsServer)
+	c.HubbleMetrics = viper.GetStringSlice(HubbleMetrics)
 
 	// Hidden options
 	c.ConfigFile = viper.GetString(ConfigFile)
