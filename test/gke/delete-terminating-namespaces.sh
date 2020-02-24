9c055a157708c0cb381e7e860144128112b8362c
Maciej Kwiek <maciej@isovalent.com>
2020-02-24 19:43:03 +0100

    ci: No error if there are no terminating ns
    test/gke/delete-terminating-namespaces.sh was erroring out when there
    were not terminating namespaces in cluster.
    
    This change adds a check to the script that fixes that.
    
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

--- test/gke/delete-terminating-namespaces.sh
+++ test/gke/delete-terminating-namespaces.sh
@@ -2,4 +2,6 @@
 
 # because of https://github.com/kubernetes/kubernetes/issues/60807 , we may end up with garbage terminating
 # namespaces leftovers after tests. This is a hacky workaround
-kubectl get ns | grep Terminating | awk '{print $1}' | xargs -n 1 bash -c 'kubectl get ns -o json $1 | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" | kubectl replace --raw /api/v1/namespaces/$1/finalize -f -' -
+kubectl get ns | \
+	awk '/Terminating/ {print $1}' | \
+   	xargs -n 1 bash -c 'if [ "$#" -ne 1 ]; then exit 0; fi; kubectl get ns -o json $1 | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" | kubectl replace --raw /api/v1/namespaces/$1/finalize -f -' -
