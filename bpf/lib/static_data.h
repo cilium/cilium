3f518564e6bc0d590b2a154f8a01c9c8b3b65e06
Daniel Borkmann <daniel@iogearbox.net>
2020-02-26 14:07:28 +0100

    bpf: split out utils into several lib headers
    Get a bit of order in here.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

---
+++ bpf/lib/static_data.h
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2016-2020 Authors of Cilium
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef __LIB_STATIC_DATA_H_
+#define __LIB_STATIC_DATA_H_
+
+#include <bpf/ctx/ctx.h>
+#include <bpf/api.h>
+
+/* fetch_* macros assist in fetching variously sized static data */
+#define fetch_u32(x) __fetch(x)
+#define fetch_u32_i(x, i) __fetch(x ## _ ## i)
+#define fetch_ipv6(x) fetch_u32_i(x, 1), fetch_u32_i(x, 2), fetch_u32_i(x, 3), fetch_u32_i(x, 4)
+#define fetch_mac(x) { { fetch_u32_i(x, 1), (__u16)fetch_u32_i(x, 2) } }
+
+/* DEFINE_* macros help to declare static data. */
+#define DEFINE_U32(NAME, value) __u32 NAME = value
+#define DEFINE_U32_I(NAME, i) __u32 NAME ## _ ## i
+#define DEFINE_IPV6(NAME, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16)	\
+DEFINE_U32_I(NAME, 1) = bpf_htonl( (a1) << 24 |  (a2) << 16 |  (a3) << 8 |  (a4));			\
+DEFINE_U32_I(NAME, 2) = bpf_htonl( (a5) << 24 |  (a6) << 16 |  (a7) << 8 |  (a8));			\
+DEFINE_U32_I(NAME, 3) = bpf_htonl( (a9) << 24 | (a10) << 16 | (a11) << 8 | (a12));			\
+DEFINE_U32_I(NAME, 4) = bpf_htonl((a13) << 24 | (a14) << 16 | (a15) << 8 | (a16))
+
+#define DEFINE_MAC(NAME, a1, a2, a3, a4, a5, a6)			\
+DEFINE_U32_I(NAME, 1) = (a1) << 24 | (a2) << 16 |  (a3) << 8 | (a4);	\
+DEFINE_U32_I(NAME, 2) =                            (a5) << 8 | (a6)
+
+#endif /* __LIB_STATIC_DATA_H_ */
