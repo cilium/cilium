c23f190d593b530422674684b81dd242dfd56915
Thomas Graf <thomas@cilium.io>
2020-02-27 11:23:59 +0100

    eni: Move Limits type to pkg/ipam
    This prepares implemenetation of IPAM limits in non ENI context.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ pkg/ipam/types/types.go
@@ -0,0 +1,28 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+// Limits specifies the IPAM relevant instance limits
+type Limits struct {
+	// Adapters specifies the maximum number of interfaces that can be
+	// attached to the instance
+	Adapters int
+
+	// IPv4 is the maximum number of IPv4 addresses per adapter/interface
+	IPv4 int
+
+	// IPv6 is the maximum number of IPv6 addresses per adapter/interface
+	IPv6 int
+}
