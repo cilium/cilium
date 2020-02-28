0d319d9a782c6f1f130ce5c96a35574716c74f45
Joe Stringer <joe@cilium.io>
2020-02-28 14:35:29 -0800

    deps: Bump grpc to v1.23.1
    Since version v1.19, grpc provided WithContextDialer() which is required
    for the following etcd client fix; Furthermore, v1.23 includes fixes for
    some known CVEs. v1.23.1 is used on master, so I'm bumping to that
    version here as well.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- vendor/google.golang.org/grpc/credentials/tls13.go
+++ vendor/google.golang.org/grpc/credentials/tls13.go
@@ -1,6 +1,8 @@
+// +build go1.12
+
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,20 +18,13 @@
  *
  */
 
-// Package envconfig contains grpc settings configured by environment variables.
-package envconfig
+package credentials
 
-import (
-	"os"
-	"strings"
-)
+import "crypto/tls"
 
-const (
-	prefix   = "GRPC_GO_"
-	retryStr = prefix + "RETRY"
-)
-
-var (
-	// Retry is set if retry is explicitly enabled via "GRPC_GO_RETRY=on".
-	Retry = strings.EqualFold(os.Getenv(retryStr), "on")
-)
+// This init function adds cipher suite constants only defined in Go 1.12.
+func init() {
+	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"
+	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"
+	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"
+}
