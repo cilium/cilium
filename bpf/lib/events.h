e2f77c6bf2007d43b6a1a42f7eee483c0c61e443
Daniel Borkmann <daniel@iogearbox.net>
2020-03-02 11:09:44 +0100

    bpf: switch .c/.h to spdx license identifier
    Remove boilerplate and switch data path to spdx license identifiers
    similarly as has been done in the Linux kernel long ago.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/lib/events.h
+++ bpf/lib/events.h
@@ -1,20 +1,6 @@
-/*
- *  Copyright (C) 2016-2017 Authors of Cilium
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2016-2020 Authors of Cilium */
+
 #ifndef __LIB_EVENTS_H_
 #define __LIB_EVENTS_H_
 
@@ -28,4 +14,4 @@ struct bpf_elf_map __section_maps EVENTS_MAP = {
 	.max_elem	= __NR_CPUS__,
 };
 
-#endif
+#endif /* __LIB_EVENTS_H_ */
