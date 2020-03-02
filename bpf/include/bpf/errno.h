e04854f7c139174c3469539e654028931a2f3524
Daniel Borkmann <daniel@iogearbox.net>
2020-03-02 09:23:06 +0100

    bpf: move errno codes into separate errno header
    Move errno codes into its own include/bpf/errno.h header since it's
    basic API for all type of programs we use.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/include/bpf/errno.h
+++ bpf/include/bpf/errno.h
@@ -15,36 +15,28 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-#ifndef __BPF_API__
-#define __BPF_API__
+#ifndef __BPF_ERRNO__
+#define __BPF_ERRNO__
 
-#include <linux/types.h>
-#include <linux/byteorder.h>
-#include <linux/bpf.h>
-#include <linux/if_packet.h>
+/* Few basic errno codes as we don't want to include errno.h. */
 
-#include "compiler.h"
-#include "section.h"
-#include "helpers.h"
-#include "builtins.h"
-#include "errno.h"
-
-#define PIN_NONE		0
-#define PIN_OBJECT_NS		1
-#define PIN_GLOBAL_NS		2
-
-struct bpf_elf_map {
-	__u32 type;
-	__u32 size_key;
-	__u32 size_value;
-	__u32 max_elem;
-	__u32 flags;
-	__u32 id;
-	__u32 pinning;
-#ifdef SOCKMAP
-	__u32 inner_id;
-	__u32 inner_idx;
+#ifndef EPERM
+# define EPERM		1
 #endif
-};
+#ifndef ENOENT
+# define ENOENT		2
+#endif
+#ifndef ENXIO
+# define ENXIO		6
+#endif
+#ifndef ENOMEM
+# define ENOMEM		12
+#endif
+#ifndef ENOTSUP
+# define ENOTSUP	95
+#endif
+#ifndef EADDRINUSE
+# define EADDRINUSE	98
+#endif
 
-#endif /* __BPF_API__ */
+#endif /* __BPF_ERRNO__ */
