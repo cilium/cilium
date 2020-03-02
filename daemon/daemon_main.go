8ac0ce7ed08e3b591b08181e479c4107e980d434
André Martins <andre@cilium.io>
2020-03-02 21:07:30 +0100

    daemon: fix cilium-agent helper message for disable-cnp-status-updates
    [ upstream commit 2e8756fe4c72ad4ad26b260359325d5c0c0c14b2 ]
    
    The helper message seems to print a string in front of it that should
    not have been printed.
    
    Fixes: 3aacf41d784e ("add capability to disable CNP NodeStatus updates")
    Signed-off-by: André Martins <andre@cilium.io>
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- daemon/daemon_main.go
+++ daemon/daemon_main.go
@@ -715,7 +715,7 @@ func init() {
 	flags.MarkHidden(option.PolicyTriggerInterval)
 	option.BindEnv(option.PolicyTriggerInterval)
 
-	flags.Bool(option.DisableCNPStatusUpdates, false, "Do not send CNP NodeStatus updates to the Kubernetes api-server (recommended to run with `cnp-node-status-gc=false` in cilium-operator)")
+	flags.Bool(option.DisableCNPStatusUpdates, false, `Do not send CNP NodeStatus updates to the Kubernetes api-server (recommended to run with "cnp-node-status-gc=false" in cilium-operator)`)
 	option.BindEnv(option.DisableCNPStatusUpdates)
 
 	viper.BindPFlags(flags)
