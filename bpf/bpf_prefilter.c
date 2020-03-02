a4574c30caae31f876f75fe81f43925fff594b66
Daniel Borkmann <daniel@iogearbox.net>
2020-03-02 09:41:30 +0100

    bpf, xdp: migrate prefilter program to generic __ctx_buff
    As the subject says, convert prefilter over to generic __ctx_buff.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/bpf_prefilter.c
+++ bpf/bpf_prefilter.c
@@ -82,24 +82,23 @@ struct bpf_elf_map __section_maps CIDR6_LMAP_NAME = {
 #endif /* CIDR6_LPM_PREFILTER */
 #endif /* CIDR6_FILTER */
 
-static __always_inline int check_v4_endpoint(struct xdp_md *xdp,
-					     struct iphdr *ipv4_hdr)
+static __always_inline int check_v4_endpoint(struct iphdr *ipv4_hdr)
 {
 	if (lookup_ip4_endpoint(ipv4_hdr))
-		return XDP_PASS;
+		return CTX_ACT_OK;
 
-	return XDP_DROP;
+	return CTX_ACT_DROP;
 }
 
-static __always_inline int check_v4(struct xdp_md *xdp)
+static __always_inline int check_v4(struct __ctx_buff *ctx)
 {
-	void *data_end = xdp_data_end(xdp);
-	void *data = xdp_data(xdp);
+	void *data_end = ctx_data_end(ctx);
+	void *data = ctx_data(ctx);
 	struct iphdr *ipv4_hdr = data + sizeof(struct ethhdr);
 	struct lpm_v4_key pfx __maybe_unused;
 
-	if (xdp_no_room(ipv4_hdr + 1, data_end))
-		return XDP_DROP;
+	if (ctx_no_room(ipv4_hdr + 1, data_end))
+		return CTX_ACT_DROP;
 
 #ifdef CIDR4_FILTER
 	__builtin_memcpy(pfx.lpm.data, &ipv4_hdr->saddr, sizeof(pfx.addr));
@@ -107,34 +106,33 @@ static __always_inline int check_v4(struct xdp_md *xdp)
 
 #ifdef CIDR4_LPM_PREFILTER
 	if (map_lookup_elem(&CIDR4_LMAP_NAME, &pfx))
-		return XDP_DROP;
+		return CTX_ACT_DROP;
 	else
 #endif /* CIDR4_LPM_PREFILTER */
 		return map_lookup_elem(&CIDR4_HMAP_NAME, &pfx) ?
-		       XDP_DROP : check_v4_endpoint(xdp, ipv4_hdr);
+		       CTX_ACT_DROP : check_v4_endpoint(ipv4_hdr);
 #else
-	return check_v4_endpoint(xdp, ipv4_hdr);
+	return check_v4_endpoint(ipv4_hdr);
 #endif /* CIDR4_FILTER */
 }
 
-static __always_inline int check_v6_endpoint(struct xdp_md *xdp,
-					     struct ipv6hdr *ipv6_hdr)
+static __always_inline int check_v6_endpoint(struct ipv6hdr *ipv6_hdr)
 {
 	if (lookup_ip6_endpoint(ipv6_hdr))
-		return XDP_PASS;
+		return CTX_ACT_OK;
 
-	return XDP_DROP;
+	return CTX_ACT_DROP;
 }
 
-static __always_inline int check_v6(struct xdp_md *xdp)
+static __always_inline int check_v6(struct __ctx_buff *ctx)
 {
-	void *data_end = xdp_data_end(xdp);
-	void *data = xdp_data(xdp);
+	void *data_end = ctx_data_end(ctx);
+	void *data = ctx_data(ctx);
 	struct ipv6hdr *ipv6_hdr = data + sizeof(struct ethhdr);
 	struct lpm_v6_key pfx __maybe_unused;
 
-	if (xdp_no_room(ipv6_hdr + 1, data_end))
-		return XDP_DROP;
+	if (ctx_no_room(ipv6_hdr + 1, data_end))
+		return CTX_ACT_DROP;
 
 #ifdef CIDR6_FILTER
 	__builtin_memcpy(pfx.lpm.data, &ipv6_hdr->saddr, sizeof(pfx.addr));
@@ -142,42 +140,42 @@ static __always_inline int check_v6(struct xdp_md *xdp)
 
 #ifdef CIDR6_LPM_PREFILTER
 	if (map_lookup_elem(&CIDR6_LMAP_NAME, &pfx))
-		return XDP_DROP;
+		return CTX_ACT_DROP;
 	else
 #endif /* CIDR6_LPM_PREFILTER */
 		return map_lookup_elem(&CIDR6_HMAP_NAME, &pfx) ?
-		       XDP_DROP : check_v6_endpoint(xdp, ipv6_hdr);
+		       CTX_ACT_DROP : check_v6_endpoint(ipv6_hdr);
 #else
-	return check_v6_endpoint(xdp, ipv6_hdr);
+	return check_v6_endpoint(ipv6_hdr);
 #endif /* CIDR6_FILTER */
 }
 
-static __always_inline int check_filters(struct xdp_md *xdp)
+static __always_inline int check_filters(struct __ctx_buff *ctx)
 {
-	void *data_end = xdp_data_end(xdp);
-	void *data = xdp_data(xdp);
+	void *data_end = ctx_data_end(ctx);
+	void *data = ctx_data(ctx);
 	struct ethhdr *eth = data;
 	__u16 proto;
 
-	if (xdp_no_room(eth + 1, data_end))
-		return XDP_DROP;
+	if (ctx_no_room(eth + 1, data_end))
+		return CTX_ACT_DROP;
 
 	proto = eth->h_proto;
 	if (proto == bpf_htons(ETH_P_IP))
-		return check_v4(xdp);
+		return check_v4(ctx);
 	else if (proto == bpf_htons(ETH_P_IPV6))
-		return check_v6(xdp);
+		return check_v6(ctx);
 	else
 		/* Pass the rest to stack, we might later do more
 		 * fine-grained filtering here.
 		 */
-		return XDP_PASS;
+		return CTX_ACT_OK;
 }
 
 __section("from-netdev")
-int xdp_start(struct xdp_md *xdp)
+int prefilter_start(struct __ctx_buff *ctx)
 {
-	return check_filters(xdp);
+	return check_filters(ctx);
 }
 
 BPF_LICENSE("GPL");
