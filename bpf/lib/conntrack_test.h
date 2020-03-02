c672eaccd5ab06686848e4129c8e8c9f9d6b4385
Daniel Borkmann <daniel@iogearbox.net>
2020-03-02 11:09:44 +0100

    bpf: switch .c/.h to spdx license identifier
    Remove boilerplate and switch data path to spdx license identifiers
    similarly as has been done in the Linux kernel long ago.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/lib/conntrack_test.h
+++ bpf/lib/conntrack_test.h
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: GPL-2.0
-// Copyright (c) 2018-2019 Authors of Cilium
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2018-2020 Authors of Cilium */
 
 enum {
 	__TUPLE_EXIST,
