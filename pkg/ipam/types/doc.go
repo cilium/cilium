cilium/cilium

commit f0dfcb4c1dc3f8bf5c70d988ae9c825e3255cd48

k8s: CiliumNode: Move generic IPAM parmeters into Spec.IPAM
Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/ipam/types/zz_generated.deepcopy.go
@@ -1,0 +1,70 @@
+// +build !ignore_autogenerated
+
+// Copyright 2017-2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Code generated by deepcopy-gen. DO NOT EDIT.
+
+package types
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *IPAMSpec) DeepCopyInto(out *IPAMSpec) {
+	*out = *in
+	if in.Pool != nil {
+		in, out := &in.Pool, &out.Pool
+		*out = make(AllocationMap, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	if in.PodCIDRs != nil {
+		in, out := &in.PodCIDRs, &out.PodCIDRs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new IPAMSpec.
+func (in *IPAMSpec) DeepCopy() *IPAMSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(IPAMSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *IPAMStatus) DeepCopyInto(out *IPAMStatus) {
+	*out = *in
+	if in.Used != nil {
+		in, out := &in.Used, &out.Used
+		*out = make(AllocationMap, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new IPAMStatus.
+func (in *IPAMStatus) DeepCopy() *IPAMStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(IPAMStatus)
+	in.DeepCopyInto(out)
+	return out
+}

pkg/k8s/apis/cilium.io/v2/zz_generated.deepcopy.go
@@ -26,22 +26,6 @@ import (
 )
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
-func (in *AllocationIP) DeepCopyInto(out *AllocationIP) {
-	*out = *in
-	return
-}
-
-// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new AllocationIP.
-func (in *AllocationIP) DeepCopy() *AllocationIP {
-	if in == nil {
-		return nil
-	}
-	out := new(AllocationIP)
-	in.DeepCopyInto(out)
-	return out
-}
-
-// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *CiliumClusterwideNetworkPolicy) DeepCopyInto(out *CiliumClusterwideNetworkPolicy) {
 	*out = *in
 	if in.CiliumNetworkPolicy != nil {
@@ -503,57 +487,6 @@ func (in *HealthAddressingSpec) DeepCopy() *HealthAddressingSpec {
 		return nil
 	}
 	out := new(HealthAddressingSpec)
-	in.DeepCopyInto(out)
-	return out
-}
-
-// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
-func (in *IPAMSpec) DeepCopyInto(out *IPAMSpec) {
-	*out = *in
-	if in.Pool != nil {
-		in, out := &in.Pool, &out.Pool
-		*out = make(map[string]AllocationIP, len(*in))
-		for key, val := range *in {
-			(*out)[key] = val
-		}
-	}
-	if in.PodCIDRs != nil {
-		in, out := &in.PodCIDRs, &out.PodCIDRs
-		*out = make([]string, len(*in))
-		copy(*out, *in)
-	}
-	return
-}
-
-// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new IPAMSpec.
-func (in *IPAMSpec) DeepCopy() *IPAMSpec {
-	if in == nil {
-		return nil
-	}
-	out := new(IPAMSpec)
-	in.DeepCopyInto(out)
-	return out
-}
-
-// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
-func (in *IPAMStatus) DeepCopyInto(out *IPAMStatus) {
-	*out = *in
-	if in.Used != nil {
-		in, out := &in.Used, &out.Used
-		*out = make(map[string]AllocationIP, len(*in))
-		for key, val := range *in {
-			(*out)[key] = val
-		}
-	}
-	return
-}
-
-// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new IPAMStatus.
-func (in *IPAMStatus) DeepCopy() *IPAMStatus {
-	if in == nil {
-		return nil
-	}
-	out := new(IPAMStatus)
 	in.DeepCopyInto(out)
 	return out
 }

Makefile
@@ -273,6 +273,7 @@ generate-health-api: api/v1/health/openapi.yaml
 generate-k8s-api:
 	$(call generate_k8s_api_all,github.com/cilium/cilium/pkg/k8s/apis,"cilium.io:v2")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium/pkg/aws,"eni:types")
+	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium/pkg,"ipam:types")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium/pkg,"policy:api")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium,"pkg:loadbalancer")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium,"pkg:k8s")

pkg/aws/eni/node.go
@@ -82,7 +82,7 @@ type Node struct {
 
 	enis map[string]eniTypes.ENI
 
-	available map[string]v2.AllocationIP
+	available ipamTypes.AllocationMap
 
 	manager *NodeManager
 
@@ -118,6 +118,33 @@ type nodeStatistics struct {
 	// allocated or have not yet exhausted the ENI specific quota of
 	// addresses
 	remainingInterfaces int
+}
+
+// GetMaxAboveWatermark returns the max-above-watermark setting for an AWS node
+func (n *Node) GetMaxAboveWatermark() int {
+	if n.resource.Spec.IPAM.MaxAboveWatermark != 0 {
+		return n.resource.Spec.IPAM.MaxAboveWatermark
+	}
+	return n.resource.Spec.ENI.MaxAboveWatermark
+}
+
+// GetPreAllocate returns the pre-allocation setting for an AWS node
+func (n *Node) GetPreAllocate() int {
+	if n.resource.Spec.IPAM.PreAllocate != 0 {
+		return n.resource.Spec.IPAM.PreAllocate
+	}
+	if n.resource.Spec.ENI.PreAllocate != 0 {
+		return n.resource.Spec.ENI.PreAllocate
+	}
+	return defaults.ENIPreAllocation
+}
+
+// GetMinAllocate returns the minimum-allocation setting of an AWS node
+func (n *Node) GetMinAllocate() int {
+	if n.resource.Spec.IPAM.MinAllocate != 0 {
+		return n.resource.Spec.IPAM.MinAllocate
+	}
+	return n.resource.Spec.ENI.MinAllocate
 }
 
 func (n *Node) logger() *logrus.Entry {
@@ -161,10 +188,6 @@ func (n *Node) getNeededAddresses() int {
 }
 
 func calculateNeededIPs(availableIPs, usedIPs, preAllocate, minAllocate int) (neededIPs int) {
-	if preAllocate == 0 {
-		preAllocate = defaults.ENIPreAllocation
-	}
-
 	neededIPs = preAllocate - (availableIPs - usedIPs)
 	if neededIPs < 0 {
 		neededIPs = 0
@@ -235,7 +258,7 @@ func (n *Node) updatedResource(resource *v2.CiliumNode) bool {
 
 func (n *Node) recalculateLocked() {
 	n.enis = map[string]eniTypes.ENI{}
-	n.available = map[string]v2.AllocationIP{}
+	n.available = ipamTypes.AllocationMap{}
 	enis := n.manager.instancesAPI.GetENIs(n.resource.Spec.ENI.InstanceID)
 	// An ec2 instance has at least one ENI attached, no ENI found implies instance not found.
 	if len(enis) == 0 {
@@ -253,13 +276,13 @@ func (n *Node) recalculateLocked() {
 		}
 
 		for _, ip := range e.Addresses {
-			n.available[ip] = v2.AllocationIP{Resource: e.ID}
+			n.available[ip] = ipamTypes.AllocationIP{Resource: e.ID}
 		}
 	}
 	n.stats.usedIPs = len(n.resource.Status.IPAM.Used)
 	n.stats.availableIPs = len(n.available)
-	n.stats.neededIPs = calculateNeededIPs(n.stats.availableIPs, n.stats.usedIPs, n.resource.Spec.ENI.PreAllocate, n.resource.Spec.ENI.MinAllocate)
-	n.stats.excessIPs = calculateExcessIPs(n.stats.availableIPs, n.stats.usedIPs, n.resource.Spec.ENI.PreAllocate, n.resource.Spec.ENI.MinAllocate, n.resource.Spec.ENI.MaxAboveWatermark)
+	n.stats.neededIPs = calculateNeededIPs(n.stats.availableIPs, n.stats.usedIPs, n.GetPreAllocate(), n.GetMinAllocate())
+	n.stats.excessIPs = calculateExcessIPs(n.stats.availableIPs, n.stats.usedIPs, n.GetPreAllocate(), n.GetMinAllocate(), n.GetMaxAboveWatermark())
 
 	n.loggerLocked().WithFields(logrus.Fields{
 		"available":                 n.stats.availableIPs,
@@ -293,8 +316,8 @@ func (n *Node) ENIs() (enis map[string]eniTypes.ENI) {
 }
 
 // Pool returns the IP allocation pool available to the node
-func (n *Node) Pool() (pool map[string]v2.AllocationIP) {
-	pool = map[string]v2.AllocationIP{}
+func (n *Node) Pool() (pool ipamTypes.AllocationMap) {
+	pool = ipamTypes.AllocationMap{}
 	n.mutex.RLock()
 	for k, allocationIP := range n.available {
 		pool[k] = allocationIP
@@ -401,7 +424,7 @@ func (n *Node) allocateENI(ctx context.Context, s *types.Subnet, a *allocatableR
 	neededAddresses := n.stats.neededIPs
 	desc := "Cilium-CNI (" + n.resource.Spec.ENI.InstanceID + ")"
 	// Must allocate secondary ENI IPs as needed, up to ENI instance limit - 1 (reserve 1 for primary IP)
-	toAllocate := int64(math.IntMin(neededAddresses+nodeResource.Spec.ENI.MaxAboveWatermark, a.limits.IPv4-1))
+	toAllocate := int64(math.IntMin(neededAddresses+n.GetMaxAboveWatermark(), a.limits.IPv4-1))
 	// Validate whether request has already been fulfilled in the meantime
 	if toAllocate == 0 {
 		n.mutex.RUnlock()
@@ -603,7 +626,7 @@ func (n *Node) determineMaintenanceAction() (*allocatableResources, error) {
 
 	// Validate that the node still requires addresses to be allocated, the
 	// request may have been resolved in the meantime.
-	maxAllocate := n.stats.neededIPs + n.resource.Spec.ENI.MaxAboveWatermark
+	maxAllocate := n.stats.neededIPs + n.GetMaxAboveWatermark()
 	if n.stats.neededIPs == 0 {
 		return nil, nil
 	}
@@ -803,7 +826,7 @@ func (n *Node) SyncToAPIServer() (err error) {
 	// fall back to the controller based background interval to retry.
 	for retry := 0; retry < 2; retry++ {
 		if node.Status.IPAM.Used == nil {
-			node.Status.IPAM.Used = map[string]v2.AllocationIP{}
+			node.Status.IPAM.Used = ipamTypes.AllocationMap{}
 		}
 
 		node.Status.ENI.ENIs = n.ENIs()
@@ -838,11 +861,11 @@ func (n *Node) SyncToAPIServer() (err error) {
 
 	for retry := 0; retry < 2; retry++ {
 		if node.Spec.IPAM.Pool == nil {
-			node.Spec.IPAM.Pool = map[string]v2.AllocationIP{}
+			node.Spec.IPAM.Pool = ipamTypes.AllocationMap{}
 		}
 
-		if node.Spec.ENI.PreAllocate == 0 {
-			node.Spec.ENI.PreAllocate = defaults.ENIPreAllocation
+		if node.Spec.IPAM.PreAllocate == 0 {
+			node.Spec.IPAM.PreAllocate = defaults.ENIPreAllocation
 		}
 
 		node.Spec.IPAM.Pool = n.Pool()

pkg/aws/eni/node_manager_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cilium/cilium/pkg/aws/types"
 	"github.com/cilium/cilium/pkg/checker"
 	metricsmock "github.com/cilium/cilium/pkg/ipam/metrics/mock"
+	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/testutils"
@@ -146,18 +147,18 @@ func newCiliumNode(node, instanceID, instanceType, az, vpcID string, firstInterf
 				InstanceID:          instanceID,
 				InstanceType:        instanceType,
 				FirstInterfaceIndex: &firstInterfaceIndex,
-				PreAllocate:         preAllocate,
-				MinAllocate:         minAllocate,
 				AvailabilityZone:    az,
 				VpcID:               vpcID,
 			},
-			IPAM: v2.IPAMSpec{
-				Pool: map[string]v2.AllocationIP{},
+			IPAM: ipamTypes.IPAMSpec{
+				Pool:        ipamTypes.AllocationMap{},
+				PreAllocate: preAllocate,
+				MinAllocate: minAllocate,
 			},
 		},
 		Status: v2.NodeStatus{
-			IPAM: v2.IPAMStatus{
-				Used: map[string]v2.AllocationIP{},
+			IPAM: ipamTypes.IPAMStatus{
+				Used: ipamTypes.AllocationMap{},
 			},
 		},
 	}
@@ -173,19 +174,19 @@ func newCiliumNodeWithSGTags(node, instanceID, instanceType, az, vpcID string, s
 				InstanceID:          instanceID,
 				InstanceType:        instanceType,
 				FirstInterfaceIndex: &firstInterfaceIndex,
-				PreAllocate:         preAllocate,
-				MinAllocate:         minAllocate,
 				AvailabilityZone:    az,
 				VpcID:               vpcID,
 				SecurityGroupTags:   sgTags,
 			},
-			IPAM: v2.IPAMSpec{
-				Pool: map[string]v2.AllocationIP{},
+			IPAM: ipamTypes.IPAMSpec{
+				Pool:        ipamTypes.AllocationMap{},
+				PreAllocate: preAllocate,
+				MinAllocate: minAllocate,
 			},
 		},
 		Status: v2.NodeStatus{
-			IPAM: v2.IPAMStatus{
-				Used: map[string]v2.AllocationIP{},
+			IPAM: ipamTypes.IPAMStatus{
+				Used: ipamTypes.AllocationMap{},
 			},
 		},
 	}
@@ -194,12 +195,12 @@ func newCiliumNodeWithSGTags(node, instanceID, instanceType, az, vpcID string, s
 }
 
 func updateCiliumNode(cn *v2.CiliumNode, available, used int) *v2.CiliumNode {
-	cn.Spec.IPAM.Pool = map[string]v2.AllocationIP{}
+	cn.Spec.IPAM.Pool = ipamTypes.AllocationMap{}
 	for i := 0; i < used; i++ {
-		cn.Spec.IPAM.Pool[fmt.Sprintf("1.1.1.%d", i)] = v2.AllocationIP{Resource: "foo"}
+		cn.Spec.IPAM.Pool[fmt.Sprintf("1.1.1.%d", i)] = ipamTypes.AllocationIP{Resource: "foo"}
 	}
 
-	cn.Status.IPAM.Used = map[string]v2.AllocationIP{}
+	cn.Status.IPAM.Used = ipamTypes.AllocationMap{}
 	for ip, ipAllocation := range cn.Spec.IPAM.Pool {
 		if used > 0 {
 			delete(cn.Spec.IPAM.Pool, ip)
@@ -435,7 +436,7 @@ func (e *ENISuite) TestNodeManagerReleaseAddress(c *check.C) {
 
 	// Announce node, wait for IPs to become available
 	cn := newCiliumNode("node3", "i-testNodeManagerReleaseAddress-1", "m4.xlarge", "us-west-1", "vpc-1", 1, 4, 10)
-	cn.Spec.ENI.MaxAboveWatermark = 4
+	cn.Spec.IPAM.MaxAboveWatermark = 4
 	mngr.Update(cn)
 	c.Assert(testutils.WaitUntil(func() bool { return reachedAddressesNeeded(mngr, "node3", 0) }, 5*time.Second), check.IsNil)
 

pkg/aws/eni/types/types.go
@@ -36,6 +36,8 @@ type ENISpec struct {
 	// the PreAllocate and MaxAboveWatermark logic takes over to continue
 	// allocating IPs.
 	//
+	// OBSOLETE: This field is obsolete, please use Spec.IPAM.MinAllocate
+	//
 	// +optional
 	MinAllocate int `json:"min-allocate,omitempty"`
 
@@ -43,6 +45,8 @@ type ENISpec struct {
 	// available for allocation in the IPAMspec. It defines the buffer of
 	// addresses available immediately without requiring cilium-operator to
 	// get involved.
+	//
+	// OBSOLETE: This field is obsolete, please use Spec.IPAM.PreAllocate
 	//
 	// +optional
 	PreAllocate int `json:"pre-allocate,omitempty"`
@@ -53,6 +57,8 @@ type ENISpec struct {
 	// allocate IPs, e.g. when a new ENI is allocated, as many secondary
 	// IPs as possible are allocated. Limiting the amount can help reduce
 	// waste of IPs.
+	//
+	// OBSOLETE: This field is obsolete, please use Spec.IPAM.MaxAboveWatermark
 	//
 	// +optional
 	MaxAboveWatermark int `json:"max-above-watermark,omitempty"`

pkg/ipam/crd.go
@@ -23,6 +23,7 @@ import (
 
 	eniTypes "github.com/cilium/cilium/pkg/aws/eni/types"
 	"github.com/cilium/cilium/pkg/cidr"
+	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	"github.com/cilium/cilium/pkg/k8s"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/informer"
@@ -203,8 +204,12 @@ func (n *nodeStore) hasMinimumIPsInPool() (minimumReached bool, required, numAva
 	}
 
 	switch {
+	case n.ownNode.Spec.IPAM.MinAllocate != 0:
+		required = n.ownNode.Spec.IPAM.MinAllocate
 	case n.ownNode.Spec.ENI.MinAllocate != 0:
 		required = n.ownNode.Spec.ENI.MinAllocate
+	case n.ownNode.Spec.IPAM.PreAllocate != 0:
+		required = n.ownNode.Spec.IPAM.PreAllocate
 	case n.ownNode.Spec.ENI.PreAllocate != 0:
 		required = n.ownNode.Spec.ENI.PreAllocate
 	case option.Config.EnableHealthChecking:
@@ -289,7 +294,7 @@ func (n *nodeStore) refreshNode() error {
 	copy(staleCopyOfAllocators, n.allocators)
 	n.mutex.RUnlock()
 
-	node.Status.IPAM.Used = map[string]ciliumv2.AllocationIP{}
+	node.Status.IPAM.Used = ipamTypes.AllocationMap{}
 
 	for _, a := range staleCopyOfAllocators {
 		a.mutex.RLock()
@@ -320,7 +325,7 @@ func (n *nodeStore) addAllocator(allocator *crdAllocator) {
 }
 
 // allocate checks if a particular IP can be allocated or return an error
-func (n *nodeStore) allocate(ip net.IP) (*ciliumv2.AllocationIP, error) {
+func (n *nodeStore) allocate(ip net.IP) (*ipamTypes.AllocationIP, error) {
 	n.mutex.RLock()
 	defer n.mutex.RUnlock()
 
@@ -341,7 +346,7 @@ func (n *nodeStore) allocate(ip net.IP) (*ciliumv2.AllocationIP, error) {
 }
 
 // allocateNext allocates the next available IP or returns an error
-func (n *nodeStore) allocateNext(allocated map[string]ciliumv2.AllocationIP, family Family) (net.IP, *ciliumv2.AllocationIP, error) {
+func (n *nodeStore) allocateNext(allocated ipamTypes.AllocationMap, family Family) (net.IP, *ipamTypes.AllocationIP, error) {
 	n.mutex.RLock()
 	defer n.mutex.RUnlock()
 
@@ -383,7 +388,7 @@ type crdAllocator struct {
 
 	// allocated is a map of all allocated IPs indexed by the allocated IP
 	// represented as string
-	allocated map[string]ciliumv2.AllocationIP
+	allocated ipamTypes.AllocationMap
 
 	// family is the address family this allocator is allocator for
 	family Family
@@ -396,7 +401,7 @@ func newCRDAllocator(family Family, owner Owner, k8sEventReg K8sEventRegister) A
 	})
 
 	allocator := &crdAllocator{
-		allocated: map[string]ciliumv2.AllocationIP{},
+		allocated: ipamTypes.AllocationMap{},
 		family:    family,
 		store:     sharedNodeStore,
 	}
@@ -420,7 +425,7 @@ func deriveGatewayIP(eni eniTypes.ENI) string {
 	return net.IPv4(addr[0], addr[1], addr[2], addr[3]+1).String()
 }
 
-func (a *crdAllocator) buildAllocationResult(ip net.IP, ipInfo *ciliumv2.AllocationIP) (result *AllocationResult, err error) {
+func (a *crdAllocator) buildAllocationResult(ip net.IP, ipInfo *ipamTypes.AllocationIP) (result *AllocationResult, err error) {
 	result = &AllocationResult{IP: ip}
 
 	// In ENI mode, the Resource points to the ENI so we can derive the
@@ -494,7 +499,7 @@ func (a *crdAllocator) Release(ip net.IP) error {
 
 // markAllocated marks a particular IP as allocated and triggers the custom
 // resource update
-func (a *crdAllocator) markAllocated(ip net.IP, owner string, ipInfo ciliumv2.AllocationIP) {
+func (a *crdAllocator) markAllocated(ip net.IP, owner string, ipInfo ipamTypes.AllocationIP) {
 	ipInfo.Owner = owner
 	a.allocated[ip.String()] = ipInfo
 	a.store.refreshTrigger.TriggerWithReason(fmt.Sprintf("allocation of IP %s", ip.String()))

pkg/ipam/types/types.go
@@ -26,3 +26,83 @@ type Limits struct {
 	// IPv6 is the maximum number of IPv6 addresses per adapter/interface
 	IPv6 int
 }
+
+// AllocationIP is an IP which is available for allocation, or already
+// has been allocated
+type AllocationIP struct {
+	// Owner is the owner of the IP. This field is set if the IP has been
+	// allocated. It will be set to the pod name or another identifier
+	// representing the usage of the IP
+	//
+	// The owner field is left blank for an entry in Spec.IPAM.Pool and
+	// filled out as the IP is used and also added to Status.IPAM.Used.
+	//
+	// +optional
+	Owner string `json:"owner,omitempty"`
+
+	// Resource is set for both available and allocated IPs, it represents
+	// what resource the IP is associated with, e.g. in combination with
+	// AWS ENI, this will refer to the ID of the ENI
+	//
+	// +optional
+	Resource string `json:"resource,omitempty"`
+}
+
+// AllocationMap is a map of allocated IPs indexed by IP
+type AllocationMap map[string]AllocationIP
+
+// IPAMSpec is the IPAM specification of the node
+//
+// This structure is embedded into v2.CiliumNode
+type IPAMSpec struct {
+	// Pool is the list of IPs available to the node for allocation. When
+	// an IP is used, the IP will remain on this list but will be added to
+	// Status.IPAM.Used
+	//
+	// +optional
+	Pool AllocationMap `json:"pool,omitempty"`
+
+	// PodCIDRs is the list of CIDRs available to the node for allocation.
+	// When an IP is used, the IP will be added to Status.IPAM.Used
+	//
+	// +optional
+	PodCIDRs []string `json:"podCIDRs,omitempty"`
+
+	// MinAllocate is the minimum number of IPs that must be allocated when
+	// the node is first bootstrapped. It defines the minimum base socket
+	// of addresses that must be available. After reaching this watermark,
+	// the PreAllocate and MaxAboveWatermark logic takes over to continue
+	// allocating IPs.
+	//
+	// +optional
+	MinAllocate int `json:"min-allocate,omitempty"`
+
+	// PreAllocate defines the number of IP addresses that must be
+	// available for allocation in the IPAMspec. It defines the buffer of
+	// addresses available immediately without requiring cilium-operator to
+	// get involved.
+	//
+	// +optional
+	PreAllocate int `json:"pre-allocate,omitempty"`
+
+	// MaxAboveWatermark is the maximum number of addresses to allocate
+	// beyond the addresses needed to reach the PreAllocate watermark.
+	// Going above the watermark can help reduce the number of API calls to
+	// allocate IPs, e.g. when a new ENI is allocated, as many secondary
+	// IPs as possible are allocated. Limiting the amount can help reduce
+	// waste of IPs.
+	//
+	// +optional
+	MaxAboveWatermark int `json:"max-above-watermark,omitempty"`
+}
+
+// IPAMStatus is the IPAM status of a node
+//
+// This structure is embedded into v2.CiliumNode
+type IPAMStatus struct {
+	// Used lists all IPs out of Spec.IPAM.Pool which have been allocated
+	// and are in use.
+	//
+	// +optional
+	Used AllocationMap `json:"used,omitempty"`
+}

commit c764ceb6a28843f2a498e72c85930c092a275507

ipam/types: fix missing deep copy fields
Fixes: f0dfcb4c1dc3 ("k8s: CiliumNode: Move generic IPAM parmeters into Spec.IPAM")
Signed-off-by: André Martins <andre@cilium.io>

pkg/ipam/types/doc.go
@@ -1,0 +1,19 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +k8s:deepcopy-gen=package,register
+// +k8s:openapi-gen=true
+
+// +groupName=ipam
+package types

pkg/ipam/types/zz_generated.deepcopy.go
@@ -19,6 +19,44 @@
 package types
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *AllocationIP) DeepCopyInto(out *AllocationIP) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new AllocationIP.
+func (in *AllocationIP) DeepCopy() *AllocationIP {
+	if in == nil {
+		return nil
+	}
+	out := new(AllocationIP)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in AllocationMap) DeepCopyInto(out *AllocationMap) {
+	{
+		in := &in
+		*out = make(AllocationMap, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+		return
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new AllocationMap.
+func (in AllocationMap) DeepCopy() AllocationMap {
+	if in == nil {
+		return nil
+	}
+	out := new(AllocationMap)
+	in.DeepCopyInto(out)
+	return *out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *IPAMSpec) DeepCopyInto(out *IPAMSpec) {
 	*out = *in
 	if in.Pool != nil {
@@ -67,4 +105,141 @@ func (in *IPAMStatus) DeepCopy() *IPAMStatus {
 	out := new(IPAMStatus)
 	in.DeepCopyInto(out)
 	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Limits) DeepCopyInto(out *Limits) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Limits.
+func (in *Limits) DeepCopy() *Limits {
+	if in == nil {
+		return nil
+	}
+	out := new(Limits)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Subnet) DeepCopyInto(out *Subnet) {
+	*out = *in
+	if in.Tags != nil {
+		in, out := &in.Tags, &out.Tags
+		*out = make(Tags, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Subnet.
+func (in *Subnet) DeepCopy() *Subnet {
+	if in == nil {
+		return nil
+	}
+	out := new(Subnet)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in SubnetMap) DeepCopyInto(out *SubnetMap) {
+	{
+		in := &in
+		*out = make(SubnetMap, len(*in))
+		for key, val := range *in {
+			var outVal *Subnet
+			if val == nil {
+				(*out)[key] = nil
+			} else {
+				in, out := &val, &outVal
+				*out = new(Subnet)
+				(*in).DeepCopyInto(*out)
+			}
+			(*out)[key] = outVal
+		}
+		return
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new SubnetMap.
+func (in SubnetMap) DeepCopy() SubnetMap {
+	if in == nil {
+		return nil
+	}
+	out := new(SubnetMap)
+	in.DeepCopyInto(out)
+	return *out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in Tags) DeepCopyInto(out *Tags) {
+	{
+		in := &in
+		*out = make(Tags, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+		return
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Tags.
+func (in Tags) DeepCopy() Tags {
+	if in == nil {
+		return nil
+	}
+	out := new(Tags)
+	in.DeepCopyInto(out)
+	return *out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *VirtualNetwork) DeepCopyInto(out *VirtualNetwork) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new VirtualNetwork.
+func (in *VirtualNetwork) DeepCopy() *VirtualNetwork {
+	if in == nil {
+		return nil
+	}
+	out := new(VirtualNetwork)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in VirtualNetworkMap) DeepCopyInto(out *VirtualNetworkMap) {
+	{
+		in := &in
+		*out = make(VirtualNetworkMap, len(*in))
+		for key, val := range *in {
+			var outVal *VirtualNetwork
+			if val == nil {
+				(*out)[key] = nil
+			} else {
+				in, out := &val, &outVal
+				*out = new(VirtualNetwork)
+				**out = **in
+			}
+			(*out)[key] = outVal
+		}
+		return
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new VirtualNetworkMap.
+func (in VirtualNetworkMap) DeepCopy() VirtualNetworkMap {
+	if in == nil {
+		return nil
+	}
+	out := new(VirtualNetworkMap)
+	in.DeepCopyInto(out)
+	return *out
 }