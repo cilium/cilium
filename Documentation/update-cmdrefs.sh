b67b960b4273b148a6a57b938e8af0a9efeb26af
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-03-12 18:02:03 +0000

    docs: Simplify the build
    - Use docker image build cache better
    - Move `make check-build` logic into a script and make it more readable
    - Move `make cmdref` logic into a script
    
    Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

---
+++ Documentation/update-cmdrefs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cmdref_dir="${source_dir}/cmdref"
+
+generators=(
+    "../cilium/cilium cmdref -d"
+    "../daemon/cilium-agent --cmdref"
+    "../bugtool/cilium-bugtool cmdref -d"
+    "../cilium-health/cilium-health --cmdref"
+    "../operator/cilium-operator --cmdref"
+)
+
+for g in "${generators[@]}" ; do
+    "${source_dir}/${g}" "${cmdref_dir}"
+done
