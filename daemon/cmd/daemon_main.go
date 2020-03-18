5f8712c08ee66c25df365615c385bd3c9d12fc82
Martynas Pumputis <m@lambda.lt>
2020-03-18 17:45:55 +0100

    daemon: Check NodePort --device names and ifindices
    Check that ifindex of a given device does not overflow uin16, and that
    the device name does not contain special symbols used to parse in
    bpf/init.sh.
    
    The first restriction will go away once we introduce the surrogate id =>
    ifindex BPF map. While the second - when we migrate bpf/init.sh to Go.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net"
 	"os"
 	"path"
@@ -1543,6 +1544,27 @@ func initKubeProxyReplacementOptions() {
 			log.WithField(logfields.Interface, device).
 				Info("Using auto-derived device for BPF node port")
 			option.Config.Devices = []string{device}
+		}
+	}
+
+	if option.Config.EnableNodePort {
+		for _, iface := range option.Config.Devices {
+			link, err := netlink.LinkByName(iface)
+			if err != nil {
+				log.WithError(err).Fatalf("Cannot retrieve %s link", iface)
+			}
+			if strings.ContainsAny(iface, ",=;") {
+				// Because we pass IPV{4,6}_NODEPORT addresses to bpf/init.sh
+				// in a form "$IFACE_NAME1=$IPV{4,6}_ADDR1;$IFACE_NAME2=...",
+				// we need to restrict the iface names. Otherwise, bpf/init.sh
+				// won't properly parse the mappings.
+				log.Fatalf("%s link name contains ',', '=' or ';' character which is not allowed")
+			}
+			if idx := link.Attrs().Index; idx > math.MaxUint16 {
+				// TODO(brb) after adding surrogate id => ifindex BPF map,
+				// the restriction can be lifted
+				log.Fatalf("%s link ifindex %d exceeds max(uint16)", iface, idx)
+			}
 		}
 	}
 
