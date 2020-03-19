738b77789d3c3144015de9a4e9e1adaf5a97f354
Martynas Pumputis <m@lambda.lt>
2020-03-19 10:37:38 +0100

    cli: Add cilium bpf ipmasq list
    The cmd lists all CIDRs in the ip-masq-agent BPF LPM map.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- bugtool/cmd/configuration.go
+++ bugtool/cmd/configuration.go
@@ -244,6 +244,7 @@ func copyCiliumInfoCommands(cmdDir string, k8sPods []string) []string {
 		"cilium bpf endpoint list",
 		"cilium bpf ct list global",
 		"cilium bpf nat list",
+		"cilium bpf ipmasq list",
 		"cilium bpf proxy list",
 		"cilium bpf ipcache list",
 		"cilium bpf policy get --all --numeric",
