cilium/cilium

commit 6778d029d1b75c07992d9f832760100c05218c72

endpoint: Only rewrite headerfile when ep changes
The endpoint headerfile is used for two purposes:
(1) to provide static data to compile/rewrite/load the datapath for the
endpoint; and
(2) to store information about the endpoint across restart/upgrade.

This commit relies upon loader.EndpointHash(e) to determine whether any
datapath variables have changed, and hence whether to rewrite the
endpoint headerfile. While this strictly speaking only takes into
account the (1) purpose above, in practice this covers almost every
field that may change after the initial regeneration (where the
headerfile would always be written):

+=======================+=========================+=====================+
| Field                 | Part of EndpointHash()? | Changes at runtime? |
+=======================+=========================+=====================+
| ID                    | No                      | No                  |
| ContainerName         | No                      | No                  |
| dockerID              | No                      | No                  |
| DockerNetworkID       | No                      | No                  |
| DockerEndpointID      | No                      | No                  |
| DatapathMapID         | Yes                     | Yes                 |
| IfName                | No                      | No                  |
| IfIndex               | Yes                     | No                  |
| OpLabels              | No                      | Yes(1)              |
| LXCMAC                | Yes                     | No                  |
| IPv6                  | Yes                     | No                  |
| IPv4                  | Yes                     | No                  |
| NodeMAC               | Yes                     | No                  |
| SecLabel              | Yes                     | Yes                 |
| Options               | Yes                     | Yes                 |

pkg/endpoint/bpf.go
@@ -699,26 +699,24 @@ func (e *Endpoint) runPreCompilationSteps(owner Owner, regenContext *regeneratio
 		stats.prepareBuild.End(preCompilationError == nil)
 	}()
 
-	// Generate header file specific to this endpoint for use in compiling
-	// BPF programs for this endpoint.
-	if err = e.writeHeaderfile(nextDir, owner); err != nil {
-		return fmt.Errorf("unable to write header file: %s", err)
-	}
-
 	// Avoid BPF program compilation and installation if the headerfile for the endpoint
 	// or the node have not changed.
+	var changed bool
 	datapathRegenCtxt.bpfHeaderfilesHash, err = loader.EndpointHash(e)
 	if err != nil {
 		e.getLogger().WithError(err).Warn("Unable to hash header file")
 		datapathRegenCtxt.bpfHeaderfilesHash = ""
-		datapathRegenCtxt.regenerationLevel = RegenerateWithDatapathRewrite
+		changed = true
 	} else {
-		changed := (datapathRegenCtxt.bpfHeaderfilesHash != e.bpfHeaderfileHash)
-		if changed {
-			datapathRegenCtxt.regenerationLevel = RegenerateWithDatapathRewrite
-		}
+		changed = (datapathRegenCtxt.bpfHeaderfilesHash != e.bpfHeaderfileHash)
 		e.getLogger().WithField(logfields.BPFHeaderfileHash, datapathRegenCtxt.bpfHeaderfilesHash).
 			Debugf("BPF header file hashed (was: %q)", e.bpfHeaderfileHash)
+	}
+	if changed {
+		datapathRegenCtxt.regenerationLevel = RegenerateWithDatapathRewrite
+		if err = e.writeHeaderfile(nextDir, owner); err != nil {
+			return fmt.Errorf("unable to write header file: %s", err)
+		}
 	}
 
 	// Cache endpoint information so that we can release the endpoint lock.

commit bca672dfe91c42c74e6f41c951ed91c42fb74bfd

endpoint: Fix bug where lxc_config.h disappears
In certain circumstances, it was possible for the endpoint lxc_config.h
headerfile to disappear if the endpoint was regenerated twice. In some
circumstances this could be mitigated by the presence of FQDN policy
which would recreate the file, but if the daemon was killed promptly
before the endpoint controller for storing FQDN state runs, or if the
user manually regenerates the endpoint multiple times, then the file
could be missing.

If you combine this with an agent restart, this may cause Cilium to
forget about managing an endpoint which it previously managed,
potentially leading later to connectivity issues when Cilium fails to
push policy entries for this endpoint to allow traffic to flow.

This patch fixes the issue by ensuring that if the headerfile is not
recreated during the regeneration process, the old one is moved on the
filesystem from the existing path into the new state dir path.

Fixes: 6778d029d1b7 ("endpoint: Only rewrite headerfile when ep changes")
Fixes: #10337
Fixes: #10603

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/endpoint/bpf.go
@@ -486,12 +486,15 @@ func (e *Endpoint) removeOldRedirects(desiredRedirects map[string]bool, proxyWai
 // ReloadDatapath forces the datapath programs to be reloaded. It does
 // not guarantee recompilation of the programs.
 // Must be called with endpoint.Mutex not held and endpoint.buildMutex held.
-// Returns the policy revision number when the regeneration has called, a
-// boolean if the BPF compilation was executed and an error in case of an error.
-func (e *Endpoint) regenerateBPF(regenContext *regenerationContext) (revnum uint64, compiled bool, reterr error) {
+//
+// Returns the policy revision number when the regeneration has called,
+// Whether the new state dir is populated with all new BPF state files, and
+// and an error if something failed.
+func (e *Endpoint) regenerateBPF(regenContext *regenerationContext) (revnum uint64, stateDirComplete bool, reterr error) {
 	var (
 		err                 error
 		compilationExecuted bool
+		headerfileChanged   bool
 	)
 
 	stats := &regenContext.Stats
@@ -508,7 +511,7 @@ func (e *Endpoint) regenerateBPF(regenContext *regenerationContext) (revnum uint
 	datapathRegenCtxt.prepareForProxyUpdates(regenContext.parentContext)
 	defer datapathRegenCtxt.completionCancel()
 
-	err = e.runPreCompilationSteps(regenContext)
+	headerfileChanged, err = e.runPreCompilationSteps(regenContext)
 
 	// Keep track of the side-effects of the regeneration that need to be
 	// reverted in case of failure.
@@ -596,7 +599,7 @@ func (e *Endpoint) regenerateBPF(regenContext *regenerationContext) (revnum uint
 		return 0, compilationExecuted, fmt.Errorf("unable to regenerate policy because PolicyMap synchronization failed: %s", err)
 	}
 
-	stateDirComplete := compilationExecuted
+	stateDirComplete = headerfileChanged && compilationExecuted
 	return datapathRegenCtxt.epInfoCache.revision, stateDirComplete, err
 }
 
@@ -651,7 +654,9 @@ func (e *Endpoint) realizeBPFState(regenContext *regenerationContext) (compilati
 // runPreCompilationSteps runs all of the regeneration steps that are necessary
 // right before compiling the BPF for the given endpoint.
 // The endpoint mutex must not be held.
-func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (preCompilationError error) {
+//
+// Returns whether the headerfile changed and/or an error.
+func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (headerfileChanged bool, preCompilationError error) {
 	stats := &regenContext.Stats
 	datapathRegenCtxt := regenContext.datapathRegenerationContext
 
@@ -659,7 +664,7 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 	err := e.lockAlive()
 	stats.waitingForLock.End(err == nil)
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	defer e.unlock()
@@ -694,7 +699,7 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 
 		// Compute policy for this endpoint.
 		if err = e.regeneratePolicy(); err != nil {
-			return fmt.Errorf("Unable to regenerate policy: %s", err)
+			return false, fmt.Errorf("Unable to regenerate policy: %s", err)
 		}
 
 		_ = e.updateAndOverrideEndpointOptions(nil)
@@ -702,27 +707,27 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 		// Dry mode needs Network Policy Updates, but the proxy wait group must
 		// not be initialized, as there is no proxy ACKing the changes.
 		if err, _ = e.updateNetworkPolicy(nil); err != nil {
-			return err
+			return false, err
 		}
 
 		if err = e.writeHeaderfile(nextDir); err != nil {
-			return fmt.Errorf("Unable to write header file: %s", err)
+			return false, fmt.Errorf("Unable to write header file: %s", err)
 		}
 
 		log.WithField(logfields.EndpointID, e.ID).Debug("Skipping bpf updates due to dry mode")
-		return nil
+		return false, nil
 	}
 
 	if e.policyMap == nil {
 		e.policyMap, _, err = policymap.OpenOrCreate(e.policyMapPath())
 		if err != nil {
-			return err
+			return false, err
 		}
 		// Clean up map contents
 		e.getLogger().Debug("flushing old PolicyMap")
 		err = e.policyMap.DeleteAll()
 		if err != nil {
-			return err
+			return false, err
 		}
 
 		// Also reset the in-memory state of the realized state as the
@@ -737,7 +742,7 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 		err = e.regeneratePolicy()
 		stats.policyCalculation.End(err == nil)
 		if err != nil {
-			return fmt.Errorf("unable to regenerate policy for '%s': %s", e.StringID(), err)
+			return false, fmt.Errorf("unable to regenerate policy for '%s': %s", e.StringID(), err)
 		}
 
 		_ = e.updateAndOverrideEndpointOptions(nil)
@@ -749,7 +754,7 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 		err, networkPolicyRevertFunc := e.updateNetworkPolicy(datapathRegenCtxt.proxyWaitGroup)
 		stats.proxyPolicyCalculation.End(err == nil)
 		if err != nil {
-			return err
+			return false, err
 		}
 		datapathRegenCtxt.revertStack.Push(networkPolicyRevertFunc)
 
@@ -766,7 +771,7 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 			desiredRedirects, err, finalizeFunc, revertFunc = e.addNewRedirects(datapathRegenCtxt.proxyWaitGroup)
 			stats.proxyConfiguration.End(err == nil)
 			if err != nil {
-				return err
+				return false, err
 			}
 			datapathRegenCtxt.finalizeList.Append(finalizeFunc)
 			datapathRegenCtxt.revertStack.Push(revertFunc)
@@ -782,7 +787,7 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 		err = e.syncPolicyMap()
 		stats.mapSync.End(err == nil)
 		if err != nil {
-			return fmt.Errorf("unable to regenerate policy because PolicyMap synchronization failed: %s", err)
+			return false, fmt.Errorf("unable to regenerate policy because PolicyMap synchronization failed: %s", err)
 		}
 
 		// At this point, traffic is no longer redirected to the proxy for
@@ -802,21 +807,20 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 
 	// Avoid BPF program compilation and installation if the headerfile for the endpoint
 	// or the node have not changed.
-	var changed bool
 	datapathRegenCtxt.bpfHeaderfilesHash, err = e.owner.Datapath().Loader().EndpointHash(e)
 	if err != nil {
 		e.getLogger().WithError(err).Warn("Unable to hash header file")
 		datapathRegenCtxt.bpfHeaderfilesHash = ""
-		changed = true
+		headerfileChanged = true
 	} else {
-		changed = (datapathRegenCtxt.bpfHeaderfilesHash != e.bpfHeaderfileHash)
+		headerfileChanged = (datapathRegenCtxt.bpfHeaderfilesHash != e.bpfHeaderfileHash)
 		e.getLogger().WithField(logfields.BPFHeaderfileHash, datapathRegenCtxt.bpfHeaderfilesHash).
 			Debugf("BPF header file hashed (was: %q)", e.bpfHeaderfileHash)
 	}
-	if changed {
+	if headerfileChanged {
 		datapathRegenCtxt.regenerationLevel = regeneration.RegenerateWithDatapathRewrite
 		if err = e.writeHeaderfile(nextDir); err != nil {
-			return fmt.Errorf("unable to write header file: %s", err)
+			return false, fmt.Errorf("unable to write header file: %s", err)
 		}
 	}
 
@@ -827,10 +831,10 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 		datapathRegenCtxt.epInfoCache = e.createEpInfoCache(currentDir)
 	}
 	if datapathRegenCtxt.epInfoCache == nil {
-		return fmt.Errorf("Unable to cache endpoint information")
+		return headerfileChanged, fmt.Errorf("Unable to cache endpoint information")
 	}
 
-	return nil
+	return headerfileChanged, nil
 }
 
 func (e *Endpoint) finalizeProxyState(regenContext *regenerationContext, err error) {