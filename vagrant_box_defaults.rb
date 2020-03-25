496f1becb3c791a39a911dc567ca15262e10b0e6
Daniel Borkmann <daniel@iogearbox.net>
2020-03-25 18:10:58 +0100

    vagrant: bump server VM image for clang/llvm
    Follow-up to cfc5e8c836c3 ("vagrant: bump development VM images
    for clang/llvm") in order to bump the SERVER_VERSION.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- vagrant_box_defaults.rb
+++ vagrant_box_defaults.rb
@@ -3,7 +3,7 @@
 Vagrant.require_version ">= 2.2.0"
 
 $SERVER_BOX = "cilium/ubuntu-dev"
-$SERVER_VERSION= "167"
+$SERVER_VERSION= "169"
 $NETNEXT_SERVER_BOX= "cilium/ubuntu-next"
 $NETNEXT_SERVER_VERSION= "48"
 @v419_SERVER_BOX= "cilium/ubuntu-4-19"
