cilium/cilium

commit e41b96574d89f0b5ffa9ff48f1157c2fb4695eee

pkg/option: ignore warnings if flags are not set
Signed-off-by: André Martins <andre@cilium.io>

pkg/option/config.go
@@ -2276,7 +2276,9 @@ func (c *DaemonConfig) populateNodePortRange() error {
 			return errors.New("NodePort range min port must be smaller than max port")
 		}
 	case 0:
-		log.Warning("NodePort range was set but is empty.")
+		if viper.IsSet(NodePortRange) {
+			log.Warning("NodePort range was set but is empty.")
+		}
 	default:
 		return fmt.Errorf("Unable to parse min/max port value for NodePort range: %s", NodePortRange)
 	}
@@ -2313,11 +2315,13 @@ func (c *DaemonConfig) populateHostServicesProtos() error {
 func sanitizeIntParam(paramName string, paramDefault int) int {
 	intParam := viper.GetInt(paramName)
 	if intParam <= 0 {
-		log.WithFields(
-			logrus.Fields{
-				"parameter":    paramName,
-				"defaultValue": paramDefault,
-			}).Warning("user-provided parameter had value <= 0 , which is invalid ; setting to default")
+		if !viper.IsSet(paramName) {
+			log.WithFields(
+				logrus.Fields{
+					"parameter":    paramName,
+					"defaultValue": paramDefault,
+				}).Warning("user-provided parameter had value <= 0 , which is invalid ; setting to default")
+		}
 		return paramDefault
 	}
 	return intParam

commit c5106a7b5bd5e50f4bec4694b2a2b96b39210ae4

pkg/option: do not log warnings if flag is not set
The condition was not correct, this commit should fix it.

Fixes: e41b96574d89 ("pkg/option: ignore warnings if flags are not set")
Signed-off-by: André Martins <andre@cilium.io>

pkg/option/config.go
@@ -2395,7 +2395,7 @@ func (c *DaemonConfig) populateHostServicesProtos() error {
 func sanitizeIntParam(paramName string, paramDefault int) int {
 	intParam := viper.GetInt(paramName)
 	if intParam <= 0 {
-		if !viper.IsSet(paramName) {
+		if viper.IsSet(paramName) {
 			log.WithFields(
 				logrus.Fields{
 					"parameter":    paramName,