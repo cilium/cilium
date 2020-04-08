036d9f5bd1f0e335a0acf753b10264a2a4549402
Jarno Rajahalme <jarno@covalent.io>
2020-04-08 08:59:58 -0700

    policy: Separate implementation of PortRuleDNS
    So far PortRuleDNS has been defined as FQDNSelector, sharing the
    struct definition. Going forward, a separate specification will be
    needed due to their difference in semantics regarding both members
    being allowed at the same time (PortRuleDNS) v.s. not (FQDNSelector).
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- pkg/policy/api/fqdn.go
+++ pkg/policy/api/fqdn.go
@@ -33,6 +33,7 @@ var (
 	allowedPatternChars = regexp.MustCompile("^[-a-zA-Z0-9.*]+$") // the * inside the [] is a literal *
 )
 
+// 'MatchName' and 'MatchPattern' are mutually exclusive
 type FQDNSelector struct {
 	// MatchName matches literal DNS names. A trailing "." is automatically added
 	// when missing.
@@ -94,8 +95,31 @@ func (s *FQDNSelector) ToRegex() (*regexp.Regexp, error) {
 	return regex, err
 }
 
-// PortRuleDNS is a list of allowed DNS lookups.
-type PortRuleDNS FQDNSelector
+// PortRuleDNS is an allowed DNS lookup.
+// Both 'MatchName' and 'MatchPattern' can be present.
+type PortRuleDNS struct {
+	// MatchName matches literal DNS names. A trailing "." is automatically added
+	// when missing.
+	MatchName string `json:"matchName,omitempty"`
+
+	// MatchPattern allows using wildcards to match DNS names. All wildcards are
+	// case insensitive. The wildcards are:
+	// - "*" matches 0 or more DNS valid characters, and may occur anywhere in
+	// the pattern. As a special case a "*" as the leftmost character, without a
+	// following "." matches all subdomains as well as the name to the right.
+	// A trailing "." is automatically added when missing.
+	//
+	// Examples:
+	// `*.cilium.io` matches subomains of cilium at that level
+	//   www.cilium.io and blog.cilium.io match, cilium.io and google.com do not
+	// `*cilium.io` matches cilium.io and all subdomains 1 level below
+	//   www.cilium.io, blog.cilium.io and cilium.io match, google.com does not
+	// sub*.cilium.io matches subdomains of cilium where the subdomain component
+	// begins with "sub"
+	//   sub.cilium.io and subdomain.cilium.io match, www.cilium.io,
+	//   blog.cilium.io, cilium.io and google.com do not
+	MatchPattern string `json:"matchPattern,omitempty"`
+}
 
 // Sanitize checks that the matchName in the portRule can be compiled as a
 // regex. It does not check that a DNS name is a valid DNS name.
