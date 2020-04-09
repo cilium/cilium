ee7a14d14f5445a9b095dd03f7aa132a90ebc5f1
André Martins <andre@cilium.io>
2020-04-09 22:08:16 +0200

    bump k8s dependencies and test to v1.18.1
    Signed-off-by: André Martins <andre@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -294,7 +294,7 @@ case $K8S_VERSION in
         sudo apt-get install -y conntrack
         KUBERNETES_CNI_VERSION="0.8.5"
         KUBERNETES_CNI_OS="-linux"
-        K8S_FULL_VERSION="1.18.0"
+        K8S_FULL_VERSION="1.18.1"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
