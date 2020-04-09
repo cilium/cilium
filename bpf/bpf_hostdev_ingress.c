49ef40964384ada97eabd730c98744012cdf9262
Joe Stringer <joe@cilium.io>
2020-04-08 17:05:22 -0700

    bpf: Make proxy / encrypt CB use more explicit
    Depending on the path through the datapath, skb->cb[0] may be used
    either for proxy magic port state transfer or encryption. Make these a
    bit more explicit using macros and enums.
    
    No functional changes.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- bpf/bpf_hostdev_ingress.c
+++ bpf/bpf_hostdev_ingress.c
@@ -10,20 +10,25 @@
 #include "lib/common.h"
 #include "lib/dbg.h"
 
+/* CB_PROXY_MAGIC overlaps with CB_ENCRYPT_MAGIC */
+#define ENCRYPT_OR_PROXY_MAGIC 0
+
 __section("to-host")
 int to_host(struct __ctx_buff *ctx)
 {
-	__u32 magic = ctx_load_meta(ctx, 0);
+	__u32 magic = ctx_load_meta(ctx, ENCRYPT_OR_PROXY_MAGIC);
+	__u32 src_label = 0;
 
 	if ((magic & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_ENCRYPT) {
-		ctx->mark = magic;
-		set_identity_mark(ctx, ctx_load_meta(ctx, 1));
+		ctx->mark = magic; // CB_ENCRYPT_MAGIC
+		src_label = ctx_load_meta(ctx, CB_ENCRYPT_IDENTITY);
+		set_identity_mark(ctx, src_label);
 	} else if ((magic & 0xFFFF) == MARK_MAGIC_TO_PROXY) {
 		/* Upper 16 bits may carry proxy port number */
 		__be16 port = magic >> 16;
 
 		ctx->mark = magic;
-		ctx_store_meta(ctx, 0, 0);
+		ctx_store_meta(ctx, 0, CB_PROXY_MAGIC);
 		ctx_change_type(ctx, PACKET_HOST);
 		cilium_dbg_capture(ctx, DBG_CAPTURE_PROXY_POST, port);
 	}
