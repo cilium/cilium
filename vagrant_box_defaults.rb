c98f7952f8bfb70e4f27c198e6f484d5fb47c271
Joe Stringer <joe@cilium.io>
2020-04-08 19:34:27 -0700

    vagrant: bump vagrant image
    Each build contains:
    
    - Bump of cached Istio container images
    
    net-next build also contains latest bpf tree on track for 5.7:
    
    - Fixes for BPF TPROXY
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- vagrant_box_defaults.rb
+++ vagrant_box_defaults.rb
@@ -3,8 +3,8 @@
 Vagrant.require_version ">= 2.2.0"
 
 $SERVER_BOX = "cilium/ubuntu-dev"
-$SERVER_VERSION= "169"
+$SERVER_VERSION= "170"
 $NETNEXT_SERVER_BOX= "cilium/ubuntu-next"
-$NETNEXT_SERVER_VERSION= "50"
+$NETNEXT_SERVER_VERSION= "52"
 @v419_SERVER_BOX= "cilium/ubuntu-4-19"
-@v419_SERVER_VERSION= "11"
+@v419_SERVER_VERSION= "12"
