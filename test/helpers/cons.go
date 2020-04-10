3d1711fe167798fa01052b9031e29c5dbd7786f2
Paul Chaignon <paul@cilium.io>
2020-04-10 16:57:20 +0200

    test: Fail in case of symbol substitution omission
    These warning messages generally indicate that a new map was introduced
    or renamed and the list of symbols in ignoredELFPrefixes wasn't updated.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- test/helpers/cons.go
+++ test/helpers/cons.go
@@ -204,17 +204,18 @@ const (
 	DockerBridgeIP = "172.17.0.1"
 
 	// Logs messages that should not be in the cilium logs.
-	panicMessage      = "panic:"
-	deadLockHeader    = "POTENTIAL DEADLOCK:"                        // from github.com/sasha-s/go-deadlock/deadlock.go:header
-	segmentationFault = "segmentation fault"                         // from https://github.com/cilium/cilium/issues/3233
-	NACKreceived      = "NACK received for version"                  // from https://github.com/cilium/cilium/issues/4003
-	RunInitFailed     = "JoinEP: "                                   // from https://github.com/cilium/cilium/pull/5052
-	sizeMismatch      = "size mismatch for BPF map"                  // from https://github.com/cilium/cilium/issues/7851
-	emptyBPFInitArg   = "empty argument passed to bpf/init.sh"       // from https://github.com/cilium/cilium/issues/10228
-	RemovingMapMsg    = "Removing map to allow for property upgrade" // from https://github.com/cilium/cilium/pull/10626
-	logBufferMessage  = "Log buffer too small to dump verifier log"  // from https://github.com/cilium/cilium/issues/10517
-	ClangErrorsMsg    = " errors generated."                         // from https://github.com/cilium/cilium/issues/10857
-	ClangErrorMsg     = "1 error generated."                         // from https://github.com/cilium/cilium/issues/10857
+	panicMessage       = "panic:"
+	deadLockHeader     = "POTENTIAL DEADLOCK:"                        // from github.com/sasha-s/go-deadlock/deadlock.go:header
+	segmentationFault  = "segmentation fault"                         // from https://github.com/cilium/cilium/issues/3233
+	NACKreceived       = "NACK received for version"                  // from https://github.com/cilium/cilium/issues/4003
+	RunInitFailed      = "JoinEP: "                                   // from https://github.com/cilium/cilium/pull/5052
+	sizeMismatch       = "size mismatch for BPF map"                  // from https://github.com/cilium/cilium/issues/7851
+	emptyBPFInitArg    = "empty argument passed to bpf/init.sh"       // from https://github.com/cilium/cilium/issues/10228
+	RemovingMapMsg     = "Removing map to allow for property upgrade" // from https://github.com/cilium/cilium/pull/10626
+	logBufferMessage   = "Log buffer too small to dump verifier log"  // from https://github.com/cilium/cilium/issues/10517
+	ClangErrorsMsg     = " errors generated."                         // from https://github.com/cilium/cilium/issues/10857
+	ClangErrorMsg      = "1 error generated."                         // from https://github.com/cilium/cilium/issues/10857
+	symbolSubstitution = "Skipping symbol substitution"
 
 	// HelmTemplate is the location of the Helm templates to install Cilium
 	HelmTemplate = "../install/kubernetes/cilium"
@@ -261,17 +262,18 @@ const CiliumConfigMapPatchKvstoreAllocator = "cilium-cm-kvstore-allocator-patch.
 // badLogMessages is a map which key is a part of a log message which indicates
 // a failure if the message does not contain any part from value list.
 var badLogMessages = map[string][]string{
-	panicMessage:      nil,
-	deadLockHeader:    nil,
-	segmentationFault: nil,
-	NACKreceived:      nil,
-	RunInitFailed:     {"signal: terminated", "signal: killed"},
-	sizeMismatch:      nil,
-	emptyBPFInitArg:   nil,
-	RemovingMapMsg:    nil,
-	logBufferMessage:  nil,
-	ClangErrorsMsg:    nil,
-	ClangErrorMsg:     nil,
+	panicMessage:       nil,
+	deadLockHeader:     nil,
+	segmentationFault:  nil,
+	NACKreceived:       nil,
+	RunInitFailed:      {"signal: terminated", "signal: killed"},
+	sizeMismatch:       nil,
+	emptyBPFInitArg:    nil,
+	RemovingMapMsg:     nil,
+	logBufferMessage:   nil,
+	ClangErrorsMsg:     nil,
+	ClangErrorMsg:      nil,
+	symbolSubstitution: nil,
 }
 
 var ciliumCLICommands = map[string]string{
