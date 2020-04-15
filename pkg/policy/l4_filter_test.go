283abf3e96773282938b785394efc35d7eb05fcf
Joe Stringer <joe@cilium.io>
2020-04-15 13:18:53 -0700

    policy: Treat remote-node as host in resolution
    In the --enable-remote-node-identity=false mode, the policy behaviour is
    intended to be identical to Cilium v1.6 or earlier, which is to say that
    if a remote node sends traffic towards a pod on the local node, then
    that traffic should be allowed.
    
    Since commit 28dc941544f1 ("bpf: Map HOST_ID to REMOTE_NODE_ID when
    encapsulating"), we have propagated the source identity from remote
    nodes as the remote node identity rather than the host, which breaks the
    above policy expectations because policy is looked up using this new
    remote-node identity.
    
    We could entirely disable the remote-node identity, but we want to
    provide a path whereby eventually all users are able to apply separate
    policy for whether they allow traffic from the localhost (where the
    typical use can is to handle kubelet health checks) vs. other nodes
    (where the typical case is hostNetworking pods on other nodes). This may
    allow some users to tighten their security posture.
    
    With this approach, the policy handling on the datapath will allow
    traffic from remote nodes if the relevant daemon options are configured,
    but in visibility tools like cilium monitor or Hubble, the actual
    identity used will be propagated. For users to migrate from
    `--enable-remote-node-identity=false` to setting this option to true,
    they can first deploy with it set to false, then monitor their traffic,
    craft their policies, and subsequently perform a migration step to turn
    this flag on. This can be iterated in a loop separately from the concern
    of upgrading their Cilium version.
    
    Fixes: #10702
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/policy/l4_filter_test.go
+++ pkg/policy/l4_filter_test.go
@@ -81,7 +81,19 @@ func (p *testPolicyContextType) GetEnvoyHTTPRules(*api.L7Rules) (*cilium.HttpNet
 	return nil, true
 }
 
-var testPolicyContext *testPolicyContextType
+var (
+	testPolicyContext               *testPolicyContextType
+	cachedRemoteNodeIdentitySetting bool
+)
+
+func (ds *PolicyTestSuite) SetUpSuite(c *C) {
+	cachedRemoteNodeIdentitySetting = option.Config.EnableRemoteNodeIdentity
+	option.Config.EnableRemoteNodeIdentity = true
+}
+
+func (ds *PolicyTestSuite) TearDownSuite(c *C) {
+	option.Config.EnableRemoteNodeIdentity = cachedRemoteNodeIdentitySetting
+}
 
 // Tests in this file:
 //
