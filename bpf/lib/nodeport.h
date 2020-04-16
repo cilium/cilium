245802e4fac2aac64322f52290342389e586d25f
Martynas Pumputis <m@lambda.lt>
2020-04-16 16:15:09 +0200

    datapath: Recreate NodePort CT entries if existing is stale
    [ upstream commit 3f588110ac9970bc5599f5495e45a4e063d0090d ]
    
    If a service endpoint is accessed via two different NodePort services
    from the same source IP and port, a reply from the endpoint will be
    wrongly rev-DNAT xlated.
    
    E.g.:
    
    0x6a60e2e6: Conntrack lookup 1/2: src=172.29.50.224:33632 dst=172.29.2.21:32532
    0x6a60e2e6: Conntrack lookup 2/2: nexthdr=6 flags=4
    0x6a60e2e6: CT verdict: New, revnat=0
    0x6a60e2e6: Slave service lookup: slave=1, dport=32532
    0x6a60e2e6: Conntrack create: proxy-port=0 revnat=8 src-identity=0 lb=0.0.0.0
    0x6a60e2e6: Conntrack lookup 1/2: src=172.29.50.224:33632 dst=10.42.1.254:3306
    0x6a60e2e6: Conntrack lookup 2/2: nexthdr=6 flags=1
    0x6a60e2e6: CT entry found lifetime=501525, revnat=22
    0x6a60e2e6: CT verdict: Established, revnat=22
    0xa140b3a1: Conntrack lookup 1/2: src=10.42.1.254:3306 dst=172.29.50.224:33632
    0xa140b3a1: Conntrack lookup 2/2: nexthdr=6 flags=0
    0xa140b3a1: CT entry found lifetime=523408, revnat=22
    0xa140b3a1: CT verdict: Reply, revnat=22
    0xa140b3a1: Reverse NAT lookup, index=22
    0xa140b3a1: Performing reverse NAT, address=192.168.58.0 port=3306
    
    TCP OUT 172.29.2.21:32532 -> 172.29.50.224:33632 service expires=501869
    RxPackets=0 RxBytes=28 RxFlagsSeen=0x00 LastRxReport=0 TxPackets=0
    TxBytes=0 TxFlagsSeen=0x02 LastTxReport=501808 Flags=0x0000 [ ] RevNAT=8
    SourceSecurityID=0
    TCP OUT 192.168.58.0:3306 -> 172.29.50.224:33632 service expires=523409
    RxPackets=0 RxBytes=28 RxFlagsSeen=0x00 LastRxReport=0 TxPackets=0
    TxBytes=0 TxFlagsSeen=0x1f LastTxReport=501808 Flags=0x0012 [ TxClosing
    SeenNonSyn ] RevNAT=22 SourceSecurityID=0
    TCP OUT 172.29.50.224:33632 -> 10.42.1.254:3306 expires=523409
    RxPackets=8 RxBytes=637 RxFlagsSeen=0x1b LastRxReport=501808
    TxPackets=13 TxBytes=861 TxFlagsSeen=0x1f LastTxReport=501808
    Flags=0x0032 [ TxClosing SeenNonSyn NodePort ] RevNAT=22
    SourceSecurityID=2
    
    First, the endpoint was accessed via 172.29.50.224:33632 ->
    192.168.58.0:3306, and afterwards by 172.29.50.224:33632 ->
    172.29.2.21:32532. In the second flow, the src IP and port of the
    reply was wrongly xlated to 192.168.58.0:3306.
    
    Fix this by recreating CT entries when stale (=of a different svc) entries
    are detected.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- bpf/lib/nodeport.h
+++ bpf/lib/nodeport.h
@@ -469,6 +469,7 @@ static inline int nodeport_lb6(struct __sk_buff *skb, __u32 src_identity)
 
 	switch (ret) {
 	case CT_NEW:
+redo_all:
 		ct_state_new.src_sec_id = SECLABEL;
 		ct_state_new.node_port = 1;
 		ret = ct_create6(get_ct_map6(&tuple), NULL, &tuple, skb,
@@ -477,7 +478,7 @@ static inline int nodeport_lb6(struct __sk_buff *skb, __u32 src_identity)
 			return ret;
 		if (backend_local) {
 			ct_flip_tuple_dir6(&tuple);
-redo:
+redo_local:
 			ct_state_new.rev_nat_index = 0;
 			ret = ct_create6(get_ct_map6(&tuple), NULL, &tuple, skb,
 					 CT_INGRESS, &ct_state_new, false);
@@ -488,13 +489,16 @@ redo:
 
 	case CT_ESTABLISHED:
 	case CT_REPLY:
+		if (unlikely(ct_state.rev_nat_index != svc->rev_nat_index))
+			goto redo_all;
+
 		if (backend_local) {
 			ct_flip_tuple_dir6(&tuple);
 			if (!__ct_entry_keep_alive(get_ct_map6(&tuple),
 						   &tuple)) {
 				ct_state_new.src_sec_id = SECLABEL;
 				ct_state_new.node_port = 1;
-				goto redo;
+				goto redo_local;
 			}
 		}
 		break;
@@ -1018,6 +1022,7 @@ static inline int nodeport_lb4(struct __sk_buff *skb, __u32 src_identity)
 
 	switch (ret) {
 	case CT_NEW:
+redo_all:
 		ct_state_new.src_sec_id = SECLABEL;
 		ct_state_new.node_port = 1;
 		ret = ct_create4(get_ct_map4(&tuple), NULL, &tuple, skb,
@@ -1026,7 +1031,9 @@ static inline int nodeport_lb4(struct __sk_buff *skb, __u32 src_identity)
 			return ret;
 		if (backend_local) {
 			ct_flip_tuple_dir4(&tuple);
-redo:
+redo_local:
+			/* Reset rev_nat_index, otherwise ipv4_policy() in
+			 * bpf_lxc will do invalid xlation */
 			ct_state_new.rev_nat_index = 0;
 			ret = ct_create4(get_ct_map4(&tuple), NULL, &tuple, skb,
 					 CT_INGRESS, &ct_state_new, false);
@@ -1037,13 +1044,18 @@ redo:
 
 	case CT_ESTABLISHED:
 	case CT_REPLY:
+		/* Recreate CT entries, as the existing one is stale and belongs
+		 * to a flow which target a different svc */
+		if (unlikely(ct_state.rev_nat_index != svc->rev_nat_index))
+			goto redo_all;
+
 		if (backend_local) {
 			ct_flip_tuple_dir4(&tuple);
 			if (!__ct_entry_keep_alive(get_ct_map4(&tuple),
 						   &tuple)) {
 				ct_state_new.src_sec_id = SECLABEL;
 				ct_state_new.node_port = 1;
-				goto redo;
+				goto redo_local;
 			}
 		}
 		break;
