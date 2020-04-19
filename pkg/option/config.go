cilium/cilium

commit 7fcf660ba6ed82c700ea75cfce91f74e0da1d128

cilium, bpf: add native HostPort mapping support
In order to use hostPort today, users can only consume it via deploying
Cilium through --set global.cni.chainingMode=portmap in order to then
have iptables set up for each pod that specifies a hostPort. We almost
have all the BPF infrastructure in place for natively supporting BPF
based hostPort. This work implements the hostPort feature as a service
mapping. It is part of the kube-proxy-free implementation since it
depends on NodePort infrastructure.

Example:

  # ./daemon/cilium-agent --identity-allocation-mode=crd --enable-ipv6=true --enable-ipv4=true --disable-envoy-version-check=true --tunnel=disabled --k8s-kubeconfig-path=$HOME/.kube/config --kube-proxy-replacement=strict --enable-l7-proxy=false

  # cat hostport.yaml
  apiVersion: v1
  kind: Pod
  metadata:
    name: nginx
  spec:
    containers:
      - name: nginx
        image: nginx
        ports:
          - containerPort: 80
            hostPort: 90
  # kubectl apply -f ./hostport.yaml
  pod/nginx created

  # ./cilium/cilium service list
  ID   Frontend            Service Type   Backend
  1    10.96.0.1:443       ClusterIP      1 => 192.168.178.29:6443
  2    10.96.0.10:53       ClusterIP      1 => 10.29.6.103:53
                                          2 => 10.29.24.228:53
  3    10.96.0.10:9153     ClusterIP      1 => 10.29.6.103:9153
                                          2 => 10.29.24.228:9153
  4    192.168.178.29:90   HostPort       1 => 10.29.245.35:80

From own node:

  # curl 192.168.178.29:90
  <!DOCTYPE html>
  <html>
  <head>
  [...]

From remote node:

  root@tank:~# curl 192.168.178.29:90
  <!DOCTYPE html>
  <html>
  <head>
  [...]

The HostPort implementation is a hybrid of ClusterIP and NodePort in
terms of implementation in that making it reachable on the own node is
just a single service map entry (as opposed to further exposing it via
loopback address, etc) and for external traffic the backend is always

pkg/option/config.go
@@ -287,6 +287,9 @@ const (
 	// LabelPrefixFile is the valid label prefixes file path
 	LabelPrefixFile = "label-prefix-file"
 
+	// EnableHostPort enables HostPort forwarding implemented by Cilium in BPF
+	EnableHostPort = "enable-host-port"
+
 	// EnableNodePort enables NodePort services implemented by Cilium in BPF
 	EnableNodePort = "enable-node-port"
 
@@ -1416,6 +1419,9 @@ type DaemonConfig struct {
 
 	// EnableNodePort enables k8s NodePort service implementation in BPF
 	EnableNodePort bool
+
+	// EnableHostPort enables k8s Pod's hostPort mapping through BPF
+	EnableHostPort bool
 
 	// NodePortMode indicates in which mode NodePort implementation should run
 	// ("snat", "dsr" or "hybrid")

pkg/datapath/linux/config/config.go
@@ -219,6 +219,9 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		if option.Config.EnableExternalIPs {
 			cDefinesMap["ENABLE_EXTERNAL_IP"] = "1"
 		}
+		if option.Config.EnableHostPort {
+			cDefinesMap["ENABLE_HOSTPORT"] = "1"
+		}
 
 		cDefinesMap["NODEPORT_PORT_MIN"] = fmt.Sprintf("%d", option.Config.NodePortMin)
 		cDefinesMap["NODEPORT_PORT_MAX"] = fmt.Sprintf("%d", option.Config.NodePortMax)

api/v1/models/kube_proxy_replacement.go
@@ -139,6 +139,9 @@ type KubeProxyReplacementFeatures struct {
 	// external i ps
 	ExternalIPs *KubeProxyReplacementFeaturesExternalIPs `json:"externalIPs,omitempty"`
 
+	// host port
+	HostPort *KubeProxyReplacementFeaturesHostPort `json:"hostPort,omitempty"`
+
 	// host reachable services
 	HostReachableServices *KubeProxyReplacementFeaturesHostReachableServices `json:"hostReachableServices,omitempty"`
 
@@ -151,6 +154,10 @@ func (m *KubeProxyReplacementFeatures) Validate(formats strfmt.Registry) error {
 	var res []error
 
 	if err := m.validateExternalIPs(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateHostPort(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -178,6 +185,24 @@ func (m *KubeProxyReplacementFeatures) validateExternalIPs(formats strfmt.Regist
 		if err := m.ExternalIPs.Validate(formats); err != nil {
 			if ve, ok := err.(*errors.Validation); ok {
 				return ve.ValidateName("features" + "." + "externalIPs")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *KubeProxyReplacementFeatures) validateHostPort(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.HostPort) { // not required
+		return nil
+	}
+
+	if m.HostPort != nil {
+		if err := m.HostPort.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("features" + "." + "hostPort")
 			}
 			return err
 		}
@@ -265,6 +290,38 @@ func (m *KubeProxyReplacementFeaturesExternalIPs) MarshalBinary() ([]byte, error
 // UnmarshalBinary interface implementation
 func (m *KubeProxyReplacementFeaturesExternalIPs) UnmarshalBinary(b []byte) error {
 	var res KubeProxyReplacementFeaturesExternalIPs
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}
+
+// KubeProxyReplacementFeaturesHostPort kube proxy replacement features host port
+// swagger:model KubeProxyReplacementFeaturesHostPort
+// +k8s:deepcopy-gen=true
+type KubeProxyReplacementFeaturesHostPort struct {
+
+	// enabled
+	Enabled bool `json:"enabled,omitempty"`
+}
+
+// Validate validates this kube proxy replacement features host port
+func (m *KubeProxyReplacementFeaturesHostPort) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *KubeProxyReplacementFeaturesHostPort) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *KubeProxyReplacementFeaturesHostPort) UnmarshalBinary(b []byte) error {
+	var res KubeProxyReplacementFeaturesHostPort
 	if err := swag.ReadJSON(b, &res); err != nil {
 		return err
 	}

api/v1/models/service_spec.go
@@ -153,7 +153,7 @@ type ServiceSpecFlags struct {
 	TrafficPolicy string `json:"trafficPolicy,omitempty"`
 
 	// Service type
-	// Enum: [ClusterIP NodePort ExternalIPs]
+	// Enum: [ClusterIP NodePort ExternalIPs HostPort]
 	Type string `json:"type,omitempty"`
 }
 
@@ -222,7 +222,7 @@ var serviceSpecFlagsTypeTypePropEnum []interface{}
 
 func init() {
 	var res []string
-	if err := json.Unmarshal([]byte(`["ClusterIP","NodePort","ExternalIPs"]`), &res); err != nil {
+	if err := json.Unmarshal([]byte(`["ClusterIP","NodePort","ExternalIPs","HostPort"]`), &res); err != nil {
 		panic(err)
 	}
 	for _, v := range res {
@@ -240,6 +240,9 @@ const (
 
 	// ServiceSpecFlagsTypeExternalIPs captures enum value "ExternalIPs"
 	ServiceSpecFlagsTypeExternalIPs string = "ExternalIPs"
+
+	// ServiceSpecFlagsTypeHostPort captures enum value "HostPort"
+	ServiceSpecFlagsTypeHostPort string = "HostPort"
 )
 
 // prop value enum

api/v1/models/zz_generated.deepcopy.go
@@ -209,6 +209,11 @@ func (in *KubeProxyReplacementFeatures) DeepCopyInto(out *KubeProxyReplacementFe
 		*out = new(KubeProxyReplacementFeaturesExternalIPs)
 		**out = **in
 	}
+	if in.HostPort != nil {
+		in, out := &in.HostPort, &out.HostPort
+		*out = new(KubeProxyReplacementFeaturesHostPort)
+		**out = **in
+	}
 	if in.HostReachableServices != nil {
 		in, out := &in.HostReachableServices, &out.HostReachableServices
 		*out = new(KubeProxyReplacementFeaturesHostReachableServices)
@@ -244,6 +249,22 @@ func (in *KubeProxyReplacementFeaturesExternalIPs) DeepCopy() *KubeProxyReplacem
 		return nil
 	}
 	out := new(KubeProxyReplacementFeaturesExternalIPs)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KubeProxyReplacementFeaturesHostPort) DeepCopyInto(out *KubeProxyReplacementFeaturesHostPort) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KubeProxyReplacementFeaturesHostPort.
+func (in *KubeProxyReplacementFeaturesHostPort) DeepCopy() *KubeProxyReplacementFeaturesHostPort {
+	if in == nil {
+		return nil
+	}
+	out := new(KubeProxyReplacementFeaturesHostPort)
 	in.DeepCopyInto(out)
 	return out
 }

api/v1/openapi.yaml
@@ -1643,6 +1643,11 @@ definitions:
                 type: integer
               portMax:
                 type: integer
+          hostPort:
+            type: object
+            properties:
+              enabled:
+                type: boolean
           externalIPs:
             type: object
             properties:
@@ -2089,6 +2094,7 @@ definitions:
             - ClusterIP
             - NodePort
             - ExternalIPs
+            - HostPort
           trafficPolicy:
             description: Service traffic policy
             type: string

api/v1/server/embedded_spec.go
@@ -2457,6 +2457,14 @@ func init() {
                 }
               }
             },
+            "hostPort": {
+              "type": "object",
+              "properties": {
+                "enabled": {
+                  "type": "boolean"
+                }
+              }
+            },
             "hostReachableServices": {
               "type": "object",
               "properties": {
@@ -3043,7 +3051,8 @@ func init() {
               "enum": [
                 "ClusterIP",
                 "NodePort",
-                "ExternalIPs"
+                "ExternalIPs",
+                "HostPort"
               ]
             }
           }
@@ -5926,6 +5935,14 @@ func init() {
                 }
               }
             },
+            "hostPort": {
+              "type": "object",
+              "properties": {
+                "enabled": {
+                  "type": "boolean"
+                }
+              }
+            },
             "hostReachableServices": {
               "type": "object",
               "properties": {
@@ -6512,7 +6529,8 @@ func init() {
               "enum": [
                 "ClusterIP",
                 "NodePort",
-                "ExternalIPs"
+                "ExternalIPs",
+                "HostPort"
               ]
             }
           }

bpf/lib/common.h
@@ -559,7 +559,8 @@ struct lb6_service {
 	__u8 external:1,	/* K8s External IPs */
 	     nodeport:1,	/* K8s NodePort service */
 	     local_scope:1,	/* K8s externalTrafficPolicy=Local */
-	     reserved:5;
+	     hostport:1,	/* K8s hostPort forwarding */
+	     reserved:4;
 	__u8 pad[3];
 };
 
@@ -594,7 +595,8 @@ struct lb4_service {
 	__u8 external:1,	/* K8s External IPs */
 	     nodeport:1,	/* K8s NodePort service */
 	     local_scope:1,	/* K8s externalTrafficPolicy=Local */
-	     reserved:5;
+	     hostport:1,	/* K8s hostPort forwarding */
+	     reserved:4;
 	__u8 pad[3];
 };
 

commit 6c44cbc8053ad619aa04b9c4299848e94f42b80f

hostport: read the hostport setting from viper
Previously, it will lost hostport setting if we do not set
kube-proxy-replacement as strict or probe.

Fixes: 7fcf660ba6ed ("cilium, bpf: add native HostPort mapping support")
Signed-off-by: Wang Li <wangli09@kuaishou.com>

pkg/option/config.go
@@ -2332,6 +2332,7 @@ func (c *DaemonConfig) Populate() {
 	c.EnableL7Proxy = viper.GetBool(EnableL7Proxy)
 	c.EnableTracing = viper.GetBool(EnableTracing)
 	c.EnableNodePort = viper.GetBool(EnableNodePort)
+	c.EnableHostPort = viper.GetBool(EnableHostPort)
 	c.NodePortMode = viper.GetString(NodePortMode)
 	c.NodePortAcceleration = viper.GetString(NodePortAcceleration)
 	c.EnableAutoProtectNodePortRange = viper.GetBool(EnableAutoProtectNodePortRange)