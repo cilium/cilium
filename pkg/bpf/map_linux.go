436f699f1ced6bd9dcfa32ee6c1e4e5555024d80
Thomas Graf <thomas@cilium.io>
2020-04-20 13:54:56 +0200

    bpf: Open map on recovery if needed
    If the first BPF map operation performed on a map would open the map and
    that fails, the queued error would not open the map on recovery.
    Recovery would be postponed until a subsequent error triggers another
    open.
    
    Fix this by opening the map if needed in the recovery synchronization.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/bpf/map_linux.go
+++ pkg/bpf/map_linux.go
@@ -1057,7 +1057,9 @@ func (m *Map) GetModel() *models.BPFMap {
 func (m *Map) resolveErrors(ctx context.Context) error {
 	started := time.Now()
 
-	m.lock.Lock()
+	if err := m.openAndLock(); err != nil {
+		return err
+	}
 	defer m.lock.Unlock()
 
 	if m.cache == nil {
