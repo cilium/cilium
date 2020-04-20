674e7752df93c50b35cc6ffb1a7c31be569891c4
Maciej Kwiek <maciej@isovalent.com>
2020-04-20 18:08:04 +0200

    ci: tag images built in CI
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

--- test/make-images-push-to-local-registry.sh
+++ test/make-images-push-to-local-registry.sh
@@ -3,11 +3,11 @@
 set -e
 
 cd ..
-make docker-image
+make docker-image DOCKER_IMAGE_TAG=$2
 
-docker tag cilium/cilium:latest $1/cilium/cilium:$2
-docker tag cilium/cilium:latest $1/cilium/cilium-dev:$2
-docker tag cilium/operator:latest $1/cilium/operator:$2
+docker tag cilium/cilium:$2 $1/cilium/cilium:$2
+docker tag cilium/cilium:$2 $1/cilium/cilium-dev:$2
+docker tag cilium/operator:$2 $1/cilium/operator:$2
 
 docker push $1/cilium/cilium:$2
 docker push $1/cilium/cilium-dev:$2
