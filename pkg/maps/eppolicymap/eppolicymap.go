cilium/cilium

commit b384ce85bb835ec9f854a8a2d05ce49d851b6ddb

eppolicymap: Factor out for better testing
Support creation of the endpoint policy map with a custom name, so that
test utilities can create them at different paths from the standard
Cilium map path.

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/maps/eppolicymap/eppolicymap.go
@@ -50,23 +50,16 @@ type EPPolicyValue struct{ Fd uint32 }
 var (
 	buildMap sync.Once
 
-	EpPolicyMap = bpf.NewMap(MapName,
-		bpf.MapTypeHashOfMaps,
-		&EndpointKey{},
-		int(unsafe.Sizeof(EndpointKey{})),
-		&EPPolicyValue{},
-		int(unsafe.Sizeof(EPPolicyValue{})),
-		MaxEntries,
-		0,
-		0,
-		bpf.ConvertKeyValue,
-	).WithCache()
+	// EpPolicyMap is the global singleton of the endpoint policy map.
+	EpPolicyMap *bpf.Map
 )
 
-// CreateEPPolicyMap will create both the innerMap (needed for map in map types) and
-// then after BPFFS is mounted create the epPolicyMap. We only create the innerFd once
-// to avoid having multiple inner maps.
-func CreateEPPolicyMap() {
+// CreateWithName creates a new endpoint policy hash of maps for
+// looking up an endpoint's policy map by the endpoint key.
+//
+// The specified mapName allows non-standard map paths to be used, for instance
+// for testing purposes.
+func CreateWithName(mapName string) error {
 	buildMap.Do(func() {
 		fd, err := bpf.CreateMap(bpf.BPF_MAP_TYPE_HASH,
 			uint32(unsafe.Sizeof(policymap.PolicyKey{})),
@@ -79,10 +72,29 @@ func CreateEPPolicyMap() {
 			return
 		}
 
+		EpPolicyMap = bpf.NewMap(mapName,
+			bpf.MapTypeHashOfMaps,
+			&EndpointKey{},
+			int(unsafe.Sizeof(EndpointKey{})),
+			&EPPolicyValue{},
+			int(unsafe.Sizeof(EPPolicyValue{})),
+			MaxEntries,
+			0,
+			0,
+			bpf.ConvertKeyValue,
+		).WithCache()
 		EpPolicyMap.InnerID = uint32(fd)
 	})
 
-	if _, err := EpPolicyMap.OpenOrCreate(); err != nil {
+	_, err := EpPolicyMap.OpenOrCreate()
+	return err
+}
+
+// CreateEPPolicyMap will create both the innerMap (needed for map in map types) and
+// then after BPFFS is mounted create the epPolicyMap. We only create the innerFd once
+// to avoid having multiple inner maps.
+func CreateEPPolicyMap() {
+	if err := CreateWithName(MapName); err != nil {
 		log.WithError(err).Warning("Unable to open or create endpoint policy map")
 	}
 }

commit 54f12458b9f2ac2af97f3cea070f35429c99d0c9

eppolicymap: exit if inner map creation fails, to avoid nil dereference
In eppolicymap.CreateWithName(), if the policy map creation in the
buildMap.Do() block fails for whatever reason, we return early and
EpPolicyMap remains nil. Calling OpenOrCreate() a few lines below
attempts to dereference that pointer to take its lock.

Ouch.

EpPolicyMap used to be created unconditionally, even if the policy map
creation would fail. This was changed in commit b384ce85bb83. We could
restore this behaviour, or we can simply exit early if inner map
creation fails, by producing a fatal error message. Let's do the latter,
as there is no point in proceeding with the creation of endpoint policy
map if creating the inner map failed.

Fixes: b384ce85bb83 ("eppolicymap: Factor out for better testing")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

pkg/maps/eppolicymap/eppolicymap.go
@@ -69,7 +69,7 @@ func CreateWithName(mapName string) error {
 			0, innerMapName)
 
 		if err != nil {
-			log.WithError(err).Warning("unable to create EP to policy map")
+			log.WithError(err).Fatal("unable to create EP to policy map")
 			return
 		}
 