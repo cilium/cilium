cilium/cilium

commit 7fcf660ba6ed82c700ea75cfce91f74e0da1d128

cilium, bpf: add native HostPort mapping support
In order to use hostPort today, users can only consume it via deploying
Cilium through --set global.cni.chainingMode=portmap in order to then
have iptables set up for each pod that specifies a hostPort. We almost
have all the BPF infrastructure in place for natively supporting BPF
based hostPort. This work implements the hostPort feature as a service
mapping. It is part of the kube-proxy-free implementation since it
depends on NodePort infrastructure.

Example:

  # ./daemon/cilium-agent --identity-allocation-mode=crd --enable-ipv6=true --enable-ipv4=true --disable-envoy-version-check=true --tunnel=disabled --k8s-kubeconfig-path=$HOME/.kube/config --kube-proxy-replacement=strict --enable-l7-proxy=false

  # cat hostport.yaml
  apiVersion: v1
  kind: Pod
  metadata:
    name: nginx
  spec:
    containers:
      - name: nginx
        image: nginx
        ports:
          - containerPort: 80
            hostPort: 90
  # kubectl apply -f ./hostport.yaml
  pod/nginx created

  # ./cilium/cilium service list
  ID   Frontend            Service Type   Backend
  1    10.96.0.1:443       ClusterIP      1 => 192.168.178.29:6443
  2    10.96.0.10:53       ClusterIP      1 => 10.29.6.103:53
                                          2 => 10.29.24.228:53
  3    10.96.0.10:9153     ClusterIP      1 => 10.29.6.103:9153
                                          2 => 10.29.24.228:9153
  4    192.168.178.29:90   HostPort       1 => 10.29.245.35:80

From own node:

  # curl 192.168.178.29:90
  <!DOCTYPE html>
  <html>
  <head>
  [...]

From remote node:

  root@tank:~# curl 192.168.178.29:90
  <!DOCTYPE html>
  <html>
  <head>
  [...]

The HostPort implementation is a hybrid of ClusterIP and NodePort in
terms of implementation in that making it reachable on the own node is
just a single service map entry (as opposed to further exposing it via
loopback address, etc) and for external traffic the backend is always

api/v1/models/kube_proxy_replacement.go
@@ -139,6 +139,9 @@ type KubeProxyReplacementFeatures struct {
 	// external i ps
 	ExternalIPs *KubeProxyReplacementFeaturesExternalIPs `json:"externalIPs,omitempty"`
 
+	// host port
+	HostPort *KubeProxyReplacementFeaturesHostPort `json:"hostPort,omitempty"`
+
 	// host reachable services
 	HostReachableServices *KubeProxyReplacementFeaturesHostReachableServices `json:"hostReachableServices,omitempty"`
 
@@ -151,6 +154,10 @@ func (m *KubeProxyReplacementFeatures) Validate(formats strfmt.Registry) error {
 	var res []error
 
 	if err := m.validateExternalIPs(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateHostPort(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -178,6 +185,24 @@ func (m *KubeProxyReplacementFeatures) validateExternalIPs(formats strfmt.Regist
 		if err := m.ExternalIPs.Validate(formats); err != nil {
 			if ve, ok := err.(*errors.Validation); ok {
 				return ve.ValidateName("features" + "." + "externalIPs")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *KubeProxyReplacementFeatures) validateHostPort(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.HostPort) { // not required
+		return nil
+	}
+
+	if m.HostPort != nil {
+		if err := m.HostPort.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("features" + "." + "hostPort")
 			}
 			return err
 		}
@@ -265,6 +290,38 @@ func (m *KubeProxyReplacementFeaturesExternalIPs) MarshalBinary() ([]byte, error
 // UnmarshalBinary interface implementation
 func (m *KubeProxyReplacementFeaturesExternalIPs) UnmarshalBinary(b []byte) error {
 	var res KubeProxyReplacementFeaturesExternalIPs
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}
+
+// KubeProxyReplacementFeaturesHostPort kube proxy replacement features host port
+// swagger:model KubeProxyReplacementFeaturesHostPort
+// +k8s:deepcopy-gen=true
+type KubeProxyReplacementFeaturesHostPort struct {
+
+	// enabled
+	Enabled bool `json:"enabled,omitempty"`
+}
+
+// Validate validates this kube proxy replacement features host port
+func (m *KubeProxyReplacementFeaturesHostPort) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *KubeProxyReplacementFeaturesHostPort) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *KubeProxyReplacementFeaturesHostPort) UnmarshalBinary(b []byte) error {
+	var res KubeProxyReplacementFeaturesHostPort
 	if err := swag.ReadJSON(b, &res); err != nil {
 		return err
 	}

api/v1/openapi.yaml
@@ -1643,6 +1643,11 @@ definitions:
                 type: integer
               portMax:
                 type: integer
+          hostPort:
+            type: object
+            properties:
+              enabled:
+                type: boolean
           externalIPs:
             type: object
             properties:
@@ -2089,6 +2094,7 @@ definitions:
             - ClusterIP
             - NodePort
             - ExternalIPs
+            - HostPort
           trafficPolicy:
             description: Service traffic policy
             type: string

daemon/cmd/daemon_main.go
@@ -509,6 +509,9 @@ func init() {
 		option.KubeProxyReplacementStrict, option.KubeProxyReplacementDisabled))
 	option.BindEnv(option.KubeProxyReplacement)
 
+	flags.Bool(option.EnableHostPort, true, fmt.Sprintf("Enable k8s hostPort mapping feature (requires enabling %s)", option.EnableNodePort))
+	option.BindEnv(option.EnableHostPort)
+
 	flags.Bool(option.EnableNodePort, false, "Enable NodePort type services by Cilium (beta)")
 	option.BindEnv(option.EnableNodePort)
 
@@ -1408,9 +1411,11 @@ func initKubeProxyReplacementOptions() {
 	}
 
 	if option.Config.KubeProxyReplacement == option.KubeProxyReplacementDisabled {
-		log.Infof("Auto-disabling %q, %q, %q features", option.EnableNodePort,
-			option.EnableExternalIPs, option.EnableHostReachableServices)
+		log.Infof("Auto-disabling %q, %q, %q, %q features", option.EnableNodePort,
+			option.EnableExternalIPs, option.EnableHostReachableServices,
+			option.EnableHostPort)
 
+		option.Config.EnableHostPort = false
 		option.Config.EnableNodePort = false
 		option.Config.EnableExternalIPs = false
 		option.Config.EnableHostReachableServices = false
@@ -1427,9 +1432,11 @@ func initKubeProxyReplacementOptions() {
 	if option.Config.KubeProxyReplacement == option.KubeProxyReplacementProbe ||
 		option.Config.KubeProxyReplacement == option.KubeProxyReplacementStrict {
 
-		log.Infof("Auto-enabling %q, %q, %q features", option.EnableNodePort,
-			option.EnableExternalIPs, option.EnableHostReachableServices)
+		log.Infof("Auto-enabling %q, %q, %q, %q features", option.EnableNodePort,
+			option.EnableExternalIPs, option.EnableHostReachableServices,
+			option.EnableHostPort)
 
+		option.Config.EnableHostPort = true
 		option.Config.EnableNodePort = true
 		option.Config.EnableExternalIPs = true
 		option.Config.EnableHostReachableServices = true
@@ -1444,6 +1451,7 @@ func initKubeProxyReplacementOptions() {
 			if strict {
 				log.Fatal(msg)
 			} else {
+				option.Config.EnableHostPort = false
 				option.Config.EnableNodePort = false
 				option.Config.EnableExternalIPs = false
 				log.Warn(msg + " Disabling the feature.")
@@ -1470,6 +1478,7 @@ func initKubeProxyReplacementOptions() {
 				log.Fatal(msg)
 			} else {
 				log.Warn(msg + " Disabling the feature.")
+				option.Config.EnableHostPort = false
 				option.Config.EnableNodePort = false
 				option.Config.EnableExternalIPs = false
 			}
@@ -1480,6 +1489,7 @@ func initKubeProxyReplacementOptions() {
 				log.Fatal(err)
 			} else {
 				log.Warn(fmt.Sprintf("%s Disabling the feature.", err))
+				option.Config.EnableHostPort = false
 				option.Config.EnableNodePort = false
 				option.Config.EnableExternalIPs = false
 			}
@@ -1494,6 +1504,7 @@ func initKubeProxyReplacementOptions() {
 				log.WithError(err).Fatal(msg)
 			} else {
 				log.WithError(err).Warn(msg + " Disabling BPF NodePort feature.")
+				option.Config.EnableHostPort = false
 				option.Config.EnableNodePort = false
 				option.Config.EnableExternalIPs = false
 			}
@@ -1540,6 +1551,7 @@ func initKubeProxyReplacementOptions() {
 	}
 
 	if !option.Config.EnableNodePort {
+		option.Config.EnableHostPort = false
 		option.Config.EnableExternalIPs = false
 	} else {
 		if option.Config.Tunnel != option.TunnelDisabled &&

daemon/cmd/status.go
@@ -127,6 +127,7 @@ func (d *Daemon) getKubeProxyReplacementStatus() *models.KubeProxyReplacement {
 
 	features := &models.KubeProxyReplacementFeatures{
 		NodePort:              &models.KubeProxyReplacementFeaturesNodePort{},
+		HostPort:              &models.KubeProxyReplacementFeaturesHostPort{},
 		ExternalIPs:           &models.KubeProxyReplacementFeaturesExternalIPs{},
 		HostReachableServices: &models.KubeProxyReplacementFeaturesHostReachableServices{},
 	}
@@ -135,6 +136,9 @@ func (d *Daemon) getKubeProxyReplacementStatus() *models.KubeProxyReplacement {
 		features.NodePort.Mode = strings.ToUpper(option.Config.NodePortMode)
 		features.NodePort.PortMin = int64(option.Config.NodePortMin)
 		features.NodePort.PortMax = int64(option.Config.NodePortMax)
+	}
+	if option.Config.EnableHostPort {
+		features.HostPort.Enabled = true
 	}
 	if option.Config.EnableExternalIPs {
 		features.ExternalIPs.Enabled = true
@@ -155,7 +159,6 @@ func (d *Daemon) getKubeProxyReplacementStatus() *models.KubeProxyReplacement {
 		Mode:     mode,
 		Features: features,
 	}
-
 }
 
 type getHealthz struct {

pkg/client/client.go
@@ -272,6 +272,10 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, allAddresses, 
 					fmt.Sprintf("NodePort (%s, %d-%d)", mode, np.PortMin, np.PortMax))
 			}
 
+			if sr.KubeProxyReplacement.Features.HostPort.Enabled {
+				features = append(features, "HostPort")
+			}
+
 			if sr.KubeProxyReplacement.Features.ExternalIPs.Enabled {
 				features = append(features, "ExternalIPs")
 			}

pkg/datapath/linux/config/config.go
@@ -219,6 +219,9 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		if option.Config.EnableExternalIPs {
 			cDefinesMap["ENABLE_EXTERNAL_IP"] = "1"
 		}
+		if option.Config.EnableHostPort {
+			cDefinesMap["ENABLE_HOSTPORT"] = "1"
+		}
 
 		cDefinesMap["NODEPORT_PORT_MIN"] = fmt.Sprintf("%d", option.Config.NodePortMin)
 		cDefinesMap["NODEPORT_PORT_MAX"] = fmt.Sprintf("%d", option.Config.NodePortMax)

pkg/k8s/watchers/pod.go
@@ -32,6 +32,7 @@ import (
 	k8sUtils "github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
@@ -168,7 +169,7 @@ func (k *K8sWatcher) addK8sPodV1(pod *types.Pod) error {
 		"hostIP":               pod.StatusHostIP,
 	})
 
-	skipped, err := k.updatePodHostIP(pod)
+	skipped, err := k.updatePodHostData(pod)
 	switch {
 	case skipped:
 		logger.WithError(err).Debug("Skipped ipcache map update on pod add")
@@ -280,7 +281,7 @@ func (k *K8sWatcher) deleteK8sPodV1(pod *types.Pod) error {
 		"hostIP":               pod.StatusHostIP,
 	})
 
-	skipped, err := k.deletePodHostIP(pod)
+	skipped, err := k.deletePodHostData(pod)
 	switch {
 	case skipped:
 		logger.WithError(err).Debug("Skipped ipcache map delete on pod delete")
@@ -292,19 +293,142 @@ func (k *K8sWatcher) deleteK8sPodV1(pod *types.Pod) error {
 	return err
 }
 
-func (k *K8sWatcher) updatePodHostIP(pod *types.Pod) (bool, error) {
-	if pod.SpecHostNetwork {
-		return true, fmt.Errorf("pod is using host networking")
+func genServiceMappings(pod *types.Pod) []loadbalancer.SVC {
+	var svcs []loadbalancer.SVC
+	for _, c := range pod.SpecContainers {
+		for _, p := range c.HostPorts {
+			if p.HostPort == 0 {
+				continue
+			}
+			feIP := net.ParseIP(p.HostIP)
+			if feIP == nil {
+				feIP = net.ParseIP(pod.StatusHostIP)
+			}
+			proto, err := loadbalancer.NewL4Type(p.Protocol)
+			if err != nil {
+				continue
+			}
+			fe := loadbalancer.L3n4AddrID{
+				L3n4Addr: loadbalancer.L3n4Addr{
+					IP: feIP,
+					L4Addr: loadbalancer.L4Addr{
+						Protocol: proto,
+						Port:     uint16(p.HostPort),
+					},
+				},
+				ID: loadbalancer.ID(0),
+			}
+			be := loadbalancer.Backend{
+				L3n4Addr: loadbalancer.L3n4Addr{
+					IP: net.ParseIP(pod.StatusPodIP),
+					L4Addr: loadbalancer.L4Addr{
+						Protocol: proto,
+						Port:     uint16(p.ContainerPort),
+					},
+				},
+			}
+			svcs = append(svcs,
+				loadbalancer.SVC{
+					Frontend: fe,
+					Backends: []loadbalancer.Backend{be},
+					Type:     loadbalancer.SVCTypeHostPort,
+					// We don't have the node name available here, but in
+					// any case in the BPF data path we drop any potential
+					// non-local backends anyway (which should never exist).
+					TrafficPolicy: loadbalancer.SVCTrafficPolicyCluster,
+				})
+		}
 	}
+
+	return svcs
+}
+
+func (k *K8sWatcher) UpsertHostPortMapping(pod *types.Pod) error {
+	if option.Config.DisableK8sServices || !option.Config.EnableHostPort {
+		return nil
+	}
+
+	svcs := genServiceMappings(pod)
+	if len(svcs) == 0 {
+		return nil
+	}
+
+	logger := log.WithFields(logrus.Fields{
+		logfields.K8sPodName:   pod.ObjectMeta.Name,
+		logfields.K8sNamespace: pod.ObjectMeta.Namespace,
+		"podIP":                pod.StatusPodIP,
+		"hostIP":               pod.StatusHostIP,
+	})
 
 	hostIP := net.ParseIP(pod.StatusHostIP)
 	if hostIP == nil {
-		return true, fmt.Errorf("no/invalid HostIP: %s", pod.StatusHostIP)
+		logger.Error("Cannot upsert HostPort service for the podIP due to missing hostIP")
+		return fmt.Errorf("no/invalid HostIP: %s", pod.StatusHostIP)
+	}
+
+	for _, dpSvc := range svcs {
+		if _, _, err := k.svcManager.UpsertService(dpSvc.Frontend, dpSvc.Backends, dpSvc.Type,
+			dpSvc.TrafficPolicy, dpSvc.HealthCheckNodePort, pod.ObjectMeta.Name+"-host-port",
+			pod.ObjectMeta.Namespace); err != nil {
+			logger.WithError(err).Error("Error while inserting service in LB map")
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (k *K8sWatcher) DeleteHostPortMapping(pod *types.Pod) error {
+	if option.Config.DisableK8sServices || !option.Config.EnableHostPort {
+		return nil
+	}
+
+	svcs := genServiceMappings(pod)
+	if len(svcs) == 0 {
+		return nil
+	}
+
+	logger := log.WithFields(logrus.Fields{
+		logfields.K8sPodName:   pod.ObjectMeta.Name,
+		logfields.K8sNamespace: pod.ObjectMeta.Namespace,
+		"podIP":                pod.StatusPodIP,
+		"hostIP":               pod.StatusHostIP,
+	})
+
+	hostIP := net.ParseIP(pod.StatusHostIP)
+	if hostIP == nil {
+		logger.Error("Cannot delete HostPort service for the podIP due to missing hostIP")
+		return fmt.Errorf("no/invalid HostIP: %s", pod.StatusHostIP)
+	}
+
+	for _, dpSvc := range svcs {
+		if _, err := k.svcManager.DeleteService(dpSvc.Frontend.L3n4Addr); err != nil {
+			logger.WithError(err).Error("Error while deleting service in LB map")
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (k *K8sWatcher) updatePodHostData(pod *types.Pod) (bool, error) {
+	if pod.SpecHostNetwork {
+		return true, fmt.Errorf("pod is using host networking")
 	}
 
 	podIP := net.ParseIP(pod.StatusPodIP)
 	if podIP == nil {
 		return true, fmt.Errorf("no/invalid PodIP: %s", pod.StatusPodIP)
+	}
+
+	err := k.UpsertHostPortMapping(pod)
+	if err != nil {
+		return true, fmt.Errorf("cannot upsert hostPort for PodIP: %s", pod.StatusPodIP)
+	}
+
+	hostIP := net.ParseIP(pod.StatusHostIP)
+	if hostIP == nil {
+		return true, fmt.Errorf("no/invalid HostIP: %s", pod.StatusHostIP)
 	}
 
 	hostKey := node.GetIPsecKeyIdentity()
@@ -328,7 +452,7 @@ func (k *K8sWatcher) updatePodHostIP(pod *types.Pod) (bool, error) {
 	return false, nil
 }
 
-func (k *K8sWatcher) deletePodHostIP(pod *types.Pod) (bool, error) {
+func (k *K8sWatcher) deletePodHostData(pod *types.Pod) (bool, error) {
 	if pod.SpecHostNetwork {
 		return true, fmt.Errorf("pod is using host networking")
 	}
@@ -337,6 +461,8 @@ func (k *K8sWatcher) deletePodHostIP(pod *types.Pod) (bool, error) {
 	if podIP == nil {
 		return true, fmt.Errorf("no/invalid PodIP: %s", pod.StatusPodIP)
 	}
+
+	k.DeleteHostPortMapping(pod)
 
 	// a small race condition exists here as deletion could occur in
 	// parallel based on another event but it doesn't matter as the

pkg/option/config.go
@@ -287,6 +287,9 @@ const (
 	// LabelPrefixFile is the valid label prefixes file path
 	LabelPrefixFile = "label-prefix-file"
 
+	// EnableHostPort enables HostPort forwarding implemented by Cilium in BPF
+	EnableHostPort = "enable-host-port"
+
 	// EnableNodePort enables NodePort services implemented by Cilium in BPF
 	EnableNodePort = "enable-node-port"
 
@@ -1416,6 +1419,9 @@ type DaemonConfig struct {
 
 	// EnableNodePort enables k8s NodePort service implementation in BPF
 	EnableNodePort bool
+
+	// EnableHostPort enables k8s Pod's hostPort mapping through BPF
+	EnableHostPort bool
 
 	// NodePortMode indicates in which mode NodePort implementation should run
 	// ("snat", "dsr" or "hybrid")

commit 4e632d2cea836688a2253a7e966692ae442fe9a2

hostport: Add an option host-port-only-local
Enable option host-port-only-local expects same behavior as kube-proxy, that is
almost same with the iptable rule 'ADDRTYPE match dst-type LOCAL'. And means not
do the destination pod ip translation locally.

Yes, the present implementation has better performance for pod to pod traffics
inside cluster, but in some special cases we still need this option to keep same
behavior with kube-proxy, and if all the traffics come from outside, we do not
need thousands of service mapping entries on each nodes.

Fixes: 7fcf660ba6ed ("cilium, bpf: add native HostPort mapping support")

Example:
--host-port-only-local=false (by default, no changes)
8    10.44.76.162:9000    HostPort       1 => 172.30.0.74:8000
9    10.44.75.162:9000    HostPort       1 => 172.30.2.178:8000
10   10.44.74.162:9000    HostPort       1 => 172.30.3.29:8000
11   10.44.76.150:9000    HostPort       1 => 172.30.1.34:8000

--host-port-only-local=true
8    10.44.76.162:9000    HostPort       1 => 172.30.0.74:8000

Signed-off-by: yangxingwu <yangxingwu@kuaishou.com>
Signed-off-by: huangxuesen <huangxuesen@kuaishou.com>
Signed-off-by: Wang Li <wangli09@kuaishou.com>

api/v1/models/kube_proxy_replacement.go
@@ -304,6 +304,9 @@ type KubeProxyReplacementFeaturesHostPort struct {
 
 	// enabled
 	Enabled bool `json:"enabled,omitempty"`
+
+	// local
+	Local bool `json:"local,omitempty"`
 }
 
 // Validate validates this kube proxy replacement features host port

api/v1/openapi.yaml
@@ -1701,6 +1701,8 @@ definitions:
             properties:
               enabled:
                 type: boolean
+              local:
+                type: boolean
           externalIPs:
             type: object
             properties:

daemon/cmd/daemon_main.go
@@ -490,6 +490,9 @@ func init() {
 	flags.Bool(option.EnableHostPort, true, fmt.Sprintf("Enable k8s hostPort mapping feature (requires enabling %s)", option.EnableNodePort))
 	option.BindEnv(option.EnableHostPort)
 
+	flags.Bool(option.HostPortOnlyLocal, false, fmt.Sprintf("Only generate hostPort mapping on local node (requires enabling %s)", option.EnableHostPort))
+	option.BindEnv(option.HostPortOnlyLocal)
+
 	flags.Bool(option.EnableNodePort, false, "Enable NodePort type services by Cilium (beta)")
 	option.BindEnv(option.EnableNodePort)
 

daemon/cmd/status.go
@@ -144,6 +144,9 @@ func (d *Daemon) getKubeProxyReplacementStatus() *models.KubeProxyReplacement {
 	}
 	if option.Config.EnableHostPort {
 		features.HostPort.Enabled = true
+		if option.Config.HostPortOnlyLocal {
+			features.HostPort.Local = true
+		}
 	}
 	if option.Config.EnableExternalIPs {
 		features.ExternalIPs.Enabled = true

install/kubernetes/cilium/charts/config/templates/configmap.yaml
@@ -303,6 +303,7 @@ data:
 {{- end }}
 {{- if .Values.global.hostPort }}
   enable-host-port: {{ .Values.global.hostPort.enabled | quote }}
+  host-port-only-local: {{ .Values.global.hostPort.onlyLocal | quote }}
 {{- end }}
 {{- if .Values.global.externalIPs }}
   enable-external-ips: {{ .Values.global.externalIPs.enabled | quote }}

install/kubernetes/cilium/values.yaml
@@ -294,6 +294,9 @@ global:
     # enabled enables the hostPort functionality
     enabled: false
 
+    # onlyLocal indicates to generate service mapping only for local node
+    onlyLocal: false
+
   # externalIPs is the configuration for ExternalIPs service handling
   externalIPs:
     # enabled enables ExternalIPs functionality

pkg/client/client.go
@@ -274,7 +274,11 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, allAddresses, 
 			}
 
 			if sr.KubeProxyReplacement.Features.HostPort.Enabled {
-				features = append(features, "HostPort")
+				if sr.KubeProxyReplacement.Features.HostPort.Local {
+					features = append(features, "HostPort (only local)")
+				} else {
+					features = append(features, "HostPort")
+				}
 			}
 
 			if sr.KubeProxyReplacement.Features.ExternalIPs.Enabled {

pkg/datapath/linux/config/config.go
@@ -245,6 +245,9 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		}
 		if option.Config.EnableHostPort {
 			cDefinesMap["ENABLE_HOSTPORT"] = "1"
+			if option.Config.HostPortOnlyLocal {
+				cDefinesMap["HOSTPORT_ONLY_LOCAL"] = "1"
+			}
 		}
 
 		cDefinesMap["NODEPORT_PORT_MIN"] = fmt.Sprintf("%d", option.Config.NodePortMin)

pkg/k8s/watchers/pod.go
@@ -278,7 +278,7 @@ func (k *K8sWatcher) deleteK8sPodV1(pod *types.Pod) error {
 	return err
 }
 
-func genServiceMappings(pod *types.Pod) []loadbalancer.SVC {
+func genHostPortServiceMappings(localIPv4 net.IP, localIPv6 net.IP, onlyLocal bool, pod *types.Pod) []loadbalancer.SVC {
 	var svcs []loadbalancer.SVC
 	for _, c := range pod.SpecContainers {
 		for _, p := range c.HostPorts {
@@ -289,6 +289,11 @@ func genServiceMappings(pod *types.Pod) []loadbalancer.SVC {
 			if feIP == nil {
 				feIP = net.ParseIP(pod.StatusHostIP)
 			}
+
+			if onlyLocal && !feIP.Equal(localIPv4) && !feIP.Equal(localIPv6) {
+				continue
+			}
+
 			proto, err := loadbalancer.NewL4Type(p.Protocol)
 			if err != nil {
 				continue
@@ -333,7 +338,9 @@ func (k *K8sWatcher) UpsertHostPortMapping(pod *types.Pod) error {
 		return nil
 	}
 
-	svcs := genServiceMappings(pod)
+	svcs := genHostPortServiceMappings(k.datapath.LocalNodeAddressing().IPv4().PrimaryExternal(),
+		k.datapath.LocalNodeAddressing().IPv6().PrimaryExternal(),
+		option.Config.HostPortOnlyLocal, pod)
 	if len(svcs) == 0 {
 		return nil
 	}
@@ -368,7 +375,9 @@ func (k *K8sWatcher) DeleteHostPortMapping(pod *types.Pod) error {
 		return nil
 	}
 
-	svcs := genServiceMappings(pod)
+	svcs := genHostPortServiceMappings(k.datapath.LocalNodeAddressing().IPv4().PrimaryExternal(),
+		k.datapath.LocalNodeAddressing().IPv6().PrimaryExternal(),
+		option.Config.HostPortOnlyLocal, pod)
 	if len(svcs) == 0 {
 		return nil
 	}

pkg/option/config.go
@@ -282,6 +282,9 @@ const (
 	// EnableHostPort enables HostPort forwarding implemented by Cilium in BPF
 	EnableHostPort = "enable-host-port"
 
+	// HostPortOnlyLocal only generates HostPort mapping on local node
+	HostPortOnlyLocal = "host-port-only-local"
+
 	// EnableNodePort enables NodePort services implemented by Cilium in BPF
 	EnableNodePort = "enable-node-port"
 
@@ -1712,6 +1715,9 @@ type DaemonConfig struct {
 	// EnableHostPort enables k8s Pod's hostPort mapping through BPF
 	EnableHostPort bool
 
+	// HostPortOnlyLocal only generates hostport mapping on local node
+	HostPortOnlyLocal bool
+
 	// NodePortMode indicates in which mode NodePort implementation should run
 	// ("snat", "dsr" or "hybrid")
 	NodePortMode string
@@ -2321,8 +2327,9 @@ func (c *DaemonConfig) Populate() {
 	c.EnableExternalIPs = viper.GetBool(EnableExternalIPs)
 	c.EnableL7Proxy = viper.GetBool(EnableL7Proxy)
 	c.EnableTracing = viper.GetBool(EnableTracing)
-	c.EnableNodePort = viper.GetBool(EnableNodePort)
 	c.EnableHostPort = viper.GetBool(EnableHostPort)
+	c.HostPortOnlyLocal = viper.GetBool(HostPortOnlyLocal)
+	c.EnableNodePort = viper.GetBool(EnableNodePort)
 	c.NodePortMode = viper.GetString(NodePortMode)
 	c.NodePortAcceleration = viper.GetString(NodePortAcceleration)
 	c.EnableAutoProtectNodePortRange = viper.GetBool(EnableAutoProtectNodePortRange)

api/v1/server/embedded_spec.go
@@ -2513,6 +2513,9 @@ func init() {
               "properties": {
                 "enabled": {
                   "type": "boolean"
+                },
+                "local": {
+                  "type": "boolean"
                 }
               }
             },
@@ -6061,6 +6064,9 @@ func init() {
               "type": "object",
               "properties": {
                 "enabled": {
+                  "type": "boolean"
+                },
+                "local": {
                   "type": "boolean"
                 }
               }