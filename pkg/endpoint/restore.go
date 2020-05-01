cilium/cilium

commit d81a5cdea511b43b1e0f0190861a306b2f3e5029

pkg/endpoint: Simplify search for C header file
Since we know the C header filename, we don't need to list the endpoint
directory's content. We can simply check if the file exists.

This commit retains the previous retry behavior: the endpoint header
file was sometimes not found before the second directory listing.
However, this commit introduces additional logging ('BUG' warning +
badLogMessages entry) to fail our E2E tests if the buggy behavior arises.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/endpoint/restore.go
@@ -18,7 +18,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -61,36 +60,29 @@ func ReadEPsFromDirNames(ctx context.Context, owner regeneration.Owner, basePath
 	possibleEPs := map[uint16]*Endpoint{}
 	for _, epDirName := range completeEPDirNames {
 		epDir := filepath.Join(basePath, epDirName)
-		readDir := func() string {
-			scopedLog := log.WithFields(logrus.Fields{
-				logfields.EndpointID: epDirName,
-				logfields.Path:       filepath.Join(epDir, common.CHeaderFileName),
-			})
-			scopedLog.Debug("Reading directory")
-			epFiles, err := ioutil.ReadDir(epDir)
-			if err != nil {
-				scopedLog.WithError(err).Warn("Error while reading directory. Ignoring it...")
-				return ""
-			}
-			cHeaderFile := common.FindEPConfigCHeader(epDir, epFiles)
-			if cHeaderFile == "" {
-				return ""
-			}
-			return cHeaderFile
-		}
-		// There's an odd issue where the first read dir doesn't work.
-		cHeaderFile := readDir()
-		if cHeaderFile == "" {
-			cHeaderFile = readDir()
-		}
-
+		cHeaderFile := filepath.Join(epDir, common.CHeaderFileName)
 		scopedLog := log.WithFields(logrus.Fields{
 			logfields.EndpointID: epDirName,
 			logfields.Path:       cHeaderFile,
 		})
+		// This function checks the presence of a bug previously observed: the
+		// file was sometimes only found after the second check.
+		// We can remove this if we haven't seen the issue occur after a while.
+		headerFileExists := func() error {
+			_, fileExists := os.Stat(cHeaderFile)
+			for i := 0; i < 2 && fileExists != nil; i++ {
+				time.Sleep(100 * time.Millisecond)
+				_, err := os.Stat(cHeaderFile)
+				if fileExists != err {
+					scopedLog.WithError(err).Warn("BUG: stat() has unstable behavior")
+				}
+				fileExists = err
+			}
+			return fileExists
+		}
 
-		if cHeaderFile == "" {
-			scopedLog.Warning("C header file not found. Ignoring endpoint")
+		if err := headerFileExists(); err != nil {
+			scopedLog.WithError(err).Warn("C header file not found. Ignoring endpoint")
 			continue
 		}
 

common/utils.go
@@ -50,17 +50,6 @@ func C2GoArray(str string) []byte {
 	return ret
 }
 
-// FindEPConfigCHeader returns the full path of the file that is the CHeaderFileName from
-// the slice of files
-func FindEPConfigCHeader(basePath string, epFiles []os.FileInfo) string {
-	for _, epFile := range epFiles {
-		if epFile.Name() == CHeaderFileName {
-			return filepath.Join(basePath, epFile.Name())
-		}
-	}
-	return ""
-}
-
 // GetCiliumVersionString returns the first line containing CiliumCHeaderPrefix.
 func GetCiliumVersionString(epCHeaderFilePath string) (string, error) {
 	f, err := os.Open(epCHeaderFilePath)

test/helpers/cons.go
@@ -217,6 +217,7 @@ const (
 	ClangErrorMsg      = "1 error generated."                         // from https://github.com/cilium/cilium/issues/10857
 	symbolSubstitution = "Skipping symbol substitution"               //
 	uninitializedRegen = "Uninitialized regeneration level"           // from https://github.com/cilium/cilium/pull/10949
+	unstableStat       = "BUG: stat() has unstable behavior"          // from https://github.com/cilium/cilium/pull/11028
 
 	// HelmTemplate is the location of the Helm templates to install Cilium
 	HelmTemplate = "../install/kubernetes/cilium"
@@ -276,6 +277,7 @@ var badLogMessages = map[string][]string{
 	ClangErrorMsg:      nil,
 	symbolSubstitution: nil,
 	uninitializedRegen: nil,
+	unstableStat:       nil,
 }
 
 var ciliumCLICommands = map[string]string{

commit 3dd7eb728dc86ae9ab5e8727d13386849c16ce3c

endpoint: Fix incorrect warning for stat(2)
Because André noticed a transient bug previously, we log a warning
message if we find stat(2) has an unstable behavior. The tests are
failed if that warning is found in the logs.

The check for stat(2)'s unstable behavior is however incorrect and leads
us to warn whenever a endpoint header file is not found. This commit
fixes it.

Fixes: d81a5cd ("pkg/endpoint: Simplify search for C header file")
Reported-by: André Martins <andre@cilium.io>
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/endpoint/restore.go
@@ -73,7 +73,7 @@ func ReadEPsFromDirNames(ctx context.Context, owner regeneration.Owner, basePath
 			for i := 0; i < 2 && fileExists != nil; i++ {
 				time.Sleep(100 * time.Millisecond)
 				_, err := os.Stat(cHeaderFile)
-				if fileExists != err {
+				if (fileExists == nil) != (err == nil) {
 					scopedLog.WithError(err).Warn("BUG: stat() has unstable behavior")
 				}
 				fileExists = err