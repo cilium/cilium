cilium/cilium

commit 308b946d1e94d686a073090017aef56838cf795a

pkg/k8s: add missing support for multi-stack
As multi-stack also means that pods can contain 2 or more IP addresses
we should also populate the local podIPs also with the ones provided by
kubernetes.

Fixes: 47a87f838709 ("pkg/k8s: add support for multi-stack")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/pod.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,8 +16,10 @@ package watchers
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
+	"strings"
 	"sync"
 
 	"github.com/cilium/cilium/pkg/annotation"
@@ -42,7 +44,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -149,7 +151,7 @@ func (k *K8sWatcher) addK8sPodV1(pod *types.Pod) error {
 	logger := log.WithFields(logrus.Fields{
 		logfields.K8sPodName:   pod.ObjectMeta.Name,
 		logfields.K8sNamespace: pod.ObjectMeta.Namespace,
-		"podIP":                pod.StatusPodIP,
+		"podIPs":               pod.StatusPodIPs,
 		"hostIP":               pod.StatusHostIP,
 	})
 
@@ -262,7 +264,7 @@ func (k *K8sWatcher) deleteK8sPodV1(pod *types.Pod) error {
 	logger := log.WithFields(logrus.Fields{
 		logfields.K8sPodName:   pod.ObjectMeta.Name,
 		logfields.K8sNamespace: pod.ObjectMeta.Namespace,
-		"podIP":                pod.StatusPodIP,
+		"podIPs":               pod.StatusPodIPs,
 		"hostIP":               pod.StatusHostIP,
 	})
 
@@ -303,19 +305,23 @@ func genServiceMappings(pod *types.Pod) []loadbalancer.SVC {
 				},
 				ID: loadbalancer.ID(0),
 			}
-			be := loadbalancer.Backend{
-				L3n4Addr: loadbalancer.L3n4Addr{
-					IP: net.ParseIP(pod.StatusPodIP),
-					L4Addr: loadbalancer.L4Addr{
-						Protocol: proto,
-						Port:     uint16(p.ContainerPort),
+			bes := make([]loadbalancer.Backend, 0, len(pod.StatusPodIPs))
+			for _, podIP := range pod.StatusPodIPs {
+				be := loadbalancer.Backend{
+					L3n4Addr: loadbalancer.L3n4Addr{
+						IP: net.ParseIP(podIP),
+						L4Addr: loadbalancer.L4Addr{
+							Protocol: proto,
+							Port:     uint16(p.ContainerPort),
+						},
 					},
-				},
+				}
+				bes = append(bes, be)
 			}
 			svcs = append(svcs,
 				loadbalancer.SVC{
 					Frontend: fe,
-					Backends: []loadbalancer.Backend{be},
+					Backends: bes,
 					Type:     loadbalancer.SVCTypeHostPort,
 					// We don't have the node name available here, but in
 					// any case in the BPF data path we drop any potential
@@ -341,7 +347,7 @@ func (k *K8sWatcher) UpsertHostPortMapping(pod *types.Pod) error {
 	logger := log.WithFields(logrus.Fields{
 		logfields.K8sPodName:   pod.ObjectMeta.Name,
 		logfields.K8sNamespace: pod.ObjectMeta.Namespace,
-		"podIP":                pod.StatusPodIP,
+		"podIPs":               pod.StatusPodIPs,
 		"hostIP":               pod.StatusHostIP,
 	})
 
@@ -376,7 +382,7 @@ func (k *K8sWatcher) DeleteHostPortMapping(pod *types.Pod) error {
 	logger := log.WithFields(logrus.Fields{
 		logfields.K8sPodName:   pod.ObjectMeta.Name,
 		logfields.K8sNamespace: pod.ObjectMeta.Namespace,
-		"podIP":                pod.StatusPodIP,
+		"podIPs":               pod.StatusPodIPs,
 		"hostIP":               pod.StatusHostIP,
 	})
 
@@ -401,14 +407,14 @@ func (k *K8sWatcher) updatePodHostData(pod *types.Pod) (bool, error) {
 		return true, fmt.Errorf("pod is using host networking")
 	}
 
-	podIP := net.ParseIP(pod.StatusPodIP)
-	if podIP == nil {
-		return true, fmt.Errorf("no/invalid PodIP: %s", pod.StatusPodIP)
+	err := validIPs(pod.StatusPodIPs)
+	if err != nil {
+		return true, err
 	}
 
-	err := k.UpsertHostPortMapping(pod)
+	err = k.UpsertHostPortMapping(pod)
 	if err != nil {
-		return true, fmt.Errorf("cannot upsert hostPort for PodIP: %s", pod.StatusPodIP)
+		return true, fmt.Errorf("cannot upsert hostPort for PodIPs: %s", pod.StatusPodIPs)
 	}
 
 	hostIP := net.ParseIP(pod.StatusHostIP)
@@ -423,18 +429,28 @@ func (k *K8sWatcher) updatePodHostData(pod *types.Pod) (bool, error) {
 		PodName:   pod.Name,
 	}
 
-	// Initial mapping of podIP <-> hostIP <-> identity. The mapping is
-	// later updated once the allocator has determined the real identity.
-	// If the endpoint remains unmanaged, the identity remains untouched.
-	selfOwned := ipcache.IPIdentityCache.Upsert(pod.StatusPodIP, hostIP, hostKey, k8sMeta, ipcache.Identity{
-		ID:     identity.ReservedIdentityUnmanaged,
-		Source: source.Kubernetes,
-	})
-	if !selfOwned {
-		return true, fmt.Errorf("ipcache entry owned by kvstore or agent")
+	var (
+		errs    []string
+		skipped bool
+	)
+	for _, podIP := range pod.StatusPodIPs {
+		// Initial mapping of podIP <-> hostIP <-> identity. The mapping is
+		// later updated once the allocator has determined the real identity.
+		// If the endpoint remains unmanaged, the identity remains untouched.
+		selfOwned := ipcache.IPIdentityCache.Upsert(podIP, hostIP, hostKey, k8sMeta, ipcache.Identity{
+			ID:     identity.ReservedIdentityUnmanaged,
+			Source: source.Kubernetes,
+		})
+		if !selfOwned {
+			skipped = true
+			errs = append(errs, fmt.Sprintf("ipcache entry for podIP %s owned by kvstore or agent", podIP))
+		}
+	}
+	if len(errs) != 0 {
+		return skipped, errors.New(strings.Join(errs, ", "))
 	}
 
-	return false, nil
+	return skipped, nil
 }
 
 func (k *K8sWatcher) deletePodHostData(pod *types.Pod) (bool, error) {
@@ -442,28 +458,54 @@ func (k *K8sWatcher) deletePodHostData(pod *types.Pod) (bool, error) {
 		return true, fmt.Errorf("pod is using host networking")
 	}
 
-	podIP := net.ParseIP(pod.StatusPodIP)
-	if podIP == nil {
-		return true, fmt.Errorf("no/invalid PodIP: %s", pod.StatusPodIP)
+	err := validIPs(pod.StatusPodIPs)
+	if err != nil {
+		return true, err
 	}
 
 	k.DeleteHostPortMapping(pod)
 
-	// a small race condition exists here as deletion could occur in
-	// parallel based on another event but it doesn't matter as the
-	// identity is going away
-	id, exists := ipcache.IPIdentityCache.LookupByIP(pod.StatusPodIP)
-	if !exists {
-		return true, fmt.Errorf("identity for IP does not exist in case")
+	var (
+		errs    []string
+		skipped bool
+	)
+
+	for _, podIP := range pod.StatusPodIPs {
+		// a small race condition exists here as deletion could occur in
+		// parallel based on another event but it doesn't matter as the
+		// identity is going away
+		id, exists := ipcache.IPIdentityCache.LookupByIP(podIP)
+		if !exists {
+			skipped = true
+			errs = append(errs, fmt.Sprintf("identity for IP %s does not exist in case", podIP))
+			continue
+		}
+
+		if id.Source != source.Kubernetes {
+			skipped = true
+			errs = append(errs, fmt.Sprintf("ipcache entry for IP %s not owned by kubernetes source", podIP))
+			continue
+		}
+
+		ipcache.IPIdentityCache.Delete(podIP, source.Kubernetes)
 	}
 
-	if id.Source != source.Kubernetes {
-		return true, fmt.Errorf("ipcache entry not owned by kubernetes source")
+	if len(errs) != 0 {
+		return skipped, errors.New(strings.Join(errs, ", "))
 	}
 
-	ipcache.IPIdentityCache.Delete(pod.StatusPodIP, source.Kubernetes)
+	return skipped, nil
+}
 
-	return false, nil
+func validIPs(ipStrs []string) error {
+	for _, ipStr := range ipStrs {
+		podIP := net.ParseIP(ipStr)
+		if podIP == nil {
+			return fmt.Errorf("no/invalid PodIP: %s", ipStr)
+		}
+	}
+
+	return nil
 }
 
 // GetCachedPod returns a pod from the local store. Depending if the Cilium
@@ -489,7 +531,7 @@ func (k *K8sWatcher) GetCachedPod(namespace, name string) (*types.Pod, error) {
 		return nil, err
 	}
 	if !exists {
-		return nil, errors.NewNotFound(schema.GroupResource{
+		return nil, k8sErrors.NewNotFound(schema.GroupResource{
 			Group:    "core",
 			Resource: "pod",
 		}, name)

pkg/k8s/factory_functions.go
@@ -16,6 +16,7 @@ package k8s
 
 import (
 	"reflect"
+	"sort"
 
 	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/comparator"
@@ -23,6 +24,7 @@ import (
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/strings"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/api/discovery/v1beta1"
@@ -210,10 +212,13 @@ func EqualV1PodContainers(c1, c2 types.PodContainer) bool {
 
 func EqualV1Pod(pod1, pod2 *types.Pod) bool {
 	// We only care about the HostIP, the PodIP and the labels of the pods.
-	if pod1.StatusPodIP != pod2.StatusPodIP ||
-		pod1.StatusHostIP != pod2.StatusHostIP ||
+	if pod1.StatusHostIP != pod2.StatusHostIP ||
 		pod1.SpecServiceAccountName != pod2.SpecServiceAccountName ||
 		pod1.SpecHostNetwork != pod2.SpecHostNetwork {
+		return false
+	}
+
+	if !strings.EqualStrings(pod1.StatusPodIPs, pod2.StatusPodIPs) {
 		return false
 	}
 
@@ -543,6 +548,30 @@ func ConvertToCNP(obj interface{}) interface{} {
 	}
 }
 
+func getPodIPs(podStats v1.PodStatus) []string {
+	// make it a set first
+	ipsMap := make(map[string]struct{}, 1+len(podStats.PodIPs))
+	if podStats.PodIP != "" {
+		ipsMap[podStats.PodIP] = struct{}{}
+	}
+	for _, podIP := range podStats.PodIPs {
+		if podIP.IP != "" {
+			ipsMap[podIP.IP] = struct{}{}
+		}
+	}
+
+	if len(ipsMap) == 0 {
+		return nil
+	}
+
+	ips := make([]string, 0, len(ipsMap))
+	for ipStr := range ipsMap {
+		ips = append(ips, ipStr)
+	}
+	sort.Strings(ips)
+	return ips
+}
+
 // ConvertToPod converts a *v1.Pod into a
 // *types.Pod or a cache.DeletedFinalStateUnknown into
 // a cache.DeletedFinalStateUnknown with a *types.Pod in its Obj.
@@ -580,7 +609,7 @@ func ConvertToPod(obj interface{}) interface{} {
 		p := &types.Pod{
 			TypeMeta:               concreteObj.TypeMeta,
 			ObjectMeta:             concreteObj.ObjectMeta,
-			StatusPodIP:            concreteObj.Status.PodIP,
+			StatusPodIPs:           getPodIPs(concreteObj.Status),
 			StatusHostIP:           concreteObj.Status.HostIP,
 			SpecServiceAccountName: concreteObj.Spec.ServiceAccountName,
 			SpecHostNetwork:        concreteObj.Spec.HostNetwork,
@@ -622,7 +651,7 @@ func ConvertToPod(obj interface{}) interface{} {
 			Obj: &types.Pod{
 				TypeMeta:               pod.TypeMeta,
 				ObjectMeta:             pod.ObjectMeta,
-				StatusPodIP:            pod.Status.PodIP,
+				StatusPodIPs:           getPodIPs(pod.Status),
 				StatusHostIP:           pod.Status.HostIP,
 				SpecServiceAccountName: pod.Spec.ServiceAccountName,
 				SpecHostNetwork:        pod.Spec.HostNetwork,

pkg/k8s/factory_functions_test.go
@@ -329,14 +329,14 @@ func (s *K8sSuite) Test_EqualV1Pod(c *C) {
 						Name: "pod1",
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 				o2: &types.Pod{
 					ObjectMeta: metav1.ObjectMeta{
 						Name: "pod1",
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.1",
+					StatusPodIPs: []string{"127.0.0.1"},
 				},
 			},
 			want: false,
@@ -349,14 +349,14 @@ func (s *K8sSuite) Test_EqualV1Pod(c *C) {
 						Name: "pod1",
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 				o2: &types.Pod{
 					ObjectMeta: metav1.ObjectMeta{
 						Name: "pod1",
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 			},
 			want: true,
@@ -372,14 +372,14 @@ func (s *K8sSuite) Test_EqualV1Pod(c *C) {
 						},
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 				o2: &types.Pod{
 					ObjectMeta: metav1.ObjectMeta{
 						Name: "pod1",
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 			},
 			want: false,
@@ -395,7 +395,7 @@ func (s *K8sSuite) Test_EqualV1Pod(c *C) {
 						},
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 				o2: &types.Pod{
 					ObjectMeta: metav1.ObjectMeta{
@@ -405,7 +405,7 @@ func (s *K8sSuite) Test_EqualV1Pod(c *C) {
 						},
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 			},
 			want: true,
@@ -421,7 +421,7 @@ func (s *K8sSuite) Test_EqualV1Pod(c *C) {
 						},
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 				o2: &types.Pod{
 					ObjectMeta: metav1.ObjectMeta{
@@ -434,7 +434,7 @@ func (s *K8sSuite) Test_EqualV1Pod(c *C) {
 						},
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 			},
 			want: false,
@@ -450,7 +450,7 @@ func (s *K8sSuite) Test_EqualV1Pod(c *C) {
 						},
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 				o2: &types.Pod{
 					ObjectMeta: metav1.ObjectMeta{
@@ -463,7 +463,7 @@ func (s *K8sSuite) Test_EqualV1Pod(c *C) {
 						},
 					},
 					StatusHostIP: "127.0.0.1",
-					StatusPodIP:  "127.0.0.2",
+					StatusPodIPs: []string{"127.0.0.2"},
 				},
 			},
 			want: true,

pkg/k8s/types/types.go
@@ -66,7 +66,7 @@ type PodContainer struct {
 type Pod struct {
 	metav1.TypeMeta
 	metav1.ObjectMeta
-	StatusPodIP            string
+	StatusPodIPs           []string
 	StatusHostIP           string
 	SpecServiceAccountName string
 	SpecHostNetwork        bool

pkg/k8s/types/zz_generated.deepcopy.go
@@ -273,6 +273,11 @@ func (in *Pod) DeepCopyInto(out *Pod) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
 	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	if in.StatusPodIPs != nil {
+		in, out := &in.StatusPodIPs, &out.StatusPodIPs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	if in.SpecContainers != nil {
 		in, out := &in.SpecContainers, &out.SpecContainers
 		*out = make([]PodContainer, len(*in))

pkg/strings/slices.go
@@ -1,0 +1,35 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package strings
+
+// EqualStrings returns true if both slices are equal.
+func EqualStrings(a, b []string) bool {
+	// If one is nil, the other must also be nil.
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}

pkg/strings/slices_test.go
@@ -1,0 +1,81 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package strings
+
+import (
+	"testing"
+)
+
+func TestEqualStrings(t *testing.T) {
+	type args struct {
+		a []string
+		b []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test-1",
+			args: args{
+				a: nil,
+				b: nil,
+			},
+			want: true,
+		},
+		{
+			name: "test-2",
+			args: args{
+				a: []string{""},
+				b: nil,
+			},
+			want: false,
+		},
+		{
+			name: "test-3",
+			args: args{
+				a: nil,
+				b: []string{"foo"},
+			},
+			want: false,
+		},
+		{
+			name: "test-4",
+			args: args{
+				a: []string{"foo"},
+				b: []string{"foo"},
+			},
+			want: true,
+		},
+		{
+			name: "test-5",
+			args: args{
+				a: []string{"bar", "foo"},
+				b: []string{"foo", "bar"},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := EqualStrings(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("EqualStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

commit 30af31a1ddd23bbf3cb4af1c5c2dc8e6b53e2228

k8s/watchers: do not consider pods with empty podIPs
Fixes: 308b946d1e94 ("pkg/k8s: add missing support for multi-stack")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/pod.go
@@ -498,6 +498,9 @@ func (k *K8sWatcher) deletePodHostData(pod *types.Pod) (bool, error) {
 }
 
 func validIPs(ipStrs []string) error {
+	if len(ipStrs) == 0 {
+		return fmt.Errorf("empty PodIPs")
+	}
 	for _, ipStr := range ipStrs {
 		podIP := net.ParseIP(ipStr)
 		if podIP == nil {