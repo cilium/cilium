cilium/cilium

commit 3c784c74acfea5083ac43ab9958bfe86119bc7ab

Move all 'common' code to 'pkg'
This PR moves the common packages to the pkg folder.

Fixes: #11293
Signed-off-by: Swaminathan Vasudevan <svasudevan@suse.com>

Vagrantfile
@@ -65,7 +65,7 @@ set -o pipefail
 
 export PATH=/home/vagrant/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 #{$makeclean}
-~/go/src/github.com/cilium/cilium/common/build.sh
+~/go/src/github.com/cilium/cilium/pkg/common/build.sh
 rm -fr ~/go/bin/cilium*
 SCRIPT
 

commit 62fbeb503386bcb72d217bbe2cebef07f29dcdaf

vagrant: Fix build in dev. VM
The build script run inside the dev. VM was inadvertently broken when
moved from common/ to pkg/common/. This commit fixes that and moves that
script to contrib/vagrant/; since only the dev. VM uses that script it
makes more sense to have it with other dev. scripts.

Fixes: 3c784c7 ("Move all 'common' code to 'pkg'")
Signed-off-by: Paul Chaignon <paul@cilium.io>

Vagrantfile
@@ -65,7 +65,7 @@ set -o pipefail
 
 export PATH=/home/vagrant/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 #{$makeclean}
-~/go/src/github.com/cilium/cilium/pkg/common/build.sh
+~/go/src/github.com/cilium/cilium/contrib/vagrant/build.sh
 rm -fr ~/go/bin/cilium*
 SCRIPT
 

contrib/vagrant/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 pushd `dirname $0` > /dev/null
-P="`pwd`/.."
+P="`pwd`/../.."
 popd > /dev/null
 
 cd "$P"