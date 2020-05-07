2119bbb07fa9ec262045df43e93a8b0c16e9a5fc
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-05-07 11:00:37 +0100

    test: Annotate lock deployment with Jenkins $BUILD_URL
    This helpful for tracing which cluster is being used by what job.
    
    Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

--- test/gke/select-cluster.sh
+++ test/gke/select-cluster.sh
@@ -28,6 +28,9 @@ while [ $locked -ne 0 ]; do
     kubectl create -f "${script_dir}/lock.yaml"
 
     kubectl annotate deployment lock lock=1
+    if [ -n "${BUILD_URL+x}" ] ; then
+      kubectl annotate deployment lock "jenkins-build-url=${BUILD_URL}"
+    fi
     locked=$?
     echo $locked
     set -e
