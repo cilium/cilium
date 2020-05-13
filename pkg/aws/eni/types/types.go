0ea1dbae21dd6adc3bae0fe2d5ea6f2279b506c4
Chris Tarazi <chris@isovalent.com>
2020-05-12 17:48:06 -0700

    k8s: Generate CiliumNode CRD
    This commit adds resource name generation via markers. It also updates
    the validation schema via markers which the old method of hardcoding had
    a few fields missing / incorrect.
    
    Here's a summary of the main differences in the validation schema:
    
    1) Old schema had eniTypes.ENISpec.MaxAllocate, which doesn't exist. It
       exists under ipamTypes.IPAMSpec.MaxAllocate.
    2) Old schema was missing eniTypes.ENISpec.{InstanceID,InstanceType},
       which is now included in this new generated schema.
    3) Old schema was missing EncryptionSpec, which is now included in this
       new generated schema.
    4) Old schema has azureTypes.AzureSpec.InstanceID, but that was removed
       and deprecated in favor of ipamTypes.IPAMSpec.InstanceID. See PR
       which forgot to remove this from the old validation schema. [1]
    
    This CRD is generated using
    https://book.kubebuilder.io/reference/generating-crd.html.
    
    [1]: https://github.com/cilium/cilium/pull/10569
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/aws/eni/types/types.go
+++ pkg/aws/eni/types/types.go
@@ -31,9 +31,13 @@ type ENISpec struct {
 	// to retrieve AWS metadata for the node.
 	//
 	// OBSOLETE: This field is obsolete, please use Spec.InstanceID
+	//
+	// +kubebuilder:validation:Optional
 	InstanceID string `json:"instance-id,omitempty"`
 
 	// InstanceType is the AWS EC2 instance type, e.g. "m5.large"
+	//
+	// +kubebuilder:validation:Optional
 	InstanceType string `json:"instance-type,omitempty"`
 
 	// MinAllocate is the minimum number of IPs that must be allocated when
@@ -44,7 +48,8 @@ type ENISpec struct {
 	//
 	// OBSOLETE: This field is obsolete, please use Spec.IPAM.MinAllocate
 	//
-	// +optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Optional
 	MinAllocate int `json:"min-allocate,omitempty"`
 
 	// PreAllocate defines the number of IP addresses that must be
@@ -54,7 +59,8 @@ type ENISpec struct {
 	//
 	// OBSOLETE: This field is obsolete, please use Spec.IPAM.PreAllocate
 	//
-	// +optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Optional
 	PreAllocate int `json:"pre-allocate,omitempty"`
 
 	// MaxAboveWatermark is the maximum number of addresses to allocate
@@ -66,7 +72,8 @@ type ENISpec struct {
 	//
 	// OBSOLETE: This field is obsolete, please use Spec.IPAM.MaxAboveWatermark
 	//
-	// +optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Optional
 	MaxAboveWatermark int `json:"max-above-watermark,omitempty"`
 
 	// FirstInterfaceIndex is the index of the first ENI to use for IP
@@ -74,39 +81,44 @@ type ENISpec struct {
 	// FirstInterfaceIndex is set to 1, then only eth1 and eth2 will be
 	// used for IP allocation, eth0 will be ignored for PodIP allocation.
 	//
-	// +optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Optional
 	FirstInterfaceIndex *int `json:"first-interface-index,omitempty"`
 
 	// SecurityGroups is the list of security groups to attach to any ENI
 	// that is created and attached to the instance.
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	SecurityGroups []string `json:"security-groups,omitempty"`
 
 	// SecurityGroupTags is the list of tags to use when evaliating what
 	// AWS security groups to use for the ENI.
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	SecurityGroupTags map[string]string `json:"security-group-tags,omitempty"`
 
 	// SubnetTags is the list of tags to use when evaluating what AWS
-	// subnets to use for ENI and IP allocation
+	// subnets to use for ENI and IP allocation.
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	SubnetTags map[string]string `json:"subnet-tags,omitempty"`
 
-	// VpcID is the VPC ID to use when allocating ENIs
+	// VpcID is the VPC ID to use when allocating ENIs.
+	//
+	// +kubebuilder:validation:Optional
 	VpcID string `json:"vpc-id,omitempty"`
 
 	// AvailabilityZone is the availability zone to use when allocating
-	// ENIs
+	// ENIs.
+	//
+	// +kubebuilder:validation:Optional
 	AvailabilityZone string `json:"availability-zone,omitempty"`
 
 	// DeleteOnTermination defines that the ENI should be deleted when the
 	// associated instance is terminated. If the parameter is not set the
 	// default behavior is to delete the ENI on instance termination.
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	DeleteOnTermination *bool `json:"delete-on-termination,omitempty"`
 }
 
