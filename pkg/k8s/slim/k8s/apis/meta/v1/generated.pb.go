cilium/cilium

commit 3a1bde594b61b1e3425a216b8ae4f7c84b0b8136

types/slim: add slim packages for k8s structures
With these slimmer structures Cilium will only unmarshal the fields
that it uses as opposed to unmarshal all fields and later on ignore the
unused fields.

For now, only Pod will be added as a PoC, later on all other k8s
structures will have a similar change.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/slim/k8s/apis/meta/v1/generated.proto
@@ -1,0 +1,128 @@
+// Copyright 2017-2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+// This file was autogenerated by go-to-protobuf. Do not edit it manually!
+
+syntax = 'proto2';
+
+package github.com.cilium.cilium.pkg.k8s.slim.k8s.apis.meta.v1;
+
+// Package-wide variables from generator "generated".
+option go_package = "v1";
+
+// ListMeta describes metadata that synthetic resources must have, including lists and
+// various status objects. A resource may have only one of {ObjectMeta, ListMeta}.
+message ListMeta {
+  // String that identifies the server's internal version of this object that
+  // can be used by clients to determine when objects have changed.
+  // Value must be treated as opaque by clients and passed unmodified back to the server.
+  // Populated by the system.
+  // Read-only.
+  // More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#concurrency-control-and-consistency
+  // +optional
+  optional string resourceVersion = 2;
+
+  // continue may be set if the user set a limit on the number of items returned, and indicates that
+  // the server has more data available. The value is opaque and may be used to issue another request
+  // to the endpoint that served this list to retrieve the next set of available objects. Continuing a
+  // consistent list may not be possible if the server configuration has changed or more than a few
+  // minutes have passed. The resourceVersion field returned when using this continue value will be
+  // identical to the value in the first response, unless you have received this token from an error
+  // message.
+  optional string continue = 3;
+
+  // remainingItemCount is the number of subsequent items in the list which are not included in this
+  // list response. If the list request contained label or field selectors, then the number of
+  // remaining items is unknown and the field will be left unset and omitted during serialization.
+  // If the list is complete (either because it is not chunking or because this is the last chunk),
+  // then there are no more remaining items and this field will be left unset and omitted during
+  // serialization.
+  // Servers older than v1.15 do not set this field.
+  // The intended use of the remainingItemCount is *estimating* the size of a collection. Clients
+  // should not rely on the remainingItemCount to be set or to be exact.
+  // +optional
+  optional int64 remainingItemCount = 4;
+}
+
+// ObjectMeta is metadata that all persisted resources must have, which includes all objects
+// users must create.
+message ObjectMeta {
+  // Name must be unique within a namespace. Is required when creating resources, although
+  // some resources may allow a client to request the generation of an appropriate name
+  // automatically. Name is primarily intended for creation idempotence and configuration
+  // definition.
+  // Cannot be updated.
+  // More info: http://kubernetes.io/docs/user-guide/identifiers#names
+  // +optional
+  optional string name = 1;
+
+  // Namespace defines the space within each name must be unique. An empty namespace is
+  // equivalent to the "default" namespace, but "default" is the canonical representation.
+  // Not all objects are required to be scoped to a namespace - the value of this field for
+  // those objects will be empty.
+  //
+  // Must be a DNS_LABEL.
+  // Cannot be updated.
+  // More info: http://kubernetes.io/docs/user-guide/namespaces
+  // +optional
+  optional string namespace = 3;
+
+  // UID is the unique in time and space value for this object. It is typically generated by
+  // the server on successful creation of a resource and is not allowed to change on PUT
+  // operations.
+  //
+  // Populated by the system.
+  // Read-only.
+  // More info: http://kubernetes.io/docs/user-guide/identifiers#uids
+  // +optional
+  optional string uid = 5;
+
+  // Map of string keys and values that can be used to organize and categorize
+  // (scope and select) objects. May match selectors of replication controllers
+  // and services.
+  // More info: http://kubernetes.io/docs/user-guide/labels
+  // +optional
+  map<string, string> labels = 11;
+
+  // Annotations is an unstructured key value map stored with a resource that may be
+  // set by external tools to store and retrieve arbitrary metadata. They are not
+  // queryable and should be preserved when modifying objects.
+  // More info: http://kubernetes.io/docs/user-guide/annotations
+  // +optional
+  map<string, string> annotations = 12;
+}
+
+// TypeMeta describes an individual object in an API response or request
+// with strings representing the type of the object and its API schema version.
+// Structures that are versioned or persisted should inline TypeMeta.
+//
+// +k8s:deepcopy-gen=false
+message TypeMeta {
+  // Kind is a string value representing the REST resource this object represents.
+  // Servers may infer this from the endpoint the client submits requests to.
+  // Cannot be updated.
+  // In CamelCase.
+  // More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+  // +optional
+  optional string kind = 1;
+
+  // APIVersion defines the versioned schema of this representation of an object.
+  // Servers should convert recognized schemas to the latest internal value, and
+  // may reject unrecognized values.
+  // More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+  // +optional
+  optional string apiVersion = 2;
+}
+

pkg/k8s/slim/k8s/apis/meta/v1/meta.go
@@ -1,0 +1,92 @@
+// Copyright 2016 The Kubernetes Authors.
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func (meta *ListMeta) GetResourceVersion() string        { return meta.ResourceVersion }
+func (meta *ListMeta) SetResourceVersion(version string) { meta.ResourceVersion = version }
+func (meta *ListMeta) GetSelfLink() string               { return "" }
+func (meta *ListMeta) SetSelfLink(_ string)              {}
+func (meta *ListMeta) GetContinue() string               { return meta.Continue }
+func (meta *ListMeta) SetContinue(c string)              { meta.Continue = c }
+func (meta *ListMeta) GetRemainingItemCount() *int64     { return meta.RemainingItemCount }
+func (meta *ListMeta) SetRemainingItemCount(c *int64)    { meta.RemainingItemCount = c }
+
+func (obj *TypeMeta) GetObjectKind() schema.ObjectKind { return obj }
+
+// SetGroupVersionKind satisfies the ObjectKind interface for all objects that embed TypeMeta
+func (obj *TypeMeta) SetGroupVersionKind(gvk schema.GroupVersionKind) {
+	obj.APIVersion, obj.Kind = gvk.ToAPIVersionAndKind()
+}
+
+// GroupVersionKind satisfies the ObjectKind interface for all objects that embed TypeMeta
+func (obj *TypeMeta) GroupVersionKind() schema.GroupVersionKind {
+	return schema.FromAPIVersionAndKind(obj.APIVersion, obj.Kind)
+}
+
+func (obj *ListMeta) GetListMeta() metav1.ListInterface { return obj }
+
+func (obj *ObjectMeta) GetObjectMeta() metav1.Object { return obj }
+
+// Namespace implements metav1.Object for any object with an ObjectMeta typed field. Allows
+// fast, direct access to metadata fields for API objects.
+func (meta *ObjectMeta) GetNamespace() string              { return meta.Namespace }
+func (meta *ObjectMeta) SetNamespace(namespace string)     { meta.Namespace = namespace }
+func (meta *ObjectMeta) GetName() string                   { return meta.Name }
+func (meta *ObjectMeta) SetName(name string)               { meta.Name = name }
+func (meta *ObjectMeta) GetGenerateName() string           { return "" }
+func (meta *ObjectMeta) SetGenerateName(string)            {}
+func (meta *ObjectMeta) GetUID() types.UID                 { return meta.UID }
+func (meta *ObjectMeta) SetUID(uid types.UID)              { meta.UID = uid }
+func (meta *ObjectMeta) GetResourceVersion() string        { return "" }
+func (meta *ObjectMeta) SetResourceVersion(_ string)       {}
+func (meta *ObjectMeta) GetGeneration() int64              { return 0 }
+func (meta *ObjectMeta) SetGeneration(_ int64)             {}
+func (meta *ObjectMeta) GetSelfLink() string               { return "" }
+func (meta *ObjectMeta) SetSelfLink(_ string)              {}
+func (meta *ObjectMeta) GetCreationTimestamp() metav1.Time { return metav1.Time{} }
+func (meta *ObjectMeta) SetCreationTimestamp(_ metav1.Time) {
+
+}
+func (meta *ObjectMeta) GetDeletionTimestamp() *metav1.Time { return &metav1.Time{} }
+func (meta *ObjectMeta) SetDeletionTimestamp(_ *metav1.Time) {
+
+}
+func (meta *ObjectMeta) GetDeletionGracePeriodSeconds() *int64 {
+	return func() *int64 { a := int64(0); return &a }()
+}
+func (meta *ObjectMeta) SetDeletionGracePeriodSeconds(_ *int64) {
+
+}
+func (meta *ObjectMeta) GetLabels() map[string]string                 { return meta.Labels }
+func (meta *ObjectMeta) SetLabels(labels map[string]string)           { meta.Labels = labels }
+func (meta *ObjectMeta) GetAnnotations() map[string]string            { return meta.Annotations }
+func (meta *ObjectMeta) SetAnnotations(annotations map[string]string) { meta.Annotations = annotations }
+func (meta *ObjectMeta) GetFinalizers() []string                      { return nil }
+func (meta *ObjectMeta) SetFinalizers(_ []string)                     {}
+func (meta *ObjectMeta) GetOwnerReferences() []metav1.OwnerReference  { return nil }
+func (meta *ObjectMeta) SetOwnerReferences(_ []metav1.OwnerReference) {
+
+}
+func (meta *ObjectMeta) GetClusterName() string                         { return "" }
+func (meta *ObjectMeta) SetClusterName(_ string)                        {}
+func (meta *ObjectMeta) GetManagedFields() []metav1.ManagedFieldsEntry  { return nil }
+func (meta *ObjectMeta) SetManagedFields(_ []metav1.ManagedFieldsEntry) {}

commit 96c7cd629f971c1908d74e10f7eabc43c455344e

slim/k8s: add missing resourceVersion field
If the resource version is not stored, the watcher won't be able to keep
up the last resource version it has seen for this object type so it will
assume that any event received from kube-apiserver is an event that
should be processed.

This explains why Cilium was seeing so many events received from
kube-apiserver.

Fixes: 3a1bde594b61 ("types/slim: add slim packages for k8s structures")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/slim/k8s/apis/meta/v1/generated.pb.go
@@ -251,57 +251,57 @@ func init() {
 }
 
 var fileDescriptor_e0f89ca41f751b36 = []byte{
-	// 791 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x54, 0x41, 0x8f, 0xdb, 0x44,
-	0x18, 0xb5, 0xe3, 0x64, 0x9b, 0x4c, 0x5a, 0x36, 0x0c, 0x05, 0xac, 0x95, 0xb0, 0x43, 0xb8, 0xa4,
-	0x12, 0xd8, 0x6c, 0x0f, 0x68, 0xe9, 0x01, 0x69, 0xdd, 0x6e, 0xa5, 0xa8, 0xdd, 0xb4, 0x9a, 0x6d,
-	0xf7, 0x00, 0x17, 0x26, 0xce, 0x90, 0x1d, 0x12, 0xcf, 0x18, 0xcf, 0x38, 0x22, 0xb7, 0x1e, 0x11,
-	0x12, 0x52, 0x4f, 0x88, 0x63, 0x57, 0xf0, 0x4f, 0xb8, 0xec, 0xb1, 0xc7, 0x1e, 0x50, 0xc4, 0x9a,
-	0x3f, 0x81, 0x7a, 0x42, 0x1e, 0x8f, 0x37, 0x4e, 0x0a, 0x07, 0x56, 0x7b, 0x9a, 0x99, 0xef, 0x7d,
-	0xf3, 0xde, 0x1b, 0xe7, 0x7b, 0x01, 0xf7, 0x27, 0x54, 0x9e, 0xa4, 0x23, 0x2f, 0xe4, 0x91, 0x1f,
-	0xd2, 0x19, 0x4d, 0x2f, 0x96, 0x78, 0x3a, 0xf1, 0xa7, 0x7b, 0xc2, 0x17, 0x33, 0x1a, 0xa9, 0x0d,
-	0x8e, 0xa9, 0xf0, 0x23, 0x22, 0xb1, 0x3f, 0xdf, 0xf5, 0x27, 0x84, 0x91, 0x04, 0x4b, 0x32, 0xf6,
-	0xe2, 0x84, 0x4b, 0x0e, 0x3f, 0x5b, 0xf1, 0x78, 0x05, 0x41, 0xb9, 0xc4, 0xd3, 0x89, 0x37, 0xdd,
-	0x13, 0x5e, 0xce, 0xa3, 0x36, 0x39, 0x8f, 0x97, 0xf3, 0x78, 0xf3, 0xdd, 0x9d, 0x4f, 0x2a, 0xfa,
-	0x13, 0x3e, 0xe1, 0xbe, 0xa2, 0x1b, 0xa5, 0xdf, 0xa8, 0x93, 0x3a, 0xa8, 0x5d, 0x21, 0xd3, 0xfb,
-	0xbb, 0x06, 0x6e, 0x3c, 0xc4, 0x23, 0x32, 0x3b, 0x22, 0x33, 0x12, 0x4a, 0x9e, 0xc0, 0x9f, 0x4c,
-	0xd0, 0x8e, 0xb0, 0x0c, 0x4f, 0x54, 0x59, 0xd8, 0x66, 0xd7, 0xea, 0xb7, 0x6f, 0x1f, 0x7b, 0x97,
-	0xf3, 0xe3, 0xad, 0x91, 0x7b, 0x87, 0x2b, 0xe2, 0x03, 0x26, 0x93, 0x45, 0xf0, 0xce, 0xd9, 0xd2,
-	0x35, 0xb2, 0xa5, 0xdb, 0xae, 0x20, 0xa8, 0xaa, 0x0f, 0x7f, 0x36, 0x41, 0x47, 0x9d, 0x0f, 0xbe,
-	0x8f, 0x13, 0x22, 0x04, 0xe5, 0x4c, 0xd8, 0x35, 0x65, 0xea, 0xf1, 0x95, 0x98, 0x42, 0xe4, 0xbb,
-	0x94, 0x26, 0x24, 0x22, 0x4c, 0x06, 0xb6, 0xb6, 0xd3, 0x39, 0xdc, 0x50, 0x44, 0x6f, 0x78, 0xd8,
-	0xf9, 0x02, 0x74, 0x36, 0x9f, 0x03, 0x3b, 0xc0, 0x9a, 0x92, 0x85, 0x6d, 0x76, 0xcd, 0x7e, 0x0b,
-	0xe5, 0x5b, 0x78, 0x13, 0x34, 0xe6, 0x78, 0x96, 0x12, 0xbb, 0xa6, 0x6a, 0xc5, 0xe1, 0x4e, 0x6d,
-	0xcf, 0xec, 0xfd, 0x66, 0x02, 0xfb, 0xbf, 0x8c, 0xc0, 0x0f, 0x2a, 0x44, 0x41, 0x5b, 0xbb, 0xb2,
-	0x1e, 0x90, 0x45, 0xc1, 0x7a, 0x00, 0x9a, 0x3c, 0xce, 0xe7, 0x85, 0x27, 0x05, 0x71, 0x70, 0x4b,
-	0xf7, 0x34, 0x1f, 0xe9, 0xfa, 0xeb, 0xa5, 0xfb, 0xee, 0x1a, 0x7d, 0x09, 0xa0, 0x8b, 0xab, 0xb0,
-	0x07, 0xb6, 0x94, 0x1f, 0x61, 0x5b, 0x5d, 0xab, 0xdf, 0x0a, 0x40, 0xb6, 0x74, 0xb7, 0x8e, 0x55,
-	0x05, 0x69, 0xa4, 0xf7, 0xbb, 0x09, 0x9a, 0x0f, 0xa9, 0x90, 0x87, 0x44, 0x62, 0xb8, 0x0f, 0xb6,
-	0x13, 0x22, 0x78, 0x9a, 0x84, 0xe4, 0x98, 0x24, 0xf9, 0x77, 0xd0, 0xf2, 0xef, 0x6b, 0xf9, 0x6d,
-	0xb4, 0x0e, 0xa3, 0xcd, 0x7e, 0xf8, 0x31, 0x68, 0x86, 0x9c, 0x49, 0xca, 0x52, 0x62, 0x5b, 0xea,
-	0x6e, 0xa7, 0xb4, 0x7e, 0x57, 0xd7, 0xd1, 0x45, 0x07, 0xbc, 0x0f, 0x60, 0x42, 0x22, 0x4c, 0x19,
-	0x65, 0x93, 0x81, 0x24, 0xd1, 0x5d, 0x9e, 0x32, 0x69, 0xd7, 0xbb, 0x66, 0xdf, 0x0a, 0xde, 0xcb,
-	0x96, 0x2e, 0x44, 0x6f, 0xa0, 0xe8, 0x5f, 0x6e, 0xf4, 0x7e, 0xad, 0x03, 0xf0, 0x68, 0xf4, 0x2d,
-	0x09, 0x8b, 0x77, 0x74, 0x41, 0x9d, 0xe1, 0x88, 0xe8, 0xef, 0x7b, 0x5d, 0x1b, 0xa8, 0x0f, 0x71,
-	0x44, 0x90, 0x42, 0xa0, 0x0f, 0x5a, 0xf9, 0x2a, 0x62, 0x1c, 0x96, 0x3e, 0xdf, 0xd6, 0x6d, 0xad,
-	0x61, 0x09, 0xa0, 0x55, 0x0f, 0x0c, 0x80, 0x95, 0xd2, 0xb1, 0xdd, 0x50, 0xad, 0x9f, 0x96, 0xbf,
-	0xd8, 0xd3, 0xc1, 0xbd, 0xd7, 0x4b, 0xf7, 0xc3, 0x7c, 0x02, 0x29, 0xcf, 0x13, 0x1f, 0xe1, 0xf0,
-	0x84, 0x32, 0x92, 0x2c, 0xd4, 0xff, 0x81, 0x5c, 0xc4, 0x44, 0x78, 0x4f, 0x07, 0xf7, 0x50, 0x7e,
-	0x19, 0xce, 0xc1, 0xd6, 0xac, 0x48, 0x5d, 0x5b, 0x0d, 0xf8, 0xf0, 0xb2, 0x03, 0xbe, 0x7a, 0xaa,
-	0x57, 0x4d, 0xdb, 0x5b, 0xda, 0xd6, 0x96, 0x0e, 0x9a, 0x56, 0x83, 0x3f, 0x9a, 0xa0, 0x8d, 0x19,
-	0xe3, 0x12, 0x4b, 0x15, 0xaf, 0xeb, 0x4a, 0xfd, 0xe8, 0x0a, 0xd4, 0xf7, 0x57, 0xac, 0x1b, 0x81,
-	0xaf, 0x20, 0xa8, 0x2a, 0xbe, 0xf3, 0x39, 0x68, 0x5f, 0x32, 0x52, 0x79, 0x24, 0x37, 0x05, 0xff,
-	0x57, 0x24, 0x67, 0xa0, 0xfe, 0x84, 0x46, 0x04, 0xde, 0x02, 0xd7, 0x04, 0x09, 0x39, 0x1b, 0x0b,
-	0x75, 0xcf, 0x0a, 0xb6, 0xb5, 0xeb, 0x6b, 0x47, 0x45, 0x19, 0x95, 0x38, 0xfc, 0x08, 0x34, 0x18,
-	0x66, 0x5c, 0x28, 0xb2, 0x46, 0x70, 0x43, 0x37, 0x36, 0x86, 0x79, 0x11, 0x15, 0xd8, 0x9d, 0x9b,
-	0xbf, 0xbc, 0x70, 0x8d, 0x1f, 0x4e, 0x5d, 0xe3, 0xf9, 0xa9, 0x6b, 0xbc, 0x38, 0x75, 0x8d, 0x67,
-	0x7f, 0x74, 0x8d, 0xde, 0x57, 0xa0, 0x95, 0xab, 0x09, 0x89, 0xa3, 0xf8, 0xaa, 0x25, 0x7b, 0x5f,
-	0x83, 0xe6, 0x93, 0x45, 0x4c, 0xca, 0x69, 0x9f, 0x52, 0x36, 0xde, 0x9c, 0xf6, 0x07, 0x94, 0x8d,
-	0x91, 0x42, 0xe0, 0x6d, 0x00, 0x70, 0x4c, 0xd7, 0x23, 0x0d, 0x75, 0x1f, 0xd8, 0x7f, 0x3c, 0x28,
-	0xd3, 0x5c, 0xe9, 0x0a, 0xfa, 0x67, 0xe7, 0x8e, 0xf1, 0xf2, 0xdc, 0x31, 0x5e, 0x9d, 0x3b, 0xc6,
-	0xb3, 0xcc, 0x31, 0xcf, 0x32, 0xc7, 0x7c, 0x99, 0x39, 0xe6, 0xab, 0xcc, 0x31, 0xff, 0xcc, 0x1c,
-	0xf3, 0xf9, 0x5f, 0x8e, 0xf1, 0x65, 0x6d, 0xbe, 0xfb, 0x4f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x40,
-	0x26, 0xb0, 0xf0, 0x14, 0x07, 0x00, 0x00,
+	// 798 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x54, 0x41, 0x6f, 0xdb, 0x36,
+	0x18, 0x95, 0x2c, 0xdb, 0xb5, 0xe9, 0x76, 0xf1, 0xb8, 0x6e, 0x13, 0x02, 0x4c, 0xf2, 0xbc, 0x8b,
+	0x0b, 0x6c, 0xd2, 0xd2, 0xc3, 0x90, 0xf5, 0x30, 0x20, 0x6a, 0x53, 0x20, 0x68, 0x93, 0x16, 0x4c,
+	0x9b, 0xc3, 0x76, 0x19, 0x2d, 0x73, 0x0e, 0x67, 0x8b, 0xd4, 0x44, 0xca, 0x98, 0x6f, 0x3d, 0x0e,
+	0x03, 0x06, 0xf4, 0x34, 0xec, 0xd8, 0x00, 0xfb, 0x27, 0xbb, 0xe4, 0xd8, 0x63, 0x0f, 0x83, 0xb1,
+	0x68, 0xc0, 0x7e, 0xc3, 0xd0, 0xd3, 0x20, 0x8a, 0x8a, 0x65, 0xb7, 0x3b, 0x24, 0xc8, 0x89, 0xe4,
+	0xf7, 0x3e, 0xbe, 0xf7, 0x44, 0xf1, 0x11, 0xdc, 0x1f, 0x53, 0x79, 0x9c, 0x0e, 0xbd, 0x90, 0x47,
+	0x7e, 0x48, 0xa7, 0x34, 0x3d, 0x1f, 0xe2, 0xc9, 0xd8, 0x9f, 0x6c, 0x0b, 0x5f, 0x4c, 0x69, 0xa4,
+	0x26, 0x38, 0xa6, 0xc2, 0x8f, 0x88, 0xc4, 0xfe, 0x6c, 0xcb, 0x1f, 0x13, 0x46, 0x12, 0x2c, 0xc9,
+	0xc8, 0x8b, 0x13, 0x2e, 0x39, 0xfc, 0x62, 0xc9, 0xe3, 0x15, 0x04, 0xe5, 0x10, 0x4f, 0xc6, 0xde,
+	0x64, 0x5b, 0x78, 0x39, 0x8f, 0x9a, 0xe4, 0x3c, 0x5e, 0xce, 0xe3, 0xcd, 0xb6, 0x36, 0x3f, 0xab,
+	0xe8, 0x8f, 0xf9, 0x98, 0xfb, 0x8a, 0x6e, 0x98, 0x7e, 0xa7, 0x56, 0x6a, 0xa1, 0x66, 0x85, 0x4c,
+	0xff, 0xdf, 0x1a, 0xb8, 0xf1, 0x10, 0x0f, 0xc9, 0xf4, 0x90, 0x4c, 0x49, 0x28, 0x79, 0x02, 0x7f,
+	0x31, 0x41, 0x27, 0xc2, 0x32, 0x3c, 0x56, 0x65, 0x61, 0x9b, 0x3d, 0x6b, 0xd0, 0xb9, 0x7d, 0xe4,
+	0x5d, 0xce, 0x8f, 0xb7, 0x42, 0xee, 0xed, 0x2f, 0x89, 0x77, 0x99, 0x4c, 0xe6, 0xc1, 0x7b, 0xa7,
+	0x0b, 0xd7, 0xc8, 0x16, 0x6e, 0xa7, 0x82, 0xa0, 0xaa, 0x3e, 0xfc, 0xd5, 0x04, 0x5d, 0xb5, 0xde,
+	0xfd, 0x31, 0x4e, 0x88, 0x10, 0x94, 0x33, 0x61, 0xd7, 0x94, 0xa9, 0xc7, 0x57, 0x62, 0x0a, 0x91,
+	0x1f, 0x52, 0x9a, 0x90, 0x88, 0x30, 0x19, 0xd8, 0xda, 0x4e, 0x77, 0x7f, 0x4d, 0x11, 0xbd, 0xe1,
+	0x61, 0xf3, 0x2b, 0xd0, 0x5d, 0xff, 0x1c, 0xd8, 0x05, 0xd6, 0x84, 0xcc, 0x6d, 0xb3, 0x67, 0x0e,
+	0xda, 0x28, 0x9f, 0xc2, 0x9b, 0xa0, 0x31, 0xc3, 0xd3, 0x94, 0xd8, 0x35, 0x55, 0x2b, 0x16, 0x77,
+	0x6a, 0xdb, 0x66, 0xff, 0x77, 0x13, 0xd8, 0xff, 0x67, 0x04, 0x7e, 0x54, 0x21, 0x0a, 0x3a, 0xda,
+	0x95, 0xf5, 0x80, 0xcc, 0x0b, 0xd6, 0x5d, 0xd0, 0xe2, 0x71, 0x7e, 0x5f, 0x78, 0x52, 0x10, 0x07,
+	0xb7, 0x74, 0x4f, 0xeb, 0x91, 0xae, 0xbf, 0x5e, 0xb8, 0xef, 0xaf, 0xd0, 0x97, 0x00, 0x3a, 0xdf,
+	0x0a, 0xfb, 0xa0, 0xa9, 0xfc, 0x08, 0xdb, 0xea, 0x59, 0x83, 0x76, 0x00, 0xb2, 0x85, 0xdb, 0x3c,
+	0x52, 0x15, 0xa4, 0x91, 0xfe, 0x1f, 0x26, 0x68, 0x3d, 0xa4, 0x42, 0xee, 0x13, 0x89, 0xe1, 0x0e,
+	0xd8, 0x48, 0x88, 0xe0, 0x69, 0x12, 0x92, 0x23, 0x92, 0xe4, 0xe7, 0xa0, 0xe5, 0x3f, 0xd4, 0xf2,
+	0x1b, 0x68, 0x15, 0x46, 0xeb, 0xfd, 0xf0, 0x53, 0xd0, 0x0a, 0x39, 0x93, 0x94, 0xa5, 0xc4, 0xb6,
+	0xd4, 0xde, 0x6e, 0x69, 0xfd, 0xae, 0xae, 0xa3, 0xf3, 0x0e, 0x78, 0x1f, 0xc0, 0x84, 0x44, 0x98,
+	0x32, 0xca, 0xc6, 0x7b, 0x92, 0x44, 0x77, 0x79, 0xca, 0xa4, 0x5d, 0xef, 0x99, 0x03, 0x2b, 0xf8,
+	0x20, 0x5b, 0xb8, 0x10, 0xbd, 0x81, 0xa2, 0xb7, 0xec, 0xe8, 0xff, 0x53, 0x07, 0xe0, 0xd1, 0xf0,
+	0x7b, 0x12, 0x16, 0xdf, 0xd1, 0x03, 0x75, 0x86, 0x23, 0xa2, 0xcf, 0xf7, 0xba, 0x36, 0x50, 0x3f,
+	0xc0, 0x11, 0x41, 0x0a, 0x81, 0x3e, 0x68, 0xe7, 0xa3, 0x88, 0x71, 0x58, 0xfa, 0x7c, 0x57, 0xb7,
+	0xb5, 0x0f, 0x4a, 0x00, 0x2d, 0x7b, 0x60, 0x00, 0xac, 0x94, 0x8e, 0xec, 0x86, 0x6a, 0xfd, 0xbc,
+	0xfc, 0x63, 0x4f, 0xf7, 0xee, 0xbd, 0x5e, 0xb8, 0x1f, 0xe7, 0x37, 0x90, 0xf2, 0x3c, 0xf1, 0x11,
+	0x0e, 0x8f, 0x29, 0x23, 0xc9, 0x5c, 0xbd, 0x07, 0x72, 0x1e, 0x13, 0xe1, 0x3d, 0xdd, 0xbb, 0x87,
+	0xf2, 0xcd, 0x6f, 0x3b, 0xde, 0xe6, 0x05, 0x8f, 0x77, 0x06, 0x9a, 0xd3, 0x22, 0xb8, 0x1d, 0x95,
+	0x91, 0x83, 0xcb, 0x66, 0x64, 0x79, 0x5a, 0x5e, 0x35, 0xb0, 0xef, 0x68, 0x27, 0x4d, 0x9d, 0x55,
+	0xad, 0x06, 0x7f, 0x36, 0x41, 0x07, 0x33, 0xc6, 0x25, 0x96, 0x2a, 0xa1, 0xd7, 0x95, 0xfa, 0xe1,
+	0x15, 0xa8, 0xef, 0x2c, 0x59, 0xd7, 0xde, 0x8c, 0x0a, 0x82, 0xaa, 0xe2, 0x9b, 0x5f, 0x82, 0xce,
+	0x25, 0x53, 0x99, 0xa7, 0x7a, 0x5d, 0xf0, 0x42, 0xa9, 0x9e, 0x82, 0xfa, 0x13, 0x1a, 0x11, 0x78,
+	0x0b, 0x5c, 0x13, 0x24, 0xe4, 0x6c, 0x24, 0xd4, 0x3e, 0x2b, 0xd8, 0xd0, 0xae, 0xaf, 0x1d, 0x16,
+	0x65, 0x54, 0xe2, 0xf0, 0x13, 0xd0, 0x60, 0x98, 0x71, 0xa1, 0xc8, 0x1a, 0xc1, 0x0d, 0xdd, 0xd8,
+	0x38, 0xc8, 0x8b, 0xa8, 0xc0, 0xee, 0xdc, 0xfc, 0xed, 0x85, 0x6b, 0xfc, 0x74, 0xe2, 0x1a, 0xcf,
+	0x4f, 0x5c, 0xe3, 0xc5, 0x89, 0x6b, 0x3c, 0xfb, 0xb3, 0x67, 0xf4, 0xbf, 0x01, 0xed, 0x5c, 0x4d,
+	0x48, 0x1c, 0xc5, 0x57, 0x2d, 0xd9, 0xff, 0x16, 0xb4, 0x9e, 0xcc, 0x63, 0x52, 0x06, 0x66, 0x42,
+	0xd9, 0x68, 0x3d, 0x30, 0x0f, 0x28, 0x1b, 0x21, 0x85, 0xc0, 0xdb, 0x00, 0xe0, 0x98, 0xae, 0xbe,
+	0x0a, 0x50, 0xf7, 0x81, 0x9d, 0xc7, 0x7b, 0xe5, 0x8d, 0xad, 0x74, 0x05, 0x83, 0xd3, 0x33, 0xc7,
+	0x78, 0x79, 0xe6, 0x18, 0xaf, 0xce, 0x1c, 0xe3, 0x59, 0xe6, 0x98, 0xa7, 0x99, 0x63, 0xbe, 0xcc,
+	0x1c, 0xf3, 0x55, 0xe6, 0x98, 0x7f, 0x65, 0x8e, 0xf9, 0xfc, 0x6f, 0xc7, 0xf8, 0xba, 0x36, 0xdb,
+	0xfa, 0x2f, 0x00, 0x00, 0xff, 0xff, 0x53, 0xd4, 0x8b, 0xf2, 0x57, 0x07, 0x00, 0x00,
 }
 
 func (m *LabelSelector) Marshal() (dAtA []byte, err error) {
@@ -513,6 +513,11 @@ func (m *ObjectMeta) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 			dAtA[i] = 0x5a
 		}
 	}
+	i -= len(m.ResourceVersion)
+	copy(dAtA[i:], m.ResourceVersion)
+	i = encodeVarintGenerated(dAtA, i, uint64(len(m.ResourceVersion)))
+	i--
+	dAtA[i] = 0x32
 	i -= len(m.UID)
 	copy(dAtA[i:], m.UID)
 	i = encodeVarintGenerated(dAtA, i, uint64(len(m.UID)))
@@ -674,6 +679,8 @@ func (m *ObjectMeta) Size() (n int) {
 	n += 1 + l + sovGenerated(uint64(l))
 	l = len(m.UID)
 	n += 1 + l + sovGenerated(uint64(l))
+	l = len(m.ResourceVersion)
+	n += 1 + l + sovGenerated(uint64(l))
 	if len(m.Labels) > 0 {
 		for k, v := range m.Labels {
 			_ = k
@@ -801,6 +808,7 @@ func (this *ObjectMeta) String() string {
 		`Name:` + fmt.Sprintf("%v", this.Name) + `,`,
 		`Namespace:` + fmt.Sprintf("%v", this.Namespace) + `,`,
 		`UID:` + fmt.Sprintf("%v", this.UID) + `,`,
+		`ResourceVersion:` + fmt.Sprintf("%v", this.ResourceVersion) + `,`,
 		`Labels:` + mapStringForLabels + `,`,
 		`Annotations:` + mapStringForAnnotations + `,`,
 		`}`,
@@ -1461,6 +1469,38 @@ func (m *ObjectMeta) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.UID = k8s_io_apimachinery_pkg_types.UID(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 6:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field ResourceVersion", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenerated
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthGenerated
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenerated
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.ResourceVersion = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 11:
 			if wireType != 2 {

pkg/k8s/slim/k8s/apis/meta/v1/generated.proto
@@ -124,6 +124,20 @@ message ObjectMeta {
   // +optional
   optional string uid = 5;
 
+  // An opaque value that represents the internal version of this object that can
+  // be used by clients to determine when objects have changed. May be used for optimistic
+  // concurrency, change detection, and the watch operation on a resource or set of resources.
+  // Clients must treat these values as opaque and passed unmodified back to the server.
+  // They may only be valid for a particular resource or set of resources.
+  //
+  // Populated by the system.
+  // Read-only.
+  // Value must be treated as opaque by clients and .
+  // More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#concurrency-control-and-consistency
+  // +optional
+  // +deepequal-gen=false
+  optional string resourceVersion = 6;
+
   // Map of string keys and values that can be used to organize and categorize
   // (scope and select) objects. May match selectors of replication controllers
   // and services.

pkg/k8s/slim/k8s/apis/meta/v1/meta.go
@@ -56,8 +56,8 @@ func (meta *ObjectMeta) GetGenerateName() string           { return "" }
 func (meta *ObjectMeta) SetGenerateName(string)            {}
 func (meta *ObjectMeta) GetUID() types.UID                 { return meta.UID }
 func (meta *ObjectMeta) SetUID(uid types.UID)              { meta.UID = uid }
-func (meta *ObjectMeta) GetResourceVersion() string        { return "" }
-func (meta *ObjectMeta) SetResourceVersion(_ string)       {}
+func (meta *ObjectMeta) GetResourceVersion() string        { return meta.ResourceVersion }
+func (meta *ObjectMeta) SetResourceVersion(ver string)     { meta.ResourceVersion = ver }
 func (meta *ObjectMeta) GetGeneration() int64              { return 0 }
 func (meta *ObjectMeta) SetGeneration(_ int64)             {}
 func (meta *ObjectMeta) GetSelfLink() string               { return "" }

pkg/k8s/slim/k8s/apis/meta/v1/types.go
@@ -117,6 +117,20 @@ type ObjectMeta struct {
 	// +optional
 	UID types.UID `json:"uid,omitempty" protobuf:"bytes,5,opt,name=uid,casttype=k8s.io/kubernetes/pkg/types.UID"`
 
+	// An opaque value that represents the internal version of this object that can
+	// be used by clients to determine when objects have changed. May be used for optimistic
+	// concurrency, change detection, and the watch operation on a resource or set of resources.
+	// Clients must treat these values as opaque and passed unmodified back to the server.
+	// They may only be valid for a particular resource or set of resources.
+	//
+	// Populated by the system.
+	// Read-only.
+	// Value must be treated as opaque by clients and .
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#concurrency-control-and-consistency
+	// +optional
+	// +deepequal-gen=false
+	ResourceVersion string `json:"resourceVersion,omitempty" protobuf:"bytes,6,opt,name=resourceVersion"`
+
 	// Map of string keys and values that can be used to organize and categorize
 	// (scope and select) objects. May match selectors of replication controllers
 	// and services.