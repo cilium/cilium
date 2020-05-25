a5fc85a0fae9ca1a66b20c6957d83c7315d3388d
Daniel Borkmann <daniel@iogearbox.net>
2020-05-25 21:18:01 +0200

    bpf: signal agent on CT map update error that CT map is full
    [ upstream commit fc5a3bd73f39012cedb99cf4b32461d45eeca812 ]
    
    There are users on 4.9 kernels which are suffering connectivity loss since
    the CT map is full and GC doesn't trigger yet. We can help improving the
    situation with the same framework we set in place for NAT when under stress.
    Upon insertion error, send a signal to the agent in order to trigger GC so
    that it can free up old entries.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- bpf/lib/conntrack.h
+++ bpf/lib/conntrack.h
@@ -27,6 +27,7 @@
 #include "dbg.h"
 #include "l4.h"
 #include "nat46.h"
+#include "signal.h"
 
 #ifdef CONNTRACK
 enum {
@@ -686,8 +687,10 @@ static __always_inline int ct_create6(const void *map_main, const void *map_rela
 	cilium_dbg3(skb, DBG_CT_CREATED6, entry.rev_nat_index, ct_state->src_sec_id, 0);
 
 	entry.src_sec_id = ct_state->src_sec_id;
-	if (map_update_elem(map_main, tuple, &entry, 0) < 0)
+	if (map_update_elem(map_main, tuple, &entry, 0) < 0) {
+		send_signal_ct_fill_up(skb, SIGNAL_PROTO_V6);
 		return DROP_CT_CREATE_FAILED;
+	}
 
 	if (map_related != NULL) {
 		/* Create an ICMPv6 entry to relate errors */
@@ -703,8 +706,10 @@ static __always_inline int ct_create6(const void *map_main, const void *map_rela
 		ipv6_addr_copy(&icmp_tuple.daddr, &tuple->daddr);
 		ipv6_addr_copy(&icmp_tuple.saddr, &tuple->saddr);
 
-		if (map_update_elem(map_related, &icmp_tuple, &entry, 0) < 0)
+		if (map_update_elem(map_related, &icmp_tuple, &entry, 0) < 0) {
+			send_signal_ct_fill_up(skb, SIGNAL_PROTO_V6);
 			return DROP_CT_CREATE_FAILED;
+		}
 	}
 	return 0;
 }
@@ -791,8 +796,10 @@ static __always_inline int ct_create4(const void *map_main, const void *map_rela
 	cilium_dbg3(skb, DBG_CT_CREATED4, entry.rev_nat_index, ct_state->src_sec_id, ct_state->addr);
 
 	entry.src_sec_id = ct_state->src_sec_id;
-	if (map_update_elem(map_main, tuple, &entry, 0) < 0)
+	if (map_update_elem(map_main, tuple, &entry, 0) < 0) {
+		send_signal_ct_fill_up(skb, SIGNAL_PROTO_V4);
 		return DROP_CT_CREATE_FAILED;
+	}
 
 	if (ct_state->addr && ct_state->loopback) {
 		__u8 flags = tuple->flags;
@@ -814,8 +821,11 @@ static __always_inline int ct_create4(const void *map_main, const void *map_rela
 			tuple->daddr = ct_state->addr;
 		}
 
-		if (map_update_elem(map_main, tuple, &entry, 0) < 0)
+		if (map_update_elem(map_main, tuple, &entry, 0) < 0) {
+			send_signal_ct_fill_up(skb, SIGNAL_PROTO_V4);
 			return DROP_CT_CREATE_FAILED;
+		}
+
 		tuple->saddr = saddr;
 		tuple->daddr = daddr;
 		tuple->flags = flags;
@@ -837,8 +847,10 @@ static __always_inline int ct_create4(const void *map_main, const void *map_rela
 		 * the below throws an error, but we might as well just let
 		 * it time out.
 		 */
-		if (map_update_elem(map_related, &icmp_tuple, &entry, 0) < 0)
+		if (map_update_elem(map_related, &icmp_tuple, &entry, 0) < 0) {
+			send_signal_ct_fill_up(skb, SIGNAL_PROTO_V4);
 			return DROP_CT_CREATE_FAILED;
+		}
 	}
 	return 0;
 }
