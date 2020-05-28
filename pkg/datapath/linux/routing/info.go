cilium/cilium

commit b58dd147ed445628ef576e03bd3b76303709acdd

Refactor enirouting pkg to use interfaces
This commit structures the code in such a way that allows the previously
duplicated parsing/validation logic to be consolidated into one place. It also
reduces the interdependencies between packages and allows for a dummy
implementation to be used for testing purposes.

Signed-off-by: Chris Tarazi <chris@isovalent.com>

plugins/cilium-cni/interface.go
@@ -21,7 +21,6 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	enirouting "github.com/cilium/cilium/pkg/aws/eni/routing"
 	"github.com/cilium/cilium/pkg/ip"
-	"github.com/cilium/cilium/pkg/mac"
 
 	"github.com/containernetworking/cni/pkg/types/current"
 )
@@ -42,25 +41,14 @@ func interfaceAdd(ipConfig *current.IPConfig, ipam *models.IPAMAddressResponse, 
 		cidrs = append(cidrs, *cidr)
 	}
 
-	if ipam.MasterMac == "" {
-		return fmt.Errorf("ENI master interface MAC address is not set")
-	}
-
-	mac, err := mac.ParseMAC(ipam.MasterMac)
+	routingInfo, err := enirouting.NewRoutingInfo(ipam.Gateway, ipam.Cidrs, ipam.MasterMac)
 	if err != nil {
-		return fmt.Errorf("unable to parse master interface MAC address %s", ipam.MasterMac)
+		return fmt.Errorf("unable to parse routing info: %v", err)
 	}
 
-	gatewayIP := net.ParseIP(ipam.Gateway)
-	if gatewayIP == nil {
-		return fmt.Errorf("unable to parse gateway IP %s", ipam.Gateway)
-	}
-
-	if err := enirouting.Install(ipConfig.Address.IP, &enirouting.RoutingInfo{
-		IPv4Gateway: gatewayIP,
-		IPv4CIDRs:   cidrs,
-		MasterIfMAC: mac,
-	}, int(conf.DeviceMTU), conf.Masquerade); err != nil {
+	if err := routingInfo.Configure(ipConfig.Address.IP,
+		int(conf.DeviceMTU),
+		conf.Masquerade); err != nil {
 		return fmt.Errorf("unable to install ip rules and routes: %s", err)
 	}
 

pkg/aws/eni/routing/info.go
@@ -1,0 +1,83 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package enirouting
+
+import (
+	"errors"
+	"fmt"
+	"net"
+
+	"github.com/cilium/cilium/pkg/mac"
+)
+
+// RoutingInfo represents information that's required to enable
+// connectivity via the local rule and route tables while in ENI mode. The
+// information in this struct is used to create rules and routes which direct
+// traffic out of the ENI devices (egress).
+//
+// This struct is mostly derived from the `ipam.AllocationResult` as the
+// information comes from IPAM.
+type RoutingInfo struct {
+	// IPv4Gateway is the gateway where outbound/egress traffic is directed.
+	IPv4Gateway net.IP
+
+	// IPv4CIDRs is a list of CIDRs which the ENI device has access to. In most
+	// cases, it'll at least contain the CIDR of the IPv4Gateway IP address.
+	IPv4CIDRs []net.IPNet
+
+	// MasterIfMAC is the MAC address of the master interface that egress
+	// traffic is directed to. This is the MAC of the ENI itself which
+	// corresponds to the IPv4Gateway IP addr.
+	MasterIfMAC mac.MAC
+}
+
+// NewRoutingInfo creates a new RoutingInfo struct, from data that will be
+// parsed and validated. Note, this code assumes IPv4 values because ENI + IPv4
+// is the only supported path currently.
+func NewRoutingInfo(gateway string, cidrs []string, mac string) (*RoutingInfo, error) {
+	return parse(gateway, cidrs, mac)
+}
+
+func parse(gateway string, cidrs []string, macAddr string) (*RoutingInfo, error) {
+	ip := net.ParseIP(gateway)
+	if ip == nil {
+		return nil, fmt.Errorf("invalid ip: %s", gateway)
+	}
+
+	if cidrs == nil || len(cidrs) == 0 {
+		return nil, errors.New("empty cidrs")
+	}
+
+	parsedCIDRs := make([]net.IPNet, 0, len(cidrs))
+	for _, cidr := range cidrs {
+		_, c, err := net.ParseCIDR(cidr)
+		if err != nil {
+			return nil, fmt.Errorf("invalid cidr: %s", cidr)
+		}
+
+		parsedCIDRs = append(parsedCIDRs, *c)
+	}
+
+	parsedMAC, err := mac.ParseMAC(macAddr)
+	if err != nil {
+		return nil, fmt.Errorf("invalid mac: %s", macAddr)
+	}
+
+	return &RoutingInfo{
+		IPv4Gateway: ip,
+		IPv4CIDRs:   parsedCIDRs,
+		MasterIfMAC: parsedMAC,
+	}, nil
+}

cilium-health/launch/endpoint.go
@@ -25,7 +25,6 @@ import (
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
-	enirouting "github.com/cilium/cilium/pkg/aws/eni/routing"
 	"github.com/cilium/cilium/pkg/datapath/connector"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/defaults"
@@ -242,7 +241,7 @@ func LaunchAsEndpoint(baseCtx context.Context,
 	epMgr EndpointAdder,
 	proxy endpoint.EndpointProxy,
 	allocator cache.IdentityAllocator,
-	eniHealthRouting *enirouting.RoutingInfo) (*Client, error) {
+	routingConfig routingConfigurer) (*Client, error) {
 
 	var (
 		cmd  = launcher.Launcher{}
@@ -345,8 +344,7 @@ func LaunchAsEndpoint(baseCtx context.Context,
 	}
 
 	if option.Config.IPAM == option.IPAMENI {
-		if err := enirouting.Install(healthIP,
-			eniHealthRouting,
+		if err := routingConfig.Configure(healthIP,
 			mtuConfig.GetDeviceMTU(),
 			option.Config.Masquerade); err != nil {
 
@@ -372,4 +370,8 @@ func LaunchAsEndpoint(baseCtx context.Context,
 	metrics.SubprocessStart.WithLabelValues(ciliumHealth).Inc()
 
 	return client, nil
+}
+
+type routingConfigurer interface {
+	Configure(ip net.IP, mtu int, masq bool) error
 }

daemon/cmd/ipam.go
@@ -15,7 +15,6 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
 	"net"
 	"strings"
@@ -30,7 +29,6 @@ import (
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/mac"
 	"github.com/cilium/cilium/pkg/node"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
@@ -185,7 +183,7 @@ func (d *Daemon) allocateDatapathIPs(family datapath.NodeAddressingFamily) (rout
 	if routerIP != nil {
 		err = d.ipam.AllocateIPWithoutSyncUpstream(routerIP, "router")
 		if err != nil {
-			log.Warningf("Router IP could not be re-allocated. Need to re-allocate. This will cause brief network disruption")
+			log.Warn("Router IP could not be re-allocated. Need to re-allocate. This will cause brief network disruption")
 
 			// The restored router IP is not part of the allocation range.
 			// This indicates that the allocation range has changed.
@@ -229,7 +227,7 @@ func (d *Daemon) allocateHealthIPs() error {
 			// endpoint traffic out of the ENIs.
 			if option.Config.IPAM == option.IPAMENI {
 				if err := d.parseHealthEndpointInfo(result); err != nil {
-					log.Warningf("Unable to allocate health information for ENI: %s", err)
+					log.WithError(err).Warn("Unable to allocate health information for ENI")
 				}
 			}
 		}
@@ -368,39 +366,9 @@ func (d *Daemon) bootstrapIPAM() {
 }
 
 func (d *Daemon) parseHealthEndpointInfo(result *ipam.AllocationResult) error {
-	if d.healthEndpointRouting == nil {
-		d.healthEndpointRouting = &enirouting.RoutingInfo{}
-	}
-
-	if ip := net.ParseIP(result.GatewayIP); ip != nil {
-		log.Debugf("IPv4 health gateway address: %s", ip)
-
-		d.healthEndpointRouting.IPv4Gateway = ip
-	} else {
-		return errors.New("IPv4 health gateway address could not be parsed")
-	}
-
-	if len(result.CIDRs) == 0 {
-		return errors.New("IPv4 health CIDR missing")
-	}
-
-	for _, c := range result.CIDRs {
-		if _, cidr, err := net.ParseCIDR(c); cidr != nil && err == nil {
-			d.healthEndpointRouting.IPv4CIDRs = append(d.healthEndpointRouting.IPv4CIDRs, *cidr)
-		} else {
-			return fmt.Errorf("IPv4 health CIDR could not be parsed: %v", err)
-		}
-	}
-
-	log.Debugf("IPv4 health CIDRs: %s", d.healthEndpointRouting.IPv4CIDRs)
-
-	if mac, err := mac.ParseMAC(result.Master); mac != nil && err == nil {
-		log.Debugf("Health master interface MAC: %s", mac)
-
-		d.healthEndpointRouting.MasterIfMAC = mac
-	} else {
-		return fmt.Errorf("health master interface MAC could not be parsed: %v", err)
-	}
-
-	return nil
+	var err error
+	d.healthEndpointRouting, err = enirouting.NewRoutingInfo(result.GatewayIP,
+		result.CIDRs,
+		result.Master)
+	return err
 }

pkg/aws/eni/routing/routing.go
@@ -33,36 +33,15 @@ var (
 	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "eni-routing")
 )
 
-// RoutingInfo represents information that's required to enable
-// connectivity via the local rule and route tables while in ENI mode. The
-// information in this struct is used to create rules and routes which direct
-// traffic out of the ENI devices (egress).
-//
-// This struct is mostly derived from the `ipam.AllocationResult` as the
-// information comes from IPAM.
-type RoutingInfo struct {
-	// IPv4Gateway is the gateway where outbound/egress traffic is directed.
-	IPv4Gateway net.IP
-
-	// IPv4CIDRs is a list of CIDRs which the ENI device has access to. In most
-	// cases, it'll at least contain the CIDR of the IPv4Gateway IP address.
-	IPv4CIDRs []net.IPNet
-
-	// MasterIfMAC is the MAC address of the master interface that egress
-	// traffic is directed to. This is the MAC of the ENI itself which
-	// corresponds to the IPv4Gateway IP addr.
-	MasterIfMAC mac.MAC
-}
-
-// Install sets up the rules and routes needed when running in ENI mode. These
-// rules and routes direct egress traffic out of the ENI device and ingress
-// traffic back to the endpoint (`ip`).
+// Configure sets up the rules and routes needed when running in ENI mode.
+// These rules and routes direct egress traffic out of the ENI device and
+// ingress traffic back to the endpoint (`ip`).
 //
 // ip: The endpoint IP address to direct traffic out / from ENI device.
 // info: The ENI device routing info used to create rules and routes.
 // mtu: The ENI device MTU.
 // masq: Whether masquerading is enabled.
-func Install(ip net.IP, info *RoutingInfo, mtu int, masq bool) error {
+func (info *RoutingInfo) Configure(ip net.IP, mtu int, masq bool) error {
 	ifindex, err := retrieveIfIndexFromMAC(info.MasterIfMAC, mtu)
 	if err != nil {
 		return fmt.Errorf("unable to find ifindex for interface MAC: %s", err)

commit 32773a727a1d3c5799883b818b0bc6ee4c5d0a2f

fix(lint): Use coalesce CIDRs  for linux routing in cni
Currently, the coalesce cidrs was derived from ipam.CIDRs with the intention
of minimizing routing rules, however original full ipam.cidrs are passed
to routing rules instead.

This PR is to correct such discrepancy

Closes #11562

Fixes: b58dd147ed44 ("Refactor enirouting pkg to use interfaces")

Signed-off-by: Tam Mach <sayboras@yahoo.com>

pkg/datapath/linux/routing/info.go
@@ -56,7 +56,7 @@ func parse(gateway string, cidrs []string, macAddr string) (*RoutingInfo, error)
 		return nil, fmt.Errorf("invalid ip: %s", gateway)
 	}
 
-	if cidrs == nil || len(cidrs) == 0 {
+	if len(cidrs) == 0 {
 		return nil, errors.New("empty cidrs")
 	}
 

plugins/cilium-cni/interface.go
@@ -36,12 +36,12 @@ func interfaceAdd(ipConfig *current.IPConfig, ipam *models.IPAMAddressResponse, 
 	}
 	// Coalesce CIDRs into minimum set needed for route rules
 	ipv4CIDRs, _ := ip.CoalesceCIDRs(allCIDRs)
-	cidrs := make([]net.IPNet, 0, len(ipv4CIDRs))
+	cidrs := make([]string, 0, len(ipv4CIDRs))
 	for _, cidr := range ipv4CIDRs {
-		cidrs = append(cidrs, *cidr)
+		cidrs = append(cidrs, cidr.String())
 	}
 
-	routingInfo, err := linuxrouting.NewRoutingInfo(ipam.Gateway, ipam.Cidrs, ipam.MasterMac)
+	routingInfo, err := linuxrouting.NewRoutingInfo(ipam.Gateway, cidrs, ipam.MasterMac)
 	if err != nil {
 		return fmt.Errorf("unable to parse routing info: %v", err)
 	}