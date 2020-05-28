cilium/cilium

commit 056fec2b6900ac417e7b08d0ee7eaf46bf0ed689

loader: Move direct routing config. to node_config.h
DIRECT_ROUTING_DEV_IFINDEX, IPV4_DIRECT_ROUTING, and IPV6_DIRECT_ROUTING
are common across all devices, so there's no need to use static data or
BPF_V6() for them.

Fixes: 275856b ("bpf: Macros to load IPV6_{NODEPORT,DIRECT_ROUTING}")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/config/config.go
@@ -20,6 +20,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net"
 	"reflect"
 	"sort"
 
@@ -28,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/datapath/iptables"
+	"github.com/cilium/cilium/pkg/datapath/link"
 	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/identity"
@@ -261,6 +263,38 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		cDefinesMap["NODEPORT_PORT_MAX_NAT"] = "65535"
 	}
 
+	if len(option.Config.Devices) > 0 {
+		// First device from the list is used for direct routing between nodes
+		directRoutingIface := option.Config.Devices[0]
+		directRoutingIfIndex, err := link.GetIfIndex(directRoutingIface)
+		if err != nil {
+			return err
+		}
+		cDefinesMap["DIRECT_ROUTING_DEV_IFINDEX"] = fmt.Sprintf("%d", directRoutingIfIndex)
+
+		if option.Config.EnableIPv4 {
+			nodePortIPv4Addrs := node.GetNodePortIPv4AddrsWithDevices()
+			ipv4 := byteorder.HostSliceToNetwork(nodePortIPv4Addrs[directRoutingIface], reflect.Uint32).(uint32)
+			cDefinesMap["IPV4_DIRECT_ROUTING"] = fmt.Sprintf("%d", ipv4)
+		}
+
+		if option.Config.EnableIPv6 {
+			directRoutingIPv6 := node.GetNodePortIPv6AddrsWithDevices()[directRoutingIface]
+			extraMacrosMap["IPV6_DIRECT_ROUTING"] = directRoutingIPv6.String()
+			fw.WriteString(FmtDefineAddress("IPV6_DIRECT_ROUTING", directRoutingIPv6))
+		}
+	} else {
+		var directRoutingIPv6 net.IP
+		cDefinesMap["DIRECT_ROUTING_DEV_IFINDEX"] = "0"
+		if option.Config.EnableIPv4 {
+			cDefinesMap["IPV4_DIRECT_ROUTING"] = "0"
+		}
+		if option.Config.EnableIPv6 {
+			extraMacrosMap["IPV6_DIRECT_ROUTING"] = directRoutingIPv6.String()
+			fw.WriteString(FmtDefineAddress("IPV6_DIRECT_ROUTING", directRoutingIPv6))
+		}
+	}
+
 	if option.Config.EnableHostFirewall {
 		cDefinesMap["ENABLE_HOST_FIREWALL"] = "1"
 	}
@@ -404,16 +438,13 @@ func (h *HeaderfileWriter) writeStaticData(fw io.Writer, e datapath.EndpointConf
 			// these values with zero for the host device and with the actual
 			// values for the native devices.
 			fmt.Fprint(fw, "/* Fake values, replaced by 0 for host device and by actual values for native devices. */\n")
-			fmt.Fprint(fw, defineUint32("DIRECT_ROUTING_DEV_IFINDEX", 1))
 			fmt.Fprint(fw, defineUint32("NATIVE_DEV_IFINDEX", 1))
 			if option.Config.EnableIPv6 {
 				placeholderIPv6 := []byte{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-				fmt.Fprint(fw, defineIPv6("IPV6_DIRECT_ROUTING", placeholderIPv6))
 				fmt.Fprint(fw, defineIPv6("IPV6_NODEPORT", placeholderIPv6))
 			}
 			if option.Config.EnableIPv4 {
 				placeholderIPv4 := []byte{1, 1, 1, 1}
-				fmt.Fprint(fw, defineIPv4("IPV4_DIRECT_ROUTING", placeholderIPv4))
 				fmt.Fprint(fw, defineIPv4("IPV4_NODEPORT", placeholderIPv4))
 			}
 			fmt.Fprint(fw, "\n")

bpf/init.sh
@@ -564,21 +564,6 @@ if [ "$MODE" = "direct" ] || [ "$MODE" = "ipvlan" ] || [ "$MODE" = "routed" ] ||
 			done
 		fi
 
-		NP_COPTS=""
-
-		if [ "$NODE_PORT" = "true" ] ; then
-			# First device from the list is used for direct routing between nodes
-			DIRECT_ROUTING_DEV=$(echo "${NATIVE_DEVS}" | cut -d\; -f1)
-			DIRECT_ROUTING_DEV_IDX=$(cat /sys/class/net/${DIRECT_ROUTING_DEV}/ifindex)
-			NP_COPTS="${NP_COPTS} -DDIRECT_ROUTING_DEV_IFINDEX=${DIRECT_ROUTING_DEV_IDX}"
-			if [ "$IP4_HOST" != "<nil>" ]; then
-				NP_COPTS="${NP_COPTS} -DIPV4_DIRECT_ROUTING=${v4_addrs[$DIRECT_ROUTING_DEV]}"
-			fi
-			if [ "$IP6_HOST" != "<nil>" ]; then
-				NP_COPTS="${NP_COPTS} -DIPV6_DIRECT_ROUTING_VAL={.addr={${v6_addrs[$DIRECT_ROUTING_DEV]}}}"
-			fi
-		fi
-
 		echo "$NATIVE_DEVS" > $RUNDIR/device.state
 	fi
 else
@@ -696,7 +681,7 @@ if [ "$XDP_DEV" != "<nil>" ]; then
 	fi
 	if [ "$NODE_PORT" = "true" ]; then
 		NATIVE_DEV_IDX=$(cat /sys/class/net/${XDP_DEV}/ifindex)
-		COPTS="${COPTS} ${NP_COPTS} -DNATIVE_DEV_IFINDEX=${NATIVE_DEV_IDX}"
+		COPTS="${COPTS} -DNATIVE_DEV_IFINDEX=${NATIVE_DEV_IDX}"
 		# Currently it assumes that XDP_DEV is listed among NATIVE_DEVS
 		if [ "$IP4_HOST" != "<nil>" ]; then
 			COPTS="${COPTS} -DIPV4_NODEPORT=${v4_addrs[$XDP_DEV]}"

bpf/lib/nodeport.h
@@ -30,16 +30,6 @@
 DEFINE_IPV6(IPV6_NODEPORT, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x1, 0x0, 0x1, 0x0, 0x0);
 # define IPV6_NODEPORT_V
 #endif
-#ifndef DIRECT_ROUTING_DEV_IFINDEX
-#define DIRECT_ROUTING_DEV_IFINDEX 0
-#endif
-#if defined(ENABLE_IPV4) && !defined(IPV4_DIRECT_ROUTING)
-#define IPV4_DIRECT_ROUTING 0
-#endif
-#if defined(ENABLE_IPV6) && !defined(IPV6_DIRECT_ROUTING_V)
-DEFINE_IPV6(IPV6_DIRECT_ROUTING, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x1, 0x0, 0x1, 0x0, 0x0);
-# define IPV6_DIRECT_ROUTING_V
-#endif
 #endif /* ENABLE_NODEPORT */
 
 #ifdef IPV6_NODEPORT_VAL
@@ -50,16 +40,6 @@ DEFINE_IPV6(IPV6_DIRECT_ROUTING, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 
 	})
 #else
 # define BPF_V6_NODEPORT(dst)	BPF_V6(dst, IPV6_NODEPORT)
-#endif
-
-#ifdef IPV6_DIRECT_ROUTING_VAL
-# define BPF_V6_DIRECT_ROUTING(dst)				\
-	({						\
-		union v6addr tmp = IPV6_DIRECT_ROUTING_VAL;	\
-		dst = tmp;				\
-	})
-#else
-# define BPF_V6_DIRECT_ROUTING(dst)	BPF_V6(dst, IPV6_DIRECT_ROUTING)
 #endif
 
 static __always_inline __maybe_unused void
@@ -378,6 +358,7 @@ int tail_nodeport_nat_ipv6(struct __ctx_buff *ctx)
 {
 	int ifindex = DIRECT_ROUTING_DEV_IFINDEX;
 	int ret, dir = ctx_load_meta(ctx, CB_NAT);
+	union v6addr tmp = IPV6_DIRECT_ROUTING;
 	struct bpf_fib_lookup fib_params = {};
 	struct ipv6_nat_target target = {
 		.min_port = NODEPORT_PORT_MIN_NAT,
@@ -387,7 +368,7 @@ int tail_nodeport_nat_ipv6(struct __ctx_buff *ctx)
 	void *data, *data_end;
 	struct ipv6hdr *ip6;
 
-	BPF_V6_DIRECT_ROUTING(target.addr);
+	target.addr = tmp;
 #ifdef ENCAP_IFINDEX
 	if (dir == NAT_DIR_EGRESS) {
 		struct remote_endpoint_info *info;

bpf/node_config.h
@@ -127,6 +127,16 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 #endif
 #endif
 
+#ifdef ENABLE_NODEPORT
+# define DIRECT_ROUTING_DEV_IFINDEX 0
+# ifdef ENABLE_IPV4
+#  define IPV4_DIRECT_ROUTING 0
+# endif
+# ifdef ENABLE_IPV6
+#  define IPV6_DIRECT_ROUTING { .addr = { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 } }
+# endif
+#endif
+
 /* It appears that we can support around the below number of prefixes in an
  * unrolled loop for LPM CIDR handling in older kernels along with the rest of
  * the logic in the datapath, hence the defines below. This number was arrived

pkg/datapath/loader/loader.go
@@ -156,26 +156,12 @@ func patchHostNetdevDatapath(ep datapath.Endpoint, objPath, dstPath, ifName stri
 	}
 
 	if option.Config.EnableNodePort {
-		// First device from the list is used for direct routing between nodes
-		directRoutingIface := option.Config.Devices[0]
-		directRoutingIfIndex, err := link.GetIfIndex(directRoutingIface)
-		if err != nil {
-			return err
-		}
-		opts["DIRECT_ROUTING_DEV_IFINDEX"] = directRoutingIfIndex
 		opts["NATIVE_DEV_IFINDEX"] = ifIndex
 		if option.Config.EnableIPv4 {
-			ipv4 := nodePortIPv4Addrs[directRoutingIface]
-			opts["IPV4_DIRECT_ROUTING"] = byteorder.HostSliceToNetwork(ipv4, reflect.Uint32).(uint32)
-			ipv4 = nodePortIPv4Addrs[ifName]
+			ipv4 := nodePortIPv4Addrs[ifName]
 			opts["IPV4_NODEPORT"] = byteorder.HostSliceToNetwork(ipv4, reflect.Uint32).(uint32)
 		}
 		if option.Config.EnableIPv6 {
-			directRoutingIPv6 := nodePortIPv6Addrs[directRoutingIface]
-			opts["IPV6_DIRECT_ROUTING_1"] = sliceToBe32(directRoutingIPv6[0:4])
-			opts["IPV6_DIRECT_ROUTING_2"] = sliceToBe32(directRoutingIPv6[4:8])
-			opts["IPV6_DIRECT_ROUTING_3"] = sliceToBe32(directRoutingIPv6[8:12])
-			opts["IPV6_DIRECT_ROUTING_4"] = sliceToBe32(directRoutingIPv6[12:16])
 			nodePortIPv6 := nodePortIPv6Addrs[ifName]
 			opts["IPV6_NODEPORT_1"] = sliceToBe32(nodePortIPv6[0:4])
 			opts["IPV6_NODEPORT_2"] = sliceToBe32(nodePortIPv6[4:8])

pkg/datapath/loader/template.go
@@ -221,10 +221,6 @@ func elfVariableSubstitutions(ep datapath.Endpoint) map[string]uint32 {
 	// These values are defined in nodeport.h regardless even for bpf_lxc (so
 	// regardless of whether it's the host endpoint).
 	if option.Config.EnableNodePort && option.Config.EnableIPv6 {
-		result["IPV6_DIRECT_ROUTING_1"] = 0
-		result["IPV6_DIRECT_ROUTING_2"] = 0
-		result["IPV6_DIRECT_ROUTING_3"] = 0
-		result["IPV6_DIRECT_ROUTING_4"] = 0
 		result["IPV6_NODEPORT_1"] = 0
 		result["IPV6_NODEPORT_2"] = 0
 		result["IPV6_NODEPORT_3"] = 0
@@ -233,10 +229,8 @@ func elfVariableSubstitutions(ep datapath.Endpoint) map[string]uint32 {
 
 	if ep.IsHost() {
 		if option.Config.EnableNodePort {
-			result["DIRECT_ROUTING_DEV_IFINDEX"] = 0
 			result["NATIVE_DEV_IFINDEX"] = 0
 			if option.Config.EnableIPv4 {
-				result["IPV4_DIRECT_ROUTING"] = 0
 				result["IPV4_NODEPORT"] = 0
 			}
 		}

commit c0d9d5edd714e025bad57b0927bf6240f53a5226

datapath: Fix panic on direct routing config
If a device is given through options but NodePort is not enabled, Cilium
panics when trying to set the direct routing configuration for the
datapath. This panic happens because we use
node.GetNodePortIPv{4,6}AddrsWithDevices() to retrieve the IP addresses,
but since NodePort is disabled, the IP addresses weren't collected and
that function returns an empty map.

Fixes: 056fec2b ("loader: Move direct routing config. to node_config.h")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/config/config.go
@@ -266,7 +266,7 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		cDefinesMap["NODEPORT_PORT_MAX_NAT"] = "65535"
 	}
 
-	if len(option.Config.Devices) > 0 {
+	if len(option.Config.Devices) > 0 && option.Config.EnableNodePort {
 		// First device from the list is used for direct routing between nodes
 		directRoutingIface := option.Config.Devices[0]
 		directRoutingIfIndex, err := link.GetIfIndex(directRoutingIface)