cilium/cilium

commit 06bce43ab84170ea281d6741f5adc31a91be6c46

aws/eni: Fix race condition leading to overaggressive ENI allocation
The state in EC2 is always considered the source of truth. For this reason, the
EC2 is queried regularly to fetch the latest state. This ensures that the
operator always recovers if it ever gets out of sync.

In order to keep ENI allocation efficient, the resync operation does not block
allocations. This means that allocations will happen while the resync is in
progress. The resync starts at T0 and ends at T1. If allocations happen between
T0 and T1, they may or may not be included in the list of ENIs returned by the
resync.

When an allocation succeeds, it declares the need for a resync to fetch the
latest state. So far, any resync that completes after the allocation has
completed was considered sufficient. However, if the resync started before the
allocation and completes afterwards it may not include the ENI that was
allocated  and thus mistakenly fulfills the resync requirement without
containing the relevant information. This can lead to too many allocations
occurring.

This commit fixes this by adding understanding of resync timestamps. It also
extends the unit tests to reproduce this problem and validate the fix.

Also, while doing this, it makes sense to add the ENI to the instances manager
based on the ENI information returned by the CreateNetworkInterface and
AttachNetworkInterface API calls. This makes the information available before
resync returns and the IPs can be made available earlier.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/aws/eni/node.go
@@ -59,17 +59,22 @@ type Node struct {
 	// printed that this node is out of adapters
 	lastMaxAdapterWarning time.Time
 
-	// resyncNeeded is true after changes have been requested via the EC2
-	// APIs but are not yet synced backed into the instances manager and
-	// allocation should be delayed until that has happened.
-	resyncNeeded bool
-
 	// instanceNotRunning is true when the EC2 instance backing the node is
 	// not running. This state is detected based on error messages returned
 	// when modifying instance state
 	instanceNotRunning bool
 
+	// waitingForAllocation is true when the node is subject to an
+	// allocation which must be performed before another allocation can be
+	// attempted
 	waitingForAllocation bool
+
+	// resyncNeeded is set to the current time when a resync with the EC2
+	// API is required. The timestamp is required to ensure that this is
+	// only reset if the resync started after the time stored in
+	// resyncNeeded. This is needed because resyncs and allocations happen
+	// in parallel.
+	resyncNeeded time.Time
 
 	enis map[string]v2.ENI
 
@@ -165,17 +170,18 @@ func (n *Node) updatedResource(resource *v2.CiliumNode) bool {
 		n.instanceNotRunning = false
 	}
 	n.resource = resource
-	allocationNeeded := n.recalculateLocked()
-	n.mutex.Unlock()
-
+	n.recalculateLocked()
+	allocationNeeded := n.allocationNeeded()
 	if allocationNeeded {
+		n.waitingForAllocation = true
 		n.deficitResolver.Trigger()
 	}
+	n.mutex.Unlock()
 
 	return allocationNeeded
 }
 
-func (n *Node) recalculateLocked() bool {
+func (n *Node) recalculateLocked() {
 	n.enis = map[string]v2.ENI{}
 	n.available = map[string]v2.AllocationIP{}
 	for _, e := range n.manager.instancesAPI.GetENIs(n.resource.Spec.ENI.InstanceID) {
@@ -192,22 +198,19 @@ func (n *Node) recalculateLocked() bool {
 	n.stats.usedIPs = len(n.resource.Status.IPAM.Used)
 	n.stats.availableIPs = len(n.available)
 	n.stats.neededIPs = calculateNeededIPs(n.stats.availableIPs, n.stats.usedIPs, n.resource.Spec.ENI.PreAllocate, n.resource.Spec.ENI.MinAllocate)
-	allocationNeeded := !n.resyncNeeded && !n.waitingForAllocation && n.stats.neededIPs > 0
 
 	n.loggerLocked().WithFields(logrus.Fields{
 		"available":            n.stats.availableIPs,
 		"used":                 n.stats.usedIPs,
 		"toAlloc":              n.stats.neededIPs,
-		"resyncNeeded":         n.resyncNeeded,
 		"waitingForAllocation": n.waitingForAllocation,
+		"resyncNeeded":         n.resyncNeeded,
 	}).Debug("Recalculated needed addresses")
+}
 
-	if allocationNeeded {
-		n.waitingForAllocation = true
-		n.resyncNeeded = true
-	}
-
-	return allocationNeeded
+// allocationNeeded returns true if this node requires IPs to be allocated
+func (n *Node) allocationNeeded() bool {
+	return !n.waitingForAllocation && n.resyncNeeded.IsZero() && n.stats.neededIPs > 0
 }
 
 // ENIs returns a copy of all ENIs attached to the node
@@ -319,7 +322,7 @@ func (n *Node) allocateENI(s *types.Subnet, a *allocatableResources) error {
 	scopedLog.Info("No more IPs available, creating new ENI")
 	n.mutex.RUnlock()
 
-	eniID, err := n.manager.ec2API.CreateNetworkInterface(toAllocate, s.ID, desc, securityGroups)
+	eniID, eni, err := n.manager.ec2API.CreateNetworkInterface(toAllocate, s.ID, desc, securityGroups)
 	if err != nil {
 		n.manager.metricsAPI.IncENIAllocationAttempt("ENI creation failed", s.ID)
 		return fmt.Errorf("unable to create ENI: %s", err)
@@ -362,6 +365,8 @@ func (n *Node) allocateENI(s *types.Subnet, a *allocatableResources) error {
 		"index":        index,
 	})
 
+	eni.Number = int(index)
+
 	scopedLog.Info("Attached ENI to instance")
 
 	if nodeResource.Spec.ENI.DeleteOnTermination {
@@ -382,6 +387,9 @@ func (n *Node) allocateENI(s *types.Subnet, a *allocatableResources) error {
 			return fmt.Errorf("unable to mark ENI for deletion on termination: %s", err)
 		}
 	}
+
+	// Add the information of the created ENI to the instances manager
+	n.manager.instancesAPI.UpdateENI(n.resource.Spec.ENI.InstanceID, eni)
 
 	n.manager.metricsAPI.IncENIAllocationAttempt("success", s.ID)
 	n.manager.metricsAPI.AddIPAllocation(s.ID, toAllocate)
@@ -578,6 +586,11 @@ func (n *Node) ResolveIPDeficit() error {
 
 	err := n.resolveIPDeficit()
 	n.mutex.Lock()
+	if err == nil {
+		n.loggerLocked().Debug("Setting resync needed")
+		n.resyncNeeded = time.Now()
+	}
+	n.recalculateLocked()
 	n.waitingForAllocation = false
 	n.mutex.Unlock()
 	n.manager.resyncTrigger.Trigger()

operator/eni.go
@@ -154,8 +154,8 @@ func startENIAllocator(awsClientQPSLimit float64, awsClientBurst int) error {
 			controller.ControllerParams{
 				RunInterval: time.Minute,
 				DoFunc: func(_ context.Context) error {
-					instances.Resync()
-					nodeManager.Resync()
+					syncTime := instances.Resync()
+					nodeManager.Resync(syncTime)
 					return nil
 				},
 			})

pkg/aws/ec2/ec2.go
@@ -19,7 +19,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/cilium/cilium/pkg/aws/eni"
 	"github.com/cilium/cilium/pkg/aws/types"
 	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/spanstat"
@@ -109,7 +108,7 @@ func (c *Client) describeNetworkInterfaces() ([]ec2.NetworkInterface, error) {
 
 // parseENI parses a ec2.NetworkInterface as returned by the EC2 service API,
 // converts it into a v2. ENI object
-func parseENI(iface *ec2.NetworkInterface, vpcs eni.VpcMap, subnets eni.SubnetMap) (instanceID string, eni *v2.ENI, err error) {
+func parseENI(iface *ec2.NetworkInterface, vpcs types.VpcMap, subnets types.SubnetMap) (instanceID string, eni *v2.ENI, err error) {
 	if iface.PrivateIpAddress == nil {
 		err = fmt.Errorf("ENI has no IP address")
 		return
@@ -146,16 +145,20 @@ func parseENI(iface *ec2.NetworkInterface, vpcs eni.VpcMap, subnets eni.SubnetMa
 	if iface.SubnetId != nil {
 		eni.Subnet.ID = *iface.SubnetId
 
-		if subnet, ok := subnets[eni.Subnet.ID]; ok {
-			eni.Subnet.CIDR = subnet.CIDR
+		if subnets != nil {
+			if subnet, ok := subnets[eni.Subnet.ID]; ok {
+				eni.Subnet.CIDR = subnet.CIDR
+			}
 		}
 	}
 
 	if iface.VpcId != nil {
 		eni.VPC.ID = *iface.VpcId
 
-		if vpc, ok := vpcs[eni.VPC.ID]; ok {
-			eni.VPC.PrimaryCIDR = vpc.PrimaryCIDR
+		if vpcs != nil {
+			if vpc, ok := vpcs[eni.VPC.ID]; ok {
+				eni.VPC.PrimaryCIDR = vpc.PrimaryCIDR
+			}
 		}
 	}
 
@@ -176,8 +179,8 @@ func parseENI(iface *ec2.NetworkInterface, vpcs eni.VpcMap, subnets eni.SubnetMa
 
 // GetInstances returns the list of all instances including their ENIs as
 // instanceMap
-func (c *Client) GetInstances(vpcs eni.VpcMap, subnets eni.SubnetMap) (eni.InstanceMap, error) {
-	instances := eni.InstanceMap{}
+func (c *Client) GetInstances(vpcs types.VpcMap, subnets types.SubnetMap) (types.InstanceMap, error) {
+	instances := types.InstanceMap{}
 
 	networkInterfaces, err := c.describeNetworkInterfaces()
 	if err != nil {
@@ -219,8 +222,8 @@ func (c *Client) describeVpcs() ([]ec2.Vpc, error) {
 }
 
 // GetVpcs retrieves and returns all Vpcs
-func (c *Client) GetVpcs() (eni.VpcMap, error) {
-	vpcs := eni.VpcMap{}
+func (c *Client) GetVpcs() (types.VpcMap, error) {
+	vpcs := types.VpcMap{}
 
 	vpcList, err := c.describeVpcs()
 	if err != nil {
@@ -254,8 +257,8 @@ func (c *Client) describeSubnets() ([]ec2.Subnet, error) {
 }
 
 // GetSubnets returns all EC2 subnets as a subnetMap
-func (c *Client) GetSubnets() (eni.SubnetMap, error) {
-	subnets := eni.SubnetMap{}
+func (c *Client) GetSubnets() (types.SubnetMap, error) {
+	subnets := types.SubnetMap{}
 
 	subnetList, err := c.describeSubnets()
 	if err != nil {
@@ -293,7 +296,7 @@ func (c *Client) GetSubnets() (eni.SubnetMap, error) {
 }
 
 // CreateNetworkInterface creates an ENI with the given parameters
-func (c *Client) CreateNetworkInterface(toAllocate int64, subnetID, desc string, groups []string) (string, error) {
+func (c *Client) CreateNetworkInterface(toAllocate int64, subnetID, desc string, groups []string) (string, *v2.ENI, error) {
 	createReq := &ec2.CreateNetworkInterfaceInput{
 		Description:                    &desc,
 		SecondaryPrivateIpAddressCount: &toAllocate,
@@ -309,10 +312,21 @@ func (c *Client) CreateNetworkInterface(toAllocate int64, subnetID, desc string,
 	resp, err := create.Send()
 	c.metricsAPI.ObserveEC2APICall("CreateNetworkInterfaceRequest", deriveStatus(create.Request, err), sinceStart.Seconds())
 	if err != nil {
-		return "", err
+		return "", nil, err
 	}
 
-	return *resp.NetworkInterface.NetworkInterfaceId, nil
+	_, eni, err := parseENI(resp.NetworkInterface, nil, nil)
+	if err != nil {
+		// The error is ignored on purpose. The allocation itself has
+		// succeeded. The ability to parse and return the ENI
+		// information is optional. Returning the ENI ID is sufficient
+		// to allow for the caller to retrieve the ENI information via
+		// the API or wait for a regular sync to fetch the information.
+		return *resp.NetworkInterface.NetworkInterfaceId, nil, nil
+	}
+
+	return eni.ID, eni, nil
+
 }
 
 // DeleteNetworkInterface deletes an ENI with the specified ID

pkg/aws/ec2/mock/mock.go
@@ -48,19 +48,21 @@ type API struct {
 	unattached map[string]*v2.ENI
 	enis       map[string]eniMap
 	subnets    map[string]*types.Subnet
+	vpcs       map[string]*types.Vpc
 	errors     map[Operation]error
 	delays     map[Operation]time.Duration
 	allocator  *ipallocator.Range
 	limiter    *rate.Limiter
 }
 
-func NewAPI(subnets []*types.Subnet) *API {
+func NewAPI(subnets []*types.Subnet, vpcs []*types.Vpc) *API {
 	_, cidr, _ := net.ParseCIDR("10.0.0.0/8")
 
 	api := &API{
 		unattached: map[string]*v2.ENI{},
 		enis:       map[string]eniMap{},
 		subnets:    map[string]*types.Subnet{},
+		vpcs:       map[string]*types.Vpc{},
 		allocator:  ipallocator.NewCIDRRange(cidr),
 		errors:     map[Operation]error{},
 		delays:     map[Operation]time.Duration{},
@@ -68,6 +70,10 @@ func NewAPI(subnets []*types.Subnet) *API {
 
 	for _, s := range subnets {
 		api.subnets[s.ID] = s
+	}
+
+	for _, v := range vpcs {
+		api.vpcs[v.ID] = v
 	}
 
 	return api
@@ -122,53 +128,6 @@ func (e *API) rateLimit() {
 	}
 }
 
-func (e *API) GetENI(instanceID string, index int) *v2.ENI {
-	e.mutex.RLock()
-	defer e.mutex.RUnlock()
-
-	for _, eni := range e.enis[instanceID] {
-		if eni.Number == index {
-			return eni
-		}
-	}
-
-	return nil
-}
-
-func (e *API) GetENIs(instanceID string) (enis []*v2.ENI) {
-	e.mutex.RLock()
-	defer e.mutex.RUnlock()
-
-	for _, eni := range e.enis[instanceID] {
-		enis = append(enis, eni)
-	}
-	return
-}
-
-func (e *API) GetSubnet(subnetID string) *types.Subnet {
-	e.mutex.RLock()
-	defer e.mutex.RUnlock()
-
-	return e.subnets[subnetID]
-}
-
-func (e *API) FindSubnetByTags(vpcID, availabilityZone string, required types.Tags) (bestSubnet *types.Subnet) {
-	e.mutex.RLock()
-	defer e.mutex.RUnlock()
-
-	for _, s := range e.subnets {
-		if s.VpcID == vpcID && s.AvailabilityZone == availabilityZone && s.Tags.Match(required) {
-			if bestSubnet == nil || bestSubnet.AvailableAddresses < s.AvailableAddresses {
-				bestSubnet = s
-			}
-		}
-	}
-	return
-}
-
-func (e *API) Resync() {
-}
-
 func (e *API) simulateDelay(op Operation) {
 	e.mutex.RLock()
 	delay, ok := e.delays[op]
@@ -178,7 +137,7 @@ func (e *API) simulateDelay(op Operation) {
 	}
 }
 
-func (e *API) CreateNetworkInterface(toAllocate int64, subnetID, desc string, groups []string) (string, error) {
+func (e *API) CreateNetworkInterface(toAllocate int64, subnetID, desc string, groups []string) (string, *v2.ENI, error) {
 	e.rateLimit()
 	e.simulateDelay(CreateNetworkInterface)
 
@@ -186,16 +145,16 @@ func (e *API) CreateNetworkInterface(toAllocate int64, subnetID, desc string, gr
 	defer e.mutex.Unlock()
 
 	if err, ok := e.errors[CreateNetworkInterface]; ok {
-		return "", err
+		return "", nil, err
 	}
 
 	subnet, ok := e.subnets[subnetID]
 	if !ok {
-		return "", fmt.Errorf("subnet %s not found", subnetID)
+		return "", nil, fmt.Errorf("subnet %s not found", subnetID)
 	}
 
 	if int(toAllocate) > subnet.AvailableAddresses {
-		return "", fmt.Errorf("subnet %s has not enough addresses available", subnetID)
+		return "", nil, fmt.Errorf("subnet %s has not enough addresses available", subnetID)
 	}
 
 	eniID := uuid.NewUUID().String()
@@ -219,7 +178,7 @@ func (e *API) CreateNetworkInterface(toAllocate int64, subnetID, desc string, gr
 	subnet.AvailableAddresses -= int(toAllocate)
 
 	e.unattached[eniID] = eni
-	return eniID, nil
+	return eniID, eni, nil
 }
 
 func (e *API) DeleteNetworkInterface(eniID string) error {
@@ -320,4 +279,55 @@ func (e *API) AssignPrivateIpAddresses(eniID string, addresses int64) error {
 		}
 	}
 	return fmt.Errorf("Unable to find ENI with ID %s", eniID)
+}
+
+func (e *API) GetInstances(vpcs types.VpcMap, subnets types.SubnetMap) (types.InstanceMap, error) {
+	instances := types.InstanceMap{}
+
+	e.mutex.RLock()
+	defer e.mutex.RUnlock()
+
+	for instanceID, enis := range e.enis {
+		for _, eni := range enis {
+			if subnets != nil {
+				if subnet, ok := subnets[eni.Subnet.ID]; ok {
+					eni.Subnet.CIDR = subnet.CIDR
+				}
+			}
+
+			if vpcs != nil {
+				if vpc, ok := vpcs[eni.VPC.ID]; ok {
+					eni.VPC.PrimaryCIDR = vpc.PrimaryCIDR
+				}
+			}
+
+			instances.Add(instanceID, eni)
+		}
+	}
+
+	return instances, nil
+}
+
+func (e *API) GetVpcs() (types.VpcMap, error) {
+	vpcs := types.VpcMap{}
+
+	e.mutex.RLock()
+	defer e.mutex.RUnlock()
+
+	for _, v := range e.vpcs {
+		vpcs[v.ID] = v
+	}
+	return vpcs, nil
+}
+
+func (e *API) GetSubnets() (types.SubnetMap, error) {
+	subnets := types.SubnetMap{}
+
+	e.mutex.RLock()
+	defer e.mutex.RUnlock()
+
+	for _, s := range e.subnets {
+		subnets[s.ID] = s
+	}
+	return subnets, nil
 }

pkg/aws/ec2/mock/mock_test.go
@@ -35,32 +35,28 @@ type MockSuite struct{}
 var _ = check.Suite(&MockSuite{})
 
 func (e *MockSuite) TestMock(c *check.C) {
-	api := NewAPI([]*types.Subnet{{ID: "s-1", AvailableAddresses: 100}})
+	api := NewAPI([]*types.Subnet{{ID: "s-1", AvailableAddresses: 100}}, []*types.Vpc{{ID: "v-1"}})
 	c.Assert(api, check.Not(check.IsNil))
 
-	eniID1, err := api.CreateNetworkInterface(8, "s-1", "desc", []string{"sg1", "sg2"})
+	eniID1, _, err := api.CreateNetworkInterface(8, "s-1", "desc", []string{"sg1", "sg2"})
 	c.Assert(err, check.IsNil)
 
-	eniID2, err := api.CreateNetworkInterface(8, "s-1", "desc", []string{"sg1", "sg2"})
+	eniID2, _, err := api.CreateNetworkInterface(8, "s-1", "desc", []string{"sg1", "sg2"})
 	c.Assert(err, check.IsNil)
 
 	_, err = api.AttachNetworkInterface(0, "i-1", eniID1)
 	c.Assert(err, check.IsNil)
 
-	eni := api.GetENI("i-1", 0)
-	c.Assert(eni, check.Not(check.IsNil))
-
-	enis := api.GetENIs("i-1")
-	c.Assert(len(enis), check.Equals, 1)
+	_, ok := api.enis["i-1"][eniID1]
+	c.Assert(ok, check.Equals, true)
 
 	_, err = api.AttachNetworkInterface(1, "i-1", eniID2)
 	c.Assert(err, check.IsNil)
 
-	eni = api.GetENI("i-1", 1)
-	c.Assert(eni, check.Not(check.IsNil))
-
-	enis = api.GetENIs("i-1")
-	c.Assert(len(enis), check.Equals, 2)
+	_, ok = api.enis["i-1"][eniID1]
+	c.Assert(ok, check.Equals, true)
+	_, ok = api.enis["i-1"][eniID2]
+	c.Assert(ok, check.Equals, true)
 
 	err = api.DeleteNetworkInterface(eniID1)
 	c.Assert(err, check.IsNil)
@@ -71,20 +67,20 @@ func (e *MockSuite) TestMock(c *check.C) {
 	err = api.DeleteNetworkInterface(eniID2)
 	c.Assert(err, check.IsNil)
 
-	c.Assert(api.GetENI("i-1", 0), check.IsNil)
-
-	c.Assert(api.GetSubnet("s-1"), check.Not(check.IsNil))
-	c.Assert(api.GetSubnet("s-2"), check.IsNil)
+	_, ok = api.enis["i-1"][eniID1]
+	c.Assert(ok, check.Equals, false)
+	_, ok = api.enis["i-1"][eniID2]
+	c.Assert(ok, check.Equals, false)
 }
 
 func (e *MockSuite) TestSetMockError(c *check.C) {
-	api := NewAPI([]*types.Subnet{})
+	api := NewAPI([]*types.Subnet{}, []*types.Vpc{})
 	c.Assert(api, check.Not(check.IsNil))
 
 	mockError := errors.New("error")
 
 	api.SetMockError(CreateNetworkInterface, mockError)
-	_, err := api.CreateNetworkInterface(8, "s-1", "desc", []string{"sg1", "sg2"})
+	_, _, err := api.CreateNetworkInterface(8, "s-1", "desc", []string{"sg1", "sg2"})
 	c.Assert(err, check.Equals, mockError)
 
 	api.SetMockError(AttachNetworkInterface, mockError)
@@ -105,7 +101,7 @@ func (e *MockSuite) TestSetMockError(c *check.C) {
 }
 
 func (e *MockSuite) TestSetDelay(c *check.C) {
-	api := NewAPI([]*types.Subnet{})
+	api := NewAPI([]*types.Subnet{}, []*types.Vpc{})
 	c.Assert(api, check.Not(check.IsNil))
 
 	api.SetDelay(AllOperations, time.Second)
@@ -117,10 +113,10 @@ func (e *MockSuite) TestSetDelay(c *check.C) {
 }
 
 func (e *MockSuite) TestSetLimiter(c *check.C) {
-	api := NewAPI([]*types.Subnet{{ID: "s-1", AvailableAddresses: 100}})
+	api := NewAPI([]*types.Subnet{{ID: "s-1", AvailableAddresses: 100}}, []*types.Vpc{{ID: "v-1"}})
 	c.Assert(api, check.Not(check.IsNil))
 
 	api.SetLimiter(10.0, 2)
-	_, err := api.CreateNetworkInterface(8, "s-1", "desc", []string{"sg1", "sg2"})
+	_, _, err := api.CreateNetworkInterface(8, "s-1", "desc", []string{"sg1", "sg2"})
 	c.Assert(err, check.IsNil)
 }

pkg/aws/eni/instances.go
@@ -16,6 +16,8 @@
 package eni
 
 import (
+	"time"
+
 	"github.com/cilium/cilium/pkg/aws/types"
 	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/lock"
@@ -24,9 +26,9 @@ import (
 )
 
 type instanceAPI interface {
-	GetInstances(vpcs VpcMap, subnets SubnetMap) (InstanceMap, error)
-	GetSubnets() (SubnetMap, error)
-	GetVpcs() (VpcMap, error)
+	GetInstances(vpcs types.VpcMap, subnets types.SubnetMap) (types.InstanceMap, error)
+	GetSubnets() (types.SubnetMap, error)
+	GetVpcs() (types.VpcMap, error)
 }
 
 // instance is the minimal representation of an AWS instance as needed by the
@@ -37,38 +39,13 @@ type instance struct {
 	enis map[string]*v2.ENI
 }
 
-// InstanceMap is the list of all instances indexed by instance ID
-type InstanceMap map[string]*instance
-
-// SubnetMap indexes AWS subnets by subnet ID
-type SubnetMap map[string]*types.Subnet
-
-// VpcMap indexes AWS VPCs by VPC ID
-type VpcMap map[string]*types.Vpc
-
-// Add adds an instance definition to the instance map. instanceMap may not be
-// subject to concurrent access while add() is used.
-func (m InstanceMap) Add(instanceID string, eni *v2.ENI) {
-	i, ok := m[instanceID]
-	if !ok {
-		i = &instance{}
-		m[instanceID] = i
-	}
-
-	if i.enis == nil {
-		i.enis = map[string]*v2.ENI{}
-	}
-
-	i.enis[eni.ID] = eni
-}
-
 // InstancesManager maintains the list of instances. It must be kept up to date
 // by calling resync() regularly.
 type InstancesManager struct {
 	mutex      lock.RWMutex
-	instances  InstanceMap
-	subnets    SubnetMap
-	vpcs       VpcMap
+	instances  types.InstanceMap
+	subnets    types.SubnetMap
+	vpcs       types.VpcMap
 	api        instanceAPI
 	metricsAPI metricsAPI
 }
@@ -76,7 +53,7 @@ type InstancesManager struct {
 // NewInstancesManager returns a new instances manager
 func NewInstancesManager(api instanceAPI, metricsAPI metricsAPI) *InstancesManager {
 	return &InstancesManager{
-		instances:  InstanceMap{},
+		instances:  types.InstanceMap{},
 		api:        api,
 		metricsAPI: metricsAPI,
 	}
@@ -112,26 +89,29 @@ func (m *InstancesManager) FindSubnetByTags(vpcID, availabilityZone string, requ
 }
 
 // Resync fetches the list of EC2 instances and subnets and updates the local
-// cache in the instanceManager
-func (m *InstancesManager) Resync() {
+// cache in the instanceManager. It returns the time when the resync has
+// started or time.Time{} if it did not complete.
+func (m *InstancesManager) Resync() time.Time {
 	m.metricsAPI.IncResyncCount()
+
+	resyncStart := time.Now()
 
 	vpcs, err := m.api.GetVpcs()
 	if err != nil {
 		log.WithError(err).Warning("Unable to synchronize EC2 VPC list")
-		return
+		return time.Time{}
 	}
 
 	subnets, err := m.api.GetSubnets()
 	if err != nil {
 		log.WithError(err).Warning("Unable to retrieve EC2 subnets list")
-		return
+		return time.Time{}
 	}
 
 	instances, err := m.api.GetInstances(vpcs, subnets)
 	if err != nil {
 		log.WithError(err).Warning("Unable to synchronize EC2 interface list")
-		return
+		return time.Time{}
 	}
 
 	log.WithFields(logrus.Fields{
@@ -145,6 +125,8 @@ func (m *InstancesManager) Resync() {
 	m.subnets = subnets
 	m.vpcs = vpcs
 	m.mutex.Unlock()
+
+	return resyncStart
 }
 
 // GetENI returns the ENI of an instance at a particular interface index
@@ -165,16 +147,16 @@ func (m *InstancesManager) GetENIs(instanceID string) []*v2.ENI {
 
 // getENIs returns the list of ENIs associated with a particular instance
 func (m *InstancesManager) getENIs(instanceID string) []*v2.ENI {
-	enis := []*v2.ENI{}
-
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
+	return m.instances.Get(instanceID)
+}
 
-	if i, ok := m.instances[instanceID]; ok {
-		for _, e := range i.enis {
-			enis = append(enis, e.DeepCopy())
-		}
-	}
-
-	return enis
+// UpdateENI updates the ENI definition of an ENI for a particular instance. If
+// the ENI is already known, the definition is updated, otherwise the ENI is
+// added to the instance.
+func (m *InstancesManager) UpdateENI(instanceID string, eni *v2.ENI) {
+	m.mutex.Lock()
+	m.instances.Update(instanceID, eni)
+	m.mutex.Unlock()
 }

pkg/aws/eni/instances_test.go
@@ -29,10 +29,10 @@ type instancesApiMock struct {
 	subnetsIteration   int
 }
 
-func (i *instancesApiMock) GetInstances(vpcs VpcMap, subnets SubnetMap) (m InstanceMap, err error) {
+func (i *instancesApiMock) GetInstances(vpcs types.VpcMap, subnets types.SubnetMap) (m types.InstanceMap, err error) {
 	i.instancesIteration++
 
-	m = InstanceMap{}
+	m = types.InstanceMap{}
 
 	m.Add("i-1", &v2.ENI{
 		ID:             "eni-1",
@@ -69,8 +69,8 @@ func (i *instancesApiMock) GetInstances(vpcs VpcMap, subnets SubnetMap) (m Insta
 	return
 }
 
-func (i *instancesApiMock) GetVpcs() (v VpcMap, err error) {
-	v = VpcMap{}
+func (i *instancesApiMock) GetVpcs() (v types.VpcMap, err error) {
+	v = types.VpcMap{}
 
 	v["vpc-0"] = &types.Vpc{
 		ID:          "vpc-0",
@@ -85,10 +85,10 @@ func (i *instancesApiMock) GetVpcs() (v VpcMap, err error) {
 	return
 }
 
-func (i *instancesApiMock) GetSubnets() (s SubnetMap, err error) {
+func (i *instancesApiMock) GetSubnets() (s types.SubnetMap, err error) {
 	i.subnetsIteration++
 
-	s = SubnetMap{}
+	s = types.SubnetMap{}
 
 	s["subnet-1"] = &types.Subnet{
 		ID:                 "subnet-1",

pkg/aws/eni/node_manager.go
@@ -26,7 +26,6 @@ import (
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/trigger"
 
-	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/semaphore"
 )
 
@@ -41,11 +40,12 @@ type nodeManagerAPI interface {
 	GetENIs(instanceID string) []*v2.ENI
 	GetSubnet(subnetID string) *types.Subnet
 	FindSubnetByTags(vpcID, availabilityZone string, required types.Tags) *types.Subnet
-	Resync()
+	Resync() time.Time
+	UpdateENI(instanceID string, eni *v2.ENI)
 }
 
 type ec2API interface {
-	CreateNetworkInterface(toAllocate int64, subnetID, desc string, groups []string) (string, error)
+	CreateNetworkInterface(toAllocate int64, subnetID, desc string, groups []string) (string, *v2.ENI, error)
 	DeleteNetworkInterface(eniID string) error
 	AttachNetworkInterface(index int64, instanceID, eniID string) (string, error)
 	ModifyNetworkInterface(eniID, attachmentID string, deleteOnTermination bool) error
@@ -99,8 +99,8 @@ func NewNodeManager(instancesAPI nodeManagerAPI, ec2API ec2API, k8sAPI k8sAPI, m
 		MinInterval:     10 * time.Millisecond,
 		MetricsObserver: metrics.ResyncTrigger(),
 		TriggerFunc: func(reasons []string) {
-			instancesAPI.Resync()
-			mngr.Resync()
+			syncTime := instancesAPI.Resync()
+			mngr.Resync(syncTime)
 		},
 	})
 	if err != nil {
@@ -232,18 +232,20 @@ type resyncStats struct {
 	nodesInDeficit      int
 }
 
-func (n *NodeManager) resyncNode(node *Node, stats *resyncStats) {
+func (n *NodeManager) resyncNode(node *Node, stats *resyncStats, syncTime time.Time) {
 	node.mutex.Lock()
 
-	// Resync() is always called after resync of the instance data,
-	// mark node as resynced
-	node.resyncNeeded = false
-	allocationNeeded := node.recalculateLocked()
-	node.loggerLocked().WithFields(logrus.Fields{
-		fieldName:   node.name,
-		"available": node.stats.availableIPs,
-		"used":      node.stats.usedIPs,
-	}).Debug("Recalculated allocation requirements")
+	if syncTime.After(node.resyncNeeded) {
+		node.loggerLocked().Debug("Resetting resyncNeeded")
+		node.resyncNeeded = time.Time{}
+	}
+
+	node.recalculateLocked()
+	allocationNeeded := node.allocationNeeded()
+	if allocationNeeded {
+		node.waitingForAllocation = true
+		node.deficitResolver.Trigger()
+	}
 
 	stats.mutex.Lock()
 	stats.totalUsed += node.stats.usedIPs
@@ -261,10 +263,6 @@ func (n *NodeManager) resyncNode(node *Node, stats *resyncStats) {
 		stats.nodesAtCapacity++
 	}
 	stats.mutex.Unlock()
-
-	if allocationNeeded && node.stats.remainingInterfaces > 0 {
-		node.deficitResolver.Trigger()
-	}
 	node.mutex.Unlock()
 
 	node.k8sSync.Trigger()
@@ -274,14 +272,14 @@ func (n *NodeManager) resyncNode(node *Node, stats *resyncStats) {
 // attendance is defined by the number of IPs needed to reach the configured
 // watermarks. Any updates to the node resource are synchronized to the
 // Kubernetes apiserver.
-func (n *NodeManager) Resync() {
+func (n *NodeManager) Resync(syncTime time.Time) {
 	stats := resyncStats{}
 	sem := semaphore.NewWeighted(n.parallelWorkers)
 
 	for _, node := range n.GetNodesByNeededAddresses() {
 		sem.Acquire(context.TODO(), 1)
 		go func(node *Node, stats *resyncStats) {
-			n.resyncNode(node, stats)
+			n.resyncNode(node, stats, syncTime)
 			sem.Release(1)
 		}(node, &stats)
 	}

commit 43253d046c93debe2a9d4c76f6ed97bce0ce3afc

eni: Use deep-copied object to prevent data race
This fixes a potential data race as `n.resource` is a live pointer to an
object.

Fixes: 06bce43ab8 ("aws/eni: Fix race condition leading to overaggressive ENI allocation")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/aws/eni/node.go
@@ -495,7 +495,7 @@ func (n *Node) allocateENI(ctx context.Context, s *types.Subnet, a *allocatableR
 	}
 
 	// Add the information of the created ENI to the instances manager
-	n.manager.instancesAPI.UpdateENI(n.resource.Spec.ENI.InstanceID, eni)
+	n.manager.instancesAPI.UpdateENI(nodeResource.Spec.ENI.InstanceID, eni)
 
 	n.manager.metricsAPI.IncENIAllocationAttempt("success", s.ID)
 	n.manager.metricsAPI.AddIPAllocation(s.ID, toAllocate)