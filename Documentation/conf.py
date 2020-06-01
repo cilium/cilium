8b3e99abd256bdbfe398991fd0b16af45078cac4
Sergey Generalov <sergey@genbit.ru>
2020-06-01 14:39:24 -0700

    docs: introduce white cilium docs version
    [ upstream commit 1319eff554ab4b7ec6320fe2d8f93d1fa92a0af5 ]
    
    [ Backporter's notes: Minor rebase issue with requirements detection,
                          Fixed by extending Makefile requirements check ]
    
    - use forked version of sphinx_rtd_theme
    - removes _themes/sphinx_rtd_theme
    - use white colors to be inline with cilium.io design
    - use cilium.io navigation for top menu
    
    Signed-off-by: Sergey Generalov <sergey@genbit.ru>
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -33,6 +33,7 @@ import semver
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+html_logo = "images/logo.svg"
 extensions = ['sphinx.ext.ifconfig',
               'sphinx.ext.githubpages',
               'sphinx.ext.extlinks',
@@ -139,7 +140,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_themes/**/*.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -153,9 +154,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes/sphinx_rtd_theme", ]
-html_style = "static/css/theme.css"
+html_theme = "sphinx_rtd_theme_cilium"
 html_context = {
     'release': release
 }
@@ -164,13 +163,14 @@ html_context = {
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['images', '_static',
-                    '_themes/sphinx_rtd_theme/sphinx_rtd_theme']
+html_static_path = ['images', '_static']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
