87d4ced59400ced73bb577a734406a41772c1c03
Sergey Generalov <sergey@genbit.ru>
2020-06-01 14:39:24 -0700

    docs: introduce white cilium docs version
    [ upstream commit 1319eff554ab4b7ec6320fe2d8f93d1fa92a0af5 ]
    
    [ Backporter's notes: Minor rebase issue with requirements detection,
                          Fixed by extending Makefile requirements check ]
    
    - use forked version of sphinx_rtd_theme
    - removes _themes/sphinx_rtd_theme
    - use white colors to be inline with cilium.io design
    - use cilium.io navigation for top menu
    
    Signed-off-by: Sergey Generalov <sergey@genbit.ru>
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -32,13 +32,14 @@ import subprocess
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+html_logo = "images/logo.svg"
 extensions = ['sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.extlinks',
-    'sphinxcontrib.openapi',
-    'sphinx_tabs.tabs',
-    'sphinxcontrib.spelling',
-    'versionwarning.extension' ]
+              'sphinx.ext.githubpages',
+              'sphinx.ext.extlinks',
+              'sphinxcontrib.openapi',
+              'sphinx_tabs.tabs',
+              'sphinxcontrib.spelling',
+              'versionwarning.extension']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,7 +50,7 @@ templates_path = ['_templates']
 # source_suffix = ['.rst', '.md']
 source_suffix = ['.rst', '.md']
 source_parsers = {
-   '.md': 'recommonmark.parser.CommonMarkParser',
+    '.md': 'recommonmark.parser.CommonMarkParser',
 }
 
 # The master toctree document.
@@ -73,7 +74,7 @@ versionwarning_body_selector = "div.document"
 # Map latest -> master.
 # Map stable -> current version number.
 branch = os.environ.get('READTHEDOCS_VERSION')
-if branch == None or branch == 'latest':
+if branch is None or branch == 'latest':
     branch = 'HEAD'
     archive_name = 'master'
 elif branch == 'stable':
@@ -84,7 +85,7 @@ else:
 githubusercontent = 'https://raw.githubusercontent.com/cilium/cilium/'
 scm_web = githubusercontent + branch
 jenkins_branch = 'https://jenkins.cilium.io/view/Cilium-v' + release[0:3]
-archive_filename = archive_name +'.tar.gz'
+archive_filename = archive_name + '.tar.gz'
 archive_link = 'https://github.com/cilium/cilium/archive/' + archive_filename
 archive_name = 'cilium-' + archive_name.strip('v')
 
@@ -95,7 +96,7 @@ rst_epilog = """
 .. |SCM_ARCHIVE_NAME| replace:: \{a}
 .. |SCM_ARCHIVE_FILENAME| replace:: \{f}
 .. |SCM_ARCHIVE_LINK| replace:: \{l}
-""".format(s = scm_web, b = branch, a = archive_name, f = archive_filename, l = archive_link)
+""".format(s=scm_web, b=branch, a=archive_name, f=archive_filename, l=archive_link)
 
 extlinks = {
     'git-tree': (scm_web + "/%s", ''),
@@ -112,7 +113,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_themes/**/*.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -126,23 +127,23 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes/sphinx_rtd_theme", ]
-html_style = "static/css/theme.css"
+html_theme = "sphinx_rtd_theme_cilium"
 html_context = {
-        'release': release
+    'release': release
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['images', '_static', '_themes/sphinx_rtd_theme/sphinx_rtd_theme']
+html_static_path = ['images', '_static']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
@@ -205,6 +206,7 @@ http_strict_mode = False
 
 # Try as hard as possible to find references
 default_role = 'any'
+
 
 def setup(app):
     app.add_stylesheet('parsed-literal.css')
