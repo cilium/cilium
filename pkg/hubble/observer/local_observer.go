cilium/cilium

commit a1d0430d9a394b801ab8846cb69bd9c1e0f2149e

hubble/server: Populate total number of flows and uptime in status
Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/hubble/observer/local_observer.go
@@ -84,6 +84,12 @@ type LocalObserverServer struct {
 	payloadParser *parser.Parser
 
 	opts observeroption.Options
+
+	// startTime is the time when this instance was started
+	startTime time.Time
+
+	// numObservedFlows counts how many flows have been observed
+	numObservedFlows uint64
 }
 
 // NewLocalServer returns a new local observer server.
@@ -112,6 +118,7 @@ func NewLocalServer(
 		stopped:       make(chan struct{}),
 		eventschan:    make(chan *observerpb.GetFlowsResponse, 100), // option here?
 		payloadParser: payloadParser,
+		startTime:     time.Now(),
 		opts:          opts,
 	}
 
@@ -161,6 +168,7 @@ nextEvent:
 			}
 		}
 
+		s.numObservedFlows++
 		// FIXME: Convert metrics into an OnDecodedFlow function
 		metrics.ProcessFlow(flow)
 
@@ -212,8 +220,10 @@ func (s *LocalObserverServer) ServerStatus(
 	ctx context.Context, req *observerpb.ServerStatusRequest,
 ) (*observerpb.ServerStatusResponse, error) {
 	return &observerpb.ServerStatusResponse{
-		MaxFlows: s.GetRingBuffer().Cap(),
-		NumFlows: s.GetRingBuffer().Len(),
+		MaxFlows:  s.GetRingBuffer().Cap(),
+		NumFlows:  s.GetRingBuffer().Len(),
+		SeenFlows: s.numObservedFlows,
+		UptimeNs:  uint64(time.Since(s.startTime).Nanoseconds()),
 	}, nil
 }
 

pkg/hubble/observer/local_observer_test.go
@@ -66,7 +66,9 @@ func TestLocalObserverServer_ServerStatus(t *testing.T) {
 	require.NoError(t, err)
 	res, err := s.ServerStatus(context.Background(), &observerpb.ServerStatusRequest{})
 	require.NoError(t, err)
-	assert.Equal(t, &observerpb.ServerStatusResponse{NumFlows: 0, MaxFlows: 2}, res)
+	assert.Equal(t, uint64(0), res.SeenFlows)
+	assert.Equal(t, uint64(0), res.NumFlows)
+	assert.Equal(t, uint64(2), res.MaxFlows)
 }
 
 func TestLocalObserverServer_GetFlows(t *testing.T) {

commit 6978bde591f1929efce12168e89d36cd248c57b9

hubble/observer: increment 'numObservedFlows' atomically
Fixes: a1d0430d9a39 ("hubble/server: Populate total number of flows and uptime in status")
Signed-off-by: André Martins <andre@cilium.io>

pkg/hubble/observer/local_observer.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	flowpb "github.com/cilium/cilium/api/v1/flow"
@@ -170,7 +171,7 @@ nextEvent:
 			}
 		}
 
-		s.numObservedFlows++
+		atomic.AddUint64(&s.numObservedFlows, 1)
 		// FIXME: Convert metrics into an OnDecodedFlow function
 		metrics.ProcessFlow(flow)
 
@@ -224,7 +225,7 @@ func (s *LocalObserverServer) ServerStatus(
 	return &observerpb.ServerStatusResponse{
 		MaxFlows:  s.GetRingBuffer().Cap(),
 		NumFlows:  s.GetRingBuffer().Len(),
-		SeenFlows: s.numObservedFlows,
+		SeenFlows: atomic.LoadUint64(&s.numObservedFlows),
 		UptimeNs:  uint64(time.Since(s.startTime).Nanoseconds()),
 	}, nil
 }