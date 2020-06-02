cilium/cilium

commit ef77a98444cb25494ad47514d96099f545086a43

eni: Fix data races and potential deadlock
This commit mostly reworks `eni.(*Node).ResyncInterfacesAndIPs()` as it
makes many unprotected accesses to shared resources. This change is to
reduce the scope of the mutexes to only when a shared resource is
accessed. It also refactors that code such that only the immediate
structure is responsible for locking / unlocking its mutex. This allows
a mixtures of read and write locks intended to minimize the impact on
performance as much as possible. This change also attempts to minimize
the nesting of mutex locks as much as possible, which can lead to
deadlocking or long hanging periods.

Not all the data race output from Golang is provided in this commit body
as there were many small, nested data races that were fixed along the
way. The main, important data race outputs are provided below.

All data races were found by running `-race` when running `make
unit-tests`.

```
WARNING: DATA RACE
Read at 0x00c00017c388 by goroutine 86:
  github.com/cilium/cilium/pkg/aws/eni.(*Node).ResyncInterfacesAndIPs()
      /home/chris/code/cilium/cilium/pkg/aws/eni/node.go:433 +0x1ca
  github.com/cilium/cilium/pkg/ipam.(*Node).recalculate()
      /home/chris/code/cilium/cilium/pkg/ipam/node.go:352 +0xfb
  github.com/cilium/cilium/pkg/ipam.(*Node).MaintainIPPool()
      /home/chris/code/cilium/cilium/pkg/ipam/node.go:669 +0xc1
  github.com/cilium/cilium/pkg/ipam.(*NodeManager).Update.func1()
      /home/chris/code/cilium/cilium/pkg/ipam/node_manager.go:241 +0x8b
  github.com/cilium/cilium/pkg/trigger.(*Trigger).waiter()
      /home/chris/code/cilium/cilium/pkg/trigger/trigger.go:206 +0x4b9

Previous write at 0x00c00017c388 by goroutine 82:
  github.com/cilium/cilium/pkg/aws/eni.(*InstancesManager).Resync()
      /home/chris/code/cilium/cilium/pkg/aws/eni/instances.go:185 +0xf24
  github.com/cilium/cilium/pkg/aws/eni.(*ENISuite).TestNodeManagerManyNodes()
      /home/chris/code/cilium/cilium/pkg/aws/eni/node_manager_test.go:589 +0xb98

pkg/aws/eni/node.go
@@ -81,7 +81,8 @@ func (n *Node) loggerLocked() *logrus.Entry {
 // resource with ENI specific information
 func (n *Node) PopulateStatusFields(k8sObj *v2.CiliumNode) {
 	k8sObj.Status.ENI.ENIs = map[string]eniTypes.ENI{}
-	n.manager.instances.ForeachInterface(n.node.InstanceID(),
+
+	n.manager.ForeachInstance(n.node.InstanceID(),
 		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
 			e, ok := rev.Resource.(*eniTypes.ENI)
 			if ok {
@@ -89,6 +90,7 @@ func (n *Node) PopulateStatusFields(k8sObj *v2.CiliumNode) {
 			}
 			return nil
 		})
+
 	return
 }
 
@@ -100,6 +102,9 @@ func (n *Node) getLimits() (ipamTypes.Limits, bool) {
 // PrepareIPRelease prepares the release of ENI IPs.
 func (n *Node) PrepareIPRelease(excessIPs int, scopedLog *logrus.Entry) *ipam.ReleaseAction {
 	r := &ipam.ReleaseAction{}
+
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
 
 	// Iterate over ENIs on this node, select the ENI with the most
 	// addresses available for release
@@ -244,7 +249,8 @@ func (n *Node) getSecurityGroupIDs(ctx context.Context) ([]string, error) {
 	}
 
 	var securityGroups []string
-	n.manager.instances.ForeachInterface(n.node.InstanceID(),
+
+	n.manager.ForeachInstance(n.node.InstanceID(),
 		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
 			e, ok := rev.Resource.(*eniTypes.ENI)
 			if ok && e.Number == 0 {
@@ -430,21 +436,28 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 // ResyncInterfacesAndIPs is called to retrieve and ENIs and IPs as known to
 // the EC2 API and return them
 func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Entry) (ipamTypes.AllocationMap, error) {
-	n.mutex.Lock()
-	defer n.mutex.Unlock()
-
 	available := ipamTypes.AllocationMap{}
-	n.enis = map[string]eniTypes.ENI{}
 
-	n.manager.instances.ForeachInterface(n.node.InstanceID(),
+	n.mutex.Lock()
+	n.enis = map[string]eniTypes.ENI{}
+	ipamNode := n.node
+	n.mutex.Unlock()
+
+	instanceID := ipamNode.InstanceID()
+
+	n.manager.ForeachInstance(instanceID,
 		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
 			e, ok := rev.Resource.(*eniTypes.ENI)
 			if !ok {
 				return nil
 			}
-			n.enis[e.ID] = *e
 
-			if e.Number < *n.k8sObj.Spec.ENI.FirstInterfaceIndex {
+			n.mutex.Lock()
+			n.enis[e.ID] = *e
+			index := *n.k8sObj.Spec.ENI.FirstInterfaceIndex
+			n.mutex.Unlock()
+
+			if e.Number < index {
 				return nil
 			}
 
@@ -454,8 +467,12 @@ func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Ent
 			return nil
 		})
 
+	n.mutex.RLock()
+	enis := len(n.enis)
+	n.mutex.RUnlock()
+
 	// An ec2 instance has at least one ENI attached, no ENI found implies instance not found.
-	if len(n.enis) == 0 {
+	if enis == 0 {
 		scopedLog.Warning("Instance not found! Please delete corresponding ciliumnode if instance has already been deleted.")
 		return nil, fmt.Errorf("unable to retrieve ENIs")
 	}

pkg/aws/eni/instances.go
@@ -200,3 +200,11 @@ func (m *InstancesManager) UpdateENI(instanceID string, eni *eniTypes.ENI) {
 	m.instances.Update(instanceID, eniRevision)
 	m.mutex.Unlock()
 }
+
+// ForeachInstance will iterate over each instance inside `instances`, and call
+// `fn`. This function is read-locked for the entire execution.
+func (m *InstancesManager) ForeachInstance(instanceID string, fn ipamTypes.InterfaceIterator) {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	m.instances.ForeachInterface(instanceID, fn)
+}

commit 0e6d653e83954810f2748a05583eeb5c9a1f377e

eni: Fix potential deadlock
Fixes: #11825
Fixes: ef77a98444 ("eni: Fix data races and potential deadlock")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/aws/eni/node.go
@@ -447,14 +447,13 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 // ResyncInterfacesAndIPs is called to retrieve and ENIs and IPs as known to
 // the EC2 API and return them
 func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Entry) (ipamTypes.AllocationMap, error) {
+	// n.node does not need to be protected by n.mutex as it is only written to
+	// upon creation of `n`
+	instanceID := n.node.InstanceID()
 	available := ipamTypes.AllocationMap{}
 
 	n.mutex.Lock()
 	n.enis = map[string]eniTypes.ENI{}
-	ipamNode := n.node
-	n.mutex.Unlock()
-
-	instanceID := ipamNode.InstanceID()
 
 	n.manager.ForeachInstance(instanceID,
 		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
@@ -463,10 +462,8 @@ func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Ent
 				return nil
 			}
 
-			n.mutex.Lock()
 			n.enis[e.ID] = *e
 			index := *n.k8sObj.Spec.ENI.FirstInterfaceIndex
-			n.mutex.Unlock()
 
 			if e.Number < index {
 				return nil
@@ -477,10 +474,8 @@ func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Ent
 			}
 			return nil
 		})
-
-	n.mutex.RLock()
 	enis := len(n.enis)
-	n.mutex.RUnlock()
+	n.mutex.Unlock()
 
 	// An ec2 instance has at least one ENI attached, no ENI found implies instance not found.
 	if enis == 0 {