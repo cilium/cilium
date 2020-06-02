cilium/cilium

commit 4699840b0e09a6d1a5536a957365e14196573ada

ipcache: Export GetK8sMetadata
Hubble needs to access this function to implement the IPGetter interface:

https://github.com/cilium/hubble/blob/04ab72591faca62a305ce0715108876167182e04/pkg/parser/getters/getters.go#L47

Ref #9925

Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

pkg/ipcache/ipcache.go
@@ -149,7 +149,8 @@ func (ipc *IPCache) getHostIPCache(ip string) (net.IP, uint8) {
 	return ipKeyPair.IP, ipKeyPair.Key
 }
 
-func (ipc *IPCache) getK8sMetadata(ip string) *K8sMetadata {
+// GetK8sMetadata returns Kubernetes metadata for the given IP address.
+func (ipc *IPCache) GetK8sMetadata(ip string) *K8sMetadata {
 	if k8sMeta, ok := ipc.ipToK8sMetadata[ip]; ok {
 		return &k8sMeta
 	}
@@ -286,7 +287,7 @@ func (ipc *IPCache) Upsert(ip string, hostIP net.IP, hostKey uint8, k8sMeta *K8s
 func (ipc *IPCache) DumpToListenerLocked(listener IPIdentityMappingListener) {
 	for ip, identity := range ipc.ipToIdentityCache {
 		hostIP, encryptKey := ipc.getHostIPCache(ip)
-		k8sMeta := ipc.getK8sMetadata(ip)
+		k8sMeta := ipc.GetK8sMetadata(ip)
 		_, cidr, err := net.ParseCIDR(ip)
 		if err != nil {
 			endpointIP := net.ParseIP(ip)
@@ -318,7 +319,7 @@ func (ipc *IPCache) deleteLocked(ip string, source source.Source) {
 	var cidr *net.IPNet
 	cacheModification := Delete
 	oldHostIP, encryptKey := ipc.getHostIPCache(ip)
-	oldK8sMeta := ipc.getK8sMetadata(ip)
+	oldK8sMeta := ipc.GetK8sMetadata(ip)
 	var newHostIP net.IP
 	var oldIdentity *identity.NumericIdentity
 	newIdentity := cachedIdentity

pkg/ipcache/ipcache_test.go
@@ -103,7 +103,7 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 
 	cachedHostIP, _ := IPIdentityCache.getHostIPCache(endpointIP)
 	c.Assert(cachedHostIP, checker.DeepEquals, hostIP)
-	c.Assert(IPIdentityCache.getK8sMetadata(endpointIP), checker.DeepEquals, k8sMeta)
+	c.Assert(IPIdentityCache.GetK8sMetadata(endpointIP), checker.DeepEquals, k8sMeta)
 
 	newIdentity := identityPkg.NumericIdentity(69)
 	IPIdentityCache.Upsert(endpointIP, hostIP, 0, k8sMeta, Identity{

commit de1e490a06a00f3115678c152091318c2776f33b

pkg/ipcache: create a GetK8sMetadata for public access
GetK8sMetadata was supposed to be used for internal usage of the
package, making this function public requires protection against
parallel access by its internal mutex.

Fixes: 4699840b0e09 ("ipcache: Export GetK8sMetadata")
Signed-off-by: André Martins <andre@cilium.io>

pkg/ipcache/ipcache.go
@@ -162,7 +162,15 @@ func (ipc *IPCache) getHostIPCache(ip string) (net.IP, uint8) {
 }
 
 // GetK8sMetadata returns Kubernetes metadata for the given IP address.
+// The returned pointer should *never* be modified.
 func (ipc *IPCache) GetK8sMetadata(ip string) *K8sMetadata {
+	ipc.mutex.RLock()
+	defer ipc.mutex.RUnlock()
+	return ipc.getK8sMetadata(ip)
+}
+
+// getK8sMetadata returns Kubernetes metadata for the given IP address.
+func (ipc *IPCache) getK8sMetadata(ip string) *K8sMetadata {
 	if k8sMeta, ok := ipc.ipToK8sMetadata[ip]; ok {
 		return &k8sMeta
 	}
@@ -395,7 +403,7 @@ func (ipc *IPCache) DumpToListenerLocked(listener IPIdentityMappingListener) {
 			continue
 		}
 		hostIP, encryptKey := ipc.getHostIPCache(ip)
-		k8sMeta := ipc.GetK8sMetadata(ip)
+		k8sMeta := ipc.getK8sMetadata(ip)
 		_, cidr, err := net.ParseCIDR(ip)
 		if err != nil {
 			endpointIP := net.ParseIP(ip)
@@ -427,7 +435,7 @@ func (ipc *IPCache) deleteLocked(ip string, source source.Source) (namedPortsCha
 	var cidr *net.IPNet
 	cacheModification := Delete
 	oldHostIP, encryptKey := ipc.getHostIPCache(ip)
-	oldK8sMeta := ipc.GetK8sMetadata(ip)
+	oldK8sMeta := ipc.getK8sMetadata(ip)
 	var newHostIP net.IP
 	var oldIdentity *identity.NumericIdentity
 	newIdentity := cachedIdentity