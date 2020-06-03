cilium/cilium

commit 8822c1d650f0ab68c8ad448e579e89e8b8df52c1

k8s: Add CRD Identities as an identity allocator backend
The ciliumidentity CRD can act as the backing store for identity
allocation in cilium. Implementation details are hidden behind the
pkg/allocator.Backend interface. Most operations are supported but,
specifically, locking is not.

The name of the CRD is the numeric identity and the labels on the CRD
object are the security labels used by cilium. Each node using a
ciliumidentity updates the status field with it's name and a timestamp.
cilium-operator can then use the list of nodes in status to reference
count users of this identity, and to expire stale usage.

Both cilium and cilium-operator need full access to create, update and
delete these entities. cilium-operator does the garbage collection on
these objects.

Signed-off-by: Ray Bejjani <ray@isovalent.com>

pkg/option/config.go
@@ -552,6 +552,18 @@ const (
 	// PolicyTriggerInterval is the amount of time between triggers of policy
 	// updates are invoked.
 	PolicyTriggerInterval = "policy-trigger-interval"
+
+	// IdentityAllocationMode specifies what mode to use for identity
+	// allocation
+	IdentityAllocationMode = "identity-allocation-mode"
+
+	// IdentityAllocationModeKVstore enables use of a key-value store such
+	// as etcd or consul for identity allocation
+	IdentityAllocationModeKVstore = "kvstore"
+
+	// IdentityAllocationModeCRD enables use of Kubernetes CRDs for
+	// identity allocation
+	IdentityAllocationModeCRD = "crd"
 )
 
 // FQDNS variables
@@ -1113,6 +1125,10 @@ type DaemonConfig struct {
 	// PolicyTriggerInterval is the amount of time between when policy updates
 	// are triggered.
 	PolicyTriggerInterval time.Duration
+
+	// IdentityAllocationMode specifies what mode to use for identity
+	// allocation
+	IdentityAllocationMode string
 }
 
 var (
@@ -1141,6 +1157,7 @@ var (
 		EnableEndpointRoutes:         defaults.EnableEndpointRoutes,
 		AnnotateK8sNode:              defaults.AnnotateK8sNode,
 		AutoCreateCiliumNodeResource: defaults.AutoCreateCiliumNodeResource,
+		IdentityAllocationMode:       IdentityAllocationModeKVstore,
 	}
 )
 
@@ -1642,6 +1659,19 @@ func (c *DaemonConfig) Populate() {
 
 	if err := c.parseExcludedLocalAddresses(viper.GetStringSlice(ExcludeLocalAddress)); err != nil {
 		log.WithError(err).Fatalf("Unable to parse excluded local addresses")
+	}
+
+	c.IdentityAllocationMode = viper.GetString(IdentityAllocationMode)
+	switch c.IdentityAllocationMode {
+	// This is here for tests. Some call Populate without the normal init
+	case "":
+		c.IdentityAllocationMode = IdentityAllocationModeKVstore
+
+	case IdentityAllocationModeKVstore, IdentityAllocationModeCRD:
+		// c.IdentityAllocationMode is set above
+
+	default:
+		log.Fatalf("Invalid identity allocation mode %q. It must be one of %s or %s", c.IdentityAllocationMode, IdentityAllocationModeKVstore, IdentityAllocationModeCRD)
 	}
 
 	// Hidden options

daemon/daemon.go
@@ -869,7 +869,7 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 	// well known identities have already been initialized above
 	// Ignore the channel returned by this function, as we want the global
 	// identity allocator to run asynchronously.
-	cache.InitIdentityAllocator(&d)
+	cache.InitIdentityAllocator(&d, k8s.CiliumClient(), nil)
 
 	d.bootstrapClusterMesh(nodeMngr)
 

daemon/daemon_main.go
@@ -540,6 +540,9 @@ func init() {
 	flags.Duration(option.IdentityChangeGracePeriod, defaults.IdentityChangeGracePeriod, "Time to wait before using new identity on endpoint identity change")
 	option.BindEnv(option.IdentityChangeGracePeriod)
 
+	flags.String(option.IdentityAllocationMode, option.IdentityAllocationModeKVstore, "Method to use for identity allocation")
+	option.BindEnv(option.IdentityAllocationMode)
+
 	flags.String(option.IPAM, "", "Backend to use for IPAM")
 	option.BindEnv(option.IPAM)
 

operator/identity_gc.go
@@ -26,6 +26,10 @@ var (
 	// identityGCInterval is the interval in which allocator identities are
 	// attempted to be expired from the kvstore
 	identityGCInterval time.Duration
+
+	// identityAllocationMode specifies what mode to use for identity
+	// allocation
+	identityAllocationMode string
 )
 
 func startIdentityGC() {

operator/k8s_identity.go
@@ -1,0 +1,127 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/informer"
+	"github.com/cilium/cilium/pkg/k8s/types"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/sirupsen/logrus"
+
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+)
+
+var identityStore cache.Store
+
+// deleteIdentity deletes an identity. It includes the resource version and
+// will error if the object has since been changed.
+func deleteIdentity(identity *types.Identity) error {
+	err := ciliumK8sClient.CiliumV2().CiliumIdentities().Delete(
+		identity.Name,
+		&metav1.DeleteOptions{
+			Preconditions: &metav1.Preconditions{
+				UID:             &identity.UID,
+				ResourceVersion: &identity.ResourceVersion,
+			},
+		})
+	if err != nil {
+		log.WithError(err).Error("Unable to delete identity")
+	}
+
+	return err
+}
+
+// identityGCIteration is a single iteration of a garbage collection. It will
+// delete identities that have node status entries that are all older than
+// k8sIdentityHeartbeatTimeout.
+// Note: cilium-operator deletes identities in the OnDelete handler when they
+// have no nodes using them (status is empty). This generally means that
+// deletes here are for longer lived identities with no active users.
+func identityGCIteration() {
+	if identityStore == nil {
+		return
+	}
+
+nextIdentity:
+	for _, identityObject := range identityStore.List() {
+		identity, ok := identityObject.(*types.Identity)
+		if !ok {
+			log.WithField(logfields.Object, identityObject).
+				Errorf("Saw %T object while expecting k8s/types.Identity", identityObject)
+			continue
+		}
+
+		for _, heartbeat := range identity.Status.Nodes {
+			if time.Since(heartbeat.Time) < k8sIdentityHeartbeatTimeout {
+				continue nextIdentity
+			}
+		}
+
+		log.WithFields(logrus.Fields{
+			logfields.Identity: identity,
+			"nodes":            identity.Status.Nodes,
+		}).Debug("Deleting unused identity")
+		deleteIdentity(identity)
+	}
+}
+
+func startCRDIdentityGC() {
+	controller.NewManager().UpdateController("crd-identity-gc",
+		controller.ControllerParams{
+			RunInterval: identityGCInterval,
+			DoFunc: func(ctx context.Context) error {
+				identityGCIteration()
+				return nil
+			},
+		})
+}
+
+func handleIdentityUpdate(identity *types.Identity) {
+	// If no more nodes are using this identity, release the ID for reuse.
+	// If deleteIdentity fails the identity will be removed by the periodic GC.
+	if len(identity.Status.Nodes) == 0 {
+		deleteIdentity(identity)
+	}
+}
+
+func startManagingK8sIdentities() {
+	identityStore = cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
+	identityInformer := informer.NewInformerWithStore(
+		cache.NewListWatchFromClient(ciliumK8sClient.CiliumV2().RESTClient(),
+			"ciliumidentities", v1.NamespaceAll, fields.Everything()),
+		&v2.CiliumIdentity{},
+		0,
+		cache.ResourceEventHandlerFuncs{
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				if identity, ok := newObj.(*types.Identity); ok {
+					handleIdentityUpdate(identity)
+				}
+			},
+		},
+		types.ConvertToIdentity,
+		identityStore,
+	)
+
+	go identityInformer.Run(wait.NeverStop)
+}

operator/main.go
@@ -66,7 +66,9 @@ var (
 	eniParallelWorkers  int64
 	enableENI           bool
 
-	ciliumK8sClient clientset.Interface
+	k8sIdentityGCInterval       time.Duration
+	k8sIdentityHeartbeatTimeout time.Duration
+	ciliumK8sClient             clientset.Interface
 )
 
 func main() {
@@ -112,9 +114,11 @@ func init() {
 	flags.StringVar(&metricsAddress, "metrics-address", ":6942", "Address to serve Prometheus metrics")
 	flags.BoolVar(&synchronizeServices, "synchronize-k8s-services", true, "Synchronize Kubernetes services to kvstore")
 	flags.BoolVar(&synchronizeNodes, "synchronize-k8s-nodes", true, "Synchronize Kubernetes nodes to kvstore and perform CNP GC")
+	flags.DurationVar(&k8sIdentityHeartbeatTimeout, "identity-heartbeat-timeout", 15*time.Minute, "Timeout after which identity expires on lack of heartbeat")
 	flags.BoolVar(&enableCepGC, "cilium-endpoint-gc", true, "Enable CiliumEndpoint garbage collector")
 	flags.DurationVar(&ciliumEndpointGCInterval, "cilium-endpoint-gc-interval", time.Minute*30, "GC interval for cilium endpoints")
-	flags.DurationVar(&identityGCInterval, "identity-gc-interval", time.Minute*10, "GC interval for security identities")
+	flags.StringVar(&identityAllocationMode, option.IdentityAllocationMode, option.IdentityAllocationModeKVstore, "Method to use for identity allocation")
+	flags.DurationVar(&identityGCInterval, "identity-gc-interval", defaults.KVstoreLeaseTTL, "GC interval for security identities")
 	flags.DurationVar(&kvNodeGCInterval, "nodes-gc-interval", time.Minute*2, "GC interval for nodes store in the kvstore")
 	flags.Int64Var(&eniParallelWorkers, "eni-parallel-workers", 50, "Maximum number of parallel workers used by ENI allocator")
 	flags.String(option.K8sNamespaceName, "", "Name of the Kubernetes namespace in which Cilium Operator is deployed in")
@@ -166,16 +170,9 @@ func initConfig() {
 }
 
 func requiresKVstore() bool {
-	if identityGCInterval != time.Duration(0) {
-		return true
-	}
-
-	switch {
-	case synchronizeServices, synchronizeNodes:
-		return true
-	}
-
-	return false
+	return identityAllocationMode == option.IdentityAllocationModeKVstore ||
+		synchronizeServices ||
+		synchronizeNodes
 }
 
 func runOperator(cmd *cobra.Command) {
@@ -278,6 +275,14 @@ func runOperator(cmd *cobra.Command) {
 		scopedLog.Info("Connecting to kvstore...")
 		if err := kvstore.Setup(kvStore, kvStoreOpts, goopts); err != nil {
 			scopedLog.WithError(err).Fatal("Unable to setup kvstore")
+		}
+	}
+
+	if identityAllocationMode == option.IdentityAllocationModeCRD {
+		startManagingK8sIdentities()
+
+		if identityGCInterval != time.Duration(0) {
+			go startCRDIdentityGC()
 		}
 	}
 

pkg/clustermesh/clustermesh_test.go
@@ -109,7 +109,8 @@ func (s *ClusterMeshTestSuite) TestClusterMesh(c *C) {
 	defer kvstore.Close()
 
 	identity.InitWellKnownIdentities()
-	<-cache.InitIdentityAllocator(&identityAllocatorOwnerMock{})
+	// The nils are only used by k8s CRD identities. We default to kvstore.
+	<-cache.InitIdentityAllocator(&identityAllocatorOwnerMock{}, nil, nil)
 	defer cache.Close()
 
 	dir, err := ioutil.TempDir("", "multicluster")

pkg/clustermesh/services_test.go
@@ -64,7 +64,8 @@ func (s *ClusterMeshServicesTestSuite) SetUpTest(c *C) {
 	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName)
 	s.svcCache = k8s.NewServiceCache()
 	identity.InitWellKnownIdentities()
-	<-cache.InitIdentityAllocator(&identityAllocatorOwnerMock{})
+	// The nils are only used by k8s CRD identities. We default to kvstore.
+	<-cache.InitIdentityAllocator(&identityAllocatorOwnerMock{}, nil, nil)
 	dir, err := ioutil.TempDir("", "multicluster")
 	s.testDir = dir
 	c.Assert(err, IsNil)

commit 91a851f85afe44c0892ac6f4869e51f856df47c3

option: Do not permit setting kvstore type in CRD allocation mode
CRD allocation mode should mean that kvstore is not used. Before this
change, user could set identity-allocation-mode=crd and kvstore options,
which resulted in trigerring the deamon to initialize a kvstore while
using CRD as the store, which resulted in a deadlock (#11690).

Fixes: 8822c1d650f0 ("k8s: Add CRD Identities as an identity allocator backend")
Fixes: #11820
Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

pkg/option/config.go
@@ -2154,7 +2154,6 @@ func (c *DaemonConfig) Populate() {
 	c.EnableWellKnownIdentities = viper.GetBool(EnableWellKnownIdentities)
 	c.EndpointInterfaceNamePrefix = viper.GetString(EndpointInterfaceNamePrefix)
 	c.DevicePreFilter = viper.GetString(PrefilterDevice)
-	c.DisableCiliumEndpointCRD = viper.GetBool(DisableCiliumEndpointCRDName)
 	c.DisableK8sServices = viper.GetBool(DisableK8sServices)
 	c.EgressMasqueradeInterfaces = viper.GetString(EgressMasqueradeInterfaces)
 	c.EnableHostReachableServices = viper.GetBool(EnableHostReachableServices)
@@ -2212,7 +2211,6 @@ func (c *DaemonConfig) Populate() {
 	c.K8sWatcherQueueSize = uint(viper.GetInt(K8sWatcherQueueSize))
 	c.K8sWatcherEndpointSelector = viper.GetString(K8sWatcherEndpointSelector)
 	c.KeepConfig = viper.GetBool(KeepConfig)
-	c.KVStore = viper.GetString(KVStore)
 	c.KVstoreLeaseTTL = viper.GetDuration(KVstoreLeaseTTL)
 	c.KVstoreKeepAliveInterval = c.KVstoreLeaseTTL / defaults.KVstoreKeepAliveIntervalFactor
 	c.KVstorePeriodicSync = viper.GetDuration(KVstorePeriodicSync)
@@ -2355,10 +2353,6 @@ func (c *DaemonConfig) Populate() {
 		c.FixedIdentityMapping = m
 	}
 
-	if m := viper.GetStringMapString(KVStoreOpt); len(m) != 0 {
-		c.KVStoreOpt = m
-	}
-
 	if m := viper.GetStringMapString(LogOpt); len(m) != 0 {
 		c.LogOpt = m
 	}
@@ -2390,27 +2384,8 @@ func (c *DaemonConfig) Populate() {
 		log.WithError(err).Fatalf("Unable to parse excluded local addresses")
 	}
 
-	c.IdentityAllocationMode = viper.GetString(IdentityAllocationMode)
-	switch c.IdentityAllocationMode {
-	// This is here for tests. Some call Populate without the normal init
-	case "":
-		c.IdentityAllocationMode = IdentityAllocationModeKVstore
-
-	case IdentityAllocationModeKVstore, IdentityAllocationModeCRD:
-		// c.IdentityAllocationMode is set above
-
-	default:
-		log.Fatalf("Invalid identity allocation mode %q. It must be one of %s or %s", c.IdentityAllocationMode, IdentityAllocationModeKVstore, IdentityAllocationModeCRD)
-	}
-	if c.KVStore == "" {
-		if c.IdentityAllocationMode != IdentityAllocationModeCRD {
-			log.Warningf("Running Cilium with %q=%q requires identity allocation via CRDs. Changing %s to %q", KVStore, c.KVStore, IdentityAllocationMode, IdentityAllocationModeCRD)
-			c.IdentityAllocationMode = IdentityAllocationModeCRD
-		}
-		if c.DisableCiliumEndpointCRD {
-			log.Warningf("Running Cilium with %q=%q requires endpoint CRDs. Changing %s to %t", KVStore, c.KVStore, DisableCiliumEndpointCRDName, false)
-			c.DisableCiliumEndpointCRD = false
-		}
+	if err := c.populateIdentityAllocation(); err != nil {
+		log.WithError(err).Fatalf("Unable to parse identity allocation option")
 	}
 
 	switch c.IPAM {
@@ -2503,6 +2478,47 @@ func (c *DaemonConfig) populateHostServicesProtos() error {
 		default:
 			return fmt.Errorf("Protocol other than %s,%s not supported for host reachable services: %s",
 				HostServicesTCP, HostServicesUDP, hostServicesProtos[i])
+		}
+	}
+
+	return nil
+}
+
+func (c *DaemonConfig) populateIdentityAllocation() error {
+	c.DisableCiliumEndpointCRD = viper.GetBool(DisableCiliumEndpointCRDName)
+	c.IdentityAllocationMode = viper.GetString(IdentityAllocationMode)
+	c.KVStore = viper.GetString(KVStore)
+
+	if m := viper.GetStringMapString(KVStoreOpt); len(m) != 0 {
+		c.KVStoreOpt = m
+	}
+
+	switch c.IdentityAllocationMode {
+	// This is here for tests. Some call Populate without the normal init
+	case "":
+		c.IdentityAllocationMode = IdentityAllocationModeKVstore
+
+	case IdentityAllocationModeKVstore:
+		// c.IdentityAllocationMode is set above
+
+	case IdentityAllocationModeCRD:
+		if c.KVStore != "" {
+			log.Warningf("Running Cilium in CRD identity allocation mode excludes using kvstore. Unsetting %s", KVStore)
+			c.KVStore = ""
+		}
+
+	default:
+		return fmt.Errorf("Invalid identity allocation mode %q. It must be one of %s or %s", c.IdentityAllocationMode, IdentityAllocationModeKVstore, IdentityAllocationModeCRD)
+	}
+
+	if c.KVStore == "" {
+		if c.IdentityAllocationMode != IdentityAllocationModeCRD {
+			log.Warningf("Running Cilium with %q=%q requires identity allocation via CRDs. Changing %s to %q", KVStore, c.KVStore, IdentityAllocationMode, IdentityAllocationModeCRD)
+			c.IdentityAllocationMode = IdentityAllocationModeCRD
+		}
+		if c.DisableCiliumEndpointCRD {
+			log.Warningf("Running Cilium with %q=%q requires endpoint CRDs. Changing %s to %t", KVStore, c.KVStore, DisableCiliumEndpointCRDName, false)
+			c.DisableCiliumEndpointCRD = false
 		}
 	}
 

pkg/option/config_test.go
@@ -700,6 +700,151 @@ func Test_populateNodePortRange(t *testing.T) {
 	}
 }
 
+func Test_populateIdentityAllocation(t *testing.T) {
+	type want struct {
+		wantIdentityAllocationMode   string
+		wantKVStore                  string
+		wantDisableCiliumEndpointCRD bool
+		wantErr                      bool
+	}
+	tests := []struct {
+		name       string
+		want       want
+		preTestRun func()
+	}{
+		{
+			name: "no identity allocation mode, no kvstore provided, should default to CRD",
+			want: want{
+				wantIdentityAllocationMode:   IdentityAllocationModeCRD,
+				wantKVStore:                  "",
+				wantDisableCiliumEndpointCRD: false,
+				wantErr:                      false,
+			},
+			preTestRun: func() {
+				viper.Reset()
+				viper.Set(IdentityAllocationMode, "")
+				viper.Set(KVStore, "")
+			},
+		},
+		{
+			name: "no identity allocation mode specified, kvstore type provided, should set kvstore mode",
+			want: want{
+				wantIdentityAllocationMode:   IdentityAllocationModeKVstore,
+				wantKVStore:                  "etcd",
+				wantDisableCiliumEndpointCRD: false,
+				wantErr:                      false,
+			},
+			preTestRun: func() {
+				viper.Reset()
+				viper.Set(IdentityAllocationMode, "")
+				viper.Set(KVStore, "etcd")
+			},
+		},
+		{
+			name: "crd mode, kvstore type provided, should unset kvstore type",
+			want: want{
+				wantIdentityAllocationMode:   IdentityAllocationModeCRD,
+				wantKVStore:                  "",
+				wantDisableCiliumEndpointCRD: false,
+				wantErr:                      false,
+			},
+			preTestRun: func() {
+				viper.Reset()
+				viper.Set(IdentityAllocationMode, IdentityAllocationModeCRD)
+				viper.Set(KVStore, "etcd")
+			},
+		},
+		{
+			name: "crd mode provided, endpoint crd enabled, should enable endpoint crd",
+			want: want{
+				wantIdentityAllocationMode:   IdentityAllocationModeCRD,
+				wantKVStore:                  "",
+				wantDisableCiliumEndpointCRD: false,
+				wantErr:                      false,
+			},
+			preTestRun: func() {
+				viper.Reset()
+				viper.Set(IdentityAllocationMode, IdentityAllocationModeCRD)
+				viper.Set(DisableCiliumEndpointCRDName, true)
+			},
+		},
+		{
+			name: "kvstore mode, no kvstore type provided, should set crd mode",
+			want: want{
+				wantIdentityAllocationMode:   IdentityAllocationModeCRD,
+				wantKVStore:                  "",
+				wantDisableCiliumEndpointCRD: false,
+				wantErr:                      false,
+			},
+			preTestRun: func() {
+				viper.Reset()
+				viper.Set(IdentityAllocationMode, IdentityAllocationModeKVstore)
+				viper.Set(KVStore, "")
+			},
+		},
+		{
+			name: "kvstore mode, kvstore type provided, crd endpoint disabled, should be unchanged",
+			want: want{
+				wantIdentityAllocationMode:   IdentityAllocationModeKVstore,
+				wantKVStore:                  "etcd",
+				wantDisableCiliumEndpointCRD: true,
+				wantErr:                      false,
+			},
+			preTestRun: func() {
+				viper.Reset()
+				viper.Set(IdentityAllocationMode, IdentityAllocationModeKVstore)
+				viper.Set(KVStore, "etcd")
+				viper.Set(DisableCiliumEndpointCRDName, true)
+			},
+		},
+		{
+			name: "crd mode provided, should be unchanged",
+			want: want{
+				wantIdentityAllocationMode:   IdentityAllocationModeCRD,
+				wantKVStore:                  "",
+				wantDisableCiliumEndpointCRD: false,
+				wantErr:                      false,
+			},
+			preTestRun: func() {
+				viper.Reset()
+				viper.Set(IdentityAllocationMode, IdentityAllocationModeCRD)
+			},
+		},
+		{
+			name: "invalid identity allocation mode, should return error",
+			want: want{
+				wantIdentityAllocationMode:   "foo",
+				wantKVStore:                  "",
+				wantDisableCiliumEndpointCRD: false,
+				wantErr:                      true,
+			},
+			preTestRun: func() {
+				viper.Reset()
+				viper.Set(IdentityAllocationMode, "foo")
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.preTestRun()
+
+			d := &DaemonConfig{}
+			err := d.populateIdentityAllocation()
+
+			got := want{
+				wantIdentityAllocationMode:   d.IdentityAllocationMode,
+				wantKVStore:                  d.KVStore,
+				wantDisableCiliumEndpointCRD: d.DisableCiliumEndpointCRD,
+				wantErr:                      err != nil,
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DaemonConfig.populateIdentityAllocation = got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func (s *OptionSuite) TestGetDefaultMonitorQueueSize(c *C) {
 	c.Assert(getDefaultMonitorQueueSize(4), Equals, 4*defaults.MonitorQueueSizePerCPU)
 	c.Assert(getDefaultMonitorQueueSize(1000), Equals, defaults.MonitorQueueSizePerCPUMaximum)