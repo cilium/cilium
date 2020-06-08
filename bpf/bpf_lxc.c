fbf184ffa4b39c20a7ea9b5739f6ca30b7225c67
John Fastabend <john.fastabend@gmail.com>
2020-06-07 23:33:01 -0700

    cilium: encryption fixes when fib helper is not supported
    Encryption fixes, when setting the encrypt ctx->mark field we need to
    skip resetting it with the source identity. Its not needed in the
    encryption case anywasy because we already checked the endpoint is
    remote before encoding encryption signal.
    
    Next if fib lookup is not available we will discover the route at
    init time and encode it in the ENCRYPT_IFACE define. If this field
    is non-zero we should use it. Otherwise in some configurations
    where there is not a route to egress in the main routing table
    the packet will be dropped.
    
    Fixes: 86db0fde493fc ("cilium: encryption, use fib_lookup to rewrite dmac/smac")
    Fixes: f25d8b97e908 ("bpf: Preserve source identity for hairpin via stack")
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -356,15 +356,17 @@ pass_to_stack:
 #ifdef IP_POOLS
 		set_encrypt_dip(ctx, tunnel_endpoint);
 #endif
+	} else
+#endif
+#endif
+	{
+		/* Always encode the source identity when passing to the stack. If the
+		 * stack hairpins the packet back to a local endpoint the source
+		 * identity can still be derived even if SNAT is performed by a
+		 * component such as portmap */
+		ctx->mark |= MARK_MAGIC_IDENTITY;
+		set_identity_mark(ctx, SECLABEL);
 	}
-#endif
-#endif
-	/* Always encode the source identity when passing to the stack. If the
-	 * stack hairpins the packet back to a local endpoint the source
-	 * identity can still be derived even if SNAT is performed by a
-	 * component such as portmap */
-	ctx->mark |= MARK_MAGIC_IDENTITY;
-	set_identity_mark(ctx, SECLABEL);
 
 	return CTX_ACT_OK;
 }
@@ -708,16 +710,17 @@ pass_to_stack:
 #ifdef IP_POOLS
 		set_encrypt_dip(ctx, tunnel_endpoint);
 #endif
+	} else
+#endif
+#endif
+	{
+		/* Always encode the source identity when passing to the stack. If the
+		 * stack hairpins the packet back to a local endpoint the source
+		 * identity can still be derived even if SNAT is performed by a
+		 * component such as portmap */
+		ctx->mark |= MARK_MAGIC_IDENTITY;
+		set_identity_mark(ctx, SECLABEL);
 	}
-#endif
-#endif
-
-	/* Always encode the source identity when passing to the stack. If the
-	 * stack hairpins the packet back to a local endpoint the source
-	 * identity can still be derived even if SNAT is performed by a
-	 * component such as portmap */
-	ctx->mark |= MARK_MAGIC_IDENTITY;
-	set_identity_mark(ctx, SECLABEL);
 
 	cilium_dbg_capture(ctx, DBG_CAPTURE_DELIVERY, 0);
 	return CTX_ACT_OK;
