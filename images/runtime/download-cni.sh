e091612061d96fe9c8851f570f157855367bef62
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-06-15 13:50:58 +0100

    build: New runtime image with multi-platform support
    Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

---
+++ images/runtime/download-cni.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright 2017-2020 Authors of Cilium
+# SPDX-License-Identifier: Apache-2.0
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+source /tmp/cni-version.sh
+
+for arch in amd64 arm64 ; do
+  curl --fail --show-error --silent --location "https://github.com/containernetworking/plugins/releases/download/v${cni_version}/cni-plugins-linux-${arch}-v${cni_version}.tgz" --output "/tmp/cni-${arch}.tgz"
+  printf "%s %s" "${cni_sha512[${arch}]}" "/tmp/cni-${arch}.tgz" | sha512sum -c
+  mkdir -p "/out/linux/${arch}/bin"
+  tar -C "/out/linux/${arch}/bin" -xf "/tmp/cni-${arch}.tgz" ./loopback
+done
+
+strip /out/linux/amd64/bin/loopback
+aarch64-linux-gnu-strip /out/linux/arm64/bin/loopback
