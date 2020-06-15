6dcddc00b3d7668e4d26b8962819a7f60100f85c
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-06-15 13:51:02 +0100

    build: New builder image supporting cross-compilation
    Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

---
+++ images/builder/install-protoc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright 2017-2020 Authors of Cilium
+# SPDX-License-Identifier: Apache-2.0
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+protoc_version="3.12.3"
+
+curl --fail --show-error --silent --location \
+  "https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protoc-${protoc_version}-linux-x86_64.zip" \
+    --output /tmp/protoc.zip
+
+unzip /tmp/protoc.zip -x readme.txt -d /usr/local
+
+# correct permissions for others
+chmod o+rx /usr/local/bin/protoc
+chmod o+rX -R /usr/local/include/google/protobuf
