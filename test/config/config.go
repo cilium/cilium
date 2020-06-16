a675f2f77acaf0349e6a1eaf66f2e79ff34329ab
Maciej Kwiek <maciej@isovalent.com>
2020-06-16 14:41:29 +0200

    test: Parse only Cilium test flags in our code
    [ upstream commit 30fa8206b4ce27b40c688e2002a22d4ca89b50ad ]
    
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- test/config/config.go
+++ test/config/config.go
@@ -16,6 +16,8 @@ package config
 
 import (
 	"flag"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -52,37 +54,48 @@ var CiliumTestConfig = CiliumTestConfigType{}
 
 // ParseFlags parses commandline flags relevant to testing.
 func (c *CiliumTestConfigType) ParseFlags() {
-	flag.BoolVar(&c.Reprovision, "cilium.provision", true,
+	flagset := flag.NewFlagSet("cilium", flag.PanicOnError)
+	flagset.BoolVar(&c.Reprovision, "cilium.provision", true,
 		"Provision Vagrant boxes and Cilium before running test")
-	flag.BoolVar(&c.HoldEnvironment, "cilium.holdEnvironment", false,
+	flagset.BoolVar(&c.HoldEnvironment, "cilium.holdEnvironment", false,
 		"On failure, hold the environment in its current state")
-	flag.BoolVar(&c.PassCLIEnvironment, "cilium.passCLIEnvironment", false,
+	flagset.BoolVar(&c.PassCLIEnvironment, "cilium.passCLIEnvironment", false,
 		"Pass the environment invoking ginkgo, including PATH, to subcommands")
-	flag.BoolVar(&c.SkipLogGathering, "cilium.skipLogs", false,
+	flagset.BoolVar(&c.SkipLogGathering, "cilium.skipLogs", false,
 		"skip gathering logs if a test fails")
-	flag.StringVar(&c.SSHConfig, "cilium.SSHConfig", "",
+	flagset.StringVar(&c.SSHConfig, "cilium.SSHConfig", "",
 		"Specify a custom command to fetch SSH configuration (eg: 'vagrant ssh-config')")
-	flag.BoolVar(&c.ShowCommands, "cilium.showCommands", false,
+	flagset.BoolVar(&c.ShowCommands, "cilium.showCommands", false,
 		"Output which commands are ran to stdout")
-	flag.StringVar(&c.TestScope, "cilium.testScope", "",
+	flagset.StringVar(&c.TestScope, "cilium.testScope", "",
 		"Specifies scope of test to be ran (k8s, Nightly, runtime)")
-	flag.StringVar(&c.CiliumImage, "cilium.image", "",
+	flagset.StringVar(&c.CiliumImage, "cilium.image", "",
 		"Specifies which image of cilium to use during tests")
-	flag.StringVar(&c.CiliumOperatorImage, "cilium.operator-image", "",
+	flagset.StringVar(&c.CiliumOperatorImage, "cilium.operator-image", "",
 		"Specifies which image of cilium-operator to use during tests")
-	flag.StringVar(&c.HubbleRelayImage, "cilium.hubble-relay-image", "",
+	flagset.StringVar(&c.HubbleRelayImage, "cilium.hubble-relay-image", "",
 		"Specifies which image of hubble-relay to use during tests")
-	flag.BoolVar(&c.ProvisionK8s, "cilium.provision-k8s", true,
+	flagset.BoolVar(&c.ProvisionK8s, "cilium.provision-k8s", true,
 		"Specifies whether Kubernetes should be deployed and installed via kubeadm or not")
-	flag.DurationVar(&c.Timeout, "cilium.timeout", 24*time.Hour,
+	flagset.DurationVar(&c.Timeout, "cilium.timeout", 24*time.Hour,
 		"Specifies timeout for test run")
-	flag.StringVar(&c.Kubeconfig, "cilium.kubeconfig", "",
+	flagset.StringVar(&c.Kubeconfig, "cilium.kubeconfig", "",
 		"Kubeconfig to be used for k8s tests")
-	flag.StringVar(&c.Registry, "cilium.registry", "", "docker registry hostname for Cilium image")
-	flag.BoolVar(&c.Benchmarks, "cilium.benchmarks", false,
+	flagset.StringVar(&c.Registry, "cilium.registry", "", "docker registry hostname for Cilium image")
+	flagset.BoolVar(&c.Benchmarks, "cilium.benchmarks", false,
 		"Specifies benchmark tests should be run which may increase test time")
-	flag.BoolVar(&c.Multinode, "cilium.multinode", true,
+	flagset.BoolVar(&c.Multinode, "cilium.multinode", true,
 		"Enable tests across multiple nodes. If disabled, such tests may silently pass")
-	flag.BoolVar(&c.RunQuarantined, "cilium.runQuarantined", false,
+	flagset.BoolVar(&c.RunQuarantined, "cilium.runQuarantined", false,
 		"Run tests that are under quarantine.")
+
+	args := make([]string, 0, len(os.Args))
+	for index, flag := range os.Args {
+		if strings.Contains(flag, "-cilium") {
+			args = append(args, flag)
+			os.Args[index] = ""
+		}
+	}
+
+	flagset.Parse(args)
 }
