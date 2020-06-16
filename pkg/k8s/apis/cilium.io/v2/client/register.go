dd93090f6d5da0b21355412a2d2b80fa59a2ef7d
André Martins <andre@cilium.io>
2020-06-16 17:36:49 +0200

    pkg/k8s: replace init with specific functions
    [ upstream commit ae19a9d402dc7d243d99e4b80b00f240664a77a4 ]
    
    Having a init function to initialize all structures does not initialize
    the different fields of 'CNPCRV' in case this variable is accessed
    outside the 'v2/client' package. Replacing the 'init' function with
    dedicated functions that initialize those fields allows 'CNPCRV' to have
    the fields rightfully initialized.
    
    Signed-off-by: André Martins <andre@cilium.io>
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- pkg/k8s/apis/cilium.io/v2/client/register.go
+++ pkg/k8s/apis/cilium.io/v2/client/register.go
@@ -899,7 +899,7 @@ var (
 		},
 	}
 
-	EndpointSelector = *LabelSelector.DeepCopy()
+	EndpointSelector = initEndpointSelector()
 	NodeSelector     = *LabelSelector.DeepCopy()
 
 	IngressRule = apiextensionsv1beta1.JSONSchemaProps{
@@ -1268,7 +1268,7 @@ var (
 					"secret",
 				},
 			},
-			"rules": L7Rules,
+			"rules": initPortRule(),
 		},
 	}
 
@@ -1488,7 +1488,7 @@ var (
 					Schema: &EgressRule,
 				},
 			},
-			"endpointSelector": EndpointSelector,
+			"endpointSelector": initRuleEndpointSelector(),
 			"ingress": {
 				Description: "Ingress is a list of IngressRule which are enforced at ingress. " +
 					"If omitted or empty, this rule does not apply at ingress.",
@@ -1515,7 +1515,7 @@ var (
 		Description: "Service wraps around selectors for services",
 		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
 			"k8sService":         K8sServiceNamespace,
-			"k8sServiceSelector": ServiceSelector,
+			"k8sServiceSelector": initK8sServiceSelector(),
 		},
 	}
 
@@ -1533,7 +1533,7 @@ var (
 		},
 	}
 
-	spec = *Rule.DeepCopy()
+	spec = initSpec()
 
 	specs = apiextensionsv1beta1.JSONSchemaProps{
 		Description: "Specs is a list of desired Cilium specific rule specification.",
@@ -1544,26 +1544,37 @@ var (
 	}
 )
 
-func init() {
-	EndpointSelector.Description = "EndpointSelector is a wrapper for k8s LabelSelector."
+func initEndpointSelector() apiextensionsv1beta1.JSONSchemaProps {
+	es := LabelSelector.DeepCopy()
+	es.Description = "EndpointSelector is a wrapper for k8s LabelSelector."
+	return *es
+}
 
-	portRuleProps := PortRule.Properties["rules"]
+func initPortRule() apiextensionsv1beta1.JSONSchemaProps {
+	portRuleProps := L7Rules.DeepCopy()
 	portRuleProps.Description = "Rules is a list of additional port level rules which must be " +
 		"met in order for the PortRule to allow the traffic. If omitted or empty, " +
 		"no layer 7 rules are enforced."
-	PortRule.Properties["rules"] = portRuleProps
+	return *portRuleProps
+}
 
-	ruleProps := Rule.Properties["endpointSelector"]
+func initRuleEndpointSelector() apiextensionsv1beta1.JSONSchemaProps {
+	ruleProps := EndpointSelector.DeepCopy()
 	ruleProps.Description = "EndpointSelector selects all endpoints which should be subject " +
 		"to this rule. Cannot be empty if nodeSelector is empty."
-	Rule.Properties["endpointSelector"] = ruleProps
+	return *ruleProps
+}
 
-	serviceProps := Service.Properties["k8sServiceSelector"]
+func initK8sServiceSelector() apiextensionsv1beta1.JSONSchemaProps {
+	serviceProps := ServiceSelector.DeepCopy()
 	serviceProps.Description = "K8sServiceSelector selects services by k8s labels. " +
 		"Not supported yet"
-	Service.Properties["k8sServiceSelector"] = serviceProps
+	return *serviceProps
+}
 
+func initSpec() apiextensionsv1beta1.JSONSchemaProps {
+	spec := Rule.DeepCopy()
 	spec.Description = "Spec is the desired Cilium specific rule specification."
 	spec.Type = "object"
-
+	return *spec
 }
