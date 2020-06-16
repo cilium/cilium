cilium/cilium

commit 9b0ae85b5fd3c3142a25cc602fd155480ecc8d88

k8s: Fix CCNP for host policies
This commit fixes the CCNP parsing to accept NodeSelectors, used to
define host policies. NodeSelectors are only accepted in CCNP resources.
Because host policies are never limited to a namespace, it would be
misleading to accept NodeSelectors in CNP resources.

This commit also adds a new CCNP example of host policies in the same
directory as the existing JSON example.

Fixes: f9c205d ("pkg/policy: Host network policies")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/k8s/apis/cilium.io/v2/client/register.go
@@ -54,7 +54,7 @@ const (
 
 	// CustomResourceDefinitionSchemaVersion is semver-conformant version of CRD schema
 	// Used to determine if CRD needs to be updated in cluster
-	CustomResourceDefinitionSchemaVersion = "1.19"
+	CustomResourceDefinitionSchemaVersion = "1.20"
 
 	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
 	CustomResourceDefinitionSchemaVersionKey = "io.cilium.k8s.crd.schema.version"
@@ -182,6 +182,27 @@ func createCCNPCRD(clientset apiextensionsclient.Interface) error {
 		CRDName = CustomResourceDefinitionPluralName + "." + SchemeGroupVersion.Group
 	)
 
+	CCNPCRV := CNPCRV.DeepCopy()
+	clusterwideSpec := CCNPCRV.OpenAPIV3Schema.Properties["spec"]
+	clusterwideSpec.Required = nil
+	clusterwideSpec.OneOf = []apiextensionsv1beta1.JSONSchemaProps{
+		{
+			Required: []string{"endpointSelector"},
+		},
+		{
+			Required: []string{"nodeSelector"},
+		},
+	}
+	CCNPCRV.OpenAPIV3Schema.Properties["spec"] = clusterwideSpec
+	CCNPCRV.OpenAPIV3Schema.Properties["specs"] = apiextensionsv1beta1.JSONSchemaProps{
+		Description: "Specs is a list of desired Cilium specific rule specification.",
+		Type:        "array",
+		Items: &apiextensionsv1beta1.JSONSchemaPropsOrArray{
+			Schema: &clusterwideSpec,
+		},
+	}
+	CCNPCRV.OpenAPIV3Schema.Properties["NodeSelector"] = NodeSelector
+
 	res := &apiextensionsv1beta1.CustomResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: CRDName,
@@ -202,7 +223,7 @@ func createCCNPCRD(clientset apiextensionsclient.Interface) error {
 				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
 			},
 			Scope:      apiextensionsv1beta1.ClusterScoped,
-			Validation: &CNPCRV,
+			Validation: CCNPCRV,
 		},
 	}
 	// Kubernetes < 1.12 does not support having the field Type set in the root
@@ -877,7 +898,9 @@ var (
 			},
 		},
 	}
+
 	EndpointSelector = *LabelSelector.DeepCopy()
+	NodeSelector     = *LabelSelector.DeepCopy()
 
 	IngressRule = apiextensionsv1beta1.JSONSchemaProps{
 		Type: "object",
@@ -1449,14 +1472,7 @@ var (
 			"ingress and egress, both ingress and egress side have to either specifically allow " +
 			"the connection or one side has to be omitted.\n\nEither ingress, egress, or both " +
 			"can be provided. If both ingress and egress are omitted, the rule has no effect.",
-		OneOf: []apiextensionsv1beta1.JSONSchemaProps{
-			{
-				Required: []string{"endpointSelector"},
-			},
-			{
-				Required: []string{"nodeSelector"},
-			},
-		},
+		Required: []string{"endpointSelector"},
 		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
 			"Description": {
 				Description: "Description is a free form string, it can be used by the creator " +
@@ -1473,7 +1489,6 @@ var (
 				},
 			},
 			"endpointSelector": EndpointSelector,
-			"nodeSelector":     EndpointSelector,
 			"ingress": {
 				Description: "Ingress is a list of IngressRule which are enforced at ingress. " +
 					"If omitted or empty, this rule does not apply at ingress.",
@@ -1542,11 +1557,6 @@ func init() {
 	ruleProps.Description = "EndpointSelector selects all endpoints which should be subject " +
 		"to this rule. Cannot be empty if nodeSelector is empty."
 	Rule.Properties["endpointSelector"] = ruleProps
-
-	ruleProps = Rule.Properties["nodeSelector"]
-	ruleProps.Description = "NodeSelector selects all nodes which should be subject " +
-		"to this rule. Cannot be empty if endpointSelector is empty."
-	Rule.Properties["nodeSelector"] = ruleProps
 
 	serviceProps := Service.Properties["k8sServiceSelector"]
 	serviceProps.Description = "K8sServiceSelector selects services by k8s labels. " +

examples/policies/l3/host/host-policy.yaml
@@ -1,0 +1,29 @@
+apiVersion: "cilium.io/v2"
+kind: CiliumClusterwideNetworkPolicy
+description: "Allow ports for kube-dns, kube-api, Cilium, and SSH, as well as queries to Google's DNS on UDP/53 only"
+metadata:
+  name: "allow-google-dns-on-udp-53-only"
+spec:
+  nodeSelector:
+    matchLabels:
+      {}
+  ingress:
+  - toPorts:
+    - ports:
+      - port: "6443"
+        protocol: TCP
+      - port: "22"
+        protocol: TCP
+  egress:
+  - toPorts:
+    - ports:
+      - port: "4240"
+        protocol: TCP
+      - port: "8080"
+        protocol: TCP
+  - toCIDR:
+    - "8.8.0.0/16"
+    toPorts:
+    - ports:
+      - port: "53"
+        protocol: UDP

pkg/k8s/apis/cilium.io/utils/utils.go
@@ -101,7 +101,7 @@ func getEndpointSelector(namespace string, labelSelector *slim_metav1.LabelSelec
 }
 
 func parseToCiliumIngressRule(namespace string, inRule, retRule *api.Rule) {
-	matchesInit := retRule.EndpointSelector.HasKey(podInitLbl)
+	matchesInit := matchesPodInit(retRule.EndpointSelector)
 
 	if inRule.Ingress != nil {
 		retRule.Ingress = make([]api.IngressRule, len(inRule.Ingress))
@@ -145,7 +145,7 @@ func parseToCiliumIngressRule(namespace string, inRule, retRule *api.Rule) {
 }
 
 func parseToCiliumEgressRule(namespace string, inRule, retRule *api.Rule) {
-	matchesInit := retRule.EndpointSelector.HasKey(podInitLbl)
+	matchesInit := matchesPodInit(retRule.EndpointSelector)
 
 	if inRule.Egress != nil {
 		retRule.Egress = make([]api.EgressRule, len(inRule.Egress))
@@ -204,6 +204,13 @@ func parseToCiliumEgressRule(namespace string, inRule, retRule *api.Rule) {
 	}
 }
 
+func matchesPodInit(epSelector api.EndpointSelector) bool {
+	if epSelector.LabelSelector == nil {
+		return false
+	}
+	return epSelector.HasKey(podInitLbl)
+}
+
 // namespacesAreValid checks the set of namespaces from a rule returns true if
 // they are not specified, or if they are specified and match the namespace
 // where the rule is being inserted.
@@ -240,6 +247,8 @@ func ParseToCiliumRule(namespace, name string, uid types.UID, r *api.Rule) *api.
 			}
 			retRule.EndpointSelector.AddMatch(podPrefixLbl, namespace)
 		}
+	} else if r.NodeSelector.LabelSelector != nil {
+		retRule.NodeSelector = api.NewESFromK8sLabelSelector("", r.NodeSelector.LabelSelector)
 	}
 
 	parseToCiliumIngressRule(namespace, r, retRule)

pkg/k8s/apis/cilium.io/v2/types.go
@@ -260,7 +260,6 @@ func (r *CiliumNetworkPolicy) Parse() (api.Rules, error) {
 	if r.Spec != nil {
 		if err := r.Spec.Sanitize(); err != nil {
 			return nil, fmt.Errorf("Invalid CiliumNetworkPolicy spec: %s", err)
-
 		}
 		cr := k8sCiliumUtils.ParseToCiliumRule(namespace, name, uid, r.Spec)
 		retRules = append(retRules, cr)

commit eb8ec18d0b4440f558837af0a0a85479c590094e

cilium/cmd: use right CCNP validation
The CCNP validation is different from the CNP validation so we need to
validate the CCNP with the right schema validation.

Fixes: 9b0ae85b5fd3 ("k8s: Fix CCNP for host policies")
Signed-off-by: André Martins <andre@cilium.io>

cilium/cmd/preflight_k8s_valid_cnp.go
@@ -86,11 +86,11 @@ func validateCNPs() error {
 
 	ctx, initCancel := context.WithTimeout(context.Background(), validateK8sPoliciesTimeout)
 	defer initCancel()
-	cnpErr := validateNPResources(ctx, apiExtensionsClient, "ciliumnetworkpolicies", "CiliumNetworkPolicy")
+	cnpErr := validateNPResources(ctx, apiExtensionsClient, &v2_client.CNPCRV, "ciliumnetworkpolicies", "CiliumNetworkPolicy")
 
 	ctx, initCancel2 := context.WithTimeout(context.Background(), validateK8sPoliciesTimeout)
 	defer initCancel2()
-	ccnpErr := validateNPResources(ctx, apiExtensionsClient, "ciliumclusterwidenetworkpolicies", "CiliumClusterwideNetworkPolicy")
+	ccnpErr := validateNPResources(ctx, apiExtensionsClient, v2_client.CCNPCRV, "ciliumclusterwidenetworkpolicies", "CiliumClusterwideNetworkPolicy")
 
 	if cnpErr != nil {
 		return cnpErr
@@ -102,7 +102,7 @@ func validateCNPs() error {
 	return nil
 }
 
-func validateNPResources(ctx context.Context, apiExtensionsClient *apiextensionsclient.Clientset, name, shortName string) error {
+func validateNPResources(ctx context.Context, apiExtensionsClient apiextensionsclient.Interface, crv *v1beta1.CustomResourceValidation, name, shortName string) error {
 	// check if the crd is installed at all
 	_, err := apiExtensionsClient.ApiextensionsV1beta1().CustomResourceDefinitions().Get(ctx, name+"."+ciliumGroup, metav1.GetOptions{})
 	switch {
@@ -115,7 +115,7 @@ func validateNPResources(ctx context.Context, apiExtensionsClient *apiextensions
 
 	var internal apiextensionsinternal.CustomResourceValidation
 	err = v1beta1.Convert_v1beta1_CustomResourceValidation_To_apiextensions_CustomResourceValidation(
-		&v2_client.CNPCRV,
+		crv,
 		&internal,
 		nil,
 	)
@@ -127,8 +127,11 @@ func validateNPResources(ctx context.Context, apiExtensionsClient *apiextensions
 		return err
 	}
 
-	var policyErr error
-	var cnps unstructured.UnstructuredList
+	var (
+		policyErr error
+		cnps      unstructured.UnstructuredList
+		cnpName   string
+	)
 	for {
 		opts := metav1.ListOptions{
 			Limit:    25,
@@ -148,7 +151,11 @@ func validateNPResources(ctx context.Context, apiExtensionsClient *apiextensions
 		}
 
 		for _, cnp := range cnps.Items {
-			cnpName := fmt.Sprintf("%s/%s", cnp.GetNamespace(), cnp.GetName())
+			if cnp.GetNamespace() != "" {
+				cnpName = fmt.Sprintf("%s/%s", cnp.GetNamespace(), cnp.GetName())
+			} else {
+				cnpName = cnp.GetName()
+			}
 			if errs := validation.ValidateCustomResource(nil, &cnp, validator); len(errs) > 0 {
 				log.Errorf("Validating %s '%s': unexpected validation error: %s",
 					shortName, cnpName, errs.ToAggregate())

pkg/k8s/apis/cilium.io/v2/client/register.go
@@ -182,27 +182,6 @@ func createCCNPCRD(clientset apiextensionsclient.Interface) error {
 		CRDName = CustomResourceDefinitionPluralName + "." + SchemeGroupVersion.Group
 	)
 
-	CCNPCRV := CNPCRV.DeepCopy()
-	clusterwideSpec := CCNPCRV.OpenAPIV3Schema.Properties["spec"]
-	clusterwideSpec.Required = nil
-	clusterwideSpec.OneOf = []apiextensionsv1beta1.JSONSchemaProps{
-		{
-			Required: []string{"endpointSelector"},
-		},
-		{
-			Required: []string{"nodeSelector"},
-		},
-	}
-	CCNPCRV.OpenAPIV3Schema.Properties["spec"] = clusterwideSpec
-	CCNPCRV.OpenAPIV3Schema.Properties["specs"] = apiextensionsv1beta1.JSONSchemaProps{
-		Description: "Specs is a list of desired Cilium specific rule specification.",
-		Type:        "array",
-		Items: &apiextensionsv1beta1.JSONSchemaPropsOrArray{
-			Schema: &clusterwideSpec,
-		},
-	}
-	CCNPCRV.OpenAPIV3Schema.Properties["NodeSelector"] = NodeSelector
-
 	res := &apiextensionsv1beta1.CustomResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: CRDName,
@@ -644,6 +623,8 @@ func getFloat64(f float64) *float64 {
 }
 
 var (
+	CCNPCRV = initCCNPCRV()
+
 	// cepCRV is a minimal validation for CEP objects. Since only the agent is
 	// creating them, it is better to be permissive and have some data, if buggy,
 	// than to have no data in k8s.
@@ -1577,4 +1558,30 @@ func initSpec() apiextensionsv1beta1.JSONSchemaProps {
 	spec.Description = "Spec is the desired Cilium specific rule specification."
 	spec.Type = "object"
 	return *spec
+}
+
+// initCCNPCRV creates the CCNP validation based on the CNP.
+func initCCNPCRV() *apiextensionsv1beta1.CustomResourceValidation {
+	ccnpCRV := CNPCRV.DeepCopy()
+	clusterwideSpec := ccnpCRV.OpenAPIV3Schema.Properties["spec"]
+	clusterwideSpec.Required = nil
+	clusterwideSpec.OneOf = []apiextensionsv1beta1.JSONSchemaProps{
+		{
+			Required: []string{"endpointSelector"},
+		},
+		{
+			Required: []string{"nodeSelector"},
+		},
+	}
+	ccnpCRV.OpenAPIV3Schema.Properties["spec"] = clusterwideSpec
+	ccnpCRV.OpenAPIV3Schema.Properties["specs"] = apiextensionsv1beta1.JSONSchemaProps{
+		Description: "Specs is a list of desired Cilium specific rule specification.",
+		Type:        "array",
+		Items: &apiextensionsv1beta1.JSONSchemaPropsOrArray{
+			Schema: &clusterwideSpec,
+		},
+	}
+	ccnpCRV.OpenAPIV3Schema.Properties["NodeSelector"] = NodeSelector
+
+	return ccnpCRV
 }