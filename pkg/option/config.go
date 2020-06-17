cilium/cilium

commit e4e83e80128a373eaab2807ed77db92a1342fba3

daemon: Allow kvstore to be unconfigured
If the kvstore is not configured, then we rely upon the identity
allocation going through the k8s CRD. In this case, don't fatal out just
because there's no kvstore. Warn the user and override their options
with sane defaults.

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/option/config.go
@@ -1689,6 +1689,16 @@ func (c *DaemonConfig) Populate() {
 	default:
 		log.Fatalf("Invalid identity allocation mode %q. It must be one of %s or %s", c.IdentityAllocationMode, IdentityAllocationModeKVstore, IdentityAllocationModeCRD)
 	}
+	if c.KVStore == "" {
+		if c.IdentityAllocationMode != IdentityAllocationModeCRD {
+			log.Warningf("Running Cilium with %q=%q requires identity allocation via CRDs. Changing %s to %q", KVStore, c.KVStore, IdentityAllocationMode, IdentityAllocationModeCRD)
+			c.IdentityAllocationMode = IdentityAllocationModeCRD
+		}
+		if c.DisableCiliumEndpointCRD {
+			log.Warningf("Running Cilium with %q=%q requires endpoint CRDs. Changing %s to %t", KVStore, c.KVStore, DisableCiliumEndpointCRDName, false)
+			c.DisableCiliumEndpointCRD = false
+		}
+	}
 
 	// Hidden options
 	c.ConfigFile = viper.GetString(ConfigFile)

daemon/daemon_main.go
@@ -1358,7 +1358,11 @@ func runDaemon() {
 	// subsystem as well in parallel so caches will start to be synchronized
 	// with k8s.
 	k8sCachesSynced := d.initK8sSubsystem()
-	d.initKVStore()
+	if option.Config.KVStore == "" {
+		log.Info("Skipping kvstore configuration")
+	} else {
+		d.initKVStore()
+	}
 
 	// Wait only for certain caches, but not all!
 	// (Check Daemon.initK8sSubsystem() for more info)

daemon/daemon_test.go
@@ -78,6 +78,8 @@ type DaemonSuite struct {
 
 func TestMain(m *testing.M) {
 	option.Config.Populate()
+	option.Config.IdentityAllocationMode = option.IdentityAllocationModeKVstore
+
 	time.Local = time.UTC
 	tempRunDir, err := ioutil.TempDir("", "cilium-test-run")
 	if err != nil {

daemon/status.go
@@ -383,7 +383,11 @@ func (d *Daemon) startStatusCollector() {
 		{
 			Name: "kvstore",
 			Probe: func(ctx context.Context) (interface{}, error) {
-				return kvstore.Client().Status()
+				if option.Config.KVStore == "" {
+					return models.StatusStateDisabled, nil
+				} else {
+					return kvstore.Client().Status()
+				}
 			},
 			OnStatusUpdate: func(status status.Status) {
 				var msg string

commit 617582a8c3250dbf0938b647c30797c981accc41

pkg/option: disable K8sEventHandover if Cilium is running without KVStore
Running Cilium with K8sEventHandover and without a KVStore configured
causes it on not being able to delete any CNP after being created.

Fixes: e4e83e80128a ("daemon: Allow kvstore to be unconfigured")
Signed-off-by: André Martins <andre@cilium.io>

pkg/option/config.go
@@ -2477,6 +2477,10 @@ func (c *DaemonConfig) Populate() {
 			log.Warningf("Running Cilium with %q=%q requires endpoint CRDs. Changing %s to %t", KVStore, c.KVStore, DisableCiliumEndpointCRDName, false)
 			c.DisableCiliumEndpointCRD = false
 		}
+		if c.K8sEventHandover {
+			log.Warningf("Running Cilium with %q=%q requires KVStore capability. Changing %s to %t", KVStore, c.KVStore, K8sEventHandover, false)
+			c.K8sEventHandover = false
+		}
 	}
 
 	switch c.IPAM {