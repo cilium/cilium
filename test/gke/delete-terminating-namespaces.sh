6be4c278b180f9f9517acc6e2b36a0583fa9cef4
Jarno Rajahalme <jarno@covalent.io>
2020-06-16 19:08:02 -0700

    test: Make GKE delete namespaces script more portable and note it in docs
    [ upstream commit abea6fcea87dd65252f4c3dd0f1b24942bc7f247 ]
    
    Basic regular expressions in sed do not support '+', while the command
    line arguments for extended regular expressions are different for GNU
    sed and Mac OSX. Use (unquoted) '*' instead, as there is no harm
    replacing '[]' with '[]'. This form work both on Linux and Mac OSX.
    
    Add a note to the e2e testing docs so that users know to run the
    script if namespaces get stuck at 'Terminating'.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- test/gke/delete-terminating-namespaces.sh
+++ test/gke/delete-terminating-namespaces.sh
@@ -2,9 +2,10 @@
 
 # because of https://github.com/kubernetes/kubernetes/issues/60807 , we may end up with garbage terminating
 # namespaces leftovers after tests. This is a hacky workaround
+# '\[[^]]*\]' below matches a pair of square brackets with anything in between, not containing a closing square bracket
 kubectl get ns | \
 	awk '/Terminating/ {print $1}' | \
-   	xargs -n 1 bash -c 'if [ "$#" -ne 1 ]; then exit 0; fi; kubectl get ns -o json $1 | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" | kubectl replace --raw /api/v1/namespaces/$1/finalize -f -' -
+   	xargs -n 1 bash -c 'if [ "$#" -ne 1 ]; then exit 0; fi; kubectl get ns -o json $1 | tr -d "\n" | sed "s/\"finalizers\": \[[^]]*\]/\"finalizers\": []/" | kubectl replace --raw /api/v1/namespaces/$1/finalize -f -' -
 
 # Note: This can be removed once GKE stops having the issue with namespaces no
 # deleting.
