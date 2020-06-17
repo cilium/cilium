650181798b4cab857909c26c6325b8472de7ea4f
Maciej Kwiek <maciej@isovalent.com>
2020-06-17 14:21:28 +0200

    ci: parametrize jenkinsfile to accept k8s version
    This change will allow developers to focus-test on any supported
    k8s-kernel version pair.
    
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

--- test/get-gh-comment-info.py
+++ test/get-gh-comment-info.py
@@ -3,9 +3,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('ghcomment', type=str) # this is for test-me-please phrases
 parser.add_argument('--focus', type=str, default="")
-parser.add_argument('--version', type=str, default="")
+parser.add_argument('--kernel_version', type=str, default="")
+parser.add_argument('--k8s_version', type=str, default="")
 parser.add_argument('--retrieve', type=str, default="focus")
 
 args = parser.parse_args()
 
-print args.__dict__[args.retrieve]
+print(args.__dict__[args.retrieve])
