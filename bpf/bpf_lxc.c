c4ad961eaecad42600caae560bc118eb9edf2913
Daniel Borkmann <daniel@iogearbox.net>
2020-06-18 11:50:19 +0200

    ci test only
    This reverts commit dfa8bb8ab3f04f555a082aa07875b1501390b127.

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -107,8 +107,7 @@ static __always_inline int ipv6_l3_from_lxc(struct __ctx_buff *ctx,
 				return ret;
 		}
 
-		if ((svc = lb6_lookup_service(&key)) != NULL &&
-		    lb6_svc_needs_lxc_xlation(svc)) {
+		if ((svc = lb6_lookup_service(&key)) != NULL) {
 			ret = lb6_local(get_ct_map6(tuple), ctx, l3_off, l4_off,
 					&csum_off, &key, tuple, svc, &ct_state_new);
 			if (IS_ERR(ret))
@@ -474,8 +473,7 @@ static __always_inline int handle_ipv4_from_lxc(struct __ctx_buff *ctx,
 				return ret;
 		}
 
-		if ((svc = lb4_lookup_service(&key)) != NULL &&
-		    lb4_svc_needs_lxc_xlation(svc)) {
+		if ((svc = lb4_lookup_service(&key)) != NULL) {
 			ret = lb4_local(get_ct_map4(&tuple), ctx, l3_off, l4_off, &csum_off,
 					&key, &tuple, svc, &ct_state_new, ip4->saddr);
 			if (IS_ERR(ret))
