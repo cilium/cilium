cilium/cilium

commit a562b74ba1a1f281162cb7087a008eaf8790d8b1

bpf: Check native-routing-cidr in BPF masquerade
This commit extends the BPF masquerade by adding a check for whether
a dst IPv4 addr is in native-routing-cidr.

The check fixes a problem when a node hasn't received an ipcache update
for a pod running on a remote node, and thus a packet sent from a local
pod to that pod is SNAT'd. This happens because the dst is considered
as WORLD_ID until the update has been received.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/datapath/linux/config/config.go
@@ -285,6 +285,12 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 
 		if option.Config.EnableBPFMasquerade {
 			cDefinesMap["ENABLE_MASQUERADE"] = "1"
+			if cidr := option.Config.IPv4NativeRoutingCIDR(); cidr != nil {
+				cDefinesMap["IPV4_NATIVE_ROUTING_CIDR"] =
+					fmt.Sprintf("%#x", byteorder.HostSliceToNetwork(cidr.IP, reflect.Uint32).(uint32))
+				ones, _ := cidr.Mask.Size()
+				cDefinesMap["IPV4_NATIVE_ROUTING_CIDR_LEN"] = fmt.Sprintf("%d", ones)
+			}
 		}
 
 		if option.Config.EnableIPMasqAgent {

bpf/lib/ipv4.h
@@ -71,6 +71,12 @@ static __always_inline bool ipv4_is_fragment(const struct iphdr *ip4)
 	return ip4->frag_off & bpf_htons(0x3FFF);
 }
 
+static __always_inline bool ipv4_is_in_subnet(__be32 addr,
+					      __be32 subnet, int prefixlen)
+{
+	return (addr & bpf_htonl(~((1<<(32-prefixlen))-1))) == subnet;
+}
+
 #ifdef ENABLE_IPV4_FRAGMENTS
 static __always_inline bool ipv4_is_not_first_fragment(const struct iphdr *ip4)
 {

bpf/lib/nodeport.h
@@ -751,12 +751,23 @@ static __always_inline bool nodeport_nat_ipv4_needed(struct __ctx_buff *ctx,
 		if (ip4->saddr == addr) {
 			return true;
 		}
-#ifdef ENABLE_MASQUERADE
+
+#ifdef ENABLE_MASQUERADE /* SNAT local pod to world packets */
 		/* Do not MASQ when this function is executed from bpf_overlay
 		 * (encap=true denotes this fact). Otherwise, a packet will be
 		 * SNAT'd to cilium_host IP addr. */
 		if (encap)
 			return false;
+
+#ifdef IPV4_NATIVE_ROUTING_CIDR
+		/* Do not MASQ if a dst IP belongs to a global PodCIDR. The
+		 * check is performed before we determine that a packet is
+		 * sent from a local pod, as this check is cheaper than
+		 * the map lookup done in the latter check. */
+		if (ipv4_is_in_subnet(ip4->daddr, IPV4_NATIVE_ROUTING_CIDR,
+				      IPV4_NATIVE_ROUTING_CIDR_LEN))
+			return false;
+#endif
 
 		if ((ep = __lookup_ip4_endpoint(ip4->saddr)) != NULL &&
 		    !(ep->flags & ENDPOINT_F_HOST)) {

commit 8d978874733f8a1d5b949c4ed124db3a1995c120

datapath: Do not set exclusion CIDR if IPv4 is disabled
Otherwise, cilium-agent will panic when --enable-ipv6=true with the
following:

    panic: runtime error: invalid memory address or nil pointer dereference
    [signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0x1b17ec7]

    [...]
    /go/src/github.com/cilium/cilium/pkg/datapath/linux/config/config.go:344 +0x31d7

Fixes: a562b74ba1a1f ("bpf: Check native-routing-cidr in BPF masquerade")
Reported-by: Robin Hahling <robin.hahling@gw-computing.net>
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/datapath/linux/config/config.go
@@ -332,7 +332,7 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 			cDefinesMap["SNAT_MAPPING_IPV6_SIZE"] = fmt.Sprintf("%d", option.Config.NATMapEntriesGlobal)
 		}
 
-		if option.Config.EnableBPFMasquerade {
+		if option.Config.EnableBPFMasquerade && option.Config.EnableIPv4 {
 			cDefinesMap["ENABLE_MASQUERADE"] = "1"
 			var cidr *cidr.CIDR
 			if c := option.Config.IPv4NativeRoutingCIDR(); c != nil {
@@ -344,11 +344,12 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 				fmt.Sprintf("%#x", byteorder.HostSliceToNetwork(cidr.IP, reflect.Uint32).(uint32))
 			ones, _ := cidr.Mask.Size()
 			cDefinesMap["IPV4_SNAT_EXCLUSION_DST_CIDR_LEN"] = fmt.Sprintf("%d", ones)
-		}
 
-		if option.Config.EnableIPMasqAgent {
-			cDefinesMap["ENABLE_IP_MASQ_AGENT"] = "1"
-			cDefinesMap["IP_MASQ_AGENT_IPV4"] = ipmasq.MapName
+			// ip-masq-agent depends on bpf-masq
+			if option.Config.EnableIPMasqAgent {
+				cDefinesMap["ENABLE_IP_MASQ_AGENT"] = "1"
+				cDefinesMap["IP_MASQ_AGENT_IPV4"] = ipmasq.MapName
+			}
 		}
 
 		ctmap.WriteBPFMacros(fw, nil)