cilium/cilium

commit aa796a2c282ec7cf945a76a862d60aec1f56ba98

build: Fix shellcheck linter
- do not assume absolute path, it is different inside GitHub Actions
- use `--source-path` flag to set script dir
- use long flag name (`-x`/`--external-source`) for readablity
- ensure linter also runs on each PR

Fixes: e091612061d9 (build: New runtime image with multi-platform support)
Fixes: 6f83fb8f3d02 (build: Fix shellcheck errors)

Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

.github/workflows/images.yaml
@@ -1,5 +1,7 @@
 name: Images
 on:
+  pull_request:
+    types: [opened, synchronize, reopened]
   push:
     branches:
       - master
@@ -9,10 +11,22 @@ on:
       - v[0-9]+.[0-9]+.[0-9]+-rc[0-9]+
 
 jobs:
-  build-and-push:
+  lint:
     if: github.repository == 'cilium/cilium'
+    name: Lint image build logic
+    runs-on: ubuntu-18.04
+    steps:
+      - uses: actions/checkout@v1
+      - uses: docker://docker.io/cilium/image-maker:bc81755ec8f6c5afcb10a416cef73f99a35fee2c
+        name: Run make lint
+        with:
+          entrypoint: make
+          args: -C images lint
+  build-and-push:
+    if: (github.repository == 'cilium/cilium' && github.event_name != 'pull_request')
     name: Build and push all images
     runs-on: ubuntu-18.04
+    needs: lint
     steps:
       - uses: actions/checkout@v1
       - uses: docker://docker.io/cilium/image-maker:bc81755ec8f6c5afcb10a416cef73f99a35fee2c

images/runtime/download-cni.sh
@@ -9,7 +9,7 @@ set -o pipefail
 set -o nounset
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# shellcheck source=/src/images/runtime/cni-version.sh
+# shellcheck source=cni-version.sh
 source "${script_dir}/cni-version.sh"
 
 for arch in amd64 arm64 ; do

images/scripts/lint.sh
@@ -15,4 +15,9 @@ if [ -z "${MAKER_CONTAINER+x}" ] ; then
    exec docker run --rm --volume "${root_dir}:/src" --workdir /src/images "${MAKER_IMAGE}" "/src/images/scripts/$(basename "${0}")"
 fi
 
-find . -name '*.sh' -executable -exec shellcheck -x {} +
+# shellcheck disable=SC2207
+scripts=($(find . -name '*.sh' -executable))
+
+for script in "${scripts[@]}" ; do
+  shellcheck --external-source --source-path="$(dirname "${script}")" "${script}"
+done

commit 21eefe63d5bc6b95f32f93f407d76f395d64b665

build: Update runtime image, add a guard rail
This was missed out from aa796a2c282e. A check is added
to ensure this doesn't happen again.

Fixes: aa796a2c282e (build: Fix shellcheck linter)

Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

.github/workflows/images.yaml
@@ -22,6 +22,11 @@ jobs:
         with:
           entrypoint: make
           args: -C images lint
+      - uses: docker://docker.io/cilium/image-maker:bc81755ec8f6c5afcb10a416cef73f99a35fee2c
+        name: Check if runtime and builder images are up-to-date
+        with:
+          entrypoint: make
+          args: -C images check-runtime-image check-builder-image
   build-and-push:
     if: (github.repository == 'cilium/cilium' && github.event_name != 'pull_request')
     name: Build and push all images

images/Makefile
@@ -28,11 +28,17 @@ runtime-image: .buildx_builder
 update-runtime-image:
 	scripts/update-cilium-runtime-image.sh
 
+check-runtime-image:
+	CHECK=true scripts/update-cilium-runtime-image.sh
+
 builder-image: .buildx_builder
 	TEST=true scripts/build-image.sh cilium-builder-dev images/builder linux/amd64 $(OUTPUT) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 update-builder-image:
 	scripts/update-cilium-builder-image.sh
+
+check-builder-image:
+	CHECK=true scripts/update-cilium-builder-image.sh
 
 cilium-image: .buildx_builder
 	ROOT_CONTEXT=true scripts/build-image.sh cilium-dev images/cilium linux/amd64,linux/arm64 $(OUTPUT) "$$(cat .buildx_builder)" $(REGISTRIES)

images/cilium/Dockerfile
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 ARG CILIUM_BUILDER_IMAGE=docker.io/cilium/cilium-builder-dev:b030f1ed16c21608d78564124a870a18ca485444
-ARG CILIUM_RUNTIME_IMAGE=docker.io/cilium/cilium-runtime-dev:f9cb7188204f011045cb18378128c9ed6268fd86
+ARG CILIUM_RUNTIME_IMAGE=docker.io/cilium/cilium-runtime-dev:74992a84a55860ffe1b0c4d6b702c8520db0d4b1
 
 FROM --platform=linux/amd64 ${CILIUM_BUILDER_IMAGE} as builder
 

images/scripts/update-cilium-builder-image.sh
@@ -24,3 +24,8 @@ used_by=($(git grep -l CILIUM_BUILDER_IMAGE= images/*/Dockerfile))
 for i in "${used_by[@]}" ; do
   sed "s|\(CILIUM_BUILDER_IMAGE=\)${image}:.*\$|\1${image}:${image_tag}|" "${i}" > "${i}.sedtmp" && mv "${i}.sedtmp" "${i}"
 done
+
+do_check="${CHECK:-false}"
+if [ "${do_check}" = "true" ] ; then
+    git diff --exit-code "${used_by[@]}"
+fi

images/scripts/update-cilium-runtime-image.sh
@@ -24,3 +24,8 @@ used_by=($(git grep -l CILIUM_RUNTIME_IMAGE= images/*/Dockerfile))
 for i in "${used_by[@]}" ; do
   sed "s|\(CILIUM_RUNTIME_IMAGE=\)${image}:.*\$|\1${image}:${image_tag}|" "${i}" > "${i}.sedtmp" && mv "${i}.sedtmp" "${i}"
 done
+
+do_check="${CHECK:-false}"
+if [ "${do_check}" = "true" ] ; then
+    git diff --exit-code "${used_by[@]}"
+fi