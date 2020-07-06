cilium/cilium

commit 74e706da2d9e5932fb530b748484fe8d29b3f157

common: Move GoArray2C to common
It's going to be used by multiple packages.

Also, add GoArray2CNoSpaces.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/common/utils.go
@@ -50,6 +50,51 @@ func C2GoArray(str string) []byte {
 	return ret
 }
 
+// GoArray2C transforms a byte slice into its hexadecimal string representation.
+// Example:
+// array := []byte{0x12, 0xFF, 0x0, 0x01}
+// fmt.Print(GoArray2C(array)) // "{ 0x12, 0xff, 0x0, 0x1 }"
+func GoArray2C(array []byte) string {
+	return goArray2C(array, true)
+}
+
+// GoArray2CNoSpaces does the same as GoArray2C, but no spaces are used in
+// the final output.
+// Example:
+// array := []byte{0x12, 0xFF, 0x0, 0x01}
+// fmt.Print(GoArray2CNoSpaces(array)) // "{0x12,0xff,0x0,0x1}"
+func GoArray2CNoSpaces(array []byte) string {
+	return goArray2C(array, false)
+}
+
+func goArray2C(array []byte, space bool) string {
+	ret := ""
+	format := ",%#x"
+	if space {
+		format = ", %#x"
+	}
+
+	for i, e := range array {
+		if i == 0 {
+			ret = ret + fmt.Sprintf("%#x", e)
+		} else {
+			ret = ret + fmt.Sprintf(format, e)
+		}
+	}
+	return ret
+}
+
+// FindEPConfigCHeader returns the full path of the file that is the CHeaderFileName from
+// the slice of files
+func FindEPConfigCHeader(basePath string, epFiles []os.FileInfo) string {
+	for _, epFile := range epFiles {
+		if epFile.Name() == CHeaderFileName {
+			return filepath.Join(basePath, epFile.Name())
+		}
+	}
+	return ""
+}
+
 // GetCiliumVersionString returns the first line containing CiliumCHeaderPrefix.
 func GetCiliumVersionString(epCHeaderFilePath string) (string, error) {
 	f, err := os.Open(epCHeaderFilePath)

pkg/datapath/linux/config/utils.go
@@ -20,30 +20,14 @@ import (
 	"reflect"
 
 	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/common"
 )
-
-// goArray2C transforms a byte slice into its hexadecimal string representation.
-// Example:
-// array := []byte{0x12, 0xFF, 0x0, 0x01}
-// fmt.Print(GoArray2C(array)) // "{ 0x12, 0xff, 0x0, 0x1 }"
-func goArray2C(array []byte) string {
-	ret := ""
-
-	for i, e := range array {
-		if i == 0 {
-			ret = ret + fmt.Sprintf("%#x", e)
-		} else {
-			ret = ret + fmt.Sprintf(", %#x", e)
-		}
-	}
-	return ret
-}
 
 // FmtDefineAddress returns the a define string from the given name and addr.
 // Example:
 // fmt.Print(FmtDefineAddress("foo", []byte{1, 2, 3})) // "#define foo { .addr = { 0x1, 0x2, 0x3 } }\n"
 func FmtDefineAddress(name string, addr []byte) string {
-	return fmt.Sprintf("#define %s { .addr = { %s } }\n", name, goArray2C(addr))
+	return fmt.Sprintf("#define %s { .addr = { %s } }\n", name, common.GoArray2C(addr))
 }
 
 // defineUint32 writes the C definition for an unsigned 32-bit value.
@@ -55,7 +39,7 @@ func defineUint32(name string, value uint32) string {
 // defineIPv4 writes the C definition for the given IPv4 address.
 func defineIPv4(name string, addr []byte) string {
 	if len(addr) != net.IPv4len {
-		return fmt.Sprintf("/* BUG: bad ip define %s %s */\n", name, goArray2C(addr))
+		return fmt.Sprintf("/* BUG: bad ip define %s %s */\n", name, common.GoArray2C(addr))
 	}
 	nboAddr := byteorder.HostSliceToNetwork(addr, reflect.Uint32).(uint32)
 	return defineUint32(name, nboAddr)
@@ -64,20 +48,20 @@ func defineIPv4(name string, addr []byte) string {
 // defineIPv6 writes the C definition for the given IPv6 address.
 func defineIPv6(name string, addr []byte) string {
 	if len(addr) != net.IPv6len {
-		return fmt.Sprintf("/* BUG: bad ip define %s %s */\n", name, goArray2C(addr))
+		return fmt.Sprintf("/* BUG: bad ip define %s %s */\n", name, common.GoArray2C(addr))
 	}
-	return fmt.Sprintf("DEFINE_IPV6(%s, %s);\n", name, goArray2C(addr))
+	return fmt.Sprintf("DEFINE_IPV6(%s, %s);\n", name, common.GoArray2C(addr))
 }
 
 func dumpRaw(name string, addr []byte) string {
-	return fmt.Sprintf(" %s%s\n", name, goArray2C(addr))
+	return fmt.Sprintf(" %s%s\n", name, common.GoArray2C(addr))
 }
 
 // defineMAC writes the C definition for the given MAC name and addr.
 func defineMAC(name string, addr []byte) string {
 	if len(addr) != 6 { /* MAC len */
-		return fmt.Sprintf("/* BUG: bad mac define %s %s */\n", name, goArray2C(addr))
+		return fmt.Sprintf("/* BUG: bad mac define %s %s */\n", name, common.GoArray2C(addr))
 	}
 	return fmt.Sprintf("DEFINE_MAC(%s, %s);\n#define %s fetch_mac(%s)\n",
-		name, goArray2C(addr), name, name)
+		name, common.GoArray2C(addr), name, name)
 }

pkg/common/utils_test.go
@@ -47,6 +47,38 @@ func (s *CommonSuite) TestC2GoArray(c *check.C) {
 	c.Assert(C2GoArray(""), checker.DeepEquals, []byte{})
 }
 
+func (s *CommonSuite) TestGoArray2C(c *check.C) {
+	tests := []struct {
+		input  []byte
+		output string
+	}{
+		{
+			input:  []byte{0, 0x01, 0x02, 0x03},
+			output: "0x0, 0x1, 0x2, 0x3",
+		},
+		{
+			input:  []byte{0, 0xFF, 0xFF, 0xFF},
+			output: "0x0, 0xff, 0xff, 0xff",
+		},
+		{
+			input:  []byte{0xa, 0xbc, 0xde, 0xf1},
+			output: "0xa, 0xbc, 0xde, 0xf1",
+		},
+		{
+			input:  []byte{0},
+			output: "0x0",
+		},
+		{
+			input:  []byte{},
+			output: "",
+		},
+	}
+
+	for _, test := range tests {
+		c.Assert(GoArray2C(test.input), check.Equals, test.output)
+	}
+}
+
 func (s *CommonSuite) TestMoveNewFilesTo(c *check.C) {
 	oldDir := c.MkDir()
 	newDir := c.MkDir()

pkg/datapath/linux/config/utils_test.go
@@ -37,35 +37,6 @@ type formatTestCase struct {
 	output string
 }
 
-func (s *ConfigSuite) TestGoArray2C(c *C) {
-	tests := []formatTestCase{
-		{
-			input:  []byte{0, 0x01, 0x02, 0x03},
-			output: "0x0, 0x1, 0x2, 0x3",
-		},
-		{
-			input:  []byte{0, 0xFF, 0xFF, 0xFF},
-			output: "0x0, 0xff, 0xff, 0xff",
-		},
-		{
-			input:  []byte{0xa, 0xbc, 0xde, 0xf1},
-			output: "0xa, 0xbc, 0xde, 0xf1",
-		},
-		{
-			input:  []byte{0},
-			output: "0x0",
-		},
-		{
-			input:  []byte{},
-			output: "",
-		},
-	}
-
-	for _, test := range tests {
-		c.Assert(goArray2C(test.input), Equals, test.output)
-	}
-}
-
 func (s *ConfigSuite) TestdefineIPv6(c *C) {
 	tests := []formatTestCase{
 		{

commit af8392218f4e8b19b0483a0244a11bffb8eaa32b

common: remove unused func FindEPConfigCHeader
This was already removed by commit d81a5cdea511 ("pkg/endpoint: Simplify
search for C header file") when its last user disappeard, but it got
readded 74e706da2d9e ("common: Move GoArray2C to common") which moved
some code around the same time. Now remove it for good.

Fixes: 74e706da2d9e ("common: Move GoArray2C to common")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

pkg/common/utils.go
@@ -83,17 +83,6 @@ func goArray2C(array []byte, space bool) string {
 	return ret
 }
 
-// FindEPConfigCHeader returns the full path of the file that is the CHeaderFileName from
-// the slice of files
-func FindEPConfigCHeader(basePath string, epFiles []os.FileInfo) string {
-	for _, epFile := range epFiles {
-		if epFile.Name() == CHeaderFileName {
-			return filepath.Join(basePath, epFile.Name())
-		}
-	}
-	return ""
-}
-
 // RequireRootPrivilege checks if the user running cmd is root. If not, it exits the program
 func RequireRootPrivilege(cmd string) {
 	if os.Getuid() != 0 {