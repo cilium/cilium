cilium/cilium

commit f9c2d70950cf96e5c70da3c2d5eb2e442e309211

bpf: implement getpeername for sock lb programs
The vast majority of applications does not bother about getpeername(),
but in a few occasions we've seen breakage when validating the peer's
address since it returns unexpectedly the backend tuple instead of the
service one.

The implementation here is actually nicely straight forward in that we
can fully reuse sock{4,6}_update_revnat() as well as the __sock{4,6}_xlate_rev()
implementation which also handles v4-in-v6. This works because the kernel
populates the struct sockaddr of its known peer in inet{,6}_getname() and
we have recorded that exactly on prior connect hook [0], so it tells us
exactly the address it wants to see. This is relevant for {un,}connected
UDP, for example.

I was originally thinking to use socket local storage for this, but i)
without BTF support it won't work and ii) we can spare an additional map
by just using LB{4,6}_REVERSE_NAT_SK_MAP in an efficient manner (that is,
w/o hitting the update spinlock every time).

Simple example:

  # ./cilium/cilium service list
  ID   Frontend     Service Type   Backend
  1    1.2.3.4:80   ClusterIP      1 => 10.0.0.10:80

Before; curl's verbose output example, no getpeername() reverse xlation:

  # curl --verbose 1.2.3.4
  * Rebuilt URL to: 1.2.3.4/
  *   Trying 1.2.3.4...
  * TCP_NODELAY set
  * Connected to 1.2.3.4 (10.0.0.10) port 80 (#0)
  > GET / HTTP/1.1
  > Host: 1.2.3.4
  > User-Agent: curl/7.58.0
  > Accept: */*
  [...]

After; with getpeername() reverse xlation:

  # curl --verbose 1.2.3.4
  * Rebuilt URL to: 1.2.3.4/
  *   Trying 1.2.3.4...
  * TCP_NODELAY set
  * Connected to 1.2.3.4 (1.2.3.4) port 80 (#0)
  > GET / HTTP/1.1
  >  Host: 1.2.3.4
  > User-Agent: curl/7.58.0
  > Accept: */*
  [...]

Attached hooks:

  # bpftool cgroup list /var/run/cilium/cgroupv2/
  ID       AttachType      AttachFlags     Name
  14492    connect4
  14487    connect6
  14494    post_bind4
  14489    post_bind6
  14495    sendmsg4
  14490    sendmsg6

bpf/init.sh
@@ -28,13 +28,14 @@ IPSEC=${10}
 ENCRYPT_DEV=${11}
 HOSTLB=${12}
 HOSTLB_UDP=${13}
-CGROUP_ROOT=${14}
-BPFFS_ROOT=${15}
-NODE_PORT=${16}
-NODE_PORT_BIND=${17}
-MCPU=${18}
-NODE_PORT_IPV4_ADDRS=${19}
-NODE_PORT_IPV6_ADDRS=${20}
+HOSTLB_PEER=${14}
+CGROUP_ROOT=${15}
+BPFFS_ROOT=${16}
+NODE_PORT=${17}
+NODE_PORT_BIND=${18}
+MCPU=${19}
+NODE_PORT_IPV4_ADDRS=${20}
+NODE_PORT_IPV6_ADDRS=${21}
 
 ID_HOST=1
 ID_WORLD=2
@@ -608,6 +609,9 @@ if [ "$HOSTLB" = "true" ]; then
 	COPTS="-DLB_L3 -DLB_L4"
 	if [ "$IP6_HOST" != "<nil>" ] || [ "$IP4_HOST" != "<nil>" ] && [ -f /proc/sys/net/ipv6/conf/all/forwarding ]; then
 		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr connect6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		if [ "$HOSTLB_PEER" = "true" ]; then
+			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr getpeername6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		fi
 		if [ "$NODE_PORT" = "true" ] && [ "$NODE_PORT_BIND" = "true" ]; then
 			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sock post_bind6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 		else
@@ -623,6 +627,9 @@ if [ "$HOSTLB" = "true" ]; then
 	fi
 	if [ "$IP4_HOST" != "<nil>" ]; then
 		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr connect4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		if [ "$HOSTLB_PEER" = "true" ]; then
+			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr getpeername4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		fi
 		if [ "$NODE_PORT" = "true" ] && [ "$NODE_PORT_BIND" = "true" ]; then
 			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sock post_bind4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 		else

bpf/bpf_sock.c
@@ -141,7 +141,7 @@ bool sock_proto_enabled(__u32 proto)
 }
 
 #ifdef ENABLE_IPV4
-#ifdef ENABLE_HOST_SERVICES_UDP
+#if defined(ENABLE_HOST_SERVICES_UDP) || defined(ENABLE_HOST_SERVICES_PEER)
 struct bpf_elf_map __section_maps LB4_REVERSE_NAT_SK_MAP = {
 	.type		= BPF_MAP_TYPE_LRU_HASH,
 	.size_key	= sizeof(struct ipv4_revnat_tuple),
@@ -182,7 +182,7 @@ int sock4_update_revnat(struct bpf_sock_addr *ctx __maybe_unused,
 {
 	return -1;
 }
-#endif /* ENABLE_HOST_SERVICES_UDP */
+#endif /* ENABLE_HOST_SERVICES_UDP || ENABLE_HOST_SERVICES_PEER */
 
 static __always_inline bool
 sock4_skip_xlate(struct lb4_service *svc, const bool in_hostns,
@@ -325,17 +325,12 @@ reselect_backend:
 	if (svc->affinity)
 		lb4_update_affinity_by_netns(svc, &id, backend_id);
 
-	/* revnat entry is not required for TCP protocol */
-	if (!udp_only && ctx->protocol == IPPROTO_TCP)
-		goto update_dst;
-
 	if (sock4_update_revnat(ctx_full, backend, &key,
 				svc->rev_nat_index) < 0) {
 		update_metrics(0, METRIC_EGRESS, REASON_LB_REVNAT_UPDATE);
 		return -ENOMEM;
 	}
 
-update_dst:
 	ctx->user_ip4 = backend->address;
 	ctx_set_port(ctx, backend->port);
 
@@ -393,14 +388,7 @@ int sock4_bind(struct bpf_sock *ctx)
 }
 #endif /* ENABLE_NODEPORT || ENABLE_EXTERNAL_IP */
 
-#ifdef ENABLE_HOST_SERVICES_UDP
-__section("sendmsg4")
-int sock4_sendmsg(struct bpf_sock_addr *ctx)
-{
-	__sock4_xlate_fwd(ctx, ctx, true);
-	return SYS_PROCEED;
-}
-
+#if defined(ENABLE_HOST_SERVICES_UDP) || defined(ENABLE_HOST_SERVICES_PEER)
 static __always_inline int __sock4_xlate_rev(struct bpf_sock_addr *ctx,
 					     struct bpf_sock_addr *ctx_full)
 {
@@ -434,18 +422,32 @@ static __always_inline int __sock4_xlate_rev(struct bpf_sock_addr *ctx,
 	return -ENXIO;
 }
 
+__section("sendmsg4")
+int sock4_sendmsg(struct bpf_sock_addr *ctx)
+{
+	__sock4_xlate_fwd(ctx, ctx, true);
+	return SYS_PROCEED;
+}
+
 __section("recvmsg4")
 int sock4_recvmsg(struct bpf_sock_addr *ctx)
 {
 	__sock4_xlate_rev(ctx, ctx);
 	return SYS_PROCEED;
 }
-#endif /* ENABLE_HOST_SERVICES_UDP */
+
+__section("getpeername4")
+int sock4_getpeername(struct bpf_sock_addr *ctx)
+{
+	__sock4_xlate_rev(ctx, ctx);
+	return SYS_PROCEED;
+}
+#endif /* ENABLE_HOST_SERVICES_UDP || ENABLE_HOST_SERVICES_PEER */
 #endif /* ENABLE_IPV4 */
 
 #if defined(ENABLE_IPV6) || defined(ENABLE_IPV4)
 #ifdef ENABLE_IPV6
-#ifdef ENABLE_HOST_SERVICES_UDP
+#if defined(ENABLE_HOST_SERVICES_UDP) || defined(ENABLE_HOST_SERVICES_PEER)
 struct bpf_elf_map __section_maps LB6_REVERSE_NAT_SK_MAP = {
 	.type		= BPF_MAP_TYPE_LRU_HASH,
 	.size_key	= sizeof(struct ipv6_revnat_tuple),
@@ -486,7 +488,7 @@ int sock6_update_revnat(struct bpf_sock_addr *ctx __maybe_unused,
 {
 	return -1;
 }
-#endif /* ENABLE_HOST_SERVICES_UDP */
+#endif /* ENABLE_HOST_SERVICES_UDP || ENABLE_HOST_SERVICES_PEER */
 #endif /* ENABLE_IPV6 */
 
 static __always_inline void ctx_get_v6_address(const struct bpf_sock_addr *ctx,
@@ -737,16 +739,12 @@ reselect_backend:
 	if (svc->affinity)
 		lb6_update_affinity_by_netns(svc, &id, backend_id);
 
-	if (!udp_only && ctx->protocol == IPPROTO_TCP)
-		goto update_dst;
-
 	if (sock6_update_revnat(ctx, backend, &key,
 			        svc->rev_nat_index) < 0) {
 		update_metrics(0, METRIC_EGRESS, REASON_LB_REVNAT_UPDATE);
 		return -ENOMEM;
 	}
 
-update_dst:
 	ctx_set_v6_address(ctx, &backend->address);
 	ctx_set_port(ctx, backend->port);
 
@@ -763,14 +761,7 @@ int sock6_connect(struct bpf_sock_addr *ctx)
 	return SYS_PROCEED;
 }
 
-#ifdef ENABLE_HOST_SERVICES_UDP
-__section("sendmsg6")
-int sock6_sendmsg(struct bpf_sock_addr *ctx)
-{
-	__sock6_xlate_fwd(ctx, true);
-	return SYS_PROCEED;
-}
-
+#if defined(ENABLE_HOST_SERVICES_UDP) || defined(ENABLE_HOST_SERVICES_PEER)
 static __always_inline int
 sock6_xlate_rev_v4_in_v6(struct bpf_sock_addr *ctx __maybe_unused)
 {
@@ -835,13 +826,27 @@ static __always_inline int __sock6_xlate_rev(struct bpf_sock_addr *ctx)
 	return sock6_xlate_rev_v4_in_v6(ctx);
 }
 
+__section("sendmsg6")
+int sock6_sendmsg(struct bpf_sock_addr *ctx)
+{
+	__sock6_xlate_fwd(ctx, true);
+	return SYS_PROCEED;
+}
+
 __section("recvmsg6")
 int sock6_recvmsg(struct bpf_sock_addr *ctx)
 {
 	__sock6_xlate_rev(ctx);
 	return SYS_PROCEED;
 }
-#endif /* ENABLE_HOST_SERVICES_UDP */
+
+__section("getpeername6")
+int sock6_getpeername(struct bpf_sock_addr *ctx)
+{
+	__sock6_xlate_rev(ctx);
+	return SYS_PROCEED;
+}
+#endif /* ENABLE_HOST_SERVICES_UDP || ENABLE_HOST_SERVICES_PEER */
 #endif /* ENABLE_IPV6 || ENABLE_IPV4 */
 
 BPF_LICENSE("GPL");

daemon/cmd/daemon_main.go
@@ -1796,6 +1796,12 @@ func initKubeProxyReplacementOptions() {
 		// be v4-in-v6 connections even if the agent has v6 support disabled.
 		probe.HaveIPv6Support()
 
+		option.Config.EnableHostServicesPeer = true
+		if option.Config.EnableIPv4 && bpf.TestDummyProg(bpf.ProgTypeCgroupSockAddr, bpf.BPF_CGROUP_INET4_GETPEERNAME) != nil ||
+			option.Config.EnableIPv6 && bpf.TestDummyProg(bpf.ProgTypeCgroupSockAddr, bpf.BPF_CGROUP_INET6_GETPEERNAME) != nil {
+			option.Config.EnableHostServicesPeer = false
+		}
+
 		if option.Config.EnableHostServicesTCP &&
 			(option.Config.EnableIPv4 && bpf.TestDummyProg(bpf.ProgTypeCgroupSockAddr, bpf.BPF_CGROUP_INET4_CONNECT) != nil ||
 				option.Config.EnableIPv6 && bpf.TestDummyProg(bpf.ProgTypeCgroupSockAddr, bpf.BPF_CGROUP_INET6_CONNECT) != nil) {

pkg/bpf/bpf.go
@@ -52,27 +52,31 @@ const (
 	BPF_TASK_FD_QUERY       = 20
 
 	// BPF syscall attach types
-	BPF_CGROUP_INET_INGRESS     = 0
-	BPF_CGROUP_INET_EGRESS      = 1
-	BPF_CGROUP_INET_SOCK_CREATE = 2
-	BPF_CGROUP_SOCK_OPS         = 3
-	BPF_SK_SKB_STREAM_PARSER    = 4
-	BPF_SK_SKB_STREAM_VERDICT   = 5
-	BPF_CGROUP_DEVICE           = 6
-	BPF_SK_MSG_VERDICT          = 7
-	BPF_CGROUP_INET4_BIND       = 8
-	BPF_CGROUP_INET6_BIND       = 9
-	BPF_CGROUP_INET4_CONNECT    = 10
-	BPF_CGROUP_INET6_CONNECT    = 11
-	BPF_CGROUP_INET4_POST_BIND  = 12
-	BPF_CGROUP_INET6_POST_BIND  = 13
-	BPF_CGROUP_UDP4_SENDMSG     = 14
-	BPF_CGROUP_UDP6_SENDMSG     = 15
-	BPF_LIRC_MODE2              = 16
-	BPF_FLOW_DISSECTOR          = 17
-	BPF_CGROUP_SYSCTL           = 18
-	BPF_CGROUP_UDP4_RECVMSG     = 19
-	BPF_CGROUP_UDP6_RECVMSG     = 20
+	BPF_CGROUP_INET_INGRESS      = 0
+	BPF_CGROUP_INET_EGRESS       = 1
+	BPF_CGROUP_INET_SOCK_CREATE  = 2
+	BPF_CGROUP_SOCK_OPS          = 3
+	BPF_SK_SKB_STREAM_PARSER     = 4
+	BPF_SK_SKB_STREAM_VERDICT    = 5
+	BPF_CGROUP_DEVICE            = 6
+	BPF_SK_MSG_VERDICT           = 7
+	BPF_CGROUP_INET4_BIND        = 8
+	BPF_CGROUP_INET6_BIND        = 9
+	BPF_CGROUP_INET4_CONNECT     = 10
+	BPF_CGROUP_INET6_CONNECT     = 11
+	BPF_CGROUP_INET4_POST_BIND   = 12
+	BPF_CGROUP_INET6_POST_BIND   = 13
+	BPF_CGROUP_UDP4_SENDMSG      = 14
+	BPF_CGROUP_UDP6_SENDMSG      = 15
+	BPF_LIRC_MODE2               = 16
+	BPF_FLOW_DISSECTOR           = 17
+	BPF_CGROUP_SYSCTL            = 18
+	BPF_CGROUP_UDP4_RECVMSG      = 19
+	BPF_CGROUP_UDP6_RECVMSG      = 20
+	BPF_CGROUP_INET4_GETPEERNAME = 29
+	BPF_CGROUP_INET6_GETPEERNAME = 30
+	BPF_CGROUP_INET4_GETSOCKNAME = 31
+	BPF_CGROUP_INET6_GETSOCKNAME = 32
 
 	// Flags for BPF_MAP_UPDATE_ELEM. Must match values from linux/bpf.h
 	BPF_ANY     = 0

pkg/datapath/linux/config/config.go
@@ -229,6 +229,9 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		if option.Config.EnableHostServicesTCP && option.Config.EnableHostServicesUDP {
 			cDefinesMap["ENABLE_HOST_SERVICES_FULL"] = "1"
 		}
+		if option.Config.EnableHostServicesPeer {
+			cDefinesMap["ENABLE_HOST_SERVICES_PEER"] = "1"
+		}
 	}
 
 	if option.Config.EnableNodePort {

pkg/datapath/loader/base.go
@@ -55,6 +55,7 @@ const (
 	initArgEncryptInterface
 	initArgHostReachableServices
 	initArgHostReachableServicesUDP
+	initArgHostReachableServicesPeer
 	initArgCgroupRoot
 	initArgBpffsRoot
 	initArgNodePort
@@ -199,9 +200,15 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		} else {
 			args[initArgHostReachableServicesUDP] = "false"
 		}
+		if option.Config.EnableHostServicesPeer {
+			args[initArgHostReachableServicesPeer] = "true"
+		} else {
+			args[initArgHostReachableServicesPeer] = "false"
+		}
 	} else {
 		args[initArgHostReachableServices] = "false"
 		args[initArgHostReachableServicesUDP] = "false"
+		args[initArgHostReachableServicesPeer] = "false"
 	}
 
 	if option.Config.EncryptInterface != "" {

pkg/option/config.go
@@ -1395,6 +1395,7 @@ type DaemonConfig struct {
 	EnableHostReachableServices   bool
 	EnableHostServicesTCP         bool
 	EnableHostServicesUDP         bool
+	EnableHostServicesPeer        bool
 	EnablePolicy                  string
 	EnableTracing                 bool
 	EnvoyLog                      string

commit 6f64aa5674473330ea45cd01768c085651c1a99f

bpf: clear getpeername{4,6} progs
Attempt to clear previously loaded getpeername progs in bpf/init.sh in
case $HOSTLB is disabled.

Fixes: f9c2d70950cf ("bpf: implement getpeername for sock lb programs")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

bpf/init.sh
@@ -652,6 +652,8 @@ else
 	bpf_clear_cgroups $CGROUP_ROOT sendmsg6
 	bpf_clear_cgroups $CGROUP_ROOT recvmsg4
 	bpf_clear_cgroups $CGROUP_ROOT recvmsg6
+	bpf_clear_cgroups $CGROUP_ROOT getpeername4
+	bpf_clear_cgroups $CGROUP_ROOT getpeername6
 fi
 
 if [ "$IPSEC" == "true" ]; then