bd5bc5b0aa2bb6e2edcc2bc0e3a0f4ed71f52c04
Laurent Bernaille <laurent.bernaille@datadoghq.com>
2020-07-07 09:12:55 +0200

    Fix small CRD issue with toGroups
    [ upstream commit 678e7f0205ee7f50566d3c2fdd0bd6a3a63bc54c ]
    
    [ Backporter's notes: Schema version was modified to 1.16 rather than
    the latest version from master, to ensure that subsequent updates to
    v1.7 or v1.8 would still refresh the schema. ]
    
    Fixes: f0049da61f4f ("pkg/k8s: fix all structural issues with CNP validation")
    
    Signed-off-by: Laurent Bernaille <laurent.bernaille@datadoghq.com>
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/k8s/apis/cilium.io/v2/register.go
+++ pkg/k8s/apis/cilium.io/v2/register.go
@@ -43,7 +43,7 @@ const (
 
 	// CustomResourceDefinitionSchemaVersion is semver-conformant version of CRD schema
 	// Used to determine if CRD needs to be updated in cluster
-	CustomResourceDefinitionSchemaVersion = "1.15"
+	CustomResourceDefinitionSchemaVersion = "1.16"
 
 	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
 	CustomResourceDefinitionSchemaVersionKey = "io.cilium.k8s.crd.schema.version"
@@ -648,12 +648,17 @@ var (
 				},
 			},
 			"toGroups": {
-				Type: "object",
+				Type: "array",
 				Description: `ToGroups is a list of constraints that will
 				gather data from third-party providers and create a new
 				derived policy.`,
-				Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
-					"aws": AWSGroup,
+				Items: &apiextensionsv1beta1.JSONSchemaPropsOrArray{
+					Schema: &apiextensionsv1beta1.JSONSchemaProps{
+						Type: "object",
+						Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
+							"aws": AWSGroup,
+						},
+					},
 				},
 			},
 			"toFQDNs": {
