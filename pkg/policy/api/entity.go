cilium/cilium

commit 0a9685e20d25d1f74fb7eb57718ff54f05e90afb

identity: Introduce reserved:unmanaged identity
Unmanaged endpoints are curently labeled with the identity reserved:init. This
change was introduced for 1.3. While reserved:init correctly implements the
cluster entity and also allows to define policy. It does not provide clear
visibility and does not allow to define policy for unmanaged pods without also
affecting managed pods in the init phase.

Introduced a new identity reserved:unmanaged to map unmanaged endpoints to
instead. The numeric value for the previous cluster identity can be utilized
for this purpose. There is almost a 1:1 mapping of the previous intent of the
cluster entity so re-using the value will cause the least potential for
disruption when upgrading from 1.2 to 1.3.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/policy/api/entity.go
@@ -62,6 +62,12 @@ var (
 		Source: labels.LabelSourceReserved,
 	})
 
+	endpointSelectorUnmanaged = NewESFromLabels(&labels.Label{
+		Key:    labels.IDNameUnmanaged,
+		Value:  "",
+		Source: labels.LabelSourceReserved,
+	})
+
 	// EntitySelectorMapping maps special entity names that come in
 	// policies to selectors
 	EntitySelectorMapping = map[Entity]EndpointSelectorSlice{
@@ -123,6 +129,7 @@ func InitEntities(clusterName string) {
 	EntitySelectorMapping[EntityCluster] = EndpointSelectorSlice{
 		endpointSelectorHost,
 		endpointSelectorInit,
+		endpointSelectorUnmanaged,
 		NewESFromLabels(&labels.Label{
 			Key:    k8sapi.PolicyLabelCluster,
 			Value:  clusterName,

bpf/lib/policy.h
@@ -32,6 +32,7 @@
  * - ReservedIdentityWorld	(2)
  *
  * The following identities are given to endpoints so return false for these:
+ * - ReservedIdentityUnmanaged  (3)
  * - ReservedIdentityHealth	(4)
  * - ReservedIdentityInit	(5)
  *
@@ -39,7 +40,7 @@
  */
 static inline bool identity_is_reserved(__u32 identity)
 {
-	return identity < HEALTH_ID;
+	return identity < UNMANAGED_ID;
 }
 
 static inline int __inline__

bpf/node_config.h
@@ -33,6 +33,7 @@
 #define HOST_IP 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xa, 0x0, 0x2, 0xf, 0xff, 0xff
 #define HOST_ID 1
 #define WORLD_ID 2
+#define UNMANAGED_ID 3
 #define HEALTH_ID 4
 #define INIT_ID 5
 #define HOST_IFINDEX_MAC { .addr = { 0xce, 0x72, 0xa7, 0x03, 0x88, 0x56 } }

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=374def1b2ed1ed39b660a30ff93e9b735e628dd9
+GO_BINDATA_SHA1SUM=afb24a37b06b8f1ab5f41bcfa765b5129c6abf46
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

daemon/daemon.go
@@ -957,6 +957,7 @@ func createNodeConfigHeaderfile() error {
 	fmt.Fprintf(fw, "#define HOST_ID %d\n", identity.GetReservedID(labels.IDNameHost))
 	fmt.Fprintf(fw, "#define WORLD_ID %d\n", identity.GetReservedID(labels.IDNameWorld))
 	fmt.Fprintf(fw, "#define HEALTH_ID %d\n", identity.GetReservedID(labels.IDNameHealth))
+	fmt.Fprintf(fw, "#define UNMANAGED_ID %d\n", identity.GetReservedID(labels.IDNameUnmanaged))
 	fmt.Fprintf(fw, "#define INIT_ID %d\n", identity.GetReservedID(labels.IDNameInit))
 	fmt.Fprintf(fw, "#define LB_RR_MAX_SEQ %d\n", lbmap.MaxSeq)
 	fmt.Fprintf(fw, "#define CILIUM_LB_MAP_MAX_ENTRIES %d\n", lbmap.MaxEntries)

daemon/k8s_watcher.go
@@ -1976,8 +1976,11 @@ func (d *Daemon) updatePodHostIP(pod *v1.Pod) (bool, error) {
 		return true, fmt.Errorf("no/invalid PodIP: %s", pod.Status.PodIP)
 	}
 
+	// Initial mapping of podIP <-> hostIP <-> identity. The mapping is
+	// later updated once the allocator has determined the real identity.
+	// If the endpoint remains unmanaged, the identity remains untouched.
 	selfOwned := ipcache.IPIdentityCache.Upsert(pod.Status.PodIP, hostIP, ipcache.Identity{
-		ID:     identity.ReservedIdentityInit,
+		ID:     identity.ReservedIdentityUnmanaged,
 		Source: ipcache.FromKubernetes,
 	})
 	if !selfOwned {

pkg/identity/identity_test.go
@@ -55,6 +55,10 @@ func (s *IdentityTestSuite) TestReservedID(c *C) {
 	c.Assert(i, Equals, NumericIdentity(5))
 	c.Assert(i.String(), Equals, "init")
 
+	i = GetReservedID("unmanaged")
+	c.Assert(i, Equals, NumericIdentity(3))
+	c.Assert(i.String(), Equals, "unmanaged")
+
 	c.Assert(GetReservedID("unknown"), Equals, IdentityUnknown)
 	unknown := NumericIdentity(700)
 	c.Assert(unknown.String(), Equals, "700")
@@ -65,6 +69,7 @@ func (s *IdentityTestSuite) TestIsReservedIdentity(c *C) {
 	c.Assert(ReservedIdentityHost.IsReservedIdentity(), Equals, true)
 	c.Assert(ReservedIdentityWorld.IsReservedIdentity(), Equals, true)
 	c.Assert(ReservedIdentityInit.IsReservedIdentity(), Equals, true)
+	c.Assert(ReservedIdentityUnmanaged.IsReservedIdentity(), Equals, true)
 
 	c.Assert(NumericIdentity(123456).IsReservedIdentity(), Equals, false)
 }
@@ -110,6 +115,15 @@ func (s *IdentityTestSuite) TestAllocateIdentityReserved(c *C) {
 	i, isNew, err = AllocateIdentity(lbls)
 	c.Assert(err, IsNil)
 	c.Assert(i.ID, Equals, ReservedIdentityInit)
+	c.Assert(isNew, Equals, false)
+
+	lbls = labels.Labels{
+		labels.IDNameUnmanaged: labels.NewLabel(labels.IDNameUnmanaged, "", labels.LabelSourceReserved),
+	}
+	c.Assert(IdentityAllocationIsLocal(lbls), Equals, true)
+	i, isNew, err = AllocateIdentity(lbls)
+	c.Assert(err, IsNil)
+	c.Assert(i.ID, Equals, ReservedIdentityUnmanaged)
 	c.Assert(isNew, Equals, false)
 }
 

pkg/identity/numericidentity.go
@@ -48,9 +48,8 @@ const (
 	// ReservedIdentityWorld represents any endpoint outside of the cluster
 	ReservedIdentityWorld
 
-	// ObsoleteReservedIdentityCluster was previously the cluster identity
-	// which has been obsoleted since.
-	ObsoleteReservedIdentityCluster
+	// ReservedIdentityUnmanaged represents unmanaged endpoints.
+	ReservedIdentityUnmanaged
 
 	// ReservedIdentityHealth represents the local cilium-health endpoint
 	ReservedIdentityHealth
@@ -193,16 +192,18 @@ func initWellKnownIdentities() {
 
 var (
 	reservedIdentities = map[string]NumericIdentity{
-		labels.IDNameHost:   ReservedIdentityHost,
-		labels.IDNameWorld:  ReservedIdentityWorld,
-		labels.IDNameHealth: ReservedIdentityHealth,
-		labels.IDNameInit:   ReservedIdentityInit,
+		labels.IDNameHost:      ReservedIdentityHost,
+		labels.IDNameWorld:     ReservedIdentityWorld,
+		labels.IDNameUnmanaged: ReservedIdentityUnmanaged,
+		labels.IDNameHealth:    ReservedIdentityHealth,
+		labels.IDNameInit:      ReservedIdentityInit,
 	}
 	reservedIdentityNames = map[NumericIdentity]string{
-		ReservedIdentityHost:   labels.IDNameHost,
-		ReservedIdentityWorld:  labels.IDNameWorld,
-		ReservedIdentityHealth: labels.IDNameHealth,
-		ReservedIdentityInit:   labels.IDNameInit,
+		ReservedIdentityHost:      labels.IDNameHost,
+		ReservedIdentityWorld:     labels.IDNameWorld,
+		ReservedIdentityUnmanaged: labels.IDNameUnmanaged,
+		ReservedIdentityHealth:    labels.IDNameHealth,
+		ReservedIdentityInit:      labels.IDNameInit,
 	}
 
 	wellKnown = wellKnownIdentities{}

commit b7c588452db3df5c85d45ea5d9ea0866f604fbe9

policy/api: Support unmanaged entity in policies
0a9685e introduced the reserved:unmanaged identity, but did not
introduce the associated entity. This commit fixes it and allows the
following in policies:

    - fromEntity:
      - unmanaged

The goal is to allow policies to work even before all pods are restarted
and managed by Cilium. In particular, this can be an issue with host
policies since the host is more likely than pods to need to talk with
unmanaged pods (e.g., on GKE right after deploying Cilium).

Fixes: 0a9685e ("identity: Introduce reserved:unmanaged identity")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/policy/api/entity.go
@@ -42,6 +42,9 @@ const (
 	// EntityInit is an entity that represents an initializing endpoint
 	EntityInit Entity = "init"
 
+	// EntityUnmanaged is an entity that represents unamanaged endpoints.
+	EntityUnmanaged Entity = "unmanaged"
+
 	// EntityRemoteNode is an entity that represents all remote nodes
 	EntityRemoteNode Entity = "remote-node"
 
@@ -76,6 +79,7 @@ var (
 		EntityInit:       {endpointSelectorInit},
 		EntityRemoteNode: {endpointSelectorRemoteNode},
 		EntityHealth:     {endpointSelectorHealth},
+		EntityUnmanaged:  {endpointSelectorUnmanaged},
 		EntityNone:       {EndpointSelectorNone},
 
 		// EntityCluster is populated with an empty entry to allow the