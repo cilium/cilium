cilium/cilium

commit 1f907666e82253fb2698cd346068ba10930f1dbd

daemon/operator: Fatal on startup when Identity CRD is enabled without k8s
[ upstream commit 1598f7448319cc57e1e5c61bc58687d7c14c2805 ]

We've seen panics where it seems k8s isn't setup correctly but CRD
related operations occur, and segfault. This occurs when the kubernetes
service is not ready by the time cilium starts up and so cilium misses the
KUBERNETES_SERVICE_{HOST,PORT} settings resulting in it being
misconfigured.
See https://github.com/kubernetes/kubernetes/issues/40973
See https://github.com/cilium/cilium/pull/11021

Signed-off-by: Ray Bejjani <ray@isovalent.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

operator/main.go
@@ -342,7 +342,12 @@ func runOperator(cmd *cobra.Command) {
 		startKvstoreWatchdog()
 	}
 
-	if identityAllocationMode == option.IdentityAllocationModeCRD {
+	switch option.Config.IdentityAllocationMode {
+	case option.IdentityAllocationModeCRD:
+		if !k8s.IsEnabled() {
+			log.Fatal("CRD Identity allocation mode requires k8s to be configured.")
+		}
+
 		startManagingK8sIdentities()
 
 		if identityGCInterval != time.Duration(0) {

daemon/daemon_main.go
@@ -999,6 +999,12 @@ func initEnv(cmd *cobra.Command) {
 		}
 	}
 
+	// This check is here instead of in DaemonConfig.Populate (invoked at the
+	// start of this function as option.Config.Populate) to avoid an import loop.
+	if option.Config.IdentityAllocationMode == option.IdentityAllocationModeCRD && !k8s.IsEnabled() {
+		log.Fatal("CRD Identity allocation mode requires k8s to be configured.")
+	}
+
 	if option.Config.PProf {
 		pprof.Enable()
 	}

commit 07afe3eb33e8abeaf6f9d0bf596efdd122da2549

operator: Fix usage of IdentityAllocationMode
The backport of [1] missed the fact that cilium-operator does not
use pkg/option.Config.IdentityAllocationMode, but instead uses a local
variable which is initialized from the --identity-allocation-mode flag.
This prevent from the CRD GC being started.

[1]: https://github.com/cilium/cilium/commit/1f907666e82253fb2698cd346068ba10930f1dbd

Fixes: 1f907666e ("daemon/operator: Fatal on startup when Identity CRD is enabled without k8s")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

operator/main.go
@@ -343,7 +343,7 @@ func runOperator(cmd *cobra.Command) {
 		startKvstoreWatchdog()
 	}
 
-	switch option.Config.IdentityAllocationMode {
+	switch identityAllocationMode {
 	case option.IdentityAllocationModeCRD:
 		if !k8s.IsEnabled() {
 			log.Fatal("CRD Identity allocation mode requires k8s to be configured.")