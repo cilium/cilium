cilium/cilium

commit bd5bc5b0aa2bb6e2edcc2bc0e3a0f4ed71f52c04

Fix small CRD issue with toGroups
[ upstream commit 678e7f0205ee7f50566d3c2fdd0bd6a3a63bc54c ]

[ Backporter's notes: Schema version was modified to 1.16 rather than
the latest version from master, to ensure that subsequent updates to
v1.7 or v1.8 would still refresh the schema. ]

Fixes: f0049da61f4f ("pkg/k8s: fix all structural issues with CNP validation")

Signed-off-by: Laurent Bernaille <laurent.bernaille@datadoghq.com>
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/k8s/apis/cilium.io/v2/register.go
@@ -43,7 +43,7 @@ const (
 
 	// CustomResourceDefinitionSchemaVersion is semver-conformant version of CRD schema
 	// Used to determine if CRD needs to be updated in cluster
-	CustomResourceDefinitionSchemaVersion = "1.15"
+	CustomResourceDefinitionSchemaVersion = "1.16"
 
 	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
 	CustomResourceDefinitionSchemaVersionKey = "io.cilium.k8s.crd.schema.version"
@@ -648,12 +648,17 @@ var (
 				},
 			},
 			"toGroups": {
-				Type: "object",
+				Type: "array",
 				Description: `ToGroups is a list of constraints that will
 				gather data from third-party providers and create a new
 				derived policy.`,
-				Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
-					"aws": AWSGroup,
+				Items: &apiextensionsv1beta1.JSONSchemaPropsOrArray{
+					Schema: &apiextensionsv1beta1.JSONSchemaProps{
+						Type: "object",
+						Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
+							"aws": AWSGroup,
+						},
+					},
 				},
 			},
 			"toFQDNs": {

commit 933d25ea18f386215eae98b7fc1abe8aa57ad947

k8s: Fix CRD schema version to 1.15.1
As per the discussions from https://github.com/cilium/cilium/pull/12493,
we should not increment the v1.6 schema version to 1.16 as this would
conflict with early v1.7 cycle versions. Fix it to 1.15.1 instead.

The 1.16 version was never part of a released v1.6.x version of Cilium.

Fixes: bd5bc5b0aa2b ("Fix small CRD issue with toGroups")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/k8s/apis/cilium.io/v2/register.go
@@ -43,7 +43,7 @@ const (
 
 	// CustomResourceDefinitionSchemaVersion is semver-conformant version of CRD schema
 	// Used to determine if CRD needs to be updated in cluster
-	CustomResourceDefinitionSchemaVersion = "1.16"
+	CustomResourceDefinitionSchemaVersion = "1.15.1"
 
 	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
 	CustomResourceDefinitionSchemaVersionKey = "io.cilium.k8s.crd.schema.version"