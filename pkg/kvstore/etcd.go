c29932160e9fc53bfb96bd5a575b7b096477e892
Thomas Graf <thomas@cilium.io>
2020-07-16 11:42:30 +0200

    etcd: Ensure that session renewal controller exit
    [ upstream commit ba9031bb45a0da79ee3a843a8c9b50807d0e40be ]
    
    The controller functions could potentially block forever while waiting
    on channels to close. Bind waiting on the channel and check of the etcd
    version to the controller and etcd client context so these operations
    get cancelled.
    
    MERGE CONFLICT:
    Due to not passing the etcd client context to the controller, the etcd client
    context is an exit case as well.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/kvstore/etcd.go
+++ pkg/kvstore/etcd.go
@@ -435,8 +435,25 @@ func (e *etcdClient) Disconnected() <-chan struct{} {
 }
 
 func (e *etcdClient) renewSession(ctx context.Context) error {
-	<-e.firstSession
-	<-e.session.Done()
+	select {
+	// wait for initial session to be established
+	case <-e.firstSession:
+	// controller has stopped or etcd client is closing
+	case <-ctx.Done():
+		return nil
+	case <-e.client.Ctx().Done():
+		return nil
+	}
+
+	select {
+	// session has ended
+	case <-e.session.Done():
+	// controller has stopped or etcd client is closing
+	case <-ctx.Done():
+		return nil
+	case <-e.client.Ctx().Done():
+		return nil
+	}
 	// This is an attempt to avoid concurrent access of a session that was
 	// already expired. It's not perfect as there is still a period between the
 	// e.session.Done() is closed and the e.Lock() is held where parallel go
