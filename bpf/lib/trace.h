5f4b660a442f3b1eb4453a0732ebed2a887e8513
Joe Stringer <joe@cilium.io>
2020-07-16 15:21:44 -0700

    bpf: Fix monitor aggregation for 'from-network'
    [ upstream commit 037bef5260918fba7fac19971593c80ff020485c ]
    
    Previously, we did not take into account 'from-network' sources in the
    monitor aggregation logic check in `send_trace_notify()`, which was fine
    because we rarely ever sent such events (limited to ipsec for instance).
    However, since commit c470e28a82a9 we also use this in bpf_host which
    suddenly means that any and all traffic from the network will trigger
    monitor events, flooding the monitor output.
    
    Fixes: 7a4b0beccbfe ("bpf: Add MonitorAggregation option")
    Fixes: c470e28a82a9 ("Adds TRACE_TO_NETWORK obs label and trace pkts in to-netdev prog.")
    Fixes: https://github.com/cilium/cilium/issues/12555
    
    Signed-off-by: Joe Stringer <joe@cilium.io>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- bpf/lib/trace.h
+++ bpf/lib/trace.h
@@ -143,6 +143,7 @@ static inline bool emit_trace_notify(__u8 obs_point, __u32 monitor)
 		case TRACE_FROM_HOST:
 		case TRACE_FROM_STACK:
 		case TRACE_FROM_OVERLAY:
+		case TRACE_FROM_NETWORK:
 			return false;
 		default:
 			break;
