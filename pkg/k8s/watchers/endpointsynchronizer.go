cilium/cilium

commit 469475ba4f0b1f8f50a161711be07328734423a3

watchers/endpointsynchronizer: do not delete CEP on initialization
During initialization, it is not required to delete the CEP as we will
overwrite its status regardless of the state stored in k8s.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/endpointsynchronizer.go
@@ -130,34 +130,37 @@ func (epSync *EndpointSynchronizer) RunK8sCiliumEndpointSync(e *endpoint.Endpoin
 						return nil
 					}
 
-					scopedLog.Debug("Deleting CEP during an initialization")
-					err := ciliumClient.CiliumEndpoints(namespace).Delete(ctx, podName, meta_v1.DeleteOptions{})
+					scopedLog.Debug("Getting CEP during an initialization")
+					localCEP, err = ciliumClient.CiliumEndpoints(namespace).Get(ctx, podName, meta_v1.GetOptions{})
 					// It's only an error if it exists but something else happened
-					if err != nil && !k8serrors.IsNotFound(err) {
-						scopedLog.WithError(err).Warn("Error deleting CEP")
-						return err
-					}
+					switch {
+					case k8serrors.IsNotFound(err):
+						// We can't create localCEP directly, it must come from the k8s
+						// server via an API call.
+						cep := &cilium_v2.CiliumEndpoint{
+							ObjectMeta: meta_v1.ObjectMeta{
+								Name: podName,
+							},
+						}
+						if mdl != nil {
+							cep.Status = *mdl
+						}
+						localCEP, err = ciliumClient.CiliumEndpoints(namespace).Create(ctx, cep, meta_v1.CreateOptions{})
+						if err != nil {
+							scopedLog.WithError(err).Error("Cannot create CEP")
+							return err
+						}
 
-					// We can't create localCEP directly, it must come from the k8s
-					// server via an API call.
-					cep := &cilium_v2.CiliumEndpoint{
-						ObjectMeta: meta_v1.ObjectMeta{
-							Name: podName,
-						},
-					}
-					if mdl != nil {
-						cep.Status = *mdl
-					}
-					localCEP, err = ciliumClient.CiliumEndpoints(namespace).Create(ctx, cep, meta_v1.CreateOptions{})
-					if err != nil {
-						scopedLog.WithError(err).Error("Cannot create CEP")
+						// We have successfully created the CEP and can return. Subsequent
+						// runs will update using localCEP.
+						needInit = false
+						return nil
+					case err != nil:
+						scopedLog.WithError(err).Warn("Error getting CEP")
 						return err
-					}
+					default:
 
-					// We have successfully created the CEP and can return. Subsequent
-					// runs will update using localCEP.
-					needInit = false
-					return nil
+					}
 				}
 
 				// We have no localCEP copy. We need to fetch it for updates, below.

commit c7aad694edda4e38d4314c51e41e3f2c886c94ed

k8s/watchers: stop init of local endpoint in success case
If we can get a local copy of the CEP we no longer need to keep getting
it from Kubernetes.

Fixes: 469475ba4f0b ("watchers/endpointsynchronizer: do not delete CEP on initialization")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/endpointsynchronizer.go
@@ -167,16 +167,17 @@ func (epSync *EndpointSynchronizer) RunK8sCiliumEndpointSync(e *endpoint.Endpoin
 							return err
 						}
 
-						needInit = false
-
 						// continue the execution so we update the endpoint
 						// status immediately upon endpoint creation
 					case err != nil:
 						scopedLog.WithError(err).Warn("Error getting CEP")
 						return err
 					default:
-
 					}
+
+					// We return earlier for all error cases so we don't need
+					// to init the local endpoint in non-error cases.
+					needInit = false
 				}
 
 				// We have no localCEP copy. We need to fetch it for updates, below.