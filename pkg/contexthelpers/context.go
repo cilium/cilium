b675a4e4831fe41df0e933a8bd52045ea4520a11
Thomas Graf <thomas@cilium.io>
2020-07-22 15:41:54 +0200

    etcd: Fix incorrect context usage in session renewal
    [ upstream commit 02628547e06fde912a8fafb0a71d61cddc72dae3 ]
    
    The context passed into NewSession() was supposed to enforce a timeout on the
    NewSession() operation which is triggering a Grant() and KeepAlive()
    instruction. However, the context passed into NewSession() will also be
    associated with the resulting lease. As per etcd documentation, this will
    result in:
    
    > If the context is canceled before Close() completes, the session's lease will
    > be abandoned and left to expire instead of being revoked.
    
    Because of this, any session renewal triggering a new session would create a
    session that is immediately closed again due to the context passed into
    NewSession being cancelled when the controller run ends successfully. This
    resulted in any renewed session to have an effective lifetime of 10
    milliseconds before requiring renewal again.
    
    Fixes: #12619
    Fixes: 8524fca879b ("kvstore: Add session renew backoff")
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>
    Signed-off-by: Paul Chaignon <paul@cilium.io>

---
+++ pkg/contexthelpers/context.go
@@ -0,0 +1,43 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package contexthelpers
+
+import (
+	"context"
+	"time"
+)
+
+type SuccessChan chan bool
+
+// NewConditionalTimeoutContext returns a context which is cancelled when
+// success is not reported within the specified timeout
+func NewConditionalTimeoutContext(ctx context.Context, timeout time.Duration) (context.Context, context.CancelFunc, SuccessChan) {
+	ch := make(SuccessChan)
+	c, cancel := context.WithCancel(ctx)
+
+	go func() {
+		select {
+		case success := <-ch:
+			if !success {
+				cancel()
+				return
+			}
+		case <-time.After(timeout):
+			cancel()
+		}
+	}()
+
+	return c, cancel, ch
+}
