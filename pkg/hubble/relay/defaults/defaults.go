75b2748e5bb32ab8e2086085c853201436310b09
Robin Hahling <robin.hahling@gw-computing.net>
2020-07-23 14:39:17 +0200

    hubble/relay: split server code into a relay/observer and relay/server
    This commit extracts the `observerpb.ObserverServer` implementation out
    of `relay.Server` and into a new `relay/observer` package. The rest of
    of `relay.Server` is moved into a new `relay/server` package.
    
    This commit also introduces the `relay/defaults` package to store all
    Hubble Relay related defaults. With the refactoring mentioned above,
    defaults for Hubble Relay were scattered in different packages which is
    not ideal. With the introduction of the `defaults` package, all options
    of `pool`, `observer` and `server` are also hidden now. This forces the
    user to use the `With...()` functions to specify different options than
    the defaults.
    
    When moving the observer server related code into `relay/observer`, it
    also became evident that the `PeerManager` interface in `pool` was too
    specific and did not respect the principle that "Go interfaces generally
    belong in the package that uses values of the interface type, not the
    package that implements those values."[0]. To address this, the
    `Start()` and `Stop()` methods have been removed from the interface and
    the interface has been split into two simpler ones
    (`observer.PeerLister` and `observer.PeerReporter`) that are combined in
    `observer.PeerListReporter`. This allows for the Server to directly
    instantiate a `pool.PeerManager`, which implements both interfaces, and
    provide it to `observer.NewServer()` while retaining the control over
    `Start()/Stop()` to start/stop the peer notifications watching process.
    
    All these changes hopefully lead to better separation of concerns and
    improve testability of individual components.
    
    [0]: https://github.com/golang/go/wiki/CodeReviewComments#interfaces
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

---
+++ pkg/hubble/relay/defaults/defaults.go
@@ -0,0 +1,49 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package defaults
+
+import (
+	"fmt"
+	"time"
+
+	ciliumDefaults "github.com/cilium/cilium/pkg/defaults"
+	hubbledefaults "github.com/cilium/cilium/pkg/hubble/defaults"
+)
+
+const (
+	// DialTimeout is the timeout that is used when establishing a new
+	// connection.
+	DialTimeout = 5 * time.Second
+	// RetryTimeout is the duration to wait between reconnection attempts.
+	RetryTimeout = 30 * time.Second
+	// HubbleTarget is the address of the local Hubble instance.
+	HubbleTarget = "unix://" + ciliumDefaults.HubbleSockPath
+
+	// SortBufferMaxLen is the max number of flows that can be buffered for
+	// sorting before being sen to the client.
+	SortBufferMaxLen = 100
+	// SortBufferDrainTimeout is the rate at which flows are drained from the
+	// sorting buffer when it is not full.
+	SortBufferDrainTimeout = 1 * time.Second
+	// ErrorAggregationWindow is the time window during which errors with the
+	// same message are coalesced.
+	ErrorAggregationWindow = 10 * time.Second
+)
+
+var (
+	// ListenAddress is the address on which the Hubble Relay server listens
+	// for incoming requests.
+	ListenAddress = fmt.Sprintf(":%d", hubbledefaults.RelayPort)
+)
--- pkg/hubble/relay/observer/observer.go
+++ pkg/hubble/relay/observer/observer.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package relay
+package observer
 
 import (
 	"context"
@@ -26,8 +26,6 @@ import (
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 
 	"github.com/golang/protobuf/ptypes"
-	"github.com/sirupsen/logrus"
-	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -43,9 +41,6 @@ func newObserverClient(cc pool.ClientConn) observerpb.ObserverClient {
 	}
 	return nil
 }
-
-// ensure that Server implements the observer.ObserverServer interface.
-var _ observerpb.ObserverServer = (*Server)(nil)
 
 func retrieveFlowsFromPeer(
 	ctx context.Context,
@@ -233,160 +228,4 @@ func aggregateErrors(
 
 	}()
 	return aggregated
-}
-
-// GetFlows implements observer.ObserverServer.GetFlows by proxying requests to
-// the hubble instance the proxy is connected to.
-func (s *Server) GetFlows(req *observerpb.GetFlowsRequest, stream observerpb.Observer_GetFlowsServer) error {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	go func() {
-		select {
-		case <-s.stop:
-			cancel()
-		case <-ctx.Done():
-		}
-	}()
-
-	peers := s.pm.List()
-	qlen := s.opts.SortBufferMaxLen // we don't want to buffer too many flows
-	if nqlen := req.GetNumber() * uint64(len(peers)); nqlen > 0 && nqlen < uint64(qlen) {
-		// don't make the queue bigger than necessary as it would be a problem
-		// with the priority queue (we pop out when the queue is full)
-		qlen = int(nqlen)
-	}
-
-	g, gctx := errgroup.WithContext(ctx)
-	flows := make(chan *observerpb.GetFlowsResponse, qlen)
-	var connectedNodes, unavailableNodes []string
-
-	for _, p := range peers {
-		p := p
-		if p.Conn == nil {
-			s.log.WithField("address", p.Address.String()).Infof(
-				"No connection to peer %s, skipping", p.Name,
-			)
-			s.pm.ReportOffline(p.Name)
-			unavailableNodes = append(unavailableNodes, p.Name)
-			continue
-		}
-		connectedNodes = append(connectedNodes, p.Name)
-		g.Go(func() error {
-			// retrieveFlowsFromPeer returns blocks until the peer finishes
-			// the request by closing the connection, an error occurs,
-			// or gctx expires.
-			err := retrieveFlowsFromPeer(gctx, p.Conn, req, flows)
-			if err != nil {
-				s.log.WithFields(logrus.Fields{
-					"error": err,
-					"peer":  p,
-				}).Warning("Failed to retrieve flows from peer")
-				select {
-				case flows <- nodeStatusError(err, p.Name):
-				case <-gctx.Done():
-				}
-			}
-			return nil
-		})
-	}
-	go func() {
-		g.Wait()
-		close(flows)
-	}()
-
-	aggregated := aggregateErrors(ctx, flows, s.opts.ErrorAggregationWindow)
-	sortedFlows := sortFlows(ctx, aggregated, qlen, s.opts.SortBufferDrainTimeout)
-
-	// inform the client about the nodes from which we expect to receive flows first
-	if len(connectedNodes) > 0 {
-		status := nodeStatusEvent(relaypb.NodeState_NODE_CONNECTED, connectedNodes...)
-		if err := stream.Send(status); err != nil {
-			return err
-		}
-	}
-	if len(unavailableNodes) > 0 {
-		status := nodeStatusEvent(relaypb.NodeState_NODE_UNAVAILABLE, unavailableNodes...)
-		if err := stream.Send(status); err != nil {
-			return err
-		}
-	}
-
-sortedFlowsLoop:
-	for {
-		select {
-		case flow, ok := <-sortedFlows:
-			if !ok {
-				break sortedFlowsLoop
-			}
-			if err := stream.Send(flow); err != nil {
-				return err
-			}
-		case <-ctx.Done():
-			break sortedFlowsLoop
-		}
-	}
-	return g.Wait()
-}
-
-// ServerStatus implements observer.ObserverServer.ServerStatus by aggregating
-// the ServerStatus answer of all hubble peers.
-func (s *Server) ServerStatus(ctx context.Context, req *observerpb.ServerStatusRequest) (*observerpb.ServerStatusResponse, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	g, ctx := errgroup.WithContext(ctx)
-	go func() {
-		select {
-		case <-s.stop:
-			cancel()
-		case <-ctx.Done():
-		}
-	}()
-
-	peers := s.pm.List()
-	statuses := make(chan *observerpb.ServerStatusResponse, len(peers))
-	for _, p := range peers {
-		p := p
-		if p.Conn == nil {
-			s.log.WithField("address", p.Address.String()).Infof(
-				"No connection to peer %s, skipping", p.Name,
-			)
-			s.pm.ReportOffline(p.Name)
-			continue
-		}
-		g.Go(func() error {
-			client := newObserverClient(p.Conn)
-			status, err := client.ServerStatus(ctx, req)
-			if err != nil {
-				s.log.WithFields(logrus.Fields{
-					"error": err,
-					"peer":  p,
-				}).Warning("Failed to retrieve server status")
-				return nil
-			}
-			select {
-			case statuses <- status:
-			case <-ctx.Done():
-			}
-			return nil
-		})
-	}
-	go func() {
-		g.Wait()
-		close(statuses)
-	}()
-	resp := &observerpb.ServerStatusResponse{}
-	for status := range statuses {
-		if status == nil {
-			continue
-		}
-		resp.MaxFlows += status.MaxFlows
-		resp.NumFlows += status.NumFlows
-		resp.SeenFlows += status.SeenFlows
-		// use the oldest uptime as a reference for the uptime as cumulating
-		// values would make little sense
-		if resp.UptimeNs == 0 || resp.UptimeNs > status.UptimeNs {
-			resp.UptimeNs = status.UptimeNs
-		}
-	}
-	return resp, g.Wait()
 }
