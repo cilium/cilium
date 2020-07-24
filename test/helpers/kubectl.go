5c2adf3a6707b5356e839c294ef8d182d75cf44c
Paul Chaignon <paul@cilium.io>
2020-07-24 19:34:17 +0200

    test/helpers: New WaitTerminatingPodsInNsWithFilter helper
    [ upstream commit b2f1bf0b6ab668518c6c94c5f8ebd9e34cdc2b54 ]
    
    WaitTerminatingPodsInNsWithFilter is similar to the existing
    WaitTerminatingPodsInNs helper. It waits for a set of pods in state
    terminating to be removed from a specific namespace, but only applies to
    pods matching a given filter.
    
    This commit also renames the helper functions
    WaitCleanAllTerminatingPodsXXX to WaitTerminatingPodsXXX for brievity.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- test/helpers/kubectl.go
+++ test/helpers/kubectl.go
@@ -2042,18 +2042,25 @@ func (kub *Kubectl) WaitForKubeDNSEntry(serviceName, serviceNamespace string) er
 		&TimeoutConfig{Timeout: DNSHelperTimeout})
 }
 
-// WaitCleanAllTerminatingPods waits until all nodes that are in `Terminating`
+// WaitTerminatingPods waits until all nodes that are in `Terminating`
 // state are deleted correctly in the platform. In case of excedding the
-// given timeout (in seconds) it returns an error
+// given timeout (in seconds) it returns an error.
 
-func (kub *Kubectl) WaitCleanAllTerminatingPods(timeout time.Duration) error {
-	return kub.WaitCleanAllTerminatingPodsInNs("", timeout)
+func (kub *Kubectl) WaitTerminatingPods(timeout time.Duration) error {
+	return kub.WaitTerminatingPodsInNsWithFilter("", "", timeout)
 }
 
-// WaitCleanAllTerminatingPodsInNs waits until all nodes that are in `Terminating`
+// WaitTerminatingPodsInNs waits until all nodes that are in `Terminating`
 // state are deleted correctly in the platform. In case of excedding the
-// given timeout (in seconds) it returns an error
-func (kub *Kubectl) WaitCleanAllTerminatingPodsInNs(ns string, timeout time.Duration) error {
+// given timeout (in seconds) it returns an error.
+func (kub *Kubectl) WaitTerminatingPodsInNs(ns string, timeout time.Duration) error {
+	return kub.WaitTerminatingPodsInNsWithFilter(ns, "", timeout)
+}
+
+// WaitTerminatingPodsInNs waits until all nodes that are in `Terminating`
+// state are deleted correctly in the platform. In case of excedding the
+// given timeout (in seconds) it returns an error.
+func (kub *Kubectl) WaitTerminatingPodsInNsWithFilter(ns, filter string, timeout time.Duration) error {
 	body := func() bool {
 		where := ns
 		if where == "" {
@@ -2062,8 +2069,8 @@ func (kub *Kubectl) WaitCleanAllTerminatingPodsInNs(ns string, timeout time.Dura
 			where = "-n " + where
 		}
 		res := kub.ExecShort(fmt.Sprintf(
-			"%s get pods %s -o jsonpath='{.items[*].metadata.deletionTimestamp}'",
-			KubectlCmd, where))
+			"%s get pods %s %s -o jsonpath='{.items[*].metadata.deletionTimestamp}'",
+			KubectlCmd, filter, where))
 		if !res.WasSuccessful() {
 			return false
 		}
