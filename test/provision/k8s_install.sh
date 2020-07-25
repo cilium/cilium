2e59e8436a9abb5ed25cb94965fce196024b9488
André Martins <andre@cilium.io>
2020-07-25 15:52:25 +0200

    k8s: update k8s dependencies to 1.17.9
    Also update k8s testing versions to 1.17.9
    
    Signed-off-by: André Martins <andre@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -246,7 +246,7 @@ case $K8S_VERSION in
         ;;
     "1.17")
         KUBERNETES_CNI_VERSION="0.7.5"
-        K8S_FULL_VERSION="1.17.8"
+        K8S_FULL_VERSION="1.17.9"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
