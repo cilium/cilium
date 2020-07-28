cilium/cilium

commit 56a9c1fe93686d05e153c025865d2042127da4f1

pkg/k8s: remove manual generated DeepCopyInto function
Replace the manual creation of the DeepCopyInto function with an
autogenerated one.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/apis/cilium.io/v2/types.go
@@ -29,12 +29,9 @@ import (
 	k8sCiliumUtils "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/utils"
 	k8sUtils "github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/labels"
-	"github.com/cilium/cilium/pkg/logging"
-	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node/addressing"
 	"github.com/cilium/cilium/pkg/policy/api"
 
-	"github.com/go-openapi/swag"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -42,11 +39,6 @@ import (
 const (
 	// subsysK8s is the value for logfields.LogSubsys
 	subsysK8s = "k8s"
-)
-
-var (
-	// log is the k8s package logger object.
-	log = logging.DefaultLogger.WithField(logfields.LogSubsys, subsysK8s)
 )
 
 // +genclient
@@ -386,7 +378,6 @@ type CiliumEndpoint struct {
 // deepcopy for EndpointStatus but not for the various models.* types it
 // includes. We can't generate functions for classes in other packages, nor can
 // we change the models.Endpoint type to use proxy types we define here.
-// +k8s:deepcopy-gen=false
 type EndpointStatus struct {
 	// The cilium-agent-local ID of the endpoint
 	ID int64 `json:"id,omitempty"`
@@ -451,7 +442,6 @@ func (c ControllerList) Sort() {
 }
 
 // ControllerStatus is the status of a failing controller
-// +k8s:deepcopy-gen=false
 type ControllerStatus struct {
 	// Name is the name of the controller
 	Name string `json:"name,omitempty"`
@@ -479,14 +469,12 @@ type ControllerStatusStatus struct {
 
 // EndpointPolicy represents the endpoint's policy by listing all allowed
 // ingress and egress identities in combination with L4 port and protocol
-// +k8s:deepcopy-gen=false
 type EndpointPolicy struct {
 	Ingress *EndpointPolicyDirection `json:"ingress,omitempty"`
 	Egress  *EndpointPolicyDirection `json:"egress,omitempty"`
 }
 
 // EndpointPolicyDirection is the list of allowed identities per direction
-// +k8s:deepcopy-gen=false
 type EndpointPolicyDirection struct {
 	Enforcing bool                `json:"enforcing"`
 	Allowed   AllowedIdentityList `json:"allowed,omitempty"`
@@ -496,7 +484,6 @@ type EndpointPolicyDirection struct {
 
 // AllowedIdentityTuple specifies an allowed peer by identity, destination port
 // and protocol
-// +k8s:deepcopy-gen=false
 type AllowedIdentityTuple struct {
 	Identity       uint64            `json:"identity,omitempty"`
 	IdentityLabels map[string]string `json:"identity-labels,omitempty"`
@@ -623,40 +610,6 @@ type EndpointNetworking struct {
 	// NodeIP is the IP of the node the endpoint is running on. The IP must
 	// be reachable between nodes.
 	NodeIP string `json:"node,omitempty"`
-}
-
-// DeepCopyInto is an inefficient hack to allow reusing models.Endpoint in the
-// CiliumEndpoint CRD.
-func (m *EndpointStatus) DeepCopyInto(out *EndpointStatus) {
-	*out = *m
-	b, err := (*EndpointStatus)(m).MarshalBinary()
-	if err != nil {
-		log.WithError(err).Error("Cannot marshal EndpointStatus during EndpointStatus deepcopy")
-		return
-	}
-	err = (*EndpointStatus)(out).UnmarshalBinary(b)
-	if err != nil {
-		log.WithError(err).Error("Cannot unmarshal EndpointStatus during EndpointStatus deepcopy")
-		return
-	}
-}
-
-// MarshalBinary interface implementation
-func (m *EndpointStatus) MarshalBinary() ([]byte, error) {
-	if m == nil {
-		return nil, nil
-	}
-	return swag.WriteJSON(m)
-}
-
-// UnmarshalBinary interface implementation
-func (m *EndpointStatus) UnmarshalBinary(b []byte) error {
-	var res EndpointStatus
-	if err := swag.ReadJSON(b, &res); err != nil {
-		return err
-	}
-	*m = res
-	return nil
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object

api/v1/models/c_id_r_policy.go
@@ -18,6 +18,8 @@ import (
 
 // CIDRPolicy CIDR endpoint policy
 //
+// +k8s:deepcopy-gen=true
+//
 // swagger:model CIDRPolicy
 type CIDRPolicy struct {
 

api/v1/models/controller_status.go
@@ -128,6 +128,7 @@ func (m *ControllerStatus) UnmarshalBinary(b []byte) error {
 // ControllerStatusConfiguration Configuration of controller
 //
 // +deepequal-gen=true
+// +k8s:deepcopy-gen=true
 //
 // swagger:model ControllerStatusConfiguration
 type ControllerStatusConfiguration struct {

api/v1/models/endpoint_policy.go
@@ -16,6 +16,8 @@ import (
 
 // EndpointPolicy Policy information of an endpoint
 //
+// +k8s:deepcopy-gen=true
+//
 // swagger:model EndpointPolicy
 type EndpointPolicy struct {
 

api/v1/models/l4_policy.go
@@ -18,6 +18,8 @@ import (
 
 // L4Policy L4 endpoint policy
 //
+// +k8s:deepcopy-gen=true
+//
 // swagger:model L4Policy
 type L4Policy struct {
 

api/v1/models/policy_rule.go
@@ -15,6 +15,8 @@ import (
 
 // PolicyRule A policy rule including the rule labels it derives from
 //
+// +k8s:deepcopy-gen=true
+//
 // swagger:model PolicyRule
 type PolicyRule struct {
 

api/v1/models/zz_generated.deepcopy.go
@@ -23,6 +23,44 @@ import (
 )
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CIDRPolicy) DeepCopyInto(out *CIDRPolicy) {
+	*out = *in
+	if in.Egress != nil {
+		in, out := &in.Egress, &out.Egress
+		*out = make([]*PolicyRule, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(PolicyRule)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	if in.Ingress != nil {
+		in, out := &in.Ingress, &out.Ingress
+		*out = make([]*PolicyRule, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(PolicyRule)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CIDRPolicy.
+func (in *CIDRPolicy) DeepCopy() *CIDRPolicy {
+	if in == nil {
+		return nil
+	}
+	out := new(CIDRPolicy)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *ClusterMeshStatus) DeepCopyInto(out *ClusterMeshStatus) {
 	*out = *in
 	if in.Clusters != nil {
@@ -109,6 +147,24 @@ func (in *ControllerStatus) DeepCopy() *ControllerStatus {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ControllerStatusConfiguration) DeepCopyInto(out *ControllerStatusConfiguration) {
+	*out = *in
+	in.ErrorRetryBase.DeepCopyInto(&out.ErrorRetryBase)
+	in.Interval.DeepCopyInto(&out.Interval)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ControllerStatusConfiguration.
+func (in *ControllerStatusConfiguration) DeepCopy() *ControllerStatusConfiguration {
+	if in == nil {
+		return nil
+	}
+	out := new(ControllerStatusConfiguration)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *ControllerStatusStatus) DeepCopyInto(out *ControllerStatusStatus) {
 	*out = *in
 	in.LastFailureTimestamp.DeepCopyInto(&out.LastFailureTimestamp)
@@ -122,6 +178,42 @@ func (in *ControllerStatusStatus) DeepCopy() *ControllerStatusStatus {
 		return nil
 	}
 	out := new(ControllerStatusStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *EndpointPolicy) DeepCopyInto(out *EndpointPolicy) {
+	*out = *in
+	if in.AllowedEgressIdentities != nil {
+		in, out := &in.AllowedEgressIdentities, &out.AllowedEgressIdentities
+		*out = make([]int64, len(*in))
+		copy(*out, *in)
+	}
+	if in.AllowedIngressIdentities != nil {
+		in, out := &in.AllowedIngressIdentities, &out.AllowedIngressIdentities
+		*out = make([]int64, len(*in))
+		copy(*out, *in)
+	}
+	if in.CidrPolicy != nil {
+		in, out := &in.CidrPolicy, &out.CidrPolicy
+		*out = new(CIDRPolicy)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.L4 != nil {
+		in, out := &in.L4, &out.L4
+		*out = new(L4Policy)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new EndpointPolicy.
+func (in *EndpointPolicy) DeepCopy() *EndpointPolicy {
+	if in == nil {
+		return nil
+	}
+	out := new(EndpointPolicy)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -376,6 +468,44 @@ func (in *KubeProxyReplacementFeaturesSessionAffinity) DeepCopy() *KubeProxyRepl
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *L4Policy) DeepCopyInto(out *L4Policy) {
+	*out = *in
+	if in.Egress != nil {
+		in, out := &in.Egress, &out.Egress
+		*out = make([]*PolicyRule, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(PolicyRule)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	if in.Ingress != nil {
+		in, out := &in.Ingress, &out.Ingress
+		*out = make([]*PolicyRule, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(PolicyRule)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new L4Policy.
+func (in *L4Policy) DeepCopy() *L4Policy {
+	if in == nil {
+		return nil
+	}
+	out := new(L4Policy)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *Masquerading) DeepCopyInto(out *Masquerading) {
 	*out = *in
 	return
@@ -450,6 +580,33 @@ func (in *NodeElement) DeepCopy() *NodeElement {
 		return nil
 	}
 	out := new(NodeElement)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PolicyRule) DeepCopyInto(out *PolicyRule) {
+	*out = *in
+	if in.DerivedFromRules != nil {
+		in, out := &in.DerivedFromRules, &out.DerivedFromRules
+		*out = make([][]string, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = make([]string, len(*in))
+				copy(*out, *in)
+			}
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PolicyRule.
+func (in *PolicyRule) DeepCopy() *PolicyRule {
+	if in == nil {
+		return nil
+	}
+	out := new(PolicyRule)
 	in.DeepCopyInto(out)
 	return out
 }

api/v1/openapi.yaml
@@ -1226,7 +1226,10 @@ definitions:
       - audit-egress
       - audit-both
   EndpointPolicy:
-    description: Policy information of an endpoint
+    description: |-
+      Policy information of an endpoint
+
+      +k8s:deepcopy-gen=true
     type: object
     properties:
       policy-revision:
@@ -1258,7 +1261,10 @@ definitions:
       cidr-policy:
         "$ref": "#/definitions/CIDRPolicy"
   PolicyRule:
-    description: A policy rule including the rule labels it derives from
+    description: |-
+      A policy rule including the rule labels it derives from
+
+      +k8s:deepcopy-gen=true
     properties:
       rule:
         description: The policy rule as json
@@ -1271,7 +1277,10 @@ definitions:
           items:
             type: string
   L4Policy:
-    description: L4 endpoint policy
+    description: |-
+      L4 endpoint policy
+
+      +k8s:deepcopy-gen=true
     type: object
     properties:
       ingress:
@@ -1285,7 +1294,10 @@ definitions:
         items:
           "$ref": "#/definitions/PolicyRule"
   CIDRPolicy:
-    description: CIDR endpoint policy
+    description: |-
+      CIDR endpoint policy
+
+      +k8s:deepcopy-gen=true
     type: object
     properties:
       ingress:
@@ -2410,6 +2422,7 @@ definitions:
           Configuration of controller
 
           +deepequal-gen=true
+          +k8s:deepcopy-gen=true
         type: object
         properties:
           interval:

commit cd65b8510968e00e163ef74379db654f09b73089

pkg/k8s: remove unused variable
Fixes: 56a9c1fe9368 ("pkg/k8s: remove manual generated DeepCopyInto function")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/apis/cilium.io/v2/types.go
@@ -36,11 +36,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const (
-	// subsysK8s is the value for logfields.LogSubsys
-	subsysK8s = "k8s"
-)
-
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 