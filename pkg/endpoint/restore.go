59df741204aba652cede412f5c8cf109a7044a85
Jarno Rajahalme <jarno@covalent.io>
2020-07-30 15:49:35 -0700

    dnsproxy: Use restored rules during restart
    [ upstream commit f1ac9a21e659a249016d694582736a9ac7012f94 ]
    
    Store current DNS rules with the Endpoint and use them in the DNS
    proxy during initial regeneration of the restrored endpoints.
    
    DNS proxy starts with restored DNS rules based on allowed IP
    addresses. These rules are removed for each endpoint as soon as the
    first regeneration completes. Such restored rules should allow DNS
    requests to be served, but for new DNS resolutions to be added to the
    Endpoint's policy the restored endpoint's must still have their first
    regeneration completed.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- pkg/endpoint/restore.go
+++ pkg/endpoint/restore.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 	"github.com/cilium/cilium/pkg/fqdn"
+	"github.com/cilium/cilium/pkg/fqdn/restore"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/labels"
@@ -378,6 +379,7 @@ func (e *Endpoint) toSerializedEndpoint() *serializableEndpoint {
 		NodeMAC:               e.nodeMAC,
 		SecurityIdentity:      e.SecurityIdentity,
 		Options:               e.Options,
+		DNSRules:              e.DNSRules,
 		DNSHistory:            e.DNSHistory,
 		DNSZombies:            e.DNSZombies,
 		K8sPodName:            e.K8sPodName,
@@ -452,6 +454,9 @@ type serializableEndpoint struct {
 	// Options determine the datapath configuration of the endpoint.
 	Options *option.IntOptions
 
+	// DNSRules is the collection of current DNS rules for this endpoint.
+	DNSRules restore.DNSRules
+
 	// DNSHistory is the collection of still-valid DNS responses intercepted for
 	// this endpoint.
 	DNSHistory *fqdn.DNSCache
@@ -511,6 +516,7 @@ func (ep *Endpoint) fromSerializedEndpoint(r *serializableEndpoint) {
 	ep.IPv4 = r.IPv4
 	ep.nodeMAC = r.NodeMAC
 	ep.SecurityIdentity = r.SecurityIdentity
+	ep.DNSRules = r.DNSRules
 	ep.DNSHistory = r.DNSHistory
 	ep.DNSZombies = r.DNSZombies
 	ep.K8sPodName = r.K8sPodName
