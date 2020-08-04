071f53e125102aaf09f22c336823477e18c8373b
Deepesh Pathak <deepshpathak@gmail.com>
2020-08-04 16:58:31 +0530

    test/helpers: remove unused cilium uninstall methods
    Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

--- test/helpers/kubectl.go
+++ test/helpers/kubectl.go
@@ -2301,20 +2301,6 @@ func (kub *Kubectl) DeleteHubbleRelay(ns string) error {
 	return kub.waitToDelete("HubbleRelay", HubbleRelayLabel)
 }
 
-// ciliumUninstallHelm uninstalls Cilium with the Helm options provided.
-func (kub *Kubectl) ciliumUninstallHelm(filename string, options map[string]string) error {
-	if err := kub.generateCiliumYaml(options, filename); err != nil {
-		return err
-	}
-
-	res := kub.Delete(filename)
-	if !res.WasSuccessful() {
-		return res.GetErr("Unable to delete YAML")
-	}
-
-	return nil
-}
-
 // CiliumInstall installs Cilium with the provided Helm options.
 func (kub *Kubectl) CiliumInstall(filename string, options map[string]string) error {
 	// First try to remove any existing cilium install. This is done by removing resources
@@ -2365,11 +2351,6 @@ func (kub *Kubectl) RunHelm(action, repo, helmName, version, namespace string, o
 		"--version=%s "+
 		"--namespace=%s "+
 		"%s", action, helmName, repo, version, namespace, optionsString)), nil
-}
-
-// CiliumUninstall uninstalls Cilium with the provided Helm options.
-func (kub *Kubectl) CiliumUninstall(filename string, options map[string]string) error {
-	return kub.ciliumUninstallHelm(filename, options)
 }
 
 // GetCiliumPods returns a list of all Cilium pods in the specified namespace,
