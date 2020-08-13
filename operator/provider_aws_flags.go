cilium/cilium

commit 053fc866ab534703601123ea05e7c7ec6125dc91

operator: Build 3 new slimmer binaries
- add per-provider binaries
  - handle --ipam flag
  - avoid adding deprecated flags in new binaries
- continue building fat binary and maintain the same name
  for compatibility

Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

operator/provider_aws_flags.go
@@ -1,0 +1,44 @@
+// Copyright 2017-2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//+build ipam_provider_aws
+
+package main
+
+import (
+	operatorOption "github.com/cilium/cilium/operator/option"
+
+	"github.com/cilium/cilium/pkg/option"
+)
+
+func init() {
+	flags := rootCmd.Flags()
+
+	flags.Var(option.NewNamedMapOptions(operatorOption.AWSInstanceLimitMapping, &operatorOption.Config.AWSInstanceLimitMapping, nil),
+		operatorOption.AWSInstanceLimitMapping,
+		`Add or overwrite mappings of AWS instance limit in the form of `+
+			`{"AWS instance type": "Maximum Network Interfaces","IPv4 Addresses `+
+			`per Interface","IPv6 Addresses per Interface"}. cli example: `+
+			`--aws-instance-limit-mapping=a1.medium=2,4,4 `+
+			`--aws-instance-limit-mapping=a2.somecustomflavor=4,5,6 `+
+			`configmap example: {"a1.medium": "2,4,4", "a2.somecustomflavor": "4,5,6"}`)
+	option.BindEnv(operatorOption.AWSInstanceLimitMapping)
+
+	flags.Bool(operatorOption.AWSReleaseExcessIPs, false, "Enable releasing excess free IP addresses from AWS ENI.")
+	option.BindEnv(operatorOption.AWSReleaseExcessIPs)
+
+	flags.Var(option.NewNamedMapOptions(operatorOption.ENITags, &operatorOption.Config.ENITags, nil),
+		operatorOption.ENITags, "ENI tags in the form of k1=v1 (multiple k/v pairs can be passed by repeating the CLI flag)")
+	option.BindEnv(operatorOption.ENITags)
+}

operator/provider_azure_flags.go
@@ -1,0 +1,33 @@
+// Copyright 2017-2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//+build ipam_provider_azure
+
+package main
+
+import (
+	operatorOption "github.com/cilium/cilium/operator/option"
+
+	"github.com/cilium/cilium/pkg/option"
+)
+
+func init() {
+	flags := rootCmd.Flags()
+
+	flags.String(operatorOption.AzureSubscriptionID, "", "Subscription ID to access Azure API")
+	option.BindEnvWithLegacyEnvFallback(operatorOption.AzureSubscriptionID, "AZURE_SUBSCRIPTION_ID")
+
+	flags.String(operatorOption.AzureResourceGroup, "", "Resource group to use for Azure IPAM")
+	option.BindEnvWithLegacyEnvFallback(operatorOption.AzureResourceGroup, "AZURE_RESOURCE_GROUP")
+}

Documentation/update-cmdref.sh
@@ -14,6 +14,9 @@ generators=(
     "bugtool/cilium-bugtool cmdref -d"
     "cilium-health/cilium-health --cmdref"
     "operator/cilium-operator --cmdref"
+    "operator/cilium-operator-aws --cmdref"
+    "operator/cilium-operator-azure --cmdref"
+    "operator/cilium-operator-generic --cmdref"
 )
 
 for g in "${generators[@]}" ; do

cilium-operator.Dockerfile
@@ -4,7 +4,9 @@ FROM docker.io/library/golang:1.14.3 as builder
 ARG CILIUM_SHA=""
 LABEL cilium-sha=${CILIUM_SHA}
 LABEL maintainer="maintainer@cilium.io"
+
 ADD . /go/src/github.com/cilium/cilium
+
 WORKDIR /go/src/github.com/cilium/cilium/operator
 ARG NOSTRIP
 ARG LOCKDEBUG
@@ -20,6 +22,10 @@ ARG CILIUM_SHA=""
 LABEL cilium-sha=${CILIUM_SHA}
 LABEL maintainer="maintainer@cilium.io"
 COPY --from=builder /go/src/github.com/cilium/cilium/operator/cilium-operator /usr/bin/cilium-operator
+COPY --from=builder /go/src/github.com/cilium/cilium/operator/cilium-operator-generic /usr/bin/cilium-operator-generic
+COPY --from=builder /go/src/github.com/cilium/cilium/operator/cilium-operator-aws /usr/bin/cilium-operator-aws
+COPY --from=builder /go/src/github.com/cilium/cilium/operator/cilium-operator-azure /usr/bin/cilium-operator-azure
+
 COPY --from=certs /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
 WORKDIR /
 CMD ["/usr/bin/cilium-operator"]

operator/.gitignore
@@ -1,1 +1,4 @@
 cilium-operator
+cilium-operator-aws
+cilium-operator-azure
+cilium-operator-generic

operator/Makefile
@@ -3,16 +3,20 @@
 
 include ../Makefile.defs
 
-# Build all ipam managers in development environments
-ifeq ($(PKG_BUILD),)
-	EXTRA_GO_BUILD_FLAGS += -tags ipam_provider_aws,ipam_provider_azure,ipam_provider_operator
-endif
+TARGETS := cilium-operator cilium-operator-generic cilium-operator-aws cilium-operator-azure
 
-TARGET := cilium-operator
+.PHONY: all $(TARGETS) clean install
 
-.PHONY: all $(TARGET) clean install
+all: $(TARGETS)
 
-all: $(TARGET)
+cilium-operator: OPERATOR_TAGS=ipam_provider_aws,ipam_provider_azure,ipam_provider_operator
+cilium-operator-generic: OPERATOR_TAGS=ipam_provider_operator
+cilium-operator-aws: OPERATOR_TAGS=ipam_provider_aws
+cilium-operator-azure: OPERATOR_TAGS=ipam_provider_azure
+
+$(TARGETS):
+	@$(ECHO_GO)
+	$(QUIET)$(GO_BUILD) -tags $(OPERATOR_TAGS) -o $(@)
 
 $(TARGET):
 	@$(ECHO_GO)
@@ -20,9 +24,5 @@ $(TARGET):
 
 clean:
 	@$(ECHO_CLEAN)
-	-$(QUIET)rm -f $(TARGET)
-	$(QUIET)$(GO_CLEAN)
-
-install:
-	$(QUIET)$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)
-	$(QUIET)$(INSTALL) -m 0755 $(TARGET) $(DESTDIR)$(BINDIR)
+	$(QUIET)rm -f $(TARGETS)
+	$(GO) clean $(GOCLEAN)

operator/flags.go
@@ -29,7 +29,7 @@ import (
 )
 
 func init() {
-	cobra.OnInitialize(option.InitConfig("Cilium-Operator", "cilium-operator"))
+	cobra.OnInitialize(option.InitConfig("Cilium-Operator", "cilium-operators"))
 
 	flags := rootCmd.Flags()
 
@@ -44,29 +44,6 @@ func init() {
 
 	flags.Float64(operatorOption.IPAMAPIQPSLimit, defaults.IPAMAPIQPSLimit, "Queries per second limit when accessing external IPAM APIs")
 	option.BindEnv(operatorOption.IPAMAPIQPSLimit)
-
-	flags.String(operatorOption.AzureSubscriptionID, "", "Subscription ID to access Azure API")
-	option.BindEnvWithLegacyEnvFallback(operatorOption.AzureSubscriptionID, "AZURE_SUBSCRIPTION_ID")
-
-	flags.String(operatorOption.AzureResourceGroup, "", "Resource group to use for Azure IPAM")
-	option.BindEnvWithLegacyEnvFallback(operatorOption.AzureResourceGroup, "AZURE_RESOURCE_GROUP")
-
-	flags.Var(option.NewNamedMapOptions(operatorOption.AWSInstanceLimitMapping, &operatorOption.Config.AWSInstanceLimitMapping, nil),
-		operatorOption.AWSInstanceLimitMapping,
-		`Add or overwrite mappings of AWS instance limit in the form of `+
-			`{"AWS instance type": "Maximum Network Interfaces","IPv4 Addresses `+
-			`per Interface","IPv6 Addresses per Interface"}. cli example: `+
-			`--aws-instance-limit-mapping=a1.medium=2,4,4 `+
-			`--aws-instance-limit-mapping=a2.somecustomflavor=4,5,6 `+
-			`configmap example: {"a1.medium": "2,4,4", "a2.somecustomflavor": "4,5,6"}`)
-	option.BindEnv(operatorOption.AWSInstanceLimitMapping)
-
-	flags.Bool(operatorOption.AWSReleaseExcessIPs, false, "Enable releasing excess free IP addresses from AWS ENI.")
-	option.BindEnv(operatorOption.AWSReleaseExcessIPs)
-
-	flags.Var(option.NewNamedMapOptions(operatorOption.ENITags, &operatorOption.Config.ENITags, nil),
-		operatorOption.ENITags, "ENI tags in the form of k1=v1 (multiple k/v pairs can be passed by repeating the CLI flag)")
-	option.BindEnv(operatorOption.ENITags)
 
 	flags.StringToStringVar(&operatorOption.Config.IPAMSubnetsTags, operatorOption.IPAMSubnetsTags, operatorOption.Config.IPAMSubnetsTags,
 		"Subnets tags in the form of k1=v1,k2=v2 (multiple k/v pairs can also be passed by repeating the CLI flag")
@@ -138,8 +115,73 @@ func init() {
 	flags.Bool(operatorOption.EnableMetrics, false, "Enable Prometheus metrics")
 	option.BindEnv(operatorOption.EnableMetrics)
 
-	flags.String(option.IPAM, ipamOption.IPAMHostScopeLegacy, "Backend to use for IPAM")
+	var defaultIPAM string
+	switch binaryName {
+	case "cilium-operator":
+		defaultIPAM = ipamOption.IPAMHostScopeLegacy
+	case "cilium-operator-aws":
+		defaultIPAM = ipamOption.IPAMENI
+	case "cilium-operator-azure":
+		defaultIPAM = ipamOption.IPAMAzure
+	case "cilium-operator-generic":
+		defaultIPAM = ipamOption.IPAMOperator
+	}
+
+	flags.String(option.IPAM, defaultIPAM, "Backend to use for IPAM")
 	option.BindEnv(option.IPAM)
+
+	rootCmd.PreRunE = func(cmd *cobra.Command, args []string) error {
+		ipamFlag := cmd.Flag(option.IPAM)
+		if !ipamFlag.Changed {
+			return nil
+		}
+		ipamFlagValue := ipamFlag.Value.String()
+
+		recommendInstead := func() string {
+			switch ipamFlagValue {
+			case ipamOption.IPAMENI:
+				return "cilium-operator-aws"
+			case ipamOption.IPAMAzure:
+				return "cilium-operator-azure"
+			case ipamOption.IPAMKubernetes, ipamOption.IPAMOperator, ipamOption.IPAMCRD:
+				return "cilium-operator-generic"
+			default:
+				return ""
+			}
+		}
+
+		unsupporterErr := func() error {
+			errMsg := fmt.Sprintf("%s doesn't support --%s=%s", binaryName, option.IPAM, ipamFlagValue)
+			if recommendation := recommendInstead(); recommendation != "" {
+				return fmt.Errorf("%s (use %s)", errMsg, recommendation)
+			}
+			return fmt.Errorf(errMsg)
+		}
+
+		switch binaryName {
+		case "cilium-operator":
+			if recommendation := recommendInstead(); recommendation != "" {
+				log.Warnf("cilium-operator will be deprecated in the future, for --%s=%s use %s as it has lower memory footprint", option.IPAM, ipamFlagValue, recommendation)
+			}
+		case "cilium-operator-aws":
+			if ipamFlagValue != ipamOption.IPAMENI {
+				return unsupporterErr()
+			}
+		case "cilium-operator-azure":
+			if ipamFlagValue != ipamOption.IPAMAzure {
+				return unsupporterErr()
+			}
+		case "cilium-operator-generic":
+			switch ipamFlagValue {
+			case ipamOption.IPAMENI, ipamOption.IPAMAzure:
+				return unsupporterErr()
+			case ipamOption.IPAMHostScopeLegacy:
+				return fmt.Errorf("%s, as it is deprecated, but it is still supported by legacy cilium-operator binary)", unsupporterErr().Error())
+			}
+		}
+
+		return nil
+	}
 
 	flags.Duration(operatorOption.IdentityHeartbeatTimeout, 2*defaults.KVstoreLeaseTTL, "Timeout after which identity expires on lack of heartbeat")
 	option.BindEnv(operatorOption.IdentityHeartbeatTimeout)

operator/main.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"time"
 
 	operatorMetrics "github.com/cilium/cilium/operator/metrics"
@@ -47,11 +48,13 @@ import (
 )
 
 var (
-	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "cilium-operator")
+	binaryName = filepath.Base(os.Args[0])
+
+	log = logging.DefaultLogger.WithField(logfields.LogSubsys, binaryName)
 
 	rootCmd = &cobra.Command{
-		Use:   "cilium-operator",
-		Short: "Run the cilium-operator",
+		Use:   binaryName,
+		Short: "Run " + binaryName,
 		Run: func(cmd *cobra.Command, args []string) {
 			cmdRefDir := viper.GetString(option.CMDRef)
 			if cmdRefDir != "" {
@@ -79,7 +82,7 @@ func initEnv() {
 	logging.DefaultLogger.Hooks.Add(metrics.NewLoggingHook(components.CiliumOperatortName))
 
 	// Logging should always be bootstrapped first. Do not add any code above this!
-	logging.SetupLogging(option.Config.LogDriver, logging.LogOptions(option.Config.LogOpt), "cilium-operator", option.Config.Debug)
+	logging.SetupLogging(option.Config.LogDriver, logging.LogOptions(option.Config.LogOpt), binaryName, option.Config.Debug)
 
 	option.LogRegisteredOptions(log)
 }
@@ -168,7 +171,7 @@ func runOperator(cmd *cobra.Command) {
 	case ipamOption.IPAMAzure, ipamOption.IPAMENI, ipamOption.IPAMOperator:
 		alloc, providerBuiltin := allocatorProviders[ipamMode]
 		if !providerBuiltin {
-			log.Fatalf("%s allocator is not supported by this version of cilium-operator", ipamMode)
+			log.Fatalf("%s allocator is not supported by this version of %s", ipamMode, binaryName)
 		}
 
 		if err := alloc.Init(); err != nil {
@@ -219,7 +222,7 @@ func runOperator(cmd *cobra.Command) {
 			if k8s.IsEnabled() {
 				svcURL, isETCDOperator := kvstore.IsEtcdOperator(option.Config.KVStore, option.Config.KVStoreOpt, option.Config.K8sNamespace)
 				if isETCDOperator {
-					scopedLog.Info("cilium-operator running with service synchronization: automatic etcd service translation enabled")
+					scopedLog.Infof("%s running with service synchronization: automatic etcd service translation enabled", binaryName)
 
 					svcGetter := k8s.ServiceIPGetter(&k8sSvcCache)
 
@@ -272,7 +275,7 @@ func runOperator(cmd *cobra.Command) {
 				}
 			}
 		} else {
-			scopedLog.Info("cilium-operator running without service synchronization: automatic etcd service translation disabled")
+			scopedLog.Infof("%s running without service synchronization: automatic etcd service translation disabled", binaryName)
 		}
 		scopedLog.Info("Connecting to kvstore...")
 		if err := kvstore.Setup(context.TODO(), option.Config.KVStore, option.Config.KVStoreOpt, goopts); err != nil {

commit 4c873a3ba7f52bf4d1f565f5e1da72fe36fe37d2

operator: bind provider-specific flags
Provider specific options passed as command line flags are currently
ignore because the flags are not bound. Add the missing viper.BindPFlags
to fix this.

Fixes: #12870
Fixes: 053fc866ab53 ("operator: Build 3 new slimmer binaries")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

operator/provider_aws_flags.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	operatorOption "github.com/cilium/cilium/operator/option"
+	"github.com/spf13/viper"
 
 	"github.com/cilium/cilium/pkg/option"
 )
@@ -44,4 +45,6 @@ func init() {
 
 	flags.Bool(operatorOption.UpdateEC2AdapterLimitViaAPI, false, "Use the EC2 API to update the instance type to adapter limits")
 	option.BindEnv(operatorOption.UpdateEC2AdapterLimitViaAPI)
+
+	viper.BindPFlags(flags)
 }

operator/provider_azure_flags.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	operatorOption "github.com/cilium/cilium/operator/option"
+	"github.com/spf13/viper"
 
 	"github.com/cilium/cilium/pkg/option"
 )
@@ -30,4 +31,6 @@ func init() {
 
 	flags.String(operatorOption.AzureResourceGroup, "", "Resource group to use for Azure IPAM")
 	option.BindEnvWithLegacyEnvFallback(operatorOption.AzureResourceGroup, "AZURE_RESOURCE_GROUP")
+
+	viper.BindPFlags(flags)
 }
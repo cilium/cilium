4c873a3ba7f52bf4d1f565f5e1da72fe36fe37d2
Tobias Klauser <tklauser@distanz.ch>
2020-08-13 15:11:54 +0200

    operator: bind provider-specific flags
    Provider specific options passed as command line flags are currently
    ignore because the flags are not bound. Add the missing viper.BindPFlags
    to fix this.
    
    Fixes: #12870
    Fixes: 053fc866ab53 ("operator: Build 3 new slimmer binaries")
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- operator/provider_aws_flags.go
+++ operator/provider_aws_flags.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	operatorOption "github.com/cilium/cilium/operator/option"
+	"github.com/spf13/viper"
 
 	"github.com/cilium/cilium/pkg/option"
 )
@@ -44,4 +45,6 @@ func init() {
 
 	flags.Bool(operatorOption.UpdateEC2AdapterLimitViaAPI, false, "Use the EC2 API to update the instance type to adapter limits")
 	option.BindEnv(operatorOption.UpdateEC2AdapterLimitViaAPI)
+
+	viper.BindPFlags(flags)
 }
