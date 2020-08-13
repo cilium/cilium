bd6600f0b4774270ab73d900ac9cd408d33c4f37
Martynas Pumputis <m@lambda.lt>
2020-08-13 17:42:31 +0200

    daemon: Rename --enable-loadbalancer-source-range-check
    Rename to --enable-svc-source-range-check, as in the future we might
    enable (e.g. via annotations) the checks for services which type !=
    LoadBalancer.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- daemon/cmd/datapath.go
+++ daemon/cmd/datapath.go
@@ -462,7 +462,7 @@ func (d *Daemon) initMaps() error {
 		}
 	}
 
-	if option.Config.EnableLoadBalancerSourceRangeCheck {
+	if option.Config.EnableSVCSourceRangeCheck {
 		if option.Config.EnableIPv4 {
 			if _, err := lbmap.SourceRange4Map.OpenOrCreate(); err != nil {
 				return err
