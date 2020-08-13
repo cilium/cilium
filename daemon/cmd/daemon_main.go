30bf30dfad90f3cd520e88e09127f812da50d3fa
Martynas Pumputis <m@lambda.lt>
2020-08-13 15:30:30 +0200

    daemon: Enable LB src range check by default
    Disable it when running in the non-strict mode if the full LPM is
    missing, or fail hard if running in the strict mode.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -517,7 +517,7 @@ func init() {
 	flags.Bool(option.EnableNodePort, false, "Enable NodePort type services by Cilium (beta)")
 	option.BindEnv(option.EnableNodePort)
 
-	flags.Bool(option.EnableLoadBalancerSourceRangeCheck, false, "Enable check of loadBalancerSourceRanges")
+	flags.Bool(option.EnableLoadBalancerSourceRangeCheck, true, "Enable check of loadBalancerSourceRanges")
 	option.BindEnv(option.EnableLoadBalancerSourceRangeCheck)
 
 	flags.String(option.NodePortMode, option.NodePortModeSNAT, "BPF NodePort mode (\"snat\", \"dsr\", \"hybrid\")")
