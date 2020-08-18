3030ba6420bc857977269ea40984bb3e5e86e046
Daniel Borkmann <daniel@iogearbox.net>
2020-08-18 10:55:46 +0200

    cilium, test: add various netperf e2e tests
    Add {Pod,Host} to Pod netperf tests over VMs with different bandwidth
    limits in direct routing and tunneling mode.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- test/helpers/cmd.go
+++ test/helpers/cmd.go
@@ -315,6 +315,29 @@ func (res *CmdRes) IntOutput() (int, error) {
 	return strconv.Atoi(strings.TrimSpace(res.stdout.String()))
 }
 
+// FloatOutput returns the stdout of res as a float
+func (res *CmdRes) FloatOutput() (float64, error) {
+	return strconv.ParseFloat(strings.TrimSpace(res.stdout.String()), 64)
+}
+
+// InRange returns nil if res matches the expected value range or error otherwise
+func (res *CmdRes) InRange(expectedValue, maxDeviation int) error {
+	min := expectedValue - maxDeviation
+	max := expectedValue + maxDeviation
+	raw, err := res.FloatOutput()
+	if err != nil {
+		return err
+	}
+	val := int(raw)
+	if val >= min && val <= max {
+		return nil
+	} else {
+		return fmt.Errorf(
+			"Expected result %d (%s) is not in the range of [%d, %d]",
+			val, strings.TrimSpace(res.stdout.String()), min, max)
+	}
+}
+
 // FindResults filters res's stdout using the provided JSONPath filter. It
 // returns an array of the values that match the filter, and an error if
 // the unmarshalling of the stdout of res fails.
