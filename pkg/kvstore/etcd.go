cilium/cilium

commit b95650b30b46b2a095854d3d0043bdc7066aa382

etcd: Shuffle list of etcd endpoints
Shuffling the list of etcd endpoints on each bootstrap has two positive
effects:
 * Agents in the cluster will connect to different etcd members.
 * In case etcd fails to fail-over to due to a bug, a Cilium agent
   restart has a chance to fail-over to a healthy etcd member.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/kvstore/etcd.go
@@ -155,6 +155,12 @@ func (e *etcdModule) getConfig() map[string]string {
 	return getOpts(e.opts)
 }
 
+func shuffleEndpoints(endpoints []string) {
+	randGen.Shuffle(len(endpoints), func(i, j int) {
+		endpoints[i], endpoints[j] = endpoints[j], endpoints[i]
+	})
+}
+
 func (e *etcdModule) newClient(ctx context.Context, opts *ExtraOptions) (BackendOperations, chan error) {
 	errChan := make(chan error, 10)
 
@@ -192,6 +198,13 @@ func (e *etcdModule) newClient(ctx context.Context, opts *ExtraOptions) (Backend
 
 	if e.config.Endpoints == nil && endpointsSet {
 		e.config.Endpoints = []string{endpointsOpt.value}
+	}
+
+	// Shuffle the order of endpoints to avoid all agents connecting to the
+	// same etcd endpoint and to work around etcd client library failover
+	// bugs. (https://github.com/etcd-io/etcd/pull/9860)
+	if e.config.Endpoints != nil {
+		shuffleEndpoints(e.config.Endpoints)
 	}
 
 	for {

pkg/kvstore/etcd_test.go
@@ -1642,3 +1642,25 @@ func TestGetSvcNamespace(t *testing.T) {
 		})
 	}
 }
+func TestShuffleEndpoints(t *testing.T) {
+	s1 := []string{"1", "2", "3", "4", "5"}
+	s2 := make([]string, len(s1))
+	copy(s2, s1)
+
+	var same int
+	for retry := 0; retry < 10; retry++ {
+		same = 0
+		shuffleEndpoints(s2)
+		for i := range s1 {
+			if s1[i] == s2[i] {
+				same++
+			}
+		}
+		if same != len(s1) {
+			break
+		}
+	}
+	if same == len(s1) {
+		t.Errorf("Shuffle() did not modify s2 in 10 retries")
+	}
+}

pkg/rand/safe_rand.go
@@ -83,3 +83,9 @@ func (sr *safeRand) Perm(n int) []int {
 	return v
 
 }
+
+func (sr *safeRand) Shuffle(n int, swap func(i, j int)) {
+	sr.mu.Lock()
+	sr.r.Shuffle(n, swap)
+	sr.mu.Unlock()
+}

pkg/rand/safe_rand_test.go
@@ -18,6 +18,7 @@ package rand
 
 import (
 	"testing"
+	"time"
 )
 
 func TestIntn(t *testing.T) {
@@ -32,5 +33,38 @@ func TestIntn(t *testing.T) {
 		if v0 != v1 {
 			t.Errorf("Intn() returned different values at iteration %d: %d != %d", i, v0, v1)
 		}
+	}
+}
+
+func TestShuffle(t *testing.T) {
+	r0 := NewSafeRand(time.Now().UnixNano())
+
+	s1 := []string{"1", "2", "3", "4", "5"}
+	s2 := make([]string, len(s1))
+	copy(s2, s1)
+
+	var same int
+	for retry := 0; retry < 1; retry++ {
+		same = 0
+
+		r0.Shuffle(len(s2), func(i, j int) {
+			s2[i], s2[j] = s2[j], s2[i]
+		})
+
+		if len(s1) != len(s2) {
+			t.Errorf("Shuffle() resulted in slices of inequal length")
+		}
+
+		for i := range s1 {
+			if s1[i] == s2[i] {
+				same++
+			}
+		}
+		if same != len(s1) {
+			break
+		}
+	}
+	if same == len(s1) {
+		t.Errorf("Shuffle() did not modify s2 in 10 retries")
 	}
 }

commit bc5aae8be976576f395ea43082066673ebd60f2f

pkg/kvstore: set endpoint shuffle in etcd client connectivity
The endpoint shuffle was happening before loading the etcd
configuration. To have the endpoints shuffled we should do it after
loading the configuration from disk.

Fixes: b95650b30b46 ("etcd: Shuffle list of etcd endpoints")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/etcd.go
@@ -201,13 +201,6 @@ func (e *etcdModule) newClient(ctx context.Context, opts *ExtraOptions) (Backend
 		e.config.Endpoints = []string{endpointsOpt.value}
 	}
 
-	// Shuffle the order of endpoints to avoid all agents connecting to the
-	// same etcd endpoint and to work around etcd client library failover
-	// bugs. (https://github.com/etcd-io/etcd/pull/9860)
-	if e.config.Endpoints != nil {
-		shuffleEndpoints(e.config.Endpoints)
-	}
-
 	for {
 		// connectEtcdClient will close errChan when the connection attempt has
 		// been successful
@@ -639,6 +632,13 @@ func connectEtcdClient(ctx context.Context, config *client.Config, cfgPath strin
 		}
 		cfg.DialOptions = append(cfg.DialOptions, config.DialOptions...)
 		config = cfg
+	}
+
+	// Shuffle the order of endpoints to avoid all agents connecting to the
+	// same etcd endpoint and to work around etcd client library failover
+	// bugs. (https://github.com/etcd-io/etcd/pull/9860)
+	if config.Endpoints != nil {
+		shuffleEndpoints(config.Endpoints)
 	}
 
 	// Set DialTimeout to 0, otherwise the creation of a new client will