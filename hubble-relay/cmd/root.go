b9675bf918661aab76d156eac96ee3ae2f99a106
Robin Hahling <robin.hahling@gw-computing.net>
2020-08-27 19:17:50 +0200

    hubble-relay: support reading config from a file
    This allows mounting Hubble Relay's ConfigMap as YAML config file.
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- hubble-relay/cmd/root.go
+++ hubble-relay/cmd/root.go
@@ -18,11 +18,16 @@ import (
 	"github.com/cilium/cilium/hubble-relay/cmd/completion"
 	"github.com/cilium/cilium/hubble-relay/cmd/serve"
 	"github.com/cilium/cilium/hubble-relay/cmd/version"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 	v "github.com/cilium/cilium/pkg/version"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+// configFilePath defines where the hubble-relay config file should be found.
+const configFilePath = "/etc/hubble-relay/config.yaml"
 
 // New creates a new hubble-relay command.
 func New() *cobra.Command {
@@ -33,7 +38,11 @@ func New() *cobra.Command {
 		SilenceUsage: true,
 		Version:      v.GetCiliumVersion().Version,
 	}
+	logger := logging.DefaultLogger.WithField(logfields.LogSubsys, "hubble-relay")
 	vp := newViper()
+	if err := vp.ReadInConfig(); err != nil {
+		logger.WithError(err).Warnf("Failed to read config from file '%s'", configFilePath)
+	}
 	rootCmd.AddCommand(
 		completion.New(),
 		serve.New(vp),
@@ -46,6 +55,7 @@ func New() *cobra.Command {
 func newViper() *viper.Viper {
 	vp := viper.New()
 	vp.SetEnvPrefix("relay")
+	vp.SetConfigFile(configFilePath)
 	vp.AutomaticEnv()
 	return vp
 }
