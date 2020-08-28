c6fac25732745d164684a29b1b33ca2998bd0bef
Paul Chaignon <paul@cilium.io>
2020-08-28 12:28:20 +0200

    Revert "test: Quarantine flaky GuestBook test"
    This reverts commit c84d0a2ed6a1a7727cbb80efafab091a8caa159d.
    
    This test has been fixed by 240d808 ("test: Fix GuestBook test").
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- test/k8sT/Policies.go
+++ test/k8sT/Policies.go
@@ -1592,7 +1592,7 @@ var _ = Describe("K8sPolicyTest", func() {
 				ExpectWithOffset(1, err).To(BeNil(), fmt.Sprintf("Error parsing JSON response: %s", res.Stdout()))
 			}
 		}
-		SkipItIf(helpers.SkipQuarantined, "checks policy example", func() {
+		It("checks policy example", func() {
 
 			waitforPods()
 
