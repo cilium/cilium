dc091ce5f78b206251f6e60491a943c6f9926018
Daniel Borkmann <daniel@iogearbox.net>
2020-09-01 12:07:57 +0200

    bpf: add helper to retrieve hash from ct tuple
    Needed in Maglev backend selection based on pkt hash.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

---
+++ bpf/lib/hash.h
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2020 Authors of Cilium */
+
+#ifndef __HASH_H_
+#define __HASH_H_
+
+#include "common.h"
+#include "jhash.h"
+
+/* The daddr is explicitly excluded from the hash here in order to allow for
+ * backend selection to choose the same backend even on different service VIPs.
+ */
+static __always_inline __u32 hash_from_tuple_v4(const struct ipv4_ct_tuple *tuple)
+{
+	return jhash_3words(tuple->saddr,
+			    ((__u32)tuple->dport << 16) | tuple->sport,
+			    tuple->nexthdr, HASH_INIT4_SEED);
+}
+
+static __always_inline __u32 hash_from_tuple_v6(const struct ipv6_ct_tuple *tuple)
+{
+	__u32 a, b, c;
+
+	a = tuple->saddr.p1;
+	b = tuple->saddr.p2;
+	c = tuple->saddr.p3;
+	__jhash_mix(a, b, c);
+	a += tuple->saddr.p4;
+	b += ((__u32)tuple->dport << 16) | tuple->sport;
+	c += tuple->nexthdr;
+	__jhash_mix(a, b, c);
+	a += HASH_INIT6_SEED;
+	__jhash_final(a, b, c);
+	return c;
+}
+
+#endif /* __HASH_H_ */
