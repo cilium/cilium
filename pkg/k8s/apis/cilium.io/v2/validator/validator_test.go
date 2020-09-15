5aa807c34809e89fa9b40dc65004b162a7e11b6d
Chris Tarazi <chris@isovalent.com>
2020-09-15 14:05:22 -0700

    preflight: Add unknown fields check for validation
    This commit implements two additional validation checks for
    `validate-cnp` preflight command:
      1) Presence of a top-level description field.
      2) Presence of extra unknown fields.
    
    This commit relies on the fork
    github.com/christarazi/structured-merge-diff, to workaround a panic when
    converting a CCNP into an `unstructured.Unstructured` type. See
    https://github.com/kubernetes-sigs/structured-merge-diff/issues/172 for
    details.
    
    Fixes: https://github.com/cilium/cilium/issues/13155
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/k8s/apis/cilium.io/v2/validator/validator_test.go
+++ pkg/k8s/apis/cilium.io/v2/validator/validator_test.go
@@ -20,6 +20,8 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/cilium/cilium/pkg/checker"
+
 	. "gopkg.in/check.v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/yaml"
@@ -221,4 +223,186 @@ spec:
 	err = validator.ValidateCCNP(&us)
 	// Err can't be nil since validation should detect the policy is not correct.
 	c.Assert(err, Not(IsNil))
+}
+
+func (s *CNPValidationSuite) Test_UnknownFieldDetection(c *C) {
+	tests := []struct {
+		name        string
+		policy      []byte
+		clusterwide bool
+		err         error
+	}{
+		{
+			name: "neither a cnp or ccnp",
+			policy: []byte(`
+kind: ServiceAccount
+apiVersion: v1
+metadata:
+  name: app1-account
+`),
+			clusterwide: false,
+			err: ErrUnknownKind{
+				kind: "ServiceAccount",
+			},
+		},
+
+		{
+			name: "cnp top-level description exists",
+			policy: []byte(`
+apiVersion: cilium.io/v2
+kind: CiliumNetworkPolicy
+description: "default deny policy"
+metadata:
+  name: cnp-test-1
+spec:
+  endpointSelector: {}
+`),
+			clusterwide: false,
+			err:         ErrTopLevelDescriptionFound,
+		},
+		{
+			name: "cnp top-level description does not exist",
+			policy: []byte(`
+apiVersion: cilium.io/v2
+kind: CiliumNetworkPolicy
+metadata:
+  name: cnp-test-1
+spec:
+  endpointSelector: {}
+`),
+			clusterwide: false,
+			err:         nil,
+		},
+		{
+			name: "ccnp top-level description exists",
+			policy: []byte(`
+apiVersion: cilium.io/v2
+kind: CiliumClusterwideNetworkPolicy
+description: "default deny policy"
+metadata:
+  name: ccnp-test-1
+spec:
+  nodeSelector: {}
+`),
+			clusterwide: true,
+			err:         ErrTopLevelDescriptionFound,
+		},
+		{
+			name: "ccnp top-level description does not exist",
+			policy: []byte(`
+apiVersion: cilium.io/v2
+kind: CiliumClusterwideNetworkPolicy
+metadata:
+  name: ccnp-test-1
+spec:
+  nodeSelector: {}
+`),
+			clusterwide: true,
+			err:         nil,
+		},
+
+		{
+			name: "cnp extra unknown fields",
+			policy: []byte(`
+apiVersion: cilium.io/v2
+kind: CiliumNetworkPolicy
+foo: bar
+metadata:
+  name: ccnp-test-1
+spec:
+  endpointSelector: {}
+  bar: baz
+`),
+			clusterwide: false,
+			err: ErrUnknownFields{
+				extras: []string{"foo", "spec.bar"},
+			},
+		},
+		{
+			name: "ccnp extra unknown fields",
+			policy: []byte(`
+apiVersion: cilium.io/v2
+kind: CiliumClusterwideNetworkPolicy
+foo: bar
+metadata:
+  name: ccnp-test-1
+spec:
+  nodeSelector: {}
+  bar: baz
+`),
+			clusterwide: true,
+			err: ErrUnknownFields{
+				extras: []string{"foo", "spec.bar"},
+			},
+		},
+		{
+			name: "cnp specs",
+			policy: []byte(`
+apiVersion: "cilium.io/v2"
+kind: CiliumNetworkPolicy
+metadata:
+  name: "cnp-specs"
+specs:
+  - description: "Policy to test multiple rules in a single file"
+    endpointSelector:
+      matchLabels:
+        app: ratings
+        version: v1
+    ingress:
+    - fromEndpoints:
+      - matchLabels:
+          app: reviews
+          track: stable
+          version: v1
+      toPorts:
+      - ports:
+        - port: "9080"
+          protocol: TCP
+        rules:
+          http:
+          - method: "GET"
+            path: "/health"
+  - endpointSelector:
+      matchLabels:
+        app: details
+        track: stable
+        version: v1
+    ingress:
+    - fromEndpoints:
+      - matchLabels:
+          app: productpage
+          track: stable
+          version: v1
+      toPorts:
+      - ports:
+        - port: "9080"
+          protocol: TCP
+        rules:
+          http:
+          - method: "GET"
+            path: "/.*"
+`),
+			clusterwide: false,
+			err:         nil,
+		},
+	}
+	for _, tt := range tests {
+		c.Log(tt.name)
+
+		jsnByte, err := yaml.YAMLToJSON(tt.policy)
+		c.Assert(err, IsNil)
+
+		us := unstructured.Unstructured{}
+		err = json.Unmarshal(jsnByte, &us)
+		c.Assert(err, IsNil)
+
+		validator, err := NewNPValidator()
+		c.Assert(err, IsNil)
+
+		if tt.clusterwide {
+			c.Assert(validator.ValidateCCNP(&us), checker.DeepEquals, tt.err)
+		} else {
+			c.Assert(validator.ValidateCNP(&us), checker.DeepEquals, tt.err)
+		}
+	}
 }
