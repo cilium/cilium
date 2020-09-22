309aac0284f7e1b20311afaddaee2ee178b839ca
Sebastian Wicki <sebastian@isovalent.com>
2020-09-22 16:16:48 +0200

    hubble: Fix filter by reply reporting flows with unknown reply state
    [ upstream commit 511b15d0e53cbe311465c624adbf9bc3fe492592 ]
    
    This fixes a bug in the reply filter on `reply=false` would report flows
    for which we actually do not know if they were replies or not. Not all
    trace points have connection tracking state available, thus looking at
    the reply flag alone is not sufficent to tell if something a flow was a
    reply or not.
    
    Ideally, we would fix this in the parser and make the `reply` an
    optional boolean, so we can distinguish between a `false` value and an
    absent value. This however is a breaking change in the Hubble API, which
    we want to avoid. Therefore, this commit modifies the reply filter to
    only report flows here for which we know that the reply field is
    reliable.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- pkg/hubble/filters/reply.go
+++ pkg/hubble/filters/reply.go
@@ -16,6 +16,7 @@ package filters
 
 import (
 	"context"
+	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 
 	flowpb "github.com/cilium/cilium/api/v1/flow"
 	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
@@ -26,9 +27,32 @@ func filterByReplyField(replyParams []bool) FilterFunc {
 		if len(replyParams) == 0 {
 			return true
 		}
-		switch ev.Event.(type) {
+		switch f := ev.Event.(type) {
 		case v1.Flow:
-			reply := ev.GetFlow().GetReply()
+			// FIMXE: In events originating from TraceNotify, the `reply`
+			// field is determined from the connection tracking state in
+			// the datapath. Unfortunately, not all trace points have the
+			// connection tracking state available. For certain trace point
+			// events, we do not know if it actually was a reply or not.
+			//
+			// Ideally, we would fix this in the parser and make the `reply`
+			// an optional boolean, so we can distinguish between a `false`
+			// value and an absent value. This however is a breaking change
+			// in the API. Therefore, we only report flows here for which we
+			// know that the reply field is reliable:
+			if f.GetEventType().GetType() == monitorAPI.MessageTypeTrace {
+				obsPoint := uint8(f.GetEventType().GetSubType())
+				if !monitorAPI.TraceObservationPointHasConnState(obsPoint) {
+					return false
+				}
+			}
+			// For PolicyVerdict and Drop events, we statically assume they
+			// always have `reply=false`, as the flow is not yet established
+			// when the policy verdict is taken.
+			// For all other events (including Accesslog/L7), we assume the
+			// parser populates the Reply field reliably.
+
+			reply := f.GetReply()
 			for _, replyParam := range replyParams {
 				if reply == replyParam {
 					return true
