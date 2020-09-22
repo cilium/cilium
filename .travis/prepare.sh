4b2a4cccd6a611c687019a2f4278d06dabe5daf9
Tobias Klauser <tklauser@distanz.ch>
2020-09-22 12:22:30 +0200

    .github, .travis: bump golangci-lint to 1.31
    Make the version consistent in the GitHub action and Travis CI.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -50,4 +50,4 @@ GO111MODULE=off go get golang.org/x/tools/cmd/cover
 GO111MODULE=off go get github.com/mattn/goveralls
 
 # Install golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.31.0
