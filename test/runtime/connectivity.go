3e133c1340e4f54a4323ea935ffb031216d2a6d2
Paul Chaignon <paul@cilium.io>
2020-09-25 19:16:13 +0200

    test: Display BPF map content on fail
    [ upstream commit ec2c18a074de2446186854188c4853c5c5664ffc ]
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>
    Signed-off-by: Nate Sweet <nathanjsweet@pm.me>

--- test/runtime/connectivity.go
+++ test/runtime/connectivity.go
@@ -386,6 +386,7 @@ var runtimeConntrackTest = func(datapathMode string) func() {
 
 		AfterFailed(func() {
 			vm.ReportFailed("cilium policy get")
+			vm.ReportFailed("cilium bpf policy get --all")
 		})
 
 		It("Conntrack-related configuration options for endpoints", func() {
