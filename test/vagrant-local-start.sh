8ef6f499f9c6274bf8f319c3e6711b26a38f3b96
Jarno Rajahalme <jarno@covalent.io>
2020-10-02 19:50:34 +0300

    test: Lauch runtime VM in the same network as k8s for the same job
    Configure cilium-k8s#{$BUILD_NUMBER}-#{$JOB_NAME}-#{$K8S_VERSION}
    private network also for the runtime VM so that it has connectivity to
    k8s nodes when run at the same time. This is needed for VM support
    testing.
    
    Document prevously undocumented options for Vagrantfiles.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- test/vagrant-local-start.sh
+++ test/vagrant-local-start.sh
@@ -4,8 +4,6 @@ set -e
 
 export K8S_VERSION=${K8S_VERSION:-1.19}
 export K8S_NODES=${K8S_NODES:-2}
-export LOCAL_BOX=k8s-box
-export LOCAL_BOXFILE=./.vagrant/${LOCAL_BOX}-package.box
 
 echo "destroying vms"
 i=1
@@ -14,21 +12,15 @@ do
   (( i++ ))
 done
 
-if [[ ! -f ${LOCAL_BOXFILE} ]]; then
-  echo "Updating vm image"
-  unset SERVER_BOX
-  unset SERVER_VERSION
-  export PRELOAD_VM=true
-  vagrant up k8s1-${K8S_VERSION} --provision
-  vagrant package k8s1-${K8S_VERSION} --output ${LOCAL_BOXFILE}
-  vagrant box add --name ${LOCAL_BOX} ${LOCAL_BOXFILE} --force
-  vagrant destroy k8s1-${K8S_VERSION} --force
+if [ "$PRELOAD_VM" != "false" ]; then
+    ./vagrant-local-create-box.sh
+else
+    # Use defaults (see ../vagrant_box_defaults.rb)
+    unset SERVER_BOX
+    unset SERVER_VERSION
 fi
 
 echo "starting vms"
-export SERVER_BOX=$LOCAL_BOX
-export SERVER_VERSION=0
-unset PRELOAD_VM
 for i in $( seq 1 ${K8S_NODES} )
 do
   echo "Starting k8s${i}-${K8S_VERSION}"
