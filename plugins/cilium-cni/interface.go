cilium/cilium

commit dd99958bdd6683568b6215c22d3320f1cd6256d8

azure: support multiple pods subnets
Support pods attached to secondary interfaces and spread over several
subnets: inject explicit routes and rules to forward pods traffic to
their subnet gateway and back, set MTU, etc. the same way Cilium does
for AWS/ENI.

We mostly need to propagate subnets gw details (collecting addressPrefix
from instances manager so we can infer the subnets gateways addresses)
down to API / CRD, which can then be leveraged by the CNI plugin as we
do with ENIs.

While at it, also support k8s clusters and vpc/subnets running on
different resource groups.

Signed-off-by: Benjamin Pineau <benjamin.pineau@datadoghq.com>

plugins/cilium-cni/interface.go
@@ -26,7 +26,7 @@ import (
 	"github.com/containernetworking/cni/pkg/types/current"
 )
 
-func eniAdd(ipConfig *current.IPConfig, ipam *models.IPAMAddressResponse, conf models.DaemonConfigurationStatus) error {
+func interfaceAdd(ipConfig *current.IPConfig, ipam *models.IPAMAddressResponse, conf models.DaemonConfigurationStatus) error {
 	allCIDRs := make([]*net.IPNet, 0, len(ipam.Cidrs))
 	for _, cidrString := range ipam.Cidrs {
 		_, cidr, err := net.ParseCIDR(cidrString)

pkg/azure/api/api.go
@@ -17,6 +17,7 @@ package api
 import (
 	"context"
 	"fmt"
+	"net"
 	"strings"
 	"time"
 
@@ -147,7 +148,7 @@ func (c *Client) describeNetworkInterfaces(ctx context.Context) ([]network.Inter
 
 // parseInterfaces parses a network.Interface as returned by the Azure API
 // converts it into a types.AzureInterface
-func parseInterface(iface *network.Interface) (instanceID string, i *types.AzureInterface) {
+func parseInterface(iface *network.Interface, subnets ipamTypes.SubnetMap) (instanceID string, i *types.AzureInterface) {
 	i = &types.AzureInterface{}
 
 	if iface.VirtualMachine != nil && iface.VirtualMachine.ID != nil {
@@ -183,6 +184,11 @@ func parseInterface(iface *network.Interface) (instanceID string, i *types.Azure
 
 				if ip.Subnet != nil {
 					addr.Subnet = *ip.Subnet.ID
+					if subnet, ok := subnets[addr.Subnet]; ok {
+						if gateway := deriveGatewayIP(subnet.CIDR.IP); gateway != "" {
+							i.GatewayIP = gateway
+						}
+					}
 				}
 
 				i.Addresses = append(i.Addresses, addr)
@@ -193,9 +199,17 @@ func parseInterface(iface *network.Interface) (instanceID string, i *types.Azure
 	return
 }
 
+// deriveGatewayIP finds the default gateway for a given Azure subnet.
+// inspired by pkg/ipam/crd.go (as AWS, Azure reserves the first subnet IP for the gw).
+// Ref: https://docs.microsoft.com/en-us/azure/virtual-network/virtual-networks-faq#are-there-any-restrictions-on-using-ip-addresses-within-these-subnets
+func deriveGatewayIP(subnetIP net.IP) string {
+	addr := subnetIP.To4()
+	return net.IPv4(addr[0], addr[1], addr[2], addr[3]+1).String()
+}
+
 // GetInstances returns the list of all instances including all attached
 // interfaces as instanceMap
-func (c *Client) GetInstances(ctx context.Context) (*ipamTypes.InstanceMap, error) {
+func (c *Client) GetInstances(ctx context.Context, subnets ipamTypes.SubnetMap) (*ipamTypes.InstanceMap, error) {
 	instances := ipamTypes.NewInstanceMap()
 
 	networkInterfaces, err := c.describeNetworkInterfaces(ctx)
@@ -204,7 +218,7 @@ func (c *Client) GetInstances(ctx context.Context) (*ipamTypes.InstanceMap, erro
 	}
 
 	for _, iface := range networkInterfaces {
-		if id, azureInterface := parseInterface(&iface); id != "" {
+		if id, azureInterface := parseInterface(&iface, subnets); id != "" {
 			instances.Update(id, ipamTypes.InterfaceRevision{Resource: azureInterface})
 		}
 	}
@@ -217,8 +231,8 @@ func (c *Client) describeVpcs(ctx context.Context) ([]network.VirtualNetwork, er
 	c.limiter.Limit(ctx, "VirtualNetworks.List")
 
 	sinceStart := spanstat.Start()
-	result, err := c.virtualnetworks.ListComplete(ctx, c.resourceGroup)
-	c.metricsAPI.ObserveAPICall("Interfaces.ListAll", deriveStatus(err), sinceStart.Seconds())
+	result, err := c.virtualnetworks.ListAllComplete(ctx)
+	c.metricsAPI.ObserveAPICall("virtualnetworks.ListAll", deriveStatus(err), sinceStart.Seconds())
 	if err != nil {
 		return nil, err
 	}

pkg/azure/api/mock/mock.go
@@ -136,7 +136,7 @@ func (a *API) rateLimit() {
 	}
 }
 
-func (a *API) GetInstances(ctx context.Context) (*ipamTypes.InstanceMap, error) {
+func (a *API) GetInstances(ctx context.Context, subnets ipamTypes.SubnetMap) (*ipamTypes.InstanceMap, error) {
 	a.rateLimit()
 	a.delaySim.Delay(GetInstances)
 

pkg/azure/api/mock/mock_test.go
@@ -42,7 +42,7 @@ func (e *MockSuite) TestMock(c *check.C) {
 	api := NewAPI([]*ipamTypes.Subnet{subnet}, []*ipamTypes.VirtualNetwork{{ID: "v-1"}})
 	c.Assert(api, check.Not(check.IsNil))
 
-	instances, err := api.GetInstances(context.Background())
+	instances, err := api.GetInstances(context.Background(), ipamTypes.SubnetMap{})
 	c.Assert(err, check.IsNil)
 	c.Assert(instances.NumInstances(), check.Equals, 0)
 
@@ -59,7 +59,7 @@ func (e *MockSuite) TestMock(c *check.C) {
 		Resource: &types.AzureInterface{ID: ifaceID, Name: "eth0"},
 	})
 	api.UpdateInstances(instances)
-	instances, err = api.GetInstances(context.Background())
+	instances, err = api.GetInstances(context.Background(), ipamTypes.SubnetMap{})
 	c.Assert(err, check.IsNil)
 	c.Assert(instances.NumInstances(), check.Equals, 1)
 	instances.ForeachInterface("", func(instanceID, interfaceID string, iface ipamTypes.InterfaceRevision) error {
@@ -70,7 +70,7 @@ func (e *MockSuite) TestMock(c *check.C) {
 
 	err = api.AssignPrivateIpAddresses(context.Background(), "vm1", "vmss1", "s-1", "eth0", 2)
 	c.Assert(err, check.IsNil)
-	instances, err = api.GetInstances(context.Background())
+	instances, err = api.GetInstances(context.Background(), ipamTypes.SubnetMap{})
 	c.Assert(err, check.IsNil)
 	c.Assert(instances.NumInstances(), check.Equals, 1)
 	instances.ForeachInterface("", func(instanceID, interfaceID string, revision ipamTypes.InterfaceRevision) error {
@@ -91,7 +91,7 @@ func (e *MockSuite) TestSetMockError(c *check.C) {
 	mockError := errors.New("error")
 
 	api.SetMockError(GetInstances, mockError)
-	_, err := api.GetInstances(context.Background())
+	_, err := api.GetInstances(context.Background(), ipamTypes.SubnetMap{})
 	c.Assert(err, check.Equals, mockError)
 
 	api.SetMockError(GetVpcsAndSubnets, mockError)
@@ -109,6 +109,6 @@ func (e *MockSuite) TestSetLimiter(c *check.C) {
 	c.Assert(api, check.Not(check.IsNil))
 
 	api.SetLimiter(10.0, 2)
-	_, err := api.GetInstances(context.Background())
+	_, err := api.GetInstances(context.Background(), ipamTypes.SubnetMap{})
 	c.Assert(err, check.IsNil)
 }

pkg/azure/ipam/instances.go
@@ -28,7 +28,7 @@ import (
 
 // AzureAPI is the API surface used of the Azure API
 type AzureAPI interface {
-	GetInstances(ctx context.Context) (*ipamTypes.InstanceMap, error)
+	GetInstances(ctx context.Context, subnets ipamTypes.SubnetMap) (*ipamTypes.InstanceMap, error)
 	GetVpcsAndSubnets(ctx context.Context) (ipamTypes.VirtualNetworkMap, ipamTypes.SubnetMap, error)
 	AssignPrivateIpAddresses(ctx context.Context, instanceID, vmssName, subnetID, interfaceName string, addresses int) error
 }
@@ -88,7 +88,7 @@ func (m *InstancesManager) Resync(ctx context.Context) time.Time {
 		return time.Time{}
 	}
 
-	instances, err := m.api.GetInstances(ctx)
+	instances, err := m.api.GetInstances(ctx, subnets)
 	if err != nil {
 		log.WithError(err).Warning("Unable to synchronize Azure instances list")
 		return time.Time{}

pkg/azure/types/types.go
@@ -110,6 +110,11 @@ type AzureInterface struct {
 	// SecurityGroup is the security group associated with the interface
 	SecurityGroup string `json:"security-group,omitempty"`
 
+	// GatewayIP is the interface subnet's default route
+	//
+	// +optional
+	GatewayIP string
+
 	// vmssName is the name of the virtual machine scale set. This field is
 	// set by extractIDs()
 	vmssName string

pkg/ipam/crd.go
@@ -503,7 +503,7 @@ func (a *crdAllocator) buildAllocationResult(ip net.IP, ipInfo *ipamTypes.Alloca
 		for _, iface := range a.store.ownNode.Status.Azure.Interfaces {
 			if iface.ID == ipInfo.Resource {
 				result.Master = iface.MAC
-
+				result.GatewayIP = iface.GatewayIP
 				return
 			}
 		}

plugins/cilium-cni/cilium-cni.go
@@ -488,18 +488,12 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 		res.Routes = append(res.Routes, routes...)
 	}
 
-	switch conf.IpamMode {
-	case option.IPAMENI:
-		err = eniAdd(ipConfig, ipam.IPV4, conf)
+	if conf.IpamMode == option.IPAMENI || conf.IpamMode == option.IPAMAzure {
+		err = interfaceAdd(ipConfig, ipam.IPV4, conf)
 		if err != nil {
-			err = fmt.Errorf("unable to setup ENI datapath: %s", err)
+			err = fmt.Errorf("unable to setup interface datapath: %s", err)
 			return
 		}
-
-	case option.IPAMAzure:
-		// No specific action is required. The standard veth based
-		// approach is selected for now. The agent will set up the
-		// routes as necessary.
 	}
 
 	var macAddrStr string

commit 21b069190abd0eb42320d5c25c60394f16864149

azure: Don't fail if gateway IP is not known
The commit dd99958bdd introduced support for multiple interfaces and
added a new requirement for the gateway IP to be known. This is not
always the case. Make it optional.

This resulted in errors like these:
```
Warning  FailedCreatePodSandBox  3m46s (x264 over 8m38s)  kubelet, aks-nodepool1-10706209-vmss000001  (combined from similar events): Failed to create pod sandbox: rpc error: code = Unknown desc = failed to set up sandbox container "600afaf8013aac81bdd001e12f4ee115ef07a9c1520dc72b7c9c449a8f3f3b86" network for pod "coredns-869cb84759-jrxdd": networkPlugin cni failed to set up pod "coredns-869cb84759-jrxdd_kube-system" network: unable to setup interface datapath: unable to parse routing info: invalid ip:
```

Fixes: #13154
Fixes: dd99958bdd ("azure: support multiple pods subnets")
Signed-off-by: Thomas Graf <thomas@cilium.io>

plugins/cilium-cni/interface.go
@@ -26,6 +26,11 @@ import (
 )
 
 func interfaceAdd(ipConfig *current.IPConfig, ipam *models.IPAMAddressResponse, conf models.DaemonConfigurationStatus) error {
+	// If the gateway IP is not available, it is already set up
+	if ipam.Gateway == "" {
+		return nil
+	}
+
 	allCIDRs := make([]*net.IPNet, 0, len(ipam.Cidrs))
 	for _, cidrString := range ipam.Cidrs {
 		_, cidr, err := net.ParseCIDR(cidrString)