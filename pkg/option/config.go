cilium/cilium

commit 1cb5e185f301387a7be021a3c42e531f4754ed13

daemon: remove deprecated conntrack-garbage-collector-interval option
The option was deprecated for Cilium 1.6 in commit df8582d16e51
("datapath: Optimize connection-tracking GC interval"), remove it for
Cilium 1.8.

Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

pkg/option/config.go
@@ -120,10 +120,6 @@ const (
 	// represents the value of that option.
 	ConfigDir = "config-dir"
 
-	// ConntrackGarbageCollectorIntervalDeprecated is the deprecated option
-	// name to set the conntrack gc interval
-	ConntrackGarbageCollectorIntervalDeprecated = "conntrack-garbage-collector-interval"
-
 	// ConntrackGCInterval is the name of the ConntrackGCInterval option
 	ConntrackGCInterval = "conntrack-gc-interval"
 
@@ -2307,6 +2303,7 @@ func (c *DaemonConfig) Populate() {
 	c.ClusterMeshConfig = viper.GetString(ClusterMeshConfigName)
 	c.CNPNodeStatusGCInterval = viper.GetDuration(CNPNodeStatusGCInterval)
 	c.CNPStatusUpdateInterval = viper.GetDuration(CNPStatusUpdateInterval)
+	c.ConntrackGCInterval = viper.GetDuration(ConntrackGCInterval)
 	c.DatapathMode = viper.GetString(DatapathMode)
 	c.Debug = viper.GetBool(DebugArg)
 	c.DebugVerbose = viper.GetStringSlice(DebugVerbose)
@@ -2545,12 +2542,6 @@ func (c *DaemonConfig) Populate() {
 
 	if m := viper.GetStringMapString(LogOpt); len(m) != 0 {
 		c.LogOpt = m
-	}
-
-	if val := viper.GetInt(ConntrackGarbageCollectorIntervalDeprecated); val != 0 {
-		c.ConntrackGCInterval = time.Duration(val) * time.Second
-	} else {
-		c.ConntrackGCInterval = viper.GetDuration(ConntrackGCInterval)
 	}
 
 	if val := viper.GetInt64(ENIParallelWorkersDeprecated); val != 0 {

daemon/cmd/daemon_main.go
@@ -233,10 +233,6 @@ func init() {
 	flags.String(option.ConfigDir, "", `Configuration directory that contains a file for each option`)
 	option.BindEnv(option.ConfigDir)
 
-	flags.Uint(option.ConntrackGarbageCollectorIntervalDeprecated, 0, "Garbage collection interval for the connection tracking table (in seconds)")
-	flags.MarkDeprecated(option.ConntrackGarbageCollectorIntervalDeprecated, fmt.Sprintf("please use --%s", option.ConntrackGCInterval))
-	option.BindEnv(option.ConntrackGarbageCollectorIntervalDeprecated)
-
 	flags.Duration(option.ConntrackGCInterval, time.Duration(0), "Overwrite the connection-tracking garbage collection interval")
 	option.BindEnv(option.ConntrackGCInterval)
 

commit ddd2efebd3ed15521c0a672d4a20c959acc3c3f7

pkg/option: re-introduce conntrack-gc-interval flag
This flag was accidentally removed, re-adding it.

Fixes: 1cb5e185f301 ("daemon: remove deprecated conntrack-garbage-collector-interval option")
Signed-off-by: André Martins <andre@cilium.io>

pkg/option/config.go
@@ -2610,6 +2610,8 @@ func (c *DaemonConfig) Populate() {
 		c.FixedIdentityMapping = m
 	}
 
+	c.ConntrackGCInterval = viper.GetDuration(ConntrackGCInterval)
+
 	if m := viper.GetStringMapString(KVStoreOpt); len(m) != 0 {
 		c.KVStoreOpt = m
 	}