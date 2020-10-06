cilium/cilium

commit e5cddc76279ed90da97a21961175d79bb3362a19

cilium: add OpenOrCreateUnpinned helper for Cilium maps
Currently OpenOrCreate() always pins a new map to the BPF fs. Add an
API where this can be avoided.

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/bpf/bpf_linux.go
@@ -428,7 +428,7 @@ func objCheck(fd int, path string, mapType int, keySize, valueSize, maxEntries, 
 	return false
 }
 
-func OpenOrCreateMap(path string, mapType int, keySize, valueSize, maxEntries, flags uint32, innerID uint32) (int, bool, error) {
+func OpenOrCreateMap(path string, mapType int, keySize, valueSize, maxEntries, flags uint32, innerID uint32, pin bool) (int, bool, error) {
 	var fd int
 
 	redo := false
@@ -471,9 +471,11 @@ recreate:
 			return 0, isNewMap, err
 		}
 
-		err = ObjPin(fd, path)
-		if err != nil {
-			return 0, isNewMap, err
+		if pin {
+			err = ObjPin(fd, path)
+			if err != nil {
+				return 0, isNewMap, err
+			}
 		}
 
 		return fd, isNewMap, nil

pkg/bpf/map_linux.go
@@ -414,7 +414,7 @@ func (m *Map) OpenParallel() (bool, error) {
 		}
 	}
 
-	return m.openOrCreate()
+	return m.openOrCreate(true)
 }
 
 // OpenOrCreate attempts to open the Map, or if it does not yet exist, create
@@ -442,10 +442,19 @@ func (m *Map) OpenOrCreate() (bool, error) {
 	m.lock.Lock()
 	defer m.lock.Unlock()
 
-	return m.openOrCreate()
+	return m.openOrCreate(true)
 }
 
-func (m *Map) openOrCreate() (bool, error) {
+// OpenOrCreateUnpinned is similar to OpenOrCreate (see above) but without
+// pinning the map to the file system if it had to be created.
+func (m *Map) OpenOrCreateUnpinned() (bool, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	return m.openOrCreate(false)
+}
+
+func (m *Map) openOrCreate(pin bool) (bool, error) {
 	if m.fd != 0 {
 		return false, nil
 	}
@@ -462,7 +471,7 @@ func (m *Map) openOrCreate() (bool, error) {
 
 	mapType := GetMapType(m.MapType)
 	flags := m.Flags | GetPreAllocateMapFlags(mapType)
-	fd, isNew, err := OpenOrCreateMap(m.path, int(mapType), m.KeySize, m.ValueSize, m.MaxEntries, flags, m.InnerID)
+	fd, isNew, err := OpenOrCreateMap(m.path, int(mapType), m.KeySize, m.ValueSize, m.MaxEntries, flags, m.InnerID, pin)
 	if err != nil {
 		return false, err
 	}

pkg/maps/cidrmap/cidrmap.go
@@ -181,7 +181,7 @@ func OpenMapElems(path string, prefixlen int, prefixdyn bool, maxelem uint32) (*
 		uint32(unsafe.Sizeof(uint32(0))+uintptr(bytes)),
 		uint32(LPM_MAP_VALUE_SIZE),
 		maxelem,
-		bpf.BPF_F_NO_PREALLOC, 0,
+		bpf.BPF_F_NO_PREALLOC, 0, true,
 	)
 
 	if err != nil {
@@ -193,7 +193,7 @@ func OpenMapElems(path string, prefixlen int, prefixdyn bool, maxelem uint32) (*
 			uint32(unsafe.Sizeof(uint32(0))+uintptr(bytes)),
 			uint32(LPM_MAP_VALUE_SIZE),
 			maxelem,
-			bpf.BPF_F_NO_PREALLOC, 0,
+			bpf.BPF_F_NO_PREALLOC, 0, true,
 		)
 		if err != nil {
 			scopedLog := log.WithError(err).WithField(logfields.Path, path)

commit de403cad45699a3e77690150302404fa352631b4

bpf: Do not try to open map when creating unpinned map
Previously, the OpenOrCreateUnpinned() method tried to open a map before
creating it. Obviously, such map could not been opened as it is not
pinned to the bpf fs. Fix this by avoiding the opening.

Also, rename the method to CreateUnpinned() to reflect better the nature
of the method.

Finally, add unit tests for the method.

Fixes: e5cddc76 ("cilium: add OpenOrCreateUnpinned helper for Cilium maps")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/bpf/bpf_linux.go
@@ -464,23 +464,31 @@ func objCheck(fd int, path string, mapType MapType, keySize, valueSize, maxEntri
 
 func OpenOrCreateMap(path string, mapType MapType, keySize, valueSize, maxEntries, flags uint32, innerID uint32, pin bool) (int, bool, error) {
 	var fd int
+	var err error
 
 	redo := false
 	isNewMap := false
 
 recreate:
-	if _, err := os.Stat(path); os.IsNotExist(err) || redo {
-		mapDir := filepath.Dir(path)
-		if _, err = os.Stat(mapDir); os.IsNotExist(err) {
-			if err = os.MkdirAll(mapDir, 0755); err != nil {
-				return 0, isNewMap, &os.PathError{
-					Op:   "Unable create map base directory",
-					Path: path,
-					Err:  err,
+	create := true
+	if pin {
+		if _, err := os.Stat(path); os.IsNotExist(err) || redo {
+			mapDir := filepath.Dir(path)
+			if _, err = os.Stat(mapDir); os.IsNotExist(err) {
+				if err = os.MkdirAll(mapDir, 0755); err != nil {
+					return 0, isNewMap, &os.PathError{
+						Op:   "Unable create map base directory",
+						Path: path,
+						Err:  err,
+					}
 				}
 			}
+		} else {
+			create = false
 		}
+	}
 
+	if create {
 		fd, err = CreateMap(
 			mapType,
 			keySize,
@@ -515,7 +523,7 @@ recreate:
 		return fd, isNewMap, nil
 	}
 
-	fd, err := ObjGet(path)
+	fd, err = ObjGet(path)
 	if err == nil {
 		redo = objCheck(
 			fd,

pkg/bpf/map_linux.go
@@ -467,13 +467,13 @@ func (m *Map) OpenOrCreate() (bool, error) {
 	return m.openOrCreate(true)
 }
 
-// OpenOrCreateUnpinned is similar to OpenOrCreate (see above) but without
-// pinning the map to the file system if it had to be created.
-func (m *Map) OpenOrCreateUnpinned() (bool, error) {
+// CreateUnpinned creates the map without pinning it to the file system.
+func (m *Map) CreateUnpinned() error {
 	m.lock.Lock()
 	defer m.lock.Unlock()
 
-	return m.openOrCreate(false)
+	_, err := m.openOrCreate(false)
+	return err
 }
 
 // Create is similar to OpenOrCreate, but closes the map after creating or

pkg/maps/lbmap/maglev.go
@@ -39,7 +39,7 @@ var MaglevOuter6Map *bpf.Map
 
 func InitMaglevMaps(ipv4 bool, ipv6 bool) error {
 	dummyInnerMap := newInnerMaglevMap("cilium_lb_maglev_dummy")
-	if _, err := dummyInnerMap.OpenOrCreateUnpinned(); err != nil {
+	if err := dummyInnerMap.CreateUnpinned(); err != nil {
 		return err
 	}
 	defer dummyInnerMap.Close()
@@ -93,7 +93,7 @@ func updateMaglevTable(ipv6 bool, revNATID uint16, backendIDs []uint16) error {
 	}
 
 	innerMap := newInnerMaglevMap(innerMapName)
-	if _, err := innerMap.OpenOrCreateUnpinned(); err != nil {
+	if err := innerMap.CreateUnpinned(); err != nil {
 		return err
 	}
 	defer innerMap.Close()

pkg/probe/probe.go
@@ -72,7 +72,7 @@ func HaveFullLPM() bool {
 			&probeKey{}, int(unsafe.Sizeof(probeKey{})),
 			&probeValue{}, int(unsafe.Sizeof(probeValue{})),
 			1, bpf.BPF_F_NO_PREALLOC, 0, bpf.ConvertKeyValue).WithCache()
-		_, err := m.OpenOrCreateUnpinned()
+		err := m.CreateUnpinned()
 		defer m.Close()
 		if err != nil {
 			return

pkg/bpf/map_linux_test.go
@@ -598,3 +598,31 @@ func (s *BPFPrivilegedTestSuite) TestUnpin(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(exist, Equals, false)
 }
+
+func (s *BPFPrivilegedTestSuite) TestCreateUnpinned(c *C) {
+	m := NewMap("cilium_test_create_unpinned",
+		MapTypeHash,
+		&TestKey{},
+		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
+		int(unsafe.Sizeof(TestValue{})),
+		maxEntries,
+		BPF_F_NO_PREALLOC,
+		0,
+		ConvertKeyValue).WithCache()
+	err := m.CreateUnpinned()
+	c.Assert(err, IsNil)
+	exist, err := m.exist()
+	c.Assert(err, IsNil)
+	c.Assert(exist, Equals, false)
+
+	key1 := &TestKey{Key: 105}
+	value1 := &TestValue{Value: 205}
+	err = m.Update(key1, value1)
+	c.Assert(err, IsNil)
+
+	var value2 TestValue
+	err = LookupElement(m.fd, unsafe.Pointer(key1), unsafe.Pointer(&value2))
+	c.Assert(err, IsNil)
+	c.Assert(*value1, Equals, value2)
+}