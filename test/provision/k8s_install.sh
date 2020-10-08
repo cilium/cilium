cd8451962e7a1c7e3c978dbae01f7a5bc20a42fb
Paul Chaignon <paul@cilium.io>
2020-10-08 19:47:04 +0200

    vagrant: New helpers to get logs and watch pods
    - kslogs dumps all Cilium logs from all agents.
    - wk displays the output of 'kubectl get pods' with updates every 2s.
      Further arguments can be passed to wk, e.g., 'wk -o wide'.
    - wks does the same as wk but for the kube-system namespace.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -442,6 +442,10 @@ alias k='kubectl'
 complete -F __start_kubectl k
 alias ks='kubectl -n kube-system'
 complete -F __start_kubectl ks
+alias kslogs='kubectl -n kube-system logs -l k8s-app=cilium --tail=-1'
+alias wk='watch -n2 kubectl get pods -o wide'
+alias wks='watch -n2 kubectl -n kube-system get pods -o wide'
+alias wka='watch -n2 kubectl get all --all-namespaces -o wide'
 cilium_pod() {
     kubectl -n kube-system get pods -l k8s-app=cilium \
             -o jsonpath="{.items[?(@.spec.nodeName == \"\$1\")].metadata.name}"
