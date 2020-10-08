783a70b6546c1296fd68a5f5ae4d136f50c4c9ef
André Martins <andre@cilium.io>
2020-10-08 20:40:21 +0200

    pkg/comparator: add function to compare maps ignoring keys
    MapStringEqualsIgnoreKeys will be used to compare maps while ignoring
    certain keys.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/comparator/comparator.go
+++ pkg/comparator/comparator.go
@@ -1,4 +1,4 @@
-// Copyright 2017-2018 Authors of Cilium
+// Copyright 2017-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -75,6 +75,46 @@ func MapBoolEquals(m1, m2 map[string]bool) bool {
 		if v2, ok := m2[k1]; !ok || v2 != v1 {
 			return false
 		}
+	}
+	return true
+}
+
+// MapStringEqualsIgnoreKeys returns true if both maps have the same values for
+// the keys that are not present in the 'ignoreKeys'.
+func MapStringEqualsIgnoreKeys(m1, m2 map[string]string, ignoreKeys []string) bool {
+	switch {
+	case m1 == nil && m2 == nil:
+		return true
+	case m1 == nil && m2 != nil,
+		m1 != nil && m2 == nil:
+		return false
+	}
+	ignoredM1 := 0
+	for k1, v1 := range m1 {
+		var ignore bool
+		for _, ig := range ignoreKeys {
+			if k1 == ig {
+				ignore = true
+				break
+			}
+		}
+		if ignore {
+			ignoredM1++
+			continue
+		}
+		if v2, ok := m2[k1]; !ok || v2 != v1 {
+			return false
+		}
+	}
+
+	ignoredM2 := 0
+	for _, ig := range ignoreKeys {
+		if _, ok := m2[ig]; ok {
+			ignoredM2++
+		}
+	}
+	if len(m1)-ignoredM1 != len(m2)-ignoredM2 {
+		return false
 	}
 	return true
 }
