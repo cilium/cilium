cilium/cilium

commit ab9cf4ba42062a95bbae53bbe35eb84dac7f5427

service: Make local ID allocator more service agnostic
As it allocates IDs not only for services (but also for backends).

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/service/id_local.go
@@ -26,36 +26,44 @@ type IDAllocator struct {
 	// Protects entitiesID, entities, nextID and maxID
 	lock.RWMutex
 
-	// entitiesID is a map of all entities indexed by service ID
+	// entitiesID is a map of all entities indexed by service or backend ID
 	entitiesID map[uint32]*loadbalancer.L3n4AddrID
 
 	// entities is a map of all entities indexed by L3n4Addr.StringID()
 	entities map[string]uint32
 
-	// nextID is the next service ID to attempt to allocate
+	// nextID is the next ID to attempt to allocate
 	nextID uint32
 
-	// maxID is the maximum service ID available for allocation
+	// maxID is the maximum ID available for allocation
 	maxID uint32
+
+	// initNextID is the initial nextID
+	initNextID uint32
+
+	// initMaxID is the initial maxID
+	initMaxID uint32
 }
 
 var (
-	serviceIDAlloc = NewIDAllocator()
-	backendIDAlloc = NewIDAllocator()
+	serviceIDAlloc = NewIDAllocator(FirstFreeServiceID, MaxSetOfServiceID)
+	backendIDAlloc = NewIDAllocator(FirstFreeServiceID, MaxSetOfServiceID)
 )
 
 // NewIDAllocator creates a new ID allocator instance.
-func NewIDAllocator() *IDAllocator {
+func NewIDAllocator(nextID uint32, maxID uint32) *IDAllocator {
 	return &IDAllocator{
 		entitiesID: map[uint32]*loadbalancer.L3n4AddrID{},
 		entities:   map[string]uint32{},
-		nextID:     FirstFreeServiceID,
-		maxID:      MaxSetOfServiceID,
+		nextID:     nextID,
+		maxID:      maxID,
+		initNextID: nextID,
+		initMaxID:  maxID,
 	}
 }
 
-func (alloc *IDAllocator) addServiceID(svc loadbalancer.L3n4Addr, id uint32) *loadbalancer.L3n4AddrID {
-	svcID := newServiceID(svc, id)
+func (alloc *IDAllocator) addID(svc loadbalancer.L3n4Addr, id uint32) *loadbalancer.L3n4AddrID {
+	svcID := newID(svc, id)
 	alloc.entitiesID[id] = svcID
 	alloc.entities[svc.StringID()] = id
 
@@ -74,7 +82,7 @@ func (alloc *IDAllocator) acquireLocalID(svc loadbalancer.L3n4Addr, desiredID ui
 
 	if desiredID != 0 {
 		if _, ok := alloc.entitiesID[desiredID]; !ok {
-			return alloc.addServiceID(svc, desiredID), nil
+			return alloc.addID(svc, desiredID), nil
 		}
 	}
 
@@ -89,7 +97,7 @@ func (alloc *IDAllocator) acquireLocalID(svc loadbalancer.L3n4Addr, desiredID ui
 		}
 
 		if _, ok := alloc.entitiesID[alloc.nextID]; !ok {
-			svcID := alloc.addServiceID(svc, alloc.nextID)
+			svcID := alloc.addID(svc, alloc.nextID)
 			alloc.nextID++
 			return svcID, nil
 		}
@@ -132,7 +140,7 @@ func (alloc *IDAllocator) setLocalIDSpace(next, max uint32) error {
 	return nil
 }
 
-func (alloc *IDAllocator) getLocalMaxServiceID() (uint32, error) {
+func (alloc *IDAllocator) getLocalMaxID() (uint32, error) {
 	alloc.RLock()
 	defer alloc.RUnlock()
 	return alloc.nextID, nil
@@ -142,13 +150,12 @@ func (alloc *IDAllocator) resetLocalID() {
 	alloc.Lock()
 	alloc.entitiesID = map[uint32]*loadbalancer.L3n4AddrID{}
 	alloc.entities = map[string]uint32{}
-	// TODO(brb) s/ServiceID//
-	alloc.nextID = FirstFreeServiceID
-	alloc.maxID = MaxSetOfServiceID
+	alloc.nextID = alloc.initNextID
+	alloc.maxID = alloc.initMaxID
 	alloc.Unlock()
 }
 
-func newServiceID(svc loadbalancer.L3n4Addr, id uint32) *loadbalancer.L3n4AddrID {
+func newID(svc loadbalancer.L3n4Addr, id uint32) *loadbalancer.L3n4AddrID {
 	return &loadbalancer.L3n4AddrID{
 		L3n4Addr: svc,
 		ID:       loadbalancer.ID(id),

pkg/service/id.go
@@ -79,7 +79,7 @@ func getMaxServiceID() (uint32, error) {
 		return getGlobalMaxServiceID()
 	}
 
-	return serviceIDAlloc.getLocalMaxServiceID()
+	return serviceIDAlloc.getLocalMaxID()
 }
 
 // AcquireBackendID acquires a new local ID for the given backend.

commit 948218ee3f998618930d1d35d47663106eea8a00

service: Use initNextID in acquireLocalID()
When rolling over, it should use initNextID instead of FirstFreeServiceID,
which doesn't belong to the IDAllocator. This would create problems if
FirstFreeServiceID and FirstFreeBackendID have different values although now
they happen to be the same.

Fixes: ab9cf4ba4206 ("service: Make local ID allocator more service agnostic")
Signed-off-by: Han Zhou <hzhou8@ebay.com>

pkg/service/id_local.go
@@ -95,7 +95,7 @@ func (alloc *IDAllocator) acquireLocalID(svc loadbalancer.L3n4Addr, desiredID ui
 		if alloc.nextID == startingID && rollover {
 			break
 		} else if alloc.nextID == alloc.maxID {
-			alloc.nextID = FirstFreeServiceID
+			alloc.nextID = alloc.initNextID
 			rollover = true
 		}
 