05b5cd33b0f7298d905bdb802852f66684bc13d5
André Martins <andre@cilium.io>
2020-10-20 23:14:35 +0200

    k8s: update k8s libraries to 1.17.13
    Also update k8s test versions to 1.17.13
    
    Signed-off-by: André Martins <andre@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -246,7 +246,7 @@ case $K8S_VERSION in
         ;;
     "1.17")
         KUBERNETES_CNI_VERSION="0.8.7"
-        K8S_FULL_VERSION="1.17.12"
+        K8S_FULL_VERSION="1.17.13"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
