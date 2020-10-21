87017ae68a41bdb98e6d6d1ba35b7eb43282124f
Chris Tarazi <chris@isovalent.com>
2020-10-21 16:49:55 -0700

    endpoint: Remove interval for metadata resolver
    [ upstream commit cb9aa3e73c72f7a9c1bf65609d9db85f2c493e5f ]
    
    This controller's run interval is actually unnecessary as this
    controller is intended to only run once as long as it succeeds. This is
    evidenced by the fact that we have a `done` channel that's closed when
    the controller succeeds and a goroutine waiting `done` to be closed to
    remove the controller, thereby stopping it.
    
    In addition, in the very unlikely case that the controller is scheduled
    again to run _and_ the goroutine waiting for the `done` channel to be
    closed has not run yet, the controller could panic because it would
    close the channel twice. This commit prevents that as the interval
    defaults to 10m if not provided. If the goroutine waiting on the channel
    doesn't run within 10m, then we very likely have much worse problems at
    hand.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/endpoint/endpoint.go
+++ pkg/endpoint/endpoint.go
@@ -1556,8 +1556,7 @@ func (e *Endpoint) RunMetadataResolver(resolveMetadata MetadataResolverCB) {
 				close(done)
 				return nil
 			},
-			RunInterval: 30 * time.Second,
-			Context:     e.aliveCtx,
+			Context: e.aliveCtx,
 		},
 	)
 }
