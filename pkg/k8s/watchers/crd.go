cilium/cilium

commit 620b243e0c408923ae9fd889f5ff4590220f8c94

pkg/k8s: use slimmer CRDs and Table types
When Cilium is waiting for the CRDs to be available in the cluster,
Cilium does not need process entire CRD nor Table objects as it is only
checking for the existence of the CRD, by checking its name. Thus, we
can use a slimmer version of the CRD as well as a slimmer version of the
Table structures to avoid resource consumption during initialization.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/crd.go
@@ -16,18 +16,21 @@ package watchers
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/cilium/cilium/pkg/k8s"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/informer"
+	slim_apiextensions_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/apiextensions/v1beta1"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	k8sversion "github.com/cilium/cilium/pkg/k8s/version"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/option"
 
-	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"github.com/sirupsen/logrus"
 	apiextclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -37,7 +40,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
-func (k *K8sWatcher) crdsInit(ctx context.Context, client apiextclientset.Interface) error {
+func (k *K8sWatcher) crdsInit(ctx context.Context, watcherClient apiextclientset.Interface) error {
 	crds := newCRDState(
 		map[string]bool{
 			cnpCRD:  false,
@@ -51,7 +54,7 @@ func (k *K8sWatcher) crdsInit(ctx context.Context, client apiextclientset.Interf
 
 	var (
 		listerWatcher = newListWatchFromClient(
-			newCRDGetter(client),
+			newCRDGetter(watcherClient),
 			fields.Everything(),
 			k8sversion.Capabilities().WatchPartialObjectMetadata,
 		)
@@ -61,7 +64,7 @@ func (k *K8sWatcher) crdsInit(ctx context.Context, client apiextclientset.Interf
 	if k8sversion.Capabilities().WatchPartialObjectMetadata {
 		_, crdController = informer.NewInformer(
 			listerWatcher,
-			&metav1.PartialObjectMetadata{},
+			&slim_metav1.PartialObjectMetadata{},
 			0,
 			cache.ResourceEventHandlerFuncs{
 				AddFunc:    func(obj interface{}) { addCRD(&crds, obj) },
@@ -76,7 +79,7 @@ func (k *K8sWatcher) crdsInit(ctx context.Context, client apiextclientset.Interf
 		// assume this apiserver only has v1beta1 CRDs.
 		_, crdController = informer.NewInformer(
 			listerWatcher,
-			&v1beta1.CustomResourceDefinition{},
+			&slim_apiextensions_v1beta1.CustomResourceDefinition{},
 			0,
 			cache.ResourceEventHandlerFuncs{
 				AddFunc:    func(obj interface{}) { addCRD(&crds, obj) },
@@ -293,7 +296,7 @@ func newListWatchFromClient(
 			// that object type because it is not a list type.
 			getter = getter.SetHeader("Accept", tableHeader)
 
-			t := &metav1.Table{}
+			t := &slim_metav1.Table{}
 			if err := getter.
 				VersionedParams(&options, metav1.ParameterCodec).
 				Do(context.TODO()).
@@ -304,7 +307,7 @@ func newListWatchFromClient(
 			return tableToPomList(t), nil
 		}
 
-		crds := &v1beta1.CustomResourceDefinitionList{}
+		crds := &slim_apiextensions_v1beta1.CustomResourceDefinitionList{}
 		if err := getter.
 			VersionedParams(&options, metav1.ParameterCodec).
 			Do(context.TODO()).
@@ -345,19 +348,32 @@ func newListWatchFromClient(
 // version promotion), the code still works. Hence, we do not need different
 // versions of this function to handle the different types. See
 // https://github.com/kubernetes/kubernetes/pull/77136.
-func tableToPomList(t *metav1.Table) *metav1.PartialObjectMetadataList {
-	list := &metav1.PartialObjectMetadataList{
+func tableToPomList(t *slim_metav1.Table) *slim_metav1.PartialObjectMetadataList {
+	list := &slim_metav1.PartialObjectMetadataList{
 		TypeMeta: t.TypeMeta,
 		ListMeta: t.ListMeta,
-		Items:    make([]metav1.PartialObjectMetadata, 0, len(t.Rows)),
+		Items:    make([]slim_metav1.PartialObjectMetadata, 0, len(t.Rows)),
+	}
+
+	// find column that contains the name field
+	idx := -1
+	for i, cd := range t.ColumnDefinitions {
+		if strings.ToLower(cd.Name) == "name" {
+			idx = i
+		}
+	}
+	if idx == -1 {
+		log.WithFields(logrus.Fields{
+			"column-definitions": t.ColumnDefinitions,
+		}).Error("Unable to find column definition with 'Name' field skipping")
+		return nil
 	}
 
 	for _, row := range t.Rows {
-		var pom metav1.PartialObjectMetadata
-		if err := json.Unmarshal(row.Object.Raw, &pom); err != nil {
-			log.WithError(err).Errorf("Converting metav1.Table to metav1.PartialObjectMetadata, unexpected type %T found, skipping",
-				row.Object.Object)
-			continue
+		pom := slim_metav1.PartialObjectMetadata{
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name: fmt.Sprintf("%s", row.Cells[idx]),
+			},
 		}
 
 		list.Items = append(list.Items, pom)

pkg/k8s/client.go
@@ -26,15 +26,16 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
 	k8smetrics "github.com/cilium/cilium/pkg/k8s/metrics"
+	slim_apiextclientsetscheme "github.com/cilium/cilium/pkg/k8s/slim/k8s/apiextensions-client/clientset/versioned/scheme"
+	watcher_apiextclientset "github.com/cilium/cilium/pkg/k8s/slim/k8s/apiextensions-clientset"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	watcher_client "github.com/cilium/cilium/pkg/k8s/slim/k8s/clientset"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/version"
 
 	apiextclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
-	apiextclientsetscheme "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/scheme"
 	"k8s.io/apimachinery/pkg/api/errors"
-	tablescheme "k8s.io/apimachinery/pkg/apis/meta/internalversion"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -56,6 +57,9 @@ var (
 
 	// k8sCiliumCLI is the dedicated apiextensions client.
 	k8sAPIExtCLI = &K8sAPIExtensionsClient{}
+
+	// k8sAPIExtWatcherCLI is the client dedicated k8s structure watchers.
+	k8sAPIExtWatcherCLI = &K8sAPIExtensionsClient{}
 )
 
 // Client returns the default Kubernetes client.
@@ -73,9 +77,14 @@ func CiliumClient() *K8sCiliumClient {
 	return k8sCiliumCLI
 }
 
-// CiliumClient returns the default Cilium Kubernetes client.
+// APIExtClient returns the default API Extension client.
 func APIExtClient() *K8sAPIExtensionsClient {
 	return k8sAPIExtCLI
+}
+
+// WatcherAPIExtClient returns the client dedicated to API Extensions watchers.
+func WatcherAPIExtClient() *K8sAPIExtensionsClient {
+	return k8sAPIExtWatcherCLI
 }
 
 // CreateConfig creates a client configuration based on the configured API
@@ -177,7 +186,16 @@ func createAPIExtensionsClient() error {
 
 	k8sAPIExtCLI.Interface = c
 
-	return err
+	createK8sWatcherAPIExtCli, err := watcher_apiextclientset.NewForConfig(restConfig)
+	if err != nil {
+		return err
+	}
+
+	k8sAPIExtWatcherCLI.Interface = createK8sWatcherAPIExtCli
+
+	createK8sWatcherAPIExtCli.RESTClient()
+
+	return nil
 }
 
 // createConfig creates a rest.Config for connecting to k8s api-server.
@@ -296,5 +314,5 @@ func isConnReady(c kubernetes.Interface) error {
 func init() {
 	// Register the metav1.Table and metav1.PartialObjectMetadata for the
 	// apiextclientset.
-	utilruntime.Must(tablescheme.AddToScheme(apiextclientsetscheme.Scheme))
+	utilruntime.Must(slim_metav1.AddMetaToScheme(slim_apiextclientsetscheme.Scheme))
 }

pkg/k8s/factory_functions.go
@@ -21,13 +21,12 @@ import (
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	slim_discover_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1beta1"
 	slim_networkingv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/networking/v1"
+	slim_apiextensions_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/apiextensions/v1beta1"
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -191,8 +190,8 @@ func ObjToV1Namespace(obj interface{}) *slim_corev1.Namespace {
 	return nil
 }
 
-func ObjToV1beta1CRD(obj interface{}) *v1beta1.CustomResourceDefinition {
-	crd, ok := obj.(*v1beta1.CustomResourceDefinition)
+func ObjToV1beta1CRD(obj interface{}) *slim_apiextensions_v1beta1.CustomResourceDefinition {
+	crd, ok := obj.(*slim_apiextensions_v1beta1.CustomResourceDefinition)
 	if ok {
 		return crd
 	}
@@ -201,7 +200,7 @@ func ObjToV1beta1CRD(obj interface{}) *v1beta1.CustomResourceDefinition {
 		// Delete was not observed by the watcher but is
 		// removed from kube-apiserver. This is the last
 		// known state and the object no longer exists.
-		crd, ok := deletedObj.Obj.(*v1beta1.CustomResourceDefinition)
+		crd, ok := deletedObj.Obj.(*slim_apiextensions_v1beta1.CustomResourceDefinition)
 		if ok {
 			return crd
 		}
@@ -211,8 +210,8 @@ func ObjToV1beta1CRD(obj interface{}) *v1beta1.CustomResourceDefinition {
 	return nil
 }
 
-func ObjToV1PartialObjectMetadata(obj interface{}) *metav1.PartialObjectMetadata {
-	pom, ok := obj.(*metav1.PartialObjectMetadata)
+func ObjToV1PartialObjectMetadata(obj interface{}) *slim_metav1.PartialObjectMetadata {
+	pom, ok := obj.(*slim_metav1.PartialObjectMetadata)
 	if ok {
 		return pom
 	}
@@ -221,7 +220,7 @@ func ObjToV1PartialObjectMetadata(obj interface{}) *metav1.PartialObjectMetadata
 		// Delete was not observed by the watcher but is
 		// removed from kube-apiserver. This is the last
 		// known state and the object no longer exists.
-		pom, ok := deletedObj.Obj.(*metav1.PartialObjectMetadata)
+		pom, ok := deletedObj.Obj.(*slim_metav1.PartialObjectMetadata)
 		if ok {
 			return pom
 		}

pkg/k8s/watchers/watcher.go
@@ -374,7 +374,7 @@ func (k *K8sWatcher) WaitForCacheSync(resourceNames ...string) {
 // watcher, as those resource controllers need the resources to be registered
 // with K8s first.
 func (k *K8sWatcher) WaitForCRDsToRegister(ctx context.Context) error {
-	return k.crdsInit(ctx, k8s.APIExtClient())
+	return k.crdsInit(ctx, k8s.WatcherAPIExtClient())
 }
 
 // InitK8sSubsystem returns a channel for which it will be closed when all

commit 8c72f79f739331bd4c6b991db8260b5c916cbaf8

pkg/k8s: keep resource version when watching CRDs
In Kubernetes watchers, we need to keep the resource version of objects
so the watchers can keep track of the newest changes of a particular
object otherwise any event received from Kubernetes will be considered
"new".

Fixes: 620b243e0c40 ("pkg/k8s: use slimmer CRDs and Table types")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/crd.go
@@ -370,7 +370,8 @@ func tableToCRDList(t *slim_metav1beta1.Table) *slim_apiextensions_v1beta1.Custo
 	for _, row := range t.Rows {
 		crd := slim_apiextensions_v1beta1.CustomResourceDefinition{
 			ObjectMeta: slim_metav1.ObjectMeta{
-				Name: fmt.Sprintf("%s", row.Cells[idx]),
+				Name:            fmt.Sprintf("%s", row.Cells[idx]),
+				ResourceVersion: t.ListMeta.GetResourceVersion(),
 			},
 		}
 
@@ -414,7 +415,8 @@ func tableToPomList(t *slim_metav1.Table) *slim_metav1.PartialObjectMetadataList
 	for _, row := range t.Rows {
 		pom := slim_metav1.PartialObjectMetadata{
 			ObjectMeta: slim_metav1.ObjectMeta{
-				Name: fmt.Sprintf("%s", row.Cells[idx]),
+				Name:            fmt.Sprintf("%s", row.Cells[idx]),
+				ResourceVersion: t.ListMeta.GetResourceVersion(),
 			},
 		}
 