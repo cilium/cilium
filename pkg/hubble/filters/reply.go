0507c620b6a0fb0acc247a6276fcf4ef9c86bd3c
Sebastian Wicki <sebastian@isovalent.com>
2020-10-26 11:45:40 +0100

    hubble: Fix reply state unknown being interpreted as false
    This fixes a long-standing issue with Hubble not being able to
    distingiush between the reply field being false and the reply field
    being absent. To address the issue, the old `reply` field is replaced
    with a tri-state `is_reply` field which can be either nil, true or
    false. All uses of the old field are removed in the source tree, but the
    field itself is preserved for backwards-compatibility.
    
    This incidentally also fixes a bug where the `port-distribution` metric
    suffered from the same confusion, which caused it to report source ports
    as destination ports.
    
    Ref: #13248
    Fixes: #13744
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- pkg/hubble/filters/reply.go
+++ pkg/hubble/filters/reply.go
@@ -16,8 +16,6 @@ package filters
 
 import (
 	"context"
-	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
-
 	flowpb "github.com/cilium/cilium/api/v1/flow"
 	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
 )
@@ -29,30 +27,11 @@ func filterByReplyField(replyParams []bool) FilterFunc {
 		}
 		switch f := ev.Event.(type) {
 		case v1.Flow:
-			// FIMXE: In events originating from TraceNotify, the `reply`
-			// field is determined from the connection tracking state in
-			// the datapath. Unfortunately, not all trace points have the
-			// connection tracking state available. For certain trace point
-			// events, we do not know if it actually was a reply or not.
-			//
-			// Ideally, we would fix this in the parser and make the `reply`
-			// an optional boolean, so we can distinguish between a `false`
-			// value and an absent value. This however is a breaking change
-			// in the API. Therefore, we only report flows here for which we
-			// know that the reply field is reliable:
-			if f.GetEventType().GetType() == monitorAPI.MessageTypeTrace {
-				obsPoint := uint8(f.GetEventType().GetSubType())
-				if !monitorAPI.TraceObservationPointHasConnState(obsPoint) {
-					return false
-				}
+			if f.GetIsReply() == nil {
+				return false
 			}
-			// For PolicyVerdict and Drop events, we statically assume they
-			// always have `reply=false`, as the flow is not yet established
-			// when the policy verdict is taken.
-			// For all other events (including Accesslog/L7), we assume the
-			// parser populates the Reply field reliably.
 
-			reply := f.GetReply()
+			reply := f.GetIsReply().GetValue()
 			for _, replyParam := range replyParams {
 				if reply == replyParam {
 					return true
