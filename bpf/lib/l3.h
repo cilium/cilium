bd9b987c37f5046556cf18fcd024ce64591ed0d0
Joe Stringer <joe@cilium.io>
2020-10-26 13:01:02 -0700

    bpf: Fix --force-local-policy-eval-at-source=false
    [ upstream commit e784604d98896ed937d2552fe426c4eed2beb8d2 ]
    
    When this flag was set to false (not the default), it failed to compile
    due to the typo. Fix it.
    
    The bpf build_all target also did not pick up on this, add these options
    to the permutations for building.
    
    Fixes: d7433cff08bf ("bpf: Fix clustermesh policy with endpoint-routes")
    Fixes: #12694
    Signed-off-by: Joe Stringer <joe@cilium.io>
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- bpf/lib/l3.h
+++ bpf/lib/l3.h
@@ -86,8 +86,8 @@ static __always_inline int ipv6_local_delivery(struct __ctx_buff *ctx, int l3_of
 
 #if defined(USE_BPF_PROG_FOR_INGRESS_POLICY) && \
 	!defined(FORCE_LOCAL_POLICY_EVAL_AT_SOURCE)
-	ctk->mark |= MARK_MAGIC_IDENTITY;
-	set_identity_mark(ctx, seclabel)
+	ctx->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, seclabel);
 	return redirect_peer(ep->ifindex, 0);
 #else
 	ctx_store_meta(ctx, CB_SRC_LABEL, seclabel);
@@ -127,8 +127,8 @@ static __always_inline int ipv4_local_delivery(struct __ctx_buff *ctx, int l3_of
 
 #if defined(USE_BPF_PROG_FOR_INGRESS_POLICY) && \
 	!defined(FORCE_LOCAL_POLICY_EVAL_AT_SOURCE)
-	ctk->mark |= MARK_MAGIC_IDENTITY;
-	set_identity_mark(ctx, seclabel)
+	ctx->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, seclabel);
 	return redirect_peer(ep->ifindex, 0);
 #else
 	ctx_store_meta(ctx, CB_SRC_LABEL, seclabel);
