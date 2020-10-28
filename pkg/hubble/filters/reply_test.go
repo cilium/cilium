cilium/cilium

commit 0507c620b6a0fb0acc247a6276fcf4ef9c86bd3c

hubble: Fix reply state unknown being interpreted as false
This fixes a long-standing issue with Hubble not being able to
distingiush between the reply field being false and the reply field
being absent. To address the issue, the old `reply` field is replaced
with a tri-state `is_reply` field which can be either nil, true or
false. All uses of the old field are removed in the source tree, but the
field itself is preserved for backwards-compatibility.

This incidentally also fixes a bug where the `port-distribution` metric
suffered from the same confusion, which caused it to report source ports
as destination ports.

Ref: #13248
Fixes: #13744

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/hubble/filters/reply.go
@@ -16,8 +16,6 @@ package filters
 
 import (
 	"context"
-	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
-
 	flowpb "github.com/cilium/cilium/api/v1/flow"
 	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
 )
@@ -29,30 +27,11 @@ func filterByReplyField(replyParams []bool) FilterFunc {
 		}
 		switch f := ev.Event.(type) {
 		case v1.Flow:
-			// FIMXE: In events originating from TraceNotify, the `reply`
-			// field is determined from the connection tracking state in
-			// the datapath. Unfortunately, not all trace points have the
-			// connection tracking state available. For certain trace point
-			// events, we do not know if it actually was a reply or not.
-			//
-			// Ideally, we would fix this in the parser and make the `reply`
-			// an optional boolean, so we can distinguish between a `false`
-			// value and an absent value. This however is a breaking change
-			// in the API. Therefore, we only report flows here for which we
-			// know that the reply field is reliable:
-			if f.GetEventType().GetType() == monitorAPI.MessageTypeTrace {
-				obsPoint := uint8(f.GetEventType().GetSubType())
-				if !monitorAPI.TraceObservationPointHasConnState(obsPoint) {
-					return false
-				}
+			if f.GetIsReply() == nil {
+				return false
 			}
-			// For PolicyVerdict and Drop events, we statically assume they
-			// always have `reply=false`, as the flow is not yet established
-			// when the policy verdict is taken.
-			// For all other events (including Accesslog/L7), we assume the
-			// parser populates the Reply field reliably.
 
-			reply := f.GetReply()
+			reply := f.GetIsReply().GetValue()
 			for _, replyParam := range replyParams {
 				if reply == replyParam {
 					return true

pkg/hubble/filters/reply_test.go
@@ -23,6 +23,7 @@ import (
 	flowpb "github.com/cilium/cilium/api/v1/flow"
 	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
+	"github.com/golang/protobuf/ptypes/wrappers"
 )
 
 func Test_filterByReplyField(t *testing.T) {
@@ -48,7 +49,7 @@ func Test_filterByReplyField(t *testing.T) {
 			name: "empty-param",
 			args: args{
 				f:  []*flowpb.FlowFilter{{Reply: []bool{}}},
-				ev: &v1.Event{Event: &flowpb.Flow{Reply: true}},
+				ev: &v1.Event{Event: &flowpb.Flow{IsReply: &wrappers.BoolValue{Value: true}}},
 			},
 			want: true,
 		},
@@ -56,7 +57,7 @@ func Test_filterByReplyField(t *testing.T) {
 			name: "empty-param-2",
 			args: args{
 				f:  []*flowpb.FlowFilter{{Reply: []bool{}}},
-				ev: &v1.Event{Event: &flowpb.Flow{Reply: false}},
+				ev: &v1.Event{Event: &flowpb.Flow{IsReply: &wrappers.BoolValue{Value: false}}},
 			},
 			want: true,
 		},
@@ -64,7 +65,7 @@ func Test_filterByReplyField(t *testing.T) {
 			name: "no-reply",
 			args: args{
 				f:  []*flowpb.FlowFilter{{Reply: []bool{false}}},
-				ev: &v1.Event{Event: &flowpb.Flow{Reply: false}},
+				ev: &v1.Event{Event: &flowpb.Flow{IsReply: &wrappers.BoolValue{Value: false}}},
 			},
 			want: true,
 		},
@@ -77,7 +78,7 @@ func Test_filterByReplyField(t *testing.T) {
 						Type:    monitorAPI.MessageTypeTrace,
 						SubType: monitorAPI.TraceFromLxc,
 					},
-					Reply: false,
+					IsReply: nil,
 				}},
 			},
 			want: false,
@@ -91,7 +92,7 @@ func Test_filterByReplyField(t *testing.T) {
 						Type:    monitorAPI.MessageTypeTrace,
 						SubType: monitorAPI.TraceToLxc,
 					},
-					Reply: false,
+					IsReply: &wrappers.BoolValue{Value: false},
 				}},
 			},
 			want: true,
@@ -100,7 +101,7 @@ func Test_filterByReplyField(t *testing.T) {
 			name: "reply",
 			args: args{
 				f:  []*flowpb.FlowFilter{{Reply: []bool{true}}},
-				ev: &v1.Event{Event: &flowpb.Flow{Reply: true}},
+				ev: &v1.Event{Event: &flowpb.Flow{IsReply: &wrappers.BoolValue{Value: true}}},
 			},
 			want: true,
 		},
@@ -108,7 +109,7 @@ func Test_filterByReplyField(t *testing.T) {
 			name: "no-match",
 			args: args{
 				f:  []*flowpb.FlowFilter{{Reply: []bool{true}}},
-				ev: &v1.Event{Event: &flowpb.Flow{Reply: false}},
+				ev: &v1.Event{Event: &flowpb.Flow{IsReply: &wrappers.BoolValue{Value: false}}},
 			},
 			want: false,
 		},
@@ -116,7 +117,7 @@ func Test_filterByReplyField(t *testing.T) {
 			name: "no-match-2",
 			args: args{
 				f:  []*flowpb.FlowFilter{{Reply: []bool{false}}},
-				ev: &v1.Event{Event: &flowpb.Flow{Reply: true}},
+				ev: &v1.Event{Event: &flowpb.Flow{IsReply: &wrappers.BoolValue{Value: true}}},
 			},
 			want: false,
 		},

pkg/hubble/api/v1/interface.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/golang/protobuf/ptypes/wrappers"
 )
 
 // Flow is an interface matching pb.Flow
@@ -39,7 +40,7 @@ type Flow interface {
 	GetSourceNames() []string
 	GetDestinationNames() []string
 	GetL7() *flowpb.Layer7
-	GetReply() bool
+	GetIsReply() *wrappers.BoolValue
 	GetEventType() *flowpb.CiliumEventType
 	GetSourceService() *flowpb.Service
 	GetDestinationService() *flowpb.Service

pkg/hubble/metrics/dns/handler.go
@@ -126,7 +126,7 @@ func (d *dnsHandler) ProcessFlow(flow v1.Flow) {
 		labels[0] = "Policy denied"
 		d.responses.WithLabelValues(labels...).Inc()
 	} else {
-		if flow.GetReply() {
+		if flow.GetIsReply().GetValue() {
 			labels[0] = rcodeNames[dns.Rcode]
 			d.responses.WithLabelValues(labels...).Inc()
 

pkg/hubble/metrics/port-distribution/handler.go
@@ -54,7 +54,8 @@ func (h *portDistributionHandler) Status() string {
 }
 
 func (h *portDistributionHandler) ProcessFlow(flow v1.Flow) {
-	if flow.GetVerdict() != flowpb.Verdict_FORWARDED || flow.GetL4() == nil || flow.GetReply() {
+	skipReply := flow.GetIsReply() == nil || flow.GetIsReply().GetValue()
+	if flow.GetVerdict() != flowpb.Verdict_FORWARDED || flow.GetL4() == nil || skipReply {
 		return
 	}
 

pkg/hubble/parser/seven/parser.go
@@ -32,6 +32,7 @@ import (
 
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/google/gopacket/layers"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/sirupsen/logrus"
@@ -142,7 +143,8 @@ func (p *Parser) Decode(r *accesslog.LogRecord, decoded *pb.Flow) error {
 	decoded.DestinationNames = destinationNames
 	decoded.L7 = decodeLayer7(r)
 	decoded.L7.LatencyNs = p.computeResponseTime(r, timestamp)
-	decoded.Reply = decodeIsReply(r.Type)
+	decoded.IsReply = decodeIsReply(r.Type)
+	decoded.Reply = decoded.GetIsReply().GetValue()
 	decoded.EventType = decodeCiliumEventType(api.MessageTypeAccessLog)
 	decoded.SourceService = sourceService
 	decoded.DestinationService = destinationService
@@ -399,8 +401,10 @@ func decodeLayer7(r *accesslog.LogRecord) *pb.Layer7 {
 	}
 }
 
-func decodeIsReply(t accesslog.FlowType) bool {
-	return t == accesslog.TypeResponse
+func decodeIsReply(t accesslog.FlowType) *wrappers.BoolValue {
+	return &wrappers.BoolValue{
+		Value: t == accesslog.TypeResponse,
+	}
 }
 
 func decodeCiliumEventType(eventType uint8) *pb.CiliumEventType {

pkg/hubble/parser/threefour/parser.go
@@ -32,6 +32,7 @@ import (
 	"github.com/cilium/cilium/pkg/monitor"
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/sirupsen/logrus"
@@ -183,7 +184,8 @@ func (p *Parser) Decode(data []byte, decoded *pb.Flow) error {
 	decoded.SourceNames = p.resolveNames(dstEndpoint.ID, srcIP)
 	decoded.DestinationNames = p.resolveNames(srcEndpoint.ID, dstIP)
 	decoded.L7 = nil
-	decoded.Reply = decodeIsReply(tn)
+	decoded.IsReply = decodeIsReply(tn)
+	decoded.Reply = decoded.GetIsReply().GetValue()
 	decoded.TrafficDirection = decodeTrafficDirection(srcEndpoint.ID, dn, tn, pvn)
 	decoded.EventType = decodeCiliumEventType(eventType, eventSubType)
 	decoded.SourceService = sourceService
@@ -428,12 +430,16 @@ func decodeICMPv6(icmp *layers.ICMPv6) *pb.Layer4 {
 	}
 }
 
-func decodeIsReply(tn *monitor.TraceNotify) bool {
-	// FIXME: Ideally, this function should return a value only if
-	// monitorAPI.TraceObservationPointHasConnState(tn.ObsPoint) is true.
-	// However, pb.Flow currently cannot distinguish between an absent value
-	// and a `false` value.
-	return tn != nil && tn.Reason & ^monitor.TraceReasonEncryptMask == monitor.TraceReasonCtReply
+func decodeIsReply(tn *monitor.TraceNotify) *wrappers.BoolValue {
+	// Unfortunately, not all trace points have the connection
+	// tracking state available. For certain trace point
+	// events, we do not know if it actually was a reply or not.
+	if tn != nil && monitorAPI.TraceObservationPointHasConnState(tn.ObsPoint) {
+		return &wrappers.BoolValue{
+			Value: tn.Reason & ^monitor.TraceReasonEncryptMask == monitor.TraceReasonCtReply,
+		}
+	}
+	return nil
 }
 
 func decodeCiliumEventType(eventType, eventSubType uint8) *pb.CiliumEventType {
@@ -485,7 +491,7 @@ func decodeTrafficDirection(srcEP uint32, dn *monitor.DropNotify, tn *monitor.Tr
 			// true if the traffic source is the local endpoint, i.e. egress
 			isSourceEP := tn.Source == uint16(srcEP)
 			// true if the packet is a reply, i.e. reverse direction
-			isReply := decodeIsReply(tn)
+			isReply := decodeIsReply(tn).GetValue()
 
 			// isSourceEP != isReply ==
 			//  (isSourceEP && !isReply) || (!isSourceEP && isReply)

pkg/hubble/testutils/fake.go
@@ -31,6 +31,7 @@ import (
 	"github.com/cilium/cilium/pkg/ipcache"
 
 	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/connectivity"
 )
@@ -369,7 +370,7 @@ type FakeFlow struct {
 	SourceNames        []string
 	DestinationNames   []string
 	L7                 *flowpb.Layer7
-	Reply              bool
+	IsReply            *wrappers.BoolValue
 	EventType          *flowpb.CiliumEventType
 	SourceService      *flowpb.Service
 	DestinationService *flowpb.Service
@@ -452,9 +453,9 @@ func (f *FakeFlow) GetL7() *flowpb.Layer7 {
 	return f.L7
 }
 
-// GetReply implements v1.Flow for the FakeFlow.
-func (f *FakeFlow) GetReply() bool {
-	return f.Reply
+// GetIsReply implements v1.Flow for the FakeFlow.
+func (f *FakeFlow) GetIsReply() *wrappers.BoolValue {
+	return f.IsReply
 }
 
 // GetEventType implements v1.Flow for the FakeFlow.

commit f58317aa845591f3e156d819e38ef9d95a2c5dc9

hubble: Fix reply filter for Hubble UI
Hubble UI always sets a reply filter when fetching flows. Before the new
tri-state `is_reply` was introduced, a filter on `reply=false` would
always include drops as well. With the new `is_reply` field, we however
do not statically assume anymore that drops only happen for non-reply
packets.

Therefore, the changes introduced in 0507c620 made it such that Hubble
UI (because it sets a filter on `reply=false`) would not display dropped
flows anymore. This commit adds a work around, to treat
`is_reply=unknown` as `is_reply=false` for dropped flows.

Fixes: 0507c620b6a0 ("hubble: Fix reply state unknown being interpreted as false")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/hubble/filters/reply.go
@@ -27,7 +27,12 @@ func filterByReplyField(replyParams []bool) FilterFunc {
 		}
 		switch f := ev.Event.(type) {
 		case v1.Flow:
-			if f.GetIsReply() == nil {
+			// FIXME: For dropped flows, we handle `is_reply=unknown` as
+			// `is_reply=false`. This is for compatibility with older clients
+			// (such as Hubble UI) which assume this filter applies to the
+			// deprecated `reply` field, where dropped flows always have
+			// `reply=false`.
+			if f.GetIsReply() == nil && f.GetVerdict() != flowpb.Verdict_DROPPED {
 				return false
 			}
 

pkg/hubble/filters/reply_test.go
@@ -106,6 +106,14 @@ func Test_filterByReplyField(t *testing.T) {
 			want: true,
 		},
 		{
+			name: "drop implies reply=false",
+			args: args{
+				f:  []*flowpb.FlowFilter{{Reply: []bool{false}}},
+				ev: &v1.Event{Event: &flowpb.Flow{Verdict: flowpb.Verdict_DROPPED, IsReply: nil}},
+			},
+			want: true,
+		},
+		{
 			name: "no-match",
 			args: args{
 				f:  []*flowpb.FlowFilter{{Reply: []bool{true}}},