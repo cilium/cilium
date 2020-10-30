cilium/cilium

commit bf3a103465dc2629f3732099b8ad433b4c09e6e5

cilium: make bandwidth manager dependent on 5.1+
The writable skb->queue_mapping is only available for 5.1+ instead of
5.0+, so update the probing mechanism & doc to reflect that.

Fixes: #13109
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/bandwidth/bandwidth.go
@@ -59,9 +59,10 @@ func ProbeBandwidthManager() {
 
 	kernelGood := false
 	if h := probes.NewProbeManager().GetHelpers("sched_cls"); h != nil {
-		// We at least need 5.0 kernel for native TCP EDT integration.
-		// Below helper is available for 5.0 kernels and onwards.
-		if _, ok := h["bpf_sk_release"]; ok {
+		// We at least need 5.1 kernel for native TCP EDT integration
+		// and writable queue_mapping that we use. Below helper is
+		// available for 5.1 kernels and onwards.
+		if _, ok := h["bpf_skb_ecn_set_ce"]; ok {
 			kernelGood = true
 		}
 	}

commit a4d2ae763fc26e912f9b8631298baf0a16592ab7

bandwidth: adjust version to 5.1 in log message
The version in the log message was not adjusted when the version check
was changed from 5.0+ to 5.1+.

Fixes: bf3a103465dc ("cilium: make bandwidth manager dependent on 5.1+")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

pkg/bandwidth/bandwidth.go
@@ -67,7 +67,7 @@ func ProbeBandwidthManager() {
 		}
 	}
 	if !kernelGood {
-		log.Warn("BPF bandwidth manager needs kernel 5.0 or newer. Disabling the feature.")
+		log.Warn("BPF bandwidth manager needs kernel 5.1 or newer. Disabling the feature.")
 		option.Config.EnableBandwidthManager = false
 		return
 	}