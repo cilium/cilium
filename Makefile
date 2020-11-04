cilium/cilium

commit 3e85bd59dcc8910828e0ef3f8d825f79f2bf7ad3

build Add a debug make target
This target is unstripped, and built with
compilation optimizations and inlining disabled,
which can be used for debugging.

Signed-off-by: Aditi Ghag <aditi@cilium.io>

Makefile
@@ -4,6 +4,9 @@
 all: precheck build postcheck
 	@echo "Build finished."
 
+debug: export NOSTRIP=1
+debug: all
+
 include Makefile.defs
 
 # This is a no-op unless DOCKER_BUILDKIT is defined

Makefile.defs
@@ -145,6 +145,10 @@ endif
 GO_BUILD_FLAGS += -ldflags '$(GO_BUILD_LDFLAGS) $(EXTRA_GO_BUILD_LDFLAGS)' -tags=$(call join-with-comma,$(GO_TAGS_FLAGS)) $(EXTRA_GO_BUILD_FLAGS)
 GO_TEST_FLAGS += -tags=$(call join-with-comma,$(GO_TAGS_FLAGS))
 
+ifeq ($(NOSTRIP),1)
+	GO_BUILD_FLAGS += -gcflags="all=-N -l"
+endif
+
 GO_BUILD += $(GO_BUILD_FLAGS)
 GO_BUILD_WITH_CGO += $(GO_BUILD_FLAGS)
 

Vagrantfile
@@ -171,6 +171,8 @@ Vagrant.configure(2) do |config|
         node_ip = "#{$master_ip}"
         cm.vm.network "forwarded_port", guest: 6443, host: 7443, auto_correct: true
         cm.vm.network "forwarded_port", guest: 9081, host: 9081, auto_correct: true
+        # 2345 is the default delv server port
+        cm.vm.network "forwarded_port", guest: 2345, host: 2345, auto_correct: true
         cm.vm.network "private_network", ip: "#{$master_ip}",
             virtualbox__intnet: "cilium-test-#{$build_id}",
             :libvirt__guest_ipv6 => "yes",

commit 702b9b019bae8c2279ba7e5b09a694ae6ed269fb

build: don't disable compiler optimizations and inlining on unstripped builds
Commit 3e85bd59dcc8 ("build Add a debug make target") introduced a `make
debug` target which re-used the existing NOSTRIP make var. This lead to
compiler optimizations and inlining being disabled on unstripped builds
(i.e.  production builds but with debug symbols) which lead to
performance penalties, see e.g. #13889.

Fix this by introducing a separate make var to the debug build (i.e.
disabling compiler optimizations and inlining) and using
NOSTRIP solely to disable stripping of debug symbols.

Fixes #13893
Fixes: 3e85bd59dcc8 ("build Add a debug make target")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

Makefile
@@ -4,6 +4,7 @@
 all: precheck build postcheck
 	@echo "Build finished."
 
+debug: export NOOPT=1
 debug: export NOSTRIP=1
 debug: all
 

Makefile.defs
@@ -145,8 +145,8 @@ endif
 GO_BUILD_FLAGS += -ldflags '$(GO_BUILD_LDFLAGS) $(EXTRA_GO_BUILD_LDFLAGS)' -tags=$(call join-with-comma,$(GO_TAGS_FLAGS)) $(EXTRA_GO_BUILD_FLAGS)
 GO_TEST_FLAGS += -tags=$(call join-with-comma,$(GO_TAGS_FLAGS))
 
-ifeq ($(NOSTRIP),1)
-	GO_BUILD_FLAGS += -gcflags="all=-N -l"
+ifeq ($(NOOPT),1)
+    GO_BUILD_FLAGS += -gcflags="all=-N -l"
 endif
 
 GO_BUILD += $(GO_BUILD_FLAGS)