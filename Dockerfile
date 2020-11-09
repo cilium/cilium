cilium/cilium

commit cf199a03f5efddac9acfdc0b4e97a10b00f5548c

Update Go to 1.15.4
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

Dockerfile
@@ -23,7 +23,7 @@ LABEL cilium-sha=${CILIUM_SHA}
 # versions to be built while allowing the new versions to make changes
 # that are not backwards compatible.
 #
-FROM quay.io/cilium/cilium-builder:2020-10-20@sha256:9038f6ae28d937223d5b7a65776470607ea39dd8017a05356525455649320c5f as builder
+FROM quay.io/cilium/cilium-builder:2020-11-09@sha256:401d3bc1464c1daf54f4205aea936cb3fb90f0ae00c03e2146ebf2a9d23d5651 as builder
 ARG CILIUM_SHA=""
 LABEL cilium-sha=${CILIUM_SHA}
 LABEL maintainer="maintainer@cilium.io"
@@ -51,7 +51,7 @@ RUN make RACE=$RACE NOSTRIP=$NOSTRIP LOCKDEBUG=$LOCKDEBUG PKG_BUILD=1 V=$V LIBNE
 # built while allowing the new versions to make changes that are not
 # backwards compatible.
 #
-FROM quay.io/cilium/cilium-runtime:2020-10-20@sha256:5478b2cca987592a80a9a8834e8e91f6f8bb3f8c37aff386fbd2dd05b36c5091
+FROM quay.io/cilium/cilium-runtime:2020-11-09-v1.9@sha256:deece66b8bb3a856fcf5afd3de86834a0bc3b13ba867b76212a161ea9c11ffa4
 ARG CILIUM_SHA=""
 LABEL cilium-sha=${CILIUM_SHA}
 LABEL maintainer="maintainer@cilium.io"

test/docker-compose.yml
@@ -15,7 +15,7 @@ services:
     command: "etcd -name etcd0 -advertise-client-urls http://0.0.0.0:4002 -listen-client-urls http://0.0.0.0:4002 -initial-cluster-token etcd-cluster-1 -initial-cluster-state new"
     privileged: true
   base_image:
-    image: "quay.io/cilium/cilium-builder:2020-10-20@sha256:9038f6ae28d937223d5b7a65776470607ea39dd8017a05356525455649320c5f"
+    image: "quay.io/cilium/cilium-builder:2020-11-09@sha256:401d3bc1464c1daf54f4205aea936cb3fb90f0ae00c03e2146ebf2a9d23d5651"
     volumes:
       - "./../:/go/src/github.com/cilium/cilium/"
     privileged: true

images/builder/Dockerfile
@@ -3,7 +3,7 @@
 
 ARG COMPILERS_IMAGE=docker.io/cilium/image-compilers:57f235db9a07e81c5b60c536498ecbf2501dd267@sha256:080245ac0d7d061e05613e6bf887dc3c8bb07392cd2ce265b8a4aaaad17f2125
 ARG TESTER_IMAGE=docker.io/cilium/image-tester:70724309b859786e0a347605e407c5261f316eb0@sha256:89cc1f577d995021387871d3dbeb771b75ab4d70073d9bcbc42e532792719781
-ARG GOLANG_IMAGE=docker.io/library/golang:1.15.3@sha256:fb98de3ec034e2a9cda11ab8f13f77403aebd2c3c0b929fa5b847f0edeb6f129
+ARG GOLANG_IMAGE=docker.io/library/golang:1.15.4@sha256:ef9a25dc0bf0a4b5e28c27cf0e8f0cd2d29fb12b4cc9cc5089471328dd0d808c
 
 FROM ${GOLANG_IMAGE} as golang-dist
 

images/cilium-test/Dockerfile
@@ -3,7 +3,7 @@
 # Copyright 2020 Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
-ARG CILIUM_BUILDER_IMAGE=docker.io/cilium/cilium-builder-dev:73e2f220d7ddf84e8eda58e9908fa24098223241
+ARG CILIUM_BUILDER_IMAGE=docker.io/cilium/cilium-builder-dev:447d11af5af32337b98c5e5911dbfa42d0cddca4
 ARG UBUNTU_IMAGE=docker.io/library/ubuntu:20.04@sha256:8bce67040cd0ae39e0beb55bcb976a824d9966d2ac8d2e4bf6119b45505cee64
 
 FROM ${UBUNTU_IMAGE} as rootfs

images/cilium/Dockerfile
@@ -3,8 +3,8 @@
 # Copyright 2020 Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
-ARG CILIUM_BUILDER_IMAGE=docker.io/cilium/cilium-builder-dev:73e2f220d7ddf84e8eda58e9908fa24098223241
-ARG CILIUM_RUNTIME_IMAGE=docker.io/cilium/cilium-runtime-dev:3aab71e5992e23e4e4ec169e9851f5fd3eb33632
+ARG CILIUM_BUILDER_IMAGE=docker.io/cilium/cilium-builder-dev:447d11af5af32337b98c5e5911dbfa42d0cddca4
+ARG CILIUM_RUNTIME_IMAGE=docker.io/cilium/cilium-runtime-dev:e03c6db3406fcd48a8d0eff0672ec32bf9975e28
 
 FROM ${CILIUM_BUILDER_IMAGE} as builder
 

images/hubble-relay/Dockerfile
@@ -3,7 +3,7 @@
 # Copyright 2020 Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
-ARG CILIUM_BUILDER_IMAGE=docker.io/cilium/cilium-builder-dev:73e2f220d7ddf84e8eda58e9908fa24098223241
+ARG CILIUM_BUILDER_IMAGE=docker.io/cilium/cilium-builder-dev:447d11af5af32337b98c5e5911dbfa42d0cddca4
 ARG CA_CERTIFICATES_IMAGE=docker.io/cilium/ca-certificates:69a9f5d66ff96bf97e8b9dc107e92aa9ddbdc9a8
 
 FROM ${CILIUM_BUILDER_IMAGE} as builder

images/operator/Dockerfile
@@ -3,7 +3,7 @@
 # Copyright 2020 Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
-ARG CILIUM_BUILDER_IMAGE=docker.io/cilium/cilium-builder-dev:73e2f220d7ddf84e8eda58e9908fa24098223241
+ARG CILIUM_BUILDER_IMAGE=docker.io/cilium/cilium-builder-dev:447d11af5af32337b98c5e5911dbfa42d0cddca4
 ARG CA_CERTIFICATES_IMAGE=docker.io/cilium/ca-certificates:69a9f5d66ff96bf97e8b9dc107e92aa9ddbdc9a8
 
 FROM ${CILIUM_BUILDER_IMAGE} as builder

images/runtime/Dockerfile
@@ -5,7 +5,7 @@
 
 ARG COMPILERS_IMAGE=docker.io/cilium/image-compilers:57f235db9a07e81c5b60c536498ecbf2501dd267@sha256:080245ac0d7d061e05613e6bf887dc3c8bb07392cd2ce265b8a4aaaad17f2125
 ARG TESTER_IMAGE=docker.io/cilium/image-tester:70724309b859786e0a347605e407c5261f316eb0@sha256:89cc1f577d995021387871d3dbeb771b75ab4d70073d9bcbc42e532792719781
-ARG GOLANG_IMAGE=docker.io/library/golang:1.15.3@sha256:fb98de3ec034e2a9cda11ab8f13f77403aebd2c3c0b929fa5b847f0edeb6f129
+ARG GOLANG_IMAGE=docker.io/library/golang:1.15.4@sha256:ef9a25dc0bf0a4b5e28c27cf0e8f0cd2d29fb12b4cc9cc5089471328dd0d808c
 ARG UBUNTU_IMAGE=docker.io/library/ubuntu:20.04@sha256:8bce67040cd0ae39e0beb55bcb976a824d9966d2ac8d2e4bf6119b45505cee64
 
 ARG CILIUM_LLVM_IMAGE=docker.io/cilium/cilium-llvm:3355296c86c669ca92077e37bd5901d41c8142b9

commit 40c2f29aa1821e8bf98649d9090570443fe46027

docker: include v1.9 suffix in cilium-builder tag
Note that this does not influence which image is used as long as the SHA
is correct. This just helps humans to understand which tag is used.

Fixes: cf199a03f5ef ("Update Go to 1.15.4")
Reported-by: André Martins <andre@cilium.io>
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

Dockerfile
@@ -23,7 +23,7 @@ LABEL cilium-sha=${CILIUM_SHA}
 # versions to be built while allowing the new versions to make changes
 # that are not backwards compatible.
 #
-FROM quay.io/cilium/cilium-builder:2020-11-09@sha256:401d3bc1464c1daf54f4205aea936cb3fb90f0ae00c03e2146ebf2a9d23d5651 as builder
+FROM quay.io/cilium/cilium-builder:2020-11-09-v1.9@sha256:401d3bc1464c1daf54f4205aea936cb3fb90f0ae00c03e2146ebf2a9d23d5651 as builder
 ARG CILIUM_SHA=""
 LABEL cilium-sha=${CILIUM_SHA}
 LABEL maintainer="maintainer@cilium.io"

test/docker-compose.yml
@@ -15,7 +15,7 @@ services:
     command: "etcd -name etcd0 -advertise-client-urls http://0.0.0.0:4002 -listen-client-urls http://0.0.0.0:4002 -initial-cluster-token etcd-cluster-1 -initial-cluster-state new"
     privileged: true
   base_image:
-    image: "quay.io/cilium/cilium-builder:2020-11-09@sha256:401d3bc1464c1daf54f4205aea936cb3fb90f0ae00c03e2146ebf2a9d23d5651"
+    image: "quay.io/cilium/cilium-builder:2020-11-09-v1.9@sha256:401d3bc1464c1daf54f4205aea936cb3fb90f0ae00c03e2146ebf2a9d23d5651"
     volumes:
       - "./../:/go/src/github.com/cilium/cilium/"
     privileged: true