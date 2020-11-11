7e6d35500a4b2bbf5268097973f4ab5b9cef2f09
Kornilios Kourtis <kornilios@isovalent.com>
2020-11-11 17:55:35 +0100

    runtime: specify ICMP ids on connectivity test
    [ upstream commit 2f38bfbcc47036599046a0dd352678887f39bab8 ]
    
    The RuntimeConntrackInVethModeTest failed transiently (#12891).
    
    The test sends pings from client->server and expects them to work, and
    pings from server->client and expects them to not work (based on cilium
    policies). If the ICMP ids for these two case match, current CT code
    allows packets in both directions. This patch uses a modified ping
    utility (called xping) that allows to specify the ICMP id when executing
    a ping to produce determenistic results.
    
    Currently, we only test the case where the ids do not match. Once the CT
    code is modified to address the above issue, we can add a test where the
    ids match.
    
    Related: #12891
    
    [ backport note:
        Add timeout for the netperf command to related deployment, added in
        commit c87f2f99a0aa ("test: update k8s tests to 1.20") ]
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- test/runtime/connectivity.go
+++ test/runtime/connectivity.go
@@ -142,14 +142,16 @@ var runtimeConntrackTest = func(datapathMode string) func() {
 					assert:      BeFalse,
 				},
 				{
+					// see comment below about ICMP ids
 					from:        helpers.Client,
-					to:          helpers.Ping6(serverDockerNetworking[helpers.IPv6]),
+					to:          helpers.Ping6WithID(serverDockerNetworking[helpers.IPv6], 1111),
 					destination: helpers.Server,
 					assert:      BeTrue,
 				},
 				{
+					// see comment below about ICMP ids
 					from:        helpers.Client,
-					to:          helpers.Ping(serverDockerNetworking[helpers.IPv4]),
+					to:          helpers.PingWithID(serverDockerNetworking[helpers.IPv4], 1111),
 					destination: helpers.Server,
 					assert:      BeTrue,
 				},
@@ -201,13 +203,19 @@ var runtimeConntrackTest = func(datapathMode string) func() {
 
 			By("Testing bidirectional connectivity from client to server")
 
+			// NB: Previous versions of this test did not specify the ICMP id, which
+			// presumably caused transient errors (see #12891) when the ICMP ids for the
+			// valid direction (client->server) matched the ICMP ids for the invalid
+			// direction (server->client). We now ensure that the ICMP ids do not match.
+			// Furthermore, the original issue can be now easily reproduced by changing
+			// 2222 to 1111 below.
 			By("container %s pinging %s IPv6 (should NOT work)", helpers.Server, helpers.Client)
-			res = vm.ContainerExec(helpers.Server, helpers.Ping6(clientDockerNetworking[helpers.IPv6]))
+			res = vm.ContainerExec(helpers.Server, helpers.Ping6WithID(clientDockerNetworking[helpers.IPv6], 2222))
 			ExpectWithOffset(1, res).ShouldNot(helpers.CMDSuccess(),
 				"container %q unexpectedly was able to ping to %q IP:%q", helpers.Server, helpers.Client, clientDockerNetworking[helpers.IPv6])
 
 			By("container %s pinging %s IPv4 (should NOT work)", helpers.Server, helpers.Client)
-			res = vm.ContainerExec(helpers.Server, helpers.Ping(clientDockerNetworking[helpers.IPv4]))
+			res = vm.ContainerExec(helpers.Server, helpers.PingWithID(clientDockerNetworking[helpers.IPv4], 2222))
 			ExpectWithOffset(1, res).ShouldNot(helpers.CMDSuccess(),
 				"%q was unexpectedly able to ping to %q IP:%q", helpers.Server, helpers.Client, clientDockerNetworking[helpers.IPv4])
 
