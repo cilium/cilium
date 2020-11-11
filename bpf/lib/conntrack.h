217ebb565aafca50eed2c27fd022e154ec86e3f0
Gilberto Bertin <gilberto@isovalent.com>
2020-11-11 14:57:37 +0100

    bpf: reduce complexity of logic to handle IPv4 fragments
    Refactor the logic to handle IPv4 fragments to have only a single call
    to `ctx_load_bytes()` in order to load the l4 ports tuple.
    
    This change fixes a complexity issue in 5.4 kernels which prevents the
    BPF program from being loaded by the verifier as it would reach the
    maximum amount of instructions that can be processed.
    
    Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

--- bpf/lib/conntrack.h
+++ bpf/lib/conntrack.h
@@ -535,14 +535,14 @@ static __always_inline int ipv4_ct_extract_l4_ports(struct __ctx_buff *ctx,
 	if (!revalidate_data(ctx, &data, &data_end, &ip4))
 		return DROP_CT_INVALID_HDR;
 
-	if (unlikely(ipv4_is_fragment(ip4)))
-		return ipv4_handle_fragment(ctx, ip4, off, dir,
-					    (struct ipv4_frag_l4ports *)&tuple->dport,
-					    has_l4_header);
-#endif
+	return ipv4_handle_fragmentation(ctx, ip4, off, dir,
+				    (struct ipv4_frag_l4ports *)&tuple->dport,
+				    has_l4_header);
+#else
 	/* load sport + dport into tuple */
 	if (ctx_load_bytes(ctx, off, &tuple->dport, 4) < 0)
 		return DROP_CT_INVALID_HDR;
+#endif
 
 	return CTX_ACT_OK;
 }
