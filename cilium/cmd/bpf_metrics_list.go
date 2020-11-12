222d8a1d82ef05acc7d168a0e77f19b4bfe4238f
Gilberto Bertin <gilberto@isovalent.com>
2020-11-12 18:46:27 +0100

    cilium/cmd: improve 'bpf metrics list' JSON output
    This commit changes the JSON output for the 'bpf metrics list'
    command from the current format:
    
    ```js
    {
      "reason:0 dir:1": [
        "count:63752 bytes:20913476"
      ],
      "reason:0 dir:2": [
        "count:29697 bytes:2489416"
      ],
      "reason:132 dir:2": [
        "count:27 bytes:1930"
      ]
    }
    ```
    
    which is hard to parse even with tools such as jq to a more structured
    format:
    
    ```js
    [
      {
        "reason": 0,
        "description": "Success",
        "values": {
          "egress": {
            "packets": 29921,
            "bytes": 2508163
          },
          "ingress": {
            "packets": 64220,
            "bytes": 21064892
          }
        }
      },
      {
        "reason": 132,
        "description": "Invalid source ip",
        "values": {
          "egress": {
            "packets": 27,
            "bytes": 1930
          }
        }
      }
    ]
    ```
    
    Fixes: #13492
    
    Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

--- cilium/cmd/bpf_metrics_list.go
+++ cilium/cmd/bpf_metrics_list.go
@@ -22,6 +22,7 @@ import (
 	"strings"
 	"text/tabwriter"
 
+	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/maps/metricsmap"
@@ -37,31 +38,92 @@ const (
 	bytesTitle     = "BYTES"
 )
 
+type metricsRow struct {
+	reasonCode int
+	reasonDesc string
+	direction  string
+	packets    int
+	bytes      int
+}
+
+type jsonMetricValues struct {
+	Packets uint64 `json:"packets"`
+	Bytes   uint64 `json:"bytes"`
+}
+
+type jsonMetric struct {
+	Reason      uint64                      `json:"reason"`
+	Description string                      `json:"description"`
+	Values      map[string]jsonMetricValues `json:"values"`
+}
+
+type jsonMetrics []jsonMetric
+
 var bpfMetricsListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List BPF datapath traffic metrics",
 	Run: func(cmd *cobra.Command, args []string) {
 		common.RequireRootPrivilege("cilium bpf metrics list")
-
-		bpfMetricsList := make(map[string][]string)
-		if err := metricsmap.Metrics.Dump(bpfMetricsList); err != nil {
-			fmt.Fprintf(os.Stderr, "error dumping contents of map: %s\n", err)
-			os.Exit(1)
-		}
-
-		if command.OutputJSON() {
-			if err := command.PrintOutput(bpfMetricsList); err != nil {
-				fmt.Fprintf(os.Stderr, "error getting output of map in JSON: %s\n", err)
-				os.Exit(1)
-			}
-			return
-		}
-
-		listMetrics(bpfMetricsList)
+		listMetrics(metricsmap.Metrics)
 	},
 }
 
-func listMetrics(bpfMetricsList map[string][]string) {
+func listMetrics(m metricsmap.MetricsMap) {
+	bpfMetricsList := make(map[string]string)
+	callback := func(key bpf.MapKey, value bpf.MapValue) {
+		bpfMetricsList[key.String()] = value.String()
+	}
+	if err := m.DumpWithCallback(callback); err != nil {
+		fmt.Fprintf(os.Stderr, "error dumping contents of map: %s\n", err)
+		os.Exit(1)
+	}
+
+	if command.OutputJSON() {
+		listJSONMetrics(bpfMetricsList)
+		return
+	}
+
+	listHumanReadableMetrics(bpfMetricsList)
+}
+
+func listJSONMetrics(bpfMetricsList map[string]string) {
+	metricsByReason := map[int]jsonMetric{}
+
+	for key, value := range bpfMetricsList {
+		row, err := extractRow(key, value)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			continue
+		}
+
+		if _, ok := metricsByReason[row.reasonCode]; !ok {
+			metricsByReason[row.reasonCode] = jsonMetric{
+				Reason:      uint64(row.reasonCode),
+				Description: monitorAPI.DropReason(uint8(row.reasonCode)),
+				Values:      map[string]jsonMetricValues{},
+			}
+		}
+
+		direction := strings.ToLower(row.direction)
+
+		metricsByReason[row.reasonCode].Values[direction] = jsonMetricValues{
+			Packets: uint64(row.packets),
+			Bytes:   uint64(row.bytes),
+		}
+	}
+
+	metrics := jsonMetrics{}
+	for _, v := range metricsByReason {
+		metrics = append(metrics, v)
+	}
+
+	if err := command.PrintOutput(metrics); err != nil {
+		fmt.Fprintf(os.Stderr, "error getting output of map in JSON: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func listHumanReadableMetrics(bpfMetricsList map[string]string) {
 	if len(bpfMetricsList) == 0 {
 		fmt.Fprintf(os.Stderr, "No entries found.\n")
 		return
@@ -74,40 +136,14 @@ func listMetrics(bpfMetricsList map[string][]string) {
 	rows := [][numColumns]string{}
 
 	for key, value := range bpfMetricsList {
-		var reason, trafficDirection, packets, bytes string
-		var keyIsValid, valueIsValid bool
-		var reasonCode, trafficDirectionCode uint8
+		row, err := extractRow(key, value)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			continue
 
-		reason, trafficDirection, keyIsValid = extractTwoValues(key)
-
-		if keyIsValid {
-			v, err := strconv.Atoi(reason)
-			reasonCode = uint8(v)
-			keyIsValid = err == nil
 		}
 
-		if keyIsValid {
-			v, err := strconv.Atoi(trafficDirection)
-			trafficDirectionCode = uint8(v)
-			keyIsValid = err == nil
-		}
-
-		if keyIsValid && len(value) == 1 {
-			packets, bytes, valueIsValid = extractTwoValues(value[0])
-		}
-
-		if keyIsValid && valueIsValid {
-			rows = append(rows, [numColumns]string{monitorAPI.DropReason(reasonCode), metricsmap.MetricDirection(trafficDirectionCode), packets, bytes})
-		} else {
-			// Fall back to best effort printing.
-			for i, v := range value {
-				if i == 0 {
-					rows = append(rows, [numColumns]string{key, v, "", ""})
-				} else {
-					rows = append(rows, [numColumns]string{"", v, "", ""})
-				}
-			}
-		}
+		rows = append(rows, [numColumns]string{row.reasonDesc, row.direction, fmt.Sprintf("%d", row.packets), fmt.Sprintf("%d", row.bytes)})
 	}
 
 	sort.Slice(rows, func(i, j int) bool {
@@ -127,6 +163,43 @@ func listMetrics(bpfMetricsList map[string][]string) {
 	}
 
 	w.Flush()
+}
+
+func extractRow(key, value string) (*metricsRow, error) {
+	reasonCodeStr, directionCodeStr, ok := extractTwoValues(key)
+	if !ok {
+		return nil, fmt.Errorf("cannot extract reason and traffic direction from map's key \"%s\"", key)
+	}
+
+	reasonCode, err := strconv.Atoi(reasonCodeStr)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse reason: %s", err)
+	}
+
+	directionCode, err := strconv.Atoi(directionCodeStr)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse direction: %s", err)
+	}
+
+	packetsStr, bytesStr, ok := extractTwoValues(value)
+	if !ok {
+		return nil, fmt.Errorf("cannot extract packets and bytes counters from map's value \"%s\"", value)
+	}
+
+	packets, err := strconv.Atoi(packetsStr)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse packets counter: %s", err)
+	}
+
+	bytes, err := strconv.Atoi(bytesStr)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse bytes counter: %s", err)
+	}
+
+	reasonDesc := monitorAPI.DropReason(uint8(reasonCode))
+	direction := metricsmap.MetricDirection(uint8(directionCode))
+
+	return &metricsRow{reasonCode, reasonDesc, direction, packets, bytes}, nil
 }
 
 func extractTwoValues(str string) (string, string, bool) {
