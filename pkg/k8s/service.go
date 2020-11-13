cilium/cilium

commit 31956817bbd4e68bc2dc9dd72d949f31331eac6e

k8s: Add and parse LoadBalancerSourceRanges field
It's going to be used by the loadBalancerSourceRanges check.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/k8s/service.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	"github.com/cilium/cilium/pkg/annotation"
+	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/comparator"
 	"github.com/cilium/cilium/pkg/datapath"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/core/v1"
@@ -107,7 +108,8 @@ func ParseService(svc *slim_corev1.Service, nodeAddressing datapath.NodeAddressi
 		}
 	}
 
-	svcInfo := NewService(clusterIP, svc.Spec.ExternalIPs, loadBalancerIPs, headless,
+	svcInfo := NewService(clusterIP, svc.Spec.ExternalIPs,
+		loadBalancerIPs, svc.Spec.LoadBalancerSourceRanges, headless,
 		trafficPolicy, uint16(svc.Spec.HealthCheckNodePort), svc.Labels, svc.Spec.Selector)
 	svcInfo.IncludeExternal = getAnnotationIncludeExternal(svc)
 	svcInfo.Shared = getAnnotationShared(svc)
@@ -243,10 +245,13 @@ type Service struct {
 	// K8sExternalIPs stores mapping of the endpoint in a string format to the
 	// externalIP in net.IP format.
 	K8sExternalIPs map[string]net.IP
+
 	// LoadBalancerIPs stores LB IPs assigned to the service (string(IP) => IP).
-	LoadBalancerIPs map[string]net.IP
-	Labels          map[string]string
-	Selector        map[string]string
+	LoadBalancerIPs          map[string]net.IP
+	LoadBalancerSourceRanges []*cidr.CIDR
+
+	Labels   map[string]string
+	Selector map[string]string
 
 	// SessionAffinity denotes whether service has the clientIP session affinity
 	SessionAffinity bool
@@ -366,12 +371,21 @@ func parseIPs(externalIPs []string) map[string]net.IP {
 }
 
 // NewService returns a new Service with the Ports map initialized.
-func NewService(ip net.IP, externalIPs []string, loadBalancerIPs []string,
+func NewService(ip net.IP, externalIPs []string,
+	loadBalancerIPs []string, loadBalancerSourceRanges []string,
 	headless bool, trafficPolicy loadbalancer.SVCTrafficPolicy,
 	healthCheckNodePort uint16, labels, selector map[string]string) *Service {
 
-	var k8sExternalIPs map[string]net.IP
-	var k8sLoadBalancerIPs map[string]net.IP
+	var (
+		k8sExternalIPs     map[string]net.IP
+		k8sLoadBalancerIPs map[string]net.IP
+	)
+	loadBalancerSourceCIDRs := make([]*cidr.CIDR, 0, len(loadBalancerSourceRanges))
+
+	for _, cidrString := range loadBalancerSourceRanges {
+		cidr, _ := cidr.ParseCIDR(cidrString)
+		loadBalancerSourceCIDRs = append(loadBalancerSourceCIDRs, cidr)
+	}
 
 	if option.Config.EnableNodePort {
 		k8sExternalIPs = parseIPs(externalIPs)
@@ -385,10 +399,11 @@ func NewService(ip net.IP, externalIPs []string, loadBalancerIPs []string,
 		TrafficPolicy:       trafficPolicy,
 		HealthCheckNodePort: healthCheckNodePort,
 
-		Ports:           map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
-		NodePorts:       map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
-		K8sExternalIPs:  k8sExternalIPs,
-		LoadBalancerIPs: k8sLoadBalancerIPs,
+		Ports:                    map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
+		NodePorts:                map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
+		K8sExternalIPs:           k8sExternalIPs,
+		LoadBalancerIPs:          k8sLoadBalancerIPs,
+		LoadBalancerSourceRanges: loadBalancerSourceCIDRs,
 
 		Labels:   labels,
 		Selector: selector,

pkg/k8s/service_test.go
@@ -106,12 +106,13 @@ func (s *K8sSuite) TestParseService(c *check.C) {
 	id, svc := ParseService(k8sSvc, fakeDatapath.NewNodeAddressing())
 	c.Assert(id, checker.DeepEquals, ServiceID{Namespace: "bar", Name: "foo"})
 	c.Assert(svc, checker.DeepEquals, &Service{
-		TrafficPolicy: loadbalancer.SVCTrafficPolicyCluster,
-		FrontendIP:    net.ParseIP("127.0.0.1"),
-		Selector:      map[string]string{"foo": "bar"},
-		Labels:        map[string]string{"foo": "bar"},
-		Ports:         map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
-		NodePorts:     map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
+		TrafficPolicy:            loadbalancer.SVCTrafficPolicyCluster,
+		FrontendIP:               net.ParseIP("127.0.0.1"),
+		Selector:                 map[string]string{"foo": "bar"},
+		Labels:                   map[string]string{"foo": "bar"},
+		Ports:                    map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
+		NodePorts:                map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
+		LoadBalancerSourceRanges: []*net.IPNet{},
 	})
 
 	k8sSvc = &slim_corev1.Service{
@@ -131,11 +132,12 @@ func (s *K8sSuite) TestParseService(c *check.C) {
 	id, svc = ParseService(k8sSvc, fakeDatapath.NewNodeAddressing())
 	c.Assert(id, checker.DeepEquals, ServiceID{Namespace: "bar", Name: "foo"})
 	c.Assert(svc, checker.DeepEquals, &Service{
-		IsHeadless:    true,
-		TrafficPolicy: loadbalancer.SVCTrafficPolicyCluster,
-		Labels:        map[string]string{"foo": "bar"},
-		Ports:         map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
-		NodePorts:     map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
+		IsHeadless:               true,
+		TrafficPolicy:            loadbalancer.SVCTrafficPolicyCluster,
+		Labels:                   map[string]string{"foo": "bar"},
+		Ports:                    map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
+		NodePorts:                map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
+		LoadBalancerSourceRanges: []*net.IPNet{},
 	})
 
 	k8sSvc = &slim_corev1.Service{
@@ -156,11 +158,12 @@ func (s *K8sSuite) TestParseService(c *check.C) {
 	id, svc = ParseService(k8sSvc, fakeDatapath.NewNodeAddressing())
 	c.Assert(id, checker.DeepEquals, ServiceID{Namespace: "bar", Name: "foo"})
 	c.Assert(svc, checker.DeepEquals, &Service{
-		FrontendIP:    net.ParseIP("127.0.0.1"),
-		TrafficPolicy: loadbalancer.SVCTrafficPolicyLocal,
-		Labels:        map[string]string{"foo": "bar"},
-		Ports:         map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
-		NodePorts:     map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
+		FrontendIP:               net.ParseIP("127.0.0.1"),
+		TrafficPolicy:            loadbalancer.SVCTrafficPolicyLocal,
+		Labels:                   map[string]string{"foo": "bar"},
+		Ports:                    map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
+		NodePorts:                map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
+		LoadBalancerSourceRanges: []*net.IPNet{},
 	})
 }
 

pkg/k8s/slim/k8s/apis/core/v1/generated.proto
@@ -592,6 +592,14 @@ message ServiceSpec {
   // +optional
   optional string sessionAffinity = 7;
 
+  // If specified and supported by the platform, this will restrict traffic through the cloud-provider
+  // load-balancer will be restricted to the specified client IPs. This field will be ignored if the
+  // cloud-provider does not support the feature."
+  // More info:
+  // https://kubernetes.io/docs/tasks/access-application-cluster/configure-cloud-provider-firewall/
+  // +optional
+  repeated string loadBalancerSourceRanges = 9;
+
   // externalTrafficPolicy denotes if this Service desires to route external
   // traffic to node-local or cluster-wide endpoints. "Local" preserves the
   // client source IP and avoids a second hop for LoadBalancer and Nodeport

pkg/k8s/slim/k8s/apis/core/v1/types.go
@@ -458,6 +458,14 @@ type ServiceSpec struct {
 	// +optional
 	SessionAffinity ServiceAffinity `json:"sessionAffinity,omitempty" protobuf:"bytes,7,opt,name=sessionAffinity,casttype=ServiceAffinity"`
 
+	// If specified and supported by the platform, this will restrict traffic through the cloud-provider
+	// load-balancer will be restricted to the specified client IPs. This field will be ignored if the
+	// cloud-provider does not support the feature."
+	// More info:
+	// https://kubernetes.io/docs/tasks/access-application-cluster/configure-cloud-provider-firewall/
+	// +optional
+	LoadBalancerSourceRanges []string `json:"loadBalancerSourceRanges,omitempty" protobuf:"bytes,9,opt,name=loadBalancerSourceRanges"`
+
 	// externalTrafficPolicy denotes if this Service desires to route external
 	// traffic to node-local or cluster-wide endpoints. "Local" preserves the
 	// client source IP and avoids a second hop for LoadBalancer and Nodeport

pkg/k8s/slim/k8s/apis/core/v1/zz_generated.deepcopy.go
@@ -755,6 +755,11 @@ func (in *ServiceSpec) DeepCopyInto(out *ServiceSpec) {
 		*out = make([]string, len(*in))
 		copy(*out, *in)
 	}
+	if in.LoadBalancerSourceRanges != nil {
+		in, out := &in.LoadBalancerSourceRanges, &out.LoadBalancerSourceRanges
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	if in.SessionAffinityConfig != nil {
 		in, out := &in.SessionAffinityConfig, &out.SessionAffinityConfig
 		*out = new(SessionAffinityConfig)

pkg/k8s/slim/k8s/apis/core/v1/zz_generated.deepequal.go
@@ -929,6 +929,23 @@ func (in *ServiceSpec) DeepEqual(other *ServiceSpec) bool {
 	if in.SessionAffinity != other.SessionAffinity {
 		return false
 	}
+	if ((in.LoadBalancerSourceRanges != nil) && (other.LoadBalancerSourceRanges != nil)) || ((in.LoadBalancerSourceRanges == nil) != (other.LoadBalancerSourceRanges == nil)) {
+		in, other := &in.LoadBalancerSourceRanges, &other.LoadBalancerSourceRanges
+		if other == nil {
+			return false
+		}
+
+		if len(*in) != len(*other) {
+			return false
+		} else {
+			for i, inElement := range *in {
+				if inElement != (*other)[i] {
+					return false
+				}
+			}
+		}
+	}
+
 	if in.ExternalTrafficPolicy != other.ExternalTrafficPolicy {
 		return false
 	}

pkg/k8s/zz_generated.deepcopy.go
@@ -21,6 +21,7 @@ package k8s
 import (
 	net "net"
 
+	cidr "github.com/cilium/cilium/pkg/cidr"
 	loadbalancer "github.com/cilium/cilium/pkg/loadbalancer"
 	store "github.com/cilium/cilium/pkg/service/store"
 )
@@ -161,6 +162,16 @@ func (in *Service) DeepCopyInto(out *Service) {
 				copy(*out, *in)
 			}
 			(*out)[key] = outVal
+		}
+	}
+	if in.LoadBalancerSourceRanges != nil {
+		in, out := &in.LoadBalancerSourceRanges, &out.LoadBalancerSourceRanges
+		*out = make([]*cidr.CIDR, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = (*in).DeepCopy()
+			}
 		}
 	}
 	if in.Labels != nil {

commit b7e036eea28448bf76e752daf489ef16e57ffd14

pkg/k8s: trim spaces from loadBalancerSourceRanges
Similarly to what is being done in upstream kube-proxy [1], but
unfortunately without explaining why, loadBalancerSourceRanges might
contain spaces which prevents the CIDR from being parsed correctly.

[1] https://github.com/kubernetes/kubernetes/pull/94107

Fixes: 31956817bbd4 ("k8s: Add and parse LoadBalancerSourceRanges field")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/service.go
@@ -117,9 +117,14 @@ func ParseService(svc *slim_corev1.Service, nodeAddressing datapath.NodeAddressi
 			loadBalancerIPs = append(loadBalancerIPs, ip.IP)
 		}
 	}
+	lbSrcRanges := make([]string, 0, len(svc.Spec.LoadBalancerSourceRanges))
+	for _, cidrString := range svc.Spec.LoadBalancerSourceRanges {
+		cidrStringTrimmed := strings.TrimSpace(cidrString)
+		lbSrcRanges = append(lbSrcRanges, cidrStringTrimmed)
+	}
 
 	svcInfo := NewService(clusterIP, svc.Spec.ExternalIPs, loadBalancerIPs,
-		svc.Spec.LoadBalancerSourceRanges, headless, trafficPolicy,
+		lbSrcRanges, headless, trafficPolicy,
 		uint16(svc.Spec.HealthCheckNodePort), svc.Labels, svc.Spec.Selector,
 		svc.GetNamespace(), svcType)
 	svcInfo.IncludeExternal = getAnnotationIncludeExternal(svc)