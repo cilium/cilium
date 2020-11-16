cb9f9ebcc4525176d0af98864b95ae7c003a51ba
Tom Payne <tom@isovalent.com>
2020-11-16 15:53:08 +0100

    pkg/hubble: Add NodeNameFilter
    Signed-off-by: Tom Payne <tom@isovalent.com>

--- pkg/hubble/filters/patterns_test.go
+++ pkg/hubble/filters/patterns_test.go
@@ -97,12 +97,85 @@ func TestCompileFQDNPattern(t *testing.T) {
 			got, err := compileFQDNPattern(tt.fqdnPatterns)
 			if tt.wantErr {
 				require.Error(t, err)
-				if tt.wantErrContains != "" {
-					assert.Contains(t, err.Error(), tt.wantErrContains)
-				}
+				assert.Contains(t, err.Error(), tt.wantErrContains)
 				return
 			}
 
+			require.NoError(t, err)
+			assert.Equal(t, tt.want, got.String())
+		})
+	}
+}
+
+func TestCompileNodeNamePatterns(t *testing.T) {
+	type test struct {
+		name             string
+		nodeNamePatterns []string
+		wantErr          bool
+		wantErrContains  string
+		want             string
+	}
+
+	tests := []test{
+		{
+			name:             "all",
+			nodeNamePatterns: []string{"/"},
+			want:             `\A(?:(?:[-0-9a-z]+(?:\.[-0-9a-z]+)*)/(?:[-0-9a-z]+(?:\.[-0-9a-z]+)*))\z`,
+		},
+		{
+			name:             "node_pattern_only",
+			nodeNamePatterns: []string{"runtime1"},
+			want:             `\A(?:(?:[-0-9a-z]+(?:\.[-0-9a-z]+)*)/runtime1)\z`,
+		},
+		{
+			name:             "cluster_pattern_only",
+			nodeNamePatterns: []string{"cluster-name/"},
+			want:             `\A(?:cluster-name/(?:[-0-9a-z]+(?:\.[-0-9a-z]+)*))\z`,
+		},
+		{
+			name:             "wildcard_node_pattern",
+			nodeNamePatterns: []string{"k8s*"},
+			want:             `\A(?:(?:[-0-9a-z]+(?:\.[-0-9a-z]+)*)/k8s[-\.0-9a-z]*)\z`,
+		},
+		{
+			name:             "multiple_patterns",
+			nodeNamePatterns: []string{"runtime1", "test-cluster/k8s1"},
+			want:             `\A(?:(?:[-0-9a-z]+(?:\.[-0-9a-z]+)*)/runtime1|test-cluster/k8s1)\z`,
+		},
+		{
+			name:             "empty_pattern",
+			nodeNamePatterns: []string{""},
+			wantErr:          true,
+			wantErrContains:  "empty pattern",
+		},
+		{
+			name:             "invalid_rune_in_node_pattern",
+			nodeNamePatterns: []string{"_"},
+			wantErr:          true,
+			wantErrContains:  "invalid rune in pattern",
+		},
+		{
+			name:             "invalid_rune_in_cluster_pattern",
+			nodeNamePatterns: []string{"_/"},
+			wantErr:          true,
+			wantErrContains:  "invalid rune in pattern",
+		},
+		{
+			name:             "too_many_slashes",
+			nodeNamePatterns: []string{"default/runtime1/k8s1"},
+			wantErr:          true,
+			wantErrContains:  "too many slashes in pattern",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := compileNodeNamePattern(tt.nodeNamePatterns)
+			if tt.wantErr {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tt.wantErrContains)
+				return
+			}
 			require.NoError(t, err)
 			assert.Equal(t, tt.want, got.String())
 		})
