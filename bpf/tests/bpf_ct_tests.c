28012de659e181b9b1a25f5a9c11958f45b600e9
Kornilios Kourtis <kornilios@isovalent.com>
2020-11-23 16:34:07 +0100

    bpf: add BPF_PROG_RUN_TEST tests
    This patch introduces bpf testing facilities using BPF_PROG_RUN_TEST. We
    use the cilium/ebpf library to load and test the programs.
    
    cilium/ebpf does not seem to support  the .pinning field for maps when
    they are read from the corresponding ELF section. Not sure how easy it
    would be to add such support, but get something working without changing
    ebpf we patch the ELF file and zero the unsupported fields out.
    
    There are three tests currently. The first two are just sanity checks
    that everything works as expected. The last one prints the timeout of a
    CT entry when a SYN TCP packet is sent and a TCP RST packet is received.
    It is intended for a subsequent patch.
    
    Use go test to run the tests instead of doing so from main.  The tests
    can now be invoked by `make go_prog_test` on the bpf/ directory.
    
    ```
    $ make go_prog_test
    clang -DENABLE_IPV4 -DENABLE_IPV6 -DENABLE_HOST_REDIRECT -DENABLE_NAT46 -DENABLE_ROUTING -DENABLE_IPSEC -DPOLICY_VERDICT_NOTIFY -I/home/kkourt/go/src/github.com/cilium/cilium/bpf/include -I/home/kkourt/go/src/github.com/cilium/cilium/bpf -D__NR_CPUS__=8 -O2 -g -target bpf -std=gnu89 -nostdinc -emit-llvm -Wall -Wextra -Werror -Wshadow -Wno-address-of-packed-member -Wno-unknown-warning-option -Wno-gnu-variable-sized-type-not-at-end -Wdeclaration-after-statement -c tests/bpf_ct_tests.c -o tests/bpf_ct_tests.ll
    llc -march=bpf -mcpu=probe -mattr=dwarfris -filetype=obj -o tests/bpf_ct_tests.o tests/bpf_ct_tests.ll
    sudo go test ./tests/prog_test
    ok      github.com/cilium/cilium/bpf/tests/prog_test    0.048s
    ```
    
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ bpf/tests/bpf_ct_tests.c
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (C) 2016-2020 Authors of Cilium */
+
+#include <bpf/ctx/skb.h>
+#include <bpf/api.h>
+
+/* most values taken from node_config.h */
+#define CONNTRACK
+#define ENDPOINTS_MAP test_cilium_lxc
+#define POLICY_PROG_MAP_SIZE ENDPOINTS_MAP_SIZE
+#define METRICS_MAP test_cilium_metrics
+#define NAT46_PREFIX {.addr = {                   \
+	0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, \
+	0x0, 0x0, 0xa, 0x0, 0x0, 0x0, 0x0, 0x0    \
+} }
+
+#define ENDPOINTS_MAP_SIZE 65536
+#define IPCACHE_MAP_SIZE 512000
+#define METRICS_MAP_SIZE 65536
+#define EVENTS_MAP test_events_map
+
+#define CT_MAP_TCP6 test_cilium_ct_tcp6_65535
+#define CT_MAP_ANY6 test_cilium_ct_any6_65535
+#define CT_MAP_TCP4 test_cilium_ct_tcp4_65535
+#define CT_MAP_ANY4 test_cilium_ct_any4_65535
+#define CT_MAP_SIZE_TCP 4096
+#define CT_MAP_SIZE_ANY 4096
+#define CT_CONNECTION_LIFETIME_TCP	21600
+#define CT_CONNECTION_LIFETIME_NONTCP	60
+#define CT_SERVICE_LIFETIME_TCP		21600
+#define CT_SERVICE_LIFETIME_NONTCP	60
+#define CT_SYN_TIMEOUT			60
+#define CT_CLOSE_TIMEOUT		10
+#define CT_REPORT_INTERVAL		5
+#define CT_REPORT_FLAGS			0xff
+#define MTU 1500
+
+#define DEBUG
+
+#include <lib/dbg.h>
+#include <lib/conntrack.h>
+#include <lib/conntrack_map.h>
+
+__section("action/test-nop")
+int test_nop(struct __ctx_buff __maybe_unused *ctx)
+{
+	return CTX_ACT_OK;
+}
+
+__section("action/test-map")
+int test_map(struct __ctx_buff __maybe_unused *ctx)
+{
+	struct ipv4_ct_tuple ct_key = (struct ipv4_ct_tuple){
+		.saddr = bpf_htonl(16843009), /* 1.1.1.1 */
+		.daddr = bpf_htonl(16843010), /* 1.1.1.2 */
+		.sport = bpf_htons(1001),
+		.dport = bpf_htons(1002),
+		.nexthdr = 0,
+		.flags = 0,
+	};
+
+	struct ct_entry ct_val = (struct ct_entry){
+		.rx_packets = 1000,
+		.tx_packets = 1000,
+	};
+	struct bpf_elf_map *ctmap = &CT_MAP_TCP4;
+
+	map_update_elem(ctmap, &ct_key, &ct_val, 0);
+	return CTX_ACT_OK;
+}
+
+__section("action/test-ct4-rst1")
+int test_ct4_rst1(struct __ctx_buff *ctx)
+{
+	struct ipv4_ct_tuple tuple = {};
+	void *data, *data_end;
+	struct iphdr *ip4;
+	int l3_off = ETH_HLEN, l4_off;
+	struct ct_state ct_state = {}, ct_state_new = {};
+	__u16 proto;
+	__u32 monitor = 0;
+	int ret;
+
+	bpf_clear_meta(ctx);
+	if (!validate_ethertype(ctx, &proto)) {
+		ret = DROP_UNSUPPORTED_L2;
+		goto out;
+	}
+	if (!revalidate_data(ctx, &data, &data_end, &ip4)) {
+		ret = DROP_INVALID;
+		goto out;
+	}
+
+	tuple.nexthdr = ip4->protocol;
+	tuple.daddr = ip4->daddr;
+	tuple.saddr = ip4->saddr;
+	l4_off = l3_off + ipv4_hdrlen(ip4);
+
+	ret = ct_lookup4(get_ct_map4(&tuple), &tuple, ctx, l4_off, CT_EGRESS,
+			 &ct_state, &monitor);
+	switch (ret) {
+	case CT_NEW:
+		ct_state_new.node_port = ct_state.node_port;
+		ct_state_new.ifindex = ct_state.ifindex;
+		ret = ct_create4(get_ct_map4(&tuple), &CT_MAP_ANY4, &tuple, ctx,
+				 CT_EGRESS, &ct_state_new, 0);
+		break;
+
+	default:
+		ret = -1;
+		break;
+	}
+
+out:
+	/* mark the termination of our program so that the go program stops
+	 * blocking on the ring buffer
+	 */
+	cilium_dbg(ctx, DBG_UNSPEC, 0xe3d, 0xe3d);
+	return ret;
+}
+
+__section("action/test-ct4-rst2")
+int test_ct4_rst2(struct __ctx_buff *ctx)
+{
+	struct ipv4_ct_tuple tuple = {};
+	void *data, *data_end;
+	struct iphdr *ip4;
+	int l3_off = ETH_HLEN, l4_off;
+	struct ct_state ct_state = {};
+	__u16 proto;
+	__u32 monitor = 0;
+	int ret;
+
+	bpf_clear_meta(ctx);
+	if (!validate_ethertype(ctx, &proto)) {
+		ret = DROP_UNSUPPORTED_L2;
+		goto out;
+	}
+
+	if (!revalidate_data(ctx, &data, &data_end, &ip4)) {
+		ret = DROP_INVALID;
+		goto out;
+	}
+
+	tuple.nexthdr = ip4->protocol;
+	tuple.daddr = ip4->daddr;
+	tuple.saddr = ip4->saddr;
+	l4_off = l3_off + ipv4_hdrlen(ip4);
+
+	ret = ct_lookup4(get_ct_map4(&tuple), &tuple, ctx, l4_off, CT_INGRESS,
+			 &ct_state, &monitor);
+	cilium_dbg(ctx, DBG_UNSPEC, 1000, ret);
+out:
+	/* mark the termination of our program so that the go program stops
+	 * blocking on the ring buffer
+	 */
+	cilium_dbg(ctx, DBG_UNSPEC, 0xe3d, 0xe3d);
+	return CTX_ACT_OK;
+}
+
+BPF_LICENSE("GPL");
---
+++ bpf/tests/prog_test/prog_test.go
@@ -0,0 +1,494 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// NB: leave this out from the CI for now.
+// +build noci
+
+package bpfprogtester
+
+import (
+	"bytes"
+	"debug/elf"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"path"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/maps/ctmap"
+	"github.com/cilium/cilium/pkg/monitor"
+	"github.com/cilium/cilium/pkg/tuple"
+	"github.com/cilium/cilium/pkg/types"
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/perf"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
+)
+
+type ct4GlobalMap map[ctmap.CtKey4Global]ctmap.CtEntry
+
+func ct4TcpMapFromBPF(ebpfMap *ebpf.Map) (ct4GlobalMap, error) {
+	var key ctmap.CtKey4Global
+	var val ctmap.CtEntry
+
+	ret := make(ct4GlobalMap)
+	iter := ebpfMap.Iterate()
+	for iter.Next(&key, &val) {
+		ret[key] = val
+	}
+
+	if err := iter.Err(); err != nil {
+		return ret, fmt.Errorf("Error iterating map: %v", err)
+	}
+
+	return ret, nil
+}
+
+func mapDeleteAll(m *ebpf.Map) error {
+	for {
+		key, err := m.NextKeyBytes(nil)
+		if err != nil {
+			return err
+		}
+		if key == nil {
+			break
+		}
+
+		if err := m.Delete(key); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func testNop(spec *ebpf.Collection) error {
+	prog := spec.Programs["test_nop"]
+	if prog == nil {
+		return errors.New("did not find test_nop program")
+	}
+
+	packetIn := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{}
+	gopacket.SerializeLayers(packetIn, opts,
+		&layers.Ethernet{
+			SrcMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 10},
+			DstMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 20},
+			EthernetType: layers.EthernetTypeIPv4,
+		},
+	)
+
+	bpfRet, bufOut, err := prog.Test(packetIn.Bytes())
+	if err != nil {
+		return fmt.Errorf("test run failed: %v", err)
+	}
+
+	if bpfRet != 0 { // CT_ACT_OK
+		return errors.New("unexpected return value")
+	}
+
+	if !reflect.DeepEqual(bufOut, packetIn.Bytes()) {
+		return errors.New("unexpected data modification")
+	}
+
+	return nil
+}
+
+func testMap(spec *ebpf.Collection) error {
+	prog := spec.Programs["test_map"]
+	if prog == nil {
+		return errors.New("did not find test_map program")
+	}
+
+	bpfCtMap := spec.Maps["test_cilium_ct_tcp4_65535"]
+	if bpfCtMap == nil {
+		return errors.New("did not find test_cilium_ct_tcp4_65535 map")
+	}
+
+	mapBefore, err := ct4TcpMapFromBPF(bpfCtMap)
+	if err != nil {
+		return fmt.Errorf("ct4TcpMapFromBPF failed: %v", err)
+	}
+	if len(mapBefore) != 0 {
+		return errors.New("BPF CT map is not empty")
+	}
+
+	packetIn := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{}
+	gopacket.SerializeLayers(packetIn, opts,
+		&layers.Ethernet{
+			SrcMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 10},
+			DstMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 20},
+			EthernetType: layers.EthernetTypeIPv4,
+		},
+	)
+
+	bpfRet, bufOut, err := prog.Test(packetIn.Bytes())
+	if err != nil {
+		return fmt.Errorf("test run failed: %v", err)
+	}
+
+	if bpfRet != 0 { // CT_ACT_OK
+		return errors.New("unexpected return value")
+	}
+
+	if !reflect.DeepEqual(bufOut, packetIn.Bytes()) {
+		return errors.New("unexpected data modification")
+	}
+
+	keyExpected := ctmap.CtKey4Global{
+		TupleKey4Global: tuple.TupleKey4Global{
+			TupleKey4: tuple.TupleKey4{
+				SourceAddr: types.IPv4{1, 1, 1, 1},
+				DestAddr:   types.IPv4{1, 1, 1, 2},
+				SourcePort: byteorder.HostToNetwork(uint16(1001)).(uint16),
+				DestPort:   byteorder.HostToNetwork(uint16(1002)).(uint16),
+				NextHeader: 0,
+				Flags:      0,
+			},
+		},
+	}
+	valExecpted := ctmap.CtEntry{
+		TxPackets: 1000,
+		RxPackets: 1000,
+	}
+	mapExpected := ct4GlobalMap{
+		keyExpected: valExecpted,
+	}
+
+	mapAfter, err := ct4TcpMapFromBPF(bpfCtMap)
+	if err != nil {
+		return fmt.Errorf("ct4TcpMapFromBPF failed: %v", err)
+	}
+
+	if !reflect.DeepEqual(mapAfter, mapExpected) {
+		return errors.New("resulting BPF CT map is not equal to expected")
+	}
+
+	mapDeleteAll(bpfCtMap)
+	return nil
+}
+
+// NB: Currently, cilium/ebpf cannot deal with non-zero bpf_elf_map fields
+// after ->flags. We don't really need them for our tests, so we zero them out
+// for now. Once support is added to cilium/ebpf for these fields, we can
+// remove this.
+func patchElf(fname string, fnameOut string) error {
+	osF, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
+	defer osF.Close()
+
+	elfF, err := elf.NewFile(osF)
+	if err != nil {
+		return err
+	}
+	defer elfF.Close()
+
+	syms, err := elfF.Symbols()
+	if err != nil {
+		return err
+	}
+
+	mapSectionsOff := map[elf.SectionIndex]uint64{}
+	for secIdx, sec := range elfF.Sections {
+		if strings.HasPrefix(sec.Name, "maps") {
+			mapSectionsOff[elf.SectionIndex(secIdx)] = sec.Offset
+		}
+	}
+
+	mapSymbols := map[string]uint64{}
+	for _, sym := range syms {
+		secOff, ok := mapSectionsOff[sym.Section]
+		if !ok {
+			continue
+		}
+
+		//  This is what the current map structure looks from bpf side, so we should expect the size to be 9*4=36
+		//
+		//  struct bpf_elf_map {
+		//  	__u32 type;
+		//  	__u32 size_key;
+		//  	__u32 size_value;
+		//  	__u32 max_elem;
+		//  	__u32 flags;
+		//  	__u32 id;
+		//  	__u32 pinning;
+		//  	__u32 inner_id;
+		//  	__u32 inner_idx;
+		//  };
+		expectedMapSize := uint64(9 * 4)
+		if sym.Size != expectedMapSize {
+			log.WithFields(log.Fields{
+				"expected": expectedMapSize,
+				"actual":   sym.Size,
+			}).Fatal("invalid size")
+		}
+		mapSymbols[sym.Name] = sym.Value + secOff
+	}
+
+	outF, err := os.Create(fnameOut)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"filename": fnameOut,
+		}).Fatalf("error creating file: %v", err)
+	}
+	if err = outF.Truncate(0); err != nil {
+		log.WithFields(log.Fields{
+			"filename": fnameOut,
+		}).Fatalf("error truncating file: %v", err)
+	}
+	defer outF.Close()
+
+	if _, err = io.Copy(outF, osF); err != nil {
+		log.WithFields(log.Fields{
+			"filename": fnameOut,
+		}).Fatalf("error copying file: %v", err)
+	}
+
+	zero := [4 * 4]byte{}
+	for symName, symOff := range mapSymbols {
+		idOff := symOff + (5 * 4)
+		_, err = outF.WriteAt(zero[:], int64(idOff))
+		if err != nil {
+			log.WithFields(log.Fields{
+				"filename": fnameOut,
+			}).Fatalf("error patching file: %v", err)
+		}
+		log.WithFields(log.Fields{
+			"map": symName,
+		}).Debug("zeroed last fields")
+	}
+
+	return nil
+
+}
+
+func dumpDebugMessages(eventsReader *perf.Reader) error {
+	fmt.Printf("Log Messages:\n")
+	for {
+		record, err := eventsReader.Read()
+		if err != nil {
+			return fmt.Errorf("error reading event buffer: %v\n", err)
+		}
+
+		dm := monitor.DebugMsg{}
+		reader := bytes.NewReader(record.RawSample)
+		if err := binary.Read(reader, byteorder.Native, &dm); err != nil {
+			return fmt.Errorf("Error while parsing debug message: %v\n", err)
+		}
+
+		// special record values set from our bpf programs to mark their termination
+		if dm.SubType == monitor.DbgUnspec && dm.Arg1 == 0xe3d && dm.Arg2 == 0xe3d {
+			break
+		}
+		dm.Dump("")
+	}
+
+	return nil
+}
+
+func testCt4Rst(spec *ebpf.Collection) error {
+	eventsReader, err := perf.NewReader(spec.Maps["test_events_map"], 1024*4096)
+	if err != nil {
+		log.WithError(err).Fatal("Cannot initialise BPF perf ring buffer sockets")
+	}
+	defer func() {
+		eventsReader.Close()
+	}()
+
+	packetSyn := gopacket.NewSerializeBuffer()
+	gopacket.SerializeLayers(packetSyn, gopacket.SerializeOptions{},
+		&layers.Ethernet{
+			SrcMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 10},
+			DstMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 20},
+			EthernetType: layers.EthernetTypeIPv4,
+		},
+		&layers.IPv4{
+			Version:  4,
+			SrcIP:    net.IP{10, 3, 0, 10},
+			DstIP:    net.IP{10, 3, 0, 20},
+			Protocol: layers.IPProtocolTCP,
+			IHL:      5,
+		},
+		&layers.TCP{
+			SrcPort:    3010,
+			DstPort:    3020,
+			DataOffset: 5,
+			SYN:        true,
+		},
+		gopacket.Payload([]byte("pizza! :-)")),
+	)
+
+	packetRst := gopacket.NewSerializeBuffer()
+	gopacket.SerializeLayers(packetRst, gopacket.SerializeOptions{},
+		&layers.Ethernet{
+			DstMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 10},
+			SrcMAC:       net.HardwareAddr{1, 0, 0, 3, 0, 20},
+			EthernetType: layers.EthernetTypeIPv4,
+		},
+		&layers.IPv4{
+			Version:  4,
+			DstIP:    net.IP{10, 3, 0, 10},
+			SrcIP:    net.IP{10, 3, 0, 20},
+			Protocol: layers.IPProtocolTCP,
+			IHL:      5,
+		},
+		&layers.TCP{
+			DstPort:    3010,
+			SrcPort:    3020,
+			DataOffset: 5,
+			RST:        true,
+		},
+		gopacket.Payload([]byte("pizza! :-)")),
+	)
+
+	bpfCtMap := spec.Maps["test_cilium_ct_tcp4_65535"]
+
+	// First Packet: SYN
+	prog1 := spec.Programs["test_ct4_rst1"]
+	if prog1 == nil {
+		return errors.New("did not find test_ct4_rst1 program")
+	}
+	bpfRet, bufOut, err := prog1.Test(packetSyn.Bytes())
+	if err != nil {
+		return fmt.Errorf("test run failed: %v", err)
+	}
+	if bpfRet != 0 { // CT_ACT_OK
+		return errors.New("unexpected return value")
+	}
+	if !reflect.DeepEqual(bufOut, packetSyn.Bytes()) {
+		return errors.New("unexpected data modification")
+	}
+	if err := dumpDebugMessages(eventsReader); err != nil {
+		return fmt.Errorf("dumpDebugMessages failed: %v", err)
+	}
+	bpfCt, err := ct4TcpMapFromBPF(bpfCtMap)
+	if err != nil {
+		return fmt.Errorf("ct4TcpMapFromBPF failed: %v", err)
+	}
+	fmt.Printf("CT Entries:\n")
+	for key, val := range bpfCt {
+		fmt.Printf(" key=%s val=%s", &key, &val)
+	}
+
+	now, err := bpf.GetMtime()
+	if err != nil {
+		return errors.New("GetMtime failed")
+	}
+	// Second Packet: RST
+	prog2 := spec.Programs["test_ct4_rst2"]
+	if prog2 == nil {
+		return errors.New("did not find test_ct4_rst2 program")
+	}
+	bpfRet, bufOut, err = prog2.Test(packetRst.Bytes())
+	if err != nil {
+		return fmt.Errorf("test run failed: %v", err)
+	}
+	if err := dumpDebugMessages(eventsReader); err != nil {
+		return fmt.Errorf("dumpDebugMessages failed: %v", err)
+	}
+	bpfCt, err = ct4TcpMapFromBPF(bpfCtMap)
+	if err != nil {
+		return fmt.Errorf("ct4TcpMapFromBPF failed: %v", err)
+	}
+	fmt.Printf("CT Entries:\n")
+	for key, val := range bpfCt {
+		fmt.Printf(" key=%s val=%s", key.ToHost(), &val)
+	}
+	if bpfRet != 0 { // CT_ACT_OK
+		return errors.New("unexpected return value")
+	}
+	if !reflect.DeepEqual(bufOut, packetRst.Bytes()) {
+		return errors.New("unexpected data modification")
+	}
+
+	key := ctmap.CtKey4Global{
+		TupleKey4Global: tuple.TupleKey4Global{
+			TupleKey4: tuple.TupleKey4{
+				SourceAddr: types.IPv4{10, 3, 0, 20},
+				DestAddr:   types.IPv4{10, 3, 0, 10},
+				SourcePort: byteorder.HostToNetwork(uint16(3010)).(uint16),
+				DestPort:   byteorder.HostToNetwork(uint16(3020)).(uint16),
+				NextHeader: 6,
+				Flags:      0,
+			},
+		},
+	}
+	val := bpfCt[key]
+	expires := (val.Lifetime - uint32(now/1000000000))
+	fmt.Printf("Entry expires in %ds\n", expires)
+
+	return nil
+}
+
+// TestCt checks connection tracking
+func TestCt(t *testing.T) {
+
+	objDir := ".."
+	fname := path.Join(objDir, "bpf_ct_tests.o")
+	fnamePatched := path.Join(objDir, "bpf_ct_tests_patched.o")
+	err := patchElf(fname, fnamePatched)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	coll, err := ebpf.LoadCollection(fnamePatched)
+	if err != nil {
+		t.Fatalf("failed to load %s: %s", fnamePatched, err)
+	}
+
+	t.Run("Nop test", func(t *testing.T) {
+		err := testNop(coll)
+		if err != nil {
+			t.Fatalf("test failed: %s", err)
+		}
+	})
+
+	t.Run("Map test", func(t *testing.T) {
+		err := testMap(coll)
+		if err != nil {
+			t.Fatalf("test failed: %s", err)
+		}
+	})
+
+	t.Run("RST handling", func(t *testing.T) {
+		err := testCt4Rst(coll)
+		if err != nil {
+			t.Fatalf("test failed: %s", err)
+		}
+	})
+}
+
+func TestMain(m *testing.M) {
+	lim := unix.Rlimit{
+		Cur: unix.RLIM_INFINITY,
+		Max: unix.RLIM_INFINITY,
+	}
+	if err := unix.Setrlimit(unix.RLIMIT_MEMLOCK, &lim); err != nil {
+		log.Fatalf("setrlimit: %v", err)
+	}
+	os.Exit(m.Run())
+}
