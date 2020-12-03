cilium/cilium

commit 0ba1967d9ec007d28def3f62600e8f6a7d89cf3f

pkg/endpoint: remove unused functions
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/policy.go
@@ -858,23 +858,3 @@ func (e *Endpoint) UpdateBandwidthPolicy(annoCB AnnotationsResolverCB) {
 	}
 	<-ch
 }
-
-// GetRealizedPolicyRuleLabelsForKey returns the list of policy rule labels
-// which match a given flow key (in host byte-order). The returned
-// LabelArrayList must not be modified. This function is exported to be
-// accessed by code outside of the Cilium code base (e.g. Hubble).
-func (e *Endpoint) GetRealizedPolicyRuleLabelsForKey(key policy.Key) (
-	derivedFrom labels.LabelArrayList,
-	revision uint64,
-	ok bool,
-) {
-	e.mutex.RLock()
-	defer e.mutex.RUnlock()
-
-	entry, ok := e.realizedPolicy.PolicyMapState[key]
-	if !ok {
-		return nil, 0, false
-	}
-
-	return entry.DerivedFromRules, e.policyRevision, true
-}

pkg/endpoint/api.go
@@ -438,17 +438,6 @@ func (e *Endpoint) policyStatus() models.EndpointPolicyEnabled {
 	return policyEnabled
 }
 
-// ValidPatchTransitionState checks whether the state to which the provided
-// model specifies is one to which an Endpoint can transition as part of a
-// call to PATCH on an Endpoint.
-func ValidPatchTransitionState(state models.EndpointState) bool {
-	switch string(state) {
-	case "", StateWaitingForIdentity, StateReady:
-		return true
-	}
-	return false
-}
-
 // ProcessChangeRequest handles the update logic for performing a PATCH operation
 // on a given Endpoint. Returns the reason which will be used for informational
 // purposes should a caller choose to try to regenerate this endpoint, as well

commit c65f8ed82f44afc142726e320c47527899733df7

pkg/endpoint: Readd GetRealizedPolicyRuleLabelsForKey
This brings back the GetRealizedPolicyRuleLabelsForKey helper which is
used by external client code and was accidentally removed as unused.
This commit also extends the Hubble unit tests to invoke it, to avoid
another accidental removal.

Fixes: 0ba1967d9ec0 ("pkg/endpoint: remove unused functions")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/endpoint/policy.go
@@ -873,3 +873,24 @@ func (e *Endpoint) UpdateBandwidthPolicy(annoCB AnnotationsResolverCB) {
 	}
 	<-ch
 }
+
+// GetRealizedPolicyRuleLabelsForKey returns the list of policy rule labels
+// which match a given flow key (in host byte-order). The returned
+// LabelArrayList is shallow-copied and therefore must not be mutated.
+// This function explicitly exported to be accessed by code outside of the
+// Cilium source code tree and for testing.
+func (e *Endpoint) GetRealizedPolicyRuleLabelsForKey(key policy.Key) (
+	derivedFrom labels.LabelArrayList,
+	revision uint64,
+	ok bool,
+) {
+	e.mutex.RLock()
+	defer e.mutex.RUnlock()
+
+	entry, ok := e.realizedPolicy.PolicyMapState[key]
+	if !ok {
+		return nil, 0, false
+	}
+
+	return entry.DerivedFromRules, e.policyRevision, true
+}

pkg/hubble/testutils/fake.go
@@ -29,6 +29,8 @@ import (
 	poolTypes "github.com/cilium/cilium/pkg/hubble/relay/pool/types"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipcache"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/policy"
 
 	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/golang/protobuf/ptypes/wrappers"
@@ -513,6 +515,9 @@ type FakeEndpointInfo struct {
 	PodName      string
 	PodNamespace string
 	Labels       []string
+
+	PolicyMap      map[policy.Key]labels.LabelArrayList
+	PolicyRevision uint64
 }
 
 // GetID returns the ID of the endpoint.
@@ -538,4 +543,13 @@ func (e *FakeEndpointInfo) GetK8sNamespace() string {
 // GetLabels returns the labels of the endpoint.
 func (e *FakeEndpointInfo) GetLabels() []string {
 	return e.Labels
+}
+
+func (e *FakeEndpointInfo) GetRealizedPolicyRuleLabelsForKey(key policy.Key) (
+	derivedFrom labels.LabelArrayList,
+	revision uint64,
+	ok bool,
+) {
+	derivedFrom, ok = e.PolicyMap[key]
+	return derivedFrom, e.PolicyRevision, ok
 }

pkg/hubble/parser/threefour/parser_test.go
@@ -32,8 +32,11 @@ import (
 	"github.com/cilium/cilium/pkg/hubble/testutils"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipcache"
+	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/monitor"
 	"github.com/cilium/cilium/pkg/monitor/api"
+	"github.com/cilium/cilium/pkg/policy"
+	"github.com/cilium/cilium/pkg/policy/trafficdirection"
 	"github.com/cilium/cilium/pkg/source"
 
 	"github.com/google/gopacket"
@@ -443,12 +446,42 @@ func TestDecodeTrafficDirection(t *testing.T) {
 	localIP := net.ParseIP("1.2.3.4")
 	localEP := uint16(1234)
 	remoteIP := net.ParseIP("5.6.7.8")
+	remoteID := uint32(5678)
+
+	directionFromProto := func(direction flowpb.TrafficDirection) trafficdirection.TrafficDirection {
+		switch direction {
+		case flowpb.TrafficDirection_INGRESS:
+			return trafficdirection.Ingress
+		case flowpb.TrafficDirection_EGRESS:
+			return trafficdirection.Egress
+		}
+		return trafficdirection.Invalid
+	}
+
+	type policyGetter interface {
+		GetRealizedPolicyRuleLabelsForKey(key policy.Key) (
+			derivedFrom labels.LabelArrayList,
+			revision uint64,
+			ok bool,
+		)
+	}
+	policyLabel := labels.LabelArrayList{labels.ParseLabelArray("foo=bar")}
+	policyKey := policy.Key{
+		Identity:         remoteID,
+		DestPort:         0,
+		Nexthdr:          0,
+		TrafficDirection: trafficdirection.Egress.Uint8(),
+	}
 
 	endpointGetter := &testutils.FakeEndpointGetter{
 		OnGetEndpointInfo: func(ip net.IP) (endpoint v1.EndpointInfo, ok bool) {
 			if ip.Equal(localIP) {
 				return &testutils.FakeEndpointInfo{
 					ID: uint64(localEP),
+					PolicyMap: map[policy.Key]labels.LabelArrayList{
+						policyKey: policyLabel,
+					},
+					PolicyRevision: 1,
 				}, true
 			}
 			return nil, false
@@ -561,13 +594,24 @@ func TestDecodeTrafficDirection(t *testing.T) {
 
 	// PolicyVerdictNotify Egress
 	pvn := monitor.PolicyVerdictNotify{
-		Type:   byte(api.MessageTypePolicyVerdict),
-		Source: localEP,
-		Flags:  api.PolicyEgress,
+		Type:        byte(api.MessageTypePolicyVerdict),
+		Source:      localEP,
+		Flags:       api.PolicyEgress,
+		RemoteLabel: identity.NumericIdentity(remoteID),
 	}
 	f = parseFlow(pvn, localIP, remoteIP)
 	assert.Equal(t, flowpb.TrafficDirection_EGRESS, f.GetTrafficDirection())
 	assert.Equal(t, uint32(localEP), f.GetSource().GetID())
+
+	ep, ok := endpointGetter.GetEndpointInfo(localIP)
+	assert.Equal(t, true, ok)
+	lbls, rev, ok := ep.(policyGetter).GetRealizedPolicyRuleLabelsForKey(policy.Key{
+		Identity:         f.GetDestination().GetIdentity(),
+		TrafficDirection: directionFromProto(f.GetTrafficDirection()).Uint8(),
+	})
+	assert.Equal(t, true, ok)
+	assert.Equal(t, lbls, policyLabel)
+	assert.Equal(t, uint64(1), rev)
 
 	// PolicyVerdictNotify Ingress
 	pvn = monitor.PolicyVerdictNotify{