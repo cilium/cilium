ac7773024a1b996e9c52a139ea10d13d89de30da
Paul Chaignon <paul@cilium.io>
2020-12-07 23:20:29 +0100

    test: Use gomega.Eventually instead of helpers.RepeatUntil
    [ upstream commit 002bd9c37c4d56c12878da1ec24772f7c7369d97 ]
    
    Suggested-by: Chris Tarazi <chris@isovalent.com>
    Signed-off-by: Paul Chaignon <paul@cilium.io>
    Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

--- test/k8sT/Policies.go
+++ test/k8sT/Policies.go
@@ -1241,13 +1241,10 @@ var _ = Describe("K8sPolicyTest", func() {
 				defer monitorCancel()
 
 				By("Asserting that the expected policy verdict logs are in the monitor output")
-				var verdicts int
-				err = helpers.RepeatUntilTrueDefaultTimeout(func() bool {
-					verdicts = len(policyVerdictDenyRegex.FindAll(monitor.CombineOutput().Bytes(), -1))
-					return verdicts >= count
-				})
-				Expect(err).To(BeNil(), "Monitor output is missing verdicts (%d < %d): %s\n%s",
-					verdicts, count, policyVerdictDenyRegex, monitor.CombineOutput().Bytes())
+				Eventually(func() int {
+					return len(policyVerdictDenyRegex.FindAll(monitor.CombineOutput().Bytes(), -1))
+				}).Should(BeNumerically(">=", count), "Monitor output is missing verdicts: %s\n%s",
+					policyVerdictDenyRegex, monitor.CombineOutput().Bytes())
 			})
 
 			It("connectivity is restored after importing ingress policy", func() {
@@ -1275,13 +1272,10 @@ var _ = Describe("K8sPolicyTest", func() {
 				defer monitorCancel()
 
 				By("Asserting that the expected policy verdict logs are in the monitor output")
-				var verdicts int
-				err = helpers.RepeatUntilTrueDefaultTimeout(func() bool {
-					verdicts = len(policyVerdictAllowRegex.FindAll(monitor.CombineOutput().Bytes(), -1))
-					return verdicts >= count
-				})
-				Expect(err).To(BeNil(), "Monitor output is missing verdicts (%d < %d): %s\n%s",
-					verdicts, count, policyVerdictAllowRegex, monitor.CombineOutput().Bytes())
+				Eventually(func() int {
+					return len(policyVerdictAllowRegex.FindAll(monitor.CombineOutput().Bytes(), -1))
+				}).Should(BeNumerically(">=", count), "Monitor output is missing verdicts: %s\n%s",
+					policyVerdictAllowRegex, monitor.CombineOutput().Bytes())
 			})
 
 			Context("With host policy", func() {
@@ -1326,13 +1320,10 @@ var _ = Describe("K8sPolicyTest", func() {
 					defer monitorCancel()
 
 					By("Asserting that the expected policy verdict logs are in the monitor output")
-					var verdicts int
-					err = helpers.RepeatUntilTrueDefaultTimeout(func() bool {
-						verdicts = len(policyVerdictDenyRegex.FindAll(monitor.CombineOutput().Bytes(), -1))
-						return verdicts >= count
-					})
-					Expect(err).To(BeNil(), "Monitor output is missing verdicts (%d < %d): %s\n%s",
-						verdicts, count, policyVerdictDenyRegex, monitor.CombineOutput().Bytes())
+					Eventually(func() int {
+						return len(policyVerdictDenyRegex.FindAll(monitor.CombineOutput().Bytes(), -1))
+					}).Should(BeNumerically(">=", count), "Monitor output is missing verdicts: %s\n%s",
+						policyVerdictDenyRegex, monitor.CombineOutput().Bytes())
 				})
 
 				It("Connectivity is restored after importing ingress policy", func() {
@@ -1360,13 +1351,10 @@ var _ = Describe("K8sPolicyTest", func() {
 					defer monitorCancel()
 
 					By("Asserting that the expected policy verdict logs are in the monitor output")
-					var verdicts int
-					err = helpers.RepeatUntilTrueDefaultTimeout(func() bool {
-						verdicts = len(policyVerdictAllowRegex.FindAll(monitor.CombineOutput().Bytes(), -1))
-						return verdicts >= count
-					})
-					Expect(err).To(BeNil(), "Monitor output is missing verdicts (%d < %d): %s\n%s",
-						verdicts, count, policyVerdictAllowRegex, monitor.CombineOutput().Bytes())
+					Eventually(func() int {
+						return len(policyVerdictAllowRegex.FindAll(monitor.CombineOutput().Bytes(), -1))
+					}).Should(BeNumerically(">=", count), "Monitor output is missing verdicts: %s\n%s",
+						policyVerdictAllowRegex, monitor.CombineOutput().Bytes())
 
 					By("Removing the fromCIDR+toPorts ingress host policy")
 					// This is to ensure this policy is always removed before the default-deny one.
