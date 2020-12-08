14fcc4ad4d1a61b04ac65e6ed7f60b7c13aa6d2a
Robin Hahling <robin.hahling@gw-computing.net>
2020-12-08 14:32:07 +0100

    api/flow: add HUBBLE_RING_BUFFER to LostEvent types
    This will allow tracking events that are lost in Hubble's ring buffer,
    typically because they have been overwritten before the reader had a
    chance to read them.
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- api/v1/observer/observer.pb.go
+++ api/v1/observer/observer.pb.go
@@ -182,6 +182,7 @@ type LostEventSource = flow.LostEventSource
 const LostEventSource_UNKNOWN_LOST_EVENT_SOURCE = flow.LostEventSource_UNKNOWN_LOST_EVENT_SOURCE
 const LostEventSource_PERF_EVENT_RING_BUFFER = flow.LostEventSource_PERF_EVENT_RING_BUFFER
 const LostEventSource_OBSERVER_EVENTS_QUEUE = flow.LostEventSource_OBSERVER_EVENTS_QUEUE
+const LostEventSource_HUBBLE_RING_BUFFER = flow.LostEventSource_HUBBLE_RING_BUFFER
 
 var LostEventSource_name = flow.LostEventSource_name
 var LostEventSource_value = flow.LostEventSource_value
