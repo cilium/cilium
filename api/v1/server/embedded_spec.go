5e98c04da96fe21860ea7c1998e54d43eee7b8f8
Chris Tarazi <chris@isovalent.com>
2020-12-09 11:34:53 -0800

    api: Extend IPAM to accept interface number
    [ upstream commit ff6b5ffa974c170a883d877f1de2d64d78eb5802 ]
    
    [ upstream commit a5d696a4e; forward-ported from v1.7 tree ]
    
    This is needed in ENI mode. In upcoming commits, the interface number
    (ENI.Number) will be used to compute the per-ENI table ID in order to
    store rules and routes for the ENI datapath.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- api/v1/server/embedded_spec.go
+++ api/v1/server/embedded_spec.go
@@ -2457,6 +2457,10 @@ func init() {
           "description": "IP of gateway",
           "type": "string"
         },
+        "interface-number": {
+          "description": "InterfaceNumber is a field for generically identifying an interface. This is only useful in ENI mode.\n",
+          "type": "string"
+        },
         "ip": {
           "description": "Allocated IP for endpoint",
           "type": "string"
@@ -6393,6 +6397,10 @@ func init() {
         },
         "gateway": {
           "description": "IP of gateway",
+          "type": "string"
+        },
+        "interface-number": {
+          "description": "InterfaceNumber is a field for generically identifying an interface. This is only useful in ENI mode.\n",
           "type": "string"
         },
         "ip": {
