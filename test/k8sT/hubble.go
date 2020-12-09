af4a10e50f7354c81d06ed0369075952f36b6cfe
Paul Chaignon <paul@cilium.io>
2020-12-09 19:16:11 +0100

    test: Run K8sHubble on GKE and 4.9 pipelines
    The Hubble tests check packet trace events that change depending on
    whether our kube-proxy replacement is used. We thus run those tests both
    on GKE (with our kube-proxy replacement) and on 4.9 (without). We
    however don't need to run on all our kernel versions.
    
    This commit only moves all the initialization code into the Hubble
    Observe Context, merging BeforeAlls, AfterAll, and variables.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- test/k8sT/hubble.go
+++ test/k8sT/hubble.go
@@ -32,129 +32,109 @@ import (
 )
 
 var _ = Describe("K8sHubbleTest", func() {
-
-	var (
-		kubectl        *helpers.Kubectl
-		ciliumFilename string
-		k8s1NodeName   string
-		ciliumPodK8s1  string
-
-		hubbleRelayNamespace = helpers.CiliumNamespace
-		hubbleRelayService   = "hubble-relay"
-		hubbleRelayAddress   string
-
-		demoPath string
-
-		app1Service    = "app1-service"
-		app1Labels     = "id=app1,zgroup=testapp"
-		apps           = []string{helpers.App1, helpers.App2, helpers.App3}
-		prometheusPort = "9091"
-	)
-
-	addVisibilityAnnotation := func(ns, podLabels, direction, port, l4proto, l7proto string) {
-		visibilityAnnotation := fmt.Sprintf("<%s/%s/%s/%s>", direction, port, l4proto, l7proto)
-		By("Adding visibility annotation %s on pod with labels %s", visibilityAnnotation, podLabels)
-
-		// Prints <node>=<ns>/<podname> for each pod the annotation was applied to
-		res := kubectl.Exec(fmt.Sprintf("%s annotate pod -n %s -l %s %s=%q"+
-			" -o 'jsonpath={.spec.nodeName}={.metadata.namespace}/{.metadata.name}{\"\\n\"}'",
-			helpers.KubectlCmd,
-			ns, app1Labels,
-			annotation.ProxyVisibility, visibilityAnnotation))
-		res.ExpectSuccess("adding proxy visibility annotation failed")
-
-		// For each pod, check that the Cilium proxy-statistics contain the new annotation
-		expectedProxyState := strings.ToLower(visibilityAnnotation)
-		for node, podName := range res.KVOutput() {
-			ciliumPod, err := kubectl.GetCiliumPodOnNode(node)
-			Expect(err).To(BeNil())
-
-			// Extract annotation from endpoint model of pod. It does not have the l4proto, so we insert it manually.
-			cmd := fmt.Sprintf("cilium endpoint get pod-name:%s"+
-				" -o jsonpath='{range [*].status.policy.proxy-statistics[*]}<{.location}/{.port}/%s/{.protocol}>{\"\\n\"}{end}'",
-				podName, strings.ToLower(l4proto))
-			err = kubectl.CiliumExecUntilMatch(ciliumPod, cmd, expectedProxyState)
-			Expect(err).To(BeNil(), "timed out waiting for endpoint to regenerate for visibility annotation")
-		}
-	}
-
-	removeVisibilityAnnotation := func(ns, podLabels string) {
-		By("Removing visibility annotation on pod with labels %s", app1Labels)
-		res := kubectl.Exec(fmt.Sprintf("%s annotate pod -n %s -l %s %s-", helpers.KubectlCmd, ns, podLabels, annotation.ProxyVisibility))
-		res.ExpectSuccess("removing proxy visibility annotation failed")
-	}
-
-	hubbleObserveUntilMatch := func(hubblePod, args, filter, expected string, timeout time.Duration) {
-		hubbleObserve := func() bool {
-			res := kubectl.HubbleObserve(hubblePod, args)
-			res.ExpectSuccess("hubble observe invocation failed: %q", res.OutputPrettyPrint())
-
-			lines, err := res.FilterLines(filter)
-			Expect(err).Should(BeNil(), "hubble observe: invalid filter: %q", filter)
-
-			for _, line := range lines {
-				if line.String() == expected {
-					return true
-				}
-			}
-
-			return false
-		}
-
-		Eventually(hubbleObserve, timeout).Should(BeTrue(),
-			"hubble observe: filter %q never matched expected string %q", filter, expected)
-	}
-
-	BeforeAll(func() {
-		kubectl = helpers.CreateKubectl(helpers.K8s1VMName(), logger)
-		k8s1NodeName, _ = kubectl.GetNodeInfo(helpers.K8s1)
-
-		demoPath = helpers.ManifestGet(kubectl.BasePath(), "demo.yaml")
-
-		ciliumFilename = helpers.TimestampFilename("cilium.yaml")
-		DeployCiliumOptionsAndDNS(kubectl, ciliumFilename, map[string]string{
-			"hubble.metrics.enabled": `"{dns:query;ignoreAAAA,drop,tcp,flow,port-distribution,icmp,http}"`,
-			"hubble.relay.enabled":   "true",
-		})
-
-		var err error
-		ciliumPodK8s1, err = kubectl.GetCiliumPodOnNodeWithLabel(helpers.K8s1)
-		Expect(err).Should(BeNil(), "unable to find hubble-cli pod on %s", helpers.K8s1)
-
-		ExpectHubbleRelayReady(kubectl, hubbleRelayNamespace)
-		hubbleRelayIP, hubbleRelayPort, err := kubectl.GetServiceHostPort(hubbleRelayNamespace, hubbleRelayService)
-		Expect(err).Should(BeNil(), "Cannot get service %s", hubbleRelayService)
-		Expect(govalidator.IsIP(hubbleRelayIP)).Should(BeTrue(), "hubbleRelayIP is not an IP")
-		hubbleRelayAddress = net.JoinHostPort(hubbleRelayIP, strconv.Itoa(hubbleRelayPort))
-	})
-
-	AfterFailed(func() {
-		kubectl.CiliumReport("cilium endpoint list")
-	})
-
-	JustAfterEach(func() {
-		kubectl.ValidateNoErrorsInLogs(CurrentGinkgoTestDescription().Duration)
-	})
-
-	AfterEach(func() {
-		ExpectAllPodsTerminated(kubectl)
-	})
-
-	AfterAll(func() {
-		kubectl.DeleteHubbleRelay(hubbleRelayNamespace)
-		UninstallCiliumFromManifest(kubectl, ciliumFilename)
-		kubectl.CloseSSHClient()
-	})
-
-	Context("Hubble Observe", func() {
+	// We want to run Hubble tests both with and without our kube-proxy
+	// replacement, as the trace events depend on it. We thus run the tests
+	// on GKE and our 4.9 pipeline.
+	SkipContextIf(helpers.RunsOnNetNextOr419Kernel, "Hubble Observe", func() {
 		var (
+			kubectl        *helpers.Kubectl
+			ciliumFilename string
+			k8s1NodeName   string
+			ciliumPodK8s1  string
+
+			hubbleRelayNamespace = helpers.CiliumNamespace
+			hubbleRelayService   = "hubble-relay"
+			hubbleRelayAddress   string
+
+			demoPath string
+
+			app1Service    = "app1-service"
+			app1Labels     = "id=app1,zgroup=testapp"
+			apps           = []string{helpers.App1, helpers.App2, helpers.App3}
+			prometheusPort = "9091"
+
 			namespaceForTest string
 			appPods          map[string]string
 			app1ClusterIP    string
 			app1Port         int
 		)
 
+		addVisibilityAnnotation := func(ns, podLabels, direction, port, l4proto, l7proto string) {
+			visibilityAnnotation := fmt.Sprintf("<%s/%s/%s/%s>", direction, port, l4proto, l7proto)
+			By("Adding visibility annotation %s on pod with labels %s", visibilityAnnotation, podLabels)
+
+			// Prints <node>=<ns>/<podname> for each pod the annotation was applied to
+			res := kubectl.Exec(fmt.Sprintf("%s annotate pod -n %s -l %s %s=%q"+
+				" -o 'jsonpath={.spec.nodeName}={.metadata.namespace}/{.metadata.name}{\"\\n\"}'",
+				helpers.KubectlCmd,
+				ns, app1Labels,
+				annotation.ProxyVisibility, visibilityAnnotation))
+			res.ExpectSuccess("adding proxy visibility annotation failed")
+
+			// For each pod, check that the Cilium proxy-statistics contain the new annotation
+			expectedProxyState := strings.ToLower(visibilityAnnotation)
+			for node, podName := range res.KVOutput() {
+				ciliumPod, err := kubectl.GetCiliumPodOnNode(node)
+				Expect(err).To(BeNil())
+
+				// Extract annotation from endpoint model of pod. It does not have the l4proto, so we insert it manually.
+				cmd := fmt.Sprintf("cilium endpoint get pod-name:%s"+
+					" -o jsonpath='{range [*].status.policy.proxy-statistics[*]}<{.location}/{.port}/%s/{.protocol}>{\"\\n\"}{end}'",
+					podName, strings.ToLower(l4proto))
+				err = kubectl.CiliumExecUntilMatch(ciliumPod, cmd, expectedProxyState)
+				Expect(err).To(BeNil(), "timed out waiting for endpoint to regenerate for visibility annotation")
+			}
+		}
+
+		removeVisibilityAnnotation := func(ns, podLabels string) {
+			By("Removing visibility annotation on pod with labels %s", app1Labels)
+			res := kubectl.Exec(fmt.Sprintf("%s annotate pod -n %s -l %s %s-", helpers.KubectlCmd, ns, podLabels, annotation.ProxyVisibility))
+			res.ExpectSuccess("removing proxy visibility annotation failed")
+		}
+
+		hubbleObserveUntilMatch := func(hubblePod, args, filter, expected string, timeout time.Duration) {
+			hubbleObserve := func() bool {
+				res := kubectl.HubbleObserve(hubblePod, args)
+				res.ExpectSuccess("hubble observe invocation failed: %q", res.OutputPrettyPrint())
+
+				lines, err := res.FilterLines(filter)
+				Expect(err).Should(BeNil(), "hubble observe: invalid filter: %q", filter)
+
+				for _, line := range lines {
+					if line.String() == expected {
+						return true
+					}
+				}
+
+				return false
+			}
+
+			Eventually(hubbleObserve, timeout).Should(BeTrue(),
+				"hubble observe: filter %q never matched expected string %q", filter, expected)
+		}
+
 		BeforeAll(func() {
+			kubectl = helpers.CreateKubectl(helpers.K8s1VMName(), logger)
+			k8s1NodeName, _ = kubectl.GetNodeInfo(helpers.K8s1)
+
+			demoPath = helpers.ManifestGet(kubectl.BasePath(), "demo.yaml")
+
+			ciliumFilename = helpers.TimestampFilename("cilium.yaml")
+			DeployCiliumOptionsAndDNS(kubectl, ciliumFilename, map[string]string{
+				"hubble.metrics.enabled": `"{dns:query;ignoreAAAA,drop,tcp,flow,port-distribution,icmp,http}"`,
+				"hubble.relay.enabled":   "true",
+			})
+
+			var err error
+			ciliumPodK8s1, err = kubectl.GetCiliumPodOnNodeWithLabel(helpers.K8s1)
+			Expect(err).Should(BeNil(), "unable to find hubble-cli pod on %s", helpers.K8s1)
+
+			ExpectHubbleRelayReady(kubectl, hubbleRelayNamespace)
+			hubbleRelayIP, hubbleRelayPort, err := kubectl.GetServiceHostPort(hubbleRelayNamespace, hubbleRelayService)
+			Expect(err).Should(BeNil(), "Cannot get service %s", hubbleRelayService)
+			Expect(govalidator.IsIP(hubbleRelayIP)).Should(BeTrue(), "hubbleRelayIP is not an IP")
+			hubbleRelayAddress = net.JoinHostPort(hubbleRelayIP, strconv.Itoa(hubbleRelayPort))
+
 			namespaceForTest = helpers.GenerateNamespaceForTest("")
 			kubectl.NamespaceDelete(namespaceForTest)
 			res := kubectl.NamespaceCreate(namespaceForTest)
@@ -163,7 +143,7 @@ var _ = Describe("K8sHubbleTest", func() {
 			res = kubectl.Apply(helpers.ApplyOptions{FilePath: demoPath, Namespace: namespaceForTest})
 			res.ExpectSuccess("could not create resource")
 
-			err := kubectl.WaitforPods(namespaceForTest, "-l zgroup=testapp", helpers.HelperTimeout)
+			err = kubectl.WaitforPods(namespaceForTest, "-l zgroup=testapp", helpers.HelperTimeout)
 			Expect(err).Should(BeNil(), "test pods are not ready after timeout")
 
 			appPods = helpers.GetAppPods(apps, namespaceForTest, kubectl, "id")
@@ -171,9 +151,25 @@ var _ = Describe("K8sHubbleTest", func() {
 			Expect(err).To(BeNil(), "unable to find service in %q namespace", namespaceForTest)
 		})
 
+		AfterFailed(func() {
+			kubectl.CiliumReport("cilium endpoint list")
+		})
+
+		JustAfterEach(func() {
+			kubectl.ValidateNoErrorsInLogs(CurrentGinkgoTestDescription().Duration)
+		})
+
+		AfterEach(func() {
+			ExpectAllPodsTerminated(kubectl)
+		})
+
 		AfterAll(func() {
 			kubectl.Delete(demoPath)
 			kubectl.NamespaceDelete(namespaceForTest)
+
+			kubectl.DeleteHubbleRelay(hubbleRelayNamespace)
+			UninstallCiliumFromManifest(kubectl, ciliumFilename)
+			kubectl.CloseSSHClient()
 		})
 
 		It("Test L3/L4 Flow", func() {
