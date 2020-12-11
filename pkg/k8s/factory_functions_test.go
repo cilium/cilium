cilium/cilium

commit 9d7082e9a57e776120c27806555175bd4fc1e27a

policy: add k8s controller to daemon for clusterwide policies
* Update k8s_watcher.go to include controller to watch for
CiliumClusterwideNetworkPolicy CRD.
* Update ExtractNamespace function in k8s utils to account for the fact
that any object with empty namespace is a cluster scoped object and we
should not automatically add default namespace to such objects.

Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

pkg/k8s/factory_functions.go
@@ -375,7 +375,8 @@ func ConvertToIngress(obj interface{}) interface{} {
 	}
 }
 
-// ConvertToCNPWithStatus converts a *cilium_v2.CiliumNetworkPolicy into a
+// ConvertToCNPWithStatus converts a *cilium_v2.CiliumNetworkPolicy or a
+// *cilium_v2.CiliumClusterwideNetworkPolicy into a
 // *types.SlimCNP or a cache.DeletedFinalStateUnknown into
 // a cache.DeletedFinalStateUnknown with a *types.SlimCNP in its Obj.
 // If the given obj can't be cast into either *cilium_v2.CiliumNetworkPolicy
@@ -386,24 +387,39 @@ func ConvertToCNPWithStatus(obj interface{}) interface{} {
 		return &types.SlimCNP{
 			CiliumNetworkPolicy: concreteObj,
 		}
+	case *cilium_v2.CiliumClusterwideNetworkPolicy:
+		return &types.SlimCNP{
+			CiliumNetworkPolicy: &concreteObj.CiliumNetworkPolicy,
+		}
 	case cache.DeletedFinalStateUnknown:
-		cnp, ok := concreteObj.Obj.(*cilium_v2.CiliumNetworkPolicy)
-		if !ok {
+		switch cnp := concreteObj.Obj.(type) {
+		case *cilium_v2.CiliumNetworkPolicy:
+			return cache.DeletedFinalStateUnknown{
+				Key: concreteObj.Key,
+				Obj: &types.SlimCNP{
+					CiliumNetworkPolicy: cnp,
+				},
+			}
+
+		case *cilium_v2.CiliumClusterwideNetworkPolicy:
+			return cache.DeletedFinalStateUnknown{
+				Key: concreteObj.Key,
+				Obj: &types.SlimCNP{
+					CiliumNetworkPolicy: &cnp.CiliumNetworkPolicy,
+				},
+			}
+
+		default:
 			return obj
 		}
-		return cache.DeletedFinalStateUnknown{
-			Key: concreteObj.Key,
-			Obj: &types.SlimCNP{
-				CiliumNetworkPolicy: cnp,
-			},
-		}
+
 	default:
 		return obj
 	}
 }
 
-// ConvertToCNP converts a *cilium_v2.CiliumNetworkPolicy into a
-// *types.SlimCNP without the Status field of the given CNP, or a
+// ConvertToCNP converts *cilium_v2.CiliumNetworkPolicy or *cilium_v2.CiliumClusterwideNetworkPolicy
+// into a *types.SlimCNP without the Status field of the given CNP, or a
 // cache.DeletedFinalStateUnknown into a cache.DeletedFinalStateUnknown with a
 // *types.SlimCNP, also without the Status field of the given CNP, in its Obj.
 // If the given obj can't be cast into either *cilium_v2.CiliumNetworkPolicy
@@ -423,24 +439,54 @@ func ConvertToCNP(obj interface{}) interface{} {
 		}
 		*concreteObj = cilium_v2.CiliumNetworkPolicy{}
 		return cnp
-	case cache.DeletedFinalStateUnknown:
-		cnp, ok := concreteObj.Obj.(*cilium_v2.CiliumNetworkPolicy)
-		if !ok {
-			return obj
-		}
-		dfsu := cache.DeletedFinalStateUnknown{
-			Key: concreteObj.Key,
-			Obj: &types.SlimCNP{
-				CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
-					TypeMeta:   cnp.TypeMeta,
-					ObjectMeta: cnp.ObjectMeta,
-					Spec:       cnp.Spec,
-					Specs:      cnp.Specs,
-				},
+
+	case *cilium_v2.CiliumClusterwideNetworkPolicy:
+		cnp := &types.SlimCNP{
+			CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
+				TypeMeta:   concreteObj.TypeMeta,
+				ObjectMeta: concreteObj.ObjectMeta,
+				Spec:       concreteObj.Spec,
+				Specs:      concreteObj.Specs,
 			},
 		}
-		*cnp = cilium_v2.CiliumNetworkPolicy{}
-		return dfsu
+		*concreteObj = cilium_v2.CiliumClusterwideNetworkPolicy{}
+		return cnp
+
+	case cache.DeletedFinalStateUnknown:
+		switch cnp := concreteObj.Obj.(type) {
+		case *cilium_v2.CiliumNetworkPolicy:
+			dfsu := cache.DeletedFinalStateUnknown{
+				Key: concreteObj.Key,
+				Obj: &types.SlimCNP{
+					CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
+						TypeMeta:   cnp.TypeMeta,
+						ObjectMeta: cnp.ObjectMeta,
+						Spec:       cnp.Spec,
+						Specs:      cnp.Specs,
+					},
+				},
+			}
+			*cnp = cilium_v2.CiliumNetworkPolicy{}
+			return dfsu
+
+		case *cilium_v2.CiliumClusterwideNetworkPolicy:
+			dfsu := cache.DeletedFinalStateUnknown{
+				Key: concreteObj.Key,
+				Obj: &types.SlimCNP{
+					CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
+						TypeMeta:   cnp.TypeMeta,
+						ObjectMeta: cnp.ObjectMeta,
+						Spec:       cnp.Spec,
+						Specs:      cnp.Specs,
+					},
+				},
+			}
+			*cnp = cilium_v2.CiliumClusterwideNetworkPolicy{}
+			return dfsu
+
+		default:
+			return obj
+		}
 	default:
 		return obj
 	}

pkg/k8s/apis/cilium.io/v2/types.go
@@ -291,58 +291,6 @@ type CiliumClusterwideNetworkPolicy struct {
 	CiliumNetworkPolicy
 }
 
-// Parse parses a CiliumClusterwideNetworkPolicy and returns a list of cilium policy
-// rules. This is similar to the parse function used in CiliumNetworkPolicy with
-// the only difference that since the CRD is cluster scoped we don't send any namespace
-// here.
-func (r *CiliumClusterwideNetworkPolicy) Parse() (api.Rules, error) {
-	if r.ObjectMeta.Name == "" {
-		return nil, fmt.Errorf("CiliumClusterwideNetworkPolicy must have name")
-	}
-
-	name := r.ObjectMeta.Name
-	uid := r.ObjectMeta.UID
-
-	retRules := api.Rules{}
-
-	if r.Spec != nil {
-		if err := r.Spec.Sanitize(); err != nil {
-			return nil, fmt.Errorf("Invalid CiliumClusterwideNetworkPolicy spec: %s", err)
-
-		}
-		// CiliumClusterwideNetworkPolicy is cluster scoped CRD so does not belong to a
-		// particular namespace.
-		cr := k8sCiliumUtils.ParseToCiliumRule("", name, uid, r.Spec)
-		retRules = append(retRules, cr)
-	}
-	if r.Specs != nil {
-		for _, rule := range r.Specs {
-			if err := rule.Sanitize(); err != nil {
-				return nil, fmt.Errorf("Invalid CiliumClusterwideNetworkPolicy specs: %s", err)
-
-			}
-			cr := k8sCiliumUtils.ParseToCiliumRule("", name, uid, rule)
-			retRules = append(retRules, cr)
-		}
-	}
-
-	return retRules, nil
-}
-
-// GetControllerName returns the unique name for the controller manager.
-func (r *CiliumClusterwideNetworkPolicy) GetControllerName() string {
-	name := r.ObjectMeta.GetName()
-	return fmt.Sprintf("%s (v2 %s)", k8sConst.CtrlPrefixPolicyStatus, name)
-}
-
-// GetIdentityLabels returns all rule labels in the CiliumNetworkPolicy.
-func (r *CiliumClusterwideNetworkPolicy) GetIdentityLabels() labels.LabelArray {
-	name := r.ObjectMeta.Name
-	uid := r.ObjectMeta.UID
-	return k8sCiliumUtils.GetPolicyLabels("", name, uid,
-		k8sCiliumUtils.ResourceTypeCiliumClusterwideNetworkPolicy)
-}
-
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // CiliumClusterwideNetworkPolicyList is a list of CiliumClusterwideNetworkPolicy objects

pkg/k8s/apis/cilium.io/v2/types_test.go
@@ -244,6 +244,7 @@ var (
 
 	ciliumRule = append(append([]byte(`{
     "metadata": {
+		"namespace": "default",
         "name": "rule1",
 		"uid": "`+uuidRule+`"
     },
@@ -251,6 +252,7 @@ var (
 }`)...)
 	ciliumRuleList = append(append(append(append([]byte(`{
     "metadata": {
+		"namespace": "default",
         "name": "rule1",
 		"uid": "`+uuidRule+`"
     },
@@ -274,6 +276,7 @@ func (s *CiliumV2Suite) TestParseSpec(c *C) {
 
 	expectedPolicyRule := &CiliumNetworkPolicy{
 		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
 			Name: "rule1",
 			UID:  uuidRule,
 		},
@@ -284,6 +287,7 @@ func (s *CiliumV2Suite) TestParseSpec(c *C) {
 
 	expectedPolicyRuleWithLabel := &CiliumNetworkPolicy{
 		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
 			Name: "rule1",
 			UID:  uuidRule,
 		},
@@ -341,6 +345,7 @@ func (s *CiliumV2Suite) TestParseRules(c *C) {
 
 	expectedPolicyRuleList := &CiliumNetworkPolicy{
 		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
 			Name: "rule1",
 			UID:  uuidRule,
 		},
@@ -351,6 +356,7 @@ func (s *CiliumV2Suite) TestParseRules(c *C) {
 
 	expectedPolicyRuleListWithLabel := &CiliumNetworkPolicy{
 		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
 			Name: "rule1",
 			UID:  uuidRule,
 		},

pkg/k8s/network_policy.go
@@ -57,7 +57,10 @@ func GetPolicyLabelsv1(np *networkingv1.NetworkPolicy) labels.LabelArray {
 		policyName = np.Name
 	}
 
-	ns := k8sUtils.ExtractNamespace(&np.ObjectMeta)
+	// Here we are using ExtractNamespaceOrDefault instead of ExtractNamespace because we know
+	// for sure that the Object is namespace scoped, so if no namespace is provided instead
+	// of assuming that the Object is cluster scoped we return the default namespace.
+	ns := k8sUtils.ExtractNamespaceOrDefault(&np.ObjectMeta)
 
 	return k8sCiliumUtils.GetPolicyLabels(ns, policyName, policyUID, resourceTypeNetworkPolicy)
 }
@@ -132,7 +135,9 @@ func ParseNetworkPolicy(np *networkingv1.NetworkPolicy) (api.Rules, error) {
 	ingresses := []api.IngressRule{}
 	egresses := []api.EgressRule{}
 
-	namespace := k8sUtils.ExtractNamespace(&np.ObjectMeta)
+	// Since we know that the object NetworkPolicy is namespace scoped we assign
+	// namespace to default namespace if the field is empty in the object.
+	namespace := k8sUtils.ExtractNamespaceOrDefault(&np.ObjectMeta)
 
 	for _, iRule := range np.Spec.Ingress {
 		fromRules := []api.IngressRule{}

pkg/k8s/utils/utils.go
@@ -21,17 +21,35 @@ import (
 )
 
 // ExtractNamespace extracts the namespace of ObjectMeta.
+// For cluster scoped objects the Namespace field is empty and this function
+// assumes that the object is returned from kubernetes itself implying that
+// the namespace is empty only and only when the Object is cluster scoped
+// and thus returns empty namespace for such objects.
 func ExtractNamespace(np metav1.Object) string {
+	return np.GetNamespace()
+}
+
+// ExtractNamespaceOrDefault extracts the namespace of ObjectMeta, it returns default
+// namespace if the namespace field in the ObjectMeta is empty.
+func ExtractNamespaceOrDefault(np metav1.Object) string {
 	ns := np.GetNamespace()
 	if ns == "" {
 		return v1.NamespaceDefault
 	}
+
 	return ns
 }
 
 // GetObjNamespaceName returns the object's namespace and name.
+// If the object is cluster scoped then the function returns only the object name
+// without any namespace prefix.
 func GetObjNamespaceName(obj metav1.Object) string {
-	return ExtractNamespace(obj) + "/" + obj.GetName()
+	ns := ExtractNamespace(obj)
+	if ns == "" {
+		return obj.GetName()
+	}
+
+	return ns + "/" + obj.GetName()
 }
 
 // GetObjUID returns the object's namespace and name.

pkg/k8s/watchers/cilium_clusterwide_network_policy.go
@@ -1,0 +1,134 @@
+// Copyright 2016-2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package watchers
+
+import (
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+
+	"github.com/cilium/cilium/pkg/k8s"
+	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/informer"
+	k8sversion "github.com/cilium/cilium/pkg/k8s/version"
+	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/serializer"
+)
+
+func (k *K8sWatcher) ciliumClusterwideNetworkPoliciesInit(ciliumNPClient *k8s.K8sCiliumClient, serCCNPs *serializer.FunctionQueue, swgCCNPs *lock.StoppableWaitGroup) {
+	var (
+		ccnpEventStore    cache.Store
+		ccnpConverterFunc informer.ConvertFunc
+	)
+	ccnpStore := cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
+	switch {
+	case k8sversion.Capabilities().Patch:
+		// k8s >= 1.13 does not require a store to update CNP status so
+		// we don't even need to keep the status of a CNP with us.
+		ccnpConverterFunc = k8s.ConvertToCNP
+	default:
+		ccnpEventStore = ccnpStore
+		ccnpConverterFunc = k8s.ConvertToCNPWithStatus
+	}
+
+	ciliumV2ClusterwidePolicyController := informer.NewInformerWithStore(
+		cache.NewListWatchFromClient(ciliumNPClient.CiliumV2().RESTClient(),
+			"ciliumclusterwidenetworkpolicies", v1.NamespaceAll, fields.Everything()),
+		&cilium_v2.CiliumClusterwideNetworkPolicy{},
+		0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				var valid, equal bool
+				defer func() { k.K8sEventReceived(metricCCNP, metricCreate, valid, equal) }()
+				if cnp := k8s.CopyObjToV2CNP(obj); cnp != nil {
+					valid = true
+					swgCCNPs.Add()
+					serCCNPs.Enqueue(func() error {
+						defer swgCCNPs.Done()
+						if cnp.RequiresDerivative() {
+							return nil
+						}
+						err := k.addCiliumNetworkPolicyV2(ciliumNPClient, ccnpEventStore, cnp)
+						k.K8sEventProcessed(metricCCNP, metricCreate, err == nil)
+						return nil
+					}, serializer.NoRetry)
+				}
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				var valid, equal bool
+				defer func() { k.K8sEventReceived(metricCCNP, metricUpdate, valid, equal) }()
+				if oldCNP := k8s.CopyObjToV2CNP(oldObj); oldCNP != nil {
+					valid = true
+					if newCNP := k8s.CopyObjToV2CNP(newObj); newCNP != nil {
+						if k8s.EqualV2CNP(oldCNP, newCNP) {
+							equal = true
+							return
+						}
+
+						swgCCNPs.Add()
+						serCCNPs.Enqueue(func() error {
+							defer swgCCNPs.Done()
+							if newCNP.RequiresDerivative() {
+								return nil
+							}
+
+							err := k.updateCiliumNetworkPolicyV2(ciliumNPClient, ccnpEventStore, oldCNP, newCNP)
+							k.K8sEventProcessed(metricCCNP, metricUpdate, err == nil)
+							return nil
+						}, serializer.NoRetry)
+					}
+				}
+			},
+			DeleteFunc: func(obj interface{}) {
+				var valid, equal bool
+				defer func() { k.K8sEventReceived(metricCCNP, metricDelete, valid, equal) }()
+				cnp := k8s.CopyObjToV2CNP(obj)
+				if cnp == nil {
+					deletedObj, ok := obj.(cache.DeletedFinalStateUnknown)
+					if !ok {
+						return
+					}
+					// Delete was not observed by the watcher but is
+					// removed from kube-apiserver. This is the last
+					// known state and the object no longer exists.
+					cnp = k8s.CopyObjToV2CNP(deletedObj.Obj)
+					if cnp == nil {
+						return
+					}
+				}
+				valid = true
+				swgCCNPs.Add()
+				serCCNPs.Enqueue(func() error {
+					defer swgCCNPs.Done()
+					err := k.deleteCiliumNetworkPolicyV2(cnp)
+					k.K8sEventProcessed(metricCCNP, metricDelete, err == nil)
+					return nil
+				}, serializer.NoRetry)
+			},
+		},
+		ccnpConverterFunc,
+		ccnpStore,
+	)
+	k.blockWaitGroupToSyncResources(
+		wait.NeverStop,
+		swgCCNPs,
+		ciliumV2ClusterwidePolicyController,
+		k8sAPIGroupCiliumClusterwideNetworkPolicyV2,
+	)
+
+	go ciliumV2ClusterwidePolicyController.Run(wait.NeverStop)
+	k.k8sAPIGroups.addAPI(k8sAPIGroupCiliumClusterwideNetworkPolicyV2)
+}

pkg/k8s/watchers/watcher.go
@@ -46,19 +46,21 @@ import (
 )
 
 const (
-	k8sAPIGroupCRD                   = "CustomResourceDefinition"
-	k8sAPIGroupNodeV1Core            = "core/v1::Node"
-	k8sAPIGroupNamespaceV1Core       = "core/v1::Namespace"
-	K8sAPIGroupServiceV1Core         = "core/v1::Service"
-	K8sAPIGroupEndpointV1Core        = "core/v1::Endpoint"
-	k8sAPIGroupPodV1Core             = "core/v1::Pods"
-	k8sAPIGroupNetworkingV1Core      = "networking.k8s.io/v1::NetworkPolicy"
-	k8sAPIGroupCiliumNetworkPolicyV2 = "cilium/v2::CiliumNetworkPolicy"
-	k8sAPIGroupCiliumNodeV2          = "cilium/v2::CiliumNode"
-	k8sAPIGroupCiliumEndpointV2      = "cilium/v2::CiliumEndpoint"
-	cacheSyncTimeout                 = 3 * time.Minute
+	k8sAPIGroupCRD                              = "CustomResourceDefinition"
+	k8sAPIGroupNodeV1Core                       = "core/v1::Node"
+	k8sAPIGroupNamespaceV1Core                  = "core/v1::Namespace"
+	K8sAPIGroupServiceV1Core                    = "core/v1::Service"
+	K8sAPIGroupEndpointV1Core                   = "core/v1::Endpoint"
+	k8sAPIGroupPodV1Core                        = "core/v1::Pods"
+	k8sAPIGroupNetworkingV1Core                 = "networking.k8s.io/v1::NetworkPolicy"
+	k8sAPIGroupCiliumNetworkPolicyV2            = "cilium/v2::CiliumNetworkPolicy"
+	k8sAPIGroupCiliumClusterwideNetworkPolicyV2 = "cilium/v2::CiliumClusterwideNetworkPolicy"
+	k8sAPIGroupCiliumNodeV2                     = "cilium/v2::CiliumNode"
+	k8sAPIGroupCiliumEndpointV2                 = "cilium/v2::CiliumEndpoint"
+	cacheSyncTimeout                            = 3 * time.Minute
 
 	metricCNP            = "CiliumNetworkPolicy"
+	metricCCNP           = "CiliumClusterwideNetworkPolicy"
 	metricEndpoint       = "Endpoint"
 	metricKNP            = "NetworkPolicy"
 	metricNS             = "Namespace"
@@ -291,6 +293,8 @@ func (k *K8sWatcher) InitK8sSubsystem() <-chan struct{} {
 			// we are enforcing the correct policies for each endpoint before
 			// restarting.
 			k8sAPIGroupCiliumNetworkPolicyV2,
+
+			k8sAPIGroupCiliumClusterwideNetworkPolicyV2,
 			// We we need to know about all other nodes
 			k8sAPIGroupCiliumNodeV2,
 			// We need all network policies in place before restoring to make sure
@@ -358,6 +362,11 @@ func (k *K8sWatcher) EnableK8sWatcher(queueSize uint) error {
 	serCNPs := serializer.NewFunctionQueue(queueSize)
 	swgCNPs := lock.NewStoppableWaitGroup()
 	k.ciliumNetworkPoliciesInit(ciliumNPClient, serCNPs, swgCNPs)
+
+	// cilium clusterwide network policy
+	serCCNPs := serializer.NewFunctionQueue(queueSize)
+	swgCCNPs := lock.NewStoppableWaitGroup()
+	k.ciliumClusterwideNetworkPoliciesInit(ciliumNPClient, serCCNPs, swgCCNPs)
 
 	// cilium nodes
 	asyncControllers.Add(1)

commit ed860a2eb7e596081f5c541fafce8b4091e9e1ca

pkg/k8s: fix nil pointer for invalid CCNP
If a CCNP does not contain a spec nor specs, which means the embedded
CNP is nil, it will cause a nil pointer exception if we try to access
those fields. To prevent this we should be checking for this embedded
CNP struct before access its fields.

Fixes: 9d7082e9a57e ("policy: add k8s controller to daemon for clusterwide policies")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/factory_functions.go
@@ -483,9 +483,11 @@ func ConvertToCCNP(obj interface{}) interface{} {
 			CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
 				TypeMeta:   concreteObj.TypeMeta,
 				ObjectMeta: concreteObj.ObjectMeta,
-				Spec:       concreteObj.Spec,
-				Specs:      concreteObj.Specs,
 			},
+		}
+		if concreteObj.CiliumNetworkPolicy != nil {
+			cnp.Spec = concreteObj.Spec
+			cnp.Specs = concreteObj.Specs
 		}
 		*concreteObj = cilium_v2.CiliumClusterwideNetworkPolicy{}
 		return cnp
@@ -495,16 +497,19 @@ func ConvertToCCNP(obj interface{}) interface{} {
 		if !ok {
 			return obj
 		}
+		slimCNP := &types.SlimCNP{
+			CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
+				TypeMeta:   cnp.TypeMeta,
+				ObjectMeta: cnp.ObjectMeta,
+			},
+		}
+		if cnp.CiliumNetworkPolicy != nil {
+			slimCNP.Spec = cnp.Spec
+			slimCNP.Specs = cnp.Specs
+		}
 		dfsu := cache.DeletedFinalStateUnknown{
 			Key: concreteObj.Key,
-			Obj: &types.SlimCNP{
-				CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
-					TypeMeta:   cnp.TypeMeta,
-					ObjectMeta: cnp.ObjectMeta,
-					Spec:       cnp.Spec,
-					Specs:      cnp.Specs,
-				},
-			},
+			Obj: slimCNP,
 		}
 		*cnp = cilium_v2.CiliumClusterwideNetworkPolicy{}
 		return dfsu

pkg/k8s/factory_functions_test.go
@@ -1174,6 +1174,15 @@ func (s *K8sSuite) Test_ConvertToCCNP(c *C) {
 			},
 		},
 		{
+			name: "A CCNP where it doesn't contain neither a spec nor specs",
+			args: args{
+				obj: &v2.CiliumClusterwideNetworkPolicy{},
+			},
+			want: &types.SlimCNP{
+				CiliumNetworkPolicy: &v2.CiliumNetworkPolicy{},
+			},
+		},
+		{
 			name: "delete final state unknown conversion",
 			args: args{
 				obj: cache.DeletedFinalStateUnknown{