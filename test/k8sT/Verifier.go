f6875de00cb292ad45205590d93d5feecf093257
Paul Chaignon <paul@cilium.io>
2020-12-15 15:26:52 +0100

    bpf: Maximize compiled program size for each kernel
    [ upstream commit 6fcc7ac439c531236416305003d130f3476a168a ]
    
    The make target for the BPF programs takes a KERNEL environment variable.
    This variable is used to try and maximize the program size for each
    kernel, by enabling as many features as possible. This new environment
    variable is also used in the existing K8sVerifier test.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- test/k8sT/Verifier.go
+++ test/k8sT/Verifier.go
@@ -72,13 +72,21 @@ var _ = Describe("K8sVerifier", func() {
 
 	SkipItIf(helpers.RunsOnNetNextOr419Kernel, "Runs the kernel verifier against Cilium's BPF datapath", func() {
 		By("Building BPF objects from the tree")
-		res := kubectl.ExecPodCmd(helpers.DefaultNamespace, podName, "make -C /cilium/bpf V=0")
+		kernel := "49"
+		switch {
+		case helpers.RunsOnNetNextKernel():
+			kernel = "netnext"
+		case helpers.RunsOn419Kernel():
+			kernel = "419"
+		}
+		cmd := fmt.Sprintf("make -C /cilium/bpf KERNEL=%s", kernel)
+		res := kubectl.ExecPodCmd(helpers.DefaultNamespace, podName, cmd)
 		res.ExpectSuccess("Expected compilation of the BPF objects to succeed")
 		res = kubectl.ExecPodCmd(helpers.DefaultNamespace, podName, "make -C /cilium/tools/maptool/")
 		res.ExpectSuccess("Expected compilation of maptool to succeed")
 
 		By("Running the verifier test script")
-		cmd := fmt.Sprintf("/cilium/test/%s", script)
+		cmd = fmt.Sprintf("/cilium/test/%s", script)
 		res = kubectl.ExecPodCmd(helpers.DefaultNamespace, podName, cmd)
 		res.ExpectSuccess("Expected the kernel verifier to pass for BPF programs")
 	})
