b870a920226e534540d6343c023fd406faa69362
Deepesh Pathak <deepshpathak@gmail.com>
2020-12-22 17:10:29 +0530

    test: remove old k8s version references/manifests from integration tests
    Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

--- test/k8sT/istio.go
+++ test/k8sT/istio.go
@@ -81,7 +81,7 @@ var _ = SkipContextIf(func() bool {
 	BeforeAll(func() {
 		k8sVersion := helpers.GetCurrentK8SEnv()
 		switch k8sVersion {
-		case "1.7", "1.8", "1.9", "1.10", "1.11", "1.12", "1.13":
+		case "1.13":
 			Skip(fmt.Sprintf("Istio %s doesn't support K8S %s", istioVersion, k8sVersion))
 		}
 
