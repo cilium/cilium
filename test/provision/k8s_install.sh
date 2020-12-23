6bffef3e83ba040e7926f95a5568164b0ce7be94
Chris Tarazi <chris@isovalent.com>
2020-12-22 16:43:15 -0800

    k8s: Update libraries to v1.18.14
    This commit also updates the older versions to v1.17.16, as well as
    bumping the CI K8s version to v1.18.14.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -285,7 +285,7 @@ case $K8S_VERSION in
         ;;
     "1.17")
         KUBERNETES_CNI_VERSION="0.8.7"
-        K8S_FULL_VERSION="1.17.15"
+        K8S_FULL_VERSION="1.17.16"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
@@ -299,7 +299,7 @@ case $K8S_VERSION in
         sudo apt-get install -y conntrack
         KUBERNETES_CNI_VERSION="0.8.7"
         KUBERNETES_CNI_OS="-linux"
-        K8S_FULL_VERSION="1.18.13"
+        K8S_FULL_VERSION="1.18.14"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
