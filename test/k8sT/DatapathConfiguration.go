9b58dd8f170674b948de38628421be3959712343
Paul Chaignon <paul@cilium.io>
2021-01-07 22:50:05 +0100

    test: Disable hostfw in monitor aggregation test
    Related: #14552
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- test/k8sT/DatapathConfiguration.go
+++ test/k8sT/DatapathConfiguration.go
@@ -95,6 +95,9 @@ var _ = Describe("K8sDatapathConfig", func() {
 				"bpf.monitorAggregation": "medium",
 				"bpf.monitorInterval":    "60s",
 				"bpf.monitorFlags":       "syn",
+				// Need to disable the host firewall for now due to complexity issue.
+				// See #14552 for details.
+				"hostFirewall": "false",
 			}, DeployCiliumOptionsAndDNS)
 
 			monitorRes, monitorCancel, targetIP := monitorConnectivityAcrossNodes(kubectl)
