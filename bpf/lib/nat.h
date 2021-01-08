263f975b0b2ee85178cdf73aca0177a090c1ff34
Martynas Pumputis <m@lambda.lt>
2021-01-08 13:59:15 +0100

    datapath: Fix unintended SNAT of ICMP ECHO
    Let's say that we have a pod sending ICMP ECHO request to outside. The
    handling of the request creates the following CT and NAT entries:
    
    CT  |    src     |     dst   | dir |
        +------------+-----------+-----+
        | outside:ID |   pod:0   | OUT |
    
    NAT |    src     |    dst    | dir |
        +------------+-----------+-----+
        | pod:ID     | outside:0 | OUT |
        +------------+-----------+-----+
        | outside:0  |   host:ID | IN  |
    
    Now, let's say that we have the outside sending ICMP echo request to the host
    running the pod with the same ID as above. The following NAT lookup is
    performed:
    
    outside:0 -> host:ID IN
    
    The lookup will find the NAT entry from the pod->outside case. This will
    translate the request making it to be delivered to the pod instead of
    the host.
    
    Fix this by making the ICMP ECHO ID placement in the NAT tuple to depend on the
    ICMP type instead of the packet direction.
    
    After this change, the NAT entries will be the same as above, but the lookup
    for the outside->host case is changed to the following:
    
    outside:ID -> host:0 IN (doesn't match any NAT entry above).
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- bpf/lib/nat.h
+++ bpf/lib/nat.h
@@ -538,7 +538,7 @@ static __always_inline __maybe_unused int snat_v4_process(struct __ctx_buff *ctx
 		if (icmphdr.type != ICMP_ECHO &&
 		    icmphdr.type != ICMP_ECHOREPLY)
 			return DROP_NAT_UNSUPP_PROTO;
-		if (dir == NAT_DIR_EGRESS) {
+		if (icmphdr.type == ICMP_ECHO) {
 			tuple.dport = 0;
 			tuple.sport = icmphdr.un.echo.id;
 		} else {
@@ -1013,7 +1013,7 @@ static __always_inline __maybe_unused int snat_v6_process(struct __ctx_buff *ctx
 		if (icmp6hdr.icmp6_type != ICMPV6_ECHO_REQUEST &&
 		    icmp6hdr.icmp6_type != ICMPV6_ECHO_REPLY)
 			return DROP_NAT_UNSUPP_PROTO;
-		if (dir == NAT_DIR_EGRESS) {
+		if (icmp6hdr.icmp6_type == ICMPV6_ECHO_REQUEST) {
 			tuple.dport = 0;
 			tuple.sport = icmp6hdr.icmp6_dataun.u_echo.identifier;
 		} else {
