0008a025979766e4be246c57085c87bf42a287c8
Chris Tarazi <chris@isovalent.com>
2021-01-11 16:58:02 -0800

    datapath, cni: Refactor masquerade parameter
    [ upstream commit 269da555ecfb2cd3bd9ea79613eb7a173ebd3164 ]
    
    Previously, we required `masquerade` to be passed in as a parameter in
    multiple places, rather than passing it in once and storing it in
    `RoutingInfo`. This commit refactors the code to do as such.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- plugins/cilium-cni/interface.go
+++ plugins/cilium-cni/interface.go
@@ -51,9 +51,7 @@ func interfaceAdd(ipConfig *current.IPConfig, ipam *models.IPAMAddressResponse, 
 		return fmt.Errorf("unable to parse routing info: %v", err)
 	}
 
-	if err := routingInfo.Configure(ipConfig.Address.IP,
-		int(conf.DeviceMTU),
-		conf.Masquerade); err != nil {
+	if err := routingInfo.Configure(ipConfig.Address.IP, int(conf.DeviceMTU)); err != nil {
 		return fmt.Errorf("unable to install ip rules and routes: %s", err)
 	}
 
