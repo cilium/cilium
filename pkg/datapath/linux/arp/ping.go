122b31e3cb0250c7174c735a56340d76ddd0d4c5
Martynas Pumputis <m@lambda.lt>
2021-01-13 15:53:29 +0100

    arp: Add retries to arping
    [ upstream commit 2284c76d4b528d56cb859836df54b6a8a58cc317 ]
    
    It has been observed that sometimes arping fails with "i/o timeout".
    Further investigation [1] has shown that this happen due to the kernel
    not sending packets. Therefore, to mitigate the issue,try multiple times
    to send the request if the timeout error is encountered.
    
    [1]: https://github.com/cilium/cilium/issues/14125#issuecomment-748499731
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- pkg/datapath/linux/arp/ping.go
+++ pkg/datapath/linux/arp/ping.go
@@ -18,6 +18,7 @@ import (
 	"bytes"
 	"errors"
 	"net"
+	"os"
 	"time"
 
 	"github.com/google/gopacket"
@@ -29,6 +30,7 @@ var (
 	ErrNotImplemented = errors.New("not implemented")
 
 	timeout = 1 * time.Second
+	retries = 3
 )
 
 var defaultSerializeOpts = gopacket.SerializeOptions{
@@ -38,7 +40,7 @@ var defaultSerializeOpts = gopacket.SerializeOptions{
 
 // PingOverLink performs arping request from 'src' IP address to the 'dst' IP address
 // over the link 'link' and returns the hardware address (MAC) of the destination
-func PingOverLink(link netlink.Link, src net.IP, dst net.IP) (net.HardwareAddr, error) {
+func PingOverLink(link netlink.Link, src net.IP, dst net.IP) (hwAddr net.HardwareAddr, err error) {
 	p, err := newPinger(link, src)
 	if err != nil {
 		return nil, err
@@ -49,7 +51,16 @@ func PingOverLink(link netlink.Link, src net.IP, dst net.IP) (net.HardwareAddr, 
 		return nil, err
 	}
 
-	return p.resolve(dst)
+	for i := 0; i < retries; i++ {
+		hwAddr, err = p.resolve(dst)
+		if err == nil {
+			return
+		}
+		if !errors.Is(err, os.ErrDeadlineExceeded) {
+			return
+		}
+	}
+	return
 }
 
 var _ net.Addr = &Addr{}
