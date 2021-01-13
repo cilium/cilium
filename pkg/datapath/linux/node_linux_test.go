612d41fb4cd8bb6c3c781a0721bb4d1e645c5e72
Martynas Pumputis <m@lambda.lt>
2021-01-13 15:53:29 +0100

    arp: Add retries to arping
    It has been observed that sometimes arping fails with "i/o timeout".
    Further investigation [1] has shown that this happen due to the kernel
    not sending packets. Therefore, to mitigate the issue,try multiple times
    to send the request if the timeout error is encountered.
    
    [1]: https://github.com/cilium/cilium/issues/14125#issuecomment-748499731
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- pkg/datapath/linux/node_linux_test.go
+++ pkg/datapath/linux/node_linux_test.go
@@ -21,7 +21,6 @@ import (
 	"net"
 	"runtime"
 	"testing"
-	"time"
 
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/cidr"
@@ -1024,9 +1023,6 @@ func (s *linuxPrivilegedIPv4OnlyTestSuite) TestArpPingHandling(c *check.C) {
 		Name:        "node1",
 		IPAddresses: []nodeTypes.Address{{nodeaddressing.NodeInternalIP, ip1}},
 	}
-	// wait 1 second to give the OS time to setup the routes for the veth pairs
-	// just created.
-	time.Sleep(time.Second)
 	err = linuxNodeHandler.NodeAdd(nodev1)
 	c.Assert(err, check.IsNil)
 
