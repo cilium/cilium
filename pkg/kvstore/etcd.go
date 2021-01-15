cilium/cilium

commit c1b05dfba81a0b3095b165920e685f59c52e76a5

pkg/kvstore: introduced a dedicated session for locks
If a cilium agent was restarted while holding a
distributed TTL in the KVStore it would make that lock
from only being released after the default TTL of the
Cilium agent expired, 15 minutes. This commit introduces
a new session dedicated for locks which will have a TTL
of 25 seconds making the lock to be released after 25
seconds if the cilium agent is restarted.

Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/etcd.go
@@ -229,9 +229,10 @@ type etcdClient struct {
 	config     *client.Config
 	configPath string
 
-	// protects session from concurrent access
+	// protects sessions from concurrent access
 	lock.RWMutex
-	session *concurrency.Session
+	session     *concurrency.Session
+	lockSession *concurrency.Session
 
 	// statusLock protects latestStatusSnapshot and latestErrorStatus for
 	// read/write access
@@ -275,10 +276,18 @@ func (e *etcdMutex) Comparator() interface{} {
 	return e.mutex.IsOwner()
 }
 
-// GetLeaseID returns the current lease ID.
-func (e *etcdClient) GetLeaseID() client.LeaseID {
+// GetSessionLeaseID returns the current lease ID.
+func (e *etcdClient) GetSessionLeaseID() client.LeaseID {
 	e.RWMutex.RLock()
 	l := e.session.Lease()
+	e.RWMutex.RUnlock()
+	return l
+}
+
+// GetLockSessionLeaseID returns the current lease ID for the lock session.
+func (e *etcdClient) GetLockSessionLeaseID() client.LeaseID {
+	e.RWMutex.RLock()
+	l := e.lockSession.Lease()
 	e.RWMutex.RUnlock()
 	return l
 }
@@ -294,6 +303,17 @@ func (e *etcdClient) checkSession(err error, leaseID client.LeaseID) {
 	}
 }
 
+// checkSession verifies if the lease is still valid from the return error of
+// an etcd API call. If the error explicitly states that a lease was not found
+// we mark the session has an orphan for this etcd client. If we would not mark
+// it as an Orphan() the session would be considered expired after the leaseTTL
+// By make it orphan we guarantee the session will be marked to be renewed.
+func (e *etcdClient) checkLockSession(err error, leaseID client.LeaseID) {
+	if err == v3rpcErrors.ErrLeaseNotFound {
+		e.closeLockSession(leaseID)
+	}
+}
+
 // closeSession closes the current session.
 func (e *etcdClient) closeSession(leaseID client.LeaseID) {
 	e.RWMutex.RLock()
@@ -301,6 +321,17 @@ func (e *etcdClient) closeSession(leaseID client.LeaseID) {
 	// session ID to avoid making any other sessions as orphan.
 	if e.session.Lease() == leaseID {
 		e.session.Orphan()
+	}
+	e.RWMutex.RUnlock()
+}
+
+// closeSession closes the current session.
+func (e *etcdClient) closeLockSession(leaseID client.LeaseID) {
+	e.RWMutex.RLock()
+	// only mark a session as orphan if the leaseID is the same as the
+	// session ID to avoid making any other sessions as orphan.
+	if e.lockSession.Lease() == leaseID {
+		e.lockSession.Orphan()
 	}
 	e.RWMutex.RUnlock()
 }
@@ -414,6 +445,29 @@ func (e *etcdClient) renewSession() error {
 	return nil
 }
 
+func (e *etcdClient) renewLockSession() error {
+	<-e.firstSession
+	<-e.lockSession.Done()
+	// This is an attempt to avoid concurrent access of a session that was
+	// already expired. It's not perfect as there is still a period between the
+	// e.lockSession.Done() is closed and the e.Lock() is held where parallel go
+	// routines can get a lease ID of an already expired lease.
+	e.Lock()
+
+	newSession, err := concurrency.NewSession(e.client, concurrency.WithTTL(int(LockLeaseTTL.Seconds())))
+	if err != nil {
+		e.UnlockIgnoreTime()
+		return fmt.Errorf("unable to renew etcd lock session: %s", err)
+	}
+
+	e.lockSession = newSession
+	e.UnlockIgnoreTime()
+
+	e.getLogger().WithField(fieldSession, newSession).Debug("Renewing etcd lock session")
+
+	return nil
+}
+
 func connectEtcdClient(config *client.Config, cfgPath string, errChan chan error, rateLimit int, opts *ExtraOptions) (BackendOperations, error) {
 	if cfgPath != "" {
 		cfg, err := clientyaml.NewConfig(cfgPath)
@@ -438,17 +492,26 @@ func connectEtcdClient(config *client.Config, cfgPath string, errChan chan error
 		"config":    cfgPath,
 	}).Info("Connecting to etcd server...")
 
-	var s concurrency.Session
+	var s, ls concurrency.Session
 	firstSession := make(chan struct{})
-	sessionChan := make(chan *concurrency.Session)
 	errorChan := make(chan error)
 
 	// create session in parallel as this is a blocking operation
 	go func() {
 		session, err := concurrency.NewSession(c, concurrency.WithTTL(int(LeaseTTL.Seconds())))
-		errorChan <- err
-		sessionChan <- session
-		close(sessionChan)
+		if err != nil {
+			errorChan <- err
+			close(errorChan)
+			return
+		}
+		lockSession, err := concurrency.NewSession(c, concurrency.WithTTL(int(LockLeaseTTL.Seconds())))
+		if err != nil {
+			errorChan <- err
+			close(errorChan)
+			return
+		}
+		s = *session
+		ls = *lockSession
 		close(errorChan)
 	}()
 
@@ -457,6 +520,7 @@ func connectEtcdClient(config *client.Config, cfgPath string, errChan chan error
 		config:               config,
 		configPath:           cfgPath,
 		session:              &s,
+		lockSession:          &ls,
 		firstSession:         firstSession,
 		controllers:          controller.NewManager(),
 		latestStatusSnapshot: "No connection to etcd",
@@ -469,27 +533,18 @@ func connectEtcdClient(config *client.Config, cfgPath string, errChan chan error
 	go func() {
 		defer close(errChan)
 
-		var session concurrency.Session
 		select {
 		case err = <-errorChan:
-			if err == nil {
-				session = *<-sessionChan
+			if err != nil {
+				errChan <- err
+				return
 			}
 		case <-time.After(initialConnectionTimeout):
-			err = fmt.Errorf("timed out while waiting for etcd session. Ensure that etcd is running on %s", config.Endpoints)
-		}
-		if err != nil {
-			errChan <- err
+			errChan <- fmt.Errorf("timed out while waiting for etcd session. Ensure that etcd is running on %s", config.Endpoints)
 			return
 		}
 
 		ec.getLogger().Debugf("Session received")
-		s = session
-		if err != nil {
-			c.Close()
-			errChan <- fmt.Errorf("unable to create default lease: %s", err)
-			return
-		}
 		close(ec.firstSession)
 
 		if err := ec.checkMinVersion(); err != nil {
@@ -503,6 +558,15 @@ func connectEtcdClient(config *client.Config, cfgPath string, errChan chan error
 		controller.ControllerParams{
 			DoFunc: func(ctx context.Context) error {
 				return ec.renewSession()
+			},
+			RunInterval: time.Duration(10) * time.Millisecond,
+		},
+	)
+
+	ec.controllers.UpdateController("kvstore-etcd-lock-session-renew",
+		controller.ControllerParams{
+			DoFunc: func(ctx context.Context) error {
+				return ec.renewLockSession()
 			},
 			RunInterval: time.Duration(10) * time.Millisecond,
 		},
@@ -565,13 +629,15 @@ func (e *etcdClient) LockPath(ctx context.Context, path string) (KVLocker, error
 	}
 
 	e.RLock()
-	mu := concurrency.NewMutex(e.session, path)
+	mu := concurrency.NewMutex(e.lockSession, path)
+	leaseID := e.lockSession.Lease()
 	e.RUnlock()
 
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
 	defer cancel()
 	err := mu.Lock(ctx)
 	if err != nil {
+		e.checkLockSession(err, leaseID)
 		return nil, Hint(err)
 	}
 
@@ -926,7 +992,7 @@ func (e *etcdClient) UpdateIfLocked(ctx context.Context, key string, value []byt
 	duration := spanstat.Start()
 	e.limiter.Wait(ctx)
 	if lease {
-		leaseID := e.GetLeaseID()
+		leaseID := e.GetSessionLeaseID()
 		opPut := client.OpPut(key, string(value), client.WithLease(leaseID))
 		cmp := lock.Comparator().(client.Cmp)
 		txnReply, err = e.client.Txn(context.Background()).If(cmp).Then(opPut).Commit()
@@ -953,7 +1019,7 @@ func (e *etcdClient) Update(ctx context.Context, key string, value []byte, lease
 
 	if lease {
 		duration := spanstat.Start()
-		leaseID := e.GetLeaseID()
+		leaseID := e.GetSessionLeaseID()
 		e.limiter.Wait(ctx)
 		_, err := e.client.Put(ctx, key, string(value), client.WithLease(leaseID))
 		e.checkSession(err, leaseID)
@@ -1049,7 +1115,7 @@ func (e *etcdClient) CreateOnlyIfLocked(ctx context.Context, key string, value [
 	duration := spanstat.Start()
 	var leaseID client.LeaseID
 	if lease {
-		leaseID = e.GetLeaseID()
+		leaseID = e.GetSessionLeaseID()
 	}
 	req := e.createOpPut(key, value, leaseID)
 	cnds := []client.Cmp{
@@ -1103,7 +1169,7 @@ func (e *etcdClient) CreateOnly(ctx context.Context, key string, value []byte, l
 	duration := spanstat.Start()
 	var leaseID client.LeaseID
 	if lease {
-		leaseID = e.GetLeaseID()
+		leaseID = e.GetSessionLeaseID()
 	}
 	req := e.createOpPut(key, value, leaseID)
 	cond := client.Compare(client.Version(key), "=", 0)
@@ -1124,7 +1190,7 @@ func (e *etcdClient) CreateIfExists(condKey, key string, value []byte, lease boo
 	duration := spanstat.Start()
 	var leaseID client.LeaseID
 	if lease {
-		leaseID = e.GetLeaseID()
+		leaseID = e.GetSessionLeaseID()
 	}
 	req := e.createOpPut(key, value, leaseID)
 	cond := client.Compare(client.Version(condKey), "!=", 0)
@@ -1223,6 +1289,7 @@ func (e *etcdClient) Close() {
 	}
 	e.RLock()
 	defer e.RUnlock()
+	e.lockSession.Close()
 	e.session.Close()
 	e.client.Close()
 }

pkg/kvstore/client.go
@@ -43,7 +43,7 @@ func initClient(module backendModule, opts *ExtraOptions) error {
 
 	go func() {
 		err, isErr := <-errChan
-		if isErr {
+		if isErr && err != nil {
 			log.WithError(err).Fatalf("Unable to connect to kvstore")
 		}
 		deleteLegacyPrefixes()

pkg/kvstore/keepalive.go
@@ -19,8 +19,11 @@ import (
 )
 
 var (
-	// LeaseTTL is the time-to-live ofthe lease
+	// LeaseTTL is the time-to-live of the lease
 	LeaseTTL = 15 * time.Minute // 15 minutes
+
+	// LockLeaseTTL is the time-to-live of the lease dedicated for locks
+	LockLeaseTTL = 25 * time.Second
 
 	// KeepAliveInterval is the interval in which the lease is being
 	// renewed. This must be set to a value lesser than the LeaseTTL

commit 74cef8d33dcefd014ca2273b2fed017a6cc15471

pkg/kvstore: fix panic when closing etcd connections on error
```
goroutine 319 [running]:
go.etcd.io/etcd/clientv3/concurrency.(*Session).Orphan(...)
        /go/src/github.com/cilium/cilium/vendor/go.etcd.io/etcd/clientv3/concurrency/session.go:90
go.etcd.io/etcd/clientv3/concurrency.(*Session).Close(0xc000dcca50, 0xc00145d790, 0x2)
        /go/src/github.com/cilium/cilium/vendor/go.etcd.io/etcd/clientv3/concurrency/session.go:96 +0x2a
github.com/cilium/cilium/pkg/kvstore.(*etcdClient).Close(0xc0014f2c30)
        /go/src/github.com/cilium/cilium/pkg/kvstore/etcd.go:1652 +0xc8
github.com/cilium/cilium/pkg/clustermesh.(*remoteCluster).restartRemoteConnection.func1(0x2e862e0, 0xc0009fc300, 0x460d7a0, 0x415c26)
        /go/src/github.com/cilium/cilium/pkg/clustermesh/remote_cluster.go:178 +0x408
github.com/cilium/cilium/pkg/controller.(*Controller).runController(0xc0008d2100)
        /go/src/github.com/cilium/cilium/pkg/controller/controller.go:205 +0xa2a
created by github.com/cilium/cilium/pkg/controller.(*Manager).updateController
        /go/src/github.com/cilium/cilium/pkg/controller/manager.go:120 +0xb09
```

Fixes: c1b05dfba81a ("pkg/kvstore: introduced a dedicated session for locks")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/etcd.go
@@ -1654,14 +1654,20 @@ func (e *etcdClient) Close() {
 	}
 	e.RLock()
 	defer e.RUnlock()
-	if err := e.lockSession.Close(); err != nil {
-		e.getLogger().WithError(err).Warning("Failed to revoke lock session while closing etcd client")
+	if e.lockSession != nil {
+		if err := e.lockSession.Close(); err != nil {
+			e.getLogger().WithError(err).Warning("Failed to revoke lock session while closing etcd client")
+		}
 	}
-	if err := e.session.Close(); err != nil {
-		e.getLogger().WithError(err).Warning("Failed to revoke main session while closing etcd client")
+	if e.session != nil {
+		if err := e.session.Close(); err != nil {
+			e.getLogger().WithError(err).Warning("Failed to revoke main session while closing etcd client")
+		}
 	}
-	if err := e.client.Close(); err != nil {
-		e.getLogger().WithError(err).Warning("Failed to close etcd client")
+	if e.client != nil {
+		if err := e.client.Close(); err != nil {
+			e.getLogger().WithError(err).Warning("Failed to close etcd client")
+		}
 	}
 }
 