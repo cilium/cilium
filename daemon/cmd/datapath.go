cilium/cilium

commit 43997f5f04ca5a4ccf2cbaf8372e5c428ab892bd

loader: Wait for node configuration to generate datapath
[ upstream commit d117d50281912460f428a86a6198e2fe014952c8 ]

Previously, a race condition between Kubernetes initialization logic and
the IPAM / address handling / NodeDiscovery logic could cause a crash on
startup in rare circumstances due to the k8s subsystem handling a policy
update event before the initialization was complete.

André pointed out that InitK8sSubsystem() occurs asynchronously to
initialize and begin handling k8s events, and this is initiated prior to
the allocation / initialization of the IP handling subsystem. Looking at
the stacktrace, we see that the "index out of range" error is triggered
from handling a policyAdd event, through the ObjectCache initialization,
through to the headerfile write / nodeconfig fetch logic, through to
logic which would serialize the node IP addressing information into the
node config file, subsequently to be used by other datapath
initialization logic:

    panic: runtime error: index out of range [3] with length 0

    goroutine 145 [running]:
    encoding/binary.bigEndian.Uint32(...)
        /usr/local/go/src/encoding/binary/binary.go:112
    github.com/cilium/cilium/pkg/byteorder.HostSliceToNetwork(...)
        /go/src/github.com/cilium/cilium/pkg/byteorder/byteorder.go:134 +0x1ee
    github.com/cilium/cilium/pkg/datapath/linux/config.(*HeaderfileWriter).WriteNodeConfig(...)
        /go/src/github.com/cilium/cilium/pkg/datapath/linux/config/config.go:108 +0x5baf
    ...
    github.com/cilium/cilium/pkg/datapath/loader.(*Loader).Reinitialize(...)
        /go/src/github.com/cilium/cilium/pkg/datapath/loader/base.go:187 +0x2c0
    github.com/cilium/cilium/daemon/cmd.(*Daemon).policyAdd(...)
        /go/src/github.com/cilium/cilium/daemon/cmd/policy.go:285 +0x1ff7
    github.com/cilium/cilium/daemon/cmd.(*PolicyAddEvent).Handle(...)
        /go/src/github.com/cilium/cilium/daemon/cmd/policy.go:217 +0x5c
    ...

daemon/cmd/datapath.go
@@ -55,6 +55,7 @@ import (
 
 // LocalConfig returns the local configuration of the daemon's nodediscovery.
 func (d *Daemon) LocalConfig() *datapath.LocalNodeConfiguration {
+	<-d.nodeDiscovery.Registered
 	return &d.nodeDiscovery.LocalConfig
 }
 

daemon/cmd/daemon.go
@@ -567,6 +567,9 @@ func NewDaemon(ctx context.Context, epMgr *endpointmanager.EndpointManager, dp d
 		return nil, nil, err
 	}
 
+	// Must occur after d.allocateIPs(), see GH-14245 and its fix.
+	d.nodeDiscovery.StartDiscovery(nodeTypes.GetName())
+
 	// Annotation of the k8s node must happen after discovery of the
 	// PodCIDR range and allocation of the health IPs.
 	if k8s.IsEnabled() && option.Config.AnnotateK8sNode {
@@ -592,8 +595,6 @@ func NewDaemon(ctx context.Context, epMgr *endpointmanager.EndpointManager, dp d
 	} else if !option.Config.AnnotateK8sNode {
 		log.Debug("Annotate k8s node is disabled.")
 	}
-
-	d.nodeDiscovery.StartDiscovery(nodeTypes.GetName())
 
 	// Trigger refresh and update custom resource in the apiserver with all restored endpoints.
 	// Trigger after nodeDiscovery.StartDiscovery to avoid custom resource update conflict.

commit 52c185a2c353a345eefc3e0a90a4f39f703fef35

daemon: Avoid blocking datapath on node discovery
Previously, the datapath relied on node discovery completing. With a
kvstore configured, this meant that node registration would also need to
complete first.

If the kvstore is deployed as pods intended to be managed by Cilium,
then this creates a chicken and egg problem. The kvstore pod cannot come
online because the datapath is blocked. The datapath is blocked because
the agent cannot register the node into the kvstore. And finally, the
node cannot be registered into kvstore because kvstore is not online.

Here's how the issue manifested itself:

```
$ kubectl -n cilium describe pods etcd-operator-59cf4cfb7c-288qx
Name:           etcd-operator-59cf4cfb7c-288qx
Namespace:      cilium
Priority:       0
Node:           gke-chris-form3-cluster-default-pool-3f30c3b5-zd0f/10.138.0.10
Start Time:     Fri, 15 Jan 2021 14:56:24 -0800
Labels:         io.cilium/app=etcd-operator
                pod-template-hash=59cf4cfb7c
...
Events:
  Type     Reason                  Age   From               Message
  ----     ------                  ----  ----               -------
  Normal   Scheduled               83s   default-scheduler  Successfully assigned cilium/etcd-operator-59cf4
  Warning  FailedCreatePodSandBox  22s   kubelet            Failed create pod sandbox: rpc error: code = Unk
5bf622367" network for pod "etcd-operator-59cf4cfb7c-288qx": networkPlugin cni failed to set up pod "etcd-op
ent client after 30.000000 seconds timeout: Get "http:///var/run/cilium/cilium.sock/v1/config": dial unix /v
Is the agent running?
  Normal  SandboxChanged  21s  kubelet  Pod sandbox changed, it will be killed and re-created.
```

To break the chicken and egg problem with the datapath, we need to split
the "node discovery" (`(*NodeDiscovery).StartDiscovery()`) into two
separate phases: (1) local node state is initialized and (2) node
registration which includes syncing to the kvstore if that's configured.
The `(*NodeDiscovery).Registered` channel will close when (2) completes.
The new channel `(*NodeDiscovery).LocalStateInitialized` will close when
(1) completes. This is because the datapath only relies on (1) being
complete and doesn't depend on (2).

This will unblock the datapath from the implicit dependency on the
kvstore and allow the kvstore pods to come online (Cilium generates
endpoints for them), while node registration into the kvstore continues
on in the background.

Fixes: 43997f5f0 ("loader: Wait for node configuration to generate datapath")

daemon/cmd/datapath.go
@@ -55,7 +55,7 @@ import (
 
 // LocalConfig returns the local configuration of the daemon's nodediscovery.
 func (d *Daemon) LocalConfig() *datapath.LocalNodeConfiguration {
-	<-d.nodeDiscovery.Registered
+	<-d.nodeDiscovery.LocalStateInitialized
 	return &d.nodeDiscovery.LocalConfig
 }
 

pkg/nodediscovery/nodediscovery.go
@@ -60,12 +60,13 @@ var log = logging.DefaultLogger.WithField(logfields.LogSubsys, nodeDiscoverySubs
 
 // NodeDiscovery represents a node discovery action
 type NodeDiscovery struct {
-	Manager     *nodemanager.Manager
-	LocalConfig datapath.LocalNodeConfiguration
-	Registrar   nodestore.NodeRegistrar
-	LocalNode   nodeTypes.Node
-	Registered  chan struct{}
-	NetConf     *cnitypes.NetConf
+	Manager               *nodemanager.Manager
+	LocalConfig           datapath.LocalNodeConfiguration
+	Registrar             nodestore.NodeRegistrar
+	LocalNode             nodeTypes.Node
+	Registered            chan struct{}
+	LocalStateInitialized chan struct{}
+	NetConf               *cnitypes.NetConf
 }
 
 func enableLocalNodeRoute() bool {
@@ -117,8 +118,9 @@ func NewNodeDiscovery(manager *nodemanager.Manager, mtuConfig mtu.Configuration,
 		LocalNode: nodeTypes.Node{
 			Source: source.Local,
 		},
-		Registered: make(chan struct{}),
-		NetConf:    netConf,
+		Registered:            make(chan struct{}),
+		LocalStateInitialized: make(chan struct{}),
+		NetConf:               netConf,
 	}
 }
 
@@ -164,6 +166,7 @@ func (n *NodeDiscovery) StartDiscovery(nodeName string) {
 	}
 
 	n.Manager.NodeUpdated(n.LocalNode)
+	close(n.LocalStateInitialized)
 
 	go func() {
 		log.WithFields(