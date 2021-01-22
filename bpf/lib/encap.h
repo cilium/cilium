f1b2c36ab92fb0de1f97257a19a423e58e3ce004
Paul Chaignon <paul@cilium.io>
2021-01-22 12:34:51 +0100

    bpf: Do not bypass conntrack if running kube-proxy+hostfw or IPSec
    [ upstream commit c8052a1fab8b24d42f8a14c567500b16945bf989 ]
    
    When IPSec or the host firewall are enabled in tunneling mode, traffic
    from a pod to a remote node is sent through the stack.
    
    This introduces an asymmetry in kube-proxy DNAT and breaks connections
    from a node (or hostns pod) to remote pods via
    externalTrafficPolicy=Local services. The following details the broken
    packet path node1 --VIP--> pod@node2 (VIP is node2IP):
    
    - SYN leaves node1 via native device with  node1IP -> VIP
    - SYN is DNATed on node2 to                node1IP -> podIP
    - SYN is deliverd to lxc device with       node1IP -> podIP
    - SYN+ACK is sent from lxc device with     podIP   -> node1IP
    - SYN+ACK is redirected in BPF directly to cilium_vxlan
    - SYN+ACK arrives on node1 via tunnel with podIP   -> node1IP
    - RST is sent because podIP doesn't match VIP
    
    Because we use redirect() in BPF, the SYN+ACK packet bypasses most of
    the stack (including conntrack) on its way from cilium_vxlan to the lxc
    device. By bypassing conntrack, we also bypass the reverse DNAT.
    
    The bug only occurs for externalTrafficPolicy=Local services because,
    without that annotation, the SYN packet is SNATed when arriving on
    node2. The SYN+ACK is therefore first sent via the stack to node2's IP
    where it is both reverse SNATed and reverse DNATed.
    
    Fixes: https://github.com/cilium/cilium/issues/14674
    Fixes: https://github.com/cilium/cilium/issues/12542
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- bpf/lib/encap.h
+++ bpf/lib/encap.h
@@ -147,9 +147,9 @@ __encap_and_redirect_with_nodeid(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 				 __u32 seclabel, __u32 monitor)
 {
 	int ret = __encap_with_nodeid(ctx, tunnel_endpoint, seclabel, monitor);
-
 	if (ret != 0)
 		return ret;
+
 	return redirect(ENCAP_IFINDEX, 0);
 }
 
@@ -193,7 +193,18 @@ encap_and_redirect_lxc(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 			return encap_and_redirect_ipsec(ctx, tunnel_endpoint,
 							encrypt_key, seclabel);
 #endif
+#if !defined(ENABLE_NODEPORT) && (defined(ENABLE_IPSEC) || defined(ENABLE_HOST_FIREWALL))
+		/* For IPSec and the host firewall, traffic from a pod to a remote node
+		 * is sent through the tunnel. In the case of node --> VIP@remote pod,
+		 * packets may be DNATed when they enter the remote node. If kube-proxy
+		 * is used, the response needs to go through the stack on the way to
+		 * the tunnel, to apply the correct reverse DNAT.
+		 * See #14674 for details.
+		 */
+		return __encap_with_nodeid(ctx, tunnel_endpoint, seclabel, monitor);
+#else
 		return __encap_and_redirect_with_nodeid(ctx, tunnel_endpoint, seclabel, monitor);
+#endif /* !ENABLE_NODEPORT && (ENABLE_IPSEC || ENABLE_HOST_FIREWALL) */
 	}
 
 	tunnel = map_lookup_elem(&TUNNEL_MAP, key);
