202f4eb86af453998d2aa0abf2b76c891d8cc24f
Joe Stringer <joe@cilium.io>
2021-01-28 10:42:45 -0800

    release: Fix script to check presence of docker images
    When this script was backported, differences in the v1.7 branch were not
    taken into account, so it was just broken. Fix up the make target and
    update the images to fetch, matching the ones v1.7 uses. Notably, there
    is no platform-specific operator images, no hubble and no
    clustermesh-apiserver (or related dependencies like etcd).
    
    Fixes: 5273db146877 ("release: add script to check presence of docker images")
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- contrib/release/check-docker-images.sh
+++ contrib/release/check-docker-images.sh
@@ -4,36 +4,35 @@ cilium_tag="${1}"
 org="cilium"
 
 external_dependencies_docker=(
-  "envoyproxy/envoy:${HUBBLE_PROXY_VERSION}" \
 )
 
 external_dependencies_quay=(
-  "coreos/etcd:${ETCD_VERSION}" \
+)
+
+external_dependencies_gcr=(
+  "google-containers/startup-script:${NODEINIT_VERSION}"
 )
 
 internal_dependencies=(
-  "certgen:${CERTGEN_VERSION}" \
   "cilium-etcd-operator:${MANAGED_ETCD_VERSION}" \
-  "startup-script:${NODEINIT_VERSION}"
-  "hubble-ui:${HUBBLE_UI_VERSION}" \
-  "hubble-ui-backend:${HUBBLE_UI_VERSION}" \
 )
 
 cilium_images=(\
   "cilium" \
-  "clustermesh-apiserver" \
   "docker-plugin" \
-  "hubble-relay" \
   "operator" \
-  "operator-azure" \
-  "operator-aws" \
-  "operator-generic" \
 )
 
 docker_tag_exists(){
   local repo="${1}"
   local tag="${2}"
   curl --silent -f -lSL "https://index.docker.io/v1/repositories/${repo}/tags/${tag}" &> /dev/null
+}
+
+gcr_tag_exists(){
+  local repo="${1}"
+  local tag="${2}"
+  curl --silent -f -lSL "https://gcr.io/api/v1/repository/${repo}/tag/${tag}/images" &> /dev/null
 }
 
 quay_tag_exists(){
@@ -47,6 +46,15 @@ for image in "${external_dependencies_docker[@]}" ; do
   image_name=${image%":$image_tag"}
   if ! docker_tag_exists "${image_name}" "${image_tag}" ; then
     echo "docker.io/${image} does not exist!"
+    not_found=true
+  fi
+done
+
+for image in "${external_dependencies_gcr[@]}" ; do
+  image_tag=${image#*:}
+  image_name=${image%":$image_tag"}
+  if ! gcr_tag_exists "${image_name}" "${image_tag}" ; then
+    echo "gcr.io/${image} does not exist!"
     not_found=true
   fi
 done
