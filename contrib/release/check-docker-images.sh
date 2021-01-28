cilium/cilium

commit 0c37855c94ba96063376d1ceb7f483d0fb747eaa

release: add script to check presence of docker images
[ upstream commit 73be2c15ced2d54aecdfa462b38d2aac2e6f631f ]

To check if images are published across all repositories the
`check-docker-images.sh` script will be able to perform this check of a
particular release.

Signed-off-by: André Martins <andre@cilium.io>
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

contrib/release/check-docker-images.sh
@@ -1,0 +1,92 @@
+#!/usr/bin/env bash
+
+cilium_tag="${1}"
+org="cilium"
+
+external_dependencies_docker=(
+  "envoyproxy/envoy:${HUBBLE_PROXY_VERSION}" \
+)
+
+external_dependencies_quay=(
+  "coreos/etcd:${ETCD_VERSION}" \
+)
+
+internal_dependencies=(
+  "certgen:${CERTGEN_VERSION}" \
+  "cilium-etcd-operator:${MANAGED_ETCD_VERSION}" \
+  "startup-script:${NODEINIT_VERSION}"
+  "hubble-ui:${HUBBLE_UI_VERSION}" \
+  "hubble-ui-backend:${HUBBLE_UI_VERSION}" \
+)
+
+cilium_images=(\
+  "cilium" \
+  "clustermesh-apiserver" \
+  "docker-plugin" \
+  "hubble-relay" \
+  "operator" \
+  "operator-azure" \
+  "operator-aws" \
+  "operator-generic" \
+)
+
+docker_tag_exists(){
+  local repo="${1}"
+  local tag="${2}"
+  curl --silent -f -lSL "https://index.docker.io/v1/repositories/${repo}/tags/${tag}" &> /dev/null
+}
+
+quay_tag_exists(){
+  local repo="${1}"
+  local tag="${2}"
+  curl --silent -f -lSL "https://quay.io/api/v1/repository/${repo}/tag/${tag}/images" &> /dev/null
+}
+
+for image in "${external_dependencies_docker[@]}" ; do
+  image_tag=${image#*:}
+  image_name=${image%":$image_tag"}
+  if ! docker_tag_exists "${image_name}" "${image_tag}" ; then
+    echo "docker.io/${image} does not exist!"
+    not_found=true
+  fi
+done
+
+for image in "${external_dependencies_quay[@]}" ; do
+  image_tag=${image#*:}
+  image_name=${image%":$image_tag"}
+  if ! quay_tag_exists "${image_name}" "${image_tag}" ; then
+    echo "quay.io/${image} does not exist!"
+    not_found=true
+  fi
+done
+
+for image in "${internal_dependencies[@]}" ; do
+  image_tag=${image#*:}
+  image_name=${org}/${image%":$image_tag"}
+  if ! docker_tag_exists "${image_name}" "${image_tag}" ; then
+    echo "docker.io/${image_name}:${image_tag} does not exist!"
+    not_found=true
+  fi
+  if ! quay_tag_exists "${image_name}" "${image_tag}" ; then
+    echo "quay.io/${image_name}:${image_tag} does not exist!"
+    not_found=true
+  fi
+done
+
+for image in "${cilium_images[@]}"; do
+  image_name="${org}/${image}"
+  if ! docker_tag_exists "${image_name}" "${cilium_tag}" ; then
+    echo "docker.io/${image_name}:${cilium_tag} does not exist!"
+    not_found=true
+  fi
+  if ! quay_tag_exists "${image_name}" "${cilium_tag}" ; then
+    echo "quay.io/${image_name}:${cilium_tag} does not exist!"
+    not_found=true
+  fi
+done
+
+if [[ -n "${not_found}" ]]; then
+  exit 1
+fi
+
+exit 0

install/kubernetes/Makefile
@@ -55,4 +55,17 @@ update-versions:
 clean:
 	$(RM) $(QUICK_INSTALL) $(EXPERIMENTAL_INSTALL)
 
-.phony: all clean update-versions
+docs:
+	$(QUIET)$(HELM_DOCS)
+
+check-docker-images:
+	$(QUIET)\
+         HUBBLE_PROXY_VERSION=$(HUBBLE_PROXY_VERSION) \
+         HUBBLE_UI_VERSION=$(HUBBLE_UI_VERSION) \
+         MANAGED_ETCD_VERSION=$(MANAGED_ETCD_VERSION) \
+         ETCD_VERSION=$(ETCD_VERSION) \
+         NODEINIT_VERSION=$(NODEINIT_VERSION) \
+         CERTGEN_VERSION=`echo $(CERTGEN_VERSION) | egrep -o '^.*@' | sed 's/@//'` \
+         ../../contrib/release/check-docker-images.sh "v$(VERSION)"
+
+.PHONY: all check-docker-images clean docs experimental-install lint quick-install update-versions

commit be421ca2ee53e373ab1209b1b6be585d544db1c2

release: Fix script to check presence of docker images
When this script was backported, differences in the v1.8 branch were not
taken into account, so it was just broken. Fix up the make target and
update the images to fetch, matching the ones v1.8 uses. Notably, there
is no hubble UI and no clustermesh-apiserver (or related dependencies like
etcd).

Fixes: 0c37855c94ba ("release: add script to check presence of docker images")
Signed-off-by: Joe Stringer <joe@cilium.io>

contrib/release/check-docker-images.sh
@@ -4,24 +4,19 @@ cilium_tag="${1}"
 org="cilium"
 
 external_dependencies_docker=(
-  "envoyproxy/envoy:${HUBBLE_PROXY_VERSION}" \
 )
 
 external_dependencies_quay=(
-  "coreos/etcd:${ETCD_VERSION}" \
 )
 
 internal_dependencies=(
-  "certgen:${CERTGEN_VERSION}" \
   "cilium-etcd-operator:${MANAGED_ETCD_VERSION}" \
   "startup-script:${NODEINIT_VERSION}"
   "hubble-ui:${HUBBLE_UI_VERSION}" \
-  "hubble-ui-backend:${HUBBLE_UI_VERSION}" \
 )
 
 cilium_images=(\
   "cilium" \
-  "clustermesh-apiserver" \
   "docker-plugin" \
   "hubble-relay" \
   "operator" \

install/kubernetes/Makefile
@@ -4,6 +4,8 @@
 include ../../Makefile.defs
 
 MANAGED_ETCD_VERSION := "v2.0.7"
+NODEINIT_VERSION := "62bfbe88c17778aad7bef9fa57ff9e2d4a9ba0d8"
+HUBBLE_UI_VERSION := "v0.6.1"
 
 QUICK_INSTALL := "$(ROOT_DIR)/$(RELATIVE_DIR)/quick-install.yaml"
 EXPERIMENTAL_INSTALL := "$(ROOT_DIR)/$(RELATIVE_DIR)/experimental-install.yaml"
@@ -60,12 +62,9 @@ docs:
 
 check-docker-images:
 	$(QUIET)\
-         HUBBLE_PROXY_VERSION=$(HUBBLE_PROXY_VERSION) \
-         HUBBLE_UI_VERSION=$(HUBBLE_UI_VERSION) \
-         MANAGED_ETCD_VERSION=$(MANAGED_ETCD_VERSION) \
-         ETCD_VERSION=$(ETCD_VERSION) \
-         NODEINIT_VERSION=$(NODEINIT_VERSION) \
-         CERTGEN_VERSION=`echo $(CERTGEN_VERSION) | egrep -o '^.*@' | sed 's/@//'` \
-         ../../contrib/release/check-docker-images.sh "v$(VERSION)"
+		HUBBLE_UI_VERSION=$(HUBBLE_UI_VERSION) \
+		MANAGED_ETCD_VERSION=$(MANAGED_ETCD_VERSION) \
+		NODEINIT_VERSION=$(NODEINIT_VERSION) \
+		$(ROOT_DIR)/contrib/release/check-docker-images.sh "v$(VERSION)"
 
 .PHONY: all check-docker-images clean docs experimental-install lint quick-install update-versions