3ce38c9788ba7769030523c410cbc1277ed5d257
Aditi Ghag <aditi@cilium.io>
2021-02-09 21:45:09 -0800

    ipam/node: Introduce ENI info for cilium_host
    This is a preparatory commit that introduces ENI related
    information for cilium_host (aka router). Specifically,
    the information includes primary ENI interface that the
    secondary cilium_host IP is associated with, primary ENI MAC address
    and VPC CIDRs.
    Also, refactor the current IPAM internal IP allocating methods
    so that the ENI information can be retrieved for cilium_host (aka router).
    
    This information can be useful for setting up encryption
    related state such as ip rules and interface to attach
    BPF programs to.
    
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

--- daemon/cmd/state.go
+++ daemon/cmd/state.go
@@ -343,7 +343,7 @@ func (d *Daemon) allocateIPsLocked(ep *endpoint.Endpoint) error {
 	var err error
 
 	if option.Config.EnableIPv6 && ep.IPv6 != nil {
-		err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv6.IP(), ep.HumanStringLocked()+" [restored]")
+		_, err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv6.IP(), ep.HumanStringLocked()+" [restored]")
 		if err != nil {
 			return fmt.Errorf("unable to reallocate %s IPv6 address: %s", ep.IPv6.IP(), err)
 		}
@@ -356,7 +356,7 @@ func (d *Daemon) allocateIPsLocked(ep *endpoint.Endpoint) error {
 	}
 
 	if option.Config.EnableIPv4 && ep.IPv4 != nil {
-		if err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv4.IP(), ep.HumanStringLocked()+" [restored]"); err != nil {
+		if _, err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv4.IP(), ep.HumanStringLocked()+" [restored]"); err != nil {
 			return fmt.Errorf("unable to reallocate %s IPv4 address: %s", ep.IPv4.IP(), err)
 		}
 	}
