60c1bd73fc4a2f5ba8fe37750918917de2147df1
André Martins <andre@cilium.io>
2021-02-16 20:02:15 +0100

    image/runtime: update Dockerfile from contrib/package/docker
    Signed-off-by: André Martins <andre@cilium.io>

--- images/scripts/update-cilium-runtime-image.sh
+++ images/scripts/update-cilium-runtime-image.sh
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -o xtrace
+set -o errexit
 set -o pipefail
 set -o nounset
 
@@ -13,7 +14,7 @@ root_dir="$(git rev-parse --show-toplevel)"
 
 cd "${root_dir}"
 
-image="docker.io/cilium/cilium-runtime-dev"
+image="quay.io/cilium/cilium-runtime"
 
 image_tag="$(WITHOUT_SUFFIX=1 "${script_dir}/make-image-tag.sh" images/runtime)"
 
@@ -21,7 +22,16 @@ image_tag="$(WITHOUT_SUFFIX=1 "${script_dir}/make-image-tag.sh" images/runtime)"
 used_by=($(git grep -l CILIUM_RUNTIME_IMAGE= images/*/Dockerfile))
 
 for i in "${used_by[@]}" ; do
-  sed "s|\(CILIUM_RUNTIME_IMAGE=\)${image}:.*\$|\1${image}:${image_tag}|" "${i}" > "${i}.sedtmp" && mv "${i}.sedtmp" "${i}"
+  image_full="${image}:${image_tag}"
+  # Detect if the image_tag already exists, if it does then we can assume the
+  # image was created and a sha256 is available for it.
+  if grep "CILIUM_RUNTIME_IMAGE=${image}:${image_tag}" "${i}" ; then
+    sha256=$("${script_dir}/get-image-digest.sh" "${image_full}")
+    if [ -n "${sha256}" ]; then
+      image_full="${image_full}@${sha256}"
+    fi
+  fi
+  sed "s|\(CILIUM_RUNTIME_IMAGE=\)${image}:.*\$|\1${image_full}|" "${i}" > "${i}.sedtmp" && mv "${i}.sedtmp" "${i}"
 done
 
 do_check="${CHECK:-false}"
