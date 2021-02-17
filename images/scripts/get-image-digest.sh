d58dc2bbc311868ccfc21f96434edae163c0a447
André Martins <andre@cilium.io>
2021-02-17 16:02:41 +0100

    re-write get-image-digest script
    Signed-off-by: André Martins <andre@cilium.io>

--- images/scripts/get-image-digest.sh
+++ images/scripts/get-image-digest.sh
@@ -7,17 +7,9 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-MAKER_IMAGE="${MAKER_IMAGE:-docker.io/cilium/image-maker:3e2ea4f151593908c362307a1de22e68610d955c}"
-
 if [ "$#" -ne 1 ] ; then
   echo "$0 supports exactly 1 argument"
   exit 1
 fi
 
-root_dir="$(git rev-parse --show-toplevel)"
-
-if [ -z "${MAKER_CONTAINER+x}" ] ; then
-   exec docker run --env DOCKER_HUB_PUBLIC_ACCESS_ONLY=true --env QUAY_PUBLIC_ACCESS_ONLY=true --rm --volume "${root_dir}:/src" --workdir /src "${MAKER_IMAGE}" "/src/images/scripts/$(basename "${0}")" "${1}"
-fi
-
-crane digest "${1}" 2> /dev/null
+docker buildx imagetools inspect "${1}" 2>/dev/null | grep Digest | awk '{ print $2 }' || true
