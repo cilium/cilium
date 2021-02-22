034fe4e803fb142ad379e2326cff542b796bb40a
André Martins <andre@cilium.io>
2021-02-22 14:51:26 +0100

    k8s: update k8s libraries to 1.20.4
    Signed-off-by: André Martins <andre@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -309,7 +309,7 @@ case $K8S_VERSION in
         sudo apt-get install -y conntrack
         KUBERNETES_CNI_VERSION="0.8.7"
         KUBERNETES_CNI_OS="-linux"
-        K8S_FULL_VERSION="1.20.3"
+        K8S_FULL_VERSION="1.20.4"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri,swap"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification,swap"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
