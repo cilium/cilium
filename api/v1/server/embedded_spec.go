cilium/cilium

commit f462603d515a0a89ba57b3a21362302758adbb9a

cli: Add LB IP to cilium status
This commit extends cilium status to include NodePort BPF IP addr (LB
IP) for each device which has bpf_host.o attached (the program is
doing N-S loadbalancing).

The example output:

$ cilium status
[..]
KubeProxyReplacement:   Strict   [wlp3s0 172.20.10.2 (Direct Routing)]

Signed-off-by: Martynas Pumputis <m@lambda.lt>

api/v1/models/kube_proxy_replacement.go
@@ -10,6 +10,7 @@ package models
 
 import (
 	"encoding/json"
+	"strconv"
 
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/strfmt"
@@ -24,8 +25,10 @@ import (
 // swagger:model KubeProxyReplacement
 type KubeProxyReplacement struct {
 
-	// devices
-	Devices []string `json:"devices"`
+	//
+	//
+	// +k8s:deepcopy-gen=true
+	Devices []*KubeProxyReplacementDevicesItems0 `json:"devices"`
 
 	// direct routing device
 	DirectRoutingDevice string `json:"directRoutingDevice,omitempty"`
@@ -42,6 +45,10 @@ type KubeProxyReplacement struct {
 func (m *KubeProxyReplacement) Validate(formats strfmt.Registry) error {
 	var res []error
 
+	if err := m.validateDevices(formats); err != nil {
+		res = append(res, err)
+	}
+
 	if err := m.validateFeatures(formats); err != nil {
 		res = append(res, err)
 	}
@@ -53,6 +60,31 @@ func (m *KubeProxyReplacement) Validate(formats strfmt.Registry) error {
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
+	return nil
+}
+
+func (m *KubeProxyReplacement) validateDevices(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Devices) { // not required
+		return nil
+	}
+
+	for i := 0; i < len(m.Devices); i++ {
+		if swag.IsZero(m.Devices[i]) { // not required
+			continue
+		}
+
+		if m.Devices[i] != nil {
+			if err := m.Devices[i].Validate(formats); err != nil {
+				if ve, ok := err.(*errors.Validation); ok {
+					return ve.ValidateName("devices" + "." + strconv.Itoa(i))
+				}
+				return err
+			}
+		}
+
+	}
+
 	return nil
 }
 
@@ -134,6 +166,45 @@ func (m *KubeProxyReplacement) MarshalBinary() ([]byte, error) {
 // UnmarshalBinary interface implementation
 func (m *KubeProxyReplacement) UnmarshalBinary(b []byte) error {
 	var res KubeProxyReplacement
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}
+
+// KubeProxyReplacementDevicesItems0
+//
+// +k8s:deepcopy-gen=true
+//
+// swagger:model KubeProxyReplacementDevicesItems0
+type KubeProxyReplacementDevicesItems0 struct {
+
+	//
+	//
+	// +k8s:deepcopy-gen=true
+	IP []string `json:"ip"`
+
+	// name
+	Name string `json:"name,omitempty"`
+}
+
+// Validate validates this kube proxy replacement devices items0
+func (m *KubeProxyReplacementDevicesItems0) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *KubeProxyReplacementDevicesItems0) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *KubeProxyReplacementDevicesItems0) UnmarshalBinary(b []byte) error {
+	var res KubeProxyReplacementDevicesItems0
 	if err := swag.ReadJSON(b, &res); err != nil {
 		return err
 	}

api/v1/models/zz_generated.deepcopy.go
@@ -410,8 +410,14 @@ func (in *KubeProxyReplacement) DeepCopyInto(out *KubeProxyReplacement) {
 	*out = *in
 	if in.Devices != nil {
 		in, out := &in.Devices, &out.Devices
-		*out = make([]string, len(*in))
-		copy(*out, *in)
+		*out = make([]*KubeProxyReplacementDevicesItems0, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(KubeProxyReplacementDevicesItems0)
+				(*in).DeepCopyInto(*out)
+			}
+		}
 	}
 	if in.Features != nil {
 		in, out := &in.Features, &out.Features
@@ -427,6 +433,27 @@ func (in *KubeProxyReplacement) DeepCopy() *KubeProxyReplacement {
 		return nil
 	}
 	out := new(KubeProxyReplacement)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KubeProxyReplacementDevicesItems0) DeepCopyInto(out *KubeProxyReplacementDevicesItems0) {
+	*out = *in
+	if in.IP != nil {
+		in, out := &in.IP, &out.IP
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KubeProxyReplacementDevicesItems0.
+func (in *KubeProxyReplacementDevicesItems0) DeepCopy() *KubeProxyReplacementDevicesItems0 {
+	if in == nil {
+		return nil
+	}
+	out := new(KubeProxyReplacementDevicesItems0)
 	in.DeepCopyInto(out)
 	return out
 }

api/v1/openapi.yaml
@@ -1754,9 +1754,28 @@ definitions:
         - Probe
         - Partial
       devices:
-          type: array
-          items:
-            type: string
+        description: |-
+
+
+          +k8s:deepcopy-gen=true
+        type: array
+        items:
+          description: |-
+
+
+            +k8s:deepcopy-gen=true
+          type: object
+          properties:
+            name:
+              type: string
+            ip:
+              description: |-
+
+
+                +k8s:deepcopy-gen=true
+              type: array
+              items:
+                type: string
       directRoutingDevice:
         type: string
       features:

daemon/cmd/status.go
@@ -41,6 +41,7 @@ import (
 	"github.com/cilium/cilium/pkg/maps/signalmap"
 	"github.com/cilium/cilium/pkg/maps/sockmap"
 	tunnelmap "github.com/cilium/cilium/pkg/maps/tunnel"
+	"github.com/cilium/cilium/pkg/node"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/rand"
@@ -199,9 +200,21 @@ func (d *Daemon) getKubeProxyReplacementStatus() *models.KubeProxyReplacement {
 		mode = models.KubeProxyReplacementModeDisabled
 	}
 
-	devices := make([]string, len(option.Config.Devices))
+	devices := make([]*models.KubeProxyReplacementDevicesItems0, len(option.Config.Devices))
+	v4Addrs := node.GetNodePortIPv4AddrsWithDevices()
+	v6Addrs := node.GetNodePortIPv6AddrsWithDevices()
 	for i, iface := range option.Config.Devices {
-		devices[i] = iface
+		info := &models.KubeProxyReplacementDevicesItems0{
+			Name: iface,
+			IP:   make([]string, 0),
+		}
+		if addr, ok := v4Addrs[iface]; ok {
+			info.IP = append(info.IP, addr.String())
+		}
+		if addr, ok := v6Addrs[iface]; ok {
+			info.IP = append(info.IP, addr.String())
+		}
+		devices[i] = info
 	}
 
 	features := &models.KubeProxyReplacementFeatures{

pkg/client/client.go
@@ -302,8 +302,8 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 		devices := ""
 		if sr.KubeProxyReplacement.Mode != models.KubeProxyReplacementModeDisabled {
 			for i, dev := range sr.KubeProxyReplacement.Devices {
-				kubeProxyDevices += dev
-				if dev == sr.KubeProxyReplacement.DirectRoutingDevice {
+				kubeProxyDevices += fmt.Sprintf("%s %s", dev.Name, strings.Join(dev.IP, " "))
+				if dev.Name == sr.KubeProxyReplacement.DirectRoutingDevice {
 					kubeProxyDevices += " (Direct Routing)"
 				}
 				if i+1 != len(sr.KubeProxyReplacement.Devices) {
@@ -420,8 +420,15 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 				if sr.KubeProxyReplacement != nil {
 					// When BPF Masquerading is enabled we don't do any masquerading for IPv6
 					// traffic so no SNAT Exclusion IPv6 CIDR is listed in status output.
+					devStr := ""
+					for i, dev := range sr.KubeProxyReplacement.Devices {
+						devStr += dev.Name
+						if i+1 != len(sr.KubeProxyReplacement.Devices) {
+							devStr += ", "
+						}
+					}
 					status += fmt.Sprintf("\t[%s]\t%s",
-						strings.Join(sr.KubeProxyReplacement.Devices, ", "),
+						devStr,
 						sr.Masquerading.SnatExclusionCidrV4)
 				}
 

api/v1/server/embedded_spec.go
@@ -2663,9 +2663,23 @@ func init() {
       "type": "object",
       "properties": {
         "devices": {
+          "description": "\n\n+k8s:deepcopy-gen=true",
           "type": "array",
           "items": {
-            "type": "string"
+            "description": "\n\n+k8s:deepcopy-gen=true",
+            "type": "object",
+            "properties": {
+              "ip": {
+                "description": "\n\n+k8s:deepcopy-gen=true",
+                "type": "array",
+                "items": {
+                  "type": "string"
+                }
+              },
+              "name": {
+                "type": "string"
+              }
+            }
           }
         },
         "directRoutingDevice": {
@@ -6635,9 +6649,10 @@ func init() {
       "type": "object",
       "properties": {
         "devices": {
+          "description": "\n\n+k8s:deepcopy-gen=true",
           "type": "array",
           "items": {
-            "type": "string"
+            "$ref": "#/definitions/KubeProxyReplacementDevicesItems0"
           }
         },
         "directRoutingDevice": {
@@ -6740,6 +6755,22 @@ func init() {
             "Probe",
             "Partial"
           ]
+        }
+      }
+    },
+    "KubeProxyReplacementDevicesItems0": {
+      "description": "\n\n+k8s:deepcopy-gen=true",
+      "type": "object",
+      "properties": {
+        "ip": {
+          "description": "\n\n+k8s:deepcopy-gen=true",
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "name": {
+          "type": "string"
         }
       }
     },

commit 5c85e2ca19d4dd640c8080ea391222d54399a883

api: Fix status compatibility: KPR devices
This partially reverts f462603d51 ("cli: Add LB IP to cilium status") to
re-estasblish API compatibility of the status call so it will work
correctly when a <= 1.9 client queries the status API.

Fixes: f462603d51 ("cli: Add LB IP to cilium status")
Signed-off-by: Thomas Graf <thomas@cilium.io>

api/v1/models/kube_proxy_replacement.go
@@ -28,7 +28,10 @@ type KubeProxyReplacement struct {
 	//
 	//
 	// +k8s:deepcopy-gen=true
-	Devices []*KubeProxyReplacementDevicesItems0 `json:"devices"`
+	DeviceList []*KubeProxyReplacementDeviceListItems0 `json:"deviceList"`
+
+	// devices
+	Devices []string `json:"devices"`
 
 	// direct routing device
 	DirectRoutingDevice string `json:"directRoutingDevice,omitempty"`
@@ -45,7 +48,7 @@ type KubeProxyReplacement struct {
 func (m *KubeProxyReplacement) Validate(formats strfmt.Registry) error {
 	var res []error
 
-	if err := m.validateDevices(formats); err != nil {
+	if err := m.validateDeviceList(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -63,21 +66,21 @@ func (m *KubeProxyReplacement) Validate(formats strfmt.Registry) error {
 	return nil
 }
 
-func (m *KubeProxyReplacement) validateDevices(formats strfmt.Registry) error {
+func (m *KubeProxyReplacement) validateDeviceList(formats strfmt.Registry) error {
 
-	if swag.IsZero(m.Devices) { // not required
+	if swag.IsZero(m.DeviceList) { // not required
 		return nil
 	}
 
-	for i := 0; i < len(m.Devices); i++ {
-		if swag.IsZero(m.Devices[i]) { // not required
+	for i := 0; i < len(m.DeviceList); i++ {
+		if swag.IsZero(m.DeviceList[i]) { // not required
 			continue
 		}
 
-		if m.Devices[i] != nil {
-			if err := m.Devices[i].Validate(formats); err != nil {
+		if m.DeviceList[i] != nil {
+			if err := m.DeviceList[i].Validate(formats); err != nil {
 				if ve, ok := err.(*errors.Validation); ok {
-					return ve.ValidateName("devices" + "." + strconv.Itoa(i))
+					return ve.ValidateName("deviceList" + "." + strconv.Itoa(i))
 				}
 				return err
 			}
@@ -173,12 +176,12 @@ func (m *KubeProxyReplacement) UnmarshalBinary(b []byte) error {
 	return nil
 }
 
-// KubeProxyReplacementDevicesItems0
+// KubeProxyReplacementDeviceListItems0
 //
 // +k8s:deepcopy-gen=true
 //
-// swagger:model KubeProxyReplacementDevicesItems0
-type KubeProxyReplacementDevicesItems0 struct {
+// swagger:model KubeProxyReplacementDeviceListItems0
+type KubeProxyReplacementDeviceListItems0 struct {
 
 	//
 	//
@@ -189,13 +192,13 @@ type KubeProxyReplacementDevicesItems0 struct {
 	Name string `json:"name,omitempty"`
 }
 
-// Validate validates this kube proxy replacement devices items0
-func (m *KubeProxyReplacementDevicesItems0) Validate(formats strfmt.Registry) error {
+// Validate validates this kube proxy replacement device list items0
+func (m *KubeProxyReplacementDeviceListItems0) Validate(formats strfmt.Registry) error {
 	return nil
 }
 
 // MarshalBinary interface implementation
-func (m *KubeProxyReplacementDevicesItems0) MarshalBinary() ([]byte, error) {
+func (m *KubeProxyReplacementDeviceListItems0) MarshalBinary() ([]byte, error) {
 	if m == nil {
 		return nil, nil
 	}
@@ -203,8 +206,8 @@ func (m *KubeProxyReplacementDevicesItems0) MarshalBinary() ([]byte, error) {
 }
 
 // UnmarshalBinary interface implementation
-func (m *KubeProxyReplacementDevicesItems0) UnmarshalBinary(b []byte) error {
-	var res KubeProxyReplacementDevicesItems0
+func (m *KubeProxyReplacementDeviceListItems0) UnmarshalBinary(b []byte) error {
+	var res KubeProxyReplacementDeviceListItems0
 	if err := swag.ReadJSON(b, &res); err != nil {
 		return err
 	}

api/v1/models/zz_generated.deepcopy.go
@@ -408,16 +408,21 @@ func (in *K8sStatus) DeepCopy() *K8sStatus {
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *KubeProxyReplacement) DeepCopyInto(out *KubeProxyReplacement) {
 	*out = *in
-	if in.Devices != nil {
-		in, out := &in.Devices, &out.Devices
-		*out = make([]*KubeProxyReplacementDevicesItems0, len(*in))
+	if in.DeviceList != nil {
+		in, out := &in.DeviceList, &out.DeviceList
+		*out = make([]*KubeProxyReplacementDeviceListItems0, len(*in))
 		for i := range *in {
 			if (*in)[i] != nil {
 				in, out := &(*in)[i], &(*out)[i]
-				*out = new(KubeProxyReplacementDevicesItems0)
+				*out = new(KubeProxyReplacementDeviceListItems0)
 				(*in).DeepCopyInto(*out)
 			}
 		}
+	}
+	if in.Devices != nil {
+		in, out := &in.Devices, &out.Devices
+		*out = make([]string, len(*in))
+		copy(*out, *in)
 	}
 	if in.Features != nil {
 		in, out := &in.Features, &out.Features
@@ -438,7 +443,7 @@ func (in *KubeProxyReplacement) DeepCopy() *KubeProxyReplacement {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
-func (in *KubeProxyReplacementDevicesItems0) DeepCopyInto(out *KubeProxyReplacementDevicesItems0) {
+func (in *KubeProxyReplacementDeviceListItems0) DeepCopyInto(out *KubeProxyReplacementDeviceListItems0) {
 	*out = *in
 	if in.IP != nil {
 		in, out := &in.IP, &out.IP
@@ -448,12 +453,12 @@ func (in *KubeProxyReplacementDevicesItems0) DeepCopyInto(out *KubeProxyReplacem
 	return
 }
 
-// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KubeProxyReplacementDevicesItems0.
-func (in *KubeProxyReplacementDevicesItems0) DeepCopy() *KubeProxyReplacementDevicesItems0 {
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KubeProxyReplacementDeviceListItems0.
+func (in *KubeProxyReplacementDeviceListItems0) DeepCopy() *KubeProxyReplacementDeviceListItems0 {
 	if in == nil {
 		return nil
 	}
-	out := new(KubeProxyReplacementDevicesItems0)
+	out := new(KubeProxyReplacementDeviceListItems0)
 	in.DeepCopyInto(out)
 	return out
 }

api/v1/openapi.yaml
@@ -1758,6 +1758,10 @@ definitions:
         - Probe
         - Partial
       devices:
+        type: array
+        items:
+          type: string
+      deviceList:
         description: |-
 
 

daemon/cmd/status.go
@@ -201,11 +201,13 @@ func (d *Daemon) getKubeProxyReplacementStatus() *models.KubeProxyReplacement {
 		mode = models.KubeProxyReplacementModeDisabled
 	}
 
-	devices := make([]*models.KubeProxyReplacementDevicesItems0, len(option.Config.Devices))
+	devicesLegacy := make([]string, len(option.Config.Devices))
+	devices := make([]*models.KubeProxyReplacementDeviceListItems0, len(option.Config.Devices))
 	v4Addrs := node.GetNodePortIPv4AddrsWithDevices()
 	v6Addrs := node.GetNodePortIPv6AddrsWithDevices()
 	for i, iface := range option.Config.Devices {
-		info := &models.KubeProxyReplacementDevicesItems0{
+		devicesLegacy[i] = iface
+		info := &models.KubeProxyReplacementDeviceListItems0{
 			Name: iface,
 			IP:   make([]string, 0),
 		}
@@ -267,7 +269,8 @@ func (d *Daemon) getKubeProxyReplacementStatus() *models.KubeProxyReplacement {
 
 	return &models.KubeProxyReplacement{
 		Mode:                mode,
-		Devices:             devices,
+		Devices:             devicesLegacy,
+		DeviceList:          devices,
 		DirectRoutingDevice: option.Config.DirectRoutingDevice,
 		Features:            features,
 	}

pkg/client/client.go
@@ -298,7 +298,7 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 	if sr.KubeProxyReplacement != nil {
 		devices := ""
 		if sr.KubeProxyReplacement.Mode != models.KubeProxyReplacementModeDisabled {
-			for i, dev := range sr.KubeProxyReplacement.Devices {
+			for i, dev := range sr.KubeProxyReplacement.DeviceList {
 				kubeProxyDevices += fmt.Sprintf("%s %s", dev.Name, strings.Join(dev.IP, " "))
 				if dev.Name == sr.KubeProxyReplacement.DirectRoutingDevice {
 					kubeProxyDevices += " (Direct Routing)"
@@ -307,7 +307,7 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 					kubeProxyDevices += ", "
 				}
 			}
-			if len(sr.KubeProxyReplacement.Devices) > 0 {
+			if len(sr.KubeProxyReplacement.DeviceList) > 0 {
 				devices = "[" + kubeProxyDevices + "]"
 			}
 		}
@@ -418,9 +418,9 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 					// When BPF Masquerading is enabled we don't do any masquerading for IPv6
 					// traffic so no SNAT Exclusion IPv6 CIDR is listed in status output.
 					devStr := ""
-					for i, dev := range sr.KubeProxyReplacement.Devices {
+					for i, dev := range sr.KubeProxyReplacement.DeviceList {
 						devStr += dev.Name
-						if i+1 != len(sr.KubeProxyReplacement.Devices) {
+						if i+1 != len(sr.KubeProxyReplacement.DeviceList) {
 							devStr += ", "
 						}
 					}

api/v1/server/embedded_spec.go
@@ -2670,7 +2670,7 @@ func init() {
       "description": "Status of kube-proxy replacement\n\n+k8s:deepcopy-gen=true",
       "type": "object",
       "properties": {
-        "devices": {
+        "deviceList": {
           "description": "\n\n+k8s:deepcopy-gen=true",
           "type": "array",
           "items": {
@@ -2688,6 +2688,12 @@ func init() {
                 "type": "string"
               }
             }
+          }
+        },
+        "devices": {
+          "type": "array",
+          "items": {
+            "type": "string"
           }
         },
         "directRoutingDevice": {
@@ -6664,11 +6670,17 @@ func init() {
       "description": "Status of kube-proxy replacement\n\n+k8s:deepcopy-gen=true",
       "type": "object",
       "properties": {
-        "devices": {
+        "deviceList": {
           "description": "\n\n+k8s:deepcopy-gen=true",
           "type": "array",
           "items": {
-            "$ref": "#/definitions/KubeProxyReplacementDevicesItems0"
+            "$ref": "#/definitions/KubeProxyReplacementDeviceListItems0"
+          }
+        },
+        "devices": {
+          "type": "array",
+          "items": {
+            "type": "string"
           }
         },
         "directRoutingDevice": {
@@ -6774,7 +6786,7 @@ func init() {
         }
       }
     },
-    "KubeProxyReplacementDevicesItems0": {
+    "KubeProxyReplacementDeviceListItems0": {
       "description": "\n\n+k8s:deepcopy-gen=true",
       "type": "object",
       "properties": {