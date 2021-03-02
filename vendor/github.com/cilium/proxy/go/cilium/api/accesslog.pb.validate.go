0cfc25b8c80771a965c27cfec3dcebf7c3d4a146
Jarno Rajahalme <jarno@covalent.io>
2021-03-02 10:40:14 -0800

    envoy: DO NOT MERGE
    Testing Envoy from main branch.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- vendor/github.com/cilium/proxy/go/cilium/api/accesslog.pb.validate.go
+++ vendor/github.com/cilium/proxy/go/cilium/api/accesslog.pb.validate.go
@@ -33,9 +33,6 @@ var (
 	_ = ptypes.DynamicAny{}
 )
 
-// define the regex for a UUID once up-front
-var _accesslog_uuidPattern = regexp.MustCompile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
-
 // Validate checks the field values on KeyValue with the rules defined in the
 // proto definition for this message. If any rules are violated, an error is returned.
 func (m *KeyValue) Validate() error {
