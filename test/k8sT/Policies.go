5dbf2b3d5ec0e4c8b1ebf3cf2558ac6b280b0c9d
Jarno Rajahalme <jarno@covalent.io>
2021-03-03 08:20:31 -0800

    test: Unquarantine TLS test for testing purposes
    Verify that L7 TLS policies still work after Envoy changes.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- test/k8sT/Policies.go
+++ test/k8sT/Policies.go
@@ -502,7 +502,7 @@ var _ = Describe("K8sPolicyTest", func() {
 				appPods[helpers.App3], clusterIP)
 		}, 500)
 
-		SkipItIf(helpers.SkipQuarantined, "TLS policy", func() {
+		It("TLS policy", func() {
 			By("Testing L7 Policy with TLS")
 
 			res := kubectl.CreateSecret("generic", "user-agent", "default", "--from-literal=user-agent=CURRL")
