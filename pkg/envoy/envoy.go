60aa69bc0ebd230bb8cf2cb67864d85d475cfe2b
Jarno Rajahalme <jarno@covalent.io>
2021-03-05 09:30:59 -0800

    envoy: Specify API version 2 explicitly on command line
    Envoy 1.17 disables APIv2 support by default. Re-enable by specifying
    '--bootstrap-version' as '2' on Envoy command line.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- pkg/envoy/envoy.go
+++ pkg/envoy/envoy.go
@@ -211,7 +211,7 @@ func StartEnvoy(stateDir, logPath string, baseID uint64) *Envoy {
 
 		for {
 			logLevel := logging.GetLevel(logging.DefaultLogger)
-			cmd := exec.Command(ciliumEnvoy, "-l", mapLogLevel(logLevel), "-c", bootstrapPath, "--base-id", strconv.FormatUint(baseID, 10), "--log-format", logFormat)
+			cmd := exec.Command(ciliumEnvoy, "-l", mapLogLevel(logLevel), "-c", bootstrapPath, "--base-id", strconv.FormatUint(baseID, 10), "--log-format", logFormat, "--bootstrap-version", "2")
 			cmd.Stderr = logWriter
 			cmd.Stdout = logWriter
 
