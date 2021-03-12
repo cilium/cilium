4d701b83664168585fd53772c715a4cac302bcea
Tobias Klauser <tobias@cilium.io>
2021-03-12 10:11:39 +0100

    images: determine image sha256 regardless of usage in CILIUM_BUILDER_IMAGE
    Since the refactoring of the get-image-digest.sh script in commit
    8e00f7836b17 ("re-write get-image-digest script") as part of the image
    build refactoring in #15023, it should no longer be necessary to check
    the usage of the image tag before getting the sha256. Just determine it
    once outside the loop, regardless of usage in the respective files.
    
    This will make it easier in the successive commit to replace the full
    image name in files other than images/*/Dockerfile.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- images/scripts/update-cilium-runtime-image.sh
+++ images/scripts/update-cilium-runtime-image.sh
@@ -18,19 +18,16 @@ image="quay.io/cilium/cilium-runtime"
 
 image_tag="$(WITHOUT_SUFFIX=1 "${script_dir}/make-image-tag.sh" images/runtime)"
 
+image_full="${image}:${image_tag}"
+sha256=$("${script_dir}/get-image-digest.sh" "${image_full}" || echo "")
+if [ -n "${sha256}" ]; then
+  image_full="${image_full}@${sha256}"
+fi
+
 # shellcheck disable=SC2207
 used_by=($(git grep -l CILIUM_RUNTIME_IMAGE= images/*/Dockerfile))
 
 for i in "${used_by[@]}" ; do
-  image_full="${image}:${image_tag}"
-  # Detect if the image_tag already exists, if it does then we can assume the
-  # image was created and a sha256 is available for it.
-  if grep "CILIUM_RUNTIME_IMAGE=${image}:${image_tag}" "${i}" ; then
-    sha256=$("${script_dir}/get-image-digest.sh" "${image_full}")
-    if [ -n "${sha256}" ]; then
-      image_full="${image_full}@${sha256}"
-    fi
-  fi
   sed "s|\(CILIUM_RUNTIME_IMAGE=\)${image}:.*\$|\1${image_full}|" "${i}" > "${i}.sedtmp" && mv "${i}.sedtmp" "${i}"
 done
 
