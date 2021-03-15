2da68e2c209e853a3f7f2332bb4e01063e97b0f4
Gobinath Krishnamoorthy <gobinathk@google.com>
2021-03-15 16:30:03 -0700

    Perform reverse NAT at Host Interface
    Description: if NETFILTER_COMPAT_MODE flag is set, guarantees that traffic will
    pass-through kernel netfilter and that Iptables rules are enforced on traffic.
    
    Problem: if Nodeport traffic ingress on the same node as a backend pod's node,
    NAT and reverse NAT translations at 2 different interfaces.
    On ingress path, NAT happens at Node's host interface, pass through kernel stack.
    on egress path reverse NAT happen at Pod's Veth interface and bypasses kernel stack.
    This asymmetric behavior, reflects in kernel conntrack entries and having a IPTABLE
    rule to DROP any packets if conntrack state is INVALID which results in packet DROP.
    
    Fixes: #11914
    
    Signed-off-by: Gobinath Krishnamoorthy <gobinathk@google.com>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -269,6 +269,9 @@ ct_recreate6:
 #ifdef ENABLE_NODEPORT
 		/* See comment in handle_ipv4_from_lxc(). */
 		if (ct_state.node_port) {
+#ifdef NETFILTER_COMPAT_MODE
+			return CTX_ACT_OK;
+#endif /* NETFILTER_COMPAT_MODE */
 			ctx->tc_index |= TC_INDEX_F_SKIP_RECIRCULATION;
 			ep_tail_call(ctx, CILIUM_CALL_IPV6_NODEPORT_REVNAT);
 			return DROP_MISSED_TAIL_CALL;
@@ -705,6 +708,12 @@ ct_recreate4:
 		 * perform the reverse DNAT.
 		 */
 		if (ct_state.node_port) {
+#ifdef NETFILTER_COMPAT_MODE
+			/* Pass the packet to the stack and let bpf_host perform
+			 * rev-DNAT at egress of the native device.
+			 */
+			return CTX_ACT_OK;
+#endif /* NETFILTER_COMPAT_MODE */
 			ctx->tc_index |= TC_INDEX_F_SKIP_RECIRCULATION;
 			ep_tail_call(ctx, CILIUM_CALL_IPV4_NODEPORT_REVNAT);
 			return DROP_MISSED_TAIL_CALL;
