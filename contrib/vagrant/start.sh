aa1e8c3bdb54493cb48abdf9d6c20ee1951df635
Shikugawa <Shikugawa@gmail.com>
2021-03-17 00:55:18 +0900

    vagrant: Follow cilium-agent options on development VM to Helm defaults
    Signed-off-by: Rei Shimizu <Shikugawa@gmail.com>

--- contrib/vagrant/start.sh
+++ contrib/vagrant/start.sh
@@ -306,7 +306,10 @@ function write_cilium_cfg() {
     ipv6_addr="${3}"
     filename="${4}"
 
-    cilium_options=" --debug --pprof --enable-hubble --hubble-listen-address :4244 --enable-k8s-event-handover --k8s-require-ipv4-pod-cidr"
+    cilium_options="\
+      --debug --pprof --enable-hubble --hubble-listen-address :4244 --enable-k8s-event-handover \
+      --k8s-require-ipv4-pod-cidr --enable-bandwidth-manager --kube-proxy-replacement probe \
+      --enable-remote-node-identity"
     cilium_operator_options=" --debug"
 
     if [[ "${IPV4}" -eq "1" ]]; then
