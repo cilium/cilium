031c32c8d5510b8bdf61aaac66ff82276b00d155
Thomas Graf <thomas@cilium.io>
2021-03-18 00:45:22 +0100

    connectivity: Make flow validation work with monitor aggregation
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Detect monitor aggregation and adjust flow validation requirements
    accordingly.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- cilium-cli/defaults/defaults.go
+++ cilium-cli/defaults/defaults.go
@@ -94,6 +94,9 @@ const (
 
 	FlowWaitTimeout   = 5 * time.Second
 	FlowRetryInterval = 500 * time.Millisecond
+
+	ConfigMapKeyMonitorAggregation      = "monitor-aggregation"
+	ConfigMapValueMonitorAggregatonNone = "none"
 )
 
 var (
