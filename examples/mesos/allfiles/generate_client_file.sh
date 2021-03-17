573e6968e44dc06b3f30ecaac08df13973c90ccf
Tobias Klauser <tobias@cilium.io>
2021-03-17 15:52:25 +0100

    examples: remove obsolete Mesos example
    Noticed while reviewing #15370
    
    The respective guide was removed from documentation in commit
    3d0e805c2dfa ("doc: Remove Mesos/Marathon guide"), thus the link in the
    README is broken already. The example hasn't been tested for the last
    few releases (v1.9: #13627, v1.8: #11903), so remove it.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- examples/mesos/allfiles/generate_client_file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "goodclient" ] || [ "$1" == "badclient" ] 
-  then
-    export IP=`cilium endpoint list | grep web-server | awk '{print $7}'`
-    sed "s/__WEBSERVER_IP__/$IP/g; s/__CLIENTTYPE__/$1/g" ./client-template.json > $1.json
-  else
-    echo "Invalid client name. Please enter goodclient or badclient."
-fi
