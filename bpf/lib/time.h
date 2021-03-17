63a3473f5b6e62864cd4970a295aa34125b49ffd
Daniel Borkmann <daniel@iogearbox.net>
2021-03-17 10:09:29 +0100

    bpf: add per-cpu ktime cache for faster subsequent access
    Add a per-cpu ktime cache we can use for the packet capturing exporter
    facility from the LB.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/lib/time.h
+++ bpf/lib/time.h
@@ -27,4 +27,38 @@
 #define bpf_sec_to_jiffies(s)	\
 	({ __u64 __x = (s) * KERNEL_HZ; __x; })
 
+/* Per-CPU ktime cache for faster clock access. */
+struct bpf_elf_map __section_maps cilium_ktime_cache = {
+	.type		= BPF_MAP_TYPE_PERCPU_ARRAY,
+	.size_key	= sizeof(__u32),
+	.size_value	= sizeof(__u64),
+	.pinning	= PIN_GLOBAL_NS,
+	.max_elem	= 1,
+};
+
+/* Currently supported clock types:
+ *
+ * - bpf_ktime_cache_set(ns)      -> CLOCK_MONOTONIC
+ * - bpf_ktime_cache_set(boot_ns) -> CLOCK_BOOTTIME
+ */
+#define bpf_ktime_cache_set(clock)					     \
+	({								     \
+		__u32 __z = 0;						     \
+		__u64 *__cache = map_lookup_elem(&cilium_ktime_cache, &__z); \
+		__u64 __ktime = ktime_get_##clock();			     \
+		if (always_succeeds(__cache))				     \
+			*__cache = __ktime;				     \
+		__ktime;						     \
+	})
+
+#define bpf_ktime_cache_get()						     \
+	({								     \
+		__u32 __z = 0;						     \
+		__u64 *__cache = map_lookup_elem(&cilium_ktime_cache, &__z); \
+		__u64 __ktime = 0;					     \
+		if (always_succeeds(__cache))				     \
+			__ktime = *__cache;				     \
+		__ktime;						     \
+	})
+
 #endif /* __LIB_TIME_H_ */
