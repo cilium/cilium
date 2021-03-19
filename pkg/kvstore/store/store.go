cilium/cilium

commit 07312c6d155d11b8a8dd5f7f621c1f13b2266066

pkg/{kvstore,node}: delay node delete event in kvstore
Instead of checking if the node is still present in the observer, we
should check if the node is still present in the store instead. Checking
if the node is still available in the observer is irrelevant as the node
will never be deleted since the check for its existence in the store
will always be true.

Instead, delay the propagation of the deletion event to the observer in
the store itself when we receive shared keys notifications.

Since the propagaton delay is done at the node observer the unit tests
will take longer to complete.

Fixes: 3137ad5eee6c ("node: Delay handling of node delete events received via kvstore")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/store/store.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging"
@@ -257,6 +258,10 @@ func (s *SharedStore) Close() {
 		}
 
 		delete(s.localKeys, name)
+		// Since we have received our own notification we also need to remove
+		// it from the shared keys.
+		delete(s.sharedKeys, name)
+
 		s.onDelete(key)
 	}
 }
@@ -426,14 +431,26 @@ func (s *SharedStore) updateKey(name string, value []byte) error {
 	return nil
 }
 
-func (s *SharedStore) deleteKey(name string) {
+func (s *SharedStore) deleteSharedKey(name string) {
 	s.mutex.Lock()
 	existingKey, ok := s.sharedKeys[name]
 	delete(s.sharedKeys, name)
 	s.mutex.Unlock()
 
 	if ok {
-		s.onDelete(existingKey)
+		go func() {
+			time.Sleep(defaults.NodeDeleteDelay)
+			s.mutex.RLock()
+			_, ok := s.sharedKeys[name]
+			s.mutex.RUnlock()
+			if ok {
+				log.Warningf("Received node delete event for node %s which re-appeared within %s",
+					name, defaults.NodeDeleteDelay)
+				return
+			}
+
+			s.onDelete(existingKey)
+		}()
 	} else {
 		s.getLogger().WithField("key", name).
 			Warning("Unable to find deleted key in local state")
@@ -488,7 +505,7 @@ func (s *SharedStore) watcher(listDone chan bool) {
 
 				s.syncLocalKey(localKey)
 			} else {
-				s.deleteKey(keyName)
+				s.deleteSharedKey(keyName)
 			}
 		}
 	}

pkg/node/store/store.go
@@ -16,9 +16,7 @@ package store
 
 import (
 	"path"
-	"time"
 
-	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/kvstore"
@@ -101,26 +99,16 @@ func (o *NodeObserver) OnDelete(k store.NamedKey) {
 		nodeCopy := n.DeepCopy()
 		nodeCopy.Source = node.FromKVStore
 
-		go func() {
-			time.Sleep(defaults.NodeDeleteDelay)
+		o.manager.NodeDeleted(*nodeCopy)
 
-			if o.manager.Exists(nodeCopy.Identity()) {
-				log.Warningf("Received node delete event for node %s which re-appeared within %s",
-					nodeCopy.Name, defaults.NodeDeleteDelay)
-				return
-			}
-
-			o.manager.NodeDeleted(*nodeCopy)
-
-			ciliumIPv4 := nodeCopy.GetCiliumInternalIP(false)
-			if ciliumIPv4 != nil {
-				ipcache.IPIdentityCache.Delete(ciliumIPv4.String(), ipcache.FromKVStore)
-			}
-			ciliumIPv6 := nodeCopy.GetCiliumInternalIP(true)
-			if ciliumIPv6 != nil {
-				ipcache.IPIdentityCache.Delete(ciliumIPv6.String(), ipcache.FromKVStore)
-			}
-		}()
+		ciliumIPv4 := nodeCopy.GetCiliumInternalIP(false)
+		if ciliumIPv4 != nil {
+			ipcache.IPIdentityCache.Delete(ciliumIPv4.String(), ipcache.FromKVStore)
+		}
+		ciliumIPv6 := nodeCopy.GetCiliumInternalIP(true)
+		if ciliumIPv6 != nil {
+			ipcache.IPIdentityCache.Delete(ciliumIPv6.String(), ipcache.FromKVStore)
+		}
 	}
 }
 

pkg/clustermesh/services_test.go
@@ -23,6 +23,7 @@ import (
 	"path"
 	"time"
 
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/k8s"
@@ -111,7 +112,7 @@ func (s *ClusterMeshServicesTestSuite) expectEvent(c *C, action k8s.CacheAction,
 		var event k8s.ServiceEvent
 		select {
 		case event = <-s.svcCache.Events:
-		case <-time.After(time.Second * 10):
+		case <-time.After(defaults.NodeDeleteDelay + time.Second*10):
 			c.Errorf("Timeout while waiting for event to be received")
 			return false
 		}
@@ -234,9 +235,14 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesUpdate(c *C) {
 	})
 
 	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName + "1")
-	s.expectEvent(c, k8s.UpdateService, svcID, nil)
-
+	// The observer will have a defaults.NodeDeleteDelay time before it receives
+	// the event. For this reason we will trigger the delete events sequentially
+	// and only do the assertion in the end. This way we wait 30seconds for the
+	// test to complete instead of 30+30 seconds.
+	time.Sleep(2 * time.Second)
 	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName + "2")
+
+	s.expectEvent(c, k8s.UpdateService, svcID, nil)
 	s.expectEvent(c, k8s.DeleteService, svcID, nil)
 }
 

pkg/kvstore/store/store_test.go
@@ -22,6 +22,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/option"
@@ -165,7 +166,7 @@ func expect(check func() bool) error {
 			return nil
 		}
 
-		if time.Since(start) > 10*time.Second {
+		if time.Since(start) > defaults.NodeDeleteDelay+10*time.Second {
 			return fmt.Errorf("timeout while waiting for expected value")
 		}
 
@@ -197,15 +198,20 @@ func (s *StoreSuite) TestStoreOperations(c *C) {
 	c.Assert(expect(func() bool { return localKey3.updated() == 0 }), IsNil)
 
 	store.DeleteLocalKey(&localKey1)
-	c.Assert(expect(func() bool { return localKey1.deleted() >= 1 }), IsNil)
+	// localKey2 will be deleted 2 times, one from local key and other from
+	// the kvstore watcher
+	c.Assert(expect(func() bool { return localKey1.deleted() == 2 }), IsNil)
 	c.Assert(expect(func() bool { return localKey2.deleted() == 0 }), IsNil)
 	c.Assert(expect(func() bool { return localKey3.deleted() == 0 }), IsNil)
 
 	store.DeleteLocalKey(&localKey3)
+	// localKey3 won't be deleted because it was never added
 	c.Assert(expect(func() bool { return localKey3.deleted() == 0 }), IsNil)
 
 	store.DeleteLocalKey(&localKey2)
 	c.Assert(expect(func() bool { return localKey1.deleted() == 2 }), IsNil)
+	// localKey2 will be deleted 2 times, one from local key and other from
+	// the kvstore watcher
 	c.Assert(expect(func() bool { return localKey2.deleted() == 2 }), IsNil)
 	c.Assert(expect(func() bool { return localKey3.deleted() == 0 }), IsNil)
 }

commit 57c5f7da3dfbde68ba51c4548219918c72e631df

kvstore: Fix aborted delayed delete warning
The code is generic and has nothing todo with nodes. Fix the warning
message.

Fixes: 07312c6d155 ("pkg/{kvstore,node}: delay node delete event in kvstore")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/kvstore/store/store.go
@@ -469,8 +469,8 @@ func (s *SharedStore) deleteSharedKey(name string) {
 			_, ok := s.sharedKeys[name]
 			s.mutex.RUnlock()
 			if ok {
-				log.Warningf("Received node delete event for node %s which re-appeared within %s",
-					name, s.conf.SharedKeyDeleteDelay)
+				s.getLogger().WithFields(logrus.Fields{"key": name, "timeWindow": s.conf.SharedKeyDeleteDelay}).
+					Warning("Received delete event for key which re-appeared within delay time window")
 				return
 			}
 