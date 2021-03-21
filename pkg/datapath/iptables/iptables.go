cilium/cilium

commit e463cfe80102486d1a3423b77f9c54afd0be94a2

Adds iptables managment for nodelocaldns.
We watch for a new annotation "io.cilium.no-track-port" when
updating endpoints. Combined with adding such annotation to
nodelocaldns's yaml, this should bring back the NOTRACK rules for
nodelocaldns.

Signed-off-by: Weilong Cui <cuiwl@google.com>

pkg/datapath/iptables/iptables.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/defaults"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
+	lb "github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/modules"
 	"github.com/cilium/cilium/pkg/node"
@@ -39,6 +40,7 @@ import (
 
 	go_version "github.com/blang/semver"
 	"github.com/mattn/go-shellwords"
+	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -62,6 +64,18 @@ const (
 var (
 	isWaitMinVersion        = versioncheck.MustCompile(">=1.4.20")
 	isWaitSecondsMinVersion = versioncheck.MustCompile(">=1.4.22")
+	noTrackPorts            = func(port uint16) []*lb.L4Addr {
+		return []*lb.L4Addr{
+			{
+				Protocol: lb.TCP,
+				Port:     port,
+			},
+			{
+				Protocol: lb.UDP,
+				Port:     port,
+			},
+		}
+	}
 )
 
 const (
@@ -688,6 +702,88 @@ func (m *IptablesManager) iptProxyRules(cmd string, proxyPort uint16, ingress bo
 			return err
 		}
 		if err := m.iptEgressProxyRule(cmd, "udp", proxyPort, name); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func noTrackRules(prog string, cmd string, IP string, port *lb.L4Addr, ingress bool) error {
+	protocol := strings.ToLower(port.Protocol)
+	p := strconv.FormatUint(uint64(port.Port), 10)
+	if ingress {
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "raw", cmd, "PREROUTING", "-p", protocol, "-d", IP, "--dport", p, "-j", "NOTRACK"}, false); err != nil {
+			return err
+		}
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "filter", cmd, "INPUT", "-p", protocol, "-d", IP, "--dport", p, "-j", "ACCEPT"}, false); err != nil {
+			return err
+		}
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "raw", cmd, "OUTPUT", "-p", protocol, "-d", IP, "--dport", p, "-j", "NOTRACK"}, false); err != nil {
+			return err
+		}
+	} else {
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "raw", cmd, "OUTPUT", "-p", protocol, "-s", IP, "--sport", p, "-j", "NOTRACK"}, false); err != nil {
+			return err
+		}
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "filter", cmd, "OUTPUT", "-p", protocol, "-s", IP, "--sport", p, "-j", "ACCEPT"}, false); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func InstallNoTrackRules(IP string, port uint16, ipv6 bool) error {
+	prog := "iptables"
+	ipField := logfields.IPv4
+	if ipv6 {
+		prog = "ip6tables"
+		ipField = logfields.IPv6
+	}
+	ports := noTrackPorts(port)
+	for _, p := range ports {
+		if err := noTrackRules(prog, "-A", IP, p, true); err != nil {
+			log.WithFields(logrus.Fields{
+				ipField:            IP,
+				logfields.Port:     p.Port,
+				logfields.Protocol: p.Protocol,
+			}).WithError(err).Warn("Unable to install ingress NOTRACK rules")
+			return err
+		}
+		if err := noTrackRules(prog, "-A", IP, p, false); err != nil {
+			log.WithFields(logrus.Fields{
+				ipField:            IP,
+				logfields.Port:     p.Port,
+				logfields.Protocol: p.Protocol,
+			}).WithError(err).Warn("Unable to install egress NOTRACK rules")
+			return err
+		}
+	}
+	return nil
+}
+
+func RemoveNoTrackRules(IP string, port uint16, ipv6 bool) error {
+	prog := "iptables"
+	ipField := logfields.IPv4
+	if ipv6 {
+		prog = "ip6tables"
+		ipField = logfields.IPv6
+	}
+	ports := noTrackPorts(port)
+	for _, p := range ports {
+		if err := noTrackRules(prog, "-D", IP, p, true); err != nil {
+			log.WithFields(logrus.Fields{
+				ipField:            IP,
+				logfields.Port:     p.Port,
+				logfields.Protocol: p.Protocol,
+			}).WithError(err).Warn("Unable to remove ingress NOTRACK rules")
+			return err
+		}
+		if err := noTrackRules(prog, "-D", IP, p, false); err != nil {
+			log.WithFields(logrus.Fields{
+				ipField:            IP,
+				logfields.Port:     p.Port,
+				logfields.Protocol: p.Protocol,
+			}).WithError(err).Warn("Unable to remove egress NOTRACK rules")
 			return err
 		}
 	}

daemon/cmd/endpoint.go
@@ -476,6 +476,13 @@ func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, e
 			}
 			return p.Annotations[bandwidth.EgressBandwidth], nil
 		})
+		ep.UpdateNoTrackRules(func(ns, podName string) (noTrackPort string, err error) {
+			p, err := d.k8sWatcher.GetCachedPod(ns, podName)
+			if err != nil {
+				return "", err
+			}
+			return p.Annotations[annotation.NoTrack], nil
+		})
 	}
 
 	regenTriggered := ep.UpdateLabels(ctx, addLabels, infoLabels, true)

examples/kubernetes-local-redirect/node-local-dns.yaml
@@ -107,6 +107,7 @@ spec:
       labels:
         k8s-app: node-local-dns
       annotations:
+        io.cilium.no-track-port: "53"
         prometheus.io/port: "9253"
         prometheus.io/scrape: "true"
     spec:

pkg/annotation/k8s.go
@@ -65,4 +65,9 @@ const (
 	// pod is redirected to the proxy for the given port / protocol in the
 	// annotation
 	ProxyVisibility = Prefix + ".proxy-visibility"
+
+	// NoTrack is the annotation name used to store the port and protocol
+	// that we should bypass kernel conntrack for a given pod. This applies for
+	// both TCP and UDP connection. Current use case is NodeLocalDNS.
+	NoTrack = Prefix + ".no-track-port"
 )

pkg/endpoint/endpoint.go
@@ -35,6 +35,7 @@ import (
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/datapath/iptables"
 	"github.com/cilium/cilium/pkg/datapath/link"
 	linuxrouting "github.com/cilium/cilium/pkg/datapath/linux/routing"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
@@ -337,6 +338,8 @@ type Endpoint struct {
 	allocator cache.IdentityAllocator
 
 	isHost bool
+
+	noTrackPort uint16
 }
 
 // SetAllocator sets the identity allocator for this endpoint.
@@ -453,6 +456,7 @@ func createEndpoint(owner regeneration.Owner, proxy EndpointProxy, allocator cac
 		regenFailedChan: make(chan struct{}, 1),
 		allocator:       allocator,
 		logLimiter:      logging.NewLimiter(10*time.Second, 3), // 1 log / 10 secs, burst of 3
+		noTrackPort:     0,
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -1670,6 +1674,13 @@ func (e *Endpoint) RunMetadataResolver(resolveMetadata MetadataResolverCB) {
 				}
 				e.SetPod(pod)
 				e.SetK8sMetadata(cp)
+				e.UpdateNoTrackRules(func(_, _ string) (noTrackPort string, err error) {
+					_, _, _, _, annotations, err := resolveMetadata(ns, podName)
+					if err != nil {
+						return "", err
+					}
+					return annotations[annotation.NoTrack], nil
+				})
 				e.UpdateVisibilityPolicy(func(_, _ string) (proxyVisibility string, err error) {
 					_, _, _, _, annotations, err := resolveMetadata(ns, podName)
 					if err != nil {
@@ -2281,6 +2292,24 @@ func (e *Endpoint) Delete(monitor monitorOwner, ipam ipReleaser, manager endpoin
 		// likely manual intervention.
 		if err := linuxrouting.Delete(e.IPv4.IP()); err != nil {
 			errs = append(errs, fmt.Errorf("unable to delete endpoint routing rules: %s", err))
+		}
+	}
+
+	if e.noTrackPort > 0 {
+		e.getLogger().WithFields(logrus.Fields{
+			"ep":     e.GetID(),
+			"ipAddr": e.GetIPv4Address(),
+		}).Debug("Deleting endpoint NOTRACK rules")
+
+		if e.IPv4.IsSet() {
+			if err := iptables.RemoveNoTrackRules(e.IPv4.String(), e.noTrackPort, false); err != nil {
+				errs = append(errs, fmt.Errorf("unable to delete endpoint NOTRACK ipv4 rules: %s", err))
+			}
+		}
+		if e.IPv6.IsSet() {
+			if err := iptables.RemoveNoTrackRules(e.IPv6.String(), e.noTrackPort, true); err != nil {
+				errs = append(errs, fmt.Errorf("unable to delete endpoint NOTRACK ipv6 rules: %s", err))
+			}
 		}
 	}
 

pkg/endpoint/events.go
@@ -18,6 +18,7 @@ import (
 	"strconv"
 
 	"github.com/cilium/cilium/pkg/bandwidth"
+	"github.com/cilium/cilium/pkg/datapath/iptables"
 	"github.com/cilium/cilium/pkg/eventqueue"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/bwmap"
@@ -117,6 +118,82 @@ func (e *Endpoint) PolicyRevisionBumpEvent(rev uint64) {
 			logfields.EndpointID:     e.ID,
 		}).Errorf("enqueue of EndpointRevisionBumpEvent failed: %s", err)
 	}
+}
+
+/// EndpointNoTrackEvent contains all fields necessary to update the NOTRACK rules.
+type EndpointNoTrackEvent struct {
+	ep     *Endpoint
+	annoCB AnnotationsResolverCB
+}
+
+// Handle handles the NOTRACK rule update.
+func (ev *EndpointNoTrackEvent) Handle(res chan interface{}) {
+	var port uint16
+
+	e := ev.ep
+
+	// If this endpoint is going away, nothing to do.
+	if err := e.lockAlive(); err != nil {
+		res <- &EndpointRegenerationResult{
+			err: nil,
+		}
+		return
+	}
+
+	defer e.unlock()
+
+	portStr, err := ev.annoCB(e.K8sNamespace, e.K8sPodName)
+	if err != nil {
+		res <- &EndpointRegenerationResult{
+			err: err,
+		}
+		return
+	}
+
+	if portStr == "" {
+		port = 0
+	} else {
+		// Validate annotation before we do any actual alteration to the endpoint.
+		p64, err := strconv.ParseUint(portStr, 10, 16)
+		// Port should be within [1-65535].
+		if err != nil || p64 == 0 {
+			res <- &EndpointRegenerationResult{
+				err: err,
+			}
+			return
+		}
+		port = uint16(p64)
+	}
+
+	if port != e.noTrackPort {
+		log.Debug("Updating NOTRACK rules")
+		if e.IPv4.IsSet() {
+			if port > 0 {
+				err = iptables.InstallNoTrackRules(e.IPv4.String(), port, false)
+				log.Warnf("Error installing iptable NOTRACK rules %s", err)
+			}
+			if e.noTrackPort > 0 {
+				err = iptables.RemoveNoTrackRules(e.IPv4.String(), e.noTrackPort, false)
+				log.Warnf("Error removing iptable NOTRACK rules %s", err)
+			}
+		}
+		if e.IPv6.IsSet() {
+			if port > 0 {
+				iptables.InstallNoTrackRules(e.IPv6.String(), port, true)
+				log.Warnf("Error installing iptable NOTRACK rules %s", err)
+			}
+			if e.noTrackPort > 0 {
+				err = iptables.RemoveNoTrackRules(e.IPv6.String(), e.noTrackPort, true)
+				log.Warnf("Error removing iptable NOTRACK rules %s", err)
+			}
+		}
+		e.noTrackPort = port
+	}
+
+	res <- &EndpointRegenerationResult{
+		err: nil,
+	}
+	return
 }
 
 // EndpointPolicyVisibilityEvent contains all fields necessary to update the

pkg/endpoint/policy.go
@@ -828,6 +828,21 @@ func (e *Endpoint) GetCIDRPrefixLengths() (s6, s4 []int) {
 // annotations.
 type AnnotationsResolverCB func(ns, podName string) (proxyVisibility string, err error)
 
+// UpdateNoTrackRules updates the NOTRACK iptable rules for this endpoint. If anno
+// is empty, then any existing NOTRACK rules will be removed. If anno cannot be parsed,
+// we remove existing NOTRACK rules too if there's any.
+func (e *Endpoint) UpdateNoTrackRules(annoCB AnnotationsResolverCB) {
+	ch, err := e.eventQueue.Enqueue(eventqueue.NewEvent(&EndpointNoTrackEvent{
+		ep:     e,
+		annoCB: annoCB,
+	}))
+	if err != nil {
+		e.getLogger().WithError(err).Error("Unable to enqueue endpoint notrack event")
+		return
+	}
+	<-ch
+}
+
 // UpdateVisibilityPolicy updates the visibility policy of this endpoint to
 // reflect the state stored in the provided proxy visibility annotation. If anno
 // is empty, then the VisibilityPolicy for the Endpoint will be empty, and will

pkg/k8s/watchers/pod.go
@@ -272,7 +272,8 @@ func (k *K8sWatcher) updateK8sPodV1(oldK8sPod, newK8sPod *slim_corev1.Pod) error
 	newAnno := newK8sPod.ObjectMeta.Annotations
 	annoChangedProxy := !k8s.AnnotationsEqual([]string{annotation.ProxyVisibility}, oldAnno, newAnno)
 	annoChangedBandwidth := !k8s.AnnotationsEqual([]string{bandwidth.EgressBandwidth}, oldAnno, newAnno)
-	annotationsChanged := annoChangedProxy || annoChangedBandwidth
+	annoChangedNoTrack := !k8s.AnnotationsEqual([]string{annotation.NoTrack}, oldAnno, newAnno)
+	annotationsChanged := annoChangedProxy || annoChangedBandwidth || annoChangedNoTrack
 
 	// Check label updates too.
 	oldPodLabels := oldK8sPod.ObjectMeta.Labels
@@ -343,6 +344,15 @@ func (k *K8sWatcher) updateK8sPodV1(oldK8sPod, newK8sPod *slim_corev1.Pod) error
 					return "", nil
 				}
 				return p.ObjectMeta.Annotations[bandwidth.EgressBandwidth], nil
+			})
+		}
+		if annoChangedNoTrack {
+			podEP.UpdateNoTrackRules(func(ns, podName string) (noTrackPort string, err error) {
+				p, err := k.GetCachedPod(ns, podName)
+				if err != nil {
+					return "", nil
+				}
+				return p.ObjectMeta.Annotations[annotation.NoTrack], nil
 			})
 		}
 		realizePodAnnotationUpdate(podEP)

commit 419b187cb6535f94ceaca11287b2816afc2559a0

iptables: use CILIUM_* chains for per-endpoint no CT rules
These per-endpoint no CT rules should be added to the Cilium managed
chains rather than to the Iptables default ones as otherwise during
initialization the agent will keep readding them even if they are
already present.

Here's an example: a pod with the `io.cilium.no-track-port` annotation
is created and the rules are correctly added:

    vagrant@k8s1:~/go/src/github.com/cilium/cilium/mydev$ sudo iptables -t raw -L PREROUTING
    Chain PREROUTING (policy ACCEPT)
    target     prot opt source               destination
    CILIUM_PRE_raw  all  --  anywhere             anywhere             /* cilium-feeder: CILIUM_PRE_raw */
    CT         tcp  --  anywhere             10.11.0.14           tcp dpt:domain NOTRACK
    CT         udp  --  anywhere             10.11.0.14           udp dpt:domain NOTRACK
    vagrant@k8s1:~/go/src/github.com/cilium/cilium/mydev$ sudo iptables -t raw -L OUTPUT
    Chain OUTPUT (policy ACCEPT)
    target     prot opt source               destination
    CILIUM_OUTPUT_raw  all  --  anywhere             anywhere             /* cilium-feeder: CILIUM_OUTPUT_raw */
    CT         tcp  --  anywhere             10.11.0.14           tcp dpt:domain NOTRACK
    CT         tcp  --  10.11.0.14           anywhere             tcp spt:domain NOTRACK
    CT         udp  --  anywhere             10.11.0.14           udp dpt:domain NOTRACK
    CT         udp  --  10.11.0.14           anywhere             udp spt:domain NOTRACK

However when the agent is restarted the same rules are incorrectly
appended again:

    vagrant@k8s1:~/go/src/github.com/cilium/cilium/mydev$ sudo iptables -t raw -L PREROUTING
    Chain PREROUTING (policy ACCEPT)
    target     prot opt source               destination
    CILIUM_PRE_raw  all  --  anywhere             anywhere             /* cilium-feeder: CILIUM_PRE_raw */
    CT         tcp  --  anywhere             10.11.0.14           tcp dpt:domain NOTRACK
    CT         udp  --  anywhere             10.11.0.14           udp dpt:domain NOTRACK
    CT         tcp  --  anywhere             10.11.0.14           tcp dpt:domain NOTRACK
    CT         udp  --  anywhere             10.11.0.14           udp dpt:domain NOTRACK
    vagrant@k8s1:~/go/src/github.com/cilium/cilium/mydev$ sudo iptables -t raw -L OUTPUT
    Chain OUTPUT (policy ACCEPT)
    target     prot opt source               destination
    CILIUM_OUTPUT_raw  all  --  anywhere             anywhere             /* cilium-feeder: CILIUM_OUTPUT_raw */
    CT         tcp  --  anywhere             10.11.0.14           tcp dpt:domain NOTRACK
    CT         tcp  --  10.11.0.14           anywhere             tcp spt:domain NOTRACK
    CT         udp  --  anywhere             10.11.0.14           udp dpt:domain NOTRACK
    CT         udp  --  10.11.0.14           anywhere             udp spt:domain NOTRACK
    CT         tcp  --  anywhere             10.11.0.14           tcp dpt:domain NOTRACK
    CT         tcp  --  10.11.0.14           anywhere             tcp spt:domain NOTRACK
    CT         udp  --  anywhere             10.11.0.14           udp dpt:domain NOTRACK
    CT         udp  --  10.11.0.14           anywhere             udp spt:domain NOTRACK

Fixes: e463cfe ("Adds iptables managment for nodelocaldns")

pkg/datapath/iptables/iptables.go
@@ -715,20 +715,20 @@ func noTrackRules(prog string, cmd string, IP string, port *lb.L4Addr, ingress b
 	protocol := strings.ToLower(port.Protocol)
 	p := strconv.FormatUint(uint64(port.Port), 10)
 	if ingress {
-		if _, err := runProgCombinedOutput(prog, []string{"-t", "raw", cmd, "PREROUTING", "-p", protocol, "-d", IP, "--dport", p, "-j", "NOTRACK"}, false); err != nil {
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "raw", cmd, ciliumPreRawChain, "-p", protocol, "-d", IP, "--dport", p, "-j", "NOTRACK"}, false); err != nil {
 			return err
 		}
-		if _, err := runProgCombinedOutput(prog, []string{"-t", "filter", cmd, "INPUT", "-p", protocol, "-d", IP, "--dport", p, "-j", "ACCEPT"}, false); err != nil {
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "filter", cmd, ciliumInputChain, "-p", protocol, "-d", IP, "--dport", p, "-j", "ACCEPT"}, false); err != nil {
 			return err
 		}
-		if _, err := runProgCombinedOutput(prog, []string{"-t", "raw", cmd, "OUTPUT", "-p", protocol, "-d", IP, "--dport", p, "-j", "NOTRACK"}, false); err != nil {
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "raw", cmd, ciliumOutputRawChain, "-p", protocol, "-d", IP, "--dport", p, "-j", "NOTRACK"}, false); err != nil {
 			return err
 		}
 	} else {
-		if _, err := runProgCombinedOutput(prog, []string{"-t", "raw", cmd, "OUTPUT", "-p", protocol, "-s", IP, "--sport", p, "-j", "NOTRACK"}, false); err != nil {
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "raw", cmd, ciliumOutputRawChain, "-p", protocol, "-s", IP, "--sport", p, "-j", "NOTRACK"}, false); err != nil {
 			return err
 		}
-		if _, err := runProgCombinedOutput(prog, []string{"-t", "filter", cmd, "OUTPUT", "-p", protocol, "-s", IP, "--sport", p, "-j", "ACCEPT"}, false); err != nil {
+		if _, err := runProgCombinedOutput(prog, []string{"-t", "filter", cmd, ciliumOutputChain, "-p", protocol, "-s", IP, "--sport", p, "-j", "ACCEPT"}, false); err != nil {
 			return err
 		}
 	}