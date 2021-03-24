2dd33cd96d88ee0f20aaced7f628a02d8e196921
Jarno Rajahalme <jarno@covalent.io>
2021-03-24 10:05:47 -0700

    fqdn: Log GCd DNS names
    [ upstream commit e79e66f5551764d54c291f74adc7ab4f5fee7923 ]
    
    Add visibility to the garbage collection of DNS name/IP mappings by
    logging the FQDNs of name entries being deleted.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>
    Signed-off-by: André Martins <andre@cilium.io>

--- daemon/cmd/fqdn.go
+++ daemon/cmd/fqdn.go
@@ -230,9 +230,15 @@ func (d *Daemon) bootstrapFQDN(possibleEndpoints map[uint16]*endpoint.Endpoint, 
 			cfg.Cache.ReplaceFromCacheByNames(namesToClean, caches...)
 
 			metrics.FQDNGarbageCollectorCleanedTotal.Add(float64(len(namesToClean)))
-			log.WithField(logfields.Controller, dnsGCJobName).Infof(
-				"FQDN garbage collector work deleted %d name entries", len(namesToClean))
 			_, err := d.dnsNameManager.ForceGenerateDNS(context.TODO(), namesToClean)
+			namesCount := len(namesToClean)
+			// Limit the amount of info level logging to some sane amount
+			if namesCount > 20 {
+				// namedsToClean is only used for logging after this so we can reslice it in place
+				namesToClean = namesToClean[:20]
+			}
+			log.WithField(logfields.Controller, dnsGCJobName).Infof(
+				"FQDN garbage collector work deleted %d name entries: %s", namesCount, strings.Join(namesToClean, ","))
 			return err
 		},
 		Context: d.ctx,
