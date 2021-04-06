a468e4b751e2dbfbf0bd9b5c7ffd3361a13365f7
Martynas Pumputis <m@lambda.lt>
2021-04-06 11:39:51 +0200

    wireguard: Completely disable rp_filter
    Just having a default route is not enough to pass the rp_filter check in
    the loose mode. So, disable it completely.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- pkg/wireguard/agent/agent.go
+++ pkg/wireguard/agent/agent.go
@@ -98,7 +98,7 @@ func (a *Agent) Init() error {
 	}
 
 	if option.Config.EnableIPv4 {
-		if err := sysctl.Write(fmt.Sprintf("net.ipv4.conf.%s.rp_filter", types.IfaceName), "2"); err != nil {
+		if err := sysctl.Disable(fmt.Sprintf("net.ipv4.conf.%s.rp_filter", types.IfaceName)); err != nil {
 			return nil
 		}
 	}
