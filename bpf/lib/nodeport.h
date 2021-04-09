09e06f84c9b47b0369866d86a530e3fd78627911
Martynas Pumputis <m@lambda.lt>
2021-04-09 16:10:20 +0200

    wireguard: Disable KPR encryption
    After removing the Wireguard tunnel IP, a traffic forwarded by NodePort
    BPF cannot be encrypted as it used to be.
    
    When running Wireguard with KPR, we want to encrypt the following
    NodePort BPF traffic:
    
        1. A request from a client to a service handled by NodePort BPF when
           a remote service endpoint is selected (after SNAT and DNAT).
        2. A reply from the service endpoint to the request above from on
           the remote node.
    
    The 1. is marked for encryption from the NodePort BPF program (once it
    detects that the service endpoint is remote). The encryption of 2. is
    more complicated.
    
    Previously, when we had a Wireguard tunnel IP, the request was SNAT-ed
    to the tunnel IP. This made the reply to automatically go over the
    Wireguard tunnel.  Unfortunately, after removing the Wireguard tunnel
    IP, this is no longer the case. The request is SNAT-ed to a node IP, and
    therefore a reply (src=podIP|dst=nodeIP) is no longer encrypted, as it
    does not go over the Wireguard tunnel.
    
    To fix this, we could mark a packet sent from a pod for the encryption
    if we detect that dst is REMOTE_NODE_ID. Then later on, the packet could
    be routed by the stack to the Wireguard tunnel device (thanks to the
    mark).  Unfortunately, this introduces an asymmetry for non-NodePort
    host2pod traffic, as host2pod will go unencrypted over a native
    device while pod2host will go over the tunnel and will be encrypted.
    
    Because of the asymmetry, disable the KPR encryption until we can
    encrypt host2pod with Wireguard. In the end, the NodePort BPF encryption
    is the host2pod and pod2host encryption.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- bpf/lib/nodeport.h
+++ bpf/lib/nodeport.h
@@ -888,9 +888,6 @@ redo_local:
 	}
 
 	if (!backend_local) {
-#ifdef ENABLE_WIREGUARD
-		set_encrypt_mark(ctx);
-#endif /* ENABLE_WIREGUARD */
 		edt_set_aggregate(ctx, 0);
 		if (nodeport_uses_dsr6(&tuple)) {
 #if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
@@ -1885,12 +1882,6 @@ redo_local:
 	}
 
 	if (!backend_local) {
-#ifdef ENABLE_WIREGUARD
-		/* The request which is to be forwarded needs to go over the
-		 * Wireguard tunnel.
-		 */
-		set_encrypt_mark(ctx);
-#endif /* ENABLE_WIREGUARD */
 		edt_set_aggregate(ctx, 0);
 		if (nodeport_uses_dsr4(&tuple)) {
 #if DSR_ENCAP_MODE == DSR_ENCAP_IPIP
