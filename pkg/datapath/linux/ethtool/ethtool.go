cilium/cilium

commit 2c878a512de941d3557965cdba0f02d84528bc90

cilium: auto-detect network interfaces for IPAMENI case
We need to auto-detect network interfaces in the EKS environment that by
default uses three interfaces eth0, eth1, and eth2. This patch will walk
the list of all devices in the system and attach to any device that is
not a veth or lo device.

This patch resolves the EKS case and leaves other cases for future work.
We could use similar logic with a bit more exclusion logic to handle the
general routing cases as well.

In the process we pull the IPAMENI IPSec configuration into Reinitialize()
path so that if net devices are added/removed we will pick up those at
reinit time. Later work can bring the remaining parts, but that required
more refactoring than I wanted for a bugfix.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/linux/ethtool/ethtool.go
@@ -1,0 +1,97 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ethtool
+
+import (
+	"bytes"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+const (
+	SIOCETHTOOL      = 0x8946
+	ETHTOOL_GDRVINFO = 0x00000003 // Get driver info
+
+	IFNAMSIZ = 16
+)
+
+const (
+	veth = "veth"
+)
+
+type ifreq struct {
+	name [IFNAMSIZ]byte
+	data uintptr
+}
+
+type ethtoolDrvInfo struct {
+	cmd         uint32
+	driver      [32]byte
+	version     [32]byte
+	fwVersion   [32]byte
+	busInfo     [32]byte
+	eromVersion [32]byte
+	reserved2   [12]byte
+	nPrivFlags  uint32
+	nStats      uint32
+	testInfoLen uint32
+	eedumpLen   uint32
+	regdumpLen  uint32
+}
+
+func ethtoolIoctl(iface string, info *ethtoolDrvInfo) error {
+	var ifname [IFNAMSIZ]byte
+
+	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, unix.IPPROTO_IP)
+	if err != nil {
+		return err
+	}
+	copy(ifname[:], iface)
+	req := ifreq{
+		name: ifname,
+		data: uintptr(unsafe.Pointer(info)),
+	}
+	_, _, ep := unix.Syscall(unix.SYS_IOCTL, uintptr(fd), SIOCETHTOOL, uintptr(unsafe.Pointer(&req)))
+	if ep != 0 {
+		return ep
+	}
+	return nil
+}
+
+func GetDeviceName(iface string) (string, error) {
+	info := ethtoolDrvInfo{
+		cmd: ETHTOOL_GDRVINFO,
+	}
+
+	if err := ethtoolIoctl(iface, &info); err != nil {
+		return "", err
+	}
+	return string(bytes.Trim(info.driver[:], "\x00")), nil
+}
+
+func IsVirtualDriver(iface string) (bool, error) {
+	drvName, err := GetDeviceName(iface)
+	if err != nil {
+		return false, err
+	}
+
+	switch drvName {
+	case veth:
+		return true, nil
+	}
+
+	return true, nil
+}

daemon/cmd/daemon_main.go
@@ -1287,7 +1287,12 @@ func initEnv(cmd *cobra.Command) {
 		}
 	}
 
-	if option.Config.EnableIPSec && option.Config.Tunnel == option.TunnelDisabled && len(option.Config.EncryptInterface) == 0 {
+	// IPAMENI IPSec is configured from Reinitialize() to pull in devices
+	// that may be added or removed at runtime.
+	if option.Config.EnableIPSec &&
+		option.Config.Tunnel == option.TunnelDisabled &&
+		len(option.Config.EncryptInterface) == 0 &&
+		option.Config.IPAM != ipamOption.IPAMENI {
 		link, err := linuxdatapath.NodeDeviceNameWithDefaultRoute()
 		if err != nil {
 			log.WithError(err).Fatal("Ipsec default interface lookup failed, consider \"encrypt-interface\" to manually configure interface.")

pkg/datapath/loader/base.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/datapath/alignchecker"
+	"github.com/cilium/cilium/pkg/datapath/linux/ethtool"
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	linuxrouting "github.com/cilium/cilium/pkg/datapath/linux/routing"
@@ -173,6 +174,49 @@ func addENIRules(sysSettings []sysctl.Setting, nodeAddressing datapath.NodeAddre
 	}
 
 	return retSettings, nil
+}
+
+// reinitializeIPSec is used to recompile and load encryption network programs.
+func (l *Loader) reinitializeIPSec(ctx context.Context) error {
+	if !option.Config.EnableIPSec {
+		return nil
+	}
+
+	interfaces := option.Config.EncryptInterface
+	if option.Config.IPAM == ipamOption.IPAMENI {
+		// IPAMENI mode supports multiple network facing interfaces that
+		// will all need Encrypt logic applied in order to decrypt any
+		// received encrypted packets. This logic will attach to all
+		// !veth devices. Only use if user has not configured interfaces.
+		if len(interfaces) == 0 {
+			if links, err := netlink.LinkList(); err == nil {
+				for _, link := range links {
+					isVirtual, err := ethtool.IsVirtualDriver(link.Attrs().Name)
+					if err == nil && !isVirtual {
+						interfaces = append(interfaces, link.Attrs().Name)
+					}
+				}
+			}
+		}
+
+		// For the ENI ipam mode on EKS, this will be the interface that
+		// the router (cilium_host) IP is associated to.
+		if len(option.Config.IPv4PodSubnets) == 0 {
+			if info := node.GetRouterInfo(); info != nil {
+				for _, c := range info.GetIPv4CIDRs() {
+					option.Config.IPv4PodSubnets = append(option.Config.IPv4PodSubnets, &c)
+				}
+			}
+		}
+	}
+
+	// No interfaces is valid in tunnel disabled case
+	if len(interfaces) != 0 {
+		if err := l.replaceNetworkDatapath(ctx, interfaces); err != nil {
+			return fmt.Errorf("failed to load encryption program: %w", err)
+		}
+	}
+	return nil
 }
 
 // Reinitialize (re-)configures the base datapath configuration including global
@@ -392,35 +436,8 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		log.Warning("Cannot check matching of C and Go common struct alignments due to old LLVM/clang version")
 	}
 
-	// Compile and load network programs, currently used for encryption.
-	if option.Config.EnableIPSec {
-		interfaces := option.Config.EncryptInterface
-
-		// For the ENI ipam mode on EKS, this will be the interface that
-		// the router (cilium_host) IP is associated to.
-		if option.Config.IPAM == ipamOption.IPAMENI {
-			if len(interfaces) == 0 {
-				if info := node.GetRouterInfo(); info != nil {
-					mac := info.GetMac()
-					iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
-					if err != nil {
-						log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
-					}
-					interfaces = append(interfaces, iface)
-				}
-			}
-			if len(option.Config.IPv4PodSubnets) == 0 {
-				if info := node.GetRouterInfo(); info != nil {
-					for _, c := range info.GetIPv4CIDRs() {
-						option.Config.IPv4PodSubnets = append(option.Config.IPv4PodSubnets, &c)
-					}
-				}
-				log.Info("Encryption IPv4PodSubnets in-use")
-			}
-		}
-		if err := l.replaceNetworkDatapath(ctx, interfaces); err != nil {
-			log.WithError(err).Fatal("failed to load encryption program")
-		}
+	if err := l.reinitializeIPSec(ctx); err != nil {
+		return err
 	}
 
 	if err := o.Datapath().Node().NodeConfigurationChanged(*o.LocalConfig()); err != nil {

commit 5c49d3cb3481938c13c0ae390fdb4f31e96dadb5

datapath/linux/ethtool: avoid leaking fd in ethtoolIoctl
Close fd after using it to avoid leaking file descriptors.

Fixes: 2c878a512de9 ("cilium: auto-detect network interfaces for IPAMENI case")
Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/datapath/linux/ethtool/ethtool.go
@@ -59,6 +59,8 @@ func ethtoolIoctl(iface string, info *ethtoolDrvInfo) error {
 	if err != nil {
 		return err
 	}
+	defer unix.Close(fd)
+
 	copy(ifname[:], iface)
 	req := ifreq{
 		name: ifname,