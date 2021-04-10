e632f2a49840b02c4712ca36667370c8771e0631
Tobias Klauser <tobias@cilium.io>
2021-04-10 15:13:07 +0200

    vendor: update github.com/google/gops to v0.3.14
    [ upstream commit b8d65ad9c99ec187f8967756b848244732807250 ]
    
    Manually updated gops to v0.3.14 using:
    
      go get github.com/google/gops@v0.3.14
      go mod tidy
      go mod vendor
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- vendor/github.com/google/gops/agent/agent.go
+++ vendor/github.com/google/gops/agent/agent.go
@@ -8,6 +8,7 @@ package agent
 
 import (
 	"bufio"
+	"context"
 	"encoding/binary"
 	"fmt"
 	"io"
@@ -55,6 +56,13 @@ type Options struct {
 	// can call Close before shutting down.
 	// Optional.
 	ShutdownCleanup bool
+
+	// ReuseSocketAddrAndPort determines whether the SO_REUSEADDR and
+	// SO_REUSEADDR socket options should be set on the listening socket of
+	// the agent. This option is only effective on unix-like OSes and if
+	// Addr is set to a fixed host:port.
+	// Optional.
+	ReuseSocketAddrAndPort bool
 }
 
 // Listen starts the gops agent on a host process. Once agent started, users
@@ -96,11 +104,14 @@ func Listen(opts Options) error {
 	if addr == "" {
 		addr = defaultAddr
 	}
-	ln, err := net.Listen("tcp", addr)
+	var lc net.ListenConfig
+	if opts.ReuseSocketAddrAndPort {
+		lc.Control = setsockoptReuseAddrAndPort
+	}
+	listener, err = lc.Listen(context.Background(), "tcp", addr)
 	if err != nil {
 		return err
 	}
-	listener = ln
 	port := listener.Addr().(*net.TCPAddr).Port
 	portfile = fmt.Sprintf("%s/%d", gopsdir, os.Getpid())
 	err = ioutil.WriteFile(portfile, []byte(strconv.Itoa(port)), os.ModePerm)
@@ -222,7 +233,10 @@ func handle(conn io.ReadWriter, msg []byte) error {
 		fmt.Fprintf(conn, "last-gc: %v\n", lastGC)
 		fmt.Fprintf(conn, "gc-pause-total: %v\n", time.Duration(s.PauseTotalNs))
 		fmt.Fprintf(conn, "gc-pause: %v\n", s.PauseNs[(s.NumGC+255)%256])
+		fmt.Fprintf(conn, "gc-pause-end: %v\n", s.PauseEnd[(s.NumGC+255)%256])
 		fmt.Fprintf(conn, "num-gc: %v\n", s.NumGC)
+		fmt.Fprintf(conn, "num-forced-gc: %v\n", s.NumForcedGC)
+		fmt.Fprintf(conn, "gc-cpu-fraction: %v\n", s.GCCPUFraction)
 		fmt.Fprintf(conn, "enable-gc: %v\n", s.EnableGC)
 		fmt.Fprintf(conn, "debug-gc: %v\n", s.DebugGC)
 	case signal.Version:
