bdaf6588c92bdbaee5628d3623870ca0153fddfd
Tobias Klauser <tobias@cilium.io>
2021-04-14 16:36:32 +0200

    api/v1/observer, hubble/exporter: introduce and use ExportEvent type
    The exporter currently uses GetFlowsResponse as a catch-all type for
    events export. However, in the successive commits agent events and debug
    events will be moved out of the GetFlows API into their own API. Thus,
    the exporter needs its own catch-all type to still be able to export all
    events.
    
    Add ExportEvent for this purpose which is a 1:1 copy of GetFlowsResponse
    at its current state.
    
    For convertions add eventToExportEvent copied from
    EventToGetFlowsResponse and adjusted to the new ExportEvent type, along
    with the respective tests.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- api/v1/observer/observer.pb.json.go
+++ api/v1/observer/observer.pb.json.go
@@ -152,3 +152,21 @@ func (msg *TLS) UnmarshalJSON(b []byte) error {
 		AllowUnknownFields: false,
 	}).Unmarshal(bytes.NewReader(b), msg)
 }
+
+// MarshalJSON implements json.Marshaler
+func (msg *ExportEvent) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	err := (&jsonpb.Marshaler{
+		EnumsAsInts:  false,
+		EmitDefaults: false,
+		OrigName:     true,
+	}).Marshal(&buf, msg)
+	return buf.Bytes(), err
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *ExportEvent) UnmarshalJSON(b []byte) error {
+	return (&jsonpb.Unmarshaler{
+		AllowUnknownFields: false,
+	}).Unmarshal(bytes.NewReader(b), msg)
+}
