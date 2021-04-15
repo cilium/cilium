cilium/cilium

commit c4581b37dc1ca2a41d4c1437ea3694d0358f23ad

loader : Log upsert and remove route errors
Fixes : #15282
Signed-off-by: Gaurav Genani <h3llix.pvt@gmail.com>

pkg/datapath/loader/loader.go
@@ -352,18 +352,32 @@ func (l *Loader) reloadDatapath(ctx context.Context, ep datapath.Endpoint, dirs 
 	}
 
 	if ip := ep.IPv4Address(); ip.IsSet() {
+		scopedLog := ep.Logger(Subsystem).WithFields(logrus.Fields{
+			logfields.Veth: ep.InterfaceName(),
+		})
 		if ep.RequireEndpointRoute() {
-			upsertEndpointRoute(ep, *ip.IPNet(32))
+			if err := upsertEndpointRoute(ep, *ip.IPNet(32)); err != nil {
+				scopedLog.WithError(err).Warn("Failed to upsert route")
+			}
 		} else {
-			removeEndpointRoute(ep, *ip.IPNet(32))
+			if err := removeEndpointRoute(ep, *ip.IPNet(32)); err != nil {
+				scopedLog.WithError(err).Warn("Failed to remove route")
+			}
 		}
 	}
 
 	if ip := ep.IPv6Address(); ip.IsSet() {
+		scopedLog := ep.Logger(Subsystem).WithFields(logrus.Fields{
+			logfields.Veth: ep.InterfaceName(),
+		})
 		if ep.RequireEndpointRoute() {
-			upsertEndpointRoute(ep, *ip.IPNet(128))
+			if err := upsertEndpointRoute(ep, *ip.IPNet(128)); err != nil {
+				scopedLog.WithError(err).Warn("Failed to upsert route")
+			}
 		} else {
-			removeEndpointRoute(ep, *ip.IPNet(128))
+			if err := removeEndpointRoute(ep, *ip.IPNet(128)); err != nil {
+				scopedLog.WithError(err).Warn("Failed to remove route")
+			}
 		}
 	}
 

commit cc56f796ec0ad4f5053c1fe02f199d2c3770852a

pkg/datapath: ignore certain error types on route delete
When removing endpoint routes it is not always necessary to log errors.
One example would be in case the returned error from the kernel would be
"no such process"

Fixes: c4581b37dc1c ("loader : Log upsert and remove route errors")
Signed-off-by: André Martins <andre@cilium.io>

pkg/datapath/loader/loader.go
@@ -16,12 +16,14 @@ package loader
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"os"
 	"path"
 	"reflect"
 	"sync"
+	"syscall"
 
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
@@ -360,7 +362,7 @@ func (l *Loader) reloadDatapath(ctx context.Context, ep datapath.Endpoint, dirs 
 				scopedLog.WithError(err).Warn("Failed to upsert route")
 			}
 		} else {
-			if err := removeEndpointRoute(ep, *ip.IPNet(32)); err != nil {
+			if err := removeEndpointRoute(ep, *ip.IPNet(32)); err != nil && !errors.Is(err, syscall.ESRCH) {
 				scopedLog.WithError(err).Warn("Failed to remove route")
 			}
 		}
@@ -375,7 +377,7 @@ func (l *Loader) reloadDatapath(ctx context.Context, ep datapath.Endpoint, dirs 
 				scopedLog.WithError(err).Warn("Failed to upsert route")
 			}
 		} else {
-			if err := removeEndpointRoute(ep, *ip.IPNet(128)); err != nil {
+			if err := removeEndpointRoute(ep, *ip.IPNet(128)); err != nil && !errors.Is(err, syscall.ESRCH) {
 				scopedLog.WithError(err).Warn("Failed to remove route")
 			}
 		}