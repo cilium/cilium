cilium/cilium

commit 501332b9e23f0f4122ab1cc3576d4619826e4f11

bpf: Remove DROP_ALL define
Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/lib/policy.h
@@ -48,9 +48,6 @@ __policy_can_access(void *map, struct __sk_buff *skb, __u32 identity,
 		    __u16 dport, __u8 proto, size_t cidr_addr_size,
 		    void *cidr_addr, int dir, bool is_fragment)
 {
-#ifdef DROP_ALL
-	return DROP_POLICY;
-#else
 	struct policy_entry *policy;
 
 	struct policy_key key = {
@@ -110,7 +107,6 @@ get_proxy_port:
 	}
 allow:
 	return TC_ACT_OK;
-#endif /* DROP_ALL */
 }
 
 /**
@@ -132,9 +128,6 @@ policy_can_access_ingress(struct __sk_buff *skb, __u32 src_identity,
 			  __u16 dport, __u8 proto, size_t cidr_addr_size,
 			  void *cidr_addr, bool is_fragment)
 {
-#ifdef DROP_ALL
-	return DROP_POLICY;
-#else
 	int ret;
 
 	ret = __policy_can_access(&POLICY_MAP, skb, src_identity, dport,
@@ -150,7 +143,6 @@ policy_can_access_ingress(struct __sk_buff *skb, __u32 src_identity,
 #else
 	return TC_ACT_OK;
 #endif
-#endif /* DROP_ALL */
 }
 
 #if defined LXC_ID
@@ -158,9 +150,6 @@ policy_can_access_ingress(struct __sk_buff *skb, __u32 src_identity,
 static inline int __inline__
 policy_can_egress(struct __sk_buff *skb, __u32 identity, __u16 dport, __u8 proto)
 {
-#ifdef DROP_ALL
-	return DROP_POLICY;
-#else
 	int ret = __policy_can_access(&POLICY_MAP, skb, identity, dport, proto,
 				      0, NULL, CT_EGRESS, false);
 	if (ret >= 0)
@@ -171,29 +160,20 @@ policy_can_egress(struct __sk_buff *skb, __u32 identity, __u16 dport, __u8 proto
 	return DROP_POLICY;
 #endif
 	return TC_ACT_OK;
-#endif /* DROP_ALL */
 }
 
 static inline int policy_can_egress6(struct __sk_buff *skb,
 				     struct ipv6_ct_tuple *tuple,
 				     __u32 identity, union v6addr *daddr)
 {
-#ifdef DROP_ALL
-	return DROP_POLICY;
-#else
 	return policy_can_egress(skb, identity, tuple->dport, tuple->nexthdr);
-#endif /* DROP_ALL */
 }
 
 static inline int policy_can_egress4(struct __sk_buff *skb,
 				     struct ipv4_ct_tuple *tuple,
 				     __u32 identity, __be32 daddr)
 {
-#ifdef DROP_ALL
-	return DROP_POLICY;
-#else
 	return policy_can_egress(skb, identity, tuple->dport, tuple->nexthdr);
-#endif /* DROP_ALL */
 }
 
 #else /* LXC_ID */
@@ -202,26 +182,16 @@ static inline int
 policy_can_egress6(struct __sk_buff *skb, struct ipv6_ct_tuple *tuple,
 		   __u32 identity, union v6addr *daddr)
 {
-#ifdef DROP_ALL
-	return DROP_POLICY;
-#else
 	return TC_ACT_OK;
-#endif
 }
 
 static inline int
 policy_can_egress4(struct __sk_buff *skb, struct ipv4_ct_tuple *tuple,
 		   __u32 identity, __be32 daddr)
 {
-#ifdef DROP_ALL
-	return DROP_POLICY;
-#else
 	return TC_ACT_OK;
-#endif
 }
 #endif /* LXC_ID */
-
-#if !defined DROP_ALL
 
 /**
  * Mark skb to skip policy enforcement
@@ -258,12 +228,7 @@ static inline void policy_clear_mark(struct __sk_buff *skb)
 
 static inline int is_policy_skip(struct __sk_buff *skb)
 {
-#ifdef DROP_ALL
-	return 0;
-#else
 	return 1;
-#endif
 }
-#endif /* !defined DROP_ALL */
 
 #endif

bpf/Makefile
@@ -55,7 +55,6 @@ bpf_lb.o: bpf_lb.c $(LIB)
 
 LXC_OPTIONS = \
 	 -DSKIP_DEBUG \
-	 -DDROP_ALL \
 	 -DHAVE_LPM_MAP_TYPE \
 	 -DHAVE_LRU_MAP_TYPE
 

bpf/bpf_lxc.c
@@ -689,14 +689,6 @@ int handle_ingress(struct __sk_buff *skb)
 
 	send_trace_notify(skb, TRACE_FROM_LXC, SECLABEL, 0, 0, 0, 0, true);
 
-#ifdef DROP_ALL
-	if (skb->protocol == bpf_htons(ETH_P_ARP)) {
-		ep_tail_call(skb, CILIUM_CALL_ARP);
-		ret = DROP_MISSED_TAIL_CALL;
-	} else if (1) {
-		ret = DROP_POLICY;
-	} else {
-#endif
 	switch (skb->protocol) {
 	case bpf_htons(ETH_P_IPV6):
 		ep_tail_call(skb, CILIUM_CALL_IPV6_FROM_LXC);
@@ -716,10 +708,6 @@ int handle_ingress(struct __sk_buff *skb)
 	default:
 		ret = DROP_UNKNOWN_L3;
 	}
-
-#ifdef DROP_ALL
-	}
-#endif
 
 	if (IS_ERR(ret))
 		return send_drop_notify(skb, SECLABEL, 0, 0, 0, ret, TC_ACT_SHOT,
@@ -1016,10 +1004,6 @@ __section_tail(CILIUM_MAP_POLICY, LXC_ID) int handle_policy(struct __sk_buff *sk
 	int ret, ifindex = skb->cb[CB_IFINDEX];
 	__u32 src_label = skb->cb[CB_SRC_LABEL];
 
-#ifdef DROP_ALL
-	ret = DROP_POLICY;
-	if (0) {
-#endif
 	switch (skb->protocol) {
 	case bpf_htons(ETH_P_IPV6):
 		ep_tail_call(skb, CILIUM_CALL_IPV6_TO_LXC);
@@ -1037,9 +1021,6 @@ __section_tail(CILIUM_MAP_POLICY, LXC_ID) int handle_policy(struct __sk_buff *sk
 		ret = DROP_UNKNOWN_L3;
 		break;
 	}
-#ifdef DROP_ALL
-	}
-#endif
 
 	if (IS_ERR(ret))
 		return send_drop_notify(skb, src_label, SECLABEL, LXC_ID,

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=c6cb18edc7f1dcd70f27cd4333a8d7ef0f157a1e
+GO_BINDATA_SHA1SUM=16cae2c6c75c6d91665541389b9ae56fab92b670
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

pkg/endpoint/bpf.go
@@ -153,18 +153,6 @@ func (e *Endpoint) writeHeaderfile(prefix string, owner Owner) error {
 	}
 	fw.WriteString(" */\n\n")
 
-	// If policy has not been derived or calculated yet, all packets must
-	// be dropped until the policy of the endpoint has been determined,
-	// except when it is known that the current policy will not drop anything,
-	// which is true when:
-	// - policy enforcement mode is "never"
-	// - policy enforcement mode is "default" and no policies are loaded
-	if !e.PolicyCalculated &&
-		!(owner.PolicyEnforcement() == option.NeverEnforce) &&
-		!(owner.PolicyEnforcement() == option.DefaultEnforcement && owner.GetPolicyRepository().Empty()) {
-		fw.WriteString("#define DROP_ALL\n")
-	}
-
 	fw.WriteString(common.FmtDefineAddress("LXC_MAC", e.LXCMAC))
 	fw.WriteString(common.FmtDefineComma("LXC_IP", e.IPv6))
 	if e.IPv4 != nil {

commit 89b44e8a02b23ab025741f96d07f8e55800616f8

bpf: Fix defines in policy.h
501332b ("bpf: Remove DROP_ALL define") forgot to remove an #else
statement, resulting in a compilation error if policy.h is included
twice.

Fixes: 501332b ("bpf: Remove DROP_ALL define")
Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/lib/policy.h
@@ -295,12 +295,4 @@ static __always_inline void policy_clear_mark(struct __ctx_buff *ctx)
 	ctx_store_meta(ctx, CB_POLICY, 0);
 }
 #endif /* SOCKMAP */
-#else
-static __always_inline void policy_mark_skip(struct __ctx_buff *ctx)
-{
-}
-
-static __always_inline void policy_clear_mark(struct __ctx_buff *ctx)
-{
-}
 #endif