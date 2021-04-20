cilium/cilium

commit 8d0211c37537b7582219a8b92f0aa4429c8f6316

pkg/identity: Watch and update labels for the host
This commit adds a k8s watcher for label updates on the host. It allows
node network policies to select the nodes based on labels. For now, the
same label filters are used for the nodes as for the labels.

Whatever the labels it receives, because we know there can be only one
host endpoint per node, the host endpoint will always retain its
security ID of 1. We therefore don't need to reload the host endpoint's
datapaths on label updates.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/k8s/watchers/node.go
@@ -1,0 +1,79 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package watchers
+
+import (
+	"github.com/cilium/cilium/pkg/comparator"
+	"github.com/cilium/cilium/pkg/k8s"
+	"github.com/cilium/cilium/pkg/k8s/informer"
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/core/v1"
+	nodeTypes "github.com/cilium/cilium/pkg/node/types"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+func (k *K8sWatcher) nodesInit(k8sClient kubernetes.Interface) {
+	_, nodeController := informer.NewInformer(
+		cache.NewListWatchFromClient(k8sClient.CoreV1().RESTClient(),
+			"nodes", v1.NamespaceAll, fields.ParseSelectorOrDie("metadata.name="+nodeTypes.GetName())),
+		&slim_corev1.Node{},
+		0,
+		cache.ResourceEventHandlerFuncs{
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				var valid, equal bool
+				if oldNode := k8s.ObjToV1Node(oldObj); oldNode != nil {
+					valid = true
+					if newNode := k8s.ObjToV1Node(newObj); newNode != nil {
+						oldNodeLabels := oldNode.GetLabels()
+						newNodeLabels := newNode.GetLabels()
+						if comparator.MapStringEquals(oldNodeLabels, newNodeLabels) {
+							equal = true
+						} else {
+							err := k.updateK8sNodeV1(oldNode, newNode)
+							k.K8sEventProcessed(metricNode, metricUpdate, err == nil)
+						}
+					}
+				}
+				k.K8sEventReceived(metricNode, metricUpdate, valid, equal)
+			},
+		},
+		nil,
+	)
+
+	k.blockWaitGroupToSyncResources(wait.NeverStop, nil, nodeController, k8sAPIGroupNodeV1Core)
+	go nodeController.Run(wait.NeverStop)
+	k.k8sAPIGroups.addAPI(k8sAPIGroupNodeV1Core)
+}
+
+func (k *K8sWatcher) updateK8sNodeV1(oldK8sNode, newK8sNode *slim_corev1.Node) error {
+	oldNodeLabels := oldK8sNode.GetLabels()
+	newNodeLabels := newK8sNode.GetLabels()
+
+	nodeEP := k.endpointManager.GetHostEndpoint()
+	if nodeEP == nil {
+		log.Error("Host endpoint not found")
+		return nil
+	}
+
+	err := updateEndpointLabels(nodeEP, oldNodeLabels, newNodeLabels)
+	if err != nil {
+		return err
+	}
+	return nil
+}

pkg/endpointmanager/manager.go
@@ -27,7 +27,9 @@ import (
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 	"github.com/cilium/cilium/pkg/endpointmanager/idallocator"
 	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/labelsfilter"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -474,10 +476,24 @@ func (mgr *EndpointManager) AddHostEndpoint(ctx context.Context, owner regenerat
 		return err
 	}
 
+	epLabels := labels.Labels{}
+	epLabels.MergeLabels(labels.LabelHost)
+
+	if k8s.IsEnabled() {
+		// Retrieve k8s labels.
+		if k8sNode, err := k8s.GetNode(k8s.Client(), nodeName); err != nil {
+			log.WithError(err).Warning("Kubernetes node resource representing own node is not available, cannot set Labels")
+		} else {
+			newLabels := labels.Map2Labels(k8sNode.GetLabels(), labels.LabelSourceK8s)
+			newIdtyLabels, _ := labelsfilter.Filter(newLabels)
+			epLabels.MergeLabels(newIdtyLabels)
+		}
+	}
+
 	// Give the endpoint a security identity
 	newCtx, cancel := context.WithTimeout(ctx, launchTime)
 	defer cancel()
-	ep.UpdateLabels(newCtx, labels.LabelHost, nil, true)
+	ep.UpdateLabels(newCtx, epLabels, nil, true)
 	if newCtx.Err() == context.DeadlineExceeded {
 		log.WithError(newCtx.Err()).Warning("Timed out while updating security identify for host endpoint")
 	}

pkg/identity/cache/cache_test.go
@@ -115,7 +115,11 @@ func (s *IdentityCacheTestSuite) TestLookupReservedIdentityByLabels(c *C) {
 					"id.foo":                   labels.ParseLabel("id.foo"),
 				},
 			},
-			want: nil,
+			want: identity.NewIdentity(identity.ReservedIdentityHost, labels.Labels{
+				labels.LabelSourceReserved: labels.ParseLabel("reserved:host"),
+				"id.foo":                   labels.ParseLabel("id.foo"),
+			},
+			),
 		},
 		{
 			name: "well-known-kvstore",

pkg/identity/identity.go
@@ -235,15 +235,26 @@ func LookupReservedIdentityByLabels(lbls labels.Labels) *Identity {
 			// If a fixed identity was not found then we return nil to avoid
 			// falling to a reserved identity.
 			return nil
-		// If it doesn't contain a fixed-identity then make sure the set of
-		// labels only contains a single label and that label is of the reserved
-		// type. This is to prevent users from adding cilium-reserved labels
-		// into the workloads.
+
 		case lbl.Source == labels.LabelSourceReserved:
+			// If it contains the reserved, local host identity, return it with
+			// the new list of labels. This is to ensure the local node retains
+			// this identity regardless of label changes.
+			id := GetReservedID(lbl.Key)
+			if id == ReservedIdentityHost {
+				identity := NewIdentity(ReservedIdentityHost, lbls)
+				// Pre-calculate the SHA256 hash.
+				identity.GetLabelsSHA256()
+				return identity
+			}
+
+			// If it doesn't contain a fixed-identity then make sure the set of
+			// labels only contains a single label and that label is of the
+			// reserved type. This is to prevent users from adding
+			// cilium-reserved labels into the workloads.
 			if len(lbls) != 1 {
 				return nil
 			}
-			id := GetReservedID(lbl.Key)
 			if id != IdentityUnknown && !IsUserReservedIdentity(id) {
 				return LookupReservedIdentity(id)
 			}

pkg/identity/identitymanager/manager.go
@@ -113,7 +113,9 @@ func (idm *IdentityManager) RemoveOldAddNew(old, new *identity.Identity) {
 	if old == nil && new == nil {
 		return
 	}
-	if old != nil && new != nil && old.ID == new.ID {
+	// The host endpoint will always retain its reserved ID, but its labels may
+	// change so we need to update its identity.
+	if old != nil && new != nil && old.ID == new.ID && new.ID != identity.ReservedIdentityHost {
 		return
 	}
 

pkg/identity/identitymanager/manager_test.go
@@ -86,6 +86,25 @@ func (s *IdentityManagerTestSuite) TestIdentityManagerLifecycle(c *C) {
 	c.Assert(idm.identities[barIdentity.ID].refCount, Equals, uint(2))
 }
 
+func (s *IdentityManagerTestSuite) TestHostIdentityLifecycle(c *C) {
+	idm := NewIdentityManager()
+	c.Assert(idm.identities, Not(IsNil))
+
+	hostIdentity := identity.NewIdentity(identity.ReservedIdentityHost, labels.LabelHost)
+	_, exists := idm.identities[hostIdentity.ID]
+	c.Assert(exists, Equals, false)
+
+	idm.Add(hostIdentity)
+	c.Assert(idm.identities[hostIdentity.ID].refCount, Equals, uint(1))
+
+	newHostLabels := labels.NewLabelsFromModel([]string{"id=foo"})
+	newHostLabels.MergeLabels(labels.LabelHost)
+	newHostIdentity := identity.NewIdentity(identity.ReservedIdentityHost, newHostLabels)
+	idm.RemoveOldAddNew(hostIdentity, newHostIdentity)
+	c.Assert(idm.identities[hostIdentity.ID].refCount, Equals, uint(1))
+	c.Assert(idm.identities[hostIdentity.ID].identity, checker.DeepEquals, newHostIdentity)
+}
+
 type identityManagerObserver struct {
 	added   []identity.NumericIdentity
 	removed []identity.NumericIdentity

pkg/k8s/watchers/watcher.go
@@ -70,6 +70,7 @@ const (
 	metricCiliumNode     = "CiliumNode"
 	metricCiliumEndpoint = "CiliumEndpoint"
 	metricPod            = "Pod"
+	metricNode           = "Node"
 	metricService        = "Service"
 	metricCreate         = "create"
 	metricDelete         = "delete"
@@ -105,6 +106,7 @@ var (
 
 type endpointManager interface {
 	GetEndpoints() []*endpoint.Endpoint
+	GetHostEndpoint() *endpoint.Endpoint
 	LookupPodName(string) *endpoint.Endpoint
 	WaitForEndpointsAtPolicyRev(ctx context.Context, rev uint64) error
 }
@@ -463,6 +465,9 @@ func (k *K8sWatcher) EnableK8sWatcher(queueSize uint) error {
 	// kubernetes pods
 	asyncControllers.Add(1)
 	go k.podsInit(k8s.WatcherCli(), asyncControllers)
+
+	// kubernetes nodes
+	k.nodesInit(k8s.WatcherCli())
 
 	// kubernetes namespaces
 	asyncControllers.Add(1)

commit 7e3a5428d44b6ae044c417eff3e3101155324e5f

watchers: Update node labels on ADD events
When the agent first receives the Node object from the kube-apiserver,
it may receive it via an ADD event. We therefore need to update node
labels on such events.

Fixes: 8d0211c ("pkg/identity: Watch and update labels for the host")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/k8s/watchers/node.go
@@ -19,6 +19,7 @@ import (
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	"github.com/cilium/cilium/pkg/node"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 
@@ -37,11 +38,13 @@ func (k *K8sWatcher) nodesInit(k8sClient kubernetes.Interface) {
 		0,
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
+				var valid bool
 				if node := k8s.ObjToV1Node(obj); node != nil {
-					if option.Config.BGPAnnounceLBIP {
-						k.bgpSpeakerManager.OnUpdateNode(node) // Need to seed the BGP speaker
-					}
+					valid = true
+					err := k.updateK8sNodeV1(nil, node)
+					k.K8sEventProcessed(metricNode, metricCreate, err == nil)
 				}
+				k.K8sEventReceived(metricNode, metricCreate, valid, false)
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
 				var valid, equal bool
@@ -70,12 +73,16 @@ func (k *K8sWatcher) nodesInit(k8sClient kubernetes.Interface) {
 }
 
 func (k *K8sWatcher) updateK8sNodeV1(oldK8sNode, newK8sNode *slim_corev1.Node) error {
-	oldNodeLabels := oldK8sNode.GetLabels()
+	var oldNodeLabels map[string]string
+	if oldK8sNode != nil {
+		oldNodeLabels = oldK8sNode.GetLabels()
+	}
 	newNodeLabels := newK8sNode.GetLabels()
 
 	nodeEP := k.endpointManager.GetHostEndpoint()
 	if nodeEP == nil {
-		log.Error("Host endpoint not found")
+		log.Debug("Host endpoint not found, updating node labels")
+		node.SetLabels(newNodeLabels)
 		return nil
 	}
 