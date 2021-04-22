b5e7439d7b6669d52f724e3969a7d992c28cb440
Martynas Pumputis <m@lambda.lt>
2021-04-22 18:36:55 +0200

    daemon: Add --arping-refresh-period
    [ upstream commit e779e96b4e07d380641bce9e9db2f1dc80765a73 ]
    
    Instead of deriving the value from
    net.ipv4.neigh.default.base_reachable_time_ms which is 30s by default
    use a user specified value for the ARP periodic refresher.
    
    Running ARP refresher every 15-45s is too often, as in most clusters
    node hw addrs don't change at all.
    
    Users who have highly dynamic environments will be able to schedule the
    refresher in higher frequency by using the flag.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -221,6 +221,9 @@ func init() {
 	flags.Bool(option.BlacklistConflictingRoutes, defaults.BlacklistConflictingRoutes, "Don't blacklist IP allocations conflicting with local non-cilium routes")
 	option.BindEnv(option.BlacklistConflictingRoutes)
 
+	flags.Duration(option.ARPPingRefreshPeriod, 5*time.Minute, "Period for remote node ARP entry refresh (set 0 to disable)")
+	option.BindEnv(option.ARPPingRefreshPeriod)
+
 	flags.Bool(option.AutoCreateCiliumNodeResource, defaults.AutoCreateCiliumNodeResource, "Automatically create CiliumNode resource for own node on startup")
 	option.BindEnv(option.AutoCreateCiliumNodeResource)
 
@@ -1477,7 +1480,7 @@ func runDaemon() {
 	}
 
 	// Start periodical arping to refresh neighbor table
-	if d.datapath.Node().NodeNeighDiscoveryEnabled() {
+	if d.datapath.Node().NodeNeighDiscoveryEnabled() && option.Config.ARPPingRefreshPeriod != 0 {
 		d.nodeDiscovery.Manager.StartNeighborRefresh(d.datapath.Node())
 	}
 
