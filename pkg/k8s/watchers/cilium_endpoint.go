cilium/cilium

commit da35c88eb9f829b7f39228530815b53df23e11cd

k8s/watchers: don't silently ignore (*K8sWatcher).updatePodHostData error
In (*K8sWatcher).addK8sPodV1, the error return value of
(*K8sWatcher).updatePodHostData is ignored due to the fact that the bool
return value is always true if there was an error. This leads to
a debug message being printed rather than a warning. Also the
error is not propagated to callers of (*K8sWatcher).addK8sPodV1, which
in one case in (*K8sWatcher).createPodController leads to metrics not
being updated appropriately.

The bool return value of (*K8sWatcher).createPodController is anyway
redundant, so remove it. Instead, check the error explicitly at the call
site and log and propagate it accordingly.

Also move the host network check out of updatePodHostData as it is not
considered a warning and can be skipped. This avoids the unnecessary
podIPs lookup in that case.

Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

pkg/k8s/watchers/pod.go
@@ -217,12 +217,15 @@ func (k *K8sWatcher) addK8sPodV1(pod *slim_corev1.Pod) error {
 		return k.deleteK8sPodV1(pod)
 	}
 
-	skipped := false
+	if pod.Spec.HostNetwork {
+		logger.Debug("pod is using host networking")
+		return nil
+	}
+
 	var err error
 	podIPs := k8sUtils.ValidIPs(pod.Status)
-
 	if len(podIPs) > 0 {
-		skipped, err = k.updatePodHostData(pod, podIPs)
+		err = k.updatePodHostData(pod, podIPs)
 
 		// There might be duplicate callbacks here since this function is also
 		// called from updateK8sPodV1, the consumer will need to handle the duplicate
@@ -233,16 +236,12 @@ func (k *K8sWatcher) addK8sPodV1(pod *slim_corev1.Pod) error {
 		}
 	}
 
-	switch {
-	case skipped:
-		logger.WithError(err).Debug("Skipped ipcache map update on pod add")
-		return nil
-	case err != nil:
+	if err != nil {
 		logger.WithError(err).Warning("Unable to update ipcache map entry on pod add")
-	default:
-		logger.Debug("Updated ipcache map entry on pod add")
+		return err
 	}
-	return err
+	logger.Debug("Updated ipcache map entry on pod add")
+	return nil
 }
 
 func (k *K8sWatcher) updateK8sPodV1(oldK8sPod, newK8sPod *slim_corev1.Pod) error {
@@ -640,19 +639,15 @@ func (k *K8sWatcher) DeleteHostPortMapping(pod *slim_corev1.Pod, podIPs []string
 	return nil
 }
 
-func (k *K8sWatcher) updatePodHostData(pod *slim_corev1.Pod, podIPs []string) (bool, error) {
-	if pod.Spec.HostNetwork {
-		return true, fmt.Errorf("pod is using host networking")
-	}
-
+func (k *K8sWatcher) updatePodHostData(pod *slim_corev1.Pod, podIPs []string) error {
 	err := k.UpsertHostPortMapping(pod, podIPs)
 	if err != nil {
-		return true, fmt.Errorf("cannot upsert hostPort for PodIPs: %s", podIPs)
+		return fmt.Errorf("cannot upsert hostPort for PodIPs: %s", podIPs)
 	}
 
 	hostIP := net.ParseIP(pod.Status.HostIP)
 	if hostIP == nil {
-		return true, fmt.Errorf("no/invalid HostIP: %s", pod.Status.HostIP)
+		return fmt.Errorf("no/invalid HostIP: %s", pod.Status.HostIP)
 	}
 
 	hostKey := node.GetIPsecKeyIdentity()
@@ -670,10 +665,10 @@ func (k *K8sWatcher) updatePodHostData(pod *slim_corev1.Pod, podIPs []string) (b
 			}
 			p, err := u8proto.ParseProtocol(string(port.Protocol))
 			if err != nil {
-				return true, fmt.Errorf("ContainerPort: invalid protocol: %s", port.Protocol)
+				return fmt.Errorf("ContainerPort: invalid protocol: %s", port.Protocol)
 			}
 			if port.ContainerPort < 1 || port.ContainerPort > 65535 {
-				return true, fmt.Errorf("ContainerPort: invalid port: %d", port.ContainerPort)
+				return fmt.Errorf("ContainerPort: invalid port: %d", port.ContainerPort)
 			}
 			if k8sMeta.NamedPorts == nil {
 				k8sMeta.NamedPorts = make(policy.NamedPortMap)
@@ -703,10 +698,10 @@ func (k *K8sWatcher) updatePodHostData(pod *slim_corev1.Pod, podIPs []string) (b
 		}
 	}
 	if len(errs) != 0 {
-		return true, errors.New(strings.Join(errs, ", "))
+		return errors.New(strings.Join(errs, ", "))
 	}
 
-	return false, nil
+	return nil
 }
 
 func (k *K8sWatcher) deletePodHostData(pod *slim_corev1.Pod) (bool, error) {

commit 3b3887c9e04f8ff89a285cbd681227311c288b2d

pkg/k8s: ignore certain ipcache errors
Certain errors can be ignored when upserting IP addresses into the
ipcache. Thus, there is need to populate the logs with some red herring
warnings.

Fixes: da35c88eb9f8 ("k8s/watchers: don't silently ignore (*K8sWatcher).updatePodHostData error")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/cilium_endpoint.go
@@ -194,7 +194,7 @@ func (k *K8sWatcher) endpointUpdated(oldEndpoint, endpoint *types.CiliumEndpoint
 	for _, pair := range endpoint.Networking.Addressing {
 		if pair.IPV4 != "" {
 			ipsAdded = append(ipsAdded, pair.IPV4)
-				portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV4, nodeIP, encryptionKey, k8sMeta,
+			portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV4, nodeIP, encryptionKey, k8sMeta,
 				ipcache.Identity{ID: id, Source: source.CustomResource})
 			if portsChanged {
 				namedPortsChanged = true
@@ -203,7 +203,7 @@ func (k *K8sWatcher) endpointUpdated(oldEndpoint, endpoint *types.CiliumEndpoint
 
 		if pair.IPV6 != "" {
 			ipsAdded = append(ipsAdded, pair.IPV6)
-				portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV6, nodeIP, encryptionKey, k8sMeta,
+			portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV6, nodeIP, encryptionKey, k8sMeta,
 				ipcache.Identity{ID: id, Source: source.CustomResource})
 			if portsChanged {
 				namedPortsChanged = true

pkg/k8s/watchers/pod.go
@@ -755,7 +755,21 @@ func (k *K8sWatcher) updatePodHostData(oldPod, newPod *slim_corev1.Pod, oldPodIP
 			namedPortsChanged = true
 		}
 		if err != nil {
-			errs = append(errs, fmt.Sprintf("ipcache entry for podIP %s owned by kvstore or agent: %s", podIP, err))
+			// It is expected to receive error overwrites where the existing
+			// source is the KVStore, this can happen as KVStore event
+			// propagation can usually be faster than k8s event propagation.
+			// It is also expected to receive an error overwrite where the
+			// existing source is local since cilium-agent receives events for
+			// local pods.
+			if !errors.Is(err, &ipcache.ErrOverwrite{
+				ExistingSrc: source.KVStore,
+				NewSrc:      source.Kubernetes,
+			}) || !errors.Is(err, &ipcache.ErrOverwrite{
+				ExistingSrc: source.Local,
+				NewSrc:      source.Kubernetes,
+			}) {
+				errs = append(errs, fmt.Sprintf("ipcache entry for podIP %s: %s", podIP, err))
+			}
 		}
 	}
 	if len(errs) != 0 {

daemon/cmd/status_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2021 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

pkg/ipcache/ipcache_test.go
@@ -17,6 +17,7 @@
 package ipcache
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"sort"
@@ -70,7 +71,7 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 		ID:     identity,
 		Source: source.Kubernetes,
 	})
-	c.Assert(err, Not(IsNil))
+	c.Assert(errors.Is(err, &ErrOverwrite{NewSrc: source.Kubernetes}), Equals, true)
 
 	IPIdentityCache.Upsert(endpointIP, nil, 0, nil, Identity{
 		ID:     identity,