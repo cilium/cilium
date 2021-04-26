cilium/cilium

commit fb0c3e592d7a9da854f9d38345fbb5720ea3b073

metrics: Add new prometheus metrics and related label
This PR is to add new metrics and labels as per the check done
by official tool promtool.

Summary of the changes can be found as below:
- Deprecate eventType, mapName, subnetId, availabilityZone, repsonseCode
  labels in favour of event_type and map_name, subnet_id, availability_zone,
  and response_code.
- Add _total suffix for counter metrics (cilium_endpoint_regenerations,
  cilium_k8s_client_api_calls_counter, cilium_policy_import_errors)
- Add _count suffix for non-histogram and non-summary metrics
  (cilium_identity_count, cilium_policy_count)

Relates: #11743
Signed-off-by: Tam Mach <sayboras@yahoo.com>

pkg/bpf/map_linux.go
@@ -842,7 +842,8 @@ func (m *Map) Update(key MapKey, value MapValue) error {
 
 	err = UpdateElement(m.fd, key.GetKeyPtr(), value.GetValuePtr(), 0)
 	if option.Config.MetricsConfig.BPFMapOps {
-		metrics.BPFMapOps.WithLabelValues(m.commonName(), metricOpUpdate, metrics.Error2Outcome(err)).Inc()
+		//TODO(sayboras): Remove deprecated label in 1.10
+		metrics.BPFMapOps.WithLabelValues(m.commonName(), m.commonName(), metricOpUpdate, metrics.Error2Outcome(err)).Inc()
 	}
 	return err
 }
@@ -890,7 +891,8 @@ func (m *Map) Delete(key MapKey) error {
 
 	_, errno := deleteElement(m.fd, key.GetKeyPtr())
 	if option.Config.MetricsConfig.BPFMapOps {
-		metrics.BPFMapOps.WithLabelValues(m.commonName(), metricOpDelete, metrics.Errno2Outcome(errno)).Inc()
+		//TODO(sayboras): Remove deprecated label in 1.10
+		metrics.BPFMapOps.WithLabelValues(m.commonName(), m.commonName(), metricOpDelete, metrics.Errno2Outcome(errno)).Inc()
 	}
 	if errno != 0 {
 		err = fmt.Errorf("unable to delete element %s from map %s: %w", key, m.name, errno)

.github/workflows/smoke-test.yaml
@@ -161,8 +161,7 @@ jobs:
           kubectl -n kube-system exec $cilium_pod -- sh -c "apt update && apt install curl -y"
           kubectl -n kube-system exec $cilium_pod -- curl http://localhost:9090/metrics > metrics.prom
           go get -u github.com/prometheus/prometheus/cmd/promtool
-          cat metrics.prom
-          cat metrics.prom | egrep -v "${{ env.DEPRECATED_METRICS }}" | $HOME/go/bin/promtool check metrics
+          egrep -v "${{ env.DEPRECATED_METRICS }}" metrics.prom | $HOME/go/bin/promtool check metrics
 
       - name: Dump cilium related logs and events
         if: ${{ failure() }}

daemon/cmd/daemon.go
@@ -295,11 +295,13 @@ func NewDaemon(ctx context.Context, epMgr *endpointmanager.EndpointManager, dp d
 	}
 
 	identity.IterateReservedIdentities(func(_ string, _ identity.NumericIdentity) {
+		metrics.Identity.Inc()
 		metrics.IdentityCount.Inc()
 	})
 	if option.Config.EnableWellKnownIdentities {
 		// Must be done before calling policy.NewPolicyRepository() below.
 		num := identity.InitWellKnownIdentities(option.Config)
+		metrics.Identity.Add(float64(num))
 		metrics.IdentityCount.Add(float64(num))
 	}
 

daemon/cmd/policy.go
@@ -271,6 +271,7 @@ func (d *Daemon) policyAdd(sourceRules policyAPI.Rules, opts *policy.AddOptions,
 	newPrefixLengths, err := d.prefixLengths.Add(prefixes)
 	if err != nil {
 		metrics.PolicyImportErrors.Inc()
+		metrics.PolicyImportErrorsTotal.Inc()
 		logger.WithError(err).WithField("prefixes", prefixes).Warn(
 			"Failed to reference-count prefix lengths in CIDR policy")
 		resChan <- &PolicyAddResult{
@@ -285,6 +286,7 @@ func (d *Daemon) policyAdd(sourceRules policyAPI.Rules, opts *policy.AddOptions,
 		if err := d.Datapath().Loader().Reinitialize(d.ctx, d, d.mtuConfig.GetDeviceMTU(), d.Datapath(), d.l7Proxy, d.ipam); err != nil {
 			_ = d.prefixLengths.Delete(prefixes)
 			metrics.PolicyImportErrors.Inc()
+			metrics.PolicyImportErrorsTotal.Inc()
 			err2 := fmt.Errorf("Unable to recompile base programs: %s", err)
 			logger.WithError(err2).WithField("prefixes", prefixes).Warn(
 				"Failed to recompile base programs due to prefix length count change")
@@ -299,6 +301,7 @@ func (d *Daemon) policyAdd(sourceRules policyAPI.Rules, opts *policy.AddOptions,
 	if _, err := ipcache.AllocateCIDRs(prefixes); err != nil {
 		_ = d.prefixLengths.Delete(prefixes)
 		metrics.PolicyImportErrors.Inc()
+		metrics.PolicyImportErrorsTotal.Inc()
 		logger.WithError(err).WithField("prefixes", prefixes).Warn(
 			"Failed to allocate identities for CIDRs during policy add")
 		resChan <- &PolicyAddResult{

examples/kubernetes/addons/prometheus/monitoring-example.yaml
@@ -2218,10 +2218,10 @@ data:
           "steppedLine": false,
           "targets": [
             {
-              "expr": "topk(5, avg(rate(cilium_bpf_map_ops_total{k8s_app=\"cilium\"}[5m])) by (pod, mapName, operation))",
+              "expr": "topk(5, avg(rate(cilium_bpf_map_ops_total{k8s_app=\"cilium\"}[5m])) by (pod, map_name, operation))",
               "format": "time_series",
               "intervalFactor": 1,
-              "legendFormat": "{{mapName}} {{operation}}",
+              "legendFormat": "{{map_name}} {{operation}}",
               "refId": "A"
             }
           ],
@@ -2319,10 +2319,10 @@ data:
           "steppedLine": false,
           "targets": [
             {
-              "expr": "topk(5, max(rate(cilium_bpf_map_ops_total{k8s_app=\"cilium\"}[5m])) by (pod, mapName, operation))",
+              "expr": "topk(5, max(rate(cilium_bpf_map_ops_total{k8s_app=\"cilium\"}[5m])) by (pod, map_name, operation))",
               "format": "time_series",
               "intervalFactor": 1,
-              "legendFormat": "{{mapName}} {{operation}}",
+              "legendFormat": "{{map_name}} {{operation}}",
               "refId": "A"
             }
           ],
@@ -2420,10 +2420,10 @@ data:
           "steppedLine": false,
           "targets": [
             {
-              "expr": "sum(rate(cilium_bpf_map_ops_total{k8s_app=\"cilium\",outcome=\"fail\"}[5m])) by (pod, mapName, operation)",
+              "expr": "sum(rate(cilium_bpf_map_ops_total{k8s_app=\"cilium\",outcome=\"fail\"}[5m])) by (pod, map_name, operation)",
               "format": "time_series",
               "intervalFactor": 1,
-              "legendFormat": "{{mapName}} {{operation}}",
+              "legendFormat": "{{map_name}} {{operation}}",
               "refId": "A"
             }
           ],
@@ -4472,10 +4472,10 @@ data:
           "steppedLine": false,
           "targets": [
             {
-              "expr": "avg(rate(cilium_nodes_all_events_received_total{k8s_app=\"cilium\"}[1m])) by (pod, eventType, source) * 60",
+              "expr": "avg(rate(cilium_nodes_all_events_received_total{k8s_app=\"cilium\"}[1m])) by (pod, event_type, source) * 60",
               "format": "time_series",
               "intervalFactor": 1,
-              "legendFormat": "{{eventType}} {{source}}",
+              "legendFormat": "{{event_type}} {{source}}",
               "refId": "B"
             }
           ],
@@ -5910,28 +5910,28 @@ data:
           "steppedLine": false,
           "targets": [
             {
-              "expr": "min(cilium_policy_count{k8s_app=\"cilium\"}) by(pod)",
+              "expr": "min(cilium_policy{k8s_app=\"cilium\"}) by(pod)",
               "format": "time_series",
               "intervalFactor": 1,
               "legendFormat": "min",
               "refId": "A"
             },
             {
-              "expr": "avg(cilium_policy_count{k8s_app=\"cilium\"}) by(pod)",
+              "expr": "avg(cilium_policy{k8s_app=\"cilium\"}) by(pod)",
               "format": "time_series",
               "intervalFactor": 1,
               "legendFormat": "avg",
               "refId": "B"
             },
             {
-              "expr": "max(cilium_policy_count{k8s_app=\"cilium\"}) by(pod)",
+              "expr": "max(cilium_policy{k8s_app=\"cilium\"}) by(pod)",
               "format": "time_series",
               "intervalFactor": 1,
               "legendFormat": "max",
               "refId": "C"
             },
             {
-              "expr": "sum(cilium_policy_import_errors{k8s_app=\"cilium\"}) by (pod)",
+              "expr": "sum(cilium_policy_import_errors_total{k8s_app=\"cilium\"}) by (pod)",
               "format": "time_series",
               "intervalFactor": 1,
               "legendFormat": "policy import errors",
@@ -6495,7 +6495,7 @@ data:
           "steppedLine": false,
           "targets": [
             {
-              "expr": "sum(rate(cilium_endpoint_regenerations{k8s_app=\"cilium\"}[30s])) by(outcome)",
+              "expr": "sum(rate(cilium_endpoint_regenerations_total{k8s_app=\"cilium\"}[30s])) by(outcome)",
               "format": "time_series",
               "instant": false,
               "intervalFactor": 1,
@@ -7280,7 +7280,7 @@ data:
           "steppedLine": false,
           "targets": [
             {
-              "expr": "sum(rate(cilium_k8s_client_api_calls_counter{k8s_app=\"cilium\"}[1m])) by (pod, method, return_code)",
+              "expr": "sum(rate(cilium_k8s_client_api_calls_total{k8s_app=\"cilium\"}[1m])) by (pod, method, return_code)",
               "format": "time_series",
               "intervalFactor": 1,
               "legendFormat": "{{method}} {{return_code}}",
@@ -8936,7 +8936,7 @@ data:
               "expr": "rate(cilium_operator_eni_aws_api_duration_seconds_sum[1m])/rate(cilium_operator_eni_aws_api_duration_seconds_count[1m])",
               "format": "time_series",
               "intervalFactor": 1,
-              "legendFormat": "{{operation}} {{responseCode}}",
+              "legendFormat": "{{operation}} {{response_code}}",
               "refId": "A"
             }
           ],

install/kubernetes/cilium/charts/prometheus-crd/crds/crd-servicemonitor.yaml
@@ -5,7 +5,6 @@ metadata:
   annotations:
     controller-gen.kubebuilder.io/version: v0.2.4
     helm.sh/hook: crd-install
-  creationTimestamp: null
   name: servicemonitors.monitoring.coreos.com
 spec:
   group: monitoring.coreos.com

pkg/api/metrics/metrics.go
@@ -38,7 +38,7 @@ func NewPrometheusMetrics(namespace, subsystem string, registry *prometheus.Regi
 		Subsystem: subsystem,
 		Name:      "api_duration_seconds",
 		Help:      "Duration of interactions with API",
-	}, []string{"operation", "responseCode"})
+	}, []string{"operation", "responseCode", "response_code"}) //TODO(sayboras): Remove deprecated tag responseCode in 1.10
 
 	m.RateLimit = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: namespace,
@@ -56,7 +56,8 @@ func NewPrometheusMetrics(namespace, subsystem string, registry *prometheus.Regi
 // ObserveAPICall must be called on every API call made with the operation
 // performed, the status code received and the duration of the call
 func (p *PrometheusMetrics) ObserveAPICall(operation, status string, duration float64) {
-	p.ApiDuration.WithLabelValues(operation, status).Observe(duration)
+	//TODO(sayboras): Remove deprecated tag responseCode in 1.10
+	p.ApiDuration.WithLabelValues(operation, status, status).Observe(duration)
 }
 
 // ObserveRateLimit must be called in case an API call was subject to rate limiting

pkg/endpoint/metrics.go
@@ -75,10 +75,12 @@ func (s *regenerationStatistics) SendMetrics() {
 	if !s.success {
 		// Endpoint regeneration failed, increase on failed metrics
 		metrics.EndpointRegenerationCount.WithLabelValues(metrics.LabelValueOutcomeFail).Inc()
+		metrics.EndpointRegenerationTotal.WithLabelValues(metrics.LabelValueOutcomeFail).Inc()
 		return
 	}
 
 	metrics.EndpointRegenerationCount.WithLabelValues(metrics.LabelValueOutcomeSuccess).Inc()
+	metrics.EndpointRegenerationTotal.WithLabelValues(metrics.LabelValueOutcomeSuccess).Inc()
 
 	sendMetrics(s, metrics.EndpointRegenerationTimeStats)
 }

commit 13e2a7f397c5e32864e841b0e9917e51eb38def8

pkg/bpf: fix panic for inconsistency label cardinality
When labels were marked for deprecation, there were certain places where
the old label was not set. This causes Cilium, running with metrics
enabled, to panic when those certain BPF map operations are attempted.

Such panic will manifest as follow:

```
goroutine 400 [running]:
github.com/prometheus/client_golang/prometheus.(*CounterVec).WithLabelValues(0xc0005239b0, 0xc005f3ed20, 0x3, 0x3, 0x2ad5ce0, 0xc005ef69c0)
	/go/src/github.com/cilium/cilium/vendor/github.com/prometheus/client_golang/prometheus/counter.go:248 +0xda
github.com/cilium/cilium/pkg/bpf.(*Map).resolveErrors(0xc000ea9180, 0x2b3cc60, 0xc005f195c0, 0x0, 0x0)
	/go/src/github.com/cilium/cilium/pkg/bpf/map_linux.go:1073 +0xda2
github.com/cilium/cilium/pkg/controller.(*Controller).runController(0xc005632000)
	/go/src/github.com/cilium/cilium/pkg/controller/controller.go:205 +0xa77
created by github.com/cilium/cilium/pkg/controller.(*Manager).updateController
	/go/src/github.com/cilium/cilium/pkg/controller/manager.go:120 +0xb50
```

To avoid this panic, as a temporary workaround, users can run Cilium
with `--metrics=-cilium_bpf_map_ops_total` or setting
`metrics: "-cilium_bpf_map_ops_total"` in Cilium's ConfigMap.

Fixes: fb0c3e592d7a ("metrics: Add new prometheus metrics and related label")
Signed-off-by: André Martins <andre@cilium.io>

pkg/bpf/map_linux.go
@@ -982,7 +982,8 @@ func (m *Map) GetNextKey(key MapKey, nextKey MapKey) error {
 
 	err := GetNextKey(m.fd, key.GetKeyPtr(), nextKey.GetKeyPtr())
 	if option.Config.MetricsConfig.BPFMapOps {
-		metrics.BPFMapOps.WithLabelValues(m.commonName(), metricOpGetNextKey, metrics.Error2Outcome(err)).Inc()
+		//TODO(sayboras): Remove deprecated label in 1.10
+		metrics.BPFMapOps.WithLabelValues(m.commonName(), m.commonName(), metricOpGetNextKey, metrics.Error2Outcome(err)).Inc()
 	}
 	return err
 }
@@ -1070,7 +1071,8 @@ func (m *Map) resolveErrors(ctx context.Context) error {
 		case Insert:
 			err := UpdateElement(m.fd, e.Key.GetKeyPtr(), e.Value.GetValuePtr(), 0)
 			if option.Config.MetricsConfig.BPFMapOps {
-				metrics.BPFMapOps.WithLabelValues(m.commonName(), metricOpUpdate, metrics.Error2Outcome(err)).Inc()
+				//TODO(sayboras): Remove deprecated label in 1.10
+				metrics.BPFMapOps.WithLabelValues(m.commonName(), m.commonName(), metricOpUpdate, metrics.Error2Outcome(err)).Inc()
 			}
 			if err == nil {
 				e.DesiredAction = OK
@@ -1085,7 +1087,8 @@ func (m *Map) resolveErrors(ctx context.Context) error {
 		case Delete:
 			_, err := deleteElement(m.fd, e.Key.GetKeyPtr())
 			if option.Config.MetricsConfig.BPFMapOps {
-				metrics.BPFMapOps.WithLabelValues(m.commonName(), metricOpDelete, metrics.Error2Outcome(err)).Inc()
+				//TODO(sayboras): Remove deprecated label in 1.10
+				metrics.BPFMapOps.WithLabelValues(m.commonName(), m.commonName(), metricOpDelete, metrics.Error2Outcome(err)).Inc()
 			}
 			if err == 0 || err == unix.ENOENT {
 				delete(m.cache, k)