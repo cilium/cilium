291f6e70ef8331225d6a2f435871082122c854aa
Sebastian Wicki <sebastian@isovalent.com>
2021-04-28 14:18:24 +0200

    eni: Fix Cilium overallocating network interfaces
    [ upstream commit 119cd19f77aeddc4c1f930d55ba74fa42cc1a118 ]
    
    This fixes a bug where Cilium wrongly overestimated the amount of available
    ENI IP addresses. This bug was introduced when we removed the primary
    ENI IP address from the IPAM pool, but forgot to adjust the number of
    addresses used to compare with the AWS instance limits.
    
    This led to the operator overestimating the number of available IP
    addresses by one. This in turn could lead to the operator first failing
    to allocate more IPs (because it exceeded the limit) and then
    unnecessarily creating a new ENI to fulfill the allocation request.
    
    Fixes: 7c1bb3592c46 ("aws/ec2: Exclude primary ENI IP from IPAM pool")
    Fixes: #15877
    
    backport-1.8: Merged comment conflicts, dropped patch from
    the `pkg/k8s/apis/cilium.io/client/crds/v2/ciliumnodes.yaml`
    file which doesn't exist in v1.8
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Glib Smaga <code@gsmaga.com>

--- pkg/aws/eni/node_test.go
+++ pkg/aws/eni/node_test.go
@@ -26,13 +26,13 @@ func (e *ENISuite) TestGetMaximumAllocatableIPv4(c *check.C) {
 	// With no k8sObj defined, it should return 0
 	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 0)
 
-	// With instance-type = m5.large and first-interface-index = 0, we should be able to allocate up to 30 addresses
+	// With instance-type = m5.large and first-interface-index = 0, we should be able to allocate up to 3x10-3 addresses
 	n.k8sObj = newCiliumNode("node", "i-testnode", "m5.large", "eu-west-1", "test-vpc", 0, 0, 0, 0)
-	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 30)
+	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 27)
 
-	// With instance-type = m5.large and first-interface-index = 1, we should be able to allocate up to 20 addresses
+	// With instance-type = m5.large and first-interface-index = 1, we should be able to allocate up to 2x10-2 addresses
 	n.k8sObj = newCiliumNode("node", "i-testnode", "m5.large", "eu-west-1", "test-vpc", 1, 0, 0, 0)
-	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 20)
+	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 18)
 
 	// With instance-type = m5.large and first-interface-index = 4, we should return 0 as there is only 3 interfaces
 	n.k8sObj = newCiliumNode("node", "i-testnode", "m5.large", "eu-west-1", "test-vpc", 4, 0, 0, 0)
