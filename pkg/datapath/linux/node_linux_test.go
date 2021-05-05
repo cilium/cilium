5951163b70fcb2c8b507dbc0e743754ee1fec7dd
Jussi Maki <jussi@isovalent.com>
2021-05-05 09:32:02 +0000

    wireguard: Set wireguard and route MTU to detected MTU
    For more optimal packet sizes in large MTU setups, set the
    cilium_wg0 MTU based on the detected MTU minus overhead.
    
    Additionally set the route MTU on container side to detected
    MTU minus wireguard overhead when wireguard is enabled.
    
    Tested manually with "--mtu 1400", causing cilium_wg0 and
    route MTU to be set to 1320.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/datapath/linux/node_linux_test.go
+++ pkg/datapath/linux/node_linux_test.go
@@ -79,7 +79,7 @@ const (
 func (s *linuxPrivilegedBaseTestSuite) SetUpTest(c *check.C, addressing datapath.NodeAddressing, enableIPv6, enableIPv4 bool) {
 	bpf.ConfigureResourceLimits()
 	s.nodeAddressing = addressing
-	s.mtuConfig = mtu.NewConfiguration(0, false, false, 1500, nil)
+	s.mtuConfig = mtu.NewConfiguration(0, false, false, false, 1500, nil)
 	s.enableIPv6 = enableIPv6
 	s.enableIPv4 = enableIPv4
 
