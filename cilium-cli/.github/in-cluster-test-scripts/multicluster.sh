b7234d95467e99ae6b3ac83e015965de51c2532a
Maciej Kwiek <maciej@isovalent.com>
2021-05-12 15:46:32 +0200

    ci: run multicluster test from within cluster
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

---
+++ cilium-cli/.github/in-cluster-test-scripts/multicluster.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -x
+
+CONTEXT1=$(kubectl config view | grep "${CLUSTER_NAME_1}" | head -1 | awk '{print $2}')
+CONTEXT2=$(kubectl config view | grep "${CLUSTER_NAME_2}" | head -1 | awk '{print $2}')
+
+cilium --context "${CONTEXT1}" install --cluster-name "${CLUSTER_NAME_1}" --cluster-id 1 --restart-unmanaged-pods=false --config monitor-aggregation=none --native-routing-cidr=10.0.0.0/9
+cilium --context "${CONTEXT1}" hubble enable
+cilium --context "${CONTEXT1}" clustermesh enable
+
+cilium --context "${CONTEXT2}" install --cluster-name "${CLUSTER_NAME_2}" --cluster-id 2  --restart-unmanaged-pods=false --config monitor-aggregation=none --native-routing-cidr=10.0.0.0/9 --inherit-ca "${CONTEXT1}"
+cilium --context "${CONTEXT2}" hubble enable --relay=false
+cilium --context "${CONTEXT2}" clustermesh enable
+
+cilium --context "${CONTEXT1}" clustermesh status --wait
+cilium --context "${CONTEXT2}" clustermesh status --wait
+
+cilium --context "${CONTEXT1}" clustermesh connect --destination-context "${CONTEXT2}"
+
+cilium --context "${CONTEXT1}" clustermesh status --wait --wait-duration 5m
+cilium --context "${CONTEXT2}" clustermesh status --wait --wait-duration 5m
+
+
+cilium --context "${CONTEXT1}" hubble port-forward &
+sleep 5s
+
+cilium --context "${CONTEXT1}" connectivity test --multi-cluster "${CONTEXT2}" --test '!/pod-to-.*-nodeport' --all-flows
+
+cilium --context "${CONTEXT1}" status
+cilium --context "${CONTEXT1}" clustermesh status
+cilium --context "${CONTEXT2}" status
+cilium --context "${CONTEXT2}" clustermesh status
