1d8d6bc3b83915e5ae3e37eb2a57836636484dfc
Joe Stringer <joe@cilium.io>
2021-05-17 16:52:53 -0700

    contrib: Optimize extract_authors.sh script
    [ upstream commit 1cd5be9e79d6094b4b472a1d0e9073ad0d834480 ]
    
    By using built-in formatting primitives instead of independently
    fetching names and emails from git in separate commands, we can reduce
    git history iteration by 50%, saving 30s per authors update on my
    system.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- contrib/scripts/extract_authors.sh
+++ contrib/scripts/extract_authors.sh
@@ -6,15 +6,11 @@ export LC_ALL=en_US.UTF-8
 
 function extract_authors() {
 	authors=$(git shortlog --summary | awk '{$1=""; print $0}' | sed -e 's/^ //')
+
+	# Iterate $authors by line
 	IFS=$'\n'
-	pad=$(printf '%0.1s' " "{1..60})
-	padlen=40
 	for i in $authors; do
-		name=$(git log --use-mailmap --author="$i" --format="%aN" | head -1)
-		mail=$(git log --use-mailmap --author="$i" --format="%aE" | head -1)
-		printf '%s' "$name"
-		printf '%*.*s' 0 $((padlen - ${#name})) "$pad"
-		printf '%s\n' "$mail"
+		git log --use-mailmap --author="$i" --format="%<|(40)%aN%aE" | head -1
 	done
 }
 
