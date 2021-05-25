cilium/cilium

commit 72e623821ecf5048aa89bf3fd94457dc41fee188

loader: Remove program and route when disable endpoint routes
When per-endpoint routes are enabled, a route is added for each
endpoint. A BPF program is also attached to the lxc devices on the path
to the containers.

When per-endpoint routes are disabled, we need to remove the routes and
the programs.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/loader/loader.go
@@ -342,16 +342,27 @@ func (l *Loader) reloadDatapath(ctx context.Context, ep datapath.Endpoint, dirs 
 				}
 				return err
 			}
+		} else {
+			err := RemoveTCFilters(ep.InterfaceName(), netlink.HANDLE_MIN_EGRESS)
+			if err != nil {
+				log.WithField("device", ep.InterfaceName()).Error(err)
+			}
 		}
 	}
 
-	if ep.RequireEndpointRoute() {
-		if ip := ep.IPv4Address(); ip.IsSet() {
+	if ip := ep.IPv4Address(); ip.IsSet() {
+		if ep.RequireEndpointRoute() {
 			upsertEndpointRoute(ep, *ip.IPNet(32))
+		} else {
+			removeEndpointRoute(ep, *ip.IPNet(32))
 		}
+	}
 
-		if ip := ep.IPv6Address(); ip.IsSet() {
+	if ip := ep.IPv6Address(); ip.IsSet() {
+		if ep.RequireEndpointRoute() {
 			upsertEndpointRoute(ep, *ip.IPNet(128))
+		} else {
+			removeEndpointRoute(ep, *ip.IPNet(128))
 		}
 	}
 

commit aec824e030d41cf7dfb2a2f8e9e62d58bf9146d4

loader: Revert removal of endpoint routes
This commit is a partial revert of 72e6238 ("loader: Remove program and
route when disable endpoint routes").

Commit 72e6238 started removing existing endpoint routes when
enable-endpoint-routes is disabled in the agent. In chaining mode
however, if Cilium isn't the primary CNI, it isn't responsible for
the endpoint's networking. In that case, the primary CNI may install
and rely on those endpoint routes and we shouldn't remove them.

This commit reverts the removal of endpoint routes. We'll provide a
proper solution to remove only endpoint routes Cilium "owns" in a
subsequent commit.

Fixes: 72e6238 ("loader: Remove program and route when disable endpoint routes")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/loader/loader.go
@@ -359,19 +359,19 @@ func (l *Loader) reloadDatapath(ctx context.Context, ep datapath.Endpoint, dirs 
 		}
 	}
 
-	if ip := ep.IPv4Address(); ip.IsSet() {
-		if ep.RequireEndpointRoute() {
-			upsertEndpointRoute(ep, *ip.IPNet(32))
-		} else {
-			removeEndpointRoute(ep, *ip.IPNet(32))
+	if ep.RequireEndpointRoute() {
+		scopedLog := ep.Logger(Subsystem).WithFields(logrus.Fields{
+			logfields.Veth: ep.InterfaceName(),
+		})
+		if ip := ep.IPv4Address(); ip.IsSet() {
+			if err := upsertEndpointRoute(ep, *ip.IPNet(32)); err != nil {
+				scopedLog.WithError(err).Warn("Failed to upsert route")
+			}
 		}
-	}
-
-	if ip := ep.IPv6Address(); ip.IsSet() {
-		if ep.RequireEndpointRoute() {
-			upsertEndpointRoute(ep, *ip.IPNet(128))
-		} else {
-			removeEndpointRoute(ep, *ip.IPNet(128))
+		if ip := ep.IPv6Address(); ip.IsSet() {
+			if err := upsertEndpointRoute(ep, *ip.IPNet(128)); err != nil {
+				scopedLog.WithError(err).Warn("Failed to upsert route")
+			}
 		}
 	}
 