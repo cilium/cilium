cilium/cilium

commit d8dbb82bc157af1d21a85e9b03df5ae180539e42

daemon, bgp, watchers: Implement LB IP announcement via BGP
This commit implements LoadBalancer IP announcements by integrating MetalLB
natively into Cilium.

Each node in the K8s cluster running a Cilium Agent will be a BGP
speaker. The speaker controller is notified of K8s events, specifically
updates to services, endpoints, and nodes.

The speaker is interested in the following K8s objects and events:

Services  -> OnUpdateService(), OnDeleteService()
Endpoints -> OnUpdateEndpoints()
Nodes     -> OnUpdateNode()

For Services updates, if the object contains a service of type
LoadBalancer with an LB IP (allocated by the Operator, see previous
commit), then the Agents will consider whether to advertise the LB IP to
the configured BGP router (provided by the user), depending on whether
the service's externalTrafficPolicy (either Cluster or Local). For the
difference between the two, see https://metallb.universe.tf/usage/#bgp.
This is also dependant on whether the service has healthy backends
(endpoints) running.

For Service deletions, if the speaker was previously announcing the
service via BGP, then it will withdraw that advertisement. This will
cause the BGP router to remove the route to the node advertising that it
can route the LB IP.

For Endpoints updates, we watch for these because we must know the
health of the backends (endpoints). When all backends of a service are
healthy, only then is a BGP announcement made. If the backends become
unhealthy, then the announcement is withdrawn. A mapping is stored
internally for services to endpoints, so that we may cross-reference
them back to a Service.

For Node updates, we watch for these because MetalLB allows users to
limit BGP peers to certain nodes. See
https://metallb.universe.tf/configuration/#limiting-peers-to-certain-nodes
for more details. For this event, we are only interested in the labels
of a Node.

All of these events described above are enqueued into an internal queue

pkg/k8s/watchers/node.go
@@ -20,6 +20,7 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
+	"github.com/cilium/cilium/pkg/option"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -35,6 +36,13 @@ func (k *K8sWatcher) nodesInit(k8sClient kubernetes.Interface) {
 		&slim_corev1.Node{},
 		0,
 		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				if node := k8s.ObjToV1Node(obj); node != nil {
+					if option.Config.BGPAnnounceLBIP {
+						k.bgpSpeakerManager.OnUpdateNode(node) // Need to seed the BGP speaker
+					}
+				}
+			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
 				var valid, equal bool
 				if oldNode := k8s.ObjToV1Node(oldObj); oldNode != nil {
@@ -75,5 +83,10 @@ func (k *K8sWatcher) updateK8sNodeV1(oldK8sNode, newK8sNode *slim_corev1.Node) e
 	if err != nil {
 		return err
 	}
+
+	if option.Config.BGPAnnounceLBIP {
+		k.bgpSpeakerManager.OnUpdateNode(newK8sNode)
+	}
+
 	return nil
 }

daemon/cmd/daemon.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	health "github.com/cilium/cilium/cilium-health/launch"
 	"github.com/cilium/cilium/pkg/bandwidth"
+	"github.com/cilium/cilium/pkg/bgp/speaker"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/cgroups"
 	"github.com/cilium/cilium/pkg/clustermesh"
@@ -172,6 +173,8 @@ type Daemon struct {
 	endpointCreations *endpointCreationManager
 
 	redirectPolicyManager *redirectpolicy.Manager
+
+	bgpSpeaker *speaker.Speaker
 
 	apiLimiterSet *rate.APILimiterSet
 
@@ -380,6 +383,9 @@ func NewDaemon(ctx context.Context, cancel context.CancelFunc, epMgr *endpointma
 	d.endpointManager.InitMetrics()
 
 	d.redirectPolicyManager = redirectpolicy.NewRedirectPolicyManager(d.svc)
+	if option.Config.BGPAnnounceLBIP {
+		d.bgpSpeaker = speaker.New()
+	}
 
 	d.k8sWatcher = watchers.NewK8sWatcher(
 		d.endpointManager,
@@ -389,11 +395,15 @@ func NewDaemon(ctx context.Context, cancel context.CancelFunc, epMgr *endpointma
 		d.svc,
 		d.datapath,
 		d.redirectPolicyManager,
+		d.bgpSpeaker,
 		option.Config,
 	)
 
 	d.redirectPolicyManager.RegisterSvcCache(&d.k8sWatcher.K8sSvcCache)
 	d.redirectPolicyManager.RegisterGetStores(d.k8sWatcher)
+	if option.Config.BGPAnnounceLBIP {
+		d.bgpSpeaker.RegisterSvcCache(&d.k8sWatcher.K8sSvcCache)
+	}
 
 	bootstrapStats.daemonInit.End(true)
 

pkg/bgp/speaker/events.go
@@ -1,0 +1,77 @@
+// Copyright 2017 Google Inc.
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package speaker
+
+import (
+	"github.com/cilium/cilium/pkg/k8s"
+
+	"go.universe.tf/metallb/pkg/k8s/types"
+	metallbspr "go.universe.tf/metallb/pkg/speaker"
+)
+
+type svcEvent struct {
+	id  k8s.ServiceID
+	svc *metallbspr.Service
+	eps *metallbspr.Endpoints
+}
+type epEvent svcEvent
+
+// nodeEvent must be a pointer to a map because maps are not hashable, aka
+// cannot be keys in a map, which queue is.
+type nodeEvent *map[string]string
+
+// run runs the reconciliation loop, fetching events off of the queue to
+// process. The events supported are svcEvent, epEvent, and nodeEvent. This
+// loop is only stopped (implicitly) when the Agent is shutting down.
+//
+// Adapted from go.universe.tf/metallb/pkg/k8s/k8s.go.
+func (s *Speaker) run() {
+	for {
+		key, quit := s.queue.Get()
+		if quit {
+			return
+		}
+		st := s.do(key)
+		switch st {
+		case types.SyncStateError:
+			s.queue.Add(key)
+		case types.SyncStateSuccess, types.SyncStateReprocessAll:
+			// SyncStateReprocessAll is returned in MetalLB when the
+			// configuration changes. However, we are not watching for
+			// configuration changes because our configuration is static and
+			// loaded once at Cilium start time.
+		}
+	}
+}
+
+// do performs the appropriate action depending on the event type. For example,
+// if it is a service event (svcEvent), then it will call into MetalLB's
+// SetService() to perform BGP announcements.
+func (s *Speaker) do(key interface{}) types.SyncState {
+	defer s.queue.Done(key)
+
+	switch k := key.(type) {
+	case svcEvent:
+		return s.SetService(s.logger, k.id.String(), k.svc, k.eps)
+	case epEvent:
+		return s.SetService(s.logger, k.id.String(), k.svc, k.eps)
+	case nodeEvent:
+		return s.SetNodeLabels(s.logger, *k)
+	default:
+		log.Debugf("Encountered an unknown key type %T in BGP speaker", k)
+		return types.SyncStateSuccess
+	}
+}

pkg/bgp/speaker/log.go
@@ -1,0 +1,22 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package speaker
+
+import (
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
+
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "bgp-speaker")

pkg/bgp/speaker/speaker.go
@@ -1,0 +1,216 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package speaker abstracts the BGP speaker controller from MetalLB. This
+// package provides BGP announcements based on K8s object event handling.
+package speaker
+
+import (
+	"os"
+
+	bgpconfig "github.com/cilium/cilium/pkg/bgp/config"
+	bgpk8s "github.com/cilium/cilium/pkg/bgp/k8s"
+	bgplog "github.com/cilium/cilium/pkg/bgp/log"
+	"github.com/cilium/cilium/pkg/k8s"
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	"github.com/cilium/cilium/pkg/lock"
+	nodetypes "github.com/cilium/cilium/pkg/node/types"
+	"github.com/cilium/cilium/pkg/option"
+
+	metallbspr "go.universe.tf/metallb/pkg/speaker"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/util/workqueue"
+)
+
+// New creates a new MetalLB BGP speaker controller.
+func New() *Speaker {
+	logger := &bgplog.Logger{Entry: log}
+	client := bgpk8s.New(logger.Logger)
+
+	c, err := metallbspr.NewController(metallbspr.ControllerConfig{
+		MyNode:        nodetypes.GetName(),
+		Logger:        logger,
+		SList:         nil, // BGP speaker doesn't use speakerlist
+		DisableLayer2: true,
+	})
+	if err != nil {
+		log.WithError(err).Fatal("Failed to initialize BGP speaker controller")
+	}
+	c.Client = client
+
+	f, err := os.Open(option.Config.BGPConfigPath)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to open BGP config file")
+	}
+	config, err := bgpconfig.Parse(f)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to parse BGP configuration")
+	}
+	c.SetConfig(logger, config)
+
+	spkr := &Speaker{
+		Controller: c,
+
+		logger:   logger,
+		queue:    workqueue.New(),
+		services: make(map[k8s.ServiceID]*slim_corev1.Service),
+	}
+	go spkr.run()
+
+	log.Info("Started BGP speaker")
+
+	return spkr
+}
+
+// Speaker represents the BGP speaker. It integrates Cilium's K8s events with
+// MetalLB's logic for making BGP announcements. It is responsible for
+// announcing BGP messages containing a loadbalancer IP address to peers.
+type Speaker struct {
+	*metallbspr.Controller
+
+	logger *bgplog.Logger
+
+	endpointsGetter endpointsGetter
+	// queue holds all the events to process for the Speaker.
+	queue workqueue.Interface
+
+	lock.Mutex
+	services map[k8s.ServiceID]*slim_corev1.Service
+}
+
+// OnUpdateService notifies the Speaker of an update to a service.
+func (s *Speaker) OnUpdateService(svc *slim_corev1.Service) {
+	svcID := k8s.ParseServiceID(svc)
+
+	eps := new(metallbspr.Endpoints)
+	epsFromSvc := s.endpointsGetter.GetEndpointsOfService(svcID)
+	if epsFromSvc != nil {
+		eps = convertInternalEndpoints(epsFromSvc)
+	}
+
+	s.Lock()
+	s.services[svcID] = svc
+	s.Unlock()
+
+	s.queue.Add(epEvent{
+		id:  svcID,
+		svc: convertService(svc),
+		eps: eps,
+	})
+}
+
+// OnDeleteService notifies the Speaker of a delete of a service.
+func (s *Speaker) OnDeleteService(svc *slim_corev1.Service) {
+	svcID := k8s.ParseServiceID(svc)
+
+	s.Lock()
+	delete(s.services, svcID)
+	s.Unlock()
+
+	// Passing nil as the service will force the MetalLB speaker to withdraw
+	// the BGP announcement.
+	s.queue.Add(svcEvent{
+		id:  svcID,
+		svc: nil,
+		eps: nil,
+	})
+}
+
+// OnUpdateEndpoints notifies the Speaker of an update to the backends of a
+// service.
+func (s *Speaker) OnUpdateEndpoints(eps *slim_corev1.Endpoints) {
+	svcID := k8s.ParseEndpointsID(eps)
+
+	s.Lock()
+	defer s.Unlock()
+	if svc, ok := s.services[svcID]; ok {
+		s.queue.Add(epEvent{
+			id:  svcID,
+			svc: convertService(svc),
+			eps: convertEndpoints(eps),
+		})
+	}
+}
+
+// OnUpdateNode notifies the Speaker of an update to a node.
+func (s *Speaker) OnUpdateNode(node *slim_corev1.Node) {
+	s.queue.Add(nodeEvent(&node.Labels))
+}
+
+// RegisterSvcCache registers the K8s watcher cache with this Speaker.
+func (s *Speaker) RegisterSvcCache(cache endpointsGetter) {
+	s.endpointsGetter = cache
+}
+
+// endpointsGetter abstracts the github.com/cilium/cilium/pkg/k8s.ServiceCache
+// object. The cache holds all services and endpoints (backends) from the K8s
+// watchers.
+type endpointsGetter interface {
+	GetEndpointsOfService(svcID k8s.ServiceID) *k8s.Endpoints
+}
+
+func convertService(in *slim_corev1.Service) *metallbspr.Service {
+	if in == nil {
+		return nil
+	}
+	ing := make([]v1.LoadBalancerIngress, len(in.Status.LoadBalancer.Ingress))
+	for i := range in.Status.LoadBalancer.Ingress {
+		ing[i].IP = in.Status.LoadBalancer.Ingress[i].IP
+	}
+	return &metallbspr.Service{
+		Type:          string(in.Spec.Type),
+		TrafficPolicy: string(in.Spec.ExternalTrafficPolicy),
+		Ingress:       ing,
+	}
+}
+
+func convertInternalEndpoints(in *k8s.Endpoints) *metallbspr.Endpoints {
+	if in == nil {
+		return nil
+	}
+	out := new(metallbspr.Endpoints)
+	for ip, be := range in.Backends {
+		ep := metallbspr.Endpoint{
+			IP:       ip,
+			NodeName: &be.NodeName,
+		}
+		out.Ready = append(out.Ready, ep)
+	}
+	return out
+}
+
+func convertEndpoints(in *slim_corev1.Endpoints) *metallbspr.Endpoints {
+	if in == nil {
+		return nil
+	}
+	out := new(metallbspr.Endpoints)
+	for _, sub := range in.Subsets {
+		for _, ep := range sub.Addresses {
+			out.Ready = append(out.Ready, metallbspr.Endpoint{
+				IP:       ep.IP,
+				NodeName: ep.NodeName,
+			})
+		}
+		// MetalLB uses the NotReadyAddresses field to know which endpoints are
+		// unhealthy in order to prevent BGP announcements until the endpoints
+		// are ready. However, Cilium has no need for this field because
+		// there's no need to also store unhealthy backends. The absence of
+		// backends inside Addresses (healthy) is equivalent to the presence of
+		// backends inside NotReadyAddresses. Therefore, Cilium chooses not to
+		// include NotReadyAddresses inside its slim version of Endpoints. This
+		// is still compatible with MetalLB because the information is
+		// equivalent.
+	}
+	return out
+}

pkg/k8s/watchers/endpoint.go
@@ -93,6 +93,9 @@ func (k *K8sWatcher) addK8sEndpointV1(ep *slim_corev1.Endpoints, swg *lock.Stopp
 
 func (k *K8sWatcher) updateK8sEndpointV1(oldEP, newEP *slim_corev1.Endpoints, swg *lock.StoppableWaitGroup) error {
 	k.K8sSvcCache.UpdateEndpoints(newEP, swg)
+	if option.Config.BGPAnnounceLBIP {
+		k.bgpSpeakerManager.OnUpdateEndpoints(newEP)
+	}
 	return nil
 }
 

pkg/k8s/watchers/service.go
@@ -89,6 +89,9 @@ func (k *K8sWatcher) addK8sServiceV1(svc *slim_corev1.Service, swg *lock.Stoppab
 			k.redirectPolicyManager.OnAddService(svcID)
 		}
 	}
+	if option.Config.BGPAnnounceLBIP {
+		k.bgpSpeakerManager.OnUpdateService(svc)
+	}
 	return nil
 }
 
@@ -103,6 +106,9 @@ func (k *K8sWatcher) deleteK8sServiceV1(svc *slim_corev1.Service, swg *lock.Stop
 		if svc.Spec.Type == slim_corev1.ServiceTypeClusterIP {
 			k.redirectPolicyManager.OnDeleteService(svcID)
 		}
+	}
+	if option.Config.BGPAnnounceLBIP {
+		k.bgpSpeakerManager.OnDeleteService(svc)
 	}
 	return nil
 }

pkg/k8s/watchers/watcher.go
@@ -147,6 +147,15 @@ type redirectPolicyManager interface {
 	OnAddPod(pod *slim_corev1.Pod)
 }
 
+type bgpSpeakerManager interface {
+	OnUpdateService(svc *slim_corev1.Service)
+	OnDeleteService(svc *slim_corev1.Service)
+
+	OnUpdateEndpoints(eps *slim_corev1.Endpoints)
+
+	OnUpdateNode(node *slim_corev1.Node)
+}
+
 type K8sWatcher struct {
 	// k8sResourceSynced maps a resource name to a channel. Once the given
 	// resource name is synchronized with k8s, the channel for which that
@@ -169,6 +178,7 @@ type K8sWatcher struct {
 	policyRepository      policyRepository
 	svcManager            svcManager
 	redirectPolicyManager redirectPolicyManager
+	bgpSpeakerManager     bgpSpeakerManager
 
 	// controllersStarted is a channel that is closed when all controllers, i.e.,
 	// k8s watchers have started listening for k8s events.
@@ -197,6 +207,7 @@ func NewK8sWatcher(
 	svcManager svcManager,
 	datapath datapath.Datapath,
 	redirectPolicyManager redirectPolicyManager,
+	bgpSpeakerManager bgpSpeakerManager,
 	cfg WatcherConfiguration,
 ) *K8sWatcher {
 	return &K8sWatcher{
@@ -210,6 +221,7 @@ func NewK8sWatcher(
 		podStoreSet:           make(chan struct{}),
 		datapath:              datapath,
 		redirectPolicyManager: redirectPolicyManager,
+		bgpSpeakerManager:     bgpSpeakerManager,
 		cfg:                   cfg,
 	}
 }

commit b9eb68c1b597c30af04e22d35a3d16f9f449ef50

watchers: Fix BGP subscriber potentially getting skipped
It is possible for the BGP speaker subscriber to be skipped in a K8s
node event if the host endpoint is not yet created. This can happen at
the very early stages of Cilium startup, as a K8s node add event is sent
to the K8s watchers as one of the first events It is also often sent
before any endpoints have been generated. If that happends, then the
consequence is that the MetalLB integration is not seeded with the node
labels, which can prevent peering with the BGP routers if the user has
node-selectors defined in their BGP configuration. In other words, the
MetalLB integration would try to match the selectors against empty
labels, which will always fail.

The short term fix is to move the BGP speaker logic slightly above where
the host endpoint logic can return so that it is guaranteed to always be
executed. Longer term, we have an issue
https://github.com/cilium/cilium/issues/15471 to refactor the
subscribers so that they are executed separately, rather than bundled
into one function like (*K8sWatcher).updateK8sNodeV1(). That would have
prevented this bug.

Fixes: d8dbb82bc1 ("daemon, bgp, watchers: Implement LB IP announcement
via BGP")
Fixes: https://github.com/cilium/cilium/issues/16340

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/k8s/watchers/node.go
@@ -79,6 +79,10 @@ func (k *K8sWatcher) updateK8sNodeV1(oldK8sNode, newK8sNode *slim_corev1.Node) e
 	}
 	newNodeLabels := newK8sNode.GetLabels()
 
+	if option.Config.BGPAnnounceLBIP {
+		k.bgpSpeakerManager.OnUpdateNode(newK8sNode)
+	}
+
 	nodeEP := k.endpointManager.GetHostEndpoint()
 	if nodeEP == nil {
 		log.Debug("Host endpoint not found, updating node labels")
@@ -90,10 +94,5 @@ func (k *K8sWatcher) updateK8sNodeV1(oldK8sNode, newK8sNode *slim_corev1.Node) e
 	if err != nil {
 		return err
 	}
-
-	if option.Config.BGPAnnounceLBIP {
-		k.bgpSpeakerManager.OnUpdateNode(newK8sNode)
-	}
-
 	return nil
 }