cilium/cilium

commit 09d9e1e0e2d9118a166a8af70e6af66e71ce66a1

policy: Disable well-known identities for non-managed etcd
Add an option --enable-well-known-identities to allow disabling well-known
identities for all new deployments which are not using managed etcd. This
reduces the number of security identities whitelisted for each endpoint.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/option/config.go
@@ -639,6 +639,11 @@ const (
 	// EnableLocalNodeRoute controls installation of the route which points
 	// the allocation prefix of the local node.
 	EnableLocalNodeRoute = "enable-local-node-route"
+
+	// EnableWellKnownIdentities enables the use of well-known identities.
+	// This is requires if identiy resolution is required to bring up the
+	// control plane, e.g. when using the managed etcd feature
+	EnableWellKnownIdentities = "enable-well-known-identities"
 )
 
 // Default string arguments
@@ -1271,6 +1276,11 @@ type DaemonConfig struct {
 	// Enabling this option reduces waste of IP addresses but may increase
 	// the number of API calls to AWS EC2 service.
 	AwsReleaseExcessIps bool
+
+	// EnableWellKnownIdentities enables the use of well-known identities.
+	// This is requires if identiy resolution is required to bring up the
+	// control plane, e.g. when using the managed etcd feature
+	EnableWellKnownIdentities bool
 }
 
 var (
@@ -1305,6 +1315,7 @@ var (
 		AutoCreateCiliumNodeResource: defaults.AutoCreateCiliumNodeResource,
 		IdentityAllocationMode:       IdentityAllocationModeKVstore,
 		AllowICMPFragNeeded:          defaults.AllowICMPFragNeeded,
+		EnableWellKnownIdentities:    defaults.EnableEndpointRoutes,
 	}
 )
 
@@ -1601,6 +1612,7 @@ func (c *DaemonConfig) Populate() {
 	c.EnableIPv4 = getIPv4Enabled()
 	c.EnableIPv6 = viper.GetBool(EnableIPv6Name)
 	c.EnableIPSec = viper.GetBool(EnableIPSecName)
+	c.EnableWellKnownIdentities = viper.GetBool(EnableWellKnownIdentities)
 	c.EndpointInterfaceNamePrefix = viper.GetString(EndpointInterfaceNamePrefix)
 	c.DevicePreFilter = viper.GetString(PrefilterDevice)
 	c.DisableCiliumEndpointCRD = viper.GetBool(DisableCiliumEndpointCRDName)

daemon/daemon.go
@@ -292,8 +292,10 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 	}
 
 	identity.UpdateReservedIdentitiesMetrics()
-	// Must be done before calling policy.NewPolicyRepository() below.
-	identity.InitWellKnownIdentities()
+	if option.Config.EnableWellKnownIdentities {
+		// Must be done before calling policy.NewPolicyRepository() below.
+		identity.InitWellKnownIdentities()
+	}
 
 	nd := nodediscovery.NewNodeDiscovery(nodeMngr, mtuConfig, netConf)
 

daemon/daemon_main.go
@@ -338,6 +338,9 @@ func init() {
 	flags.Bool(option.EnableTracing, false, "Enable tracing while determining policy (debugging)")
 	option.BindEnv(option.EnableTracing)
 
+	flags.Bool(option.EnableWellKnownIdentities, defaults.EnableWellKnownIdentities, "Enable well-known identities for known Kubernetes components")
+	option.BindEnv(option.EnableWellKnownIdentities)
+
 	flags.String(option.EnvoyLog, "", "Path to a separate Envoy log file, if any")
 	option.BindEnv(option.EnvoyLog)
 

install/kubernetes/cilium/charts/config/templates/configmap.yaml
@@ -296,3 +296,8 @@ data:
   # Chaining mode was enabled, disabling health checking
   enable-endpoint-health-checking: "false"
 {{- end }}
+{{- if or .Values.global.wellKnownIdentities.enabled .Values.global.etcd.managed }}
+  enable-well-known-identities: "true"
+{{- else }}
+  enable-well-known-identities: "false"
+{{- end }}

install/kubernetes/cilium/values.yaml
@@ -301,3 +301,7 @@ global:
 
   daemon:
     runPath: "/var/run/cilium"
+
+  wellKnownIdentities:
+    # enabled enables the use of well-known identities
+    enabled: false

install/kubernetes/quick-install.yaml
@@ -122,6 +122,7 @@ data:
   install-iptables-rules: "true"
   auto-direct-node-routes: "false"
   enable-node-port: "false"
+  enable-well-known-identities: "false"
 
 ---
 # Source: cilium/charts/agent/templates/serviceaccount.yaml
@@ -627,7 +628,6 @@ spec:
           initialDelaySeconds: 60
           periodSeconds: 10
           timeoutSeconds: 3
-
       hostNetwork: true
       restartPolicy: Always
       serviceAccount: cilium-operator
@@ -638,6 +638,7 @@ spec:
 
 ---
 # Source: cilium/charts/agent/templates/svc.yaml
+
 
 ---
 # Source: cilium/charts/operator/templates/servicemonitor.yaml

pkg/defaults/defaults.go
@@ -321,4 +321,8 @@ const (
 
 	// RestoreV6Addr is used as match for cilium_host v6 (router) address
 	RestoreV6Addr = "cilium.v6.internal.raw "
+
+	// EnableWellKnownIdentities is enabled by default as this is the
+	// original behavior. New default Helm templates will disable this.
+	EnableWellKnownIdentities = true
 )

commit fdeca3e333e6be1f985542e59aaf7f2911080320

pkg/option: Fix default assignment of EnableWellKnownIdentities
Fixes: 09d9e1e0e2d9 ("policy: Disable well-known identities for non-managed etcd")

Signed-off-by: Mauricio Vásquez <mauricio@accuknox.com>
Signed-off-by: Mauricio Vásquez <mauricio@kinvolk.io>

pkg/option/config.go
@@ -2010,7 +2010,7 @@ var (
 		AutoCreateCiliumNodeResource: defaults.AutoCreateCiliumNodeResource,
 		IdentityAllocationMode:       IdentityAllocationModeKVstore,
 		AllowICMPFragNeeded:          defaults.AllowICMPFragNeeded,
-		EnableWellKnownIdentities:    defaults.EnableEndpointRoutes,
+		EnableWellKnownIdentities:    defaults.EnableWellKnownIdentities,
 		K8sEnableK8sEndpointSlice:    defaults.K8sEnableEndpointSlice,
 		k8sEnableAPIDiscovery:        defaults.K8sEnableAPIDiscovery,
 		AllocatorListTimeout:         defaults.AllocatorListTimeout,