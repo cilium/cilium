1b64d3d97c3dd614338fc06b28233279a7612608
Mauricio Vásquez <mauricio@kinvolk.io>
2021-06-04 10:36:36 -0500

    pkg/option: Fix default assignment of EnableWellKnownIdentities
    [ upstream commit 67b946de0539edea49e7fd1079c5b83681a30f74 ]
    
    Fixes: 09d9e1e0e2d9 ("policy: Disable well-known identities for non-managed etcd")
    
    Signed-off-by: Mauricio Vásquez <mauricio@accuknox.com>
    Signed-off-by: Mauricio Vásquez <mauricio@kinvolk.io>
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -2121,7 +2121,7 @@ var (
 		AutoCreateCiliumNodeResource: defaults.AutoCreateCiliumNodeResource,
 		IdentityAllocationMode:       IdentityAllocationModeKVstore,
 		AllowICMPFragNeeded:          defaults.AllowICMPFragNeeded,
-		EnableWellKnownIdentities:    defaults.EnableEndpointRoutes,
+		EnableWellKnownIdentities:    defaults.EnableWellKnownIdentities,
 		K8sEnableK8sEndpointSlice:    defaults.K8sEnableEndpointSlice,
 		k8sEnableAPIDiscovery:        defaults.K8sEnableAPIDiscovery,
 
