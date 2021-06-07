fd1f42f1fd90a86a55992fce9004241d6ec67769
Nicolas Busseneau <nicolas@isovalent.com>
2021-06-07 15:06:58 +0200

    workflows: reformat/standardize in-cluster shell scripts
    - Add comments to describe and standardize with steps name in step-based
      corresponding workflows on `cilium/cilium`.
    - Standardize steps with corresponding workflows on `cilium/cilium`
      (e.g. switching from 5s to 10s sleep after relay enable).
    - Remove `--restart-unmanaged-pods=false` flag from Cilium installs on
      GKE as it is unneeded and for consistency with `cilium/cilium`.
    - Add missing `set -e` in multicluster install script.
    
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- cilium-cli/.github/in-cluster-test-scripts/gke.sh
+++ cilium-cli/.github/in-cluster-test-scripts/gke.sh
@@ -3,16 +3,24 @@
 set -x
 set -e
 
-cilium install --cluster-name "${CLUSTER_NAME}" --restart-unmanaged-pods=false --config monitor-aggregation=none --native-routing-cidr="${CLUSTER_CIDR}"
+# Install Cilium
+cilium install \
+  --cluster-name "${CLUSTER_NAME}" \
+  --config monitor-aggregation=none \
+  --native-routing-cidr="${CLUSTER_CIDR}"
 
+# Enable Relay
 cilium hubble enable
 
+# Wait for Cilium status to be ready
 cilium status --wait
 
+# Port forward Relay
 cilium hubble port-forward&
+sleep 10s
 
-sleep 5
-
+# Run connectivity test
 cilium connectivity test --all-flows
 
+# Retrieve Cilium status
 cilium status
