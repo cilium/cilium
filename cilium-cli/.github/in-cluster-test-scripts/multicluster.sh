f541d094d5a43c540b9bb752baf91c3146eb408f
Nicolas Busseneau <nicolas@isovalent.com>
2021-06-07 15:25:59 +0200

    workflows: fix missing CIDRs in multicluster install script
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- cilium-cli/.github/in-cluster-test-scripts/multicluster.sh
+++ cilium-cli/.github/in-cluster-test-scripts/multicluster.sh
@@ -5,11 +5,11 @@ set -x
 CONTEXT1=$(kubectl config view | grep "${CLUSTER_NAME_1}" | head -1 | awk '{print $2}')
 CONTEXT2=$(kubectl config view | grep "${CLUSTER_NAME_2}" | head -1 | awk '{print $2}')
 
-cilium --context "${CONTEXT1}" install --cluster-name "${CLUSTER_NAME_1}" --cluster-id 1 --restart-unmanaged-pods=false --config monitor-aggregation=none --native-routing-cidr=10.0.0.0/9
+cilium --context "${CONTEXT1}" install --cluster-name "${CLUSTER_NAME_1}" --cluster-id 1 --restart-unmanaged-pods=false --config monitor-aggregation=none --native-routing-cidr="${CLUSTER_CIDR_1}"
 cilium --context "${CONTEXT1}" hubble enable
 cilium --context "${CONTEXT1}" clustermesh enable
 
-cilium --context "${CONTEXT2}" install --cluster-name "${CLUSTER_NAME_2}" --cluster-id 2  --restart-unmanaged-pods=false --config monitor-aggregation=none --native-routing-cidr=10.0.0.0/9 --inherit-ca "${CONTEXT1}"
+cilium --context "${CONTEXT2}" install --cluster-name "${CLUSTER_NAME_2}" --cluster-id 2  --restart-unmanaged-pods=false --config monitor-aggregation=none --native-routing-cidr="${CLUSTER_CIDR_2}" --inherit-ca "${CONTEXT1}"
 cilium --context "${CONTEXT2}" hubble enable --relay=false
 cilium --context "${CONTEXT2}" clustermesh enable
 
