cilium/cilium

commit ae23400cb944c722f629b9b3f0d7e8af58af1b34

add AlibabaCloud operator
The AlibabaCloud allocator is specific to Cilium deployments running in the AlibabaCloud and performs IP allocation based on IPs of ENI.

More for ENI https://www.alibabacloud.com/help/doc-detail/58496.htm

Signed-off-by: l1b0k <libokang.dev@gmail.com>

pkg/ipam/crd.go
@@ -25,7 +25,7 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	eniTypes "github.com/cilium/cilium/pkg/aws/eni/types"
+	alibabaCloud "github.com/cilium/cilium/pkg/alibabacloud/utils"
 	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/ip"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
@@ -216,6 +216,13 @@ func deriveVpcCIDR(node *ciliumv2.CiliumNode) (result *cidr.CIDR) {
 			return
 		}
 	}
+	// return AlibabaCloud vpc CIDR
+	if len(node.Status.AlibabaCloud.ENIs) > 0 {
+		c, err := cidr.ParseCIDR(node.Spec.AlibabaCloud.CIDRBlock)
+		if err == nil {
+			result = c
+		}
+	}
 	return
 }
 
@@ -251,7 +258,7 @@ func (n *nodeStore) hasMinimumIPsInPool() (minimumReached bool, required, numAva
 			minimumReached = true
 		}
 
-		if n.conf.IPAMMode() == ipamOption.IPAMENI {
+		if n.conf.IPAMMode() == ipamOption.IPAMENI || n.conf.IPAMMode() == ipamOption.IPAMAlibabaCloud {
 			if vpcCIDR := deriveVpcCIDR(n.ownNode); vpcCIDR != nil {
 				if nativeCIDR := n.conf.IPv4NativeRoutingCIDR(); nativeCIDR != nil {
 					logFields := logrus.Fields{
@@ -452,18 +459,18 @@ func newCRDAllocator(family Family, c Configuration, owner Owner, k8sEventReg K8
 	return allocator
 }
 
-func deriveGatewayIP(eni eniTypes.ENI) string {
-	subnetIP, _, err := net.ParseCIDR(eni.Subnet.CIDR)
+// deriveGatewayIP accept the CIDR and the index of the IP in this CIDR.
+func deriveGatewayIP(cidr string, index int) string {
+	_, ipNet, err := net.ParseCIDR(cidr)
 	if err != nil {
-		log.WithError(err).Warningf("Unable to parse AWS subnet CIDR %s", eni.Subnet.CIDR)
+		log.WithError(err).Warningf("Unable to parse subnet CIDR %s", cidr)
 		return ""
 	}
-
-	addr := subnetIP.To4()
-
-	// The gateway for a subnet and VPC is always x.x.x.1
-	// Ref: https://docs.aws.amazon.com/vpc/latest/userguide/VPC_Route_Tables.html
-	return net.IPv4(addr[0], addr[1], addr[2], addr[3]+1).String()
+	gw := ip.GetIPAtIndex(*ipNet, int64(index))
+	if gw == nil {
+		return ""
+	}
+	return gw.String()
 }
 
 func (a *crdAllocator) buildAllocationResult(ip net.IP, ipInfo *ipamTypes.AllocationIP) (result *AllocationResult, err error) {
@@ -491,16 +498,15 @@ func (a *crdAllocator) buildAllocationResult(ip net.IP, ipInfo *ipamTypes.Alloca
 					result.CIDRs = append(result.CIDRs, a.conf.IPv4NativeRoutingCIDR().String())
 				}
 				if eni.Subnet.CIDR != "" {
-					result.GatewayIP = deriveGatewayIP(eni)
+					// The gateway for a subnet and VPC is always x.x.x.1
+					// Ref: https://docs.aws.amazon.com/vpc/latest/userguide/VPC_Route_Tables.html
+					result.GatewayIP = deriveGatewayIP(eni.Subnet.CIDR, 1)
 				}
 				result.InterfaceNumber = strconv.Itoa(eni.Number)
 
 				return
 			}
 		}
-
-		result = nil
-		err = fmt.Errorf("unable to find ENI %s", ipInfo.Resource)
 
 	// In Azure mode, the Resource points to the azure interface so we can
 	// derive the master interface
@@ -513,10 +519,25 @@ func (a *crdAllocator) buildAllocationResult(ip net.IP, ipInfo *ipamTypes.Alloca
 			}
 		}
 
-		result = nil
-		err = fmt.Errorf("unable to find ENI %s", ipInfo.Resource)
+	// In AlibabaCloud mode, the Resource points to the ENI so we can derive the
+	// master interface and all CIDRs of the VPC
+	case ipamOption.IPAMAlibabaCloud:
+		for _, eni := range a.store.ownNode.Status.AlibabaCloud.ENIs {
+			if eni.NetworkInterfaceID != ipInfo.Resource {
+				continue
+			}
+			result.PrimaryMAC = eni.MACAddress
+			result.CIDRs = []string{eni.VSwitch.CIDRBlock}
+
+			// Ref: https://www.alibabacloud.com/help/doc-detail/65398.html
+			result.GatewayIP = deriveGatewayIP(eni.VSwitch.CIDRBlock, -3)
+			result.InterfaceNumber = strconv.Itoa(alibabaCloud.GetENIIndexFromTags(eni.Tags))
+			return
+		}
 	}
 
+	result = nil
+	err = fmt.Errorf("unable to find ENI %s", ipInfo.Resource)
 	return
 }
 

.github/workflows/images-legacy-hotfix-releases.yaml
@@ -25,6 +25,9 @@ jobs:
           - name: operator-azure
             dockerfile: ./images/operator-azure/Dockerfile
 
+          - name: operator-alibabacloud
+            dockerfile: ./images/operator-alibabacloud/Dockerfile
+
           - name: operator-generic
             dockerfile: ./images/operator-generic/Dockerfile
 

.github/workflows/images-legacy-releases.yaml
@@ -26,6 +26,9 @@ jobs:
           - name: operator-azure
             dockerfile: ./images/operator-azure/Dockerfile
 
+          - name: operator-alibabacloud
+            dockerfile: ./images/operator-alibabacloud/Dockerfile
+
           - name: operator-generic
             dockerfile: ./images/operator-generic/Dockerfile
 

.github/workflows/images-legacy.yaml
@@ -29,6 +29,9 @@ jobs:
           - name: operator-azure
             dockerfile: ./images/operator-azure/Dockerfile
 
+          - name: operator-alibabacloud
+            dockerfile: ./images/operator-alibabacloud/Dockerfile
+
           - name: operator-generic
             dockerfile: ./images/operator-generic/Dockerfile
 

Documentation/update-cmdref.sh
@@ -17,6 +17,7 @@ generators=(
     "operator/cilium-operator-aws --cmdref"
     "operator/cilium-operator-azure --cmdref"
     "operator/cilium-operator-generic --cmdref"
+    "operator/cilium-operator-alibabacloud --cmdref"
 )
 
 for g in "${generators[@]}" ; do

Makefile
@@ -397,6 +397,7 @@ generate-k8s-api: ## Generate Cilium k8s API client, deepcopy and deepequal Go s
 	aws:types\
 	azure:types\
 	ipam:types\
+	alibabacloud:types\
 	k8s:types\
 	maps:ctmap\
 	maps:encrypt\
@@ -420,6 +421,7 @@ generate-k8s-api: ## Generate Cilium k8s API client, deepcopy and deepequal Go s
 	$(call generate_k8s_api_deepcopy_deepequal,github.com/cilium/cilium/pkg/policy,"api:kafka")
 	$(call generate_k8s_api_all,github.com/cilium/cilium/pkg/k8s/apis,"cilium.io:v2 cilium.io:v2alpha1")
 	$(call generate_k8s_api_deepcopy_deepequal,github.com/cilium/cilium/pkg/aws,"eni:types")
+	$(call generate_k8s_api_deepcopy_deepequal,github.com/cilium/cilium/pkg/alibabacloud,"eni:types")
 	$(call generate_k8s_api_deepcopy_deepequal,github.com/cilium/cilium/api,"v1:models")
 	$(call generate_k8s_api_deepcopy_deepequal,github.com/cilium/cilium,"$\
 	pkg:bpf\

Makefile.docker
@@ -125,8 +125,8 @@ $(eval $(call DOCKER_IMAGE_TEMPLATE,dev-docker-opera%-image,images/opera%/Docker
 #
 docker-images-all: docker-cilium-image docker-plugin-image docker-hubble-relay-image docker-clustermesh-apiserver-image docker-operator-images-all ## Build all Cilium related docker images.
 
-docker-images-all-unstripped: docker-cilium-image-unstripped docker-plugin-image-unstripped docker-hubble-relay-image-unstripped docker-clustermesh-apiserver-image-unstripped docker-operator-images-all-unstripped ## Build all Cilium related unstripped docker images. 
+docker-images-all-unstripped: docker-cilium-image-unstripped docker-plugin-image-unstripped docker-hubble-relay-image-unstripped docker-clustermesh-apiserver-image-unstripped docker-operator-images-all-unstripped ## Build all Cilium related unstripped docker images.
 
-docker-operator-images-all: docker-operator-image docker-operator-aws-image docker-operator-azure-image docker-operator-generic-image ## Build all variants of cilium-operator images.
+docker-operator-images-all: docker-operator-image docker-operator-aws-image docker-operator-azure-image docker-operator-alibabacloud-image docker-operator-generic-image ## Build all variants of cilium-operator images.
 
-docker-operator-images-all-unstripped: docker-operator-image-unstripped docker-operator-aws-image-unstripped docker-operator-azure-image-unstripped docker-operator-generic-image-unstripped ## Build all variants of unstripped cilium-operator images.
+docker-operator-images-all-unstripped: docker-operator-image-unstripped docker-operator-aws-image-unstripped docker-operator-azure-image-unstripped docker-operator-alibabacloud-image-unstripped docker-operator-generic-image-unstripped ## Build all variants of unstripped cilium-operator images.

cilium-health/launch/endpoint.go
@@ -338,7 +338,7 @@ func LaunchAsEndpoint(baseCtx context.Context,
 		return nil, fmt.Errorf("Error while configuring routes: %s", err)
 	}
 
-	if option.Config.IPAM == ipamOption.IPAMENI {
+	if option.Config.IPAM == ipamOption.IPAMENI || option.Config.IPAM == ipamOption.IPAMAlibabaCloud {
 		// ENI mode does not support IPv6.
 		if err := routingConfig.Configure(
 			healthIP,

commit 75d4e37b370f9ee80a786ef6e23b4237279dd0d5

ipam: fix crd mode
Fix regression affecting IPAM CRD mode introduced in #15160.

In buildAllocationResult an "ENI" error was wrongly constructed
in fallthrough case affecting all modes not specifically handled
within the switch statement.

Fixes: #16492
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/ipam/crd.go
@@ -515,6 +515,7 @@ func (a *crdAllocator) buildAllocationResult(ip net.IP, ipInfo *ipamTypes.Alloca
 				return
 			}
 		}
+		return nil, fmt.Errorf("unable to find ENI %s", ipInfo.Resource)
 
 	// In Azure mode, the Resource points to the azure interface so we can
 	// derive the master interface
@@ -538,6 +539,7 @@ func (a *crdAllocator) buildAllocationResult(ip net.IP, ipInfo *ipamTypes.Alloca
 				return
 			}
 		}
+		return nil, fmt.Errorf("unable to find ENI %s", ipInfo.Resource)
 
 	// In AlibabaCloud mode, the Resource points to the ENI so we can derive the
 	// master interface and all CIDRs of the VPC
@@ -554,10 +556,9 @@ func (a *crdAllocator) buildAllocationResult(ip net.IP, ipInfo *ipamTypes.Alloca
 			result.InterfaceNumber = strconv.Itoa(alibabaCloud.GetENIIndexFromTags(eni.Tags))
 			return
 		}
+		return nil, fmt.Errorf("unable to find ENI %s", ipInfo.Resource)
 	}
 
-	result = nil
-	err = fmt.Errorf("unable to find ENI %s", ipInfo.Resource)
 	return
 }
 