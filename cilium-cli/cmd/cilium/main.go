9f9694064a0fc8ac3d2264c01d895114e881f606
Jarno Rajahalme <jarno@isovalent.com>
2021-06-15 01:36:39 -0700

    main: Add gops agent
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- cilium-cli/cmd/cilium/main.go
+++ cilium-cli/cmd/cilium/main.go
@@ -16,12 +16,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/cilium/cilium-cli/internal/cli/cmd"
+
+	gops "github.com/google/gops/agent"
 )
 
 func main() {
+	if err := gops.Listen(gops.Options{}); err != nil {
+		log.Printf("Unable to start gops: %s", err)
+	}
+
 	if err := cmd.NewDefaultCiliumCommand().Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
