2a1fc5b903d26202e4b7cc596e63dc33f76dfffd
Gilberto Bertin <gilberto@isovalent.com>
2021-06-18 09:04:49 +0200

    maps: switch maglev to cilium/ebpf package
    Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

--- pkg/ebpf/map.go
+++ pkg/ebpf/map.go
@@ -31,8 +31,15 @@ type MapSpec = ciliumebpf.MapSpec
 
 const (
 	PerCPUHash = ciliumebpf.PerCPUHash
+	Array      = ciliumebpf.Array
+	HashOfMaps = ciliumebpf.HashOfMaps
 
 	PinByName = ciliumebpf.PinByName
+)
+
+var (
+	ErrKeyNotExist = ciliumebpf.ErrKeyNotExist
+	LoadPinnedMap  = ciliumebpf.LoadPinnedMap
 )
 
 // IterateCallback represents the signature of the callback function expected by
@@ -56,6 +63,37 @@ func NewMap(spec *MapSpec) *Map {
 	}
 }
 
+// OpenMap opens the given bpf map and generates the Map object based on the
+// information stored in the bpf map.
+func OpenMap(mapName string) (*Map, error) {
+	path := bpf.MapPath(mapName)
+
+	newMap, err := LoadPinnedMap(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	m := &Map{
+		Map:  newMap,
+		path: path,
+	}
+
+	registerMap(m)
+
+	return m, nil
+}
+
+func MapFromID(id int) (*Map, error) {
+	newMap, err := ciliumebpf.NewMapFromID(ciliumebpf.MapID(id))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Map{
+		Map: newMap,
+	}, nil
+}
+
 // OpenOrCreate tries to open or create the eBPF map identified by the spec in
 // the Map object.
 func (m *Map) OpenOrCreate() error {
@@ -64,6 +102,10 @@ func (m *Map) OpenOrCreate() error {
 
 	if m.Map != nil {
 		return nil
+	}
+
+	if m.spec == nil {
+		return fmt.Errorf("cannot create map: nil map spec")
 	}
 
 	opts := ciliumebpf.MapOptions{
