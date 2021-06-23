b14522264906c744f8cb11408ad5722300a0908b
Paul Chaignon <paul@cilium.io>
2021-06-23 16:00:21 +0200

    iptables: Don't masquerade traffic to cluster nodes
    When using BPF masquerading, we don't masquerade traffic destined to
    cluster nodes in native routing mode. We detect those destination
    using the security identity.
    
    When using iptables masquerading, we cannot implement the exact same
    because we can't match on security identities. Instead, we need to
    maintain an ipset of IP addresses belonging to cluster nodes and skip
    the iptables masquerading rules when a packet is destined to an IP in
    the ipset.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/datapath/iptables/iptables_test.go
+++ pkg/datapath/iptables/iptables_test.go
@@ -44,6 +44,7 @@ type expectation struct {
 type mockIptables struct {
 	c            *check.C
 	prog         string
+	ipset        string
 	expectations []expectation
 	index        int
 }
@@ -54,6 +55,10 @@ func (ipt *mockIptables) addExpectation(args string, out []byte, err error) {
 
 func (ipt *mockIptables) getProg() string {
 	return ipt.prog
+}
+
+func (ipt *mockIptables) getIpset() string {
+	return ipt.ipset
 }
 
 func (ipt *mockIptables) getVersion() (semver.Version, error) {
