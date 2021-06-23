18dfbe417cb01e0576684a986125aed2495ecc0c
Michi Mutsuzaki <michi@isovalent.com>
2021-06-22 19:38:42 -0700

    Revert "test: Add HTTP visibility annotation to echo-other-node"
    [ cherry-picked from cilium/cilium-cli repository ]
    
    This reverts commit 7e54477864c4251119aea676f12ffa94b9f66f74.
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- cilium-cli/connectivity/check/deployment.go
+++ cilium-cli/connectivity/check/deployment.go
@@ -49,7 +49,6 @@ type deploymentParameters struct {
 	Affinity       *corev1.Affinity
 	ReadinessProbe *corev1.Probe
 	Labels         map[string]string
-	Annotations    map[string]string
 }
 
 func newDeployment(p deploymentParameters) *appsv1.Deployment {
@@ -73,7 +72,6 @@ func newDeployment(p deploymentParameters) *appsv1.Deployment {
 						"name": p.Name,
 						"kind": p.Kind,
 					},
-					Annotations: make(map[string]string, len(p.Annotations)),
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
@@ -111,9 +109,6 @@ func newDeployment(p deploymentParameters) *appsv1.Deployment {
 
 	for k, v := range p.Labels {
 		dep.Spec.Template.ObjectMeta.Labels[k] = v
-	}
-	for k, v := range p.Annotations {
-		dep.Spec.Template.ObjectMeta.Annotations[k] = v
 	}
 
 	return dep
@@ -321,7 +316,6 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 					},
 				},
 				ReadinessProbe: newLocalReadinessProbe(8080, "/"),
-				Annotations:    map[string]string{"io.cilium.proxy-visibility": "<Ingress/8080/TCP/HTTP>"},
 			})
 
 			_, err = ct.clients.dst.CreateDeployment(ctx, ct.params.TestNamespace, echoOtherNodeDeployment, metav1.CreateOptions{})
