9532e5a09e660643003f39438dbe4083cebb5c0c
Tobias Klauser <tobias@cilium.io>
2021-06-29 09:40:53 +0200

    version: allow to build on non-unix platforms
    [ upstream commit a4123c84c91735b2e2325720ba384aac8e1d5b6b ]
    
    This package is a transitive dependency in cilium-cli, but currently it
    fails to build on non-unix platforms (e.g. windows) due to its use of
    golang.org/x/sys/unix.
    
    However, we want still to be able to build it for some these platforms,
    see e.g. https://github.com/cilium/cilium-cli/issues/231. Fix the build
    by moving all unix-specific functionality (i.e. the Uname call) to a
    separate file, protected by build tags.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2017-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,14 +18,8 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"regexp"
 	"runtime"
 	"strings"
-
-	"github.com/cilium/cilium/pkg/versioncheck"
-
-	"github.com/blang/semver/v4"
-	"golang.org/x/sys/unix"
 )
 
 // CiliumVersion provides a minimal structure to the version string
@@ -85,35 +79,4 @@ func Base64() (string, error) {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(jsonBytes), nil
-}
-
-func parseKernelVersion(ver string) (semver.Version, error) {
-	verStrs := strings.Split(ver, ".")
-	switch {
-	case len(verStrs) < 2:
-		return semver.Version{}, fmt.Errorf("unable to get kernel version from %q", ver)
-	case len(verStrs) < 3:
-		verStrs = append(verStrs, "0")
-	}
-	// We are assuming the kernel version will be something as:
-	// 4.9.17-040917-generic
-
-	// If verStrs is []string{ "4", "9", "17-040917-generic" }
-	// then we need to retrieve patch number.
-	patch := regexp.MustCompilePOSIX(`^[0-9]+`).FindString(verStrs[2])
-	if patch == "" {
-		verStrs[2] = "0"
-	} else {
-		verStrs[2] = patch
-	}
-	return versioncheck.Version(strings.Join(verStrs[:3], "."))
-}
-
-// GetKernelVersion returns the version of the Linux kernel running on this host.
-func GetKernelVersion() (semver.Version, error) {
-	var unameBuf unix.Utsname
-	if err := unix.Uname(&unameBuf); err != nil {
-		return semver.Version{}, err
-	}
-	return parseKernelVersion(string(unameBuf.Release[:]))
 }
