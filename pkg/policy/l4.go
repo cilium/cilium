fe78c118333c83688a34f2ad5c822fd5b7e2f0fe
André Martins <andre@cilium.io>
2021-07-04 01:22:39 +0200

    Revert "policy: Make selectorcache callbacks lock-free"
    [ upstream commit a97bd0d8f99fa18be533570b9f6afa4ce4649f3a ]
    
    This reverts commit a75599da7964fb5e24c3362dfbdedf7d2f455089.
    has it seems to be causing a lot of FQDN flakes through the entire CI.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/policy/l4.go
+++ pkg/policy/l4.go
@@ -34,7 +34,7 @@ import (
 	"github.com/cilium/cilium/pkg/policy/api"
 	"github.com/cilium/cilium/pkg/policy/trafficdirection"
 	"github.com/cilium/cilium/pkg/u8proto"
-	cilium "github.com/cilium/proxy/go/cilium/api"
+	"github.com/cilium/proxy/go/cilium/api"
 
 	"github.com/sirupsen/logrus"
 )
@@ -426,8 +426,8 @@ func (l4Filter *L4Filter) ToMapState(policyOwner PolicyOwner, direction trafficd
 }
 
 // IdentitySelectionUpdated implements CachedSelectionUser interface
-// This call is made from a single goroutine in FIFO order to keep add
-// and delete events ordered properly. No locks are held.
+// This call is made while holding name manager and selector cache
+// locks, must beware of deadlocking!
 //
 // The caller is responsible for making sure the same identity is not
 // present in both 'added' and 'deleted'.
