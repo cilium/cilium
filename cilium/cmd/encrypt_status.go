3e8a5db8f3badebb836802fb9c8186c3b9c74b74
Gaurav Genani <h3llix.pvt@gmail.com>
2021-07-05 02:31:12 +0530

    feat(cilium encrypt status):  max seq. number for ipsec states
    Signed-off-by: Gaurav Genani <h3llix.pvt@gmail.com>

--- cilium/cmd/encrypt_status.go
+++ cilium/cmd/encrypt_status.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,10 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
 	"reflect"
+	"regexp"
+	"strings"
 
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
@@ -35,7 +38,7 @@ const (
 	EncryptionModeIPsec string = "IPsec"
 
 	// EncryptionModeWireguard captures enum value "Wireguard"
-	EncryptionModeWireguard string = "Wireguard"
+	EncryptionModeWireguard string = "WireGuard"
 )
 
 type void struct{}
@@ -47,7 +50,7 @@ var (
 
 var encryptStatusCmd = &cobra.Command{
 	Use:   "status",
-	Short: "Display current state of IPSec configurations",
+	Short: "Display current state of encryption configurations",
 	Run: func(cmd *cobra.Command, args []string) {
 		common.RequireRootPrivilege("cilium encrypt status")
 		dumpStatus()
@@ -59,7 +62,7 @@ func init() {
 	command.AddJSONOutput(encryptStatusCmd)
 }
 
-func getIpsecmode() string {
+func getEncryptionMode() string {
 	if option.Config.EnableIPSec {
 		return EncryptionModeIPsec
 	} else if option.Config.EnableWireguard {
@@ -69,10 +72,15 @@ func getIpsecmode() string {
 }
 
 func getXfrmStats() (int, map[string]int) {
-	fs, _ := procfs.NewDefaultFS()
-	stats, _ := fs.NewXfrmStat()
+	fs, err := procfs.NewDefaultFS()
+	if err != nil {
+		Fatalf("Cannot get a new proc FS: %s", err)
+	}
+	stats, err := fs.NewXfrmStat()
+	if err != nil {
+		Fatalf("Cannot get XFRM states from proc filesystem: %s", err)
+	}
 	v := reflect.ValueOf(stats)
-
 	errorMap := make(map[string]int)
 	for i := 0; i < v.NumField(); i++ {
 		name := v.Type().Field(i).Name
@@ -88,14 +96,42 @@ func getXfrmStats() (int, map[string]int) {
 func countUniqueIpsecKeys() int {
 	// trying to mimic set type data structure
 	// using void data type as struct{} because it does not use any memory
-	set := make(map[string]void)
+	keys := make(map[string]void)
 	xfrmStates, _ := netlink.XfrmStateList(0)
 	for _, v := range xfrmStates {
-		set[string(v.Crypt.Key)] = voidType
+		keys[string(v.Crypt.Key)] = voidType
 	}
-	return len(set)
+	return len(keys)
+}
+
+func maxSequenceNumber() string {
+	var maxSeqNum string
+	out, err := exec.Command("ip", "xfrm", "state").Output()
+	if err != nil {
+		Fatalf("Cannot get xfrm states: %s", err)
+	}
+	commandOutput := string(out)
+	lines := strings.Split(commandOutput, "\n")
+	regex := regexp.MustCompile("oseq[[:blank:]](0[xX][[:xdigit:]]+)?")
+	for _, line := range lines {
+		matched := regex.FindStringSubmatchIndex(line)
+		if matched != nil {
+			maxSeqNum = line[matched[2]:matched[3]]
+		}
+	}
+	return maxSeqNum
 }
 
 func dumpStatus() {
-	fmt.Printf("Status%-26s", getIpsecmode())
+	fmt.Printf("Encryption:%-26s\n", getEncryptionMode())
+	fmt.Printf("Node Encryption: <TODO>\n")
+	fmt.Printf("Keys in use:%-26d\n", countUniqueIpsecKeys())
+	fmt.Printf("Max Seq. Number:%s/%s\n", maxSequenceNumber(), "0xffffffff")
+	errCount, errMap := getXfrmStats()
+	if errCount != 0 {
+		fmt.Printf("Errors:%-26d", errCount)
+		for k, v := range errMap {
+			fmt.Println(k, ":", v)
+		}
+	}
 }
