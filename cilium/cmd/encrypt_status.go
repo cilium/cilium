aad57126a5b2364ac737b405872e6fbb7578bc47
Gaurav Genani <h3llix.pvt@gmail.com>
2021-07-04 22:46:18 +0530

    feat(cilium encypt): get IPsec status and number of unique IPsec keys
    Signed-off-by: Gaurav Genani <h3llix.pvt@gmail.com>

--- cilium/cmd/encrypt_status.go
+++ cilium/cmd/encrypt_status.go
@@ -16,11 +16,33 @@ package cmd
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/prometheus/procfs"
+	"github.com/vishvananda/netlink"
 
 	"github.com/spf13/cobra"
+)
+
+const (
+	// EncryptionModeDisabled captures enum value "Disabled"
+	EncryptionModeDisabled string = "Disabled"
+
+	// EncryptionModeIPsec captures enum value "IPsec"
+	EncryptionModeIPsec string = "IPsec"
+
+	// EncryptionModeWireguard captures enum value "Wireguard"
+	EncryptionModeWireguard string = "Wireguard"
+)
+
+type void struct{}
+
+var (
+	voidType    void
+	countErrors int
 )
 
 var encryptStatusCmd = &cobra.Command{
@@ -37,6 +59,43 @@ func init() {
 	command.AddJSONOutput(encryptStatusCmd)
 }
 
+func getIpsecmode() string {
+	if option.Config.EnableIPSec {
+		return EncryptionModeIPsec
+	} else if option.Config.EnableWireguard {
+		return EncryptionModeWireguard
+	}
+	return EncryptionModeDisabled
+}
+
+func getXfrmStats() (int, map[string]int) {
+	fs, _ := procfs.NewDefaultFS()
+	stats, _ := fs.NewXfrmStat()
+	v := reflect.ValueOf(stats)
+
+	errorMap := make(map[string]int)
+	for i := 0; i < v.NumField(); i++ {
+		name := v.Type().Field(i).Name
+		value := v.Field(i).Interface().(int)
+		if value != 0 {
+			countErrors += value
+			errorMap[name] = value
+		}
+	}
+	return countErrors, errorMap
+}
+
+func countUniqueIpsecKeys() int {
+	// trying to mimic set type data structure
+	// using void data type as struct{} because it does not use any memory
+	set := make(map[string]void)
+	xfrmStates, _ := netlink.XfrmStateList(0)
+	for _, v := range xfrmStates {
+		set[string(v.Crypt.Key)] = voidType
+	}
+	return len(set)
+}
+
 func dumpStatus() {
-	fmt.Print("Worked")
+	fmt.Printf("Status%-26s", getIpsecmode())
 }
