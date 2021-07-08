1280f998758dff14af4df51b0499c4d20783704d
Joe Stringer <joe@cilium.io>
2021-07-08 13:40:00 -0700

    test: Redeploy DNS pods in AfterAll for datapath tests
    Commit a0e77127dcd7 ("test: Redeploy DNS after changing endpointRoutes")
    didn't go quite far enough: It ensured that between individual tests in
    a given file, the DNS pods would be redeployed during the next run if
    there were significant enough datapath changes. However, the way it did
    this was by storing state within the 'kubectl' variable, which is
    recreated in each test file. So if the last test in one CI run enabled
    endpoint routes mode, then the DNS pods would not be redeployed to
    disable endpoint routes mode as part of the next test.
    
    Fix it by redeploying DNS after removing Cilium from the cluster.
    Kubernetes will remove the current DNS pods and reschedule them, but
    they will not launch until the next test deploys a new version of
    Cilium.
    
    Reported-by: Chris Tarazi <chris@isovalent.com>
    Fixes: 0e77127dcd7 ("test: Redeploy DNS after changing endpointRoutes")
    Related: #16717
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- test/k8sT/CustomCalls.go
+++ test/k8sT/CustomCalls.go
@@ -113,6 +113,7 @@ var _ = Describe("K8sCustomCalls", func() {
 		AfterAll(func() {
 			deploymentManager.DeleteCilium()
 			deploymentManager.DeleteAll()
+			kubectl.RedeployDNS()
 		})
 
 		installPods := func() {
