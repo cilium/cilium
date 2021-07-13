cilium/cilium

commit b6989723a7cc0ebc07345d78d28b63bf3825f7ba

cilium: ipsec, support rolling updates
Currently, rolling updates may get stuck due to a time period between
when some set of nodes have started with encryption enabled and another
set exist without encryption enabled. At this point these two sets of
nodes can only communicate from non-encrypted to encrypted set. The
set with encryption enabled will encrypt traffic that will in turn be
dropped by the set that has yet to enable encryption.

To resolve we make encryption a property of the endpoint id. Keeping
the key identifier with the endpoint to inform cilium which key
should be used during an upgrade. Because we use the mark space
to identify keys we limit the key space to two keys currently.

After this key secrets will need to be updated to include an id
field as follows,

  keys: "1 hmac(sha256) 0123456789abcdef0123456789abcdef cbc(aes) 0123456789abcdef0123456789abcdef"

Where '1' is the id here. IDs are enforced to be less than 16. This
is a bit arbitrary we could go as high as 256 without hitting mark
bit limits. However, 16 feels sufficient and we cant take bits back
later so start low and bump up if needed. The id '0' is a special id
and should not be used it represents the absence of keys in the
datapath.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -25,6 +25,7 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
@@ -43,7 +44,7 @@ const (
 )
 
 type ipSecKey struct {
-	Spi   int
+	Spi   uint8
 	ReqID int
 	Auth  *netlink.XfrmStateAlgo
 	Crypt *netlink.XfrmStateAlgo
@@ -79,7 +80,7 @@ func ipSecAttachPolicyTempl(policy *netlink.XfrmPolicy, keys *ipSecKey, srcIP, d
 	tmpl := netlink.XfrmPolicyTmpl{
 		Proto: netlink.XFRM_PROTO_ESP,
 		Mode:  netlink.XFRM_MODE_TUNNEL,
-		Spi:   keys.Spi,
+		Spi:   int(keys.Spi),
 		Reqid: keys.ReqID,
 		Dst:   dstIP,
 		Src:   srcIP,
@@ -91,22 +92,20 @@ func ipSecAttachPolicyTempl(policy *netlink.XfrmPolicy, keys *ipSecKey, srcIP, d
 func ipSecJoinState(state *netlink.XfrmState, keys *ipSecKey) {
 	state.Auth = keys.Auth
 	state.Crypt = keys.Crypt
-	state.Spi = keys.Spi
+	state.Spi = int(keys.Spi)
 	state.Reqid = keys.ReqID
 }
 
-func ipSecReplaceState(remoteIP, localIP net.IP, spi int) error {
-	state := ipSecNewState()
-
+func ipSecReplaceState(remoteIP, localIP net.IP) (uint8, error) {
 	key := getIPSecKeys(localIP)
 	if key == nil {
-		return fmt.Errorf("IPSec key missing")
+		return 0, fmt.Errorf("IPSec key missing")
 	}
-	key.Spi = spi
+	state := ipSecNewState()
 	ipSecJoinState(state, key)
 	state.Src = localIP
 	state.Dst = remoteIP
-	return netlink.XfrmStateAdd(state)
+	return key.Spi, netlink.XfrmStateAdd(state)
 }
 
 func ipSecReplacePolicyIn(src, dst *net.IPNet) error {
@@ -119,35 +118,42 @@ func ipSecReplacePolicyIn(src, dst *net.IPNet) error {
 }
 
 func ipSecReplacePolicyInFwd(src, dst *net.IPNet, dir netlink.Dir) error {
+	var spiWide uint32
+
+	key := getIPSecKeys(dst.IP)
+	if key == nil {
+		return fmt.Errorf("IPSec key missing")
+	}
+	spiWide = uint32(key.Spi)
+
 	policy := ipSecNewPolicy()
 	policy.Dir = dir
 	policy.Src = src
 	policy.Dst = dst
 	policy.Mark = &netlink.XfrmMark{
-		Value: linux_defaults.RouteMarkDecrypt,
-		Mask:  linux_defaults.RouteMarkMask,
-	}
-
-	key := getIPSecKeys(dst.IP)
-	if key == nil {
-		return fmt.Errorf("IPSec key missing")
+		Value: ((spiWide << 12) | linux_defaults.RouteMarkDecrypt),
+		Mask:  linux_defaults.IPsecMarkMask,
 	}
 	ipSecAttachPolicyTempl(policy, key, src.IP, dst.IP)
 	return netlink.XfrmPolicyUpdate(policy)
 }
 
-func ipSecReplacePolicyOut(src, dst *net.IPNet) error {
+func ipSecReplacePolicyOut(src, dst *net.IPNet, dir IPSecDir) error {
+	var spiWide uint32
+
+	key := getIPSecKeys(dst.IP)
+	if key == nil {
+		return fmt.Errorf("IPSec key missing")
+	}
+	spiWide = uint32(key.Spi)
+
 	policy := ipSecNewPolicy()
 	policy.Dir = netlink.XFRM_DIR_OUT
 	policy.Src = src
 	policy.Dst = dst
 	policy.Mark = &netlink.XfrmMark{
-		Value: linux_defaults.RouteMarkEncrypt,
-		Mask:  linux_defaults.RouteMarkMask,
-	}
-	key := getIPSecKeys(dst.IP)
-	if key == nil {
-		return fmt.Errorf("IPSec key missing")
+		Value: ((spiWide << 12) | linux_defaults.RouteMarkEncrypt),
+		Mask:  linux_defaults.IPsecMarkMask,
 	}
 	ipSecAttachPolicyTempl(policy, key, src.IP, dst.IP)
 	return netlink.XfrmPolicyUpdate(policy)
@@ -175,7 +181,7 @@ func ipSecDeletePolicy(src, local net.IP) error {
 	return nil
 }
 
-/* UpsertIPSecEndpoint updates the IPSec context for a new endpoint inserted in
+/* UpsertIPsecEndpoint updates the IPSec context for a new endpoint inserted in
  * the ipcache. Currently we support a global crypt/auth keyset that will encrypt
  * all traffic between endpoints. An IPSec context consists of two pieces a policy
  * and a state, the security policy database (SPD) and security association
@@ -215,7 +221,10 @@ func ipSecDeletePolicy(src, local net.IP) error {
  * state space. Basic idea would be to reference a state using any key generated
  * from BPF program allowing for a single state per security ctx.
  */
-func UpsertIPSecEndpoint(local, remote *net.IPNet, spi int, dir IPSecDir) error {
+func UpsertIPsecEndpoint(local, remote *net.IPNet, dir IPSecDir) (uint8, error) {
+	var spi uint8
+	var err error
+
 	/* TODO: state reference ID is (dip,spi) which can be duplicated in the current global
 	 * mode. The duplication is on _all_ ingress states because dst_ip == host_ip in this
 	 * case and only a single spi entry is in use. Currently no check is done to avoid
@@ -229,33 +238,33 @@ func UpsertIPSecEndpoint(local, remote *net.IPNet, spi int, dir IPSecDir) error 
 	 */
 	if !local.IP.Equal(remote.IP) {
 		if dir == IPSecDirIn || dir == IPSecDirBoth {
-			if err := ipSecReplaceState(local.IP, remote.IP, spi); err != nil {
+			if spi, err = ipSecReplaceState(local.IP, remote.IP); err != nil {
 				if !os.IsExist(err) {
-					return fmt.Errorf("unable to replace local state: %s", err)
+					return 0, fmt.Errorf("unable to replace local state: %s", err)
 				}
 			}
-			if err := ipSecReplacePolicyIn(remote, local); err != nil {
+			if err = ipSecReplacePolicyIn(remote, local); err != nil {
 				if !os.IsExist(err) {
-					return fmt.Errorf("unable to replace policy in: %s", err)
+					return 0, fmt.Errorf("unable to replace policy in: %s", err)
 				}
 			}
 		}
 
 		if dir == IPSecDirOut || dir == IPSecDirBoth {
-			if err := ipSecReplaceState(remote.IP, local.IP, spi); err != nil {
+			if spi, err = ipSecReplaceState(remote.IP, local.IP); err != nil {
 				if !os.IsExist(err) {
-					return fmt.Errorf("unable to replace remote state: %s", err)
+					return 0, fmt.Errorf("unable to replace remote state: %s", err)
 				}
 			}
 
-			if err := ipSecReplacePolicyOut(local, remote); err != nil {
+			if err = ipSecReplacePolicyOut(local, remote, dir); err != nil {
 				if !os.IsExist(err) {
-					return fmt.Errorf("unable to replace policy out: %s", err)
+					return 0, fmt.Errorf("unable to replace policy out: %s", err)
 				}
 			}
 		}
 	}
-	return nil
+	return spi, nil
 }
 
 // DeleteIPSecEndpoint deletes the endpoint from IPSec SPD and SAD
@@ -289,21 +298,24 @@ func decodeIPSecKey(keyRaw string) ([]byte, error) {
 
 // LoadIPSecKeysFile imports IPSec auth and crypt keys from a file. The format
 // is to put a key per line as follows, (auth-algo auth-key enc-algo enc-key)
-func LoadIPSecKeysFile(path string) error {
+func LoadIPSecKeysFile(path string) (uint8, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	defer file.Close()
 	return loadIPSecKeys(file)
 }
 
-func loadIPSecKeys(r io.Reader) error {
+func loadIPSecKeys(r io.Reader) (uint8, error) {
+	var spi uint8
+
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
+		offset := 0
+
 		ipSecKey := &ipSecKey{
-			Spi:   1,
 			ReqID: 1,
 		}
 
@@ -311,20 +323,32 @@ func loadIPSecKeys(r io.Reader) error {
 		//    auth-algo auth-key enc-algo enc-key
 		s := strings.Split(scanner.Text(), " ")
 		if len(s) < 4 {
-			return fmt.Errorf("missing IPSec keys or invalid format")
+			return 0, fmt.Errorf("missing IPSec keys or invalid format")
 		}
 
-		authkey, err := decodeIPSecKey(s[1])
+		spiI, err := strconv.Atoi(s[0])
 		if err != nil {
-			return fmt.Errorf("unable to decode authkey string %q", s[1])
+			// If no version info is provided assume using key format without
+			// versioning and assign SPI.
+			spiI = 1
+			offset = -1
 		}
-		authname := s[0]
+		if spiI > linux_defaults.IPsecMaxKeyVersion {
+			return 0, fmt.Errorf("encryption Key space exhausted, id must be less than %d. Attempted %q", linux_defaults.IPsecMaxKeyVersion, s[0])
+		}
+		spi = uint8(spiI)
 
-		enckey, err := decodeIPSecKey(s[3])
+		authkey, err := decodeIPSecKey(s[2+offset])
 		if err != nil {
-			return fmt.Errorf("unable to decode enckey string %q", s[3])
+			return 0, fmt.Errorf("unable to decode authkey string %q", s[1+offset])
 		}
-		encname := s[2]
+		authname := s[1+offset]
+
+		enckey, err := decodeIPSecKey(s[4+offset])
+		if err != nil {
+			return 0, fmt.Errorf("unable to decode enckey string %q", s[3+offset])
+		}
+		encname := s[3+offset]
 
 		ipSecKey.Auth = &netlink.XfrmStateAlgo{
 			Name: authname,
@@ -334,13 +358,15 @@ func loadIPSecKeys(r io.Reader) error {
 			Name: encname,
 			Key:  enckey,
 		}
-		if len(s) == 5 {
-			ipSecKeysGlobal[s[4]] = ipSecKey
+		ipSecKey.Spi = spi
+
+		if len(s) == 6+offset {
+			ipSecKeysGlobal[s[5+offset]] = ipSecKey
 		} else {
 			ipSecKeysGlobal[""] = ipSecKey
 		}
 	}
-	return nil
+	return spi, nil
 }
 
 // EnableIPv6Forwarding sets proc file to enable IPv6 forwarding

pkg/datapath/linux/linux_defaults/linux_defaults.go
@@ -36,9 +36,9 @@ const (
 	// TunnelDeviceName the default name of the tunnel device when using vxlan
 	TunnelDeviceName = "cilium_vxlan"
 
-	// IPSec SPI value for endpoint rules
-	IPSecEndpointSPI = 1
+	// IPSec offset value for node rules
+	IPsecMaxKeyVersion = 16
 
-	// IPSec SPI value for node rules
-	IPSecNodeSPI = 2
+	// IPsecMarkMask is the mask required for the IPsec SPI and encrypt/decrypt bits
+	IPsecMarkMask = 0xFF00
 )

bpf/bpf_ipsec.c
@@ -27,8 +27,8 @@
 __section("from-netdev")
 int from_netdev(struct __sk_buff *skb)
 {
-	if (skb->cb[0] == MARK_MAGIC_ENCRYPT) {
-		skb->mark = MARK_MAGIC_ENCRYPT;
+	if ((skb->cb[0] & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_ENCRYPT) {
+		skb->mark = skb->cb[0];
 		set_identity(skb, skb->cb[1]);
 	}
 	return TC_ACT_OK;

bpf/bpf_lxc.c
@@ -146,6 +146,7 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	void *data, *data_end;
 	union v6addr *daddr, orig_dip;
 	__u32 tunnel_endpoint = 0;
+	__u8 encrypt_key = 0;
 	__u32 monitor = 0;
 
 	if (unlikely(!is_valid_lxc_src_ip(ip6)))
@@ -221,6 +222,7 @@ skip_service_lookup:
 		if (info != NULL && info->sec_label) {
 			*dstID = info->sec_label;
 			tunnel_endpoint = info->tunnel_endpoint;
+			encrypt_key = info->key;
 		} else {
 			*dstID = WORLD_ID;
 		}
@@ -331,7 +333,7 @@ skip_service_lookup:
 	/* The packet goes to a peer not managed by this agent instance */
 #ifdef ENCAP_IFINDEX
 	if (tunnel_endpoint) {
-		ret = encap_and_redirect_with_nodeid_from_lxc(skb, tunnel_endpoint, SECLABEL, monitor);
+		ret = encap_and_redirect_with_nodeid_from_lxc(skb, tunnel_endpoint, encrypt_key, SECLABEL, monitor);
 		/* If not redirected noteable due to IPSEC then pass up to stack
 		 * for further processing.
 		 */
@@ -475,6 +477,7 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 	struct ct_state ct_state = {};
 	__be32 orig_dip;
 	__u32 tunnel_endpoint = 0;
+	__u8 encrypt_key = 0;
 	__u32 monitor = 0;
 
 	if (!revalidate_data(skb, &data, &data_end, &ip4))
@@ -537,6 +540,7 @@ skip_service_lookup:
 		if (info != NULL && info->sec_label) {
 			*dstID = info->sec_label;
 			tunnel_endpoint = info->tunnel_endpoint;
+			encrypt_key = info->key;
 		} else {
 			*dstID = WORLD_ID;
 		}
@@ -649,6 +653,7 @@ skip_service_lookup:
 #ifdef ENCAP_IFINDEX
 	if (tunnel_endpoint) {
 		int ret = encap_and_redirect_with_nodeid_from_lxc(skb, tunnel_endpoint,
+								  encrypt_key,
 								  SECLABEL, monitor);
 		/* If not redirected noteably due to IPSEC then pass up to stack
 		 * for further processing.

bpf/bpf_netdev.c
@@ -242,6 +242,7 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 	info = ipcache_lookup6(&IPCACHE_MAP, dst, V6_CACHE_KEY_LEN);
 	if (info != NULL && info->tunnel_endpoint != 0) {
 		int ret = encap_and_redirect_with_nodeid(skb, info->tunnel_endpoint,
+							 info->key,
 							 secctx, TRACE_PAYLOAD_LEN);
 
 		/* If IPSEC is needed recirc through ingress to use xfrm stack
@@ -443,6 +444,7 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 	info = ipcache_lookup4(&IPCACHE_MAP, ip4->daddr, V4_CACHE_KEY_LEN);
 	if (info != NULL && info->tunnel_endpoint != 0) {
 		int ret = encap_and_redirect_with_nodeid(skb, info->tunnel_endpoint,
+							 info->key,
 							 secctx, TRACE_PAYLOAD_LEN);
 
 		if (ret == IPSEC_ENDPOINT)

bpf/bpf_overlay.c
@@ -64,6 +64,8 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 		 */
 		if (ip6->nexthdr != IPPROTO_ESP)
 			goto not_esp;
+
+		/* Decrypt "key" is determined by SPI */
 		skb->mark = MARK_MAGIC_DECRYPT;
 		set_identity(skb, key.tunnel_id);
 		/* To IPSec stack on cilium_vxlan we are going to pass
@@ -148,6 +150,7 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 		 */
 		if (ip4->protocol != IPPROTO_ESP)
 			goto not_esp;
+		/* Decrypt "key" is determined by SPI */
 		skb->mark = MARK_MAGIC_DECRYPT;
 		set_identity(skb, key.tunnel_id);
 		/* To IPSec stack on cilium_vxlan we are going to pass

bpf/lib/common.h
@@ -162,7 +162,7 @@ struct endpoint_key {
 		union v6addr	ip6;
 	};
 	__u8 family;
-	__u8 pad4;
+	__u8 key;
 	__u16 pad5;
 } __attribute__((packed));
 
@@ -182,6 +182,7 @@ struct endpoint_info {
 struct remote_endpoint_info {
 	__u32		sec_label;
 	__u32		tunnel_endpoint;
+	__u8		key;
 };
 
 struct policy_key {
@@ -298,17 +299,24 @@ enum {
  * packets security identity. The lower/upper halves are swapped to recover
  * the identity.
  *
- * The 4 bits at 0XF00 provide
+ * The 4 bits at 0X0F00 provide
  *  - the magic marker values which indicate whether the packet is coming from
  *    an ingress or egress proxy, a local process and its current encryption
  *    status.
+ *
+ * The 4 bits at 0xF000 provide
+ *  - the key index to use for encryption when multiple keys are in-flight.
+ *    In the IPsec case this becomes the SPI on the wire.
  */
-#define MARK_MAGIC_HOST_MASK		0xF00
-#define MARK_MAGIC_PROXY_INGRESS	0xA00
-#define MARK_MAGIC_PROXY_EGRESS		0xB00
-#define MARK_MAGIC_HOST			0xC00
-#define MARK_MAGIC_DECRYPT		0xD00
-#define MARK_MAGIC_ENCRYPT		0xE00
+#define MARK_MAGIC_HOST_MASK		0x0F00
+#define MARK_MAGIC_PROXY_INGRESS	0x0A00
+#define MARK_MAGIC_PROXY_EGRESS		0x0B00
+#define MARK_MAGIC_HOST			0x0C00
+#define MARK_MAGIC_DECRYPT		0x0D00
+#define MARK_MAGIC_ENCRYPT		0x0E00
+
+#define MARK_MAGIC_KEY_ID		0xF000
+#define MARK_MAGIC_KEY_MASK		0xFF00
 
 /**
  * get_identity - returns source identity from the mark field
@@ -323,8 +331,24 @@ static inline int __inline__ get_identity(struct __sk_buff *skb)
  */
 static inline void __inline__ set_identity(struct __sk_buff *skb, __u32 identity)
 {
-	skb->mark = skb->mark & MARK_MAGIC_HOST_MASK;
+	skb->mark = skb->mark & MARK_MAGIC_KEY_MASK;
 	skb->mark |= ((identity & 0xFFFF) << 16) | ((identity & 0xFF0000) >> 16);
+}
+
+/**
+ * or_encrypt_key - mask and shift key into encryption format
+ */
+static inline __u32 __inline__ or_encrypt_key(__u8 key)
+{
+	return (((__u32)key & 0x0F) << 12) | MARK_MAGIC_ENCRYPT;
+}
+
+/**
+ * set_encrypt_key - pushes 8 bit key and encryption marker into skb mark value.
+ */
+static inline void __inline__ set_encrypt_key(struct __sk_buff *skb, __u8 key)
+{
+	skb->mark = or_encrypt_key(key);
 }
 
 /*

bpf/lib/encap.h
@@ -24,7 +24,7 @@
 #ifdef ENCAP_IFINDEX
 #ifdef ENABLE_IPSEC
 static inline int __inline__
-enacap_and_redirect_nomark_ipsec(struct __sk_buff *skb, __u32 tunnel_endpoint,
+enacap_and_redirect_nomark_ipsec(struct __sk_buff *skb, __u32 tunnel_endpoint, __u8 key,
 			 __u32 seclabel, __u32 monitor)
 {
 	/* Traffic from local host in tunnel mode will be passed to
@@ -40,14 +40,14 @@ enacap_and_redirect_nomark_ipsec(struct __sk_buff *skb, __u32 tunnel_endpoint,
 	 * in both cases because xfrm layer would overwrite them. We
 	 * use cb[4] here so it doesn't need to be reset by bpf_ipsec.
 	 */
-	skb->cb[0] = MARK_MAGIC_ENCRYPT;
+	skb->cb[0] = or_encrypt_key(key);
 	skb->cb[1] = seclabel;
 	skb->cb[4] = tunnel_endpoint;
 	return IPSEC_ENDPOINT;
 }
 
 static inline int __inline__
-encap_and_redirect_ipsec(struct __sk_buff *skb, __u32 tunnel_endpoint,
+encap_and_redirect_ipsec(struct __sk_buff *skb, __u32 tunnel_endpoint, __u8 key,
 			 __u32 seclabel, __u32 monitor)
 {
 	/* IPSec is performed by the stack on any packets with the
@@ -57,7 +57,7 @@ encap_and_redirect_ipsec(struct __sk_buff *skb, __u32 tunnel_endpoint,
 	 * label is stashed in the mark and extracted in bpf_netdev
 	 * to send skb onto tunnel for encap.
 	 */
-	skb->mark = MARK_MAGIC_ENCRYPT;
+	set_encrypt_key(skb, key);
 	set_identity(skb, seclabel);
 	skb->cb[4] = tunnel_endpoint;
 	return IPSEC_ENDPOINT;
@@ -96,24 +96,24 @@ __encap_and_redirect_with_nodeid(struct __sk_buff *skb, __u32 tunnel_endpoint,
  */
 static inline int __inline__
 encap_and_redirect_with_nodeid(struct __sk_buff *skb, __u32 tunnel_endpoint,
-			        __u32 seclabel, __u32 monitor)
+			       __u8 key, __u32 seclabel, __u32 monitor)
 {
 #ifdef ENABLE_IPSEC
-	return enacap_and_redirect_nomark_ipsec(skb, tunnel_endpoint, seclabel, monitor);
-#else
-	return __encap_and_redirect_with_nodeid(skb, tunnel_endpoint, seclabel, monitor);
+	if (key)
+		return enacap_and_redirect_nomark_ipsec(skb, tunnel_endpoint, key, seclabel, monitor);
 #endif
+	return __encap_and_redirect_with_nodeid(skb, tunnel_endpoint, seclabel, monitor);
 }
 
 static inline int __inline__
-encap_and_redirect_with_nodeid_from_lxc(struct __sk_buff *skb, __u32 tunnel_endpoint,
+encap_and_redirect_with_nodeid_from_lxc(struct __sk_buff *skb, __u32 tunnel_endpoint, __u8 key,
 				        __u32 seclabel, __u32 monitor)
 {
 #ifdef ENABLE_IPSEC
-	return encap_and_redirect_ipsec(skb, tunnel_endpoint, seclabel, monitor);
-#else
-	return __encap_and_redirect_with_nodeid(skb, tunnel_endpoint, seclabel, monitor);
+	if (key)
+		return encap_and_redirect_ipsec(skb, tunnel_endpoint, key, seclabel, monitor);
 #endif
+	return __encap_and_redirect_with_nodeid(skb, tunnel_endpoint, seclabel, monitor);
 }
 
 /* encap_and_redirect based on ENABLE_IPSEC flag and from_host bool will decide
@@ -137,14 +137,22 @@ encap_and_redirect(struct __sk_buff *skb, struct endpoint_key *k,
 	}
 
 #ifdef ENABLE_IPSEC
-	if (from_host)
-		return enacap_and_redirect_nomark_ipsec(skb, tunnel->ip4,
-						        seclabel, monitor);
-	else
-		return encap_and_redirect_ipsec(skb, tunnel->ip4, seclabel, monitor);
-#else
-	return __encap_and_redirect_with_nodeid(skb, tunnel->ip4, seclabel, monitor);
+	if (tunnel->key) {
+		if (from_host)
+			return enacap_and_redirect_nomark_ipsec(skb,
+								tunnel->ip4,
+								tunnel->key,
+							        seclabel,
+								monitor);
+		else
+			return encap_and_redirect_ipsec(skb,
+							tunnel->ip4,
+							tunnel->key,
+							seclabel,
+							monitor);
+	}
 #endif
+	return __encap_and_redirect_with_nodeid(skb, tunnel->ip4, seclabel, monitor);
 }
 #endif /* ENCAP_IFINDEX */
 #endif /* __LIB_ENCAP_H_ */

commit 43d7dd0b736a44dac50dfd0c6cee0a9f7ef66bdd

ipsec: Fix off-by-one error on max SPI
We encoded the SPI (aka keyID) on 4 bits [1] in the xfrm and packet
marks. The maximum value is therefore 15 and not 16. This commit fixes
the check on the maximum keyID value.

Note the documentation for IPsec key rotation already has the correct
value [2] so there shouldn't be any users with an incorrect keyID.

1 - https://github.com/cilium/cilium/blob/v1.10.1/pkg/datapath/linux/ipsec/ipsec_linux.go#L147-L150
2 - https://docs.cilium.io/en/v1.10/gettingstarted/encryption-ipsec/#key-rotation
Fixes: b698972 ("cilium: ipsec, support rolling updates")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -518,10 +518,10 @@ func loadIPSecKeys(r io.Reader) (int, uint8, error) {
 			offset = -1
 		}
 		if spiI > linux_defaults.IPsecMaxKeyVersion {
-			return 0, 0, fmt.Errorf("encryption Key space exhausted, id must be nonzero and less than %d. Attempted %q", linux_defaults.IPsecMaxKeyVersion, s[0])
+			return 0, 0, fmt.Errorf("encryption Key space exhausted, id must be nonzero and less than %d. Attempted %q", linux_defaults.IPsecMaxKeyVersion+1, s[0])
 		}
 		if spiI == 0 {
-			return 0, 0, fmt.Errorf("zero is not a valid key to disable encryption use `--enable-ipsec=false`, id must be nonzero and less than %d. Attempted %q", linux_defaults.IPsecMaxKeyVersion, s[0])
+			return 0, 0, fmt.Errorf("zero is not a valid key to disable encryption use `--enable-ipsec=false`, id must be nonzero and less than %d. Attempted %q", linux_defaults.IPsecMaxKeyVersion+1, s[0])
 		}
 		spi = uint8(spiI)
 

pkg/datapath/linux/linux_defaults/linux_defaults.go
@@ -96,7 +96,7 @@ const (
 	TunnelDeviceName = "cilium_vxlan"
 
 	// IPSec offset value for node rules
-	IPsecMaxKeyVersion = 16
+	IPsecMaxKeyVersion = 15
 
 	// IPsecMarkMask is the mask required for the IPsec SPI and encrypt/decrypt bits
 	IPsecMarkMask = 0xFF00