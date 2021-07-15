a6de926fa136a8422e959730f470161de9eb47dd
Tobias Klauser <tobias@cilium.io>
2021-07-15 11:44:17 +0200

    go.mod, vendor: bump github.com/pkg/browser to latest version
    Changes: https://github.com/pkg/browser/compare/a7b7a6107d32...7d21f8c997e2
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/vendor/github.com/pkg/browser/browser_windows.go
+++ cilium-cli/vendor/github.com/pkg/browser/browser_windows.go
@@ -1,9 +1,7 @@
-//go:generate mkwinsyscall -output zbrowser_windows.go browser_windows.go
-//sys shellExecute(hwnd int, verb string, file string, args string, cwd string, showCmd int) (err error) = shell32.ShellExecuteW
 package browser
 
-const sW_SHOWNORMAL = 1
+import "golang.org/x/sys/windows"
 
 func openBrowser(url string) error {
-	return shellExecute(0, "", url, "", "", sW_SHOWNORMAL)
+	return windows.ShellExecute(0, nil, windows.StringToUTF16Ptr(url), nil, nil, windows.SW_SHOWNORMAL)
 }
--- cilium-cli/vendor/github.com/pkg/browser/go.mod
+++ cilium-cli/vendor/github.com/pkg/browser/go.mod
@@ -2,4 +2,4 @@ module github.com/pkg/browser
 
 go 1.14
 
-require golang.org/x/sys v0.0.0-20210319071255-635bc2c9138d // indirect
+require golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71
