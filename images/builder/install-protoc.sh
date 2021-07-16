1e4fe0ebbdc243cd4f71e37e22a103a67026d25d
Robin Hahling <robin.hahling@gw-computing.net>
2021-07-16 17:04:30 +0200

    images/builder: bump protoc to v3.17.3, protoc-gen-go to v1.27.1
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- images/builder/install-protoc.sh
+++ images/builder/install-protoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2017-2020 Authors of Cilium
+# Copyright 2017-2021 Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
 set -o xtrace
@@ -8,7 +8,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-protoc_version="3.12.4"
+protoc_version="3.17.3"
 
 curl --fail --show-error --silent --location \
   "https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protoc-${protoc_version}-linux-x86_64.zip" \
