619a59ec079c4f9617fda507ba9efbc62b509c3f
Robin Hahling <robin.hahling@gw-computing.net>
2021-07-16 17:04:30 +0200

    images/builder: bump protoc to v3.17.3, protoc-gen-go to v1.27.1
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# Copyright 2017-2021 Authors of Cilium
+# SPDX-License-Identifier: Apache-2.0
+
 set -o xtrace
 set -o errexit
 set -o pipefail
 set -o nounset
 
-# FIXME: Update these versions once etcd-client 3.5 is released.
-# These versions are pinned to support our older grpc-go 1.29 library,
-# which required to build the version of the etcd client used in Cilium.
-# See https://github.com/cilium/cilium/pull/13405#issuecomment-704766707
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@cee815d
-go install github.com/golang/protobuf/protoc-gen-go@v1.4.3
+# 938f6e2f7550e542bd78f3b9e8812665db109e02 == tag: cmd/protoc-gen-go-grpc/v1.1.0
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@938f6e2f7550e542bd78f3b9e8812665db109e02
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
 go install github.com/mitchellh/protoc-gen-go-json@v1.0.0
