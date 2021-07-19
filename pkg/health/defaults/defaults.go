fb184e354e3abdb5a16586044c34dce51bc96d34
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-07-19 16:17:41 +0100

    health: Delete unused constants
    These constants had been defined, yet no longer in use anywhere.
    
    Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

--- pkg/health/defaults/defaults.go
+++ pkg/health/defaults/defaults.go
@@ -16,13 +16,4 @@ const (
 
 	// HTTPPathPort is used for probing base HTTP path connectivity
 	HTTPPathPort = 4240
-
-	// L7PathPort is used for probing L7 path connectivity
-	L7PathPort = 4241
-
-	// ServicePathPort is used for probing service redirect path connectivity
-	ServicePathPort = 4242
-
-	// ServiceL7PathPort is used for probing service redirect path connectivity with L7
-	ServiceL7PathPort = 4243
 )
