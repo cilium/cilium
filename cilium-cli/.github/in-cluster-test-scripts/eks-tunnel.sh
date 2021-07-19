d9c55e0d41e84879884e0d77292c069b0b091074
Nicolas Busseneau <nicolas@isovalent.com>
2021-07-19 11:52:33 +0200

    workflows: refactor EKS workflows
    Following merge of #400, we broke #391.
    
    This PR fixes the issue by refactoring both EKS workflows to be
    consistent with other workflows, now that we are creating a cluster with
    nodes available right away just like on other platforms, and thus do not
    need to split the `install`/`test` steps anymore.
    
    Fixes 3d2fbdbecb22b37b32417b6a548be1ae212ba3e5
    
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- cilium-cli/.github/in-cluster-test-scripts/eks-tunnel.sh
+++ cilium-cli/.github/in-cluster-test-scripts/eks-tunnel.sh
@@ -3,11 +3,22 @@
 set -x
 set -e
 
+# Install Cilium
+cilium install \
+  --cluster-name "${CLUSTER_NAME}" \
+  --wait=false \
+  --config monitor-aggregation=none \
+  --datapath-mode=tunnel \
+  --ipam cluster-pool
+
 # Enable Relay
 cilium hubble enable
 
 # Wait for Cilium status to be ready
 cilium status --wait
+
+# Make sure the 'aws-node' DaemonSet exists but has no scheduled pods
+[[ $(kubectl -n kube-system get ds/aws-node -o jsonpath='{.status.currentNumberScheduled}') == 0 ]]
 
 # Port forward Relay
 cilium hubble port-forward&
--- cilium-cli/.github/in-cluster-test-scripts/eks-uninstall.sh
+++ cilium-cli/.github/in-cluster-test-scripts/eks-uninstall.sh
@@ -5,3 +5,6 @@ set -e
 
 # Uninstall Cilium
 cilium uninstall --wait
+
+# Make sure the 'aws-node' DaemonSet blocking nodeSelector was removed
+[[ ! $(kubectl -n kube-system get ds/aws-node -o jsonpath="{.spec.template.spec.nodeSelector['io\.cilium/aws-node-enabled']}") ]]
