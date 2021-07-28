f6528dd1b2907da2e429aa81c05e336b9e4906d9
Martynas Pumputis <m@lambda.lt>
2021-07-28 14:25:28 +0200

    github: Collect cilium-sysdump in L4LB upon failure
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/l4lb/test.sh
+++ test/l4lb/test.sh
@@ -64,6 +64,13 @@ WORKER_IP=$(docker exec kind-worker ip -o -4 a s eth0 | awk '{print $4}' | cut -
 nsenter -t $(docker inspect kind-worker -f '{{ .State.Pid }}') -n /bin/sh -c \
     'tc qdisc add dev eth0 clsact && tc filter add dev eth0 ingress bpf direct-action object-file ./test_tc_tunnel.o section decap'
 
+collect_sysdump() {
+    curl -sLO https://github.com/cilium/cilium-sysdump/releases/latest/download/cilium-sysdump.zip
+    python cilium-sysdump.zip --output /tmp/cilium-sysdump-out
+}
+
+trap collect_sysdump ERR
+
 CILIUM_POD_NAME=$(kubectl -n kube-system get pod -l k8s-app=cilium -o=jsonpath='{.items[0].metadata.name}')
 kubectl -n kube-system wait --for=condition=Ready pod "$CILIUM_POD_NAME" --timeout=5m
 
