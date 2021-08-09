6130308645c52bf9854e48a8087ea7df4bf6f510
Xinyuan Zhang <zhangxinyuan@google.com>
2021-08-09 22:44:19 +0000

    ebpf unit testing -- handle tailcalls and support user-space map emulation
    This commit contains a demo on how to handle tailcalls and also supports
    user-space eBPF map emulation which is a part of our fake kernel model library.
    
    We resolve tailcalls by mocking the tailcall helper function and stubbing a
    callback since tailcalls are achieved by the tailcall helper function. In this
    way, the callback will be called instead whenever the tailcall helper function
    is encountered.
    
    We also provide a library to support user-space map emulation. Since eBPF maps
    are maintained by the kernel, we do not actually know if the map operations are
    safe or not without testing in the actual kernel. Considering that eBPF maps
    are actually hashmaps, we create a user-space hashmap and emulate the eBPF map
    operations in the user space via callbacks.
    
    new file:
    bpf/mock/fake_maps.h: wrap up raw hashmap operations.
    bpf/tests/drop_notify_test.h: include a demo test showing how to handle
    tailcalls
    test/bpf/drop_notify_test.c: contains main function to run test functions in drop_notify_test.h
    
    modified:
    bpf/mock/Dockerfile: add hashmap library
    bpf/tests/nat_test.h: add a demo test showing how to use user-space map emulation
    test/bpf/nat-test.c: add the new test in bpf/tests/nat_test.h into main
    function
    bpf/mock/Makefile: put all the generated files into folder "mocks" for
    later cleaning
    bpf/mock/mock_helpers.sh: put all the generated files into folder "mocks" for
    later cleaning
    test/bpf/Makefile: clean bpf/mock/mocks
    
    Signed-off-by: Xinyuan Zhang <zhangxinyuan@google.com>

--- bpf/tests/nat_test.h
+++ bpf/tests/nat_test.h
@@ -18,12 +18,14 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <errno.h>
 
 // Include unity test framework and all the mock libraries.
 #include "unity.h"
-#include "mock/mock_helpers.h"
-#include "mock/mock_helpers_skb.h"
-#include "mock/mock_conntrack_stub.h"
+#include "mocks/mock_helpers.h"
+#include "mocks/mock_helpers_skb.h"
+#include "mocks/mock_conntrack_stub.h"
+#include "fake_maps.h"
 
 // To avoid conflict between functions in bpf/builtins.h and string.h, define
 // __BPF_BUILTINS__ in conntrack_stub.h to make sure the mock library can be
@@ -98,4 +100,59 @@ void test_snat_v4_track_local() {
     // successfully creates the ipv4_ct_tuple.
     assert(!snat_v4_track_local(&ctx, &tuple, &state, NAT_DIR_EGRESS, 0,
                                 &target));
+}
+
+// Create a fake map
+HASHMAP(struct ipv4_ct_tuple, struct ipv4_nat_entry) ipv4_ct_tuple_map;
+
+// Define the compare_func
+// We need to wrap the cmp func upon using since memcmp requires the length.
+int bpf_compare_ipv4_ct_tuple(const void *a, const void *b)
+{
+    return __bpf_memcmp_builtin(a, b, sizeof(struct ipv4_ct_tuple));
+}
+
+// Define the hash_func
+// We need to wrap the cmp func upon using since hashmap_hash_default also
+// requires the length.
+size_t bpf_hash_ipv4_ct_tuple(const void *key)
+{
+    return hashmap_hash_default(key, sizeof(struct ipv4_ct_tuple));
+}
+
+// Wrap the fake map operations into callbacks
+// The "map" argument (the real map) is actually not used, but it has to be
+// encoded in the callback functions as required by the stub functions in CMock.
+// So we need different callback funcs for different fake maps.
+void *ipv4_ct_tuple_map_lookup_elem_callback(const void* map, const void* key, int cmock_num_calls) {
+  return fake_lookup_elem(&ipv4_ct_tuple_map, key);
+}
+
+int ipv4_ct_tuple_map_update_elem_callback(const void* map, const void* key, const void* value, __u32 flags, int cmock_num_calls) {
+  return fake_update_elem(&ipv4_ct_tuple_map, key, value, flags, SNAT_MAPPING_IPV4_SIZE);
+}
+
+int ipv4_ct_tuple_map_delete_elem_callback(const void* map, const void* key, int cmock_num_calls) {
+  return fake_delete_elem(&ipv4_ct_tuple_map, key);
+}
+
+void test_snat_v4_new_mapping() {
+    struct __ctx_buff ctx;
+    struct ipv4_ct_tuple otuple;
+    struct ipv4_nat_entry ostate;
+    struct ipv4_nat_target target;
+
+    // Initiate the map.
+    fake_init_map(&ipv4_ct_tuple_map, bpf_hash_ipv4_ct_tuple, bpf_compare_ipv4_ct_tuple);
+
+    get_prandom_u32_ExpectAndReturn(0);
+    // Stub the map helpers with the callbacks defined above.
+    map_lookup_elem_Stub(ipv4_ct_tuple_map_lookup_elem_callback);
+    map_update_elem_Stub(ipv4_ct_tuple_map_update_elem_callback);
+    map_update_elem_Stub(ipv4_ct_tuple_map_update_elem_callback);
+    ktime_get_ns_ExpectAndReturn(0);
+    skb_event_output_IgnoreAndReturn(0);
+    // snat_v4_new_mapping will return 0 because rtuple will not be found by
+    // snat_v4_lookup and then snat_v4_update will update successfully.
+    assert(!snat_v4_new_mapping(&ctx, &otuple, &ostate, &target));
 }
