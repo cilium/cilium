a83cb0532b623f5e00f89f47707f0ddbdd3441ce
Tobias Klauser <tobias@cilium.io>
2021-08-10 10:44:16 +0200

    images/scripts: remove golang-alpine image update
    The golang-alpine image is no longer used since commit 2960b4e6fd63
    ("images: Move hubble-proto into cilium-builder"). Remove the
    corresponding update step from update-golang-image.sh.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- images/scripts/update-golang-image.sh
+++ images/scripts/update-golang-image.sh
@@ -20,9 +20,6 @@ root_dir="$(git rev-parse --show-toplevel)"
 cd "${root_dir}"
 
 go_version=1.16.5
-# Do not upgrade to alpine 3.13 as its nslookup tool returns 1, instead of 0
-# for domain name lookups.
-go_version_alpine=${go_version}-alpine3.12
 
 image="${1:-docker.io/library/golang:${go_version}}"
 image_digest="$("${script_dir}/get-image-digest.sh" "${image}")"
@@ -32,20 +29,10 @@ if [ -z "${image_digest}" ]; then
   exit 1
 fi
 
-image_alpine="${1:-docker.io/library/golang:${go_version_alpine}}"
-image_alpine_digest="$("${script_dir}/get-image-digest.sh" "${image}")"
-
-if [ -z "${image_alpine_digest}" ]; then
-  echo "Image alpine digest not available"
-  exit 1
-fi
-
 # shellcheck disable=SC2207
 used_by=($(git grep -l GOLANG_IMAGE= images/*/Dockerfile))
 
 for i in "${used_by[@]}" ; do
     # golang images with image digest
     sed "s|GOLANG_IMAGE=docker\.io/library/golang:[0-9][0-9]*\.[0-9][0-9]*\(\.[0-9][0-9]*\)\?@.*|GOLANG_IMAGE=${image}@${image_digest}|" "${i}" > "${i}.sedtmp" && mv "${i}.sedtmp" "${i}"
-    # other golang images (e.g. golang-alpine images)
-    sed "s|GOLANG_IMAGE=docker\.io/library/golang:[0-9][0-9]*\.[0-9][0-9]*\(\.[0-9][0-9]*\)\?-\(.*\)@.*|GOLANG_IMAGE=${image_alpine}@${image_alpine_digest}|" "${i}" > "${i}.sedtmp" && mv "${i}.sedtmp" "${i}"
 done
