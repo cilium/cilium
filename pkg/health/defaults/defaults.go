5cfd76153ec524900ab99114844cc147b8a3d30d
Joe Stringer <joe@cilium.io>
2021-08-11 18:11:24 -0700

    cilium: Don't report health error when disabled
    When the user configures --enable-health-checking=false, 'cilium status'
    would previously print:
    
      $ cilium status
      ...
      Cluster health:               Warning   cilium-health daemon unreachable
    
    This would occur despite the user explicitly disabling the feature. To
    better reflect whether the feature is enabled or not, check in the
    status response whether there is a health endpoint. If there is one,
    then the feature is enabled and we can query & print its status. If
    there isn't one, then the feature is disabled and we won't get a
    successful response anyway, so just report back that the feature is
    disabled instead:
    
      $ cilium status
      ...
      Cluster health:               Probe disabled
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/health/defaults/defaults.go
+++ pkg/health/defaults/defaults.go
@@ -16,4 +16,8 @@ const (
 
 	// HTTPPathPort is used for probing base HTTP path connectivity
 	HTTPPathPort = daemon.ClusterHealthPort
+
+	// HealthEPName is the name used for the health endpoint, which is also
+	// used by the CLI client to detect when connectivity health is enabled
+	HealthEPName = "cilium-health-ep"
 )
