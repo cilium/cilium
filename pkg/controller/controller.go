cilium/cilium

commit c61d02fc4233fe925e4d0ca87fa768723190b195

controller: allow to manually trigger it
Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

pkg/controller/controller.go
@@ -149,6 +149,7 @@ type Controller struct {
 	uuid              string
 	stop              chan struct{}
 	update            chan struct{}
+	trigger           chan struct{}
 	ctxDoFunc         context.Context
 	cancelDoFunc      context.CancelFunc
 
@@ -178,6 +179,14 @@ func (c *Controller) GetLastError() error {
 	defer c.mutex.RUnlock()
 
 	return c.lastError
+}
+
+// Trigger triggers the controller
+func (c *Controller) Trigger() {
+	select {
+	case c.trigger <- struct{}{}:
+	default:
+	}
 }
 
 // GetLastErrorTimestamp returns the last error returned
@@ -283,6 +292,7 @@ func (c *Controller) runController() {
 			runFunc = true
 
 		case <-runTimer.After(interval):
+		case <-c.trigger:
 		}
 
 	}

pkg/controller/manager.go
@@ -101,6 +101,7 @@ func (m *Manager) updateController(name string, params ControllerParams) *Contro
 			uuid:       uuid.New().String(),
 			stop:       make(chan struct{}),
 			update:     make(chan struct{}, 1),
+			trigger:    make(chan struct{}, 1),
 			terminated: make(chan struct{}),
 		}
 		ctrl.updateParamsLocked(params)
@@ -245,6 +246,16 @@ func (m *Manager) GetStatusModel() models.ControllerStatuses {
 	return statuses
 }
 
+// TriggerController triggers the controller with the specified name.
+func (m *Manager) TriggerController(name string) {
+	controller := m.lookup(name)
+	if controller == nil {
+		return
+	}
+
+	controller.Trigger()
+}
+
 // FakeManager returns a fake controller manager with the specified number of
 // failing controllers. The returned manager is identical in any regard except
 // for internal pointers.
@@ -259,6 +270,7 @@ func FakeManager(failingControllers int) *Manager {
 			uuid:              fmt.Sprintf("%d", i),
 			stop:              make(chan struct{}),
 			update:            make(chan struct{}, 1),
+			trigger:           make(chan struct{}, 1),
 			terminated:        make(chan struct{}),
 			lastError:         fmt.Errorf("controller failed"),
 			failureCount:      1,

commit 91b8de56fa10c20a429303f3658a3b77c94bc620

pkg/controller: set runFunc to when controller is manually triggered
`runFunc` should be set to true when the controller is manually
triggered otherwise the controller might never be executed in the case
where its run interval is zero.

Fixes: c61d02fc4233 ("controller: allow to manually trigger it")
Signed-off-by: André Martins <andre@cilium.io>

pkg/controller/controller.go
@@ -282,6 +282,7 @@ func (c *Controller) runController() {
 
 		case <-runTimer.After(interval):
 		case <-c.trigger:
+			runFunc = true
 		}
 
 	}