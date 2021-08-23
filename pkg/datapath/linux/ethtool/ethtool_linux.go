6e3336eaad7136864f76b3538f7eeaa2045074cf
Tobias Klauser <tobias@cilium.io>
2021-08-23 12:16:05 +0200

    datapath/linux/ethtool: use IoctlGetEthtoolDrvinfo
    Use the ioctl wrapper provided in the golang.org/x/sys/unix package with
    the correctly padded ifreqData struct, rather than providing our own
    wrapper and struct which is incorrectly padded.
    
    Also add a simple unit test and make sure the package is only built on
    Linux.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

---
+++ pkg/datapath/linux/ethtool/ethtool_linux.go
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 Authors of Cilium
+
+package ethtool
+
+import (
+	"bytes"
+
+	"golang.org/x/sys/unix"
+)
+
+func IsVirtualDriver(iface string) (bool, error) {
+	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, unix.IPPROTO_IP)
+	if err != nil {
+		return false, err
+	}
+	defer unix.Close(fd)
+
+	info, err := unix.IoctlGetEthtoolDrvinfo(fd, iface)
+	if err != nil {
+		return false, err
+	}
+	return string(bytes.TrimRight(info.Driver[:], "\x00")) == "veth", nil
+}
