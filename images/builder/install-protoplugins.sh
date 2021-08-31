62bbaaba025ecbda40f24fffeb8e2b9ca5d41c12
Robin Hahling <robin.hahling@gw-computing.net>
2021-08-31 09:55:45 +0200

    images/builder: update protoc-gen-go-json from v1.0.0 to v1.1.0
    This new version uses protojson instead of the deprecated jsonpb module.
    This means that code generated by this plugin does not depend on the
    deprecated github.com/golang/protobuf module anymore.
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -11,4 +11,4 @@ set -o nounset
 # 938f6e2f7550e542bd78f3b9e8812665db109e02 == tag: cmd/protoc-gen-go-grpc/v1.1.0
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@938f6e2f7550e542bd78f3b9e8812665db109e02
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
-go install github.com/mitchellh/protoc-gen-go-json@v1.0.0
+go install github.com/mitchellh/protoc-gen-go-json@v1.1.0
