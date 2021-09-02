7291d79454e3e7b8e76a1974f8e263c58e570342
Bruno Miguel Custódio <brunomcustodio@gmail.com>
2021-09-02 09:25:43 +0100

    sysdump: collect 'clustermesh-apiserver'
    [ cherry-picked from cilium/cilium-cli repository ]
    
    This commit adds support for collecting logs for 'clustermesh-apiserver'
    as well as the 'Deployment' resource itself.
    
    Signed-off-by: Bruno Miguel Custódio <brunomcustodio@gmail.com>

--- cilium-cli/sysdump/defaults.go
+++ cilium-cli/sysdump/defaults.go
@@ -14,26 +14,28 @@ const (
 )
 
 const (
-	DefaultCiliumLabelSelector         = labelPrefix + "cilium"
-	DefaultCiliumNamespace             = "kube-system"
-	DefaultCiliumOperatorLabelSelector = "io.cilium/app=operator"
-	DefaultCiliumOperatorNamespace     = DefaultCiliumNamespace
-	DefaultDebug                       = false
-	DefaultHubbleLabelSelector         = labelPrefix + "hubble"
-	DefaultHubbleNamespace             = DefaultCiliumNamespace
-	DefaultHubbleFlowsCount            = 10000
-	DefaultHubbleFlowsTimeout          = 5 * time.Second
-	DefaultHubbleRelayLabelSelector    = labelPrefix + "hubble-relay"
-	DefaultHubbleRelayNamespace        = DefaultCiliumNamespace
-	DefaultHubbleUILabelSelector       = labelPrefix + "hubble-ui"
-	DefaultHubbleUINamespace           = DefaultCiliumNamespace
-	DefaultLargeSysdumpAbortTimeout    = 5 * time.Second
-	DefaultLargeSysdumpThreshold       = 20
-	DefaultLogsSinceTime               = 8760 * time.Hour // 1y
-	DefaultLogsLimitBytes              = 1073741824       // 1GiB
-	DefaultNodeList                    = ""
-	DefaultQuick                       = false
-	DefaultOutputFileName              = "cilium-sysdump-<ts>" // "<ts>" will be replaced with the timestamp
+	DefaultCiliumLabelSelector               = labelPrefix + "cilium"
+	DefaultCiliumNamespace                   = "kube-system"
+	DefaultCiliumOperatorLabelSelector       = "io.cilium/app=operator"
+	DefaultCiliumOperatorNamespace           = DefaultCiliumNamespace
+	DefaultClustermeshApiserverLabelSelector = "k8s-app=clustermesh-apiserver"
+	DefaultClustermeshApiserverLabelSelector = labelPrefix + "clustermesh-apiserver"
+	DefaultDebug                             = false
+	DefaultHubbleLabelSelector               = labelPrefix + "hubble"
+	DefaultHubbleNamespace                   = DefaultCiliumNamespace
+	DefaultHubbleFlowsCount                  = 10000
+	DefaultHubbleFlowsTimeout                = 5 * time.Second
+	DefaultHubbleRelayLabelSelector          = labelPrefix + "hubble-relay"
+	DefaultHubbleRelayNamespace              = DefaultCiliumNamespace
+	DefaultHubbleUILabelSelector             = labelPrefix + "hubble-ui"
+	DefaultHubbleUINamespace                 = DefaultCiliumNamespace
+	DefaultLargeSysdumpAbortTimeout          = 5 * time.Second
+	DefaultLargeSysdumpThreshold             = 20
+	DefaultLogsSinceTime                     = 8760 * time.Hour // 1y
+	DefaultLogsLimitBytes                    = 1073741824       // 1GiB
+	DefaultNodeList                          = ""
+	DefaultQuick                             = false
+	DefaultOutputFileName                    = "cilium-sysdump-<ts>" // "<ts>" will be replaced with the timestamp
 )
 
 var (
