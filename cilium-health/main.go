badac9b20908d45085e69d96d3fee5b038898942
Tom Payne <tom@isovalent.com>
2021-09-06 15:40:40 +0200

    treewide: Ensure that binaries are built with at least Go 1.17
    Cilium relies on a number of Go features that are not covered by Go's
    backward compatibility guarantee, for example:
    
    * //go:embed (available since Go 1.16)
    * //go:build (available since Go 1.17)
    
    There may be other cases where, for example, we rely on bug fixes in the
    standard library.
    
    Building Cilium with an earlier version of Go may complete without
    error, but there is no guarantee that the binary will behave correctly.
    
    This PR adds //go:build tags to the main source file of each of the
    principle binaries that will cause compilation to fail early and loudly
    so the use of old toolchains can be detected quickly.
    
    Signed-off-by: Tom Payne <tom@isovalent.com>

--- cilium-health/main.go
+++ cilium-health/main.go
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2017 Authors of Cilium
 
+// Ensure build fails on versions of Go that are not supported by Cilium.
+// This build tag should be kept in sync with the version specified in go.mod.
+//go:build go1.17
+// +build go1.17
+
 package main
 
 import "github.com/cilium/cilium/cilium-health/cmd"
