9f2ec74441da661ef969e59621b3cab0560053ec
Maciej Kwiek <maciej@isovalent.com>
2021-09-08 13:42:38 +0200

    dnsproxy: unit test GetSelectorRegexMap
    [ upstream commit fcc345ec0a00ad4a385f650910f1d35e653a1a2e ]
    
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>
    
    Signed-off-by: Glib Smaga <code@gsmaga.com>

--- pkg/fqdn/dnsproxy/helpers_test.go
+++ pkg/fqdn/dnsproxy/helpers_test.go
@@ -17,9 +17,68 @@
 package dnsproxy
 
 import (
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/policy"
+	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/golang/groupcache/lru"
 	. "gopkg.in/check.v1"
+	"testing"
 )
 
 type DNSProxyHelperTestSuite struct{}
 
 var _ = Suite(&DNSProxyHelperTestSuite{})
+
+// Hook up gocheck into the "go test" runner.
+func TestNonPrivileged(t *testing.T) {
+	TestingT(t)
+}
+
+func (s *DNSProxyHelperTestSuite) TestGetSelectorRegexMap(c *C) {
+	selector := MockCachedSelector{}
+
+	dnsName := "example.name."
+
+	l7 := policy.L7DataMap{
+		selector: &policy.PerSelectorPolicy{
+			L7Rules: api.L7Rules{DNS: []api.PortRuleDNS{
+				{
+					MatchName: dnsName,
+				},
+			}},
+		},
+	}
+	cache := &lru.Cache{}
+	m, err := GetSelectorRegexMap(l7, cache)
+
+	c.Assert(err, Equals, nil)
+
+	regex, ok := m[selector]
+
+	c.Assert(ok, Equals, true)
+
+	c.Assert(regex.MatchString(dnsName), Equals, true)
+	c.Assert(regex.MatchString(dnsName+"trolo"), Equals, false)
+}
+
+type MockCachedSelector struct{}
+
+func (m MockCachedSelector) GetSelections() []identity.NumericIdentity {
+	return nil
+}
+
+func (m MockCachedSelector) Selects(_ identity.NumericIdentity) bool {
+	return false
+}
+
+func (m MockCachedSelector) IsWildcard() bool {
+	return false
+}
+
+func (m MockCachedSelector) IsNone() bool {
+	return false
+}
+
+func (m MockCachedSelector) String() string {
+	return "string"
+}
