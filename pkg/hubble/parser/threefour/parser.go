72564e93bc29dde17d0466894ee4b58434543886
Paul Chaignon <paul@cilium.io>
2021-09-15 21:12:15 +0200

    hubble: Display proxy redirects in policy verdict events
    Before this commit, Hubble was ignoring proxy redirection information
    from the policy-verdict events it received from the datapath. For
    example, a cilium monitor event such as:
    
        Policy verdict log: flow 0x0 local EP ID 1531, remote ID 35429, proto 17, egress, action redirect, match L3-L4, 10.240.0.62:37282 -> 10.240.0.63:53 udp
    
    would be displayed in hubble observe as:
    
        Sep 15 17:23:11.960: cilium-test/client-6488dcf5d4-f9kfl:37282 -> kube-system/coredns-d4866bcb7-zh5jv:53 L3-L4 FORWARDED (UDP)
    
    This commit adds a new verdict REDIRECTED to signal such event. Such
    events now appear as:
    
        default/pod-to-external-fqdn-allow-google-cnp-5ff4986c89-n87h2:58314 -> kube-system/coredns-755cd654d4-j4vzh:53 UNKNOWN 5 (UDP)
    
    A subsequent patch to the Hubble command line will display value 5 as
    "REDIRECTED".
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/hubble/parser/threefour/parser.go
+++ pkg/hubble/parser/threefour/parser.go
@@ -394,6 +394,9 @@ func decodeVerdict(dn *monitor.DropNotify, tn *monitor.TraceNotify, pvn *monitor
 		if pvn.Verdict < 0 {
 			return pb.Verdict_DROPPED
 		}
+		if pvn.Verdict > 0 {
+			return pb.Verdict_REDIRECTED
+		}
 		if pvn.IsTrafficAudited() {
 			return pb.Verdict_AUDIT
 		}
