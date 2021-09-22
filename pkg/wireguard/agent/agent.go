24e2ca3776a76c2328e698def1f8ae03cb7e117c
Sebastian Wicki <sebastian@isovalent.com>
2021-09-22 10:12:47 +0200

    wireguard: Do not mask error when disabling rp_filter
    It should be propagated to the caller, as we exit the function when
    rp_filter cannot be disabled.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- pkg/wireguard/agent/agent.go
+++ pkg/wireguard/agent/agent.go
@@ -111,7 +111,7 @@ func (a *Agent) Init(mtuConfig mtu.Configuration) error {
 
 	if option.Config.EnableIPv4 {
 		if err := sysctl.Disable(fmt.Sprintf("net.ipv4.conf.%s.rp_filter", types.IfaceName)); err != nil {
-			return nil
+			return fmt.Errorf("failed to disable rp_filter: %w", err)
 		}
 	}
 
