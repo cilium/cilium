18f4b5b65411646721f350828f946324f99fedd7
Chris Tarazi <chris@isovalent.com>
2021-09-28 10:09:23 -0700

    daemon, ipam, option: Introduce ability to bypass IP availability error
    [ upstream commit baa9e9a200a96eb4cf97a3e660af3f5f02ae3762 ]
    
    In CRD-backed IPAM modes such as ENI mode, some IPs were recently
    removed from the IPAM pool [1]. In user environments where Cilium was
    running a version prior to [1], it is possible for endpoints to be
    assigned "unavailable" IPs. Upon upgrade (to a version which includes
    [1]), endpoint restoration will fail with [2].
    
    In order to workaround this failure and not disrupt the upgrade, this
    change introduces a hidden flag
    (`--bypass-ip-availability-upon-restore`) which will inform Cilium to
    continue on if the restored endpoint's IP is not available for
    reallocation, bypassing the specific error "IP is not available". Other
    errors will not be bypassed, in order to reduce the scope of this
    stop-gap solution.
    
    With the flag set, restored endpoints which had "unavailable" IPs will
    keep them. Any new endpoints / pods will be assigned fresh, valid IPs
    from the pool.
    
    This flag is only meant to be enabled with CRD-backed IPAM modes such as
    ENI mode. The reason is because of the change described in [1], where
    the primary ENI IP was removed from the IPAM pool. In any other mode
    that this flag is enabled in, the user is warned that the flag is not
    intended for other modes and will have no effect.
    
    This patch is intended to be reverted in the future, as this stop-gap
    solution will no longer be required, as users of Cilium don't upgrade
    from versions prior to [1]. I propose that we revert this in the
    following release that this patch makes it in (N+1).
    
    How was this tested?
    
    1) Deployed a Cilium version that doesn't include [1] on EKS cluster
    2) Created a Deployment object which I scaled to max out the ENI IPs,
       such that at least one pod is assigned an "unavailable" IP
    3) Upgraded Cilium to a version which does include [1] and observe [2]
       failures
    4) Reset cluster back to state from (2)
    5) Upgrade Cilium to the version that contains this commit
    6) Observe log msgs from this commit and endpoint restoration succeeding
    7) Scale Deployment to 0 and back up, to restart all pods
    8) Observe that they all get fresh IPs and none of the "unavailable"
       IPs
    
    [1]: https://github.com/cilium/cilium/pull/15453
    [2]:
    
    ```
    {
    "time":"2021-09-20T16:57:00.400086481Z",
    "level":"WARN",
    "origin":"cilium.io/agent",
    "message":"Unable to restore endpoint, ignoring",
    "params":{
        "endpointID":"992",
        "error":"Failed to re-allocate IP of endpoint: unable to reallocate 10.0.133.193 IPv4 address: IP 10.0.133.193 is not available",
        "k8sPodName":"default/pod-1",
        "subsys":"daemon"
      }
    }
    ```
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    
    Signed-off-by: Glib Smaga <code@gsmaga.com>

--- daemon/cmd/state.go
+++ daemon/cmd/state.go
@@ -16,6 +16,7 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -23,6 +24,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint"
+	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/k8s/watchers"
 	"github.com/cilium/cilium/pkg/labels"
@@ -344,7 +346,7 @@ func (d *Daemon) allocateIPsLocked(ep *endpoint.Endpoint) error {
 	if option.Config.EnableIPv6 && ep.IPv6 != nil {
 		_, err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv6.IP(), ep.HumanStringLocked()+" [restored]")
 		if err != nil {
-			return fmt.Errorf("unable to reallocate %s IPv6 address: %s", ep.IPv6.IP(), err)
+			return fmt.Errorf("unable to reallocate %s IPv6 address: %w", ep.IPv6.IP(), err)
 		}
 
 		defer func() {
@@ -355,8 +357,30 @@ func (d *Daemon) allocateIPsLocked(ep *endpoint.Endpoint) error {
 	}
 
 	if option.Config.EnableIPv4 && ep.IPv4 != nil {
-		if _, err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv4.IP(), ep.HumanStringLocked()+" [restored]"); err != nil {
-			return fmt.Errorf("unable to reallocate %s IPv4 address: %s", ep.IPv4.IP(), err)
+		_, err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv4.IP(), ep.HumanStringLocked()+"[restored]")
+		switch {
+		// We only check for BypassIPAllocUponRestore for IPv4 because we
+		// assume that this flag is only turned on for IPv4-only IPAM modes
+		// such as ENI.
+		//
+		// Additionally, only check for a specific error which can be caused by
+		// https://github.com/cilium/cilium/pull/15453. Other errors are not
+		// bypassed.
+		case err != nil &&
+			errors.Is(err, ipam.NewIPNotAvailableInPoolError(ep.IPv4.IP())) &&
+			option.Config.BypassIPAvailabilityUponRestore:
+			log.WithError(err).WithFields(logrus.Fields{
+				logfields.IPAddr:     ep.IPv4.IP(),
+				logfields.EndpointID: ep.ID,
+				logfields.K8sPodName: ep.GetK8sNamespaceAndPodName(),
+			}).Warn(
+				"Bypassing IP not available error on endpoint restore. This is " +
+					"to prevent errors upon Cilium upgrade and should not be " +
+					"relied upon. Consider restarting this pod in order to get " +
+					"a fresh IP from the pool.",
+			)
+		case err != nil:
+			return fmt.Errorf("unable to reallocate %s IPv4 address: %w", ep.IPv4.IP(), err)
 		}
 	}
 
