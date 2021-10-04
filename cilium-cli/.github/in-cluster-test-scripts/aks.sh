0642968c2b3b0fb717e0142715db2962e5e5877b
Tobias Klauser <tobias@cilium.io>
2021-10-04 13:21:51 +0200

    .github/workflows: remove superfluous `cilium status --wait` commands
    Now that `cilium hubble enable` correctly waits for Cilium and Hubble to
    be ready, there is no need anymore for separate `cilium status --wait`
    calls after enabling Hubble.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/.github/in-cluster-test-scripts/aks.sh
+++ cilium-cli/.github/in-cluster-test-scripts/aks.sh
@@ -6,9 +6,6 @@ set -e
 # Enable Relay
 cilium hubble enable
 
-# Wait for Cilium status to be ready
-cilium status --wait
-
 # Port forward Relay
 cilium hubble port-forward&
 sleep 10s
--- cilium-cli/.github/in-cluster-test-scripts/gke.sh
+++ cilium-cli/.github/in-cluster-test-scripts/gke.sh
@@ -12,9 +12,6 @@ cilium install \
 # Enable Relay
 cilium hubble enable
 
-# Wait for Cilium status to be ready
-cilium status --wait
-
 # Port forward Relay
 cilium hubble port-forward&
 sleep 10s
