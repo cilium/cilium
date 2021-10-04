080b1f382c2b4cfc5874b9551b77b1160486b9fe
Tobias Klauser <tobias@cilium.io>
2021-10-04 13:21:47 +0200

    hubble: wait for deployments to be ready after enabling them
    By default, wait for the Relay (and UI, if `--ui` is specified)
    deployments to be ready as part of enabling Hubble. This behavior can be
    disabled by setting `--wait=false`.
    
    In combination with the previous commit this allows to avoid invoking
    `cilium status --wait` after enabling Hubble.
    
    Fixes #164
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/internal/cli/cmd/hubble.go
+++ cilium-cli/internal/cli/cmd/hubble.go
@@ -56,6 +56,8 @@ func newCmdHubbleEnable() *cobra.Command {
 	cmd.Flags().BoolVar(&params.UI, "ui", false, "Enable Hubble UI")
 	cmd.Flags().BoolVar(&params.CreateCA, "create-ca", false, "Automatically create CA if needed")
 	cmd.Flags().StringVar(&contextName, "context", "", "Kubernetes configuration context")
+	cmd.Flags().BoolVar(&params.Wait, "wait", true, "Wait for status to report success (no errors)")
+	cmd.Flags().DurationVar(&params.WaitDuration, "wait-duration", 15*time.Minute, "Maximum time to wait for status")
 	cmd.Flags().DurationVar(&params.CiliumReadyTimeout, "cilium-ready-timeout", 5*time.Minute,
 		"Timeout for Cilium to become ready before deploying Hubble components")
 
