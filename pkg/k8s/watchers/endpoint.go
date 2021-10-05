cb11ae2074397ffdb2e9b43e3d6aa4070af666a5
Chris Tarazi <chris@isovalent.com>
2021-10-05 15:01:37 -0700

    k8s/watchers: Push kube-apiserver IPs into IDMD
    By leveraging the special "kubernetes" Service / Endpoint object, we can
    learn about which IPs the kube-apiserver is behind. These IPs will be
    upserted into the ipcache with an identity that is to-be-allocated.  In
    order to allocate an identity for these IPs, the
    kube-apiserver-associated IPs are inserted into the IDMD map with the
    "reserved:kube-apiserver" label. Then the watcher will trigger the label
    injection via ipcache.TriggerLabelInjection().
    
    We do not make a distinction on where the kube-apiserver is running,
    whether it's on the local node, on a remote node in the cluster, or if
    it's running outside of the cluster. All kube-apiserver-associated IPs
    will have the "reserved:kube-apiserver" label.
    
    Note that we are not hooking into the delete event. We only subscribe to
    the add / update events. This is because we perform a diff on the
    currently associated kube-apiserver IPs inside the IDMD and what the add
    / update event contains (desired IPs), then remove the current IPs that
    are not in the desired set. This operation is equivalent to subscribing
    to the delete event and removing the "desired" IPs from the IDMD.
    However, we don't need to subscribe to the delete event because this
    special Service is automatically recreated / reconciled by the apiserver
    itself if the object is removed or edited.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    Co-authored-by: Joe Stringer <joe@cilium.io>
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/k8s/watchers/endpoint.go
+++ pkg/k8s/watchers/endpoint.go
@@ -4,11 +4,14 @@
 package watchers
 
 import (
+	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/source"
 
 	v1 "k8s.io/api/core/v1"
 	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -85,10 +88,87 @@ func (k *K8sWatcher) updateK8sEndpointV1(oldEP, newEP *slim_corev1.Endpoints, sw
 	if option.Config.BGPAnnounceLBIP {
 		k.bgpSpeakerManager.OnUpdateEndpoints(newEP)
 	}
+	k.addKubeAPIServerServiceEPs(newEP)
 	return nil
 }
 
 func (k *K8sWatcher) deleteK8sEndpointV1(ep *slim_corev1.Endpoints, swg *lock.StoppableWaitGroup) error {
 	k.K8sSvcCache.DeleteEndpoints(ep, swg)
 	return nil
+}
+
+// handleKubeAPIServerServiceEPChanges associates the set of 'desiredIPs' with
+// the 'reserved:kube-apiserver' label in the IPCache. This allows policy
+// selectors for the kube-apiserver entity to match these peers.
+//
+// Any IPs currently associated with the apiserver that are not part of
+// 'desiredIPs' will be disassociated from the apiserver following a call to
+// this function.
+//
+// The actual implementation of this logic down to the datapath is handled
+// asynchronously.
+func (k *K8sWatcher) handleKubeAPIServerServiceEPChanges(desiredIPs map[string]struct{}) {
+	// Use CustomResource as the source similar to the way the CiliumNode
+	// (pkg/node/manager.Manager) handler does because the ipcache entry needs
+	// to be overwrite-able by this handler and the CiliumNode handler. If we
+	// used Kubernetes as the source, then the ipcache entries inserted (first)
+	// by the CN handler wouldn't be overwrite-able by the entries inserted
+	// from this handler.
+	src := source.CustomResource
+
+	// We must perform a diff on the ipcache.identityMetadata map in order to
+	// figure out which IPs are stale and should be removed, before we inject
+	// new IPs into the ipcache. The reason is because kube-apiserver will
+	// constantly reconcile this specific object, even when it's been deleted;
+	// effectively, this means we can avoid listening for the delete event.
+	// Therefore, any changes to this specific object can be handled in a
+	// "flattened" manner, since the most up-to-date form of it will be an add
+	// or update event. The former is sent when Cilium is syncing with K8s and
+	// the latter is sent anytime after.
+	//
+	// For example:
+	//   * if a backend is removed or updated, then this will be in the form of
+	//     an update event.
+	//   * if the entire object is deleted, then it will quickly be recreated
+	//     and this will be in the form of an add event.
+	oldAPIServerIPs := ipcache.FilterMetadataByLabels(labels.LabelKubeAPIServer)
+	toRemove := make(map[string]labels.Labels)
+	for _, ip := range oldAPIServerIPs {
+		if _, ok := desiredIPs[ip]; !ok {
+			toRemove[ip] = labels.LabelKubeAPIServer
+		}
+	}
+	ipcache.RemoveLabelsFromIPs(
+		toRemove,
+		src,
+		k.policyRepository.GetSelectorCache(),
+		k.policyManager,
+	)
+
+	for ip := range desiredIPs {
+		ipcache.UpsertMetadata(ip, labels.LabelKubeAPIServer)
+	}
+
+	ipcache.IPIdentityCache.TriggerLabelInjection(
+		src,
+		k.policyRepository.GetSelectorCache(),
+		k.policyManager,
+	)
+}
+
+// TODO(christarazi): Convert to subscriber model along with the corresponding
+// EndpointSlice version.
+func (k *K8sWatcher) addKubeAPIServerServiceEPs(ep *slim_corev1.Endpoints) {
+	if ep == nil || ep.Name != "kubernetes" || ep.Namespace != "default" {
+		return
+	}
+
+	desiredIPs := make(map[string]struct{})
+	for _, sub := range ep.Subsets {
+		for _, addr := range sub.Addresses {
+			desiredIPs[addr.IP] = struct{}{}
+		}
+	}
+
+	k.handleKubeAPIServerServiceEPChanges(desiredIPs)
 }
