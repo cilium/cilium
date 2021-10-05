0fb621112998232b8cf3782033819eb203539c46
Chris Tarazi <chris@isovalent.com>
2021-10-05 14:36:35 -0700

    identity, labels: Add kube-apiserver identity and label
    This commit adds the new kube-apiserver label which enables traffic to
    be identified as kube-apiserver traffic. Along with the new label, we
    create a new reserved ID which will be associated with traffic going to
    the kube-apiserver.
    
    There are a couple nuances to note here. The kube-apiserver label will
    used in three scenarios:
      * Attached along with the local host label if the kube-apiserver is
        running on the local host. The identity of the local host will now
        be used for traffic to the local host + to the local kube-apiserver.
        The reserved ID of the local host *does not* change however. It is
        still 1, to prevent breaking changes with connectivity.
      * Attached along with the remote-node label as part of the new
        kube-apiserver reserved identity. This identity is only used if the
        kube-apiserver is running on remote nodes.
      * Attached along with the CIDR labels if the kube-apiserver is running
        outside of the cluster. This will result in a regular CIDR identity
        allocation, just with the extra kube-apiserver label.
    
    In subsequent commits, the above three cases are what will be
    implemented to allow policy matching on the kube-apiserver. This commit
    setting the groundwork for that.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/identity/identity_test.go
+++ pkg/identity/identity_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/labels/cidr"
 
+	"github.com/stretchr/testify/assert"
 	. "gopkg.in/check.v1"
 )
 
@@ -52,12 +53,17 @@ func (s *IdentityTestSuite) TestReservedID(c *C) {
 	c.Assert(i, Equals, NumericIdentity(3))
 	c.Assert(i.String(), Equals, "unmanaged")
 
+	i = GetReservedID("kube-apiserver")
+	c.Assert(i, Equals, NumericIdentity(7))
+	c.Assert(i.String(), Equals, "kube-apiserver")
+
 	c.Assert(GetReservedID("unknown"), Equals, IdentityUnknown)
 	unknown := NumericIdentity(700)
 	c.Assert(unknown.String(), Equals, "700")
 }
 
 func (s *IdentityTestSuite) TestIsReservedIdentity(c *C) {
+	c.Assert(ReservedIdentityKubeAPIServer.IsReservedIdentity(), Equals, true)
 	c.Assert(ReservedIdentityHealth.IsReservedIdentity(), Equals, true)
 	c.Assert(ReservedIdentityHost.IsReservedIdentity(), Equals, true)
 	c.Assert(ReservedIdentityWorld.IsReservedIdentity(), Equals, true)
@@ -91,4 +97,140 @@ func (s *IdentityTestSuite) TestNewIdentityFromLabelArray(c *C) {
 	c.Assert(id.ID, Equals, NumericIdentity(1001))
 	c.Assert(id.Labels, checker.DeepEquals, lbls)
 	c.Assert(id.LabelArray, checker.DeepEquals, lbls.LabelArray())
+}
+
+func TestLookupReservedIdentityByLabels(t *testing.T) {
+	type args struct {
+	}
+	type want struct {
+		id     NumericIdentity
+		labels labels.Labels
+	}
+	tests := []struct {
+		name string
+		args labels.Labels
+		want *want
+	}{
+		{
+			name: "nil",
+			args: nil,
+			want: nil,
+		},
+		{
+			name: "host",
+			args: labels.LabelHost,
+			want: &want{
+				id:     ReservedIdentityHost,
+				labels: labels.LabelHost,
+			},
+		},
+		{
+			name: "non-reserved",
+			args: labels.NewLabelsFromModel([]string{"foo"}),
+			want: nil,
+		},
+		{
+			name: "non-reserved-2",
+			args: labels.NewLabelsFromModel([]string{"reserved:init", "foo"}),
+			want: nil,
+		},
+		{
+			name: "health",
+			args: labels.LabelHealth,
+			want: &want{
+				id:     ReservedIdentityHealth,
+				labels: labels.LabelHealth,
+			},
+		},
+		{
+			name: "remote-node",
+			args: labels.LabelRemoteNode,
+			want: &want{
+				id:     ReservedIdentityRemoteNode,
+				labels: labels.LabelRemoteNode,
+			},
+		},
+		{
+			name: "kube-apiserver",
+			args: labels.Map2Labels(map[string]string{
+				labels.LabelKubeAPIServer.String(): "",
+				labels.LabelRemoteNode.String():    "",
+			}, ""),
+			want: &want{
+				id: ReservedIdentityKubeAPIServer,
+				labels: labels.Map2Labels(map[string]string{
+					labels.LabelKubeAPIServer.String(): "",
+					labels.LabelRemoteNode.String():    "",
+				}, ""),
+			},
+		},
+		{
+			name: "kube-apiserver-and-host",
+			args: labels.Map2Labels(map[string]string{
+				labels.LabelKubeAPIServer.String(): "",
+				labels.LabelHost.String():          "",
+			}, ""),
+			want: &want{ // Should always still be host reserved identity
+				id: ReservedIdentityHost,
+				labels: labels.Map2Labels(map[string]string{
+					labels.LabelKubeAPIServer.String(): "",
+					labels.LabelHost.String():          "",
+				}, ""),
+			},
+		},
+		{
+			name: "host-and-kube-apiserver",
+			args: labels.Map2Labels(map[string]string{
+				labels.LabelHost.String():          "",
+				labels.LabelKubeAPIServer.String(): "",
+			}, ""),
+			want: &want{ // Should always still be host reserved identity
+				id: ReservedIdentityHost,
+				labels: labels.Map2Labels(map[string]string{
+					labels.LabelHost.String():          "",
+					labels.LabelKubeAPIServer.String(): "",
+				}, ""),
+			},
+		},
+		{
+			name: "kube-apiserver-and-remote-node",
+			args: labels.Map2Labels(map[string]string{
+				labels.LabelKubeAPIServer.String(): "",
+				labels.LabelRemoteNode.String():    "",
+			}, ""),
+			want: &want{
+				id: ReservedIdentityKubeAPIServer,
+				labels: labels.Map2Labels(map[string]string{
+					labels.LabelKubeAPIServer.String(): "",
+					labels.LabelRemoteNode.String():    "",
+				}, ""),
+			},
+		},
+		{
+			name: "remote-node-and-kube-apiserver",
+			args: labels.Map2Labels(map[string]string{
+				labels.LabelRemoteNode.String():    "",
+				labels.LabelKubeAPIServer.String(): "",
+			}, ""),
+			want: &want{
+				id: ReservedIdentityKubeAPIServer,
+				labels: labels.Map2Labels(map[string]string{
+					labels.LabelRemoteNode.String():    "",
+					labels.LabelKubeAPIServer.String(): "",
+				}, ""),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			id := LookupReservedIdentityByLabels(tt.args)
+			if tt.want == nil {
+				assert.Nil(t, id)
+				return
+			}
+			assert.NotNil(t, id)
+			assert.Equal(t, tt.want.id, id.ID)
+			assert.Equal(t, tt.want.labels, id.Labels)
+		})
+	}
 }
