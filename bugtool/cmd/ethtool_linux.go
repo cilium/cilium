e29f215b58a9c08ce4bfd7601bd5501d84fcbca4
Tobias Klauser <tobias@cilium.io>
2021-10-06 13:46:33 +0200

    bugtool: preallocate ethtool command slices
    Avoid reallocations and GC pressure in the loop.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- bugtool/cmd/ethtool_linux.go
+++ bugtool/cmd/ethtool_linux.go
@@ -13,16 +13,16 @@ import (
 )
 
 func ethtoolCommands() []string {
-	sources := []string{}
-	// Append ethtool links
-	if links, err := netlink.LinkList(); err == nil {
-		for _, link := range links {
-			// query current settings
-			sources = append(sources, fmt.Sprintf("ethtool %s", link.Attrs().Name))
-			// query for driver information
-			sources = append(sources, fmt.Sprintf("ethtool -i %s", link.Attrs().Name))
-		}
+	links, err := netlink.LinkList()
+	if err != nil {
+		return nil
 	}
-
+	sources := make([]string, 0, len(links)*2)
+	for _, link := range links {
+		// query current settings
+		sources = append(sources, fmt.Sprintf("ethtool %s", link.Attrs().Name))
+		// query for driver information
+		sources = append(sources, fmt.Sprintf("ethtool -i %s", link.Attrs().Name))
+	}
 	return sources
 }
