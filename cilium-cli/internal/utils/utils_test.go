b45b4769b2a6afbf2e61e05cbd6ae0ea859e662e
Tobias Klauser <tobias@cilium.io>
2021-10-08 10:12:19 +0200

    internal/utils: add TestBuildImagePath
    Make sure we don't regress.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/internal/utils/utils_test.go
+++ cilium-cli/internal/utils/utils_test.go
@@ -3,17 +3,23 @@
 
 package utils
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCheckVersion(t *testing.T) {
 	tests := []struct {
 		name string
 		want bool
 	}{
+		{"0.0.1", true},
+		{"v0.0.1", true},
 		{"v1.9.6", true},
 		{"1.9.6", true},
 		{"v1.10.0-rc1", true},
 		{"1.10.0-rc1", true},
+		{"10.42.0", true},
 		{"1.9", false},
 		{"v1.9", false},
 		{"1", false},
@@ -29,6 +35,35 @@ func TestCheckVersion(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := CheckVersion(tt.name); got != tt.want {
 				t.Errorf("CheckVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBuildImagePath(t *testing.T) {
+	tests := []struct {
+		userImage      string
+		defaultImage   string
+		userVersion    string
+		defaultVersion string
+		want           string
+	}{
+		{"", "", "", "", ":"},
+		{"", "", "", "v1.10.4", ":v1.10.4"},
+		{"", "", "v1.9.10", "v1.10.4", ":v1.9.10"},
+		{"", "", "1.11.0-rc1", "v1.10.4", ":v1.11.0-rc1"},
+		{"", "quay.io/cilium/cilium", "", "v1.10.4", "quay.io/cilium/cilium:v1.10.4"},
+		{"", "quay.io/cilium/cilium", "v1.9.10", "v1.10.4", "quay.io/cilium/cilium:v1.9.10"},
+		{"", "quay.io/cilium/cilium", "1.9.10", "v1.10.4", "quay.io/cilium/cilium:v1.9.10"},
+		{"quay.io/cilium/cilium-ci", "quay.io/cilium/cilium", "v1.9.10", "v1.10.4", "quay.io/cilium/cilium-ci:v1.9.10"},
+		{"quay.io/cilium/cilium-ci", "quay.io/cilium/cilium", "latest", "v1.10.4", "quay.io/cilium/cilium-ci:latest"},
+	}
+	for _, tt := range tests {
+		ui, di, uv, dv := tt.userImage, tt.defaultImage, tt.userVersion, tt.defaultVersion
+		fn := fmt.Sprintf("BuildImagePath(%q, %q, %q, %q)", ui, di, uv, dv)
+		t.Run(fn, func(t *testing.T) {
+			if got := BuildImagePath(ui, di, uv, dv); got != tt.want {
+				t.Errorf("%s = %q, want %q", fn, got, tt.want)
 			}
 		})
 	}
