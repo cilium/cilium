c9bf53e3c76ba8a886a8d3de9b21f6de4a46b32f
Chris Tarazi <chris@isovalent.com>
2021-10-11 14:36:20 -0700

    ipcache, policy: Inject labels from identity metadata
    Any update to the IdentityMetadata (IDMD) map must trigger an update to
    the ipcache. Since the IdentityMetadata maps IPS / CIDRs to labels, if
    an update was made, we must to check if that update was related to the
    labels. If so, that means it might affect identities. Hence, we must to
    allocate a new identity, trigger policy updating, and push it into the
    ipcache. This commit provides the mechanism to do so (InjectLabels()).
    In future commits, this will be triggered by other subsystems as they
    encounter IPs / CIDRs for which we need to associate labels with.
    
    Corresponding functions to find and delete IPs / CIDRs with specific
    labels is also provided, which include releasing any allocated
    identities and deletion from the ipcache.
    
    Here's a diagram of how this works for the kube-apiserver policy
    matching:
    
      +------------+  (1)        (1)  +------------+
      | EP Watcher +-----+      +-----+ CN Watcher |
      +-----+------+   W |      | W   +------+-----+
            |            |      |            |
            |            v      v            |
            |            +------+            |
            |            | IDMD |            |
            |            +------+            |
            |               ^                |
            |               |                |
            |           (3) |R               |
            | (2)    +------+--------+   (2) |
            +------> |Label Injector |<------+
           Trigger   +-------+-------+ Trigger
                         (4) |W
                             |
                             v
                           +---+
                           |IPC|
                           +---+
      legend:
      * W means write
      * R means read
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    Co-authored-by: Joe Stringer <joe@cilium.io>
    Signed-off-by: Joe Stringer <joe@cilium.io>

---
+++ pkg/ipcache/metadata_test.go
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 Authors of Cilium
+
+//go:build !privileged_tests
+// +build !privileged_tests
+
+package ipcache
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/source"
+	testidentity "github.com/cilium/cilium/pkg/testutils/identity"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInjectLabels(t *testing.T) {
+	setupTest(t)
+
+	assert.Len(t, IdentityMetadata, 1)
+	assert.NoError(t, InjectLabels(source.Local, &mockUpdater{}, &mockTriggerer{}))
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 1)
+
+	// Insert kube-apiserver IP from outside of the cluster. This should create
+	// a CIDR ID for this IP.
+	UpsertMetadata("10.0.0.4", labels.LabelKubeAPIServer)
+	assert.Len(t, IdentityMetadata, 2)
+	assert.NoError(t, InjectLabels(source.Local, &mockUpdater{}, &mockTriggerer{}))
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 2)
+	assert.True(t, IPIdentityCache.ipToIdentityCache["10.0.0.4"].ID.HasLocalScope())
+
+	// Upsert node labels to the kube-apiserver to validate that the CIDR ID is
+	// deallocated and the kube-apiserver reserved ID is associated with this
+	// IP now.
+	UpsertMetadata("10.0.0.4", labels.LabelRemoteNode)
+	assert.Len(t, IdentityMetadata, 2)
+	assert.NoError(t, InjectLabels(source.Local, &mockUpdater{}, &mockTriggerer{}))
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 2)
+	assert.False(t, IPIdentityCache.ipToIdentityCache["10.0.0.4"].ID.HasLocalScope())
+}
+
+func TestFilterMetadataByLabels(t *testing.T) {
+	setupTest(t)
+
+	UpsertMetadata("2.1.1.1", labels.LabelWorld)
+	UpsertMetadata("3.1.1.1", labels.LabelWorld)
+
+	assert.Len(t, FilterMetadataByLabels(labels.LabelKubeAPIServer), 1)
+	assert.Len(t, FilterMetadataByLabels(labels.LabelWorld), 2)
+}
+
+func TestRemoveLabelsFromIPs(t *testing.T) {
+	setupTest(t)
+
+	assert.Len(t, IdentityMetadata, 1)
+	assert.NoError(t, InjectLabels(source.Local, &mockUpdater{}, &mockTriggerer{}))
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 1)
+
+	RemoveLabelsFromIPs(map[string]labels.Labels{
+		"1.1.1.1": labels.LabelKubeAPIServer,
+	}, source.Local, &mockUpdater{}, &mockTriggerer{})
+	assert.Len(t, IdentityMetadata, 1)
+	assert.Equal(t, labels.LabelHost, IdentityMetadata["1.1.1.1"])
+}
+
+func setupTest(t *testing.T) {
+	IPIdentityCache = NewIPCache()
+	IPIdentityCache.k8sSyncedChecker = &mockK8sSyncedChecker{}
+
+	IdentityAllocator = testidentity.NewMockIdentityAllocator(nil)
+	IdentityMetadata = make(map[string]labels.Labels)
+
+	UpsertMetadata("1.1.1.1", labels.LabelKubeAPIServer)
+	UpsertMetadata("1.1.1.1", labels.LabelHost)
+}
+
+type mockK8sSyncedChecker struct{}
+
+func (m *mockK8sSyncedChecker) K8sCacheIsSynced() bool { return true }
+
+type mockUpdater struct{}
+
+func (m *mockUpdater) UpdateIdentities(_, _ cache.IdentityCache, _ *sync.WaitGroup) {}
+
+type mockTriggerer struct{}
+
+func (m *mockTriggerer) TriggerPolicyUpdates(bool, string) {}
