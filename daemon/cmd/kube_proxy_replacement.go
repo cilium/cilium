b129f7bf8382244b8061ead6ad1208ab05eae96f
Paul Chaignon <paul@cilium.io>
2021-10-12 22:13:45 +0200

    option: Introduce TunnelingEnabled helper
    [ upstream commit 54fdf6ee745a3e8ad51b6d81034c7a628a517a4f ]
    
    This new method will be used in the following commit to check if
    tunneling is enabled from the node package.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- daemon/cmd/kube_proxy_replacement.go
+++ daemon/cmd/kube_proxy_replacement.go
@@ -320,7 +320,7 @@ func initKubeProxyReplacementOptions() (strict bool) {
 	}
 
 	if option.Config.EnableNodePort {
-		if option.Config.Tunnel != option.TunnelDisabled &&
+		if option.Config.TunnelingEnabled() &&
 			option.Config.NodePortMode != option.NodePortModeSNAT {
 
 			log.Warnf("Disabling NodePort's %q mode feature due to tunneling mode being enabled",
@@ -329,7 +329,7 @@ func initKubeProxyReplacementOptions() (strict bool) {
 		}
 
 		if option.Config.NodePortAcceleration != option.NodePortAccelerationDisabled {
-			if option.Config.Tunnel != option.TunnelDisabled {
+			if option.Config.TunnelingEnabled() {
 				log.Fatalf("Cannot use NodePort acceleration with tunneling. Either run cilium-agent with --%s=%s or --%s=%s",
 					option.NodePortAcceleration, option.NodePortAccelerationDisabled, option.TunnelName, option.TunnelDisabled)
 			}
@@ -625,7 +625,7 @@ func finishKubeProxyReplacementInit(isKubeProxyReplacementStrict bool) {
 	}
 
 	if option.Config.EnableIPv4 &&
-		option.Config.Tunnel == option.TunnelDisabled &&
+		!option.Config.TunnelingEnabled() &&
 		option.Config.NodePortMode != option.NodePortModeSNAT &&
 		len(option.Config.Devices) > 1 {
 
