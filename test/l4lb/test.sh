1b7231642a08a672c59e7b14a6d7c7c60886b286
Martynas Pumputis <m@lambda.lt>
2021-10-22 15:49:34 +0200

    test: Test XDP_REDIRECT in L4LB suite
    [ upstream commit f5f0537b6a8ac64f2f5b1fbf78cfb2ea02fc9596 ]
    
    To test that, add another veth pair to the LB node (aka
    "kind-control-plane"), and steer the LB request through it (done by
    installing ip route: $LB_VIP via $ANOTHER_VETH_PAIR_IP).  After
    receiving the request, the LB node will do XDP_REDIRECT to the previous
    veth pair (identified by the --direct-routing-device) which is used to
    connect with the kind-worker which is running the service backend.
    
    [ Backport note: Created upgrade notes for v1.10.6 instead of v1.11. ]
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- test/l4lb/test.sh
+++ test/l4lb/test.sh
@@ -31,6 +31,19 @@ clang -O2 -Wall -target bpf -c test_tc_tunnel.c -o test_tc_tunnel.o
 # The LB cilium does not connect to the kube-apiserver. For now we use Kind
 # just to create Docker-in-Docker containers.
 kind create cluster --config kind-config.yaml
+
+# Create additional veth pair which is going to be used to test XDP_REDIRECT.
+ip l a l4lb-veth0 type veth peer l4lb-veth1
+SECOND_LB_NODE_IP=3.3.3.2
+ip a a "3.3.3.1/24" dev l4lb-veth0
+CONTROL_PLANE_PID=$(docker inspect kind-control-plane -f '{{ .State.Pid }}')
+ip l s dev l4lb-veth1 netns $CONTROL_PLANE_PID
+ip l s dev l4lb-veth0 up
+nsenter -t $CONTROL_PLANE_PID -n /bin/sh -c "\
+    ip a a "${SECOND_LB_NODE_IP}/24" dev l4lb-veth1 && \
+    ip l s dev l4lb-veth1 up"
+
+# Install Cilium as standalone L4LB
 helm install cilium ../../install/kubernetes/cilium \
     --wait \
     --namespace kube-system \
@@ -44,7 +57,8 @@ helm install cilium ../../install/kubernetes/cilium \
     --set loadBalancer.mode=dsr \
     --set loadBalancer.acceleration=native \
     --set loadBalancer.dsrDispatch=ipip \
-    --set devices=eth0 \
+    --set devices='{eth0,l4lb-veth1}' \
+    --set nodePort.directRoutingDevice=eth0 \
     --set ipv6.enabled=false \
     --set affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].key="kubernetes.io/hostname" \
     --set affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].operator=In \
@@ -53,11 +67,13 @@ helm install cilium ../../install/kubernetes/cilium \
 IFIDX=$(docker exec -i kind-control-plane \
     /bin/sh -c 'echo $(( $(ip -o l show eth0 | awk "{print $1}" | cut -d: -f1) ))')
 LB_VETH_HOST=$(ip -o l | grep "if$IFIDX" | awk '{print $2}' | cut -d@ -f1)
-ip l set dev $LB_VETH_HOST  xdp obj bpf_xdp_veth_host.o
+ip l set dev $LB_VETH_HOST xdp obj bpf_xdp_veth_host.o
+ip l set dev l4lb-veth0 xdp obj bpf_xdp_veth_host.o
 
 # Disable TX and RX csum offloading, as veth does not support it. Otherwise,
 # the forwarded packets by the LB to the worker node will have invalid csums.
 ethtool -K $LB_VETH_HOST rx off tx off
+ethtool -K l4lb-veth0 rx off tx off
 
 docker exec kind-worker /bin/sh -c 'apt-get update && apt-get install -y nginx && systemctl start nginx'
 WORKER_IP=$(docker exec kind-worker ip -o -4 a s eth0 | awk '{print $4}' | cut -d/ -f1)
@@ -81,6 +97,16 @@ kubectl -n kube-system exec "${CILIUM_POD_NAME}" -- \
 
 LB_NODE_IP=$(docker exec kind-control-plane ip -o -4 a s eth0 | awk '{print $4}' | cut -d/ -f1)
 ip r a "${LB_VIP}/32" via "$LB_NODE_IP"
+
+# Issue 10 requests to LB
+for i in $(seq 1 10); do
+    curl -o /dev/null "${LB_VIP}:80"
+done
+
+# Now steer the traffic to LB_VIP via the secondary device so that XDP_REDIRECT
+# can be tested on the L4LB node
+ip r d "${LB_VIP}/32"
+ip r a "${LB_VIP}/32" via "$SECOND_LB_NODE_IP"
 
 # Issue 10 requests to LB
 for i in $(seq 1 10); do
