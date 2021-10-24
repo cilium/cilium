f9ca2c452072bc6f59cc60cda94d191636a61144
Raphael Campos <raphael@accuknox.com>
2021-10-24 20:56:10 +0000

    Validate label keys as SPIFFE IDs too
    Update the label keys validation logic to consider SPIFFE IDs.
    
    Signed-off-by: Raphael Campos <raphael@accuknox.com>
    Signed-off-by: Mauricio Vásquez <mauricio@accuknox.com>
    Signed-off-by: Mauricio Vásquez <mauricio@kinvolk.io>

--- pkg/k8s/slim/k8s/apis/labels/selector.go
+++ pkg/k8s/slim/k8s/apis/labels/selector.go
@@ -11,6 +11,11 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/selection"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/spiffe/go-spiffe/v2/spiffeid"
+
+	"github.com/google/go-cmp/cmp"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
@@ -884,6 +889,17 @@ func parse(selector string, _ *field.Path) (internalSelector, error) {
 }
 
 func validateLabelKey(k string, path *field.Path) *field.Error {
+	spiffeLabel := labels.LabelSourceSpiffe
+	if strings.HasPrefix(k, spiffeLabel) {
+		if len(k) > len(spiffeLabel)+1 {
+			spiffeID := "spiffe://" + k[len(spiffeLabel)+1:]
+			if _, err := spiffeid.FromString(spiffeID); err != nil {
+				errs := []string{err.Error()}
+				return field.Invalid(path, k, strings.Join(errs, "; "))
+			}
+		}
+		return nil
+	}
 	if errs := validation.IsQualifiedName(k); len(errs) != 0 {
 		return field.Invalid(path, k, strings.Join(errs, "; "))
 	}
