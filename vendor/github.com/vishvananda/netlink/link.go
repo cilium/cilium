63aeb6d47aee84a4aeb1428bf3fd2c11166fce1d
Daniel Borkmann <daniel@iogearbox.net>
2021-11-04 21:49:26 +0000

    cilium, vendor: Pull in latest changes from vishvananda/netlink library
    For the neighboring subsystem, we need the NTF_EXT_MANAGED flag for newer
    kernels. Additionally, the netlink library is broken with regards to pushing
    nil lladdresses into the kernel.
    
    We addressed both recently in vishvananda/netlink.
    
    While we were waiting for those two patches to land upstream, we temporarily
    used cilium/netlink with included patches. The following was used for importing
    cilium/netlink instead:
    
      go mod edit -replace github.com/vishvananda/netlink=github.com/cilium/netlink@master
      go mod tidy
      go mod vendor
      git add vendor/ go.mod go.sum && git commit -sa
    
    However, just few days ago our PR was merged, so just regular update is already
    sufficient (above instructions are kept in log for future reference). We used
    the following for this patch:
    
      go get github.com/vishvananda/netlink@master
      go mod tidy
      go mod vendor
      git add vendor/ go.mod go.sum && git commit -sa
    
    Related: https://git.kernel.org/pub/scm/linux/kernel/git/netdev/net-next.git/commit/?id=7482e3841d520a368426ac196720601687e2dc47
    Related: https://github.com/vishvananda/netlink/pull/707
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- vendor/github.com/vishvananda/netlink/link.go
+++ vendor/github.com/vishvananda/netlink/link.go
@@ -35,10 +35,13 @@ type LinkAttrs struct {
 	Alias        string
 	Statistics   *LinkStatistics
 	Promisc      int
+	Allmulti     int
+	Multi        int
 	Xdp          *LinkXdp
 	EncapType    string
 	Protinfo     *Protinfo
 	OperState    LinkOperState
+	PhysSwitchID int
 	NetNsID      int
 	NumTxQueues  int
 	NumRxQueues  int
@@ -454,6 +457,19 @@ func (ipvlan *IPVlan) Attrs() *LinkAttrs {
 
 func (ipvlan *IPVlan) Type() string {
 	return "ipvlan"
+}
+
+// IPVtap - IPVtap is a virtual interfaces based on ipvlan
+type IPVtap struct {
+	IPVlan
+}
+
+func (ipvtap *IPVtap) Attrs() *LinkAttrs {
+	return &ipvtap.LinkAttrs
+}
+
+func (ipvtap IPVtap) Type() string {
+	return "ipvtap"
 }
 
 // VlanProtocol type
@@ -946,6 +962,14 @@ func (b *BondSlave) SlaveType() string {
 	return "bond"
 }
 
+type VrfSlave struct {
+	Table uint32
+}
+
+func (v *VrfSlave) SlaveType() string {
+	return "vrf"
+}
+
 // Geneve devices must specify RemoteIP and ID (VNI) on create
 // https://github.com/torvalds/linux/blob/47ec5303d73ea344e84f46660fff693c57641386/drivers/net/geneve.c#L1209-L1223
 type Geneve struct {
@@ -1260,11 +1284,27 @@ func (ipoib *IPoIB) Type() string {
 	return "ipoib"
 }
 
+type BareUDP struct {
+	LinkAttrs
+	Port       uint16
+	EtherType  uint16
+	SrcPortMin uint16
+	MultiProto bool
+}
+
+func (bareudp *BareUDP) Attrs() *LinkAttrs {
+	return &bareudp.LinkAttrs
+}
+
+func (bareudp *BareUDP) Type() string {
+	return "bareudp"
+}
+
 // iproute2 supported devices;
 // vlan | veth | vcan | dummy | ifb | macvlan | macvtap |
 // bridge | bond | ipoib | ip6tnl | ipip | sit | vxlan |
 // gre | gretap | ip6gre | ip6gretap | vti | vti6 | nlmon |
-// bond_slave | ipvlan | xfrm
+// bond_slave | ipvlan | xfrm | bareudp
 
 // LinkNotFoundError wraps the various not found errors when
 // getting/reading links. This is intended for better error
