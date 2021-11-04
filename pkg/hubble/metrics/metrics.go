bd7b117d173c5524ba44af9cc755628b22914354
Michi Mutsuzaki <michi@isovalent.com>
2021-11-03 21:01:35 -0700

    hubble: Add "flows-to-world" metric
    This metric counts non-reply flows to external services (i.e. the
    destination identity contains `reserved:world`) with protocol and
    policy verdict as labels. The primary use case for this metric is
    to monitor policy decisions on traffic that reaches outside the cluster.
    
    Options:
    
    - any-drop: Count all drops regardless of the drop reason. By default,
                this metric counts dropped flows if and only if the drop
                reason is "Policy denied".
    
    - port    : Include the destination port as a label. Use this option
                with caution; using the destination port as a label could
                potentially cause high cardinality depending on the traffic
                pattern.
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>
    
    Co-authored-by: Alexandre Perrin <alex@kaworu.ch>
    Co-authored-by: Quentin Monnet <quentin@isovalent.com>
    Co-authored-by: Robin Hahling <robin.hahling@gw-computing.net>
    Co-authored-by: Sebastian Wicki <gandro@gmx.net>

--- pkg/hubble/metrics/metrics.go
+++ pkg/hubble/metrics/metrics.go
@@ -17,6 +17,7 @@ import (
 	_ "github.com/cilium/cilium/pkg/hubble/metrics/dns"               // invoke init
 	_ "github.com/cilium/cilium/pkg/hubble/metrics/drop"              // invoke init
 	_ "github.com/cilium/cilium/pkg/hubble/metrics/flow"              // invoke init
+	_ "github.com/cilium/cilium/pkg/hubble/metrics/flows-to-world"    // invoke init
 	_ "github.com/cilium/cilium/pkg/hubble/metrics/http"              // invoke init
 	_ "github.com/cilium/cilium/pkg/hubble/metrics/icmp"              // invoke init
 	_ "github.com/cilium/cilium/pkg/hubble/metrics/port-distribution" // invoke init
