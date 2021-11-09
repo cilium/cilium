1c9106b7082bffc56b202064534a401f11fec1db
Nate Sweet <nathanjsweet@pm.me>
2021-11-08 18:21:27 -0600

    tests: Disable K8s upstream tests that we do not support
    Tests for internalTrafficPolicy=Local and SCTP that were added
    in 1.22 should be disabled as we support neither scenario
    
    Co-authored-by: Nicolas Busseneau <nicolas@isovalent.com>
    
    Signed-off-by: Nate Sweet <nathanjsweet@pm.me>
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- test/kubernetes-test.sh
+++ test/kubernetes-test.sh
@@ -100,6 +100,10 @@ ${HOME}/go/bin/kubetest --provider=local --test \
 #  - More info at https://github.com/cilium/cilium/issues/9209
 #  - Cilium does not distinguish between UDP and TCP
 # should enforce ingress policy allowing any port traffic to a server on a specific protocol
+# should respect internalTrafficPolicy=Local Pod to Pod [Feature:ServiceInternalTrafficPolicy]
+#  - Cilium does not support internalTrafficPolicy as Local Redirect Policy allows user to enable node-local redirection.
+# should properly isolate pods that are selected by a policy allowing SCTP, even if the plugin doesn't supportSCTP [Feature:NetworkPolicy]
+#  - Cilium does not support SCTP yet
 echo "Running upstream NetworkPolicy tests"
 ${HOME}/go/bin/kubetest --provider=local --test \
-  --test_args="--ginkgo.focus=Net.*ol.* --e2e-verify-service-account=false --host ${KUBE_MASTER_URL} --ginkgo.skip=(should.not.allow.access.by.TCP.when.a.policy.specifies.only.SCTP)|(should.allow.egress.access.to.server.in.CIDR.block)|(should.enforce.except.clause.while.egress.access.to.server.in.CIDR.block)|(should.ensure.an.IP.overlapping.both.IPBlock.CIDR.and.IPBlock.Except.is.allowed)|(NetworkPolicy.between.server.and.client.using.SCTP)|(should.enforce.ingress.policy.allowing.any.port.traffic.to.a.server.on.a.specific.protocol)"
+  --test_args="--ginkgo.focus=Net.*ol.* --e2e-verify-service-account=false --host ${KUBE_MASTER_URL} --ginkgo.skip=(should.not.allow.access.by.TCP.when.a.policy.specifies.only.SCTP)|(should.allow.egress.access.to.server.in.CIDR.block)|(should.enforce.except.clause.while.egress.access.to.server.in.CIDR.block)|(should.ensure.an.IP.overlapping.both.IPBlock.CIDR.and.IPBlock.Except.is.allowed)|(NetworkPolicy.between.server.and.client.using.SCTP)|(should.enforce.ingress.policy.allowing.any.port.traffic.to.a.server.on.a.specific.protocol)|(should.respect.internalTrafficPolicy.Local.Pod.*to.Pod.*.Feature.ServiceInternalTrafficPolicy.)|(should.properly.isolate.pods.that.are.selected.by.a.policy.allowing.SCTP..even.if.the.plugin.doesn.t.support.SCTP..Feature.NetworkPolicy.)"
