cilium/cilium

commit 38d00498100f934ac2d8d5e36f397c5f41037e42

node-neigh: Do not return early if nextHop is the same
It can happen that the insertNeighbor(refresh=true) gets called before
insertNeighbor(refresh=false), and only the latter can increment the
neigh refcounting.

Remove the optimization for the sake of refcounting correctness.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/datapath/linux/node.go
@@ -698,25 +698,10 @@ func (n *linuxNodeHandler) insertNeighbor(ctx context.Context, newNode *nodeType
 	scopedLog = scopedLog.WithField(logfields.IPAddr, nextHopIPv4)
 
 	n.neighLock.Lock()
-	locked := true
-	defer func() {
-		if locked {
-			n.neighLock.Unlock()
-		}
-	}()
 
+	nextHopIsNew := false
 	if existingNextHopStr, found := n.neighNextHopByNode[newNode.Identity()]; found {
-		if existingNextHopStr == nextHopStr {
-			// We already know about the nextHop of the given newNode. Can happen
-			// when insertNeighbor is called by NodeUpdate multiple times for
-			// the same node.
-			if !refresh {
-				// In the case of refresh, don't return early, as we want to
-				// update the related neigh entry even if the nextHop is the same
-				// (e.g. to detect the GW MAC addr change).
-				return
-			}
-		} else if n.neighNextHopRefCount.Delete(existingNextHopStr) {
+		if existingNextHopStr != nextHopStr && n.neighNextHopRefCount.Delete(existingNextHopStr) {
 			// nextHop has changed and nobody else is using it, so remove the old one.
 			neigh, found := n.neighByNextHop[existingNextHopStr]
 			if found {
@@ -736,17 +721,17 @@ func (n *linuxNodeHandler) insertNeighbor(ctx context.Context, newNode *nodeType
 				}
 			}
 		}
+	} else {
+		// nextHop for the given node was previously not found, so let's
+		// increment ref counter.  This can happen upon regular NodeUpdate event
+		// or by the periodic ARP refresher which got executed before
+		// NodeUpdate().
+		nextHopIsNew = n.neighNextHopRefCount.Add(nextHopStr)
 	}
 
 	n.neighNextHopByNode[newNode.Identity()] = nextHopStr
 
-	nextHopIsNew := false
-	if !refresh {
-		nextHopIsNew = n.neighNextHopRefCount.Add(nextHopStr)
-	}
-
 	n.neighLock.Unlock() // to allow concurrent arpings below
-	locked = false
 
 	// nextHop hasn't been arpinged before OR we are refreshing neigh entry
 	var hwAddr net.HardwareAddr
@@ -761,7 +746,7 @@ func (n *linuxNodeHandler) insertNeighbor(ctx context.Context, newNode *nodeType
 	}
 
 	n.neighLock.Lock()
-	locked = true
+	defer n.neighLock.Unlock()
 
 	if hwAddr != nil {
 		if prevHwAddr, found := n.neighByNextHop[nextHopStr]; found && prevHwAddr.String() == hwAddr.String() {
@@ -817,6 +802,7 @@ func (n *linuxNodeHandler) deleteNeighbor(oldNode *nodeTypes.Node) {
 	defer func() { delete(n.neighNextHopByNode, oldNode.Identity()) }()
 
 	if n.neighNextHopRefCount.Delete(nextHopStr) {
+
 		neigh, found := n.neighByNextHop[nextHopStr]
 		delete(n.neighByNextHop, nextHopStr)
 

commit 58cd74a259e773fef303f387d79e5b152798933a

neigh: Fix changing next hop refcount management
In the case we hit existingNextHopStr != nextHopStr condition for insertNeighbor*()
then this means that a next hop changed, and while we delete the old one, that is,
existingNextHopStr, we do not [create and/or] bump the refcount of the new one,
that is nextHopStr. This may lead to invalid removed neighbor entry where the BPF
program's fib lookup could fail.

Fixes: 38d00498100f ("node-neigh: Do not return early if nextHop is the same")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/datapath/linux/node.go
@@ -751,25 +751,29 @@ func (n *linuxNodeHandler) insertNeighbor4(ctx context.Context, newNode *nodeTyp
 
 	nextHopIsNew := false
 	if existingNextHopStr, found := n.neighNextHopByNode4[newNode.Identity()]; found {
-		if existingNextHopStr != nextHopStr && n.neighNextHopRefCount.Delete(existingNextHopStr) {
-			// nextHop has changed and nobody else is using it, so remove the old one.
-			neigh, found := n.neighByNextHop[existingNextHopStr]
-			if found {
-				// Note that we don't move the removal via netlink which might
-				// block from the hot path (e.g. with defer), as this case can
-				// happen very rarely.
-				//
-				// The neighbor's HW address is ignored on delete. Only the IP
-				// address and device is checked.
-				if err := netlink.NeighDel(neigh); err != nil {
-					scopedLog.WithFields(logrus.Fields{
-						logfields.NextHop:   neigh.IP,
-						logfields.LinkIndex: neigh.LinkIndex,
-					}).WithError(err).Info("Unable to remove next hop")
+		if existingNextHopStr != nextHopStr {
+			if n.neighNextHopRefCount.Delete(existingNextHopStr) {
+				neigh, found := n.neighByNextHop[existingNextHopStr]
+				if found {
+					// Note that we don't move the removal via netlink which might
+					// block from the hot path (e.g. with defer), as this case can
+					// happen very rarely.
+					//
+					// The neighbor's HW address is ignored on delete. Only the IP
+					// address and device is checked.
+					if err := netlink.NeighDel(neigh); err != nil {
+						scopedLog.WithFields(logrus.Fields{
+							logfields.NextHop:   neigh.IP,
+							logfields.LinkIndex: neigh.LinkIndex,
+						}).WithError(err).Info("Unable to remove next hop")
+					}
+					delete(n.neighByNextHop, existingNextHopStr)
+					delete(n.neighLastPingByNextHop, existingNextHopStr)
 				}
-				delete(n.neighByNextHop, existingNextHopStr)
-				delete(n.neighLastPingByNextHop, existingNextHopStr)
 			}
+			// Given nextHop has changed and we removed the old one, we
+			// now need to increment ref counter for the new one.
+			nextHopIsNew = n.neighNextHopRefCount.Add(nextHopStr)
 		}
 	} else {
 		// nextHop for the given node was previously not found, so let's
@@ -812,25 +816,30 @@ func (n *linuxNodeHandler) insertNeighbor6(ctx context.Context, newNode *nodeTyp
 
 	nextHopIsNew := false
 	if existingNextHopStr, found := n.neighNextHopByNode6[newNode.Identity()]; found {
-		if existingNextHopStr != nextHopStr && n.neighNextHopRefCount.Delete(existingNextHopStr) {
-			// nextHop has changed and nobody else is using it, so remove the old one.
-			neigh, found := n.neighByNextHop[existingNextHopStr]
-			if found {
-				// Note that we don't move the removal via netlink which might
-				// block from the hot path (e.g. with defer), as this case can
-				// happen very rarely.
-				//
-				// The neighbor's HW address is ignored on delete. Only the IP
-				// address and device is checked.
-				if err := netlink.NeighDel(neigh); err != nil {
-					scopedLog.WithFields(logrus.Fields{
-						logfields.NextHop:   neigh.IP,
-						logfields.LinkIndex: neigh.LinkIndex,
-					}).WithError(err).Info("Unable to remove next hop")
+		if existingNextHopStr != nextHopStr {
+			if n.neighNextHopRefCount.Delete(existingNextHopStr) {
+				// nextHop has changed and nobody else is using it, so remove the old one.
+				neigh, found := n.neighByNextHop[existingNextHopStr]
+				if found {
+					// Note that we don't move the removal via netlink which might
+					// block from the hot path (e.g. with defer), as this case can
+					// happen very rarely.
+					//
+					// The neighbor's HW address is ignored on delete. Only the IP
+					// address and device is checked.
+					if err := netlink.NeighDel(neigh); err != nil {
+						scopedLog.WithFields(logrus.Fields{
+							logfields.NextHop:   neigh.IP,
+							logfields.LinkIndex: neigh.LinkIndex,
+						}).WithError(err).Info("Unable to remove next hop")
+					}
+					delete(n.neighByNextHop, existingNextHopStr)
+					delete(n.neighLastPingByNextHop, existingNextHopStr)
 				}
-				delete(n.neighByNextHop, existingNextHopStr)
-				delete(n.neighLastPingByNextHop, existingNextHopStr)
 			}
+			// Given nextHop has changed and we removed the old one, we
+			// now need to increment ref counter for the new one.
+			nextHopIsNew = n.neighNextHopRefCount.Add(nextHopStr)
 		}
 	} else {
 		// nextHop for the given node was previously not found, so let's