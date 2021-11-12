701cfb62549173027fcc3ab497cb9dea8ec38da2
Daniel Borkmann <daniel@iogearbox.net>
2021-11-12 14:24:26 +0000

    neigh: Add runtime test for changing IPv4 nextHop address
    Add the same test to IPv4 that we have for IPv6.
    
    Both test suites check for proper handling of a2754041dcee ("neigh: Fix
    changing next hop refcount management").
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- pkg/datapath/linux/node_linux_test.go
+++ pkg/datapath/linux/node_linux_test.go
@@ -1740,6 +1740,7 @@ func (s *linuxPrivilegedIPv4OnlyTestSuite) TestArpPingHandling(c *check.C) {
 	_, ipnet, err := net.ParseCIDR("9.9.9.252/29")
 	ip0 := net.ParseIP("9.9.9.249")
 	ip1 := net.ParseIP("9.9.9.250")
+	ipG := net.ParseIP("9.9.9.251")
 	ipnet.IP = ip0
 	addr := &netlink.Addr{IPNet: ipnet}
 	err = netlink.AddrAdd(veth0, addr)
@@ -1756,6 +1757,10 @@ func (s *linuxPrivilegedIPv4OnlyTestSuite) TestArpPingHandling(c *check.C) {
 		veth1, err := netlink.LinkByName("veth1")
 		c.Assert(err, check.IsNil)
 		ipnet.IP = ip1
+		addr = &netlink.Addr{IPNet: ipnet}
+		netlink.AddrAdd(veth1, addr)
+		c.Assert(err, check.IsNil)
+		ipnet.IP = ipG
 		addr = &netlink.Addr{IPNet: ipnet}
 		netlink.AddrAdd(veth1, addr)
 		c.Assert(err, check.IsNil)
@@ -2226,6 +2231,193 @@ refetch5:
 		}
 	}
 	c.Assert(found, check.Equals, false)
+
+	// Setup routine for the 3. test
+	setupNewGateway := func(vethCIDR, gwIP string) (errRet error) {
+		ipGw := net.ParseIP(gwIP)
+		ip, ipnet, err := net.ParseCIDR(vethCIDR)
+		if err != nil {
+			errRet = err
+			return
+		}
+		ipnet.IP = ip
+		route := &netlink.Route{
+			Dst: ipnet,
+			Gw:  ipGw,
+		}
+		errRet = netlink.RouteReplace(route)
+		return
+	}
+
+	// In the last test, we add node 2,3 again, and then change the nextHop
+	// address to check the refcount behavior, and that the old one was
+	// deleted from the neighbor table as well as the new one added.
+	now = time.Now()
+	c.Assert(linuxNodeHandler.NodeAdd(nodev2), check.IsNil)
+	wait(nodev2.Identity(), &now, false)
+
+	now = time.Now()
+	c.Assert(linuxNodeHandler.NodeAdd(nodev3), check.IsNil)
+	wait(nodev3.Identity(), &now, false)
+
+	nextHop = net.ParseIP("9.9.9.250")
+refetch6:
+	// Check that both node{2,3} are via nextHop (gw)
+	neighs, err = netlink.NeighList(veth0.Attrs().Index, netlink.FAMILY_V4)
+	c.Assert(err, check.IsNil)
+	found = false
+	for _, n := range neighs {
+		if n.IP.Equal(nextHop) {
+			good, retry := neighStateOk(n)
+			if good {
+				found = true
+				break
+			}
+			if retry {
+				goto refetch6
+			}
+		} else if n.IP.Equal(node2IP) || n.IP.Equal(node3IP) {
+			c.ExpectFailure("node{2,3} should not be in the same L2")
+		}
+	}
+	c.Assert(found, check.Equals, true)
+
+	// Switch to new nextHop address for node2
+	err = setupNewGateway("8.8.8.248/29", "9.9.9.251")
+	c.Assert(err, check.IsNil)
+
+	// waitGw waits for the nextHop to appear in the agent's nextHop table
+	waitGw := func(nextHopNew string, nodeID nodeTypes.Identity, before *time.Time) {
+		err := testutils.WaitUntil(func() bool {
+			linuxNodeHandler.neighLock.Lock()
+			defer linuxNodeHandler.neighLock.Unlock()
+			nextHop, found := linuxNodeHandler.neighNextHopByNode4[nodeID]
+			if !found {
+				return false
+			}
+			if nextHop != nextHopNew {
+				return false
+			}
+			lastPing, found := linuxNodeHandler.neighLastPingByNextHop[nextHop]
+			if !found {
+				return false
+			}
+			return before.Before(lastPing)
+		}, 5*time.Second)
+		c.Assert(err, check.IsNil)
+	}
+
+	// insertNeighbor is invoked async, so thus this wait based on last ping
+	now = time.Now()
+	linuxNodeHandler.NodeNeighborRefresh(context.Background(), nodev2)
+	linuxNodeHandler.NodeNeighborRefresh(context.Background(), nodev3)
+	waitGw("9.9.9.251", nodev2.Identity(), &now)
+	waitGw("9.9.9.250", nodev3.Identity(), &now)
+
+	// Both nextHops now need to be present
+	nextHop = net.ParseIP("9.9.9.250")
+refetch7:
+	neighs, err = netlink.NeighList(veth0.Attrs().Index, netlink.FAMILY_V4)
+	c.Assert(err, check.IsNil)
+	found = false
+	for _, n := range neighs {
+		if n.IP.Equal(nextHop) {
+			good, retry := neighStateOk(n)
+			if good {
+				found = true
+				break
+			}
+			if retry {
+				goto refetch7
+			}
+		} else if n.IP.Equal(node2IP) || n.IP.Equal(node3IP) {
+			c.ExpectFailure("node{2,3} should not be in the same L2")
+		}
+	}
+	c.Assert(found, check.Equals, true)
+
+	nextHop = net.ParseIP("9.9.9.251")
+refetch8:
+	neighs, err = netlink.NeighList(veth0.Attrs().Index, netlink.FAMILY_V4)
+	c.Assert(err, check.IsNil)
+	found = false
+	for _, n := range neighs {
+		if n.IP.Equal(nextHop) {
+			good, retry := neighStateOk(n)
+			if good {
+				found = true
+				break
+			}
+			if retry {
+				goto refetch8
+			}
+		} else if n.IP.Equal(node2IP) || n.IP.Equal(node3IP) {
+			c.ExpectFailure("node{2,3} should not be in the same L2")
+		}
+	}
+	c.Assert(found, check.Equals, true)
+
+	// Now also switch over the other node.
+	err = setupNewGateway("7.7.7.248/29", "9.9.9.251")
+	c.Assert(err, check.IsNil)
+
+	// insertNeighbor is invoked async, so thus this wait based on last ping
+	now = time.Now()
+	linuxNodeHandler.NodeNeighborRefresh(context.Background(), nodev2)
+	linuxNodeHandler.NodeNeighborRefresh(context.Background(), nodev3)
+	waitGw("9.9.9.251", nodev2.Identity(), &now)
+	waitGw("9.9.9.251", nodev3.Identity(), &now)
+
+	nextHop = net.ParseIP("9.9.9.250")
+refetch9:
+	// Check that old nextHop address got removed
+	neighs, err = netlink.NeighList(veth0.Attrs().Index, netlink.FAMILY_V4)
+	c.Assert(err, check.IsNil)
+	found = false
+	for _, n := range neighs {
+		if n.IP.Equal(nextHop) {
+			good, retry := neighStateOk(n)
+			if good {
+				found = true
+				break
+			}
+			if retry {
+				goto refetch9
+			}
+		} else if n.IP.Equal(node2IP) || n.IP.Equal(node3IP) {
+			c.ExpectFailure("node{2,3} should not be in the same L2")
+		}
+	}
+	c.Assert(found, check.Equals, false)
+
+	nextHop = net.ParseIP("9.9.9.251")
+refetch10:
+	// Check that new nextHop address got added
+	neighs, err = netlink.NeighList(veth0.Attrs().Index, netlink.FAMILY_V4)
+	c.Assert(err, check.IsNil)
+	found = false
+	for _, n := range neighs {
+		if n.IP.Equal(nextHop) {
+			good, retry := neighStateOk(n)
+			if good {
+				found = true
+				break
+			}
+			if retry {
+				goto refetch10
+			}
+		} else if n.IP.Equal(node2IP) || n.IP.Equal(node3IP) {
+			c.ExpectFailure("node{2,3} should not be in the same L2")
+		}
+	}
+	c.Assert(found, check.Equals, true)
+
+	c.Assert(linuxNodeHandler.NodeDelete(nodev3), check.IsNil)
+	wait(nodev3.Identity(), nil, true)
+	c.Assert(linuxNodeHandler.NodeDelete(nodev2), check.IsNil)
+	wait(nodev2.Identity(), nil, true)
+
+	linuxNodeHandler.NodeCleanNeighbors(false)
 }
 
 func (s *linuxPrivilegedBaseTestSuite) benchmarkNodeUpdate(c *check.C, config datapath.LocalNodeConfiguration) {
