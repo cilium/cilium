7f8cb9dc0f9cf11937ff199df0b448975ca85a59
Paul Chaignon <paul@cilium.io>
2021-11-12 09:41:22 +0100

    bpf: Perform SRv6 policy lookup
    To perform the SRv6 policy lookup, we must first lookup the appropriate
    VRF where to perform the policy lookup. This first lookup is performed
    in LPM maps using source and destination addresses. The second lookup
    then uses the retrieved VRF ID and the destination address in another
    LPM map to get the SID, used as the outer destination IPv6 address.
    
    The lookups should only be performed for connections egressing the
    cluster (i.e., same as for the egress gateway). Otherwise, a destination
    pod IP address could match an entry in the LPM map if e.g. the user
    defined a redirect for destination CIDR 0.0.0.0/0. In addition, we don't
    want to perform the lookup and encapsulation for reply traffic (which
    should leave the cluster the same way it ingressed).
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- bpf/lib/egress_policies.h
+++ bpf/lib/egress_policies.h
@@ -29,4 +29,86 @@ struct egress_gw_policy_entry *lookup_ip4_egress_gw_policy(__be32 saddr, __be32 
 }
 
 #endif /* ENABLE_EGRESS_GATEWAY */
+
+#ifdef ENABLE_SRV6
+# ifdef ENABLE_IPV4
+
+/* SRV6_VRF_STATIC_PREFIX4 gets sizeof non-IP, non-prefix part of
+ * srv6_vrf_key4.
+ */
+#  define SRV6_VRF_STATIC_PREFIX4						\
+	(8 * (sizeof(struct srv6_vrf_key4) - sizeof(struct bpf_lpm_trie_key)\
+	      - 4))
+#  define SRV6_VRF_PREFIX4_LEN(PREFIX) (SRV6_VRF_STATIC_PREFIX4 + (PREFIX))
+#  define SRV6_VRF_IPV4_PREFIX SRV6_VRF_PREFIX4_LEN(32)
+static __always_inline __u32*
+srv6_lookup_vrf4(__be32 sip, __be32 dip)
+{
+	struct srv6_vrf_key4 key = {
+		.lpm = { SRV6_VRF_IPV4_PREFIX, {} },
+		.src_ip = sip,
+		.dst_cidr = dip,
+	};
+	return map_lookup_elem(&SRV6_VRF_MAP4, &key);
+}
+
+/* SRV6_POLICY_STATIC_PREFIX4 gets sizeof non-IP, non-prefix part of
+ * srv6_policy_key4.
+ */
+#  define SRV6_POLICY_STATIC_PREFIX4						\
+	(8 * (sizeof(struct srv6_policy_key4) - sizeof(struct bpf_lpm_trie_key)	\
+	      - 4))
+#  define SRV6_POLICY_PREFIX4_LEN(PREFIX) (SRV6_POLICY_STATIC_PREFIX4 + (PREFIX))
+#  define SRV6_POLICY_IPV4_PREFIX SRV6_POLICY_PREFIX4_LEN(32)
+static __always_inline union v6addr *
+srv6_lookup_policy4(__u32 vrf_id, __be32 dip)
+{
+	struct srv6_policy_key4 key = {
+		.lpm = { SRV6_POLICY_IPV4_PREFIX, {} },
+		.vrf_id = vrf_id,
+		.dst_cidr = dip,
+	};
+	return map_lookup_elem(&SRV6_POLICY_MAP4, &key);
+}
+# endif /* ENABLE_IPV4 */
+
+/* SRV6_VRF_STATIC_PREFIX6 gets sizeof non-IP, non-prefix part of
+ * srv6_vrf_key6.
+ */
+#  define SRV6_VRF_STATIC_PREFIX6						\
+	(8 * (sizeof(struct srv6_vrf_key6) - sizeof(struct bpf_lpm_trie_key)\
+	      - 4))
+#  define SRV6_VRF_PREFIX6_LEN(PREFIX) (SRV6_VRF_STATIC_PREFIX6 + (PREFIX))
+#  define SRV6_VRF_IPV6_PREFIX SRV6_VRF_PREFIX6_LEN(32)
+static __always_inline __u32*
+srv6_lookup_vrf6(const struct in6_addr *sip, const struct in6_addr *dip)
+{
+	struct srv6_vrf_key6 key = {
+		.lpm = { SRV6_VRF_IPV6_PREFIX, {} },
+		.src_ip = *(union v6addr *)sip,
+		.dst_cidr = *(union v6addr *)dip,
+	};
+	return map_lookup_elem(&SRV6_VRF_MAP6, &key);
+}
+
+/* SRV6_POLICY_STATIC_PREFIX6 gets sizeof non-IP, non-prefix part of
+ * srv6_policy_key6.
+ */
+# define SRV6_POLICY_STATIC_PREFIX6						\
+	(8 * (sizeof(struct srv6_policy_key6) - sizeof(struct bpf_lpm_trie_key)	\
+	      - 4))
+# define SRV6_POLICY_PREFIX6_LEN(PREFIX) (SRV6_POLICY_STATIC_PREFIX6 + (PREFIX))
+# define SRV6_POLICY_IPV6_PREFIX SRV6_POLICY_PREFIX6_LEN(128)
+
+static __always_inline union v6addr *
+srv6_lookup_policy6(__u32 vrf_id, const struct in6_addr *dip)
+{
+	struct srv6_policy_key6 key = {
+		.lpm = { SRV6_POLICY_IPV6_PREFIX, {} },
+		.vrf_id = vrf_id,
+		.dst_cidr = *(union v6addr *)dip,
+	};
+	return map_lookup_elem(&SRV6_POLICY_MAP6, &key);
+}
+#endif /* ENABLE_SRV6 */
 #endif /* __LIB_EGRESS_POLICIES_H_ */
