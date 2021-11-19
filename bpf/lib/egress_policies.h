0a3801b2846875371c14dd6eee06079ebe6e3c85
Paul Chaignon <paul@cilium.io>
2021-11-19 17:17:00 +0100

    bpf: Implement SRv6 encapsulation
    IPv4 and IPv6 packets are encapsulated in a IPv6 packet with the SID as
    the destination IP address, as follows:
    
    [srcPodIPv6 -> dstIPv6] => [srcPodIPv6 -> SID][srcPodIPv6 -> dstIPv6]
    [srcPodIPv4 -> dstIPv4] => [srcPodIPv4-mapped IPv6 -> SID][srcPodIPv4 -> dstIPv4]
    
    For IPv6, the encapsulation is straighforward: we insert space after the
    L2 header and insert the full outer IPv6 header there.
    
    IPv4 is a bit more involved: because the L3 protocol will now be IPv6,
    we need to change the protocol field in the packet and in the skb. Doing
    this requires a call to ctx_change_proto which insert space after the L2
    header, but not enough. So we still need to insert additional space
    afterward, taking into account what ctx_change_proto already inserted.
    
    Initial IPv4 packet:
      +-------+---------+------------+
      | L2    | IPv4    | Payload... |
      +-------+---------+------------+
    After ctx_change_proto() call:
      +-------+---+---------+------------+
      | L2    |   | IPv4    | Payload... |
      +-------+---+---------+------------+
    After ctx_adjust_hroom() call:
      +-------+-------------+---------+------------+
      | L2    |             | IPv4    | Payload... |
      +-------+-------------+---------+------------+
    Before sending to stack:
      +-------+-------------+---------+------------+
      | L2    | IPv6        | IPv4    | Payload... |
      +-------+-------------+---------+------------+
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- bpf/lib/egress_policies.h
+++ bpf/lib/egress_policies.h
@@ -107,6 +107,118 @@ srv6_lookup6(const void *map, const union v6addr *sip,
 	srv6_lookup6(&SRV6_MAP6, (union v6addr *)sip, (union v6addr *)dip)
 
 # ifndef SKIP_SRV6_HANDLING
+static __always_inline __u64 ctx_adjust_hroom_flags(void)
+{
+#ifdef BPF_HAVE_CSUM_LEVEL
+	return BPF_F_ADJ_ROOM_NO_CSUM_RESET;
+#else
+	return 0;
+#endif
+}
+
+static __always_inline int
+srv6_encapsulation(struct __ctx_buff *ctx, int growth, __u16 new_payload_len,
+		   __u8 nexthdr, struct in6_addr *saddr, struct in6_addr *sid)
+{
+	__u32 len = sizeof(struct ipv6hdr) - 2 * sizeof(struct in6_addr);
+	struct ipv6hdr new_ip6 = {
+		.version     = 0x6,
+		.payload_len = bpf_htons(new_payload_len),
+		.nexthdr     = nexthdr,
+		.hop_limit   = IPDEFTTL,
+	};
+
+	/* Add room between Ethernet and network headers. */
+	if (ctx_adjust_hroom(ctx, growth, BPF_ADJ_ROOM_MAC,
+			     ctx_adjust_hroom_flags()))
+		return DROP_INVALID;
+	if (ctx_store_bytes(ctx, ETH_HLEN, &new_ip6, len, 0) < 0)
+		return DROP_WRITE_ERROR;
+	if (ctx_store_bytes(ctx, ETH_HLEN + offsetof(struct ipv6hdr, saddr),
+			    saddr, sizeof(struct in6_addr), 0) < 0)
+		return DROP_WRITE_ERROR;
+	if (ctx_store_bytes(ctx, ETH_HLEN + offsetof(struct ipv6hdr, daddr),
+			    sid, sizeof(struct in6_addr), 0) < 0)
+		return DROP_WRITE_ERROR;
+	return 0;
+}
+
+static __always_inline int
+srv6_handling(struct __ctx_buff *ctx, struct in6_addr *sid)
+{
+	__u16 inner_proto, new_payload_len, outer_proto = bpf_htons(ETH_P_IPV6);
+	union v6addr nat46_prefix = NAT46_PREFIX;
+	void *data, *data_end;
+	struct in6_addr saddr;
+	struct ipv6hdr *ip6;
+	struct iphdr *ip4;
+	__u8 nexthdr;
+	int growth;
+
+	if (!validate_ethertype(ctx, &inner_proto))
+		return DROP_UNSUPPORTED_L2;
+
+	switch (inner_proto) {
+#  ifdef ENABLE_IPV6
+	case bpf_htons(ETH_P_IPV6):
+		/* Inner packet is IPv6. */
+		if (!revalidate_data(ctx, &data, &data_end, &ip6))
+			return DROP_INVALID;
+		nexthdr = IPPROTO_IPV6;
+		new_payload_len = bpf_ntohs(ip6->payload_len) + sizeof(struct ipv6hdr);
+		saddr = ip6->saddr;
+		growth = sizeof(struct ipv6hdr);
+		break;
+#  endif /* ENABLE_IPV6 */
+#  ifdef ENABLE_IPV4
+	case bpf_htons(ETH_P_IP):
+		/* Inner packet is IPv4. */
+		if (!revalidate_data(ctx, &data, &data_end, &ip4))
+			return DROP_INVALID;
+		nexthdr = IPPROTO_IPIP;
+		/* IPv4's tot_len fields has the size of the entire packet
+		 * including headers while IPv6's payload_len field has only
+		 * the size of the IPv6 payload. Therefore, without IPv6
+		 * extension headers (none here), the outer IPv6 payload_len
+		 * is equal to the inner IPv4 tot_len.
+		 */
+		new_payload_len = bpf_ntohs(ip4->tot_len) - (ip4->ihl << 2) + sizeof(struct iphdr);
+		/* We need to specify an SRv6 source address to which the
+		 * reply will be sent. We use the IPv6-mapped IPv4 source
+		 * address of the inner packet for that purpose.
+		 */
+		saddr.s6_addr32[0] = nat46_prefix.p1;
+		/* TODO: Temporary inverted to avoid drop in kernel (cf. dcc32f4f). */
+		saddr.s6_addr32[1] = nat46_prefix.p3;
+		saddr.s6_addr32[2] = nat46_prefix.p2;
+		saddr.s6_addr32[3] = bpf_htonl((bpf_ntohl(nat46_prefix.p4) & 0xFFFF0000) |
+					       (bpf_ntohl(ip4->saddr) & 0xFFFF));
+
+		/* We need to change skb->protocol and the corresponding packet
+		 * field because the L3 protocol will now be IPv6.
+		 */
+		if (ctx_change_proto(ctx, outer_proto, 0) < 0)
+			return DROP_WRITE_ERROR;
+		if (ctx_store_bytes(ctx, offsetof(struct ethhdr, h_proto),
+				    &outer_proto, sizeof(outer_proto), 0) < 0)
+			return DROP_WRITE_ERROR;
+		/* ctx_change_proto above grows the packet from IPv4 header
+		 * length to IPv6 header length. It adds the additional space
+		 * before the inner L3 header, in the same place we will later
+		 * add the outer IPv6 header.
+		 * Thus, deduce this space from the next packet growth.
+		 */
+		growth = sizeof(struct iphdr);
+		break;
+#  endif /* ENABLE_IPV4 */
+	default:
+		return DROP_INVALID;
+	}
+
+	return srv6_encapsulation(ctx, growth, new_payload_len, nexthdr,
+				  &saddr, sid);
+}
+
 static __always_inline void
 srv6_load_meta_sid(struct __ctx_buff *ctx, struct in6_addr *sid)
 {
@@ -132,7 +244,7 @@ int tail_srv6_encap(struct __ctx_buff *ctx)
 	int ret = 0;
 
 	srv6_load_meta_sid(ctx, &sid);
-	/* TODO: Perform SRv6 encapsulation. */
+	ret = srv6_handling(ctx, &sid);
 
 	if (ret < 0)
 		return send_drop_notify_error(ctx, SECLABEL, ret, CTX_ACT_DROP,
