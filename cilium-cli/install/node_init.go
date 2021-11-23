c9bed1c396a385e44ed5432a6a139c6c556b46c3
Bruno M. Custódio <brunomcustodio@gmail.com>
2021-11-23 11:08:58 +0000

    Add a node-init script for EKS.
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: Bruno M. Custódio <brunomcustodio@gmail.com>

--- cilium-cli/install/node_init.go
+++ cilium-cli/install/node_init.go
@@ -16,6 +16,23 @@ import (
 
 var (
 	nodeInitScript = map[k8s.Kind]string{
+		k8s.KindEKS: bash(`
+# When running in AWS ENI mode, it's likely that 'aws-node' has
+# had a chance to install SNAT iptables rules. These can result
+# in dropped traffic, so we should attempt to remove them.
+# We do it using a 'postStart' hook since this may need to run
+# for nodes which might have already been init'ed but may still
+# have dangling rules. This is safe because there are no
+# dependencies on anything that is part of the startup script
+# itself, and can be safely run multiple times per node (e.g. in
+# case of a restart).
+if [[ "$(iptables-save | grep -c AWS-SNAT-CHAIN)" != "0" ]];
+then
+	echo 'Deleting iptables rules created by the AWS CNI VPC plugin'
+	iptables-save | grep -v AWS-SNAT-CHAIN | iptables-restore
+fi
+echo 'Done!'
+`),
 		k8s.KindGKE: bash(`
 echo "Changing kubelet configuration to --network-plugin=cni --cni-bin-dir=/home/kubernetes/bin"
 mkdir -p /home/kubernetes/bin
