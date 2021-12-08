cilium/cilium

commit f559cf1cecb0780eb3a815c951fe599f4918e474

selectorcache: Release identities on selector removal
FQDN selectors hold references to CIDR identities, one for each
'fqdnSelector.cachedSelections' entry. Previously, these would be
associated with the fqdnSelector during creation of the selector but
they were not released when the fqdnSelector is deleted from the cache.

Commit de10e82639cc ("fqdn: Move identity allocation to FQDN selector")
fixed a similar bug recently during the update of existing selectors;
this code is balancing the release for identities allocated during the
initial creation of each FQDN selector.

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/policy/selectorcache.go
@@ -144,6 +144,11 @@ type identitySelector interface {
 	// Called with NameManager and SelectorCache locks held
 	removeUser(CachedSelectionUser, identityNotifier) (last bool)
 
+	// releaseIdentityMappings must be called exactly once upon release of
+	// this selector to ensure that resources are cleaned up upon deletion
+	// of the selector.
+	releaseIdentityMappings(cache.IdentityAllocator)
+
 	// This may be called while the NameManager lock is held. wg.Wait()
 	// returns after user notifications have been completed, which may require
 	// taking Endpoint and SelectorCache locks, so these locks must not be
@@ -448,6 +453,11 @@ func (f *fqdnSelector) notifyUsers(sc *SelectorCache, added, deleted []identity.
 // upon new use of an FQDN selector with the same string representation of a
 // previously-cached selector (for example the same toFQDNs match pattern in
 // different rules).
+//
+// Calls to allocateIdentityMappings() will allocate identities and associate
+// them with the fqdnSelector, therefore the corresponding
+// releaseIdentityMappings() function must also be eventually called to clean
+// up this selector.
 func (f *fqdnSelector) allocateIdentityMappings(idAllocator cache.IdentityAllocator, selectorIPMapping map[api.FQDNSelector][]net.IP) {
 	// We don't know whether the IPs are associated with this selector
 	// until we map those IPs to identities, which requires potentially
@@ -499,6 +509,20 @@ func (f *fqdnSelector) allocateIdentityMappings(idAllocator cache.IdentityAlloca
 	ctx, cancel := context.WithTimeout(context.TODO(), option.Config.KVstoreConnectivityTimeout)
 	defer cancel()
 	idAllocator.ReleaseCIDRIdentitiesByID(ctx, identitiesToRelease)
+}
+
+// releaseIdentityMappings must be called exactly once for the received
+// 'fqdnSelector' in order to release CIDR identity references held in this
+// selector's cachedSelections.
+func (f *fqdnSelector) releaseIdentityMappings(idAllocator cache.IdentityAllocator) {
+	ids := make([]identity.NumericIdentity, 0, len(f.cachedSelections))
+	for id := range f.cachedSelections {
+		ids = append(ids, id)
+	}
+
+	ctx, cancel := context.WithTimeout(context.TODO(), option.Config.KVstoreConnectivityTimeout)
+	defer cancel()
+	idAllocator.ReleaseCIDRIdentitiesByID(ctx, ids)
 }
 
 // identityNotifier provides a means for other subsystems to be made aware of a
@@ -580,6 +604,10 @@ func (l *labelIdentitySelector) matchesNamespace(ns string) bool {
 
 func (l *labelIdentitySelector) matches(identity scIdentity) bool {
 	return l.matchesNamespace(identity.namespace) && l.selector.Matches(identity.lbls)
+}
+
+func (l *labelIdentitySelector) releaseIdentityMappings(idAllocator cache.IdentityAllocator) {
+	// labelIdentitySelectors don't retain identity references, so no-op.
 }
 
 //
@@ -869,6 +897,7 @@ func (sc *SelectorCache) removeSelectorLocked(selector CachedSelector, user Cach
 		if sel.removeUser(user, sc.localIdentityNotifier) {
 			delete(sc.selectors, key)
 		}
+		sel.releaseIdentityMappings(sc.idAllocator)
 	}
 }
 

commit 64ca48cdcf4edb6e7a08fb8e1a0af1dcc0323a80

policy: Fix selector identity release for FQDN
Alexander reports in GitHub issue 18023 that establishing a connection
via an FQDN policy, then modifying that FQDN policy, would cause
subsequent traffic to the FQDN to be dropped, even if the new policy
still allowed the same traffic via a toFQDN statement.

This was caused by overzealous release of CIDR identities while
generating a new policy. Although the policy calculation itself keeps
all selectorcache entries alive during the policy generation phase (see
cachedSelectorPolicy.setPolicy() ), after the new policy is inserted
into the PolicyCache, the distillery package would clean up the old
policy. As part of that cleanup, it would call into the individual
selector to call the RemoveSelectors() function.

The previous implementation of this logic unintentionally released the
underlying identities any time a user of a selector was released, rather
than only releasing the underlying identities when the number of users
reached zero and the selector itself would be released. This meant that
rather than the selectorcache retaining references to the underlying
identities when a policy was updated, instead the references would be
released (and all corresponding BPF resources cleaned up) at the end of
the process. This then triggered subsequent connectivity outages.

Fix it by only releasing the identity references once the cached
selector itself is removed from the SelectorCache.

Fixes: f559cf1cecb0 ("selectorcache: Release identities on selector removal")
Reported-by: Alexander Block <ablock84@gmail.com>
Suggested-by: Jarno Rajahalme <jarno@cilium.io>
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/policy/selectorcache.go
@@ -896,8 +896,8 @@ func (sc *SelectorCache) removeSelectorLocked(selector CachedSelector, user Cach
 	if exists {
 		if sel.removeUser(user, sc.localIdentityNotifier) {
 			delete(sc.selectors, key)
+			sel.releaseIdentityMappings(sc.idAllocator)
 		}
-		sel.releaseIdentityMappings(sc.idAllocator)
 	}
 }
 