73b69ba3ecf1abca32714ce5118c3b6967164954
Martynas Pumputis <m@lambda.lt>
2021-12-09 10:34:19 +0100

    test: Skip graceful term tests if no kube-proxy
    The graceful termination suite disables KPR (only bpf_lxc per-packet LB
    is enabled for pod netns), which makes the K8s cluster half-functioning
    if there is no kube-proxy installed. E.g., DNS resolution via CoreDNS
    from host netns will fail.
    
    The suite should be tested by 4.19 considering that it runs K8s 1.22.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/k8sT/Services.go
+++ test/k8sT/Services.go
@@ -1552,7 +1552,8 @@ Secondary Interface %s :: IPv4: (%s, %s), IPv6: (%s, %s)`, helpers.DualStackSupp
 	SkipContextIf(func() bool {
 		// The graceful termination feature depends on enabling an alpha feature
 		// EndpointSliceTerminatingCondition in Kubernetes.
-		return helpers.SkipK8sVersions("<1.20.0") || helpers.RunsOnGKE() || helpers.RunsOnEKS()
+		return helpers.SkipK8sVersions("<1.20.0") || helpers.RunsOnGKE() || helpers.RunsOnEKS() ||
+			helpers.RunsWithoutKubeProxy()
 	}, "Checks graceful termination of service endpoints", func() {
 		const (
 			clientPodLabel = "app=graceful-term-client"
