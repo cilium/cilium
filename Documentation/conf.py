cilium/cilium

commit 1802445639e80786525db19d65d10dc85cc07b3f

docs: prevent search engines from indexing old branches
To avoid users find on old results on search engines, this commit adds
the required configuration for that to happen.

Based on https://stackoverflow.com/questions/63542354/readthedocs-robots-txt-and-sitemap-xml

Signed-off-by: André Martins <andre@cilium.io>

Documentation/conf.py
@@ -147,6 +147,9 @@ language = None
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Add static html files
+html_extra_path = ["_html"]
+
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 

Documentation/_html/sitemap-index.html
@@ -1,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">
+    <url>
+        <loc>https://docs.cilium.io/en/stable/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1</priority>
+    </url>
+    <url>
+        <loc>https://docs.cilium.io/en/latest/</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.9</priority>
+    </url>
+</urlset>

commit da6a018a2de7c05ba477151eb02aff710d6305ac

docs: set robots.txt in the right directory
Accordingly the official documentation [1], the robots.txt should be set
in the root documentation. This commit will attempt that proposal.

[1] https://docs.readthedocs.io/en/latest/faq.html#how-can-i-avoid-search-results-having-a-deprecated-version-of-my-docs

Fixes: 1802445639e8 ("docs: prevent search engines from indexing old branches")
Signed-off-by: André Martins <andre@cilium.io>

Documentation/conf.py
@@ -147,9 +147,6 @@ language = None
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# Add static html files
-html_extra_path = ["_html"]
-
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 