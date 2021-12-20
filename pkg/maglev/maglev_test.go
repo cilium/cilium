7b3d1fa03de1488ea869a45e3601d498f057ae38
Ondrej Blazek <ondrej.blazek@firma.seznam.cz>
2021-12-20 07:24:45 +0100

    test: add unit tests for maglev weighted backends
    Signed-off-by: Ondrej Blazek <ondrej.blazek@firma.seznam.cz>

--- pkg/maglev/maglev_test.go
+++ pkg/maglev/maglev_test.go
@@ -13,6 +13,7 @@ import (
 	. "gopkg.in/check.v1"
 
 	"github.com/cilium/cilium/pkg/checker"
+	"github.com/cilium/cilium/pkg/loadbalancer"
 )
 
 func Test(t *testing.T) { TestingT(t) }
@@ -56,12 +57,18 @@ func (s *MaglevTestSuite) TestPermutations(c *C) {
 
 func (s *MaglevTestSuite) TestBackendRemoval(c *C) {
 	m := uint64(1021) // 3 (backends) * 100 should be less than M
-	backends := []string{"one", "two", "three"}
 	changesInExistingBackends := 0
 
-	before := GetLookupTable(backends, m)
-	// Remove backend "three"
-	after := GetLookupTable(backends[:len(backends)-1], m)
+	backendsMap := map[string]loadbalancer.Backend{
+		"one":   {Weight: 1, ID: 0},
+		"three": {Weight: 1, ID: 1},
+		"two":   {Weight: 1, ID: 2},
+	}
+	before := GetLookupTable(backendsMap, m)
+
+	// Remove backend "two"
+	delete(backendsMap, "two")
+	after := GetLookupTable(backendsMap, m)
 
 	for pos, backend := range before {
 		if (backend == 0 || backend == 1) && after[pos] != before[pos] {
@@ -77,6 +84,52 @@ func (s *MaglevTestSuite) TestBackendRemoval(c *C) {
 	c.Assert(float64(changesInExistingBackends)/float64(m)*float64(100) < 1.0, Equals, true)
 }
 
+func (s *MaglevTestSuite) TestWeightedBackendWithRemoval(c *C) {
+	m := uint64(1021) // 4 (backends) * 100 is still less than M
+	changesInExistingBackends := 0
+
+	// using following formula we can get the approximate number of times
+	// the backendID is found in the computed lut
+	// m / len(weightSum) * backend.Weight
+	backendsMap := map[string]loadbalancer.Backend{
+		"one":   {Weight: 2, ID: 0},   // approx. 15x times
+		"three": {Weight: 13, ID: 1},  // approx. 97.5x times
+		"two":   {Weight: 111, ID: 2}, // approx. 833x times
+		"tzwe":  {Weight: 10, ID: 3},  // approx. 75x times
+	}
+
+	backendsCounter := make(map[int]uint64, len(backendsMap))
+
+	before := GetLookupTable(backendsMap, m)
+
+	// Remove the backend "one"
+	delete(backendsMap, "one")
+	after := GetLookupTable(backendsMap, m)
+
+	for pos, backend := range before {
+		// count how many times backend position changed, take into consideration
+		// that IDs are decreased by 1 in the "after" lut
+		if (backend == 1 || backend == 2 || backend == 3) && after[pos] != before[pos] {
+			changesInExistingBackends++
+		} else {
+			// Check that there is no ID 0 as backend "one" with ID 0 has been removed
+			c.Assert(after[pos] == 1 || after[pos] == 2 || after[pos] == 3, Equals, true)
+		}
+		backendsCounter[backend]++
+	}
+
+	// Check that count of changes of existing backends is less than
+	// 1% (should be guaranteed by |backends| * 100 < M)
+	c.Assert(float64(changesInExistingBackends)/float64(m)*float64(100) < 1.0, Equals, true)
+
+	// Check that each backend is present x times using following formula:
+	// m / len(weightSum) * backend.Weight; e.g. 1021 / (2+13+111+10) * 13 = 97.6 => 98
+	c.Assert(backendsCounter[0] == 16, Equals, true)
+	c.Assert(backendsCounter[1] == 98, Equals, true)
+	c.Assert(backendsCounter[2] == 832, Equals, true)
+	c.Assert(backendsCounter[3] == 75, Equals, true)
+}
+
 func (s *MaglevTestSuite) BenchmarkGetMaglevTable(c *C) {
 	backendCount := 1000
 	m := uint64(131071)
@@ -85,9 +138,9 @@ func (s *MaglevTestSuite) BenchmarkGetMaglevTable(c *C) {
 		c.Fatal(err)
 	}
 
-	backends := make([]string, 0, backendCount)
+	backends := make(map[string]loadbalancer.Backend, backendCount)
 	for i := 0; i < backendCount; i++ {
-		backends = append(backends, fmt.Sprintf("backend-%d", i))
+		backends[fmt.Sprintf("backend-%d", i)] = loadbalancer.Backend{Weight: 1}
 	}
 
 	c.ResetTimer()
