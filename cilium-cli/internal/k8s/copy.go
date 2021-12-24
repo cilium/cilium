20c6f881b47c35c670b5322a9a31a37422bb6aa1
Tam Mach <sayboras@yahoo.com>
2021-12-24 18:05:48 +1100

    Add retry mechanism while copying files
    [ cherry-picked from cilium/cilium-cli repository ]
    
    This commit is to add CopyFromPod function in current k8s client, which
    supports resuming copy from last byte offset. Small refactor for exec in pod
    method was done to avoid copying reader multiple times.
    
    Relates to https://github.com/cilium/cilium-cli/issues/616#issuecomment-990747365
    
    Signed-off-by: Tam Mach <sayboras@yahoo.com>

---
+++ cilium-cli/internal/k8s/copy.go
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 Authors of Cilium
+
+package k8s
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	defaultReadFromByteCmd = "tail -c+%d %s"
+	defaultMaxTries        = 5
+)
+
+// CopyFromPod is to copy srcFile in a given pod to local destFile with defaultMaxTries.
+func (c *Client) CopyFromPod(ctx context.Context, namespace, pod, container string, srcFile, destFile string) error {
+	pipe := newPipe(&CopyOptions{
+		MaxTries: defaultMaxTries,
+		ReadFunc: readFromPod(ctx, c, namespace, pod, container, srcFile),
+	})
+
+	outFile, err := os.Create(destFile)
+	if err != nil {
+		return err
+	}
+
+	if _, err = io.Copy(outFile, pipe); err != nil {
+		return err
+	}
+	return nil
+}
+
+func readFromPod(ctx context.Context, client *Client, namespace, pod, container, srcFile string) ReadFunc {
+	return func(offset uint64, writer io.Writer) error {
+		command := []string{"sh", "-c", fmt.Sprintf(defaultReadFromByteCmd, offset, srcFile)}
+		return client.execInPodWithWriters(ctx, ExecParameters{
+			Namespace: namespace,
+			Pod:       pod,
+			Container: container,
+			Command:   command,
+		}, writer, writer)
+	}
+}
