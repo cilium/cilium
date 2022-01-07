91073d174c8a30954e4947ba0548b7e962942e11
tennix <ztennix@gmail.com>
2022-01-07 14:47:27 +0800

    Copy r2d2 parser as tidb parser

--- proxylib/proxylib.go
+++ proxylib/proxylib.go
@@ -21,6 +21,7 @@ import (
 	. "github.com/cilium/cilium/proxylib/proxylib"
 	_ "github.com/cilium/cilium/proxylib/r2d2"
 	_ "github.com/cilium/cilium/proxylib/testparsers"
+	_ "github.com/cilium/cilium/proxylib/tidb"
 )
 
 var (
