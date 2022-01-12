c5f6865a5c86bfbaa2d78cb2fa8ffc37bd2b300a
Tam Mach <sayboras@yahoo.com>
2022-01-12 23:37:25 +1100

    cli/metrics: Sort label in metrics list command
    As map structure in golang is not tree map, the ordering is not really
    deterministic. This makes the output of `cilium metrics list` hard to
    read, especially for the one having a lot of labels. This commit is to
    make sure that the labels are sorted in output.
    
    Signed-off-by: Tam Mach <sayboras@yahoo.com>

--- cilium/cmd/metrics_list.go
+++ cilium/cmd/metrics_list.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 	"strings"
 	"text/tabwriter"
 
@@ -54,8 +55,13 @@ var MetricsListCmd = &cobra.Command{
 			label := ""
 			if len(metric.Labels) > 0 {
 				labelArray := []string{}
-				for key, value := range metric.Labels {
-					labelArray = append(labelArray, fmt.Sprintf(`%s="%s"`, key, value))
+				keys := make([]string, 0, len(metric.Labels))
+				for k := range metric.Labels {
+					keys = append(keys, k)
+				}
+				sort.Strings(keys)
+				for _, k := range keys {
+					labelArray = append(labelArray, fmt.Sprintf(`%s="%s"`, k, metric.Labels[k]))
 				}
 				label = strings.Join(labelArray, " ")
 			}
