a825cc7a9aea175cd0243b38c97bba2469121b96
Joe Stringer <joe@cilium.io>
2022-01-25 12:26:33 -0800

    hubble/parser: Simplify unit test
    This unit test previously relied on fetching the loopback device ifindex
    on the node where it's run, but this is no longer necessary now that
    link name fetching is abstracted by the previous commits.
    
    Suggested-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/hubble/parser/threefour/parser_test.go
+++ pkg/hubble/parser/threefour/parser_test.go
@@ -23,7 +23,6 @@ import (
 	flowpb "github.com/cilium/cilium/api/v1/flow"
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/byteorder"
-	"github.com/cilium/cilium/pkg/datapath/link"
 	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
 	"github.com/cilium/cilium/pkg/hubble/parser/errors"
 	"github.com/cilium/cilium/pkg/hubble/testutils"
@@ -950,15 +949,11 @@ func TestDebugCapture(t *testing.T) {
 	parser, err := New(log, &testutils.NoopEndpointGetter, &testutils.NoopIdentityGetter, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
-	// Obtaining the index for the loopback device might not succeed depending
-	// on the system on which the test is running, therefore we just skip that
-	// part of the test with a warning if it fails.
+	// The testutils.NoopLinkGetter above will mock out the device name
+	// lookup to always return 'lo', so we can just hardcode it here and
+	// check that the events below get decoded with this link name.
 	loIfName := "lo"
-	loIfIndex, err := link.GetIfIndex(loIfName)
-	if err != nil {
-		t.Logf("warning: failed to get ifIndex for %q: %s", loIfName, err)
-		loIfIndex = 0
-	}
+	loIfIndex := uint32(1)
 
 	dbg := monitor.DebugCapture{
 		Type:    api.MessageTypeCapture,
@@ -989,13 +984,10 @@ func TestDebugCapture(t *testing.T) {
 	assert.Equal(t, ip.DstIP.String(), f.IP.Destination)
 	assert.NotNil(t, f.L4.GetTCP())
 
-	// Only checked if loIfIndex was populated by the test code above
-	if loIfIndex != 0 {
-		assert.Equal(t, &flowpb.NetworkInterface{
-			Index: loIfIndex,
-			Name:  loIfName,
-		}, f.Interface)
-	}
+	assert.Equal(t, &flowpb.NetworkInterface{
+		Index: loIfIndex,
+		Name:  loIfName,
+	}, f.Interface)
 
 	dbg = monitor.DebugCapture{
 		Type:    api.MessageTypeCapture,
