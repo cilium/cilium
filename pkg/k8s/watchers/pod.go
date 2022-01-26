cilium/cilium

commit 0ab4fa184d3a1a1bf1e7f916bcb5c6e10cf69bb5

pkg/k8s: ignore certain ipcache errors
Certain errors can be ignored when upserting IP addresses into the
ipcache. Thus, there is need to populate the logs with some red herring
warnings.

Fixes: da35c88eb9f8 ("k8s/watchers: don't silently ignore (*K8sWatcher).updatePodHostData error")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/pod.go
@@ -755,7 +755,21 @@ func (k *K8sWatcher) updatePodHostData(oldPod, newPod *slim_corev1.Pod, oldPodIP
 			namedPortsChanged = true
 		}
 		if err != nil {
-			errs = append(errs, fmt.Sprintf("ipcache entry for podIP %s owned by kvstore or agent: %s", podIP, err))
+			// It is expected to receive error overwrites where the existing
+			// source is the KVStore, this can happen as KVStore event
+			// propagation can usually be faster than k8s event propagation.
+			// It is also expected to receive an error overwrite where the
+			// existing source is local since cilium-agent receives events for
+			// local pods.
+			if !errors.Is(err, &ipcache.ErrOverwrite{
+				ExistingSrc: source.KVStore,
+				NewSrc:      source.Kubernetes,
+			}) || !errors.Is(err, &ipcache.ErrOverwrite{
+				ExistingSrc: source.Local,
+				NewSrc:      source.Kubernetes,
+			}) {
+				errs = append(errs, fmt.Sprintf("ipcache entry for podIP %s: %s", podIP, err))
+			}
 		}
 	}
 	if len(errs) != 0 {

daemon/cmd/status_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2021 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

pkg/ipcache/ipcache_test.go
@@ -17,6 +17,7 @@
 package ipcache
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"sort"
@@ -70,7 +71,7 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 		ID:     identity,
 		Source: source.Kubernetes,
 	})
-	c.Assert(err, Not(IsNil))
+	c.Assert(errors.Is(err, &ErrOverwrite{NewSrc: source.Kubernetes}), Equals, true)
 
 	IPIdentityCache.Upsert(endpointIP, nil, 0, nil, Identity{
 		ID:     identity,

pkg/k8s/watchers/cilium_endpoint.go
@@ -194,7 +194,7 @@ func (k *K8sWatcher) endpointUpdated(oldEndpoint, endpoint *types.CiliumEndpoint
 	for _, pair := range endpoint.Networking.Addressing {
 		if pair.IPV4 != "" {
 			ipsAdded = append(ipsAdded, pair.IPV4)
-				portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV4, nodeIP, encryptionKey, k8sMeta,
+			portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV4, nodeIP, encryptionKey, k8sMeta,
 				ipcache.Identity{ID: id, Source: source.CustomResource})
 			if portsChanged {
 				namedPortsChanged = true
@@ -203,7 +203,7 @@ func (k *K8sWatcher) endpointUpdated(oldEndpoint, endpoint *types.CiliumEndpoint
 
 		if pair.IPV6 != "" {
 			ipsAdded = append(ipsAdded, pair.IPV6)
-				portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV6, nodeIP, encryptionKey, k8sMeta,
+			portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV6, nodeIP, encryptionKey, k8sMeta,
 				ipcache.Identity{ID: id, Source: source.CustomResource})
 			if portsChanged {
 				namedPortsChanged = true

commit adc8ebdf0797192bdb5e9a04823900c0534ade2b

k8s/watchers: Fix local redirect policies selecting host networked pods
Commits da35c88eb9f8 and 0ab4fa184d3a introduced a regression for
local-redirect use cases like kiam, whereby host networked pod
updates were skipped. As a result, node-local redirection for
cases where LRPs select host networked pods as backends broke.

Tested the fix on an EKS configured with kiam setup.

Fixes: da35c88eb9f8 ("k8s/watchers: don't silently ignore (*K8sWatcher).updatePodHostData error")
Fixes: 0ab4fa184d3a (pkg/k8s: ignore certain ipcache errors)
Fixes: #16920

Signed-off-by: Aditi Ghag <aditi@cilium.io>

pkg/k8s/watchers/pod.go
@@ -210,8 +210,8 @@ func (k *K8sWatcher) addK8sPodV1(pod *slim_corev1.Pod) error {
 		return k.deleteK8sPodV1(pod)
 	}
 
-	if pod.Spec.HostNetwork {
-		logger.Debug("Pod is using host networking")
+	if pod.Spec.HostNetwork && !option.Config.EnableLocalRedirectPolicy {
+		logger.Debug("Skip pod event using host networking")
 		return nil
 	}
 
@@ -259,8 +259,8 @@ func (k *K8sWatcher) updateK8sPodV1(oldK8sPod, newK8sPod *slim_corev1.Pod) error
 		return k.deleteK8sPodV1(newK8sPod)
 	}
 
-	if newK8sPod.Spec.HostNetwork {
-		logger.Debug("Pod is using host networking")
+	if newK8sPod.Spec.HostNetwork && !option.Config.EnableLocalRedirectPolicy {
+		logger.Debug("Skip pod event using host networking")
 		return nil
 	}
 
@@ -665,6 +665,15 @@ func (k *K8sWatcher) deleteHostPortMapping(pod *slim_corev1.Pod, podIPs []string
 }
 
 func (k *K8sWatcher) updatePodHostData(oldPod, newPod *slim_corev1.Pod, oldPodIPs, newPodIPs k8sTypes.IPSlice) error {
+	if newPod.Spec.HostNetwork {
+		logger := log.WithFields(logrus.Fields{
+			logfields.K8sPodName:   newPod.ObjectMeta.Name,
+			logfields.K8sNamespace: newPod.ObjectMeta.Namespace,
+		})
+		logger.Debug("Pod is using host networking")
+		return nil
+	}
+
 	var namedPortsChanged bool
 
 	ipSliceEqual := oldPodIPs != nil && oldPodIPs.DeepEqual(&newPodIPs)