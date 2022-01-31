0d44ec758a57295b7b0b95a6ac5448d1e737c44c
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2022-02-01 00:35:14 +0900

    ipsec: Trace ESP packets coming from the network as encrypted
    [ upstream commit cdda826fffb167e538cad01334c2e83035b31ae4 ]
    
    When IPSec is enabled, it is helpful to trace ESP packets as encrypted.
    Fix the current program to do that. In non-IPSec mode, it won't happen
    because we are not interested in the encrypted packets. Please see the
    comment as well for more details.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- bpf/bpf_network.c
+++ bpf/bpf_network.c
@@ -14,19 +14,10 @@
 __section("from-network")
 int from_network(struct __ctx_buff *ctx)
 {
+	int ret;
 #ifdef ENABLE_IPSEC
 	__u16 proto;
-
-	if ((ctx->mark & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_DECRYPT) {
-		send_trace_notify(ctx, TRACE_FROM_NETWORK, get_identity(ctx), 0, 0,
-				  ctx->ingress_ifindex,
-				  TRACE_REASON_ENCRYPTED, TRACE_PAYLOAD_LEN);
-	} else
 #endif
-	{
-		send_trace_notify(ctx, TRACE_FROM_NETWORK, 0, 0, 0,
-				  ctx->ingress_ifindex, 0, TRACE_PAYLOAD_LEN);
-	}
 
 	bpf_clear_meta(ctx);
 
@@ -35,11 +26,46 @@ int from_network(struct __ctx_buff *ctx)
 	if (!validate_ethertype(ctx, &proto))
 		return CTX_ACT_OK;
 
-	return do_decrypt(ctx, proto);
+	ret = do_decrypt(ctx, proto);
 #else
 	/* nop if IPSec is disabled */
-	return CTX_ACT_OK;
+	ret = CTX_ACT_OK;
 #endif
+
+/* We need to handle following possible packets come to this program
+ *
+ * 1. ESP packets coming from network (encrypted and not marked)
+ * 2. Non-ESP packets coming from network (plain and not marked)
+ * 3. Non-ESP packets coming from stack re-inserted by xfrm (plain
+ *    and marked with MARK_MAGIC_DECRYPT, IPSec mode only)
+ *
+ * 1. will be traced with TRACE_REASON_ENCRYPTED, because
+ * do_decrypt marks them with MARK_MAGIC_DECRYPT.
+ *
+ * 2. will be traced without TRACE_REASON_ENCRYPTED, because
+ * do_decrypt does't touch to mark.
+ *
+ * 3. will be traced without TRACE_REASON_ENCRYPTED, because
+ * do_decrypt clears the mark.
+ *
+ * Note that 1. contains the ESP packets someone else generated.
+ * In that case, we trace it as "encrypted", but it doesn't mean
+ * "encrypted by Cilium".
+ *
+ * We won't use TRACE_REASON_ENCRYPTED even if the packets are ESP,
+ * because it doesn't matter for the non-IPSec mode.
+ */
+#ifdef ENABLE_IPSEC
+	if ((ctx->mark & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_DECRYPT)
+		send_trace_notify(ctx, TRACE_FROM_NETWORK, get_identity(ctx), 0, 0,
+				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED,
+				  TRACE_PAYLOAD_LEN);
+	else
+#endif
+		send_trace_notify(ctx, TRACE_FROM_NETWORK, 0, 0, 0,
+				  ctx->ingress_ifindex, 0, TRACE_PAYLOAD_LEN);
+
+	return ret;
 }
 
 BPF_LICENSE("GPL");
