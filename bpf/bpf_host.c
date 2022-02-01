1f1231cdfda57cf9612dedf3d99354cd0e51aad2
Martynas Pumputis <m@lambda.lt>
2022-02-01 16:03:17 +0100

    WIP: host2pod works, but KPR still not
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -1110,7 +1110,8 @@ out:
 			return DROP_INVALID;
 
 		info = lookup_ip4_remote_endpoint(ip4->daddr);
-		if (((ctx->mark & 0x4d2) != 0x4d2) && info != NULL && info->sec_label == REMOTE_NODE_ID) {
+		if (((ctx->mark & 0x4d2) != 0x4d2) && info != NULL &&
+		    (identity_is_remote_node(info->sec_label) || (info->tunnel_endpoint != 0))) {
 			return ctx_redirect(ctx, WG_IFINDEX, 0);
 		}
 	}
