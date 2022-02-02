42421315be7d696c5aacc6332b582d9df7e1bc7b
Robin Hahling <robin.hahling@gw-computing.net>
2022-02-02 11:06:12 +0100

    images/builder: add protoc-gen-doc plugin
    The protoc-gen-doc plugin allows to generate documentation from proto
    files as Markdown, HTML, DocBook and JSON.
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -12,3 +12,4 @@ set -o nounset
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@c285fc70e095eccc98d79b9a133e1e328141aefd
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.0
 go install github.com/mitchellh/protoc-gen-go-json@v1.1.0
+go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
