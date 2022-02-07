cilium/cilium

commit 560c85fa550c5cedb18897577d3a08063f83516f

daemon: add WaitUntilK8sCacheIsSynced method
which will block the caller until the agent has fully sync its
k8s cache.

Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

daemon/cmd/daemon.go
@@ -1044,3 +1044,9 @@ func (d *Daemon) K8sCacheIsSynced() bool {
 		return false
 	}
 }
+
+// WaitUntilK8sCacheIsSynced waits until the agent has fully synced its k8s cache with the API
+// server
+func (d *Daemon) WaitUntilK8sCacheIsSynced() {
+	_, _ = <-d.k8sCachesSynced
+}

commit c93f4b181a8a7c0c521073f24cce1ee5a1bc2f99

daemon: Removed unused method
There are no callers of the method `WaitUntilK8sCacheIsSynced`.

Fixes: 560c85fa550c5cedb18897577d3a08063f83516f
Signed-off-by: Aditi Ghag <aditi@cilium.io>

daemon/cmd/daemon.go
@@ -1106,9 +1106,3 @@ func (d *Daemon) K8sCacheIsSynced() bool {
 		return false
 	}
 }
-
-// WaitUntilK8sCacheIsSynced waits until the agent has fully synced its k8s cache with the API
-// server
-func (d *Daemon) WaitUntilK8sCacheIsSynced() {
-	_, _ = <-d.k8sCachesSynced
-}