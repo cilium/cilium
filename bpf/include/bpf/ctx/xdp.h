cilium/cilium

commit 206430c4f37683feff81f9d7cffe5205073fe138

bpf: Encode ports on 16 bits in DSR IPv6 options
We use the IPv6 option to pass information for DSR between nodes. The L4
port in there is encoded on 32 bits, which doesn't match the expectation
of the rest of the datapath (16 bits as usual) and leads to implicit int
casts. This commit changes the structure to encode the port on 16 bits.

Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/include/bpf/ctx/xdp.h
@@ -235,7 +235,7 @@ ctx_adjust_hroom(struct xdp_md *ctx, const __s32 len_diff, const __u32 mode,
 		case 48: /* struct {ipv6hdr + icmp6hdr} */
 			break;
 		case 40: /* struct ipv6hdr */
-		case 24: /* struct dsr_opt_v6 */
+		case 22: /* struct dsr_opt_v6 */
 			if (data + move_len_v6 + len_diff <= data_end)
 				__bpf_memmove_fwd(data, data + len_diff,
 						  move_len_v6);

bpf/lib/nodeport.h
@@ -86,7 +86,7 @@ struct dsr_opt_v6 {
 	__u8 opt_type;
 	__u8 opt_len;
 	union v6addr addr;
-	__be32 port;
+	__be16 port;
 };
 #endif /* ENABLE_IPV6 */
 

bpf/lib/common.h
@@ -536,8 +536,8 @@ enum metric_dir {
  * [1]:  https://www.iana.org/assignments/ipv6-parameters/ipv6-parameters.xhtml#ipv6-parameters-2
  */
 #define DSR_IPV6_OPT_TYPE	0x1B
-#define DSR_IPV6_OPT_LEN	0x14	/* to store ipv6 addr + port */
-#define DSR_IPV6_EXT_LEN	0x2	/* = (sizeof(dsr_opt_v6) - 8) / 8 */
+#define DSR_IPV6_OPT_LEN	(sizeof(struct dsr_opt_v6) - 4)
+#define DSR_IPV6_EXT_LEN	((sizeof(struct dsr_opt_v6) - 8) / 8)
 
 /* We cap key index at 4 bits because mark value is used to map ctx to key */
 #define MAX_KEY_INDEX 15

commit efaf6c7a242821527569ce8869d7756f752450cc

datapath: Fix IPv6 extension hdr for DSR
The commit 206430c4 changed the port field size from 32 to 16 in the
dsr_opt_v6 struct. The struct is used to encode a service metadata in a
form of the IPv6 header extension used by the DSR.

This broke the requirement for the IPv6 header extensions to be 8-bytes
aligned. This was spotted with tcpdump reporting the following:

16:06:40.113481 IP6 fd04::13 > fd02::16f: DSTOPT 0 > 17: Flags [FRPW]
   [bad hdr length 48 - too long, > 46]

Fixes: 206430c4 ("bpf: Encode ports on 16 bits in DSR IPv6 options")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/include/bpf/ctx/xdp.h
@@ -235,7 +235,7 @@ ctx_adjust_hroom(struct xdp_md *ctx, const __s32 len_diff, const __u32 mode,
 		case 48: /* struct {ipv6hdr + icmp6hdr} */
 			break;
 		case 40: /* struct ipv6hdr */
-		case 22: /* struct dsr_opt_v6 */
+		case 24: /* struct dsr_opt_v6 */
 			if (data + move_len_v6 + len_diff <= data_end)
 				__bpf_memmove_fwd(data, data + len_diff,
 						  move_len_v6);

bpf/lib/nodeport.h
@@ -87,6 +87,7 @@ struct dsr_opt_v6 {
 	__u8 opt_len;
 	union v6addr addr;
 	__be16 port;
+	__u16 pad;
 };
 #endif /* ENABLE_IPV6 */
 
@@ -310,6 +311,9 @@ static __always_inline int dsr_set_ext6(struct __ctx_buff *ctx,
 {
 	struct dsr_opt_v6 opt __align_stack_8 = {};
 	__u16 payload_len = bpf_ntohs(ip6->payload_len) + sizeof(opt);
+
+	/* The IPv6 extension should be 8-bytes aligned */
+	build_bug_on((sizeof(struct dsr_opt_v6) % 8) != 0);
 
 	if (dsr_is_too_big(ctx, payload_len)) {
 		*ohead = sizeof(opt);