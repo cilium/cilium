bbe176fc5c950d734d119bd4b1ef13170932ec07
Jarno Rajahalme <jarno@isovalent.com>
2022-02-11 14:06:10 +0200

    accesslog: Pass destination identity from L7 proxies to access logs
    Pass destination security identity, as used at the time of policy
    enforcement, from L7 proxies to access logs. This can be 0 if not
    resolved by the proxy (e.g., by an ingress proxy), but in those cases the
    logger now resolves the destination IP to the corresponding security
    identity even when the destination is not a local endpoint.
    
    Unify EndpointInfoRegistry interface calls FillEndpointIdentityByID() and
    FillEndpointIdentityByIP() into a new FillEndpointInfo() that takes both
    IP and numeric identity as parameters. This is used for both source and
    destination endpoint info in logger.Addressing() so that EndpointInfo no
    longer needs to be overwritten in notifyOnDNSMsg() after the Addressing()
    tag.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- pkg/envoy/accesslog_server.go
+++ pkg/envoy/accesslog_server.go
@@ -18,6 +18,7 @@ import (
 	"golang.org/x/sys/unix"
 
 	"github.com/cilium/cilium/pkg/flowdebug"
+	"github.com/cilium/cilium/pkg/identity"
 	kafka_api "github.com/cilium/cilium/pkg/policy/api/kafka"
 	"github.com/cilium/cilium/pkg/proxy/accesslog"
 	"github.com/cilium/cilium/pkg/proxy/logger"
@@ -169,7 +170,8 @@ func logRecord(endpointInfoRegistry logger.EndpointInfoRegistry, localEndpoint l
 		logger.LogTags.Addressing(logger.AddressingInfo{
 			SrcIPPort:   pblog.SourceAddress,
 			DstIPPort:   pblog.DestinationAddress,
-			SrcIdentity: pblog.SourceSecurityId,
+			SrcIdentity: identity.NumericIdentity(pblog.SourceSecurityId),
+			DstIdentity: identity.NumericIdentity(pblog.DestinationSecurityId),
 		}), l7tags)
 
 	r.Log()
