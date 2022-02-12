c8224c42a2bd06070b47553f0dc42b7e2deba1bd
Vincent Li <v.li@f5.com>
2022-02-12 19:19:47 +0000

    Refactor pkg/mac with type Uint64MAC as uint64 for MAC address
    lxcmap and future VTEP map implementation will shares common code
    for MAC address, this is to prepare pkg/mac for that to reduce code
    duplication
    
    Co-authored-by: Joe Stringer <joe@cilium.io>
    Signed-off-by: Joe Stringer <joe@cilium.io>
    Signed-off-by: Vincent Li <v.li@f5.com>

--- pkg/mac/mac_test.go
+++ pkg/mac/mac_test.go
@@ -28,7 +28,7 @@ func (s *MACSuite) TestUint64(c *C) {
 	m := MAC([]byte{0x11, 0x12, 0x23, 0x34, 0x45, 0x56})
 	v, err := m.Uint64()
 	c.Assert(err, IsNil)
-	c.Assert(v, Equals, uint64(0x564534231211))
+	c.Assert(v, Equals, Uint64MAC(0x564534231211))
 }
 
 func (s *MACSuite) TestUnmarshalJSON(c *C) {
