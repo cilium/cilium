33b6c976ef1c752ae48c4f3990d6f823af08f57f
Quentin Monnet <quentin@isovalent.com>
2022-02-15 14:35:17 +0000

    licensing: switch to SPDX tags for some more files
    Commit a8e3fa283cf4 ("licensing: spdx convert code base") dropped the
    license boilerplates in favour of SPDX tags for a large portion of the
    code base. However, some files were not updated. This happened for a
    variety of reasons:
    
    - Some were not yet committed (work-in-progress) at the time this commit
      was merged.
    - Some had an additional space between the comment marker and the
      beginning of the copyright/license mention, making them pass through
      the search pattern undetected.
    - Some may be in directories that were not processed at the time, or may
      have been skipped for some other reasons.
    
    This commit turns more files to the SPDX tags. However, it does not
    touch files for which the copyright does not go to the authors of
    Cilium, only.
    
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- contrib/scripts/netperf_reporter.py
+++ contrib/scripts/netperf_reporter.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python3
-
+# SPDX-License-Identifier: Apache-2.0
 # Copyright 2018 Authors of Cilium
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# ----------------------------------------------------------------------
-# This script read the output of netperf results from Kubernetes upstream
+# This script reads the output of netperf results from Kubernetes upstream
 # project and pushes the results to the given Prometheus server.
 # Needed variables to run this script:
 # - PROMETHEUS_URL: metrics gateway URL, example:
