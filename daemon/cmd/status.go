79cc4f635e072228e689792bf9e747cde0232cdd
Sebastian Wicki <sebastian@isovalent.com>
2022-02-22 13:57:33 +0100

    nodediscovery: Unify source of truth for node name
    [ upstream commit 9c1cb4856322a80174799dae142ef761e51e2310 ]
    
    Clustermesh related code is relying on
    `NodeDiscovery.LocalNode.FullName()` to obtain the globally unique name
    of the local node. This field is initialized from `nodeTypes.GetName()`.
    This code removes all uses of `LocalNode.FullName()` and replaces it
    with an equivalent call to `nodeTypes.GetAbsoluteNodeName()`. This
    allows us to make the `NodeDiscovery.LocalNode` private in a subsequent
    commit.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- daemon/cmd/status.go
+++ daemon/cmd/status.go
@@ -398,7 +398,7 @@ func NewGetHealthzHandler(d *Daemon) GetHealthzHandler {
 
 func (d *Daemon) getNodeStatus() *models.ClusterStatus {
 	clusterStatus := models.ClusterStatus{
-		Self: d.nodeDiscovery.LocalNode.Fullname(),
+		Self: nodeTypes.GetAbsoluteNodeName(),
 	}
 	for _, node := range d.nodeDiscovery.Manager.GetNodes() {
 		clusterStatus.Nodes = append(clusterStatus.Nodes, node.GetModel())
@@ -559,7 +559,7 @@ func (h *getNodes) Handle(params GetClusterNodesParams) middleware.Responder {
 			lastSync: time.Now(),
 			ClusterNodeStatus: &models.ClusterNodeStatus{
 				ClientID: clientID,
-				Self:     h.d.nodeDiscovery.LocalNode.Fullname(),
+				Self:     nodeTypes.GetAbsoluteNodeName(),
 			},
 		}
 		h.d.nodeDiscovery.Manager.Subscribe(c)
@@ -575,7 +575,7 @@ func (h *getNodes) Handle(params GetClusterNodesParams) middleware.Responder {
 	// added / removed.
 	c.ClusterNodeStatus = &models.ClusterNodeStatus{
 		ClientID: clientID,
-		Self:     h.d.nodeDiscovery.LocalNode.Fullname(),
+		Self:     nodeTypes.GetAbsoluteNodeName(),
 	}
 	c.lastSync = time.Now()
 	c.Unlock()
@@ -809,7 +809,7 @@ func (d *Daemon) startStatusCollector() {
 			Name: "cluster",
 			Probe: func(ctx context.Context) (interface{}, error) {
 				clusterStatus := &models.ClusterStatus{
-					Self: d.nodeDiscovery.LocalNode.Fullname(),
+					Self: nodeTypes.GetAbsoluteNodeName(),
 				}
 				return clusterStatus, nil
 			},
