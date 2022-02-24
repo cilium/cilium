04bd53c19f467221e8bf4cde2bcdcff8456cda69
Tobias Klauser <tobias@cilium.io>
2022-02-24 09:09:17 +0100

    Update default Cilium version to v1.11.2
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/defaults/defaults.go
+++ cilium-cli/defaults/defaults.go
@@ -69,7 +69,7 @@ const (
 	ConnectivityCheckJSONMockImage   = "quay.io/cilium/json-mock:v1.3.0@sha256:2729064827fa9dbfface8d3df424feb6c792a0ba07117b844349635c93c06d2b"
 
 	ConfigMapName   = "cilium-config"
-	Version         = "v1.11.1"
+	Version         = "v1.11.2"
 	HubbleUIVersion = "v0.8.5"
 
 	TunnelType = "vxlan"
