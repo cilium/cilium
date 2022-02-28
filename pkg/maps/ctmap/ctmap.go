cilium/cilium

commit c9810bf7b235ee371279c85f025535a86b1ea675

ctmap: GC orphan SNAT entries
This commit adds a mechanism to remove orphan SNAT entries.

We call an SNAT entry orphan if it does not have either a corresponding
CT entry or an SNAT entry in a reverse order. Both cases can happen due
to LRU eviction heuristics (both CT and NAT maps are of the LRU type).

The mechanism for the removal is based on the GC signaling in the
datapath. When the datapath SNAT routine fails to find a free mapping
after SNAT_SIGNAL_THRES attempts, it sends the signal via the perf ring
buffer. The consumer of the buffer is the daemon. After receiving the
signal it invokes the CT GC.

The newly implemented GC addition iterates over all SNAT entries and
checks whether a corresponding CT entry is found, and if not, it tries
to remove both SNAT entries (for original and reverse flows).

For now, I didn't add GC of orphan SNAT entries created by DSR to keep
complexity of changes as low as possible. This will come as a follow up.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/maps/ctmap/ctmap.go
@@ -16,6 +16,7 @@ package ctmap
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"math"
@@ -34,8 +35,10 @@ import (
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/tuple"
+	"github.com/cilium/cilium/pkg/u8proto"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 )
 
 var (
@@ -103,6 +106,10 @@ type NatMap interface {
 	Open() error
 	Close() error
 	DeleteMapping(key tuple.TupleKey) error
+	DumpWithCallback(bpf.DumpCallback) error
+	DumpReliablyWithCallback(bpf.DumpCallback, *bpf.DumpStats) error
+	Delete(bpf.MapKey) error
+	DumpStats() *bpf.DumpStats
 }
 
 type mapAttributes struct {
@@ -508,6 +515,83 @@ func GC(m *Map, filter *GCFilter) int {
 	}
 
 	return doGC(m, filter)
+}
+
+// PurgeOrphanNATEntries removes orphan SNAT entries. We call an SNAT entry
+// orphan if it does not have a corresponding CT entry.
+//
+// This can happen when the CT entry is removed by the LRU eviction which
+// happens when the CT map becomes full.
+//
+// PurgeOrphanNATEntries() is triggered by the datapath via the GC signaling
+// mechanism. When the datapath SNAT fails to find free mapping after
+// SNAT_SIGNAL_THRES attempts, it sends the signal via the perf ring buffer.
+// The consumer of the buffer invokes the function.
+//
+// The SNAT is being used for the following cases:
+// 1. By NodePort BPF on an intermediate node before fwd'ing request from outside
+//     to a destination node.
+// 2. A packet from local endpoint sent to outside (BPF-masq).
+// 3. A packet from a host local application (i.e. running in the host netns)
+//    This is needed to prevent SNAT from hijacking such connections.
+// 4. By DSR on a backend node to SNAT responses with service IP+port before
+//    sending to a client.
+//
+// In the case of 1-3, we always create a CT_EGRESS CT entry. This allows the
+// CT GC to remove corresponding SNAT entries. In the case of 4, will create
+// CT_INGRESS CT entry. See the unit test TestOrphanNatGC for more examples.
+//
+// The function only handles 1-3 cases, the 4. case is TODO(brb).
+func PurgeOrphanNATEntries(ctMap *Map) *NatGCStats {
+	if option.Config.NodePortMode == option.NodePortModeDSR ||
+		option.Config.NodePortMode == option.NodePortModeHybrid {
+		return nil
+	}
+
+	natMap := mapInfo[ctMap.mapType].natMap
+	if natMap == nil {
+		return nil
+	}
+
+	isCTMapTCP := ctMap.mapType == mapTypeIPv4TCPLocal ||
+		ctMap.mapType == mapTypeIPv6TCPLocal ||
+		ctMap.mapType == mapTypeIPv4TCPGlobal ||
+		ctMap.mapType == mapTypeIPv6TCPGlobal
+	stats := newNatGCStats(natMap)
+
+	cb := func(key bpf.MapKey, value bpf.MapValue) {
+		natKey := key.(nat.NatKey)
+		natVal := value.(nat.NatEntry)
+
+		// In opposite to the CT maps, TCP and UDP entries are stored in the same
+		// SNAT map. Therefore, to avoid a case when the given ctMap does not
+		// store entries of the given natKey.NextHeader proto, we should return
+		// early. Otherwise, the natKey entries will be removed, which is wrong.
+		if (natKey.GetNextHeader() == u8proto.TCP) != isCTMapTCP {
+			return
+		}
+
+		if natKey.GetFlags()&tuple.TUPLE_F_IN == 1 { // natKey is r(everse)tuple
+			ctKey := egressCTKeyFromIngressNatKeyAndVal(natKey, natVal)
+			if _, err := ctMap.Lookup(ctKey); errors.Is(err, unix.ENOENT) {
+				// No CT entry is found, so delete SNAT for both original and
+				// reverse flows
+				oNatKey := oNatKeyFromReverse(natKey, natVal)
+				if err := natMap.Delete(oNatKey); err == nil {
+					stats.EgressDeleted += 1
+				}
+				if err := natMap.Delete(natKey); err == nil {
+					stats.IngressDeleted += 1
+				}
+			} else {
+				stats.IngressAlive += 1
+			}
+		}
+	}
+
+	natMap.DumpReliablyWithCallback(cb, stats.DumpStats)
+
+	return &stats
 }
 
 // Flush runs garbage collection for map m with the name mapType, deleting all

pkg/maps/ctmap/ctmap_privileged_test.go
@@ -38,6 +38,12 @@ func Test(t *testing.T) {
 	TestingT(t)
 }
 
+func (k *CTMapTestSuite) SetUpSuite(c *C) {
+	bpf.CheckOrMountFS("", false)
+	err := bpf.ConfigureResourceLimits()
+	c.Assert(err, IsNil)
+}
+
 func (k *CTMapTestSuite) Benchmark_MapUpdate(c *C) {
 	m := newMap(MapNameTCP4Global+"_test", mapTypeIPv4TCPGlobal)
 	_, err := m.OpenOrCreate()
@@ -98,13 +104,9 @@ func (k *CTMapTestSuite) Benchmark_MapUpdate(c *C) {
 // TestCtGcIcmp tests whether ICMP NAT entries are removed upon a removal of
 // their CT entry (GH#12625).
 func (k *CTMapTestSuite) TestCtGcIcmp(c *C) {
-	bpf.CheckOrMountFS("", false)
-	err := bpf.ConfigureResourceLimits()
-	c.Assert(err, IsNil)
-
 	// Init maps
 	natMap := nat.NewMap("cilium_nat_any4_test", true, 1000)
-	_, err = natMap.OpenOrCreate()
+	_, err := natMap.OpenOrCreate()
 	c.Assert(err, IsNil)
 	defer natMap.Map.Unpin()
 
@@ -206,6 +208,230 @@ func (k *CTMapTestSuite) TestCtGcIcmp(c *C) {
 	c.Assert(stats.aliveEntries, Equals, uint32(0))
 	c.Assert(stats.deleted, Equals, uint32(1))
 
+	buf = make(map[string][]string)
+	err = natMap.Map.Dump(buf)
+	c.Assert(err, IsNil)
+	c.Assert(len(buf), Equals, 0)
+}
+
+// TestOrphanNat checks whether dangling NAT entries are GC'd (GH#12686)
+func (k *CTMapTestSuite) TestOrphanNatGC(c *C) {
+	// Init maps
+	natMap := nat.NewMap("cilium_nat_any4_test", true, 1000)
+	_, err := natMap.OpenOrCreate()
+	c.Assert(err, IsNil)
+	defer natMap.Map.Unpin()
+
+	ctMapName := MapNameAny4Global + "_test"
+	setupMapInfo(mapTypeIPv4AnyGlobal, ctMapName,
+		&CtKey4Global{}, int(unsafe.Sizeof(CtKey4Global{})),
+		100, natMap)
+	ctMap := newMap(ctMapName, mapTypeIPv4AnyGlobal)
+	_, err = ctMap.OpenOrCreate()
+	c.Assert(err, IsNil)
+	defer ctMap.Map.Unpin()
+
+	ctTCPMapName := MapNameTCP4Global + "_test"
+	setupMapInfo(mapTypeIPv4TCPGlobal, ctTCPMapName,
+		&CtKey4Global{}, int(unsafe.Sizeof(CtKey4Global{})),
+		100, natMap)
+	ctTCPMap := newMap(ctTCPMapName, mapTypeIPv4TCPGlobal)
+	_, err = ctTCPMap.OpenOrCreate()
+	c.Assert(err, IsNil)
+	defer ctTCPMap.Map.Unpin()
+
+	// Create the following entries and check that SNAT entries are NOT GC-ed
+	// (as we have the CT entry which they belong to):
+	//
+	// - Host local traffic (no SNAT):
+	//		CT:		UDP OUT 10.23.32.45:54864 -> 10.23.53.48:8472
+	//		NAT:	UDP IN  10.23.53.48:8472 -> 10.23.32.45:54865 XLATE_DST 10.23.32.45:54864
+	//	 			UDP OUT 10.23.32.45:54864 -> 10.23.53.48:8472 XLATE_SRC 10.23.32.45:54865
+	//
+	// The example above covers other SNAT cases. E.g. (not used in unit tests below, just
+	// to show for completion):
+	//
+	// - NodePort request from outside (subject to NodePort SNAT):
+	// 		CT: 	TCP OUT 192.168.34.1:63000 -> 10.0.1.99:80
+	// 		NAT: 	TCP IN 10.0.1.99:80 -> 10.0.0.134:63000 XLATE_DST 192.168.34.1:63000
+	// 		NAT: 	TCP OUT 192.168.34.1:63000 -> 10.0.1.99:80 XLATE_SRC 10.0.0.134:63000
+	//
+	// - Local endpoint request to outside (subject to BPF-masq):
+	//		CT: 	TCP OUT 10.0.1.99:34520 -> 1.1.1.1:80
+	//		NAT: 	TCP IN 1.1.1.1:80 -> 10.0.2.15:34520 XLATE_DST 10.0.1.99:34520
+	//				TCP OUT 10.0.1.99:34520 -> 1.1.1.1:80 XLATE_SRC 10.0.2.15:34520
+
+	ctKey := &CtKey4Global{
+		tuple.TupleKey4Global{
+			tuple.TupleKey4{
+				DestAddr:   types.IPv4{10, 23, 32, 45},
+				SourceAddr: types.IPv4{10, 23, 53, 48},
+				SourcePort: 0x50d6,
+				DestPort:   0x1821,
+				NextHeader: u8proto.UDP,
+				Flags:      tuple.TUPLE_F_OUT,
+			},
+		},
+	}
+	ctVal := &CtEntry{
+		TxPackets: 1,
+		TxBytes:   216,
+		Lifetime:  37459,
+	}
+	err = bpf.UpdateElement(ctMap.Map.GetFd(), unsafe.Pointer(ctKey),
+		unsafe.Pointer(ctVal), 0)
+	c.Assert(err, IsNil)
+
+	natKey := &nat.NatKey4{
+		tuple.TupleKey4Global{
+			tuple.TupleKey4{
+				SourceAddr: types.IPv4{10, 23, 32, 45},
+				DestAddr:   types.IPv4{10, 23, 53, 48},
+				SourcePort: 0x50d6,
+				DestPort:   0x1821,
+				NextHeader: u8proto.UDP,
+				Flags:      tuple.TUPLE_F_OUT,
+			},
+		},
+	}
+	natVal := &nat.NatEntry4{
+		Created:   37400,
+		HostLocal: 1,
+		Addr:      types.IPv4{10, 23, 32, 45},
+		Port:      0x51d6,
+	}
+	err = bpf.UpdateElement(natMap.Map.GetFd(), unsafe.Pointer(natKey),
+		unsafe.Pointer(natVal), 0)
+	c.Assert(err, IsNil)
+	natKey = &nat.NatKey4{
+		tuple.TupleKey4Global{
+			tuple.TupleKey4{
+				DestAddr:   types.IPv4{10, 23, 32, 45},
+				SourceAddr: types.IPv4{10, 23, 53, 48},
+				DestPort:   0x51d6,
+				SourcePort: 0x1821,
+				NextHeader: u8proto.UDP,
+				Flags:      tuple.TUPLE_F_IN,
+			},
+		},
+	}
+	natVal = &nat.NatEntry4{
+		Created:   37400,
+		HostLocal: 1,
+		Addr:      types.IPv4{10, 23, 32, 45},
+		Port:      0x50d6,
+	}
+	err = bpf.UpdateElement(natMap.Map.GetFd(), unsafe.Pointer(natKey),
+		unsafe.Pointer(natVal), 0)
+	c.Assert(err, IsNil)
+
+	ctKeyTCP := &CtKey4Global{
+		tuple.TupleKey4Global{
+			tuple.TupleKey4{
+				DestAddr:   types.IPv4{10, 23, 32, 45},
+				SourceAddr: types.IPv4{10, 23, 53, 48},
+				SourcePort: 0x50d6,
+				DestPort:   0x1821,
+				NextHeader: u8proto.TCP,
+				Flags:      tuple.TUPLE_F_OUT,
+			},
+		},
+	}
+	err = bpf.UpdateElement(ctTCPMap.Map.GetFd(), unsafe.Pointer(ctKeyTCP),
+		unsafe.Pointer(ctVal), 0)
+	c.Assert(err, IsNil)
+
+	// UDP SNAT entries should not be removed when the TCP CT map is given
+	stats := PurgeOrphanNATEntries(ctTCPMap)
+	c.Assert(stats.IngressDeleted, Equals, uint32(0))
+	c.Assert(stats.EgressDeleted, Equals, uint32(0))
+
+	stats = PurgeOrphanNATEntries(ctMap)
+	c.Assert(stats.IngressAlive, Equals, uint32(1))
+	c.Assert(stats.IngressDeleted, Equals, uint32(0))
+	c.Assert(stats.EgressDeleted, Equals, uint32(0))
+	// Check that both entries haven't removed
+	buf := make(map[string][]string)
+	err = natMap.Map.Dump(buf)
+	c.Assert(err, IsNil)
+	c.Assert(len(buf), Equals, 2)
+
+	// Now remove the CT entry which should remove both NAT entries
+	err = bpf.DeleteElement(ctMap.Map.GetFd(), unsafe.Pointer(ctKey))
+	c.Assert(err, IsNil)
+	stats = PurgeOrphanNATEntries(ctMap)
+	c.Assert(stats.IngressDeleted, Equals, uint32(1))
+	c.Assert(stats.IngressAlive, Equals, uint32(0))
+	c.Assert(stats.EgressDeleted, Equals, uint32(1))
+	// Check that both orphan NAT entries have been removed
+	buf = make(map[string][]string)
+	err = natMap.Map.Dump(buf)
+	c.Assert(err, IsNil)
+	c.Assert(len(buf), Equals, 0)
+
+	// Create only CT_INGRESS NAT entry which should be removed
+	err = bpf.UpdateElement(natMap.Map.GetFd(), unsafe.Pointer(natKey),
+		unsafe.Pointer(natVal), 0)
+	c.Assert(err, IsNil)
+
+	stats = PurgeOrphanNATEntries(ctMap)
+	c.Assert(stats.IngressDeleted, Equals, uint32(1))
+	c.Assert(stats.EgressDeleted, Equals, uint32(0))
+	buf = make(map[string][]string)
+	err = natMap.Map.Dump(buf)
+	c.Assert(err, IsNil)
+	c.Assert(len(buf), Equals, 0)
+
+	// Let's check IPv6
+
+	natMapV6 := nat.NewMap("cilium_nat_any6_test", false, 1000)
+	_, err = natMapV6.OpenOrCreate()
+	c.Assert(err, IsNil)
+	defer natMapV6.Map.Unpin()
+
+	ctMapAnyName = MapNameAny6Global + "_test"
+	setupMapInfo(mapTypeIPv6AnyGlobal, ctMapAnyName,
+		&CtKey6Global{}, int(unsafe.Sizeof(CtKey6Global{})),
+		100, natMapV6)
+	ctMapAnyV6 := newMap(ctMapAnyName, mapTypeIPv6AnyGlobal)
+	_, err = ctMapAnyV6.OpenOrCreate()
+	c.Assert(err, IsNil)
+	defer ctMapAnyV6.Map.Unpin()
+
+	ctMapTCPName = MapNameTCP6Global + "_test"
+	setupMapInfo(mapTypeIPv6TCPGlobal, ctMapTCPName,
+		&CtKey6Global{}, int(unsafe.Sizeof(CtKey6Global{})),
+		100, natMapV6)
+	ctMapTCPV6 := newMap(ctMapTCPName, mapTypeIPv6TCPGlobal)
+	_, err = ctMapTCP.OpenOrCreate()
+	c.Assert(err, IsNil)
+	defer ctMapTCPV6.Map.Unpin()
+
+	natKeyV6 := &nat.NatKey6{
+		tuple.TupleKey6Global{
+			tuple.TupleKey6{
+				SourceAddr: types.IPv6{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				DestAddr:   types.IPv6{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+				SourcePort: 0x50d6,
+				DestPort:   0x1821,
+				NextHeader: u8proto.UDP,
+				Flags:      tuple.TUPLE_F_IN,
+			},
+		},
+	}
+	natValV6 := &nat.NatEntry6{
+		Created:   37400,
+		HostLocal: 1,
+		Addr:      types.IPv6{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+		Port:      0x51d6,
+	}
+	err = bpf.UpdateElement(natMapV6.Map.GetFd(), unsafe.Pointer(natKeyV6),
+		unsafe.Pointer(natValV6), 0)
+	c.Assert(err, IsNil)
+
+	stats = PurgeOrphanNATEntries(ctMapTCPV6, ctMapAnyV6)
+	c.Assert(stats.IngressDeleted, Equals, uint32(1))
+	c.Assert(stats.EgressDeleted, Equals, uint32(0))
 	buf = make(map[string][]string)
 	err = natMap.Map.Dump(buf)
 	c.Assert(err, IsNil)

pkg/maps/ctmap/gc/gc.go
@@ -49,6 +49,7 @@ func Enable(ipv4, ipv6 bool, restoredEndpoints []*endpoint.Endpoint, mgr Endpoin
 		var wakeup = make(chan signal.SignalData)
 		ipv4Orig := ipv4
 		ipv6Orig := ipv6
+		triggeredBySignal := false
 		for {
 			var (
 				maxDeleteRatio float64
@@ -87,14 +88,14 @@ func Enable(ipv4, ipv6 bool, restoredEndpoints []*endpoint.Endpoint, mgr Endpoin
 			}
 
 			if len(eps) > 0 || initialScan {
-				mapType, maxDeleteRatio = runGC(nil, ipv4, ipv6, createGCFilter(initialScan, restoredEndpoints, emitEntryCB))
+				mapType, maxDeleteRatio = runGC(nil, ipv4, ipv6, triggeredBySignal, createGCFilter(initialScan, restoredEndpoints, emitEntryCB))
 			}
 			for _, e := range eps {
 				if !e.ConntrackLocal() {
 					// Skip because GC was handled above.
 					continue
 				}
-				runGC(e, ipv4, ipv6, &ctmap.GCFilter{RemoveExpired: true, EmitCTEntryCB: emitEntryCB})
+				runGC(e, ipv4, ipv6, triggeredBySignal, &ctmap.GCFilter{RemoveExpired: true, EmitCTEntryCB: emitEntryCB})
 			}
 
 			// Mark the CT GC as over in each EP DNSZombies instance
@@ -111,9 +112,11 @@ func Enable(ipv4, ipv6 bool, restoredEndpoints []*endpoint.Endpoint, mgr Endpoin
 				signal.MuteChannel(signal.SignalWakeGC)
 			}
 
+			triggeredBySignal = false
 			signal.UnmuteChannel(signal.SignalWakeGC)
 			select {
 			case x := <-wakeup:
+				triggeredBySignal = true
 				ipv4 = false
 				ipv6 = false
 				if x == signal.SignalProtoV4 {
@@ -154,7 +157,7 @@ func Enable(ipv4, ipv6 bool, restoredEndpoints []*endpoint.Endpoint, mgr Endpoin
 // The provided endpoint is optional; if it is provided, then its map will be
 // garbage collected and any failures will be logged to the endpoint log.
 // Otherwise it will garbage-collect the global map and use the global log.
-func runGC(e *endpoint.Endpoint, ipv4, ipv6 bool, filter *ctmap.GCFilter) (mapType bpf.MapType, maxDeleteRatio float64) {
+func runGC(e *endpoint.Endpoint, ipv4, ipv6, triggeredBySignal bool, filter *ctmap.GCFilter) (mapType bpf.MapType, maxDeleteRatio float64) {
 	var maps []*ctmap.Map
 
 	if e == nil {
@@ -195,6 +198,17 @@ func runGC(e *endpoint.Endpoint, ipv4, ipv6 bool, filter *ctmap.GCFilter) (mapTy
 				logfields.Path: path,
 				"count":        deleted,
 			}).Debug("Deleted filtered entries from map")
+		}
+
+		if triggeredBySignal {
+			stats := ctmap.PurgeOrphanNATEntries(m)
+			if stats != nil && (stats.EgressDeleted != 0 || stats.IngressDeleted != 0) {
+				log.WithFields(logrus.Fields{
+					"ingressDeleted": stats.IngressDeleted,
+					"egressDeleted":  stats.EgressDeleted,
+					"ingressAlive":   stats.IngressAlive,
+				}).Info("Deleted orphan SNAT entries from map")
+			}
 		}
 	}
 

pkg/maps/ctmap/metrics.go
@@ -122,3 +122,19 @@ func (s *gcStats) finish() {
 	metrics.ConntrackGCDuration.WithLabelValues(family, proto, status).Observe(duration.Seconds())
 	metrics.ConntrackGCKeyFallbacks.WithLabelValues(family, proto).Add(float64(s.KeyFallback))
 }
+
+type NatGCStats struct {
+	*bpf.DumpStats
+
+	IngressAlive   uint32
+	IngressDeleted uint32
+	EgressDeleted  uint32
+	// It's not possible with the current PurgeOrphanNATEntries implementation
+	// to correctly count EgressAlive, so skip it
+}
+
+func newNatGCStats(m NatMap) NatGCStats {
+	return NatGCStats{
+		DumpStats: m.DumpStats(),
+	}
+}

pkg/maps/ctmap/utils.go
@@ -1,0 +1,85 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ctmap
+
+import (
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/maps/nat"
+	"github.com/cilium/cilium/pkg/tuple"
+)
+
+// NOTE: the function does NOT copy addr fields, so it's not safe to
+// reuse the returned natKey.
+func oNatKeyFromReverse(k nat.NatKey, v nat.NatEntry) nat.NatKey {
+	natKey, ok := k.(*nat.NatKey4)
+	if ok { // ipv4
+		natVal := v.(*nat.NatEntry4)
+		return &nat.NatKey4{TupleKey4Global: tuple.TupleKey4Global{
+			TupleKey4: tuple.TupleKey4{
+				SourceAddr: natVal.Addr,
+				SourcePort: natVal.Port,
+				DestAddr:   natKey.SourceAddr,
+				DestPort:   natKey.SourcePort,
+				NextHeader: natKey.NextHeader,
+				Flags:      tuple.TUPLE_F_OUT,
+			}}}
+	}
+
+	{ // ipv6
+		natKey := k.(*nat.NatKey6)
+		natVal := v.(*nat.NatEntry6)
+		return &nat.NatKey6{TupleKey6Global: tuple.TupleKey6Global{
+			TupleKey6: tuple.TupleKey6{
+				SourceAddr: natVal.Addr,
+				SourcePort: natVal.Port,
+				DestAddr:   natKey.SourceAddr,
+				DestPort:   natKey.SourcePort,
+				NextHeader: natKey.NextHeader,
+				Flags:      tuple.TUPLE_F_OUT,
+			}}}
+	}
+}
+
+// NOTE: the function does NOT copy addr fields, so it's not safe to
+// reuse the returned ctKey.
+func egressCTKeyFromIngressNatKeyAndVal(k nat.NatKey, v nat.NatEntry) bpf.MapKey {
+	natKey, ok := k.(*nat.NatKey4)
+	if ok { // ipv4
+		natVal := v.(*nat.NatEntry4)
+		return &tuple.TupleKey4Global{TupleKey4: tuple.TupleKey4{
+			// Workaround #5848
+			SourceAddr: natKey.SourceAddr,
+			DestPort:   natKey.SourcePort,
+			DestAddr:   natVal.Addr,
+			SourcePort: natVal.Port,
+			NextHeader: natKey.NextHeader,
+			Flags:      tuple.TUPLE_F_OUT,
+		}}
+	}
+
+	{ // ipv6
+		natKey := k.(*nat.NatKey6)
+		natVal := v.(*nat.NatEntry6)
+		return &tuple.TupleKey6Global{TupleKey6: tuple.TupleKey6{
+			// Workaround #5848
+			SourceAddr: natKey.SourceAddr,
+			DestPort:   natKey.SourcePort,
+			DestAddr:   natVal.Addr,
+			SourcePort: natVal.Port,
+			NextHeader: natKey.NextHeader,
+			Flags:      tuple.TUPLE_F_OUT,
+		}}
+	}
+}

pkg/maps/nat/nat.go
@@ -119,6 +119,18 @@ func NewMap(name string, v4 bool, entries int) *Map {
 	}
 }
 
+func (m *Map) Delete(k bpf.MapKey) error {
+	return (&m.Map).Delete(k)
+}
+
+func (m *Map) DumpStats() *bpf.DumpStats {
+	return bpf.NewDumpStats(&m.Map)
+}
+
+func (m *Map) DumpReliablyWithCallback(cb bpf.DumpCallback, stats *bpf.DumpStats) error {
+	return (&m.Map).DumpReliablyWithCallback(cb, stats)
+}
+
 // DoDumpEntries iterates through Map m and writes the values of the
 // nat entries in m to a string.
 func DoDumpEntries(m NatMap) (string, error) {

pkg/maps/nat/types.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/tuple"
+	"github.com/cilium/cilium/pkg/u8proto"
 )
 
 type NatKey interface {
@@ -36,6 +37,9 @@ type NatKey interface {
 
 	// GetFlags flags containing the direction of the TupleKey.
 	GetFlags() uint8
+
+	// GetNextHeader returns the proto of the NatKey
+	GetNextHeader() u8proto.U8proto
 }
 
 // NatKey4 is needed to provide NatEntry type to Lookup values
@@ -76,6 +80,10 @@ func (k *NatKey4) ToHost() NatKey {
 // GetKeyPtr returns the unsafe.Pointer for k.
 func (k *NatKey4) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
 
+func (k *NatKey4) GetNextHeader() u8proto.U8proto {
+	return k.NextHeader
+}
+
 // NatKey6 is needed to provide NatEntry type to Lookup values
 // +k8s:deepcopy-gen=true
 // +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
@@ -113,3 +121,7 @@ func (k *NatKey6) ToHost() NatKey {
 
 // GetKeyPtr returns the unsafe.Pointer for k.
 func (k *NatKey6) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+
+func (k *NatKey6) GetNextHeader() u8proto.U8proto {
+	return k.NextHeader
+}

commit 86cc78db57a2863fe3d327b7a618c14ba817e6e7

ctmap: Fix data race for accessing nat maps
Commit c9810bf7b2 introduced garbage collection for cleaning orphan entries
in the nat maps whereby concurrent accesses to the maps weren't serialized.

The nat maps accessed via ct map construct are susceptible
to data races in asynchronously running goroutines upon
agent restart when endpoints are restored -

```
2022-02-21T02:42:13.757888057Z WARNING: DATA RACE
2022-02-21T02:42:13.757895621Z Write at 0x00c00081a830 by goroutine 360:
2022-02-21T02:42:13.757912783Z   github.com/cilium/cilium/pkg/bpf.(*Map).Close()
2022-02-21T02:42:13.757920669Z       /go/src/github.com/cilium/cilium/pkg/bpf/map_linux.go:581 +0x1c4
2022-02-21T02:42:13.757927597Z   github.com/cilium/cilium/pkg/maps/ctmap.doGC4·dwrap·4()
2022-02-21T02:42:13.757934561Z       /go/src/github.com/cilium/cilium/pkg/maps/ctmap/ctmap.go:422 +0x39
2022-02-21T02:42:13.757941184Z   github.com/cilium/cilium/pkg/maps/ctmap.doGC4()
2022-02-21T02:42:13.757947352Z       /go/src/github.com/cilium/cilium/pkg/maps/ctmap/ctmap.go:482 +0x4d5
2022-02-21T02:42:13.757953881Z   github.com/cilium/cilium/pkg/maps/ctmap.doGC()
2022-02-21T02:42:13.757960362Z       /go/src/github.com/cilium/cilium/pkg/maps/ctmap/ctmap.go:517 +0x17e
2022-02-21T02:42:13.757966185Z   github.com/cilium/cilium/pkg/maps/ctmap.GC()
2022-02-21T02:42:13.757972307Z       /go/src/github.com/cilium/cilium/pkg/maps/ctmap/ctmap.go:537 +0xc6
2022-02-21T02:42:13.757978599Z   github.com/cilium/cilium/pkg/endpoint.(*Endpoint).garbageCollectConntrack()
2022-02-21T02:42:13.757986321Z       /go/src/github.com/cilium/cilium/pkg/endpoint/bpf.go:1034 +0x804
2022-02-21T02:42:13.757992160Z   github.com/cilium/cilium/pkg/endpoint.(*Endpoint).scrubIPsInConntrackTableLocked()
2022-02-21T02:42:13.757998853Z       /go/src/github.com/cilium/cilium/pkg/endpoint/bpf.go:1039 +0x173
2022-02-21T02:42:13.758004601Z   github.com/cilium/cilium/pkg/endpoint.(*Endpoint).scrubIPsInConntrackTable()

pkg/maps/ctmap/ctmap.go
@@ -94,6 +94,7 @@ const (
 )
 
 var globalDeleteLock [mapTypeMax]lock.Mutex
+var natMapsLock [mapTypeMax]*lock.Mutex
 
 type mapAttributes struct {
 	mapKey     bpf.MapKey
@@ -103,6 +104,7 @@ type mapAttributes struct {
 	maxEntries int
 	parser     bpf.DumpParser
 	bpfDefine  string
+	natMapLock *lock.Mutex // Serializes concurrent accesses to natMap
 	natMap     *nat.Map
 }
 
@@ -134,6 +136,7 @@ func setupMapInfo(m mapType, define string, mapKey bpf.MapKey, keySize int, maxE
 		valueSize:  SizeofCtEntry,
 		maxEntries: maxEntries,
 		parser:     bpf.ConvertKeyValue,
+		natMapLock: natMapsLock[m],
 		natMap:     nat,
 	}
 }
@@ -157,6 +160,12 @@ func InitMapInfo(tcpMaxEntries, anyMaxEntries int, v4, v6, nodeport bool) {
 		mapTypeIPv4AnyGlobal: global4Map,
 		mapTypeIPv6AnyGlobal: global6Map,
 	}
+	global4MapLock := &lock.Mutex{}
+	global6MapLock := &lock.Mutex{}
+	natMapsLock[mapTypeIPv4TCPGlobal] = global4MapLock
+	natMapsLock[mapTypeIPv6TCPGlobal] = global6MapLock
+	natMapsLock[mapTypeIPv4AnyGlobal] = global4MapLock
+	natMapsLock[mapTypeIPv6AnyGlobal] = global6MapLock
 
 	setupMapInfo(mapTypeIPv4TCPLocal, "CT_MAP_TCP4",
 		&CtKey4{}, int(unsafe.Sizeof(CtKey4{})),
@@ -333,7 +342,10 @@ func purgeCtEntry6(m *Map, key CtKey, natMap *nat.Map) error {
 // doGC6 iterates through a CTv6 map and drops entries based on the given
 // filter.
 func doGC6(m *Map, filter *GCFilter) gcStats {
-	natMap := mapInfo[m.mapType].natMap
+	ctMap := mapInfo[m.mapType]
+	ctMap.natMapLock.Lock()
+	defer ctMap.natMapLock.Unlock()
+	natMap := ctMap.natMap
 	stats := statStartGc(m)
 	defer stats.finish()
 
@@ -413,7 +425,10 @@ func purgeCtEntry4(m *Map, key CtKey, natMap *nat.Map) error {
 // doGC4 iterates through a CTv4 map and drops entries based on the given
 // filter.
 func doGC4(m *Map, filter *GCFilter) gcStats {
-	natMap := mapInfo[m.mapType].natMap
+	ctMap := mapInfo[m.mapType]
+	ctMap.natMapLock.Lock()
+	defer ctMap.natMapLock.Unlock()
+	natMap := ctMap.natMap
 	stats := statStartGc(m)
 	defer stats.finish()
 
@@ -570,7 +585,10 @@ func PurgeOrphanNATEntries(ctMapTCP, ctMapAny *Map) *NatGCStats {
 
 	// Both CT maps should point to the same natMap, so use the first one
 	// to determine natMap
-	natMap := mapInfo[ctMapTCP.mapType].natMap
+	ctMap := mapInfo[ctMapTCP.mapType]
+	ctMap.natMapLock.Lock()
+	defer ctMap.natMapLock.Unlock()
+	natMap := ctMap.natMap
 	if natMap == nil {
 		return nil
 	}