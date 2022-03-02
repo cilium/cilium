cilium/cilium

commit 0d585f110846237eb3611e88d0992ba26a162f72

policy: Do not expose internal maps to callers
MapState internal state could be modified by callers via internal
maps. Nil them on entries stored outside of the MapState itself.

Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

pkg/policy/mapstate.go
@@ -176,6 +176,8 @@ func (keys MapState) addKeyWithChanges(key Key, entry MapStateEntry, adds, delet
 
 	// Record an incremental Add if desired and entry is new or changed
 	if adds != nil && (!exists || !oldEntry.DatapathEqual(&entry)) {
+		// Do not leak internal maps to callers
+		updatedEntry.owners = nil
 		adds[key] = updatedEntry
 		// Key add overrides any previous delete of the same key
 		if deletes != nil {
@@ -203,6 +205,8 @@ func (keys MapState) deleteKeyWithChanges(key Key, cs MapStateOwner, adds, delet
 			}
 		}
 		if deletes != nil {
+			// Do not leak internal maps to callers
+			entry.owners = nil
 			deletes[key] = entry
 			// Remove a potential previously added key
 			if adds != nil {

pkg/policy/distillery_test.go
@@ -316,13 +316,13 @@ var (
 	mapKeyAllowAll__ = Key{0, 0, 0, dirIngress}
 	// Desired map entries for no L7 redirect / redirect to Proxy
 	mapEntryL7None_ = func(lbls ...labels.LabelArray) MapStateEntry {
-		return NewMapStateEntry(nil, labels.LabelArrayList(lbls).Sort(), false, false).WithoutOwners()
+		return NewMapStateEntry(nil, labels.LabelArrayList(lbls).Sort(), false, false).WithOwners()
 	}
 	mapEntryL7Deny_ = func(lbls ...labels.LabelArray) MapStateEntry {
-		return NewMapStateEntry(nil, labels.LabelArrayList(lbls).Sort(), false, true).WithoutOwners()
+		return NewMapStateEntry(nil, labels.LabelArrayList(lbls).Sort(), false, true).WithOwners()
 	}
 	mapEntryL7Proxy = func(lbls ...labels.LabelArray) MapStateEntry {
-		return NewMapStateEntry(nil, labels.LabelArrayList(lbls).Sort(), true, false).WithoutOwners()
+		return NewMapStateEntry(nil, labels.LabelArrayList(lbls).Sort(), true, false).WithOwners()
 	}
 )
 

pkg/policy/mapstate_test.go
@@ -27,7 +27,8 @@ func (e MapStateEntry) WithOwners(owners ...MapStateOwner) MapStateEntry {
 // WithoutOwners returns a copy of 'e', but owners replaced with 'nil'. 'e' is not modified.
 // Note: This is used only in unit tests and helps test readability.
 func (e MapStateEntry) WithoutOwners() MapStateEntry {
-	return e.WithOwners()
+	e.owners = nil
+	return e
 }
 
 func (ds *PolicyTestSuite) TestPolicyKeyTrafficDirection(c *check.C) {
@@ -762,7 +763,9 @@ func (ds *PolicyTestSuite) TestMapState_AccumulateMapChanges(c *check.C) {
 			ProxyPort: proxyPort,
 			IsDeny:    deny,
 		}
-		entry.owners = make(map[MapStateOwner]struct{}, len(owners))
+		if len(owners) > 0 {
+			entry.owners = make(map[MapStateOwner]struct{}, len(owners))
+		}
 		for _, cs := range owners {
 			entry.owners[cs] = struct{}{}
 		}
@@ -795,7 +798,7 @@ func (ds *PolicyTestSuite) TestMapState_AccumulateMapChanges(c *check.C) {
 			HttpIngressKey(42): TestEntry(0, false, csFoo),
 		},
 		adds: MapState{
-			HttpIngressKey(42): TestEntry(0, false, csFoo),
+			HttpIngressKey(42): TestEntry(0, false),
 		},
 		deletes: MapState{},
 	}, {
@@ -819,8 +822,8 @@ func (ds *PolicyTestSuite) TestMapState_AccumulateMapChanges(c *check.C) {
 			HttpIngressKey(43): TestEntry(0, false, csFoo),
 		},
 		adds: MapState{
-			HttpIngressKey(42): TestEntry(0, false, csFoo),
-			HttpIngressKey(43): TestEntry(0, false, csFoo),
+			HttpIngressKey(42): TestEntry(0, false),
+			HttpIngressKey(43): TestEntry(0, false),
 		},
 		deletes: MapState{},
 	}, {
@@ -835,7 +838,7 @@ func (ds *PolicyTestSuite) TestMapState_AccumulateMapChanges(c *check.C) {
 			HttpIngressKey(44): TestEntry(0, false, csBar),
 		},
 		adds: MapState{
-			HttpIngressKey(44): TestEntry(0, false, csBar),
+			HttpIngressKey(44): TestEntry(0, false),
 		},
 		deletes: MapState{},
 	}, {
@@ -906,10 +909,10 @@ func (ds *PolicyTestSuite) TestMapState_AccumulateMapChanges(c *check.C) {
 			DNSTCPEgressKey(42): TestEntry(0, false, csBar),
 		},
 		adds: MapState{
-			AnyIngressKey():     TestEntry(0, false, nil),
-			HostIngressKey():    TestEntry(0, false, nil),
-			DNSUDPEgressKey(42): TestEntry(0, false, csBar),
-			DNSTCPEgressKey(42): TestEntry(0, false, csBar),
+			AnyIngressKey():     TestEntry(0, false),
+			HostIngressKey():    TestEntry(0, false),
+			DNSUDPEgressKey(42): TestEntry(0, false),
+			DNSTCPEgressKey(42): TestEntry(0, false),
 		},
 		deletes: MapState{},
 	}, {
@@ -926,7 +929,7 @@ func (ds *PolicyTestSuite) TestMapState_AccumulateMapChanges(c *check.C) {
 			HttpEgressKey(43):   TestEntry(1, false, csFoo),
 		},
 		adds: MapState{
-			HttpEgressKey(43): TestEntry(1, false, csFoo),
+			HttpEgressKey(43): TestEntry(1, false),
 		},
 		deletes: MapState{},
 	}, {

pkg/policy/resolve_deny_test.go
@@ -488,8 +488,8 @@ func (ds *PolicyTestSuite) TestMapStateWithIngressDeny(c *C) {
 	// maps on the policy got cleared
 
 	c.Assert(adds, checker.Equals, MapState{
-		{Identity: 192, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry,
-		{Identity: 194, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry,
+		{Identity: 192, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry.WithoutOwners(),
+		{Identity: 194, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry.WithoutOwners(),
 	})
 	c.Assert(deletes, checker.Equals, MapState{
 		{Identity: 193, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry.WithoutOwners(),

pkg/policy/resolve_test.go
@@ -660,8 +660,8 @@ func (ds *PolicyTestSuite) TestMapStateWithIngress(c *C) {
 	c.Assert(policy.policyMapChanges.changes, IsNil)
 
 	c.Assert(adds, checker.Equals, MapState{
-		{Identity: 192, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry,
-		{Identity: 194, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry,
+		{Identity: 192, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry.WithoutOwners(),
+		{Identity: 194, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry.WithoutOwners(),
 	})
 	c.Assert(deletes, checker.Equals, MapState{
 		{Identity: 193, DestPort: 80, Nexthdr: 6}: rule1MapStateEntry.WithoutOwners(),

commit 04eacd3b2742bb95efeaf19eee96b5f922986eda

policy: Track policy entries with no owners to avoid BPF state leaks
When an identity is deleted, the agent processes the incremental policy
map changes against previously saved policy state with owners
and dependents of the identity key to decide if an identity can be marked for
deletion in the `deletes` map. In some cases, the `owners` field for the identity
entry was nil, and the deleteKeyWithChanges would return early without tracking
the identity for deletion. As a result, when this information is consumed
later to delete the identity from the BPF maps, the identity was being leaked
in those cases.

Fix : While removing contributions of an owner passed via incremental map
changes to an identity entry, check if the store policy map state for the
entry had any previously populated owners.
If no owners found, the key entry will be removed from the map as expected
so that the deleted identity is correctly tracked in the `deletes` map.

Snippet of leaked identity entries in an affected cluster -

$ kubectl get ciliumidentity 2025
NAME   NAMESPACE   AGE
2025   default     99s

$ kubectl -n kube-system exec cilium-ktdhh -c cilium-agent -- cilium bpf policy get -n 2382 | grep 2025

$ kubectl get ciliumidentity 2025
Error from server (NotFound): ciliumidentities.cilium.io "2025" not found

$ kubectl -n kube-system exec cilium-ktdhh -c cilium-agent -- cilium bpf policy get -n 2382 | grep 2025
Allow    Ingress     2025       ANY          NONE         0          0

Fixes: 0d585f1

Co-authored-by: Jarno Rajahalme <jarno@covalent.io>
Signed-off-by: Aditi Ghag <aditi@cilium.io>

pkg/policy/mapstate.go
@@ -242,9 +242,11 @@ func (keys MapState) addKeyWithChanges(key Key, entry MapStateEntry, adds, delet
 
 // deleteKeyWithChanges deletes a 'key' from 'keys' keeping track of incremental changes in 'adds' and 'deletes'.
 // The key is unconditionally deleted if 'cs' is nil, otherwise only the contribution of this 'cs' is removed.
+// They key is also deleted from the map state if it doesn't have any owners anymore.
 func (keys MapState) deleteKeyWithChanges(key Key, owner MapStateOwner, adds, deletes MapState) {
 	if entry, exists := keys[key]; exists {
-		if owner != nil {
+		// Check contributions of the passed owner.
+		if owner != nil && len(entry.owners) != 0 {
 			// remove the contribution of the given selector only
 			if _, exists = entry.owners[owner]; exists {
 				// Remove the contribution of this selector from the entry