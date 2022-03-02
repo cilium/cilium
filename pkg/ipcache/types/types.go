fde3ad28b7d0be717b140b76675779a91c4b59b4
Joe Stringer <joe@cilium.io>
2022-03-01 22:43:52 -0800

    ipcache: Initialize global later during daemon
    Avoid initializing the IPCache statically once in the package, and
    instead initialize it as part of the regular daemon startup. This allows
    us to inject references to other subsystems as part of the IPCache
    initialization, which will lead to code simplification elsewhere in
    propagating these references as part of the upcoming patches.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

---
+++ pkg/ipcache/types/types.go
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package ipcachetypes
+
+import (
+	"context"
+	"sync"
+
+	"github.com/cilium/cilium/pkg/identity/cache"
+)
+
+// PolicyHandler is responsible for handling identity updates into the core
+// policy engine. See SelectorCache.UpdateIdentities() for more details.
+type PolicyHandler interface {
+	UpdateIdentities(added, deleted cache.IdentityCache, wg *sync.WaitGroup)
+}
+
+// DatapathHandler is responsible for ensuring that policy updates in the
+// core policy engine are pushed into the underlying BPF policy maps, to ensure
+// that the policies are actively being enforced in the datapath for any new
+// identities that have been updated using 'PolicyHandler'.
+//
+// Wait on the returned sync.WaitGroup to ensure that the operation is complete
+// before updating the datapath's IPCache maps.
+type DatapathHandler interface {
+	UpdatePolicyMaps(context.Context, *sync.WaitGroup) *sync.WaitGroup
+}
