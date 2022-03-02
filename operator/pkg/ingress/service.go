7f785a856230d1b3cf1ad10ed0d1cd5b50f82311
Tam Mach <tam.mach@isovalent.com>
2022-03-02 21:36:03 +1100

    ingress: Perform deep equal check in update handler
    This commit is to perform deep equal check before adding update event
    into the queue.
    
    Signed-off-by: Tam Mach <tam.mach@isovalent.com>

--- operator/pkg/ingress/service.go
+++ operator/pkg/ingress/service.go
@@ -108,6 +108,9 @@ func (sm *serviceManager) handleUpdateService(oldObj, newObj interface{}) {
 	if newService != nil {
 		return
 	}
+	if oldService.DeepEqual(newService) {
+		return
+	}
 	sm.queue.Add(serviceUpdatedEvent{oldService: oldService, newService: newService})
 }
 
@@ -140,13 +143,11 @@ func (sm *serviceManager) handleEvent(event interface{}) error {
 	case serviceAddedEvent:
 		log.WithField("service", ev.service.Name).Info("Handling service added event")
 		err = sm.handleServiceAddedEvent(ev)
-		break
 	case serviceUpdatedEvent:
 		log.WithField("service", ev.newService.Name).Info("Handling service updated event")
 		err = sm.handleServiceUpdatedEvent(ev)
-		break
 	case serviceDeletedEvent:
-		break
+		//doing nothing right now
 	default:
 		err = fmt.Errorf("received an unknown event: %s", ev)
 	}
