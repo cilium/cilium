f97f112b8401bd83f5ceaf4f9e08a4c9eaebf66a
Paul Chaignon <paul@cilium.io>
2022-03-03 14:40:37 +0100

    Revert "ipsec: set interface ID different from 0"
    This reverts commit 735ac6a7b0464b0936b3b8c42b0b92b0e3ade0cd.
    
    Linux commit 68ac0f3810e7 ("xfrm: state and policy should fail if
    XFRMA_IF_ID 0") broke userspace applications by refusing xfrm states and
    policies with a zero XFRMA_IF_ID. Commit 735ac6a7 ("ipsec: set interface
    ID different from 0") attempted to work around this by defining a
    non-zero XFRMA_IF_ID.
    
    Unfortunately, this workaround breaks IPsec connectivity between nodes.
    Once the XFRMA_IF_ID is set to the placeholder value (1), traffic that
    should be encrypted leave the node without any encryption. On GKE and
    self-managed clusters, that's the only noticeable impact. However, on
    AKS and EKS, we also have BPF logic to rewrite the outer IP address to
    the proper IP. This still happens despite the failure to encrypt
    traffic, leading to packet drops.
    
    The traffic leaves the node unencrypted because packets don't match the
    xfrm policies anymore, due to the non-zero XFRMA_IF_ID.
    
    Thus, we didn't notice this regression in the pull request introducing
    the workaround because 1) GKE and Jenkins tests didn't fail and 2) the
    EKS and AKS IPsec tests are currently disabled. This was noticed while
    attempting to reenable the AKS IPsec test.
    
    This revert was tested in CI by running the whole workflow three times
    successfully and locally by running the connectivity tests 10 times on
    the same cluster.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -33,9 +33,6 @@ const (
 	IPSecDirOut     IPSecDir = "IPSEC_OUT"
 	IPSecDirBoth    IPSecDir = "IPSEC_BOTH"
 	IPSecDirOutNode IPSecDir = "IPSEC_OUT_NODE"
-	// XfrmInterfaceID must be different from 0 to avoid
-	// error while constructing xfrm state or policy.
-	XfrmInterfaceID int = 1
 )
 
 type ipSecKey struct {
@@ -63,15 +60,12 @@ func ipSecNewState() *netlink.XfrmState {
 		Mode:  netlink.XFRM_MODE_TUNNEL,
 		Proto: netlink.XFRM_PROTO_ESP,
 		ESN:   false,
-		Ifid:  XfrmInterfaceID,
 	}
 	return &state
 }
 
 func ipSecNewPolicy() *netlink.XfrmPolicy {
-	policy := netlink.XfrmPolicy{
-		Ifid: XfrmInterfaceID,
-	}
+	policy := netlink.XfrmPolicy{}
 	return &policy
 }
 
