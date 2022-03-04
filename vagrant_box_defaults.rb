7c8d651d34fc9116a564529dc514ef100b26ca25
Joe Stringer <joe@cilium.io>
2022-03-04 12:24:17 -0800

    vagrant: Bump runtime box image
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- vagrant_box_defaults.rb
+++ vagrant_box_defaults.rb
@@ -3,7 +3,7 @@
 Vagrant.require_version ">= 2.2.0"
 
 $SERVER_BOX = "cilium/ubuntu-dev"
-$SERVER_VERSION= "202"
+$SERVER_VERSION= "215"
 $NETNEXT_SERVER_BOX= "cilium/ubuntu-next"
 $NETNEXT_SERVER_VERSION= "97"
 @v54_SERVER_BOX= "cilium/ubuntu-5-4"
