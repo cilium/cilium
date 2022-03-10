cilium/cilium

commit 501c2a0bac4ab2f592440b87f52ae83dcb68481a

bpf: Monitor DNS with MTU-sized payload len
Signed-off-by: Joe Stringer <joe@covalent.io>

bpf/lib/conntrack.h
@@ -408,6 +408,8 @@ static inline int __inline__ ct_lookup6(void *map, struct ipv6_ct_tuple *tuple,
 #endif
 out:
 	cilium_dbg(skb, DBG_CT_VERDICT, ret < 0 ? -ret : ret, ct_state->rev_nat_index);
+	if (conn_is_dns(tuple->dport))
+		*monitor = MTU;
 	return ret;
 }
 
@@ -558,6 +560,8 @@ static inline int __inline__ ct_lookup4(void *map, struct ipv4_ct_tuple *tuple,
 	}
 out:
 	cilium_dbg(skb, DBG_CT_VERDICT, ret < 0 ? -ret : ret, ct_state->rev_nat_index);
+	if (conn_is_dns(tuple->dport))
+		*monitor = MTU;
 	return ret;
 }
 

bpf/lib/common.h
@@ -518,4 +518,22 @@ static inline void relax_verifier(void)
 	csum_diff(0, 0, &foo, 1, 0);
 }
 
+/* conn_is_dns returns true if the connection is DNS, false otherwise.
+ *
+ * @dport: Connection destination port.
+ *
+ * To reduce program complexity, we ignore nexthdr and dir here:
+ * nexthdr: The parser will not fill dport if nexthdr is not TCP/UDP.
+ * dir:     Ideally we would only consider responses, but requests are likely
+ *          to be small anyway.
+ * */
+static inline bool conn_is_dns(__u16 dport)
+{
+	if (dport == bpf_htons(53)) {
+		relax_verifier();
+		return true;
+	}
+	return false;
+}
+
 #endif

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=41f34972e641c74e1f8cb438eab72f1dcff174f7
+GO_BINDATA_SHA1SUM=10ce44082871ef1ea9e2fedaa36a3a268b3bfc4d
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

commit ee47b2ff0dbd530e0aace38d2481798177fcbc3a

bpf: Remove DNS quirk for monitor aggregation
This was a left-over quirk for DNS visibility a long time ago, and these
days we rely on DNS rules for that visibility instead. Remove this legacy
work-around so that monitor aggregation can do its job as expected also
with other ports. Effectively, this reverts commit 501c2a0bac4a ("bpf:
Monitor DNS with MTU-sized payload len").

Fixes: 501c2a0bac4a ("bpf: Monitor DNS with MTU-sized payload len")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/lib/conntrack.h
@@ -24,20 +24,6 @@ enum {
 	ACTION_CLOSE,
 };
 
-/* conn_is_dns returns true if the connection is DNS, false otherwise.
- *
- * @dport: Connection destination port.
- *
- * To reduce program complexity, we ignore nexthdr and dir here:
- * nexthdr: The parser will not fill dport if nexthdr is not TCP/UDP.
- * dir:     Ideally we would only consider responses, but requests are likely
- *          to be small anyway.
- */
-static __always_inline bool conn_is_dns(__u16 dport)
-{
-	return dport == bpf_htons(53);
-}
-
 static __always_inline bool ct_entry_seen_both_syns(const struct ct_entry *entry)
 {
 	bool rx_syn = entry->rx_flags_seen & TCP_FLAG_SYN;
@@ -454,8 +440,6 @@ static __always_inline int ct_lookup6(const void *map,
 #endif
 out:
 	cilium_dbg(ctx, DBG_CT_VERDICT, ret < 0 ? -ret : ret, ct_state->rev_nat_index);
-	if (conn_is_dns(tuple->dport))
-		*monitor = MTU;
 	return ret;
 }
 
@@ -754,8 +738,6 @@ static __always_inline int ct_lookup4(const void *map,
 	}
 out:
 	cilium_dbg(ctx, DBG_CT_VERDICT, ret < 0 ? -ret : ret, ct_state->rev_nat_index);
-	if (conn_is_dns(tuple->dport))
-		*monitor = MTU;
 	return ret;
 }
 