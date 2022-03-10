cilium/cilium

commit 51647bbdbd221d488c07120b116e06de1f1ed7d8

Makefile: Simplify to run faster
Avoid using grep options that are not supported on macOS. This
eliminates errors like this:

    grep: repetition-operator operand invalid
    expr: syntax error
    usage: grep [-abcDEFGHhIiJLlmnOoqRSsUVvwxZ] [-A num] [-B num] [-C[num]]
            [-e pattern] [-f file] [--binary-files=value] [--color=when]
            [--context[=num]] [--directories=action] [--label] [--line-buffered]
            [--null] [pattern] [file ...]

Make 'go list' faster by adding '-find' option that eliminates
resolution of some state we are not using:

     "The -find flag causes list to identify the named packages but not
     resolve their dependencies: the Imports and Deps lists will be empty."

This makes 'make nothing' to run about twice as fast as before,
especially in the dev VM.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

Makefile
@@ -26,12 +26,13 @@ ifdef LIBNETWORK_PLUGIN
 SUBDIRS_CILIUM_CONTAINER += plugins/cilium-docker
 endif
 
-GOFILES_EVAL := $(subst _$(ROOT_DIR)/,,$(shell $(GO_LIST) -e ./...))
+GOFILES_EVAL := $(subst _$(ROOT_DIR)/,,$(shell $(GO_LIST) -find -e ./...))
 GOFILES ?= $(GOFILES_EVAL)
 TESTPKGS_EVAL := $(subst github.com/cilium/cilium/,,$(shell echo $(GOFILES) | \
 	sed 's/ /\n/g' | \
 	grep -v '/api/v1\|/vendor\|/contrib\|/$(BUILD_DIR)/' | \
-	grep -v -P 'test(?!/helpers/logutils)'))
+	grep -v '/test'))
+TESTPKGS_EVAL += "test/helpers/logutils"
 TESTPKGS ?= $(TESTPKGS_EVAL)
 GOLANG_SRCFILES := $(shell for pkg in $(subst github.com/cilium/cilium/,,$(GOFILES)); do find $$pkg -name *.go -print; done | grep -v vendor | sort | uniq)
 K8S_CRD_EVAL := $(addprefix $(ROOT_DIR)/,$(shell git ls-files $(ROOT_DIR)/examples/crds | grep -v .gitignore | tr "\n" ' '))

Makefile.defs
@@ -109,7 +109,7 @@ ifeq ($(GO111MODULE),on)
     GO_TEST_FLAGS += -mod=vendor
     GO_CLEAN_FLAGS += -mod=vendor
 else
-    GO_MAJOR_VERSION_GE_1 := $(shell expr `$(GO) version | grep -E 'go[0-9]{1}+' -o | sed 's/go//g'` \>= 1)
+    GO_MAJOR_VERSION_GE_1 := $(shell expr `$(GO) version | grep -E 'go[0-9]+' -o | sed 's/go//g'` \>= 1)
     ifeq ($(GO_MAJOR_VERSION_GE_1),1)
         GO_MINOR_VERSION_GE_13 := $(shell expr `$(GO) version | grep -E 'go[^ ]+' -o | sed 's/go1.//g'` \>= 13)
         ifeq ($(GO_MINOR_VERSION_GE_13),1)

commit 3cbe0c0e9d56523e752b6af0383ef5adccd729f8

Makefile: Fix TESTPKGS commandline
Commit 51647bbdbd22 ("Makefile: Simplify to run faster") mistakenly
added a '/' to the end of the '$(ROOT_DIR)' in the substitution, which
invalidated the greps that attempted to exclude directories that do not
need to trigger unit tests, for instance the `test/` directory. As a
result, when running commands like `make integration-tests`, it was easy
to inadvertently attempt to run the end-to-end tests in an environment
that cannot run the end-to-end-tests.

Fix this issue by injecting a leading '/' before each package, so that
the package grep can match on that leading '/' in order to properly
select directories based on their path from the root directory. The
extra sed command will then filter the leading '/' back out before
passing that variable on.

While we're at it, fold the '/test' match into the main grep expression
and avoid updating the TESTPKGS_EVAL during initial Makefile evaluation,
to further speed up the calculation.

Fixes: 51647bbdbd22 ("Makefile: Simplify to run faster")
Signed-off-by: Joe Stringer <joe@cilium.io>

Makefile
@@ -23,10 +23,10 @@ endif
 GOFILES_EVAL := $(subst _$(ROOT_DIR)/,,$(shell $(GO_LIST) -find -e ./...))
 GOFILES ?= $(GOFILES_EVAL)
 TESTPKGS_EVAL := $(subst github.com/cilium/cilium/,,$(shell echo $(GOFILES) | \
-	sed 's/ /\n/g' | \
-	grep -v '/api/v1\|/vendor\|/contrib' | \
-	grep -v '/test'))
-TESTPKGS_EVAL += "test/helpers/logutils"
+		sed 's/ /\n\//g' | \
+		grep -v '/api/v1\|/vendor\|/contrib\|/test' | \
+		sed 's/^\///g')) \
+	test/helpers/logutils
 TESTPKGS ?= $(TESTPKGS_EVAL)
 GOLANG_SRCFILES := $(shell for pkg in $(subst github.com/cilium/cilium/,,$(GOFILES)); do find $$pkg -name *.go -print; done | grep -v vendor | sort | uniq)
 