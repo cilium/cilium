cilium/cilium

commit 16ee6205703cb03388717337d8cf18a60eb09a52

api,cli: add identity range in status response & cli output
This information helps a lot in ClusterMesh environments.

Effect preview:

```shell
$ cilium status
...
Controller Status:      130/130 healthy
Proxy Status:           OK, ip 10.22.64.63, 0 redirects active on ports 10000-20000
Identity Range:         min 262144, max 327679
...
```

Signed-off-by: ArthurChiao <arthurchiao@hotmail.com>

api/v1/models/identity_range.go
@@ -1,0 +1,51 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright 2017-2022 Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package models
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
+)
+
+// IdentityRange Status of identity range of the cluster
+//
+// +k8s:deepcopy-gen=true
+//
+// swagger:model IdentityRange
+type IdentityRange struct {
+
+	// Maximum identity of the cluster
+	MaxIdentity int64 `json:"max-identity,omitempty"`
+
+	// Minimum identity of the cluster
+	MinIdentity int64 `json:"min-identity,omitempty"`
+}
+
+// Validate validates this identity range
+func (m *IdentityRange) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *IdentityRange) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *IdentityRange) UnmarshalBinary(b []byte) error {
+	var res IdentityRange
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}

api/v1/models/status_response.go
@@ -67,6 +67,9 @@ type StatusResponse struct {
 	// Status of Hubble server
 	Hubble *HubbleStatus `json:"hubble,omitempty"`
 
+	// Status of identity range of the cluster
+	IdentityRange *IdentityRange `json:"identity-range,omitempty"`
+
 	// Status of IP address management
 	Ipam *IPAMStatus `json:"ipam,omitempty"`
 
@@ -145,6 +148,10 @@ func (m *StatusResponse) Validate(formats strfmt.Registry) error {
 	}
 
 	if err := m.validateHubble(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateIdentityRange(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -410,6 +417,24 @@ func (m *StatusResponse) validateHubble(formats strfmt.Registry) error {
 		if err := m.Hubble.Validate(formats); err != nil {
 			if ve, ok := err.(*errors.Validation); ok {
 				return ve.ValidateName("hubble")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *StatusResponse) validateIdentityRange(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.IdentityRange) { // not required
+		return nil
+	}
+
+	if m.IdentityRange != nil {
+		if err := m.IdentityRange.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("identity-range")
 			}
 			return err
 		}

api/v1/models/zz_generated.deepcopy.go
@@ -433,6 +433,22 @@ func (in *IPAMStatus) DeepCopy() *IPAMStatus {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *IdentityRange) DeepCopyInto(out *IdentityRange) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new IdentityRange.
+func (in *IdentityRange) DeepCopy() *IdentityRange {
+	if in == nil {
+		return nil
+	}
+	out := new(IdentityRange)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *K8sStatus) DeepCopyInto(out *K8sStatus) {
 	*out = *in
 	if in.K8sAPIVersions != nil {
@@ -997,6 +1013,11 @@ func (in *StatusResponse) DeepCopyInto(out *StatusResponse) {
 		in, out := &in.Hubble, &out.Hubble
 		*out = new(HubbleStatus)
 		(*in).DeepCopyInto(*out)
+	}
+	if in.IdentityRange != nil {
+		in, out := &in.IdentityRange, &out.IdentityRange
+		*out = new(IdentityRange)
+		**out = **in
 	}
 	if in.Ipam != nil {
 		in, out := &in.Ipam, &out.Ipam

api/v1/openapi.yaml
@@ -1744,6 +1744,9 @@ definitions:
       proxy:
         description: Status of proxy
         "$ref": "#/definitions/ProxyStatus"
+      identity-range:
+        description: Status of identity range of the cluster
+        "$ref": "#/definitions/IdentityRange"
       bandwidth-manager:
         description: Status of bandwidth manager
         "$ref": "#/definitions/BandwidthManager"
@@ -2690,6 +2693,18 @@ definitions:
         type: array
         items:
           "$ref": "#/definitions/ProxyRedirect"
+  IdentityRange:
+    description: |-
+      Status of identity range of the cluster
+
+      +k8s:deepcopy-gen=true
+    properties:
+      min-identity:
+        description: Minimum identity of the cluster
+        type: integer
+      max-identity:
+        description: Maximum identity of the cluster
+        type: integer
   ProxyRedirect:
     description: Configured proxy redirection state
     type: object

api/v1/server/embedded_spec.go
@@ -2833,6 +2833,19 @@ func init() {
         }
       }
     },
+    "IdentityRange": {
+      "description": "Status of identity range of the cluster\n\n+k8s:deepcopy-gen=true",
+      "properties": {
+        "max-identity": {
+          "description": "Maximum identity of the cluster",
+          "type": "integer"
+        },
+        "min-identity": {
+          "description": "Minimum identity of the cluster",
+          "type": "integer"
+        }
+      }
+    },
     "IpvlanConfiguration": {
       "description": "Setup for datapath when operating in ipvlan mode.",
       "type": "object",
@@ -3925,6 +3938,10 @@ func init() {
         "hubble": {
           "description": "Status of Hubble server",
           "$ref": "#/definitions/HubbleStatus"
+        },
+        "identity-range": {
+          "description": "Status of identity range of the cluster",
+          "$ref": "#/definitions/IdentityRange"
         },
         "ipam": {
           "description": "Status of IP address management",
@@ -7335,6 +7352,19 @@ func init() {
         }
       }
     },
+    "IdentityRange": {
+      "description": "Status of identity range of the cluster\n\n+k8s:deepcopy-gen=true",
+      "properties": {
+        "max-identity": {
+          "description": "Maximum identity of the cluster",
+          "type": "integer"
+        },
+        "min-identity": {
+          "description": "Minimum identity of the cluster",
+          "type": "integer"
+        }
+      }
+    },
     "IpvlanConfiguration": {
       "description": "Setup for datapath when operating in ipvlan mode.",
       "type": "object",
@@ -8673,6 +8703,10 @@ func init() {
         "hubble": {
           "description": "Status of Hubble server",
           "$ref": "#/definitions/HubbleStatus"
+        },
+        "identity-range": {
+          "description": "Status of identity range of the cluster",
+          "$ref": "#/definitions/IdentityRange"
         },
         "ipam": {
           "description": "Status of IP address management",

daemon/cmd/status.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/datapath"
 	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
+	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/k8s"
 	k8smetrics "github.com/cilium/cilium/pkg/k8s/metrics"
 	"github.com/cilium/cilium/pkg/kvstore"
@@ -684,6 +685,15 @@ func (d *Daemon) getStatus(brief bool) models.StatusResponse {
 	return sr
 }
 
+func (d *Daemon) getIdentityRange() *models.IdentityRange {
+	s := &models.IdentityRange{
+		MinIdentity: int64(identity.MinimalAllocationIdentity),
+		MaxIdentity: int64(identity.MaximumAllocationIdentity),
+	}
+
+	return s
+}
+
 func (d *Daemon) startStatusCollector() {
 	probes := []status.Probe{
 		{
@@ -997,6 +1007,7 @@ func (d *Daemon) startStatusCollector() {
 	d.statusResponse.ClockSource = d.getClockSourceStatus()
 	d.statusResponse.BpfMaps = d.getBPFMapStatus()
 	d.statusResponse.CniChaining = d.getCNIChainingStatus()
+	d.statusResponse.IdentityRange = d.getIdentityRange()
 
 	d.statusCollector = status.NewCollector(probes, status.Config{})
 

pkg/client/client.go
@@ -510,6 +510,13 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 		fmt.Fprintf(w, "Proxy Status:\tNo managed proxy redirect\n")
 	}
 
+	if sr.IdentityRange != nil {
+		fmt.Fprintf(w, "Global Identity Range:\tmin %d, max %d\n",
+			sr.IdentityRange.MinIdentity, sr.IdentityRange.MaxIdentity)
+	} else {
+		fmt.Fprintf(w, "Global Identity Range:\tUnknown\n")
+	}
+
 	if sr.Hubble != nil {
 		var fields []string
 

commit 0976a70b0a995e9da63a482fabdb49ebb5a8c47a

api: identity_range - update Copyright header
Fixing failure introduced by 16ee620570.

Signed-off-by: Timo Beckers <timo@isovalent.com>

api/v1/models/identity_range.go
@@ -1,6 +1,6 @@
 // Code generated by go-swagger; DO NOT EDIT.
 
-// Copyright 2017-2022 Authors of Cilium
+// Copyright Authors of Cilium
 // SPDX-License-Identifier: Apache-2.0
 
 package models