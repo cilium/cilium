4f359a8ae0fd4529c729cb43b49546fc22df72c5
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2022-03-16 16:10:14 +0900

    bpf: Remove unused src_id in to-netdev
    [ upstream commit d378a911b5e7bd155306040778f6e060c8a03542 ]
    
    src_id variable in to_netdev in bpf_host.c is always zero, so safe to
    delete.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    Signed-off-by: Alexandre Perrin <alex@kaworu.ch>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -1012,7 +1012,6 @@ int from_host(struct __ctx_buff *ctx)
 __section("to-netdev")
 int to_netdev(struct __ctx_buff *ctx __maybe_unused)
 {
-	__u32 __maybe_unused src_id = 0;
 	__u16 __maybe_unused proto = 0;
 	__u32 monitor = 0;
 	__u32 __maybe_unused vlan_id;
@@ -1062,7 +1061,7 @@ int to_netdev(struct __ctx_buff *ctx __maybe_unused)
 	}
 out:
 	if (IS_ERR(ret))
-		return send_drop_notify_error(ctx, src_id, ret, CTX_ACT_DROP,
+		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
 					      METRIC_EGRESS);
 #endif /* ENABLE_HOST_FIREWALL */
 
@@ -1099,7 +1098,7 @@ out:
 		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
 					      METRIC_EGRESS);
 #endif
-	send_trace_notify(ctx, TRACE_TO_NETWORK, src_id, 0, 0,
+	send_trace_notify(ctx, TRACE_TO_NETWORK, 0, 0, 0,
 			  0, 0, monitor);
 
 	return ret;
