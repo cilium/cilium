77020565075d29d51d5f680610c17d24916d1a5b
Chris Tarazi <chris@isovalent.com>
2022-04-06 16:27:10 -0700

    endpoint: Fix lock contention header file sync
    [ upstream commit 507332a082c8c4780e67f219dadf42f61772e908 ]
    
    Upon a successful DNS response, Cilium's DNS proxy code will sync the
    DNS history state to the individual endpoint's header file. Previously,
    this sync was done inside a trigger, however the calling code,
    (*Endpoint).SyncEndpointHeaderFile(), acquired a write-lock for no good
    reason. This effectively negated the benefits of having the DNS history
    sync behind a trigger of 5 seconds.
    
    This is especially suboptimal because the header file sync is actually
    causing Cilium to serialize processing the DNS request for a single
    endpoint.
    
    To illustrate the impact of the above a bit more concretely, if a single
    endpoint does 10 DNS requests at the same time, acquiring the write-lock
    causes the processing of those 10 requests to be done one at a time. For
    the sake of posterity, this is not the case if 10 endpoints were to make
    DNS requests in parallel.
    
    This obviously has a performance impact both in terms of being slow
    CPU-wise, but also memory-wise. Take for example a DNS request bursty
    environment, it could cause an uptick in memory usage due to many
    goroutines being created and blocking due to the serialized nature of
    locking.
    
    Now that the code is all executing behind a trigger, we can remove the
    lock completely and initialize the trigger setup where the Endpoint
    object is created (e.g. createEndpoint(), parseEndpoint()). Now the lock
    is only taken in every 5 seconds when the trigger runs.
    
    This should relieve the lock contention drastrically. For context, in a
    user's environment where the pprof was shared with us, there were around
    440 goroutines with 203 of them stuck waiting inside
    SyncEndpointHeaderFile().
    
    We can also modify SyncEndpointHeaderFile() to no longer return an
    error, because it's not possible for invoking the trigger to fail. If we
    fail to initialize the trigger itself, then we log an error, but this is
    essentially impossible because it can only fail if the trigger func is
    nil (which we control).
    
    Understanding the locking contention came from inspecting the pprof via
    the following command and subsequent code inspection.
    
    ```
    go tool trace -http :8080 ./cilium ./pprof-trace
    ```
    
    Suggested-by: Michi Mutsuzaki <michi@isovalent.com>
    Suggested-by: André Martins <andre@cilium.io>
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/endpoint/endpoint.go
+++ pkg/endpoint/endpoint.go
@@ -477,6 +477,8 @@ func createEndpoint(owner regeneration.Owner, proxy EndpointProxy, allocator cac
 		noTrackPort:     0,
 	}
 
+	ep.initDNSHistoryTrigger()
+
 	ctx, cancel := context.WithCancel(context.Background())
 	ep.aliveCancel = cancel
 	ep.aliveCtx = ctx
@@ -486,6 +488,19 @@ func createEndpoint(owner regeneration.Owner, proxy EndpointProxy, allocator cac
 	ep.SetDefaultOpts(option.Config.Opts)
 
 	return ep
+}
+
+func (e *Endpoint) initDNSHistoryTrigger() {
+	// Note: This can only fail if the trigger func is nil.
+	var err error
+	e.dnsHistoryTrigger, err = trigger.NewTrigger(trigger.Parameters{
+		Name:        "sync_endpoint_header_file",
+		MinInterval: 5 * time.Second,
+		TriggerFunc: e.syncEndpointHeaderFile,
+	})
+	if err != nil {
+		log.WithField(logfields.EndpointID, e.ID).WithError(err).Error("Failed to create the endpoint header file sync trigger")
+	}
 }
 
 // CreateHostEndpoint creates the endpoint corresponding to the host.
@@ -841,6 +856,8 @@ func parseEndpoint(ctx context.Context, owner regeneration.Owner, bEp []byte) (*
 	if err := parseBase64ToEndpoint(epSlice[1], &ep); err != nil {
 		return nil, fmt.Errorf("failed to parse restored endpoint: %s", err)
 	}
+
+	ep.initDNSHistoryTrigger()
 
 	// Validate the options that were parsed
 	ep.SetDefaultOpts(ep.Options)
@@ -2151,30 +2168,12 @@ func (e *Endpoint) syncEndpointHeaderFile(reasons []string) {
 	}
 }
 
-// SyncEndpointHeaderFile it bumps the current DNS History information for the
-// endpoint in the ep_config.h file.
-func (e *Endpoint) SyncEndpointHeaderFile() error {
-	if err := e.lockAlive(); err != nil {
-		// endpoint was removed in the meanwhile, return
-		return nil
+// SyncEndpointHeaderFile triggers the header file sync to the ep_config.h
+// file. This includes updating the current DNS History information.
+func (e *Endpoint) SyncEndpointHeaderFile() {
+	if e.dnsHistoryTrigger != nil {
+		e.dnsHistoryTrigger.Trigger()
 	}
-	defer e.unlock()
-
-	if e.dnsHistoryTrigger == nil {
-		t, err := trigger.NewTrigger(trigger.Parameters{
-			Name:        "sync_endpoint_header_file",
-			MinInterval: 5 * time.Second,
-			TriggerFunc: func(reasons []string) { e.syncEndpointHeaderFile(reasons) },
-		})
-		if err != nil {
-			return fmt.Errorf(
-				"Sync Endpoint header file trigger for endpoint cannot be activated: %s",
-				err)
-		}
-		e.dnsHistoryTrigger = t
-	}
-	e.dnsHistoryTrigger.Trigger()
-	return nil
 }
 
 // Delete cleans up all resources associated with this endpoint, including the
