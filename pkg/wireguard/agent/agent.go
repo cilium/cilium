e24cda96021ead02a9f2b1dd29b88fcb0553e9bd
Sebastian Wicki <sebastian@isovalent.com>
2022-04-06 18:52:29 +0200

    wireguard: Reject duplicate public keys
    [ upstream commit 130506e63f66bcb73339aecb857dbd374ef9094b ]
    
    This fixes an issue where a node could duplicate another node's public
    key and thereby force all cilium-agent instances to overwrite the
    original node's WireGuard peer.
    
    While the impersonating node cannot decrypt any traffic (it does not
    know the original node's public key, nor would traffic be redirected to
    the impersonating node due to the lack of AllowedIPs on the
    impersonating peer), the impersonating node could still effectively deny
    any traffic to the original node.
    
    This commit fixes the issues by keeping track of which node has added a
    given public key. Because node names are guaranteed to be unique, only
    one node can own a certain public key this way. Any attempt to upsert a
    public key already owned by another node will be rejected and a warning
    will be emitted in the cilium-agent logs.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- pkg/wireguard/agent/agent.go
+++ pkg/wireguard/agent/agent.go
@@ -74,6 +74,7 @@ type Agent struct {
 	privKey          wgtypes.Key
 	peerByNodeName   map[string]*peerConfig
 	nodeNameByNodeIP map[string]string
+	nodeNameByPubKey map[wgtypes.Key]string
 	restoredPubKeys  map[wgtypes.Key]struct{}
 }
 
@@ -98,6 +99,7 @@ func NewAgent(privKeyPath string) (*Agent, error) {
 		listenPort:       listenPort,
 		peerByNodeName:   map[string]*peerConfig{},
 		nodeNameByNodeIP: map[string]string{},
+		nodeNameByPubKey: map[wgtypes.Key]string{},
 		restoredPubKeys:  map[wgtypes.Key]struct{}{},
 	}, nil
 }
@@ -230,10 +232,22 @@ func (a *Agent) UpdatePeer(nodeName, pubKeyHex string, nodeIPv4, nodeIPv6 net.IP
 	a.Lock()
 	defer a.Unlock()
 
+	pubKey, err := wgtypes.ParseKey(pubKeyHex)
+	if err != nil {
+		return err
+	}
+
+	if prevNodeName, ok := a.nodeNameByPubKey[pubKey]; ok {
+		if nodeName != prevNodeName {
+			return fmt.Errorf("detected duplicate public key. "+
+				"node %q uses same key as existing node %q", nodeName, prevNodeName)
+		}
+	}
+
 	var allowedIPs []net.IPNet = nil
 	if prev := a.peerByNodeName[nodeName]; prev != nil {
 		// Handle pubKey change
-		if prev.pubKey.String() != pubKeyHex {
+		if prev.pubKey != pubKey {
 			log.WithField(logfields.NodeName, nodeName).Debug("Pubkey has changed")
 			// pubKeys differ, so delete old peer
 			if err := a.deletePeerByPubKey(prev.pubKey); err != nil {
@@ -267,11 +281,6 @@ func (a *Agent) UpdatePeer(nodeName, pubKeyHex string, nodeIPv4, nodeIPv6 net.IP
 			lookupIPv6 = nodeIPv6
 		}
 		allowedIPs = append(allowedIPs, a.ipCache.LookupByHostRLocked(lookupIPv4, lookupIPv6)...)
-	}
-
-	pubKey, err := wgtypes.ParseKey(pubKeyHex)
-	if err != nil {
-		return err
 	}
 
 	ep := ""
@@ -308,6 +317,7 @@ func (a *Agent) UpdatePeer(nodeName, pubKeyHex string, nodeIPv4, nodeIPv6 net.IP
 	}
 
 	a.peerByNodeName[nodeName] = peer
+	a.nodeNameByPubKey[pubKey] = nodeName
 	if nodeIPv4 != nil {
 		a.nodeNameByNodeIP[nodeIPv4.String()] = nodeName
 	}
@@ -332,6 +342,8 @@ func (a *Agent) DeletePeer(nodeName string) error {
 	}
 
 	delete(a.peerByNodeName, nodeName)
+	delete(a.nodeNameByPubKey, peer.pubKey)
+
 	if peer.nodeIPv4 != nil {
 		delete(a.nodeNameByNodeIP, peer.nodeIPv4.String())
 	}
