aa7ab0b28a546f144f177448b9e814855b8df4bf
Martynas Pumputis <m@lambda.lt>
2022-04-07 15:15:54 +0200

    daemon: Drop L7 check for WireGuard
    There is no longer skb mark conflict with L7 proxy, so we can drop the
    check. This means that the L7 proxy can work together with the WG
    integration.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/k8s/datapath_configuration.go
+++ test/k8s/datapath_configuration.go
@@ -572,7 +572,6 @@ var _ = Describe("K8sDatapathConfig", func() {
 				"autoDirectNodeRoutes": "true",
 				"encryption.enabled":   "true",
 				"encryption.type":      "wireguard",
-				"l7Proxy":              "false",
 			}, DeployCiliumOptionsAndDNS)
 
 			privateIface, err := kubectl.GetPrivateIface(helpers.K8s1)
@@ -585,7 +584,6 @@ var _ = Describe("K8sDatapathConfig", func() {
 				"tunnel":             "vxlan",
 				"encryption.enabled": "true",
 				"encryption.type":    "wireguard",
-				"l7Proxy":            "false",
 			}, DeployCiliumOptionsAndDNS)
 
 			testWireguard("cilium_vxlan")
@@ -597,7 +595,6 @@ var _ = Describe("K8sDatapathConfig", func() {
 				"endpointRoutes.enabled": "true",
 				"encryption.enabled":     "true",
 				"encryption.type":        "wireguard",
-				"l7Proxy":                "false",
 			}, DeployCiliumOptionsAndDNS)
 
 			testWireguard("cilium_vxlan")
