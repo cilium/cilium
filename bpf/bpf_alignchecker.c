cilium/cilium

commit 006ebebc28e60bf9b0ce45b5edc9a6bf16f057ef

bpf: Adjust alignchecker to avoid stack limits
When adding more fields to align-check, I hit this error:

  error: bpf_alignchecker.c:80:25: in function main i32 (): Looks like the
  BPF stack limit of 512 bytes is exceeded. Please move large on stack
  variables into BPF per-cpu array map.

Avoid this by nesting the structure declarations. While we're at it, use
a simple iterator to reduce boilerplate when adding new structs.

Signed-off-by: Joe Stringer <joe@cilium.io>

bpf/bpf_alignchecker.c
@@ -22,50 +22,42 @@
 #include "lib/maps.h"
 #include "sockops/bpf_sockops.h"
 
+// DECLARE_STRUCT declares a unique usage of the struct 'x' on the stack.
+//
+// To prevent compiler from optimizing away the var, we pass a reference
+// to the var to a BPF helper function which accepts a reference as
+// an argument.
+#define DECLARE_STRUCT(x, iter)			\
+{						\
+	struct x s ## iter = {};		\
+	trace_printk("%p", 1, &s ## iter);	\
+	iter++;					\
+}
+
 // This function is a placeholder for C struct definitions shared with Go, and
 // it's never being executed.
 int main() {
-    struct ipv4_ct_tuple s0 = {};
-    // To prevent compiler from optimizing away the var, we pass a reference
-    // to the var to a BPF helper function which accepts a reference as
-    // an argument.
-    trace_printk("%p", 1, &s0);
-    struct ipv6_ct_tuple s1 = {};
-    trace_printk("%p", 1, &s1);
-    struct ct_entry s2 = {};
-    trace_printk("%p", 1, &s2);
-    struct ipcache_key s3 = {};
-    trace_printk("%p", 1, &s3);
-    struct remote_endpoint_info s4 = {};
-    trace_printk("%p", 1, &s4);
-    struct lb4_key s5 = {};
-    trace_printk("%p", 1, &s5);
-    struct lb4_service s6 = {};
-    trace_printk("%p", 1, &s6);
-    struct lb6_key s7 = {};
-    trace_printk("%p", 1, &s7);
-    struct lb6_service s8 = {};
-    trace_printk("%p", 1, &s8);
-    struct endpoint_key s9 = {};
-    trace_printk("%p", 1, &s9);
-    struct endpoint_info s10 = {};
-    trace_printk("%p", 1, &s10);
-    struct metrics_key s11 = {};
-    trace_printk("%p", 1, &s11);
-    struct metrics_value s12 = {};
-    trace_printk("%p", 1, &s12);
-    struct proxy4_tbl_key s13 = {};
-    trace_printk("%p", 1, &s13);
-    struct proxy4_tbl_value s14 = {};
-    trace_printk("%p", 1, &s14);
-    struct proxy6_tbl_key s15 = {};
-    trace_printk("%p", 1, &s15);
-    struct proxy6_tbl_value s16 = {};
-    trace_printk("%p", 1, &s16);
-    struct sock_key s17 = {};
-    trace_printk("%p", 1, &s17);
-    struct ep_config s18 = {};
-    trace_printk("%p", 1, &s18);
+    int iter = 0;
+
+    DECLARE_STRUCT(ipv4_ct_tuple, iter);
+    DECLARE_STRUCT(ipv6_ct_tuple, iter);
+    DECLARE_STRUCT(ct_entry, iter);
+    DECLARE_STRUCT(ipcache_key, iter);
+    DECLARE_STRUCT(remote_endpoint_info, iter);
+    DECLARE_STRUCT(lb4_key, iter);
+    DECLARE_STRUCT(lb4_service, iter);
+    DECLARE_STRUCT(lb6_key, iter);
+    DECLARE_STRUCT(lb6_service, iter);
+    DECLARE_STRUCT(endpoint_key, iter);
+    DECLARE_STRUCT(endpoint_info, iter);
+    DECLARE_STRUCT(metrics_key, iter);
+    DECLARE_STRUCT(metrics_value, iter);
+    DECLARE_STRUCT(proxy4_tbl_key, iter)
+    DECLARE_STRUCT(proxy4_tbl_value, iter);
+    DECLARE_STRUCT(proxy6_tbl_key, iter);
+    DECLARE_STRUCT(proxy6_tbl_value, iter);
+    DECLARE_STRUCT(sock_key, iter);
+    DECLARE_STRUCT(ep_config, iter);
 
     return 0;
 }

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=e8564fbeddf21622b2c36d0b9a5a56a666bcc404
+GO_BINDATA_SHA1SUM=c8ba5cebb412fa7801a6a0c356691be3bd0bcaf6
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

commit a4cdf531c9b92216398ef7c292d3fb881ad8d5bb

alignchecker: fix LLVM 15 build by removing an unused variable
With LLVM 15 build breaks with

    bpf_alignchecker.c:47:6: error: variable 'iter' set but not used [-Werror,-Wunused-but-set-variable]
        int iter = 0;
            ^

The iter variable was intended to help to declare structures with names s0, s1,
etc., but instead all the structures were named "siter":

    DECLARE(struct, ipv4_ct_tuple, iter)

expands to

    {
        struct ipv4_ct_tuple siter = {};
        trace_printk("%p", 1, &siter);
        iter++
    }

Remove the unused variable and simplify the DECLARE macro to take only one
argument, so a new type check can be added, e.g., in the following way:

    DECLARE(struct ipv4_ct_tuple)

Fixes: 006ebebc28e ("bpf: Adjust alignchecker to avoid stack limits")

Signed-off-by: Anton Protopopov <a.s.protopopov@gmail.com>

bpf/bpf_alignchecker.c
@@ -33,11 +33,10 @@
  * to the var to a BPF helper function which accepts a reference as
  * an argument.
  */
-#define DECLARE(datatype, x, iter)		\
-{						\
-	datatype x s ## iter = {};		\
-	trace_printk("%p", 1, &s ## iter);	\
-	iter++;					\
+#define DECLARE(type)			\
+{					\
+	type s = {};			\
+	trace_printk("%p", 1, &s);	\
 }
 
 /* This function is a placeholder for C struct definitions shared with Go,
@@ -45,55 +44,53 @@
  */
 int main(void)
 {
-	int iter = 0;
-
-	DECLARE(struct, ipv4_ct_tuple, iter);
-	DECLARE(struct, ipv6_ct_tuple, iter);
-	DECLARE(struct, ct_entry, iter);
-	DECLARE(struct, ipcache_key, iter);
-	DECLARE(struct, remote_endpoint_info, iter);
-	DECLARE(struct, lb4_key, iter);
-	DECLARE(struct, lb4_service, iter);
-	DECLARE(struct, lb4_backend, iter);
-	DECLARE(struct, lb6_key, iter);
-	DECLARE(struct, lb6_service, iter);
-	DECLARE(struct, lb6_backend, iter);
-	DECLARE(struct, endpoint_key, iter);
-	DECLARE(struct, endpoint_info, iter);
-	DECLARE(struct, metrics_key, iter);
-	DECLARE(struct, metrics_value, iter);
-	DECLARE(struct, sock_key, iter);
-	DECLARE(struct, policy_key, iter);
-	DECLARE(struct, policy_entry, iter);
-	DECLARE(struct, ipv4_nat_entry, iter);
-	DECLARE(struct, ipv6_nat_entry, iter);
-	DECLARE(struct, trace_notify, iter);
-	DECLARE(struct, drop_notify, iter);
-	DECLARE(struct, policy_verdict_notify, iter);
-	DECLARE(struct, debug_msg, iter);
-	DECLARE(struct, debug_capture_msg, iter);
-	DECLARE(struct, ipv4_revnat_tuple, iter);
-	DECLARE(struct, ipv4_revnat_entry, iter);
-	DECLARE(struct, ipv6_revnat_tuple, iter);
-	DECLARE(struct, ipv6_revnat_entry, iter);
-	DECLARE(struct, ipv4_frag_id, iter);
-	DECLARE(struct, ipv4_frag_l4ports, iter);
-	DECLARE(union, macaddr, iter);
-	DECLARE(struct, lb4_affinity_key, iter);
-	DECLARE(struct, lb6_affinity_key, iter);
-	DECLARE(struct, lb_affinity_val, iter);
-	DECLARE(struct, lb_affinity_match, iter);
-	DECLARE(struct, lb4_src_range_key, iter);
-	DECLARE(struct, lb6_src_range_key, iter);
-	DECLARE(struct, edt_id, iter);
-	DECLARE(struct, edt_info, iter);
-	DECLARE(struct, egress_gw_policy_key, iter);
-	DECLARE(struct, egress_gw_policy_entry, iter);
-	DECLARE(struct, vtep_key, iter);
-	DECLARE(struct, vtep_value, iter);
-	DECLARE(struct, capture4_wcard, iter);
-	DECLARE(struct, capture6_wcard, iter);
-	DECLARE(struct, capture_rule, iter);
+	DECLARE(struct ipv4_ct_tuple);
+	DECLARE(struct ipv6_ct_tuple);
+	DECLARE(struct ct_entry);
+	DECLARE(struct ipcache_key);
+	DECLARE(struct remote_endpoint_info);
+	DECLARE(struct lb4_key);
+	DECLARE(struct lb4_service);
+	DECLARE(struct lb4_backend);
+	DECLARE(struct lb6_key);
+	DECLARE(struct lb6_service);
+	DECLARE(struct lb6_backend);
+	DECLARE(struct endpoint_key);
+	DECLARE(struct endpoint_info);
+	DECLARE(struct metrics_key);
+	DECLARE(struct metrics_value);
+	DECLARE(struct sock_key);
+	DECLARE(struct policy_key);
+	DECLARE(struct policy_entry);
+	DECLARE(struct ipv4_nat_entry);
+	DECLARE(struct ipv6_nat_entry);
+	DECLARE(struct trace_notify);
+	DECLARE(struct drop_notify);
+	DECLARE(struct policy_verdict_notify);
+	DECLARE(struct debug_msg);
+	DECLARE(struct debug_capture_msg);
+	DECLARE(struct ipv4_revnat_tuple);
+	DECLARE(struct ipv4_revnat_entry);
+	DECLARE(struct ipv6_revnat_tuple);
+	DECLARE(struct ipv6_revnat_entry);
+	DECLARE(struct ipv4_frag_id);
+	DECLARE(struct ipv4_frag_l4ports);
+	DECLARE(union macaddr);
+	DECLARE(struct lb4_affinity_key);
+	DECLARE(struct lb6_affinity_key);
+	DECLARE(struct lb_affinity_val);
+	DECLARE(struct lb_affinity_match);
+	DECLARE(struct lb4_src_range_key);
+	DECLARE(struct lb6_src_range_key);
+	DECLARE(struct edt_id);
+	DECLARE(struct edt_info);
+	DECLARE(struct egress_gw_policy_key);
+	DECLARE(struct egress_gw_policy_entry);
+	DECLARE(struct vtep_key);
+	DECLARE(struct vtep_value);
+	DECLARE(struct capture4_wcard);
+	DECLARE(struct capture6_wcard);
+	DECLARE(struct capture_rule);
 
 	return 0;
 }