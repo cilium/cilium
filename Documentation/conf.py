7d30daf013ab0dd86245bca64dd2d438d00d7dca
André Martins <andre@cilium.io>
2022-04-26 15:41:26 +0200

    add robots.txt to Cilium documentation
    It looks that we can't do a Page redirect from 'robots.txt' in
    readthedocs settings. This commit is another attempt of having a
    manually defined robots.txt file.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -189,6 +189,11 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['images', '_static']
 
+# Add any extra paths that contain custom files (such as robots.txt or
+# .htaccess) here, relative to this directory. These files are copied
+# directly to the root of the documentation.
+html_extra_path = ['robots']
+
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
