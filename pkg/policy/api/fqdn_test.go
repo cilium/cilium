d171b98b2a02bdf2121b05bd40e0dfe9545a6de7
Chris Tarazi <chris@isovalent.com>
2022-04-25 17:39:09 -0700

    pkg/policy/api: Optimize FQDNSelector String()
    [ upstream commit 4c2c2446577d92fb15f0314efa487d77a6ce0812 ]
    
    Use strings.Builder instead of fmt.Sprintf() and preallocate the size of
    the string so that Go doesn't need to over-allocate if the string ends
    up longer than what the buffer growth algorithm predicts.
    
    Results:
    
    ```
    $ go test -v -run '^$' -bench 'BenchmarkFQDNSelectorString' -benchmem ./pkg/policy/api > old.txt
    $ go test -v -run '^$' -bench 'BenchmarkFQDNSelectorString' -benchmem ./pkg/policy/api > new.txt
    $ benchcmp old.txt new.txt
    benchmark                         old ns/op     new ns/op     delta
    BenchmarkFQDNSelectorString-8     690           180           -73.97%
    
    benchmark                         old allocs     new allocs     delta
    BenchmarkFQDNSelectorString-8     9              4              -55.56%
    
    benchmark                         old bytes     new bytes     delta
    BenchmarkFQDNSelectorString-8     288           208           -27.78%
    ```
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

--- pkg/policy/api/fqdn_test.go
+++ pkg/policy/api/fqdn_test.go
@@ -7,6 +7,8 @@
 package api
 
 import (
+	"testing"
+
 	. "gopkg.in/check.v1"
 )
 
@@ -63,5 +65,22 @@ func (s *PolicyAPITestSuite) TestPortRuleDNSSanitize(c *C) {
 	} {
 		err := reject.Sanitize()
 		c.Assert(err, Not(IsNil), Commentf("PortRuleDNS %+v was accepted but it should be invalid", reject))
+	}
+}
+
+// TestPortRuleDNSSanitize tests that the sanitizer correctly catches bad
+// cases, and allows good ones.
+func BenchmarkFQDNSelectorString(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, s := range []FQDNSelector{
+			{MatchName: "cilium.io"},
+			{MatchPattern: "[a-z]*.cilium.io"},
+			{MatchName: "a{1,2}.cilium.io", MatchPattern: "[a-z]*.cilium.io"},
+			{MatchPattern: "*.cilium.io"},
+		} {
+			_ = s.String()
+		}
 	}
 }
