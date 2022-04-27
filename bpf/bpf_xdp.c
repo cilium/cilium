cilium/cilium

commit 964c3fb9e609f1f640fb4f4bbe52a4c8f040e3f0

bpf: initial datapath acceleration for nodeport in xdp
Rework the XDP prefilter early drop to now also include NodePort service
handling. Idea is that after prefilter phase, we can accelerate the case
where the NodePort(/ExternalIP) backend is remote and we are forced to
push the packet back out again via SNAT or DSR.

Instead of first crafting an skb, pushing it through GRO engine, packet
taps to eventually end up at tc ingress of the phy device, we can do all
this right in the driver's XDP layer instead at a much higher per packet
rate. This is the case where XDP can really help to help scaling up the
backends. XDP won't be able to help much for the case where the backend
is local to the node, and when we're hitting cases where we need to punt
to remote backends, then can can do so at max the rate that tc ingress
can handle, so moving this down into XDP will improve that limit. At
the same time it will also create less burden given unneeded layers are
bypassed.

All major 10/40G NICs these days support XDP in the upstream kernel. This
is also in particular useful e.g. in cloud-based environments with SRIO-V
networking or on bare metal machines in general.

This work piggy-back on the entire datapath conversion that has been done
for Cilium's BPF code in order to be generic for skb and xdp.

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/bpf_xdp.c
@@ -8,15 +8,14 @@
 #include <netdev_config.h>
 #include <filter_config.h>
 
-#include <linux/if_ether.h>
-
-#define SKIP_CALLS_MAP
+#define SKIP_POLICY_MAP 1
 
 #include "lib/utils.h"
 #include "lib/common.h"
 #include "lib/maps.h"
 #include "lib/eps.h"
 #include "lib/events.h"
+#include "lib/nodeport.h"
 
 #ifndef HAVE_LPM_TRIE_MAP_TYPE
 # undef CIDR4_LPM_PREFILTER
@@ -82,13 +81,35 @@ struct bpf_elf_map __section_maps CIDR6_LMAP_NAME = {
 #endif /* CIDR6_LPM_PREFILTER */
 #endif /* CIDR6_FILTER */
 
-static __always_inline int check_v4_endpoint(struct iphdr *ipv4_hdr)
+#ifdef ENABLE_IPV4
+#ifdef ENABLE_NODEPORT_ACCELERATION
+__section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV4_FROM_LXC)
+int tail_lb_ipv4(struct __ctx_buff *ctx)
 {
-	if (lookup_ip4_endpoint(ipv4_hdr))
-		return CTX_ACT_OK;
+	int ret = CTX_ACT_OK;
 
-	return CTX_ACT_DROP;
+	if (!bpf_skip_nodeport(ctx)) {
+		ret = nodeport_lb4(ctx, 0);
+		if (IS_ERR(ret))
+			return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
+						      METRIC_INGRESS);
+	}
+
+	return ret;
 }
+
+static __always_inline int check_v4_lb(struct __ctx_buff *ctx)
+{
+	ep_tail_call(ctx, CILIUM_CALL_IPV4_FROM_LXC);
+	return send_drop_notify_error(ctx, 0, DROP_MISSED_TAIL_CALL, CTX_ACT_DROP,
+				      METRIC_INGRESS);
+}
+#else
+static __always_inline int check_v4_lb(struct __ctx_buff *ctx __maybe_unused)
+{
+	return CTX_ACT_OK;
+}
+#endif /* ENABLE_NODEPORT_ACCELERATION */
 
 static __always_inline int check_v4(struct __ctx_buff *ctx)
 {
@@ -110,19 +131,42 @@ static __always_inline int check_v4(struct __ctx_buff *ctx)
 	else
 #endif /* CIDR4_LPM_PREFILTER */
 		return map_lookup_elem(&CIDR4_HMAP_NAME, &pfx) ?
-		       CTX_ACT_DROP : check_v4_endpoint(ipv4_hdr);
+		       CTX_ACT_DROP : check_v4_lb(ctx);
 #else
-	return check_v4_endpoint(ipv4_hdr);
+	return check_v4_lb(ctx);
 #endif /* CIDR4_FILTER */
 }
+#endif /* ENABLE_IPV4 */
 
-static __always_inline int check_v6_endpoint(struct ipv6hdr *ipv6_hdr)
+#ifdef ENABLE_IPV6
+#ifdef ENABLE_NODEPORT
+__section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV6_FROM_LXC)
+int tail_lb_ipv6(struct __ctx_buff *ctx)
 {
-	if (lookup_ip6_endpoint(ipv6_hdr))
-		return CTX_ACT_OK;
+	int ret = CTX_ACT_OK;
 
-	return CTX_ACT_DROP;
+	if (!bpf_skip_nodeport(ctx)) {
+		ret = nodeport_lb6(ctx, 0);
+		if (IS_ERR(ret))
+			return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
+						      METRIC_INGRESS);
+	}
+
+	return ret;
 }
+
+static __always_inline int check_v6_lb(struct __ctx_buff *ctx)
+{
+	ep_tail_call(ctx, CILIUM_CALL_IPV6_FROM_LXC);
+	return send_drop_notify_error(ctx, 0, DROP_MISSED_TAIL_CALL, CTX_ACT_DROP,
+				      METRIC_INGRESS);
+}
+#else
+static __always_inline int check_v6_lb(struct __ctx_buff *ctx __maybe_unused)
+{
+	return CTX_ACT_OK;
+}
+#endif /* ENABLE_NODEPORT */
 
 static __always_inline int check_v6(struct __ctx_buff *ctx)
 {
@@ -144,36 +188,45 @@ static __always_inline int check_v6(struct __ctx_buff *ctx)
 	else
 #endif /* CIDR6_LPM_PREFILTER */
 		return map_lookup_elem(&CIDR6_HMAP_NAME, &pfx) ?
-		       CTX_ACT_DROP : check_v6_endpoint(ipv6_hdr);
+		       CTX_ACT_DROP : check_v6_lb(ctx);
 #else
-	return check_v6_endpoint(ipv6_hdr);
+	return check_v6_lb(ctx);
 #endif /* CIDR6_FILTER */
 }
+#endif /* ENABLE_IPV6 */
 
 static __always_inline int check_filters(struct __ctx_buff *ctx)
 {
-	void *data_end = ctx_data_end(ctx);
-	void *data = ctx_data(ctx);
-	struct ethhdr *eth = data;
+	int ret = CTX_ACT_OK;
 	__u16 proto;
 
-	if (ctx_no_room(eth + 1, data_end))
-		return CTX_ACT_DROP;
-
-	proto = eth->h_proto;
-	if (proto == bpf_htons(ETH_P_IP))
-		return check_v4(ctx);
-	else if (proto == bpf_htons(ETH_P_IPV6))
-		return check_v6(ctx);
-	else
-		/* Pass the rest to stack, we might later do more
-		 * fine-grained filtering here.
-		 */
+	if (!validate_ethertype(ctx, &proto))
 		return CTX_ACT_OK;
+	if (ctx_adjust_meta(ctx, -META_PIVOT))
+		return CTX_ACT_OK;
+
+	bpf_skip_nodeport_clear(ctx);
+
+	switch (proto) {
+#ifdef ENABLE_IPV4
+	case bpf_htons(ETH_P_IP):
+		ret = check_v4(ctx);
+		break;
+#endif /* ENABLE_IPV4 */
+#ifdef ENABLE_IPV6
+	case bpf_htons(ETH_P_IPV6):
+		ret = check_v6(ctx);
+		break;
+#endif /* ENABLE_IPV6 */
+	default:
+		break;
+	}
+
+	return ret;
 }
 
 __section("from-netdev")
-int prefilter_start(struct __ctx_buff *ctx)
+int bpf_xdp_entry(struct __ctx_buff *ctx)
 {
 	return check_filters(ctx);
 }

bpf/Makefile
@@ -4,8 +4,8 @@ include ../Makefile.defs
 
 SUBDIRS = sockops
 
-BPF_SIMPLE = bpf_prefilter.o bpf_network.o bpf_alignchecker.o bpf_hostdev_ingress.o
-BPF = bpf_lxc.o bpf_overlay.o bpf_sock.o bpf_netdev.o $(BPF_SIMPLE)
+BPF_SIMPLE = bpf_network.o bpf_alignchecker.o bpf_hostdev_ingress.o
+BPF = bpf_lxc.o bpf_overlay.o bpf_sock.o bpf_netdev.o bpf_xdp.o $(BPF_SIMPLE)
 
 TARGET=cilium-map-migrate
 
@@ -52,7 +52,8 @@ LB_OPTIONS = \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_EXTERNAL_IP \
-	-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_IPSEC:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_EXTERNAL_IP
+	-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_IPSEC:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_EXTERNAL_IP \
+	-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_IPSEC:-DLB_L3:-DLB_L4:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_EXTERNAL_IP:-DENABLE_NODEPORT_ACCELERATION
 
 # These options are intended to max out the BPF program complexity. it is load
 # tested as well.
@@ -122,6 +123,21 @@ bpf_netdev.ll: bpf_netdev.c $(LIB)
 	$(QUIET) ${CLANG} ${MAX_NETDEV_OPTIONS} ${CLANG_FLAGS} -c $< -o $@
 
 bpf_netdev.o: bpf_netdev.ll
+	@$(ECHO_CC)
+	$(QUIET) ${LLC} ${LLC_FLAGS} -filetype=obj -o $@ $(patsubst %.o,%.ll,$@)
+
+bpf_xdp.ll: bpf_xdp.c $(LIB)
+	$(QUIET) set -e; \
+	if [ $(BUILD_PERMUTATIONS) != "" ]; then \
+		$(foreach OPTS,$(NETDEV_OPTIONS), \
+			$(ECHO_CC) " [$(subst :,$(space),$(OPTS))]"; \
+			${CLANG} $(subst :,$(space),$(OPTS)) ${CLANG_FLAGS} -c $< -o $@; \
+			${LLC} ${LLC_FLAGS} -o /dev/null $@; ) \
+	fi
+	@$(ECHO_CC)
+	$(QUIET) ${CLANG} ${MAX_NETDEV_OPTIONS} ${CLANG_FLAGS} -c $< -o $@
+
+bpf_xdp.o: bpf_xdp.ll
 	@$(ECHO_CC)
 	$(QUIET) ${LLC} ${LLC_FLAGS} -filetype=obj -o $@ $(patsubst %.o,%.ll,$@)
 

bpf/init.sh
@@ -292,7 +292,10 @@ function xdp_load()
 	SEC=$6
 	CIDR_MAP=$7
 
-	bpf_compile $IN $OUT obj "$OPTS"
+	NODE_MAC=$(ip link show $DEV | grep ether | awk '{print $2}')
+	NODE_MAC="{.addr=$(mac2array $NODE_MAC)}"
+
+	bpf_compile $IN $OUT obj "$OPTS -DNODE_MAC=${NODE_MAC}"
 
 	ip link set dev $DEV $MODE off
 	rm -f "$CILIUM_BPF_MNT/xdp/globals/$CIDR_MAP" 2> /dev/null || true
@@ -518,7 +521,7 @@ if [ "$MODE" = "vxlan" -o "$MODE" = "geneve" ]; then
 	echo "#define ENCAP_IFINDEX $ENCAP_IDX" >> $RUNDIR/globals/node_config.h
 
 	CALLS_MAP="cilium_calls_overlay_${ID_WORLD}"
-	COPTS="-DSECLABEL=${ID_WORLD}"
+	COPTS="-DSECLABEL=${ID_WORLD} -DFROM_ENCAP_DEV=1"
 	if [ "$NODE_PORT" = "true" ]; then
 		COPTS="${COPTS} -DLB_L3 -DLB_L4 -DDISABLE_LOOPBACK_LB"
 	fi
@@ -642,8 +645,11 @@ fi
 
 if [ "$XDP_DEV" != "<nil>" ]; then
 	CIDR_MAP="cilium_cidr_v*"
-	COPTS=""
-	xdp_load $XDP_DEV $XDP_MODE "$COPTS" bpf_prefilter.c bpf_prefilter.o from-netdev $CIDR_MAP
+	COPTS="-DSECLABEL=${ID_WORLD} -DCALLS_MAP=cilium_calls_xdp"
+	if [ "$NODE_PORT" = "true" ]; then
+		COPTS="${COPTS} -DLB_L3 -DLB_L4 -DDISABLE_LOOPBACK_LB"
+	fi
+	xdp_load $XDP_DEV $XDP_MODE "$COPTS" bpf_xdp.c bpf_xdp.o from-netdev $CIDR_MAP
 fi
 
 # Compile dummy BPF file containing all shared struct definitions used by

bpf/lib/nodeport.h
@@ -73,6 +73,19 @@ struct dsr_opt_v6 {
 };
 #endif /* ENABLE_IPV6 */
 
+static __always_inline bool nodeport_must_drop_remote(void)
+{
+#if defined(ENABLE_NODEPORT_ACCELERATION) && !defined(FROM_ENCAP_DEV) && \
+    __ctx_is == __ctx_skb
+	/* We already handled remote backends on XDP layer. Anything
+	 * landing here that is remote is a bug.
+	 */
+	return true;
+#else
+	return false;
+#endif
+}
+
 static __always_inline bool nodeport_uses_dsr(__u8 nexthdr __maybe_unused)
 {
 # if defined(ENABLE_DSR) && !defined(ENABLE_DSR_HYBRID)
@@ -503,7 +516,8 @@ static __always_inline int nodeport_lb6(struct __ctx_buff *ctx,
 		return DROP_INVALID;
 
 	backend_local = lookup_ip6_endpoint(ip6);
-	if (lb6_svc_is_hostport(svc) && !backend_local)
+	if (!backend_local && (lb6_svc_is_hostport(svc) ||
+			       nodeport_must_drop_remote()))
 		return DROP_INVALID;
 
 	switch (ret) {
@@ -1061,7 +1075,8 @@ static __always_inline int nodeport_lb4(struct __ctx_buff *ctx,
 		return DROP_INVALID;
 
 	backend_local = lookup_ip4_endpoint(ip4);
-	if (lb4_svc_is_hostport(svc) && !backend_local)
+	if (!backend_local && (lb4_svc_is_hostport(svc) ||
+			       nodeport_must_drop_remote()))
 		return DROP_INVALID;
 
 	switch (ret) {

test/bpf/verifier-test.sh
@@ -23,7 +23,7 @@ ALL_TC_PROGS="bpf_hostdev_ingress bpf_lxc bpf_netdev bpf_network bpf_overlay"
 TC_PROGS=${TC_PROGS:-$ALL_TC_PROGS}
 ALL_CG_PROGS="bpf_sock sockops/bpf_sockops sockops/bpf_redir"
 CG_PROGS=${CG_PROGS:-$ALL_CG_PROGS}
-ALL_XDP_PROGS="bpf_prefilter"
+ALL_XDP_PROGS="bpf_xdp"
 XDP_PROGS=${XDP_PROGS:-$ALL_XDP_PROGS}
 IGNORED_PROGS="bpf_alignchecker"
 ALL_PROGS="${IGNORED_PROGS} ${ALL_CG_PROGS} ${ALL_TC_PROGS} ${ALL_XDP_PROGS}"
@@ -237,7 +237,7 @@ function load_xdp {
 			load_prog_dev "$IPROUTE2 link set" "xdpgeneric" ${p}
 		done
 	else
-		echo "=> Skipping ${DIR}/bpf_prefilter.c."
+		echo "=> Skipping ${DIR}/bpf_xdp.c."
 		echo "Ensure you have linux >= 4.12 and recent iproute2 to test XDP." 1>&2
 	fi
 }

commit 2b989a59088f16591b25f6ac26022fad9fd8d24e

bpf: fix selection of Nodeport XDP accel for IPv6
Fix a s/ENABLE_NODEPORT/ENABLE_NODEPORT_ACCELERATION typo, so that the
XDP-based acceleration for IPv6 Nodeport only gets compiled when selected.
This matches the IPv4 path.

Only makes a difference if the XDP program gets loaded for some other
feature. Right now that would only be XDP-based CIDR filtering.

Fixes: 964c3fb9e609 ("bpf: initial datapath acceleration for nodeport in xdp")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/bpf_xdp.c
@@ -182,7 +182,7 @@ static __always_inline int check_v4(struct __ctx_buff *ctx)
 #endif /* ENABLE_IPV4 */
 
 #ifdef ENABLE_IPV6
-#ifdef ENABLE_NODEPORT
+#ifdef ENABLE_NODEPORT_ACCELERATION
 __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV6_FROM_LXC)
 int tail_lb_ipv6(struct __ctx_buff *ctx)
 {
@@ -209,7 +209,7 @@ static __always_inline int check_v6_lb(struct __ctx_buff *ctx __maybe_unused)
 {
 	return CTX_ACT_OK;
 }
-#endif /* ENABLE_NODEPORT */
+#endif /* ENABLE_NODEPORT_ACCELERATION */
 
 #ifdef ENABLE_PREFILTER
 static __always_inline int check_v6(struct __ctx_buff *ctx)