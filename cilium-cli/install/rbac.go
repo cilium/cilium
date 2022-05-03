627e204257f83025598d6b119b5c6a1c8cad12d9
Tam Mach <tam.mach@cilium.io>
2022-05-03 22:58:22 +1000

    ingress: Add steps to create role and rolebinding
    This is to create role and role binding resources for both agent and
    operator.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- cilium-cli/install/rbac.go
+++ cilium-cli/install/rbac.go
@@ -4,9 +4,10 @@
 package install
 
 import (
-	"github.com/cilium/cilium/pkg/versioncheck"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+
+	"github.com/cilium/cilium/pkg/versioncheck"
 
 	"github.com/cilium/cilium-cli/defaults"
 	"github.com/cilium/cilium-cli/internal/utils"
@@ -99,5 +100,56 @@ func (k *K8sInstaller) NewClusterRoleBinding(crbName string) *rbacv1.ClusterRole
 
 	var crb rbacv1.ClusterRoleBinding
 	utils.MustUnmarshalYAML([]byte(crbFile), &crb)
+	return &crb
+}
+
+func (k *K8sInstaller) NewRole(name string) *rbacv1.Role {
+	var (
+		roleFileName string
+	)
+
+	ciliumVer := k.getCiliumVersion()
+	switch {
+	case versioncheck.MustCompile(">1.11.99")(ciliumVer):
+		switch name {
+		case defaults.AgentSecretsRoleName:
+			roleFileName = "templates/cilium-agent/role.yaml"
+		case defaults.OperatorSecretsRoleName:
+			roleFileName = "templates/cilium-operator/role.yaml"
+		}
+	}
+
+	rFile, exists := k.manifests[roleFileName]
+	if !exists {
+		return nil
+	}
+
+	var cr rbacv1.Role
+	utils.MustUnmarshalYAML([]byte(rFile), &cr)
+	return &cr
+}
+
+func (k *K8sInstaller) NewRoleBinding(crbName string) *rbacv1.RoleBinding {
+	var (
+		rbFileName string
+	)
+
+	ciliumVer := k.getCiliumVersion()
+	switch {
+	case versioncheck.MustCompile(">1.11.99")(ciliumVer):
+		switch crbName {
+		case defaults.AgentSecretsRoleName:
+			rbFileName = "templates/cilium-agent/rolebinding.yaml"
+		case defaults.OperatorSecretsRoleName:
+			rbFileName = "templates/cilium-operator/rolebinding.yaml"
+		}
+	}
+
+	rbFile, exists := k.manifests[rbFileName]
+	if !exists {
+		return nil
+	}
+	var crb rbacv1.RoleBinding
+	utils.MustUnmarshalYAML([]byte(rbFile), &crb)
 	return &crb
 }
