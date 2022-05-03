cilium/cilium

commit 74ef715769cb6345fad59e3fe39403f134feb163

contrib: Make KIND cluster ipFamily configurable
Not so long ago we forcefully enabled the dual stack on KIND by setting "ipFamily:
dual". As reported by Sebastian, this broke when running KIND with
kube-proxy:

    unable to create proxier: invalid CIDR address: 10.244.0.0/16,fd00:10:244::/56

Fix this by defaulting to the ipv4 family, and allow a user to change
it (e.g., when running the K8sServices test suite which assumes that
KIND cluster runs with the dual stack enabled).

Reported-by: Sebastian Wicki <sebastian@isovalent.com>
Signed-off-by: Martynas Pumputis <m@lambda.lt>

contrib/scripts/kind.sh
@@ -9,6 +9,7 @@ default_workers=1
 default_cluster_name=""
 default_image=""
 default_kubeproxy_mode="iptables"
+default_ipfamily="ipv4"
 
 PROG=${0}
 CONTROLPLANES="${1:-${default_controlplanes}}"
@@ -17,6 +18,7 @@ CLUSTER_NAME="${3:-${default_cluster_name}}"
 # IMAGE controls the K8s version as well (e.g. kindest/node:v1.11.10)
 IMAGE="${4:-${default_image}}"
 KUBEPROXY_MODE="${5:-${default_kubeproxy_mode}}"
+IPFAMILY="${6:-${default_ipfamily}}"
 CILIUM_ROOT="$(realpath $(dirname $(readlink -ne $BASH_SOURCE))/../..)"
 
 usage() {
@@ -91,7 +93,7 @@ $(workers)
 networking:
   disableDefaultCNI: true
   kubeProxyMode: ${KUBEPROXY_MODE}
-  ipFamily: dual
+  ipFamily: ${IPFAMILY}
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]

commit 858af15258e4fd26e7b2d5ecefae54f3d8787ad7

contrib: Fix passing ipFamily to kind.sh
Previously, the script was bailing out with the error when the ipFamily
was specified.

Fixes: 74ef715769 ("contrib: Make KIND cluster ipFamily configurable")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

contrib/scripts/kind.sh
@@ -22,7 +22,7 @@ IPFAMILY="${6:-${default_ipfamily}}"
 CILIUM_ROOT="$(realpath $(dirname $(readlink -ne $BASH_SOURCE))/../..)"
 
 usage() {
-  echo "Usage: ${PROG} [control-plane node count] [worker node count] [cluster-name] [node image] [kube-proxy mode]"
+  echo "Usage: ${PROG} [control-plane node count] [worker node count] [cluster-name] [node image] [kube-proxy mode] [ip-family]"
 }
 
 have_kind() {
@@ -34,12 +34,12 @@ if ! have_kind; then
     echo "  https://kind.sigs.k8s.io/docs/user/quick-start/#installation"
 fi
 
-if [[ "${#}" -gt 5 ]]; then
+if [[ "${#}" -gt 6 ]]; then
   usage
   exit 1
 fi
 
-if [[ "${#}" -gt 5 ]] ||
+if [[ "${#}" -gt 6 ]] ||
    [[ "${CONTROLPLANES}" == "-h" ||
       "${CONTROLPLANES}" == "--help" ]]; then
   usage