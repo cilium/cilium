db8016a888e41e849df04d0908ef592b99e8cd68
MikeLing <sabergeass@gmail.com>
2022-05-05 08:06:01 +0000

    pkg/policy/api/decision: Optimize decision MarshalJSON()
    Use strings.Builder instead of fmt.Sprintf() and preallocate the size of
    the string so that Go doesn't need to over-allocate if the string ends
    up longer than what the buffer growth algorithm predicts.
    
    Result:
    ```
    $ go test -benchmem -run=^$ -bench ^BenchmarkDecisionMarshalJSON$ ./pkg/policy/api > BenchmarkDecisionMarshalJSON_old.txt
    $ go test -benchmem -run=^$ -bench ^BenchmarkDecisionMarshalJSON$ ./pkg/policy/api > BenchmarkDecisionMarshalJSON_new.txt
    $ benchcmp BenchmarkDecisionMarshalJSON_old.txt BenchmarkDecisionMarshalJSON_new.txt
    benchmark                          old ns/op     new ns/op     delta
    BenchmarkDecisionMarshalJSON-4     751           157           -79.13%
    
    benchmark                          old allocs     new allocs     delta
    BenchmarkDecisionMarshalJSON-4     3              3              +0.00%
    
    benchmark                          old bytes     new bytes     delta
    BenchmarkDecisionMarshalJSON-4     40            40            +0.00%
    ```
    
    issue reference : #19571
    
    Signed-off-by: MikeLing <sabergeass@gmail.com>

---
+++ pkg/policy/api/decision_test.go
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+//go:build !privileged_tests
+
+package api
+
+import (
+	"testing"
+)
+
+func BenchmarkDecisionMarshalJSON(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, d := range []Decision{
+			Undecided,
+			Allowed,
+			Denied,
+		} {
+			_, _ = d.MarshalJSON()
+		}
+	}
+}
