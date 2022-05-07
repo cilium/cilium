e63fc95d92e0a818a6a2997b83f467aa663ca786
Gilberto Bertin <jibi@cilium.io>
2022-05-07 18:51:56 +0100

    iptables: refactor InstallNoTrackRules
    No need to pass the ingress parameter to endpointNoTrackRules as we are
    always installing rules for both ingress and egress.
    
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- pkg/datapath/iptables/iptables.go
+++ pkg/datapath/iptables/iptables.go
@@ -726,70 +726,69 @@ func (m *IptablesManager) iptProxyRules(proxyPort uint16, ingress bool, name str
 	return nil
 }
 
-func (m *IptablesManager) endpointNoTrackRules(prog iptablesInterface, cmd string, IP string, port *lb.L4Addr, ingress bool) error {
+func (m *IptablesManager) endpointNoTrackRules(prog iptablesInterface, cmd string, IP string, port *lb.L4Addr) error {
 	protocol := strings.ToLower(port.Protocol)
 	p := strconv.FormatUint(uint64(port.Port), 10)
-	if ingress {
-		if err := prog.runProg([]string{
-			"-t", "raw",
-			cmd, ciliumPreRawChain,
-			"-p", protocol,
-			"-d", IP,
-			"--dport", p,
-			"-j", "CT",
-			"--notrack"}); err != nil {
-			return err
-		}
-		if err := prog.runProg([]string{
-			"-t", "filter",
-			cmd, ciliumInputChain,
-			"-p", protocol,
-			"-d", IP,
-			"--dport",
-			p, "-j",
-			"ACCEPT"}); err != nil {
-			return err
-		}
-		if err := prog.runProg([]string{
-			"-t", "raw",
-			cmd, ciliumOutputRawChain,
-			"-p", protocol,
-			"-d", IP,
-			"--dport", p,
-			"-j", "CT",
-			"--notrack"}); err != nil {
-			return err
-		}
-		if err := prog.runProg([]string{
-			"-t", "filter",
-			cmd, ciliumOutputChain,
-			"-p", protocol,
-			"-d", IP,
-			"--dport", p,
-			"-j", "ACCEPT"}); err != nil {
-			return err
-		}
-	} else {
-		if err := prog.runProg([]string{
-			"-t", "raw",
-			cmd, ciliumOutputRawChain,
-			"-p", protocol,
-			"-s", IP,
-			"--sport", p,
-			"-j", "CT",
-			"--notrack"}); err != nil {
-			return err
-		}
-		if err := prog.runProg([]string{
-			"-t", "filter",
-			cmd, ciliumOutputChain,
-			"-p", protocol,
-			"-s", IP,
-			"--sport", p,
-			"-j", "ACCEPT"}); err != nil {
-			return err
-		}
+
+	if err := prog.runProg([]string{
+		"-t", "raw",
+		cmd, ciliumPreRawChain,
+		"-p", protocol,
+		"-d", IP,
+		"--dport", p,
+		"-j", "CT",
+		"--notrack"}); err != nil {
+		return err
 	}
+	if err := prog.runProg([]string{
+		"-t", "filter",
+		cmd, ciliumInputChain,
+		"-p", protocol,
+		"-d", IP,
+		"--dport",
+		p, "-j",
+		"ACCEPT"}); err != nil {
+		return err
+	}
+	if err := prog.runProg([]string{
+		"-t", "raw",
+		cmd, ciliumOutputRawChain,
+		"-p", protocol,
+		"-d", IP,
+		"--dport", p,
+		"-j", "CT",
+		"--notrack"}); err != nil {
+		return err
+	}
+	if err := prog.runProg([]string{
+		"-t", "filter",
+		cmd, ciliumOutputChain,
+		"-p", protocol,
+		"-d", IP,
+		"--dport", p,
+		"-j", "ACCEPT"}); err != nil {
+		return err
+	}
+	if err := prog.runProg([]string{
+		"-t", "raw",
+		cmd, ciliumOutputRawChain,
+		"-p", protocol,
+		"-s", IP,
+		"--sport", p,
+		"-j", "CT",
+		"--notrack"}); err != nil {
+		return err
+	}
+	if err := prog.runProg([]string{
+		"-t", "filter",
+		cmd, ciliumOutputChain,
+		"-p", protocol,
+		"-s", IP,
+		"--sport", p,
+		"-j", "ACCEPT"}); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -814,10 +813,7 @@ func (m *IptablesManager) InstallNoTrackRules(IP string, port uint16, ipv6 bool)
 	}
 
 	for _, p := range noTrackPorts(port) {
-		if err := m.endpointNoTrackRules(prog, "-A", IP, p, true); err != nil {
-			return err
-		}
-		if err := m.endpointNoTrackRules(prog, "-A", IP, p, false); err != nil {
+		if err := m.endpointNoTrackRules(prog, "-A", IP, p); err != nil {
 			return err
 		}
 	}
@@ -842,10 +838,7 @@ func (m *IptablesManager) RemoveNoTrackRules(IP string, port uint16, ipv6 bool) 
 	}
 
 	for _, p := range noTrackPorts(port) {
-		if err := m.endpointNoTrackRules(prog, "-D", IP, p, true); err != nil {
-			return err
-		}
-		if err := m.endpointNoTrackRules(prog, "-D", IP, p, false); err != nil {
+		if err := m.endpointNoTrackRules(prog, "-D", IP, p); err != nil {
 			return err
 		}
 	}
