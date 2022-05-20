cilium/cilium

commit a4aac43d1ed7f37eb34a776b30430425d71f4698

bpf: Support hostfw with endpoint routes
When per-endpoint routes are enabled, packets between the hostns and the
pods do not flow through cilium_host and therefore skip the host
firewall. To fix this we need to tail call into the host policy
enforcement code from bpf_lxc.

To that end, we can reuse the policy map which contains mappings
endpoint ID -> bpf_lxc program for all endpoints except the host
endpoint. We first define a new BPF program in bpf_host that will be the
target of the policy map and tail calls. We then populate the policy map
with the appropriate mapping and tail call from bpf_lxc on egress to the
hostns and ingress from the hostns.

Contrary to the tail called programs in bpf_lxc, our new BPF program
must handle both ingress and egress of the hostns. We distinguish
between the two cases with the metadata bit CB_FROM_HOST.

To populate the policy map, we need to set the MAC address for the host
endpoint or we end up with:

    Exposing new BPF failed: invalid LXC MAC: invalid MAC address

Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/bpf_host.c
@@ -11,6 +11,11 @@
 
 #define EVENT_SOURCE HOST_EP_ID
 
+/* Host endpoint ID for the template bpf_host object file. Will be replaced
+ * at compile-time with the proper host endpoint ID.
+ */
+#define TEMPLATE_HOST_EP_ID 0xffff
+
 /* These are configuration options which have a default value in their
  * respective header files and must thus be defined beforehand:
  */
@@ -1127,5 +1132,121 @@ out:
 
 	return ret;
 }
+
+#if defined(ENABLE_HOST_FIREWALL) && !defined(ENABLE_ROUTING)
+static __always_inline int
+/* Handles packet from a local endpoint entering the host namespace. Applies
+ * ingress host policies.
+ */
+to_host_from_lxc(struct __ctx_buff *ctx __maybe_unused)
+{
+	int ret = CTX_ACT_OK;
+	__u32 src_id = 0;
+	__u16 proto = 0;
+
+	if (!validate_ethertype(ctx, &proto)) {
+		ret = DROP_UNSUPPORTED_L2;
+		goto out;
+	}
+
+	switch (proto) {
+# if defined ENABLE_ARP_PASSTHROUGH || defined ENABLE_ARP_RESPONDER
+	case bpf_htons(ETH_P_ARP):
+		ret = CTX_ACT_OK;
+		break;
+# endif
+# ifdef ENABLE_IPV6
+	case bpf_htons(ETH_P_IPV6):
+		ret = ipv6_host_policy_ingress(ctx, &src_id);
+		break;
+# endif
+# ifdef ENABLE_IPV4
+	case bpf_htons(ETH_P_IP):
+		ret = ipv4_host_policy_ingress(ctx, &src_id);
+		break;
+# endif
+	default:
+		ret = DROP_UNKNOWN_L3;
+		break;
+	}
+
+out:
+	if (IS_ERR(ret))
+		return send_drop_notify_error(ctx, src_id, ret, CTX_ACT_DROP,
+					      METRIC_INGRESS);
+	return ret;
+}
+
+/* Handles packets that left the host namespace and will enter a local
+ * endpoint's namespace. Applies egress host policies before handling
+ * control back to bpf_lxc.
+ */
+static __always_inline int
+from_host_to_lxc(struct __ctx_buff *ctx)
+{
+	int ret = CTX_ACT_OK;
+	__u16 proto = 0;
+
+	if (!validate_ethertype(ctx, &proto))
+		return DROP_UNSUPPORTED_L2;
+
+	switch (proto) {
+# if defined ENABLE_ARP_PASSTHROUGH || defined ENABLE_ARP_RESPONDER
+	case bpf_htons(ETH_P_ARP):
+		ret = CTX_ACT_OK;
+		break;
+# endif
+# ifdef ENABLE_IPV6
+	case bpf_htons(ETH_P_IPV6):
+		ret = ipv6_host_policy_egress(ctx, HOST_ID);
+		break;
+# endif
+# ifdef ENABLE_IPV4
+	case bpf_htons(ETH_P_IP):
+		/* The last parameter, ipcache_srcid, is only required when
+		 * the src_id is not HOST_ID. For details, see
+		 * whitelist_snated_egress_connections.
+		 * We only arrive here from bpf_lxc if we know the
+		 * src_id is HOST_ID. Therefore, we don't need to pass a value
+		 * for the last parameter. That avoids an ipcache lookup.
+		 */
+		ret = ipv4_host_policy_egress(ctx, HOST_ID, 0);
+		break;
+# endif
+	default:
+		ret = DROP_UNKNOWN_L3;
+		break;
+	}
+
+	return ret;
+}
+
+/* When per-endpoint routes are enabled, packets to and from local endpoints
+ * will tail call into this program to enforce egress and ingress host policies.
+ * Packets to the local endpoints will then tail call back to the original
+ * bpf_lxc program.
+ */
+__section_tail(CILIUM_MAP_POLICY, TEMPLATE_HOST_EP_ID)
+handle_lxc_traffic(struct __ctx_buff *ctx)
+{
+	bool from_host = ctx_load_meta(ctx, CB_FROM_HOST);
+	__u32 lxc_id;
+	int ret;
+
+	if (from_host) {
+		ret = from_host_to_lxc(ctx);
+		if (IS_ERR(ret))
+			return send_drop_notify_error(ctx, HOST_ID, ret, CTX_ACT_DROP,
+						      METRIC_EGRESS);
+
+		lxc_id = ctx_load_meta(ctx, CB_DST_ENDPOINT_ID);
+		ctx_store_meta(ctx, CB_SRC_LABEL, HOST_ID);
+		tail_call_dynamic(ctx, &POLICY_CALL_MAP, lxc_id);
+		return DROP_MISSED_TAIL_CALL;
+	}
+
+	return to_host_from_lxc(ctx);
+}
+#endif /* ENABLE_HOST_FIREWALL && !ENABLE_ROUTING */
 
 BPF_LICENSE("GPL");

bpf/bpf_lxc.c
@@ -333,6 +333,17 @@ ct_recreate6:
 		}
 	}
 
+#if defined(ENABLE_HOST_FIREWALL) && !defined(ENABLE_ROUTING)
+	/* If the destination is the local host and per-endpoint routes are
+	 * enabled, jump to the bpf_host program to enforce ingress host policies.
+	 */
+	if (*dstID == HOST_ID) {
+		ctx_store_meta(ctx, CB_FROM_HOST, 0);
+		tail_call_static(ctx, &POLICY_CALL_MAP, HOST_EP_ID);
+		return DROP_MISSED_TAIL_CALL;
+	}
+#endif /* ENABLE_HOST_FIREWALL && !ENABLE_ROUTING */
+
 	/* The packet goes to a peer not managed by this agent instance */
 #ifdef ENCAP_IFINDEX
 	{
@@ -735,6 +746,17 @@ ct_recreate4:
 						   ep, METRIC_EGRESS, false);
 		}
 	}
+
+#if defined(ENABLE_HOST_FIREWALL) && !defined(ENABLE_ROUTING)
+	/* If the destination is the local host and per-endpoint routes are
+	 * enabled, jump to the bpf_host program to enforce ingress host policies.
+	 */
+	if (*dstID == HOST_ID) {
+		ctx_store_meta(ctx, CB_FROM_HOST, 0);
+		tail_call_static(ctx, &POLICY_CALL_MAP, HOST_EP_ID);
+		return DROP_MISSED_TAIL_CALL;
+	}
+#endif /* ENABLE_HOST_FIREWALL && !ENABLE_ROUTING */
 
 #ifdef ENABLE_EGRESS_GATEWAY
 	{
@@ -1646,6 +1668,19 @@ int handle_to_container(struct __ctx_buff *ctx)
 
 	send_trace_notify(ctx, trace, identity, 0, 0,
 			  ctx->ingress_ifindex, 0, TRACE_PAYLOAD_LEN);
+
+#if defined(ENABLE_HOST_FIREWALL) && !defined(ENABLE_ROUTING)
+	/* If the packet comes from the hostns and per-endpoint routes are enabled,
+	 * jump to bpf_host to enforce egress host policies before anything else.
+	 * We will jump back to bpf_lxc once host policies are enforced.
+	 */
+	if (identity == HOST_ID) {
+		ctx_store_meta(ctx, CB_FROM_HOST, 1);
+		ctx_store_meta(ctx, CB_DST_ENDPOINT_ID, LXC_ID);
+		tail_call_static(ctx, &POLICY_CALL_MAP, HOST_EP_ID);
+		return DROP_MISSED_TAIL_CALL;
+	}
+#endif /* ENABLE_HOST_FIREWALL && !ENABLE_ROUTING */
 
 	ctx_store_meta(ctx, CB_SRC_LABEL, identity);
 

bpf/lib/common.h
@@ -562,6 +562,7 @@ enum {
 #define	CB_HINT			CB_SRC_LABEL	/* Alias, non-overlapping */
 #define	CB_PROXY_MAGIC		CB_SRC_LABEL	/* Alias, non-overlapping */
 #define	CB_ENCRYPT_MAGIC	CB_SRC_LABEL	/* Alias, non-overlapping */
+#define	CB_DST_ENDPOINT_ID	CB_SRC_LABEL    /* Alias, non-overlapping */
 	CB_IFINDEX,
 #define	CB_ADDR_V4		CB_IFINDEX	/* Alias, non-overlapping */
 #define	CB_ADDR_V6_1		CB_IFINDEX	/* Alias, non-overlapping */

pkg/datapath/loader/template.go
@@ -179,9 +179,13 @@ func elfMapSubstitutions(ep datapath.Endpoint) map[string]string {
 		}
 	}
 
-	if !ep.IsHost() {
+	// The policy map is only used for the host endpoint is per-endpoint
+	// routes and the host firewall are enabled.
+	if !ep.IsHost() ||
+		(option.Config.EnableEndpointRoutes && option.Config.EnableHostFirewall) {
 		result[policymap.CallString(templateLxcID)] = policymap.CallString(epID)
 	}
+
 	return result
 }
 

pkg/endpoint/bpf.go
@@ -643,7 +643,8 @@ func (e *Endpoint) regenerateBPF(regenContext *regenerationContext) (revnum uint
 		return datapathRegenCtxt.epInfoCache.revision, compilationExecuted, err
 	}
 
-	if !datapathRegenCtxt.epInfoCache.IsHost() {
+	if !datapathRegenCtxt.epInfoCache.IsHost() ||
+		(option.Config.EnableHostFirewall && option.Config.EnableEndpointRoutes) {
 		// Hook the endpoint into the endpoint and endpoint to policy tables then expose it
 		stats.mapSync.Start()
 		epErr := eppolicymap.WriteEndpoint(datapathRegenCtxt.epInfoCache, e.policyMap)

pkg/endpoint/endpoint.go
@@ -488,6 +488,7 @@ func CreateHostEndpoint(owner regeneration.Owner, proxy EndpointProxy, allocator
 
 	ep := createEndpoint(owner, proxy, allocator, 0, ifName)
 	ep.isHost = true
+	ep.mac = mac
 	ep.nodeMAC = mac
 	ep.DatapathConfiguration = NewDatapathConfiguration()
 

commit baa76198485b79862777034ef3a17a57cb0c7649

bpf: specify handle_lxc_traffic return type to fix -Wimplicit-int error
This fixes the following error when building with clang 15:

    bpf_host.c:1414:1: error: type specifier missing, defaults to 'int' [-Werror,-Wimplicit-int]
    handle_lxc_traffic(struct __ctx_buff *ctx)
    ^
    1 error generated.

Fixes: a4aac43d1ed7 ("bpf: Support hostfw with endpoint routes")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

bpf/bpf_host.c
@@ -1411,7 +1411,7 @@ from_host_to_lxc(struct __ctx_buff *ctx)
  * bpf_lxc program.
  */
 __section_tail(CILIUM_MAP_POLICY, TEMPLATE_HOST_EP_ID)
-handle_lxc_traffic(struct __ctx_buff *ctx)
+int handle_lxc_traffic(struct __ctx_buff *ctx)
 {
 	bool from_host = ctx_load_meta(ctx, CB_FROM_HOST);
 	__u32 lxc_id;