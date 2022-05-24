cilium/cilium

commit cf387ce50583b00d6fe6eacd463f9018e8f6bb59

fqdn: Introduce TTL-aware cache for DNS retention
The DNS cache holds the most recent DNS lookup for every IP. This allows
us to keep the list of entries small, since one entry will likely
provide multiple IPs. Entries that expire later are preferred, and the
cache handles TTL expiration per IP entry.
This should support a few scenarios:
- Respect TTL perfectly
- Accumulate all IPs seen for a name for some period, set via the TTL on
  insert
- Use IPs from multiple sources to inform the endpoint policy

Signed-off-by: Ray Bejjani <ray@covalent.io>

pkg/fqdn/cache.go
@@ -1,0 +1,212 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fqdn
+
+import (
+	"bytes"
+	"net"
+	"sort"
+	"time"
+
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+// DefaultDNSCache is a global, shared, DNS cache. It is the default cache used
+// by DNSPoller instances, unless initialized to use another.
+// Note: The DNSCache type returns all DNS information, regardless of source.
+var DefaultDNSCache = NewDNSCache()
+
+// cacheEntry objects hold data passed in via DNSCache.Update, nominally
+// equating to a DNS lookup. They are internal to DNSCache and should not be
+// returned.
+// cacheEntry objects are immutable once created.
+type cacheEntry struct {
+	// Name is a DNS name, it my be not fully qualified (e.g. myservice.namespace)
+	Name string
+
+	// LookupTime is when the data begins being valid
+	LookupTime time.Time
+
+	// ExpirationTime is a calcutated time when the DNS data stops being valid.
+	// It is simply LookupTime + TTL
+	ExpirationTime time.Time
+
+	// TTL represents the number of seconds past LookupTime that this data is
+	// valid.
+	TTL int
+
+	// IPs are the IPs associated with Name for this cacheEntry.
+	IPs []net.IP
+}
+
+// isExpiredBy returns true if entry is no longer valid at pointInTime
+func (entry *cacheEntry) isExpiredBy(pointInTime time.Time) bool {
+	return pointInTime.After(entry.ExpirationTime)
+}
+
+// ipEntries maps a unique IP to the cacheEntry that provides it in .IPs.
+// Multiple IPs may point to the same cacheEntry, or they may all be different.
+// Crucially, an IP may be present in a cacheEntry but the IP in ipEntries
+// points to another cacheEntry. This is because the second cacheEntry has a
+// later expiration for this specific IP, and may not include the other IPs
+// provided by the first entry.
+// The DNS name in the entries is not checked, but is assumed to be the same
+// for all entries.
+// Note: They are guarded by the DNSCache mutex.
+type ipEntries map[string]*cacheEntry
+
+// getIPs returns a sorted list of non-expired unique IPs.
+// This needs a read-lock
+func (s ipEntries) getIPs(now time.Time) []net.IP {
+	ips := make([]net.IP, 0, len(s)) // worst case size
+	for ip, entry := range s {
+		if entry != nil && !entry.isExpiredBy(now) {
+			ips = append(ips, net.ParseIP(ip))
+		}
+	}
+
+	return keepUniqueIPs(ips) // sorts IPs
+}
+
+// DNSCache manages DNS data that will expire after a certain TTL. Information
+// is tracked per-IP address, retaining the latest-expiring DNS data for each
+// address.
+// For most real-world DNS data, the entry per name remains small because newer
+// lookups replace older ones. Large TTLs may cause entries to grow if many
+// unique IPs are returned in separate lookups.
+// Redundant or expired entries are removed on insert.
+// Lookups check for expired entries.
+type DNSCache struct {
+	lock.RWMutex
+
+	// forward DNS lookups name -> IPEntries
+	// IPEntries maps IP -> entry that provides it. An entry may provide multiple IPs.
+	forward map[string]ipEntries
+}
+
+// NewDNSCache returns an initialized DNSCache
+func NewDNSCache() *DNSCache {
+	c := &DNSCache{
+		forward: make(map[string]ipEntries),
+	}
+
+	return c
+}
+
+// Update inserts a new entry into the cache.
+// After insertion cache entries for name are expired and redundant entries
+// evicted. This is O(number of new IPs) for eviction, and O(number of IPs for
+// name) for expiration.
+// lookupTime is the time the DNS information began being valid. It should be
+// in the past.
+// name is used as is and may be an unqualified name (e.g. myservice.namespace).
+// ips may be an IPv4 or IPv6 IP. Duplicates will be removed.
+// ttl is the DNS TTL for ips and is a seconds value.
+func (c *DNSCache) Update(lookupTime time.Time, name string, ips []net.IP, ttl int) {
+	entry := &cacheEntry{
+		Name:           name,
+		LookupTime:     lookupTime,
+		ExpirationTime: lookupTime.Add(time.Duration(ttl) * time.Second),
+		TTL:            ttl,
+		IPs:            ips,
+	}
+
+	c.Lock()
+	defer c.Unlock()
+
+	entries, exists := c.forward[name]
+	if !exists {
+		entries = make(map[string]*cacheEntry)
+		c.forward[name] = entries
+	}
+	c.updateWithEntryIPs(entries, entry)
+	// When lookupTime is much earlier than time.Now(), we may not expire all
+	// entries that should be expired, leaving more work for .Lookup.
+	c.removeExpired(entries, time.Now())
+}
+
+// Lookup returns a set of unique IPs that are currently unexpired for name, if
+// any exist. An empty list indicates no valid records exist. The IPs are
+// returned sorted.
+func (c *DNSCache) Lookup(name string) (ips []net.IP) {
+	c.RLock()
+	defer c.RUnlock()
+
+	return c.lookupByTime(time.Now(), name)
+}
+
+// lookupByTime takes a timestamp for expiration comparisions, and is only
+// intended for testing.
+func (c *DNSCache) lookupByTime(now time.Time, name string) (ips []net.IP) {
+	entries, found := c.forward[name]
+	if !found {
+		return nil
+	}
+
+	return entries.getIPs(now)
+}
+
+// updateWithEntry adds a mapping for every IP found in `entry` to `ipEntries`
+// (which maps IP -> cacheEntry). It will replace existing IP->old mappings in
+// `entries` if the current entry expires sooner (or has already expired).
+// This needs a write lock
+func (c *DNSCache) updateWithEntryIPs(entries ipEntries, entry *cacheEntry) {
+	for _, ip := range entry.IPs {
+		ipStr := ip.String()
+		old, exists := entries[ipStr]
+		if old == nil || !exists || old.isExpiredBy(entry.ExpirationTime) {
+			entries[ipStr] = entry
+		}
+	}
+}
+
+// removeExpired removes expired (or nil) cacheEntry pointers from entries, an
+// ipEntries for a specific name.
+// This needs a write lock
+func (c *DNSCache) removeExpired(entries ipEntries, now time.Time) {
+	for ip, entry := range entries {
+		if entry == nil || entry.isExpiredBy(now) {
+			delete(entries, ip)
+		}
+	}
+}
+
+// keepUniqueIPs transforms the provided multiset of IPs into a single set,
+// lexicographically sorted via a byte-wise comparison of the IP slices (i.e.
+// IPv4 addresses show up before IPv6).
+// The slice is manipulated in-place destructively.
+//
+// 1- Sort the slice by comparing the IPs as bytes
+// 2- For every unseen unique IP in the sorted slice, move it to the start of
+// the return slice.
+// Note that the slice is always large enough and, because it is sorted, we
+// will not overwrite a valid element with another. To overwrite an element i
+// with j, i must have come before j AND we decided it was a duplicate of the
+// element at i-1.
+func keepUniqueIPs(ips []net.IP) []net.IP {
+	sort.Slice(ips, func(i, j int) bool {
+		return bytes.Compare(ips[i], ips[j]) == -1
+	})
+
+	returnIPs := ips[:0] // len==0 but cap==cap(ips)
+	for readIdx, ip := range ips {
+		if len(returnIPs) == 0 ||
+			bytes.Compare(returnIPs[len(returnIPs)-1], ips[readIdx]) != 0 {
+			returnIPs = append(returnIPs, ip)
+		}
+	}
+
+	return returnIPs
+}

pkg/fqdn/cache_test.go
@@ -1,0 +1,253 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fqdn
+
+import (
+	"fmt"
+	"math/rand"
+	"net"
+	"sort"
+	"time"
+
+	. "gopkg.in/check.v1"
+)
+
+type DNSCacheTestSuite struct{}
+
+var _ = Suite(&DNSCacheTestSuite{})
+
+func sortByName(entries []*cacheEntry) {
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].Name < entries[j].Name
+	})
+}
+
+// TestKeepUniqueIPs tests that KeepUniqueIPs returns a slice with only the unique IPs
+func (ds *DNSCacheTestSuite) TestKeepUniqueIPs(c *C) {
+	// test nil/empty handling
+	ips := keepUniqueIPs(nil)
+	c.Assert(len(ips), Equals, 0, Commentf("Non-empty slice returned with empty input"))
+
+	// test all duplicate
+	ips = keepUniqueIPs([]net.IP{net.ParseIP("1.1.1.1"), net.ParseIP("1.1.1.1"), net.ParseIP("1.1.1.1")})
+	c.Assert(len(ips), Equals, 1, Commentf("Too many IPs returned for only 1 unique"))
+	c.Assert(ips[0].String(), Equals, "1.1.1.1", Commentf("Incorrect unique IP returned"))
+
+	// test all unique
+	ipSource := []net.IP{net.ParseIP("1.1.1.1"), net.ParseIP("2.2.2.2"), net.ParseIP("3.3.3.3")}
+	ips = keepUniqueIPs(ipSource)
+	c.Assert(len(ips), Equals, 3, Commentf("Too few IPs returned for only 3 uniques"))
+	for i := range ipSource {
+		c.Assert(ips[i].String(), Equals, ipSource[i].String(), Commentf("Incorrect unique IP returned"))
+	}
+
+	// test mixed
+	ipSource = []net.IP{net.ParseIP("1.1.1.1"), net.ParseIP("2.2.2.2"), net.ParseIP("3.3.3.3"), net.ParseIP("2.2.2.2")}
+	ips = keepUniqueIPs(ipSource)
+	c.Assert(len(ips), Equals, 3, Commentf("Too few IPs returned for only 3 uniques"))
+	for i := range ipSource[:3] {
+		c.Assert(ips[i].String(), Equals, ipSource[i].String(), Commentf("Incorrect unique IP returned"))
+	}
+}
+
+// TestUpdateLookup tests that we can insert DNS data and retrieve it. We
+// iterate through time, ensuring that data is expired as appropriate. We also
+// insert redundant DNS entries that should not change the output.
+func (ds *DNSCacheTestSuite) TestUpdateLookup(c *C) {
+	name := "test.com"
+	now := time.Now()
+	cache := NewDNSCache()
+	endTimeSeconds := 4
+
+	// Add 1 new entry "per second", and one with a redundant IP (with ttl/2).
+	// The IP reflects the second in which it will expire, and should show up for
+	// all now+ttl that is less than it.
+	for i := 1; i <= endTimeSeconds; i++ {
+		ttl := i
+		cache.Update(now,
+			name,
+			[]net.IP{net.ParseIP(fmt.Sprintf("1.1.1.%d", i)), net.ParseIP(fmt.Sprintf("2.2.2.%d", i))},
+			ttl)
+
+		cache.Update(now,
+			name,
+			[]net.IP{net.ParseIP(fmt.Sprintf("1.1.1.%d", i))},
+			ttl/2)
+	}
+
+	// lookup our entries
+	//  - no redundant entries (the 1.1.1.x is not repeated)
+	//  - with each step of secondsPastNow, fewer entries are returned
+	for secondsPastNow := 1; secondsPastNow <= endTimeSeconds; secondsPastNow++ {
+		ips := cache.lookupByTime(now.Add(time.Duration(secondsPastNow)*time.Second), name)
+		c.Assert(len(ips), Equals, 2*(endTimeSeconds-secondsPastNow+1), Commentf("Incorrect number of IPs returned"))
+
+		// Check that we returned each 1.1.1.x entry where x={1..endTimeSeconds}
+		// These are sorted, and are in the first half of the array
+		// Similarly, check the 2.2.2.x entries in the second half of the array
+		j := secondsPastNow
+		halfIndex := endTimeSeconds - secondsPastNow + 1
+		for _, ip := range ips[:halfIndex] {
+			c.Assert(ip.String(), Equals, fmt.Sprintf("1.1.1.%d", j), Commentf("Incorrect IP returned (j=%d, secondsPastNow=%d)", j, secondsPastNow))
+			j++
+		}
+		j = secondsPastNow
+		for _, ip := range ips[halfIndex:] {
+			c.Assert(ip.String(), Equals, fmt.Sprintf("2.2.2.%d", j), Commentf("Incorrect IP returned (j=%d, secondsPastNow=%d)", j, secondsPastNow))
+			j++
+		}
+
+	}
+}
+
+/* Benchmarks
+ * These are here to help gauge the relative costs of operations in DNSCache.
+ * Note: some are on arrays `size` elements, so the benchmark "op time" is too
+ * large.
+ */
+
+var (
+	now         = time.Now()
+	size        = 1000 // size of array to operate on
+	entriesOrig = makeEntries(now, 1+size/3, 1+size/3, 1+size/3)
+	ipsOrig     = func() []net.IP {
+		ips := make([]net.IP, 0, size)
+		for i := 0; i < size; i++ {
+			ips = append(ips, net.IPv4(byte(i>>24), byte(i>>16), byte(i>>8), byte(i>>0)))
+		}
+		return ips
+	}()
+)
+
+func makeEntries(now time.Time, live, redundant, expired int) (entries []*cacheEntry) {
+	liveTTL := 120
+	redundantTTL := 60
+
+	for ; live > 0; live-- {
+		ip := net.IPv4(byte(live>>24), byte(live>>16), byte(live>>8), byte(live>>0))
+
+		entries = append(entries, &cacheEntry{
+			Name:           fmt.Sprintf("live-%s", ip.String()),
+			LookupTime:     now,
+			ExpirationTime: now.Add(time.Duration(liveTTL) * time.Second),
+			TTL:            liveTTL,
+			IPs:            []net.IP{ip}})
+
+		if redundant > 0 {
+			redundant--
+			entries = append(entries, &cacheEntry{
+				Name:           fmt.Sprintf("redundant-%s", ip.String()),
+				LookupTime:     now,
+				ExpirationTime: now.Add(time.Duration(redundantTTL) * time.Second),
+				TTL:            redundantTTL,
+				IPs:            []net.IP{ip}})
+		}
+
+		if expired > 0 {
+			expired--
+			entries = append(entries, &cacheEntry{
+				Name:           fmt.Sprintf("expired-%s", ip.String()),
+				LookupTime:     now.Add(-time.Duration(liveTTL) * time.Second),
+				ExpirationTime: now.Add(-time.Second),
+				TTL:            liveTTL,
+				IPs:            []net.IP{ip}})
+		}
+	}
+
+	rand.Shuffle(len(entries), func(i, j int) {
+		entries[i], entries[j] = entries[j], entries[i]
+	})
+
+	return entries
+}
+
+// Note: each "op" works on size things
+func (ds *DNSCacheTestSuite) BenchmarkGetIPs(c *C) {
+	c.StopTimer()
+	now := time.Now()
+	cache := NewDNSCache()
+	cache.Update(now, "test.com", []net.IP{net.ParseIP("1.2.3.4")}, 60)
+	entries := cache.forward["test.com"]
+	for _, entry := range entriesOrig {
+		cache.updateWithEntryIPs(entries, entry)
+	}
+	c.StartTimer()
+
+	for i := 0; i < c.N; i++ {
+		entries.getIPs(now)
+	}
+}
+
+// Note: each "op" works on size things
+func (ds *DNSCacheTestSuite) BenchmarkUpdateIPs(c *C) {
+	for i := 0; i < c.N; i++ {
+		c.StopTimer()
+		now := time.Now()
+		cache := NewDNSCache()
+		cache.Update(now, "test.com", []net.IP{net.ParseIP("1.2.3.4")}, 60)
+		entries := cache.forward["test.com"]
+		c.StartTimer()
+
+		for _, entry := range entriesOrig {
+			cache.updateWithEntryIPs(entries, entry)
+			cache.removeExpired(entries, now)
+		}
+	}
+}
+
+// Note: each "op" works on size things
+func (ds *DNSCacheTestSuite) BenchmarkKeepUniqueIPs(c *C) {
+	ips := make([]net.IP, 0, len(ipsOrig))
+
+	copy(ips, ipsOrig)
+	for i := 0; i < c.N; i++ {
+		c.StopTimer()
+		rand.Shuffle(len(ips), func(i, j int) {
+			ips[i], ips[j] = ips[j], ips[i]
+		})
+		c.StartTimer()
+
+		keepUniqueIPs(ips)
+	}
+}
+
+func (ds *DNSCacheTestSuite) BenchmarkIPString(c *C) {
+	for i := 0; i < c.N; i++ {
+		_ = net.IPv4(byte(i>>24), byte(i>>16), byte(i>>8), byte(i>>0)).String()
+	}
+}
+
+func (ds *DNSCacheTestSuite) BenchmarkParseIPSimple(c *C) {
+	ip := ipsOrig[0].String()
+	for i := 0; i < c.N; i++ {
+		_ = net.ParseIP(ip)
+	}
+}
+
+// Note: each "op" works on size things
+func (ds *DNSCacheTestSuite) BenchmarkParseIP(c *C) {
+	c.StopTimer()
+	ips := make([]string, 0, len(ipsOrig))
+	for _, ip := range ipsOrig {
+		ips = append(ips, ip.String())
+	}
+	c.StartTimer()
+
+	for i := 0; i < c.N; i++ {
+		for _, ipStr := range ips {
+			_ = net.ParseIP(ipStr)
+		}
+	}
+}

commit 04317b0b10fe7392f1325553256f06f8bbd5c06a

pkg/fqdn: Fix missing delete for forward map
This commit fixes a bug where the keys of the forward map inside the DNS
cache were never removed, causing the map to grow forever. By contrast,
the reverse map keys were being deleted.

For both the forward and reverse maps (which are both maps whose values
are another map), the inner map keys were being deleted.

In other words, the delete on the outer map key was missing for the
forward map.

In addition to fixing the bug, this commit expands the unit test
coverage to assert after any deletes (entries expiring or GC) that the
forward and reverse maps contain what we expect.

Particularly, in an environment where there are many unique DNS lookups
(unique FQDNs) being done, this forward map could grow quite large over
time, especially for a long-lived workload (endpoint). This fixes this
memory-leak-like bug.

Fixes: cf387ce5058 ("fqdn: Introduce TTL-aware cache for DNS retention")
Fixes: f6ce522d55d ("FQDN: Added garbage collector functions.")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/fqdn/cache.go
@@ -300,7 +300,7 @@ func (c *DNSCache) cleanupOverLimitEntries() (affectedNames []string, removed ma
 		for i := 0; i < overlimit; i++ {
 			key := sortedEntries[i]
 			delete(entries, key.ip)
-			c.removeReverse(key.ip, key.entry)
+			c.remove(key.ip, key.entry)
 			removed[key.ip] = append(removed[key.ip], key.entry)
 		}
 		affectedNames = append(affectedNames, dnsName)
@@ -502,7 +502,7 @@ func (c *DNSCache) removeExpired(entries ipEntries, now time.Time, expireLookups
 	for ip, entry := range entries {
 		if entry == nil || entry.isExpiredBy(now) || entry.LookupTime.Before(expireLookupsBefore) {
 			delete(entries, ip)
-			c.removeReverse(ip, entry)
+			c.remove(ip, entry)
 			removed[ip] = entry
 		}
 	}
@@ -523,11 +523,31 @@ func (c *DNSCache) upsertReverse(ip string, entry *cacheEntry) {
 	entries[entry.Name] = entry
 }
 
-// removeReverse removes the reference between ip and the name stored in entry.
+// remove removes the reference between ip and the name stored in entry from
+// the DNS cache (both in forward and reverse maps). This assumes the write
+// lock is taken.
+func (c *DNSCache) remove(ip string, entry *cacheEntry) {
+	c.removeForward(ip, entry)
+	c.removeReverse(ip, entry)
+}
+
+// removeForward removes the reference between ip and the name stored in entry.
 // When no more references from ip to any name exist, the map entry is deleted
 // outright.
 // It is assumed that entry includes ip.
-// This needs a write lock
+// This needs a write lock.
+func (c *DNSCache) removeForward(ip string, entry *cacheEntry) {
+	entries, exists := c.forward[entry.Name]
+	if entries == nil || !exists {
+		return
+	}
+	delete(entries, ip)
+	if len(entries) == 0 {
+		delete(c.forward, entry.Name)
+	}
+}
+
+// removeReverse is the equivalent of removeForward() but for the reverse map.
 func (c *DNSCache) removeReverse(ip string, entry *cacheEntry) {
 	entries, exists := c.reverse[ip]
 	if entries == nil || !exists {

pkg/fqdn/cache_test.go
@@ -99,6 +99,10 @@ func (ds *DNSCacheTestSuite) TestDelete(c *C) {
 	for _, name := range []string{"test1.com", "test2.com", "test3.com"} {
 		ips := cache.lookupByTime(now, name)
 		c.Assert(len(ips), Equals, 2, Commentf("Wrong count of IPs returned (%v) for non-deleted name '%s'", ips, name))
+		c.Assert(cache.forward, checker.HasKey, name, Commentf("Expired name '%s' not deleted from forward", name))
+		for _, ip := range ips {
+			c.Assert(cache.reverse, checker.HasKey, ip.String(), Commentf("Expired IP '%s' not deleted from reverse", ip))
+		}
 	}
 
 	// Delete a single name and check that
@@ -111,9 +115,17 @@ func (ds *DNSCacheTestSuite) TestDelete(c *C) {
 	c.Assert(namesAffected[0], Equals, "test1.com", Commentf("Incorrect affected name returned on forced expire: %s", namesAffected))
 	ips := cache.lookupByTime(now, "test1.com")
 	c.Assert(len(ips), Equals, 0, Commentf("IPs returned (%v) for deleted name 'test1.com'", ips))
+	c.Assert(cache.forward, Not(checker.HasKey), "test1.com", Commentf("Expired name 'test1.com' not deleted from forward"))
+	for _, ip := range ips {
+		c.Assert(cache.reverse, Not(checker.HasKey), ip.String(), Commentf("Expired IP '%s' not deleted from reverse", ip))
+	}
 	for _, name := range []string{"test2.com", "test3.com"} {
 		ips = cache.lookupByTime(now, name)
 		c.Assert(len(ips), Equals, 2, Commentf("Wrong count of IPs returned (%v) for non-deleted name '%s'", ips, name))
+		c.Assert(cache.forward, checker.HasKey, name, Commentf("Expired name '%s' not deleted from forward", name))
+		for _, ip := range ips {
+			c.Assert(cache.reverse, checker.HasKey, ip.String(), Commentf("Expired IP '%s' not deleted from reverse", ip))
+		}
 	}
 
 	// Delete the whole cache. This should leave no data.
@@ -127,6 +139,8 @@ func (ds *DNSCacheTestSuite) TestDelete(c *C) {
 		ips = cache.lookupByTime(now, name)
 		c.Assert(len(ips), Equals, 0, Commentf("Returned IP data for %s after the cache was fully cleared: %v", name, ips))
 	}
+	c.Assert(cache.forward, HasLen, 0)
+	c.Assert(cache.reverse, HasLen, 0)
 	dump := cache.Dump()
 	c.Assert(len(dump), Equals, 0, Commentf("Returned cache entries from cache dump after the cache was fully cleared: %v", dump))
 }
@@ -886,7 +900,14 @@ func (ds *DNSCacheTestSuite) TestCacheToZombiesGCCascade(c *C) {
 
 	// Cascade expirations from cache to zombies. The 3.3.3.3 lookup has not expired
 	now = now.Add(4 * time.Second)
-	cache.GC(now, zombies)
+	expired := cache.GC(now, zombies)
+	c.Assert(expired, HasLen, 1) // test.com
+	// Not all IPs expired (3.3.3.3 still alive) so we expect test.com to be
+	// present in the cache.
+	c.Assert(cache.forward, checker.HasKey, "test.com")
+	c.Assert(cache.reverse, checker.HasKey, "3.3.3.3")
+	c.Assert(cache.reverse, Not(checker.HasKey), "1.1.1.1")
+	c.Assert(cache.reverse, Not(checker.HasKey), "2.2.2.2")
 	alive, dead := zombies.GC()
 	c.Assert(dead, HasLen, 0)
 	assertZombiesContain(c, alive, map[string][]string{
@@ -897,7 +918,13 @@ func (ds *DNSCacheTestSuite) TestCacheToZombiesGCCascade(c *C) {
 	// Cascade expirations from cache to zombies. The 3.3.3.3 lookup has expired
 	// but the older zombies are still alive.
 	now = now.Add(4 * time.Second)
-	cache.GC(now, zombies)
+	expired = cache.GC(now, zombies)
+	c.Assert(expired, HasLen, 1) // test.com
+	// Now all IPs expired so we expect test.com to be removed from the cache.
+	c.Assert(cache.forward, Not(checker.HasKey), "test.com")
+	c.Assert(cache.forward, HasLen, 0)
+	c.Assert(cache.reverse, Not(checker.HasKey), "3.3.3.3")
+	c.Assert(cache.reverse, HasLen, 0)
 	alive, dead = zombies.GC()
 	c.Assert(dead, HasLen, 0)
 	assertZombiesContain(c, alive, map[string][]string{