99e9816ceacfc6c4f5a0c80966f7461a6c869a9a
Robin Hahling <robin.hahling@gw-computing.net>
2022-05-25 17:26:33 +0200

    images/builder: bump protoc-gen-go to v1.28.0
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -10,5 +10,5 @@ set -o nounset
 
 # c285fc70e095eccc98d79b9a133e1e328141aefd == tag: cmd/protoc-gen-go-grpc/v1.2.0
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@c285fc70e095eccc98d79b9a133e1e328141aefd
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.0
 go install github.com/mitchellh/protoc-gen-go-json@v1.1.0
