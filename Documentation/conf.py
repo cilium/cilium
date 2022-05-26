aa8cd3ebfa99a1f014dfaaac7d350f179555894d
MikeLing <sabergeass@gmail.com>
2022-05-26 22:21:29 +0800

    Chinese localization
    Signed-off-by: MikeLing <sabergeass@gmail.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -48,6 +48,10 @@ extensions = ['myst_parser',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+locale_dirs = ['locale/']
+gettext_uuid = True
+gettext_compact = False
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
