2ba0b4fd4bff14fbc0fab4ce8068608eb35474e0
Dylan Reimerink <dylan.reimerink@isovalent.com>
2022-05-30 14:58:04 +0200

    bpf: Add `BPF_PROG_TEST_RUN` based testing framework
    This commit adds a new BPF testing framework for datapath code. This
    framework will allow us to write unit tests in C which will be executed,
    as eBPF programs in the kernel. This allows us to test our code against
    the actual kernel facilities and helper calls as well as confirming that
    code will pass the verifier.
    
    This framework differs from other related works in the sense that we do
    the test setup, execution and verification all in C/eBPF code instead
    of having to do half the work in userspace and the other half in eBPF.
    A lightweight loader program is used to automatically detect test
    programs in ELF files in a given directory, load, and execute them using
    the `BPF_PROG_TEST_RUN` feature. The test results are passed back to the
    loader which will convert them into sub-tests within the golang testing
    framework to allow for easy integration into existing tooling.
    
    Doing setup in C allows us to easily mock out code with `#define`
    preprocessor tricks, replace tailcalls with stubs/code, and leverage
    conditional testing depending on which flags are set / features enabled.
    It also decouples datapath testing from the agent allowing us to verify
    that the eBPF programs work as expected separately from the agent /
    userspace.
    
    Each ELF file can contain multiple `CHECK`'s each of which will become
    a separate program which can fail(`test_fail`/`test_fail_now()`) or pass
    (default if not failed). Tests can log messages with `test_log("msg")`
    for debugging purposes or as fail message(`test_fatal("msg")` logs and
    `test_fail_now()`). These messages can include parameters analogous to
    `bpf_trace_printk`, for example: `test_log("Expected 123, found: %llu",
    some_var)`. Or for compact checks the `assert(some_var == 123)` style
    can be used which will report the file and line of failed asserts and
    stop the test.
    
    These check programs can also define sub-tests with a `TEST("name", {`
    block which can pass/fail independently from the parent test. The appeal
    is that such sub-tests run after each other in the exact same context
    and can for example test different aspects of the same test setup(ctx,
    map state).
    
    Both `CHECK` and `TEST` can be skipped by calling `test_skip()`/
    `test_skip_now()`, to mark a test as skipped, for example if applies to
    a feature which is disabled or depends on features which are not
    available in the current kernel version. Skipped tests are explicitly
    marked as such instead of silently ignored. Tests can use `#ifdef`
    preprocessor statements to determine to skip or not.
    
    Tests that do not require tailcalls can do setup, execution and
    pass/fail checking all in the `CHECK` program. Tests that do need to
    tests across multiple tailcalls need to use a `SETUP` program in
    addition, in which case the setup and execution will happen in the
    `SETUP` program which will exit after the last tailcall is done. The
    loader will run any `SETUP` program before the `CHECK` program of the
    same name, the `CHECK` program will be invoked with the result context
    of the `SETUP` program, the result number of the `SETUP` program will
    be pretended to the context(4 bytes), this allows the `CHECK` program
    to verify the return value, context and map state and determine a pass/
    fail result.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

---
+++ test/bpf_tests/bpf_test.go
@@ -0,0 +1,452 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+//go:build privileged_tests
+
+//go:generate protoc --go_out=. trf.proto
+package bpftests
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/perf"
+	"github.com/golang/protobuf/proto"
+	"github.com/vishvananda/netlink/nl"
+	"google.golang.org/protobuf/encoding/protowire"
+
+	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/datapath/link"
+	"github.com/cilium/cilium/pkg/monitor"
+)
+
+var testPath = flag.String("bpf-test-path", "", "Path to the eBPF tests")
+
+func TestBPF(t *testing.T) {
+	if testPath == nil || *testPath == "" {
+		t.Fatal("-bpf-test-path is a required flag")
+	}
+
+	entries, err := os.ReadDir(*testPath)
+	if err != nil {
+		t.Fatal("os readdir: ", err)
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		if !strings.HasSuffix(entry.Name(), ".o") {
+			continue
+		}
+
+		loadAndRunSpec(t, entry)
+	}
+}
+
+func loadAndRunSpec(t *testing.T, entry fs.DirEntry) {
+	elfPath := path.Join(*testPath, entry.Name())
+	spec := loadAndPrepSpec(t, elfPath)
+
+	coll, err := ebpf.NewCollectionWithOptions(spec, ebpf.CollectionOptions{
+		Programs: ebpf.ProgramOptions{
+			LogSize: 4 << 20, // 4 MiB
+		},
+	})
+	if err != nil {
+		// Use this definition of ENOSPC instead of syscall.ENOSPC, since the stdlib constant is only
+		// available on linux. Even if this doesn't run, we still want it to compile on non-linux systems.
+		const ENOSPC = syscall.Errno(0x1c)
+		// ENOSPC usually means that the log size was to small, so increase and retry.
+		if errors.Is(err, ENOSPC) {
+			t.Fatal("Verifier logs larger than 4MiB, increase the log size passed to ebpf.NewCollectionWithOptions " +
+				"or decrease the size/complexity of the program")
+		}
+
+		t.Fatal("new coll:", err)
+	}
+	defer coll.Close()
+
+	err = loadCallsMap(spec, coll)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testNameToPrograms := make(map[string]programSet)
+
+	for progName, spec := range spec.Programs {
+		match := checkProgRegex.FindStringSubmatch(spec.SectionName)
+		if len(match) == 0 {
+			continue
+		}
+
+		progs := testNameToPrograms[match[1]]
+		if match[2] == "setup" {
+			progs.setupProg = coll.Programs[progName]
+		}
+		if match[2] == "check" {
+			progs.checkProg = coll.Programs[progName]
+		}
+		testNameToPrograms[match[1]] = progs
+	}
+
+	for progName, set := range testNameToPrograms {
+		if set.checkProg == nil {
+			t.Fatalf(
+				"File '%s' contains a setup program in section '%s' but no check program.",
+				elfPath,
+				spec.Programs[progName].SectionName,
+			)
+		}
+	}
+
+	// Collect debug events and add them as logs of the main test
+	var globalLogReader *perf.Reader
+	if m := coll.Maps["test_cilium_events"]; m != nil {
+		globalLogReader, err = perf.NewReader(m, os.Getpagesize()*16)
+		if err != nil {
+			t.Fatalf("new global log reader: %s", err.Error())
+		}
+		defer globalLogReader.Close()
+
+		linkCache := link.NewLinkCache()
+
+		go func() {
+			for {
+				rec, err := globalLogReader.Read()
+				if err != nil {
+					return
+				}
+
+				dm := monitor.DebugMsg{}
+				reader := bytes.NewReader(rec.RawSample)
+				if err := binary.Read(reader, byteorder.Native, &dm); err != nil {
+					return
+				}
+
+				t.Log(dm.Message(linkCache))
+			}
+		}()
+	}
+
+	for name, progs := range testNameToPrograms {
+		t.Run(name, subTest(progs, coll.Maps[suiteResultMap]))
+	}
+
+	if globalLogReader != nil {
+		// Give the global log buf some time to empty
+		// TODO: replace with flush on the buffer, as soon as cilium/ebpf supports that
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
+func loadAndPrepSpec(t *testing.T, elfPath string) *ebpf.CollectionSpec {
+	spec, err := ebpf.LoadCollectionSpec(elfPath)
+	if err != nil {
+		t.Fatal("load spec: ", elfPath, err)
+	}
+
+	for _, mspec := range spec.Maps {
+		// Drain extra info from map specs, cilium/ebpf will complain if we don't
+		if mspec.Extra != nil {
+			io.ReadAll(mspec.Extra)
+		}
+
+		mspec.Pinning = ebpf.PinNone
+	}
+
+	// Detect program type mismatches
+	var progTestType ebpf.ProgramType
+	for _, prog := range spec.Programs {
+		if progTestType != prog.Type {
+			if progTestType == ebpf.UnspecifiedProgram {
+				progTestType = prog.Type
+				continue
+			}
+			if prog.Type == ebpf.UnspecifiedProgram {
+				continue
+			}
+
+			t.Fatalf(
+				"File '%s' contains both '%s' and '%s' program types, "+
+					"only one program type per ELF file allowed:",
+				elfPath,
+				progTestType,
+				prog.Type,
+			)
+		}
+	}
+
+	if progTestType == ebpf.UnspecifiedProgram {
+		t.Fatalf("File '%s' only contains unspecified program types", elfPath)
+	}
+
+	// Give all tail call programs the same program type as the test programs
+	for _, spec := range spec.Programs {
+		spec.Type = progTestType
+	}
+
+	return spec
+}
+
+// Fill the tail calls map with the tail calls based on the calls .id and names of the program sections.
+func loadCallsMap(spec *ebpf.CollectionSpec, coll *ebpf.Collection) error {
+	callMap, found := coll.Maps[callsMapName]
+	if !found {
+		// If we can't find the map, tailcalls aren't required for the current tests
+		return nil
+	}
+
+	for name, prog := range coll.Programs {
+		if strings.HasPrefix(spec.Programs[name].SectionName, callsMapID) {
+			indexStr := strings.TrimPrefix(spec.Programs[name].SectionName, callsMapID)
+			index, err := strconv.Atoi(indexStr)
+			if err != nil {
+				return fmt.Errorf("atoi tail call index: %w", err)
+			}
+
+			index32 := uint32(index)
+			err = callMap.Update(&index32, prog, ebpf.UpdateAny)
+			if err != nil {
+				return fmt.Errorf("update tailcall map: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+type programSet struct {
+	setupProg *ebpf.Program
+	checkProg *ebpf.Program
+}
+
+var checkProgRegex = regexp.MustCompile(`[^/]+/test/([^/]+)/((?:check)|(?:setup))`)
+
+const (
+	ResultSuccess = 1
+
+	suiteResultMap = "suite_result_map"
+
+	callsMapName = "test_cilium_calls_65535"
+	// TODO we should read the .id field from the maps BTF, but the current cilium/ebpf version doesn't make the raw
+	// map BTF available.
+	callsMapID = "2/"
+)
+
+func subTest(progSet programSet, resultMap *ebpf.Map) func(t *testing.T) {
+	return func(t *testing.T) {
+		// create ctx with the max allowed size(4k - head room - tailroom)
+		ctx := make([]byte, 4096-256-320)
+
+		if progSet.setupProg != nil {
+			statusCode, result, err := progSet.setupProg.Test(ctx)
+			if err != nil {
+				t.Fatalf("error while running setup prog: %s", err)
+			}
+
+			ctx = make([]byte, len(result)+4)
+			nl.NativeEndian().PutUint32(ctx, statusCode)
+			copy(ctx[4:], result)
+		}
+
+		// Run test, input a
+		statusCode, _, err := progSet.checkProg.Test(ctx)
+		if err != nil {
+			t.Fatal("error while running check program:", err)
+		}
+
+		// Clear map value after each test
+		defer func() {
+			var key int32
+			value := make([]byte, resultMap.ValueSize())
+			resultMap.Lookup(&key, &value)
+			for i := 0; i < len(value); i++ {
+				value[i] = 0
+			}
+			resultMap.Update(&key, &value, ebpf.UpdateAny)
+		}()
+
+		var key int32
+		value := make([]byte, resultMap.ValueSize())
+		err = resultMap.Lookup(&key, &value)
+		if err != nil {
+			t.Fatal("error while getting suite result:", err)
+		}
+
+		// Detect the length of the result, since the proto.Unmarshal doesn't like trailing zeros.
+		valueLen := 0
+		valueC := value
+		for {
+			_, _, len := protowire.ConsumeField(valueC)
+			if len <= 0 {
+				break
+			}
+			valueLen += len
+			valueC = valueC[len:]
+		}
+
+		result := &SuiteResult{}
+		err = proto.Unmarshal(value[:valueLen], result)
+		if err != nil {
+			t.Fatal("error while unmarshalling suite result:", err)
+		}
+
+		for _, testResult := range result.Results {
+			// Remove the C-string, null-terminator.
+			name := strings.TrimSuffix(testResult.Name, "\x00")
+			t.Run(name, func(tt *testing.T) {
+				if len(testResult.TestLog) > 0 && testing.Verbose() || testResult.Status != SuiteResult_TestResult_PASS {
+					for _, log := range testResult.TestLog {
+						tt.Logf("%s", log.FmtString())
+					}
+				}
+
+				switch testResult.Status {
+				case SuiteResult_TestResult_ERROR:
+					tt.Fatal("Test failed due to unknown error in test framework")
+				case SuiteResult_TestResult_FAIL:
+					tt.Fail()
+				case SuiteResult_TestResult_SKIP:
+					tt.Skip()
+				}
+			})
+		}
+
+		if len(result.SuiteLog) > 0 && testing.Verbose() ||
+			SuiteResult_TestResult_TestStatus(statusCode) != SuiteResult_TestResult_PASS {
+			for _, log := range result.SuiteLog {
+				t.Logf("%s", log.FmtString())
+			}
+		}
+
+		switch SuiteResult_TestResult_TestStatus(statusCode) {
+		case SuiteResult_TestResult_ERROR:
+			t.Fatal("Test failed due to unknown error in test framework")
+		case SuiteResult_TestResult_FAIL:
+			t.Fail()
+		case SuiteResult_TestResult_SKIP:
+			t.SkipNow()
+		}
+	}
+}
+
+type suiteTestResult struct {
+	name string
+	logs []testLog
+	code byte
+}
+
+type testLog struct {
+	fmt  string
+	args []uint64
+}
+
+// A simplified version of fmt.Printf logic, the meaning of % specifiers changed to match the kernels printk specifiers.
+// In the eBPF code a user can for example call `test_log("expected 123, got %llu", some_val)` the %llu meaning
+// long-long-unsigned translates into a uint64, the rendered out would for example be -> 'expected 123, got 234'.
+// https://www.kernel.org/doc/Documentation/printk-formats.txt
+// https://github.com/libbpf/libbpf/blob/4eb6485c08867edaa5a0a81c64ddb23580420340/src/bpf_helper_defs.h#L152
+func (l *Log) FmtString() string {
+	var sb strings.Builder
+
+	end := len(l.Fmt)
+	argNum := 0
+
+	for i := 0; i < end; {
+		lasti := i
+		for i < end && l.Fmt[i] != '%' {
+			i++
+		}
+		if i > lasti {
+			sb.WriteString(strings.TrimSuffix(l.Fmt[lasti:i], "\x00"))
+		}
+		if i >= end {
+			// done processing format string
+			break
+		}
+
+		// Process one verb
+		i++
+
+		var spec []byte
+	loop:
+		for ; i < end; i++ {
+			c := l.Fmt[i]
+			switch c {
+			case 'd', 'i', 'u', 'x', 's':
+				spec = append(spec, c)
+				break loop
+			case 'l':
+				spec = append(spec, c)
+			default:
+				break loop
+			}
+		}
+		// Advance to to next char
+		i++
+
+		// No argument left over to print for the current verb.
+		if argNum >= len(l.Args) {
+			sb.WriteString("%!")
+			sb.WriteString(string(spec))
+			sb.WriteString("(MISSING)")
+			continue
+		}
+
+		switch string(spec) {
+		case "u":
+			fmt.Fprint(&sb, uint16(l.Args[argNum]))
+		case "d", "i", "s":
+			fmt.Fprint(&sb, int16(l.Args[argNum]))
+		case "x":
+			hb := make([]byte, 2)
+			nl.NativeEndian().PutUint16(hb, uint16(l.Args[argNum]))
+			fmt.Fprint(&sb, hex.EncodeToString(hb))
+
+		case "lu":
+			fmt.Fprint(&sb, uint32(l.Args[argNum]))
+		case "ld", "li", "ls":
+			fmt.Fprint(&sb, int32(l.Args[argNum]))
+		case "lx":
+			hb := make([]byte, 4)
+			nl.NativeEndian().PutUint32(hb, uint32(l.Args[argNum]))
+			fmt.Fprint(&sb, hex.EncodeToString(hb))
+
+		case "llu":
+			fmt.Fprint(&sb, uint64(l.Args[argNum]))
+		case "lld", "lli", "lls":
+			fmt.Fprint(&sb, int64(l.Args[argNum]))
+		case "llx":
+			hb := make([]byte, 8)
+			nl.NativeEndian().PutUint64(hb, uint64(l.Args[argNum]))
+			fmt.Fprint(&sb, hex.EncodeToString(hb))
+
+		default:
+			sb.WriteString("%!")
+			sb.WriteString(string(spec))
+			sb.WriteString("(INVALID)")
+			continue
+		}
+
+		argNum++
+	}
+
+	return sb.String()
+}
