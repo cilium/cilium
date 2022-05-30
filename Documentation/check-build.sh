cilium/cilium

commit f98307558301044c44fe69a81eea2cd244e18752

docs: Clean up check-build.sh
Various miscellaneous clean-up and improvements:

- Remove the previous warnings.txt file before running the script, to
  avoid printing outdated messages

- Attempt to clean up the calls to sphinx-build: do not redirect into
  tee, only use the warnings.txt file for printing warnings. This makes
  the script easier to understand.

- Rework some options passed to shpinx-build.
    - Use "--color" to have colors, even if the warnings are printed to
      a file, so we can later dump them with colors.
    - Use "-n" to tell sphinx-build to be nitpicky when reporting
      errors; this does not raise any additional warning at this time.
    - Use "-W --keep-going" for checks so we can rely on the exit status
      of the command (which does not break the script, because it's
      called inside of a "if" through build_with_spellchecker()).
    - Do NOT pass "-E" if we run all the checks. The checks re-read all
      files already; there is no reason to read them all again when
      building the final version.

- Add a note on the apparent non-feasability to call sphinx only once,
  even if moving the script to Python

- Rework a bit the file, by moving the call to rstcheck to a dedicated
  function

- Add some comments for various functions/invocations

- Add function cat_warnings(), which doesn't look very useful but will
  be reused to reintroduce filters on warnings.

Note: I also wanted to pass "-j auto" to sphinx-build in order to enable
parallel builds, but on some setups (macOS M1) this produced warnings
about duplicated declarations of HTTP methods when generating the API
documentation through the openapi extension (which in turns rely on
httpdomain) - see [0].

[0] https://github.com/sphinx-contrib/httpdomain/commit/b00105a884639504d7d079238e221290785a66d0#diff-ee3c6c3e25cffc74b15c05f2b6c0e72c8d29d9de8fb5c8a51309fb45c102e9e4R710

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

Documentation/check-build.sh
@@ -7,6 +7,9 @@ set -o pipefail
 # This was ported from the makefile, it can probably be converted
 # to Python (see e.g. https://stackoverflow.com/a/47657926), that
 # way we might be able to call Sphinx only once and make it quicker
+# Update 2022-05: It seems that Sphinx cannot run multiple builders
+# (e.g. html + spelling) at once so we're stuck calling it multiple
+# times anyway.
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -19,71 +22,96 @@ shift
 
 cd "${script_dir}"
 mkdir -p "${build_dir}"
+rm -f -- "${warnings}"
+
+has_spelling_errors() {
+    # If spelling errors were found, Sphinx wrote them to files under
+    # ${spelldir}. Let's check whether the directory is empty.
+    test -n "$(ls "${spelldir}")"
+}
+
+cat_warnings() {
+    [ -s "${warnings}" ] || return
+    cat "${warnings}"
+}
+
+# Returns non-0 if we have relevant build warnings
+has_build_warnings() {
+    [ -s "${warnings}" ] || return 1
+}
+
+describe_spelling_errors() {
+    local new_words
+
+    # Show all misspelled words; display source path relative to root repository
+    find "${spelldir}" -type f -print0 | xargs -0 sed 's/^/* Documentation\//'
+
+    # Print a hint on how to add new correct words to the list of good words
+    new_words="$(sed -E "s/^([^:]+:){2} \(([^ ]+)\).*/\2/g" "${spelldir}"/* | sort -u | tr '\r\n' ' ' | sed "s/'/\\\\\\\\'/g")"
+    printf "\nIf the words are not misspelled, run:\n%s %s\n" \
+        "Documentation/update-spelling_wordlist.sh" "${new_words}"
+}
 
 build_with_spellchecker() {
     rm -rf "${spelldir}"
-    sphinx-build -b spelling -d "${build_dir}/doctrees" . "${spelldir}" -q -E 2> "${warnings}"
+    # Call with -q -W --keep-going: suppresses regular output (keeps warning;
+    # -Q would suppress warnings as well including those we write to a file),
+    # consider warnings as errors for exit status, but keep going on
+    # warning/errors so that we get the full list of errors.
+    sphinx-build -b spelling \
+        -d "${build_dir}/doctrees" . "${spelldir}" \
+        -E -n --color -q -w "${warnings}" -W --keep-going 2>/dev/null
 }
 
 build_with_linkchecker() {
     sphinx-build -b linkcheck -d "${build_dir}/doctrees" . "${spelldir}" -q -E
 }
 
-has_spelling_errors() {
-    test -n "$(ls "${spelldir}")"
+run_linter() {
+    local CONF_PY_ROLES CONF_PY_SUBSTITUTIONS ignored_messages
+
+    CONF_PY_ROLES=$(sed -n "/^extlinks = {$/,/^}$/ s/^ *'\([^']\+\)':.*/\1/p" conf.py | tr '\n' ',')
+    CONF_PY_SUBSTITUTIONS="$(sed -n 's/^\.\. |\([^|]\+\)|.*/\1/p' conf.py | tr '\n' ',')release"
+    ignored_messages="("
+    ignored_messages="${ignored_messages}bpf.rst:.*: \(INFO/1\) Enumerated list start value not ordinal"
+    ignored_messages="${ignored_messages}|Hyperlink target .*is not referenced\."
+    ignored_messages="${ignored_messages}|Duplicate implicit target name:"
+    ignored_messages="${ignored_messages}|Malformed table\."
+    ignored_messages="${ignored_messages})"
+    rstcheck \
+        --report info \
+        --ignore-language bash \
+        --ignore-message "${ignored_messages}" \
+        --ignore-directives tabs \
+        --ignore-roles "${CONF_PY_ROLES}" \
+        --ignore-substitutions "${CONF_PY_SUBSTITUTIONS}" \
+        -r .
 }
 
-describe_spelling_errors() {
-    printf "\nPlease fix the following spelling mistakes:\n"
-    # show file paths relative to repo root
-    find "${spelldir}" -type f -print0 | xargs -0 sed 's/^/* Documentation\//'
-}
-
-hint_about_wordlist_update() {
-    new_words="$(sed -E "s/^([^:]+:){2} \(([^ ]+)\).*/\2/g" "${spelldir}"/* | sort -u | tr '\r\n' ' ' | sed "s/'/\\\\\\\\'/g")"
-    printf "\nIf the words are not misspelled, run:\n%s %s\n" \
-        "Documentation/update-spelling_wordlist.sh" "${new_words}"
-}
+read_all_opt=""
 
 if [ -n "${SKIP_LINT-}" ]; then
+  # Read all files for final build if we don't read them all with linting
+  read_all_opt="-E"
+
   echo "Skipping syntax and spelling validations..."
 else
   echo "Running linter..."
-  CONF_PY_ROLES=$(sed -n "/^extlinks = {$/,/^}$/ s/^ *'\([^']\+\)':.*/\1/p" conf.py | tr '\n' ',')
-  CONF_PY_SUBSTITUTIONS="$(sed -n 's/^\.\. |\([^|]\+\)|.*/\1/p' conf.py | tr '\n' ',')release"
-  ignored_messages="("
-  ignored_messages="${ignored_messages}bpf.rst:.*: \(INFO/1\) Enumerated list start value not ordinal"
-  ignored_messages="${ignored_messages}|Hyperlink target .*is not referenced\."
-  ignored_messages="${ignored_messages}|Duplicate implicit target name:"
-  ignored_messages="${ignored_messages}|Malformed table\."
-  ignored_messages="${ignored_messages})"
-  rstcheck \
-      --report info \
-      --ignore-language bash \
-      --ignore-message "${ignored_messages}" \
-      --ignore-directives tabs \
-      --ignore-roles ${CONF_PY_ROLES}\
-      --ignore-substitutions ${CONF_PY_SUBSTITUTIONS} \
-      -r .
+  run_linter
 
   echo "Validating documentation (syntax, spelling)..."
-  if build_with_spellchecker ; then
-    status_ok=0
-    if [ -s ${warnings} ]; then
+  if ! build_with_spellchecker ; then
+    if has_build_warnings ; then
         printf "\nPlease fix the following documentation warnings:\n"
-        cat ${warnings}
-        status_ok=1
+        cat_warnings
     fi
 
     if has_spelling_errors ; then
+        printf "\nPlease fix the following spelling mistakes:\n"
         describe_spelling_errors
-        hint_about_wordlist_update
-        status_ok=1
     fi
 
-    if [ "${status_ok}" -ne 0 ] ; then
-        exit 1
-    fi
+    exit 1
   fi
 fi
 
@@ -96,11 +124,13 @@ fi
 # fi
 
 echo "Building documentation (${target})..."
-sphinx-build -M "${target}" "${script_dir}" "${build_dir}" $@ -q 2> >(tee "${warnings}" >&2)
+sphinx-build -M "${target}" "${script_dir}" "${build_dir}" $@ \
+    ${read_all_opt} -n --color -q -w "${warnings}" 2>/dev/null
 
 # We can have warnings but no errors here, or sphinx-build would return non-0
 # and we would have exited because of "set -o errexit".
-if [ -s "${warnings}" ] ; then
-    echo "Please fix the above documentation warnings"
+if has_build_warnings ; then
+    echo "Please fix the documentation warnings below"
+    cat_warnings
     exit 1
 fi

commit 477849d34ce1b34fdfbac680d1ea9d68ca612a77

docs: fix check-build.sh exit status
The `check-build.sh` script should only return with exit 1 if errors
have actually been found, even for the non-spelling case.

Fixes: f98307558301044c44fe69a81eea2cd244e18752

Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

Documentation/check-build.sh
@@ -105,17 +105,22 @@ else
 
   echo "Validating documentation (syntax, spelling)..."
   if ! build_with_spellchecker ; then
+    status_ok=0
     if has_build_warnings ; then
         printf "\nPlease fix the following documentation warnings:\n"
         filter_warnings
+        status_ok=1
     fi
 
     if has_spelling_errors ; then
         printf "\nPlease fix the following spelling mistakes:\n"
         describe_spelling_errors
+        status_ok=1
     fi
 
-    exit 1
+    if [ "${status_ok}" -ne 0 ] ; then
+        exit 1
+    fi
   fi
 fi
 