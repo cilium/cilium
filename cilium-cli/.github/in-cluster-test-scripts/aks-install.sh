ac118eeb50862d348f23eb907252ba04d0fabe6f
Nicolas Busseneau <nicolas@isovalent.com>
2022-06-03 16:54:57 +0200

    ci: use BYOCNI in CI
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- cilium-cli/.github/in-cluster-test-scripts/aks-install.sh
+++ cilium-cli/.github/in-cluster-test-scripts/aks-install.sh
@@ -7,10 +7,6 @@ set -e
 cilium install \
   --version "${CILIUM_VERSION}" \
   --disable-check=az-binary \
-  --azure-subscription-id "${AZURE_SUBSCRIPTION_ID}" \
-  --azure-node-resource-group "${AZURE_NODE_RESOURCE_GROUP}" \
-  --azure-tenant-id "${AZURE_TENANT_ID}" \
-  --azure-client-id "${AZURE_CLIENT_ID}" \
-  --azure-client-secret "${AZURE_CLIENT_SECRET}" \
+  --datapath-mode=aks-byocni \
   --wait=false \
   --config monitor-aggregation=none
