978f14c0049d347e93e99682b49917e292b5ef62
Joe Stringer <joe@cilium.io>
2022-06-07 21:39:12 -0700

    policy: Re-label metric sources using pkg/source
    Previously, this code was relying on the metrics package to provide a
    string for prometheus to highlight the source of particular policies.
    However, we already have a pkg/source which serves a similar purpose.
    Reuse pkg/source for this variable type as well, and extend the variable
    type to have a new source type reflecting resources added on the local
    API exposed on the node.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/k8s/watchers/network_policy.go
+++ pkg/k8s/watchers/network_policy.go
@@ -17,6 +17,7 @@ import (
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/policy"
+	"github.com/cilium/cilium/pkg/source"
 )
 
 func (k *K8sWatcher) networkPoliciesInit(slimClient slimclientset.Interface, swgKNPs *lock.StoppableWaitGroup) {
@@ -87,7 +88,10 @@ func (k *K8sWatcher) addK8sNetworkPolicyV1(k8sNP *slim_networkingv1.NetworkPolic
 	}
 	scopedLog = scopedLog.WithField(logfields.K8sNetworkPolicyName, k8sNP.ObjectMeta.Name)
 
-	opts := policy.AddOptions{Replace: true, Source: metrics.LabelEventSourceK8s}
+	opts := policy.AddOptions{
+		Replace: true,
+		Source:  source.Kubernetes,
+	}
 	if _, err := k.policyManager.PolicyAdd(rules, &opts); err != nil {
 		metrics.PolicyImportErrorsTotal.Inc() // Deprecated in Cilium 1.14, to be removed in 1.15.
 		metrics.PolicyChangeTotal.WithLabelValues(metrics.LabelValueOutcomeFail).Inc()
