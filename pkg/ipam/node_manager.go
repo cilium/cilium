cilium/cilium

commit b5c5ca9b63cc14db6bf2d8bac5669f20e0b798cb

eni/azure: Don't perform mutating operations while instances API is unstable
It is possible for the cloud APIs being used by the operator to get into
a state where POST and PATCH operations are still succeeding while GET
operations are failing. This can result in the operator to continously
creating resources while being unable to ever synchronize the state
successfully. Require a successful synchronization of all resources in
order to continue performing mutating operations.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/ipam/node_manager.go
@@ -134,14 +134,15 @@ type nodeMap map[string]*Node
 
 // NodeManager manages all nodes with ENIs
 type NodeManager struct {
-	mutex            lock.RWMutex
-	nodes            nodeMap
-	instancesAPI     AllocationImplementation
-	k8sAPI           CiliumNodeGetterUpdater
-	metricsAPI       MetricsAPI
-	resyncTrigger    *trigger.Trigger
-	parallelWorkers  int64
-	releaseExcessIPs bool
+	mutex              lock.RWMutex
+	nodes              nodeMap
+	instancesAPI       AllocationImplementation
+	k8sAPI             CiliumNodeGetterUpdater
+	metricsAPI         MetricsAPI
+	resyncTrigger      *trigger.Trigger
+	parallelWorkers    int64
+	releaseExcessIPs   bool
+	stableInstancesAPI bool
 }
 
 // NewNodeManager returns a new NodeManager
@@ -164,8 +165,7 @@ func NewNodeManager(instancesAPI AllocationImplementation, k8sAPI CiliumNodeGett
 		MinInterval:     10 * time.Millisecond,
 		MetricsObserver: metrics.ResyncTrigger(),
 		TriggerFunc: func(reasons []string) {
-			syncTime := instancesAPI.Resync(context.TODO())
-			if !syncTime.IsZero() {
+			if syncTime, ok := mngr.instancesAPIResync(context.TODO()); ok {
 				mngr.Resync(context.TODO(), syncTime)
 			}
 		},
@@ -175,16 +175,25 @@ func NewNodeManager(instancesAPI AllocationImplementation, k8sAPI CiliumNodeGett
 	}
 
 	mngr.resyncTrigger = resyncTrigger
+	// Assume readiness, the initial blocking resync in Start() will update
+	// the readiness
+	mngr.SetInstancesAPIReadiness(true)
 
 	return mngr, nil
+}
+
+func (n *NodeManager) instancesAPIResync(ctx context.Context) (time.Time, bool) {
+	syncTime := n.instancesAPI.Resync(ctx)
+	success := !syncTime.IsZero()
+	n.SetInstancesAPIReadiness(success)
+	return syncTime, success
 }
 
 // Start kicks of the NodeManager by performing the initial state
 // synchronization and starting the background sync go routine
 func (n *NodeManager) Start(ctx context.Context) error {
 	// Trigger the initial resync in a blocking manner
-	resyncTime := n.instancesAPI.Resync(ctx)
-	if resyncTime.IsZero() {
+	if _, ok := n.instancesAPIResync(ctx); !ok {
 		return fmt.Errorf("Initial synchronization with instances API failed")
 	}
 
@@ -199,8 +208,7 @@ func (n *NodeManager) Start(ctx context.Context) error {
 			controller.ControllerParams{
 				RunInterval: time.Minute,
 				DoFunc: func(ctx context.Context) error {
-					syncTime := n.instancesAPI.Resync(ctx)
-					if !syncTime.IsZero() {
+					if syncTime, ok := n.instancesAPIResync(ctx); ok {
 						n.Resync(ctx, syncTime)
 					}
 					return nil
@@ -209,6 +217,20 @@ func (n *NodeManager) Start(ctx context.Context) error {
 	}()
 
 	return nil
+}
+
+// SetInstancesAPIReadiness sets the readiness state of the instances API
+func (n *NodeManager) SetInstancesAPIReadiness(ready bool) {
+	n.mutex.Lock()
+	n.stableInstancesAPI = ready
+	n.mutex.Unlock()
+}
+
+// InstancesAPIIsReady returns true if the instances API is stable and ready
+func (n *NodeManager) InstancesAPIIsReady() bool {
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+	return n.stableInstancesAPI
 }
 
 // GetNames returns the list of all node names
@@ -256,6 +278,17 @@ func (n *NodeManager) Update(resource *v2.CiliumNode) bool {
 			node.logger().WithError(err).Error("Unable to create pool-maintainer trigger")
 			return false
 		}
+
+		retry, err := trigger.NewTrigger(trigger.Parameters{
+			Name:        fmt.Sprintf("ipam-pool-maintainer-%s-retry", resource.Name),
+			MinInterval: time.Minute, // large minimal interval to not retry too often
+			TriggerFunc: func(reasons []string) { poolMaintainer.Trigger() },
+		})
+		if err != nil {
+			node.logger().WithError(err).Error("Unable to create pool-maintainer-retry trigger")
+			return false
+		}
+		node.retry = retry
 
 		k8sSync, err := trigger.NewTrigger(trigger.Parameters{
 			Name:            fmt.Sprintf("ipam-node-k8s-sync-%s", resource.Name),

pkg/ipam/node.go
@@ -17,6 +17,7 @@ package ipam
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/cilium/cilium/pkg/defaults"
@@ -94,6 +95,10 @@ type Node struct {
 
 	// ops is the IPAM implementation to used for this node
 	ops NodeOperations
+
+	// retry is the trigger used to retry pool maintenance while the
+	// instances API is unstable
+	retry *trigger.Trigger
 }
 
 // Statistics represent the IP allocation statistics of a node
@@ -653,6 +658,15 @@ func (n *Node) updateLastResync(syncTime time.Time) {
 // MaintainIPPool attempts to allocate or release all required IPs to fulfill
 // the needed gap. If required, interfaces are created.
 func (n *Node) MaintainIPPool(ctx context.Context) error {
+	// As long as the instances API is unstable, don't perform any
+	// operation that can mutate state.
+	if !n.manager.InstancesAPIIsReady() {
+		if n.retry != nil {
+			n.retry.Trigger()
+		}
+		return fmt.Errorf("instances API is unstable. Blocking mutating operations. See logs for details.")
+	}
+
 	// If the instance has stopped running for less than a minute, don't attempt any deficit
 	// resolution and wait for the custom resource to be updated as a sign
 	// of life.

commit 4a538ac33d90465754e6840450fd73ce0a911d93

ipam: Shutdown retry trigger on node deletion
Like all the other triggers that are a part of Node, add shutdown for
the missing `retry` trigger.

Found via code inspection.

Fixes: b5c5ca9b63c ("eni/azure: Don't perform mutating operations while
instances API is unstable")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipam/node_manager.go
@@ -329,12 +329,16 @@ func (n *NodeManager) Update(resource *v2.CiliumNode) (nodeSynced bool) {
 // Kubernetes apiserver
 func (n *NodeManager) Delete(nodeName string) {
 	n.mutex.Lock()
+
 	if node, ok := n.nodes[nodeName]; ok {
 		if node.poolMaintainer != nil {
 			node.poolMaintainer.Shutdown()
 		}
 		if node.k8sSync != nil {
 			node.k8sSync.Shutdown()
+		}
+		if node.retry != nil {
+			node.retry.Shutdown()
 		}
 	}
 