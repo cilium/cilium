47f68a44072b126648d2e66500945dd0874560ac
Gilberto Bertin <jibi@cilium.io>
2022-06-14 18:58:42 +0200

    bpf: switch egress gateway logic to identity_is_cluster()
    [ upstream commit 4d0da72577f08197e558bc10dcb1290965d4cd20 ]
    
    Currently to determine if a given destination IP belongs to the cluster,
    the egress gateway datapath logic uses is_cluster_destination(), which
    relies on different sources of truth (tunnel_endpoint, security ID and EP
    map).
    
    This commit switches that logic to the new identity_is_cluster(),
    which relies solely on security IDs.
    
    Fixes: #19784
    
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- bpf/lib/egress_policies.h
+++ bpf/lib/egress_policies.h
@@ -17,33 +17,6 @@
 #define EGRESS_PREFIX_LEN(PREFIX) (EGRESS_STATIC_PREFIX + (PREFIX))
 #define EGRESS_IPV4_PREFIX EGRESS_PREFIX_LEN(32)
 
-/* is_cluster_destination returns true if the given destination is part of the
- * cluster. It uses the ipcache and endpoint maps information.
- */
-static __always_inline bool
-is_cluster_destination(struct iphdr *ip4, __u32 dst_id, __u32 tunnel_endpoint)
-{
-	/* If tunnel endpoint is found in ipcache, it means the remote endpoint
-	 * is in cluster.
-	 */
-	if (tunnel_endpoint != 0)
-		return true;
-
-	/* If the destination is a Cilium-managed node (remote or local), it's
-	 * part of the cluster.
-	 */
-	if (identity_is_node(dst_id))
-		return true;
-
-	/* Use the endpoint map to know if the destination is a local endpoint.
-	 */
-	if (lookup_ip4_endpoint(ip4))
-		return true;
-
-	/* Everything else is outside the cluster. */
-	return false;
-}
-
 static __always_inline
 struct egress_gw_policy_entry *lookup_ip4_egress_gw_policy(__be32 saddr, __be32 daddr)
 {
