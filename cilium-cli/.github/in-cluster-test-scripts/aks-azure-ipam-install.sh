48efe8f76eb8014c0df863533dbbb1a7dd5b7ec0
Nicolas Busseneau <nicolas@isovalent.com>
2022-06-15 15:26:55 +0200

    ci: re-add AKS (Azure IPAM) workflow
    This is almost the same as the one that was removed in
    ba5ecd6e92d1d8a127afaf5beeebbab4a47280ca, except that we have removed
    taints as they are not working due to an AKS-side change.
    
    Taint removal is sub-optimal from a user experience standpoint because
    one might get nodes with pods that are not properly handled by Cilium
    until they are restarted, but in CI it basically makes no difference and
    at least allows us to verify that the Azure IPAM is working as intended.
    
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

---
+++ cilium-cli/.github/in-cluster-test-scripts/aks-azure-ipam-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -x
+set -e
+
+# Install Cilium
+cilium install \
+  --version "${CILIUM_VERSION}" \
+  --disable-check=az-binary \
+  --datapath-mode=azure \
+  --azure-subscription-id "${AZURE_SUBSCRIPTION_ID}" \
+  --azure-node-resource-group "${AZURE_NODE_RESOURCE_GROUP}" \
+  --azure-tenant-id "${AZURE_TENANT_ID}" \
+  --azure-client-id "${AZURE_CLIENT_ID}" \
+  --azure-client-secret "${AZURE_CLIENT_SECRET}" \
+  --wait=false \
+  --config monitor-aggregation=none
