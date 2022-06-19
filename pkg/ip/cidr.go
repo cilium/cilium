111fc65af7cab1ec0ad3f595cd8e6b23157041d4
Joe Stringer <joe@cilium.io>
2022-06-19 07:12:59 -0700

    ip: Add helpers to assist net -> netip transition
    These helpers can come in useful to convert between old and new IP types
    from the standard library in order to do incremental updates to
    individual packages without reworking IP address/prefix usage across
    the entire application all at once.
    
    Once we've fully migrated to netip, these should no longer be necessary
    and can be deleted again.
    
    Co-authored-by: Chris Tarazi <chris@isovalent.com>
    Co-authored-by: Tobias Klauser <tobias@cilium.io>
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/ip/cidr.go
+++ pkg/ip/cidr.go
@@ -5,6 +5,7 @@ package ip
 
 import (
 	"net"
+	"net/netip"
 )
 
 // ParseCIDRs fetches all CIDRs referred to by the specified slice and returns
@@ -29,4 +30,53 @@ func ParseCIDRs(cidrs []string) (valid []*net.IPNet, invalid []string) {
 		}
 	}
 	return valid, invalid
+}
+
+// PrefixToIPNet is a convenience helper for migrating from the older 'net'
+// standard library types to the newer 'netip' types. Use this to plug the
+// new types in newer code into older types in older code during the migration.
+func PrefixToIPNet(prefix netip.Prefix) *net.IPNet {
+	if !prefix.IsValid() {
+		return nil
+	}
+	addr := prefix.Masked().Addr()
+	return &net.IPNet{
+		IP:   addr.AsSlice(),
+		Mask: net.CIDRMask(prefix.Bits(), addr.BitLen()),
+	}
+}
+
+// IPNetToPrefix is a convenience helper for migrating from the older 'net'
+// standard library types to the newer 'netip' types. Use this to plug the
+// new types in newer code into older types in older code during the migration.
+func IPNetToPrefix(prefix *net.IPNet) netip.Prefix {
+	if prefix == nil {
+		return netip.Prefix{}
+	}
+	ip, ok := netip.AddrFromSlice(prefix.IP)
+	if !ok {
+		return netip.Prefix{}
+	}
+	ones, bits := prefix.Mask.Size()
+	if bits != net.IPv4len*8 && bits != net.IPv6len*8 {
+		// invalid mask
+		return netip.Prefix{}
+	}
+	return netip.PrefixFrom(ip, ones)
+}
+
+// IPToNetPrefix is a convenience helper for migrating from the older 'net'
+// standard library types to the newer 'netip' types. Use this to plug the new
+// types in newer code into older types in older code during the migration.
+//
+// Note: This function assumes that the result of net.IP.To4() or net.IP.To16()
+// are passed in. This is because the net package always creates net.IP with a
+// length of 16 (IPv6) which causes this function to return an IPv6
+// netip.Prefix.
+func IPToNetPrefix(ip net.IP) netip.Prefix {
+	a, ok := netip.AddrFromSlice(ip)
+	if !ok {
+		return netip.Prefix{}
+	}
+	return netip.PrefixFrom(a, a.BitLen())
 }
