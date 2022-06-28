1b33eadb4990894cfe1c04f27c297c4a77c04cf7
Fabio Falzoi <fabio.falzoi@isovalent.com>
2022-06-28 17:24:28 +0200

    operator: preallocate cnp list backing array
    The number of returned CiliumClusterwideNetworkPolicies is known in
    advance, so the preallocation of the backing array will avoid
    reallocations after the append.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- operator/cmd/cilium_node.go
+++ operator/cmd/cilium_node.go
@@ -390,7 +390,7 @@ func runCNPNodeStatusGC(name string, clusterwide bool, clientset k8sClient.Clien
 							return err
 						}
 
-						cnpItemsList = make([]cilium_v2.CiliumNetworkPolicy, 0)
+						cnpItemsList = make([]cilium_v2.CiliumNetworkPolicy, 0, len(ccnpList.Items))
 						for _, ccnp := range ccnpList.Items {
 							cnpItemsList = append(cnpItemsList, cilium_v2.CiliumNetworkPolicy{
 								ObjectMeta: meta_v1.ObjectMeta{
