e7e62eb14b2614fb44e60d3554363e7f88630926
Anton Protopopov <aspsk@isovalent.com>
2022-07-01 10:26:59 +0000

    datapath: add file and line info to drop notifications
    [ upstream commit f3f1f28c686f111a5dbf9d7ac0c1978461c7e35c ]
    
    Add source file and line information to the output of `cilium monitor -t drop`.
    The information is passed to user-space in the new `line` (u16) and `file` (u8)
    fields of the struct drop_notify structure.  In order to pass this information
    over through a tail call the 24 upper bits of the meta[4] field were used.
    Previously meta[4] kept only the exit code which is a small number (XDP or TC
    return codes), and should fit in the dedicated 8 bits:
    
            8           8             16
      | exit code | file number | line number |
    
    The file number is defined in the loader.sourceNameToId map which maps all C
    files from the bpf/ directory to ids.  The map is to be maintained manually
    (in the initial version of this patch it was generated by go generate, but
    this could lead to inconsistencies if agent and client were built separately).
    The consistency of this map is checked during build, so if one adds a new C
    file to the bpf/ directory and doesn't update the map, then the build fails
    (see make check-sources).
    
    In order to send this info we need to gather it in the first place.  To do this
    with minimal changes to the codebase the send_drop_notify{,_error} functions
    were redefined as macros:
    
        send_drop_notify(...)       -> _send_drop_notify(__MAGIC_FILE__, __LINE__, ...)
        send_drop_notify_error(...) -> _send_drop_notify_error(__MAGIC_FILE__, __LINE__, ...)
    
    The __LINE__ expands to the line number.  Be aware though that if a macro call
    expands to several lines, like here
    
        581       if (IS_ERR(ret))
        582               return send_drop_notify(ctx, SECLABEL, dst_id, 0, ret,
        583                                       CTX_ACT_DROP, METRIC_EGRESS);
    
    then clang translates the __LINE__ to a slightly wrong number—the last line the
    macro call occupies, 583 in this case. gcc would translate it to 582.
    
    The __MAGIC_FILE__ constant is redefined according to the loader.sourceNameToId
    mapping by the loader when we compile a program.  (If the __MAGIC_FILE__ macro
    is not defined during the compilation it defaults to zero.)
    
    With all these changes applied, one now can see corresponding filenames and
    line numbers in the output of `cilium monitor -t drop`:
    
        xx drop (Invalid source ip) flow 0x0 to endpoint 0, file bpf_lxc.c line 695, ...
        xx drop (Stale or unroutable IP) flow 0x15a7e11f to endpoint 0, file bpf_host.c line 351, ...
    
    Signed-off-by: Anton Protopopov <aspsk@isovalent.com>
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- bpf/lib/drop.h
+++ bpf/lib/drop.h
@@ -26,7 +26,9 @@ struct drop_notify {
 	__u32		src_label;
 	__u32		dst_label;
 	__u32		dst_id;
-	__u32		unused;
+	__u16		line;
+	__u8		file;
+	__u8		unused;
 };
 
 __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_DROP_NOTIFY)
@@ -36,6 +38,10 @@ int __send_drop_notify(struct __ctx_buff *ctx)
 	int error = ctx_load_meta(ctx, 2) & 0xFFFFFFFF;
 	__u64 ctx_len = ctx_full_len(ctx);
 	__u64 cap_len = min_t(__u64, TRACE_PAYLOAD_LEN, ctx_len);
+	__u32 meta4 = ctx_load_meta(ctx, 4);
+	__u16 line = (__u16)(meta4 >> 16);
+	__u8 file = (__u8)(meta4 >> 8);
+	__u8 exitcode = (__u8)meta4;
 	struct drop_notify msg;
 
 	if (error < 0)
@@ -47,13 +53,15 @@ int __send_drop_notify(struct __ctx_buff *ctx)
 		.src_label	= ctx_load_meta(ctx, 0),
 		.dst_label	= ctx_load_meta(ctx, 1),
 		.dst_id		= ctx_load_meta(ctx, 3),
+		.line           = line,
+		.file           = file,
 	};
 
 	ctx_event_output(ctx, &EVENTS_MAP,
 			 (cap_len << 32) | BPF_F_CURRENT_CPU,
 			 &msg, sizeof(msg));
 
-	return ctx_load_meta(ctx, 4);
+	return exitcode;
 }
 
 /**
@@ -70,14 +78,21 @@ int __send_drop_notify(struct __ctx_buff *ctx)
  * NOTE: This is terminal function and will cause the BPF program to exit
  */
 static __always_inline int
-send_drop_notify(struct __ctx_buff *ctx, __u32 src, __u32 dst, __u32 dst_id,
-		 int reason, int exitcode, enum metric_dir direction)
+_send_drop_notify(__u8 file, __u16 line, struct __ctx_buff *ctx,
+		  __u32 src, __u32 dst, __u32 dst_id,
+		  int reason, __u32 exitcode, enum metric_dir direction)
 {
+	/* These fields should be constants and fit (together) in 32 bits */
+	if (!__builtin_constant_p(exitcode) || exitcode > 0xff ||
+	    !__builtin_constant_p(file) || file > 0xff ||
+	    !__builtin_constant_p(line) || line > 0xffff)
+		__throw_build_bug();
+
 	ctx_store_meta(ctx, 0, src);
 	ctx_store_meta(ctx, 1, dst);
 	ctx_store_meta(ctx, 2, reason);
 	ctx_store_meta(ctx, 3, dst_id);
-	ctx_store_meta(ctx, 4, exitcode);
+	ctx_store_meta(ctx, 4, exitcode | file << 8 | line << 16);
 
 	update_metrics(ctx_full_len(ctx), direction, (__u8)-reason);
 	ep_tail_call(ctx, CILIUM_CALL_DROP_NOTIFY);
@@ -86,20 +101,29 @@ send_drop_notify(struct __ctx_buff *ctx, __u32 src, __u32 dst, __u32 dst_id,
 }
 #else
 static __always_inline
-int send_drop_notify(struct __ctx_buff *ctx, __u32 src __maybe_unused,
-		     __u32 dst __maybe_unused, __u32 dst_id __maybe_unused,
-		     int reason, int exitcode, enum metric_dir direction)
+int _send_drop_notify(__u8 file __maybe_unused, __u16 line __maybe_unused,
+		      struct __ctx_buff *ctx, __u32 src __maybe_unused,
+		      __u32 dst __maybe_unused, __u32 dst_id __maybe_unused,
+		      int reason, __u32 exitcode, enum metric_dir direction)
 {
 	update_metrics(ctx_full_len(ctx), direction, (__u8)-reason);
 	return exitcode;
 }
 #endif /* DROP_NOTIFY */
 
-static __always_inline int send_drop_notify_error(struct __ctx_buff *ctx, __u32 src,
-						  int error, int exitcode,
-						  enum metric_dir direction)
+static __always_inline int _send_drop_notify_error(__u8 file, __u16 line,
+						   struct __ctx_buff *ctx, __u32 src,
+						   int error, __u32 exitcode,
+						   enum metric_dir direction)
 {
-	return send_drop_notify(ctx, src, 0, 0, error, exitcode, direction);
+	return _send_drop_notify(file, line, ctx, src, 0, 0, error, exitcode, direction);
 }
+
+#ifndef __MAGIC_FILE__
+#define __MAGIC_FILE__ 0
+#endif
+
+#define send_drop_notify(...) _send_drop_notify(__MAGIC_FILE__, __LINE__, __VA_ARGS__)
+#define send_drop_notify_error(...) _send_drop_notify_error(__MAGIC_FILE__, __LINE__, __VA_ARGS__)
 
 #endif /* __LIB_DROP__ */
