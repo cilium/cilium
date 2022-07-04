44d9939a0f96140b15dcb2a4025ef922d321b4d7
Jussi Maki <jussi@isovalent.com>
2022-07-04 14:57:17 +0200

    test/control-plane: Add tests for nodeport and hostport
    Test case for checking that lbmap entries for nodeport and hostport
    are correctly created. Test inputs generated by generate.sh that
    creates a simple echo server that is exposed via ClusterIP, NodePort and
    HostPort services.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

---
+++ test/control-plane/services/nodeport/generate.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# Generate the golden test files for the NodePort test.
+# Reuses kind configs from the dual-stack test.
+#
+
+set -eux
+
+export KUBECONFIG=kubeconfig
+
+versions=(1.20 1.22 1.24)
+
+for version in ${versions[*]}; do
+    mkdir -p v${version}
+
+    : Start a kind cluster
+    kind create cluster --config ../dual-stack/manifests/kind-config-${version}.yaml --name nodeport
+
+    : Wait for service account to be created
+    until kubectl get serviceaccount/default; do
+        sleep 5
+    done
+
+    : Install cilium
+    cilium install --wait
+
+    : Dump the initial state
+    kubectl get nodes,ciliumnodes,services,endpoints,endpointslices -o yaml > v${version}/init.yaml
+
+    : Apply the manifest
+    kubectl create namespace test
+    kubectl apply -f manifests/nodeport.yaml
+
+    : Wait for all pods
+    kubectl wait -n test --for=condition=ready --timeout=60s --all pods
+
+    : Dump the services and endpoints
+    kubectl get -n test services,endpoints,endpointslices,pods -o yaml > v${version}/state1.yaml
+
+    : Tear down the cluster
+    kind delete clusters nodeport
+    rm -f kubeconfig
+
+done
---
+++ test/control-plane/services/nodeport/v1.24/nodeport_test.go
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package v1_24
+
+import (
+	"testing"
+
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/test/control-plane/services"
+)
+
+func TestNodePort(t *testing.T) {
+	modConfig := func(c *option.DaemonConfig) { c.EnableNodePort = true }
+	services.NewGoldenServicesTest(t, "nodeport-control-plane").Run(t, "1.24", modConfig)
+}
