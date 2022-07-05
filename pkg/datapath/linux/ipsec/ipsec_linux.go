cilium/cilium

commit 3d6ed0432ad94e0615c3417cb17fc487270331b8

nodediscovery: add LocalNode method
This method will be used in a subsequent commit to retrieve a copy of
the localNode object stored in the nodeDiscovery one.

Signed-off-by: Gilberto Bertin <jibi@cilium.io>

pkg/nodediscovery/nodediscovery.go
@@ -39,6 +39,7 @@ import (
 	"github.com/cilium/cilium/pkg/node/addressing"
 	nodemanager "github.com/cilium/cilium/pkg/node/manager"
 	nodestore "github.com/cilium/cilium/pkg/node/store"
+	"github.com/cilium/cilium/pkg/node/types"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/source"
@@ -322,6 +323,16 @@ func (n *NodeDiscovery) UpdateLocalNode() {
 
 	n.fillLocalNode()
 	n.updateLocalNode()
+}
+
+// LocalNode syncs the localNode object with the information stored in the node
+// package and then returns a copy of the localNode object
+func (n *NodeDiscovery) LocalNode() types.Node {
+	n.localNodeLock.Lock()
+	defer n.localNodeLock.Unlock()
+
+	n.fillLocalNode()
+	return n.localNode
 }
 
 // Close shuts down the node discovery engine

commit 9f1ea7c828bc587f11d035d487f213746f958233

nodediscovery: make LocalNode return a deep copy of localNode
as that object contains a few slices that will get modified after
StartDiscovery() is called

Fixes: 3d6ed0432a ("nodediscovery: add LocalNode method")
Suggested-by: Sebastian Wicki <sebastian@isovalent.com>
Signed-off-by: Gilberto Bertin <jibi@cilium.io>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -692,7 +692,7 @@ func keyfileWatcher(ctx context.Context, watcher *fswatcher.Watcher, keyfilePath
 			// nodeUpdate(), which is responsible for updating the
 			// IPSec policies and states for all the different EPs
 			// with ipsec.UpsertIPsecEndpoint()
-			nodeHandler.NodeValidateImplementation(nodediscovery.LocalNode())
+			nodeHandler.NodeValidateImplementation(*nodediscovery.LocalNode())
 
 			// Publish the updated node information to k8s/KVStore
 			nodediscovery.UpdateLocalNode()

pkg/nodediscovery/nodediscovery.go
@@ -327,12 +327,12 @@ func (n *NodeDiscovery) UpdateLocalNode() {
 
 // LocalNode syncs the localNode object with the information stored in the node
 // package and then returns a copy of the localNode object
-func (n *NodeDiscovery) LocalNode() types.Node {
+func (n *NodeDiscovery) LocalNode() *types.Node {
 	n.localNodeLock.Lock()
 	defer n.localNodeLock.Unlock()
 
 	n.fillLocalNode()
-	return n.localNode
+	return n.localNode.DeepCopy()
 }
 
 // Close shuts down the node discovery engine