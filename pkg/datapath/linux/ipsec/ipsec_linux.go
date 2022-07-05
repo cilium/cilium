5ebda99ae23b05e75b1af5cbdcdc1f83e4e7c224
Gilberto Bertin <jibi@cilium.io>
2022-07-05 11:17:19 +0200

    nodediscovery: make LocalNode return a deep copy of localNode
    [ upstream commit 88e683528e92e20fe9557520a869030f234738a5 ]
    
    as that object contains a few slices that will get modified after
    StartDiscovery() is called
    
    Fixes: 3d6ed0432a ("nodediscovery: add LocalNode method")
    Suggested-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -660,7 +660,7 @@ func keyfileWatcher(ctx context.Context, watcher *fswatcher.Watcher, keyfilePath
 			// nodeUpdate(), which is responsible for updating the
 			// IPSec policies and states for all the different EPs
 			// with ipsec.UpsertIPsecEndpoint()
-			nodeHandler.NodeValidateImplementation(nodediscovery.LocalNode())
+			nodeHandler.NodeValidateImplementation(*nodediscovery.LocalNode())
 
 			// Publish the updated node information to k8s/KVStore
 			nodediscovery.UpdateLocalNode()
