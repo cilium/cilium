bfdd96655f4ac34012c9679c75ee481a65d349f6
Aditi Ghag <aditi@cilium.io>
2022-07-05 22:45:19 -0700

    pkg/k8s/endpoint: Track terminating endpoint events
    Increment the metric used to track terminating endpoint
    events received from Kubernetes. This can help
    to check if service endpoints can be gracefully
    terminated by cilium. Cilium agent will receive
    these events (and thereby increment the metric)
    only when the Kubernetes feature gate
    `EndpointSliceTerminatingCondition` is enabled, and
    service endpoint pods are correctly configured with
    a graceful termination period.
    
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

--- pkg/k8s/endpoints.go
+++ pkg/k8s/endpoints.go
@@ -19,6 +19,7 @@ import (
 	slim_discovery_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1beta1"
 	"github.com/cilium/cilium/pkg/k8s/version"
 	"github.com/cilium/cilium/pkg/loadbalancer"
+	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	serviceStore "github.com/cilium/cilium/pkg/service/store"
 )
@@ -196,6 +197,7 @@ func ParseEndpointSliceV1Beta1(ep *slim_discovery_v1beta1.EndpointSlice) (Endpoi
 				if option.Config.EnableK8sTerminatingEndpoint {
 					if sub.Conditions.Terminating != nil && *sub.Conditions.Terminating {
 						backend.Terminating = true
+						metrics.TerminatingEndpointsEvents.WithLabelValues(metrics.LabelSourceNodeName)
 					}
 				}
 			}
@@ -281,6 +283,7 @@ func ParseEndpointSliceV1(ep *slim_discovery_v1.EndpointSlice) (EndpointSliceID,
 				if option.Config.EnableK8sTerminatingEndpoint {
 					if sub.Conditions.Terminating != nil && *sub.Conditions.Terminating {
 						backend.Terminating = true
+						metrics.TerminatingEndpointsEvents.WithLabelValues(metrics.LabelSourceNodeName)
 					}
 				}
 			}
