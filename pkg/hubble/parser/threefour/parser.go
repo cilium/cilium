cilium/cilium

commit ac7f8950acd64ec86958528eb5d050bd49dd1568

datapath/link: Initialize link monitor explicitly
Explicitly initializing the link cache as part of the outermost binaries
that depend on it: cilium-agent and the in-pod cilium CLI. This makes
the dependencies more explicit, makes the logic easier to track at
runtime (eg via `cilium status`), more resilient to failures, allows the
dependencies to be tracked from the daemon more concretely, and reduces
platform dependencies in pkg/ code & tests.

The package-level variable introduced here in pkg/monitor to track the
LinkCache will be removed in a subsequent change, this is just helpful
for now to reduce the size of this particular patch.

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/hubble/parser/threefour/parser.go
@@ -18,7 +18,6 @@ import (
 
 	pb "github.com/cilium/cilium/api/v1/flow"
 	"github.com/cilium/cilium/pkg/byteorder"
-	"github.com/cilium/cilium/pkg/datapath/link"
 	"github.com/cilium/cilium/pkg/hubble/parser/errors"
 	"github.com/cilium/cilium/pkg/hubble/parser/getters"
 	"github.com/cilium/cilium/pkg/identity"
@@ -36,6 +35,7 @@ type Parser struct {
 	dnsGetter      getters.DNSGetter
 	ipGetter       getters.IPGetter
 	serviceGetter  getters.ServiceGetter
+	linkGetter     getters.LinkGetter
 
 	// TODO: consider using a pool of these
 	packet *packet
@@ -63,6 +63,7 @@ func New(
 	dnsGetter getters.DNSGetter,
 	ipGetter getters.IPGetter,
 	serviceGetter getters.ServiceGetter,
+	linkGetter getters.LinkGetter,
 ) (*Parser, error) {
 	packet := &packet{}
 	packet.decLayer = gopacket.NewDecodingLayerParser(
@@ -81,6 +82,7 @@ func New(
 		identityGetter: identityGetter,
 		ipGetter:       ipGetter,
 		serviceGetter:  serviceGetter,
+		linkGetter:     linkGetter,
 		packet:         packet,
 	}, nil
 }
@@ -204,7 +206,7 @@ func (p *Parser) Decode(data []byte, decoded *pb.Flow) error {
 	decoded.DestinationService = destinationService
 	decoded.PolicyMatchType = decodePolicyMatchType(pvn)
 	decoded.DebugCapturePoint = decodeDebugCapturePoint(dbg)
-	decoded.Interface = decodeNetworkInterface(tn, dbg)
+	decoded.Interface = p.decodeNetworkInterface(tn, dbg)
 	decoded.ProxyPort = decodeProxyPort(dbg)
 	decoded.Summary = summary
 
@@ -656,7 +658,7 @@ func decodeDebugCapturePoint(dbg *monitor.DebugCapture) pb.DebugCapturePoint {
 	return pb.DebugCapturePoint(dbg.SubType)
 }
 
-func decodeNetworkInterface(tn *monitor.TraceNotify, dbg *monitor.DebugCapture) *pb.NetworkInterface {
+func (p *Parser) decodeNetworkInterface(tn *monitor.TraceNotify, dbg *monitor.DebugCapture) *pb.NetworkInterface {
 	ifIndex := uint32(0)
 	if tn != nil {
 		ifIndex = tn.Ifindex
@@ -678,7 +680,7 @@ func decodeNetworkInterface(tn *monitor.TraceNotify, dbg *monitor.DebugCapture) 
 
 	// if the interface is not found, `name` will be an empty string and thus
 	// omitted in the protobuf message
-	name, _ := link.GetIfNameCached(int(ifIndex))
+	name, _ := p.linkGetter.GetIfNameCached(int(ifIndex))
 	return &pb.NetworkInterface{
 		Index: ifIndex,
 		Name:  name,

pkg/hubble/parser/threefour/parser_test.go
@@ -48,7 +48,8 @@ func init() {
 
 func TestL34DecodeEmpty(t *testing.T) {
 	parser, err := New(log, &testutils.NoopEndpointGetter, &testutils.NoopIdentityGetter,
-		&testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter)
+		&testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter,
+		&testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
 	var d []byte
@@ -149,7 +150,7 @@ func TestL34Decode(t *testing.T) {
 		},
 	}
 	identityCache := &testutils.NoopIdentityGetter
-	parser, err := New(log, endpointGetter, identityCache, dnsGetter, ipGetter, serviceGetter)
+	parser, err := New(log, endpointGetter, identityCache, dnsGetter, ipGetter, serviceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
 	f := &flowpb.Flow{}
@@ -218,7 +219,7 @@ func TestL34Decode(t *testing.T) {
 	}
 	ipGetter = &testutils.NoopIPGetter
 	serviceGetter = &testutils.NoopServiceGetter
-	parser, err = New(log, endpointGetter, identityCache, dnsGetter, ipGetter, serviceGetter)
+	parser, err = New(log, endpointGetter, identityCache, dnsGetter, ipGetter, serviceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
 	err = parser.Decode(d2, f)
@@ -257,7 +258,7 @@ func BenchmarkL34Decode(b *testing.B) {
 	ipGetter := &testutils.NoopIPGetter
 	serviceGetter := &testutils.NoopServiceGetter
 	identityCache := &testutils.NoopIdentityGetter
-	parser, err := New(log, endpointGetter, identityCache, dnsGetter, ipGetter, serviceGetter)
+	parser, err := New(log, endpointGetter, identityCache, dnsGetter, ipGetter, serviceGetter, &testutils.NoopLinkGetter)
 	require.NoError(b, err)
 
 	f := &flowpb.Flow{}
@@ -301,7 +302,7 @@ func TestDecodeTraceNotify(t *testing.T) {
 		return nil, fmt.Errorf("identity not found for %d", securityIdentity)
 	}}
 
-	parser, err := New(log, &testutils.NoopEndpointGetter, identityGetter, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter)
+	parser, err := New(log, &testutils.NoopEndpointGetter, identityGetter, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
 	f := &flowpb.Flow{}
@@ -346,7 +347,7 @@ func TestDecodeDropNotify(t *testing.T) {
 		},
 	}
 
-	parser, err := New(log, &testutils.NoopEndpointGetter, identityGetter, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter)
+	parser, err := New(log, &testutils.NoopEndpointGetter, identityGetter, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
 	f := &flowpb.Flow{}
@@ -367,7 +368,7 @@ func TestDecodePolicyVerdictNotify(t *testing.T) {
 		},
 	}
 
-	parser, err := New(log, &testutils.NoopEndpointGetter, identityGetter, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter)
+	parser, err := New(log, &testutils.NoopEndpointGetter, identityGetter, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
 	// PolicyVerdictNotify for forwarded flow
@@ -427,7 +428,7 @@ func TestDecodeDropReason(t *testing.T) {
 	data, err := testutils.CreateL3L4Payload(dn)
 	require.NoError(t, err)
 
-	parser, err := New(log, nil, nil, nil, nil, nil)
+	parser, err := New(log, nil, nil, nil, nil, nil, nil)
 	require.NoError(t, err)
 
 	f := &flowpb.Flow{}
@@ -452,7 +453,7 @@ func TestDecodeLocalIdentity(t *testing.T) {
 		},
 	}
 
-	parser, err := New(log, nil, identityGetter, nil, nil, nil)
+	parser, err := New(log, nil, identityGetter, nil, nil, nil, nil)
 	require.NoError(t, err)
 
 	f := &flowpb.Flow{}
@@ -509,7 +510,7 @@ func TestDecodeTrafficDirection(t *testing.T) {
 		},
 	}
 
-	parser, err := New(log, endpointGetter, nil, nil, nil, nil)
+	parser, err := New(log, endpointGetter, nil, nil, nil, nil, nil)
 	require.NoError(t, err)
 	parseFlow := func(event interface{}, srcIPv4, dstIPv4 net.IP) *flowpb.Flow {
 		data, err := testutils.CreateL3L4Payload(event,
@@ -649,7 +650,7 @@ func TestDecodeIsReply(t *testing.T) {
 	localIP := net.ParseIP("1.2.3.4")
 	remoteIP := net.ParseIP("5.6.7.8")
 
-	parser, err := New(log, nil, nil, nil, nil, nil)
+	parser, err := New(log, nil, nil, nil, nil, nil, nil)
 	require.NoError(t, err)
 	parseFlow := func(event interface{}, srcIPv4, dstIPv4 net.IP) *flowpb.Flow {
 		data, err := testutils.CreateL3L4Payload(event,
@@ -801,7 +802,7 @@ func Test_filterCIDRLabels(t *testing.T) {
 
 func TestTraceNotifyOriginalIP(t *testing.T) {
 	f := &flowpb.Flow{}
-	parser, err := New(log, &testutils.NoopEndpointGetter, nil, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter)
+	parser, err := New(log, &testutils.NoopEndpointGetter, nil, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
 	v0 := monitor.TraceNotifyV0{
@@ -839,7 +840,7 @@ func TestTraceNotifyOriginalIP(t *testing.T) {
 }
 
 func TestICMP(t *testing.T) {
-	parser, err := New(log, &testutils.NoopEndpointGetter, nil, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter)
+	parser, err := New(log, &testutils.NoopEndpointGetter, nil, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 	message := monitor.TraceNotifyV1{
 		TraceNotifyV0: monitor.TraceNotifyV0{
@@ -911,7 +912,7 @@ func TestTraceNotifyLocalEndpoint(t *testing.T) {
 		},
 	}
 
-	parser, err := New(log, endpointGetter, nil, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter)
+	parser, err := New(log, endpointGetter, nil, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
 	v0 := monitor.TraceNotifyV0{
@@ -946,7 +947,7 @@ func TestTraceNotifyLocalEndpoint(t *testing.T) {
 func TestDebugCapture(t *testing.T) {
 	f := &flowpb.Flow{}
 
-	parser, err := New(log, &testutils.NoopEndpointGetter, &testutils.NoopIdentityGetter, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter)
+	parser, err := New(log, &testutils.NoopEndpointGetter, &testutils.NoopIdentityGetter, &testutils.NoopDNSGetter, &testutils.NoopIPGetter, &testutils.NoopServiceGetter, &testutils.NoopLinkGetter)
 	require.NoError(t, err)
 
 	// Obtaining the index for the loopback device might not succeed depending

pkg/hubble/parser/parser.go
@@ -38,10 +38,11 @@ func New(
 	dnsGetter getters.DNSGetter,
 	ipGetter getters.IPGetter,
 	serviceGetter getters.ServiceGetter,
+	linkGetter getters.LinkGetter,
 	opts ...options.Option,
 ) (*Parser, error) {
 
-	l34, err := threefour.New(log, endpointGetter, identityGetter, dnsGetter, ipGetter, serviceGetter)
+	l34, err := threefour.New(log, endpointGetter, identityGetter, dnsGetter, ipGetter, serviceGetter, linkGetter)
 	if err != nil {
 		return nil, err
 	}

pkg/hubble/parser/parser_test.go
@@ -34,7 +34,7 @@ func init() {
 }
 
 func Test_InvalidPayloads(t *testing.T) {
-	p, err := New(log, nil, nil, nil, nil, nil)
+	p, err := New(log, nil, nil, nil, nil, nil, nil)
 	assert.NoError(t, err)
 
 	_, err = p.Decode(nil)
@@ -59,7 +59,7 @@ func Test_InvalidPayloads(t *testing.T) {
 }
 
 func Test_ParserDispatch(t *testing.T) {
-	p, err := New(log, nil, nil, nil, nil, nil)
+	p, err := New(log, nil, nil, nil, nil, nil, nil)
 	assert.NoError(t, err)
 
 	// Test L3/L4 record
@@ -94,7 +94,7 @@ func Test_ParserDispatch(t *testing.T) {
 }
 
 func Test_EventType_RecordLost(t *testing.T) {
-	p, err := New(log, nil, nil, nil, nil, nil)
+	p, err := New(log, nil, nil, nil, nil, nil, nil)
 	assert.NoError(t, err)
 
 	ts := time.Now()

cilium/cmd/monitor.go
@@ -18,6 +18,7 @@ import (
 	"github.com/spf13/viper"
 
 	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/datapath/link"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/monitor"
 	"github.com/cilium/cilium/pkg/monitor/agent/listener"
@@ -239,6 +240,8 @@ func runMonitor(args []string) {
 				nm.Cpus, nm.Npages, nm.Pagesize)
 		}
 	}
+	linkCache := link.NewLinkCache()
+	monitor.Init(linkCache)
 	fmt.Fprintf(os.Stderr, "Press Ctrl-C to quit\n")
 
 	// On EOF, retry

daemon/cmd/daemon.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/counter"
 	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/datapath/link"
 	"github.com/cilium/cilium/pkg/datapath/linux/probes"
 	linuxrouting "github.com/cilium/cilium/pkg/datapath/linux/routing"
 	"github.com/cilium/cilium/pkg/datapath/loader"
@@ -154,6 +155,7 @@ type Daemon struct {
 	// endpoint's routing in ENI or Azure IPAM mode
 	healthEndpointRouting *linuxrouting.RoutingInfo
 
+	linkCache      *link.LinkCache
 	hubbleObserver *observer.LocalObserverServer
 
 	// k8sCachesSynced is closed when all essential Kubernetes caches have

daemon/cmd/hubble.go
@@ -18,6 +18,7 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	observerpb "github.com/cilium/cilium/api/v1/observer"
 	"github.com/cilium/cilium/pkg/crypto/certloader"
+	"github.com/cilium/cilium/pkg/datapath/link"
 	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
 	"github.com/cilium/cilium/pkg/hubble/container"
 	"github.com/cilium/cilium/pkg/hubble/exporter"
@@ -40,6 +41,7 @@ import (
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	ciliumMonitor "github.com/cilium/cilium/pkg/monitor"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 )
@@ -109,7 +111,9 @@ func (d *Daemon) launchHubble() {
 		observerOpts = append(observerOpts, opt)
 	}
 
-	payloadParser, err := parser.New(logger, d, d, d, d, d)
+	d.linkCache = link.NewLinkCache()
+	ciliumMonitor.Init(d.linkCache)
+	payloadParser, err := parser.New(logger, d, d, d, d, d, d.linkCache)
 	if err != nil {
 		logger.WithError(err).Error("Failed to initialize Hubble")
 		return

pkg/datapath/link/link.go
@@ -24,10 +24,6 @@ var (
 	once      sync.Once
 )
 
-func init() {
-	NewLinkCache()
-}
-
 // DeleteByName deletes the interface with the name ifName.
 func DeleteByName(ifName string) error {
 	iface, err := netlink.LinkByName(ifName)
@@ -118,6 +114,6 @@ func (c *LinkCache) lookupName(ifIndex int) (string, bool) {
 
 // GetIfNameCached returns the name of an interface (if it exists) by looking
 // it up in a regularly updated cache
-func GetIfNameCached(ifIndex int) (string, bool) {
-	return linkCache.lookupName(ifIndex)
+func (c *LinkCache) GetIfNameCached(ifIndex int) (string, bool) {
+	return c.lookupName(ifIndex)
 }

commit 6462115a5735bc2d568f15a5ad2003b67e187d7b

hubble/parser/threefour: check (*Parser).linkGetter before accessing it
It could be nil, though there are currently no code paths in Cilium that
pass it as nil. Add a test to verify that decoding from a (*Parser)
with all nil getters doesn't segfault.

Found by OSS-Fuzz: https://github.com/cilium/cilium/runs/7253558124?check_suite_focus=true

Fixes: ac7f8950acd6 ("datapath/link: Initialize link monitor explicitly")
Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/hubble/parser/threefour/parser.go
@@ -664,9 +664,12 @@ func (p *Parser) decodeNetworkInterface(tn *monitor.TraceNotify, dbg *monitor.De
 		return nil
 	}
 
-	// if the interface is not found, `name` will be an empty string and thus
-	// omitted in the protobuf message
-	name, _ := p.linkGetter.GetIfNameCached(int(ifIndex))
+	var name string
+	if p.linkGetter != nil {
+		// if the interface is not found, `name` will be an empty string and thus
+		// omitted in the protobuf message
+		name, _ = p.linkGetter.GetIfNameCached(int(ifIndex))
+	}
 	return &pb.NetworkInterface{
 		Index: ifIndex,
 		Name:  name,

pkg/monitor/datapath_debug.go
@@ -254,7 +254,10 @@ func (n *DebugMsg) Message(linkMonitor getters.LinkGetter) string {
 	case DbgEncap:
 		return fmt.Sprintf("Encapsulating to node %d (%#x) from seclabel %d", n.Arg1, n.Arg1, n.Arg2)
 	case DbgLxcFound:
-		ifname := linkMonitor.Name(n.Arg1)
+		var ifname string
+		if linkMonitor != nil {
+			ifname = linkMonitor.Name(n.Arg1)
+		}
 		return fmt.Sprintf("Local container found ifindex %s seclabel %d", ifname, byteorder.NetworkToHost16(uint16(n.Arg2)))
 	case DbgPolicyDenied:
 		return fmt.Sprintf("Policy evaluation would deny packet from %d to %d", n.Arg1, n.Arg2)

pkg/hubble/parser/threefour/parser_test.go
@@ -180,6 +180,11 @@ func TestL34Decode(t *testing.T) {
 
 	assert.Equal(t, flowpb.TraceObservationPoint_FROM_HOST, f.GetTraceObservationPoint())
 
+	nilParser, err := New(log, nil, nil, nil, nil, nil, nil)
+	require.NoError(t, err)
+	err = nilParser.Decode(d, f)
+	require.NoError(t, err)
+
 	// ICMP packet so no ports until that support is merged into master
 	//
 	//SOURCE              DESTINATION          TYPE   SUMMARY
@@ -240,6 +245,9 @@ func TestL34Decode(t *testing.T) {
 	assert.Equal(t, (*flowpb.TCPFlags)(nil), f.L4.GetTCP().GetFlags())
 
 	assert.Equal(t, flowpb.TraceObservationPoint_FROM_ENDPOINT, f.GetTraceObservationPoint())
+
+	err = nilParser.Decode(d, f)
+	require.NoError(t, err)
 }
 
 func BenchmarkL34Decode(b *testing.B) {
@@ -979,6 +987,11 @@ func TestDebugCapture(t *testing.T) {
 		Name:  loIfName,
 	}, f.Interface)
 
+	nilParser, err := New(log, nil, nil, nil, nil, nil, nil)
+	require.NoError(t, err)
+	err = nilParser.Decode(data, f)
+	require.NoError(t, err)
+
 	dbg = monitor.DebugCapture{
 		Type:    api.MessageTypeCapture,
 		SubType: monitor.DbgCaptureProxyPost,
@@ -994,6 +1007,9 @@ func TestDebugCapture(t *testing.T) {
 	assert.Equal(t, int32(dbg.SubType), f.EventType.SubType)
 	assert.Equal(t, flowpb.DebugCapturePoint_DBG_CAPTURE_PROXY_POST, f.DebugCapturePoint)
 	assert.Equal(t, uint32(1234), f.ProxyPort)
+
+	err = nilParser.Decode(data, f)
+	require.NoError(t, err)
 }
 
 func TestTraceNotifyProxyPort(t *testing.T) {