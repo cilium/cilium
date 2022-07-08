b4543a6068bf44d964bbe2985b6547bd9c32beda
Tobias Klauser <tobias@cilium.io>
2022-07-08 10:40:30 +0200

    go.mod, vendor: update gops to v0.3.25
    [ upstream commit 574e6552ca3cddb55d1ad59e583229852b151b1e ]
    
    This fixes a data race in the gops agent Listen method.
    
    Release notes: https://github.com/google/gops/releases/tag/v0.3.25
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- vendor/github.com/google/gops/agent/agent.go
+++ vendor/github.com/google/gops/agent/agent.go
@@ -120,14 +120,14 @@ func Listen(opts Options) error {
 		return err
 	}
 
-	go listen()
+	go listen(listener)
 	return nil
 }
 
-func listen() {
+func listen(l net.Listener) {
 	buf := make([]byte, 1)
 	for {
-		fd, err := listener.Accept()
+		fd, err := l.Accept()
 		if err != nil {
 			// No great way to check for this, see https://golang.org/issues/4373.
 			if !strings.Contains(err.Error(), "use of closed network connection") {
