09d51574fdc9c74ab269fb735cad3787be6f9fad
Tobias Klauser <tobias@cilium.io>
2022-07-08 16:41:18 +0200

    connectivity: test L7 ingress policy with HTTP headers
    The echo service we use for L7 HTTP policy testing conveniently provides
    two endpoints: /public and /private (see [1]). Adjust the existing
    echo-ingress-l7-http.yaml policy and the respective test to match
    against both endpoints. To test L7 HTTP policies with HTTP headers [2],
    the /private endpoint requires the X-Very-Secret-Token HTTP header
    to be set to 42.
    
    With this extended test in place the L7 policy connectivity tests cover
    the same functionality as the l7_demos.go Star Wars demo test [3] in the
    cilium/cilium repository which can now be dropped.
    
    [1] https://github.com/cilium/json-mock/blob/master/default.json
    [2] https://docs.cilium.io/en/v1.12.0-rc3/policy/language/#all-get-path1-and-put-path2-when-header-set
    [3] https://github.com/cilium/cilium/blob/4298879e53a2c9ff6bce3442920f9ab8b9d812da/test/k8s/l7_demos.go
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/connectivity/tests/common.go
+++ cilium-cli/connectivity/tests/common.go
@@ -10,17 +10,19 @@ import (
 	"github.com/cilium/cilium-cli/connectivity/check"
 )
 
-func curl(peer check.TestPeer) []string {
-	return []string{"curl",
+func curl(peer check.TestPeer, opts ...string) []string {
+	cmd := []string{"curl",
 		"-w", "%{local_ip}:%{local_port} -> %{remote_ip}:%{remote_port} = %{response_code}",
 		"--silent", "--fail", "--show-error",
 		"--connect-timeout", "5",
 		"--output", "/dev/null",
-		fmt.Sprintf("%s://%s%s",
-			peer.Scheme(),
-			net.JoinHostPort(peer.Address(), fmt.Sprint(peer.Port())),
-			peer.Path()),
 	}
+	cmd = append(cmd, opts...)
+	cmd = append(cmd, fmt.Sprintf("%s://%s%s",
+		peer.Scheme(),
+		net.JoinHostPort(peer.Address(), fmt.Sprint(peer.Port())),
+		peer.Path()))
+	return cmd
 }
 
 func ping(peer check.TestPeer) []string {
