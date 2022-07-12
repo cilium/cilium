5cacb1bbb9e4a7e6dc5f09d688e51373b748c781
Tam Mach <tam.mach@cilium.io>
2022-07-12 17:45:55 +1000

    k8s: Filter out cilium owned from pod labels
    This commit is to make sure that any of cilium owned labels (i.e. prefix
    by io.cilium.k8s.*) should be filtered out if available in pod labels.
    The main reason is to prevent any malicious injection of cilium labels
    especially io.cilium.k8s.namespace.labels.*.
    
    Test Application
    
    ```yaml
    ---
    apiVersion: apps/v1
    kind: Deployment
    metadata:
      labels:
        io.cilium.k8s.namespace.labels.foo.bar/baz: malicious-pod-level-override
        io.cilium.k8s.namespace.labels.kubernetes.io/metadata.name: kube-system
        io.cilium.k8s.policy.cluster: minikube-bad
        io.cilium.k8s.policy.serviceaccount: root
        io.cilium.k8s.namespace.labels.random: foo
        app: netshoot
      name: netshoot
    spec:
      selector:
        matchLabels:
          io.cilium.k8s.namespace.labels.foo.bar/baz: malicious-pod-level-override
          io.cilium.k8s.namespace.labels.kubernetes.io/metadata.name: kube-system
          io.cilium.k8s.policy.cluster: minikube-bad
          io.cilium.k8s.policy.serviceaccount: root
          io.cilium.k8s.namespace.labels.random: foo
          app: netshoot
      replicas: 1
      template:
        metadata:
          labels:
            io.cilium.k8s.namespace.labels.foo.bar/baz: malicious-pod-level-override
            io.cilium.k8s.namespace.labels.kubernetes.io/metadata.name: kube-system
            io.cilium.k8s.policy.cluster: minikube-bad
            io.cilium.k8s.policy.serviceaccount: root
            io.cilium.k8s.namespace.labels.random: foo
            app: netshoot
        spec:
          containers:
            - name: netshoot
              args:
                - sleep
                - infinity
              image: nicolaka/netshoot:latest
    ```
    
    Before:
    
    ```
    31978   k8s:app=netshoot
            k8s:io.cilium.k8s.namespace.labels.foo.bar/baz=malicious-pod-level-override
            k8s:io.cilium.k8s.namespace.labels.kubernetes.io/metadata.name=default
            k8s:io.cilium.k8s.namespace.labels.random=foo
            k8s:io.cilium.k8s.policy.cluster=default
            k8s:io.cilium.k8s.policy.serviceaccount=default
            k8s:io.kubernetes.pod.namespace=default
    ```
    
    After:
    
    ```
    768     k8s:app=netshoot
            k8s:io.cilium.k8s.namespace.labels.kubernetes.io/metadata.name=default
            k8s:io.cilium.k8s.policy.cluster=default
            k8s:io.cilium.k8s.policy.serviceaccount=default
            k8s:io.kubernetes.pod.namespace=default
    ```
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/k8s/labels.go
+++ pkg/k8s/labels.go
@@ -16,6 +16,7 @@ package k8s
 
 import (
 	"regexp"
+	"strings"
 
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
@@ -105,10 +106,8 @@ func GetPodMetadata(k8sNs *slim_corev1.Namespace, pod *slim_corev1.Pod) (contain
 
 	objMetaCpy := pod.ObjectMeta.DeepCopy()
 	annotations := objMetaCpy.Annotations
-	k8sLabels := objMetaCpy.Labels
-	if k8sLabels == nil {
-		k8sLabels = map[string]string{}
-	}
+	k8sLabels := filterPodLabels(objMetaCpy.Labels)
+
 	for k, v := range k8sNs.GetLabels() {
 		k8sLabels[policy.JoinPath(k8sConst.PodNamespaceMetaLabels, k)] = v
 	}
@@ -124,8 +123,9 @@ func GetPodMetadata(k8sNs *slim_corev1.Namespace, pod *slim_corev1.Pod) (contain
 	// Cilium, add a label to notify that.
 	// If the pod already contains that label to explicitly enable or disable
 	// the sidecar proxy mode, keep it as is.
-	if _, ok := k8sLabels[k8sConst.PolicyLabelIstioSidecarProxy]; !ok &&
-		isInjectedWithIstioSidecarProxy(scopedLog, pod) {
+	if val, ok := objMetaCpy.Labels[k8sConst.PolicyLabelIstioSidecarProxy]; ok {
+		k8sLabels[k8sConst.PolicyLabelIstioSidecarProxy] = val
+	} else if isInjectedWithIstioSidecarProxy(scopedLog, pod) {
 		k8sLabels[k8sConst.PolicyLabelIstioSidecarProxy] = "true"
 	}
 
@@ -138,4 +138,16 @@ func GetPodMetadata(k8sNs *slim_corev1.Namespace, pod *slim_corev1.Pod) (contain
 	}
 
 	return containerPorts, k8sLabels, annotations, nil
+}
+
+// filterPodLabels returns a copy of the given labels map, without the labels owned by Cilium.
+func filterPodLabels(labels map[string]string) map[string]string {
+	res := map[string]string{}
+	for k, v := range labels {
+		if strings.HasPrefix(k, k8sConst.LabelPrefix) {
+			continue
+		}
+		res[k] = v
+	}
+	return res
 }
