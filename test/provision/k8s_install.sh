9a550ca5bceea214b3522ab143d7dd58ce1424cc
André Martins <andre@cilium.io>
2022-07-13 20:04:53 +0200

    update k8s versions to the latest releases
    Update k8s library versions:
    - 1.21.14
    
    Followed the steps mentioned in https://docs.cilium.io/en/latest/contributing/development/dev_setup/#patch-version
    
    Signed-off-by: André Martins <andre@cilium.io>

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -291,7 +291,7 @@ case $K8S_VERSION in
         sudo apt-get install -y conntrack
         KUBERNETES_CNI_VERSION="0.8.7"
         KUBERNETES_CNI_OS="-linux"
-        K8S_FULL_VERSION="1.21.11"
+        K8S_FULL_VERSION="1.21.14"
         KUBEADM_OPTIONS="--ignore-preflight-errors=cri,swap"
         KUBEADM_SLAVE_OPTIONS="--discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors=cri,SystemVerification,swap"
         sudo ln -sf $COREDNS_DEPLOYMENT $DNS_DEPLOYMENT
