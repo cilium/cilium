fbf8ab20f989a2d961f57ae5b5a7d93f943efb08
Jared Ledvina <jared.ledvina@datadoghq.com>
2022-07-14 14:54:41 -0400

    Support custom agent daemonset name
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: Jared Ledvina <jared.ledvina@datadoghq.com>

--- cilium-cli/internal/cli/cmd/connectivity.go
+++ cilium-cli/internal/cli/cmd/connectivity.go
@@ -111,6 +111,7 @@ func newCmdConnectivityTest() *cobra.Command {
 	cmd.Flags().BoolVar(&params.Hubble, "hubble", true, "Automatically use Hubble for flow validation & troubleshooting")
 	cmd.Flags().StringVar(&params.HubbleServer, "hubble-server", "localhost:4245", "Address of the Hubble endpoint for flow validation")
 	cmd.Flags().StringVar(&params.TestNamespace, "test-namespace", defaults.ConnectivityCheckNamespace, "Namespace to perform the connectivity test in")
+	cmd.Flags().StringVar(&params.AgentDaemonSetName, "agent-daemonset-name", defaults.AgentDaemonSetName, "Name of cilium agent daemonset")
 	cmd.Flags().StringVar(&params.MultiCluster, "multi-cluster", "", "Test across clusters to given context")
 	cmd.Flags().StringSliceVar(&tests, "test", []string{}, "Run tests that match one of the given regular expressions, skip tests by starting the expression with '!', target Scenarios with e.g. '/pod-to-cidr'")
 	cmd.Flags().StringVar(&params.FlowValidation, "flow-validation", check.FlowValidationModeWarning, "Enable Hubble flow validation { disabled | warning | strict }")
