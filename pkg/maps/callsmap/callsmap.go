38aa8692fb55a38a614a6b07aa617120f59e4bbb
Paul Chaignon <paul@cilium.io>
2022-07-15 17:18:59 +0200

    loader: Allow TestCompileAndLoadHostEndpoint to work without IPVLAN
    The TestCompileAndLoadHostEndpoint "unit" test is covering the loading
    of the host datapath (reloadHostDatapath). Currently, when IPVLAN is
    disabled, part of this code path consists in patching and loading a
    second object file for the second host device (cilium_net). This part is
    a trickier to test because it requires a bit more information. So the
    test skips it by enabling IPVLAN.
    
    A subsequent commit will remove all IPVLAN support, so we need to find
    another way to make the test work. This commit achieves that by setting
    up all configuration necessary for the patching and loading of
    cilium_net's BPF programs to succeed.
    
    The main struggle here is that instead of working from the object file
    for cilium_host and patching it with the information for cilium_net, we
    are working from an object file compiled with node_config.h's default
    contents.
    
    First, the node_config.h defines the endpoint ID as 65535 so we need
    to assign that ID to the host endpoint to avoid errors such as:
    
        no such string \"test_cilium_calls_00001\" in ELF
    
    Then, node_config.h adds a test_ prefix to map names so we need to
    redefine map names in the agent to avoid the following errors. In order
    to do that, we make callsmap.{Host,Netdev}MapName variables instead of
    constants.
    
        failed to write ELF file: test_loader/bpf_host_cilium_net.o: no such string \"cilium_policy_65535\" in ELF
        failed to write ELF file: test_loader/bpf_host_cilium_net.o: no such string \"cilium_calls_hostns_65535\" in ELF
    
    Of course, we also need to create the endpoint's state directory to be
    able to write the patched object file there.
    
    Finally, we need to make SECCTX_FROM_IPCACHE into a proper static data
    variable in our node_config.h so that it appears as a symbol we can
    replace in the object file.
    
    With all that, we can patch and load the object file for cilium_net.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/maps/callsmap/callsmap.go
+++ pkg/maps/callsmap/callsmap.go
@@ -6,11 +6,14 @@ package callsmap
 const (
 	// MapName is the prefix of the BPF map.
 	MapName = "cilium_calls_"
+	// CustomCallsMapName is the name prefix for the per-endpoint prog
+	// array maps used for loading user-defined eBPF programs.
+	CustomCallsMapName = MapName + "custom_"
+)
+
+var (
 	// HostMapName and NetdevMapName are name prefixes for the host
 	// datapath BPF maps. They must be different but have the same length.
 	HostMapName   = MapName + "hostns_"
 	NetdevMapName = MapName + "netdev_"
-	// CustomCallsMapName is the name prefix for the per-endpoint prog
-	// array maps used for loading user-defined eBPF programs.
-	CustomCallsMapName = MapName + "custom_"
 )
