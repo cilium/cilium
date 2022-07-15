cilium/cilium

commit 5b05cc92dd664f918f0712247b0f4d3a48850393

bpf: Split handle_lxc_traffic with tail calls
handle_lxc_traffic handles packets to and from local endpoints when
per-endpoint routes are enabled. For that reason, it calls into the host
firewall enforcement functions for ingress and egress, IPv4 and IPv6.

All of this code causes program size issues on older kernels where the
4K instructions limit applies. To work around this we can split the
ingress handling into IPv4 and IPv6 programs.

Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/bpf_host.c
@@ -1134,6 +1134,38 @@ out:
 }
 
 #if defined(ENABLE_HOST_FIREWALL) && !defined(ENABLE_ROUTING)
+#ifdef ENABLE_IPV6
+declare_tailcall_if(__or(__and(is_defined(ENABLE_IPV4), is_defined(ENABLE_IPV6)),
+			 is_defined(DEBUG)), CILIUM_CALL_IPV6_TO_HOST_POLICY_ONLY)
+int tail_ipv6_host_policy_ingress(struct __ctx_buff *ctx)
+{
+	__u32 srcID = 0;
+	int ret;
+
+	ret = ipv6_host_policy_ingress(ctx, &srcID);
+	if (IS_ERR(ret))
+		return send_drop_notify_error(ctx, srcID, ret, CTX_ACT_DROP,
+					      METRIC_INGRESS);
+	return ret;
+}
+#endif /* ENABLE_IPV6 */
+
+#ifdef ENABLE_IPV4
+declare_tailcall_if(__or(__and(is_defined(ENABLE_IPV4), is_defined(ENABLE_IPV6)),
+			 is_defined(DEBUG)), CILIUM_CALL_IPV4_TO_HOST_POLICY_ONLY)
+int tail_ipv4_host_policy_ingress(struct __ctx_buff *ctx)
+{
+	__u32 srcID = 0;
+	int ret;
+
+	ret = ipv4_host_policy_ingress(ctx, &srcID);
+	if (IS_ERR(ret))
+		return send_drop_notify_error(ctx, srcID, ret, CTX_ACT_DROP,
+					      METRIC_INGRESS);
+	return ret;
+}
+#endif /* ENABLE_IPV4 */
+
 static __always_inline int
 /* Handles packet from a local endpoint entering the host namespace. Applies
  * ingress host policies.
@@ -1141,7 +1173,6 @@ static __always_inline int
 to_host_from_lxc(struct __ctx_buff *ctx __maybe_unused)
 {
 	int ret = CTX_ACT_OK;
-	__u32 src_id = 0;
 	__u16 proto = 0;
 
 	if (!validate_ethertype(ctx, &proto)) {
@@ -1157,12 +1188,20 @@ to_host_from_lxc(struct __ctx_buff *ctx __maybe_unused)
 # endif
 # ifdef ENABLE_IPV6
 	case bpf_htons(ETH_P_IPV6):
-		ret = ipv6_host_policy_ingress(ctx, &src_id);
+		invoke_tailcall_if(__or(__and(is_defined(ENABLE_IPV4),
+					      is_defined(ENABLE_IPV6)),
+					is_defined(DEBUG)),
+				   CILIUM_CALL_IPV6_TO_HOST_POLICY_ONLY,
+				   tail_ipv6_host_policy_ingress);
 		break;
 # endif
 # ifdef ENABLE_IPV4
 	case bpf_htons(ETH_P_IP):
-		ret = ipv4_host_policy_ingress(ctx, &src_id);
+		invoke_tailcall_if(__or(__and(is_defined(ENABLE_IPV4),
+					      is_defined(ENABLE_IPV6)),
+					is_defined(DEBUG)),
+				   CILIUM_CALL_IPV6_TO_HOST_POLICY_ONLY,
+				   tail_ipv4_host_policy_ingress);
 		break;
 # endif
 	default:
@@ -1172,7 +1211,7 @@ to_host_from_lxc(struct __ctx_buff *ctx __maybe_unused)
 
 out:
 	if (IS_ERR(ret))
-		return send_drop_notify_error(ctx, src_id, ret, CTX_ACT_DROP,
+		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
 					      METRIC_INGRESS);
 	return ret;
 }

bpf/lib/common.h
@@ -81,7 +81,9 @@
 #define CILIUM_CALL_NAT46			9
 #define CILIUM_CALL_IPV6_FROM_LXC		10
 #define CILIUM_CALL_IPV4_TO_LXC_POLICY_ONLY	11
+#define CILIUM_CALL_IPV4_TO_HOST_POLICY_ONLY	CILIUM_CALL_IPV4_TO_LXC_POLICY_ONLY
 #define CILIUM_CALL_IPV6_TO_LXC_POLICY_ONLY	12
+#define CILIUM_CALL_IPV6_TO_HOST_POLICY_ONLY	CILIUM_CALL_IPV6_TO_LXC_POLICY_ONLY
 #define CILIUM_CALL_IPV4_TO_ENDPOINT		13
 #define CILIUM_CALL_IPV6_TO_ENDPOINT		14
 #define CILIUM_CALL_IPV4_NODEPORT_NAT		15

commit 0f6513399d66b7302f3fd11613430f47118e6b42

bpf: Fix typo in host firewall tail call
[ upstream commit 56bbbedfb8adf49ee95dca2ce336f7735104ccd4 ]

There's a typo in the host firewall tail call that can lead to us
executing the IPv6 code for an IPv4 packet when enforcing host policies
for a packet coming from a local pod, when both IPv4, IPv6, and endpoint
routes are all enabled. The typo was found while working on splitting
bpf_host into more BPF programs.

The next commit explains why the end-to-end tests didn't uncover this
bug and improves those tests.

Fixes: 5b05cc92 ("bpf: Split handle_lxc_traffic with tail calls")
Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/bpf_host.c
@@ -1264,7 +1264,7 @@ to_host_from_lxc(struct __ctx_buff *ctx __maybe_unused)
 		invoke_tailcall_if(__or(__and(is_defined(ENABLE_IPV4),
 					      is_defined(ENABLE_IPV6)),
 					is_defined(DEBUG)),
-				   CILIUM_CALL_IPV6_TO_HOST_POLICY_ONLY,
+				   CILIUM_CALL_IPV4_TO_HOST_POLICY_ONLY,
 				   tail_ipv4_host_policy_ingress);
 		break;
 # endif