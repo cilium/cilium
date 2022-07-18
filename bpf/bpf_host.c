cilium/cilium

commit f42a49011ca7fac4ae824cf767de18059b40da78

bpf: Perform SRv6 policy lookup
To perform the SRv6 policy lookup, we must first lookup the appropriate
VRF where to perform the policy lookup. This first lookup is performed
in LPM maps using source and destination addresses. The second lookup
then uses the retrieved VRF ID and the destination address in another
LPM map to get the SID, used as the outer destination IPv6 address.

The lookups should only be performed for connections egressing the
cluster (i.e., same as for the egress gateway). Otherwise, a destination
pod IP address could match an entry in the LPM map if e.g. the user
defined a redirect for destination CIDR 0.0.0.0/0. In addition, we don't
want to perform the lookup and encapsulation for reply traffic (which
should leave the cluster the same way it ingressed).

Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/bpf_host.c
@@ -55,6 +55,7 @@
 #include "lib/nodeport.h"
 #include "lib/eps.h"
 #include "lib/host_firewall.h"
+#include "lib/egress_policies.h"
 #include "lib/overloadable.h"
 #include "lib/encrypt.h"
 
@@ -1037,6 +1038,80 @@ handle_netdev(struct __ctx_buff *ctx, const bool from_host)
 	return do_netdev(ctx, proto, from_host);
 }
 
+#ifdef ENABLE_SRV6
+static __always_inline
+handle_srv6(struct __ctx_buff *ctx)
+{
+	__u32 *vrf_id, dst_id, tunnel_ep = 0;
+	struct iphdr *ip4 __maybe_unused;
+	struct remote_endpoint_info *ep;
+	void *data, *data_end;
+	struct ipv6hdr *ip6;
+	union v6addr *sid;
+	__u16 proto;
+
+	if (!validate_ethertype(ctx, &proto))
+		return DROP_UNSUPPORTED_L2;
+
+	switch (proto) {
+	case bpf_htons(ETH_P_IPV6):
+		if (!revalidate_data(ctx, &data, &data_end, &ip6))
+			return DROP_INVALID;
+
+		ep = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr);
+		if (ep) {
+			tunnel_ep = ep->tunnel_endpoint;
+			dst_id = ep->sec_label;
+		} else {
+			dst_id = WORLD_ID;
+		}
+
+		if (identity_is_cluster(dst_id))
+			return CTX_ACT_OK;
+
+		vrf_id = srv6_lookup_vrf6(&ip6->saddr, &ip6->daddr);
+		if (!vrf_id)
+			return CTX_ACT_OK;
+
+		sid = srv6_lookup_policy6(*vrf_id, &ip6->daddr);
+		if (!sid)
+			return CTX_ACT_OK;
+
+		/* TODO: Implement SRv6 encapsulation. */
+		break;
+# ifdef ENABLE_IPV4
+	case bpf_htons(ETH_P_IP):
+		if (!revalidate_data(ctx, &data, &data_end, &ip4))
+			return DROP_INVALID;
+
+		ep = lookup_ip4_remote_endpoint(ip4->daddr);
+		if (ep) {
+			tunnel_ep = ep->tunnel_endpoint;
+			dst_id = ep->sec_label;
+		} else {
+			dst_id = WORLD_ID;
+		}
+
+		if (identity_is_cluster(dst_id))
+			return CTX_ACT_OK;
+
+		vrf_id = srv6_lookup_vrf4(ip4->saddr, ip4->daddr);
+		if (!vrf_id)
+			return CTX_ACT_OK;
+
+		sid = srv6_lookup_policy4(*vrf_id, ip4->daddr);
+		if (!sid)
+			return CTX_ACT_OK;
+
+		/* TODO: Implement SRv6 encapsulation. */
+		break;
+# endif
+	}
+
+	return CTX_ACT_OK;
+}
+#endif /* ENABLE_SRV6 */
+
 /*
  * from-netdev is attached as a tc ingress filter to one or more physical devices
  * managed by Cilium (e.g., eth0). This program is only attached when:
@@ -1167,6 +1242,13 @@ out:
 		return ret;
 	}
 #endif
+
+#ifdef ENABLE_SRV6
+	ret = handle_srv6(ctx);
+	if (ret != CTX_ACT_OK)
+		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
+					      METRIC_EGRESS);
+#endif /* ENABLE_SRV6 */
 
 #if defined(ENABLE_NODEPORT) && \
 	(!defined(ENABLE_DSR) || \

bpf/Makefile
@@ -172,7 +172,9 @@ HOST_OPTIONS = $(LXC_OPTIONS) \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_DSR:-DENABLE_DSR_HYBRID:-DENABLE_PREFILTER: \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_DSR:-DENABLE_DSR_HYBRID:-DENABLE_PREFILTER:-DENABLE_HOST_FIREWALL: \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_DSR:-DENABLE_DSR_HYBRID:-DENABLE_PREFILTER:-DENABLE_SESSION_AFFINITY:-DENABLE_HOST_FIREWALL: \
-	-DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_DSR:-DENABLE_DSR_HYBRID:-DENABLE_PREFILTER:-DENABLE_SESSION_AFFINITY:-DENABLE_HOST_FIREWALL:-DENABLE_ICMP_RULE:
+	-DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_DSR:-DENABLE_DSR_HYBRID:-DENABLE_PREFILTER:-DENABLE_SESSION_AFFINITY:-DENABLE_HOST_FIREWALL:-DENABLE_ICMP_RULE: \
+	-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_DSR:-DENABLE_DSR_HYBRID:-DENABLE_PREFILTER:-DENABLE_SESSION_AFFINITY:-DENABLE_HOST_FIREWALL:-DENABLE_ICMP_RULE:-DENABLE_SRV6: \
+	-DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_HOST_SERVICES_UDP:-DENABLE_HOST_SERVICES_TCP:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_DSR:-DENABLE_DSR_HYBRID:-DENABLE_PREFILTER:-DENABLE_SESSION_AFFINITY:-DENABLE_HOST_FIREWALL:-DENABLE_ICMP_RULE:-DENABLE_SRV6:
 
 ifndef MAX_HOST_OPTIONS
 MAX_HOST_OPTIONS = $(MAX_BASE_OPTIONS) -DENCAP_IFINDEX=1 -DTUNNEL_MODE=1

bpf/lib/egress_policies.h
@@ -29,4 +29,86 @@ struct egress_gw_policy_entry *lookup_ip4_egress_gw_policy(__be32 saddr, __be32 
 }
 
 #endif /* ENABLE_EGRESS_GATEWAY */
+
+#ifdef ENABLE_SRV6
+# ifdef ENABLE_IPV4
+
+/* SRV6_VRF_STATIC_PREFIX4 gets sizeof non-IP, non-prefix part of
+ * srv6_vrf_key4.
+ */
+#  define SRV6_VRF_STATIC_PREFIX4						\
+	(8 * (sizeof(struct srv6_vrf_key4) - sizeof(struct bpf_lpm_trie_key)\
+	      - 4))
+#  define SRV6_VRF_PREFIX4_LEN(PREFIX) (SRV6_VRF_STATIC_PREFIX4 + (PREFIX))
+#  define SRV6_VRF_IPV4_PREFIX SRV6_VRF_PREFIX4_LEN(32)
+static __always_inline __u32*
+srv6_lookup_vrf4(__be32 sip, __be32 dip)
+{
+	struct srv6_vrf_key4 key = {
+		.lpm = { SRV6_VRF_IPV4_PREFIX, {} },
+		.src_ip = sip,
+		.dst_cidr = dip,
+	};
+	return map_lookup_elem(&SRV6_VRF_MAP4, &key);
+}
+
+/* SRV6_POLICY_STATIC_PREFIX4 gets sizeof non-IP, non-prefix part of
+ * srv6_policy_key4.
+ */
+#  define SRV6_POLICY_STATIC_PREFIX4						\
+	(8 * (sizeof(struct srv6_policy_key4) - sizeof(struct bpf_lpm_trie_key)	\
+	      - 4))
+#  define SRV6_POLICY_PREFIX4_LEN(PREFIX) (SRV6_POLICY_STATIC_PREFIX4 + (PREFIX))
+#  define SRV6_POLICY_IPV4_PREFIX SRV6_POLICY_PREFIX4_LEN(32)
+static __always_inline union v6addr *
+srv6_lookup_policy4(__u32 vrf_id, __be32 dip)
+{
+	struct srv6_policy_key4 key = {
+		.lpm = { SRV6_POLICY_IPV4_PREFIX, {} },
+		.vrf_id = vrf_id,
+		.dst_cidr = dip,
+	};
+	return map_lookup_elem(&SRV6_POLICY_MAP4, &key);
+}
+# endif /* ENABLE_IPV4 */
+
+/* SRV6_VRF_STATIC_PREFIX6 gets sizeof non-IP, non-prefix part of
+ * srv6_vrf_key6.
+ */
+#  define SRV6_VRF_STATIC_PREFIX6						\
+	(8 * (sizeof(struct srv6_vrf_key6) - sizeof(struct bpf_lpm_trie_key)\
+	      - 4))
+#  define SRV6_VRF_PREFIX6_LEN(PREFIX) (SRV6_VRF_STATIC_PREFIX6 + (PREFIX))
+#  define SRV6_VRF_IPV6_PREFIX SRV6_VRF_PREFIX6_LEN(32)
+static __always_inline __u32*
+srv6_lookup_vrf6(const struct in6_addr *sip, const struct in6_addr *dip)
+{
+	struct srv6_vrf_key6 key = {
+		.lpm = { SRV6_VRF_IPV6_PREFIX, {} },
+		.src_ip = *(union v6addr *)sip,
+		.dst_cidr = *(union v6addr *)dip,
+	};
+	return map_lookup_elem(&SRV6_VRF_MAP6, &key);
+}
+
+/* SRV6_POLICY_STATIC_PREFIX6 gets sizeof non-IP, non-prefix part of
+ * srv6_policy_key6.
+ */
+# define SRV6_POLICY_STATIC_PREFIX6						\
+	(8 * (sizeof(struct srv6_policy_key6) - sizeof(struct bpf_lpm_trie_key)	\
+	      - 4))
+# define SRV6_POLICY_PREFIX6_LEN(PREFIX) (SRV6_POLICY_STATIC_PREFIX6 + (PREFIX))
+# define SRV6_POLICY_IPV6_PREFIX SRV6_POLICY_PREFIX6_LEN(128)
+
+static __always_inline union v6addr *
+srv6_lookup_policy6(__u32 vrf_id, const struct in6_addr *dip)
+{
+	struct srv6_policy_key6 key = {
+		.lpm = { SRV6_POLICY_IPV6_PREFIX, {} },
+		.vrf_id = vrf_id,
+		.dst_cidr = *(union v6addr *)dip,
+	};
+	return map_lookup_elem(&SRV6_POLICY_MAP6, &key);
+}
+#endif /* ENABLE_SRV6 */
 #endif /* __LIB_EGRESS_POLICIES_H_ */

commit 3914e13f9a3d1d6cbc30e12482acb60c63aa95a3

bpf: specify handle_srv6 return type to fix -Wimplicit-int error
This fixes the following error when building with clang 15:

    bpf_host.c:1055:1: error: type specifier missing, defaults to 'int' [-Werror,-Wimplicit-int]
    handle_srv6(struct __ctx_buff *ctx)

Fixes: f42a49011ca7 ("bpf: Perform SRv6 policy lookup")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

bpf/bpf_host.c
@@ -1051,7 +1051,7 @@ handle_netdev(struct __ctx_buff *ctx, const bool from_host)
 }
 
 #ifdef ENABLE_SRV6
-static __always_inline
+static __always_inline int
 handle_srv6(struct __ctx_buff *ctx)
 {
 	__u32 *vrf_id, dst_id, tunnel_ep = 0;