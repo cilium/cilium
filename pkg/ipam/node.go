cilium/cilium

commit bcdb763ca31cd05107236c73886b621058eeac0c

Fix for CN update order issue by adding new handshake state "released"
After the handshake is complete and the operator is done releasing an IP,
CiliumNode status (release-ips) and spec (pool) are updated in two
consecutive requests. There's a tiny window between the two updates where
the entry is removed from .status.ipam.release-ips but the IP is still
present in spec.ipam.pool. It was possible that the IP could be allocated
between these requests.

This commit introduces a new state called released to deal with this.
Now agent removes the entry from release-ips only when the IP was
removed from .spec.ipam.pool as well.

Signed-off-by: Hemanth Malla <hemanth.malla@datadoghq.com>

pkg/ipam/node.go
@@ -97,9 +97,10 @@ type Node struct {
 	ipsMarkedForRelease map[string]time.Time
 
 	// ipReleaseStatus tracks the state for every IP considered for release.
-	// 0 - IPAMMarkForRelease : Marked for Release
-	// 1 - IPAMReadyForRelease : Acknowledged as safe to release by agent
-	// 2 - IPAMDoNotRelease : Release request denied by agent
+	// IPAMMarkForRelease  : Marked for Release
+	// IPAMReadyForRelease : Acknowledged as safe to release by agent
+	// IPAMDoNotRelease    : Release request denied by agent
+	// IPAMReleased        : IP released by the operator
 	ipReleaseStatus map[string]string
 }
 
@@ -569,6 +570,20 @@ func (n *Node) determineMaintenanceAction() (*maintenanceAction, error) {
 	return a, nil
 }
 
+// removeStaleReleaseIPs Removes stale entries in local n.ipReleaseStatus. Once the handshake is complete agent would
+// remove entries from IP release status map in ciliumnode CRD's status. These IPs need to be purged from
+// n.ipReleaseStatus
+func (n *Node) removeStaleReleaseIPs() {
+	for ip, status := range n.ipReleaseStatus {
+		if status != ipamOption.IPAMReleased {
+			continue
+		}
+		if _, ok := n.resource.Status.IPAM.ReleaseIPs[ip]; !ok {
+			delete(n.ipReleaseStatus, ip)
+		}
+	}
+}
+
 // maintainIPPool attempts to allocate or release all required IPs to fulfill the needed gap.
 // returns instanceMutated which tracks if state changed with the cloud provider and is used
 // to determine if IPAM pool maintainer trigger func needs to be invoked.
@@ -609,6 +624,9 @@ func (n *Node) maintainIPPool(ctx context.Context) (instanceMutated bool, err er
 		// Resetting ipsMarkedForRelease if there are no IPs to release in this iteration
 		n.ipsMarkedForRelease = make(map[string]time.Time)
 	}
+
+	n.removeStaleReleaseIPs()
+
 	for markedIP, ts := range n.ipsMarkedForRelease {
 		// Determine which IPs are still marked for release.
 		stillMarkedForRelease := false
@@ -671,7 +689,7 @@ func (n *Node) maintainIPPool(ctx context.Context) (instanceMutated bool, err er
 			// Remove the IPs from ipsMarkedForRelease
 			for _, ip := range ipsToRelease {
 				delete(n.ipsMarkedForRelease, ip)
-				delete(n.ipReleaseStatus, ip)
+				n.ipReleaseStatus[ip] = ipamOption.IPAMReleased
 			}
 			return true, nil
 		}

pkg/ipam/crd.go
@@ -337,12 +337,20 @@ func (n *nodeStore) updateLocalNodeResource(node *ciliumv2.CiliumNode) {
 	releaseUpstreamSyncNeeded := false
 	// ACK or NACK IPs marked for release by the operator
 	for ip, status := range n.ownNode.Status.IPAM.ReleaseIPs {
-		if status != ipamOption.IPAMMarkForRelease || n.ownNode.Spec.IPAM.Pool == nil {
+		if n.ownNode.Spec.IPAM.Pool == nil {
 			continue
 		}
-		// NACK the IP, if this node doesn't own the IP
+		if status == ipamOption.IPAMReadyForRelease || status == ipamOption.IPAMDoNotRelease {
+			continue
+		}
 		if _, ok := n.ownNode.Spec.IPAM.Pool[ip]; !ok {
-			n.ownNode.Status.IPAM.ReleaseIPs[ip] = ipamOption.IPAMDoNotRelease
+			if status == ipamOption.IPAMReleased {
+				// Remove entry from release-ips only when its removed the .spec.ipam.pool
+				delete(n.ownNode.Status.IPAM.ReleaseIPs, ip)
+			} else if status == ipamOption.IPAMMarkForRelease {
+				// NACK the IP, if this node doesn't own the IP
+				n.ownNode.Status.IPAM.ReleaseIPs[ip] = ipamOption.IPAMDoNotRelease
+			}
 			continue
 		}
 		// Retrieve the appropriate allocator
@@ -469,7 +477,7 @@ func (n *nodeStore) isIPInReleaseHandshake(ip string) bool {
 		return false
 	}
 	if status, ok := n.ownNode.Status.IPAM.ReleaseIPs[ip]; ok {
-		if status == ipamOption.IPAMMarkForRelease || status == ipamOption.IPAMReadyForRelease {
+		if status == ipamOption.IPAMMarkForRelease || status == ipamOption.IPAMReadyForRelease || status == ipamOption.IPAMReleased {
 			return true
 		}
 	}

pkg/ipam/option/option.go
@@ -31,4 +31,5 @@ const (
 	IPAMMarkForRelease  = "marked-for-release"
 	IPAMReadyForRelease = "ready-for-release"
 	IPAMDoNotRelease    = "do-not-release"
+	IPAMReleased        = "released"
 )

pkg/ipam/types/types.go
@@ -102,7 +102,7 @@ type IPAMSpec struct {
 
 // IPReleaseStatus  defines the valid states in IP release handshake
 //
-// +kubebuilder:validation:Enum=marked-for-release;ready-for-release;do-not-release
+// +kubebuilder:validation:Enum=marked-for-release;ready-for-release;do-not-release;released
 type IPReleaseStatus string
 
 // IPAMStatus is the IPAM status of a node
@@ -125,6 +125,7 @@ type IPAMStatus struct {
 	// * marked-for-release : Set by operator as possible candidate for IP
 	// * ready-for-release  : Acknowledged as safe to release by agent
 	// * do-not-release     : IP already in use / not owned by the node. Set by agent
+	// * released           : IP successfully released. Set by operator
 	//
 	// +optional
 	ReleaseIPs map[string]IPReleaseStatus `json:"release-ips,omitempty"`

pkg/k8s/apis/cilium.io/client/crds/v2/ciliumnodes.yaml
@@ -527,12 +527,14 @@ spec:
                       - marked-for-release
                       - ready-for-release
                       - do-not-release
+                      - released
                       type: string
                     description: 'ReleaseIPs tracks the state for every IP considered
                       for release. value can be one of the following string : * marked-for-release
                       : Set by operator as possible candidate for IP * ready-for-release  :
                       Acknowledged as safe to release by agent * do-not-release     :
-                      IP already in use / not owned by the node. Set by agent'
+                      IP already in use / not owned by the node. Set by agent * released           :
+                      IP successfully released. Set by operator'
                     type: object
                   used:
                     additionalProperties:

commit 839324991df1cd07d10d0f528217ffbffb19b5d5

Trigger NodeManager.Resync immediately after new ENI successfully created
Current successfully call createInterface not trigger n.manager.resyncTrigger.
It cause spec.ipam.pool update delay, so agent not able allocate ip for new pod
until periodic trigger NodeManager.Resync.

This patch trigger NodeManager.Resync immediately to update ciliumNode resource.

Fixes: #20595
Fixes: bcdb763ca31cd (Fix for excess IP release race condition | handshake between agent and operator)

Signed-off-by: xiaoqing <xiaoqingnb@gmail.com>

pkg/ipam/node.go
@@ -469,7 +469,7 @@ func (n *Node) ResourceCopy() *v2.CiliumNode {
 // attaches it to the instance as specified by the CiliumNode. neededAddresses
 // of secondary IPs are assigned to the interface up to the maximum number of
 // addresses as allowed by the instance.
-func (n *Node) createInterface(ctx context.Context, a *AllocationAction) error {
+func (n *Node) createInterface(ctx context.Context, a *AllocationAction) (created bool, err error) {
 	if a.AvailableInterfaces == 0 {
 		// This is not a failure scenario, warn once per hour but do
 		// not track as interface allocation failure. There is a
@@ -480,7 +480,7 @@ func (n *Node) createInterface(ctx context.Context, a *AllocationAction) error {
 			n.lastMaxAdapterWarning = time.Now()
 		}
 		n.mutex.Unlock()
-		return nil
+		return false, nil
 	}
 
 	scopedLog := n.logger()
@@ -488,13 +488,13 @@ func (n *Node) createInterface(ctx context.Context, a *AllocationAction) error {
 	if err != nil {
 		scopedLog.Warningf("Unable to create interface on instance: %s", err)
 		n.manager.metricsAPI.IncAllocationAttempt(errCondition, string(a.PoolID))
-		return err
+		return false, err
 	}
 
 	n.manager.metricsAPI.IncAllocationAttempt("success", string(a.PoolID))
 	n.manager.metricsAPI.AddIPAllocation(string(a.PoolID), int64(toAllocate))
 
-	return nil
+	return true, nil
 }
 
 // AllocationAction is the action to be taken to resolve allocation deficits
@@ -835,8 +835,7 @@ func (n *Node) handleIPAllocation(ctx context.Context, a *maintenanceAction) (in
 		}).WithError(err).Warning("Unable to assign additional IPs to interface, will create new interface")
 	}
 
-	err = n.createInterface(ctx, a.allocation)
-	return err != nil, err
+	return n.createInterface(ctx, a.allocation)
 }
 
 // maintainIPPool attempts to allocate or release all required IPs to fulfill the needed gap.