ea2ad4e32c4cf7f13259fc5951e015159633e607
Aditi Ghag <aditi@cilium.io>
2022-08-03 22:54:11 -0700

    test extend type slim
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

--- pkg/k8s/slim/k8s/api/core/v1/types.go
+++ pkg/k8s/slim/k8s/api/core/v1/types.go
@@ -180,6 +180,25 @@ type PodCondition struct {
 	Message string `json:"message,omitempty" protobuf:"bytes,6,opt,name=message"`
 }
 
+// ContainerStatus represents the status of a container
+type ContainerStatus struct {
+	// +optional
+	ContainerID string `protobuf:"bytes,8,opt,name=containerID"`
+}
+
+// PodQOSClass defines the supported qos classes of Pods.
+// +enum
+type PodQOSClass string
+
+const (
+	// PodQOSGuaranteed is the Guaranteed qos class.
+	PodQOSGuaranteed PodQOSClass = "Guaranteed"
+	// PodQOSBurstable is the Burstable qos class.
+	PodQOSBurstable PodQOSClass = "Burstable"
+	// PodQOSBestEffort is the BestEffort qos class.
+	PodQOSBestEffort PodQOSClass = "BestEffort"
+)
+
 // The node this Taint is attached to has the "effect" on
 // any pod that does not tolerate the Taint.
 type Taint struct {
@@ -323,6 +342,18 @@ type PodStatus struct {
 	// This is before the Kubelet pulled the container image(s) for the pod.
 	// +optional
 	StartTime *slim_metav1.Time `json:"startTime,omitempty" protobuf:"bytes,7,opt,name=startTime"`
+
+	// The list has one entry per container in the manifest.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	ContainerStatuses []ContainerStatus `json:"statuses,omitempty" protobuf:"bytes,8,rep,name=containerStatuses"`
+
+	// The Quality of Service (QOS) classification assigned to the pod based on resource requirements
+	// See PodQOSClass type for available QOS classes
+	// More info: https://git.k8s.io/community/contributors/design-proposals/node/resource-qos.md
+	// +optional
+	QOSClass PodQOSClass `json:"qosClass,omitempty" protobuf:"bytes,9,rep,name=qosClass"`
 }
 
 // +genclient
