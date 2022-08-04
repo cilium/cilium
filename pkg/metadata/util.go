eb3270838774bd19f31538e41c7af617e286d488
Aditi Ghag <aditi@cilium.io>
2022-08-03 20:44:52 -0700

    pkg/metadata: Utility functions to implement CRI client
    This is a preparatory commit that takes code from [1].
    The reason this isn't imported as a package is because
    it's not imported with a valid version as the API can change.
    As the utility functions are fairly straightforward, copy over
    the code in order to avoid importing the dependency with these
    errors -
    
    `k8s.io/api@v0.0.0: reading k8s.io/api/go.mod at revision v0.0.0: unknown revision v0.0.0`
    
    [1] k8s.io/kubernetes/pkg/kubelet/util/util.go
    
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

---
+++ pkg/metadata/util.go
@@ -0,0 +1,61 @@
+package metadata
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/url"
+)
+
+// This code is taken from k8s.io/kubernetes/pkg/kubelet/util/util.go.
+
+const (
+	// unixProtocol is the network protocol of unix socket.
+	unixProtocol = "unix"
+)
+
+func dial(ctx context.Context, addr string) (net.Conn, error) {
+	return (&net.Dialer{}).DialContext(ctx, unixProtocol, addr)
+}
+
+func parseEndpointWithFallbackProtocol(endpoint string, fallbackProtocol string) (protocol string, addr string, err error) {
+	if protocol, addr, err = parseEndpoint(endpoint); err != nil && protocol == "" {
+		fallbackEndpoint := fallbackProtocol + "://" + endpoint
+		protocol, addr, err = parseEndpoint(fallbackEndpoint)
+	}
+	return
+}
+
+func parseEndpoint(endpoint string) (string, string, error) {
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return "", "", err
+	}
+
+	switch u.Scheme {
+	case "tcp":
+		return "tcp", u.Host, nil
+
+	case "unix":
+		return "unix", u.Path, nil
+
+	case "":
+		return "", "", fmt.Errorf("using %q as endpoint is deprecated, please consider using full url format", endpoint)
+
+	default:
+		return u.Scheme, "", fmt.Errorf("protocol %q not supported", u.Scheme)
+	}
+}
+
+// GetAddressAndDialer returns the address parsed from the given endpoint and a context dialer.
+func GetAddressAndDialer(endpoint string) (string, func(ctx context.Context, addr string) (net.Conn, error), error) {
+	protocol, addr, err := parseEndpointWithFallbackProtocol(endpoint, unixProtocol)
+	if err != nil {
+		return "", nil, err
+	}
+	if protocol != unixProtocol {
+		return "", nil, fmt.Errorf("only support unix socket endpoint")
+	}
+
+	return addr, dial, nil
+}
