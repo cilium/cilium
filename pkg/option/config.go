cilium/cilium

commit 030077272e9cc3b42355bad9f6f47b31184c932c

datapath: Add a flag to set VXLAN and Geneve ports
This change makes it possible for user to set a custom port
for VXLAN or Geneve. In order to anable that, the defaulting
logic was introduced in the agent, so kernel defaults are no
longer relied upon.

Default ports remain unchanged.

Towards: #15956

Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

pkg/option/config.go
@@ -465,6 +465,8 @@ const (
 
 	// TunnelName is the name of the Tunnel option
 	TunnelName = "tunnel"
+	// TunnelPortName is the name of the TunnelPort option
+	TunnelPortName = "tunnel-port"
 
 	// SingleClusterRouteName is the name of the SingleClusterRoute option
 	//
@@ -1198,6 +1200,7 @@ type DaemonConfig struct {
 
 	DatapathMode string // Datapath mode
 	Tunnel       string // Tunnel mode
+	TunnelPort   int    // Tunnel port
 
 	DryMode bool // Do not create BPF maps, devices, ..
 
@@ -2547,7 +2550,6 @@ func (c *DaemonConfig) Populate() {
 	c.SocketPath = viper.GetString(SocketPath)
 	c.SockopsEnable = viper.GetBool(SockopsEnableName)
 	c.TracePayloadlen = viper.GetInt(TracePayloadlen)
-	c.Tunnel = viper.GetString(TunnelName)
 	c.Version = viper.GetString(Version)
 	c.WriteCNIConfigurationWhenReady = viper.GetString(WriteCNIConfigurationWhenReady)
 	c.PolicyTriggerInterval = viper.GetDuration(PolicyTriggerInterval)
@@ -2583,6 +2585,18 @@ func (c *DaemonConfig) Populate() {
 	c.EgressMultiHomeIPRuleCompat = viper.GetBool(EgressMultiHomeIPRuleCompat)
 
 	c.VLANBPFBypass = viper.GetIntSlice(VLANBPFBypass)
+
+	c.Tunnel = viper.GetString(TunnelName)
+	c.TunnelPort = viper.GetInt(TunnelPortName)
+
+	if c.TunnelPort == 0 {
+		switch c.Tunnel {
+		case TunnelVXLAN:
+			c.TunnelPort = defaults.TunnelPortVXLAN
+		case TunnelGeneve:
+			c.TunnelPort = defaults.TunnelPortGeneve
+		}
+	}
 
 	nativeRoutingCIDR := viper.GetString(NativeRoutingCIDR)
 	ipv4NativeRoutingCIDR := viper.GetString(IPv4NativeRoutingCIDR)

bpf/init.sh
@@ -20,22 +20,24 @@ IP4_HOST=$3
 IP6_HOST=$4
 MODE=$5
 TUNNEL_MODE=$6
+# Only set if TUNNEL_MODE = "vxlan", "geneve"
+TUNNEL_PORT=$7
 # Only set if MODE = "direct", "ipvlan"
-NATIVE_DEVS=$7
-HOST_DEV1=$8
-HOST_DEV2=$9
-MTU=${10}
-HOSTLB=${11}
-HOSTLB_UDP=${12}
-HOSTLB_PEER=${13}
-CGROUP_ROOT=${14}
-BPFFS_ROOT=${15}
-NODE_PORT=${16}
-NODE_PORT_BIND=${17}
-MCPU=${18}
-NR_CPUS=${19}
-ENDPOINT_ROUTES=${20}
-PROXY_RULE=${21}
+NATIVE_DEVS=$8
+HOST_DEV1=$9
+HOST_DEV2=${10}
+MTU=${11}
+HOSTLB=${12}
+HOSTLB_UDP=${13}
+HOSTLB_PEER=${14}
+CGROUP_ROOT=${15}
+BPFFS_ROOT=${16}
+NODE_PORT=${17}
+NODE_PORT_BIND=${18}
+MCPU=${19}
+NR_CPUS=${20}
+ENDPOINT_ROUTES=${21}
+PROXY_RULE=${22}
 
 ID_HOST=1
 ID_WORLD=2
@@ -311,6 +313,15 @@ function bpf_clear_cgroups()
 	fi
 }
 
+function create_encap_dev()
+{
+	TUNNEL_OPTS="external"
+	if [ "${TUNNEL_PORT}" != "<nil>" ]; then
+		TUNNEL_OPTS="dstport $TUNNEL_PORT $TUNNEL_OPTS"
+	fi
+	ip link add name $ENCAP_DEV address $(rnd_mac_addr) type $TUNNEL_MODE $TUNNEL_OPTS || encap_fail
+}
+
 function encap_fail()
 {
 	(>&2 echo "ERROR: Setup of encapsulation device $ENCAP_DEV has failed. Is another program using a $MODE device?")
@@ -423,10 +434,16 @@ fi
 
 if [ "${TUNNEL_MODE}" != "<nil>" ]; then
 	ENCAP_DEV="cilium_${TUNNEL_MODE}"
-	ip link show $ENCAP_DEV || {
-		ip link add name $ENCAP_DEV address $(rnd_mac_addr) type $TUNNEL_MODE external || encap_fail
-	}
+
+	ip link show $ENCAP_DEV || create_encap_dev
 	ip link set $ENCAP_DEV mtu $MTU || encap_fail
+
+	if [ "${TUNNEL_PORT}" != "<nil>" ]; then
+		ip -details link show $ENCAP_DEV | grep "dstport $TUNNEL_PORT" || {
+			ip link delete name $ENCAP_DEV type $TUNNEL_MODE
+			create_encap_dev
+		}
+	fi
 
 	setup_dev $ENCAP_DEV || encap_fail
 

daemon/cmd/daemon_main.go
@@ -746,6 +746,9 @@ func init() {
 	flags.StringP(option.TunnelName, "t", "", fmt.Sprintf("Tunnel mode {%s} (default \"vxlan\" for the \"veth\" datapath mode)", option.GetTunnelModes()))
 	option.BindEnv(option.TunnelName)
 
+	flags.Int(option.TunnelPortName, 0, fmt.Sprintf("Tunnel port (default %d for \"vxlan\" and %d for \"geneve\")", defaults.TunnelPortVXLAN, defaults.TunnelPortGeneve))
+	option.BindEnv(option.TunnelPortName)
+
 	flags.Int(option.TracePayloadlen, 128, "Length of payload to capture when tracing")
 	option.BindEnv(option.TracePayloadlen)
 

install/kubernetes/cilium/templates/cilium-configmap.yaml
@@ -358,6 +358,10 @@ data:
   tunnel: {{ .Values.tunnel }}
 {{- end }}
 
+{{- if hasKey .Values "tunnelPort" }}
+  tunnel-port: "{{ .Values.tunnelPort }}"
+{{- end }}
+
 {{- if .Values.eni.enabled }}
   enable-endpoint-routes: "true"
   auto-create-cilium-node-resource: "true"

pkg/datapath/loader/base.go
@@ -42,6 +42,7 @@ const (
 	initArgIPv6NodeIP
 	initArgMode
 	initArgTunnelMode
+	initArgTunnelPort
 	initArgDevices
 	initArgHostDev1
 	initArgHostDev2
@@ -350,6 +351,12 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		// Enable tunnel mode to vxlan if egress gateway is configured
 		// Tunnel is required for egress traffic under this config
 		args[initArgTunnelMode] = option.TunnelVXLAN
+	}
+
+	args[initArgTunnelPort] = "<nil>"
+	switch args[initArgTunnelMode] {
+	case option.TunnelVXLAN, option.TunnelGeneve:
+		args[initArgTunnelPort] = fmt.Sprintf("%d", option.Config.TunnelPort)
 	}
 
 	if option.Config.EnableNodePort {

pkg/defaults/defaults.go
@@ -436,4 +436,9 @@ const (
 
 	// EnableICMPRules enables ICMP-based rule support for Cilium Network Policies.
 	EnableICMPRules = false
+
+	// TunnelPortVXLAN is the default VXLAN port
+	TunnelPortVXLAN = 8472
+	// TunnelPortGeneve is the default Geneve port
+	TunnelPortGeneve = 6081
 )

commit 9e447d267a6b9723a20fd9b9bbb236ed126ebc17

datapath: avoid delete/add flap for cilium_vxlan on startup
In a configuration with EgressGW and direct-routing, cilium still uses a
cilium_vxlan interface to transport the EgressGW traffic between source
nodes and Gateway nodes.

If no --tunnel-port is configured we stick to the default value (0) instead
of using the VXLAN default port (8472), and when bpf/init.sh later tries to
create the interface with port 0 the kernel simply falls back to using
port 8472 instead.

But if cilium_vxlan already exists from a previous run, the corresponding
check in bpf/init.sh fails (as we look for dstport 0, but find cilium_vxlan
with dstport 8472). So we delete the interface, just to add it again in the
same effective configuration.

Avoid the overhead (and flapping ifindex) by initializing c.TunnelPort with
the VXLAN port, even when the user didn't select tunneling mode.

Fixes: 030077272e9c ("datapath: Add a flag to set VXLAN and Geneve ports")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

pkg/option/config.go
@@ -2973,6 +2973,9 @@ func (c *DaemonConfig) Populate() {
 
 	if c.TunnelPort == 0 {
 		switch c.Tunnel {
+		case TunnelDisabled:
+			// tunnel might still be used by eg. EgressGW
+			c.TunnelPort = defaults.TunnelPortVXLAN
 		case TunnelVXLAN:
 			c.TunnelPort = defaults.TunnelPortVXLAN
 		case TunnelGeneve: