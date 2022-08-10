cilium/cilium

commit ed934cb958c52515be37998038301229959a0fd2

health: Move endpoint IP to node package
The Cilium health endpoint IP is the only static local node field which
is not accessible from the node package. To avoid code modifying the
`NodeDiscovery.LocalNode` field (which will become more dynamic in a
subsequent mode), the health IP is now moved to the node package with a
corresponding getter and setter like all other static node fields.

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

cilium-health/launch/endpoint.go
@@ -34,7 +34,6 @@ import (
 	"github.com/cilium/cilium/pkg/mtu"
 	"github.com/cilium/cilium/pkg/netns"
 	"github.com/cilium/cilium/pkg/node"
-	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/pidfile"
 	"github.com/cilium/cilium/pkg/policy"
@@ -228,7 +227,6 @@ type EndpointAdder interface {
 func LaunchAsEndpoint(baseCtx context.Context,
 	owner regeneration.Owner,
 	policyGetter policyRepoGetter,
-	n *nodeTypes.Node,
 	mtuConfig mtu.Configuration,
 	epMgr EndpointAdder,
 	proxy endpoint.EndpointProxy,
@@ -246,13 +244,11 @@ func LaunchAsEndpoint(baseCtx context.Context,
 		ip4Address, ip6Address *net.IPNet
 	)
 
-	if n.IPv6HealthIP != nil {
-		healthIP = n.IPv6HealthIP
+	if healthIP = node.GetEndpointHealthIPv6(); healthIP != nil {
 		info.Addressing.IPV6 = healthIP.String()
 		ip6Address = &net.IPNet{IP: healthIP, Mask: defaults.ContainerIPv6Mask}
 	}
-	if n.IPv4HealthIP != nil {
-		healthIP = n.IPv4HealthIP
+	if healthIP = node.GetEndpointHealthIPv4(); healthIP != nil {
 		info.Addressing.IPV4 = healthIP.String()
 		ip4Address = &net.IPNet{IP: healthIP, Mask: defaults.ContainerIPv4Mask}
 	}

daemon/cmd/daemon.go
@@ -922,8 +922,8 @@ func NewDaemon(ctx context.Context, cancel context.CancelFunc, epMgr *endpointma
 		log.WithFields(logrus.Fields{
 			logfields.V4Prefix:       node.GetIPv4AllocRange(),
 			logfields.V6Prefix:       node.GetIPv6AllocRange(),
-			logfields.V4HealthIP:     d.nodeDiscovery.LocalNode.IPv4HealthIP,
-			logfields.V6HealthIP:     d.nodeDiscovery.LocalNode.IPv6HealthIP,
+			logfields.V4HealthIP:     node.GetEndpointHealthIPv4(),
+			logfields.V6HealthIP:     node.GetEndpointHealthIPv6(),
 			logfields.V4CiliumHostIP: node.GetInternalIPv4Router(),
 			logfields.V6CiliumHostIP: node.GetIPv6Router(),
 		}).Info("Annotating k8s node")
@@ -931,7 +931,7 @@ func NewDaemon(ctx context.Context, cancel context.CancelFunc, epMgr *endpointma
 		err := k8s.Client().AnnotateNode(nodeTypes.GetName(),
 			encryptKeyID,
 			node.GetIPv4AllocRange(), node.GetIPv6AllocRange(),
-			d.nodeDiscovery.LocalNode.IPv4HealthIP, d.nodeDiscovery.LocalNode.IPv6HealthIP,
+			node.GetEndpointHealthIPv4(), node.GetEndpointHealthIPv6(),
 			node.GetInternalIPv4Router(), node.GetIPv6Router())
 		if err != nil {
 			log.WithError(err).Warning("Cannot annotate k8s node with CIDR range")

daemon/cmd/health.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cilium/cilium/pkg/health/defaults"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/pidfile"
 )
@@ -69,7 +70,6 @@ func (d *Daemon) initHealth() {
 						ctx,
 						d,
 						d,
-						&d.nodeDiscovery.LocalNode,
 						d.mtuConfig,
 						d.endpointManager,
 						d.l7Proxy,
@@ -104,18 +104,18 @@ func (d *Daemon) initHealth() {
 }
 
 func (d *Daemon) cleanupHealthEndpoint() {
-	localNode := d.nodeDiscovery.LocalNode
-
 	// Delete the process
 	health.KillEndpoint()
 
 	// Clean up agent resources
 	var ep *endpoint.Endpoint
-	if localNode.IPv4HealthIP != nil {
-		ep = d.endpointManager.LookupIPv4(localNode.IPv4HealthIP.String())
+	healthIPv4 := node.GetEndpointHealthIPv4()
+	healthIPv6 := node.GetEndpointHealthIPv6()
+	if healthIPv4 != nil {
+		ep = d.endpointManager.LookupIPv4(healthIPv4.String())
 	}
-	if ep == nil && localNode.IPv6HealthIP != nil {
-		ep = d.endpointManager.LookupIPv6(localNode.IPv6HealthIP.String())
+	if ep == nil && healthIPv6 != nil {
+		ep = d.endpointManager.LookupIPv6(healthIPv6.String())
 	}
 	if ep == nil {
 		log.Debug("Didn't find existing cilium-health endpoint to delete")

daemon/cmd/ipam.go
@@ -252,7 +252,7 @@ func (d *Daemon) allocateHealthIPs() error {
 			}
 
 			log.Debugf("IPv4 health endpoint address: %s", result.IP)
-			d.nodeDiscovery.LocalNode.IPv4HealthIP = result.IP
+			node.SetEndpointHealthIPv4(result.IP)
 
 			// In ENI and AlibabaCloud ENI mode, we require the gateway, CIDRs, and the ENI MAC addr
 			// in order to set up rules and routes on the local node to direct
@@ -267,13 +267,14 @@ func (d *Daemon) allocateHealthIPs() error {
 		if option.Config.EnableIPv6 {
 			result, err := d.ipam.AllocateNextFamilyWithoutSyncUpstream(ipam.IPv6, "health")
 			if err != nil {
-				if d.nodeDiscovery.LocalNode.IPv4HealthIP != nil {
-					d.ipam.ReleaseIP(d.nodeDiscovery.LocalNode.IPv4HealthIP)
+				if healthIPv4 := node.GetEndpointHealthIPv4(); healthIPv4 != nil {
+					d.ipam.ReleaseIP(healthIPv4)
+					node.SetEndpointHealthIPv4(nil)
 				}
 				return fmt.Errorf("unable to allocate health IPs: %s,see https://cilium.link/ipam-range-full", err)
 			}
 
-			d.nodeDiscovery.LocalNode.IPv6HealthIP = result.IP
+			node.SetEndpointHealthIPv6(result.IP)
 			log.Debugf("IPv6 health endpoint address: %s", result.IP)
 		}
 	}

pkg/node/address.go
@@ -43,6 +43,10 @@ var (
 	ipv4ExternalAddress net.IP
 	ipv6ExternalAddress net.IP
 
+	// Addresses of the cilium-health endpoint located on the node
+	ipv4HealthIP net.IP
+	ipv6HealthIP net.IP
+
 	// k8s Node IP (either InternalIP or ExternalIP or nil; the former is preferred)
 	k8sNodeIP net.IP
 
@@ -684,6 +688,26 @@ func SetWireguardPubKey(key string) {
 
 func GetWireguardPubKey() string {
 	return wireguardPubKey
+}
+
+// SetEndpointHealthIPv4 sets the IPv6 cilium-health endpoint address.
+func SetEndpointHealthIPv4(ip net.IP) {
+	ipv4HealthIP = ip
+}
+
+// GetEndpointHealthIPv4 returns the IPv4 cilium-health endpoint address.
+func GetEndpointHealthIPv4() net.IP {
+	return ipv4HealthIP
+}
+
+// SetEndpointHealthIPv6 sets the IPv6 cilium-health endpoint address.
+func SetEndpointHealthIPv6(ip net.IP) {
+	ipv6HealthIP = ip
+}
+
+// GetEndpointHealthIPv6 returns the IPv6 cilium-health endpoint address.
+func GetEndpointHealthIPv6() net.IP {
+	return ipv6HealthIP
 }
 
 func copyStringToNetIPMap(in map[string]net.IP) map[string]net.IP {

pkg/nodediscovery/nodediscovery.go
@@ -172,6 +172,8 @@ func (n *NodeDiscovery) StartDiscovery(nodeName string) {
 	n.LocalNode.IPAddresses = []nodeTypes.Address{}
 	n.LocalNode.IPv4AllocCIDR = node.GetIPv4AllocRange()
 	n.LocalNode.IPv6AllocCIDR = node.GetIPv6AllocRange()
+	n.LocalNode.IPv4HealthIP = node.GetEndpointHealthIPv4()
+	n.LocalNode.IPv6HealthIP = node.GetEndpointHealthIPv6()
 	n.LocalNode.ClusterID = option.Config.ClusterID
 	n.LocalNode.EncryptionKey = node.GetIPsecKeyIdentity()
 	n.LocalNode.WireguardPubKey = node.GetWireguardPubKey()

commit 56648f3fd20cf960a33db8ae21293c3990a054be

cilium-health: fix probing for IPv6-only clusters
Due some refactoring done in ed934cb958c5, a bug was introduced in the
code that overwrote the IP address used to perform health checks.
In IPv6-only clusters, this IP address was overwritten by an empty IPv4
address which would then be used to perform health checks. Obviously
failing to perform such health checks since the address was "<nil>",
Cilium would report that `cilium-health-ep` controllers were failing.

Fixes: ed934cb958c5 ("health: Move endpoint IP to node package")
Signed-off-by: André Martins <andre@cilium.io>

cilium-health/launch/endpoint.go
@@ -243,13 +243,15 @@ func LaunchAsEndpoint(baseCtx context.Context,
 		ip4Address, ip6Address *net.IPNet
 	)
 
-	if healthIP = node.GetEndpointHealthIPv6(); healthIP != nil {
-		info.Addressing.IPV6 = healthIP.String()
-		ip6Address = &net.IPNet{IP: healthIP, Mask: defaults.ContainerIPv6Mask}
+	if healthIPv6 := node.GetEndpointHealthIPv6(); healthIPv6 != nil {
+		info.Addressing.IPV6 = healthIPv6.String()
+		ip6Address = &net.IPNet{IP: healthIPv6, Mask: defaults.ContainerIPv6Mask}
+		healthIP = healthIPv6
 	}
-	if healthIP = node.GetEndpointHealthIPv4(); healthIP != nil {
-		info.Addressing.IPV4 = healthIP.String()
-		ip4Address = &net.IPNet{IP: healthIP, Mask: defaults.ContainerIPv4Mask}
+	if healthIPv4 := node.GetEndpointHealthIPv4(); healthIPv4 != nil {
+		info.Addressing.IPV4 = healthIPv4.String()
+		ip4Address = &net.IPNet{IP: healthIPv4, Mask: defaults.ContainerIPv4Mask}
+		healthIP = healthIPv4
 	}
 
 	if option.Config.EnableEndpointRoutes {