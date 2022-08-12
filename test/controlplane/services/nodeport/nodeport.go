0b72c1ba2cc0754cf3d27285a167bdebe0194a5b
Jussi Maki <jussi@isovalent.com>
2022-08-12 13:41:00 +0200

    test/controlplane: Extend nodeport with externalTrafficPolicy=Local
    This extends the nodeport test with a service that has been marked with
    externalTrafficPolicy=Local. The test checks that services of this kind
    have corresponding lbmap entries with their scope marked as internal.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- test/controlplane/services/nodeport/nodeport.go
+++ test/controlplane/services/nodeport/nodeport.go
@@ -4,10 +4,13 @@
 package nodeport
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"testing"
 
+	"github.com/cilium/cilium/pkg/datapath/fake"
+	lb "github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/test/controlplane/services/helpers"
 	"github.com/cilium/cilium/test/controlplane/suite"
@@ -33,9 +36,55 @@ func init() {
 					UpdateObjectsFromFile(abs("init.yaml")).
 					StartAgent(modConfig).
 					UpdateObjectsFromFile(abs("state1.yaml")).
-					Eventually(func() error { return helpers.ValidateLBMapGoldenFile(abs("lbmap1.golden"), test.Datapath) }).
+					Eventually(func() error { return validate(test, abs("lbmap1.golden")) }).
 					StopAgent()
 			})
 		}
 	})
+}
+
+func validate(test *suite.ControlPlaneTest, goldenFile string) error {
+	if err := helpers.ValidateLBMapGoldenFile(goldenFile, test.Datapath); err != nil {
+		return err
+	}
+	if err := validateExternalTrafficPolicyLocal(test.Datapath); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateExternalTrafficPolicyLocal(dp *fake.FakeDatapath) error {
+	lbmap := dp.LBMockMap()
+	lbmap.Lock()
+	defer lbmap.Unlock()
+
+	// Collect all echo-local services with internal ("local") scope.
+	localServices := []*lb.SVC{}
+	for _, svc := range dp.LBMockMap().ServiceByID {
+		if svc.Name.Name == "echo-local" && svc.Frontend.Scope == lb.ScopeInternal {
+			localServices = append(localServices, svc)
+		}
+	}
+
+	expectedFrontendIPs := map[string]bool{}
+	for _, ip := range dp.LocalNodeAddressing().IPv4().LoadBalancerNodeAddresses() {
+		expectedFrontendIPs[ip.String()] = true
+	}
+
+	// Check that all expected service entries exist with the expected frontends.
+	for _, svc := range localServices {
+		ip := svc.Frontend.IP.String()
+		if _, ok := expectedFrontendIPs[ip]; !ok {
+			return fmt.Errorf("unexpected frontend IP %q for service %s, expected one of %v", ip, svc.Name, expectedFrontendIPs)
+		}
+		delete(expectedFrontendIPs, ip)
+		if len(svc.Backends) != 1 {
+			return fmt.Errorf("missing backend for %s, expected 1, got %d", svc.Name, len(svc.Backends))
+		}
+	}
+	if len(expectedFrontendIPs) > 0 {
+		return fmt.Errorf("missing services for frontends: %v", expectedFrontendIPs)
+	}
+
+	return nil
 }
