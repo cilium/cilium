047607f0161b81a8f5115c98d2bccec2a4ec2e1a
André Martins <andre@cilium.io>
2022-08-18 01:32:12 +0200

    operator: do not GC kvstore nodes if CiliumNodes are not available
    [ upstream commit 62548f2bc1fad0093b88eef34edc477bd4aad98c ]
    
    If users deploy Cilium without creating any CiliumNodes, Cilium Operator
    will GC all kvstore nodes once it starts. This commits adds a guardrail
    to prevent such behavior.
    
    Signed-off-by: André Martins <andre@cilium.io>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- operator/k8s_node.go
+++ operator/k8s_node.go
@@ -107,6 +107,11 @@ func runNodeWatcher(nodeManager *allocator.NodeEventHandler) error {
 			delete(kvStoreNodes, kvStoreNodeName)
 		}
 
+		if len(listOfCiliumNodes) == 0 && len(kvStoreNodes) != 0 {
+			log.Warn("Preventing GC of nodes in the KVStore due the nonexistence of any CiliumNodes in kube-apiserver")
+			return
+		}
+
 		for _, kvStoreNode := range kvStoreNodes {
 			// Only delete the nodes that belong to our cluster
 			if strings.HasPrefix(kvStoreNode.GetKeyName(), option.Config.ClusterName) {
