db4936b510e064eace6895cc7e75d28e66a62e0e
Tobias Klauser <tobias@cilium.io>
2022-08-22 10:25:00 +0200

    go.mod, vendor: update cilium to v1.12.1
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/vendor/github.com/cilium/cilium/pkg/logging/logfields/logfields.go
+++ cilium-cli/vendor/github.com/cilium/cilium/pkg/logging/logfields/logfields.go
@@ -204,9 +204,6 @@ const (
 	// Interface is an interface id/name on the system
 	Interface = "interface"
 
-	// Ipvlan is a ipvlan object or ID
-	Ipvlan = "ipvlan"
-
 	// Veth is a veth object or ID
 	Veth = "veth"
 
@@ -333,9 +330,6 @@ const (
 
 	// DirectRoutingDevice is the name of the direct routing device
 	DirectRoutingDevice = "directRoutingDevice"
-
-	// IpvlanMasterDevice is the ipvlan master device name
-	IpvlanMasterDevice = "ipvlanMasterDevice"
 
 	// DatapathMode is the datapath mode name
 	DatapathMode = "datapathMode"
