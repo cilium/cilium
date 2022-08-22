c4fd33a487088048746880cf48ab0011691e41a6
Federico Hernandez <f@ederi.co>
2022-08-22 16:49:11 +0200

    daemon: Coalesce endpoint CIDRs in ENI mode
    [ upstream commit c87cdeb2496ed99fe3550efa25b515797fe6ab20 ]
    
    Fixes: #18868. Multiple CIDRs are currently not coalesced for the health
    endpoint when setting up routing the corresponding routing tables. This
    results in orphaned routing entries that may conflict when IPs are
    reused for workload pods after an agent restart.
    
    Addresses comment https://github.com/cilium/cilium/pull/20112#issuecomment-1180343763
    
    Signed-off-by: Simone Sciarrati <s.sciarrati@gmail.com>
    Signed-off-by: Federico Hernandez <f@ederi.co>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/datapath/loader/base.go
+++ pkg/datapath/loader/base.go
@@ -37,7 +37,6 @@ import (
 	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/datapath/prefilter"
 	"github.com/cilium/cilium/pkg/defaults"
-	iputil "github.com/cilium/cilium/pkg/ip"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
@@ -167,16 +166,6 @@ func addENIRules(sysSettings []sysctl.Setting, nodeAddressing datapath.NodeAddre
 	routerIP := net.IPNet{
 		IP:   nodeAddressing.IPv4().Router(),
 		Mask: net.CIDRMask(32, 32),
-	}
-
-	cidrs2 := make([]*net.IPNet, 0, 0)
-	for i := range cidrs {
-		cidrs2 = append(cidrs2, &cidrs[i])
-	}
-	resultcidr, _ := iputil.CoalesceCIDRs(cidrs2)
-	cidrs = make([]net.IPNet, 0, len(resultcidr))
-	for _, cidr := range resultcidr {
-		cidrs = append(cidrs, *cidr)
 	}
 
 	for _, cidr := range cidrs {
