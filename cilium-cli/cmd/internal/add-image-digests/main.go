742fde36941526f4fdbe99492d1b6f868757adff
Tobias Klauser <tobias@cilium.io>
2022-08-23 11:50:47 +0200

    all: remove year from copyright notice
    Cilium source files have a header with a SPDX tag and a copyright
    notice, stating that the copyright goes to the Authors of Cilium, and
    containing a year or year range to specify to what period the copyright
    extends.
    
    The year or year range are not necessary in this statement. The CNCF
    recommends not using the year or the copyright symbol, to keep files
    easy to process [0]. The history of the source code is conserved through
    the version control system, and is easily available for all to check
    out.
    
    [0] https://github.com/cncf/foundation/blob/main/copyright-notices.md#copyright-notices
    
    Drop the year and year range for the entire codebase.
    
    The files were processed as follows:
    
        $ cat years.sed
        # Skip after line 6 (branch to end so we still print the line)
        6, $ b
        # Process copyright notice preceded by a SPDX tag
        /^\(#\|\/\/\|\/\*\) *SPDX-License-Identifier: \(GPL-2\.0\|Apache-2\.0\)\( \*\/\)\?$/ {
            N; s/Copyright .* Authors of Cilium/Copyright Authors of Cilium/
        }
        # Process copyright notice followed by a SPDX tag
        /^\(#\|\/\/\|\/\*\) *Copyright .* Authors of Cilium\( \*\/\)\?$/ {
            N; {
                /\(#\|\/\/\|\/\*\) *SPDX-License-Identifier: \(GPL-2\.0\|Apache-2\.0\)\( \*\/\)\?$/ s/Copyright .* Authors of Cilium/Copyright Authors of Cilium/
            }
        }
    
        $ find clustermesh cmd config connectivity defaults hubble install \
        internal k8s release status sysdump .github Dockerfile Makefile \
            -type f -print0 | xargs -0 -P 8 sed -i -f years.sed
    
    Also add copyright headers to some Go source files that were previously
    missing the headers.
    
    Follows cilium/cilium#18813
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/cmd/internal/add-image-digests/main.go
+++ cilium-cli/cmd/internal/add-image-digests/main.go
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2021-2022 Authors of Cilium
+// Copyright Authors of Cilium
 
 // add-image-digests updates defaults/imagedigests.json.
 package main
