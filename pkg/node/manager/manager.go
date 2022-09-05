cilium/cilium

commit 7d58110ca98307ac66a2e46238d89be045965193

node: Skip ipcache for remote node IPs if IPsec is enabled
Before this commit, if IPsec is enabled, we add all remote node IP
addresses to the ipcache of all nodes, regardless of whether
enable-remote-node-identity is true or false.

This commit reverts that behavior to only add those IP addresses if
remote-node identities, node encryption, or encryption+tunneling are
enabled. If encryption+native routing is enabled, we don't need to
expose the remote node IP addresses via the ipcache.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/node/manager/manager.go
@@ -319,7 +319,19 @@ func (m *Manager) legacyNodeIpBehavior() bool {
 	// ipcache. This resulted in a behavioral change. New deployments will
 	// provide this behavior out of the gate, existing deployments will
 	// have to opt into this by enabling remote-node identities.
-	return !m.conf.EncryptionEnabled() && !m.conf.RemoteNodeIdentitiesEnabled()
+	if m.conf.RemoteNodeIdentitiesEnabled() {
+		return false
+	}
+	// Needed to store the SPI for nodes in the ipcache.
+	if m.conf.NodeEncryptionEnabled() {
+		return false
+	}
+	// Needed to store the SPI for pod->remote node in the ipcache since
+	// that path goes through the tunnel.
+	if m.conf.EncryptionEnabled() && m.conf.TunnelingEnabled() {
+		return false
+	}
+	return true
 }
 
 // NodeUpdated is called after the information of a node has been updated. The

commit 7d2d71be7bc51937bcd87826e13dd5219fb1a4e2

node: Fix incorrect code comment
The code comment was incorrect. We won't even add an SPI for pod->remote
node unless node encryption is enabled. The reason we care about that
case is because we want such traffic to go through the tunnel, hence to
have a tunnel endpoint in the ipcache.

Fixes: 7d58110c ("node: Skip ipcache for remote node IPs if IPsec is enabled")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/node/manager/manager.go
@@ -374,8 +374,8 @@ func (m *Manager) legacyNodeIpBehavior() bool {
 	if m.conf.NodeEncryptionEnabled() {
 		return false
 	}
-	// Needed to store the SPI for pod->remote node in the ipcache since
-	// that path goes through the tunnel.
+	// Needed to store the tunnel endpoint for pod->remote node in the
+	// ipcache so that this traffic goes through the tunnel.
 	if m.conf.EncryptionEnabled() && m.conf.TunnelingEnabled() {
 		return false
 	}