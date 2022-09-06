8179b07ed94029ea7848b6d9a43fcb59cdd599bb
Paul Chaignon <paul@cilium.io>
2022-09-06 15:10:14 +0200

    tunnel, node: Populate tunnel map with node IDs
    [ upstream commit 9a70864c6384a5708f30d71386315305af149e4f ]
    
    Same as with the ipcache, we need the tunnel map to carry a node ID for
    each remote node it references. We can use some existing padding for
    that purpose so the map size won't change.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/maps/tunnel/tunnel.go
+++ pkg/maps/tunnel/tunnel.go
@@ -93,8 +93,8 @@ func (k TunnelKey) String() string {
 // +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapValue
 type TunnelValue struct {
 	TunnelIP
-	Key uint8  `align:"key"`
-	Pad uint16 `align:"pad"`
+	Key    uint8  `align:"key"`
+	NodeID uint16 `align:"node_id"`
 }
 
 // GetValuePtr returns the unsafe pointer to the BPF key for users that
@@ -104,7 +104,7 @@ func (k *TunnelValue) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(k) }
 // String provides a string representation of the TunnelValue.
 func (k TunnelValue) String() string {
 	if ip := k.toIP(); ip != nil {
-		return ip.String() + ":" + fmt.Sprintf("%d", k.Key)
+		return ip.String() + ":" + fmt.Sprintf("%d %d", k.Key, k.NodeID)
 	}
 	return "nil"
 }
@@ -128,10 +128,11 @@ func newTunnelKey(ip net.IP) *TunnelKey {
 
 func (v TunnelKey) NewValue() bpf.MapValue { return &TunnelValue{} }
 
-func newTunnelValue(ip net.IP, key uint8) *TunnelValue {
+func newTunnelValue(ip net.IP, key uint8, nodeID uint16) *TunnelValue {
 	result := TunnelValue{}
 	result.TunnelIP = newTunnelIP(ip)
 	result.Key = key
+	result.NodeID = nodeID
 	return &result
 }
 
@@ -150,14 +151,15 @@ func newTunnelIP(ip net.IP) TunnelIP {
 func (v TunnelValue) NewValue() bpf.MapValue { return &TunnelValue{} }
 
 // SetTunnelEndpoint adds/replaces a prefix => tunnel-endpoint mapping
-func (m *Map) SetTunnelEndpoint(encryptKey uint8, prefix, endpoint net.IP) error {
+func (m *Map) SetTunnelEndpoint(encryptKey uint8, nodeID uint16, prefix, endpoint net.IP) error {
 	key := newTunnelKey(prefix)
-	val := newTunnelValue(endpoint, encryptKey)
+	val := newTunnelValue(endpoint, encryptKey, nodeID)
 
 	log.WithFields(logrus.Fields{
 		fieldPrefix:   prefix,
 		fieldEndpoint: endpoint,
 		fieldKey:      encryptKey,
+		fieldNodeID:   nodeID,
 	}).Debug("Updating tunnel map entry")
 
 	return TunnelMap().Update(key, val)
