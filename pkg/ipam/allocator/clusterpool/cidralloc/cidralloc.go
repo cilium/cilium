b865d657d84cfba2e9bdaf73f00ae3b5adba28bd
Sebastian Wicki <sebastian@isovalent.com>
2022-09-07 14:50:37 +0200

    clusterpool: Move CIDRAllocator into own package
    This commit contains no functional changes.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

---
+++ pkg/ipam/allocator/clusterpool/cidralloc/cidralloc.go
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cidralloc
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/cilium/ipam/cidrset"
+
+	"github.com/cilium/cilium/pkg/ip"
+)
+
+type CIDRAllocator interface {
+	fmt.Stringer
+
+	Occupy(cidr *net.IPNet) error
+	AllocateNext() (*net.IPNet, error)
+	Release(cidr *net.IPNet) error
+	IsAllocated(cidr *net.IPNet) (bool, error)
+	IsFull() bool
+	InRange(cidr *net.IPNet) bool
+}
+
+type ErrCIDRCollision struct {
+	cidr      string
+	allocator CIDRAllocator
+}
+
+func (e ErrCIDRCollision) Error() string {
+	return fmt.Sprintf("requested CIDR %s collides with %s", e.cidr, e.allocator)
+}
+
+func (e *ErrCIDRCollision) Is(target error) bool {
+	t, ok := target.(*ErrCIDRCollision)
+	if !ok {
+		return false
+	}
+	return t.cidr == e.cidr
+}
+
+func NewCIDRSets(isV6 bool, strCIDRs []string, maskSize int) ([]CIDRAllocator, error) {
+	cidrAllocators := make([]CIDRAllocator, 0, len(strCIDRs))
+	for _, strCIDR := range strCIDRs {
+		addr, cidr, err := net.ParseCIDR(strCIDR)
+		if err != nil {
+			return nil, err
+		}
+		// Check if CIDRs collide with each other.
+		for _, cidrAllocator := range cidrAllocators {
+			if cidrAllocator.InRange(cidr) {
+				return nil, &ErrCIDRCollision{
+					cidr:      strCIDR,
+					allocator: cidrAllocator,
+				}
+			}
+		}
+		cidrSet, err := NewCIDRSet(isV6, addr, cidr, maskSize)
+		if err != nil {
+			return nil, err
+		}
+		cidrAllocators = append(cidrAllocators, cidrSet)
+	}
+	return cidrAllocators, nil
+}
+
+func NewCIDRSet(isV6 bool, addr net.IP, cidr *net.IPNet, maskSize int) (CIDRAllocator, error) {
+	switch {
+	case isV6 && ip.IsIPv4(addr):
+		return nil, fmt.Errorf("CIDR is not v6 family: %s", cidr)
+	case !isV6 && !ip.IsIPv4(addr):
+		return nil, fmt.Errorf("CIDR is not v4 family: %s", cidr)
+	}
+
+	return cidrset.NewCIDRSet(cidr, maskSize)
+}
