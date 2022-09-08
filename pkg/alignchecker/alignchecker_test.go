dcbb9c3328da2243df3d9de0763dfcee1530b011
Timo Beckers <timo@isovalent.com>
2022-09-08 11:33:07 +0200

    alignchecker: convert test suite to testing.T
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/alignchecker/alignchecker_test.go
+++ pkg/alignchecker/alignchecker_test.go
@@ -7,48 +7,37 @@ package alignchecker
 
 import (
 	"reflect"
+	"strings"
 	"testing"
-
-	. "gopkg.in/check.v1"
 )
 
-type AlignCheckerSuite struct{}
+func TestAlignChecker(t *testing.T) {
+	type foo struct {
+		ipv6 [4]uint32 `align:"$union0"`
+		misc uint32    `align:"$union1"`
+		f    uint8     `align:"family"`
+		pad4 uint8     `align:"pad4"`
+		pad5 uint16    `align:"pad5"`
+	}
 
-var _ = Suite(&AlignCheckerSuite{})
+	type foo2 struct {
+		foo
+	}
 
-func Test(t *testing.T) {
-	TestingT(t)
-}
+	type fooInvalidSize struct {
+		ipv6 uint32
+	}
 
-const path = "testdata/bpf_foo.o"
+	type fooInvalidOffset struct {
+		ipv6 [4]uint32 `align:"$union0"`
+		misc uint32    `align:"$union1"`
+		f    uint16    `align:"family"`
+		pad4 uint8     `align:"pad4"`
+		pad5 uint8     `align:"pad5"`
+	}
 
-type foo struct {
-	ipv6 [4]uint32 `align:"$union0"`
-	misc uint32    `align:"$union1"`
-	f    uint8     `align:"family"`
-	pad4 uint8     `align:"pad4"`
-	pad5 uint16    `align:"pad5"`
-}
+	type toCheck map[string][]reflect.Type
 
-type foo2 struct {
-	foo
-}
-
-type fooInvalidSize struct {
-	ipv6 uint32
-}
-
-type fooInvalidOffset struct {
-	ipv6 [4]uint32 `align:"$union0"`
-	misc uint32    `align:"$union1"`
-	f    uint16    `align:"family"`
-	pad4 uint8     `align:"pad4"`
-	pad5 uint8     `align:"pad5"`
-}
-
-type toCheck map[string][]reflect.Type
-
-func (t *AlignCheckerSuite) TestCheckStructAlignments(c *C) {
 	testCases := []struct {
 		cName   string
 		goTypes []reflect.Type
@@ -81,14 +70,14 @@ func (t *AlignCheckerSuite) TestCheckStructAlignments(c *C) {
 			[]reflect.Type{
 				reflect.TypeOf(fooInvalidSize{}),
 			},
-			`*.fooInvalidSize\(4\) size does not match foo\(24\)`,
+			"fooInvalidSize(4) size does not match foo(24)",
 		},
 		{
 			"foo",
 			[]reflect.Type{
 				reflect.TypeOf(fooInvalidOffset{}),
 			},
-			`*.fooInvalidOffset.pad4 offset\(22\) does not match foo.pad4\(21\)`,
+			"fooInvalidOffset.pad4 offset(22) does not match foo.pad4(21)",
 		},
 		{
 			"bar",
@@ -100,11 +89,16 @@ func (t *AlignCheckerSuite) TestCheckStructAlignments(c *C) {
 	}
 
 	for _, tt := range testCases {
-		err := CheckStructAlignments(path, toCheck{tt.cName: tt.goTypes}, true)
-		if tt.err == "" {
-			c.Assert(err, IsNil)
-		} else {
-			c.Assert(err, ErrorMatches, tt.err)
+		err := CheckStructAlignments("testdata/bpf_foo.o", toCheck{tt.cName: tt.goTypes}, true)
+		if tt.err != "" {
+			if !strings.Contains(err.Error(), tt.err) {
+				t.Fatalf("error '%s' did not contain string '%s'", err, tt.err)
+			}
+			return
+		}
+
+		if err != nil {
+			t.Fatalf("unexpected error checking %s: %s", tt.cName, err)
 		}
 	}
 }
