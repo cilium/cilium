260a90ed5c351a18ea567759bf7bf1379d1ed7e9
Tam Mach <tam.mach@cilium.io>
2022-09-08 18:24:05 +1000

    envoy: Add skeleton for service LB via envoy
    This commit contains just a simple skeleton for L7 load balancing
    capability via envoy proxy. There is no processing logic at all
    right now.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

---
+++ operator/pkg/ciliumenvoyconfig/envoy_config.go
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package ciliumenvoyconfig
+
+import (
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+
+	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/k8s/informer"
+	"github.com/cilium/cilium/pkg/k8s/utils"
+)
+
+type envoyConfigManager struct {
+	client     client.Clientset
+	informer   cache.Controller
+	store      cache.Store
+	maxRetries int
+}
+
+func newEnvoyConfigManager(client client.Clientset, maxRetries int) (*envoyConfigManager, error) {
+	manager := &envoyConfigManager{
+		client:     client,
+		maxRetries: maxRetries,
+	}
+
+	manager.store, manager.informer = informer.NewInformer(
+		utils.ListerWatcherFromTyped[*v2.CiliumEnvoyConfigList](client.CiliumV2().CiliumEnvoyConfigs(corev1.NamespaceAll)),
+		&v2.CiliumEnvoyConfig{},
+		0,
+		cache.ResourceEventHandlerFuncs{},
+		nil,
+	)
+
+	go manager.informer.Run(wait.NeverStop)
+	if !cache.WaitForCacheSync(wait.NeverStop, manager.informer.HasSynced) {
+		return manager, fmt.Errorf("unable to sync envoy configs")
+	}
+	return manager, nil
+}
+
+// getByKey is a wrapper of Store.GetByKey but with concrete CiliumEnvoyConfig object
+func (em *envoyConfigManager) getByKey(key string) (*v2.CiliumEnvoyConfig, bool, error) {
+	objFromCache, exists, err := em.store.GetByKey(key)
+	if objFromCache == nil || !exists || err != nil {
+		return nil, exists, err
+	}
+	envoyConfig, ok := objFromCache.(*v2.CiliumEnvoyConfig)
+	if !ok {
+		return nil, exists, fmt.Errorf("got invalid object from cache")
+	}
+	return envoyConfig, exists, err
+}
