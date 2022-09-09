cc37ec0a047d18254655e5badbe2399fd8da7554
Timo Beckers <timo@isovalent.com>
2022-09-09 12:28:09 +0200

    bpf: alignchecker - prune imports, clang-format testdata
    Some of these imports are (no longer) necessary and can be removed.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/alignchecker/testdata/bpf_foo.c
+++ pkg/alignchecker/testdata/bpf_foo.c
@@ -6,32 +6,32 @@
 #include <linux/types.h>
 
 union v6addr {
-        struct {
-                __u32 p1;
-                __u32 p2;
-                __u32 p3;
-                __u32 p4;
-        };
-        __u8 addr[16];
+  struct {
+    __u32 p1;
+    __u32 p2;
+    __u32 p3;
+    __u32 p4;
+  };
+  __u8 addr[16];
 };
 
 struct foo {
-	union {
-		struct {
-			__u32		ip4;
-			__u32		pad1;
-			__u32		pad2;
-			__u32		pad3;
-		};
-		union v6addr	ip6;
-	};
-    union {
-        __u32   p1;
-        __u32   p2;
+  union {
+    struct {
+      __u32 ip4;
+      __u32 pad1;
+      __u32 pad2;
+      __u32 pad3;
     };
-	__u8 family;
-	__u8 pad4;
-	__u16 pad5;
+    union v6addr ip6;
+  };
+  union {
+    __u32 p1;
+    __u32 p2;
+  };
+  __u8 family;
+  __u8 pad4;
+  __u16 pad5;
 } __attribute__((packed));
 
 struct foo _1;
