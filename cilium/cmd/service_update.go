cilium/cilium

commit a5558f6e47849d7a9e102aca698d86ab718dadeb

fix: logic to allow backend weights moved to service_update.go file
Signed-off-by: Ondrej Blazek <ondrej.blazek@firma.seznam.cz>

cilium/cmd/service_update.go
@@ -13,7 +13,9 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cilium/cilium/api/v1/models"
+	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/loadbalancer"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 var (
@@ -154,8 +156,13 @@ func updateService(cmd *cobra.Command, args []string) {
 		}
 	}
 
-	if len(backendWeights) > 0 && len(backendWeights) != len(backends) {
-		Fatalf("Mismatch between number of backend weights and number of backends")
+	if len(backendWeights) > 0 {
+		if option.Config.DatapathMode != datapathOption.DatapathModeLBOnly {
+			Fatalf("Backend weights are supported currently only in lb-only mode")
+		}
+		if len(backendWeights) != len(backends) {
+			Fatalf("Mismatch between number of backend weights and number of backends")
+		}
 	}
 
 	spec.BackendAddresses = nil

daemon/cmd/loadbalancer.go
@@ -11,10 +11,8 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/service"
 	"github.com/cilium/cilium/pkg/api"
-	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/service"
 )
 
@@ -58,9 +56,6 @@ func (h *putServiceID) Handle(params PutServiceIDParams) middleware.Responder {
 	}
 	backends := []*loadbalancer.Backend{}
 	for _, v := range params.Config.BackendAddresses {
-		if v.Weight != nil && option.Config.DatapathMode != datapathOption.DatapathModeLBOnly {
-			return api.Error(PutServiceIDInvalidBackendCode, fmt.Errorf("backend weights are supported currently only in lb-only mode"))
-		}
 		b, err := loadbalancer.NewBackendFromBackendModel(v)
 		if err != nil {
 			return api.Error(PutServiceIDInvalidBackendCode, err)

commit 680b847197883b911159e30f8b8e56ee0b77e547

cilium/cmd: check datapath mode on running daemon
The current check fails because on the CLI we don't set the datapath
mode. We need to retrieve the config of the running daemon and check the
datapath mode there instead.

Fixes: #21303
Fixes: a5558f6e4784 ("fix: logic to allow backend weights moved to service_update.go file")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

cilium/cmd/service_update.go
@@ -15,7 +15,6 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/loadbalancer"
-	"github.com/cilium/cilium/pkg/option"
 )
 
 var (
@@ -157,7 +156,15 @@ func updateService(cmd *cobra.Command, args []string) {
 	}
 
 	if len(backendWeights) > 0 {
-		if option.Config.DatapathMode != datapathOption.DatapathModeLBOnly {
+		resp, err := client.ConfigGet()
+		if err != nil {
+			Fatalf("Unable to retrieve cilium configuration: %s", err)
+		}
+		if resp.Status == nil {
+			Fatalf("Unable to retrieve cilium configuration: empty response")
+		}
+
+		if resp.Status.DatapathMode != datapathOption.DatapathModeLBOnly {
 			Fatalf("Backend weights are supported currently only in lb-only mode")
 		}
 		if len(backendWeights) != len(backends) {