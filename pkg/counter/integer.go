8e74ea918da5dc0c7c899471b2d4167f3c4afa80
Tobias Klauser <tobias@isovalent.com>
2022-09-14 16:41:44 +0200

    counter: add generic implementation and use it to replace StringCounter
    The generic implementation will be used in a follow-up change to replace
    StringCounter instances tracking backends in pkg/service  to use
    counter.Counter[netip.Addr].
    
    Keep IntCounter as is for now because for IntCounter.ToBPFData it
    requires the map elements to be ordered (i.e. constraints.Ordered), not
    just comparable.  This method is only used on instances of type
    IntCounter so far and making the map elements ordered would limit its
    use e.g. with netip.Addr.
    
    Signed-off-by: Tobias Klauser <tobias@isovalent.com>

--- pkg/counter/integer.go
+++ pkg/counter/integer.go
@@ -12,42 +12,26 @@ import (
 // No threadsafety is provided within this structure, the user is expected to
 // handle concurrent access to this structure if it is used from multiple
 // threads.
-type IntCounter map[int]int
-
-// DeepCopy makes a new copy of the received IntCounter.
-func (i IntCounter) DeepCopy() IntCounter {
-	result := make(IntCounter, len(i))
-	for k, v := range i {
-		result[k] = v
-	}
-	return result
-}
+type IntCounter Counter[int]
 
 // Add increments the reference count for the specified integer key.
 func (i IntCounter) Add(key int) (changed bool) {
-	value, exists := i[key]
-	if !exists {
-		changed = true
-	}
-	i[key] = value + 1
-	return changed
+	return Counter[int](i).Add(key)
 }
 
 // Delete decrements the reference count for the specified integer key.
 func (i IntCounter) Delete(key int) bool {
-	value := i[key]
-	if value <= 1 {
-		delete(i, key)
-		return true
-	}
-	i[key] = value - 1
-	return false
+	return Counter[int](i).Delete(key)
+}
+
+// DeepCopy makes a new copy of the received IntCounter.
+func (i IntCounter) DeepCopy() IntCounter {
+	return IntCounter(Counter[int](i).DeepCopy())
 }
 
 // ToBPFData returns the keys as a slice, sorted from high to low.
 func (i IntCounter) ToBPFData() []int {
 	result := make([]int, 0, len(i))
-
 	for key := range i {
 		result = append(result, key)
 	}
