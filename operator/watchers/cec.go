b7690394fded1f5cb9842b68f22b4adbbbb9f553
Tam Mach <tam.mach@cilium.io>
2022-09-18 22:08:42 +1000

    envoy: Support list of global ports for redirect
    [ upstream commit 281d95a6ae18c18ffa51d2b44262ad943626cb60 ]
    
    This commit is to support envoy redirect for all services having port
    number in list, so that user didn't need to annotate one by one. Just a
    note that for more customized and advanced use case, service annotation
    will be still required.
    
    One caveat is that the current CEC operates in service level i.e. there
    is no granular control to only redirect one particular service port.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- operator/watchers/cec.go
+++ operator/watchers/cec.go
@@ -15,14 +15,14 @@ import (
 // StartCECController starts the service watcher if it hasn't already and looks
 // for service of type with envoy enabled LB annotation. Once such service is
 // found, it will try to create one CEC associated with the service.
-func StartCECController(ctx context.Context, clientset k8sClient.Clientset, services resource.Resource[*slim_corev1.Service]) {
+func StartCECController(ctx context.Context, clientset k8sClient.Clientset, services resource.Resource[*slim_corev1.Service], ports []string) {
 	go func() {
 		store, err := services.Store(ctx)
 		if err != nil {
 			log.WithError(err).Fatal("Failed to retrieve service store")
 		}
 
-		m, err := ciliumenvoyconfig.New(clientset, store.CacheStore())
+		m, err := ciliumenvoyconfig.New(clientset, store.CacheStore(), ports )
 		if err != nil {
 			log.WithError(err).Fatal("Error creating CiliumEnvoyConfiguration manager")
 		}
