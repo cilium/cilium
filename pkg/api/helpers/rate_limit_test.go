8039191d6af65fee3fccf78c3473b83609b3fa5f
Sebastian Wicki <sebastian@isovalent.com>
2022-09-19 18:10:15 +0200

    helpers/rate: Fix issue where refill rate was effectively halved
    This fixes a bug in the helpers/rate package (used for rate limiting
    cilium-operator access to external cloud APIs such as
    AWS/Azure/AlibabaCloud etc). Before this commit, the `Limit` function
    would accidentally request two wait tokens instead of one when the rate
    limit was hit: Once in `Reserve`, and then once again by calling `Wait`.
    This effectively halved the bucket refill rate (aka "QPS") under load.
    
    This commit extends the unit tests with a regression test. The old code
    will fail in the newly added TestRateLimitWait test. The existing test
    is extended to valudate the expected burst behavior.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- pkg/api/helpers/rate_limit_test.go
+++ pkg/api/helpers/rate_limit_test.go
@@ -13,6 +13,7 @@ import (
 	"gopkg.in/check.v1"
 
 	"github.com/cilium/cilium/pkg/api/metrics/mock"
+	"github.com/cilium/cilium/pkg/checker"
 )
 
 func Test(t *testing.T) {
@@ -23,14 +24,48 @@ type HelpersSuite struct{}
 
 var _ = check.Suite(&HelpersSuite{})
 
-func (e *HelpersSuite) TestRateLimit(c *check.C) {
+func (e *HelpersSuite) TestRateLimitBurst(c *check.C) {
 	metricsAPI := mock.NewMockMetrics()
-	limiter := NewApiLimiter(metricsAPI, 10.0, 4)
+	limiter := NewApiLimiter(metricsAPI, 1, 10)
 	c.Assert(limiter, check.Not(check.IsNil))
 
+	// Exhaust bucket (rate limit should not kick in)
 	for i := 0; i < 10; i++ {
 		limiter.Limit(context.TODO(), "test")
 	}
+	c.Assert(metricsAPI.RateLimit("test"), check.Equals, time.Duration(0))
 
-	c.Assert(metricsAPI.RateLimit("test"), check.Not(check.DeepEquals), time.Duration(0))
+	// Rate limit should now kick in (use an expired context to avoid waiting 1sec)
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Microsecond)
+	defer cancel()
+	limiter.Limit(ctx, "test")
+	c.Assert(metricsAPI.RateLimit("test"), check.Not(checker.Equals), time.Duration(0))
+}
+
+func (e *HelpersSuite) TestRateLimitWait(c *check.C) {
+	metricsAPI := mock.NewMockMetrics()
+	limiter := NewApiLimiter(metricsAPI, 100, 1)
+	c.Assert(limiter, check.Not(check.IsNil))
+
+	// Exhaust bucket
+	limiter.Limit(context.TODO(), "test")
+	c.Assert(metricsAPI.RateLimit("test"), checker.Equals, time.Duration(0))
+
+	// Hit rate limit 15 times. The bucket refill rate is 100 per second,
+	// meaning we expect this to take around 15 * 10 = 150 milliseconds
+	start := time.Now()
+	for i := 0; i < 15; i++ {
+		limiter.Limit(context.TODO(), "test")
+	}
+	measured := time.Since(start)
+
+	// Measured duration should be approximately the accounted duration
+	accounted := metricsAPI.RateLimit("test")
+	if measured > 2*accounted {
+		// We allow the wait to be up to 2x larger than the expected wait time
+		// to avoid flaky tests. If you are reading this because this test has
+		// been flaky despite the 100% margin of error, my recommendation
+		// is to disable this check by replacing the c.Errorf below with c.Logf
+		c.Errorf("waited longer than expected (expected %s (+/-100%%), measured %s)", accounted, measured)
+	}
 }
