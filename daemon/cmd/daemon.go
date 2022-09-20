cilium/cilium

commit 266f70588825716c5ef4d0ceff9201ba8e6fa44b

dnsproxy: add cleanup
This change adds Cleanup function to dnsproxy which is added to daemon
cleanup module.

The cleanup closes TCP and UDP sockets, which will cause proxy to stop
serving DNS traffic before shutdown.

Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

daemon/cmd/daemon.go
@@ -842,6 +842,11 @@ func NewDaemon(ctx context.Context, cleaner *daemonCleanup, epMgr *endpointmanag
 		bootstrapStats.fqdn.EndError(err)
 		return nil, restoredEndpoints, err
 	}
+	// This is done in preCleanup so that proxy stops serving DNS traffic before shutdown
+	cleaner.preCleanupFuncs.Add(func() {
+		proxy.DefaultDNSProxy.Cleanup()
+	})
+
 	bootstrapStats.fqdn.End(true)
 
 	if k8s.IsEnabled() {

pkg/fqdn/dnsproxy/proxy.go
@@ -113,6 +113,7 @@ type DNSProxy struct {
 	// ConcurrencyGracePeriod is the grace period for waiting on
 	// ConcurrencyLimit before timing out
 	ConcurrencyGracePeriod time.Duration
+
 	// logLimiter limits log msgs that could be bursty and too verbose.
 	// Currently used when ConcurrencyLimit is set.
 	logLimiter logging.Limiter
@@ -151,6 +152,9 @@ type DNSProxy struct {
 	// rejectReply is the OPCode send from the DNS-proxy to the endpoint if the
 	// DNS request is invalid
 	rejectReply int32
+
+	// UnbindAddress unbinds dns servers from socket in order to stop serving DNS traffic before proxy shutdown
+	unbindAddress func()
 }
 
 // perEPAllow maps EndpointIDs to ports + selectors + rules
@@ -494,6 +498,12 @@ func StartDNSProxy(address string, port uint16, enableDNSCompression bool, maxRe
 			}
 			log.Fatalf("Failed to start %s DNS Proxy on %s", server.Net, server.Addr)
 		}(s)
+	}
+
+	// This function is called in proxy.Cleanup, which is added to Daemon cleanup module in bootstrapFQDN
+	p.unbindAddress = func() {
+		UDPConn.Close()
+		TCPListener.Close()
 	}
 
 	return p, nil
@@ -978,4 +988,10 @@ func GetSelectorRegexMap(l7 policy.L7DataMap) (CachedSelectorREEntry, error) {
 	}
 
 	return newRE, nil
+}
+
+func (p *DNSProxy) Cleanup() {
+	if p.unbindAddress != nil {
+		p.unbindAddress()
+	}
 }

pkg/fqdn/proxy/proxy.go
@@ -16,6 +16,7 @@ type DNSProxier interface {
 	GetBindPort() uint16
 	SetRejectReply(string)
 	RestoreRules(op *endpoint.Endpoint)
+	Cleanup()
 }
 
 type MockFQDNProxy struct{}
@@ -41,5 +42,9 @@ func (m MockFQDNProxy) SetRejectReply(s string) {
 }
 
 func (m MockFQDNProxy) RestoreRules(op *endpoint.Endpoint) {
+	return
+}
+
+func (m MockFQDNProxy) Cleanup() {
 	return
 }

commit e7b02d6c14a11f115439ba22d99a14fe225c5afc

daemon: Fix a nil dereference on cleanup when DNS proxy is not enabled
The DNS proxy is only allocated if L7 proxy is enabled. The cleanup code did
not check if it allocated causing a nil deref on shutdown.

controlplane test was modified to only set the mock DNS proxy when L7 proxy
is enabled to reflect behavior of bootstrapFDQN and catch similar issue
in the future.

Fixes: #21264
Fixes: 266f705888 ("dnsproxy: add cleanup")

Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/daemon.go
@@ -840,10 +840,12 @@ func NewDaemon(ctx context.Context, cleaner *daemonCleanup,
 		bootstrapStats.fqdn.EndError(err)
 		return nil, restoredEndpoints, err
 	}
-	// This is done in preCleanup so that proxy stops serving DNS traffic before shutdown
-	cleaner.preCleanupFuncs.Add(func() {
-		proxy.DefaultDNSProxy.Cleanup()
-	})
+	if proxy.DefaultDNSProxy != nil {
+		// This is done in preCleanup so that proxy stops serving DNS traffic before shutdown
+		cleaner.preCleanupFuncs.Add(func() {
+			proxy.DefaultDNSProxy.Cleanup()
+		})
+	}
 
 	bootstrapStats.fqdn.End(true)
 

test/controlplane/suite/testcase.go
@@ -104,8 +104,6 @@ func (cpt *ControlPlaneTest) SetupEnvironment(modConfig func(*agentOption.Daemon
 	version.Update(cpt.clients, true)
 	k8s.SetClients(cpt.clients, cpt.clients.Slim(), cpt.clients, cpt.clients)
 
-	proxy.DefaultDNSProxy = fqdnproxy.MockFQDNProxy{}
-
 	agentOption.Config.Populate(agentCmd.Vp)
 	agentOption.Config.IdentityAllocationMode = agentOption.IdentityAllocationModeCRD
 	agentOption.Config.DryMode = true
@@ -130,6 +128,9 @@ func (cpt *ControlPlaneTest) SetupEnvironment(modConfig func(*agentOption.Daemon
 	// Apply the test specific configuration
 	modConfig(agentOption.Config, operatorOption.Config)
 
+	if agentOption.Config.EnableL7Proxy {
+		proxy.DefaultDNSProxy = fqdnproxy.MockFQDNProxy{}
+	}
 	return cpt
 }
 