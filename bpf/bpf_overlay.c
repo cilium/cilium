cilium/cilium

commit 15862efb0a5ddda392f5864cb24af537d8124659

ipsec: Add missing -> stack encrypted trace for overlay
Currently, -> stack encrypted trace is missing when the packet leaves the
overlay device to stack for decryption. Add it for both IPv4 and v6.

Example trace output of simple Pod-to-Pod ICMP Echo Reply path

Before

```
<- overlay encrypted ...
(missing)
<- overlay flow 0x560bdfad , identity 1404->unknown ... EchoReply
-> endpoint 1217 flow 0x560bdfad ... EchoReply
```

After

```
<- overlay encrypted ...
-> stack encrypted ...
<- overlay flow 0x560bdfad , identity 1404->unknown ... EchoReply
-> endpoint 1217 flow 0x560bdfad ... EchoReply
```

Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

bpf/bpf_overlay.c
@@ -116,6 +116,11 @@ static __always_inline int handle_ipv6(struct __ctx_buff *ctx,
 		 * packet.
 		 */
 		ctx_change_type(ctx, PACKET_HOST);
+
+		send_trace_notify(ctx, TRACE_TO_STACK, 0, 0, 0,
+				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED,
+				  TRACE_PAYLOAD_LEN);
+
 		return CTX_ACT_OK;
 	}
 	ctx->mark = 0;
@@ -270,6 +275,11 @@ static __always_inline int handle_ipv4(struct __ctx_buff *ctx, __u32 *identity)
 		 * packet.
 		 */
 		ctx_change_type(ctx, PACKET_HOST);
+
+		send_trace_notify(ctx, TRACE_TO_STACK, 0, 0, 0,
+				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED,
+				  TRACE_PAYLOAD_LEN);
+
 		return CTX_ACT_OK;
 	}
 	ctx->mark = 0;

commit 2881fa86958a727588b08d88b73613d5a1bd9c23

bpf: Add missing identity to TRACE_TO_STACK packet traces
Fixes: 15862efb ("ipsec: Add missing -> stack encrypted trace for overlay")
Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/bpf_overlay.c
@@ -127,7 +127,7 @@ static __always_inline int handle_ipv6(struct __ctx_buff *ctx,
 		 */
 		ctx_change_type(ctx, PACKET_HOST);
 
-		send_trace_notify(ctx, TRACE_TO_STACK, 0, 0, 0,
+		send_trace_notify(ctx, TRACE_TO_STACK, *identity, 0, 0,
 				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED,
 				  TRACE_PAYLOAD_LEN);
 
@@ -286,7 +286,7 @@ skip_vtep:
 		 */
 		ctx_change_type(ctx, PACKET_HOST);
 
-		send_trace_notify(ctx, TRACE_TO_STACK, 0, 0, 0,
+		send_trace_notify(ctx, TRACE_TO_STACK, *identity, 0, 0,
 				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED,
 				  TRACE_PAYLOAD_LEN);
 