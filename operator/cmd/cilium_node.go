694892c74c1e204ac63cc5e639cda35269367794
Andrey Klimentyev <andrey.klimentyev@flant.com>
2022-09-22 09:55:54 +0300

    Fixed CCNP garbage collection
    CCNPs are converted internally into CNPs, but metadata.name has been forgotten
    
    Fixes #21393
    
    Signed-off-by: Andrey Klimentyev <andrey.klimentyev@flant.com>

--- operator/cmd/cilium_node.go
+++ operator/cmd/cilium_node.go
@@ -369,6 +369,9 @@ func runCNPNodeStatusGC(name string, clusterwide bool, clientset k8sClient.Clien
 						cnpItemsList = make([]cilium_v2.CiliumNetworkPolicy, 0)
 						for _, ccnp := range ccnpList.Items {
 							cnpItemsList = append(cnpItemsList, cilium_v2.CiliumNetworkPolicy{
+								ObjectMeta: meta_v1.ObjectMeta{
+									Name: ccnp.Name,
+								},
 								Status: ccnp.Status,
 							})
 						}
