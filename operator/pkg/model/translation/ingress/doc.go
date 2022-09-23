052520ce18e80c7c041beb37ef127895e21c9655
Tam Mach <tam.mach@cilium.io>
2022-09-23 21:37:46 +1000

    ingress/translation: Add dedicated LB translation
    The core logic is same as shared translator, except:
    
    - Resource names are coming from Ingress
    - Service and Endpoint resources are required
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/model/translation/ingress/doc.go
+++ operator/pkg/model/translation/ingress/doc.go
@@ -3,7 +3,4 @@
 
 // Package ingress contains the translation logic from Ingress to CiliumEnvoyConfig
 // and related resources.
-//
-// Currently, there is only one translator i.e. shared LB Ingress translator. However,
-// the long term goal is to consolidate dedicated LB Ingress translator into this package.
 package ingress
