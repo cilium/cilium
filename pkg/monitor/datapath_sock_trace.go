3fdb434a71e14236edabeaad7dce4768278db513
Aditi Ghag <aditi@cilium.io>
2022-09-29 15:42:29 -0700

    monitor: Add parser for socket-lb tracing events
    This is a no-frills parser that only prints
    parsed events in the debug mode. The logic
    will be extended in future (e.g., filtering).
    Although as compared to per packet events,
    the socket-lb tracing events will be less frequent.
    
    Testing:
    
    $ kubectl exec mediabot -- curl http://172.20.0.178
    
    Output snippet:
    
    ```
    $cilium monitor -v -t trace-sock
    
    CPU 03: [pre-xlate-fwd] cgroup_id: 13125 sock_cookie: 114605, dst [172.20.0.178:80] tcp
    CPU 03: [post-xlate-fwd] cgroup_id: 13125 sock_cookie: 114605, dst [10.12.0.43:80] tcp
    CPU 03: [pre-xlate-rev] cgroup_id: 13125 sock_cookie: 114605, dst [10.12.0.43:80] tcp
    CPU 03: [post-xlate-rev] cgroup_id: 13125 sock_cookie: 114605, dst [172.20.0.178:80] tcp
    
    $ find /run/cilium/cgroupv2/ -inum 13125
    
    /run/cilium/cgroupv2/kubepods/besteffort/poda48639f8-0ab5-40cc-aaa2-ae63adb71fe5/ae31f464145ffd38ec0bb80943ba5a23a47acb5b69e8ac52c912cf58430a1404
    ```
    
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

--- pkg/monitor/datapath_sock_trace.go
+++ pkg/monitor/datapath_sock_trace.go
@@ -1,8 +1,15 @@
 package monitor
 
-import "github.com/cilium/cilium/pkg/types"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
 
-// Socket trace event point with respect to service translation
+	"github.com/cilium/cilium/pkg/types"
+)
+
+// Service translation event point in socket trace event messages
 const (
 	XlatePointUnknown = iota
 	XlatePointPreDirectionFwd
@@ -11,12 +18,14 @@ const (
 	XlatePointPostDirectionRev
 )
 
-// L4 protocol for socket trace event
+// L4 protocol for socket trace event messages
 const (
 	L4ProtocolUnknown = iota
 	L4ProtocolTCP
 	L4ProtocolUDP
 )
+
+const TraceSockNotifyFlagIPv6 uint8 = 0x1
 
 // TraceSockNotify is message format for socket trace notifications sent from datapath.
 // Keep this in sync to the datapath structure (trace_sock_notify) defined in
@@ -30,4 +39,46 @@ type TraceSockNotify struct {
 	CgroupId   uint64
 	L4Proto    uint8
 	Flags      uint8
+}
+
+func (t *TraceSockNotify) DumpDebug(prefix string) {
+	buf := bufio.NewWriter(os.Stdout)
+
+	fmt.Fprintf(buf, "%s [%s] cgroup_id: %d sock_cookie: %d, dst [%s]:%d %s \n",
+		prefix, t.XlatePointStr(), t.CgroupId, t.SockCookie, t.IP(), t.DstPort, t.L4ProtoStr())
+	buf.Flush()
+}
+
+func (t *TraceSockNotify) XlatePointStr() string {
+	switch t.XlatePoint {
+	case XlatePointPreDirectionFwd:
+		return "pre-xlate-fwd"
+	case XlatePointPostDirectionFwd:
+		return "post-xlate-fwd"
+	case XlatePointPreDirectionRev:
+		return "pre-xlate-rev"
+	case XlatePointPostDirectionRev:
+		return "post-xlate-rev"
+	default:
+		return "unknown"
+	}
+}
+
+// IP returns the IPv4 or IPv6 address field.
+func (t *TraceSockNotify) IP() net.IP {
+	if (t.Flags & TraceSockNotifyFlagIPv6) != 0 {
+		return t.DstIP[:]
+	}
+	return t.DstIP[:4]
+}
+
+func (t *TraceSockNotify) L4ProtoStr() string {
+	switch t.L4Proto {
+	case L4ProtocolTCP:
+		return "tcp"
+	case L4ProtocolUDP:
+		return "udp"
+	default:
+		return "unknown"
+	}
 }
