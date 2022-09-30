60d2e3062c61a92d7912695760119da6d3ef1209
Dylan Reimerink <dylan.reimerink@isovalent.com>
2022-09-30 15:27:31 +0200

    ipam: Fix overlapping PodCIDR allocation
    [ upstream commit 4c9c1d352abd78ef85917402d3169bdb611456b8 ]
    
    This commit fixes an edge case in the `NodesPodCIDRManager`.
    If there were any nodes on operator startup which have no PodCIDRs, the
    operator would sometimes assign PodCIDRs to these nodes which have
    already been allocated to other nodes.
    
    The operator assumed that when `k8sCiliumNodesCacheSynced` closes, all
    node events have been processed. And it proceeds to call `Resync` on
    the `nodeManager`.
    
    The `NodesPodCIDRManager` will queue any nodes
    without PodCIDRs to be allocated once the `canAllocatePodCIDRs` variable
    is set. This variable is set by the `Resync`.
    
    So, the assumption/expected behavior is that the
    `NodesPodCIDRManager.Update` function has been called for all nodes in
    the cache before `Resync` is called. However, this wasn't the case.
    The `startSynchronizingCiliumNodes` function starts the informer
    and connects the nodeManager to it. But instead of handling the events
    at once, the callbacks enqueue the events, to be handled by a separate
    go routine. This means that `k8sCiliumNodesCacheSynced` is closed once
    all of the node events are enqueued, not when they have been processed
    by the `nodeManager`.
    
    This commit fixes this behavior by processing all events at once
    in the informer callbacks until the full sync is complete, at which
    point we will switch over to using the workqueue.
    
    Fixes: #21482
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- operator/main.go
+++ operator/main.go
@@ -521,7 +521,7 @@ func onOperatorStartLeading(ctx context.Context) {
 		// Once the CiliumNodes are synchronized with the operator we will
 		// be able to watch for K8s Node events which they will be used
 		// to create the remaining CiliumNodes.
-		<-k8sCiliumNodesCacheSynced
+		<-ciliumNodeManagerQueueSynced
 
 		// We don't want CiliumNodes that don't have podCIDRs to be
 		// allocated with a podCIDR already being used by another node.
