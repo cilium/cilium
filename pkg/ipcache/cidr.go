cilium/cilium

commit 40e13ea2a5a944a45761fc433c4c971536957f4b

ipcache: Fix race in identity/ipcache release
Create a critical section for identity release + removal from ipcache.
Otherwise, it's possible to trigger the following race condition:

    Goroutine 1                | Goroutine 2
    ---------------------------+--------------------------------------
    releaseCIDRIdentities()    | AllocateCIDRs()
    -> Release(..., id, ...)   |
                               | -> allocate(...)
                               | -> ipc.UpsertGeneratedIdentities(...)
    -> ipc.deleteLocked(...)   |

In this case, the expectation from Goroutine 2 is that a new identity
is allocated and that identity is inserted into the ipcache, but the
result is that the identity is allocated but the ipcache entry is
missing. This is partly because the identity released in goroutine 1 is
different from the newly allocated identity in goroutine 2, however
goroutine 1 will delete the ipcache entry based on the prefix and not
the identity. Therefore it's possible for goroutine 1 to delete the
ipcache entry corresponding to the identity allocated in goroutine 2.

Note that for balancing the upsert / release, we perhaps should cover
the entire allocation + ipcache push in Upsert() with the same locking.
However, on upsert there is an optional feature of the API to defer the
ipcache upsert to a later point, governed by the caller. There is
currently no way to extend the locking over that much longer time
period, so we only cover the allocation step there. This should still be
safe, as one of the following cases should occur:

    Goroutine 1                | Goroutine 2
    ---------------------------+--------------------------------------
    Lock                       |
    Release()                  |
    deleteLocked()             |
    Unlock                     |
                               | Lock
                               | ipc.allocate()
                               | Unlock
                               | .... (repeat below)

pkg/ipcache/cidr.go
@@ -47,6 +47,7 @@ func (ipc *IPCache) AllocateCIDRs(
 		newlyAllocatedIdentities = map[string]*identity.Identity{}
 	}
 
+	ipc.Lock()
 	allocatedIdentities := make(map[string]*identity.Identity, len(prefixes))
 	for i, p := range prefixes {
 		if p == nil {
@@ -72,6 +73,7 @@ func (ipc *IPCache) AllocateCIDRs(
 			newlyAllocatedIdentities[prefixStr] = id
 		}
 	}
+	ipc.Unlock()
 
 	// Only upsert into ipcache if identity wasn't allocated
 	// before and the caller does not care doing this
@@ -141,6 +143,21 @@ func (ipc *IPCache) allocate(prefix *net.IPNet, lbls labels.Labels, oldNID ident
 }
 
 func (ipc *IPCache) releaseCIDRIdentities(ctx context.Context, identities map[string]*identity.Identity) {
+	// Create a critical section for identity release + removal from ipcache.
+	// Otherwise, it's possible to trigger the following race condition:
+	//
+	// Goroutine 1                | Goroutine 2
+	// releaseCIDRIdentities()    | AllocateCIDRs()
+	// -> Release(..., id, ...)   |
+	//                            | -> allocate(...)
+	//                            | -> ipc.UpsertGeneratedIdentities(...)
+	// -> ipc.deleteLocked(...)   |
+	//
+	// In this case, the expectation from Goroutine 2 is that an identity
+	// is allocated and that identity is in the ipcache, but the result
+	// is that the identity is allocated but the ipcache entry is missing.
+	ipc.Lock()
+	defer ipc.Unlock()
 	for prefix, id := range identities {
 		released, err := ipc.IdentityAllocator.Release(ctx, id, false)
 		if err != nil {
@@ -151,7 +168,7 @@ func (ipc *IPCache) releaseCIDRIdentities(ctx context.Context, identities map[st
 		}
 
 		if released {
-			ipc.Delete(prefix, source.Generated)
+			ipc.deleteLocked(prefix, source.Generated)
 		}
 	}
 }

commit 3f27143b048479157c24d2e8a07791ea77f5e8d1

ipcache: Fix metadata access from CIDR allocation
The locking order for metadata -> IPCache should be first grabbing the
metadata lock then the IPCache lock, according to the documentation in
the metadata structure. Correct this lock ordering to conform to the
documented pattern.

This sort of improper lock ordering could theoretically cause a deadlock
if for instance the label injector ran at the same time as this function
is called.

Found by inspection.

Fixes: 40e13ea2a5a9 ("ipcache: Fix race in identity/ipcache release")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/ipcache/cidr.go
@@ -52,11 +52,12 @@ func (ipc *IPCache) AllocateCIDRs(
 	allocateCtx, cancel := context.WithTimeout(context.Background(), option.Config.IPAllocationTimeout)
 	defer cancel()
 
+	ipc.metadata.RLock()
 	ipc.Lock()
 	allocatedIdentities := make(map[netip.Prefix]*identity.Identity, len(prefixes))
 	for i, prefix := range prefixes {
 		lbls := cidr.GetCIDRLabels(prefix)
-		lbls.MergeLabels(ipc.metadata.get(prefix).ToLabels())
+		lbls.MergeLabels(ipc.metadata.getLocked(prefix).ToLabels())
 		oldNID := identity.InvalidIdentity
 		if oldNIDs != nil && len(oldNIDs) > i {
 			oldNID = oldNIDs[i]
@@ -75,6 +76,7 @@ func (ipc *IPCache) AllocateCIDRs(
 		}
 	}
 	ipc.Unlock()
+	ipc.metadata.RUnlock()
 
 	// Only upsert into ipcache if identity wasn't allocated
 	// before and the caller does not care doing this