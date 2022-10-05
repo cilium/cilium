cilium/cilium

commit d3a71e2d2c5e3c8abbba0c84047c9547d035c8ef

ip: Add MustAddrFromIP
This function is the same as AddrFromIP() but assumes the input is
valid. It doesn't return an error and instead panics if assumptions are
not true.

This is useful to have a fast-path without error handling when we are
certain the input is valid.

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ip/ip.go
@@ -944,3 +944,13 @@ func AddrFromIP(ip net.IP) (netip.Addr, bool) {
 	}
 	return addr.Unmap(), ok
 }
+
+// MustAddrFromIP is the same as AddrFromIP except that it assumes the input is
+// a valid IP address and always returns a valid netip.Addr.
+func MustAddrFromIP(ip net.IP) netip.Addr {
+	addr, ok := netip.AddrFromSlice(ip)
+	if !ok {
+		panic("addr is not a valid IP address")
+	}
+	return addr.Unmap()
+}

commit 3e0d0203a8c1e05a264251f6ac0a5bcabbde5de3

ip: Simplify MustAddrFromIP
As suggested by Timo, this function can be simplified.

Fixes: d3a71e2d2c5 ("ip: Add MustAddrFromIP")
Suggested-by: Timo Beckers <timo@isovalent.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ip/ip.go
@@ -966,9 +966,9 @@ func AddrFromIP(ip net.IP) (netip.Addr, bool) {
 // MustAddrFromIP is the same as AddrFromIP except that it assumes the input is
 // a valid IP address and always returns a valid netip.Addr.
 func MustAddrFromIP(ip net.IP) netip.Addr {
-	addr, ok := netip.AddrFromSlice(ip)
+	addr, ok := AddrFromIP(ip)
 	if !ok {
 		panic("addr is not a valid IP address")
 	}
-	return addr.Unmap()
+	return addr
 }