d02e4a1819cfde72abc43a190548f789717adadb
Tobias Klauser <tobias@cilium.io>
2022-10-06 17:51:58 +0200

    defaults, install, utils: extract no-schedule node affinity and labels into constants
    [ cherry-picked from cilium/cilium-cli repository ]
    
    This will be reused by upcoming changes which will apply the same node
    affinity and labels during connectivity tests.
    
    Also add a utility function to escape a string tino JSON patch format
    per RFC 6901 instead of manual conversion.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/install/helm.go
+++ cilium-cli/install/helm.go
@@ -257,9 +257,9 @@ func (k *K8sInstaller) generateManifests(ctx context.Context) error {
 	// Set affinity to prevent Cilium from being scheduled on nodes labeled with
 	// "cilium.io/no-schedule=true"
 	if len(k.params.NodesWithoutCilium) != 0 {
-		helmMapOpts["affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].key"] = "cilium.io/no-schedule"
-		helmMapOpts["affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].operator"] = "NotIn"
-		helmMapOpts["affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].values[0]"] = "true"
+		for k, v := range defaults.CiliumScheduleAffinity {
+			helmMapOpts[k] = v
+		}
 	}
 
 	// Store all the options passed by --config into helm extraConfig
