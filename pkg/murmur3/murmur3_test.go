baacee3bdf0f3fc7cd216a0fca173ff88583a245
Timo Beckers <timo@isovalent.com>
2022-10-10 13:17:42 +0200

    murmur3: replace reference implementation by table-driven test
    In order to be able to run the test suite with CGo disabled, the reference
    test needs to be removed.
    
    The replacement suite results in 100% test coverage on the Go implementation.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/murmur3/murmur3_test.go
+++ pkg/murmur3/murmur3_test.go
@@ -4,32 +4,32 @@
 package murmur3
 
 import (
-	"math/rand"
 	"testing"
-
-	. "gopkg.in/check.v1"
 )
 
-func Test(t *testing.T) { TestingT(t) }
+func TestMurmur3(t *testing.T) {
+	var tests = []struct {
+		seed uint32
+		h1   uint64
+		h2   uint64
+		s    string
+	}{
+		{0, 0x0000000000000000, 0x0000000000000000, ""},
+		{1234, 0x1629cce705a7069c, 0x316c1fbd953aaecd, "hello world"},
+		{500, 0x188f69f0abbd67de, 0x1b0eeb31b4c00cb6, "lorem ipsum dolor sit amet"},
+		{31, 0x24b05ffca412286a, 0x7d81ac914b62fe96, "this is a test of 31 bytes long"},
+		{0xd09, 0x5e0fd714b3169ae6, 0x2f36e811c1535dc7, "The quick brown fox jumps over the lazy dog."},
+	}
 
-type MM3Suite struct{}
-
-var _ = Suite(&MM3Suite{})
-
-// TestMurmur3_128_x64 tests against the reference implementation of
-// the murmur3 128-bit x64.
-func (s *MM3Suite) TestMurmur3_128_x64(c *C) {
-	for len := 0; len < 1000; len++ {
-		for i := 0; i < 100; i++ {
-			seed := rand.Uint32()
-			data := make([]byte, len)
-			rand.Read(data)
-			h1, h2 := Hash128(data, seed)
-			h1Ref, h2Ref := hash128Ref(data, seed)
-			if h1 != h1Ref || h2 != h2Ref {
-				c.Errorf("Hash mismatch %v %x %x!=%x || %x!=%x\n", data, seed,
-					h1, h1Ref, h2, h2Ref)
+	for _, tt := range tests {
+		t.Run(tt.s, func(t *testing.T) {
+			h1, h2 := Hash128([]byte(tt.s), tt.seed)
+			if want, got := tt.h1, h1; want != got {
+				t.Errorf("Unexpected h1:\n\twant:\t0x%x,\n\tgot:\t0x%x", want, got)
 			}
-		}
+			if want, got := tt.h2, h2; want != got {
+				t.Errorf("Unexpected h2:\n\twant:\t0x%x,\n\tgot:\t0x%x", want, got)
+			}
+		})
 	}
 }
