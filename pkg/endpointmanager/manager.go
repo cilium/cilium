cilium/cilium

commit dae07b58f9f49017f54a72b36f0f02461d843732

endpointmanager: Remove goroutine for ID release
Previously, this code did two things in a separate goroutine:
* Release the endpoint ID, a numeric identifier for the endpoint which
  is allocated in-memory by the Cilium agent
* Get the state of the endpoint to decide whether failures to deallocate
  are worthy of a log message.

Given that the endpoint ID allocation is handled in-memory, modulo any
potential locking issues, it should be possible to handle deallocation
of the endpoint ID within a reasonable period inside the daemon.

Until recently, this code would be called while holding the endpoint
lock; this would prevent the GetState() call here from grabbing the
state to determine whether to log; if this was run in the same goroutine
as the outer logic, it would have caused a deadlock.

However, now that the locking is changed (and also there is now a
SetState() call earlier in this same function which invalidates the
check), we can simplify the logging here.

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/endpointmanager/manager.go
@@ -302,20 +302,16 @@ func (mgr *EndpointManager) ReleaseID(ep *endpoint.Endpoint) error {
 	return idallocator.Release(ep.ID)
 }
 
-// unexpose removes the endpoint from the EndpointManager so that other
-// subsystems will no longer find it during future lookups. The endpoint may
-// still be in use from other subsystems if it was found prior to this call.
-func (mgr *EndpointManager) unexpose(ep *endpoint.Endpoint) chan struct{} {
-	epRemoved := make(chan struct{})
-
+// unexpose removes the endpoint from the endpointmanager, so subsequent
+// lookups will no longer find the endpoint.
+func (mgr *EndpointManager) unexpose(ep *endpoint.Endpoint) {
 	// Fetch the identifiers; this will only fail if the endpoint is
 	// already disconnected, in which case we don't need to proceed with
 	// the rest of cleaning up the endpoint.
 	identifiers, err := ep.Identifiers()
 	if err != nil {
 		// Already disconnecting
-		close(epRemoved)
-		return epRemoved
+		return
 	}
 	previousState := ep.GetState()
 
@@ -326,25 +322,19 @@ func (mgr *EndpointManager) unexpose(ep *endpoint.Endpoint) chan struct{} {
 	mgr.removeIDLocked(ep.ID)
 	mgr.RemoveIPv6Address(ep.IPv6)
 
-	go func(ep *endpoint.Endpoint, state string) {
-		err := mgr.ReleaseID(ep)
-		if err != nil {
-			// While restoring, endpoint IDs may not have been reused yet.
-			// Failure to release means that the endpoint ID was not reused
-			// yet.
-			//
-			// While endpoint is disconnecting, ID is already available in ID cache.
-			//
-			// Avoid irritating warning messages.
-			if state != endpoint.StateRestoring && state != endpoint.StateDisconnecting && state != endpoint.StateDisconnected {
-				log.WithError(err).WithField("state", state).Warning("Unable to release endpoint ID")
-			}
+	// We haven't yet allocated the ID for a restoring endpoint, so no
+	// need to release it.
+	if previousState != endpoint.StateRestoring {
+		if err = mgr.ReleaseID(ep); err != nil {
+			log.WithError(err).WithFields(logrus.Fields{
+				"state":               previousState,
+				logfields.ContainerID: ep.GetShortContainerID(),
+				logfields.K8sPodName:  ep.GetK8sNamespaceAndPodName(),
+			}).Warning("Unable to release endpoint ID")
 		}
+	}
 
-		close(epRemoved)
-	}(ep, previousState)
 	mgr.removeReferencesLocked(identifiers)
-	return epRemoved
 }
 
 // RemoveEndpoint stops the active handling of events by the specified endpoint,
@@ -352,15 +342,15 @@ func (mgr *EndpointManager) unexpose(ep *endpoint.Endpoint) chan struct{} {
 func (mgr *EndpointManager) RemoveEndpoint(monitor regeneration.Owner, ipam endpoint.IPReleaser, ep *endpoint.Endpoint, conf endpoint.DeleteConfig) []error {
 	defer monitor.SendNotification(monitorAPI.EndpointDeleteMessage(ep))
 
-	<-mgr.unexpose(ep)
+	mgr.unexpose(ep)
 	return ep.Delete(ipam, conf)
 }
 
 // WaitEndpointRemoved waits until all operations associated with Remove of
 // the endpoint have been completed.
-// Note: only used for unit tests
+// Note: only used for unit tests, to avoid ep.Delete()
 func (mgr *EndpointManager) WaitEndpointRemoved(ep *endpoint.Endpoint) {
-	<-mgr.unexpose(ep)
+	mgr.unexpose(ep)
 	ep.Stop()
 }
 

commit 1e37eb32aec8440ab7af272e97b7c0decd20dc63

EndpointManager: fix deadlock when releasing an endpoint
In high-churn clusters, there can be a three-party deadlock between the
EndpointManager, the PolicyRepository, and a given Endpoint. One of the
"links in the chain" is merely trying to get the container ID and
namespace+name of an Endpoint for logging. Which we already have.

So, rather than trying to lock an Endpoint to get it's identifiers
again, just use the copy we already have.

Fixes: dae07b58 (endpointmanager: Remove goroutine for ID release)
Signed-off-by: Casey Callendrello <cdc@isovalent.com>

pkg/endpointmanager/manager.go
@@ -364,8 +364,8 @@ func (mgr *EndpointManager) unexpose(ep *endpoint.Endpoint) {
 		if err = mgr.ReleaseID(ep); err != nil {
 			log.WithError(err).WithFields(logrus.Fields{
 				"state":               previousState,
-				logfields.ContainerID: ep.GetShortContainerID(),
-				logfields.K8sPodName:  ep.GetK8sNamespaceAndPodName(),
+				logfields.ContainerID: identifiers[endpointid.ContainerIdPrefix],
+				logfields.K8sPodName:  identifiers[endpointid.PodNamePrefix],
 			}).Warning("Unable to release endpoint ID")
 		}
 	}