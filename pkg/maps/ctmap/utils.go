87f1155c2dac84f42350b21387617cbe6dff83b0
Gilberto Bertin <jibi@cilium.io>
2022-10-18 11:33:58 +0200

    ctmap: refactor NAT GC logic
    [ upstream commit 183726c8da72d899ef9c205bb7f80d1a2577c099 ]
    
    After adding support to the GC for NAT entries created by the DSR logic,
    we ended up handling twice the case for orphan egress entries as
    currently when we find an orphan ingress one we derive the related
    egress one and delete it as well.
    
    Since this case is already handled by the logic that goes through
    egress NAT entries, simply remove it.
    
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- pkg/maps/ctmap/utils.go
+++ pkg/maps/ctmap/utils.go
@@ -4,42 +4,14 @@
 package ctmap
 
 import (
+	"errors"
+
+	"golang.org/x/sys/unix"
+
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/maps/nat"
 	"github.com/cilium/cilium/pkg/tuple"
 )
-
-// NOTE: the function does NOT copy addr fields, so it's not safe to
-// reuse the returned natKey.
-func oNatKeyFromReverse(k nat.NatKey, v nat.NatEntry) nat.NatKey {
-	natKey, ok := k.(*nat.NatKey4)
-	if ok { // ipv4
-		natVal := v.(*nat.NatEntry4)
-		return &nat.NatKey4{TupleKey4Global: tuple.TupleKey4Global{
-			TupleKey4: tuple.TupleKey4{
-				SourceAddr: natVal.Addr,
-				SourcePort: natVal.Port,
-				DestAddr:   natKey.SourceAddr,
-				DestPort:   natKey.SourcePort,
-				NextHeader: natKey.NextHeader,
-				Flags:      tuple.TUPLE_F_OUT,
-			}}}
-	}
-
-	{ // ipv6
-		natKey := k.(*nat.NatKey6)
-		natVal := v.(*nat.NatEntry6)
-		return &nat.NatKey6{TupleKey6Global: tuple.TupleKey6Global{
-			TupleKey6: tuple.TupleKey6{
-				SourceAddr: natVal.Addr,
-				SourcePort: natVal.Port,
-				DestAddr:   natKey.SourceAddr,
-				DestPort:   natKey.SourcePort,
-				NextHeader: natKey.NextHeader,
-				Flags:      tuple.TUPLE_F_OUT,
-			}}}
-	}
-}
 
 // NOTE: the function does NOT copy addr fields, so it's not safe to
 // reuse the returned ctKey.
@@ -159,4 +131,9 @@ func egressCTKeyFromEgressNatKey(k nat.NatKey) bpf.MapKey {
 
 		return &tuple.TupleKey6Global{TupleKey6: t}
 	}
+}
+
+func ctEntryExist(ctMap *Map, ctKey bpf.MapKey) bool {
+	_, err := ctMap.Lookup(ctKey)
+	return !errors.Is(err, unix.ENOENT)
 }
