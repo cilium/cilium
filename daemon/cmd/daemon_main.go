d210ad993d1b3d3679c0ed617093fb8a1a4f4af6
Paul Chaignon <paul@cilium.io>
2022-10-27 14:54:13 +0200

    api, daemon, node: Expose allocated node ID in agent API
    [ upstream commit 7289744f84c81114e1a0fe32f7e70354bcb40a34 ]
    
    This commit exposes the node IDs allocated by the agent in the REST API,
    alongside the associated node IP addresses. The main use case is
    troubleshooting as it may be useful to dump the current list of node IDs
    and their associated IP addresses in case of incidents.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -2032,6 +2032,9 @@ func (d *Daemon) instantiateAPI() *restapi.CiliumAPIAPI {
 	// /ip/
 	restAPI.PolicyGetIPHandler = NewGetIPHandler(d)
 
+	// /node/ids
+	restAPI.DaemonGetNodeIdsHandler = NewGetNodeIDsHandler(d.datapath.Node())
+
 	return restAPI
 }
 
--- pkg/node/manager/manager_test.go
+++ pkg/node/manager/manager_test.go
@@ -14,6 +14,7 @@ import (
 
 	"gopkg.in/check.v1"
 
+	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/checker"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/datapath/fake"
@@ -174,6 +175,10 @@ func (n *signalNodeHandler) NodeCleanNeighbors(migrateOnly bool) {
 
 func (n *signalNodeHandler) AllocateNodeID(_ net.IP) uint16 {
 	return 0
+}
+
+func (n *signalNodeHandler) DumpNodeIDs() []*models.NodeID {
+	return nil
 }
 
 func (s *managerTestSuite) SetUpSuite(c *check.C) {
