cilium/cilium

commit d4d73681026bdf0335699d6ad262397613d143e4

hubble/metrics: Replace panic in contextLabels with error log
Instead of panicing when contextLabels doesn't handle a defined label,
return the error up the stack and log it.

Signed-off-by: Chance Zibolski <chance.zibolski@gmail.com>

pkg/hubble/metrics/tcp/handler.go
@@ -41,19 +41,23 @@ func (h *tcpHandler) Status() string {
 	return h.context.Status()
 }
 
-func (h *tcpHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
+func (h *tcpHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	if (flow.GetVerdict() != flowpb.Verdict_FORWARDED && flow.GetVerdict() != flowpb.Verdict_REDIRECTED) ||
 		flow.GetL4() == nil {
-		return
+		return nil
 	}
 
 	ip := flow.GetIP()
 	tcp := flow.GetL4().GetTCP()
 	if ip == nil || tcp == nil || tcp.Flags == nil {
-		return
+		return nil
 	}
 
-	labels := append([]string{"", ip.IpVersion.String()}, h.context.GetLabelValues(flow)...)
+	labels, err := h.context.GetLabelValues(flow)
+	if err != nil {
+		return err
+	}
+	labels = append(labels, "", ip.IpVersion.String())
 
 	if tcp.Flags.FIN {
 		labels[0] = "FIN"
@@ -74,4 +78,6 @@ func (h *tcpHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
 		labels[0] = "RST"
 		h.tcpFlags.WithLabelValues(labels...).Inc()
 	}
+
+	return nil
 }

pkg/hubble/metrics/dns/handler.go
@@ -89,21 +89,24 @@ func (d *dnsHandler) Status() string {
 	return strings.Join(append(status, d.context.Status()), ",")
 }
 
-func (d *dnsHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
+func (d *dnsHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	if flow.GetL7() == nil {
-		return
+		return nil
 	}
 
 	dns := flow.GetL7().GetDns()
 	if dns == nil {
-		return
+		return nil
 	}
 
 	if d.ignoreAAAA && len(dns.Qtypes) == 1 && dns.Qtypes[0] == "AAAA" {
-		return
+		return nil
 	}
 
-	labelValues := d.context.GetLabelValues(flow)
+	labelValues, err := d.context.GetLabelValues(flow)
+	if err != nil {
+		return err
+	}
 	labels := []string{"", strings.Join(dns.Qtypes, ","), fmt.Sprintf("%d", len(dns.Ips))}
 	if d.includeQuery {
 		labels = append(labels, dns.Query)
@@ -120,19 +123,21 @@ func (d *dnsHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
 			d.responses.WithLabelValues(labels...).Inc()
 
 			if len(dns.Rrtypes) > 0 {
-				labels := []string{"", strings.Join(dns.Qtypes, ",")}
+				newLabels := []string{"", strings.Join(dns.Qtypes, ",")}
 				if d.includeQuery {
-					labels = append(labels, dns.Query)
+					newLabels = append(newLabels, dns.Query)
 				}
-				labels = append(labels, d.context.GetLabelValues(flow)...)
+				newLabels = append(newLabels, labelValues...)
 
 				for _, t := range dns.Rrtypes {
-					labels[0] = t
-					d.responseTypes.WithLabelValues(labels...).Inc()
+					newLabels[0] = t
+					d.responseTypes.WithLabelValues(newLabels...).Inc()
 				}
 			}
 		} else {
 			d.queries.WithLabelValues(labels...).Inc()
 		}
 	}
+
+	return nil
 }

pkg/hubble/metrics/drop/handler.go
@@ -43,13 +43,18 @@ func (d *dropHandler) Status() string {
 	return d.context.Status()
 }
 
-func (d *dropHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
+func (d *dropHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	if flow.GetVerdict() != flowpb.Verdict_DROPPED {
-		return
+		return nil
 	}
 
-	labels := []string{monitorAPI.DropReason(uint8(flow.GetDropReason())), v1.FlowProtocol(flow)}
-	labels = append(labels, d.context.GetLabelValues(flow)...)
+	labels, err := d.context.GetLabelValues(flow)
+	if err != nil {
+		return err
+	}
+
+	labels = append(labels, monitorAPI.DropReason(uint8(flow.GetDropReason())), v1.FlowProtocol(flow))
 
 	d.drops.WithLabelValues(labels...).Inc()
+	return nil
 }

pkg/hubble/metrics/flow/handler.go
@@ -44,7 +44,11 @@ func (h *flowHandler) Status() string {
 	return h.context.Status()
 }
 
-func (h *flowHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
+func (h *flowHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
+	labelValues, err := h.context.GetLabelValues(flow)
+	if err != nil {
+		return err
+	}
 	var typeName, subType string
 	eventType := flow.GetEventType().GetType()
 	switch eventType {
@@ -75,7 +79,8 @@ func (h *flowHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
 	}
 
 	labels := []string{v1.FlowProtocol(flow), typeName, subType, flow.GetVerdict().String()}
-	labels = append(labels, h.context.GetLabelValues(flow)...)
+	labels = append(labels, labelValues...)
 
 	h.flows.WithLabelValues(labels...).Inc()
+	return nil
 }

pkg/hubble/metrics/flows-to-world/handler.go
@@ -68,23 +68,28 @@ func (h *flowsToWorldHandler) isReservedWorld(endpoint *flowpb.Endpoint) bool {
 	return false
 }
 
-func (h *flowsToWorldHandler) ProcessFlow(_ context.Context, flow *flowpb.Flow) {
+func (h *flowsToWorldHandler) ProcessFlow(_ context.Context, flow *flowpb.Flow) error {
 	l4 := flow.GetL4()
 	if flow.GetDestination() == nil ||
 		!h.isReservedWorld(flow.GetDestination()) ||
 		flow.GetEventType() == nil ||
 		l4 == nil {
-		return
+		return nil
 	}
 	// if "any-drop" option is not set, non-policy drops are ignored.
 	if flow.GetVerdict() == flowpb.Verdict_DROPPED && !h.anyDrop && flow.GetDropReasonDesc() != flowpb.DropReason_POLICY_DENIED {
-		return
+		return nil
 	}
 	// if this is potentially a forwarded reply packet, ignore it to avoid collecting statistics about ephemeral ports
 	isReply := flow.GetIsReply() == nil || flow.GetIsReply().GetValue()
 	if flow.GetVerdict() != flowpb.Verdict_DROPPED && isReply {
-		return
+		return nil
 	}
+	labelValues, err := h.context.GetLabelValues(flow)
+	if err != nil {
+		return err
+	}
+
 	labels := []string{v1.FlowProtocol(flow), flow.GetVerdict().String()}
 
 	// if "port" option is set, add port to the label.
@@ -97,6 +102,7 @@ func (h *flowsToWorldHandler) ProcessFlow(_ context.Context, flow *flowpb.Flow) 
 		}
 		labels = append(labels, port)
 	}
-	labels = append(labels, h.context.GetLabelValues(flow)...)
+	labels = append(labels, labelValues...)
 	h.flowsToWorld.WithLabelValues(labels...).Inc()
+	return nil
 }

pkg/hubble/metrics/http/handler.go
@@ -55,16 +55,19 @@ func (h *httpHandler) Status() string {
 	return h.context.Status()
 }
 
-func (h *httpHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
+func (h *httpHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	l7 := flow.GetL7()
 	if l7 == nil {
-		return
+		return nil
 	}
 	http := l7.GetHttp()
 	if http == nil {
-		return
+		return nil
 	}
-	labelValues := h.context.GetLabelValues(flow)
+	labelValues, err := h.context.GetLabelValues(flow)
+	if err != nil {
+		return err
+	}
 	reporter := "unknown"
 	switch flow.GetTrafficDirection() {
 	case flowpb.TrafficDirection_EGRESS:
@@ -79,4 +82,5 @@ func (h *httpHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
 		h.responses.WithLabelValues(append(labelValues, status, http.Method, reporter)...).Inc()
 		h.duration.WithLabelValues(append(labelValues, http.Method, reporter)...).Observe(float64(l7.LatencyNs) / float64(time.Second))
 	}
+	return nil
 }

pkg/hubble/metrics/icmp/handler.go
@@ -42,21 +42,28 @@ func (h *icmpHandler) Status() string {
 	return h.context.Status()
 }
 
-func (h *icmpHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
+func (h *icmpHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	l4 := flow.GetL4()
 	if l4 == nil {
-		return
+		return nil
+	}
+
+	labelValues, err := h.context.GetLabelValues(flow)
+	if err != nil {
+		return err
 	}
 
 	if icmp := l4.GetICMPv4(); icmp != nil {
 		labels := []string{"IPv4", layers.CreateICMPv4TypeCode(uint8(icmp.Type), uint8(icmp.Code)).String()}
-		labels = append(labels, h.context.GetLabelValues(flow)...)
+		labels = append(labels, labelValues...)
 		h.icmp.WithLabelValues(labels...).Inc()
 	}
 
 	if icmp := l4.GetICMPv6(); icmp != nil {
 		labels := []string{"IPv4", layers.CreateICMPv6TypeCode(uint8(icmp.Type), uint8(icmp.Code)).String()}
-		labels = append(labels, h.context.GetLabelValues(flow)...)
+		labels = append(labels, labelValues...)
 		h.icmp.WithLabelValues(labels...).Inc()
 	}
+
+	return nil
 }

pkg/hubble/metrics/policy/handler.go
@@ -44,20 +44,25 @@ func (d *policyHandler) Status() string {
 	return d.context.Status()
 }
 
-func (d *policyHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) {
+func (d *policyHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	if flow.GetEventType().GetType() != monitorAPI.MessageTypePolicyVerdict {
-		return
+		return nil
 	}
 	// ignore verdict if the source is host since host is allowed to connect to any local endpoints.
 	if flow.GetSource().GetIdentity() == uint32(identity.ReservedIdentityHost) {
-		return
+		return nil
+	}
+	labelValues, err := d.context.GetLabelValues(flow)
+	if err != nil {
+		return err
 	}
 
 	direction := strings.ToLower(flow.GetTrafficDirection().String())
 	match := strings.ToLower(monitorAPI.PolicyMatchType(flow.GetPolicyMatchType()).String())
 	action := strings.ToLower(flow.Verdict.String())
 	labels := []string{direction, match, action}
-	labels = append(labels, d.context.GetLabelValues(flow)...)
+	labels = append(labels, labelValues...)
 
 	d.verdicts.WithLabelValues(labels...).Inc()
+	return nil
 }

commit 8b9d3626cfb58f2ded732da32a9ea73249584c94

hubble/metrics: Fix label ordering in Hubble TCP metrics
The code setting the flag label value assumes that it's the first label in the
slice. If context options are enabled, then it's not true, so one of the
context labels incorrectly gets the flag value, and the flag label gets
discarded.

Fixes: d4d73681026b ("hubble/metrics: Replace panic in contextLabels with error log")

Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

pkg/hubble/metrics/tcp/handler.go
@@ -23,8 +23,8 @@ func (h *tcpHandler) Init(registry *prometheus.Registry, options api.Options) er
 		return err
 	}
 	h.context = c
-	contextLabels := h.context.GetLabelNames()
-	labels := append(contextLabels, "flag", "family")
+	labels := []string{"flag", "family"}
+	labels = append(labels, h.context.GetLabelNames()...)
 
 	h.tcpFlags = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: api.DefaultPrometheusNamespace,
@@ -56,7 +56,7 @@ func (h *tcpHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	if err != nil {
 		return err
 	}
-	labels := append(contextLabels, "", ip.IpVersion.String())
+	labels := append([]string{"", ip.IpVersion.String()}, contextLabels...)
 
 	if tcp.Flags.FIN {
 		labels[0] = "FIN"