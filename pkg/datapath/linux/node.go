6c5a1e3efc853901964a952297ef885bf4fd51a6
Paul Chaignon <paul@cilium.io>
2022-11-02 15:43:09 +0100

    bpf: Remove FIB lookup for IPsec
    [ upstream commit 3a650c32f2c36871bce4de8f461a996da1824132 ]
    
    When we know the encryption interface, we can jump directly from
    bpf_host to that interface using bpf_redirect. For that to work, we
    however need to rewrite the MAC addresses. This is currently done in
    bpf_host with a FIB lookup to retrieve the MAC addresses.
    
    The performance gain we get from that redirect is however expected to be
    negligible because we already traversed the stack several times for
    IPsec and we also spent a fair amount of cycles just encrypting the
    payloads.
    
    This commit therefore removes the redirect and related FIB lookup. This
    change makes the logic for IPsec a little simpler (less error cases
    without the FIB lookup). It also makes the logic more consistent across
    setups (the FIB lookup was currently only possible on AKS & GKE).
    Finally, a later change to IPsec will break the FIB lookup on AKS
    anyway.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/datapath/linux/node.go
+++ pkg/datapath/linux/node.go
@@ -1517,7 +1517,6 @@ func (n *linuxNodeHandler) NodeConfigurationChanged(newConfig datapath.LocalNode
 			// kernels with FIB lookup helpers we do a lookup from
 			// the datapath side and ignore this value.
 			ifaceNames = append(ifaceNames, option.Config.EncryptInterface[0])
-			n.enableNeighDiscovery = true
 		}
 
 		if n.enableNeighDiscovery {
