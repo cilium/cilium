633cc5d5d478618ff02f7e6676339b6aa4410260
Vishal Choudhary <contactvishaltech@gmail.com>
2022-11-12 19:38:06 +0530

    replaced error with warning
    Signed-off-by: Vishal Choudhary <contactvishaltech@gmail.com>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -3446,8 +3446,9 @@ func (c *DaemonConfig) checkMapSizeLimits() error {
 			c.PolicyMapEntries, PolicyMapMin)
 	}
 	if c.PolicyMapEntries > PolicyMapMax {
-		return fmt.Errorf("specified PolicyMap max entries %d must not exceed maximum %d",
+		log.Warnf("specified PolicyMap max entries %d must not exceed maximum %d, lowering it to the maximum value",
 			c.PolicyMapEntries, PolicyMapMax)
+		c.PolicyMapEntries = PolicyMapMax
 	}
 
 	if c.FragmentsMapEntries < FragmentsMapMin {
