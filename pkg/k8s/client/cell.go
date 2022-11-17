cilium/cilium

commit 00e40bb41683b9b3462a94879a93841751197629

k8s: Drop k8s.Init and k8s.Configure
Drop the now unused k8s.Init and k8s.Configure methods, and
configuration types and methods related to them.

Drop unused call to k8s.Configure in informer benchmark.
The benchmark setup called k8s.Configure if INTEGRATION environment variable
was set, but none of the code in the benchmark made any use of it.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/k8s/client/cell.go
@@ -208,7 +208,7 @@ func (c *compositeClientset) onStart(startCtx context.Context) error {
 	c.startHeartbeat()
 
 	// Update the global K8s clients, K8s version and the capabilities.
-	if err := k8sversion.Update(c, c.Config()); err != nil {
+	if err := k8sversion.Update(c, cfg.EnableK8sAPIDiscovery); err != nil {
 		return err
 	}
 

daemon/cmd/config.go
@@ -13,7 +13,6 @@ import (
 	. "github.com/cilium/cilium/api/v1/server/restapi/daemon"
 	"github.com/cilium/cilium/pkg/api"
 	"github.com/cilium/cilium/pkg/eventqueue"
-	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
@@ -181,8 +180,8 @@ func (h *getConfig) Handle(params GetConfigParams) middleware.Responder {
 
 	status := &models.DaemonConfigurationStatus{
 		Addressing:       node.GetNodeAddressing(),
-		K8sConfiguration: k8s.GetKubeconfigPath(),
-		K8sEndpoint:      k8s.GetAPIServerURL(),
+		K8sConfiguration: d.clientset.Config().K8sKubeConfigPath,
+		K8sEndpoint:      d.clientset.Config().K8sAPIServer,
 		NodeMonitor:      d.monitorAgent.State(),
 		KvstoreConfiguration: &models.KVstoreConfiguration{
 			Type:    option.Config.KVStore,

daemon/cmd/daemon.go
@@ -53,6 +53,7 @@ import (
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/k8s"
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/watchers"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
@@ -106,6 +107,7 @@ const (
 // monitoring when a LXC starts.
 type Daemon struct {
 	ctx              context.Context
+	clientset        k8sClient.Clientset
 	buildEndpointSem *semaphore.Weighted
 	l7Proxy          *proxy.Proxy
 	svc              *service.Service
@@ -366,7 +368,10 @@ func removeOldRouterState(ipv6 bool, restoredIP net.IP) error {
 }
 
 // NewDaemon creates and returns a new Daemon with the parameters set in c.
-func NewDaemon(ctx context.Context, cleaner *daemonCleanup, epMgr *endpointmanager.EndpointManager, dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
+func NewDaemon(ctx context.Context, cleaner *daemonCleanup,
+	epMgr *endpointmanager.EndpointManager, dp datapath.Datapath,
+	clientset k8sClient.Clientset,
+) (*Daemon, *endpointRestoreState, error) {
 
 	var (
 		err           error
@@ -495,6 +500,7 @@ func NewDaemon(ctx context.Context, cleaner *daemonCleanup, epMgr *endpointmanag
 
 	d := Daemon{
 		ctx:               ctx,
+		clientset:         clientset,
 		prefixLengths:     createPrefixLengthCounter(),
 		buildEndpointSem:  semaphore.NewWeighted(int64(numWorkerThreads())),
 		compilationMutex:  new(lock.RWMutex),

daemon/cmd/daemon_main.go
@@ -1553,7 +1553,6 @@ func (d *Daemon) initKVStore() {
 // If an object still owned by Daemon is required in a module, it should be provided indirectly, e.g. via
 // a callback.
 func registerDaemonHooks(lc fx.Lifecycle, shutdowner fx.Shutdowner, cfg DaemonCellConfig, clientset k8sClient.Clientset) error {
-
 	if cfg.SkipDaemon {
 		return nil
 	}
@@ -1640,7 +1639,8 @@ func runDaemon(ctx context.Context, cleaner *daemonCleanup, shutdowner fx.Shutdo
 
 	d, restoredEndpoints, err := NewDaemon(ctx, cleaner,
 		WithDefaultEndpointManager(ctx, endpoint.CheckHealth),
-		linuxdatapath.NewDatapath(datapathConfig, iptablesManager, wgAgent))
+		linuxdatapath.NewDatapath(datapathConfig, iptablesManager, wgAgent),
+		clientset)
 	if err != nil {
 		log.Fatalf("daemon creation failed: %s", err)
 	}

daemon/cmd/daemon_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cilium/cilium/pkg/fqdn/restore"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/identity/identitymanager"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labelsfilter"
 	"github.com/cilium/cilium/pkg/lock"
@@ -134,11 +135,14 @@ func (ds *DaemonSuite) SetUpTest(c *C) {
 	ds.oldPolicyEnabled = policy.GetPolicyEnabled()
 	policy.SetPolicyEnabled(option.DefaultEnforcement)
 
+	_, clientset := k8sClient.NewFakeClientset()
+
 	ctx, cancel := context.WithCancel(context.Background())
 	ds.cancel = cancel
 	d, _, err := NewDaemon(ctx, NewDaemonCleanup(),
 		WithCustomEndpointManager(&dummyEpSyncher{}),
-		fakedatapath.NewDatapath())
+		fakedatapath.NewDatapath(),
+		clientset)
 	c.Assert(err, IsNil)
 	ds.d = d
 

pkg/k8s/client/config.go
@@ -39,19 +39,6 @@ func (cfg Config) CellFlags(flags *pflag.FlagSet) {
 	flags.Bool(option.K8sEnableAPIDiscovery, defaults.K8sEnableAPIDiscovery, "Enable discovery of Kubernetes API groups and resources with the discovery API")
 }
 
-// K8sAPIDiscoveryEnabled returns true if API discovery of API groups and
-// resources is enabled
-func (cfg Config) K8sAPIDiscoveryEnabled() bool {
-	return cfg.EnableK8sAPIDiscovery
-}
-
-// K8sLeasesFallbackDiscoveryEnabled returns true if we should fallback to direct API
-// probing when checking for support of Leases in case Discovery API fails to discover
-// required groups.
-func (cfg Config) K8sLeasesFallbackDiscoveryEnabled() bool {
-	return cfg.K8sAPIDiscoveryEnabled()
-}
-
 func (cfg Config) isEnabled() bool {
 	return cfg.K8sAPIServer != "" ||
 		cfg.K8sKubeConfigPath != "" ||

pkg/k8s/config.go
@@ -4,77 +4,9 @@
 // Package k8s abstracts all Kubernetes specific behaviour
 package k8s
 
-import (
-	"os"
-	"strings"
-)
-
-var (
-	// config is the configuration of Kubernetes related settings
-	config configuration
-)
-
-type configuration struct {
-	// APIServerURL is the URL address of the API server
-	APIServerURL string
-
-	// KubeconfigPath is the local path to the kubeconfig configuration
-	// file on the filesystem
-	KubeconfigPath string
-
-	// QPS is the QPS to pass to the kubernetes client configuration.
-	QPS float32
-
-	// Burst is the burst to pass to the kubernetes client configuration.
-	Burst int
-}
-
-// GetAPIServerURL returns the configured API server URL address
-func GetAPIServerURL() string {
-	return config.APIServerURL
-}
-
-// GetKubeconfigPath returns the configured path to the kubeconfig
-// configuration file
-func GetKubeconfigPath() string {
-	return config.KubeconfigPath
-}
-
-// GetQPS gets the QPS of the K8s configuration.
-func GetQPS() float32 {
-	return config.QPS
-}
-
-// GetBurst gets the burst limit of the K8s configuration.
-func GetBurst() int {
-	return config.Burst
-}
-
-// Configure sets the parameters of the Kubernetes package
-func Configure(apiServerURL, kubeconfigPath string, qps float32, burst int) {
-	config.APIServerURL = apiServerURL
-	config.KubeconfigPath = kubeconfigPath
-	config.QPS = qps
-	config.Burst = burst
-
-	if IsEnabled() &&
-		config.APIServerURL != "" &&
-		!strings.HasPrefix(apiServerURL, "http") {
-		config.APIServerURL = "http://" + apiServerURL // default to HTTP
-	}
-}
-
 // IsEnabled checks if Cilium is being used in tandem with Kubernetes.
 func IsEnabled() bool {
 	// If clients have been set, then consider k8s enabled regardless
 	// of config.
-	if k8sCLI.Interface != nil {
-		return true
-	}
-
-	return config.APIServerURL != "" ||
-		config.KubeconfigPath != "" ||
-		(os.Getenv("KUBERNETES_SERVICE_HOST") != "" &&
-			os.Getenv("KUBERNETES_SERVICE_PORT") != "") ||
-		os.Getenv("K8S_NODE_NAME") != ""
+	return k8sCLI.Interface != nil
 }

pkg/k8s/config/config.go
@@ -1,38 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-package config
-
-import (
-	"github.com/cilium/cilium/pkg/defaults"
-)
-
-// Configuration is the configuration interface for the k8s package
-type Configuration interface {
-	K8sAPIDiscoveryEnabled() bool
-
-	K8sLeasesFallbackDiscoveryEnabled() bool
-}
-
-// DefaultConfiguration is an implementation of Configuration with default
-// values
-type DefaultConfiguration struct{}
-
-// NewDefaultConfiguration returns an implementation of Configuration with
-// default values
-func NewDefaultConfiguration() Configuration {
-	return &DefaultConfiguration{}
-}
-
-// K8sAPIDiscoveryEnabled returns true if API discovery of API groups and
-// resources is enabled
-func (d *DefaultConfiguration) K8sAPIDiscoveryEnabled() bool {
-	return defaults.K8sEnableAPIDiscovery
-}
-
-// K8sLeasesFallbackDiscoveryEnabled returns true if we should fallback to direct API
-// probing when checking for support of Leases in case Discovery API fails to discover
-// required groups.
-func (d *DefaultConfiguration) K8sLeasesFallbackDiscoveryEnabled() bool {
-	return defaults.K8sEnableLeasesFallbackDiscovery
-}

commit 1c91f8959abfaac221dde86136d1744a64a1c06c

k8s/client: respect QPS and burst setting for clientset
When constructing a new clientset, the provided QPS and burst values are
currently not considered.

Fixes: 00e40bb41683 ("k8s: Drop k8s.Init and k8s.Configure")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/k8s/client/cell.go
@@ -306,6 +306,8 @@ func createConfig(apiServerURL, kubeCfgPath string, qps float32, burst int) (*re
 		config = &rest.Config{Host: apiServerURL, UserAgent: userAgent}
 	}
 
+	config.QPS = qps
+	config.Burst = burst
 	return config, nil
 }
 