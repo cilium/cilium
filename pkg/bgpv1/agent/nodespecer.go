77c7ce295808621c51348b1c452536df1522f521
Dylan Reimerink <dylan.reimerink@isovalent.com>
2022-11-18 14:48:02 +0100

    BGP: Made nodespecer into a cell, implemented via node local store
    This commit makes `nodespecer` into a hive cell. It deduplicates the
    logic to transform node resources based on the IPAM mode. This logic
    is already implemented in the local node store which we now use.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- pkg/bgpv1/agent/nodespecer.go
+++ pkg/bgpv1/agent/nodespecer.go
@@ -5,100 +5,108 @@ package agent
 
 import (
 	"context"
-	"fmt"
 
-	v1 "k8s.io/client-go/listers/core/v1"
-	"k8s.io/client-go/tools/cache"
-
-	v2 "github.com/cilium/cilium/pkg/k8s/client/listers/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/node/types"
+	"github.com/cilium/cilium/pkg/option"
 )
 
-// nodeSpecer abstracts the underlying mechanism to list information about the
+type nodeSpecer interface {
+	Annotations() (map[string]string, error)
+	Labels() (map[string]string, error)
+	PodCIDRs() ([]string, error)
+}
+
+type localNodeStoreSpecerParams struct {
+	cell.In
+
+	Lifecycle      hive.Lifecycle
+	Config         *option.DaemonConfig
+	LocalNodeStore node.LocalNodeStore
+	Signaler       Signaler
+	Shutdowner     hive.Shutdowner
+}
+
+func NewLocalNodeStoreSpecer(params localNodeStoreSpecerParams) (nodeSpecer, error) {
+	specer := &localNodeStoreSpecer{
+		LocalNodeStore: params.LocalNodeStore,
+		Signaler:       params.Signaler,
+		Shutdowner:     params.Shutdowner,
+	}
+	specer.ctx, specer.cancel = context.WithCancel(context.Background())
+	params.Lifecycle.Append(specer)
+	return specer, nil
+}
+
+// localNodeStoreSpecer abstracts the underlying mechanism to list information about the
 // Node resource Cilium is running on.
 //
-// The BGP Control Plane needs to obtain Node information however, it may need
-// to query the Kubernetes Node or the Cilium Node object depending on IPAM
-// configuration.
-type nodeSpecer interface {
-	PodCIDRs() ([]string, error)
-	Labels() (map[string]string, error)
-	Annotations() (map[string]string, error)
-	Run(ctx context.Context)
+// The localNodeStoreSpecer observes changes to the local node info and signals the Signaler
+// when it changes.
+type localNodeStoreSpecer struct {
+	LocalNodeStore node.LocalNodeStore
+	Signaler       Signaler
+	Shutdowner     hive.Shutdowner
+
+	ctx      context.Context
+	cancel   context.CancelFunc
+	doneChan chan struct{}
 }
 
-type kubernetesNodeSpecer struct {
-	Name         string
-	NodeLister   v1.NodeLister
-	nodeInformer cache.SharedIndexInformer
+func (s *localNodeStoreSpecer) Start(ctx hive.HookContext) error {
+	s.LocalNodeStore.Observe(s.ctx, func(_ types.Node) {
+		s.Signaler.Event(nil)
+	}, func(err error) {
+		if err != nil {
+			s.Shutdowner.Shutdown(hive.ShutdownWithError(err))
+		}
+
+		close(s.doneChan)
+	})
+
+	return nil
 }
 
-func (s *kubernetesNodeSpecer) Run(ctx context.Context) {
-	go s.nodeInformer.Run(ctx.Done())
-}
+func (s *localNodeStoreSpecer) Stop(ctx hive.HookContext) error {
+	s.cancel()
 
-func (s *kubernetesNodeSpecer) Annotations() (map[string]string, error) {
-	n, err := s.NodeLister.Get(s.Name)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get node %v: %v", s.Name, err)
+	select {
+	case <-s.doneChan:
+	case <-s.ctx.Done():
 	}
+
+	return nil
+}
+
+func (s *localNodeStoreSpecer) Annotations() (map[string]string, error) {
+	n := s.LocalNodeStore.Get()
 	return n.Annotations, nil
 }
 
-func (s *kubernetesNodeSpecer) Labels() (map[string]string, error) {
-	n, err := s.NodeLister.Get(s.Name)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get node %v: %v", s.Name, err)
-	}
+func (s *localNodeStoreSpecer) Labels() (map[string]string, error) {
+	n := s.LocalNodeStore.Get()
 	return n.Labels, nil
 }
 
-func (s *kubernetesNodeSpecer) PodCIDRs() ([]string, error) {
-	n, err := s.NodeLister.Get(s.Name)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get node %v: %v", s.Name, err)
-	}
-	if n.Spec.PodCIDRs != nil {
-		return n.Spec.PodCIDRs, nil
-	}
-	if n.Spec.PodCIDR != "" {
-		return []string{n.Spec.PodCIDR}, nil
-	}
-	return []string{}, nil
-}
+func (s *localNodeStoreSpecer) PodCIDRs() ([]string, error) {
+	n := s.LocalNodeStore.Get()
 
-type ciliumNodeSpecer struct {
-	Name         string
-	NodeLister   v2.CiliumNodeLister
-	nodeInformer cache.SharedIndexInformer
-}
-
-func (s *ciliumNodeSpecer) Run(ctx context.Context) {
-	go s.nodeInformer.Run(ctx.Done())
-}
-
-func (s *ciliumNodeSpecer) Annotations() (map[string]string, error) {
-	n, err := s.NodeLister.Get(s.Name)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get node %v: %v", s.Name, err)
+	var podCIDRs []string
+	if n.IPv4AllocCIDR != nil {
+		podCIDRs = append(podCIDRs, n.IPv4AllocCIDR.String())
+		for _, secCidr := range n.IPv4SecondaryAllocCIDRs {
+			podCIDRs = append(podCIDRs, secCidr.String())
+		}
 	}
-	return n.Annotations, nil
-}
 
-func (s *ciliumNodeSpecer) Labels() (map[string]string, error) {
-	n, err := s.NodeLister.Get(s.Name)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get node %v: %v", s.Name, err)
+	if n.IPv6AllocCIDR != nil {
+		podCIDRs = append(podCIDRs, n.IPv6AllocCIDR.String())
+		for _, secCidr := range n.IPv6SecondaryAllocCIDRs {
+			podCIDRs = append(podCIDRs, secCidr.String())
+		}
 	}
-	return n.Labels, nil
-}
 
-func (s *ciliumNodeSpecer) PodCIDRs() ([]string, error) {
-	n, err := s.NodeLister.Get(s.Name)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get node %v: %v", s.Name, err)
-	}
-	if n.Spec.IPAM.PodCIDRs != nil {
-		return n.Spec.IPAM.PodCIDRs, nil
-	}
-	return []string{}, nil
+	return podCIDRs, nil
 }
--- pkg/bgpv1/cell.go
+++ pkg/bgpv1/cell.go
@@ -13,7 +13,12 @@ var Cell = cell.Module(
 	"bgp-cp",
 	"BGP Control Plane",
 
+	// The Controller which is the entry point of the module
 	cell.Provide(agent.NewController),
+	// Signaler is used by all cells that observe resources to signal the controller to start reconciliation.
+	cell.ProvidePrivate(agent.NewSignaler),
+	// Local Node Store Specer provides the module with information about the current node.
+	cell.ProvidePrivate(agent.NewLocalNodeStoreSpecer),
 	// goBGP is currently the only supported RouterManager, if more are
 	// implemented, provide the manager via a Cell that pics implementation based on configuration.
 	cell.ProvidePrivate(gobgp.NewBGPRouterManager),
