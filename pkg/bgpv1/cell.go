ba78c97f92066d8b60d92d07196a7784062212fb
Dylan Reimerink <dylan.reimerink@isovalent.com>
2022-11-18 17:19:28 +0100

    bgp: Replace BGPPolicy indexers with Resources
    This commit replaces the indexers with resources. Using resources has
    a few advantages over the classic indexers. Namely easier sharing of
    stores across the whole of cilium and a retry mechanism.
    
    We are not yet able to profit from these advantages due to the structure
    of the remaining code. Future commits can restructure the code so we
    can do retries.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- pkg/bgpv1/cell.go
+++ pkg/bgpv1/cell.go
@@ -6,7 +6,13 @@ package bgpv1
 import (
 	"github.com/cilium/cilium/pkg/bgpv1/agent"
 	"github.com/cilium/cilium/pkg/bgpv1/gobgp"
+	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/hive/cell"
+	v2alpha1api "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/k8s/utils"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 var Cell = cell.Module(
@@ -19,7 +25,25 @@ var Cell = cell.Module(
 	cell.ProvidePrivate(agent.NewSignaler),
 	// Local Node Store Specer provides the module with information about the current node.
 	cell.ProvidePrivate(agent.NewLocalNodeStoreSpecer),
+	// BGP Peering Policy resource provides the module with a stream of events for the BGPPeeringPolicy resource.
+	cell.ProvidePrivate(newBGPPeeringPolicyResource),
 	// goBGP is currently the only supported RouterManager, if more are
 	// implemented, provide the manager via a Cell that picks implementation based on configuration.
 	cell.ProvidePrivate(gobgp.NewBGPRouterManager),
 )
+
+func newBGPPeeringPolicyResource(lc hive.Lifecycle, c client.Clientset, dc *option.DaemonConfig) resource.Resource[*v2alpha1api.CiliumBGPPeeringPolicy] {
+	// Do not create this resource if the BGP Control Plane is disabled
+	if !dc.BGPControlPlaneEnabled() {
+		return nil
+	}
+
+	if !c.IsEnabled() {
+		return nil
+	}
+
+	return resource.New[*v2alpha1api.CiliumBGPPeeringPolicy](
+		lc, utils.ListerWatcherFromTyped[*v2alpha1api.CiliumBGPPeeringPolicyList](
+			c.CiliumV2alpha1().CiliumBGPPeeringPolicies(),
+		))
+}
