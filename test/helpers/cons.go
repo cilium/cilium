307d07d385176fbc90df18937b8e0ef0e986da88
André Martins <andre@cilium.io>
2022-11-22 13:13:41 +0100

    test: Fail on router IP mismatch warnings
    [ upstream commit ee4ea1ac21a7a24c46e499168aff1c5aa9d20322 ]
    
    We try to restore the router IP both from the filesystem (first) and
    from Kubernetes objects (as a fallback). If the two IP addresses don't
    match, we emit a warning.
    
    There is no good reason for this to happen in CI so we should fail the
    test if that warning ever shows up. Doing so would have prevented the
    flake fixed by the previous commit.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- test/helpers/cons.go
+++ test/helpers/cons.go
@@ -231,6 +231,7 @@ const (
 	removeTransientRule = "Unable to process chain CILIUM_TRANSIENT_FORWARD with ip" // from https://github.com/cilium/cilium/issues/11276
 	missingIptablesWait = "Missing iptables wait arg (-w):"
 	localIDRestoreFail  = "Could not restore all CIDR identities" // from https://github.com/cilium/cilium/pull/19556
+	routerIPMismatch    = "Mismatch of router IPs found during restoration"
 
 	// ...and their exceptions.
 	lrpExists                = "local-redirect service exists for frontend"                         // cf. https://github.com/cilium/cilium/issues/16400
@@ -304,6 +305,7 @@ var badLogMessages = map[string][]string{
 	removeTransientRule: nil,
 	missingIptablesWait: nil,
 	localIDRestoreFail:  nil,
+	routerIPMismatch:    nil,
 	"DATA RACE":         nil,
 	// Exceptions for level=error should only be added as a last resort, if the
 	// error cannot be fixed in Cilium or in the test.
