7b43201b253b4b62476edd86be86aab318f9322d
Fabio Falzoi <fabio.falzoi@isovalent.com>
2022-11-24 11:59:52 +0100

    chore: fix typo in enableCNPWatcher comment
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- operator/cmd/cnp_event.go
+++ operator/cmd/cnp_event.go
@@ -35,8 +35,8 @@ func init() {
 	}
 }
 
-// enableCNPWatcher waits for the CiliumNetowrkPolicy CRD availability and then
-// garbage collects stale CiliumNetowrkPolicy status field entries.
+// enableCNPWatcher waits for the CiliumNetworkPolicy CRD availability and then
+// garbage collects stale CiliumNetworkPolicy status field entries.
 func enableCNPWatcher(clientset k8sClient.Clientset) error {
 	enableCNPStatusUpdates := kvstoreEnabled() && option.Config.K8sEventHandover && !option.Config.DisableCNPStatusUpdates
 	if enableCNPStatusUpdates {
