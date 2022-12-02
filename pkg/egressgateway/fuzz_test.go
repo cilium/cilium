7dfe7f2beeae047223ef97f2118d621d71227602
AdamKorcz <adam@adalogics.com>
2022-12-02 13:05:13 +0000

    modify getIfaceFirstIPv4Address fuzzer to call higher up the calltree
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- pkg/egressgateway/fuzz_test.go
+++ pkg/egressgateway/fuzz_test.go
@@ -6,14 +6,45 @@ package egressgateway
 import (
 	"testing"
 
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
-	fuzz "github.com/AdaLogics/go-fuzz-headers"
+	slimv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	nodeTypes "github.com/cilium/cilium/pkg/node/types"
+	"github.com/cilium/cilium/pkg/policy/api"
 )
 
-func FuzzGetIfaceFirstIPv4Address(f *testing.F) {
-	f.Fuzz(func(t *testing.T, ifaceName string) {
-		_, _, _ = getIfaceFirstIPv4Address(ifaceName)
+func FuzzRegenerateGatewayConfigs(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		ff := fuzz.NewConsumer(data)
+		nodes := make([]nodeTypes.Node, 0)
+		ff.CreateSlice(&nodes)
+		if len(nodes) == 0 {
+			t.Skip()
+		}
+		policyConfigs := make(map[policyID]*PolicyConfig)
+		ff.FuzzMap(&policyConfigs)
+		if len(policyConfigs) == 0 {
+			t.Skip()
+		}
+		for _, pc := range policyConfigs {
+			if pc.policyGwConfig == nil {
+				pc.policyGwConfig = &policyGatewayConfig{}
+				iface, err := ff.GetString()
+				if err != nil {
+					t.Skip()
+				}
+				pc.policyGwConfig.iface = iface
+			}
+			selector := &slimv1.LabelSelector{}
+			ff.GenerateStruct(selector)
+			if len(selector.MatchLabels) == 0 {
+				t.Skip()
+			}
+			pc.policyGwConfig.nodeSelector = api.NewESFromK8sLabelSelector("", selector)
+		}
+		manager := &Manager{nodes: nodes, policyConfigs: policyConfigs}
+		manager.regenerateGatewayConfigs()
 	})
 }
 
