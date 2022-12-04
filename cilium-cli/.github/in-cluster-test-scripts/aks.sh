83fefc6e70b1bce71804c26a8acb5d58fff54011
Paul Chaignon <paul@cilium.io>
2022-12-04 16:56:56 +0100

    workflows: Collect sysdump immediatly after test failures
    [ cherry-picked from cilium/cilium-cli repository ]
    
    A new --collect-sysdump-on-failure option was recently introduced in the
    cilium connectivity test command by Jibi to collect a sysdump every time
    a connectivity test case fails, immediatly after the failure. That
    ensures we get the state as it was at the time of the failure and not as
    it is only at the end of the full workflows.
    
    This flag was tested on the cilium/cilium AKS workflows and this commit
    now extends it to all workflows in cilium/cilium-cli.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- cilium-cli/.github/in-cluster-test-scripts/aks.sh
+++ cilium-cli/.github/in-cluster-test-scripts/aks.sh
@@ -16,7 +16,7 @@ sleep 10s
 [[ $(pgrep -f "cilium.*hubble.*port-forward|kubectl.*port-forward.*hubble-relay" | wc -l) == 2 ]]
 
 # Run connectivity test
-cilium connectivity test --debug --all-flows
+cilium connectivity test --debug --all-flows --collect-sysdump-on-failure
 
 # Run performance test
 cilium connectivity test --perf --perf-duration 1s
