cilium/cilium

commit 0040d667caea89049fee56659dc4773b9a254491

bpf: Add initial NAT 4->6 datapath implementation
Manual 4->6 example setup with tcpdump extract of traffic being processed
on the LB node:

  # ./cilium/cilium service list
  ID   Frontend          Service Type   Backend
  1    10.96.0.1:443     ClusterIP      1 => 192.168.178.91:6443
  2    10.96.0.10:53     ClusterIP
  3    10.96.0.10:9153   ClusterIP

  # ./cilium/cilium service update --frontend="192.168.178.91:8080" --backends="[2a03:2880:f11c:8183:face:b00c:0:25de]:80" --id 8 --k8s-external
  Creating new service with id '8'
  Added service with 1 backends

  # ./cilium/cilium service list
  ID   Frontend              Service Type   Backend
  1    10.96.0.1:443         ClusterIP      1 => 192.168.178.91:6443
  2    10.96.0.10:53         ClusterIP
  3    10.96.0.10:9153       ClusterIP
  8    192.168.178.91:8080   ExternalIPs    1 => [2a03:2880:f11c:8183:face:b00c:0:25de]:80

  # ./cilium/cilium bpf lb list
  SERVICE ADDRESS         BACKEND ADDRESS
  10.96.0.10:9153         0.0.0.0:0 (3) [ClusterIP, non-routable]
  10.96.0.1:443           0.0.0.0:0 (1) [ClusterIP, non-routable]
                          192.168.178.91:6443 (1)
  10.96.0.10:53           0.0.0.0:0 (2) [ClusterIP, non-routable]
  192.168.178.91:8080     0.0.0.0:0 (8) [ExternalIPs, 46x64]
  [192.168.178.91]:8080   [::]:0 (8) [ExternalIPs, 46x64]
                          [2a03:2880:f11c:8183:face:b00c:0:25de]:80 (8)

  # tcpdump -i enp5s0 port 80 or port 8080 -n
  [...]
  13:14:43.918327 IP 192.168.178.92.37702 > 192.168.178.91.8080: Flags [S], seq 3599416146, win 42340, options [mss 1460,sackOK,TS val 991279130 ecr 0,nop,wscale 9], length 0
  13:14:43.918354 IP6 2001:1620:665:0:1ac0:4dff:fe09:c164.37702 > 2a03:2880:f11c:8183:face:b00c:0:25de.80: Flags [S], seq 3599416146, win 42340, options [mss 1460,sackOK,TS val 991279130 ecr 0,nop,wscale 9], length 0

bpf/lib/nodeport.h
@@ -956,8 +956,10 @@ static __always_inline int rev_nodeport_lb6(struct __ctx_buff *ctx, int *ifindex
 __section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV6_NODEPORT_REVNAT)
 int tail_rev_nodeport_lb6(struct __ctx_buff *ctx)
 {
-	int ifindex = 0;
-	int ret = 0;
+	int ifindex = 0, ret = 0;
+	void *data, *data_end;
+	struct ipv6hdr *ip6;
+
 #if defined(ENABLE_HOST_FIREWALL) && defined(IS_BPF_HOST)
 	/* We only enforce the host policies if nodeport.h is included from
 	 * bpf_host.
@@ -975,12 +977,21 @@ int tail_rev_nodeport_lb6(struct __ctx_buff *ctx)
 #endif
 	ret = rev_nodeport_lb6(ctx, &ifindex);
 	if (IS_ERR(ret))
-		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP, METRIC_EGRESS);
+		goto drop;
+	if (!revalidate_data(ctx, &data, &data_end, &ip6))
+		goto drop;
+	if (is_v4_in_v6((union v6addr *)&ip6->saddr)) {
+		ret = lb6_to_lb4(ctx, ip6);
+		if (ret)
+			goto drop;
+	}
 
 	edt_set_aggregate(ctx, 0);
 	cilium_capture_out(ctx);
 
 	return ctx_redirect(ctx, ifindex, 0);
+drop:
+	return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP, METRIC_EGRESS);
 }
 
 declare_tailcall_if(__or(__and(is_defined(ENABLE_IPV4),
@@ -1723,11 +1734,16 @@ static __always_inline int nodeport_lb4(struct __ctx_buff *ctx,
 
 		if (!lb4_src_range_ok(svc, ip4->saddr))
 			return DROP_NOT_IN_SRC_RANGE;
-
-		ret = lb4_local(get_ct_map4(&tuple), ctx, l3_off, l4_off,
-				&csum_off, &key, &tuple, svc, &ct_state_new,
-				ip4->saddr, ipv4_has_l4_header(ip4),
-				skip_l3_xlate);
+		if (lb4_to_lb6_service(svc)) {
+			ret = lb4_to_lb6(ctx, ip4, l3_off);
+			if (!ret)
+				return NAT_46X64_RECIRC;
+		} else {
+			ret = lb4_local(get_ct_map4(&tuple), ctx, l3_off, l4_off,
+					&csum_off, &key, &tuple, svc, &ct_state_new,
+					ip4->saddr, ipv4_has_l4_header(ip4),
+					skip_l3_xlate);
+		}
 		if (IS_ERR(ret))
 			return ret;
 	}

bpf/bpf_host.c
@@ -478,8 +478,16 @@ handle_ipv4(struct __ctx_buff *ctx, __u32 secctx,
 		if (ctx_get_xfer(ctx) != XFER_PKT_NO_SVC &&
 		    !bpf_skip_nodeport(ctx)) {
 			ret = nodeport_lb4(ctx, secctx);
-			if (ret < 0)
+			if (ret == NAT_46X64_RECIRC) {
+				ctx_store_meta(ctx, CB_SRC_IDENTITY, secctx);
+				ep_tail_call(ctx, CILIUM_CALL_IPV6_FROM_LXC);
+				return send_drop_notify_error(ctx, secctx,
+							      DROP_MISSED_TAIL_CALL,
+							      CTX_ACT_DROP,
+							      METRIC_INGRESS);
+			} else if (ret < 0) {
 				return ret;
+			}
 		}
 		/* Verifier workaround: modified ctx access. */
 		if (!revalidate_data(ctx, &data, &data_end, &ip4))

bpf/bpf_xdp.c
@@ -121,9 +121,15 @@ int tail_lb_ipv4(struct __ctx_buff *ctx)
 
 	if (!bpf_skip_nodeport(ctx)) {
 		ret = nodeport_lb4(ctx, 0);
-		if (IS_ERR(ret))
+		if (ret == NAT_46X64_RECIRC) {
+			ep_tail_call(ctx, CILIUM_CALL_IPV6_FROM_LXC);
+			return send_drop_notify_error(ctx, 0, DROP_MISSED_TAIL_CALL,
+						      CTX_ACT_DROP,
+						      METRIC_INGRESS);
+		} else if (IS_ERR(ret)) {
 			return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
 						      METRIC_INGRESS);
+		}
 	}
 
 	return bpf_xdp_exit(ctx, ret);

bpf/lib/common.h
@@ -413,7 +413,7 @@ enum {
 #define DROP_NO_SERVICE		-158
 #define DROP_UNUSED8		-159 /* unused */
 #define DROP_NO_TUNNEL_ENDPOINT -160
-#define DROP_UNUSED9		-161 /* unused */
+#define DROP_NAT_46X64_DISABLED	-161
 #define DROP_EDT_HORIZON	-162
 #define DROP_UNKNOWN_CT		-163
 #define DROP_HOST_UNREACHABLE	-164
@@ -438,6 +438,7 @@ enum {
 #define DROP_INVALID_VNI	-183
 
 #define NAT_PUNT_TO_STACK	DROP_NAT_NOT_NEEDED
+#define NAT_46X64_RECIRC	100
 
 /* Cilium metrics reasons for forwarding packets and other stats.
  * If reason is larger than below then this is a drop reason and
@@ -641,6 +642,7 @@ enum {
 /* Service flags (lb{4,6}_service->flags2) */
 enum {
 	SVC_FLAG_LOCALREDIRECT = (1 << 0),  /* local redirect */
+	SVC_FLAG_NAT_46X64     = (1 << 1),  /* NAT-46/64 entry */
 };
 
 struct ipv6_ct_tuple {

bpf/lib/lb.h
@@ -9,6 +9,7 @@
 #include "ipv4.h"
 #include "hash.h"
 #include "ids.h"
+#include "nat_46x64.h"
 
 #ifdef ENABLE_IPV6
 struct {
@@ -750,6 +751,17 @@ lb6_update_affinity_by_netns(const struct lb6_service *svc __maybe_unused,
 #endif
 }
 
+static __always_inline int
+lb6_to_lb4(struct __ctx_buff *ctx, const struct ipv6hdr *ip6)
+{
+	__be32 src4, dst4;
+
+	build_v4_from_v6((const union v6addr *)&ip6->saddr, &src4);
+	build_v4_from_v6((const union v6addr *)&ip6->daddr, &dst4);
+
+	return ipv6_to_ipv4(ctx, src4, dst4);
+}
+
 static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
 				     int l3_off, int l4_off,
 				     struct csum_offset *csum_off,
@@ -1082,6 +1094,16 @@ bool lb4_src_range_ok(const struct lb4_service *svc __maybe_unused,
 #endif /* ENABLE_SRC_RANGE_CHECK */
 }
 
+static __always_inline bool
+lb4_to_lb6_service(const struct lb4_service *svc __maybe_unused)
+{
+#ifdef ENABLE_NAT_46X64
+	return svc->flags2 & SVC_FLAG_NAT_46X64;
+#else
+	return false;
+#endif
+}
+
 static __always_inline
 struct lb4_service *lb4_lookup_service(struct lb4_key *key,
 				       const bool scope_switch)
@@ -1093,7 +1115,7 @@ struct lb4_service *lb4_lookup_service(struct lb4_key *key,
 	svc = map_lookup_elem(&LB4_SERVICES_MAP_V2, key);
 	if (svc) {
 		if (!scope_switch || !lb4_svc_is_local_scope(svc))
-			return svc->count ? svc : NULL;
+			return svc->count || lb4_to_lb6_service(svc) ? svc : NULL;
 		key->scope = LB_LOOKUP_SCOPE_INT;
 		svc = map_lookup_elem(&LB4_SERVICES_MAP_V2, key);
 		if (svc && svc->count)
@@ -1334,6 +1356,17 @@ lb4_update_affinity_by_netns(const struct lb4_service *svc __maybe_unused,
 #if defined(ENABLE_SESSION_AFFINITY)
 	__lb4_update_affinity(svc, true, id, backend_id);
 #endif
+}
+
+static __always_inline int
+lb4_to_lb6(struct __ctx_buff *ctx, const struct iphdr *ip4, int l3_off)
+{
+	union v6addr src6, dst6;
+
+	build_v4_in_v6(&src6, ip4->saddr);
+	build_v4_in_v6(&dst6, ip4->daddr);
+
+	return ipv4_to_ipv6(ctx, l3_off, &src6, &dst6);
 }
 
 static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,

bpf/lib/nat_46x64.h
@@ -28,13 +28,19 @@ static __always_inline __maybe_unused bool is_v4_in_v6(const union v6addr *daddr
 	return ipv6_addrcmp(&dprobe, &dmasked) == 0;
 }
 
-static __always_inline __maybe_unused void build_v4_in_v6(union v6addr *daddr,
-							  __be32 v4)
+static __always_inline __maybe_unused
+void build_v4_in_v6(union v6addr *daddr, __be32 v4)
 {
 	memset(daddr, 0, sizeof(*daddr));
 	daddr->addr[10] = 0xff;
 	daddr->addr[11] = 0xff;
 	daddr->p4 = v4;
+}
+
+static __always_inline __maybe_unused
+void build_v4_from_v6(const union v6addr *v6, __be32 *daddr)
+{
+	*daddr = v6->p4;
 }
 
 static __always_inline int get_csum_offset(__u8 protocol)

commit d7e0169b75dca5784994ba77dcdfa7b55e65c373

bpf: nodeport: fix drop notification in IPv6 revNAT
If revalidate_data() fails, set the drop reason before bailing out.

Fixes: 0040d667caea ("bpf: Add initial NAT 4->6 datapath implementation")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -1147,8 +1147,11 @@ int tail_rev_nodeport_lb6(struct __ctx_buff *ctx)
 	ret = rev_nodeport_lb6(ctx, &ifindex, &ext_err);
 	if (IS_ERR(ret))
 		goto drop;
-	if (!revalidate_data(ctx, &data, &data_end, &ip6))
+	if (!revalidate_data(ctx, &data, &data_end, &ip6)) {
+		ret = DROP_INVALID;
 		goto drop;
+	}
+
 	if (is_v4_in_v6((union v6addr *)&ip6->saddr)) {
 		int ret2;
 