1a4b0c31909fb593aaf737a3f7dacccc4bb38c2e
Dylan Reimerink <dylan.reimerink@isovalent.com>
2022-12-06 19:40:46 +0100

    bgp: Added LoadBalancer Service reconciler
    This commit adds a new reconciler which manages announcements for
    loadbalancer services. This reconcilers utilizes a `DiffStore` to
    get a change set since the last time the reconciler was invoked. This
    partial synchronization is necessary to be able to handle large
    amounts of services.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- daemon/cmd/cells.go
+++ daemon/cmd/cells.go
@@ -55,7 +55,7 @@ var (
 		// daemonCell wraps the legacy daemon initialization and provides Promise[*Daemon].
 		daemonCell,
 
-		// The BGP Control Plane
+		// The BGP Control Plane which enables various BGP related interop.
 		bgpv1.Cell,
 	)
 
--- pkg/bgpv1/cell.go
+++ pkg/bgpv1/cell.go
@@ -4,15 +4,22 @@
 package bgpv1
 
 import (
+	"time"
+
 	"github.com/cilium/cilium/pkg/bgpv1/agent"
 	"github.com/cilium/cilium/pkg/bgpv1/gobgp"
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/hive/cell"
 	v2alpha1api "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/k8s/client"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/resource"
+	slim_core_v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	"github.com/cilium/cilium/pkg/k8s/utils"
+	k8sutils "github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/option"
+
+	"k8s.io/client-go/util/workqueue"
 )
 
 var Cell = cell.Module(
@@ -21,15 +28,21 @@ var Cell = cell.Module(
 
 	// The Controller which is the entry point of the module
 	cell.Provide(agent.NewController),
-	// Signaler is used by all cells that observe resources to signal the controller to start reconciliation.
-	cell.ProvidePrivate(agent.NewSignaler),
-	// Local Node Store Specer provides the module with information about the current node.
-	cell.ProvidePrivate(agent.NewLocalNodeStoreSpecer),
-	// BGP Peering Policy resource provides the module with a stream of events for the BGPPeeringPolicy resource.
-	cell.ProvidePrivate(newBGPPeeringPolicyResource),
-	// goBGP is currently the only supported RouterManager, if more are
-	// implemented, provide the manager via a Cell that picks implementation based on configuration.
-	cell.ProvidePrivate(gobgp.NewBGPRouterManager),
+	cell.ProvidePrivate(
+		// Signaler is used by all cells that observe resources to signal the controller to start reconciliation.
+		agent.NewSignaler,
+		// Local Node Store Specer provides the module with information about the current node.
+		agent.NewLocalNodeStoreSpecer,
+		// BGP Peering Policy resource provides the module with a stream of events for the BGPPeeringPolicy resource.
+		newBGPPeeringPolicyResource,
+		// goBGP is currently the only supported RouterManager, if more are
+		// implemented, provide the manager via a Cell that pics implementation based on configuration.
+		gobgp.NewBGPRouterManager,
+		// Create a slim service resource
+		newSlimServiceResource,
+		// Create a slim service DiffStore
+		gobgp.NewDiffStore[*slim_core_v1.Service],
+	),
 	// Provides the reconcilers used by the route manager to update the config
 	gobgp.ConfigReconcilers,
 )
@@ -48,4 +61,35 @@ func newBGPPeeringPolicyResource(lc hive.Lifecycle, c client.Clientset, dc *opti
 		lc, utils.ListerWatcherFromTyped[*v2alpha1api.CiliumBGPPeeringPolicyList](
 			c.CiliumV2alpha1().CiliumBGPPeeringPolicies(),
 		))
+}
+
+// Constructs a slim service resource with perpetual retries at a exponential backoff
+func newSlimServiceResource(lc hive.Lifecycle, c k8sClient.Clientset, dc *option.DaemonConfig) (resource.Resource[*slim_core_v1.Service], error) {
+	// Do not create this resource if the BGP Control Plane is disabled
+	if !dc.BGPControlPlaneEnabled() {
+		return nil, nil
+	}
+
+	if !c.IsEnabled() {
+		return nil, nil
+	}
+
+	optsModifier, err := k8sutils.GetServiceListOptionsModifier(option.Config)
+	if err != nil {
+		return nil, err
+	}
+	return resource.New[*slim_core_v1.Service](
+		lc,
+		k8sutils.ListerWatcherWithModifier(
+			k8sutils.ListerWatcherFromTyped[*slim_core_v1.ServiceList](c.Slim().CoreV1().Services("")),
+			optsModifier),
+		resource.WithErrorHandler(resource.AlwaysRetry),
+		resource.WithRateLimiter(newErrorRateLimiter),
+	), nil
+}
+
+func newErrorRateLimiter() workqueue.RateLimiter {
+	// This rate limiter will retry in the following pattern
+	// 250ms, 500ms, 1s, 2s, 4s, 8s, 16s, 32s, .... max 5m
+	return workqueue.NewItemExponentialFailureRateLimiter(250*time.Millisecond, 5*time.Minute)
 }
