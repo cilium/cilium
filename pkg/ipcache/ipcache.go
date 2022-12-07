cilium/cilium

commit c414ebe709c9c0e6847908a2678896d6f5bbe197

ipcache: Fix handling of endpoint IP events
Move the slice of ipcache listeners from Daemon into IPCache.
Factorize and encapsulate the handling of listeners into
IPCache's Upsert and Delete methods to reduce the complexity
of callers.

Modify the signature of the OnIPIdentityCacheChange method in the
IPIdentityMappingListener interface.
Specify that the given IP is always in CIDR form, with a prefix
length, even when it represents an endpoint IP.
Use stronger types in the signature to clarify that only the
identity and host IP associated with the CIDR may be modified.

Fix the Upsert and Delete to handle all cases of CIDR shadowing
by endpoint IP.
If inserting a new CIDR that is shadowed by an existing endpoint
IP, don't callback the listeners about the insert.
If inserting a new endpoint IP that is shadowing an existing
CIDR, notify the listeners of an update of the identity associated
with the CIDR, instead of an insert. If the shadowed CIDR and the
endpoint IP are associated with the same identity, don't callback.
If deleting a CIDR that was shadowed by an endpoint IP, don't
callback the listeners about the delete.
If deleting an endpoint IP that was shadowing a CIDR, notify the
listeners of an update of the identity associated with the CIDR,
instead of a delete. If the shadowed CIDR and the endpoint IP were
associated with the same identity, don't callback.

In Upsert, when updating an IP's identity, delete the old identity
from the identity-to-IP map.

Fixes: https://github.com/cilium/cilium/issues/4803
Fixes: https://github.com/cilium/cilium/issues/4798
Signed-off-by: Romain Lenglet <romain@covalent.io>

pkg/ipcache/ipcache.go
@@ -45,7 +45,7 @@ const (
 	FromKVStore Source = "kvstore"
 
 	// FromAgentLocal is the source used for identities derived during the
-	// agent bootup process. This includes identities for host IPs.
+	// agent bootup process. This includes identities for endpoint IPs.
 	FromAgentLocal Source = "agent-local"
 )
 
@@ -69,6 +69,8 @@ type IPCache struct {
 	// particular prefix lengths for the mask.
 	v4PrefixLengths map[int]int
 	v6PrefixLengths map[int]int
+
+	listeners []IPIdentityMappingListener
 }
 
 // Implementation represents a concrete datapath implementation of the IPCache
@@ -107,6 +109,13 @@ func (ipc *IPCache) RLock() {
 // RUnlock RUnlocks the IPCache's mutex.
 func (ipc *IPCache) RUnlock() {
 	ipc.mutex.RUnlock()
+}
+
+// SetListeners sets the listeners for this IPCache.
+func (ipc *IPCache) SetListeners(listeners []IPIdentityMappingListener) {
+	ipc.mutex.Lock()
+	ipc.listeners = listeners
+	ipc.mutex.Unlock()
 }
 
 func checkPrefixLengthsAgainstMap(impl Implementation, prefixes []*net.IPNet, existingPrefixes map[int]int) error {
@@ -184,46 +193,107 @@ func allowOverwrite(existing, new Source) bool {
 // Returns false if the entry is not owned by the self declared source, i.e.
 // returns false if the kubernetes layer is trying to upsert an entry now
 // managed by the kvstore layer. See allowOverwrite() for rules on ownership.
-func (ipc *IPCache) Upsert(IP string, identity Identity) bool {
+// hostIP is the location of the given IP. It is optional (may be nil) and is
+// propagated to the listeners.
+func (ipc *IPCache) Upsert(ip string, hostIP net.IP, newIdentity Identity) bool {
+	scopedLog := log.WithFields(logrus.Fields{
+		logfields.IPAddr:   ip,
+		logfields.Identity: newIdentity,
+	})
+
 	ipc.mutex.Lock()
 	defer ipc.mutex.Unlock()
 
-	if existingIdentity, found := ipc.ipToIdentityCache[IP]; found {
-		if !allowOverwrite(existingIdentity.Source, identity.Source) {
+	var cidr *net.IPNet
+	var oldIdentity *identity.NumericIdentity
+	callbackListeners := true
+
+	cachedIdentity, found := ipc.ipToIdentityCache[ip]
+	if found {
+		if !allowOverwrite(cachedIdentity.Source, newIdentity.Source) {
 			return false
 		}
 
 		// Skip update if IP is already mapped to the given identity
-		if existingIdentity == identity {
+		if cachedIdentity == newIdentity {
 			return true
 		}
+
+		oldIdentity = &cachedIdentity.ID
 	}
 
-	// An update is treated as a deletion and then an insert.
-	ipc.deleteLocked(IP)
-
-	log.WithFields(logrus.Fields{
-		logfields.IPAddr:   IP,
-		logfields.Identity: identity,
-	}).Debug("Upserting into ipcache layer")
-
-	// Update both maps.
-	ipc.ipToIdentityCache[IP] = identity
-
-	_, found := ipc.identityToIPCache[identity.ID]
-	if !found {
-		ipc.identityToIPCache[identity.ID] = map[string]struct{}{}
-	}
-	ipc.identityToIPCache[identity.ID][IP] = struct{}{}
-
-	// Add a reference for the prefix length if this is a CIDR.
-	if _, cidr, err := net.ParseCIDR(IP); err == nil {
+	// Endpoint IP identities take precedence over CIDR identities, so if the
+	// IP is a full CIDR prefix and there's an existing equivalent endpoint IP,
+	// don't notify the listeners.
+	var err error
+	if _, cidr, err = net.ParseCIDR(ip); err == nil {
+		// Add a reference for the prefix length if this is a CIDR.
 		pl, bits := cidr.Mask.Size()
 		switch bits {
 		case net.IPv6len * 8:
 			refPrefixLength(ipc.v6PrefixLengths, pl)
 		case net.IPv4len * 8:
 			refPrefixLength(ipc.v4PrefixLengths, pl)
+		}
+
+		ones, bits := cidr.Mask.Size()
+		if ones == bits {
+			if _, endpointIPFound := ipc.ipToIdentityCache[cidr.IP.String()]; endpointIPFound {
+				scopedLog.Debug("Ignoring CIDR to identity mapping as it would shadow an endpoint IP")
+				// Skip calling back the listeners, since the endpoint IP has
+				// precedence over the new CIDR.
+				callbackListeners = false
+			}
+		}
+	} else if endpointIP := net.ParseIP(ip); endpointIP != nil { // Endpoint IP.
+		// Convert the endpoint IP into an equivalent full CIDR.
+		bits := net.IPv6len * 8
+		if endpointIP.To4() != nil {
+			bits = net.IPv4len * 8
+		}
+		cidr = &net.IPNet{
+			IP:   endpointIP,
+			Mask: net.CIDRMask(bits, bits),
+		}
+
+		// Check whether the upserted endpoint IP will shadow that CIDR, and
+		// replace its mapping with the listeners if that was the case.
+		if !found {
+			if cidrIdentity, cidrFound := ipc.ipToIdentityCache[cidr.String()]; cidrFound {
+				if cidrIdentity.ID != newIdentity.ID {
+					scopedLog.Debug("New endpoint IP started shadowing existing CIDR to identity mapping")
+					oldIdentity = &cidrIdentity.ID
+				} else {
+					// The endpoint IP and the CIDR are associated with the
+					// same identity. Nothing changes for the listeners.
+					callbackListeners = false
+				}
+			}
+		}
+	} else {
+		scopedLog.Error("Attempt to upsert invalid IP into ipcache layer")
+		return false
+	}
+
+	scopedLog.Debug("Upserting IP into ipcache layer")
+
+	// Update both maps.
+	ipc.ipToIdentityCache[ip] = newIdentity
+	// Delete the old identity, if any.
+	if found {
+		delete(ipc.identityToIPCache[cachedIdentity.ID], ip)
+		if len(ipc.identityToIPCache[cachedIdentity.ID]) == 0 {
+			delete(ipc.identityToIPCache, cachedIdentity.ID)
+		}
+	}
+	if _, ok := ipc.identityToIPCache[newIdentity.ID]; !ok {
+		ipc.identityToIPCache[newIdentity.ID] = map[string]struct{}{}
+	}
+	ipc.identityToIPCache[newIdentity.ID][ip] = struct{}{}
+
+	if callbackListeners {
+		for _, listener := range ipc.listeners {
+			listener.OnIPIdentityCacheChange(Upsert, *cidr, hostIP, oldIdentity, newIdentity.ID)
 		}
 	}
 
@@ -232,28 +302,82 @@ func (ipc *IPCache) Upsert(IP string, identity Identity) bool {
 
 // deleteLocked removes removes the provided IP-to-security-identity mapping
 // from ipc with the assumption that the IPCache's mutex is held.
-func (ipc *IPCache) deleteLocked(IP string) {
-	log.WithFields(logrus.Fields{
-		logfields.IPAddr: IP,
-	}).Debug("Removing from ipcache layer")
+func (ipc *IPCache) deleteLocked(ip string) {
+	scopedLog := log.WithFields(logrus.Fields{
+		logfields.IPAddr: ip,
+	})
 
-	identity, found := ipc.ipToIdentityCache[IP]
-	if found {
-		delete(ipc.ipToIdentityCache, IP)
-		delete(ipc.identityToIPCache[identity.ID], IP)
-		if len(ipc.identityToIPCache[identity.ID]) == 0 {
-			delete(ipc.identityToIPCache, identity.ID)
+	cachedIdentity, found := ipc.ipToIdentityCache[ip]
+	if !found {
+		scopedLog.Debug("Attempt to remove non-existing IP from ipcache layer")
+		return
+	}
+
+	var cidr *net.IPNet
+	cacheModification := Delete
+	var oldIdentity *identity.NumericIdentity
+	newIdentity := cachedIdentity
+	callbackListeners := true
+
+	var err error
+	if _, cidr, err = net.ParseCIDR(ip); err == nil {
+		// Remove a reference for the prefix length if this is a CIDR.
+		pl, bits := cidr.Mask.Size()
+		switch bits {
+		case net.IPv6len * 8:
+			unrefPrefixLength(ipc.v6PrefixLengths, pl)
+		case net.IPv4len * 8:
+			unrefPrefixLength(ipc.v4PrefixLengths, pl)
 		}
 
-		// Remove a reference for the prefix length if this is a CIDR.
-		if _, cidr, err := net.ParseCIDR(IP); err == nil {
-			pl, bits := cidr.Mask.Size()
-			switch bits {
-			case net.IPv6len * 8:
-				unrefPrefixLength(ipc.v6PrefixLengths, pl)
-			case net.IPv4len * 8:
-				unrefPrefixLength(ipc.v4PrefixLengths, pl)
+		// Check whether the deleted CIDR was shadowed by an endpoint IP. In
+		// this case, skip calling back the listeners since they don't know
+		// about its mapping.
+		if _, endpointIPFound := ipc.ipToIdentityCache[cidr.IP.String()]; endpointIPFound {
+			scopedLog.Debug("Deleting CIDR shadowed by endpoint IP")
+			callbackListeners = false
+		}
+	} else if endpointIP := net.ParseIP(ip); endpointIP != nil { // Endpoint IP.
+		// Convert the endpoint IP into an equivalent full CIDR.
+		bits := net.IPv6len * 8
+		if endpointIP.To4() != nil {
+			bits = net.IPv4len * 8
+		}
+		cidr = &net.IPNet{
+			IP:   endpointIP,
+			Mask: net.CIDRMask(bits, bits),
+		}
+
+		// Check whether the deleted endpoint IP was shadowing that CIDR, and
+		// restore its mapping with the listeners if that was the case.
+		if cidrIdentity, cidrFound := ipc.ipToIdentityCache[cidr.String()]; cidrFound {
+			if cidrIdentity.ID != cachedIdentity.ID {
+				scopedLog.Debug("Removal of endpoint IP revives shadowed CIDR to identity mapping")
+				cacheModification = Upsert
+				oldIdentity = &cachedIdentity.ID
+				newIdentity = cidrIdentity
+			} else {
+				// The endpoint IP and the CIDR were associated with the same
+				// identity. Nothing changes for the listeners.
+				callbackListeners = false
 			}
+		}
+	} else {
+		scopedLog.Error("Attempt to delete invalid IP from ipcache layer")
+		return
+	}
+
+	scopedLog.Debug("Deleting IP from ipcache layer")
+
+	delete(ipc.ipToIdentityCache, ip)
+	delete(ipc.identityToIPCache[cachedIdentity.ID], ip)
+	if len(ipc.identityToIPCache[cachedIdentity.ID]) == 0 {
+		delete(ipc.identityToIPCache, cachedIdentity.ID)
+	}
+
+	if callbackListeners {
+		for _, listener := range ipc.listeners {
+			listener.OnIPIdentityCacheChange(cacheModification, *cidr, nil, oldIdentity, newIdentity.ID)
 		}
 	}
 }

daemon/daemon.go
@@ -133,9 +133,6 @@ type Daemon struct {
 	// Used to synchronize generation of daemon's BPF programs and endpoint BPF
 	// programs.
 	compilationMutex *lock.RWMutex
-
-	// ipcacheListeners lists all parties interested in IP -> ID mappings.
-	ipcacheListeners []ipcache.IPIdentityMappingListener
 }
 
 // UpdateProxyRedirect updates the redirect rules in the proxy for a particular
@@ -832,7 +829,8 @@ func (d *Daemon) init() error {
 
 		// Set up the list of IPCache listeners in the daemon, to be
 		// used by syncLXCMap().
-		d.ipcacheListeners = []ipcache.IPIdentityMappingListener{&envoy.NetworkPolicyHostsCache, d}
+		ipcache.IPIdentityCache.SetListeners([]ipcache.IPIdentityMappingListener{
+			&envoy.NetworkPolicyHostsCache, d})
 
 		// Insert local host entries to bpf maps
 		if err := d.syncLXCMap(); err != nil {
@@ -1041,38 +1039,12 @@ func (d *Daemon) syncLXCMap() error {
 				log.WithField(logfields.IPAddr, ipIDPair.IP).Debugf("Added local ip to endpoint map")
 			}
 		}
-		prefix := ipIDPair.PrefixString()
-		cachedIdentity, exists := ipcache.IPIdentityCache.LookupByIP(prefix)
-		if !exists || cachedIdentity.ID != ipIDPair.ID {
-			// Upsert will not propagate (reserved:foo->ID) mappings across the cluster,
-			// and we specifically don't want to do so.
-			// Manually push it into each IPIdentityMappingListener.
-			log.WithFields(logrus.Fields{
-				logfields.IPAddr:       ipIDPair.IP,
-				logfields.IPMask:       ipIDPair.Mask,
-				logfields.OldIdentity:  cachedIdentity.ID,
-				logfields.Identity:     ipIDPair.ID,
-				logfields.Modification: ipcache.Upsert,
-			}).Debug("Adding special identity to ipcache")
-			ipcache.IPIdentityCache.Upsert(prefix, ipcache.Identity{
-				ID:     ipIDPair.ID,
-				Source: ipcache.FromAgentLocal,
-			})
-
-			var oldIPIDPair *identity.IPIdentityPair
-			if cachedIdentity.ID != ipIDPair.ID {
-				// If an existing mapping is updated,
-				// provide the existing mapping to the
-				// listener so it can easily clean up
-				// the old mapping.
-				pair := ipIDPair
-				pair.ID = cachedIdentity.ID
-				oldIPIDPair = &pair
-			}
-			for _, listener := range d.ipcacheListeners {
-				listener.OnIPIdentityCacheChange(ipcache.Upsert, oldIPIDPair, ipIDPair)
-			}
-		}
+		// Upsert will not propagate (reserved:foo->ID) mappings across the cluster,
+		// and we specifically don't want to do so.
+		ipcache.IPIdentityCache.Upsert(ipIDPair.PrefixString(), nil, ipcache.Identity{
+			ID:     ipIDPair.ID,
+			Source: ipcache.FromAgentLocal,
+		})
 	}
 	return nil
 }
@@ -1286,7 +1258,7 @@ func NewDaemon() (*Daemon, error) {
 	// Start watcher for endpoint IP --> identity mappings in key-value store.
 	// this needs to be done *after* init() for the daemon in that function,
 	// we populate the IPCache with the host's IP(s).
-	ipcache.InitIPIdentityWatcher(d.ipcacheListeners)
+	ipcache.InitIPIdentityWatcher()
 
 	// FIXME: Make the port range configurable.
 	d.l7Proxy = proxy.StartProxySupport(10000, 20000, option.Config.RunDir,

daemon/endpoint.go
@@ -18,6 +18,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"net"
 	"os"
 	"sync"
 	"time"
@@ -849,47 +850,49 @@ func (h *putEndpointIDLabels) Handle(params PatchEndpointIDLabelsParams) middlew
 // 'oldIPIDPair' is ignored here, because in the BPF maps an update for the
 // IP->ID mapping will replace any existing contents; knowledge of the old pair
 // is not required to upsert the new pair.
-func (d *Daemon) OnIPIdentityCacheChange(modType ipcache.CacheModification, oldIPIDPair *identity.IPIdentityPair, newIPIDPair identity.IPIdentityPair) {
+func (d *Daemon) OnIPIdentityCacheChange(modType ipcache.CacheModification, cidr net.IPNet, hostIP net.IP,
+	oldID *identity.NumericIdentity, newID identity.NumericIdentity) {
+	scopedLog := log.WithFields(logrus.Fields{
+		logfields.IPAddr:       cidr,
+		logfields.Identity:     newID,
+		logfields.Modification: modType,
+	})
 
-	log.WithFields(logrus.Fields{logfields.Modification: modType,
-		logfields.IPAddr:   newIPIDPair.IP,
-		logfields.IPMask:   newIPIDPair.Mask,
-		"hostIP":           newIPIDPair.HostIP,
-		logfields.Identity: newIPIDPair.ID}).
-		Debug("daemon notified of IP-Identity cache state change")
+	scopedLog.Debug("Daemon notified of IP-Identity cache state change")
 
 	// TODO - see if we can factor this into an interface under something like
 	// pkg/datapath instead of in the daemon directly so that the code is more
 	// logically located.
 
 	// Update BPF Maps.
-	key := ipCacheBPF.NewKey(newIPIDPair.IP, newIPIDPair.Mask)
+
+	key := ipCacheBPF.NewKey(cidr.IP, cidr.Mask)
 
 	switch modType {
 	case ipcache.Upsert:
 		value := ipCacheBPF.RemoteEndpointInfo{
-			SecurityIdentity: uint32(newIPIDPair.ID),
+			SecurityIdentity: uint32(newID),
 		}
 
-		if newIPIDPair.HostIP != nil {
-			if ip4 := newIPIDPair.HostIP.To4(); ip4 != nil {
+		if hostIP != nil {
+			if ip4 := hostIP.To4(); ip4 != nil {
 				copy(value.TunnelEndpoint[:], ip4)
 			}
 		}
 		err := ipCacheBPF.IPCache.Update(&key, &value)
 		if err != nil {
-			log.WithError(err).WithFields(logrus.Fields{"key": key.String(),
+			scopedLog.WithError(err).WithFields(logrus.Fields{"key": key.String(),
 				"value": value.String()}).
 				Warning("unable to update bpf map")
 		}
 	case ipcache.Delete:
 		err := ipCacheBPF.Delete(&key)
 		if err != nil {
-			log.WithError(err).WithFields(logrus.Fields{"key": key.String()}).
+			scopedLog.WithError(err).WithFields(logrus.Fields{"key": key.String()}).
 				Warning("unable to delete from bpf map")
 		}
 	default:
-		log.WithField("modificationType", modType).Warning("cache modification type not supported")
+		scopedLog.Warning("cache modification type not supported")
 	}
 }
 

daemon/k8s_watcher.go
@@ -1543,22 +1543,12 @@ func (d *Daemon) updatePodHostIP(pod *v1.Pod) (bool, error) {
 		return true, fmt.Errorf("no/invalid PodIP: %s", pod.Status.PodIP)
 	}
 
-	selfOwned := ipcache.IPIdentityCache.Upsert(pod.Status.PodIP, ipcache.Identity{
+	selfOwned := ipcache.IPIdentityCache.Upsert(pod.Status.PodIP, hostIP, ipcache.Identity{
 		ID:     identity.ReservedIdentityCluster,
 		Source: ipcache.FromKubernetes,
 	})
 	if !selfOwned {
 		return true, fmt.Errorf("ipcache entry owned by kvstore or agent")
-	}
-
-	id := identity.IPIdentityPair{
-		IP:     podIP,
-		HostIP: hostIP,
-		ID:     identity.ReservedIdentityCluster,
-	}
-
-	for _, listener := range d.ipcacheListeners {
-		listener.OnIPIdentityCacheChange(ipcache.Upsert, nil, id)
 	}
 
 	return false, nil
@@ -1587,11 +1577,6 @@ func (d *Daemon) deletePodHostIP(pod *v1.Pod) (bool, error) {
 	}
 
 	ipcache.IPIdentityCache.Delete(pod.Status.PodIP)
-
-	idPair := identity.IPIdentityPair{IP: podIP}
-	for _, listener := range d.ipcacheListeners {
-		listener.OnIPIdentityCacheChange(ipcache.Delete, nil, idPair)
-	}
 
 	return false, nil
 }
@@ -1683,42 +1668,36 @@ func (d *Daemon) updateK8sNodeTunneling(k8sNodeOld, k8sNodeNew *v1.Node) error {
 		return nil
 	}
 
-	getIDs := func(node *node.Node, k8sNode *v1.Node) (*identity.IPIdentityPair, string, error) {
+	getIDs := func(node *node.Node, k8sNode *v1.Node) (string, net.IP, error) {
 		if node == nil {
-			return nil, "", nil
+			return "", nil, nil
 		}
 		hostIP := node.GetNodeIP(false)
 		if ip4 := hostIP.To4(); ip4 == nil {
-			return nil, "", fmt.Errorf("HostIP is not an IPv4 address %s", hostIP)
+			return "", nil, fmt.Errorf("HostIP is not an IPv4 address %s", hostIP)
 		}
 
 		ciliumIPStr := k8sNode.GetAnnotations()[annotation.CiliumHostIP]
 		ciliumIP := net.ParseIP(ciliumIPStr)
 		if ciliumIP == nil {
-			return nil, "", fmt.Errorf("no/invalid Cilium-Host IP: %s", ciliumIPStr)
+			return "", nil, fmt.Errorf("no/invalid Cilium-Host IP: %s", ciliumIPStr)
 		}
 
-		id := &identity.IPIdentityPair{
-			IP:     ciliumIP,
-			HostIP: hostIP,
-			ID:     identity.ReservedIdentityHost,
-		}
-		return id, ciliumIPStr, nil
+		return ciliumIPStr, ciliumIP, nil
 	}
 
-	identityPairNew, ciliumIPStrNew, err := getIDs(nodeNew, k8sNodeNew)
-	if err != nil || identityPairNew == nil {
+	ciliumIPStrNew, hostIPNew, err := getIDs(nodeNew, k8sNodeNew)
+	if err != nil || ciliumIPStrNew == "" || hostIPNew == nil {
 		return err
 	}
 
-	var identityPairOld *identity.IPIdentityPair
 	if k8sNodeOld != nil {
 		nodeOld := k8s.ParseNode(k8sNodeOld)
 		var (
 			err            error
 			ciliumIPStrOld string
 		)
-		identityPairOld, ciliumIPStrOld, err = getIDs(nodeOld, k8sNodeOld)
+		ciliumIPStrOld, _, err = getIDs(nodeOld, k8sNodeOld)
 		if err != nil {
 			return err
 		}
@@ -1731,16 +1710,12 @@ func (d *Daemon) updateK8sNodeTunneling(k8sNodeOld, k8sNodeNew *v1.Node) error {
 		}
 	}
 
-	selfOwned := ipcache.IPIdentityCache.Upsert(ciliumIPStrNew, ipcache.Identity{
+	selfOwned := ipcache.IPIdentityCache.Upsert(ciliumIPStrNew, hostIPNew, ipcache.Identity{
 		ID:     identity.ReservedIdentityHost,
 		Source: ipcache.FromKubernetes,
 	})
 	if !selfOwned {
 		return fmt.Errorf("ipcache entry owned by kvstore or agent")
-	}
-
-	for _, listener := range d.ipcacheListeners {
-		listener.OnIPIdentityCacheChange(ipcache.Upsert, identityPairOld, *identityPairNew)
 	}
 	return nil
 }
@@ -1785,9 +1760,5 @@ func (d *Daemon) deleteK8sNodeV1(k8sNode *v1.Node) {
 	if ciliumIP == nil {
 		logger.Warning("Unable to parse Cilium IP")
 		return
-	}
-
-	for _, listener := range d.ipcacheListeners {
-		listener.OnIPIdentityCacheChange(ipcache.Delete, nil, identity.IPIdentityPair{IP: ciliumIP})
 	}
 }

pkg/envoy/resources.go
@@ -15,6 +15,7 @@
 package envoy
 
 import (
+	"net"
 	"sort"
 
 	envoyAPI "github.com/cilium/cilium/pkg/envoy/cilium"
@@ -61,27 +62,29 @@ func (cache *NPHDSCache) OnIPIdentityCacheGC() {
 
 // OnIPIdentityCacheChange pushes modifications to the IP<->Identity mapping
 // into the Network Policy Host Discovery Service (NPHDS).
-func (cache *NPHDSCache) OnIPIdentityCacheChange(modType ipcache.CacheModification,
-	oldIPIDPair *identity.IPIdentityPair, newIPIDPair identity.IPIdentityPair) {
-
+func (cache *NPHDSCache) OnIPIdentityCacheChange(modType ipcache.CacheModification, cidr net.IPNet, hostIP net.IP,
+	oldID *identity.NumericIdentity, newID identity.NumericIdentity) {
 	// An upsert where an existing pair exists should translate into a
 	// delete (for the old Identity) followed by an upsert (for the new).
-	if oldIPIDPair != nil && modType == ipcache.Upsert {
+	if oldID != nil && modType == ipcache.Upsert {
 		// Skip update if identity is identical
-		if oldIPIDPair.ID == newIPIDPair.ID {
+		if *oldID == newID {
 			return
 		}
 
-		cache.OnIPIdentityCacheChange(ipcache.Delete, nil, *oldIPIDPair)
+		cache.OnIPIdentityCacheChange(ipcache.Delete, cidr, nil, nil, *oldID)
 	}
 
+	cidrStr := cidr.String()
+
 	scopedLog := log.WithFields(logrus.Fields{
-		logfields.IPAddr:       newIPIDPair.IP,
-		logfields.Identity:     newIPIDPair.ID,
+		logfields.IPAddr:       cidrStr,
+		logfields.Identity:     newID,
 		logfields.Modification: modType,
 	})
+
 	// Look up the current resources for the specified Identity.
-	resourceName := newIPIDPair.ID.StringID()
+	resourceName := newID.StringID()
 	msg, err := cache.Lookup(NetworkPolicyHostsTypeURL, resourceName)
 	if err != nil {
 		scopedLog.WithError(err).Warning("Can't lookup NPHDS cache")
@@ -100,11 +103,11 @@ func (cache *NPHDSCache) OnIPIdentityCacheChange(modType ipcache.CacheModificati
 			hostAddresses = make([]string, 0, len(npHost.HostAddresses)+1)
 			hostAddresses = append(hostAddresses, npHost.HostAddresses...)
 		}
-		hostAddresses = append(hostAddresses, newIPIDPair.PrefixString())
+		hostAddresses = append(hostAddresses, cidrStr)
 		sort.Strings(hostAddresses)
 
 		newNpHost := envoyAPI.NetworkPolicyHosts{
-			Policy:        uint64(newIPIDPair.ID),
+			Policy:        uint64(newID),
 			HostAddresses: hostAddresses,
 		}
 		if err := newNpHost.Validate(); err != nil {
@@ -119,7 +122,7 @@ func (cache *NPHDSCache) OnIPIdentityCacheChange(modType ipcache.CacheModificati
 			// Doesn't exist; already deleted.
 			return
 		}
-		cache.handleIPDelete(msg.(*envoyAPI.NetworkPolicyHosts), resourceName, newIPIDPair.PrefixString())
+		cache.handleIPDelete(msg.(*envoyAPI.NetworkPolicyHosts), resourceName, cidrStr)
 	}
 }
 

pkg/ipcache/ipcache_test.go
@@ -45,7 +45,7 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 	// Deletion of key that doesn't exist doesn't cause panic.
 	IPIdentityCache.Delete(endpointIP)
 
-	IPIdentityCache.Upsert(endpointIP, Identity{
+	IPIdentityCache.Upsert(endpointIP, nil, Identity{
 		ID:     identity,
 		Source: FromKVStore,
 	})
@@ -60,13 +60,13 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 	c.Assert(cachedIdentity.Source, Equals, FromKVStore)
 
 	// kubernetes source cannot update kvstore source
-	updated := IPIdentityCache.Upsert(endpointIP, Identity{
+	updated := IPIdentityCache.Upsert(endpointIP, nil, Identity{
 		ID:     identity,
 		Source: FromKubernetes,
 	})
 	c.Assert(updated, Equals, false)
 
-	IPIdentityCache.Upsert(endpointIP, Identity{
+	IPIdentityCache.Upsert(endpointIP, nil, Identity{
 		ID:     identity,
 		Source: FromKVStore,
 	})
@@ -85,13 +85,13 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 
 	c.Assert(exists, Equals, false)
 
-	IPIdentityCache.Upsert(endpointIP, Identity{
+	IPIdentityCache.Upsert(endpointIP, nil, Identity{
 		ID:     identity,
 		Source: FromKVStore,
 	})
 
 	newIdentity := identityPkg.NumericIdentity(69)
-	IPIdentityCache.Upsert(endpointIP, Identity{
+	IPIdentityCache.Upsert(endpointIP, nil, Identity{
 		ID:     newIdentity,
 		Source: FromKVStore,
 	})
@@ -118,7 +118,7 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 	identities := []identityPkg.NumericIdentity{5, 67, 29, 29, 29}
 
 	for index := range endpointIPs {
-		IPIdentityCache.Upsert(endpointIPs[index], Identity{
+		IPIdentityCache.Upsert(endpointIPs[index], nil, Identity{
 			ID:     identities[index],
 			Source: FromKVStore,
 		})

commit 57d23431c45c3a7aa0241ce3a72df9f996c75bec

ipcache: Warn when trying to remove non-existing entry
When removing IPcache entries (e.g., on remote-node deletion), we check
if the entry is even know to us. If it isn't, we increase an error
counter and log... a debug message.

As far as I can see, this should be a warning message instead. The
debug message was introduced in commit c414ebe7; that commit doesn't
provide an explanation for the lower severity.

This commit changes the log level to warn to raise attention to such
issues in the future.

Fixes: c414ebe7 ("ipcache: Fix handling of endpoint IP events")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/ipcache/ipcache.go
@@ -507,7 +507,7 @@ func (ipc *IPCache) deleteLocked(ip string, source source.Source) (namedPortsCha
 
 	cachedIdentity, found := ipc.ipToIdentityCache[ip]
 	if !found {
-		scopedLog.Debug("Attempt to remove non-existing IP from ipcache layer")
+		scopedLog.Warn("Attempt to remove non-existing IP from ipcache layer")
 		metrics.IPCacheErrorsTotal.WithLabelValues(
 			metricTypeDelete, metricErrorNoExist,
 		).Inc()