b31c9eb23b03aa3da23e54213b8c904754277e28
Gilberto Bertin <jibi@cilium.io>
2022-12-08 17:29:09 +0100

    WIP: debug AKS workflow
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

---
+++ test/nsenter.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -x
+
+node=${1}
+nodeName=$(kubectl get node ${node} -o template --template='{{index .metadata.labels "kubernetes.io/hostname"}}') 
+nodeSelector='"nodeSelector": { "kubernetes.io/hostname": "'${nodeName:?}'" },'
+podName=${USER}-nsenter-${node}-2
+
+kubectl run ${podName:?} --restart=Never -it --rm --image overriden --overrides '
+{
+  "spec": {
+    "hostPID": true,
+    "hostNetwork": true,
+    '"${nodeSelector?}"'
+    "tolerations": [{
+        "operator": "Exists"
+    }],
+    "containers": [
+      {
+        "name": "nsenter",
+        "image": "alexeiled/nsenter:2.34",
+        "command": [
+          "/nsenter", "--all", "--target=1", "--", "su", "-c", "/bin/journalctl --no-pager -au kubelet"
+        ],
+        "stdin": true,
+        "tty": true,
+        "securityContext": {
+          "privileged": true
+        }
+      }
+    ]
+  }
+}' --attach "$@"
