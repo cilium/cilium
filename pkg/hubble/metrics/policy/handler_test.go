5f0b0068834bab9f9156bd2d9080926b914e6d62
Raphaël Pinson <raphael@isovalent.com>
2022-12-09 08:42:42 +0100

    feat(hubble): add L7 protocol to verdict metric match label
    [ upstream commit 9144bb795eb4261bbc63b5e42f592aac0db6c4a5 ]
    
    Signed-off-by: Raphaël Pinson <raphael@isovalent.com>
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/hubble/metrics/policy/handler_test.go
+++ pkg/hubble/metrics/policy/handler_test.go
@@ -41,16 +41,23 @@ func TestPolicyHandler(t *testing.T) {
 	flow.Source = &flowpb.Endpoint{Identity: uint32(identity.ReservedIdentityHost)}
 	h.ProcessFlow(context.Background(), &flow)
 
-	// l7
+	// l7/http
 	flow.EventType = &flowpb.CiliumEventType{Type: monitorAPI.MessageTypeAccessLog}
 	flow.Verdict = flowpb.Verdict_DROPPED
+	flow.L7 = &flowpb.Layer7{
+		Record: &flowpb.Layer7_Http{Http: &flowpb.HTTP{
+			Code:     0,
+			Method:   "POST",
+			Url:      "http://myhost/some/path",
+			Protocol: "http/1.1",
+		}}}
 	h.ProcessFlow(context.Background(), &flow)
 
 	expected := strings.NewReader(`# HELP hubble_policy_verdicts_total Total number of Cilium network policy verdicts
 # TYPE hubble_policy_verdicts_total counter
 hubble_policy_verdicts_total{action="dropped",direction="egress",match="none"} 1
 hubble_policy_verdicts_total{action="redirected",direction="ingress",match="l3-l4"} 1
-hubble_policy_verdicts_total{action="dropped",direction="ingress",match="l7"} 1
+hubble_policy_verdicts_total{action="dropped",direction="ingress",match="l7/http"} 1
 `)
 	assert.NoError(t, testutil.CollectAndCompare(h.verdicts, expected))
 }
