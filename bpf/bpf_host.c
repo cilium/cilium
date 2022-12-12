11534057273a2a6622e4ac5b22a2f2b965f8b7c6
Julian Wiedmann <jwi@isovalent.com>
2022-12-12 09:46:52 +0200

    bpf: add drop notification for missed L7 LB tailcall in to-netdev
    [ upstream commit 49db46744e981f73cc690b3768c2915373b2a930 ]
    
    cil_to_netdev() is the upper-most function in the program. So don't just
    return a raw DROP reason to the kernel, but translate it to CTX_ACT_DROP
    and raise a drop notification.
    
    Fixes: d1d8e7a35b35 ("datapath: Add support for re-entering LXC egress path after L7 LB")
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -1027,7 +1027,8 @@ int to_netdev(struct __ctx_buff *ctx __maybe_unused)
 
 			ctx->mark = 0;
 			tail_call_dynamic(ctx, &POLICY_EGRESSCALL_MAP, lxc_id);
-			return DROP_MISSED_TAIL_CALL;
+			return send_drop_notify_error(ctx, 0, DROP_MISSED_TAIL_CALL,
+						      CTX_ACT_DROP, METRIC_EGRESS);
 		}
 	}
 #endif
