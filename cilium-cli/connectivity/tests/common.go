0291177fae6e2116d022cca891ce8df343ce419f
Tobias Klauser <tobias@cilium.io>
2022-12-12 14:09:21 +0100

    connectivity: convert curl/ping command construction *ConnectivityTest methods
    [ cherry-picked from cilium/cilium-cli repository ]
    
    This will allow to provide user-specified parameters (e.g. connection
    timeout) to these commands in a successive commit.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/connectivity/tests/common.go
+++ cilium-cli/connectivity/tests/common.go
@@ -3,13 +3,6 @@
 
 package tests
 
-import (
-	"fmt"
-	"net"
-
-	"github.com/cilium/cilium-cli/connectivity/check"
-)
-
 type labelsContainer interface {
 	HasLabel(key, value string) bool
 }
@@ -47,23 +40,4 @@ func hasAllLabels(labelsContainer labelsContainer, filters map[string]string) bo
 		}
 	}
 	return true
-}
-
-func curl(peer check.TestPeer, opts ...string) []string {
-	cmd := []string{"curl",
-		"-w", "%{local_ip}:%{local_port} -> %{remote_ip}:%{remote_port} = %{response_code}",
-		"--silent", "--fail", "--show-error",
-		"--connect-timeout", "5",
-		"--output", "/dev/null",
-	}
-	cmd = append(cmd, opts...)
-	cmd = append(cmd, fmt.Sprintf("%s://%s%s",
-		peer.Scheme(),
-		net.JoinHostPort(peer.Address(), fmt.Sprint(peer.Port())),
-		peer.Path()))
-	return cmd
-}
-
-func ping(peer check.TestPeer) []string {
-	return []string{"ping", "-w", "3", "-c", "1", peer.Address()}
 }
