cilium/cilium

commit 322510d4d9f48f5368e2e84a0a7ea64168124a3a

bpf: Add missing packet tracing for handle_nat_fwd
In to-netdev in bpf_host.c, the packet tracing is missing when the
handle_nat_fwd uses tailcall. This PR adds new packet tracing inside the
handle_nat_fwd, but there were two complicated issue in here.

1. handle_nat_fwd is called from to-netdev as well as to-overlay

There are two possible caller for this function, so we needed to use
IS_BPF_OVERLAY macro to make sure where it is called.

2. handle_nat_fwd does not always tailcalls

Internally, it uses conditional tailcall. When it returns without
tailcall, we will hit the packet tracing hook at the bottom of the
to_netdev and have duplicated packet trace. We need to make sure whether
we already trace the packet or not and skip it.

Below is an example of simple ICMP Echo to outside of Pod network

Before the fix (with tailcall)
```
<- endpoint ... 10.0.1.185 -> 1.1.1.1 EchoRequest
-> stack ... 10.0.1.185 -> 1.1.1.1 EchoRequest
["-> network" is missing here]
<- network ... 1.1.1.1 -> 172.18.0.3 EchoReply
<- host ... 1.1.1.1 -> 10.0.1.185 EchoReply
-> endpoint ... 1.1.1.1 -> 10.0.1.185 EchoReply
```

Before the fix (without tailcall)
```
<- endpoint ... 10.0.1.185 -> 1.1.1.1 EchoRequest
-> stack ... 10.0.1.185 -> 1.1.1.1 EchoRequest
-> network ... 172.18.0.3 -> 1.1.1.1 EchoRequest
<- network ... 1.1.1.1 -> 172.18.0.3 EchoReply
<- host ... 1.1.1.1 -> 10.0.1.185 EchoReply
-> endpoint ... 1.1.1.1 -> 10.0.1.185 EchoReply
[no problem]
```

After the fix (with tailcall)
```
<- endpoint ... 10.0.1.185 -> 1.1.1.1 EchoRequest
-> stack ... 10.0.1.185 -> 1.1.1.1 EchoRequest
-> network ... 172.18.0.3 -> 1.1.1.1 EchoRequest
<- network ... 1.1.1.1 -> 172.18.0.3 EchoReply
<- host ... 1.1.1.1 -> 10.0.1.185 EchoReply
-> endpoint ... 1.1.1.1 -> 10.0.1.185 EchoReply
[no problem]
```

After the fix (without tailcall)
```
<- endpoint ... 10.0.1.185 -> 1.1.1.1 EchoRequest
-> stack ... 10.0.1.185 -> 1.1.1.1 EchoRequest
-> network ... 172.18.0.3 -> 1.1.1.1 EchoRequest

bpf/bpf_host.c
@@ -1067,6 +1067,7 @@ int to_netdev(struct __ctx_buff *ctx __maybe_unused)
 	__u16 __maybe_unused proto = 0;
 	__u32 __maybe_unused vlan_id;
 	int ret = CTX_ACT_OK;
+	bool traced = false;
 
 	/* Filter allowed vlan id's and pass them back to kernel.
 	 */
@@ -1155,6 +1156,14 @@ out:
 			return send_drop_notify_error(ctx, 0, ret,
 						      CTX_ACT_DROP,
 						      METRIC_EGRESS);
+
+		/*
+		 * Depending on the condition, handle_nat_fwd may return
+		 * without tail calling. Since we have packet tracing inside
+		 * the handle_nat_fwd, we need to avoid tracing the packet
+		 * twice.
+		 */
+		traced = true;
 	}
 #endif
 #ifdef ENABLE_HEALTH_CHECK
@@ -1163,8 +1172,9 @@ out:
 		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
 					      METRIC_EGRESS);
 #endif
-	send_trace_notify(ctx, TRACE_TO_NETWORK, 0, 0, 0,
-			  0, trace.reason, trace.monitor);
+	if (!traced)
+		send_trace_notify(ctx, TRACE_TO_NETWORK, 0, 0, 0,
+				  0, trace.reason, trace.monitor);
 
 	return ret;
 }

bpf/lib/nodeport.h
@@ -1047,13 +1047,23 @@ declare_tailcall_if(__or(__and(is_defined(ENABLE_IPV4),
 		    CILIUM_CALL_IPV6_ENCAP_NODEPORT_NAT)
 int tail_handle_nat_fwd_ipv6(struct __ctx_buff *ctx)
 {
+	int ret;
+	enum trace_point obs_point;
 #if defined(TUNNEL_MODE) && defined(IS_BPF_OVERLAY)
 	union v6addr addr = { .p1 = 0 };
 	BPF_V6(addr, ROUTER_IP);
+	obs_point = TRACE_TO_OVERLAY;
 #else
 	union v6addr addr = IPV6_DIRECT_ROUTING;
+	obs_point = TRACE_TO_NETWORK;
 #endif
-	return nodeport_nat_ipv6_fwd(ctx, &addr);
+	ret = nodeport_nat_ipv6_fwd(ctx, &addr);
+	if (IS_ERR(ret))
+		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP, METRIC_EGRESS);
+
+	send_trace_notify(ctx, obs_point, 0, 0, 0, 0, TRACE_REASON_UNKNOWN, 0);
+
+	return ret;
 }
 #endif /* ENABLE_IPV6 */
 
@@ -2132,7 +2142,22 @@ declare_tailcall_if(__or3(__and(is_defined(ENABLE_IPV4),
 		    CILIUM_CALL_IPV4_ENCAP_NODEPORT_NAT)
 int tail_handle_nat_fwd_ipv4(struct __ctx_buff *ctx)
 {
-	return nodeport_nat_ipv4_fwd(ctx);
+	int ret;
+	enum trace_point obs_point;
+
+#if defined(TUNNEL_MODE) && defined(IS_BPF_OVERLAY)
+	obs_point = TRACE_TO_OVERLAY;
+#else
+	obs_point = TRACE_TO_NETWORK;
+#endif
+
+	ret = nodeport_nat_ipv4_fwd(ctx);
+	if (IS_ERR(ret))
+		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP, METRIC_EGRESS);
+
+	send_trace_notify(ctx, obs_point, 0, 0, 0, 0, TRACE_REASON_UNKNOWN, 0);
+
+	return ret;
 }
 #endif /* ENABLE_IPV4 */
 

commit eb581b05510758bc91b8ed9ac5f5617e3d6b0ac6

bpf: nodeport: fix tracing for handle_nat_fwd()
We currently attempt to suppress tracing when handle_nat_fwd() returns,
assuming that the inner function (ie. handle_nat_fwd_ipv*()) was inlined
and already raised a trace message.

But there's a number of cases where this falls short:
1. handle_nat_fwd() can return without even going down the IPv4 / IPv6
   paths,
2. handle_nat_fwd() can return with DROP_MISSED_TAIL_CALL,
3. the inner function *is* inlined, encounters an error and sends a drop
   notification. It returns CTX_ACT_DROP to cil_to_netdev(), which sends
   a second drop notification.

So just clean up things for good by wiring up an alternative function
for the inline config. Don't write trace messages from that function.

Note that the tracing in to-overlay still looks completely confused, but
that's a separate issue.

Fixes: 322510d4d9f4 ("bpf: Add missing packet tracing for handle_nat_fwd")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/bpf_host.c
@@ -1105,7 +1105,6 @@ int cil_to_netdev(struct __ctx_buff *ctx __maybe_unused)
 	__u16 __maybe_unused proto = 0;
 	__u32 __maybe_unused vlan_id;
 	int ret = CTX_ACT_OK;
-	bool traced = false;
 
 	/* Filter allowed vlan id's and pass them back to kernel.
 	 */
@@ -1201,14 +1200,6 @@ out:
 			return send_drop_notify_error(ctx, 0, ret,
 						      CTX_ACT_DROP,
 						      METRIC_EGRESS);
-
-		/*
-		 * Depending on the condition, handle_nat_fwd may return
-		 * without tail calling. Since we have packet tracing inside
-		 * the handle_nat_fwd, we need to avoid tracing the packet
-		 * twice.
-		 */
-		traced = true;
 	}
 #endif
 #ifdef ENABLE_HEALTH_CHECK
@@ -1217,9 +1208,8 @@ out:
 		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
 					      METRIC_EGRESS);
 #endif
-	if (!traced)
-		send_trace_notify(ctx, TRACE_TO_NETWORK, 0, 0, 0,
-				  0, trace.reason, trace.monitor);
+	send_trace_notify(ctx, TRACE_TO_NETWORK, 0, 0, 0,
+			  0, trace.reason, trace.monitor);
 
 	return ret;
 }

bpf/lib/nodeport.h
@@ -1175,6 +1175,19 @@ drop:
 	return send_drop_notify_error_ext(ctx, 0, ret, ext_err, CTX_ACT_DROP, METRIC_EGRESS);
 }
 
+static __always_inline int handle_nat_fwd_ipv6(struct __ctx_buff *ctx)
+{
+#if defined(TUNNEL_MODE) && defined(IS_BPF_OVERLAY)
+	union v6addr addr = { .p1 = 0 };
+
+	BPF_V6(addr, ROUTER_IP);
+#else
+	union v6addr addr = IPV6_DIRECT_ROUTING;
+#endif
+
+	return nodeport_nat_ipv6_fwd(ctx, &addr);
+}
+
 declare_tailcall_if(__or(__and(is_defined(ENABLE_IPV4),
 			       is_defined(ENABLE_IPV6)),
 			 __and(is_defined(ENABLE_HOST_FIREWALL),
@@ -1184,15 +1197,14 @@ int tail_handle_nat_fwd_ipv6(struct __ctx_buff *ctx)
 {
 	int ret;
 	enum trace_point obs_point;
+
 #if defined(TUNNEL_MODE) && defined(IS_BPF_OVERLAY)
-	union v6addr addr = { .p1 = 0 };
-	BPF_V6(addr, ROUTER_IP);
 	obs_point = TRACE_TO_OVERLAY;
 #else
-	union v6addr addr = IPV6_DIRECT_ROUTING;
 	obs_point = TRACE_TO_NETWORK;
 #endif
-	ret = nodeport_nat_ipv6_fwd(ctx, &addr);
+
+	ret = handle_nat_fwd_ipv6(ctx);
 	if (IS_ERR(ret))
 		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP, METRIC_EGRESS);
 
@@ -2141,6 +2153,11 @@ int tail_rev_nodeport_lb4(struct __ctx_buff *ctx)
 	return ret;
 }
 
+static __always_inline int handle_nat_fwd_ipv4(struct __ctx_buff *ctx)
+{
+	return nodeport_nat_ipv4_fwd(ctx);
+}
+
 declare_tailcall_if(__or3(__and(is_defined(ENABLE_IPV4),
 				is_defined(ENABLE_IPV6)),
 			  __and(is_defined(ENABLE_HOST_FIREWALL),
@@ -2158,7 +2175,7 @@ int tail_handle_nat_fwd_ipv4(struct __ctx_buff *ctx)
 	obs_point = TRACE_TO_NETWORK;
 #endif
 
-	ret = nodeport_nat_ipv4_fwd(ctx);
+	ret = handle_nat_fwd_ipv4(ctx);
 	if (IS_ERR(ret))
 		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP, METRIC_EGRESS);
 
@@ -2276,7 +2293,7 @@ static __always_inline int handle_nat_fwd(struct __ctx_buff *ctx)
 					       is_defined(IS_BPF_HOST)),
 					 is_defined(ENABLE_EGRESS_GATEWAY)),
 				   CILIUM_CALL_IPV4_ENCAP_NODEPORT_NAT,
-				   tail_handle_nat_fwd_ipv4);
+				   handle_nat_fwd_ipv4);
 		break;
 #endif /* ENABLE_IPV4 */
 #ifdef ENABLE_IPV6
@@ -2286,7 +2303,7 @@ static __always_inline int handle_nat_fwd(struct __ctx_buff *ctx)
 					__and(is_defined(ENABLE_HOST_FIREWALL),
 					      is_defined(IS_BPF_HOST))),
 				   CILIUM_CALL_IPV6_ENCAP_NODEPORT_NAT,
-				   tail_handle_nat_fwd_ipv6);
+				   handle_nat_fwd_ipv6);
 		break;
 #endif /* ENABLE_IPV6 */
 	default: