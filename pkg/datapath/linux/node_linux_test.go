be144030996ee8e7696e9bd9c7bee2e6de250842
Paul Chaignon <paul@cilium.io>
2022-12-19 16:54:36 +0100

    datapath: Update BPF map with node IDs
    [ upstream commit a3041709c1b2af553032138a95b5ec35b9cd1605 ]
    
    We need to update that BPF map when we are mapping a new node IP to a
    new or existing node ID. Similarly, we need to update the map when
    removing a mapping.
    
    This commit refactors the existing code for the in-memory Golang map
    such that the Golang and BPF maps are updated at the same time. It tries
    to ensure that they both stay in sync.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/datapath/linux/node_linux_test.go
+++ pkg/datapath/linux/node_linux_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/fake"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/datapath/types"
+	"github.com/cilium/cilium/pkg/maps/nodemap"
 	"github.com/cilium/cilium/pkg/maps/tunnel"
 	"github.com/cilium/cilium/pkg/mtu"
 	"github.com/cilium/cilium/pkg/netns"
@@ -102,6 +103,10 @@ func (s *linuxPrivilegedBaseTestSuite) SetUpTest(c *check.C, addressing types.No
 
 	tunnel.TunnelMap = tunnel.NewTunnelMap("test_cilium_tunnel_map")
 	_, err = tunnel.TunnelMap.OpenOrCreate()
+	c.Assert(err, check.IsNil)
+
+	nodemap.SetNodeMap(nodemap.NewNodeMap("test_cilium_node_map"))
+	err = nodemap.NodeMap().OpenOrCreate()
 	c.Assert(err, check.IsNil)
 }
 
