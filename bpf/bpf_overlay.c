47bffa2d3398bd78948a4dc8fa248ee7bddd5aa3
Aditi Ghag <aditi@cilium.io>
2022-12-18 20:33:23 -0800

    bpf: Fix LB loopback path with ingress policy
    [ upstream commit 2c9c8c17aeebcd2e295ecd29fd99305000bc7790 ]
    
    When a service backend endpoint connects to itself via its service cluster IP
    address, the traffic is hairpin'd using a loopback IP address. We skip policy
    enforcement on egress, as a pod is allowed to connect to itself, and users don't
    have to specify additional policies to allow the hairpin'd traffic. We have a
    similar expectation on the ingress side.
    The ingress side was broken because of a regression due to which replies were
    dropped on the ingress policy enforcement path. The patch that introduced the
    regression split per packet load-balancing logic into a separate tail
    call, where (limited) LB state is stored in packet context, and restored later
    while handling rest of the packet processing including conntrack, and policy
    enforcement.
    When a service pod connects to itself via its clusterIP, post the forward
    service translation, conntrack state update is done on the conntrack entry
    with loopback address (the restored state). As a result, when a reply is
    reverse SNAT'd and DNAT'd, the policy verdict for the `CT_NEW` entry is
    denied, and the reply packet is dropped.  Prior to the regression, the
    original conntrack entry would have the updated state, and loopback flag set.
    
    Fix: When hairpin'd traffic is reverse translated, and sent for a local delivery
    in case of bpf_lxc, we should directly redirect it to the endpoint, thereby
    skipping the tail call with ingress policy enforcement altogether.
    
    Here is the packet flow -
    
    Request path: podA -> clusterIP --> svc xlate (dAddr=podA) --> SNAT using
    loopback address (saddr=loopback IP) --> conntrack entry created (loopback IP,
    backend IP) with loopback flag state
    
    Reply path: cluster IP -> podA --> svc rev-xlate (saddr=podA address) --> SNAT
    using loopback (daddr=loopback IP) --> CT_REPLY match --> local_delivery
    
    Fixes: 7575ba03cc ("datapath: Reduce from LXC complexity")
    Signed-off-by: Aditi Ghag <aditi@cilium.io>
    Signed-off-by: André Martins <andre@cilium.io>

--- bpf/bpf_overlay.c
+++ bpf/bpf_overlay.c
@@ -154,7 +154,7 @@ not_esp:
 			return hdrlen;
 
 		return ipv6_local_delivery(ctx, l3_off, *identity, ep,
-					   METRIC_INGRESS, false);
+					   METRIC_INGRESS, false, false);
 	}
 
 	/* A packet entering the node from the tunnel and not going to a local
@@ -306,7 +306,7 @@ not_esp:
 			goto to_host;
 
 		return ipv4_local_delivery(ctx, ETH_HLEN, *identity, ip4, ep,
-					   METRIC_INGRESS, false);
+					   METRIC_INGRESS, false, false);
 	}
 
 	/* A packet entering the node from the tunnel and not going to a local
