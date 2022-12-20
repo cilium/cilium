9598dcfbfad943eba7f603992454c0ff58e2852f
Daniel Borkmann <daniel@iogearbox.net>
2022-12-20 11:00:23 +0000

    bpf: Fix recirculation bug into upper stack for nat6->4
    For the case when IPv4 replies comes from the remote destination and we
    need to retransform the packet into an IPv6 one via reverse SNAT, what
    is needed is fib lookup for the target ifindex as well as figuring out
    the next hop.
    
    While the code hits rev_nodeport_lb6(), we don't have a CT entry there
    given these NAT gateway entries are not service-related. What happened
    was that it hit upper stack for recirculation given we did not enter the
    branch where we do fib lookup and push it back out.
    
    This was also why for XDP some traffic could be observed via tcpdump
    which is obviously a bug. The two snat_remap* cases in IPv4 code path
    differ in that the first case the IPv4 reply from remote comes back in,
    and given a service must have been configured, it'll find a rev_nodeport_lb6()
    CT lookup, but for the generic stateful NAT64 GW gateway this is not the
    case.
    
    However, we still need to enter the fib_lookup(): While there is no
    reverse DNAT in this case, we need to find the ifindex, and lookup the
    next hop with fallback to our BPF neighbor table if the original client
    was not seen in the regular neighbor table of the kernel.
    
    This fixes the bug, and also addresses another one at the same time, that
    is, if the original request came from a client in the same L2, we can now
    figure out the recorded neighbor address via BPF map as fallback if it was
    not in the neighbor cache.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/lib/nat_46x64.h
+++ bpf/lib/nat_46x64.h
@@ -396,4 +396,16 @@ nat64_rfc8215(struct __ctx_buff *ctx __maybe_unused,
 	return ipv6_to_ipv4(ctx, src4, dst4);
 }
 
+#define NAT46x64_MODE_XLATE	1
+#define NAT46x64_MODE_ROUTE	2
+
+static __always_inline bool nat46x64_cb_route(struct __ctx_buff *ctx)
+{
+	return ctx_load_meta(ctx, CB_NAT_46X64) == NAT46x64_MODE_ROUTE;
+}
+
+static __always_inline bool nat46x64_cb_xlate(struct __ctx_buff *ctx)
+{
+	return ctx_load_meta(ctx, CB_NAT_46X64) == NAT46x64_MODE_XLATE;
+}
 #endif /* __LIB_NAT_46X64__ */
