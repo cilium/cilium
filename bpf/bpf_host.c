cilium/cilium

commit 14232bb5959fac2f1521b18fec184df17f254370

bpf: Implement SRv6 decapsulation on ingress
For any IPv6 packet received on ingress of the native devices, we check
if the destination IP address is a known SID. If it is, we tail call
into a program to perform the actual decapsulation. That simply consist
in removing the outer IPv6 header and, if the inner header is IPv4,
changing the protocol on the skb. After that, the packet is sent ot the
stack.

Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/bpf_host.c
@@ -250,6 +250,18 @@ handle_ipv6(struct __ctx_buff *ctx, __u32 secctx, const bool from_host)
 		return CTX_ACT_OK;
 
 skip_host_firewall:
+#ifdef ENABLE_SRV6
+	if (!from_host) {
+		if (is_srv6_packet(ip6) && srv6_lookup_sid(&ip6->daddr)) {
+			/* This packet is destined to an SID so we need to decapsulate it
+			 * and forward it.
+			 */
+			ep_tail_call(ctx, CILIUM_CALL_SRV6_DECAP);
+			return DROP_MISSED_TAIL_CALL;
+		}
+	}
+#endif /* ENABLE_SRV6 */
+
 	if (from_host) {
 		/* If we are attached to cilium_host at egress, this will
 		 * rewrite the destination MAC address to the MAC of cilium_net.

bpf/lib/common.h
@@ -98,7 +98,8 @@
 #define CILIUM_CALL_IPV6_CT_INGRESS_POLICY_ONLY	31
 #define CILIUM_CALL_IPV6_CT_EGRESS		32
 #define CILIUM_CALL_SRV6_ENCAP			33
-#define CILIUM_CALL_SIZE			34
+#define CILIUM_CALL_SRV6_DECAP			34
+#define CILIUM_CALL_SIZE			35
 
 typedef __u64 mac_t;
 

bpf/lib/egress_policies.h
@@ -111,6 +111,28 @@ srv6_lookup_policy6(__u32 vrf_id, const struct in6_addr *dip)
 	return map_lookup_elem(&SRV6_POLICY_MAP6, &key);
 }
 
+static __always_inline __u32
+srv6_lookup_sid(const struct in6_addr *sid)
+{
+	__u32 *vrf_id;
+
+	vrf_id = map_lookup_elem(&SRV6_SID_MAP, sid);
+	if (vrf_id)
+		return *vrf_id;
+	return 0;
+}
+
+static __always_inline bool
+is_srv6_packet(const struct ipv6hdr *ip6)
+{
+#ifndef ENABLE_SRV6_REDUCED_ENCAP
+	if (ip6->nexthdr == NEXTHDR_ROUTING)
+		return true;
+#endif
+	return ip6->nexthdr == IPPROTO_IPIP ||
+	       ip6->nexthdr == IPPROTO_IPV6;
+}
+
 # ifndef SKIP_SRV6_HANDLING
 static __always_inline __u64 ctx_adjust_hroom_flags(void)
 {
@@ -145,6 +167,45 @@ srv6_encapsulation(struct __ctx_buff *ctx, int growth, __u16 new_payload_len,
 	if (ctx_store_bytes(ctx, ETH_HLEN + offsetof(struct ipv6hdr, daddr),
 			    sid, sizeof(struct in6_addr), 0) < 0)
 		return DROP_WRITE_ERROR;
+	return 0;
+}
+
+static __always_inline int
+srv6_decapsulation(struct __ctx_buff *ctx)
+{
+	__u16 new_proto = bpf_htons(ETH_P_IP);
+	void *data, *data_end;
+	struct ipv6hdr *ip6;
+	int shrink;
+
+	if (!revalidate_data(ctx, &data, &data_end, &ip6))
+		return DROP_INVALID;
+
+	switch (ip6->nexthdr) {
+	case IPPROTO_IPIP:
+		if (ctx_change_proto(ctx, new_proto, 0) < 0)
+			return DROP_WRITE_ERROR;
+		if (ctx_store_bytes(ctx, offsetof(struct ethhdr, h_proto),
+				    &new_proto, sizeof(new_proto), 0) < 0)
+			return DROP_WRITE_ERROR;
+		/* ctx_change_proto above shrinks the packet from IPv6 header
+		 * length to IPv4 header length. It removes that space from the
+		 * same header we will later delete.
+		 * Thus, deduce this space from the next packet shrinking.
+		 */
+		shrink = sizeof(struct iphdr);
+		break;
+	case IPPROTO_IPV6:
+		shrink = sizeof(struct ipv6hdr);
+		break;
+	default:
+		return DROP_INVALID;
+	}
+
+	/* Remove the outer IPv6 header. */
+	if (ctx_adjust_hroom(ctx, -shrink, BPF_ADJ_ROOM_MAC,
+			     ctx_adjust_hroom_flags()))
+		return DROP_INVALID;
 	return 0;
 }
 
@@ -290,6 +351,23 @@ int tail_srv6_encap(struct __ctx_buff *ctx)
 	send_trace_notify(ctx, TRACE_TO_STACK, SECLABEL, 0, 0, 0,
 			  TRACE_REASON_UNKNOWN, 0);
 	return CTX_ACT_OK;
+}
+
+__section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_SRV6_DECAP)
+int tail_srv6_decap(struct __ctx_buff *ctx)
+{
+	int ret = 0;
+
+	ret = srv6_decapsulation(ctx);
+	if (ret < 0)
+		goto error_drop;
+
+	send_trace_notify(ctx, TRACE_TO_STACK, SECLABEL, 0, 0, 0,
+			  TRACE_REASON_UNKNOWN, 0);
+	return CTX_ACT_OK;
+error_drop:
+		return send_drop_notify_error(ctx, SECLABEL, ret, CTX_ACT_DROP,
+					      METRIC_EGRESS);
 }
 # endif /* SKIP_SRV6_HANDLING */
 #endif /* ENABLE_SRV6 */

test/bpf/check-complexity.sh
@@ -44,10 +44,11 @@ function annotate_section_names {
 	    -e "s/\(section '2\/30'\)/\1 (tail_call IPV6_CT_INGRESS)/" \
 	    -e "s/\(section '2\/31'\)/\1 (tail_call IPV6_CT_INGRESS_POLICY_ONLY)/" \
 	    -e "s/\(section '2\/32'\)/\1 (tail_call IPV6_CT_EGRESS)/" \
-	    -e "s/\(section '2\/33'\)/\1 (tail_call SRV6_ENCAP)/"
+	    -e "s/\(section '2\/33'\)/\1 (tail_call SRV6_ENCAP)/" \
+	    -e "s/\(section '2\/34'\)/\1 (tail_call SRV6_DECAP)/"
 }
 
-if ! grep -q "CILIUM_CALL_SIZE.*34" "$BPFDIR/lib/common.h" ; then
+if ! grep -q "CILIUM_CALL_SIZE.*35" "$BPFDIR/lib/common.h" ; then
 	echo "This script is out of date compared to CILIUM_CALL_SIZE." 1>&2
 	exit 1
 fi

commit 496a2bd3fc5a6bd7d1efc196993bd6780e437bb8

bpf: srv6: check for SRv6 decap before passing to host stack
In configs where iptables is active and Host-Routing is unavailable, the
from-netdev path needs to apply the SRv6 decap check a bit earlier.
Otherwise all such traffic gets passed to the stack without decapsulation.

Fixes: 14232bb5959f ("bpf: Implement SRv6 decapsulation on ingress")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/bpf_host.c
@@ -241,12 +241,6 @@ handle_ipv6(struct __ctx_buff *ctx, __u32 secctx, const bool from_host,
 #endif /* ENABLE_HOST_FIREWALL */
 
 skip_host_firewall:
-#if defined(NO_REDIRECT) && !defined(ENABLE_HOST_ROUTING)
-	/* See IPv4 case for NO_REDIRECT/ENABLE_HOST_ROUTING comments */
-	if (!from_host)
-		return CTX_ACT_OK;
-#endif /* NO_REDIRECT && !ENABLE_HOST_ROUTING */
-
 #ifdef ENABLE_SRV6
 	if (!from_host) {
 		if (is_srv6_packet(ip6) && srv6_lookup_sid(&ip6->daddr)) {
@@ -258,6 +252,12 @@ skip_host_firewall:
 		}
 	}
 #endif /* ENABLE_SRV6 */
+
+#if defined(NO_REDIRECT) && !defined(ENABLE_HOST_ROUTING)
+	/* See IPv4 case for NO_REDIRECT/ENABLE_HOST_ROUTING comments */
+	if (!from_host)
+		return CTX_ACT_OK;
+#endif /* NO_REDIRECT && !ENABLE_HOST_ROUTING */
 
 	if (from_host) {
 		/* If we are attached to cilium_host at egress, this will