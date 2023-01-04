cilium/cilium

commit 41d77e168f6bd6b5045775a5c043a5d7457b4e9c

metrics: Add scope labels events_ts metrics.
To allow tracking k8s events per resource/action type,
add event scope and action labels to events_ts metrics.
Refactored all events_ts metrics into a single gauge vector.
API metrics will be labelled with the url resource path and
request type.
Containerd metrics removed due to not being used.
Also added test to ensure that cache with no controller doesn't wait.

Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

pkg/metrics/metrics.go
@@ -150,8 +150,12 @@ const (
 	// started by cilium (Envoy, monitor, etc..)
 	LabelSubsystem = "subsystem"
 
-	// LabelKind is the kind a label
+	// LabelKind is the kind of a label
 	LabelKind = "kind"
+
+	// LabelEventSource is the source of a label for event metrics
+	// i.e. k8s, containerd, api.
+	LabelEventSource = "source"
 
 	// LabelPath is the label for the API path
 	LabelPath = "path"
@@ -285,17 +289,11 @@ var (
 	// event that we will handle
 	// source is one of k8s, docker or apia
 
-	// EventTSK8s is the timestamp of k8s events
-	EventTSK8s = NoOpGauge
+	// EventTS is the timestamp of k8s resource events.
+	EventTS = NoOpGaugeVec
 
 	// EventLagK8s is the lag calculation for k8s Pod events.
 	EventLagK8s = NoOpGauge
-
-	// EventTSContainerd is the timestamp of docker events
-	EventTSContainerd = NoOpGauge
-
-	// EventTSAPI is the timestamp of docker events
-	EventTSAPI = NoOpGauge
 
 	// L7 statistics
 
@@ -523,7 +521,7 @@ type Configuration struct {
 	PolicyEndpointStatusEnabled             bool
 	PolicyImplementationDelayEnabled        bool
 	IdentityCountEnabled                    bool
-	EventTSK8sEnabled                       bool
+	EventTSEnabled                          bool
 	EventLagK8sEnabled                      bool
 	EventTSContainerdEnabled                bool
 	EventTSAPIEnabled                       bool
@@ -552,6 +550,7 @@ type Configuration struct {
 	SubprocessStartEnabled                  bool
 	KubernetesEventProcessedEnabled         bool
 	KubernetesEventReceivedEnabled          bool
+	KubernetesTimeBetweenEventsEnabled      bool
 	KubernetesAPIInteractionsEnabled        bool
 	KubernetesAPICallsEnabled               bool
 	KubernetesCNPStatusCompletionEnabled    bool
@@ -780,15 +779,14 @@ func CreateConfiguration(metricsEnabled []string) (Configuration, []prometheus.C
 			c.IdentityCountEnabled = true
 
 		case Namespace + "_event_ts":
-			EventTSK8s = prometheus.NewGauge(prometheus.GaugeOpts{
-				Namespace:   Namespace,
-				Name:        "event_ts",
-				Help:        "Last timestamp when we received an event",
-				ConstLabels: prometheus.Labels{"source": LabelEventSourceK8s},
-			})
+			EventTS = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+				Namespace: Namespace,
+				Name:      "event_ts",
+				Help:      "Last timestamp when we received an event",
+			}, []string{LabelEventSource, LabelScope, LabelAction})
 
-			collectors = append(collectors, EventTSK8s)
-			c.EventTSK8sEnabled = true
+			collectors = append(collectors, EventTS)
+			c.EventTSEnabled = true
 
 			EventLagK8s = prometheus.NewGauge(prometheus.GaugeOpts{
 				Namespace:   Namespace,
@@ -799,26 +797,6 @@ func CreateConfiguration(metricsEnabled []string) (Configuration, []prometheus.C
 
 			collectors = append(collectors, EventLagK8s)
 			c.EventLagK8sEnabled = true
-
-			EventTSContainerd = prometheus.NewGauge(prometheus.GaugeOpts{
-				Namespace:   Namespace,
-				Name:        "event_ts",
-				Help:        "Last timestamp when we received an event",
-				ConstLabels: prometheus.Labels{"source": LabelEventSourceContainerd},
-			})
-
-			collectors = append(collectors, EventTSContainerd)
-			c.EventTSContainerdEnabled = true
-
-			EventTSAPI = prometheus.NewGauge(prometheus.GaugeOpts{
-				Namespace:   Namespace,
-				Name:        "event_ts",
-				Help:        "Last timestamp when we received an event",
-				ConstLabels: prometheus.Labels{"source": LabelEventSourceAPI},
-			})
-
-			collectors = append(collectors, EventTSAPI)
-			c.EventTSAPIEnabled = true
 
 		case Namespace + "_proxy_redirects":
 			ProxyRedirects = prometheus.NewGaugeVec(prometheus.GaugeOpts{

api/v1/server/configure_cilium_api.go
@@ -304,7 +304,7 @@ func setupMiddlewares(handler http.Handler) http.Handler {
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
 	eventsHelper := &ciliumMetrics.APIEventTSHelper{
 		Next:      handler,
-		TSGauge:   ciliumMetrics.EventTSAPI,
+		TSGauge:   ciliumMetrics.EventTS,
 		Histogram: ciliumMetrics.APIInteractions,
 	}
 

daemon/cmd/daemon_main.go
@@ -51,7 +51,7 @@ import (
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	"github.com/cilium/cilium/pkg/ipmasq"
 	"github.com/cilium/cilium/pkg/k8s"
-	"github.com/cilium/cilium/pkg/k8s/watchers"
+	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/labelsfilter"
@@ -556,7 +556,7 @@ func initializeFlags() {
 		option.KVStoreOpt, "Key-value store options e.g. etcd.address=127.0.0.1:4001")
 	option.BindEnv(option.KVStoreOpt)
 
-	flags.Duration(option.K8sSyncTimeoutName, defaults.K8sSyncTimeout, "Timeout for synchronizing k8s resources before exiting")
+	flags.Duration(option.K8sSyncTimeoutName, defaults.K8sSyncTimeout, "Timeout after last K8s event for synchronizing k8s resources before exiting")
 	flags.MarkHidden(option.K8sSyncTimeoutName)
 	option.BindEnv(option.K8sSyncTimeoutName)
 
@@ -1636,10 +1636,10 @@ func (d *Daemon) initKVStore() {
 		// to the service IP as well perform the service -> backend IPs for
 		// that service IP.
 		d.k8sWatcher.WaitForCacheSync(
-			watchers.K8sAPIGroupServiceV1Core,
-			watchers.K8sAPIGroupEndpointV1Core,
-			watchers.K8sAPIGroupEndpointSliceV1Discovery,
-			watchers.K8sAPIGroupEndpointSliceV1Beta1Discovery,
+			resources.K8sAPIGroupServiceV1Core,
+			resources.K8sAPIGroupEndpointV1Core,
+			resources.K8sAPIGroupEndpointSliceV1Discovery,
+			resources.K8sAPIGroupEndpointSliceV1Beta1Discovery,
 		)
 		log := log.WithField(logfields.LogSubsys, "etcd")
 		goopts.DialOption = []grpc.DialOption{

daemon/cmd/state.go
@@ -19,7 +19,7 @@ import (
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/k8s"
-	"github.com/cilium/cilium/pkg/k8s/watchers"
+	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
@@ -66,7 +66,7 @@ func (d *Daemon) validateEndpoint(ep *endpoint.Endpoint) (valid bool, err error)
 	}
 
 	if ep.K8sPodName != "" && ep.K8sNamespace != "" && k8s.IsEnabled() {
-		d.k8sWatcher.WaitForCacheSync(watchers.K8sAPIGroupPodV1Core)
+		d.k8sWatcher.WaitForCacheSync(resources.K8sAPIGroupPodV1Core)
 		pod, err := d.k8sWatcher.GetCachedPod(ep.K8sNamespace, ep.K8sPodName)
 		if err != nil && k8serrors.IsNotFound(err) {
 			return false, fmt.Errorf("Kubernetes pod %s/%s does not exist", ep.K8sNamespace, ep.K8sPodName)

operator/ccnp_event.go
@@ -17,11 +17,16 @@ import (
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy/groups"
 )
+
+func k8sEventMetric(scope, action string) {
+	metrics.EventTS.WithLabelValues(metrics.LabelEventSourceK8s, scope, action).SetToCurrentTime()
+}
 
 // enableCCNPWatcher is similar to enableCNPWatcher but handles the watch events for
 // clusterwide policies. Since, internally Clusterwide policies are implemented
@@ -65,9 +70,8 @@ func enableCCNPWatcher() error {
 		0,
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEventMetric(resources.MetricCCNP, resources.MetricCreate)
 				if cnp := k8s.ObjToSlimCNP(obj); cnp != nil {
-
 					// We need to deepcopy this structure because we are writing
 					// fields.
 					// See https://github.com/cilium/cilium/blob/27fee207f5422c95479422162e9ea0d2f2b6c770/pkg/policy/api/ingress.go#L112-L134
@@ -80,7 +84,7 @@ func enableCCNPWatcher() error {
 				}
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEventMetric(resources.MetricCCNP, resources.MetricUpdate)
 				if oldCNP := k8s.ObjToSlimCNP(oldObj); oldCNP != nil {
 					if newCNP := k8s.ObjToSlimCNP(newObj); newCNP != nil {
 						if oldCNP.DeepEqual(newCNP) {
@@ -98,7 +102,7 @@ func enableCCNPWatcher() error {
 				}
 			},
 			DeleteFunc: func(obj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEventMetric(resources.MetricCCNP, resources.MetricDelete)
 				cnp := k8s.ObjToSlimCNP(obj)
 				if cnp == nil {
 					return

operator/cnp_event.go
@@ -18,8 +18,8 @@ import (
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/informer"
+	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/kvstore/store"
-	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy/groups"
 )
@@ -76,9 +76,8 @@ func enableCNPWatcher() error {
 		0,
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEventMetric(resources.MetricCNP, resources.MetricCreate)
 				if cnp := k8s.ObjToSlimCNP(obj); cnp != nil {
-
 					// We need to deepcopy this structure because we are writing
 					// fields.
 					// See https://github.com/cilium/cilium/blob/27fee207f5422c95479422162e9ea0d2f2b6c770/pkg/policy/api/ingress.go#L112-L134
@@ -91,7 +90,7 @@ func enableCNPWatcher() error {
 				}
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEventMetric(resources.MetricCNP, resources.MetricUpdate)
 				if oldCNP := k8s.ObjToSlimCNP(oldObj); oldCNP != nil {
 					if newCNP := k8s.ObjToSlimCNP(newObj); newCNP != nil {
 						if oldCNP.DeepEqual(newCNP) {
@@ -109,7 +108,7 @@ func enableCNPWatcher() error {
 				}
 			},
 			DeleteFunc: func(obj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEventMetric(resources.MetricCNP, resources.MetricDelete)
 				cnp := k8s.ObjToSlimCNP(obj)
 				if cnp == nil {
 					return

operator/watchers/k8s_service_sync.go
@@ -24,6 +24,7 @@ import (
 	slim_discover_v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1"
 	slim_discover_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1beta1"
 	"github.com/cilium/cilium/pkg/k8s/utils"
+	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/k8s/watchers/subscriber"
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	"github.com/cilium/cilium/pkg/loadbalancer"
@@ -53,6 +54,10 @@ var (
 
 	serviceSubscribers = subscriber.NewServiceChain()
 )
+
+func k8sEventMetric(scope, action string) {
+	metrics.EventTS.WithLabelValues(metrics.LabelEventSourceK8s, scope, action)
+}
 
 func k8sServiceHandler(clusterName string) {
 	serviceHandler := func(event k8s.ServiceEvent) {
@@ -196,13 +201,13 @@ func InitServiceWatcher(
 			0,
 			cache.ResourceEventHandlerFuncs{
 				AddFunc: func(obj interface{}) {
-					metrics.EventTSK8s.SetToCurrentTime()
+					k8sEventMetric(resources.MetricService, resources.MetricCreate)
 					if k8sSvc := k8s.ObjToV1Services(obj); k8sSvc != nil {
 						serviceSubscribers.OnAddService(k8sSvc)
 					}
 				},
 				UpdateFunc: func(oldObj, newObj interface{}) {
-					metrics.EventTSK8s.SetToCurrentTime()
+					k8sEventMetric(resources.MetricService, resources.MetricUpdate)
 					if oldk8sSvc := k8s.ObjToV1Services(oldObj); oldk8sSvc != nil {
 						if newk8sSvc := k8s.ObjToV1Services(newObj); newk8sSvc != nil {
 							if oldk8sSvc.DeepEqual(newk8sSvc) {
@@ -213,7 +218,7 @@ func InitServiceWatcher(
 					}
 				},
 				DeleteFunc: func(obj interface{}) {
-					metrics.EventTSK8s.SetToCurrentTime()
+					k8sEventMetric(resources.MetricService, resources.MetricDelete)
 					k8sSvc := k8s.ObjToV1Services(obj)
 					if k8sSvc == nil {
 						return
@@ -252,13 +257,13 @@ func endpointsInit(k8sClient kubernetes.Interface, swgEps *lock.StoppableWaitGro
 		0,
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEventMetric(resources.MetricEndpoint, resources.MetricCreate)
 				if k8sEP := k8s.ObjToV1Endpoints(obj); k8sEP != nil {
 					K8sSvcCache.UpdateEndpoints(k8sEP, swgEps)
 				}
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEventMetric(resources.MetricEndpoint, resources.MetricUpdate)
 				if oldk8sEP := k8s.ObjToV1Endpoints(oldObj); oldk8sEP != nil {
 					if newk8sEP := k8s.ObjToV1Endpoints(newObj); newk8sEP != nil {
 						if oldk8sEP.DeepEqual(newk8sEP) {
@@ -269,7 +274,7 @@ func endpointsInit(k8sClient kubernetes.Interface, swgEps *lock.StoppableWaitGro
 				}
 			},
 			DeleteFunc: func(obj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEventMetric(resources.MetricEndpoint, resources.MetricDelete)
 				k8sEP := k8s.ObjToV1Endpoints(obj)
 				if k8sEP == nil {
 					return
@@ -301,13 +306,13 @@ func endpointSlicesInit(k8sClient kubernetes.Interface, swgEps *lock.StoppableWa
 				// so it means the cluster has endpoint slices enabled.
 				close(hasEndpointSlices)
 			})
-			metrics.EventTSK8s.SetToCurrentTime()
+			k8sEventMetric(resources.MetricEndpointSlice, resources.MetricCreate)
 			if k8sEP := k8s.ObjToV1EndpointSlice(obj); k8sEP != nil {
 				K8sSvcCache.UpdateEndpointSlicesV1(k8sEP, swgEps)
 			}
 		}
 		updateFunc = func(oldObj, newObj interface{}) {
-			metrics.EventTSK8s.SetToCurrentTime()
+			k8sEventMetric(resources.MetricEndpointSlice, resources.MetricUpdate)
 			if oldk8sEP := k8s.ObjToV1EndpointSlice(oldObj); oldk8sEP != nil {
 				if newk8sEP := k8s.ObjToV1EndpointSlice(newObj); newk8sEP != nil {
 					if oldk8sEP.DeepEqual(newk8sEP) {
@@ -318,7 +323,7 @@ func endpointSlicesInit(k8sClient kubernetes.Interface, swgEps *lock.StoppableWa
 			}
 		}
 		delFunc = func(obj interface{}) {
-			metrics.EventTSK8s.SetToCurrentTime()
+			k8sEventMetric(resources.MetricEndpointSlice, resources.MetricDelete)
 			k8sEP := k8s.ObjToV1EndpointSlice(obj)
 			if k8sEP == nil {
 				return
@@ -334,13 +339,13 @@ func endpointSlicesInit(k8sClient kubernetes.Interface, swgEps *lock.StoppableWa
 				// so it means the cluster has endpoint slices enabled.
 				close(hasEndpointSlices)
 			})
-			metrics.EventTSK8s.SetToCurrentTime()
+			k8sEventMetric(resources.MetricEndpointSlice, resources.MetricCreate)
 			if k8sEP := k8s.ObjToV1Beta1EndpointSlice(obj); k8sEP != nil {
 				K8sSvcCache.UpdateEndpointSlicesV1Beta1(k8sEP, swgEps)
 			}
 		}
 		updateFunc = func(oldObj, newObj interface{}) {
-			metrics.EventTSK8s.SetToCurrentTime()
+			k8sEventMetric(resources.MetricEndpointSlice, resources.MetricUpdate)
 			if oldk8sEP := k8s.ObjToV1Beta1EndpointSlice(oldObj); oldk8sEP != nil {
 				if newk8sEP := k8s.ObjToV1Beta1EndpointSlice(newObj); newk8sEP != nil {
 					if oldk8sEP.DeepEqual(newk8sEP) {
@@ -351,7 +356,7 @@ func endpointSlicesInit(k8sClient kubernetes.Interface, swgEps *lock.StoppableWa
 			}
 		}
 		delFunc = func(obj interface{}) {
-			metrics.EventTSK8s.SetToCurrentTime()
+			k8sEventMetric(resources.MetricEndpointSlice, resources.MetricDelete)
 			k8sEP := k8s.ObjToV1Beta1EndpointSlice(obj)
 			if k8sEP == nil {
 				return

pkg/defaults/defaults.go
@@ -266,8 +266,8 @@ const (
 	// invoked only for endpoints which are selected by policy changes.
 	SelectiveRegeneration = true
 
-	// K8sSyncTimeout specifies the standard time to allow for synchronizing
-	// local caches with Kubernetes state before exiting.
+	// K8sSyncTimeout specifies the default time to wait after the last event
+	// of a Kubernetes resource type before timing out while waiting for synchronization.
 	K8sSyncTimeout = 3 * time.Minute
 
 	// AllocatorListTimeout specifies the standard time to allow for listing

commit 39e1cc539de71fe9d7e994416da8844de6d0df84

pkg/metrics: Remove unused metric variables
Fixes: 41d77e168f ("metrics: Add scope labels events_ts metrics.")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/metrics/metrics.go
@@ -542,8 +542,6 @@ type Configuration struct {
 	IdentityCountEnabled                    bool
 	EventTSEnabled                          bool
 	EventLagK8sEnabled                      bool
-	EventTSContainerdEnabled                bool
-	EventTSAPIEnabled                       bool
 	ProxyRedirectsEnabled                   bool
 	ProxyPolicyL7Enabled                    bool
 	ProxyParseErrorsEnabled                 bool