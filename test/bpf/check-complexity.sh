330af019e3c70c17fe8ea77dfc7e88fbda507605
Julian Wiedmann <jwi@isovalent.com>
2023-01-05 15:08:44 +0200

    bpf: nodeport: rename *_ENCAP_NODEPORT_NAT tail-calls
    [ upstream commit a12b9eced11b02de6ae454555fe0ef9b1dd08891 ]
    
    We have
    1. NODEPORT_NAT_EGRESS for SNAT of requests that get forwarded by the LB,
    2. NODEPORT_NAT_INGRESS for revSNAT / revDNAT of replies that enter the LB.
    
    Pick NODEPORT_NAT_FWD for the path where local traffic leaves the node
    via to-netdev or to-overlay. This is used for BPF Masquerading, and in the
    future also for handling replies by a local backend.
    
    Also adjust the naming of some related functions.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: André Martins <andre@cilium.io>

--- test/bpf/check-complexity.sh
+++ test/bpf/check-complexity.sh
@@ -32,12 +32,12 @@ function annotate_section_names {
 	    -e "s/\(section '2\/16'\)/\1 (tail_call IPV6_NODEPORT_NAT_EGRESS)/" \
 	    -e "s/\(section '2\/17'\)/\1 (tail_call IPV4_NODEPORT_REVNAT)/" \
 	    -e "s/\(section '2\/18'\)/\1 (tail_call IPV6_NODEPORT_REVNAT)/" \
-	    -e "s/\(section '2\/19'\)/\1 (tail_call IPV4_ENCAP_NODEPORT_NAT)/" \
+	    -e "s/\(section '2\/19'\)/\1 (tail_call IPV4_NODEPORT_NAT_FWD)/" \
 	    -e "s/\(section '2\/20'\)/\1 (tail_call IPV4_NODEPORT_DSR)/" \
 	    -e "s/\(section '2\/21'\)/\1 (tail_call IPV6_NODEPORT_DSR)/" \
 	    -e "s/\(section '2\/22'\)/\1 (tail_call IPV4_FROM_HOST)/" \
 	    -e "s/\(section '2\/23'\)/\1 (tail_call IPV6_FROM_HOST)/" \
-	    -e "s/\(section '2\/24'\)/\1 (tail_call IPV6_ENCAP_NODEPORT_NAT)/" \
+	    -e "s/\(section '2\/24'\)/\1 (tail_call IPV6_NODEPORT_NAT_FWD)/" \
 	    -e "s/\(section '2\/25'\)/\1 (tail_call IPV4_FROM_LXC_CONT)/" \
 	    -e "s/\(section '2\/26'\)/\1 (tail_call IPV6_FROM_LXC_CONT)/" \
 	    -e "s/\(section '2\/27'\)/\1 (tail_call IPV4_CT_INGRESS)/" \
