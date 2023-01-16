cilium/cilium

commit fd52b2fb138299f48a9f887371c2eafeb0692e64

bpf: nodeport: skip CT lookup for non-NodePort replies in RevDNAT path
Consider a pod-to-external connection that gets masqueraded in to-netdev.
A reply for such a connection first gets revSNATed in
tail_nodeport_nat_ingress_ipv4(), and then passes into rev_nodeport_lb4()
to check whether it's actually a reply to a forwarded NodePort connection.

Here the CT lookup succeeds, but then the additional checks for
ct_state.node_port and ct_state.rev_nat_index fail. Therefore the reply
tail-calls back into CILIUM_CALL_IPV4_FROM_NETDEV and eventually gets
delivered towards the pod. On the pod's ingress path there is a second
(expected) CT lookup.

As the first CT lookup succeeded, we end up double-accounting the reply
in __ct_lookup().

A similar scenario affects host-to-external connections. With non-DSR
NodePort they are also in scope of our SNAT code (to avoid clashes with
forwarded Nodeport connections), and get connection-tracked. The
external-to-host reply traffic will then do a CT lookup in the revSNAT
path, and the gratuitious CT lookup in the revDNAT path results in
double accounting.

So before calling ct_lookup4() in the RevDNAT nodeport path, first check if
we will find a relevant CT entry. As ct_has_nodeport_egress_entry4()
expects a full 5-tuple, add a new LB-specific helper that extracts the
tuple. Make the helper generic enough (ie. handle ICMP) so that we can
re-use it for the SVC lookup path in the future.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -982,7 +982,7 @@ static __always_inline int rev_nodeport_lb6(struct __ctx_buff *ctx, __u32 *ifind
 					    int *ext_err)
 {
 	const bool nat_46x64_fib = nat46x64_cb_route(ctx);
-	int ret, fib_ret, ret2, l3_off = ETH_HLEN, l4_off, hdrlen;
+	int ret, fib_ret, ret2, l4_off;
 	struct ipv6_ct_tuple tuple = {};
 	void *data, *data_end;
 	struct ipv6hdr *ip6;
@@ -1000,14 +1000,16 @@ static __always_inline int rev_nodeport_lb6(struct __ctx_buff *ctx, __u32 *ifind
 		goto skip_rev_dnat;
 #endif
 
-	tuple.nexthdr = ip6->nexthdr;
-	ipv6_addr_copy(&tuple.daddr, (union v6addr *) &ip6->daddr);
-	ipv6_addr_copy(&tuple.saddr, (union v6addr *) &ip6->saddr);
+	ret = lb6_extract_tuple(ctx, ip6, &l4_off, &tuple);
+	if (ret) {
+		if (ret == DROP_NO_SERVICE || ret == DROP_UNKNOWN_L4)
+			goto out;
+		return ret;
+	}
 
-	hdrlen = ipv6_hdrlen(ctx, &tuple.nexthdr);
-	if (hdrlen < 0)
-		return hdrlen;
-	l4_off = l3_off + hdrlen;
+	if (!ct_has_nodeport_egress_entry6(get_ct_map6(&tuple), &tuple))
+		goto out;
+
 	csum_l4_offset_and_flags(tuple.nexthdr, &csum_off);
 
 	ret = ct_lookup6(get_ct_map6(&tuple), &tuple, ctx, l4_off, CT_INGRESS, &ct_state,
@@ -1098,6 +1100,7 @@ skip_rev_dnat:
 		return CTX_ACT_REDIRECT;
 	}
 
+out:
 	if (bpf_skip_recirculation(ctx))
 		return DROP_NAT_NO_MAPPING;
 
@@ -2002,11 +2005,17 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, __u32 *ifind
 		goto encap_redirect;
 #endif /* ENABLE_EGRESS_GATEWAY */
 
-	tuple.nexthdr = ip4->protocol;
-	tuple.daddr = ip4->daddr;
-	tuple.saddr = ip4->saddr;
+	ret = lb4_extract_tuple(ctx, ip4, &l4_off, &tuple);
+	if (ret) {
+		/* If it's not a SVC protocol, we don't need to check for RevDNAT: */
+		if (ret == DROP_NO_SERVICE || ret == DROP_UNKNOWN_L4)
+			goto out;
+		return ret;
+	}
 
-	l4_off = l3_off + ipv4_hdrlen(ip4);
+	if (!ct_has_nodeport_egress_entry4(get_ct_map4(&tuple), &tuple))
+		goto out;
+
 	csum_l4_offset_and_flags(tuple.nexthdr, &csum_off);
 
 	ret = ct_lookup4(get_ct_map4(&tuple), &tuple, ctx, l4_off, CT_INGRESS, &ct_state,
@@ -2101,6 +2110,7 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, __u32 *ifind
 		return CTX_ACT_REDIRECT;
 	}
 
+out:
 	if (bpf_skip_recirculation(ctx))
 		return DROP_NAT_NO_MAPPING;
 

bpf/lib/l4.h
@@ -51,4 +51,10 @@ static __always_inline int l4_load_port(struct __ctx_buff *ctx, int off,
 {
 	return ctx_load_bytes(ctx, off, port, sizeof(__be16));
 }
+
+static __always_inline int l4_load_ports(struct __ctx_buff *ctx, int off,
+					 __be16 *ports)
+{
+	return ctx_load_bytes(ctx, off, ports, 2 * sizeof(__be16));
+}
 #endif

bpf/lib/lb.h
@@ -536,6 +536,38 @@ static __always_inline int lb6_extract_key(struct __ctx_buff *ctx __maybe_unused
 			       NULL);
 }
 
+static __always_inline int
+lb6_extract_tuple(struct __ctx_buff *ctx, struct ipv6hdr *ip6, int *l4_off,
+		  struct ipv6_ct_tuple *tuple)
+{
+	int ret;
+
+	tuple->nexthdr = ip6->nexthdr;
+	ipv6_addr_copy(&tuple->daddr, (union v6addr *)&ip6->daddr);
+	ipv6_addr_copy(&tuple->saddr, (union v6addr *)&ip6->saddr);
+
+	ret = ipv6_hdrlen(ctx, &tuple->nexthdr);
+	if (ret < 0)
+		return ret;
+
+	*l4_off = ETH_HLEN + ret;
+
+	switch (tuple->nexthdr) {
+	case IPPROTO_TCP:
+	case IPPROTO_UDP:
+#ifdef ENABLE_SCTP
+	case IPPROTO_SCTP:
+#endif  /* ENABLE_SCTP */
+		if (l4_load_ports(ctx, *l4_off, &tuple->dport) < 0)
+			return DROP_CT_INVALID_HDR;
+		return 0;
+	case IPPROTO_ICMPV6:
+		return DROP_NO_SERVICE;
+	default:
+		return DROP_UNKNOWN_L4;
+	}
+}
+
 static __always_inline
 bool lb6_src_range_ok(const struct lb6_service *svc __maybe_unused,
 		      const union v6addr *saddr __maybe_unused)
@@ -1160,6 +1192,31 @@ static __always_inline int lb4_extract_key(struct __ctx_buff *ctx __maybe_unused
 		csum_l4_offset_and_flags(ip4->protocol, csum_off);
 
 	return extract_l4_port(ctx, ip4->protocol, l4_off, CT_EGRESS, &key->dport, ip4);
+}
+
+static __always_inline int
+lb4_extract_tuple(struct __ctx_buff *ctx, struct iphdr *ip4, int *l4_off,
+		  struct ipv4_ct_tuple *tuple)
+{
+	tuple->nexthdr = ip4->protocol;
+	tuple->daddr = ip4->daddr;
+	tuple->saddr = ip4->saddr;
+
+	*l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
+
+	switch (tuple->nexthdr) {
+	case IPPROTO_TCP:
+	case IPPROTO_UDP:
+#ifdef ENABLE_SCTP
+	case IPPROTO_SCTP:
+#endif  /* ENABLE_SCTP */
+		return ipv4_ct_extract_l4_ports(ctx, *l4_off, CT_EGRESS,
+						tuple, NULL);
+	case IPPROTO_ICMP:
+		return DROP_NO_SERVICE;
+	default:
+		return DROP_UNKNOWN_L4;
+	}
 }
 
 static __always_inline

commit f7a38736a49b2df32eaae0f46e0c684d19696fde

bpf: nodeport: fix-up error check in rev_nodeport_lb*() for XDP
lb4_extract_tuple() calls ipv4_ct_extract_l4_ports(), which returns
CTX_ACT_OK on success. This is 0 in TC, but 2 in XDP. This caused us to
erronously bail out from the RevDNAT check in the XDP NAT reply path.

Fix up the error check accordingly. Also adjust lb6_extract_tuple() for
consistency.

Fixes: fd52b2fb1382 ("bpf: nodeport: skip CT lookup for non-NodePort replies in RevDNAT path")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -1001,7 +1001,7 @@ static __always_inline int rev_nodeport_lb6(struct __ctx_buff *ctx, __u32 *ifind
 #endif
 
 	ret = lb6_extract_tuple(ctx, ip6, &l4_off, &tuple);
-	if (ret) {
+	if (ret < 0) {
 		if (ret == DROP_NO_SERVICE || ret == DROP_UNKNOWN_L4)
 			goto out;
 		return ret;
@@ -2006,7 +2006,7 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, __u32 *ifind
 #endif /* ENABLE_EGRESS_GATEWAY */
 
 	ret = lb4_extract_tuple(ctx, ip4, &l4_off, &tuple);
-	if (ret) {
+	if (ret < 0) {
 		/* If it's not a SVC protocol, we don't need to check for RevDNAT: */
 		if (ret == DROP_NO_SERVICE || ret == DROP_UNKNOWN_L4)
 			goto out;