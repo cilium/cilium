218611f64b2643809fa1c26494d3dcb65310123e
Daniel Borkmann <daniel@iogearbox.net>
2023-01-17 13:39:11 +0000

    bpf: Rename redirect_direct_v{4,6} to fib-related helper
    Small rename into fib_redirect_v{4,6}, no change in functionality.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -604,7 +604,7 @@ ct_recreate6:
 	if (is_defined(ENABLE_HOST_ROUTING)) {
 		int oif;
 
-		ret = redirect_direct_v6(ctx, ETH_HLEN, ip6, &oif);
+		ret = fib_redirect_v6(ctx, ETH_HLEN, ip6, &oif);
 		if (likely(ret == CTX_ACT_REDIRECT))
 			send_trace_notify(ctx, TRACE_TO_NETWORK, SECLABEL,
 					  *dst_id, 0, oif,
@@ -1107,7 +1107,7 @@ skip_vtep:
 	if (is_defined(ENABLE_HOST_ROUTING)) {
 		int oif;
 
-		ret = redirect_direct_v4(ctx, ETH_HLEN, ip4, &oif);
+		ret = fib_redirect_v4(ctx, ETH_HLEN, ip4, &oif);
 		if (likely(ret == CTX_ACT_REDIRECT))
 			send_trace_notify(ctx, TRACE_TO_NETWORK, SECLABEL,
 					  *dst_id, 0, oif,
