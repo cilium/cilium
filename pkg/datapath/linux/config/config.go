983d9ceacfaefdda89070a0ea493cb71c9bcd6ca
Daniel Borkmann <daniel@iogearbox.net>
2023-01-17 12:48:31 +0000

    bpf: Remove ENABLE_SKIP_FIB config for time being
    Remove for now to simplify later refactoring. Later commits add a reworked
    version for single-node devices.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- pkg/datapath/linux/config/config.go
+++ pkg/datapath/linux/config/config.go
@@ -905,9 +905,6 @@ func (h *HeaderfileWriter) writeTemplateConfig(fw *bufio.Writer, e datapath.Endp
 			return err
 		}
 		fmt.Fprintf(fw, "#define DIRECT_ROUTING_DEV_IFINDEX %d\n", directRoutingIfIndex)
-		if len(option.Config.GetDevices()) == 1 {
-			fmt.Fprintf(fw, "#define ENABLE_SKIP_FIB 1\n")
-		}
 	}
 
 	if e.IsHost() {
