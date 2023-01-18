cilium/cilium

commit b4335eede37028a9d8a4048367e858354c813251

k8s: Introduce shared resources
This introduces a set of centrally managed resource.Resource[T] objects. These aim
to replace the current k8s/watchers and k8s/watchers/subscriber, with a single
interface (Resource[T]) that one can be observed and its store queried from any cell.
With this we'll gradually remove K8sWatcher by moving code from k8s/watchers/ into feature
specific packages that subscribe to Resource[T]. See pkg/k8s/resource/example.go for usage
example of Resource[T].

To start things off, in this patch the Resource[*Service] and Resource[*Node] are introduced
into the object graph and the existing service and node watchers are refactored to be
implemented on top of it. This keeps the existing code working, but allows new cells to
use these objects directly without dependency on K8sWatcher.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/k8s/watchers/node.go
@@ -8,17 +8,11 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/tools/cache"
 
 	"github.com/cilium/cilium/pkg/comparator"
-	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/k8s/client"
-	"github.com/cilium/cilium/pkg/k8s/informer"
-	"github.com/cilium/cilium/pkg/k8s/utils"
+	"github.com/cilium/cilium/pkg/k8s/resource"
 	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/k8s/watchers/subscriber"
 	"github.com/cilium/cilium/pkg/lock"
@@ -48,63 +42,66 @@ func nodeEventsAreEqual(oldNode, newNode *v1.Node) bool {
 }
 
 func (k *K8sWatcher) NodesInit(k8sClient client.Clientset) {
-	apiGroup := k8sAPIGroupNodeV1Core
 	k.nodesInitOnce.Do(func() {
+		synced := false
 		swg := lock.NewStoppableWaitGroup()
-
-		nodeStore, nodeController := informer.NewInformer(
-			utils.ListerWatcherWithFields(
-				utils.ListerWatcherFromTyped[*v1.NodeList](k8sClient.CoreV1().Nodes()),
-				fields.ParseSelectorOrDie("metadata.name="+nodeTypes.GetName())),
-			&v1.Node{},
-			0,
-			cache.ResourceEventHandlerFuncs{
-				AddFunc: func(obj interface{}) {
-					var valid bool
-					if node := k8s.ObjToV1Node(obj); node != nil {
-						valid = true
-						errs := k.NodeChain.OnAddNode(node, swg)
-						k.K8sEventProcessed(metricNode, resources.MetricCreate, errs == nil)
-					}
-					k.K8sEventReceived(apiGroup, metricNode, resources.MetricCreate, valid, false)
-				},
-				UpdateFunc: func(oldObj, newObj interface{}) {
-					var valid, equal bool
-					if oldNode := k8s.ObjToV1Node(oldObj); oldNode != nil {
-						valid = true
-						if newNode := k8s.ObjToV1Node(newObj); newNode != nil {
-							equal = nodeEventsAreEqual(oldNode, newNode)
-							if !equal {
-								errs := k.NodeChain.OnUpdateNode(oldNode, newNode, swg)
-								k.K8sEventProcessed(metricNode, resources.MetricCreate, errs == nil)
-							}
-						}
-					}
-					k.K8sEventReceived(apiGroup, metricNode, resources.MetricCreate, valid, false)
-				},
-				DeleteFunc: func(obj interface{}) {
-				},
-			},
-			nil,
+		k.blockWaitGroupToSyncResources(
+			k.stop,
+			swg,
+			func() bool { return synced },
+			k8sAPIGroupNodeV1Core,
 		)
-
-		k.nodeStore = nodeStore
-
-		k.blockWaitGroupToSyncResources(wait.NeverStop, swg, nodeController.HasSynced, k8sAPIGroupNodeV1Core)
-		go nodeController.Run(k.stop)
-		k.k8sAPIGroups.AddAPI(apiGroup)
+		go k.nodeEventLoop(&synced, swg)
 	})
 }
 
-// GetK8sNode returns the *local Node* from the local store.
-func (k *K8sWatcher) GetK8sNode(_ context.Context, nodeName string) (*v1.Node, error) {
-	k.WaitForCacheSync(k8sAPIGroupNodeV1Core)
-	pName := &v1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: nodeName,
-		},
+func (k *K8sWatcher) nodeEventLoop(synced *bool, swg *lock.StoppableWaitGroup) {
+	apiGroup := k8sAPIGroupNodeV1Core
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	events := k.sharedResources.LocalNode.Events(ctx)
+	var oldNode *v1.Node
+	for {
+		select {
+		case <-k.stop:
+			cancel()
+		case event, ok := <-events:
+			if !ok {
+				return
+			}
+			switch event.Kind {
+			case resource.Sync:
+				*synced = true
+			case resource.Upsert:
+				newNode := event.Object
+				if oldNode == nil {
+					k.K8sEventReceived(apiGroup, metricNode, resources.MetricCreate, true, false)
+					errs := k.NodeChain.OnAddNode(newNode, swg)
+					k.K8sEventProcessed(metricNode, resources.MetricCreate, errs == nil)
+				} else {
+					equal := nodeEventsAreEqual(oldNode, newNode)
+					k.K8sEventReceived(apiGroup, metricNode, resources.MetricUpdate, true, equal)
+					if !equal {
+						errs := k.NodeChain.OnUpdateNode(oldNode, newNode, swg)
+						k.K8sEventProcessed(metricNode, resources.MetricUpdate, errs == nil)
+					}
+				}
+				oldNode = newNode
+			}
+			event.Done(nil)
+		}
 	}
-	nodeInterface, exists, err := k.nodeStore.Get(pName)
+}
+
+// GetK8sNode returns the *local Node* from the local store.
+func (k *K8sWatcher) GetK8sNode(ctx context.Context, nodeName string) (*v1.Node, error) {
+	// Retrieve the store. Blocks until synced (or ctx cancelled).
+	store, err := k.sharedResources.LocalNode.Store(ctx)
+	if err != nil {
+		return nil, err
+	}
+	node, exists, err := store.GetByKey(resource.Key{Name: nodeName})
 	if err != nil {
 		return nil, err
 	}
@@ -114,7 +111,7 @@ func (k *K8sWatcher) GetK8sNode(_ context.Context, nodeName string) (*v1.Node, e
 			Resource: "Node",
 		}, nodeName)
 	}
-	return nodeInterface.(*v1.Node).DeepCopy(), nil
+	return node.DeepCopy(), nil
 }
 
 // ciliumNodeUpdater implements the subscriber.Node interface and is used

pkg/k8s/watchers/service.go
@@ -4,76 +4,66 @@
 package watchers
 
 import (
-	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/tools/cache"
+	"context"
 
 	"github.com/cilium/cilium/pkg/k8s"
-	"github.com/cilium/cilium/pkg/k8s/informer"
+	"github.com/cilium/cilium/pkg/k8s/resource"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
-	slimclientset "github.com/cilium/cilium/pkg/k8s/slim/k8s/client/clientset/versioned"
-	"github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/option"
 )
 
-func (k *K8sWatcher) servicesInit(slimClient slimclientset.Interface, swgSvcs *lock.StoppableWaitGroup, optsModifier func(*v1meta.ListOptions)) {
-	apiGroup := resources.K8sAPIGroupServiceV1Core
-	_, svcController := informer.NewInformer(
-		utils.ListerWatcherWithModifier(
-			utils.ListerWatcherFromTyped[*slim_corev1.ServiceList](slimClient.CoreV1().Services("")),
-			optsModifier),
-		&slim_corev1.Service{},
-		0,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				var valid, equal bool
-				defer func() {
-					k.K8sEventReceived(apiGroup, resources.MetricService, resources.MetricCreate, valid, equal)
-				}()
-				if k8sSvc := k8s.ObjToV1Services(obj); k8sSvc != nil {
-					valid = true
-					err := k.addK8sServiceV1(k8sSvc, swgSvcs)
-					k.K8sEventProcessed(resources.MetricService, resources.MetricCreate, err == nil)
-				}
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				var valid, equal bool
-				defer func() { k.K8sEventReceived(apiGroup, resources.MetricService, resources.MetricUpdate, valid, equal) }()
-				if oldk8sSvc := k8s.ObjToV1Services(oldObj); oldk8sSvc != nil {
-					if newk8sSvc := k8s.ObjToV1Services(newObj); newk8sSvc != nil {
-						valid = true
-						if k8s.EqualV1Services(oldk8sSvc, newk8sSvc, k.datapath.LocalNodeAddressing()) {
-							equal = true
-							return
-						}
+func (k *K8sWatcher) servicesInit() {
+	synced := false
+	swgSvcs := lock.NewStoppableWaitGroup()
 
-						err := k.updateK8sServiceV1(oldk8sSvc, newk8sSvc, swgSvcs)
-						k.K8sEventProcessed(resources.MetricService, resources.MetricUpdate, err == nil)
-					}
-				}
-			},
-			DeleteFunc: func(obj interface{}) {
-				var valid, equal bool
-				defer func() { k.K8sEventReceived(apiGroup, resources.MetricService, resources.MetricDelete, valid, equal) }()
-				k8sSvc := k8s.ObjToV1Services(obj)
-				if k8sSvc == nil {
-					return
-				}
-
-				valid = true
-				err := k.deleteK8sServiceV1(k8sSvc, swgSvcs)
-				k.K8sEventProcessed(resources.MetricService, resources.MetricDelete, err == nil)
-			},
-		},
-		nil,
+	k.blockWaitGroupToSyncResources(
+		k.stop,
+		swgSvcs,
+		func() bool { return synced },
+		resources.K8sAPIGroupServiceV1Core,
 	)
-	k.blockWaitGroupToSyncResources(k.stop, swgSvcs, svcController.HasSynced, resources.K8sAPIGroupServiceV1Core)
-	go svcController.Run(k.stop)
-	k.k8sAPIGroups.AddAPI(apiGroup)
+	go k.serviceEventLoop(&synced, swgSvcs)
+
+	k.k8sAPIGroups.AddAPI(resources.K8sAPIGroupServiceV1Core)
 }
 
-func (k *K8sWatcher) addK8sServiceV1(svc *slim_corev1.Service, swg *lock.StoppableWaitGroup) error {
+func (k *K8sWatcher) serviceEventLoop(synced *bool, swg *lock.StoppableWaitGroup) {
+	apiGroup := resources.K8sAPIGroupServiceV1Core
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	events := k.sharedResources.Services.Events(ctx)
+	for {
+		select {
+		case <-k.stop:
+			cancel()
+		case event, ok := <-events:
+			if !ok {
+				return
+			}
+			var err error
+			switch event.Kind {
+			case resource.Sync:
+				*synced = true
+			case resource.Upsert:
+				svc := event.Object
+				k.K8sEventReceived(apiGroup, resources.MetricService, resources.MetricUpdate, true, false)
+				err = k.upsertK8sServiceV1(svc, swg)
+				k.K8sEventProcessed(resources.MetricService, resources.MetricUpdate, err == nil)
+			case resource.Delete:
+				svc := event.Object
+				k.K8sEventReceived(apiGroup, resources.MetricService, resources.MetricUpdate, true, false)
+				err = k.deleteK8sServiceV1(svc, swg)
+				k.K8sEventProcessed(resources.MetricService, resources.MetricDelete, err == nil)
+			}
+			event.Done(err)
+		}
+	}
+}
+
+func (k *K8sWatcher) upsertK8sServiceV1(svc *slim_corev1.Service, swg *lock.StoppableWaitGroup) error {
 	svcID := k.K8sSvcCache.UpdateService(svc, swg)
 	if option.Config.EnableLocalRedirectPolicy {
 		if svc.Spec.Type == slim_corev1.ServiceTypeClusterIP {
@@ -86,10 +76,6 @@ func (k *K8sWatcher) addK8sServiceV1(svc *slim_corev1.Service, swg *lock.Stoppab
 		k.bgpSpeakerManager.OnUpdateService(svc)
 	}
 	return nil
-}
-
-func (k *K8sWatcher) updateK8sServiceV1(oldSvc, newSvc *slim_corev1.Service, swg *lock.StoppableWaitGroup) error {
-	return k.addK8sServiceV1(newSvc, swg)
 }
 
 func (k *K8sWatcher) deleteK8sServiceV1(svc *slim_corev1.Service, swg *lock.StoppableWaitGroup) error {

daemon/cmd/cells.go
@@ -8,6 +8,7 @@ import (
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/gops"
 	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/k8s"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
@@ -51,6 +52,10 @@ var (
 		// LocalNodeStore holds onto the information about the local node and allows
 		// observing changes to it.
 		node.LocalNodeStoreCell,
+
+		// Shared resources provide access to k8s resources as event streams or as
+		// read-only stores.
+		k8s.SharedResourcesCell,
 
 		// daemonCell wraps the legacy daemon initialization and provides Promise[*Daemon].
 		daemonCell,

daemon/cmd/daemon.go
@@ -404,6 +404,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup,
 	epMgr *endpointmanager.EndpointManager, dp datapath.Datapath,
 	wgAgent *wg.Agent,
 	clientset k8sClient.Clientset,
+	sharedResources k8s.SharedResources,
 ) (*Daemon, *endpointRestoreState, error) {
 
 	var (
@@ -701,6 +702,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup,
 		option.Config,
 		d.ipcache,
 		d.cgroupManager,
+		sharedResources,
 	)
 	nd.RegisterK8sGetters(d.k8sWatcher)
 

daemon/cmd/daemon_main.go
@@ -1638,13 +1638,14 @@ var daemonCell = cell.Module(
 type daemonParams struct {
 	cell.In
 
-	Lifecycle      hive.Lifecycle
-	Clientset      k8sClient.Clientset
-	Datapath       datapath.Datapath
-	WGAgent        *wg.Agent `optional:"true"`
-	LocalNodeStore node.LocalNodeStore
-	BGPController  *bgpv1.Controller
-	Shutdowner     hive.Shutdowner
+	Lifecycle       hive.Lifecycle
+	Clientset       k8sClient.Clientset
+	Datapath        datapath.Datapath
+	WGAgent         *wg.Agent `optional:"true"`
+	LocalNodeStore  node.LocalNodeStore
+	BGPController   *bgpv1.Controller
+	Shutdowner      hive.Shutdowner
+	SharedResources k8s.SharedResources
 }
 
 func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
@@ -1671,7 +1672,8 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 				WithDefaultEndpointManager(daemonCtx, params.Clientset, endpoint.CheckHealth),
 				params.Datapath,
 				params.WGAgent,
-				params.Clientset)
+				params.Clientset,
+				params.SharedResources)
 			if err != nil {
 				return fmt.Errorf("daemon creation failed: %w", err)
 			}

daemon/cmd/root.go
@@ -37,7 +37,9 @@ var (
 	dotGraphCmd = &cobra.Command{
 		Use:   "dot-graph",
 		Short: "Output the internal dependencies of cilium-agent in graphviz dot format",
-		Run:   func(cmd *cobra.Command, args []string) { agentHive.PrintDotGraph() },
+		Run: func(cmd *cobra.Command, args []string) {
+			agentHive.PrintDotGraph()
+		},
 	}
 
 	objectsCmd = &cobra.Command{

pkg/bgpv1/cell.go
@@ -10,11 +10,9 @@ import (
 	"github.com/cilium/cilium/pkg/hive/cell"
 	v2alpha1api "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/k8s/client"
-	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/resource"
 	slim_core_v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	"github.com/cilium/cilium/pkg/k8s/utils"
-	k8sutils "github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/option"
 )
 
@@ -34,8 +32,6 @@ var Cell = cell.Module(
 		// goBGP is currently the only supported RouterManager, if more are
 		// implemented, provide the manager via a Cell that pics implementation based on configuration.
 		gobgp.NewBGPRouterManager,
-		// Create a slim service resource
-		newSlimServiceResource,
 		// Create a slim service DiffStore
 		gobgp.NewDiffStore[*slim_core_v1.Service],
 	),
@@ -58,27 +54,3 @@ func newBGPPeeringPolicyResource(lc hive.Lifecycle, c client.Clientset, dc *opti
 			c.CiliumV2alpha1().CiliumBGPPeeringPolicies(),
 		))
 }
-
-// Constructs a slim service resource with perpetual retries at a exponential backoff
-func newSlimServiceResource(lc hive.Lifecycle, c k8sClient.Clientset, dc *option.DaemonConfig) (resource.Resource[*slim_core_v1.Service], error) {
-	// Do not create this resource if the BGP Control Plane is disabled
-	if !dc.BGPControlPlaneEnabled() {
-		return nil, nil
-	}
-
-	if !c.IsEnabled() {
-		return nil, nil
-	}
-
-	optsModifier, err := k8sutils.GetServiceListOptionsModifier(option.Config)
-	if err != nil {
-		return nil, err
-	}
-	return resource.New[*slim_core_v1.Service](
-		lc,
-		k8sutils.ListerWatcherWithModifier(
-			k8sutils.ListerWatcherFromTyped[*slim_core_v1.ServiceList](c.Slim().CoreV1().Services("")),
-			optsModifier),
-	), nil
-}
-

pkg/k8s/shared_resources.go
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package k8s
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	"github.com/cilium/cilium/pkg/k8s/utils"
+	nodeTypes "github.com/cilium/cilium/pkg/node/types"
+	"github.com/cilium/cilium/pkg/option"
+)
+
+var (
+	// SharedResourceCell provides a set of shared handles to Kubernetes resources used throughout the
+	// Cilium agent. Each of the resources share a client-go informer and backing store so we only
+	// have one watch API call for each resource kind and that we maintain only one copy of each object.
+	//
+	// See pkg/k8s/resource/resource.go for documentation on the Resource[T] type.
+	SharedResourcesCell = cell.Module(
+		"k8s-shared-resources",
+		"Shared Kubernetes resources",
+
+		cell.Provide(
+			serviceResource,
+			localNodeResource,
+		),
+	)
+)
+
+type SharedResources struct {
+	cell.In
+	Services  resource.Resource[*slim_corev1.Service]
+	LocalNode resource.Resource[*corev1.Node]
+}
+
+func serviceResource(lc hive.Lifecycle, cs client.Clientset) (resource.Resource[*slim_corev1.Service], error) {
+	if !cs.IsEnabled() {
+		return nil, nil
+	}
+	optsModifier, err := utils.GetServiceListOptionsModifier(option.Config)
+	if err != nil {
+		return nil, err
+	}
+	lw := utils.ListerWatcherFromTyped[*slim_corev1.ServiceList](cs.Slim().CoreV1().Services(""))
+	lw = utils.ListerWatcherWithModifier(lw, optsModifier)
+	return resource.New[*slim_corev1.Service](lc, lw), nil
+}
+
+func localNodeResource(lc hive.Lifecycle, cs client.Clientset) (resource.Resource[*corev1.Node], error) {
+	if !cs.IsEnabled() {
+		return nil, nil
+	}
+	lw := utils.ListerWatcherFromTyped[*corev1.NodeList](cs.CoreV1().Nodes())
+	lw = utils.ListerWatcherWithFields(lw, fields.ParseSelectorOrDie("metadata.name="+nodeTypes.GetName()))
+	return resource.New[*corev1.Node](lc, lw), nil
+}

commit c66cec7b5bea0f84b4547ef03fd7dd0d74ba1719

k8s/watchers: Fix race condition in init functions
Use an atomic variable as the previous boolean was being passed around
to different goroutines without any synchronization mechanism.

Fixes: b4335eede37 ("k8s: Introduce shared resources")
Fixes: ee2ccdae7f0 ("k8s: Add shared resource for namespaces")
Fixes: https://github.com/cilium/cilium/issues/23166

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/k8s/watchers/namespace.go
@@ -6,6 +6,7 @@ package watchers
 import (
 	"context"
 	"errors"
+	"sync/atomic"
 
 	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -24,12 +25,13 @@ import (
 func (k *K8sWatcher) namespacesInit() {
 	apiGroup := k8sAPIGroupNamespaceV1Core
 
-	synced := false
+	var synced atomic.Bool
+	synced.Store(false)
 
 	k.blockWaitGroupToSyncResources(
 		k.stop,
 		nil,
-		func() bool { return synced },
+		func() bool { return synced.Load() },
 		apiGroup,
 	)
 	k.k8sAPIGroups.AddAPI(apiGroup)
@@ -54,7 +56,7 @@ func (k *K8sWatcher) namespacesInit() {
 				var err error
 				switch event.Kind {
 				case resource.Sync:
-					synced = true
+					synced.Store(true)
 				case resource.Upsert:
 					err = nsUpdater.update(event.Object)
 					k.K8sEventProcessed(metricNS, resources.MetricUpdate, err == nil)

pkg/k8s/watchers/node.go
@@ -5,6 +5,7 @@ package watchers
 
 import (
 	"context"
+	"sync/atomic"
 
 	v1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
@@ -43,19 +44,20 @@ func nodeEventsAreEqual(oldNode, newNode *v1.Node) bool {
 
 func (k *K8sWatcher) NodesInit(k8sClient client.Clientset) {
 	k.nodesInitOnce.Do(func() {
-		synced := false
+		var synced atomic.Bool
+		synced.Store(false)
 		swg := lock.NewStoppableWaitGroup()
 		k.blockWaitGroupToSyncResources(
 			k.stop,
 			swg,
-			func() bool { return synced },
+			func() bool { return synced.Load() },
 			k8sAPIGroupNodeV1Core,
 		)
 		go k.nodeEventLoop(&synced, swg)
 	})
 }
 
-func (k *K8sWatcher) nodeEventLoop(synced *bool, swg *lock.StoppableWaitGroup) {
+func (k *K8sWatcher) nodeEventLoop(synced *atomic.Bool, swg *lock.StoppableWaitGroup) {
 	apiGroup := k8sAPIGroupNodeV1Core
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -72,7 +74,7 @@ func (k *K8sWatcher) nodeEventLoop(synced *bool, swg *lock.StoppableWaitGroup) {
 			}
 			switch event.Kind {
 			case resource.Sync:
-				*synced = true
+				synced.Store(true)
 			case resource.Upsert:
 				newNode := event.Object
 				if oldNode == nil {

pkg/k8s/watchers/service.go
@@ -5,6 +5,7 @@ package watchers
 
 import (
 	"context"
+	"sync/atomic"
 
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/k8s/resource"
@@ -15,13 +16,14 @@ import (
 )
 
 func (k *K8sWatcher) servicesInit() {
-	synced := false
+	var synced atomic.Bool
+	synced.Store(false)
 	swgSvcs := lock.NewStoppableWaitGroup()
 
 	k.blockWaitGroupToSyncResources(
 		k.stop,
 		swgSvcs,
-		func() bool { return synced },
+		func() bool { return synced.Load() },
 		resources.K8sAPIGroupServiceV1Core,
 	)
 	go k.serviceEventLoop(&synced, swgSvcs)
@@ -29,7 +31,7 @@ func (k *K8sWatcher) servicesInit() {
 	k.k8sAPIGroups.AddAPI(resources.K8sAPIGroupServiceV1Core)
 }
 
-func (k *K8sWatcher) serviceEventLoop(synced *bool, swg *lock.StoppableWaitGroup) {
+func (k *K8sWatcher) serviceEventLoop(synced *atomic.Bool, swg *lock.StoppableWaitGroup) {
 	apiGroup := resources.K8sAPIGroupServiceV1Core
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -46,7 +48,7 @@ func (k *K8sWatcher) serviceEventLoop(synced *bool, swg *lock.StoppableWaitGroup
 			var err error
 			switch event.Kind {
 			case resource.Sync:
-				*synced = true
+				synced.Store(true)
 			case resource.Upsert:
 				svc := event.Object
 				k.K8sEventReceived(apiGroup, resources.MetricService, resources.MetricUpdate, true, false)