cilium/cilium

commit fe64c7f02c3d52ac6347281054b3b5e5ac472288

Revert "install: move cni config management to the agent"
This reverts commit 0d6346dbe5bada046eb72ab2f797fa4c0f86d07e.

This commit is causing issues on AKS where newly-created pods are not
getting managed by Cilium for some reason. Reverting to unblock AKS
testing.

Signed-off-by: Paul Chaignon <paul@cilium.io>

images/cilium/Dockerfile
@@ -61,6 +61,7 @@ RUN --mount=type=bind,readwrite,target=/go/src/github.com/cilium/cilium --mount=
 
 COPY images/cilium/init-container.sh \
      plugins/cilium-cni/cni-install.sh \
+     plugins/cilium-cni/cni-uninstall.sh \
        /tmp/install/${TARGETOS}/${TARGETARCH}
 
 #

install/kubernetes/cilium/templates/cilium-agent/daemonset.yaml
@@ -188,6 +188,18 @@ spec:
               fieldPath: metadata.namespace
         - name: CILIUM_CLUSTERMESH_CONFIG
           value: /var/lib/cilium/clustermesh/
+        - name: CILIUM_CNI_CHAINING_MODE
+          valueFrom:
+            configMapKeyRef:
+              name: cilium-config
+              key: cni-chaining-mode
+              optional: true
+        - name: CILIUM_CUSTOM_CNI_CONF
+          valueFrom:
+            configMapKeyRef:
+              name: cilium-config
+              key: custom-cni-conf
+              optional: true
         {{- if .Values.k8sServiceHost }}
         - name: KUBERNETES_SERVICE_HOST
           value: {{ .Values.k8sServiceHost | quote }}
@@ -198,6 +210,20 @@ spec:
         {{- end }}
         {{- with .Values.extraEnv }}
         {{- toYaml . | trim | nindent 8 }}
+        {{- end }}
+        {{- if .Values.cni.install }}
+        lifecycle:
+          postStart:
+            exec:
+              command:
+              - "/cni-install.sh"
+              - "--enable-debug={{ .Values.debug.enabled }}"
+              - "--cni-exclusive={{ .Values.cni.exclusive }}"
+              - "--log-file={{ .Values.cni.logFile }}"
+          preStop:
+            exec:
+              command:
+              - /cni-uninstall.sh
         {{- end }}
         {{- with .Values.resources }}
         resources:
@@ -612,19 +638,6 @@ spec:
             done
         terminationMessagePolicy: FallbackToLogsOnError
       {{- end }} # wait-for-kube-proxy
-      {{ if .Values.cni.install }}
-      - name: install-cni-binary
-        image: {{ include "cilium.image" .Values.image | quote }}
-        imagePullPolicy: {{ .Values.image.pullPolicy }}
-        securityContext:
-          privileged: true
-        command:
-          - "/cni-install.sh"
-        terminationMessagePolicy: FallbackToLogsOnError
-        volumeMounts:
-        - name: cni-path
-          mountPath: /host/opt/cni/bin
-      {{- end }} # install-cni-binary
       restartPolicy: Always
       priorityClassName: {{ include "cilium.priorityClass" (list $ .Values.priorityClassName "system-node-critical") }}
       serviceAccount: {{ .Values.serviceAccounts.cilium.name | quote }}

install/kubernetes/cilium/templates/cilium-configmap.yaml
@@ -244,6 +244,12 @@ data:
   clean-cilium-bpf-state: "true"
 {{- end }}
 
+{{- if hasKey .Values.cni "customConf" }}
+  # Users who wish to specify their own custom CNI configuration file must set
+  # custom-cni-conf to "true", otherwise Cilium may overwrite the configuration.
+  custom-cni-conf: "{{ .Values.cni.customConf }}"
+{{- end }}
+
 {{- if hasKey .Values "bpfClockProbe" }}
   enable-bpf-clock-probe: {{ .Values.bpfClockProbe | quote }}
 {{- else if eq $defaultBpfClockProbe "true" }}
@@ -438,6 +444,16 @@ data:
 {{- end }}
 
 {{- if ne .Values.cni.chainingMode "none" }}
+  # Enable chaining with another CNI plugin
+  #
+  # Supported modes:
+  #  - none
+  #  - aws-cni
+  #  - flannel
+  #  - generic-veth
+  #  - portmap (Enables HostPort support for Cilium)
+  cni-chaining-mode: {{ .Values.cni.chainingMode }}
+
 {{- if hasKey .Values "enableIdentityMark" }}
   enable-identity-mark: {{ .Values.enableIdentityMark | quote }}
 {{- else if (ne $enableIdentityMark "true") }}
@@ -670,36 +686,12 @@ data:
 {{- if and .Values.endpointRoutes .Values.endpointRoutes.enabled }}
   enable-endpoint-routes: {{ .Values.endpointRoutes.enabled | quote }}
 {{- end }}
-
-  ## CNI configuration
-{{- if and .Values.cni.install  (not .Values.cni.customConf)  }}
-  # direct Cilium agent to write a CNI configuration to disk on startup
-  write-cni-conf-when-ready: {{ print .Values.cni.hostConfDirMountPath "/05-cilium.conflist" | quote }}
-
-{{- if hasKey .Values.cni "configMap" }}
-  # user-supplied CNI configuration via a ConfigMap
+{{- if .Values.cni.configMap }}
   read-cni-conf: {{ .Values.cni.confFileMountPath }}/{{ .Values.cni.configMapKey }}
-{{- else if hasKey .Values.cni "readCniConf" }}
-  # user-supplied CNI configuration from disk
+  write-cni-conf-when-ready: {{ .Values.cni.hostConfDirMountPath }}/05-cilium.conflist
+{{- else if .Values.cni.readCniConf }}
   read-cni-conf: {{ .Values.cni.readCniConf }}
 {{- end }}
-
-  # Enable chaining with another CNI plugin
-  #
-  # Supported modes:
-  #  - none
-  #  - aws-cni
-  #  - flannel
-  #  - generic-veth
-  #  - portmap (Enables HostPort support for Cilium)
-  cni-chaining-mode: {{ .Values.cni.chainingMode | quote }}
-{{- if .Values.cni.logFile }}
-  cni-log-file: {{ .Values.cni.logFile}}
-{{- end }}
-  # if cni-exclusive is set to true, Cilium will remove any pre-existing CNI configurations
-  cni-exclusive: {{ .Values.cni.exclusive | quote}}
-{{- end }}
-
 {{- if .Values.kubeConfigPath }}
   k8s-kubeconfig-path: {{ .Values.kubeConfigPath | quote }}
 {{- end }}

install/kubernetes/cilium/values.yaml
@@ -465,7 +465,7 @@ cni:
   # This can be useful if you want to manage your CNI
   # configuration outside of a Kubernetes environment. This parameter is
   # mutually exclusive with the 'cni.configMap' parameter.
-  # readCniConf: /host/etc/cni/net.d/05-cilium.conf.template
+  # readCniConf: /host/etc/cni/net.d/05-cilium.conf
 
   # -- When defined, configMap will mount the provided value as ConfigMap and
   # interpret the cniConf variable as CNI configuration file and write it

install/kubernetes/cilium/values.yaml.tmpl
@@ -462,7 +462,7 @@ cni:
   # This can be useful if you want to manage your CNI
   # configuration outside of a Kubernetes environment. This parameter is
   # mutually exclusive with the 'cni.configMap' parameter.
-  # readCniConf: /host/etc/cni/net.d/05-cilium.conf.template
+  # readCniConf: /host/etc/cni/net.d/05-cilium.conf
 
   # -- When defined, configMap will mount the provided value as ConfigMap and
   # interpret the cniConf variable as CNI configuration file and write it

plugins/cilium-cni/cni-install.sh
@@ -1,30 +1,257 @@
 #!/bin/bash
 
-# This script copies over the cilium-cni and localhost CNI plugin binaries
-# to the host.
-
 set -e
 
+HOST_PREFIX=${HOST_PREFIX:-/host}
+
+case "$CILIUM_CNI_CHAINING_MODE" in
+"flannel")
+	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conflist}
+	;;
+"generic-veth")
+	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conflist}
+	;;
+"portmap")
+	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conflist}
+	;;
+"aws-cni")
+	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conflist}
+	;;
+*)
+	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conf}
+	;;
+esac
+
+ENABLE_DEBUG=false
+CNI_EXCLUSIVE=true
+LOG_FILE="/var/run/cilium/cilium-cni.log"
+
+while test $# -gt 0; do
+  case "$1" in
+    --enable-debug*)
+      # shellcheck disable=SC2001
+      # the below sed is to support both formats "--flag value" and "--flag=value"
+      ENABLE_DEBUG=$(echo "$1" | sed -e 's/^[^=]*=//g')
+      shift
+      ;;
+    --cni-exclusive*)
+      # shellcheck disable=SC2001
+      # the below sed is to support both formats "--flag value" and "--flag=value"
+      CNI_EXCLUSIVE=$(echo "$1" | sed -e 's/^[^=]*=//g')
+      shift
+      ;;
+    --log-file*)
+      # shellcheck disable=SC2001
+      # the below sed is to support both formats "--flag value" and "--flag=value"
+      LOG_FILE=$(echo "$1" | sed -e 's/^[^=]*=//g')
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
 BIN_NAME=cilium-cni
-CNI_BIN_DIR=/host/opt/cni/bin
+CNI_DIR=${CNI_DIR:-${HOST_PREFIX}/opt/cni}
+CILIUM_CNI_CONF=${CILIUM_CNI_CONF:-${HOST_PREFIX}/etc/cni/net.d/${CNI_CONF_NAME}}
+CNI_CONF_DIR="$(dirname "$CILIUM_CNI_CONF")"
+CILIUM_CUSTOM_CNI_CONF=${CILIUM_CUSTOM_CNI_CONF:-false}
 
-function atomic_cp {
-  local src="$1"
-  local dst="$2"
-
-  cp "${src}" "${dst}.tmp"
-  mv "${dst}.tmp" "${dst}"
-}
+if [ ! -d "${CNI_DIR}/bin" ]; then
+	mkdir -p "${CNI_DIR}/bin"
+fi
 
 # Install the CNI loopback driver if not installed already
-if [ ! -f "${CNI_BIN_DIR}/loopback" ]; then
+if [ ! -f "${CNI_DIR}/bin/loopback" ]; then
 	echo "Installing loopback driver..."
 
 	# Don't fail hard if this fails as it is usually not required
-	cp /cni/loopback "${CNI_BIN_DIR}/loopback" || true
+	cp /cni/loopback "${CNI_DIR}/bin/" || true
 fi
 
-echo "Installing ${BIN_NAME} to ${CNI_BIN_DIR} ..."
+echo "Installing ${BIN_NAME} to ${CNI_DIR}/bin/ ..."
 
+# Move an eventual old existing binary out of the way, we can't delete it
+# as it might be in use right now.
+if [ -f "${CNI_DIR}/bin/${BIN_NAME}" ]; then
+	rm -f "${CNI_DIR}/bin/${BIN_NAME}.old" || true
+	mv "${CNI_DIR}/bin/${BIN_NAME}" "${CNI_DIR}/bin/${BIN_NAME}.old"
+fi
 
-atomic_cp "/opt/cni/bin/${BIN_NAME}" "${CNI_BIN_DIR}/${BIN_NAME}"
+cp "/opt/cni/bin/${BIN_NAME}" "${CNI_DIR}/bin/"
+
+# The CILIUM_CUSTOM_CNI_CONF env is set by the `cni.customConf` Helm option.
+# It stops this script from touching the host's CNI config directory.
+# However, the agent will still write to the location specified by the
+# `--write-cni-conf-when-ready` flag when `cni.configMap` is set.
+if [ "${CILIUM_CUSTOM_CNI_CONF}" == "true" ]; then
+	echo "User is managing Cilium's CNI config externally, exiting..."
+	exit 0
+fi
+
+# Remove any active Cilium CNI configurations left over from previous installs
+# to make sure the one we're installing later will take effect.
+# Ignore the file specified by CNI_CONF_NAME. The agent will use this
+# filename to write a user-specified CNI config and races against this script.
+echo "Removing active Cilium CNI configurations from ${CNI_CONF_DIR}})..."
+find "${CNI_CONF_DIR}" -maxdepth 1 -type f \
+  -name '*cilium*' -and \( \
+    -name '*.conf' -or \
+    -name '*.conflist' \
+  \) \
+  -not -name "${CNI_CONF_NAME}" \
+  -delete
+
+# Check that a CNI configuration already exists in certain chaining modes.
+# Exit if it does not - provoking a restart later on. This is required to
+# wait until the main CNI has been initialized, e.g. by another pod.
+# *.cilium_bak files are also accepted in case this script already ran on
+# the host.
+case "$CILIUM_CNI_CHAINING_MODE" in
+"aws-cni")
+  tries=30
+  while [[ $tries -gt 0 ]]; do
+  if test -n "$(find "${CNI_CONF_DIR}" -maxdepth 1 -type f -name '*.conf' -or -name '*.conflist' -or -name '*.cilium_bak' )"; then
+      echo "Found existing CNI config for chaining"
+      break
+    else
+      echo "Could not find existing AWS VPC CNI config for chaining, will retry..."
+      tries=$((tries-1))
+      sleep 5
+    fi
+  done
+  if [[ $tries -eq 0 ]]; then
+    echo "Existing CNI config is required for chaining but does not exist yet, exiting..."
+    exit 1
+  fi
+  ;;
+*)
+  echo "Existing CNI config is not required"
+  ;;
+esac
+
+# Rename all remaining CNI configurations to *.cilium_bak. This ensures only
+# Cilium's CNI plugin will remain active. This makes sure Pods are not
+# scheduled by another CNI when the Cilium agent is down during upgrades
+# or restarts. See GH-14128 and related issues for more context.
+if [ "${CNI_EXCLUSIVE}" != "false" ]; then
+  find "$(dirname "${CILIUM_CNI_CONF}")" \
+     -maxdepth 1 \
+     -type f \
+     \( -name '*.conf' \
+     -or -name '*.conflist' \
+     -or -name '*.json' \
+     \) \
+     -not \( -name '*.cilium_bak' \
+     -or -name "${CNI_CONF_NAME}" \) \
+     -exec mv {} {}.cilium_bak \;
+fi
+
+echo "Installing new ${CILIUM_CNI_CONF}..."
+case "$CILIUM_CNI_CHAINING_MODE" in
+"flannel")
+	cat > "${CNI_CONF_NAME}" <<EOF
+{
+  "cniVersion": "0.3.1",
+  "name": "flannel",
+  "plugins": [
+    {
+      "type": "flannel",
+      "delegate": {
+         "hairpinMode": true,
+         "isDefaultGateway": true
+      }
+    },
+    {
+      "type": "portmap",
+      "capabilities": {
+        "portMappings": true
+      }
+    },
+    {
+       "name": "cilium",
+       "type": "cilium-cni",
+       "enable-debug": ${ENABLE_DEBUG},
+       "log-file": "${LOG_FILE}"
+    }
+  ]
+}
+EOF
+	;;
+
+"portmap")
+	cat > "${CNI_CONF_NAME}" <<EOF
+{
+  "cniVersion": "0.3.1",
+  "name": "portmap",
+  "plugins": [
+    {
+       "name": "cilium",
+       "type": "cilium-cni",
+       "enable-debug": ${ENABLE_DEBUG},
+       "log-file": "${LOG_FILE}"
+    },
+    {
+      "type": "portmap",
+      "capabilities": {"portMappings": true}
+    }
+  ]
+}
+EOF
+	;;
+
+"aws-cni")
+  if [ -f "${CNI_CONF_DIR}/10-aws.conflist.cilium_bak" ]; then
+    jq --argjson 'ENABLE_DEBUG' "$ENABLE_DEBUG" --arg 'LOG_FILE' "$LOG_FILE" \
+          '.plugins += [{"name": "cilium", "type": "cilium-cni", "enable-debug": $ENABLE_DEBUG, "log-file": $LOG_FILE}]' \
+          "${CNI_CONF_DIR}/10-aws.conflist.cilium_bak" > "${CNI_CONF_NAME}"
+  else
+    cat > "${CNI_CONF_NAME}" <<EOF
+{
+  "cniVersion": "0.3.1",
+  "name": "aws-cni",
+  "plugins": [
+    {
+      "name": "aws-cni",
+      "type": "aws-cni",
+      "vethPrefix": "eni",
+      "mtu": "9001",
+      "pluginLogFile": "/var/log/aws-routed-eni/plugin.log",
+      "pluginLogLevel": "DEBUG"
+    },
+    {
+      "type": "portmap",
+      "capabilities": {"portMappings": true},
+      "snat": true
+    },
+    {
+       "name": "cilium",
+       "type": "cilium-cni",
+       "enable-debug": ${ENABLE_DEBUG},
+       "log-file": "${LOG_FILE}"
+    }
+  ]
+}
+EOF
+  fi
+  ;;
+
+*)
+	cat > "${CNI_CONF_NAME}" <<EOF
+{
+  "cniVersion": "0.3.1",
+  "name": "cilium",
+  "type": "cilium-cni",
+  "enable-debug": ${ENABLE_DEBUG},
+  "log-file": "${LOG_FILE}"
+}
+EOF
+	;;
+esac
+
+if [ ! -d "$(dirname "$CILIUM_CNI_CONF")" ]; then
+	mkdir -p "$(dirname "$CILIUM_CNI_CONF")"
+fi
+
+mv "${CNI_CONF_NAME}" "${CILIUM_CNI_CONF}"

commit 0479e9037c2d4095315b84ae0a28f2fac0eaf450

install: move cni config management to the agent (try 2)
This commit makes use of the logic added to the agent to manage the CNI
configuration file. The "cni-uninstall.sh" script is no longer
necessary, so we can just remove that. The cni-install.sh script only
does binary installation now.

It plumbs some helm variables a bit differently, but shouldn't change
any behavior.

This commit fixes a small issue found in the original commit (which was
reverted).

Fixes: fe64c7f02
Fixes: 0d6346dbe

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

images/cilium/Dockerfile
@@ -60,7 +60,6 @@ RUN --mount=type=bind,readwrite,target=/go/src/github.com/cilium/cilium --mount=
 
 COPY images/cilium/init-container.sh \
      plugins/cilium-cni/cni-install.sh \
-     plugins/cilium-cni/cni-uninstall.sh \
        /tmp/install/${TARGETOS}/${TARGETARCH}
 
 #

install/kubernetes/cilium/templates/cilium-agent/daemonset.yaml
@@ -188,18 +188,6 @@ spec:
               fieldPath: metadata.namespace
         - name: CILIUM_CLUSTERMESH_CONFIG
           value: /var/lib/cilium/clustermesh/
-        - name: CILIUM_CNI_CHAINING_MODE
-          valueFrom:
-            configMapKeyRef:
-              name: cilium-config
-              key: cni-chaining-mode
-              optional: true
-        - name: CILIUM_CUSTOM_CNI_CONF
-          valueFrom:
-            configMapKeyRef:
-              name: cilium-config
-              key: custom-cni-conf
-              optional: true
         {{- if .Values.k8sServiceHost }}
         - name: KUBERNETES_SERVICE_HOST
           value: {{ .Values.k8sServiceHost | quote }}
@@ -210,20 +198,6 @@ spec:
         {{- end }}
         {{- with .Values.extraEnv }}
         {{- toYaml . | trim | nindent 8 }}
-        {{- end }}
-        {{- if .Values.cni.install }}
-        lifecycle:
-          postStart:
-            exec:
-              command:
-              - "/cni-install.sh"
-              - "--enable-debug={{ .Values.debug.enabled }}"
-              - "--cni-exclusive={{ .Values.cni.exclusive }}"
-              - "--log-file={{ .Values.cni.logFile }}"
-          preStop:
-            exec:
-              command:
-              - /cni-uninstall.sh
         {{- end }}
         {{- with .Values.resources }}
         resources:
@@ -664,6 +638,19 @@ spec:
             done
         terminationMessagePolicy: FallbackToLogsOnError
       {{- end }} # wait-for-kube-proxy
+      {{ if .Values.cni.install }}
+      - name: install-cni-binary
+        image: {{ include "cilium.image" .Values.image | quote }}
+        imagePullPolicy: {{ .Values.image.pullPolicy }}
+        securityContext:
+          privileged: true
+        command:
+          - "/cni-install.sh"
+        terminationMessagePolicy: FallbackToLogsOnError
+        volumeMounts:
+        - name: cni-path
+          mountPath: /host/opt/cni/bin
+      {{- end }} # install-cni-binary
       restartPolicy: Always
       priorityClassName: {{ include "cilium.priorityClass" (list $ .Values.priorityClassName "system-node-critical") }}
       serviceAccount: {{ .Values.serviceAccounts.cilium.name | quote }}

install/kubernetes/cilium/templates/cilium-configmap.yaml
@@ -256,12 +256,6 @@ data:
   clean-cilium-bpf-state: "true"
 {{- end }}
 
-{{- if hasKey .Values.cni "customConf" }}
-  # Users who wish to specify their own custom CNI configuration file must set
-  # custom-cni-conf to "true", otherwise Cilium may overwrite the configuration.
-  custom-cni-conf: "{{ .Values.cni.customConf }}"
-{{- end }}
-
 {{- if hasKey .Values "bpfClockProbe" }}
   enable-bpf-clock-probe: {{ .Values.bpfClockProbe | quote }}
 {{- else if eq $defaultBpfClockProbe "true" }}
@@ -460,16 +454,6 @@ data:
 {{- end }}
 
 {{- if ne .Values.cni.chainingMode "none" }}
-  # Enable chaining with another CNI plugin
-  #
-  # Supported modes:
-  #  - none
-  #  - aws-cni
-  #  - flannel
-  #  - generic-veth
-  #  - portmap (Enables HostPort support for Cilium)
-  cni-chaining-mode: {{ .Values.cni.chainingMode }}
-
 {{- if hasKey .Values "enableIdentityMark" }}
   enable-identity-mark: {{ .Values.enableIdentityMark | quote }}
 {{- else if (ne $enableIdentityMark "true") }}
@@ -701,12 +685,41 @@ data:
 {{- if and .Values.endpointRoutes .Values.endpointRoutes.enabled }}
   enable-endpoint-routes: {{ .Values.endpointRoutes.enabled | quote }}
 {{- end }}
-{{- if .Values.cni.configMap }}
+
+  ## CNI configuration
+{{- if .Values.cni.install }}
+
+# cni.customConf disables CNI file management **except** if cni.configMap is set
+{{- if or (not .Values.cni.customConf) (hasKey .Values.cni "configMap") }}
+  # direct Cilium agent to write a CNI configuration to disk on startup
+  write-cni-conf-when-ready: {{ print .Values.cni.hostConfDirMountPath "/05-cilium.conflist" | quote }}
+{{- end }}
+
+{{- if hasKey .Values.cni "configMap" }}
+  # user-supplied CNI configuration via a ConfigMap
   read-cni-conf: {{ .Values.cni.confFileMountPath }}/{{ .Values.cni.configMapKey }}
-  write-cni-conf-when-ready: {{ .Values.cni.hostConfDirMountPath }}/05-cilium.conflist
-{{- else if .Values.cni.readCniConf }}
+{{- else if hasKey .Values.cni "readCniConf" }}
+  # user-supplied CNI configuration from disk
   read-cni-conf: {{ .Values.cni.readCniConf }}
 {{- end }}
+
+  # Enable chaining with another CNI plugin
+  #
+  # Supported modes:
+  #  - none
+  #  - aws-cni
+  #  - flannel
+  #  - generic-veth
+  #  - portmap (Enables HostPort support for Cilium)
+  cni-chaining-mode: {{ .Values.cni.chainingMode | quote }}
+{{- if .Values.cni.logFile }}
+  cni-log-file: {{ .Values.cni.logFile }}
+{{- end }}
+  # if cni-exclusive is set to true, Cilium will remove any pre-existing CNI configurations
+  cni-exclusive: {{ .Values.cni.exclusive | quote}}
+{{- end }}
+  ## end CNI configuration
+
 {{- if .Values.kubeConfigPath }}
   k8s-kubeconfig-path: {{ .Values.kubeConfigPath | quote }}
 {{- end }}

install/kubernetes/cilium/values.yaml
@@ -465,7 +465,7 @@ cni:
   # This can be useful if you want to manage your CNI
   # configuration outside of a Kubernetes environment. This parameter is
   # mutually exclusive with the 'cni.configMap' parameter.
-  # readCniConf: /host/etc/cni/net.d/05-cilium.conf
+  # readCniConf: /host/etc/cni/net.d/05-cilium.conf.template
 
   # -- When defined, configMap will mount the provided value as ConfigMap and
   # interpret the cniConf variable as CNI configuration file and write it

install/kubernetes/cilium/values.yaml.tmpl
@@ -462,7 +462,7 @@ cni:
   # This can be useful if you want to manage your CNI
   # configuration outside of a Kubernetes environment. This parameter is
   # mutually exclusive with the 'cni.configMap' parameter.
-  # readCniConf: /host/etc/cni/net.d/05-cilium.conf
+  # readCniConf: /host/etc/cni/net.d/05-cilium.conf.template
 
   # -- When defined, configMap will mount the provided value as ConfigMap and
   # interpret the cniConf variable as CNI configuration file and write it

plugins/cilium-cni/cni-install.sh
@@ -1,257 +1,30 @@
 #!/bin/bash
 
+# This script copies over the cilium-cni and localhost CNI plugin binaries
+# to the host.
+
 set -e
 
-HOST_PREFIX=${HOST_PREFIX:-/host}
-
-case "$CILIUM_CNI_CHAINING_MODE" in
-"flannel")
-	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conflist}
-	;;
-"generic-veth")
-	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conflist}
-	;;
-"portmap")
-	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conflist}
-	;;
-"aws-cni")
-	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conflist}
-	;;
-*)
-	CNI_CONF_NAME=${CNI_CONF_NAME:-05-cilium.conf}
-	;;
-esac
-
-ENABLE_DEBUG=false
-CNI_EXCLUSIVE=true
-LOG_FILE="/var/run/cilium/cilium-cni.log"
-
-while test $# -gt 0; do
-  case "$1" in
-    --enable-debug*)
-      # shellcheck disable=SC2001
-      # the below sed is to support both formats "--flag value" and "--flag=value"
-      ENABLE_DEBUG=$(echo "$1" | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --cni-exclusive*)
-      # shellcheck disable=SC2001
-      # the below sed is to support both formats "--flag value" and "--flag=value"
-      CNI_EXCLUSIVE=$(echo "$1" | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --log-file*)
-      # shellcheck disable=SC2001
-      # the below sed is to support both formats "--flag value" and "--flag=value"
-      LOG_FILE=$(echo "$1" | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    *)
-      break
-      ;;
-  esac
-done
-
 BIN_NAME=cilium-cni
-CNI_DIR=${CNI_DIR:-${HOST_PREFIX}/opt/cni}
-CILIUM_CNI_CONF=${CILIUM_CNI_CONF:-${HOST_PREFIX}/etc/cni/net.d/${CNI_CONF_NAME}}
-CNI_CONF_DIR="$(dirname "$CILIUM_CNI_CONF")"
-CILIUM_CUSTOM_CNI_CONF=${CILIUM_CUSTOM_CNI_CONF:-false}
+CNI_BIN_DIR=/host/opt/cni/bin
 
-if [ ! -d "${CNI_DIR}/bin" ]; then
-	mkdir -p "${CNI_DIR}/bin"
-fi
+function atomic_cp {
+  local src="$1"
+  local dst="$2"
+
+  cp "${src}" "${dst}.tmp"
+  mv "${dst}.tmp" "${dst}"
+}
 
 # Install the CNI loopback driver if not installed already
-if [ ! -f "${CNI_DIR}/bin/loopback" ]; then
+if [ ! -f "${CNI_BIN_DIR}/loopback" ]; then
 	echo "Installing loopback driver..."
 
 	# Don't fail hard if this fails as it is usually not required
-	cp /cni/loopback "${CNI_DIR}/bin/" || true
+	cp /cni/loopback "${CNI_BIN_DIR}/loopback" || true
 fi
 
-echo "Installing ${BIN_NAME} to ${CNI_DIR}/bin/ ..."
+echo "Installing ${BIN_NAME} to ${CNI_BIN_DIR} ..."
 
-# Move an eventual old existing binary out of the way, we can't delete it
-# as it might be in use right now.
-if [ -f "${CNI_DIR}/bin/${BIN_NAME}" ]; then
-	rm -f "${CNI_DIR}/bin/${BIN_NAME}.old" || true
-	mv "${CNI_DIR}/bin/${BIN_NAME}" "${CNI_DIR}/bin/${BIN_NAME}.old"
-fi
 
-cp "/opt/cni/bin/${BIN_NAME}" "${CNI_DIR}/bin/"
-
-# The CILIUM_CUSTOM_CNI_CONF env is set by the `cni.customConf` Helm option.
-# It stops this script from touching the host's CNI config directory.
-# However, the agent will still write to the location specified by the
-# `--write-cni-conf-when-ready` flag when `cni.configMap` is set.
-if [ "${CILIUM_CUSTOM_CNI_CONF}" == "true" ]; then
-	echo "User is managing Cilium's CNI config externally, exiting..."
-	exit 0
-fi
-
-# Remove any active Cilium CNI configurations left over from previous installs
-# to make sure the one we're installing later will take effect.
-# Ignore the file specified by CNI_CONF_NAME. The agent will use this
-# filename to write a user-specified CNI config and races against this script.
-echo "Removing active Cilium CNI configurations from ${CNI_CONF_DIR}})..."
-find "${CNI_CONF_DIR}" -maxdepth 1 -type f \
-  -name '*cilium*' -and \( \
-    -name '*.conf' -or \
-    -name '*.conflist' \
-  \) \
-  -not -name "${CNI_CONF_NAME}" \
-  -delete
-
-# Check that a CNI configuration already exists in certain chaining modes.
-# Exit if it does not - provoking a restart later on. This is required to
-# wait until the main CNI has been initialized, e.g. by another pod.
-# *.cilium_bak files are also accepted in case this script already ran on
-# the host.
-case "$CILIUM_CNI_CHAINING_MODE" in
-"aws-cni")
-  tries=30
-  while [[ $tries -gt 0 ]]; do
-  if test -n "$(find "${CNI_CONF_DIR}" -maxdepth 1 -type f -name '*.conf' -or -name '*.conflist' -or -name '*.cilium_bak' )"; then
-      echo "Found existing CNI config for chaining"
-      break
-    else
-      echo "Could not find existing AWS VPC CNI config for chaining, will retry..."
-      tries=$((tries-1))
-      sleep 5
-    fi
-  done
-  if [[ $tries -eq 0 ]]; then
-    echo "Existing CNI config is required for chaining but does not exist yet, exiting..."
-    exit 1
-  fi
-  ;;
-*)
-  echo "Existing CNI config is not required"
-  ;;
-esac
-
-# Rename all remaining CNI configurations to *.cilium_bak. This ensures only
-# Cilium's CNI plugin will remain active. This makes sure Pods are not
-# scheduled by another CNI when the Cilium agent is down during upgrades
-# or restarts. See GH-14128 and related issues for more context.
-if [ "${CNI_EXCLUSIVE}" != "false" ]; then
-  find "$(dirname "${CILIUM_CNI_CONF}")" \
-     -maxdepth 1 \
-     -type f \
-     \( -name '*.conf' \
-     -or -name '*.conflist' \
-     -or -name '*.json' \
-     \) \
-     -not \( -name '*.cilium_bak' \
-     -or -name "${CNI_CONF_NAME}" \) \
-     -exec mv {} {}.cilium_bak \;
-fi
-
-echo "Installing new ${CILIUM_CNI_CONF}..."
-case "$CILIUM_CNI_CHAINING_MODE" in
-"flannel")
-	cat > "${CNI_CONF_NAME}" <<EOF
-{
-  "cniVersion": "0.3.1",
-  "name": "flannel",
-  "plugins": [
-    {
-      "type": "flannel",
-      "delegate": {
-         "hairpinMode": true,
-         "isDefaultGateway": true
-      }
-    },
-    {
-      "type": "portmap",
-      "capabilities": {
-        "portMappings": true
-      }
-    },
-    {
-       "name": "cilium",
-       "type": "cilium-cni",
-       "enable-debug": ${ENABLE_DEBUG},
-       "log-file": "${LOG_FILE}"
-    }
-  ]
-}
-EOF
-	;;
-
-"portmap")
-	cat > "${CNI_CONF_NAME}" <<EOF
-{
-  "cniVersion": "0.3.1",
-  "name": "portmap",
-  "plugins": [
-    {
-       "name": "cilium",
-       "type": "cilium-cni",
-       "enable-debug": ${ENABLE_DEBUG},
-       "log-file": "${LOG_FILE}"
-    },
-    {
-      "type": "portmap",
-      "capabilities": {"portMappings": true}
-    }
-  ]
-}
-EOF
-	;;
-
-"aws-cni")
-  if [ -f "${CNI_CONF_DIR}/10-aws.conflist.cilium_bak" ]; then
-    jq --argjson 'ENABLE_DEBUG' "$ENABLE_DEBUG" --arg 'LOG_FILE' "$LOG_FILE" \
-          '.plugins += [{"name": "cilium", "type": "cilium-cni", "enable-debug": $ENABLE_DEBUG, "log-file": $LOG_FILE}]' \
-          "${CNI_CONF_DIR}/10-aws.conflist.cilium_bak" > "${CNI_CONF_NAME}"
-  else
-    cat > "${CNI_CONF_NAME}" <<EOF
-{
-  "cniVersion": "0.3.1",
-  "name": "aws-cni",
-  "plugins": [
-    {
-      "name": "aws-cni",
-      "type": "aws-cni",
-      "vethPrefix": "eni",
-      "mtu": "9001",
-      "pluginLogFile": "/var/log/aws-routed-eni/plugin.log",
-      "pluginLogLevel": "DEBUG"
-    },
-    {
-      "type": "portmap",
-      "capabilities": {"portMappings": true},
-      "snat": true
-    },
-    {
-       "name": "cilium",
-       "type": "cilium-cni",
-       "enable-debug": ${ENABLE_DEBUG},
-       "log-file": "${LOG_FILE}"
-    }
-  ]
-}
-EOF
-  fi
-  ;;
-
-*)
-	cat > "${CNI_CONF_NAME}" <<EOF
-{
-  "cniVersion": "0.3.1",
-  "name": "cilium",
-  "type": "cilium-cni",
-  "enable-debug": ${ENABLE_DEBUG},
-  "log-file": "${LOG_FILE}"
-}
-EOF
-	;;
-esac
-
-if [ ! -d "$(dirname "$CILIUM_CNI_CONF")" ]; then
-	mkdir -p "$(dirname "$CILIUM_CNI_CONF")"
-fi
-
-mv "${CNI_CONF_NAME}" "${CILIUM_CNI_CONF}"
+atomic_cp "/opt/cni/bin/${BIN_NAME}" "${CNI_BIN_DIR}/${BIN_NAME}"

plugins/cilium-cni/cni-uninstall.sh
@@ -1,28 +1,0 @@
-#!/bin/bash
-
-set -e
-
-HOST_PREFIX=${HOST_PREFIX:-/host}
-BIN_NAME=cilium-cni
-CNI_DIR=${CNI_DIR:-${HOST_PREFIX}/opt/cni}
-CNI_CONF_DIR=${CNI_CONF_DIR:-${HOST_PREFIX}/etc/cni/net.d}
-CILIUM_CUSTOM_CNI_CONF=${CILIUM_CUSTOM_CNI_CONF:-false}
-
-# Do not interact with the host's CNI directory when the user specified they
-# are managing CNI configs externally.
-if [ "${CILIUM_CUSTOM_CNI_CONF}" != "true" ]; then
-    # .conf/.conflist/.json (undocumented) are read by kubelet/dockershim's CNI implementation.
-    # Remove any active Cilium CNI configurations to prevent scheduling Pods during agent
-    # downtime. Configs belonging to other CNI implementations have already been renamed
-    # to *.cilium_bak during agent startup.
-    echo "Removing active Cilium CNI configurations from ${CNI_CONF_DIR}..."
-    find "${CNI_CONF_DIR}" -maxdepth 1 -type f \
-    -name '*cilium*' -and \( \
-        -name '*.conf' -or \
-        -name '*.conflist' \
-    \) -delete
-fi
-
-echo "Removing ${CNI_DIR}/bin/cilium-cni..."
-rm -f "${CNI_DIR}/bin/${BIN_NAME}"
-rm -f "${CNI_DIR}/bin/${BIN_NAME}.old"