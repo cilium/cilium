a38fa327f99fe3417585053e1a3d81f3a016e4ed
Dylan Reimerink <dylan.reimerink@isovalent.com>
2023-01-20 15:45:34 +0100

    vendor: Bumped CoverBee to v0.3.0 and cilium/ebpf to v0.10.0
    Updated CoverBee to v0.3.0, this version is dependent on
    cilum/ebpf@v0.10.0 so this PR bumps it as well so everything will
    still compile.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- vendor/github.com/cilium/ebpf/features/map.go
+++ vendor/github.com/cilium/ebpf/features/map.go
@@ -2,166 +2,228 @@ package features
 
 import (
 	"errors"
+	"fmt"
 	"os"
-	"sync"
 	"unsafe"
 
 	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/internal"
 	"github.com/cilium/ebpf/internal/sys"
 	"github.com/cilium/ebpf/internal/unix"
 )
 
-func init() {
-	mc.mapTypes = make(map[ebpf.MapType]error)
-	mc.mapFlags = make(map[MapFlags]error)
-}
-
-var (
-	mc mapCache
-)
-
-type mapCache struct {
-	sync.Mutex
-	mapTypes map[ebpf.MapType]error
-	mapFlags map[MapFlags]error
-}
-
-func createMapTypeAttr(mt ebpf.MapType) *sys.MapCreateAttr {
-	a := &sys.MapCreateAttr{
-		MapType:    sys.MapType(mt),
-		KeySize:    4,
-		ValueSize:  4,
-		MaxEntries: 1,
-	}
-
-	// switch on map types to generate correct MapCreateAttr
-	switch mt {
-	case ebpf.StackTrace:
-		// valueSize needs to be sizeof(uint64)
-		a.ValueSize = 8
-	case ebpf.LPMTrie:
-		// keySize and valueSize need to be sizeof(struct{u32 + u8}) + 1 + padding = 8
-		// BPF_F_NO_PREALLOC needs to be set
-		// checked at allocation time for lpm_trie maps
-		a.KeySize = 8
-		a.ValueSize = 8
-		a.MapFlags = unix.BPF_F_NO_PREALLOC
-	case ebpf.ArrayOfMaps, ebpf.HashOfMaps:
-		// assign invalid innerMapFd to pass validation check
-		// will return EBADF
-		a.InnerMapFd = ^uint32(0)
-	case ebpf.CGroupStorage, ebpf.PerCPUCGroupStorage:
-		// keySize needs to be sizeof(struct{u32 + u64}) = 12 (+ padding = 16)
-		// by using unsafe.Sizeof(int) we are making sure that this works on 32bit and 64bit archs
-		// checked at allocation time
-		var align int
-		a.KeySize = uint32(8 + unsafe.Sizeof(align))
-		a.MaxEntries = 0
-	case ebpf.Queue, ebpf.Stack:
-		// keySize needs to be 0, see alloc_check for queue and stack maps
-		a.KeySize = 0
-	case ebpf.RingBuf:
-		// keySize and valueSize need to be 0
-		// maxEntries needs to be power of 2 and PAGE_ALIGNED
-		// checked at allocation time
-		a.KeySize = 0
-		a.ValueSize = 0
-		a.MaxEntries = uint32(os.Getpagesize())
-	case ebpf.SkStorage, ebpf.InodeStorage, ebpf.TaskStorage:
-		// maxEntries needs to be 0
-		// BPF_F_NO_PREALLOC needs to be set
-		// btf* fields need to be set
-		// see alloc_check for local_storage map types
-		a.MaxEntries = 0
-		a.MapFlags = unix.BPF_F_NO_PREALLOC
-		a.BtfKeyTypeId = 1   // BTF_KIND_INT
-		a.BtfValueTypeId = 3 // BTF_KIND_ARRAY
-		a.BtfFd = ^uint32(0)
-	case ebpf.StructOpsMap:
-		// StructOps requires setting a vmlinux type id, but id 1 will always
-		// resolve to some type of integer. This will cause ENOTSUPP.
-		a.BtfVmlinuxValueTypeId = 1
-	}
-
-	return a
-}
-
 // HaveMapType probes the running kernel for the availability of the specified map type.
 //
 // See the package documentation for the meaning of the error return value.
-func HaveMapType(mt ebpf.MapType) (err error) {
-	defer func() {
-		// This closure modifies a named return variable.
-		err = wrapProbeErrors(err)
-	}()
-
-	if err := validateMaptype(mt); err != nil {
-		return err
-	}
-
-	return haveMapType(mt)
+func HaveMapType(mt ebpf.MapType) error {
+	return haveMapTypeMatrix.Result(mt)
 }
 
-func validateMaptype(mt ebpf.MapType) error {
-	if mt > mt.Max() {
-		return os.ErrInvalid
-	}
-	return nil
+func probeCgroupStorageMap(mt sys.MapType) error {
+	// keySize needs to be sizeof(struct{u32 + u64}) = 12 (+ padding = 16)
+	// by using unsafe.Sizeof(int) we are making sure that this works on 32bit and 64bit archs
+	return createMap(&sys.MapCreateAttr{
+		MapType:    mt,
+		ValueSize:  4,
+		KeySize:    uint32(8 + unsafe.Sizeof(int(0))),
+		MaxEntries: 0,
+	})
 }
 
-func haveMapType(mt ebpf.MapType) error {
-	mc.Lock()
-	defer mc.Unlock()
-	err, ok := mc.mapTypes[mt]
-	if ok {
-		return err
+func probeStorageMap(mt sys.MapType) error {
+	// maxEntries needs to be 0
+	// BPF_F_NO_PREALLOC needs to be set
+	// btf* fields need to be set
+	// see alloc_check for local_storage map types
+	err := createMap(&sys.MapCreateAttr{
+		MapType:        mt,
+		KeySize:        4,
+		ValueSize:      4,
+		MaxEntries:     0,
+		MapFlags:       unix.BPF_F_NO_PREALLOC,
+		BtfKeyTypeId:   1,
+		BtfValueTypeId: 1,
+		BtfFd:          ^uint32(0),
+	})
+	if errors.Is(err, unix.EBADF) {
+		// Triggered by BtfFd.
+		return nil
 	}
+	return err
+}
 
-	fd, err := sys.MapCreate(createMapTypeAttr(mt))
+func probeNestedMap(mt sys.MapType) error {
+	// assign invalid innerMapFd to pass validation check
+	// will return EBADF
+	err := probeMap(&sys.MapCreateAttr{
+		MapType:    mt,
+		InnerMapFd: ^uint32(0),
+	})
+	if errors.Is(err, unix.EBADF) {
+		return nil
+	}
+	return err
+}
+
+func probeMap(attr *sys.MapCreateAttr) error {
+	if attr.KeySize == 0 {
+		attr.KeySize = 4
+	}
+	if attr.ValueSize == 0 {
+		attr.ValueSize = 4
+	}
+	attr.MaxEntries = 1
+	return createMap(attr)
+}
+
+func createMap(attr *sys.MapCreateAttr) error {
+	fd, err := sys.MapCreate(attr)
 	if err == nil {
 		fd.Close()
+		return nil
 	}
 
 	switch {
-	// For nested and storage map types we accept EBADF as indicator that these maps are supported
-	case errors.Is(err, unix.EBADF):
-		if isMapOfMaps(mt) || isStorageMap(mt) {
-			err = nil
-		}
-
-	// ENOTSUPP means the map type is at least known to the kernel.
-	case errors.Is(err, sys.ENOTSUPP):
-		if mt == ebpf.StructOpsMap {
-			err = nil
-		}
-
 	// EINVAL occurs when attempting to create a map with an unknown type.
 	// E2BIG occurs when MapCreateAttr contains non-zero bytes past the end
 	// of the struct known by the running kernel, meaning the kernel is too old
 	// to support the given map type.
 	case errors.Is(err, unix.EINVAL), errors.Is(err, unix.E2BIG):
-		err = ebpf.ErrNotSupported
+		return ebpf.ErrNotSupported
 	}
-
-	mc.mapTypes[mt] = err
 
 	return err
 }
 
-func isMapOfMaps(mt ebpf.MapType) bool {
-	switch mt {
-	case ebpf.ArrayOfMaps, ebpf.HashOfMaps:
-		return true
-	}
-	return false
+var haveMapTypeMatrix = internal.FeatureMatrix[ebpf.MapType]{
+	ebpf.Hash:           {Version: "3.19"},
+	ebpf.Array:          {Version: "3.19"},
+	ebpf.ProgramArray:   {Version: "4.2"},
+	ebpf.PerfEventArray: {Version: "4.3"},
+	ebpf.PerCPUHash:     {Version: "4.6"},
+	ebpf.PerCPUArray:    {Version: "4.6"},
+	ebpf.StackTrace: {
+		Version: "4.6",
+		Fn: func() error {
+			return probeMap(&sys.MapCreateAttr{
+				MapType:   sys.BPF_MAP_TYPE_STACK_TRACE,
+				ValueSize: 8, // sizeof(uint64)
+			})
+		},
+	},
+	ebpf.CGroupArray: {Version: "4.8"},
+	ebpf.LRUHash:     {Version: "4.10"},
+	ebpf.LRUCPUHash:  {Version: "4.10"},
+	ebpf.LPMTrie: {
+		Version: "4.11",
+		Fn: func() error {
+			// keySize and valueSize need to be sizeof(struct{u32 + u8}) + 1 + padding = 8
+			// BPF_F_NO_PREALLOC needs to be set
+			return probeMap(&sys.MapCreateAttr{
+				MapType:   sys.BPF_MAP_TYPE_LPM_TRIE,
+				KeySize:   8,
+				ValueSize: 8,
+				MapFlags:  unix.BPF_F_NO_PREALLOC,
+			})
+		},
+	},
+	ebpf.ArrayOfMaps: {
+		Version: "4.12",
+		Fn:      func() error { return probeNestedMap(sys.BPF_MAP_TYPE_ARRAY_OF_MAPS) },
+	},
+	ebpf.HashOfMaps: {
+		Version: "4.12",
+		Fn:      func() error { return probeNestedMap(sys.BPF_MAP_TYPE_HASH_OF_MAPS) },
+	},
+	ebpf.DevMap:   {Version: "4.14"},
+	ebpf.SockMap:  {Version: "4.14"},
+	ebpf.CPUMap:   {Version: "4.15"},
+	ebpf.XSKMap:   {Version: "4.18"},
+	ebpf.SockHash: {Version: "4.18"},
+	ebpf.CGroupStorage: {
+		Version: "4.19",
+		Fn:      func() error { return probeCgroupStorageMap(sys.BPF_MAP_TYPE_CGROUP_STORAGE) },
+	},
+	ebpf.ReusePortSockArray: {Version: "4.19"},
+	ebpf.PerCPUCGroupStorage: {
+		Version: "4.20",
+		Fn:      func() error { return probeCgroupStorageMap(sys.BPF_MAP_TYPE_PERCPU_CGROUP_STORAGE) },
+	},
+	ebpf.Queue: {
+		Version: "4.20",
+		Fn: func() error {
+			return createMap(&sys.MapCreateAttr{
+				MapType:    sys.BPF_MAP_TYPE_QUEUE,
+				KeySize:    0,
+				ValueSize:  4,
+				MaxEntries: 1,
+			})
+		},
+	},
+	ebpf.Stack: {
+		Version: "4.20",
+		Fn: func() error {
+			return createMap(&sys.MapCreateAttr{
+				MapType:    sys.BPF_MAP_TYPE_STACK,
+				KeySize:    0,
+				ValueSize:  4,
+				MaxEntries: 1,
+			})
+		},
+	},
+	ebpf.SkStorage: {
+		Version: "5.2",
+		Fn:      func() error { return probeStorageMap(sys.BPF_MAP_TYPE_SK_STORAGE) },
+	},
+	ebpf.DevMapHash: {Version: "5.4"},
+	ebpf.StructOpsMap: {
+		Version: "5.6",
+		Fn: func() error {
+			// StructOps requires setting a vmlinux type id, but id 1 will always
+			// resolve to some type of integer. This will cause ENOTSUPP.
+			err := probeMap(&sys.MapCreateAttr{
+				MapType:               sys.BPF_MAP_TYPE_STRUCT_OPS,
+				BtfVmlinuxValueTypeId: 1,
+			})
+			if errors.Is(err, sys.ENOTSUPP) {
+				// ENOTSUPP means the map type is at least known to the kernel.
+				return nil
+			}
+			return err
+		},
+	},
+	ebpf.RingBuf: {
+		Version: "5.8",
+		Fn: func() error {
+			// keySize and valueSize need to be 0
+			// maxEntries needs to be power of 2 and PAGE_ALIGNED
+			return createMap(&sys.MapCreateAttr{
+				MapType:    sys.BPF_MAP_TYPE_RINGBUF,
+				KeySize:    0,
+				ValueSize:  0,
+				MaxEntries: uint32(os.Getpagesize()),
+			})
+		},
+	},
+	ebpf.InodeStorage: {
+		Version: "5.10",
+		Fn:      func() error { return probeStorageMap(sys.BPF_MAP_TYPE_INODE_STORAGE) },
+	},
+	ebpf.TaskStorage: {
+		Version: "5.11",
+		Fn:      func() error { return probeStorageMap(sys.BPF_MAP_TYPE_TASK_STORAGE) },
+	},
 }
 
-func isStorageMap(mt ebpf.MapType) bool {
-	switch mt {
-	case ebpf.SkStorage, ebpf.InodeStorage, ebpf.TaskStorage:
-		return true
+func init() {
+	for mt, ft := range haveMapTypeMatrix {
+		ft.Name = mt.String()
+		if ft.Fn == nil {
+			// Avoid referring to the loop variable in the closure.
+			mt := sys.MapType(mt)
+			ft.Fn = func() error { return probeMap(&sys.MapCreateAttr{MapType: mt}) }
+		}
 	}
-	return false
 }
 
 // MapFlags document which flags may be feature probed.
@@ -169,11 +231,11 @@ type MapFlags = sys.MapFlags
 
 // Flags which may be feature probed.
 const (
-	BPF_F_NO_PREALLOC MapFlags = unix.BPF_F_NO_PREALLOC
-	BPF_F_RDONLY_PROG MapFlags = unix.BPF_F_RDONLY_PROG
-	BPF_F_WRONLY_PROG MapFlags = unix.BPF_F_WRONLY_PROG
-	BPF_F_MMAPABLE    MapFlags = unix.BPF_F_MMAPABLE
-	BPF_F_INNER_MAP   MapFlags = unix.BPF_F_INNER_MAP
+	BPF_F_NO_PREALLOC = sys.BPF_F_NO_PREALLOC
+	BPF_F_RDONLY_PROG = sys.BPF_F_RDONLY_PROG
+	BPF_F_WRONLY_PROG = sys.BPF_F_WRONLY_PROG
+	BPF_F_MMAPABLE    = sys.BPF_F_MMAPABLE
+	BPF_F_INNER_MAP   = sys.BPF_F_INNER_MAP
 )
 
 // HaveMapFlag probes the running kernel for the availability of the specified map flag.
@@ -181,63 +243,79 @@ const (
 // Returns an error if flag is not one of the flags declared in this package.
 // See the package documentation for the meaning of the error return value.
 func HaveMapFlag(flag MapFlags) (err error) {
-	defer func() {
-		// This closure modifies a named return variable.
-		err = wrapProbeErrors(err)
-	}()
-
-	return haveMapFlag(flag)
+	return haveMapFlagsMatrix.Result(flag)
 }
 
-func haveMapFlag(flag MapFlags) error {
-	mc.Lock()
-	defer mc.Unlock()
-	err, ok := mc.mapFlags[flag]
-	if ok {
-		return err
-	}
-
-	attr, err := createMapFlagTypeAttr(flag)
-	if err != nil {
-		return err
-	}
-
-	fd, err := sys.MapCreate(attr)
-	if err == nil {
-		fd.Close()
-	}
-
-	// EINVAL occurs when attempting to create a map with an unknown type or an unknown flag.
-	if errors.Is(err, unix.EINVAL) {
-		err = ebpf.ErrNotSupported
-	}
-
-	mc.mapFlags[flag] = err
-
-	return err
-}
-
-func createMapFlagTypeAttr(flag MapFlags) (*sys.MapCreateAttr, error) {
-	a := &sys.MapCreateAttr{
-		KeySize:    4,
-		ValueSize:  4,
-		MaxEntries: 1,
-		MapFlags:   flag,
-	}
-
+func probeMapFlag(attr *sys.MapCreateAttr) error {
 	// For now, we do not check if the map type is supported because we only support
 	// probing for flags defined on arrays and hashs that are always supported.
 	// In the future, if we allow probing on flags defined on newer types, checking for map type
 	// support will be required.
-
-	switch flag {
-	case unix.BPF_F_MMAPABLE, unix.BPF_F_INNER_MAP, unix.BPF_F_RDONLY_PROG, unix.BPF_F_WRONLY_PROG:
-		a.MapType = sys.MapType(ebpf.Array)
-		return a, nil
-	case unix.BPF_F_NO_PREALLOC:
-		a.MapType = sys.MapType(ebpf.Hash)
-		return a, nil
+	if attr.MapType == sys.BPF_MAP_TYPE_UNSPEC {
+		attr.MapType = sys.BPF_MAP_TYPE_ARRAY
 	}
 
-	return nil, errors.New("probe not implemented")
+	attr.KeySize = 4
+	attr.ValueSize = 4
+	attr.MaxEntries = 1
+
+	fd, err := sys.MapCreate(attr)
+	if err == nil {
+		fd.Close()
+	} else if errors.Is(err, unix.EINVAL) {
+		// EINVAL occurs when attempting to create a map with an unknown type or an unknown flag.
+		err = ebpf.ErrNotSupported
+	}
+
+	return err
+}
+
+var haveMapFlagsMatrix = internal.FeatureMatrix[MapFlags]{
+	BPF_F_NO_PREALLOC: {
+		Version: "4.6",
+		Fn: func() error {
+			return probeMapFlag(&sys.MapCreateAttr{
+				MapType:  sys.BPF_MAP_TYPE_HASH,
+				MapFlags: BPF_F_NO_PREALLOC,
+			})
+		},
+	},
+	BPF_F_RDONLY_PROG: {
+		Version: "5.2",
+		Fn: func() error {
+			return probeMapFlag(&sys.MapCreateAttr{
+				MapFlags: BPF_F_RDONLY_PROG,
+			})
+		},
+	},
+	BPF_F_WRONLY_PROG: {
+		Version: "5.2",
+		Fn: func() error {
+			return probeMapFlag(&sys.MapCreateAttr{
+				MapFlags: BPF_F_WRONLY_PROG,
+			})
+		},
+	},
+	BPF_F_MMAPABLE: {
+		Version: "5.5",
+		Fn: func() error {
+			return probeMapFlag(&sys.MapCreateAttr{
+				MapFlags: BPF_F_MMAPABLE,
+			})
+		},
+	},
+	BPF_F_INNER_MAP: {
+		Version: "5.10",
+		Fn: func() error {
+			return probeMapFlag(&sys.MapCreateAttr{
+				MapFlags: BPF_F_INNER_MAP,
+			})
+		},
+	},
+}
+
+func init() {
+	for mf, ft := range haveMapFlagsMatrix {
+		ft.Name = fmt.Sprint(mf)
+	}
 }
