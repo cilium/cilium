19948afd7b513d3b128a70cb627fd3e104fe2d47
Tobias Klauser <tobias@cilium.io>
2023-01-25 11:15:23 +0100

    clustermesh: pass controller context to KVStore operations
    [ upstream commit 4d757e40dce89e2b65380ce4cbebf8e593b715cf ]
    
    Make sure these operations get cancelled properly in case the controller
    terminates.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/clustermesh/remote_cluster.go
+++ pkg/clustermesh/remote_cluster.go
@@ -167,7 +167,7 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 					extraOpts.ClusterSizeDependantInterval = rc.mesh.conf.NodeManager.ClusterSizeDependantInterval
 				}
 
-				backend, errChan := kvstore.NewClient(context.TODO(), kvstore.EtcdBackendName,
+				backend, errChan := kvstore.NewClient(ctx, kvstore.EtcdBackendName,
 					map[string]string{
 						kvstore.EtcdOptionConfig: rc.configPath,
 					}, &extraOpts)
@@ -213,7 +213,7 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 					},
 				})
 				if err != nil {
-					remoteNodes.Close(context.TODO())
+					remoteNodes.Close(ctx)
 					backend.Close()
 					return err
 				}
@@ -221,8 +221,8 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 
 				remoteIdentityCache, err := allocator.WatchRemoteIdentities(backend)
 				if err != nil {
-					remoteServices.Close(context.TODO())
-					remoteNodes.Close(context.TODO())
+					remoteServices.Close(ctx)
+					remoteNodes.Close(ctx)
 					backend.Close()
 					return err
 				}
