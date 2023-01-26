dc4421b291e00155b7136523bd2c5997a156ca62
Daniel Borkmann <daniel@iogearbox.net>
2023-01-26 16:30:11 +0000

    bpf: Add conditional maybe_add_l2_hdr integration
    Pull in the needs_l2_check functionality into fib_redirect_v{4,6} and
    control this via const bool needs_l2_check given not all call-sites
    need this. Mainly only when we push from bpf_lxc to an L3 device to
    outside world such as wireguard.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -604,7 +604,7 @@ ct_recreate6:
 	if (is_defined(ENABLE_HOST_ROUTING)) {
 		int oif;
 
-		ret = fib_redirect_v6(ctx, ETH_HLEN, ip6,
+		ret = fib_redirect_v6(ctx, ETH_HLEN, ip6, false,
 				      ctx->ingress_ifindex, &oif);
 		if (likely(ret == CTX_ACT_REDIRECT))
 			send_trace_notify(ctx, TRACE_TO_NETWORK, SECLABEL,
@@ -1108,7 +1108,7 @@ skip_vtep:
 	if (is_defined(ENABLE_HOST_ROUTING)) {
 		int oif;
 
-		ret = fib_redirect_v4(ctx, ETH_HLEN, ip4,
+		ret = fib_redirect_v4(ctx, ETH_HLEN, ip4, false,
 				      ctx->ingress_ifindex, &oif);
 		if (likely(ret == CTX_ACT_REDIRECT))
 			send_trace_notify(ctx, TRACE_TO_NETWORK, SECLABEL,
