cilium/cilium

commit f0465215aec02e7c21887d672840f6ff03780374

clustermesh: Correct shared service annotation behaviour
The changes in previous PR #18766 was assumed shared service annotation
is for local cluster, rather than for remote cluster.

This commit is to perform the adjustment. It's easier to explain with
concrete example. Let's say we have one clusters A and B, if the service
in cluster B is having shared-service: false annotation added, below
behavior will happen:

- requests to cluster A service will LB to local endpoint only, as
  service B is no longer shared.
- requests to cluster B service will LB to local and remote endpoints
  as usual.

Relates #18766

Signed-off-by: Tam Mach <tam.mach@isovalent.com>

pkg/k8s/service.go
@@ -524,6 +524,9 @@ func NewClusterService(id ServiceID, k8sService *Service, k8sEndpoints *Endpoint
 		svc.Backends[ipString] = backend.Ports
 	}
 
+	svc.Shared = k8sService.Shared
+	svc.IncludeExternal = k8sService.IncludeExternal
+
 	return svc
 }
 

pkg/k8s/service_cache.go
@@ -495,7 +495,7 @@ func (s *ServiceCache) correlateEndpoints(id ServiceID) (*Endpoints, bool) {
 		}
 	}
 
-	if svcFound && svc.IncludeExternal && svc.Shared {
+	if svcFound && svc.IncludeExternal {
 		externalEndpoints, hasExternalEndpoints := s.externalEndpoints[id]
 		if hasExternalEndpoints {
 			// remote cluster endpoints already contain all Endpoints from all
@@ -519,7 +519,7 @@ func (s *ServiceCache) correlateEndpoints(id ServiceID) (*Endpoints, bool) {
 	}
 
 	// Report the service as ready if a local endpoints object exists or if
-	// external endpoints have have been identified
+	// external endpoints have been identified
 	return endpoints, hasLocalEndpoints || len(endpoints.Backends) > 0
 }
 
@@ -548,23 +548,27 @@ func (s *ServiceCache) mergeServiceUpdateLocked(service *serviceStore.ClusterSer
 		s.externalEndpoints[id] = externalEndpoints
 	}
 
-	scopedLog.Debugf("Updating backends to %+v", service.Backends)
-	backends := map[string]*Backend{}
-	for ipString, portConfig := range service.Backends {
-		backends[ipString] = &Backend{Ports: portConfig}
-	}
-	externalEndpoints.endpoints[service.Cluster] = &Endpoints{
-		Backends: backends,
+	// we don't need to check if the current cluster is remote or local,
+	// as externalEndpoints should not have any local cluster endpoints anyway.
+	if service.IncludeExternal && !service.Shared {
+		delete(externalEndpoints.endpoints, service.Cluster)
+	} else {
+		scopedLog.Debugf("Updating backends to %+v", service.Backends)
+		backends := map[string]*Backend{}
+		for ipString, portConfig := range service.Backends {
+			backends[ipString] = &Backend{Ports: portConfig}
+		}
+		externalEndpoints.endpoints[service.Cluster] = &Endpoints{
+			Backends: backends,
+		}
 	}
 
 	svc, ok := s.services[id]
 
 	endpoints, serviceReady := s.correlateEndpoints(id)
 
-	// Only send event notification if service is shared and ready.
-	// External endpoints are still tracked but correlation will not happen
-	// until the service is marked as shared.
-	if ok && svc.Shared && serviceReady {
+	// Only send event notification if service is ready.
+	if ok && serviceReady {
 		swg.Add()
 		s.Events <- ServiceEvent{
 			Action:     UpdateService,
@@ -604,9 +608,7 @@ func (s *ServiceCache) MergeExternalServiceDelete(service *serviceStore.ClusterS
 
 		endpoints, serviceReady := s.correlateEndpoints(id)
 
-		// Only send event notification if service is shared. External
-		// endpoints are still tracked but correlation will not happen
-		// until the service is marked as shared.
+		// Only send event notification if service is shared.
 		if ok && svc.Shared {
 			swg.Add()
 			event := ServiceEvent{

pkg/k8s/service_cache_test.go
@@ -422,11 +422,48 @@ func (s *K8sSuite) TestServiceMerging(c *check.C) {
 				"port": {Protocol: loadbalancer.TCP, Port: 80},
 			},
 		},
+		IncludeExternal: true,
+		Shared:          false,
 	},
 		swgSvcs,
 	)
 
-	// Adding remote endpoints will trigger a service update
+	// Adding non-shared remote endpoints will not trigger a service update
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+
+		c.Assert(len(event.Endpoints.Backends), checker.Equals, 1)
+		c.Assert(event.Endpoints.Backends["2.2.2.2"], checker.DeepEquals, &Backend{
+			Ports: serviceStore.PortConfiguration{
+				"http-test-svc": {Protocol: loadbalancer.TCP, Port: 8080},
+			},
+		})
+
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	svcCache.MergeExternalServiceUpdate(&serviceStore.ClusterService{
+		Cluster:   "cluster1",
+		Namespace: "bar",
+		Name:      "foo",
+		Frontends: map[string]serviceStore.PortConfiguration{
+			"1.1.1.1": {},
+		},
+		Backends: map[string]serviceStore.PortConfiguration{
+			"3.3.3.3": map[string]*loadbalancer.L4Addr{
+				"port": {Protocol: loadbalancer.TCP, Port: 80},
+			},
+		},
+		IncludeExternal: true,
+		Shared:          true,
+	},
+		swgSvcs,
+	)
+
+	// Adding shared remote endpoints will trigger a service update
 	c.Assert(testutils.WaitUntil(func() bool {
 		event := <-svcCache.Events
 		defer event.SWG.Done()
@@ -461,6 +498,8 @@ func (s *K8sSuite) TestServiceMerging(c *check.C) {
 				"port": {Protocol: loadbalancer.TCP, Port: 80},
 			},
 		},
+		IncludeExternal: true,
+		Shared:          true,
 	},
 		swgSvcs,
 	)
@@ -517,9 +556,11 @@ func (s *K8sSuite) TestServiceMerging(c *check.C) {
 				"port": {Protocol: loadbalancer.TCP, Port: 80},
 			},
 		},
+		IncludeExternal: true,
+		Shared:          true,
 	}
 
-	// Adding another cluster to the first service will triger an event
+	// Adding another cluster to the first service will trigger an event
 	svcCache.MergeExternalServiceUpdate(cluster2svc, swgSvcs)
 	c.Assert(testutils.WaitUntil(func() bool {
 		event := <-svcCache.Events
@@ -554,7 +595,7 @@ func (s *K8sSuite) TestServiceMerging(c *check.C) {
 		return true
 	}, 2*time.Second), check.IsNil)
 
-	// When readding the service, the remote endpoints of cluster1 must still be present
+	// When re-adding the service, the remote endpoints of cluster1 must still be present
 	svcCache.UpdateService(k8sSvc, swgSvcs)
 	c.Assert(testutils.WaitUntil(func() bool {
 		event := <-svcCache.Events
@@ -586,7 +627,7 @@ func (s *K8sSuite) TestServiceMerging(c *check.C) {
 	}, 2*time.Second), check.IsNil)
 }
 
-func (s *K8sSuite) TestNonSharedServie(c *check.C) {
+func (s *K8sSuite) TestNonSharedService(c *check.C) {
 	svcCache := NewServiceCache(fakeDatapath.NewNodeAddressing())
 
 	k8sSvc := &slim_corev1.Service{

pkg/clustermesh/services_test.go
@@ -35,7 +35,7 @@ var etcdConfig = []byte(fmt.Sprintf("endpoints:\n- %s\n", kvstore.EtcdDummyAddre
 
 func (s *ClusterMeshServicesTestSuite) prepareServiceUpdate(clusterSuffix, backendIP, portName, port string) (string, string) {
 	return "cilium/state/services/v1/" + s.randomName + clusterSuffix + "/default/foo",
-		`{"cluster":"` + s.randomName + clusterSuffix + `","namespace":"default","name":"foo","frontends":{"172.20.0.177":{"port":{"protocol":"TCP","port":80}}},"backends":{"` + backendIP + `":{"` + portName + `":{"protocol":"TCP","port":` + port + `}}},"labels":{},"selector":{"name":"foo"}}`
+		`{"cluster":"` + s.randomName + clusterSuffix + `","namespace":"default","name":"foo","frontends":{"172.20.0.177":{"port":{"protocol":"TCP","port":80}}},"backends":{"` + backendIP + `":{"` + portName + `":{"protocol":"TCP","port":` + port + `}}},"labels":{},"selector":{"name":"foo"},"shared":true,"includeExternal":true}`
 
 }
 

pkg/service/store/store.go
@@ -57,7 +57,7 @@ type ClusterService struct {
 	// Frontends is a map indexed by the frontend IP address
 	Frontends map[string]PortConfiguration `json:"frontends"`
 
-	// Backends is is map indexed by the backend IP address
+	// Backends is map indexed by the backend IP address
 	Backends map[string]PortConfiguration `json:"backends"`
 
 	// Labels are the labels of the service
@@ -65,6 +65,13 @@ type ClusterService struct {
 
 	// Selector is the label selector used to select backends
 	Selector map[string]string `json:"selector"`
+
+	// IncludeExternal is true when external endpoints from other clusters
+	// should be included
+	IncludeExternal bool `json:"includeExternal"`
+
+	// Shared is true when the service should be exposed/shared to other clusters
+	Shared bool `json:"shared"`
 }
 
 func (s *ClusterService) String() string {

commit ad29ba352ba6ecf780762fbd966e837f60aa19e7

clustermesh: uniform global and shared service annotations behavior
Currently, marking a service as *global* through the dedicated
annotation in the local cluster only leads to the usage of both local and
remote endpoints in that cluster, and in that case the *shared* service
annotation possibly specified in the remote cluster(s) is not respected.

This commit adapts the behavior such that:
* the backends of a given service are shared only in case that service
  has the global-service annotation set to true, and the shared-service
  annotation is either not present, or set to true;
* the shared-service and service-affinity annotations take effect only
  if the global-service one is also present, otherwise they are
  silently ignored.

The full list of possibilities, in case of two clusters, is exemplified
in the following:
1. the local svc is not marked as global:
   => local svc backends include local endpoints only;
2. the local svc is marked as global, the remote one has no annotations:
   => local svc backends include local endpoints only;
3. the local svc is marked as global, the remote one is marked as global
   (thus, implicitly as shared):
   => local svc backends include local and remote endpoints;
4. the local svc is marked as global, the remote one is marked as
   shared (but not global):
   => local svc backends include local endpoints only;
5. the local svc is marked as global, the remote one is marked as global
   and not shared:
   => local svc backends include local endpoints only.

No changes are introduced in case of external workloads (i.e., all
services' backends are still synchronized).

Fixes: f0465215aec0 ("clustermesh: Correct shared service annotation behaviour")

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/k8s/service.go
@@ -44,14 +44,25 @@ func getAnnotationIncludeExternal(svc *slim_corev1.Service) bool {
 }
 
 func getAnnotationShared(svc *slim_corev1.Service) bool {
+	// The SharedService annotation is ignored if the service is not declared as global.
+	if !getAnnotationIncludeExternal(svc) {
+		return false
+	}
+
 	if value, ok := svc.ObjectMeta.Annotations[annotation.SharedService]; ok {
 		return strings.ToLower(value) == "true"
 	}
 
-	return getAnnotationIncludeExternal(svc)
+	// A global service is marked as shared by default.
+	return true
 }
 
 func getAnnotationServiceAffinity(svc *slim_corev1.Service) string {
+	// The ServiceAffinity annotation is ignored if the service is not declared as global.
+	if !getAnnotationIncludeExternal(svc) {
+		return serviceAffinityNone
+	}
+
 	if value, ok := svc.ObjectMeta.Annotations[annotation.ServiceAffinity]; ok {
 		return strings.ToLower(value)
 	}

pkg/k8s/service_cache.go
@@ -553,9 +553,10 @@ func (s *ServiceCache) mergeServiceUpdateLocked(service *serviceStore.ClusterSer
 		s.externalEndpoints[id] = externalEndpoints
 	}
 
-	// we don't need to check if the current cluster is remote or local,
-	// as externalEndpoints should not have any local cluster endpoints anyway.
-	if service.IncludeExternal && !service.Shared {
+	// The cluster the service belongs to will match the current one when dealing with external
+	// workloads (and in that case all endpoints shall be always present), and not match in the
+	// cluster-mesh case (where remote endpoints shall be used only if it is global and shared).
+	if service.Cluster != option.Config.ClusterName && !(service.IncludeExternal && service.Shared) {
 		delete(externalEndpoints.endpoints, service.Cluster)
 	} else {
 		scopedLog.Debugf("Updating backends to %+v", service.Backends)

pkg/k8s/service_cache_test.go
@@ -327,7 +327,7 @@ func (s *K8sSuite) TestCacheActionString(c *check.C) {
 	c.Assert(DeleteService.String(), check.Equals, "service-deleted")
 }
 
-func (s *K8sSuite) TestServiceMerging(c *check.C) {
+func (s *K8sSuite) TestExternalServiceMerging(c *check.C) {
 	svcCache := NewServiceCache(fakeDatapath.NewNodeAddressing())
 
 	k8sSvc := &slim_corev1.Service{
@@ -423,13 +423,87 @@ func (s *K8sSuite) TestServiceMerging(c *check.C) {
 				"port": {Protocol: loadbalancer.TCP, Port: 80},
 			},
 		},
+		IncludeExternal: false,
+		Shared:          false,
+	},
+		swgSvcs,
+	)
+
+	// Adding non-shared remote endpoints will not trigger a service update, regardless of whether
+	// IncludeExternal is set (i.e., the service is marked as a global one in the remote cluster).
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+
+		c.Assert(len(event.Endpoints.Backends), checker.Equals, 1)
+		c.Assert(event.Endpoints.Backends[cmtypes.MustParseAddrCluster("2.2.2.2")], checker.DeepEquals, &Backend{
+			Ports: serviceStore.PortConfiguration{
+				"http-test-svc": {Protocol: loadbalancer.TCP, Port: 8080},
+			},
+		})
+
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	svcCache.MergeExternalServiceUpdate(&serviceStore.ClusterService{
+		Cluster:   "cluster1",
+		Namespace: "bar",
+		Name:      "foo",
+		Frontends: map[string]serviceStore.PortConfiguration{
+			"1.1.1.1": {},
+		},
+		Backends: map[string]serviceStore.PortConfiguration{
+			"3.3.3.3": map[string]*loadbalancer.L4Addr{
+				"port": {Protocol: loadbalancer.TCP, Port: 80},
+			},
+		},
 		IncludeExternal: true,
 		Shared:          false,
 	},
 		swgSvcs,
 	)
 
-	// Adding non-shared remote endpoints will not trigger a service update
+	// Adding non-shared remote endpoints will not trigger a service update, regardless of whether
+	// IncludeExternal is set (i.e., the service is marked as a global one in the remote cluster).
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+
+		c.Assert(len(event.Endpoints.Backends), checker.Equals, 1)
+		c.Assert(event.Endpoints.Backends[cmtypes.MustParseAddrCluster("2.2.2.2")], checker.DeepEquals, &Backend{
+			Ports: serviceStore.PortConfiguration{
+				"http-test-svc": {Protocol: loadbalancer.TCP, Port: 8080},
+			},
+		})
+
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	svcCache.MergeExternalServiceUpdate(&serviceStore.ClusterService{
+		Cluster:   "cluster1",
+		Namespace: "bar",
+		Name:      "foo",
+		Frontends: map[string]serviceStore.PortConfiguration{
+			"1.1.1.1": {},
+		},
+		Backends: map[string]serviceStore.PortConfiguration{
+			"3.3.3.3": map[string]*loadbalancer.L4Addr{
+				"port": {Protocol: loadbalancer.TCP, Port: 80},
+			},
+		},
+		IncludeExternal: false,
+		Shared:          true,
+	},
+		swgSvcs,
+	)
+
+	// Adding shared remote endpoints will not trigger a service update, in case IncludeExternal
+	// is not set (i.e., the service is not marked as a global one in the remote cluster).
+	// Nonetheless, this condition should never happen, since a shared service shall always be global.
 	c.Assert(testutils.WaitUntil(func() bool {
 		event := <-svcCache.Events
 		defer event.SWG.Done()
@@ -464,7 +538,8 @@ func (s *K8sSuite) TestServiceMerging(c *check.C) {
 		swgSvcs,
 	)
 
-	// Adding shared remote endpoints will trigger a service update
+	// Adding shared remote endpoints will trigger a service update, in case IncludeExternal
+	// is set (i.e., the service is marked as a global one in the remote cluster).
 	c.Assert(testutils.WaitUntil(func() bool {
 		event := <-svcCache.Events
 		defer event.SWG.Done()
@@ -624,6 +699,72 @@ func (s *K8sSuite) TestServiceMerging(c *check.C) {
 	swgEps.Stop()
 	c.Assert(testutils.WaitUntil(func() bool {
 		swgEps.Wait()
+		return true
+	}, 2*time.Second), check.IsNil)
+}
+
+func (s *K8sSuite) TestClusterServiceMerging(c *check.C) {
+	svcCache := NewServiceCache(fakeDatapath.NewNodeAddressing())
+	swgSvcs := lock.NewStoppableWaitGroup()
+	swgEps := lock.NewStoppableWaitGroup()
+
+	svcID := ServiceID{Name: "foo", Namespace: "bar"}
+
+	svcCache.UpdateEndpoints(&slim_corev1.Endpoints{
+		ObjectMeta: slim_metav1.ObjectMeta{
+			Namespace: svcID.Namespace,
+			Name:      svcID.Name,
+		},
+		Subsets: []slim_corev1.EndpointSubset{
+			{
+				Addresses: []slim_corev1.EndpointAddress{{IP: "2.2.2.2"}},
+				Ports: []slim_corev1.EndpointPort{
+					{
+						Name:     "http-test-svc",
+						Port:     8080,
+						Protocol: slim_corev1.ProtocolTCP,
+					},
+				},
+			},
+		},
+	}, swgEps)
+
+	svcCache.MergeClusterServiceUpdate(&serviceStore.ClusterService{
+		Cluster:   option.Config.ClusterName,
+		Namespace: svcID.Namespace,
+		Name:      svcID.Name,
+		Frontends: map[string]serviceStore.PortConfiguration{
+			"1.1.1.1": {},
+		},
+		Backends: map[string]serviceStore.PortConfiguration{
+			"3.3.3.3": map[string]*loadbalancer.L4Addr{
+				"port": {Protocol: loadbalancer.TCP, Port: 80},
+			},
+		},
+		IncludeExternal: false,
+		Shared:          false,
+	}, swgSvcs)
+
+	// Adding a service will trigger the corresponding update containing all ready backends,
+	// regardless of whether it is marked as global or shared (since the cluster name matches).
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+
+		c.Assert(event.Endpoints.Backends[cmtypes.MustParseAddrCluster("2.2.2.2")], checker.DeepEquals, &Backend{
+			Ports: serviceStore.PortConfiguration{
+				"http-test-svc": {Protocol: loadbalancer.TCP, Port: 8080},
+			},
+		})
+
+		c.Assert(event.Endpoints.Backends[cmtypes.MustParseAddrCluster("3.3.3.3")], checker.DeepEquals, &Backend{
+			Ports: serviceStore.PortConfiguration{
+				"port": {Protocol: loadbalancer.TCP, Port: 80},
+			},
+		})
+
 		return true
 	}, 2*time.Second), check.IsNil)
 }

pkg/k8s/service_test.go
@@ -54,7 +54,12 @@ func (s *K8sSuite) TestGetAnnotationShared(c *check.C) {
 	c.Assert(getAnnotationShared(svc), check.Equals, true)
 
 	svc = &slim_corev1.Service{ObjectMeta: slim_metav1.ObjectMeta{
-		Annotations: map[string]string{"io.cilium/shared-service": "True"},
+		Annotations: map[string]string{"io.cilium/shared-service": "true"},
+	}}
+	c.Assert(getAnnotationShared(svc), check.Equals, false)
+
+	svc = &slim_corev1.Service{ObjectMeta: slim_metav1.ObjectMeta{
+		Annotations: map[string]string{"io.cilium/global-service": "true", "io.cilium/shared-service": "True"},
 	}}
 	c.Assert(getAnnotationShared(svc), check.Equals, true)
 
@@ -66,14 +71,19 @@ func (s *K8sSuite) TestGetAnnotationShared(c *check.C) {
 
 func (s *K8sSuite) TestGetAnnotationServiceAffinity(c *check.C) {
 	svc := &slim_corev1.Service{ObjectMeta: slim_metav1.ObjectMeta{
-		Annotations: map[string]string{"io.cilium/service-affinity": "local"},
+		Annotations: map[string]string{"io.cilium/global-service": "true", "io.cilium/service-affinity": "local"},
 	}}
 	c.Assert(getAnnotationServiceAffinity(svc), check.Equals, serviceAffinityLocal)
 
 	svc = &slim_corev1.Service{ObjectMeta: slim_metav1.ObjectMeta{
-		Annotations: map[string]string{"io.cilium/service-affinity": "remote"},
+		Annotations: map[string]string{"io.cilium/global-service": "true", "io.cilium/service-affinity": "remote"},
 	}}
 	c.Assert(getAnnotationServiceAffinity(svc), check.Equals, serviceAffinityRemote)
+
+	svc = &slim_corev1.Service{ObjectMeta: slim_metav1.ObjectMeta{
+		Annotations: map[string]string{"io.cilium/service-affinity": "remote"},
+	}}
+	c.Assert(getAnnotationServiceAffinity(svc), check.Equals, serviceAffinityNone)
 
 	svc = &slim_corev1.Service{ObjectMeta: slim_metav1.ObjectMeta{
 		Annotations: map[string]string{},