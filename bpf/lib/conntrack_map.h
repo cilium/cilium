0919b29a19860f766f3cc0f506de000e26007b2c
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2023-01-27 11:14:41 +0900

    bpf: Add helper functions to get per-cluster CT
    Add helper functions get_cluster_ct_map4/6 to get per-cluster conntrack
    map. When cluster-aware addressing is disabled or cluster_id = 0, these
    functions are identical to get_ct_map4/6.
    
    We also added get_cluster_ct_any_map4/6 function that will be used to
    get conntrack related map. Currently, &CT_MAP_ANY4/6 are hard-coded into
    the code, but with cluster-aware addressing, we need to lookup
    per-cluster conntrack map. Without cluster-aware addressing or
    cluster_id = 0, it returns &CT_MAP_ANY4/6, so that it doesn't change
    existing behavior.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

--- bpf/lib/conntrack_map.h
+++ bpf/lib/conntrack_map.h
@@ -76,6 +76,33 @@ get_ct_map6(const struct ipv6_ct_tuple *tuple)
 
 	return &CT_MAP_ANY6;
 }
+
+static __always_inline void *
+get_cluster_ct_map6(const struct ipv6_ct_tuple *tuple, __u32 cluster_id __maybe_unused)
+{
+#ifdef ENABLE_CLUSTER_AWARE_ADDRESSING
+	if (cluster_id != 0 && cluster_id != CLUSTER_ID) {
+		if (tuple->nexthdr == IPPROTO_TCP)
+			return map_lookup_elem(&PER_CLUSTER_CT_TCP6, &cluster_id);
+
+		return map_lookup_elem(&PER_CLUSTER_CT_ANY6, &cluster_id);
+	}
+#endif
+	if (tuple->nexthdr == IPPROTO_TCP)
+		return &CT_MAP_TCP6;
+
+	return &CT_MAP_ANY6;
+}
+
+static __always_inline void *
+get_cluster_ct_any_map6(__u32 cluster_id __maybe_unused)
+{
+#ifdef ENABLE_CLUSTER_AWARE_ADDRESSING
+	if (cluster_id != 0 && cluster_id != CLUSTER_ID)
+		return map_lookup_elem(&PER_CLUSTER_CT_ANY6, &cluster_id);
+#endif
+	return &CT_MAP_ANY6;
+}
 #endif
 
 #ifdef ENABLE_IPV4
@@ -131,6 +158,33 @@ get_ct_map4(const struct ipv4_ct_tuple *tuple)
 	if (tuple->nexthdr == IPPROTO_TCP)
 		return &CT_MAP_TCP4;
 
+	return &CT_MAP_ANY4;
+}
+
+static __always_inline void *
+get_cluster_ct_map4(const struct ipv4_ct_tuple *tuple, __u32 cluster_id __maybe_unused)
+{
+#ifdef ENABLE_CLUSTER_AWARE_ADDRESSING
+	if (cluster_id != 0 && cluster_id != CLUSTER_ID) {
+		if (tuple->nexthdr == IPPROTO_TCP)
+			return map_lookup_elem(&PER_CLUSTER_CT_TCP4, &cluster_id);
+
+		return map_lookup_elem(&PER_CLUSTER_CT_ANY4, &cluster_id);
+	}
+#endif
+	if (tuple->nexthdr == IPPROTO_TCP)
+		return &CT_MAP_TCP4;
+
+	return &CT_MAP_ANY4;
+}
+
+static __always_inline void *
+get_cluster_ct_any_map4(__u32 cluster_id __maybe_unused)
+{
+#ifdef ENABLE_CLUSTER_AWARE_ADDRESSING
+	if (cluster_id != 0 && cluster_id != CLUSTER_ID)
+		return map_lookup_elem(&PER_CLUSTER_CT_ANY4, &cluster_id);
+#endif
 	return &CT_MAP_ANY4;
 }
 #endif
