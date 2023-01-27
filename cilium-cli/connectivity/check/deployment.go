5584762940cb14793d33ecdd0eb13c8b5346953a
Marco Iorio <marco.iorio@isovalent.com>
2023-01-27 15:00:45 +0100

    clustermesh: adapt global service annotations
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Follow-up of cilium/cilium#23298, adding the new clustermesh
    service.cilium.io/global annotation alongside the previous one.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- cilium-cli/connectivity/check/deployment.go
+++ cilium-cli/connectivity/check/deployment.go
@@ -542,6 +542,7 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 			ct.Logf("✨ [%s] Deploying %s service...", ct.clients.src.ClusterName(), echoOtherNodeDeploymentName)
 			svc := newService(echoOtherNodeDeploymentName, map[string]string{"name": echoOtherNodeDeploymentName}, serviceLabels, "http", 8080)
 			svc.ObjectMeta.Annotations = map[string]string{}
+			svc.ObjectMeta.Annotations["service.cilium.io/global"] = "true"
 			svc.ObjectMeta.Annotations["io.cilium/global-service"] = "true"
 
 			_, err = ct.clients.src.CreateService(ctx, ct.params.TestNamespace, svc, metav1.CreateOptions{})
@@ -690,6 +691,7 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 
 			if ct.params.MultiCluster != "" {
 				svc.ObjectMeta.Annotations = map[string]string{}
+				svc.ObjectMeta.Annotations["service.cilium.io/global"] = "true"
 				svc.ObjectMeta.Annotations["io.cilium/global-service"] = "true"
 			}
 
