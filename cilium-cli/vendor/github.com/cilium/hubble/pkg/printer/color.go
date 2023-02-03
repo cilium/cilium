7cd189136d15f274c46c1a2a901a2a964a43d572
Tobias Klauser <tobias@cilium.io>
2023-02-03 11:10:48 +0100

    go.mod, vendor: update github.com/cilium/hubble to v0.11.1
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Fixes #1366
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/vendor/github.com/cilium/hubble/pkg/printer/color.go
+++ cilium-cli/vendor/github.com/cilium/hubble/pkg/printer/color.go
@@ -1,16 +1,5 @@
-// Copyright 2021 Authors of Hubble
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Hubble
 
 package printer
 
@@ -109,5 +98,13 @@ func (c colorer) verdictDropped(a interface{}) string {
 }
 
 func (c colorer) verdictAudit(a interface{}) string {
+	return c.yellow.Sprint(a)
+}
+
+func (c colorer) verdictTraced(a interface{}) string {
+	return c.yellow.Sprint(a)
+}
+
+func (c colorer) verdictTranslated(a interface{}) string {
 	return c.yellow.Sprint(a)
 }
