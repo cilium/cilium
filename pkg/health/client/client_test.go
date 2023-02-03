cilium/cilium

commit 3d8de4cb5b5d41c66d1f2985feb623287cdfb194

health/client: Include secondary endpoint status
This commit changes the way cilium health status is displayed in `cilium
status` and `cilium-health status` with regards to secondary addresses
(i.e. IPv6 host or health endpoint IP addresses in dual-stack setups).

Firstly, in the brief output format (i.e. the output of `cilium
status`), we do not consider a host or endpoint as "reachable" if a
secondary path is unreachable. This is similar to how we already treat
partial reachability for a health probe if ICMP succeeded but HTTP did
not.

Secondly, in `cilium-health status --verbose`, we now also print the
status of secondary endpoint addresses (which we usually probe, but
before this commit never displayed anywhere).

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/health/client/client.go
@@ -158,6 +158,23 @@ func GetPathConnectivityStatusType(cp *models.PathStatus) ConnectivityStatusType
 	return status
 }
 
+func SummarizePathConnectivityStatusType(cps []*models.PathStatus) ConnectivityStatusType {
+	status := ConnStatusReachable
+	for _, cp := range cps {
+		switch GetPathConnectivityStatusType(cp) {
+		case ConnStatusUnreachable:
+			// If any status is unreachable, return it immediately.
+			return ConnStatusUnreachable
+		case ConnStatusUnknown:
+			// If the status is unknown, prepare to return it. It's
+			// going to be returned if there is no unreachable
+			// status in next iterations.
+			status = ConnStatusUnknown
+		}
+	}
+	return status
+}
+
 func formatConnectivityStatus(w io.Writer, cs *models.ConnectivityStatus, path, indent string) {
 	status := cs.Status
 	switch GetConnectivityStatusType(cs) {
@@ -186,30 +203,32 @@ func formatPathStatus(w io.Writer, name string, cp *models.PathStatus, indent st
 	}
 }
 
-// pathIsHealthyOrUnknown checks whether ICMP and TCP(HTTP) connectivity to the
-// given path is available or had no explicit error status (which usually is the
-// case when the new node is provisioned).
-func pathIsHealthyOrUnknown(cp *models.PathStatus) bool {
-	if cp == nil {
-		return false
-	}
-
-	statuses := []*models.ConnectivityStatus{
-		cp.Icmp,
-		cp.HTTP,
-	}
-	for _, status := range statuses {
-		switch GetConnectivityStatusType(status) {
-		case ConnStatusUnreachable:
+// allPathsAreHealthyOrUnknown checks whether ICMP and TCP(HTTP) connectivity
+// to the given paths is available or had no explicit error status
+// (which usually is the case when the new node is provisioned).
+func allPathsAreHealthyOrUnknown(cps []*models.PathStatus) bool {
+	for _, cp := range cps {
+		if cp == nil {
 			return false
+		}
+
+		statuses := []*models.ConnectivityStatus{
+			cp.Icmp,
+			cp.HTTP,
+		}
+		for _, status := range statuses {
+			switch GetConnectivityStatusType(status) {
+			case ConnStatusUnreachable:
+				return false
+			}
 		}
 	}
 	return true
 }
 
 func nodeIsHealthy(node *models.NodeStatus) bool {
-	return pathIsHealthyOrUnknown(GetHostPrimaryAddress(node)) &&
-		(node.Endpoint == nil || pathIsHealthyOrUnknown(node.Endpoint))
+	return allPathsAreHealthyOrUnknown(GetAllHostAddresses(node)) &&
+		allPathsAreHealthyOrUnknown(GetAllEndpointAddresses(node))
 }
 
 func nodeIsLocalhost(node *models.NodeStatus, self *models.SelfStatus) bool {
@@ -234,6 +253,38 @@ func GetHostPrimaryAddress(node *models.NodeStatus) *models.PathStatus {
 	return node.Host.PrimaryAddress
 }
 
+// GetAllHostAddresses returns a list of all addresses (primary and any
+// and any secondary) for the host of a given node. If node.Host is nil,
+// returns nil.
+func GetAllHostAddresses(node *models.NodeStatus) []*models.PathStatus {
+	if node.Host == nil {
+		return nil
+	}
+
+	return append([]*models.PathStatus{node.Host.PrimaryAddress}, node.Host.SecondaryAddresses...)
+}
+
+// GetEndpointPrimaryAddress returns the PrimaryAddress for the health endpoint
+// within node. If node.HealthEndpoint is nil, returns nil.
+func GetEndpointPrimaryAddress(node *models.NodeStatus) *models.PathStatus {
+	if node.HealthEndpoint == nil {
+		return nil
+	}
+
+	return node.HealthEndpoint.PrimaryAddress
+}
+
+// GetAllEndpointAddresses returns a list of all addresses (primary and any
+// and any secondary) for the health endpoint within a given node.
+// If node.HealthEndpoint is nil, returns nil.
+func GetAllEndpointAddresses(node *models.NodeStatus) []*models.PathStatus {
+	if node.HealthEndpoint == nil {
+		return nil
+	}
+
+	return node.HealthEndpoint.SecondaryAddresses
+}
+
 func formatNodeStatus(w io.Writer, node *models.NodeStatus, printAll, succinct, verbose, localhost bool) {
 	localStr := ""
 	if localhost {
@@ -241,11 +292,10 @@ func formatNodeStatus(w io.Writer, node *models.NodeStatus, printAll, succinct, 
 	}
 	if succinct {
 		if printAll || !nodeIsHealthy(node) {
-
 			fmt.Fprintf(w, "  %s%s\t%s\t%s\t%s\n", node.Name,
 				localStr, getPrimaryAddressIP(node),
-				GetPathConnectivityStatusType(GetHostPrimaryAddress(node)).String(),
-				GetPathConnectivityStatusType(node.Endpoint).String())
+				SummarizePathConnectivityStatusType(GetAllHostAddresses(node)).String(),
+				SummarizePathConnectivityStatusType(GetAllEndpointAddresses(node)).String())
 		}
 	} else {
 		fmt.Fprintf(w, "  %s%s:\n", node.Name, localStr)
@@ -255,7 +305,12 @@ func formatNodeStatus(w io.Writer, node *models.NodeStatus, printAll, succinct, 
 				formatPathStatus(w, "Secondary", addr, "      ", verbose)
 			}
 		}
-		formatPathStatus(w, "Endpoint", node.Endpoint, "    ", verbose)
+		formatPathStatus(w, "Endpoint", GetEndpointPrimaryAddress(node), "    ", verbose)
+		if verbose && node.HealthEndpoint != nil {
+			for _, addr := range node.HealthEndpoint.SecondaryAddresses {
+				formatPathStatus(w, "Secondary", addr, "      ", verbose)
+			}
+		}
 	}
 }
 

pkg/health/client/client_test.go
@@ -205,18 +205,30 @@ func (s *ClientTestSuite) TestFormatNodeStatus(c *C) {
 		possibleHostStatuses = append(possibleHostStatuses, hostStatus)
 	}
 
+	// Assemble possible health-endpoint statuses.
+	possibleEndpointStatuses := []*models.EndpointStatus{
+		nil,
+	}
+	for _, possiblePrimaryAddress := range possiblePathStatuses {
+		hostStatus := &models.EndpointStatus{
+			PrimaryAddress:     possiblePrimaryAddress,
+			SecondaryAddresses: possibleSecondaryAddresses,
+		}
+		possibleEndpointStatuses = append(possibleEndpointStatuses, hostStatus)
+	}
+
 	printAllOptions := []bool{true, false}
 	succinctOptions := []bool{true, false}
 	verboseOptions := []bool{true, false}
 	localhostOptions := []bool{true, false}
 
-	for _, possibleEndpointStatus := range possiblePathStatuses {
+	for _, possibleEndpointStatus := range possibleEndpointStatuses {
 		for _, hostStatus := range possibleHostStatuses {
 			for _, name := range possibleNames {
 				ns := &models.NodeStatus{
-					Endpoint: possibleEndpointStatus,
-					Host:     hostStatus,
-					Name:     name,
+					HealthEndpoint: possibleEndpointStatus,
+					Host:           hostStatus,
+					Name:           name,
 				}
 				for _, printAllOpt := range printAllOptions {
 					for _, succintOpt := range succinctOptions {

commit 2a71844d3637c1ea01515a52cf4c33f03eb6d21e

cilium-health status: fix endpoint reachability in succinct view
This commit fixes the reporting of `cilium-health status --succinct`,
and specifically of the endpoints reachability column in succinct view.
The issue was caused by the `GetAllEndpointAddresses` function returning
only secondary addresses, leading the status to be reported as reachable
in case of failures associated with the primary address (or if this was
the only one present).

Fixes: 3d8de4cb5b5d ("health/client: Include secondary endpoint status")

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/health/client/client.go
@@ -294,14 +294,14 @@ func GetEndpointSecondaryAddresses(node *models.NodeStatus) []*models.PathStatus
 }
 
 // GetAllEndpointAddresses returns a list of all addresses (primary and any
-// and any secondary) for the health endpoint within a given node.
+// secondary) for the health endpoint within a given node.
 // If node.HealthEndpoint is nil, returns nil.
 func GetAllEndpointAddresses(node *models.NodeStatus) []*models.PathStatus {
 	if node.HealthEndpoint == nil {
 		return nil
 	}
 
-	return node.HealthEndpoint.SecondaryAddresses
+	return append([]*models.PathStatus{node.HealthEndpoint.PrimaryAddress}, node.HealthEndpoint.SecondaryAddresses...)
 }
 
 func formatNodeStatus(w io.Writer, node *models.NodeStatus, printAll, succinct, verbose, localhost bool) {

pkg/health/client/client_test.go
@@ -10,6 +10,7 @@ import (
 	. "gopkg.in/check.v1"
 
 	"github.com/cilium/cilium/api/v1/health/models"
+	"github.com/cilium/cilium/pkg/checker"
 )
 
 // Hook up gocheck into the "go test" runner.
@@ -293,4 +294,40 @@ func (s *ClientTestSuite) TestGetPrimaryAddressIP(c *C) {
 
 	pathStatus = getPrimaryAddressIP(primaryAddressNS)
 	c.Assert(pathStatus, Equals, "")
+}
+
+func (s *ClientTestSuite) TestGetAllEndpointAddresses(c *C) {
+	var (
+		primary    = models.PathStatus{IP: "1.1.1.1"}
+		secondary1 = models.PathStatus{IP: "2.2.2.2"}
+		secondary2 = models.PathStatus{IP: "3.3.3.3"}
+	)
+
+	tests := []struct {
+		node     *models.NodeStatus
+		expected []*models.PathStatus
+	}{
+		{
+			node:     &models.NodeStatus{},
+			expected: nil,
+		},
+		{
+			node: &models.NodeStatus{
+				HealthEndpoint: &models.EndpointStatus{
+					PrimaryAddress: &primary,
+				}},
+			expected: []*models.PathStatus{&primary},
+		},
+		{
+			node: &models.NodeStatus{
+				HealthEndpoint: &models.EndpointStatus{
+					PrimaryAddress:     &primary,
+					SecondaryAddresses: []*models.PathStatus{&secondary1, &secondary2},
+				}},
+			expected: []*models.PathStatus{&primary, &secondary1, &secondary2},
+		},
+	}
+	for _, tc := range tests {
+		c.Assert(GetAllEndpointAddresses(tc.node), checker.DeepEquals, tc.expected)
+	}
 }