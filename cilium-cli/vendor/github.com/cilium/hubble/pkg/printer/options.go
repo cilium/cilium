1984be8bd185fd8cd859f1de7a09901dafe41245
Tobias Klauser <tobias@cilium.io>
2023-02-03 11:10:48 +0100

    go.mod, vendor: update github.com/cilium/hubble to v0.11.1
    Fixes #1366
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/vendor/github.com/cilium/hubble/pkg/printer/options.go
+++ cilium-cli/vendor/github.com/cilium/hubble/pkg/printer/options.go
@@ -1,23 +1,10 @@
-// Copyright 2019-2021 Authors of Hubble
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Hubble
 
 package printer
 
 import (
-	"fmt"
 	"io"
-	"os"
 )
 
 // Output enum of the printer.
@@ -26,8 +13,8 @@ type Output int
 const (
 	// TabOutput prints flows in even tab-aligned columns.
 	TabOutput Output = iota
-	// JSONOutput prints flows as json.
-	JSONOutput
+	// JSONLegacyOutput prints flows as json in the legacy format
+	JSONLegacyOutput
 	// CompactOutput prints flows as compact as possible (similar to monitor).
 	CompactOutput
 	// DictOutput presents the same information as TabOutput, but each flow is
@@ -52,11 +39,10 @@ type Options struct {
 // Option ...
 type Option func(*Options)
 
-// JSON encoded output from the printer.
-func JSON() Option {
-	fmt.Fprintln(os.Stderr, "WARNING: The --output=json option has been deprecated. Use --output=jsonpb instead")
+// JSONLegacy encoded output from the printer.
+func JSONLegacy() Option {
 	return func(opts *Options) {
-		opts.output = JSONOutput
+		opts.output = JSONLegacyOutput
 	}
 }
 
@@ -103,10 +89,11 @@ func IgnoreStderr() Option {
 }
 
 // WithColor set the color mode. The when argument is one of:
-// - "auto": color mode is enabled when the standard output is connected to a
-//   terminal.
-// - "always": color mode is enabled no matter to standard output.
-// - "never": color mode is always disabled.
+//   - "auto": color mode is enabled when the standard output is connected to a
+//     terminal.
+//   - "always": color mode is enabled no matter to standard output.
+//   - "never": color mode is always disabled.
+//
 // Any other value of when means "auto", which is the default.
 // The color mode is only applied with in Dict or Compact mode. For any other
 // mode, color is always disabled.
@@ -138,7 +125,7 @@ func WithNodeName() Option {
 }
 
 // WithTimeFormat specifies the time format layout to use when printing out
-// timestamps. This option has no effect if JSON or JSONPB option is used.
+// timestamps. This option has no effect if JSONLegacy or JSONPB option is used.
 // The layout must be a time format layout as specified in the standard
 // library's time package.
 func WithTimeFormat(layout string) Option {
