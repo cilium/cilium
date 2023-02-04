1fe9dcfd6c33f4476e993710ef1426fb5f95a531
Mohit Marathe <96789026+mohit-marathe@users.noreply.github.com>
2023-02-04 19:31:19 +0530

    Delete pkg/maps/sockmap directory
    Signed-off-by: Mohit Marathe <96789026+mohit-marathe@users.noreply.github.com>

--- pkg/maps/sockmap/doc.go
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-// Package Sockmap represents the map from 5-tuple to the socket. It
-// is primarily managed from the datapath using a sockops program. Cilium
-// side is primarily for pretty printing.
-// +groupName=maps
-package sockmap
