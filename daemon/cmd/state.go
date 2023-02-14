cilium/cilium

commit 27f19674c894b1642cb9b86898d9b7cbdcafa3c5

k8s: wait until k8s services to be synchronized with datapath
With the help of StoppableWaitGroup we will be able to detect when the
services are correctly synchronized with the dapatah.

Signed-off-by: André Martins <andre@cilium.io>

pkg/clustermesh/services.go
@@ -27,8 +27,8 @@ import (
 // services. The functions have to merge service updates and deletions with
 // local services to provide a shared view.
 type ServiceMerger interface {
-	MergeExternalServiceUpdate(service *service.ClusterService)
-	MergeExternalServiceDelete(service *service.ClusterService)
+	MergeExternalServiceUpdate(service *service.ClusterService, swg *lock.StoppableWaitGroup)
+	MergeExternalServiceDelete(service *service.ClusterService, swg *lock.StoppableWaitGroup)
 }
 
 type globalService struct {
@@ -123,6 +123,9 @@ func (c *globalServiceCache) onClusterDelete(clusterName string) {
 
 type remoteServiceObserver struct {
 	remoteCluster *remoteCluster
+	// swg provides a mechanism to known when the services were synchronized
+	// with the datapath.
+	swg *lock.StoppableWaitGroup
 }
 
 // OnUpdate is called when a service in a remote cluster is updated
@@ -135,7 +138,8 @@ func (r *remoteServiceObserver) OnUpdate(key store.Key) {
 		mesh.globalServices.onUpdate(svc)
 
 		if merger := mesh.conf.ServiceMerger; merger != nil {
-			merger.MergeExternalServiceUpdate(svc)
+			r.swg.Add()
+			merger.MergeExternalServiceUpdate(svc, r.swg)
 		} else {
 			scopedLog.Debugf("Ignoring remote service update. Missing merger function")
 		}
@@ -154,7 +158,8 @@ func (r *remoteServiceObserver) OnDelete(key store.NamedKey) {
 		mesh.globalServices.onDelete(svc)
 
 		if merger := mesh.conf.ServiceMerger; merger != nil {
-			merger.MergeExternalServiceDelete(svc)
+			r.swg.Add()
+			merger.MergeExternalServiceDelete(svc, r.swg)
 		} else {
 			scopedLog.Debugf("Ignoring remote service update. Missing merger function")
 		}

daemon/state.go
@@ -346,6 +346,14 @@ func (d *Daemon) initRestore(restoredEndpoints *endpointRestoreState) chan struc
 
 		go func() {
 			if k8s.IsEnabled() {
+				// Also wait for all cluster mesh to be synchronized with the
+				// datapath before proceeding.
+				if option.Config.ClusterID != 0 {
+					err := d.clustermesh.ClustersSynced(context.Background())
+					if err != nil {
+						log.WithError(err).Fatal("timeout while waiting for all clusters to be locally synchronized")
+					}
+				}
 				// Start controller which removes any leftover Kubernetes
 				// services that may have been deleted while Cilium was not
 				// running. Once this controller succeeds, because it has no

daemon/k8s_watcher.go
@@ -490,7 +490,7 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 					swgSvcs.Add()
 					serSvcs.Enqueue(func() error {
 						defer swgSvcs.Done()
-						err := d.addK8sServiceV1(k8sSvc)
+						err := d.addK8sServiceV1(k8sSvc, swgSvcs)
 						d.K8sEventProcessed(metricService, metricCreate, err == nil)
 						return nil
 					}, serializer.NoRetry)
@@ -510,7 +510,7 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 						swgSvcs.Add()
 						serSvcs.Enqueue(func() error {
 							defer swgSvcs.Done()
-							err := d.updateK8sServiceV1(oldk8sSvc, newk8sSvc)
+							err := d.updateK8sServiceV1(oldk8sSvc, newk8sSvc, swgSvcs)
 							d.K8sEventProcessed(metricService, metricUpdate, err == nil)
 							return nil
 						}, serializer.NoRetry)
@@ -539,7 +539,7 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 				swgSvcs.Add()
 				serSvcs.Enqueue(func() error {
 					defer swgSvcs.Done()
-					err := d.deleteK8sServiceV1(k8sSvc)
+					err := d.deleteK8sServiceV1(k8sSvc, swgSvcs)
 					d.K8sEventProcessed(metricService, metricDelete, err == nil)
 					return nil
 				}, serializer.NoRetry)
@@ -567,7 +567,7 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 					swgEps.Add()
 					serEps.Enqueue(func() error {
 						defer swgEps.Done()
-						err := d.addK8sEndpointV1(k8sEP)
+						err := d.addK8sEndpointV1(k8sEP, swgEps)
 						d.K8sEventProcessed(metricEndpoint, metricCreate, err == nil)
 						return nil
 					}, serializer.NoRetry)
@@ -587,7 +587,7 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 						swgEps.Add()
 						serEps.Enqueue(func() error {
 							defer swgEps.Done()
-							err := d.updateK8sEndpointV1(oldk8sEP, newk8sEP)
+							err := d.updateK8sEndpointV1(oldk8sEP, newk8sEP, swgEps)
 							d.K8sEventProcessed(metricEndpoint, metricUpdate, err == nil)
 							return nil
 						}, serializer.NoRetry)
@@ -615,7 +615,7 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 				swgEps.Add()
 				serEps.Enqueue(func() error {
 					defer swgEps.Done()
-					err := d.deleteK8sEndpointV1(k8sEP)
+					err := d.deleteK8sEndpointV1(k8sEP, swgEps)
 					d.K8sEventProcessed(metricEndpoint, metricDelete, err == nil)
 					return nil
 				}, serializer.NoRetry)
@@ -1130,11 +1130,8 @@ func (d *Daemon) deleteK8sNetworkPolicyV1(k8sNP *types.NetworkPolicy) error {
 }
 
 func (d *Daemon) k8sServiceHandler() {
-	for {
-		event, ok := <-d.k8sSvcCache.Events
-		if !ok {
-			return
-		}
+	eventHandler := func(event k8s.ServiceEvent) {
+		defer event.SWG.Done()
 
 		svc := event.Service
 
@@ -1156,7 +1153,7 @@ func (d *Daemon) k8sServiceHandler() {
 			}
 
 			if !svc.IsExternal() {
-				continue
+				return
 			}
 
 			serviceImportMeta, cacheOK := endpointMetadataCache.get(event.ID)
@@ -1186,7 +1183,7 @@ func (d *Daemon) k8sServiceHandler() {
 			}
 
 			if !svc.IsExternal() {
-				continue
+				return
 			}
 
 			endpointMetadataCache.delete(event.ID)
@@ -1202,38 +1199,45 @@ func (d *Daemon) k8sServiceHandler() {
 			}
 		}
 	}
+	for {
+		event, ok := <-d.k8sSvcCache.Events
+		if !ok {
+			return
+		}
+		eventHandler(event)
+	}
 }
 
 func (d *Daemon) runK8sServiceHandler() {
 	go d.k8sServiceHandler()
 }
 
-func (d *Daemon) addK8sServiceV1(svc *types.Service) error {
-	d.k8sSvcCache.UpdateService(svc)
+func (d *Daemon) addK8sServiceV1(svc *types.Service, swg *lock.StoppableWaitGroup) error {
+	d.k8sSvcCache.UpdateService(svc, swg)
 	return nil
 }
 
-func (d *Daemon) updateK8sServiceV1(oldSvc, newSvc *types.Service) error {
-	return d.addK8sServiceV1(newSvc)
+func (d *Daemon) updateK8sServiceV1(oldSvc, newSvc *types.Service, swg *lock.StoppableWaitGroup) error {
+	return d.addK8sServiceV1(newSvc, swg)
 }
 
-func (d *Daemon) deleteK8sServiceV1(svc *types.Service) error {
-	d.k8sSvcCache.DeleteService(svc)
+func (d *Daemon) deleteK8sServiceV1(svc *types.Service, swg *lock.StoppableWaitGroup) error {
+	d.k8sSvcCache.DeleteService(svc, swg)
 	return nil
 }
 
-func (d *Daemon) addK8sEndpointV1(ep *types.Endpoints) error {
-	d.k8sSvcCache.UpdateEndpoints(ep)
+func (d *Daemon) addK8sEndpointV1(ep *types.Endpoints, swg *lock.StoppableWaitGroup) error {
+	d.k8sSvcCache.UpdateEndpoints(ep, swg)
 	return nil
 }
 
-func (d *Daemon) updateK8sEndpointV1(oldEP, newEP *types.Endpoints) error {
-	d.k8sSvcCache.UpdateEndpoints(newEP)
+func (d *Daemon) updateK8sEndpointV1(oldEP, newEP *types.Endpoints, swg *lock.StoppableWaitGroup) error {
+	d.k8sSvcCache.UpdateEndpoints(newEP, swg)
 	return nil
 }
 
-func (d *Daemon) deleteK8sEndpointV1(ep *types.Endpoints) error {
-	d.k8sSvcCache.DeleteEndpoints(ep)
+func (d *Daemon) deleteK8sEndpointV1(ep *types.Endpoints, swg *lock.StoppableWaitGroup) error {
+	d.k8sSvcCache.DeleteEndpoints(ep, swg)
 	return nil
 }
 

operator/k8s_service_sync.go
@@ -20,9 +20,11 @@ import (
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	"github.com/cilium/cilium/pkg/kvstore/store"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/serializer"
 	"github.com/cilium/cilium/pkg/service"
 
 	"github.com/sirupsen/logrus"
@@ -41,11 +43,8 @@ var (
 )
 
 func k8sServiceHandler() {
-	for {
-		event, ok := <-k8sSvcCache.Events
-		if !ok {
-			return
-		}
+	serviceHandler := func(event k8s.ServiceEvent) {
+		defer event.SWG.Done()
 
 		svc := k8s.NewClusterService(event.ID, event.Service, event.Endpoints)
 		svc.Cluster = option.Config.ClusterName
@@ -62,7 +61,7 @@ func k8sServiceHandler() {
 		if !event.Service.Shared {
 			// The annotation may have been added, delete an eventual existing service
 			servicesStore.DeleteLocalKey(&svc)
-			continue
+			return
 		}
 
 		switch event.Action {
@@ -72,6 +71,14 @@ func k8sServiceHandler() {
 		case k8s.DeleteService:
 			servicesStore.DeleteLocalKey(&svc)
 		}
+	}
+	for {
+		event, ok := <-k8sSvcCache.Events
+		if !ok {
+			return
+		}
+
+		serviceHandler(event)
 	}
 }
 
@@ -97,6 +104,12 @@ func startSynchronizingServices() {
 		close(readyChan)
 	}()
 
+	serSvcs := serializer.NewFunctionQueue(1024)
+	swgSvcs := lock.NewStoppableWaitGroup()
+
+	serEps := serializer.NewFunctionQueue(1024)
+	swgEps := lock.NewStoppableWaitGroup()
+
 	// Watch for v1.Service changes and push changes into ServiceCache
 	_, svcController := informer.NewInformer(
 		cache.NewListWatchFromClient(k8s.Client().CoreV1().RESTClient(),
@@ -108,7 +121,12 @@ func startSynchronizingServices() {
 				metrics.EventTSK8s.SetToCurrentTime()
 				if k8sSvc := k8s.CopyObjToV1Services(obj); k8sSvc != nil {
 					log.Debugf("Received service addition %+v", k8sSvc)
-					k8sSvcCache.UpdateService(k8sSvc)
+					swgSvcs.Add()
+					serSvcs.Enqueue(func() error {
+						defer swgSvcs.Done()
+						k8sSvcCache.UpdateService(k8sSvc, swgSvcs)
+						return nil
+					}, serializer.NoRetry)
 				}
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
@@ -120,7 +138,12 @@ func startSynchronizingServices() {
 						}
 
 						log.Debugf("Received service update %+v", newk8sSvc)
-						k8sSvcCache.UpdateService(newk8sSvc)
+						swgSvcs.Add()
+						serSvcs.Enqueue(func() error {
+							defer swgSvcs.Done()
+							k8sSvcCache.UpdateService(newk8sSvc, swgSvcs)
+							return nil
+						}, serializer.NoRetry)
 					}
 				}
 			},
@@ -141,7 +164,12 @@ func startSynchronizingServices() {
 					}
 				}
 				log.Debugf("Received service deletion %+v", k8sSvc)
-				k8sSvcCache.DeleteService(k8sSvc)
+				swgSvcs.Add()
+				serSvcs.Enqueue(func() error {
+					defer swgSvcs.Done()
+					k8sSvcCache.DeleteService(k8sSvc, swgSvcs)
+					return nil
+				}, serializer.NoRetry)
 			},
 		},
 		k8s.ConvertToK8sService,
@@ -162,7 +190,12 @@ func startSynchronizingServices() {
 			AddFunc: func(obj interface{}) {
 				metrics.EventTSK8s.SetToCurrentTime()
 				if k8sEP := k8s.CopyObjToV1Endpoints(obj); k8sEP != nil {
-					k8sSvcCache.UpdateEndpoints(k8sEP)
+					swgEps.Add()
+					serEps.Enqueue(func() error {
+						defer swgEps.Done()
+						k8sSvcCache.UpdateEndpoints(k8sEP, swgEps)
+						return nil
+					}, serializer.NoRetry)
 				}
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
@@ -172,7 +205,12 @@ func startSynchronizingServices() {
 						if k8s.EqualV1Endpoints(oldk8sEP, newk8sEP) {
 							return
 						}
-						k8sSvcCache.UpdateEndpoints(newk8sEP)
+						swgEps.Add()
+						serEps.Enqueue(func() error {
+							defer swgEps.Done()
+							k8sSvcCache.UpdateEndpoints(newk8sEP, swgEps)
+							return nil
+						}, serializer.NoRetry)
 					}
 				}
 			},
@@ -192,7 +230,12 @@ func startSynchronizingServices() {
 						return
 					}
 				}
-				k8sSvcCache.DeleteEndpoints(k8sEP)
+				swgEps.Add()
+				serEps.Enqueue(func() error {
+					defer swgEps.Done()
+					k8sSvcCache.DeleteEndpoints(k8sEP, swgEps)
+					return nil
+				}, serializer.NoRetry)
 			},
 		},
 		k8s.ConvertToK8sEndpoints,
@@ -202,7 +245,12 @@ func startSynchronizingServices() {
 
 	go func() {
 		cache.WaitForCacheSync(wait.NeverStop, svcController.HasSynced)
+		swgSvcs.Stop()
 		cache.WaitForCacheSync(wait.NeverStop, endpointController.HasSynced)
+		swgEps.Stop()
+
+		swgSvcs.Wait()
+		swgEps.Wait()
 		close(k8sSvcCacheSynced)
 	}()
 

pkg/clustermesh/clustermesh.go
@@ -15,6 +15,7 @@
 package clustermesh
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/cilium/cilium/pkg/allocator"
@@ -146,6 +147,7 @@ func (cm *ClusterMesh) newRemoteCluster(name, path string) *remoteCluster {
 		mesh:        cm,
 		changed:     make(chan bool, configNotificationsChannelSize),
 		controllers: controller.NewManager(),
+		swg:         lock.NewStoppableWaitGroup(),
 	}
 }
 
@@ -202,4 +204,24 @@ func (cm *ClusterMesh) NumReadyClusters() int {
 	}
 
 	return nready
+}
+
+// ClustersSynced returns after all clusters were synchronized with the bpf
+// datapath.
+func (cm *ClusterMesh) ClustersSynced(ctx context.Context) error {
+	cm.mutex.RLock()
+	swgs := make([]*lock.StoppableWaitGroup, 0, len(cm.clusters))
+	for _, cluster := range cm.clusters {
+		swgs = append(swgs, cluster.swg)
+	}
+	cm.mutex.RUnlock()
+
+	for _, swg := range swgs {
+		select {
+		case <-swg.WaitChannel():
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+	return nil
 }

pkg/clustermesh/remote_cluster.go
@@ -79,6 +79,8 @@ type remoteCluster struct {
 
 	// backend is the kvstore backend being used
 	backend kvstore.BackendOperations
+
+	swg *lock.StoppableWaitGroup
 }
 
 var (
@@ -181,6 +183,7 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 					Backend:                 backend,
 					Observer: &remoteServiceObserver{
 						remoteCluster: rc,
+						swg:           rc.swg,
 					},
 				})
 				if err != nil {
@@ -188,6 +191,7 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 					backend.Close()
 					return err
 				}
+				rc.swg.Stop()
 
 				ipCacheWatcher := ipcache.NewIPIdentityWatcher(backend)
 				go ipCacheWatcher.Watch()

pkg/clustermesh/services_test.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/loadbalancer"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/testutils"
 	"github.com/cilium/cilium/pkg/testutils/allocator"
 
@@ -120,6 +121,7 @@ func (s *ClusterMeshServicesTestSuite) expectEvent(c *C, action k8s.CacheAction,
 			c.Errorf("Timeout while waiting for event to be received")
 			return false
 		}
+		defer event.SWG.Done()
 
 		c.Assert(event.Action, Equals, action)
 		c.Assert(event.ID, Equals, id)
@@ -136,6 +138,7 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesGlobal(c *C) {
 	kvstore.Set(s.prepareServiceUpdate("1", "10.0.185.196", "http", "80"))
 	kvstore.Set(s.prepareServiceUpdate("2", "20.0.185.196", "http2", "90"))
 
+	swgSvcs := lock.NewStoppableWaitGroup()
 	k8sSvc := &types.Service{
 		Service: &v1.Service{
 			ObjectMeta: metav1.ObjectMeta{
@@ -152,7 +155,7 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesGlobal(c *C) {
 		},
 	}
 
-	svcID := s.svcCache.UpdateService(k8sSvc)
+	svcID := s.svcCache.UpdateService(k8sSvc, swgSvcs)
 
 	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
 		return event.Endpoints.Backends["10.0.185.196"] != nil &&
@@ -180,12 +183,13 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesGlobal(c *C) {
 		},
 	}
 
-	s.svcCache.UpdateEndpoints(k8sEndpoints)
+	swgEps := lock.NewStoppableWaitGroup()
+	s.svcCache.UpdateEndpoints(k8sEndpoints, swgEps)
 	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
 		return event.Endpoints.Backends["30.0.185.196"] != nil
 	})
 
-	s.svcCache.DeleteEndpoints(k8sEndpoints)
+	s.svcCache.DeleteEndpoints(k8sEndpoints, swgEps)
 	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
 		return event.Endpoints.Backends["30.0.185.196"] == nil
 	})
@@ -195,6 +199,18 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesGlobal(c *C) {
 
 	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName + "2")
 	s.expectEvent(c, k8s.DeleteService, svcID, nil)
+
+	swgSvcs.Stop()
+	c.Assert(testutils.WaitUntil(func() bool {
+		swgSvcs.Wait()
+		return true
+	}, 2*time.Second), IsNil)
+
+	swgEps.Stop()
+	c.Assert(testutils.WaitUntil(func() bool {
+		swgEps.Wait()
+		return true
+	}, 2*time.Second), IsNil)
 }
 
 func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesUpdate(c *C) {
@@ -217,7 +233,8 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesUpdate(c *C) {
 		},
 	}
 
-	svcID := s.svcCache.UpdateService(k8sSvc)
+	swgSvcs := lock.NewStoppableWaitGroup()
+	svcID := s.svcCache.UpdateService(k8sSvc, swgSvcs)
 
 	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
 		return event.Endpoints.Backends["10.0.185.196"]["http"].Equals(
@@ -248,6 +265,12 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesUpdate(c *C) {
 
 	s.expectEvent(c, k8s.UpdateService, svcID, nil)
 	s.expectEvent(c, k8s.DeleteService, svcID, nil)
+
+	swgSvcs.Stop()
+	c.Assert(testutils.WaitUntil(func() bool {
+		swgSvcs.Wait()
+		return true
+	}, 2*time.Second), IsNil)
 }
 
 func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesNonGlobal(c *C) {
@@ -268,7 +291,8 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesNonGlobal(c *C) {
 		},
 	}
 
-	s.svcCache.UpdateService(k8sSvc)
+	swgSvcs := lock.NewStoppableWaitGroup()
+	s.svcCache.UpdateService(k8sSvc, swgSvcs)
 
 	time.Sleep(100 * time.Millisecond)
 	select {
@@ -276,4 +300,10 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesNonGlobal(c *C) {
 		c.Errorf("Unexpected service event received: %+v", event)
 	default:
 	}
+
+	swgSvcs.Stop()
+	c.Assert(testutils.WaitUntil(func() bool {
+		swgSvcs.Wait()
+		return true
+	}, 2*time.Second), IsNil)
 }

pkg/k8s/service_cache.go
@@ -63,6 +63,10 @@ type ServiceEvent struct {
 
 	// Endpoints is the endpoints structured correlated with the service
 	Endpoints *Endpoints
+
+	// SWG provides a mechanism to detect if a service was synchronized with
+	// the datapath.
+	SWG *lock.StoppableWaitGroup
 }
 
 // ServiceCache is a list of services correlated with the matching endpoints.
@@ -108,7 +112,7 @@ func (s *ServiceCache) GetRandomBackendIP(svcID ServiceID) *loadbalancer.L3n4Add
 // ServiceCache. Returns the ServiceID unless the Kubernetes service could not
 // be parsed and a bool to indicate whether the service was changed in the
 // cache or not.
-func (s *ServiceCache) UpdateService(k8sSvc *types.Service) ServiceID {
+func (s *ServiceCache) UpdateService(k8sSvc *types.Service, swg *lock.StoppableWaitGroup) ServiceID {
 	svcID, newService := ParseService(k8sSvc)
 	if newService == nil {
 		return svcID
@@ -128,11 +132,13 @@ func (s *ServiceCache) UpdateService(k8sSvc *types.Service) ServiceID {
 	// Check if the corresponding Endpoints resource is already available
 	endpoints, serviceReady := s.correlateEndpoints(svcID)
 	if serviceReady {
+		swg.Add()
 		s.Events <- ServiceEvent{
 			Action:    UpdateService,
 			ID:        svcID,
 			Service:   newService,
 			Endpoints: endpoints,
+			SWG:       swg,
 		}
 	}
 
@@ -141,7 +147,7 @@ func (s *ServiceCache) UpdateService(k8sSvc *types.Service) ServiceID {
 
 // DeleteService parses a Kubernetes service and removes it from the
 // ServiceCache
-func (s *ServiceCache) DeleteService(k8sSvc *types.Service) {
+func (s *ServiceCache) DeleteService(k8sSvc *types.Service, swg *lock.StoppableWaitGroup) {
 	svcID := ParseServiceID(k8sSvc)
 
 	s.mutex.Lock()
@@ -152,11 +158,13 @@ func (s *ServiceCache) DeleteService(k8sSvc *types.Service) {
 	delete(s.services, svcID)
 
 	if serviceOK {
+		swg.Add()
 		s.Events <- ServiceEvent{
 			Action:    DeleteService,
 			ID:        svcID,
 			Service:   oldService,
 			Endpoints: endpoints,
+			SWG:       swg,
 		}
 	}
 }
@@ -165,7 +173,7 @@ func (s *ServiceCache) DeleteService(k8sSvc *types.Service) {
 // ServiceCache. Returns the ServiceID unless the Kubernetes endpoints could not
 // be parsed and a bool to indicate whether the endpoints was changed in the
 // cache or not.
-func (s *ServiceCache) UpdateEndpoints(k8sEndpoints *types.Endpoints) (ServiceID, *Endpoints) {
+func (s *ServiceCache) UpdateEndpoints(k8sEndpoints *types.Endpoints, swg *lock.StoppableWaitGroup) (ServiceID, *Endpoints) {
 	svcID, newEndpoints := ParseEndpoints(k8sEndpoints)
 
 	s.mutex.Lock()
@@ -183,11 +191,13 @@ func (s *ServiceCache) UpdateEndpoints(k8sEndpoints *types.Endpoints) (ServiceID
 	service, ok := s.services[svcID]
 	endpoints, serviceReady := s.correlateEndpoints(svcID)
 	if ok && serviceReady {
+		swg.Add()
 		s.Events <- ServiceEvent{
 			Action:    UpdateService,
 			ID:        svcID,
 			Service:   service,
 			Endpoints: endpoints,
+			SWG:       swg,
 		}
 	}
 
@@ -196,7 +206,7 @@ func (s *ServiceCache) UpdateEndpoints(k8sEndpoints *types.Endpoints) (ServiceID
 
 // DeleteEndpoints parses a Kubernetes endpoints and removes it from the
 // ServiceCache
-func (s *ServiceCache) DeleteEndpoints(k8sEndpoints *types.Endpoints) ServiceID {
+func (s *ServiceCache) DeleteEndpoints(k8sEndpoints *types.Endpoints, swg *lock.StoppableWaitGroup) ServiceID {
 	svcID := ParseEndpointsID(k8sEndpoints)
 
 	s.mutex.Lock()
@@ -207,11 +217,13 @@ func (s *ServiceCache) DeleteEndpoints(k8sEndpoints *types.Endpoints) ServiceID 
 	endpoints, serviceReady := s.correlateEndpoints(svcID)
 
 	if serviceOK {
+		swg.Add()
 		event := ServiceEvent{
 			Action:    DeleteService,
 			ID:        svcID,
 			Service:   service,
 			Endpoints: endpoints,
+			SWG:       swg,
 		}
 
 		if serviceReady {
@@ -326,7 +338,7 @@ func (s *ServiceCache) correlateEndpoints(id ServiceID) (*Endpoints, bool) {
 // MergeExternalServiceUpdate merges a cluster service of a remote cluster into
 // the local service cache. The service endpoints are stored as external endpoints
 // and are correlated on demand with local services via correlateEndpoints().
-func (s *ServiceCache) MergeExternalServiceUpdate(service *service.ClusterService) {
+func (s *ServiceCache) MergeExternalServiceUpdate(service *service.ClusterService, swg *lock.StoppableWaitGroup) {
 	id := ServiceID{Name: service.Name, Namespace: service.Namespace}
 	scopedLog := log.WithFields(logrus.Fields{logfields.ServiceName: service.String()})
 
@@ -358,11 +370,13 @@ func (s *ServiceCache) MergeExternalServiceUpdate(service *service.ClusterServic
 	// External endpoints are still tracked but correlation will not happen
 	// until the service is marked as shared.
 	if ok && svc.Shared && serviceReady {
+		swg.Add()
 		s.Events <- ServiceEvent{
 			Action:    UpdateService,
 			ID:        id,
 			Service:   svc,
 			Endpoints: endpoints,
+			SWG:       swg,
 		}
 	}
 }
@@ -371,7 +385,7 @@ func (s *ServiceCache) MergeExternalServiceUpdate(service *service.ClusterServic
 // remote cluster into the local service cache. The service endpoints are
 // stored as external endpoints and are correlated on demand with local
 // services via correlateEndpoints().
-func (s *ServiceCache) MergeExternalServiceDelete(service *service.ClusterService) {
+func (s *ServiceCache) MergeExternalServiceDelete(service *service.ClusterService, swg *lock.StoppableWaitGroup) {
 	scopedLog := log.WithFields(logrus.Fields{logfields.ServiceName: service.String()})
 	id := ServiceID{Name: service.Name, Namespace: service.Namespace}
 
@@ -398,11 +412,13 @@ func (s *ServiceCache) MergeExternalServiceDelete(service *service.ClusterServic
 		// endpoints are still tracked but correlation will not happen
 		// until the service is marked as shared.
 		if ok && svc.Shared {
+			swg.Add()
 			event := ServiceEvent{
 				Action:    UpdateService,
 				ID:        id,
 				Service:   svc,
 				Endpoints: endpoints,
+				SWG:       swg,
 			}
 
 			if !serviceReady {

commit 2497387c26d3e72370115260f5debeaf31dca7a1

clustermesh: fix cluster synchronization wait group increment
Previously, the StoppableWaitGroup associated with a given remote
cluster was incremented twice every service notification (both before
the merging process into the cache, and when sending the event
notification), and decremented once after the event was processed.

This delayed the start of the controller responsible for the removal of
possible leftover services deleted while Cilium was not running, until
it had reached zero through subsequent service notifications.

This commit removes the redundant Add operations, and changes the
context used to wait for the synchronization to the one associated with
the daemon.

Fixes: 27f19674c894 ("k8s: wait until k8s services to be synchronized with datapath")

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

daemon/cmd/state.go
@@ -400,10 +400,11 @@ func (d *Daemon) initRestore(restoredEndpoints *endpointRestoreState) chan struc
 				// Also wait for all cluster mesh to be synchronized with the
 				// datapath before proceeding.
 				if d.clustermesh != nil {
-					err := d.clustermesh.ClustersSynced(context.Background())
+					err := d.clustermesh.ClustersSynced(d.ctx)
 					if err != nil {
 						log.WithError(err).Fatal("timeout while waiting for all clusters to be locally synchronized")
 					}
+					log.Debug("all clusters have been correctly synchronized locally")
 				}
 				// Start controller which removes any leftover Kubernetes
 				// services that may have been deleted while Cilium was not

pkg/clustermesh/services.go
@@ -156,7 +156,6 @@ func (r *remoteServiceObserver) OnUpdate(key store.Key) {
 		mesh.globalServices.onUpdate(svc)
 
 		if merger := mesh.conf.ServiceMerger; merger != nil {
-			r.swg.Add()
 			merger.MergeExternalServiceUpdate(svc, r.swg)
 		} else {
 			scopedLog.Debugf("Ignoring remote service update. Missing merger function")
@@ -176,7 +175,6 @@ func (r *remoteServiceObserver) OnDelete(key store.NamedKey) {
 		mesh.globalServices.onDelete(svc)
 
 		if merger := mesh.conf.ServiceMerger; merger != nil {
-			r.swg.Add()
 			merger.MergeExternalServiceDelete(svc, r.swg)
 		} else {
 			scopedLog.Debugf("Ignoring remote service delete. Missing merger function")