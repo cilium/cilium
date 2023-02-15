3144f292f44d4795977393430bd6fea12289e430
Michi Mutsuzaki <michi@isovalent.com>
2023-02-15 15:01:32 +0000

    vendor: Pick up github.com/cilium/cilium v1.13.0
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Pick up cilium v1.13.0, and also update cilium/charts dependency to
    include cilium-1.13.0 Helm chart.
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- cilium-cli/vendor/github.com/cilium/proxy/go/envoy/extensions/transport_sockets/tls/v3/tls.pb.validate.go
+++ cilium-cli/vendor/github.com/cilium/proxy/go/envoy/extensions/transport_sockets/tls/v3/tls.pb.validate.go
@@ -506,6 +506,183 @@ var _ interface {
 	ErrorName() string
 } = DownstreamTlsContextValidationError{}
 
+// Validate checks the field values on TlsKeyLog with the rules defined in the
+// proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *TlsKeyLog) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on TlsKeyLog with the rules defined in
+// the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in TlsKeyLogMultiError, or nil
+// if none found.
+func (m *TlsKeyLog) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *TlsKeyLog) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if utf8.RuneCountInString(m.GetPath()) < 1 {
+		err := TlsKeyLogValidationError{
+			field:  "Path",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	for idx, item := range m.GetLocalAddressRange() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, TlsKeyLogValidationError{
+						field:  fmt.Sprintf("LocalAddressRange[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, TlsKeyLogValidationError{
+						field:  fmt.Sprintf("LocalAddressRange[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return TlsKeyLogValidationError{
+					field:  fmt.Sprintf("LocalAddressRange[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	for idx, item := range m.GetRemoteAddressRange() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, TlsKeyLogValidationError{
+						field:  fmt.Sprintf("RemoteAddressRange[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, TlsKeyLogValidationError{
+						field:  fmt.Sprintf("RemoteAddressRange[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return TlsKeyLogValidationError{
+					field:  fmt.Sprintf("RemoteAddressRange[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return TlsKeyLogMultiError(errors)
+	}
+	return nil
+}
+
+// TlsKeyLogMultiError is an error wrapping multiple validation errors returned
+// by TlsKeyLog.ValidateAll() if the designated constraints aren't met.
+type TlsKeyLogMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m TlsKeyLogMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m TlsKeyLogMultiError) AllErrors() []error { return m }
+
+// TlsKeyLogValidationError is the validation error returned by
+// TlsKeyLog.Validate if the designated constraints aren't met.
+type TlsKeyLogValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e TlsKeyLogValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e TlsKeyLogValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e TlsKeyLogValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e TlsKeyLogValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e TlsKeyLogValidationError) ErrorName() string { return "TlsKeyLogValidationError" }
+
+// Error satisfies the builtin error interface
+func (e TlsKeyLogValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sTlsKeyLog.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = TlsKeyLogValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = TlsKeyLogValidationError{}
+
 // Validate checks the field values on CommonTlsContext with the rules defined
 // in the proto definition for this message. If any rules are violated, the
 // first error encountered is returned, or nil if there are no violations.
@@ -746,6 +923,35 @@ func (m *CommonTlsContext) validate(all bool) error {
 		if err := v.Validate(); err != nil {
 			return CommonTlsContextValidationError{
 				field:  "CustomHandshaker",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if all {
+		switch v := interface{}(m.GetKeyLog()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, CommonTlsContextValidationError{
+					field:  "KeyLog",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, CommonTlsContextValidationError{
+					field:  "KeyLog",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetKeyLog()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return CommonTlsContextValidationError{
+				field:  "KeyLog",
 				reason: "embedded message failed validation",
 				cause:  err,
 			}
