1c01d518141b011ab75f398a350b4ead7c6b316b
Chris Tarazi <chris@isovalent.com>
2023-02-14 22:08:11 -0800

    ipcache: Fix incorrect CIDR labels associated with reserved identities
    An IP / CIDR can be associated with the reserved:world + CIDR labels in
    the IPcache, for example if a user applies a ToCIDR. Later, if this CIDR
    is then associated with an entity within the cluster, then the IPcache
    subsystem will not correctly handle this scenario. It will append the
    reserved:remote-node or reserved:host label, while keeping the
    reserved:world + CIDR labels.
    
    This doesn't make sense as reserved:world + CIDR labels are meant to
    represent entities outside of the cluster.
    
    This scenario can happen when a user wants to allow traffic to an IP
    that is firstly outside of the cluster, and then later joins the
    cluster.
    
    For example, a user has a K8s cluster and an external VM with an IP of
    x.y.z.w. The user wants to allow traffic to x.y.z.w so they create a
    ToCIDR rule allowing it. Later, the user joins the VM to the cluster and
    it becomes a worker node. Now x.y.z.w is an entity within the cluster
    and is therefore associated with either the reserved:remote-node or
    reserved:host label.
    
    Fix this by disassociating the reserved:world + CIDR labels from an
    entity that's now within the cluster.
    
    Related: https://github.com/cilium/cilium/issues/17962
    Fixes: https://github.com/cilium/cilium/issues/23750
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/ipcache/metadata_test.go
+++ pkg/ipcache/metadata_test.go
@@ -22,6 +22,7 @@ import (
 var (
 	worldPrefix     = netip.MustParsePrefix("1.1.1.1/32")
 	inClusterPrefix = netip.MustParsePrefix("10.0.0.4/32")
+	aPrefix         = netip.MustParsePrefix("100.4.16.32/32")
 )
 
 func TestInjectLabels(t *testing.T) {
@@ -75,6 +76,23 @@ func TestInjectLabels(t *testing.T) {
 	assert.Len(t, IPIdentityCache.ipToIdentityCache, 2)
 	assert.False(t, IPIdentityCache.ipToIdentityCache["10.0.0.4/32"].ID.HasLocalScope())
 	assert.Equal(t, identity.ReservedIdentityHealth, IPIdentityCache.ipToIdentityCache["10.0.0.4/32"].ID)
+
+	// Assert that a CIDR identity can be overridden automatically (without
+	// overrideIdentity=true) when the prefix becomes associated with an entity
+	// within the cluster.
+	IPIdentityCache.metadata.upsertLocked(aPrefix, source.Generated, "cnp-uid", labels.LabelWorld)
+	assert.Len(t, IPIdentityCache.metadata.m, 3)
+	remaining, err = IPIdentityCache.InjectLabels(ctx, []netip.Prefix{aPrefix})
+	assert.NoError(t, err)
+	assert.Len(t, remaining, 0)
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 3)
+	assert.True(t, IPIdentityCache.ipToIdentityCache["100.4.16.32/32"].ID.HasLocalScope())
+	IPIdentityCache.metadata.upsertLocked(aPrefix, source.CustomResource, "node-uid", labels.LabelRemoteNode)
+	remaining, err = IPIdentityCache.InjectLabels(ctx, []netip.Prefix{aPrefix})
+	assert.NoError(t, err)
+	assert.Len(t, remaining, 0)
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 3)
+	assert.False(t, IPIdentityCache.ipToIdentityCache["100.4.16.32/32"].ID.HasLocalScope())
 }
 
 func TestFilterMetadataByLabels(t *testing.T) {
