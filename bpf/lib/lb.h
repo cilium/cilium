29cce2fb6b85ae49834bec1f22ee742a129e4481
Julian Wiedmann <jwi@isovalent.com>
2023-02-20 13:24:14 +0200

    bpf: lb: don't handle CT_RELATED in lb_local()
    CT_RELATED is only relevant for CT tuples that have TUPLE_F_RELATED set.
    The LB code doesn't build such tuples - it uses ct_lb_lookup(), and hence
    never passes through the relevant ICMP code in ct_lookup().
    
    So treat this as unexpected condition, and drop the packet.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/lb.h
+++ bpf/lib/lb.h
@@ -887,7 +887,6 @@ static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
 		goto update_state;
 	case CT_REOPENED:
 	case CT_ESTABLISHED:
-	case CT_RELATED:
 	case CT_REPLY:
 		/* See lb4_local comment */
 		if (state->rev_nat_index == 0) {
@@ -1563,7 +1562,6 @@ static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
 		goto update_state;
 	case CT_REOPENED:
 	case CT_ESTABLISHED:
-	case CT_RELATED:
 	case CT_REPLY:
 		/* For backward-compatibility we need to update reverse NAT
 		 * index in the CT_SERVICE entry for old connections, as later
