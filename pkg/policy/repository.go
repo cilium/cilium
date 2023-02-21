cilium/cilium

commit ab7003b8bd17f867d73d4142673684dfb7d45712

certificatemanager,daemon: Modularized the certificate manager
This commit added a Cell for the certificate manager. The cell exposes
two new interfaces `CertificateManager` and `SecretManager` instead
of the manager directly.

The configuration/flags for the manager has been moved into the Cell.

Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

pkg/policy/repository.go
@@ -14,6 +14,7 @@ import (
 	cilium "github.com/cilium/proxy/go/cilium/api"
 
 	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/crypto/certificatemanager"
 	"github.com/cilium/cilium/pkg/eventqueue"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
@@ -137,9 +138,10 @@ type Repository struct {
 	// PolicyCache tracks the selector policies created from this repo
 	policyCache *PolicyCache
 
-	certManager CertificateManager
+	certManager   certificatemanager.CertificateManager
+	secretManager certificatemanager.SecretManager
 
-	getEnvoyHTTPRules func(CertificateManager, *api.L7Rules, string) (*cilium.HttpNetworkPolicyRules, bool)
+	getEnvoyHTTPRules func(certificatemanager.SecretManager, *api.L7Rules, string) (*cilium.HttpNetworkPolicyRules, bool)
 }
 
 // GetSelectorCache() returns the selector cache used by the Repository
@@ -147,7 +149,7 @@ func (p *Repository) GetSelectorCache() *SelectorCache {
 	return p.selectorCache
 }
 
-func (p *Repository) SetEnvoyRulesFunc(f func(CertificateManager, *api.L7Rules, string) (*cilium.HttpNetworkPolicyRules, bool)) {
+func (p *Repository) SetEnvoyRulesFunc(f func(certificatemanager.SecretManager, *api.L7Rules, string) (*cilium.HttpNetworkPolicyRules, bool)) {
 	p.getEnvoyHTTPRules = f
 }
 
@@ -155,7 +157,7 @@ func (p *Repository) GetEnvoyHTTPRules(l7Rules *api.L7Rules, ns string) (*cilium
 	if p.getEnvoyHTTPRules == nil {
 		return nil, true
 	}
-	return p.getEnvoyHTTPRules(p.certManager, l7Rules, ns)
+	return p.getEnvoyHTTPRules(p.secretManager, l7Rules, ns)
 }
 
 // GetPolicyCache() returns the policy cache used by the Repository
@@ -164,7 +166,12 @@ func (p *Repository) GetPolicyCache() *PolicyCache {
 }
 
 // NewPolicyRepository creates a new policy repository.
-func NewPolicyRepository(idAllocator cache.IdentityAllocator, idCache cache.IdentityCache, certManager CertificateManager) *Repository {
+func NewPolicyRepository(
+	idAllocator cache.IdentityAllocator,
+	idCache cache.IdentityCache,
+	certManager certificatemanager.CertificateManager,
+	secretManager certificatemanager.SecretManager,
+) *Repository {
 	repoChangeQueue := eventqueue.NewEventQueueBuffered("repository-change-queue", option.Config.PolicyQueueSize)
 	ruleReactionQueue := eventqueue.NewEventQueueBuffered("repository-reaction-queue", option.Config.PolicyQueueSize)
 	repoChangeQueue.Run()

daemon/cmd/cells.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"github.com/cilium/cilium/pkg/bgpv1"
+	"github.com/cilium/cilium/pkg/crypto/certificatemanager"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/gops"
@@ -64,6 +65,9 @@ var (
 
 		// NodeManager maintains a collection of other nodes in the cluster.
 		nodeManager.Cell,
+
+		// Certificate manager provides an API for retrieving secrets and certificate in the form of TLS contexts.
+		certificatemanager.Cell,
 
 		// daemonCell wraps the legacy daemon initialization and provides Promise[*Daemon].
 		daemonCell,

daemon/cmd/daemon.go
@@ -31,6 +31,7 @@ import (
 	"github.com/cilium/cilium/pkg/clustermesh"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/counter"
+	"github.com/cilium/cilium/pkg/crypto/certificatemanager"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/datapath/link"
 	linuxdatapath "github.com/cilium/cilium/pkg/datapath/linux"
@@ -407,6 +408,8 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup,
 	wgAgent *wg.Agent,
 	clientset k8sClient.Clientset,
 	sharedResources k8s.SharedResources,
+	certManager certificatemanager.CertificateManager,
+	secretManager certificatemanager.SecretManager,
 ) (*Daemon, *endpointRestoreState, error) {
 
 	var (
@@ -603,7 +606,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup,
 	// TODO: convert these package level variables to types for easier unit
 	// testing in the future.
 	d.identityAllocator = NewCachingIdentityAllocator(&d)
-	if err := d.initPolicy(epMgr); err != nil {
+	if err := d.initPolicy(epMgr, certManager, secretManager); err != nil {
 		return nil, nil, fmt.Errorf("error while initializing policy subsystem: %w", err)
 	}
 	d.ipcache = ipcache.NewIPCache(&ipcache.Configuration{

daemon/cmd/daemon_main.go
@@ -32,6 +32,7 @@ import (
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/components"
 	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/crypto/certificatemanager"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/datapath/iptables"
 	"github.com/cilium/cilium/pkg/datapath/link"
@@ -782,9 +783,6 @@ func initializeFlags() {
 
 	flags.Int(option.CTMapEntriesGlobalTCPName, option.CTMapEntriesGlobalTCPDefault, "Maximum number of entries in TCP CT table")
 	option.BindEnvWithLegacyEnvFallback(Vp, option.CTMapEntriesGlobalTCPName, "CILIUM_GLOBAL_CT_MAX_TCP")
-
-	flags.String(option.CertsDirectory, defaults.CertsDirectory, "Root directory to find certificates specified in L7 TLS policy enforcement")
-	option.BindEnv(Vp, option.CertsDirectory)
 
 	flags.Int(option.CTMapEntriesGlobalAnyName, option.CTMapEntriesGlobalAnyDefault, "Maximum number of entries in non-TCP CT table")
 	option.BindEnvWithLegacyEnvFallback(Vp, option.CTMapEntriesGlobalAnyName, "CILIUM_GLOBAL_CT_MAX_ANY")
@@ -1662,6 +1660,8 @@ type daemonParams struct {
 	SharedResources k8s.SharedResources
 	NodeManager     nodeManager.NodeManager
 	EndpointManager endpointmanager.EndpointManager
+	CertManager     certificatemanager.CertificateManager
+	SecretManager   certificatemanager.SecretManager
 }
 
 func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
@@ -1690,7 +1690,9 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 				params.Datapath,
 				params.WGAgent,
 				params.Clientset,
-				params.SharedResources)
+				params.SharedResources,
+				params.CertManager,
+				params.SecretManager)
 			if err != nil {
 				return fmt.Errorf("daemon creation failed: %w", err)
 			}

daemon/cmd/fqdn_test.go
@@ -123,7 +123,7 @@ func (ds *DaemonFQDNSuite) SetUpTest(c *C) {
 	d := &Daemon{}
 	d.initDNSProxyContext(128)
 	d.identityAllocator = NewFakeIdentityAllocator(nil)
-	d.policy = policy.NewPolicyRepository(d.identityAllocator, nil, nil)
+	d.policy = policy.NewPolicyRepository(d.identityAllocator, nil, nil, nil)
 	d.dnsNameManager = fqdn.NewNameManager(fqdn.Config{
 		MinTTL:          1,
 		Cache:           fqdn.NewDNSCache(0),

daemon/cmd/policy.go
@@ -41,7 +41,11 @@ import (
 )
 
 // initPolicy initializes the core policy components of the daemon.
-func (d *Daemon) initPolicy(epMgr endpointmanager.EndpointManager) error {
+func (d *Daemon) initPolicy(
+	epMgr endpointmanager.EndpointManager,
+	certManager certificatemanager.CertificateManager,
+	secretManager certificatemanager.SecretManager,
+) error {
 	// Reuse policy.TriggerMetrics and PolicyTriggerInterval here since
 	// this is only triggered by agent configuration changes for now and
 	// should be counted in pol.TriggerMetrics.
@@ -58,7 +62,9 @@ func (d *Daemon) initPolicy(epMgr endpointmanager.EndpointManager) error {
 
 	d.policy = policy.NewPolicyRepository(d.identityAllocator,
 		d.identityAllocator.GetIdentityCache(),
-		certificatemanager.NewManager(option.Config.CertDirectory, d.clientset))
+		certManager,
+		secretManager,
+	)
 	d.policy.SetEnvoyRulesFunc(envoy.GetEnvoyHTTPRules)
 	d.policyUpdater, err = policy.NewUpdater(d.policy, epMgr)
 	if err != nil {

daemon/cmd/policy_test.go
@@ -1023,11 +1023,11 @@ func (ds *DaemonSuite) Test_addCiliumNetworkPolicyV2(c *C) {
 							},
 						},
 					},
-					repo: policy.NewPolicyRepository(nil, nil, nil),
+					repo: policy.NewPolicyRepository(nil, nil, nil, nil),
 				}
 			},
 			setupWanted: func() wanted {
-				r := policy.NewPolicyRepository(nil, nil, nil)
+				r := policy.NewPolicyRepository(nil, nil, nil, nil)
 				r.AddList(api.Rules{
 					api.NewRule().
 						WithEndpointSelector(api.EndpointSelector{
@@ -1056,7 +1056,7 @@ func (ds *DaemonSuite) Test_addCiliumNetworkPolicyV2(c *C) {
 		{
 			name: "have a rule with user labels and update it without user labels, all other rules should be deleted",
 			setupArgs: func() args {
-				r := policy.NewPolicyRepository(nil, nil, nil)
+				r := policy.NewPolicyRepository(nil, nil, nil, nil)
 				lbls := utils.GetPolicyLabels("production", "db", uuid, utils.ResourceTypeCiliumNetworkPolicy)
 				lbls = append(lbls, labels.ParseLabelArray("foo=bar")...).Sort()
 				r.AddList(api.Rules{
@@ -1099,7 +1099,7 @@ func (ds *DaemonSuite) Test_addCiliumNetworkPolicyV2(c *C) {
 				}
 			},
 			setupWanted: func() wanted {
-				r := policy.NewPolicyRepository(nil, nil, nil)
+				r := policy.NewPolicyRepository(nil, nil, nil, nil)
 				r.AddList(api.Rules{
 					api.NewRule().
 						WithEndpointSelector(api.EndpointSelector{
@@ -1128,7 +1128,7 @@ func (ds *DaemonSuite) Test_addCiliumNetworkPolicyV2(c *C) {
 		{
 			name: "have a rule without user labels and update it with user labels, all other rules should be deleted",
 			setupArgs: func() args {
-				r := policy.NewPolicyRepository(nil, nil, nil)
+				r := policy.NewPolicyRepository(nil, nil, nil, nil)
 				r.AddList(api.Rules{
 					{
 						EndpointSelector: api.EndpointSelector{
@@ -1170,7 +1170,7 @@ func (ds *DaemonSuite) Test_addCiliumNetworkPolicyV2(c *C) {
 				}
 			},
 			setupWanted: func() wanted {
-				r := policy.NewPolicyRepository(nil, nil, nil)
+				r := policy.NewPolicyRepository(nil, nil, nil, nil)
 				lbls := utils.GetPolicyLabels("production", "db", uuid, utils.ResourceTypeCiliumNetworkPolicy)
 				lbls = append(lbls, labels.ParseLabelArray("foo=bar")...).Sort()
 				r.AddList(api.Rules{
@@ -1196,7 +1196,7 @@ func (ds *DaemonSuite) Test_addCiliumNetworkPolicyV2(c *C) {
 		{
 			name: "have a rule policy installed with multiple rules and apply an empty spec should delete all rules installed",
 			setupArgs: func() args {
-				r := policy.NewPolicyRepository(nil, nil, nil)
+				r := policy.NewPolicyRepository(nil, nil, nil, nil)
 				r.AddList(api.Rules{
 					{
 						EndpointSelector: api.EndpointSelector{
@@ -1243,7 +1243,7 @@ func (ds *DaemonSuite) Test_addCiliumNetworkPolicyV2(c *C) {
 				}
 			},
 			setupWanted: func() wanted {
-				r := policy.NewPolicyRepository(nil, nil, nil)
+				r := policy.NewPolicyRepository(nil, nil, nil, nil)
 				r.AddList(api.Rules{
 					{
 						EndpointSelector: api.EndpointSelector{

pkg/crypto/certificatemanager/certificate_manager.go
@@ -9,37 +9,65 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/cilium/cilium/pkg/hive/cell"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/policy/api"
+
+	"github.com/spf13/pflag"
 )
 
-const (
-	caDefaultName      = "ca.crt"
-	publicDefaultName  = "tls.crt"
-	privateDefaultName = "tls.key"
+var Cell = cell.Module(
+	"certificate-manager",
+	"Provides TLS certificates and secrets",
+
+	cell.Provide(NewManager),
+
+	cell.Config(defaultManagerConfig),
 )
 
-type k8sClient interface {
-	GetSecrets(ctx context.Context, ns, name string) (map[string][]byte, error)
+type CertificateManager interface {
+	GetTLSContext(ctx context.Context, tlsCtx *api.TLSContext, ns string) (ca, public, private string, err error)
+}
+
+type SecretManager interface {
+	GetSecrets(ctx context.Context, secret *api.Secret, ns string) (string, map[string][]byte, error)
+	GetSecretString(ctx context.Context, secret *api.Secret, ns string) (string, error)
+}
+
+var defaultManagerConfig = managerConfig{
+	CertificatesDirectory: "/var/run/cilium/certs",
+}
+
+type managerConfig struct {
+	// CertificatesDirectory is the root directory to be used by cilium to find
+	// certificates locally.
+	CertificatesDirectory string
+}
+
+func (mc managerConfig) Flags(flags *pflag.FlagSet) {
+	flags.String("certificates-directory", mc.CertificatesDirectory, "Root directory to find certificates specified in L7 TLS policy enforcement")
 }
 
 // Manager will manage the way certificates are retrieved based in the given
 // k8sClient and rootPath.
-type Manager struct {
+type manager struct {
 	rootPath  string
-	k8sClient k8sClient
+	k8sClient k8sClient.Clientset
 }
 
 // NewManager returns a new manager.
-func NewManager(certsRootPath string, k8sClient k8sClient) *Manager {
-	return &Manager{
-		rootPath:  certsRootPath,
-		k8sClient: k8sClient,
+func NewManager(cfg managerConfig, clientset k8sClient.Clientset) (CertificateManager, SecretManager) {
+	m := &manager{
+		rootPath:  cfg.CertificatesDirectory,
+		k8sClient: clientset,
 	}
+
+	return m, m
 }
 
 // GetSecrets returns either local or k8s secrets, giving precedence for local secrets if configured.
 // The 'ns' parameter is used as the secret namespace if 'secret.Namespace' is an empty string.
-func (m *Manager) GetSecrets(ctx context.Context, secret *api.Secret, ns string) (string, map[string][]byte, error) {
+func (m *manager) GetSecrets(ctx context.Context, secret *api.Secret, ns string) (string, map[string][]byte, error) {
 	if secret == nil {
 		return "", nil, fmt.Errorf("Secret must not be nil")
 	}
@@ -78,9 +106,15 @@ func (m *Manager) GetSecrets(ctx context.Context, secret *api.Secret, ns string)
 	return nsName, secrets, err
 }
 
+const (
+	caDefaultName      = "ca.crt"
+	publicDefaultName  = "tls.crt"
+	privateDefaultName = "tls.key"
+)
+
 // GetTLSContext returns a new ca, public and private certificates found based
 // in the given api.TLSContext.
-func (m *Manager) GetTLSContext(ctx context.Context, tlsCtx *api.TLSContext, ns string) (ca, public, private string, err error) {
+func (m *manager) GetTLSContext(ctx context.Context, tlsCtx *api.TLSContext, ns string) (ca, public, private string, err error) {
 	name, secrets, err := m.GetSecrets(ctx, tlsCtx.Secret, ns)
 	if err != nil {
 		return "", "", "", err
@@ -127,7 +161,7 @@ func (m *Manager) GetTLSContext(ctx context.Context, tlsCtx *api.TLSContext, ns 
 }
 
 // GetSecretString returns a secret string stored in a k8s secret
-func (m *Manager) GetSecretString(ctx context.Context, secret *api.Secret, ns string) (string, error) {
+func (m *manager) GetSecretString(ctx context.Context, secret *api.Secret, ns string) (string, error) {
 	name, secrets, err := m.GetSecrets(ctx, secret, ns)
 	if err != nil {
 		return "", err

commit c7ea0d47ab8e2a65c7738ac36fde8cd6574150ff

policy: set Repository.secretManager in NewPolicyRepository
Otherwise Repository.secretManager would always be nil regardless of the
secretManager parameter passed to NewPolicyRepository.

Without this change, the currently quarantined k8s TLS policy test will
consistently fail with ""Failed fetching K8s Secret, header match will
fail" in the logs, see e.g.
https://jenkins.cilium.io/view/Quarantine%20Pipelines/job/cilium-master-k8s-1.16-kernel-4.19-quarantine/1599

Fixes: ab7003b8bd17 ("certificatemanager,daemon: Modularized the certificate manager")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/policy/repository.go
@@ -179,6 +179,7 @@ func NewPolicyRepository(
 		RuleReactionQueue:     ruleReactionQueue,
 		selectorCache:         selectorCache,
 		certManager:           certManager,
+		secretManager:         secretManager,
 	}
 	repo.policyCache = NewPolicyCache(repo, true)
 	return repo