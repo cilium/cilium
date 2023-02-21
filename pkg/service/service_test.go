96f296d15f087892680263d2523b07db54c629cf
Aditi Ghag <aditi@cilium.io>
2023-02-21 06:12:18 -0800

    pkg/service: Extend unit test cases
    [ upstream commit bca13aef3c29228567cf56d3b81190d82b3150f2 ]
    
    The commit adds a unit test to delete a service with non-active
    backends. This is to catch any regressions (such as backend leaks) [1]
    in the logic that gracefully terminates such backends.
    
    [1] https://github.com/cilium/cilium/pull/23858.
    
    Signed-off-by: Aditi Ghag <aditi@cilium.io>
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -191,6 +191,10 @@ var (
 	backends4 = []*lb.Backend{
 		lb.NewBackend(0, lb.TCP, cmtypes.MustParseAddrCluster("10.0.0.4"), 8080),
 	}
+	backends5 = []*lb.Backend{
+		lb.NewBackend(0, lb.TCP, cmtypes.MustParseAddrCluster("10.0.0.5"), 8080),
+		lb.NewBackend(0, lb.TCP, cmtypes.MustParseAddrCluster("10.0.0.6"), 8080),
+	}
 )
 
 func (m *ManagerTestSuite) TestUpsertAndDeleteService(c *C) {
@@ -1798,4 +1802,32 @@ func (m *ManagerTestSuite) TestTrafficPolicy(c *C) {
 	found, err = m.svc.DeleteServiceByID(lb.ServiceID(id2))
 	c.Assert(err, IsNil)
 	c.Assert(found, Equals, true)
+}
+
+// Tests whether delete service handles non-active backends.
+func (m *ManagerTestSuite) TestDeleteServiceWithTerminatingBackends(c *C) {
+	backends := backends5
+	backends[0].State = lb.BackendStateTerminating
+	p := &lb.SVC{
+		Frontend: frontend1,
+		Backends: backends,
+		Name:     lb.ServiceName{Name: "svc1", Namespace: "ns1"},
+	}
+
+	created, id1, err := m.svc.UpsertService(p)
+
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, true)
+	c.Assert(id1, Equals, lb.ID(1))
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id1)].Backends), Equals, 2)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
+	c.Assert(m.svc.svcByID[id1].svcName, Equals, lb.ServiceName{Name: "svc1", Namespace: "ns1"})
+
+	// Delete service.
+	found, err := m.svc.DeleteServiceByID(lb.ServiceID(id1))
+
+	c.Assert(err, IsNil)
+	c.Assert(found, Equals, true)
+	c.Assert(len(m.lbmap.ServiceByID), Equals, 0)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 0)
 }
