d9d11fa3ee29d79892d20acd555b59bef06003a1
Julian Wiedmann <jwi@isovalent.com>
2023-02-21 18:23:29 +0200

    egressgw: make nodeSelector optional
    When no nodeSelector is specified, fall back to using the SNAT egressIP as
    gatewayIP.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- test/k8s/egress.go
+++ test/k8s/egress.go
@@ -313,6 +313,43 @@ var _ = SkipDescribeIf(func() bool {
 					testConnectivity(true, ciliumOpts)
 				})
 			})
+
+			Context("egress gw policy without nodeSelector", func() {
+				BeforeAll(func() {
+					applyEgressPolicy("egress-gateway-policy-without-nodeselector.yaml")
+
+					// Wait for 6 entries:
+					// - 3 policies, each with:
+					//   - 2 matching endpoints
+					//   - 1 destination CIDR
+
+					err := kubectl.WaitForEgressPolicyEntries(helpers.K8s1, 6)
+					Expect(err).Should(BeNil(), "Failed waiting for egress policy map entries")
+
+					err = kubectl.WaitForEgressPolicyEntries(helpers.K8s2, 6)
+					Expect(err).Should(BeNil(), "Failed waiting for egress policy map entries")
+				})
+				AfterAll(func() {
+					kubectl.Delete(policyYAML)
+
+					err := kubectl.WaitForEgressPolicyEntries(helpers.K8s1, 0)
+					Expect(err).Should(BeNil(), "Failed waiting for egress policy map entries")
+
+					err = kubectl.WaitForEgressPolicyEntries(helpers.K8s2, 0)
+					Expect(err).Should(BeNil(), "Failed waiting for egress policy map entries")
+				})
+
+				AfterFailed(func() {
+					kubectl.CiliumReport("cilium bpf egress list", "cilium bpf nat list")
+				})
+
+				It("both egress gw and basic connectivity work", func() {
+					testEgressGateway(false)
+					testEgressGateway(true)
+					testConnectivity(false, ciliumOpts)
+					testConnectivity(true, ciliumOpts)
+				})
+			})
 		})
 	}
 
