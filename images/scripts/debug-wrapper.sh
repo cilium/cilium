41588a8d9c3b424129b3cb9041139238fa6ce1a6
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-02-21 11:26:35 +0100

    images: configurable debug port in debug-wrapper.sh
    This commit adds the possibility to configure the debug port as
    environment variable DEBUG_PORT. It still defaults to 2345.
    
    This adds the possibility to use the debug-wrapper for the cilium
    operator which needs to expose the debugger on a different port than the
    agent.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- images/scripts/debug-wrapper.sh
+++ images/scripts/debug-wrapper.sh
@@ -4,8 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+DEBUG_PORT="${DEBUG_PORT:=2345}"
+
 /usr/bin/dlv \
-    --listen=:2345 \
+    --listen=":${DEBUG_PORT}" \
     --headless=true \
     --log=true \
     --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc \
