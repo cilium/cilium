e0908612c4958a5ad00269ba8980c7abca074117
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-02-21 11:28:05 +0100

    images: optionally continue delve debugger at startup via flag --continue
    By passing the environment variable DEBUG_CONTINUE=true to the make
    target, the devle flag --continue is set. This way, the cilium agent
    & operator don't wait for an attached debugger.
    
    Without this change, it might gets a tedious action to attach the
    debugger to all pods.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- images/scripts/debug-wrapper.sh
+++ images/scripts/debug-wrapper.sh
@@ -5,10 +5,12 @@ set -o nounset
 set -o pipefail
 
 DEBUG_PORT="${DEBUG_PORT:=2345}"
+DEBUG_CONTINUE="${DEBUG_CONTINUE:=false}"
 
 /usr/bin/dlv \
     --listen=":${DEBUG_PORT}" \
     --headless=true \
+    --continue="${DEBUG_CONTINUE}" \
     --log=true \
     --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc \
     --accept-multiclient \
