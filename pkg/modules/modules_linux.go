49166d45dba301a8e94d17e0197d89373a7f3aa7
TheAifam5 <theaifam5@gmail.com>
2023-02-22 23:36:50 +0100

    modules_linux: Add support for builtin modules
    [ upstream commit 51630941ac2f19db83d3d7a88e2a8a17fcf8e814 ]
    
    Previously, kernel modules names were read from the `/proc/modules` only which prevented k8s linux distribution with kernel modules builtin into the kernel to pass the validation of required kernel modules by Cilium.
    
    This patch adds support for systems with builtin modules by trying to read the kernel module names from following files additionally:
    - `/lib/modules/<kernel-version>/modules.builtin`
    - `/usr/lib/modules/<kernel-version>/modules.builtin`
    - `/usr/lib/debug/lib/modules/<kernel-version>/modules.builtin`
    
    Fixes: #23863
    
    Signed-off-by: Mateusz Paluszkiewicz (TheAifam5) <theaifam5@gmail.com>
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/modules/modules_linux.go
+++ pkg/modules/modules_linux.go
@@ -5,22 +5,40 @@ package modules
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
+
+	linux "golang.org/x/sys/unix"
 )
 
 const (
-	modulesFilepath = "/proc/modules"
+	loadedModulesFilepath = "/proc/modules"
 )
 
 func moduleLoader() string {
 	return "modprobe"
 }
 
-// parseModulesFile returns the list of loaded kernel modules names.
-func parseModulesFile(r io.Reader) ([]string, error) {
+// kernelRelease returns the release string of the running kernel.
+// Its format depends on the Linux distribution and corresponds to directory
+// names in /lib/modules by convention. Some examples are 5.15.17-1-lts and
+// 4.19.0-16-amd64.
+// Note: copied from /vendor/github.com/cilium/ebpf/internal/version.go
+func kernelRelease() (string, error) {
+	var uname linux.Utsname
+	if err := linux.Uname(&uname); err != nil {
+		return "", fmt.Errorf("uname failed: %w", err)
+	}
+
+	return linux.ByteSliceToString(uname.Release[:]), nil
+}
+
+// parseLoadedModulesFile returns the list of loaded kernel modules names.
+func parseLoadedModulesFile(r io.Reader) ([]string, error) {
 	var result []string
 
 	scanner := bufio.NewScanner(r)
@@ -45,15 +63,94 @@ func parseModulesFile(r io.Reader) ([]string, error) {
 	return result, nil
 }
 
-// listModules returns the list of loaded kernel modules names parsed from
-// /proc/modules.
-func listModules() ([]string, error) {
-	fModules, err := os.Open(modulesFilepath)
+// parseBuiltinModulesFile returns the list of builtin kernel modules names.
+func parseBuiltinModulesFile(r io.Reader) ([]string, error) {
+	var result []string
+
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		modulePathRaw := scanner.Text()
+		moduleFileName := filepath.Base(modulePathRaw)
+		moduleFileExt := filepath.Ext(modulePathRaw)
+		moduleName := strings.TrimSuffix(moduleFileName, moduleFileExt)
+		result = append(result, moduleName)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// listLoadedModules returns the parsed list of loaded kernel modules names.
+func listLoadedModules() ([]string, error) {
+	fModules, err := os.Open(loadedModulesFilepath)
 	if err != nil {
 		return nil, fmt.Errorf(
-			"failed to open modules information at %s: %s",
-			modulesFilepath, err)
+			"failed to open loaded modules information at %s: %w",
+			loadedModulesFilepath, err)
 	}
 	defer fModules.Close()
-	return parseModulesFile(fModules)
+	return parseLoadedModulesFile(fModules)
+}
+
+// listBuiltinModules returns the parsed list of builtin kernel modules names.
+func listBuiltinModules() ([]string, error) {
+	var result []string
+
+	locations := []string{
+		"/lib/modules/%s/modules.builtin",
+		"/usr/lib/modules/%s/modules.builtin",
+		"/usr/lib/debug/lib/modules/%s/modules.builtin",
+	}
+
+	release, err := kernelRelease()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, location := range locations {
+		fModulePath := fmt.Sprintf(location, release)
+
+		fModules, err := os.Open(fModulePath)
+		if errors.Is(err, os.ErrNotExist) {
+			continue
+		}
+
+		if err != nil {
+			return nil, fmt.Errorf(
+				"failed to open builtin modules information at %s: %w",
+				fModulePath, err)
+		}
+
+		defer fModules.Close()
+
+		result, err = parseBuiltinModulesFile(fModules)
+		if err != nil {
+			return nil, err
+		}
+
+		break
+	}
+
+	return result, nil
+}
+
+// listModules returns the list of loaded kernel modules names parsed from
+// /proc/modules and from /lib/modules/<version>/modules.builtin.
+func listModules() ([]string, error) {
+	loadedModules, err := listLoadedModules()
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve loaded modules names: %w", err)
+	}
+
+	builtinModules, err := listBuiltinModules()
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve builtin modules names: %w", err)
+	}
+
+	return append(loadedModules, builtinModules...), nil
 }
