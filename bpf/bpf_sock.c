c24d7e932bffcf178395379fdbb845691cd74777
Martynas Pumputis <m@lambda.lt>
2023-02-23 14:39:39 +0100

    Revert "bpf: Return -EADDRINUSE from post_bind where possible"
    This reverts commit 6937f679d02af3508607b7ddab75e527059403ae.

--- bpf/bpf_sock.c
+++ bpf/bpf_sock.c
@@ -505,13 +505,8 @@ static __always_inline int __sock4_post_bind(struct bpf_sock *ctx,
 __section("cgroup/post_bind4")
 int cil_sock4_post_bind(struct bpf_sock *ctx)
 {
-	int err;
-
-	err = __sock4_post_bind(ctx, ctx);
-	if (err < 0) {
-		try_set_retval(err);
+	if (__sock4_post_bind(ctx, ctx) < 0)
 		return SYS_REJECT;
-	}
 
 	return SYS_PROCEED;
 }
@@ -869,13 +864,8 @@ static __always_inline int __sock6_post_bind(struct bpf_sock *ctx)
 __section("cgroup/post_bind6")
 int cil_sock6_post_bind(struct bpf_sock *ctx)
 {
-	int err;
-
-	err = __sock6_post_bind(ctx);
-	if (err < 0) {
-		try_set_retval(err);
+	if (__sock6_post_bind(ctx) < 0)
 		return SYS_REJECT;
-	}
 
 	return SYS_PROCEED;
 }
