3cdf6a5bd12524543f8d1037d55223154ef50865
Jussi Maki <jussi@isovalent.com>
2023-02-28 18:30:48 +0100

    daemon: Check for leaked goroutines in cells test
    To make sure constructors don't spawn goroutines, use
    goleak in the TestAgentCell test to catch them.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- daemon/cmd/cells_test.go
+++ daemon/cmd/cells_test.go
@@ -7,18 +7,28 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/goleak"
 
 	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/metrics"
 )
+
+var goleakOptions = []goleak.Option{
+	// Ignore all the currently running goroutines spawned
+	// by prior tests or by package init() functions (like the
+	// client-go logger).
+	goleak.IgnoreCurrent(),
+}
 
 // TestAgentCell verifies that the Agent hive can be instantiated with
 // default configuration and thus the Agent hive can be inspected with
 // the hive commands and documentation can be generated from it.
 func TestAgentCell(t *testing.T) {
+	defer goleak.VerifyNone(t, goleakOptions...)
 	defer metrics.ResetMetrics()
 
+	logging.SetLogLevelToDebug()
 	err := hive.New(Agent).Populate()
 	assert.NoError(t, err, "Populate()")
-
 }
