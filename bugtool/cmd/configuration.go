cilium/cilium

commit b13dc89166e9a3d9eafe4a77fd96b389a05cfe1a

Bugtool: Add additional tc commands.
The tc command prints out information not shown by bpftool.
As well, it is possible that we may need information about
tc entities that are not managed by Cilium when debugging
Cilium issues.

This adds extra bugtool commands to be run with cilium-bugtool.
Including listing tc qdisc and getting filter/class/chain info
for all network interfaces.

Fixes: #17468

Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

bugtool/cmd/configuration.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 	"strings"
@@ -121,6 +122,9 @@ func defaultCommands(confDir string, cmdDir string, k8sPods []string) []string {
 		// Get list of open file descriptors managed by the agent
 		fmt.Sprintf("ls -la /proc/$(pidof %s)/fd", components.CiliumAgentName),
 		"lsmod",
+		// tc
+		"tc -s qdisc", // Show statistics on queuing disciplines
+		"tc qdisc show",
 	}
 
 	if bpffsMountpoint := bpffsMountpoint(); bpffsMountpoint != "" {
@@ -194,6 +198,13 @@ func defaultCommands(confDir string, cmdDir string, k8sPods []string) []string {
 	commands = append(commands, copyConfigCommands(confDir, k8sPods)...)
 	commands = append(commands, copyCiliumInfoCommands(cmdDir, k8sPods)...)
 
+	tcCommands, err := tcInterfaceCommands()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to generate per interface tc commands: %s\n", err)
+	} else {
+		commands = append(commands, tcCommands...)
+	}
+
 	return k8sCommands(commands, k8sPods)
 }
 
@@ -227,6 +238,23 @@ func loadConfigFile(path string) (*BugtoolConfiguration, error) {
 	var c BugtoolConfiguration
 	err = json.Unmarshal(content, &c)
 	return &c, err
+}
+
+// Listing tc filter/chain/classes requires specific interface names.
+// Commands are generated per-interface.
+func tcInterfaceCommands() ([]string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, fmt.Errorf("could not list network interfaces: %v", err)
+	}
+	commands := []string{}
+	for _, iface := range ifaces {
+		commands = append(commands,
+			fmt.Sprintf("tc filter show dev %s", iface.Name),
+			fmt.Sprintf("tc chain show dev %s", iface.Name),
+			fmt.Sprintf("tc class show dev %s", iface.Name))
+	}
+	return commands, nil
 }
 
 func catCommands() []string {

commit 656d5239f36df08c2023d1ecffa80a00fd905c27

bugtool: Add ingress/egress tc filter dump
tc filter show dev <dev> doesn't typically show anything, you need to
add ingress/egress direction afterwards to pull the actual output.

CC: Tom Hadlaw <tom.hadlaw@isovalent.com>
Fixes: b13dc89166e9 ("Bugtool: Add additional tc commands.")
Signed-off-by: Joe Stringer <joe@cilium.io>

bugtool/cmd/configuration.go
@@ -256,7 +256,8 @@ func tcInterfaceCommands() ([]string, error) {
 	commands := []string{}
 	for _, iface := range ifaces {
 		commands = append(commands,
-			fmt.Sprintf("tc filter show dev %s", iface.Name),
+			fmt.Sprintf("tc filter show dev %s ingress", iface.Name),
+			fmt.Sprintf("tc filter show dev %s egress", iface.Name),
 			fmt.Sprintf("tc chain show dev %s", iface.Name),
 			fmt.Sprintf("tc class show dev %s", iface.Name))
 	}