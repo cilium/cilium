a4fd26ed5c5bbf8f70e48ba3f3d6545d7a386c1b
Tobias Klauser <tobias@cilium.io>
2023-03-01 10:43:09 +0100

    go.mod, vendor: pull in latest cilium charts for v1.14.0-snapshot.0
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/vendor/github.com/cilium/charts/generate_readme.sh
+++ cilium-cli/vendor/github.com/cilium/charts/generate_readme.sh
@@ -7,7 +7,13 @@ This repository holds helm templates for the following Cilium releases:
 
 EOF
 
-for version in $(find -- * -name 'cilium-*.tgz' ! -name "*dev*" | cut -d - -f 2- | cut -d . -f -3 | sed '/-/!{s/$/_/}' | sort -Vr | sed 's/_$//'); do
+for version in \
+    $(find -- * -name 'cilium-*.tgz' ! -name "*dev*" \
+    | cut -d - -f 2- \
+    | xargs basename -s .tgz \
+    | sed '/-/!{s/$/_/}' \
+    | sort -Vr \
+    | sed 's/_$//'); do
   echo "* [v$version](https://github.com/cilium/cilium/releases/tag/v$version) (_[source](https://github.com/cilium/cilium/tree/v$version/install/kubernetes/cilium)_)"
 done
 
@@ -17,7 +23,13 @@ This repository holds helm templates for the following Tetragon releases:
 
 EOF
 
-for version in $(find -- * -name 'tetragon-*.tgz' ! -name "*dev*" | cut -d - -f 2- | cut -d . -f -3 | sed '/-/!{s/$/_/}' | sort -Vr | sed 's/_$//'); do
+for version in \
+    $(find -- * -name 'tetragon-*.tgz' ! -name "*dev*" \
+    | cut -d - -f 2- \
+    | xargs basename -s .tgz \
+    | sed '/-/!{s/$/_/}' \
+    | sort -Vr \
+    | sed 's/_$//'); do
   echo "* [v$version](https://github.com/cilium/tetragon/releases/tag/v$version) (_[source](https://github.com/cilium/tetragon/tree/v$version/install/kubernetes)_)"
 done
 
@@ -27,7 +39,10 @@ The following Cilium development releases pull the latest backports for the corr
 
 EOF
 
-for version in $(find -- * -name 'cilium-*-dev.tgz' | cut -d - -f 2 | sort -Vr ); do
+for version in \
+    $(find -- * -name 'cilium-*-dev.tgz' \
+    | cut -d - -f 2  \
+    | sort -Vr ); do
   echo "* [v$version-dev](https://github.com/cilium/cilium/tree/v$version) (_[source](https://github.com/cilium/cilium/tree/v$version/install/kubernetes/cilium)_)"
 done
 
