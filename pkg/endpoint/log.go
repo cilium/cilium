cilium/cilium

commit f3a4c4d204cf84af3d40f4782aa68e7c2da98440

endpoint: convert Endpoint from addressing.CiliumIPv{4,6} to netip.Addr
A netip.Addr always uses 24 bytes, regardless of the address type. An
addressing.CiliumIPv4 uses 28 bytes (24 bytes for the slice header and 4
bytes for the address). An addressing.CiliumIPv6 uses 56 bytes (24 bytes
for the slice header and 32 bytes for the address). Also see [1] for
more details on further advantages of the netip types in terms of
performance.

[1] https://tailscale.com/blog/netaddr-new-ip-type-for-go/

This change thus slightly reduces the in-memory size of the Endpoint
type and also allows to drop the custom addressing types in favor of Go
standard library types with corresponding helper functions and methods.

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/endpoint/log.go
@@ -117,8 +117,8 @@ func (e *Endpoint) UpdateLogger(fields map[string]interface{}) {
 		logfields.ContainerID:            e.getShortContainerID(),
 		logfields.DatapathPolicyRevision: e.policyRevision,
 		logfields.DesiredPolicyRevision:  e.nextPolicyRevision,
-		logfields.IPv4:                   e.IPv4.String(),
-		logfields.IPv6:                   e.IPv6.String(),
+		logfields.IPv4:                   e.GetIPv4Address(),
+		logfields.IPv6:                   e.GetIPv4Address(),
 		logfields.K8sPodName:             e.getK8sNamespaceAndPodName(),
 	})
 
@@ -176,8 +176,8 @@ func (e *Endpoint) updatePolicyLogger(fields map[string]interface{}) {
 			logfields.ContainerID:            e.getShortContainerID(),
 			logfields.DatapathPolicyRevision: e.policyRevision,
 			logfields.DesiredPolicyRevision:  e.nextPolicyRevision,
-			logfields.IPv4:                   e.IPv4.String(),
-			logfields.IPv6:                   e.IPv6.String(),
+			logfields.IPv4:                   e.GetIPv4Address(),
+			logfields.IPv6:                   e.GetIPv4Address(),
 			logfields.K8sPodName:             e.getK8sNamespaceAndPodName(),
 		})
 

daemon/cmd/endpoint.go
@@ -344,11 +344,11 @@ func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, e
 
 	var checkIDs []string
 
-	if ep.IPv4.IsSet() {
+	if ep.IPv4.IsValid() {
 		checkIDs = append(checkIDs, endpointid.NewID(endpointid.IPv4Prefix, ep.IPv4.String()))
 	}
 
-	if ep.IPv6.IsSet() {
+	if ep.IPv6.IsValid() {
 		checkIDs = append(checkIDs, endpointid.NewID(endpointid.IPv6Prefix, ep.IPv6.String()))
 	}
 
@@ -713,13 +713,13 @@ func (d *Daemon) EndpointDeleted(ep *endpoint.Endpoint, conf endpoint.DeleteConf
 
 	if !conf.NoIPRelease {
 		if option.Config.EnableIPv4 {
-			if err := d.ipam.ReleaseIP(ep.IPv4.IP()); err != nil {
+			if err := d.ipam.ReleaseIP(ep.IPv4.AsSlice()); err != nil {
 				scopedLog := ep.Logger(daemonSubsys).WithError(err)
 				scopedLog.Warning("Unable to release IPv4 address during endpoint deletion")
 			}
 		}
 		if option.Config.EnableIPv6 {
-			if err := d.ipam.ReleaseIP(ep.IPv6.IP()); err != nil {
+			if err := d.ipam.ReleaseIP(ep.IPv6.AsSlice()); err != nil {
 				scopedLog := ep.Logger(daemonSubsys).WithError(err)
 				scopedLog.Warning("Unable to release IPv6 address during endpoint deletion")
 			}

daemon/cmd/policy_test.go
@@ -8,6 +8,7 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"net/netip"
 	"os"
 	"sort"
 	"sync"
@@ -22,7 +23,6 @@ import (
 	k8sTypes "k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/cache"
 
-	"github.com/cilium/cilium/pkg/addressing"
 	"github.com/cilium/cilium/pkg/checker"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
@@ -44,10 +44,10 @@ import (
 )
 
 var (
-	QAIPv6Addr, _   = addressing.NewCiliumIPv6("beef:beef:beef:beef:aaaa:aaaa:1111:1112")
-	QAIPv4Addr, _   = addressing.NewCiliumIPv4("10.11.12.13")
-	ProdIPv6Addr, _ = addressing.NewCiliumIPv6("cafe:cafe:cafe:cafe:aaaa:aaaa:1111:1112")
-	ProdIPv4Addr, _ = addressing.NewCiliumIPv4("10.11.12.14")
+	QAIPv6Addr   = netip.MustParseAddr("beef:beef:beef:beef:aaaa:aaaa:1111:1112")
+	QAIPv4Addr   = netip.MustParseAddr("10.11.12.13")
+	ProdIPv6Addr = netip.MustParseAddr("cafe:cafe:cafe:cafe:aaaa:aaaa:1111:1112")
+	ProdIPv4Addr = netip.MustParseAddr("10.11.12.14")
 
 	lblProd = labels.ParseLabel("Prod")
 	lblQA   = labels.ParseLabel("QA")

daemon/cmd/state.go
@@ -216,8 +216,8 @@ func (d *Daemon) restoreOldEndpoints(state *endpointRestoreState, clean bool) er
 		state.restored = append(state.restored, ep)
 
 		if existingEndpoints != nil {
-			delete(existingEndpoints, ep.IPv4.String())
-			delete(existingEndpoints, ep.IPv6.String())
+			delete(existingEndpoints, ep.GetIPv4Address())
+			delete(existingEndpoints, ep.GetIPv6Address())
 		}
 	}
 
@@ -339,21 +339,21 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) (resto
 func (d *Daemon) allocateIPsLocked(ep *endpoint.Endpoint) error {
 	var err error
 
-	if option.Config.EnableIPv6 && ep.IPv6 != nil {
-		_, err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv6.IP(), ep.HumanStringLocked()+" [restored]")
+	if option.Config.EnableIPv6 && ep.IPv6.IsValid() {
+		_, err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv6.AsSlice(), ep.HumanStringLocked()+" [restored]")
 		if err != nil {
-			return fmt.Errorf("unable to reallocate %s IPv6 address: %w", ep.IPv6.IP(), err)
+			return fmt.Errorf("unable to reallocate %s IPv6 address: %w", ep.IPv6, err)
 		}
 
 		defer func() {
 			if err != nil {
-				d.ipam.ReleaseIP(ep.IPv6.IP())
+				d.ipam.ReleaseIP(ep.IPv6.AsSlice())
 			}
 		}()
 	}
 
-	if option.Config.EnableIPv4 && ep.IPv4 != nil {
-		_, err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv4.IP(), ep.HumanStringLocked()+"[restored]")
+	if option.Config.EnableIPv4 && ep.IPv4.IsValid() {
+		_, err = d.ipam.AllocateIPWithoutSyncUpstream(ep.IPv4.AsSlice(), ep.HumanStringLocked()+"[restored]")
 		switch {
 		// We only check for BypassIPAllocUponRestore for IPv4 because we
 		// assume that this flag is only turned on for IPv4-only IPAM modes
@@ -363,10 +363,10 @@ func (d *Daemon) allocateIPsLocked(ep *endpoint.Endpoint) error {
 		// https://github.com/cilium/cilium/pull/15453. Other errors are not
 		// bypassed.
 		case err != nil &&
-			errors.Is(err, ipam.NewIPNotAvailableInPoolError(ep.IPv4.IP())) &&
+			errors.Is(err, ipam.NewIPNotAvailableInPoolError(ep.IPv4.AsSlice())) &&
 			option.Config.BypassIPAvailabilityUponRestore:
 			log.WithError(err).WithFields(logrus.Fields{
-				logfields.IPAddr:     ep.IPv4.IP(),
+				logfields.IPAddr:     ep.IPv4,
 				logfields.EndpointID: ep.ID,
 				logfields.K8sPodName: ep.GetK8sNamespaceAndPodName(),
 			}).Warn(
@@ -376,7 +376,7 @@ func (d *Daemon) allocateIPsLocked(ep *endpoint.Endpoint) error {
 					"a fresh IP from the pool.",
 			)
 		case err != nil:
-			return fmt.Errorf("unable to reallocate %s IPv4 address: %w", ep.IPv4.IP(), err)
+			return fmt.Errorf("unable to reallocate %s IPv4 address: %w", ep.IPv4, err)
 		}
 	}
 

pkg/datapath/config.go
@@ -5,8 +5,8 @@ package datapath
 
 import (
 	"io"
+	"net/netip"
 
-	"github.com/cilium/cilium/pkg/addressing"
 	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/mac"
@@ -43,8 +43,8 @@ type LoadTimeConfiguration interface {
 	// This function should be removed in favour of GetIdentity()
 	GetIdentityLocked() identity.NumericIdentity
 
-	IPv4Address() addressing.CiliumIPv4
-	IPv6Address() addressing.CiliumIPv6
+	IPv4Address() netip.Addr
+	IPv6Address() netip.Addr
 	GetNodeMAC() mac.MAC
 }
 

pkg/datapath/linux/config/config.go
@@ -833,11 +833,11 @@ func (h *HeaderfileWriter) writeStaticData(fw io.Writer, e datapath.EndpointConf
 		// This results in a template BPF object without an "LXC_IP" defined,
 		// __but__ the endpoint still has "LXC_IP" defined. This causes a later
 		// call to loader.ELFSubstitutions() to fail on missing a symbol "LXC_IP".
-		if e.IPv6Address() != nil {
-			fmt.Fprint(fw, defineIPv6("LXC_IP", e.IPv6Address()))
+		if ipv6 := e.IPv6Address(); ipv6.IsValid() {
+			fmt.Fprint(fw, defineIPv6("LXC_IP", ipv6.AsSlice()))
 		}
 
-		fmt.Fprint(fw, defineIPv4("LXC_IPV4", e.IPv4Address()))
+		fmt.Fprint(fw, defineIPv4("LXC_IPV4", e.IPv4Address().AsSlice()))
 		fmt.Fprint(fw, defineUint16("LXC_ID", uint16(e.GetID())))
 	}
 

pkg/datapath/linux/config/config_test.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"net/netip"
 	"strings"
 	"testing"
 
@@ -38,8 +39,8 @@ var (
 	dummyNodeCfg  = datapath.LocalNodeConfiguration{}
 	dummyDevCfg   = testutils.NewTestEndpoint()
 	dummyEPCfg    = testutils.NewTestEndpoint()
-	ipv4DummyAddr = []byte{192, 0, 2, 3}
-	ipv6DummyAddr = []byte{0x20, 0x01, 0xdb, 0x8, 0x0b, 0xad, 0xca, 0xfe, 0x60, 0x0d, 0xbe, 0xe2, 0x0b, 0xad, 0xca, 0xfe}
+	ipv4DummyAddr = netip.MustParseAddr("192.0.2.3")
+	ipv6DummyAddr = netip.MustParseAddr("2001:db08:0bad:cafe:600d:bee2:0bad:cafe")
 )
 
 func (s *ConfigSuite) SetUpSuite(c *C) {
@@ -50,8 +51,8 @@ func (s *ConfigSuite) SetUpTest(c *C) {
 	err := rlimit.RemoveMemlock()
 	c.Assert(err, IsNil)
 	node.InitDefaultPrefix("")
-	node.SetInternalIPv4Router(ipv4DummyAddr)
-	node.SetIPv4Loopback(ipv4DummyAddr)
+	node.SetInternalIPv4Router(ipv4DummyAddr.AsSlice())
+	node.SetIPv4Loopback(ipv4DummyAddr.AsSlice())
 }
 
 func (s *ConfigSuite) TearDownTest(c *C) {
@@ -144,7 +145,7 @@ func (s *ConfigSuite) TestWriteEndpointConfig(c *C) {
 			preTestRun: func(t *testutils.TestEndpoint, e *testutils.TestEndpoint) {
 				option.Config.EnableIPv6 = false
 				t.IPv6 = ipv6DummyAddr // Template bpf prog always has dummy IPv6
-				e.IPv6 = nil           // This endpoint does not have an IPv6 addr
+				e.IPv6 = netip.Addr{}  // This endpoint does not have an IPv6 addr
 			},
 			templateExp: true,
 			endpointExp: false,
@@ -180,7 +181,7 @@ func (s *ConfigSuite) TestWriteEndpointConfig(c *C) {
 			preTestRun: func(t *testutils.TestEndpoint, e *testutils.TestEndpoint) {
 				option.Config.EnableIPv6 = true
 				t.IPv6 = ipv6DummyAddr
-				e.IPv6 = nil
+				e.IPv6 = netip.Addr{}
 			},
 			templateExp: true,
 			endpointExp: false,

pkg/datapath/linux/routing/routing.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"net/netip"
 
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
@@ -144,17 +145,15 @@ func (info *RoutingInfo) Configure(ip net.IP, mtu int, compat bool) error {
 // main routing table. Due to multiple routing CIDRs, there might be more than
 // one egress rule. Deletion of any rule only proceeds if the rule matches
 // the IP & priority. If more than one rule matches, then deletion is skipped.
-func Delete(ip net.IP, compat bool) error {
-	if ip.To4() == nil {
+func Delete(ip netip.Addr, compat bool) error {
+	if !ip.Is4() {
 		log.WithFields(logrus.Fields{
 			"endpointIP": ip,
 		}).Warning("Unable to delete rules because IP is not an IPv4 address")
 		return errors.New("IP not compatible")
 	}
-	ipWithMask := net.IPNet{
-		IP:   ip,
-		Mask: net.CIDRMask(32, 32),
-	}
+
+	ipWithMask := iputil.AddrToIPNet(ip)
 
 	scopedLog := log.WithFields(logrus.Fields{
 		"ip": ipWithMask.String(),
@@ -163,7 +162,7 @@ func Delete(ip net.IP, compat bool) error {
 	// Ingress rules
 	ingress := route.Rule{
 		Priority: linux_defaults.RulePriorityIngress,
-		To:       &ipWithMask,
+		To:       ipWithMask,
 		Table:    route.MainTable,
 	}
 	if err := deleteRule(ingress); err != nil {
@@ -193,7 +192,7 @@ func Delete(ip net.IP, compat bool) error {
 		for _, cidr := range routingCIDRs {
 			egress := route.Rule{
 				Priority: priority,
-				From:     &ipWithMask,
+				From:     ipWithMask,
 				To:       cidr,
 			}
 			if err := deleteRule(egress); err != nil {
@@ -204,7 +203,7 @@ func Delete(ip net.IP, compat bool) error {
 	} else {
 		egress := route.Rule{
 			Priority: priority,
-			From:     &ipWithMask,
+			From:     ipWithMask,
 		}
 		if err := deleteRule(egress); err != nil {
 			return fmt.Errorf("unable to delete egress rule with ip %s: %w", ipWithMask.String(), err)
@@ -220,7 +219,7 @@ func Delete(ip net.IP, compat bool) error {
 		// In CRD-based IPAM, when an IP is unassigned from the CiliumNode, we delete this route
 		// to avoid blackholing traffic to this IP if it gets reassigned to another node
 		if err := netlink.RouteReplace(&netlink.Route{
-			Dst:   &ipWithMask,
+			Dst:   ipWithMask,
 			Table: route.MainTable,
 			Type:  unix.RTN_UNREACHABLE,
 		}); err != nil {

commit 4a4f22e767a18816a19e9698d72814b094d15184

endpoint: correctly log IPv6 addresses
Reference: https://github.com/cilium/cilium/commit/f3a4c4d204cf84af3d40f4782aa68e7c2da98440#r103598930
Fixes: f3a4c4d204cf ("endpoint: convert Endpoint from addressing.CiliumIPv{4,6} to netip.Addr")
Reported-by: Joe Stringer <joe@cilium.io>
Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/endpoint/log.go
@@ -113,7 +113,7 @@ func (e *Endpoint) UpdateLogger(fields map[string]interface{}) {
 		logfields.DatapathPolicyRevision: e.policyRevision,
 		logfields.DesiredPolicyRevision:  e.nextPolicyRevision,
 		logfields.IPv4:                   e.GetIPv4Address(),
-		logfields.IPv6:                   e.GetIPv4Address(),
+		logfields.IPv6:                   e.GetIPv6Address(),
 		logfields.K8sPodName:             e.getK8sNamespaceAndPodName(),
 	})
 
@@ -171,7 +171,7 @@ func (e *Endpoint) updatePolicyLogger(fields map[string]interface{}) {
 			logfields.DatapathPolicyRevision: e.policyRevision,
 			logfields.DesiredPolicyRevision:  e.nextPolicyRevision,
 			logfields.IPv4:                   e.GetIPv4Address(),
-			logfields.IPv6:                   e.GetIPv4Address(),
+			logfields.IPv6:                   e.GetIPv6Address(),
 			logfields.K8sPodName:             e.getK8sNamespaceAndPodName(),
 		})
 