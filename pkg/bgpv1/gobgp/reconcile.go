2853b0b4a50601a41940cc47e8e6e841be9fb898
Dylan Reimerink <dylan.reimerink@isovalent.com>
2023-03-10 18:09:20 +0100

    bgpv1: Clear announcement maps after server recreation
    [ upstream commit 175b2f1c878cd7b827c33decd29275e10a2b6dde ]
    
    We maintain a cache/shadow map for all advertisements we have made.
    During reconciliation we check the desired state against this shadow
    state which lives in the `PodCIDRAnnouncements` and
    `ServiceAnnouncements` variables.
    
    This works until the `preflightReconciler` decides to recreate the
    server, which happens if the router-id or the listen port changes.
    At this point the GoBGP route server is recreated which also clears
    the tables. But we forget to clear our shadow state as well. This
    causes any existing routes to not be added since the reconcilers think
    they are already there.
    
    Fixes: #24069
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>
    Signed-off-by: Nikolay Aleksandrov <nikolay@isovalent.com>

--- pkg/bgpv1/gobgp/reconcile.go
+++ pkg/bgpv1/gobgp/reconcile.go
@@ -172,6 +172,9 @@ func preflightReconciler(ctx context.Context, _ *BGPRouterManager, sc *ServerWit
 	// actions as if this is a new BgpServer.
 	sc.Config = nil
 
+	// Clear the shadow state since any advertisements will be gone now that the server has been recreated.
+	sc.PodCIDRAnnouncements = nil
+
 	return nil
 }
 
