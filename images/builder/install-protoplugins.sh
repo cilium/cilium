66682c6fba75c181084c1593f2c87186781f1658
Robin Hahling <robin.hahling@gw-computing.net>
2023-03-13 10:11:40 +0100

    images/builder: bump proto plugins
    Release notes for protoc-gen-go-grpc v1.3.0:
    https://github.com/grpc/grpc-go/releases/tag/cmd/protoc-gen-go-grpc/v1.3.0
    
    Release notes for cmd/protoc-gen-go v1.29.0:
    https://github.com/protocolbuffers/protobuf-go/releases/tag/v1.29.0
    
    Release notes for cmd/protoc-gen-go v1.30.0:
    https://github.com/protocolbuffers/protobuf-go/releases/tag/v1.30.0
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -8,8 +8,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# c285fc70e095eccc98d79b9a133e1e328141aefd == tag: cmd/protoc-gen-go-grpc/v1.2.0
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@c285fc70e095eccc98d79b9a133e1e328141aefd
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.0
+# 8ba23be9613c672d40ae261d2a1335d639bdd59b == tag: cmd/protoc-gen-go-grpc/v1.3.0
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@8ba23be9613c672d40ae261d2a1335d639bdd59b
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.30.0
 go install github.com/mitchellh/protoc-gen-go-json@v1.1.0
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
