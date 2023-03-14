cilium/cilium

commit 1b92a4d14daddd22f7a58daef2597b2ca4898d62

Fix operator crash race condition for CES identity map concurrent read/write
Cilium operator can crash in case when the identityToCES map is being accessed without a lock during a concurrent write. The error message is fatal error: concurrent map read and map write. The fix is just to add a read lock to the one location in code where the read is not guarded by a lock.

Signed-off-by: Dorde Lapcevic <dordel@google.com>

operator/pkg/ciliumendpointslice/manager.go
@@ -584,6 +584,11 @@ func (c *cesManagerIdentity) InsertCEPInCache(cep *cilium_v2.CoreCiliumEndpoint,
 	// If given cep object isn't packed in any of the CES. find a new ces
 	// to pack this cep.
 	cb, cesName := func() (*cesTracker, string) {
+		// The identityLock and backendMutex locks always need to be acquired in the
+		// same order to avoid deadlocks. First identityLock and then backendMutex,
+		// because identityLock is a higher level lock of cesManagerIdentity which
+		// contains cesTrackers with backendMutex locks within it.
+		c.identityLock.RLock()
 		// get first available CES
 		if cess, exist := c.identityToCES[cep.IdentityID]; exist {
 			for _, ces := range cess {
@@ -593,9 +598,12 @@ func (c *cesManagerIdentity) InsertCEPInCache(cep *cilium_v2.CoreCiliumEndpoint,
 					continue
 				}
 				defer ces.backendMutex.RUnlock()
+				defer c.identityLock.RUnlock()
 				return ces, ces.ces.GetName()
 			}
 		}
+		c.identityLock.RUnlock()
+
 		// allocate a new cesTracker and return
 		ces := c.createCES("")
 		// Update the namespace to CES

commit ed60c55bca37aef086fe8fbeeda3bd13ddefb1ce

Fix operator deadlock introduced in the #23605.
Change the order of locking to keep it consistent in the
CiliumEndpointSlice Manager.

Lock identityLock always before locking backends lock.

Fixes: #24206
Fixes: a2d0596d6d947a62211898d32aa6fb2135da1e37 Fix operator crash race condition for CES identity map concurrent read/write

Signed-off-by: Alan Kutniewski <kutniewski@google.com>

operator/pkg/ciliumendpointslice/manager.go
@@ -27,6 +27,10 @@ var (
 // in next sync with k8s-apiserver.
 type cesTracker struct {
 	// Mutex to protect cep insert/removal in ces and removedCEPs
+	// The identityLock and backendMutex locks always need to be acquired in the
+	// same order to avoid deadlocks. First identityLock and then backendMutex,
+	// because identityLock is a higher level lock of cesManagerIdentity which
+	// contains cesTrackers with backendMutex locks within it.
 	backendMutex lock.RWMutex
 	// The desired state of ces object
 	ces *cilium_v2.CiliumEndpointSlice
@@ -97,6 +101,10 @@ type cesManagerFcfs struct {
 type cesManagerIdentity struct {
 	cesMgr
 	// Mutex to protect cep insert/removal in ces and removedCEPs
+	// The identityLock and backendMutex locks always need to be acquired in the
+	// same order to avoid deadlocks. First identityLock and then backendMutex,
+	// because identityLock is a higher level lock of cesManagerIdentity which
+	// contains cesTrackers with backendMutex locks within it.
 	identityLock lock.RWMutex
 	// CEP identity to cesTracker map
 	identityToCES map[int64][]*cesTracker
@@ -635,6 +643,12 @@ func (c *cesManagerIdentity) InsertCEPInCache(cep *cilium_v2.CoreCiliumEndpoint,
 // isDeepCopy flag is set to false.
 func (c *cesManagerIdentity) updateCESInCache(srcCES *cilium_v2.CiliumEndpointSlice, isDeepCopy bool) {
 	if ces, ok := c.desiredCESs.getCESTracker(srcCES.GetName()); ok {
+		// The identityLock and backendMutex locks always need to be acquired in the
+		// same order to avoid deadlocks. First identityLock and then backendMutex,
+		// because identityLock is a higher level lock of cesManagerIdentity which
+		// contains cesTrackers with backendMutex locks within it.
+		c.identityLock.Lock()
+		defer c.identityLock.Unlock()
 		ces.backendMutex.Lock()
 		defer ces.backendMutex.Unlock()
 		if !isDeepCopy {
@@ -645,10 +659,8 @@ func (c *cesManagerIdentity) updateCESInCache(srcCES *cilium_v2.CiliumEndpointSl
 			for _, cep := range ces.ces.Endpoints {
 				// Update the identityToCES and cesToIdentity maps respectively.
 				if !exist {
-					c.identityLock.Lock()
 					c.identityToCES[cep.IdentityID] = append(c.identityToCES[cep.IdentityID], ces)
 					c.cesToIdentity[srcCES.GetName()] = cep.IdentityID
-					c.identityLock.Unlock()
 					exist = true
 				}
 				// Update the desiredCESs, to reflect all CEPs are packed in a CES