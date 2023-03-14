7f63fd42d1a56dace4166fb26bf674f0f0bae114
Julian Wiedmann <jwi@isovalent.com>
2023-03-14 16:04:26 +0200

    bpf: host: add drop notification for missed tail call in to-lxc policy path
    [ upstream commit 375b345f3b443e706dab4b687e8bc1788b0621d3 ]
    
    Straight-forward fix for a missing drop notification.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -1471,7 +1471,8 @@ int handle_lxc_traffic(struct __ctx_buff *ctx)
 		lxc_id = ctx_load_meta(ctx, CB_DST_ENDPOINT_ID);
 		ctx_store_meta(ctx, CB_SRC_LABEL, HOST_ID);
 		tail_call_dynamic(ctx, &POLICY_CALL_MAP, lxc_id);
-		return DROP_MISSED_TAIL_CALL;
+		return send_drop_notify_error(ctx, HOST_ID, DROP_MISSED_TAIL_CALL,
+					      CTX_ACT_DROP, METRIC_EGRESS);
 	}
 
 	return to_host_from_lxc(ctx);
