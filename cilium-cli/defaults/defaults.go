b723391d9c5c2e41c26bd87bd001c6ebd7af4693
Maartje Eyskens <maartje.eyskens@isovalent.com>
2023-03-20 09:51:53 +0100

    Add a l7 header replace test
    [ cherry-picked from cilium/cilium-cli repository ]
    
    This adds a test for the L7 policy to add/replace a HTTP header in a
    request. It does this by sending a request to a new endpoint that
    required an auth header, which will succeed if injected from a secret
    
    Signed-off-by: Maartje Eyskens <maartje.eyskens@isovalent.com>

--- cilium-cli/defaults/defaults.go
+++ cilium-cli/defaults/defaults.go
@@ -69,7 +69,7 @@ const (
 
 	ConnectivityCheckAlpineCurlImage = "quay.io/cilium/alpine-curl:v1.6.0@sha256:408430f548a8390089b9b83020148b0ef80b0be1beb41a98a8bfe036709c196e"
 	ConnectivityPerformanceImage     = "quay.io/cilium/network-perf:a816f935930cb2b40ba43230643da4d5751a5711@sha256:679d3a370c696f63884da4557a4466f3b5569b4719bb4f86e8aac02fbe390eea"
-	ConnectivityCheckJSONMockImage   = "quay.io/cilium/json-mock:v1.3.3@sha256:f26044a2b8085fcaa8146b6b8bb73556134d7ec3d5782c6a04a058c945924ca0"
+	ConnectivityCheckJSONMockImage   = "quay.io/cilium/json-mock:v1.3.4@sha256:d5fbc5d7762530b53af27b0ba247e61a2fa5da42c8b5a58956933fe6a6ce15b8"
 	ConnectivityDNSTestServerImage   = "docker.io/coredns/coredns:1.10.0@sha256:017727efcfeb7d053af68e51436ce8e65edbc6ca573720afb4f79c8594036955"
 
 	ConfigMapName = "cilium-config"
