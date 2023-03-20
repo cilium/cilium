f077611488eb440f0b988f3aa2dd02f56c56c238
Michi Mutsuzaki <michi@isovalent.com>
2023-03-20 20:51:04 +0000

    vendor: Pick up the latest cilium
    Need https://github.com/cilium/cilium/pull/24470 to fix this error that
    happens during cilium connectivity test setup:
    
        connectivity test failed: unmarshaling cilium runtime config json: json: cannot unmarshal array into Go struct field DaemonConfig.NodeEncryptionOptOutLabels of type labels.Selector
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- cilium-cli/vendor/github.com/cilium/cilium/pkg/labels/arraylist.go
+++ cilium-cli/vendor/github.com/cilium/cilium/pkg/labels/arraylist.go
@@ -55,3 +55,38 @@ func (ls LabelArrayList) Sort() LabelArrayList {
 
 	return ls
 }
+
+// Merge incorporates new LabelArrays into an existing LabelArrayList, without
+// introducing duplicates, returning the result for convenience. The LabelArrays
+// contained in either LabelArrayList must already be sorted. Existing
+// duplication in either list is not removed.
+func (lsp *LabelArrayList) Merge(include ...LabelArray) LabelArrayList {
+	lsp.Sort()
+	incl := LabelArrayList(include).Sort()
+	return lsp.mergeSorted(incl)
+}
+
+func (lsp *LabelArrayList) mergeSorted(include LabelArrayList) LabelArrayList {
+	ls := *lsp
+	merged := make(LabelArrayList, 0, len(include)+len(ls))
+
+	var i, j int
+	for i < len(include) && j < len(ls) {
+		if ls[j].Less(include[i]) {
+			merged = append(merged, ls[j])
+			j++
+		} else if ls[j].Equals(include[i]) {
+			merged = append(merged, ls[j])
+			i++
+			j++
+		} else {
+			merged = append(merged, include[i])
+			i++
+		}
+	}
+
+	merged = append(merged, ls[j:]...)
+	merged = append(merged, include[i:]...)
+	*lsp = merged
+	return merged
+}
