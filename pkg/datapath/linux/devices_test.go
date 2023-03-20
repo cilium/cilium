cilium/cilium

commit 5283ec938a5f2663126061e3f43bc85c0ded3af1

datapath: Introduce DirectRoutingDeviceRequired helper
The Direct Routing Device is currently used by BPF Host
Routing and BPF Node Port. With both features, we don't
need it for tunneling mode. The DirectRoutingDeviceRequired
helper function checks the above conditions and returns if
the Direct Routing Device is required for the given
configuration.

Upcoming commits will fix several places that use Direct
Routing Device without tunneling or BPF Host Routing in
an account using DirectRoutingDeviceRequired.

Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

pkg/option/config.go
@@ -1230,7 +1230,7 @@ type DaemonConfig struct {
 	RunDir              string     // Cilium runtime directory
 	NAT46Prefix         *net.IPNet // NAT46 IPv6 Prefix
 	Devices             []string   // bpf_host device
-	DirectRoutingDevice string     // Direct routing device (used only by NodePort BPF)
+	DirectRoutingDevice string     // Direct routing device (used by BPF NodePort and BPF Host Routing)
 	LBDevInheritIPAddr  string     // Device which IP addr used by bpf_host devices
 	EnableXDPPrefilter  bool       // Enable XDP-based prefiltering
 	DevicePreFilter     string     // Prefilter device
@@ -2326,6 +2326,15 @@ func (c *DaemonConfig) K8sAPIDiscoveryEnabled() bool {
 // required groups.
 func (c *DaemonConfig) K8sLeasesFallbackDiscoveryEnabled() bool {
 	return c.K8sEnableAPIDiscovery
+}
+
+// DirectRoutingDeviceRequired return whether the Direct Routing Device is needed under
+// the current configuration.
+func (c *DaemonConfig) DirectRoutingDeviceRequired() bool {
+	// BPF NodePort and BPF Host Routing are using the direct routing device now.
+	// When tunneling is enabled, node-to-node redirection will be done by tunneling.
+	BPFHostRoutingEnabled := !c.EnableHostLegacyRouting
+	return (c.EnableNodePort || BPFHostRoutingEnabled) && !c.TunnelingEnabled()
 }
 
 // EnableK8sLeasesFallbackDiscovery enables using direct API probing as a fallback to check

commit 66c288b278b4d345f1acf7d20595d07e7776a331

datapath: fix NodePort to remote hostns backend with tunnel config
When forwarding external service requests to a remote backend in
hostNetwork, the NodePort code in tail_nodeport_nat_egress_ipv4() doesn't
redirect into the tunnel (as it's effectively a host-to-host connection).

Therefore it uses IPV4_DIRECT_ROUTING as src address. So if we're not
populating IPV4_DIRECT_ROUTING, such forwarded requests end up being SNATed
with 0.0.0.0.

Fixes: https://github.com/cilium/cilium/issues/22557
Fixes: 5283ec938a5f ("datapath: Introduce DirectRoutingDeviceRequired helper")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

pkg/option/config.go
@@ -2643,9 +2643,8 @@ func (c *DaemonConfig) K8sGatewayAPIEnabled() bool {
 // the current configuration.
 func (c *DaemonConfig) DirectRoutingDeviceRequired() bool {
 	// BPF NodePort and BPF Host Routing are using the direct routing device now.
-	// When tunneling is enabled, node-to-node redirection will be done by tunneling.
 	BPFHostRoutingEnabled := !c.EnableHostLegacyRouting
-	return (c.EnableNodePort || BPFHostRoutingEnabled || Config.EnableWireguard) && !c.TunnelingEnabled()
+	return (c.EnableNodePort || BPFHostRoutingEnabled || Config.EnableWireguard)
 }
 
 func (c *DaemonConfig) validateIPv6ClusterAllocCIDR() error {

pkg/datapath/linux/devices_test.go
@@ -23,17 +23,18 @@ import (
 )
 
 type DevicesSuite struct {
-	currentNetNS                  netns.NsHandle
-	prevConfigDevices             []string
-	prevConfigDirectRoutingDevice string
-	prevConfigIPv6MCastDevice     string
-	prevConfigEnableIPv4          bool
-	prevConfigEnableIPv6          bool
-	prevConfigEnableNodePort      bool
-	prevConfigTunnel              string
-	prevConfigEnableIPv6NDP       bool
-	prevK8sNodeIP                 net.IP
-	prevK8sNodeIPv6               net.IP
+	currentNetNS                      netns.NsHandle
+	prevConfigDevices                 []string
+	prevConfigDirectRoutingDevice     string
+	prevConfigIPv6MCastDevice         string
+	prevConfigEnableIPv4              bool
+	prevConfigEnableIPv6              bool
+	prevConfigEnableHostLegacyRouting bool
+	prevConfigEnableNodePort          bool
+	prevConfigTunnel                  string
+	prevConfigEnableIPv6NDP           bool
+	prevK8sNodeIP                     net.IP
+	prevK8sNodeIPv6                   net.IP
 }
 
 var _ = Suite(&DevicesSuite{})
@@ -47,6 +48,7 @@ func (s *DevicesSuite) SetUpSuite(c *C) {
 	s.prevConfigDirectRoutingDevice = option.Config.DirectRoutingDevice
 	s.prevConfigEnableIPv4 = option.Config.EnableIPv4
 	s.prevConfigEnableIPv6 = option.Config.EnableIPv6
+	s.prevConfigEnableHostLegacyRouting = option.Config.EnableHostLegacyRouting
 	s.prevConfigEnableNodePort = option.Config.EnableNodePort
 	s.prevConfigTunnel = option.Config.Tunnel
 	s.prevConfigEnableIPv6NDP = option.Config.EnableIPv6NDP
@@ -62,6 +64,7 @@ func (s *DevicesSuite) TearDownTest(c *C) {
 	option.Config.DirectRoutingDevice = s.prevConfigDirectRoutingDevice
 	option.Config.EnableIPv4 = s.prevConfigEnableIPv4
 	option.Config.EnableIPv6 = s.prevConfigEnableIPv6
+	option.Config.EnableHostLegacyRouting = s.prevConfigEnableHostLegacyRouting
 	option.Config.EnableNodePort = s.prevConfigEnableNodePort
 	option.Config.Tunnel = s.prevConfigTunnel
 	option.Config.EnableIPv6NDP = s.prevConfigEnableIPv6NDP
@@ -77,25 +80,27 @@ func (s *DevicesSuite) TestDetect(c *C) {
 
 		option.Config.SetDevices([]string{})
 		option.Config.DirectRoutingDevice = ""
+		option.Config.EnableHostLegacyRouting = true
+		option.Config.EnableNodePort = false
 
 		// 1. No devices, nothing to detect.
 		devices, err := dm.Detect(false)
 		c.Assert(err, IsNil)
 		c.Assert(devices, checker.DeepEquals, []string{})
 
-		// 2. Node IP not set, can still detect. Direct routing device shouldn't be detected.
+		// 2. Nodeport, detection is performed:
 		option.Config.EnableNodePort = true
 		c.Assert(createDummy("dummy0", "192.168.0.1/24", false), IsNil)
-		node.SetIPv4(nil)
+		node.SetIPv4(net.ParseIP("192.168.0.1"))
 
 		devices, err = dm.Detect(true)
 		c.Assert(err, IsNil)
 		c.Assert(devices, checker.DeepEquals, []string{"dummy0"})
 		c.Assert(option.Config.GetDevices(), checker.DeepEquals, devices)
-		c.Assert(option.Config.DirectRoutingDevice, Equals, "")
+		c.Assert(option.Config.DirectRoutingDevice, Equals, "dummy0")
+		option.Config.DirectRoutingDevice = ""
 
 		// 3. Manually specified devices, no detection is performed
-		option.Config.EnableNodePort = true
 		node.SetIPv4(net.ParseIP("192.168.0.1"))
 		c.Assert(createDummy("dummy1", "192.168.1.1/24", false), IsNil)
 		option.Config.SetDevices([]string{"dummy0"})
@@ -104,8 +109,9 @@ func (s *DevicesSuite) TestDetect(c *C) {
 		c.Assert(err, IsNil)
 		c.Assert(devices, checker.DeepEquals, []string{"dummy0"})
 		c.Assert(option.Config.GetDevices(), checker.DeepEquals, devices)
-		c.Assert(option.Config.DirectRoutingDevice, Equals, "")
+		c.Assert(option.Config.DirectRoutingDevice, Equals, "dummy0")
 		option.Config.SetDevices([]string{})
+		option.Config.DirectRoutingDevice = ""
 
 		// 4. Direct routing mode, should find all devices and set direct
 		// routing device to the one with k8s node ip.
@@ -332,6 +338,8 @@ func (s *DevicesSuite) TestListenAfterDelete(c *C) {
 		timeout := time.After(time.Second)
 
 		option.Config.SetDevices([]string{"dummy+"})
+		option.Config.DirectRoutingDevice = "dummy0"
+
 		dm, err := NewDeviceManager()
 		c.Assert(err, IsNil)
 