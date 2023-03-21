cilium/cilium

commit 85671ad30e0ed94a08ad9df43b4079c6c58610b1

Add support for is-default-class on IngressClass
This adds support for the ingressclass.kubernetes.io/is-default-class flag on the IngressClass.
If set Cilium ingress wil pick up all ingress objects where no class is set also.

This also adds exposure to enable it in Helm under ingressController.default

Fixes: #23181

Signed-off-by: Maartje Eyskens <maartje.eyskens@isovalent.com>

operator/pkg/ingress/ingress.go
@@ -6,6 +6,7 @@ package ingress
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/sirupsen/logrus"
@@ -72,10 +73,11 @@ type Controller struct {
 	ingressInformer cache.Controller
 	ingressStore    cache.Store
 
-	serviceManager     *serviceManager
-	endpointManager    *endpointManager
-	envoyConfigManager *envoyConfigManager
-	secretManager      secretManager
+	serviceManager      *serviceManager
+	endpointManager     *endpointManager
+	envoyConfigManager  *envoyConfigManager
+	ingressClassManager *ingressClassManager
+	secretManager       secretManager
 
 	queue      workqueue.RateLimitingInterface
 	maxRetries int
@@ -90,6 +92,7 @@ type Controller struct {
 	sharedLBServiceName     string
 	ciliumNamespace         string
 	defaultLoadbalancerMode string
+	isDefaultIngressClass   bool
 
 	sharedLBStatus *slim_corev1.LoadBalancerStatus
 }
@@ -150,6 +153,12 @@ func NewController(clientset k8sClient.Clientset, options ...Option) (*Controlle
 		nil,
 	)
 
+	ingressClassManager, err := newIngressClassManager(clientset, ic.queue, opts.MaxRetries)
+	if err != nil {
+		return nil, err
+	}
+	ic.ingressClassManager = ingressClassManager
+
 	serviceManager, err := newServiceManager(clientset, ic.queue, opts.MaxRetries)
 	if err != nil {
 		return nil, err
@@ -189,6 +198,7 @@ func (ic *Controller) Run() {
 		return
 	}
 
+	go ic.ingressClassManager.Run()
 	go ic.serviceManager.Run()
 	go ic.secretManager.Run()
 
@@ -223,9 +233,31 @@ func getIngressClassName(ingress *slim_networkingv1.Ingress) *string {
 	return ingress.Spec.IngressClassName
 }
 
+func hasEmptyIngressClass(ingress *slim_networkingv1.Ingress) bool {
+	className := getIngressClassName(ingress)
+
+	return className == nil || *className == ""
+}
+
+func (ic *Controller) isCiliumIngressEntry(ingress *slim_networkingv1.Ingress) bool {
+	className := getIngressClassName(ingress)
+
+	if (className == nil || *className == "") && ic.isDefaultIngressClass {
+		return true
+	}
+
+	return className != nil && *className == ciliumIngressClassName
+}
+
 func (ic *Controller) handleIngressAddedEvent(event ingressAddedEvent) error {
-	ingressClass := getIngressClassName(event.ingress)
-	if ingressClass == nil || *ingressClass != ciliumIngressClassName {
+	if !ic.isCiliumIngressEntry(event.ingress) {
+		// this could have been our class before we should clean up
+		err := ic.garbageCollectOwnedResources(event.ingress)
+		if err != nil {
+			return err
+		}
+
+		log.WithField(logfields.Ingress, event.ingress.Name).WithField(logfields.K8sNamespace, event.ingress.Namespace).Debug("Skipping ingress as it is not the cilium class or default")
 		return nil
 	}
 
@@ -234,8 +266,7 @@ func (ic *Controller) handleIngressAddedEvent(event ingressAddedEvent) error {
 }
 
 func (ic *Controller) handleIngressUpdatedEvent(event ingressUpdatedEvent) error {
-	ingressClass := getIngressClassName(event.newIngress)
-	if ingressClass == nil || *ingressClass != ciliumIngressClassName {
+	if !ic.isCiliumIngressEntry(event.newIngress) {
 		return nil
 	}
 	ic.secretManager.Add(event)
@@ -270,7 +301,7 @@ func (ic *Controller) handleIngressUpdatedEvent(event ingressUpdatedEvent) error
 }
 
 func (ic *Controller) handleIngressDeletedEvent(event ingressDeletedEvent) error {
-	log.WithField(logfields.Ingress, event.ingress.Name).Debug("Deleting CiliumEnvoyConfig for ingress")
+	log.WithField(logfields.Ingress, event.ingress.Name).WithField(logfields.K8sNamespace, event.ingress.Namespace).Debug("Deleting CiliumEnvoyConfig for ingress")
 	ic.secretManager.Add(event)
 
 	if ic.isEffectiveLoadbalancerModeDedicated(event.ingress) {
@@ -291,9 +322,7 @@ func (ic *Controller) handleIngressServiceUpdatedEvent(ingressServiceUpdated ing
 		ic.sharedLBStatus = &service.Status.LoadBalancer
 		for _, ing := range ic.ingressStore.ListKeys() {
 			item, _ := ic.getByKey(ing)
-			ingressClassName := getIngressClassName(item)
-			if ingressClassName == nil ||
-				*ingressClassName != ciliumIngressClassName ||
+			if !ic.isCiliumIngressEntry(item) ||
 				item.GetDeletionTimestamp() != nil {
 				continue
 			}
@@ -316,6 +345,74 @@ func (ic *Controller) handleIngressServiceUpdatedEvent(ingressServiceUpdated ing
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func (ic *Controller) handleCiliumIngressClassUpdatedEvent(event ciliumIngressClassUpdatedEvent) error {
+	log.Debugf("Cilium IngressClass updated")
+	previousValue := ic.isDefaultIngressClass
+	if val, ok := event.ingressClass.GetAnnotations()[slim_networkingv1.AnnotationIsDefaultIngressClass]; ok {
+		isDefault, err := strconv.ParseBool(val)
+		if err != nil {
+			log.WithError(err).Warnf("Failed to parse annotation value for %q", slim_networkingv1.AnnotationIsDefaultIngressClass)
+			return err
+		}
+		ic.isDefaultIngressClass = isDefault
+	} else {
+		// if the annotation is not set we are not the default ingress class
+		ic.isDefaultIngressClass = false
+	}
+
+	if previousValue != ic.isDefaultIngressClass {
+		log.Debugf("Cilium IngressClass default value changed, re-syncing ingresses")
+		// ensure that all ingresses are in the correct state
+		for _, k := range ic.ingressStore.ListKeys() {
+			ing, err := ic.getByKey(k)
+			if err != nil {
+				return err
+			}
+
+			if ic.isCiliumIngressEntry(ing) {
+				// make sure that the ingress is in the correct state
+				if err := ic.ensureResources(ing, false); err != nil {
+					return err
+				}
+			} else if hasEmptyIngressClass(ing) && !ic.isDefaultIngressClass {
+				// if we are no longer the default ingress class, we need to clean up
+				// the resources that we created for the ingress
+				if err := ic.deleteResources(ing); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (ic *Controller) handleCiliumIngressClassDeletedEvent(event ciliumIngressClassDeletedEvent) error {
+	log.Debug("Cilium IngressClass deleted")
+
+	if ic.isDefaultIngressClass {
+		// if we were the default ingress class, we need to clean up all ingresses
+		for _, k := range ic.ingressStore.ListKeys() {
+			ing, err := ic.getByKey(k)
+			if err != nil {
+				return err
+			}
+
+			if hasEmptyIngressClass(ing) {
+				// if we are no longer the default ingress class, we need to clean up
+				// the resources that we created for the ingress
+				if err := ic.deleteResources(ing); err != nil {
+					return err
+				}
+			}
+		}
+
+		// disable the default ingress class behavior
+		ic.isDefaultIngressClass = false
 	}
 	return nil
 }
@@ -393,17 +490,23 @@ func (ic *Controller) handleEvent(event interface{}) error {
 	var err error
 	switch ev := event.(type) {
 	case ingressAddedEvent:
-		log.WithField(logfields.Ingress, ev.ingress.Name).Debug("Handling ingress added event")
+		log.WithField(logfields.Ingress, ev.ingress.Name).WithField(logfields.K8sNamespace, ev.ingress.Namespace).Debug("Handling ingress added event")
 		err = ic.handleIngressAddedEvent(ev)
 	case ingressUpdatedEvent:
-		log.WithField(logfields.Ingress, ev.newIngress.Name).Debug("Handling ingress updated event")
+		log.WithField(logfields.Ingress, ev.newIngress.Name).WithField(logfields.K8sNamespace, ev.newIngress.Namespace).Debug("Handling ingress updated event")
 		err = ic.handleIngressUpdatedEvent(ev)
 	case ingressDeletedEvent:
-		log.WithField(logfields.Ingress, ev.ingress.Name).Debug("Handling ingress deleted event")
+		log.WithField(logfields.Ingress, ev.ingress.Name).WithField(logfields.K8sNamespace, ev.ingress.Namespace).Debug("Handling ingress deleted event")
 		err = ic.handleIngressDeletedEvent(ev)
 	case ingressServiceUpdatedEvent:
-		log.WithField(logfields.ServiceKey, ev.ingressService.Name).Debug("Handling ingress service updated event")
+		log.WithField(logfields.ServiceKey, ev.ingressService.Name).WithField(logfields.K8sNamespace, ev.ingressService.Namespace).Debug("Handling ingress service updated event")
 		err = ic.handleIngressServiceUpdatedEvent(ev)
+	case ciliumIngressClassUpdatedEvent:
+		log.WithField(logfields.IngressClass, ev.ingressClass.Name).Debug("Handling cilium ingress class updated event")
+		err = ic.handleCiliumIngressClassUpdatedEvent(ev)
+	case ciliumIngressClassDeletedEvent:
+		log.WithField(logfields.IngressClass, ev.ingressClass.Name).Debug("Handling cilium ingress class deleted event")
+		err = ic.handleCiliumIngressClassDeletedEvent(ev)
 	default:
 		err = fmt.Errorf("received an unknown event: %t", ev)
 	}
@@ -489,10 +592,7 @@ func (ic *Controller) regenerate(ing *slim_networkingv1.Ingress, forceShared boo
 		translator = ic.sharedTranslator
 		for _, k := range ic.ingressStore.ListKeys() {
 			item, _ := ic.getByKey(k)
-			ingressClassName := getIngressClassName(item)
-			if ingressClassName == nil ||
-				*ingressClassName != ciliumIngressClassName ||
-				ic.isEffectiveLoadbalancerModeDedicated(item) ||
+			if !ic.isCiliumIngressEntry(item) || ic.isEffectiveLoadbalancerModeDedicated(item) ||
 				ing.GetDeletionTimestamp() != nil {
 				continue
 			}
@@ -546,4 +646,54 @@ func (ic *Controller) isEffectiveLoadbalancerModeDedicated(ing *slim_networkingv
 	default:
 		return ic.defaultLoadbalancerMode == dedicatedLoadbalancerMode
 	}
+}
+
+func (ic *Controller) garbageCollectOwnedResources(ing *slim_networkingv1.Ingress) error {
+	cec, svc, ep, err := ic.regenerate(ing, false)
+	if err != nil {
+		return err
+	}
+
+	if err := deleteObjectIfExists(cec, ic.envoyConfigManager.getByKey, ic.clientset.CiliumV2().CiliumEnvoyConfigs(cec.GetNamespace()).Delete); err != nil {
+		return err
+	}
+
+	if err := deleteObjectIfExists(svc, ic.serviceManager.getByKey, ic.clientset.CoreV1().Services(svc.GetNamespace()).Delete); err != nil {
+		return err
+	}
+
+	if err := deleteObjectIfExists(ep, ic.endpointManager.getByKey, ic.clientset.CoreV1().Endpoints(ep.GetNamespace()).Delete); err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+// deleteObjectIfExists checks the caches to see if the object exists and if so, deletes it. It uses caches as to limit API server requests for objects may have never existed.
+func deleteObjectIfExists[T any](obj metav1.Object, getByKey func(string) (T, bool, error), deleter func(ctx context.Context, name string, opts metav1.DeleteOptions) error) error {
+	if obj == nil {
+		return nil
+	}
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		log.WithError(err).Warn("MetaNamespaceKeyFunc returned an error")
+		return err
+	}
+	_, exists, err := getByKey(key)
+	if err != nil {
+		log.WithError(err).WithField(logfields.Object, obj).Warn("Cache lookup failed")
+		return err
+	}
+
+	if exists {
+		err = deleter(context.Background(), obj.GetName(), metav1.DeleteOptions{})
+		if err != nil {
+			log.WithError(err).WithField(logfields.Object, obj).Warn("Failed to delete object")
+			return err
+		}
+		log.WithField(logfields.Object, obj).Debug("Deleted object which was no longer tracked")
+		return nil
+	}
+	return nil
 }

.github/workflows/conformance-ingress-default.yaml
@@ -1,0 +1,168 @@
+name: ConformanceIngress (Default Ingress)
+
+# Any change in triggers needs to be reflected in the concurrency group.
+on:
+  pull_request:
+    paths-ignore:
+      - 'Documentation/**'
+      - 'test/**'
+  push:
+    branches:
+      - master
+      - ft/master/**
+    paths-ignore:
+      - 'Documentation/**'
+      - 'test/**'
+
+permissions: read-all
+
+concurrency:
+  group: ${{ github.workflow }}-${{ github.event.pull_request.number || github.event.after }}
+  cancel-in-progress: true
+
+env:
+  cilium_cli_version: v0.12.12
+  minikube_version: 1.28.0
+  timeout: 5m
+
+jobs:
+  ingress-conformance-test:
+    runs-on: ubuntu-latest
+    timeout-minutes: 120
+    steps:
+      - name: Checkout master branch to access local actions
+        uses: actions/checkout@93ea575cb5d8a053eaa0ac8fa3b40d7e05a33cc8 # v3.1.0
+        with:
+          ref: ${{ github.event.repository.default_branch }}
+          persist-credentials: false
+      - name: Set Environment Variables
+        uses: ./.github/actions/set-env-variables
+
+      - name: Install Cilium CLI
+        run: |
+          curl -sSL --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${{ env.cilium_cli_version }}/cilium-linux-amd64.tar.gz{,.sha256sum}
+          sha256sum --check cilium-linux-amd64.tar.gz.sha256sum
+          sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
+          rm cilium-linux-amd64.tar.gz{,.sha256sum}
+          cilium version
+
+      - name: Set image tag
+        id: vars
+        run: |
+          if [ ${{ github.event.pull_request }} ]; then
+            SHA=${{ github.event.pull_request.head.sha }}
+          else
+            SHA=${{ github.sha }}
+          fi
+          echo sha=${SHA} >> $GITHUB_OUTPUT
+
+      - name: Checkout
+        uses: actions/checkout@93ea575cb5d8a053eaa0ac8fa3b40d7e05a33cc8 # v3.1.0
+        with:
+          ref: ${{ steps.vars.outputs.sha }}
+          persist-credentials: false
+
+      - name: Create minikube cluster with multiple nodes
+        uses: medyagh/setup-minikube@ab221dee176f8eabd8deddf849b5bf1d6244a6e8 # v0.0.11
+        with:
+          minikube-version: ${{ env.minikube_version }}
+          network-plugin: cni
+          cni: false
+          wait: false
+          start-args: --nodes 3 --docker-opt "default-ulimit=nofile=102400:102400"
+
+      # Start minikube tunnel
+      - name: Setup minikube
+        run: |
+          docker pull ${{ env.ECHO_SERVER_IMAGE }}
+          minikube image load ${{ env.ECHO_SERVER_IMAGE }}
+          minikube tunnel &
+        env:
+          ECHO_SERVER_IMAGE: k8s.gcr.io/ingressconformance/echoserver:v0.0.1@sha256:9b34b17f391f87fb2155f01da2f2f90b7a4a5c1110ed84cb5379faa4f570dc52
+
+      - name: Checkout ingress-controller-conformance
+        uses: actions/checkout@93ea575cb5d8a053eaa0ac8fa3b40d7e05a33cc8 # v3.1.0
+        with:
+          repository: kubernetes-sigs/ingress-controller-conformance
+          path: ingress-controller-conformance
+          ref: a38bfcddfc7b10a7b479c1f274b87380c7390c96
+          persist-credentials: false
+
+      - name: Install Ingress conformance test tool
+        timeout-minutes: 10
+        run: |
+          cd ingress-controller-conformance
+          make build
+
+      - name: Wait for images to be available
+        timeout-minutes: 30
+        shell: bash
+        run: |
+          for image in cilium-ci operator-generic-ci ; do
+            until docker manifest inspect quay.io/${{ env.QUAY_ORGANIZATION_DEV }}/$image:${{ steps.vars.outputs.sha }} &> /dev/null; do sleep 45s; done
+          done
+
+      - name: Install Cilium
+        run: |
+          helm upgrade -i cilium ./install/kubernetes/cilium \
+            --wait \
+            --namespace kube-system \
+            --set kubeProxyReplacement=strict \
+            --set nodeinit.enabled=true \
+            --set ipam.mode=kubernetes \
+            --set image.repository=quay.io/${{ env.QUAY_ORGANIZATION_DEV }}/cilium-ci \
+            --set image.tag=${{ steps.vars.outputs.sha }} \
+            --set image.pullPolicy=IfNotPresent \
+            --set image.useDigest=false \
+            --set operator.image.repository=quay.io/${{ env.QUAY_ORGANIZATION_DEV }}/operator \
+            --set operator.image.suffix=-ci \
+            --set operator.image.tag=${{ steps.vars.outputs.sha }} \
+            --set operator.image.pullPolicy=IfNotPresent \
+            --set operator.image.useDigest=false \
+            --set securityContext.privileged=true \
+            --set ingressController.enabled=true \
+            --set ingressController.default=true \
+            --set ingressController.loadbalancerMode=dedicated
+
+          kubectl wait -n kube-system --for=condition=Ready -l app.kubernetes.io/part-of=cilium pod --timeout=${{ env.timeout }}
+          kubectl rollout -n kube-system status deploy/coredns --timeout=${{ env.timeout }}
+
+          # To make sure that cilium envoy CRs are available
+          kubectl wait --for condition=Established crd/ciliumenvoyconfigs.cilium.io --timeout=${{ env.timeout }}
+          kubectl wait --for condition=Established crd/ciliumclusterwideenvoyconfigs.cilium.io --timeout=${{ env.timeout }}
+
+      # Run the quick sanity check
+      - name: Run Sanity check
+        timeout-minutes: 5
+        run: |
+          kubectl apply -n default -f https://raw.githubusercontent.com/istio/istio/release-1.11/samples/bookinfo/platform/kube/bookinfo.yaml
+          # renove ingressClassName line from basic-ingress.yaml
+          sed -i '/ingressClassName/d' examples/kubernetes/servicemesh/basic-ingress.yaml
+          kubectl apply -n default -f examples/kubernetes/servicemesh/basic-ingress.yaml
+          kubectl wait -n default --for=condition=Ready --all pod --timeout=${{ env.timeout }}
+
+          lb=$(kubectl get ingress basic-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+          curl -s -v --connect-timeout 5 --max-time 20 --retry 3 --fail -- http://"$lb"
+          curl -s -v --connect-timeout 5 --max-time 20 --retry 3 --fail -- http://"$lb"/details/1
+
+      - name: Run Ingress conformance test
+        timeout-minutes: 30
+        run: |
+          cd ingress-controller-conformance
+          ./ingress-controller-conformance -ingress-class cilium -wait-time-for-ingress-status 10s -wait-time-for-ready 30s
+
+      - name: Post-test information gathering
+        if: ${{ !success() }}
+        run: |
+          kubectl get pods --all-namespaces -o wide
+          cilium status
+          cilium sysdump --output-filename cilium-sysdump-out
+        shell: bash {0} # Disable default fail-fast behaviour so that all commands run independently
+
+      - name: Upload artifacts
+        if: ${{ !success() }}
+        uses: actions/upload-artifact@0b7f8abb1508181956e8e162db84b466c27e18ce # v3.1.2
+        with:
+          name: cilium-sysdump-out.zip
+          path: cilium-sysdump-out.zip
+          retention-days: 5

install/kubernetes/cilium/templates/cilium-ingress-class.yaml
@@ -3,6 +3,10 @@ apiVersion: networking.k8s.io/v1
 kind: IngressClass
 metadata:
   name: cilium
+  {{- if .Values.ingressController.default }}
+  annotations:
+    ingressclass.kubernetes.io/is-default-class: "true"
+  {{- end}}
 spec:
   controller: cilium.io/ingress-controller
 {{- end}}

install/kubernetes/cilium/templates/cilium-operator/clusterrole.yaml
@@ -228,6 +228,7 @@ rules:
   - networking.k8s.io
   resources:
   - ingresses
+  - ingressclasses
   verbs:
   - get
   - list

install/kubernetes/cilium/values.yaml
@@ -598,6 +598,10 @@ ingressController:
   # This will automatically set enable-envoy-config as well.
   enabled: false
 
+  # -- Set cilium ingress controller to be the default ingress controller
+  # This will let cilium ingress controller route entries without ingress class set
+  default: false
+
   # -- Default ingress load balancer mode
   # Supported values: shared, dedicated
   # For granular control, use the following annotations on the ingress resource

install/kubernetes/cilium/values.yaml.tmpl
@@ -595,6 +595,10 @@ ingressController:
   # This will automatically set enable-envoy-config as well.
   enabled: false
 
+  # -- Set cilium ingress controller to be the default ingress controller
+  # This will let cilium ingress controller route entries without ingress class set
+  default: false
+
   # -- Default ingress load balancer mode
   # Supported values: shared, dedicated
   # For granular control, use the following annotations on the ingress resource

operator/pkg/ingress/ingress_class.go
@@ -1,0 +1,175 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package ingress
+
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+
+	"github.com/cilium/cilium/pkg/k8s"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/k8s/informer"
+	slim_networkingv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/networking/v1"
+	"github.com/cilium/cilium/pkg/k8s/utils"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
+
+type ingressClassAddedEvent struct {
+	ingressClass *slim_networkingv1.IngressClass
+}
+
+type ingressClassUpdatedEvent struct {
+	oldIngressClass *slim_networkingv1.IngressClass
+	newIngressClass *slim_networkingv1.IngressClass
+}
+
+type ingressClassDeletedEvent struct {
+	ingressClass *slim_networkingv1.IngressClass
+}
+
+type ingressClassManager struct {
+	informer cache.Controller
+	store    cache.Store
+
+	queue      workqueue.RateLimitingInterface
+	maxRetries int
+
+	ingressQueue workqueue.RateLimitingInterface
+}
+
+// type used to signal changes to the ingress controller queue
+type ciliumIngressClassUpdatedEvent struct {
+	ingressClass *slim_networkingv1.IngressClass
+}
+
+type ciliumIngressClassDeletedEvent struct {
+	ingressClass *slim_networkingv1.IngressClass
+}
+
+func newIngressClassManager(clientset k8sClient.Clientset, ingressQueue workqueue.RateLimitingInterface, maxRetries int) (*ingressClassManager, error) {
+	manager := &ingressClassManager{
+		queue:        workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
+		ingressQueue: ingressQueue,
+		maxRetries:   maxRetries,
+	}
+
+	manager.store, manager.informer = informer.NewInformer(
+		utils.ListerWatcherFromTyped[*slim_networkingv1.IngressClassList](clientset.Slim().NetworkingV1().IngressClasses()),
+		&slim_networkingv1.IngressClass{},
+		0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc:    manager.handleAddIngressClass,
+			UpdateFunc: manager.handleUpdateIngressClass,
+			DeleteFunc: manager.handleDeleteIngressClass,
+		},
+		nil,
+	)
+
+	go manager.informer.Run(wait.NeverStop)
+	if !cache.WaitForCacheSync(wait.NeverStop, manager.informer.HasSynced) {
+		return manager, fmt.Errorf("unable to sync service")
+	}
+	log.WithField("existing-ingressclasses", manager.store.ListKeys()).Debug("ingress classes synced")
+	return manager, nil
+}
+
+// Run kicks off the control loop
+func (i *ingressClassManager) Run() {
+	for i.processEvent() {
+	}
+}
+
+func (i *ingressClassManager) handleAddIngressClass(obj interface{}) {
+	if ic := k8s.ObjToV1IngressClass(obj); ic != nil {
+		i.queue.Add(ingressClassAddedEvent{ingressClass: ic})
+	}
+}
+
+func (i *ingressClassManager) handleUpdateIngressClass(oldObj, newObj interface{}) {
+	oldIngressClass := k8s.ObjToV1IngressClass(oldObj)
+	if oldIngressClass == nil {
+		return
+	}
+	newIngressClass := k8s.ObjToV1IngressClass(newObj)
+	if newIngressClass == nil {
+		return
+	}
+	if oldIngressClass.DeepEqual(newIngressClass) {
+		return
+	}
+	i.queue.Add(ingressClassUpdatedEvent{oldIngressClass: oldIngressClass, newIngressClass: newIngressClass})
+}
+
+func (i *ingressClassManager) handleDeleteIngressClass(obj interface{}) {
+	if ic := k8s.ObjToV1IngressClass(obj); ic != nil {
+		i.queue.Add(ingressClassDeletedEvent{ingressClass: ic})
+	}
+}
+
+func (i *ingressClassManager) processEvent() bool {
+	event, shutdown := i.queue.Get()
+	if shutdown {
+		return false
+	}
+	defer i.queue.Done(event)
+	err := i.handleEvent(event)
+	if err == nil {
+		i.queue.Forget(event)
+	} else if i.queue.NumRequeues(event) < i.maxRetries {
+		i.queue.AddRateLimited(event)
+	} else {
+		log.Errorf("failed to process event: %s", event)
+		i.queue.Forget(event)
+	}
+	return true
+}
+
+func (i *ingressClassManager) handleEvent(event interface{}) error {
+	var err error
+	switch ev := event.(type) {
+	case ingressClassAddedEvent:
+		log.WithField(logfields.IngressClass, ev.ingressClass.Name).Debug("Handling ingress class added event")
+		err = i.handleIngressClassAddedEvent(ev)
+	case ingressClassUpdatedEvent:
+		log.WithField(logfields.IngressClass, ev.newIngressClass.Name).Debug("Handling ingress class updated event")
+		err = i.handleIngressClassUpdatedEvent(ev)
+	case ingressClassDeletedEvent:
+		log.WithField(logfields.IngressClass, ev.ingressClass.Name).Debug("Handling ingress class deleted event")
+		err = i.handleIngressClassDeletedEvent(ev)
+	default:
+		err = fmt.Errorf("received an unknown event: %s", ev)
+	}
+	return err
+}
+
+func (i *ingressClassManager) handleIngressClassAddedEvent(event ingressClassAddedEvent) error {
+	log.WithField(logfields.IngressClass, event.ingressClass).Debug("Handling ingress class add")
+	i.notify(event.ingressClass)
+	return nil
+}
+
+func (i *ingressClassManager) handleIngressClassUpdatedEvent(event ingressClassUpdatedEvent) error {
+	log.WithField("old", event.oldIngressClass).WithField("new", event.newIngressClass).Debug("Handling ingress class update")
+	i.notify(event.newIngressClass)
+	return nil
+}
+
+func (i *ingressClassManager) handleIngressClassDeletedEvent(event ingressClassDeletedEvent) error {
+	log.WithField(logfields.IngressClass, event.ingressClass).Debug("Handling ingress class delete")
+
+	if event.ingressClass.GetName() == ciliumIngressClassName {
+		i.ingressQueue.Add(ciliumIngressClassDeletedEvent{ingressClass: event.ingressClass})
+	}
+	return nil
+}
+
+// notify informs parent ingress about change in our ingress class configuration
+func (i *ingressClassManager) notify(ic *slim_networkingv1.IngressClass) {
+	if ic.GetName() == ciliumIngressClassName {
+		i.ingressQueue.Add(ciliumIngressClassUpdatedEvent{ingressClass: ic})
+	}
+}

commit ac3ac98db845f9b9146ccbe44ed42212415b5033

ingress: Avoid potential nil pointer during cleanup
As part of garbage collected steps in default Ingress class changes, the
garbage collection process kicks off based on our translation logic.
However, depending on which load-balancer mode is used, the translated
service or endpoint might be nil. This commit is to make sure that we
only performs GC if applicable. Ideally, only Service and Endpoint objs
should be checked, but for readability, nil check for CEC object is also
done.

Fixes: 85671ad3
Fixes: #24347
Signed-off-by: Tam Mach <tam.mach@cilium.io>

operator/pkg/ingress/ingress.go
@@ -654,16 +654,22 @@ func (ic *Controller) garbageCollectOwnedResources(ing *slim_networkingv1.Ingres
 		return err
 	}
 
-	if err := deleteObjectIfExists(cec, ic.envoyConfigManager.getByKey, ic.clientset.CiliumV2().CiliumEnvoyConfigs(cec.GetNamespace()).Delete); err != nil {
-		return err
+	if cec != nil {
+		if err := deleteObjectIfExists(cec, ic.envoyConfigManager.getByKey, ic.clientset.CiliumV2().CiliumEnvoyConfigs(cec.GetNamespace()).Delete); err != nil {
+			return err
+		}
 	}
 
-	if err := deleteObjectIfExists(svc, ic.serviceManager.getByKey, ic.clientset.CoreV1().Services(svc.GetNamespace()).Delete); err != nil {
-		return err
+	if svc != nil {
+		if err := deleteObjectIfExists(svc, ic.serviceManager.getByKey, ic.clientset.CoreV1().Services(svc.GetNamespace()).Delete); err != nil {
+			return err
+		}
 	}
 
-	if err := deleteObjectIfExists(ep, ic.endpointManager.getByKey, ic.clientset.CoreV1().Endpoints(ep.GetNamespace()).Delete); err != nil {
-		return err
+	if ep != nil {
+		if err := deleteObjectIfExists(ep, ic.endpointManager.getByKey, ic.clientset.CoreV1().Endpoints(ep.GetNamespace()).Delete); err != nil {
+			return err
+		}
 	}
 
 	return nil