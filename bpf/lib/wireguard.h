cilium/cilium

commit 29479334c197748fa1ad1bcb990a8d1e8a7933c3

datapath: Change WG integration to support host2host case
This commit completely changes the WireGuard integration in the
datapath to enable the host2host encryption (also, pod2host and
host2pod).

Previously, we supported only the pod2pod case. This was implemented by
marking a to be encrypted packet, and then letting the IP rule installed
in the host netns to forward the packet to the WireGuard tunnel device
"cilium_wg0" for the encryption, as shown below:

┌─────────────────┐
│   Pod A netns   │
│   ┌────────┐    │
│   │  eth0  │    │
└───┴────┬───┴────┘
    ┌────┴──────────┐
    │ bpf_lxc@veth0 │               (host netns)
    └────┬──────────┘
         │1."from-container" in bpf_lxc sets MARK_MAGIC_ENCRYPT
         │2. ip rule matches the mark and routes packet to WG netdev
         │       ┌───────────┐
         └──────►│cilium_wg0 │
                 └────┬──────┘
                      │
                  ┌───▼───┐
                  │ eth0  │
                  └───────┘

This was working fine for the pod2pod case (albeit one danger that a
sysadmin could nuke the rule making the packet to bypass the WG dev).

However, with this approach it was not possible to enable the host2host
case, as a packet originating from the host netns was never handled by
bpf_lxc. Thus, we needed to change the datapath.

To encrypt a host2host packet we need to attach bpf_host to the outgoing
device connecting cluster nodes which in the picture is "eth0". Then the
program "to-netdev" from bpf_host can forward the packet to the WG dev.
Once encrypted, the packet will be again hitting the same bpf_host
program. To avoid the packet looping forever, we can configure the WG
netdev to set the skb mark after the encryption. Then, in the program
we can skip the redirection to the WG netdev if the mark is set.

The flow below shows the new integration.

┌─────────────────┐
│   Pod A netns   │
│   ┌────────┐    │
│   │  eth0  │    │
└───┴────┬───┴────┘
    ┌────┴──────────┐

bpf/lib/wireguard.h
@@ -1,0 +1,91 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright Authors of Cilium */
+
+#ifdef ENABLE_WIREGUARD
+
+#ifndef __WIREGUARD_H_
+#define __WIREGUARD_H_
+
+#include <bpf/ctx/ctx.h>
+#include <bpf/api.h>
+
+#include "tailcall.h"
+#include "common.h"
+#include "overloadable.h"
+
+static __always_inline int
+wg_maybe_redirect_to_encrypt(struct __ctx_buff *ctx)
+{
+	struct remote_endpoint_info *dst;
+	struct remote_endpoint_info __maybe_unused *src = NULL;
+	void *data, *data_end;
+	__u16 proto = 0;
+	struct ipv6hdr __maybe_unused *ip6;
+	struct iphdr __maybe_unused *ip4;
+
+	if (!validate_ethertype(ctx, &proto))
+		return DROP_UNSUPPORTED_L2;
+
+	switch (proto) {
+#ifdef ENABLE_IPV6
+	case bpf_htons(ETH_P_IPV6):
+		if (!revalidate_data(ctx, &data, &data_end, &ip6))
+			return DROP_INVALID;
+		dst = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr, 0);
+#ifndef ENABLE_NODE_ENCRYPTION
+		src = lookup_ip6_remote_endpoint((union v6addr *)&ip6->saddr, 0);
+#endif /* ENABLE_NODE_ENCRYPTION */
+		break;
+#endif
+#ifdef ENABLE_IPV4
+	case bpf_htons(ETH_P_IP):
+		if (!revalidate_data(ctx, &data, &data_end, &ip4))
+			return DROP_INVALID;
+		dst = lookup_ip4_remote_endpoint(ip4->daddr, 0);
+#ifndef ENABLE_NODE_ENCRYPTION
+		src = lookup_ip4_remote_endpoint(ip4->saddr, 0);
+#endif /* ENABLE_NODE_ENCRYPTION */
+		break;
+#endif
+	default:
+		goto out;
+	}
+
+	/* Redirect to the WireGuard tunnel device if the encryption is
+	 * required.
+	 *
+	 * After the packet has been encrypted, the WG tunnel device
+	 * will set the MARK_MAGIC_WG_ENCRYPTED skb mark. So, to avoid
+	 * looping forever (e.g., bpf_host@eth0 => cilium_wg0 =>
+	 * bpf_host@eth0 => ...; this happens when eth0 is used to send
+	 * encrypted WireGuard UDP packets), we check whether the mark
+	 * is set before the redirect.
+	 */
+	if ((ctx->mark & MARK_MAGIC_WG_ENCRYPTED) == MARK_MAGIC_WG_ENCRYPTED)
+		goto out;
+
+	/* Unless node encryption is enabled, we don't want to encrypt
+	 * traffic from the hostns.
+	 *
+	 * NB: if iptables has SNAT-ed the packet, its sec id is HOST_ID.
+	 * This means that the packet won't be encrypted. This is fine,
+	 * as with --encrypt-node=false we encrypt only pod-to-pod packets.
+	 */
+#ifndef ENABLE_NODE_ENCRYPTION
+	if (!src || src->sec_label == HOST_ID)
+		goto out;
+#endif /* ENABLE_NODE_ENCRYPTION */
+
+	/* Redirect to the WireGuard tunnel device if the encryption is
+	 * required.
+	 */
+	if (dst && dst->key)
+		return ctx_redirect(ctx, WG_IFINDEX, 0);
+
+out:
+	return CTX_ACT_OK;
+}
+
+#endif /* __WIREGUARD_H_ */
+
+#endif /* ENABLE_WIREGUARD */

bpf/Makefile
@@ -180,7 +180,7 @@ HOST_OPTIONS = $(LXC_OPTIONS) \
 	-DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_DSR:-DENABLE_DSR_HYBRID:-DENABLE_PREFILTER:-DENABLE_SESSION_AFFINITY:-DENABLE_HOST_FIREWALL:-DENABLE_ICMP_RULE:-DENABLE_SRV6:-DENABLE_SRV6_SRH_ENCAP:-DENABLE_SCTP:
 
 ifndef MAX_HOST_OPTIONS
-MAX_HOST_OPTIONS = $(MAX_BASE_OPTIONS) -DENCAP_IFINDEX=1 -DTUNNEL_MODE=1
+MAX_HOST_OPTIONS = $(MAX_BASE_OPTIONS) -DENCAP_IFINDEX=1 -DTUNNEL_MODE=1 -DENABLE_WIREGUARD=1
 
 ifeq ("$(KERNEL)","54")
 MAX_HOST_OPTIONS+= -DENABLE_VTEP=1
@@ -271,8 +271,7 @@ LXC_OPTIONS = \
 	 -DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DUSE_BPF_PROG_FOR_INGRESS_POLICY: \
 	 -DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DHAVE_LPM_TRIE_MAP_TYPE:-DHAVE_LRU_HASH_MAP_TYPE:-DENABLE_TPROXY:-DENABLE_HOST_ROUTING: \
 	 -DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DHAVE_LPM_TRIE_MAP_TYPE:-DHAVE_LRU_HASH_MAP_TYPE:-DENABLE_TPROXY:-DENABLE_HOST_ROUTING:-DENABLE_SKIP_FIB: \
-	 -DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DHAVE_LPM_TRIE_MAP_TYPE:-DHAVE_LRU_HASH_MAP_TYPE:-DENABLE_TPROXY:-DENABLE_HOST_ROUTING:-DENABLE_SKIP_FIB:-DENABLE_WIREGUARD: \
-	 -DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DHAVE_LPM_TRIE_MAP_TYPE:-DHAVE_LRU_HASH_MAP_TYPE:-DENABLE_TPROXY:-DENABLE_HOST_ROUTING:-DENABLE_SKIP_FIB:-DENABLE_WIREGUARD:-DENABLE_ICMP_RULE:-DENABLE_SCTP:
+	 -DENABLE_IPV4:-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DHAVE_LPM_TRIE_MAP_TYPE:-DHAVE_LRU_HASH_MAP_TYPE:-DENABLE_TPROXY:-DENABLE_HOST_ROUTING:-DENABLE_SKIP_FIB:-DENABLE_ICMP_RULE:-DENABLE_SCTP:
 
 # These options are intended to max out the BPF program complexity. it is load
 # tested as well.

bpf/bpf_host.c
@@ -58,6 +58,7 @@
 #include "lib/egress_policies.h"
 #include "lib/overloadable.h"
 #include "lib/encrypt.h"
+#include "lib/wireguard.h"
 
 static __always_inline bool allow_vlan(__u32 __maybe_unused ifindex, __u32 __maybe_unused vlan_id) {
 	VLAN_FILTER(ifindex, vlan_id);
@@ -1191,6 +1192,25 @@ out:
 		return ret;
 	}
 #endif
+
+#ifdef ENABLE_WIREGUARD
+	/* Redirect the packet to the WireGuard tunnel device for encryption
+	 * if needed.
+	 * We assume that a packet, which is a subject to the encryption, is
+	 * NOT a subject to the BPF SNAT (happening below), as the former's
+	 * destination resides in the cluster, while the latter - outside the
+	 * cluster.
+	 * Once the assumption is no longer true, we will need to recirculate
+	 * the packet back to the "to-netdev" section for the SNAT instead of
+	 * returning TC_ACT_REDIRECT.
+	 */
+	ret = wg_maybe_redirect_to_encrypt(ctx);
+	if (ret == CTX_ACT_REDIRECT)
+		return ret;
+	else if (IS_ERR(ret))
+		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
+					      METRIC_EGRESS);
+#endif /* ENABLE_WIREGUARD */
 
 #ifdef ENABLE_SRV6
 	ret = handle_srv6(ctx);

bpf/bpf_lxc.c
@@ -220,7 +220,6 @@ static __always_inline int handle_ipv6_from_lxc(struct __ctx_buff *ctx, __u32 *d
 	bool hairpin_flow = false; /* endpoint wants to access itself via service IP */
 	__u8 policy_match_type = POLICY_MATCH_NONE;
 	__u8 audited = 0;
-	bool __maybe_unused dst_remote_ep = false;
 	__u16 proxy_port = 0;
 	bool from_l7lb = false;
 
@@ -241,11 +240,6 @@ static __always_inline int handle_ipv6_from_lxc(struct __ctx_buff *ctx, __u32 *d
 			*dst_id = info->sec_label;
 			tunnel_endpoint = info->tunnel_endpoint;
 			encrypt_key = get_min_encrypt_key(info->key);
-#ifdef ENABLE_WIREGUARD
-			if (info->tunnel_endpoint != 0 &&
-			    !identity_is_node(info->sec_label))
-				dst_remote_ep = true;
-#endif /* ENABLE_WIREGUARD */
 		} else {
 			*dst_id = WORLD_ID;
 		}
@@ -475,9 +469,6 @@ ct_recreate6:
 
 	/* The packet goes to a peer not managed by this agent instance */
 #ifdef TUNNEL_MODE
-# ifdef ENABLE_WIREGUARD
-	if (!dst_remote_ep)
-# endif /* ENABLE_WIREGUARD */
 	{
 		struct endpoint_key key = {};
 		union v6addr *daddr = (union v6addr *)&ip6->daddr;
@@ -539,11 +530,7 @@ pass_to_stack:
 	if (ipv6_store_flowlabel(ctx, ETH_HLEN, SECLABEL_NB) < 0)
 		return DROP_WRITE_ERROR;
 
-#ifdef ENABLE_WIREGUARD
-	if (dst_remote_ep)
-		set_encrypt_mark(ctx);
-	else
-#elif !defined(TUNNEL_MODE)
+#ifndef TUNNEL_MODE
 # ifdef ENABLE_IPSEC
 	if (encrypt_key && tunnel_endpoint) {
 		set_encrypt_key_mark(ctx, encrypt_key);
@@ -555,7 +542,7 @@ pass_to_stack:
 #  endif /* ENABLE_IDENTITY_MARK */
 	} else
 # endif /* ENABLE_IPSEC */
-#endif /* ENABLE_WIREGUARD */
+#endif /* TUNNEL_MODE */
 	{
 #ifdef ENABLE_IDENTITY_MARK
 		/* Always encode the source identity when passing to the stack.
@@ -746,7 +733,6 @@ static __always_inline int handle_ipv4_from_lxc(struct __ctx_buff *ctx, __u32 *d
 	struct ct_buffer4 *ct_buffer;
 	__u8 audited = 0;
 	bool has_l4_header = false;
-	bool __maybe_unused dst_remote_ep = false;
 	enum ct_status ct_status;
 	__u16 proxy_port = 0;
 	bool from_l7lb = false;
@@ -765,17 +751,6 @@ static __always_inline int handle_ipv4_from_lxc(struct __ctx_buff *ctx, __u32 *d
 			*dst_id = info->sec_label;
 			tunnel_endpoint = info->tunnel_endpoint;
 			encrypt_key = get_min_encrypt_key(info->key);
-#ifdef ENABLE_WIREGUARD
-			/* If we detect that the dst is a remote endpoint, we
-			 * need to mark the packet. The ip rule which matches
-			 * on the MARK_MAGIC_ENCRYPT mark will steer the packet
-			 * to the Wireguard tunnel. The marking happens lower
-			 * in the code in the same place where we handle IPSec.
-			 */
-			if (info->tunnel_endpoint != 0 &&
-			    !identity_is_node(info->sec_label))
-				dst_remote_ep = true;
-#endif /* ENABLE_WIREGUARD */
 		} else {
 			*dst_id = WORLD_ID;
 		}
@@ -1058,12 +1033,6 @@ skip_vtep:
 #endif
 
 #ifdef TUNNEL_MODE
-# ifdef ENABLE_WIREGUARD
-	/* In the tunnel mode we encapsulate pod2pod traffic only via Wireguard
-	 * device, i.e. we do not encapsulate twice.
-	 */
-	if (!dst_remote_ep)
-# endif /* ENABLE_WIREGUARD */
 	{
 		struct endpoint_key key = {};
 
@@ -1115,11 +1084,7 @@ pass_to_stack:
 		return ret;
 #endif
 
-#ifdef ENABLE_WIREGUARD
-	if (dst_remote_ep)
-		set_encrypt_mark(ctx);
-	else /* Wireguard and identity mark are mutually exclusive */
-#elif !defined(TUNNEL_MODE)
+#ifndef TUNNEL_MODE
 # ifdef ENABLE_IPSEC
 	if (encrypt_key && tunnel_endpoint) {
 		set_encrypt_key_mark(ctx, encrypt_key);
@@ -1131,7 +1096,7 @@ pass_to_stack:
 #  endif
 	} else
 # endif /* ENABLE_IPSEC */
-#endif /* ENABLE_WIREGUARD */
+#endif /* TUNNEL_MODE */
 	{
 #ifdef ENABLE_IDENTITY_MARK
 		/* Always encode the source identity when passing to the stack.

bpf/lib/common.h
@@ -576,6 +576,13 @@ enum metric_dir {
 #define MARK_MAGIC_KEY_MASK		0xFF00
 
 
+/* The mark is used to indicate that the WireGuard tunnel device is done
+ * encrypting a packet. The MSB invades the Kubernetes mark "space" which is
+ * fine, as it's not used by K8s. See pkg/datapath/linux/linux_defaults/mark.go
+ * for more details.
+ */
+#define MARK_MAGIC_WG_ENCRYPTED		0x1E00
+
 /* MARK_MAGIC_HEALTH_IPIP_DONE can overlap with MARK_MAGIC_SNAT_DONE with both
  * being mutual exclusive given former is only under DSR. Used to push health
  * probe packets to ipip tunnel device & to avoid looping back.

bpf/lib/overloadable_skb.h
@@ -73,17 +73,6 @@ set_encrypt_key_meta(struct __sk_buff *ctx, __u8 key)
 	ctx->cb[CB_ENCRYPT_MAGIC] = or_encrypt_key(key);
 }
 
-/**
- * set_encrypt_mark - sets the encryption mark to make skb to match ip rule
- * used to steer packet into Wireguard tunnel device (cilium_wg0) in order to
- * encrypt it.
- */
-static __always_inline __maybe_unused void
-set_encrypt_mark(struct __sk_buff *ctx)
-{
-	ctx->mark |= MARK_MAGIC_ENCRYPT;
-}
-
 static __always_inline __maybe_unused int
 redirect_self(const struct __sk_buff *ctx)
 {

bpf/node_config.h
@@ -223,6 +223,10 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 # define LB_SELECTION		LB_SELECTION_RANDOM
 #endif
 
+#ifdef ENABLE_WIREGUARD
+# define WG_IFINDEX	42
+#endif
+
 #ifdef ENABLE_VTEP
 # define VTEP_MASK 0xffffff
 #endif

pkg/datapath/linux/config/config.go
@@ -241,6 +241,9 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 
 	if option.Config.EnableWireguard {
 		cDefinesMap["ENABLE_WIREGUARD"] = "1"
+		if option.Config.EncryptNode {
+			cDefinesMap["ENABLE_NODE_ENCRYPTION"] = "1"
+		}
 	}
 
 	if option.Config.EnableBPFTProxy {

commit 30ea50f3f53a516dd04ef5b4ea365115cff1948c

bpf/wireguard: Skip encryption for cluster-external traffic
Egress gateway traffic travels from a client pod to a gateway node via
the overlay to be SNATed at the gateway node's native device. The reply
traffic should then take the same path back to the client pod.

When WireGuard is also enabled, however, we observe that the reply
traffic goes through the WireGuard device instead of the overlay device.
More specifically, after being reverse SNATed in bpf_host's handle_ipv4,
this reply traffic goes through encap_and_redirect_with_nodeid to be
redirected to the overlay device. There, it runs through
wg_maybe_redirect_to_encrypt, which was never taught to skip traffic from
outside the cluster.

Without the egress gateway, this scenario doesn't happen for WireGuard
because all traffic coming from outside the cluster is either:
- destined to the local node and not subject to WireGuard encryption, or
- destined to a service with a remote backend, but redirected in the
  BPF NodePort logic.

This commit fixes this bug by adding a new case in
wg_maybe_redirect_to_encrypt, to skip encryption for any traffic that
originates from outside the cluster.

Fixes: 29479334c19 ("datapath: Change WG integration to support host2host case")
Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/lib/wireguard.h
@@ -92,6 +92,15 @@ wg_maybe_redirect_to_encrypt(struct __ctx_buff *ctx)
 		goto out;
 #endif /* ENABLE_NODE_ENCRYPTION */
 
+	/* We don't want to encrypt any traffic that originates from outside
+	 * the cluster.
+	 * Without this check, that may happen for the egress gateway, when
+	 * reply traffic arrives from the cluster-external server and goes to
+	 * the client pod.
+	 */
+	if (!src || !identity_is_cluster(src->sec_label))
+		goto out;
+
 	/* Redirect to the WireGuard tunnel device if the encryption is
 	 * required.
 	 */