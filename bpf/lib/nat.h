cilium/cilium

commit 156c30e511854869bb8486a6c66d55e3892fbd6f

bpf/nat: implement support of ICMP6 Error packet too big
Running in masquerading BPF mode.

Assuming that, Pod is sending a packet to the world of a size bigger
than the accepted MTU in the networking path.

A networking equipment in that networking path would return an ICMP
Error packet with code PACKET_TOO_BIG. According to the RFE 5508,
that networking equipment should embed the original packet in its
response.

Previously because of the missing support of PACKET_TOO_BIG, the NAT
session to return the packet to Pod was never resolved. Meaning that
the ICMP Error packet got dropped.

 a) The src IP of the packet becomes the networking equipment that is
 generating the error response.  b) The protocol used for the response
 is ICMP where the packet sent may be TDP/UPD.

This change is updating the NAT process to read the embedded packet in
the ICMP Error packet that to determine the NAT session and send back
the packet to Pod.

This change is for IPv6.

Fixes: #12968
Signed-off-by: Sahid Orentino Ferdjaoui <sahid.ferdjaoui@industrialdiscipline.com>

bpf/lib/nat.h
@@ -951,7 +951,7 @@ snat_v4_rev_nat(struct __ctx_buff *ctx, const struct ipv4_nat_target *target)
 					tuple.dport = identifier;
 					break;
 				default:
-					return DROP_INVALID;
+					return DROP_UNKNOWN_L4;
 				}
 				state = snat_v4_lookup(&tuple);
 				if (!state)
@@ -1267,6 +1267,69 @@ snat_v6_rev_nat_handle_mapping(struct __ctx_buff *ctx,
 		       NAT_PUNT_TO_STACK : DROP_NAT_NO_MAPPING;
 }
 
+static __always_inline int snat_v6_icmp_rewrite_embedded(struct __ctx_buff *ctx,
+							 struct ipv6_ct_tuple *tuple,
+							 struct ipv6_nat_entry *state,
+							 __u32 l4_off, __u32 inner_l4_off)
+{
+	struct csum_offset csum = {};
+
+	if (ipv6_addrcmp(&state->to_daddr, &tuple->daddr) == 0 &&
+	    state->to_dport == tuple->dport)
+		return 0;
+
+	csum_l4_offset_and_flags(tuple->nexthdr, &csum);
+	if (state->to_dport != tuple->dport) {
+		__be32 suml4 = 0;
+
+		switch (tuple->nexthdr) {
+		case IPPROTO_TCP:
+		case IPPROTO_UDP:
+#ifdef ENABLE_SCTP
+		case IPPROTO_SCTP:
+
+#endif  /* ENABLE_SCTP */
+			/* In case that the destination port has been NATed from
+			 * target to dest. We want the embedded packet which
+			 * should refer to endpoint dest going back to original.
+			 */
+			if (ctx_store_bytes(ctx, inner_l4_off + offsetof(struct tcphdr, source),
+					    &state->to_dport, sizeof(state->to_dport), 0) < 0)
+				return DROP_WRITE_ERROR;
+			break;
+		case IPPROTO_ICMPV6: {
+			/* In case that the ID has been used as source port during
+			 * NAT from target to dest. We want the embedded packet
+			 * which should refer to endpoint -> dest going back to
+			 * original.
+			 */
+			if (ctx_store_bytes(ctx, inner_l4_off +
+					    offsetof(struct icmp6hdr,
+						     icmp6_dataun.u_echo.identifier),
+					    &state->to_dport,
+					    sizeof(state->to_dport), 0) < 0)
+				return DROP_WRITE_ERROR;
+			break;
+		}
+		default:
+			return DROP_INVALID;
+		}
+		/* By recomputing L4 checksum of inner packet we avoid having
+		 * to recompute L4 of the ICMP Error.
+		 */
+		suml4 = csum_diff(&tuple->dport, 4, &state->to_dport, 4, 0);
+		if (csum_l4_replace(ctx, inner_l4_off, &csum, 0, suml4, BPF_F_PSEUDO_HDR) < 0)
+			return DROP_CSUM_L4;
+	}
+	/* Change IP of source address of inner packet to refer the
+	 * endpoint.
+	 */
+	if (ipv6_store_saddr(ctx, (__u8 *)&state->to_daddr,
+			     l4_off + sizeof(struct icmp6hdr) - 4) < 0)
+		return DROP_WRITE_ERROR;
+	return 0;
+}
+
 static __always_inline int snat_v6_rewrite_egress(struct __ctx_buff *ctx,
 						  struct ipv6_ct_tuple *tuple,
 						  struct ipv6_nat_entry *state,
@@ -1539,6 +1602,7 @@ snat_v6_rev_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 		__be16 dport;
 	} l4hdr;
 	__u32 off;
+	__u8 nexthdr;
 
 	build_bug_on(sizeof(struct ipv6_nat_entry) > 64);
 
@@ -1551,6 +1615,7 @@ snat_v6_rev_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 		return hdrlen;
 
 	snat_v6_init_tuple(ip6, NAT_DIR_INGRESS, &tuple);
+	nexthdr = tuple.nexthdr;
 
 	off = ((void *)ip6 - data) + hdrlen;
 	switch (tuple.nexthdr) {
@@ -1580,6 +1645,82 @@ snat_v6_rev_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 			tuple.dport = icmp6hdr.icmp6_dataun.u_echo.identifier;
 			tuple.sport = 0;
 			break;
+		case ICMPV6_PKT_TOOBIG: {
+			struct ipv6hdr iphdr;
+			__be16 identifier;
+			__u8 type;
+			__u32 icmpoff = off;
+
+			/* According to the RFC 5508, any networking
+			 * equipment that is responding with an ICMP Error
+			 * packet should embed the original packet in its
+			 * response.
+			 */
+
+			/* Note related to how is computed the offset. The
+			 * ICMPV6_PKT_TOOBIG does not include identifer and
+			 * sequence in its headers.
+			 */
+			icmpoff += sizeof(icmp6hdr) - sizeof(icmp6hdr.icmp6_dataun.u_echo);
+
+			if (ctx_load_bytes(ctx, icmpoff, &iphdr,
+					   sizeof(iphdr)) < 0)
+				return DROP_INVALID;
+
+			/* From the embedded IP headers we should be able
+			 * to determine corresponding protocol, IP src/dst
+			 * of the packet sent to resolve the NAT session.
+			 */
+
+			tuple.nexthdr = iphdr.nexthdr;
+			ipv6_addr_copy(&tuple.saddr, (union v6addr *)&iphdr.daddr);
+			ipv6_addr_copy(&tuple.daddr, (union v6addr *)&iphdr.saddr);
+
+			icmpoff += ipv6_hdrlen_offset(ctx, &iphdr.nexthdr, icmpoff);
+			switch (tuple.nexthdr) {
+			case IPPROTO_TCP:
+			case IPPROTO_UDP:
+				/* No reasons to handle IP fragmentation for this case
+				 * as it is expected that DF isn't set for this particular
+				 * context.
+				 */
+				if (l4_load_ports(ctx, icmpoff, &tuple.dport) < 0)
+					return DROP_INVALID;
+				break;
+			case IPPROTO_ICMPV6:
+				/* No reasons to see a packet different than
+				 * ICMPV6_ECHO_REQUEST.
+				 */
+				if (ctx_load_bytes(ctx, icmpoff, &type, sizeof(type)) < 0 ||
+				    type != ICMPV6_ECHO_REQUEST)
+					return DROP_INVALID;
+				if (ctx_load_bytes(ctx, icmpoff +
+						   offsetof(struct icmp6hdr,
+							    icmp6_dataun.u_echo.identifier),
+						   &identifier, sizeof(identifier)) < 0)
+					return DROP_INVALID;
+				tuple.sport = 0;
+				tuple.dport = identifier;
+				break;
+			default:
+				return DROP_INVALID;
+			}
+			state = snat_v6_lookup(&tuple);
+			if (!state)
+				return NAT_PUNT_TO_STACK;
+
+			/* We found SNAT entry to rev-NAT embedded packet. The source addr
+			 * should point to endpoint that initiated the packet, as-well if
+			 * dest port had been NATed.
+			 */
+			ret = snat_v6_icmp_rewrite_embedded(ctx, &tuple, state, off, icmpoff);
+			if (IS_ERR(ret))
+				return ret;
+
+			/* Switch back to the outer header. */
+			tuple.nexthdr = nexthdr;
+			goto rewrite_ingress;
+		}
 		default:
 			return DROP_NAT_UNSUPP_PROTO;
 		}
@@ -1596,6 +1737,7 @@ snat_v6_rev_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 	if (ret < 0)
 		return ret;
 
+rewrite_ingress:
 	return snat_v6_rewrite_ingress(ctx, &tuple, state, off);
 }
 #else

commit 9e25d33bdba9e908d30ee72db013f4c28bd0e24f

bpf: nat: fix error handling for ICMPV6_PKT_TOOBIG
ipv6_hdrlen_offset() can return an error, handle it.

Fixes: 156c30e51185 ("bpf/nat: implement support of ICMP6 Error packet too big")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nat.h
@@ -1740,7 +1740,12 @@ snat_v6_rev_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 			ipv6_addr_copy(&tuple.saddr, (union v6addr *)&iphdr.daddr);
 			ipv6_addr_copy(&tuple.daddr, (union v6addr *)&iphdr.saddr);
 
-			icmpoff += ipv6_hdrlen_offset(ctx, &iphdr.nexthdr, icmpoff);
+			hdrlen = ipv6_hdrlen_offset(ctx, &iphdr.nexthdr, icmpoff);
+			if (hdrlen < 0)
+				return hdrlen;
+
+			icmpoff += hdrlen;
+
 			switch (tuple.nexthdr) {
 			case IPPROTO_TCP:
 			case IPPROTO_UDP: