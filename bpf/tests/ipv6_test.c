cilium/cilium

commit 1ce3c7f6a4745bb6c420fb59106cfe0587ff18fa

bpf: Skip over IPv6 extension headers
Acked-by: Daniel Borkmann <daniel@cilium.io>
Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/lib/ipv6.h
@@ -11,6 +11,75 @@
 #define IPV6_TCLASS_MASK (IPV6_FLOWINFO_MASK & ~IPV6_FLOWLABEL_MASK)
 #define IPV6_TCLASS_SHIFT       20
 
+/* Number of extension headers that can be skipped */
+#define IPV6_MAX_HEADERS 10
+
+#define NEXTHDR_HOP             0       /* Hop-by-hop option header. */
+#define NEXTHDR_TCP             6       /* TCP segment. */
+#define NEXTHDR_UDP             17      /* UDP message. */
+#define NEXTHDR_IPV6            41      /* IPv6 in IPv6 */
+#define NEXTHDR_ROUTING         43      /* Routing header. */
+#define NEXTHDR_FRAGMENT        44      /* Fragmentation/reassembly header. */
+#define NEXTHDR_GRE             47      /* GRE header. */
+#define NEXTHDR_ESP             50      /* Encapsulating security payload. */
+#define NEXTHDR_AUTH            51      /* Authentication header. */
+#define NEXTHDR_ICMP            58      /* ICMP for IPv6. */
+#define NEXTHDR_NONE            59      /* No next header */
+#define NEXTHDR_DEST            60      /* Destination options header. */
+#define NEXTHDR_SCTP            132     /* SCTP message. */
+#define NEXTHDR_MOBILITY        135     /* Mobility header. */
+
+#define NEXTHDR_MAX             255
+
+static inline int ipv6_optlen(struct ipv6_opt_hdr *opthdr)
+{
+	return (opthdr->hdrlen + 1) << 3;
+}
+
+static inline int ipv6_authlen(struct ipv6_opt_hdr *opthdr)
+{
+	return (opthdr->hdrlen + 2) << 2;
+}
+
+static inline int __inline__ ipv6_hdrlen(struct __sk_buff *skb, int l3_off, __u8 *nexthdr)
+{
+	int i, len = sizeof(struct ipv6hdr);
+	struct ipv6_opt_hdr opthdr;
+	__u8 nh = *nexthdr;
+
+#pragma unroll
+	for (i = 0; i < IPV6_MAX_HEADERS; i++) {
+		switch (nh) {
+		case NEXTHDR_NONE:
+			return DROP_INVALID_EXTHDR;
+
+		case NEXTHDR_FRAGMENT:
+			return DROP_FRAG_NOSUPPORT;
+
+		case NEXTHDR_HOP:
+		case NEXTHDR_ROUTING:
+		case NEXTHDR_AUTH:
+		case NEXTHDR_DEST:
+			if (skb_load_bytes(skb, l3_off + len, &opthdr, sizeof(opthdr)) < 0)
+				return DROP_INVALID;
+
+			nh = opthdr.nexthdr;
+			if (nh == NEXTHDR_AUTH)
+				len += ipv6_authlen(&opthdr);
+			else
+				len += ipv6_optlen(&opthdr);
+			break;
+
+		default:
+			*nexthdr = nh;
+			return len;
+		}
+	}
+
+	/* Reached limit of supported extension headers */
+	return DROP_INVALID_EXTHDR;
+}
+
 static inline void ipv6_addr_copy(union v6addr *dst, union v6addr *src)
 {
 	dst->p1 = src->p1;

bpf/bpf_lxc.c
@@ -80,12 +80,12 @@ static inline int __inline__ lxc_encap(struct __sk_buff *skb, __u32 node_id)
 #endif
 
 static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
-				   struct ipv6_ct_tuple *tuple, int nh_off,
+				   struct ipv6_ct_tuple *tuple, int l3_off,
 				   struct ethhdr *eth, struct ipv6hdr *ip6)
 {
 	union macaddr router_mac = NODE_MAC;
 	union v6addr host_ip = HOST_IP;
-	int do_nat46 = 0, ret;
+	int do_nat46 = 0, ret, l4_off;
 
 	if (unlikely(!valid_src_mac(eth)))
 		return DROP_INVALID_SMAC;
@@ -108,8 +108,8 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	 */
 	ipv6_addr_copy(&tuple->addr, (union v6addr *) &ip6->daddr);
 
-	/* FIXME: Handle extensions header size */
-	ret = map_lxc_out(skb, nh_off + sizeof(*ip6), ip6->nexthdr);
+	l4_off = l3_off + ipv6_hdrlen(skb, l3_off, &tuple->nexthdr);
+	ret = map_lxc_out(skb, l4_off, tuple->nexthdr);
 	if (IS_ERR(ret))
 		return ret;
 
@@ -119,7 +119,7 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	 * entry to allow reverse packets and return set cb[CB_POLICY] to
 	 * POLICY_SKIP if the packet is a reply packet to an existing
 	 * incoming connection. */
-	ret = ct_lookup6(&CT_MAP6, tuple, skb, ETH_HLEN, SECLABEL, 0);
+	ret = ct_lookup6(&CT_MAP6, tuple, skb, l4_off, SECLABEL, 0);
 	if (ret < 0)
 		return ret;
 
@@ -170,7 +170,7 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 		if (data + sizeof(struct ipv6hdr) + ETH_HLEN > data_end)
 			return DROP_INVALID;
 
-		return ipv6_local_delivery(skb, nh_off, SECLABEL, ip6);
+		return ipv6_local_delivery(skb, l3_off, l4_off, SECLABEL, ip6, tuple->nexthdr);
 	} else {
 #ifdef ENABLE_NAT46
 		/* FIXME: Derive from prefix constant */
@@ -193,7 +193,7 @@ to_host:
 
 		cilium_trace(skb, DBG_TO_HOST, skb->cb[CB_POLICY], 0);
 
-		ret = ipv6_l3(skb, nh_off, (__u8 *) &router_mac.addr, (__u8 *) &host_mac.addr);
+		ret = ipv6_l3(skb, l3_off, (__u8 *) &router_mac.addr, (__u8 *) &host_mac.addr);
 		if (ret != TC_ACT_OK)
 			return ret;
 
@@ -224,11 +224,11 @@ to_host:
 pass_to_stack:
 	cilium_trace(skb, DBG_TO_STACK, skb->cb[CB_POLICY], 0);
 
-	ret = ipv6_l3(skb, nh_off, NULL, (__u8 *) &router_mac.addr);
+	ret = ipv6_l3(skb, l3_off, NULL, (__u8 *) &router_mac.addr);
 	if (unlikely(ret != TC_ACT_OK))
 		return ret;
 
-	if (ipv6_store_flowlabel(skb, nh_off, SECLABEL_NB) < 0)
+	if (ipv6_store_flowlabel(skb, l3_off, SECLABEL_NB) < 0)
 		return DROP_WRITE_ERROR;
 
 #ifndef POLICY_ENFORCEMENT
@@ -311,7 +311,7 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	 */
 	tuple.addr = ip4->daddr;
 	l4_off = l3_off + ipv4_hdrlen(ip4);
-	ret = map_lxc_out(skb, l4_off, ip4->protocol);
+	ret = map_lxc_out(skb, l4_off, tuple.nexthdr);
 	if (IS_ERR(ret))
 		return ret;
 
@@ -497,7 +497,7 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	void *data = (void *) (long) skb->data;
 	void *data_end = (void *) (long) skb->data_end;
 	struct ipv6hdr *ip6 = data + ETH_HLEN;
-	int ret;
+	int ret, l4_off;
 
 	if (data + sizeof(struct ipv6hdr) + ETH_HLEN > data_end)
 		return DROP_INVALID;
@@ -506,7 +506,8 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	tuple.nexthdr = ip6->nexthdr;
 	ipv6_addr_copy(&tuple.addr, (union v6addr *) &ip6->saddr);
 
-	ret = ct_lookup6(&CT_MAP6, &tuple, skb, ETH_HLEN, SECLABEL, 1);
+	l4_off = ETH_HLEN + ipv6_hdrlen(skb, ETH_HLEN, &tuple.nexthdr);
+	ret = ct_lookup6(&CT_MAP6, &tuple, skb, l4_off, SECLABEL, 1);
 	if (ret < 0)
 		return ret;
 

bpf/bpf_netdev.c
@@ -94,13 +94,18 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 	void *data_end = (void *) (long) skb->data_end;
 	struct ipv6hdr *ip6 = data + ETH_HLEN;
 	union v6addr *dst = (union v6addr *) &ip6->daddr;
+	int l4_off, l3_off = ETH_HLEN;
+	__u8 nexthdr;
 	__u32 flowlabel;
 
-	if (data + ETH_HLEN + sizeof(*ip6) > data_end)
+	if (data + l3_off + sizeof(*ip6) > data_end)
 		return DROP_INVALID;
 
+	nexthdr = ip6->nexthdr;
+	l4_off = l3_off + ipv6_hdrlen(skb, l3_off, &nexthdr);
+
 #ifdef HANDLE_NS
-	if (unlikely(ip6->nexthdr == IPPROTO_ICMPV6)) {
+	if (unlikely(nexthdr == IPPROTO_ICMPV6)) {
 		int ret = icmp6_handle(skb, ETH_HLEN, ip6);
 		if (IS_ERR(ret))
 			return ret;
@@ -110,7 +115,7 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 	flowlabel = derive_sec_ctx(skb, &node_ip, ip6);
 
 	if (likely(is_node_subnet(dst, &node_ip)))
-		return ipv6_local_delivery(skb, ETH_HLEN, flowlabel, ip6);
+		return ipv6_local_delivery(skb, l3_off, l4_off, flowlabel, ip6, nexthdr);
 
 	return TC_ACT_OK;
 }

bpf/bpf_overlay.c
@@ -22,9 +22,10 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 	struct ipv6hdr *ip6 = data + ETH_HLEN;
 	union v6addr *dst = (union v6addr *) &ip6->daddr;
 	struct bpf_tunnel_key key = {};
+	int l4_off, l3_off = ETH_HLEN;
 	__u32 node_id;
 
-	if (data + sizeof(*ip6) + ETH_HLEN > data_end)
+	if (data + sizeof(*ip6) + l3_off > data_end)
 		return DROP_INVALID;
 
 	if (unlikely(skb_get_tunnel_key(skb, &key, sizeof(key), 0) < 0))
@@ -51,8 +52,11 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 
 	if (unlikely(node_id != NODE_ID))
 		return DROP_NON_LOCAL;
-	else
-		return ipv6_local_delivery(skb, ETH_HLEN, key.tunnel_id, ip6);
+	else {
+		__u8 nexthdr = ip6->nexthdr;
+		l4_off = l3_off + ipv6_hdrlen(skb, l3_off, &nexthdr);
+		return ipv6_local_delivery(skb, l3_off, l4_off, key.tunnel_id, ip6, nexthdr);
+	}
 }
 
 static inline int handle_ipv4(struct __sk_buff *skb)

bpf/lib/common.h
@@ -117,6 +117,8 @@ struct drop_notify {
 #define DROP_CSUM_L3		-153
 #define DROP_CSUM_L4		-154
 #define DROP_CT_CREATE_FAILED	-155
+#define DROP_INVALID_EXTHDR	-156
+#define DROP_FRAG_NOSUPPORT	-157
 
 enum {
 	CB_SRC_LABEL,

bpf/lib/conntrack.h
@@ -100,7 +100,7 @@ static inline void __inline__ ipv6_ct_tuple_reverse(struct ipv6_ct_tuple *tuple)
 
 /* Offset must point to IPv6 */
 static inline int __inline__ ct_lookup6(void *map, struct ipv6_ct_tuple *tuple,
-					struct __sk_buff *skb, int off, __u32 secctx, int in)
+					struct __sk_buff *skb, int l4_off, __u32 secctx, int in)
 {
 	int ret, action = ACTION_UNSPEC;
 
@@ -121,15 +121,12 @@ static inline int __inline__ ct_lookup6(void *map, struct ipv6_ct_tuple *tuple,
 	else
 		tuple->flags = TUPLE_F_IN;
 
-	/* FIXME: handle extension headers */
-	off += sizeof(struct ipv6hdr);
-
 	switch (tuple->nexthdr) {
 	case IPPROTO_ICMPV6:
 		if (1) {
 			__u8 type;
 
-			if (skb_load_bytes(skb, off, &type, 1) < 0)
+			if (skb_load_bytes(skb, l4_off, &type, 1) < 0)
 				return DROP_CT_INVALID_HDR;
 
 			tuple->sport = 0;
@@ -161,7 +158,7 @@ static inline int __inline__ ct_lookup6(void *map, struct ipv6_ct_tuple *tuple,
 		if (1) {
 			struct tcp_flags flags;
 
-			if (skb_load_bytes(skb, off + 12, &flags, 2) < 0)
+			if (skb_load_bytes(skb, l4_off + 12, &flags, 2) < 0)
 				return DROP_CT_INVALID_HDR;
 
 			if (unlikely(flags.syn && !flags.ack))
@@ -179,7 +176,7 @@ static inline int __inline__ ct_lookup6(void *map, struct ipv6_ct_tuple *tuple,
 
 	case IPPROTO_UDP:
 		/* load sport + dport into tuple */
-		if (skb_load_bytes(skb, off, &tuple->dport, 4) < 0)
+		if (skb_load_bytes(skb, l4_off, &tuple->dport, 4) < 0)
 			return DROP_CT_INVALID_HDR;
 
 		action = ACTION_CREATE;

bpf/lib/l3.h
@@ -48,18 +48,18 @@ static inline int do_encapsulation(struct __sk_buff *skb, __u32 node_id,
 }
 #endif /* ENCAP_IFINDEX */
 
-static inline int __inline__ ipv6_l3(struct __sk_buff *skb, int nh_off,
+static inline int __inline__ ipv6_l3(struct __sk_buff *skb, int l3_off,
 				     __u8 *smac, __u8 *dmac)
 {
 	int ret;
 
-	ret = ipv6_dec_hoplimit(skb, nh_off);
+	ret = ipv6_dec_hoplimit(skb, l3_off);
 	if (IS_ERR(ret))
 		return ret;
 
 	if (ret > 0) {
 		/* Hoplimit was reached */
-		return icmp6_send_time_exceeded(skb, nh_off);
+		return icmp6_send_time_exceeded(skb, l3_off);
 	}
 
 	if (smac && eth_store_saddr(skb, smac, 0) < 0)
@@ -121,8 +121,8 @@ static inline int __inline__ map_lxc_in(struct __sk_buff *skb, int l4_off,
 }
 #endif /* DISABLE_PORT_MAP */
 
-static inline int ipv6_local_delivery(struct __sk_buff *skb, int nh_off,
-				      __u32 seclabel, struct ipv6hdr *ip6)
+static inline int ipv6_local_delivery(struct __sk_buff *skb, int l3_off, int l4_off,
+				      __u32 seclabel, struct ipv6hdr *ip6, __u8 nexthdr)
 {
 	union v6addr *dst = (union v6addr *) &ip6->daddr;
 	__u32 lxc_id = derive_lxc_id(dst);
@@ -135,15 +135,14 @@ static inline int ipv6_local_delivery(struct __sk_buff *skb, int nh_off,
 	if (dst_lxc) {
 		mac_t lxc_mac = dst_lxc->mac;
 		mac_t router_mac = dst_lxc->node_mac;
-		__u8 nexthdr = ip6->nexthdr;
 
 		/* This will invalidate the size check */
-		ret = ipv6_l3(skb, nh_off, (__u8 *) &router_mac, (__u8 *) &lxc_mac);
+		ret = ipv6_l3(skb, l3_off, (__u8 *) &router_mac, (__u8 *) &lxc_mac);
 		if (ret != TC_ACT_OK)
 			return ret;
 
 #ifndef DISABLE_PORT_MAP
-		ret = map_lxc_in(skb, nh_off + sizeof(*ip6), dst_lxc, nexthdr);
+		ret = map_lxc_in(skb, l4_off, dst_lxc, nexthdr);
 		if (IS_ERR(ret))
 			return ret;
 #endif /* DISABLE_PORT_MAP */

common/bpf/drop.go
@@ -59,6 +59,8 @@ var errors = map[uint8]string{
 	153: "Error while correcting L3 checksum",
 	154: "Error while correcting L4 checksum",
 	155: "CT: Map insertion failed",
+	156: "Invalid IPv6 extension header",
+	157: "IPv6 fragmentation not supported",
 }
 
 func DropReason(reason uint8) string {

commit 48d564da7f247960d990463a9433bbb66aa5ecf5

bpf: fix ipv6 extension header parsing error
The ipv6_hdrlen function incorrectly sets the length of the extension header
during parsing, causing cilum to obtain the wrong next header and resulting
in packet loss.

This issue will affect the parsing of IPv6 packets that carry both the "auth"
and other extension headers, such as `ipv6/auth/hopbyhop/tcp`.

Fixes: 1ce3c7f6a4745bb6c420fb59106cfe0587ff18fa ("bpf: Skip over IPv6 extension headers")
Fixes: #24187
Signed-off-by: chenyuezhou <zcy.chenyue.zhou@gmail.com>

bpf/lib/ipv6.h
@@ -60,11 +60,12 @@ static __always_inline int ipv6_hdrlen_offset(struct __ctx_buff *ctx, __u8 *next
 			if (ctx_load_bytes(ctx, l3_off + len, &opthdr, sizeof(opthdr)) < 0)
 				return DROP_INVALID;
 
-			nh = opthdr.nexthdr;
 			if (nh == NEXTHDR_AUTH)
 				len += ipv6_authlen(&opthdr);
 			else
 				len += ipv6_optlen(&opthdr);
+
+			nh = opthdr.nexthdr;
 			break;
 
 		default:

bpf/tests/ipv6_test.c
@@ -3,8 +3,9 @@
 
 #include "common.h"
 
-#include <bpf/ctx/skb.h>
+#include <bpf/ctx/xdp.h>
 
+#include "pktgen.h"
 #include "node_config.h"
 
 #include "lib/common.h"
@@ -29,6 +30,178 @@ LPM_LOOKUP_FN(lpm4_lookup31, __be32, PREFIX31, dummy_map, match_dummy_prefix)
 LPM_LOOKUP_FN(lpm4_lookup22, __be32, PREFIX22, dummy_map, match_dummy_prefix)
 LPM_LOOKUP_FN(lpm4_lookup11, __be32, PREFIX11, dummy_map, match_dummy_prefix)
 LPM_LOOKUP_FN(lpm4_lookup0, __be32, PREFIX0, dummy_map, match_dummy_prefix)
+
+PKTGEN("xdp", "ipv6_without_extension_header")
+int ipv6_without_extension_header_pktgen(struct __ctx_buff *ctx)
+{
+	struct pktgen builder;
+	struct tcphdr *l4;
+	struct ethhdr *l2;
+	struct ipv6hdr *l3;
+
+	pktgen__init(&builder, ctx);
+
+	l2 = pktgen__push_ethhdr(&builder);
+	if (!l2)
+		return TEST_ERROR;
+
+	ethhdr__set_macs(l2, (__u8 *)mac_one, (__u8 *)mac_two);
+
+	l3 = pktgen__push_default_ipv6hdr(&builder);
+	if (!l3)
+		return TEST_ERROR;
+
+	memcpy(&l3->saddr, (__u8 *)v6_node_one, sizeof(l3->saddr));
+	memcpy(&l3->daddr, (__u8 *)v6_node_two, sizeof(l3->daddr));
+
+	l4 = pktgen__push_default_tcphdr(&builder);
+	if (!l4)
+		return TEST_ERROR;
+
+	l4->source = tcp_src_one;
+	l4->dest = tcp_svc_one;
+
+	pktgen__finish(&builder);
+
+	return 0;
+}
+
+SETUP("xdp", "ipv6_without_extension_header")
+int ipv6_without_extension_header_setup(__maybe_unused struct __ctx_buff *ctx)
+{
+	return 123;
+}
+
+CHECK("xdp", "ipv6_without_extension_header")
+int ipv6_without_extension_header_check(struct __ctx_buff *ctx)
+{
+	struct ethhdr *l2;
+	struct ipv6hdr *l3;
+	__u32 *status_code;
+	__u8 nexthdr;
+
+	test_init();
+
+	if (ctx_data(ctx) + sizeof(__u32) > ctx_data_end(ctx))
+		test_fatal("status code out of bounds");
+
+	status_code = ctx_data(ctx);
+	assert(*status_code == 123);
+
+	xdp_adjust_head(ctx, 4);
+	l2 = ctx_data(ctx);
+	if ((void *)(l2 + 1) > ctx_data_end(ctx))
+		test_fatal("l2 out of bounds");
+
+	assert(l2->h_proto == __bpf_htons(ETH_P_IPV6));
+
+	l3 = (void *)l2 + ETH_HLEN;
+	if ((void *)(l3 + 1) > ctx_data_end(ctx))
+		test_fatal("l3 out of bounds");
+
+	nexthdr = l3->nexthdr;
+	assert(ipv6_hdrlen(ctx, &nexthdr) > 0);
+	assert(nexthdr == IPPROTO_TCP);
+
+	test_finish();
+}
+
+struct ipv6_authhdr {
+	struct ipv6_opt_hdr opt;
+	__u16 reserved;
+	int spi;
+	int seq;
+	char icv[];
+};
+
+PKTGEN("xdp", "ipv6_with_auth_hop_tcp")
+int ipv6_with_hop_auth_tcp_pktgen(struct __ctx_buff *ctx)
+{
+	struct pktgen builder;
+	struct tcphdr *l4;
+	struct ethhdr *l2;
+	struct ipv6hdr *l3;
+	struct ipv6_authhdr *authhdr;
+	struct ipv6_opt_hdr *l3_next;
+
+	pktgen__init(&builder, ctx);
+
+	l2 = pktgen__push_ethhdr(&builder);
+	if (!l2)
+		return TEST_ERROR;
+
+	ethhdr__set_macs(l2, (__u8 *)mac_one, (__u8 *)mac_two);
+
+	l3 = pktgen__push_default_ipv6hdr(&builder);
+	if (!l3)
+		return TEST_ERROR;
+
+	memcpy(&l3->saddr, (__u8 *)v6_node_one, sizeof(l3->saddr));
+	memcpy(&l3->daddr, (__u8 *)v6_node_two, sizeof(l3->daddr));
+
+	l3_next = pktgen__append_ipv6_extension_header(&builder, NEXTHDR_AUTH);
+	if (!l3_next)
+		return TEST_ERROR;
+
+	authhdr = (struct ipv6_authhdr *)l3_next;
+	authhdr->spi = 0x222;
+	authhdr->seq = 1;
+
+	l3_next = pktgen__append_ipv6_extension_header(&builder, NEXTHDR_HOP);
+	if (!l3_next)
+		return TEST_ERROR;
+
+	l4 = pktgen__push_default_tcphdr(&builder);
+	if (!l4)
+		return TEST_ERROR;
+
+	l4->source = tcp_src_one;
+	l4->dest = tcp_svc_one;
+
+	pktgen__finish(&builder);
+
+	return 0;
+}
+
+SETUP("xdp", "ipv6_with_auth_hop_tcp")
+int ipv6_with_hop_auth_tcp_setup(__maybe_unused struct __ctx_buff *ctx)
+{
+	return 1234;
+}
+
+CHECK("xdp", "ipv6_with_auth_hop_tcp")
+int ipv6_with_hop_auth_tcp_check(struct __ctx_buff *ctx)
+{
+	struct ethhdr *l2;
+	struct ipv6hdr *l3;
+	__u32 *status_code;
+	__u8 nexthdr;
+
+	test_init();
+
+	if (ctx_data(ctx) + sizeof(__u32) > ctx_data_end(ctx))
+		test_fatal("status code out of bounds");
+
+	status_code = ctx_data(ctx);
+	assert(*status_code == 1234);
+
+	xdp_adjust_head(ctx, 4);
+	l2 = ctx_data(ctx);
+	if ((void *)(l2 + 1) > ctx_data_end(ctx))
+		test_fatal("l2 out of bounds");
+
+	assert(l2->h_proto == __bpf_htons(ETH_P_IPV6));
+
+	l3 = (void *)l2 + ETH_HLEN;
+	if ((void *)(l3 + 1) > ctx_data_end(ctx))
+		test_fatal("l3 out of bounds");
+
+	nexthdr = l3->nexthdr;
+	assert(ipv6_hdrlen(ctx, &nexthdr) > 0);
+	assert(nexthdr == IPPROTO_TCP);
+
+	test_finish();
+}
 
 CHECK("xdp", "ipv6")
 int bpf_test(__maybe_unused struct xdp_md *ctx)

bpf/tests/pktgen.h
@@ -67,6 +67,14 @@ static volatile const __u8 v6_pod_two[] = {0xfd, 0x04, 0, 0, 0, 0, 0, 0,
 static volatile const __u8 v6_pod_three[] = {0xfd, 0x04, 0, 0, 0, 0, 0, 0,
 					   0, 0, 0, 0, 0, 0, 0, 3};
 
+/* IPv6 addresses for nodes in the cluster */
+static volatile const __u8 v6_node_one[] = {0xfd, 0x05, 0, 0, 0, 0, 0, 0,
+					   0, 0, 0, 0, 0, 0, 0, 1};
+static volatile const __u8 v6_node_two[] = {0xfd, 0x06, 0, 0, 0, 0, 0, 0,
+					   0, 0, 0, 0, 0, 0, 0, 2};
+static volatile const __u8 v6_node_three[] = {0xfd, 0x07, 0, 0, 0, 0, 0, 0,
+					   0, 0, 0, 0, 0, 0, 0, 3};
+
 /* Source port to be used by a client */
 #define tcp_src_one	__bpf_htons(22334)
 #define tcp_src_two	__bpf_htons(33445)
@@ -77,6 +85,23 @@ static volatile const __u8 v6_pod_three[] = {0xfd, 0x04, 0, 0, 0, 0, 0, 0,
 #define tcp_svc_three	__bpf_htons(53)
 
 #define default_data "Should not change!!"
+
+#define NEXTHDR_HOP             0       /* Hop-by-hop option header. */
+#define NEXTHDR_TCP             6       /* TCP segment. */
+#define NEXTHDR_UDP             17      /* UDP message. */
+#define NEXTHDR_IPV6            41      /* IPv6 in IPv6 */
+#define NEXTHDR_ROUTING         43      /* Routing header. */
+#define NEXTHDR_FRAGMENT        44      /* Fragmentation/reassembly header. */
+#define NEXTHDR_GRE             47      /* GRE header. */
+#define NEXTHDR_ESP             50      /* Encapsulating security payload. */
+#define NEXTHDR_AUTH            51      /* Authentication header. */
+#define NEXTHDR_ICMP            58      /* ICMP for IPv6. */
+#define NEXTHDR_NONE            59      /* No next header */
+#define NEXTHDR_DEST            60      /* Destination options header. */
+#define NEXTHDR_SCTP            132     /* SCTP message. */
+#define NEXTHDR_MOBILITY        135     /* Mobility header. */
+
+#define NEXTHDR_MAX             255
 
 /* Define SCTP header here because this is all we need. */
 struct sctphdr {
@@ -97,7 +122,10 @@ enum pkt_layer {
 	PKT_LAYER_IPV4,
 	PKT_LAYER_IPV6,
 	PKT_LAYER_ARP,
-	/* TODO IPv6 extension headers */
+
+	/* IPv6 extension headers */
+	PKT_LAYER_IPV6_HOP_BY_HOP,
+	PKT_LAYER_IPV6_AUTH,
 
 	/* L4 layers */
 	PKT_LAYER_TCP,
@@ -109,6 +137,8 @@ enum pkt_layer {
 	/* Packet data*/
 	PKT_LAYER_DATA,
 };
+
+#define IPV6_DEFAULT_HOPLIMIT 64
 
 #define PKT_BUILDER_LAYERS 6
 
@@ -144,6 +174,29 @@ int pktgen__free_layer(const struct pktgen *builder)
 	}
 
 	return -1;
+}
+
+static __always_inline
+__attribute__((warn_unused_result))
+int ctx_data_valid(const struct __ctx_buff *ctx, __maybe_unused __u16 off)
+{
+	int ret = 0;
+
+	/* try to open up a range on the pkt reg */
+	asm volatile("r1 = *(u32 *)(%[ctx] +0)\n\t"
+		     "r2 = *(u32 *)(%[ctx] +4)\n\t"
+		     "%[off] &= %[offmax]\n\t"
+		     "r1 += %[off]\n\t"
+		     "if r1 > r2 goto +2\n\t"
+		     "%[ret] = 0\n\t"
+		     "goto +1\n\t"
+		     "%[ret] = %[errno]\n\t"
+		     : [ret]"=r"(ret)
+		     : [ctx]"r"(ctx), [off]"r"(off),
+		       [offmax]"i"(0xff), [errno]"i"(-EINVAL)
+		     : "r1", "r2");
+
+	return ret;
 }
 
 /* Push an empty ethernet header onto the packet */
@@ -291,16 +344,88 @@ struct iphdr *pktgen__push_default_iphdr(struct pktgen *builder)
 	return pktgen__push_default_iphdr_with_options(builder, 0);
 }
 
-/* Push an IPv6 header with sane defaults onto the packet */
-struct ipv6hdr *pktgen__push_default_ipv6hdr(struct pktgen *builder)
+static __always_inline
+__attribute__((warn_unused_result))
+void *pktgen__push_rawhdr(struct pktgen *builder, __u16 hdrsize, enum pkt_layer type)
 {
-	struct ipv6hdr *hdr = pktgen__push_ipv6hdr(builder);
+	struct __ctx_buff *ctx = builder->ctx;
+	void *layer = NULL;
+	int layer_idx;
+
+	ctx_adjust_troom(ctx, builder->cur_off + hdrsize - ctx_full_len(ctx));
+	if (ctx_data(ctx) + builder->cur_off + hdrsize > ctx_data_end(ctx))
+		return NULL;
+
+	if (builder->cur_off >= MAX_PACKET_OFF - hdrsize)
+		return NULL;
+
+	layer = ctx_data(ctx) + builder->cur_off;
+	layer_idx = pktgen__free_layer(builder);
+
+	if (layer_idx < 0)
+		return NULL;
+
+	builder->layers[layer_idx] = type;
+	builder->layer_offsets[layer_idx] = builder->cur_off;
+	builder->cur_off += hdrsize;
+
+	return layer;
+}
+
+static __always_inline
+__attribute__((warn_unused_result))
+struct ipv6_opt_hdr *pktgen__append_ipv6_extension_header(struct pktgen *builder,
+							  __maybe_unused __u8 nexthdr)
+{
+	struct ipv6_opt_hdr *hdr = NULL;
+	__u8 hdrlen = 0;
+	__u8 length = 0;
+	/* TODO improve */
+	switch (nexthdr) {
+	case NEXTHDR_HOP:
+		length = (0 + 1) << 3;
+		hdr = pktgen__push_rawhdr(builder, length, PKT_LAYER_IPV6_HOP_BY_HOP);
+		break;
+	case NEXTHDR_AUTH:
+		length = (2 + 2) << 2;
+		hdr = pktgen__push_rawhdr(builder, length, PKT_LAYER_IPV6_AUTH);
+		hdrlen = 2;
+		break;
+	default:
+		break;
+	}
 
 	if (!hdr)
-		return 0;
+		return NULL;
 
+	/* after multiple extension headers are added, LLVM tends to generate
+	 * code that verifier doesn't understand. try to open up a range on
+	 * the pkt reg
+	 */
+	if (ctx_data_valid(builder->ctx, builder->cur_off))
+		return NULL;
+
+	hdr->hdrlen = hdrlen;
+
+	return hdr;
+}
+
+static __always_inline
+__attribute__((warn_unused_result))
+struct ipv6hdr *pktgen__push_default_ipv6hdr(struct pktgen *builder)
+{
+	struct ipv6hdr *hdr = pktgen__push_rawhdr(builder,
+			sizeof(struct ipv6hdr), PKT_LAYER_IPV6);
+
+	if (!hdr)
+		return NULL;
+
+	if ((void *) hdr + sizeof(struct ipv6hdr) > ctx_data_end(builder->ctx))
+		return NULL;
+
+	memset(hdr, 0, sizeof(struct ipv6hdr));
 	hdr->version = 6;
-	hdr->hop_limit = 255;
+	hdr->hop_limit = IPV6_DEFAULT_HOPLIMIT;
 
 	return hdr;
 }
@@ -454,6 +579,7 @@ void pktgen__finish(const struct pktgen *builder)
 	struct ethhdr *eth_layer;
 	struct iphdr *ipv4_layer;
 	struct ipv6hdr *ipv6_layer;
+	struct ipv6_opt_hdr *ipv6_opt_layer;
 	struct tcphdr *tcp_layer;
 	__u16 layer_off;
 	__u16 v4len;
@@ -558,6 +684,12 @@ void pktgen__finish(const struct pktgen *builder)
 				return;
 
 			switch (builder->layers[i + 1]) {
+			case PKT_LAYER_IPV6_HOP_BY_HOP:
+				ipv6_layer->nexthdr = NEXTHDR_HOP;
+				break;
+			case PKT_LAYER_IPV6_AUTH:
+				ipv6_layer->nexthdr = NEXTHDR_AUTH;
+				break;
 			case PKT_LAYER_TCP:
 				ipv6_layer->nexthdr = IPPROTO_TCP;
 				break;
@@ -579,6 +711,44 @@ void pktgen__finish(const struct pktgen *builder)
 
 			/* Calculate payload length, which doesn't include the header size */
 			ipv6_layer->payload_len = __bpf_htons(v6len);
+
+			break;
+
+		case PKT_LAYER_IPV6_HOP_BY_HOP:
+		case PKT_LAYER_IPV6_AUTH:
+			layer_off = builder->layer_offsets[i];
+			if (layer_off >= MAX_PACKET_OFF - sizeof(struct ipv6_opt_hdr))
+				return;
+
+			ipv6_opt_layer = ctx_data(builder->ctx) + layer_off;
+			if ((void *)(ipv6_opt_layer + 1) > ctx_data_end(builder->ctx))
+				return;
+
+			if (i + 1 >= PKT_BUILDER_LAYERS)
+				return;
+
+			switch (builder->layers[i + 1]) {
+			case PKT_LAYER_IPV6_HOP_BY_HOP:
+				ipv6_opt_layer->nexthdr = NEXTHDR_HOP;
+				break;
+			case PKT_LAYER_IPV6_AUTH:
+				ipv6_opt_layer->nexthdr = NEXTHDR_AUTH;
+				break;
+			case PKT_LAYER_TCP:
+				ipv6_opt_layer->nexthdr = IPPROTO_TCP;
+				break;
+			case PKT_LAYER_UDP:
+				ipv6_opt_layer->nexthdr = IPPROTO_UDP;
+				break;
+			case PKT_LAYER_ICMPV6:
+				ipv6_opt_layer->nexthdr = IPPROTO_ICMPV6;
+				break;
+			case PKT_LAYER_SCTP:
+				ipv6_opt_layer->nexthdr = IPPROTO_SCTP;
+				break;
+			default:
+				break;
+			}
 
 			break;
 