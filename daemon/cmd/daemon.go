cilium/cilium

commit 291852afc80a353c7b6128e1f8a8f1202122819e

datapath/linux: make sure we have a local rule with proto kernel
Add a check on agent startup which ensures that a local lookup rule with
proto kernel exists and also cleans up an old local proto unspec rule in
case it exists and is before the new rule because rule deletion doesn't
take protocol into account, i.e. "rule del pref 100 proto kernel" will
delete a pref 100 proto unspec if it was added before the proto kernel
one. Check for the rule only if the proxy rule is also present,
otherwise init.sh won't have to move it.

Signed-off-by: Nikolay Aleksandrov <nikolay@isovalent.com>

daemon/cmd/daemon.go
@@ -285,6 +285,10 @@ func (d *Daemon) init() error {
 			return fmt.Errorf("failed while reinitializing datapath: %w", err)
 		}
 
+		if err := linuxdatapath.NodeEnsureLocalIPRule(); err != nil {
+			return fmt.Errorf("failed to ensure local IP rules: %w", err)
+		}
+
 		if option.Config.SockopsEnable {
 			eppolicymap.CreateEPPolicyMap()
 			if err := sockops.SockmapEnable(); err != nil {

pkg/datapath/linux/node.go
@@ -1883,3 +1883,90 @@ func NodeDeviceNameWithDefaultRoute() (string, error) {
 	}
 	return link.Attrs().Name, nil
 }
+
+func deleteOldLocalRule(rule route.Rule, family int) error {
+	var familyStr string
+
+	// sanity check, nothing to do if the rule is the same
+	if linux_defaults.RTProto == unix.RTPROT_UNSPEC {
+		return nil
+	}
+
+	if family == netlink.FAMILY_V4 {
+		familyStr = "IPv4"
+	} else {
+		familyStr = "IPv6"
+	}
+
+	localRules, err := route.ListRules(family, &rule)
+	if err != nil {
+		return fmt.Errorf("could not list local %s rules: %w", familyStr, err)
+	}
+
+	// we need to check for the old rule and make sure it's before the new one
+	oldPos := -1
+	found := false
+	for pos, rule := range localRules {
+		// mark the first unspec rule that matches
+		if oldPos == -1 && rule.Protocol == unix.RTPROT_UNSPEC {
+			oldPos = pos
+		}
+
+		if rule.Protocol == linux_defaults.RTProto {
+			// mark it as found only if it's before the new one
+			if oldPos != -1 {
+				found = true
+			}
+			break
+		}
+	}
+
+	if found == true {
+		err := route.DeleteRule(rule)
+		if err != nil {
+			return fmt.Errorf("could not delete old %s local rule: %w", familyStr, err)
+		}
+		log.WithFields(logrus.Fields{"family": familyStr}).Info("Deleting old local lookup rule")
+	}
+
+	return nil
+}
+
+// NodeEnsureLocalIPRule checks if Cilium local lookup rule (usually 100)
+// was installed and has proper protocol
+func NodeEnsureLocalIPRule() error {
+	// we have the Cilium local lookup rule only if the proxy rule is present
+	if !option.Config.InstallIptRules || !option.Config.EnableL7Proxy {
+		return nil
+	}
+
+	localRule := route.Rule{Priority: linux_defaults.RulePriorityLocalLookup, Table: unix.RT_TABLE_LOCAL, Mark: -1, Mask: -1, Protocol: linux_defaults.RTProto}
+	oldRule := localRule
+	oldRule.Protocol = unix.RTPROT_UNSPEC
+
+	if option.Config.EnableIPv4 {
+		err := route.ReplaceRule(localRule)
+		if err != nil {
+			return fmt.Errorf("could not replace IPv4 local rule: %w", err)
+		}
+
+		err = deleteOldLocalRule(oldRule, netlink.FAMILY_V4)
+		if err != nil {
+			return err
+		}
+	}
+
+	if option.Config.EnableIPv6 {
+		err := route.ReplaceRuleIPv6(localRule)
+		if err != nil {
+			return fmt.Errorf("could not replace IPv6 local rule: %w", err)
+		}
+
+		err = deleteOldLocalRule(oldRule, netlink.FAMILY_V6)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}

commit cc7eb3f9b1331f83e1ecc2257681f44f304cbe9e

daemon: ignore EEXIST on NodeEnsureLocalIPRule
According to Nik, some Kernel versions (namely 4.9 which is still
supported in v1.13) doesn't support the fib rule protocol attribute.
The call fails with the following error:

    daemon creation failed: error while initializing daemon: failed to ensure local IP rules: could not replace IPv4 local rule: file exists"

See https://github.com/cilium/cilium/pull/24607#issuecomment-1488485253

In order to ease backporting, let's also apply this change on master
even though 4.9 is no longer supported (ref. #22116). The EEXIST case
should not occur on kernels which support the fib rule protocol
attribute.

Fixes: 291852afc80a ("datapath/linux: make sure we have a local rule with proto kernel")
Suggested-by: Nikolay Aleksandrov <nikolay@isovalent.com>
Co-authored-by: Nikolay Aleksandrov <nikolay@isovalent.com>
Signed-off-by: Tobias Klauser <tobias@cilium.io>

daemon/cmd/daemon.go
@@ -19,6 +19,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
 	"golang.org/x/sync/semaphore"
+	"golang.org/x/sys/unix"
 
 	"github.com/cilium/cilium/api/v1/models"
 	health "github.com/cilium/cilium/cilium-health/launch"
@@ -285,7 +286,9 @@ func (d *Daemon) init() error {
 			return fmt.Errorf("failed while reinitializing datapath: %w", err)
 		}
 
-		if err := linuxdatapath.NodeEnsureLocalIPRule(); err != nil {
+		if err := linuxdatapath.NodeEnsureLocalIPRule(); errors.Is(err, unix.EEXIST) {
+			log.WithError(err).Warn("Failed to ensure local IP rules")
+		} else if err != nil {
 			return fmt.Errorf("failed to ensure local IP rules: %w", err)
 		}
 