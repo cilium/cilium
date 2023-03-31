24e7f405ee391146fb5253f48754886ea567ef23
André Martins <andre@cilium.io>
2023-03-31 15:49:48 +0200

    Revert "vendor: update vishvananda/netlink/"
    This reverts commit 0557242f8b5c106f3529f6142fa64d7301483592.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- vendor/github.com/vishvananda/netlink/nl/xfrm_linux.go
+++ vendor/github.com/vishvananda/netlink/nl/xfrm_linux.go
@@ -131,15 +131,7 @@ func (x *XfrmAddress) ToIP() net.IP {
 	return ip
 }
 
-// family is only used when x and prefixlen are both 0
-func (x *XfrmAddress) ToIPNet(prefixlen uint8, family uint16) *net.IPNet {
-	empty := [SizeofXfrmAddress]byte{}
-	if bytes.Equal(x[:], empty[:]) && prefixlen == 0 {
-		if family == FAMILY_V6 {
-			return &net.IPNet{IP: net.ParseIP("::"), Mask: net.CIDRMask(int(prefixlen), 128)}
-		}
-		return &net.IPNet{IP: net.ParseIP("0.0.0.0"), Mask: net.CIDRMask(int(prefixlen), 32)}
-	}
+func (x *XfrmAddress) ToIPNet(prefixlen uint8) *net.IPNet {
 	ip := x.ToIP()
 	if GetIPFamily(ip) == FAMILY_V4 {
 		return &net.IPNet{IP: ip, Mask: net.CIDRMask(int(prefixlen), 32)}
