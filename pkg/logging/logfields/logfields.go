ddd491bd8e100f94ca275c87e81f7e2be042c8db
Paul Chaignon <paul@cilium.io>
2023-04-01 18:59:56 +0200

    ipsec: Custom check for XFRM state existence
    UpsertIPsecEndpoint is currently unable to replace stale XFRM states. We
    use XfrmStateAdd, which fails with EEXIST if a state with the same key
    (IPs, SPI, and mark) already exists. We can't use XfrmStateUpdate because
    it fails with ESRCH is no state with the specified key exist.
    
    Note we don't have the same issue for XFRM policies because
    XfrmPolicyUpdate doesn't return ESRCH if no such policy already exists.
    No idea why the two APIs are not consistent.
    
    We therefore need to implement a proper 'update or insert' logic
    for XFRM states ourselves.
    
    To that end, we first check if the state we want to add already exists.
    If it doesn't, we attempt to add it. If it fails with EEXIST, we know
    that some other state is conflicting. In that case, we attempt to remove
    any conflicting XFRM states that are found and then attempt to add the
    new state again. To find conflicting XFRM states, we use the same logic
    as the kernel does (cf. __xfrm_state_lookup).
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- pkg/logging/logfields/logfields.go
+++ pkg/logging/logfields/logfields.go
@@ -630,6 +630,8 @@ const (
 	// SourceIP is a source IP
 	SourceIP = "sourceIP"
 
+	DestinationIP = "destinationIP"
+
 	// DestinationCIDR is a destination CIDR
 	DestinationCIDR = "destinationCIDR"
 
