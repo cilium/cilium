cilium/cilium

commit 4ba5d94415884a5042796b860a8bfcf7bc450775

contrib: Support vX.Y.Z-snapshot.W versions
Update remaining scripts that don't support `-snapshot` versions.

Previously the CRD schema generation step could crash out at determining
the CRD schema versions for a prerelease version. This crash would be
caused by attempting to find the CRD schema version on the branch
corresponding to the upcoming prerelease build, but that branch didn't
yet exist. Fix it by reverting to generating CRD schema only for
existing branches if this is a prerelease where a branch doesn't exist
yet.

Signed-off-by: Joe Stringer <joe@cilium.io>

contrib/release/start-release.sh
@@ -83,8 +83,8 @@ main() {
     git fetch -q $REMOTE
     if [ "$branch" = "master" ]; then
         git checkout -b pr/prepare-$version $REMOTE/$branch
-        if echo "$version" | grep -q 'rc'; then
-            old_version="$(git tag -l "$VERSION_GLOB" | grep -v 'snapshot' | sort -V | tail -n 1)"
+        if version_is_prerelease "$version"; then
+            old_version="$(git tag -l "$VERSION_GLOB" | grep -v 'rc\|snapshot' | sort -V | tail -n 1)"
         else
             old_version="$(git tag -l "$VERSION_GLOB" | sort -V | tail -n 1)"
         fi
@@ -107,6 +107,9 @@ main() {
     fi
 
     target_branch=$(echo "$version" | sed "$BRANCH_REGEX")
+    if ! git ls-remote --exit-code --heads $REMOTE $target_branch; then
+        target_branch=$(echo "$old_version" | sed "$BRANCH_REGEX")
+    fi
     $DIR/../../Documentation/check-crd-compat-table.sh "$target_branch" --update
     if [ "${old_branch}" != "" ]; then
       $DIR/prep-changelog.sh "$old_version" "$version" "$old_branch"

contrib/release/prep-changelog.sh
@@ -33,12 +33,12 @@ handle_args() {
 
     if ! echo "$1" | grep -q "$RELEASE_REGEX"; then
         usage 2>&1
-        common::exit 1 "Invalid OLD-VERSION ARG \"$1\"; Expected X.Y.Z[-rcW]"
+        common::exit 1 "Invalid OLD-VERSION ARG \"$1\"; Expected X.Y.Z[-rc.W|-snapshot.W]"
     fi
 
     if ! echo "$2" | grep -q "$RELEASE_REGEX"; then
         usage 2>&1
-        common::exit 1 "Invalid NEW-VERSION ARG \"$2\"; Expected X.Y.Z[-rcW]"
+        common::exit 1 "Invalid NEW-VERSION ARG \"$2\"; Expected X.Y.Z[-rc.W|-snapshot.W]"
     fi
 
     if [ "$#" -eq 3 ] && ! echo "$3" | grep -q "[0-9]\+\.[0-9]\+"; then

commit e82d97805d9a759fde5e24e97d75bbb9c9930855

contrib: detect pre-release version correctly
The script was not detecting the pre-release of a version correctly.
This commit fixes it by negating the return value of
'version_is_prerelease'

Fixes: 4ba5d9441588 ("contrib: Support vX.Y.Z-snapshot.W versions")
Signed-off-by: André Martins <andre@cilium.io>

contrib/release/start-release.sh
@@ -83,7 +83,7 @@ main() {
     git fetch -q $REMOTE
     if [ "$branch" = "master" ]; then
         git checkout -b pr/prepare-$version $REMOTE/$branch
-        if version_is_prerelease "$version"; then
+        if ! version_is_prerelease "$version"; then
             old_version="$(git tag -l "$VERSION_GLOB" | grep -v 'rc\|snapshot' | sort -V | tail -n 1)"
         else
             old_version="$(git tag -l "$VERSION_GLOB" | sort -V | tail -n 1)"