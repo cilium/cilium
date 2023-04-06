a3a2df4ee30e85fc00f0a4bdd88b72b2f8895623
Chris Tarazi <chris@isovalent.com>
2023-04-05 17:17:31 -0700

    connectivity: Implement FromCIDR tests
    [ cherry-picked from cilium/cilium-cli repository ]
    
    These should cover the tests from the Cilium (github.com/cilium/cilium)
    Ginkgo suite for FromCIDRs, see [1].
    
    [1]:
    https://github.com/cilium/cilium/blob/8f9daa8c4e9ccf17c4badaa28aa6c1b0b51924a8/test/k8s/net_policies.go#L488-L557.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

---
+++ cilium-cli/connectivity/tests/from-cidr.go
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package tests
+
+import (
+	"context"
+	"net"
+	"strconv"
+
+	"github.com/cilium/cilium-cli/connectivity/check"
+)
+
+// FromCIDRToPod generates HTTP request from each node without Cilium to the
+// echo pods within the Cilium / K8s cluster.
+func FromCIDRToPod() check.Scenario {
+	return &fromCIDRToPod{}
+}
+
+// fromCIDRToPod implements a Scenario.
+type fromCIDRToPod struct{}
+
+func (f *fromCIDRToPod) Name() string {
+	return "from-cidr-to-pod"
+}
+
+func (f *fromCIDRToPod) Run(ctx context.Context, t *check.Test) {
+	clientPod := t.Context().HostNetNSPodsByNode()[t.NodesWithoutCilium()[0]]
+	i := 0
+
+	for _, pod := range t.Context().EchoPods() {
+		ep := check.HTTPEndpoint(
+			"http-endpoint",
+			// scheme://[ip:port]/path
+			pod.Scheme()+"://"+net.JoinHostPort(pod.Address(check.IPFamilyAny), strconv.FormatUint(uint64(pod.Port()), 10))+pod.Path(),
+		)
+
+		t.NewAction(f, "host-netns-to-pod", &clientPod, pod, check.IPFamilyAny).Run(func(a *check.Action) {
+			a.ExecInPod(ctx, t.Context().CurlCommand(ep, check.IPFamilyAny))
+		})
+		i++
+	}
+}
