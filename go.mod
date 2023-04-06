5ec835720212a35da16f5aea70e11192945c323c
Tam Mach <tam.mach@cilium.io>
2023-04-06 14:23:52 +1000

    vendor: Update cilium/proxy version to latest
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- go.mod
+++ go.mod
@@ -24,7 +24,7 @@ require (
 	github.com/cilium/ipam v0.0.0-20220824141044-46ef3d556735
 	github.com/cilium/kafka v0.0.0-20180809090225-01ce283b732b
 	github.com/cilium/lumberjack/v2 v2.3.0
-	github.com/cilium/proxy v0.0.0-20230224085913-505865fff55a
+	github.com/cilium/proxy v0.0.0-20230406035048-5726d27ec0bf
 	github.com/cilium/workerpool v1.2.0
 	github.com/containernetworking/cni v1.1.2
 	github.com/containernetworking/plugins v1.2.0
--- vendor/github.com/cilium/proxy/go/envoy/extensions/filters/http/ext_authz/v3/ext_authz.pb.validate.go
+++ vendor/github.com/cilium/proxy/go/envoy/extensions/filters/http/ext_authz/v3/ext_authz.pb.validate.go
@@ -227,6 +227,35 @@ func (m *ExtAuthz) validate(all bool) error {
 
 	// no validation rules for BootstrapMetadataLabelsKey
 
+	if all {
+		switch v := interface{}(m.GetAllowedHeaders()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ExtAuthzValidationError{
+					field:  "AllowedHeaders",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ExtAuthzValidationError{
+					field:  "AllowedHeaders",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetAllowedHeaders()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ExtAuthzValidationError{
+				field:  "AllowedHeaders",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	switch m.Services.(type) {
 
 	case *ExtAuthz_GrpcService:
