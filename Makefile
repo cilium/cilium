cilium/cilium

commit 2975c75613a176167516edd42b0041e75b97bf26

contrib/kind: no longer create local docker registry
Now that we use `kind load docker-image`, it's not necessary to push
images to a local registry as well.

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

Makefile
@@ -513,8 +513,8 @@ kind-install-cilium-clustermesh: kind-clustermesh-ready ## Install a local Ciliu
 		--helm-values=$(ROOT_DIR)/contrib/testing/kind-clustermesh2.yaml \
 		--version=
 	@echo "  Enabling clustermesh"
-	cilium clustermesh enable --context kind-clustermesh1 --service-type NodePort --apiserver-image localhost:5000/cilium/clustermesh-apiserver:local
-	cilium clustermesh enable --context kind-clustermesh2 --service-type NodePort --apiserver-image localhost:5000/cilium/clustermesh-apiserver:local
+	cilium clustermesh enable --context kind-clustermesh1 --service-type NodePort --apiserver-image $(LOCAL_CLUSTERMESH_IMAGE)
+	cilium clustermesh enable --context kind-clustermesh2 --service-type NodePort --apiserver-image $(LOCAL_CLUSTERMESH_IMAGE)
 	cilium clustermesh status --context kind-clustermesh1 --wait
 	cilium clustermesh status --context kind-clustermesh2 --wait
 	cilium clustermesh connect --context kind-clustermesh1 --destination-context kind-clustermesh2
@@ -531,8 +531,6 @@ kind-ready:
 $(eval $(call KIND_ENV,kind-build-image-agent))
 kind-build-image-agent: ## Build cilium-dev docker image
 	$(QUIET)$(MAKE) dev-docker-image$(DEBUGGER_SUFFIX) DOCKER_IMAGE_TAG=$(LOCAL_IMAGE_TAG)
-	@echo "  DEPLOY image to kind ($(LOCAL_AGENT_IMAGE))"
-	$(QUIET)$(CONTAINER_ENGINE) push $(LOCAL_AGENT_IMAGE)
 
 $(eval $(call KIND_ENV,kind-image-agent))
 kind-image-agent: kind-ready kind-build-image-agent ## Build cilium-dev docker image and import it into kind.
@@ -541,8 +539,6 @@ kind-image-agent: kind-ready kind-build-image-agent ## Build cilium-dev docker i
 $(eval $(call KIND_ENV,kind-build-image-operator))
 kind-build-image-operator: ## Build cilium-operator-dev docker image
 	$(QUIET)$(MAKE) dev-docker-operator-generic-image$(DEBUGGER_SUFFIX) DOCKER_IMAGE_TAG=$(LOCAL_IMAGE_TAG)
-	@echo "  DEPLOY image to kind ($(LOCAL_OPERATOR_IMAGE))"
-	$(QUIET)$(CONTAINER_ENGINE) push $(LOCAL_OPERATOR_IMAGE)
 
 $(eval $(call KIND_ENV,kind-image-operator))
 kind-image-operator: kind-ready kind-build-image-operator ## Build cilium-operator-dev docker image and import it into kind.
@@ -551,8 +547,6 @@ kind-image-operator: kind-ready kind-build-image-operator ## Build cilium-operat
 $(eval $(call KIND_ENV,kind-build-clustermesh-apiserver))
 kind-build-clustermesh-apiserver: ## Build cilium-clustermesh-apiserver docker image
 	$(QUIET)$(MAKE) docker-clustermesh-apiserver-image DOCKER_IMAGE_TAG=$(LOCAL_IMAGE_TAG)
-	@echo "  DEPLOY image to kind ($(LOCAL_CLUSTERMESH_IMAGE))"
-	$(QUIET)$(CONTAINER_ENGINE) push $(LOCAL_CLUSTERMESH_IMAGE)
 
 .PHONY: kind-image
 kind-image: ## Build cilium and operator images and import them into kind.
@@ -602,7 +596,7 @@ kind-debug-agent: ## Create a local kind development environment with cilium-age
 		|| $(MAKE) kind
 	$(MAKE) kind-image-agent-debug
 	# Not debugging cilium-operator here; any image is good enough.
-	$(CONTAINER_ENGINE) push $(LOCAL_OPERATOR_IMAGE) \
+	kind load docker-image $(LOCAL_OPERATOR_IMAGE) \
 		|| $(MAKE) kind-image-operator
 	$(MAKE) kind-check-cilium 2>/dev/null \
 		|| $(MAKE) kind-install-cilium

contrib/scripts/kind-down.sh
@@ -9,10 +9,5 @@ if ! have_kind; then
     echo "  https://kind.sigs.k8s.io/docs/user/quick-start/#installation"
 fi
 
-if [ "${1:-}" != "--keep-registry" ]; then
-    docker kill kind-registry && \
-        docker rm kind-registry
-fi
-
 kind delete clusters kind && \
     docker network rm kind-cilium

contrib/scripts/kind.sh
@@ -64,27 +64,6 @@ if [[ "${controlplanes}" == "-h" || "${controlplanes}" == "--help" ]]; then
   exit 0
 fi
 
-# Registry will be localhost:5000
-reg_name="kind-registry"
-reg_port="5000"
-running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
-if [[ "${running}" != "true" ]]; then
-  retry_count=0
-  while ! docker pull registry:2
-  do
-    retry_count=$((retry_count+1))
-    if [[ "$retry_count" -ge 10 ]]; then
-      echo "ERROR: 'docker pull registry:2' failed $retry_count times. Please make sure docker is running"
-      exit 1
-    fi
-    echo "docker pull registry:2 failed. Sleeping for 1 second and trying again..."
-    sleep 1
-  done
-  docker run \
-    -d --restart=always -p "${reg_port}:5000" --name "${reg_name}" \
-    registry:2
-fi
-
 kind_cmd="kind create cluster"
 
 if [[ -n "${cluster_name}" ]]; then
@@ -160,10 +139,6 @@ kubeadmConfigPatches:
     apiServer:
       extraArgs:
         "v": "3"
-containerdConfigPatches:
-- |-
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
-    endpoint = ["http://${reg_name}:${reg_port}"]
 EOF
 
 if [ "${xdp}" = true ]; then
@@ -186,14 +161,7 @@ if [ "${xdp}" = true ]; then
   done
 fi
 
-docker network connect "${default_network}" "${reg_name}" || true
-
-for node in $(kubectl get nodes --no-headers -o custom-columns=:.metadata.name); do
-  kubectl annotate node "${node}" "kind.x-k8s.io/registry=localhost:${reg_port}";
-done
-
 set +e
-kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 set -e
 

contrib/testing/kind-clustermesh1.yaml
@@ -5,9 +5,11 @@ debug:
   enabled: true
 image:
   override: "localhost:5000/cilium/cilium-dev:local"
+  pullPolicy: Never
 operator:
   image:
     override: "localhost:5000/cilium/operator-generic:local"
+    pullPolicy: Never
     suffix: ""
 ipam:
   mode: kubernetes

contrib/testing/kind-clustermesh2.yaml
@@ -5,9 +5,11 @@ debug:
   enabled: true
 image:
   override: "localhost:5000/cilium/cilium-dev:local"
+  pullPolicy: Never
 operator:
   image:
     override: "localhost:5000/cilium/operator-generic:local"
+    pullPolicy: Never
     suffix: ""
 ipam:
   mode: kubernetes

contrib/testing/kind-values.yaml
@@ -2,9 +2,11 @@ debug:
   enabled: true
 image:
   override: "localhost:5000/cilium/cilium-dev:local"
+  pullPolicy: Never
 operator:
   image:
     override: "localhost:5000/cilium/operator-generic:local"
+    pullPolicy: Never
     suffix: ""
   nodeSelector:
     kubernetes.io/os: linux

commit 9b19cb44713f9b8355fb144917eae786978f661d

contrib/kind: fix uninitialized variable in install-clustermesh target
2975c75613a1 dropped the usage of a local docker registry and, among
others, changed the `kind-install-cilium-clustermesh` target to use a
parametrized name for the clustermesh-apiserver image. Yet, that
variable was not initialized, causing the target to fail. This commit
fixes it, calling the initialization logic.

Fixes: 2975c75613a1 ("contrib/kind: no longer create local docker registry")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

Makefile
@@ -458,7 +458,7 @@ kind-clustermesh-images: kind-clustermesh-ready kind-build-clustermesh-apiserver
 	$(QUIET)kind load docker-image $(LOCAL_OPERATOR_IMAGE) --name clustermesh1
 	$(QUIET)kind load docker-image $(LOCAL_OPERATOR_IMAGE) --name clustermesh2
 
-.PHONY: kind-install-cilium-clustermesh
+$(eval $(call KIND_ENV,kind-install-cilium-clustermesh))
 kind-install-cilium-clustermesh: kind-clustermesh-ready ## Install a local Cilium version into the clustermesh clusters and enable clustermesh.
 	@echo "  INSTALL cilium on clustermesh1 cluster"
 	kubectl config use kind-clustermesh1