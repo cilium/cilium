5e4f2e23c42024f91c67eaa148432f1aec504c0a
Andrew Sauber <2046750+asauber@users.noreply.github.com>
2023-04-14 17:12:00 -0400

    Expose helm reset/reuse values as flags for upgrade
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: Andrew Sauber <andrew.sauber@isovalent.com>

--- cilium-cli/internal/cli/cmd/install.go
+++ cilium-cli/internal/cli/cmd/install.go
@@ -364,6 +364,10 @@ cilium upgrade --helm-set cluster.id=1 --helm-set cluster.name=cluster1
 
 	addCommonInstallFlags(cmd, &params)
 	addCommonHelmFlags(cmd, &params)
+	cmd.Flags().BoolVar(&params.HelmResetValues, "reset-values", false,
+		"When upgrading, reset the helm values to the ones built into the chart")
+	cmd.Flags().BoolVar(&params.HelmReuseValues, "reuse-values", true,
+		"When upgrading, reuse the helm values from the latest release unless any overrides from are set from other flags. This option takes precedence over HelmResetValues")
 	cmd.Flags().BoolVar(&params.DryRun, "dry-run", false,
 		"Write resources to be installed to stdout without actually installing them")
 	cmd.Flags().BoolVar(&params.DryRunHelmValues, "dry-run-helm-values", false,
