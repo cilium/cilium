a225c8a1aa49a4e35cb418d9470f7bca1a272384
Leonard Cohnen <lc@edgeless.systems>
2023-04-14 15:48:49 +0200

    bpf/wireguard: always lookup src identity
    When WireGuard node encryption is enabled, src is never set. Therefore,
    the unguarded check in line 97 always evaluates to true and no traffic
    is encrypted.
    
    This commit fixes this bug by always looking up the src identity,
    independent of if WireGuard node encryption is enabled or not.
    
    Fixes: 17419c9 ("bpf/wireguard: Skip encryption for cluster-external traffic")
    Signed-off-by: Leonard Cohnen <lc@edgeless.systems>

--- bpf/lib/wireguard.h
+++ bpf/lib/wireguard.h
@@ -48,9 +48,7 @@ wg_maybe_redirect_to_encrypt(struct __ctx_buff *ctx)
 		}
 #endif
 		dst = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr, 0);
-#ifndef ENABLE_NODE_ENCRYPTION
 		src = lookup_ip6_remote_endpoint((union v6addr *)&ip6->saddr, 0);
-#endif /* ENABLE_NODE_ENCRYPTION */
 		break;
 #endif
 #ifdef ENABLE_IPV4
@@ -58,9 +56,7 @@ wg_maybe_redirect_to_encrypt(struct __ctx_buff *ctx)
 		if (!revalidate_data(ctx, &data, &data_end, &ip4))
 			return DROP_INVALID;
 		dst = lookup_ip4_remote_endpoint(ip4->daddr, 0);
-#ifndef ENABLE_NODE_ENCRYPTION
 		src = lookup_ip4_remote_endpoint(ip4->saddr, 0);
-#endif /* ENABLE_NODE_ENCRYPTION */
 		break;
 #endif
 	default:
