925e26ac3ac7c3bfa4fbd627bb087f1aa63c5717
Timo Beckers <timo@isovalent.com>
2023-04-17 15:31:46 +0200

    testutils: add test helpers for marking integration tests
    Following the pattern of privileged tests, support using the INTEGRATION_TESTS
    environment variable for enabling integration tests instead of build tags.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/testutils/privileged.go
+++ pkg/testutils/privileged.go
@@ -13,6 +13,7 @@ import (
 
 const (
 	privilegedEnv            = "PRIVILEGED_TESTS"
+	integrationEnv           = "INTEGRATION_TESTS"
 	gatewayAPIConformanceEnv = "GATEWAY_API_CONFORMANCE_TESTS"
 )
 
@@ -27,6 +28,30 @@ func PrivilegedTest(tb testing.TB) {
 func PrivilegedCheck(c *check.C) {
 	if os.Getenv(privilegedEnv) == "" {
 		c.Skip(fmt.Sprintf("Set %s to run this test", privilegedEnv))
+	}
+}
+
+// IntegrationTests returns true if integration tests are requested.
+func IntegrationTests() bool {
+	if os.Getenv(integrationEnv) != "" {
+		return true
+	}
+	return false
+}
+
+// IntegrationTest only executes tb if integration tests are requested.
+func IntegrationTest(tb testing.TB) {
+	tb.Helper()
+
+	if os.Getenv(integrationEnv) == "" {
+		tb.Skip(fmt.Sprintf("Set %s to run this test", integrationEnv))
+	}
+}
+
+// IntegrationCheck only executes c if integration tests are requested.
+func IntegrationCheck(c *check.C) {
+	if os.Getenv(integrationEnv) == "" {
+		c.Skip(fmt.Sprintf("Set %s to run this test", integrationEnv))
 	}
 }
 
