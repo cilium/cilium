0e16a770bf8e2ddd51f17c6842d9f970e5900948
Paul Chaignon <paul@cilium.io>
2023-04-18 14:38:28 +0200

    vagrant: Default to 4.19
    Given we removed the 4.9 VM image in 02041e7714 ("vagrant: remove 4.9
    vagrant box") and dropped support in Cilium v1.13, we should also make
    4.19 the default for the development VM.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- vagrant_box_defaults.rb
+++ vagrant_box_defaults.rb
@@ -2,8 +2,8 @@
 # vi: set ft=ruby
 Vagrant.require_version ">= 2.2.0"
 
-$SERVER_BOX = "cilium/ubuntu-dev"
-$SERVER_VERSION= "234"
+$SERVER_BOX = "cilium/ubuntu-4-19"
+$SERVER_VERSION= "70"
 $NETNEXT_SERVER_BOX= "cilium/ubuntu-next"
 $NETNEXT_SERVER_VERSION= "161"
 @v54_SERVER_BOX= "cilium/ubuntu-5-4"
