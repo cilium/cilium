2f9c29bbb6caacc47a045e5cc1400ad5311fea4d
Andrew Sauber <2046750+asauber@users.noreply.github.com>
2023-04-18 10:39:40 -0400

    Default Helm Upgrade to not reuse values
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Previously, we were defaulting Helm's reuseValues flag to true. This
    ended up not giving us the expected behavior when only our --version
    flag was passed to an upgrade command (the most common case
    for this flag). The result was that prior image versions from the
    current release would override that version. By defaulting this flag to
    false, we get the expect behavior in the common case.
    
    Signed-off-by: Andrew Sauber <andrew.sauber@isovalent.com>

--- cilium-cli/internal/cli/cmd/install.go
+++ cilium-cli/internal/cli/cmd/install.go
@@ -366,7 +366,7 @@ cilium upgrade --helm-set cluster.id=1 --helm-set cluster.name=cluster1
 	addCommonHelmFlags(cmd, &params)
 	cmd.Flags().BoolVar(&params.HelmResetValues, "reset-values", false,
 		"When upgrading, reset the helm values to the ones built into the chart")
-	cmd.Flags().BoolVar(&params.HelmReuseValues, "reuse-values", true,
+	cmd.Flags().BoolVar(&params.HelmReuseValues, "reuse-values", false,
 		"When upgrading, reuse the helm values from the latest release unless any overrides from are set from other flags. This option takes precedence over HelmResetValues")
 	cmd.Flags().BoolVar(&params.DryRun, "dry-run", false,
 		"Write resources to be installed to stdout without actually installing them")
