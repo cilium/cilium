98ddd8497a3f0ba765a62a86635862b1ce8a358f
Joe Stringer <joe@cilium.io>
2023-04-18 19:14:58 -0700

    daemon: Add Cells for Cilium API specifications
    This preparatory commit introduces a new Cell for each Swagger API
    Specification in order to allow reuse earlier on during the agent
    initialization / lifecycle. No functional changes intended.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -18,7 +18,6 @@ import (
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/asm"
 	"github.com/cilium/ebpf/rlimit"
-	"github.com/go-openapi/loads"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
@@ -1592,6 +1591,7 @@ type daemonParams struct {
 	IPCache              *ipcache.IPCache
 	EgressGatewayManager *egressgateway.Manager
 	CNIConfigManager     cni.CNIConfigManager
+	SwaggerSpec          *server.Spec
 }
 
 func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
@@ -1805,7 +1805,7 @@ func runDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *daem
 
 	// Start up the local api socket
 	bootstrapStats.initAPI.Start()
-	srv := server.NewServer(d.instantiateAPI())
+	srv := server.NewServer(d.instantiateAPI(params.SwaggerSpec))
 	srv.EnabledListeners = []string{"unix"}
 	srv.SocketPath = option.Config.SocketPath
 	srv.ReadTimeout = apiTimeout
@@ -1860,14 +1860,9 @@ func runDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *daem
 	}
 }
 
-func (d *Daemon) instantiateAPI() *restapi.CiliumAPIAPI {
-	swaggerSpec, err := loads.Analyzed(server.SwaggerJSON, "")
-	if err != nil {
-		log.WithError(err).Fatal("Cannot load swagger spec")
-	}
-
+func (d *Daemon) instantiateAPI(swaggerSpec *server.Spec) *restapi.CiliumAPIAPI {
 	log.Info("Initializing Cilium API")
-	restAPI := restapi.NewCiliumAPIAPI(swaggerSpec)
+	restAPI := restapi.NewCiliumAPIAPI(swaggerSpec.Document)
 
 	restAPI.Logger = log.Infof
 
