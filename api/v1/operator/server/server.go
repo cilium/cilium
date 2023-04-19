016ca947278cbe2def34a0f4b4df690f627cc126
Joe Stringer <joe@cilium.io>
2023-04-19 12:51:32 -0700

    api: Add flags to configure API access from spec
    Use the autogenerated API specifications in order to populate "allowed
    APIs" flags in the api specification objects for each API. This code
    will be hooked in for usage in subsequent commits.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- api/v1/operator/server/server.go
+++ api/v1/operator/server/server.go
@@ -22,6 +22,7 @@ import (
 	"github.com/go-openapi/loads"
 	"github.com/go-openapi/swag"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	"golang.org/x/net/netutil"
 
 	"github.com/cilium/cilium/api/v1/operator/server/restapi"
@@ -113,25 +114,52 @@ var (
 	tlsCACertificate  string
 )
 
+type ServerConfig struct {
+	EnableCiliumOperatorServerAccess []string
+}
+
+var (
+	defaultServerConfig = ServerConfig{
+		EnableCiliumOperatorServerAccess: []string{"*"},
+	}
+	AdminEnableFlag = "enable-cilium-operator-server-access"
+)
+
+func (cfg ServerConfig) Flags(flags *pflag.FlagSet) {
+	flags.StringSlice(AdminEnableFlag, cfg.EnableCiliumOperatorServerAccess,
+		"List of cilium operator APIs which are administratively enabled. Supports '*'.")
+}
+
 var SpecCell = cell.Module(
 	"cilium-operator-spec",
 	"cilium operator Specification",
 
+	cell.Config(defaultServerConfig),
 	cell.Provide(newSpec),
 )
 
 type Spec struct {
 	*loads.Document
+
+	// DeniedAPIs is a set of APIs that are administratively disabled.
+	DeniedAPIs api.PathSet
 }
 
-func newSpec() (*Spec, error) {
+func newSpec(cfg ServerConfig) (*Spec, error) {
 	swaggerSpec, err := loads.Analyzed(SwaggerJSON, "")
 	if err != nil {
 		return nil, fmt.Errorf("failed to load swagger spec: %w", err)
 	}
 
+	deniedAPIs, err := api.AllowedFlagsToDeniedPaths(swaggerSpec, cfg.EnableCiliumOperatorServerAccess)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse %q flag: %w",
+			AdminEnableFlag, err)
+	}
+
 	return &Spec{
-		Document: swaggerSpec,
+		Document:   swaggerSpec,
+		DeniedAPIs: deniedAPIs,
 	}, nil
 }
 
