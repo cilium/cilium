2a03998cb1a59ed128438293f81547a3489661ac
Maxim Mikityanskiy <maxim@isovalent.com>
2023-04-20 20:57:49 +0300

    bpf: Rename host_local to needs_ct in struct nat_entry
    [ upstream commit 83d2621f8082005a0260f67c5ac5e7b192d8d6cc ]
    
    This flag will be reused for more cases that need connection tracking.
    Currently it's only used for host-local connections, see commit
    0e785f96756b ("bpf: initial bpf-based masquerading support") for
    details.
    
    Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

--- bpf/lib/nat.h
+++ bpf/lib/nat.h
@@ -29,7 +29,7 @@ enum  nat_dir {
 
 struct nat_entry {
 	__u64 created;
-	__u64 host_local;	/* Only single bit used. */
+	__u64 needs_ct;		/* Only single bit used. */
 	__u64 pad1;		/* Future use. */
 	__u64 pad2;		/* Future use. */
 };
@@ -236,8 +236,9 @@ static __always_inline int snat_v4_new_mapping(struct __ctx_buff *ctx,
 	rtuple.daddr = target->addr;
 
 	if (otuple->saddr == target->addr) {
-		ostate->common.host_local = 1;
-		rstate.common.host_local = ostate->common.host_local;
+		/* Host-local connection. */
+		ostate->common.needs_ct = 1;
+		rstate.common.needs_ct = ostate->common.needs_ct;
 	}
 
 #pragma unroll
@@ -277,7 +278,7 @@ static __always_inline int snat_v4_track_connection(struct __ctx_buff *ctx,
 	enum ct_dir where;
 	int ret;
 
-	if (state && state->common.host_local) {
+	if (state && state->common.needs_ct) {
 		needs_ct = true;
 #if defined(ENABLE_EGRESS_GATEWAY)
 	/* Track egress gateway connections, but only if they are related to a
@@ -1016,8 +1017,9 @@ static __always_inline int snat_v6_new_mapping(struct __ctx_buff *ctx,
 	rtuple.daddr = target->addr;
 
 	if (!ipv6_addrcmp(&otuple->saddr, &rtuple.daddr)) {
-		ostate->common.host_local = 1;
-		rstate.common.host_local = ostate->common.host_local;
+		/* Host-local connection. */
+		ostate->common.needs_ct = 1;
+		rstate.common.needs_ct = ostate->common.needs_ct;
 	}
 
 #pragma unroll
@@ -1057,7 +1059,7 @@ static __always_inline int snat_v6_track_connection(struct __ctx_buff *ctx,
 	enum ct_dir where;
 	int ret;
 
-	if (state && state->common.host_local) {
+	if (state && state->common.needs_ct) {
 		needs_ct = true;
 	} else if (!state && dir == NAT_DIR_EGRESS) {
 		if (!ipv6_addrcmp(&tuple->saddr, (void *)&target->addr))
