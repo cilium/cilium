37a685a3582b61b65ac07d19a7fa58c9c09ce349
Joe Stringer <joe@cilium.io>
2023-04-26 14:31:22 -0700

    daemon: Remove IP prefix length tracking
    🧹🧹🧹
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- daemon/cmd/policy_test.go
+++ daemon/cmd/policy_test.go
@@ -735,25 +735,6 @@ func (ds *DaemonSuite) TestReplacePolicy(c *C) {
 	ds.d.policy.Mutex.RLock()
 	c.Assert(len(ds.d.policy.SearchRLocked(lbls)), Equals, 2)
 	ds.d.policy.Mutex.RUnlock()
-
-	// Updating of prefix lengths may complete *after* PolicyAdd returns. Add a
-	// wait for prefix lengths to be correct after timeout to ensure that we
-	// do not assert before the releasing of CIDRs has been performed.
-	testutils.WaitUntil(func() bool {
-		_, s4 := ds.d.prefixLengths.ToBPFData()
-		sort.Ints(s4)
-		if len(s4) != 2 {
-			c.Logf("IPv4 Prefix lengths incorrect (expected [0, 32]). This may be because CIDRs were not released on replace. %+v", s4)
-			return false
-		}
-		for i, v := range []int{0, 32} {
-			if s4[i] != v {
-				c.Logf("Unexpected IPv4 Prefix length. This may be because CIDRs were not released on replace. %+v", s4)
-				return false
-			}
-		}
-		return true
-	}, time.Second*5)
 }
 
 func (ds *DaemonSuite) TestRemovePolicy(c *C) {
