cilium/cilium

commit 93cd67f2994a618865fbcdb0294bcd9b4d44036a

ipcache: fix potential deadlock in GetNamedPorts
We have a partial lock ordering of IPCache > Endpoint as established in
InjectLabels, where the IPCache lock is held and all endpoint locks are
acquired one by one. On the other hand, GetNamedPorts was called in a
context with the Endpoint lock held (cf *Endpoint.GetNamedPortLocked),
and attempts to acquire the IPCache lock. This would establish EP >
IPCache, which potentially deadlocks with the above.

To fix it, remove the requirement for a write lock on IPCache for
GetNamedPorts. Instead, we always pre-compute the namedPorts map in
Upsert and Delete and simply return it in GetNamedPorts. To ensure
atomicity, we use atomics to load and store read-only pointer to the
namedPorts map and the needNamedPorts flag.

Co-authored-by: Sebastian Wicki <sebastian@isovalent.com>
Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

pkg/ipcache/ipcache.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"net"
 	"net/netip"
+	"sync/atomic"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -103,18 +104,17 @@ type IPCache struct {
 	// controllers manages the async controllers for this IPCache
 	controllers *controller.Manager
 
-	// needNamedPorts is initially 'false', but will be changd to 'true' when the
-	// clusterwide named port mappings are needed for network policy computation
-	// for the first time. This avoids the overhead of maintaining 'namedPorts' map
-	// when it is known not to be needed.
-	// Protected by 'mutex'.
-	needNamedPorts bool
+	// needNamedPorts is initially 'false', but will atomically be changed to 'true'
+	// when the clusterwide named port mappings are needed for network policy
+	// computation for the first time. This avoids the overhead of unnecessarily
+	// triggering policy updates when it is known not to be needed.
+	needNamedPorts atomic.Bool
 
 	// namedPorts is a collection of all named ports in the cluster. This is needed
 	// only if an egress policy refers to a port by name.
 	// This map is returned to users so all updates must be made into a fresh map that
-	// is then swapped in place while 'mutex' is being held.
-	namedPorts types.NamedPortMultiMap
+	// is then swapped in place atomically.
+	namedPorts atomic.Pointer[types.NamedPortMultiMap]
 
 	cacheStatus k8s.CacheStatus
 
@@ -141,7 +141,7 @@ func NewIPCache(c *Configuration) *IPCache {
 		ipToHostIPCache:   map[string]IPKeyPair{},
 		ipToK8sMetadata:   map[string]K8sMetadata{},
 		controllers:       controller.NewManager(),
-		namedPorts:        nil,
+		namedPorts:        atomic.Pointer[types.NamedPortMultiMap]{},
 		metadata:          newMetadata(),
 		Configuration:     c,
 	}
@@ -244,13 +244,19 @@ func (ipc *IPCache) getK8sMetadata(ip string) *K8sMetadata {
 	return nil
 }
 
-// updateNamedPorts accumulates named ports from all K8sMetadata entries to a single map
-func (ipc *IPCache) updateNamedPorts() (namedPortsChanged bool) {
-	if !ipc.needNamedPorts {
-		return false
-	}
+// updateNamedPortsRLocked accumulates named ports from all K8sMetadata entries
+// to a single map. It is required to hold _at least_ the read lock when calling
+// this function, holding the write lock is also allowed.
+func (ipc *IPCache) updateNamedPortsRLocked() (namedPortsChanged bool) {
 	// Collect new named Ports
-	npm := make(types.NamedPortMultiMap, len(ipc.namedPorts))
+	var oldLen = 0
+	var old types.NamedPortMultiMap
+	if m := ipc.namedPorts.Load(); m != nil {
+		old = *m
+		oldLen = len(old)
+	}
+
+	npm := make(types.NamedPortMultiMap, oldLen)
 	for _, km := range ipc.ipToK8sMetadata {
 		for name, port := range km.NamedPorts {
 			if npm[name] == nil {
@@ -259,15 +265,20 @@ func (ipc *IPCache) updateNamedPorts() (namedPortsChanged bool) {
 			npm[name][port] = struct{}{}
 		}
 	}
-	namedPortsChanged = !npm.Equal(ipc.namedPorts)
+	namedPortsChanged = !npm.Equal(old)
 	if namedPortsChanged {
-		// swap the new map in
-		if len(npm) == 0 {
-			ipc.namedPorts = nil
-		} else {
-			ipc.namedPorts = npm
-		}
+		// atomically swap the new map in
+		ipc.namedPorts.Store(&npm)
 	}
+
+	// Set namedPortsChanged to false if they have not (yet) been requested.
+	// This avoids triggering policy updates if named ports changed, but no
+	// policy actually needs them. We still pre-compute the namedPorts map,
+	// so we don't have to acquire the IPCache lock in GetNamedPorts
+	if !ipc.needNamedPorts.Load() {
+		namedPortsChanged = false
+	}
+
 	return namedPortsChanged
 }
 
@@ -465,7 +476,7 @@ func (ipc *IPCache) upsertLocked(
 		if namedPortsChanged {
 			// It is possible that some other POD defines same values, check if
 			// anything changes over all the PODs.
-			namedPortsChanged = ipc.updateNamedPorts()
+			namedPortsChanged = ipc.updateNamedPortsRLocked()
 		}
 	}
 
@@ -658,7 +669,7 @@ func (ipc *IPCache) deleteLocked(ip string, source source.Source) (namedPortsCha
 	// Update named ports
 	namedPortsChanged = false
 	if oldK8sMeta != nil && len(oldK8sMeta.NamedPorts) > 0 {
-		namedPortsChanged = ipc.updateNamedPorts()
+		namedPortsChanged = ipc.updateNamedPortsRLocked()
 	}
 
 	if newHostIP != nil {
@@ -680,15 +691,23 @@ func (ipc *IPCache) deleteLocked(ip string, source source.Source) (namedPortsCha
 
 // GetNamedPorts returns a copy of the named ports map. May return nil.
 func (ipc *IPCache) GetNamedPorts() (npm types.NamedPortMultiMap) {
-	ipc.mutex.Lock()
-	if !ipc.needNamedPorts {
-		ipc.needNamedPorts = true
-		ipc.updateNamedPorts()
-	}
+	// We must not acquire the IPCache mutex here, as that would establish a lock ordering of
+	// Endpoint > IPCache (as endpoint.mutex can be held while calling GetNamedPorts)
+	// Since InjectLabels requires IPCache > Endpoint, a deadlock can occur otherwise.
+
+	// needNamedPorts is initially set to 'false'. This means that we will not trigger
+	// policy updates upon changes to named ports. Once this is set to 'true' though,
+	// Upsert and Delete will start to return 'namedPortsChanged = true' if the upsert
+	// or delete changed a named port, enabling the caller to trigger a policy update.
+	// Note that at the moment, this will never be set back to false, even if no policy
+	// uses named ports anymore.
+	ipc.needNamedPorts.Store(true)
+
 	// Caller can keep using the map after the lock is released, as the map is never changed
 	// once published.
-	npm = ipc.namedPorts
-	ipc.mutex.Unlock()
+	if ptr := ipc.namedPorts.Load(); ptr != nil {
+		npm = *ptr
+	}
 	return npm
 }
 

pkg/ipcache/ipcache_test.go
@@ -550,7 +550,7 @@ func (s *IPCacheTestSuite) TestIPCacheNamedPorts(c *C) {
 	namedPortsChanged = IPIdentityCache.Delete(endpointIP2, source.Kubernetes)
 	c.Assert(namedPortsChanged, Equals, true)
 	npm = IPIdentityCache.GetNamedPorts()
-	c.Assert(npm, IsNil)
+	c.Assert(npm, HasLen, 0)
 }
 
 type dummyListener struct {

commit f5bebf20c2873c29d0e4a9f939a651e6a4b50b95

ipcache: switch named ports to reference counting
This commit introduces reference counting for named ports. Using the
reference counting, we know when to add or remove named ports in our
bookkeeping, without having to look through all other pods. This leads
to a significant speedup when inserting many pods, as is the case on
agent startup.

Note that this patch does _not_ restore the original optimization of not
keeping track of named ports until used in a policy. We did not find a
good way of doing so while also avoiding re-introducing the deadlock
fixed in the commit referenced below. Instead, we always do the
bookkeeping, but in a more efficient manner.

The reference counting requires the use of a RWLock, since read and
write accesses to a map must be protected. Thus, there is potential for
introducing yet another deadlock. However, we argue that this is not the
case: We do _not_ acquire any other lock while holding our mutex M. A
wait cycle/deadlock involving M must include holding M when acquiring
some other lock, however. Therefore this patch cannot introduce a
deadlock.

Fixes: 93cd67f299 (ipcache: fix potential deadlock in GetNamedPorts)
Fixes: #24987

Suggested-by: Sebastian Wicki <sebastian@isovalent.com>
Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

pkg/ipcache/ipcache.go
@@ -113,9 +113,10 @@ type IPCache struct {
 
 	// namedPorts is a collection of all named ports in the cluster. This is needed
 	// only if an egress policy refers to a port by name.
-	// This map is returned to users so all updates must be made into a fresh map that
-	// is then swapped in place atomically.
-	namedPorts atomic.Pointer[types.NamedPortMultiMap]
+	// This map is returned (read-only, as a NamedPortMultiMap) to users.
+	// Therefore, all updates must be made atomically, which is guaranteed by the
+	// interface.
+	namedPorts namedPortMultiMapUpdater
 
 	cacheStatus k8s.CacheStatus
 
@@ -142,7 +143,7 @@ func NewIPCache(c *Configuration) *IPCache {
 		ipToHostIPCache:   map[string]IPKeyPair{},
 		ipToK8sMetadata:   map[string]K8sMetadata{},
 		controllers:       controller.NewManager(),
-		namedPorts:        atomic.Pointer[types.NamedPortMultiMap]{},
+		namedPorts:        types.NewNamedPortMultiMap(),
 		metadata:          newMetadata(),
 		Configuration:     c,
 	}
@@ -248,38 +249,9 @@ func (ipc *IPCache) getK8sMetadata(ip string) *K8sMetadata {
 // updateNamedPortsRLocked accumulates named ports from all K8sMetadata entries
 // to a single map. It is required to hold _at least_ the read lock when calling
 // this function, holding the write lock is also allowed.
-func (ipc *IPCache) updateNamedPortsRLocked() (namedPortsChanged bool) {
-	// Collect new named Ports
-	var old types.NamedPortMultiMap
-	if m := ipc.namedPorts.Load(); m != nil {
-		old = *m
-	}
-
-	npm := types.NewNamedPortMultiMap()
-	for _, km := range ipc.ipToK8sMetadata {
-		for name, port := range km.NamedPorts {
-			if npm[name] == nil {
-				npm[name] = make(types.PortProtoSet)
-			}
-			npm[name][port] = struct{}{}
-		}
-	}
-	namedPortsChanged = !npm.Equal(old)
-	if namedPortsChanged {
-		// atomically swap the new map in
-		n := types.NamedPortMultiMap(npm)
-		ipc.namedPorts.Store(&n)
-	}
-
-	// Set namedPortsChanged to false if they have not (yet) been requested.
-	// This avoids triggering policy updates if named ports changed, but no
-	// policy actually needs them. We still pre-compute the namedPorts map,
-	// so we don't have to acquire the IPCache lock in GetNamedPorts
-	if !ipc.needNamedPorts.Load() {
-		namedPortsChanged = false
-	}
-
-	return namedPortsChanged
+func (ipc *IPCache) updateNamedPortsRLocked(old, new types.NamedPortMap) (namedPortsChanged bool) {
+	namedPortsChanged = ipc.namedPorts.Update(old, new)
+	return namedPortsChanged && ipc.needNamedPorts.Load()
 }
 
 // Upsert adds / updates the provided IP (endpoint or CIDR prefix) and identity
@@ -476,7 +448,7 @@ func (ipc *IPCache) upsertLocked(
 		if namedPortsChanged {
 			// It is possible that some other POD defines same values, check if
 			// anything changes over all the PODs.
-			namedPortsChanged = ipc.updateNamedPortsRLocked()
+			namedPortsChanged = ipc.updateNamedPortsRLocked(oldK8sMeta.NamedPorts, newNamedPorts)
 		}
 	}
 
@@ -706,7 +678,7 @@ func (ipc *IPCache) deleteLocked(ip string, source source.Source) (namedPortsCha
 	// Update named ports
 	namedPortsChanged = false
 	if oldK8sMeta != nil && len(oldK8sMeta.NamedPorts) > 0 {
-		namedPortsChanged = ipc.updateNamedPortsRLocked()
+		namedPortsChanged = ipc.updateNamedPortsRLocked(oldK8sMeta.NamedPorts, nil)
 	}
 
 	if newHostIP != nil {
@@ -740,12 +712,8 @@ func (ipc *IPCache) GetNamedPorts() (npm types.NamedPortMultiMap) {
 	// uses named ports anymore.
 	ipc.needNamedPorts.Store(true)
 
-	// Caller can keep using the map after the lock is released, as the map is never changed
-	// once published.
-	if ptr := ipc.namedPorts.Load(); ptr != nil {
-		npm = *ptr
-	}
-	return npm
+	// Caller can keep using the map, operations on it are protected by its mutex.
+	return ipc.namedPorts
 }
 
 // DeleteOnMetadataMatch removes the provided IP to security identity mapping from the IPCache

pkg/ipcache/types.go
@@ -9,17 +9,18 @@ import (
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/cilium/cilium/pkg/ipcache/types"
+	ipcacheTypes "github.com/cilium/cilium/pkg/ipcache/types"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/source"
+	"github.com/cilium/cilium/pkg/types"
 )
 
 // prefixInfo holds all of the information (labels, etc.) about a given prefix
 // independently based on the ResourceID of the origin of that information, and
 // provides convenient accessors to consistently merge the stored information
 // to generate ipcache output based on a range of inputs.
-type prefixInfo map[types.ResourceID]*resourceInfo
+type prefixInfo map[ipcacheTypes.ResourceID]*resourceInfo
 
 // IdentityOverride can be used to override the identity of a given prefix.
 // Must be provided together with a set of labels. Any other labels associated
@@ -46,6 +47,13 @@ type resourceInfo struct {
 // cannot be used in conjunction with methods, which is how the information
 // gets injected into the IPCache.
 type IPMetadata any
+
+// namedPortMultiMapUpdater allows for mutation of the NamedPortMultiMap, which
+// is otherwise read-only.
+type namedPortMultiMapUpdater interface {
+	types.NamedPortMultiMap
+	Update(old, new types.NamedPortMap) (namedPortChanged bool)
+}
 
 // merge overwrites the field in 'resourceInfo' corresponding to 'info'. This
 // associates the new information with the prefix and ResourceID that this
@@ -149,7 +157,7 @@ func (s prefixInfo) identityOverride() (lbls labels.Labels, hasOverride bool) {
 func (s prefixInfo) logConflicts(scopedLog *logrus.Entry) {
 	var (
 		override           labels.Labels
-		overrideResourceID types.ResourceID
+		overrideResourceID ipcacheTypes.ResourceID
 	)
 
 	for resourceID, info := range s {

pkg/types/portmap.go
@@ -8,7 +8,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/cilium/cilium/pkg/counter"
 	"github.com/cilium/cilium/pkg/iana"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/u8proto"
 )
 
@@ -30,8 +32,8 @@ type PortProto struct {
 // NamedPortMap maps port names to port numbers and protocols.
 type NamedPortMap map[string]PortProto
 
-// PortProtoSet is a set of unique PortProto values.
-type PortProtoSet map[PortProto]struct{}
+// PortProtoSet is a reference-counted set of unique PortProto values.
+type PortProtoSet counter.Counter[PortProto]
 
 // Equal returns true if the PortProtoSets are equal.
 func (pps PortProtoSet) Equal(other PortProtoSet) bool {
@@ -47,6 +49,16 @@ func (pps PortProtoSet) Equal(other PortProtoSet) bool {
 	return true
 }
 
+// Add increments the reference count for the specified key.
+func (pps PortProtoSet) Add(pp PortProto) bool {
+	return counter.Counter[PortProto](pps).Add(pp)
+}
+
+// Delete decrements the reference count for the specified key.
+func (pps PortProtoSet) Delete(pp PortProto) bool {
+	return counter.Counter[PortProto](pps).Delete(pp)
+}
+
 // NamedPortMultiMap may have multiple entries for a name if multiple PODs
 // define the same name with different values.
 type NamedPortMultiMap interface {
@@ -54,31 +66,53 @@ type NamedPortMultiMap interface {
 	GetNamedPort(name string, proto uint8) (uint16, error)
 	// Len returns the number of Name->PortProtoSet mappings known.
 	Len() int
-	Equal(other NamedPortMultiMap) bool
 }
 
-func NewNamedPortMultiMap() namedPortMultiMap {
-	return make(namedPortMultiMap)
-}
-
-type namedPortMultiMap map[string]PortProtoSet
-
-// Equal returns true if the NamedPortMultiMaps are equal.
-func (npm namedPortMultiMap) Equal(other NamedPortMultiMap) bool {
-	o, ok := other.(namedPortMultiMap)
-	if !ok || len(npm) != len(o) {
-		return false
+func NewNamedPortMultiMap() *namedPortMultiMap {
+	return &namedPortMultiMap{
+		m: make(map[string]PortProtoSet),
 	}
-	for name, ports := range npm {
-		if otherPorts, exists := o[name]; !exists || !ports.Equal(otherPorts) {
-			return false
+}
+
+// Implements NamedPortMultiMap and allows changes through Update. All accesses
+// must be protected by its RW mutex.
+type namedPortMultiMap struct {
+	lock.RWMutex
+	m map[string]PortProtoSet
+}
+
+func (npm *namedPortMultiMap) Len() int {
+	npm.RLock()
+	defer npm.RUnlock()
+	return len(npm.m)
+}
+
+// Update applies potential changes in named ports, and returns whether there were any.
+func (npm *namedPortMultiMap) Update(old, new NamedPortMap) (namedPortsChanged bool) {
+	npm.Lock()
+	defer npm.Unlock()
+	// The order is important here. Increment the refcount first, and then
+	// decrement it again for old ports, so that we don't hit zero if there are
+	// no changes.
+	for name, port := range new {
+		c, ok := npm.m[name]
+		if !ok {
+			c = make(PortProtoSet)
+			npm.m[name] = c
+		}
+		if c.Add(port) {
+			namedPortsChanged = true
 		}
 	}
-	return true
-}
-
-func (npm namedPortMultiMap) Len() int {
-	return len(npm)
+	for name, port := range old {
+		if npm.m[name].Delete(port) {
+			namedPortsChanged = true
+			if len(npm.m[name]) == 0 {
+				delete(npm.m, name)
+			}
+		}
+	}
+	return namedPortsChanged
 }
 
 // ValidatePortName checks that the port name conforms to the IANA Service Names spec
@@ -146,11 +180,16 @@ func (npm NamedPortMap) GetNamedPort(name string, proto uint8) (uint16, error) {
 }
 
 // GetNamedPort returns the port number for the named port, if any.
-func (npm namedPortMultiMap) GetNamedPort(name string, proto uint8) (uint16, error) {
+func (npm *namedPortMultiMap) GetNamedPort(name string, proto uint8) (uint16, error) {
 	if npm == nil {
 		return 0, ErrNilMap
 	}
-	pps, ok := npm[name]
+	npm.RLock()
+	defer npm.RUnlock()
+	if npm.m == nil {
+		return 0, ErrNilMap
+	}
+	pps, ok := npm.m[name]
 	if !ok {
 		// Return an error the caller can filter out as this happens only for egress policy
 		// and it is likely the destination POD with the port name is simply not scheduled yet.

pkg/types/portmap_test.go
@@ -88,14 +88,14 @@ func (ds *PortsTestSuite) TestPolicyNamedPortMap(c *C) {
 
 func (ds *PortsTestSuite) TestPolicyPortProtoSet(c *C) {
 	a := PortProtoSet{
-		PortProto{Port: 80, Proto: 6}:  struct{}{},
-		PortProto{Port: 443, Proto: 6}: struct{}{},
-		PortProto{Port: 53, Proto: 17}: struct{}{},
+		PortProto{Port: 80, Proto: 6}:  1,
+		PortProto{Port: 443, Proto: 6}: 1,
+		PortProto{Port: 53, Proto: 17}: 1,
 	}
 	b := PortProtoSet{
-		PortProto{Port: 80, Proto: 6}:  struct{}{},
-		PortProto{Port: 443, Proto: 6}: struct{}{},
-		PortProto{Port: 53, Proto: 6}:  struct{}{},
+		PortProto{Port: 80, Proto: 6}:  1,
+		PortProto{Port: 443, Proto: 6}: 1,
+		PortProto{Port: 53, Proto: 6}:  1,
 	}
 	c.Assert(a.Equal(a), Equals, true)
 	c.Assert(a.Equal(b), Equals, false)
@@ -103,43 +103,43 @@ func (ds *PortsTestSuite) TestPolicyPortProtoSet(c *C) {
 }
 
 func (ds *PortsTestSuite) TestPolicyNamedPortMultiMap(c *C) {
-	a := namedPortMultiMap{
-		"http": PortProtoSet{
-			PortProto{Port: 80, Proto: 6}:   struct{}{},
-			PortProto{Port: 8080, Proto: 6}: struct{}{},
-		},
-		"https": PortProtoSet{
-			PortProto{Port: 443, Proto: 6}: struct{}{},
-		},
-		"zero": PortProtoSet{
-			PortProto{Port: 0, Proto: 6}: struct{}{},
-		},
-		"none": PortProtoSet{},
-		"dns": PortProtoSet{
-			PortProto{Port: 53, Proto: 17}: struct{}{},
-			PortProto{Port: 53, Proto: 6}:  struct{}{},
-		},
-	}
-	b := namedPortMultiMap{
-		"http": PortProtoSet{
-			PortProto{Port: 80, Proto: 6}:   struct{}{},
-			PortProto{Port: 8080, Proto: 6}: struct{}{},
-		},
-		"https": PortProtoSet{
-			PortProto{Port: 443, Proto: 6}: struct{}{},
-		},
-		"zero": PortProtoSet{
-			PortProto{Port: 0, Proto: 6}: struct{}{},
-		},
-		"none": PortProtoSet{},
-		"dns": PortProtoSet{
-			PortProto{Port: 53, Proto: 0}: struct{}{},
+	a := &namedPortMultiMap{
+		m: map[string]PortProtoSet{
+			"http": {
+				PortProto{Port: 80, Proto: 6}:   1,
+				PortProto{Port: 8080, Proto: 6}: 1,
+			},
+			"https": {
+				PortProto{Port: 443, Proto: 6}: 1,
+			},
+			"zero": {
+				PortProto{Port: 0, Proto: 6}: 1,
+			},
+			"none": {},
+			"dns": {
+				PortProto{Port: 53, Proto: 17}: 1,
+				PortProto{Port: 53, Proto: 6}:  1,
+			},
 		},
 	}
-
-	c.Assert(a.Equal(a), Equals, true)
-	c.Assert(a.Equal(b), Equals, false)
-	c.Assert(b.Equal(b), Equals, true)
+	b := &namedPortMultiMap{
+		m: map[string]PortProtoSet{
+			"http": {
+				PortProto{Port: 80, Proto: 6}:   1,
+				PortProto{Port: 8080, Proto: 6}: 1,
+			},
+			"https": {
+				PortProto{Port: 443, Proto: 6}: 1,
+			},
+			"zero": {
+				PortProto{Port: 0, Proto: 6}: 1,
+			},
+			"none": {},
+			"dns": {
+				PortProto{Port: 53, Proto: 0}: 1,
+			},
+		},
+	}
 
 	port, err := a.GetNamedPort("http", 6)
 	c.Assert(err, Equals, ErrDuplicateNamedPorts)
@@ -161,7 +161,7 @@ func (ds *PortsTestSuite) TestPolicyNamedPortMultiMap(c *C) {
 	c.Assert(err, Equals, ErrUnknownNamedPort)
 	c.Assert(port, Equals, uint16(0))
 
-	var nilvalued namedPortMultiMap
+	var nilvalued *namedPortMultiMap
 	port, err = NamedPortMultiMap(nilvalued).GetNamedPort("unknown", 6)
 	c.Assert(err, Equals, ErrNilMap)
 	c.Assert(port, Equals, uint16(0))
@@ -185,4 +185,54 @@ func (ds *PortsTestSuite) TestPolicyNamedPortMultiMap(c *C) {
 	port, err = b.GetNamedPort("dns", 17)
 	c.Assert(err, IsNil)
 	c.Assert(port, Equals, uint16(53))
+}
+
+func (ds *PortsTestSuite) TestPolicyNamedPortMultiMapUpdate(c *C) {
+	npm := NewNamedPortMultiMap()
+
+	pod1PortsOld := map[string]PortProto{}
+	pod1PortsNew := map[string]PortProto{
+		"http": {80, uint8(u8proto.TCP)},
+	}
+
+	// Insert http=80/TCP from pod1
+	changed := npm.Update(pod1PortsOld, pod1PortsNew)
+	c.Assert(changed, Equals, true)
+
+	// No changes
+	changed = npm.Update(pod1PortsNew, pod1PortsNew)
+	c.Assert(changed, Equals, false)
+
+	// Assert http=80/TCP
+	port, err := npm.GetNamedPort("http", uint8(u8proto.TCP))
+	c.Assert(err, IsNil)
+	c.Assert(port, Equals, uint16(80))
+
+	pod2PortsOld := map[string]PortProto{}
+	pod2PortsNew := map[string]PortProto{
+		"http": {8080, uint8(u8proto.UDP)},
+	}
+
+	// Insert http=8080/UDP from pod2, retain http=80/TCP from pod1
+	changed = npm.Update(pod2PortsOld, pod2PortsNew)
+	c.Assert(changed, Equals, true)
+
+	port, err = npm.GetNamedPort("http", uint8(u8proto.TCP))
+	c.Assert(err, IsNil)
+	c.Assert(port, Equals, uint16(80))
+	port, err = npm.GetNamedPort("http", uint8(u8proto.UDP))
+	c.Assert(err, IsNil)
+	c.Assert(port, Equals, uint16(8080))
+
+	// Delete http=80/TCP from pod1, retain http=8080/UDP from pod2
+	pod1PortsOld = pod1PortsNew
+	pod1PortsNew = map[string]PortProto{}
+
+	// Delete http=80/TCP from pod1
+	changed = npm.Update(pod1PortsOld, pod1PortsNew)
+	c.Assert(changed, Equals, true)
+
+	port, err = npm.GetNamedPort("http", uint8(u8proto.UDP))
+	c.Assert(err, IsNil)
+	c.Assert(port, Equals, uint16(8080))
 }