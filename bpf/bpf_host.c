dbc095bd5f80d82546560682cc743134cd99167e
Julian Wiedmann <jwi@isovalent.com>
2023-04-27 11:51:03 +0300

    bpf: also pull IP header for from-host traffic
    [ upstream commit 1e7fc437711febd679ed519a52ea750573f83eb8 ]
    
    Don't trust that host traffic arrives with the IP header in the skb's
    linear space. So have resolve_srcid_ipv*() always pull the necessary data,
    without differentiating between from-netdev and from-host traffic.
    
    This means that for to-netdev traffic we now have two places that can
    handle the traffic. This is just temporary, and will go away with the next
    patch.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -122,7 +122,7 @@ resolve_srcid_ipv6(struct __ctx_buff *ctx, __u32 srcid_from_proxy,
 	union v6addr *src;
 	int ret;
 
-	if (!revalidate_data_maybe_pull(ctx, &data, &data_end, &ip6, !from_host))
+	if (!revalidate_data_pull(ctx, &data, &data_end, &ip6))
 		return DROP_INVALID;
 
 	if (!from_host) {
@@ -410,11 +410,11 @@ resolve_srcid_ipv4(struct __ctx_buff *ctx, __u32 srcid_from_proxy,
 	struct iphdr *ip4;
 
 	/* This is the first time revalidate_data() is going to be called in
-	 * the "from-netdev" path. Make sure that we don't legitimately drop
-	 * the packet if the skb arrived with the header not being not in the
-	 * linear data.
+	 * the "from-netdev" and "from-host" paths. Make sure that we don't
+	 * legitimately drop the packet if the skb arrived with the header
+	 * not being not in the linear data.
 	 */
-	if (!revalidate_data_maybe_pull(ctx, &data, &data_end, &ip4, !from_host))
+	if (!revalidate_data_pull(ctx, &data, &data_end, &ip4))
 		return DROP_INVALID;
 
 	/* Packets from the proxy will already have a real identity. */
