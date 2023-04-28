cilium/cilium

commit b79a4a537001bb3fa1ceabce7a4deb885b2fbccc

pkg/service: Gracefully terminate service backends
Objective:
Upon removal of a service backend, drain all the existing
connections to the backend, and ensure that the backend is
not selected for new connections.

Implementation:
Backend selection in the datapath is done using `lb service`
map lookups, and backend information is retrieved via `lb backend`
maps. Earlier, we removed the backend entry from the maps as soon
as it was deleted. This can break existing active connections to
the backend for per-packet based load-balancing, or unconnected UDP,
where backend information retrieval lookups fail, and connections are
redirected to other backends.
Kubernetes allows users to specify a `terminationGracefulPeriod` in the
pod spec in order to allow server pods to gracefully terminate active
connections. In k8s v1.20, a new endpoint condition called as
`Terminating` [1] was added. When an endpoint is deleted, k8s api
server first sends an update endpoint slice event with the terminating
state set to true for the endpoint. This state is propagated to the
internal backend state that's used to program datapath BPF maps.
When we receive such a backend, we skip adding it to the service map so
that the backend isn't selected for new connections. But we keep the entry
in the backend and affinity maps so that active connections continue
are not disrupted. Once the graceful termination period is over, k8s
watcher received the event to delete the endpoint. At that point, the
backend is removed from the remaining maps.

[1] https://kubernetes.io/docs/concepts/services-networking/endpoint-slices/#terminating

Related: #14844
Signed-off-by: Aditi Ghag <aditi@cilium.io>

pkg/service/service.go
@@ -787,7 +787,16 @@ func (s *Service) upsertServiceIntoLBMaps(svc *svcInfo, onlyLocalBackends bool,
 	// Upsert service entries into BPF maps
 	backends := make(map[string]lb.BackendID, len(svc.backends))
 	for _, b := range svc.backends {
-		backends[b.String()] = b.ID
+		// Skip adding the terminating backend to the service map so that it
+		// won't be selected to serve new requests. However, the backend is still
+		// kept in the affinity and backend maps so that existing connections
+		// are able to terminate gracefully. The final clean-up for the backend
+		// will happen once the agent receives a delete event for the service
+		// endpoint as it'll be part of obsoleteBackendIDs/obsoleteSVCBackendIDs
+		// list passed to this function.
+		if !b.Terminating {
+			backends[b.String()] = b.ID
+		}
 	}
 
 	p := &lbmap.UpsertServiceParams{

commit 53a2b2333e099f80dfb476901eacabd11f7c7f31

pkg/service: Extend logic to handle leaked backends
This commit revises the fix made in commit 5311f81505 that
handles leaked backends.
Consider the following leaked backend scenarios where (1) and
(2a) were previously handled:
(1) Backend entries leaked, no duplicates: previously, such
   backends would be deleted as orphan backends after sync with
   kubernetes api server.
(2) Backend entries leaked with duplicates:
	(a) backend with overlapping L3nL4Addr hash is associated with service(s)
        Sequence of events:
        Backends were leaked prior to agent restart, but there was at least
        one service that the backend by hash is associated with.
        s.backendByHash will have a non-zero reference count for the
        overlapping L3nL4Addr hash.
	(b) none of the backends are associated with services
	    Sequence of events:
        All the services these backends were associated with were deleted
        prior to agent restart.
        s.backendByHash will not have an entry for the backends hash.

This commit updates the logic to handle all the scenarios in one place.

Fixes: b79a4a5 (pkg/service: Gracefully terminate service backends)

Signed-off-by: Aditi Ghag <aditi@cilium.io>

pkg/service/service.go
@@ -1367,6 +1367,7 @@ func (s *Service) restoreBackendsLocked(svcBackendsById map[lb.BackendID]struct{
 		return fmt.Errorf("Unable to dump backend maps: %s", err)
 	}
 
+	svcBackendsCount := len(svcBackendsById)
 	for _, b := range backends {
 		log.WithFields(logrus.Fields{
 			logfields.BackendID:        b.ID,
@@ -1374,32 +1375,54 @@ func (s *Service) restoreBackendsLocked(svcBackendsById map[lb.BackendID]struct{
 			logfields.BackendState:     b.State,
 			logfields.BackendPreferred: b.Preferred,
 		}).Debug("Restoring backend")
-		if _, ok := svcBackendsById[b.ID]; !ok && s.backendRefCount[b.L3n4Addr.Hash()] != 0 {
-			// If a backend by id isn't referenced by any of the service entries,
-			// it's likely to be a duplicate backend. This can happen when agent
-			// leaked backend entries in the backends map prior to restart, and created
-			// duplicate with different IDs but same L3n4Addr (hash).
+		if _, ok := svcBackendsById[b.ID]; !ok && (svcBackendsCount != 0) {
+			// If a backend by ID isn't referenced by any of the services, it's
+			// likely a leaked backend. In case of duplicate leaked backends,
+			// there would be multiple IDs allocated for the same backend resource
+			// identified by its L3nL4Addr hash. The second check for service
+			// backends count is added for unusual cases where there might've been
+			// a problem with reading entries from the services map. In such cases,
+			// the agent should not wipe out the backends map, as this can disrupt
+			// existing connections. SyncWithK8sFinished will later sync the backends
+			// map with the latest state.
+			// Leaked backend scenarios:
+			// 1) Backend entries leaked, no duplicates
+			// 2) Backend entries leaked with duplicates:
+			// 	a) backend with overlapping L3nL4Addr hash is associated with service(s)
+			//     Sequence of events:
+			//     Backends were leaked prior to agent restart, but there was at least
+			//     one service that the backend by hash is associated with.
+			//     s.backendByHash will have a non-zero reference count for the
+			//     overlapping L3nL4Addr hash.
+			// 	b) none of the backends are associated with services
+			//     Sequence of events:
+			// 	   All the services these backends were associated with were deleted
+			//     prior to agent restart.
+			//     s.backendByHash will not have an entry for the backends hash.
 			// As none of the service entries have a reference to these backends
-			// in the services map, the duplicate backends were not available for
+			// in the services map, the backends were likely not available for
 			// load-balancing new traffic. While there is a slim chance that the
-			// duplicate backends could have previously established active connections,
+			// backends could have previously established active connections,
 			// and these connections can get disrupted. However, the leaks likely
 			// happened when service entries were deleted, so those connections
 			// were also expected to be terminated.
 			// Regardless, delete the duplicates as this can affect restoration of current
 			// active backends, and may prevent new backends getting added as map
-			// size is limited, which can result in connectivity issues.
+			// size is limited, which can lead to connectivity disruptions.
 			id := b.ID
 			DeleteBackendID(id)
 			if err := s.lbmap.DeleteBackendByID(id); err != nil {
-				log.Errorf("unable to delete duplicate backend: %v", id)
+				// As the backends map is not expected to be updated during restore,
+				// the deletion call shouldn't fail. But log the error, just
+				// in case...
+				log.Errorf("unable to delete leaked backend: %v", id)
 			}
 			log.WithFields(logrus.Fields{
 				logfields.BackendID:        b.ID,
 				logfields.L3n4Addr:         b.L3n4Addr,
 				logfields.BackendState:     b.State,
 				logfields.BackendPreferred: b.Preferred,
-			}).Debug("Duplicate backend entry not restored")
+			}).Debug("Leaked backend entry not restored")
 			skipped++
 			continue
 		}