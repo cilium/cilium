cce4965d376139adcde8b7f724ede02f5d674828
Michi Mutsuzaki <michi@isovalent.com>
2023-05-01 18:16:57 +0000

    helm mode: Make Helm chart repository configurable
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Add --repository flag in helm mode install / upgrade commands to override
    the default https://helm.cilium.io repository.
    
    I intentionally avoided adding this flag to the classic mode since it's
    being deprecated in the next minor release (v0.15). The behavior of the
    classic mode is unchanged in this pull request.
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- cilium-cli/internal/helm/helm.go
+++ cilium-cli/internal/helm/helm.go
@@ -38,8 +38,6 @@ import (
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
-const ciliumChart = "https://helm.cilium.io"
-
 var settings = cli.New()
 
 // State contains Helm state for the current Cilium installation. Cilium CLI retrieves this
@@ -217,7 +215,7 @@ func newChartFromDirectory(directory string) (*chart.Chart, error) {
 
 // newChartFromRemoteWithCache fetches the chart from remote repository, the chart file
 // is then stored in the local cache directory for future usage.
-func newChartFromRemoteWithCache(ciliumVersion semver2.Version) (*chart.Chart, error) {
+func newChartFromRemoteWithCache(ciliumVersion semver2.Version, repository string) (*chart.Chart, error) {
 	cacheDir, err := ciliumCacheDir()
 	if err != nil {
 		return nil, err
@@ -232,7 +230,7 @@ func newChartFromRemoteWithCache(ciliumVersion semver2.Version) (*chart.Chart, e
 		// Download the chart from remote repository
 		pull := action.NewPullWithOpts(action.WithConfig(new(action.Configuration)))
 		pull.Settings = settings
-		pull.RepoURL = ciliumChart
+		pull.RepoURL = repository
 		pull.Version = ciliumVersion.String()
 		pull.DestDir = cacheDir
 
@@ -289,7 +287,8 @@ func GenManifests(
 			if !errors.Is(err, fs.ErrNotExist) {
 				return nil, err
 			}
-			helmChart, err = newChartFromRemoteWithCache(ciliumVer)
+			// Helm repository is not configurable in the classic mode. Always use the default Helm repository.
+			helmChart, err = newChartFromRemoteWithCache(ciliumVer, defaults.HelmRepository)
 			if err != nil {
 				return nil, err
 			}
@@ -420,11 +419,15 @@ func ListVersions() ([]string, error) {
 	return versions, nil
 }
 
-// ResolveHelmChartVersion resolves Helm chart version based on --version and --chart-directory flags.
-func ResolveHelmChartVersion(versionFlag, chartDirectoryFlag string) (semver2.Version, *chart.Chart, error) {
+// ResolveHelmChartVersion resolves Helm chart version based on --version, --chart-directory, and --repository flags.
+func ResolveHelmChartVersion(versionFlag, chartDirectoryFlag, repository string) (semver2.Version, *chart.Chart, error) {
+	// If repository is empty, set it to the default Helm repository ("https://helm.cilium.io") for backward compatibility.
+	if repository == "" {
+		repository = defaults.HelmRepository
+	}
 	if chartDirectoryFlag == "" {
 		// If --chart-directory flag is not specified, use the version specified with --version flag.
-		return resolveChartVersion(versionFlag)
+		return resolveChartVersion(versionFlag, repository)
 	}
 
 	// Get the chart version from the local Helm chart specified with --chart-directory flag.
@@ -435,22 +438,25 @@ func ResolveHelmChartVersion(versionFlag, chartDirectoryFlag string) (semver2.Ve
 	return versioncheck.MustVersion(localChart.Metadata.Version), localChart, nil
 }
 
-func resolveChartVersion(versionFlag string) (semver2.Version, *chart.Chart, error) {
+func resolveChartVersion(versionFlag string, repository string) (semver2.Version, *chart.Chart, error) {
 	version, err := utils.ParseCiliumVersion(versionFlag)
 	if err != nil {
 		return semver2.Version{}, nil, err
 	}
 
-	helmChart, err := newChartFromEmbeddedFile(version)
-	if err == nil {
-		return version, helmChart, nil
+	// If the repository is the default repository ("https://helm.cilium.io"), check embedded charts first.
+	if repository == defaults.HelmRepository {
+		helmChart, err := newChartFromEmbeddedFile(version)
+		if err == nil {
+			return version, helmChart, nil
+		}
+
+		if !errors.Is(err, fs.ErrNotExist) {
+			return semver2.Version{}, nil, err
+		}
 	}
 
-	if !errors.Is(err, fs.ErrNotExist) {
-		return semver2.Version{}, nil, err
-	}
-
-	helmChart, err = newChartFromRemoteWithCache(version)
+	helmChart, err := newChartFromRemoteWithCache(version, repository)
 	if err != nil {
 		return semver2.Version{}, nil, err
 	}
