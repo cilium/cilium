be111c71a06af52afbeb075865530c857949b208
Maxim Mikityanskiy <maxim@isovalent.com>
2023-05-02 20:19:22 +0300

    bpf: Rename ct_lb_lookup4 to ct_lazy_lookup4 and add action parameter
    Rename ct_lb_lookup4 to ct_lazy_lookup4, because it's going to be used
    not only in LB flows, but now also in NAT. Add the action parameter to
    cover ICMP packets in NAT flows.
    
    The actual usage comes in the next commit, this one doesn't contain
    functional changes.
    
    Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

--- bpf/lib/lb.h
+++ bpf/lib/lb.h
@@ -1527,8 +1527,8 @@ static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
 	if (unlikely(svc->count == 0))
 		return DROP_NO_SERVICE;
 
-	ret = ct_lb_lookup4(map, tuple, ctx, l4_off, has_l4_header, CT_SERVICE,
-			    state, &monitor);
+	ret = ct_lazy_lookup4(map, tuple, ctx, l4_off, has_l4_header, ACTION_CREATE,
+			      CT_SERVICE, state, &monitor);
 	switch (ret) {
 	case CT_NEW:
 #ifdef ENABLE_SESSION_AFFINITY
