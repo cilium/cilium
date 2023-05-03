0f7ea6c815997a74d9f6d492766a1dfb1644d085
Donia Chaiehloudj <donia.cld@isovalent.com>
2023-05-03 18:33:54 +0200

    Add metrics increase validation and a simple test to check metrics on total request
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: Donia Chaiehloudj <donia.cld@isovalent.com>

--- cilium-cli/connectivity/check/metrics_test.go
+++ cilium-cli/connectivity/check/metrics_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	dto "github.com/prometheus/client_model/go"
 	prommodel "github.com/prometheus/client_model/go"
 	"github.com/stretchr/testify/assert"
 )
@@ -50,4 +51,83 @@ cilium_forward_count_total{direction="INGRESS"} 812973
 	got, err := parseMetrics(reader)
 	assert.NoError(t, err)
 	assert.Exactly(t, want, got)
+}
+
+func TestMetricsIncrease(t *testing.T) {
+	metricName := "cilium_forward_count_total"
+	metricHelp := "Total forwarded packets, tagged by ingress/egress direction"
+	metricTypeCounter := prommodel.MetricType_COUNTER
+	labelName := "direction"
+	labelEgress := "EGRESS"
+	labelIngress := "INGRESS"
+	valueEgress := 444088.
+	valueIngress := 812973.
+
+	valueEgressAfter := 1599128.
+	valueIngressAfter := 3789798.
+
+	ciliumForwardCountTotalBefore := dto.MetricFamily{
+		Name: &metricName,
+		Help: &metricHelp,
+		Type: &metricTypeCounter,
+		Metric: []*prommodel.Metric{
+			{
+				Label:   []*prommodel.LabelPair{{Name: &labelName, Value: &labelEgress}},
+				Counter: &prommodel.Counter{Value: &valueEgress},
+			},
+			{
+				Label:   []*prommodel.LabelPair{{Name: &labelName, Value: &labelIngress}},
+				Counter: &prommodel.Counter{Value: &valueIngress},
+			},
+		},
+	}
+
+	ciliumForwardCountTotalAfter := dto.MetricFamily{
+		Name: &metricName,
+		Help: &metricHelp,
+		Type: &metricTypeCounter,
+		Metric: []*prommodel.Metric{
+			{
+				Label:   []*prommodel.LabelPair{{Name: &labelName, Value: &labelEgress}},
+				Counter: &prommodel.Counter{Value: &valueEgressAfter},
+			},
+			{
+				Label:   []*prommodel.LabelPair{{Name: &labelName, Value: &labelIngress}},
+				Counter: &prommodel.Counter{Value: &valueIngressAfter},
+			},
+		},
+	}
+
+	tt := map[string]struct {
+		before dto.MetricFamily
+		after  dto.MetricFamily
+		err    bool
+	}{
+		"metric increases": {
+			before: ciliumForwardCountTotalBefore,
+			after:  ciliumForwardCountTotalAfter,
+			err:    false,
+		},
+		"metrics are equals": {
+			before: ciliumForwardCountTotalBefore,
+			after:  ciliumForwardCountTotalBefore,
+			err:    true,
+		},
+		"metric decreases": {
+			before: ciliumForwardCountTotalAfter,
+			after:  ciliumForwardCountTotalBefore,
+			err:    true,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			err := metricsIncrease(tc.before, tc.after)
+			if tc.err {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
