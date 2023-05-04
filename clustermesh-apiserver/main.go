3cbabeec59b58ff5a767197aa55f7a637ce9af02
Marco Iorio <marco.iorio@isovalent.com>
2023-05-04 12:18:23 +0200

    clustermesh-apiserver: add flag to disable external workloads support
    [ upstream commit e772ba82d9acbc9da932e581c6e5c7be90929f6a ]
    
    Currently, the support for external workloads is always enabled in the
    clustermesh-apiserver. Yet, this feature requires the synchronization of
    all the services present in the cluster to the kvstore, including the
    corresponding backends (plain clustermesh instead only requires the
    synchronization of shared services and associated backends).
    
    Given that full synchronization is quite onerous in large clusters,
    let's make it configurable through a dedicated flag. By default, it
    is enabled, in order not to modify the current behavior when unspecified
    (e.g., by the legacy Cilium CLI). When installing cilium through Helm,
    instead, it is configured according to the pre-existing
    externalWorkloads.enabled configuration entry.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- clustermesh-apiserver/main.go
+++ clustermesh-apiserver/main.go
@@ -61,8 +61,9 @@ import (
 )
 
 type configuration struct {
-	clusterName      string
-	serviceProxyName string
+	clusterName             string
+	serviceProxyName        string
+	enableExternalWorkloads bool
 }
 
 func (c configuration) LocalClusterName() string {
@@ -272,6 +273,11 @@ func runApiserver() error {
 
 	flags.Int(option.PProfPort, defaults.PprofPortAPIServer, "Port that pprof listens on")
 	option.BindEnv(vp, option.PProfPort)
+
+	// The default values is set to true to match the existing behavior in case
+	// the flag is not configured (for instance by the legacy cilium CLI).
+	flags.BoolVar(&cfg.enableExternalWorkloads, option.EnableExternalWorkloads, true, "Enable support for external workloads")
+	option.BindEnv(vp, option.EnableExternalWorkloads)
 
 	vp.BindPFlags(flags)
 
@@ -589,8 +595,6 @@ func startServer(startCtx hive.HookContext, clientset k8sClient.Clientset, servi
 		synced.SyncCRDs(startCtx, clientset, synced.AllCRDResourceNames(), &synced.Resources{}, &synced.APIGroups{})
 	}
 
-	mgr := NewVMManager(clientset)
-
 	var err error
 	if err = kvstore.Setup(context.Background(), "etcd", option.Config.KVStoreOpt, nil); err != nil {
 		log.WithError(err).Fatal("Unable to connect to etcd")
@@ -604,14 +608,17 @@ func startServer(startCtx hive.HookContext, clientset k8sClient.Clientset, servi
 		log.WithError(err).Fatal("Unable to set local cluster config on kvstore")
 	}
 
-	_, err = store.JoinSharedStore(store.Configuration{
-		Prefix:               nodeStore.NodeRegisterStorePrefix,
-		KeyCreator:           nodeStore.RegisterKeyCreator,
-		SharedKeyDeleteDelay: defaults.NodeDeleteDelay,
-		Observer:             mgr,
-	})
-	if err != nil {
-		log.WithError(err).Fatal("Unable to set up node register store in etcd")
+	if cfg.enableExternalWorkloads {
+		mgr := NewVMManager(clientset)
+		_, err = store.JoinSharedStore(store.Configuration{
+			Prefix:               nodeStore.NodeRegisterStorePrefix,
+			KeyCreator:           nodeStore.RegisterKeyCreator,
+			SharedKeyDeleteDelay: defaults.NodeDeleteDelay,
+			Observer:             mgr,
+		})
+		if err != nil {
+			log.WithError(err).Fatal("Unable to set up node register store in etcd")
+		}
 	}
 
 	ciliumNodeStore, err = store.JoinSharedStore(store.Configuration{
@@ -630,7 +637,7 @@ func startServer(startCtx hive.HookContext, clientset k8sClient.Clientset, servi
 		synchronizeIdentities(clientset)
 		synchronizeNodes(clientset)
 		synchronizeCiliumEndpoints(clientset)
-		operatorWatchers.StartSynchronizingServices(context.Background(), &sync.WaitGroup{}, clientset, services, false, cfg)
+		operatorWatchers.StartSynchronizingServices(context.Background(), &sync.WaitGroup{}, clientset, services, !cfg.enableExternalWorkloads, cfg)
 	}
 
 	go func() {
