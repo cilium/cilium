cilium/cilium

commit aa3c85fc7048697d75401a0d034555aaee71df25

k8s: Add preferred attribute in Endpoints
This commit is to add preferred attribute in Backend struct, which is to
indicate this backend is highly preferred if active.

Also, setting the value for this attribute accordingly based on related
annotation.

Signed-off-by: Tam Mach <tam.mach@isovalent.com>

pkg/k8s/endpoints.go
@@ -58,6 +58,7 @@ type Backend struct {
 	NodeName      string
 	Terminating   bool
 	HintsForZones []string
+	Preferred     bool
 }
 
 // String returns the string representation of an endpoints resource, with

pkg/k8s/service_cache.go
@@ -491,6 +491,7 @@ func (s *ServiceCache) correlateEndpoints(id ServiceID) (*Endpoints, bool) {
 		localEndpoints = s.filterEndpoints(localEndpoints, svc)
 
 		for ip, e := range localEndpoints.Backends {
+			e.Preferred = svcFound && svc.IncludeExternal && svc.ServiceAffinity == serviceAffinityLocal
 			endpoints.Backends[ip] = e
 		}
 	}
@@ -511,6 +512,7 @@ func (s *ServiceCache) correlateEndpoints(id ServiceID) (*Endpoints, bool) {
 							"cluster":              clusterName,
 						}).Warning("Conflicting service backend IP")
 					} else {
+						e.Preferred = svc.ServiceAffinity == serviceAffinityRemote
 						endpoints.Backends[ip] = e
 					}
 				}

commit 4cf827b7249246fa6a77be234ab3e37b15fb53ea

k8s: Fix data race between ServiceCache and K8sWatcher
correlateEndpoints was modifying a Backend object that was read by
K8sWatcher:

  WARNING: DATA RACE
  Read at 0x00c000942278 by goroutine 71:
    github.com/cilium/cilium/pkg/k8s/watchers.genCartesianProduct()
        /home/chris/code/cilium/cilium/pkg/k8s/watchers/watcher.go:787 +0xb56
    github.com/cilium/cilium/pkg/k8s/watchers.datapathSVCs()
        /home/chris/code/cilium/cilium/pkg/k8s/watchers/watcher.go:852 +0x77e
    github.com/cilium/cilium/pkg/k8s/watchers.(*K8sWatcher).addK8sSVCs()
    ...
  Previous write at 0x00c000942278 by goroutine 70:
    github.com/cilium/cilium/pkg/k8s.(*ServiceCache).correlateEndpoints()
        /home/chris/code/cilium/cilium/pkg/k8s/service_cache.go:501 +0x409
    github.com/cilium/cilium/pkg/k8s.(*ServiceCache).UpdateService()
        /home/chris/code/cilium/cilium/pkg/k8s/service_cache.go:214 +0x3e4
    github.com/cilium/cilium/pkg/k8s/watchers.(*K8sWatcherSuite).TestChangeSVCPort()
        /home/chris/code/cilium/cilium/pkg/k8s/watchers/watcher_test.go:685 +0x1d09
    ...

Since the backends are owned by the ServiceCache and allowing to mutate them when
holding a lock is something one might expect, fix the issues by making all public
ServiceCache methods return a DeepCopy'd *Endpoint and *Backends.

Fixes: #25071
Fixes: aa3c85fc7048 ("k8s: Add preferred attribute in Endpoints")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/k8s/endpoints.go
@@ -436,7 +436,7 @@ func (es *EndpointSlices) GetEndpoints() *Endpoints {
 			// example-custom-endpoints-g6r6v   IPv4          8090    10.244.1.49   28s
 			b, ok := allEps.Backends[backend]
 			if !ok {
-				allEps.Backends[backend] = ep
+				allEps.Backends[backend] = ep.DeepCopy()
 			} else {
 				clone := b.DeepCopy()
 				for k, v := range ep.Ports {

pkg/k8s/service_cache.go
@@ -499,7 +499,7 @@ func (s *ServiceCache) correlateEndpoints(id ServiceID) (*Endpoints, bool) {
 
 		for ip, e := range localEndpoints.Backends {
 			e.Preferred = svcFound && svc.IncludeExternal && svc.ServiceAffinity == serviceAffinityLocal
-			endpoints.Backends[ip] = e
+			endpoints.Backends[ip] = e.DeepCopy()
 		}
 	}
 
@@ -522,7 +522,7 @@ func (s *ServiceCache) correlateEndpoints(id ServiceID) (*Endpoints, bool) {
 						}).Warning("Conflicting service backend IP")
 					} else {
 						e.Preferred = svc.ServiceAffinity == serviceAffinityRemote
-						endpoints.Backends[ip] = e
+						endpoints.Backends[ip] = e.DeepCopy()
 					}
 				}
 			}