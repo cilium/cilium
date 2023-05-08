2582ce59c00c8c91deb377cc8a111ad6c23dfe04
Michi Mutsuzaki <michi@isovalent.com>
2023-05-08 18:11:28 +0000

    Remove az-binary check
    [ cherry-picked from cilium/cilium-cli repository ]
    
    I assert that az-binary check is a bit too excessive since it's not
    always required to have az binary installed when you are installing
    Cilium on AKS.
    
    Instead of forcing users to specify `--disable-check=az-binary` flag,
    let's just fail if az binary is needed but not found. This behavior is
    consistent with how we handle gcloud binary.
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- cilium-cli/.github/in-cluster-test-scripts/aks-byocni-install.sh
+++ cilium-cli/.github/in-cluster-test-scripts/aks-byocni-install.sh
@@ -6,7 +6,6 @@ set -e
 # Install Cilium
 cilium install \
   --version "${CILIUM_VERSION}" \
-  --disable-check=az-binary \
   --datapath-mode=aks-byocni \
   --wait=false \
   --helm-set loadBalancer.l7.backend=envoy \
