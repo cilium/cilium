cilium/cilium

commit 080857bdedca67d58ec39f8f96c5f38b22f6dc0b

cilium: Add scope knob for local address to be considered host id in ipcache
In some advanced environments, there may be devices in the hostns which could
have a link scoped 10.x.y.z address. The default behavior of Cilium when
populating its local ipcache is to skip all link local addresses as per
listLocalAddresses().

Depending on the datapath configuration, this may cause issues when a Pod
wants to talk to such an address in the hostns. For example, when routing
doesn't go via stack such as the case in BPF host routing, then for such
addresses, the ipcache will fall-back to WORLD id instead of HOST id. The
datapath then assumes that this needs to be xmitted to the given device
from tc layer instead of pushing traffic up the local stack as the case
with HOST id traffic. Then, if such device is f.e. a dummy dev, such traffic
is being blackholed.

We tested that changing scope to global for such address would make traffic
flow working, so the culprit really is in listLocalAddresses()'s logic
which unconditionally skips all addr.Scope == int(netlink.SCOPE_LINK).

Allow to customize this, given the kernel also allows many other scope
values. The agent gets a new --local-max-addr-scope param for this so
that e.g. link local scope can be included via `--local-max-addr-scope=253`
or via `--local-max-addr-scope=link`. To preserve the default, it's still
excluded.

Example, default:

  # ./daemon/cilium-agent --identity-allocation-mode=crd \
  --enable-ipv6=true --enable-ipv4=true --disable-envoy-version-check=true \
  --tunnel=disabled --k8s-kubeconfig-path=$HOME/.kube/config \
  --kube-proxy-replacement=strict --enable-l7-proxy=false \
  --auto-direct-node-routes=true --enable-bandwidth-manager=true \
  --ipv4-native-routing-cidr=10.91.0.0/16 --ipv6-native-routing-cidr=f00d::a5b:0:0:0/96 \
  --enable-ipv4-masquerade=false --enable-ipv6-masquerade=false

  root@zh-lab-node-1:~/go/src/github.com/cilium/cilium# ./cilium/cilium bpf ipcache list | grep "identity=1 "

pkg/datapath/linux/node_addressing.go
@@ -13,42 +13,45 @@ import (
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 // FIXME: This currently maps to the code in pkg/node/node_address.go. That
 // code should really move into this package.
 
 func listLocalAddresses(family int) ([]net.IP, error) {
+	var addresses []net.IP
+
 	ipsToExclude := node.GetExcludedIPs()
 	addrs, err := netlink.AddrList(nil, family)
 	if err != nil {
 		return nil, err
 	}
 
-	var addresses []net.IP
-
 	for _, addr := range addrs {
-		if addr.Scope == int(netlink.SCOPE_LINK) {
+		if addr.Scope > option.Config.AddressScopeMax {
 			continue
 		}
 		if ip.IsExcluded(ipsToExclude, addr.IP) {
 			continue
 		}
-		if addr.IP.IsLoopback() {
+		if addr.IP.IsLoopback() || addr.IP.IsLinkLocalUnicast() {
 			continue
 		}
 
 		addresses = append(addresses, addr.IP)
 	}
 
-	if hostDevice, err := netlink.LinkByName(defaults.HostDevice); hostDevice != nil && err == nil {
-		addrs, err = netlink.AddrList(hostDevice, family)
-		if err != nil {
-			return nil, err
-		}
-		for _, addr := range addrs {
-			if addr.Scope == int(netlink.SCOPE_LINK) {
-				addresses = append(addresses, addr.IP)
+	if option.Config.AddressScopeMax < int(netlink.SCOPE_LINK) {
+		if hostDevice, err := netlink.LinkByName(defaults.HostDevice); hostDevice != nil && err == nil {
+			addrs, err = netlink.AddrList(hostDevice, family)
+			if err != nil {
+				return nil, err
+			}
+			for _, addr := range addrs {
+				if addr.Scope == int(netlink.SCOPE_LINK) {
+					addresses = append(addresses, addr.IP)
+				}
 			}
 		}
 	}

daemon/cmd/daemon_main.go
@@ -585,6 +585,10 @@ func initializeFlags() {
 	flags.Bool(option.EnableSVCSourceRangeCheck, true, "Enable check of service source ranges (currently, only for LoadBalancer)")
 	option.BindEnv(option.EnableSVCSourceRangeCheck)
 
+	flags.String(option.AddressScopeMax, fmt.Sprintf("%d", defaults.AddressScopeMax), "Maximum local address scope for ipcache to consider host addresses")
+	flags.MarkHidden(option.AddressScopeMax)
+	option.BindEnv(option.AddressScopeMax)
+
 	flags.Bool(option.EnableBandwidthManager, false, "Enable BPF bandwidth manager")
 	option.BindEnv(option.EnableBandwidthManager)
 

pkg/defaults/defaults.go
@@ -5,11 +5,17 @@ package defaults
 
 import (
 	"time"
+
+	"github.com/vishvananda/netlink"
 )
 
 const (
 	// AgentHealthPort is the default value for option.AgentHealthPort
 	AgentHealthPort = 9876
+
+	// AddressScopeMax controls the maximum address scope for addresses to be
+	// considered local ones with HOST_ID in the ipcache
+	AddressScopeMax = int(netlink.SCOPE_LINK) - 1
 
 	// ClusterHealthPort is the default value for option.ClusterHealthPort
 	ClusterHealthPort = 4240

pkg/ip/ip.go
@@ -9,6 +9,9 @@ import (
 	"math/big"
 	"net"
 	"sort"
+	"strconv"
+
+	"github.com/vishvananda/netlink"
 )
 
 const (
@@ -855,6 +858,24 @@ func IsIPv4(ip net.IP) bool {
 // IsIPv6 returns if netIP is IPv6.
 func IsIPv6(ip net.IP) bool {
 	return ip != nil && ip.To4() == nil
+}
+
+// ParseScope returns the parsed address scope number.
+func ParseScope(scope string) (int, error) {
+	switch scope {
+	case "global":
+		return int(netlink.SCOPE_UNIVERSE), nil
+	case "nowhere":
+		return int(netlink.SCOPE_NOWHERE), nil
+	case "host":
+		return int(netlink.SCOPE_HOST), nil
+	case "link":
+		return int(netlink.SCOPE_LINK), nil
+	case "site":
+		return int(netlink.SCOPE_SITE), nil
+	default:
+		return strconv.Atoi(scope)
+	}
 }
 
 // SortIPList sorts the provided net.IP slice in place.

pkg/option/config.go
@@ -316,6 +316,10 @@ const (
 	// conflicting marks.
 	EnableIdentityMark = "enable-identity-mark"
 
+	// AddressScopeMax controls the maximum address scope for addresses to be
+	// considered local ones with HOST_ID in the ipcache
+	AddressScopeMax = "local-max-addr-scope"
+
 	// EnableBandwidthManager enables EDT-based pacing
 	EnableBandwidthManager = "enable-bandwidth-manager"
 
@@ -1855,6 +1859,10 @@ type DaemonConfig struct {
 	// features in BPF datapath
 	KubeProxyReplacement string
 
+	// AddressScopeMax controls the maximum address scope for addresses to be
+	// considered local ones with HOST_ID in the ipcache
+	AddressScopeMax int
+
 	// EnableBandwidthManager enables EDT-based pacing
 	EnableBandwidthManager bool
 
@@ -2864,6 +2872,15 @@ func (c *DaemonConfig) Populate() {
 		case TunnelGeneve:
 			c.TunnelPort = defaults.TunnelPortGeneve
 		}
+	}
+
+	if viper.IsSet(AddressScopeMax) {
+		c.AddressScopeMax, err = ip.ParseScope(viper.GetString(AddressScopeMax))
+		if err != nil {
+			log.WithError(err).Fatalf("Cannot parse scope integer from --%s option", AddressScopeMax)
+		}
+	} else {
+		c.AddressScopeMax = defaults.AddressScopeMax
 	}
 
 	nativeRoutingCIDR := viper.GetString(NativeRoutingCIDR)

commit dac638ece96400d92527de3b11b0d05e3533bfdf

datapath: link-local unicast addresses are "host"
This is a fix for a regression in the local addresses logic, introduced
in 080857bdedca67d58ec39f8f96c5f38b22f6dc0b as part of the
implementation for AddressScopeMax. Addresses with the form of
link-local unicast addresses began to be filtered out of the local
address aggregation, causing them to labeled with the "world" identity
for the sake of policy enforcement. Examples of such addresses include:

169.254.10.10
fe80::1234

This caused issues for a variety of users, whose policies allowing
"host" traffic would no longer allow traffic to these addresses, forcing
the use of workarounds involving CIDR policies, which is not the
intended behavior for this type of address. This was a regression as of
Cilium 1.12.0-rc2. One reason for this regression was that logic prior
to the change looked at the address scope, whereas logic after the
change looked at the address bytes, and it was found that many users had
assigned addresses of the forms above but with scope global, causing
them to again be filtered unconditionally.

This patch factors out the local address filtering logic into a
function, removes the skip over IsLinkLocalUnicast(), and adds a variety
of unit tests for that function.

fixes: https://github.com/cilium/cilium/issues/25242
fixes: https://github.com/cilium/cilium/issues/23910
fixes: https://github.com/cilium/cilium/issues/16308
fixes: https://github.com/cilium/cilium/issues/20055

Signed-off-by: Andrew Sauber <andrew.sauber@isovalent.com>

pkg/datapath/linux/node_addressing.go
@@ -28,20 +28,11 @@ func listLocalAddresses(family int) ([]net.IP, error) {
 		return nil, err
 	}
 
-	for _, addr := range addrs {
-		if addr.Scope > option.Config.AddressScopeMax {
-			continue
-		}
-		if ip.ListContainsIP(ipsToExclude, addr.IP) {
-			continue
-		}
-		if addr.IP.IsLoopback() || addr.IP.IsLinkLocalUnicast() {
-			continue
-		}
+	filteredIPs := filterLocalAddresses(addrs, ipsToExclude, option.Config.AddressScopeMax)
+	addresses = append(addresses, filteredIPs...)
 
-		addresses = append(addresses, addr.IP)
-	}
-
+	// If AddressScopeMax is a scope more broad (numerically less than) than SCOPE_LINK then include
+	// all addresses at SCOPE_LINK which are assigned to the Cilium host device.
 	if option.Config.AddressScopeMax < int(netlink.SCOPE_LINK) {
 		if hostDevice, err := netlink.LinkByName(defaults.HostDevice); hostDevice != nil && err == nil {
 			addrs, err = netlink.AddrList(hostDevice, family)
@@ -57,6 +48,28 @@ func listLocalAddresses(family int) ([]net.IP, error) {
 	}
 
 	return addresses, nil
+}
+
+func filterLocalAddresses(addrs []netlink.Addr, ipsToExclude []net.IP, addrScopeMax int) []net.IP {
+	var filteredIPs []net.IP
+	for _, addr := range addrs {
+		// This address is at a scope which is more narrow (numerically greater than) the configured
+		// max address scope. For example, if this addr is SCOPE_NOWHERE, and our addrScopeMax is
+		// SCOPE_LINK, then we do NOT treat the address as a local address. Similarly, if this addr
+		// is SCOPE_HOST, and our addrScopeMax is SCOPE_LINK, we do NOT treat the address as a local
+		// address.
+		if addr.Scope > addrScopeMax {
+			continue
+		}
+		if ip.ListContainsIP(ipsToExclude, addr.IP) {
+			continue
+		}
+		if addr.IP.IsLoopback() {
+			continue
+		}
+		filteredIPs = append(filteredIPs, addr.IP)
+	}
+	return filteredIPs
 }
 
 type addressFamilyIPv4 struct{}

pkg/datapath/linux/node_addressing_test.go
@@ -1,0 +1,300 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package linux
+
+import (
+	"net"
+	"reflect"
+	"testing"
+
+	"github.com/vishvananda/netlink"
+)
+
+func TestFilterLocalAddresses(t *testing.T) {
+	tests := []struct {
+		name         string
+		addrs        []netlink.Addr
+		ipsToExclude []net.IP
+		addrScopeMax int
+		want         []net.IP
+	}{
+		{
+			name: "simple",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+			},
+		},
+		{
+			name: "multiple",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+				net.ParseIP("10.0.0.2"),
+			},
+		},
+		{
+			name: "scopeMaxLink",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_LINK),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.3")},
+					Scope: int(netlink.SCOPE_NOWHERE),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_LINK),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+			},
+		},
+		{
+			name: "scopeMaxHost",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_LINK),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.3")},
+					Scope: int(netlink.SCOPE_NOWHERE),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+				net.ParseIP("10.0.0.2"),
+			},
+		},
+		{
+			name: "scopeMaxNowhere",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_LINK),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.3")},
+					Scope: int(netlink.SCOPE_NOWHERE),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_NOWHERE),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+				net.ParseIP("10.0.0.2"),
+				net.ParseIP("10.0.0.3"),
+			},
+		},
+		{
+			name: "exclude",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{
+				net.ParseIP("10.0.0.2"),
+			},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+			},
+		},
+		{
+			name: "excludeMultiple",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.3")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{
+				net.ParseIP("10.0.0.2"),
+				net.ParseIP("10.0.0.3"),
+			},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+			},
+		},
+		{
+			name: "ipv6 simple",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("2001:db8::")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("2001:db8::"),
+			},
+		},
+		{
+			name: "ipv6 multiple",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("2001:db8::")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("2600:beef::")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("2001:db8::"),
+				net.ParseIP("2600:beef::"),
+			},
+		},
+		{
+			name: "v4/v6 mix",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("2001:db8::")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+				net.ParseIP("2001:db8::"),
+			},
+		},
+		{
+			name: "v6 exclude",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("2001:db8::")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{
+				net.ParseIP("2001:db8::"),
+			},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+			},
+		},
+		{
+			name: "include link-local v4",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("169.254.20.10")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("169.254.169.254")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+				net.ParseIP("169.254.20.10"),
+				net.ParseIP("169.254.169.254"),
+			},
+		},
+		{
+			name: "include link-local v6",
+			addrs: []netlink.Addr{
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("fe80::")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+				{
+					IPNet: &net.IPNet{IP: net.ParseIP("fe80::1234")},
+					Scope: int(netlink.SCOPE_HOST),
+				},
+			},
+			ipsToExclude: []net.IP{},
+			addrScopeMax: int(netlink.SCOPE_HOST),
+			want: []net.IP{
+				net.ParseIP("10.0.0.1"),
+				net.ParseIP("fe80::"),
+				net.ParseIP("fe80::1234"),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := filterLocalAddresses(tt.addrs, tt.ipsToExclude, tt.addrScopeMax)
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("filterLocalAddresses(): got = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+}