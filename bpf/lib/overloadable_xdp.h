a7cfeb6dc7844b43b95228a07e0360fab9d3d23e
Paul Chaignon <paul.chaignon@gmail.com>
2023-05-14 23:16:14 +0200

    bpf: Don't encrypt on path hostns -> remote pod
    In pod-to-pod encryption with IPsec and tunneling, Cilium currently
    encrypts traffic on the path hostns -> remote pod even though traffic
    is in plain-text on the path remote pod -> hostns. When using native
    routing, neither of those paths is encrypted because traffic from the
    hostns doesn't go through the bpf_host BPF program.
    
    Cilium's Transparent Encryption with IPsec aims at encrypting pod-to-pod
    traffic. It is therefore unclear why we are encrypting traffic from the
    hostns. The simple fact that only one direction of the connection is
    encrypted begs the question of its usefulness. It's possible that this
    traffic was encrypted by mistake: some of this logic is necessary for
    node-to-node encryption with IPsec (not supported anymore) and
    pod-to-pod encryption may have been somewhat simplified to encrypt
    *-to-pod traffic.
    
    Encrypting traffic from the hostns nevertheless creates several issues.
    First, this situation creates a path asymmetry between the forward and
    reply paths of hostns<>remote pod connections. Path asymmetry issues are
    well known to be a source of bugs, from of '--ctstate INVALID -j DROP'
    iptables rules to NAT issues.
    
    Second, Gray recently uncovered a separate bug which, when combined with
    this encryption from hostns, can prevent Cilium from starting. That
    separate bug is still being investigated but it seems to cause the
    reload of bpf_host to depend on Cilium connecting to etcd in a
    clustermesh context. If this etcd is a remote pod, Cilium connects to it
    on hostns -> remote pod path. The bpf_host program being unloaded[1], it
    fails. We end up in a cyclic dependency: bpf_host requires connectivity
    to etcd, connectivity to etcd requires bpf_host.
    
    This commit therefore removes encryption with IPsec for the path hostns
    -> remote pod when using tunneling (already unencrypted when using
    native routing).
    
    1 - More specifically, in Gray's case, the bpf_host program is already
        loaded, but it needs to be reloaded because the IPsec XFRM config
        changed. Without this reload, encryption fails.
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- bpf/lib/overloadable_xdp.h
+++ bpf/lib/overloadable_xdp.h
@@ -39,12 +39,6 @@ set_encrypt_key_mark(struct xdp_md *ctx __maybe_unused, __u8 key __maybe_unused,
 }
 
 static __always_inline __maybe_unused void
-set_encrypt_key_meta(struct xdp_md *ctx __maybe_unused, __u8 key __maybe_unused,
-		     __u32 node_id __maybe_unused)
-{
-}
-
-static __always_inline __maybe_unused void
 ctx_set_cluster_id_mark(struct xdp_md *ctx __maybe_unused, __u32 cluster_id __maybe_unused)
 {
 }
