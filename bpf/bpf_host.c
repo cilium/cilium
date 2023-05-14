0551b5dfa61fe539eca20637d075fa4319855b51
Paul Chaignon <paul.chaignon@gmail.com>
2023-05-14 23:16:14 +0200

    bpf: Don't encrypt on path hostns -> remote pod
    [ upstream commit 5fe2b2d6da76d6b1d334f3ce9c0c59371b239892 ]
    
    In pod-to-pod encryption with IPsec and tunneling, Cilium currently
    encrypts traffic on the path hostns -> remote pod even though traffic
    is in plain-text on the path remote pod -> hostns. When using native
    routing, neither of those paths is encrypted because traffic from the
    hostns doesn't go through the bpf_host BPF program.
    
    Cilium's Transparent Encryption with IPsec aims at encrypting pod-to-pod
    traffic. It is therefore unclear why we are encrypting traffic from the
    hostns. The simple fact that only one direction of the connection is
    encrypted begs the question of its usefulness. It's possible that this
    traffic was encrypted by mistake: some of this logic is necessary for
    node-to-node encryption with IPsec (not supported anymore) and
    pod-to-pod encryption may have been somewhat simplified to encrypt
    *-to-pod traffic.
    
    Encrypting traffic from the hostns nevertheless creates several issues.
    First, this situation creates a path asymmetry between the forward and
    reply paths of hostns<>remote pod connections. Path asymmetry issues are
    well known to be a source of bugs, from of '--ctstate INVALID -j DROP'
    iptables rules to NAT issues.
    
    Second, Gray recently uncovered a separate bug which, when combined with
    this encryption from hostns, can prevent Cilium from starting. That
    separate bug is still being investigated but it seems to cause the
    reload of bpf_host to depend on Cilium connecting to etcd in a
    clustermesh context. If this etcd is a remote pod, Cilium connects to it
    on hostns -> remote pod path. The bpf_host program being unloaded[1], it
    fails. We end up in a cyclic dependency: bpf_host requires connectivity
    to etcd, connectivity to etcd requires bpf_host.
    
    This commit therefore removes encryption with IPsec for the path hostns
    -> remote pod when using tunneling (already unencrypted when using
    native routing).
    
    1 - More specifically, in Gray's case, the bpf_host program is already
        loaded, but it needs to be reloaded because the IPsec XFRM config
        changed. Without this reload, encryption fails.
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -22,9 +22,6 @@
 /* Pass unknown ICMPv6 NS to stack */
 #define ACTION_UNKNOWN_ICMP6_NS CTX_ACT_OK
 
-/* CB_PROXY_MAGIC overlaps with CB_ENCRYPT_MAGIC */
-#define ENCRYPT_OR_PROXY_MAGIC 0
-
 /* Controls the inclusion of the CILIUM_CALL_SEND_ICMP6_ECHO_REPLY section in
  * the bpf_lxc object file.
  */
@@ -288,18 +285,9 @@ skip_host_firewall:
 	dst = (union v6addr *) &ip6->daddr;
 	info = ipcache_lookup6(&IPCACHE_MAP, dst, V6_CACHE_KEY_LEN);
 	if (info != NULL && info->tunnel_endpoint != 0) {
-		ret = encap_and_redirect_with_nodeid(ctx, info->tunnel_endpoint,
-							 info->key, info->node_id,
-							 secctx, TRACE_PAYLOAD_LEN);
-
-		/* If IPSEC is needed recirc through ingress to use xfrm stack
-		 * and then result will routed back through bpf_netdev on egress
-		 * but with encrypt marks.
-		 */
-		if (ret == IPSEC_ENDPOINT)
-			return CTX_ACT_OK;
-		else
-			return ret;
+		return encap_and_redirect_with_nodeid(ctx, info->tunnel_endpoint,
+						      info->node_id, secctx,
+						      TRACE_PAYLOAD_LEN);
 	} else {
 		struct tunnel_key key = {};
 
@@ -554,14 +542,9 @@ handle_ipv4(struct __ctx_buff *ctx, __u32 secctx,
 #ifdef TUNNEL_MODE
 	info = ipcache_lookup4(&IPCACHE_MAP, ip4->daddr, V4_CACHE_KEY_LEN);
 	if (info != NULL && info->tunnel_endpoint != 0) {
-		ret = encap_and_redirect_with_nodeid(ctx, info->tunnel_endpoint,
-						     info->key, info->node_id,
-						     secctx, TRACE_PAYLOAD_LEN);
-
-		if (ret == IPSEC_ENDPOINT)
-			return CTX_ACT_OK;
-		else
-			return ret;
+		return encap_and_redirect_with_nodeid(ctx, info->tunnel_endpoint,
+						      info->node_id, secctx,
+						      TRACE_PAYLOAD_LEN);
 	} else {
 		/* IPv4 lookup key: daddr & IPV4_MASK */
 		struct tunnel_key key = {};
@@ -1052,16 +1035,13 @@ out:
 __section("to-host")
 int to_host(struct __ctx_buff *ctx)
 {
-	__u32 magic = ctx_load_meta(ctx, ENCRYPT_OR_PROXY_MAGIC);
+	__u32 magic = ctx_load_meta(ctx, CB_PROXY_MAGIC);
 	__u16 __maybe_unused proto = 0;
 	int ret = CTX_ACT_OK;
 	bool traced = false;
 	__u32 src_id = 0;
 
-	if ((magic & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_ENCRYPT) {
-		ctx->mark = magic; /* CB_ENCRYPT_MAGIC */
-		src_id = ctx_load_meta(ctx, CB_ENCRYPT_IDENTITY);
-	} else if ((magic & 0xFFFF) == MARK_MAGIC_TO_PROXY) {
+	if ((magic & 0xFFFF) == MARK_MAGIC_TO_PROXY) {
 		/* Upper 16 bits may carry proxy port number */
 		__be16 port = magic >> 16;
 
