1ba7d983c7a34aedad463b643807897a45007698
David Bimmler <david.bimmler@isovalent.com>
2023-05-15 14:12:31 +0200

    dp/types: split NodeNeighbors out of NodeHandler
    [ upstream commit 1aa06c6456d943badfcddb8c5e3765bdd128c469 ]
    
    [ backporter notes: conflics due to moved files ]
    
    The NodeHandler interface still contains two different concepts. Remove
    the NodeNeighbor discovery/handling from it, and delete the different
    stub implementations.
    
    Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

--- pkg/datapath/datapath.go
+++ pkg/datapath/datapath.go
@@ -19,6 +19,8 @@ type Datapath interface {
 
 	NodeIDs() NodeIDHandler
 
+	NodeNeighbors() NodeNeighbors
+
 	// LocalNodeAddressing must return the node addressing implementation
 	// of the local node
 	LocalNodeAddressing() types.NodeAddressing
