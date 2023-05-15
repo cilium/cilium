693605f99f6f3c8cfd27a630c7d5b651b03bf193
harsimran pabla <hpabla@isovalent.com>
2023-05-15 09:33:30 -0400

    bgpv1: Introduction of bgp component tests
    Introducing bgp control plane component tests. This change introduces
    test harness and basic BGP tests related to podCIDR advertisements, lb
    service advertisements and neighbor events.
    
    Motivation of this change is to increase test coverage around boundaries
    of BGP subsystem and also to validate actual peering state at high
    level for primary BGP features.
    
    Types of test cases to add here would range from exposed BGP features
    like route advertisements and various neighbor configuration settings.
    
    Signed-off-by: harsimran pabla <hpabla@isovalent.com>

---
+++ pkg/bgpv1/test/neighbor_test.go
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/bgpv1/types"
+	cilium_api_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/testutils"
+
+	"github.com/stretchr/testify/require"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// peeringState helper struct containing peering information of BGP neighbor
+type peeringState struct {
+	peerASN     uint32
+	peerAddr    string
+	peerSession string
+}
+
+// Test_NeighborAddDel validates neighbor add and delete are working as expected. Test validates this using
+// peering status which is reported from BGP control plane.
+// Topology - (BGP CP) === (2 x gobgp instances)
+func Test_NeighborAddDel(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	var steps = []struct {
+		description        string
+		neighbors          []cilium_api_v2alpha1.CiliumBGPNeighbor
+		waitState          []string
+		expectedPeerStates []peeringState
+	}{
+		{
+			description: "add two neighbors",
+			neighbors: []cilium_api_v2alpha1.CiliumBGPNeighbor{
+				{
+					PeerAddress: dummies[instance1Link].ipv4.String(),
+					PeerASN:     int(gobgpASN),
+				},
+				{
+					PeerAddress: dummies[instance2Link].ipv4.String(),
+					PeerASN:     int(gobgpASN2),
+				},
+			},
+			waitState: []string{"ESTABLISHED"},
+			expectedPeerStates: []peeringState{
+				{
+					peerASN:     gobgpASN,
+					peerAddr:    dummies[instance1Link].ipv4.Addr().String(),
+					peerSession: types.SessionEstablished.String(),
+				},
+				{
+					peerASN:     gobgpASN2,
+					peerAddr:    dummies[instance2Link].ipv4.Addr().String(),
+					peerSession: types.SessionEstablished.String(),
+				},
+			},
+		},
+		{
+			description:        "delete both neighbors",
+			neighbors:          []cilium_api_v2alpha1.CiliumBGPNeighbor{},
+			waitState:          []string{"IDLE", "ACTIVE"},
+			expectedPeerStates: []peeringState{},
+		},
+	}
+
+	testCtx, testDone := context.WithTimeout(context.Background(), maxTestDuration)
+	defer testDone()
+
+	// test setup, we configure two gobgp instances here.
+	gobgpInstances, fixture, cleanup, err := setup(testCtx, []gobgpConfig{gobgpConf, gobgpConf2}, fixtureConf)
+	require.NoError(t, err)
+	require.Len(t, gobgpInstances, 2)
+	defer cleanup()
+
+	for _, step := range steps {
+		t.Run(step.description, func(t *testing.T) {
+			// update bgp policy with neighbors defined in test step
+			policyObj := newPolicyObj(policyConfig{
+				nodeSelector: labels,
+				virtualRouters: []cilium_api_v2alpha1.CiliumBGPVirtualRouter{
+					{
+						LocalASN:      int(ciliumASN),
+						ExportPodCIDR: true,
+						Neighbors:     step.neighbors,
+					},
+				},
+			})
+			_, err = fixture.policyClient.Update(testCtx, &policyObj, meta_v1.UpdateOptions{})
+			require.NoError(t, err, step.description)
+
+			// wait for peers to reach expected state
+			for _, gobgpInstance := range gobgpInstances {
+				err = gobgpInstance.waitForSessionState(testCtx, step.waitState)
+				require.NoError(t, err, step.description)
+			}
+
+			// validate expected state vs state reported by BGP CP
+			var peers []*models.BgpPeer
+			peers, err = fixture.bgp.BGPMgr.GetPeers(testCtx)
+			require.NoError(t, err, step.description)
+
+			var runningState []peeringState
+			for _, peer := range peers {
+				runningState = append(runningState, peeringState{
+					peerASN:     uint32(peer.PeerAsn),
+					peerAddr:    peer.PeerAddress,
+					peerSession: peer.SessionState,
+				})
+			}
+
+			require.ElementsMatch(t, step.expectedPeerStates, runningState, step.description)
+		})
+	}
+}
