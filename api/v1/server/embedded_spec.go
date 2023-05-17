f6fbd84515a31fc0b8df6361d5de9b362923a892
Sebastian Wicki <sebastian@isovalent.com>
2023-05-17 15:50:14 +0200

    api: Add IPAM pool name to allocated addresses
    This adds the IPAM pool name to the AddressPair type which is both the
    result of an IPAM allocation and an argument of the EndpointCreate
    request. This allows the API client to know the pool effectively used to
    allocate an IP.  The caller needs to keep track of this, as it will have
    to pass the pool back to the IPAM subsystem when the IP is released.
    
    In Cilium, the IP is typically not released not by the CNI plugin, but
    directly by the endpoint package when the endpoint is deleted. This is
    why the field is added to the AddressPair type (which shared between the
    IPAM and endpoint types), rather than just to the IPAMAddressResponse.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- api/v1/server/embedded_spec.go
+++ api/v1/server/embedded_spec.go
@@ -1566,12 +1566,20 @@ func init() {
           "description": "UUID of IPv4 expiration timer",
           "type": "string"
         },
+        "ipv4-pool-name": {
+          "description": "IPAM pool from which this IPv4 address was allocated",
+          "type": "string"
+        },
         "ipv6": {
           "description": "IPv6 address",
           "type": "string"
         },
         "ipv6-expiration-uuid": {
           "description": "UUID of IPv6 expiration timer",
+          "type": "string"
+        },
+        "ipv6-pool-name": {
+          "description": "IPAM pool from which this IPv6 address was allocated",
           "type": "string"
         }
       }
@@ -6431,12 +6439,20 @@ func init() {
           "description": "UUID of IPv4 expiration timer",
           "type": "string"
         },
+        "ipv4-pool-name": {
+          "description": "IPAM pool from which this IPv4 address was allocated",
+          "type": "string"
+        },
         "ipv6": {
           "description": "IPv6 address",
           "type": "string"
         },
         "ipv6-expiration-uuid": {
           "description": "UUID of IPv6 expiration timer",
+          "type": "string"
+        },
+        "ipv6-pool-name": {
+          "description": "IPAM pool from which this IPv6 address was allocated",
           "type": "string"
         }
       }
