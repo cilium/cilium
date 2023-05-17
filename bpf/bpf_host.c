6e4cb719765587555c67b5abe4330fb55bc4ee67
Paul Chaignon <paul.chaignon@gmail.com>
2023-05-17 11:32:24 +0200

    bpf: Remove IPsec dead code in bpf_host
    [ upstream commit fd6fa25103d5170f294edd283393fe222c5fef8b ]
    
    TL;DR. this commit removes a bit of dead code that seems to have been
    intended for IPsec in native routing mode but is never actually
    executed.
    
    These code paths are only executed if going through cilium_host and
    coming from the host (see !from_host check above). For remote
    destinations, we only go through cilium_host if the destination is part
    of a remote pod CIDR and we are running in tunneling mode. In native
    routing mode, we go straight to the native device.
    
    Example routing table for tunneling (10.0.0.0/24 is the remote pod CIDR):
    
        10.0.0.0/24 via 10.0.1.61 dev cilium_host src 10.0.1.61 mtu 1373 <- we follow this
        10.0.1.0/24 via 10.0.1.61 dev cilium_host src 10.0.1.61
        10.0.1.61 dev cilium_host scope link
        192.168.56.0/24 dev enp0s8 proto kernel scope link src 192.168.56.11**
    
    Example routing table for native routing:
    
        10.0.0.0/24 via 192.168.56.12 dev enp0s8 <- we follow this
        10.0.1.0/24 via 10.0.1.61 dev cilium_host src 10.0.1.61
        10.0.1.61 dev cilium_host scope link
        192.168.56.0/24 dev enp0s8 proto kernel scope link src 192.168.56.11
    
    Thus, this code path is only used for tunneling with IPsec. However,
    IPsec in tunneling mode should already be handled by the
    encap_and_redirect_with_nodeid call above in the same functions (see
    info->key argument).
    
    So why was this added? It was added in commit b76e6eb59 ("cilium:
    ipsec, support direct routing modes") to support "direct routing modes".
    I found that very suspicious because, per the above, in native routing
    mode, traffic from the hostns shouldn't even go through cilium_host.
    
    I thus tested it out. I've checked IPsec with native routing mode, with
    and without endpoint routes. I can confirm that, in all those cases,
    traffic from the hostns is not encrypted when going to a remote pod.
    
    Therefore, this code is dead. I'm unsure when it died.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -333,15 +333,6 @@ skip_host_firewall:
 		/* See IPv4 comment. */
 		return DROP_UNROUTABLE;
 	}
-
-#ifdef ENABLE_IPSEC
-	if (info && info->key && info->tunnel_endpoint) {
-		__u8 key = get_min_encrypt_key(info->key);
-
-		set_encrypt_key_meta(ctx, key, info->node_id);
-		set_identity_meta(ctx, secctx);
-	}
-#endif
 	return CTX_ACT_OK;
 }
 
@@ -644,15 +635,6 @@ skip_vtep:
 		 */
 		return DROP_UNROUTABLE;
 	}
-
-#ifdef ENABLE_IPSEC
-	if (info && info->key && info->tunnel_endpoint) {
-		__u8 key = get_min_encrypt_key(info->key);
-
-		set_encrypt_key_meta(ctx, key, info->node_id);
-		set_identity_meta(ctx, secctx);
-	}
-#endif
 	return CTX_ACT_OK;
 }
 
