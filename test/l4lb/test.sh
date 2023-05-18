4774ab651797ae8b77e6dd22e0c84a941aee6ee8
Martynas Pumputis <m@lambda.lt>
2023-05-18 11:55:58 +0200

    test: Use DinD in L4LB / NAT46 tests
    The DinD for the tests was introduced in [1]. However, it never made
    into the v1.12 branch which made the GHA always to fail.
    
    Fix this by taking the test.sh files from the main branch.
    
    [1]: https://github.com/cilium/cilium/pull/22653
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/l4lb/test.sh
+++ test/l4lb/test.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 PS4='+[\t] '
 set -eux
 
+export LC_NUMERIC=C
+
 IMG_OWNER=${1:-cilium}
 IMG_TAG=${2:-latest}
-HELM_CHART_DIR=${3:-/vagrant/install/kubernetes/cilium}
 
 ###########
 #  SETUP  #
@@ -27,48 +28,52 @@ clang -O2 -Wall -target bpf -c bpf_xdp_veth_host.c -o bpf_xdp_veth_host.o
 # The program is taken from the Linux kernel selftests.
 clang -O2 -Wall -target bpf -c test_tc_tunnel.c -o test_tc_tunnel.o
 
-# With Kind we create two nodes cluster:
+# With Docker-in-Docker we create two nodes:
 #
-# * "kind-control-plane" runs cilium in the LB-only mode.
-# * "kind-worker" runs the nginx server.
-#
-# The LB cilium does not connect to the kube-apiserver. For now we use Kind
-# just to create Docker-in-Docker containers.
-kind create cluster --config kind-config.yaml --image=kindest/node:v1.24.3
+# * "lb-node" runs cilium in the LB-only mode.
+# * "nginx" runs the nginx server.
+
+docker network create cilium-l4lb
+docker run --privileged --name lb-node -d \
+    --network cilium-l4lb -v /lib/modules:/lib/modules \
+    docker:dind
+docker run --name nginx -d --network cilium-l4lb nginx
 
 # Create additional veth pair which is going to be used to test XDP_REDIRECT.
 ip l a l4lb-veth0 type veth peer l4lb-veth1
 SECOND_LB_NODE_IP=3.3.3.2
 ip a a "3.3.3.1/24" dev l4lb-veth0
-CONTROL_PLANE_PID=$(docker inspect kind-control-plane -f '{{ .State.Pid }}')
+CONTROL_PLANE_PID=$(docker inspect lb-node -f '{{ .State.Pid }}')
 ip l s dev l4lb-veth1 netns $CONTROL_PLANE_PID
 ip l s dev l4lb-veth0 up
 nsenter -t $CONTROL_PLANE_PID -n /bin/sh -c "\
     ip a a "${SECOND_LB_NODE_IP}/24" dev l4lb-veth1 && \
     ip l s dev l4lb-veth1 up"
 
-# Install Cilium as standalone L4LB
-helm install cilium ${HELM_CHART_DIR} \
-    --wait \
-    --namespace kube-system \
-    --set debug.enabled=true \
-    --set image.repository="quay.io/${IMG_OWNER}/cilium-ci" \
-    --set image.tag="${IMG_TAG}" \
-    --set image.useDigest=false \
-    --set image.pullPolicy=IfNotPresent \
-    --set operator.enabled=false \
-    --set loadBalancer.standalone=true \
-    --set loadBalancer.algorithm=maglev \
-    --set loadBalancer.mode=dsr \
-    --set loadBalancer.acceleration=native \
-    --set loadBalancer.dsrDispatch=ipip \
-    --set devices='{eth0,l4lb-veth1}' \
-    --set nodePort.directRoutingDevice=eth0 \
-    --set affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].key="kubernetes.io/hostname" \
-    --set affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].operator=In \
-    --set affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].values[0]=kind-control-plane
+# Wait until Docker is ready in the lb-node node
+while ! docker exec -t lb-node docker ps >/dev/null; do sleep 1; done
 
-IFIDX=$(docker exec -i kind-control-plane \
+# Install Cilium as standalone L4LB
+docker exec -t lb-node mount bpffs /sys/fs/bpf -t bpf
+docker exec -t lb-node \
+  docker run --name cilium-lb -td \
+    -v /sys/fs/bpf:/sys/fs/bpf \
+    -v /lib/modules:/lib/modules \
+    --privileged=true \
+    --network=host \
+    quay.io/${IMG_OWNER}/cilium-ci:${IMG_TAG} \
+    cilium-agent \
+    --enable-ipv4=true \
+    --enable-ipv6=false \
+    --datapath-mode=lb-only \
+    --bpf-lb-algorithm=maglev \
+    --bpf-lb-dsr-dispatch=ipip \
+    --bpf-lb-acceleration=native \
+    --bpf-lb-mode=dsr \
+    --devices="eth0,l4lb-veth1" \
+    --direct-routing-device=eth0
+
+IFIDX=$(docker exec -i lb-node \
     /bin/sh -c 'echo $(( $(ip -o l show eth0 | awk "{print $1}" | cut -d: -f1) ))')
 LB_VETH_HOST=$(ip -o l | grep "if$IFIDX" | awk '{print $2}' | cut -d@ -f1)
 ip l set dev $LB_VETH_HOST xdp obj bpf_xdp_veth_host.o
@@ -79,13 +84,16 @@ ip l set dev l4lb-veth0 xdp obj bpf_xdp_veth_host.o
 ethtool -K $LB_VETH_HOST rx off tx off
 ethtool -K l4lb-veth0 rx off tx off
 
-docker exec kind-worker /bin/sh -c 'apt-get update && apt-get install -y nginx && systemctl start nginx'
-WORKER_IP=$(docker exec kind-worker ip -o -4 a s eth0 | awk '{print $4}' | cut -d/ -f1)
-nsenter -t $(docker inspect kind-worker -f '{{ .State.Pid }}') -n /bin/sh -c \
+NGINX_PID=$(docker inspect nginx -f '{{ .State.Pid }}')
+WORKER_IP=$(nsenter -t $NGINX_PID -n ip -o -4 a s eth0 | awk '{print $4}' | cut -d/ -f1)
+WORKER_MAC=$(nsenter -t $NGINX_PID -n ip -o l show dev eth0 | grep -oP '(?<=link/ether )[^ ]+')
+
+# Install BPF program to terminate encapsulated packets coming from the LB
+nsenter -t $NGINX_PID -n /bin/sh -c \
     'tc qdisc add dev eth0 clsact && tc filter add dev eth0 ingress bpf direct-action object-file ./test_tc_tunnel.o section decap'
 
-CILIUM_POD_NAME=$(kubectl -n kube-system get pod -l k8s-app=cilium -o=jsonpath='{.items[0].metadata.name}')
-kubectl -n kube-system wait --for=condition=Ready pod "$CILIUM_POD_NAME" --timeout=5m
+# Wait until Cilium is ready
+while ! docker exec -t lb-node docker exec -t cilium-lb cilium status; do sleep 1; done
 
 ##########
 #  TEST  #
@@ -93,14 +101,19 @@ kubectl -n kube-system wait --for=condition=Ready pod "$CILIUM_POD_NAME" --timeo
 
 LB_VIP="10.0.0.2"
 
-nsenter -t $(docker inspect kind-worker -f '{{ .State.Pid }}') -n /bin/sh -c \
+nsenter -t $(docker inspect nginx -f '{{ .State.Pid }}') -n /bin/sh -c \
     "ip a a dev eth0 ${LB_VIP}/32"
 
-kubectl -n kube-system exec "${CILIUM_POD_NAME}" -- \
+docker exec -t lb-node docker exec -t cilium-lb \
     cilium service update --id 1 --frontend "${LB_VIP}:80" --backends "${WORKER_IP}:80" --k8s-node-port
 
-LB_NODE_IP=$(docker exec kind-control-plane ip -o -4 a s eth0 | awk '{print $4}' | cut -d/ -f1)
+LB_NODE_IP=$(docker exec lb-node ip -o -4 a s eth0 | awk '{print $4}' | cut -d/ -f1)
 ip r a "${LB_VIP}/32" via "$LB_NODE_IP"
+
+# Add the neighbor entry for the nginx node to avoid the LB failing to forward
+# the requests due to the FIB lookup drops (nsenter, as busybox iproute2
+# doesn't support neigh entries creation).
+nsenter -t $CONTROL_PLANE_PID -n ip neigh add ${WORKER_IP} dev eth0 lladdr ${WORKER_MAC}
 
 # Issue 10 requests to LB
 for i in $(seq 1 10); do
@@ -117,4 +130,6 @@ for i in $(seq 1 10); do
 done
 
 # Cleanup
-kind delete cluster
+docker rm -f lb-node
+docker rm -f nginx
+docker network rm cilium-l4lb
