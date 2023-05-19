c9856935159bb70c8bc47f2b66395e316bb655f5
Michi Mutsuzaki <michi@isovalent.com>
2023-05-19 17:12:18 +0000

    contrib/scripts: Ignore all vendor sub-directories
    Make these check scripts slightly more future-proof by ignoring all
    sub-directories named vendor.
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- contrib/scripts/rand-check.sh
+++ contrib/scripts/rand-check.sh
@@ -12,7 +12,7 @@ for l in rand\.NewSource; do
 	  -not -path "./_build/*" \
 	  -not -path "./contrib/*" \
 	  -not -path "./pkg/rand/*" \
-	  -not -path "./vendor/*" \
+	  -not -regex ".*/vendor/.*" \
 	  -not -path "./test/*" \
 	  -print0 \
 	  | xargs -0 grep --exclude NewSafeSource "$l")
