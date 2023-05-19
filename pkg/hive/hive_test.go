dd61492c4a2b653831e3bfa7aaba58f72afae8e9
David Bimmler <david.bimmler@isovalent.com>
2023-05-19 10:27:15 +0200

    hive: test: fix order of arguments in assert.Equal
    Confusingly, assert.Equal takes 'expected' before 'actual'. Fixing this
    helps making test errors easier to debug, since there assert explicitly
    calls out what is 'expected'.
    
    Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

--- pkg/hive/hive_test.go
+++ pkg/hive/hive_test.go
@@ -52,8 +52,8 @@ func TestHiveGoodConfig(t *testing.T) {
 	err = hive.Stop(context.TODO())
 	assert.NoError(t, err, "expected Stop to succeed")
 
-	assert.Equal(t, cfg.Foo, "test", "Config.Foo not set correctly")
-	assert.Equal(t, cfg.Bar, 13, "Config.Bar not set correctly")
+	assert.Equal(t, "test", cfg.Foo, "Config.Foo not set correctly")
+	assert.Equal(t, 13, cfg.Bar, "Config.Bar not set correctly")
 }
 
 // BadConfig has a field that matches no flags, and Flags
@@ -104,7 +104,7 @@ func TestHiveConfigOverride(t *testing.T) {
 	err = h.Stop(context.TODO())
 	assert.NoError(t, err, "expected Stop to succeed")
 
-	assert.Equal(t, cfg.Foo, "override", "Config.Foo not set correctly")
+	assert.Equal(t, "override", cfg.Foo, "Config.Foo not set correctly")
 }
 
 type SomeObject struct {
@@ -309,8 +309,8 @@ func TestRunRollback(t *testing.T) {
 
 	// We should see 2 start hooks invoked, and then 1 stop hook as first
 	// one is rolled back.
-	assert.Equal(t, started, 2)
-	assert.Equal(t, stopped, 1)
+	assert.Equal(t, 2, started)
+	assert.Equal(t, 1, stopped)
 }
 
 var shutdownOnStartCell = cell.Invoke(func(lc hive.Lifecycle, shutdowner hive.Shutdowner) {
