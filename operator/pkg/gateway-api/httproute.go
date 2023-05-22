e01be8c43f6ad2a2801689f3eec50430cbffa5ca
Tam Mach <tam.mach@cilium.io>
2023-05-22 15:28:45 +1000

    gateway-api: Race condition between routes and Gateway
    [ upstream commit 8507ecd1d0aaf699503270f4dc8f7133e75c091d ]
    
    There was a race condition between http/tls routes and the underlying
    Gateway resource. Basically, if the HTTP or TLS Route resource is created
    before Gateway resource, its status will be updated with the value
    `gateway not found`. However, the reconciliation for HTTP or TLS routes
    is not triggered even after mentioned Gateway is created. The issue is due
    to an unnecessary predicate on GenerationChangedPredicate, which only
    checks for changes in spec.
    
    This commit is to remove GenerationChangedPredicate predicate in HTTP and
    TLS routes.
    
    Status for gateway not found
    
    ```json
    {
      "lastTransitionTime": "2023-05-22T05:24:01Z",
      "message": "Gateway.gateway.networking.k8s.io \"gateway-tlsroute\" not found",
      "observedGeneration": 1,
      "reason": "InvalidTLSRoute",
      "status": "False",
      "type": "Accepted"
    }
    ```
    
    Testing was done before and after the changes with below step:
    
    - Create one TLSRoute with a non-existent Gateway. This TLSRoute status should say `gateway not found`
    - Create mentioned Gateway Resource with a valid GatewayClass
    - Verify the TLSRoute status, which should be updated to Accepted.
    
    Fixes: #25487
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/gateway-api/httproute.go
+++ operator/pkg/gateway-api/httproute.go
@@ -88,15 +88,13 @@ func (r *httpRouteReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	}
 
 	return ctrl.NewControllerManagedBy(mgr).
-		// Watch for changes to HTTPRoute, but not the status
-		For(&gatewayv1beta1.HTTPRoute{}, builder.WithPredicates(predicate.GenerationChangedPredicate{})).
+		// Watch for changes to HTTPRoute
+		For(&gatewayv1beta1.HTTPRoute{}).
 		// Watch for changes to Backend services
 		Watches(&source.Kind{Type: &corev1.Service{}}, r.enqueueRequestForBackendService()).
 		// Watch for changes to Gateways and enqueue HTTPRoutes that reference them,
-		// only if there is a change in the spec
 		Watches(&source.Kind{Type: &gatewayv1beta1.Gateway{}}, r.enqueueRequestForGateway(),
 			builder.WithPredicates(
-				predicate.GenerationChangedPredicate{},
 				predicate.NewPredicateFuncs(hasMatchingController(context.Background(), mgr.GetClient(), controllerName)))).
 		Complete(r)
 }
