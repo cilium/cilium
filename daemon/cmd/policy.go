cilium/cilium

commit 8f20d3bfd73b603e3ff8d2c81c11d54292f60ec5

daemon: Postpone ipcache upserts until after policy changes have been regenerated by endpoints.
Move ipcache CIDR upserts and releases to the policy reaction queue,
where upserts can be executed after regenerations have been completed,
i.e. after endpoint policy maps have been updated. This way IP
addresses are mapped to newly allocated identities only after endpoint
policy maps are ready to classify them.

Correspondingly, on deletes the to-be-deleted CIDR identities are
first deleted from ipcache so that when they are deleted from endpoint
policy maps they are no longer used in classification. Releases of
CIDR identities must still be serialized with ipcache upserts via the
policy reaction queue so that they are executed in the same order
w.r.t. ipcache upserts as policy deletes and adds.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

daemon/cmd/policy.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 	"github.com/cilium/cilium/pkg/eventqueue"
+	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/labels"
@@ -296,11 +297,12 @@ func (d *Daemon) policyAdd(sourceRules policyAPI.Rules, opts *policy.AddOptions,
 		}
 	}
 
-	// TODO: Collect new identities to be upserted to the ipcache only after all endpoints have
-	// been regenerated below. This would make sure that any CIDRs in the policy would be first
-	// pushed to the endpoint policies and then to the ipcache to avoid traffic mapping to an ID
-	// that the endpoint policy maps do not know about yet.
-	if _, err := ipcache.AllocateCIDRs(prefixes, nil); err != nil {
+	// Any newly allocated identities MUST be upserted to the ipcache if no error is returned.
+	// With SelectiveRegeneration this is postponed to the rule reaction queue to be done
+	// after the affected endpoints have been regenerated, otherwise new identities are
+	// upserted to the ipcache before we return.
+	newlyAllocatedIdentities := make(map[string]*identity.Identity)
+	if _, err := ipcache.AllocateCIDRs(prefixes, newlyAllocatedIdentities); err != nil {
 		_ = d.prefixLengths.Delete(prefixes)
 		metrics.PolicyImportErrorsTotal.Inc()
 		logger.WithError(err).WithField("prefixes", prefixes).Warn(
@@ -417,12 +419,16 @@ func (d *Daemon) policyAdd(sourceRules policyAPI.Rules, opts *policy.AddOptions,
 		// select any at all). The "reacting" to rule updates enqueues events
 		// for all endpoints. Once all endpoints have events queued up, this
 		// function will return.
-
+		//
+		// With selective regeneration upserting CIDRs to ipcache is performed after
+		// endpoint regeneration and serialized with the corresponding ipcache deletes via
+		// the policy reaction queue.
 		r := &PolicyReactionEvent{
 			wg:                &policySelectionWG,
 			epsToBumpRevision: endpointsToBumpRevision,
 			endpointsToRegen:  endpointsToRegen,
 			newRev:            newRev,
+			upsertIdentities:  newlyAllocatedIdentities,
 		}
 
 		ev := eventqueue.NewEvent(r)
@@ -436,6 +442,10 @@ func (d *Daemon) policyAdd(sourceRules policyAPI.Rules, opts *policy.AddOptions,
 	} else {
 		// Regenerate all endpoints unconditionally.
 		d.TriggerPolicyUpdates(false, "policy rules added")
+		// TODO: Remove 'enable-selective-regeneration' agent option.  Without selective
+		// regeneration we retain the old behavior of upserting new identities to ipcache
+		// before endpoint policy maps have been updated.
+		ipcache.UpsertGeneratedIdentities(newlyAllocatedIdentities)
 	}
 
 	return
@@ -449,6 +459,8 @@ type PolicyReactionEvent struct {
 	epsToBumpRevision *policy.EndpointSet
 	endpointsToRegen  *policy.EndpointSet
 	newRev            uint64
+	upsertIdentities  map[string]*identity.Identity // deferred CIDR identity upserts, if any
+	releasePrefixes   []*net.IPNet                  // deferred CIDR identity deletes, if any
 }
 
 // Handle implements pkg/eventqueue/EventHandler interface.
@@ -456,15 +468,24 @@ func (r *PolicyReactionEvent) Handle(res chan interface{}) {
 	// Wait until we have calculated which endpoints need to be selected
 	// across multiple goroutines.
 	r.wg.Wait()
-	reactToRuleUpdates(r.epsToBumpRevision, r.endpointsToRegen, r.newRev)
+	reactToRuleUpdates(r.epsToBumpRevision, r.endpointsToRegen, r.newRev, r.upsertIdentities, r.releasePrefixes)
 }
 
 // reactToRuleUpdates does the following:
 // * regenerate all endpoints in epsToRegen
 // * bump the policy revision of all endpoints not in epsToRegen, but which are
 //   in allEps, to revision rev.
-func reactToRuleUpdates(epsToBumpRevision, epsToRegen *policy.EndpointSet, rev uint64) {
+// * wait for the regenerations to be finished
+// * upsert or delete CIDR identities to the ipcache, as needed.
+func reactToRuleUpdates(epsToBumpRevision, epsToRegen *policy.EndpointSet, rev uint64, upsertIdentities map[string]*identity.Identity, releasePrefixes []*net.IPNet) {
 	var enqueueWaitGroup sync.WaitGroup
+
+	// Release CIDR identities before regenerations have been started, if any. This makes sure
+	// the stale identities are not used in policy map classifications after we regenerate the
+	// endpoints below.
+	if len(releasePrefixes) != 0 {
+		ipcache.ReleaseCIDRs(releasePrefixes)
+	}
 
 	// Bump revision of endpoints which don't need to be regenerated.
 	epsToBumpRevision.ForEachGo(&enqueueWaitGroup, func(epp policy.Endpoint) {
@@ -493,6 +514,13 @@ func reactToRuleUpdates(epsToBumpRevision, epsToRegen *policy.EndpointSet, rev u
 	})
 
 	enqueueWaitGroup.Wait()
+
+	// Upsert new identities after regeneration has completed, if any. This makes sure the
+	// policy maps are ready to classify packets using the newly allocated identities before
+	// they are upserted to the ipcache here.
+	if upsertIdentities != nil {
+		ipcache.UpsertGeneratedIdentities(upsertIdentities)
+	}
 }
 
 // PolicyDeleteEvent is a wrapper around deletion of policy rules with a given
@@ -596,7 +624,6 @@ func (d *Daemon) policyDelete(labels labels.LabelArray, res chan interface{}) {
 	// not appropriately performing garbage collection.
 	prefixes := policy.GetCIDRPrefixes(rules)
 	log.WithField("prefixes", prefixes).Debug("Policy deleted via API, found prefixes...")
-	ipcache.ReleaseCIDRs(prefixes)
 
 	prefixesChanged := d.prefixLengths.Delete(prefixes)
 	if !bpfIPCache.BackedByLPM() && prefixesChanged {
@@ -615,11 +642,16 @@ func (d *Daemon) policyDelete(labels labels.LabelArray, res chan interface{}) {
 	}
 
 	if option.Config.SelectiveRegeneration {
+		// With selective regeneration releasing prefixes from ipcache is serialized with
+		// the corresponding ipcache upserts via the policy reaction queue. Execution order
+		// w.r.t. to endpoint regenerations remains the same, endpoints are regenerated
+		// after any prefixes have been removed from the ipcache.
 		r := &PolicyReactionEvent{
 			wg:                &policySelectionWG,
 			epsToBumpRevision: epsToBumpRevision,
 			endpointsToRegen:  endpointsToRegen,
 			newRev:            rev,
+			releasePrefixes:   prefixes,
 		}
 
 		ev := eventqueue.NewEvent(r)
@@ -631,6 +663,7 @@ func (d *Daemon) policyDelete(labels labels.LabelArray, res chan interface{}) {
 			log.WithError(err).WithField(logfields.PolicyRevision, rev).Error("enqueue of RuleReactionEvent failed")
 		}
 	} else {
+		ipcache.ReleaseCIDRs(prefixes)
 		d.TriggerPolicyUpdates(true, "policy rules deleted")
 	}
 

commit 62ff9bee8ecfc6017a69ffbd09ab34c06c155900

daemon: Update code comment regarding PolicyReactionEvent
This code comment is confusing because it states that
reactToRuleUpdates() waits until all endpoints have been regenerated.

Upon code inspection, this is incorrect and not necessary either. It is
not necessary because 2820a1fc72 ("daemon: Switch policy over to new
IPCache interface") fixed the previously buggy behavior where prefixes
were pushed into the ipcache BPF map before the policy recalculation
(i.e. endpoint regeneration) has completed (endpoint policymap entries).
This buggy behavior likely caused CI flakes of some sort when new
policies were added.

The 8f20d3bfd7 commit mistakenly asserted this, so fix it in this
commit.

Fixes: 8f20d3bfd7 ("daemon: Postpone ipcache upserts until after policy
changes have been regenerated by endpoints.")

Reported-by: Joe Stringer <joe@cilium.io>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

daemon/cmd/policy.go
@@ -421,7 +421,7 @@ func (r *PolicyReactionEvent) Handle(res chan interface{}) {
 //   - regenerate all endpoints in epsToRegen
 //   - bump the policy revision of all endpoints not in epsToRegen, but which are
 //     in allEps, to revision rev.
-//   - wait for the regenerations to be finished
+//   - wait for the all endpoint regenerations to be _queued_.
 //   - upsert or delete CIDR identities to the ipcache, as needed.
 func (r *PolicyReactionEvent) reactToRuleUpdates(epsToBumpRevision, epsToRegen *policy.EndpointSet, rev uint64, upsertPrefixes, releasePrefixes []netip.Prefix) {
 	var enqueueWaitGroup sync.WaitGroup