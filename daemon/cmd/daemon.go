cilium/cilium

commit 2c878a512de941d3557965cdba0f02d84528bc90

cilium: auto-detect network interfaces for IPAMENI case
We need to auto-detect network interfaces in the EKS environment that by
default uses three interfaces eth0, eth1, and eth2. This patch will walk
the list of all devices in the system and attach to any device that is
not a veth or lo device.

This patch resolves the EKS case and leaves other cases for future work.
We could use similar logic with a bit more exclusion logic to handle the
general routing cases as well.

In the process we pull the IPAMENI IPSec configuration into Reinitialize()
path so that if net devices are added/removed we will pick up those at
reinit time. Later work can bring the remaining parts, but that required
more refactoring than I wanted for a bugfix.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/loader/base.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/datapath/alignchecker"
+	"github.com/cilium/cilium/pkg/datapath/linux/ethtool"
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	linuxrouting "github.com/cilium/cilium/pkg/datapath/linux/routing"
@@ -173,6 +174,49 @@ func addENIRules(sysSettings []sysctl.Setting, nodeAddressing datapath.NodeAddre
 	}
 
 	return retSettings, nil
+}
+
+// reinitializeIPSec is used to recompile and load encryption network programs.
+func (l *Loader) reinitializeIPSec(ctx context.Context) error {
+	if !option.Config.EnableIPSec {
+		return nil
+	}
+
+	interfaces := option.Config.EncryptInterface
+	if option.Config.IPAM == ipamOption.IPAMENI {
+		// IPAMENI mode supports multiple network facing interfaces that
+		// will all need Encrypt logic applied in order to decrypt any
+		// received encrypted packets. This logic will attach to all
+		// !veth devices. Only use if user has not configured interfaces.
+		if len(interfaces) == 0 {
+			if links, err := netlink.LinkList(); err == nil {
+				for _, link := range links {
+					isVirtual, err := ethtool.IsVirtualDriver(link.Attrs().Name)
+					if err == nil && !isVirtual {
+						interfaces = append(interfaces, link.Attrs().Name)
+					}
+				}
+			}
+		}
+
+		// For the ENI ipam mode on EKS, this will be the interface that
+		// the router (cilium_host) IP is associated to.
+		if len(option.Config.IPv4PodSubnets) == 0 {
+			if info := node.GetRouterInfo(); info != nil {
+				for _, c := range info.GetIPv4CIDRs() {
+					option.Config.IPv4PodSubnets = append(option.Config.IPv4PodSubnets, &c)
+				}
+			}
+		}
+	}
+
+	// No interfaces is valid in tunnel disabled case
+	if len(interfaces) != 0 {
+		if err := l.replaceNetworkDatapath(ctx, interfaces); err != nil {
+			return fmt.Errorf("failed to load encryption program: %w", err)
+		}
+	}
+	return nil
 }
 
 // Reinitialize (re-)configures the base datapath configuration including global
@@ -392,35 +436,8 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		log.Warning("Cannot check matching of C and Go common struct alignments due to old LLVM/clang version")
 	}
 
-	// Compile and load network programs, currently used for encryption.
-	if option.Config.EnableIPSec {
-		interfaces := option.Config.EncryptInterface
-
-		// For the ENI ipam mode on EKS, this will be the interface that
-		// the router (cilium_host) IP is associated to.
-		if option.Config.IPAM == ipamOption.IPAMENI {
-			if len(interfaces) == 0 {
-				if info := node.GetRouterInfo(); info != nil {
-					mac := info.GetMac()
-					iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
-					if err != nil {
-						log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
-					}
-					interfaces = append(interfaces, iface)
-				}
-			}
-			if len(option.Config.IPv4PodSubnets) == 0 {
-				if info := node.GetRouterInfo(); info != nil {
-					for _, c := range info.GetIPv4CIDRs() {
-						option.Config.IPv4PodSubnets = append(option.Config.IPv4PodSubnets, &c)
-					}
-				}
-				log.Info("Encryption IPv4PodSubnets in-use")
-			}
-		}
-		if err := l.replaceNetworkDatapath(ctx, interfaces); err != nil {
-			log.WithError(err).Fatal("failed to load encryption program")
-		}
+	if err := l.reinitializeIPSec(ctx); err != nil {
+		return err
 	}
 
 	if err := o.Datapath().Node().NodeConfigurationChanged(*o.LocalConfig()); err != nil {

daemon/cmd/daemon_main.go
@@ -1287,7 +1287,12 @@ func initEnv(cmd *cobra.Command) {
 		}
 	}
 
-	if option.Config.EnableIPSec && option.Config.Tunnel == option.TunnelDisabled && len(option.Config.EncryptInterface) == 0 {
+	// IPAMENI IPSec is configured from Reinitialize() to pull in devices
+	// that may be added or removed at runtime.
+	if option.Config.EnableIPSec &&
+		option.Config.Tunnel == option.TunnelDisabled &&
+		len(option.Config.EncryptInterface) == 0 &&
+		option.Config.IPAM != ipamOption.IPAMENI {
 		link, err := linuxdatapath.NodeDeviceNameWithDefaultRoute()
 		if err != nil {
 			log.WithError(err).Fatal("Ipsec default interface lookup failed, consider \"encrypt-interface\" to manually configure interface.")

pkg/datapath/linux/ethtool/ethtool.go
@@ -1,0 +1,97 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ethtool
+
+import (
+	"bytes"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+const (
+	SIOCETHTOOL      = 0x8946
+	ETHTOOL_GDRVINFO = 0x00000003 // Get driver info
+
+	IFNAMSIZ = 16
+)
+
+const (
+	veth = "veth"
+)
+
+type ifreq struct {
+	name [IFNAMSIZ]byte
+	data uintptr
+}
+
+type ethtoolDrvInfo struct {
+	cmd         uint32
+	driver      [32]byte
+	version     [32]byte
+	fwVersion   [32]byte
+	busInfo     [32]byte
+	eromVersion [32]byte
+	reserved2   [12]byte
+	nPrivFlags  uint32
+	nStats      uint32
+	testInfoLen uint32
+	eedumpLen   uint32
+	regdumpLen  uint32
+}
+
+func ethtoolIoctl(iface string, info *ethtoolDrvInfo) error {
+	var ifname [IFNAMSIZ]byte
+
+	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, unix.IPPROTO_IP)
+	if err != nil {
+		return err
+	}
+	copy(ifname[:], iface)
+	req := ifreq{
+		name: ifname,
+		data: uintptr(unsafe.Pointer(info)),
+	}
+	_, _, ep := unix.Syscall(unix.SYS_IOCTL, uintptr(fd), SIOCETHTOOL, uintptr(unsafe.Pointer(&req)))
+	if ep != 0 {
+		return ep
+	}
+	return nil
+}
+
+func GetDeviceName(iface string) (string, error) {
+	info := ethtoolDrvInfo{
+		cmd: ETHTOOL_GDRVINFO,
+	}
+
+	if err := ethtoolIoctl(iface, &info); err != nil {
+		return "", err
+	}
+	return string(bytes.Trim(info.driver[:], "\x00")), nil
+}
+
+func IsVirtualDriver(iface string) (bool, error) {
+	drvName, err := GetDeviceName(iface)
+	if err != nil {
+		return false, err
+	}
+
+	switch drvName {
+	case veth:
+		return true, nil
+	}
+
+	return true, nil
+}

commit 3c091cbfbe68ae0127ac390c638047b3245feabf

loader: Attach IPsec program to new ENI interfaces
We currently have a function reinitializeIPSec that detects any
interface likely to be an ENI interface and attaches bpf_network to it
to be able to decrypt IPsec traffic ingressing via that interface.
We however only run this function when the base datapath is reloaded
(Reinitialize). We don't currently reload the base datapath in reaction
to new ENI interfaces being added.

This commit changes that to ensure that, whenever we add a new ENI
interface, reinitializeIPSec is called and bpf_network attached to that
interface. To that end, if running IPsec in ENI mode, we subscribe the
datapath Loader to CiliumNode events. On update of CiliumNode objects,
we will check if any ENI was added and call reinitializeIPSec if that
is the case.

We can't simply call reinitializeIPSec when the new ENIs are added (vs.
when we see them propagated in CiliumNode) because the addition of ENI
interfaces happens in the operator and not in the agents.

reinitializeIPSec currently reloads bpf_network on all detected
interfaces, regardless of whether it is already attached or not. We
could probably optimize this to only attach to new interfaces, but given
this will only run when new ENIs are added, it probably doesn't matter
much. It probably also doesn't hurt to have this extra insurance that we
are always attached to all interfaces.

Fixes: 2c878a512de9 ("cilium: auto-detect network interfaces for IPAMENI case")
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

daemon/cmd/daemon.go
@@ -1187,6 +1187,13 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		return nil, restoredEndpoints, fmt.Errorf("error while initializing daemon: %w", err)
 	}
 
+	// Register the datapath loader as a CiliumNode subscriber if running IPsec
+	// and ENI mode so that we can attach the IPsec BPF program on new ENIs as
+	// they are added.
+	if option.Config.EnableIPSec && option.Config.IPAM == ipamOption.IPAMENI {
+		d.k8sWatcher.RegisterCiliumNodeSubscriber(d.Datapath().Loader())
+	}
+
 	// iptables rules can be updated only after d.init() intializes the iptables above.
 	err = d.updateDNSDatapathRules(d.ctx)
 	if err != nil {

pkg/datapath/fake/datapath.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/cilium/cilium/pkg/datapath/loader/metrics"
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
+	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/testutils/mockmaps"
 )
 
@@ -143,5 +145,17 @@ func (f *fakeLoader) CustomCallsMapPath(id uint16) string {
 
 // Reinitialize does nothing.
 func (f *fakeLoader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, deviceMTU int, iptMgr datapath.IptablesManager, p datapath.Proxy) error {
+	return nil
+}
+
+func (f *fakeLoader) OnUpdateCiliumNode(oldNode, newNode *ciliumv2.CiliumNode, _ *lock.StoppableWaitGroup) error {
+	return nil
+}
+
+func (f *fakeLoader) OnAddCiliumNode(node *ciliumv2.CiliumNode, swg *lock.StoppableWaitGroup) error {
+	return nil
+}
+
+func (f *fakeLoader) OnDeleteCiliumNode(node *ciliumv2.CiliumNode, swg *lock.StoppableWaitGroup) error {
 	return nil
 }

pkg/datapath/loader/base.go
@@ -27,7 +27,11 @@ import (
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/ipam"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
+	"github.com/cilium/cilium/pkg/k8s"
+	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/mac"
 	"github.com/cilium/cilium/pkg/node"
@@ -507,5 +511,65 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		}
 	}
 
+	return nil
+}
+
+// OnUpdateCiliumNode is only called if running with IPAM ENI and IPsec. In
+// that case, we need to rerun reinitializeIPSec whenever new ENIs are added to
+// the local node, to attach our BPF program for IPsec decryption to the new
+// interfaces.
+func (l *Loader) OnUpdateCiliumNode(oldNode, newNode *ciliumv2.CiliumNode, _ *lock.StoppableWaitGroup) error {
+	if !k8s.IsLocalCiliumNode(newNode) {
+		return nil
+	}
+
+	log.Debug("Checking for new ENIs on local node")
+
+	newENIs := make([]string, 0)
+	for newENI := range newNode.Status.ENI.ENIs {
+		existed := false
+		for oldENI := range oldNode.Status.ENI.ENIs {
+			if newENI == oldENI {
+				existed = true
+				break
+			}
+		}
+		if !existed {
+			newENIs = append(newENIs, newENI)
+		}
+	}
+
+	if len(newENIs) == 0 {
+		return nil
+	}
+
+	// ... and we can then trigger loading of BPF programs to those interfaces.
+	log.WithField(logfields.NewENIs, newENIs).Info("New ENIs detected")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// If we have any new ENIs, we first need to wait for the corresponding
+	// interfaces to show up...
+	missingENIByMac, err := ipam.WaitForENIInterfaces(oldNode, newNode)
+	if err != nil {
+		log.WithError(err).WithFields(logrus.Fields{
+			logfields.MissingENIs: missingENIByMac,
+		}).Error("Failed while waiting for ENIs to be attached")
+		return err
+	}
+
+	return l.reinitializeIPSec(ctx)
+}
+
+// OnAddCiliumNode doesn't need to perform anything contrary to
+// OnUpdateCiliumNode because we are not interested in new remote nodes.
+func (l *Loader) OnAddCiliumNode(node *ciliumv2.CiliumNode, swg *lock.StoppableWaitGroup) error {
+	return nil
+}
+
+// OnDeleteCiliumNode doesn't need to perform anything contrary to
+// OnUpdateCiliumNode because we are not interested in remote nodes.
+func (l *Loader) OnDeleteCiliumNode(node *ciliumv2.CiliumNode, swg *lock.StoppableWaitGroup) error {
 	return nil
 }

pkg/datapath/types/loader.go
@@ -9,6 +9,7 @@ import (
 	"net"
 
 	"github.com/cilium/cilium/pkg/datapath/loader/metrics"
+	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/lock"
 )
 
@@ -22,6 +23,9 @@ type Loader interface {
 	EndpointHash(cfg EndpointConfiguration) (string, error)
 	Unload(ep Endpoint)
 	Reinitialize(ctx context.Context, o BaseProgramOwner, deviceMTU int, iptMgr IptablesManager, p Proxy) error
+	OnUpdateCiliumNode(oldNode, newNode *ciliumv2.CiliumNode, swg *lock.StoppableWaitGroup) error
+	OnAddCiliumNode(node *ciliumv2.CiliumNode, swg *lock.StoppableWaitGroup) error
+	OnDeleteCiliumNode(node *ciliumv2.CiliumNode, swg *lock.StoppableWaitGroup) error
 }
 
 // BaseProgramOwner is any type for which a loader is building base programs.

pkg/logging/logfields/logfields.go
@@ -614,6 +614,9 @@ const (
 	// MissingENIs are the ENIs that we expected to be available but were not found.
 	MissingENIs = "missingENIs"
 
+	// NewENIs are ENIs that have been added to the CiliumNode.
+	NewENIs = "newENIs"
+
 	// Hint helps nudge the user in the right direction when troubleshooting.
 	Hint = "hint"
 