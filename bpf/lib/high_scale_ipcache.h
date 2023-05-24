2ad106afd88def430135566d7dff5f752d7c51f9
Paul Chaignon <paul.chaignon@gmail.com>
2023-05-24 11:23:44 +0200

    bpf: Don't encapsulate traffic to local pods in HS ipcache mode
    In high-scale IPcache mode, pods are excluded from the IPcache and we
    therefore can't rely on the IPcache to know when to encapsulate traffic.
    We decide to encapsulate or not based on a user-provided set of CIDRs.
    
    Of course, we never want to encapsulate traffic going to local pods. If
    endpoint routes are disabled, that is already the case: we will never go
    through the encapsulation logic as we will directly deliver packets to
    their destination endpoint with a bpf_redirect.
    
    If endpoint routes are enabled however, we need to check if the
    destination is a local endpoint, in which case we don't want to
    encapsulate. We do that in this commit using the local endpoint map
    which contains only endpoints hosted on the node.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- bpf/lib/high_scale_ipcache.h
+++ bpf/lib/high_scale_ipcache.h
@@ -29,6 +29,27 @@ world_cidrs_lookup4(__u32 addr)
 	return matches != NULL;
 }
 
+static __always_inline bool
+needs_encapsulation(__u32 addr)
+{
+# ifndef ENABLE_ROUTING
+	/* If endpoint routes are enabled, we need to check if the destination
+	 * is a local endpoint, in which case we don't want to encapsulate. If
+	 * endpoint routes are disabled, we don't need to check this because we
+	 * will never reach this point and the packet will be redirected to the
+	 * destination endpoint directly.
+	 */
+	if (__lookup_ip4_endpoint(addr))
+		return false;
+# endif /* ENABLE_ROUTING */
+	/* If the destination doesn't match one of the world CIDRs, we assume
+	 * it's destined to a remote pod. In that case, since the high-scale
+	 * ipcache is enabled, we want to encapsulate with the remote pod's IP
+	 * itself.
+	 */
+	return !world_cidrs_lookup4(addr);
+}
+
 static __always_inline int
 decapsulate_overlay(struct __ctx_buff *ctx, __u32 *src_id)
 {
