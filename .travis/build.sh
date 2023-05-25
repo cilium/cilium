4a88c09549c67840c2c4450e7adc9a67f1a50edd
Timo Beckers <timo@isovalent.com>
2023-05-25 09:32:22 +0200

    .travis: increase verbosity of 'make integration-tests'
    We're having issues with Travis jobs timing out due to no output being
    generated for over 10 minutes.
    
    Integration tests run with V=0 and --quiet barely generate any output,
    since all 'go test' output is hidden, and Makefile steps are also hidden.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,13 @@ set -o errexit
 
 export CFLAGS="-Werror"
 
-make -j 2 --quiet
-make integration-tests --quiet
+# Travis kills builds that don't generate any output for 10 minutes.
+# Set V=0 here to get GO/CHECK/CC lines, --quiet to hide long clang invocations.
+V=0 make -j 2 --quiet
+
+# Run with default verbosity here since this builds all Go code by running
+# 'go vet' and all integration tests. At least one line of output is generated
+# after each Go package is built and tested.
+make integration-tests
 
 exit 0
