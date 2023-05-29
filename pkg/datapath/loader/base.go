66f43838c5cd8151abcc2295b28016a5bf063041
Jussi Maki <jussi@isovalent.com>
2023-05-29 15:57:53 +0300

    loader: Reinitialize IPsec on device changes on ENI
    If IPsec is enabled along with the ENI IPAM mode we
    need to load the bpf_network program onto new ENI devices
    when they're added at runtime.
    
    To fix this, we subscribe to netlink link updates to detect when
    new (non-veth) devices are added and reinitialize IPsec to load the BPF
    program onto the devices. The compilation of the bpf_netowrk program
    has been moved to Reinitialize() to avoid spurious recompilation on
    reinitialize.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/datapath/loader/base.go
+++ pkg/datapath/loader/base.go
@@ -193,6 +193,9 @@ func (l *Loader) reinitializeIPSec(ctx context.Context) error {
 		return nil
 	}
 
+	l.ipsecMu.Lock()
+	defer l.ipsecMu.Unlock()
+
 	interfaces := option.Config.EncryptInterface
 	if option.Config.IPAM == ipamOption.IPAMENI {
 		// IPAMENI mode supports multiple network facing interfaces that
@@ -496,8 +499,19 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		log.WithError(err).Fatal("C and Go structs alignment check failed")
 	}
 
-	if err := l.reinitializeIPSec(ctx); err != nil {
-		return err
+	if option.Config.EnableIPSec {
+		if err := compileNetwork(ctx); err != nil {
+			log.WithError(err).Fatal("failed to compile encryption programs")
+		}
+
+		if err := l.reinitializeIPSec(ctx); err != nil {
+			return err
+		}
+
+		if firstInitialization {
+			// Start a background worker to reinitialize IPsec if links change.
+			l.reloadIPSecOnLinkChanges()
+		}
 	}
 
 	if err := l.reinitializeOverlay(ctx, encapProto); err != nil {
