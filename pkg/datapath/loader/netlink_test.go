9555fcf75dd604c9c101e66bb7010732b618626b
Robin Gögge <r.goegge@isovalent.com>
2023-05-30 11:29:06 +0200

    loader: convert netlink gocheck test suite
    Signed-off-by: Robin Gögge <r.goegge@isovalent.com>

--- pkg/datapath/loader/netlink_test.go
+++ pkg/datapath/loader/netlink_test.go
@@ -7,71 +7,75 @@ package loader
 
 import (
 	"fmt"
+	"testing"
 
-	. "github.com/cilium/checkmate"
+	"github.com/containernetworking/plugins/pkg/ns"
+	"github.com/stretchr/testify/require"
 	"github.com/vishvananda/netlink"
 
+	"github.com/cilium/cilium/pkg/netns"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/sysctl"
 	"github.com/cilium/cilium/pkg/testutils"
 )
 
-type NetlinkTestSuite struct {
-	prevConfigEnableIPv4 bool
-	prevConfigEnableIPv6 bool
-}
+func TestSetupDev(t *testing.T) {
+	testutils.PrivilegedTest(t)
 
-var _ = Suite(&NetlinkTestSuite{})
-
-func (s *NetlinkTestSuite) SetUpSuite(c *C) {
-	testutils.PrivilegedTest(c)
-
-	s.prevConfigEnableIPv4 = option.Config.EnableIPv4
-	s.prevConfigEnableIPv6 = option.Config.EnableIPv6
-
+	prevConfigEnableIPv4 := option.Config.EnableIPv4
+	prevConfigEnableIPv6 := option.Config.EnableIPv6
+	t.Cleanup(func() {
+		option.Config.EnableIPv4 = prevConfigEnableIPv4
+		option.Config.EnableIPv6 = prevConfigEnableIPv6
+	})
 	option.Config.EnableIPv4 = true
 	option.Config.EnableIPv6 = true
-}
 
-func (s *NetlinkTestSuite) TearDownSuite(c *C) {
-	option.Config.EnableIPv4 = s.prevConfigEnableIPv4
-	option.Config.EnableIPv6 = s.prevConfigEnableIPv6
-}
+	netnsName := "test-setup-dev"
+	netns0, err := netns.ReplaceNetNSWithName(netnsName)
+	require.NoError(t, err)
+	require.NotNil(t, netns0)
+	t.Cleanup(func() {
+		netns0.Close()
+		netns.RemoveNetNSWithName(netnsName)
+	})
 
-func (s *NetlinkTestSuite) TestSetupDev(c *C) {
-	ifName := "dummy9"
+	netns0.Do(func(_ ns.NetNS) error {
+		ifName := "dummy"
+		dummy := &netlink.Dummy{
+			LinkAttrs: netlink.LinkAttrs{
+				Name: ifName,
+			},
+		}
+		err := netlink.LinkAdd(dummy)
+		require.NoError(t, err)
 
-	dummy := &netlink.Dummy{
-		LinkAttrs: netlink.LinkAttrs{
-			Name: ifName,
-		},
-	}
-	err := netlink.LinkAdd(dummy)
-	c.Assert(err, IsNil)
+		err = setupDev(dummy)
+		require.NoError(t, err)
 
-	err = setupDev(dummy)
-	c.Assert(err, IsNil)
+		enabledSettings := []string{
+			fmt.Sprintf("net.ipv6.conf.%s.forwarding", ifName),
+			fmt.Sprintf("net.ipv4.conf.%s.forwarding", ifName),
+			fmt.Sprintf("net.ipv4.conf.%s.accept_local", ifName),
+		}
+		disabledSettings := []string{
+			fmt.Sprintf("net.ipv4.conf.%s.rp_filter", ifName),
+			fmt.Sprintf("net.ipv4.conf.%s.send_redirects", ifName),
+		}
+		for _, setting := range enabledSettings {
+			s, err := sysctl.Read(setting)
+			require.NoError(t, err)
+			require.Equal(t, s, "1")
+		}
+		for _, setting := range disabledSettings {
+			s, err := sysctl.Read(setting)
+			require.NoError(t, err)
+			require.Equal(t, s, "0")
+		}
 
-	enabledSettings := []string{
-		fmt.Sprintf("net.ipv6.conf.%s.forwarding", ifName),
-		fmt.Sprintf("net.ipv4.conf.%s.forwarding", ifName),
-		fmt.Sprintf("net.ipv4.conf.%s.accept_local", ifName),
-	}
-	disabledSettings := []string{
-		fmt.Sprintf("net.ipv4.conf.%s.rp_filter", ifName),
-		fmt.Sprintf("net.ipv4.conf.%s.send_redirects", ifName),
-	}
-	for _, setting := range enabledSettings {
-		s, err := sysctl.Read(setting)
-		c.Assert(err, IsNil)
-		c.Assert(s, Equals, "1")
-	}
-	for _, setting := range disabledSettings {
-		s, err := sysctl.Read(setting)
-		c.Assert(err, IsNil)
-		c.Assert(s, Equals, "0")
-	}
+		err = netlink.LinkDel(dummy)
+		require.NoError(t, err)
 
-	err = netlink.LinkDel(dummy)
-	c.Assert(err, IsNil)
+		return nil
+	})
 }
