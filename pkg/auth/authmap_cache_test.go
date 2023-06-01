a9aab4028a2c7f8290fa289917e41245ee3abea8
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-06-01 11:27:10 +0200

    auth: authmapCache.All() returns copy of map
    This commit lets authmapCache.All() returning a copy of the map of
    cached entries instead of the map instead. This is necessary to prevent
    issues when using the map without acquiring the lock.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/auth/authmap_cache_test.go
+++ pkg/auth/authmap_cache_test.go
@@ -33,4 +33,46 @@ func Test_authMapCache_restoreCache(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Len(t, am.cacheEntries, 1)
+
+	val, err := am.Get(authKey{
+		localIdentity:  1,
+		remoteIdentity: 2,
+		remoteNodeID:   10,
+		authType:       policy.AuthTypeDisabled,
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, val)
+}
+
+func Test_authMapCache_allReturnsCopy(t *testing.T) {
+	am := authMapCache{
+		authmap: &fakeAuthMap{
+			entries: map[authKey]authInfo{},
+		},
+		cacheEntries: map[authKey]authInfo{
+			{
+				localIdentity:  1,
+				remoteIdentity: 2,
+				remoteNodeID:   10,
+				authType:       policy.AuthTypeDisabled,
+			}: {
+				expiration: time.Now().Add(10 * time.Minute),
+			},
+		},
+	}
+
+	all, err := am.All()
+	assert.NoError(t, err)
+	assert.Len(t, all, 1)
+
+	all[authKey{
+		localIdentity:  10,
+		remoteIdentity: 20,
+		remoteNodeID:   100,
+		authType:       policy.AuthTypeDisabled,
+	}] = authInfo{
+		expiration: time.Now().Add(10 * time.Minute),
+	}
+	assert.Len(t, all, 2)
+	assert.Len(t, am.cacheEntries, 1)
 }
