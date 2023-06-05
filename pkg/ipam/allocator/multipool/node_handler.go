207903682ac5aa4aa60b06f7891242440ec27ad1
Tobias Klauser <tobias@cilium.io>
2023-06-05 17:08:26 +0200

    ipam/allocator/multipool: retry node allocations on all failures
    In case a CiliumPodIPPool didn't yet exist on resync, allocating a
    CIDR to a restored CiliumNode would currently fail without being
    retried. This e.g. leads to a bootstrapping problem when the
    cilium-operator didn't yet register the CiliumPodIPPool CRD with the k8s
    apiserver, but nodes would already come up before the CiliumPodIPPool
    they request their podCIDR from has been created.
    
    Fix this by keeping nodes in the map of nodes to be restored in case of
    all errors until either the resync is completed (and the controller
    would take care to consolidate the state) or the allocation succeeds.
    
    Co-authored-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- pkg/ipam/allocator/multipool/node_handler.go
+++ pkg/ipam/allocator/multipool/node_handler.go
@@ -24,6 +24,7 @@ type NodeHandler struct {
 	nodeUpdater ipam.CiliumNodeGetterUpdater
 
 	nodesPendingAllocation map[string]*v2.CiliumNode
+	restoreFinished        bool
 
 	controllerManager *controller.Manager
 }
@@ -68,57 +69,55 @@ func (n *NodeHandler) Delete(resource *v2.CiliumNode) {
 	n.controllerManager.RemoveController(controllerName(resource.Name))
 }
 
-func (n *NodeHandler) Resync(ctx context.Context, time time.Time) {
+func (n *NodeHandler) Resync(context.Context, time.Time) {
 	n.mutex.Lock()
 	defer n.mutex.Unlock()
+
 	n.poolManager.RestoreFinished()
 	for _, cn := range n.nodesPendingAllocation {
 		delete(n.nodesPendingAllocation, cn.Name)
-		n.upsertLocked(cn)
+		n.createUpsertController(cn)
 	}
+	n.restoreFinished = true
+	n.nodesPendingAllocation = nil
 }
 
 func (n *NodeHandler) upsertLocked(resource *v2.CiliumNode) bool {
-	err := n.poolManager.AllocateToNode(resource)
-	if err != nil {
-		if errors.Is(err, ErrAllocatorNotReady) {
-			n.nodesPendingAllocation[resource.Name] = resource
-			return false // try again later
-		} else {
-			log.WithField(logfields.NodeName, resource.Name).WithError(err).
-				Warning("Failed to allocate PodCIDRs to node")
-		}
+	if !n.restoreFinished {
+		n.nodesPendingAllocation[resource.Name] = resource
+		err := n.poolManager.AllocateToNode(resource)
+		return err == nil
 	}
+	n.createUpsertController(resource)
+	return true
+}
 
-	// refreshNode is set to true if the node needs to be refreshed before
-	// performing the update
-	refreshNode := false
-	// errorMessage is written to the resource status
-	errorMessage := ""
-	if err != nil {
-		errorMessage = err.Error()
-	}
-
+func (n *NodeHandler) createUpsertController(resource *v2.CiliumNode) {
+	// This controller serves two purposes:
+	// 1. It will retry allocations upon failure, e.g. if a pool does not exist yet.
+	// 2. Will try to synchronize the allocator's state with the CiliumNode CRD in k8s.
 	n.controllerManager.UpdateController(controllerName(resource.Name), controller.ControllerParams{
 		DoFunc: func(ctx context.Context) error {
-			if refreshNode {
-				resource, err = n.nodeUpdater.Get(resource.Name)
-				if err != nil {
-					return fmt.Errorf("failed to refresh node: %w", err)
-				}
+			// errorMessage is written to the resource status
+			errorMessage := ""
+			var controllerErr error
+
+			err := n.poolManager.AllocateToNode(resource)
+			if err != nil {
+				log.WithField(logfields.NodeName, resource.Name).WithError(err).
+					Warning("Failed to allocate PodCIDRs to node")
+				errorMessage = err.Error()
+				controllerErr = err
 			}
+
 			newResource := resource.DeepCopy()
 			newResource.Status.IPAM.OperatorStatus.Error = errorMessage
 
-			n.mutex.Lock()
 			newResource.Spec.IPAM.Pools.Allocated = n.poolManager.AllocatedPools(newResource.Name)
-			n.mutex.Unlock()
 
-			var controllerErr error
 			if !newResource.Spec.IPAM.Pools.DeepEqual(&resource.Spec.IPAM.Pools) {
 				_, err = n.nodeUpdater.Update(resource, newResource)
 				if err != nil {
-					refreshNode = true
 					controllerErr = errors.Join(controllerErr, fmt.Errorf("failed to update spec: %w", err))
 				}
 			}
@@ -126,7 +125,6 @@ func (n *NodeHandler) upsertLocked(resource *v2.CiliumNode) bool {
 			if !newResource.Status.IPAM.OperatorStatus.DeepEqual(&resource.Status.IPAM.OperatorStatus) {
 				_, err = n.nodeUpdater.UpdateStatus(resource, newResource)
 				if err != nil {
-					refreshNode = true
 					controllerErr = errors.Join(controllerErr, fmt.Errorf("failed to update status: %w", err))
 				}
 			}
@@ -134,7 +132,6 @@ func (n *NodeHandler) upsertLocked(resource *v2.CiliumNode) bool {
 			return controllerErr
 		},
 	})
-	return true
 }
 
 func controllerName(nodeName string) string {
