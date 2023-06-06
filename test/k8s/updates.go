0fcd96dc4b29c6967bd4e7064d4c2c9714e3ee69
Tim Horner <timothy.horner@isovalent.com>
2023-06-06 12:09:27 -0400

    test: set Cluster ID in upgrade-downgrade test
    Setting Cluster ID in the upgrade-downgrade test will provide test
    coverage for the updates made to BPF maps and the datapath to provide
    support for extended clustermesh. These changes include expanding the
    Cluster ID field on lxc, endpoint, lb4backend, and lb6backend maps, as
    well as modifications to skb->mark parsing for identity.
    
    Signed-off-by: Tim Horner <timothy.horner@isovalent.com>

--- test/k8s/updates.go
+++ test/k8s/updates.go
@@ -17,6 +17,19 @@ import (
 	"github.com/cilium/cilium/test/helpers"
 )
 
+const (
+	// cluster name and ID are set to provide coverage for changes to
+	// ClusterMesh, including updates to BPF map updates
+	clusterName = "test-upgrade-downgrade"
+	// clusterID of 170(10101010) was chosen to utilize all 8 bits of the
+	// default ClusterID size.
+	// note: It's possible to extend the size of ClusterID by setting
+	// MaxConnectedClusters, however this feature is only available to new
+	// clusters - thus upgrade and downgrade are not possible. Test coverage
+	// for this feature should be added after it is released.
+	clusterID = "170"
+)
+
 var (
 	// These are set in BeforeAll
 	demoPath         string
@@ -170,6 +183,8 @@ func InstallAndValidateCiliumUpgrades(kubectl *helpers.Kubectl, oldHelmChartVers
 			"sleepAfterInit":     "true",
 			"operator.enabled":   "false ",
 			"hubble.tls.enabled": "false",
+			"cluster.name":       clusterName,
+			"cluster.id":         clusterID,
 		}
 		if imageName != "" {
 			opts["image.repository"] = imageName
@@ -242,6 +257,8 @@ func InstallAndValidateCiliumUpgrades(kubectl *helpers.Kubectl, oldHelmChartVers
 			"operator.image.repository":              "quay.io/cilium/operator",
 			"hubble.relay.image.repository":          "quay.io/cilium/hubble-relay-ci",
 			"clustermesh.apiserver.image.repository": "quay.io/cilium/clustermesh-apiserver-ci",
+			"cluster.name":                           clusterName,
+			"cluster.id":                             clusterID,
 		}
 
 		hasNewHelmValues := versioncheck.MustCompile(">=1.12.90")
