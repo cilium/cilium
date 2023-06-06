7f5b43fb907cf9ace5d9acd74b93bd124de660ae
Chris Tarazi <chris@isovalent.com>
2023-06-06 11:35:05 -0700

    cilium/cmd: Deprecate `cilium endpoint regenerate` command
    As mentioned in GH-25948, let's deprecate this command and queue it for
    removal in v1.15.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- cilium/cmd/endpoint_regenerate.go
+++ cilium/cmd/endpoint_regenerate.go
@@ -6,16 +6,24 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
 // endpointRegenerateCmd represents the endpoint_regenerate command
 var endpointRegenerateCmd = &cobra.Command{
-	Use:    "regenerate <endpoint-id>",
-	Short:  "Force regeneration of endpoint program",
-	PreRun: requireEndpointID,
+	Use:   "regenerate <endpoint-id>",
+	Short: "Force regeneration of endpoint program",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		log.WithFields(logrus.Fields{
+			logfields.URL:         "https://github.com/cilium/cilium/issues/25948",
+			logfields.HelpMessage: "For more information, see the linked URL.",
+		}).Warn("This command is deprecated and will be removed in Cilium v1.15.")
+		requireEndpointID(cmd, args)
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		id := args[0]
 		cfg := &models.EndpointConfigurationSpec{}
