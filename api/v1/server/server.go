807b0197505dcaadd905c83e7c1627964e8f88fe
derailed <fernand.galiana@gmail.com>
2023-06-08 08:42:59 -0600

    Add agent health endpoint
    We need a new endpoint to collect modules health status via the agent api.
    This will be used initialy by the `cilium status` command to hydrate a new
    section listing out all instrumented modules health.
    
    - Update swagger config to surface health endpoint
    
    Signed-off-by: Fernand Galiana <fernand.galiana@isovalent.com>

--- api/v1/server/server.go
+++ api/v1/server/server.go
@@ -80,6 +80,7 @@ type serverParams struct {
 	PolicyGetFqdnCacheHandler            policy.GetFqdnCacheHandler
 	PolicyGetFqdnCacheIDHandler          policy.GetFqdnCacheIDHandler
 	PolicyGetFqdnNamesHandler            policy.GetFqdnNamesHandler
+	DaemonGetHealthHandler               daemon.GetHealthHandler
 	DaemonGetHealthzHandler              daemon.GetHealthzHandler
 	PolicyGetIPHandler                   policy.GetIPHandler
 	PolicyGetIdentityHandler             policy.GetIdentityHandler
@@ -139,6 +140,7 @@ func newForCell(p serverParams) (*Server, error) {
 	api.PolicyGetFqdnCacheHandler = p.PolicyGetFqdnCacheHandler
 	api.PolicyGetFqdnCacheIDHandler = p.PolicyGetFqdnCacheIDHandler
 	api.PolicyGetFqdnNamesHandler = p.PolicyGetFqdnNamesHandler
+	api.DaemonGetHealthHandler = p.DaemonGetHealthHandler
 	api.DaemonGetHealthzHandler = p.DaemonGetHealthzHandler
 	api.PolicyGetIPHandler = p.PolicyGetIPHandler
 	api.PolicyGetIdentityHandler = p.PolicyGetIdentityHandler
