cilium/cilium

commit eb653b60338e6da29c30746df53fa9d94e93dbbb

auth: use Resource.Observe for jobs
This commit replaces Resource.Events in combination with
stream.FromChannel by using Resource.Observe(...) directly.

This has the additional advantage that context handling is automatically
handled.

Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

pkg/auth/cell.go
@@ -4,7 +4,6 @@
 package auth
 
 import (
-	"context"
 	"fmt"
 	"runtime/pprof"
 	"time"
@@ -22,7 +21,6 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/resource"
 	"github.com/cilium/cilium/pkg/maps/authmap"
 	"github.com/cilium/cilium/pkg/signal"
-	"github.com/cilium/cilium/pkg/stream"
 )
 
 // Cell invokes authManager which is responsible for request authentication.
@@ -149,24 +147,15 @@ func registerSignalAuthenticationJob(jobGroup job.Group, mgr *authManager, sm si
 func registerGCJobs(jobGroup job.Group, mapGC *authMapGarbageCollector, params authManagerParams) {
 	// Add identities based auth gc if k8s client is enabled
 	if params.CiliumIdentities != nil {
-		jobGroup.Add(job.Observer("auth identities gc",
-			mapGC.handleCiliumIdentityEvent,
-			stream.FromChannel(params.CiliumIdentities.Events(context.Background())),
-		))
+		jobGroup.Add(job.Observer[resource.Event[*ciliumv2.CiliumIdentity]]("auth identities gc", mapGC.handleCiliumIdentityEvent, params.CiliumIdentities))
 	}
 
 	// Add node based auth gc if k8s client is enabled
 	if params.CiliumNodes != nil {
-		jobGroup.Add(job.Observer("auth nodes gc",
-			mapGC.handleCiliumNodeEvent,
-			stream.FromChannel(params.CiliumNodes.Events(context.Background())),
-		))
+		jobGroup.Add(job.Observer[resource.Event[*ciliumv2.CiliumNode]]("auth nodes gc", mapGC.handleCiliumNodeEvent, params.CiliumNodes))
 	}
 
-	jobGroup.Add(job.Timer("auth expiration gc",
-		mapGC.CleanupExpiredEntries,
-		params.Config.MeshAuthExpiredGCInterval,
-	))
+	jobGroup.Add(job.Timer("auth expiration gc", mapGC.CleanupExpiredEntries, params.Config.MeshAuthExpiredGCInterval))
 }
 
 type authHandlerResult struct {

commit 626c10f9d88dd2b4bc03af66758f85c19c16f884

auth: add missing stream package import
This commit adds back the stream package import, which appears to be
missing due to a merge race between #25927 and #25934.

Fixes: eb653b60338e ("auth: use Resource.Observe for jobs")
Fixes: ebb6fc38803a ("auth: implement re-authentication in case of rotated certificates")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/auth/cell.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/resource"
 	"github.com/cilium/cilium/pkg/maps/authmap"
 	"github.com/cilium/cilium/pkg/signal"
+	"github.com/cilium/cilium/pkg/stream"
 )
 
 // Cell invokes authManager which is responsible for request authentication.