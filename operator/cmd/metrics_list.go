cilium/cilium

commit 11d5856e170e2064a81d73efa60c032bb2b9c487

operator/api: Modularize api server
Modularize the operator api server into its own independent cell.  The
refactoring aims to keep the same behavior of the previous
implementation. Specifically, the refactored server:

- adds a custom handler for /healthz as an alias to /v1/healthz
- starts two HTTP servers, one listening on "127.0.0.1:0" and the other
  on "[::1]:0", in case the user is running ipv4 or ipv6 only

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

operator/cmd/root.go
@@ -108,11 +108,24 @@ var (
 			}
 		}),
 
+		cell.Provide(func(
+			operatorCfg *operatorOption.OperatorConfig,
+		) api.SharedConfig {
+			return api.SharedConfig{
+				EnableK8s: operatorCfg.EnableK8s,
+			}
+		}),
+
 		// These cells are started only after the operator is elected leader.
 		WithLeaderLifecycle(
 			// The CRDs registration should be the first operation to be invoked after the operator is elected leader.
 			client.RegisterCRDsCell,
 			k8s.SharedResourcesCell,
+
+			api.HealthHandlerCell(kvstoreEnabled),
+			api.MetricsHandlerCell,
+			api.ServerCell,
+
 			lbipam.Cell,
 			auth.Cell,
 			legacyCell,
@@ -140,11 +153,9 @@ func Execute() {
 }
 
 func registerOperatorHooks(lc hive.Lifecycle, llc *LeaderLifecycle, clientset k8sClient.Clientset, shutdowner hive.Shutdowner) {
-	apiServerShutdownSignal := make(chan struct{})
-
 	lc.Append(hive.Hook{
 		OnStart: func(hive.HookContext) error {
-			go runOperator(llc, clientset, shutdowner, apiServerShutdownSignal)
+			go runOperator(llc, clientset, shutdowner)
 			return nil
 		},
 		OnStop: func(ctx hive.HookContext) error {
@@ -152,7 +163,6 @@ func registerOperatorHooks(lc hive.Lifecycle, llc *LeaderLifecycle, clientset k8
 				return err
 			}
 			doCleanup()
-			close(apiServerShutdownSignal)
 			return nil
 		},
 	})
@@ -210,7 +220,6 @@ func initEnv() {
 	// Prepopulate option.Config with options from CLI.
 	option.Config.Populate(vp)
 	operatorOption.Config.Populate(vp)
-	operatorAddr = vp.GetString(operatorOption.OperatorAPIServeAddr)
 
 	// add hooks after setting up metrics in the option.Confog
 	logging.DefaultLogger.Hooks.Add(metrics.NewLoggingHook(components.CiliumOperatortName))
@@ -231,63 +240,14 @@ func doCleanup() {
 	leaderElectionCtxCancel()
 }
 
-func getAPIServerAddr() []string {
-	if operatorOption.Config.OperatorAPIServeAddr == "" {
-		return []string{"127.0.0.1:0", "[::1]:0"}
-	}
-	return []string{operatorOption.Config.OperatorAPIServeAddr}
-}
-
-// checkStatus checks the connection status to the kvstore and
-// k8s apiserver and returns an error if any of them is unhealthy
-func checkStatus(clientset k8sClient.Clientset) error {
-	if kvstoreEnabled() {
-		// We check if we are the leader here because only the leader has
-		// access to the kvstore client. Otherwise, the kvstore client check
-		// will block. It is safe for a non-leader to skip this check, as the
-		// it is the leader's responsibility to report the status of the
-		// kvstore client.
-		if leader, ok := IsLeader.Load().(bool); ok && leader {
-			if client := kvstore.Client(); client == nil {
-				return fmt.Errorf("kvstore client not configured")
-			} else if _, err := client.Status(); err != nil {
-				return err
-			}
-		}
-	}
-
-	if _, err := clientset.Discovery().ServerVersion(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // runOperator implements the logic of leader election for cilium-operator using
 // built-in leader election capbility in kubernetes.
 // See: https://github.com/kubernetes/client-go/blob/master/examples/leader-election/main.go
-func runOperator(lc *LeaderLifecycle, clientset k8sClient.Clientset, shutdowner hive.Shutdowner, apiServerShutdownSignal chan struct{}) {
+func runOperator(lc *LeaderLifecycle, clientset k8sClient.Clientset, shutdowner hive.Shutdowner) {
 	log.Infof("Cilium Operator %s", version.Version)
 
-	allSystemsGo := make(chan struct{})
 	IsLeader.Store(false)
 	leaderElectionCtx, leaderElectionCtxCancel = context.WithCancel(context.Background())
-
-	// Configure API server for the operator.
-	srv, err := api.NewServer(apiServerShutdownSignal, allSystemsGo, getAPIServerAddr()...)
-	if err != nil {
-		log.WithError(err).Fatalf("Unable to create operator apiserver")
-	}
-	close(allSystemsGo)
-
-	if operatorOption.Config.EnableK8s {
-		go func() {
-			err = srv.WithStatusCheckFunc(func() error { return checkStatus(clientset) }).StartServer()
-			if err != nil {
-				log.WithError(err).Fatalf("Unable to start operator apiserver")
-			}
-		}()
-	}
 
 	if operatorOption.Config.EnableMetrics {
 		operatorMetrics.Register()

operator/api/cell.go
@@ -3,6 +3,43 @@
 
 package api
 
+import (
+	"github.com/spf13/pflag"
+
+	"github.com/cilium/cilium/pkg/hive/cell"
+)
+
+const (
+	// OperatorAPIServeAddr is the "<ip>:<port>" on which to serve api requests
+	// from the operator.
+	// Use ":<port>" to bind on all interfaces.
+	// Use an empty string to bind on both "127.0.0.1:0" and "[::1]:0".
+	OperatorAPIServeAddr = "operator-api-serve-addr"
+)
+
+const (
+	// OperatorAPIServeAddrDefault is the default "<ip>:<port>" value on which to serve
+	// api requests from the operator.
+	OperatorAPIServeAddrDefault = "localhost:9234"
+)
+
+var ServerCell = cell.Module(
+	"cilium-operator-api",
+	"Cilium Operator API Server",
+
+	cell.Config(Config{}),
+	cell.Provide(newServer),
+	cell.Invoke(func(Server) {}),
+)
+
+type Config struct {
+	OperatorAPIServeAddr string
+}
+
+func (def Config) Flags(flags *pflag.FlagSet) {
+	flags.String(OperatorAPIServeAddr, OperatorAPIServeAddrDefault, "Address to serve API requests")
+}
+
 // SharedConfig contains the configuration that is shared between
 // this module and others.
 // This is done to avoid polluting this module with a direct dependency

operator/api/health.go
@@ -16,31 +16,6 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore"
 )
 
-type getHealthz struct {
-	*Server
-}
-
-// NewGetHealthzHandler handles health requests.
-func NewGetHealthzHandler(s *Server) operator.GetHealthzHandler {
-	return &getHealthz{Server: s}
-}
-
-// Handle handles GET requests for /healthz .
-func (h *getHealthz) Handle(params operator.GetHealthzParams) middleware.Responder {
-	select {
-	// only start serving the real health check once all systems all up and running
-	case <-h.Server.allSystemsGo:
-		if err := h.Server.checkStatus(); err != nil {
-			log.WithError(err).Warn("Health check status")
-
-			return operator.NewGetHealthzInternalServerError().WithPayload(err.Error())
-		}
-	default:
-	}
-
-	return operator.NewGetHealthzOK().WithPayload("ok")
-}
-
 type kvstoreEnabledFunc func() bool
 
 func HealthHandlerCell(kvstoreEnabled kvstoreEnabledFunc) cell.Cell {

operator/api/metrics.go
@@ -11,25 +11,6 @@ import (
 	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
-type getMetrics struct {
-	*Server
-}
-
-// NewGetMetricsHandler handles metrics requests.
-func NewGetMetricsHandler(s *Server) metrics.GetMetricsHandler {
-	return &getMetrics{Server: s}
-}
-
-// Handle handles GET requests for /metrics/ .
-func (h *getMetrics) Handle(params metrics.GetMetricsParams) middleware.Responder {
-	m, err := opMetrics.DumpMetrics()
-	if err != nil {
-		return metrics.NewGetMetricsFailed()
-	}
-
-	return metrics.NewGetMetricsOK().WithPayload(m)
-}
-
 var MetricsHandlerCell = cell.Module(
 	"metrics-handler",
 	"Operator metrics HTTP handler",

operator/api/server.go
@@ -4,7 +4,6 @@
 package api
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"net"
@@ -13,161 +12,158 @@ import (
 
 	"github.com/go-openapi/loads"
 	"github.com/go-openapi/runtime"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 
 	operatorApi "github.com/cilium/cilium/api/v1/operator/server"
 	"github.com/cilium/cilium/api/v1/operator/server/restapi"
+	"github.com/cilium/cilium/api/v1/operator/server/restapi/metrics"
 	"github.com/cilium/cilium/api/v1/operator/server/restapi/operator"
-	"github.com/cilium/cilium/pkg/logging"
-	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
-var (
-	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "cilium-operator-api")
+type Server interface {
+	// Ports returns the ports at which the server is listening
+	Ports() []int
+}
 
-	noOpFunc = func() error {
-		return nil
-	}
-)
+// params contains all the dependencies for the api server.
+// They will be provided through dependency injection.
+type params struct {
+	cell.In
 
-// Server is the type corresponding to cilium-operator apiserver.
-type Server struct {
+	Cfg       Config
+	SharedCfg SharedConfig
+
+	HealthHandler  operator.GetHealthzHandler
+	MetricsHandler metrics.GetMetricsHandler
+
+	Logger     logrus.FieldLogger
+	Lifecycle  hive.Lifecycle
+	Shutdowner hive.Shutdowner
+}
+
+type server struct {
 	operatorApi.Server
 
-	shutdownSignal <-chan struct{}
-	allSystemsGo   <-chan struct{}
+	logger     logrus.FieldLogger
+	shutdowner hive.Shutdowner
 
-	checkStatus func() error
+	address  string
+	httpSrvs []httpServer
 
-	// This is the /healthz handler outside of the open-api spec.
-	healthzHandler *getHealthz
-
-	listenAddrs []string
+	healthHandler  operator.GetHealthzHandler
+	metricsHandler metrics.GetMetricsHandler
 }
 
-// newServer instantiates a new instance of the cilium-operator API server.
-func (s *Server) newServer(spec *loads.Document) *operatorApi.Server {
-	api := restapi.NewCiliumOperatorAPI(spec)
-	api.Logger = log.Debugf
-
-	// Register API handlers for the operator apiserver
-	api.OperatorGetHealthzHandler = NewGetHealthzHandler(s)
-	api.MetricsGetMetricsHandler = NewGetMetricsHandler(s)
-
-	srv := operatorApi.NewServer(api)
-	srv.EnabledListeners = []string{"http"}
-
-	srv.ConfigureAPI()
-
-	return srv
+type httpServer struct {
+	address  string
+	listener net.Listener
+	server   *http.Server
 }
 
-// NewServer creates a server to handle cilium-operator requests.
-func NewServer(shutdownSignal <-chan struct{}, allSystemsGo <-chan struct{}, addrs ...string) (*Server, error) {
-	server := &Server{
-		listenAddrs: addrs,
-
-		shutdownSignal: shutdownSignal,
-		allSystemsGo:   allSystemsGo,
-
-		checkStatus: noOpFunc,
+func newServer(
+	p params,
+) (Server, error) {
+	if !p.SharedCfg.EnableK8s {
+		return nil, nil
 	}
 
-	server.healthzHandler = &getHealthz{Server: server}
-
-	swaggerSpec, err := loads.Analyzed(operatorApi.SwaggerJSON, "")
-	if err != nil {
-		return nil, err
+	server := &server{
+		logger:         p.Logger,
+		shutdowner:     p.Shutdowner,
+		address:        p.Cfg.OperatorAPIServeAddr,
+		healthHandler:  p.HealthHandler,
+		metricsHandler: p.MetricsHandler,
 	}
+	p.Lifecycle.Append(server)
 
-	server.Server = *server.newServer(swaggerSpec)
 	return server, nil
 }
 
-// WithStatusCheckFunc returns the server configuring the check status function
-// to return the health of the operator.
-func (s *Server) WithStatusCheckFunc(f func() error) *Server {
-	s.checkStatus = f
-	return s
-}
-
-// StartServer starts the HTTP listeners for the apiserver.
-func (s *Server) StartServer() error {
-	errs := make(chan error, 1)
-	nServers := 0
-
-	// Since we are opening this on localhost only, we need to make sure
-	// we can open for both v4 and v6 localhost. In case the user is running
-	// v4-only or v6-only.
-	for _, addr := range s.listenAddrs {
-		if addr == "" {
-			continue
-		}
-		nServers++
-
-		mux := http.NewServeMux()
-
-		// Index handler is the the handler for Open-API router.
-		mux.Handle("/", s.Server.GetHandler())
-		// Create a custom handler for /healthz as an alias to /v1/healthz. A http mux
-		// is required for this because open-api spec does not allow multiple base paths
-		// to be specified.
-		mux.HandleFunc("/healthz", func(rw http.ResponseWriter, _ *http.Request) {
-			resp := s.healthzHandler.Handle(operator.GetHealthzParams{})
-			resp.WriteResponse(rw, runtime.TextProducer())
-		})
-
-		srv := &http.Server{
-			Addr:    addr,
-			Handler: mux,
-		}
-		errCh := make(chan error, 1)
-
-		lc := net.ListenConfig{Control: setsockoptReuseAddrAndPort}
-		ln, err := lc.Listen(context.Background(), "tcp", addr)
-		if err != nil {
-			log.WithError(err).Fatalf("Unable to listen on %s for healthz apiserver", addr)
-		}
-
-		go func() {
-			err := srv.Serve(ln)
-			if err != nil {
-				// If the error is due to the server being shutdown, then send nil to
-				// the server errors channel.
-				if errors.Is(err, http.ErrServerClosed) {
-					log.WithField("address", addr).Debug("Operator API server closed")
-					errs <- nil
-				} else {
-					errCh <- err
-					errs <- err
-				}
-			}
-		}()
-
-		go func() {
-			select {
-			case <-s.shutdownSignal:
-				if err := srv.Shutdown(context.Background()); err != nil {
-					log.WithError(err).Error("apiserver shutdown")
-				}
-			case err := <-errCh:
-				log.WithError(err).Warn("Unable to start operator API server")
-			}
-		}()
-
-		log.Infof("Starting apiserver on address %s", addr)
+func (s *server) Start(ctx hive.HookContext) error {
+	spec, err := loads.Analyzed(operatorApi.SwaggerJSON, "")
+	if err != nil {
+		return err
 	}
 
-	var retErr error
-	for err := range errs {
-		if err != nil {
-			retErr = err
-		}
+	api := restapi.NewCiliumOperatorAPI(spec)
+	api.Logger = s.logger.Debugf
+	api.OperatorGetHealthzHandler = s.healthHandler
+	api.MetricsGetMetricsHandler = s.metricsHandler
 
-		nServers--
-		if nServers == 0 {
-			return retErr
+	srv := operatorApi.NewServer(api)
+	srv.EnabledListeners = []string{"http"}
+	srv.ConfigureAPI()
+	s.Server = *srv
+
+	mux := http.NewServeMux()
+
+	// Index handler is the the handler for Open-API router.
+	mux.Handle("/", s.Server.GetHandler())
+	// Create a custom handler for /healthz as an alias to /v1/healthz. A http mux
+	// is required for this because open-api spec does not allow multiple base paths
+	// to be specified.
+	mux.HandleFunc("/healthz", func(rw http.ResponseWriter, _ *http.Request) {
+		resp := s.healthHandler.Handle(operator.GetHealthzParams{})
+		resp.WriteResponse(rw, runtime.TextProducer())
+	})
+
+	if s.address == "" {
+		// Since we are opening this on localhost only, we need to make sure
+		// we can open for both v4 and v6 localhost, in case the user is running
+		// v4-only or v6-only.
+		s.httpSrvs = make([]httpServer, 2)
+		s.httpSrvs[0].address = "127.0.0.1:0"
+		s.httpSrvs[1].address = "[::1]:0"
+	} else {
+		s.httpSrvs = make([]httpServer, 1)
+		s.httpSrvs[0].address = s.address
+	}
+
+	var errs []error
+	for i := range s.httpSrvs {
+		lc := net.ListenConfig{Control: setsockoptReuseAddrAndPort}
+		ln, err := lc.Listen(ctx, "tcp", s.httpSrvs[i].address)
+		if err != nil {
+			errs = append(errs, fmt.Errorf("unable to listen on %s: %w", s.httpSrvs[i].address, err))
+			continue
 		}
+		s.httpSrvs[i].listener = ln
+
+		s.logger.WithFields(logrus.Fields{
+			fmt.Sprintf("address-%d", i): ln.Addr().String(),
+		})
+
+		s.httpSrvs[i].server = &http.Server{
+			Addr:    s.httpSrvs[i].address,
+			Handler: mux,
+		}
+	}
+
+	// if no apiserver can be started, we stop the cell
+	if (len(s.httpSrvs) == 1 && s.httpSrvs[0].server == nil) ||
+		(len(s.httpSrvs) == 2 && s.httpSrvs[0].server == nil && s.httpSrvs[1].server == nil) {
+		s.shutdowner.Shutdown()
+		return errors.Join(errs...)
+	}
+
+	// otherwise just log any possible error and continue
+	for _, err := range errs {
+		s.logger.WithError(err).Error("apiserver start failed")
+	}
+
+	for _, srv := range s.httpSrvs {
+		if srv.server == nil {
+			continue
+		}
+		go func(srv httpServer) {
+			if err := srv.server.Serve(srv.listener); !errors.Is(err, http.ErrServerClosed) {
+				s.logger.WithError(err).Error("server stopped unexpectedly")
+				s.shutdowner.Shutdown()
+			}
+		}(srv)
 	}
 
 	return nil
@@ -176,6 +172,30 @@ func (s *Server) StartServer() error {
 // setsockoptReuseAddrAndPort sets the SO_REUSEADDR and SO_REUSEPORT socket options on c's
 // underlying socket in order to improve the chance to re-bind to the same address and port
 // upon restart.
+func (s *server) Stop(ctx hive.HookContext) error {
+	for _, srv := range s.httpSrvs {
+		if srv.server == nil {
+			continue
+		}
+		if err := srv.server.Shutdown(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *server) Ports() []int {
+	ports := make([]int, 0, len(s.httpSrvs))
+	for _, srv := range s.httpSrvs {
+		if srv.server == nil {
+			continue
+		}
+		ports = append(ports, srv.listener.Addr().(*net.TCPAddr).Port)
+	}
+	return ports
+}
+
+// setsockoptReuseAddrAndPort sets SO_REUSEADDR and SO_REUSEPORT
 func setsockoptReuseAddrAndPort(network, address string, c syscall.RawConn) error {
 	var soerr error
 	if err := c.Control(func(su uintptr) {
@@ -187,11 +207,10 @@ func setsockoptReuseAddrAndPort(network, address string, c syscall.RawConn) erro
 			soerr = fmt.Errorf("failed to setsockopt(SO_REUSEADDR): %w", err)
 			return
 		}
-		// Allow reuse of recently-used ports. This gives the agent a
+
+		// Allow reuse of recently-used ports. This gives the operator a
 		// better chance to re-bind upon restarts.
-		if err := unix.SetsockoptInt(s, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
-			soerr = fmt.Errorf("failed to Setsockopt(SO_REUSEPORT): %w", err)
-		}
+		soerr = unix.SetsockoptInt(s, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
 	}); err != nil {
 		return err
 	}

operator/api/server_test.go
@@ -1,0 +1,148 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"go.uber.org/goleak"
+
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
+)
+
+func TestAPIServerDisabled(t *testing.T) {
+	defer goleak.VerifyNone(
+		t,
+		// ignore goroutine started from sigs.k8s.io/controller-runtime/pkg/log.go init function
+		goleak.IgnoreTopFunction("time.Sleep"),
+	)
+
+	var testSrv Server
+
+	hive := hive.New(
+		k8sClient.FakeClientCell,
+		MetricsHandlerCell,
+		HealthHandlerCell(func() bool {
+			return false
+		}),
+		cell.Provide(func() SharedConfig {
+			return SharedConfig{
+				EnableK8s: false,
+			}
+		}),
+		cell.Provide(func() Config {
+			return Config{
+				OperatorAPIServeAddr: "localhost:0",
+			}
+		}),
+
+		cell.Provide(newServer),
+
+		cell.Invoke(func(srv Server) {
+			testSrv = srv
+		}),
+	)
+
+	if err := hive.Start(context.Background()); err != nil {
+		t.Fatalf("failed to start: %s", err)
+	}
+
+	if testSrv != nil {
+		t.Fatalf("listeners unexpectedly started on ports %v", testSrv.Ports())
+	}
+
+	if err := hive.Stop(context.Background()); err != nil {
+		t.Fatalf("failed to stop: %s", err)
+	}
+}
+
+func TestAPIServerEnabled(t *testing.T) {
+	defer goleak.VerifyNone(
+		t,
+		// ignore goroutine started from sigs.k8s.io/controller-runtime/pkg/log.go init function
+		goleak.IgnoreTopFunction("time.Sleep"),
+	)
+
+	var testSrv Server
+
+	hive := hive.New(
+		k8sClient.FakeClientCell,
+		MetricsHandlerCell,
+		HealthHandlerCell(func() bool {
+			return false
+		}),
+		cell.Provide(func() SharedConfig {
+			return SharedConfig{
+				EnableK8s: true,
+			}
+		}),
+		cell.Provide(func() Config {
+			return Config{
+				OperatorAPIServeAddr: "localhost:0",
+			}
+		}),
+
+		cell.Provide(newServer),
+
+		cell.Invoke(func(srv Server) {
+			testSrv = srv
+		}),
+	)
+
+	if err := hive.Start(context.Background()); err != nil {
+		t.Fatalf("failed to start: %s", err)
+	}
+
+	if len(testSrv.Ports()) != 1 {
+		t.Fatalf("expected a single opened port, got: %v", len(testSrv.Ports()))
+	}
+	port := testSrv.Ports()[0]
+
+	if err := testEndpoint(t, port, "/v1/healthz"); err != nil {
+		t.Fatalf("failed to query endpoint: %s", err)
+	}
+	if err := testEndpoint(t, port, "/v1/metrics"); err != nil {
+		t.Fatalf("failed to query endpoint: %s", err)
+	}
+	if err := testEndpoint(t, port, "/healthz"); err != nil {
+		t.Fatalf("failed to query endpoint: %s", err)
+	}
+
+	if err := hive.Stop(context.Background()); err != nil {
+		t.Fatalf("failed to stop: %s", err)
+	}
+}
+
+func testEndpoint(t *testing.T, port int, path string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodGet,
+		fmt.Sprintf("http://localhost:%d%s", port, path),
+		nil,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create http request: %s", err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("http request for healthz failed: %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("expected http status code %d, got: %d", http.StatusOK, res.StatusCode)
+	}
+
+	return nil
+}

operator/cmd/flags.go
@@ -243,9 +243,6 @@ func init() {
 	flags.String(operatorOption.OperatorPrometheusServeAddr, operatorOption.PrometheusServeAddr, "Address to serve Prometheus metrics")
 	option.BindEnv(Vp, operatorOption.OperatorPrometheusServeAddr)
 
-	flags.String(operatorOption.OperatorAPIServeAddr, "localhost:9234", "Address to serve API requests")
-	option.BindEnv(Vp, operatorOption.OperatorAPIServeAddr)
-
 	flags.Bool(operatorOption.SyncK8sServices, true, "Synchronize Kubernetes services to kvstore")
 	option.BindEnv(Vp, operatorOption.SyncK8sServices)
 

operator/option/config.go
@@ -87,10 +87,6 @@ const (
 	// NodesGCInterval is the duration for which the cilium nodes are GC.
 	NodesGCInterval = "nodes-gc-interval"
 
-	// OperatorAPIServeAddr IP:Port on which to serve api requests in
-	// operator (pass ":Port" to bind on all interfaces, "" is off)
-	OperatorAPIServeAddr = "operator-api-serve-addr"
-
 	// OperatorPrometheusServeAddr IP:Port on which to serve prometheus
 	// metrics (pass ":Port" to bind on all interfaces, "" is off).
 	OperatorPrometheusServeAddr = "operator-prometheus-serve-addr"
@@ -343,7 +339,6 @@ type OperatorConfig struct {
 	// will simply return.
 	EndpointGCInterval time.Duration
 
-	OperatorAPIServeAddr        string
 	OperatorPrometheusServeAddr string
 
 	// SyncK8sServices synchronizes k8s services into the kvstore
@@ -571,7 +566,6 @@ func (c *OperatorConfig) Populate(vp *viper.Viper) {
 	c.CNPStatusCleanupBurst = vp.GetInt(CNPStatusCleanupBurst)
 	c.EnableMetrics = vp.GetBool(EnableMetrics)
 	c.EndpointGCInterval = vp.GetDuration(EndpointGCInterval)
-	c.OperatorAPIServeAddr = vp.GetString(OperatorAPIServeAddr)
 	c.OperatorPrometheusServeAddr = vp.GetString(OperatorPrometheusServeAddr)
 	c.SyncK8sServices = vp.GetBool(SyncK8sServices)
 	c.SyncK8sNodes = vp.GetBool(SyncK8sNodes)

commit 113d8bf1a56850025b74b60e05f029b4eeccd7a4

operator: Fix default API server addr in metrics subcommand
The operator API server has been modularized into its independent cell
in a previous commit.

Doing that, the flag to specify a custom address for the server has been
moved from the global operator config to the new cell config.

The `metrics list` subcommand needs to know the operator API server
address to query the "/metrics" endpoint.  Before the modularization,
the subcommand was at least able to query the server if that was started
at the default address.

This commit adds a new flag to specify the address of the operator API
server. Also, the default value for this flag is set to the same default
used in the operator/api server cell.  Doing that fixes the bug
introduced in the modularization and also allows to specify another
address if the operator API server is listening on a non-default one.

Fixes: 11d5856e17 ("operator/api: Modularize api server")

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

operator/cmd/metrics_list.go
@@ -15,10 +15,14 @@ import (
 
 	"github.com/cilium/cilium/api/v1/operator/client"
 	"github.com/cilium/cilium/api/v1/operator/models"
+	"github.com/cilium/cilium/operator/api"
 	"github.com/cilium/cilium/pkg/command"
 )
 
-var matchPattern string
+var (
+	matchPattern string
+	operatorAddr string
+)
 
 // MetricsListCmd dumps all metrics into stdout
 var MetricsListCmd = &cobra.Command{
@@ -74,5 +78,6 @@ func init() {
 	MetricsCmd.AddCommand(MetricsListCmd)
 
 	MetricsListCmd.Flags().StringVarP(&matchPattern, "match-pattern", "p", "", "Show only metrics whose names match matchpattern")
+	MetricsListCmd.Flags().StringVarP(&operatorAddr, "server-address", "s", api.OperatorAPIServeAddrDefault, "Address of the operator API server")
 	command.AddOutputOption(MetricsListCmd)
 }

operator/cmd/root.go
@@ -74,8 +74,6 @@ var (
 	leaderElectionCtx       context.Context
 	leaderElectionCtxCancel context.CancelFunc
 
-	operatorAddr string
-
 	// isLeader is an atomic boolean value that is true when the Operator is
 	// elected leader. Otherwise, it is false.
 	isLeader atomic.Bool