cilium/cilium

commit 3bffa18e520da7bed034fa503365b0a3bfc49fa3

operator: Add core logic for IPAM pools
This commit adds the core logic for allocating pod CIDRs from specific
pools to nodes. The logic is not yet hooked up with the CiliumNode
watcher, that will follow in a subsequent commit.

The main idea behind the `PoolAllocator` is that it tracks both the
available IP pools, but also which node has allocated which CIDR from
which pool. Each pool is implemented as a slice of `CIDRAllocators`
(based on the `github.com/cilium/ipam/cidrset` package), which means it
manages the free-lists for each pool, but does not keep track of who
allocated the CIDR. This is done separately to easily be able to obtain
a list of CIDRs allocated to a node, without having to iterate over all
pools.

As with other IPAM allocators, a main concern is state restoration after
the operator has been restarted. The approach we take here is similar to
the one used by clusterpool (v1): In an initial phase (indicated by
`PoolAllocator.ready=false`), the watcher passes in all known
CiliumNodes to `AllocateToNode`. This will cause the `PoolAllocator` to
mark any previously (before the operator restart) allocated CIDR as
occupied. Once all CiliumNodes have been observed, the flip is switched
via `RestoreFinished`, at which point the watcher will have to invoke
`AllocateToNode` again for every node, allowing new CIDRs now to be
allocated to that node if necessary. This ensures that we make sure we
know what CIDRs have been previously allocated before we hand out new
CIDRs.

Co-authored-by: Tobias Klauser <tobias@cilium.io>
Signed-off-by: Tobias Klauser <tobias@cilium.io>
Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/ipam/allocator/multipool/pool_allocator.go
@@ -1,0 +1,410 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package multipool
+
+import (
+	"errors"
+	"fmt"
+	"math/big"
+	"net/netip"
+	"sort"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/exp/slices"
+
+	"github.com/cilium/cilium/pkg/ipam"
+	"github.com/cilium/cilium/pkg/ipam/allocator/clusterpool/cidralloc"
+	"github.com/cilium/cilium/pkg/ipam/types"
+	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/option"
+)
+
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "ipam-allocator-clusterpool-v2")
+
+type cidrPool struct {
+	v4 []cidralloc.CIDRAllocator
+	v6 []cidralloc.CIDRAllocator
+}
+
+type cidrSet map[netip.Prefix]struct{}
+
+func (c cidrSet) PodCIDRSlice() []types.IPAMPodCIDR {
+	cidrs := make([]types.IPAMPodCIDR, 0, len(c))
+	for cidr := range c {
+		cidrs = append(cidrs, types.IPAMPodCIDR(cidr.String()))
+	}
+	slices.Sort(cidrs)
+	return cidrs
+}
+
+// availableAddrs returns the number of available addresses in this set
+func (c cidrSet) availableAddrs() *big.Int {
+	total := big.NewInt(0)
+	for p := range c {
+		total.Add(total, addrsInPrefix(p))
+	}
+	return total
+}
+
+type cidrSets struct {
+	v4 cidrSet
+	v6 cidrSet
+}
+
+type poolToCIDRs map[string]cidrSets // poolName -> list of allocated CIDRs
+
+type errAllocatorNotReady struct{}
+
+var ErrAllocatorNotReady = errAllocatorNotReady{}
+
+func (m errAllocatorNotReady) Error() string {
+	return "allocator not ready"
+}
+
+func (m errAllocatorNotReady) Is(target error) bool {
+	return errors.Is(target, ErrAllocatorNotReady)
+}
+
+func addrsInPrefix(p netip.Prefix) *big.Int {
+	// compute number of addresses in prefix, i.e. 2^bits
+	addrs := new(big.Int)
+	addrs.Lsh(big.NewInt(1), uint(p.Addr().BitLen()-p.Bits()))
+	// subtract network and broadcast address, which are not available for
+	// allocation in the cilium/ipam library for now
+	addrs.Sub(addrs, big.NewInt(2))
+	if addrs.Sign() < 0 {
+		return big.NewInt(0)
+	}
+
+	return addrs
+}
+
+type PoolAllocator struct {
+	pools map[string]cidrPool    // poolName -> pool
+	nodes map[string]poolToCIDRs // nodeName -> pool -> cidrs
+	ready bool
+}
+
+func NewPoolAllocator() *PoolAllocator {
+	return &PoolAllocator{
+		pools: map[string]cidrPool{},
+		nodes: map[string]poolToCIDRs{},
+	}
+}
+
+func (p *PoolAllocator) RestoreFinished() {
+	p.ready = true
+}
+
+func (p *PoolAllocator) AddPool(poolName string, ipv4CIDRs []string, ipv4MaskSize int, ipv6CIDRs []string, ipv6MaskSize int) error {
+	if _, ok := p.pools[poolName]; ok {
+		return fmt.Errorf("pool %q already exists", poolName)
+	}
+
+	v4, err := cidralloc.NewCIDRSets(false, ipv4CIDRs, ipv4MaskSize)
+	if err != nil {
+		return err
+	}
+
+	v6, err := cidralloc.NewCIDRSets(true, ipv6CIDRs, ipv6MaskSize)
+	if err != nil {
+		return err
+	}
+
+	p.pools[poolName] = cidrPool{
+		v4: v4,
+		v6: v6,
+	}
+
+	return nil
+}
+
+func (p *PoolAllocator) AllocateToNode(cn *v2.CiliumNode) error {
+	// We first need to check for CIDRs which we want to occupy, i.e. mark as
+	// allocated to the node. This needs to happen before allocations, to avoid
+	// handing out the same CIDR twice.
+	var err error
+
+	allocatedSet := make(map[string]map[netip.Prefix]struct{}, len(cn.Spec.IPAM.Pools.Allocated))
+	for _, allocatedPool := range cn.Spec.IPAM.Pools.Allocated {
+		allocatedSet[allocatedPool.Pool] = make(map[netip.Prefix]struct{}, len(allocatedPool.CIDRs))
+
+		for _, cidrStr := range allocatedPool.CIDRs {
+			prefix, parseErr := netip.ParsePrefix(string(cidrStr))
+			if parseErr != nil {
+				err = errors.Join(err,
+					fmt.Errorf("failed to parse CIDR of pool %q: %w", allocatedPool.Pool, parseErr))
+				continue
+			}
+
+			occupyErr := p.occupyCIDR(cn.Name, allocatedPool.Pool, prefix)
+			if occupyErr != nil {
+				err = errors.Join(err, occupyErr)
+			}
+
+			allocatedSet[allocatedPool.Pool][prefix] = struct{}{}
+		}
+	}
+
+	// release any cidrs no longer present in allocatedPool
+	for poolName := range p.nodes[cn.Name] {
+		retainErrs := p.retainCIDRs(cn.Name, poolName, allocatedSet[poolName])
+		if retainErrs != nil {
+			err = errors.Join(err, retainErrs)
+		}
+	}
+
+	// Delay allocation until we have occupied the CIDRs of all existing nodes.
+	// The node manager will call us again once it has ensured that all nodes
+	// had their CIDRs occupied, after which p.ready will be set to true
+	if !p.ready {
+		return ErrAllocatorNotReady
+	}
+
+	for _, reqPool := range cn.Spec.IPAM.Pools.Requested {
+		allocatedCIDRs := p.nodes[cn.Name][reqPool.Pool]
+
+		if option.Config.EnableIPv4 {
+			neededIPv4Addrs := big.NewInt(int64(reqPool.Needed.IPv4Addrs))
+			toAllocate := neededIPv4Addrs.Sub(neededIPv4Addrs, allocatedCIDRs.v4.availableAddrs())
+
+			allocErr := p.allocateCIDRs(cn.Name, reqPool.Pool, ipam.IPv4, toAllocate)
+			if allocErr != nil {
+				err = errors.Join(err,
+					fmt.Errorf("failed to allocate ipv4 address for node %q from pool %q: %w",
+						cn.Name, reqPool.Pool, allocErr))
+			}
+		}
+		if option.Config.EnableIPv6 {
+			neededIPv6Addrs := big.NewInt(int64(reqPool.Needed.IPv6Addrs))
+			toAllocate := neededIPv6Addrs.Sub(neededIPv6Addrs, allocatedCIDRs.v6.availableAddrs())
+
+			allocErr := p.allocateCIDRs(cn.Name, reqPool.Pool, ipam.IPv6, toAllocate)
+			if allocErr != nil {
+				err = errors.Join(err,
+					fmt.Errorf("failed to allocate ipv6 address for node %q from pool %q: %w",
+						cn.Name, reqPool.Pool, allocErr))
+			}
+		}
+	}
+	return err
+}
+
+func (p *PoolAllocator) ReleaseNode(nodeName string) error {
+	// Release CIDRs back into pools
+	var err error
+	for poolName, cidrs := range p.nodes[nodeName] {
+		pool, ok := p.pools[poolName]
+		if !ok {
+			err = errors.Join(err,
+				fmt.Errorf("cannot release from non-existing pool: %s", poolName))
+			continue
+		}
+
+		for cidr := range cidrs.v4 {
+			err = errors.Join(err, releaseCIDR(pool.v4, cidr))
+		}
+		for cidr := range cidrs.v6 {
+			err = errors.Join(err, releaseCIDR(pool.v6, cidr))
+		}
+	}
+
+	// Remove bookkeeping for this node
+	delete(p.nodes, nodeName)
+
+	return err
+}
+
+func (p *PoolAllocator) AllocatedPools(targetNode string) (pools []types.IPAMPoolAllocation) {
+	for poolName, cidrs := range p.nodes[targetNode] {
+		v4CIDRs := cidrs.v4.PodCIDRSlice()
+		v6CIDRs := cidrs.v6.PodCIDRSlice()
+
+		pools = append(pools, types.IPAMPoolAllocation{
+			Pool:  poolName,
+			CIDRs: append(v4CIDRs, v6CIDRs...),
+		})
+	}
+
+	sort.Slice(pools, func(i, j int) bool {
+		return pools[i].Pool < pools[j].Pool
+	})
+
+	return pools
+}
+
+func (p *PoolAllocator) isAllocated(targetNode, sourcePool string, cidr netip.Prefix) bool {
+	var found bool
+	switch {
+	case cidr.Addr().Is4():
+		_, found = p.nodes[targetNode][sourcePool].v4[cidr]
+	case cidr.Addr().Is6():
+		_, found = p.nodes[targetNode][sourcePool].v6[cidr]
+	}
+	return found
+}
+
+func (p *PoolAllocator) markAllocated(targetNode, sourcePool string, cidr netip.Prefix) {
+	pools, ok := p.nodes[targetNode]
+	if !ok {
+		pools = poolToCIDRs{}
+		p.nodes[targetNode] = pools
+	}
+
+	cidrs, ok := pools[sourcePool]
+	if !ok {
+		cidrs = cidrSets{
+			v4: cidrSet{},
+			v6: cidrSet{},
+		}
+		pools[sourcePool] = cidrs
+	}
+
+	switch {
+	case cidr.Addr().Is4():
+		cidrs.v4[cidr] = struct{}{}
+	case cidr.Addr().Is6():
+		cidrs.v6[cidr] = struct{}{}
+	}
+}
+
+func (p *PoolAllocator) markReleased(targetNode, sourcePool string, cidr netip.Prefix) {
+	pools, ok := p.nodes[targetNode]
+	if !ok {
+		return
+	}
+
+	cidrs, ok := pools[sourcePool]
+	if !ok {
+		return
+	}
+
+	switch {
+	case cidr.Addr().Is4():
+		delete(cidrs.v4, cidr)
+	case cidr.Addr().Is6():
+		delete(cidrs.v6, cidr)
+	}
+
+	// remove pool reference if it is now empty
+	if len(cidrs.v4) == 0 && len(cidrs.v6) == 0 {
+		delete(pools, sourcePool)
+	}
+}
+
+// allocateCIDRs allocates additional IPs from the sourcePool to the targetNode.
+// The number of to be allocated IPs in toAllocate may be zero or negative, in
+// which case this function is a no-op.
+func (p *PoolAllocator) allocateCIDRs(targetNode, sourcePool string, family ipam.Family, toAllocate *big.Int) error {
+	zero := new(big.Int)
+	if toAllocate.Cmp(zero) <= 0 {
+		return nil // nothing to allocate
+	}
+
+	pool, ok := p.pools[sourcePool]
+	if !ok {
+		return fmt.Errorf("cannot allocate from non-existing pool: %s", sourcePool)
+	}
+
+	log.WithFields(logrus.Fields{
+		"targetNode": targetNode,
+		"sourcePool": sourcePool,
+		"family":     family,
+		"toAllocate": toAllocate,
+	}).Debug("allocating cidr")
+
+	for toAllocate.Cmp(zero) > 0 {
+		cidr, err := pool.allocCIDR(family)
+		if err != nil {
+			return err
+		}
+
+		p.markAllocated(targetNode, sourcePool, cidr)
+		toAllocate.Sub(toAllocate, addrsInPrefix(cidr))
+	}
+
+	return nil
+}
+
+func (p *PoolAllocator) occupyCIDR(targetNode, sourcePool string, cidr netip.Prefix) error {
+	// avoid allocating CIDRs twice
+	if p.isAllocated(targetNode, sourcePool, cidr) {
+		return nil
+	}
+
+	log.WithFields(logrus.Fields{
+		"targetNode": targetNode,
+		"sourcePool": sourcePool,
+		"cidr":       cidr.String(),
+	}).Debug("occupying cidr")
+
+	pool, ok := p.pools[sourcePool]
+	if !ok {
+		return fmt.Errorf("cannot reuse from non-existing pool: %s", sourcePool)
+	}
+
+	err := pool.occupyCIDR(cidr)
+	if err != nil {
+		return fmt.Errorf("unable to reuse from pool %s: %w", sourcePool, err)
+	}
+
+	p.markAllocated(targetNode, sourcePool, cidr)
+
+	return nil
+}
+
+// retainCIDRs releases all CIDRs in sourcePool of targetNode if they are _not_ present in the retain set
+func (p *PoolAllocator) retainCIDRs(targetNode, sourcePool string, retain map[netip.Prefix]struct{}) (err error) {
+	for prefix := range p.nodes[targetNode][sourcePool].v4 {
+		if _, ok := retain[prefix]; ok {
+			continue
+		}
+
+		releaseErr := p.releaseCIDR(targetNode, sourcePool, prefix)
+		if releaseErr != nil {
+			err = errors.Join(err, releaseErr)
+		}
+	}
+	for prefix := range p.nodes[targetNode][sourcePool].v6 {
+		if _, ok := retain[prefix]; ok {
+			continue
+		}
+
+		releaseErr := p.releaseCIDR(targetNode, sourcePool, prefix)
+		if releaseErr != nil {
+			err = errors.Join(err, releaseErr)
+		}
+	}
+
+	return err
+}
+
+func (p *PoolAllocator) releaseCIDR(targetNode, sourcePool string, cidr netip.Prefix) error {
+	// do not release CIDRs not allocated to the node
+	if !p.isAllocated(targetNode, sourcePool, cidr) {
+		return nil
+	}
+
+	log.WithFields(logrus.Fields{
+		"targetNode": targetNode,
+		"sourcePool": sourcePool,
+		"cidr":       cidr.String(),
+	}).Debug("releasing cidr")
+
+	pool, ok := p.pools[sourcePool]
+	if !ok {
+		return fmt.Errorf("cannot release from non-existing pool: %s", sourcePool)
+	}
+
+	err := pool.releaseCIDR(cidr)
+	if err != nil {
+		return fmt.Errorf("unable to release from pool %s: %w", sourcePool, err)
+	}
+
+	p.markReleased(targetNode, sourcePool, cidr)
+
+	return nil
+}

go.mod
@@ -91,6 +91,7 @@ require (
 	go.uber.org/goleak v1.2.1
 	go.uber.org/multierr v1.11.0
 	go.universe.tf/metallb v0.11.0
+	go4.org/netipx v0.0.0-20230303233057-f1b76eb4bb35
 	golang.org/x/crypto v0.9.0
 	golang.org/x/exp v0.0.0-20230510235704-dd950f8aeaea
 	golang.org/x/mod v0.10.0

go.sum
@@ -989,6 +989,8 @@ go.uber.org/zap v1.10.0/go.mod h1:vwi/ZaCAaUcBkycHslxD9B2zi4UTXhF60s6SWpuDF0Q=
 go.uber.org/zap v1.17.0/go.mod h1:MXVU+bhUf/A7Xi2HNOnopQOrmycQ5Ih87HtOu4q5SSo=
 go.uber.org/zap v1.24.0 h1:FiJd5l1UOLj0wCgbSE0rwwXHzEdAZS6hiiSnxJN/D60=
 go.uber.org/zap v1.24.0/go.mod h1:2kMP+WWQ8aoFoedH3T2sq6iJ2yDWpHbP0f6MQbS9Gkg=
+go4.org/netipx v0.0.0-20230303233057-f1b76eb4bb35 h1:nJAwRlGWZZDOD+6wni9KVUNHMpHko/OnRwsrCYeAzPo=
+go4.org/netipx v0.0.0-20230303233057-f1b76eb4bb35/go.mod h1:TQvodOM+hJTioNQJilmLXu08JNb8i+ccq418+KWu1/Y=
 golang.org/x/crypto v0.0.0-20180904163835-0709b304e793/go.mod h1:6SG95UA2DQfeDnfUPMdvaQW0Q7yPrPDi9nlGo2tz2b4=
 golang.org/x/crypto v0.0.0-20181029021203-45a5f77698d3/go.mod h1:6SG95UA2DQfeDnfUPMdvaQW0Q7yPrPDi9nlGo2tz2b4=
 golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2/go.mod h1:djNgcEr1/C05ACkg1iLfiJU5Ep61QUkGW8qpdssI0+w=

pkg/ipam/allocator/multipool/pool.go
@@ -1,0 +1,112 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package multipool
+
+import (
+	"errors"
+	"fmt"
+	"net/netip"
+
+	"go4.org/netipx"
+
+	"github.com/cilium/cilium/pkg/ip"
+	"github.com/cilium/cilium/pkg/ipam"
+	"github.com/cilium/cilium/pkg/ipam/allocator/clusterpool/cidralloc"
+)
+
+var (
+	errPoolEmpty = errors.New("pool empty")
+)
+
+func allocFirstFreeCIDR(allocators []cidralloc.CIDRAllocator) (netip.Prefix, error) {
+	for _, alloc := range allocators {
+		if alloc.IsFull() {
+			continue
+		}
+
+		ipnet, err := alloc.AllocateNext()
+		if err != nil {
+			return netip.Prefix{}, err
+		}
+
+		prefix, ok := netipx.FromStdIPNet(ipnet)
+		if !ok {
+			return netip.Prefix{}, fmt.Errorf("invalid cidr %s allocated", ipnet)
+		}
+		return prefix, nil
+	}
+
+	return netip.Prefix{}, errPoolEmpty
+}
+
+func occupyCIDR(allocators []cidralloc.CIDRAllocator, cidr netip.Prefix) error {
+	ipnet := ip.PrefixToIPNet(cidr)
+	for _, alloc := range allocators {
+		if !alloc.InRange(ipnet) {
+			continue
+		}
+		if alloc.IsFull() {
+			return errPoolEmpty
+		}
+		allocated, err := alloc.IsAllocated(ipnet)
+		if err != nil {
+			return err
+		}
+		if allocated {
+			return fmt.Errorf("cidr %s has already been allocated", cidr)
+		}
+
+		return alloc.Occupy(ipnet)
+	}
+
+	return fmt.Errorf("cidr %s is not part of the requested pool", cidr)
+}
+
+func releaseCIDR(allocators []cidralloc.CIDRAllocator, cidr netip.Prefix) error {
+	ipnet := ip.PrefixToIPNet(cidr)
+	for _, alloc := range allocators {
+		if !alloc.InRange(ipnet) {
+			continue
+		}
+
+		allocated, err := alloc.IsAllocated(ipnet)
+		if err != nil {
+			return err
+		}
+		if !allocated {
+			return nil // not an error to release a cidr twice
+		}
+
+		return alloc.Release(ipnet)
+	}
+
+	return fmt.Errorf("released cidr %s was not part the pool", cidr)
+}
+
+func (c *cidrPool) allocCIDR(family ipam.Family) (netip.Prefix, error) {
+	switch family {
+	case ipam.IPv4:
+		return allocFirstFreeCIDR(c.v4)
+	case ipam.IPv6:
+		return allocFirstFreeCIDR(c.v6)
+	default:
+		return netip.Prefix{}, fmt.Errorf("invalid cidr family: %s", family)
+	}
+}
+
+func (c *cidrPool) occupyCIDR(cidr netip.Prefix) error {
+	if cidr.Addr().Is4() {
+		return occupyCIDR(c.v4, cidr)
+	} else {
+		return occupyCIDR(c.v6, cidr)
+	}
+}
+
+func (c *cidrPool) releaseCIDR(cidr netip.Prefix) error {
+	if cidr.Addr().Is4() {
+		return releaseCIDR(c.v4, cidr)
+	} else {
+		return releaseCIDR(c.v6, cidr)
+	}
+}

pkg/ipam/allocator/multipool/pool_allocator_test.go
@@ -1,0 +1,398 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package multipool
+
+import (
+	"math/big"
+	"net/netip"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
+	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+)
+
+func TestPoolAllocator(t *testing.T) {
+	p := NewPoolAllocator()
+	err := p.AddPool("default",
+		[]string{"10.100.0.0/16", "10.200.0.0/16"}, 24,
+		[]string{"fd00:100::/80", "fc00:100::/80"}, 96,
+	)
+	assert.NoError(t, err)
+
+	// node1 is a node which has some previously allocated CIDRs
+	node1 := &v2.CiliumNode{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node1",
+		},
+		Spec: v2.NodeSpec{
+			IPAM: ipamTypes.IPAMSpec{
+				Pools: ipamTypes.IPAMPoolSpec{
+					Requested: []ipamTypes.IPAMPoolRequest{
+						{
+							Pool: "default",
+							Needed: ipamTypes.IPAMPoolDemand{
+								IPv4Addrs: 10,
+								IPv6Addrs: 10,
+							},
+						},
+					},
+					Allocated: []ipamTypes.IPAMPoolAllocation{
+						{
+							Pool: "default",
+							CIDRs: []ipamTypes.IPAMPodCIDR{
+								"fd00:100:0:0:0:10::/96",
+								"10.100.20.0/24",
+								"10.100.10.0/24",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	// node2 is a new node which needs a fresh allocation
+	node2 := &v2.CiliumNode{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node2",
+		},
+		Spec: v2.NodeSpec{
+			IPAM: ipamTypes.IPAMSpec{
+				Pools: ipamTypes.IPAMPoolSpec{
+					Requested: []ipamTypes.IPAMPoolRequest{
+						{
+							Pool: "default",
+							Needed: ipamTypes.IPAMPoolDemand{
+								IPv4Addrs: 10,
+								IPv6Addrs: 10,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	// node3 is a new node which is attempting to steal a CIDR from node1
+	node3 := &v2.CiliumNode{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node3",
+		},
+		Spec: v2.NodeSpec{
+			IPAM: ipamTypes.IPAMSpec{
+				Pools: ipamTypes.IPAMPoolSpec{
+					Allocated: []ipamTypes.IPAMPoolAllocation{
+						{
+							Pool: "default",
+							CIDRs: []ipamTypes.IPAMPodCIDR{
+								"10.100.10.0/24", // already allocated to node1
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	// node1 has some pre-allocated pools that need to be restored
+	err = p.AllocateToNode(node1)
+	assert.ErrorIs(t, ErrAllocatorNotReady, err)
+	assert.Equal(t, []ipamTypes.IPAMPoolAllocation{
+		{
+			Pool: "default",
+			CIDRs: []ipamTypes.IPAMPodCIDR{ // must be sorted
+				"10.100.10.0/24",
+				"10.100.20.0/24",
+				"fd00:100::10:0:0/96",
+			},
+		},
+	}, p.AllocatedPools(node1.Name))
+
+	// node2 must not allocate before restoration has finished
+	err = p.AllocateToNode(node2)
+	assert.ErrorIs(t, ErrAllocatorNotReady, err)
+	assert.Empty(t, p.AllocatedPools(node2.Name))
+
+	// node3 must not steal the restored CIDR from node1
+	err = p.AllocateToNode(node3)
+	assert.ErrorIs(t, ErrAllocatorNotReady, err)
+	assert.Empty(t, p.AllocatedPools(node3.Name))
+
+	// Mark as ready
+	p.RestoreFinished()
+
+	// The following is a no-op, but should not return any errors
+	err = p.AllocateToNode(node1)
+	assert.NoError(t, err)
+	node1.Spec.IPAM.Pools.Allocated = p.AllocatedPools(node1.Name)
+	assert.Equal(t, []ipamTypes.IPAMPoolAllocation{
+		{
+			Pool: "default",
+			CIDRs: []ipamTypes.IPAMPodCIDR{ // must be sorted
+				"10.100.10.0/24",
+				"10.100.20.0/24",
+				"fd00:100::10:0:0/96",
+			},
+		},
+	}, node1.Spec.IPAM.Pools.Allocated)
+
+	// The following should allocate one IPv4 and IPv6 CIDR each to node2
+	err = p.AllocateToNode(node2)
+	assert.NoError(t, err)
+	node2.Spec.IPAM.Pools.Allocated = p.AllocatedPools(node2.Name)
+	assert.Equal(t, []ipamTypes.IPAMPoolAllocation{
+		{
+			Pool: "default",
+			CIDRs: []ipamTypes.IPAMPodCIDR{
+				"10.100.0.0/24",
+				"fd00:100::/96",
+			},
+		},
+	}, node2.Spec.IPAM.Pools.Allocated)
+
+	// The following should be rejected, because the CIDR is owned by node1
+	err = p.AllocateToNode(node3)
+	assert.EqualError(t, err, "unable to reuse from pool default: cidr 10.100.10.0/24 has already been allocated")
+	assert.Empty(t, p.AllocatedPools(node3.Name))
+
+	// Release 10.100.10.0/24 from node1
+	node1.Spec.IPAM.Pools.Allocated = []ipamTypes.IPAMPoolAllocation{
+		{
+			Pool: "default",
+			CIDRs: []ipamTypes.IPAMPodCIDR{
+				"10.100.20.0/24",
+				"fd00:100::10:0:0/96",
+			},
+		},
+	}
+	err = p.AllocateToNode(node1)
+	assert.NoError(t, err)
+	assert.Equal(t, node1.Spec.IPAM.Pools.Allocated, p.AllocatedPools(node1.Name))
+
+	// node3 can now allocate 10.100.10.0/24
+	err = p.AllocateToNode(node3)
+	assert.NoError(t, err)
+	node3.Spec.IPAM.Pools.Allocated = p.AllocatedPools(node3.Name)
+	assert.Equal(t, []ipamTypes.IPAMPoolAllocation{
+		{
+			Pool: "default",
+			CIDRs: []ipamTypes.IPAMPodCIDR{
+				"10.100.10.0/24",
+			},
+		},
+	}, node3.Spec.IPAM.Pools.Allocated)
+
+	// Release node2
+	err = p.ReleaseNode(node2.Name)
+	assert.NoError(t, err)
+	assert.Empty(t, p.AllocatedPools(node2.Name))
+
+	// Try to allocate released CIDR from node2 to node3
+	node3.Spec.IPAM.Pools.Allocated = []ipamTypes.IPAMPoolAllocation{
+		{
+			Pool: "default",
+			CIDRs: []ipamTypes.IPAMPodCIDR{
+				"10.100.0.0/24",
+				"10.100.10.0/24",
+			},
+		},
+	}
+	err = p.AllocateToNode(node3)
+	assert.NoError(t, err)
+	assert.Equal(t, node3.Spec.IPAM.Pools.Allocated, p.AllocatedPools(node3.Name))
+
+	// Increase demand for node1, this should allocate a new CIDR
+	node1.Spec.IPAM.Pools.Requested = []ipamTypes.IPAMPoolRequest{
+		{
+			Pool: "default",
+			Needed: ipamTypes.IPAMPoolDemand{
+				IPv4Addrs: 300,
+				IPv6Addrs: 10,
+			},
+		},
+	}
+	err = p.AllocateToNode(node1)
+	assert.NoError(t, err)
+	node1.Spec.IPAM.Pools.Allocated = p.AllocatedPools(node1.Name)
+	assert.Equal(t, []ipamTypes.IPAMPoolAllocation{
+		{
+			Pool: "default",
+			CIDRs: []ipamTypes.IPAMPodCIDR{
+				"10.100.1.0/24",
+				"10.100.20.0/24",
+				"fd00:100::10:0:0/96",
+			},
+		},
+	}, node1.Spec.IPAM.Pools.Allocated)
+}
+
+func TestPoolAllocator_PoolErrors(t *testing.T) {
+	p := NewPoolAllocator()
+	p.RestoreFinished()
+
+	node := &v2.CiliumNode{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node1",
+		},
+		Spec: v2.NodeSpec{
+			IPAM: ipamTypes.IPAMSpec{
+				Pools: ipamTypes.IPAMPoolSpec{
+					Requested: []ipamTypes.IPAMPoolRequest{
+						{
+							Pool: "no-exist",
+							Needed: ipamTypes.IPAMPoolDemand{
+								IPv4Addrs: 10,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := p.AllocateToNode(node)
+	assert.ErrorContains(t, err, `failed to allocate ipv4 address for node "node1" from pool "no-exist"`)
+	assert.ErrorContains(t, err, `cannot allocate from non-existing pool: no-exist`)
+
+	err = p.AddPool("ipv4-only",
+		[]string{"10.0.0.0/16"}, 24,
+		nil, 0,
+	)
+	assert.NoError(t, err)
+	node.Spec.IPAM.Pools.Requested = []ipamTypes.IPAMPoolRequest{
+		{
+			Pool: "ipv4-only",
+			Needed: ipamTypes.IPAMPoolDemand{
+				IPv6Addrs: 10,
+			},
+		},
+	}
+	err = p.AllocateToNode(node)
+	assert.ErrorContains(t, err, `failed to allocate ipv6 address for node "node1" from pool "ipv4-only"`)
+	assert.ErrorContains(t, err, `pool empty`)
+
+	err = p.AddPool("ipv4-only-same-cidr",
+		[]string{"10.0.0.0/16"}, 24,
+		nil, 0,
+	)
+	assert.NoError(t, err)
+	err = p.AddPool("ipv6-only",
+		nil, 0,
+		[]string{"fd00:100::/80"}, 96,
+	)
+	assert.NoError(t, err)
+	node.Spec.IPAM.Pools.Requested = []ipamTypes.IPAMPoolRequest{
+		{
+			Pool: "ipv4-only",
+			Needed: ipamTypes.IPAMPoolDemand{
+				IPv4Addrs: 10,
+				IPv6Addrs: 10,
+			},
+		},
+		{
+			Pool: "ipv4-only-same-cidr",
+			Needed: ipamTypes.IPAMPoolDemand{
+				IPv4Addrs: 10,
+				IPv6Addrs: 10,
+			},
+		},
+		{
+			Pool: "ipv6-only",
+			Needed: ipamTypes.IPAMPoolDemand{
+				IPv4Addrs: 10,
+				IPv6Addrs: 10,
+			},
+		},
+	}
+	err = p.AllocateToNode(node)
+	assert.ErrorContains(t, err, `failed to allocate ipv6 address for node "node1" from pool "ipv4-only"`)
+	assert.ErrorContains(t, err, `failed to allocate ipv6 address for node "node1" from pool "ipv4-only-same-cidr"`)
+	assert.ErrorContains(t, err, `failed to allocate ipv4 address for node "node1" from pool "ipv6-only"`)
+	assert.ErrorContains(t, err, `pool empty`)
+	// Some allocations will have failed, but we still expect everything else to have succeeded
+	node.Spec.IPAM.Pools.Allocated = []ipamTypes.IPAMPoolAllocation{
+		{
+			Pool: "ipv4-only",
+			CIDRs: []ipamTypes.IPAMPodCIDR{
+				"10.0.0.0/24",
+			},
+		},
+		{
+			Pool: "ipv4-only-same-cidr",
+			CIDRs: []ipamTypes.IPAMPodCIDR{
+				"10.0.0.0/24",
+			},
+		},
+		{
+			Pool: "ipv6-only",
+			CIDRs: []ipamTypes.IPAMPodCIDR{
+				"fd00:100::/96",
+			},
+		},
+	}
+	assert.Equal(t, node.Spec.IPAM.Pools.Allocated, p.AllocatedPools(node.Name))
+
+	// Try to occupy invalid CIDR
+	node.Spec.IPAM.Pools.Allocated[0] = ipamTypes.IPAMPoolAllocation{
+		Pool: "ipv4-only",
+		CIDRs: []ipamTypes.IPAMPodCIDR{
+			"10.0.0.0/24",
+			"333.444.555.666/77",
+		},
+	}
+	err = p.AllocateToNode(node)
+	assert.ErrorContains(t, err, `failed to parse CIDR of pool "ipv4-only"`)
+}
+
+func Test_addrsInPrefix(t *testing.T) {
+	mustParseBigInt := func(s string) *big.Int {
+		r := new(big.Int)
+		r.SetString(s, 0)
+		return r
+	}
+
+	tests := []struct {
+		name string
+		args netip.Prefix
+		want *big.Int
+	}{
+		{
+			name: "ipv4",
+			args: netip.MustParsePrefix("10.0.0.0/24"),
+			want: big.NewInt(254),
+		},
+		{
+			name: "ipv6",
+			args: netip.MustParsePrefix("f00d::/48"),
+			want: mustParseBigInt("1208925819614629174706174"),
+		},
+		{
+			name: "zero",
+			args: netip.Prefix{},
+			want: big.NewInt(0),
+		},
+		{
+			name: "two",
+			args: netip.MustParsePrefix("10.0.0.0/30"),
+			want: big.NewInt(2),
+		},
+		{
+			name: "underflow /31",
+			args: netip.MustParsePrefix("10.0.0.0/31"),
+			want: big.NewInt(0),
+		},
+		{
+			name: "underflow /32",
+			args: netip.MustParsePrefix("10.0.0.0/32"),
+			want: big.NewInt(0),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addrsInPrefix(tt.args); got.Cmp(tt.want) != 0 {
+				t.Errorf("addrsInPrefix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

vendor/go4.org/netipx/.gitignore
@@ -1,0 +1,3 @@
+crashers
+suppressions
+netaddr-fuzz.zip

vendor/go4.org/netipx/.gitmodules
@@ -1,0 +1,3 @@
+[submodule "corpus"]
+	path = corpus
+	url = https://github.com/inetaf/netaddr-corpus.git

vendor/go4.org/netipx/AUTHORS
@@ -1,0 +1,4 @@
+Alex Willmer <alex@moreati.org.uk>
+Matt Layher <mdlayher@gmail.com>
+Tailscale Inc.
+Tobias Klauser <tklauser@distanz.ch>

commit 145ed34cad23617d66583d1298e08ff970573279

multipool: Fix logging subsystem
This commit fixes the name of the logging subsystem used by the
multi-pool allocator. The multi-pool allocator was previously named
cluster-pool-v2beta2 during development, but has since been renamed.

Fixes: 3bffa18e520d ("operator: Add core logic for IPAM pools")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/ipam/allocator/multipool/allocator.go
@@ -16,7 +16,11 @@ import (
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/ipam/allocator"
 	cilium_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 )
+
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "ipam-allocator-multi-pool")
 
 // Allocator implements allocator.AllocatorProvider
 type Allocator struct {

pkg/ipam/allocator/multipool/pool_allocator.go
@@ -19,12 +19,8 @@ import (
 	"github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/lock"
-	"github.com/cilium/cilium/pkg/logging"
-	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 )
-
-var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "ipam-allocator-clusterpool-v2")
 
 type cidrPool struct {
 	v4         []cidralloc.CIDRAllocator