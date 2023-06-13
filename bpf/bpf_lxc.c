5f16038a572f9ba32b4e185904c8e976e6f9b63a
Julian Wiedmann <jwi@isovalent.com>
2023-06-13 12:16:03 +0300

    bpf: lxc: add error handling for invoke_tailcall_if() in CT tail-call
    [ upstream commit fb48741b49648f9e828c25a835bd189ab24ed0f3 ]
    
    If the invoked function is inlined, it can return error that we need to
    handle. If the function is tail-called, we still need to handle the
    DROP_MISSED_TAIL_CALL.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -139,6 +139,9 @@ int NAME(struct __ctx_buff *ctx)						\
 		return drop_for_direction(ctx, DIR, DROP_INVALID_TC_BUFFER);	\
 										\
 	ret = invoke_tailcall_if(CONDITION, TARGET_ID, TARGET_NAME);		\
+	if (IS_ERR(ret))							\
+		return drop_for_direction(ctx, DIR, ret);			\
+										\
 	return ret;								\
 }
 
@@ -180,6 +183,9 @@ int NAME(struct __ctx_buff *ctx)						\
 			DROP_INVALID_TC_BUFFER);				\
 										\
 	ret = invoke_tailcall_if(CONDITION, TARGET_ID, TARGET_NAME);		\
+	if (IS_ERR(ret))							\
+		return drop_for_direction(ctx, DIR, ret);			\
+										\
 	return ret;								\
 }
 
