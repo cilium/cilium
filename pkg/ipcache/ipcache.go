cilium/cilium

commit c12cbe426c9ba56eb101c54a2922aa7710cbf554

egressgateway, ipcache: depend on k8s.CacheStatus
Introduce a small wrapper around chan struct{} to be able to
provide it via hive to egressgateway and ipcache.

Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

pkg/ipcache/ipcache.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	ipcacheTypes "github.com/cilium/cilium/pkg/ipcache/types"
+	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -83,6 +84,7 @@ type Configuration struct {
 	ipcacheTypes.PolicyHandler
 	ipcacheTypes.DatapathHandler
 	ipcacheTypes.NodeHandler
+	k8s.CacheStatus
 }
 
 // IPCache is a collection of mappings:
@@ -114,9 +116,7 @@ type IPCache struct {
 	// is then swapped in place while 'mutex' is being held.
 	namedPorts types.NamedPortMultiMap
 
-	// k8sSyncedChecker knows how to check for whether the K8s watcher cache
-	// has been fully synced.
-	k8sSyncedChecker k8sSyncedChecker
+	cacheStatus k8s.CacheStatus
 
 	// Configuration provides pointers towards other agent components that
 	// the IPCache relies upon at runtime.
@@ -791,12 +791,6 @@ func (ipc *IPCache) LookupByHostRLocked(hostIPv4, hostIPv6 net.IP) (cidrs []net.
 	return cidrs
 }
 
-// RegisterK8sWaiter registers the object that checks for wehther the K8s cache
-// has been fully synced.
-func (ipc *IPCache) RegisterK8sSyncedChecker(c k8sSyncedChecker) {
-	ipc.k8sSyncedChecker = c
-}
-
 // Equal returns true if two K8sMetadata pointers contain the same data or are
 // both nil.
 func (m *K8sMetadata) Equal(o *K8sMetadata) bool {
@@ -814,10 +808,4 @@ func (m *K8sMetadata) Equal(o *K8sMetadata) bool {
 		}
 	}
 	return m.Namespace == o.Namespace && m.PodName == o.PodName
-}
-
-// k8sCacheIsSynced is an interface for checking if the K8s watcher cache has
-// been fully synced.
-type k8sSyncedChecker interface {
-	K8sCacheIsSynced() bool
 }

pkg/ipcache/metadata.go
@@ -162,7 +162,7 @@ func (ipc *IPCache) InjectLabels(ctx context.Context, modifiedPrefixes []netip.P
 		return modifiedPrefixes, ErrLocalIdentityAllocatorUninitialized
 	}
 
-	if ipc.k8sSyncedChecker == nil || !ipc.k8sSyncedChecker.K8sCacheIsSynced() {
+	if !ipc.cacheStatus.Synchronized() {
 		return modifiedPrefixes, errors.New("k8s cache not fully synced")
 	}
 

daemon/cmd/daemon.go
@@ -181,10 +181,6 @@ type Daemon struct {
 	linkCache      *link.LinkCache
 	hubbleObserver *observer.LocalObserverServer
 
-	// k8sCachesSynced is closed when all essential Kubernetes caches have
-	// been fully synchronized
-	k8sCachesSynced <-chan struct{}
-
 	// endpointCreations is a map of all currently ongoing endpoint
 	// creation events
 	endpointCreations *endpointCreationManager
@@ -414,6 +410,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup,
 	secretManager certificatemanager.SecretManager,
 	nodeLocalStore node.LocalNodeStore,
 	authManager auth.Manager,
+	cacheStatus k8s.CacheStatus,
 ) (*Daemon, *endpointRestoreState, error) {
 
 	var (
@@ -620,6 +617,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup,
 		PolicyHandler:     d.policy.GetSelectorCache(),
 		DatapathHandler:   epMgr,
 		NodeHandler:       dp.Node(),
+		CacheStatus:       cacheStatus,
 	})
 	// Preallocate IDs for old CIDRs. This must be done before any Identity allocations are
 	// possible so that the old IDs are still available. That is why we do this ASAP after the
@@ -694,7 +692,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup,
 	d.cgroupManager = manager.NewCgroupManager()
 
 	if option.Config.EnableIPv4EgressGateway {
-		d.egressGatewayManager = egressgateway.NewEgressGatewayManager(&d, d.identityAllocator, option.Config.InstallEgressGatewayRoutes)
+		d.egressGatewayManager = egressgateway.NewEgressGatewayManager(cacheStatus, d.identityAllocator, option.Config.InstallEgressGatewayRoutes)
 	}
 
 	d.k8sWatcher = watchers.NewK8sWatcher(
@@ -715,8 +713,6 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup,
 		sharedResources,
 	)
 	nd.RegisterK8sGetters(d.k8sWatcher)
-
-	d.ipcache.RegisterK8sSyncedChecker(&d)
 
 	d.k8sWatcher.RegisterNodeSubscriber(d.endpointManager)
 	if option.Config.BGPAnnounceLBIP || option.Config.BGPAnnouncePodCIDR {
@@ -1083,15 +1079,12 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup,
 	if clientset.IsEnabled() {
 		bootstrapStats.k8sInit.Start()
 
-		// Initialize d.k8sCachesSynced before any k8s watchers are alive, as they may
-		// access it to check the status of k8s initialization
-		cachesSynced := make(chan struct{})
-		d.k8sCachesSynced = cachesSynced
-
 		// Launch the K8s watchers in parallel as we continue to process other
 		// daemon options.
-		d.k8sWatcher.InitK8sSubsystem(d.ctx, cachesSynced)
+		d.k8sWatcher.InitK8sSubsystem(d.ctx, cacheStatus)
 		bootstrapStats.k8sInit.End(true)
+	} else {
+		close(cacheStatus)
 	}
 
 	bootstrapStats.cleanup.Start()
@@ -1499,18 +1492,4 @@ func (d *Daemon) GetNodeSuffix() string {
 	}
 
 	return ip.String()
-}
-
-// K8sCacheIsSynced returns true if the agent has fully synced its k8s cache
-// with the API server
-func (d *Daemon) K8sCacheIsSynced() bool {
-	if !d.clientset.IsEnabled() {
-		return true
-	}
-	select {
-	case <-d.k8sCachesSynced:
-		return true
-	default:
-		return false
-	}
 }

daemon/cmd/daemon_main.go
@@ -1647,6 +1647,7 @@ var daemonCell = cell.Module(
 	"Legacy Daemon",
 
 	cell.Provide(newDaemonPromise),
+	cell.Provide(func() k8s.CacheStatus { return make(k8s.CacheStatus) }),
 	cell.Invoke(func(promise.Promise[*Daemon]) {}), // Force instantiation.
 )
 
@@ -1661,6 +1662,7 @@ type daemonParams struct {
 	BGPController   *bgpv1.Controller
 	Shutdowner      hive.Shutdowner
 	SharedResources k8s.SharedResources
+	CacheStatus     k8s.CacheStatus
 	NodeManager     nodeManager.NodeManager
 	EndpointManager endpointmanager.EndpointManager
 	CertManager     certificatemanager.CertificateManager
@@ -1698,7 +1700,9 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 				params.CertManager,
 				params.SecretManager,
 				params.LocalNodeStore,
-				params.AuthManager)
+				params.AuthManager,
+				params.CacheStatus,
+			)
 			if err != nil {
 				return fmt.Errorf("daemon creation failed: %w", err)
 			}
@@ -1749,7 +1753,7 @@ func runDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *daem
 	if params.Clientset.IsEnabled() {
 		// Wait only for certain caches, but not all!
 		// (Check Daemon.InitK8sSubsystem() for more info)
-		<-d.k8sCachesSynced
+		<-params.CacheStatus
 	}
 	bootstrapStats.k8sInit.End(true)
 	restoreComplete := d.initRestore(restoredEndpoints)

pkg/egressgateway/manager.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/identity"
 	identityCache "github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/k8s"
 	k8sTypes "github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
@@ -30,10 +31,6 @@ var (
 	log      = logging.DefaultLogger.WithField(logfields.LogSubsys, "egressgateway")
 	zeroIPv4 = net.ParseIP("0.0.0.0")
 )
-
-type k8sCacheSyncedChecker interface {
-	K8sCacheIsSynced() bool
-}
 
 type eventType int
 
@@ -54,9 +51,9 @@ const (
 type Manager struct {
 	lock.Mutex
 
-	// k8sCacheSyncedChecker is used to check if the agent has synced its
+	// cacheStatus is used to check if the agent has synced its
 	// cache with the k8s API server
-	k8sCacheSyncedChecker k8sCacheSyncedChecker
+	cacheStatus k8s.CacheStatus
 
 	// nodeDataStore stores node name to node mapping
 	nodeDataStore map[string]nodeTypes.Node
@@ -84,9 +81,9 @@ type Manager struct {
 }
 
 // NewEgressGatewayManager returns a new Egress Gateway Manager.
-func NewEgressGatewayManager(k8sCacheSyncedChecker k8sCacheSyncedChecker, identityAlocator identityCache.IdentityAllocator, installRoutes bool) *Manager {
+func NewEgressGatewayManager(cacheStatus k8s.CacheStatus, identityAlocator identityCache.IdentityAllocator, installRoutes bool) *Manager {
 	manager := &Manager{
-		k8sCacheSyncedChecker:   k8sCacheSyncedChecker,
+		cacheStatus:             cacheStatus,
 		nodeDataStore:           make(map[string]nodeTypes.Node),
 		policyConfigs:           make(map[policyID]*PolicyConfig),
 		policyConfigsBySourceIP: make(map[string][]*PolicyConfig),
@@ -121,7 +118,7 @@ func (manager *Manager) getIdentityLabels(securityIdentity uint32) (labels.Label
 func (manager *Manager) runReconciliationAfterK8sSync() {
 	go func() {
 		for {
-			if manager.k8sCacheSyncedChecker.K8sCacheIsSynced() {
+			if manager.cacheStatus.Synchronized() {
 				break
 			}
 
@@ -555,7 +552,7 @@ nextPolicyKey:
 // Whenever it encounters an error, it will just log it and move to the next
 // item, in order to reconcile as many states as possible.
 func (manager *Manager) reconcile(e eventType) {
-	if !manager.k8sCacheSyncedChecker.K8sCacheIsSynced() {
+	if !manager.cacheStatus.Synchronized() {
 		return
 	}
 

pkg/egressgateway/manager_privileged_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/k8s"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	slimv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
@@ -95,14 +96,6 @@ type parsedEgressRule struct {
 	gatewayIP net.IP
 }
 
-type k8sCacheSyncedCheckerMock struct {
-	synced bool
-}
-
-func (k *k8sCacheSyncedCheckerMock) K8sCacheIsSynced() bool {
-	return k.synced
-}
-
 // Hook up gocheck into the "go test" runner.
 type EgressGatewayTestSuite struct{}
 
@@ -134,13 +127,13 @@ func (k *EgressGatewayTestSuite) TestEgressGatewayManager(c *C) {
 
 	defer cleanupPolicies()
 
-	k8sCacheSyncedChecker := &k8sCacheSyncedCheckerMock{}
+	cacheStatus := make(k8s.CacheStatus)
 
-	egressGatewayManager := NewEgressGatewayManager(k8sCacheSyncedChecker, identityAllocator, option.Config.InstallEgressGatewayRoutes)
+	egressGatewayManager := NewEgressGatewayManager(cacheStatus, identityAllocator, option.Config.InstallEgressGatewayRoutes)
 	c.Assert(egressGatewayManager, NotNil)
 	assertIPRules(c, []ipRule{})
 
-	k8sCacheSyncedChecker.synced = true
+	close(cacheStatus)
 
 	node1 := newCiliumNode(node1, node1IP, nodeGroup1Labels)
 	egressGatewayManager.OnUpdateNode(node1)

pkg/ipcache/metadata_test.go
@@ -159,7 +159,6 @@ func TestOverrideIdentity(t *testing.T) {
 		PolicyHandler:     &mockUpdater{},
 		DatapathHandler:   &mockTriggerer{},
 	})
-	ipc.k8sSyncedChecker = &mockK8sSyncedChecker{}
 	ctx := context.Background()
 
 	// Create CIDR identity from labels
@@ -245,7 +244,6 @@ func setupTest(t *testing.T) (cleanup func()) {
 		PolicyHandler:     &mockUpdater{},
 		DatapathHandler:   &mockTriggerer{},
 	})
-	IPIdentityCache.k8sSyncedChecker = &mockK8sSyncedChecker{}
 
 	IPIdentityCache.metadata.upsertLocked(worldPrefix, source.CustomResource, "kube-uid", labels.LabelKubeAPIServer)
 	IPIdentityCache.metadata.upsertLocked(worldPrefix, source.Local, "host-uid", labels.LabelHost)
@@ -255,10 +253,6 @@ func setupTest(t *testing.T) (cleanup func()) {
 		IPIdentityCache.Shutdown()
 	}
 }
-
-type mockK8sSyncedChecker struct{}
-
-func (m *mockK8sSyncedChecker) K8sCacheIsSynced() bool { return true }
 
 type mockUpdater struct{}
 

pkg/k8s/cache_status.go
@@ -1,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package k8s
+
+// CacheStatus allows waiting for k8s caches to synchronize.
+type CacheStatus chan struct{}
+
+// Sychronized returns true if caches have been synchronized at least once.
+//
+// Returns true for an uninitialized [CacheStatus].
+func (cs CacheStatus) Synchronized() bool {
+	if cs == nil {
+		return true
+	}
+
+	select {
+	case <-cs:
+		return true
+	default:
+		return false
+	}
+}

commit 7b18eabde9455b59e79c83713631b207ecec5fc3

ipcache: fix not waiting for k8s caches to sync
This variable was refactored, but we missed reading from the new source.

Fixes: c12cbe426

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

pkg/ipcache/ipcache.go
@@ -118,8 +118,6 @@ type IPCache struct {
 	// interface.
 	namedPorts namedPortMultiMapUpdater
 
-	cacheStatus k8s.CacheStatus
-
 	// Configuration provides pointers towards other agent components that
 	// the IPCache relies upon at runtime.
 	*Configuration

pkg/ipcache/metadata.go
@@ -171,7 +171,7 @@ func (ipc *IPCache) InjectLabels(ctx context.Context, modifiedPrefixes []netip.P
 		return modifiedPrefixes, ErrLocalIdentityAllocatorUninitialized
 	}
 
-	if !ipc.cacheStatus.Synchronized() {
+	if !ipc.Configuration.CacheStatus.Synchronized() {
 		return modifiedPrefixes, errors.New("k8s cache not fully synced")
 	}
 