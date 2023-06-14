cilium/cilium

commit e7d4f5c6af7d7bb6be8630c8d36c6595022d828f

daemon: validate IPv4NativeRoutingCIDR value in DaemonConfig
Signed-off-by: Rene Zbinden <rene.zbinden@postfinance.ch>

pkg/option/config.go
@@ -2169,6 +2169,10 @@ func (c *DaemonConfig) Validate() error {
 		return err
 	}
 
+	if err := c.checkIPv4NativeRoutingCIDR(); err != nil {
+		return nil
+	}
+
 	// Validate that the KVStore Lease TTL value lies between a particular range.
 	if c.KVstoreLeaseTTL > defaults.KVstoreLeaseMaxTTL || c.KVstoreLeaseTTL < defaults.LockLeaseTTL {
 		return fmt.Errorf("KVstoreLeaseTTL does not lie in required range(%ds, %ds)",
@@ -2753,6 +2757,15 @@ func (c *DaemonConfig) checkMapSizeLimits() error {
 	if c.FragmentsMapEntries > FragmentsMapMax {
 		return fmt.Errorf("specified max entries %d for fragment-tracking map must not exceed maximum %d",
 			c.FragmentsMapEntries, FragmentsMapMax)
+	}
+
+	return nil
+}
+
+func (c *DaemonConfig) checkIPv4NativeRoutingCIDR() error {
+	if c.IPv4NativeRoutingCIDR() == nil && c.Masquerade && c.Tunnel == TunnelDisabled && c.IPAMMode() != IPAMENI {
+		return fmt.Errorf("native routing cidr must be configured with option --%s in combination with --%s --%s=%s --%s=%s",
+			IPv4NativeRoutingCIDR, Masquerade, TunnelName, c.Tunnel, IPAM, c.IPAMMode())
 	}
 
 	return nil

pkg/option/config_test.go
@@ -25,6 +25,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/google/go-cmp/cmp"
 	flag "github.com/spf13/pflag"
@@ -462,6 +463,70 @@ func TestCheckMapSizeLimits(t *testing.T) {
 				if err != nil {
 					t.Error(err)
 				}
+			}
+		})
+	}
+}
+
+func TestCheckIPv4NativeRoutingCIDR(t *testing.T) {
+	tests := []struct {
+		name    string
+		d       *DaemonConfig
+		wantErr bool
+	}{
+		{
+			name: "with native routing cidr",
+			d: &DaemonConfig{
+				Masquerade:            true,
+				Tunnel:                TunnelDisabled,
+				IPAM:                  IPAMAzure,
+				ipv4NativeRoutingCIDR: cidr.MustParseCIDR("10.127.64.0/18"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "without native routing cidr and no masquerade",
+			d: &DaemonConfig{
+				Masquerade: false,
+				Tunnel:     TunnelDisabled,
+				IPAM:       IPAMAzure,
+			},
+			wantErr: false,
+		},
+		{
+			name: "without native routing cidr and tunnel enabled",
+			d: &DaemonConfig{
+				Masquerade: true,
+				Tunnel:     TunnelVXLAN,
+				IPAM:       IPAMAzure,
+			},
+			wantErr: false,
+		},
+		{
+			name: "without native routing cidr and tunnel enabled",
+			d: &DaemonConfig{
+				Masquerade: true,
+				Tunnel:     TunnelDisabled,
+				IPAM:       IPAMENI,
+			},
+			wantErr: false,
+		},
+		{
+			name: "without native routing cidr and with masquerade and tunnel disabled and ipam not eni",
+			d: &DaemonConfig{
+				Masquerade: true,
+				Tunnel:     TunnelDisabled,
+				IPAM:       IPAMAzure,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.d.checkIPv4NativeRoutingCIDR()
+			if tt.wantErr && err == nil {
+				t.Error("expected error, but got nil")
 			}
 		})
 	}

install/kubernetes/cilium/charts/config/templates/configmap.yaml
@@ -295,9 +295,9 @@ data:
   enable-xt-socket-fallback: {{ .Values.global.enableXTSocketFallback | quote }}
   install-iptables-rules: {{ .Values.global.installIptablesRules | quote }}
   auto-direct-node-routes: {{ .Values.global.autoDirectNodeRoutes | quote }}
-  {{- if .Values.global.nativeRoutingCIDR }}
+{{- if .Values.global.nativeRoutingCIDR }}
   native-routing-cidr: {{ .Values.global.nativeRoutingCIDR }}
-  {{- end }}
+{{- end }}
 
 {{- if .Values.global.kubeProxyReplacement }}
   kube-proxy-replacement:  {{ .Values.global.kubeProxyReplacement | quote }}

commit 38b60ccefdd3f2bfc3c39b8d9c2a2a592154253f

daemon: Do not require native routing CIDR if ipmasq-agent is enabled
Cilium's built-in [ipmasq-agent replacement](https://docs.cilium.io/en/v1.13/network/concepts/masquerading/#ebpf-based)
acts as a replacement for the native routing CIDR. Therefore, it does
not make sense to require the native routing CIDR if the ipmasq-agent is
enabled, since the two flags are basically mutually exclusive.

In addition, this commit also slightly restructures and aligns the if
condition with the error message, to make it a bit easier to read.

Fixes: e7d4f5c6af7d ("daemon: validate IPv4NativeRoutingCIDR value in DaemonConfig")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/option/config.go
@@ -3638,26 +3638,36 @@ func (c *DaemonConfig) checkMapSizeLimits() error {
 }
 
 func (c *DaemonConfig) checkIPv4NativeRoutingCIDR() error {
-	if c.GetIPv4NativeRoutingCIDR() == nil && c.EnableIPv4Masquerade && !c.TunnelingEnabled() &&
-		c.IPAMMode() != ipamOption.IPAMENI && c.EnableIPv4 && c.IPAMMode() != ipamOption.IPAMAlibabaCloud {
+	if c.GetIPv4NativeRoutingCIDR() == nil &&
+		c.EnableIPv4 && c.EnableIPv4Masquerade &&
+		!c.EnableIPMasqAgent &&
+		!c.TunnelingEnabled() &&
+		c.IPAMMode() != ipamOption.IPAMENI && c.IPAMMode() != ipamOption.IPAMAlibabaCloud {
 		return fmt.Errorf(
 			"native routing cidr must be configured with option --%s "+
-				"in combination with --%s --%s=%s --%s=%s --%s=true",
-			IPv4NativeRoutingCIDR, EnableIPv4Masquerade, RoutingMode, RoutingModeNative,
-			IPAM, c.IPAMMode(), EnableIPv4Name)
+				"in combination with --%s=true --%s=true --%s=false --%s=%s --%s=%s",
+			IPv4NativeRoutingCIDR,
+			EnableIPv4Name, EnableIPv4Masquerade,
+			EnableIPMasqAgent,
+			RoutingMode, RoutingModeNative,
+			IPAM, c.IPAMMode())
 	}
 
 	return nil
 }
 
 func (c *DaemonConfig) checkIPv6NativeRoutingCIDR() error {
-	if c.GetIPv6NativeRoutingCIDR() == nil && c.EnableIPv6Masquerade && !c.TunnelingEnabled() &&
-		c.EnableIPv6 {
+	if c.GetIPv6NativeRoutingCIDR() == nil &&
+		c.EnableIPv6 && c.EnableIPv6Masquerade &&
+		!c.EnableIPMasqAgent &&
+		!c.TunnelingEnabled() {
 		return fmt.Errorf(
 			"native routing cidr must be configured with option --%s "+
-				"in combination with --%s --%s=%s --%s=true",
-			IPv6NativeRoutingCIDR, EnableIPv6Masquerade, RoutingMode, RoutingModeNative,
-			EnableIPv6Name)
+				"in combination with --%s=true --%s=true --%s=false --%s=%s",
+			IPv6NativeRoutingCIDR,
+			EnableIPv6Name, EnableIPv6Masquerade,
+			EnableIPMasqAgent,
+			RoutingMode, RoutingModeNative)
 	}
 
 	return nil

pkg/option/config_test.go
@@ -571,6 +571,18 @@ func TestCheckIPv4NativeRoutingCIDR(t *testing.T) {
 			},
 			wantErr: true,
 		},
+		{
+			name: "without native routing cidr and tunnel disabled, but ipmasq-agent",
+			d: &DaemonConfig{
+				EnableIPv4Masquerade: true,
+				EnableIPv6Masquerade: true,
+				RoutingMode:          RoutingModeNative,
+				IPAM:                 ipamOption.IPAMKubernetes,
+				EnableIPv4:           true,
+				EnableIPMasqAgent:    true,
+			},
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range tests {
@@ -632,6 +644,17 @@ func TestCheckIPv6NativeRoutingCIDR(t *testing.T) {
 				EnableIPv6:           true,
 			},
 			wantErr: true,
+		},
+		{
+			name: "without native routing cidr and tunnel disabled, but ipmasq-agent",
+			d: &DaemonConfig{
+				EnableIPv4Masquerade: true,
+				EnableIPv6Masquerade: true,
+				RoutingMode:          RoutingModeNative,
+				EnableIPv6:           true,
+				EnableIPMasqAgent:    true,
+			},
+			wantErr: false,
 		},
 	}
 