a2266f4ae566f2f738b3187b834106c4999af592
Aleksander Mistewicz <amistewicz@google.com>
2023-06-15 16:30:46 +0000

    Add deepcopy plugin
    It is needed to include FlowFilter in the generated CRD.
    
    Signed-off-by: Aleksander Mistewicz <amistewicz@google.com>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -13,3 +13,4 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@8ba23be9613c672d40ae261
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.30.0
 go install github.com/mitchellh/protoc-gen-go-json@v1.1.0
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
+go install github.com/protobuf-tools/protoc-gen-deepcopy@v0.0.3
