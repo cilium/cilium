43bb253f3e1f7bd505209db7bfb248058387f944
Jarno Rajahalme <jarno@isovalent.com>
2023-06-16 13:13:09 +0300

    policy: Optimize getNets()
    [ upstream commit d9b9e650dce8b555af4ab95ff83a0ea5733466b1 ]
    
    getNets is used in DenyPreferredInsert for MapState. It is somewhat
    costly, and uses a cache to compute the result at most once for each
    MapStateEntry. Speed up the computation with two strategies:
    
    - skip looking for CIDR labels when the identity is not a local
      identity. This works due to CIDR identities always being locally
      allocated
    
    - skip allocating a slice when not needed, returning a nil map instead if
      the locally allocated identity has no CIDR labels
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/policy/distillery_test.go
+++ pkg/policy/distillery_test.go
@@ -38,6 +38,10 @@ var (
 	ep2 = testutils.NewTestEndpoint()
 )
 
+func localIdentity(n uint32) identity.NumericIdentity {
+	return identity.NumericIdentity(n) | identity.LocalIdentityFlag
+
+}
 func (s *DistilleryTestSuite) TestCacheManagement(c *C) {
 	repo := NewPolicyRepository(nil, nil, nil)
 	cache := repo.policyCache
@@ -1196,7 +1200,7 @@ var (
 		owners:           map[MapStateOwner]struct{}{},
 	}
 
-	worldIPIdentity    = identity.NumericIdentity(16324)
+	worldIPIdentity    = localIdentity(16324)
 	worldCIDR          = api.CIDR("192.0.2.3/32")
 	lblWorldIP         = labels.ParseSelectLabelArray(fmt.Sprintf("%s:%s", labels.LabelSourceCIDR, worldCIDR))
 	ruleL3AllowWorldIP = api.NewRule().WithIngressRules([]api.IngressRule{{
@@ -1209,7 +1213,7 @@ var (
 		},
 	}}).WithEndpointSelector(api.WildcardEndpointSelector)
 
-	worldSubnetIdentity = identity.NumericIdentity(16325)
+	worldSubnetIdentity = localIdentity(16325)
 	worldSubnet         = api.CIDR("192.0.2.0/24")
 	worldSubnetRule     = api.CIDRRule{
 		Cidr: worldSubnet,
