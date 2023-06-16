cilium/cilium

commit bfba74097e817eb165099f0994bc4369446a51e0

bpf: Handle reply SRv6 traffic
To know how to encapsulate reply SRv6 traffic, we need to save the outer
IP header on ingress. We do this before decapsulation and save it in the
SRv6 state maps. Then, on egress, we first lookup the state maps to know
if the packet is part of an established SRv6 connection. If it is, we
tail call to a program that performs the actual encapsulation. If it
isn't, we perform the usual SRv6 lookup.

Note the program to encapsulate reply traffic performs the lookup into
state maps a second time. This is a bit wasteful because we've already
performed the lookup before the tail call and know there is an entry
matching. But unfortunately, we can't pass the outer header via the
skb->cb fields (only 5 32-bit slots) and passing it via a map would be
also the same as doing the same lookup a second time.

Garbage collection of state entries is not implemented yet.

Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/lib/egress_policies.h
@@ -210,6 +210,64 @@ srv6_decapsulation(struct __ctx_buff *ctx)
 }
 
 static __always_inline int
+srv6_create_state_entry(struct __ctx_buff *ctx)
+{
+	struct srv6_ipv6_2tuple *outer_ips;
+	void *data, *data_end;
+	struct ipv6hdr *ip6;
+
+	if (!revalidate_data(ctx, &data, &data_end, &ip6))
+		return DROP_INVALID;
+
+	outer_ips = (struct srv6_ipv6_2tuple *)&ip6->saddr;
+
+	switch (ip6->nexthdr) {
+	case IPPROTO_IPV6: {
+		struct ipv6hdr *inner = ip6 + 1;
+		struct srv6_ipv6_2tuple *inner_ips;
+
+		if ((void *)inner + sizeof(*inner) > data_end)
+			return DROP_INVALID;
+		inner_ips = (struct srv6_ipv6_2tuple *)&inner->saddr;
+
+		if (map_update_elem(&SRV6_STATE_MAP6, inner_ips, outer_ips, 0) < 0)
+			return DROP_INVALID;
+	}
+#  ifdef ENABLE_IPV4
+	case IPPROTO_IPIP: {
+		struct iphdr *inner = (struct iphdr *)(ip6 + 1);
+		struct srv6_ipv4_2tuple *inner_ips;
+
+		if ((void *)inner + sizeof(*inner) > data_end)
+			return DROP_INVALID;
+		inner_ips = (struct srv6_ipv4_2tuple *)&inner->saddr;
+
+		if (map_update_elem(&SRV6_STATE_MAP4, inner_ips, outer_ips, 0) < 0)
+			return DROP_INVALID;
+	}
+#  endif /* ENABLE_IPV4 */
+	}
+
+	return 0;
+}
+
+#  ifdef ENABLE_IPV4
+static __always_inline struct srv6_ipv6_2tuple *
+srv6_lookup_state_entry4(struct iphdr *ip4)
+{
+	return map_lookup_elem(&SRV6_STATE_MAP4,
+			       (struct srv6_ipv4_2tuple *)&ip4->saddr);
+}
+#  endif /* ENABLE_IPV4 */
+
+static __always_inline struct srv6_ipv6_2tuple *
+srv6_lookup_state_entry6(struct ipv6hdr *ip6)
+{
+	return map_lookup_elem(&SRV6_STATE_MAP6,
+			       (struct srv6_ipv6_2tuple *)&ip6->saddr);
+}
+
+static __always_inline int
 srv6_handling4(struct __ctx_buff *ctx, union v6addr *src_sid,
 	       struct in6_addr *dst_sid)
 {
@@ -314,6 +372,46 @@ srv6_handling(struct __ctx_buff *ctx, __u32 vrf_id, struct in6_addr *dst_sid)
 	}
 }
 
+static __always_inline int
+srv6_reply(struct __ctx_buff *ctx)
+{
+	struct srv6_ipv6_2tuple *outer_ips;
+	struct iphdr *ip4 __maybe_unused;
+	void *data, *data_end;
+	struct ipv6hdr *ip6;
+	__u16 proto;
+
+	if (!validate_ethertype(ctx, &proto))
+		return DROP_UNSUPPORTED_L2;
+
+	switch (proto) {
+	case bpf_htons(ETH_P_IPV6):
+		if (!revalidate_data(ctx, &data, &data_end, &ip6))
+			return DROP_INVALID;
+
+		outer_ips = srv6_lookup_state_entry6(ip6);
+		if (!outer_ips)
+			return DROP_MISSING_SRV6_STATE;
+
+		return srv6_handling6(ctx, &outer_ips->src,
+				      (struct in6_addr *)&outer_ips->dst);
+#  ifdef ENABLE_IPV4
+	case bpf_htons(ETH_P_IP):
+		if (!revalidate_data(ctx, &data, &data_end, &ip4))
+			return DROP_INVALID;
+
+		outer_ips = srv6_lookup_state_entry4(ip4);
+		if (!outer_ips)
+			return DROP_MISSING_SRV6_STATE;
+
+		return srv6_handling4(ctx, &outer_ips->src,
+				      (struct in6_addr *)&outer_ips->dst);
+#  endif /* ENABLE_IPV4 */
+	}
+
+	return CTX_ACT_OK;
+}
+
 static __always_inline void
 srv6_load_meta_sid(struct __ctx_buff *ctx, struct in6_addr *sid)
 {
@@ -358,6 +456,10 @@ int tail_srv6_decap(struct __ctx_buff *ctx)
 {
 	int ret = 0;
 
+	ret = srv6_create_state_entry(ctx);
+	if (ret < 0)
+		goto error_drop;
+
 	ret = srv6_decapsulation(ctx);
 	if (ret < 0)
 		goto error_drop;
@@ -368,6 +470,18 @@ int tail_srv6_decap(struct __ctx_buff *ctx)
 error_drop:
 		return send_drop_notify_error(ctx, SECLABEL, ret, CTX_ACT_DROP,
 					      METRIC_EGRESS);
+}
+
+__section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_SRV6_REPLY)
+int tail_srv6_reply(struct __ctx_buff *ctx)
+{
+	int ret;
+
+	ret = srv6_reply(ctx);
+	if (ret < 0)
+		return send_drop_notify_error(ctx, SECLABEL, ret, CTX_ACT_DROP,
+					      METRIC_EGRESS);
+	return CTX_ACT_OK;
 }
 # endif /* SKIP_SRV6_HANDLING */
 #endif /* ENABLE_SRV6 */

api/v1/flow/flow.proto
@@ -294,6 +294,7 @@ enum DropReason {
     INVALID_VNI = 183;
     INVALID_TC_BUFFER = 184;
     NO_SID = 185;
+    MISSING_SRV6_STATE = 186;
 }
 
 enum TrafficDirection {

api/v1/observer/observer.pb.go
@@ -146,6 +146,7 @@ const DropReason_VLAN_FILTERED = flow.DropReason_VLAN_FILTERED
 const DropReason_INVALID_VNI = flow.DropReason_INVALID_VNI
 const DropReason_INVALID_TC_BUFFER = flow.DropReason_INVALID_TC_BUFFER
 const DropReason_NO_SID = flow.DropReason_NO_SID
+const DropReason_MISSING_SRV6_STATE = flow.DropReason_MISSING_SRV6_STATE
 
 var DropReason_name = flow.DropReason_name
 var DropReason_value = flow.DropReason_value

bpf/bpf_host.c
@@ -1055,6 +1055,7 @@ static __always_inline
 handle_srv6(struct __ctx_buff *ctx)
 {
 	__u32 *vrf_id, dst_id, tunnel_ep = 0;
+	struct srv6_ipv6_2tuple *outer_ips;
 	struct iphdr *ip4 __maybe_unused;
 	struct remote_endpoint_info *ep;
 	void *data, *data_end;
@@ -1069,6 +1070,12 @@ handle_srv6(struct __ctx_buff *ctx)
 	case bpf_htons(ETH_P_IPV6):
 		if (!revalidate_data(ctx, &data, &data_end, &ip6))
 			return DROP_INVALID;
+
+		outer_ips = srv6_lookup_state_entry6(ip6);
+		if (outer_ips) {
+			ep_tail_call(ctx, CILIUM_CALL_SRV6_REPLY);
+			return DROP_MISSED_TAIL_CALL;
+		}
 
 		ep = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr);
 		if (ep) {
@@ -1097,6 +1104,12 @@ handle_srv6(struct __ctx_buff *ctx)
 	case bpf_htons(ETH_P_IP):
 		if (!revalidate_data(ctx, &data, &data_end, &ip4))
 			return DROP_INVALID;
+
+		outer_ips = srv6_lookup_state_entry4(ip4);
+		if (outer_ips) {
+			ep_tail_call(ctx, CILIUM_CALL_SRV6_REPLY);
+			return DROP_MISSED_TAIL_CALL;
+		}
 
 		ep = lookup_ip4_remote_endpoint(ip4->daddr);
 		if (ep) {

bpf/lib/common.h
@@ -99,7 +99,8 @@
 #define CILIUM_CALL_IPV6_CT_EGRESS		32
 #define CILIUM_CALL_SRV6_ENCAP			33
 #define CILIUM_CALL_SRV6_DECAP			34
-#define CILIUM_CALL_SIZE			35
+#define CILIUM_CALL_SRV6_REPLY			35
+#define CILIUM_CALL_SIZE			36
 
 typedef __u64 mac_t;
 
@@ -482,6 +483,7 @@ enum {
 #define DROP_INVALID_VNI	-183
 #define DROP_INVALID_TC_BUFFER  -184
 #define DROP_NO_SID		-185
+#define DROP_MISSING_SRV6_STATE	-186
 
 #define NAT_PUNT_TO_STACK	DROP_NAT_NOT_NEEDED
 #define NAT_46X64_RECIRC	100

pkg/monitor/api/drop.go
@@ -81,6 +81,7 @@ var errors = map[uint8]string{
 	183: "Incorrect VNI from VTEP",
 	184: "Failed to update or lookup TC buffer",
 	185: "No SID was found for the IP address",
+	186: "SRv6 state was removed during tail call",
 }
 
 func extendedReason(reason uint8, extError int8) string {

test/bpf/check-complexity.sh
@@ -45,10 +45,11 @@ function annotate_section_names {
 	    -e "s/\(section '2\/31'\)/\1 (tail_call IPV6_CT_INGRESS_POLICY_ONLY)/" \
 	    -e "s/\(section '2\/32'\)/\1 (tail_call IPV6_CT_EGRESS)/" \
 	    -e "s/\(section '2\/33'\)/\1 (tail_call SRV6_ENCAP)/" \
-	    -e "s/\(section '2\/34'\)/\1 (tail_call SRV6_DECAP)/"
+	    -e "s/\(section '2\/34'\)/\1 (tail_call SRV6_DECAP)/" \
+	    -e "s/\(section '2\/35'\)/\1 (tail_call SRV6_REPLY)/"
 }
 
-if ! grep -q "CILIUM_CALL_SIZE.*35" "$BPFDIR/lib/common.h" ; then
+if ! grep -q "CILIUM_CALL_SIZE.*36" "$BPFDIR/lib/common.h" ; then
 	echo "This script is out of date compared to CILIUM_CALL_SIZE." 1>&2
 	exit 1
 fi

commit c703f4b38e98c17da3b665ad4888ffc39bb5fc20

bpf: Add missing "break;" in srv6_create_state_entry()
Prevent falling through from IPv6 to IPIP when creating SRV6 state
entries based on the encapsulated header protocol.

Caught by compiling with -Wimplicit-fallthrough.

Fixes: bfba74097e81 ("bpf: Handle reply SRv6 traffic")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

bpf/lib/egress_policies.h
@@ -372,6 +372,7 @@ srv6_create_state_entry(struct __ctx_buff *ctx)
 
 		if (map_update_elem(&SRV6_STATE_MAP6, inner_ips, outer_ips, 0) < 0)
 			return DROP_INVALID;
+		break;
 	}
 #  ifdef ENABLE_IPV4
 	case IPPROTO_IPIP: {
@@ -384,6 +385,7 @@ srv6_create_state_entry(struct __ctx_buff *ctx)
 
 		if (map_update_elem(&SRV6_STATE_MAP4, inner_ips, outer_ips, 0) < 0)
 			return DROP_INVALID;
+		break;
 	}
 #  endif /* ENABLE_IPV4 */
 	}