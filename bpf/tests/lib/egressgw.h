ef5be442dfe47ce9011120577a6020b009fa7cba
Gilberto Bertin <jibi@cilium.io>
2023-06-16 23:57:01 +0200

    bpf: test: add {add, del}_egressgw_policy_entry helpers
    and use them to always clean up egress gateway policies after each test
    run
    
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- bpf/tests/lib/egressgw.h
+++ bpf/tests/lib/egressgw.h
@@ -17,3 +17,31 @@ static __always_inline __be16 client_port(enum egressgw_test t)
 {
 	return CLIENT_PORT + (__be16)t;
 }
+
+static __always_inline void add_egressgw_policy_entry(__be32 saddr, __be32 daddr, __u8 cidr,
+						      __be32 gateway_ip, __be32 egress_ip)
+{
+	struct egress_gw_policy_key in_key = {
+		.lpm_key = { EGRESS_PREFIX_LEN(cidr), {} },
+		.saddr   = saddr,
+		.daddr   = daddr,
+	};
+
+	struct egress_gw_policy_entry in_val = {
+		.egress_ip  = egress_ip,
+		.gateway_ip = gateway_ip,
+	};
+
+	map_update_elem(&EGRESS_POLICY_MAP, &in_key, &in_val, 0);
+}
+
+static __always_inline void del_egressgw_policy_entry(__be32 saddr, __be32 daddr, __u8 cidr)
+{
+	struct egress_gw_policy_key in_key = {
+		.lpm_key = { EGRESS_PREFIX_LEN(cidr), {} },
+		.saddr   = saddr,
+		.daddr   = daddr,
+	};
+
+	map_delete_elem(&EGRESS_POLICY_MAP, &in_key);
+}
