d675d6fd7d3c185bdff9c1943cfae31e3714f8c9
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2023-06-18 21:30:22 +0900

    contrib/kind: Add secondary iface to KIND network
    Add secondary iface to KIND network, so that we can test the NodePort
    via secondary iface.
    
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- contrib/scripts/kind.sh
+++ contrib/scripts/kind.sh
@@ -15,6 +15,7 @@ default_service_subnet=""
 default_agent_port_prefix="234"
 default_operator_port_prefix="235"
 default_network="kind-cilium"
+secondary_network="${default_network}-secondary"
 
 PROG=${0}
 
@@ -26,6 +27,13 @@ if [ "${1:-}" = "--xdp" ]; then
   shift
 fi
 readonly xdp
+
+secondary_network_flag=false
+if [ "${1:-}" = "--secondary-network" ]; then
+  secondary_network_flag=true
+  shift
+fi
+readonly secondary_network_flag
 
 controlplanes="${1:-${CONTROLPLANES:=${default_controlplanes}}}"
 workers="${2:-${WORKERS:=${default_workers}}}"
@@ -40,11 +48,13 @@ agent_port_prefix="${AGENTPORTPREFIX:=${default_agent_port_prefix}}"
 operator_port_prefix="${OPERATORPORTPREFIX:=${default_operator_port_prefix}}"
 
 bridge_dev="br-${default_network}"
+bridge_dev_secondary="${bridge_dev}2"
 v6_prefix="fc00:c111::/64"
+v6_prefix_secondary="fc00:c112::/64"
 CILIUM_ROOT="$(git rev-parse --show-toplevel)"
 
 usage() {
-  echo "Usage: ${PROG} [--xdp] [control-plane node count] [worker node count] [cluster-name] [node image] [kube-proxy mode] [ip-family]"
+  echo "Usage: ${PROG} [--xdp] [--secondary-network] [control-plane node count] [worker node count] [cluster-name] [node image] [kube-proxy mode] [ip-family]"
 }
 
 have_kind() {
@@ -150,6 +160,21 @@ kubeadmConfigPatches:
         "v": "3"
 EOF
 
+if [ "${secondary_network_flag}" = true ]; then
+  if ! docker network inspect "${secondary_network}" >/dev/null 2>&1; then
+    docker network create -d=bridge \
+      -o "com.docker.network.bridge.enable_ip_masquerade=false" \
+      -o "com.docker.network.bridge.name=${bridge_dev_secondary}" \
+      --ipv6 --subnet "${v6_prefix_secondary}" \
+      "${secondary_network}"
+  fi
+
+  nodes=$(docker ps -a --filter label=io.x-k8s.kind.cluster=${cluster_name:-kind} --format {{.Names}})
+  for node in $nodes; do
+    docker network connect ${secondary_network} $node
+  done
+fi
+
 if [ "${xdp}" = true ]; then
   if ! [ -f "${CILIUM_ROOT}/test/l4lb/bpf_xdp_veth_host.o" ]; then
     pushd "${CILIUM_ROOT}/test/l4lb/" > /dev/null
@@ -157,8 +182,8 @@ if [ "${xdp}" = true ]; then
     popd > /dev/null
   fi
 
-  for ifc in /sys/class/net/"${bridge_dev}"/brif/*; do
-    ifc="${ifc#"/sys/class/net/${bridge_dev}/brif/"}"
+  for ifc in /sys/class/net/"${bridge_dev}"*/brif/*; do
+    ifc=$(echo $ifc | "${SED}" "s,/sys/class/net/${bridge_dev}.*/brif/,,")
 
     # Attach a dummy XDP prog to the host side of the veth so that XDP_TX in the
     # pod side works.
