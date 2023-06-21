09bd8c042b2ba698844d644d4ad71586db659f35
Paul Chaignon <paul.chaignon@gmail.com>
2023-06-21 20:33:28 +0200

    ipsec: Remove workarounds for path asymmetry that was removed
    Commits 3b3e8d0b1 ("node: Don't encrypt traffic to CiliumInternalIP")
    and 5fe2b2d6d ("bpf: Don't encrypt on path hostns -> remote pod")
    removed a path asymmetry on the paths hostns <> remote pod. They however
    failed to remove workarounds that we have for this path asymmetry.
    
    In particular, we would encrypt packets on the path pod -> remote node
    (set SPI in the node manager) to try and avoid the path asymmetry, by
    also encrypting the replies. And, as a result of this first change, we
    would also need to handle a corner case in the datapath to appluy the
    correct reverse DNAT for reply traffic.
    
    All of that is unnecessary now that we fixed the path asymmetry.
    
    Fixes: 3b3e8d0b1 ("node: Don't encrypt traffic to CiliumInternalIP")
    Fixes: 5fe2b2d6d ("bpf: Don't encrypt on path hostns -> remote pod")
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- bpf/lib/encap.h
+++ bpf/lib/encap.h
@@ -127,12 +127,12 @@ __encap_and_redirect_lxc(struct __ctx_buff *ctx, __be32 tunnel_endpoint,
 						seclabel);
 #endif
 
-#if !defined(ENABLE_NODEPORT) && (defined(ENABLE_IPSEC) || defined(ENABLE_HOST_FIREWALL))
-	/* For IPSec and the host firewall, traffic from a pod to a remote node
-	 * is sent through the tunnel. In the case of node --> VIP@remote pod,
-	 * packets may be DNATed when they enter the remote node. If kube-proxy
-	 * is used, the response needs to go through the stack on the way to
-	 * the tunnel, to apply the correct reverse DNAT.
+#if !defined(ENABLE_NODEPORT) && defined(ENABLE_HOST_FIREWALL)
+	/* For the host firewall, traffic from a pod to a remote node is sent
+	 * through the tunnel. In the case of node --> VIP@remote pod, packets may
+	 * be DNATed when they enter the remote node. If kube-proxy is used, the
+	 * response needs to go through the stack on the way to the tunnel, to
+	 * apply the correct reverse DNAT.
 	 * See #14674 for details.
 	 */
 	ret = __encap_with_nodeid(ctx, 0, 0, tunnel_endpoint, seclabel, dstid,
@@ -146,7 +146,7 @@ __encap_and_redirect_lxc(struct __ctx_buff *ctx, __be32 tunnel_endpoint,
 #else
 	return __encap_and_redirect_with_nodeid(ctx, 0, tunnel_endpoint,
 						seclabel, dstid, NOT_VTEP_DST, trace);
-#endif /* !ENABLE_NODEPORT && (ENABLE_IPSEC || ENABLE_HOST_FIREWALL) */
+#endif /* !ENABLE_NODEPORT && ENABLE_HOST_FIREWALL */
 }
 
 #if defined(TUNNEL_MODE) || defined(ENABLE_HIGH_SCALE_IPCACHE)
