a40e6fcaccc0c9c36b35249e89a2274dfc98a313
Casey Callendrello <cdc@isovalent.com>
2023-06-22 11:54:48 +0200

    endpoint: add policy engine race test
    [ upstream commit 8e163a9b70d9b5e09516828831a5378f0bed79bf ]
    
    This adds a small test that ensures incremental updates are never lost,
    even in the face of significant identity churn.
    
    It simulates a churning ipcache flinging identities in to the policy
    engine, and similarly recalculates policy constantly.
    
    Signed-off-by: Casey Callendrello <cdc@isovalent.com>
    Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

--- pkg/endpoint/policy_test.go
+++ pkg/endpoint/policy_test.go
@@ -8,8 +8,23 @@ package endpoint
 import (
 	"gopkg.in/check.v1"
 
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/ipcache"
+	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/policy"
+	"github.com/cilium/cilium/pkg/policy/api"
 	testidentity "github.com/cilium/cilium/pkg/testutils/identity"
 	"github.com/cilium/cilium/pkg/u8proto"
 )
@@ -39,4 +54,166 @@ func (s *EndpointSuite) TestUpdateVisibilityPolicy(c *check.C) {
 		return "", nil
 	})
 	c.Assert(ep.visibilityPolicy, check.IsNil)
+}
+
+// This test fuzzes the incremental update engine from an end-to-end perspective
+// to ensure we don't ever miss an incremental update.
+//
+// It works by simulating a "churning" IPcache that is constantly allocating new identities.
+// There is a single policy that -- funnily enough -- selects all of the new identities.
+// We then continuously simulate endpoint regeneration and ensure the computed policy contains
+// all the generated identities.
+//
+// By default, we test 1000 identities, which should take less than 10 seconds. If this test fails,
+// please bump the factor to something massive and start debugging :-).
+func TestIncrementalUpdatesDuringPolicyGeneration(t *testing.T) {
+	const testfactor = 1000 // bump this to stress-test
+
+	pe := policy.GetPolicyEnabled()
+	policy.SetPolicyEnabled("always")
+	defer policy.SetPolicyEnabled(pe)
+
+	idcache := make(cache.IdentityCache, testfactor)
+	fakeAllocator := testidentity.NewMockIdentityAllocator(idcache)
+	repo := policy.NewPolicyRepository(fakeAllocator, fakeAllocator.GetIdentityCache(), nil)
+
+	defer func() {
+		repo.RepositoryChangeQueue.Stop()
+		repo.RuleReactionQueue.Stop()
+		repo.RepositoryChangeQueue.WaitToBeDrained()
+		repo.RuleReactionQueue.WaitToBeDrained()
+	}()
+
+	addIdentity := func(labelKeys ...string) *identity.Identity {
+		t.Helper()
+		lbls := labels.Labels{}
+		for _, labelKey := range labelKeys {
+			lbls[labelKey] = labels.NewLabel("k8s:"+labelKey, "", "")
+		}
+		id, _, err := fakeAllocator.AllocateIdentity(context.Background(), lbls, false, 0)
+		if err != nil {
+			t.Fatal(err)
+		}
+		//t.Logf("allocated label %s id %d", labelKeys, id.ID) // commented out for speed
+
+		wg := &sync.WaitGroup{}
+		repo.GetSelectorCache().UpdateIdentities(cache.IdentityCache{
+			id.ID: id.LabelArray,
+		}, nil, wg)
+		wg.Wait()
+		return id
+	}
+
+	podID := addIdentity("pod")
+	repo.GetPolicyCache().LocalEndpointIdentityAdded(podID)
+
+	ep := Endpoint{
+		SecurityIdentity: podID,
+		policyGetter:     &mockPolicyGetter{repo},
+	}
+	ep.UpdateLogger(nil)
+
+	podSelectLabel := labels.ParseSelectLabel("pod")
+	egressSelectLabel := labels.ParseSelectLabel("peer")
+
+	// Create a rule for our pod that selects all peer identities
+	egressDenyRule := &api.Rule{
+		EndpointSelector: api.NewESFromLabels(podSelectLabel),
+		EgressDeny: []api.EgressDenyRule{
+			{
+				EgressCommonRule: api.EgressCommonRule{
+					ToEndpoints: []api.EndpointSelector{
+						api.NewESFromLabels(egressSelectLabel),
+					},
+				},
+				ToPorts: []api.PortDenyRule{
+					{
+						Ports: []api.PortProtocol{
+							{
+								Port:     "80",
+								Protocol: "TCP",
+							},
+						},
+					},
+				},
+			},
+		},
+		Labels: labels.LabelArray{
+			labels.NewLabel(k8sConst.PolicyLabelName, "egressDenyRule", labels.LabelSourceAny),
+		},
+	}
+
+	_, _, err := repo.Add(*egressDenyRule, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Track all IDs we allocate so we can validate later that we never miss any
+	checkMutex := lock.Mutex{}
+	allocatedIDs := make(map[identity.NumericIdentity]struct{}, testfactor)
+	done := false
+
+	// simulate ipcache churn: continuously allocate IDs and push them to the policy engine.
+	go func() {
+		for i := 0; i < testfactor; i++ {
+			if i%100 == 0 {
+				t.Log(i)
+			}
+			id := addIdentity("peer", fmt.Sprintf("peer%d", i))
+
+			// note: we could stop checking here and the last ID would be missing from allocatedIDs
+			// so we will have to handle the case where we select one more ID than is in allocatedIDs
+			checkMutex.Lock()
+			allocatedIDs[id.ID] = struct{}{}
+			checkMutex.Unlock()
+
+		}
+		done = true
+	}()
+
+	// Continuously compute policy for the pod and ensure we never missed an incremental update.
+	for {
+		t.Log("Calculating policy...")
+		res, err := ep.regeneratePolicy()
+		assert.Nil(t, err)
+
+		// Sleep a random amount, so we accumulate some changes
+		// This does not slow down the test, since we always generate testFactor identities.
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
+
+		// Now, check that all the expected entries are there
+		checkMutex.Lock()
+		t.Log("Checking policy...")
+
+		// Apply any pending incremental changes
+		// This mirrors the existing code, where we consume map changes
+		// while holding the endpoint lock
+		res.endpointPolicy.ConsumeMapChanges()
+		haveIDs := make(map[identity.NumericIdentity]struct{}, testfactor)
+		for k := range res.endpointPolicy.PolicyMapState {
+			haveIDs[identity.NumericIdentity(k.Identity)] = struct{}{}
+		}
+
+		// It is okay if we have *more* IDs than allocatedIDs, since we may have propagated
+		// an ID change through the policy system but not yet added to the extra list we're
+		// keeping in this test.
+		//
+		// It is confusing, but this assertion checks that allocatedIDs is a subset of haveIDs,
+		// not the other way around.
+		assert.Subset(t, haveIDs, allocatedIDs, "stress-testing the incremental update system failed! DO NOT just retest, there is a race condition!")
+
+		checkMutex.Unlock()
+
+		if done {
+			break
+		}
+	}
+}
+
+type mockPolicyGetter struct {
+	repo *policy.Repository
+}
+
+func (m *mockPolicyGetter) GetPolicyRepository() *policy.Repository {
+	return m.repo
 }
