cilium/cilium

commit 87981acb4cc6117a0ebac87639a161fe1570709b

bpf: Implement IPv6 BPF masquerading
This commit implements BPF masquerading for IPv6 by following the
implementation for IPv4. The main function changed here is
snat_v6_needed with almost the exact same steps as IPv4.

Support for the ipmasq agent in IPv6 is not implemented here.

[ Quentin: Addressed feedback from latest reviews, rebased on main
  branch. Used ENABLE_MASQUERADE_IPV6 instead of sharing
  ENABLE_MASQUERADE with IPv4. Addressed conflicts in nodeport.h and
  nat.h due to commits:
  - 0ea434115253 ("bpf: Make union v6addr const where possible")
  - c62b2094dbec ("bpf: nat: move snat_v{4,6}_needed into lib/nat.h")
  - b3747c8a1bf0 ("bpf: Change ipcache lookup functions to take ClusterID")
  - 9f6fc7e16df9 ("bpf: nodeport: fix tracing for handle_nat_fwd()")
  - 54a86312782f ("bpf: nodeport: handle revDNAT for local backends at to-netdev/to-overlay")
  - eee902f44979 ("bpf: conntrack: introduce an optimized CT lookup for LB")
  - 05d084baeef1 ("bpf: nodeport: move DSR handling from bpf_lxc into from/to-netdev")
  - 306c2b4c83a6 ("bpf: nodeport: Extract DSR specific option from the Geneve header")
  - d749a590069f ("bpf: Pass ext_err to ct_create4 and ct_create6")
  - 8b6aa6eda919 ("bpf: nodeport: fix up trace point in to-overlay NAT paths")
  - 2ec713e8e3c9 ("bpf: rename remote_endpoint_info.sec_label to sec_identity")
  - 5ba20bd7e7e5 ("bpf: Use ct_extract_ports4 for ct_lookup4")
  - "bpf: Change ipv6_addrcmp() into ipv6_addr_equals()" from same PR
]

Signed-off-by: Paul Chaignon <paul@cilium.io>
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

bpf/lib/nat.h
@@ -45,8 +45,6 @@ struct nat_entry {
 # define SNAT_SIGNAL_THRES		16
 #endif
 
-static __always_inline bool nodeport_uses_dsr(__u8 nexthdr __maybe_unused);
-
 static __always_inline __be16 __snat_clamp_port_range(__u16 start, __u16 end,
 						      __u16 val)
 {
@@ -1667,27 +1665,98 @@ static __always_inline void snat_v6_init_tuple(const struct ipv6hdr *ip6,
 }
 
 static __always_inline bool snat_v6_needed(struct __ctx_buff *ctx,
-					   const union v6addr *addr)
+					   union v6addr *addr)
 {
+	union v6addr masq_addr __maybe_unused;
+	const union v6addr dr_addr = IPV6_DIRECT_ROUTING;
+	struct remote_endpoint_info *remote_ep __maybe_unused;
+	struct endpoint_info *local_ep __maybe_unused;
 	void *data, *data_end;
 	struct ipv6hdr *ip6;
 
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
 		return false;
-#ifdef ENABLE_DSR_HYBRID
-	{
-		__u8 nexthdr = ip6->nexthdr;
-		int ret;
 
-		ret = ipv6_hdrlen(ctx, &nexthdr);
-		if (ret > 0) {
-			if (nodeport_uses_dsr(nexthdr))
-				return false;
+	/* See comment in snat_v4_prepare_state(). */
+	if (DIRECT_ROUTING_DEV_IFINDEX == NATIVE_DEV_IFINDEX &&
+	    ipv6_addr_equals((union v6addr *)&ip6->saddr, &dr_addr)) {
+		ipv6_addr_copy(addr, &dr_addr);
+		return true;
+	}
+#ifdef ENABLE_MASQUERADE_IPV6 /* SNAT local pod to world packets */
+	BPF_V6(masq_addr, IPV6_MASQUERADE);
+	if (ipv6_addr_equals((union v6addr *)&ip6->saddr, &masq_addr)) {
+		ipv6_addr_copy(addr, &masq_addr);
+		return true;
+	}
+
+# ifdef IS_BPF_OVERLAY
+	/* See comment in snat_v4_prepare_state(). */
+	return false;
+# endif /* IS_BPF_OVERLAY */
+
+# ifdef IPV6_SNAT_EXCLUSION_DST_CIDR
+	{
+		union v6addr excl_cidr_mask = IPV6_SNAT_EXCLUSION_DST_CIDR_MASK;
+		union v6addr excl_cidr = IPV6_SNAT_EXCLUSION_DST_CIDR;
+
+		/* See comment in snat_v4_prepare_state(). */
+		if (ipv6_addr_in_net((union v6addr *)&ip6->daddr, &excl_cidr,
+				     &excl_cidr_mask))
+			return false;
+	}
+# endif /* IPV6_SNAT_EXCLUSION_DST_CIDR */
+
+	/* if this is a localhost endpoint, no SNAT is needed */
+	local_ep = __lookup_ip6_endpoint((union v6addr *)&ip6->saddr);
+	if (local_ep && (local_ep->flags & ENDPOINT_F_HOST))
+		return false;
+
+	remote_ep = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr, 0);
+	if (remote_ep) {
+		bool is_reply = false;
+
+# ifndef TUNNEL_MODE
+		/* See comment in snat_v4_prepare_state(). */
+		if (identity_is_remote_node(remote_ep->sec_identity))
+			return false;
+# endif /* TUNNEL_MODE */
+
+		/* See comment in snat_v4_prepare_state(). */
+		if (local_ep) {
+			struct ipv6_ct_tuple tuple = {};
+			int l4_off;
+
+			tuple.nexthdr = ip6->nexthdr;
+			ipv6_addr_copy(&tuple.daddr, (union v6addr *)&ip6->daddr);
+			ipv6_addr_copy(&tuple.saddr, (union v6addr *)&ip6->saddr);
+
+			/* ipv6_hdrlen() can return an error. If it does, it
+			 * makes no sense marking this packet as a reply based
+			 * on a wrong offset.
+			 *
+			 * We should probably improve this code in the future to
+			 * report the error to the caller. Same thing for errors
+			 * from ct_is_reply6() below, and ct_is_reply4() in
+			 * snat_v4_prepare_state().
+			 */
+			l4_off = ipv6_hdrlen(ctx, &tuple.nexthdr);
+			if (l4_off >= 0) {
+				l4_off += ETH_HLEN;
+				ct_is_reply6(get_ct_map6(&tuple), ctx, l4_off,
+					     &tuple, &is_reply);
+			}
+		}
+
+		/* See comment in snat_v4_prepare_state(). */
+		if (!is_reply && local_ep) {
+			ipv6_addr_copy(addr, &masq_addr);
+			return true;
 		}
 	}
-#endif /* ENABLE_DSR_HYBRID */
-	/* See snat_v4_prepare_state(). */
-	return ipv6_addr_equals((union v6addr *)&ip6->saddr, addr);
+#endif /* ENABLE_MASQUERADE_IPV6 */
+
+	return false;
 }
 
 static __always_inline __maybe_unused int

bpf/lib/conntrack.h
@@ -395,6 +395,35 @@ ipv6_ct_tuple_swap_ports(struct ipv6_ct_tuple *tuple)
 }
 
 static __always_inline int
+ct_extract_ports6(struct __ctx_buff *ctx, int off,
+		  enum ct_dir dir __maybe_unused, struct ipv6_ct_tuple *tuple,
+		  bool *has_l4_header __maybe_unused)
+{
+	switch (tuple->nexthdr) {
+	/* TCP, UDP, and SCTP all have the ports at the same location */
+	case IPPROTO_TCP:
+	case IPPROTO_UDP:
+#ifdef ENABLE_SCTP
+	case IPPROTO_SCTP:
+#endif  /* ENABLE_SCTP */
+		/* load sport + dport into tuple */
+		if (l4_load_ports(ctx, off, &tuple->dport) < 0)
+			return DROP_CT_INVALID_HDR;
+
+		break;
+
+	default:
+		/* Can't handle ICMP or extension headers yet */
+		return DROP_CT_UNKNOWN_PROTO;
+	}
+
+	return 0;
+}
+
+/* This defines the ct_is_reply6 function. */
+DEFINE_FUNC_CT_IS_REPLY(6)
+
+static __always_inline int
 __ct_lookup6(const void *map, struct ipv6_ct_tuple *tuple, struct __ctx_buff *ctx,
 	     int l4_off, enum ct_action action, enum ct_dir dir, enum ct_scope scope,
 	     struct ct_state *ct_state, __u32 *monitor)

bpf/lib/nodeport.h
@@ -98,7 +98,6 @@ static __always_inline bool nodeport_uses_dsr6(const struct ipv6_ct_tuple *tuple
 }
 
 static __always_inline int nodeport_snat_fwd_ipv6(struct __ctx_buff *ctx,
-						  const union v6addr *addr,
 						  __s8 *ext_err)
 {
 	struct ipv6_nat_target target = {
@@ -107,9 +106,7 @@ static __always_inline int nodeport_snat_fwd_ipv6(struct __ctx_buff *ctx,
 	};
 	int ret;
 
-	ipv6_addr_copy(&target.addr, addr);
-
-	ret = snat_v6_needed(ctx, addr) ?
+	ret = snat_v6_needed(ctx, &target.addr) ?
 	      snat_v6_nat(ctx, &target, ext_err) : CTX_ACT_OK;
 	if (ret == NAT_PUNT_TO_STACK)
 		ret = CTX_ACT_OK;
@@ -1037,7 +1034,10 @@ skip_service_lookup:
 			if (IS_ERR(ret))
 				return ret;
 #endif
+
+#ifndef ENABLE_MASQUERADE_IPV6
 			return CTX_ACT_OK;
+#endif /* ENABLE_MASQUERADE_IPV6 */
 		}
 #endif /* ENABLE_DSR */
 
@@ -1325,13 +1325,6 @@ int tail_handle_snat_fwd_ipv6(struct __ctx_buff *ctx)
 	enum trace_point obs_point;
 	int ret;
 	__s8 ext_err = 0;
-#if defined(TUNNEL_MODE) && defined(IS_BPF_OVERLAY)
-	union v6addr addr = { .p1 = 0 };
-
-	BPF_V6(addr, ROUTER_IP);
-#else
-	union v6addr addr = IPV6_DIRECT_ROUTING;
-#endif
 
 #ifdef IS_BPF_OVERLAY
 	obs_point = TRACE_TO_OVERLAY;
@@ -1339,7 +1332,7 @@ int tail_handle_snat_fwd_ipv6(struct __ctx_buff *ctx)
 	obs_point = TRACE_TO_NETWORK;
 #endif
 
-	ret = nodeport_snat_fwd_ipv6(ctx, &addr, &ext_err);
+	ret = nodeport_snat_fwd_ipv6(ctx, &ext_err);
 	if (IS_ERR(ret))
 		return send_drop_notify_error_ext(ctx, 0, ret, ext_err,
 						  CTX_ACT_DROP, METRIC_EGRESS);
@@ -1359,7 +1352,8 @@ __handle_nat_fwd_ipv6(struct __ctx_buff *ctx, struct trace_ctx *trace)
 		return ret;
 
 #if !defined(ENABLE_DSR) ||						\
-    (defined(ENABLE_DSR) && defined(ENABLE_DSR_HYBRID))
+    (defined(ENABLE_DSR) && defined(ENABLE_DSR_HYBRID)) ||		\
+     defined(ENABLE_MASQUERADE_IPV6)
 	if (!ctx_snat_done(ctx)) {
 		ep_tail_call(ctx, CILIUM_CALL_IPV6_NODEPORT_SNAT_FWD);
 		ret = DROP_MISSED_TAIL_CALL;

bpf/lib/stubs.h
@@ -20,6 +20,10 @@
 # ifndef IPV4_DIRECT_ROUTING
 #  define IPV4_DIRECT_ROUTING 0
 # endif
+# if defined(ENABLE_MASQUERADE_IPV6) && !defined(IPV6_MASQUERADE_V)
+DEFINE_IPV6(IPV6_MASQUERADE, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+#  define IPV6_MASQUERADE_V
+# endif
 #endif
 
 #endif /* __STUBS_H_ */

bpf/node_config.h
@@ -111,6 +111,10 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 #endif /* ENABLE_IPV4 */
 
 #ifdef ENABLE_IPV6
+# ifdef ENABLE_MASQUERADE_IPV6
+#  define IPV6_SNAT_EXCLUSION_DST_CIDR      { .addr = { 0xfa, 0xce, 0xff, 0xff, 0xff, 0x0 } }
+#  define IPV6_SNAT_EXCLUSION_DST_CIDR_MASK { .addr = { 0xff, 0xff, 0xff, 0xff, 0xff, 0x0 } }
+# endif /* ENABLE_MASQUERADE_IPV6 */
 #ifdef ENABLE_NODEPORT
 #define SNAT_MAPPING_IPV6 test_cilium_snat_v6_external
 #define PER_CLUSTER_SNAT_MAPPING_IPV6 test_cilium_per_cluster_snat_v6_external

commit e21ca347b30995d64ba0de5abcbc88b6af879903

bpf: nat: Do not skip IPv6 SNAT for packets from local endpoints
We're not setting or using target->from_local_endpoint in the IPv6 BPF
masquerading, so that we may skip masquerading for packets from local
endpoints. Let's fix and align on what we do for IPv4.

Fixes: 87981acb4cc6 ("bpf: Implement IPv6 BPF masquerading")
Reported-by: Julian Wiedmann <jwi@isovalent.com>
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

bpf/lib/nat.h
@@ -1221,6 +1221,7 @@ struct ipv6_nat_target {
 	const __u16 min_port; /* host endianness */
 	const __u16 max_port; /* host endianness */
 	bool src_from_world;
+	bool from_local_endpoint;
 };
 
 #if defined(ENABLE_IPV6) && defined(ENABLE_NODEPORT)
@@ -1636,7 +1637,9 @@ snat_v6_nat_can_skip(const struct ipv6_nat_target *target, const struct ipv6_ct_
 {
 	__u16 sport = bpf_ntohs(tuple->sport);
 
-	return (!target->src_from_world && sport < NAT_MIN_EGRESS) || icmp_echoreply;
+	return (!target->from_local_endpoint && !target->src_from_world &&
+		sport < NAT_MIN_EGRESS) ||
+		icmp_echoreply;
 }
 
 static __always_inline bool
@@ -1721,6 +1724,8 @@ static __always_inline bool snat_v6_needed(struct __ctx_buff *ctx,
 		tuple.nexthdr = ip6->nexthdr;
 		ipv6_addr_copy(&tuple.daddr, (union v6addr *)&ip6->daddr);
 		ipv6_addr_copy(&tuple.saddr, (union v6addr *)&ip6->saddr);
+
+		target->from_local_endpoint = true;
 
 		/* ipv6_hdrlen() can return an error. If it does, it makes no
 		 * sense marking this packet as a reply based on a wrong offset.