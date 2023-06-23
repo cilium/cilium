839d656d7fedb00103a2b72a11a400e8cd525799
Tobias Klauser <tobias@cilium.io>
2023-06-23 11:49:08 +0200

    renovate: let renovate update default Cilium version
    Let renovate update the Cilium version which is installed by default and
    used in GH actions workflows. This avoid creating these update PRs
    manually, see e.g. https://github.com/cilium/cilium-cli/pull/1686
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/defaults/defaults.go
+++ cilium-cli/defaults/defaults.go
@@ -6,6 +6,9 @@ package defaults
 import "time"
 
 const (
+	// renovate: datasource=github-releases depName=cilium/cilium
+	Version = "v1.13.3"
+
 	AgentContainerName      = "cilium-agent"
 	AgentServiceAccountName = "cilium"
 	AgentClusterRoleName    = "cilium"
@@ -86,7 +89,6 @@ const (
 	ConnectivityDNSTestServerImage = "docker.io/coredns/coredns:1.10.1@sha256:a0ead06651cf580044aeb0a0feba63591858fb2e43ade8c9dea45a6a89ae7e5e"
 
 	ConfigMapName = "cilium-config"
-	Version       = "v1.13.3"
 
 	StatusWaitDuration = 5 * time.Minute
 
