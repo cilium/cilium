5951d1ddef1551dad0a28ffd256a89cae2550336
Quentin Monnet <quentin@isovalent.com>
2023-06-23 10:32:31 +0100

    docs: Run rstcheck on README.rst, too.
    Let's run rstcheck on the README.rst file in addition to the
    documentation sources. One motivation is to help catch issues on the
    releases table before merging. We update this table with a script after
    cutting new releases, and it has occasionally led to formatting issues
    in the past.
    
    Fixes: #13366
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- Documentation/check-build.sh
+++ Documentation/check-build.sh
@@ -114,7 +114,7 @@ run_linter() {
         --ignore-directives "tabs,openapi" \
         --ignore-roles "${CONF_PY_ROLES}" \
         --ignore-substitutions "${CONF_PY_SUBSTITUTIONS}" \
-       -r . 2>&1 | \
+       -r . ../README.rst 2>&1 | \
        grep -v 'CRITICAL:rstcheck_core.checker:An `AttributeError` error occured. This is most propably due to a code block directive (code/code-block/sourcecode) without a specified language.'
 }
 
