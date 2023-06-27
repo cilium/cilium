7b2cba9c65d838a309148eb46d9744aac2495d27
Martynas Pumputis <m@lambda.lt>
2023-06-27 15:36:07 +0300

    connectivity: Add op=Exists toleration for test-conn-disrupt
    [ cherry-picked from cilium/cilium-cli repository ]
    
    It was observed on a Kind cluster, that during Cilium upgrade (v1.13.4
    -> main) kubelet set the taint node.kubernetes.io/network-unavailable
    which evicted test-conn-disrupt pods. This resulted in a false negative
    in the upgrade tests.
    
    Fix the issue by tolerating any taint.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- cilium-cli/connectivity/check/deployment.go
+++ cilium-cli/connectivity/check/deployment.go
@@ -600,6 +600,9 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 				Labels:   map[string]string{"app": "test-conn-disrupt-server"},
 				Command:  []string{"tcd-server", "8000"},
 				Port:     8000,
+				Tolerations: []corev1.Toleration{
+					{Operator: corev1.TolerationOpExists},
+				},
 			})
 			_, err = ct.clients.src.CreateServiceAccount(ctx, ct.params.TestNamespace, k8s.NewServiceAccount(testConnDisruptServerDeploymentName), metav1.CreateOptions{})
 			if err != nil {
@@ -646,6 +649,9 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 					fmt.Sprintf("test-conn-disrupt.%s.svc.cluster.local.:8000", ct.params.TestNamespace),
 				},
 				ReadinessProbe: readinessProbe,
+				Tolerations: []corev1.Toleration{
+					{Operator: corev1.TolerationOpExists},
+				},
 			})
 
 			_, err = ct.clients.src.CreateServiceAccount(ctx, ct.params.TestNamespace, k8s.NewServiceAccount(testConnDisruptClientDeploymentName), metav1.CreateOptions{})
