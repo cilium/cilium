2b3d8a6e9a2a23c4eb4e625c71eab00e3a8f0e3d
czybjtu <smartczy@outlook.com>
2023-06-28 10:20:39 +0800

    add host-side interface info to cni.Result
    This commit adds host-side interface name and
    mac address to cni.Result according to CNI Specification
    (https://www.cni.dev/docs/spec/#section-5-result-types)
    
    Signed-off-by: czybjtu <smartczy@outlook.com>

--- plugins/cilium-cni/main.go
+++ plugins/cilium-cni/main.go
@@ -483,6 +483,8 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 		ep.DatapathConfiguration.ExternalIpam = true
 	}
 
+	res := &cniTypesV1.Result{}
+
 	switch conf.DatapathMode {
 	case datapathOption.DatapathModeVeth:
 		var (
@@ -504,6 +506,11 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 			}
 		}()
 
+		res.Interfaces = append(res.Interfaces, &cniTypesV1.Interface{
+			Name: veth.Attrs().Name,
+			Mac:  veth.Attrs().HardwareAddr.String(),
+		})
+
 		if err = netlink.LinkSetNsFd(peer, int(netNs.Fd())); err != nil {
 			return fmt.Errorf("unable to move veth pair '%v' to netns: %s", peer, err)
 		}
@@ -517,8 +524,6 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 	state := CmdState{
 		HostAddr: ipam.HostAddressing,
 	}
-
-	res := &cniTypesV1.Result{}
 
 	if !ipv6IsEnabled(ipam) && !ipv4IsEnabled(ipam) {
 		return fmt.Errorf("IPAM did not provide IPv4 or IPv6 address")
@@ -537,6 +542,8 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 		if err != nil {
 			return fmt.Errorf("unable to prepare IP addressing for '%s': %s", ep.Addressing.IPV6, err)
 		}
+		// set the addresses interface index to that of the container-side veth
+		ipConfig.Interface = cniTypesV1.Int(len(res.Interfaces))
 		res.IPs = append(res.IPs, ipConfig)
 		res.Routes = append(res.Routes, routes...)
 	}
@@ -550,6 +557,8 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 		if err != nil {
 			return fmt.Errorf("unable to prepare IP addressing for '%s': %s", ep.Addressing.IPV4, err)
 		}
+		// set the addresses interface index to that of the container-side veth
+		ipConfig.Interface = cniTypesV1.Int(len(res.Interfaces))
 		res.IPs = append(res.IPs, ipConfig)
 		res.Routes = append(res.Routes, routes...)
 	}
@@ -580,11 +589,6 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 		Mac:     macAddrStr,
 		Sandbox: args.Netns,
 	})
-
-	// Add to the result the Interface as index of Interfaces
-	for i := range res.Interfaces {
-		res.IPs[i].Interface = cniTypesV1.Int(i)
-	}
 
 	// Specify that endpoint must be regenerated synchronously. See GH-4409.
 	ep.SyncBuildEndpoint = true
