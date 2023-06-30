2152bcbe0c936302347f7d00143146448b9ebdfb
Nate Sweet <nathanjsweet@pm.me>
2023-06-30 14:54:24 -0500

    Update k8s_version.txt with 1.27.2 Remove all references to 1.24 Generate Files
    Signed-off-by: Nate Sweet <nathanjsweet@pm.me>

--- test/controlplane/suite/testcase.go
+++ test/controlplane/suite/testcase.go
@@ -391,18 +391,17 @@ var (
 	// This is mostly relevant for the feature detection at pkg/k8s/version/version.go.
 	// The lists here are currently not exhaustive and expanded on need-by-need basis.
 	apiResources = map[string][]*metav1.APIResourceList{
-		"1.24": {
-			corev1APIResources,
-			discoveryV1APIResources,
-			discoveryV1beta1APIResources,
-			ciliumv2APIResources,
-		},
 		"1.25": {
 			corev1APIResources,
 			discoveryV1APIResources,
 			ciliumv2APIResources,
 		},
 		"1.26": {
+			corev1APIResources,
+			discoveryV1APIResources,
+			ciliumv2APIResources,
+		},
+		"1.27": {
 			corev1APIResources,
 			discoveryV1APIResources,
 			ciliumv2APIResources,
