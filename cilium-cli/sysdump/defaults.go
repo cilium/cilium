b77c9d2541c0cb2d28b73bf7026564e508757cbc
Tam Mach <tam.mach@cilium.io>
2023-06-30 20:07:52 +1000

    sysdump: Use intermediate import package for supported envoy resources
    [ cherry-picked from cilium/cilium-cli repository ]
    
    This is to avoid the similar happening again in the future, when we
    add more envoy resource types.
    
    Relates: https://github.com/cilium/cilium-cli/pull/1756
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- cilium-cli/sysdump/defaults.go
+++ cilium-cli/sysdump/defaults.go
@@ -6,34 +6,6 @@ package sysdump
 import (
 	"runtime"
 	"time"
-
-	// for envoy unmarshalling
-	_ "github.com/cilium/proxy/go/envoy/config/cluster/v3"
-	_ "github.com/cilium/proxy/go/envoy/config/core/v3"
-	_ "github.com/cilium/proxy/go/envoy/config/endpoint/v3"
-	_ "github.com/cilium/proxy/go/envoy/config/listener/v3"
-	_ "github.com/cilium/proxy/go/envoy/config/route/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/clusters/dynamic_forward_proxy/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/http/dynamic_forward_proxy/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/http/ext_authz/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/http/local_ratelimit/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/http/ratelimit/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/http/router/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/http/set_metadata/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/listener/tls_inspector/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/network/connection_limit/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/network/ext_authz/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/network/http_connection_manager/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/network/local_ratelimit/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/network/ratelimit/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/network/sni_cluster/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/network/sni_dynamic_forward_proxy/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/filters/network/tcp_proxy/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/transport_sockets/tls/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/upstreams/http/http/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/upstreams/http/tcp/v3"
-	_ "github.com/cilium/proxy/go/envoy/extensions/upstreams/http/v3"
-	_ "github.com/cilium/proxy/go/envoy/type/matcher/v3"
 )
 
 const (
