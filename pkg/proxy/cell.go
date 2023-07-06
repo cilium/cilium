ee5c22efb887f589048827eadca30fc3bc026bac
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-07-06 07:07:30 +0200

    proxy: redirect integration
    Until now, the proxy had to be aware of internals of the proxy
    implementations (Envoy & FQDN), to be able to create redirections for
    these types.
    
    This commit introduces proxyintegration components for the actual proxy
    implementations which created separately in the proxy cell and passed to
    the proxy.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/proxy/cell.go
+++ pkg/proxy/cell.go
@@ -7,7 +7,6 @@ import (
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/envoy"
 	"github.com/cilium/cilium/pkg/hive/cell"
-	"github.com/cilium/cilium/pkg/ipcache"
 	monitoragent "github.com/cilium/cilium/pkg/monitor/agent"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/proxy/logger"
@@ -22,17 +21,20 @@ var Cell = cell.Module(
 	"L7 Proxy provides support for L7 network policies",
 
 	cell.Provide(newProxy),
+	cell.Provide(newEnvoyProxyIntegration),
+	cell.Provide(newDNSProxyIntegration),
 	cell.ProvidePrivate(endpoint.NewEndpointInfoRegistry),
 )
 
 type proxyParams struct {
 	cell.In
 
-	IPCache              *ipcache.IPCache
-	Datapath             datapath.Datapath
-	EndpointInfoRegistry logger.EndpointInfoRegistry
-	MonitorAgent         monitoragent.Agent
-	XdsServer            envoy.XDSServer
+	Datapath              datapath.Datapath
+	EndpointInfoRegistry  logger.EndpointInfoRegistry
+	MonitorAgent          monitoragent.Agent
+	EnvoyProxyIntegration *envoyProxyIntegration
+	DNSProxyIntegration   *dnsProxyIntegration
+	XdsServer             envoy.XDSServer
 }
 
 func newProxy(params proxyParams) *Proxy {
@@ -47,7 +49,34 @@ func newProxy(params proxyParams) *Proxy {
 	configureProxyLogger(params.EndpointInfoRegistry, params.MonitorAgent, option.Config.AgentLabels)
 
 	// FIXME: Make the port range configurable.
-	return createProxy(10000, 20000, option.Config.RunDir, params.Datapath, params.EndpointInfoRegistry, params.XdsServer)
+	return createProxy(10000, 20000, option.Config.RunDir, params.Datapath, params.EnvoyProxyIntegration, params.DNSProxyIntegration, params.XdsServer)
+}
+
+type envoyProxyIntegrationParams struct {
+	cell.In
+
+	Datapath  datapath.Datapath
+	XdsServer envoy.XDSServer
+}
+
+func newEnvoyProxyIntegration(params envoyProxyIntegrationParams) *envoyProxyIntegration {
+	if !option.Config.EnableL7Proxy {
+		return nil
+	}
+
+	return &envoyProxyIntegration{
+		runDir:    option.Config.RunDir,
+		xdsServer: params.XdsServer,
+		datapath:  params.Datapath,
+	}
+}
+
+func newDNSProxyIntegration() *dnsProxyIntegration {
+	if !option.Config.EnableL7Proxy {
+		return nil
+	}
+
+	return &dnsProxyIntegration{}
 }
 
 func configureProxyLogger(eir logger.EndpointInfoRegistry, monitorAgent monitoragent.Agent, agentLabels []string) {
