cilium/cilium

commit 43dffb2981730d5afcc75ae794d00d2f1bb330a1

bpf: encap: manually set src IP and port from nodeport XDP paths
In preparation for manually building the tunnel headers from XDP context,
pass the outer header's Src IP and Port to the encap helpers.

Set the OuterSrcIP as IPV4_DIRECT_ROUTING. For the OuterSrcPort we're
simply re-using TUNNEL_PORT for now, but this should eventually turn into
a hash of the flow's 5-tuple
(see https://www.rfc-editor.org/rfc/rfc8926.html#name-udp-header).

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -288,11 +288,16 @@ static __always_inline int encap_geneve_dsr_opt6(struct __ctx_buff *ctx,
 	dst = (union v6addr *)&ip6->daddr;
 	info = ipcache_lookup6(&IPCACHE_MAP, dst, V6_CACHE_KEY_LEN, 0);
 	if (info && info->tunnel_endpoint != 0) {
+		__be16 src_port = tunnel_gen_src_port_v6();
+
 		if (need_opt) {
 			struct geneve_dsr_opt6 gopt;
 
 			set_geneve_dsr_opt6(svc_port, svc_addr, &gopt);
-			return  __encap_with_nodeid_opt(ctx, info->tunnel_endpoint,
+			return  __encap_with_nodeid_opt(ctx,
+							IPV4_DIRECT_ROUTING,
+							src_port,
+							info->tunnel_endpoint,
 							WORLD_ID,
 							info->sec_identity,
 							NOT_VTEP_DST,
@@ -304,7 +309,10 @@ static __always_inline int encap_geneve_dsr_opt6(struct __ctx_buff *ctx,
 							ifindex);
 		}
 
-		return __encap_with_nodeid(ctx, 0, info->tunnel_endpoint,
+		return __encap_with_nodeid(ctx,
+					   IPV4_DIRECT_ROUTING,
+					   src_port,
+					   info->tunnel_endpoint,
 					   WORLD_ID,
 					   info->sec_identity,
 					   NOT_VTEP_DST,
@@ -859,7 +867,12 @@ int tail_nodeport_nat_egress_ipv6(struct __ctx_buff *ctx)
 	ctx_snat_done_set(ctx);
 #ifdef TUNNEL_MODE
 	if (tunnel_endpoint) {
-		ret = __encap_with_nodeid(ctx, 0, tunnel_endpoint,
+		__be16 src_port = tunnel_gen_src_port_v6();
+
+		ret = __encap_with_nodeid(ctx,
+					  IPV4_DIRECT_ROUTING,
+					  src_port,
+					  tunnel_endpoint,
 					  WORLD_ID,
 					  dst_sec_identity,
 					  NOT_VTEP_DST,
@@ -1156,6 +1169,7 @@ static __always_inline int rev_nodeport_lb6(struct __ctx_buff *ctx, __s8 *ext_er
 	__u32 monitor = TRACE_PAYLOAD_LEN;
 	__u32 tunnel_endpoint __maybe_unused = 0;
 	__u32 dst_sec_identity __maybe_unused = 0;
+	__be16 src_port __maybe_unused = 0;
 	int ifindex = 0;
 
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
@@ -1229,7 +1243,10 @@ out:
 	return DROP_MISSED_TAIL_CALL;
 #ifdef TUNNEL_MODE
 encap_redirect:
-	ret = __encap_with_nodeid(ctx, 0, tunnel_endpoint, SECLABEL, dst_sec_identity,
+	src_port = tunnel_gen_src_port_v6();
+
+	ret = __encap_with_nodeid(ctx, IPV4_DIRECT_ROUTING, src_port,
+				  tunnel_endpoint, SECLABEL, dst_sec_identity,
 				  NOT_VTEP_DST, reason, monitor, &ifindex);
 	if (ret == CTX_ACT_REDIRECT)
 		ret = ctx_redirect(ctx, ifindex, 0);
@@ -1565,11 +1582,16 @@ static __always_inline int encap_geneve_dsr_opt4(struct __ctx_buff *ctx,
 
 	info = ipcache_lookup4(&IPCACHE_MAP, ip4->daddr, V4_CACHE_KEY_LEN, 0);
 	if (info && info->tunnel_endpoint != 0) {
+		__be16 src_port = tunnel_gen_src_port_v4();
+
 		if (need_opt) {
 			struct geneve_dsr_opt4 gopt;
 
 			set_geneve_dsr_opt4(svc_port, svc_addr, &gopt);
-			return  __encap_with_nodeid_opt(ctx, info->tunnel_endpoint,
+			return  __encap_with_nodeid_opt(ctx,
+							IPV4_DIRECT_ROUTING,
+							src_port,
+							info->tunnel_endpoint,
 							WORLD_ID,
 							info->sec_identity,
 							NOT_VTEP_DST,
@@ -1581,7 +1603,10 @@ static __always_inline int encap_geneve_dsr_opt4(struct __ctx_buff *ctx,
 							ifindex);
 		}
 
-		return __encap_with_nodeid(ctx, 0, info->tunnel_endpoint,
+		return __encap_with_nodeid(ctx,
+					   IPV4_DIRECT_ROUTING,
+					   src_port,
+					   info->tunnel_endpoint,
 					   WORLD_ID,
 					   info->sec_identity,
 					   NOT_VTEP_DST,
@@ -2071,6 +2096,8 @@ int tail_nodeport_nat_egress_ipv4(struct __ctx_buff *ctx)
 	ctx_snat_done_set(ctx);
 #ifdef TUNNEL_MODE
 	if (tunnel_endpoint) {
+		__be16 src_port = tunnel_gen_src_port_v4();
+
 		/* The request came from outside, so we need to
 		 * set the security id in the tunnel header to WORLD_ID.
 		 * Otherwise, the remote node will assume, that the
@@ -2078,7 +2105,10 @@ int tail_nodeport_nat_egress_ipv4(struct __ctx_buff *ctx)
 		 * bypass any netpol which disallows LB requests from
 		 * outside.
 		 */
-		ret = __encap_with_nodeid(ctx, 0, tunnel_endpoint,
+		ret = __encap_with_nodeid(ctx,
+					  IPV4_DIRECT_ROUTING,
+					  src_port,
+					  tunnel_endpoint,
 					  WORLD_ID,
 					  dst_sec_identity,
 					  NOT_VTEP_DST,
@@ -2405,6 +2435,7 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, __s8 *ext_er
 	__u32 monitor = TRACE_PAYLOAD_LEN;
 	__u32 tunnel_endpoint __maybe_unused = 0;
 	__u32 dst_sec_identity __maybe_unused = 0;
+	__be16 src_port __maybe_unused = 0;
 	bool has_l4_header;
 
 	if (!revalidate_data(ctx, &data, &data_end, &ip4))
@@ -2467,7 +2498,10 @@ out:
 	return DROP_MISSED_TAIL_CALL;
 #if defined(ENABLE_EGRESS_GATEWAY) || defined(TUNNEL_MODE)
 encap_redirect:
-	ret = __encap_with_nodeid(ctx, 0, tunnel_endpoint, SECLABEL, dst_sec_identity,
+	src_port = tunnel_gen_src_port_v4();
+
+	ret = __encap_with_nodeid(ctx, IPV4_DIRECT_ROUTING, src_port,
+				  tunnel_endpoint, SECLABEL, dst_sec_identity,
 				  NOT_VTEP_DST, reason, monitor, &ifindex);
 	if (ret == CTX_ACT_REDIRECT)
 		ret = ctx_redirect(ctx, ifindex, 0);

bpf/lib/encap.h
@@ -31,7 +31,8 @@ encap_and_redirect_ipsec(struct __ctx_buff *ctx, __u8 key, __u16 node_id,
 #endif /* ENABLE_IPSEC */
 
 static __always_inline int
-__encap_with_nodeid(struct __ctx_buff *ctx, __u32 src_ip, __be32 tunnel_endpoint,
+__encap_with_nodeid(struct __ctx_buff *ctx, __u32 src_ip, __be16 src_port,
+		    __be32 tunnel_endpoint,
 		    __u32 seclabel, __u32 dstid, __u32 vni __maybe_unused,
 		    enum trace_reason ct_reason, __u32 monitor, int *ifindex)
 {
@@ -49,8 +50,8 @@ __encap_with_nodeid(struct __ctx_buff *ctx, __u32 src_ip, __be32 tunnel_endpoint
 
 	cilium_dbg(ctx, DBG_ENCAP, node_id, seclabel);
 
-	ret = ctx_set_encap_info(ctx, src_ip, node_id, seclabel, dstid, vni,
-				 NULL, 0, false, ifindex);
+	ret = ctx_set_encap_info(ctx, src_ip, src_port, node_id, seclabel,
+				 dstid, vni, NULL, 0, false, ifindex);
 	if (ret == CTX_ACT_REDIRECT)
 		send_trace_notify(ctx, TRACE_TO_OVERLAY, seclabel, dstid, 0, *ifindex,
 				  ct_reason, monitor);
@@ -82,7 +83,7 @@ __encap_and_redirect_with_nodeid(struct __ctx_buff *ctx, __u32 src_ip __maybe_un
 		return ret;
 #endif /* ENABLE_WIREGUARD */
 
-	ret = __encap_with_nodeid(ctx, src_ip, tunnel_endpoint, seclabel, dstid,
+	ret = __encap_with_nodeid(ctx, src_ip, 0, tunnel_endpoint, seclabel, dstid,
 				  vni, trace->reason, trace->monitor,
 				  &ifindex);
 	if (ret != CTX_ACT_REDIRECT)
@@ -133,7 +134,7 @@ __encap_and_redirect_lxc(struct __ctx_buff *ctx, __be32 tunnel_endpoint,
 	 * the tunnel, to apply the correct reverse DNAT.
 	 * See #14674 for details.
 	 */
-	ret = __encap_with_nodeid(ctx, 0, tunnel_endpoint, seclabel, dstid,
+	ret = __encap_with_nodeid(ctx, 0, 0, tunnel_endpoint, seclabel, dstid,
 				  NOT_VTEP_DST, trace->reason, trace->monitor,
 				  &ifindex);
 	if (ret != CTX_ACT_REDIRECT)
@@ -214,9 +215,30 @@ encap_and_redirect_netdev(struct __ctx_buff *ctx, struct tunnel_key *k,
 }
 #endif /* TUNNEL_MODE || ENABLE_HIGH_SCALE_IPCACHE */
 
+static __always_inline __be16 tunnel_gen_src_port_v4(void)
+{
+#if __ctx_is == __ctx_xdp
+	/* TODO hash, based on CT tuple */
+	return bpf_htons(TUNNEL_PORT);
+#else
+	return 0;
+#endif
+}
+
+static __always_inline __be16 tunnel_gen_src_port_v6(void)
+{
+#if __ctx_is == __ctx_xdp
+	/* TODO hash, based on CT tuple */
+	return bpf_htons(TUNNEL_PORT);
+#else
+	return 0;
+#endif
+}
+
 #if defined(ENABLE_DSR) && DSR_ENCAP_MODE == DSR_ENCAP_GENEVE
 static __always_inline int
-__encap_with_nodeid_opt(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
+__encap_with_nodeid_opt(struct __ctx_buff *ctx, __u32 src_ip, __be16 src_port,
+			__u32 tunnel_endpoint,
 			__u32 seclabel, __u32 dstid, __u32 vni,
 			void *opt, __u32 opt_len, bool is_ipv6,
 			enum trace_reason ct_reason,
@@ -236,7 +258,7 @@ __encap_with_nodeid_opt(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 
 	cilium_dbg(ctx, DBG_ENCAP, node_id, seclabel);
 
-	ret = ctx_set_encap_info(ctx, 0, node_id, seclabel, dstid, vni,
+	ret = ctx_set_encap_info(ctx, src_ip, src_port, node_id, seclabel, dstid, vni,
 				 opt, opt_len, is_ipv6, ifindex);
 	if (ret == CTX_ACT_REDIRECT)
 		send_trace_notify(ctx, TRACE_TO_OVERLAY, seclabel, dstid, 0, *ifindex,
@@ -253,7 +275,7 @@ encap_and_redirect_with_nodeid_opt(struct __ctx_buff *ctx, __u32 tunnel_endpoint
 {
 	int ifindex = 0;
 
-	int ret = __encap_with_nodeid_opt(ctx, tunnel_endpoint, seclabel, dstid,
+	int ret = __encap_with_nodeid_opt(ctx, 0, 0, tunnel_endpoint, seclabel, dstid,
 					  vni, opt, opt_len, is_ipv6,
 					  trace->reason, trace->monitor, &ifindex);
 	if (ret != CTX_ACT_REDIRECT)

bpf/lib/overloadable_skb.h
@@ -185,7 +185,8 @@ static __always_inline bool ctx_snat_done(const struct __sk_buff *ctx)
 
 #ifdef HAVE_ENCAP
 static __always_inline __maybe_unused int
-ctx_set_encap_info(struct __sk_buff *ctx, __u32 src_ip, __u32 node_id,
+ctx_set_encap_info(struct __sk_buff *ctx, __u32 src_ip,
+		   __be16 src_port __maybe_unused, __u32 node_id,
 		   __u32 seclabel, __u32 dstid __maybe_unused,
 		   __u32 vni __maybe_unused,
 		   void *opt, __u32 opt_len, bool is_ipv6 __maybe_unused,

bpf/lib/overloadable_xdp.h
@@ -179,6 +179,7 @@ static __always_inline bool ctx_snat_done(struct xdp_md *ctx)
 #ifdef HAVE_ENCAP
 static __always_inline __maybe_unused int
 ctx_set_encap_info(struct xdp_md *ctx __maybe_unused, __u32 src_ip __maybe_unused,
+		   __be16 src_port __maybe_unused,
 		   __u32 node_id __maybe_unused,
 		   __u32 seclabel __maybe_unused,
 		   __u32 dstid __maybe_unused,

commit bafcfcd78f82e1ebc8f518eb6fd06066b4373057

bpf: nodeport: only set outer src IP for tunnel encap in XDP
As part of introducing native tunnel support for XDP, we started passing
a `src_ip` parameter to `__encap_with_nodeid()`. This was needed to
manually build the packet's outer headers.

But for TC context we actually don't want to specify the outer src IP, and
defer its selection to the kernel stack instead. Worse, specifying the
outer src IP means that the skb implementation for `ctx_set_encap_info()`
starts to use the `local_ipv4` field in the `bpf_tunnel_key`. And that's
not supported on older kernels, resulting in drops.

Fix this by only setting the `src_ip` parameter in XDP context.

Reported-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
Fixes: 43dffb298173 ("bpf: encap: manually set src IP and port from nodeport XDP paths")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -72,6 +72,39 @@ bpf_skip_recirculation(const struct __ctx_buff *ctx __maybe_unused)
 #endif
 }
 
+#ifdef HAVE_ENCAP
+static __always_inline int
+nodeport_add_tunnel_encap(struct __ctx_buff *ctx, __u32 src_ip, __be16 src_port,
+			  __be32 dst_ip, __u32 src_sec_identity, __u32 dst_sec_identity,
+			  enum trace_reason ct_reason, __u32 monitor, int *ifindex)
+{
+	/* Let kernel choose the outer source ip */
+	if (ctx_is_skb())
+		src_ip = 0;
+
+	return __encap_with_nodeid(ctx, src_ip, src_port, dst_ip,
+				   src_sec_identity, dst_sec_identity, NOT_VTEP_DST,
+				   ct_reason, monitor, ifindex);
+}
+
+# if defined(ENABLE_DSR) && DSR_ENCAP_MODE == DSR_ENCAP_GENEVE
+static __always_inline int
+nodeport_add_tunnel_encap_opt(struct __ctx_buff *ctx, __u32 src_ip, __be16 src_port,
+			      __be32 dst_ip, __u32 src_sec_identity, __u32 dst_sec_identity,
+			      void *opt, __u32 opt_len, enum trace_reason ct_reason,
+			      __u32 monitor, int *ifindex)
+{
+	/* Let kernel choose the outer source ip */
+	if (ctx_is_skb())
+		src_ip = 0;
+
+	return __encap_with_nodeid_opt(ctx, src_ip, src_port, dst_ip,
+				       src_sec_identity, dst_sec_identity, NOT_VTEP_DST,
+				       opt, opt_len, ct_reason, monitor, ifindex);
+}
+# endif
+#endif /* HAVE_ENCAP */
+
 static __always_inline bool dsr_fail_needs_reply(int code __maybe_unused)
 {
 #ifdef ENABLE_DSR_ICMP_ERRORS
@@ -300,29 +333,27 @@ static __always_inline int encap_geneve_dsr_opt6(struct __ctx_buff *ctx,
 	}
 
 	if (need_opt)
-		return  __encap_with_nodeid_opt(ctx,
-						IPV4_DIRECT_ROUTING,
-						src_port,
-						tunnel_endpoint,
-						WORLD_ID,
-						dst_sec_identity,
-						NOT_VTEP_DST,
-						&gopt,
-						sizeof(gopt),
-						(enum trace_reason)CT_NEW,
-						TRACE_PAYLOAD_LEN,
-						ifindex);
+		return nodeport_add_tunnel_encap_opt(ctx,
+						     IPV4_DIRECT_ROUTING,
+						     src_port,
+						     tunnel_endpoint,
+						     WORLD_ID,
+						     dst_sec_identity,
+						     &gopt,
+						     sizeof(gopt),
+						     (enum trace_reason)CT_NEW,
+						     TRACE_PAYLOAD_LEN,
+						     ifindex);
 
-	return __encap_with_nodeid(ctx,
-				   IPV4_DIRECT_ROUTING,
-				   src_port,
-				   tunnel_endpoint,
-				   WORLD_ID,
-				   dst_sec_identity,
-				   NOT_VTEP_DST,
-				   (enum trace_reason)CT_NEW,
-				   TRACE_PAYLOAD_LEN,
-				   ifindex);
+	return nodeport_add_tunnel_encap(ctx,
+					 IPV4_DIRECT_ROUTING,
+					 src_port,
+					 tunnel_endpoint,
+					 WORLD_ID,
+					 dst_sec_identity,
+					 (enum trace_reason)CT_NEW,
+					 TRACE_PAYLOAD_LEN,
+					 ifindex);
 }
 #endif /* DSR_ENCAP_MODE */
 
@@ -898,15 +929,14 @@ int tail_nodeport_nat_egress_ipv6(struct __ctx_buff *ctx)
 
 		src_port = tunnel_gen_src_port_v6(&tuple);
 
-		ret = __encap_with_nodeid(ctx,
-					  IPV4_DIRECT_ROUTING,
-					  src_port,
-					  tunnel_endpoint,
-					  WORLD_ID,
-					  dst_sec_identity,
-					  NOT_VTEP_DST,
-					  (enum trace_reason)CT_NEW,
-					  TRACE_PAYLOAD_LEN, &oif);
+		ret = nodeport_add_tunnel_encap(ctx,
+						IPV4_DIRECT_ROUTING,
+						src_port,
+						tunnel_endpoint,
+						WORLD_ID,
+						dst_sec_identity,
+						(enum trace_reason)CT_NEW,
+						TRACE_PAYLOAD_LEN, &oif);
 		if (IS_ERR(ret))
 			goto drop_err;
 
@@ -1261,9 +1291,9 @@ out:
 encap_redirect:
 	src_port = tunnel_gen_src_port_v6(&tuple);
 
-	ret = __encap_with_nodeid(ctx, IPV4_DIRECT_ROUTING, src_port,
-				  tunnel_endpoint, SECLABEL, dst_sec_identity,
-				  NOT_VTEP_DST, reason, monitor, &ifindex);
+	ret = nodeport_add_tunnel_encap(ctx, IPV4_DIRECT_ROUTING, src_port,
+					tunnel_endpoint, SECLABEL, dst_sec_identity,
+					reason, monitor, &ifindex);
 	if (IS_ERR(ret))
 		return ret;
 
@@ -1719,29 +1749,27 @@ static __always_inline int encap_geneve_dsr_opt4(struct __ctx_buff *ctx, int l3_
 #endif
 
 	if (need_opt)
-		return  __encap_with_nodeid_opt(ctx,
-						IPV4_DIRECT_ROUTING,
-						src_port,
-						tunnel_endpoint,
-						src_sec_identity,
-						dst_sec_identity,
-						NOT_VTEP_DST,
-						&gopt,
-						sizeof(gopt),
-						(enum trace_reason)CT_NEW,
-						TRACE_PAYLOAD_LEN,
-						ifindex);
+		return nodeport_add_tunnel_encap_opt(ctx,
+						     IPV4_DIRECT_ROUTING,
+						     src_port,
+						     tunnel_endpoint,
+						     src_sec_identity,
+						     dst_sec_identity,
+						     &gopt,
+						     sizeof(gopt),
+						     (enum trace_reason)CT_NEW,
+						     TRACE_PAYLOAD_LEN,
+						     ifindex);
 
-	return __encap_with_nodeid(ctx,
-				   IPV4_DIRECT_ROUTING,
-				   src_port,
-				   tunnel_endpoint,
-				   src_sec_identity,
-				   dst_sec_identity,
-				   NOT_VTEP_DST,
-				   (enum trace_reason)CT_NEW,
-				   TRACE_PAYLOAD_LEN,
-				   ifindex);
+	return nodeport_add_tunnel_encap(ctx,
+					 IPV4_DIRECT_ROUTING,
+					 src_port,
+					 tunnel_endpoint,
+					 src_sec_identity,
+					 dst_sec_identity,
+					 (enum trace_reason)CT_NEW,
+					 TRACE_PAYLOAD_LEN,
+					 ifindex);
 }
 #endif /* DSR_ENCAP_MODE */
 
@@ -2240,15 +2268,14 @@ int tail_nodeport_nat_egress_ipv4(struct __ctx_buff *ctx)
 		 * bypass any netpol which disallows LB requests from
 		 * outside.
 		 */
-		ret = __encap_with_nodeid(ctx,
-					  IPV4_DIRECT_ROUTING,
-					  src_port,
-					  tunnel_endpoint,
-					  WORLD_ID,
-					  dst_sec_identity,
-					  NOT_VTEP_DST,
-					  (enum trace_reason)CT_NEW,
-					  TRACE_PAYLOAD_LEN, &oif);
+		ret = nodeport_add_tunnel_encap(ctx,
+						IPV4_DIRECT_ROUTING,
+						src_port,
+						tunnel_endpoint,
+						WORLD_ID,
+						dst_sec_identity,
+						(enum trace_reason)CT_NEW,
+						TRACE_PAYLOAD_LEN, &oif);
 		if (IS_ERR(ret))
 			goto drop_err;
 
@@ -2660,9 +2687,9 @@ out:
 encap_redirect:
 	src_port = tunnel_gen_src_port_v4(&tuple);
 
-	ret = __encap_with_nodeid(ctx, IPV4_DIRECT_ROUTING, src_port,
-				  tunnel_endpoint, SECLABEL, dst_sec_identity,
-				  NOT_VTEP_DST, reason, monitor, &ifindex);
+	ret = nodeport_add_tunnel_encap(ctx, IPV4_DIRECT_ROUTING, src_port,
+					tunnel_endpoint, SECLABEL, dst_sec_identity,
+					reason, monitor, &ifindex);
 	if (IS_ERR(ret))
 		return ret;
 