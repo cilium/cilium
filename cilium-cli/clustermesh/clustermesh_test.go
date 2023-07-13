1eb0cd0b256920fdd88466c03d2b50da2be84fda
Marco Iorio <marco.iorio@isovalent.com>
2023-07-13 12:41:47 +0200

    clustermesh status: improve error reporting when not ready
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Let's leverage the additional status flags introduced in
    cilium/cilium#26788 to provide more detailed information
    concerning why a node cannot connect to a remote cluster,
    and simplify troubleshooting. In particular, it might be
    due to an etcd connection problem, to the cluster config
    being required but not found, or to the synchronization
    process being still in progress.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- cilium-cli/clustermesh/clustermesh_test.go
+++ cilium-cli/clustermesh/clustermesh_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/cilium/cilium/api/v1/models"
 	"github.com/stretchr/testify/assert"
 	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/release"
@@ -202,6 +203,74 @@ func TestRemoveFromClustermeshConfig(t *testing.T) {
 				return
 			}
 			assert.Equal(t, u.e, ee)
+		})
+	}
+}
+
+func TestRemoteClusterStatusToError(t *testing.T) {
+	tests := []struct {
+		name     string
+		status   *models.RemoteCluster
+		expected string
+	}{
+		{
+			name:     "nil status",
+			expected: "unknown status",
+		},
+		{
+			name:     "not connected",
+			status:   &models.RemoteCluster{Status: "foo"},
+			expected: "foo",
+		},
+		{
+			name:     "connected, unknown config",
+			status:   &models.RemoteCluster{Connected: true},
+			expected: "remote cluster configuration retrieval status unknown",
+		},
+		{
+			name: "connected, config not found",
+			status: &models.RemoteCluster{
+				Connected: true, Config: &models.RemoteClusterConfig{Required: true}},
+			expected: "remote cluster configuration required but not found",
+		},
+		{
+			name: "connected, config not required, sync status unknown",
+			status: &models.RemoteCluster{
+				Connected: true, Config: &models.RemoteClusterConfig{}},
+			expected: "synchronization status unknown",
+		},
+		{
+			name: "connected, config found, no resource type synced",
+			status: &models.RemoteCluster{
+				Connected: true,
+				Config:    &models.RemoteClusterConfig{Required: true, Retrieved: true},
+				Synced:    &models.RemoteClusterSynced{},
+			},
+			expected: "synchronization in progress for endpoints, identities, nodes, services",
+		},
+		{
+			name: "connected, config found, some resource type not synced",
+			status: &models.RemoteCluster{
+				Connected: true,
+				Config:    &models.RemoteClusterConfig{Required: true, Retrieved: true},
+				Synced:    &models.RemoteClusterSynced{Endpoints: true, Nodes: true, Services: true},
+			},
+			expected: "synchronization in progress for identities",
+		},
+		{
+			name: "none of the above",
+			status: &models.RemoteCluster{
+				Connected: true,
+				Config:    &models.RemoteClusterConfig{Required: true, Retrieved: true},
+				Synced:    &models.RemoteClusterSynced{Endpoints: true, Identities: true, Nodes: true, Services: true},
+			},
+			expected: "not ready",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, remoteClusterStatusToError(tt.status).Error())
 		})
 	}
 }
