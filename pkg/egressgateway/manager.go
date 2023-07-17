cilium/cilium

commit 9b9e7c2a6259fa583e456aef24543b180390c100

egressgw: improve removal of IP routes
To delete stale EgressGW-specific IP routes, we currently iterate over all
interfaces on the node. If an interface is not in active use by the
EgressGW, we fetch any old EgressGW routes and delete them.

But under the covers, the netlink.RouteListFiltered() will *fetch* all
routes and only then apply the filter. Considering the potential number of
links on a worker node with many pods, this can add up to quite a bit of
overhead.

Take a different approach instead - fetch all routes just *once*, and then
simply delete all those EgressGW-associated routes that don't point to an
active EgressGW interface.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

pkg/egressgateway/manager.go
@@ -630,21 +630,27 @@ nextIpRule:
 		}
 	}
 
-	// Then go through each interface on the node
-	links, err := netlink.LinkList()
+	// Fetch all IP routes, and delete the unused EgressGW-specific routes:
+	routes, err := netlink.RouteList(nil, netlink.FAMILY_V4)
 	if err != nil {
-		logger.WithError(err).Error("Cannot list interfaces")
+		logger.WithError(err).Error("Cannot list IP routes")
 		return
 	}
 
-	for _, l := range links {
-		// If egress gateway is active for this interface, move to the next interface
-		if _, ok := activeEgressGwIfaceIndexes[l.Attrs().Index]; ok {
+	for _, route := range routes {
+		linkIndex := route.LinkIndex
+
+		// Keep the route if it was not created by EgressGW.
+		if route.Table != egressGatewayRoutingTableIdx(linkIndex) {
 			continue
 		}
 
-		// Otherwise delete the whole routing table for that interface
-		deleteIpRouteTable(egressGatewayRoutingTableIdx(l.Attrs().Index))
+		// Keep the route if EgressGW still uses this interface.
+		if _, ok := activeEgressGwIfaceIndexes[linkIndex]; ok {
+			continue
+		}
+
+		deleteIpRoute(route)
 	}
 }
 

pkg/egressgateway/net.go
@@ -159,19 +159,6 @@ func deleteIpRule(ipRule netlink.Rule) {
 		})
 }
 
-func deleteIpRouteTable(tableIndex int) {
-	routes, err := netlink.RouteListFiltered(netlink.FAMILY_V4,
-		&netlink.Route{Table: tableIndex}, uint64(netlink.RT_FILTER_TABLE))
-	if err != nil {
-		log.WithError(err).Error("Cannot list IP routes")
-		return
-	}
-
-	for _, route := range routes {
-		deleteIpRoute(route)
-	}
-}
-
 func deleteIpRoute(ipRoute netlink.Route) {
 	logger := log.WithFields(logrus.Fields{})
 

commit 569da6d69ef54b5c9a80253cb2ff063e2c2e7e02

egressgw: fix up removal for IP routes
Without specifying a filter, we're only getting the routes from the default
table. So explicitly get the routes from all tables for the reconciliation
of stale EgressGW IP routes.

Fixes: 9b9e7c2a6259 ("egressgw: improve removal of IP routes")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

pkg/egressgateway/manager.go
@@ -15,6 +15,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
 
@@ -768,7 +769,10 @@ nextIpRule:
 	}
 
 	// Fetch all IP routes, and delete the unused EgressGW-specific routes:
-	routes, err := netlink.RouteList(nil, netlink.FAMILY_V4)
+	routes, err := netlink.RouteListFiltered(netlink.FAMILY_V4, &netlink.Route{
+		Table: unix.RT_TABLE_UNSPEC,
+	}, netlink.RT_FILTER_TABLE)
+
 	if err != nil {
 		logger.WithError(err).Error("Cannot list IP routes")
 		return