cilium/cilium

commit 7dbd40f280661fac757f411dc04f98be93cebbb5

ipsec: Remove workarounds for path asymmetry that was removed
[ upstream commit 0a8f2c4ee43e55d8f82e67bba2af186435ff4a29 ]

Commits 3b3e8d0b1 ("node: Don't encrypt traffic to CiliumInternalIP")
and 5fe2b2d6d ("bpf: Don't encrypt on path hostns -> remote pod")
removed a path asymmetry on the paths hostns <> remote pod. They however
failed to remove workarounds that we have for this path asymmetry.

In particular, we would encrypt packets on the path pod -> remote node
(set SPI in the node manager) to try and avoid the path asymmetry, by
also encrypting the replies. And, as a result of this first change, we
would also need to handle a corner case in the datapath to appluy the
correct reverse DNAT for reply traffic.

All of that is unnecessary now that we fixed the path asymmetry.

Fixes: 3b3e8d0b1 ("node: Don't encrypt traffic to CiliumInternalIP")
Fixes: 5fe2b2d6d ("bpf: Don't encrypt on path hostns -> remote pod")
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

pkg/node/manager/manager.go
@@ -65,7 +65,6 @@ type Configuration interface {
 	TunnelingEnabled() bool
 	RemoteNodeIdentitiesEnabled() bool
 	NodeEncryptionEnabled() bool
-	EncryptionEnabled() bool
 }
 
 // Notifier is the interface the wraps Subscribe and Unsubscribe. An
@@ -367,11 +366,6 @@ func (m *Manager) legacyNodeIpBehavior() bool {
 	if m.conf.NodeEncryptionEnabled() {
 		return false
 	}
-	// Needed to store the tunnel endpoint for pod->remote node in the
-	// ipcache so that this traffic goes through the tunnel.
-	if m.conf.EncryptionEnabled() && m.conf.TunnelingEnabled() {
-		return false
-	}
 	return true
 }
 
@@ -411,7 +405,7 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 		// through the tunnel to preserve the source identity as part of the
 		// encapsulation. In encryption case we also want to use vxlan device
 		// to create symmetric traffic when sending nodeIP->pod and pod->nodeIP.
-		if address.Type == addressing.NodeCiliumInternalIP || m.conf.EncryptionEnabled() ||
+		if address.Type == addressing.NodeCiliumInternalIP ||
 			option.Config.EnableHostFirewall || option.Config.JoinCluster {
 			tunnelIP = nodeIP
 		}

bpf/lib/encap.h
@@ -162,12 +162,12 @@ __encap_and_redirect_lxc(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 						seclabel);
 #endif
 
-#if !defined(ENABLE_NODEPORT) && (defined(ENABLE_IPSEC) || defined(ENABLE_HOST_FIREWALL))
-	/* For IPSec and the host firewall, traffic from a pod to a remote node
-	 * is sent through the tunnel. In the case of node --> VIP@remote pod,
-	 * packets may be DNATed when they enter the remote node. If kube-proxy
-	 * is used, the response needs to go through the stack on the way to
-	 * the tunnel, to apply the correct reverse DNAT.
+#if !defined(ENABLE_NODEPORT) && defined(ENABLE_HOST_FIREWALL)
+	/* For the host firewall, traffic from a pod to a remote node is sent
+	 * through the tunnel. In the case of node --> VIP@remote pod, packets may
+	 * be DNATed when they enter the remote node. If kube-proxy is used, the
+	 * response needs to go through the stack on the way to the tunnel, to
+	 * apply the correct reverse DNAT.
 	 * See #14674 for details.
 	 */
 	ret = __encap_with_nodeid(ctx, tunnel_endpoint, seclabel, dstid, NOT_VTEP_DST,
@@ -180,7 +180,7 @@ __encap_and_redirect_lxc(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 #else
 	return __encap_and_redirect_with_nodeid(ctx, tunnel_endpoint,
 						seclabel, dstid, NOT_VTEP_DST, trace);
-#endif /* !ENABLE_NODEPORT && (ENABLE_IPSEC || ENABLE_HOST_FIREWALL) */
+#endif /* !ENABLE_NODEPORT && ENABLE_HOST_FIREWALL */
 }
 
 #ifdef TUNNEL_MODE

commit 28ac94acf91f208fd739806f21a89002acf180db

node: Fix node encryption condition in incorrect backport
Julian noticed that backported commit 7dbd40f280 ("ipsec: Remove workarounds
for path asymmetry that was removed") didn't match the upstream commit
due to a wrong conflict resolution. This commit fixes it.

We don't expect much impact here as node encryption was never stable,
barely used, and eventually removed, but best to fix it, if only for
consistency with v1.11 and v1.12.

Fixes: 7dbd40f280 ("ipsec: Remove workarounds for path asymmetry that was removed")
Reported-by: Julian Wiedmann <jwi@isovalent.com>
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

pkg/node/manager/manager.go
@@ -405,7 +405,7 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 		// through the tunnel to preserve the source identity as part of the
 		// encapsulation. In encryption case we also want to use vxlan device
 		// to create symmetric traffic when sending nodeIP->pod and pod->nodeIP.
-		if address.Type == addressing.NodeCiliumInternalIP ||
+		if address.Type == addressing.NodeCiliumInternalIP || m.conf.NodeEncryptionEnabled() ||
 			option.Config.EnableHostFirewall || option.Config.JoinCluster {
 			tunnelIP = nodeIP
 		}