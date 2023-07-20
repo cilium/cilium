24ef2f8920692f754789d435c118c1d18ddae53a
Julian Wiedmann <jwi@isovalent.com>
2023-07-20 16:14:33 +0300

    bpf: lxc: add drop notification for high-scale ipcache path in to-container
    [ upstream commit 614c174b93ae309214bf8822f79d88eda9e423d7 ]
    
    Don't just return DROP_INVALID, but also throw a drop notification.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -2214,8 +2214,10 @@ int cil_to_container(struct __ctx_buff *ctx)
 		void *data, *data_end;
 		struct ipv6hdr *ip6;
 
-		if (!revalidate_data(ctx, &data, &data_end, &ip6))
-			return DROP_INVALID;
+		if (!revalidate_data(ctx, &data, &data_end, &ip6)) {
+			ret = DROP_INVALID;
+			goto out;
+		}
 
 		ep = __lookup_ip6_endpoint((union v6addr *)&ip6->saddr);
 		if (ep)
@@ -2235,8 +2237,10 @@ int cil_to_container(struct __ctx_buff *ctx)
 		void *data, *data_end;
 		struct iphdr *ip4;
 
-		if (!revalidate_data(ctx, &data, &data_end, &ip4))
-			return DROP_INVALID;
+		if (!revalidate_data(ctx, &data, &data_end, &ip4)) {
+			ret = DROP_INVALID;
+			goto out;
+		}
 
 		ep = __lookup_ip4_endpoint(ip4->saddr);
 		if (ep)
