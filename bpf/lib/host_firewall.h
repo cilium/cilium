12d2760649e1787e3070f3d57c8b36c444e9255f
Paul Chaignon <paul.chaignon@gmail.com>
2023-07-21 19:08:57 +0200

    bpf/auth: Stop relying on ipcache node_id field
    [ upstream commit c1f4a63aac4d54252445e81aec11a50b7f7acf2b ]
    
    The Mutual Auth feature keeps track of authenticated remote nodes in a
    BPF map keyed by security identities and by the remote node ID. It
    currently retrieves that remote node ID from the ipcache.
    
    In a subsequent commit, however, we will remove this node ID information
    from the ipcache (to simplify node ID management in the agent). So the
    Mutual Auth datapath must retrieve the node ID from a different place.
    
    Fortunately, we already have a BPF map containing all node IP to node ID
    mappings. Thus, in this commit, we use the remote node IP obtained from
    the ipcache (ep->tunnel_endpoint) to lookup the remote node ID in this
    BPF map. That additional map lookup removes the dependency of Mutual
    Auth on the node_id field of the ipcache.
    
    An alternative approach would be to modify the auth BPF map to be keyed
    by remote node IP instead of remote node ID. That is however a much
    bigger change, both in terms of agent changes and potential impact.
    
    Note we need to handle a special case here for when the remote node ID
    (ep->tunnel_endpoint) is 0.0.0.0. That corresponds to the local node, so
    ipcache entries of local pods. To maintain the previous mutual auth
    behavior where an entry was created in the auth map for the local node,
    we set the remote_node_id to 0 in that case. In userspace, 0 is also the
    node ID used for the local node (although it's not populated in the node
    ID map).
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- bpf/lib/host_firewall.h
+++ bpf/lib/host_firewall.h
@@ -49,7 +49,7 @@ __ipv6_host_policy_egress(struct __ctx_buff *ctx, struct ipv6hdr *ip6,
 {
 	struct ct_state ct_state_new = {};
 	struct ipv6_ct_tuple *tuple = &ct_buffer->tuple;
-	__u16 node_id = 0;
+	__u32 tunnel_endpoint = 0;
 	int ret = ct_buffer->ret;
 	int verdict;
 	__u8 policy_match_type = POLICY_MATCH_NONE;
@@ -66,7 +66,7 @@ __ipv6_host_policy_egress(struct __ctx_buff *ctx, struct ipv6hdr *ip6,
 	info = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr, 0);
 	if (info && info->sec_identity) {
 		dst_sec_identity = info->sec_identity;
-		node_id = info->node_id;
+		tunnel_endpoint = info->tunnel_endpoint;
 	}
 	cilium_dbg(ctx, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
 		   ip6->daddr.s6_addr32[3], dst_sec_identity);
@@ -80,7 +80,7 @@ __ipv6_host_policy_egress(struct __ctx_buff *ctx, struct ipv6hdr *ip6,
 				     &policy_match_type, &audited, ext_err, &proxy_port);
 	if (verdict == DROP_POLICY_AUTH_REQUIRED) {
 		auth_type = (__u8)*ext_err;
-		verdict = auth_lookup(ctx, HOST_ID, dst_sec_identity, node_id, auth_type);
+		verdict = auth_lookup(ctx, HOST_ID, dst_sec_identity, tunnel_endpoint, auth_type);
 	}
 
 	/* Only create CT entry for accepted connections */
@@ -166,7 +166,7 @@ __ipv6_host_policy_ingress(struct __ctx_buff *ctx, struct ipv6hdr *ip6,
 {
 	struct ct_state ct_state_new = {};
 	struct ipv6_ct_tuple *tuple = &ct_buffer->tuple;
-	__u16 node_id = 0;
+	__u32 tunnel_endpoint = 0;
 	int ret = ct_buffer->ret;
 	int verdict = CTX_ACT_OK;
 	__u8 policy_match_type = POLICY_MATCH_NONE;
@@ -182,7 +182,7 @@ __ipv6_host_policy_ingress(struct __ctx_buff *ctx, struct ipv6hdr *ip6,
 	info = lookup_ip6_remote_endpoint((union v6addr *)&ip6->saddr, 0);
 	if (info && info->sec_identity) {
 		*src_sec_identity = info->sec_identity;
-		node_id = info->node_id;
+		tunnel_endpoint = info->tunnel_endpoint;
 	}
 	cilium_dbg(ctx, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
 		   ip6->saddr.s6_addr32[3], *src_sec_identity);
@@ -197,7 +197,7 @@ __ipv6_host_policy_ingress(struct __ctx_buff *ctx, struct ipv6hdr *ip6,
 					    &policy_match_type, &audited, ext_err, &proxy_port);
 	if (verdict == DROP_POLICY_AUTH_REQUIRED) {
 		auth_type = (__u8)*ext_err;
-		verdict = auth_lookup(ctx, HOST_ID, *src_sec_identity, node_id, auth_type);
+		verdict = auth_lookup(ctx, HOST_ID, *src_sec_identity, tunnel_endpoint, auth_type);
 	}
 
 	/* Only create CT entry for accepted connections */
@@ -315,7 +315,7 @@ __ipv4_host_policy_egress(struct __ctx_buff *ctx, bool is_host_id __maybe_unused
 {
 	struct ct_state ct_state_new = {};
 	struct ipv4_ct_tuple *tuple = &ct_buffer->tuple;
-	__u16 node_id = 0;
+	__u32 tunnel_endpoint = 0;
 	int ret = ct_buffer->ret;
 	int verdict;
 	__u8 policy_match_type = POLICY_MATCH_NONE;
@@ -339,7 +339,7 @@ __ipv4_host_policy_egress(struct __ctx_buff *ctx, bool is_host_id __maybe_unused
 	info = lookup_ip4_remote_endpoint(ip4->daddr, 0);
 	if (info && info->sec_identity) {
 		dst_sec_identity = info->sec_identity;
-		node_id = info->node_id;
+		tunnel_endpoint = info->tunnel_endpoint;
 	}
 	cilium_dbg(ctx, info ? DBG_IP_ID_MAP_SUCCEED4 : DBG_IP_ID_MAP_FAILED4,
 		   ip4->daddr, dst_sec_identity);
@@ -353,7 +353,7 @@ __ipv4_host_policy_egress(struct __ctx_buff *ctx, bool is_host_id __maybe_unused
 				     &policy_match_type, &audited, ext_err, &proxy_port);
 	if (verdict == DROP_POLICY_AUTH_REQUIRED) {
 		auth_type = (__u8)*ext_err;
-		verdict = auth_lookup(ctx, HOST_ID, dst_sec_identity, node_id, auth_type);
+		verdict = auth_lookup(ctx, HOST_ID, dst_sec_identity, tunnel_endpoint, auth_type);
 	}
 
 	/* Only create CT entry for accepted connections */
@@ -434,7 +434,7 @@ __ipv4_host_policy_ingress(struct __ctx_buff *ctx, struct iphdr *ip4,
 {
 	struct ct_state ct_state_new = {};
 	struct ipv4_ct_tuple *tuple = &ct_buffer->tuple;
-	__u16 node_id = 0;
+	__u32 tunnel_endpoint = 0;
 	int ret = ct_buffer->ret;
 	int verdict = CTX_ACT_OK;
 	__u8 policy_match_type = POLICY_MATCH_NONE;
@@ -451,7 +451,7 @@ __ipv4_host_policy_ingress(struct __ctx_buff *ctx, struct iphdr *ip4,
 	info = lookup_ip4_remote_endpoint(ip4->saddr, 0);
 	if (info && info->sec_identity) {
 		*src_sec_identity = info->sec_identity;
-		node_id = info->node_id;
+		tunnel_endpoint = info->tunnel_endpoint;
 	}
 	cilium_dbg(ctx, info ? DBG_IP_ID_MAP_SUCCEED4 : DBG_IP_ID_MAP_FAILED4,
 		   ip4->saddr, *src_sec_identity);
@@ -474,7 +474,7 @@ __ipv4_host_policy_ingress(struct __ctx_buff *ctx, struct iphdr *ip4,
 					    &policy_match_type, &audited, ext_err, &proxy_port);
 	if (verdict == DROP_POLICY_AUTH_REQUIRED) {
 		auth_type = (__u8)*ext_err;
-		verdict = auth_lookup(ctx, HOST_ID, *src_sec_identity, node_id, auth_type);
+		verdict = auth_lookup(ctx, HOST_ID, *src_sec_identity, tunnel_endpoint, auth_type);
 	}
 
 	/* Only create CT entry for accepted connections */
