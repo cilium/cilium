efbba5916347640007bce74fe20afebedbbbe561
Paul Chaignon <paul.chaignon@gmail.com>
2023-07-24 23:48:38 +0200

    datapath, ipcache: Remove unused node ID functions
    The AllocateNodeID function was used to allocate node IDs from the
    ipcache logic. Since we don't populate the ipcache with node IDs
    anymore, it's unused and can be removed. The DeallocateNodeID function
    was never used (a sibling function was used instead).
    
    This commit has no functional changes.
    
    Reported-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- pkg/ipcache/types/fake/nodeidhandler.go
+++ pkg/ipcache/types/fake/nodeidhandler.go
@@ -11,10 +11,6 @@ func (m *FakeNodeIDHandler) GetNodeID(_ net.IP) (nodeID uint16, exists bool) {
 	return 0, true
 }
 
-func (m *FakeNodeIDHandler) AllocateNodeID(_ net.IP) uint16 {
-	return 0
-}
-
 func (m *FakeNodeIDHandler) GetNodeIP(_ uint16) string {
 	return ""
 }
