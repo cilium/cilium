803a4b63ced159e395ef8bc30af854e7bd059689
Paul Chaignon <paul.chaignon@gmail.com>
2023-07-24 13:07:51 +0200

    bpf: Move IPsec prep logic to encrypt.h
    [ upstream commit 6d848825b63d3fa2bb50f1a5baf1a444ae579c11 ]
    
    [ backporter's notes: Trivial conflict on the unusual include of
      encrypt.h and due to macro guards at the top. ]
    
    This commit has no functional changes. It simply moves the logic to
    prepare packets for IPsec encryption to the dedicated encrypt.h file.
    Function set_ipsec_encrypt_mark is also created to simplify a subsequent
    commit (no, it won't remain a function that simply calls another
    function :)).
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- bpf/lib/encap.h
+++ bpf/lib/encap.h
@@ -9,28 +9,15 @@
 #include "trace.h"
 #include "l3.h"
 
+#if __ctx_is == __ctx_skb
+#include "encrypt.h"
+#endif /* __ctx_is == __ctx_skb */
+
 #ifdef ENCAP_IFINDEX
 /* NOT_VTEP_DST is passed to an encapsulation function when the
  * destination of the tunnel is not a VTEP.
  */
 #define NOT_VTEP_DST 0
-#ifdef ENABLE_IPSEC
-static __always_inline int
-encap_and_redirect_ipsec(struct __ctx_buff *ctx, __u8 key, __u16 node_id,
-			 __u32 seclabel)
-{
-	/* IPSec is performed by the stack on any packets with the
-	 * MARK_MAGIC_ENCRYPT bit set. During the process though we
-	 * lose the lxc context (seclabel and tunnel endpoint). The
-	 * tunnel endpoint can be looked up from daddr but the sec
-	 * label is stashed in the mark and extracted in bpf_host
-	 * to send ctx onto tunnel for encap.
-	 */
-	set_encrypt_key_mark(ctx, key, node_id);
-	set_identity_meta(ctx, seclabel);
-	return IPSEC_ENDPOINT;
-}
-#endif /* ENABLE_IPSEC */
 
 static __always_inline int
 encap_remap_v6_host_address(struct __ctx_buff *ctx __maybe_unused,
@@ -176,8 +163,8 @@ encap_and_redirect_lxc(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 	if (tunnel_endpoint) {
 #ifdef ENABLE_IPSEC
 		if (encrypt_key)
-			return encap_and_redirect_ipsec(ctx, encrypt_key,
-						        node_id, seclabel);
+			return set_ipsec_encrypt(ctx, encrypt_key,
+						 node_id, seclabel);
 #endif
 #if !defined(ENABLE_NODEPORT) && defined(ENABLE_HOST_FIREWALL)
 		/* For the host firewall, traffic from a pod to a remote node
@@ -203,8 +190,8 @@ encap_and_redirect_lxc(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 	if (tunnel->key) {
 		__u8 min_encrypt_key = get_min_encrypt_key(tunnel->key);
 
-		return encap_and_redirect_ipsec(ctx, min_encrypt_key, node_id,
-						seclabel);
+		return set_ipsec_encrypt(ctx, min_encrypt_key, node_id,
+					 seclabel);
 	}
 #endif
 	return __encap_and_redirect_with_nodeid(ctx, tunnel->ip4, seclabel,
