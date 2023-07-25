bdb6a3b4c8be9e4f1214264a1cc4072fdf2d6f06
Paul Chaignon <paul.chaignon@gmail.com>
2023-07-25 18:49:59 +0200

    node: Allocate node ID even if IPsec is disabled
    [ upstream commit d2523ff4cea6facb8374a0720fd34b398e2323c4 ]
    
    [ backporter's notes: The mocked node ID BPF map doesn't exist in v1.13
      and changes to the integration tests (see last paragraph below) were
      not necessary, so I skipped them. ]
    
    Before this commit, we would allocate a node ID for new nodes if IPsec
    was enabled. That wasn't consistent with (1) allocations triggered by
    pod creations, where we allocate even if IPsec is disabled, and (2)
    deallocations triggered by node deletions which also happen regardless
    of IPsec being enabled.
    
    This commit changes the nodeUpdate logic slightly such that the node ID
    is allocated regardless of IPsec being enabled. As a consequence, we
    need to update several integration tests to mock the node ID BPF map.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- pkg/datapath/linux/node.go
+++ pkg/datapath/linux/node.go
@@ -1148,6 +1148,7 @@ func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *nodeTypes.Node, firstAdd
 		newIP6                                   = newNode.GetNodeIP(true)
 		oldKey, newKey                           uint8
 		isLocalNode                              = false
+		remoteNodeID                             = n.allocateIDForNode(newNode)
 	)
 
 	if oldNode != nil {
@@ -1161,7 +1162,6 @@ func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *nodeTypes.Node, firstAdd
 	}
 
 	if n.nodeConfig.EnableIPSec && !n.nodeConfig.EncryptNode {
-		remoteNodeID := n.allocateIDForNode(newNode)
 		n.enableIPsec(newNode, remoteNodeID)
 		newKey = newNode.EncryptionKey
 	}
