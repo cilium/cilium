1e911b641a31f43c13d80285ad0223afda838039
Marco Iorio <marco.iorio@isovalent.com>
2023-07-28 16:29:13 +0200

    maps: rework CT/NAT maps GC signal handler to remove global variables
    Lets convert the signal handler into a proper structure provided through
    Hive, so that we can get rid of the associated global variables.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/maps/ctmap/gc/cell.go
+++ pkg/maps/ctmap/gc/cell.go
@@ -4,11 +4,8 @@
 package gc
 
 import (
-	"errors"
-
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/hive/cell"
-	"github.com/cilium/cilium/pkg/signal"
 )
 
 var Cell = cell.Module(
@@ -25,55 +22,9 @@ var Cell = cell.Module(
 	cell.ProvidePrivate(
 		New,
 
+		// Register the signal handler for CT and NAT fill-up signals.
+		newSignalHandler,
 		// Provide the reduced interface used by the GC logic.
 		func(mgr endpointmanager.EndpointManager) EndpointManager { return mgr },
 	),
-
-	// Register a signal handler for CT and NAT fill-up signals.
-	cell.Invoke(registerSignalHandler),
 )
-
-// SignalData holds the IP address family type BPF program sent along with
-// the SignalNatFillUp and SignalCTFillUp signals.
-type SignalData uint32
-
-const (
-	// SignalProtoV4 denotes IPv4 protocol
-	SignalProtoV4 SignalData = iota
-	// SignalProtoV6 denotes IPv6 protocol
-	SignalProtoV6
-	SignalProtoMax
-)
-
-var signalProto = [SignalProtoMax]string{
-	SignalProtoV4: "ipv4",
-	SignalProtoV6: "ipv6",
-}
-
-// String implements fmt.Stringer for SignalData
-func (d SignalData) String() string {
-	return signalProto[d]
-}
-
-// Ugly global variables to bridge the old with the new
-
-// wakeup is buffered to not block the sender while GC is running. If this overflows it is possible
-// to lose a wakeup signal for IPv4 while IPv6 GC is running, and all the buffered wakeups are for
-// IPv6 and the ones for IPv4 overflowed. Keep the buffer big enough for that to be unlikely.
-var wakeup = make(chan SignalData, 1024)
-var muteSignals = func() error { return errors.New("muteSignals not implemented") }
-var unmuteSignals = func() error { return errors.New("unmuteSignals not implemented") }
-
-func registerSignalHandler(sm signal.SignalManager) {
-	err := sm.RegisterHandler(signal.ChannelHandler(wakeup), signal.SignalCTFillUp, signal.SignalNatFillUp)
-	if err != nil {
-		log.WithError(err).Warningf("Failed to set up signal channel for CT & NAT fill-up events!")
-		return
-	}
-	muteSignals = func() error {
-		return sm.MuteSignals(signal.SignalCTFillUp, signal.SignalNatFillUp)
-	}
-	unmuteSignals = func() error {
-		return sm.UnmuteSignals(signal.SignalCTFillUp, signal.SignalNatFillUp)
-	}
-}
