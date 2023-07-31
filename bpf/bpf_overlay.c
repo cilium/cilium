9ecfb0fca8df19437bff693da8b7654ebb4a644e
Ryan Drew <ryan.drew@isovalent.com>
2023-07-31 12:44:35 -0600

    ipsec: discard trace events when monitor aggregation is enabled
    [ upstream commit 3e5282274d1a415e4496e5ce51956d59b277f7be ]
    
    This commit modifies calls to `send_trace_notify` in IPSec contexts
    where connection tracking information is not available to drop events
    when monitor aggregation is enabled.
    
    Benchmarks were performed on a two node, e2-custom-4-8192,
    cluster, using pod to pod netperf stream, RR and CRR tests. Cilium
    v1.14.0 was installed using unstripped images with IPSec, VXLAN
    tunnel mode, and Hubble enabled. Tests were performed in the order
    of stream, RR and CRR for each configuration. Between each test,
    the netserver was reset and each node's conntrack tables were
    cleared.
    
    Pod to pod netperf stream test on v1.14.0, one client per core:
    
    ```csv
    Throughput,Throughput Units,Elapsed Time (sec)
    464.06,10^6bits/s,180.04
    461.25,10^6bits/s,180.02
    441.73,10^6bits/s,180.00
    482.26,10^6bits/s,180.04
    ```
    
    Pod to pod netperf stream test on this commit, one client per core:
    
    ```csv
    Throughput,Throughput Units,Elapsed Time (sec)
    698.48,10^6bits/s,180.03
    643.81,10^6bits/s,180.05
    780.22,10^6bits/s,180.05
    552.11,10^6bits/s,180.03
    ```
    
    Taking the average of each client's throughput, this change leads to
    an increase in throughput by +45%.
    
    Pod to pod netperf RR test on v1.14.0, one client per core:
    
    ```csv
    50th Percentile Latency Microseconds,90th Percentile Latency Microseconds,99th Percentile Latency Microseconds,Round Trip Latency usec/tran,Request Size Bytes,Response Size Bytes,Elapsed Time (sec)
    302,486,1350,361.258,1,1,180.00
    312,501,1304,372.272,1,1,180.00
    320,530,1338,383.703,1,1,180.00
    302,459,1286,355.595,1,1,180.00
    ```
    
    Pod to pod netperf RR test on this commit, one client per core:
    
    ```csv
    50th Percentile Latency Microseconds,90th Percentile Latency Microseconds,99th Percentile Latency Microseconds,Round Trip Latency usec/tran,Request Size Bytes,Response Size Bytes,Elapsed Time (sec)
    199,256,370,213.484,1,1,180.00
    198,254,366,212.594,1,1,180.00
    202,261,371,216.930,1,1,180.00
    199,257,366,213.430,1,1,180.00
    ```
    
    Taking the worst 99th percentile latency from each test, this change
    leads to a reduction in p99 latency by 76.5%.
    
    Pod to pod netperf CRR test on v1.14.0, one client per core:
    
    ```csv
    50th Percentile Latency Microseconds,90th Percentile Latency Microseconds,99th Percentile Latency Microseconds,Round Trip Latency usec/tran,Request Size Bytes,Response Size Bytes,Elapsed Time (sec)
    935,3910,6920,3337.723,1,1,180.00
    914,3308,6432,2502.398,1,1,180.00
    933,3797,6843,3199.433,1,1,180.00
    927,3761,6812,2855.196,1,1,180.00
    ```
    
    Pod to pod netperf CRR test on this commit, one client per core:
    
    ```csv
    50th Percentile Latency Microseconds,90th Percentile Latency Microseconds,99th Percentile Latency Microseconds,Round Trip Latency usec/tran,Request Size Bytes,Response Size Bytes,Elapsed Time (sec)
    683,4680,5803,3155.128,1,1,180.00
    678,4627,5782,2958.921,1,1,180.00
    683,4658,5830,3095.390,1,1,180.00
    680,4646,5787,3092.147,1,1,180.00
    ```
    
    Taking the worst 99th percentile latency from each test, this change
    leads to a reduction in p99 latency by 15.8%.
    
    Fixes: #26648
    
    Signed-off-by: Ryan Drew <ryan.drew@isovalent.com>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- bpf/bpf_overlay.c
+++ bpf/bpf_overlay.c
@@ -122,8 +122,7 @@ static __always_inline int handle_ipv6(struct __ctx_buff *ctx,
 		ctx_change_type(ctx, PACKET_HOST);
 
 		send_trace_notify(ctx, TRACE_TO_STACK, *identity, 0, 0,
-				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED,
-				  TRACE_PAYLOAD_LEN);
+				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED, 0);
 
 		return CTX_ACT_OK;
 	}
@@ -283,8 +282,7 @@ skip_vtep:
 		ctx_change_type(ctx, PACKET_HOST);
 
 		send_trace_notify(ctx, TRACE_TO_STACK, *identity, 0, 0,
-				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED,
-				  TRACE_PAYLOAD_LEN);
+				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED, 0);
 
 		return CTX_ACT_OK;
 	}
@@ -465,8 +463,7 @@ int from_overlay(struct __ctx_buff *ctx)
 #ifdef ENABLE_IPSEC
 	if (is_esp(ctx, proto))
 		send_trace_notify(ctx, TRACE_FROM_OVERLAY, 0, 0, 0,
-				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED,
-				  TRACE_PAYLOAD_LEN);
+				  ctx->ingress_ifindex, TRACE_REASON_ENCRYPTED, 0);
 	else
 #endif
 	{
