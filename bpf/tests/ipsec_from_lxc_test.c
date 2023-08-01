5850b111d68a24029c06c21f3ccabff2ab247b61
Paul Chaignon <paul.chaignon@gmail.com>
2023-08-01 13:36:19 +0200

    bpf/tests: Cover new local IPsec key
    This commit adds a BPF unit test to cover the case 1 of key rotations:
    the new key is configured on the source node but not yet on the
    destination node. We must therefore keep using the old key.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- bpf/tests/ipsec_from_lxc_test.c
+++ bpf/tests/ipsec_from_lxc_test.c
@@ -216,7 +216,50 @@ int ipv4_from_lxc_encrypt_check(__maybe_unused const struct __ctx_buff *ctx)
 	test_finish();
 }
 
-PKTGEN("tc", "03_ipv6_from_lxc_encrypt")
+PKTGEN("tc", "03_ipv4_from_lxc_new_local_key")
+int ipv4_from_lxc_new_local_key_pktgen(struct __ctx_buff *ctx)
+{
+	return pktgen_from_lxc(ctx);
+}
+
+SETUP("tc", "03_ipv4_from_lxc_new_local_key")
+int ipv4_from_lxc_new_local_key_setup(struct __ctx_buff *ctx)
+{
+	/* The new key is configured locally but not yet on the destination node.
+	 */
+	__u32 encrypt_key = 0;
+	struct encrypt_config encrypt_value = { .encrypt_key = ENCRYPT_KEY + 1 };
+
+	map_update_elem(&ENCRYPT_MAP, &encrypt_key, &encrypt_value, BPF_ANY);
+
+	tail_call_static(ctx, &entry_call_map, FROM_CONTAINER);
+	return TEST_ERROR;
+}
+
+CHECK("tc", "03_ipv4_from_lxc_new_local_key")
+int ipv4_from_lxc_new_local_key_check(__maybe_unused const struct __ctx_buff *ctx)
+{
+	void *data;
+	void *data_end;
+	__u32 *status_code;
+
+	test_init();
+
+	data = (void *)(long)ctx->data;
+	data_end = (void *)(long)ctx->data_end;
+
+	if (data + sizeof(*status_code) > data_end)
+		test_fatal("status code out of bounds");
+
+	status_code = data;
+	assert(*status_code == CTX_ACT_OK);
+	assert(ctx->mark == (NODE_ID << 16 | ENCRYPT_KEY << 12 | MARK_MAGIC_ENCRYPT));
+	assert(ctx_load_meta(ctx, CB_ENCRYPT_IDENTITY) == SECLABEL);
+
+	test_finish();
+}
+
+PKTGEN("tc", "04_ipv6_from_lxc_encrypt")
 int ipv6_from_lxc_encrypt_pktgen(struct __ctx_buff *ctx)
 {
 	struct pktgen builder;
@@ -251,7 +294,7 @@ int ipv6_from_lxc_encrypt_pktgen(struct __ctx_buff *ctx)
 	return 0;
 }
 
-SETUP("tc", "03_ipv6_from_lxc_encrypt")
+SETUP("tc", "04_ipv6_from_lxc_encrypt")
 int ipv6_from_lxc_encrypt_setup(struct __ctx_buff *ctx)
 {
 	struct policy_key policy_key = { .egress = 1 };
@@ -277,7 +320,7 @@ int ipv6_from_lxc_encrypt_setup(struct __ctx_buff *ctx)
 	return TEST_ERROR;
 }
 
-CHECK("tc", "03_ipv6_from_lxc_encrypt")
+CHECK("tc", "04_ipv6_from_lxc_encrypt")
 int ipv6_from_lxc_encrypt_check(__maybe_unused const struct __ctx_buff *ctx)
 {
 	void *data;
