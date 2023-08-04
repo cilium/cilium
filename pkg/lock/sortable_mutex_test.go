8c9f8bd753c5ead8703db93f3e691d020f97ccc7
Jussi Maki <jussi@isovalent.com>
2023-08-04 15:06:05 +0300

    lock: SortableMutex for ordered locking of a mutex set
    SortableMutex is a mutex paired with a (globally unique) sequence number
    which allows sorting a set of them into a predicable order for ordered
    locking.
    
    This will be used in a follow-up commit to implement per-table locks for
    StateDB in a way that avoids deadlocks due to misordered locking sequence
    (A->B and B->A).
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

---
+++ pkg/lock/sortable_mutex_test.go
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package lock
+
+import (
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/slices"
+)
+
+func TestSortableMutex(t *testing.T) {
+	smu1 := NewSortableMutex()
+	smu2 := NewSortableMutex()
+	require.Greater(t, smu2.Seq(), smu1.Seq())
+	smu1.Lock()
+	smu2.Lock()
+	smu1.Unlock()
+	smu2.Unlock()
+	smus := SortableMutexes{smu1, smu2}
+	smus.Lock()
+	smus.Unlock()
+	smus.Lock()
+	smus.Unlock()
+}
+
+func TestSortableMutex_Chaos(t *testing.T) {
+	smus := SortableMutexes{
+		NewSortableMutex(),
+		NewSortableMutex(),
+		NewSortableMutex(),
+		NewSortableMutex(),
+		NewSortableMutex(),
+	}
+
+	nMonkeys := 10
+	iterations := 100
+	var wg sync.WaitGroup
+	wg.Add(nMonkeys)
+
+	monkey := func() {
+		defer wg.Done()
+		for i := 0; i < iterations; i++ {
+			// Take a random subset of the sortable mutexes.
+			subSmus := slices.Clone(smus)
+			rand.Shuffle(len(subSmus), func(i, j int) {
+				subSmus[i], subSmus[j] = subSmus[j], subSmus[i]
+			})
+			n := rand.Intn(len(subSmus))
+			subSmus = subSmus[:n]
+
+			time.Sleep(time.Microsecond)
+			subSmus.Lock()
+			time.Sleep(time.Microsecond)
+			subSmus.Unlock()
+			time.Sleep(time.Microsecond)
+		}
+	}
+
+	for i := 0; i < nMonkeys; i++ {
+		go monkey()
+	}
+
+	wg.Wait()
+}
