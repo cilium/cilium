a33784b6458034f9c28434f61f782375e34762cf
Alan Kutniewski <kutniewski@google.com>
2023-08-07 16:38:29 +0200

    operator/k8s: Add CiliumEndpoint resource
    Signed-off-by: Alan Kutniewski <kutniewski@google.com>

---
+++ operator/k8s/resource_ctors.go
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package k8s
+
+import (
+	"github.com/cilium/cilium/pkg/hive"
+	cilium_api_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/k8s/utils"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func CiliumEndpointResource(lc hive.Lifecycle, cs client.Clientset, opts ...func(*metav1.ListOptions)) (resource.Resource[*cilium_api_v2.CiliumEndpoint], error) {
+	if !cs.IsEnabled() {
+		return nil, nil
+	}
+	lw := utils.ListerWatcherWithModifiers(
+		utils.ListerWatcherFromTyped[*cilium_api_v2.CiliumEndpointList](cs.CiliumV2().CiliumEndpoints("")),
+		opts...,
+	)
+	return resource.New[*cilium_api_v2.CiliumEndpoint](lc, lw, resource.WithMetric("CiliumEndpoint")), nil
+}
