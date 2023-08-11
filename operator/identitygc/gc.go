e7f71924d5df09b18b5da7fd96ae06f61919325e
Alan Kutniewski <kutniewski@google.com>
2023-08-11 11:09:47 +0200

    operator: Migrate Identity GC to use CEP and CES from hive.
    Signed-off-by: Alan Kutniewski <kutniewski@google.com>

--- operator/identitygc/gc.go
+++ operator/identitygc/gc.go
@@ -17,6 +17,7 @@ import (
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/hive/cell"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	ciliumV2 "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/typed/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/resource"
@@ -32,9 +33,11 @@ type params struct {
 	Logger    logrus.FieldLogger
 	Lifecycle hive.Lifecycle
 
-	Clientset          k8sClient.Clientset
-	Identity           resource.Resource[*v2.CiliumIdentity]
-	AuthIdentityClient authIdentity.Provider
+	Clientset           k8sClient.Clientset
+	Identity            resource.Resource[*v2.CiliumIdentity]
+	CiliumEndpoint      resource.Resource[*v2.CiliumEndpoint]
+	CiliumEndpointSlice resource.Resource[*v2alpha1.CiliumEndpointSlice]
+	AuthIdentityClient  authIdentity.Provider
 
 	Cfg         Config
 	SharedCfg   SharedConfig
@@ -45,9 +48,11 @@ type params struct {
 type GC struct {
 	logger logrus.FieldLogger
 
-	clientset          ciliumV2.CiliumIdentityInterface
-	identity           resource.Resource[*v2.CiliumIdentity]
-	authIdentityClient authIdentity.Provider
+	clientset           ciliumV2.CiliumIdentityInterface
+	identity            resource.Resource[*v2.CiliumIdentity]
+	ciliumEndpoint      resource.Resource[*v2.CiliumEndpoint]
+	ciliumEndpointSlice resource.Resource[*v2alpha1.CiliumEndpointSlice]
+	authIdentityClient  authIdentity.Provider
 
 	clusterInfo    cmtypes.ClusterInfo
 	allocationMode string
@@ -88,16 +93,18 @@ func registerGC(p params) {
 	}
 
 	gc := &GC{
-		logger:             p.Logger,
-		clientset:          p.Clientset.CiliumV2().CiliumIdentities(),
-		identity:           p.Identity,
-		authIdentityClient: p.AuthIdentityClient,
-		clusterInfo:        p.ClusterInfo,
-		allocationMode:     p.SharedCfg.IdentityAllocationMode,
-		gcInterval:         p.Cfg.Interval,
-		heartbeatTimeout:   p.Cfg.HeartbeatTimeout,
-		gcRateInterval:     p.Cfg.RateInterval,
-		gcRateLimit:        p.Cfg.RateLimit,
+		logger:              p.Logger,
+		clientset:           p.Clientset.CiliumV2().CiliumIdentities(),
+		identity:            p.Identity,
+		ciliumEndpoint:      p.CiliumEndpoint,
+		ciliumEndpointSlice: p.CiliumEndpointSlice,
+		authIdentityClient:  p.AuthIdentityClient,
+		clusterInfo:         p.ClusterInfo,
+		allocationMode:      p.SharedCfg.IdentityAllocationMode,
+		gcInterval:          p.Cfg.Interval,
+		heartbeatTimeout:    p.Cfg.HeartbeatTimeout,
+		gcRateInterval:      p.Cfg.RateInterval,
+		gcRateLimit:         p.Cfg.RateLimit,
 		heartbeatStore: newHeartbeatStore(
 			p.Cfg.HeartbeatTimeout,
 		),
--- operator/k8s/resource_ctors.go
+++ operator/k8s/resource_ctors.go
@@ -4,13 +4,19 @@
 package k8s
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/cache"
+
 	"github.com/cilium/cilium/pkg/hive"
 	cilium_api_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	cilium_api_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/resource"
 	"github.com/cilium/cilium/pkg/k8s/utils"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func CiliumEndpointResource(lc hive.Lifecycle, cs client.Clientset, opts ...func(*metav1.ListOptions)) (resource.Resource[*cilium_api_v2.CiliumEndpoint], error) {
@@ -21,7 +27,24 @@ func CiliumEndpointResource(lc hive.Lifecycle, cs client.Clientset, opts ...func
 		utils.ListerWatcherFromTyped[*cilium_api_v2.CiliumEndpointList](cs.CiliumV2().CiliumEndpoints("")),
 		opts...,
 	)
-	return resource.New[*cilium_api_v2.CiliumEndpoint](lc, lw, resource.WithMetric("CiliumEndpoint")), nil
+	indexers := cache.Indexers{
+		cache.NamespaceIndex:        cache.MetaNamespaceIndexFunc,
+		CiliumEndpointIndexIdentity: identityIndexFunc,
+	}
+	return resource.New[*cilium_api_v2.CiliumEndpoint](
+		lc, lw, resource.WithMetric("CiliumEndpoint"), resource.WithIndexers(indexers)), nil
+}
+
+func identityIndexFunc(obj interface{}) ([]string, error) {
+	switch t := obj.(type) {
+	case *cilium_api_v2.CiliumEndpoint:
+		if t.Status.Identity != nil {
+			id := strconv.FormatInt(t.Status.Identity.ID, 10)
+			return []string{id}, nil
+		}
+		return []string{"0"}, nil
+	}
+	return nil, fmt.Errorf("%w - found %T", errors.New("object is not a *cilium_api_v2.CiliumEndpoint"), obj)
 }
 
 func CiliumEndpointSliceResource(lc hive.Lifecycle, cs client.Clientset, opts ...func(*metav1.ListOptions)) (resource.Resource[*cilium_api_v2alpha1.CiliumEndpointSlice], error) {
