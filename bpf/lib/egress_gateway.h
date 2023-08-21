369a6fdf929d178ab438a9171db23a6c3388e469
Gilberto Bertin <jibi@cilium.io>
2023-08-21 14:56:02 +0200

    bpf: egressgw: add support for fib_lookup
    when a packet matching an egress gateway policy needs to leave the node,
    it can do so through the wrong interface. This can happen when the
    interface with the route for that packet is different from the one with
    the egress IP assigned to.
    
    The current solution to this is to install additional IP rules and
    routes that match egress gateway traffic and steer it to the correct
    interface with the egress IP. The drawback of this approach is that
    installing and maintaining these rules is getting more and more complex
    and consumes resources on the host.
    
    This commit introduces a new approach to solve this, based on
    fib lookups and redirects in the datapath. In practice, after a
    packet matching an egress gateway policy has been masqueraded, we run a
    fib lookup on the packet with the new source (egress) IP, and in case
    the packet is redirected to the correct interface with the egress IP
    assigned to.
    
    Fixes: #23504
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- bpf/lib/egress_gateway.h
+++ bpf/lib/egress_gateway.h
@@ -4,7 +4,9 @@
 #ifndef __LIB_EGRESS_GATEWAY_H_
 #define __LIB_EGRESS_GATEWAY_H_
 
+#include "lib/fib.h"
 #include "lib/identity.h"
+#include "lib/overloadable.h"
 
 #include "maps.h"
 
@@ -21,6 +23,24 @@
  */
 #define EGRESS_GATEWAY_NO_GATEWAY (0)
 #define EGRESS_GATEWAY_EXCLUDED_CIDR bpf_htonl(1)
+
+static __always_inline
+int egress_gw_fib_lookup_and_redirect(struct __ctx_buff *ctx, __be32 egress_ip, __be32 daddr,
+				      __s8 *ext_err)
+{
+	struct bpf_fib_lookup_padded fib_params = {};
+	__u32 old_oif = ctx_get_ifindex(ctx);
+
+	*ext_err = (__s8)fib_lookup_v4(ctx, &fib_params, egress_ip, daddr, 0);
+
+	if (*ext_err != BPF_FIB_LKUP_RET_SUCCESS && *ext_err != BPF_FIB_LKUP_RET_NO_NEIGH)
+		return DROP_NO_FIB;
+
+	if (old_oif == fib_params.l.ifindex)
+		return CTX_ACT_OK;
+
+	return fib_do_redirect(ctx, true, &fib_params, ext_err, (int *)&old_oif);
+}
 
 #ifdef ENABLE_EGRESS_GATEWAY
 static __always_inline
