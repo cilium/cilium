75460648140868daa6dbcbd955c0c8d58ed38338
Rastislav Szabo <rastislav.szabo@isovalent.com>
2023-08-22 13:37:56 +0200

    bgpv1: Introduce BGPCPResourceStore for BGP CP reconcilers usage
    Introduces a new BGPCPResourceStore that provides and easy to use interface
    to a resource.Store for k8s objects that needs to be processed by the
    BGP Control Plane reconcilers. It automatically signals the BGP Control Plane
    whenever an event happens on the resource, so that individual reconcilers do
    not need to do that themselves. Also provides a respective mock type that can
    be used in unit tests.
    
    Signed-off-by: Rastislav Szabo <rastislav.szabo@isovalent.com>

---
+++ pkg/bgpv1/manager/resource_store.go
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package manager
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cilium/cilium/pkg/bgpv1/agent/signaler"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+
+	k8sRuntime "k8s.io/apimachinery/pkg/runtime"
+)
+
+// BGPCPResourceStore is a super set of the resource.Store for the BGP Control Plane reconcilers usage.
+// It automatically signals the BGP Control Plane whenever an event happens on the resource.
+type BGPCPResourceStore[T k8sRuntime.Object] interface {
+	resource.Store[T]
+}
+
+var _ BGPCPResourceStore[*k8sRuntime.Unknown] = (*bgpCPResourceStore[*k8sRuntime.Unknown])(nil)
+
+type bgpCPResourceStoreParams[T k8sRuntime.Object] struct {
+	cell.In
+
+	Lifecycle hive.Lifecycle
+	Resource  resource.Resource[T]
+	Signaler  *signaler.BGPCPSignaler
+}
+
+// bgpCPResourceStore takes a resource.Resource[T] and watches for events. It can still be used as a normal Store,
+// but in addition to that it will signal the BGP Control plane upon each event via the passed BGPCPSignaler.
+type bgpCPResourceStore[T k8sRuntime.Object] struct {
+	resource.Store[T]
+
+	resource resource.Resource[T]
+	signaler *signaler.BGPCPSignaler
+
+	ctx      context.Context
+	cancel   context.CancelFunc
+	doneChan chan struct{}
+}
+
+func NewBGPCPResourceStore[T k8sRuntime.Object](params bgpCPResourceStoreParams[T]) BGPCPResourceStore[T] {
+	if params.Resource == nil {
+		return nil
+	}
+
+	s := &bgpCPResourceStore[T]{
+		resource: params.Resource,
+		signaler: params.Signaler,
+		doneChan: make(chan struct{}),
+	}
+	s.ctx, s.cancel = context.WithCancel(context.Background())
+
+	params.Lifecycle.Append(s)
+
+	return s
+}
+
+// Start implements hive.HookInterface
+func (s *bgpCPResourceStore[T]) Start(ctx hive.HookContext) error {
+	var err error
+	s.Store, err = s.resource.Store(ctx)
+	if err != nil {
+		return fmt.Errorf("resource.Store(): %w", err)
+	}
+
+	go s.run()
+	return nil
+}
+
+// Stop implements hive.HookInterface
+func (s *bgpCPResourceStore[T]) Stop(stopCtx hive.HookContext) error {
+	s.cancel()
+
+	select {
+	case <-s.doneChan:
+	case <-stopCtx.Done():
+		return stopCtx.Err()
+	}
+
+	return nil
+}
+
+func (s *bgpCPResourceStore[T]) run() {
+	defer close(s.doneChan)
+
+	for event := range s.resource.Events(s.ctx) {
+		s.signaler.Event(struct{}{})
+		event.Done(nil)
+	}
+}
---
+++ pkg/bgpv1/manager/resource_store_mock.go
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package manager
+
+import (
+	"golang.org/x/exp/maps"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/cache"
+
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+var _ BGPCPResourceStore[*runtime.Unknown] = (*mockBGPCPResourceStore[*runtime.Unknown])(nil)
+
+type mockBGPCPResourceStore[T runtime.Object] struct {
+	objMu   lock.Mutex
+	objects map[resource.Key]T
+}
+
+func newMockBGPCPResourceStore[T runtime.Object]() *mockBGPCPResourceStore[T] {
+	return &mockBGPCPResourceStore[T]{
+		objects: make(map[resource.Key]T),
+	}
+}
+
+func (mds *mockBGPCPResourceStore[T]) List() []T {
+	mds.objMu.Lock()
+	defer mds.objMu.Unlock()
+	return maps.Values(mds.objects)
+}
+
+func (mds *mockBGPCPResourceStore[T]) IterKeys() resource.KeyIter {
+	mds.objMu.Lock()
+	defer mds.objMu.Unlock()
+	return newMockKeyIter(maps.Keys(mds.objects))
+}
+
+func (mds *mockBGPCPResourceStore[T]) Upsert(obj T) {
+	mds.objMu.Lock()
+	defer mds.objMu.Unlock()
+
+	key := resource.NewKey(obj)
+	mds.objects[key] = obj
+}
+
+func (mds *mockBGPCPResourceStore[T]) Delete(key resource.Key) {
+	mds.objMu.Lock()
+	defer mds.objMu.Unlock()
+
+	delete(mds.objects, key)
+}
+
+func (mds *mockBGPCPResourceStore[T]) IndexKeys(indexName, indexedValue string) ([]string, error) {
+	return nil, nil
+}
+
+func (mds *mockBGPCPResourceStore[T]) ByIndex(indexName, indexedValue string) ([]T, error) {
+	return nil, nil
+}
+
+func (mds *mockBGPCPResourceStore[T]) CacheStore() cache.Store {
+	return nil
+}
+
+func (mds *mockBGPCPResourceStore[T]) Get(obj T) (item T, exists bool, err error) {
+	return mds.GetByKey(resource.NewKey(obj))
+}
+
+func (mds *mockBGPCPResourceStore[T]) GetByKey(key resource.Key) (item T, exists bool, err error) {
+	mds.objMu.Lock()
+	defer mds.objMu.Unlock()
+
+	item, exists = mds.objects[key]
+
+	return item, exists, nil
+}
