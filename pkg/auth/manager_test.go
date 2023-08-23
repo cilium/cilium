f6e2eb29b69635d6ad914bc637248dd0ee622c58
Maartje Eyskens <maartje.eyskens@isovalent.com>
2023-08-23 16:49:47 +0200

    Delete auth map entries of deleted SVID
    This change adds a watcher on the SPIRE updates for deleted SVIDs.
    If one is deleted it will be signaled over the rotation channel.
    Should the ID be present in the auth map it will be immediately removed.
    
    Signed-off-by: Maartje Eyskens <maartje.eyskens@isovalent.com>

--- pkg/auth/manager_test.go
+++ pkg/auth/manager_test.go
@@ -172,6 +172,28 @@ func Test_authManager_handleCertificateRotationEvent(t *testing.T) {
 	assert.True(t, handleAuthCalled)
 }
 
+func Test_authManager_handleCertificateDeletionEvent(t *testing.T) {
+	authHandlers := []authHandler{newAlwaysPassAuthHandler(logrus.New())}
+	aMap := &fakeAuthMap{
+		entries: map[authKey]authInfo{
+			{localIdentity: 1, remoteIdentity: 2, remoteNodeID: 1, authType: 100}: {expiration: time.Now()},
+			{localIdentity: 2, remoteIdentity: 3, remoteNodeID: 1, authType: 100}: {expiration: time.Now()},
+			{localIdentity: 3, remoteIdentity: 4, remoteNodeID: 1, authType: 100}: {expiration: time.Now()},
+		},
+	}
+
+	am, err := newAuthManager(logrus.New(), authHandlers, aMap, nil, time.Second)
+	assert.NoError(t, err)
+	assert.NotNil(t, am)
+
+	err = am.handleCertificateRotationEvent(context.Background(), certs.CertificateRotationEvent{
+		Identity: identity.NumericIdentity(2),
+		Deleted:  true,
+	})
+	assert.NoError(t, err)
+	assert.Len(t, aMap.entries, 1)
+}
+
 // Fake NodeIDHandler
 type fakeNodeIDHandler struct {
 	nodeIdMappings map[uint16]string
