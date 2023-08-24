7e799437184e20bbe9b90177468e0a1683ad9fa8
Timo Beckers <timo@isovalent.com>
2023-08-24 12:40:53 +0200

    connectivity: display reason for skipping Tests and Scenarios
    I wanted to re-run a specific Test for reproducing a CI failure, but was
    surprised to see the test wouldn't run despite targeting it explicitly
    with --test. The test in question being north-south-loadbalancing, has a
    feature constraint that was not met in my environment. Thus began the
    printf rabbit hole.
    
    This patch introduces skip reasons included in brackets after each skipped
    Test or Scenario. Currently, only one reason is shown, but care was taken
    to pick the most surprising reason for a skip. For example, excluding all
    tests by means of `--test "!.*"` would still display feature or version
    constraints despite the obvious test exclusion.
    
    Some example output (using '--test no-policies/pod-to-world'):
    
    ```
    [=] Test [no-policies]
      [-] Skipping Scenario [no-policies/pod-to-cidr] (skipped by user)
    ...
      [-] Scenario [no-policies/pod-to-world]
      [.] Action [no-policies/pod-to-world/http-to-one.one.one.one-0: ... (10.244.1.89) -> ... (one.one.one.one:80)]
    ...
      [-] Skipping Scenario [no-policies/pod-to-host] (skipped by user)
      [-] Skipping Scenario [no-policies/pod-to-external-workload] (skipped by user)
    [=] Skipping Test [no-policies-from-outside] (feature node-without-cilium is disabled)
    [=] Skipping Test [no-policies-extra] (skipped by user)
    ```
    
    Since the amount of output generated by the connectivity tests has grown quite
    a bit since I last worked on it, I removed the first ct.Log() in
    ConnectivityTest.Skip() to make the output a little more compact.
    
    RequiredCiliumVSN was renamed and changed to a string since storing the
    semver.Range does not allow communicating the original version constraint to the user.
    
    A few internal methods on Test were shuffled around to keep concerns separated.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- cilium-cli/connectivity/check/features_test.go
+++ cilium-cli/connectivity/check/features_test.go
@@ -7,11 +7,11 @@ import "testing"
 
 func TestFeatureSetMatchRequirements(t *testing.T) {
 	features := FeatureSet{}
-	matches := features.MatchRequirements()
+	matches, _ := features.MatchRequirements()
 	if !matches {
 		t.Error("empty requirements should always match")
 	}
-	matches = features.MatchRequirements(RequireFeatureEnabled(FeatureL7Proxy))
+	matches, _ = features.MatchRequirements(RequireFeatureEnabled(FeatureL7Proxy))
 	if matches {
 		t.Error("empty features should not match any requirement")
 	}
@@ -19,11 +19,11 @@ func TestFeatureSetMatchRequirements(t *testing.T) {
 	features[FeatureL7Proxy] = FeatureStatus{
 		Enabled: true,
 	}
-	matches = features.MatchRequirements()
+	matches, _ = features.MatchRequirements()
 	if !matches {
 		t.Error("empty requirements should always match")
 	}
-	matches = features.MatchRequirements(RequireFeatureEnabled(FeatureL7Proxy))
+	matches, _ = features.MatchRequirements(RequireFeatureEnabled(FeatureL7Proxy))
 	if !matches {
 		t.Errorf("expected features %v to match feature %v", features, FeatureL7Proxy)
 	}
@@ -33,20 +33,20 @@ func TestFeatureSetMatchRequirements(t *testing.T) {
 		Enabled: true,
 		Mode:    cniMode,
 	}
-	matches = features.MatchRequirements()
+	matches, _ = features.MatchRequirements()
 	if !matches {
 		t.Error("empty requirements should always match")
 	}
-	matches = features.MatchRequirements(RequireFeatureEnabled(FeatureL7Proxy))
+	matches, _ = features.MatchRequirements(RequireFeatureEnabled(FeatureL7Proxy))
 	if !matches {
 		t.Errorf("expected features %v to match feature %v", features, FeatureL7Proxy)
 	}
-	matches = features.MatchRequirements(RequireFeatureEnabled(FeatureCNIChaining), RequireFeatureMode(FeatureCNIChaining, cniMode))
+	matches, _ = features.MatchRequirements(RequireFeatureEnabled(FeatureCNIChaining), RequireFeatureMode(FeatureCNIChaining, cniMode))
 	if !matches {
 		t.Errorf("expected features %v to match feature %v with mode %v", features, FeatureCNIChaining, cniMode)
 	}
 	cniMode = "generic-veth"
-	matches = features.MatchRequirements(RequireFeatureEnabled(FeatureCNIChaining), RequireFeatureMode(FeatureCNIChaining, cniMode))
+	matches, _ = features.MatchRequirements(RequireFeatureEnabled(FeatureCNIChaining), RequireFeatureMode(FeatureCNIChaining, cniMode))
 	if matches {
 		t.Errorf("features %v unexpectedly matched feature %v with mode %v", features, FeatureCNIChaining, cniMode)
 	}
