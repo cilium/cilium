44cbcec5f9ac973f24d48f76b9fd3b0e9d3cc41a
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2023-08-25 06:53:43 +0000

    chart: define the image variable in the makefile
    [ upstream commit fa3cf343e292a9da06dad2639e70d0e866b7d811 ]
    
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- images/scripts/update-cilium-envoy-image.sh
+++ images/scripts/update-cilium-envoy-image.sh
@@ -17,7 +17,7 @@ github_repo="cilium/proxy"
 github_branch="main"
 image="quay.io/cilium/cilium-envoy"
 
-latest_commit_sha="$(curl -s https://api.github.com/repos/${github_repo}/commits/${github_branch} | jq -r '.sha')"
+latest_commit_sha="$(curl -s https://api.github.com/repos/${github_repo}/commits/${github_branch} | jq -r --exit-status '.sha')"
 envoy_version="$(curl -s https://raw.githubusercontent.com/${github_repo}/"${latest_commit_sha}"/ENVOY_VERSION)"
 
 image_tag="${envoy_version//envoy-/v}-${latest_commit_sha}"
@@ -30,17 +30,18 @@ fi
 
 echo "Latest image from branch ${github_branch}: ${image_full}"
 
-echo "Updating image in ./images/cilium/Dockerfile"
-sed -i -E "s|(FROM ${image}:)(.*)(@sha256:[0-9a-z]*)( as cilium-envoy)|\1${image_tag}@${image_sha256}\4|" ./images/cilium/Dockerfile
+DOCKERFILEPATH="./images/cilium/Dockerfile"
+echo "Updating image in ${DOCKERFILEPATH}"
+sed -i -E "s|(FROM ${image}:)(.*)(@sha256:[0-9a-z]*)( as cilium-envoy)|\1${image_tag}@${image_sha256}\4|" ${DOCKERFILEPATH}
 
-echo "Updating image in ./install/kubernetes/cilium/values.yaml.tmpl"
-# Using tr to workaround matching the multiline regex with sed
-# yq would change formatting: https://github.com/mikefarah/yq/issues/465
-# use of envoy.image.override (which would allow match in one line) isn't optimal either
-< ./install/kubernetes/cilium/values.yaml.tmpl tr '\n' '\f' |
-  sed -E "s|(# -- Envoy container image\..*tag: \")(v[0-9a-zA-Z\.-]*)(\")|\1${image_tag}\3|" |
-  sed -E "s|(# -- Envoy container image\..*digest: \")(sha256:[0-9a-z]*)(\")|\1${image_sha256}\3|" |
-  tr '\f' '\n' > ./install/kubernetes/cilium/values.yaml.tmpl_tmp &&
-  mv ./install/kubernetes/cilium/values.yaml.tmpl_tmp ./install/kubernetes/cilium/values.yaml.tmpl
+MAKEFILEPATH="./install/kubernetes/Makefile.values"
+echo "Updating image in ${MAKEFILEPATH}"
+sed -i -E "s|export[[:space:]]+CILIUM_ENVOY_VERSION:=.*|export CILIUM_ENVOY_VERSION:=${image_tag}|" ${MAKEFILEPATH}
+sed -i -E "s|export[[:space:]]+CILIUM_ENVOY_DIGEST:=.*|export CILIUM_ENVOY_DIGEST:=${image_sha256}|" ${MAKEFILEPATH}
 
-echo "Please don't forget to execute 'make -C Documentation update-helm-values && make -C install/kubernetes'"
+if git diff --exit-code ./install/kubernetes/Makefile.values ./images/cilium/Dockerfile &>/dev/null ; then
+  echo "The envoy image is already up to date"
+else
+  echo "Updated the envoy image to be a latest version"
+  echo "Please don't forget to execute 'make -C Documentation update-helm-values && make -C install/kubernetes'"
+fi
