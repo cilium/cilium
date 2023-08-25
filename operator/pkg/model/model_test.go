cilium/cilium

commit 677e8b49481c4c6e3a1bf27d1e4da8cce2726ef9

Implement TLSRoute
This implements the TLSRoute for GatewayAPI.
It is based on the envoy configuration aproach we took for HTTPRoute.
It also adds additional logic needed to re-use most of the code parts.

Signed-off-by: Maartje Eyskens <maartje.eyskens@isovalent.com>

operator/pkg/model/model.go
@@ -13,6 +13,26 @@ import (
 // of various types of Kubernetes config to Cilium config.
 type Model struct {
 	HTTP []HTTPListener `json:"http,omitempty"`
+	TLS  []TLSListener  `json:"tls,omitempty"`
+}
+
+func (m *Model) GetListeners() []Listener {
+	var listeners []Listener
+
+	for _, l := range m.HTTP {
+		listeners = append(listeners, &l)
+	}
+
+	for _, l := range m.TLS {
+		listeners = append(listeners, &l)
+	}
+
+	return listeners
+}
+
+type Listener interface {
+	GetSources() []FullyQualifiedResource
+	GetPort() uint32
 }
 
 // HTTPListener holds configuration for any listener that terminates and proxies HTTP
@@ -44,6 +64,50 @@ type HTTPListener struct {
 	Routes []HTTPRoute `json:"routes,omitempty"`
 	// Service configuration
 	Service *Service `json:"service,omitempty"`
+}
+
+func (l *HTTPListener) GetSources() []FullyQualifiedResource {
+	return l.Sources
+}
+
+func (l *HTTPListener) GetPort() uint32 {
+	return l.Port
+}
+
+// TLSListener holds configuration for any listener that proxies TLS
+// based on the SNI value.
+// Each holds the configuration info for one distinct TLS listener, by
+//   - Hostname
+//   - Address
+//   - Port
+type TLSListener struct {
+	// Name of the TLSListener
+	Name string `json:"name,omitempty"`
+	// Sources is a slice of fully qualified resources this TLSListener is sourced
+	// from.
+	Sources []FullyQualifiedResource `json:"sources,omitempty"`
+	// IPAddress that the listener should listen on.
+	// The string must be parseable as an IP address.
+	Address string `json:"address,omitempty"`
+	// Port on which the service can be expected to be accessed by clients.
+	Port uint32 `json:"port,omitempty"`
+	// Hostname that the listener should match.
+	// Wildcards are supported in prefix or suffix forms, or the special wildcard `*`.
+	// An empty list means that the Listener should match all hostnames.
+	Hostname string `json:"hostname,omitempty"`
+	// Routes associated with traffic to the service.
+	// An empty list means that traffic will not be routed.
+	Routes []TLSRoute `json:"routes,omitempty"`
+	// Service configuration
+	Service *Service `json:"service,omitempty"`
+}
+
+func (l *TLSListener) GetSources() []FullyQualifiedResource {
+	return l.Sources
+}
+
+func (l *TLSListener) GetPort() uint32 {
+	return l.Port
 }
 
 // Service holds the configuration for desired Service details
@@ -192,6 +256,15 @@ func (r *HTTPRoute) GetMatchKey() string {
 	}
 
 	return sb.String()
+}
+
+// TLSRoute holds all the details needed to route TLS traffic to a backend.
+type TLSRoute struct {
+	Name string `json:"name,omitempty"`
+	// Hostnames that the route should match
+	Hostnames []string `json:"hostnames,omitempty"`
+	// Backend is the backend handling the requests
+	Backends []Backend `json:"backends,omitempty"`
 }
 
 // StringMatch describes various types of string matching.

operator/pkg/model/model_test.go
@@ -1,0 +1,70 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package model
+
+import (
+	"reflect"
+	"testing"
+)
+
+var testTLSListener = TLSListener{
+	Name: "test-tls-listener",
+	Port: 443,
+}
+
+var testHTTPListener = HTTPListener{
+	Name: "test-http-listener",
+	Port: 80,
+}
+
+func TestModel_GetListeners(t *testing.T) {
+	type fields struct {
+		HTTP []HTTPListener
+		TLS  []TLSListener
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []Listener
+	}{
+		{
+			name: "Combine HTTP and TLS listeners",
+			fields: fields{
+				HTTP: []HTTPListener{testHTTPListener},
+				TLS:  []TLSListener{testTLSListener},
+			},
+			want: []Listener{&testHTTPListener, &testTLSListener},
+		},
+		{
+			name: "Only HTTP listeners",
+			fields: fields{
+				HTTP: []HTTPListener{testHTTPListener},
+			},
+			want: []Listener{&testHTTPListener},
+		},
+		{
+			name: "Only TLS listeners",
+			fields: fields{
+				TLS: []TLSListener{testTLSListener},
+			},
+			want: []Listener{&testTLSListener},
+		},
+		{
+			name:   "No listeners",
+			fields: fields{},
+			want:   nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Model{
+				HTTP: tt.fields.HTTP,
+				TLS:  tt.fields.TLS,
+			}
+			if got := m.GetListeners(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Model.GetListeners() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

.github/workflows/conformance-gateway-api.yaml
@@ -109,12 +109,14 @@ jobs:
           kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/${{ env.gateway_api_version }}/config/crd/experimental/gateway.networking.k8s.io_gatewayclasses.yaml
           kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/${{ env.gateway_api_version }}/config/crd/experimental/gateway.networking.k8s.io_gateways.yaml
           kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/${{ env.gateway_api_version }}/config/crd/experimental/gateway.networking.k8s.io_httproutes.yaml
+          kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/${{ env.gateway_api_version }}/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml
           kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/${{ env.gateway_api_version }}/config/crd/experimental/gateway.networking.k8s.io_referencegrants.yaml
 
           # To make sure that Gateway API CRs are available
           kubectl wait --for condition=Established crd/gatewayclasses.gateway.networking.k8s.io --timeout=${{ env.timeout }}
           kubectl wait --for condition=Established crd/gateways.gateway.networking.k8s.io --timeout=${{ env.timeout }}
           kubectl wait --for condition=Established crd/httproutes.gateway.networking.k8s.io --timeout=${{ env.timeout }}
+          kubectl wait --for condition=Established crd/tlsroutes.gateway.networking.k8s.io --timeout=${{ env.timeout }}
           kubectl wait --for condition=Established crd/referencegrants.gateway.networking.k8s.io --timeout=${{ env.timeout }}
 
       - name: Install Cilium

install/kubernetes/cilium/templates/cilium-operator/clusterrole.yaml
@@ -247,6 +247,7 @@ rules:
   resources:
   - gatewayclasses
   - gateways
+  - tlsroutes
   - httproutes
   - referencegrants
   - referencepolicies
@@ -260,6 +261,7 @@ rules:
   - gatewayclasses/status
   - gateways/status
   - httproutes/status
+  - tlsroutes/status
   verbs:
   - update
   - patch

operator/pkg/gateway-api/conformance_test.go
@@ -20,7 +20,7 @@ import (
 
 // TestConformance runs the conformance tests for Gateway API
 // Adapted from https://github.com/kubernetes-sigs/gateway-api/blob/v0.6.1/conformance/conformance_test.go
-// Some features are not supported by Cilium (e.g. TLSRoute), so we skip them.
+// Some features are not supported by Cilium, so we skip them.
 // This test should be adjusted as new features are added to the Gateway API.
 //
 // The below command can be used to run the conformance tests locally, you can also run directly from

operator/pkg/gateway-api/controller.go
@@ -97,6 +97,15 @@ func NewController(enableSecretSync bool, secretsNamespace string) (*Controller,
 		return nil, err
 	}
 
+	tlsReconciler := &tlsRouteReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+		Model:  m,
+	}
+	if err = tlsReconciler.SetupWithManager(mgr); err != nil {
+		return nil, err
+	}
+
 	rgReconciler := &referenceGrantReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
@@ -266,6 +275,13 @@ func onlyStatusChanged() predicate.Predicate {
 			case *gatewayv1beta1.HTTPRoute:
 				o, _ := e.ObjectOld.(*gatewayv1beta1.HTTPRoute)
 				n, ok := e.ObjectNew.(*gatewayv1beta1.HTTPRoute)
+				if !ok {
+					return false
+				}
+				return !cmp.Equal(o.Status, n.Status, option)
+			case *gatewayv1alpha2.TLSRoute:
+				o, _ := e.ObjectOld.(*gatewayv1alpha2.TLSRoute)
+				n, ok := e.ObjectNew.(*gatewayv1alpha2.TLSRoute)
 				if !ok {
 					return false
 				}

operator/pkg/gateway-api/controller_test.go
@@ -19,6 +19,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 
 	"github.com/cilium/cilium/operator/pkg/model"
@@ -85,6 +86,24 @@ var controllerTestFixture = []client.Object{
 				{
 					Name: "https",
 					Port: 80,
+				},
+			},
+		},
+	},
+
+	// Gateway for TLSRoute
+	&gatewayv1beta1.Gateway{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "gateway-tlsroute",
+			Namespace: "default",
+		},
+		Spec: gatewayv1beta1.GatewaySpec{
+			GatewayClassName: "cilium",
+			Listeners: []gatewayv1beta1.Listener{
+				{
+					Name:     "tls",
+					Protocol: gatewayv1beta1.TLSProtocolType,
+					Port:     443,
 				},
 			},
 		},
@@ -634,6 +653,102 @@ func Test_onlyStatusChanged(t *testing.T) {
 												ObservedGeneration: 100,
 												Reason:             "Accepted",
 												Message:            "Valid HTTPRoute",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "no change in TLSRoute status",
+			args: args{
+				evt: event.UpdateEvent{
+					ObjectOld: &gatewayv1alpha2.TLSRoute{},
+					ObjectNew: &gatewayv1alpha2.TLSRoute{},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "change in TLSRoute status",
+			args: args{
+				evt: event.UpdateEvent{
+					ObjectOld: &gatewayv1alpha2.TLSRoute{},
+					ObjectNew: &gatewayv1alpha2.TLSRoute{
+						Status: gatewayv1alpha2.TLSRouteStatus{
+							RouteStatus: gatewayv1beta1.RouteStatus{
+								Parents: []gatewayv1beta1.RouteParentStatus{
+									{
+										ParentRef: gatewayv1beta1.ParentReference{
+											Name: "test-gateway",
+										},
+										ControllerName: "io.cilium/gateway-controller",
+										Conditions: []metav1.Condition{
+											{
+												Type:               "Accepted",
+												Status:             "True",
+												ObservedGeneration: 100,
+												Reason:             "Accepted",
+												Message:            "Valid TLSRoute",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "only change LastTransitionTime in TLSRoute status",
+			args: args{
+				evt: event.UpdateEvent{
+					ObjectOld: &gatewayv1alpha2.TLSRoute{
+						Status: gatewayv1alpha2.TLSRouteStatus{
+							RouteStatus: gatewayv1beta1.RouteStatus{
+								Parents: []gatewayv1beta1.RouteParentStatus{
+									{
+										ParentRef: gatewayv1beta1.ParentReference{
+											Name: "test-gateway",
+										},
+										ControllerName: "io.cilium/gateway-controller",
+										Conditions: []metav1.Condition{
+											{
+												Type:               "Accepted",
+												Status:             "True",
+												ObservedGeneration: 100,
+												Reason:             "Accepted",
+												Message:            "Valid TLSRoute",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					ObjectNew: &gatewayv1alpha2.TLSRoute{
+						Status: gatewayv1alpha2.TLSRouteStatus{
+							RouteStatus: gatewayv1beta1.RouteStatus{
+								Parents: []gatewayv1beta1.RouteParentStatus{
+									{
+										ParentRef: gatewayv1beta1.ParentReference{
+											Name: "test-gateway",
+										},
+										ControllerName: "io.cilium/gateway-controller",
+										Conditions: []metav1.Condition{
+											{
+												Type:               "Accepted",
+												Status:             "True",
+												ObservedGeneration: 100,
+												Reason:             "Accepted",
+												Message:            "Valid TLSRoute",
 											},
 										},
 									},

operator/pkg/gateway-api/gateway.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -17,6 +18,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+	gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -60,6 +62,11 @@ func (r *gatewayReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		// always update status always at least for observedGeneration value.
 		Watches(&source.Kind{Type: &gatewayv1beta1.HTTPRoute{}},
 			r.enqueueRequestForOwningHTTPRoute(),
+			builder.WithPredicates(onlyStatusChanged())).
+		// Watch TLS Route status changes, there is one assumption that any change in spec will
+		// always update status always at least for observedGeneration value.
+		Watches(&source.Kind{Type: &gatewayv1alpha2.TLSRoute{}},
+			r.enqueueRequestForOwningTLSRoute(),
 			builder.WithPredicates(onlyStatusChanged())).
 		// Watch related secrets used to configure TLS
 		Watches(&source.Kind{Type: &corev1.Secret{}}, r.enqueueRequestForTLSSecret(),
@@ -139,39 +146,57 @@ func (r *gatewayReconciler) enqueueRequestForOwningResource() handler.EventHandl
 // belonging to the given Gateway
 func (r *gatewayReconciler) enqueueRequestForOwningHTTPRoute() handler.EventHandler {
 	return handler.EnqueueRequestsFromMapFunc(func(a client.Object) []reconcile.Request {
-		scopedLog := log.WithFields(logrus.Fields{
-			logfields.Controller: gateway,
-			logfields.Resource:   a.GetName(),
-		})
-
-		var reqs []reconcile.Request
-
 		hr, ok := a.(*gatewayv1beta1.HTTPRoute)
 		if !ok {
 			return nil
 		}
 
-		for _, parent := range hr.Spec.ParentRefs {
-			if !IsGateway(parent) {
-				continue
-			}
-
-			ns := namespaceDerefOr(parent.Namespace, hr.GetNamespace())
-			scopedLog.WithFields(logrus.Fields{
-				logfields.K8sNamespace: ns,
-				logfields.Resource:     parent.Name,
-				httpRoute:              hr.GetName(),
-			}).Info("Enqueued gateway for HTTPRoute")
-
-			reqs = append(reqs, reconcile.Request{
-				NamespacedName: types.NamespacedName{
-					Namespace: ns,
-					Name:      string(parent.Name),
-				},
-			})
-		}
-		return reqs
+		return getReconcileRequestsForRoute(a, hr.Spec.CommonRouteSpec)
 	})
+}
+
+// enqueueRequestForOwningTLSRoute returns an event handler for any changes with TLS Routes
+// belonging to the given Gateway
+func (r *gatewayReconciler) enqueueRequestForOwningTLSRoute() handler.EventHandler {
+	return handler.EnqueueRequestsFromMapFunc(func(a client.Object) []reconcile.Request {
+		hr, ok := a.(*gatewayv1alpha2.TLSRoute)
+		if !ok {
+			return nil
+		}
+
+		return getReconcileRequestsForRoute(a, hr.Spec.CommonRouteSpec)
+	})
+}
+
+func getReconcileRequestsForRoute(object metav1.Object, route gatewayv1beta1.CommonRouteSpec) []reconcile.Request {
+	var reqs []reconcile.Request
+
+	scopedLog := log.WithFields(logrus.Fields{
+		logfields.Controller: gateway,
+		logfields.Resource:   object.GetName(),
+	})
+
+	for _, parent := range route.ParentRefs {
+		if !IsGateway(parent) {
+			continue
+		}
+
+		ns := namespaceDerefOr(parent.Namespace, object.GetNamespace())
+		scopedLog.WithFields(logrus.Fields{
+			logfields.K8sNamespace: ns,
+			logfields.Resource:     parent.Name,
+			logfields.Route:        object.GetName(),
+		}).Info("Enqueued gateway for Route")
+
+		reqs = append(reqs, reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Namespace: ns,
+				Name:      string(parent.Name),
+			},
+		})
+	}
+
+	return reqs
 }
 
 // enqueueRequestForOwningTLSCertificate returns an event handler for any changes with TLS secrets

commit 3c05c8d233663812a065b76a090ed1e03f883e2f

GatewayAPI: Fix listener parsing
Previously, parsing the listeners of a Gateway object only worked correctly,
if only one listener of a given type (HTTP or TLS) was present, as looping
over them was incorrectly handled.

Fixes: #27533
Fixes: 677e8b49481c ("Implement TLSRoute")

Signed-off-by: Patrick Reich <patrick@neodyme.io>

operator/pkg/model/model.go
@@ -19,12 +19,12 @@ type Model struct {
 func (m *Model) GetListeners() []Listener {
 	var listeners []Listener
 
-	for _, l := range m.HTTP {
-		listeners = append(listeners, &l)
+	for i := range m.HTTP {
+		listeners = append(listeners, &m.HTTP[i])
 	}
 
-	for _, l := range m.TLS {
-		listeners = append(listeners, &l)
+	for i := range m.TLS {
+		listeners = append(listeners, &m.TLS[i])
 	}
 
 	return listeners

operator/pkg/model/model_test.go
@@ -13,8 +13,18 @@ var testTLSListener = TLSListener{
 	Port: 443,
 }
 
+var testTLSListener2 = TLSListener{
+	Name: "test-tls-listener2",
+	Port: 443,
+}
+
 var testHTTPListener = HTTPListener{
 	Name: "test-http-listener",
+	Port: 80,
+}
+
+var testHTTPListener2 = HTTPListener{
+	Name: "test-http-listener2",
 	Port: 80,
 }
 
@@ -31,24 +41,24 @@ func TestModel_GetListeners(t *testing.T) {
 		{
 			name: "Combine HTTP and TLS listeners",
 			fields: fields{
-				HTTP: []HTTPListener{testHTTPListener},
-				TLS:  []TLSListener{testTLSListener},
+				HTTP: []HTTPListener{testHTTPListener, testHTTPListener2},
+				TLS:  []TLSListener{testTLSListener, testTLSListener2},
 			},
-			want: []Listener{&testHTTPListener, &testTLSListener},
+			want: []Listener{&testHTTPListener, &testHTTPListener2, &testTLSListener, &testTLSListener2},
 		},
 		{
 			name: "Only HTTP listeners",
 			fields: fields{
-				HTTP: []HTTPListener{testHTTPListener},
+				HTTP: []HTTPListener{testHTTPListener, testHTTPListener2},
 			},
-			want: []Listener{&testHTTPListener},
+			want: []Listener{&testHTTPListener, &testHTTPListener2},
 		},
 		{
 			name: "Only TLS listeners",
 			fields: fields{
-				TLS: []TLSListener{testTLSListener},
+				TLS: []TLSListener{testTLSListener, testTLSListener2},
 			},
-			want: []Listener{&testTLSListener},
+			want: []Listener{&testTLSListener, &testTLSListener2},
 		},
 		{
 			name:   "No listeners",