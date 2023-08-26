44fb862a698bec4e3403960319442a3e7229df75
Haiyue Wang <haiyue.wang@intel.com>
2023-08-26 21:24:22 +0800

    ipsec: Fix Godoc document comment typo
    Fix the commit b2b901fb19 ("cilium: ipsec, add go API to configure xfrm (IPSec)")
    one Godoc comment typo.
    
    Signed-off-by: Haiyue Wang <haiyue.wang@intel.com>

--- pkg/datapath/linux/ipsec/doc.go
+++ pkg/datapath/linux/ipsec/doc.go
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-// Package ipsec provides the Linux datpaath specific abstraction and
+// Package ipsec provides the Linux datapath specific abstraction and
 // useful helpers to manage IPSec via Linux xfrm.
 package ipsec
