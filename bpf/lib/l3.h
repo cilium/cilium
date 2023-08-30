afbf754e8b1d2bf787b1202a6b91c093273c80fc
Julian Wiedmann <jwi@isovalent.com>
2023-08-30 08:04:54 +0300

    bpf: lxc: transfer sec identity for per-EP loopback in reply direction
    l3_local_delivery() contains a special case to bypass ingress policy for
    loopback traffic. But when redirecting loopback requests / replies in a
    per-EP config, they don't actually bypass the policy program.
    The redirect_ep() delivers them to the host-side veth interface, and
    ingress policy is applied as the packet passes to the pod-side veth peer.
    
    https://github.com/cilium/cilium/pull/27602 recently changed the logic so
    that `hairpin_flow` is only set for replies. A subtle side-effect of that
    change is that in a per-EP config, l3_local_delivery() will now copy the
    the src sec identity into the skb->mark. cil_to_container() then extracts
    it, and tail_ipv4_to_endpoint() no longer performs a ipcache lookup for
    ip4->saddr (which is IPV4_LOOPBACK, and would result in WORLD_ID).
    
    Right now this is mostly a cosmetic improvement, as the identity is not
    used for policy enforcement of loopback traffic. But there are still users
    (eg. drop notifications) in tail_ipv4_to_endpoint() that benefit from
    having the correct security identity in place.
    
    Therefore make the same change in reply direction, so that we also get
    the correct security identity there (instead of an ipcache lookup for the
    Service IP, which would return WORLD_ID). For this push the loopback check
    into the tail-call specific path of l3_local_delivery(), while the per-EP
    path inserts the identity into skb->mark and then just calls its "normal"
    redirect_ep().
    
    Reported-by: Timo Beckers <timo@isovalent.com>
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/l3.h
+++ bpf/lib/l3.h
@@ -71,7 +71,8 @@ static __always_inline int ipv4_l3(struct __ctx_buff *ctx, int l3_off,
 #ifndef SKIP_POLICY_MAP
 static __always_inline int
 l3_local_delivery(struct __ctx_buff *ctx, __u32 seclabel,
-		  const struct endpoint_info *ep, __u8 direction __maybe_unused,
+		  const struct endpoint_info *ep __maybe_unused,
+		  __u8 direction __maybe_unused,
 		  bool from_host __maybe_unused, bool hairpin_flow __maybe_unused,
 		  bool from_tunnel __maybe_unused, __u32 cluster_id __maybe_unused)
 {
@@ -83,16 +84,6 @@ l3_local_delivery(struct __ctx_buff *ctx, __u32 seclabel,
 	 */
 	update_metrics(ctx_full_len(ctx), direction, REASON_FORWARDED);
 #endif
-
-#ifndef DISABLE_LOOPBACK_LB
-	/* Skip ingress policy enforcement for hairpin traffic. As the hairpin
-	 * traffic is destined to a local pod (more specifically, the same pod
-	 * the traffic originated from, we skip the tail call for ingress policy
-	 * enforcement, and directly redirect it to the endpoint).
-	 */
-	if (unlikely(hairpin_flow))
-		return redirect_ep(ctx, ep->ifindex, from_host);
-#endif /* DISABLE_LOOPBACK_LB */
 
 #if defined(USE_BPF_PROG_FOR_INGRESS_POLICY) && \
 	!defined(FORCE_LOCAL_POLICY_EVAL_AT_SOURCE)
@@ -113,6 +104,16 @@ l3_local_delivery(struct __ctx_buff *ctx, __u32 seclabel,
 	return redirect_ep(ctx, ep->ifindex, from_host);
 # endif /* !ENABLE_ROUTING && TUNNEL_MODE && !ENABLE_NODEPORT */
 #else
+# ifndef DISABLE_LOOPBACK_LB
+	/* Skip ingress policy enforcement for hairpin traffic. As the hairpin
+	 * traffic is destined to a local pod (more specifically, the same pod
+	 * the traffic originated from) we skip the tail call for ingress policy
+	 * enforcement, and directly redirect it to the endpoint.
+	 */
+	if (unlikely(hairpin_flow))
+		return redirect_ep(ctx, ep->ifindex, from_host);
+# endif /* DISABLE_LOOPBACK_LB */
+
 	/* Jumps to destination pod's BPF program to enforce ingress policies. */
 	ctx_store_meta(ctx, CB_SRC_LABEL, seclabel);
 	ctx_store_meta(ctx, CB_IFINDEX, ep->ifindex);
