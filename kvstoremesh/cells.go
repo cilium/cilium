043adc2d9fff104ec2971355f8e0543c79149e96
Marco Iorio <marco.iorio@isovalent.com>
2023-08-30 17:48:04 +0200

    kvstoremesh: refactor cobra cmd and hive to avoid globals
    Refactor kvstoremesh to mimic the structure adopted by the agent, as a
    preparation to merge it with the clustermesh-apiserver in a single binary.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

---
+++ kvstoremesh/cells.go
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package main
+
+import (
+	cmmetrics "github.com/cilium/cilium/clustermesh-apiserver/metrics"
+	"github.com/cilium/cilium/clustermesh-apiserver/option"
+	"github.com/cilium/cilium/pkg/clustermesh/kvstoremesh"
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/gops"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/kvstore"
+	"github.com/cilium/cilium/pkg/pprof"
+)
+
+var Cell = cell.Module(
+	"kvstoremesh",
+	"Cilium KVStoreMesh",
+
+	cell.Config(option.DefaultLegacyKVStoreMeshConfig),
+
+	cell.Config(cmtypes.DefaultClusterInfo),
+	cell.Invoke(registerClusterInfoValidator),
+
+	pprof.Cell,
+	cell.Config(pprof.Config{
+		PprofAddress: option.PprofAddress,
+		PprofPort:    option.PprofPortKVStoreMesh,
+	}),
+	controller.Cell,
+
+	gops.Cell(defaults.GopsPortKVStoreMesh),
+	cmmetrics.Cell,
+
+	kvstore.Cell(kvstore.EtcdBackendName),
+	cell.Provide(func() *kvstore.ExtraOptions { return nil }),
+	kvstoremesh.Cell,
+
+	cell.Invoke(func(*kvstoremesh.KVStoreMesh) {}),
+)
--- kvstoremesh/main.go
+++ kvstoremesh/main.go
@@ -10,74 +10,42 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
-	cmmetrics "github.com/cilium/cilium/clustermesh-apiserver/metrics"
-	kmopt "github.com/cilium/cilium/kvstoremesh/option"
-	"github.com/cilium/cilium/pkg/clustermesh/kvstoremesh"
 	"github.com/cilium/cilium/pkg/clustermesh/types"
-	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
-	"github.com/cilium/cilium/pkg/controller"
-	"github.com/cilium/cilium/pkg/defaults"
-	"github.com/cilium/cilium/pkg/gops"
 	"github.com/cilium/cilium/pkg/hive"
-	"github.com/cilium/cilium/pkg/hive/cell"
-	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
-	"github.com/cilium/cilium/pkg/pprof"
 )
 
 var (
 	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "kvstoremesh")
+)
 
-	rootHive *hive.Hive
-
-	rootCmd = &cobra.Command{
+func NewCmd(h *hive.Hive) *cobra.Command {
+	rootCmd := &cobra.Command{
 		Use:   "kvstoremesh",
 		Short: "Run KVStoreMesh",
 		Run: func(cmd *cobra.Command, args []string) {
-			if err := rootHive.Run(); err != nil {
+			if err := h.Run(); err != nil {
 				log.Fatal(err)
 			}
 		},
 		PreRun: func(cmd *cobra.Command, args []string) {
 			// Overwrite the metrics namespace with the one specific for KVStoreMesh
 			metrics.Namespace = metrics.CiliumKVStoreMeshNamespace
-			option.Config.Populate(rootHive.Viper())
+			option.Config.Populate(h.Viper())
 			if option.Config.Debug {
 				log.Logger.SetLevel(logrus.DebugLevel)
 			}
-			option.LogRegisteredOptions(rootHive.Viper(), log)
+			option.LogRegisteredOptions(h.Viper(), log)
 		},
 	}
-)
 
-func init() {
-	rootHive = hive.New(
-		pprof.Cell,
-		cell.Config(pprof.Config{
-			PprofAddress: kmopt.PprofAddress,
-			PprofPort:    kmopt.PprofPort,
-		}),
-		controller.Cell,
+	h.RegisterFlags(rootCmd.Flags())
+	rootCmd.AddCommand(h.Command())
 
-		gops.Cell(defaults.GopsPortKVStoreMesh),
-		cmmetrics.Cell,
-
-		cell.Config(kmopt.KVStoreMeshConfig{}),
-		cell.Config(cmtypes.DefaultClusterInfo),
-		cell.Invoke(registerClusterInfoValidator),
-
-		kvstore.Cell(kvstore.EtcdBackendName),
-		cell.Provide(func() *kvstore.ExtraOptions { return nil }),
-		kvstoremesh.Cell,
-
-		cell.Invoke(func(*kvstoremesh.KVStoreMesh) {}),
-	)
-
-	rootHive.RegisterFlags(rootCmd.Flags())
-	rootCmd.AddCommand(rootHive.Command())
+	return rootCmd
 }
 
 func registerClusterInfoValidator(lc hive.Lifecycle, cinfo types.ClusterInfo) {
@@ -87,7 +55,7 @@ func registerClusterInfoValidator(lc hive.Lifecycle, cinfo types.ClusterInfo) {
 }
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := NewCmd(hive.New(Cell)).Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
