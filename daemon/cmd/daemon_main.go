cilium/cilium

commit 9dc1350fdb81eca6dcf00076dd6ec39258e80d0a

endpoint: Enhance policy map sync
When syncing policy map with dump, compare the desired policy map to
the dumped map for both deletes and adds. Record and log any
differences found.

Fixes: #14358
Fixes: #14357
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/endpoint/bpf.go
@@ -1238,65 +1238,95 @@ func (e *Endpoint) applyPolicyMapChanges() (proxyChanges bool, err error) {
 // difference between the realized and desired policy state without
 // dumping the bpf policy map.
 func (e *Endpoint) syncPolicyMap() error {
-	e.policyDebug(logrus.Fields{
-		"policyRealized": e.realizedPolicy == e.desiredPolicy,
-	}, "syncPolicyMap")
-
-	// Nothing to do if the desired policy is already fully realized.
-	if e.realizedPolicy != e.desiredPolicy {
-		errors := 0
-
-		// Add policy map entries before deleting to avoid transient drops
-		err := e.addPolicyMapDelta()
-		if err != nil {
-			errors++
-		}
-
-		// Delete policy keys present in the realized state, but not present in the desired state
-		for keyToDelete := range e.realizedPolicy.PolicyMapState {
-			// If key that is in realized state is not in desired state, just remove it.
-			if _, ok := e.desiredPolicy.PolicyMapState[keyToDelete]; !ok {
-				if !e.deletePolicyKey(keyToDelete, false, nil) {
-					errors++
-				}
-			}
-		}
-
-		if errors > 0 {
-			return fmt.Errorf("syncPolicyMap failed")
-		}
+	// Apply pending policy map changes first so that desired map is up-to-date before
+	// we diff the maps below.
+	_, err := e.applyPolicyMapChanges()
+	if err != nil {
+		return err
 	}
 
-	// Still may have changes due to identities added and/or
-	// deleted after the desired policy was computed.
-	_, err := e.applyPolicyMapChanges()
-	return err
-}
-
-// addPolicyMapDelta adds new or updates existing bpf policy map state based
-// on the difference between the realized and desired policy state without
-// dumping the bpf policy map.
-func (e *Endpoint) addPolicyMapDelta() error {
 	// Nothing to do if the desired policy is already fully realized.
 	if e.realizedPolicy == e.desiredPolicy {
+		e.policyDebug(nil, "syncPolicyMap(): not syncing as desired == realized")
 		return nil
 	}
 
+	// Diffs between the maps are expected here, so do not bother collecting them
+	_, _, err = e.syncDesiredPolicyMapWith(e.realizedPolicy.PolicyMapState, false)
+	return err
+}
+
+// syncDesiredPolicyMapWith updates the bpf policy map state based on the
+// difference between the given 'realized' and desired policy state without
+// dumping the bpf policy map.
+func (e *Endpoint) syncDesiredPolicyMapWith(realized policy.MapState, withDiffs bool) (diffCount int, diffs []policy.MapChange, err error) {
 	errors := 0
 
+	// Add policy map entries before deleting to avoid transient drops
 	for keyToAdd, entry := range e.desiredPolicy.PolicyMapState {
-		if oldEntry, ok := e.realizedPolicy.PolicyMapState[keyToAdd]; !ok || !oldEntry.DatapathEqual(&entry) {
+		if oldEntry, ok := realized[keyToAdd]; !ok || !oldEntry.DatapathEqual(&entry) {
+			// Redirect entries currently come in with a dummy redirect port ("1"), replace it with
+			// the actual proxy port number. This is due to the fact that proxies may not yet have
+			// bound to a specific port when a proxy policy is first instantiated.
+			if entry.IsRedirectEntry() {
+				// Will change to 0 if on a sidecar
+				entry.ProxyPort = e.realizedRedirects[policy.ProxyIDFromKey(e.ID, keyToAdd)]
+			}
 			if !e.addPolicyKey(keyToAdd, entry, false) {
 				errors++
+			}
+			diffCount++
+			if withDiffs {
+				diffs = append(diffs, policy.MapChange{Add: true, Key: keyToAdd, Value: entry})
+			}
+		}
+	}
+
+	// Delete policy keys present in the realized state, but not present in the desired state
+	for keyToDelete := range realized {
+		// If key that is in realized state is not in desired state, just remove it.
+		if entry, ok := e.desiredPolicy.PolicyMapState[keyToDelete]; !ok {
+			if !e.deletePolicyKey(keyToDelete, false, nil) {
+				errors++
+			}
+			diffCount++
+			if withDiffs {
+				diffs = append(diffs, policy.MapChange{Add: false, Key: keyToDelete, Value: entry})
 			}
 		}
 	}
 
 	if errors > 0 {
-		return fmt.Errorf("updating desired PolicyMap state failed")
+		err = fmt.Errorf("syncPolicyMap failed")
 	}
+	return diffCount, diffs, err
+}
 
-	return nil
+func (e *Endpoint) dumpPolicyMapToMapState() (policy.MapState, error) {
+	currentMap := make(policy.MapState)
+
+	cb := func(key bpf.MapKey, value bpf.MapValue) {
+		// Convert key to host byte-order. ToHost() makes a copy.
+		keyHostOrder := key.(*policymap.PolicyKey).ToHost()
+		// Convert from policymap.Key to policy.Key
+		policyKey := policy.Key{
+			Identity:         keyHostOrder.Identity,
+			DestPort:         keyHostOrder.DestPort,
+			Nexthdr:          keyHostOrder.Nexthdr,
+			TrafficDirection: keyHostOrder.TrafficDirection,
+		}
+		// Convert value to host byte-order. ToHost() makes a copy.
+		entryHostOrder := value.(*policymap.PolicyEntry).ToHost()
+		// Convert from policymap.PolicyEntry to policy.MapStateEntry.
+		policyEntry := policy.MapStateEntry{
+			ProxyPort: entryHostOrder.ProxyPort,
+			IsDeny:    policymap.PolicyEntryFlags(entryHostOrder.GetFlags()).IsDeny(),
+		}
+		currentMap[policyKey] = policyEntry
+	}
+	err := e.policyMap.DumpWithCallback(cb)
+
+	return currentMap, err
 }
 
 // syncPolicyMapWithDump attempts to synchronize the PolicyMap for this endpoint to
@@ -1309,16 +1339,18 @@ func (e *Endpoint) addPolicyMapDelta() error {
 // PolicyMap is unable to be dumped, or any update operation to the map fails.
 // Must be called with e.mutex Lock()ed.
 func (e *Endpoint) syncPolicyMapWithDump() error {
-
-	if e.realizedPolicy.PolicyMapState == nil {
-		e.realizedPolicy.PolicyMapState = make(policy.MapState)
-	}
-
 	if e.policyMap == nil {
 		return fmt.Errorf("not syncing PolicyMap state for endpoint because PolicyMap is nil")
 	}
 
-	currentMapContents, err := e.policyMap.DumpKeysToSlice()
+	// Apply pending policy map changes first so that desired map is up-to-date before
+	// we diff the maps below.
+	_, err := e.applyPolicyMapChanges()
+	if err != nil {
+		return err
+	}
+
+	currentMap, err := e.dumpPolicyMapToMapState()
 
 	// If map is unable to be dumped, attempt to close map and open it again.
 	// See GH-4229.
@@ -1339,44 +1371,21 @@ func (e *Endpoint) syncPolicyMapWithDump() error {
 		}
 
 		// Try to dump again, fail if error occurs.
-		currentMapContents, err = e.policyMap.DumpKeysToSlice()
+		currentMap, err = e.dumpPolicyMapToMapState()
 		if err != nil {
 			return err
 		}
 	}
 
-	errors := 0
-
 	// Log full policy map for every dump
-	e.policyDebug(logrus.Fields{
-		"dumpedPolicyMap": currentMapContents,
-	}, "syncPolicyMapWithDump")
+	e.policyDebug(logrus.Fields{"dumpedPolicyMap": currentMap}, "syncPolicyMapWithDump")
+	// Diffs between the maps indicate an error in the policy map update logic.
+	// Collect and log diffs if policy logging is enabled.
+	diffCount, diffs, err := e.syncDesiredPolicyMapWith(currentMap, e.getPolicyLogger() != nil)
 
-	for _, entry := range currentMapContents {
-		// Convert key to host-byte order for lookup in the desiredMapState.
-		keyHostOrder := entry.ToHost()
-
-		// Convert from policymap.Key to policy.Key
-		keyToDelete := policy.Key{
-			Identity:         keyHostOrder.Identity,
-			DestPort:         keyHostOrder.DestPort,
-			Nexthdr:          keyHostOrder.Nexthdr,
-			TrafficDirection: keyHostOrder.TrafficDirection,
-		}
-
-		// If key that is in policy map is not in desired state, just remove it.
-		if _, ok := e.desiredPolicy.PolicyMapState[keyToDelete]; !ok {
-			e.getLogger().WithField(logfields.BPFMapKey, entry.String()).Debug("syncPolicyMapWithDump removing a bpf policy entry not in the desired state")
-			if !e.deletePolicyKey(keyToDelete, false, nil) {
-				errors++
-			}
-		}
-	}
-
-	err = e.addPolicyMapDelta()
-
-	if errors > 0 {
-		return fmt.Errorf("synchronizing desired PolicyMap state failed")
+	if diffCount > 0 {
+		e.getLogger().WithField(logfields.Count, diffCount).Warning("Policy map sync fixed errors, consider running with debug verbose = policy to get detailed dumps")
+		e.policyDebug(logrus.Fields{"dumpedDiffs": diffs}, "syncPolicyMapWithDump")
 	}
 
 	return err

pkg/maps/policymap/policymap.go
@@ -123,6 +123,18 @@ type PolicyEntry struct {
 	Bytes     uint64 `align:"bytes"`
 }
 
+// ToHost returns a copy of entry with fields converted from network byte-order
+// to host-byte-order if necessary.
+func (pe *PolicyEntry) ToHost() PolicyEntry {
+	if pe == nil {
+		return PolicyEntry{}
+	}
+
+	n := *pe
+	n.ProxyPort = byteorder.NetworkToHost(n.ProxyPort).(uint16)
+	return n
+}
+
 func (pe *PolicyEntry) SetFlags(flags uint8) {
 	pe.Flags = flags
 }
@@ -373,18 +385,6 @@ func (pm *PolicyMap) DumpToSlice() (PolicyEntriesDump, error) {
 	err := pm.DumpWithCallback(cb)
 
 	return entries, err
-}
-
-func (pm *PolicyMap) DumpKeysToSlice() ([]PolicyKey, error) {
-	var policyKeys []PolicyKey
-
-	cb := func(key bpf.MapKey, value bpf.MapValue) {
-		keyDump := *key.DeepCopyMapKey().(*PolicyKey)
-		policyKeys = append(policyKeys, keyDump)
-	}
-	err := pm.DumpWithCallback(cb)
-
-	return policyKeys, err
 }
 
 func newMap(path string) *PolicyMap {

pkg/maps/policymap/policymap_privileged_test.go
@@ -105,32 +105,6 @@ func (pm *PolicyMapTestSuite) TestPolicyMapDumpToSlice(c *C) {
 	c.Assert(len(dump), Equals, 2)
 }
 
-func (pm *PolicyMapTestSuite) TestPolicyMapDumpKeysToSlice(c *C) {
-	c.Assert(testMap, NotNil)
-
-	fooEntry := newKey(1, 1, 1, 1)
-	err := testMap.AllowKey(fooEntry, 0)
-	c.Assert(err, IsNil)
-
-	dump, err := testMap.DumpKeysToSlice()
-	c.Assert(err, IsNil)
-	c.Assert(len(dump), Equals, 1)
-
-	// FIXME: It's weird that AllowKey() does the implicit byteorder
-	//        conversion above. But not really a bug, so work around it.
-	fooEntry = fooEntry.ToNetwork()
-	c.Assert(dump[0], checker.DeepEquals, fooEntry)
-
-	// Special case: allow-all entry
-	barEntry := newKey(0, 0, 0, 0)
-	err = testMap.AllowKey(barEntry, 0)
-	c.Assert(err, IsNil)
-
-	dump, err = testMap.DumpKeysToSlice()
-	c.Assert(err, IsNil)
-	c.Assert(len(dump), Equals, 2)
-}
-
 func (pm *PolicyMapTestSuite) TestDeleteNonexistentKey(c *C) {
 	key := newKey(27, 80, u8proto.ANY, trafficdirection.Ingress)
 	err := testMap.Map.Delete(&key)
@@ -141,34 +115,6 @@ func (pm *PolicyMapTestSuite) TestDeleteNonexistentKey(c *C) {
 }
 
 func (pm *PolicyMapTestSuite) TestDenyPolicyMapDumpToSlice(c *C) {
-	c.Assert(testMap, NotNil)
-
-	fooEntry := newKey(1, 1, 1, 1)
-	fooValue := newEntry(0, NewPolicyEntryFlag(&PolicyEntryFlagParam{IsDeny: true}))
-	err := testMap.DenyKey(fooEntry)
-	c.Assert(err, IsNil)
-
-	dump, err := testMap.DumpToSlice()
-	c.Assert(err, IsNil)
-	c.Assert(len(dump), Equals, 1)
-
-	// FIXME: It's weird that AllowKey() does the implicit byteorder
-	//        conversion above. But not really a bug, so work around it.
-	fooEntry = fooEntry.ToNetwork()
-	c.Assert(dump[0].Key, checker.DeepEquals, fooEntry)
-	c.Assert(dump[0].PolicyEntry, checker.DeepEquals, fooValue)
-
-	// Special case: deny-all entry
-	barEntry := newKey(0, 0, 0, 0)
-	err = testMap.DenyKey(barEntry)
-	c.Assert(err, IsNil)
-
-	dump, err = testMap.DumpToSlice()
-	c.Assert(err, IsNil)
-	c.Assert(len(dump), Equals, 2)
-}
-
-func (pm *PolicyMapTestSuite) TestDenyPolicyMapDumpKeysToSlice(c *C) {
 	c.Assert(testMap, NotNil)
 
 	fooEntry := newKey(1, 1, 1, 1)

pkg/policy/mapstate.go
@@ -476,9 +476,9 @@ type MapChanges struct {
 }
 
 type MapChange struct {
-	add   bool // false deletes
-	key   Key
-	value MapStateEntry
+	Add   bool // false deletes
+	Key   Key
+	Value MapStateEntry
 }
 
 // AccumulateMapChanges accumulates the given changes to the
@@ -515,11 +515,11 @@ func (mc *MapChanges) AccumulateMapChanges(cs CachedSelector, adds, deletes []id
 	mc.mutex.Lock()
 	for _, id := range adds {
 		key.Identity = id.Uint32()
-		mc.changes = append(mc.changes, MapChange{true, key, value})
+		mc.changes = append(mc.changes, MapChange{Add: true, Key: key, Value: value})
 	}
 	for _, id := range deletes {
 		key.Identity = id.Uint32()
-		mc.changes = append(mc.changes, MapChange{false, key, value})
+		mc.changes = append(mc.changes, MapChange{Add: false, Key: key, Value: value})
 	}
 	mc.mutex.Unlock()
 }
@@ -532,15 +532,15 @@ func (mc *MapChanges) consumeMapChanges(policyMapState MapState) (adds, deletes 
 	deletes = make(MapState, len(mc.changes))
 
 	for i := range mc.changes {
-		if mc.changes[i].add {
+		if mc.changes[i].Add {
 			// insert but do not allow non-redirect entries to overwrite a redirect entry,
 			// nor allow non-deny entries to overwrite deny entries.
 			// Collect the incremental changes to the overall state in 'mc.adds' and 'mc.deletes'.
-			policyMapState.denyPreferredInsertWithChanges(mc.changes[i].key, mc.changes[i].value, adds, deletes)
+			policyMapState.denyPreferredInsertWithChanges(mc.changes[i].Key, mc.changes[i].Value, adds, deletes)
 		} else {
 			// Delete the contribution of this cs to the key and collect incremental changes
-			for cs := range mc.changes[i].value.selectors { // get the sole selector
-				policyMapState.deleteKeyWithChanges(mc.changes[i].key, cs, adds, deletes)
+			for cs := range mc.changes[i].Value.selectors { // get the sole selector
+				policyMapState.deleteKeyWithChanges(mc.changes[i].Key, cs, adds, deletes)
 			}
 		}
 	}

commit f005f930764d204fa8dc999b4799bf1f89b5c98a

endpoint: Only perform full synchronization periodically
Performing a full dump of the policy map on every policy map synchronization
is expensive and is only necessary to catch rare cases where the agent's view
of the policy map has diverged from the kernel's view which should only happen
either due to kernel or other bugs or some other application modifying the
endpoint policy map.

To reduce the cost of synchronizing large endpoint policy maps perform the
full synchronization only periodically. The full synchronization is defaults
to 15 minutes. Configurable with the hidden option
"--policy-map-full-reconciliation-interval".

Fixes: 9dc1350fdb ("endpoint: Enhance policy map sync")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/daemon_main.go
@@ -840,6 +840,10 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.Int(option.PolicyMapEntriesName, policymap.MaxEntries, "Maximum number of entries in endpoint policy map (per endpoint)")
 	option.BindEnv(vp, option.PolicyMapEntriesName)
 
+	flags.Duration(option.PolicyMapFullReconciliationIntervalName, 15*time.Minute, "Interval for full reconciliation of endpoint policy map")
+	option.BindEnv(vp, option.PolicyMapFullReconciliationIntervalName)
+	flags.MarkHidden(option.PolicyMapFullReconciliationIntervalName)
+
 	flags.Int(option.SockRevNatEntriesName, option.SockRevNATMapEntriesDefault, "Maximum number of entries for the SockRevNAT BPF map")
 	option.BindEnv(vp, option.SockRevNatEntriesName)
 

pkg/endpoint/bpf.go
@@ -1277,18 +1277,24 @@ func (e *Endpoint) dumpPolicyMapToMapState() (policy.MapState, error) {
 	return currentMap, err
 }
 
-// syncPolicyMapWithDump attempts to synchronize the PolicyMap for this endpoint to
-// contain the set of PolicyKeys represented by the endpoint's desiredMapState.
-// It checks the current contents of the endpoint's PolicyMap and deletes any
-// PolicyKeys that are not present in the endpoint's desiredMapState. It then
-// adds any keys that are not present in the map. When a key from desiredMapState
-// is inserted successfully to the endpoint's BPF PolicyMap, it is added to the
-// endpoint's realizedMapState field. Returns an error if the endpoint's BPF
-// PolicyMap is unable to be dumped, or any update operation to the map fails.
+// syncPolicyMapWithDump is invoked periodically to perform a full reconciliation
+// of the endpoint's PolicyMap against the BPF maps to catch cases where either
+// due to kernel issue or user intervention the agent's view of the PolicyMap
+// state has diverged from the kernel. A warning is logged if this method finds
+// such an discrepancy.
+//
+// Returns an error if the endpoint's BPF PolicyMap is unable to be dumped,
+// or any update operation to the map fails.
 // Must be called with e.mutex Lock()ed.
 func (e *Endpoint) syncPolicyMapWithDump() error {
 	if e.policyMap == nil {
 		return fmt.Errorf("not syncing PolicyMap state for endpoint because PolicyMap is nil")
+	}
+
+	// Endpoint not yet fully initialized or currently regenerating. Skip the check
+	// this round.
+	if e.getState() != StateReady {
+		return nil
 	}
 
 	// Apply pending policy map changes first so that desired map is up-to-date before
@@ -1339,12 +1345,18 @@ func (e *Endpoint) syncPolicyMapWithDump() error {
 	return err
 }
 
-func (e *Endpoint) syncPolicyMapController() {
+const (
+	// syncPolicyMapInterval is the interval for periodic full reconciliation of
+	// the policy map to catch unexpected discrepancies with agent and kernel state.
+	syncPolicyMapInterval = 15 * time.Minute
+)
+
+func (e *Endpoint) startSyncPolicyMapController() {
 	ctrlName := fmt.Sprintf("sync-policymap-%d", e.ID)
-	e.controllers.UpdateController(ctrlName,
+	e.controllers.CreateController(ctrlName,
 		controller.ControllerParams{
 			Group: syncPolicymapControllerGroup,
-			DoFunc: func(ctx context.Context) (reterr error) {
+			DoFunc: func(ctx context.Context) error {
 				// Failure to lock is not an error, it means
 				// that the endpoint was disconnected and we
 				// should exit gracefully.
@@ -1354,7 +1366,7 @@ func (e *Endpoint) syncPolicyMapController() {
 				defer e.unlock()
 				return e.syncPolicyMapWithDump()
 			},
-			RunInterval: 1 * time.Minute,
+			RunInterval: syncPolicyMapInterval,
 			Context:     e.aliveCtx,
 		},
 	)

pkg/endpoint/policy.go
@@ -526,9 +526,10 @@ func (e *Endpoint) updateRealizedState(stats *regenerationStatistics, origDir st
 		return fmt.Errorf("error synchronizing endpoint BPF program directories: %s", err)
 	}
 
-	// Keep PolicyMap for this endpoint in sync with desired / realized state.
+	// Start periodic background full reconciliation of the policy map.
+	// Does nothing if it has already been started.
 	if !option.Config.DryMode {
-		e.syncPolicyMapController()
+		e.startSyncPolicyMapController()
 	}
 
 	if e.desiredPolicy != e.realizedPolicy {

pkg/option/config.go
@@ -658,6 +658,10 @@ const (
 	// PolicyMapEntriesName configures max entries for BPF policymap.
 	PolicyMapEntriesName = "bpf-policy-map-max"
 
+	// PolicyMapFullReconciliationInterval sets the interval for performing the full
+	// reconciliation of the endpoint policy map.
+	PolicyMapFullReconciliationIntervalName = "bpf-policy-map-full-reconciliation-interval"
+
 	// SockRevNatEntriesName configures max entries for BPF sock reverse nat
 	// entries.
 	SockRevNatEntriesName = "bpf-sock-rev-map-max"
@@ -1581,6 +1585,10 @@ type DaemonConfig struct {
 	// PolicyMapEntries is the maximum number of peer identities that an
 	// endpoint may allow traffic to exchange traffic with.
 	PolicyMapEntries int
+
+	// PolicyMapFullReconciliationInterval is the interval at which to perform
+	// the full reconciliation of the endpoint policy map.
+	PolicyMapFullReconciliationInterval time.Duration
 
 	// SockRevNatEntries is the maximum number of sock rev nat mappings
 	// allowed in the BPF rev nat table
@@ -3869,6 +3877,7 @@ func (c *DaemonConfig) calculateBPFMapSizes(vp *viper.Viper) error {
 	c.NATMapEntriesGlobal = vp.GetInt(NATMapEntriesGlobalName)
 	c.NeighMapEntriesGlobal = vp.GetInt(NeighMapEntriesGlobalName)
 	c.PolicyMapEntries = vp.GetInt(PolicyMapEntriesName)
+	c.PolicyMapFullReconciliationInterval = vp.GetDuration(PolicyMapFullReconciliationIntervalName)
 	c.SockRevNatEntries = vp.GetInt(SockRevNatEntriesName)
 	c.LBMapEntries = vp.GetInt(LBMapEntriesName)
 	c.LBServiceMapEntries = vp.GetInt(LBServiceMapMaxEntries)