cilium/cilium

commit f6ce522d55d549e8ebba75d1c56fa9d6a7c65bfe

FQDN: Added garbage collector functions.
Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

pkg/fqdn/cache.go
@@ -112,19 +112,47 @@ type DNSCache struct {
 	// LastCleanup is the last time that the cleanup happens.
 	lastCleanup time.Time
 
-	// cleanup is the map of TTL expiration time and the entries that are going
-	// to expire will be deleted by CleanupExpiredEntries.
+	// cleanup maps the TTL expiration times (in seconds since the epoch) to
+	// DNS names that expire in that second. On every new insertion where the
+	// new data is actually inserted into the cache (i.e. it expires later than
+	// an existing entry) cleanup will be updated. CleanupExpiredEntries cleans
+	// up these entries on demand.
+	// Note: Lookup functions will not return expired entries, and this is used
+	// to proactively enforce expirations.
+	// Note: It is important to periodically call CleanupExpiredEntries
+	// otherwise this map will grow forever.
 	cleanup map[int64][]string
+
+	// overLimit is a set of DNS names that were over the per-host configured
+	// limit when they received an update. The excess IPs will be removed when
+	// cleanupOverLimitEntries is called, but will continue to be returned by
+	// Lookup until then.
+	// Note: It is important to periodically call GC otherwise this map will
+	// grow forever (it is very bounded, however).
+	overLimit map[string]bool
+
+	// perHostLimit is the number of maximum number of IP per host.
+	perHostLimit int
 }
 
 // NewDNSCache returns an initialized DNSCache
 func NewDNSCache() *DNSCache {
 	c := &DNSCache{
-		forward:     make(map[string]ipEntries),
-		reverse:     make(map[string]nameEntries),
-		lastCleanup: time.Now(),
-		cleanup:     map[int64][]string{},
+		forward:      make(map[string]ipEntries),
+		reverse:      make(map[string]nameEntries),
+		lastCleanup:  time.Now(),
+		cleanup:      map[int64][]string{},
+		overLimit:    map[string]bool{},
+		perHostLimit: 0,
 	}
+	return c
+}
+
+// NewDNSCache returns an initialized DNSCache and set the max host limit to
+// the given argument
+func NewDNSCacheWithLimit(limit int) *DNSCache {
+	c := NewDNSCache()
+	c.perHostLimit = limit
 	return c
 }
 
@@ -166,11 +194,15 @@ func (c *DNSCache) updateWithEntry(entry *cacheEntry) {
 	// When lookupTime is much earlier than time.Now(), we may not expire all
 	// entries that should be expired, leaving more work for .Lookup.
 	c.removeExpired(entries, time.Now(), time.Time{})
+
+	if c.perHostLimit > 0 && len(entries) > c.perHostLimit {
+		c.overLimit[entry.Name] = true
+	}
 }
 
 // AddNameToCleanup adds the IP with the given TTL to the the cleanup map to
 // delete the entry from the policy when it expires.
-// Need to be called with the lock
+// Need to be called with a write lock
 func (c *DNSCache) addNameToCleanup(entry *cacheEntry) {
 	if entry.ExpirationTime.Before(c.lastCleanup) {
 		// ExpirationTime can be before the lastCleanup don't add that value to
@@ -185,16 +217,13 @@ func (c *DNSCache) addNameToCleanup(entry *cacheEntry) {
 	c.cleanup[expiration] = append(expiredEntries, entry.Name)
 }
 
-// CleanupExpiredEntries cleans all the expired entries from the lastTime that
+// cleanupExpiredEntries cleans all the expired entries from the lastTime that
 // runs to the give time. It will lock the struct until retrieves all the data.
 // It returns the list of names that need to be deleted from the policies.
-func (c *DNSCache) CleanupExpiredEntries(expires time.Time) ([]string, time.Time) {
-	c.Lock()
-	defer c.Unlock()
+func (c *DNSCache) cleanupExpiredEntries(expires time.Time) ([]string, time.Time) {
 	timediff := int(expires.Sub(c.lastCleanup).Seconds())
 	startTime := c.lastCleanup
 	expiredEntries := []string{}
-
 	for i := 0; i < int(timediff); i++ {
 		expiredTime := c.lastCleanup.Add(time.Second)
 		key := expiredTime.Unix()
@@ -206,28 +235,80 @@ func (c *DNSCache) CleanupExpiredEntries(expires time.Time) ([]string, time.Time
 		expiredEntries = append(expiredEntries, entries...)
 		delete(c.cleanup, key)
 	}
-	uniqMap := make(map[string]bool)
-	for _, v := range expiredEntries {
-		uniqMap[v] = true
-	}
 
-	uniqExpires := make([]string, 0, len(uniqMap))
-	for v := range uniqMap {
-		uniqExpires = append(uniqExpires, v)
-	}
-	for _, name := range uniqExpires {
+	result := []string{}
+	for _, name := range KeepUniqueNames(expiredEntries) {
 		if entries, exists := c.forward[name]; exists {
-			c.removeExpired(entries, c.lastCleanup, time.Time{})
+			if !c.removeExpired(entries, c.lastCleanup, time.Time{}) {
+				continue
+			}
+			result = append(result, name)
 		}
 	}
-	return uniqExpires, startTime
+	return result, startTime
+}
+
+// cleanupOverLimitEntries returns the names that has reached the max number of
+// IP per host. Internally the function sort the entries by the expiration
+// time.
+func (c *DNSCache) cleanupOverLimitEntries() []string {
+	type IPEntry struct {
+		ip    string
+		entry *cacheEntry
+	}
+	affectedNames := []string{}
+
+	// For global cache the limit maybe is not used at all.
+	if c.perHostLimit == 0 {
+		return affectedNames
+	}
+
+	for dnsName := range c.overLimit {
+		entries, ok := c.forward[dnsName]
+		if !ok {
+			continue
+		}
+		overlimit := len(entries) - c.perHostLimit
+		if overlimit <= 0 {
+			continue
+		}
+		sortedEntries := []IPEntry{}
+		for ip, entry := range entries {
+			sortedEntries = append(sortedEntries, IPEntry{ip, entry})
+		}
+
+		sort.Slice(sortedEntries, func(i, j int) bool {
+			return sortedEntries[i].entry.ExpirationTime.Before(sortedEntries[j].entry.ExpirationTime)
+		})
+
+		for i := 0; i < overlimit; i++ {
+			key := sortedEntries[i]
+			delete(entries, key.ip)
+			c.removeReverse(key.ip, key.entry)
+		}
+		affectedNames = append(affectedNames, dnsName)
+	}
+	c.overLimit = map[string]bool{}
+	return affectedNames
+}
+
+// GC garbage collector function that clean expired entries and return the
+// entries that are deleted.
+func (c *DNSCache) GC() []string {
+	c.Lock()
+	expiredEntries, _ := c.cleanupExpiredEntries(time.Now())
+	overLimitEntries := c.cleanupOverLimitEntries()
+	c.Unlock()
+	return KeepUniqueNames(append(expiredEntries, overLimitEntries...))
 }
 
 // UpdateFromCache is a utility function that allows updating a DNSCache
 // instance with all the internal entries of another. Latest-Expiration still
 // applies, thus the merged outcome is consistent with adding the entries
 // individually.
-func (c *DNSCache) UpdateFromCache(update *DNSCache) {
+// When namesToUpdate has non-zero length only those names are updated from
+// update, otherwise all DNS names in update are used.
+func (c *DNSCache) UpdateFromCache(update *DNSCache, namesToUpdate []string) {
 	if update == nil {
 		return
 	}
@@ -237,8 +318,16 @@ func (c *DNSCache) UpdateFromCache(update *DNSCache) {
 
 	c.Lock()
 	defer c.Unlock()
-
-	for _, newEntries := range update.forward {
+	if len(namesToUpdate) == 0 {
+		for name := range update.forward {
+			namesToUpdate = append(namesToUpdate, name)
+		}
+	}
+	for _, name := range namesToUpdate {
+		newEntries, exists := update.forward[name]
+		if !exists {
+			continue
+		}
 		for _, newEntry := range newEntries {
 			c.updateWithEntry(newEntry)
 		}

pkg/fqdn/cache_test.go
@@ -25,6 +25,7 @@ import (
 	"sort"
 	"time"
 
+	"github.com/cilium/cilium/pkg/checker"
 	. "gopkg.in/check.v1"
 )
 
@@ -465,12 +466,79 @@ func benchmarkUnmarshalJSON(c *C, numDNSEntries int) {
 	}
 }
 
-func (ds *DNSCacheTestSuite) TestCleanupEntries(c *C) {
+func (ds *DNSCacheTestSuite) TestTTLCleanupEntries(c *C) {
 	cache := NewDNSCache()
 	cache.Update(now, "test.com", []net.IP{net.ParseIP("1.2.3.4")}, 3)
 	c.Assert(len(cache.cleanup), Equals, 1)
-	entries, _ := cache.CleanupExpiredEntries(time.Now().Add(5 * time.Second))
-	c.Assert(len(entries), Equals, 1)
-	c.Assert(len(cache.cleanup), Equals, 0)
-	c.Assert(len(cache.forward["test.com"]), Equals, 0)
+	entries, _ := cache.cleanupExpiredEntries(time.Now().Add(5 * time.Second))
+	c.Assert(entries, HasLen, 1)
+	c.Assert(cache.cleanup, HasLen, 0)
+	c.Assert(cache.Lookup("test.com"), HasLen, 0)
+}
+
+func (ds *DNSCacheTestSuite) TestTTLCleanupWithoutForward(c *C) {
+	cache := NewDNSCache()
+	now := time.Now()
+	cache.cleanup[now.Unix()] = []string{"test.com"}
+	// To make sure that all entries are validated correctly
+	cache.lastCleanup = time.Now().Add(-1 * time.Minute)
+	entries, _ := cache.cleanupExpiredEntries(time.Now().Add(5 * time.Second))
+	c.Assert(entries, HasLen, 0)
+	c.Assert(cache.cleanup, HasLen, 0)
+}
+
+func (ds *DNSCacheTestSuite) TestOverlimitEntriesWithValidLimit(c *C) {
+	limit := 5
+	cache := NewDNSCacheWithLimit(limit)
+
+	cache.Update(now, "foo.bar", []net.IP{net.ParseIP("1.1.1.1")}, 1)
+	cache.Update(now, "bar.foo", []net.IP{net.ParseIP("2.1.1.1")}, 1)
+	for i := 1; i < limit+2; i++ {
+		cache.Update(now, "test.com", []net.IP{net.ParseIP(fmt.Sprintf("1.1.1.%d", i))}, i)
+	}
+	c.Assert(cache.cleanupOverLimitEntries(), checker.DeepEquals, []string{"test.com"})
+	c.Assert(cache.Lookup("test.com"), HasLen, limit)
+	c.Assert(cache.LookupIP(net.ParseIP("1.1.1.1")), checker.DeepEquals, []string{"foo.bar"})
+	c.Assert(cache.forward["test.com"]["1.1.1.1"], IsNil)
+	c.Assert(cache.Lookup("foo.bar"), HasLen, 1)
+	c.Assert(cache.Lookup("bar.foo"), HasLen, 1)
+	c.Assert(cache.overLimit, HasLen, 0)
+}
+
+func (ds *DNSCacheTestSuite) TestOverlimitEntriesWithoutLimit(c *C) {
+	limit := 0
+	cache := NewDNSCacheWithLimit(limit)
+	for i := 0; i < 5; i++ {
+		cache.Update(now, "test.com", []net.IP{net.ParseIP(fmt.Sprintf("1.1.1.%d", i))}, i)
+	}
+	c.Assert(cache.cleanupOverLimitEntries(), checker.DeepEquals, []string{})
+	c.Assert(cache.Lookup("test.com"), HasLen, 4)
+}
+
+func (ds *DNSCacheTestSuite) TestGCOverlimitAfterTTLCleanup(c *C) {
+	limit := 5
+	cache := NewDNSCacheWithLimit(limit)
+
+	// Make sure that the cleanup takes all the changes from 1 minute ago.
+	cache.lastCleanup = time.Now().Add(-1 * time.Minute)
+	for i := 1; i < limit+2; i++ {
+		cache.Update(now, "test.com", []net.IP{net.ParseIP(fmt.Sprintf("1.1.1.%d", i))}, 1)
+	}
+
+	c.Assert(cache.Lookup("test.com"), HasLen, limit+1)
+	c.Assert(cache.overLimit, HasLen, 1)
+
+	result, _ := cache.cleanupExpiredEntries(time.Now().Add(5 * time.Second))
+	c.Assert(result, checker.DeepEquals, []string{"test.com"})
+
+	// Due all entries are deleted on TTL, the overlimit should return 0 entries.
+	c.Assert(cache.cleanupOverLimitEntries(), checker.DeepEquals, []string{})
+}
+
+func (ds *DNSCacheTestSuite) TestOverlimitAfterDeleteForwardEntry(c *C) {
+	// Validate if something delete the forward entry no invalid key access on
+	// CG operation
+	dnsCache := NewDNSCache()
+	dnsCache.overLimit["test.com"] = true
+	c.Assert(dnsCache.cleanupOverLimitEntries(), checker.DeepEquals, []string{})
 }

daemon/fqdn.go
@@ -119,14 +119,42 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState) (err err
 	}
 
 	// Controller to cleanup TTL expired entries from the DNS policies.
-	controller.NewManager().UpdateController("dns-ttl-cleanup-job", controller.ControllerParams{
-		RunInterval: time.Second,
+	dnsGCJobName := "dns-garbage-collector-job"
+	controller.NewManager().UpdateController(dnsGCJobName, controller.ControllerParams{
+		RunInterval: 1 * time.Minute,
 		DoFunc: func() error {
-			namesToClean, _ := cfg.Cache.CleanupExpiredEntries(time.Now())
-			d.dnsRuleGen.ForceGenerateDNS(namesToClean)
-			return nil
+
+			namesToClean := []string{}
+			endpoints := endpointmanager.GetEndpoints()
+			for _, ep := range endpoints {
+				namesToClean = append(namesToClean, ep.DNSHistory.GC()...)
+			}
+
+			namesToClean = fqdn.KeepUniqueNames(namesToClean)
+			if len(namesToClean) == 0 {
+				return nil
+			}
+
+			//Before doing the loop the DNS names to clean will be removed from
+			//cfg.Cache, to make sure that data is persistant across cache.
+			namesRegex, err := regexp.Compile("^" + strings.Join(namesToClean, ".|") + ".$")
+			if err != nil {
+				return err
+			}
+			cfg.Cache.ForceExpire(time.Now(), namesRegex)
+
+			// A second loop is needed to update the global cache from the
+			// endpoints cache. Looping this way is generally safe despite not
+			// locking; If a new lookup happens during these updates the new
+			// DNS data will be reinserted from the endpoint.DNSHistory cache
+			// that made the request.
+			for _, ep := range endpoints {
+				cfg.Cache.UpdateFromCache(ep.DNSHistory, namesToClean)
+			}
+			log.WithField(logfields.Controller, dnsGCJobName).Infof(
+				"FQDN garbage collector work deleted %d name entries", len(namesToClean))
+			return d.dnsRuleGen.ForceGenerateDNS(namesToClean)
 		},
-		StopFunc: func() error { return nil },
 	})
 
 	// Prefill the cache with DNS lookups from restored endpoints. This is needed
@@ -136,10 +164,9 @@ func (d *Daemon) bootstrapFQDN(restoredEndpoints *endpointRestoreState) (err err
 	for _, restoredEP := range restoredEndpoints.restored {
 		// Upgrades from old ciliums have this nil
 		if restoredEP.DNSHistory != nil {
-			fqdn.DefaultDNSCache.UpdateFromCache(restoredEP.DNSHistory)
+			fqdn.DefaultDNSCache.UpdateFromCache(restoredEP.DNSHistory, []string{})
 		}
 	}
-
 	// Once we stop returning errors from StartDNSProxy this should live in
 	// StartProxySupport
 	proxy.DefaultDNSProxy, err = dnsproxy.StartDNSProxy("", uint16(option.Config.ToFQDNsProxyPort),
@@ -470,7 +497,7 @@ func deleteDNSLookups(endpoints []*endpoint.Endpoint, expireLookupsBefore time.T
 	namesToRegen = append(namesToRegen, fqdn.DefaultDNSCache.ForceExpire(expireLookupsBefore, nameMatcher)...)
 	for _, ep := range endpoints {
 		namesToRegen = append(namesToRegen, ep.DNSHistory.ForceExpire(expireLookupsBefore, nameMatcher)...)
-		fqdn.DefaultDNSCache.UpdateFromCache(ep.DNSHistory)
+		fqdn.DefaultDNSCache.UpdateFromCache(ep.DNSHistory, nil)
 	}
 
 	return namesToRegen, nil

daemon/daemon_main.go
@@ -667,6 +667,9 @@ func init() {
 	flags.StringVar(&option.Config.FQDNRejectResponse, option.FQDNRejectResponseCode, option.FQDNProxyDenyWithRefused, fmt.Sprintf("DNS response code for rejecting DNS requests, available options are '%v'", option.FQDNRejectOptions))
 	option.BindEnv(option.FQDNRejectResponseCode)
 
+	flags.Int(option.ToFQDNsMaxIPsPerHost, defaults.ToFQDNsMaxIPsPerHost, "Maximum number of IPs to maintain per FQDN name for each endpoint")
+	option.BindEnv(option.ToFQDNsMaxIPsPerHost)
+
 	viper.BindPFlags(flags)
 }
 

pkg/defaults/defaults.go
@@ -94,6 +94,10 @@ const (
 	// This or ToFQDNsMinTTL is used in DaemonConfig.Populate
 	ToFQDNsMinTTLPoller = 3600 // 1 hour in seconds
 
+	// ToFQDNsMaxIPsPerHost defines the maximum number of IPs to maintain
+	// for each FQDN name in an endpoint's FQDN cache
+	ToFQDNsMaxIPsPerHost = 50
+
 	// IdentityChangeGracePeriod is the grace period that needs to pass
 	// before an endpoint that has changed its identity will start using
 	// that new identity. During the grace period, the new identity has

pkg/endpoint/endpoint.go
@@ -415,7 +415,7 @@ func NewEndpointWithState(ID uint16, state string) *Endpoint {
 		Options:       option.NewIntOptions(&EndpointMutableOptionLibrary),
 		OpLabels:      pkgLabels.NewOpLabels(),
 		Status:        NewEndpointStatus(),
-		DNSHistory:    fqdn.NewDNSCache(),
+		DNSHistory:    fqdn.NewDNSCacheWithLimit(option.Config.ToFQDNsMaxIPsPerHost),
 		state:         state,
 		hasBPFProgram: make(chan struct{}, 0),
 		controllers:   controller.NewManager(),
@@ -442,7 +442,7 @@ func NewEndpointFromChangeModel(base *models.EndpointChangeRequest) (*Endpoint, 
 		DatapathMapID:    int(base.DatapathMapID),
 		IfIndex:          int(base.InterfaceIndex),
 		OpLabels:         pkgLabels.NewOpLabels(),
-		DNSHistory:       fqdn.NewDNSCache(),
+		DNSHistory:       fqdn.NewDNSCacheWithLimit(option.Config.ToFQDNsMaxIPsPerHost),
 		state:            "",
 		Status:           NewEndpointStatus(),
 		hasBPFProgram:    make(chan struct{}, 0),
@@ -1191,7 +1191,10 @@ func ParseEndpoint(strEp string) (*Endpoint, error) {
 	if len(strEpSlice) != 2 {
 		return nil, fmt.Errorf("invalid format %q. Should contain a single ':'", strEp)
 	}
-	ep := Endpoint{OpLabels: pkgLabels.NewOpLabels(), DNSHistory: fqdn.NewDNSCache()}
+	ep := Endpoint{
+		OpLabels:   pkgLabels.NewOpLabels(),
+		DNSHistory: fqdn.NewDNSCacheWithLimit(option.Config.ToFQDNsMaxIPsPerHost),
+	}
 	if err := parseBase64ToEndpoint(strEpSlice[1], &ep); err != nil {
 		return nil, fmt.Errorf("failed to parse base64toendpoint: %s", err)
 	}

pkg/fqdn/helpers.go
@@ -165,3 +165,19 @@ func sortedIPsAreEqual(a, b []net.IP) bool {
 func prepareMatchName(matchName string) string {
 	return strings.ToLower(dns.Fqdn(matchName))
 }
+
+// KeepUniqueNames it gets a array of strings and return a new array of strings
+// with the unique names.
+func KeepUniqueNames(names []string) []string {
+	result := []string{}
+	entries := map[string]bool{}
+
+	for _, item := range names {
+		if _, ok := entries[item]; ok {
+			continue
+		}
+		entries[item] = true
+		result = append(result, item)
+	}
+	return result
+}

pkg/fqdn/helpers_test.go
@@ -1,0 +1,39 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package fqdn
+
+import (
+	"github.com/cilium/cilium/pkg/checker"
+	. "gopkg.in/check.v1"
+)
+
+func (ds *DNSCacheTestSuite) TestKeepUniqueNames(c *C) {
+	testData := []struct {
+		argument []string
+		expected []string
+	}{
+		{[]string{"a", "b", "c"}, []string{"a", "b", "c"}},
+		{[]string{"a", "b", "a", "c"}, []string{"a", "b", "c"}},
+		{[]string{""}, []string{""}},
+		{[]string{}, []string{}},
+	}
+
+	for _, item := range testData {
+		val := KeepUniqueNames(item.argument)
+		c.Assert(val, checker.DeepEquals, item.expected)
+	}
+}

commit e972e3de4ff74d29990ebe62fc2c91d43dfbcccb

fqdn: Garbage collect FQDNs left by deleted endpoints
If an FQDN policy applies to multiple pods, but only subset of them
query a specific FQDN then the identities and policy map entries for
that name and IPs may be left behind and not garbage collected until
the remaining pods are either deleted or also query the name.

To reiterate with an example:

  NetworkPolicy:
    toFQDNs:
      matchName: foo.bar

  T0:
    pod1: queries foo.bar => 1.1.1.1
    pod1 policy map: "Allow 1.1.1.1"

    pod2: idles
    pod2 policy map: []

    identities: 1.1.1.1 => 167777217

  T+1: policy map regeneration
    DNS garbage collection:
      namesToClean = []
      for endpoint in [pod1, pod2]:
        namesToClean += "foo.bar"
      ForceGenerateDNS(namesToClean) // ["foo.bar"]

    pod1 policy map: "Allow 1.1.1.1"
    pod2 policy map: "Allow 1.1.1.1"

    identities: 1.1.1.1 => 167777217

  T+2:
    pod1: terminates
    pod2: idles

    pod2 policy map: "Allow 1.1.1.1"
    identities: 1.1.1.1 => 167777217

  T+3:
    DNS garbage collection:
      namesToClean = []
      for endpoint in [pod2]:
        pass, no FQDN entries
      ForceGenerateDNS(namesToClean) // []

    pod2 policy map: "Allow 1.1.1.1"
    identities: 1.1.1.1 => 167777217

Since the DNS garbage collection job only collects from existing endpoints
the name "foo.bar" and identity for 1.1.1.1 are never released.

To fix the issue, the FQDN GC subscribes to endpoint deletions and stores
all FQDN names that have been mentioned in deleted endpoints and will use
that information on the next GC round:

  T+2:
    pod1: terminates
    namesFromDeletedEndpoints += "foo.bar"

  T+3:
    DNS garbage collection:
      namesToClean = namesFromDeletedEndpoints
      namesFromDeletedEndpoints = []
      for endpoint in [pod2]:
        pass, no FQDN entries

      ForceGenerateDNS(namesToClean) // ["foo.bar"]

    pod2 policy map: []
    identities: []

Fixes: f6ce522d55 ("FQDN: Added garbage collector functions.")

daemon/cmd/fqdn.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cilium/cilium/pkg/api"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint"
+	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/fqdn"
 	"github.com/cilium/cilium/pkg/fqdn/dnsproxy"
 	"github.com/cilium/cilium/pkg/fqdn/matchpattern"
@@ -35,6 +36,7 @@ import (
 	"github.com/cilium/cilium/pkg/identity"
 	secIDCache "github.com/cilium/cilium/pkg/identity/cache"
 	ippkg "github.com/cilium/cilium/pkg/ip"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
@@ -158,6 +160,51 @@ func (d *Daemon) updateSelectorCacheFQDNs(ctx context.Context, selectors map[pol
 	return d.endpointManager.UpdatePolicyMaps(ctx, notifyWg)
 }
 
+// deletedEndpointNameCollector collects the FQDN entries from deleted endpoints'
+// DNSCaches which can then be used to GC the FQDN entries in cases where endpoints
+// matching FQDN policies still remain, but do not contact these names.
+type deletedEndpointNameCollector struct {
+	lock.Mutex
+	names map[string]struct{}
+}
+
+// EndpointCreated implements endpointmanager.Subscriber.
+func (*deletedEndpointNameCollector) EndpointCreated(ep *endpoint.Endpoint) {
+}
+
+// EndpointDeleted implements endpointmanager.Subscriber.
+func (dc *deletedEndpointNameCollector) EndpointDeleted(ep *endpoint.Endpoint, conf endpoint.DeleteConfig) {
+	dc.Lock()
+	defer dc.Unlock()
+
+	if dc.names == nil {
+		dc.names = make(map[string]struct{})
+	}
+
+	for _, name := range ep.DNSHistory.Names() {
+		dc.names[name] = struct{}{}
+	}
+
+	for _, name := range ep.DNSZombies.Names() {
+		dc.names[name] = struct{}{}
+	}
+}
+
+func (dc *deletedEndpointNameCollector) gather() (names map[string]struct{}) {
+	dc.Lock()
+	defer dc.Unlock()
+
+	if dc.names != nil {
+		names = dc.names
+		dc.names = nil
+	} else {
+		names = make(map[string]struct{})
+	}
+	return
+}
+
+var _ endpointmanager.Subscriber = &deletedEndpointNameCollector{}
+
 // bootstrapFQDN initializes the toFQDNs related subsystems: dnsNameManager and the DNS proxy.
 // dnsNameManager will use the default resolver and, implicitly, the
 // default DNS cache. The proxy binds to all interfaces, and uses the
@@ -178,6 +225,11 @@ func (d *Daemon) bootstrapFQDN(possibleEndpoints map[uint16]*endpoint.Endpoint, 
 	rg := fqdn.NewNameManager(cfg)
 	d.policy.GetSelectorCache().SetLocalIdentityNotifier(rg)
 	d.dnsNameManager = rg
+
+	// Collect names from deleted endpoints to avoid leaving around FQDNs that
+	// other remaining endpoints refer to but don't contact.
+	var deletedEndpointNameCollector deletedEndpointNameCollector
+	d.endpointManager.Subscribe(&deletedEndpointNameCollector)
 
 	// Controller to cleanup TTL expired entries from the DNS policies.
 	// dns-garbage-collector-job runs the logic to remove stale or undesired
@@ -208,7 +260,8 @@ func (d *Daemon) bootstrapFQDN(possibleEndpoints map[uint16]*endpoint.Endpoint, 
 				activeConnectionsTTL = int(2 * dnsGCJobInterval.Seconds())
 				activeConnections    = fqdn.NewDNSCache(activeConnectionsTTL)
 			)
-			namesToClean := make(map[string]struct{})
+
+			namesToClean := deletedEndpointNameCollector.gather()
 
 			// Cleanup each endpoint cache, deferring deletions via DNSZombies.
 			endpoints := d.endpointManager.GetEndpoints()

pkg/fqdn/cache.go
@@ -595,6 +595,18 @@ func (c *DNSCache) ForceExpire(expireLookupsBefore time.Time, nameMatch *regexp.
 	return slices.Unique(namesAffected)
 }
 
+// Names returns the FQDN entries in the cache
+func (c *DNSCache) Names() (names []string) {
+	c.RLock()
+	defer c.RUnlock()
+
+	names = make([]string, 0, len(c.forward))
+	for name := range c.forward {
+		names = append(names, name)
+	}
+	return names
+}
+
 func (c *DNSCache) forceExpireByNames(expireLookupsBefore time.Time, names []string) (namesAffected []string) {
 	for _, name := range names {
 		entries, exists := c.forward[name]
@@ -768,6 +780,18 @@ func NewDNSZombieMappings(max, perHostLimit int) *DNSZombieMappings {
 		max:          max,
 		perHostLimit: perHostLimit,
 	}
+}
+
+// Names returns the FQDN entries referred to in the DNSZombieMappings
+func (zombies *DNSZombieMappings) Names() []string {
+	zombies.Lock()
+	defer zombies.Unlock()
+
+	names := make([]string, 0, len(zombies.deletes))
+	for _, zombie := range zombies.deletes {
+		names = append(names, zombie.Names...)
+	}
+	return slices.Unique(names)
 }
 
 // Upsert enqueues the ip -> qname as a possible deletion

pkg/fqdn/cache_test.go
@@ -313,6 +313,24 @@ func (ds *DNSCacheTestSuite) TestCountIPs(c *C) {
 	c.Assert(int(ips), Equals, len(names)*2)
 }
 
+func (ds *DNSCacheTestSuite) TestNames(c *C) {
+	nameToAddr := map[string]netip.Addr{
+		"test1.com": netip.MustParseAddr("1.1.1.1"),
+		"test2.com": netip.MustParseAddr("2.2.2.2"),
+		"test3.com": netip.MustParseAddr("3.3.3.3")}
+	sharedIP := netip.MustParseAddr("8.8.8.8")
+	cache := NewDNSCache(0)
+
+	// Insert 3 records all sharing one IP and 1 unique IP.
+	cache.Update(now, "test1.com", []netip.Addr{sharedIP, nameToAddr["test1.com"]}, 5)
+	cache.Update(now, "test2.com", []netip.Addr{sharedIP, nameToAddr["test2.com"]}, 5)
+	cache.Update(now, "test3.com", []netip.Addr{sharedIP, nameToAddr["test3.com"]}, 5)
+
+	names := cache.Names()
+	sort.Strings(names)
+	c.Assert(names, checker.DeepEquals, []string{"test1.com", "test2.com", "test3.com"})
+}
+
 /* Benchmarks
  * These are here to help gauge the relative costs of operations in DNSCache.
  * Note: some are on arrays `size` elements, so the benchmark "op time" is too
@@ -1277,4 +1295,18 @@ func Test_sortZombieMappingSlice(t *testing.T) {
 			validateZombieSort(t, tt.args.zombies)
 		})
 	}
+}
+
+func (ds *DNSCacheTestSuite) TestZombiesNames(c *C) {
+	now := time.Now()
+	zombies := NewDNSZombieMappings(defaults.ToFQDNsMaxDeferredConnectionDeletes, defaults.ToFQDNsMaxIPsPerHost)
+
+	zombies.Upsert(now, netip.MustParseAddr("1.1.1.1"), "test.com")
+	zombies.Upsert(now, netip.MustParseAddr("2.2.2.2"), "example.com")
+	zombies.Upsert(now, netip.MustParseAddr("3.3.3.3"), "example.org")
+	zombies.Upsert(now, netip.MustParseAddr("4.4.4.4"), "example.org")
+
+	names := zombies.Names()
+	sort.Strings(names)
+	c.Assert(names, checker.DeepEquals, []string{"example.com", "example.org", "test.com"})
 }