a2f6a353e18018e4b5aab6b58e105dca7d41abee
Sebastian Wicki <sebastian@isovalent.com>
2023-09-06 13:51:25 +0200

    ipam/multipool: Fix bug where allocator was unable to update CiliumNode
    [ upstream commit 525b1ea80e43c332dd539e90aada2659e33690ac ]
    
    This commit fixes an issue where the multi-pool allocator was unable to
    update a CiliumNode resource because of concurrent writes. This
    manifested in the following error being emitted repeatedly:
    
    ```
    level=debug msg="Controller run failed" consecutiveErrors=48
    error="failed to update spec: Operation cannot be fulfilled on
    ciliumnodes.cilium.io \"kind-worker\": the object has been modified;
    please apply your changes to the latest version and try again"
    name=ipam-multi-pool-sync-kind-worker subsys=controller
    uuid=12ba9a52-d36f-48fe-a7b7-3cf97c2cdb26
    ```
    
    This would happen because the operator CiliumNode watcher does not call
    the `Upsert` function if only the metadata (e.g. resource version,
    labels, annotations, etc) of a node changes. This meant that the
    allocator was working with a stale `resourceVersion` of the CiliumNode
    object, causing any updates to fail until `Upsert` would be called again
    because some non-metadata field changed.
    
    This commit fixes that issue by having the controller fetch the most
    recent version of the `CiliumNode` if the Kubernetes API reports that
    there have been concurrent changes. This behavior matches the behavior
    of the cluster-pool and ENI/Azure/AlibabaCloud implementation, which
    already correctly fetched the resource again upon conflicts.
    
    In addition, this commit also adds a unit test to test this new
    behavior.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- pkg/ipam/allocator/multipool/node_handler.go
+++ pkg/ipam/allocator/multipool/node_handler.go
@@ -9,6 +9,8 @@ import (
 	"fmt"
 	"time"
 
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/ipam/allocator"
@@ -26,7 +28,8 @@ type NodeHandler struct {
 	nodesPendingAllocation map[string]*v2.CiliumNode
 	restoreFinished        bool
 
-	controllerManager *controller.Manager
+	controllerManager                *controller.Manager
+	controllerErrorRetryBaseDuration time.Duration // only set in unit tests
 }
 
 var _ allocator.NodeEventHandler = (*NodeHandler)(nil)
@@ -90,11 +93,23 @@ func (n *NodeHandler) createUpsertController(resource *v2.CiliumNode) {
 	// This controller serves two purposes:
 	// 1. It will retry allocations upon failure, e.g. if a pool does not exist yet.
 	// 2. Will try to synchronize the allocator's state with the CiliumNode CRD in k8s.
+	refetchNode := false
 	n.controllerManager.UpdateController(controllerName(resource.Name), controller.ControllerParams{
+		ErrorRetryBaseDuration: n.controllerErrorRetryBaseDuration,
 		DoFunc: func(ctx context.Context) error {
 			// errorMessage is written to the resource status
 			errorMessage := ""
 			var controllerErr error
+
+			// If a previous run of the controller failed due to a conflict,
+			// we need to re-fetch the node to make sure we have the latest version.
+			if refetchNode {
+				resource, controllerErr = n.nodeUpdater.Get(resource.Name)
+				if controllerErr != nil {
+					return controllerErr
+				}
+				refetchNode = false
+			}
 
 			err := n.poolManager.AllocateToNode(resource)
 			if err != nil {
@@ -113,13 +128,19 @@ func (n *NodeHandler) createUpsertController(resource *v2.CiliumNode) {
 				_, err = n.nodeUpdater.Update(resource, newResource)
 				if err != nil {
 					controllerErr = errors.Join(controllerErr, fmt.Errorf("failed to update spec: %w", err))
+					if k8sErrors.IsConflict(err) {
+						refetchNode = true
+					}
 				}
 			}
 
-			if !newResource.Status.IPAM.OperatorStatus.DeepEqual(&resource.Status.IPAM.OperatorStatus) {
+			if !newResource.Status.IPAM.OperatorStatus.DeepEqual(&resource.Status.IPAM.OperatorStatus) && !refetchNode {
 				_, err = n.nodeUpdater.UpdateStatus(resource, newResource)
 				if err != nil {
 					controllerErr = errors.Join(controllerErr, fmt.Errorf("failed to update status: %w", err))
+					if k8sErrors.IsConflict(err) {
+						refetchNode = true
+					}
 				}
 			}
 
