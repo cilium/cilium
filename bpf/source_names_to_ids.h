2c2c5ae08e7e6eeef106a2800caa71b2c958ea02
Dylan Reimerink <dylan.reimerink@isovalent.com>
2023-09-08 13:59:56 +0200

    bpf: Send ICMP unreachable when no service backends are available
    Before this commit we would always drop packets if we were unable to
    find a backend for a service. This causes a client to retry multiple
    times until a timeout is reached. This is not expected behavior since
    kube-proxy and other CNI's will send an ICMP reply.
    
    If enabled, we can now reply with an ICMP port unreachable message when
    no backend is available for a service. This is implemented with a
    separate tail call since crating the ICMP message can be quite complex.
    
    This functionality is implemented for both IPv4 and IPv6. For IPv4
    traffic the received packet is truncated to 64 bytes and  included in
    the ICMP response. For IPv6 traffic up to 1232 bytes of the original
    message is included in the ICMPv6 response. This matches the behavior
    of kube-proxy.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- bpf/source_names_to_ids.h
+++ bpf/source_names_to_ids.h
@@ -31,6 +31,7 @@ __source_file_name_to_id(const char *const header_name)
 	_strcase_(103, "egress_policies.h");
 	_strcase_(104, "icmp6.h");
 	_strcase_(105, "nodeport.h");
+	_strcase_(106, "lb.h");
 
 	return 0;
 }
