cilium/cilium

commit 5d4504b18b32923cc054bd38bf118ccd859488dc

renovate: automate golangci-lint upgrades
This commit introduces automated upgrades of golangci-lint with
renovate.

Currently it's only activated on the master branch.

Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

Makefile
@@ -420,7 +420,7 @@ ifneq (,$(findstring $(GOLANGCILINT_WANT_VERSION),$(GOLANGCILINT_VERSION)))
 	@$(ECHO_CHECK) golangci-lint $(GOLANGCI_LINT_ARGS)
 	$(QUIET) golangci-lint run $(GOLANGCI_LINT_ARGS)
 else
-	$(QUIET) $(CONTAINER_ENGINE) run --rm -v `pwd`:/app -w /app docker.io/golangci/golangci-lint:v$(GOLANGCILINT_WANT_VERSION)@$(GOLANGCILINT_IMAGE_SHA) golangci-lint run $(GOLANGCI_LINT_ARGS)
+	$(QUIET) $(CONTAINER_ENGINE) run --rm -v `pwd`:/app -w /app docker.io/golangci/golangci-lint:$(GOLANGCILINT_WANT_VERSION)@$(GOLANGCILINT_IMAGE_SHA) golangci-lint run $(GOLANGCI_LINT_ARGS)
 endif
 
 golangci-lint-fix: ## Run golangci-lint to automatically fix warnings

Makefile.defs
@@ -87,7 +87,8 @@ ifneq ($(V),0)
 endif
 endif
 
-GOLANGCILINT_WANT_VERSION = 1.51.2
+# renovate: datasource=docker depName=golangci/golangci-lint
+GOLANGCILINT_WANT_VERSION = v1.51.2
 GOLANGCILINT_IMAGE_SHA = sha256:be42f5df32011553183947c2b2502718bde485f566e15bbd2541f38a077e4d29
 GOLANGCILINT_VERSION = $(shell golangci-lint version 2>/dev/null)
 

.github/renovate.json5
@@ -10,7 +10,8 @@
     "images/**",
     "examples/hubble/*",
     "go.mod",
-    "go.sum"
+    "go.sum",
+    "Makefile.defs"
   ],
   postUpdateOptions: [
     "gomodTidy"
@@ -215,6 +216,18 @@
         "cilium/hubble",
         "quay.io/cilium/hubble"
       ],
+    },
+    {
+      // Do not allow any updates into stable branches.
+      "enabled": false,
+      "matchDepNames": [
+        "golangci/golangci-lint"
+      ],
+      "matchBaseBranches": [
+        "v1.13",
+        "v1.12",
+        "v1.11"
+      ]
     }
   ],
   "kubernetes": {
@@ -248,7 +261,18 @@
       // similar to the examples shown here:
       //   https://docs.renovatebot.com/modules/manager/regex/#advanced-capture
       "matchStrings": [
-        "# renovate: datasource=(?<datasource>.*?) depName=(?<depName>.*?)\\s+.+_version: (?<currentValue>.*)",
+        "# renovate: datasource=(?<datasource>.*?) depName=(?<depName>.*?)\\s+.+version: (?<currentValue>.*)",
+      ]
+    },
+    {
+      "fileMatch": [
+        "^Makefile\\.defs$"
+      ],
+      // This regex manages version strings in the Makefile.defs file,
+      // similar to the examples shown here:
+      //   https://docs.renovatebot.com/modules/manager/regex/#advanced-capture
+      "matchStrings": [
+        "# renovate: datasource=(?<datasource>.*?) depName=(?<depName>.*?)\\s+.+_VERSION = (?<currentValue>.*)\\s+.+_SHA = (?<currentDigest>sha256:[a-f0-9]+)"
       ]
     }
   ]

.github/workflows/lint-go.yaml
@@ -47,6 +47,7 @@ jobs:
       - name: Run golangci-lint
         uses: golangci/golangci-lint-action@08e2f20817b15149a52b5b3ebe7de50aff2ba8c5 # v3.4.0
         with:
+          # renovate: datasource=docker depName=golangci/golangci-lint
           version: v1.51.2
           skip-cache: true
 

commit bbd46f911b046805c6cd16a45118af89a79eb06e

build: fix usage of local golangci-lint installation
The make target `make lint` should prefer the locally installed
`golangci-lint` if the installed version matches the expected one.
This comes with the advantage of using the local cache.

This behaviour is broken since #24664 (automate golangci-lint upgrades
with renovate).

This commit re-introduces the feature.

Fixes: #24664

Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

Makefile
@@ -414,7 +414,7 @@ govet: ## Run govet on Go source files in the repository.
 	$(QUIET) $(GO_VET) ./...
 
 golangci-lint: ## Run golangci-lint
-ifneq (,$(findstring $(GOLANGCILINT_WANT_VERSION),$(GOLANGCILINT_VERSION)))
+ifneq (,$(findstring $(GOLANGCILINT_WANT_VERSION:v%=%),$(GOLANGCILINT_VERSION)))
 	@$(ECHO_CHECK) golangci-lint $(GOLANGCI_LINT_ARGS)
 	$(QUIET) golangci-lint run $(GOLANGCI_LINT_ARGS)
 else

Makefile.defs
@@ -97,7 +97,7 @@ endif
 # renovate: datasource=docker depName=golangci/golangci-lint
 GOLANGCILINT_WANT_VERSION = v1.54.2
 GOLANGCILINT_IMAGE_SHA = sha256:2082f5379c48c46e447bc1b890512f3aa9339db5eeed1a483a34aae9476ba6ee
-GOLANGCILINT_VERSION = $(shell golangci-lint version 2>/dev/null)
+GOLANGCILINT_VERSION = $(shell golangci-lint version --format short 2>/dev/null)
 
 VERSION = $(shell cat $(dir $(lastword $(MAKEFILE_LIST)))/VERSION)
 VERSION_MAJOR = $(shell cat $(dir $(lastword $(MAKEFILE_LIST)))/VERSION | cut -d. -f1)