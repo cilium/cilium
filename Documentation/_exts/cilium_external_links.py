63e523a9438968e8910c79d6496330496c946b4c
Quentin Monnet <quentin@isovalent.com>
2023-09-14 23:51:49 +0100

    docs: Drop is_html5_writer_available() in cilium_external_links.py
    [ upstream commit c36548c184ebbc79779c9493cf00b5fcc9b6384c ]
    
    Function is_html5_writer_available() is deprecated, its invocation is
    unnecessary (the writer _is_ available in the versions of Sphinx we're
    using), and it has been removed in more recent versions of Sphinx. Let's
    get rid of it.
    
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- Documentation/_exts/cilium_external_links.py
+++ Documentation/_exts/cilium_external_links.py
@@ -1,11 +1,7 @@
-from sphinx.writers.html import HTMLTranslator
 from sphinx.writers.html5 import HTML5Translator
-from sphinx.util.docutils import is_html5_writer_available
 
 # Make all external links open in new tabs
-class PatchedHTMLTranslator(
-    HTML5Translator if is_html5_writer_available() else HTMLTranslator
-): 
+class PatchedHTMLTranslator(HTML5Translator):
     def starttag(self, node, tagname, *args, **attrs):
         if (
             tagname == "a"
