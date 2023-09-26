cilium/cilium

commit eaa486d787efd37110c70784c95c52535969f8ea

endpoint: Wait for CT cleanup to complete before BPF compilation
Signed-off-by: Romain Lenglet <romain@covalent.io>

pkg/endpoint/bpf.go
@@ -695,6 +695,11 @@ func (e *Endpoint) regenerateBPF(owner Owner, currentDir, nextDir string, regenC
 
 	e.Unlock()
 
+	// Wait for connection tracking cleaning to complete
+	stats.waitingForCTClean.Start()
+	<-ctCleaned
+	stats.waitingForCTClean.End(true)
+
 	e.getLogger().WithField("bpfHeaderfilesChanged", bpfHeaderfilesChanged).Debug("Preparing to compile BPF")
 
 	stats.prepareBuild.End(true)
@@ -733,11 +738,6 @@ func (e *Endpoint) regenerateBPF(owner Owner, currentDir, nextDir string, regenC
 	if err != nil {
 		return 0, compilationExecuted, fmt.Errorf("Error while configuring proxy redirects: %s", err)
 	}
-
-	// Wait for connection tracking cleaning to be complete
-	stats.waitingForCTClean.Start()
-	<-ctCleaned
-	stats.waitingForCTClean.End(true)
 
 	stats.waitingForLock.Start()
 	err = e.LockAlive()

commit c8f96a40818546d400dacccbc65cf3b7a1f49413

endpoint: correct stats for prepareBuild
Previous change relocated CT cleanup blocker, but left prepareBuild
stats calculated after that. Afterwards, some modifications also been
done for the code related to prebuild. So the stats related to prepareBuild
should be handled within runPreCompilationSteps.

Fixes: eaa486d787ef ("endpoint: Wait for CT cleanup to complete before BPF compilation")

Signed-off-by: Li Chun <chun2.li@intel.com>

pkg/endpoint/bpf.go
@@ -543,8 +543,6 @@ func (e *Endpoint) regenerateBPF(regenContext *regenerationContext) (revnum uint
 	<-datapathRegenCtxt.ctCleaned
 	stats.waitingForCTClean.End(true)
 
-	stats.prepareBuild.End(true)
-
 	compilationExecuted, err = e.realizeBPFState(regenContext)
 	if err != nil {
 		return datapathRegenCtxt.epInfoCache.revision, compilationExecuted, err