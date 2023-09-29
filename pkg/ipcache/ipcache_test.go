5eb57b9a2d3ac6dcb9c8c1dd03fbc95f771ac2e9
Casey Callendrello <cdc@isovalent.com>
2023-09-29 15:05:45 +0200

    ipcache: aggregate labels from all IPs with local host identity
    [ upstream commit d50a525059a332bb2b96450f181a574cdc02119c ]
    
    [ Backporter's notes: minor renames, otherwise a clean backport ]
    
    The `reserved:host` identity is special: the numeric identity is fixed and the
    set of labels is mutable. (The datapath requires this.) So, we need to
    determine all prefixes that have the `reserved:host` label and capture their
    labels. Then, we must aggregate *all* labels from all IPs and insert them as
    the `reserved:host` identity labels.
    
    However, the code as written has a race condition whenever the local node has
    more than one IP address. This can happen when, for example vxlan or ipv6 is
    enabled. The basic sequence is this:
    
    1. Insert IP A as `reserved:host` in to the ipcache. ID 1 now has labels
      `reserved:host`
    2. Insert IP A as `reserved:kube-apiserver` in to the ipcache. ID 1 is updated
      with labels `reserved:host, reserved:kube-apsierver`
    3. Insert IP B as `reserved:host` in to the ipcache. ID 1 is updated with
      labels `reserved:host`. And now policies that select
      `reserved:kube-apiserver` are broken
    
    Likewise, we need to always update the SelectorCache; we cannot short-circuit
    if the ipcache already has that identity. Again, this is needed because the
    identity is mutable. So this bug can take another form:
    
    1. Insert IP A as `reserved:host` in to the ipcache. Because IP A is not known
    to the ipcache, treat ID 1 as a new identity and update the selector cache
    2. Insert IP A as `reserved:kube-apiserver`. Mutate the labels of ID 1. But,
    because IP A already has ID 1, short-circuit the update to the selector cache
    (if the Source is the same, which it _may_ be).
    3. Now the selector cache has incorrect labels for ID 1.
    
    Without this, when there are multiple IPs with the host label, the identity may
    flap and the SelectorCache may be missing updates.
    
    Fixes: #28259
    Fixes: e0d403adc
    Fixes: 308c14225
    
    Signed-off-by: Casey Callendrello <cdc@isovalent.com>

--- pkg/ipcache/ipcache_test.go
+++ pkg/ipcache/ipcache_test.go
@@ -20,6 +20,7 @@ import (
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	identityPkg "github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipcache/types/fake"
+	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/source"
 	testidentity "github.com/cilium/cilium/pkg/testutils/identity"
 	"github.com/cilium/cilium/pkg/types"
@@ -34,6 +35,7 @@ type IPCacheTestSuite struct {
 var (
 	_               = Suite(&IPCacheTestSuite{})
 	IPIdentityCache *IPCache
+	PolicyHandler   *mockUpdater
 )
 
 func Test(t *testing.T) {
@@ -43,10 +45,13 @@ func Test(t *testing.T) {
 func (s *IPCacheTestSuite) SetUpTest(c *C) {
 	ctx, cancel := context.WithCancel(context.Background())
 	allocator := testidentity.NewMockIdentityAllocator(nil)
+	PolicyHandler = &mockUpdater{
+		identities: make(map[identityPkg.NumericIdentity]labels.LabelArray),
+	}
 	IPIdentityCache = NewIPCache(&Configuration{
 		Context:           ctx,
 		IdentityAllocator: allocator,
-		PolicyHandler:     &mockUpdater{},
+		PolicyHandler:     PolicyHandler,
 		DatapathHandler:   &mockTriggerer{},
 		NodeIDHandler:     &fake.FakeNodeIDHandler{},
 	})
