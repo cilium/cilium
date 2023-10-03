cilium/cilium

commit 5f88bbffeb50a3de415549a3a9d50e34453ccd34

docs: Add Makefile and documentation for "fast" development targets
This commit introduces documentation for "fast" development targets in
Cilium. The "fast" targets allow for quicker development workflows by
building Cilium locally and deploying it on an existing kind cluster.
This approach eliminates the need to create and deploy Docker images
each time a source code change is made.

Additionally, the documentation has been updated to include these new
Makefile targets and provides a quick start guide with essential
information for working with Cilium.

When a cluster is already running with a Cilium container, we can
simply volume mount the binaries into the container and restart the
pod to pick up the changes. A similar approach can be used for C code,
with the caveat that the bpf/lib directory needs write permissions by
Cilium to generate the C Header features file, making it impossible
to use the volume as read-only. Since all nodes share the same kernel
features, there won't be any conflicts when multiple nodes write
to the same volume mount.

Signed-off-by: André Martins <andre@cilium.io>

Makefile
@@ -563,6 +563,61 @@ kind-image: ## Build cilium and operator images and import them into kind.
 	$(MAKE) kind-image-agent
 	$(MAKE) kind-image-operator
 
+.PHONY: kind-install-cilium-fast
+kind-install-cilium-fast: kind-ready ## Install a local Cilium version into the cluster.
+	@echo "  INSTALL cilium"
+	# cilium-cli doesn't support idempotent installs, so we uninstall and
+	# reinstall here. https://github.com/cilium/cilium-cli/issues/205
+	-$(CILIUM_CLI) uninstall >/dev/null
+	# cilium-cli's --wait flag doesn't work, so we just force it to run
+	# in the background instead and wait for the resources to be available.
+	# https://github.com/cilium/cilium-cli/issues/1070
+	$(CILIUM_CLI) install \
+		--chart-directory=$(ROOT_DIR)/install/kubernetes/cilium \
+		--helm-values=$(ROOT_DIR)/contrib/testing/kind-common.yaml \
+		--helm-values=$(ROOT_DIR)/contrib/testing/kind-fast.yaml \
+		--version= \
+		>/dev/null 2>&1 &
+
+.PHONY: kind-image-fast
+kind-image-fast: ## Build cilium, operator, clustermesh-apiserver and mount them into a volume for all available Kind clusters.
+	$(eval dst:=/cilium-binaries)
+	$(MAKE) -C cilium
+	$(MAKE) -C daemon
+	$(MAKE) -C operator cilium-operator-generic
+	$(MAKE) -C clustermesh-apiserver
+
+	for cluster_name in $${KIND_CLUSTERS:-$(shell kind get clusters)}; do \
+	    for node_name in $(shell kind get nodes -n "${cluster_name}"); do \
+			docker exec -ti $${node_name} mkdir -p "${dst}"; \
+			\
+			docker exec -ti $${node_name} rm -rf "${dst}/var/lib/cilium"; \
+			docker exec -ti $${node_name} mkdir -p "${dst}/var/lib/cilium"; \
+			docker cp "./bpf/" $${node_name}:"${dst}/var/lib/cilium/bpf"; \
+			docker exec -ti $${node_name} find "${dst}/var/lib/cilium/bpf" -type f -exec chmod 0644 {} + ;\
+			\
+			docker exec -ti $${node_name} rm -f "${dst}/cilium"; \
+			docker cp "./cilium/cilium" $${node_name}:"${dst}"; \
+			docker exec -ti $${node_name} chmod +x "${dst}/cilium"; \
+			\
+			docker exec -ti $${node_name} rm -f "${dst}/cilium-agent"; \
+			docker cp "./daemon/cilium-agent" $${node_name}:"${dst}"; \
+			docker exec -ti $${node_name} chmod +x "${dst}/cilium-agent"; \
+			\
+			docker exec -ti $${node_name} rm -f "${dst}/cilium-operator-generic"; \
+			docker cp "./operator/cilium-operator-generic" $${node_name}:"${dst}"; \
+			docker exec -ti $${node_name} chmod +x "${dst}/cilium-operator-generic"; \
+			\
+			docker exec -ti $${node_name} rm -f "${dst}/clustermesh-apiserver"; \
+			docker cp "./clustermesh-apiserver/clustermesh-apiserver" $${node_name}:"${dst}"; \
+			docker exec -ti $${node_name} chmod +x "${dst}/clustermesh-apiserver"; \
+			\
+			kubectl --context=kind-$${cluster_name} delete pods -n kube-system -l k8s-app=cilium --force; \
+			kubectl --context=kind-$${cluster_name} delete pods -n kube-system -l name=cilium-operator --force; \
+			kubectl --context=kind-$${cluster_name} delete pods -n kube-system -l k8s-app=clustermesh-apiserver --force; \
+		done; \
+	done
+
 .PHONY: kind-install-cilium
 kind-install-cilium: kind-ready ## Install a local Cilium version into the cluster.
 	@echo "  INSTALL cilium"
@@ -574,6 +629,7 @@ kind-install-cilium: kind-ready ## Install a local Cilium version into the clust
 	# https://github.com/cilium/cilium-cli/issues/1070
 	$(CILIUM_CLI) install \
 		--chart-directory=$(ROOT_DIR)/install/kubernetes/cilium \
+		--helm-values=$(ROOT_DIR)/contrib/testing/kind-common.yaml \
 		--helm-values=$(ROOT_DIR)/contrib/testing/kind-values.yaml \
 		--version= \
 		>/dev/null 2>&1 &

contrib/testing/kind-common.yaml
@@ -1,0 +1,20 @@
+debug:
+  enabled: true
+operator:
+  nodeSelector:
+    kubernetes.io/os: linux
+    kubernetes.io/hostname: kind-worker
+ipam:
+  mode: kubernetes
+ipv6:
+  enabled: true
+ipv4:
+  enabled: true
+bpf:
+  monitorAggregation: none
+livenessProbe:
+  failureThreshold: 9999
+readinessProbe:
+  failureThreshold: 9999
+startupProbe:
+  failureThreshold: 9999

contrib/testing/kind-fast.yaml
@@ -1,0 +1,43 @@
+extraVolumes:
+  - name: cilium-cli-binary
+    hostPath:
+      path: /cilium-binaries/cilium
+      type: File
+  - name: cilium-agent-binary
+    hostPath:
+      path: /cilium-binaries/cilium-agent
+      type: File
+  - name: cilium-c-files
+    hostPath:
+      path: /cilium-binaries/var/lib/cilium/bpf
+      type: Directory
+extraVolumeMounts:
+  - name: cilium-cli-binary
+    mountPath: /usr/bin/cilium
+    readOnly: true
+  - name: cilium-agent-binary
+    mountPath: /usr/bin/cilium-agent
+    readOnly: true
+  - name: cilium-c-files
+    mountPath: /var/lib/cilium/bpf
+operator:
+  extraVolumeMounts:
+  - mountPath: /usr/bin/cilium-operator-generic
+    name: cilium-operator-binary
+    readOnly: true
+  extraVolumes:
+  - hostPath:
+      path: /cilium-binaries/cilium-operator-generic
+      type: File
+    name: cilium-operator-binary
+clustermesh:
+  apiserver:
+    extraVolumeMounts:
+      - mountPath: /usr/bin/clustermesh-apiserver
+        name: clustermesh-apiserver-binary
+        readOnly: true
+    extraVolumes:
+      - hostPath:
+          path: /cilium-binaries/clustermesh-apiserver
+          type: File
+        name: clustermesh-apiserver-binary

contrib/testing/kind-values.yaml
@@ -1,5 +1,3 @@
-debug:
-  enabled: true
 image:
   override: "localhost:5000/cilium/cilium-dev:local"
   pullPolicy: Never
@@ -8,20 +6,3 @@ operator:
     override: "localhost:5000/cilium/operator-generic:local"
     pullPolicy: Never
     suffix: ""
-  nodeSelector:
-    kubernetes.io/os: linux
-    kubernetes.io/hostname: kind-worker
-ipam:
-  mode: kubernetes
-ipv6:
-  enabled: true
-ipv4:
-  enabled: true
-bpf:
-  monitorAggregation: none
-livenessProbe:
-  failureThreshold: 9999
-readinessProbe:
-  failureThreshold: 9999
-startupProbe:
-  failureThreshold: 9999

commit e9c10b8b37df751ab4ca973a07a326fbf0eced84

Makefile fix kind-install-cilium-fast target
The kind-install-cilium-fast should install Cilium on all available
kind clusters.

Fixes: 5f88bbffeb50 ("docs: Add Makefile and documentation for "fast" development targets")
Signed-off-by: André Martins <andre@cilium.io>

Makefile
@@ -575,18 +575,13 @@ endif
 .PHONY: kind-install-cilium-fast
 kind-install-cilium-fast: kind-ready ## Install a local Cilium version into the cluster.
 	@echo "  INSTALL cilium"
-	# cilium-cli doesn't support idempotent installs, so we uninstall and
-	# reinstall here. https://github.com/cilium/cilium-cli/issues/205
-	-@$(CILIUM_CLI) uninstall >/dev/null 2>&1 || true
-
-	# cilium-cli's --wait flag doesn't work, so we just force it to run
-	# in the background instead and wait for the resources to be available.
-	# https://github.com/cilium/cilium-cli/issues/1070
-	$(CILIUM_CLI) install \
-		--chart-directory=$(ROOT_DIR)/install/kubernetes/cilium \
-		$(KIND_VALUES_FAST_FILES) \
-		--version= \
-		>/dev/null 2>&1 &
+	for cluster_name in $${KIND_CLUSTERS:-$(shell kind get clusters)}; do \
+		$(CILIUM_CLI) --context=kind-$$cluster_name uninstall >/dev/null 2>&1 || true; \
+		$(CILIUM_CLI) install --context=kind-$$cluster_name \
+			--chart-directory=$(ROOT_DIR)/install/kubernetes/cilium \
+			$(KIND_VALUES_FAST_FILES) \
+			--version= >/dev/null 2>&1 & \
+	done
 
 .PHONY: build-cli
 build-cli: ## Build cilium cli binary