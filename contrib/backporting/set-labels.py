cilium/cilium

commit 2966b0346974dde439e19cc70c6b9ad92031a92f

backporting: Update docs after introduction of Label Updater
Update the documentation and the backporting scripts to take into
account the new Label Updater workflow that will automatically update
the labels of each backported Pr.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

contrib/backporting/set-labels.py
@@ -19,15 +19,12 @@ except ImportError:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pr_number', type=int)
-actions = ["pending", "done"]
-parser.add_argument('action', type=str, choices=actions)
 parser.add_argument('version', type=str, default="1.0", nargs='?')
 
 args = parser.parse_args()
 
 token = os.environ["GITHUB_TOKEN"]
 pr_number = args.pr_number
-action = args.action
 version = args.version
 
 g = Github(token)
@@ -39,34 +36,18 @@ old_label_len = len(pr_labels)
 cilium_labels = cilium.get_labels()
 
 print("Setting labels for PR {}... ".format(pr_number), end="")
-if action == "pending":
-    pr_labels = [l for l in pr_labels
-                 if l.name != "needs-backport/"+version]
-    if old_label_len - 1 != len(pr_labels):
-        print("needs-backport/"+version+" label not found in PR, exiting")
-        sys.exit(1)
+pr_labels = [l for l in pr_labels
+                if l.name != "needs-backport/"+version]
+if old_label_len - 1 != len(pr_labels):
+    print("needs-backport/"+version+" label not found in PR, exiting")
+    sys.exit(1)
 
-    pr_labels.append(
-        [l for l in cilium_labels if l.name == "backport-pending/"+version][0])
+pr_labels.append(
+    [l for l in cilium_labels if l.name == "backport-pending/"+version][0])
 
-    if old_label_len != len(pr_labels):
-        print("error adding backport-pending/"+version+" label to PR, exiting")
-        sys.exit(2)
-    pr.set_labels(*pr_labels)
-
-if action == "done":
-    pr_labels = [l for l in pr_labels
-                 if l.name != "backport-pending/"+version]
-    if old_label_len - 1 != len(pr_labels):
-        print("backport-pending/"+version+" label not found in PR, exiting")
-        sys.exit(1)
-
-    pr_labels.append(
-        [l for l in cilium_labels if l.name == "backport-done/"+version][0])
-
-    if old_label_len != len(pr_labels):
-        print("error adding backport-done/"+version+" label to PR, exiting")
-        sys.exit(2)
-    pr.set_labels(*pr_labels)
+if old_label_len != len(pr_labels):
+    print("error adding backport-pending/"+version+" label to PR, exiting")
+    sys.exit(2)
+pr.set_labels(*pr_labels)
 
 print("✓")

contrib/backporting/check-stable
@@ -206,12 +206,11 @@ if [[ ${#stable_prs[@]} > 0 ]]; then
     generate_commit_list_for_pr $pr $merged_at
     echo ""
   done
-  echo "When you have backported the above commits, you can update the PR labels via this command:"
-  echo "$ for pr in ${stable_prs[@]}; do contrib/backporting/set-labels.py \$pr pending ${STABLE_BRANCH}; done"
+  echo "When you have backported the above commits, a GitHub action will automatically update the PR labels"
   if [[ ! -z $SUMMARY_LOG ]]; then
-    echo -e "\nOnce this PR is merged, you can update the PR labels via:" >> $SUMMARY_LOG
+    echo -e "\nOnce this PR is merged, a GitHub action will update the labels of these PRs:" >> $SUMMARY_LOG
     echo "\`\`\`upstream-prs" >> $SUMMARY_LOG
-    echo "$ for pr in ${stable_prs[@]}; do contrib/backporting/set-labels.py \$pr done ${STABLE_BRANCH}; done" >> $SUMMARY_LOG
+    echo "${stable_prs[@]}" >> $SUMMARY_LOG
     echo "\`\`\`" >> $SUMMARY_LOG
   fi
 fi

contrib/backporting/submit-backport
@@ -59,12 +59,12 @@ else
   hub pull-request -b "v$BRANCH" -l kind/backports,backport/$BRANCH -F $SUMMARY -r $REVIEWERS
 fi
 
-prs=$(grep "contrib/backporting/set-labels.py" $SUMMARY | sed -e 's/^.*for pr in \([0-9 ]\+\);.*$/\1/g')
+prs=$(sed -En "/upstream-prs/ { n; p }" < "$SUMMARY")
 echo -e "\nUpdating labels for PRs $prs\n" 2>&1
 echo -n "Set labels for all PRs above? [Y/n] "
 read set_all_labels
 if [ "$set_all_labels" != "n" ]; then
     for pr in $prs; do
-        $DIR/set-labels.py $pr pending $BRANCH;
+        $DIR/set-labels.py $pr $BRANCH;
     done
 fi

commit 1ccb25ca2a2ce1ecf830401527b7b6289f1f927f

backporting: Revert changes until the new workflow will be in place
The new GH workflow "Update labels of backported PRs", called from each
stable branch, updates each `backport-pending` PRs to mark them as
`backport-done` when the related backport is merged. So, there should be
no need to use the contrib/backporting/set-labels.py to do that anymore.

However, there are still some in-flight backport PRs that rely on the
script to do that manually.  Thus, this commit reverts the changes in
2966b03469 to restore the old script code.

Once all the new GH workflows will be in place for the stable branches
and the backport PRs will be created following the new format documented
in the backporting docs, the script may be updated.

Fixes: 2966b03469 ("backporting: Update docs after introduction of Label Updater")

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

contrib/backporting/set-labels.py
@@ -19,12 +19,15 @@ except ImportError:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pr_number', type=int)
+actions = ["pending", "done"]
+parser.add_argument('action', type=str, choices=actions)
 parser.add_argument('version', type=str, default="1.0", nargs='?')
 
 args = parser.parse_args()
 
 token = os.environ["GITHUB_TOKEN"]
 pr_number = args.pr_number
+action = args.action
 version = args.version
 
 g = Github(token)
@@ -35,19 +38,47 @@ old_label_len = len(pr_labels)
 
 cilium_labels = cilium.get_labels()
 
+# After the introduction of the "Update labels of backported PRs" GH
+# workflow and all the workflows in the stable branches that calls it,
+# there is no need to use this script to update the backported PRs label.
+# Specifically, this script shouldn't be used with the `action` parameter
+# set to `done` anymore.
+# However, since there might still be in-flight backporting PRs relying on
+# the old backporting workflow (based on this script to update the labels),
+# we leave the code here.
+#
+# This can be updated once all the new workflows will be in place and the
+# "old" backport PRs will have their labels updated.
+
 print("Setting labels for PR {}... ".format(pr_number), end="")
-pr_labels = [l for l in pr_labels
-                if l.name != "needs-backport/"+version]
-if old_label_len - 1 != len(pr_labels):
-    print("needs-backport/"+version+" label not found in PR, exiting")
-    sys.exit(1)
+if action == "pending":
+    pr_labels = [l for l in pr_labels
+                 if l.name != "needs-backport/"+version]
+    if old_label_len - 1 != len(pr_labels):
+        print("needs-backport/"+version+" label not found in PR, exiting")
+        sys.exit(1)
 
-pr_labels.append(
-    [l for l in cilium_labels if l.name == "backport-pending/"+version][0])
+    pr_labels.append(
+        [l for l in cilium_labels if l.name == "backport-pending/"+version][0])
 
-if old_label_len != len(pr_labels):
-    print("error adding backport-pending/"+version+" label to PR, exiting")
-    sys.exit(2)
-pr.set_labels(*pr_labels)
+    if old_label_len != len(pr_labels):
+        print("error adding backport-pending/"+version+" label to PR, exiting")
+        sys.exit(2)
+    pr.set_labels(*pr_labels)
+
+if action == "done":
+    pr_labels = [l for l in pr_labels
+                 if l.name != "backport-pending/"+version]
+    if old_label_len - 1 != len(pr_labels):
+        print("backport-pending/"+version+" label not found in PR, exiting")
+        sys.exit(1)
+
+    pr_labels.append(
+        [l for l in cilium_labels if l.name == "backport-done/"+version][0])
+
+    if old_label_len != len(pr_labels):
+        print("error adding backport-done/"+version+" label to PR, exiting")
+        sys.exit(2)
+    pr.set_labels(*pr_labels)
 
 print("✓")