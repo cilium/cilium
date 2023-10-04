9f92b88d014600affbd2e2eb0b5d31fb1e210a4f
Paul Chaignon <paul.chaignon@gmail.com>
2023-10-04 12:24:03 +0200

    common/ipsec: Helper functions to count XFRM configs
    [ upstream commit ac529da64b1ebb81cde94067aeb7c196655a0a91 ]
    
    Provide two new helper functions to count the number of XFRM policies
    and states on the node, broken down by directions. XFRM policies can
    have directions in, out, and fwd, whereas XFRM states can only be in or
    out. For XFRM states, the direction is not a built-in parameter, but
    something Cilium encodes in the packet mark.
    
    These helper functions will be used in the subsequent commit to expose
    new metrics.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- pkg/common/ipsec/utils.go
+++ pkg/common/ipsec/utils.go
@@ -7,6 +7,12 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
+const (
+	maskStateDir = 0xf00
+	markStateIn  = 0xd00
+	markStateOut = 0xe00
+)
+
 func CountUniqueIPsecKeys(states []netlink.XfrmState) int {
 	keys := make(map[string]bool)
 	for _, s := range states {
@@ -17,4 +23,38 @@ func CountUniqueIPsecKeys(states []netlink.XfrmState) int {
 	}
 
 	return len(keys)
+}
+
+func CountXfrmStatesByDir(states []netlink.XfrmState) (int, int) {
+	nbXfrmIn := 0
+	nbXfrmOut := 0
+	for _, s := range states {
+		if s.Mark == nil {
+			continue
+		}
+		switch s.Mark.Value & maskStateDir {
+		case markStateIn:
+			nbXfrmIn++
+		case markStateOut:
+			nbXfrmOut++
+		}
+	}
+	return nbXfrmIn, nbXfrmOut
+}
+
+func CountXfrmPoliciesByDir(states []netlink.XfrmPolicy) (int, int, int) {
+	nbXfrmIn := 0
+	nbXfrmOut := 0
+	nbXfrmFwd := 0
+	for _, p := range states {
+		switch p.Dir {
+		case netlink.XFRM_DIR_IN:
+			nbXfrmIn++
+		case netlink.XFRM_DIR_OUT:
+			nbXfrmOut++
+		case netlink.XFRM_DIR_FWD:
+			nbXfrmFwd++
+		}
+	}
+	return nbXfrmIn, nbXfrmOut, nbXfrmFwd
 }
