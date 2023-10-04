6a5db330880b20bfc9b4508676493aa42d4df22f
Paul Chaignon <paul.chaignon@gmail.com>
2023-10-04 12:24:03 +0200

    common/ipsec: Helper functions to count XFRM configs
    Provide two new helper functions to count the number of XFRM policies
    and states on the node, broken down by directions. XFRM policies can
    have directions in, out, and fwd, whereas XFRM states can only be in or
    out. For XFRM states, the direction is not a built-in parameter, but
    something Cilium encodes in the packet mark.
    
    These helper functions will be used in the subsequent commit to expose
    new metrics.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- pkg/common/ipsec/utils_test.go
+++ pkg/common/ipsec/utils_test.go
@@ -12,8 +12,6 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-type IPsecUtilsSuite struct{}
-
 func getXfrmState(src string, dst string, spi int, key string, mark uint32) netlink.XfrmState {
 	k, _ := hex.DecodeString(key)
 	return netlink.XfrmState{
@@ -27,6 +25,23 @@ func getXfrmState(src string, dst string, spi int, key string, mark uint32) netl
 			Key:    k,
 			ICVLen: 64,
 		},
+		Mark: &netlink.XfrmMark{
+			Value: mark,
+			Mask:  0xffffff00,
+		},
+	}
+}
+
+func getXfrmPolicy(t *testing.T, src string, dst string, dir netlink.Dir) netlink.XfrmPolicy {
+	srcCIDR, err := netlink.ParseIPNet(src)
+	require.NoError(t, err)
+	dstCIDR, err := netlink.ParseIPNet(dst)
+	require.NoError(t, err)
+	return netlink.XfrmPolicy{
+		Dir:   dir,
+		Src:   srcCIDR,
+		Dst:   dstCIDR,
+		Proto: netlink.XFRM_PROTO_ESP,
 	}
 }
 
@@ -46,4 +61,39 @@ func TestCountUniqueIPsecKeys(t *testing.T) {
 
 	keys = CountUniqueIPsecKeys(xfrmStates)
 	require.Equal(t, keys, 2)
+}
+
+func TestCountXfrmStatesByDir(t *testing.T) {
+	var xfrmStates []netlink.XfrmState
+
+	nbIn, nbOut := CountXfrmStatesByDir(xfrmStates)
+	require.Equal(t, nbIn, 0)
+	require.Equal(t, nbOut, 0)
+
+	xfrmStates = append(xfrmStates, getXfrmState("10.0.0.1", "10.0.0.2", 2, "611d0c8049dd88600ec4f9eded7b1ed540ea607f", 0x12343e00))
+	xfrmStates = append(xfrmStates, getXfrmState("10.0.0.2", "10.0.0.1", 1, "611d0c8049dd88600ec4f9eded7b1ed540ea607f", 0x12343d00))
+	xfrmStates = append(xfrmStates, getXfrmState("10.0.0.3", "10.0.0.1", 1, "611d0c8049dd88600ec4f9eded7b1ed540ea607f", 0x12343d00))
+
+	nbIn, nbOut = CountXfrmStatesByDir(xfrmStates)
+	require.Equal(t, nbIn, 2)
+	require.Equal(t, nbOut, 1)
+}
+
+func TestCountXfrmPoliciesByDir(t *testing.T) {
+	var xfrmPolicies []netlink.XfrmPolicy
+
+	nbIn, nbOut, nbFwd := CountXfrmPoliciesByDir(xfrmPolicies)
+	require.Equal(t, nbIn, 0)
+	require.Equal(t, nbOut, 0)
+	require.Equal(t, nbFwd, 0)
+
+	xfrmPolicies = append(xfrmPolicies, getXfrmPolicy(t, "10.0.1.0/24", "10.0.0.0/24", netlink.XFRM_DIR_IN))
+	xfrmPolicies = append(xfrmPolicies, getXfrmPolicy(t, "10.0.0.0/24", "10.0.1.0/24", netlink.XFRM_DIR_OUT))
+	xfrmPolicies = append(xfrmPolicies, getXfrmPolicy(t, "10.0.0.0/24", "10.0.2.0/24", netlink.XFRM_DIR_OUT))
+	xfrmPolicies = append(xfrmPolicies, getXfrmPolicy(t, "10.0.0.0/16", "10.0.0.0/16", netlink.XFRM_DIR_FWD))
+
+	nbIn, nbOut, nbFwd = CountXfrmPoliciesByDir(xfrmPolicies)
+	require.Equal(t, nbIn, 1)
+	require.Equal(t, nbOut, 2)
+	require.Equal(t, nbFwd, 1)
 }
