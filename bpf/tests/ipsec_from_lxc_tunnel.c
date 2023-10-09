a1be8f4169bb4255c63e30ce8375915a5f89a1c6
Gray Liang <gray.liang@isovalent.com>
2023-10-09 11:31:39 +0800

    Split ipsec_from_lxc_test.c into *_generic.h and *_tunnel.c
    The original ipsec_from_lxc_test.c covers tunnel=vxlan plus
    endpointRoutes=disabled only. This commit splits it into generic part
    (*_generic.h) and configure-specific part (*_tunnel.c).
    
    Signed-off-by: Zhichuan Liang <gray.liang@isovalent.com>

---
+++ bpf/tests/ipsec_from_lxc_tunnel.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#define TUNNEL_MODE
+#define HAVE_ENCAP
+#define ENABLE_ROUTING
+
+#include "ipsec_from_lxc_generic.h"
