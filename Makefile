cilium/cilium

commit 4deff3cd65c51abb34f9c680859b290713375bd9

cilium: Rename local CLI to cilium-dbg
Signed-off-by: Joe Stringer <joe@cilium.io>

Makefile
@@ -12,7 +12,7 @@ debug: all
 
 include Makefile.defs
 
-SUBDIRS_CILIUM_CONTAINER := cilium daemon cilium-health bugtool tools/mount tools/sysctlfix
+SUBDIRS_CILIUM_CONTAINER := cilium-dbg daemon cilium-health bugtool tools/mount tools/sysctlfix
 SUBDIR_OPERATOR_CONTAINER := operator
 
 # Add the ability to override variables

contrib/testing/kind-fast.yaml
@@ -12,8 +12,8 @@ extraVolumes:
       path: /cilium-binaries/var/lib/cilium/bpf
       type: Directory
 extraVolumeMounts:
-  - name: cilium-cli-binary
-    mountPath: /usr/bin/cilium
+  - name: cilium-dbg-binary
+    mountPath: /usr/bin/cilium-dbg
     readOnly: true
   - name: cilium-agent-binary
     mountPath: /usr/bin/cilium-agent

cilium-dbg/Makefile
@@ -5,7 +5,8 @@ ROOT_DIR := $(shell dirname "$(realpath $(lastword $(MAKEFILE_LIST)))")
 
 include ${ROOT_DIR}/../Makefile.defs
 
-TARGET := cilium
+TARGET := cilium-dbg
+TARGET_OLD := cilium
 
 .PHONY: all $(TARGET) clean install
 
@@ -25,6 +26,7 @@ install: install-binary install-bash-completion-only
 install-binary:
 	$(QUIET)$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)
 	$(QUIET)$(INSTALL) -m 0755 $(TARGET) $(DESTDIR)$(BINDIR)
+	$(QUIET)ln -rs $(DESTDIR)$(BINDIR)/$(TARGET) $(DESTDIR)$(BINDIR)/$(TARGET_OLD)
 
 install-bash-completion: $(TARGET) install-bash-completion-only
 

commit 424142f8217b7280c6fe6505d367f7834bca0856

makefile: fix 'fast' targets for cilium-dbg
Fixes: 4deff3cd65c5 ("cilium: Rename local CLI to cilium-dbg")
Signed-off-by: André Martins <andre@cilium.io>

Makefile
@@ -622,7 +622,7 @@ kind-install-cilium-fast: kind-ready ## Install a local Cilium version into the 
 
 .PHONY: build-cli
 build-cli: ## Build cilium cli binary
-	$(QUIET)$(MAKE) -C cilium
+	$(QUIET)$(MAKE) -C cilium-dbg
 
 .PHONY: build-agent
 build-agent: ## Build cilium daemon binary
@@ -648,9 +648,9 @@ kind-image-fast-agent: kind-ready build-cli build-agent ## Build cilium cli and 
 			docker cp "./bpf/" $${node_name}:"${dst}/var/lib/cilium/bpf"; \
 			docker exec -ti $${node_name} find "${dst}/var/lib/cilium/bpf" -type f -exec chmod 0644 {} + ;\
 			\
-			docker exec -ti $${node_name} rm -f "${dst}/cilium"; \
-			docker cp "./cilium/cilium" $${node_name}:"${dst}"; \
-			docker exec -ti $${node_name} chmod +x "${dst}/cilium"; \
+			docker exec -ti $${node_name} rm -f "${dst}/cilium-dbg"; \
+			docker cp "./cilium-dbg/cilium-dbg" $${node_name}:"${dst}"; \
+			docker exec -ti $${node_name} chmod +x "${dst}/cilium-dbg"; \
 			\
 			docker exec -ti $${node_name} rm -f "${dst}/cilium-agent"; \
 			docker cp "./daemon/cilium-agent" $${node_name}:"${dst}"; \

contrib/testing/kind-fast.yaml
@@ -1,7 +1,7 @@
 extraVolumes:
-  - name: cilium-cli-binary
+  - name: cilium-dbg-binary
     hostPath:
-      path: /cilium-binaries/cilium
+      path: /cilium-binaries/cilium-dbg
       type: File
   - name: cilium-agent-binary
     hostPath: