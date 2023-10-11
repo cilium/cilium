9234df64f9ce4e8f55c64b17c15b89575a96c580
André Martins <andre@cilium.io>
2023-10-11 14:08:25 +0200

    go.mod: bump grpc to 1.56.3
    This dependency needs to be updated in order the fix the security
    vulnerability https://github.com/advisories/GHSA-qppj-fm5r-hxr3
    
    Signed-off-by: André Martins <andre@cilium.io>

--- vendor/github.com/envoyproxy/protoc-gen-validate/validate/validate.h
+++ vendor/github.com/envoyproxy/protoc-gen-validate/validate/validate.h
@@ -5,8 +5,8 @@
 #include <regex>
 #include <stdexcept>
 #include <string>
-#include <typeinfo>
 #include <typeindex>
+#include <typeinfo>
 #include <unordered_map>
 
 #if !defined(_WIN32)
@@ -27,7 +27,7 @@
 
 #endif
 
-#include "google/protobuf/stubs/strutil.h" // for UTF8Len
+#include "google/protobuf/message.h"
 
 namespace pgv {
 using std::string;
@@ -42,67 +42,78 @@ public:
 using ValidationMsg = std::string;
 
 class BaseValidator {
+public:
+  /**
+   * Validate/check a generic message object with a registered validator for the concrete message
+   * type.
+   * @param m supplies the message to check.
+   * @param err supplies the place to return error information.
+   * @return true if the validation passes OR there is no registered validator for the concrete
+   *         message type. false is returned if validation explicitly fails.
+   */
+  static bool AbstractCheckMessage(const google::protobuf::Message& m, ValidationMsg* err) {
+    // Polymorphic lookup is used to see if there is a matching concrete validator. If so, call it.
+    // Otherwise return success.
+    auto it = abstractValidators().find(std::type_index(typeid(m)));
+    if (it == abstractValidators().end()) {
+      return true;
+    }
+    return it->second(m, err);
+  }
+
 protected:
-  static std::unordered_map<std::type_index, BaseValidator*>& validators() {
-    static auto* validator_map = new std::unordered_map<std::type_index, BaseValidator*>();
+  // Used to implement AbstractCheckMessage() above. Every message that is linked into the binary
+  // will register itself by type_index, allowing for polymorphic lookup later.
+  static std::unordered_map<std::type_index,
+                            std::function<bool(const google::protobuf::Message&, ValidationMsg*)>>&
+  abstractValidators() {
+    static auto* validator_map = new std::unordered_map<
+        std::type_index, std::function<bool(const google::protobuf::Message&, ValidationMsg*)>>();
     return *validator_map;
   }
 };
 
-template <typename T>
-class Validator : public BaseValidator {
+template <typename T> class Validator : public BaseValidator {
 public:
-  Validator(std::function<bool(const T&, ValidationMsg*)> check) : check_(check)
-  {
-    validators()[std::type_index(typeid(T))] = this;
-  }
-
-  static bool CheckMessage(const T& m, ValidationMsg* err)
-  {
-    auto val = static_cast<Validator<T>*>(validators()[std::type_index(typeid(T))]);
-    if (val) {
-      return val->check_(m, err);
-    }
-    return true;
+  Validator(std::function<bool(const T&, ValidationMsg*)> check) : check_(check) {
+    abstractValidators()[std::type_index(typeid(T))] = [this](const google::protobuf::Message& m,
+                                                              ValidationMsg* err) -> bool {
+      return check_(dynamic_cast<const T&>(m), err);
+    };
   }
 
 private:
   std::function<bool(const T&, ValidationMsg*)> check_;
 };
 
-static inline std::string String(const ValidationMsg& msg)
-{
-  return std::string(msg);
-}
+static inline std::string String(const ValidationMsg& msg) { return std::string(msg); }
 
-static inline bool IsPrefix(const string& maybe_prefix, const string& search_in)
-{
+static inline bool IsPrefix(const string& maybe_prefix, const string& search_in) {
   return search_in.compare(0, maybe_prefix.size(), maybe_prefix) == 0;
 }
 
-static inline bool IsSuffix(const string& maybe_suffix, const string& search_in)
-{
-  return maybe_suffix.size() <= search_in.size() && search_in.compare(search_in.size() - maybe_suffix.size(), maybe_suffix.size(), maybe_suffix) == 0;
+static inline bool IsSuffix(const string& maybe_suffix, const string& search_in) {
+  return maybe_suffix.size() <= search_in.size() &&
+         search_in.compare(search_in.size() - maybe_suffix.size(), maybe_suffix.size(),
+                           maybe_suffix) == 0;
 }
 
-static inline bool Contains(const string& search_in, const string& to_find)
-{
+static inline bool Contains(const string& search_in, const string& to_find) {
   return search_in.find(to_find) != string::npos;
 }
 
-static inline bool NotContains(const string& search_in, const string& to_find)
-{
+static inline bool NotContains(const string& search_in, const string& to_find) {
   return !Contains(search_in, to_find);
 }
 
 static inline bool IsIpv4(const string& to_validate) {
-	struct sockaddr_in sa;
-	return !(inet_pton(AF_INET, to_validate.c_str(), &sa.sin_addr) < 1);
+  struct sockaddr_in sa;
+  return !(inet_pton(AF_INET, to_validate.c_str(), &sa.sin_addr) < 1);
 }
 
 static inline bool IsIpv6(const string& to_validate) {
   struct sockaddr_in6 sa_six;
-	return !(inet_pton(AF_INET6, to_validate.c_str(), &sa_six.sin6_addr) < 1);
+  return !(inet_pton(AF_INET6, to_validate.c_str(), &sa_six.sin6_addr) < 1);
 }
 
 static inline bool IsIp(const string& to_validate) {
@@ -118,7 +129,7 @@ static inline bool IsHostname(const string& to_validate) {
   const auto iter_end = std::sregex_token_iterator();
   auto iter = std::sregex_token_iterator(to_validate.begin(), to_validate.end(), dot_regex, -1);
   for (; iter != iter_end; ++iter) {
-    const std::string &part = *iter;
+    const std::string& part = *iter;
     if (part.empty() || part.length() > 63) {
       return false;
     }
@@ -128,8 +139,9 @@ static inline bool IsHostname(const string& to_validate) {
     if (part.at(part.length() - 1) == '-') {
       return false;
     }
-    for (const auto &character : part) {
-      if ((character < 'A' || character > 'Z') && (character < 'a' || character > 'z') && (character < '0' || character > '9') && character != '-') {
+    for (const auto& character : part) {
+      if ((character < 'A' || character > 'Z') && (character < 'a' || character > 'z') &&
+          (character < '0' || character > '9') && character != '-') {
         return false;
       }
     }
@@ -138,19 +150,33 @@ static inline bool IsHostname(const string& to_validate) {
   return true;
 }
 
-static inline size_t Utf8Len(const string& narrow_string) {
-  const char *str_char = narrow_string.c_str();
+namespace {
+
+inline int OneCharLen(const char* src) {
+  return "\1\1\1\1\1\1\1\1\1\1\1\1\2\2\3\4"[(*src & 0xFF) >> 4];
+}
+
+inline int UTF8FirstLetterNumBytes(const char *utf8_str, int str_len) {
+  if (str_len == 0)
+    return 0;
+  return OneCharLen(utf8_str);
+}
+
+inline size_t Utf8Len(const string& narrow_string) {
+  const char* str_char = narrow_string.c_str();
   ptrdiff_t byte_len = narrow_string.length();
   size_t unicode_len = 0;
   int char_len = 1;
   while (byte_len > 0 && char_len > 0) {
-    char_len = google::protobuf::UTF8FirstLetterNumBytes(str_char, byte_len);
+    char_len = UTF8FirstLetterNumBytes(str_char, byte_len);
     str_char += char_len;
     byte_len -= char_len;
     ++unicode_len;
   }
   return unicode_len;
 }
+
+} // namespace
 
 } // namespace pgv
 
