cilium/cilium

commit 73afc5fac2ad83b80d79feb1fa91354e7c63993b

Remove daemon health from being reported via the CLI
The daemon module currently does not support health rollups and hence
reports the default status aka Unknown.
This is less than ideal and may yields to confusion.
This PR removes daemon health from being reported via the cilium health
command.

Signed-off-by: Fernand Galiana <fernand.galiana@isovalent.com>

daemon/cmd/vitals.go
@@ -14,6 +14,12 @@ import (
 	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
+// moduleExcludes tracks modules that should be excluded from the health report.
+var moduleExcludes = map[string]struct{}{
+	// daemon currently does not rollup status and hence reports default health aka unknown.
+	"daemon": {},
+}
+
 type getHealth struct {
 	daemon *Daemon
 }
@@ -33,6 +39,9 @@ func (d *Daemon) getHealthReport() models.ModulesHealth {
 	mm := d.healthProvider.All()
 	rr := make([]*models.ModuleHealth, 0, len(mm))
 	for _, m := range mm {
+		if _, ok := moduleExcludes[m.ModuleID]; ok {
+			continue
+		}
 		rr = append(rr, toModuleHealth(m))
 	}
 

commit 6a510c271118b063d20fbb89204c519bf56fba58

daemon: Remove moduleExcludes in health status
The exclusion mechanism is error-prone and we should not have to resort
to excluding status, but should rather not report unknown status in the
first place by depending on HealthReporter and not using it.

Fixes: 73afc5fac2 ("Remove daemon health from being reported via the CLI")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/daemon.go
@@ -223,7 +223,6 @@ type Daemon struct {
 	settings cellSettings
 	// enable modules health support
 	healthProvider cell.Health
-	healthReporter cell.HealthReporter
 }
 
 func (d *Daemon) initDNSProxyContext(size int) {
@@ -529,7 +528,6 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		authManager:          params.AuthManager,
 		settings:             params.Settings,
 		healthProvider:       params.HealthProvider,
-		healthReporter:       params.HealthReporter,
 	}
 
 	d.configModifyQueue = eventqueue.NewEventQueueBuffered("config-modify-queue", ConfigModifyQueueSize)

daemon/cmd/daemon_main.go
@@ -1665,7 +1665,6 @@ type daemonParams struct {
 	AuthManager          *auth.AuthManager
 	Settings             cellSettings
 	HealthProvider       cell.Health
-	HealthReporter       cell.HealthReporter
 	DeviceManager        *linuxdatapath.DeviceManager `optional:"true"`
 	// Grab the GC object so that we can start the CT/NAT map garbage collection.
 	// This is currently necessary because these maps have not yet been modularized,

daemon/cmd/vitals.go
@@ -14,12 +14,6 @@ import (
 	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
-// moduleExcludes tracks modules that should be excluded from the health report.
-var moduleExcludes = map[string]struct{}{
-	// daemon currently does not rollup status and hence reports default health aka unknown.
-	"agent.daemon": {},
-}
-
 type getHealth struct {
 	daemon *Daemon
 }
@@ -39,9 +33,6 @@ func (d *Daemon) getHealthReport() models.ModulesHealth {
 	mm := d.healthProvider.All()
 	rr := make([]*models.ModuleHealth, 0, len(mm))
 	for _, m := range mm {
-		if _, ok := moduleExcludes[m.FullModuleID.String()]; ok {
-			continue
-		}
 		rr = append(rr, toModuleHealth(m))
 	}
 