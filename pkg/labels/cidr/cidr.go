87a99725304c62a281ad250105fd707876356164
Fabio Falzoi <fabio.falzoi@isovalent.com>
2023-10-13 12:45:19 +0200

    labels/cidr: Use a lru cache to store CIDR labels
    [ upstream commit 3debf6dc67c3581a0d5f6322646fe6fe92177dc4 ]
    
    To avoid excessive heap usage, limit the number of cached labels using a
    LRU map. Maximum cache size is empirically set to 16384 and a lock is
    used to serialize concurrent accesses to the cache.
    
    Note that, being a LRU cache, the Get operations modify the cache
    internal status, so a classic mutex has been used instead of a rwmutex.
    
    Benchmark results to compare LRU-based memoization against the
    non-memoized version:
    
    name                             old time/op    new time/op    delta
    GetCIDRLabels/0.0.0.0/0-8           204ns ± 4%     351ns ± 2%  +71.85%  (p=0.000 n=8+10)
    GetCIDRLabels/10.16.0.0/16-8       4.20µs ±10%    1.73µs ± 3%  -58.84%  (p=0.000 n=10+9)
    GetCIDRLabels/192.0.2.3/32-8       8.02µs ± 4%    3.27µs ± 2%  -59.25%  (p=0.000 n=9+9)
    GetCIDRLabels/192.0.2.3/24-8       6.65µs ±11%    3.26µs ± 2%  -51.02%  (p=0.000 n=10+8)
    GetCIDRLabels/192.0.2.0/24-8       6.52µs ±10%    2.86µs ± 1%  -56.10%  (p=0.000 n=10+9)
    GetCIDRLabels/::/0-8                330ns ± 2%     354ns ± 5%   +7.28%  (p=0.000 n=9+10)
    GetCIDRLabels/fdff::ff/128-8       52.9µs ± 6%    12.4µs ± 6%  -76.48%  (p=0.000 n=10+10)
    GetCIDRLabels/f00d:42::ff/128-8    55.3µs ± 5%    12.6µs ± 5%  -77.27%  (p=0.000 n=10+10)
    GetCIDRLabels/f00d:42::ff/96-8     41.8µs ± 7%    12.4µs ± 2%  -70.20%  (p=0.000 n=10+9)
    
    name                             old alloc/op   new alloc/op   delta
    GetCIDRLabels/0.0.0.0/0-8            656B ± 0%      624B ± 0%   -4.88%  (p=0.000 n=10+10)
    GetCIDRLabels/10.16.0.0/16-8       3.17kB ± 0%    2.40kB ± 0%  -24.46%  (p=0.000 n=10+10)
    GetCIDRLabels/192.0.2.3/32-8       6.88kB ± 0%    5.01kB ± 0%  -27.21%  (p=0.000 n=8+8)
    GetCIDRLabels/192.0.2.3/24-8       6.32kB ± 0%    5.01kB ± 0%  -20.73%  (p=0.000 n=9+9)
    GetCIDRLabels/192.0.2.0/24-8       6.32kB ± 0%    4.93kB ± 0%  -22.03%  (p=0.000 n=10+10)
    GetCIDRLabels/::/0-8                 656B ± 0%      624B ± 0%   -4.88%  (p=0.000 n=10+10)
    GetCIDRLabels/fdff::ff/128-8       25.9kB ± 0%    18.5kB ± 0%  -28.58%  (p=0.000 n=10+10)
    GetCIDRLabels/f00d:42::ff/128-8    28.8kB ± 0%    18.5kB ± 0%  -35.70%  (p=0.000 n=10+10)
    GetCIDRLabels/f00d:42::ff/96-8     25.1kB ± 0%    18.5kB ± 0%  -26.20%  (p=0.000 n=10+8)
    
    name                             old allocs/op  new allocs/op  delta
    GetCIDRLabels/0.0.0.0/0-8            3.00 ± 0%      2.00 ± 0%  -33.33%  (p=0.000 n=10+10)
    GetCIDRLabels/10.16.0.0/16-8         37.0 ± 0%       2.0 ± 0%  -94.59%  (p=0.000 n=10+10)
    GetCIDRLabels/192.0.2.3/32-8         69.0 ± 0%       2.0 ± 0%  -97.10%  (p=0.000 n=10+10)
    GetCIDRLabels/192.0.2.3/24-8         53.0 ± 0%       2.0 ± 0%  -96.23%  (p=0.000 n=10+10)
    GetCIDRLabels/192.0.2.0/24-8         53.0 ± 0%       2.0 ± 0%  -96.23%  (p=0.000 n=10+10)
    GetCIDRLabels/::/0-8                 3.00 ± 0%      2.00 ± 0%  -33.33%  (p=0.000 n=10+10)
    GetCIDRLabels/fdff::ff/128-8          449 ± 0%         3 ± 0%  -99.33%  (p=0.000 n=10+10)
    GetCIDRLabels/f00d:42::ff/128-8       449 ± 0%         3 ± 0%  -99.33%  (p=0.000 n=10+10)
    GetCIDRLabels/f00d:42::ff/96-8        295 ± 0%         3 ± 0%  -98.98%  (p=0.000 n=10+10)
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/labels/cidr/cidr.go
+++ pkg/labels/cidr/cidr.go
@@ -10,7 +10,10 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/hashicorp/golang-lru/v2/simplelru"
+
 	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/lock"
 )
 
 // maskedIPToLabelString is the base method for serializing an IP + prefix into
@@ -81,6 +84,11 @@ func IPStringToLabel(ip string) (labels.Label, error) {
 //
 // The identity reserved:world is always added as it includes any CIDR.
 func GetCIDRLabels(prefix netip.Prefix) labels.Labels {
+	once.Do(func() {
+		// simplelru.NewLRU fails only when given a negative size, so we can skip the error check
+		cidrLabelsCache, _ = simplelru.NewLRU[netip.Prefix, []labels.Label](cidrLabelsCacheMaxSize, nil)
+	})
+
 	addr := prefix.Addr()
 	ones := prefix.Bits()
 	lbls := make(labels.Labels, 1 /* this CIDR */ +ones /* the prefixes */ +1 /*world label*/)
@@ -94,42 +102,51 @@ func GetCIDRLabels(prefix netip.Prefix) labels.Labels {
 		return lbls
 	}
 
-	cache := cidrLabelsCache.Get().(map[netip.Prefix][]labels.Label)
 	computeCIDRLabels(
-		cache,
+		cidrLabelsCache,
 		lbls,
 		nil, // avoid allocating space for the intermediate results until we need it
 		addr,
 		ones,
 		0,
 	)
-	cidrLabelsCache.Put(cache)
 	lbls[worldLabel.Key] = worldLabel
 
 	return lbls
 }
 
-// cidrLabelsCache stores the partial computations for CIDR labels.
-// This both avoids repeatedly computing the prefixes and makes sure the
-// CIDR strings are reused to reduce memory usage.
-// Stored in a sync.Pool to allow GC to garbage collect the cache if needed.
-// With lots of contention, multiple cache maps might exist.
-//
-// Stores e.g. for prefix "10.0.0.0/8" the labels ["10.0.0.0/8", ..., "0.0.0.0/0"].
-var cidrLabelsCache = sync.Pool{
-	New: func() any { return make(map[netip.Prefix][]labels.Label) },
-}
+var (
+	// cidrLabelsCache stores the partial computations for CIDR labels.
+	// This both avoids repeatedly computing the prefixes and makes sure the
+	// CIDR strings are reused to reduce memory usage.
+	// Stored in a lru map to limit memory usage.
+	//
+	// Stores e.g. for prefix "10.0.0.0/8" the labels ["10.0.0.0/8", ..., "0.0.0.0/0"].
+	cidrLabelsCache *simplelru.LRU[netip.Prefix, []labels.Label]
 
-var worldLabel = labels.Label{Key: labels.IDNameWorld, Source: labels.LabelSourceReserved}
+	// mutex to serialize concurrent accesses to the cidrLabelsCache.
+	mu lock.Mutex
+)
 
-func computeCIDRLabels(cache map[netip.Prefix][]labels.Label, lbls labels.Labels, results []labels.Label, addr netip.Addr, ones, i int) []labels.Label {
+const cidrLabelsCacheMaxSize = 16384
+
+var (
+	once sync.Once
+
+	worldLabel = labels.Label{Key: labels.IDNameWorld, Source: labels.LabelSourceReserved}
+)
+
+func computeCIDRLabels(cache *simplelru.LRU[netip.Prefix, []labels.Label], lbls labels.Labels, results []labels.Label, addr netip.Addr, ones, i int) []labels.Label {
 	if i > ones {
 		return results
 	}
 
 	prefix := netip.PrefixFrom(addr, i)
 
-	if cachedLbls, ok := cache[prefix]; ok {
+	mu.Lock()
+	cachedLbls, ok := cache.Get(prefix)
+	mu.Unlock()
+	if ok {
 		for _, lbl := range cachedLbls {
 			lbls[lbl.Key] = lbl
 		}
@@ -151,8 +168,11 @@ func computeCIDRLabels(cache map[netip.Prefix][]labels.Label, lbls labels.Labels
 		append(results, prefixLabel),
 		addr, ones, i+1,
 	)
+
 	// Cache the resulting labels derived from this prefix, e.g. /8, /7, ...
-	cache[prefix] = results[i:]
+	mu.Lock()
+	cache.Add(prefix, results[i:])
+	mu.Unlock()
 
 	return results
 }
