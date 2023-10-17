3eae19120ab180a2733c248bc0316ab886bffbfb
Ryan Drew <ryan.drew@isovalent.com>
2023-10-17 16:11:27 -0600

    operator: Fix logic used to sync Cilium's IngressClass on startup
    [ upstream commit 2cfc825bad7b33b17a5ff29a64ebcd1261ce3200 ]
    
    This commit introduces changes to the ingress class manager piece of the
    ingress controller, in order to address bugs impacting the proper
    synching of Cilium's IngressClass during start up. The following changes
    are made:
    
    * Replace use of an Informer with Resource[T] for IngressClass. This
      helps simplify the logic used to perform the initial sync.
    * Move the responsibility of tracking if Cilium should act as the default
      IngressClass into the ingress class manager, rather than having the
      ingress controller track this itself when processing IngressClass
      events.
    
    After the ingress class manager is constructed, the ingress controller
    will be able to determine if Cilium is the default IngressClass for a
    cluster through the ingress class manager. The ingress controller no
    longer has to wait to process an event for Cilium's IngressClass to
    learn if Cilium should be the default.
    
    Before this commit, the ingress controller would process all Ingress
    resources before processing IngressClass resources. This is because the
    Ingress resource informer would be started before the ingress class
    manager, so all events related to Ingress resources would appear in the
    ingress controller's event queue before events relating to IngressClass
    resources. This presented a problem, because the ingress controller
    would always believe that it was not the default IngressClass for a
    cluster on startup while processing each Ingress resource for the first
    time. This could lead to the following situation:
    
    1. The ingress controller processes all Ingress resources.
    2. The ingress controller processes IngressClass resources, and learns
       that it should act as the default IngressClass for the cluster.
    3. A resync of Ingress resources is triggered.
    
    This double-sync overhead can act as a problem for large-scale clusters.
    
    Signed-off-by: Ryan Drew <ryan.drew@isovalent.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- operator/pkg/ingress/ingress_class.go
+++ operator/pkg/ingress/ingress_class.go
@@ -4,172 +4,196 @@
 package ingress
 
 import (
-	"fmt"
+	"context"
+	"strconv"
+	"sync/atomic"
 
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 
-	"github.com/cilium/cilium/pkg/k8s"
-	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
-	"github.com/cilium/cilium/pkg/k8s/informer"
+	"github.com/cilium/cilium/pkg/k8s/resource"
 	slim_networkingv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/networking/v1"
-	"github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
-type ingressClassAddedEvent struct {
-	ingressClass *slim_networkingv1.IngressClass
+// isIngressClassMarkedDefault determines if the given IngressClass has an annotation marking it as the
+// default IngressClass for the cluster.
+// If the annotation's value fails to parse, then an error is returned to signal that processing the
+// IngressClass should be retried at a later point in time.
+// There are four possible cases:
+// 1. Annotation is set to "true": we are the default IngressClass.
+// 2. Annotation is set to "false", a non-bool value, or is missing: we are not the default IngressClass.
+func isIngressClassMarkedDefault(obj *slim_networkingv1.IngressClass) (bool, error) {
+	var err error
+
+	// If the annotation is not set, or set to an improper value,
+	// we should not be the default ingress class.
+	isDefault := false
+
+	if val, ok := obj.GetAnnotations()[slim_networkingv1.AnnotationIsDefaultIngressClass]; ok {
+		isDefault, err = strconv.ParseBool(val)
+		if err != nil {
+			log.WithError(err).Warnf("Failed to parse annotation value for %q", slim_networkingv1.AnnotationIsDefaultIngressClass)
+
+			return false, err
+		}
+	}
+
+	return isDefault, nil
 }
 
-type ingressClassUpdatedEvent struct {
-	oldIngressClass *slim_networkingv1.IngressClass
-	newIngressClass *slim_networkingv1.IngressClass
+// isIngressClassCilium returns true if the given IngressClass resource has the same name as
+// the constant 'ciliumIngressClassName'.
+func isIngressClassCilium(obj *slim_networkingv1.IngressClass) bool {
+	return obj.GetName() == ciliumIngressClassName
 }
 
-type ingressClassDeletedEvent struct {
-	ingressClass *slim_networkingv1.IngressClass
-}
-
-type ingressClassManager struct {
-	informer cache.Controller
-	store    cache.Store
-
-	queue      workqueue.RateLimitingInterface
-	maxRetries int
-
-	ingressQueue workqueue.RateLimitingInterface
-}
-
-// type used to signal changes to the ingress controller queue
 type ciliumIngressClassUpdatedEvent struct {
-	ingressClass *slim_networkingv1.IngressClass
+	isDefault bool
+	changed   bool
 }
 
 type ciliumIngressClassDeletedEvent struct {
-	ingressClass *slim_networkingv1.IngressClass
+	wasDefault bool
 }
 
-func newIngressClassManager(clientset k8sClient.Clientset, ingressQueue workqueue.RateLimitingInterface, maxRetries int) (*ingressClassManager, error) {
+type ingressClassManager struct {
+	isDefaultIngressClass atomic.Bool
+	synced                atomic.Bool
+	queue                 workqueue.RateLimitingInterface
+	ingressClassEvents    <-chan resource.Event[*slim_networkingv1.IngressClass]
+}
+
+// newIngressClassManager creates a new ingressClassManager.
+func newIngressClassManager(
+	ctx context.Context,
+	queue workqueue.RateLimitingInterface,
+	ingressClasses resource.Resource[*slim_networkingv1.IngressClass],
+) *ingressClassManager {
 	manager := &ingressClassManager{
-		queue:        workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
-		ingressQueue: ingressQueue,
-		maxRetries:   maxRetries,
+		isDefaultIngressClass: atomic.Bool{},
+		synced:                atomic.Bool{},
+		ingressClassEvents:    ingressClasses.Events(ctx),
+		queue:                 queue,
 	}
 
-	manager.store, manager.informer = informer.NewInformer(
-		utils.ListerWatcherFromTyped[*slim_networkingv1.IngressClassList](clientset.Slim().NetworkingV1().IngressClasses()),
-		&slim_networkingv1.IngressClass{},
-		0,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc:    manager.handleAddIngressClass,
-			UpdateFunc: manager.handleUpdateIngressClass,
-			DeleteFunc: manager.handleDeleteIngressClass,
+	manager.isDefaultIngressClass.Store(false)
+	manager.synced.Store(false)
+
+	return manager
+}
+
+func (i *ingressClassManager) IsDefault() bool {
+	return i.isDefaultIngressClass.Load()
+}
+
+// WaitForSync blocks until a Sync event is received from the IngressClasses Resource.
+// If a Sync event has already been received, this method immediately returns.
+func (i *ingressClassManager) WaitForSync(ctx context.Context) error {
+	if i.synced.Load() {
+		return nil
+	}
+
+	// This function will only return "false" if ctx.Done() is closed.
+	// Instead of returning a bool, this method returns an error, which will
+	// be easier for callers to handle.
+	success := cache.WaitForNamedCacheSync(
+		"ingressClassManager", ctx.Done(),
+		func() bool {
+			return i.synced.Load()
 		},
-		nil,
 	)
 
-	go manager.informer.Run(wait.NeverStop)
-	if !cache.WaitForCacheSync(wait.NeverStop, manager.informer.HasSynced) {
-		return manager, fmt.Errorf("unable to sync service")
+	if success {
+		return nil
 	}
-	log.WithField("existing-ingressclasses", manager.store.ListKeys()).Debug("ingress classes synced")
-	return manager, nil
+
+	return ctx.Err()
 }
 
-// Run kicks off the control loop
-func (i *ingressClassManager) Run() {
-	for i.processEvent() {
-	}
-}
+// Run kicks off the the main control loop for the ingressClassManager.
+// The ingressClassManager will start processing IngressClass events and signaling updates by
+// sending events on the queue that was provided during construction.
+// This method returns when the given context is cancelled or when then IngressClasses
+// resource given during construction is stopped.
+func (i *ingressClassManager) Run(ctx context.Context) error {
+	log.Debug("Starting ingressClassManager")
 
-func (i *ingressClassManager) handleAddIngressClass(obj interface{}) {
-	if ic := k8s.ObjToV1IngressClass(obj); ic != nil {
-		i.queue.Add(ingressClassAddedEvent{ingressClass: ic})
-	}
-}
-
-func (i *ingressClassManager) handleUpdateIngressClass(oldObj, newObj interface{}) {
-	oldIngressClass := k8s.ObjToV1IngressClass(oldObj)
-	if oldIngressClass == nil {
-		return
-	}
-	newIngressClass := k8s.ObjToV1IngressClass(newObj)
-	if newIngressClass == nil {
-		return
-	}
-	if oldIngressClass.DeepEqual(newIngressClass) {
-		return
-	}
-	i.queue.Add(ingressClassUpdatedEvent{oldIngressClass: oldIngressClass, newIngressClass: newIngressClass})
-}
-
-func (i *ingressClassManager) handleDeleteIngressClass(obj interface{}) {
-	if ic := k8s.ObjToV1IngressClass(obj); ic != nil {
-		i.queue.Add(ingressClassDeletedEvent{ingressClass: ic})
-	}
-}
-
-func (i *ingressClassManager) processEvent() bool {
-	event, shutdown := i.queue.Get()
-	if shutdown {
-		return false
-	}
-	defer i.queue.Done(event)
-	err := i.handleEvent(event)
-	if err == nil {
-		i.queue.Forget(event)
-	} else if i.queue.NumRequeues(event) < i.maxRetries {
-		i.queue.AddRateLimited(event)
-	} else {
-		log.Errorf("failed to process event: %s", event)
-		i.queue.Forget(event)
-	}
-	return true
-}
-
-func (i *ingressClassManager) handleEvent(event interface{}) error {
 	var err error
-	switch ev := event.(type) {
-	case ingressClassAddedEvent:
-		log.WithField(logfields.IngressClass, ev.ingressClass.Name).Debug("Handling ingress class added event")
-		err = i.handleIngressClassAddedEvent(ev)
-	case ingressClassUpdatedEvent:
-		log.WithField(logfields.IngressClass, ev.newIngressClass.Name).Debug("Handling ingress class updated event")
-		err = i.handleIngressClassUpdatedEvent(ev)
-	case ingressClassDeletedEvent:
-		log.WithField(logfields.IngressClass, ev.ingressClass.Name).Debug("Handling ingress class deleted event")
-		err = i.handleIngressClassDeletedEvent(ev)
-	default:
-		err = fmt.Errorf("received an unknown event: %s", ev)
+
+	for {
+		select {
+		case event, ok := <-i.ingressClassEvents:
+			if !ok {
+				return nil
+			}
+
+			err = nil
+
+			switch event.Kind {
+			case resource.Sync:
+				err = i.handleSyncEvent()
+			case resource.Upsert:
+				err = i.handleUpsertEvent(event)
+			case resource.Delete:
+				err = i.handleDeleteEvent(event)
+			}
+
+			event.Done(err)
+		case <-ctx.Done():
+			return ctx.Err()
+		}
 	}
+}
+
+// handleSyncEvent handles a Sync event for the events of an IngressClasses Resource.
+func (i *ingressClassManager) handleSyncEvent() error {
+	log.Debug("Handling IngressClass Sync event")
+	i.synced.Store(true)
+
+	return nil
+}
+
+// handleUpsertEvent handles an Upsert event for an IngressClass resource.
+// If the IngressClass has a name that is not the Cilium IngressClass name, it will be ignored.
+// If an error is returned, then the IngressClass contained in the event should be retried at a later
+// point in time.
+func (i *ingressClassManager) handleUpsertEvent(event resource.Event[*slim_networkingv1.IngressClass]) error {
+	log.WithField(logfields.IngressClass, event.Object).Warn("Handling IngressClass Upsert event")
+
+	if event.Object == nil || !isIngressClassCilium(event.Object) {
+		return nil
+	}
+
+	// Even if an error occurs due to a bad annotation value, we still need to perform an update to
+	// signal that we are no longer the default ingress class.
+	isDefault, err := isIngressClassMarkedDefault(event.Object)
+	old := i.isDefaultIngressClass.Swap(isDefault)
+
+	i.queue.Add(ciliumIngressClassUpdatedEvent{
+		isDefault: isDefault,
+		changed:   !(old == isDefault),
+	})
+
 	return err
 }
 
-func (i *ingressClassManager) handleIngressClassAddedEvent(event ingressClassAddedEvent) error {
-	log.WithField(logfields.IngressClass, event.ingressClass).Debug("Handling ingress class add")
-	i.notify(event.ingressClass)
-	return nil
-}
+// handleDeleteEvent handles a Delete event for an IngressClass resource.
+// If the IngressClass has a name that is not the Cilium IngressClass name, it will be ignored.
+// If an error is returned, then the IngressClass contained in the event should be retried at a later
+// point in time.
+func (i *ingressClassManager) handleDeleteEvent(event resource.Event[*slim_networkingv1.IngressClass]) error {
+	log.WithField(logfields.IngressClass, event.Object).Debug("Handling IngressClass Delete event")
 
-func (i *ingressClassManager) handleIngressClassUpdatedEvent(event ingressClassUpdatedEvent) error {
-	log.WithField("old", event.oldIngressClass).WithField("new", event.newIngressClass).Debug("Handling ingress class update")
-	i.notify(event.newIngressClass)
-	return nil
-}
-
-func (i *ingressClassManager) handleIngressClassDeletedEvent(event ingressClassDeletedEvent) error {
-	log.WithField(logfields.IngressClass, event.ingressClass).Debug("Handling ingress class delete")
-
-	if event.ingressClass.GetName() == ciliumIngressClassName {
-		i.ingressQueue.Add(ciliumIngressClassDeletedEvent{ingressClass: event.ingressClass})
+	if event.Object == nil || !isIngressClassCilium(event.Object) {
+		return nil
 	}
-	return nil
-}
 
-// notify informs parent ingress about change in our ingress class configuration
-func (i *ingressClassManager) notify(ic *slim_networkingv1.IngressClass) {
-	if ic.GetName() == ciliumIngressClassName {
-		i.ingressQueue.Add(ciliumIngressClassUpdatedEvent{ingressClass: ic})
-	}
+	old := i.isDefaultIngressClass.Swap(false)
+
+	i.queue.Add(ciliumIngressClassDeletedEvent{
+		wasDefault: old,
+	})
+
+	return nil
 }
