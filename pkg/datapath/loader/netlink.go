cilium/cilium

commit ecd4fc0adf9840e1a08786610a0e705cb3d73fb9

bpf/init.sh: move host device ip assignment to go
Signed-off-by: Robin Gögge <r.goegge@isovalent.com>

pkg/datapath/loader/netlink.go
@@ -414,3 +414,33 @@ func (l *Loader) reloadIPSecOnLinkChanges() {
 		}
 	}()
 }
+
+// addHostDeviceAddr add internal ipv4 and ipv6 addresses to the cilium_host device.
+func addHostDeviceAddr(hostDev netlink.Link, ipv4, ipv6 net.IP) error {
+	if ipv4 != nil {
+		addr := netlink.Addr{
+			IPNet: &net.IPNet{
+				IP:   ipv4,
+				Mask: net.CIDRMask(32, 32), // corresponds to /32
+			},
+		}
+
+		if err := netlink.AddrReplace(hostDev, &addr); err != nil {
+			return err
+		}
+	}
+	if ipv6 != nil {
+		addr := netlink.Addr{
+			IPNet: &net.IPNet{
+				IP:   ipv6,
+				Mask: net.CIDRMask(64, 128), // corresponds to /64
+			},
+		}
+
+		if err := netlink.AddrReplace(hostDev, &addr); err != nil {
+			return err
+		}
+
+	}
+	return nil
+}

bpf/init.sh
@@ -181,15 +181,6 @@ function encap_fail()
 	exit 1
 }
 
-	# If the host does not have an IPv6 address assigned, assign our generated host
-	# IP to make the host accessible to endpoints
-	if [ "$IP6_HOST" != "<nil>" ]; then
-		[ -n "$(ip -6 addr show to $IP6_HOST dev $HOST_DEV1)" ] || ip -6 addr add $IP6_HOST dev $HOST_DEV1
-	fi
-	if [ "$IP4_HOST" != "<nil>" ]; then
-		[ -n "$(ip -4 addr show to $IP4_HOST dev $HOST_DEV1)" ] || ip -4 addr add $IP4_HOST dev $HOST_DEV1
-	fi
-
 if [ "$PROXY_RULE" = "true" ]; then
 # Decrease priority of the rule to identify local addresses
 move_local_rules

pkg/datapath/loader/base.go
@@ -319,13 +319,16 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 	}
 	args[initArgMode] = string(mode)
 
+	var nodeIPv4, nodeIPv6 net.IP
 	args[initArgIPv4NodeIP] = "<nil>"
 	args[initArgIPv6NodeIP] = "<nil>"
 	if option.Config.EnableIPv4 {
-		args[initArgIPv4NodeIP] = node.GetInternalIPv4Router().String()
+		nodeIPv4 = node.GetInternalIPv4Router()
+		args[initArgIPv4NodeIP] = nodeIPv4.String()
 	}
 	if option.Config.EnableIPv6 {
-		args[initArgIPv6NodeIP] = node.GetIPv6Router().String()
+		nodeIPv6 = node.GetIPv6Router()
+		args[initArgIPv6NodeIP] = nodeIPv6.String()
 		// Docker <17.05 has an issue which causes IPv6 to be disabled in the initns for all
 		// interface (https://github.com/docker/libnetwork/issues/1720)
 		// Enable IPv6 for now
@@ -350,6 +353,11 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 
 	// Any code that relies on sysctl settings being applied needs to be called after this.
 	sysctl.ApplySettings(sysSettings)
+
+	// add internal ipv4 and ipv6 addresses to cilium_host
+	if err := addHostDeviceAddr(hostDev1, nodeIPv4, nodeIPv6); err != nil {
+		return fmt.Errorf("failed to add internal IP address to %s: %w", hostDev1.Attrs().Name, err)
+	}
 
 	if err := l.writeNodeConfigHeader(o); err != nil {
 		log.WithError(err).Error("Unable to write node config header")

pkg/datapath/loader/netlink_test.go
@@ -7,6 +7,7 @@ package loader
 
 import (
 	"fmt"
+	"net"
 	"testing"
 
 	"github.com/containernetworking/plugins/pkg/ns"
@@ -72,6 +73,57 @@ func TestSetupDev(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, s, "0")
 		}
+
+		err = netlink.LinkDel(dummy)
+		require.NoError(t, err)
+
+		return nil
+	})
+}
+
+func TestAddHostDeviceAddr(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	// test IP addresses
+	testIPv4 := net.ParseIP("1.2.3.4")
+	testIPv6 := net.ParseIP("2001:db08:0bad:cafe:600d:bee2:0bad:cafe")
+
+	netnsName := "test-internal-node-ips"
+	netns0, err := netns.ReplaceNetNSWithName(netnsName)
+	require.NoError(t, err)
+	require.NotNil(t, netns0)
+	t.Cleanup(func() {
+		netns0.Close()
+		netns.RemoveNetNSWithName(netnsName)
+	})
+
+	netns0.Do(func(_ ns.NetNS) error {
+		ifName := "dummy"
+		dummy := &netlink.Dummy{
+			LinkAttrs: netlink.LinkAttrs{
+				Name: ifName,
+			},
+		}
+		err := netlink.LinkAdd(dummy)
+		require.NoError(t, err)
+
+		err = addHostDeviceAddr(dummy, testIPv4, testIPv6)
+		require.NoError(t, err)
+
+		addrs, err := netlink.AddrList(dummy, netlink.FAMILY_ALL)
+		require.NoError(t, err)
+
+		var foundIPv4, foundIPv6 bool
+		for _, addr := range addrs {
+			if testIPv4.Equal(addr.IP) {
+				foundIPv4 = true
+			}
+			if testIPv6.Equal(addr.IP) {
+				foundIPv6 = true
+			}
+		}
+		require.Equal(t, foundIPv4, true)
+		require.Equal(t, foundIPv6, true)
 
 		err = netlink.LinkDel(dummy)
 		require.NoError(t, err)

commit 12e83d0696e3763c250ab485fb4dfeb69f46fae6

Fix IPv6 route on cilium_host iface
Fixes #28327

This patch fixes the subnet assigned to the cilium_host device from /64 back to
/128, a bug introduced in ecd4fc0adf9840e1a08786610a0e705cb3d73fb9 when
converting the init.sh script to Go code. The conversion never meant to change
the behavior in init.sh and the bug caused connectivity issues in certain cilium
configurations.

Signed-off-by: Ashley Reese <ashley@victorianfox.com>

pkg/datapath/loader/netlink.go
@@ -430,7 +430,7 @@ func addHostDeviceAddr(hostDev netlink.Link, ipv4, ipv6 net.IP) error {
 		addr := netlink.Addr{
 			IPNet: &net.IPNet{
 				IP:   ipv6,
-				Mask: net.CIDRMask(64, 128), // corresponds to /64
+				Mask: net.CIDRMask(128, 128), // corresponds to /128
 			},
 		}
 