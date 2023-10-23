d847520bd376501786fe7e5922048757122937a6
Julian Wiedmann <jwi@isovalent.com>
2023-10-23 10:52:57 +0700

    bpf: nat: report pre-SNAT address in trace event
    [ upstream commit 2853c5232ee528c1a2f4550f57f19e19271a162f ]
    
    When applying SNAT to a packet, also report the original source address in
    the subsequent trace event. This helps to associate the internal and
    external view of a connection.
    
    We use the `orig_addr` field in the trace event, which was originally
    introduced back with b3aa583d494a ("bpf: Report original source IP in TRACE_TO_LXC")
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- bpf/lib/nodeport.h
+++ bpf/lib/nodeport.h
@@ -187,6 +187,7 @@ nodeport_has_nat_conflict_ipv6(const struct ipv6hdr *ip6 __maybe_unused,
 }
 
 static __always_inline int nodeport_snat_fwd_ipv6(struct __ctx_buff *ctx,
+						  union v6addr *saddr,
 						  struct trace_ctx *trace,
 						  __s8 *ext_err)
 {
@@ -220,6 +221,7 @@ static __always_inline int nodeport_snat_fwd_ipv6(struct __ctx_buff *ctx,
 		goto out;
 
 apply_snat:
+	ipv6_addr_copy(saddr, &tuple.saddr);
 	ret = snat_v6_nat(ctx, &tuple, l4_off, &target, trace, ext_err);
 	if (IS_ERR(ret))
 		goto out;
@@ -1531,6 +1533,7 @@ int tail_handle_snat_fwd_ipv6(struct __ctx_buff *ctx)
 		.monitor = 0,
 	};
 	enum trace_point obs_point;
+	union v6addr saddr = {};
 	int ret;
 	__s8 ext_err = 0;
 
@@ -1540,7 +1543,7 @@ int tail_handle_snat_fwd_ipv6(struct __ctx_buff *ctx)
 	obs_point = TRACE_TO_NETWORK;
 #endif
 
-	ret = nodeport_snat_fwd_ipv6(ctx, &trace, &ext_err);
+	ret = nodeport_snat_fwd_ipv6(ctx, &saddr, &trace, &ext_err);
 	if (IS_ERR(ret))
 		return send_drop_notify_error_ext(ctx, 0, ret, ext_err,
 						  CTX_ACT_DROP, METRIC_EGRESS);
@@ -1553,7 +1556,8 @@ int tail_handle_snat_fwd_ipv6(struct __ctx_buff *ctx)
 	 * for IPv6, and so it's not possible yet for masqueraded traffic to get
 	 * redirected to another interface
 	 */
-	send_trace_notify(ctx, obs_point, 0, 0, 0, 0, trace.reason, trace.monitor);
+	send_trace_notify6(ctx, obs_point, 0, 0, &saddr, 0, 0,
+			   trace.reason, trace.monitor);
 
 	return ret;
 }
@@ -1661,6 +1665,7 @@ nodeport_has_nat_conflict_ipv4(const struct iphdr *ip4 __maybe_unused,
 
 static __always_inline int nodeport_snat_fwd_ipv4(struct __ctx_buff *ctx,
 						  __u32 cluster_id __maybe_unused,
+						  __be32 *saddr,
 						  struct trace_ctx *trace,
 						  __s8 *ext_err)
 {
@@ -1694,6 +1699,7 @@ static __always_inline int nodeport_snat_fwd_ipv4(struct __ctx_buff *ctx,
 		goto out;
 
 apply_snat:
+	*saddr = tuple.saddr;
 	ret = snat_v4_nat(ctx, &tuple, ip4, l4_off, ipv4_has_l4_header(ip4),
 			  &target, trace, ext_err);
 	if (IS_ERR(ret))
@@ -3082,6 +3088,7 @@ int tail_handle_snat_fwd_ipv4(struct __ctx_buff *ctx)
 		.monitor = 0,
 	};
 	enum trace_point obs_point;
+	__be32 saddr = 0;
 	int ret;
 	__s8 ext_err = 0;
 
@@ -3093,7 +3100,7 @@ int tail_handle_snat_fwd_ipv4(struct __ctx_buff *ctx)
 	obs_point = TRACE_TO_NETWORK;
 #endif
 
-	ret = nodeport_snat_fwd_ipv4(ctx, cluster_id, &trace, &ext_err);
+	ret = nodeport_snat_fwd_ipv4(ctx, cluster_id, &saddr, &trace, &ext_err);
 	if (IS_ERR(ret))
 		return send_drop_notify_error_ext(ctx, 0, ret, ext_err,
 						  CTX_ACT_DROP, METRIC_EGRESS);
@@ -3104,7 +3111,8 @@ int tail_handle_snat_fwd_ipv4(struct __ctx_buff *ctx)
 	 * the interface to which the egress IP is assigned to.
 	 */
 	if (ret == CTX_ACT_OK)
-		send_trace_notify(ctx, obs_point, 0, 0, 0, 0, trace.reason, trace.monitor);
+		send_trace_notify4(ctx, obs_point, 0, 0, saddr, 0, 0,
+				   trace.reason, trace.monitor);
 
 	return ret;
 }
