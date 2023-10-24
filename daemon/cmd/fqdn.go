cilium/cilium

commit 867e41a610fac2268cea7af8acbfd748afa64556

endpoint: Only perform full synchronization periodically
Performing a full dump of the policy map on every policy map synchronization
is expensive and is only necessary to catch rare cases where the agent's view
of the policy map has diverged from the kernel's view which should only happen
either due to kernel or other bugs or some other application modifying the
endpoint policy map.

To reduce the cost of synchronizing large endpoint policy maps perform the
full synchronization only periodically. The full synchronization is defaults
to 15 minutes. Configurable with the hidden option
"--policy-map-full-reconciliation-interval".

Fixes: 9dc1350fdb ("endpoint: Enhance policy map sync")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/endpoint/bpf.go
@@ -1277,18 +1277,24 @@ func (e *Endpoint) dumpPolicyMapToMapState() (policy.MapState, error) {
 	return currentMap, err
 }
 
-// syncPolicyMapWithDump attempts to synchronize the PolicyMap for this endpoint to
-// contain the set of PolicyKeys represented by the endpoint's desiredMapState.
-// It checks the current contents of the endpoint's PolicyMap and deletes any
-// PolicyKeys that are not present in the endpoint's desiredMapState. It then
-// adds any keys that are not present in the map. When a key from desiredMapState
-// is inserted successfully to the endpoint's BPF PolicyMap, it is added to the
-// endpoint's realizedMapState field. Returns an error if the endpoint's BPF
-// PolicyMap is unable to be dumped, or any update operation to the map fails.
+// syncPolicyMapWithDump is invoked periodically to perform a full reconciliation
+// of the endpoint's PolicyMap against the BPF maps to catch cases where either
+// due to kernel issue or user intervention the agent's view of the PolicyMap
+// state has diverged from the kernel. A warning is logged if this method finds
+// such an discrepancy.
+//
+// Returns an error if the endpoint's BPF PolicyMap is unable to be dumped,
+// or any update operation to the map fails.
 // Must be called with e.mutex Lock()ed.
 func (e *Endpoint) syncPolicyMapWithDump() error {
 	if e.policyMap == nil {
 		return fmt.Errorf("not syncing PolicyMap state for endpoint because PolicyMap is nil")
+	}
+
+	// Endpoint not yet fully initialized or currently regenerating. Skip the check
+	// this round.
+	if e.getState() != StateReady {
+		return nil
 	}
 
 	// Apply pending policy map changes first so that desired map is up-to-date before
@@ -1339,12 +1345,18 @@ func (e *Endpoint) syncPolicyMapWithDump() error {
 	return err
 }
 
-func (e *Endpoint) syncPolicyMapController() {
+const (
+	// syncPolicyMapInterval is the interval for periodic full reconciliation of
+	// the policy map to catch unexpected discrepancies with agent and kernel state.
+	syncPolicyMapInterval = 15 * time.Minute
+)
+
+func (e *Endpoint) startSyncPolicyMapController() {
 	ctrlName := fmt.Sprintf("sync-policymap-%d", e.ID)
-	e.controllers.UpdateController(ctrlName,
+	e.controllers.CreateController(ctrlName,
 		controller.ControllerParams{
 			Group: syncPolicymapControllerGroup,
-			DoFunc: func(ctx context.Context) (reterr error) {
+			DoFunc: func(ctx context.Context) error {
 				// Failure to lock is not an error, it means
 				// that the endpoint was disconnected and we
 				// should exit gracefully.
@@ -1354,7 +1366,7 @@ func (e *Endpoint) syncPolicyMapController() {
 				defer e.unlock()
 				return e.syncPolicyMapWithDump()
 			},
-			RunInterval: 1 * time.Minute,
+			RunInterval: syncPolicyMapInterval,
 			Context:     e.aliveCtx,
 		},
 	)

pkg/endpoint/policy.go
@@ -526,9 +526,10 @@ func (e *Endpoint) updateRealizedState(stats *regenerationStatistics, origDir st
 		return fmt.Errorf("error synchronizing endpoint BPF program directories: %s", err)
 	}
 
-	// Keep PolicyMap for this endpoint in sync with desired / realized state.
+	// Start periodic background full reconciliation of the policy map.
+	// Does nothing if it has already been started.
 	if !option.Config.DryMode {
-		e.syncPolicyMapController()
+		e.startSyncPolicyMapController()
 	}
 
 	if e.desiredPolicy != e.realizedPolicy {

commit e4adf6f557f6fc55f588901e97b8505d181f1b81

endpoint: Incremental policy map updates via sync-policy-map controller
The previous fix to avoid full dumping of policy map on every policy map change
(867e41a610) missed the likely accidental use of the sync-policy-map controller
for retrying failures in ApplyPolicyMapChanges.

Fix this by using the sync-policy-map controller to apply pending changes to
the endpoint policy map. Use the controller's executed channel for signalling
completion to coalesce multiple requests to apply changes into a single one to reduce overhead.

Set a minimum retry wait time of 100 millis and a maximum of 1 minute. We want to
avoid aggressive retrying when the policy map is full.

Refactor UpdatePolicyMaps to leverage the triggering behaviour and to avoid
spawning a goroutine per endpoint.

Fixes: 867e41a610 ("endpoint: Only perform full synchronization periodically")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/fqdn.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/policy"
 	"github.com/cilium/cilium/pkg/api"
+	"github.com/cilium/cilium/pkg/channels"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/fqdn"
@@ -124,11 +125,11 @@ func identitiesForFQDNSelectorIPs(selectorsWithIPsToUpdate map[policyApi.FQDNSel
 	return selectorIdentitySliceMapping, usedIdentities, newlyAllocatedIdentities, nil
 }
 
-func (d *Daemon) updateSelectorCacheFQDNs(ctx context.Context, selectors map[policyApi.FQDNSelector][]*identity.Identity, selectorsWithoutIPs []policyApi.FQDNSelector) *sync.WaitGroup {
+func (d *Daemon) updateSelectorCacheFQDNs(ctx context.Context, selectors map[policyApi.FQDNSelector][]*identity.Identity, selectorsWithoutIPs []policyApi.FQDNSelector) channels.DoneChan {
 	// There may be nothing to update - in this case, we exit and do not need
 	// to trigger policy updates for all endpoints.
 	if len(selectors) == 0 && len(selectorsWithoutIPs) == 0 {
-		return &sync.WaitGroup{}
+		return channels.ClosedDoneChan
 	}
 
 	notifyWg := &sync.WaitGroup{}
@@ -412,12 +413,12 @@ func (d *Daemon) updateDNSDatapathRules(ctx context.Context) error {
 // updateSelectors propagates the mapping of FQDNSelector to identity, as well
 // as the set of FQDNSelectors which have no IPs which correspond to them
 // (usually due to TTL expiry), down to policy layer managed by this daemon.
-func (d *Daemon) updateSelectors(ctx context.Context, selectorWithIPsToUpdate map[policyApi.FQDNSelector][]net.IP, selectorsWithoutIPs []policyApi.FQDNSelector) (wg *sync.WaitGroup, usedIdentities []*identity.Identity, newlyAllocatedIdentities map[netip.Prefix]*identity.Identity, err error) {
+func (d *Daemon) updateSelectors(ctx context.Context, selectorWithIPsToUpdate map[policyApi.FQDNSelector][]net.IP, selectorsWithoutIPs []policyApi.FQDNSelector) (done channels.DoneChan, usedIdentities []*identity.Identity, newlyAllocatedIdentities map[netip.Prefix]*identity.Identity, err error) {
 	// Convert set of selectors with IPs to update to set of selectors
 	// with identities corresponding to said IPs.
 	selectorsIdentities, usedIdentities, newlyAllocatedIdentities, err := identitiesForFQDNSelectorIPs(selectorWithIPsToUpdate, d.identityAllocator)
 	if err != nil {
-		return &sync.WaitGroup{}, nil, nil, err
+		return channels.ClosedDoneChan, nil, nil, err
 	}
 
 	// Update mapping in selector cache with new identities.
@@ -626,7 +627,7 @@ func (d *Daemon) notifyOnDNSMsg(lookupTime time.Time, ep *endpoint.Endpoint, epI
 		defer updateCancel()
 		updateStart := time.Now()
 
-		wg, usedIdentities, newlyAllocatedIdentities, err := d.dnsNameManager.UpdateGenerateDNS(updateCtx, lookupTime, map[string]*fqdn.DNSIPRecords{
+		doneChan, usedIdentities, newlyAllocatedIdentities, err := d.dnsNameManager.UpdateGenerateDNS(updateCtx, lookupTime, map[string]*fqdn.DNSIPRecords{
 			qname: {
 				IPs: responseIPs,
 				TTL: int(TTL),
@@ -637,17 +638,12 @@ func (d *Daemon) notifyOnDNSMsg(lookupTime time.Time, ep *endpoint.Endpoint, epI
 
 		stat.PolicyGenerationTime.End(true)
 		stat.DataplaneTime.Start()
-		updateComplete := make(chan struct{})
-		go func(wg *sync.WaitGroup, done chan struct{}) {
-			wg.Wait()
-			close(updateComplete)
-		}(wg, updateComplete)
 
 		select {
 		case <-updateCtx.Done():
 			log.Error("Timed out waiting for datapath updates of FQDN IP information; returning response")
 			metrics.ProxyDatapathUpdateTimeout.Inc()
-		case <-updateComplete:
+		case <-doneChan:
 		}
 
 		log.WithFields(logrus.Fields{

pkg/endpoint/bpf.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/channels"
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
@@ -1099,10 +1100,16 @@ func (e *Endpoint) addPolicyKey(keyToAdd policy.Key, entry policy.MapStateEntry,
 		err = e.policyMap.AllowKey(policymapKey, entry.AuthType.Uint8(), entry.ProxyPort)
 	}
 	if err != nil {
-		e.getLogger().WithError(err).WithFields(logrus.Fields{
-			logfields.BPFMapKey: policymapKey,
-			logfields.Port:      entry.ProxyPort,
-		}).Error("Failed to add PolicyMap key")
+		if e.logLimiter.Allow() {
+			// Rate-limiting logging of the error as errors may
+			// occur at very high rates when the policy map is
+			// full. Since these operations are retried indefinitely
+			// we don't need to worry about this going unnoticed.
+			e.getLogger().WithError(err).WithFields(logrus.Fields{
+				logfields.BPFMapKey: policymapKey,
+				logfields.Port:      entry.ProxyPort,
+			}).Error("Failed to add PolicyMap key")
+		}
 		return false
 	}
 
@@ -1120,25 +1127,19 @@ func (e *Endpoint) addPolicyKey(keyToAdd policy.Key, entry policy.MapStateEntry,
 
 // ApplyPolicyMapChanges updates the Endpoint's PolicyMap with the changes
 // that have accumulated for the PolicyMap via various outside events (e.g.,
-// identities added / deleted).
-// 'proxyWaitGroup' may not be nil.
-func (e *Endpoint) ApplyPolicyMapChanges(proxyWaitGroup *completion.WaitGroup) error {
+// identities added / deleted) by triggering the sync-policy-map controller.
+// Returns a channel that is closed when the changes are applied. The channel
+// is closed even if errors occurred. The controller will retry the changes.
+func (e *Endpoint) ApplyPolicyMapChanges() channels.DoneChan {
 	if err := e.lockAlive(); err != nil {
-		return err
+		return channels.ClosedDoneChan
 	}
 	defer e.unlock()
 
-	e.PolicyDebug(nil, "ApplyPolicyMapChanges")
-
-	err := e.applyPolicyMapChanges()
-	if err != nil {
-		return err
-	}
-
-	// Ignoring the revertFunc; keep all successful changes even if some fail.
-	err, _ = e.updateNetworkPolicy(proxyWaitGroup)
-
-	return err
+	// Trigger the sync-policy-map controller to apply the changes.
+	// Returns a channel that is closed when the controller has been
+	// executed after this call.
+	return e.updateSyncPolicyMapController()
 }
 
 // applyPolicyMapChanges applies any incremental policy map changes
@@ -1320,24 +1321,18 @@ func (e *Endpoint) dumpPolicyMapToMapState() (policy.MapState, error) {
 	return currentMap, err
 }
 
-// syncPolicyMapWithDump is invoked periodically to perform a full reconciliation
-// of the endpoint's PolicyMap against the BPF maps to catch cases where either
-// due to kernel issue or user intervention the agent's view of the PolicyMap
-// state has diverged from the kernel. A warning is logged if this method finds
-// such an discrepancy.
+// syncPolicyMapWithDump is invoked by the sync-policy-map controller to perform
+// incremental and full reconciliation of the policy map. The full reconciliation
+// is performed at an interval set by 'option.Config.PolicyMapFullReconciliationInterval'.
 //
 // Returns an error if the endpoint's BPF PolicyMap is unable to be dumped,
-// or any update operation to the map fails.
+// or any update operation to the map fails. Such errors will by retried by the sync-policy-map
+// controller.
+//
 // Must be called with e.mutex Lock()ed.
-func (e *Endpoint) syncPolicyMapWithDump() error {
+func (e *Endpoint) syncPolicyMapWithDump(ctx context.Context) error {
 	if e.policyMap == nil {
 		return fmt.Errorf("not syncing PolicyMap state for endpoint because PolicyMap is nil")
-	}
-
-	// Endpoint not yet fully initialized or currently regenerating. Skip the check
-	// this round.
-	if e.getState() != StateReady {
-		return nil
 	}
 
 	// Apply pending policy map changes first so that desired map is up-to-date before
@@ -1345,6 +1340,11 @@ func (e *Endpoint) syncPolicyMapWithDump() error {
 	err := e.applyPolicyMapChanges()
 	if err != nil {
 		return err
+	}
+
+	// Skip full reconciliation if it has been performed recently.
+	if time.Since(e.policyMapLastFullReconciliation) < option.Config.PolicyMapFullReconciliationInterval {
+		return nil
 	}
 
 	currentMap, err := e.dumpPolicyMapToMapState()
@@ -1385,31 +1385,62 @@ func (e *Endpoint) syncPolicyMapWithDump() error {
 		e.PolicyDebug(logrus.Fields{"dumpedDiffs": diffs}, "syncPolicyMapWithDump")
 	}
 
+	if err == nil {
+		e.policyMapLastFullReconciliation = time.Now()
+	}
+
 	return err
 }
 
-func (e *Endpoint) startSyncPolicyMapController() {
-	// Skip the controller if the endpoint has no policy map
-	if !e.HasBPFPolicyMap() {
-		return
+func (e *Endpoint) syncPolicyMapControllerName() string {
+	return fmt.Sprintf("sync-policymap-%d", e.ID)
+}
+
+// updateSyncPolicyMapController creates or triggers a controller to  reconcile
+// pending policy map changes. Every PolicyMapFullReconciliationInterval the
+// controller also does full reconciliation by dumping the policy map.
+func (e *Endpoint) updateSyncPolicyMapController() channels.DoneChan {
+	// Skip the controller if the endpoint has no policy map or is
+	// being disconnected.
+	if !e.HasBPFPolicyMap() || e.IsDisconnecting() {
+		return channels.ClosedDoneChan
 	}
 
-	ctrlName := fmt.Sprintf("sync-policymap-%d", e.ID)
-	e.controllers.CreateController(ctrlName,
+	return e.controllers.UpdateController(
+		e.syncPolicyMapControllerName(),
 		controller.ControllerParams{
 			Group:          syncPolicymapControllerGroup,
 			HealthReporter: e.GetReporter("policy map sync"),
 			DoFunc: func(ctx context.Context) error {
-				// that the endpoint was disconnected and we
-				// should exit gracefully.
-				if err := e.lockAlive(); err != nil {
+				e.unconditionalLock()
+				defer e.unlock()
+				if e.IsDisconnecting() {
 					return controller.NewExitReason("Endpoint disappeared")
 				}
-				defer e.unlock()
-				return e.syncPolicyMapWithDump()
+
+				err := e.syncPolicyMapWithDump(ctx)
+				if err != nil {
+					return err
+				}
+
+				// Apply policy changes to L7 proxy
+				proxyWaitGroup := completion.NewWaitGroup(ctx)
+
+				// Ignoring the revertFunc; keep all successful changes even if some fail.
+				if err, _ = e.updateNetworkPolicy(proxyWaitGroup); err != nil {
+					e.getLogger().WithError(err).Warn("failed to publish policy update to L7 proxy")
+				}
+				proxyWaitGroup.Wait()
+
+				return nil
 			},
-			RunInterval: option.Config.PolicyMapFullReconciliationInterval,
-			Context:     e.aliveCtx,
+			StopFunc: func(ctx context.Context) error {
+				return nil
+			},
+			RunInterval:            option.Config.PolicyMapFullReconciliationInterval,
+			MaxRetryInterval:       time.Minute,
+			ErrorRetryBaseDuration: 100 * time.Millisecond,
+			Context:                e.aliveCtx,
 		},
 	)
 }

pkg/endpoint/endpoint.go
@@ -227,6 +227,11 @@ type Endpoint struct {
 	// PolicyMapPressureUpdater updates the policymap pressure metric.
 	PolicyMapPressureUpdater policyMapPressureUpdater
 
+	// policyMapLastFullReconciliation is the timestamp of the last full
+	// policy map reconciliation, e.g. full comparison of the desired
+	// policy map state against the BPF map state.
+	policyMapLastFullReconciliation time.Time
+
 	// Options determine the datapath configuration of the endpoint.
 	Options *option.IntOptions
 

pkg/endpoint/policy.go
@@ -531,10 +531,9 @@ func (e *Endpoint) updateRealizedState(stats *regenerationStatistics, origDir st
 		return fmt.Errorf("error synchronizing endpoint BPF program directories: %s", err)
 	}
 
-	// Start periodic background full reconciliation of the policy map.
-	// Does nothing if it has already been started.
+	// Trigger reconciliation of the policy map.
 	if !option.Config.DryMode {
-		e.startSyncPolicyMapController()
+		e.updateSyncPolicyMapController()
 	}
 
 	if e.desiredPolicy != e.realizedPolicy {

pkg/endpointmanager/cell.go
@@ -9,6 +9,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/cilium/cilium/pkg/channels"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 	"github.com/cilium/cilium/pkg/hive"
@@ -136,11 +137,11 @@ type EndpointManager interface {
 	// Unsubscribe from endpoint events.
 	Unsubscribe(s Subscriber)
 
-	// UpdatePolicyMaps returns a WaitGroup which is signaled upon once all endpoints
+	// UpdatePolicyMaps returns a channel which is closed once all endpoints
 	// have had their PolicyMaps updated against the Endpoint's desired policy state.
 	//
-	// Endpoints will wait on the 'notifyWg' parameter before updating policy maps.
-	UpdatePolicyMaps(ctx context.Context, notifyWg *sync.WaitGroup) *sync.WaitGroup
+	// Will wait on the 'notifyWg' parameter before updating policy maps.
+	UpdatePolicyMaps(ctx context.Context, notifyWg *sync.WaitGroup) channels.DoneChan
 
 	// RegenerateAllEndpoints calls a setState for each endpoint and
 	// regenerates if state transaction is valid. During this process, the endpoint

pkg/endpointmanager/manager.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/cilium/cilium/pkg/completion"
+	"github.com/cilium/cilium/pkg/channels"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint"
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
@@ -133,62 +133,36 @@ func (mgr *endpointManager) WithPeriodicEndpointGC(ctx context.Context, checkHea
 	return mgr
 }
 
-// waitForProxyCompletions blocks until all proxy changes have been completed.
-func waitForProxyCompletions(proxyWaitGroup *completion.WaitGroup) error {
-	err := proxyWaitGroup.Context().Err()
-	if err != nil {
-		return fmt.Errorf("context cancelled before waiting for proxy updates: %s", err)
-	}
-
-	start := time.Now()
-	log.Debug("Waiting for proxy updates to complete...")
-	err = proxyWaitGroup.Wait()
-	if err != nil {
-		return fmt.Errorf("proxy updates failed: %s", err)
-	}
-	log.Debug("Wait time for proxy updates: ", time.Since(start))
-
-	return nil
-}
-
-// UpdatePolicyMaps returns a WaitGroup which is signaled upon once all endpoints
+// UpdatePolicyMaps returns a channel which is signaled upon once all endpoints
 // have had their PolicyMaps updated against the Endpoint's desired policy state.
 //
 // Endpoints will wait on the 'notifyWg' parameter before updating policy maps.
-func (mgr *endpointManager) UpdatePolicyMaps(ctx context.Context, notifyWg *sync.WaitGroup) *sync.WaitGroup {
-	var epWG sync.WaitGroup
-	var wg sync.WaitGroup
+func (mgr *endpointManager) UpdatePolicyMaps(ctx context.Context, notifyWg *sync.WaitGroup) channels.DoneChan {
+	done := make(chan struct{})
 
-	proxyWaitGroup := completion.NewWaitGroup(ctx)
-
-	eps := mgr.GetEndpoints()
-	epWG.Add(len(eps))
-	wg.Add(1)
-
-	// This is in a goroutine to allow the caller to proceed with other tasks before waiting for the ACKs to complete
 	go func() {
-		// Wait for all the eps to have applied policy map
-		// changes before waiting for the changes to be ACKed
-		epWG.Wait()
-		if err := waitForProxyCompletions(proxyWaitGroup); err != nil {
-			log.WithError(err).Warning("Failed to apply L7 proxy policy changes. These will be re-applied in future updates.")
+		defer close(done)
+
+		// Wait until caller has updated the desired policy state.
+		notifyWg.Wait()
+
+		eps := mgr.GetEndpoints()
+		doneChannels := make([]<-chan struct{}, len(eps))
+		// Trigger applying of the policy map changes.
+		for i, ep := range eps {
+			doneChannels[i] = ep.ApplyPolicyMapChanges()
 		}
-		wg.Done()
+		// Wait for completion.
+		for _, done := range doneChannels {
+			select {
+			case <-ctx.Done():
+				return
+			case <-done:
+			}
+		}
 	}()
 
-	// TODO: bound by number of CPUs?
-	for _, ep := range eps {
-		go func(ep *endpoint.Endpoint) {
-			// Proceed only after all notifications have been delivered to endpoints
-			notifyWg.Wait()
-			if err := ep.ApplyPolicyMapChanges(proxyWaitGroup); err != nil {
-				ep.Logger("endpointmanager").WithError(err).Warning("Failed to apply policy map changes. These will be re-applied in future updates.")
-			}
-			epWG.Done()
-		}(ep)
-	}
-
-	return &wg
+	return done
 }
 
 // InitMetrics hooks the endpointManager into the metrics subsystem. This can

pkg/fqdn/config.go
@@ -7,8 +7,8 @@ import (
 	"context"
 	"net"
 	"net/netip"
-	"sync"
 
+	"github.com/cilium/cilium/pkg/channels"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/policy/api"
 )
@@ -25,5 +25,5 @@ type Config struct {
 
 	// UpdateSelectors is a callback to update the mapping of FQDNSelector to
 	// sets of IPs.
-	UpdateSelectors func(ctx context.Context, selectorsWithIPs map[api.FQDNSelector][]net.IP, selectorsWithoutIPs []api.FQDNSelector) (*sync.WaitGroup, []*identity.Identity, map[netip.Prefix]*identity.Identity, error)
+	UpdateSelectors func(ctx context.Context, selectorsWithIPs map[api.FQDNSelector][]net.IP, selectorsWithoutIPs []api.FQDNSelector) (channels.DoneChan, []*identity.Identity, map[netip.Prefix]*identity.Identity, error)
 }

pkg/fqdn/name_manager.go
@@ -8,12 +8,12 @@ import (
 	"net"
 	"net/netip"
 	"regexp"
-	"sync"
 	"time"
 
 	"github.com/sirupsen/logrus"
 
 	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/channels"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/lock"
@@ -117,8 +117,8 @@ func NewNameManager(config Config) *NameManager {
 	}
 
 	if config.UpdateSelectors == nil {
-		config.UpdateSelectors = func(ctx context.Context, selectorsWithIPs map[api.FQDNSelector][]net.IP, selectorsWithoutIPs []api.FQDNSelector) (*sync.WaitGroup, []*identity.Identity, map[netip.Prefix]*identity.Identity, error) {
-			return &sync.WaitGroup{}, nil, nil, nil
+		config.UpdateSelectors = func(ctx context.Context, selectorsWithIPs map[api.FQDNSelector][]net.IP, selectorsWithoutIPs []api.FQDNSelector) (channels.DoneChan, []*identity.Identity, map[netip.Prefix]*identity.Identity, error) {
+			return channels.ClosedDoneChan, nil, nil, nil
 		}
 	}
 
@@ -137,7 +137,7 @@ func (n *NameManager) GetDNSCache() *DNSCache {
 
 // UpdateGenerateDNS inserts the new DNS information into the cache. If the IPs
 // have changed for a name they will be reflected in updatedDNSIPs.
-func (n *NameManager) UpdateGenerateDNS(ctx context.Context, lookupTime time.Time, updatedDNSIPs map[string]*DNSIPRecords) (wg *sync.WaitGroup, usedIdentities []*identity.Identity, newlyAllocatedIdentities map[netip.Prefix]*identity.Identity, err error) {
+func (n *NameManager) UpdateGenerateDNS(ctx context.Context, lookupTime time.Time, updatedDNSIPs map[string]*DNSIPRecords) (done channels.DoneChan, usedIdentities []*identity.Identity, newlyAllocatedIdentities map[netip.Prefix]*identity.Identity, err error) {
 	n.RWMutex.Lock()
 	defer n.RWMutex.Unlock()
 
@@ -165,7 +165,7 @@ func (n *NameManager) UpdateGenerateDNS(ctx context.Context, lookupTime time.Tim
 // matchNames that match them will cause these rules to regenerate.
 // Note: This is used only when DNS entries are cleaned up, not when new results
 // are ingested.
-func (n *NameManager) ForceGenerateDNS(ctx context.Context, namesToRegen []string) (wg *sync.WaitGroup, err error) {
+func (n *NameManager) ForceGenerateDNS(ctx context.Context, namesToRegen []string) (done <-chan struct{}, err error) {
 	n.RWMutex.Lock()
 	defer n.RWMutex.Unlock()
 
@@ -186,8 +186,8 @@ func (n *NameManager) ForceGenerateDNS(ctx context.Context, namesToRegen []strin
 
 	// Emit the new rules.
 	// Ignore newly allocated IDs (3rd result) as this is only used for deletes.
-	wg, _, _, err = n.config.UpdateSelectors(ctx, selectorIPMapping, namesMissingIPs)
-	return wg, err
+	done, _, _, err = n.config.UpdateSelectors(ctx, selectorIPMapping, namesMissingIPs)
+	return done, err
 }
 
 func (n *NameManager) CompleteBootstrap() {

pkg/ipcache/metadata.go
@@ -429,8 +429,7 @@ func (ipc *IPCache) UpdatePolicyMaps(ctx context.Context, addedIdentities, delet
 	if addedIdentities != nil {
 		ipc.PolicyHandler.UpdateIdentities(addedIdentities, nil, &wg)
 	}
-	policyImplementedWG := ipc.DatapathHandler.UpdatePolicyMaps(ctx, &wg)
-	policyImplementedWG.Wait()
+	<-ipc.DatapathHandler.UpdatePolicyMaps(ctx, &wg)
 }
 
 // resolveIdentity will either return a previously-allocated identity for the

pkg/ipcache/types/types.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/cilium/cilium/pkg/channels"
 	"github.com/cilium/cilium/pkg/identity/cache"
 )
 
@@ -28,7 +29,7 @@ type PolicyHandler interface {
 // Wait on the returned sync.WaitGroup to ensure that the operation is complete
 // before updating the datapath's IPCache maps.
 type DatapathHandler interface {
-	UpdatePolicyMaps(context.Context, *sync.WaitGroup) *sync.WaitGroup
+	UpdatePolicyMaps(context.Context, *sync.WaitGroup) channels.DoneChan
 }
 
 // ResourceID identifies a unique copy of a resource that provides a source for

pkg/k8s/watchers/watcher.go
@@ -133,7 +133,6 @@ type endpointManager interface {
 	GetHostEndpoint() *endpoint.Endpoint
 	GetEndpointsByPodName(string) []*endpoint.Endpoint
 	WaitForEndpointsAtPolicyRev(ctx context.Context, rev uint64) error
-	UpdatePolicyMaps(context.Context, *sync.WaitGroup) *sync.WaitGroup
 }
 
 type nodeDiscoverManager interface {

pkg/fqdn/name_manager_test.go
@@ -7,11 +7,11 @@ import (
 	"context"
 	"net"
 	"net/netip"
-	"sync"
 	"time"
 
 	. "github.com/cilium/checkmate"
 
+	"github.com/cilium/cilium/pkg/channels"
 	"github.com/cilium/cilium/pkg/fqdn/dns"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/policy/api"
@@ -30,11 +30,11 @@ func (ds *FQDNTestSuite) TestNameManagerCIDRGeneration(c *C) {
 			MinTTL: 1,
 			Cache:  NewDNSCache(0),
 
-			UpdateSelectors: func(ctx context.Context, selectorIPMapping map[api.FQDNSelector][]net.IP, selectorsWithoutIPs []api.FQDNSelector) (*sync.WaitGroup, []*identity.Identity, map[netip.Prefix]*identity.Identity, error) {
+			UpdateSelectors: func(ctx context.Context, selectorIPMapping map[api.FQDNSelector][]net.IP, selectorsWithoutIPs []api.FQDNSelector) (channels.DoneChan, []*identity.Identity, map[netip.Prefix]*identity.Identity, error) {
 				for k, v := range selectorIPMapping {
 					selIPMap[k] = v
 				}
-				return &sync.WaitGroup{}, nil, nil, nil
+				return channels.ClosedDoneChan, nil, nil, nil
 			},
 		})
 	)
@@ -76,11 +76,11 @@ func (ds *FQDNTestSuite) TestNameManagerMultiIPUpdate(c *C) {
 			MinTTL: 1,
 			Cache:  NewDNSCache(0),
 
-			UpdateSelectors: func(ctx context.Context, selectorIPMapping map[api.FQDNSelector][]net.IP, selectorsWithoutIPs []api.FQDNSelector) (*sync.WaitGroup, []*identity.Identity, map[netip.Prefix]*identity.Identity, error) {
+			UpdateSelectors: func(ctx context.Context, selectorIPMapping map[api.FQDNSelector][]net.IP, selectorsWithoutIPs []api.FQDNSelector) (<-chan struct{}, []*identity.Identity, map[netip.Prefix]*identity.Identity, error) {
 				for k, v := range selectorIPMapping {
 					selIPMap[k] = v
 				}
-				return &sync.WaitGroup{}, nil, nil, nil
+				return channels.ClosedDoneChan, nil, nil, nil
 			},
 		})
 	)

pkg/ipcache/metadata_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/cilium/cilium/pkg/channels"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/ipcache/types"
@@ -729,6 +730,6 @@ func (m *mockUpdater) UpdateIdentities(added, deleted cache.IdentityCache, _ *sy
 
 type mockTriggerer struct{}
 
-func (m *mockTriggerer) UpdatePolicyMaps(ctx context.Context, wg *sync.WaitGroup) *sync.WaitGroup {
-	return wg
+func (m *mockTriggerer) UpdatePolicyMaps(ctx context.Context, wg *sync.WaitGroup) channels.DoneChan {
+	return channels.ClosedDoneChan
 }