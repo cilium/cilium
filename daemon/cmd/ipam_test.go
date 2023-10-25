51dd8028e1e917a829b9f473e97822d61ad380ee
Sebastian Wicki <sebastian@isovalent.com>
2023-10-25 13:55:14 +0200

    ipam: Add unit test for `reallocateDatapathIPs`
    This slightly modifies the `reallocateDatapathIPs` function to take a
    mock allocator as its first argument, and adds a basic unit test which
    tests for the precedence logic we want.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- daemon/cmd/ipam_test.go
+++ daemon/cmd/ipam_test.go
@@ -4,7 +4,16 @@
 package cmd
 
 import (
+	"fmt"
+	"net"
 	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/cilium/cilium/pkg/cidr"
+	"github.com/cilium/cilium/pkg/ipam"
+	"github.com/cilium/cilium/pkg/logging"
 )
 
 func TestCoalesceCIDRs(t *testing.T) {
@@ -63,4 +72,66 @@ func TestCoalesceCIDRs(t *testing.T) {
 	if len(newCIDR) != len(expectedCIDR) || newCIDR[0] != expectedCIDR[0] {
 		t.Errorf("got %v, want %v\n", newCIDR, expectedCIDR)
 	}
+}
+
+type mockAllocateIP func(ip net.IP, owner string, pool ipam.Pool) (*ipam.AllocationResult, error)
+
+func (m mockAllocateIP) AllocateIPWithoutSyncUpstream(ip net.IP, owner string, pool ipam.Pool) (*ipam.AllocationResult, error) {
+	return m(ip, owner, pool)
+}
+
+func TestDaemon_reallocateDatapathIPs(t *testing.T) {
+	level := logging.GetLevel(logging.DefaultLogger)
+	logging.SetLogLevel(logrus.FatalLevel)
+	logging.AddHooks()
+	t.Cleanup(func() {
+		logging.SetLogLevel(level)
+	})
+
+	allocCIDR := cidr.MustParseCIDR("10.20.30.0/24")
+	alloc := mockAllocateIP(func(ip net.IP, owner string, pool ipam.Pool) (*ipam.AllocationResult, error) {
+		if !allocCIDR.Contains(ip) {
+			return nil, fmt.Errorf("cannot allocate IP %s", ip)
+		}
+		return &ipam.AllocationResult{IP: ip}, nil
+	})
+
+	fromFS := net.ParseIP("10.20.30.42")
+	fromK8s := net.ParseIP("10.20.30.41")
+
+	invalidFromFS := net.ParseIP("172.16.0.42")
+	invalidFromK8s := net.ParseIP("172.16.0.41")
+
+	// no restoration needed
+	result := reallocateDatapathIPs(alloc, nil, nil)
+	assert.Nil(t, result)
+
+	// fromK8s if fromFS is not available
+	result = reallocateDatapathIPs(alloc, fromK8s, nil)
+	assert.NotNil(t, result)
+	assert.Equal(t, result.IP, fromK8s)
+
+	// fromFS if fromK8s is not available
+	result = reallocateDatapathIPs(alloc, nil, fromFS)
+	assert.NotNil(t, result)
+	assert.Equal(t, result.IP, fromFS)
+
+	// fromFS should be preferred
+	result = reallocateDatapathIPs(alloc, fromK8s, fromFS)
+	assert.NotNil(t, result)
+	assert.Equal(t, result.IP, fromFS)
+
+	// reject restoration if the IP is not in the allocation CIDR
+	result = reallocateDatapathIPs(alloc, invalidFromFS, invalidFromK8s)
+	assert.Nil(t, result)
+
+	// fromFS with invalid fromK8s
+	result = reallocateDatapathIPs(alloc, invalidFromK8s, fromFS)
+	assert.NotNil(t, result)
+	assert.Equal(t, result.IP, fromFS)
+
+	// fromFS with invalid fromK8s
+	result = reallocateDatapathIPs(alloc, fromK8s, invalidFromFS)
+	assert.NotNil(t, result)
+	assert.Equal(t, result.IP, fromK8s)
 }
