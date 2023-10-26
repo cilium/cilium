2204989705808b666152678b88fc3b2460a58a0b
Jussi Maki <jussi@isovalent.com>
2023-10-26 11:10:25 +0300

    datapath/fake: Fix up the fake
    Provide and populate the devices table. Still TBD is to remove
    the fake node addressing implementation and instead use the real
    one.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/datapath/fake/cells.go
+++ pkg/datapath/fake/cells.go
@@ -4,14 +4,18 @@
 package fake
 
 import (
+	"net"
 	"time"
 
 	"github.com/cilium/cilium/pkg/datapath/tables"
 	"github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/maps/authmap"
 	"github.com/cilium/cilium/pkg/maps/egressmap"
 	"github.com/cilium/cilium/pkg/maps/signalmap"
+	"github.com/cilium/cilium/pkg/statedb"
+	"golang.org/x/sys/unix"
 
 	fakeauthmap "github.com/cilium/cilium/pkg/maps/authmap/fake"
 	fakesignalmap "github.com/cilium/cilium/pkg/maps/signalmap/fake"
@@ -19,7 +23,7 @@ import (
 
 // Cell provides a fake version of the datapath cell.
 //
-// Used in integration tests such as test/controlplane.
+// Used in integration tests in daemon/cmd and test/controlplane.
 var Cell = cell.Module(
 	"fake-datapath",
 	"Fake Datapath",
@@ -39,4 +43,28 @@ var Cell = cell.Module(
 	// between datapath components and more high-level components.
 	tables.Cell,
 	tables.DeviceTableCell,
+	cell.Provide(fakeDevices),
 )
+
+func fakeDevices(db *statedb.DB, devices statedb.RWTable[*tables.Device]) statedb.Table[*tables.Device] {
+	txn := db.WriteTxn(devices)
+	defer txn.Commit()
+
+	// FIXME: Remove "FakeNodeAddressing" and instead derive it from the devices here.
+	devices.Insert(txn, &tables.Device{
+		Index:        1,
+		MTU:          1500,
+		Name:         "test",
+		HardwareAddr: []byte{1, 2, 3, 4, 5, 6},
+		Flags:        net.FlagUp,
+		Addrs: []tables.DeviceAddress{
+			{Addr: ip.MustAddrFromIP(IPv4NodePortAddress), Scope: unix.RT_SCOPE_UNIVERSE},
+			{Addr: ip.MustAddrFromIP(IPv4InternalAddress), Scope: unix.RT_SCOPE_SITE},
+			{Addr: ip.MustAddrFromIP(IPv6NodePortAddress), Scope: unix.RT_SCOPE_UNIVERSE},
+			{Addr: ip.MustAddrFromIP(IPv6InternalAddress), Scope: unix.RT_SCOPE_SITE},
+		},
+		Type:     "test",
+		Selected: true,
+	})
+	return devices
+}
