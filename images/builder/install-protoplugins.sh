10eb37a1a0430d3528e0aed114cf869ee6d148e3
David Bimmler <david.bimmler@isovalent.com>
2023-11-02 11:52:15 +0100

    Revert "Add deepcopy plugin"
    This reverts commit 9f4dcea6b2dadab1a659b6a07b07eb8b5f48e86f.
    
    Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -13,4 +13,3 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@8ba23be9613c672d40ae261
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.31.0
 go install github.com/mitchellh/protoc-gen-go-json@49905733154f04e47d685de62c2cc2b72613b69e
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
-go install github.com/protobuf-tools/protoc-gen-deepcopy@v0.0.3
