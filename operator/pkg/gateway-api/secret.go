30994e359ab7886545fa42d40870fda4945fa641
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-11-03 13:43:49 +0100

    gateway-api: watch synced secrets for delete & update events
    Currently, Gateway API secret sync reconciliation isn't triggered
    when a synced k8s Secret in the secrets namespace (defaults to
    `cilium-secrets`) gets deleted or updated.
    
    Therefore, this commit introduces an additional watcher that
    watches for updated & deleted secrets and triggers a reconciliation
    of the owning Secret by using the labels
    `io.cilium.gateway/owning-secret-namespace` &
    `io.cilium.gateway/owning-secret-name` on the synced Secret.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- operator/pkg/gateway-api/secret.go
+++ operator/pkg/gateway-api/secret.go
@@ -13,6 +13,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -49,7 +50,10 @@ func newSecretSyncReconciler(mgr ctrl.Manager, secretsNamespace string) *secretS
 func (r *secretSyncer) SetupWithManager(mgr ctrl.Manager) error {
 	hasMatchingControllerFn := hasMatchingController(context.Background(), r.Client, r.controllerName)
 	return ctrl.NewControllerManagedBy(mgr).
+		// Source Secrets outside of the secrets namespace
 		For(&corev1.Secret{}, r.notInSecretsNamespace()).
+		// Synced Secrets in the secrets namespace
+		Watches(&corev1.Secret{}, enqueueOwningSecretFromLabels(), r.deletedOrChangedInSecretsNamespace()).
 		Watches(&gatewayv1.Gateway{}, r.enqueueRequestForGatewayTLS(), builder.WithPredicates(predicate.NewPredicateFuncs(hasMatchingControllerFn))).
 		Complete(r)
 }
@@ -91,4 +95,58 @@ func (r *secretSyncer) enqueueRequestForGatewayTLS() handler.EventHandler {
 		}
 		return reqs
 	})
+}
+
+func enqueueOwningSecretFromLabels() handler.EventHandler {
+	return handler.EnqueueRequestsFromMapFunc(func(_ context.Context, o client.Object) []reconcile.Request {
+		labels := o.GetLabels()
+
+		if labels == nil {
+			return nil
+		}
+
+		owningSecretNamespace, owningSecretNamespacePresent := labels[owningSecretNamespace]
+		owningSecretName, owningSecretNamePresent := labels[owningSecretName]
+
+		if !owningSecretNamespacePresent || !owningSecretNamePresent {
+			return nil
+		}
+
+		return []reconcile.Request{
+			{
+				NamespacedName: types.NamespacedName{
+					Namespace: owningSecretNamespace,
+					Name:      owningSecretName,
+				},
+			},
+		}
+	})
+}
+
+func (r *secretSyncer) deletedOrChangedInSecretsNamespace() builder.Predicates {
+	return builder.WithPredicates(&deletedOrChangedInSecretsNamespaceStruct{
+		secretsNamespace: r.SecretsNamespace,
+	})
+}
+
+var _ predicate.Predicate = &deletedOrChangedInSecretsNamespaceStruct{}
+
+type deletedOrChangedInSecretsNamespaceStruct struct {
+	secretsNamespace string
+}
+
+func (r *deletedOrChangedInSecretsNamespaceStruct) Create(event.CreateEvent) bool {
+	return false
+}
+
+func (r *deletedOrChangedInSecretsNamespaceStruct) Update(event event.UpdateEvent) bool {
+	return event.ObjectOld.GetNamespace() == r.secretsNamespace
+}
+
+func (r *deletedOrChangedInSecretsNamespaceStruct) Delete(event event.DeleteEvent) bool {
+	return event.Object.GetNamespace() == r.secretsNamespace
+}
+
+func (r *deletedOrChangedInSecretsNamespaceStruct) Generic(event.GenericEvent) bool {
+	return false
 }
