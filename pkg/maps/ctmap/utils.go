04413f2e42530ddac3cee46eda5290ca36e6b7b7
Julian Wiedmann <jwi@isovalent.com>
2023-11-05 16:24:31 +0700

    ctmap: consider CT entry's .dsr flag in PurgeOrphanNATEntries()
    [ upstream commit dfbae95eb544fc218a9cb251edd281afac6a96c2 ]
    
    The BPF datapath potentially re-creates a CT entry, in particular when a
    DSR connection gets re-opened as local connection. Such a re-purposed CT
    entry then leaves a DSR NAT entry behind.
    
    Currently we wouldn't clean up such NAT entries (as the matching CT entry
    still exists). But once we look at the CT entry's .dsr flag, we understand
    that the CT entry is actually no longer a match for the NAT entry.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- pkg/maps/ctmap/utils.go
+++ pkg/maps/ctmap/utils.go
@@ -172,7 +172,16 @@ func egressCTKeyFromEgressNatKey(k nat.NatKey) bpf.MapKey {
 	}
 }
 
-func ctEntryExist(ctMap *Map, ctKey bpf.MapKey) bool {
-	_, err := ctMap.Lookup(ctKey)
-	return !errors.Is(err, unix.ENOENT)
+func ctEntryExist(ctMap *Map, ctKey bpf.MapKey, f func(*CtEntry) bool) bool {
+	v, err := ctMap.Lookup(ctKey)
+
+	if err != nil {
+		return !errors.Is(err, unix.ENOENT)
+	}
+
+	if f == nil {
+		return true
+	}
+
+	return f(v.(*CtEntry))
 }
