70ae8d0ef536de807aab849291e5a68758cb8d4d
Rastislav Szabo <rastislav.szabo@isovalent.com>
2023-11-07 10:39:14 +0100

    bugtool: Include `cilium-dbg bgp route-policies` command output
    Includes BGP route policies output in the bugtool to have this
    BGP Control Plane information included in the sysdumps.
    
    Signed-off-by: Rastislav Szabo <rastislav.szabo@isovalent.com>

--- bugtool/cmd/configuration.go
+++ bugtool/cmd/configuration.go
@@ -435,6 +435,7 @@ func copyCiliumInfoCommands(cmdDir string, k8sPods []string) []string {
 		"cilium-dbg bgp peers",
 		"cilium-dbg bgp routes available ipv4 unicast",
 		"cilium-dbg bgp routes available ipv6 unicast",
+		"cilium-dbg bgp route-policies",
 	}
 	var commands []string
 
