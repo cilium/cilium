cilium/cilium

commit 1e41011ee983ceefe19a0c6080be7646faaa74e2

api, cmd, policy: Show rule labels in `policy selectors` output
Use the functionality implemented in previous commits to output the rule
labels in the selector output. This is useful for understanding which
policy the selector comes from, which makes debugging issues much
easier.

Example output:

```
root@kind-worker:/home/cilium# cilium policy selectors
SELECTOR                                                                                                                                                            LABELS                          USERS   IDENTITIES
&LabelSelector{MatchLabels:map[string]string{k8s.io.kubernetes.pod.namespace: kube-system,k8s.k8s-app: kube-dns,},MatchExpressions:[]LabelSelectorRequirement{},}   default/tofqdn-dns-visibility   1       16500
&LabelSelector{MatchLabels:map[string]string{reserved.none: ,},MatchExpressions:[]LabelSelectorRequirement{},}                                                      default/tofqdn-dns-visibility   1
MatchName: , MatchPattern: *                                                                                                                                        default/tofqdn-dns-visibility   1       16777217
                                                                                                                                                                                                            16777218
                                                                                                                                                                                                            16777219
```

Signed-off-by: Chris Tarazi <chris@isovalent.com>

api/v1/models/selector_identity_mapping.go
@@ -23,6 +23,9 @@ type SelectorIdentityMapping struct {
 	// identities mapping to this selector
 	Identities []int64 `json:"identities"`
 
+	// Labels are the metadata labels associated with the selector
+	Labels interface{} `json:"labels,omitempty"`
+
 	// string form of selector
 	Selector string `json:"selector,omitempty"`
 

api/v1/openapi.yaml
@@ -2864,6 +2864,9 @@ definitions:
       selector:
         description: string form of selector
         type: string
+      labels:
+        description: Labels are the metadata labels associated with the selector
+        type: object
       identities:
         description: identities mapping to this selector
         type: array

pkg/policy/selectorcache.go
@@ -277,6 +277,7 @@ func (sc *SelectorCache) GetModel() models.SelectorCache {
 			Selector:   selector,
 			Identities: ids,
 			Users:      int64(idSel.numUsers()),
+			Labels:     idSel.GetMetadataLabels(),
 		}
 		selCacheMdl = append(selCacheMdl, selMdl)
 	}

api/v1/server/embedded_spec.go
@@ -4630,6 +4630,10 @@ func init() {
             "type": "integer"
           }
         },
+        "labels": {
+          "description": "Labels are the metadata labels associated with the selector",
+          "type": "object"
+        },
         "selector": {
           "description": "string form of selector",
           "type": "string"
@@ -10525,6 +10529,10 @@ func init() {
           "items": {
             "type": "integer"
           }
+        },
+        "labels": {
+          "description": "Labels are the metadata labels associated with the selector",
+          "type": "object"
         },
         "selector": {
           "description": "string form of selector",

cilium/cmd/policy_selectors.go
@@ -12,7 +12,11 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cilium/cilium/pkg/command"
+	k8sconst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	"github.com/cilium/cilium/pkg/labels"
 )
+
+var verbosePolicySelectors bool
 
 // policyCacheGetCmd represents the policy selectors command
 var policyCacheGetCmd = &cobra.Command{
@@ -31,10 +35,22 @@ var policyCacheGetCmd = &cobra.Command{
 			sort.Slice(resp, func(i, j int) bool {
 				return resp[i].Selector < resp[j].Selector
 			})
-			fmt.Fprintf(w, "SELECTOR\tUSERS\tIDENTITIES\n")
+			fmt.Fprintf(w, "SELECTOR\tLABELS\tUSERS\tIDENTITIES\n")
+
 			for _, mapping := range resp {
+				lbls := constructLabelsArrayFromAPIType(mapping.Labels)
+
 				first := true
 				fmt.Fprintf(w, "%s", mapping.Selector)
+				if verbosePolicySelectors {
+					var lstr string
+					if len(lbls) != 0 {
+						lstr = lbls.Sort().String()
+					}
+					fmt.Fprintf(w, "\t%s", lstr)
+				} else {
+					fmt.Fprintf(w, "\t%s", getNameAndNamespaceFromLabels(lbls))
+				}
 				fmt.Fprintf(w, "\t%d", mapping.Users)
 				if len(mapping.Identities) == 0 {
 					fmt.Fprintf(w, "\t\n")
@@ -44,7 +60,7 @@ var policyCacheGetCmd = &cobra.Command{
 						fmt.Fprintf(w, "\t%d\t\n", idty)
 						first = false
 					} else {
-						fmt.Fprintf(w, "\t\t%d\t\n", idty)
+						fmt.Fprintf(w, "\t\t\t%d\t\n", idty)
 					}
 				}
 			}
@@ -54,7 +70,24 @@ var policyCacheGetCmd = &cobra.Command{
 	},
 }
 
+func getNameAndNamespaceFromLabels(lbls labels.LabelArray) string {
+	ns := lbls.Get(labels.LabelSourceK8sKeyPrefix + k8sconst.PolicyLabelNamespace)
+	if ns == "" {
+		return ""
+	}
+	return ns + "/" + lbls.Get(labels.LabelSourceK8sKeyPrefix+k8sconst.PolicyLabelName)
+}
+
+func constructLabelsArrayFromAPIType(in interface{}) labels.LabelArray {
+	lbls, ok := in.(labels.LabelArray)
+	if !ok {
+		return nil
+	}
+	return lbls
+}
+
 func init() {
+	policyCacheGetCmd.Flags().BoolVarP(&verbosePolicySelectors, "verbose", "v", false, "Show the full labels")
 	PolicyCmd.AddCommand(policyCacheGetCmd)
 	command.AddOutputOption(policyCacheGetCmd)
 }

commit 15bfc5a4eb6657ef3845715aac4cf94db4115aa4

cilium-dbg, policy, api: Fix labels in policy selectors output
In the previous iteration, the labels type was an interface{} which we
would type assert to the labels.LabelArray type. Upon investigation,
this failed most of the time, but for some reason it had worked before,
as the fixed commit was eventually merged.

This commit fixes it permanently by specifying the type in the OpenAPI
spec so that a type assertion is no longer needed.

Fixes: 1e41011ee9 ("api, cmd, policy: Show rule labels in `policy
selectors` output")

Reported-by: Tom Hadlaw <tom.hadlaw@isovalent.com>
Suggested-by: Tom Hadlaw <tom.hadlaw@isovalent.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

api/v1/models/label.go
@@ -1,0 +1,59 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package models
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"context"
+
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
+)
+
+// Label Label is the Cilium's representation of a container label
+//
+// swagger:model Label
+type Label struct {
+
+	// key
+	Key string `json:"key,omitempty"`
+
+	// Source can be one of the above values (e.g. LabelSourceContainer)
+	Source string `json:"source,omitempty"`
+
+	// value
+	Value string `json:"value,omitempty"`
+}
+
+// Validate validates this label
+func (m *Label) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// ContextValidate validates this label based on context it is used
+func (m *Label) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *Label) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *Label) UnmarshalBinary(b []byte) error {
+	var res Label
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}

api/v1/models/label_array.go
@@ -1,0 +1,76 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package models
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
+)
+
+// LabelArray LabelArray is an array of labels forming a set
+//
+// swagger:model LabelArray
+type LabelArray []*Label
+
+// Validate validates this label array
+func (m LabelArray) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	for i := 0; i < len(m); i++ {
+		if swag.IsZero(m[i]) { // not required
+			continue
+		}
+
+		if m[i] != nil {
+			if err := m[i].Validate(formats); err != nil {
+				if ve, ok := err.(*errors.Validation); ok {
+					return ve.ValidateName(strconv.Itoa(i))
+				} else if ce, ok := err.(*errors.CompositeError); ok {
+					return ce.ValidateName(strconv.Itoa(i))
+				}
+				return err
+			}
+		}
+
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
+
+// ContextValidate validate this label array based on the context it is used
+func (m LabelArray) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
+	var res []error
+
+	for i := 0; i < len(m); i++ {
+
+		if m[i] != nil {
+			if err := m[i].ContextValidate(ctx, formats); err != nil {
+				if ve, ok := err.(*errors.Validation); ok {
+					return ve.ValidateName(strconv.Itoa(i))
+				} else if ce, ok := err.(*errors.CompositeError); ok {
+					return ce.ValidateName(strconv.Itoa(i))
+				}
+				return err
+			}
+		}
+
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}

api/v1/models/selector_identity_mapping.go
@@ -11,6 +11,7 @@ package models
 import (
 	"context"
 
+	"github.com/go-openapi/errors"
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
@@ -24,7 +25,7 @@ type SelectorIdentityMapping struct {
 	Identities []int64 `json:"identities"`
 
 	// Labels are the metadata labels associated with the selector
-	Labels interface{} `json:"labels,omitempty"`
+	Labels LabelArray `json:"labels,omitempty"`
 
 	// string form of selector
 	Selector string `json:"selector,omitempty"`
@@ -35,11 +36,60 @@ type SelectorIdentityMapping struct {
 
 // Validate validates this selector identity mapping
 func (m *SelectorIdentityMapping) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.validateLabels(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
 	return nil
 }
 
-// ContextValidate validates this selector identity mapping based on context it is used
+func (m *SelectorIdentityMapping) validateLabels(formats strfmt.Registry) error {
+	if swag.IsZero(m.Labels) { // not required
+		return nil
+	}
+
+	if err := m.Labels.Validate(formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("labels")
+		} else if ce, ok := err.(*errors.CompositeError); ok {
+			return ce.ValidateName("labels")
+		}
+		return err
+	}
+
+	return nil
+}
+
+// ContextValidate validate this selector identity mapping based on the context it is used
 func (m *SelectorIdentityMapping) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.contextValidateLabels(ctx, formats); err != nil {
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
+
+func (m *SelectorIdentityMapping) contextValidateLabels(ctx context.Context, formats strfmt.Registry) error {
+
+	if err := m.Labels.ContextValidate(ctx, formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("labels")
+		} else if ce, ok := err.(*errors.CompositeError); ok {
+			return ce.ValidateName("labels")
+		}
+		return err
+	}
+
 	return nil
 }
 

api/v1/openapi.yaml
@@ -2920,7 +2920,7 @@ definitions:
         type: string
       labels:
         description: Labels are the metadata labels associated with the selector
-        type: object
+        "$ref": "#/definitions/LabelArray"
       identities:
         description: identities mapping to this selector
         type: array
@@ -4060,3 +4060,19 @@ definitions:
       base64:
         description: Base64-encoded BGP path attribute in the BGP UPDATE message format
         type: string
+  Label:
+    description: Label is the Cilium's representation of a container label
+    type: object
+    properties:
+      key:
+        type: string
+      value:
+        type: string
+      source:
+        description: Source can be one of the above values (e.g. LabelSourceContainer)
+        type: string
+  LabelArray:
+    description: LabelArray is an array of labels forming a set
+    type: array
+    items:
+      "$ref": "#/definitions/Label"

cilium-dbg/cmd/policy_selectors.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/command"
 	k8sconst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	"github.com/cilium/cilium/pkg/labels"
@@ -78,10 +79,14 @@ func getNameAndNamespaceFromLabels(lbls labels.LabelArray) string {
 	return ns + "/" + lbls.Get(labels.LabelSourceK8sKeyPrefix+k8sconst.PolicyLabelName)
 }
 
-func constructLabelsArrayFromAPIType(in interface{}) labels.LabelArray {
-	lbls, ok := in.(labels.LabelArray)
-	if !ok {
-		return nil
+func constructLabelsArrayFromAPIType(in models.LabelArray) labels.LabelArray {
+	lbls := make(labels.LabelArray, 0, len(in))
+	for _, l := range in {
+		lbls = append(lbls, labels.Label{
+			Key:    l.Key,
+			Value:  l.Value,
+			Source: l.Source,
+		})
 	}
 	return lbls
 }

pkg/policy/selectorcache.go
@@ -277,12 +277,24 @@ func (sc *SelectorCache) GetModel() models.SelectorCache {
 			Selector:   selector,
 			Identities: ids,
 			Users:      int64(idSel.numUsers()),
-			Labels:     idSel.GetMetadataLabels(),
+			Labels:     labelArrayToModel(idSel.GetMetadataLabels()),
 		}
 		selCacheMdl = append(selCacheMdl, selMdl)
 	}
 
 	return selCacheMdl
+}
+
+func labelArrayToModel(arr labels.LabelArray) models.LabelArray {
+	lbls := make(models.LabelArray, 0, len(arr))
+	for _, l := range arr {
+		lbls = append(lbls, &models.Label{
+			Key:    l.Key,
+			Value:  l.Value,
+			Source: l.Source,
+		})
+	}
+	return lbls
 }
 
 func (sc *SelectorCache) handleUserNotifications() {

api/v1/server/embedded_spec.go
@@ -3843,6 +3843,29 @@ func init() {
         }
       }
     },
+    "Label": {
+      "description": "Label is the Cilium's representation of a container label",
+      "type": "object",
+      "properties": {
+        "key": {
+          "type": "string"
+        },
+        "source": {
+          "description": "Source can be one of the above values (e.g. LabelSourceContainer)",
+          "type": "string"
+        },
+        "value": {
+          "type": "string"
+        }
+      }
+    },
+    "LabelArray": {
+      "description": "LabelArray is an array of labels forming a set",
+      "type": "array",
+      "items": {
+        "$ref": "#/definitions/Label"
+      }
+    },
     "LabelConfiguration": {
       "description": "Label configuration of an endpoint",
       "type": "object",
@@ -4679,7 +4702,7 @@ func init() {
         },
         "labels": {
           "description": "Labels are the metadata labels associated with the selector",
-          "type": "object"
+          "$ref": "#/definitions/LabelArray"
         },
         "selector": {
           "description": "string form of selector",
@@ -9862,6 +9885,29 @@ func init() {
         }
       }
     },
+    "Label": {
+      "description": "Label is the Cilium's representation of a container label",
+      "type": "object",
+      "properties": {
+        "key": {
+          "type": "string"
+        },
+        "source": {
+          "description": "Source can be one of the above values (e.g. LabelSourceContainer)",
+          "type": "string"
+        },
+        "value": {
+          "type": "string"
+        }
+      }
+    },
+    "LabelArray": {
+      "description": "LabelArray is an array of labels forming a set",
+      "type": "array",
+      "items": {
+        "$ref": "#/definitions/Label"
+      }
+    },
     "LabelConfiguration": {
       "description": "Label configuration of an endpoint",
       "type": "object",
@@ -10712,7 +10758,7 @@ func init() {
         },
         "labels": {
           "description": "Labels are the metadata labels associated with the selector",
-          "type": "object"
+          "$ref": "#/definitions/LabelArray"
         },
         "selector": {
           "description": "string form of selector",