cilium/cilium

commit 299648fef30ebefab6bfc93399f005473624f703

gateway-api: Enable HTTP redirect filter
This commit is to enable HTTP redirect for port, schema and path. Small
fix for TLS filter match on server name is done to avoid the below
issue.

As part of recent changes in upstream, if scheme and port are not
specified, gateway listener port must be used.

```console
2023-08-16T11:28:20.797264246Z level=warning msg="NACK received for versions after 52 and up to 53; waiting for a version update before sending again" subsys=xds xdsAckedVersion=52 xdsClientNode="host~127.0.0.1~no-id~localdomain" xdsDetail="Error adding/updating listener(s) gateway-conformance-infra/cilium-gateway-same-namespace-with-https-listener/listener: error adding listener '127.0.0.1:10605': partial wildcards are not supported in \"server_names\"\n" xdsNonce=53 xdsStreamID=2 xdsTypeURL=type.googleapis.com/envoy.config.listener.v3.Listener
```

Signed-off-by: Tam Mach <tam.mach@cilium.io>

operator/pkg/model/translation/translator.go
@@ -188,9 +188,9 @@ func (i *defaultTranslator) getEnvoyHTTPRouteConfiguration(m *model.Model) []cil
 
 	for _, l := range m.HTTP {
 		for _, r := range l.Routes {
-			port := "insecure"
+			port := insecureHost
 			if l.TLS != nil {
-				port = "secure"
+				port = secureHost
 			}
 
 			if len(r.Hostnames) == 0 {
@@ -216,7 +216,12 @@ func (i *defaultTranslator) getEnvoyHTTPRouteConfiguration(m *model.Model) []cil
 		// Add HTTPs redirect virtual host for secure host
 		if port == insecureHost && i.enforceHTTPs {
 			for _, h := range slices.Unique(portHostName[secureHost]) {
-				vhs, _ := NewVirtualHostWithDefaults([]string{h}, true, i.hostNameSuffixMatch, hostNameRoutes[h])
+				vhs, _ := NewVirtualHostWithDefaults(hostNameRoutes[h], VirtualHostParameter{
+					HostNames:           []string{h},
+					HTTPSRedirect:       true,
+					HostNameSuffixMatch: i.hostNameSuffixMatch,
+					ListenerPort:        m.HTTP[0].Port,
+				})
 				virtualhosts = append(virtualhosts, vhs)
 				redirectedHost[h] = struct{}{}
 			}
@@ -231,7 +236,12 @@ func (i *defaultTranslator) getEnvoyHTTPRouteConfiguration(m *model.Model) []cil
 			if !exists {
 				continue
 			}
-			vhs, _ := NewVirtualHostWithDefaults([]string{h}, false, i.hostNameSuffixMatch, routes)
+			vhs, _ := NewVirtualHostWithDefaults(routes, VirtualHostParameter{
+				HostNames:           []string{h},
+				HTTPSRedirect:       false,
+				HostNameSuffixMatch: i.hostNameSuffixMatch,
+				ListenerPort:        m.HTTP[0].Port,
+			})
 			virtualhosts = append(virtualhosts, vhs)
 		}
 

operator/pkg/model/translation/gateway-api/translator_fixture_test.go
@@ -2494,6 +2494,7 @@ var requestRedirectHTTPListenersCiliumEnvoyConfig = &ciliumv2.CiliumEnvoyConfig{
 									Action: &envoy_config_route_v3.Route_Redirect{
 										Redirect: &envoy_config_route_v3.RedirectAction{
 											HostRedirect: "example.com",
+											PortRedirect: 80,
 										},
 									},
 								},
@@ -2504,7 +2505,9 @@ var requestRedirectHTTPListenersCiliumEnvoyConfig = &ciliumv2.CiliumEnvoyConfig{
 										},
 									},
 									Action: &envoy_config_route_v3.Route_Redirect{
-										Redirect: &envoy_config_route_v3.RedirectAction{},
+										Redirect: &envoy_config_route_v3.RedirectAction{
+											PortRedirect: 80,
+										},
 									},
 								},
 								{
@@ -2516,6 +2519,7 @@ var requestRedirectHTTPListenersCiliumEnvoyConfig = &ciliumv2.CiliumEnvoyConfig{
 									Action: &envoy_config_route_v3.Route_Redirect{
 										Redirect: &envoy_config_route_v3.RedirectAction{
 											HostRedirect: "example.com",
+											PortRedirect: 80,
 										},
 									},
 								},

.github/workflows/conformance-gateway-api.yaml
@@ -111,13 +111,10 @@ jobs:
           fi
           echo sha=${SHA} >> $GITHUB_OUTPUT
           
-          SUPPORTED_FEATURES="ReferenceGrant,HTTPRoute,TLSRoute,HTTPRouteQueryParamMatching,HTTPRouteMethodMatching,GatewayClassObservedGenerationBump,HTTPRouteHostRewrite,HTTPRoutePathRewrite"
+          SUPPORTED_FEATURES="ReferenceGrant,HTTPRoute,TLSRoute,HTTPRouteQueryParamMatching,HTTPRouteMethodMatching,GatewayClassObservedGenerationBump,HTTPRouteHostRewrite,HTTPRoutePathRewrite,HTTPRouteSchemeRedirect,HTTPRoutePathRedirect,HTTPRoutePortRedirect"
           if [ ${{ matrix.crd-channel }} == "experimental" ]; then
             SUPPORTED_FEATURES+=",HTTPResponseHeaderModification,RouteDestinationPortMatching"
           fi
-          
-          # Enable HTTPRouteRedirectHostAndStatus once https://github.com/kubernetes-sigs/gateway-api/issues/1805 is fixed upstream
-          SKIPPED_TESTS="TestConformance/HTTPRouteRedirectHostAndStatus"
 
           CILIUM_INSTALL_DEFAULTS="--chart-directory=install/kubernetes/cilium \
             --helm-set=debug.enabled=true \

operator/pkg/model/translation/envoy_listener.go
@@ -133,10 +133,7 @@ func NewHTTPListener(name string, ciliumSecretNamespace string, tls map[model.TL
 		}
 
 		filterChains = append(filterChains, &envoy_config_listener.FilterChain{
-			FilterChainMatch: &envoy_config_listener.FilterChainMatch{
-				ServerNames:       slices.SortedUnique(hostNames),
-				TransportProtocol: tlsTransportProtocol,
-			},
+			FilterChainMatch: toFilterChainMatch(hostNames),
 			Filters: []*envoy_config_listener.Filter{
 				{
 					Name: httpConnectionManagerType,
@@ -197,10 +194,7 @@ func NewSNIListener(name string, backendsForHost map[string][]string, mutatorFun
 
 	for backed, hostNames := range backendsForHost {
 		filterChains = append(filterChains, &envoy_config_listener.FilterChain{
-			FilterChainMatch: &envoy_config_listener.FilterChainMatch{
-				ServerNames:       slices.SortedUnique(hostNames),
-				TransportProtocol: tlsTransportProtocol,
-			},
+			FilterChainMatch: toFilterChainMatch(hostNames),
 			Filters: []*envoy_config_listener.Filter{
 				{
 					Name: tcpProxyType,
@@ -279,4 +273,16 @@ func newTransportSocket(ciliumSecretNamespace string, tls []model.TLSSecret) (*e
 			},
 		},
 	}, nil
+}
+
+func toFilterChainMatch(hostNames []string) *envoy_config_listener.FilterChainMatch {
+	res := &envoy_config_listener.FilterChainMatch{
+		TransportProtocol: tlsTransportProtocol,
+	}
+	// ServerNames must be sorted and unique, however, envoy don't support "*" as a server name
+	serverNames := slices.SortedUnique(hostNames)
+	if len(serverNames) > 1 || (len(serverNames) == 1 && serverNames[0] != "*") {
+		res.ServerNames = serverNames
+	}
+	return res
 }

operator/pkg/model/translation/envoy_virtual_host.go
@@ -113,24 +113,32 @@ func (s SortableRoute) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
+// VirtualHostParameter is the parameter for NewVirtualHost
+type VirtualHostParameter struct {
+	HostNames           []string
+	HTTPSRedirect       bool
+	HostNameSuffixMatch bool
+	ListenerPort        uint32
+}
+
 // NewVirtualHostWithDefaults is same as NewVirtualHost but with a few
 // default mutator function. If there are multiple http routes having
 // the same path matching (e.g. exact, prefix or regex), the incoming
 // request will be load-balanced to multiple backends equally.
-func NewVirtualHostWithDefaults(hostnames []string, httpsRedirect bool, hostNameSuffixMatch bool, httpRoutes []model.HTTPRoute, mutators ...VirtualHostMutator) (*envoy_config_route_v3.VirtualHost, error) {
+func NewVirtualHostWithDefaults(httpRoutes []model.HTTPRoute, param VirtualHostParameter, mutators ...VirtualHostMutator) (*envoy_config_route_v3.VirtualHost, error) {
 	fns := append(mutators,
 		WithMaxStreamDuration(0),
 	)
-	return NewVirtualHost(hostnames, httpsRedirect, hostNameSuffixMatch, httpRoutes, fns...)
+	return NewVirtualHost(httpRoutes, param, fns...)
 }
 
 // NewVirtualHost creates a new VirtualHost with the given host and routes.
-func NewVirtualHost(hostnames []string, httpsRedirect bool, hostNameSuffixMatch bool, httpRoutes []model.HTTPRoute, mutators ...VirtualHostMutator) (*envoy_config_route_v3.VirtualHost, error) {
+func NewVirtualHost(httpRoutes []model.HTTPRoute, param VirtualHostParameter, mutators ...VirtualHostMutator) (*envoy_config_route_v3.VirtualHost, error) {
 	var routes SortableRoute
-	if httpsRedirect {
-		routes = envoyHTTPSRoutes(httpRoutes, hostnames, hostNameSuffixMatch)
+	if param.HTTPSRedirect {
+		routes = envoyHTTPSRoutes(httpRoutes, param.HostNames, param.HostNameSuffixMatch)
 	} else {
-		routes = envoyHTTPRoutes(httpRoutes, hostnames, hostNameSuffixMatch)
+		routes = envoyHTTPRoutes(httpRoutes, param.HostNames, param.HostNameSuffixMatch, param.ListenerPort)
 	}
 
 	// This is to make sure that the Exact match is always having higher priority.
@@ -140,7 +148,7 @@ func NewVirtualHost(hostnames []string, httpsRedirect bool, hostNameSuffixMatch 
 	sort.Stable(routes)
 
 	var domains []string
-	for _, host := range hostnames {
+	for _, host := range param.HostNames {
 		if host == wildCard {
 			domains = []string{wildCard}
 			break
@@ -200,7 +208,7 @@ func envoyHTTPSRoutes(httpRoutes []model.HTTPRoute, hostnames []string, hostName
 	return routes
 }
 
-func envoyHTTPRoutes(httpRoutes []model.HTTPRoute, hostnames []string, hostNameSuffixMatch bool) []*envoy_config_route_v3.Route {
+func envoyHTTPRoutes(httpRoutes []model.HTTPRoute, hostnames []string, hostNameSuffixMatch bool, listenerPort uint32) []*envoy_config_route_v3.Route {
 	matchBackendMap := make(map[string][]model.HTTPRoute)
 	for _, r := range httpRoutes {
 		matchBackendMap[r.GetMatchKey()] = append(matchBackendMap[r.GetMatchKey()], r)
@@ -236,7 +244,7 @@ func envoyHTTPRoutes(httpRoutes []model.HTTPRoute, hostnames []string, hostNameS
 		}
 
 		if hRoutes[0].RequestRedirect != nil {
-			route.Action = getRouteRedirect(hRoutes[0].RequestRedirect)
+			route.Action = getRouteRedirect(hRoutes[0].RequestRedirect, listenerPort)
 		} else {
 			route.Action = getRouteAction(backends, r.Rewrite)
 		}
@@ -277,8 +285,7 @@ func pathFullReplaceMutation(rewrite *model.HTTPURLRewriteFilter) routeActionMut
 		}
 		route.Route.RegexRewrite = &envoy_type_matcher_v3.RegexMatchAndSubstitute{
 			Pattern: &envoy_type_matcher_v3.RegexMatcher{
-				EngineType: &envoy_type_matcher_v3.RegexMatcher_GoogleRe2{},
-				Regex:      "^/.*$",
+				Regex: "^/.*$",
 			},
 			Substitution: rewrite.Path.Exact,
 		}
@@ -336,7 +343,7 @@ func getRouteAction(backends []model.Backend, rewrite *model.HTTPURLRewriteFilte
 	return routeAction
 }
 
-func getRouteRedirect(redirect *model.HTTPRequestRedirectFilter) *envoy_config_route_v3.Route_Redirect {
+func getRouteRedirect(redirect *model.HTTPRequestRedirectFilter, listenerPort uint32) *envoy_config_route_v3.Route_Redirect {
 	redirectAction := &envoy_config_route_v3.RedirectAction{}
 
 	if redirect.Scheme != nil {
@@ -351,6 +358,16 @@ func getRouteRedirect(redirect *model.HTTPRequestRedirectFilter) *envoy_config_r
 
 	if redirect.Port != nil {
 		redirectAction.PortRedirect = uint32(*redirect.Port)
+	} else {
+		if redirect.Scheme != nil {
+			if *redirect.Scheme == "https" {
+				redirectAction.PortRedirect = 443
+			} else if *redirect.Scheme == "http" {
+				redirectAction.PortRedirect = 80
+			}
+		} else {
+			redirectAction.PortRedirect = listenerPort
+		}
 	}
 
 	if redirect.StatusCode != nil {

commit 97e1dcb46453b09120c0ef0d267dd272eacbba0e

gateway-api: Correct list of HTTPRoutes in VirtualHost
This is to fix the issue in which the wrong list of HTTP routes is used
to construct virtual host for insecure and secure routes. Ideally, only
related HTTP routes for the same listener ports should be considered.

Normally, this should not have any side effect, except using weightage
clusters for the multiple, but the same, backends. However, in case of
redirect filter, this will cause redirect loop as mentioned in the below
issue.

Fixes: 299648fef30ebefab6bfc93399f005473624f703
Fixes: #28186
Signed-off-by: Tam Mach <tam.mach@cilium.io>

operator/pkg/model/translation/translator.go
@@ -194,7 +194,7 @@ func (i *defaultTranslator) getEnvoyHTTPRouteConfiguration(m *model.Model) []cil
 	var res []ciliumv2.XDSResource
 
 	portHostName := map[string][]string{}
-	hostNameRoutes := map[string][]model.HTTPRoute{}
+	hostNamePortRoutes := map[string]map[string][]model.HTTPRoute{}
 
 	for _, l := range m.HTTP {
 		for _, r := range l.Routes {
@@ -205,12 +205,18 @@ func (i *defaultTranslator) getEnvoyHTTPRouteConfiguration(m *model.Model) []cil
 
 			if len(r.Hostnames) == 0 {
 				portHostName[port] = append(portHostName[port], l.Hostname)
-				hostNameRoutes[l.Hostname] = append(hostNameRoutes[l.Hostname], r)
+				if _, ok := hostNamePortRoutes[l.Hostname]; !ok {
+					hostNamePortRoutes[l.Hostname] = map[string][]model.HTTPRoute{}
+				}
+				hostNamePortRoutes[l.Hostname][port] = append(hostNamePortRoutes[l.Hostname][port], r)
 				continue
 			}
 			for _, h := range r.Hostnames {
 				portHostName[port] = append(portHostName[port], h)
-				hostNameRoutes[h] = append(hostNameRoutes[h], r)
+				if _, ok := hostNamePortRoutes[h]; !ok {
+					hostNamePortRoutes[h] = map[string][]model.HTTPRoute{}
+				}
+				hostNamePortRoutes[h][port] = append(hostNamePortRoutes[h][port], r)
 			}
 		}
 	}
@@ -226,7 +232,7 @@ func (i *defaultTranslator) getEnvoyHTTPRouteConfiguration(m *model.Model) []cil
 		// Add HTTPs redirect virtual host for secure host
 		if port == insecureHost && i.enforceHTTPs {
 			for _, h := range slices.Unique(portHostName[secureHost]) {
-				vhs, _ := NewVirtualHostWithDefaults(hostNameRoutes[h], VirtualHostParameter{
+				vhs, _ := NewVirtualHostWithDefaults(hostNamePortRoutes[h][secureHost], VirtualHostParameter{
 					HostNames:           []string{h},
 					HTTPSRedirect:       true,
 					HostNameSuffixMatch: i.hostNameSuffixMatch,
@@ -242,7 +248,7 @@ func (i *defaultTranslator) getEnvoyHTTPRouteConfiguration(m *model.Model) []cil
 					continue
 				}
 			}
-			routes, exists := hostNameRoutes[h]
+			routes, exists := hostNamePortRoutes[h][port]
 			if !exists {
 				continue
 			}

operator/pkg/model/translation/gateway-api/translator_fixture_test.go
@@ -3364,6 +3364,72 @@ var mirrorHTTPListenersCiliumEnvoyConfig = &ciliumv2.CiliumEnvoyConfig{
 	},
 }
 
+var multipleListenerGatewayListeners = []model.HTTPListener{
+	{
+		Name: "http-example",
+		Sources: []model.FullyQualifiedResource{
+			{
+				Name:      "my-gateway",
+				Namespace: "default",
+				Group:     "gateway.networking.k8s.io",
+				Version:   "v1",
+				Kind:      "Gateway",
+			},
+		},
+		Address:  "",
+		Port:     80,
+		Hostname: "example.com",
+		Routes: []model.HTTPRoute{
+			{
+				Hostnames: []string{"example.com"},
+				DirectResponse: &model.DirectResponse{
+					StatusCode: 500,
+				},
+				RequestRedirect: &model.HTTPRequestRedirectFilter{
+					Scheme:     model.AddressOf("https"),
+					StatusCode: model.AddressOf(301),
+				},
+				Backends: []model.Backend{},
+			},
+		},
+	},
+	{
+		Name: "https-example",
+		Sources: []model.FullyQualifiedResource{
+			{
+				Name:      "my-gateway",
+				Namespace: "default",
+				Group:     "gateway.networking.k8s.io",
+				Version:   "v1",
+				Kind:      "Gateway",
+			},
+		},
+		Address:  "",
+		Port:     443,
+		Hostname: "example.com",
+		TLS: []model.TLSSecret{
+			{
+				Name:      "example-cert",
+				Namespace: "default",
+			},
+		},
+		Routes: []model.HTTPRoute{
+			{
+				Hostnames: []string{"example.com"},
+				Backends: []model.Backend{
+					{
+						Name:      "my-service",
+						Namespace: "default",
+						Port: &model.BackendPort{
+							Port: 8080,
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
 func toEnvoyCluster(namespace, name, port string) *envoy_config_cluster_v3.Cluster {
 	return &envoy_config_cluster_v3.Cluster{
 		Name: fmt.Sprintf("%s:%s:%s", namespace, name, port),

operator/pkg/model/translation/gateway-api/translator_test.go
@@ -6,6 +6,8 @@ package gateway_api
 import (
 	"testing"
 
+	envoy_config_route_v3 "github.com/cilium/proxy/go/envoy/config/route/v3"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/cilium/cilium/operator/pkg/model"
@@ -193,6 +195,109 @@ func Test_translator_Translate(t *testing.T) {
 			cec, _, _, err := trans.Translate(tt.args.m)
 			require.Equal(t, tt.wantErr, err != nil, "Error mismatch")
 			require.Equal(t, tt.want, cec, "CiliumEnvoyConfig did not match")
+		})
+	}
+}
+
+func Test_translator_TranslateResource(t *testing.T) {
+	type args struct {
+		m *model.Model
+	}
+	tests := []struct {
+		name          string
+		args          args
+		wantErr       bool
+		validateFuncs []func(config *ciliumv2.CiliumEnvoyConfig) bool
+	}{
+		{
+			name: "MultipleListenerGateway",
+			args: args{
+				m: &model.Model{
+					HTTP: multipleListenerGatewayListeners,
+				},
+			},
+			validateFuncs: []func(cec *ciliumv2.CiliumEnvoyConfig) bool{
+				func(cec *ciliumv2.CiliumEnvoyConfig) bool {
+					resource := ciliumv2.XDSResource{
+						Any: toAny(&envoy_config_route_v3.RouteConfiguration{
+							Name: "listener-insecure",
+							VirtualHosts: []*envoy_config_route_v3.VirtualHost{
+								{
+									Name: "example.com",
+									Domains: []string{
+										"example.com",
+										"example.com:*",
+									},
+									Routes: []*envoy_config_route_v3.Route{
+										{
+											Match: &envoy_config_route_v3.RouteMatch{
+												PathSpecifier: &envoy_config_route_v3.RouteMatch_Prefix{
+													Prefix: "/",
+												},
+											},
+											Action: &envoy_config_route_v3.Route_Redirect{
+												Redirect: &envoy_config_route_v3.RedirectAction{
+													SchemeRewriteSpecifier: &envoy_config_route_v3.RedirectAction_SchemeRedirect{
+														SchemeRedirect: "https",
+													},
+													PortRedirect: 443,
+												},
+											},
+										},
+									},
+								},
+							},
+						}),
+					}
+
+					expected, _ := resource.MarshalJSON()
+					got, _ := cec.Spec.Resources[1].MarshalJSON()
+					return assert.Equal(t, string(expected), string(got), "Route Configuration mismatch")
+				},
+				func(cec *ciliumv2.CiliumEnvoyConfig) bool {
+					resource := ciliumv2.XDSResource{
+						Any: toAny(&envoy_config_route_v3.RouteConfiguration{
+							Name: "listener-secure",
+							VirtualHosts: []*envoy_config_route_v3.VirtualHost{
+								{
+									Name: "example.com",
+									Domains: []string{
+										"example.com",
+										"example.com:*",
+									},
+									Routes: []*envoy_config_route_v3.Route{
+										{
+											Match: &envoy_config_route_v3.RouteMatch{
+												PathSpecifier: &envoy_config_route_v3.RouteMatch_Prefix{
+													Prefix: "/",
+												},
+											},
+											Action: toRouteAction("default", "my-service", "8080"),
+										},
+									},
+								},
+							},
+						}),
+					}
+
+					expected, _ := resource.MarshalJSON()
+					got, _ := cec.Spec.Resources[2].MarshalJSON()
+					return assert.Equal(t, string(expected), string(got), "Route Configuration mismatch")
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			trans := &translator{
+				idleTimeoutSeconds: 60,
+			}
+			cec, _, _, err := trans.Translate(tt.args.m)
+			require.Equal(t, tt.wantErr, err != nil, "Error mismatch")
+			for _, fn := range tt.validateFuncs {
+				require.True(t, fn(cec), "Validation failed")
+			}
 		})
 	}
 }

operator/pkg/model/translation/ingress/dedicated_ingress_fixture_test.go
@@ -108,26 +108,6 @@ func toRouteAction(namespace, name, port string) *envoy_config_route_v3.Route_Ro
 	}
 }
 
-func toWeightedClusterRouteAction(names []string) *envoy_config_route_v3.Route_Route {
-	weightedClusters := make([]*envoy_config_route_v3.WeightedCluster_ClusterWeight, 0, len(names))
-	for _, name := range names {
-		weightedClusters = append(weightedClusters, &envoy_config_route_v3.WeightedCluster_ClusterWeight{
-			Name:   name,
-			Weight: &wrapperspb.UInt32Value{Value: 1},
-		})
-	}
-
-	return &envoy_config_route_v3.Route_Route{
-		Route: &envoy_config_route_v3.RouteAction{
-			ClusterSpecifier: &envoy_config_route_v3.RouteAction_WeightedClusters{
-				WeightedClusters: &envoy_config_route_v3.WeightedCluster{
-					Clusters: weightedClusters,
-				},
-			},
-		},
-	}
-}
-
 func toHTTPSRedirectAction() *envoy_config_route_v3.Route_Redirect {
 	return &envoy_config_route_v3.Route_Redirect{
 		Redirect: &envoy_config_route_v3.RedirectAction{
@@ -549,10 +529,7 @@ var hostRulesListenersCiliumEnvoyConfig = &ciliumv2.CiliumEnvoyConfig{
 											Prefix: "/",
 										},
 									},
-									Action: toWeightedClusterRouteAction([]string{
-										"random-namespace:foo-bar-com:http",
-										"random-namespace:foo-bar-com:http",
-									}),
+									Action: toRouteAction("random-namespace", "foo-bar-com", "http"),
 								},
 							},
 						},