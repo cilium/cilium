68a95b46ca3729302bc6aeb4ae1c6c51e51911e1
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-11-13 09:51:44 +0100

    l7-loadbalancing: remove package log
    This commit removes the package global log.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- operator/pkg/ciliumenvoyconfig/doc.go
+++ operator/pkg/ciliumenvoyconfig/doc.go
@@ -3,12 +3,3 @@
 
 // Package service contains the logic for Cilium Load Balancer Controller via envoy config
 package ciliumenvoyconfig
-
-import (
-	"github.com/cilium/cilium/pkg/logging"
-	"github.com/cilium/cilium/pkg/logging/logfields"
-)
-
-const Subsys = "envoy-lb-controller"
-
-var log = logging.DefaultLogger.WithField(logfields.LogSubsys, Subsys)
