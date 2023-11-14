cilium/cilium

commit 03ad61b6b356cc47996dabfd7832053223b36ef4

datapath/linux: Implement DevicesController
DevicesController populates the devices and routes tables.

To avoid large refactoring of the existing code around option.Config.GetDevices()
this opts for an incremental approach: keep the DeviceManager API as-is and reuse
its test to validate that the existing functionality does not break.

The tests required some slight adaptation: the "DeviceManager" is re-created after
each test case to recreate the initial devices table.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/datapath/linux/devices.go
@@ -1,527 +1,140 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-// This module implements Cilium's network device detection.
-
 package linux
 
 import (
 	"context"
 	"fmt"
 	"net"
-	"sort"
 	"strings"
 
-	"github.com/cilium/ebpf"
-	"github.com/cilium/ebpf/asm"
-	"github.com/vishvananda/netlink"
-	"github.com/vishvananda/netns"
-	"golang.org/x/sys/unix"
+	"golang.org/x/exp/slices"
 
-	"github.com/cilium/cilium/pkg/cidr"
-	"github.com/cilium/cilium/pkg/datapath/linux/probes"
-	"github.com/cilium/cilium/pkg/defaults"
-	"github.com/cilium/cilium/pkg/lock"
-	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/mac"
+	"github.com/cilium/cilium/pkg/datapath/tables"
+	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 )
 
-var (
-	// Route filter to look at all routing tables.
-	routeFilter = netlink.Route{
-		Table: unix.RT_TABLE_UNSPEC,
-	}
-	routeFilterMask = netlink.RT_FILTER_TABLE
-)
-
-type DeviceManager struct {
-	lock.Mutex
-	devices map[string]struct{}
-	filter  deviceFilter
-	handle  *netlink.Handle
-	netns   netns.NsHandle
-}
-
-func NewDeviceManager() (*DeviceManager, error) {
-	return NewDeviceManagerAt(netns.None())
-}
-
-func NewDeviceManagerAt(netns netns.NsHandle) (*DeviceManager, error) {
-	handle, err := netlink.NewHandleAt(netns)
-	if err != nil {
-		return nil, fmt.Errorf("unable to setup device manager: %w", err)
-	}
-	return &DeviceManager{
-		devices: make(map[string]struct{}),
-		filter:  deviceFilter(option.Config.GetDevices()),
-		handle:  handle,
-		netns:   netns,
-	}, nil
-}
-
-// Detect tries to detect devices to which BPF programs may be loaded.
-// See AreDevicesRequired() for features that require the device information.
+// DeviceManager is a temporary compatibility bridge to keep DeviceManager uses as is and reuse its tests
+// against DevicesController and the devices table.
 //
-// The devices are detected by looking at all the configured global unicast
-// routes in the system.
+// This will be refactored away in follow-up PRs that convert code over to the devices table.
+// The DirectRoutingDevice and IPv6MCastDevice would computed from the devices table as necessary.
+type DeviceManager struct {
+	params         devicesControllerParams
+	initialDevices []string
+	hive           *hive.Hive
+}
+
 func (dm *DeviceManager) Detect(k8sEnabled bool) ([]string, error) {
-	dm.Lock()
-	defer dm.Unlock()
-	dm.devices = make(map[string]struct{})
-
-	if err := dm.expandDevices(); err != nil {
-		return nil, err
+	hasWildcard := false
+	userDevices := option.Config.GetDevices()
+	for _, name := range userDevices {
+		hasWildcard = hasWildcard || strings.HasSuffix(name, "+")
 	}
 
-	if err := dm.expandDirectRoutingDevice(); err != nil {
-		return nil, err
+	devs, _ := tables.SelectedDevices(dm.params.DeviceTable.Reader(dm.params.DB.ReadTxn()))
+	names := tables.DeviceNames(devs)
+
+	if len(names) == 0 && hasWildcard {
+		// Fail if user provided a device wildcard which didn't match anything.
+		return nil, fmt.Errorf("No device found matching %v", userDevices)
 	}
 
-	l3DevOK := true
-	if !option.Config.EnableHostLegacyRouting && !option.Config.DryMode {
-		// Probe whether BPF host routing is supported for L3 devices. This will
-		// invoke bpftool and requires root privileges, so we're only probing
-		// when necessary.
-		l3DevOK = supportL3Dev()
-	}
+	option.Config.SetDevices(names)
+	dm.initialDevices = names
 
-	if len(option.Config.GetDevices()) == 0 && option.Config.AreDevicesRequired() {
-		// Detect the devices from the system routing table by finding the devices
-		// which have global unicast routes.
-		family := netlink.FAMILY_ALL
-		if option.Config.EnableIPv4 && !option.Config.EnableIPv6 {
-			family = netlink.FAMILY_V4
-		} else if !option.Config.EnableIPv4 && option.Config.EnableIPv6 {
-			family = netlink.FAMILY_V6
-		}
-
-		routes, err := dm.handle.RouteListFiltered(family, &routeFilter, routeFilterMask)
-		if err != nil {
-			return nil, fmt.Errorf("cannot retrieve routes for device detection: %w", err)
-		}
-		dm.updateDevicesFromRoutes(l3DevOK, routes)
-	} else {
-		for _, dev := range option.Config.GetDevices() {
-			dm.devices[dev] = struct{}{}
+	// Look up the device that holds the node IP. Used as fallback for direct-routing
+	// and multicast devices.
+	var nodeDevice *tables.Device
+	if k8sEnabled {
+		nodeIP := node.GetK8sNodeIP()
+		for _, dev := range devs {
+			if dev.HasIP(nodeIP) {
+				nodeDevice = dev
+				break
+			}
 		}
 	}
 
-	detectDirectRoutingDev := option.Config.DirectRoutingDeviceRequired()
-	if option.Config.DirectRoutingDeviceRequired() && option.Config.DirectRoutingDevice != "" {
-		dm.devices[option.Config.DirectRoutingDevice] = struct{}{}
-		detectDirectRoutingDev = false
-	}
-
-	detectIPv6MCastDev := option.Config.EnableIPv6NDP
-	if option.Config.IPv6MCastDevice != "" {
-		dm.devices[option.Config.IPv6MCastDevice] = struct{}{}
-		detectIPv6MCastDev = false
-	}
-
-	if detectDirectRoutingDev || detectIPv6MCastDev {
-		k8sNodeDev := ""
-		k8sNodeLink, err := findK8SNodeIPLink()
-		if err == nil {
-			k8sNodeDev = k8sNodeLink.Attrs().Name
-			dm.devices[k8sNodeDev] = struct{}{}
-		} else if k8sEnabled {
-			return nil, fmt.Errorf("k8s is enabled, but still failed to find node IP: %w", err)
+	if option.Config.DirectRoutingDeviceRequired() {
+		var filter deviceFilter
+		if option.Config.DirectRoutingDevice != "" {
+			filter = deviceFilter(strings.Split(option.Config.DirectRoutingDevice, ","))
 		}
-
-		if detectDirectRoutingDev {
-			// If only one device found, use that one. Otherwise use the device with k8s node IP.
-			if len(dm.devices) == 1 {
-				for dev := range dm.devices {
-					option.Config.DirectRoutingDevice = dev
+		option.Config.DirectRoutingDevice = ""
+		if filter.nonEmpty() {
+			// User has defined a direct-routing device. Try to find the first matching
+			// device.
+			for _, dev := range devs {
+				if filter.match(dev.Name) {
+					option.Config.DirectRoutingDevice = dev.Name
 					break
 				}
-			} else if k8sNodeDev != "" {
-				option.Config.DirectRoutingDevice = k8sNodeDev
-			} else {
-				return nil, fmt.Errorf("unable to determine direct routing device. Use --%s to specify it",
-					option.DirectRoutingDevice)
 			}
-			log.WithField(option.DirectRoutingDevice, option.Config.DirectRoutingDevice).
-				Info("Direct routing device detected")
+		} else if len(devs) == 1 {
+			option.Config.DirectRoutingDevice = devs[0].Name
+		} else if nodeDevice != nil {
+			option.Config.DirectRoutingDevice = nodeDevice.Name
 		}
-
-		if detectIPv6MCastDev {
-			if k8sNodeLink != nil && k8sNodeLink.Attrs().Flags&net.FlagMulticast != 0 {
-				option.Config.IPv6MCastDevice = k8sNodeDev
-				log.WithField(option.IPv6MCastDevice, option.Config.IPv6MCastDevice).Info("IPv6 multicast device detected")
-			} else {
-				return nil, fmt.Errorf("unable to determine Multicast device. Use --%s to specify it",
-					option.IPv6MCastDevice)
-			}
+		if option.Config.DirectRoutingDevice == "" {
+			return nil, fmt.Errorf("unable to determine direct routing device. Use --%s to specify it",
+				option.DirectRoutingDevice)
 		}
+		log.WithField(option.DirectRoutingDevice, option.Config.DirectRoutingDevice).
+			Info("Direct routing device detected")
 	}
 
-	deviceList := dm.getDeviceList()
-	option.Config.SetDevices(deviceList)
-	log.WithField(logfields.Devices, deviceList).Info("Detected devices")
-	return deviceList, nil
-}
-
-func (dm *DeviceManager) getDeviceList() []string {
-	devs := make([]string, 0, len(dm.devices))
-	for dev := range dm.devices {
-		devs = append(devs, dev)
-	}
-	sort.Strings(devs)
-	return devs
-}
-
-// Exclude devices that have one or more of these flags set.
-var excludedIfFlagsMask uint32 = unix.IFF_SLAVE | unix.IFF_LOOPBACK
-
-// isViableDevice returns true if the given link is usable and Cilium should attach
-// programs to it.
-func (dm *DeviceManager) isViableDevice(l3DevOK, hasDefaultRoute bool, link netlink.Link) bool {
-	name := link.Attrs().Name
-
-	// Do not consider any of the excluded devices.
-	for _, p := range defaults.ExcludedDevicePrefixes {
-		if strings.HasPrefix(name, p) {
-			log.WithField(logfields.Device, name).
-				Debugf("Skipping device as it has excluded prefix '%s'", p)
-			return false
-		}
-	}
-
-	// Skip devices that have an excluded interface flag set.
-	if link.Attrs().RawFlags&excludedIfFlagsMask != 0 {
-		log.WithField(logfields.Device, name).Debugf("Skipping device as it has excluded flag (%x)", link.Attrs().RawFlags)
-		return false
-	}
-
-	// Ignore L3 devices if we cannot support them.
-	if !l3DevOK && !mac.LinkHasMacAddr(link) {
-		log.WithField(logfields.Device, name).
-			Info("Ignoring L3 device; >= 5.8 kernel is required.")
-		return false
-	}
-
-	// If user specified devices or wildcards, then skip the device if it doesn't match.
-	if !dm.filter.match(name) {
-		return false
-	}
-
-	switch link.Type() {
-	case "veth":
-		// Skip veth devices that don't have a default route.
-		// This is a workaround for kubernetes-in-docker. We want to avoid
-		// veth devices in general as they may be leftovers from another CNI.
-		if !hasDefaultRoute {
-			log.WithField(logfields.Device, name).
-				Debug("Ignoring veth device as it has no default route")
-			return false
-		}
-
-	case "bridge", "openvswitch":
-		// Skip bridge devices as they're very unlikely to be used for K8s
-		// purposes. In the rare cases where a user wants to load datapath
-		// programs onto them they can override device detection with --devices.
-		log.WithField(logfields.Device, name).Debug("Ignoring bridge-like device")
-		return false
-
-	}
-
-	if link.Attrs().MasterIndex > 0 {
-		if master, err := dm.handle.LinkByIndex(link.Attrs().MasterIndex); err == nil {
-			switch master.Type() {
-			case "bridge", "openvswitch":
-				log.WithField(logfields.Device, name).Debug("Ignoring device attached to bridge")
-				return false
-
-			case "bond", "team":
-				log.WithField(logfields.Device, name).Debug("Ignoring bonded device")
-				return false
-			}
-
-		}
-	}
-
-	return true
-}
-
-type linkInfo struct {
-	hasDefaultRoute bool
-}
-
-func genZeroNet(family int) *net.IPNet {
-	switch family {
-	case netlink.FAMILY_V4:
-		return &net.IPNet{
-			IP:   net.IPv4zero,
-			Mask: net.CIDRMask(0, 8*net.IPv4len),
-		}
-	case netlink.FAMILY_V6:
-		return &net.IPNet{
-			IP:   net.IPv6zero,
-			Mask: net.CIDRMask(0, 8*net.IPv6len),
-		}
-	}
-	return nil
-}
-
-// updateDevicesFromRoutes processes a batch of routes and updates the set of
-// devices. Returns true if devices changed.
-func (dm *DeviceManager) updateDevicesFromRoutes(l3DevOK bool, routes []netlink.Route) bool {
-	linkInfos := make(map[int]linkInfo)
-
-	// Collect all link indices mentioned in the route update batch
-	for _, route := range routes {
-		// Only consider devices that have global unicast routes,
-		// e.g. skip loopback, multicast.
-		if !cidr.Equal(route.Dst, genZeroNet(route.Family)) && !route.Dst.IP.IsGlobalUnicast() {
-			continue
-		}
-		linkInfo := linkInfos[route.LinkIndex]
-		linkInfo.hasDefaultRoute = linkInfo.hasDefaultRoute || cidr.Equal(route.Dst, genZeroNet(route.Family))
-		linkInfos[route.LinkIndex] = linkInfo
-	}
-
-	changed := false
-	for index, info := range linkInfos {
-		link, err := dm.handle.LinkByIndex(index)
-		if err != nil {
-			log.WithError(err).WithField(logfields.LinkIndex, index).
-				Warn("Failed to get link by index")
-			continue
-		}
-		name := link.Attrs().Name
-
-		// Skip devices we already know.
-		if _, exists := dm.devices[name]; exists {
-			continue
-		}
-
-		viable := dm.isViableDevice(l3DevOK, info.hasDefaultRoute, link)
-		if viable {
-			dm.devices[name] = struct{}{}
-			changed = true
+	if option.Config.EnableIPv6NDP && option.Config.IPv6MCastDevice == "" {
+		if nodeDevice != nil && nodeDevice.Flags&net.FlagMulticast != 0 {
+			option.Config.IPv6MCastDevice = nodeDevice.Name
 		} else {
-			log.WithField(logfields.Device, name).Debug("Skipping unviable device")
+			return nil, fmt.Errorf("unable to determine Multicast device. Use --%s to specify it",
+				option.IPv6MCastDevice)
 		}
 	}
-	return changed
+
+	return names, nil
 }
 
-// Listen starts listening to changes to network devices. When devices change the new set
-// of devices is sent on the returned channel.
 func (dm *DeviceManager) Listen(ctx context.Context) (chan []string, error) {
-	l3DevOK := supportL3Dev()
-	routeChan := make(chan netlink.RouteUpdate)
-	closeChan := make(chan struct{})
-
-	err := netlink.RouteSubscribeWithOptions(routeChan, closeChan,
-		netlink.RouteSubscribeOptions{
-			// List existing routes to make sure we did not miss changes that happened after Detect().
-			ListExisting: true,
-			Namespace:    &dm.netns,
-		})
-	if err != nil {
-		return nil, err
-	}
-
-	linkChan := make(chan netlink.LinkUpdate)
-
-	err = netlink.LinkSubscribeWithOptions(linkChan, closeChan, netlink.LinkSubscribeOptions{
-		ListExisting: false,
-		Namespace:    &dm.netns,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	devicesChan := make(chan []string, 1)
-
-	// Find links deleted after Detect()
-	if allLinks, err := dm.handle.LinkList(); err == nil {
-		changed := false
-		linksByName := map[string]struct{}{}
-		for _, link := range allLinks {
-			linksByName[link.Attrs().Name] = struct{}{}
-		}
-		dm.Lock()
-		for name := range dm.devices {
-			if _, exists := linksByName[name]; !exists {
-				delete(dm.devices, name)
-				changed = true
-			}
-		}
-		devices := dm.getDeviceList()
-		dm.Unlock()
-
-		if changed {
-			log.WithField(logfields.Devices, devices).Info("Devices changed")
-			devicesChan <- devices
-		}
-	}
-
+	devs := make(chan []string)
 	go func() {
-		log.Info("Listening for device changes")
+		defer close(devs)
 
+		prevDevices := dm.initialDevices
 		for {
-			devicesChanged := false
-			var devices []string
-
+			iter, invalidated := tables.SelectedDevices(dm.params.DeviceTable.Reader(dm.params.DB.ReadTxn()))
+			newDevices := tables.DeviceNames(iter)
+			if slices.Equal(prevDevices, newDevices) {
+				continue
+			}
 			select {
+			case devs <- newDevices:
 			case <-ctx.Done():
-				log.Debug("context closed, Listen() stopping")
-				close(closeChan)
-				close(devicesChan)
-				// drain the route and link channels
-				for range routeChan {
-				}
-				for range linkChan {
-				}
 				return
-
-			case update := <-routeChan:
-				if update.Type == unix.RTM_NEWROUTE {
-					dm.Lock()
-					devicesChanged = dm.updateDevicesFromRoutes(l3DevOK, []netlink.Route{update.Route})
-					devices = dm.getDeviceList()
-					dm.Unlock()
-				}
-
-			case update := <-linkChan:
-				if update.Header.Type == unix.RTM_DELLINK {
-					name := update.Attrs().Name
-					dm.Lock()
-					if _, ok := dm.devices[name]; ok {
-						delete(dm.devices, name)
-						devicesChanged = true
-					}
-					devices = dm.getDeviceList()
-					dm.Unlock()
-				}
 			}
-
-			if devicesChanged {
-				log.WithField(logfields.Devices, devices).Info("Devices changed")
-				devicesChan <- devices
+			select {
+			case <-invalidated:
+			case <-ctx.Done():
+				return
 			}
+			prevDevices = newDevices
 		}
 	}()
-	return devicesChan, nil
+	return devs, nil
 }
 
-// expandDevices expands all wildcard device names to concrete devices.
-// e.g. device "eth+" expands to "eth0,eth1" etc. Non-matching wildcards are ignored.
-func (dm *DeviceManager) expandDevices() error {
-	expandedDevices, err := dm.expandDeviceWildcards(option.Config.GetDevices(), option.Devices)
-	if err != nil {
-		return err
-	}
-	option.Config.SetDevices(expandedDevices)
-	return nil
+// newDeviceManager constructs a DeviceManager that implements the old DeviceManager API.
+// Dummy dependency to *devicesController to make sure devices table is populated.
+func newDeviceManager(p devicesControllerParams, _ *devicesController) *DeviceManager {
+	return &DeviceManager{params: p, hive: nil}
 }
 
-// expandDirectRoutingDevice expands all wildcard device names to concrete devices and picks a first one.
-func (dm *DeviceManager) expandDirectRoutingDevice() error {
-	if option.Config.DirectRoutingDevice == "" {
-		return nil
+func (dm *DeviceManager) Stop() {
+	if dm.hive != nil {
+		dm.hive.Stop(context.TODO())
 	}
-	expandedDevices, err := dm.expandDeviceWildcards([]string{option.Config.DirectRoutingDevice}, option.DirectRoutingDevice)
-	if err != nil {
-		return err
-	}
-	option.Config.DirectRoutingDevice = expandedDevices[0]
-	return nil
-}
-
-func (dm *DeviceManager) expandDeviceWildcards(devices []string, option string) ([]string, error) {
-	allLinks, err := dm.handle.LinkList()
-	if err != nil {
-		return nil, fmt.Errorf("device wildcard expansion failed to fetch devices: %w", err)
-	}
-	expandedDevicesMap := make(map[string]struct{})
-	for _, iface := range devices {
-		if strings.HasSuffix(iface, "+") {
-			prefix := strings.TrimRight(iface, "+")
-			for _, link := range allLinks {
-				attrs := link.Attrs()
-				if strings.HasPrefix(attrs.Name, prefix) {
-					expandedDevicesMap[attrs.Name] = struct{}{}
-				}
-			}
-		} else {
-			expandedDevicesMap[iface] = struct{}{}
-		}
-	}
-	if len(devices) > 0 && len(expandedDevicesMap) == 0 {
-		// User defined devices, but expansion yielded no devices. Fail here to not
-		// surprise with auto-detection.
-		return nil, fmt.Errorf("device wildcard expansion failed to detect devices. Please verify --%s option.",
-			option)
-	}
-
-	expandedDevices := make([]string, 0, len(expandedDevicesMap))
-	for dev := range expandedDevicesMap {
-		expandedDevices = append(expandedDevices, dev)
-	}
-	sort.Strings(expandedDevices)
-	return expandedDevices, nil
-}
-
-func findK8SNodeIPLink() (netlink.Link, error) {
-	nodeIP := node.GetK8sNodeIP()
-
-	if nodeIP == nil {
-		return nil, fmt.Errorf("failed to find K8s node device as node IP is not known")
-	}
-
-	var family int
-	if nodeIP.To4() != nil {
-		family = netlink.FAMILY_V4
-	} else {
-		family = netlink.FAMILY_V6
-	}
-
-	if addrs, err := netlink.AddrList(nil, family); err == nil {
-		for _, a := range addrs {
-			if a.IP.Equal(nodeIP) {
-				link, err := netlink.LinkByIndex(a.LinkIndex)
-				if err != nil {
-					return nil, err
-				}
-
-				// When IPv6 is enabled and the node has an IPv6 address, the 'cilium_host'
-				// interface is assigned the same IP of the node itself. Let's skip it here.
-				if link.Attrs().Name == defaults.HostDevice {
-					continue
-				}
-
-				return link, nil
-			}
-		}
-	}
-	return nil, fmt.Errorf("K8s node device not found")
-}
-
-// supportL3Dev returns true if the kernel is new enough to support BPF host routing of
-// packets coming from L3 devices using bpf_skb_redirect_peer.
-func supportL3Dev() bool {
-	return probes.HaveProgramHelper(ebpf.SchedCLS, asm.FnSkbChangeHead) == nil
-}
-
-type deviceFilter []string
-
-func (lst deviceFilter) match(dev string) bool {
-	if len(lst) == 0 {
-		return true
-	}
-	for _, entry := range lst {
-		if strings.HasSuffix(entry, "+") {
-			prefix := strings.TrimRight(entry, "+")
-			return strings.HasPrefix(dev, prefix)
-		} else if dev == entry {
-			return true
-		}
-	}
-	return false
 }

daemon/cmd/daemon.go
@@ -506,11 +506,6 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 
 	nd := nodediscovery.NewNodeDiscovery(params.NodeManager, params.Clientset, mtuConfig, params.CNIConfigManager.GetCustomNetConf())
 
-	devMngr, err := linuxdatapath.NewDeviceManager()
-	if err != nil {
-		return nil, nil, err
-	}
-
 	d := Daemon{
 		ctx:               ctx,
 		clientset:         params.Clientset,
@@ -519,7 +514,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		compilationMutex:  new(lock.RWMutex),
 		mtuConfig:         mtuConfig,
 		datapath:          params.Datapath,
-		deviceManager:     devMngr,
+		deviceManager:     params.DeviceManager,
 		nodeDiscovery:     nd,
 		nodeLocalStore:    params.LocalNodeStore,
 		endpointCreations: newEndpointCreationManager(params.Clientset),
@@ -904,14 +899,16 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 	// This is because the device detection requires self (Cilium)Node object,
 	// and the k8s service watcher depends on option.Config.EnableNodePort flag
 	// which can be modified after the device detection.
-	devices, err := d.deviceManager.Detect(params.Clientset.IsEnabled())
-	if err != nil {
-		if option.Config.AreDevicesRequired() {
-			// Fail hard if devices are required to function.
-			return nil, nil, fmt.Errorf("failed to detect devices: %w", err)
+	var devices []string
+	if d.deviceManager != nil {
+		if _, err := d.deviceManager.Detect(params.Clientset.IsEnabled()); err != nil {
+			if option.Config.AreDevicesRequired() {
+				// Fail hard if devices are required to function.
+				return nil, nil, fmt.Errorf("failed to detect devices: %w", err)
+			}
+			log.WithError(err).Warn("failed to detect devices, disabling BPF NodePort")
+			disableNodePort()
 		}
-		log.WithError(err).Warn("failed to detect devices, disabling BPF NodePort")
-		disableNodePort()
 	}
 
 	if d.l2announcer != nil {
@@ -1209,7 +1206,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 	identitymanager.Subscribe(d.policy)
 
 	// Start listening to changed devices if requested.
-	if option.Config.EnableRuntimeDeviceDetection {
+	if option.Config.EnableRuntimeDeviceDetection && d.deviceManager != nil {
 		if option.Config.AreDevicesRequired() {
 			devicesChan, err := d.deviceManager.Listen(ctx)
 			if err != nil {

daemon/cmd/daemon_main.go
@@ -1643,6 +1643,7 @@ type daemonParams struct {
 	Settings             cellSettings
 	HealthProvider       cell.Health
 	HealthReporter       cell.HealthReporter
+	DeviceManager        *linuxdatapath.DeviceManager `optional:"true"`
 }
 
 func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {

pkg/datapath/cells.go
@@ -71,6 +71,15 @@ var Cell = cell.Module(
 	cell.Provide(func(dp types.Datapath) types.NodeIDHandler {
 		return dp.NodeIDs()
 	}),
+
+	// DevicesController manages the devices and routes tables
+	linuxdatapath.DevicesControllerCell,
+	cell.Provide(func(cfg *option.DaemonConfig) linuxdatapath.DevicesConfig {
+		// Provide the configured devices to the devices controller.
+		// This is temporary until DevicesController takes ownership of the
+		// device-related configuration options.
+		return linuxdatapath.DevicesConfig{Devices: cfg.GetDevices()}
+	}),
 )
 
 func newWireguardAgent(lc hive.Lifecycle, localNodeStore *node.LocalNodeStore) *wg.Agent {
@@ -143,4 +152,9 @@ type datapathParams struct {
 	BpfMaps []bpf.BpfMap `group:"bpf-maps"`
 
 	NodeMap nodemap.Map
+
+	// Depend on DeviceManager to ensure devices have been resolved.
+	// This is required until option.Config.GetDevices() has been removed and
+	// uses of it converted to Table[Device].
+	DeviceManager *linuxdatapath.DeviceManager
 }

commit e012de0aa0a786a1594f934ca397d8c694846187

devices: fix busy loop
If previous devices and new devices are equal, we don't wait for the
invalidation of the query, and run into a busy loop until the devices
change.

Fixes: 03ad61b6b3 (datapath/linux: Implement DevicesController)

Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

pkg/datapath/linux/devices.go
@@ -116,14 +116,14 @@ func (dm *DeviceManager) Listen(ctx context.Context) (chan []string, error) {
 			devices, invalidated := tables.SelectedDevices(dm.params.DeviceTable, rxn)
 			newDevices := tables.DeviceNames(devices)
 
-			if slices.Equal(prevDevices, newDevices) {
-				continue
+			if !slices.Equal(prevDevices, newDevices) {
+				select {
+				case devs <- newDevices:
+				case <-ctx.Done():
+					return
+				}
 			}
-			select {
-			case devs <- newDevices:
-			case <-ctx.Done():
-				return
-			}
+
 			select {
 			case <-invalidated:
 			case <-ctx.Done():