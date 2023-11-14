c0ff70f0e68c04e8515480d5fdf312f39ad13644
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-11-14 13:42:25 +0100

    ingress: introduce controller-runtime reconciler
    This commit introduces and registers a new Cilium Ingress reconciler
    based on the controller-runtime library.
    
    It replaces the current k8s informer based Cilium Ingress controller.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- operator/pkg/ingress/ingress.go
+++ operator/pkg/ingress/ingress.go
@@ -15,6 +15,8 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/cilium/cilium/operator/pkg/ingress/annotations"
 	"github.com/cilium/cilium/operator/pkg/model"
@@ -40,6 +42,75 @@ const (
 	dedicatedLoadbalancerMode = "dedicated"
 	sharedLoadbalancerMode    = "shared"
 )
+
+// ingressReconciler reconciles a Ingress object
+type ingressReconciler struct {
+	logger logrus.FieldLogger
+	client client.Client
+
+	maxRetries              int
+	enforcedHTTPS           bool
+	useProxyProtocol        bool
+	secretsNamespace        string
+	lbAnnotationPrefixes    []string
+	sharedLBServiceName     string
+	ciliumNamespace         string
+	defaultLoadbalancerMode string
+	defaultSecretNamespace  string
+	defaultSecretName       string
+	idleTimeoutSeconds      int
+
+	sharedTranslator    translation.Translator
+	dedicatedTranslator translation.Translator
+}
+
+func newIngressReconciler(
+	logger logrus.FieldLogger,
+	c client.Client,
+	ciliumNamespace string,
+	enforceHTTPS bool,
+	useProxyProtocol bool,
+	secretsNamespace string,
+	lbAnnotationPrefixes []string,
+	sharedLBServiceName string,
+	defaultLoadbalancerMode string,
+	defaultSecretNamespace string,
+	defaultSecretName string,
+	proxyIdleTimeoutSeconds int,
+) *ingressReconciler {
+	return &ingressReconciler{
+		logger: logger,
+		client: c,
+
+		sharedTranslator:    ingressTranslation.NewSharedIngressTranslator(sharedLBServiceName, ciliumNamespace, secretsNamespace, enforceHTTPS, useProxyProtocol, proxyIdleTimeoutSeconds),
+		dedicatedTranslator: ingressTranslation.NewDedicatedIngressTranslator(secretsNamespace, enforceHTTPS, useProxyProtocol, proxyIdleTimeoutSeconds),
+
+		maxRetries:              10,
+		enforcedHTTPS:           enforceHTTPS,
+		useProxyProtocol:        useProxyProtocol,
+		secretsNamespace:        secretsNamespace,
+		lbAnnotationPrefixes:    lbAnnotationPrefixes,
+		sharedLBServiceName:     sharedLBServiceName,
+		ciliumNamespace:         ciliumNamespace,
+		defaultLoadbalancerMode: defaultLoadbalancerMode,
+		defaultSecretNamespace:  defaultSecretNamespace,
+		defaultSecretName:       defaultSecretName,
+		idleTimeoutSeconds:      proxyIdleTimeoutSeconds,
+	}
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ingressReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&networkingv1.Ingress{}).
+		// (LoadBalancer) Service resource with OwnerReference to the Ingress with dedicated loadbalancing mode
+		Owns(&corev1.Service{}).
+		// Endpoints resource with OwnerReference to the Ingress with dedicated loadbalancing mode
+		Owns(&corev1.Endpoints{}).
+		// CiliumEnvoyConfig resource with OwnerReference to the Ingress with dedicated loadbalancing mode
+		Owns(&ciliumv2.CiliumEnvoyConfig{}).
+		Complete(r)
+}
 
 // event types
 type ingressAddedEvent struct {
