79d9abb89daa4e6e0b18677fcd832cf43f5bf474
Marco Iorio <marco.iorio@isovalent.com>
2023-11-15 12:21:58 +0100

    ipcache listener: drop garbage collection logic
    Crafting a dedicated unit test revealed that this logic was broken,
    because we considered an entry for deletion if and only if it the
    given prefix was not found in the ipcache struct, and the source
    of the corresponding identity was either kvstore or local. But
    when the prefix is not found, we retrieve the default value of
    the identity type, which will never match the source filter.
    
    Now, considering that:
    
    * the ipcache map gets recreated from scratch at every agent restart,
    * the garbage collection logic was triggered only when either Cilium
      was configured in kvstore mode or was connected to at least one
      remote cluster,
    * the etcd watcher already implements a dedicated logic to replay
      possibly missed deletion events both after relist and in case of
      complete reconnection,
    * fixing this logic would likely expose us to even more headaches,
      given that we may remove entries when the ipcache is not yet
      fully synchronized from the other sources, causing the removal
      of valid entries and the disruption of existing connections,
    
    let's just remove it.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/datapath/ipcache/listener.go
+++ pkg/datapath/ipcache/listener.go
@@ -4,16 +4,11 @@
 package ipcache
 
 import (
-	"context"
-	"fmt"
 	"net"
-	"sync"
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/cilium/cilium/pkg/bpf"
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
-	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -21,14 +16,10 @@ import (
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
-	"github.com/cilium/cilium/pkg/source"
-	"github.com/cilium/cilium/pkg/time"
 )
 
 var (
 	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "datapath-ipcache")
-
-	ipcacheBPFGCControllerGroup = controller.NewGroup("ipcache-bpf-garbage-collection")
 )
 
 // monitorNotify is an interface to notify the monitor about ipcache changes.
@@ -38,10 +29,6 @@ type monitorNotify interface {
 
 // BPFListener implements the ipcache.IPIdentityMappingBPFListener
 // interface with an IPCache store that is backed by BPF maps.
-//
-// One listener is shared between callers of OnIPIdentityCacheChange() and the
-// controller launched from OnIPIdentityCacheGC(). However, The listener is not
-// updated after initialization so no locking is provided for access.
 type BPFListener struct {
 	// bpfMap is the BPF map that this listener will update when events are
 	// received from the IPCache.
@@ -49,21 +36,18 @@ type BPFListener struct {
 
 	// monitorNotify is used to notify the monitor about ipcache updates
 	monitorNotify monitorNotify
-
-	ipcache *ipcache.IPCache
 }
 
-func newListener(m *ipcacheMap.Map, mn monitorNotify, ipc *ipcache.IPCache) *BPFListener {
+func newListener(m *ipcacheMap.Map, mn monitorNotify) *BPFListener {
 	return &BPFListener{
 		bpfMap:        m,
 		monitorNotify: mn,
-		ipcache:       ipc,
 	}
 }
 
 // NewListener returns a new listener to push IPCache entries into BPF maps.
 func NewListener(mn monitorNotify, ipc *ipcache.IPCache) *BPFListener {
-	return newListener(ipcacheMap.IPCacheMap(), mn, ipc)
+	return newListener(ipcacheMap.IPCacheMap(), mn)
 }
 
 func (l *BPFListener) notifyMonitor(modType ipcache.CacheModification,
@@ -177,86 +161,4 @@ func (l *BPFListener) OnIPIdentityCacheChange(modType ipcache.CacheModification,
 	}
 }
 
-// updateStaleEntriesFunction returns a DumpCallback that will update the
-// specified "keysToRemove" map with entries that exist in the BPF map which
-// do not exist in the in-memory ipcache.
-//
-// Must be called while holding l.ipcache.Lock for reading.
-func (l *BPFListener) updateStaleEntriesFunction(keysToRemove map[string]ipcacheMap.Key) bpf.DumpCallback {
-	return func(key bpf.MapKey, _ bpf.MapValue) {
-		k := key.(*ipcacheMap.Key)
-		keyToIP := k.String()
-
-		// Don't RLock as part of the same goroutine.
-		if i, exists := l.ipcache.LookupByPrefixRLocked(keyToIP); !exists {
-			switch i.Source {
-			case source.KVStore, source.Local:
-				// Cannot delete from map during callback because DumpWithCallback
-				// RLocks the map.
-				keysToRemove[keyToIP] = *k
-			}
-		}
-	}
-}
-
-// garbageCollect implements GC of the ipcache map in the following way:
-//
-//	Periodically sweep through every element in the BPF map and check it
-//	against the in-memory copy of the map. If it doesn't exist in memory,
-//	delete the entry.
-//
-// Returns an error if garbage collection failed to occur.
-func (l *BPFListener) garbageCollect(ctx context.Context) (*sync.WaitGroup, error) {
-	log.Debug("Running garbage collection for BPF IPCache")
-
-	// Since controllers run asynchronously, need to make sure
-	// IPIdentityCache is not being updated concurrently while we
-	// do GC;
-	l.ipcache.RLock()
-	defer l.ipcache.RUnlock()
-
-	keysToRemove := map[string]ipcacheMap.Key{}
-	if err := l.bpfMap.DumpWithCallback(l.updateStaleEntriesFunction(keysToRemove)); err != nil {
-		return nil, fmt.Errorf("error dumping ipcache BPF map: %s", err)
-	}
-
-	// Remove all keys which are not in in-memory cache from BPF map
-	// for consistency.
-	for _, k := range keysToRemove {
-		log.WithFields(logrus.Fields{logfields.BPFMapKey: k}).
-			Debug("deleting from ipcache BPF map")
-		if err := l.bpfMap.Delete(&k); err != nil {
-			return nil, fmt.Errorf("error deleting key %s from ipcache BPF map: %s", k, err)
-		}
-	}
-	return nil, nil
-}
-
-// OnIPIdentityCacheGC spawns a controller which synchronizes the BPF IPCache Map
-// with the in-memory IP-Identity cache.
-func (l *BPFListener) OnIPIdentityCacheGC() {
-	// This controller ensures that the in-memory IP-identity cache is in-sync
-	// with the BPF map on disk. These can get out of sync if the cilium-agent
-	// is offline for some time, as the maps persist on the BPF filesystem.
-	// In the case that there is some loss of event history in the key-value
-	// store (e.g., compaction in etcd), we cannot rely upon the key-value store
-	// fully to give us the history of all events. As such, periodically check
-	// for inconsistencies in the data-path with that in the agent to ensure
-	// consistent state.
-	l.ipcache.UpdateController("ipcache-bpf-garbage-collection",
-		controller.ControllerParams{
-			Group: ipcacheBPFGCControllerGroup,
-			DoFunc: func(ctx context.Context) error {
-				wg, err := l.garbageCollect(ctx)
-				if err != nil {
-					return err
-				}
-				if wg != nil {
-					wg.Wait()
-				}
-				return nil
-			},
-			RunInterval: 5 * time.Minute,
-		},
-	)
-}
+func (l *BPFListener) OnIPIdentityCacheGC() {}
