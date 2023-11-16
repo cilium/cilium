b318b939d943b2ef287e857f6da4a1afbf7cbf27
Yusuke Suzuki <yusuke.suzuki@isovalent.com>
2023-11-16 18:33:49 +0900

    egressgateway: Redirect from bpf_overlay to egress gw SNAT netdev
    This commit fixes the problem that an iptables SNAT rule in the host netns
    interferes with packets to egress gw by redirecting from bpf_overlay.
    
    Fixes: https://github.com/cilium/cilium/issues/17848
    
    Signed-off-by: Yusuke Suzuki <yusuke.suzuki@isovalent.com>

--- bpf/bpf_overlay.c
+++ bpf/bpf_overlay.c
@@ -32,6 +32,7 @@
 #include "lib/nodeport.h"
 #include "lib/clustermesh.h"
 #include "lib/wireguard.h"
+#include "lib/egress_gateway.h"
 
 #ifdef ENABLE_VTEP
 #include "lib/arp.h"
@@ -386,6 +387,35 @@ skip_vtep:
 	ctx->mark = 0;
 not_esp:
 #endif
+
+#if defined(ENABLE_EGRESS_GATEWAY_COMMON)
+	{
+		__be32 snat_addr, daddr;
+		int ret;
+		struct remote_endpoint_info *remote_ep;
+
+		remote_ep = lookup_ip4_remote_endpoint(ip4->daddr, 0);
+		/* If the packet is destined to an entity inside the cluster, either EP
+		 * or node, skip SNAT since only traffic leaving the cluster is supposed
+		 * to be masqueraded with an egress IP.
+		 */
+		if (remote_ep &&
+		    identity_is_cluster(remote_ep->sec_identity))
+			goto skip_egress_gateway;
+
+		daddr = ip4->daddr;
+		if (egress_gw_snat_needed_hook(ip4->saddr, daddr, &snat_addr)) {
+			ret = ipv4_l3(ctx, ETH_HLEN, NULL, NULL, ip4);
+			if (unlikely(ret != CTX_ACT_OK))
+				return ret;
+
+			/* to-netdev@bpf_host handles SNAT, so no need to do it here. */
+			return egress_gw_fib_lookup_and_redirect(ctx, snat_addr,
+								 daddr, ext_err);
+		}
+	}
+skip_egress_gateway:
+#endif /* ENABLE_EGRESS_GATEWAY_COMMON */
 
 	/* Deliver to local (non-host) endpoint: */
 	ep = lookup_ip4_endpoint(ip4);
