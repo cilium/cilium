408a56713bd66161a12f9d692d62136d4dbcb928
Dario Mader <maderdario@gmail.com>
2023-11-17 10:41:26 +0100

    connectivity: Add latency measurement
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Add netperf based latency tests that can be triggered by running `cilium
    connectivity test --perf --perf-latency --perf-samples 5`
    
    Signed-off-by: Dario Mader <maderdario@gmail.com>

--- cilium-cli/connectivity/tests/perfpod.go
+++ cilium-cli/connectivity/tests/perfpod.go
@@ -31,6 +31,7 @@ type netPerfPodtoPod struct {
 }
 
 var netPerfRegex = regexp.MustCompile(`\s+\d+\s+\d+\s+(\d+|\S+)\s+(\S+|\d+)\s+(\S+)+\s+(\S+)?`)
+var netPerfRegexLatency = regexp.MustCompile(`(\d+(?:\.\d+)?),(\d+(?:\.\d+)?),(\d+(?:\.\d+)?),(\d+(?:\.\d+)?),(\d+(?:\.\d+)?),(\d+(?:\.\d+)?)`)
 
 func (s *netPerfPodtoPod) Name() string {
 	tn := "perf-pod-to-pod"
@@ -44,6 +45,7 @@ func (s *netPerfPodtoPod) Run(ctx context.Context, t *check.Test) {
 	samples := t.Context().Params().PerfSamples
 	duration := t.Context().Params().PerfDuration
 	crr := t.Context().Params().PerfCRR
+	latency := t.Context().Params().PerfLatency
 	for _, c := range t.Context().PerfClientPods() {
 		c := c
 		for _, server := range t.Context().PerfServerPod() {
@@ -57,7 +59,9 @@ func (s *netPerfPodtoPod) Run(ctx context.Context, t *check.Test) {
 			action.CollectFlows = false
 			action.Run(func(a *check.Action) {
 				if crr {
-					netperf(ctx, server.Pod.Status.PodIP, c.Pod.Name, "TCP_CRR", a, t.Context().PerfResults, 1, 30, scenarioName)
+					netperf(ctx, server.Pod.Status.PodIP, c.Pod.Name, "TCP_CRR", a, t.Context().PerfResults, samples, duration, scenarioName)
+				} else if latency {
+					netperf(ctx, server.Pod.Status.PodIP, c.Pod.Name, "TCP_RR_LATENCY", a, t.Context().PerfResults, samples, duration, scenarioName)
 				} else {
 					netperf(ctx, server.Pod.Status.PodIP, c.Pod.Name, "TCP_RR", a, t.Context().PerfResults, samples, duration, scenarioName)
 					netperf(ctx, server.Pod.Status.PodIP, c.Pod.Name, "TCP_STREAM", a, t.Context().PerfResults, samples, duration, scenarioName)
@@ -69,47 +73,97 @@ func (s *netPerfPodtoPod) Run(ctx context.Context, t *check.Test) {
 	}
 }
 
+func buildExecCommand(test string, sip string, duration time.Duration, args []string) []string {
+	exec := []string{"/usr/local/bin/netperf", "-H", sip, "-l", duration.String(), "-t", test, "--", "-R", "1", "-m", fmt.Sprintf("%d", messageSize)}
+	exec = append(exec, args...)
+
+	return exec
+}
+
 func netperf(ctx context.Context, sip string, podname string, test string, a *check.Action, result map[check.PerfTests]check.PerfResult, samples int, duration time.Duration, scenarioName string) {
 	// Define test about to be executed and from which pod
 	k := check.PerfTests{
 		Pod:  podname,
 		Test: test,
 	}
-	metric := string("OP/s")
-	if strings.Contains(test, "STREAM") {
-		metric = "Mb/s"
+
+	res := check.PerfResult{
+		Duration: duration,
+		Samples:  samples,
+		Scenario: scenarioName,
 	}
 
-	exec := []string{"/usr/local/bin/netperf", "-H", sip, "-l", duration.String(), "-t", test, "--", "-R", "1", "-m", fmt.Sprintf("%d", messageSize)}
-	//  recv socketsize		send socketsize 	msg size|okmsg	duration	value
-	values := []float64{}
-	// Result data
-	for i := 0; i < samples; i++ {
-		a.ExecInPod(ctx, exec)
-		d := netPerfRegex.FindStringSubmatch(a.CmdOutput())
-		if len(d) < 5 {
-			a.Fatal("Unable to process netperf result")
+	if strings.Contains(test, "LATENCY") {
+		test = strings.ReplaceAll(test, "_LATENCY", "")
+		k.Test = test
+		metric := string("μs")
+		latency := make(map[string][]float64)
+
+		args := []string{"-o", "min_latency,mean_latency,max_latency,P50_LATENCY,P90_LATENCY,P99_LATENCY"}
+		exec := buildExecCommand(test, sip, duration, args)
+
+		latencyMetricNames := []string{
+			"min", "mean", "max", "p50", "p90", "p99",
 		}
-		nv := ""
-		if len(d[len(d)-1]) > 0 {
-			nv = d[len(d)-1]
-		} else {
-			nv = d[len(d)-2]
+
+		var latencyMetricValue float64
+		var err error
+		for i := 0; i < samples; i++ {
+			a.ExecInPod(ctx, exec)
+			d := netPerfRegexLatency.FindStringSubmatch(a.CmdOutput())
+
+			if len(d) != 7 {
+				a.Fatal("Unable to process netperf result")
+			}
+
+			for m, metric := range latencyMetricNames {
+				latencyMetricValue, err = strconv.ParseFloat(d[m+1], 64)
+				if err != nil {
+					a.Fatal(fmt.Sprintf("Unable to parse netperf result %s", metric))
+				}
+				latency[metric] = append(latency[metric], latencyMetricValue)
+			}
 		}
-		f, err := strconv.ParseFloat(nv, 64)
-		if err == nil {
-			values = append(values, f)
-		} else {
-			a.Fatal("Unable to parse netperf result")
+
+		for _, metric := range latencyMetricNames {
+			latency[metric] = []float64{listAvg(latency[metric])}
 		}
-	}
-	res := check.PerfResult{
-		Scenario: scenarioName,
-		Metric:   metric,
-		Duration: duration,
-		Values:   values,
-		Samples:  samples,
-		Avg:      listAvg(values),
+
+		res.Metric = metric
+		res.Latency = latency
+	} else {
+		metric := string("OP/s")
+		if strings.Contains(test, "STREAM") {
+			metric = "Mb/s"
+		}
+
+		exec := buildExecCommand(test, sip, duration, []string{})
+		//  recv socketsize		send socketsize 	msg size|okmsg	duration	value
+		// Result data
+		values := []float64{}
+		for i := 0; i < samples; i++ {
+			a.ExecInPod(ctx, exec)
+			d := netPerfRegex.FindStringSubmatch(a.CmdOutput())
+			if len(d) < 5 {
+				a.Fatal("Unable to process netperf result")
+			}
+			nv := ""
+			if len(d[len(d)-1]) > 0 {
+				nv = d[len(d)-1]
+			} else {
+				nv = d[len(d)-2]
+			}
+			f, err := strconv.ParseFloat(nv, 64)
+			if err == nil {
+				values = append(values, f)
+			} else {
+				a.Fatal("Unable to parse netperf result")
+			}
+		}
+
+		res.Metric = metric
+		res.Values = values
+		res.Avg = listAvg(values)
 	}
 	result[k] = res
 }
