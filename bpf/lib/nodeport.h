cilium/cilium

commit eff26cf680e5dcfce17ff0553b02f183c4200568

datapath: Fix double SNAT
We have observed, that the same packet can be handled multiple times by
the bpf_host's to-netdev. This can happen when the to-netdev is attached
to a bridge and to an outgoing netdev which is attached to the bridge.
This can result e.g., into multiple unnecessary SNATs for the same
packet which can break the host-firewall (the host-firewall for a reply
to such a packet is invoked after only the first rev-SNAT).

To fix that particular case set the SNAT done flag (an SKB mark).

Tested manually. On kind-worker node (172.18.0.3):

    ip link add br0 type bridge
    ip addr add 172.18.0.3/16 dev br0
    ip addr del 172.18.0.3/16 dev eth0
    ip link set dev br0 up
    ip link set dev eth0 master br0
    ip route replace 0.0.0.0/0 via 172.18.0.1

Make sure that Cilium attached to both:

    cilium status | grep KubeProxy
    KubeProxyReplacement:    Strict [br0 172.18.0.3, eth0 172.18.0.3]

Then start any pod on kind-worker, and run tcpdump on kind-work. curl
1.1.1.1 from that pod.

    tcpdump -i any -n 'dst port 80'
    lxc2663158aafe1 In  IP 10.244.1.161.43710 > 1.1.1.1.80: Flags [S]
    br0   Out IP 172.18.0.3.43710 > 1.1.1.1.80: Flags [S]
    eth0  Out IP 172.18.0.3.43710 > 1.1.1.1.80: Flags [S]

Redo the same test w/o the fix. The relevant tcpdump output:

    tcpdump -i any -n 'dst host 1.1.1.1'
    lxcbce3c2f349e1 In  IP 10.244.1.120.57408 > 1.1.1.1.80: Flags [S]
    br0   Out IP 172.18.0.3.57408 > 1.1.1.1.80: Flags [S]
    eth0  Out IP 172.18.0.3.56763 > 1.1.1.1.80: Flags [S] <-- 2nd SNAT!

Suggested-by: Julian Wiedmann <jwi@isovalent.com>
Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/lib/nodeport.h
@@ -114,6 +114,10 @@ static __always_inline int nodeport_snat_fwd_ipv6(struct __ctx_buff *ctx,
 	      snat_v6_nat(ctx, &target, ext_err) : CTX_ACT_OK;
 	if (ret == NAT_PUNT_TO_STACK)
 		ret = CTX_ACT_OK;
+
+	/* See the equivalent v4 path for comment */
+	ctx_snat_done_set(ctx);
+
 	return ret;
 }
 
@@ -1375,6 +1379,12 @@ static __always_inline int nodeport_snat_fwd_ipv4(struct __ctx_buff *ctx,
 		ret = snat_v4_nat(ctx, &target, ext_err);
 	if (ret == NAT_PUNT_TO_STACK)
 		ret = CTX_ACT_OK;
+
+	/* If multiple netdevs process an outgoing packet, then this packets will
+	 * be handled multiple times by the "to-netdev" section. This can lead
+	 * to multiple SNATs. To prevent from that, set the SNAT done flag.
+	 */
+	ctx_snat_done_set(ctx);
 
 	return ret;
 }

commit 4a9fd60f6202e9c69b066db82f50080fc7e2c8e0

bpf: nat: only set SNAT_DONE when packet was actually SNATed
With eff26cf680e5 ("datapath: Fix double SNAT") the outbound SNAT path now
sets ctx_snat_done_set() after checking whether a packet requires SNAT.
This was meant to avoid double-NATing when a packet passes through multiple
network interfaces with a to-netdev program.

But looking at the SNAT code in detail, some of its conditions only apply
on specific interfaces (see nodeport_has_nat_conflict_ipv4(), which
checks for `NATIVE_DEV_IFINDEX == DIRECT_ROUTING_DEV_IFINDEX`). So if a
packet passes through multiple interfaces which all have `to-netdev`
attached, the first `to-netdev` program might set SNAT_DONE even when some
subsequent program (attached to DIRECT_ROUTING_DEV_IFINDEX) would still
want to apply SNAT to the packet.

Therefore we should apply the SNAT checks on *each* interface, until we
have actually SNATed the packet. Only then set the SNAT_DONE marker.

Note that this also fixes an EgressGW bug in nodeport_snat_fwd_ipv4(),
where we would redirect the packet even if snat_v4_nat() reported an error.

Fixes: eff26cf680e5 ("datapath: Fix double SNAT")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -221,13 +221,15 @@ static __always_inline int nodeport_snat_fwd_ipv6(struct __ctx_buff *ctx,
 
 apply_snat:
 	ret = snat_v6_nat(ctx, &tuple, l4_off, &target, trace, ext_err);
+	if (IS_ERR(ret))
+		goto out;
+
+	/* See the equivalent v4 path for comment */
+	ctx_snat_done_set(ctx);
 
 out:
 	if (ret == NAT_PUNT_TO_STACK)
 		ret = CTX_ACT_OK;
-
-	/* See the equivalent v4 path for comment */
-	ctx_snat_done_set(ctx);
 
 	return ret;
 }
@@ -1680,10 +1682,8 @@ static __always_inline int nodeport_snat_fwd_ipv4(struct __ctx_buff *ctx,
 apply_snat:
 	ret = snat_v4_nat(ctx, &tuple, ip4, l4_off, ipv4_has_l4_header(ip4),
 			  &target, trace, ext_err);
-
-out:
-	if (ret == NAT_PUNT_TO_STACK)
-		ret = CTX_ACT_OK;
+	if (IS_ERR(ret))
+		goto out;
 
 	/* If multiple netdevs process an outgoing packet, then this packets will
 	 * be handled multiple times by the "to-netdev" section. This can lead
@@ -1695,6 +1695,10 @@ out:
 	if (target.egress_gateway)
 		return egress_gw_fib_lookup_and_redirect(ctx, target.addr, tuple.daddr, ext_err);
 #endif
+
+out:
+	if (ret == NAT_PUNT_TO_STACK)
+		ret = CTX_ACT_OK;
 
 	return ret;
 }