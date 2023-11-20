165f5afdb265d70e01c9263c5d242aeb0e4bd8bf
Jarno Rajahalme <jarno@isovalent.com>
2023-11-20 16:09:30 +0200

    dnsproxy: Do not use original source when not possible
    [ upstream commit 824e969f26d8bc68ee1a00cddbe25c29a876544c ]
    
    Do not use original source for server running in the local node, or when
    the destination is outside of the cluster, as there is a risk of missing
    masquarade on the upstream connection.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- pkg/identity/numericidentity.go
+++ pkg/identity/numericidentity.go
@@ -561,3 +561,13 @@ func (id NumericIdentity) HasLocalScope() bool {
 func (id NumericIdentity) IsWorld() bool {
 	return id == ReservedIdentityWorld
 }
+
+// IsCluster returns true if the identity is a cluster identity by excluding all
+// identities that are known to be non-cluster identities.
+// NOTE: keep this and bpf identity_is_cluster() in sync!
+func (id NumericIdentity) IsCluster() bool {
+	if id.IsWorld() || id.HasLocalScope() {
+		return false
+	}
+	return true
+}
