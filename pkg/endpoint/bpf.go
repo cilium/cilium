fdbca0562e4f207fc50b54e2f0318645f647d06a
Jarno Rajahalme <jarno@isovalent.com>
2023-11-21 12:30:27 +0200

    endpoint: Resolve named ports for redirects
    Implement l4Policy wrapper with resolved named port so that proxy package
    can get the actual resolved named port number when calling
    GetPort(). Previously, if policy used a named port, GetPort returned 0.
    
    GetPort() is only used for DNS proxy function UpdateAllowed(). This means
    that using a named port for a DNS policy destination port likely has not
    functioned as intended. Since DNS port is typically 53, it seems likely
    that named ports are not used in DNS policies.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- pkg/endpoint/bpf.go
+++ pkg/endpoint/bpf.go
@@ -171,6 +171,25 @@ func (e *Endpoint) writeHeaderfile(prefix string) error {
 	return f.CloseAtomicallyReplace()
 }
 
+// proxyPolicy implements policy.ProxyPolicy interface, and passes most of the calls
+// to policy.L4Filter, but re-implements GetPort() to return the resolved named port,
+// instead of returning a 0 port number.
+type proxyPolicy struct {
+	*policy.L4Filter
+	port uint16
+}
+
+// newProxyPolicy returns a new instance of proxyPolicy by value
+func (e *Endpoint) newProxyPolicy(l4 *policy.L4Filter, port uint16) proxyPolicy {
+	return proxyPolicy{L4Filter: l4, port: port}
+}
+
+// GetPort returns the destination port number on which the proxy policy applies
+// This version properly returns the port resolved from a named port, if any.
+func (p *proxyPolicy) GetPort() uint16 {
+	return p.port
+}
+
 // addNewRedirectsFromDesiredPolicy must be called while holding the endpoint lock for
 // writing. On success, returns nil; otherwise, returns an error indicating the
 // problem that occurred while adding an l7 redirect for the specified policy.
@@ -202,7 +221,9 @@ func (e *Endpoint) addNewRedirectsFromDesiredPolicy(ingress bool, desiredRedirec
 				var finalizeFunc revert.FinalizeFunc
 				var revertFunc revert.RevertFunc
 
-				proxyID := e.proxyID(l4)
+				// proxyID() returns also the destination port for the policy,
+				// which may be resolved from a named port
+				proxyID, dstPort := e.proxyID(l4)
 				if proxyID == "" {
 					// Skip redirects for which a proxyID cannot be created.
 					// This may happen due to the named port mapping not
@@ -213,8 +234,9 @@ func (e *Endpoint) addNewRedirectsFromDesiredPolicy(ingress bool, desiredRedirec
 					return 0, false
 				}
 
+				pp := e.newProxyPolicy(l4, dstPort)
 				var err error
-				redirectPort, err, finalizeFunc, revertFunc = e.proxy.CreateOrUpdateRedirect(e.aliveCtx, l4, proxyID, e, proxyWaitGroup)
+				redirectPort, err, finalizeFunc, revertFunc = e.proxy.CreateOrUpdateRedirect(e.aliveCtx, &pp, proxyID, e, proxyWaitGroup)
 				if err != nil {
 					// Skip redirects that can not be created or updated.  This
 					// can happen when a listener is missing, for example when
