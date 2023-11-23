cilium/cilium

commit f34371ce8f27387ecb06434a0aec26dbb3622144

ipam: Add routes for cilium_host ENI address
Previously, we weren't adding policy based routing rules
for cilium_host secondary IP address. These rules ensure
that traffic from source node's cilium_host interface destined to the
remote node in the cluster VPC is routed through the primary
ENI interface for cilium_host. From there on, the traffic is routed to the
AWS infrastructure router, which in turn routes it to the destination node.

Fix involves setting up ip rules to the per ENI routing table.

Verified that the correct rule is added, and encrypted traffic is
reaching the destination node.

root@ip-192-168-38-245:/home/cilium# ip addr show cilium_host
5: cilium_host@cilium_net: <BROADCAST,MULTICAST,NOARP,UP,LOWER_UP> mtu 9001 qdisc noqueue state UP group default qlen 1000
    link/ether da:61:19:32:0b:37 brd ff:ff:ff:ff:ff:ff
    inet 192.168.148.108/32 scope link cilium_host
       valid_lft forever preferred_lft forever
    inet6 fe80::d861:19ff:fe32:b37/64 scope link
       valid_lft forever preferred_lft forever
root@ip-192-168-38-245:/home/cilium# ip rule show | grep 192.168.148.108
111:	from 192.168.148.108 to 192.168.0.0/16 lookup 11

Tcpdump on the remote node shows encrypted packets getting received -

tcpdump -eni eth1 dst 192.168.142.218
tcpdump: verbose output suppressed, use -v or -vv for full protocol decode
listening on eth1, link-type EN10MB (Ethernet), capture size 262144 bytes
17:32:52.216989 02:93:9c:00:a2:eb > 02:72:ef:86:3e:17, ethertype IPv4 (0x0800), length 130: 192.168.117.26 > 192.168.142.218: ESP(spi=0x00000003,seq=0x64), length 96
17:32:52.217694 02:93:9c:00:a2:eb > 02:72:ef:86:3e:17, ethertype IPv4 (0x0800), length 122: 192.168.117.26 > 192.168.142.218: ESP(spi=0x00000003,seq=0x65), length 88
17:32:52.217896 02:93:9c:00:a2:eb > 02:72:ef:86:3e:17, ethertype IPv4 (0x0800), length 206: 192.168.117.26 > 192.168.142.218: ESP(spi=0x00000003,seq=0x66), length 172

Signed-off-by: Aditi Ghag <aditi@cilium.io>

pkg/datapath/linux/routing/routing.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/mac"
+	"github.com/cilium/cilium/pkg/option"
 
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
@@ -195,6 +196,33 @@ func Delete(ip net.IP, compat bool) error {
 	return nil
 }
 
+// SetupRules installs routing rules based on the passed attributes. It accounts
+// for option.Config.EgressMultiHomeIPRuleCompat while configuring the rules.
+func SetupRules(from, to *net.IPNet, mac string, ifaceNum int) error {
+	var (
+		prio    int
+		tableId int
+	)
+
+	if option.Config.EgressMultiHomeIPRuleCompat {
+		prio = linux_defaults.RulePriorityEgress
+		ifindex, err := retrieveIfaceIdxFromMAC(mac)
+		if err != nil {
+			return fmt.Errorf("unable to find ifindex for interface MAC: %w", err)
+		}
+		tableId = ifindex
+	} else {
+		prio = linux_defaults.RulePriorityEgressv2
+		tableId = computeTableIDFromIfaceNumber(ifaceNum)
+	}
+	return route.ReplaceRule(route.Rule{
+		Priority: prio,
+		From:     from,
+		To:       to,
+		Table:    tableId,
+	})
+}
+
 func deleteRule(r route.Rule) error {
 	rules, err := route.ListRules(netlink.FAMILY_V4, &r)
 	if err != nil {
@@ -255,6 +283,41 @@ func retrieveIfIndexFromMAC(mac mac.MAC, mtu int) (index int, err error) {
 	return
 }
 
+// computeTableIDFromIfaceNumber returns a computed per-ENI route table ID for the given
+// ENI interface number.
 func computeTableIDFromIfaceNumber(num int) int {
 	return linux_defaults.RouteTableInterfacesOffset + num
+}
+
+// retrieveIfaceIdxFromMAC finds the corresponding interface index for a
+// given MAC address.
+// It returns -1 as the index for error conditions.
+func retrieveIfaceIdxFromMAC(mac string) (int, error) {
+	iface, err := retrieveIfaceFromMAC(mac)
+	if err != nil {
+		err = fmt.Errorf("failed to get iface index with MAC %w", err)
+		return -1, err
+	}
+	return iface.Attrs().Index, nil
+}
+
+// retrieveIfaceFromFromMAC finds the corresponding interface for a
+// given MAC address.
+func retrieveIfaceFromMAC(mac string) (link netlink.Link, err error) {
+	var links []netlink.Link
+
+	links, err = netlink.LinkList()
+	if err != nil {
+		err = fmt.Errorf("unable to list interfaces: %w", err)
+		return
+	}
+	for _, l := range links {
+		if l.Attrs().HardwareAddr.String() == mac {
+			link = l
+			return
+		}
+	}
+
+	err = fmt.Errorf("interface with MAC not found")
+	return
 }

pkg/datapath/loader/base.go
@@ -18,6 +18,7 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 	"strings"
@@ -30,6 +31,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/alignchecker"
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
+	linuxrouting "github.com/cilium/cilium/pkg/datapath/linux/routing"
 	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/datapath/prefilter"
 	"github.com/cilium/cilium/pkg/defaults"
@@ -117,7 +119,7 @@ func writePreFilterHeader(preFilter *prefilter.PreFilter, dir string) error {
 	return fw.Flush()
 }
 
-func addENIRules(sysSettings []sysctl.Setting) ([]sysctl.Setting, error) {
+func addENIRules(sysSettings []sysctl.Setting, nodeAddressing datapath.NodeAddressing) ([]sysctl.Setting, error) {
 	// AWS ENI mode requires symmetric routing, see
 	// iptables.addCiliumENIRules().
 	// The default AWS daemonset installs the following rules that are used
@@ -156,6 +158,19 @@ func addENIRules(sysSettings []sysctl.Setting) ([]sysctl.Setting, error) {
 		Table:    route.MainTable,
 	}); err != nil {
 		return nil, fmt.Errorf("unable to install ip rule for ENI multi-node NodePort: %w", err)
+	}
+
+	// Add rules for router (cilium_host).
+	info := node.GetRouterInfo()
+	cidrs := info.GetIPv4CIDRs()
+	routerIP := net.IPNet{
+		IP:   nodeAddressing.IPv4().Router(),
+		Mask: net.CIDRMask(32, 32),
+	}
+	for _, cidr := range cidrs {
+		if err = linuxrouting.SetupRules(&routerIP, &cidr, info.GetMac().String(), info.GetInterfaceNumber()); err != nil {
+			return nil, fmt.Errorf("unable to install ip rule for cilium_host: %w", err)
+		}
 	}
 
 	return retSettings, nil
@@ -332,7 +347,7 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 
 	if option.Config.IPAM == ipamOption.IPAMENI {
 		var err error
-		if sysSettings, err = addENIRules(sysSettings); err != nil {
+		if sysSettings, err = addENIRules(sysSettings, o.Datapath().LocalNodeAddressing()); err != nil {
 			return fmt.Errorf("unable to install ip rule for ENI multi-node NodePort: %w", err)
 		}
 	}

commit f4b9743ac75ed66de00eb9d613ae2ae4cc85b9f9

datapath: Fix ENI egress routing table for cilium_host IP
On ENI, we install source-based egress routing rules that steer traffic
from Cilium-mananged IPs (i.e. pods, but also the health IP, ingress IP
and router IP) to the correct egress interface.

For pod IPs, this is done in the CNI plugin:

https://github.com/cilium/cilium/blob/7875f6acb5a2fd2b0e3e6c993c9995c0d322e55d/plugins/cilium-cni/interface.go#L59-L63

For ingress and health IP, this is done from cilium-agent:

https://github.com/cilium/cilium/blob/ed20c8acde8c76d405d6c9fac3c9de44aa3bb403/daemon/cmd/ipam.go#L401-L405
https://github.com/cilium/cilium/blob/e49430286b5d63b00062758a10a2b37458f94525/cilium-health/launch/endpoint.go#L329-L333

For the `cilium_host` (aka router) IP however, this was done differently.
Commit f34371ce8f added a new `routing.SetupRules` function that
duplicated parts of the `routing.RoutingInfo.Configure` logic, but
missed a crucial part: Namely the creation of the per-ENI routing table
that the source-based egress rule points towards.

This means that if the `cilium_host` IP address was allocated from a
different ENI than the pod, health and ingress IP addresses, that the
routing table for that ENI was never created. This led to connectivity
issues, in particular in combination with IPSec.

This commit addresses that issue by having the `cilium_host` IP use the same
code path as the other IP users: Using `RoutingInfo.Configure`. This not
only fixes the bug, but removes some code that was otherwise only used
for the router IP.

There is one major difference between other users of
`RoutingInfo.Configure` and the newly introduced use for the
`cilium_host` IP: For the `cilium_host` IP, we skip the creation of the
ingress rule (by passing in `host=true`), as otherwise the `cilium_host`
IP would not be considered a local address of the host network
namespace. This is consistent with the old `SetupRules` function did
also not create such an ingress rule.

Long-term, it remains questionable if the setup of egress rules in ENI
mode should be left to IPAM clients, as every client seems to do it
slightly differently. Maybe this is better done by either the IPAM subsystem
or a separate device manager.

Fixes: f34371ce8f27 ("ipam: Add routes for cilium_host ENI address")

daemon/cmd/ipam.go
@@ -291,6 +291,15 @@ func (d *Daemon) allocateDatapathIPs(family types.NodeAddressingFamily, fromK8s,
 		if err != nil {
 			return nil, fmt.Errorf("failed to create router info %w", err)
 		}
+		if err = routingInfo.Configure(
+			result.IP,
+			d.mtuConfig.GetDeviceMTU(),
+			option.Config.EgressMultiHomeIPRuleCompat,
+			true,
+		); err != nil {
+			return nil, fmt.Errorf("failed to configure router IP rules and routes %w", err)
+		}
+
 		node.SetRouterInfo(routingInfo)
 	}
 

pkg/datapath/linux/routing/info.go
@@ -48,14 +48,6 @@ func (info *RoutingInfo) GetIPv4CIDRs() []net.IPNet {
 	return info.IPv4CIDRs
 }
 
-func (info *RoutingInfo) GetMac() mac.MAC {
-	return info.MasterIfMAC
-}
-
-func (info *RoutingInfo) GetInterfaceNumber() int {
-	return info.InterfaceNumber
-}
-
 // NewRoutingInfo creates a new RoutingInfo struct, from data that will be
 // parsed and validated. Note, this code assumes IPv4 values because IPv4
 // (on either ENI or Azure interface) is the only supported path currently.

pkg/datapath/linux/routing/routing.go
@@ -243,45 +243,6 @@ func Delete(ip netip.Addr, compat bool) error {
 	return nil
 }
 
-// SetupRules installs routing rules based on the passed attributes. It accounts
-// for option.Config.EgressMultiHomeIPRuleCompat while configuring the rules.
-func SetupRules(from, to *net.IPNet, mac string, ifaceNum int) error {
-	var (
-		prio    int
-		tableId int
-	)
-
-	if option.Config.EgressMultiHomeIPRuleCompat {
-		prio = linux_defaults.RulePriorityEgress
-		ifindex, err := retrieveIfaceIdxFromMAC(mac)
-		if err != nil {
-			return fmt.Errorf("unable to find ifindex for interface MAC: %w", err)
-		}
-		tableId = ifindex
-	} else {
-		prio = linux_defaults.RulePriorityEgressv2
-		tableId = computeTableIDFromIfaceNumber(ifaceNum)
-	}
-	return route.ReplaceRule(route.Rule{
-		Priority: prio,
-		From:     from,
-		To:       to,
-		Table:    tableId,
-		Protocol: linux_defaults.RTProto,
-	})
-}
-
-// RetrieveIfaceNameFromMAC finds the corresponding device name for a
-// given MAC address.
-func RetrieveIfaceNameFromMAC(mac string) (string, error) {
-	iface, err := retrieveIfaceFromMAC(mac)
-	if err != nil {
-		err = fmt.Errorf("failed to get iface name with MAC %w", err)
-		return "", err
-	}
-	return iface.Attrs().Name, nil
-}
-
 func deleteRule(r route.Rule) error {
 	rules, err := route.ListRules(netlink.FAMILY_V4, &r)
 	if err != nil {
@@ -351,37 +312,4 @@ func retrieveIfIndexFromMAC(mac mac.MAC, mtu int) (int, error) {
 // ENI interface number.
 func computeTableIDFromIfaceNumber(num int) int {
 	return linux_defaults.RouteTableInterfacesOffset + num
-}
-
-// retrieveIfaceIdxFromMAC finds the corresponding interface index for a
-// given MAC address.
-// It returns -1 as the index for error conditions.
-func retrieveIfaceIdxFromMAC(mac string) (int, error) {
-	iface, err := retrieveIfaceFromMAC(mac)
-	if err != nil {
-		err = fmt.Errorf("failed to get iface index with MAC %w", err)
-		return -1, err
-	}
-	return iface.Attrs().Index, nil
-}
-
-// retrieveIfaceFromFromMAC finds the corresponding interface for a
-// given MAC address.
-func retrieveIfaceFromMAC(mac string) (link netlink.Link, err error) {
-	var links []netlink.Link
-
-	links, err = netlink.LinkList()
-	if err != nil {
-		err = fmt.Errorf("unable to list interfaces: %w", err)
-		return
-	}
-	for _, l := range links {
-		if l.Attrs().HardwareAddr.String() == mac {
-			link = l
-			return
-		}
-	}
-
-	err = fmt.Errorf("interface with MAC not found")
-	return
 }

pkg/datapath/loader/base.go
@@ -20,7 +20,6 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/linux/ethtool"
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
-	linuxrouting "github.com/cilium/cilium/pkg/datapath/linux/routing"
 	"github.com/cilium/cilium/pkg/datapath/prefilter"
 	"github.com/cilium/cilium/pkg/datapath/tunnel"
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
@@ -98,7 +97,7 @@ func writePreFilterHeader(preFilter *prefilter.PreFilter, dir string) error {
 	return fw.Flush()
 }
 
-func addENIRules(sysSettings []sysctl.Setting, nodeAddressing datapath.NodeAddressing) ([]sysctl.Setting, error) {
+func addENIRules(sysSettings []sysctl.Setting) ([]sysctl.Setting, error) {
 	// AWS ENI mode requires symmetric routing, see
 	// iptables.addCiliumENIRules().
 	// The default AWS daemonset installs the following rules that are used
@@ -138,20 +137,6 @@ func addENIRules(sysSettings []sysctl.Setting, nodeAddressing datapath.NodeAddre
 		Protocol: linux_defaults.RTProto,
 	}); err != nil {
 		return nil, fmt.Errorf("unable to install ip rule for ENI multi-node NodePort: %w", err)
-	}
-
-	// Add rules for router (cilium_host).
-	info := node.GetRouterInfo()
-	cidrs := info.GetIPv4CIDRs()
-	routerIP := net.IPNet{
-		IP:   nodeAddressing.IPv4().Router(),
-		Mask: net.CIDRMask(32, 32),
-	}
-
-	for _, cidr := range cidrs {
-		if err = linuxrouting.SetupRules(&routerIP, &cidr, info.GetMac().String(), info.GetInterfaceNumber()); err != nil {
-			return nil, fmt.Errorf("unable to install ip rule for cilium_host: %w", err)
-		}
 	}
 
 	return retSettings, nil
@@ -327,7 +312,7 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 
 	if option.Config.IPAM == ipamOption.IPAMENI {
 		var err error
-		if sysSettings, err = addENIRules(sysSettings, o.Datapath().LocalNodeAddressing()); err != nil {
+		if sysSettings, err = addENIRules(sysSettings); err != nil {
 			return fmt.Errorf("unable to install ip rule for ENI multi-node NodePort: %w", err)
 		}
 	}

pkg/node/address.go
@@ -21,7 +21,6 @@ import (
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/mac"
 	"github.com/cilium/cilium/pkg/option"
 	wgTypes "github.com/cilium/cilium/pkg/wireguard/types"
 )
@@ -66,8 +65,6 @@ type addresses struct {
 
 type RouterInfo interface {
 	GetIPv4CIDRs() []net.IPNet
-	GetMac() mac.MAC
-	GetInterfaceNumber() int
 }
 
 func makeIPv6HostIP() net.IP {