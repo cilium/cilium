cilium/cilium

commit 23e762cffd906e8ab88b367c170cf2adf792d014

node: add extra validation
Add a validation function to prevent unmarshalling invalid node
objects retrieved from the kvstore, which could then possibly
trigger unexpected behavior. Currently, we only check that the
cluster ID is in the valid range.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/node/types/node.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/cidr"
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/defaults"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
@@ -615,7 +616,23 @@ func (n *Node) Unmarshal(_ string, data []byte) error {
 		return err
 	}
 
+	if err := newNode.validate(); err != nil {
+		return err
+	}
+
 	*n = newNode
+
+	return nil
+}
+
+func (n *Node) validate() error {
+	// Skip the ClusterID check if it matches the local one, as we assume that
+	// it has already been validated, and to allow it to be zero.
+	if n.ClusterID != option.Config.ClusterID {
+		if err := cmtypes.ValidateClusterID(n.ClusterID); err != nil {
+			return err
+		}
+	}
 
 	return nil
 }

pkg/node/types/node_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	. "github.com/cilium/checkmate"
+	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/cilium/cilium/pkg/annotation"
@@ -241,4 +242,34 @@ func (s *NodeSuite) TestNode_ToCiliumNode(c *C) {
 			NodeIdentity: uint64(12345),
 		},
 	})
+}
+
+func TestClusterServiceValidate(t *testing.T) {
+	tests := []struct {
+		name   string
+		node   Node
+		assert assert.ErrorAssertionFunc
+	}{
+		{
+			name:   "empty cluster ID",
+			node:   Node{},
+			assert: assert.NoError,
+		},
+		{
+			name:   "valid cluster ID",
+			node:   Node{ClusterID: 99},
+			assert: assert.NoError,
+		},
+		{
+			name:   "invalid cluster ID",
+			node:   Node{ClusterID: 260},
+			assert: assert.Error,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.assert(t, tt.node.validate())
+		})
+	}
 }

commit 63130883f6db53ce15379ec41084b895fecfa65d

node: skip ClusterID validation for external workloads
23e762cffd90 ("node: add extra validation") introduced extra validation
logic executed when unmarshalling node objects retrieved from the kvstore,
ensuring in particular that the ClusterID is in the expected range and
non-zero (unless the local ClusterID is zero).

Yet, this validation broke the external workloads support, if the
ClusterID is set to a non-zero value. Indeed, the RegisterNode type
used in this case is just a wrapper for the Node one, and reuses the
same unmarshalling logic. Given that the RegisterNode object is initially
created configuring the name only, it causes an unmarshalling failure
when processed by the clustermesh-apiserver, due to the ClusterID being
zero (only if that of the cluster is different from zero).

To fix this issue, let's override the Unmarshal function to skip the
validation logic for the external workloads type, as used differently.
Another possibility could be to explicitly configure the ClusterID; yet
this would be trickier to backport to v1.14, as historically the
ClusterID was not configured for external workloads agents, and inferred
at runtime from that of the cluster it connected to.

Fixes: 23e762cffd90 ("node: add extra validation")
Fixes: #29355
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/node/types/node.go
@@ -203,6 +203,16 @@ func (n *RegisterNode) DeepKeyCopy() store.LocalKey {
 	return n.DeepCopy()
 }
 
+func (n *RegisterNode) Unmarshal(_ string, data []byte) error {
+	newNode := Node{}
+	if err := json.Unmarshal(data, &newNode); err != nil {
+		return err
+	}
+
+	n.Node = newNode
+	return nil
+}
+
 // Node contains the nodes name, the list of addresses to this address
 //
 // +k8s:deepcopy-gen=true