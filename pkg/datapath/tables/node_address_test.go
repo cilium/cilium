cilium/cilium

commit 9d7e452c20c8631a35413a9a9df5593fff2f3c4c

datapath: Reimplement NodeAddressing around Table[NodeAddress]
As a step towards removing the global address maps in pkg/node
(ipv4NodeportAddrs, ipv4MasqAddrs etc.), reimplement NodeAddressing
as queries against the Table[NodeAddress].

The goal is to eventually remove NodeAddressing and have modules
query the Table[NodeAddress] directly and to react to changes, but
as an intermediary step without having to refactor whole bunch of
uses of NodeAddressing, this still serves the purpose of allowing
removal of some global variables and removing the other ways of
extracting the device and address information from the system.

Signed-off-by: Jussi Maki <jussi@isovalent.com>
Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

pkg/datapath/tables/node_address.go
@@ -302,7 +302,6 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device) (addrs sets.
 		if n.AddressScopeMax < unix.RT_SCOPE_LINK {
 			for _, addr := range sortedAddresses(dev.Addrs) {
 				if addr.Scope == unix.RT_SCOPE_LINK {
-					n.Log.Infof("HII2 - adding addr: %v prim: %v np: %v dev: %v", addr.Addr, !addr.Secondary, false, dev.Name)
 					addrs.Insert(NodeAddress{
 						Addr:       addr.Addr,
 						NodePort:   false,
@@ -361,8 +360,6 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device) (addrs sets.
 				ipv6Found = true
 			}
 		}
-
-		n.Log.Infof("HII - adding addr: %v prim: %v np: %v dev: %v", addr.Addr, primary, nodePort, dev.Name)
 
 		addrs.Insert(NodeAddress{
 			Addr:       addr.Addr,

pkg/datapath/cells.go
@@ -71,6 +71,9 @@ var Cell = cell.Module(
 	// Provides the Table[NodeAddress] and the controller that populates it from Table[*Device]
 	tables.NodeAddressCell,
 
+	// Provides the legacy accessor for the above, the NodeAddressing interface.
+	tables.NodeAddressingCell,
+
 	// This cell periodically updates the agent liveness value in configmap.Map to inform
 	// the datapath of the liveness of the agent.
 	agentliveness.Cell,
@@ -104,7 +107,9 @@ var Cell = cell.Module(
 		// Provide the configured devices to the devices controller.
 		// This is temporary until DevicesController takes ownership of the
 		// device-related configuration options.
-		return linuxdatapath.DevicesConfig{Devices: cfg.GetDevices()}
+		return linuxdatapath.DevicesConfig{
+			Devices: cfg.GetDevices(),
+		}
 	}),
 
 	// Synchronizes the userspace ipcache with the corresponding BPF map.
@@ -147,10 +152,11 @@ func newDatapath(params datapathParams) types.Datapath {
 	}
 
 	datapath := linuxdatapath.NewDatapath(linuxdatapath.DatapathParams{
-		ConfigWriter: params.ConfigWriter,
-		RuleManager:  params.IptablesManager,
-		WGAgent:      params.WgAgent,
-		NodeMap:      params.NodeMap,
+		ConfigWriter:   params.ConfigWriter,
+		RuleManager:    params.IptablesManager,
+		WGAgent:        params.WgAgent,
+		NodeMap:        params.NodeMap,
+		NodeAddressing: params.NodeAddressing,
 	}, datapathConfig)
 
 	params.LC.Append(hive.Hook{
@@ -174,6 +180,8 @@ type datapathParams struct {
 	BpfMaps []bpf.BpfMap `group:"bpf-maps"`
 
 	NodeMap nodemap.Map
+
+	NodeAddressing types.NodeAddressing
 
 	// Depend on DeviceManager to ensure devices have been resolved.
 	// This is required until option.Config.GetDevices() has been removed and

pkg/datapath/fake/cells.go
@@ -4,6 +4,11 @@
 package fake
 
 import (
+	"net"
+	"net/netip"
+
+	"golang.org/x/sys/unix"
+
 	"github.com/cilium/cilium/pkg/datapath/iptables"
 	"github.com/cilium/cilium/pkg/datapath/linux/bandwidth"
 	"github.com/cilium/cilium/pkg/datapath/linux/bigtcp"
@@ -11,6 +16,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/tunnel"
 	"github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/maps/authmap"
 	"github.com/cilium/cilium/pkg/maps/egressmap"
 	"github.com/cilium/cilium/pkg/maps/signalmap"
@@ -23,14 +29,14 @@ import (
 
 // Cell provides a fake version of the datapath cell.
 //
-// Used in integration tests such as test/controlplane.
+// Used in integration tests in daemon/cmd and test/controlplane.
 var Cell = cell.Module(
 	"fake-datapath",
 	"Fake Datapath",
 
 	cell.Provide(
-		func() (*FakeDatapath, types.Datapath, types.NodeIDHandler) {
-			dp := NewDatapath()
+		func(na types.NodeAddressing) (*FakeDatapath, types.Datapath, types.NodeIDHandler) {
+			dp := newDatapath(na)
 			return dp, dp, dp.NodeIDs()
 		},
 
@@ -41,12 +47,13 @@ var Cell = cell.Module(
 		func() *iptables.Manager { return &iptables.Manager{} },
 		func() bandwidth.Manager { return &BandwidthManager{} },
 
-		tables.NewDeviceTable, statedb.RWTable[*tables.Device].ToTable,
+		tables.NewDeviceTable,
 		tables.NewL2AnnounceTable, statedb.RWTable[*tables.L2AnnounceEntry].ToTable,
 		tables.NewRouteTable, statedb.RWTable[*tables.Route].ToTable,
 	),
 
 	tables.NodeAddressCell,
+	tables.NodeAddressingCell,
 
 	cell.Invoke(
 		statedb.RegisterTable[*tables.Device],
@@ -55,4 +62,43 @@ var Cell = cell.Module(
 	),
 
 	tunnel.Cell,
+	cell.Provide(fakeDevices),
 )
+
+func fakeDevices(db *statedb.DB, devices statedb.RWTable[*tables.Device]) statedb.Table[*tables.Device] {
+	txn := db.WriteTxn(devices)
+	defer txn.Commit()
+
+	devices.Insert(txn, &tables.Device{
+		Index:        1,
+		MTU:          1500,
+		Name:         "test0",
+		HardwareAddr: []byte{1, 2, 3, 4, 5, 6},
+		Flags:        net.FlagUp,
+		Addrs: []tables.DeviceAddress{
+			{Addr: ip.MustAddrFromIP(IPv4NodePortAddress), Scope: unix.RT_SCOPE_UNIVERSE},
+			{Addr: ip.MustAddrFromIP(IPv6NodePortAddress), Scope: unix.RT_SCOPE_UNIVERSE},
+		},
+		Type:     "test",
+		Selected: true,
+	})
+
+	devices.Insert(txn, &tables.Device{
+		Index:        2,
+		MTU:          1500,
+		Name:         "test1",
+		HardwareAddr: []byte{2, 3, 4, 5, 6, 7},
+		Flags:        net.FlagUp,
+		Addrs: []tables.DeviceAddress{
+			{Addr: ip.MustAddrFromIP(IPv4InternalAddress), Scope: unix.RT_SCOPE_UNIVERSE},
+			{Addr: ip.MustAddrFromIP(IPv6InternalAddress), Scope: unix.RT_SCOPE_UNIVERSE},
+
+			{Addr: netip.MustParseAddr("10.0.0.4"), Scope: unix.RT_SCOPE_UNIVERSE, Secondary: true},
+			{Addr: netip.MustParseAddr("f00d::3"), Scope: unix.RT_SCOPE_UNIVERSE, Secondary: true},
+		},
+		Type:     "test",
+		Selected: true,
+	})
+
+	return devices
+}

pkg/datapath/fake/datapath.go
@@ -25,9 +25,13 @@ type FakeDatapath struct {
 
 // NewDatapath returns a new fake datapath
 func NewDatapath() *FakeDatapath {
+	return newDatapath(NewNodeAddressing())
+}
+
+func newDatapath(na datapath.NodeAddressing) *FakeDatapath {
 	return &FakeDatapath{
 		node:           NewNodeHandler(),
-		nodeAddressing: NewNodeAddressing(),
+		nodeAddressing: na,
 		loader:         &fakeLoader{},
 		lbmap:          mockmaps.NewLBMockMap(),
 	}

pkg/datapath/linux/datapath.go
@@ -36,11 +36,12 @@ type linuxDatapath struct {
 }
 
 type DatapathParams struct {
-	ConfigWriter datapath.ConfigWriter
-	RuleManager  datapath.IptablesManager
-	WGAgent      datapath.WireguardAgent
-	NodeMap      nodemap.Map
-	BWManager    bandwidth.Manager
+	ConfigWriter   datapath.ConfigWriter
+	RuleManager    datapath.IptablesManager
+	WGAgent        datapath.WireguardAgent
+	NodeMap        nodemap.Map
+	BWManager      bandwidth.Manager
+	NodeAddressing datapath.NodeAddressing
 }
 
 // NewDatapath creates a new Linux datapath
@@ -48,7 +49,7 @@ func NewDatapath(p DatapathParams, cfg DatapathConfiguration) datapath.Datapath 
 	dp := &linuxDatapath{
 		ConfigWriter:    p.ConfigWriter,
 		IptablesManager: p.RuleManager,
-		nodeAddressing:  NewNodeAddressing(),
+		nodeAddressing:  p.NodeAddressing,
 		config:          cfg,
 		loader:          loader.NewLoader(),
 		wgAgent:         p.WGAgent,

pkg/datapath/linux/node_addressing.go
@@ -1,163 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-package linux
-
-import (
-	"net"
-
-	"github.com/vishvananda/netlink"
-
-	"github.com/cilium/cilium/pkg/cidr"
-	"github.com/cilium/cilium/pkg/datapath/types"
-	"github.com/cilium/cilium/pkg/defaults"
-	"github.com/cilium/cilium/pkg/ip"
-	"github.com/cilium/cilium/pkg/node"
-	"github.com/cilium/cilium/pkg/option"
-)
-
-// FIXME: This currently maps to the code in pkg/node/node_address.go. That
-// code should really move into this package.
-
-func listLocalAddresses(family int) ([]net.IP, error) {
-	var addresses []net.IP
-
-	ipsToExclude := node.GetExcludedIPs()
-	addrs, err := netlink.AddrList(nil, family)
-	if err != nil {
-		return nil, err
-	}
-
-	filteredIPs := filterLocalAddresses(addrs, ipsToExclude, option.Config.AddressScopeMax)
-	addresses = append(addresses, filteredIPs...)
-
-	// If AddressScopeMax is a scope more broad (numerically less than) than SCOPE_LINK then include
-	// all addresses at SCOPE_LINK which are assigned to the Cilium host device.
-	if option.Config.AddressScopeMax < int(netlink.SCOPE_LINK) {
-		if hostDevice, err := netlink.LinkByName(defaults.HostDevice); hostDevice != nil && err == nil {
-			addrs, err = netlink.AddrList(hostDevice, family)
-			if err != nil {
-				return nil, err
-			}
-			for _, addr := range addrs {
-				if addr.Scope == int(netlink.SCOPE_LINK) {
-					addresses = append(addresses, addr.IP)
-				}
-			}
-		}
-	}
-
-	return addresses, nil
-}
-
-func filterLocalAddresses(addrs []netlink.Addr, ipsToExclude []net.IP, addrScopeMax int) []net.IP {
-	var filteredIPs []net.IP
-	for _, addr := range addrs {
-		// This address is at a scope which is more narrow (numerically greater than) the configured
-		// max address scope. For example, if this addr is SCOPE_NOWHERE, and our addrScopeMax is
-		// SCOPE_LINK, then we do NOT treat the address as a local address. Similarly, if this addr
-		// is SCOPE_HOST, and our addrScopeMax is SCOPE_LINK, we do NOT treat the address as a local
-		// address.
-		if addr.Scope > addrScopeMax {
-			continue
-		}
-		if ip.ListContainsIP(ipsToExclude, addr.IP) {
-			continue
-		}
-		if addr.IP.IsLoopback() {
-			continue
-		}
-		filteredIPs = append(filteredIPs, addr.IP)
-	}
-	return filteredIPs
-}
-
-type addressFamilyIPv4 struct{}
-
-func (a *addressFamilyIPv4) Router() net.IP {
-	return node.GetInternalIPv4Router()
-}
-
-func (a *addressFamilyIPv4) PrimaryExternal() net.IP {
-	return node.GetIPv4()
-}
-
-func (a *addressFamilyIPv4) AllocationCIDR() *cidr.CIDR {
-	return node.GetIPv4AllocRange()
-}
-
-func (a *addressFamilyIPv4) LocalAddresses() ([]net.IP, error) {
-	addrs, err := listLocalAddresses(netlink.FAMILY_V4)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if externalAddress := node.GetK8sExternalIPv4(); externalAddress != nil {
-		addrs = append(addrs, externalAddress)
-	}
-
-	return addrs, nil
-}
-
-// LoadBalancerNodeAddresses returns all IPv4 node addresses on which the
-// loadbalancer should implement HostPort and NodePort services.
-func (a *addressFamilyIPv4) LoadBalancerNodeAddresses() []net.IP {
-	addrs := node.GetNodePortIPv4Addrs()
-	addrs = append(addrs, net.IPv4zero)
-	return addrs
-}
-
-type addressFamilyIPv6 struct{}
-
-func (a *addressFamilyIPv6) Router() net.IP {
-	return node.GetIPv6Router()
-}
-
-func (a *addressFamilyIPv6) PrimaryExternal() net.IP {
-	return node.GetIPv6()
-}
-
-func (a *addressFamilyIPv6) AllocationCIDR() *cidr.CIDR {
-	return node.GetIPv6AllocRange()
-}
-
-func (a *addressFamilyIPv6) LocalAddresses() ([]net.IP, error) {
-	addrs, err := listLocalAddresses(netlink.FAMILY_V6)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if externalAddress := node.GetK8sExternalIPv6(); externalAddress != nil {
-		addrs = append(addrs, externalAddress)
-	}
-
-	return addrs, nil
-}
-
-// LoadBalancerNodeAddresses returns all IPv6 node addresses on which the
-// loadbalancer should implement HostPort and NodePort services.
-func (a *addressFamilyIPv6) LoadBalancerNodeAddresses() []net.IP {
-	addrs := node.GetNodePortIPv6Addrs()
-	addrs = append(addrs, net.IPv6zero)
-	return addrs
-}
-
-type linuxNodeAddressing struct {
-	ipv6 addressFamilyIPv6
-	ipv4 addressFamilyIPv4
-}
-
-// NewNodeAddressing returns a new linux node addressing model
-func NewNodeAddressing() types.NodeAddressing {
-	return &linuxNodeAddressing{}
-}
-
-func (n *linuxNodeAddressing) IPv6() types.NodeAddressingFamily {
-	return &n.ipv6
-}
-
-func (n *linuxNodeAddressing) IPv4() types.NodeAddressingFamily {
-	return &n.ipv4
-}

pkg/datapath/linux/node_addressing_test.go
@@ -1,300 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-package linux
-
-import (
-	"net"
-	"reflect"
-	"testing"
-
-	"github.com/vishvananda/netlink"
-)
-
-func TestFilterLocalAddresses(t *testing.T) {
-	tests := []struct {
-		name         string
-		addrs        []netlink.Addr
-		ipsToExclude []net.IP
-		addrScopeMax int
-		want         []net.IP
-	}{
-		{
-			name: "simple",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-			},
-		},
-		{
-			name: "multiple",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-				net.ParseIP("10.0.0.2"),
-			},
-		},
-		{
-			name: "scopeMaxLink",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_LINK),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.3")},
-					Scope: int(netlink.SCOPE_NOWHERE),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_LINK),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-			},
-		},
-		{
-			name: "scopeMaxHost",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_LINK),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.3")},
-					Scope: int(netlink.SCOPE_NOWHERE),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-				net.ParseIP("10.0.0.2"),
-			},
-		},
-		{
-			name: "scopeMaxNowhere",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_LINK),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.3")},
-					Scope: int(netlink.SCOPE_NOWHERE),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_NOWHERE),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-				net.ParseIP("10.0.0.2"),
-				net.ParseIP("10.0.0.3"),
-			},
-		},
-		{
-			name: "exclude",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{
-				net.ParseIP("10.0.0.2"),
-			},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-			},
-		},
-		{
-			name: "excludeMultiple",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.2")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.3")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{
-				net.ParseIP("10.0.0.2"),
-				net.ParseIP("10.0.0.3"),
-			},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-			},
-		},
-		{
-			name: "ipv6 simple",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("2001:db8::")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("2001:db8::"),
-			},
-		},
-		{
-			name: "ipv6 multiple",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("2001:db8::")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("2600:beef::")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("2001:db8::"),
-				net.ParseIP("2600:beef::"),
-			},
-		},
-		{
-			name: "v4/v6 mix",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("2001:db8::")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-				net.ParseIP("2001:db8::"),
-			},
-		},
-		{
-			name: "v6 exclude",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("2001:db8::")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{
-				net.ParseIP("2001:db8::"),
-			},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-			},
-		},
-		{
-			name: "include link-local v4",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("169.254.20.10")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("169.254.169.254")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-				net.ParseIP("169.254.20.10"),
-				net.ParseIP("169.254.169.254"),
-			},
-		},
-		{
-			name: "include link-local v6",
-			addrs: []netlink.Addr{
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("10.0.0.1")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("fe80::")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-				{
-					IPNet: &net.IPNet{IP: net.ParseIP("fe80::1234")},
-					Scope: int(netlink.SCOPE_HOST),
-				},
-			},
-			ipsToExclude: []net.IP{},
-			addrScopeMax: int(netlink.SCOPE_HOST),
-			want: []net.IP{
-				net.ParseIP("10.0.0.1"),
-				net.ParseIP("fe80::"),
-				net.ParseIP("fe80::1234"),
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := filterLocalAddresses(tt.addrs, tt.ipsToExclude, tt.addrScopeMax)
-
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("filterLocalAddresses(): got = %v, want = %v", got, tt.want)
-			}
-		})
-	}
-}

commit 86147328599d8570a26966ae402d3e5ab21ec6bb

datapath: Fix --bpf-lb-dev-ip-addr-inherit option
The handling of the --bpf-lb-dev-ip-addr-inherit option was broken by the
PR that implemented NodeAddressing in terms of Table[NodeAddress].

Add the handling back in by implementing the inheriting in the controller
that populates Table[NodeAddress].

Additionally add in the "Selected" check to the handling of "--nodeport-addresses"
as we don't want to pick the NodePort addresses from any possible device.

Fixes: 9d7e452c20 ("datapath: Reimplement NodeAddressing around Table[NodeAddress]")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/daemon_main.go
@@ -230,9 +230,6 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.Bool(option.EnableRuntimeDeviceDetection, false, "Enable runtime device detection and datapath reconfiguration (experimental)")
 	option.BindEnv(vp, option.EnableRuntimeDeviceDetection)
 
-	flags.String(option.LBDevInheritIPAddr, "", fmt.Sprintf("Device name which IP addr is inherited by devices running LB BPF program (--%s)", option.Devices))
-	option.BindEnv(vp, option.LBDevInheritIPAddr)
-
 	flags.String(option.DatapathMode, defaults.DatapathMode, "Datapath mode name")
 	option.BindEnv(vp, option.DatapathMode)
 

pkg/datapath/tables/node_address.go
@@ -60,10 +60,6 @@ func (n *NodeAddress) String() string {
 	return fmt.Sprintf("%s (%s)", n.Addr, n.DeviceName)
 }
 
-type NodeAddressConfig struct {
-	NodePortAddresses []*cidr.CIDR `mapstructure:"nodeport-addresses"`
-}
-
 var (
 	// NodeAddressIndex is the primary index for node addresses:
 	//
@@ -136,6 +132,14 @@ func newAddressScopeMax(cfg NodeAddressConfig, daemonCfg *option.DaemonConfig) (
 	return AddressScopeMax(daemonCfg.AddressScopeMax), nil
 }
 
+type NodeAddressConfig struct {
+	NodePortAddresses []*cidr.CIDR `mapstructure:"nodeport-addresses"`
+
+	// LBDevInheritIPAddr is device name which IP addr is inherited by devices
+	// running BPF loadbalancer program
+	LBDevInheritIPAddr string `mapstructure:"bpf-lb-dev-ip-addr-inherit"`
+}
+
 func (cfg NodeAddressConfig) getNets() []*net.IPNet {
 	nets := make([]*net.IPNet, len(cfg.NodePortAddresses))
 	for i, cidr := range cfg.NodePortAddresses {
@@ -149,6 +153,12 @@ func (NodeAddressConfig) Flags(flags *pflag.FlagSet) {
 		"nodeport-addresses",
 		nil,
 		"A whitelist of CIDRs to limit which IPs are used for NodePort. If not set, primary IPv4 and/or IPv6 address of each native device is used.")
+
+	flags.String(
+		"bpf-lb-dev-ip-addr-inherit",
+		"",
+		fmt.Sprintf("Device name which IP addr is inherited by devices running LB BPF program (--%s)", option.Devices),
+	)
 }
 
 type nodeAddressControllerParams struct {
@@ -349,15 +359,24 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device) (addrs sets.
 				nodePort = true
 				primary = true
 			}
-		} else if ip.NetsContainsAny(n.Config.getNets(), []*net.IPNet{ip.IPToPrefix(addr.AsIP())}) {
-			// User specified --nodeport-addresses and this address was within the range.
-			nodePort = true
-			if addr.Addr.Is4() && !ipv4Found {
-				primary = true
-				ipv4Found = true
-			} else if addr.Addr.Is6() && !ipv6Found {
-				primary = true
-				ipv6Found = true
+		} else {
+			// True if user specified --nodeport-addresses and this address was within the range.
+			inRange := dev.Selected && ip.NetsContainsAny(n.Config.getNets(), []*net.IPNet{ip.IPToPrefix(addr.AsIP())})
+
+			// True if user specified --bpf-lb-dev-ip-addr-inherit and the device name matches.
+			// This is used in the edge-case where a bridge device is used and we want the IP address
+			// from the bridge device, but want to load BPF programs to the bridge slaves.
+			inheritFromDev := n.Config.LBDevInheritIPAddr != "" && n.Config.LBDevInheritIPAddr == dev.Name
+
+			if inRange || inheritFromDev {
+				nodePort = true
+				if addr.Addr.Is4() && !ipv4Found {
+					primary = true
+					ipv4Found = true
+				} else if addr.Addr.Is6() && !ipv6Found {
+					primary = true
+					ipv6Found = true
+				}
 			}
 		}
 

pkg/option/config.go
@@ -126,10 +126,6 @@ const (
 	// direct routing mode (only required by BPF NodePort)
 	DirectRoutingDevice = "direct-routing-device"
 
-	// LBDevInheritIPAddr is device name which IP addr is inherited by devices
-	// running BPF loadbalancer program
-	LBDevInheritIPAddr = "bpf-lb-dev-ip-addr-inherit"
-
 	// DisableEnvoyVersionCheck do not perform Envoy binary version check on startup
 	DisableEnvoyVersionCheck = "disable-envoy-version-check"
 
@@ -1421,7 +1417,6 @@ type DaemonConfig struct {
 	devicesMu           lock.RWMutex // Protects devices
 	devices             []string     // bpf_host device
 	DirectRoutingDevice string       // Direct routing device (used by BPF NodePort and BPF Host Routing)
-	LBDevInheritIPAddr  string       // Device which IP addr used by bpf_host devices
 	EnableXDPPrefilter  bool         // Enable XDP-based prefiltering
 	XDPMode             string       // XDP mode, values: { xdpdrv | xdpgeneric | none }
 	HostV4Addr          net.IP       // Host v4 address of the snooping device
@@ -3020,7 +3015,6 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 	c.Debug = vp.GetBool(DebugArg)
 	c.DebugVerbose = vp.GetStringSlice(DebugVerbose)
 	c.DirectRoutingDevice = vp.GetString(DirectRoutingDevice)
-	c.LBDevInheritIPAddr = vp.GetString(LBDevInheritIPAddr)
 	c.EnableIPv4 = vp.GetBool(EnableIPv4Name)
 	c.EnableIPv6 = vp.GetBool(EnableIPv6Name)
 	c.EnableIPv6NDP = vp.GetBool(EnableIPv6NDPName)

pkg/datapath/tables/node_address_test.go
@@ -272,14 +272,7 @@ func TestNodeAddress(t *testing.T) {
 			<-watch // wait for propagation
 
 			iter, _ := nodeAddrs.All(db.ReadTxn())
-			local := []string{}
-			nodePort := []string{}
-			for addr, _, ok := iter.Next(); ok; addr, _, ok = iter.Next() {
-				local = append(local, addr.Addr.String())
-				if addr.NodePort {
-					nodePort = append(nodePort, addr.Addr.String())
-				}
-			}
+			local, nodePort := collectAddrStrings(iter)
 			assert.ElementsMatch(t, local, ipStrings(tt.wantLocal), "LocalAddresses do not match")
 			assert.ElementsMatch(t, nodePort, ipStrings(tt.wantNodePort), "LoadBalancerNodeAddresses do not match")
 			assert.NoError(t, h.Stop(context.TODO()), "Stop")
@@ -446,20 +439,90 @@ func TestNodeAddressWhitelist(t *testing.T) {
 			<-watch // wait for propagation
 
 			iter, _ := nodeAddrs.All(db.ReadTxn())
-			local := []string{}
-			nodePort := []string{}
-			for addr, _, ok := iter.Next(); ok; addr, _, ok = iter.Next() {
-				local = append(local, addr.Addr.String())
-				if addr.NodePort {
-					nodePort = append(nodePort, addr.Addr.String())
-				}
-			}
+			local, nodePort := collectAddrStrings(iter)
 			assert.ElementsMatch(t, local, ipStrings(tt.wantLocal), "LocalAddresses do not match")
 			assert.ElementsMatch(t, nodePort, ipStrings(tt.wantNodePort), "LoadBalancerNodeAddresses do not match")
 			assert.NoError(t, h.Stop(context.TODO()), "Stop")
 		})
 	}
+}
 
+func TestNodeAddressInheritDev(t *testing.T) {
+	t.Parallel()
+
+	var (
+		db        *statedb.DB
+		devices   statedb.RWTable[*tables.Device]
+		nodeAddrs statedb.Table[tables.NodeAddress]
+	)
+	h := hive.New(
+		job.Cell,
+		statedb.Cell,
+		tables.NodeAddressCell,
+		cell.Provide(
+			tables.NewDeviceTable,
+			statedb.RWTable[*tables.Device].ToTable,
+		),
+		cell.Invoke(func(db_ *statedb.DB, d statedb.RWTable[*tables.Device], na statedb.Table[tables.NodeAddress]) {
+			db = db_
+			devices = d
+			nodeAddrs = na
+			db.RegisterTable(d)
+		}),
+
+		// option.DaemonConfig needed for AddressMaxScope. This flag will move into NodeAddressConfig
+		// in a follow-up PR.
+		cell.Provide(func() *option.DaemonConfig {
+			return &option.DaemonConfig{
+				AddressScopeMax: defaults.AddressScopeMax,
+			}
+		}),
+	)
+
+	// Inherit the address of br0
+	h.Viper().Set("bpf-lb-dev-ip-addr-inherit", "br0")
+
+	if !assert.NoError(t, h.Start(context.TODO()), "Start") {
+		return
+	}
+
+	txn := db.WriteTxn(devices)
+	_, watch := nodeAddrs.All(txn)
+
+	addrs := []tables.DeviceAddress{
+		{
+			Addr:  netip.MustParseAddr("10.0.0.1"),
+			Scope: unix.RT_SCOPE_SITE,
+		},
+	}
+
+	devices.Insert(txn,
+		&tables.Device{
+			Index:    2,
+			Name:     "br0",
+			Selected: false,
+			Flags:    net.FlagUp,
+			Addrs:    addrs,
+		})
+
+	devices.Insert(txn,
+		&tables.Device{
+			Index:    3,
+			Name:     "eth0",
+			Selected: true,
+			Flags:    net.FlagUp,
+			Addrs:    nil,
+		})
+
+	txn.Commit()
+
+	<-watch // wait for propagation
+
+	iter, _ := nodeAddrs.All(db.ReadTxn())
+	_, nodePort := collectAddrStrings(iter)
+
+	assert.ElementsMatch(t, nodePort, []string{"10.0.0.1"}, "LoadBalancerNodeAddresses do not match")
+	assert.NoError(t, h.Stop(context.TODO()), "Stop")
 }
 
 // ipStrings converts net.IP to a string. Used to assert equalence without having to deal
@@ -469,6 +532,16 @@ func ipStrings(ips []net.IP) (ss []string) {
 		ss = append(ss, ips[i].String())
 	}
 	sort.Strings(ss)
+	return
+}
+
+func collectAddrStrings(iter statedb.Iterator[tables.NodeAddress]) (local, nodePort []string) {
+	for addr, _, ok := iter.Next(); ok; addr, _, ok = iter.Next() {
+		local = append(local, addr.Addr.String())
+		if addr.NodePort {
+			nodePort = append(nodePort, addr.Addr.String())
+		}
+	}
 	return
 }
 