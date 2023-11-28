cilium/cilium

commit b836cb1209a8ac29eb7845a17826d2fe285e5857

helm: Add missing type to poststart iptables regex
We recently introduced AWS-CONNMARK-CHAIN iptables rules deletion, but
didn't add them to an if statement guarding actual deletion.

Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

install/kubernetes/cilium/files/agent/poststart-eni.bash
@@ -11,7 +11,7 @@ set -o nounset
 # dependencies on anything that is part of the startup script
 # itself, and can be safely run multiple times per node (e.g. in
 # case of a restart).
-if [[ "$(iptables-save | grep -c AWS-SNAT-CHAIN)" != "0" ]];
+if [[ "$(iptables-save | grep -c 'AWS-SNAT-CHAIN|AWS-CONNMARK-CHAIN')" != "0" ]];
 then
     echo 'Deleting iptables rules created by the AWS CNI VPC plugin'
     iptables-save | grep -v 'AWS-SNAT-CHAIN|AWS-CONNMARK-CHAIN' | iptables-restore

commit 1ac8c68cad62d451408922b032a1072b8018aa2c

install: fix stale aws iptables chains deletion
The poststart-eni.sh script got recently modified to delete both AWS-SNAT
and AWS-CONNMARK related chains. Yet, the filter is now a regular expression,
and does not return any match with plain grep. Let's fix this by setting the
`-E` (--extended-regexp) flag.

Fixes: b836cb1209a8 ("helm: Add missing type to poststart iptables regex")
Fixes: 8c86d079949b ("install: Remove AWS-CONNMARK-CHAIN iptables")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

install/kubernetes/cilium/files/agent/poststart-eni.bash
@@ -11,9 +11,9 @@ set -o nounset
 # dependencies on anything that is part of the startup script
 # itself, and can be safely run multiple times per node (e.g. in
 # case of a restart).
-if [[ "$(iptables-save | grep -c 'AWS-SNAT-CHAIN|AWS-CONNMARK-CHAIN')" != "0" ]];
+if [[ "$(iptables-save | grep -E -c 'AWS-SNAT-CHAIN|AWS-CONNMARK-CHAIN')" != "0" ]];
 then
     echo 'Deleting iptables rules created by the AWS CNI VPC plugin'
-    iptables-save | grep -v 'AWS-SNAT-CHAIN|AWS-CONNMARK-CHAIN' | iptables-restore
+    iptables-save | grep -E -v 'AWS-SNAT-CHAIN|AWS-CONNMARK-CHAIN' | iptables-restore
 fi
 echo 'Done!'