573c7a647a2e8b31735ec20ed843a71512148dac
Marco Iorio <marco.iorio@isovalent.com>
2023-11-28 16:41:06 +0100

    contrib: lint usages of sync.Map
    Extend the lock-check.sh linting script to additionally flag usages of
    the sync.Map type, and suggest the usage of the generic wrapper.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- contrib/scripts/lock-check.sh
+++ contrib/scripts/lock-check.sh
@@ -9,3 +9,8 @@ for l in sync.Mutex sync.RWMutex; do
     exit 1
   fi
 done
+
+if grep -r --exclude-dir={.git,_build,vendor,externalversions,lock,contrib} -i --include \*.go "sync.Map" .; then
+  echo "Found sync.Map usages. Please use the generic pkg/lock.Map wrapper instead";
+  exit 1
+fi
