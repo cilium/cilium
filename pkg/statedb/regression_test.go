0ee2e595e18d7cb4216080bfc32b0e13040b039d
Jussi Maki <jussi@isovalent.com>
2023-11-28 13:48:54 +0200

    statedb: Add multi-index checks to 29324 regression test
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/statedb/regression_test.go
+++ pkg/statedb/regression_test.go
@@ -17,7 +17,8 @@ import (
 // https://github.com/cilium/cilium/issues/29324
 func Test_Regression_29324(t *testing.T) {
 	type object struct {
-		ID string
+		ID  string
+		Tag string
 	}
 	idIndex := Index[object, string]{
 		Name: "id",
@@ -27,16 +28,24 @@ func Test_Regression_29324(t *testing.T) {
 		FromKey: index.String,
 		Unique:  true,
 	}
+	tagIndex := Index[object, string]{
+		Name: "tag",
+		FromObject: func(t object) index.KeySet {
+			return index.NewKeySet(index.String(t.Tag))
+		},
+		FromKey: index.String,
+		Unique:  false,
+	}
 
-	db, _, _ := newTestDB(t, tagsIndex)
-	table, err := NewTable[object]("objects", idIndex)
+	db, _, _ := newTestDB(t)
+	table, err := NewTable[object]("objects", idIndex, tagIndex)
 	require.NoError(t, err)
 	require.NoError(t, db.RegisterTable(table))
 
 	wtxn := db.WriteTxn(table)
-	table.Insert(wtxn, object{"foo"})
-	table.Insert(wtxn, object{"foobar"})
-	table.Insert(wtxn, object{"baz"})
+	table.Insert(wtxn, object{"foo", "aa"})
+	table.Insert(wtxn, object{"foobar", "aaa"})
+	table.Insert(wtxn, object{"baz", "aaaa"})
 	wtxn.Commit()
 
 	// Exact match should only return "foo"
@@ -51,4 +60,17 @@ func Test_Regression_29324(t *testing.T) {
 	iter, _ = table.Get(txn, idIndex.Query("foob"))
 	items = Collect(iter)
 	assert.Len(t, items, 0, "Get(\"foob\") should return nothing")
+
+	// Query on non-unique index should only return exact match
+	iter, _ = table.Get(txn, tagIndex.Query("aa"))
+	items = Collect(iter)
+	if assert.Len(t, items, 1, "Get(\"aa\") on tags should return one match") {
+		assert.EqualValues(t, "foo", items[0].ID)
+	}
+
+	// Partial match on prefix should not return anything on non-unique index
+	iter, _ = table.Get(txn, idIndex.Query("a"))
+	items = Collect(iter)
+	assert.Len(t, items, 0, "Get(\"a\") should return nothing")
+
 }
