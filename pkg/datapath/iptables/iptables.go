cilium/cilium

commit a6a2b73f4fee2112af53514bb2a1acf66161e963

iptables: Add a cell for iptables config manager
Modularize iptables configuration manager into its own cell.

The previous Init method is replaced with an Invoke function in order to
be executed when starting the iptables manager cell.

Also, enableIPForwarding is moved in the iptables package and kept
unexported, since it is not used outside of the package.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/datapath/iptables/iptables.go
@@ -28,6 +28,8 @@ import (
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/fqdn/proxy/ipfamily"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
 	"github.com/cilium/cilium/pkg/ip"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	lb "github.com/cilium/cilium/pkg/loadbalancer"
@@ -254,6 +256,11 @@ func (m *Manager) removeCiliumRules(table string, prog iptablesInterface, match 
 
 // Manager manages the iptables-related configuration for Cilium.
 type Manager struct {
+	logger     logrus.FieldLogger
+	modulesMgr *modules.Manager
+	cfg        Config
+	sharedCfg  SharedConfig
+
 	// This lock ensures there are no concurrent executions of the InstallRules() and
 	// InstallProxyRules() methods, as otherwise we may end up with errors (as rules may have
 	// been already removed or installed by a different execution of the method) or with an
@@ -267,46 +274,74 @@ type Manager struct {
 	CNIChainingMode      string
 }
 
-// Init initializes the iptables manager and checks for iptables kernel modules
-// availability.
-func (m *Manager) Init(modulesManager *modules.Manager) {
-	haveIp6tables := true
-	if err := modulesManager.FindOrLoadModules(
-		"ip_tables", "iptable_nat", "iptable_mangle", "iptable_raw",
-		"iptable_filter"); err != nil {
-		log.WithError(err).Warning(
+type params struct {
+	cell.In
+
+	Logger    logrus.FieldLogger
+	Lifecycle hive.Lifecycle
+
+	ModulesMgr *modules.Manager
+
+	Cfg       Config
+	SharedCfg SharedConfig
+}
+
+func newIptablesManager(p params) *Manager {
+	iptMgr := &Manager{
+		logger:        p.Logger,
+		modulesMgr:    p.ModulesMgr,
+		sharedCfg:     p.SharedCfg,
+		haveIp6tables: true,
+	}
+
+	p.Lifecycle.Append(iptMgr)
+
+	return iptMgr
+}
+
+// Start initializes the iptables manager and checks for iptables kernel modules availability.
+func (m *Manager) Start(ctx hive.HookContext) error {
+	if err := enableIPForwarding(); err != nil {
+		m.logger.WithError(err).Warning("enabling IP forwarding via sysctl failed")
+	}
+
+	if err := m.modulesMgr.FindOrLoadModules(
+		"ip_tables", "iptable_nat", "iptable_mangle", "iptable_raw", "iptable_filter",
+	); err != nil {
+		m.logger.WithError(err).Warning(
 			"iptables modules could not be initialized. It probably means that iptables is not available on this system")
 	}
-	if err := modulesManager.FindOrLoadModules(
-		"ip6_tables", "ip6table_mangle", "ip6table_raw", "ip6table_filter"); err != nil {
-		if option.Config.EnableIPv6 {
-			log.WithError(err).Fatal(
-				"IPv6 is enabled and ip6tables modules could not be initialized (try disabling IPv6 in Cilium or loading ip6_tables, ip6table_mangle, ip6table_raw and ip6table_filter kernel modules)")
+
+	if err := m.modulesMgr.FindOrLoadModules(
+		"ip6_tables", "ip6table_mangle", "ip6table_raw", "ip6table_filter",
+	); err != nil {
+		if m.sharedCfg.EnableIPv6 {
+			return fmt.Errorf(
+				"IPv6 is enabled and ip6tables modules initialization failed: %w "+
+					"(try disabling IPv6 in Cilium or loading ip6_tables, ip6table_mangle, ip6table_raw and ip6table_filter kernel modules)", err)
 		}
-		log.WithError(err).Debug(
+		m.logger.WithError(err).Debug(
 			"ip6tables kernel modules could not be loaded, so IPv6 cannot be used")
-		haveIp6tables = false
-	}
-
-	ipv6Disabled, err := os.ReadFile("/sys/module/ipv6/parameters/disable")
-	if err != nil {
-		if option.Config.EnableIPv6 {
-			log.WithError(err).Fatal(
-				"IPv6 is enabled but IPv6 kernel support could not be probed")
+		m.haveIp6tables = false
+	} else {
+		ipv6Disabled, err := os.ReadFile("/sys/module/ipv6/parameters/disable")
+		if err != nil {
+			if m.sharedCfg.EnableIPv6 {
+				return fmt.Errorf(
+					"IPv6 is enabled but IPv6 kernel support probing failed with: %w", err)
+			}
+			m.logger.WithError(err).Warning(
+				"Unable to read /sys/module/ipv6/parameters/disable, disabling IPv6 iptables support")
+			m.haveIp6tables = false
+		} else if strings.TrimSuffix(string(ipv6Disabled), "\n") == "1" {
+			m.logger.Debug(
+				"Kernel does not support IPv6, disabling IPv6 iptables support")
+			m.haveIp6tables = false
 		}
-		log.WithError(err).Warning(
-			"Unable to read /sys/module/ipv6/parameters/disable, disabling IPv6 iptables support")
-		haveIp6tables = false
-	} else if strings.TrimSuffix(string(ipv6Disabled), "\n") == "1" {
-		log.Debug(
-			"Kernel does not support IPv6, disabling IPv6 iptables support")
-		haveIp6tables = false
 	}
 
-	m.haveIp6tables = haveIp6tables
-
-	if err := modulesManager.FindOrLoadModules("xt_socket"); err != nil {
-		if !option.Config.TunnelingEnabled() {
+	if err := m.modulesMgr.FindOrLoadModules("xt_socket"); err != nil {
+		if !m.isTunnelingEnabled() {
 			// xt_socket module is needed to circumvent an explicit drop in ip_forward()
 			// logic for packets for which a local socket is found by ip early
 			// demux. xt_socket performs a local socket match and sets an skb mark on
@@ -322,32 +357,38 @@ func (m *Manager) Init(modulesManager *modules.Manager) {
 			// We would not need the xt_socket at all if the datapath universally would
 			// set the "to proxy" skb mark bits on before the packet hits policy routing
 			// stage. Currently this is not true for endpoint routing modes.
-			log.WithError(err).Warning("xt_socket kernel module could not be loaded")
+			m.logger.WithError(err).Warning("xt_socket kernel module could not be loaded")
 
-			if option.Config.EnableXTSocketFallback {
+			if m.sharedCfg.EnableXTSocketFallback {
 				v4disabled := true
 				v6disabled := true
-				if option.Config.EnableIPv4 {
+				if m.sharedCfg.EnableIPv4 {
 					v4disabled = sysctl.Disable("net.ipv4.ip_early_demux") == nil
 				}
-				if option.Config.EnableIPv6 {
+				if m.sharedCfg.EnableIPv6 {
 					v6disabled = sysctl.Disable("net.ipv6.ip_early_demux") == nil
 				}
 				if v4disabled && v6disabled {
 					m.ipEarlyDemuxDisabled = true
-					log.Warning("Disabled ip_early_demux to allow proxy redirection with original source/destination address without xt_socket support also in non-tunneled datapath modes.")
+					m.logger.Warning("Disabled ip_early_demux to allow proxy redirection with original source/destination address without xt_socket support also in non-tunneled datapath modes.")
 				} else {
-					log.WithError(err).Warning("Could not disable ip_early_demux, traffic redirected due to an HTTP policy or visibility may be dropped unexpectedly")
+					m.logger.WithError(err).Warning("Could not disable ip_early_demux, traffic redirected due to an HTTP policy or visibility may be dropped unexpectedly")
 				}
 			}
 		}
 	} else {
 		m.haveSocketMatch = true
 	}
-	m.haveBPFSocketAssign = option.Config.EnableBPFTProxy
+	m.haveBPFSocketAssign = m.sharedCfg.EnableBPFTProxy
 
-	ip4tables.initArgs(int(option.Config.IPTablesLockTimeout / time.Second))
-	ip6tables.initArgs(int(option.Config.IPTablesLockTimeout / time.Second))
+	ip4tables.initArgs(int(m.cfg.IPTablesLockTimeout / time.Second))
+	ip6tables.initArgs(int(m.cfg.IPTablesLockTimeout / time.Second))
+
+	return nil
+}
+
+func (m *Manager) Stop(ctx hive.HookContext) error {
+	return nil
 }
 
 // SupportsOriginalSourceAddr tells if an L7 proxy can use POD's original source address and port in
@@ -1415,6 +1456,10 @@ func (m *Manager) installMasqueradeRules(prog iptablesInterface, ifName, localDe
 	}
 
 	return nil
+}
+
+func (m *Manager) isTunnelingEnabled() bool {
+	return m.sharedCfg.RoutingMode != option.RoutingModeNative
 }
 
 func createIpset(name string, ipv6 bool) error {

daemon/cmd/daemon_main.go
@@ -718,9 +718,6 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.MarkHidden(option.InstallIptRules)
 	option.BindEnv(vp, option.InstallIptRules)
 
-	flags.Duration(option.IPTablesLockTimeout, 5*time.Second, "Time to pass to each iptables invocation to wait for xtables lock acquisition")
-	option.BindEnv(vp, option.IPTablesLockTimeout)
-
 	flags.Bool(option.IPTablesRandomFully, false, "Set iptables flag random-fully on masquerading rules")
 	option.BindEnv(vp, option.IPTablesRandomFully)
 

pkg/datapath/cells.go
@@ -57,6 +57,9 @@ var Cell = cell.Module(
 	// The modules manager to search and load kernel modules.
 	modules.Cell,
 
+	// Manages Cilium-specific iptables rules.
+	iptables.Cell,
+
 	cell.Provide(
 		newWireguardAgent,
 		newDatapath,
@@ -131,20 +134,7 @@ func newDatapath(params datapathParams) types.Datapath {
 		ProcFs:     option.Config.ProcFs,
 	}
 
-	iptablesManager := &iptables.Manager{}
-
-	params.LC.Append(hive.Hook{
-		OnStart: func(hive.HookContext) error {
-			// FIXME enableIPForwarding should not live here
-			if err := enableIPForwarding(); err != nil {
-				log.Fatalf("enabling IP forwarding via sysctl failed: %s", err)
-			}
-
-			iptablesManager.Init(params.ModulesManager)
-			return nil
-		}})
-
-	datapath := linuxdatapath.NewDatapath(datapathConfig, iptablesManager, params.WgAgent, params.NodeMap, params.ConfigWriter)
+	datapath := linuxdatapath.NewDatapath(datapathConfig, params.IptablesManager, params.WgAgent, params.NodeMap, params.ConfigWriter)
 
 	params.LC.Append(hive.Hook{
 		OnStart: func(hive.HookContext) error {
@@ -174,6 +164,8 @@ type datapathParams struct {
 	DeviceManager *linuxdatapath.DeviceManager
 
 	ModulesManager *modules.Manager
+
+	IptablesManager *iptables.Manager
 
 	ConfigWriter types.ConfigWriter
 }

pkg/datapath/iptables/cell.go
@@ -1,0 +1,53 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package iptables
+
+import (
+	"github.com/spf13/pflag"
+
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/time"
+)
+
+var Cell = cell.Module(
+	"iptables",
+	"Handle iptables-related configuration for Cilium",
+
+	cell.Config(defaultConfig),
+	cell.ProvidePrivate(func(
+		cfg *option.DaemonConfig,
+	) SharedConfig {
+		return SharedConfig{
+			EnableIPv4:             cfg.EnableIPv4,
+			EnableIPv6:             cfg.EnableIPv6,
+			RoutingMode:            cfg.RoutingMode,
+			EnableXTSocketFallback: cfg.EnableXTSocketFallback,
+			EnableBPFTProxy:        cfg.EnableBPFTProxy,
+		}
+	}),
+	cell.Provide(newIptablesManager),
+)
+
+type Config struct {
+	// IPTablesLockTimeout defines the "-w" iptables option when the
+	// iptables CLI is directly invoked from the Cilium agent.
+	IPTablesLockTimeout time.Duration
+}
+
+var defaultConfig = Config{
+	IPTablesLockTimeout: 5 * time.Second,
+}
+
+func (def Config) Flags(flags *pflag.FlagSet) {
+	flags.Duration("iptables-lock-timeout", def.IPTablesLockTimeout, "Time to pass to each iptables invocation to wait for xtables lock acquisition")
+}
+
+type SharedConfig struct {
+	EnableIPv4             bool
+	EnableIPv6             bool
+	RoutingMode            string
+	EnableXTSocketFallback bool
+	EnableBPFTProxy        bool
+}

pkg/datapath/iptables/sysctl_darwin.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package datapath
+package iptables
 
 // enableIPForwarding on OS X and Darwin is not doing anything. It just exists
 // to make compilation possible.

pkg/datapath/iptables/sysctl_linux.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package datapath
+package iptables
 
 import (
 	"github.com/cilium/cilium/pkg/option"

pkg/datapath/iptables/sysctl_linux_test.go
@@ -3,7 +3,7 @@
 
 //go:build linux
 
-package datapath
+package iptables
 
 import (
 	"github.com/cilium/cilium/pkg/testutils"

pkg/option/config.go
@@ -400,8 +400,6 @@ const (
 	// to skip netfilter connection tracking on all pod traffic.
 	InstallNoConntrackIptRules = "install-no-conntrack-iptables-rules"
 
-	IPTablesLockTimeout = "iptables-lock-timeout"
-
 	// IPTablesRandomFully sets iptables flag random-fully on masquerading rules
 	IPTablesRandomFully = "iptables-random-fully"
 
@@ -1680,10 +1678,6 @@ type DaemonConfig struct {
 	// PrependIptablesChains is the name of the option to enable prepending
 	// iptables chains instead of appending
 	PrependIptablesChains bool
-
-	// IPTablesLockTimeout defines the "-w" iptables option when the
-	// iptables CLI is directly invoked from the Cilium agent.
-	IPTablesLockTimeout time.Duration
 
 	// IPTablesRandomFully defines the "--random-fully" iptables option when the
 	// iptables CLI is directly invoked from the Cilium agent.
@@ -3213,7 +3207,6 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 	c.EnvoyConfigTimeout = vp.GetDuration(EnvoyConfigTimeout)
 	c.IPMasqAgentConfigPath = vp.GetString(IPMasqAgentConfigPath)
 	c.InstallIptRules = vp.GetBool(InstallIptRules)
-	c.IPTablesLockTimeout = vp.GetDuration(IPTablesLockTimeout)
 	c.IPTablesRandomFully = vp.GetBool(IPTablesRandomFully)
 	c.IPSecKeyFile = vp.GetString(IPSecKeyFileName)
 	c.IPsecKeyRotationDuration = vp.GetDuration(IPsecKeyRotationDuration)

commit a53d1d46ab1a96165305e2c1a32bca118aaa6a3c

iptables: Copy config at cell startup
Copy the cell specific config parameters passed through dependency
injection into the manager struct.

Fixes: a6a2b73f4f ("iptables: Add a cell for iptables config manager")

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/datapath/iptables/iptables.go
@@ -289,6 +289,7 @@ func newIptablesManager(p params) *Manager {
 	iptMgr := &Manager{
 		logger:        p.Logger,
 		modulesMgr:    p.ModulesMgr,
+		cfg:           p.Cfg,
 		sharedCfg:     p.SharedCfg,
 		haveIp6tables: true,
 	}