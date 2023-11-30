735807fb16806abf13c71c936efbf887b8ee00a5
Lorenz Bauer <lmb@isovalent.com>
2023-11-30 13:25:51 +0000

    test/verifier: fix complexity tests not being recompiled
    TestVerifier is accidentally reusing the result of previous subtest
    compilations. This means that only the first set of configurations was tested.
    
    Invoke clean for every new compilation. The generated object files are
    moved to the test directory to make them accessible as artifacts from CI.
    
    Fixes: d3ef5b2ac8 ("test/verifier: Avoid pruning object files before testing the next file")
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- test/verifier/verifier_test.go
+++ test/verifier/verifier_test.go
@@ -116,12 +116,6 @@ func TestVerifier(t *testing.T) {
 	kernelVersion, source := getCIKernelVersion(t)
 	t.Logf("CI kernel version: %s (%s)", kernelVersion, source)
 
-	cmd := exec.Command("make", "-C", "bpf/", "clean")
-	cmd.Dir = *ciliumBasePath
-	if out, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("Failed to clean bpf objects: %v\ncommand output: %s", err, out)
-	}
-
 	for _, bpfProgram := range []struct {
 		name      string
 		macroName string
@@ -163,7 +157,7 @@ func TestVerifier(t *testing.T) {
 					datapathConfig := readDatapathConfig(t, file)
 
 					name := fmt.Sprintf("%s_%s", bpfProgram.name, configName)
-					cmd = exec.Command("make", "-C", "bpf", fmt.Sprintf("%s.o", bpfProgram.name))
+					cmd := exec.Command("make", "-C", "bpf", "clean", fmt.Sprintf("%s.o", bpfProgram.name))
 					cmd.Dir = *ciliumBasePath
 					cmd.Env = append(cmd.Env,
 						fmt.Sprintf("%s=%s", bpfProgram.macroName, datapathConfig),
@@ -173,7 +167,7 @@ func TestVerifier(t *testing.T) {
 						t.Fatalf("Failed to compile bpf objects: %v\ncommand output: %s", err, out)
 					}
 
-					objFile := path.Join(*ciliumBasePath, "bpf", name+".o")
+					objFile := filepath.Join("./", name+".o")
 					// Rename object file to avoid subsequent runs to overwrite it,
 					// so we can keep it for CI's artifact upload.
 					if err = os.Rename(initObjFile, objFile); err != nil {
