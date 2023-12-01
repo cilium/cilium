266f88797e741c83ed9328ad42fb91693e424e4f
derailed <fernand.galiana@gmail.com>
2023-12-01 15:02:15 -0700

    Make insertNeighor return err
    In order to surface module health signals we need insertNeighbor to
    return error vs simple logging to the console.
    
    Signed-off-by: Fernand Galiana <fernand.galiana@gmail.com>

--- pkg/datapath/fake/types/node.go
+++ pkg/datapath/fake/types/node.go
@@ -68,7 +68,8 @@ func (n *FakeNodeHandler) NodeNeighDiscoveryEnabled() bool {
 	return false
 }
 
-func (n *FakeNodeHandler) NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node) {
+func (n *FakeNodeHandler) NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node) error {
+	return nil
 }
 
 func (n *FakeNodeHandler) NodeCleanNeighbors(migrateOnly bool) {
