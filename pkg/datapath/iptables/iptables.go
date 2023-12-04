cilium/cilium

commit 0d10aca58f446488b5df705ddd20d22b9135f1c3

cilium, iptables: Extend to cover default route in enable-masquerade-to-route-source
Extend the enable-masquerade-to-route-source option to cover also SNAT targets
for the default route on a given interface.

The example from commit e731cbe27ef2 ("cilium, iptables: Support option to
masquerade source IP from routing layer") now extends to inserting a default
route (***):

  [...]
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 -m set --match-set cilium_node_set_v4 dst -m comment --comment "exclude traffic to cluster nodes from masquerade" -j ACCEPT
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 -d 1.1.1.1/32 -m comment --comment "cilium snat non-cluster via source route" -j SNAT --to-source 192.168.2.99
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 -d 192.168.2.13/32 -o enp5s0 -m comment --comment "cilium snat non-cluster via source route" -j SNAT --to-source 192.168.2.99
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 -o enp5s0 -m comment --comment "cilium snat non-cluster via source route" -j SNAT --to-source 192.168.2.12   (***)
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 ! -d 10.244.0.0/16 ! -o cilium_+ -m comment --comment "cilium masquerade non-cluster" -j MASQUERADE
  [...]

This needs to be done in a second pass as otherwise iptables' linear processing
would not be able to process the more specific rules.

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/datapath/iptables/iptables.go
@@ -1224,6 +1224,8 @@ func (m *IptablesManager) installMasqueradeRules(prog iptablesInterface, ifName,
 	// If this option is enabled, then it takes precedence over the catch-all
 	// MASQUERADE further below.
 	if option.Config.EnableMasqueradeRouteSource {
+		var defaultRoutes []netlink.Route
+
 		devices := option.Config.GetDevices()
 		if len(option.Config.MasqueradeInterfaces) > 0 {
 			devices = option.Config.MasqueradeInterfaces
@@ -1232,7 +1234,9 @@ func (m *IptablesManager) installMasqueradeRules(prog iptablesInterface, ifName,
 		if prog == ip6tables {
 			family = netlink.FAMILY_V6
 		}
+		initialPass := true
 		if routes, err := netlink.RouteList(nil, family); err == nil {
+		nextPass:
 			for _, r := range routes {
 				var link netlink.Link
 				match := false
@@ -1262,9 +1266,11 @@ func (m *IptablesManager) installMasqueradeRules(prog iptablesInterface, ifName,
 					match = true
 				}
 				_, exclusionCIDR, err := net.ParseCIDR(snatDstExclusionCIDR)
-				if !match || r.Src == nil ||
-					cidr.Equal(r.Dst, cidr.ZeroNet(r.Family)) ||
-					(err == nil && cidr.Equal(r.Dst, exclusionCIDR)) {
+				if !match || r.Src == nil || (err == nil && cidr.Equal(r.Dst, exclusionCIDR)) {
+					continue
+				}
+				if initialPass && cidr.Equal(r.Dst, cidr.ZeroNet(r.Family)) {
+					defaultRoutes = append(defaultRoutes, r)
 					continue
 				}
 				progArgs := []string{
@@ -1289,6 +1295,11 @@ func (m *IptablesManager) installMasqueradeRules(prog iptablesInterface, ifName,
 				if err := prog.runProg(progArgs); err != nil {
 					return err
 				}
+			}
+			if initialPass {
+				initialPass = false
+				routes = defaultRoutes
+				goto nextPass
 			}
 		}
 	}

commit a2ab8893e308fc8abeb591bc8fd65db81af2d925

cilium, iptables: Do not install catch-all masq rule under masq-to-route-source
When `--enable-masquerade-to-route-source` option is used to support more
fine-grained masquerading for advanced use cases, then do not install the
regular catch-all masquerading iptables rule. In a user environment, we've
seen that this messes with the source selection and picks the wrong IP.
Since `--enable-masquerade-to-route-source` also handles default routes,
it replaces the catch-all masquerading one already, hence move this into
an else branch. No other functional changes.

Fixes: 0d10aca58f44 ("cilium, iptables: Extend to cover default route in enable-masquerade-to-route-source")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/datapath/iptables/iptables.go
@@ -1325,47 +1325,47 @@ func (m *Manager) installMasqueradeRules(prog iptablesInterface, ifName, localDe
 				goto nextPass
 			}
 		}
-	}
-
-	// Masquerade all egress traffic leaving the node (catch-all)
-	//
-	// This rule must be first as the node ipset rule as it has different
-	// exclusion criteria than the other rules in this table.
-	//
-	// The following conditions must be met:
-	// * May not leave on a cilium_ interface, this excludes all
-	//   tunnel traffic
-	// * Must originate from an IP in the local allocation range
-	// * Must not be reply if BPF NodePort is enabled
-	// * Tunnel mode:
-	//   * May not be targeted to an IP in the local allocation
-	//     range
-	// * Non-tunnel mode:
-	//   * May not be targeted to an IP in the cluster range
-	progArgs := []string{
-		"-t", "nat",
-		"-A", ciliumPostNatChain,
-		"!", "-d", snatDstExclusionCIDR,
-	}
-	if len(m.sharedCfg.MasqueradeInterfaces) > 0 {
-		progArgs = append(
-			progArgs,
-			"-o", strings.Join(m.sharedCfg.MasqueradeInterfaces, ","))
 	} else {
+		// Masquerade all egress traffic leaving the node (catch-all)
+		//
+		// This rule must be first as the node ipset rule as it has different
+		// exclusion criteria than the other rules in this table.
+		//
+		// The following conditions must be met:
+		// * May not leave on a cilium_ interface, this excludes all
+		//   tunnel traffic
+		// * Must originate from an IP in the local allocation range
+		// * Must not be reply if BPF NodePort is enabled
+		// * Tunnel mode:
+		//   * May not be targeted to an IP in the local allocation
+		//     range
+		// * Non-tunnel mode:
+		//   * May not be targeted to an IP in the cluster range
+		progArgs := []string{
+			"-t", "nat",
+			"-A", ciliumPostNatChain,
+			"!", "-d", snatDstExclusionCIDR,
+		}
+		if len(m.sharedCfg.MasqueradeInterfaces) > 0 {
+			progArgs = append(
+				progArgs,
+				"-o", strings.Join(m.sharedCfg.MasqueradeInterfaces, ","))
+		} else {
+			progArgs = append(
+				progArgs,
+				"-s", allocRange,
+				"!", "-o", "cilium_+")
+		}
 		progArgs = append(
 			progArgs,
-			"-s", allocRange,
-			"!", "-o", "cilium_+")
-	}
-	progArgs = append(
-		progArgs,
-		"-m", "comment", "--comment", "cilium masquerade non-cluster",
-		"-j", "MASQUERADE")
-	if m.cfg.IPTablesRandomFully {
-		progArgs = append(progArgs, "--random-fully")
-	}
-	if err := prog.runProg(progArgs); err != nil {
-		return err
+			"-m", "comment", "--comment", "cilium masquerade non-cluster",
+			"-j", "MASQUERADE")
+		if m.cfg.IPTablesRandomFully {
+			progArgs = append(progArgs, "--random-fully")
+		}
+		if err := prog.runProg(progArgs); err != nil {
+			return err
+		}
 	}
 
 	// The following rule exclude traffic from the remaining rules in this chain.