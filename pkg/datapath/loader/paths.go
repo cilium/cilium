ea666486c6d8f1107119541d23a08e329782de3d
Robin Gögge <r.goegge@isovalent.com>
2023-12-04 16:41:31 +0100

    loader: add per-endpoint bpffs directory helpers
    Signed-off-by: Robin Gögge <r.goegge@isovalent.com>

--- pkg/datapath/loader/paths.go
+++ pkg/datapath/loader/paths.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 
 	"github.com/vishvananda/netlink"
+
+	datapath "github.com/cilium/cilium/pkg/datapath/types"
 )
 
 // bpffsDevicesDir returns the path to the 'devices' directory on bpffs, usually
@@ -26,4 +28,23 @@ func bpffsDevicesDir(base string) string {
 // during tests.
 func bpffsDeviceLinksDir(base string, device netlink.Link) string {
 	return filepath.Join(bpffsDevicesDir(base), device.Attrs().Name, "links")
+}
+
+// bpffsEndpointsDir returns the path to the 'endpoints' directory on bpffs, usually
+// /sys/fs/bpf/cilium/endpoints. It does not ensure the directory exists.
+//
+// base is typically set to /sys/fs/bpf/cilium, but can be a temp directory
+// during tests.
+func bpffsEndpointsDir(base string) string {
+	return filepath.Join(base, "endpoints")
+}
+
+// bpffsEndpointLinksDir returns the bpffs path to the per-endpoint links directory,
+// usually /sys/fs/bpf/cilium/endpoints/<endpoint-id>/links. It does not ensure the
+// directory exists.
+//
+// base is typically set to /sys/fs/bpf/cilium, but can be a temp directory
+// during tests.
+func bpffsEndpointLinksDir(base string, ep datapath.Endpoint) string {
+	return filepath.Join(bpffsEndpointsDir(base), ep.StringID(), "links")
 }
