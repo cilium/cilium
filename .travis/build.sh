7dd9a422e3ae052acc3239a4f360ae65969d6740
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-12-07 16:23:11 +0100

    ci: don't execute postcheck on integration tests (travis & gha)
    Currently, part of the integration test execution is executing
    `make` in `.travis/build.sh`. The default target (`all`) implicitly
    executes `precheck`, `build` & `postcheck`.
    
    With the execution of the postcheck in the documentation workflow,
    it's not longer necessary to execute them in the integrations too.
    
    Therefore, this commit removes its execution.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,7 +6,7 @@ export CFLAGS="-Werror"
 
 # Travis kills builds that don't generate any output for 10 minutes.
 # Set V=0 here to get GO/CHECK/CC lines, --quiet to hide long clang invocations.
-V=0 make -j 2 --quiet
+V=0 make precheck build -j 2 --quiet
 
 # Run with default verbosity here since this builds all Go code by running
 # 'go vet' and all integration tests. At least one line of output is generated
