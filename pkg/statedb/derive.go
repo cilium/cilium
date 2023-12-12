a24b8b01bd55da8522c9e3369238d2b436ca4dc3
Jussi Maki <jussi@isovalent.com>
2023-12-12 11:31:25 +0200

    statedb: Add Derive utility
    Derive transforms objects from an input table to an output table.
    Useful in conjunction with a reconciler where the desired state
    is derived from a single input table.
    
    Example:
    
      // Assuming we have Table[*Foo] and RWTable[*Bar] and that
      // *Bar is an object we want reconciled.
      cell.Invoke(
        statedb.Derive[*Foo, *Bar](
          func(foo *Foo, deleted bool) (*Bar, statedb.DeriveResult) {
            if deleted {
                return &Bar{
                  ID: foo.ID, // Only need enough for primary key
                  Status: reconciler.StatusPendingDelete(),
                }, statedb.DeriveUpdate
            }
            return &Bar{
              ID: foo.ID,
              Quux: foo.Quux,
              Status: reconciler.StatusPending(),
            }, statedb.DeriveInsert
          },
        ),
      )
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

---
+++ pkg/statedb/derive.go
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package statedb
+
+import (
+	"context"
+
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/hive/job"
+)
+
+type DeriveResult int
+
+const (
+	DeriveInsert DeriveResult = 0 // Insert the object
+	DeriveUpdate DeriveResult = 1 // Update the object (if it exists)
+	DeriveDelete DeriveResult = 2 // Delete the object
+	DeriveSkip   DeriveResult = 3 // Skip
+)
+
+type DeriveParams[In, Out any] struct {
+	cell.In
+
+	Lifecycle hive.Lifecycle
+	Jobs      job.Registry
+	Scope     cell.Scope
+	DB        *DB
+	InTable   Table[In]
+	OutTable  RWTable[Out]
+}
+
+// Derive constructs and registers a job to transform objects from the input table to the
+// output table, e.g. derive the output table from the input table. Useful when constructing
+// a reconciler that has its desired state solely derived from a single table. For example
+// the bandwidth manager's desired state is directly derived from the devices table.
+//
+// Derive is parametrized with the transform function that transforms the input object
+// into the output object. If the transform function returns false, then the object
+// is skipped.
+//
+// Example use:
+//
+//	cell.Invoke(
+//	  statedb.Derive[*tables.Device, *Foo](
+//	    func(d *Device, deleted bool) (*Foo, DeriveResult) {
+//	      if deleted {
+//	        return &Foo{Index: d.Index}, DeriveDelete
+//	      }
+//	      return &Foo{Index: d.Index}, DeriveInsert
+//	    }),
+//	)
+func Derive[In, Out any](jobName string, transform func(obj In, deleted bool) (Out, DeriveResult)) func(DeriveParams[In, Out]) {
+	return func(p DeriveParams[In, Out]) {
+		g := p.Jobs.NewGroup(p.Scope)
+		loop := func(ctx context.Context, health cell.HealthReporter) error {
+			out := p.OutTable
+			wtxn := p.DB.WriteTxn(p.InTable)
+			tracker, err := p.InTable.DeleteTracker(wtxn, jobName)
+			if err != nil {
+				wtxn.Abort()
+				return err
+			}
+			wtxn.Commit()
+			defer tracker.Close()
+			revision := Revision(0)
+			for {
+				wtxn := p.DB.WriteTxn(out)
+
+				var watch <-chan struct{}
+				revision, watch, err = tracker.Process(
+					wtxn,
+					revision,
+					func(obj In, deleted bool, rev Revision) (err error) {
+						outObj, result := transform(obj, deleted)
+						switch result {
+						case DeriveInsert:
+							_, _, err = out.Insert(wtxn, outObj)
+						case DeriveUpdate:
+							_, _, found := out.First(wtxn, out.PrimaryIndexer().QueryFromObject(outObj))
+							if found {
+								_, _, err = out.Insert(wtxn, outObj)
+							}
+						case DeriveDelete:
+							_, _, err = out.Delete(wtxn, outObj)
+						case DeriveSkip:
+						}
+						return err
+					},
+				)
+				wtxn.Commit()
+
+				if err != nil {
+					return err
+				}
+
+				select {
+				case <-watch:
+				case <-ctx.Done():
+					return nil
+				}
+			}
+		}
+		g.Add(job.OneShot(jobName, loop))
+		p.Lifecycle.Append(g)
+	}
+
+}
