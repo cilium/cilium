cilium/cilium

commit f8befdf480037266516f107f6a6659cb88ea8dd7

proxy: allow to provide fixed port for DNS proxy via cell
By default, the value will still be 0 and thus a random port will be
allocated. While at it, also provide the port range as configureable
parameters.

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/proxy/cell.go
@@ -20,11 +20,26 @@ var Cell = cell.Module(
 	"l7-proxy",
 	"L7 Proxy provides support for L7 network policies",
 
+	cell.Provide(func() ProxyConfig { return DefaultProxyConfig }),
+
 	cell.Provide(newProxy),
 	cell.Provide(newEnvoyProxyIntegration),
 	cell.Provide(newDNSProxyIntegration),
 	cell.ProvidePrivate(endpoint.NewEndpointInfoRegistry),
 )
+
+type ProxyConfig struct {
+	minPort, maxPort uint16
+	dnsProxyPort     uint16
+}
+
+var DefaultProxyConfig = ProxyConfig{
+	minPort: 10000,
+	maxPort: 20000,
+	// The default value for the DNS proxy port is set to 0 to allocate a random
+	// port.
+	dnsProxyPort: 0,
+}
 
 type proxyParams struct {
 	cell.In
@@ -37,7 +52,7 @@ type proxyParams struct {
 	XdsServer             envoy.XDSServer
 }
 
-func newProxy(params proxyParams) *Proxy {
+func newProxy(params proxyParams, cfg ProxyConfig) *Proxy {
 	if !option.Config.EnableL7Proxy {
 		log.Info("L7 proxies are disabled")
 		if option.Config.EnableEnvoyConfig {
@@ -48,8 +63,7 @@ func newProxy(params proxyParams) *Proxy {
 
 	configureProxyLogger(params.EndpointInfoRegistry, params.MonitorAgent, option.Config.AgentLabels)
 
-	// FIXME: Make the port range configurable.
-	return createProxy(10000, 20000, params.Datapath, params.EnvoyProxyIntegration, params.DNSProxyIntegration, params.XdsServer)
+	return createProxy(cfg.minPort, cfg.maxPort, cfg.dnsProxyPort, params.Datapath, params.EnvoyProxyIntegration, params.DNSProxyIntegration, params.XdsServer)
 }
 
 type envoyProxyIntegrationParams struct {

pkg/proxy/proxy.go
@@ -119,7 +119,15 @@ type Proxy struct {
 	dnsIntegration   *dnsProxyIntegration
 }
 
-func createProxy(minPort uint16, maxPort uint16, datapathUpdater DatapathUpdater, envoyIntegration *envoyProxyIntegration, dnsIntegration *dnsProxyIntegration, xdsServer envoy.XDSServer) *Proxy {
+func createProxy(
+	minPort uint16,
+	maxPort uint16,
+	dnsProxyPort uint16,
+	datapathUpdater DatapathUpdater,
+	envoyIntegration *envoyProxyIntegration,
+	dnsIntegration *dnsProxyIntegration,
+	xdsServer envoy.XDSServer,
+) *Proxy {
 	return &Proxy{
 		XDSServer:        xdsServer,
 		rangeMin:         minPort,
@@ -127,13 +135,13 @@ func createProxy(minPort uint16, maxPort uint16, datapathUpdater DatapathUpdater
 		redirects:        make(map[string]*Redirect),
 		datapathUpdater:  datapathUpdater,
 		allocatedPorts:   make(map[uint16]bool),
-		proxyPorts:       defaultProxyPortMap(),
+		proxyPorts:       defaultProxyPortMap(dnsProxyPort),
 		envoyIntegration: envoyIntegration,
 		dnsIntegration:   dnsIntegration,
 	}
 }
 
-func defaultProxyPortMap() map[string]*ProxyPort {
+func defaultProxyPortMap(dnsProxyPort uint16) map[string]*ProxyPort {
 	return map[string]*ProxyPort{
 		"cilium-http-egress": {
 			proxyType: types.ProxyTypeHTTP,
@@ -149,6 +157,7 @@ func defaultProxyPortMap() map[string]*ProxyPort {
 			proxyType: types.ProxyTypeDNS,
 			ingress:   false,
 			localOnly: true,
+			proxyPort: dnsProxyPort,
 		},
 		"cilium-proxylib-egress": {
 			proxyType: types.ProxyTypeAny,

pkg/proxy/proxy_test.go
@@ -42,7 +42,7 @@ func (s *ProxySuite) TestPortAllocator(c *C) {
 	err := os.MkdirAll(socketDir, 0700)
 	c.Assert(err, IsNil)
 
-	p := createProxy(10000, 20000, mockDatapathUpdater, nil, nil, nil)
+	p := createProxy(10000, 20000, 0, mockDatapathUpdater, nil, nil, nil)
 
 	port, err := p.AllocateProxyPort("listener1", false, true)
 	c.Assert(err, IsNil)
@@ -210,7 +210,7 @@ func (s *ProxySuite) TestCreateOrUpdateRedirectMissingListener(c *C) {
 	err := os.MkdirAll(socketDir, 0700)
 	c.Assert(err, IsNil)
 
-	p := createProxy(10000, 20000, mockDatapathUpdater, nil, nil, nil)
+	p := createProxy(10000, 20000, 0, mockDatapathUpdater, nil, nil, nil)
 
 	ep := &endpointtest.ProxyUpdaterMock{
 		Id:       1000,

commit 6fdf3227ac9539edb93d79c163cdb13804bc8e16

proxy: export ProxyConfig fields
Otherwise there is no way to set them from outside the proxy package. No
functional change as currently always the DefaultProxyConfig is applied.

Fixes: f8befdf48003 ("proxy: allow to provide fixed port for DNS proxy via cell")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/proxy/cell.go
@@ -29,16 +29,16 @@ var Cell = cell.Module(
 )
 
 type ProxyConfig struct {
-	minPort, maxPort uint16
-	dnsProxyPort     uint16
+	MinPort, MaxPort uint16
+	DNSProxyPort     uint16
 }
 
 var DefaultProxyConfig = ProxyConfig{
-	minPort: 10000,
-	maxPort: 20000,
+	MinPort: 10000,
+	MaxPort: 20000,
 	// The default value for the DNS proxy port is set to 0 to allocate a random
 	// port.
-	dnsProxyPort: 0,
+	DNSProxyPort: 0,
 }
 
 type proxyParams struct {
@@ -63,7 +63,7 @@ func newProxy(params proxyParams, cfg ProxyConfig) *Proxy {
 
 	configureProxyLogger(params.EndpointInfoRegistry, params.MonitorAgent, option.Config.AgentLabels)
 
-	return createProxy(cfg.minPort, cfg.maxPort, cfg.dnsProxyPort, params.Datapath, params.EnvoyProxyIntegration, params.DNSProxyIntegration, params.XdsServer)
+	return createProxy(cfg.MinPort, cfg.MaxPort, cfg.DNSProxyPort, params.Datapath, params.EnvoyProxyIntegration, params.DNSProxyIntegration, params.XdsServer)
 }
 
 type envoyProxyIntegrationParams struct {