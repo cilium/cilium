5355fd023a0b8e20f5951c9fd9fb26394812578c
David Bimmler <david.bimmler@isovalent.com>
2023-12-12 13:37:17 +0100

    datapath: move BandwidthManager interface to types
    To prevent an import cycle from occurring when importing the device
    table for the bandwidth manager.
    
    Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

--- pkg/datapath/types/datapath.go
+++ pkg/datapath/types/datapath.go
@@ -3,8 +3,6 @@
 
 package types
 
-import "github.com/cilium/cilium/pkg/datapath/linux/bandwidth"
-
 // Datapath is the interface to abstract all datapath interactions. The
 // abstraction allows to implement the datapath requirements with multiple
 // implementations
@@ -35,5 +33,5 @@ type Datapath interface {
 
 	Procfs() string
 
-	BandwidthManager() bandwidth.Manager
+	BandwidthManager() BandwidthManager
 }
