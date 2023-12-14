f87ffd5f059d16348d30e94a256757568acca326
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-12-14 17:11:57 +0100

    gateway-api: improve status reconcile error handling
    Currently, errors during writing the status in the defer function
    aren't returned to the caller (controller-runtime). Hence, a
    successful reconciliation (error = nil) but a failure during
    writing the status would result in a non-updated status (because
    it wouldn't result in an additional reconciliation).
    
    To avoid having to introduce named return params (to let the defer
    function modifying the returned error), this commit gets rid of the
    defer statement at all and replaced it with explicit calls to a newly
    introduced function `handleReconcileErrorWithStatus` that tries to
    update the resource status and reflects any errors during updating the
    status in the returned error.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- operator/pkg/gateway-api/tlsroute_reconcile.go
+++ operator/pkg/gateway-api/tlsroute_reconcile.go
@@ -50,16 +50,11 @@ func (r *tlsRouteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 	}
 
 	tr := original.DeepCopy()
-	defer func() {
-		if err := r.updateStatus(ctx, original, tr); err != nil {
-			scopedLog.WithError(err).Error("Failed to update TLSRoute status")
-		}
-	}()
 
 	// check if this cert is allowed to be used by this gateway
 	grants := &gatewayv1beta1.ReferenceGrantList{}
 	if err := r.Client.List(ctx, grants); err != nil {
-		return controllerruntime.Fail(fmt.Errorf("failed to retrieve reference grants: %w", err))
+		return r.handleReconcileErrorWithStatus(ctx, fmt.Errorf("failed to retrieve reference grants: %w", err), original, tr)
 	}
 
 	// input for the validators
@@ -100,7 +95,7 @@ func (r *tlsRouteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		} {
 			continueCheck, err := fn(i, parent)
 			if err != nil {
-				return ctrl.Result{}, err
+				return r.handleReconcileErrorWithStatus(ctx, fmt.Errorf("failed to apply route check: %w", err), original, tr)
 			}
 
 			if !continueCheck {
@@ -116,9 +111,18 @@ func (r *tlsRouteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		routechecks.CheckBackendIsService,
 		routechecks.CheckBackendIsExistingService,
 	} {
-		if continueCheck, err := fn(i); err != nil || !continueCheck {
-			return ctrl.Result{}, err
+		continueCheck, err := fn(i)
+		if err != nil {
+			return r.handleReconcileErrorWithStatus(ctx, fmt.Errorf("failed to apply Gateway check: %w", err), original, tr)
 		}
+
+		if !continueCheck {
+			break
+		}
+	}
+
+	if err := r.updateStatus(ctx, original, tr); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to update TLSRoute status: %w", err)
 	}
 
 	scopedLog.Info("Successfully reconciled TLSRoute")
@@ -134,4 +138,12 @@ func (r *tlsRouteReconciler) updateStatus(ctx context.Context, original *gateway
 		return nil
 	}
 	return r.Client.Status().Update(ctx, new)
+}
+
+func (r *tlsRouteReconciler) handleReconcileErrorWithStatus(ctx context.Context, reconcileErr error, original *gatewayv1alpha2.TLSRoute, modified *gatewayv1alpha2.TLSRoute) (ctrl.Result, error) {
+	if err := r.updateStatus(ctx, original, modified); err != nil {
+		return controllerruntime.Fail(fmt.Errorf("failed to update TLSRoute status while handling the reconcile error %w: %w", reconcileErr, err))
+	}
+
+	return controllerruntime.Fail(reconcileErr)
 }
