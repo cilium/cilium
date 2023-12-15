9c597c6b3513521cb82cfc94c5ea0b92061a6825
Paul Chaignon <paul.chaignon@gmail.com>
2023-12-15 17:25:19 +0100

    connectivity, utils: Refactor computeExpectedDropReasons
    [ cherry-picked from cilium/cilium-cli repository ]
    
    This commit refactors computeExpectedDropReasons to move the core logic
    to the utils package. We will need to reuse that logic for another set
    of exceptions, this time for XFRM errors.
    
    There are no functional changes in this commit.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- cilium-cli/utils/features/utils.go
+++ cilium-cli/utils/features/utils.go
@@ -40,3 +40,46 @@ func GetIPFamily(addr string) IPFamily {
 
 	return IPFamilyAny
 }
+
+// ComputeFailureExceptions computes a list of failure exceptions for various
+// tests, from a default list of exceptions and a diff given via a CLI flag.
+// The diff is given as a list of exceptions, with optional leading +/- signs.
+// A minus sign means the exception should be removed from the defaults; a plus
+// sign means the exception should be added to the defaults. If there are
+// neither minus nor plus signs, then the given exceptions are used directly
+// without considering the defaults.
+// See the unit tests for examples.
+func ComputeFailureExceptions(defaultExceptions, inputExceptions []string) []string {
+	exceptions := map[string]bool{}
+	if len(inputExceptions) > 0 {
+		// Build final list of failure exceptions based on default exceptions,
+		// added exceptions (+ prefix), and removed exceptions (- prefix).
+		addedAllDefaults := false
+		for _, exception := range inputExceptions {
+			if exception[0] == '+' || exception[0] == '-' {
+				if !addedAllDefaults {
+					for _, r := range defaultExceptions {
+						exceptions[r] = true
+					}
+					addedAllDefaults = true
+				}
+			}
+			switch exception[0] {
+			case '+':
+				exceptions[exception[1:]] = true
+			case '-':
+				exceptions[exception[1:]] = false
+			default:
+				exceptions[exception] = true
+			}
+		}
+	}
+
+	exceptionList := []string{}
+	for exception, isIn := range exceptions {
+		if isIn {
+			exceptionList = append(exceptionList, exception)
+		}
+	}
+	return exceptionList
+}
