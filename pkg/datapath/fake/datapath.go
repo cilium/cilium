098c074bd8d466b8ef02771f5537a0f7c698994b
Fabio Falzoi <fabio.falzoi@isovalent.com>
2023-12-15 17:29:33 +0100

    test/controlplane: Add fake sysctl
    Add a fake implementation of Sysctl interface to be used in controlplane
    testing.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/datapath/fake/datapath.go
+++ pkg/datapath/fake/datapath.go
@@ -116,10 +116,6 @@ func (f *FakeDatapath) WireguardAgent() datapath.WireguardAgent {
 	return nil
 }
 
-func (f *FakeDatapath) Procfs() string {
-	return "/proc"
-}
-
 func (f *FakeDatapath) LBMap() datapath.LBMap {
 	return f.lbmap
 }
