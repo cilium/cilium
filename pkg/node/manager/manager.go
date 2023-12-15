cilium/cilium

commit 454df0c065bd94aa30a9d703732d64a24d3b6073

ipcache/md: Convert metadata key to netip.Prefix
This commit moves most of the newer ipcache logic over to "net/ipnet" so
that we can have stronger types when dealing with later additions to the
core IP structures. Previously with the string keys, the expectation was
that users of the metadata cache would insert IP addresses rather than
prefixes. However, features like CIDR policy need to insert using
prefixes. We could ambiguously insert with either IPs or CIDRs like the
main IPCache structure does, but this can easily introduce subtle bugs.
Instead, swap out the key for the ipcache.metadata structure for
netip.Prefix to force use of prefixes as the key even for existing use
cases that are based around IP addresses. As a side benefit, netip can
be used as map keys and it's supposed to be more efficient than the
prior net types.

This commit does not currently swap the main IPCache structure over to
net/ipnet, as this would force us to deal with conflicts between IP and
prefix keys in the main map. We can first introduce this in the metadata
cache, then introduce a newer IPCache metadata API that handles
conflicts (upcoming commit), migrate the existing codepaths that inject
into the ipcache to the new API, and then finally migrate the main
IPCache structure over to net/netip types.

Co-authored-by: Chris Tarazi <chris@isovalent.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/node/manager/manager.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"math"
 	"net"
+	"net/netip"
 	"sync"
 	"time"
 
@@ -19,6 +20,7 @@ import (
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/inctimer"
+	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/ipcache"
 	ipcacheTypes "github.com/cilium/cilium/pkg/ipcache/types"
 	"github.com/cilium/cilium/pkg/labels"
@@ -54,7 +56,7 @@ type IPCache interface {
 	Upsert(ip string, hostIP net.IP, hostKey uint8, k8sMeta *ipcache.K8sMetadata, newIdentity ipcache.Identity) (bool, error)
 	Delete(IP string, source source.Source) bool
 	TriggerLabelInjection()
-	UpsertMetadata(prefix string, lbls labels.Labels, src source.Source, rid ipcacheTypes.ResourceID)
+	UpsertMetadata(prefix netip.Prefix, lbls labels.Labels, src source.Source, rid ipcacheTypes.ResourceID)
 }
 
 // Configuration is the set of configuration options the node manager depends
@@ -440,14 +442,19 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 			key = 0
 		}
 
-		ipAddrStr := address.IP.String()
+		var prefix netip.Prefix
+		if v4 := address.IP.To4(); v4 != nil {
+			prefix = ip.IPToNetPrefix(v4)
+		} else {
+			prefix = ip.IPToNetPrefix(address.IP.To16())
+		}
+		ipAddrStr := prefix.String()
 		_, err := m.ipcache.Upsert(ipAddrStr, tunnelIP, key, nil, ipcache.Identity{
 			ID:     remoteHostIdentity,
 			Source: n.Source,
 		})
-
 		resource := ipcacheTypes.NewResourceID(ipcacheTypes.ResourceKindNode, "", n.Name)
-		m.upsertIntoIDMD(ipAddrStr, remoteHostIdentity, resource)
+		m.upsertIntoIDMD(prefix, remoteHostIdentity, resource)
 
 		// Upsert() will return true if the ipcache entry is owned by
 		// the source of the node update that triggered this node
@@ -561,7 +568,7 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 // upsertIntoIDMD upserts the given CIDR into the ipcache.identityMetadata
 // (IDMD) map. The given node identity determines which labels are associated
 // with the CIDR.
-func (m *Manager) upsertIntoIDMD(prefix string, id identity.NumericIdentity, rid ipcacheTypes.ResourceID) {
+func (m *Manager) upsertIntoIDMD(prefix netip.Prefix, id identity.NumericIdentity, rid ipcacheTypes.ResourceID) {
 	if id == identity.ReservedIdentityHost {
 		m.ipcache.UpsertMetadata(prefix, labels.LabelHost, source.Local, rid)
 	} else {

daemon/cmd/datapath.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"net"
+	"net/netip"
 	"os"
 	"strings"
 	"time"
@@ -258,7 +259,7 @@ func (d *Daemon) syncEndpointsAndHostIPs() error {
 		// ipcache. Until then, it is expected to succeed.
 		d.ipcache.Upsert(ipIDPair.PrefixString(), nil, hostKey, nil, ipcache.Identity{
 			ID:     ipIDPair.ID,
-			Source: d.sourceByIP(ipIDPair.IP.String(), source.Local),
+			Source: d.sourceByIP(ipIDPair.IP, source.Local),
 		})
 	}
 
@@ -272,7 +273,7 @@ func (d *Daemon) syncEndpointsAndHostIPs() error {
 				log.Debugf("Removed outdated host ip %s from endpoint map", hostIP)
 			}
 
-			d.ipcache.Delete(hostIP, d.sourceByIP(hostIP, source.Local))
+			d.ipcache.Delete(hostIP, d.sourceByIP(ip, source.Local))
 		}
 	}
 
@@ -291,11 +292,16 @@ func (d *Daemon) syncEndpointsAndHostIPs() error {
 	return nil
 }
 
-func (d *Daemon) sourceByIP(prefix string, defaultSrc source.Source) source.Source {
-	if lbls := d.ipcache.GetIDMetadataByIP(prefix); lbls.Has(
-		labels.LabelKubeAPIServer[labels.IDNameKubeAPIServer],
-	) {
-		return source.KubeAPIServer
+func (d *Daemon) sourceByIP(ip net.IP, defaultSrc source.Source) source.Source {
+	if addr, ok := netip.AddrFromSlice(ip); ok {
+		lbls := d.ipcache.GetIDMetadataByIP(addr)
+		if lbls.Has(labels.LabelKubeAPIServer[labels.IDNameKubeAPIServer]) {
+			return source.KubeAPIServer
+		}
+	} else {
+		log.WithFields(logrus.Fields{
+			logfields.IPAddr: ip,
+		}).Warning("BUG: Invalid addr detected in host stack. Please report this bug to the Cilium developers.")
 	}
 	return defaultSrc
 }

pkg/ipcache/cidr.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"net/netip"
 	"strings"
 
 	"github.com/sirupsen/logrus"
@@ -52,30 +53,30 @@ func (ipc *IPCache) AllocateCIDRs(
 	defer cancel()
 
 	ipc.Lock()
-	allocatedIdentities := make(map[string]*identity.Identity, len(prefixes))
+	allocatedIdentities := make(map[netip.Prefix]*identity.Identity, len(prefixes))
 	for i, p := range prefixes {
 		if p == nil {
 			continue
 		}
 
 		lbls := cidr.GetCIDRLabels(p)
-		lbls.MergeLabels(ipc.GetIDMetadataByIP(p.IP.String()))
+		prefix := ip.IPNetToPrefix(p)
+		lbls.MergeLabels(ipc.metadata.get(prefix).ToLabels())
 		oldNID := identity.InvalidIdentity
 		if oldNIDs != nil && len(oldNIDs) > i {
 			oldNID = oldNIDs[i]
 		}
-		id, isNew, err := ipc.allocate(allocateCtx, p, lbls, oldNID)
+		id, isNew, err := ipc.allocate(allocateCtx, prefix, lbls, oldNID)
 		if err != nil {
 			ipc.IdentityAllocator.ReleaseSlice(context.Background(), nil, usedIdentities)
 			ipc.Unlock()
 			return nil, err
 		}
 
-		prefixStr := p.String()
 		usedIdentities = append(usedIdentities, id)
-		allocatedIdentities[prefixStr] = id
+		allocatedIdentities[prefix] = id
 		if isNew {
-			newlyAllocatedIdentities[prefixStr] = id
+			newlyAllocatedIdentities[prefix.String()] = id
 		}
 	}
 	ipc.Unlock()
@@ -169,11 +170,7 @@ func (ipc *IPCache) UpsertGeneratedIdentities(newlyAllocatedIdentities map[strin
 //
 // It is up to the caller to provide the full set of labels for identity
 // allocation.
-func (ipc *IPCache) allocate(ctx context.Context, prefix *net.IPNet, lbls labels.Labels, oldNID identity.NumericIdentity) (*identity.Identity, bool, error) {
-	if prefix == nil {
-		return nil, false, nil
-	}
-
+func (ipc *IPCache) allocate(ctx context.Context, prefix netip.Prefix, lbls labels.Labels, oldNID identity.NumericIdentity) (*identity.Identity, bool, error) {
 	id, isNew, err := ipc.IdentityAllocator.AllocateIdentity(ctx, lbls, false, oldNID)
 	if err != nil {
 		return nil, isNew, fmt.Errorf("failed to allocate identity for cidr %s: %s", prefix, err)
@@ -186,7 +183,7 @@ func (ipc *IPCache) allocate(ctx context.Context, prefix *net.IPNet, lbls labels
 	return id, isNew, err
 }
 
-func (ipc *IPCache) releaseCIDRIdentities(ctx context.Context, identities map[string]*identity.Identity) {
+func (ipc *IPCache) releaseCIDRIdentities(ctx context.Context, identities map[netip.Prefix]*identity.Identity) {
 	// Create a critical section for identity release + removal from ipcache.
 	// Otherwise, it's possible to trigger the following race condition:
 	//
@@ -212,7 +209,7 @@ func (ipc *IPCache) releaseCIDRIdentities(ctx context.Context, identities map[st
 		}
 
 		if released {
-			ipc.deleteLocked(prefix, source.Generated)
+			ipc.deleteLocked(prefix.String(), source.Generated)
 		}
 	}
 }
@@ -224,16 +221,17 @@ func (ipc *IPCache) ReleaseCIDRIdentitiesByCIDR(prefixes []*net.IPNet) {
 	releaseCtx, cancel := context.WithTimeout(context.TODO(), option.Config.KVstoreConnectivityTimeout)
 	defer cancel()
 
-	identities := make(map[string]*identity.Identity, len(prefixes))
+	identities := make(map[netip.Prefix]*identity.Identity, len(prefixes))
 	for _, prefix := range prefixes {
 		if prefix == nil {
 			continue
 		}
 
+		p := ip.IPNetToPrefix(prefix)
 		if id := ipc.IdentityAllocator.LookupIdentity(releaseCtx, cidr.GetCIDRLabels(prefix)); id != nil {
-			identities[prefix.String()] = id
+			identities[p] = id
 		} else {
-			log.Errorf("Unable to find identity of previously used CIDR %s", prefix.String())
+			log.Errorf("Unable to find identity of previously used CIDR %s", p.String())
 		}
 	}
 
@@ -243,7 +241,7 @@ func (ipc *IPCache) ReleaseCIDRIdentitiesByCIDR(prefixes []*net.IPNet) {
 // ReleaseCIDRIdentitiesByID releases the specified identities.
 // When the last use of the identity is released, the ipcache entry is deleted.
 func (ipc *IPCache) ReleaseCIDRIdentitiesByID(ctx context.Context, identities []identity.NumericIdentity) {
-	fullIdentities := make(map[string]*identity.Identity, len(identities))
+	fullIdentities := make(map[netip.Prefix]*identity.Identity, len(identities))
 	for _, nid := range identities {
 		if id := ipc.IdentityAllocator.LookupIdentityByID(ctx, nid); id != nil {
 			cidr, ok := cidrLabelToPrefix(id.CIDRLabel.String())
@@ -254,7 +252,15 @@ func (ipc *IPCache) ReleaseCIDRIdentitiesByID(ctx context.Context, identities []
 				}).Warn("Unexpected release of non-CIDR identity, will leak this identity. Please report this issue to the developers.")
 				continue
 			}
-			fullIdentities[cidr] = id
+			prefix, err := netip.ParsePrefix(strings.TrimPrefix(cidr, labels.LabelSourceCIDR+":"))
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					logfields.Identity: nid,
+					logfields.Labels:   id.Labels,
+				}).Warn("BUG: Cannot parse prefix from CIDR label during CIDR identity release. Please report this issue to the developers.")
+				continue
+			}
+			fullIdentities[prefix] = id
 		} else {
 			log.WithFields(logrus.Fields{
 				logfields.Identity: nid,

pkg/ipcache/metadata.go
@@ -8,13 +8,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net"
+	"net/netip"
 	"sync"
 
 	"github.com/sirupsen/logrus"
 
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/ipcache/types"
 	"github.com/cilium/cilium/pkg/labels"
 	cidrlabels "github.com/cilium/cilium/pkg/labels/cidr"
@@ -65,7 +66,7 @@ type metadata struct {
 	lock.RWMutex
 
 	// m is the actual map containing the mappings.
-	m map[string]prefixInfo
+	m map[netip.Prefix]prefixInfo
 
 	// queued* handle updates into the IPCache. Whenever a label is added
 	// or removed from a specific IP prefix, that prefix is added into
@@ -73,29 +74,29 @@ type metadata struct {
 	// process the metadata changes for these prefixes and potentially
 	// generate updates into the ipcache, policy engine and datapath.
 	queuedChangesMU lock.Mutex
-	queuedPrefixes  map[string]struct{}
+	queuedPrefixes  map[netip.Prefix]struct{}
 }
 
 func newMetadata() *metadata {
 	return &metadata{
-		m:              make(map[string]prefixInfo),
-		queuedPrefixes: make(map[string]struct{}),
+		m:              make(map[netip.Prefix]prefixInfo),
+		queuedPrefixes: make(map[netip.Prefix]struct{}),
 	}
 }
 
-func (m *metadata) dequeuePrefixUpdates() (modifiedPrefixes []string) {
+func (m *metadata) dequeuePrefixUpdates() (modifiedPrefixes []netip.Prefix) {
 	m.queuedChangesMU.Lock()
-	modifiedPrefixes = make([]string, 0, len(m.queuedPrefixes))
+	modifiedPrefixes = make([]netip.Prefix, 0, len(m.queuedPrefixes))
 	for p := range m.queuedPrefixes {
 		modifiedPrefixes = append(modifiedPrefixes, p)
 	}
-	m.queuedPrefixes = make(map[string]struct{})
+	m.queuedPrefixes = make(map[netip.Prefix]struct{})
 	m.queuedChangesMU.Unlock()
 
 	return
 }
 
-func (m *metadata) enqueuePrefixUpdates(prefixes ...string) {
+func (m *metadata) enqueuePrefixUpdates(prefixes ...netip.Prefix) {
 	m.queuedChangesMU.Lock()
 	defer m.queuedChangesMU.Unlock()
 
@@ -110,14 +111,14 @@ func (m *metadata) enqueuePrefixUpdates(prefixes ...string) {
 //
 // The caller must subsequently call ipc.TriggerLabelInjection() to implement
 // these metadata updates into the datapath.
-func (ipc *IPCache) UpsertMetadata(prefix string, lbls labels.Labels, src source.Source, rid types.ResourceID) {
+func (ipc *IPCache) UpsertMetadata(prefix netip.Prefix, lbls labels.Labels, src source.Source, rid types.ResourceID) {
 	ipc.metadata.Lock()
 	ipc.metadata.upsertLocked(prefix, src, rid, lbls)
 	ipc.metadata.Unlock()
 	ipc.metadata.enqueuePrefixUpdates(prefix)
 }
 
-func (m *metadata) upsertLocked(prefix string, src source.Source, resource types.ResourceID, info ...IPMetadata) {
+func (m *metadata) upsertLocked(prefix netip.Prefix, src source.Source, resource types.ResourceID, info ...IPMetadata) {
 	if _, ok := m.m[prefix]; !ok {
 		m.m[prefix] = make(prefixInfo)
 	}
@@ -135,20 +136,21 @@ func (m *metadata) upsertLocked(prefix string, src source.Source, resource types
 // GetIDMetadataByIP returns the associated labels with an IP. The caller must
 // not modifying the returned object as it's a live reference to the underlying
 // map.
-func (ipc *IPCache) GetIDMetadataByIP(prefix string) labels.Labels {
+func (ipc *IPCache) GetIDMetadataByIP(addr netip.Addr) labels.Labels {
+	prefix := netip.PrefixFrom(addr, addr.BitLen())
 	if info := ipc.metadata.get(prefix); info != nil {
 		return info.ToLabels()
 	}
 	return nil
 }
 
-func (m *metadata) get(prefix string) prefixInfo {
+func (m *metadata) get(prefix netip.Prefix) prefixInfo {
 	m.RLock()
 	defer m.RUnlock()
 	return m.getLocked(prefix)
 }
 
-func (m *metadata) getLocked(prefix string) prefixInfo {
+func (m *metadata) getLocked(prefix netip.Prefix) prefixInfo {
 	return m.m[prefix]
 }
 
@@ -168,33 +170,33 @@ func (m *metadata) getLocked(prefix string) prefixInfo {
 // Returns the CIDRs that were not yet processed, for example due to an
 // unexpected error while processing the identity updates for those CIDRs
 // The caller should attempt to retry injecting labels for those CIDRs.
-func (ipc *IPCache) InjectLabels(ctx context.Context, modifiedCIDRs []string) (remainingCIDRs []string, err error) {
+func (ipc *IPCache) InjectLabels(ctx context.Context, modifiedPrefixes []netip.Prefix) (remainingPrefixes []netip.Prefix, err error) {
 	if ipc.IdentityAllocator == nil {
-		return modifiedCIDRs, ErrLocalIdentityAllocatorUninitialized
+		return modifiedPrefixes, ErrLocalIdentityAllocatorUninitialized
 	}
 
 	if ipc.k8sSyncedChecker == nil || !ipc.k8sSyncedChecker.K8sCacheIsSynced() {
-		return modifiedCIDRs, errors.New("k8s cache not fully synced")
+		return modifiedPrefixes, errors.New("k8s cache not fully synced")
 	}
 
 	var (
 		// previouslyAllocatedIdentities maps IP Prefix -> Identity for
 		// old identities where the prefix will now map to a new identity
-		previouslyAllocatedIdentities = make(map[string]Identity)
+		previouslyAllocatedIdentities = make(map[netip.Prefix]Identity)
 		// idsToAdd stores the identities that must be updated via the
 		// selector cache.
 		idsToAdd    = make(map[identity.NumericIdentity]labels.LabelArray)
 		idsToDelete = make(map[identity.NumericIdentity]labels.LabelArray)
 		// entriesToReplace stores the identity to replace in the ipcache.
-		entriesToReplace   = make(map[string]Identity)
-		entriesToDelete    = make(map[string]Identity)
-		forceIPCacheUpdate = make(map[string]bool) // prefix => force
+		entriesToReplace   = make(map[netip.Prefix]Identity)
+		entriesToDelete    = make(map[netip.Prefix]Identity)
+		forceIPCacheUpdate = make(map[netip.Prefix]bool) // prefix => force
 	)
 
 	ipc.metadata.RLock()
 
-	for i, prefix := range modifiedCIDRs {
-		id, entryExists := ipc.LookupByIP(prefix)
+	for i, prefix := range modifiedPrefixes {
+		id, entryExists := ipc.LookupByIP(prefix.String())
 		prefixInfo := ipc.metadata.getLocked(prefix)
 		if prefixInfo == nil {
 			if !entryExists {
@@ -226,7 +228,7 @@ func (ipc *IPCache) InjectLabels(ctx context.Context, modifiedCIDRs []string) (r
 				}).Warning(
 					"Failed to allocate new identity while handling change in labels associated with a prefix.",
 				)
-				remainingCIDRs = modifiedCIDRs[i:]
+				remainingPrefixes = modifiedPrefixes[i:]
 				err = fmt.Errorf("failed to allocate new identity during label injection: %w", err)
 				break
 			}
@@ -280,19 +282,20 @@ func (ipc *IPCache) InjectLabels(ctx context.Context, modifiedCIDRs []string) (r
 
 	ipc.mutex.Lock()
 	defer ipc.mutex.Unlock()
-	for ip, id := range entriesToReplace {
-		hIP, key := ipc.getHostIPCache(ip)
-		meta := ipc.getK8sMetadata(ip)
+	for p, id := range entriesToReplace {
+		prefix := p.String()
+		hIP, key := ipc.getHostIPCache(prefix)
+		meta := ipc.getK8sMetadata(prefix)
 		if _, err2 := ipc.upsertLocked(
-			ip,
+			prefix,
 			hIP,
 			key,
 			meta,
 			id,
-			forceIPCacheUpdate[ip],
+			forceIPCacheUpdate[p],
 		); err2 != nil {
 			log.WithError(err2).WithFields(logrus.Fields{
-				logfields.IPAddr:   ip,
+				logfields.IPAddr:   prefix,
 				logfields.Identity: id,
 			}).Error("Failed to replace ipcache entry with new identity after label removal. Traffic may be disrupted.")
 		}
@@ -330,11 +333,11 @@ func (ipc *IPCache) InjectLabels(ctx context.Context, modifiedCIDRs []string) (r
 	if len(idsToDelete) > 0 {
 		ipc.UpdatePolicyMaps(ctx, nil, idsToDelete)
 	}
-	for ip, id := range entriesToDelete {
-		ipc.deleteLocked(ip, id.Source)
+	for prefix, id := range entriesToDelete {
+		ipc.deleteLocked(prefix.String(), id.Source)
 	}
 
-	return remainingCIDRs, err
+	return remainingPrefixes, err
 }
 
 // UpdatePolicyMaps pushes updates for the specified identities into the policy
@@ -371,7 +374,7 @@ func (ipc *IPCache) UpdatePolicyMaps(ctx context.Context, addedIdentities, delet
 // policy is applied will need to be converted (released and re-allocated) to
 // account for the new kube-apiserver label that will be attached to them. This
 // is a known issue, see GH-17962 below.
-func (ipc *IPCache) injectLabels(ctx context.Context, prefix string, lbls labels.Labels) (*identity.Identity, bool, error) {
+func (ipc *IPCache) injectLabels(ctx context.Context, prefix netip.Prefix, lbls labels.Labels) (*identity.Identity, bool, error) {
 	if lbls.Has(labels.LabelHost[labels.IDNameHost]) {
 		// Associate any new labels with the host identity.
 		//
@@ -414,34 +417,18 @@ func (ipc *IPCache) injectLabels(ctx context.Context, prefix string, lbls labels
 
 // injectLabelsForCIDR will allocate a CIDR identity for the given prefix. The
 // release of the identity must be managed by the caller.
-func (ipc *IPCache) injectLabelsForCIDR(ctx context.Context, p string, lbls labels.Labels) (*identity.Identity, bool, error) {
-	var prefix string
-
-	ip := net.ParseIP(p)
-	if ip == nil {
-		return nil, false, fmt.Errorf("Invalid IP inserted into IdentityMetadata: %s", prefix)
-	} else if ip.To4() != nil {
-		prefix = p + "/32"
-	} else {
-		prefix = p + "/128"
-	}
-
-	_, cidr, err := net.ParseCIDR(prefix)
-	if err != nil {
-		return nil, false, err
-	}
-
-	allLbls := cidrlabels.GetCIDRLabels(cidr)
+func (ipc *IPCache) injectLabelsForCIDR(ctx context.Context, prefix netip.Prefix, lbls labels.Labels) (*identity.Identity, bool, error) {
+	allLbls := cidrlabels.GetCIDRLabels(ip.PrefixToIPNet(prefix))
 	allLbls.MergeLabels(lbls)
 
 	log.WithFields(logrus.Fields{
-		logfields.CIDR:   cidr,
+		logfields.CIDR:   prefix,
 		logfields.Labels: lbls, // omitting allLbls as CIDR labels would make this massive
 	}).Debug(
 		"Injecting CIDR labels for prefix",
 	)
 
-	return ipc.allocate(ctx, cidr, allLbls, identity.InvalidIdentity)
+	return ipc.allocate(ctx, prefix, allLbls, identity.InvalidIdentity)
 }
 
 // RemoveLabelsExcluded removes the given labels from all IPs inside the IDMD
@@ -451,7 +438,7 @@ func (ipc *IPCache) injectLabelsForCIDR(ctx context.Context, p string, lbls labe
 // these changes down into the policy engine and ipcache datapath maps.
 func (ipc *IPCache) RemoveLabelsExcluded(
 	lbls labels.Labels,
-	toExclude map[string]struct{},
+	toExclude map[netip.Prefix]struct{},
 	rid types.ResourceID,
 ) {
 	ipc.metadata.Lock()
@@ -470,8 +457,8 @@ func (ipc *IPCache) RemoveLabelsExcluded(
 // full match.
 //
 // Assumes that the ipcache metadata read lock is taken!
-func (m *metadata) filterByLabels(filter labels.Labels) []string {
-	var matching []string
+func (m *metadata) filterByLabels(filter labels.Labels) []netip.Prefix {
+	var matching []netip.Prefix
 	sortedFilter := filter.SortedList()
 	for prefix, info := range m.m {
 		lbls := info.ToLabels()
@@ -485,7 +472,7 @@ func (m *metadata) filterByLabels(filter labels.Labels) []string {
 // removeLabels asynchronously removes the labels association for a prefix.
 //
 // This function assumes that the ipcache metadata lock is held for writing.
-func (m *metadata) remove(prefix string, resource types.ResourceID, aux ...IPMetadata) {
+func (m *metadata) remove(prefix netip.Prefix, resource types.ResourceID, aux ...IPMetadata) {
 	info, ok := m.m[prefix]
 	if !ok {
 		return

commit fc885b3940da83e5b51459d9153d01e4d73e0c0c

node: Fix IP removal from ipset on node updates
In CI, on upgrade/downgrade tests for IPsec, we have observed regular
failures on some workflows for the node-to-node-encryption test. In this
test, we look for some unencrypted packets from a client pod to a host
on a distinct node; but sometimes, these packets do not show up.

Investigation showed that the packets do show up, but not with the
expected IP address, thus failing to be caught by the tcpdump filter in
use. In the failure state, instead of having the client pod IP as source
address, packets have the origin host IP as source address. This is not
supposed to happen: we use an iptables rule to exclude packets from
masquerading in such case. Why then masquerading still occur? This is
because the rule doesn't apply, the relevant host IP being missing in
the ipset used by the rule.

Looking at the agent, the relevant node IP is missing from the ipset
because on node updates, we sometimes remove it, when we should not. And
the reason for that is that we're not comparing the right values to
decide whether this removal should happen. Indeed, in the following:

    !slices.Contains(ipsAdded, address.IP.String())

The address.IP.String() represents a mere IP address, when the values
added to ipsAdded are netip.Prefix.String(), containing the IP followed
by a CIDR mask with the slash notation. For an example, ipsAdded can be
[172.19.0.2/32 fc00:c111::2/128], and address.IP.String() can be
172.19.0.2 (no CIDR mask): the search fails and we do remove the IP from
the ipset, even if we just added it above in the function.

This commit fixes the comparison to ensure that we're looking for the
relevant values in the slice, which in turns fixes the set of IPs
contained in the ipset, and applies the masquerading exclusion to the
relevant packets.

Fixes: 454df0c065bd ("ipcache/md: Convert metadata key to netip.Prefix")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

pkg/node/manager/manager.go
@@ -527,18 +527,18 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 		// Delete the old node IP addresses if they have changed in this node.
 		var oldNodeIPAddrs []string
 		for _, address := range oldNode.IPAddresses {
-			if option.Config.NodeIpsetNeeded() && address.Type == addressing.NodeInternalIP &&
-				!slices.Contains(ipsAdded, address.IP.String()) {
-				iptables.RemoveFromNodeIpset(address.IP)
-			}
-			if skipIPCache(address) {
-				continue
-			}
 			var prefix netip.Prefix
 			if v4 := address.IP.To4(); v4 != nil {
 				prefix = ip.IPToNetPrefix(v4)
 			} else {
 				prefix = ip.IPToNetPrefix(address.IP.To16())
+			}
+			if option.Config.NodeIpsetNeeded() && address.Type == addressing.NodeInternalIP &&
+				!slices.Contains(ipsAdded, prefix.String()) {
+				iptables.RemoveFromNodeIpset(address.IP)
+			}
+			if skipIPCache(address) {
+				continue
 			}
 			oldNodeIPAddrs = append(oldNodeIPAddrs, prefix.String())
 		}