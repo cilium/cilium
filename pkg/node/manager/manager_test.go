485051c31e418fc7e8ce1bdac7d644a7d99d457a
Quentin Monnet <quentin@isovalent.com>
2023-12-15 16:51:10 +0000

    pkg/node/manager: Add test to validate ipset entries
    Following a bug in the way we update the list of ipset entries in the
    node manager, add a test to ensure that entries are added or removed as
    we expect on node updates.
    
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- pkg/node/manager/manager_test.go
+++ pkg/node/manager/manager_test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net"
 	"net/netip"
+	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -16,6 +17,7 @@ import (
 	"github.com/cilium/cilium/pkg/checker"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/datapath/fake"
+	"github.com/cilium/cilium/pkg/datapath/iptables"
 	"github.com/cilium/cilium/pkg/inctimer"
 	"github.com/cilium/cilium/pkg/ipcache"
 	ipcacheTypes "github.com/cilium/cilium/pkg/ipcache/types"
@@ -34,10 +36,12 @@ type managerTestSuite struct{}
 var _ = check.Suite(&managerTestSuite{})
 
 type configMock struct {
-	Tunneling          bool
-	RemoteNodeIdentity bool
-	NodeEncryption     bool
-	Encryption         bool
+	Tunneling            bool
+	RemoteNodeIdentity   bool
+	NodeEncryption       bool
+	Encryption           bool
+	EnableIPv4Masquerade bool
+	EnableIPv6Masquerade bool
 }
 
 func (c *configMock) TunnelingEnabled() bool {
@@ -54,6 +58,10 @@ func (c *configMock) NodeEncryptionEnabled() bool {
 
 func (c *configMock) EncryptionEnabled() bool {
 	return c.Encryption
+}
+
+func (c *configMock) NodeIpsetNeeded() bool {
+	return !c.Tunneling && (c.EnableIPv4Masquerade || c.EnableIPv6Masquerade)
 }
 
 type nodeEvent struct {
@@ -777,4 +785,130 @@ func (s *managerTestSuite) TestNode(c *check.C) {
 	c.Assert(ok, check.Equals, true)
 	// Needs to be the same as n2
 	c.Assert(n, checker.DeepEquals, *n1V2)
+}
+
+func (s *managerTestSuite) TestNodeIpset(c *check.C) {
+	ipcacheMock := newIPcacheMock()
+	ipsetExpect := func(ip string, expected bool) {
+		setName := iptables.CiliumNodeIpsetV6
+		if v4 := net.ParseIP(ip).To4(); v4 != nil {
+			setName = iptables.CiliumNodeIpsetV4
+		}
+		found := iptables.IpsetContains(setName, strings.ToLower(ip))
+		if found && !expected {
+			c.Errorf("ipset %s contains IP %s but it should not", setName, ip)
+		}
+		if !found && expected {
+			c.Errorf("ipset %s does not contain expected IP %s", setName, ip)
+		}
+	}
+
+	dp := newSignalNodeHandler()
+	dp.EnableNodeAddEvent = true
+	dp.EnableNodeUpdateEvent = true
+	dp.EnableNodeDeleteEvent = true
+	mngr, err := NewManager("test", dp, &configMock{
+		// Tunneling and EnableIPv4Masquerade are disabled and enabled,
+		// respectively, to make sure we update the ipset in the
+		// manager (see NodeIpsetNeeded()).
+		Tunneling:            false,
+		EnableIPv4Masquerade: true,
+		// RemoteNodeIdentity is enabled to make sure we don't skip the
+		// ipcache update in NodeUpdated(), and in particular, the
+		// update to ipsAdded.
+		//
+		// Note: If we don't updated ipsAdded, NodeUpdated() will
+		// remove the ipset entry it just added. I'm not sure whether
+		// this is intended or not, if the ipset entry is not needed in
+		// that case we should check before adding it; if it is needed
+		// even without the ipcache update, then we might have a bug.
+		// Bug this has changed and works differently on the "main'
+		// branch, so I didn't want to change the code too much here.
+		// So we just enable RemoteNodeIdentity for our test.
+		RemoteNodeIdentity: true,
+	}, nil, nil)
+	mngr = mngr.WithIPCache(ipcacheMock)
+	c.Assert(err, check.IsNil)
+	defer mngr.Close()
+	defer iptables.RemoveIpset(iptables.CiliumNodeIpsetV4)
+	defer iptables.RemoveIpset(iptables.CiliumNodeIpsetV6)
+
+	n1 := nodeTypes.Node{
+		Name:    "node1",
+		Cluster: "c1",
+		IPAddresses: []nodeTypes.Address{
+			{
+				Type: addressing.NodeCiliumInternalIP,
+				IP:   net.ParseIP("192.0.2.1"),
+			},
+			{
+				Type: addressing.NodeCiliumInternalIP,
+				IP:   net.ParseIP("2001:DB8::1"),
+			},
+			{
+				Type: addressing.NodeInternalIP,
+				IP:   net.ParseIP("10.0.0.1"),
+			},
+			{
+				Type: addressing.NodeInternalIP,
+				IP:   net.ParseIP("2001:ABCD::1"),
+			},
+		},
+		IPv4HealthIP: net.ParseIP("192.0.2.2"),
+		IPv6HealthIP: net.ParseIP("2001:DB8::2"),
+		Source:       source.KVStore,
+	}
+	mngr.NodeUpdated(n1)
+
+	select {
+	case nodeEvent := <-dp.NodeAddEvent:
+		c.Assert(nodeEvent, checker.DeepEquals, n1)
+	case nodeEvent := <-dp.NodeUpdateEvent:
+		c.Errorf("Unexpected NodeUpdate() event %#v", nodeEvent)
+	case nodeEvent := <-dp.NodeDeleteEvent:
+		c.Errorf("Unexpected NodeDelete() event %#v", nodeEvent)
+	case <-time.After(3 * time.Second):
+		c.Errorf("timeout while waiting for NodeAdd() event")
+	}
+
+	ipsetExpect("192.0.2.1", false)
+	ipsetExpect("2001:DB8::1", false)
+	ipsetExpect("10.0.0.1", true)
+	ipsetExpect("2001:ABCD::1", true)
+
+	n1.IPv4HealthIP = net.ParseIP("192.0.2.20")
+	mngr.NodeUpdated(n1)
+
+	select {
+	case nodeEvent := <-dp.NodeAddEvent:
+		c.Errorf("Unexpected NodeAdd() event %#v", nodeEvent)
+	case nodeEvent := <-dp.NodeUpdateEvent:
+		c.Assert(nodeEvent, checker.DeepEquals, n1)
+	case nodeEvent := <-dp.NodeDeleteEvent:
+		c.Errorf("Unexpected NodeDelete() event %#v", nodeEvent)
+	case <-time.After(3 * time.Second):
+		c.Errorf("timeout while waiting for NodeUpdate() event")
+	}
+
+	ipsetExpect("192.0.2.1", false)
+	ipsetExpect("2001:DB8::1", false)
+	ipsetExpect("10.0.0.1", true)
+	ipsetExpect("2001:ABCD::1", true)
+
+	mngr.NodeDeleted(n1)
+	select {
+	case nodeEvent := <-dp.NodeDeleteEvent:
+		c.Assert(nodeEvent, checker.DeepEquals, n1)
+	case nodeEvent := <-dp.NodeAddEvent:
+		c.Errorf("Unexpected NodeAdd() event %#v", nodeEvent)
+	case nodeEvent := <-dp.NodeUpdateEvent:
+		c.Errorf("Unexpected NodeUpdate() event %#v", nodeEvent)
+	case <-time.After(3 * time.Second):
+		c.Errorf("timeout while waiting for NodeDelete() event")
+	}
+
+	ipsetExpect("192.0.2.1", false)
+	ipsetExpect("2001:DB8::1", false)
+	ipsetExpect("10.0.0.1", false)
+	ipsetExpect("2001:ABCD::1", false)
 }
