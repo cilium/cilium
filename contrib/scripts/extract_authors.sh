98f4343ef9dc5822665ecd33439898d6eac48cfc
ritik <20ritiksingh@gmail.com>
2023-12-16 10:32:04 +0530

    Optimize make update-authors
    extract_author function improed.
    Signed-off-by: Ritik Singh <20ritiksingh@gmail.com>

--- contrib/scripts/extract_authors.sh
+++ contrib/scripts/extract_authors.sh
@@ -5,16 +5,18 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 function extract_authors() {
-	authors=$(git shortlog --summary \
-		  | awk '{$1=""; print $0}' \
-		  | sed -e 's/^ //' \
-			-e '/vagrant/d')
-
-	# Iterate $authors by line
-	IFS=$'\n'
-	for i in $authors; do
-		git log --use-mailmap --author="$i" --format="%<|(40)%aN%aE" | head -1
-	done
+	git log --use-mailmap  --format="%<|(40)%aN%aE"
 }
 
-extract_authors | sort -u
+extract_authors #!/usr/bin/env bash
+
+# Ensure sort order doesn't depend on locale
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+function extract_authors() {
+	git log --use-mailmap  --format="%<|(40)%aN%aE" |sort -u
+}
+
+extract_authors
+
