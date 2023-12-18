e4d877d0044e841898c7bfc9da220be1bf98a8c5
Sebastian Wicki <sebastian@isovalent.com>
2023-12-18 16:41:41 +0100

    nodediscovery: Fix bug where CiliumInternalIP was flapping
    [ upstream commit 263e689a299688e2233e009c22a131d77a1cc89b ]
    
    [ backporter's notes: Trivial conflicts on includes. Non-trivial
      conflicts in the mutateNodeResource function since it was
      significantly refactored. Conflict resolved by moving the
      conflicting code before all appending to the slice of node IP
      addresses. slices.DeleteFunc also doesn't exist in Go 1.20 so this
      commit introduces it in our own slices package. ]
    
    This fixes a bug in `UpdateCiliumNodeResource` where the
    `CiliumInternalIP` (aka `cilium_host` IP, aka router IP) was flapping in
    the node manager during restoration (i.e. during cilium-agent restarts).
    
    In particular in `cluster-pool` mode, `UpdateCiliumNodeResource` is
    called before the `cilium_host` IP has been restored, as there are some
    circular dependencies: The restored IP can only be fully validated after
    the IPAM subsystem is ready, but that in turn can only happen if the
    `CiliumNode` object has been created. The `UpdateCiliumNodeResource`
    function however will only announce the `cilium_host` IP if it has been
    restored.
    
    This commit attempts to break that cycle by not overwriting any already
    existing `CiliumInternalIP` in the CiliumNode resource.
    
    Overall, this change is rather hacky, in particular it does not address
    the fact that other less crucial node information (like the health IP)
    also flaps. But since we want to backport this bugfix to older stable
    branches too, this change is intentionally kept as minimal as possible.
    
    Example node event (as observed by other nodes) before this change:
    
    ```
    2023-12-18T12:58:20.070330814Z level=debug msg="Received node update event from custom-resource" node="{\"Name\":\"kind-worker\",\"Cluster\":\"default\",\"IPAddresses\":[{\"Type\":\"InternalIP\",\"IP\":\"172.18.0.4\"},{\"Type\":\"InternalIP\",\"IP\":\"fc00:c111::4\"}],..." subsys=nodemanager
    2023-12-18T12:58:20.208082226Z level=debug msg="Received node update event from custom-resource" node="{\"Name\":\"kind-worker\",\"Cluster\":\"default\",\"IPAddresses\":[{\"Type\":\"InternalIP\",\"IP\":\"172.18.0.4\"},{\"Type\":\"InternalIP\",\"IP\":\"fc00:c111::4\"},{\"Type\":\"CiliumInternalIP\",\"IP\":\"10.0.1.245\"}],..." subsys=nodemanager
    ```
    
    After this change (note the `CiliumInternalIP` present in both events):
    
    ```
    2023-12-18T15:38:23.695653876Z level=debug msg="Received node update event from custom-resource" node="{\"Name\":\"kind-worker\",\"Cluster\":\"default\",\"IPAddresses\":[{\"Type\":\"CiliumInternalIP\",\"IP\":\"10.0.1.245\"},{\"Type\":\"InternalIP\",\"IP\":\"172.18.0.4\"},{\"Type\":\"InternalIP\",\"IP\":\"fc00:c111::4\"}],..." subsys=nodemanager
    2023-12-18T15:38:23.838604573Z level=debug msg="Received node update event from custom-resource" node="{\"Name\":\"kind-worker\",\"Cluster\":\"default\",\"IPAddresses\":[{\"Type\":\"InternalIP\",\"IP\":\"172.18.0.4\"},{\"Type\":\"InternalIP\",\"IP\":\"fc00:c111::4\"},{\"Type\":\"CiliumInternalIP\",\"IP\":\"10.0.1.245\"}],...}" subsys=nodemanager
    ```
    
    Reported-by: Paul Chaignon <paul.chaignon@gmail.com>
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- pkg/slices/slices.go
+++ pkg/slices/slices.go
@@ -145,3 +145,24 @@ func SubsetOf[S ~[]T, T comparable](a, b S) (bool, []T) {
 	d := Diff(a, b)
 	return len(d) == 0, d
 }
+
+// DeleteFunc removes any elements from s for which del returns true,
+// returning the modified slice.
+// When DeleteFunc removes m elements, it might not modify the elements
+// s[len(s)-m:len(s)]. If those elements contain pointers you might consider
+// zeroing those elements so that objects they reference can be garbage
+// collected.
+func DeleteFunc[S ~[]E, E any](s S, del func(E) bool) S {
+	i := slices.IndexFunc(s, del)
+	if i == -1 {
+		return s
+	}
+	// Don't start copying elements until we find one to delete.
+	for j := i + 1; j < len(s); j++ {
+		if v := s[j]; !del(v) {
+			s[i] = v
+			i++
+		}
+	}
+	return s[:i]
+}
