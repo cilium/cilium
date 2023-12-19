5488ff463bb1ea4b6320cc43d5aa83712a1466d1
Maxime VISONNEAU <maxime.visonneau@gmail.com>
2023-12-19 12:48:08 +0100

    pkg/metrics/bpf: new bpf_maps & bpf_progs metrics
    This change introduces two new metrics to monitor the amount of bpf maps and programs currently defined on the system.
    
    We recently discovered that abnormally large amounts of maps could be found on some of our nodes.
    
    Additionally, this change introduces request concurrency limit as well as dynamic definition of the metrics list throughout the Describe() function.
    
    Signed-off-by: Maxime Visonneau <maxime.visonneau@gmail.com>

--- pkg/metrics/bpf.go
+++ pkg/metrics/bpf.go
@@ -8,26 +8,52 @@ import (
 	"encoding/json"
 	"fmt"
 	"os/exec"
+	"slices"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/singleflight"
 
 	"github.com/cilium/cilium/pkg/time"
 )
 
 type bpfCollector struct {
-	bpfMapsMemory *prometheus.Desc
-	bpfProgMemory *prometheus.Desc
+	sfg singleflight.Group
+
+	bpfMapsCount      *prometheus.Desc
+	bpfMapsMemory     *prometheus.Desc
+	bpfProgramsCount  *prometheus.Desc
+	bpfProgramsMemory *prometheus.Desc
+}
+
+type bpfUsage struct {
+	ids                   []uint64
+	virtualMemoryMaxBytes float64
+}
+
+func (bu bpfUsage) count() float64 {
+	return float64(len(bu.ids))
 }
 
 func newbpfCollector() *bpfCollector {
 	return &bpfCollector{
+		bpfMapsCount: prometheus.NewDesc(
+			prometheus.BuildFQName(Namespace, "", "bpf_maps"),
+			"Total count of BPF maps.",
+			nil, nil,
+		),
 		bpfMapsMemory: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "bpf_maps_virtual_memory_max_bytes"),
 			"BPF maps kernel max memory usage size in bytes.",
 			nil, nil,
 		),
-		bpfProgMemory: prometheus.NewDesc(
+		bpfProgramsCount: prometheus.NewDesc(
+			prometheus.BuildFQName(Namespace, "", "bpf_progs"),
+			"Total count of BPF programs.",
+			nil, nil,
+		),
+		bpfProgramsMemory: prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "", "bpf_progs_virtual_memory_max_bytes"),
 			"BPF programs kernel max memory usage size in bytes.",
 			nil, nil,
@@ -36,55 +62,111 @@ func newbpfCollector() *bpfCollector {
 }
 
 func (s *bpfCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- s.bpfMapsMemory
-	ch <- s.bpfProgMemory
+	prometheus.DescribeByCollect(s, ch)
 }
 
 type memoryEntry struct {
+	ID           uint64 `json:"id"`
+	Name         string `json:"name"`
 	BytesMemlock uint64 `json:"bytes_memlock"`
+
+	// (returned only for programs)
+	MapIDs []uint64 `json:"map_ids"`
 }
 
-func getMemoryUsage(typ string) (uint64, error) {
+func getBPFUsage(typ string, filter func(memoryEntry) bool) (bpfUsage, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	cmd := exec.CommandContext(ctx, "bpftool", "-j", typ, "show")
 	out, err := cmd.Output()
 	if err != nil {
-		return 0, fmt.Errorf("unable to get bpftool output: %w", err)
+		return bpfUsage{}, fmt.Errorf("unable to get bpftool output: %w", err)
 	}
 
-	var memoryEntries []memoryEntry
+	var (
+		memoryEntries []memoryEntry
+		usage         bpfUsage
+	)
+
 	err = json.Unmarshal(out, &memoryEntries)
 	if err != nil {
-		return 0, fmt.Errorf("unable to unmarshal bpftool output: %w", err)
+		return usage, fmt.Errorf("unable to unmarshal bpftool output: %w", err)
 	}
-	var totalMem uint64
+
 	for _, entry := range memoryEntries {
-		totalMem += entry.BytesMemlock
+		if !filter(entry) {
+			continue
+		}
+
+		usage.ids = append(usage.ids, entry.ID)
+		usage.virtualMemoryMaxBytes += float64(entry.BytesMemlock)
 	}
-	return totalMem, nil
+
+	return usage, nil
 }
 
 func (s *bpfCollector) Collect(ch chan<- prometheus.Metric) {
-	mapMem, err := getMemoryUsage("map")
-	if err != nil {
-		logrus.WithError(err).Error("Error while getting BPF maps memory usage")
-	} else {
-		ch <- prometheus.MustNewConstMetric(
-			s.bpfMapsMemory,
-			prometheus.GaugeValue,
-			float64(mapMem),
-		)
+	type bpfUsageResults struct {
+		maps     bpfUsage
+		programs bpfUsage
 	}
 
-	progMem, err := getMemoryUsage("prog")
-	if err != nil {
-		logrus.WithError(err).Error("Error while getting BPF progs memory usage")
-	} else {
-		ch <- prometheus.MustNewConstMetric(
-			s.bpfProgMemory,
-			prometheus.GaugeValue,
-			float64(progMem),
+	// Avoid querying BPF multiple times concurrently, if it happens, additional callers will wait for the
+	// first one to finish and reuse its resulting values.
+	results, err, _ := s.sfg.Do("collect", func() (interface{}, error) {
+		var (
+			results = bpfUsageResults{}
+			err     error
 		)
+
+		if results.maps, err = getBPFUsage("map", func(entry memoryEntry) bool {
+			// Filter on maps prefixed with cilium_
+			return strings.HasPrefix(entry.Name, "cilium_")
+		}); err != nil {
+			return results, err
+		}
+
+		if results.programs, err = getBPFUsage("prog", func(entry memoryEntry) bool {
+			// Filter on programs related to cilium maps
+			for i := 0; i < len(entry.MapIDs); i++ {
+				if slices.Contains(results.maps.ids, entry.MapIDs[i]) {
+					return true
+				}
+			}
+			return false
+		}); err != nil {
+			return results, err
+		}
+
+		return results, nil
+	})
+
+	if err != nil {
+		logrus.WithError(err).Error("retrieving BPF maps & programs usage")
+		return
 	}
+
+	ch <- prometheus.MustNewConstMetric(
+		s.bpfMapsCount,
+		prometheus.GaugeValue,
+		results.(bpfUsageResults).maps.count(),
+	)
+
+	ch <- prometheus.MustNewConstMetric(
+		s.bpfMapsMemory,
+		prometheus.GaugeValue,
+		results.(bpfUsageResults).maps.virtualMemoryMaxBytes,
+	)
+
+	ch <- prometheus.MustNewConstMetric(
+		s.bpfProgramsCount,
+		prometheus.GaugeValue,
+		results.(bpfUsageResults).programs.count(),
+	)
+
+	ch <- prometheus.MustNewConstMetric(
+		s.bpfProgramsMemory,
+		prometheus.GaugeValue,
+		results.(bpfUsageResults).programs.virtualMemoryMaxBytes,
+	)
 }
