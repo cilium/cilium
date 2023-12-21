d2c5ac0c0e7c669212b5f6551ac995d06f09529a
Rastislav Szabo <rastislav.szabo@isovalent.com>
2023-12-21 13:02:50 +0100

    bgpv1: Allow retrieving advertised routes for all peers in GetRoutes API
    [ upstream commit 134d88fa797b1d5fcb4bea12ebe7d99ce22ebf23 ]
    
    [ backporter's note: types.ParseTableType() call replaced with
      api.ToAgentTableType() in BGP manager code, as this has been
      moved/renamed in upstream ]
    
    This enhances the BGP GetRoutes API to allow retrieving advertised
    BGP routes for all configured peers, without the need for specifying
    an exact peer address. The peer address is included in the BgpRoute
    response in case that the source of the route is adj-rib-in or adj-rib-out.
    
    Signed-off-by: Rastislav Szabo <rastislav.szabo@isovalent.com>

--- api/v1/server/embedded_spec.go
+++ api/v1/server/embedded_spec.go
@@ -2051,6 +2051,10 @@ func init() {
     "BgpRoute": {
       "description": "Single BGP route retrieved from the RIB of underlying router",
       "properties": {
+        "neighbor": {
+          "description": "IP address specifying a BGP neighbor if the source table type is adj-rib-in or adj-rib-out",
+          "type": "string"
+        },
         "paths": {
           "description": "List of routing paths leading towards the prefix",
           "type": "array",
@@ -7331,6 +7335,10 @@ func init() {
     "BgpRoute": {
       "description": "Single BGP route retrieved from the RIB of underlying router",
       "properties": {
+        "neighbor": {
+          "description": "IP address specifying a BGP neighbor if the source table type is adj-rib-in or adj-rib-out",
+          "type": "string"
+        },
         "paths": {
           "description": "List of routing paths leading towards the prefix",
           "type": "array",
