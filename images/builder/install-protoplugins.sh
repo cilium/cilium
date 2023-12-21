3777dfbed262b0f0e2c2acbf29b5dcf4232e52e8
Robin Hahling <robin.hahling@gw-computing.net>
2023-12-21 13:40:55 +0100

    images/builder: replace mitchellh/protoc-gen-go-json by mfridman's
    [ upstream commit 6a7993396b054b209a0054bd0f87e0125f5b859e ]
    
    [ backporter's notes: changed the version to v1.2.0 to match the
      currently configured commit sha. ]
    
    Mitchell Hashimoto has announced that he'll be archiving his Go
    repositories in early 2024. Switch to using the "blessed fork" for
    protoc-gen-go-json.
    
    Ref: https://gist.github.com/mitchellh/90029601268e59a29e64e55bab1c5bdc
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -11,5 +11,5 @@ set -o nounset
 # 8ba23be9613c672d40ae261d2a1335d639bdd59b == tag: cmd/protoc-gen-go-grpc/v1.3.0
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@8ba23be9613c672d40ae261d2a1335d639bdd59b
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.31.0
-go install github.com/mitchellh/protoc-gen-go-json@49905733154f04e47d685de62c2cc2b72613b69e
+go install github.com/mfridman/protoc-gen-go-json@v1.2.0
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
