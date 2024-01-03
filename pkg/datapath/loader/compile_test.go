cilium/cilium

commit d2ee07e80526ccb89a2da17d67325d279905a461

loader: log maximum RSS used by clang for compilation
Add the RSS used by clang for compilation to log output. This will
give us a rough idea how much memory a particular compilation took.
The messages look like:

    msg="Compilation had peak RSS of 103808 bytes" compiler-pid=95606
    output=/tmp/TestCompile3849793333/001/bpf_lxc.dbg.o subsys=datapath-loader

Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

pkg/datapath/loader/compile.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"path"
 	"sync"
+	"syscall"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/asm"
@@ -239,12 +240,19 @@ func compile(ctx context.Context, prog *progInfo, dir *directoryInfo) (string, e
 	compileCmd.Stderr = &compilerStderr
 
 	err = compileCmd.Run()
+
+	var maxRSS int64
+	if usage, ok := compileCmd.ProcessState.SysUsage().(*syscall.Rusage); ok {
+		maxRSS = usage.Maxrss
+	}
+
 	if err != nil {
 		err = fmt.Errorf("Failed to compile %s: %w", prog.Output, err)
 
 		if !errors.Is(err, context.Canceled) {
 			log.WithFields(logrus.Fields{
 				"compiler-pid": pidFromProcess(compileCmd.Process),
+				"max-rss":      maxRSS,
 			}).Error(err)
 		}
 
@@ -254,6 +262,13 @@ func compile(ctx context.Context, prog *progInfo, dir *directoryInfo) (string, e
 		}
 
 		return "", err
+	}
+
+	if maxRSS > 0 {
+		log.WithFields(logrus.Fields{
+			"compiler-pid": compileCmd.Process.Pid,
+			"output":       output.Name(),
+		}).Debugf("Compilation had peak RSS of %d bytes", maxRSS)
 	}
 
 	return output.Name(), nil

commit c3f6e61658a53de07955d4ccaefa87e0671e42b2

datapath/loader: fix nil deref in compile
exec.Cmd.ProcessState is populated by Cmd.Wait(). Cmd.Run() does invoke
Wait(), but only if Start() was successful. If start fails (for example
because the clang binary is missing) we never execute Wait and therefore
ProcessState is nil. Trying to retrieve rusage then panics due to a nil
deref. Ensure that ProcessState is not nil before retrieving rusage.

Fixes #29989

Fixes: d2ee07e805 ("loader: log maximum RSS used by clang for compilation")
Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

pkg/datapath/loader/compile.go
@@ -200,8 +200,10 @@ func compile(ctx context.Context, prog *progInfo, dir *directoryInfo) (string, e
 	err = compileCmd.Run()
 
 	var maxRSS int64
-	if usage, ok := compileCmd.ProcessState.SysUsage().(*syscall.Rusage); ok {
-		maxRSS = usage.Maxrss
+	if compileCmd.ProcessState != nil {
+		if usage, ok := compileCmd.ProcessState.SysUsage().(*syscall.Rusage); ok {
+			maxRSS = usage.Maxrss
+		}
 	}
 
 	if err != nil {

pkg/datapath/loader/compile_test.go
@@ -43,3 +43,11 @@ func TestCompile(t *testing.T) {
 		})
 	}
 }
+
+func TestCompileError(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	t.Setenv("PATH", "")
+	_, err := compile(context.Background(), epProg, getDirs(t))
+	require.Error(t, err)
+}