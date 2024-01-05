4b5388475badc98c74f6c8cbad71cfae8d6ad2fc
Timo Beckers <timo@isovalent.com>
2024-01-05 14:26:04 +0100

    bpf: Add 'host not ready' drop reason if host policy cannot be enforced
    [ upstream commit aa51114c3bb1722548c4ffaca82b93f722e4ba1d ]
    [ backporter's notes: The 3 return sites in bpf_lxc were moved in 1.14,
      so these had to be amended. No functional differences. ]
    
    bpf_lxc.c contains multiple tail calls into POLICY_CALL_MAP at the HOST_EP_ID
    slot. The program in this slot is provided by bpf_host.c. During first agent
    startup, there are often multiple Pods pending creation due to no CNI being
    available. As soon as the agent's local API becomes available, these outstanding
    CNI requests have a chance to be accepted by the API handler.
    
    If one such request is serviced before the host datapath controller has a chance
    to grab the compilation lock, the endpoint program will compile and attach first.
    If the host firewall is enabled, and this new workload endpoint sends a packet
    to the host, the host's ingress policy needs to be enforced. However, because
    bpf_host hasn't been loaded yet, this policy program is not yet present in
    POLICY_CALL_MAP, resulting in a missed tail call.
    
    One potential solution to this problem would be making sure the host datapath
    always attaches before workload endpoints do. There's one problem with this
    solution: clustermesh requires data from other clusters in order to correctly
    populate the local ipcache, and the ipcache currently needs to be populated for
    the host endpoint to finish attaching. It obtains this information through
    clustermesh-apiserver, typically deployed onto the local node as a regular Pod.
    This means workload endpoints must be able to deploy before the host endpoint.
    
    As a stop gap, tolerate these kinds of drops and assign them a specific meaning,
    without letting them spill over into the generic 'missed tail call' bucket. To
    stabilize end-to-end tests, we're aiming to enforce zero dropped tail calls in
    all CI scenarios, since it leads to packets that mysteriously go missing,
    introducing chaos that's impossible to troubleshoot.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- api/v1/observer/observer.pb.go
+++ api/v1/observer/observer.pb.go
@@ -156,6 +156,7 @@ const DropReason_AUTH_REQUIRED = flow.DropReason_AUTH_REQUIRED
 const DropReason_NO_EGRESS_GATEWAY = flow.DropReason_NO_EGRESS_GATEWAY
 const DropReason_TTL_EXCEEDED = flow.DropReason_TTL_EXCEEDED
 const DropReason_NO_NODE_ID = flow.DropReason_NO_NODE_ID
+const DropReason_DROP_HOST_NOT_READY = flow.DropReason_DROP_HOST_NOT_READY
 
 var DropReason_name = flow.DropReason_name
 var DropReason_value = flow.DropReason_value
