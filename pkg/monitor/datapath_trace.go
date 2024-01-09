ee27b9c5bd4602f9e4c2e3b5d98fcc3a6d42c0fe
ldelossa <louis.delos@gmail.com>
2024-01-09 08:17:48 -0500

    srv6,bpf: add srv6 related trace reasons
    Include a trace reason for SRv6 encapsulation and decapsulation.
    
    This greatly improves the debugging process, indicating whether SRv6
    VPN related packets are processed by our datapath.
    
    Signed-off-by: ldelossa <louis.delos@gmail.com>

--- pkg/monitor/datapath_trace.go
+++ pkg/monitor/datapath_trace.go
@@ -128,6 +128,8 @@ const (
 	TraceReasonCtRelated
 	TraceReasonCtReopened
 	TraceReasonUnknown
+	TraceReasonSRv6Encap
+	TraceReasonSRv6Decap
 )
 
 var traceReasons = map[uint8]string{
@@ -137,6 +139,8 @@ var traceReasons = map[uint8]string{
 	TraceReasonCtRelated:     "related",
 	TraceReasonCtReopened:    "reopened",
 	TraceReasonUnknown:       "unknown",
+	TraceReasonSRv6Encap:     "srv6-encap",
+	TraceReasonSRv6Decap:     "srv6-decap",
 }
 
 func connState(reason uint8) string {
