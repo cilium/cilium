a5cb3cc81912d6d10a35209265479e05fa96bd79
Julian Wiedmann <jwi@isovalent.com>
2024-01-10 11:28:56 +0200

    bpf: fib: require opt-in for neighour map fallback in fib_do_redirect()
    [ upstream commit bb06f2eb1fba730f2b548b660d4b1ab7593843b5 ]
    
    The neighbour map is populated by the inbound nodeport path, and used to
    cache the client's MAC address. Therefore it only makes sense to use this
    fallback in the LB's reply path.
    
    Opt-out from using it in
    - the LB NAT forward path
    - the LB DSR forward path
    - the outbound EgressGW paths
    - bpf_lxc's reply path, as that's only used with ENABLE_HOST_ROUTING and
      thus can always use the neigh-resolver.
    
    Note that callers which can't use the neigh-map will need *some* sort of
    toleration for failed L2 resolution / DROP_NO_FIB result.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -662,7 +662,7 @@ ct_recreate6:
 	if (is_defined(ENABLE_HOST_ROUTING)) {
 		int oif = 0;
 
-		ret = fib_redirect_v6(ctx, ETH_HLEN, ip6, false, ext_err, &oif);
+		ret = fib_redirect_v6(ctx, ETH_HLEN, ip6, false, false, ext_err, &oif);
 		if (fib_ok(ret))
 			send_trace_notify(ctx, TRACE_TO_NETWORK, SECLABEL_IPV6,
 					  *dst_sec_identity, 0, oif,
@@ -1223,7 +1223,7 @@ skip_vtep:
 	if (is_defined(ENABLE_HOST_ROUTING)) {
 		int oif = 0;
 
-		ret = fib_redirect_v4(ctx, ETH_HLEN, ip4, false, ext_err, &oif);
+		ret = fib_redirect_v4(ctx, ETH_HLEN, ip4, false, false, ext_err, &oif);
 		if (fib_ok(ret))
 			send_trace_notify(ctx, TRACE_TO_NETWORK, SECLABEL_IPV4,
 					  *dst_sec_identity, 0, oif,
