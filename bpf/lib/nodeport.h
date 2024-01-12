bf2888ccf56e4cfac5efc27c41467c01641eaea5
Julian Wiedmann <jwi@isovalent.com>
2024-01-12 09:55:57 +0200

    bpf: nodeport: opt-out from neighbour map when XDP-forwarding via tunnel
    [ upstream commit cc25b9194c89be5d5b0f245335cae5b0d74f21c0 ]
    
    When XDP manually builds the tunnel headers and forwards to a remote node,
    it makes no sense to rely on the neighbour map for L2 resolution.
    
    We have to trust that the agent installs managed neigh entries for all
    other nodes, and thus the FIB lookup will always return a L2 resolution.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- bpf/lib/nodeport.h
+++ bpf/lib/nodeport.h
@@ -931,6 +931,7 @@ nodeport_rev_dnat_ingress_ipv6(struct __ctx_buff *ctx, struct trace_ctx *trace,
 	__u32 tunnel_endpoint __maybe_unused = 0;
 	__u32 dst_sec_identity __maybe_unused = 0;
 	__be16 src_port __maybe_unused = 0;
+	bool allow_neigh_map = true;
 	int ifindex = 0;
 
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
@@ -997,6 +998,8 @@ encap_redirect:
 	if (ret == CTX_ACT_REDIRECT && ifindex)
 		return ctx_redirect(ctx, ifindex, 0);
 
+	/* neigh map doesn't contain DMACs for other nodes */
+	allow_neigh_map = false;
 	goto fib_ipv4;
 #endif
 
@@ -1023,7 +1026,7 @@ fib_ipv4:
 		ipv6_addr_copy((union v6addr *)&fib_params.l.ipv6_dst,
 			       (union v6addr *)&ip6->daddr);
 	}
-	return fib_redirect(ctx, true, &fib_params, true, ext_err, &ifindex);
+	return fib_redirect(ctx, true, &fib_params, allow_neigh_map, ext_err, &ifindex);
 }
 
 declare_tailcall_if(__or(__not(is_defined(HAVE_LARGE_INSN_LIMIT)),
@@ -2397,6 +2400,7 @@ nodeport_rev_dnat_ingress_ipv4(struct __ctx_buff *ctx, struct trace_ctx *trace,
 	struct iphdr *ip4;
 	__u32 tunnel_endpoint __maybe_unused = 0;
 	__u32 dst_sec_identity __maybe_unused = 0;
+	bool allow_neigh_map = true;
 	bool check_revdnat = true;
 	bool has_l4_header;
 
@@ -2477,6 +2481,9 @@ redirect:
 
 		if (ret == CTX_ACT_REDIRECT && ifindex)
 			return ctx_redirect(ctx, ifindex, 0);
+
+		/* neigh map doesn't contain DMACs for other nodes */
+		allow_neigh_map = false;
 	}
 #endif
 
@@ -2486,7 +2493,7 @@ redirect:
 	fib_params.l.ipv4_src = ip4->saddr;
 	fib_params.l.ipv4_dst = ip4->daddr;
 
-	return fib_redirect(ctx, true, &fib_params, true, ext_err, &ifindex);
+	return fib_redirect(ctx, true, &fib_params, allow_neigh_map, ext_err, &ifindex);
 }
 
 declare_tailcall_if(__or(__not(is_defined(HAVE_LARGE_INSN_LIMIT)),
