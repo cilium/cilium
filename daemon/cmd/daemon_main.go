57889caad370d647ac87018285ff6f9b8c7ef6dc
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-01-16 16:04:09 +0100

    k8s watcher: remove dependencies to Proxy & EnvoyBackendSyncer
    The global `k8swatcher` no longer depends on the `Proxy` and the
    `EnvoyL7BackendSyncer`. Hence, let's remove the dependencies from
    the `k8swatcher` and the daemon (`EnvoyL7BackendSyncer` only -
    as other components initialized by the daemon rely on the `Proxy`).
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -1650,7 +1650,6 @@ type daemonParams struct {
 	ServiceManager       service.ServiceManager
 	L7Proxy              *proxy.Proxy
 	EnvoyXdsServer       envoy.XDSServer
-	EnvoyBackendSyncer   *envoy.EnvoyServiceBackendSyncer
 	DB                   *statedb.DB
 	APILimiterSet        *rate.APILimiterSet
 	AuthManager          *auth.AuthManager
