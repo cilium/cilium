40bc0c11999ac258f7e2c7a7d2c42692a6178066
Fabio Falzoi <fabio.falzoi@isovalent.com>
2024-01-16 16:04:29 +0100

    iptables: Add rules runtime reconciliation
    As an attempt to make the agent sensitive to runtime changes, the
    iptables manager reinstalls all the iptables rules, including the ones
    related to the proxies, when the datapath is explicitly reinitialized.
    
    This approach does not consider the updates to the external facing
    network devices, that are read only once at the module startup through
    the daemon configuration options.
    
    This commit adds a reconciler to update the rules following any updates
    to the external facing network devices or to the local node.  The
    reconciler listens for those updates and independently rewrites all the
    rules accordingly, keeping into account proxy rules and no track pod
    rules too.
    
    This reconciler is not based on the stateDB generic one but is designed
    from scratch for this specific case. This is needed since the order in
    which the iptables rules are installed in the chains matters, and the
    stateDB generic reconciler does not care about ordering.
    
    To migrate to this new asynchronous reconciliation approach, the manager
    is reworked so that the methods exposed do not immediately reconcile the
    rules, but collect the desired state and ask the underlying reconciler
    to rewrite the rules accordingly.
    
    To rate limit the calls to the iptables command, a ticker is used to
    decide when to reconcile.  This also has the advantage of coalescing a
    burst of changes into a single call to the iptables/ip6tables commands.
    Since a rewrite operation can be pretty expensive in terms of time, this
    results in an overall lower latency for those burst updates.
    
    With this new iptables manager implementation the rules reinstallation
    request from the datapath reinitilization is not needed anymore, hence
    it is removed.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

---
+++ pkg/datapath/iptables/reconciler.go
@@ -0,0 +1,203 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package iptables
+
+import (
+	"context"
+	"net"
+	"net/netip"
+
+	"github.com/cilium/stream"
+	"k8s.io/apimachinery/pkg/util/sets"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/cilium/cilium/pkg/datapath/tables"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/time"
+)
+
+type desiredState struct {
+	installRules bool
+
+	devices       sets.Set[string]
+	localNodeInfo localNodeInfo
+	proxies       sets.Set[proxyInfo]
+	noTrackPods   sets.Set[noTrackPodInfo]
+}
+
+type localNodeInfo struct {
+	internalIPv4  net.IP
+	internalIPv6  net.IP
+	ipv4AllocCIDR string
+	ipv6AllocCIDR string
+}
+
+func (lni localNodeInfo) equal(other localNodeInfo) bool {
+	if lni.internalIPv4.Equal(other.internalIPv4) &&
+		lni.internalIPv6.Equal(other.internalIPv6) &&
+		lni.ipv4AllocCIDR == other.ipv4AllocCIDR &&
+		lni.ipv6AllocCIDR == other.ipv6AllocCIDR {
+		return true
+	}
+	return false
+}
+
+func toLocalNodeInfo(n node.LocalNode) localNodeInfo {
+	var v4AllocCIDR, v6AllocCIDR string
+
+	if n.IPv4AllocCIDR != nil {
+		v4AllocCIDR = n.IPv4AllocCIDR.String()
+	}
+	if n.IPv6AllocCIDR != nil {
+		v6AllocCIDR = n.IPv6AllocCIDR.String()
+	}
+
+	return localNodeInfo{
+		internalIPv4:  n.GetCiliumInternalIP(false),
+		internalIPv6:  n.GetCiliumInternalIP(true),
+		ipv4AllocCIDR: v4AllocCIDR,
+		ipv6AllocCIDR: v6AllocCIDR,
+	}
+}
+
+type proxyInfo struct {
+	name        string
+	port        uint16
+	isLocalOnly bool
+}
+
+type noTrackPodInfo struct {
+	ip   netip.Addr
+	port uint16
+}
+
+func reconciliationLoop(
+	ctx context.Context,
+	log logrus.FieldLogger,
+	health cell.HealthReporter,
+	installIptRules bool,
+	params *reconcilerParams,
+	updateRules func(state desiredState, firstInit bool) error,
+) error {
+	// The minimum interval between reconciliation attempts
+	const minReconciliationInterval = 200 * time.Millisecond
+
+	state := desiredState{
+		installRules: installIptRules,
+		proxies:      sets.New[proxyInfo](),
+		noTrackPods:  sets.New[noTrackPodInfo](),
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	localNodeEvents := stream.ToChannel(ctx, params.localNodeStore)
+	state.localNodeInfo = toLocalNodeInfo(<-localNodeEvents)
+
+	devices, devicesWatch := tables.SelectedDevices(params.devices, params.db.ReadTxn())
+	state.devices = sets.New(tables.DeviceNames(devices)...)
+
+	// Use a ticker to limit how often the desired state is reconciled to avoid doing
+	// lots of operations when e.g. ipset updates.
+	ticker := time.NewTicker(minReconciliationInterval)
+	defer ticker.Stop()
+
+	// stateChanged is true when the desired state has changed or when reconciling it
+	// has failed. It's set to false when reconciling succeeds.
+	stateChanged := true
+
+	firstInit := true
+
+	if err := updateRules(state, firstInit); err != nil {
+		health.Degraded("iptables rules update failed", err)
+		// Keep stateChanged=true and firstInit=true to try again on the next tick.
+	} else {
+		health.OK("iptables rules update completed")
+		firstInit = false
+		stateChanged = false
+	}
+
+stop:
+	for {
+		select {
+		case <-ctx.Done():
+			break stop
+		case <-devicesWatch:
+			devices, devicesWatch = tables.SelectedDevices(params.devices, params.db.ReadTxn())
+			newDevices := sets.New(tables.DeviceNames(devices)...)
+			if newDevices.Equal(state.devices) {
+				continue
+			}
+			state.devices = newDevices
+			stateChanged = true
+		case localNode, ok := <-localNodeEvents:
+			if !ok {
+				break stop
+			}
+			localNodeInfo := toLocalNodeInfo(localNode)
+			if localNodeInfo.equal(state.localNodeInfo) {
+				continue
+			}
+			state.localNodeInfo = localNodeInfo
+			stateChanged = true
+		case proxyInfo, ok := <-params.proxies:
+			if !ok {
+				break stop
+			}
+			if state.proxies.Has(proxyInfo) {
+				continue
+			}
+			state.proxies.Insert(proxyInfo)
+			stateChanged = true
+		case noTrackPod, ok := <-params.addNoTrackPod:
+			if !ok {
+				break stop
+			}
+			if state.noTrackPods.Has(noTrackPod) {
+				continue
+			}
+			state.noTrackPods.Insert(noTrackPod)
+			stateChanged = true
+		case noTrackPod, ok := <-params.delNoTrackPod:
+			if !ok {
+				break stop
+			}
+			if !state.noTrackPods.Has(noTrackPod) {
+				continue
+			}
+			state.noTrackPods.Delete(noTrackPod)
+			stateChanged = true
+		case <-ticker.C:
+			if !stateChanged {
+				continue
+			}
+
+			if err := updateRules(state, firstInit); err != nil {
+				log.WithError(err).Warning("iptables rules full reconciliation failed, will retry a full reconciliation")
+				health.Degraded("iptables rules full reconciliation failed", err)
+				// Keep stateChanged=true to try again on the next tick.
+			} else {
+				health.OK("iptables rules full reconciliation completed")
+				firstInit = false
+				stateChanged = false
+			}
+		}
+	}
+
+	cancel()
+
+	// drain channels
+	for range localNodeEvents {
+	}
+	for range params.proxies {
+	}
+	for range params.addNoTrackPod {
+	}
+	for range params.delNoTrackPod {
+	}
+
+	return nil
+}
