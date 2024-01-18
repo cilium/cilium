cilium/cilium

commit f67260a842eb6943e4aefb5f953d8f9dda158761

bpf: l3: limit kube-proxy workaround in l3_local_delivery() to bpf_overlay
[ upstream commit 334f7f0456b58abe0bc3b2f0ef5648154bf9965b ]

https://github.com/cilium/cilium/pull/22333 fixed a bug for configs with
tunnel-routing and per-EP routes. Here ingress policy was applied twice:
first via tail-call, and then a second time by the to-container program as
the packet traverses the veth pair.

The fix was to avoid the tail-call, and only apply policy with the
to-container program. But the tail-call also contains a kube-proxy
workaround (potential service replies need to pass through kube-proxy for
RevDNAT, so the tail-call punts them to the stack instead of calling
redirect_ep() to forward them straight to the endpoint). So we copied that
workaround into the l3_local_delivery() path.

The tail-call is compiled as part of bpf_lxc, and thus couldn't easily tell
if a packet was received from the tunnel. But as l3_local_delivery() is
inlined into bpf_overlay, we can now limit the work-around to
IS_BPF_OVERLAY. This ensures that the workaround is not applied to eg.
plain pod-to-pod traffic, where bpf_lxc also calls l3_local_delivery().

Fixes: 3d2ceaf3b24d ("bpf: Preserve overlay->lxc path with kube-proxy")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
Signed-off-by: Gilberto Bertin <jibi@cilium.io>

bpf/lib/l3.h
@@ -104,7 +104,7 @@ static __always_inline int ipv6_local_delivery(struct __ctx_buff *ctx, int l3_of
 	ctx->mark |= MARK_MAGIC_IDENTITY;
 	set_identity_mark(ctx, seclabel);
 
-# if defined(TUNNEL_MODE) && !defined(ENABLE_NODEPORT)
+# if defined(IS_BPF_OVERLAY) && !defined(ENABLE_NODEPORT)
 	/* In tunneling mode, we execute this code to send the packet from
 	 * cilium_vxlan to lxc*. If we're using kube-proxy, we don't want to use
 	 * redirect() because that would bypass conntrack and the reverse DNAT.
@@ -116,7 +116,7 @@ static __always_inline int ipv6_local_delivery(struct __ctx_buff *ctx, int l3_of
 	return CTX_ACT_OK;
 # else
 	return redirect_ep(ctx, ep->ifindex, from_host);
-# endif /* !ENABLE_ROUTING && TUNNEL_MODE && !ENABLE_NODEPORT */
+# endif /* IS_BPF_OVERLAY && !ENABLE_NODEPORT */
 #else
 	/* Jumps to destination pod's BPF program to enforce ingress policies. */
 	ctx_store_meta(ctx, CB_SRC_LABEL, seclabel);

commit 98e76bb11fbed95e0bcf3578d60e3f1b6b287810

bpf: l3: fix-up kube-proxy workaround in l3_local_delivery() to bpf_overlay
When backporting
334f7f0456b5 ("bpf: l3: limit kube-proxy workaround in l3_local_delivery() to bpf_overlay"),
I missed that v1.13 still has separate paths for IPv4 and IPv6.

The backport only changed ipv6_local_delivery(), so we still need to fix up
ipv4_local_delivery().

Fixes: f67260a842eb ("bpf: l3: limit kube-proxy workaround in l3_local_delivery() to bpf_overlay")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/l3.h
@@ -175,7 +175,7 @@ static __always_inline int ipv4_local_delivery(struct __ctx_buff *ctx, int l3_of
 	ctx->mark |= MARK_MAGIC_IDENTITY;
 	set_identity_mark(ctx, seclabel);
 
-# if defined(TUNNEL_MODE) && !defined(ENABLE_NODEPORT)
+# if defined(IS_BPF_OVERLAY) && !defined(ENABLE_NODEPORT)
 	/* In tunneling mode, we execute this code to send the packet from
 	 * cilium_vxlan to lxc*. If we're using kube-proxy, we don't want to use
 	 * redirect() because that would bypass conntrack and the reverse DNAT.
@@ -187,7 +187,7 @@ static __always_inline int ipv4_local_delivery(struct __ctx_buff *ctx, int l3_of
 	return CTX_ACT_OK;
 # else
 	return redirect_ep(ctx, ep->ifindex, from_host);
-# endif /* !ENABLE_ROUTING && TUNNEL_MODE && !ENABLE_NODEPORT */
+# endif /* IS_BPF_OVERLAY && !ENABLE_NODEPORT */
 #else
 	/* Jumps to destination pod's BPF program to enforce ingress policies. */
 	ctx_store_meta(ctx, CB_SRC_LABEL, seclabel);