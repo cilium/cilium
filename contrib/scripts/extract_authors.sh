59a3ab99c7afedb83eff8d20c4934088da5487f9
Shivam Purohit <shivampurohit900@gmail.com>
2024-01-18 14:45:18 +0530

    improve extract_authors script
    Signed-off-by: Shivam Purohit <shivampurohit900@gmail.com>

--- contrib/scripts/extract_authors.sh
+++ contrib/scripts/extract_authors.sh
@@ -5,16 +5,7 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 function extract_authors() {
-	authors=$(git shortlog --summary \
-		  | awk '{$1=""; print $0}' \
-		  | sed -e 's/^ //' \
-			-e '/vagrant/d')
-
-	# Iterate $authors by line
-	IFS=$'\n'
-	for i in $authors; do
-		git log --use-mailmap --author="$i" --format="%<|(40)%aN%aE" | head -1
-	done
+	git log --use-mailmap --format="%<|(40)%aN%aE" | sort -f | uniq -w 40 -i | grep -v -e vagrant -e '\[bot\]'
 }
 
-extract_authors | sort -u
+extract_authors
