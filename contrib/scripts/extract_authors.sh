9962bc84c2c446c5de40e1eaeda4e8a39d2beba9
Shivam Purohit <shivampurohit900@gmail.com>
2024-01-21 17:35:27 +0530

    update:script
    Signed-off-by: Shivam Purohit <shivampurohit900@gmail.com>

--- contrib/scripts/extract_authors.sh
+++ contrib/scripts/extract_authors.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Ensure sort order doesn't depend on locale
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+export LC_COLLATE=C
 
 function extract_authors() {
-	git log --use-mailmap --format="%<|(40)%aN%aE" | sort -f | uniq -w 40 -i | grep -v -e vagrant -e '\[bot\]'
+	git log --use-mailmap --format="%<|(40)%aN%aE" | sort -fu | uniq -w 40 | grep -v -e vagrant -e '\[bot\]'
 }
 
 extract_authors
