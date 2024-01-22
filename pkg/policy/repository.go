cilium/cilium

commit 4cb094060d5a6b93313e222516c644ef5c51da07

pkg/policy: index policies by k8s UID
As k8s UID are unique, we can index the policies by their UId. This will
decrease Cilium's boot-up time for clusters with a large number of
network policies.

For a cluster with a lot of network policies that was taking ~4m20s to
boot Cilium, with this change it will take ~20 seconds.

Signed-off-by: André Martins <andre@cilium.io>

pkg/policy/repository.go
@@ -111,6 +111,9 @@ type Repository struct {
 	Mutex lock.RWMutex
 	rules ruleSlice
 
+	// rulesIndexByK8sUID indexes the rules by k8s UID.
+	rulesIndexByK8sUID map[string]*rule
+
 	// revision is the revision of the policy repository. It will be
 	// incremented whenever the policy repository is changed.
 	// Always positive (>0).
@@ -190,9 +193,10 @@ func NewStoppedPolicyRepository(
 ) *Repository {
 	selectorCache := NewSelectorCache(idAllocator, idCache)
 	repo := &Repository{
-		selectorCache: selectorCache,
-		certManager:   certManager,
-		secretManager: secretManager,
+		rulesIndexByK8sUID: map[string]*rule{},
+		selectorCache:      selectorCache,
+		certManager:        certManager,
+		secretManager:      secretManager,
 	}
 	repo.revision.Store(1)
 	repo.policyCache = NewPolicyCache(repo, true)
@@ -367,6 +371,13 @@ func (p *Repository) AllowsEgressRLocked(ctx *SearchContext) api.Decision {
 func (p *Repository) SearchRLocked(lbls labels.LabelArray) api.Rules {
 	result := api.Rules{}
 
+	if uid := lbls.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PolicyLabelUID); uid != "" {
+		r, ok := p.rulesIndexByK8sUID[uid]
+		if ok {
+			result = append(result, &r.Rule)
+		}
+		return result
+	}
 	for _, r := range p.rules {
 		if r.Labels.Contains(lbls) {
 			result = append(result, &r.Rule)
@@ -406,6 +417,9 @@ func (p *Repository) AddListLocked(rules api.Rules) (ruleSlice, uint64) {
 			metadata: newRuleMetadata(),
 		}
 		newList[i] = newRule
+		if uid := rules[i].Labels.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PolicyLabelUID); uid != "" {
+			p.rulesIndexByK8sUID[uid] = newRule
+		}
 	}
 
 	p.rules = append(p.rules, newList...)
@@ -516,6 +530,9 @@ func (p *Repository) DeleteByLabelsLocked(lbls labels.LabelArray) (ruleSlice, ui
 	if deleted > 0 {
 		p.BumpRevision()
 		p.rules = new
+		if uid := lbls.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PolicyLabelUID); uid != "" {
+			delete(p.rulesIndexByK8sUID, uid)
+		}
 		metrics.Policy.Sub(float64(deleted))
 	}
 

commit 5748d9227a0f1a565e03430c31aabf19f4d48919

pkg/policy: fix policy UID index consistency
So, a single {,C}CNP can have multiple rules. This can be archived by
using the "specs" field. In that case, we would end up with more than
one rule in the policy repository that has the same UID. We need to
properly handle this to avoid leaking policies and memory.

In general I assume we can expect that the number of rules with the same
UID is pretty low compared to the total number of rules.

This should also improve performance when running "DeleteByLabelsLocked"
with a label set that has a UID and no rules exists matching all those
labels. In that way we can exit early.

When inserting new rules with a UID we can simply append to the list in
the index. Deleting is a bit more tricky where we need to ensure the
consistency of the index and the rule list.

note; this consistency only works when there is a single UID label in
the labelsets, as having multiple with unstable order would mess up the
consistency. This change makes it better, but will not fully solve that
case.

Fixes: 4cb094060d ("pkg/policy: index policies by k8s UID")
Signed-off-by: Odin Ugedal <odin@uged.al>
Signed-off-by: Odin Ugedal <ougedal@palantir.com>

pkg/policy/repository.go
@@ -112,7 +112,7 @@ type Repository struct {
 	rules ruleSlice
 
 	// rulesIndexByK8sUID indexes the rules by k8s UID.
-	rulesIndexByK8sUID map[string]*rule
+	rulesIndexByK8sUID map[string][]*rule
 
 	// revision is the revision of the policy repository. It will be
 	// incremented whenever the policy repository is changed.
@@ -193,7 +193,7 @@ func NewStoppedPolicyRepository(
 ) *Repository {
 	selectorCache := NewSelectorCache(idAllocator, idCache)
 	repo := &Repository{
-		rulesIndexByK8sUID: map[string]*rule{},
+		rulesIndexByK8sUID: map[string][]*rule{},
 		selectorCache:      selectorCache,
 		certManager:        certManager,
 		secretManager:      secretManager,
@@ -372,9 +372,11 @@ func (p *Repository) SearchRLocked(lbls labels.LabelArray) api.Rules {
 	result := api.Rules{}
 
 	if uid := lbls.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PolicyLabelUID); uid != "" {
-		r, ok := p.rulesIndexByK8sUID[uid]
+		rules, ok := p.rulesIndexByK8sUID[uid]
 		if ok {
-			result = append(result, &r.Rule)
+			for _, r := range rules {
+				result = append(result, &r.Rule)
+			}
 		}
 		return result
 	}
@@ -418,7 +420,7 @@ func (p *Repository) AddListLocked(rules api.Rules) (ruleSlice, uint64) {
 		}
 		newList[i] = newRule
 		if uid := rules[i].Labels.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PolicyLabelUID); uid != "" {
-			p.rulesIndexByK8sUID[uid] = newRule
+			p.rulesIndexByK8sUID[uid] = append(p.rulesIndexByK8sUID[uid], newRule)
 		}
 	}
 
@@ -513,10 +515,29 @@ func (r ruleSlice) UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpoints
 // contain the specified labels. Returns the revision of the policy repository
 // after deleting the rules, as well as now many rules were deleted.
 func (p *Repository) DeleteByLabelsLocked(lbls labels.LabelArray) (ruleSlice, uint64, int) {
-
 	deleted := 0
 	new := p.rules[:0]
 	deletedRules := ruleSlice{}
+
+	if uid := lbls.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PolicyLabelUID); uid != "" {
+		indexedRules, found := p.rulesIndexByK8sUID[uid]
+		if !found {
+			return deletedRules, p.GetRevision(), deleted
+		}
+		newIndex := indexedRules[:0]
+		for _, r := range indexedRules {
+			if !r.Labels.Contains(lbls) {
+				newIndex = append(newIndex, r)
+			}
+		}
+		if len(newIndex) == len(indexedRules) {
+			return deletedRules, p.GetRevision(), deleted
+		} else if len(newIndex) > 0 {
+			p.rulesIndexByK8sUID[uid] = newIndex
+		} else {
+			delete(p.rulesIndexByK8sUID, uid)
+		}
+	}
 
 	for _, r := range p.rules {
 		if !r.Labels.Contains(lbls) {
@@ -527,12 +548,25 @@ func (p *Repository) DeleteByLabelsLocked(lbls labels.LabelArray) (ruleSlice, ui
 		}
 	}
 
+	if uid := lbls.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PolicyLabelUID); deleted > 0 && uid == "" {
+		for uid, indexedRules := range p.rulesIndexByK8sUID {
+			newIndex := indexedRules[:0]
+			for _, r := range indexedRules {
+				if !r.Labels.Contains(lbls) {
+					newIndex = append(newIndex, r)
+				}
+			}
+			if len(newIndex) > 0 {
+				p.rulesIndexByK8sUID[uid] = newIndex
+			} else {
+				delete(p.rulesIndexByK8sUID, uid)
+			}
+		}
+	}
+
 	if deleted > 0 {
 		p.BumpRevision()
 		p.rules = new
-		if uid := lbls.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PolicyLabelUID); uid != "" {
-			delete(p.rulesIndexByK8sUID, uid)
-		}
 		metrics.Policy.Sub(float64(deleted))
 	}
 

pkg/policy/repository_test.go
@@ -248,6 +248,7 @@ func (ds *PolicyTestSuite) TestAddSearchDelete(c *C) {
 	lbls1 := labels.LabelArray{
 		labels.ParseLabel("tag1"),
 		labels.ParseLabel("tag2"),
+		labels.ParseLabel("k8s:io.cilium.k8s.policy.uid=uid-here"),
 	}
 	rule1 := api.Rule{
 		EndpointSelector: api.NewESFromLabels(labels.ParseSelectLabel("foo")),