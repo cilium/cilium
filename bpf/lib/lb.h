5fc07e17073400427d7835086c24b6f211539f78
Julian Wiedmann <jwi@isovalent.com>
2024-01-24 13:57:24 +0200

    bpf: lb: return drop reasons from __lb4_rev_nat()
    [ upstream commit 3932a4b9f4577a2133a6d436cc20542d9b48f8ef ]
    
    Fix up some ctx_load_bytes() usage to return a drop reason, and not the
    raw kernel errno.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- bpf/lib/lb.h
+++ bpf/lib/lb.h
@@ -1048,9 +1048,8 @@ static __always_inline int __lb4_rev_nat(struct __ctx_buff *ctx, int l3_off, int
 		old_sip = tuple->saddr;
 		tuple->saddr = nat->address;
 	} else {
-		ret = ctx_load_bytes(ctx, l3_off + offsetof(struct iphdr, saddr), &old_sip, 4);
-		if (IS_ERR(ret))
-			return ret;
+		if (ctx_load_bytes(ctx, l3_off + offsetof(struct iphdr, saddr), &old_sip, 4) < 0)
+			return DROP_INVALID;
 	}
 
 #ifndef DISABLE_LOOPBACK_LB
@@ -1063,9 +1062,8 @@ static __always_inline int __lb4_rev_nat(struct __ctx_buff *ctx, int l3_off, int
 		 */
 		__be32 old_dip;
 
-		ret = ctx_load_bytes(ctx, l3_off + offsetof(struct iphdr, daddr), &old_dip, 4);
-		if (IS_ERR(ret))
-			return ret;
+		if (ctx_load_bytes(ctx, l3_off + offsetof(struct iphdr, daddr), &old_dip, 4) < 0)
+			return DROP_INVALID;
 
 		cilium_dbg_lb(ctx, DBG_LB4_LOOPBACK_SNAT_REV, old_dip, old_sip);
 
