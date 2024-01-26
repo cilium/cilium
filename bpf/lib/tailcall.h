3b7ca5bee25ced15f6398431ef2f2b498ae8ffc9
Dylan Reimerink <dylan.reimerink@isovalent.com>
2024-01-26 16:09:08 +0100

    bpf: Remove `declare_tailcall_if`
    Remove `declare_tailcall_if`, so we always emit the tailcall programs
    into the ELF. The followup commit will implement pruning logic based
    on the actual usage of the tail calls. This means that we will only
    need the `invoke_tailcall_if` without the need to keep both the
    declaration and invocation in sync.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- bpf/lib/tailcall.h
+++ bpf/lib/tailcall.h
@@ -35,71 +35,17 @@
 #define __not_1 0
 #define __not(x) __eval(__not_, x)
 
-/* declare_tailcall_if() and invoke_tailcall_if() is a pair
- * of helpers which based on COND either selects to emit a
- * tail call for the underlying function when true or emits
- * it as inlined when false. COND can be selected by one or
- * multiple compile time flags.
- *
- * Usage example:
- *
- * 1) Declaration:
- *
- * declare_tailcall_if(__and(is_defined(ENABLE_IPV4), is_defined(ENABLE_IPV6)),
- *                     CILIUM_CALL_FOO)
- * int foo_fn(struct __ctx_buff *ctx)
- * {
- *    [...]
- * }
- *
- * 2) Call-site:
+/* invoke_tailcall_if() is a helper which based on COND either selects to emit
+ * a tail call for the underlying function when true or emits it as inlined
+ * when false. COND can be selected by one or multiple compile time flags.
  *
  * [...]
  * invoke_tailcall_if(__and(is_defined(ENABLE_IPV4), is_defined(ENABLE_IPV6)),
  *                    CILIUM_CALL_FOO, foo_fn);
  * [...]
  *
- * 3) Compilation result:
- *
- * When compiled with -DENABLE_IPV4 and -DENABLE_IPV6 both
- * set, then above emits a tail call as follows:
- *
- * __attribute__((section("2" "/" "10"), used))
- * int foo_fn(struct __ctx_buff *ctx)
- * {
- *    [...]
- * }
- *
- * [...]
- * do { ep_tail_call(ctx, 10); ret = -140; } while (0);
- * [...]
- *
- * The fall-through side sets DROP_MISSED_TAIL_CALL as ret.
- *
- * When only one of them is set in the above example or none
- * of them, then the code emission looks like:
- *
- * static __inline __attribute__ ((__always_inline__))
- * int foo_fn(struct __ctx_buff *ctx)
- * {
- *    [...]
- * }
- *
- * [...]
- * return foo_fn(ctx);
- * [...]
- *
- * Selectors can be single is_defined(), or multiple ones
- * combined with __and() or __or() macros. COND must be
- * the same expression for declare_tailcall_if() and the
- * invoke_tailcall_if() part.
+ * The loader will only load tail calls if they are invoked at least once.
  */
-#define __declare_tailcall_if_0(NAME)         \
-	static __always_inline
-#define __declare_tailcall_if_1(NAME)         \
-	__section_tail(CILIUM_MAP_CALLS, NAME)
-#define declare_tailcall_if(COND, NAME)       \
-	__eval(__declare_tailcall_if_, COND)(NAME)
 
 #define __invoke_tailcall_if_0(NAME, FUNC, EXT_ERR)			\
 	FUNC(ctx)
