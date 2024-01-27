d42a1423ef2f87eac27c5044ccf580575d63d2b2
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2024-01-27 20:48:01 +0200

    `encrypt rotate-key` command implementation.
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>
    
     Please enter the commit message for your changes. Lines starting
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- cilium-cli/internal/cli/cmd/encrypt.go
+++ cilium-cli/internal/cli/cmd/encrypt.go
@@ -22,6 +22,7 @@ func newCmdEncrypt() *cobra.Command {
 		Aliases: []string{"encrypt"},
 	}
 	cmd.AddCommand(newCmdEncryptStatus())
+	cmd.AddCommand(newCmdIPsecRotateKey())
 	return cmd
 }
 
@@ -43,6 +44,26 @@ func newCmdEncryptStatus() *cobra.Command {
 	cmd.Flags().StringVar(&params.AgentPodSelector, "agent-pod-selector", defaults.AgentPodSelector, "Label on cilium-agent pods to select with")
 	cmd.Flags().StringVar(&params.NodeName, "node", "", "Node from which encryption status will be fetched, omit to select all nodes")
 	cmd.Flags().BoolVar(&params.PerNodeDetails, "per-node-details", false, "Encryption status will be displayed for each cluster node separately")
+	cmd.Flags().DurationVar(&params.WaitDuration, "wait-duration", 1*time.Minute, "Maximum time to wait for result, default 1 minute")
+	cmd.Flags().StringVarP(&params.Output, "output", "o", status.OutputSummary, "Output format. One of: json, summary")
+	return cmd
+}
+
+func newCmdIPsecRotateKey() *cobra.Command {
+	params := encrypt.Parameters{}
+	cmd := &cobra.Command{
+		Use:   "rotate-key",
+		Short: "Rotate IPsec key",
+		Long:  "This command rotates IPsec encryption key in the cluster",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			params.CiliumNamespace = namespace
+			s := encrypt.NewStatus(k8sClient, params)
+			if err := s.IPsecRotateKey(context.Background()); err != nil {
+				fatalf("Unable to rotate IPsec key: %s", err)
+			}
+			return nil
+		},
+	}
 	cmd.Flags().DurationVar(&params.WaitDuration, "wait-duration", 1*time.Minute, "Maximum time to wait for result, default 1 minute")
 	cmd.Flags().StringVarP(&params.Output, "output", "o", status.OutputSummary, "Output format. One of: json, summary")
 	return cmd
