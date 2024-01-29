e7dc978035428c636c8684ea2571d79ffee59335
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2024-01-29 11:05:14 +0200

    IPsec key id renamed to spi. SPI suffix added to support SPI with `+` sign. Tests added.
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- cilium-cli/encrypt/ipsec_rotate_key.go
+++ cilium-cli/encrypt/ipsec_rotate_key.go
@@ -18,10 +18,11 @@ import (
 )
 
 type ipsecKey struct {
-	id     int
-	algo   string
-	random string
-	size   int
+	spi       int
+	spiSuffix bool
+	algo      string
+	random    string
+	size      int
 }
 
 // IPsecRotateKey rotates IPsec key.
@@ -50,34 +51,44 @@ func (s *Status) IPsecRotateKey(ctx context.Context) error {
 		return fmt.Errorf("failed to patch IPsec secret with new key: %s", err)
 	}
 
-	_, err = fmt.Printf("IPsec key successfully rotated, new key ID: %d\n", newKey.id)
+	_, err = fmt.Printf("IPsec key successfully rotated, new key SPI: %d\n", newKey.spi)
 	return err
 }
 
 func (k ipsecKey) String() string {
-	return fmt.Sprintf("%d %s %s %d", k.id, k.algo, k.random, k.size)
+	spiSuffix := ""
+	if k.spiSuffix {
+		spiSuffix = "+"
+	}
+	return fmt.Sprintf("%d%s %s %s %d", k.spi, spiSuffix, k.algo, k.random, k.size)
 }
 
-var ipsecKeyRegex = regexp.MustCompile(`^([[:digit:]]+)[[:space:]](\S+)[[:space:]]([[:alnum:]]+)[[:space:]]([[:digit:]]+)$`)
+var ipsecKeyRegex = regexp.MustCompile(`^([[:digit:]]+\+?)[[:space:]](\S+)[[:space:]]([[:alnum:]]+)[[:space:]]([[:digit:]]+)$`)
 
 func ipsecKeyFromString(s string) (ipsecKey, error) {
 	parts := ipsecKeyRegex.FindStringSubmatch(s)
 	if len(parts) != 5 {
 		return ipsecKey{}, fmt.Errorf("IPsec key has unsupported format")
 	}
-	id, err := strconv.Atoi(parts[1])
+	spiSuffix := false
+	if strings.HasSuffix(parts[1], "+") {
+		spiSuffix = true
+		parts[1] = strings.TrimSuffix(parts[1], "+")
+	}
+	spi, err := strconv.Atoi(parts[1])
 	if err != nil {
-		return ipsecKey{}, fmt.Errorf("invalid IPsec key ID: %s", parts[1])
+		return ipsecKey{}, fmt.Errorf("invalid IPsec key SPI: %s", parts[1])
 	}
 	size, err := strconv.Atoi(parts[4])
 	if err != nil {
 		return ipsecKey{}, fmt.Errorf("invalid IPsec key size: %s", parts[4])
 	}
 	key := ipsecKey{
-		id:     id,
-		algo:   parts[2],
-		random: parts[3],
-		size:   size,
+		spi:       spi,
+		spiSuffix: spiSuffix,
+		algo:      parts[2],
+		random:    parts[3],
+		size:      size,
 	}
 	return key, nil
 }
@@ -95,15 +106,16 @@ func (k ipsecKey) rotate() (ipsecKey, error) {
 		random.WriteString(fmt.Sprintf("%02x", c))
 	}
 
-	id := k.id + 1
-	if id >= maxIPsecSPI {
-		id = 1
+	spi := k.spi + 1
+	if spi >= maxIPsecSPI {
+		spi = 1
 	}
 	key := ipsecKey{
-		id:     id,
-		algo:   k.algo,
-		random: random.String(),
-		size:   k.size,
+		spi:       spi,
+		spiSuffix: k.spiSuffix,
+		algo:      k.algo,
+		random:    random.String(),
+		size:      k.size,
 	}
 	return key, nil
 }
