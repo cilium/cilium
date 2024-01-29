9a2c1fc981585d58a85e6a9be6c21dcc79081460
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2024-01-29 11:05:14 +0200

    IPsec key id renamed to spi. SPI suffix added to support SPI with `+` sign. Tests added.
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- cilium-cli/encrypt/ipsec_rotate_key_test.go
+++ cilium-cli/encrypt/ipsec_rotate_key_test.go
@@ -17,10 +17,21 @@ func Test_ipsecKeyFromString(t *testing.T) {
 		{
 			have: "3 rfc4106(gcm(aes)) 41049390e1e2b5d6543901daab6435f4042155fe 128",
 			expected: ipsecKey{
-				id:     3,
-				algo:   "rfc4106(gcm(aes))",
-				random: "41049390e1e2b5d6543901daab6435f4042155fe",
-				size:   128,
+				spi:       3,
+				spiSuffix: false,
+				algo:      "rfc4106(gcm(aes))",
+				random:    "41049390e1e2b5d6543901daab6435f4042155fe",
+				size:      128,
+			},
+		},
+		{
+			have: "3+ rfc4106(gcm(aes)) 41049390e1e2b5d6543901daab6435f4042155fe 128",
+			expected: ipsecKey{
+				spi:       3,
+				spiSuffix: true,
+				algo:      "rfc4106(gcm(aes))",
+				random:    "41049390e1e2b5d6543901daab6435f4042155fe",
+				size:      128,
 			},
 		},
 	}
@@ -35,18 +46,39 @@ func Test_ipsecKeyFromString(t *testing.T) {
 }
 
 func Test_ipsecKey_String(t *testing.T) {
-	key := ipsecKey{
-		id:     3,
-		algo:   "rfc4106(gcm(aes))",
-		random: "41049390e1e2b5d6543901daab6435f4042155fe",
-		size:   128,
+	testCases := []struct {
+		have     ipsecKey
+		expected string
+	}{
+		{
+			have: ipsecKey{
+				spi:       3,
+				spiSuffix: false,
+				algo:      "rfc4106(gcm(aes))",
+				random:    "41049390e1e2b5d6543901daab6435f4042155fe",
+				size:      128,
+			},
+			expected: "3 rfc4106(gcm(aes)) 41049390e1e2b5d6543901daab6435f4042155fe 128",
+		},
+		{
+			have: ipsecKey{
+				spi:       3,
+				spiSuffix: true,
+				algo:      "rfc4106(gcm(aes))",
+				random:    "41049390e1e2b5d6543901daab6435f4042155fe",
+				size:      128,
+			},
+			expected: "3+ rfc4106(gcm(aes)) 41049390e1e2b5d6543901daab6435f4042155fe 128",
+		},
 	}
-	expected := "3 rfc4106(gcm(aes)) 41049390e1e2b5d6543901daab6435f4042155fe 128"
 
-	// function to test
-	actual := key.String()
+	for _, tt := range testCases {
+		// function to test
+		actual := tt.have.String()
 
-	require.Equal(t, expected, actual)
+		require.Equal(t, tt.expected, actual)
+	}
+
 }
 
 func Test_ipsecKey_rotate(t *testing.T) {
@@ -56,14 +88,16 @@ func Test_ipsecKey_rotate(t *testing.T) {
 	}{
 		{
 			have: ipsecKey{
-				id:     3,
-				algo:   "rfc4106(gcm(aes))",
-				random: "41049390e1e2b5d6543901daab6435f4042155fe",
-				size:   128,
+				spi:       3,
+				spiSuffix: false,
+				algo:      "rfc4106(gcm(aes))",
+				random:    "41049390e1e2b5d6543901daab6435f4042155fe",
+				size:      128,
 			},
 			expected: ipsecKey{
-				id:   4,
-				algo: "rfc4106(gcm(aes))",
+				spi:       4,
+				spiSuffix: false,
+				algo:      "rfc4106(gcm(aes))",
 				// this field will be randomly generated, `require.NotEqual` used for verification
 				random: "41049390e1e2b5d6543901daab6435f4042155fe",
 				size:   128,
@@ -71,14 +105,50 @@ func Test_ipsecKey_rotate(t *testing.T) {
 		},
 		{
 			have: ipsecKey{
-				id:     16,
-				algo:   "rfc4106(gcm(aes))",
+				spi:       16,
+				spiSuffix: false,
+				algo:      "rfc4106(gcm(aes))",
+				random:    "41049390e1e2b5d6543901daab6435f4042155fe",
+				size:      128,
+			},
+			expected: ipsecKey{
+				spi:       1,
+				spiSuffix: false,
+				algo:      "rfc4106(gcm(aes))",
+				// this field will be randomly generated, `require.NotEqual` used for verification
 				random: "41049390e1e2b5d6543901daab6435f4042155fe",
 				size:   128,
 			},
+		},
+		{
+			have: ipsecKey{
+				spi:       3,
+				spiSuffix: true,
+				algo:      "rfc4106(gcm(aes))",
+				random:    "41049390e1e2b5d6543901daab6435f4042155fe",
+				size:      128,
+			},
 			expected: ipsecKey{
-				id:   1,
-				algo: "rfc4106(gcm(aes))",
+				spi:       4,
+				spiSuffix: true,
+				algo:      "rfc4106(gcm(aes))",
+				// this field will be randomly generated, `require.NotEqual` used for verification
+				random: "41049390e1e2b5d6543901daab6435f4042155fe",
+				size:   128,
+			},
+		},
+		{
+			have: ipsecKey{
+				spi:       16,
+				spiSuffix: true,
+				algo:      "rfc4106(gcm(aes))",
+				random:    "41049390e1e2b5d6543901daab6435f4042155fe",
+				size:      128,
+			},
+			expected: ipsecKey{
+				spi:       1,
+				spiSuffix: true,
+				algo:      "rfc4106(gcm(aes))",
 				// this field will be randomly generated, `require.NotEqual` used for verification
 				random: "41049390e1e2b5d6543901daab6435f4042155fe",
 				size:   128,
@@ -91,7 +161,8 @@ func Test_ipsecKey_rotate(t *testing.T) {
 		actual, err := tt.have.rotate()
 
 		require.NoError(t, err)
-		require.Equal(t, tt.expected.id, actual.id)
+		require.Equal(t, tt.expected.spi, actual.spi)
+		require.Equal(t, tt.expected.spiSuffix, actual.spiSuffix)
 		require.Equal(t, tt.expected.algo, actual.algo)
 		require.Equal(t, len(tt.expected.random), len(actual.random))
 		require.NotEqual(t, tt.expected.random, actual.random)
