1028c8345d2d20f720282dcc95b11390b56423b7
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2024-01-29 12:11:57 +0200

    Cipher IPsec key support added.
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- cilium-cli/encrypt/ipsec_rotate_key.go
+++ cilium-cli/encrypt/ipsec_rotate_key.go
@@ -18,11 +18,13 @@ import (
 )
 
 type ipsecKey struct {
-	spi       int
-	spiSuffix bool
-	algo      string
-	random    string
-	size      int
+	spi        int
+	spiSuffix  bool
+	algo       string // Purposefully ambiguous here because of modes like GCM
+	key        string
+	size       int
+	cipherMode string
+	cipherKey  string
 }
 
 // IPsecRotateKey rotates IPsec key.
@@ -60,15 +62,33 @@ func (k ipsecKey) String() string {
 	if k.spiSuffix {
 		spiSuffix = "+"
 	}
-	return fmt.Sprintf("%d%s %s %s %d", k.spi, spiSuffix, k.algo, k.random, k.size)
+	if k.cipherMode == "" {
+		return fmt.Sprintf("%d%s %s %s %d", k.spi, spiSuffix, k.algo, k.key, k.size)
+	}
+	return fmt.Sprintf("%d%s %s %s %s %s", k.spi, spiSuffix, k.algo, k.key, k.cipherMode, k.cipherKey)
 }
 
-var ipsecKeyRegex = regexp.MustCompile(`^([[:digit:]]+\+?)[[:space:]](\S+)[[:space:]]([[:alnum:]]+)[[:space:]]([[:digit:]]+)$`)
+var (
+	ipsecKeyRegex       = regexp.MustCompile(`^([[:digit:]]+\+?)[[:space:]](\S+)[[:space:]]([[:alnum:]]+)[[:space:]]([[:digit:]]+)$`)
+	cipherIPsecKeyRegex = regexp.MustCompile(`^([[:digit:]]+\+?)[[:space:]](\S+)[[:space:]]([[:alnum:]]+)[[:space:]](\S+)[[:space:]]([[:alnum:]]+)$`)
+	ipsecParsers        = map[*regexp.Regexp]func([]string) (ipsecKey, error){
+		ipsecKeyRegex:       keyFromSlice,
+		cipherIPsecKeyRegex: cipherKeyFromSlice,
+	}
+)
 
 func ipsecKeyFromString(s string) (ipsecKey, error) {
-	parts := ipsecKeyRegex.FindStringSubmatch(s)
+	for matcher, parser := range ipsecParsers {
+		if matcher.MatchString(s) {
+			return parser(matcher.FindStringSubmatch(s))
+		}
+	}
+	return ipsecKey{}, fmt.Errorf("IPsec key has unsupported format")
+}
+
+func keyFromSlice(parts []string) (ipsecKey, error) {
 	if len(parts) != 5 {
-		return ipsecKey{}, fmt.Errorf("IPsec key has unsupported format")
+		return ipsecKey{}, fmt.Errorf("IPsec key invalid [expected parts: 5, actual parts: %d]", len(parts))
 	}
 	spiSuffix := false
 	if strings.HasSuffix(parts[1], "+") {
@@ -87,8 +107,32 @@ func ipsecKeyFromString(s string) (ipsecKey, error) {
 		spi:       spi,
 		spiSuffix: spiSuffix,
 		algo:      parts[2],
-		random:    parts[3],
+		key:       parts[3],
 		size:      size,
+	}
+	return key, nil
+}
+
+func cipherKeyFromSlice(parts []string) (ipsecKey, error) {
+	if len(parts) != 6 {
+		return ipsecKey{}, fmt.Errorf("IPsec key invalid [expected parts: 6, actual parts: %d]", len(parts))
+	}
+	spiSuffix := false
+	if strings.HasSuffix(parts[1], "+") {
+		spiSuffix = true
+		parts[1] = strings.TrimSuffix(parts[1], "+")
+	}
+	spi, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return ipsecKey{}, fmt.Errorf("invalid cipher IPsec key SPI: %s", parts[1])
+	}
+	key := ipsecKey{
+		spi:        spi,
+		spiSuffix:  spiSuffix,
+		algo:       parts[2],
+		key:        parts[3],
+		cipherMode: parts[4],
+		cipherKey:  parts[5],
 	}
 	return key, nil
 }
@@ -96,26 +140,44 @@ func ipsecKeyFromString(s string) (ipsecKey, error) {
 const maxIPsecSPI = 16
 
 func (k ipsecKey) rotate() (ipsecKey, error) {
-	buf := make([]byte, len(k.random)/2)
-	if _, err := rand.Read(buf); err != nil {
-		return ipsecKey{}, fmt.Errorf("failed to generate random part: %s", err)
+	key, err := generateRandomHex(len(k.key))
+	if err != nil {
+		return ipsecKey{}, fmt.Errorf("failed to generate authentication key: %s", err)
 	}
-	random := &strings.Builder{}
-	random.Grow(len(buf))
-	for _, c := range buf {
-		random.WriteString(fmt.Sprintf("%02x", c))
+
+	cipherKey := ""
+	if k.cipherMode != "" {
+		cipherKey, err = generateRandomHex(len(k.cipherKey))
+		if err != nil {
+			return ipsecKey{}, fmt.Errorf("failed to generate symmetric encryption key: %s", err)
+		}
 	}
 
 	spi := k.spi + 1
 	if spi >= maxIPsecSPI {
 		spi = 1
 	}
-	key := ipsecKey{
-		spi:       spi,
-		spiSuffix: k.spiSuffix,
-		algo:      k.algo,
-		random:    random.String(),
-		size:      k.size,
+	newKey := ipsecKey{
+		spi:        spi,
+		spiSuffix:  k.spiSuffix,
+		algo:       k.algo,
+		key:        key,
+		size:       k.size,
+		cipherMode: k.cipherMode,
+		cipherKey:  cipherKey,
 	}
-	return key, nil
+	return newKey, nil
+}
+
+func generateRandomHex(size int) (string, error) {
+	buf := make([]byte, size/2)
+	if _, err := rand.Read(buf); err != nil {
+		return "", err
+	}
+	random := strings.Builder{}
+	random.Grow(len(buf))
+	for _, c := range buf {
+		random.WriteString(fmt.Sprintf("%02x", c))
+	}
+	return random.String(), nil
 }
