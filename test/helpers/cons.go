9caf3c26d8e291d2b25b5c85dd0e29f966f348f3
Tam Mach <tam.mach@cilium.io>
2024-01-30 15:15:42 +1100

    envoy: Temporarily allow envoy warning logs
    The log can be removed after v1.28.1 as mentioned in below upstream
    issue.
    
    Relates: https://github.com/envoyproxy/envoy/issues/31429
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- test/helpers/cons.go
+++ test/helpers/cons.go
@@ -268,7 +268,8 @@ const (
 	cantRecreateMasterKey      = "unable to re-create missing master key"                                // cf. https://github.com/cilium/cilium/issues/29738
 	cantUpdateCRDIdentity      = "Unable update CRD identity information with a reference for this node" // cf. https://github.com/cilium/cilium/issues/29739
 	cantDeleteFromPolicyMap    = "cilium_call_policy: delete: key does not exist"                        // cf. https://github.com/cilium/cilium/issues/29754
-
+	deprecatedEnvoyRuntimeKey  = "Usage of the deprecated runtime key overload.global_downstream_max_connections"
+	noConnLimitEnvoy           = "There is no configured limit to the number of allowed active downstream connections"
 	// HelmTemplate is the location of the Helm templates to install Cilium
 	HelmTemplate = "../install/kubernetes/cilium"
 
@@ -341,7 +342,7 @@ var badLogMessages = map[string][]string{
 		failedPeerSync, unableRestoreRouterIP, routerIPReallocated,
 		cantFindIdentityInCache, kubeApiserverConnLost1, kubeApiserverConnLost2, heartbeatTimedOut,
 		keyAllocFailedFoundMaster, cantRecreateMasterKey, cantUpdateCRDIdentity,
-		cantDeleteFromPolicyMap},
+		cantDeleteFromPolicyMap, deprecatedEnvoyRuntimeKey, noConnLimitEnvoy},
 }
 
 var ciliumCLICommands = map[string]string{
