e14cef7c76b2ba643590d8fe852cffa8fe2c3a63
chaunceyjiang <chaunceyjiang@gmail.com>
2024-01-31 16:25:13 +0800

    socket options - change from 'STATE_LISTENING' to 'STATE_PREBIND'
    [ upstream commit 92c2641c2346d730ad9193b87c573eb839404de1 ]
    
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- operator/pkg/model/translation/gateway-api/translator_fixture_test.go
+++ operator/pkg/model/translation/gateway-api/translator_fixture_test.go
@@ -3579,7 +3579,7 @@ func toSocketOptions() []*envoy_config_core_v3.SocketOption {
 			Value: &envoy_config_core_v3.SocketOption_IntValue{
 				IntValue: 1,
 			},
-			State: envoy_config_core_v3.SocketOption_STATE_LISTENING,
+			State: envoy_config_core_v3.SocketOption_STATE_PREBIND,
 		},
 		{
 			Description: "TCP keep-alive idle time (in seconds) (defaults to 10s)",
@@ -3588,7 +3588,7 @@ func toSocketOptions() []*envoy_config_core_v3.SocketOption {
 			Value: &envoy_config_core_v3.SocketOption_IntValue{
 				IntValue: 10,
 			},
-			State: envoy_config_core_v3.SocketOption_STATE_LISTENING,
+			State: envoy_config_core_v3.SocketOption_STATE_PREBIND,
 		},
 		{
 			Description: "TCP keep-alive probe intervals (in seconds) (defaults to 5s)",
@@ -3597,7 +3597,7 @@ func toSocketOptions() []*envoy_config_core_v3.SocketOption {
 			Value: &envoy_config_core_v3.SocketOption_IntValue{
 				IntValue: 5,
 			},
-			State: envoy_config_core_v3.SocketOption_STATE_LISTENING,
+			State: envoy_config_core_v3.SocketOption_STATE_PREBIND,
 		},
 		{
 			Description: "TCP keep-alive probe max failures.",
@@ -3606,7 +3606,7 @@ func toSocketOptions() []*envoy_config_core_v3.SocketOption {
 			Value: &envoy_config_core_v3.SocketOption_IntValue{
 				IntValue: 10,
 			},
-			State: envoy_config_core_v3.SocketOption_STATE_LISTENING,
+			State: envoy_config_core_v3.SocketOption_STATE_PREBIND,
 		},
 	}
 }
