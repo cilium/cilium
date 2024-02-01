587015177c7a935e8443ba3bd7f32360ce3ac01a
Fabio Falzoi <fabio.falzoi@isovalent.com>
2024-02-01 12:02:07 +0100

    Revert "policy: Fix Deny Precedence Bug"
    This reverts commit 80f7fef957fbc43b1c3e28d81dc9db59cc38ad7f.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/policy/selectorcache.go
+++ pkg/policy/selectorcache.go
@@ -1091,11 +1091,3 @@ func (sc *SelectorCache) RemoveIdentitiesFQDNSelectors(fqdnSels []api.FQDNSelect
 	sc.mutex.Unlock()
 	sc.releaseIdentityMappings(identitiesToRelease)
 }
-
-func (sc *SelectorCache) GetLabels(id identity.NumericIdentity) labels.LabelArray {
-	ident, ok := sc.idCache[id]
-	if !ok {
-		return labels.LabelArray{}
-	}
-	return ident.lbls
-}
