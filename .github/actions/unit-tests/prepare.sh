744f9554772bc8f604288d857f53d64eb019d60d
Maxim Mikityanskiy <maxim@isovalent.com>
2024-02-07 20:58:09 +0200

    images: Update LLVM to 17.0.6
    Binary builds of LLVM are no longer signed by upstream.
    
    Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

--- .github/actions/unit-tests/prepare.sh
+++ .github/actions/unit-tests/prepare.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-CLANG_VERSION=10.0.0
+CLANG_VERSION=17.0.6
 
 function setup_env() {
 case `uname -m` in
   'x86_64' )
-    CLANG_DIR="clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-18.04"
+    CLANG_DIR="clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-22.04"
     ;;
   'aarch64' )
     CLANG_DIR="clang+llvm-$CLANG_VERSION-aarch64-linux-gnu"
@@ -15,29 +15,15 @@ esac
 
 function install_clang() {
   CLANG_FILE="$CLANG_DIR.tar.xz"
-  CLANG_FILE_SIG="$CLANG_FILE.sig"
 
   CLANG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-$CLANG_VERSION/$CLANG_FILE"
   wget -nv $CLANG_URL
-  CLANG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-$CLANG_VERSION/$CLANG_FILE_SIG"
-  wget -nv $CLANG_URL
-
-  wget -nv https://releases.llvm.org/$CLANG_VERSION/hans-gpg-key.asc
-  gpg --import hans-gpg-key.asc
-
-  if gpg --verify $CLANG_FILE_SIG $CLANG_FILE
-  then
-    echo $CLANG_FILE verified successfully
-  else
-    echo ERROR: Failed to verify $CLANG_FILE
-    exit 1
-  fi
 
   sudo rm -rf /usr/local/clang
   sudo mkdir -p /usr/local
   sudo tar -C /usr/local -xJf $CLANG_FILE
   sudo ln -s /usr/local/$CLANG_DIR /usr/local/clang
-  rm $CLANG_FILE $CLANG_FILE_SIG hans-gpg-key.asc
+  rm $CLANG_FILE
 }
 
 setup_env
