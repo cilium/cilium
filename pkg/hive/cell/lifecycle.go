cilium/cilium

commit 07595dae12fcd52fbea8e339f8fca6d1a20fb2b2

hive: Add module ID to hive start/stop hook output
The move of the lifecycle into separate package and having two
implementations caused the hook output to be broken. This moves the
lifecycle to the cell package and removes the internalLifecycle.
With the lifecycle in the cell package we then add module IDs to the
hooks to provide where the hook was added from as there's already many
duplicate start/stop functions (e.g. from health reporter and job groups).

"go run ./daemon hive" before:

    • hive.(*internalLifecycle).Append.func2 (pkg/hive/hive.go:185)
    • *job.group.Stop

After:

    • *cell.reporterHooks.Stop (agent.datapath.bandwidth-manager)
    • *job.group.Stop (agent.datapath.l2-responder)

Fixes: 7f94c897a1 ("hive: move lifecycle types to separate package.")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hive/cell/lifecycle.go
@@ -1,0 +1,223 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cell
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/cilium/cilium/pkg/hive/internal"
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+// HookContext is a context passed to a lifecycle hook that is cancelled
+// in case of timeout. Hooks that perform long blocking operations directly
+// in the start or stop function (e.g. connecting to external services to
+// initialize) must abort any such operation if this context is cancelled.
+type HookContext context.Context
+
+// HookInterface mirrors the Hook interface from pkg/hive/lifecycle.go.
+// Because pkg/hive/cell depends on HookInterface then we need to have a
+// copy of it here.
+// Hive provides a "cell" version of this HookInterface interface that does
+// not depend on pkg/hive/lifecycle.go thus allowing the cell package to define
+// lifecycle hooks.
+type HookInterface interface {
+	Start(HookContext) error
+	Stop(HookContext) error
+}
+
+// Hook is a pair of start and stop callbacks. Both are optional.
+// They're paired up to make sure that on failed start all corresponding
+// stop hooks are executed.
+type Hook struct {
+	OnStart func(HookContext) error
+	OnStop  func(HookContext) error
+}
+
+func (h Hook) Start(ctx HookContext) error {
+	if h.OnStart == nil {
+		return nil
+	}
+	return h.OnStart(ctx)
+}
+
+func (h Hook) Stop(ctx HookContext) error {
+	if h.OnStop == nil {
+		return nil
+	}
+	return h.OnStop(ctx)
+}
+
+// Lifecycle enables cells to register start and stop hooks, either
+// from a constructor or an invoke function.
+type Lifecycle interface {
+	Append(HookInterface)
+
+	Start(context.Context) error
+	Stop(context.Context) error
+	PrintHooks()
+}
+
+// DefaultLifecycle lifecycle implements a simple lifecycle management that conforms
+// to Lifecycle. It is exported for use in applications that have nested lifecycles
+// (e.g. operator).
+type DefaultLifecycle struct {
+	mu         lock.Mutex
+	hooks      []augmentedHook
+	numStarted int
+}
+
+type augmentedHook struct {
+	HookInterface
+	moduleID FullModuleID
+}
+
+func (lc *DefaultLifecycle) Append(hook HookInterface) {
+	lc.mu.Lock()
+	defer lc.mu.Unlock()
+
+	lc.hooks = append(lc.hooks, augmentedHook{hook, nil})
+}
+
+func (lc *DefaultLifecycle) Start(ctx context.Context) error {
+	lc.mu.Lock()
+	defer lc.mu.Unlock()
+
+	// Wrap the context to make sure it gets cancelled after
+	// start hooks have completed in order to discourage using
+	// the context for unintended purposes.
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	for _, hook := range lc.hooks {
+		fnName, exists := getHookFuncName(hook, true)
+
+		if !exists {
+			// Count as started as there might be a stop hook.
+			lc.numStarted++
+			continue
+		}
+
+		l := log.WithField("function", fnName)
+		l.Debug("Executing start hook")
+		t0 := time.Now()
+		if err := hook.Start(ctx); err != nil {
+			l.WithError(err).Error("Start hook failed")
+			return err
+		}
+		d := time.Since(t0)
+		l.WithField("duration", d).Info("Start hook executed")
+		lc.numStarted++
+	}
+	return nil
+}
+
+func (lc *DefaultLifecycle) Stop(ctx context.Context) error {
+	lc.mu.Lock()
+	defer lc.mu.Unlock()
+
+	// Wrap the context to make sure it gets cancelled after
+	// stop hooks have completed in order to discourage using
+	// the context for unintended purposes.
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var errs error
+	for ; lc.numStarted > 0; lc.numStarted-- {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+		hook := lc.hooks[lc.numStarted-1]
+
+		fnName, exists := getHookFuncName(hook, false)
+		if !exists {
+			continue
+		}
+		l := log.WithField("function", fnName)
+		l.Debug("Executing stop hook")
+		t0 := time.Now()
+		if err := hook.Stop(ctx); err != nil {
+			l.WithError(err).Error("Stop hook failed")
+			errs = errors.Join(errs, err)
+		} else {
+			d := time.Since(t0)
+			l.WithField("duration", d).Info("Stop hook executed")
+		}
+	}
+	return errs
+}
+
+func (lc *DefaultLifecycle) PrintHooks() {
+	lc.mu.Lock()
+	defer lc.mu.Unlock()
+
+	fmt.Printf("Start hooks:\n\n")
+	for _, hook := range lc.hooks {
+		fnName, exists := getHookFuncName(hook.HookInterface, true)
+		if !exists {
+			continue
+		}
+		fmt.Printf("  • %s (%s)\n", fnName, hook.moduleID)
+	}
+
+	fmt.Printf("\nStop hooks:\n\n")
+	for i := len(lc.hooks) - 1; i >= 0; i-- {
+		hook := lc.hooks[i]
+		fnName, exists := getHookFuncName(hook.HookInterface, false)
+		if !exists {
+			continue
+		}
+		fmt.Printf("  • %s (%s)\n", fnName, hook.moduleID)
+	}
+}
+
+type augmentedLifecycle struct {
+	*DefaultLifecycle
+	moduleID FullModuleID
+}
+
+func (lc augmentedLifecycle) Append(hook HookInterface) {
+	lc.mu.Lock()
+	defer lc.mu.Unlock()
+
+	lc.hooks = append(lc.hooks, augmentedHook{hook, lc.moduleID})
+}
+
+func getHookFuncName(hook HookInterface, start bool) (name string, hasHook bool) {
+	// Ok, we need to get a bit fancy here as runtime.FuncForPC does
+	// not return what we want: we get "hive.Hook.Stop()" when we want
+	// "*foo.Stop(). We do know the concrete type, and we do know
+	// the method name, so we check here whether we're dealing with
+	// "Hook" the struct, or an object implementing HookInterface.
+	//
+	// We could use reflection + FuncForPC to get around this, but it
+	// still wouldn't work for generic types (file would be "<autogenerated>")
+	// and the type params would be missing, so instead we'll just use the
+	// type name + method name.
+	switch hook := hook.(type) {
+	case Hook:
+		if start {
+			if hook.OnStart == nil {
+				return "", false
+			}
+			return internal.FuncNameAndLocation(hook.OnStart), true
+		}
+		if hook.OnStop == nil {
+			return "", false
+		}
+		return internal.FuncNameAndLocation(hook.OnStop), true
+
+	default:
+		if start {
+			return internal.PrettyType(hook) + ".Start", true
+		}
+		return internal.PrettyType(hook) + ".Stop", true
+
+	}
+}
+
+var _ Lifecycle = &DefaultLifecycle{}

operator/cmd/lifecycle.go
@@ -12,7 +12,7 @@ import (
 // operator instance is elected leader. It implements hive.Lifecycle allowing cells
 // to use it.
 type LeaderLifecycle struct {
-	hive.DefaultLifecycle
+	cell.DefaultLifecycle
 }
 
 func WithLeaderLifecycle(cells ...cell.Cell) cell.Cell {

pkg/hive/cell/lifecycle/lifecycle.go
@@ -1,29 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-package lifecycle
-
-import "context"
-
-// HookContext is a context passed to a lifecycle hook that is cancelled
-// in case of timeout. Hooks that perform long blocking operations directly
-// in the start or stop function (e.g. connecting to external services to
-// initialize) must abort any such operation if this context is cancelled.
-type HookContext context.Context
-
-// HookInterface mirrors the Hook interface from pkg/hive/lifecycle.go.
-// Because pkg/hive/cell depends on HookInterface then we need to have a
-// copy of it here.
-// Hive provides a "cell" version of this HookInterface interface that does
-// not depend on pkg/hive/lifecycle.go thus allowing the cell package to define
-// lifecycle hooks.
-type HookInterface interface {
-	Start(HookContext) error
-	Stop(HookContext) error
-}
-
-// Lifecycle enables cells to register start and stop hooks, either
-// from a constructor or an invoke function.
-type Lifecycle interface {
-	Append(HookInterface)
-}

pkg/hive/hivetest/lifecycle.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
 // lifecycle implements [hive.Lifecycle] for testing purposes.
@@ -15,7 +16,7 @@ type lifecycle struct {
 	tb testing.TB
 }
 
-var _ (hive.Lifecycle) = (*lifecycle)(nil)
+var _ (cell.Lifecycle) = (*lifecycle)(nil)
 
 // Lifecycle returns a [hive.Lifecycle] which executes start hooks immediately
 // and queues stop hooks for the end of the test.
@@ -43,4 +44,19 @@ func (lc *lifecycle) Append(hook hive.HookInterface) {
 			lc.tb.Fatal("Execute stop hook:", err)
 		}
 	})
+}
+
+// PrintHooks implements cell.Lifecycle.
+func (*lifecycle) PrintHooks() {
+	panic("unimplemented")
+}
+
+// Start implements cell.Lifecycle.
+func (*lifecycle) Start(context.Context) error {
+	panic("unimplemented")
+}
+
+// Stop implements cell.Lifecycle.
+func (*lifecycle) Stop(context.Context) error {
+	panic("unimplemented")
 }

pkg/hive/lifecycle.go
@@ -3,205 +3,11 @@
 
 package hive
 
-import (
-	"context"
-	"errors"
-	"fmt"
-	"time"
+import "github.com/cilium/cilium/pkg/hive/cell"
 
-	"github.com/cilium/cilium/pkg/hive/cell/lifecycle"
-	"github.com/cilium/cilium/pkg/hive/internal"
-	"github.com/cilium/cilium/pkg/lock"
+type (
+	Hook          = cell.Hook
+	HookInterface = cell.HookInterface
+	HookContext   = cell.HookContext
+	Lifecycle     = cell.Lifecycle
 )
-
-// HookContext is a context passed to a lifecycle hook that is cancelled
-// in case of timeout. Hooks that perform long blocking operations directly
-// in the start or stop function (e.g. connecting to external services to
-// initialize) must abort any such operation if this context is cancelled.
-type HookContext lifecycle.HookContext
-
-// Hook is a pair of start and stop callbacks. Both are optional.
-// They're paired up to make sure that on failed start all corresponding
-// stop hooks are executed.
-type Hook struct {
-	OnStart func(HookContext) error
-	OnStop  func(HookContext) error
-}
-
-func (h Hook) Start(ctx HookContext) error {
-	if h.OnStart == nil {
-		return nil
-	}
-	return h.OnStart(ctx)
-}
-
-func (h Hook) Stop(ctx HookContext) error {
-	if h.OnStop == nil {
-		return nil
-	}
-	return h.OnStop(ctx)
-}
-
-type HookInterface interface {
-	// Start hook is called when the hive is started.
-	// Returning a non-nil error causes the start to abort and
-	// the stop hooks for already started cells to be called.
-	//
-	// The context is valid only for the duration of the start
-	// and is used to allow aborting of start hook on timeout.
-	Start(HookContext) error
-
-	// Stop hook is called when the hive is stopped or start aborted.
-	// Returning a non-nil error does not abort stopping. The error
-	// is recorded and rest of the stop hooks are executed.
-	Stop(HookContext) error
-}
-
-// Lifecycle enables cells to register start and stop hooks, either
-// from a constructor or an invoke function.
-type Lifecycle interface {
-	Append(HookInterface)
-}
-
-// DefaultLifecycle lifecycle implements a simple lifecycle management that conforms
-// to Lifecycle. It is exported for use in applications that have nested lifecycles
-// (e.g. operator).
-type DefaultLifecycle struct {
-	mu         lock.Mutex
-	hooks      []HookInterface
-	numStarted int
-}
-
-func (lc *DefaultLifecycle) Append(hook HookInterface) {
-	lc.mu.Lock()
-	defer lc.mu.Unlock()
-
-	lc.hooks = append(lc.hooks, hook)
-}
-
-func (lc *DefaultLifecycle) Start(ctx context.Context) error {
-	lc.mu.Lock()
-	defer lc.mu.Unlock()
-
-	// Wrap the context to make sure it gets cancelled after
-	// start hooks have completed in order to discourage using
-	// the context for unintended purposes.
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	for _, hook := range lc.hooks {
-		fnName, exists := getHookFuncName(hook, true)
-
-		if !exists {
-			// Count as started as there might be a stop hook.
-			lc.numStarted++
-			continue
-		}
-
-		l := log.WithField("function", fnName)
-		l.Debug("Executing start hook")
-		t0 := time.Now()
-		if err := hook.Start(ctx); err != nil {
-			l.WithError(err).Error("Start hook failed")
-			return err
-		}
-		d := time.Since(t0)
-		l.WithField("duration", d).Info("Start hook executed")
-		lc.numStarted++
-	}
-	return nil
-}
-
-func (lc *DefaultLifecycle) Stop(ctx context.Context) error {
-	lc.mu.Lock()
-	defer lc.mu.Unlock()
-
-	// Wrap the context to make sure it gets cancelled after
-	// stop hooks have completed in order to discourage using
-	// the context for unintended purposes.
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	var errs error
-	for ; lc.numStarted > 0; lc.numStarted-- {
-		if ctx.Err() != nil {
-			return ctx.Err()
-		}
-		hook := lc.hooks[lc.numStarted-1]
-
-		fnName, exists := getHookFuncName(hook, false)
-		if !exists {
-			continue
-		}
-		l := log.WithField("function", fnName)
-		l.Debug("Executing stop hook")
-		t0 := time.Now()
-		if err := hook.Stop(ctx); err != nil {
-			l.WithError(err).Error("Stop hook failed")
-			errs = errors.Join(errs, err)
-		} else {
-			d := time.Since(t0)
-			l.WithField("duration", d).Info("Stop hook executed")
-		}
-	}
-	return errs
-}
-
-func (lc *DefaultLifecycle) PrintHooks() {
-	lc.mu.Lock()
-	defer lc.mu.Unlock()
-
-	fmt.Printf("Start hooks:\n\n")
-	for _, hook := range lc.hooks {
-		fnName, exists := getHookFuncName(hook, true)
-		if !exists {
-			continue
-		}
-		fmt.Printf("  • %s\n", fnName)
-	}
-
-	fmt.Printf("\nStop hooks:\n\n")
-	for i := len(lc.hooks) - 1; i >= 0; i-- {
-		hook := lc.hooks[i]
-		fnName, exists := getHookFuncName(hook, false)
-		if !exists {
-			continue
-		}
-		fmt.Printf("  • %s\n", fnName)
-	}
-}
-
-func getHookFuncName(hook HookInterface, start bool) (name string, hasHook bool) {
-	// Ok, we need to get a bit fancy here as runtime.FuncForPC does
-	// not return what we want: we get "hive.Hook.Stop()" when we want
-	// "*foo.Stop(). We do know the concrete type, and we do know
-	// the method name, so we check here whether we're dealing with
-	// "Hook" the struct, or an object implementing HookInterface.
-	//
-	// We could use reflection + FuncForPC to get around this, but it
-	// still wouldn't work for generic types (file would be "<autogenerated>")
-	// and the type params would be missing, so instead we'll just use the
-	// type name + method name.
-	switch hook := hook.(type) {
-	case Hook:
-		if start {
-			if hook.OnStart == nil {
-				return "", false
-			}
-			return internal.FuncNameAndLocation(hook.OnStart), true
-		}
-		if hook.OnStop == nil {
-			return "", false
-		}
-		return internal.FuncNameAndLocation(hook.OnStop), true
-
-	default:
-		if start {
-			return internal.PrettyType(hook) + ".Start", true
-		}
-		return internal.PrettyType(hook) + ".Stop", true
-
-	}
-}
-
-var _ Lifecycle = &DefaultLifecycle{}

pkg/datapath/l2responder/l2responder_test.go
@@ -59,7 +59,7 @@ func newFixture() *fixture {
 
 	return &fixture{
 		reconciler: NewL2ResponderReconciler(params{
-			Lifecycle:           &hive.DefaultLifecycle{},
+			Lifecycle:           &cell.DefaultLifecycle{},
 			Logger:              logrus.New(),
 			L2AnnouncementTable: tbl,
 			StateDB:             db,

pkg/gops/cell.go
@@ -36,11 +36,11 @@ func (def GopsConfig) Flags(flags *pflag.FlagSet) {
 	flags.Uint16(option.GopsPort, def.GopsPort, "Port for gops server to listen on")
 }
 
-func registerGopsHooks(lc hive.Lifecycle, log logrus.FieldLogger, cfg GopsConfig) {
+func registerGopsHooks(lc cell.Lifecycle, log logrus.FieldLogger, cfg GopsConfig) {
 	addr := fmt.Sprintf("127.0.0.1:%d", cfg.GopsPort)
 	addrField := logrus.Fields{"address": addr, logfields.LogSubsys: "gops"}
 	log = log.WithFields(addrField)
-	lc.Append(hive.Hook{
+	lc.Append(cell.Hook{
 		OnStart: func(hive.HookContext) error {
 			log.Info("Started gops server")
 			return gopsAgent.Listen(gopsAgent.Options{

pkg/hive/cell/lifecycle_test.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package hive_test
+package cell_test
 
 import (
 	"context"
@@ -10,7 +10,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
 var (
@@ -18,48 +18,48 @@ var (
 
 	errLifecycle = errors.New("nope")
 
-	goodHook = hive.Hook{
-		OnStart: func(hive.HookContext) error {
+	goodHook = cell.Hook{
+		OnStart: func(cell.HookContext) error {
 			started++
 			return nil
 		},
-		OnStop: func(hive.HookContext) error {
+		OnStop: func(cell.HookContext) error {
 			stopped++
 			return nil
 		},
 	}
 
-	badStartHook = hive.Hook{
-		OnStart: func(hive.HookContext) error {
+	badStartHook = cell.Hook{
+		OnStart: func(cell.HookContext) error {
 			return errLifecycle
 		},
 	}
 
-	badStopHook = hive.Hook{
-		OnStart: func(hive.HookContext) error {
+	badStopHook = cell.Hook{
+		OnStart: func(cell.HookContext) error {
 			started++
 			return nil
 		},
-		OnStop: func(hive.HookContext) error {
+		OnStop: func(cell.HookContext) error {
 			return errLifecycle
 		},
 	}
 
-	nilHook = hive.Hook{nil, nil}
+	nilHook = cell.Hook{nil, nil}
 )
 
 func TestLifecycle(t *testing.T) {
-	var lc hive.DefaultLifecycle
+	var lc cell.DefaultLifecycle
 
 	// Test without any hooks
-	lc = hive.DefaultLifecycle{}
+	lc = cell.DefaultLifecycle{}
 	err := lc.Start(context.TODO())
 	assert.NoError(t, err, "expected Start to succeed")
 	err = lc.Stop(context.TODO())
 	assert.NoError(t, err, "expected Stop to succeed")
 
 	// Test with 3 good, 1 nil hook, all successful.
-	lc = hive.DefaultLifecycle{}
+	lc = cell.DefaultLifecycle{}
 	lc.Append(goodHook)
 	lc.Append(goodHook)
 	lc.Append(goodHook)
@@ -77,7 +77,7 @@ func TestLifecycle(t *testing.T) {
 
 	// Test with 2 good, 1 bad start. Should see
 	// the good ones stopped.
-	lc = hive.DefaultLifecycle{}
+	lc = cell.DefaultLifecycle{}
 	lc.Append(goodHook)
 	lc.Append(goodHook)
 	lc.Append(badStartHook)
@@ -90,7 +90,7 @@ func TestLifecycle(t *testing.T) {
 	stopped = 0
 
 	// Test with 2 good, 1 bad stop. Stop should return the error.
-	lc = hive.DefaultLifecycle{}
+	lc = cell.DefaultLifecycle{}
 	lc.Append(goodHook)
 	lc.Append(goodHook)
 	lc.Append(badStopHook)
@@ -107,9 +107,9 @@ func TestLifecycle(t *testing.T) {
 	stopped = 0
 
 	// Test that one can have hook with a stop and no start.
-	lc = hive.DefaultLifecycle{}
-	lc.Append(hive.Hook{
-		OnStop: func(hive.HookContext) error { stopped++; return nil },
+	lc = cell.DefaultLifecycle{}
+	lc.Append(cell.Hook{
+		OnStop: func(cell.HookContext) error { stopped++; return nil },
 	})
 	err = lc.Start(context.TODO())
 	assert.NoError(t, err, "expected Start to succeed")
@@ -126,9 +126,9 @@ func TestLifecycleCancel(t *testing.T) {
 	cancel()
 
 	// Test cancellation in start hook
-	lc := hive.DefaultLifecycle{}
-	lc.Append(hive.Hook{
-		OnStart: func(ctx hive.HookContext) error {
+	lc := cell.DefaultLifecycle{}
+	lc.Append(cell.Hook{
+		OnStart: func(ctx cell.HookContext) error {
 			<-ctx.Done()
 			return ctx.Err()
 		},
@@ -140,9 +140,9 @@ func TestLifecycleCancel(t *testing.T) {
 	expectedErr := errors.New("stop cancelled")
 	ctx, cancel = context.WithCancel(context.Background())
 	inStop := make(chan struct{})
-	lc = hive.DefaultLifecycle{}
-	lc.Append(hive.Hook{
-		OnStop: func(ctx hive.HookContext) error {
+	lc = cell.DefaultLifecycle{}
+	lc.Append(cell.Hook{
+		OnStop: func(ctx cell.HookContext) error {
 			close(inStop)
 			<-ctx.Done()
 			assert.ErrorIs(t, ctx.Err(), context.Canceled)
@@ -150,7 +150,7 @@ func TestLifecycleCancel(t *testing.T) {
 		},
 	})
 
-	// Only cancel once we're inside stop as hive.Stop() short-circuits
+	// Only cancel once we're inside stop as cell.Stop() short-circuits
 	// when context is cancelled.
 	go func() {
 		<-inStop

commit 47d3ee73423bc2e2e2a72a824a0404ecc1e651c3

hive: Fix start hook log output
Commit 07595dae12fc added module ID to the start/stop hook output by carrying
the module ID in the hook. Unfortunately I forgot to fix the getHooKFuncName to
actually handle this wrapped handle which caused the log output to always show
"augmentedHook.Start" as the function name. Fix this by looking through the
wrapper.

Fixes: 07595dae12fc ("hive: Add module ID to hive start/stop hook output")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hive/cell/lifecycle.go
@@ -199,6 +199,9 @@ func getHookFuncName(hook HookInterface, start bool) (name string, hasHook bool)
 	// and the type params would be missing, so instead we'll just use the
 	// type name + method name.
 	switch hook := hook.(type) {
+	case augmentedHook:
+		name, hasHook = getHookFuncName(hook.HookInterface, start)
+		return
 	case Hook:
 		if start {
 			if hook.OnStart == nil {