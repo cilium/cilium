4a7a3531dba120b4ab790e41ac87be7ada02ad9e
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2024-02-20 08:53:48 +0200

    Use encryption models from Cilium dependency.
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- cilium-cli/encrypt/model.go
+++ cilium-cli/encrypt/model.go
@@ -3,8 +3,6 @@
 
 package encrypt
 
-import "github.com/go-openapi/strfmt"
-
 type clusterStatus struct {
 	TotalNodeCount          int              `json:"total-node-count,omitempty"`
 	EncDisabledNodeCount    int              `json:"enc-disabled-node-count,omitempty"`
@@ -15,46 +13,4 @@ type clusterStatus struct {
 	IPsecErrCount           int64            `json:"ipsec-err-count,omitempty"`
 	XfrmErrors              map[string]int64 `json:"xfrm-errors,omitempty"`
 	XfrmErrorNodeCount      map[string]int64 `json:"xfrm-error-node-count,omitempty"`
-}
-
-// EncryptionStatus Status of transparent encryption
-type EncryptionStatus struct {
-	Ipsec     *IPsecStatus     `json:"ipsec,omitempty"`
-	Mode      string           `json:"mode,omitempty"`
-	Msg       string           `json:"msg,omitempty"`
-	Wireguard *WireguardStatus `json:"wireguard,omitempty"`
-}
-
-// IPsecStatus Status of the IPsec agent
-type IPsecStatus struct {
-	DecryptInterfaces []string         `json:"decrypt-interfaces"`
-	ErrorCount        int64            `json:"error-count,omitempty"`
-	KeysInUse         int64            `json:"keys-in-use,omitempty"`
-	MaxSeqNumber      string           `json:"max-seq-number,omitempty"`
-	XfrmErrors        map[string]int64 `json:"xfrm-errors,omitempty"`
-}
-
-// WireguardStatus Status of the WireGuard agent
-type WireguardStatus struct {
-	Interfaces     []*WireguardInterface `json:"interfaces"`
-	NodeEncryption string                `json:"node-encryption,omitempty"`
-}
-
-// WireguardInterface Status of a WireGuard interface
-type WireguardInterface struct {
-	ListenPort int64            `json:"listen-port,omitempty"`
-	Name       string           `json:"name,omitempty"`
-	PeerCount  int64            `json:"peer-count,omitempty"`
-	Peers      []*WireguardPeer `json:"peers"`
-	PublicKey  string           `json:"public-key,omitempty"`
-}
-
-// WireguardPeer Status of a WireGuard peer
-type WireguardPeer struct {
-	AllowedIps        []string        `json:"allowed-ips"`
-	Endpoint          string          `json:"endpoint,omitempty"`
-	LastHandshakeTime strfmt.DateTime `json:"last-handshake-time,omitempty"`
-	PublicKey         string          `json:"public-key,omitempty"`
-	TransferRx        int64           `json:"transfer-rx,omitempty"`
-	TransferTx        int64           `json:"transfer-tx,omitempty"`
 }
