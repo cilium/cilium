755a6b93a1c0a6af2cd43188c8625e7c5fd1a020
Lorenz Bauer <lmb@isovalent.com>
2024-02-20 14:28:12 +0000

    datapath/linux: remove kernel and clang version check
    We check for a minimum kernel and clang version on start up. The
    kernel version check is dubious since the minimum version we need
    is 4.19. It also doesn't handle the fact that RHEL 8 kernels report
    a version of 4.18 even though being functionally close to a 5.4
    kernel. The clang version check is similarly dubious, since 3.8
    hasn't worked in a long time.
    
    Remove the check outright.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- pkg/datapath/linux/requirements.go
+++ pkg/datapath/linux/requirements.go
@@ -5,14 +5,9 @@ package linux
 
 import (
 	"errors"
-	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"regexp"
-	"strings"
 
-	"github.com/blang/semver/v4"
 	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
 
@@ -20,60 +15,12 @@ import (
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
-	"github.com/cilium/cilium/pkg/version"
-	"github.com/cilium/cilium/pkg/versioncheck"
 )
 
-const (
-	minKernelVer = "4.8.0"
-	minClangVer  = "3.8.0"
-	recKernelVer = "4.9.0"
-	recClangVer  = "3.9.0"
-)
-
-var (
-	isMinKernelVer = versioncheck.MustCompile(">=" + minKernelVer)
-	isMinClangVer  = versioncheck.MustCompile(">=" + minClangVer)
-
-	isRecKernelVer = versioncheck.MustCompile(">=" + recKernelVer)
-	isRecClangVer  = versioncheck.MustCompile(">=" + recClangVer)
-)
-
-func getClangVersion(filePath string) (semver.Version, error) {
-	verOut, err := exec.Command(filePath, "--version").CombinedOutput()
-	if err != nil {
-		log.WithError(err).Fatal("clang version: NOT OK")
-	}
-	res := regexp.MustCompile(`(clang version )([^\s]*)`).FindStringSubmatch(string(verOut))
-	if len(res) != 3 {
-		log.Fatalf("clang version: NOT OK: unable to get clang's version "+
-			"from: %q", string(verOut))
-	}
-	// at this point res is []string{"clang", "version", "maj.min.patch"}
-	verStrs := strings.Split(res[2], ".")
-	if len(verStrs) < 3 {
-		return semver.Version{}, fmt.Errorf("unable to get clang version from %q", string(verOut))
-	}
-	v := strings.Join(verStrs[:3], ".")
-	// Handle Ubuntu versioning by removing the dash and everything after.
-	// F. ex. `4.0.0-1ubuntu1~16 -> 4.0.0` and `3.8.0-2ubuntu4 -> 3.8.0`.
-	v = strings.Split(v, "-")[0]
-	return versioncheck.Version(v)
-}
-
-// CheckMinRequirements checks that minimum kernel requirements are met for
+// CheckRequirements checks that minimum kernel requirements are met for
 // configuring the BPF datapath. If not, fatally exits.
-func CheckMinRequirements() {
-	kernelVersion, err := version.GetKernelVersion()
-	if err != nil {
-		log.WithError(err).Fatal("kernel version: NOT OK")
-	}
-	if !isMinKernelVer(kernelVersion) {
-		log.Fatalf("kernel version: NOT OK: minimal supported kernel "+
-			"version is %s; kernel version that is running is: %s", minKernelVer, kernelVersion)
-	}
-
-	_, err = netlink.RuleList(netlink.FAMILY_V4)
+func CheckRequirements() {
+	_, err := netlink.RuleList(netlink.FAMILY_V4)
 	if errors.Is(err, unix.EAFNOSUPPORT) {
 		log.WithError(err).Error("Policy routing:NOT OK. " +
 			"Please enable kernel configuration item CONFIG_IP_MULTIPLE_TABLES")
@@ -83,26 +30,6 @@ func CheckMinRequirements() {
 		if _, err := os.Stat("/proc/net/if_inet6"); os.IsNotExist(err) {
 			log.Fatalf("kernel: ipv6 is enabled in agent but ipv6 is either disabled or not compiled in the kernel")
 		}
-	}
-
-	if filePath, err := exec.LookPath("clang"); err != nil {
-		log.WithError(err).Fatal("clang: NOT OK")
-	} else {
-		clangVersion, err := getClangVersion(filePath)
-		if err != nil {
-			log.WithError(err).Fatal("clang: NOT OK")
-		}
-		if !isMinClangVer(clangVersion) {
-			log.Fatalf("clang version: NOT OK: minimal supported clang "+
-				"version is %s; clang version that is running is: %s", minClangVer, clangVersion)
-		}
-		//clang >= 3.9 / kernel < 4.9 - does not work
-		if isRecClangVer(clangVersion) && !isRecKernelVer(kernelVersion) {
-			log.Fatalf("clang (%s) and kernel (%s) version: NOT OK: please upgrade "+
-				"your kernel version to at least %s",
-				clangVersion, kernelVersion, recKernelVer)
-		}
-		log.Infof("clang (%s) and kernel (%s) versions: OK!", clangVersion, kernelVersion)
 	}
 
 	globalsDir := option.Config.GetGlobalsDir()
