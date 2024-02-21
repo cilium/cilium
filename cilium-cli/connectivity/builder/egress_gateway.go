7fe033869021c9dceaa9af671c34c7b071adb348
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2024-02-21 12:34:27 +0200

    Connectivity test builder component implemented.
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

---
+++ cilium-cli/connectivity/builder/egress_gateway.go
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package builder
+
+import (
+	"github.com/cilium/cilium-cli/connectivity/check"
+	"github.com/cilium/cilium-cli/connectivity/tests"
+	"github.com/cilium/cilium-cli/utils/features"
+)
+
+type egressGateway struct{}
+
+func (t egressGateway) build(ct *check.ConnectivityTest, _ map[string]string) {
+	newTest("egress-gateway", ct).
+		WithCondition(func() bool { return ct.Params().IncludeUnsafeTests }).
+		WithCiliumEgressGatewayPolicy(check.CiliumEgressGatewayPolicyParams{
+			Name:            "cegp-sample-client",
+			PodSelectorKind: "client",
+		}).
+		WithCiliumEgressGatewayPolicy(check.CiliumEgressGatewayPolicyParams{
+			Name:            "cegp-sample-echo",
+			PodSelectorKind: "echo",
+		}).
+		WithIPRoutesFromOutsideToPodCIDRs().
+		WithFeatureRequirements(
+			features.RequireEnabled(features.EgressGateway),
+			features.RequireEnabled(features.NodeWithoutCilium),
+		).
+		WithScenarios(tests.EgressGateway())
+}
