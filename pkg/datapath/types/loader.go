c47572857c27d6650096b136c7bbe1d67aa0a0e1
Fabio Falzoi <fabio.falzoi@isovalent.com>
2024-02-27 19:49:55 +0100

    iptables: Wait for reconciliation for no track pods and proxy updates
    The current manager implementation relies on the reconciler to rewrite
    the rules in case of no track pods or proxy updates.  Unlike the
    previous implementation, this is carried out asynchronously.
    
    Since all the tests in the CI expects these operations to be executed
    synchronously, a notification channel is returned and closed when the
    reconciliation has been completed successfully.
    
    Despite this should not be strictly needed, waiting for the iptables
    rules to be updated avoids a lot of flakes in the current e2e tests
    implementation.
    
    Being needed just for backward compatibility with the tests suite, it
    may be removed in the future when the tests are rewritten to be less
    unforgiven.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/datapath/types/loader.go
+++ pkg/datapath/types/loader.go
@@ -57,7 +57,7 @@ type Proxy interface {
 type IptablesManager interface {
 	// InstallProxyRules creates the necessary datapath config (e.g., iptables
 	// rules for redirecting host proxy traffic on a specific ProxyPort)
-	InstallProxyRules(proxyPort uint16, isLocalOnly bool, name string)
+	InstallProxyRules(proxyPort uint16, localOnly bool, name string)
 
 	// SupportsOriginalSourceAddr tells if the datapath supports
 	// use of original source addresses in proxy upstream
