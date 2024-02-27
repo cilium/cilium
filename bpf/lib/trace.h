1651948ea782df7d7905e0b03473f1cbe2e84887
ldelossa <louis.delos@gmail.com>
2024-02-27 15:18:46 -0500

    bpf: add trace notification for overlay encryption
    Add a trace notification when we are redirecting a packet back into the
    stack for XFRM encryption.
    
    Trace example:
    -> stack flow 0xc218244b , identity unknown->unknown state encrypt-overlay ifindex 0 orig-ip 0.0.0.0: 172.18.0.3:58167 -> 172.18.0.2:8472 udp
    
    Signed-off-by: ldelossa <louis.delos@isovalent.com>

--- bpf/lib/trace.h
+++ bpf/lib/trace.h
@@ -54,6 +54,7 @@ enum trace_reason {
 	TRACE_REASON_UNKNOWN,
 	TRACE_REASON_SRV6_ENCAP,
 	TRACE_REASON_SRV6_DECAP,
+	TRACE_REASON_ENCRYPT_OVERLAY,
 	/* Note: TRACE_REASON_ENCRYPTED is used as a mask. Beware if you add
 	 * new values below it, they would match with that mask.
 	 */
