cilium/cilium

commit d735c5017bd19e78f380ffe0431bd7de1cae5551

introduce 'properties' for endpoints
With the ability to store any random metadata in the endpoints we will
be able to make use of it for unit tests which will also help to further
decrease the dependency on option.Config in the pkg/endpoint.

Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/endpoint.go
@@ -69,6 +69,24 @@ const (
 	resolveLabels   = "resolve-labels"
 )
 
+const (
+	// PropertyFakeEndpoint marks the endpoint as being "fake". By "fake" it
+	// means that it doesn't have any datapath bpf programs regenerated.
+	PropertyFakeEndpoint = "property-fake-endpoint"
+
+	// PropertyWithouteBPFDatapath marks the endpoint that doesn't contain a
+	// eBPF datapath program.
+	PropertyWithouteBPFDatapath = "property-without-bpf-endpoint"
+
+	// PropertySkipBPFPolicy will mark the endpoint to skip ebpf
+	// policy regeneration.
+	PropertySkipBPFPolicy = "property-skip-bpf-policy"
+
+	// PropertySkipBPFRegeneration will mark the endpoint to skip ebpf
+	// regeneration.
+	PropertySkipBPFRegeneration = "property-skip-bpf-regeneration"
+)
+
 var (
 	EndpointMutableOptionLibrary = option.GetEndpointMutableOptionLibrary()
 
@@ -385,6 +403,9 @@ type Endpoint struct {
 	// mutable! must hold the endpoint lock to read
 	ciliumEndpointUID k8sTypes.UID
 
+	// properties is used to store some internal properties about this Endpoint.
+	properties map[string]interface{}
+
 	// Root scope for all of this endpoints reporters.
 	reporterScope       cell.Scope
 	closeHealthReporter func()
@@ -514,6 +535,7 @@ func (e *Endpoint) waitForProxyCompletions(proxyWaitGroup *completion.WaitGroup)
 func NewTestEndpointWithState(_ testing.TB, owner regeneration.Owner, policyGetter policyRepoGetter, namedPortsGetter namedPortsGetter, proxy EndpointProxy, allocator cache.IdentityAllocator, ID uint16, state State) *Endpoint {
 	endpointQueueName := "endpoint-" + strconv.FormatUint(uint64(ID), 10)
 	ep := createEndpoint(owner, policyGetter, namedPortsGetter, proxy, allocator, ID, "")
+	ep.SetPropertyValue(PropertyFakeEndpoint, true)
 	ep.state = state
 	ep.eventQueue = eventqueue.NewEventQueueBuffered(endpointQueueName, option.Config.EndpointQueueSize)
 
@@ -546,6 +568,7 @@ func createEndpoint(owner regeneration.Owner, policyGetter policyRepoGetter, nam
 		allocator:        allocator,
 		logLimiter:       logging.NewLimiter(10*time.Second, 3), // 1 log / 10 secs, burst of 3
 		noTrackPort:      0,
+		properties:       map[string]interface{}{},
 	}
 
 	ep.initDNSHistoryTrigger()
@@ -580,6 +603,12 @@ func CreateIngressEndpoint(owner regeneration.Owner, policyGetter policyRepoGett
 	ep.DatapathConfiguration = NewDatapathConfiguration()
 
 	ep.isIngress = true
+	// An ingress endpoint is defined without a veth interface and no bpf
+	// programs or maps are created for it. Thus, we will set its properties
+	// to not have a bpf policy map nor a bpf datapath.
+	ep.properties[PropertySkipBPFPolicy] = true
+	ep.properties[PropertyWithouteBPFDatapath] = true
+
 	// node.GetIngressIPv4 has been parsed with net.ParseIP() and may be in IPv4 mapped IPv6
 	// address format. Use ippkg.AddrFromIP() to make sure we get a plain IPv4 address.
 	ep.IPv4, _ = ippkg.AddrFromIP(node.GetIngressIPv4())
@@ -1210,7 +1239,7 @@ type DeleteConfig struct {
 func (e *Endpoint) leaveLocked(proxyWaitGroup *completion.WaitGroup, conf DeleteConfig) []error {
 	errors := []error{}
 
-	if !option.Config.DryMode {
+	if !e.isProperty(PropertyFakeEndpoint) {
 		e.owner.Datapath().Loader().Unload(e.createEpInfoCache(""))
 	}
 
@@ -1265,7 +1294,7 @@ func (e *Endpoint) leaveLocked(proxyWaitGroup *completion.WaitGroup, conf Delete
 
 	if e.ConntrackLocalLocked() {
 		ctmap.CloseLocalMaps(e.conntrackName())
-	} else if !option.Config.DryMode {
+	} else if !e.isProperty(PropertyFakeEndpoint) {
 		e.scrubIPsInConntrackTableLocked()
 	}
 
@@ -2404,7 +2433,7 @@ func (e *Endpoint) Delete(conf DeleteConfig) []error {
 	e.setState(StateDisconnecting, "Deleting endpoint")
 
 	// If dry mode is enabled, no changes to BPF maps are performed
-	if !option.Config.DryMode {
+	if !e.isProperty(PropertyFakeEndpoint) {
 		if errs2 := lxcmap.DeleteElement(e); errs2 != nil {
 			errs = append(errs, errs2...)
 		}
@@ -2544,4 +2573,38 @@ func (e *Endpoint) setDefaultPolicyConfig() {
 // GetCreatedAt returns the endpoint creation time.
 func (e *Endpoint) GetCreatedAt() time.Time {
 	return e.createdAt
+}
+
+// GetMetadataValue returns the metadata value for this key.
+func (e *Endpoint) GetMetadataValue(key string) interface{} {
+	e.mutex.RWMutex.RLock()
+	defer e.mutex.RWMutex.RUnlock()
+	return e.properties[key]
+}
+
+// SetPropertyValue sets the metadata value for this key.
+func (e *Endpoint) SetPropertyValue(key string, value interface{}) interface{} {
+	e.mutex.RWMutex.Lock()
+	defer e.mutex.RWMutex.Unlock()
+	old := e.properties[key]
+	e.properties[key] = value
+	return old
+}
+
+// IsProperty checks if the value of the properties map is set, it's a boolean
+// and its value is 'true'.
+func (e *Endpoint) IsProperty(propertyKey string) bool {
+	e.mutex.RWMutex.RLock()
+	defer e.mutex.RWMutex.RUnlock()
+	return e.isProperty(propertyKey)
+}
+
+// isProperty checks if the value of the properties map is set, it's a boolean
+// and its value is 'true'.
+func (e *Endpoint) isProperty(propertyKey string) bool {
+	if v, ok := e.properties[propertyKey]; ok {
+		isSet, ok := v.(bool)
+		return ok && isSet
+	}
+	return false
 }

api/v1/models/endpoint_change_request.go
@@ -79,6 +79,9 @@ type EndpointChangeRequest struct {
 	// Whether policy enforcement is enabled or not
 	PolicyEnabled bool `json:"policy-enabled,omitempty"`
 
+	// Properties is used to store information about the endpoint at creation. Useful for tests.
+	Properties map[string]interface{} `json:"properties,omitempty"`
+
 	// Current state of endpoint
 	// Required: true
 	State *EndpointState `json:"state"`

api/v1/openapi.yaml
@@ -1545,6 +1545,13 @@ definitions:
         type: boolean
       datapath-configuration:
         "$ref": "#/definitions/EndpointDatapathConfiguration"
+      properties:
+        description: >-
+          Properties is used to store information about the endpoint at
+          creation.
+          Useful for tests.
+        additionalProperties:
+          type: object
   EndpointBatchDeleteRequest:
     description: |
       Properties selecting a batch of endpoints to delete.

api/v1/server/embedded_spec.go
@@ -2950,6 +2950,12 @@ func init() {
           "description": "Whether policy enforcement is enabled or not",
           "type": "boolean"
         },
+        "properties": {
+          "description": "Properties is used to store information about the endpoint at creation. Useful for tests.",
+          "additionalProperties": {
+            "type": "object"
+          }
+        },
         "state": {
           "description": "Current state of endpoint",
           "$ref": "#/definitions/EndpointState"
@@ -8816,6 +8822,12 @@ func init() {
         "policy-enabled": {
           "description": "Whether policy enforcement is enabled or not",
           "type": "boolean"
+        },
+        "properties": {
+          "description": "Properties is used to store information about the endpoint at creation. Useful for tests.",
+          "additionalProperties": {
+            "type": "object"
+          }
         },
         "state": {
           "description": "Current state of endpoint",

daemon/cmd/endpoint_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	apiEndpoint "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
 	"github.com/cilium/cilium/pkg/checker"
+	"github.com/cilium/cilium/pkg/endpoint"
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipam"
@@ -34,6 +35,10 @@ func getEPTemplate(c *C, d *Daemon) *models.EndpointChangeRequest {
 		Addressing: &models.AddressPair{
 			IPV6: ip6.IP.String(),
 			IPV4: ip4.IP.String(),
+		},
+		Properties: map[string]interface{}{
+			endpoint.PropertySkipBPFRegeneration: true,
+			endpoint.PropertyFakeEndpoint:        true,
 		},
 	}
 }

daemon/cmd/policy_test.go
@@ -214,6 +214,9 @@ func (ds *DaemonSuite) prepareEndpoint(c *C, identity *identity.Identity, qa boo
 		testEndpointID = testQAEndpointID
 	}
 	e := endpoint.NewTestEndpointWithState(c, ds.d, ds.d, testipcache.NewMockIPCache(), ds.d.l7Proxy, ds.d.identityAllocator, testEndpointID, endpoint.StateWaitingForIdentity)
+	e.SetPropertyValue(endpoint.PropertyFakeEndpoint, false)
+	e.SetPropertyValue(endpoint.PropertyWithouteBPFDatapath, true)
+	e.SetPropertyValue(endpoint.PropertySkipBPFPolicy, true)
 	if qa {
 		e.IPv6 = QAIPv6Addr
 		e.IPv4 = QAIPv4Addr

daemon/cmd/state.go
@@ -63,6 +63,10 @@ func checkLink(linkName string) error {
 // Returns true to indicate that the endpoint is valid to restore, and an
 // optional error.
 func (d *Daemon) validateEndpoint(ep *endpoint.Endpoint) (valid bool, err error) {
+	if ep.IsProperty(endpoint.PropertyFakeEndpoint) {
+		return true, nil
+	}
+
 	// On each restart, the health endpoint is supposed to be recreated.
 	// Hence we need to clean health endpoint state unconditionally.
 	if ep.HasLabels(labels.LabelHealth) {

daemon/cmd/watchdogs.go
@@ -102,7 +102,7 @@ func (d *Daemon) checkEndpointBPFPrograms(ctx context.Context, p epBPFProgWatchd
 		if ep.GetState() != endpoint.StateReady {
 			continue
 		}
-		if !ep.HasBPFPolicyMap() {
+		if ep.IsProperty(endpoint.PropertyWithouteBPFDatapath) {
 			// Skip Endpoints without BPF datapath
 			continue
 		}

commit cc306b1159d46033f48b5df0624e56930f195ad1

endpoint: rename GetMetadataValue to GetPropertyValue
During the PR review for d735c5017bd1 it was suggested to change the
field name from endpoint.Metadata to endpoint.Properties. Unfortunately,
this method was missed during the renaming and this commit sets the
right name to it.

Fixes: d735c5017bd1 ("introduce 'properties' for endpoints")
Signed-off-by: André Martins <andre@isovalent.com>
Signed-off-by: Gilberto Bertin <jibi@cilium.io>

pkg/endpoint/endpoint.go
@@ -2611,8 +2611,8 @@ func (e *Endpoint) GetCreatedAt() time.Time {
 	return e.createdAt
 }
 
-// GetMetadataValue returns the metadata value for this key.
-func (e *Endpoint) GetMetadataValue(key string) interface{} {
+// GetPropertyValue returns the metadata value for this key.
+func (e *Endpoint) GetPropertyValue(key string) interface{} {
 	e.mutex.RWMutex.RLock()
 	defer e.mutex.RWMutex.RUnlock()
 	return e.properties[key]