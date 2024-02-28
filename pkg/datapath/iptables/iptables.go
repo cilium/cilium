cilium/cilium

commit 1254bf403f95d5fc7bc796eb8c6db31badd290ab

daemon / cni: move to Cell, watch for changes
Refactor the CNI controller to use the Hive / Cell mechanism. This means
simplifying the underlying controller and making it run immediately,
rather than waiting for the Daemon to start. This mirrors the existing
behavior of the postStart hook, which will be going away.

The controller now also watches the CNI configuration directory and
reconciles whenever a file has changed; this is needed because some
chaining modes (e.g. AWS) need to wait until the aws-cni plugin installs
its own CNI configuration file. We then want to install our chained
plugin ASAP.

Previously, we would just loop until the configuration was created, but
that is fragile. Rather, set up an fsnotify and watch for changes that
way. This has the advantage of re-generating the chained configuration should
the underlying network change.

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

pkg/datapath/iptables/iptables.go
@@ -279,6 +279,7 @@ type IptablesManager struct {
 	haveSocketMatch      bool
 	haveBPFSocketAssign  bool
 	ipEarlyDemuxDisabled bool
+	CNIChainingMode      string
 }
 
 // Init initializes the iptables manager and checks for iptables kernel modules
@@ -1031,11 +1032,11 @@ func (m *IptablesManager) doGetProxyPort(prog iptablesInterface, name string) ui
 	return uint16(portUInt64)
 }
 
-func getDeliveryInterface(ifName string) string {
+func (m *IptablesManager) getDeliveryInterface(ifName string) string {
 	switch {
 	case option.Config.EnableEndpointRoutes:
 		// aws-cni creates container interfaces with names like eni621c0fc8425.
-		if option.Config.CNIChainingMode == "aws-cni" {
+		if m.CNIChainingMode == "aws-cni" {
 			return "eni+"
 		}
 		return "lxc+"
@@ -1494,7 +1495,7 @@ func (m *IptablesManager) installRules(ifName string) error {
 		return fmt.Errorf("cannot install xfrm rules: %w", err)
 	}
 
-	localDeliveryInterface := getDeliveryInterface(ifName)
+	localDeliveryInterface := m.getDeliveryInterface(ifName)
 
 	if err := m.installForwardChainRules(ifName, localDeliveryInterface, ciliumForwardChain); err != nil {
 		return fmt.Errorf("cannot install forward chain rules to %s: %w", ciliumForwardChain, err)

daemon/cmd/cells.go
@@ -4,6 +4,7 @@
 package cmd
 
 import (
+	"github.com/cilium/cilium/daemon/cmd/cni"
 	"github.com/cilium/cilium/pkg/auth"
 	"github.com/cilium/cilium/pkg/bgpv1"
 	"github.com/cilium/cilium/pkg/crypto/certificatemanager"
@@ -50,6 +51,8 @@ var (
 
 		// Provides Clientset, API for accessing Kubernetes objects.
 		k8sClient.Cell,
+
+		cni.Cell,
 
 		// Provide option.Config via hive so cells can depend on the agent config.
 		cell.Provide(func() *option.DaemonConfig { return option.Config }),

daemon/cmd/cni.go
@@ -1,325 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-package cmd
-
-import (
-	"bytes"
-	"context"
-	"fmt"
-	"os"
-	"path"
-	"strings"
-	"text/template"
-	"time"
-
-	"github.com/cilium/cilium/pkg/controller"
-	"github.com/cilium/cilium/pkg/option"
-
-	"github.com/google/renameio/v2"
-	"github.com/tidwall/sjson"
-)
-
-// Some functions around managing the CNI configuration file
-//
-// By default, we write the CNI configuration file to disk after the
-// daemon has successfully started up.
-
-// cniConfigs are the default configurations, per chaining mode
-var cniConfigs map[string]string = map[string]string{
-	// the default
-	"none": `
-{
-  "cniVersion": "0.3.1",
-  "name": "cilium",
-  "plugins": [
-    {
-       "type": "cilium-cni",
-       "enable-debug": {{.Debug | js }},
-       "log-file": "{{.LogFile | js }}"
-    }
-  ]
-}`,
-
-	"flannel": `
-{
-  "cniVersion": "0.3.1",
-  "name": "flannel",
-  "plugins": [
-    {
-      "type": "flannel",
-      "delegate": {
-         "hairpinMode": true,
-         "isDefaultGateway": true
-      }
-    },
-    {
-      "type": "portmap",
-      "capabilities": {
-        "portMappings": true
-      }
-    },
-    {
-       "type": "cilium-cni",
-       "enable-debug": {{.Debug | js }},
-       "log-file": "{{.LogFile | js }}"
-    }
-  ]
-}
-`,
-	"portmap": `
-{
-  "cniVersion": "0.3.1",
-  "name": "portmap",
-  "plugins": [
-    {
-       "type": "cilium-cni",
-       "enable-debug": {{.Debug | js }},
-       "log-file": "{{.LogFile | js }}"
-    },
-    {
-      "type": "portmap",
-      "capabilities": {"portMappings": true}
-    }
-  ]
-}
-`,
-}
-
-// The CNI plugin config we inject in to the plugins[] array of existing AWS configs
-const awsCNIEntry = `
-{
-	"type": "cilium-cni",
-	"enable-debug": {{.Debug | js }},
-	"log-file": "{{.LogFile | js }}"
-}
-`
-
-const cniControllerName = "write-cni-file"
-
-// startCNIConfWriter writes the CNI configuration file to disk.
-// This is done once the daemon has started up, to signify to the
-// kubelet that we're ready to handle sandbox creation.
-// There are numerous, conflicting CNI options, exposed in Helm and
-// the cilium-config config map.
-//
-// This consumes the following config map keys (or equivalent arguments):
-// - write-cni-conf-when-ready=PATH -- path to write the CNI config. If blank, don't manage CNI config
-// - read-cni-conf -- A "source" CNI file to use, rather than generating one
-// - cni-chaining-mode=MODE -- The CNI configuration format to use, e.g. aws-cni, flannel.
-// - cni-exlusive -- if true, then remove other existing CNI configurations
-// - cni-log-file=PATH -- A file for the CNI plugin to use for logging
-// - debug -- Whether or not the CNI plugin binary should be verbose
-//
-// As well as the following deprecated environment variables:
-// - CILIUM_CUSTOM_CNI_CONF -- if true, then don't touch CNI configuration
-// - CNI_CONF_NAME -- the filename (NOT full path) to write the CNI configuration to
-func (d *Daemon) startCNIConfWriter(opts *option.DaemonConfig, cleaner *daemonCleanup) {
-	if opts.WriteCNIConfigurationWhenReady == "" {
-		return
-	}
-
-	d.controllers.UpdateController(cniControllerName,
-		controller.ControllerParams{
-			DoFunc: func(ctx context.Context) error {
-				err := installCNIConfFile(opts)
-				if err != nil {
-					log.Printf("Failed to write CNI config file (will retry): %v", err)
-				}
-				return err
-			},
-			Context:                d.ctx,
-			ErrorRetryBaseDuration: 10 * time.Second,
-		},
-	)
-
-	cleaner.cleanupFuncs.Add(func() {
-		removeCNIConfFile(opts)
-	})
-}
-
-// installCNIConfFile tries to render and write the CNI configuration file to disk.
-// Returns error on failure.
-func installCNIConfFile(opts *option.DaemonConfig) error {
-	// Determine the path to the CNI conf directory
-	confDir, filename := path.Split(opts.WriteCNIConfigurationWhenReady)
-
-	// we used to document overriding the CNI file with the env var CNI_CONF_NAME
-	// this is no longer documented, but we still support it
-	if override := os.Getenv("CNI_CONF_NAME"); override != "" {
-		filename = override
-	}
-
-	var contents []byte
-	var err error
-
-	// generate CNI config, either by reading a user-supplied
-	// template file or rendering our own.
-	if opts.ReadCNIConfiguration != "" {
-		contents, err = os.ReadFile(opts.ReadCNIConfiguration)
-		if err != nil {
-			return fmt.Errorf("failed to read source CNI config file at %s: %w", opts.ReadCNIConfiguration, err)
-		}
-		log.Infof("Reading CNI configuration file from %s", opts.ReadCNIConfiguration)
-	} else {
-		contents, err = renderCNIConf(opts, confDir)
-		if err != nil {
-			return fmt.Errorf("failed to render CNI configuration file: %w", err)
-		}
-	}
-
-	// commit CNI config
-	if err := renameio.WriteFile(path.Join(confDir, filename), contents, 0644); err != nil {
-		return fmt.Errorf("failed to write CNI configuration file at %s: %w", opts.WriteCNIConfigurationWhenReady, err)
-	}
-	log.Infof("Wrote CNI configuration file to %s", opts.WriteCNIConfigurationWhenReady)
-
-	// Remove the old non-conflist cilium file
-	if filename != "05-cilium.conf" {
-		_ = os.Remove(path.Join(confDir, "05-cilium.conf"))
-	}
-	// Rename any non-cilium CNI config files.
-	if opts.CNIExclusive {
-		cleanupOtherCNI(confDir, filename)
-	}
-
-	return nil
-}
-
-func removeCNIConfFile(opts *option.DaemonConfig) {
-	// Don't touch anything if we're not in charge of CNI configuration, or
-	// we didn't ensure the CNI configuration directory was empty.
-	// This is because we don't want any existing CNI configurations to "take over"
-	// when we remove cilium.
-	if opts.WriteCNIConfigurationWhenReady == "" || !opts.CNIExclusive {
-		return
-	}
-
-	err := os.Remove(opts.WriteCNIConfigurationWhenReady)
-	if err != nil {
-		log.Errorf("Failed to remove CNI configuration file at %s: %v", opts.WriteCNIConfigurationWhenReady, err)
-	}
-	log.Infof("Removed CNI configuration file at %s", opts.WriteCNIConfigurationWhenReady)
-}
-
-// renderCNIConf renders the CNI configuration file based on the parameters.
-// Note: this is somewhat special for AWS, see tryInjectAWSConfig.
-func renderCNIConf(opts *option.DaemonConfig, confDir string) (cniConfig []byte, err error) {
-	// special case: AWS adjusts existing file rather than creating
-	// one from scratch
-	if opts.CNIChainingMode == "aws-cni" {
-		pluginConfig := renderCNITemplate(awsCNIEntry, opts)
-		cniConfig, err = mergeExistingAWSCNIConfig(confDir, pluginConfig)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		mode := opts.CNIChainingMode
-		if mode == "" {
-			mode = "none"
-		}
-		log.Infof("Generating CNI configuration file with mode %s", mode)
-		tmpl := cniConfigs[opts.CNIChainingMode]
-		cniConfig = []byte(renderCNITemplate(tmpl, opts))
-	}
-
-	if len(cniConfig) == 0 {
-		return nil, fmt.Errorf("invalid CNI chaining mode: %s", opts.CNIChainingMode)
-	}
-
-	return cniConfig, nil
-}
-
-// mergeExistingAWSCNIConfig looks for an existing AWS cni configuration file
-// and modifies it to include Cilium. This is because they do something
-// "special" for v6-only clusters.
-//
-// cniDir is the directory in the container to look for AWS CNI config, and
-// pluginConfig is the raw json to insert
-//
-// See PR #18522 for details.
-func mergeExistingAWSCNIConfig(confDir string, pluginConfig []byte) ([]byte, error) {
-	awsFiles := []string{"10-aws.conflist", "10-aws.conflist.cilium_bak"}
-	found, err := findFile(confDir, awsFiles)
-	if err != nil {
-		return nil, fmt.Errorf("could not find existing AWS CNI config for chaining %w", err)
-	}
-
-	contents, err := os.ReadFile(found)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read existing AWS CNI config %s: %w", found, err)
-	}
-
-	// We found the CNI configuration,
-	// inject Cilium as the last chained plugin
-	out, err := sjson.SetRawBytes(contents, "plugins.-1", pluginConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to modify existing AWS CNI config at %s: %w", found, err)
-	}
-	log.Infof("Inserting cilium in to CNI configuration file at %s", found)
-	return out, nil
-}
-
-// renderCNITemplate applies any cni template replacements
-// right now, just sets .Debug and Logfile
-func renderCNITemplate(in string, opts *option.DaemonConfig) []byte {
-	data := struct {
-		Debug   bool
-		LogFile string
-	}{
-		Debug:   opts.Debug,
-		LogFile: opts.CNILogFile,
-	}
-
-	t := template.Must(template.New("cni").Parse(in))
-
-	out := bytes.Buffer{}
-	if err := t.Execute(&out, data); err != nil {
-		panic(err) // impossible
-	}
-	return out.Bytes()
-}
-
-// cleanupOldCNI renames any existing CNI configuration files with the suffix
-// ".cilium_bak", excepting files in keep
-func cleanupOtherCNI(confDir, keep string) error {
-	files, err := os.ReadDir(confDir)
-	if err != nil {
-		return fmt.Errorf("failed to list CNI conf dir %s: %w", confDir, err)
-	}
-
-	for _, f := range files {
-		if f.IsDir() {
-			continue
-		}
-		name := f.Name()
-		if name == keep {
-			continue
-		}
-		if !(strings.HasSuffix(name, ".conf") || strings.HasSuffix(name, ".conflist") || strings.HasSuffix(name, ".json")) {
-			continue
-		}
-
-		log.Infof("Renaming non-Cilium CNI configuration file %s to %s.cilium_bak", name, name)
-		_ = os.Rename(path.Join(confDir, name), path.Join(confDir, name+".cilium_bak"))
-	}
-	return nil
-}
-
-// findFile looks in basedir for any filenames listed in paths.
-// returns the full path to the first one found, or error if none found
-func findFile(basedir string, paths []string) (string, error) {
-	for _, p := range paths {
-		_, err := os.Stat(path.Join(basedir, p))
-		if err != nil {
-			if os.IsNotExist(err) {
-				continue
-			}
-			return "", err
-		} else {
-			return path.Join(basedir, p), nil
-		}
-	}
-
-	return "", os.ErrNotExist
-}

daemon/cmd/cni/cell.go
@@ -1,0 +1,76 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cni
+
+import (
+	"context"
+	"fmt"
+	"path"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/option"
+)
+
+var Cell = cell.Module(
+	"cni-config",
+	"CNI configuration manager",
+
+	cell.Config(defaultConfig),
+	cell.Provide(enableConfigManager),
+)
+
+type Config struct {
+	WriteCNIConfWhenReady string
+	ReadCNIConf           string
+	CNIChainingMode       string
+	CNILogFile            string
+	CNIExclusive          bool
+}
+
+type CNIConfigManager interface {
+	// GetMTU provides the MTU from the provided CNI configuration file.
+	// This is only useful if ReadCNIConfiguration is set *and* the file specifies an MTU.
+	GetMTU() int
+
+	// GetChainingMode returns the configured CNI chaining mode
+	GetChainingMode() string
+}
+
+var defaultConfig = Config{
+	CNIChainingMode: "none",
+	CNILogFile:      "/var/run/cilium/cilium-cni.log",
+}
+
+func (cfg Config) Flags(flags *pflag.FlagSet) {
+	flags.String(option.WriteCNIConfigurationWhenReady, defaultConfig.WriteCNIConfWhenReady, "Write the CNI configuration to the specified path when agent is ready")
+	flags.String(option.ReadCNIConfiguration, defaultConfig.ReadCNIConf, fmt.Sprintf("CNI configuration file to use as a source for --%s. If not supplied, a suitable one will be generated.", option.WriteCNIConfigurationWhenReady))
+	flags.String(option.CNIChainingMode, defaultConfig.CNIChainingMode, "Enable CNI chaining with the specified plugin")
+	flags.String(option.CNILogFile, defaultConfig.CNILogFile, "Path where the CNI plugin should write logs")
+	flags.Bool(option.CNIExclusive, defaultConfig.CNIExclusive, "Whether to remove other CNI configurations")
+}
+
+func enableConfigManager(lc hive.Lifecycle, log logrus.FieldLogger, cfg Config, dcfg *option.DaemonConfig /*only for .Debug*/) CNIConfigManager {
+	c := newConfigManager(log, cfg, dcfg.Debug)
+	lc.Append(c)
+	return c
+}
+
+func newConfigManager(log logrus.FieldLogger, cfg Config, debug bool) *cniConfigManager {
+	c := &cniConfigManager{
+		config:     cfg,
+		debug:      debug,
+		log:        log,
+		controller: controller.NewManager(),
+	}
+
+	c.cniConfDir, c.cniConfFile = path.Split(cfg.WriteCNIConfWhenReady)
+	c.ctx, c.doneFunc = context.WithCancel(context.Background())
+
+	return c
+}

daemon/cmd/cni/cni_test.go
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package cmd
+package cni
 
 import (
 	"os"
 	"path"
 	"testing"
 
-	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/logging"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/tidwall/gjson"
@@ -16,42 +16,45 @@ import (
 
 func TestInstallCNIConfFile(t *testing.T) {
 	workdir := t.TempDir()
-	opts := &option.DaemonConfig{
-		Debug:                          false,
-		CNILogFile:                     "/opt\"/cni.log", // test escaping :-)
-		CNIChainingMode:                "none",
-		CNIExclusive:                   true,
-		WriteCNIConfigurationWhenReady: path.Join(workdir, "05-cilium.conflist"),
+	cfg := Config{
+		CNILogFile:            `/opt"/cni.log`,
+		CNIChainingMode:       "none",
+		CNIExclusive:          true,
+		WriteCNIConfWhenReady: path.Join(workdir, "05-cilium.conflist"),
 	}
+	c := newConfigManager(logging.DefaultLogger, cfg, false)
 
 	touch(t, workdir, "other.conflist")
 	touch(t, workdir, "05-cilium.conf") // older config file we no longer create
 
-	err := installCNIConfFile(opts)
+	err := c.setupCNIConfFile()
 	assert.NoError(t, err)
 
 	filenames := ls(t, workdir)
 
 	assert.ElementsMatch(t, filenames, []string{
 		"05-cilium.conflist",
+		"05-cilium.conf.cilium_bak",
 		"other.conflist.cilium_bak",
 	})
 
 	data, err := os.ReadFile(path.Join(workdir, "05-cilium.conflist"))
 	assert.NoError(t, err)
 	val := gjson.GetBytes(data, "plugins.0.type")
-	assert.Equal(t, val.String(), "cilium-cni")
+	assert.Equal(t, "cilium-cni", val.String())
 
 }
 
 func TestMergeExistingAWSConfig(t *testing.T) {
-	opts := &option.DaemonConfig{
-		Debug:           false,
-		CNILogFile:      "/opt\"/cni.log", // test escaping :-)
-		CNIChainingMode: "aws-cni",
-	}
-
 	workdir := t.TempDir()
+	cfg := Config{
+		CNILogFile:            `/opt"/cni.log`,
+		CNIChainingMode:       "aws-cni",
+		CNIExclusive:          true,
+		WriteCNIConfWhenReady: path.Join(workdir, "05-cilium.conflist"),
+	}
+	c := newConfigManager(logging.DefaultLogger, cfg, false)
+
 	// write an "existing" AWS config
 	existing := `
 {
@@ -69,34 +72,34 @@ func TestMergeExistingAWSConfig(t *testing.T) {
 `
 	assert.NoError(t, os.WriteFile(path.Join(workdir, "10-aws.conflist"), []byte(existing), 0644))
 
-	res, err := renderCNIConf(opts, workdir)
+	res, err := c.renderCNIConf()
 	assert.NoError(t, err)
 
 	val := gjson.GetBytes(res, `plugins.#`)
-	assert.Equal(t, val.Int(), int64(3))
+	assert.Equal(t, int64(3), val.Int())
 
 	val = gjson.GetBytes(res, `plugins.@reverse.0.type`)
-	assert.Equal(t, val.String(), "cilium-cni")
+	assert.Equal(t, "cilium-cni", val.String())
 
 }
 
 func TestRenderCNIConf(t *testing.T) {
-	opts := &option.DaemonConfig{
-		Debug:      false,
-		CNILogFile: "/opt\"/cni.log", // test escaping :-)
+	cfg := Config{
+		CNILogFile:      `/opt"/cni.log`,
+		CNIChainingMode: "aws-cni",
 	}
+	c := newConfigManager(logging.DefaultLogger, cfg, false)
 	// check that all templates compile
 	for mode := range cniConfigs {
-		opts.CNIChainingMode = mode
-		conf, err := renderCNIConf(opts, "/")
+		c.config.CNIChainingMode = mode
+		conf, err := c.renderCNIConf()
 		assert.NoError(t, err)
 		res := gjson.GetBytes(conf, `plugins.#(type=="cilium-cni").log-file`)
 		assert.True(t, res.Exists(), mode)
-		assert.Equal(t, res.String(), "/opt\"/cni.log", mode)
+		assert.Equal(t, `/opt"/cni.log`, res.String(), mode)
 	}
 
 	assert.Equal(t,
-		string(renderCNITemplate(awsCNIEntry, opts)),
 		`
 {
 	"type": "cilium-cni",
@@ -104,6 +107,7 @@ func TestRenderCNIConf(t *testing.T) {
 	"log-file": "/opt\"/cni.log"
 }
 `,
+		string(c.renderCNITemplate(awsCNIEntry)),
 	)
 
 }
@@ -117,16 +121,20 @@ func TestFindFile(t *testing.T) {
 	touch(t, workdir, "b")
 
 	found, _ := findFile(workdir, []string{"a", "b"})
-	assert.Equal(t, found, path.Join(workdir, "b"))
+	assert.Equal(t, path.Join(workdir, "b"), found)
 
 	touch(t, workdir, "a")
 	found, _ = findFile(workdir, []string{"a", "b"})
-	assert.Equal(t, found, path.Join(workdir, "a"))
+	assert.Equal(t, path.Join(workdir, "a"), found)
 }
 
 func TestCleanupOtherCNI(t *testing.T) {
-
 	workdir := t.TempDir()
+	cfg := Config{
+		CNIExclusive:          true,
+		WriteCNIConfWhenReady: path.Join(workdir, "42-keep.json"),
+	}
+	c := newConfigManager(logging.DefaultLogger, cfg, false)
 
 	for _, name := range []string{
 		"01-someoneelse.conf",
@@ -137,9 +145,8 @@ func TestCleanupOtherCNI(t *testing.T) {
 		touch(t, workdir, name)
 	}
 
-	if err := cleanupOtherCNI(workdir, "42-keep.json"); err != nil {
-		t.Fatal(err)
-	}
+	err := c.cleanupOtherCNI()
+	assert.NoError(t, err)
 
 	filenames := ls(t, workdir)
 	assert.ElementsMatch(t, filenames, []string{

commit 8a3861cc8e91b8374ef49426e898e502afbb8ca3

iptables: Read CNI chaining mode from CNI config manager
CNI chaining mode option has been moved to the CNI cell in commit
1254bf403f.

Since it is not a global config option anymore, iptables manager will
not see any change to that value, and its field `CNIChainingMode` will
always be an empty string.
Thus, with the following config option values:

- "enable-endpoint-routes": true
- "cni-chaining-mode": "aws-cni"

the delivery interface referenced in the rules installed by the manager
is "lxc+" instead of "eni+".

This commit fixes this adding a CNI config manager reference to the
iptables manager parameters, in order to read the current setting for
the chaining mode during rules installation.

Fixes: 1254bf403f ("daemon / cni: move to Cell, watch for changes")

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/datapath/iptables/iptables.go
@@ -18,6 +18,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
 
+	"github.com/cilium/cilium/daemon/cmd/cni"
 	"github.com/cilium/cilium/pkg/backoff"
 	"github.com/cilium/cilium/pkg/byteorder"
 	"github.com/cilium/cilium/pkg/cidr"
@@ -270,7 +271,7 @@ type Manager struct {
 	haveSocketMatch      bool
 	haveBPFSocketAssign  bool
 	ipEarlyDemuxDisabled bool
-	CNIChainingMode      string
+	cniConfigManager     cni.CNIConfigManager
 }
 
 type params struct {
@@ -279,8 +280,9 @@ type params struct {
 	Logger    logrus.FieldLogger
 	Lifecycle cell.Lifecycle
 
-	ModulesMgr *modules.Manager
-	Sysctl     sysctl.Sysctl
+	ModulesMgr       *modules.Manager
+	Sysctl           sysctl.Sysctl
+	CNIConfigManager cni.CNIConfigManager
 
 	Cfg       Config
 	SharedCfg SharedConfig
@@ -288,12 +290,13 @@ type params struct {
 
 func newIptablesManager(p params) *Manager {
 	iptMgr := &Manager{
-		logger:        p.Logger,
-		modulesMgr:    p.ModulesMgr,
-		sysctl:        p.Sysctl,
-		cfg:           p.Cfg,
-		sharedCfg:     p.SharedCfg,
-		haveIp6tables: true,
+		logger:           p.Logger,
+		modulesMgr:       p.ModulesMgr,
+		sysctl:           p.Sysctl,
+		cfg:              p.Cfg,
+		sharedCfg:        p.SharedCfg,
+		haveIp6tables:    true,
+		cniConfigManager: p.CNIConfigManager,
 	}
 
 	p.Lifecycle.Append(iptMgr)
@@ -1088,7 +1091,7 @@ func (m *Manager) getDeliveryInterface(ifName string) string {
 	switch {
 	case m.sharedCfg.EnableEndpointRoutes:
 		// aws-cni creates container interfaces with names like eni621c0fc8425.
-		if m.CNIChainingMode == "aws-cni" {
+		if m.cniConfigManager.GetChainingMode() == "aws-cni" {
 			return "eni+"
 		}
 		return "lxc+"