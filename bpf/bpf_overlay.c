cilium/cilium

commit a94fa56f6713dcbccd99a584097a513d489e6537

Fix CIDR to World Entity Conversion Bug
Previously if either the "0.0.0.0/0" CIDR (hereafter "world-ipv4")
or a world-ipv6 CIDR was present the selector creation logic
for CIDRs would also add the "reserved:world" entity to the
selector labels generated by the CIDR.

This does not work when Cilium is dual-stack mode as "reserved:world"
covers both IPv4 and IPv6, so both CIDRs should be present before such
an addition is made. Moreover, dual-stack mode lacked any mechanism to
distinguish world-ipv4 and world-ipv6 from another. This bug has prevented
the possibility of managing world-ipv4 and world-ipv6 separately from one
another in the policy engine.

This inccorect assumption goes all the way to the datapath, where SECLABEL
and WORLD_ID were part of drop notification, policy logic, and tunnel logic.
SECLABLE and WORLD_ID were both set by userspace compilation logic.

Separate identities (and labels, etc) for world-ipv4 and world-ipv6 now exist
so they can be distinguished in userspace and in the datapath.

Some utility methods have been added in userspace and datapath to check
for the necessity of using world-ipv4 and world-ipv6 identities.

Explicit tests for dual-stack mode have been added.

The generic WORLD_ID is still used as the VNI tag for all tunnel logic
in the datapath. When tunnel code is encapsulating any WORLD_IPX_ID
identity it will convert the VNI to WORLD_ID. When tunnel code is
decapsulating any WORLD_ID VNI it will convert the VNI to either
WORLD_IPV4_ID or WORLD_IPV6_ID based on the underlying packet
protocol.

Signed-off-by: Nate Sweet <nathanjsweet@pm.me>

bpf/bpf_overlay.c
@@ -75,13 +75,15 @@ static __always_inline int handle_ipv6(struct __ctx_buff *ctx,
 
 	decrypted = ((ctx->mark & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_DECRYPT);
 	if (decrypted) {
-		if (info)
-			*identity = key.tunnel_id = info->sec_identity;
+		if (info) {
+			*identity = info->sec_identity;
+			key.tunnel_id = get_tunnel_id(info->sec_identity);
+		}
 	} else {
 		key_size = TUNNEL_KEY_WITHOUT_SRC_IP;
 		if (unlikely(ctx_get_tunnel_key(ctx, &key, key_size, 0) < 0))
 			return DROP_NO_TUNNEL_KEY;
-		*identity = key.tunnel_id;
+		*identity = get_id_from_tunnel_id(key.tunnel_id, ctx_get_protocol(ctx));
 
 		/* Any node encapsulating will map any HOST_ID source to be
 		 * presented as REMOTE_NODE_ID, therefore any attempt to signal
@@ -321,16 +323,18 @@ static __always_inline int handle_ipv4(struct __ctx_buff *ctx,
 	decrypted = ((ctx->mark & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_DECRYPT);
 	/* If packets are decrypted the key has already been pushed into metadata. */
 	if (decrypted) {
-		if (info)
-			*identity = key.tunnel_id = info->sec_identity;
+		if (info) {
+			*identity = info->sec_identity;
+			key.tunnel_id = get_tunnel_id(info->sec_identity);
+		}
 	} else {
 #ifdef ENABLE_HIGH_SCALE_IPCACHE
-		key.tunnel_id = *identity;
+		key.tunnel_id = get_tunnel_id(*identity);
 #else
 		key_size = TUNNEL_KEY_WITHOUT_SRC_IP;
 		if (unlikely(ctx_get_tunnel_key(ctx, &key, key_size, 0) < 0))
 			return DROP_NO_TUNNEL_KEY;
-		*identity = key.tunnel_id;
+		*identity = get_id_from_tunnel_id(key.tunnel_id, ctx_get_protocol(ctx));
 #endif /* ENABLE_HIGH_SCALE_IPCACHE */
 
 		if (*identity == HOST_ID)
@@ -345,7 +349,7 @@ static __always_inline int handle_ipv4(struct __ctx_buff *ctx,
 			if (!vtep)
 				goto skip_vtep;
 			if (vtep->tunnel_endpoint) {
-				if (*identity != WORLD_ID)
+				if (identity_is_world_ipv4(*identity))
 					return DROP_INVALID_VNI;
 			}
 		}
@@ -488,8 +492,8 @@ int tail_handle_arp(struct __ctx_buff *ctx)
 		return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP, METRIC_EGRESS);
 	if (info->tunnel_endpoint) {
 		ret = __encap_and_redirect_with_nodeid(ctx, 0, info->tunnel_endpoint,
-						       LOCAL_NODE_ID, WORLD_ID,
-						       WORLD_ID, &trace);
+						       LOCAL_NODE_ID, WORLD_IPV4_ID,
+						       WORLD_IPV4_ID, &trace);
 		if (IS_ERR(ret))
 			goto drop_err;
 

bpf/bpf_host.c
@@ -95,7 +95,7 @@ static __always_inline __u32
 resolve_srcid_ipv6(struct __ctx_buff *ctx, struct ipv6hdr *ip6,
 		   __u32 srcid_from_proxy, const bool from_host)
 {
-	__u32 src_id = WORLD_ID, srcid_from_ipcache = srcid_from_proxy;
+	__u32 src_id = WORLD_IPV6_ID, srcid_from_ipcache = srcid_from_proxy;
 	struct remote_endpoint_info *info = NULL;
 	union v6addr *src;
 
@@ -244,7 +244,7 @@ handle_ipv6_cont(struct __ctx_buff *ctx, __u32 secctx, const bool from_host,
 	if (from_host_raw & FROM_HOST_FLAG_NEED_HOSTFW) {
 		struct ct_buffer6 *ct_buffer;
 		__u32 zero = 0;
-		__u32 remote_id = WORLD_ID;
+		__u32 remote_id = WORLD_IPV6_ID;
 
 		ct_buffer = map_lookup_elem(&CT_TAIL_CALL_BUFFER6, &zero);
 		if (!ct_buffer)
@@ -357,7 +357,7 @@ handle_ipv6_cont(struct __ctx_buff *ctx, __u32 secctx, const bool from_host,
 	}
 #endif
 
-	if (!info || info->sec_identity == WORLD_ID) {
+	if (!info || identity_is_world_ipv6(info->sec_identity)) {
 		/* See IPv4 comment. */
 		return DROP_UNROUTABLE;
 	}
@@ -478,7 +478,7 @@ resolve_srcid_ipv4(struct __ctx_buff *ctx, struct iphdr *ip4,
 		   __u32 srcid_from_proxy, __u32 *sec_identity,
 		   const bool from_host)
 {
-	__u32 src_id = WORLD_ID, srcid_from_ipcache = srcid_from_proxy;
+	__u32 src_id = WORLD_IPV4_ID, srcid_from_ipcache = srcid_from_proxy;
 	struct remote_endpoint_info *info = NULL;
 
 	/* Packets from the proxy will already have a real identity. */
@@ -736,7 +736,8 @@ handle_ipv4_cont(struct __ctx_buff *ctx, __u32 secctx, const bool from_host,
 			if (eth_store_daddr(ctx, (__u8 *)&vtep->vtep_mac, 0) < 0)
 				return DROP_WRITE_ERROR;
 			return __encap_and_redirect_with_nodeid(ctx, 0, vtep->tunnel_endpoint,
-								secctx, WORLD_ID, WORLD_ID, &trace);
+								secctx, WORLD_IPV4_ID,
+								WORLD_IPV4_ID, &trace);
 		}
 	}
 skip_vtep:
@@ -763,7 +764,7 @@ skip_vtep:
 	}
 #endif
 
-	if (!info || info->sec_identity == WORLD_ID) {
+	if (!info || identity_is_world_ipv4(info->sec_identity)) {
 		/* We have received a packet for which no ipcache entry exists,
 		 * we do not know what to do with this packet, drop it.
 		 *
@@ -1191,12 +1192,24 @@ static __always_inline int
 handle_netdev(struct __ctx_buff *ctx, const bool from_host)
 {
 	__u16 proto;
-
 	if (!validate_ethertype(ctx, &proto)) {
 #ifdef ENABLE_HOST_FIREWALL
 		int ret = DROP_UNSUPPORTED_L2;
-
-		return send_drop_notify(ctx, SECLABEL, WORLD_ID, 0, ret,
+		__u32 id = WORLD_ID;
+		__u32 sec_label = SECLABEL;
+#if defined ENABLE_IPV4 && defined ENABLE_IPV6
+		switch (proto) {
+		case bpf_htons(ETH_P_IP):
+			id = WORLD_IPV4_ID;
+			sec_label = SECLABEL_IPV4;
+			break;
+		case bpf_htons(ETH_P_IPV6):
+			id = WORLD_IPV6_ID;
+			sec_label = SECLABEL_IPV6;
+			break;
+		}
+#endif
+		return send_drop_notify(ctx, sec_label, id, 0, ret,
 					CTX_ACT_DROP, METRIC_EGRESS);
 #else
 		send_trace_notify(ctx, TRACE_TO_STACK, HOST_ID, 0, 0, 0,
@@ -1240,7 +1253,7 @@ handle_srv6(struct __ctx_buff *ctx)
 		if (ep) {
 			dst_sec_identity = ep->sec_identity;
 		} else {
-			dst_sec_identity = WORLD_ID;
+			dst_sec_identity = WORLD_IPV6_ID;
 		}
 
 		if (identity_is_cluster(dst_sec_identity))
@@ -1273,7 +1286,7 @@ handle_srv6(struct __ctx_buff *ctx)
 		if (ep) {
 			dst_sec_identity = ep->sec_identity;
 		} else {
-			dst_sec_identity = WORLD_ID;
+			dst_sec_identity = WORLD_IPV6_ID;
 		}
 
 		if (identity_is_cluster(dst_sec_identity))

bpf/ep_config.h
@@ -25,6 +25,10 @@ DEFINE_U16(LXC_ID, 0x2A);
 #define LXC_ID fetch_u16(LXC_ID)
 DEFINE_U32(SECLABEL, 0xfffff);
 #define SECLABEL fetch_u32(SECLABEL)
+DEFINE_U32(SECLABEL_IPV4, 0xfffff);
+#define SECLABEL_IPV4 fetch_u32(SECLABEL_IPV4)
+DEFINE_U32(SECLABEL_IPV6, 0xfffff);
+#define SECLABEL_IPV6 fetch_u32(SECLABEL_IPV6)
 DEFINE_U32(SECLABEL_NB, 0xfffff);
 #define SECLABEL_NB fetch_u32(SECLABEL_NB)
 

bpf/lib/common.h
@@ -219,6 +219,30 @@ __revalidate_data_pull(struct __ctx_buff *ctx, void **data, void **data_end,
 					l3_off);
 }
 
+static __always_inline __u32 get_tunnel_id(__u32 identity)
+{
+#if defined ENABLE_IPV4 && defined ENABLE_IPV6
+	if (identity == WORLD_IPV4_ID || identity == WORLD_IPV6_ID)
+		return WORLD_ID;
+#endif
+	return identity;
+}
+
+static __always_inline __u32 get_id_from_tunnel_id(__u32 tunnel_id, __u16 proto  __maybe_unused)
+{
+#if defined ENABLE_IPV4 && defined ENABLE_IPV6
+	if (tunnel_id == WORLD_ID) {
+		switch (proto) {
+		case bpf_htons(ETH_P_IP):
+			return WORLD_IPV4_ID;
+		case bpf_htons(ETH_P_IPV6):
+			return WORLD_IPV6_ID;
+		}
+	}
+#endif
+	return tunnel_id;
+}
+
 /* revalidate_data_pull() initializes the provided pointers from the ctx and
  * ensures that the data is pulled in for access. Should be used the first
  * time that the ctx data is accessed, subsequent calls can be made to

bpf/lib/egress_policies.h
@@ -550,17 +550,17 @@ int tail_srv6_encap(struct __ctx_buff *ctx)
 
 	ret = srv6_handling(ctx, vrf_id, &dst_sid);
 	if (ret < 0)
-		return send_drop_notify_error(ctx, SECLABEL, ret, CTX_ACT_DROP,
+		return send_drop_notify_error(ctx, SECLABEL_IPV6, ret, CTX_ACT_DROP,
 					      METRIC_EGRESS);
 
 #ifdef ENABLE_IPV6
 	ret = srv6_refib(ctx, &ext_err);
 	if (ret < 0)
-		return send_drop_notify_ext(ctx, SECLABEL, 0, 0, ret, ext_err,
+		return send_drop_notify_ext(ctx, SECLABEL_IPV6, 0, 0, ret, ext_err,
 					   CTX_ACT_DROP, METRIC_EGRESS);
 #endif
 
-	send_trace_notify(ctx, TRACE_TO_STACK, SECLABEL, 0, 0, 0,
+	send_trace_notify(ctx, TRACE_TO_STACK, SECLABEL_IPV6, 0, 0, 0,
 			  TRACE_REASON_UNKNOWN, 0);
 
 	return ret;
@@ -579,11 +579,11 @@ int tail_srv6_decap(struct __ctx_buff *ctx)
 	if (ret < 0)
 		goto error_drop;
 
-	send_trace_notify(ctx, TRACE_TO_STACK, SECLABEL, 0, 0, 0,
+	send_trace_notify(ctx, TRACE_TO_STACK, SECLABEL_IPV6, 0, 0, 0,
 			  TRACE_REASON_UNKNOWN, 0);
 	return CTX_ACT_OK;
 error_drop:
-		return send_drop_notify_error(ctx, SECLABEL, ret, CTX_ACT_DROP,
+		return send_drop_notify_error(ctx, SECLABEL_IPV6, ret, CTX_ACT_DROP,
 					      METRIC_EGRESS);
 }
 
@@ -594,7 +594,7 @@ int tail_srv6_reply(struct __ctx_buff *ctx)
 
 	ret = srv6_reply(ctx);
 	if (ret < 0)
-		return send_drop_notify_error(ctx, SECLABEL, ret, CTX_ACT_DROP,
+		return send_drop_notify_error(ctx, SECLABEL_IPV6, ret, CTX_ACT_DROP,
 					      METRIC_EGRESS);
 	return CTX_ACT_OK;
 }

bpf/lib/high_scale_ipcache.h
@@ -129,6 +129,21 @@ decapsulate_overlay(struct __ctx_buff *ctx, __u32 *src_id)
 
 	if (ctx_adjust_hroom(ctx, -shrink, BPF_ADJ_ROOM_MAC, ctx_adjust_hroom_flags()))
 		return DROP_INVALID;
+
+#if defined ENABLE_IPV4 && defined ENABLE_IPV6
+	/* If dual-stack mode is enabled then world identity must be specific to its protocol. */
+	if (*src_id == WORLD_ID) {
+		switch (proto) {
+		case bpf_htons(ETH_P_IP):
+			*src_id = WORLD_IPV4_ID;
+			break;
+		case bpf_htons(ETH_P_IPV6):
+			*src_id = WORLD_IPV6_ID;
+			break;
+		}
+		ctx_store_meta(ctx, CB_SRC_LABEL, *src_id);
+	}
+#endif
 	return ctx_redirect(ctx, ENCAP_IFINDEX, BPF_F_INGRESS);
 }
 #endif /* ENABLE_HIGH_SCALE_IPCACHE */

bpf/lib/host_firewall.h
@@ -128,7 +128,7 @@ static __always_inline bool
 ipv6_host_policy_ingress_lookup(struct __ctx_buff *ctx, struct ipv6hdr *ip6,
 				struct ct_buffer6 *ct_buffer)
 {
-	__u32 dst_sec_identity = WORLD_ID;
+	__u32 dst_sec_identity = WORLD_IPV6_ID;
 	struct remote_endpoint_info *info;
 	struct ipv6_ct_tuple *tuple = &ct_buffer->tuple;
 	int hdrlen;
@@ -401,7 +401,7 @@ static __always_inline bool
 ipv4_host_policy_ingress_lookup(struct __ctx_buff *ctx, struct iphdr *ip4,
 				struct ct_buffer4 *ct_buffer)
 {
-	__u32 dst_sec_identity = WORLD_ID;
+	__u32 dst_sec_identity = WORLD_IPV4_ID;
 	struct remote_endpoint_info *info;
 	struct ipv4_ct_tuple *tuple = &ct_buffer->tuple;
 	int l3_off = ETH_HLEN;

commit 7f87fbacd78c5572075d5a50effdf84bf2102373

bpf: Fix VTEP drop check
Commit a94fa56f6713 ("Fix CIDR to World Entity Conversion Bug") seems to
have inadvertently swapped a check for "is not world" to a check for "is
world" in order to drop. This has likely broken the VTEP feature. Fix
it.

Fixes: a94fa56f6713 ("Fix CIDR to World Entity Conversion Bug")
Reported-by: Jan Hugo Prins <jhp@jhprins.org>
Signed-off-by: Joe Stringer <joe@cilium.io>

bpf/bpf_overlay.c
@@ -342,7 +342,7 @@ static __always_inline int handle_ipv4(struct __ctx_buff *ctx,
 			if (!vtep)
 				goto skip_vtep;
 			if (vtep->tunnel_endpoint) {
-				if (identity_is_world_ipv4(*identity))
+				if (!identity_is_world_ipv4(*identity))
 					return DROP_INVALID_VNI;
 			}
 		}