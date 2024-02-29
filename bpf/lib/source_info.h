e1f316d2e9247367cca4927a30c6061cba46fc3c
Timo Beckers <timo@isovalent.com>
2024-02-29 10:43:34 +0100

    bpf,metrics: add line/file information to `cilium-dbg bpf metrics list`
    Fixes #27748
    
    Example output:
    ```
    ~ cilium bpf metrics list
    REASON                    DIRECTION   PACKETS   BYTES    LINE   FILE
    Interface                 INGRESS     151       9368     1092   bpf_host.c
    Interface                 INGRESS     731       139248   659    bpf_overlay.c
    Success                   EGRESS      137       8212     1531   bpf_host.c
    Success                   EGRESS      9         744      1286   bpf_lxc.c
    Success                   INGRESS     11        926      86     l3.h
    Success                   INGRESS     1403      299078   231    trace.h
    Unsupported L3 protocol   EGRESS      6         460      1466   bpf_lxc.c
    ```
    
    ```
    ~ cilium bpf metrics list -o json
      {
        "reason": "Success",
        "direction": "egress",
        "packets": 406587,
        "bytes": 78023315,
        "line": 40,
        "file": "encap.h"
      },
      {
        "reason": "Unsupported L3 protocol",
        "direction": "egress",
        "packets": 167,
        "bytes": 12050,
        "line": 1466,
        "file": "bpf_lxc.c"
      }
    ```
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- bpf/lib/source_info.h
+++ bpf/lib/source_info.h
@@ -35,6 +35,8 @@ __id_for_file(const char *const header_name)
 	_strcase_(2, "bpf_lxc.c");
 	_strcase_(3, "bpf_overlay.c");
 	_strcase_(4, "bpf_xdp.c");
+	_strcase_(5, "bpf_sock.c");
+	_strcase_(6, "bpf_network.c");
 
 	/* header files from bpf/lib/ */
 	_strcase_(101, "arp.h");
@@ -43,6 +45,14 @@ __id_for_file(const char *const header_name)
 	_strcase_(104, "icmp6.h");
 	_strcase_(105, "nodeport.h");
 	_strcase_(106, "lb.h");
+	_strcase_(107, "mcast.h");
+	_strcase_(108, "ipv4.h");
+	_strcase_(109, "conntrack.h");
+	_strcase_(110, "l3.h");
+	_strcase_(111, "trace.h");
+	_strcase_(112, "encap.h");
+
+	/* @@ source files list end */
 
 	return 0;
 }
