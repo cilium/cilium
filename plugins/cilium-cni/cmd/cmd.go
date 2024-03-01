50522a741eb9703485236beb6df9b03f0f30e3e0
Tamilmani <tamanoha@microsoft.com>
2024-03-01 20:44:09 +0000

    fix: Delegated ipam not configure ipv6 in ipv6 disabled case
    [ upstream commit 034aee74f57905898741b89549430c409bef99e3 ]
    
    Delegated ipam returns ipv6 address to cilium cni even if ipv6 disabled
    in cilium agent config. In this scenario, ipv6 node addressing is not
    set and its causing cilium cni to crash if delegated ipam returns ipv6
    but disabled in cilium agent.
    
    Signed-off-by: Tamilmani <tamanoha@microsoft.com>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- plugins/cilium-cni/cmd/cmd.go
+++ plugins/cilium-cni/cmd/cmd.go
@@ -102,11 +102,11 @@ func ipv6IsEnabled(ipam *models.IPAMResponse) bool {
 		return false
 	}
 
-	if ipam.HostAddressing != nil && ipam.HostAddressing.IPV6 != nil {
-		return ipam.HostAddressing.IPV6.Enabled
+	if ipam.HostAddressing == nil || ipam.HostAddressing.IPV6 == nil {
+		return false
 	}
 
-	return true
+	return ipam.HostAddressing.IPV6.Enabled
 }
 
 func ipv4IsEnabled(ipam *models.IPAMResponse) bool {
@@ -114,11 +114,11 @@ func ipv4IsEnabled(ipam *models.IPAMResponse) bool {
 		return false
 	}
 
-	if ipam.HostAddressing != nil && ipam.HostAddressing.IPV4 != nil {
-		return ipam.HostAddressing.IPV4.Enabled
+	if ipam.HostAddressing == nil || ipam.HostAddressing.IPV4 == nil {
+		return false
 	}
 
-	return true
+	return ipam.HostAddressing.IPV4.Enabled
 }
 
 func getConfigFromCiliumAgent(client *client.Client) (*models.DaemonConfigurationStatus, error) {
@@ -203,7 +203,8 @@ func allocateIPsWithDelegatedPlugin(
 		if ipv4 := ipNet.IP.To4(); ipv4 != nil {
 			ipam.Address.IPV4 = ipNet.String()
 			ipam.IPV4 = &models.IPAMAddressResponse{IP: ipv4.String()}
-		} else {
+		} else if conf.Addressing.IPV6 != nil {
+			// assign ipam ipv6 address only if agent ipv6 config is enabled
 			ipam.Address.IPV6 = ipNet.String()
 			ipam.IPV6 = &models.IPAMAddressResponse{IP: ipNet.IP.String()}
 		}
@@ -554,7 +555,7 @@ func (cmd *Cmd) Add(args *skel.CmdArgs) (err error) {
 			ipConfig *cniTypesV1.IPConfig
 			routes   []*cniTypes.Route
 		)
-		if ipv6IsEnabled(ipam) {
+		if ipv6IsEnabled(ipam) && conf.Addressing.IPV6 != nil {
 			ep.Addressing.IPV6 = ipam.Address.IPV6
 			ep.Addressing.IPV6PoolName = ipam.Address.IPV6PoolName
 			ep.Addressing.IPV6ExpirationUUID = ipam.IPV6.ExpirationUUID
@@ -569,7 +570,7 @@ func (cmd *Cmd) Add(args *skel.CmdArgs) (err error) {
 			res.Routes = append(res.Routes, routes...)
 		}
 
-		if ipv4IsEnabled(ipam) {
+		if ipv4IsEnabled(ipam) && conf.Addressing.IPV4 != nil {
 			ep.Addressing.IPV4 = ipam.Address.IPV4
 			ep.Addressing.IPV4PoolName = ipam.Address.IPV4PoolName
 			ep.Addressing.IPV4ExpirationUUID = ipam.IPV4.ExpirationUUID
