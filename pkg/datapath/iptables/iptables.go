d720aae817d7d09ffdf523e41a5bff8cb644c22b
Julian Wiedmann <jwi@isovalent.com>
2024-03-01 09:21:00 +0200

    datapath: mark to-overlay traffic
    [ upstream commit 2860ded6cc37bc2d63e6c3bce2ec8aceddbf9fe1 ]
    
    [ backporter's notes: minor conflict in bpf_overlay, as handle_nat_fwd()
      didn't have the trace_ctx / ext_err parameters ]
    
    To make smarter decisions at the native-device level (in to-netdev), mark
    traffic that is created by cilium's overlay interface.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- pkg/datapath/iptables/iptables.go
+++ pkg/datapath/iptables/iptables.go
@@ -1407,6 +1407,7 @@ func (m *IptablesManager) installHostTrafficMarkRule(prog iptablesInterface) err
 	// originated from the host.
 	matchFromIPSecEncrypt := fmt.Sprintf("%#08x/%#08x", linux_defaults.RouteMarkDecrypt, linux_defaults.RouteMarkMask)
 	matchFromIPSecDecrypt := fmt.Sprintf("%#08x/%#08x", linux_defaults.RouteMarkEncrypt, linux_defaults.RouteMarkMask)
+	matchOverlay := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkOverlay, linux_defaults.MagicMarkHostMask)
 	matchFromProxy := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIsProxy, linux_defaults.MagicMarkProxyMask)
 	matchFromProxyEPID := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIsProxyEPID, linux_defaults.MagicMarkProxyMask)
 	matchFromDNSProxy := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIdentity, linux_defaults.MagicMarkHostMask)
@@ -1417,6 +1418,7 @@ func (m *IptablesManager) installHostTrafficMarkRule(prog iptablesInterface) err
 		"-A", ciliumOutputChain,
 		"-m", "mark", "!", "--mark", matchFromIPSecDecrypt, // Don't match ipsec traffic
 		"-m", "mark", "!", "--mark", matchFromIPSecEncrypt, // Don't match ipsec traffic
+		"-m", "mark", "!", "--mark", matchOverlay, // Don't match Cilium's overlay traffic
 		"-m", "mark", "!", "--mark", matchFromProxy, // Don't match proxy traffic
 		"-m", "mark", "!", "--mark", matchFromProxyEPID, // Don't match proxy traffic
 		"-m", "mark", "!", "--mark", matchFromDNSProxy, // Don't match DNS proxy egress traffic
