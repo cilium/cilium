cilium/cilium

commit 490ecc5016b5d22ae55a65f75b49681534486792

bpf: nodeport: don't forward local CIDR identities
Signed-off-by: Gilberto Bertin <jibi@cilium.io>

bpf/lib/nodeport.h
@@ -2920,6 +2920,15 @@ skip_service_lookup:
 			src_sec_identity = WORLD_IPV4_ID;
 #endif
 
+		 /* Before forwarding the identity, make sure it's not a CIDR
+		  * identity, as these are __u32 values, but transporting them
+		  * via the VNI field in the VXLAN / Geneve header allows for
+		  * only 24 bits.
+		  */
+
+		if (identity_is_cidr_range(src_sec_identity))
+			return DROP_INVALID_IDENTITY;
+
 		/* lookup with SCOPE_FORWARD: */
 		__ipv4_ct_tuple_reverse(&tuple);
 

bpf/lib/identity.h
@@ -110,6 +110,15 @@ static __always_inline bool identity_is_world_ipv6(__u32 identity)
 }
 
 /**
+ * identity_is_cidr_range is used to determine whether an identity is assigned
+ * to a CIDR range.
+ */
+static __always_inline bool identity_is_cidr_range(__u32 identity)
+{
+	return identity_in_range(identity, CIDR_IDENTITY_RANGE_START, CIDR_IDENTITY_RANGE_END);
+}
+
+/**
  * identity_is_cluster is used to determine whether an identity is assigned to
  * an entity inside the cluster.
  *
@@ -139,8 +148,7 @@ static __always_inline bool identity_is_cluster(__u32 identity)
 		return false;
 #endif
 
-	if (identity_in_range(identity, CIDR_IDENTITY_RANGE_START,
-			      CIDR_IDENTITY_RANGE_END))
+	if (identity_is_cidr_range(identity))
 		return false;
 
 	return true;

commit 7bd98287182e7ee3df4b44103a6a91300d51da1a

bpf: nodeport: fix check to forward identity in nodeport_lb4
as the original intent of this logic was to avoid forwarding local
identities, fix the check to ensure none of the 8MSB of the identity are
set before forwarding the identity

Fixes: 490ecc5016b ("bpf: nodeport: don't forward local CIDR identities")
Suggested-by: Joe Stringer <joe@cilium.io>
Signed-off-by: Gilberto Bertin <jibi@cilium.io>

bpf/lib/nodeport.h
@@ -2927,13 +2927,10 @@ skip_service_lookup:
 			src_sec_identity = WORLD_IPV4_ID;
 #endif
 
-		 /* Before forwarding the identity, make sure it's not a CIDR
-		  * identity, as these are __u32 values, but transporting them
-		  * via the VNI field in the VXLAN / Geneve header allows for
-		  * only 24 bits.
+		 /* Before forwarding the identity, make sure it's not local,
+		  * as in that case the next hop would't understand it.
 		  */
-
-		if (identity_is_cidr_range(src_sec_identity))
+		if (identity_is_local(src_sec_identity))
 			return DROP_INVALID_IDENTITY;
 
 		/* lookup with SCOPE_FORWARD: */