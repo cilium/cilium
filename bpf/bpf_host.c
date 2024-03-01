cilium/cilium

commit cf28a8a10bb4f61a337f299cb3861bd173b27303

bpf: Reply NA when recv ND for local IPv6 endpoints
When receiving ICMPv6 ND message from native NIC, lookup local endpoints
for target IPv6 address. If local endpoint was found, reply NA message
via the same NIC.

This enables pod's IPv6 address discoverable on the subnet to which the
node is connected to.

Torwards #10935
Signed-off-by: Yongkun Gui <ygui@google.com>

bpf/lib/icmp6.h
@@ -9,6 +9,7 @@
 #include "common.h"
 #include "eth.h"
 #include "drop.h"
+#include "eps.h"
 
 #define ICMP6_TYPE_OFFSET (sizeof(struct ipv6hdr) + offsetof(struct icmp6hdr, icmp6_type))
 #define ICMP6_CSUM_OFFSET (sizeof(struct ipv6hdr) + offsetof(struct icmp6hdr, icmp6_cksum))
@@ -156,8 +157,17 @@ static __always_inline int icmp6_send_echo_reply(struct __ctx_buff *ctx,
 	return DROP_MISSED_TAIL_CALL;
 }
 
+/*
+ * send_icmp6_ndisc_adv
+ * @ctx:	socket buffer
+ * @nh_off:	offset to the IPv6 header
+ * @mac:	device mac address
+ * @to_router:	ndisc is sent to router, otherwise ndisc is sent to an endpoint.
+ *
+ * Send an ICMPv6 nadv reply in return to an ICMPv6 ndisc.
+ */
 static __always_inline int send_icmp6_ndisc_adv(struct __ctx_buff *ctx,
-						int nh_off, union macaddr *mac)
+						int nh_off, union macaddr *mac, bool to_router)
 {
 	struct icmp6hdr icmp6hdr __align_stack_8 = {}, icmp6hdr_old __align_stack_8;
 	__u8 opts[8], opts_old[8];
@@ -173,9 +183,16 @@ static __always_inline int send_icmp6_ndisc_adv(struct __ctx_buff *ctx,
 	icmp6hdr.icmp6_code = 0;
 	icmp6hdr.icmp6_cksum = icmp6hdr_old.icmp6_cksum;
 	icmp6hdr.icmp6_dataun.un_data32[0] = 0;
-	icmp6hdr.icmp6_router = 1;
-	icmp6hdr.icmp6_solicited = 1;
-	icmp6hdr.icmp6_override = 0;
+
+	if (to_router) {
+		icmp6hdr.icmp6_router = 1;
+		icmp6hdr.icmp6_solicited = 1;
+		icmp6hdr.icmp6_override = 0;
+	} else {
+		icmp6hdr.icmp6_router = 0;
+		icmp6hdr.icmp6_solicited = 1;
+		icmp6hdr.icmp6_override = 1;
+	}
 
 	if (ctx_store_bytes(ctx, nh_off + sizeof(struct ipv6hdr), &icmp6hdr,
 			    sizeof(icmp6hdr), 0) < 0)
@@ -349,6 +366,7 @@ static __always_inline int icmp6_send_time_exceeded(struct __ctx_buff *ctx,
 static __always_inline int __icmp6_handle_ns(struct __ctx_buff *ctx, int nh_off)
 {
 	union v6addr target, router;
+	struct endpoint_info *ep;
 
 	if (ctx_load_bytes(ctx, nh_off + ICMP6_ND_TARGET_OFFSET, target.addr,
 			   sizeof(((struct ipv6hdr *)NULL)->saddr)) < 0)
@@ -357,10 +375,18 @@ static __always_inline int __icmp6_handle_ns(struct __ctx_buff *ctx, int nh_off)
 	cilium_dbg(ctx, DBG_ICMP6_NS, target.p3, target.p4);
 
 	BPF_V6(router, ROUTER_IP);
+
 	if (ipv6_addrcmp(&target, &router) == 0) {
 		union macaddr router_mac = NODE_MAC;
 
-		return send_icmp6_ndisc_adv(ctx, nh_off, &router_mac);
+		return send_icmp6_ndisc_adv(ctx, nh_off, &router_mac, true);
+	}
+
+	ep = lookup_ip6_endpoint_from_daddr(&target);
+	if (ep) {
+		union macaddr router_mac = NODE_MAC;
+
+		return send_icmp6_ndisc_adv(ctx, nh_off, &router_mac, false);
 	}
 
 	/* Unknown target address, drop */
@@ -430,54 +456,58 @@ icmp6_host_handle(struct __ctx_buff *ctx __maybe_unused)
 {
 	__u8 type __maybe_unused;
 
+	type = icmp6_load_type(ctx, ETH_HLEN);
+	if (type == ICMP6_NS_MSG_TYPE)
+		return icmp6_handle_ns(ctx, ETH_HLEN, METRIC_INGRESS);
+
 #ifdef ENABLE_HOST_FIREWALL
 	/* When the host firewall is enabled, we drop and allow ICMPv6 messages
 	 * according to RFC4890, except for echo request and reply messages which
 	 * are handled by host policies and can be dropped.
-	 * |          ICMPv6 Message         |   Action     | Type |
-	 * |---------------------------------|--------------|------|
-	 * |          ICMPv6-unreach         |  CTX_ACT_OK  |   1  |
-	 * |          ICMPv6-too-big         |  CTX_ACT_OK  |   2  |
-	 * |           ICMPv6-timed          |  CTX_ACT_OK  |   3  |
-	 * |         ICMPv6-parameter        |  CTX_ACT_OK  |   4  |
-	 * |    ICMPv6-err-private-exp-100   | CTX_ACT_DROP |  100 |
-	 * |    ICMPv6-err-private-exp-101   | CTX_ACT_DROP |  101 |
-	 * |       ICMPv6-err-expansion      | CTX_ACT_DROP |  127 |
-	 * |       ICMPv6-echo-message       |   Firewall   |  128 |
-	 * |        ICMPv6-echo-reply        |   Firewall   |  129 |
-	 * |      ICMPv6-mult-list-query     |  CTX_ACT_OK  |  130 |
-	 * |      ICMPv6-mult-list-report    |  CTX_ACT_OK  |  131 |
-	 * |      ICMPv6-mult-list-done      |  CTX_ACT_OK  |  132 |
-	 * |      ICMPv6-router-solici       |  CTX_ACT_OK  |  133 |
-	 * |      ICMPv6-router-advert       |  CTX_ACT_OK  |  134 |
-	 * |     ICMPv6-neighbor-solicit     |  CTX_ACT_OK  |  135 |
-	 * |      ICMPv6-neighbor-advert     |  CTX_ACT_OK  |  136 |
-	 * |     ICMPv6-redirect-message     | CTX_ACT_DROP |  137 |
-	 * |      ICMPv6-router-renumber     |  CTX_ACT_OK  |  138 |
-	 * |      ICMPv6-node-info-query     | CTX_ACT_DROP |  139 |
-	 * |     ICMPv6-node-info-response   | CTX_ACT_DROP |  140 |
-	 * |   ICMPv6-inv-neighbor-solicit   |  CTX_ACT_OK  |  141 |
-	 * |    ICMPv6-inv-neighbor-advert   |  CTX_ACT_OK  |  142 |
-	 * |    ICMPv6-mult-list-report-v2   |  CTX_ACT_OK  |  143 |
-	 * | ICMPv6-home-agent-disco-request | CTX_ACT_DROP |  144 |
-	 * |  ICMPv6-home-agent-disco-reply  | CTX_ACT_DROP |  145 |
-	 * |      ICMPv6-mobile-solicit      | CTX_ACT_DROP |  146 |
-	 * |      ICMPv6-mobile-advert       | CTX_ACT_DROP |  147 |
-	 * |      ICMPv6-send-solicit        |  CTX_ACT_OK  |  148 |
-	 * |       ICMPv6-send-advert        |  CTX_ACT_OK  |  149 |
-	 * |       ICMPv6-mobile-exp         | CTX_ACT_DROP |  150 |
-	 * |    ICMPv6-mult-router-advert    |  CTX_ACT_OK  |  151 |
-	 * |    ICMPv6-mult-router-solicit   |  CTX_ACT_OK  |  152 |
-	 * |     ICMPv6-mult-router-term     |  CTX_ACT_OK  |  153 |
-	 * |         ICMPv6-FMIPv6           | CTX_ACT_DROP |  154 |
-	 * |       ICMPv6-rpl-control        | CTX_ACT_DROP |  155 |
-	 * |   ICMPv6-info-private-exp-200   | CTX_ACT_DROP |  200 |
-	 * |   ICMPv6-info-private-exp-201   | CTX_ACT_DROP |  201 |
-	 * |      ICMPv6-info-expansion      | CTX_ACT_DROP |  255 |
-	 * |       ICMPv6-unallocated        | CTX_ACT_DROP |      |
-	 * |       ICMPv6-unassigned         | CTX_ACT_DROP |      |
+	 * |          ICMPv6 Message         |     Action      | Type |
+	 * |---------------------------------|-----------------|------|
+	 * |          ICMPv6-unreach         |   CTX_ACT_OK    |   1  |
+	 * |          ICMPv6-too-big         |   CTX_ACT_OK    |   2  |
+	 * |           ICMPv6-timed          |   CTX_ACT_OK    |   3  |
+	 * |         ICMPv6-parameter        |   CTX_ACT_OK    |   4  |
+	 * |    ICMPv6-err-private-exp-100   |  CTX_ACT_DROP   |  100 |
+	 * |    ICMPv6-err-private-exp-101   |  CTX_ACT_DROP   |  101 |
+	 * |       ICMPv6-err-expansion      |  CTX_ACT_DROP   |  127 |
+	 * |       ICMPv6-echo-message       |    Firewall     |  128 |
+	 * |        ICMPv6-echo-reply        |    Firewall     |  129 |
+	 * |      ICMPv6-mult-list-query     |   CTX_ACT_OK    |  130 |
+	 * |      ICMPv6-mult-list-report    |   CTX_ACT_OK    |  131 |
+	 * |      ICMPv6-mult-list-done      |   CTX_ACT_OK    |  132 |
+	 * |      ICMPv6-router-solici       |   CTX_ACT_OK    |  133 |
+	 * |      ICMPv6-router-advert       |   CTX_ACT_OK    |  134 |
+	 * |     ICMPv6-neighbor-solicit     | icmp6_handle_ns |  135 |
+	 * |      ICMPv6-neighbor-advert     |   CTX_ACT_OK    |  136 |
+	 * |     ICMPv6-redirect-message     |  CTX_ACT_DROP   |  137 |
+	 * |      ICMPv6-router-renumber     |   CTX_ACT_OK    |  138 |
+	 * |      ICMPv6-node-info-query     |  CTX_ACT_DROP   |  139 |
+	 * |     ICMPv6-node-info-response   |  CTX_ACT_DROP   |  140 |
+	 * |   ICMPv6-inv-neighbor-solicit   |   CTX_ACT_OK    |  141 |
+	 * |    ICMPv6-inv-neighbor-advert   |   CTX_ACT_OK    |  142 |
+	 * |    ICMPv6-mult-list-report-v2   |   CTX_ACT_OK    |  143 |
+	 * | ICMPv6-home-agent-disco-request |  CTX_ACT_DROP   |  144 |
+	 * |  ICMPv6-home-agent-disco-reply  |  CTX_ACT_DROP   |  145 |
+	 * |      ICMPv6-mobile-solicit      |  CTX_ACT_DROP   |  146 |
+	 * |      ICMPv6-mobile-advert       |  CTX_ACT_DROP   |  147 |
+	 * |      ICMPv6-send-solicit        |   CTX_ACT_OK    |  148 |
+	 * |       ICMPv6-send-advert        |   CTX_ACT_OK    |  149 |
+	 * |       ICMPv6-mobile-exp         |  CTX_ACT_DROP   |  150 |
+	 * |    ICMPv6-mult-router-advert    |   CTX_ACT_OK    |  151 |
+	 * |    ICMPv6-mult-router-solicit   |   CTX_ACT_OK    |  152 |
+	 * |     ICMPv6-mult-router-term     |   CTX_ACT_OK    |  153 |
+	 * |         ICMPv6-FMIPv6           |  CTX_ACT_DROP   |  154 |
+	 * |       ICMPv6-rpl-control        |  CTX_ACT_DROP   |  155 |
+	 * |   ICMPv6-info-private-exp-200   |  CTX_ACT_DROP   |  200 |
+	 * |   ICMPv6-info-private-exp-201   |  CTX_ACT_DROP   |  201 |
+	 * |      ICMPv6-info-expansion      |  CTX_ACT_DROP   |  255 |
+	 * |       ICMPv6-unallocated        |  CTX_ACT_DROP   |      |
+	 * |       ICMPv6-unassigned         |  CTX_ACT_DROP   |      |
 	 */
-	type = icmp6_load_type(ctx, ETH_HLEN);
+
 	if (type == ICMP6_ECHO_REQUEST_MSG_TYPE || type == ICMP6_ECHO_REPLY_MSG_TYPE)
 		/* Decision is deferred to the host policies. */
 		return CTX_ACT_OK;

bpf/lib/eps.h
@@ -10,14 +10,20 @@
 #include "maps.h"
 
 static __always_inline __maybe_unused struct endpoint_info *
-lookup_ip6_endpoint(struct ipv6hdr *ip6)
+lookup_ip6_endpoint_from_daddr(const union v6addr *ip6)
 {
 	struct endpoint_key key = {};
 
-	key.ip6 = *((union v6addr *) &ip6->daddr);
+	key.ip6 = *ip6;
 	key.family = ENDPOINT_KEY_IPV6;
 
 	return map_lookup_elem(&ENDPOINTS_MAP, &key);
+}
+
+static __always_inline __maybe_unused struct endpoint_info *
+lookup_ip6_endpoint(struct ipv6hdr *ip6)
+{
+	return lookup_ip6_endpoint_from_daddr((union v6addr *)&ip6->daddr);
 }
 
 static __always_inline __maybe_unused struct endpoint_info *

commit f1e58bc61514c8dcbe07fe96f02dd5c1d9a5609a

bpf: Re-introduce ICMPv6 NS responder on from-netdev
This reverts commit 658071414ca4606e537bc4bbb37dcae5e18cd7dc, to fix
the breakage of "IPv6 NS responder for pod" introduced by
https://github.com/cilium/cilium/pull/12086 (bpf: Reply NA when recv ND for local IPv6 endpoints).

658071414ca4606e537bc4bbb37dcae5e18cd7dc was merged to solve https://github.com/cilium/cilium/issues/14509.
To not revive #14509, this commit also passes through ICMPv6 NS if the
target is native node IP (eth0's addr). By letting stack take care of
those NS-for-node-IP packets, we managed to:

1. Solve #14509 again, but in a way keeping NS responder. The cause of
   #14509 was NS responder always generates ND whose source IP is
   "router_ip" (cilium_internal_ip) rather than "node_ip". Once we pass
   those NS-for-node-IP packets to stack, the ND response would
   naturally have "node_ip" as source.

2. Avoid the fib_lookup failure mentioned at https://github.com/cilium/cilium/pull/30837#issuecomment-1960897445.

icmp6_host_handle() also has a new parameter `handle_ns` to control if
we want NS responder to be active. If it is called from `to-netdev` code
path, handle_ns is set to false. This is suggested by julianwiedmann.

Signed-off-by: Zhichuan Liang <gray.liang@isovalent.com>

bpf/bpf_host.c
@@ -144,31 +144,28 @@ handle_ipv6(struct __ctx_buff *ctx, __u32 secctx __maybe_unused,
 #ifdef ENABLE_HOST_FIREWALL
 	struct ct_buffer6 ct_buffer = {};
 	bool need_hostfw = false;
-	__u8 nexthdr;
-	int hdrlen;
 	bool is_host_id = false;
 #endif /* ENABLE_HOST_FIREWALL */
 	void *data, *data_end;
 	struct ipv6hdr *ip6;
-	int __maybe_unused ret;
+	int ret, hdrlen;
+	__u8 nexthdr;
 
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
 		return DROP_INVALID;
 
-#ifdef ENABLE_HOST_FIREWALL
 	nexthdr = ip6->nexthdr;
 	hdrlen = ipv6_hdrlen(ctx, &nexthdr);
 	if (hdrlen < 0)
 		return hdrlen;
 
 	if (likely(nexthdr == IPPROTO_ICMPV6)) {
-		ret = icmp6_host_handle(ctx, ETH_HLEN + hdrlen);
+		ret = icmp6_host_handle(ctx, ETH_HLEN + hdrlen, ext_err, !from_host);
 		if (ret == SKIP_HOST_FIREWALL)
 			goto skip_host_firewall;
 		if (IS_ERR(ret))
 			return ret;
 	}
-#endif /* ENABLE_HOST_FIREWALL */
 
 #ifdef ENABLE_NODEPORT
 	if (!from_host) {
@@ -213,8 +210,10 @@ handle_ipv6(struct __ctx_buff *ctx, __u32 secctx __maybe_unused,
 		if (map_update_elem(&CT_TAIL_CALL_BUFFER6, &zero, &ct_buffer, 0) < 0)
 			return DROP_INVALID_TC_BUFFER;
 	}
+#endif /* ENABLE_HOST_FIREWALL */
 
 skip_host_firewall:
+#ifdef ENABLE_HOST_FIREWALL
 	ctx_store_meta(ctx, CB_FROM_HOST,
 		       (need_hostfw ? FROM_HOST_FLAG_NEED_HOSTFW : 0) |
 		       (is_host_id ? FROM_HOST_FLAG_HOST_ID : 0));
@@ -497,7 +496,7 @@ handle_to_netdev_ipv6(struct __ctx_buff *ctx, struct trace_ctx *trace, __s8 *ext
 		return hdrlen;
 
 	if (likely(nexthdr == IPPROTO_ICMPV6)) {
-		ret = icmp6_host_handle(ctx, ETH_HLEN + hdrlen);
+		ret = icmp6_host_handle(ctx, ETH_HLEN + hdrlen, ext_err, false);
 		if (ret == SKIP_HOST_FIREWALL)
 			return CTX_ACT_OK;
 		if (IS_ERR(ret))

bpf/lib/icmp6.h
@@ -299,6 +299,7 @@ static __always_inline int __icmp6_handle_ns(struct __ctx_buff *ctx, int nh_off)
 {
 	union v6addr target, router;
 	struct endpoint_info *ep;
+	union macaddr router_mac = NODE_MAC;
 
 	if (ctx_load_bytes(ctx, nh_off + ICMP6_ND_TARGET_OFFSET, target.addr,
 			   sizeof(((struct ipv6hdr *)NULL)->saddr)) < 0)
@@ -309,15 +310,27 @@ static __always_inline int __icmp6_handle_ns(struct __ctx_buff *ctx, int nh_off)
 	BPF_V6(router, ROUTER_IP);
 
 	if (ipv6_addr_equals(&target, &router)) {
-		union macaddr router_mac = NODE_MAC;
 
 		return send_icmp6_ndisc_adv(ctx, nh_off, &router_mac, true);
 	}
 
 	ep = __lookup_ip6_endpoint(&target);
 	if (ep) {
-		union macaddr router_mac = NODE_MAC;
-
+		if (ep->flags & ENDPOINT_F_HOST) {
+			/* Target must be a node_ip, because of ENDPOINT_F_HOST flag
+			 * and target != router_ip.
+			 *
+			 * We pass these packets to stack to make sure:
+			 *
+			 * 1. The response NA has node IP as source address instead of
+			 * router IP, to address https://github.com/cilium/cilium/issues/14509.
+			 *
+			 * 2. Kernel stack can record a neighbor entry for the
+			 * source IP, to avoid bpf_fib_lookup failure as mentioned at
+			 * https://github.com/cilium/cilium/pull/30837#issuecomment-1960897445.
+			 */
+			return CTX_ACT_OK;
+		}
 		return send_icmp6_ndisc_adv(ctx, nh_off, &router_mac, false);
 	}
 
@@ -392,13 +405,17 @@ static __always_inline int icmp6_ndp_handle(struct __ctx_buff *ctx, int nh_off,
 }
 
 static __always_inline int
-icmp6_host_handle(struct __ctx_buff *ctx, int l4_off)
+icmp6_host_handle(struct __ctx_buff *ctx, int l4_off, __s8 *ext_err, bool handle_ns)
 {
 	__u8 type;
 
 	if (icmp6_load_type(ctx, l4_off, &type) < 0)
 		return DROP_INVALID;
 
+	if (type == ICMP6_NS_MSG_TYPE && handle_ns)
+		return icmp6_handle_ns(ctx, ETH_HLEN, METRIC_INGRESS, ext_err);
+
+#ifdef ENABLE_HOST_FIREWALL
 	/* When the host firewall is enabled, we drop and allow ICMPv6 messages
 	 * according to RFC4890, except for echo request and reply messages which
 	 * are handled by host policies and can be dropped.
@@ -418,7 +435,7 @@ icmp6_host_handle(struct __ctx_buff *ctx, int l4_off)
 	 * |      ICMPv6-mult-list-done      |   CTX_ACT_OK    |  132 |
 	 * |      ICMPv6-router-solici       |   CTX_ACT_OK    |  133 |
 	 * |      ICMPv6-router-advert       |   CTX_ACT_OK    |  134 |
-	 * |     ICMPv6-neighbor-solicit     |   CTX_ACT_OK    |  135 |
+	 * |     ICMPv6-neighbor-solicit     | icmp6_handle_ns |  135 |
 	 * |      ICMPv6-neighbor-advert     |   CTX_ACT_OK    |  136 |
 	 * |     ICMPv6-redirect-message     |  CTX_ACT_DROP   |  137 |
 	 * |      ICMPv6-router-renumber     |   CTX_ACT_OK    |  138 |
@@ -460,6 +477,9 @@ icmp6_host_handle(struct __ctx_buff *ctx, int l4_off)
 		(ICMP6_MULT_RA_MSG_TYPE <= type && type <= ICMP6_MULT_RT_MSG_TYPE))
 		return SKIP_HOST_FIREWALL;
 	return DROP_FORBIDDEN_ICMP6;
+#else
+	return CTX_ACT_OK;
+#endif /* ENABLE_HOST_FIREWALL */
 }
 
 #endif