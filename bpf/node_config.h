8503f967ff77e6457c8457bf820eafc96e21d6cd
Louis DeLosSantos <louis.delos@isovalent.com>
2024-03-04 17:36:59 -0500

    datapath: add EncryptedOverlayID reserved ID 11
    This commit adds a new reserved security identity for signaling overlay
    traffic which must be IPSec encrypted.
    
    When the eBPF datapath encounters an egress packet with this security
    identity in an overlay header (currently only VXLan supported) it will
    subject the packet to IPSec encryption and rewrite the overlay header
    with the correct security identity before the packet leaves the host.
    
    Therefore, this identity should NEVER be seen on traffic ingress or
    egress the node from the network.
    
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- bpf/node_config.h
+++ bpf/node_config.h
@@ -62,6 +62,12 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 #define LOCAL_NODE_ID 6
 #define REMOTE_NODE_ID 6
 #define KUBE_APISERVER_NODE_ID 7
+/* This identity should never be seen on ingress or egress traffic to/from a
+ * node.
+ * It signals that the skb is overlay traffic that must be IPSec encrypted
+ * before it leaves the host.
+ */
+#define ENCRYPTED_OVERLAY_ID 11
 #define HOST_IFINDEX_MAC { .addr = { 0xce, 0x72, 0xa7, 0x03, 0x88, 0x56 } }
 #define NODEPORT_PORT_MIN 30000
 #define NODEPORT_PORT_MAX 32767
