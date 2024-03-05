cilium/cilium

commit c49ef45399fc998ceb306b8dd16a403102a671d0

metrics: Modularize daemon metrics registry
This commit modularizes the metrics package. We now have a registry
cell which collects all metrics exposed via `cell.Metric` and registers
them with an internal prometheus registry. This registry operates
independently now, the HTTP server is part of its lifecycle and the
daemon and daemon config are no longer involved directly.

We still have some metric types which implement dynamic registration
and logic external to the metrics package. The global functions now
await a promise to the Registry so they still work until we can replace/
refactor these external dynamic metrics.

All of the metrics defined in the metrics package have been transformed
into `metrics/metric` types and are make available via a Metrics cell
called `LegacyMetics`. This cell will also assign the individual metrics
to the existing global variables so they can be used until the callsites
can be adjusted to take `LegacyMetrics` as dependency or to split off
some of the metrics contained inside.

Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

pkg/metrics/registry.go
@@ -1,0 +1,249 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package metrics
+
+import (
+	"errors"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	metricpkg "github.com/cilium/cilium/pkg/metrics/metric"
+	"github.com/cilium/cilium/pkg/option"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+)
+
+var defaultRegistryConfig = RegistryConfig{
+	PrometheusServeAddr: ":9962",
+}
+
+type RegistryConfig struct {
+	// PrometheusServeAddr IP:Port on which to serve prometheus metrics (pass ":Port" to bind on all interfaces, "" is off)
+	PrometheusServeAddr string
+	// This is a list of metrics to be enabled or disabled, format is `+`/`-` + `{metric name}`
+	Metrics []string
+}
+
+func (rc RegistryConfig) Flags(flags *pflag.FlagSet) {
+	flags.String("prometheus-serve-addr", rc.PrometheusServeAddr, "IP:Port on which to serve prometheus metrics (pass \":Port\" to bind on all interfaces, \"\" is off)")
+	flags.StringSlice("metrics", rc.Metrics, "Metrics that should be enabled or disabled from the default metric list. (+metric_foo to enable metric_foo, -metric_bar to disable metric_bar)")
+}
+
+// RegistryParams are the parameters needed to construct a Registry
+type RegistryParams struct {
+	cell.In
+
+	Logger     logrus.FieldLogger
+	Shutdowner hive.Shutdowner
+	Lifecycle  hive.Lifecycle
+
+	AutoMetrics []metricpkg.WithMetadata `group:"hive-metrics"`
+	Config      RegistryConfig
+
+	DaemonConfig *option.DaemonConfig
+}
+
+// Registry is a cell around a prometheus registry. This registry starts an HTTP server as part of its lifecycle
+// on which all enabled metrics will be available. A reference to this registry can also be used to dynamically
+// register or unregister `prometheus.Collector`s.
+type Registry struct {
+	inner *prometheus.Registry
+
+	params RegistryParams
+}
+
+func NewRegistry(params RegistryParams) *Registry {
+	reg := &Registry{
+		params: params,
+	}
+
+	reg.Reinitialize()
+
+	// Resolve the global registry variable for as long as we still have global functions
+	registryResolver.Resolve(reg)
+
+	if params.Config.PrometheusServeAddr != "" {
+		// The Handler function provides a default handler to expose metrics
+		// via an HTTP server. "/metrics" is the usual endpoint for that.
+		mux := http.NewServeMux()
+		mux.Handle("/metrics", promhttp.HandlerFor(reg.inner, promhttp.HandlerOpts{}))
+		srv := http.Server{
+			Addr:    params.Config.PrometheusServeAddr,
+			Handler: mux,
+		}
+
+		params.Lifecycle.Append(hive.Hook{
+			OnStart: func(hc hive.HookContext) error {
+				go func() {
+					params.Logger.Infof("Serving prometheus metrics on %s", params.Config.PrometheusServeAddr)
+					err := srv.ListenAndServe()
+					if err != nil && !errors.Is(err, http.ErrServerClosed) {
+						params.Shutdowner.Shutdown(hive.ShutdownWithError(err))
+					}
+				}()
+				return nil
+			},
+			OnStop: func(hc hive.HookContext) error {
+				return srv.Shutdown(hc)
+			},
+		})
+	}
+
+	return reg
+}
+
+// Register registers a collector
+func (r *Registry) Register(c prometheus.Collector) error {
+	return r.inner.Register(c)
+}
+
+// Unregister unregisters a collector
+func (r *Registry) Unregister(c prometheus.Collector) bool {
+	return r.inner.Unregister(c)
+}
+
+// goCustomCollectorsRX tracks enabled go runtime metrics.
+var goCustomCollectorsRX = regexp.MustCompile(`^/sched/latencies:seconds`)
+
+// Reinitialize creates a new internal registry and re-registers metrics to it.
+func (r *Registry) Reinitialize() {
+	r.inner = prometheus.NewPedanticRegistry()
+
+	// Default metrics which can't be disabled.
+	r.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{Namespace: Namespace}))
+	r.MustRegister(collectors.NewGoCollector(
+		collectors.WithGoCollectorRuntimeMetrics(
+			collectors.GoRuntimeMetricsRule{Matcher: goCustomCollectorsRX},
+		)))
+	r.MustRegister(newStatusCollector())
+	r.MustRegister(newbpfCollector())
+
+	metrics := make(map[string]metricpkg.WithMetadata)
+	for i, autoMetric := range r.params.AutoMetrics {
+		metrics[autoMetric.Opts().ConfigName] = r.params.AutoMetrics[i]
+	}
+
+	// This is a bodge for a very specific feature, inherited from the old `Daemon.additionalMetrics`.
+	// We should really find a more generic way to handle such cases.
+	metricFlags := r.params.Config.Metrics
+	if r.params.DaemonConfig.DNSProxyConcurrencyLimit > 0 {
+		metricFlags = append(metricFlags, "+"+Namespace+"_"+SubsystemFQDN+"_semaphore_rejected_total")
+	}
+
+	for _, metricFlag := range metricFlags {
+		metricFlag = strings.TrimSpace(metricFlag)
+
+		// This is a temporary hack which allows us to get rid of the centralized metric config without refactoring the
+		// dynamic map pressure registration/unregistion mechanism.
+		// Long term the map pressure metric becomes a smarter component so this is no longer needed.
+		if metricFlag[1:] == "-"+Namespace+"_"+SubsystemBPF+"_map_pressure" {
+			BPFMapPressure = false
+			continue
+		}
+
+		metric := metrics[metricFlag[1:]]
+		if metric == nil {
+			continue
+		}
+
+		switch metricFlag[0] {
+		case '+':
+			metric.SetEnabled(true)
+		case '-':
+			metric.SetEnabled(false)
+		default:
+			r.params.Logger.Warning(
+				"--metrics flag contains value which does not start with + or -, '%s', ignoring",
+				metricFlag,
+			)
+		}
+	}
+
+	for _, m := range metrics {
+		if c, ok := m.(prometheus.Collector); ok {
+			r.MustRegister(c)
+		}
+	}
+}
+
+// MustRegister adds the collector to the registry, exposing this metric to
+// prometheus scrapes.
+// It will panic on error.
+func (r *Registry) MustRegister(c ...prometheus.Collector) {
+	r.inner.MustRegister(c...)
+}
+
+// RegisterList registers a list of collectors. If registration of one
+// collector fails, no collector is registered.
+func (r *Registry) RegisterList(list []prometheus.Collector) error {
+	registered := []prometheus.Collector{}
+
+	for _, c := range list {
+		if err := r.Register(c); err != nil {
+			for _, c := range registered {
+				r.Unregister(c)
+			}
+			return err
+		}
+
+		registered = append(registered, c)
+	}
+
+	return nil
+}
+
+// DumpMetrics gets the current Cilium metrics and dumps all into a
+// models.Metrics structure.If metrics cannot be retrieved, returns an error
+func (r *Registry) DumpMetrics() ([]*models.Metric, error) {
+	result := []*models.Metric{}
+	currentMetrics, err := r.inner.Gather()
+	if err != nil {
+		return result, err
+	}
+
+	for _, val := range currentMetrics {
+		metricName := val.GetName()
+		metricType := val.GetType()
+
+		for _, metricLabel := range val.Metric {
+			labels := map[string]string{}
+			for _, label := range metricLabel.GetLabel() {
+				labels[label.GetName()] = label.GetValue()
+			}
+
+			var value float64
+			switch metricType {
+			case dto.MetricType_COUNTER:
+				value = metricLabel.Counter.GetValue()
+			case dto.MetricType_GAUGE:
+				value = metricLabel.GetGauge().GetValue()
+			case dto.MetricType_UNTYPED:
+				value = metricLabel.GetUntyped().GetValue()
+			case dto.MetricType_SUMMARY:
+				value = metricLabel.GetSummary().GetSampleSum()
+			case dto.MetricType_HISTOGRAM:
+				value = metricLabel.GetHistogram().GetSampleSum()
+			default:
+				continue
+			}
+
+			metric := &models.Metric{
+				Name:   metricName,
+				Labels: labels,
+				Value:  value,
+			}
+			result = append(result, metric)
+		}
+	}
+	return result, nil
+}

clustermesh-apiserver/metrics/metrics.go
@@ -64,6 +64,9 @@ func (mm *metricsManager) Start(hive.HookContext) error {
 
 	mm.registry.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
 	mm.registry.MustRegister(collectors.NewGoCollector())
+	// Constructing the legacy metrics and register them at the metrics global variable.
+	// This is a hack until we can unify this metrics manager with the metrics.Registry.
+	metrics.NewLegacyMetrics()
 	mm.registry.MustRegister(
 		metrics.KVStoreOperationsDuration,
 		metrics.KVStoreEventsQueueDuration,

daemon/cmd/cells.go
@@ -22,6 +22,7 @@ import (
 	ipamMetadata "github.com/cilium/cilium/pkg/ipam/metadata"
 	"github.com/cilium/cilium/pkg/k8s"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/node"
 	nodeManager "github.com/cilium/cilium/pkg/node/manager"
 	"github.com/cilium/cilium/pkg/option"
@@ -60,6 +61,9 @@ var (
 		k8sClient.Cell,
 
 		cni.Cell,
+
+		// Provide the modular metrics registry, metric HTTP server and legacy metrics cell.
+		metrics.Cell,
 
 		// Provide option.Config via hive so cells can depend on the agent config.
 		cell.Provide(func() *option.DaemonConfig { return option.Config }),

daemon/cmd/cells_test.go
@@ -29,7 +29,7 @@ var goleakOptions = []goleak.Option{
 // the hive commands and documentation can be generated from it.
 func TestAgentCell(t *testing.T) {
 	defer goleak.VerifyNone(t, goleakOptions...)
-	defer metrics.ResetMetrics()
+	defer metrics.Reinitialize()
 
 	logging.SetLogLevelToDebug()
 	err := hive.New(Agent).Populate()

daemon/cmd/daemon_main.go
@@ -5,10 +5,8 @@ package cmd
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net"
-	"net/http"
 	"os"
 	"path"
 	"path/filepath"
@@ -695,9 +693,6 @@ func initializeFlags() {
 	flags.MarkHidden(option.MaxCtrlIntervalName)
 	option.BindEnv(Vp, option.MaxCtrlIntervalName)
 
-	flags.StringSlice(option.Metrics, []string{}, "Metrics that should be enabled or disabled from the default metric list. The list is expected to be separated by a space. (+metric_foo to enable metric_foo , -metric_bar to disable metric_bar)")
-	option.BindEnv(Vp, option.Metrics)
-
 	flags.String(option.MonitorAggregationName, "None",
 		"Level of monitor aggregation for traces from the datapath")
 	option.BindEnvWithLegacyEnvFallback(Vp, option.MonitorAggregationName, "CILIUM_MONITOR_AGGREGATION_LEVEL")
@@ -765,14 +760,6 @@ func initializeFlags() {
 
 	flags.Bool(option.PreAllocateMapsName, defaults.PreAllocateMaps, "Enable BPF map pre-allocation")
 	option.BindEnv(Vp, option.PreAllocateMapsName)
-
-	// We expect only one of the possible variables to be filled. The evaluation order is:
-	// --prometheus-serve-addr, CILIUM_PROMETHEUS_SERVE_ADDR, then PROMETHEUS_SERVE_ADDR
-	// The second environment variable (without the CILIUM_ prefix) is here to
-	// handle the case where someone uses a new image with an older spec, and the
-	// older spec used the older variable name.
-	flags.String(option.PrometheusServeAddr, ":9962", "IP:Port on which to serve prometheus metrics (pass \":Port\" to bind on all interfaces, \"\" is off)")
-	option.BindEnvWithLegacyEnvFallback(Vp, option.PrometheusServeAddr, "PROMETHEUS_SERVE_ADDR")
 
 	flags.Int(option.AuthMapEntriesName, option.AuthMapEntriesDefault, "Maximum number of entries in auth map")
 	option.BindEnv(Vp, option.AuthMapEntriesName)
@@ -1810,14 +1797,6 @@ func runDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *daem
 	bootstrapStats.healthCheck.End(true)
 
 	d.startStatusCollector(cleaner)
-
-	go func(errs <-chan error) {
-		err := <-errs
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.WithError(err).Error("Cannot start metrics server")
-			params.Shutdowner.Shutdown(hive.ShutdownWithError(err))
-		}
-	}(initMetrics())
 
 	d.startAgentHealthHTTPService()
 	if option.Config.KubeProxyReplacementHealthzBindAddr != "" {

daemon/cmd/daemon_test.go
@@ -11,9 +11,6 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/cilium/checkmate"
-	"github.com/prometheus/client_golang/prometheus"
-
 	"github.com/cilium/cilium/api/v1/models"
 	cnicell "github.com/cilium/cilium/daemon/cmd/cni"
 	fakecni "github.com/cilium/cilium/daemon/cmd/cni/fake"
@@ -44,6 +41,8 @@ import (
 	"github.com/cilium/cilium/pkg/proxy"
 	"github.com/cilium/cilium/pkg/testutils"
 	"github.com/cilium/cilium/pkg/types"
+
+	. "github.com/cilium/checkmate"
 )
 
 type DaemonSuite struct {
@@ -64,9 +63,6 @@ type DaemonSuite struct {
 	OnGetCompilationLock   func() *lock.RWMutex
 	OnSendNotification     func(typ monitorAPI.AgentNotifyMessage) error
 	OnGetCIDRPrefixLengths func() ([]int, []int)
-
-	// Metrics
-	collectors []prometheus.Collector
 }
 
 func setupTestDirectories() {
@@ -138,17 +134,6 @@ func (epSync *dummyEpSyncher) DeleteK8sCiliumEndpointSync(e *endpoint.Endpoint) 
 
 func (ds *DaemonSuite) SetUpSuite(c *C) {
 	testutils.IntegrationCheck(c)
-
-	// Register metrics once before running the suite
-	_, ds.collectors = metrics.CreateConfiguration([]string{"cilium_endpoint_state"})
-	metrics.MustRegister(ds.collectors...)
-}
-
-func (ds *DaemonSuite) TearDownSuite(c *C) {
-	// Unregister the metrics after the suite has finished
-	for _, c := range ds.collectors {
-		metrics.Unregister(c)
-	}
 }
 
 func (ds *DaemonSuite) SetUpTest(c *C) {

daemon/cmd/fqdn.go
@@ -211,12 +211,12 @@ func (d *Daemon) bootstrapFQDN(possibleEndpoints map[uint16]*endpoint.Endpoint, 
 			endpoints := d.endpointManager.GetEndpoints()
 			for _, ep := range endpoints {
 				epID := ep.StringID()
-				if option.Config.MetricsConfig.FQDNActiveNames || option.Config.MetricsConfig.FQDNActiveIPs {
+				if metrics.FQDNActiveNames.IsEnabled() || metrics.FQDNActiveIPs.IsEnabled() {
 					countFQDNs, countIPs := ep.DNSHistory.Count()
-					if option.Config.MetricsConfig.FQDNActiveNames {
+					if metrics.FQDNActiveNames.IsEnabled() {
 						metrics.FQDNActiveNames.WithLabelValues(epID).Set(float64(countFQDNs))
 					}
-					if option.Config.MetricsConfig.FQDNActiveIPs {
+					if metrics.FQDNActiveIPs.IsEnabled() {
 						metrics.FQDNActiveIPs.WithLabelValues(epID).Set(float64(countIPs))
 					}
 				}
@@ -227,7 +227,7 @@ func (d *Daemon) bootstrapFQDN(possibleEndpoints map[uint16]*endpoint.Endpoint, 
 					}
 				}
 				alive, dead := ep.DNSZombies.GC()
-				if option.Config.MetricsConfig.FQDNActiveZombiesConnections {
+				if metrics.FQDNAliveZombieConnections.IsEnabled() {
 					metrics.FQDNAliveZombieConnections.WithLabelValues(epID).Set(float64(len(alive)))
 				}
 

daemon/cmd/metrics.go
@@ -12,7 +12,6 @@ import (
 	restapi "github.com/cilium/cilium/api/v1/server/restapi/metrics"
 	"github.com/cilium/cilium/pkg/api"
 	"github.com/cilium/cilium/pkg/metrics"
-	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/spanstat"
 )
 
@@ -34,17 +33,6 @@ func (h *getMetrics) Handle(params restapi.GetMetricsParams) middleware.Responde
 	}
 
 	return restapi.NewGetMetricsOK().WithPayload(metrics)
-}
-
-func initMetrics() <-chan error {
-	var errs <-chan error
-
-	if option.Config.PrometheusServeAddr != "" {
-		log.Infof("Serving prometheus metrics on %s", option.Config.PrometheusServeAddr)
-		errs = metrics.Enable(option.Config.PrometheusServeAddr)
-	}
-
-	return errs
 }
 
 type bootstrapStatistics struct {
@@ -71,7 +59,7 @@ type bootstrapStatistics struct {
 }
 
 func (b *bootstrapStatistics) updateMetrics() {
-	if !option.Config.MetricsConfig.BootstrapTimesEnabled {
+	if !metrics.BootstrapTimes.IsEnabled() {
 		return
 	}
 

commit e00e5b3d0748c6a0f5c7009e374ef07ef923b2b7

metrics: Disable prometheus metrics by default
Prior to commit c49ef45399fc9, the prometheus metrics server was
disabled by default in Cilium. Typically we would expect users to
specify in helm both `prometheus.enabled` and `prometheus.port` to
determine how to configure the prometheus server to ensure that the
prometheus port doesn't also conflict with other services that the user
is running on their nodes in their clusters.

With the refactor in the aforementioned commit, the default was set to
`:9962`. This means that even if the user installed Cilium without
prometheus settings, or explicitly configured helm with
`prometheus.enabled: false`, the prometheus metrics server would be
enabled.

This patch reverts the default back to the pre-v1.14 default.

Fixes: c49ef45399fc ("metrics: Modularize daemon metrics registry")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/metrics/registry.go
@@ -24,7 +24,7 @@ import (
 )
 
 var defaultRegistryConfig = RegistryConfig{
-	PrometheusServeAddr: ":9962",
+	PrometheusServeAddr: "",
 }
 
 type RegistryConfig struct {