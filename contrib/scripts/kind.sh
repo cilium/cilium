cilium/cilium

commit f7fdeef2cc19bf083a62361209118a7bf028921d

ipfamily should be set by platform configuration.
Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

contrib/scripts/kind.sh
@@ -9,7 +9,11 @@ default_workers=1
 default_cluster_name=""
 default_image=""
 default_kubeproxy_mode="iptables"
-default_ipfamily="dual"
+if [ "$(uname 2>/dev/null)" == "Linux" ] && [ $(sysctl -n net.ipv6.conf.all.disable_ipv6) == 1 ] ; then
+  default_ipfamily="ipv4"
+else
+  default_ipfamily="dual"
+fi
 default_pod_subnet=""
 default_service_subnet=""
 default_agent_port_prefix="234"

commit d9e95a0d3d43103460b552f59391737d5794893f

contrib: don't use sysctl to check if IPv6 is enabled
The sysctl command may not be installed, or in the default path (e.g., in
case of Debian), causing errors like:

  ./contrib/scripts/kind.sh: line 12: sysctl: command not found
  ./contrib/scripts/kind.sh: line 12: [: ==: unary operator expected

Instead, let's just directly access the content of the corresponding
proc file, so that we don't depend on external tools.

Fixes: f7fdeef2cc19 ("ipfamily should be set by platform configuration.")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

contrib/scripts/kind.sh
@@ -9,7 +9,7 @@ default_workers=1
 default_cluster_name=""
 default_image=""
 default_kubeproxy_mode="iptables"
-if [ "$(uname 2>/dev/null)" == "Linux" ] && [ $(sysctl -n net.ipv6.conf.all.disable_ipv6) == 1 ] ; then
+if [ "$(uname 2>/dev/null)" == "Linux" ] && [ "$(</proc/sys/net/ipv6/conf/all/disable_ipv6)" == 1 ] ; then
   default_ipfamily="ipv4"
 else
   default_ipfamily="dual"