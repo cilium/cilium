3f8e3b966f0000538c060fde43201fe4bc092474
Fabio Falzoi <fabio.falzoi@isovalent.com>
2024-03-06 17:10:56 +0100

    Revert "policy: Inline selectorPolicy.L4Policy"
    This reverts commit 71a607f6f37538a2feaff0455029d4213c62a9d0.

--- pkg/endpoint/policy.go
+++ pkg/endpoint/policy.go
@@ -125,8 +125,8 @@ func (e *Endpoint) updateNetworkPolicy(proxyWaitGroup *completion.WaitGroup) (re
 		return nil, nil
 	}
 
-	// If desired policy is nil then no policy change is needed.
-	if e.desiredPolicy == nil {
+	// If desired L4Policy is nil then no policy change is needed.
+	if e.desiredPolicy == nil || e.desiredPolicy.L4Policy == nil {
 		return nil, nil
 	}
 
@@ -135,7 +135,7 @@ func (e *Endpoint) updateNetworkPolicy(proxyWaitGroup *completion.WaitGroup) (re
 	}
 
 	// Publish the updated policy to L7 proxies.
-	return e.proxy.UpdateNetworkPolicy(e, e.visibilityPolicy, &e.desiredPolicy.L4Policy, e.desiredPolicy.IngressPolicyEnabled, e.desiredPolicy.EgressPolicyEnabled, proxyWaitGroup)
+	return e.proxy.UpdateNetworkPolicy(e, e.visibilityPolicy, e.desiredPolicy.L4Policy, e.desiredPolicy.IngressPolicyEnabled, e.desiredPolicy.EgressPolicyEnabled, proxyWaitGroup)
 }
 
 // setNextPolicyRevision updates the desired policy revision field
