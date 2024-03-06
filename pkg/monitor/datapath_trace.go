e78cf7cff17501ab95b570f2261aca3baf975c61
Alexandre Perrin <alex@isovalent.com>
2024-03-06 19:06:43 +0000

    hubble: fix traffic direction and reply on encrypted trace notifications
    [ upstream commit 9939fa2b0848ddd056e81f14a548f179f59027f3 ]
    
    Before this patch, Hubble would wrongly report known traffic direction
    and reply status when IPSec was enabled.
    
    Signed-off-by: Alexandre Perrin <alex@isovalent.com>
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/monitor/datapath_trace.go
+++ pkg/monitor/datapath_trace.go
@@ -109,7 +109,7 @@ func connState(reason uint8) string {
 }
 
 func TraceReasonIsKnown(reason uint8) bool {
-	switch reason {
+	switch reason & ^TraceReasonEncryptMask {
 	case TraceReasonUnknown:
 		return false
 	default:
@@ -118,7 +118,7 @@ func TraceReasonIsKnown(reason uint8) bool {
 }
 
 func TraceReasonIsEncap(reason uint8) bool {
-	switch reason {
+	switch reason & ^TraceReasonEncryptMask {
 	case TraceReasonSRv6Encap:
 		return true
 	default:
@@ -127,7 +127,7 @@ func TraceReasonIsEncap(reason uint8) bool {
 }
 
 func TraceReasonIsDecap(reason uint8) bool {
-	switch reason {
+	switch reason & ^TraceReasonEncryptMask {
 	case TraceReasonSRv6Decap:
 		return true
 	default:
