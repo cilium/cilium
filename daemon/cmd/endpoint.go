529f8ef6373811440ee5cbb638d725358d2980ba
Vipul Singh <vipul21sept@gmail.com>
2024-03-06 21:07:32 +0000

    feat: Add the http return code to metric api_processed_total
    [ upstream commit 76867e23700ea899bdfdfe247998723cbe9512b2 ]
    
    Signed-off-by: Vipul Singh <vipul21sept@gmail.com>
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- daemon/cmd/endpoint.go
+++ daemon/cmd/endpoint.go
@@ -57,7 +57,7 @@ func getEndpointHandler(d *Daemon, params GetEndpointParams) middleware.Responde
 	resEPs := d.getEndpointList(params)
 
 	if params.Labels != nil && len(resEPs) == 0 {
-		r.Error(errEndpointNotFound)
+		r.Error(errEndpointNotFound, GetEndpointNotFoundCode)
 		return NewGetEndpointNotFound()
 	}
 
@@ -135,10 +135,11 @@ func deleteEndpointHandler(d *Daemon, params DeleteEndpointParams) middleware.Re
 	defer r.Done()
 
 	if nerr, err := d.deleteEndpointByContainerID(params.Endpoint.ContainerID); err != nil {
-		r.Error(err)
 		if apierr, ok := err.(*api.APIError); ok {
+			r.Error(err, apierr.GetCode())
 			return apierr
 		}
+		r.Error(err, DeleteEndpointInvalidCode)
 		return api.Error(DeleteEndpointInvalidCode, err)
 	} else if nerr > 0 {
 		return NewDeleteEndpointErrors().WithPayload(int64(nerr))
@@ -159,10 +160,10 @@ func getEndpointIDHandler(d *Daemon, params GetEndpointIDParams) middleware.Resp
 	ep, err := d.endpointManager.Lookup(params.ID)
 
 	if err != nil {
-		r.Error(err)
+		r.Error(err, GetEndpointIDInvalidCode)
 		return api.Error(GetEndpointIDInvalidCode, err)
 	} else if ep == nil {
-		r.Error(errEndpointNotFound)
+		r.Error(errEndpointNotFound, GetEndpointIDNotFoundCode)
 		return NewGetEndpointIDNotFound()
 	} else {
 		return NewGetEndpointIDOK().WithPayload(ep.GetModel())
@@ -580,7 +581,7 @@ func putEndpointIDHandler(d *Daemon, params PutEndpointIDParams) (resp middlewar
 
 	ep, code, err := d.createEndpoint(params.HTTPRequest.Context(), d, epTemplate)
 	if err != nil {
-		r.Error(err)
+		r.Error(err, code)
 		return api.Error(code, err)
 	}
 
@@ -632,7 +633,7 @@ func patchEndpointIDHandler(d *Daemon, params PatchEndpointIDParams) middleware.
 	// Note: newEp's labels are ignored.
 	newEp, err2 := endpoint.NewEndpointFromChangeModel(d.ctx, d, d, d.ipcache, d.l7Proxy, d.identityAllocator, epTemplate)
 	if err2 != nil {
-		r.Error(err2)
+		r.Error(err2, PutEndpointIDInvalidCode)
 		return api.Error(PutEndpointIDInvalidCode, err2)
 	}
 
@@ -648,15 +649,15 @@ func patchEndpointIDHandler(d *Daemon, params PatchEndpointIDParams) middleware.
 
 	ep, err := d.endpointManager.Lookup(params.ID)
 	if err != nil {
-		r.Error(err)
+		r.Error(err, GetEndpointIDInvalidCode)
 		return api.Error(GetEndpointIDInvalidCode, err)
 	}
 	if ep == nil {
-		r.Error(errEndpointNotFound)
+		r.Error(errEndpointNotFound, PatchEndpointIDNotFoundCode)
 		return NewPatchEndpointIDNotFound()
 	}
 	if err = endpoint.APICanModify(ep); err != nil {
-		r.Error(err)
+		r.Error(err, PatchEndpointIDInvalidCode)
 		return api.Error(PatchEndpointIDInvalidCode, err)
 	}
 
@@ -668,7 +669,7 @@ func patchEndpointIDHandler(d *Daemon, params PatchEndpointIDParams) middleware.
 	//  Support arbitrary changes? Support only if unset?
 	reason, err := ep.ProcessChangeRequest(newEp, validStateTransition)
 	if err != nil {
-		r.Error(err)
+		r.Error(err, PatchEndpointIDNotFoundCode)
 		return NewPatchEndpointIDNotFound()
 	}
 
@@ -681,7 +682,7 @@ func patchEndpointIDHandler(d *Daemon, params PatchEndpointIDParams) middleware.
 			err := api.Error(PatchEndpointIDFailedCode,
 				fmt.Errorf("error while regenerating endpoint."+
 					" For more info run: 'cilium endpoint get %d'", ep.ID))
-			r.Error(err)
+			r.Error(err, PatchEndpointIDFailedCode)
 			return err
 		}
 		// FIXME: Special return code to indicate regeneration happened?
@@ -822,10 +823,11 @@ func deleteEndpointIDHandler(d *Daemon, params DeleteEndpointIDParams) middlewar
 	defer r.Done()
 
 	if nerr, err := d.DeleteEndpoint(params.ID); err != nil {
-		r.Error(err)
 		if apierr, ok := err.(*api.APIError); ok {
+			r.Error(err, apierr.GetCode())
 			return apierr
 		}
+		r.Error(err, DeleteEndpointIDErrorsCode)
 		return api.Error(DeleteEndpointIDErrorsCode, err)
 	} else if nerr > 0 {
 		return NewDeleteEndpointIDErrors().WithPayload(int64(nerr))
@@ -870,10 +872,11 @@ func patchEndpointIDConfigHandler(d *Daemon, params PatchEndpointIDConfigParams)
 	defer r.Done()
 
 	if err := d.EndpointUpdate(params.ID, params.EndpointConfiguration); err != nil {
-		r.Error(err)
 		if apierr, ok := err.(*api.APIError); ok {
+			r.Error(err, apierr.GetCode())
 			return apierr
 		}
+		r.Error(err, PatchEndpointIDFailedCode)
 		return api.Error(PatchEndpointIDFailedCode, err)
 	}
 
@@ -891,10 +894,10 @@ func getEndpointIDConfigHandler(d *Daemon, params GetEndpointIDConfigParams) mid
 
 	ep, err := d.endpointManager.Lookup(params.ID)
 	if err != nil {
-		r.Error(err)
+		r.Error(err, GetEndpointIDInvalidCode)
 		return api.Error(GetEndpointIDInvalidCode, err)
 	} else if ep == nil {
-		r.Error(errEndpointNotFound)
+		r.Error(errEndpointNotFound, GetEndpointIDConfigNotFoundCode)
 		return NewGetEndpointIDConfigNotFound()
 	} else {
 		cfgStatus := ep.GetConfigurationStatus()
@@ -914,17 +917,17 @@ func getEndpointIDLabelsHandler(d *Daemon, params GetEndpointIDLabelsParams) mid
 
 	ep, err := d.endpointManager.Lookup(params.ID)
 	if err != nil {
-		r.Error(err)
+		r.Error(err, GetEndpointIDInvalidCode)
 		return api.Error(GetEndpointIDInvalidCode, err)
 	}
 	if ep == nil {
-		r.Error(errEndpointNotFound)
+		r.Error(errEndpointNotFound, GetEndpointIDLabelsNotFoundCode)
 		return NewGetEndpointIDLabelsNotFound()
 	}
 
 	cfg, err := ep.GetLabelsModel()
 	if err != nil {
-		r.Error(err)
+		r.Error(err, GetEndpointIDInvalidCode)
 		return api.Error(GetEndpointIDInvalidCode, err)
 	}
 
@@ -943,10 +946,10 @@ func getEndpointIDLogHandler(d *Daemon, params GetEndpointIDLogParams) middlewar
 	ep, err := d.endpointManager.Lookup(params.ID)
 
 	if err != nil {
-		r.Error(err)
+		r.Error(err, GetEndpointIDLogInvalidCode)
 		return api.Error(GetEndpointIDLogInvalidCode, err)
 	} else if ep == nil {
-		r.Error(errEndpointNotFound)
+		r.Error(errEndpointNotFound, GetEndpointIDLogNotFoundCode)
 		return NewGetEndpointIDLogNotFound()
 	} else {
 		return NewGetEndpointIDLogOK().WithPayload(ep.GetStatusModel())
@@ -965,10 +968,10 @@ func getEndpointIDHealthzHandler(d *Daemon, params GetEndpointIDHealthzParams) m
 	ep, err := d.endpointManager.Lookup(params.ID)
 
 	if err != nil {
-		r.Error(err)
+		r.Error(err, GetEndpointIDHealthzInvalidCode)
 		return api.Error(GetEndpointIDHealthzInvalidCode, err)
 	} else if ep == nil {
-		r.Error(errEndpointNotFound)
+		r.Error(errEndpointNotFound, GetEndpointIDHealthzNotFoundCode)
 		return NewGetEndpointIDHealthzNotFound()
 	} else {
 		return NewGetEndpointIDHealthzOK().WithPayload(ep.GetHealthModel())
@@ -1023,22 +1026,22 @@ func putEndpointIDLabelsHandler(d *Daemon, params PatchEndpointIDLabelsParams) m
 
 	ep, err := d.endpointManager.Lookup(params.ID)
 	if err != nil {
-		r.Error(err)
+		r.Error(err, PutEndpointIDInvalidCode)
 		return api.Error(PutEndpointIDInvalidCode, err)
 	} else if ep == nil {
-		r.Error(errEndpointNotFound)
+		r.Error(errEndpointNotFound, PatchEndpointIDLabelsNotFoundCode)
 		return NewPatchEndpointIDLabelsNotFound()
 	}
 
 	add, del, err := ep.ApplyUserLabelChanges(lbls)
 	if err != nil {
-		r.Error(err)
+		r.Error(err, PutEndpointIDInvalidCode)
 		return api.Error(PutEndpointIDInvalidCode, err)
 	}
 
 	code, err := d.modifyEndpointIdentityLabelsFromAPI(params.ID, add, del)
 	if err != nil {
-		r.Error(err)
+		r.Error(err, code)
 		return api.Error(code, err)
 	}
 	return NewPatchEndpointIDLabelsOK()
--- pkg/rate/api_limiter.go
+++ pkg/rate/api_limiter.go
@@ -82,6 +82,8 @@ const (
 	outcomeParallelMaxWait outcome = "fail-parallel-wait"
 	outcomeLimitMaxWait    outcome = "fail-limit-wait"
 	outcomeReqCancelled    outcome = "request-cancelled"
+	outcomeErrorCode       int     = 429
+	outcomeSuccessCode     int     = 200
 )
 
 // APILimiter is an extension to x/time/rate.Limiter specifically for Cilium
@@ -436,7 +438,7 @@ func (l *APILimiter) adjustedParallelRequests() int {
 	return int(l.adjustmentLimit(newParallelRequests, float64(l.params.ParallelRequests)))
 }
 
-func (l *APILimiter) requestFinished(r *limitedRequest, err error) {
+func (l *APILimiter) requestFinished(r *limitedRequest, err error, code int) {
 	if r.finished {
 		return
 	}
@@ -518,6 +520,7 @@ func (l *APILimiter) requestFinished(r *limitedRequest, err error) {
 		AdjustmentFactor:            l.adjustmentFactor,
 		Error:                       err,
 		Outcome:                     string(r.outcome),
+		ReturnCode:                  code,
 	}
 
 	if l.limiter != nil {
@@ -547,7 +550,7 @@ func calcMeanDuration(durations []time.Duration) float64 {
 // WaitDuration() concurrently.
 type LimitedRequest interface {
 	Done()
-	Error(err error)
+	Error(err error, code int)
 	WaitDuration() time.Duration
 }
 
@@ -569,12 +572,12 @@ func (l *limitedRequest) WaitDuration() time.Duration {
 
 // Done must be called when the API request has been successfully processed
 func (l *limitedRequest) Done() {
-	l.limiter.requestFinished(l, nil)
+	l.limiter.requestFinished(l, nil, outcomeSuccessCode)
 }
 
 // Error must be called when the API request resulted in an error
-func (l *limitedRequest) Error(err error) {
-	l.limiter.requestFinished(l, err)
+func (l *limitedRequest) Error(err error, code int) {
+	l.limiter.requestFinished(l, err, code)
 }
 
 // Wait blocks until the next API call is allowed to be processed. If the
@@ -584,7 +587,7 @@ func (l *limitedRequest) Error(err error) {
 func (l *APILimiter) Wait(ctx context.Context) (LimitedRequest, error) {
 	req, err := l.wait(ctx)
 	if err != nil {
-		l.requestFinished(req, err)
+		l.requestFinished(req, err, outcomeErrorCode)
 		return nil, err
 	}
 	return req, nil
@@ -813,6 +816,7 @@ type MetricsValues struct {
 	CurrentRequestsInFlight     int
 	AdjustmentFactor            float64
 	Error                       error
+	ReturnCode                  int
 }
 
 // MetricsObserver is the interface that must be implemented to extract metrics
@@ -867,7 +871,7 @@ type dummyRequest struct{}
 
 func (d dummyRequest) WaitDuration() time.Duration { return 0 }
 func (d dummyRequest) Done()                       {}
-func (d dummyRequest) Error(err error)             {}
+func (d dummyRequest) Error(err error, code int)   {}
 
 // Wait invokes Wait() on the APILimiter with the given name. If the limiter
 // does not exist, a dummy limiter is used which will not impose any
