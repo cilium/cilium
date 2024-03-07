cilium/cilium

commit 7ba0e83acc458e8e3c20cc70e31542c0f22f093f

cilium: from-netdev and from-network BPF programs conflicting hooks
Currently, enabling IPsec and a feature that uses a from-netdev program
type may conflict when both attempt to load on the network facing device.
If this happens then init.sh will load the ipsec program from-network
and then after that the golang bpf loader will replace it with the
from-netdev program. Which could be OK if the from-netdev program handled
IPsec decryption correctly. Unfortunately, it doesn't and what we get
is dropped ESP (IPsec encryption protocol) packets and broken connections.

To fix teach from-netdev how to handle ingress IPsec decryption. And while
doing this cleanup the encryption code blocks so encryption/decryption
logic is split into from_host and from_network parts. This way encrypt
piece is used in from_host and decrypt is used in from_network case.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bpf/bpf_host.c
@@ -47,6 +47,7 @@
 #include "lib/eps.h"
 #include "lib/host_firewall.h"
 #include "lib/overloadable.h"
+#include "lib/encrypt.h"
 
 #if defined(ENABLE_IPV4) || defined(ENABLE_IPV6)
 static __always_inline int rewrite_dmac_to_host(struct __ctx_buff *ctx,
@@ -767,11 +768,16 @@ do_netdev(struct __ctx_buff *ctx, __u16 proto, const bool from_host)
 	int ret;
 
 #ifdef ENABLE_IPSEC
-	if (1) {
+	if (from_host) {
 		__u32 magic = ctx->mark & MARK_MAGIC_HOST_MASK;
 
 		if (magic == MARK_MAGIC_ENCRYPT)
 			return do_netdev_encrypt(ctx, proto);
+	} else {
+		int done = do_decrypt(ctx, proto);
+
+		if (!done)
+			return CTX_ACT_OK;
 	}
 #endif
 	bpf_clear_meta(ctx);

commit de82a79d6b0df58fc585b700918da0ba1d4bb75e

bpf: Don't skip local delivery for plain-text packets
The current IPsec decryption handling in bpf_host is buggy: when
executing do_decrypt, we skip all subsequent logic in the same BPF
program and return to stack. That subsequent logic includes (1) all
service handling logic including NAT, (2) the host policies enforcement,
and (3) delivery to the destination pods.

(1) and (2) are not an issue today because KPR and the host firewall are
incompatible with IPsec. (3) is also not really an issue because packets
will flow through cilium_host where they will run through the local
delivery logic again; that's however less efficient.

This commit changes the logic a bit, to not skip the subsequent BPF
processing for plain-text ingressing packets. If the packet is
encrypted and needs decryption, we should send it to the stack, but any
other packet needs should be fully processed by bpf_host.

Fixes: 7ba0e83acc ("cilium: from-netdev and from-network BPF programs
conflicting hooks")
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

bpf/bpf_host.c
@@ -1058,8 +1058,18 @@ do_netdev(struct __ctx_buff *ctx, __u16 proto, const bool from_host)
 	int ret;
 
 #ifdef ENABLE_IPSEC
-	if (!from_host && !do_decrypt(ctx, proto))
-		return CTX_ACT_OK;
+	if (!from_host) {
+		/* If the packet needs decryption, we want to send it straight to the
+		 * stack. There's no need to run service handling logic, host firewall,
+		 * etc. on an encrypted packet.
+		 * In all other cases (packet doesn't need decryption or already
+		 * decrypted), we want to run all subsequent logic here. We therefore
+		 * ignore the return value from do_decrypt.
+		 */
+		do_decrypt(ctx, proto);
+		if (ctx->mark == MARK_MAGIC_DECRYPT)
+			return CTX_ACT_OK;
+	}
 #endif
 
 	if (from_host) {