b0e893cd223964c9f530b530907152e6049d9f78
Tam Mach <tam.mach@cilium.io>
2024-03-10 21:46:43 +1100

    ingress: Update backend service for Ingress
    [ cherry-picked from cilium/cilium-cli repository ]
    
    This is to use echo-same-node instead of echo-other-node, which is only
    available for multi-node cluster. The reason is mainly for local
    development, no impact on CI.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- cilium-cli/connectivity/check/deployment.go
+++ cilium-cli/connectivity/check/deployment.go
@@ -366,7 +366,7 @@ func newIngress() *networkingv1.Ingress {
 									}(),
 									Backend: networkingv1.IngressBackend{
 										Service: &networkingv1.IngressServiceBackend{
-											Name: echoOtherNodeDeploymentName,
+											Name: echoSameNodeDeploymentName,
 											Port: networkingv1.ServiceBackendPort{
 												Number: 8080,
 											},
