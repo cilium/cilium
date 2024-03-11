5b4baad0c1f760d34c71ee2cc49e75a6e72fe579
Martynas Pumputis <m@lambda.lt>
2024-03-11 07:11:05 +0100

    connectivity: Extend pod-to-pod encryption tests
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Add L7 policy checks. Only for WG, while IPsec is currently suffering
    from https://github.com/cilium/cilium/issues/33168.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- cilium-cli/connectivity/builder/pod_to_pod_encryption.go
+++ cilium-cli/connectivity/builder/pod_to_pod_encryption.go
@@ -4,10 +4,15 @@
 package builder
 
 import (
+	_ "embed"
+
 	"github.com/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium-cli/connectivity/tests"
 	"github.com/cilium/cilium-cli/utils/features"
 )
+
+//go:embed manifests/client-egress-l7-http-from-any.yaml
+var clientsEgressL7HTTPFromAnyPolicyYAML string
 
 type podToPodEncryption struct{}
 
@@ -19,4 +24,19 @@ func (t podToPodEncryption) build(ct *check.ConnectivityTest, _ map[string]strin
 		WithScenarios(
 			tests.PodToPodEncryption(features.RequireEnabled(features.EncryptionPod)),
 		)
+
+	newTest("pod-to-pod-with-l7-policy-encryption", ct).
+		WithCondition(func() bool { return !ct.Params().SingleNode }).
+		WithFeatureRequirements(
+			features.RequireEnabled(features.L7Proxy),
+			// Once https://github.com/cilium/cilium/issues/33168 is fixed, we
+			// can enable for IPsec too.
+			features.RequireMode(features.EncryptionPod, "wireguard"),
+		).
+		WithCiliumPolicy(clientsEgressL7HTTPFromAnyPolicyYAML).
+		WithCiliumPolicy(echoIngressL7HTTPFromAnywherePolicyYAML).
+		WithScenarios(
+			tests.PodToPodEncryption(features.RequireEnabled(features.EncryptionPod)),
+		)
+
 }
