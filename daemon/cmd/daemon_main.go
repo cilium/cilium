ee433ceaec02a758197bf626cd999f6ffab6d053
Sebastian Wicki <sebastian@isovalent.com>
2024-03-12 16:48:05 +0100

    daemon: Remove `ip-allocation-timeout` flag
    This removes the `ip-allocation-timeout` flag from cilium-agent. It was
    used by the now removed `AllocateCIDRs` function. As the timeout does
    apply to the asynchronous allocation mechanism, it is removed.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -530,9 +530,6 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.Duration(option.KVstoreConnectivityTimeout, defaults.KVstoreConnectivityTimeout, "Time after which an incomplete kvstore operation  is considered failed")
 	option.BindEnv(vp, option.KVstoreConnectivityTimeout)
 
-	flags.Duration(option.IPAllocationTimeout, defaults.IPAllocationTimeout, "Time after which an incomplete CIDR allocation is considered failed")
-	option.BindEnv(vp, option.IPAllocationTimeout)
-
 	flags.Var(option.NewNamedMapOptions(option.KVStoreOpt, &option.Config.KVStoreOpt, nil),
 		option.KVStoreOpt, "Key-value store options e.g. etcd.address=127.0.0.1:4001")
 	option.BindEnv(vp, option.KVStoreOpt)
