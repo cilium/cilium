ec1d364621eaefe92ef5a9ff0aaa6b5b45183efd
Michi Mutsuzaki <michi@isovalent.com>
2024-03-13 01:04:58 +0000

    Add InitializeCommand hook
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Call InitializeCommand hook with the root command before returning from
    cli.NewCiliumCommand.
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

---
+++ cilium-cli/cli/cmd_test.go
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cli
+
+import (
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+)
+
+const usage = "override usage"
+
+type testHooks struct {
+	NopHooks
+}
+
+func (th *testHooks) InitializeCommand(rootCmd *cobra.Command) {
+	rootCmd.Use = usage
+}
+
+func TestInitializeCommandHook(t *testing.T) {
+	cmd := NewCiliumCommand(&testHooks{})
+	assert.Equal(t, usage, cmd.Use)
+}
