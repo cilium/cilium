671f2af23d0065d0d9a118e45f59438d93a7ce2f
Fabian Fischer <fabian.fischer@isovalent.com>
2024-03-13 15:17:04 +0100

    hubble/relay: Fix certificate reloading in PeerManager
    This change fixes an issue with the PeerManager that can lead to Relay
    being unable to reconnect to some or all peers when the client
    certificate expires or the Certificate Authority is replaced.
    
    Before this change, when the client certificate changes, we did not
    redial or update the exiting gRPC ClientConns. When the old certificate
    becomes invalid, (expiring, changed CA, or revoked) The connection will
    eventually fail with a certificate error.
    However, the gRPC ClientConn is not closed, but treats the certificate
    error as a transient failure and will retry connecting with the old
    credentials indefinitely.
    In most cases this will cause the relay health checks to fail. Relay
    will restart and successfully reconnect to all peers. However, if a new
    peer joins between the certificate being updated and the connections
    failing, Relay may keep on running in a degraded state.
    
    This issue was introduced by #28595. Before that change, Relay
    aggressively closed and re-dialed ClientConns on any error, mitigating
    this problem.
    
    We fix this issue by wrapping the provided gRPC transport credentials
    and updating the TLS configuration whenever a new TLS connection is
    established. This means every TLS connection will use up-to-date
    certificates and gRPC ClientConns will be able to recover when their
    certificate changes.
    
    Fixes: aca4d42ce80d ("hubble/relay: Fix connection leak when reconnecting to peer service")
    
    Signed-off-by: Fabian Fischer <fabian.fischer@isovalent.com>

--- pkg/hubble/relay/pool/client.go
+++ pkg/hubble/relay/pool/client.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -15,6 +16,7 @@ import (
 	"github.com/cilium/cilium/pkg/crypto/certloader"
 	poolTypes "github.com/cilium/cilium/pkg/hubble/relay/pool/types"
 	hubbleopts "github.com/cilium/cilium/pkg/hubble/server/serveroption"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/time"
 )
 
@@ -55,11 +57,56 @@ func (b GRPCClientConnBuilder) ClientConn(target, hostname string) (poolTypes.Cl
 	} else {
 		// NOTE: gosec is unable to resolve the constant and warns about "TLS
 		// MinVersion too low".
-		tlsConfig := b.TLSConfig.ClientConfig(&tls.Config{ //nolint:gosec
+		baseConf := &tls.Config{ //nolint:gosec
 			ServerName: hostname,
 			MinVersion: hubbleopts.MinTLSVersion,
-		})
-		opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+		}
+		opts = append(opts, grpc.WithTransportCredentials(
+			&grpcTLSCredentialsWrapper{
+				TransportCredentials: credentials.NewTLS(b.TLSConfig.ClientConfig(baseConf)),
+				baseConf:             baseConf,
+				TLSConfig:            b.TLSConfig,
+			},
+		))
 	}
 	return grpc.DialContext(ctx, target, opts...)
+}
+
+var _ credentials.TransportCredentials = &grpcTLSCredentialsWrapper{}
+
+// grpcTLSCredentialsWrapper wraps gRPC TransportCredentials and fetches the
+// newest TLS configuration from certloader whenever we a new TLS connection
+// is established.
+//
+// A gRPC ClientConn will call ClientHandshake whenever it tries to establish
+// a new TLS connection. This happens in the beginning when dialing, but also
+// when the connection is lost and gRPC tries to reestablish the connection.
+// Wrapping the ClientHandshake and fetching the updated certificate and CA,
+// allows us to transparently reload certificates when they change, without
+// closing and redialing the gRPC ClientConn.
+type grpcTLSCredentialsWrapper struct {
+	credentials.TransportCredentials
+
+	mu        lock.Mutex
+	baseConf  *tls.Config
+	TLSConfig certloader.ClientConfigBuilder
+}
+
+// ClientHandshake implements credentials.TransportCredentials.
+func (w *grpcTLSCredentialsWrapper) ClientHandshake(ctx context.Context, addr string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	w.TransportCredentials = credentials.NewTLS(w.TLSConfig.ClientConfig(w.baseConf))
+	return w.TransportCredentials.ClientHandshake(ctx, addr, conn)
+}
+
+// Clone implements credentials.TransportCredentials.
+func (w *grpcTLSCredentialsWrapper) Clone() credentials.TransportCredentials {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	return &grpcTLSCredentialsWrapper{
+		baseConf:             w.baseConf.Clone(),
+		TransportCredentials: w.TransportCredentials.Clone(),
+		TLSConfig:            w.TLSConfig,
+	}
 }
