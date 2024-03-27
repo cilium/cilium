8440660e380e693a6bb4beff906fad0dac823f5e
Alexandre Perrin <alex@isovalent.com>
2024-03-27 16:30:32 +0100

    hubble: fix traffic direction for TraceReasonEncryptOverlay flows
    Before this patch, TraceReasonEncryptOverlay traces would result in
    flows with ingress traffic direction.
    
    Since the flow source is the local host and destination a remote node,
    egress arguably make more sense to expose at a high level. Thus, this
    patch set the traffic direction to egress consistently for
    TraceReasonEncryptOverlay hubble flows.
    
    Signed-off-by: Alexandre Perrin <alex@isovalent.com>

--- pkg/hubble/parser/threefour/parser.go
+++ pkg/hubble/parser/threefour/parser.go
@@ -475,10 +475,17 @@ func decodeTrafficDirection(srcEP uint32, dn *monitor.DropNotify, tn *monitor.Tr
 			// true if the packet is a reply, i.e. reverse direction
 			isReply := tn.TraceReasonIsReply()
 
+			switch {
+			// Although technically the corresponding packet is ingressing the
+			// stack (TraceReasonEncryptOverlay traces are TraceToStack), it is
+			// ultimately originating from the local node and destinated to a
+			// remote node, so egress make more sense to expose at a high
+			// level.
+			case tn.TraceReason() == monitor.TraceReasonEncryptOverlay:
+				return pb.TrafficDirection_EGRESS
 			// isSourceEP != isReply ==
 			//  (isSourceEP && !isReply) || (!isSourceEP && isReply)
-			// GH-31226: currently broken for monitor.TraceReasonEncryptOverlay showing INGRESS
-			if isSourceEP != isReply {
+			case isSourceEP != isReply:
 				return pb.TrafficDirection_EGRESS
 			}
 			return pb.TrafficDirection_INGRESS
