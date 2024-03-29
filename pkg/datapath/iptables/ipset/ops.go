df844c1b0948a3c85b406597b918c13d5d4fe889
Fabio Falzoi <fabio.falzoi@isovalent.com>
2024-03-29 18:50:24 +0100

    iptables/ipset: Store a single addr per table entry
    Rework the ipset table to store a single address per entry. This allows
    to rewrite the reconciler in a more canonical way, instead of mixing
    address insertions and removals in the Update operation.
    
    In the current setup, the Update and Delete respectively add and remove
    a single IP address in the related set.  The Prune operation performs a
    full reconciliation for both IPv4 and IPv6 sets.
    
    Since each table entry now stores a single address instead of an entire
    set, the usage of immutable sets is replaced by a simpler netip.Addr
    field.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/datapath/iptables/ipset/ops.go
+++ pkg/datapath/iptables/ipset/ops.go
@@ -5,16 +5,19 @@ package ipset
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/netip"
 
 	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/util/sets"
 
 	"github.com/cilium/cilium/pkg/datapath/tables"
 	"github.com/cilium/cilium/pkg/statedb"
 	"github.com/cilium/cilium/pkg/statedb/reconciler"
 )
 
-func newOps(logger logrus.FieldLogger, ipset *ipset, cfg config) reconciler.Operations[*tables.IPSet] {
+func newOps(logger logrus.FieldLogger, ipset *ipset, cfg config) reconciler.Operations[*tables.IPSetEntry] {
 	return &ops{
 		enabled: cfg.NodeIPSetNeeded,
 		ipset:   ipset,
@@ -26,37 +29,18 @@ type ops struct {
 	ipset   *ipset
 }
 
-func (ops *ops) Update(ctx context.Context, _ statedb.ReadTxn, s *tables.IPSet, changed *bool) error {
+func (ops *ops) Update(ctx context.Context, _ statedb.ReadTxn, entry *tables.IPSetEntry, changed *bool) error {
 	if !ops.enabled {
 		return nil
 	}
 
 	// create the set if does not exist
-	if err := ops.ipset.create(ctx, s.Name, string(s.Family)); err != nil {
+	if err := ops.ipset.create(ctx, entry.Name, string(entry.Family)); err != nil {
 		return err
 	}
 
-	cur, err := ops.ipset.list(ctx, s.Name)
-	if err != nil {
-		return fmt.Errorf("failed to list ips in ipset %s: %w", s.Name, err)
-	}
-
-	if s.Addrs.Equal(cur) {
-		return nil
-	}
-
-	// reconcile the set
-	toAdd := s.Addrs.Difference(cur).AsSlice()
-	for _, addr := range toAdd {
-		if err := ops.ipset.add(ctx, s.Name, addr); err != nil {
-			return err
-		}
-	}
-	toDel := cur.Difference(s.Addrs).AsSlice()
-	for _, addr := range toDel {
-		if err := ops.ipset.del(ctx, s.Name, addr); err != nil {
-			return err
-		}
+	if err := ops.ipset.add(ctx, entry.Name, entry.Addr); err != nil {
+		return err
 	}
 
 	if changed != nil {
@@ -66,11 +50,69 @@ func (ops *ops) Update(ctx context.Context, _ statedb.ReadTxn, s *tables.IPSet, 
 	return nil
 }
 
-func (ops *ops) Delete(ctx context.Context, _ statedb.ReadTxn, s *tables.IPSet) error {
-	return ops.ipset.remove(ctx, s.Name)
+func (ops *ops) Delete(ctx context.Context, _ statedb.ReadTxn, entry *tables.IPSetEntry) error {
+	if !ops.enabled {
+		return nil
+	}
+
+	// check that the set exists
+	if _, err := ops.ipset.list(ctx, entry.Name); err != nil {
+		return nil
+	}
+	return ops.ipset.del(ctx, entry.Name, entry.Addr)
 }
 
-func (ops *ops) Prune(ctx context.Context, _ statedb.ReadTxn, _ statedb.Iterator[*tables.IPSet]) error {
-	// ipsets not managed by Cilium should not be changed
+func (ops *ops) Prune(ctx context.Context, _ statedb.ReadTxn, iter statedb.Iterator[*tables.IPSetEntry]) error {
+	if !ops.enabled {
+		return nil
+	}
+
+	desiredV4Set, desiredV6Set := sets.Set[netip.Addr]{}, sets.Set[netip.Addr]{}
+	statedb.ProcessEach(iter, func(obj *tables.IPSetEntry, _ uint64) error {
+		if obj.Name == CiliumNodeIPSetV4 {
+			desiredV4Set.Insert(obj.Addr)
+		} else if obj.Name == CiliumNodeIPSetV6 {
+			desiredV6Set.Insert(obj.Addr)
+		}
+		return nil
+	})
+
+	return errors.Join(
+		reconcile(ctx, ops.ipset, CiliumNodeIPSetV4, INetFamily, desiredV4Set),
+		reconcile(ctx, ops.ipset, CiliumNodeIPSetV6, INet6Family, desiredV6Set),
+	)
+}
+
+func reconcile(
+	ctx context.Context,
+	ipset *ipset,
+	name string,
+	family Family,
+	desired sets.Set[netip.Addr],
+) error {
+	// create the IP set if it doesn't exist
+	if err := ipset.create(ctx, name, string(family)); err != nil {
+		return fmt.Errorf("unable to create ipset %s: %w", name, err)
+	}
+
+	curSet, err := ipset.list(ctx, name)
+	if err != nil {
+		return fmt.Errorf("unable to list ipset %s: %w", name, err)
+	}
+
+	toDel := curSet.Difference(desired)
+	for addr := range toDel {
+		if err := ipset.del(ctx, name, addr); err != nil {
+			return fmt.Errorf("unable to delete addr %s from ipset %s: %w", name, addr, err)
+		}
+	}
+
+	toAdd := desired.Difference(curSet)
+	for addr := range toAdd {
+		if err := ipset.add(ctx, name, addr); err != nil {
+			return fmt.Errorf("unable to add addr %s from ipset %s: %w", name, addr, err)
+		}
+	}
+
 	return nil
 }
