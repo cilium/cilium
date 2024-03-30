e3a3c9f5c53dabba03c4d2e64e94ae8f53fb4dda
Tam Mach <tam.mach@cilium.io>
2024-03-30 13:47:58 +1100

    test: Update unit-tests/build.sh
    Remove the comment related to travis, also run with all available procs.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- .github/actions/unit-tests/build.sh
+++ .github/actions/unit-tests/build.sh
@@ -4,9 +4,8 @@ set -o errexit
 
 export CFLAGS="-Werror"
 
-# Travis kills builds that don't generate any output for 10 minutes.
 # Set V=0 here to get GO/CHECK/CC lines, --quiet to hide long clang invocations.
-V=0 make precheck build -j 2 --quiet
+V=0 make precheck build -j $(nproc) --quiet
 
 # Run with default verbosity here since this builds all Go code by running
 # 'go vet' and all integration tests. At least one line of output is generated
