14fe54e328f6b1283e110b425ca97b2582775017
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2024-04-02 12:56:10 +0300

    connectivity: introduce host firewall tests
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Introduce two new tests covering the host firewall functionality, i.e.,
    asserting that both ingress and egress CiliumClusterwideNetworkPolicies
    specifying a NodeSelector correctly block the expected traffic. The
    tests are executed only when the unsafe tests are enabled, as
    potentially disruptive if executed against a live cluster.
    
    Original commit by Marco Iorio
    https://github.com/cilium/cilium-cli/commit/7a928d68204172547ff2cbf0a858da0e27233737
    has been accidentally dropped by the following commit during merge
    https://github.com/cilium/cilium-cli/commit/486eb991b228aef825294008a1ce11a098c0c377
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

---
+++ cilium-cli/connectivity/builder/host_firewall_egress.go
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package builder
+
+import (
+	_ "embed"
+
+	"github.com/cilium/cilium-cli/connectivity/check"
+	"github.com/cilium/cilium-cli/connectivity/tests"
+	"github.com/cilium/cilium-cli/utils/features"
+)
+
+//go:embed manifests/host-firewall-egress.yaml
+var hostFirewallEgressPolicyYAML string
+
+type hostFirewallEgress struct{}
+
+func (t hostFirewallEgress) build(ct *check.ConnectivityTest, _ map[string]string) {
+	newTest("host-firewall-egress", ct).
+		WithCondition(func() bool { return ct.Params().IncludeUnsafeTests }).
+		WithFeatureRequirements(features.RequireEnabled(features.HostFirewall)).
+		WithCiliumClusterwidePolicy(hostFirewallEgressPolicyYAML).
+		WithScenarios(tests.HostToPod()).
+		WithExpectations(func(a *check.Action) (egress check.Result, ingress check.Result) {
+			if a.Destination().HasLabel("name", "echo-other-node") {
+				return check.ResultPolicyDenyEgressDrop, check.ResultOK
+			}
+			return check.ResultOK, check.ResultOK
+		})
+}
