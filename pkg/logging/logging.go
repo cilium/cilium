cilium/cilium

commit a099bf1571f1a090ccfd6ccbba545828a6b3b63c

cni: use default logger with timestamps.
Unlike runtime agent/operator logs, CNI logs are just written to disk so we have no way to attach timestamps to them.
This makes it harder to debug CNI issues as we have no way to correlate when things happened between Agent logs and CNI events.

This switches CNI to use the same default logger, except with timestamps enabled.

Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

pkg/logging/logging.go
@@ -28,12 +28,20 @@ const (
 	FormatOpt = "format"
 
 	LogFormatText          LogFormat = "text"
+	LogFormatTextTimestamp LogFormat = "text-ts"
 	LogFormatJSON          LogFormat = "json"
 	LogFormatJSONTimestamp LogFormat = "json-ts"
 
 	// DefaultLogFormat is the string representation of the default logrus.Formatter
 	// we want to use (possible values: text or json)
 	DefaultLogFormat LogFormat = LogFormatText
+
+	// DefaultLogFormat is the string representation of the default logrus.Formatter
+	// including timestamps.
+	// We don't use this for general runtime logs since kubernetes log capture handles those.
+	// This is only used for applications such as CNI which is written to disk so we have no
+	// way to correlate with other logs.
+	DefaultLogFormatTimestamp LogFormat = LogFormatTextTimestamp
 
 	// DefaultLogLevel is the default log level we want to use for our logrus.Formatter
 	DefaultLogLevel logrus.Level = logrus.InfoLevel
@@ -200,6 +208,11 @@ func GetFormatter(format LogFormat) logrus.Formatter {
 	case LogFormatText:
 		return &logrus.TextFormatter{
 			DisableTimestamp: true,
+			DisableColors:    true,
+		}
+	case LogFormatTextTimestamp:
+		return &logrus.TextFormatter{
+			DisableTimestamp: false,
 			DisableColors:    true,
 		}
 	case LogFormatJSON:

plugins/cilium-cni/cmd/cmd.go
@@ -375,7 +375,7 @@ func prepareIP(ipAddr string, state *CmdState, mtu int) (*cniTypesV1.IPConfig, [
 func setupLogging(n *types.NetConf) error {
 	f := n.LogFormat
 	if f == "" {
-		f = string(logging.DefaultLogFormat)
+		f = string(logging.DefaultLogFormatTimestamp)
 	}
 	logOptions := logging.LogOptions{
 		logging.FormatOpt: f,

commit 9ba0ed8673a24306386b20421f7d8619882ad217

cni: Allow text-ts log format value
The new log format (e.g. text-ts) is added recently in the below commit,
so we need to allow it in regex. Additionally, text-ts is used as the
default value if not specified or invalid.

Fixes: a099bf1571f1a090ccfd6ccbba545828a6b3b63c
Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/logging/logging.go
@@ -36,7 +36,7 @@ const (
 	// we want to use (possible values: text or json)
 	DefaultLogFormat LogFormat = LogFormatText
 
-	// DefaultLogFormat is the string representation of the default logrus.Formatter
+	// DefaultLogFormatTimestamp is the string representation of the default logrus.Formatter
 	// including timestamps.
 	// We don't use this for general runtime logs since kubernetes log capture handles those.
 	// This is only used for applications such as CNI which is written to disk so we have no
@@ -85,7 +85,7 @@ type LogOptions map[string]string
 // settings.
 func initializeDefaultLogger() (logger *logrus.Logger) {
 	logger = logrus.New()
-	logger.SetFormatter(GetFormatter(DefaultLogFormat))
+	logger.SetFormatter(GetFormatter(DefaultLogFormatTimestamp))
 	logger.SetLevel(DefaultLogLevel)
 	return
 }
@@ -112,16 +112,16 @@ func (o LogOptions) GetLogLevel() (level logrus.Level) {
 func (o LogOptions) GetLogFormat() LogFormat {
 	formatOpt, ok := o[FormatOpt]
 	if !ok {
-		return DefaultLogFormat
+		return DefaultLogFormatTimestamp
 	}
 
 	formatOpt = strings.ToLower(formatOpt)
-	re := regexp.MustCompile(`^(text|json|json-ts)$`)
+	re := regexp.MustCompile(`^(text|text-ts|json|json-ts)$`)
 	if !re.MatchString(formatOpt) {
 		logrus.WithError(
-			fmt.Errorf("incorrect log format configured '%s', expected 'text', 'json' or 'json-ts'", formatOpt),
+			fmt.Errorf("incorrect log format configured '%s', expected 'text', 'text-ts', 'json' or 'json-ts'", formatOpt),
 		).Warning("Ignoring user-configured log format")
-		return DefaultLogFormat
+		return DefaultLogFormatTimestamp
 	}
 
 	return LogFormat(formatOpt)
@@ -149,7 +149,7 @@ func SetLogFormat(logFormat LogFormat) {
 
 // SetDefaultLogFormat updates the DefaultLogger with the DefaultLogFormat
 func SetDefaultLogFormat() {
-	DefaultLogger.SetFormatter(GetFormatter(DefaultLogFormat))
+	DefaultLogger.SetFormatter(GetFormatter(DefaultLogFormatTimestamp))
 }
 
 // AddHooks adds additional logrus hook to default logger

pkg/logging/logging_test.go
@@ -40,7 +40,7 @@ func (s *LoggingSuite) TestGetLogFormat(c *C) {
 	c.Assert(opts.GetLogFormat(), Equals, LogFormatJSON)
 
 	opts[FormatOpt] = "Invalid"
-	c.Assert(opts.GetLogFormat(), Equals, DefaultLogFormat)
+	c.Assert(opts.GetLogFormat(), Equals, DefaultLogFormatTimestamp)
 
 	opts[FormatOpt] = "JSON-TS"
 	c.Assert(opts.GetLogFormat(), Equals, LogFormatJSONTimestamp)