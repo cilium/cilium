cilium/cilium

commit 782f934641df5bafd4a9ee737e00872f65f56b64

Remove relevant metrics series on pod deletion.
Using pod source/destinationContext may result in big metrics
cardinality. Additionally this one component is exposing metrics for all
pods running on the same k8s node. Over time with each created and
removed pod number of metric data series will grow which will eventually
result in slower and bigger responses on metrics endpoint and may even
lead to OOM problems in cilium-agent itself or prometheus metrics
collector.

This change removes data series bound do certain pods when they are
deleted. This is safe, because after pod deletion its metric value will
never change.

There is a 1 minute delay introduced between pod deletion and metric
data series removal, so scrapers will have time to collect last value of
given metric.

Fixes: #23162
Signed-off-by: Marek Chodor <mchodor@google.com>

pkg/hubble/metrics/metrics.go
@@ -7,11 +7,13 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/util/workqueue"
 
 	pb "github.com/cilium/cilium/api/v1/flow"
 	"github.com/cilium/cilium/pkg/hubble/metrics/api"
@@ -25,11 +27,18 @@ import (
 	_ "github.com/cilium/cilium/pkg/hubble/metrics/policy"            // invoke init
 	_ "github.com/cilium/cilium/pkg/hubble/metrics/port-distribution" // invoke init
 	_ "github.com/cilium/cilium/pkg/hubble/metrics/tcp"               // invoke init
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 )
 
+type PodDeletionHandler struct {
+	gracefulPeriod time.Duration
+	queue          workqueue.DelayingInterface
+}
+
 var (
-	enabledMetrics *api.Handlers
-	registry       = prometheus.NewPedanticRegistry()
+	enabledMetrics     *api.Handlers
+	registry           = prometheus.NewPedanticRegistry()
+	podDeletionHandler *PodDeletionHandler
 )
 
 // ProcessFlow processes a flow and updates metrics
@@ -40,18 +49,18 @@ func ProcessFlow(ctx context.Context, flow *pb.Flow) error {
 	return nil
 }
 
-// initMetrics initialies the metrics system
-func initMetrics(address string, enabled api.Map, grpcMetrics *grpc_prometheus.ServerMetrics, enableOpenMetrics bool) (<-chan error, error) {
-	e, err := api.DefaultRegistry().ConfigureHandlers(registry, enabled)
-	if err != nil {
-		return nil, err
+func ProcessPodDeletion(pod *slim_corev1.Pod) error {
+	if podDeletionHandler != nil && enabledMetrics != nil {
+		podDeletionHandler.queue.AddAfter(pod, podDeletionHandler.gracefulPeriod)
 	}
-	enabledMetrics = e
+	return nil
+}
 
-	registry.MustRegister(grpcMetrics)
+func initMetricHandlers(enabled api.Map) (*api.Handlers, error) {
+	return api.DefaultRegistry().ConfigureHandlers(registry, enabled)
+}
 
-	errChan := make(chan error, 1)
-
+func initMetricsServer(address string, enableOpenMetrics bool, errChan chan error) {
 	go func() {
 		mux := http.NewServeMux()
 		mux.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{
@@ -63,6 +72,40 @@ func initMetrics(address string, enabled api.Map, grpcMetrics *grpc_prometheus.S
 		}
 		errChan <- srv.ListenAndServe()
 	}()
+
+}
+
+func initPodDeletionHandler() {
+	podDeletionHandler = &PodDeletionHandler{
+		gracefulPeriod: time.Minute,
+		queue:          workqueue.NewDelayingQueue(),
+	}
+
+	go func() {
+		for {
+			pod, quit := podDeletionHandler.queue.Get()
+			if quit {
+				return
+			}
+			enabledMetrics.ProcessPodDeletion(pod.(*slim_corev1.Pod))
+		}
+	}()
+}
+
+// initMetrics initializes the metrics system
+func initMetrics(address string, enabled api.Map, grpcMetrics *grpc_prometheus.ServerMetrics, enableOpenMetrics bool) (<-chan error, error) {
+	e, err := initMetricHandlers(enabled)
+	if err != nil {
+		return nil, err
+	}
+	enabledMetrics = e
+
+	registry.MustRegister(grpcMetrics)
+
+	errChan := make(chan error, 1)
+
+	initMetricsServer(address, enableOpenMetrics, errChan)
+	initPodDeletionHandler()
 
 	return errChan, nil
 }

pkg/hubble/metrics/api/api.go
@@ -13,6 +13,7 @@ import (
 	"go.uber.org/multierr"
 
 	pb "github.com/cilium/cilium/api/v1/flow"
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 )
@@ -73,6 +74,12 @@ type Handler interface {
 	// specifies Prometheus registry
 	Init(registry *prometheus.Registry, options Options) error
 
+	// ListMetricVec returns an array of MetricVec used by a handler
+	ListMetricVec() []*prometheus.MetricVec
+
+	// Context used by this metrics handler
+	Context() *ContextOptions
+
 	// Status returns the configuration status of the metric handler
 	Status() string
 }
@@ -118,6 +125,18 @@ func (h Handlers) ProcessFlow(ctx context.Context, flow *pb.Flow) error {
 		processingErr = multierr.Append(processingErr, err)
 	}
 	return processingErr
+}
+
+// ProcessPodDeletion queries all handlers for a list of MetricVec and removes
+// metrics directly associated to deleted pod.
+func (h Handlers) ProcessPodDeletion(pod *slim_corev1.Pod) {
+	for _, h := range h.handlers {
+		for _, mv := range h.ListMetricVec() {
+			if ctx := h.Context(); ctx != nil {
+				ctx.DeleteMetricsAssociatedWithPod(pod.GetName(), pod.GetNamespace(), mv)
+			}
+		}
+	}
 }
 
 var registry = NewRegistry(

pkg/hubble/metrics/api/context.go
@@ -9,6 +9,7 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/utils/strings/slices"
 
 	pb "github.com/cilium/cilium/api/v1/flow"
@@ -137,12 +138,16 @@ type ContextOptions struct {
 	// Destination is the destination context to include in metrics for ingress traffic (overrides Destination)
 	DestinationIngress ContextIdentifierList
 
+	allDestinationCtx ContextIdentifierList
+
 	// Source is the source context to include in metrics for both egress and ingress traffic
 	Source ContextIdentifierList
 	// Source is the source context to include in metrics for egress traffic (overrides Source)
 	SourceEgress ContextIdentifierList
 	// Source is the source context to include in metrics for ingress traffic (overrides Source)
 	SourceIngress ContextIdentifierList
+
+	allSourceCtx ContextIdentifierList
 
 	// Labels is the full set of labels that have been allowlisted when using the
 	// ContextLabels ContextIdentifier.
@@ -206,31 +211,37 @@ func ParseContextOptions(options Options) (*ContextOptions, error) {
 		switch strings.ToLower(key) {
 		case "destinationcontext":
 			o.Destination, err = parseContext(value)
+			o.allDestinationCtx = append(o.allDestinationCtx, o.Destination...)
 			if err != nil {
 				return nil, err
 			}
 		case "destinationegresscontext":
 			o.DestinationEgress, err = parseContext(value)
+			o.allDestinationCtx = append(o.allDestinationCtx, o.DestinationEgress...)
 			if err != nil {
 				return nil, err
 			}
 		case "destinationingresscontext":
 			o.DestinationIngress, err = parseContext(value)
+			o.allDestinationCtx = append(o.allDestinationCtx, o.DestinationIngress...)
 			if err != nil {
 				return nil, err
 			}
 		case "sourcecontext":
 			o.Source, err = parseContext(value)
+			o.allSourceCtx = append(o.allSourceCtx, o.Source...)
 			if err != nil {
 				return nil, err
 			}
 		case "sourceegresscontext":
 			o.SourceEgress, err = parseContext(value)
+			o.allSourceCtx = append(o.allSourceCtx, o.SourceEgress...)
 			if err != nil {
 				return nil, err
 			}
 		case "sourceingresscontext":
 			o.SourceIngress, err = parseContext(value)
+			o.allSourceCtx = append(o.allSourceCtx, o.SourceIngress...)
 			if err != nil {
 				return nil, err
 			}
@@ -523,4 +534,34 @@ func (o *ContextOptions) Status() string {
 	sort.Strings(status)
 
 	return strings.Join(status, ",")
+}
+
+func (o *ContextOptions) DeleteMetricsAssociatedWithPod(name string, namespace string, vec *prometheus.MetricVec) {
+	for _, contextID := range o.allSourceCtx {
+		if contextID == ContextPod {
+			vec.DeletePartialMatch(prometheus.Labels{
+				"source": namespace + "/" + name,
+			})
+		}
+	}
+	for _, contextID := range o.allDestinationCtx {
+		if contextID == ContextPod {
+			vec.DeletePartialMatch(prometheus.Labels{
+				"destination": namespace + "/" + name,
+			})
+		}
+	}
+
+	if o.Labels.HasLabel("source_pod") && o.Labels.HasLabel("source_namespace") {
+		vec.DeletePartialMatch(prometheus.Labels{
+			"source_namespace": namespace,
+			"source_pod":       name,
+		})
+	}
+	if o.Labels.HasLabel("destination_pod") && o.Labels.HasLabel("destination_namespace") {
+		vec.DeletePartialMatch(prometheus.Labels{
+			"destination_namespace": namespace,
+			"destination_pod":       name,
+		})
+	}
 }

pkg/hubble/metrics/api/registry_test.go
@@ -11,14 +11,20 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	pb "github.com/cilium/cilium/api/v1/flow"
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 )
 
-type testPlugin struct{}
+type testPlugin struct {
+	handler Handler
+}
 
 func (t *testPlugin) NewHandler() Handler {
-	return &testHandler{}
+	return t.handler
 }
 
 func (t *testPlugin) HelpText() string {
@@ -26,8 +32,11 @@ func (t *testPlugin) HelpText() string {
 }
 
 type testHandler struct {
-	ProcessCalled int
-	InitCalled    int
+	ContextOptions   *ContextOptions
+	counter          *prometheus.CounterVec
+	ProcessCalled    int
+	InitCalled       int
+	ListMetricCalled int
 }
 
 func (t *testHandler) Init(registry *prometheus.Registry, options Options) error {
@@ -39,27 +48,232 @@ func (t *testHandler) Status() string {
 	return ""
 }
 
+func (t *testHandler) Context() *ContextOptions {
+	return t.ContextOptions
+}
+
+func (t *testHandler) ListMetricVec() []*prometheus.MetricVec {
+	t.ListMetricCalled++
+	return []*prometheus.MetricVec{t.counter.MetricVec}
+}
+
 func (t *testHandler) ProcessFlow(ctx context.Context, p *pb.Flow) error {
+	labels, _ := t.ContextOptions.GetLabelValues(p)
+	t.counter.WithLabelValues(labels...).Inc()
 	t.ProcessCalled++
 	return nil
 }
 
 func TestRegister(t *testing.T) {
+
+	flow1 := &pb.Flow{
+		EventType: &pb.CiliumEventType{Type: monitorAPI.MessageTypeAccessLog},
+		L7: &pb.Layer7{
+			Record: &pb.Layer7_Http{Http: &pb.HTTP{}},
+		},
+		Source:      &pb.Endpoint{Namespace: "foo", PodName: "foo-123"},
+		Destination: &pb.Endpoint{Namespace: "bar", PodName: "bar-123"},
+		Verdict:     pb.Verdict_FORWARDED,
+	}
+
+	flow2 := &pb.Flow{
+		EventType: &pb.CiliumEventType{Type: monitorAPI.MessageTypeAccessLog},
+		L7: &pb.Layer7{
+			Record: &pb.Layer7_Http{Http: &pb.HTTP{}},
+		},
+		Source:      &pb.Endpoint{Namespace: "abc", PodName: "abc-456"},
+		Destination: &pb.Endpoint{Namespace: "bar", PodName: "bar-123"},
+		Verdict:     pb.Verdict_FORWARDED,
+	}
 	log := logrus.New()
 	log.SetOutput(io.Discard)
+
+	t.Run("Should not register handler", func(t *testing.T) {
+
+		r := NewRegistry(log)
+
+		handler := &testHandler{}
+
+		r.Register("test", &testPlugin{handler: handler})
+
+		handlers, err := r.ConfigureHandlers(nil, Map{})
+		assert.EqualValues(t, err, nil)
+		assert.EqualValues(t, len(handlers.handlers), 0)
+	})
+
+	t.Run("Should register handler", func(t *testing.T) {
+
+		promRegistry := prometheus.NewRegistry()
+		opts, _ := ParseContextOptions(Options{"sourceContext": "pod", "destinationContext": "pod"})
+		initHandlers(t, opts, promRegistry, log)
+	})
+
+	t.Run("Should remove metrics series with ContextPod", func(t *testing.T) {
+
+		promRegistry := prometheus.NewRegistry()
+		opts, _ := ParseContextOptions(Options{"sourceContext": "pod", "destinationContext": "pod"})
+		handlers := initHandlers(t, opts, promRegistry, log)
+
+		handlers.ProcessFlow(context.TODO(), flow1)
+		handlers.ProcessFlow(context.TODO(), flow2)
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ProcessCalled, 2)
+
+		verifyMetricSeriesExists(t, promRegistry, 2)
+
+		handlers.ProcessPodDeletion(&slim_corev1.Pod{
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:      "foo-123",
+				Namespace: "foo",
+			},
+		})
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ListMetricCalled, 1)
+
+		verifyMetricSeriesExists(t, promRegistry, 1)
+
+		handlers.ProcessPodDeletion(&slim_corev1.Pod{
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:      "bar-123",
+				Namespace: "bar",
+			},
+		})
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ListMetricCalled, 2)
+
+		verifyMetricSeriesNotExists(t, promRegistry)
+	})
+
+	t.Run("Should not remove metrics series with ContextPodShort", func(t *testing.T) {
+
+		promRegistry := prometheus.NewRegistry()
+		opts, _ := ParseContextOptions(Options{"sourceContext": "pod-short", "destinationContext": "pod-short"})
+		handlers := initHandlers(t, opts, promRegistry, log)
+
+		handlers.ProcessFlow(context.TODO(), flow1)
+		handlers.ProcessFlow(context.TODO(), flow2)
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ProcessCalled, 2)
+
+		verifyMetricSeriesExists(t, promRegistry, 2)
+
+		handlers.ProcessPodDeletion(&slim_corev1.Pod{
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:      "foo-123",
+				Namespace: "foo",
+			},
+		})
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ListMetricCalled, 1)
+
+		verifyMetricSeriesExists(t, promRegistry, 2)
+
+		handlers.ProcessPodDeletion(&slim_corev1.Pod{
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:      "bar-123",
+				Namespace: "bar",
+			},
+		})
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ListMetricCalled, 2)
+
+		verifyMetricSeriesExists(t, promRegistry, 2)
+	})
+
+	t.Run("Should remove metrics series with LabelsContext", func(t *testing.T) {
+
+		promRegistry := prometheus.NewRegistry()
+		opts, _ := ParseContextOptions(Options{"labelsContext": "source_pod,source_namespace,destination_pod,destination_namespace"})
+		handlers := initHandlers(t, opts, promRegistry, log)
+
+		handlers.ProcessFlow(context.TODO(), flow1)
+		handlers.ProcessFlow(context.TODO(), flow2)
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ProcessCalled, 2)
+
+		verifyMetricSeriesExists(t, promRegistry, 2)
+
+		handlers.ProcessPodDeletion(&slim_corev1.Pod{
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:      "foo-123",
+				Namespace: "foo",
+			},
+		})
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ListMetricCalled, 1)
+
+		verifyMetricSeriesExists(t, promRegistry, 1)
+
+		handlers.ProcessPodDeletion(&slim_corev1.Pod{
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:      "bar-123",
+				Namespace: "bar",
+			},
+		})
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ListMetricCalled, 2)
+
+		verifyMetricSeriesNotExists(t, promRegistry)
+	})
+
+	t.Run("Should not remove metrics series with LabelsContext without namespace", func(t *testing.T) {
+
+		promRegistry := prometheus.NewRegistry()
+		opts, _ := ParseContextOptions(Options{"labelsContext": "source_pod,destination_pod"})
+		handlers := initHandlers(t, opts, promRegistry, log)
+
+		handlers.ProcessFlow(context.TODO(), flow1)
+		handlers.ProcessFlow(context.TODO(), flow2)
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ProcessCalled, 2)
+
+		verifyMetricSeriesExists(t, promRegistry, 2)
+
+		handlers.ProcessPodDeletion(&slim_corev1.Pod{
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:      "foo-123",
+				Namespace: "foo",
+			},
+		})
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ListMetricCalled, 1)
+
+		verifyMetricSeriesExists(t, promRegistry, 2)
+
+		handlers.ProcessPodDeletion(&slim_corev1.Pod{
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:      "bar-123",
+				Namespace: "bar",
+			},
+		})
+		assert.EqualValues(t, handlers.handlers[0].(*testHandler).ListMetricCalled, 2)
+
+		verifyMetricSeriesExists(t, promRegistry, 2)
+	})
+
+}
+
+func initHandlers(t *testing.T, opts *ContextOptions, promRegistry *prometheus.Registry, log *logrus.Logger) *Handlers {
+	counter := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "test",
+		Name:      "events",
+	}, opts.GetLabelNames())
+	promRegistry.MustRegister(counter)
+
 	r := NewRegistry(log)
 
-	r.Register("test", &testPlugin{})
+	handler := &testHandler{}
+	handler.ContextOptions = opts
+	handler.counter = counter
 
-	handlers, err := r.ConfigureHandlers(nil, Map{})
-	assert.EqualValues(t, err, nil)
-	assert.EqualValues(t, len(handlers.handlers), 0)
+	r.Register("test", &testPlugin{handler: handler})
 
-	handlers, err = r.ConfigureHandlers(nil, Map{"test": Options{}})
+	handlers, err := r.ConfigureHandlers(nil, Map{"test": Options{}})
 	assert.EqualValues(t, err, nil)
 	assert.EqualValues(t, len(handlers.handlers), 1)
 	assert.EqualValues(t, handlers.handlers[0].(*testHandler).InitCalled, 1)
+	return handlers
+}
 
-	handlers.ProcessFlow(context.TODO(), nil)
-	assert.EqualValues(t, handlers.handlers[0].(*testHandler).ProcessCalled, 1)
+func verifyMetricSeriesExists(t *testing.T, promRegistry *prometheus.Registry, expectedCount int) {
+	metricFamilies, err := promRegistry.Gather()
+	require.NoError(t, err)
+	require.Len(t, metricFamilies, 1)
+	assert.Equal(t, "test_events", *metricFamilies[0].Name)
+	require.Len(t, metricFamilies[0].Metric, expectedCount)
+}
+
+func verifyMetricSeriesNotExists(t *testing.T, promRegistry *prometheus.Registry) {
+	metricFamilies, err := promRegistry.Gather()
+	require.NoError(t, err)
+	require.Len(t, metricFamilies, 0)
 }

pkg/hubble/metrics/dns/handler.go
@@ -87,6 +87,14 @@ func (d *dnsHandler) Status() string {
 	return strings.Join(append(status, d.context.Status()), ",")
 }
 
+func (d *dnsHandler) Context() *api.ContextOptions {
+	return d.context
+}
+
+func (d *dnsHandler) ListMetricVec() []*prometheus.MetricVec {
+	return []*prometheus.MetricVec{d.queries.MetricVec, d.responses.MetricVec, d.responseTypes.MetricVec}
+}
+
 func (d *dnsHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	if flow.GetL7() == nil {
 		return nil

pkg/hubble/metrics/drop/handler.go
@@ -42,6 +42,14 @@ func (d *dropHandler) Status() string {
 	return d.context.Status()
 }
 
+func (d *dropHandler) Context() *api.ContextOptions {
+	return d.context
+}
+
+func (d *dropHandler) ListMetricVec() []*prometheus.MetricVec {
+	return []*prometheus.MetricVec{d.drops.MetricVec}
+}
+
 func (d *dropHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	if flow.GetVerdict() != flowpb.Verdict_DROPPED {
 		return nil

pkg/hubble/metrics/flow/handler.go
@@ -44,6 +44,14 @@ func (h *flowHandler) Status() string {
 	return h.context.Status()
 }
 
+func (h *flowHandler) Context() *api.ContextOptions {
+	return h.context
+}
+
+func (h *flowHandler) ListMetricVec() []*prometheus.MetricVec {
+	return []*prometheus.MetricVec{h.flows.MetricVec}
+}
+
 func (h *flowHandler) ProcessFlow(ctx context.Context, flow *flowpb.Flow) error {
 	labelValues, err := h.context.GetLabelValues(flow)
 	if err != nil {

pkg/hubble/metrics/flow/handler_test.go
@@ -128,4 +128,5 @@ func TestFlowHandler(t *testing.T) {
 		assert.Equal(t, "verdict", *metric.Label[5].Name)
 		assert.Equal(t, "DROPPED", *metric.Label[5].Value)
 	})
+
 }

commit a5526d9815a6deed4ca578c3c290c6c2adc23e1b

metric: Avoid memory leak/increase
This commit is to make sure that the processed item in pod deletion
queue is removed by explicitly call Done() function as per suggestion
in godoc[^1].

The impact of not having this change will be increasing of memory in
cilium agent when the hubble metrics are enabled. This might take days
(if not weeks) to observe in a normal Cilium deployment due to low number
of Pod deletion events (i.e. in high churn environment, the memory will
be increasing in a faster pace).

Testing is done before and after the changes as per below.

Sample workload to simulate high number of pod deletion events

```yaml
apiVersion: batch/v1
kind: Job
metadata:
  name: pod-churn-job
spec:
  completions: 50000000
  parallelism: 100
  template:
    metadata:
      labels:
        app: pod-churn-job
    spec:
      containers:
      - name: churn-app
        image: sandeshkv92/highpodchurn:linux_amd64
      restartPolicy: Never
```

Before this change, the cilium agent memory keeps increasing from 150MB
to ~500MB in less than 3 hours, while with the same workload configured
and this change, the memory is quite stable for a longer period (e.g. 5
hours).

[^1]: https://pkg.go.dev/k8s.io/client-go@v0.29.3/util/workqueue#Type.Get

Fixes: 782f934641df5bafd4a9ee737e00872f65f56b64
Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/hubble/metrics/metrics.go
@@ -117,6 +117,7 @@ func initPodDeletionHandler() {
 				return
 			}
 			enabledMetrics.ProcessPodDeletion(pod.(*slim_corev1.Pod))
+			podDeletionHandler.queue.Done(pod)
 		}
 	}()
 }