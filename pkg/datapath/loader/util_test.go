d7688ec761ada4513bfb85aa125f3a3c03c77856
Lorenz Bauer <lmb@isovalent.com>
2024-04-05 15:28:25 +0100

    loader: rewrite cache tests
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- pkg/datapath/loader/util_test.go
+++ pkg/datapath/loader/util_test.go
@@ -4,9 +4,12 @@
 package loader
 
 import (
+	"fmt"
+	"path/filepath"
 	"testing"
 
 	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 func setupLocalNodeStore(tb testing.TB) {
@@ -15,4 +18,24 @@ func setupLocalNodeStore(tb testing.TB) {
 	node.SetInternalIPv4Router(templateIPv4[:])
 	node.SetIPv4Loopback(templateIPv4[:])
 	tb.Cleanup(node.UnsetTestLocalNodeStore)
+}
+
+func setupCompilationDirectories(tb testing.TB) {
+	option.Config.DryMode = true
+	option.Config.BpfDir = bpfDir
+	option.Config.StateDir = bpfDir
+	testIncludes = []string{
+		// Unit tests rely on using bpf/ep_config.h instead of
+		// the real per endpoint config. Otherwise you get compilation
+		// errors due to redefined macros and such. *sigh*
+		fmt.Sprintf("-I%s", bpfDir),
+		fmt.Sprintf("-I%s", filepath.Join(bpfDir, "include")),
+	}
+
+	tb.Cleanup(func() {
+		option.Config.DryMode = false
+		option.Config.BpfDir = ""
+		option.Config.StateDir = ""
+		testIncludes = nil
+	})
 }
