486f198af1a19e821ec54d7a3c39bb71606555d6
Louis DeLosSantos <louis.delos@isovalent.com>
2024-04-04 23:18:17 -0400

    bpf: use node map spi field in set_ipsec_encrypt
    Updates `set_ipsec_encrypt` to allow an SPI to be retrieved from the
    node map when creating an encryption mark.
    
    This is especially useful when Encrypted Overlay is enabled, allowing us
    to obtain the SPI via the VXLAN tunnel endpoint IP.
    
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- bpf/lib/encap.h
+++ bpf/lib/encap.h
@@ -75,7 +75,7 @@ encap_and_redirect_with_nodeid(struct __ctx_buff *ctx, __be32 tunnel_endpoint,
 #ifdef ENABLE_IPSEC
 	if (encrypt_key)
 		return set_ipsec_encrypt(ctx, encrypt_key, tunnel_endpoint,
-					 seclabel, true);
+					 seclabel, true, false);
 #endif
 
 	return __encap_and_redirect_with_nodeid(ctx, 0, tunnel_endpoint,
@@ -97,7 +97,7 @@ __encap_and_redirect_lxc(struct __ctx_buff *ctx, __be32 tunnel_endpoint,
 #ifdef ENABLE_IPSEC
 	if (encrypt_key)
 		return set_ipsec_encrypt(ctx, encrypt_key, tunnel_endpoint,
-					 seclabel, false);
+					 seclabel, false, false);
 #endif
 
 	return encap_and_redirect_with_nodeid(ctx, tunnel_endpoint, 0, seclabel,
@@ -147,7 +147,7 @@ encap_and_redirect_lxc(struct __ctx_buff *ctx,
 		__u8 min_encrypt_key = get_min_encrypt_key(tunnel->key);
 
 		return set_ipsec_encrypt(ctx, min_encrypt_key, tunnel->ip4,
-					 seclabel, false);
+					 seclabel, false, false);
 	}
 # endif
 	return encap_and_redirect_with_nodeid(ctx, tunnel->ip4, 0, seclabel, dstid,
@@ -169,7 +169,7 @@ encap_and_redirect_netdev(struct __ctx_buff *ctx, struct tunnel_key *k,
 #ifdef ENABLE_IPSEC
 	if (encrypt_key)
 		return set_ipsec_encrypt(ctx, encrypt_key, tunnel->ip4,
-					 seclabel, true);
+					 seclabel, true, false);
 #endif
 
 	return encap_and_redirect_with_nodeid(ctx, tunnel->ip4, 0, seclabel, 0,
