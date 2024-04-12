c86e680f436a7561231fd0f836b18f47c166f758
Gray Liang <gray.liang@isovalent.com>
2024-04-12 17:48:58 +0800

    proxy/routes: Remove fromEgressProxyRule for cilium downgrade
    [ upstream commit: 53133ff80ef4408ae673e5aa030f645bcd449afa ]
    
    [ backporter's note: v1.13 uses bpf/init.sh to install proxy rules so we
    have to do a customized backport. ]
    
    Although we don't install fromEgressProxyRule for now, this commit
    insists on removing it to make sure further downgrade can go smoothly.
    
    Soon We'll have another PR to install fromEgressProxyRule, and cilium
    downgrade from that PR to branch tip (patch downgrade, 1.X.Y ->
    1.X.{Y-1}) will be broken if we don't handle the new ip rule carefullly.
    
    Without this patch, downgrade from higher version will leave
    fromEgressProxyRule on the lower version cilium, cluster will be in a
    wrong status of "having stale ip rule + not having other necessary
    settings (iptables)", breaking the connectivity.
    
    Signed-off-by: Zhichuan Liang <gray.liang@isovalent.com>
    Signed-off-by: Zhichuan Liang <gray.liang@isovalent.com>

--- bpf/init.sh
+++ bpf/init.sh
@@ -145,6 +145,9 @@ function setup_proxy_rules()
 					ip -4 rule delete $from_ingress_rulespec || true
 				fi
 			fi
+                        if [ ! -z "$(ip -4 rule list $from_egress_rulespec)" ]; then
+                                ip -4 rule delete $from_egress_rulespec 2> /dev/null || true
+                        fi
 		fi
 
 		# Traffic to the host proxy is local
@@ -178,6 +181,9 @@ function setup_proxy_rules()
 					ip -6 rule delete $from_ingress_rulespec
 				fi
 			fi
+                        if [ ! -z "$(ip -6 rule list $from_egress_rulespec)" ]; then
+                                ip -6 rule delete $from_egress_rulespec 2> /dev/null || true
+                        fi
 		fi
 
 		IP6_LLADDR=$(ip -6 addr show dev $HOST_DEV2 | grep inet6 | head -1 | awk '{print $2}' | awk -F'/' '{print $1}')
