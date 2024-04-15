c3097bfe3fb5afa09afbb017652fbd4851e61630
Gray Liang <gray.liang@isovalent.com>
2024-04-15 16:42:37 +0800

    proxy/routes: Remove fromEgressProxyRule for cilium downgrade
    Although we don't install fromEgressProxyRule for now, this commit
    insists on removing it to make sure further downgrade can go smoothly.
    
    Soon We'll have another PR to install fromEgressProxyRule, and cilium
    downgrade from that PR to branch tip (patch downgrade, 1.X.Y ->
    1.X.{Y-1}) will be broken if we don't handle the new ip rule carefullly.
    
    Without this patch, downgrade from higher version will leave
    fromEgressProxyRule on the lower version cilium, cluster will be in a
    wrong status of "having stale ip rule + not having other necessary
    settings (iptables)", breaking the connectivity.
    
    Signed-off-by: Zhichuan Liang <gray.liang@isovalent.com>

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -408,6 +408,9 @@ func (p *Proxy) ReinstallRoutingRules() error {
 			return err
 		}
 
+		if err := removeFromEgressProxyRoutesIPv4(); err != nil {
+			return err
+		}
 		if !option.Config.EnableIPSec || option.Config.TunnelingEnabled() {
 			if err := removeFromIngressProxyRoutesIPv4(); err != nil {
 				return err
@@ -431,6 +434,9 @@ func (p *Proxy) ReinstallRoutingRules() error {
 			return err
 		}
 
+		if err := removeFromEgressProxyRoutesIPv6(); err != nil {
+			return err
+		}
 		if !option.Config.EnableIPSec || option.Config.TunnelingEnabled() {
 			if err := removeFromIngressProxyRoutesIPv6(); err != nil {
 				return err
