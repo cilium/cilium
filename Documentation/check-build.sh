a9de0c003747895ae172459938dc3cfcdd816c40
Quentin Monnet <qmo@qmon.net>
2024-04-16 16:32:39 +0100

    docs: Document the build framework for documentation
    Add documentation about "everything related to the build and maintenance
    of Cilium's documentation", with the hope that it may help contributors
    understand how this is all connected, in the future.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- Documentation/check-build.sh
+++ Documentation/check-build.sh
@@ -107,7 +107,7 @@ run_linter() {
         --ignore-languages "bash,c" \
         --ignore-messages "${ignored_messages}" \
         --ignore-directives "tabs,openapi" \
-        --ignore-roles "${CONF_PY_ROLES},spelling:ignore" \
+        --ignore-roles "${CONF_PY_ROLES},spelling:ignore,spelling:word" \
         --ignore-substitutions "${CONF_PY_SUBSTITUTIONS}" \
        -r . ../README.rst 2>&1 | \
        grep -v 'WARNING:rstcheck_core.checker:An `AttributeError` error occured. This is most probably due to a code block directive (code/code-block/sourcecode) without a specified language.'
