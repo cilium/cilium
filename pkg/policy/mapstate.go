cilium/cilium

commit 2f829958b714feae93fd65c8e39d6fdaf54c8a3f

daemon,test: remove enableRemoteNodeIdentity flag
The enable-remote-node-identity agent flag was marked as deprecated for
1.15 in commit cf472ef ("daemon: Deprecate
EnableRemoteNodeIdentity").

Signed-off-by: Donia Chaiehloudj <donia.cld@isovalent.com>

pkg/policy/mapstate.go
@@ -28,11 +28,6 @@ var (
 		Identity:         identity.ReservedIdentityHost.Uint32(),
 		TrafficDirection: trafficdirection.Ingress.Uint8(),
 	}
-	// localRemoteNodeKey represents an ingress L3 allow from remote nodes.
-	localRemoteNodeKey = Key{
-		Identity:         identity.ReservedIdentityRemoteNode.Uint32(),
-		TrafficDirection: trafficdirection.Ingress.Uint8(),
-	}
 	// allKey represents a key for unknown traffic, i.e., all traffic.
 	allKey = Key{
 		Identity: identity.IdentityUnknown.Uint32(),
@@ -1296,18 +1291,6 @@ func (ms *mapState) determineAllowLocalhostIngress() {
 		}
 		es := NewMapStateEntry(nil, derivedFrom, 0, "", 0, false, ExplicitAuthType, AuthTypeDisabled) // Authentication never required for local host ingress
 		ms.denyPreferredInsert(localHostKey, es, nil, allFeatures)
-		if !option.Config.EnableRemoteNodeIdentity {
-			var isHostDenied bool
-			v, ok := ms.Get(localHostKey)
-			isHostDenied = ok && v.IsDeny
-			derivedFrom := labels.LabelArrayList{
-				labels.LabelArray{
-					labels.NewLabel(LabelKeyPolicyDerivedFrom, LabelAllowRemoteHostIngress, labels.LabelSourceReserved),
-				},
-			}
-			es := NewMapStateEntry(nil, derivedFrom, 0, "", 0, isHostDenied, ExplicitAuthType, AuthTypeDisabled) // Authentication never required for remote node ingress
-			ms.denyPreferredInsert(localRemoteNodeKey, es, nil, allFeatures)
-		}
 	}
 }
 

.github/actions/ginkgo/main-focus.yaml
@@ -64,7 +64,6 @@ include:
   ###
   # K8sAgentPolicyTest Multi-node policy test validates fromEntities policies Validates fromEntities all policy
   # K8sAgentPolicyTest Multi-node policy test validates fromEntities policies Validates fromEntities cluster policy
-  # K8sAgentPolicyTest Multi-node policy test validates fromEntities policies with remote-node identity disabled Allows from all hosts with cnp fromEntities host policy
   # K8sAgentPolicyTest Multi-node policy test validates fromEntities policies with remote-node identity enabled Validates fromEntities remote-node policy
   # K8sAgentPolicyTest Multi-node policy test with L7 policy using connectivity-check to check datapath
   - focus: "f04-agent-policy-multi-node-1"

daemon/cmd/daemon.go
@@ -642,8 +642,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		})
 	}
 
-	treatRemoteNodeAsHost := option.Config.AlwaysAllowLocalhost() && !option.Config.EnableRemoteNodeIdentity
-	policyAPI.InitEntities(params.ClusterInfo.Name, treatRemoteNodeAsHost)
+	policyAPI.InitEntities(params.ClusterInfo.Name)
 
 	bootstrapStats.restore.Start()
 	// fetch old endpoints before k8s is configured.
@@ -750,9 +749,6 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		case !option.Config.EnableNodePort:
 			err = fmt.Errorf("BPF masquerade requires NodePort (--%s=\"true\")",
 				option.EnableNodePort)
-		case !option.Config.EnableRemoteNodeIdentity:
-			err = fmt.Errorf("BPF masquerade requires remote node identities (--%s=\"true\")",
-				option.EnableRemoteNodeIdentity)
 		case len(option.Config.MasqueradeInterfaces) > 0:
 			err = fmt.Errorf("BPF masquerade does not allow to specify devices via --%s (use --%s instead)",
 				option.MasqueradeInterfaces, option.Devices)

daemon/cmd/daemon_main.go
@@ -282,10 +282,6 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.String(option.IPv6MCastDevice, "", "Device that joins a Solicited-Node multicast group for IPv6")
 	option.BindEnv(vp, option.IPv6MCastDevice)
 
-	flags.Bool(option.EnableRemoteNodeIdentity, defaults.EnableRemoteNodeIdentity, "Enable use of remote node identity")
-	flags.MarkDeprecated(option.EnableRemoteNodeIdentity, "Remote Node Identity is needed for various other features to work or work fully, including EgressGateway and Policies. There is no benefit to having it turned off. It will be removed in v1.16.")
-	option.BindEnv(vp, option.EnableRemoteNodeIdentity)
-
 	flags.String(option.EncryptInterface, "", "Transparent encryption interface")
 	option.BindEnv(vp, option.EncryptInterface)
 
@@ -1378,10 +1374,6 @@ func initEnv(vp *viper.Viper) {
 	if option.Config.EnableHostFirewall {
 		if option.Config.EnableIPSec {
 			log.Fatal("IPSec cannot be used with the host firewall.")
-		}
-		if option.Config.EnableEndpointRoutes && !option.Config.EnableRemoteNodeIdentity {
-			log.Fatalf("The host firewall requires remote-node identities (%s) when running with %s",
-				option.EnableRemoteNodeIdentity, option.EnableEndpointRoutes)
 		}
 	}
 

daemon/cmd/status_test.go
@@ -30,10 +30,9 @@ var (
 	nm         manager.NodeManager
 	mtuConfig  = mtu.NewConfiguration(0, false, false, false, false, 0, nil)
 	fakeConfig = &option.DaemonConfig{
-		RoutingMode:              option.RoutingModeTunnel,
-		EnableRemoteNodeIdentity: true,
-		EnableIPSec:              true,
-		EncryptNode:              true,
+		RoutingMode: option.RoutingModeTunnel,
+		EnableIPSec: true,
+		EncryptNode: true,
 	}
 )
 

pkg/defaults/defaults.go
@@ -467,9 +467,6 @@ const (
 	// specified in the L7 policies.
 	CertsDirectory = RuntimePath + "/certs"
 
-	// EnableRemoteNodeIdentity is the default value for option.EnableRemoteNodeIdentity
-	EnableRemoteNodeIdentity = true
-
 	// IPAMExpiration is the timeout after which an IP subject to expiratio
 	// is being released again if no endpoint is being created in time.
 	IPAMExpiration = 10 * time.Minute

pkg/egressgateway/manager.go
@@ -189,12 +189,6 @@ func NewEgressGatewayManager(p Params) (out struct {
 		return out, fmt.Errorf("egress gateway requires --%s=\"true\" and --%s=\"true\"", option.EnableIPv4Masquerade, option.EnableBPFMasquerade)
 	}
 
-	if !dcfg.EnableRemoteNodeIdentity {
-		// datapath code depends on remote node identities to distinguish between
-		// cluster-local and cluster-egress traffic.
-		return out, fmt.Errorf("egress gateway requires remote node identities (--%s=\"true\")", option.EnableRemoteNodeIdentity)
-	}
-
 	if dcfg.EnableL7Proxy {
 		log.WithField(logfields.URL, "https://github.com/cilium/cilium/issues/19642").
 			Warningf("both egress gateway and L7 proxy (--%s) are enabled. This is currently not fully supported: "+

pkg/ipcache/kvstore.go
@@ -312,13 +312,12 @@ func (iw *IPIdentityWatcher) OnUpdate(k storepkg.Key) {
 	}
 
 	peerIdentity := ipIDPair.ID
-	if option.Config.EnableRemoteNodeIdentity && peerIdentity == identity.ReservedIdentityHost {
+	if peerIdentity == identity.ReservedIdentityHost {
 		// The only way we can discover IPs associated with the local host
 		// is directly via the NodeDiscovery package. If someone is informing
 		// this agent about IPs corresponding to the "host" via the kvstore,
 		// then they're sharing their own perspective on their own node IPs'
-		// identity. However, this node has remote-node enabled, so we should
-		// treat the peer as a "remote-node", not a "host".
+		// identity. We should treat the peer as a "remote-node", not a "host".
 		peerIdentity = identity.ReservedIdentityRemoteNode
 	}
 

commit 9ff97e75ed96a1cbbeb822b805013b233888e4d1

policy: Remove unused `allow-remotehost-ingress` derivedFrom label
Now that the `enable-remote-node-identity` flag has been removed, we no
longer create policy map entries to allow ingress from remote nodes.

Fixes: 2f829958b714 ("daemon,test: remove enableRemoteNodeIdentity flag")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/policy/mapstate.go
@@ -35,12 +35,11 @@ var (
 )
 
 const (
-	LabelKeyPolicyDerivedFrom   = "io.cilium.policy.derived-from"
-	LabelAllowLocalHostIngress  = "allow-localhost-ingress"
-	LabelAllowRemoteHostIngress = "allow-remotehost-ingress"
-	LabelAllowAnyIngress        = "allow-any-ingress"
-	LabelAllowAnyEgress         = "allow-any-egress"
-	LabelVisibilityAnnotation   = "visibility-annotation"
+	LabelKeyPolicyDerivedFrom  = "io.cilium.policy.derived-from"
+	LabelAllowLocalHostIngress = "allow-localhost-ingress"
+	LabelAllowAnyIngress       = "allow-any-ingress"
+	LabelAllowAnyEgress        = "allow-any-egress"
+	LabelVisibilityAnnotation  = "visibility-annotation"
 
 	// Using largest possible port value since it has the lowest priority
 	unrealizedRedirectPort = uint16(65535)