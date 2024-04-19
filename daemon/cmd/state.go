8a094633609996145073fba1e5c93b676217ee95
Tobias Klauser <tobias@cilium.io>
2024-04-19 11:17:35 +0200

    daemon: remove unused (*Daemon).restoreOldEndpoints return value and parameter
    (*Daemon).restoreOldEndpoints always returns a nil error and the only
    caller always sets clean=true.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- daemon/cmd/state.go
+++ daemon/cmd/state.go
@@ -176,10 +176,8 @@ func (d *Daemon) fetchOldEndpoints(dir string) (*endpointRestoreState, error) {
 // allocating their existing IPs out of the CIDR block and then inserting the
 // endpoints into the endpoints list. It needs to be followed by a call to
 // regenerateRestoredEndpoints() once the endpoint builder is ready.
-//
-// If clean is true, endpoints which cannot be associated with a container
-// workloads are deleted.
-func (d *Daemon) restoreOldEndpoints(state *endpointRestoreState, clean bool) error {
+// Endpoints which cannot be associated with a container workload are deleted.
+func (d *Daemon) restoreOldEndpoints(state *endpointRestoreState) {
 	failed := 0
 	defer func() {
 		state.possible = nil
@@ -187,7 +185,7 @@ func (d *Daemon) restoreOldEndpoints(state *endpointRestoreState, clean bool) er
 
 	if !option.Config.RestoreState {
 		log.Info("Endpoint restore is disabled, skipping restore step")
-		return nil
+		return
 	}
 
 	emf := &cachedEndpointMetadataFetcher{k8sWatcher: d.k8sWatcher}
@@ -234,9 +232,7 @@ func (d *Daemon) restoreOldEndpoints(state *endpointRestoreState, clean bool) er
 			}
 		}
 		if !restore {
-			if clean {
-				state.toClean = append(state.toClean, ep)
-			}
+			state.toClean = append(state.toClean, ep)
 			continue
 		}
 
@@ -269,8 +265,6 @@ func (d *Daemon) restoreOldEndpoints(state *endpointRestoreState, clean bool) er
 			}
 		}
 	}
-
-	return nil
 }
 
 func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState, endpointsRegenerator *endpoint.Regenerator) {
