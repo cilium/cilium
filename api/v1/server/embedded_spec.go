23a9607e4e88dd9a8e139108bd2ba20b441ae0ff
Sebastian Wicki <sebastian@isovalent.com>
2024-04-22 13:44:45 +0200

    api: Introduce ipLocalReservedPorts field
    This field will be used by the CNI plugin to set the
    `ip_local_reserved_ports` sysctl option.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- api/v1/server/embedded_spec.go
+++ api/v1/server/embedded_spec.go
@@ -2691,6 +2691,10 @@ func init() {
           "description": "Immutable configuration (read-only)",
           "$ref": "#/definitions/ConfigurationMap"
         },
+        "ipLocalReservedPorts": {
+          "description": "Comma-separated list of IP ports should be reserved in the workload network namespace",
+          "type": "string"
+        },
         "ipam-mode": {
           "description": "Configured IPAM mode",
           "type": "string"
@@ -8502,6 +8506,10 @@ func init() {
         "immutable": {
           "description": "Immutable configuration (read-only)",
           "$ref": "#/definitions/ConfigurationMap"
+        },
+        "ipLocalReservedPorts": {
+          "description": "Comma-separated list of IP ports should be reserved in the workload network namespace",
+          "type": "string"
         },
         "ipam-mode": {
           "description": "Configured IPAM mode",
