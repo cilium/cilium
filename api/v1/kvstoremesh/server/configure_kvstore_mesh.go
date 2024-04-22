b8be605ebb299478c8213535e860f1c941475a53
Marco Iorio <marco.iorio@isovalent.com>
2024-04-22 15:55:12 +0000

    api: introduce kvstoremesh API to report remote clusters status
    [ upstream commit 796bb184dc79fcf2163b52228c5d8a562b073d45 ]
    
    [ backporter's notes: skipped the Makefile.defs hunk, as the comment is
      not present. ]
    
    Introduce a new KVStoreMesh API definition, which currently exposes a
    /clusters path to provide information about the status of the connection
    to remote clusters, mimicking the data exposed by Cilium agents.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

---
+++ api/v1/kvstoremesh/server/configure_kvstore_mesh.go
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package server
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/cilium/cilium/api/v1/kvstoremesh/server/restapi"
+	"github.com/cilium/cilium/api/v1/kvstoremesh/server/restapi/cluster"
+)
+
+//go:generate swagger generate server --target ../../kvstoremesh --name KvstoreMesh --spec ../openapi.yaml --api-package restapi --server-package server --principal interface{}
+
+func configureFlags(api *restapi.KvstoreMeshAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *restapi.KvstoreMeshAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.UseSwaggerUI()
+	// To continue using redoc as your UI, uncomment the following line
+	// api.UseRedoc()
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	if api.ClusterGetClusterHandler == nil {
+		api.ClusterGetClusterHandler = cluster.GetClusterHandlerFunc(func(params cluster.GetClusterParams) middleware.Responder {
+			return middleware.NotImplemented("operation cluster.GetCluster has not yet been implemented")
+		})
+	}
+
+	api.PreServerShutdown = func() {}
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix".
+func configureServer(s *http.Server, scheme, addr string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation.
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics.
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
