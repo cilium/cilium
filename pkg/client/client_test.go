b90408c90b23589b35541398cf449bee115ca6fa
Tam Mach <tam.mach@cilium.io>
2024-04-24 22:57:32 +1000

    pkg/client: Replace gocheck with built-in go test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -10,32 +10,23 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/cilium/checkmate"
+	"github.com/stretchr/testify/require"
 
 	"github.com/cilium/cilium/api/v1/models"
 )
 
-// Hook up gocheck into the "go test" runner.
-type ClientTestSuite struct{}
-
-var _ = Suite(&ClientTestSuite{})
-
-func Test(t *testing.T) {
-	TestingT(t)
-}
-
-func (cs *ClientTestSuite) TestHint(c *C) {
+func TestHint(t *testing.T) {
 	var err error
-	c.Assert(Hint(err), IsNil)
+	require.NoError(t, Hint(err))
 
 	err = errors.New("foo bar")
-	c.Assert(Hint(err), ErrorMatches, "foo bar")
+	require.EqualError(t, Hint(err), "foo bar")
 
 	err = fmt.Errorf("ayy lmao")
-	c.Assert(Hint(err), ErrorMatches, "ayy lmao")
+	require.EqualError(t, Hint(err), "ayy lmao")
 
 	err = context.DeadlineExceeded
-	c.Assert(Hint(err), ErrorMatches, "Cilium API client timeout exceeded")
+	require.EqualError(t, Hint(err), "Cilium API client timeout exceeded")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
 	defer cancel()
@@ -43,17 +34,17 @@ func (cs *ClientTestSuite) TestHint(c *C) {
 	<-ctx.Done()
 	err = ctx.Err()
 
-	c.Assert(Hint(err), ErrorMatches, "Cilium API client timeout exceeded")
+	require.EqualError(t, Hint(err), "Cilium API client timeout exceeded")
 }
 
-func (cs *ClientTestSuite) TestClusterReadiness(c *C) {
-	c.Assert(clusterReadiness(&models.RemoteCluster{Ready: true}), Equals, "ready")
-	c.Assert(clusterReadiness(&models.RemoteCluster{Ready: false}), Equals, "not-ready")
+func TestClusterReadiness(t *testing.T) {
+	require.Equal(t, "ready", clusterReadiness(&models.RemoteCluster{Ready: true}))
+	require.Equal(t, "not-ready", clusterReadiness(&models.RemoteCluster{Ready: false}))
 }
 
-func (cs *ClientTestSuite) TestNumReadyClusters(c *C) {
-	c.Assert(numReadyClusters(&models.ClusterMeshStatus{}), Equals, 0)
-	c.Assert(numReadyClusters(&models.ClusterMeshStatus{
+func TestNumReadyClusters(t *testing.T) {
+	require.Equal(t, 0, numReadyClusters(&models.ClusterMeshStatus{}))
+	require.Equal(t, 2, numReadyClusters(&models.ClusterMeshStatus{
 		Clusters: []*models.RemoteCluster{{Ready: true}, {Ready: true}, {Ready: false}},
-	}), Equals, 2)
+	}))
 }
