39feb34fefe3a0b847cd8f73e8a244f73374bfc0
Marco Iorio <marco.iorio@isovalent.com>
2024-04-24 14:32:25 +0200

    clustermesh-apiserver: add version sub-command
    It outputs the clustermesh-apiserver version, as well as that of the
    etcd binary packaged as part of the same image.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- clustermesh-apiserver/etcdinit/root.go
+++ clustermesh-apiserver/etcdinit/root.go
@@ -25,9 +25,9 @@ import (
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
-// etcdBinaryLocation is hardcoded because we expect this command to be run inside a Cilium container that places the
+// EtcdBinaryLocation is hardcoded because we expect this command to be run inside a Cilium container that places the
 // etcd binary in a specific location.
-const etcdBinaryLocation = "/usr/bin/etcd"
+const EtcdBinaryLocation = "/usr/bin/etcd"
 
 var (
 	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "etcdinit")
@@ -126,14 +126,14 @@ func InitEtcdLocal() (returnErr error) {
 	}).
 		Info("Starting localhost-only etcd process")
 	// Specify the full path to the etcd binary to avoid any PATH search binary replacement nonsense
-	etcdCmd := exec.CommandContext(ctx, etcdBinaryLocation,
+	etcdCmd := exec.CommandContext(ctx, EtcdBinaryLocation,
 		fmt.Sprintf("--data-dir=%s", etcdDataDir),
 		fmt.Sprintf("--name=%s", etcdClusterName),
 		fmt.Sprintf("--listen-client-urls=%s", loopbackEndpoint),
 		fmt.Sprintf("--advertise-client-urls=%s", loopbackEndpoint),
 		fmt.Sprintf("--initial-cluster-token=%s", etcdInitialClusterToken),
 		"--initial-cluster-state=new")
-	log.WithField("etcdBinary", etcdBinaryLocation).
+	log.WithField("etcdBinary", EtcdBinaryLocation).
 		WithField("etcdFlags", etcdCmd.Args).
 		Debug("Executing etcd")
 
@@ -141,7 +141,7 @@ func InitEtcdLocal() (returnErr error) {
 	// it'll never complete of course.
 	err = etcdCmd.Start()
 	if err != nil {
-		log.WithField("etcdBinary", etcdBinaryLocation).
+		log.WithField("etcdBinary", EtcdBinaryLocation).
 			WithField("etcdFlags", etcdCmd.Args).
 			WithError(err).
 			Error("Failed to launch etcd process")
