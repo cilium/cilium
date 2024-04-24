79dd9384d6c1650e0188740cd569c8c00f266e63
Tam Mach <tam.mach@cilium.io>
2024-04-24 21:31:31 +1000

    pkg/byteorder: Replace gocheck with built-in go test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/byteorder/byteorder_test.go
+++ pkg/byteorder/byteorder_test.go
@@ -8,40 +8,31 @@ import (
 	"net"
 	"testing"
 
-	. "github.com/cilium/checkmate"
+	"github.com/stretchr/testify/require"
 )
 
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) {
-	TestingT(t)
+func TestNativeIsInitialized(t *testing.T) {
+	require.NotNil(t, Native)
 }
 
-type ByteorderSuite struct{}
-
-var _ = Suite(&ByteorderSuite{})
-
-func (b *ByteorderSuite) TestNativeIsInitialized(c *C) {
-	c.Assert(Native, NotNil)
-}
-
-func (b *ByteorderSuite) TestHostToNetwork(c *C) {
+func TestHostToNetwork(t *testing.T) {
 	switch Native {
 	case binary.LittleEndian:
-		c.Assert(HostToNetwork16(0xAABB), Equals, uint16(0xBBAA))
-		c.Assert(HostToNetwork32(0xAABBCCDD), Equals, uint32(0xDDCCBBAA))
+		require.Equal(t, uint16(0xBBAA), HostToNetwork16(0xAABB))
+		require.Equal(t, uint32(0xDDCCBBAA), HostToNetwork32(0xAABBCCDD))
 	case binary.BigEndian:
-		c.Assert(HostToNetwork16(0xAABB), Equals, uint16(0xAABB))
-		c.Assert(HostToNetwork32(0xAABBCCDD), Equals, uint32(0xAABBCCDD))
+		require.Equal(t, uint16(0xAABB), HostToNetwork16(0xAABB))
+		require.Equal(t, uint32(0xAABBCCDD), HostToNetwork32(0xAABBCCDD))
 	}
 }
 
-func (b *ByteorderSuite) TestNetIPv4ToHost32(c *C) {
+func TestNetIPv4ToHost32(t *testing.T) {
 	switch Native {
 	case binary.LittleEndian:
-		c.Assert(NetIPv4ToHost32(net.ParseIP("10.11.129.91")), Equals, uint32(0x5b810b0a))
-		c.Assert(NetIPv4ToHost32(net.ParseIP("10.11.138.214")), Equals, uint32(0xd68a0b0a))
+		require.Equal(t, uint32(0x5b810b0a), NetIPv4ToHost32(net.ParseIP("10.11.129.91")))
+		require.Equal(t, uint32(0xd68a0b0a), NetIPv4ToHost32(net.ParseIP("10.11.138.214")))
 	case binary.BigEndian:
-		c.Assert(NetIPv4ToHost32(net.ParseIP("10.11.129.91")), Equals, uint32(0x0a0b815b))
-		c.Assert(NetIPv4ToHost32(net.ParseIP("10.11.138.214")), Equals, uint32(0x0a0b8ad6))
+		require.Equal(t, uint32(0x0a0b815b), NetIPv4ToHost32(net.ParseIP("10.11.129.91")))
+		require.Equal(t, uint32(0x0a0b8ad6), NetIPv4ToHost32(net.ParseIP("10.11.138.214")))
 	}
 }
