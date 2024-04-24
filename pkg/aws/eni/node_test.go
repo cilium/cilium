a3e9f0f1bebaf6309aa5a74e18b254c1f34ebcc0
Tam Mach <tam.mach@cilium.io>
2024-04-24 21:31:31 +1000

    pkg/aws: Replace gocheck with built-in go test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/aws/eni/node_test.go
+++ pkg/aws/eni/node_test.go
@@ -4,37 +4,39 @@
 package eni
 
 import (
-	check "github.com/cilium/checkmate"
+	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/cilium/cilium/pkg/aws/eni/types"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 )
 
-func (e *ENISuite) TestGetMaximumAllocatableIPv4(c *check.C) {
+func TestGetMaximumAllocatableIPv4(t *testing.T) {
 	n := &Node{}
 
 	// With no k8sObj defined, it should return 0
-	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 0)
+	require.Equal(t, 0, n.GetMaximumAllocatableIPv4())
 
 	// With instance-type = m5.large and first-interface-index = 0, we should be able to allocate up to 3x10-3 addresses
 	n.k8sObj = newCiliumNode("node", withInstanceType("m5.large"), withFirstInterfaceIndex(0))
-	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 27)
+	require.Equal(t, 27, n.GetMaximumAllocatableIPv4())
 
 	// With instance-type = m5.large and first-interface-index = 1, we should be able to allocate up to 2x10-2 addresses
 	n.k8sObj = newCiliumNode("node", withInstanceType("m5.large"), withFirstInterfaceIndex(1))
-	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 18)
+	require.Equal(t, 18, n.GetMaximumAllocatableIPv4())
 
 	// With instance-type = m5.large and first-interface-index = 4, we should return 0 as there is only 3 interfaces
 	n.k8sObj = newCiliumNode("node", withInstanceType("m5.large"), withFirstInterfaceIndex(4))
-	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 0)
+	require.Equal(t, 0, n.GetMaximumAllocatableIPv4())
 
 	// With instance-type = foo we should return 0
 	n.k8sObj = newCiliumNode("node", withInstanceType("foo"))
-	c.Assert(n.GetMaximumAllocatableIPv4(), check.Equals, 0)
+	require.Equal(t, 0, n.GetMaximumAllocatableIPv4())
 }
 
 // TestGetUsedIPWithPrefixes tests the logic computing used IPs on a node when prefix delegation is enabled.
-func (e *ENISuite) TestGetUsedIPWithPrefixes(c *check.C) {
+func TestGetUsedIPWithPrefixes(t *testing.T) {
 	cn := newCiliumNode("node1", withInstanceType("m5a.large"))
 	n := &Node{k8sObj: cn}
 	eniName := "eni-1"
@@ -47,5 +49,5 @@ func (e *ENISuite) TestGetUsedIPWithPrefixes(c *check.C) {
 	allocationMap["10.10.128.2"] = ipamTypes.AllocationIP{Resource: eniName}
 	allocationMap["10.10.128.18"] = ipamTypes.AllocationIP{Resource: eniName}
 	n.k8sObj.Status.IPAM.Used = allocationMap
-	c.Assert(n.GetUsedIPWithPrefixes(), check.Equals, 32)
+	require.Equal(t, 32, n.GetUsedIPWithPrefixes())
 }
