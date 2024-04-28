bd2c35b2db1ceeee2af2a13da32b56cabe30b1de
Tam Mach <tam.mach@cilium.io>
2024-04-28 22:29:29 +1000

    pkg/iana: Replace gocheck with built-in go test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/iana/svcname_test.go
+++ pkg/iana/svcname_test.go
@@ -6,29 +6,22 @@ package iana
 import (
 	"testing"
 
-	. "github.com/cilium/checkmate"
+	"github.com/stretchr/testify/require"
 )
 
-type IANATestSuite struct{}
-
-var _ = Suite(&IANATestSuite{})
-
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) { TestingT(t) }
-
-func (s *IANATestSuite) TestIsSvcName(c *C) {
-	c.Assert(IsSvcName(""), Equals, false)                 // Too short
-	c.Assert(IsSvcName("1234567890abcdef"), Equals, false) // Too long
-	c.Assert(IsSvcName("1"), Equals, false)                // Missing letter
-	c.Assert(IsSvcName("1a"), Equals, true)
-	c.Assert(IsSvcName("Z"), Equals, true)
-	c.Assert(IsSvcName("a9"), Equals, true)
-	c.Assert(IsSvcName("a-9"), Equals, true)
-	c.Assert(IsSvcName("a--9"), Equals, false) // Two consecutive dashes
-	c.Assert(IsSvcName("-a9"), Equals, false)  // Begins with a dash
-	c.Assert(IsSvcName("a9-"), Equals, false)  // Ends with a dash
-	c.Assert(IsSvcName("a-b9-1"), Equals, true)
-	c.Assert(IsSvcName("1-a-9"), Equals, true)
-	c.Assert(IsSvcName("a-b-c-d-e-f"), Equals, true)
-	c.Assert(IsSvcName("1-2-3-4"), Equals, false) // No letter(s)
+func TestIsSvcName(t *testing.T) {
+	require.Equal(t, IsSvcName(""), false)                 // Too short
+	require.Equal(t, IsSvcName("1234567890abcdef"), false) // Too long
+	require.Equal(t, IsSvcName("1"), false)                // Missing letter
+	require.Equal(t, IsSvcName("1a"), true)
+	require.Equal(t, IsSvcName("Z"), true)
+	require.Equal(t, IsSvcName("a9"), true)
+	require.Equal(t, IsSvcName("a-9"), true)
+	require.Equal(t, IsSvcName("a--9"), false) // Two consecutive dashes
+	require.Equal(t, IsSvcName("-a9"), false)  // Begins with a dash
+	require.Equal(t, IsSvcName("a9-"), false)  // Ends with a dash
+	require.Equal(t, IsSvcName("a-b9-1"), true)
+	require.Equal(t, IsSvcName("1-a-9"), true)
+	require.Equal(t, IsSvcName("a-b-c-d-e-f"), true)
+	require.Equal(t, IsSvcName("1-2-3-4"), false) // No letter(s)
 }
