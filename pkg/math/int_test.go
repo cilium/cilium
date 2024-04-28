002668fd8497cc6c95a19a0390f237ceae77acc3
Tam Mach <tam.mach@cilium.io>
2024-04-28 23:32:28 +1000

    pkg/math: Replace gocheck with built-in go test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/math/int_test.go
+++ pkg/math/int_test.go
@@ -6,36 +6,28 @@ package math
 import (
 	"testing"
 
-	check "github.com/cilium/checkmate"
+	"github.com/stretchr/testify/require"
 )
-
-func Test(t *testing.T) {
-	check.TestingT(t)
-}
-
-type MathSuite struct{}
-
-var _ = check.Suite(&MathSuite{})
 
 const (
 	maxIntValue = int(^uint(0) >> 1)
 	minIntValue = -maxIntValue - 1
 )
 
-func (b *MathSuite) TestIntMin(c *check.C) {
-	c.Assert(IntMin(10, 20), check.Equals, 10)
-	c.Assert(IntMin(20, 10), check.Equals, 10)
-	c.Assert(IntMin(10, 10), check.Equals, 10)
-	c.Assert(IntMin(-10, 10), check.Equals, -10)
-	c.Assert(IntMin(0, 10), check.Equals, 0)
-	c.Assert(IntMin(minIntValue, maxIntValue), check.Equals, minIntValue)
+func TestIntMin(t *testing.T) {
+	require.Equal(t, IntMin(10, 20), 10)
+	require.Equal(t, IntMin(20, 10), 10)
+	require.Equal(t, IntMin(10, 10), 10)
+	require.Equal(t, IntMin(-10, 10), -10)
+	require.Equal(t, IntMin(0, 10), 0)
+	require.Equal(t, IntMin(minIntValue, maxIntValue), minIntValue)
 }
 
-func (b *MathSuite) TestIntMax(c *check.C) {
-	c.Assert(IntMax(10, 20), check.Equals, 20)
-	c.Assert(IntMax(20, 10), check.Equals, 20)
-	c.Assert(IntMax(10, 10), check.Equals, 10)
-	c.Assert(IntMax(-10, 10), check.Equals, 10)
-	c.Assert(IntMax(0, 10), check.Equals, 10)
-	c.Assert(IntMax(minIntValue, maxIntValue), check.Equals, maxIntValue)
+func TestIntMax(t *testing.T) {
+	require.Equal(t, IntMax(10, 20), 20)
+	require.Equal(t, IntMax(20, 10), 20)
+	require.Equal(t, IntMax(10, 10), 10)
+	require.Equal(t, IntMax(-10, 10), 10)
+	require.Equal(t, IntMax(0, 10), 10)
+	require.Equal(t, IntMax(minIntValue, maxIntValue), maxIntValue)
 }
