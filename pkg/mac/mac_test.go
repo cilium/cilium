5b253f0a9a9b0eab47bfd41a3e2dd4d81e5eeea2
Tam Mach <tam.mach@cilium.io>
2024-04-29 13:50:51 +1000

    pkg/mac: Replace gocheck with built-in go test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/mac/mac_test.go
+++ pkg/mac/mac_test.go
@@ -7,47 +7,36 @@ import (
 	"encoding/json"
 	"testing"
 
-	. "github.com/cilium/checkmate"
-
-	"github.com/cilium/cilium/pkg/checker"
+	"github.com/stretchr/testify/require"
 )
 
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) {
-	TestingT(t)
-}
-
-type MACSuite struct{}
-
-var _ = Suite(&MACSuite{})
-
-func (s *MACSuite) TestUint64(c *C) {
+func TestUint64(t *testing.T) {
 	m := MAC([]byte{0x11, 0x12, 0x23, 0x34, 0x45, 0x56})
 	v, err := m.Uint64()
-	c.Assert(err, IsNil)
-	c.Assert(v, Equals, Uint64MAC(0x564534231211))
+	require.Nil(t, err)
+	require.Equal(t, Uint64MAC(0x564534231211), v)
 }
 
-func (s *MACSuite) TestUnmarshalJSON(c *C) {
+func TestUnmarshalJSON(t *testing.T) {
 	m := MAC([]byte{0x11, 0x12, 0x23, 0x34, 0x45, 0x56})
 	w := MAC([]byte{0x11, 0x12, 0x23, 0x34, 0x45, 0xAB})
 	d, err := json.Marshal(m)
-	c.Assert(err, IsNil)
-	c.Assert(d, checker.DeepEquals, []byte(`"11:12:23:34:45:56"`))
-	var t MAC
-	err = json.Unmarshal([]byte(`"11:12:23:34:45:AB"`), &t)
-	c.Assert(err, IsNil)
-	c.Assert(t, checker.DeepEquals, w)
-	err = json.Unmarshal([]byte(`"11:12:23:34:45:A"`), &t)
-	c.Assert(err, NotNil)
+	require.Nil(t, err)
+	require.EqualValues(t, []byte(`"11:12:23:34:45:56"`), d)
+	var t1 MAC
+	err = json.Unmarshal([]byte(`"11:12:23:34:45:AB"`), &t1)
+	require.Nil(t, err)
+	require.EqualValues(t, w, t1)
+	err = json.Unmarshal([]byte(`"11:12:23:34:45:A"`), &t1)
+	require.Error(t, err)
 
 	m = MAC([]byte{})
 	w = MAC([]byte{})
 	d, err = json.Marshal(m)
-	c.Assert(err, Equals, nil)
-	c.Assert(d, checker.DeepEquals, []byte(`""`))
+	require.Equal(t, nil, err)
+	require.EqualValues(t, []byte(`""`), d)
 	var t2 MAC
 	err = json.Unmarshal([]byte(`""`), &t2)
-	c.Assert(err, IsNil)
-	c.Assert(t2, checker.DeepEquals, w)
+	require.Nil(t, err)
+	require.EqualValues(t, w, t2)
 }
