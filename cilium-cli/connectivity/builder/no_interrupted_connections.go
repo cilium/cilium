b650d649ec0b52ae3d24ac604ec70ebad1e25d13
Martynas Pumputis <m@lambda.lt>
2024-04-29 16:12:15 +0200

    connectivity: Delete test-conn-disrupt after test run
    Delete the test-conn-disrupt pods immediately after the test run has
    finished to reduce CPU consumption (the pods generate a lot of traffic,
    and in most CI jobs Cilium runs with monitor aggregation disabled).
    
    Suggested-by: Marco Iorio <marco.iorio@isovalent.com>
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- cilium-cli/connectivity/builder/no_interrupted_connections.go
+++ cilium-cli/connectivity/builder/no_interrupted_connections.go
@@ -4,6 +4,8 @@
 package builder
 
 import (
+	"context"
+
 	"github.com/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium-cli/connectivity/tests"
 )
@@ -13,5 +15,19 @@ type noInterruptedConnections struct{}
 func (t noInterruptedConnections) build(ct *check.ConnectivityTest, _ map[string]string) {
 	newTest("no-interrupted-connections", ct).
 		WithCondition(func() bool { return ct.Params().IncludeConnDisruptTest }).
-		WithScenarios(tests.NoInterruptedConnections())
+		WithScenarios(tests.NoInterruptedConnections()).
+		WithFinalizer(func(ctx context.Context) error {
+			// Delete the test-conn-disrupt pods immediately after the test run
+			// has finished to reduce CPU consumption (the pods generate a lot
+			// of traffic, and in most CI jobs Cilium runs with monitor
+			// aggregation disabled).
+			if !ct.Params().ConnDisruptTestSetup {
+				for _, client := range ct.Clients() {
+					if err := ct.DeleteConnDisruptTestDeployment(ctx, client); err != nil {
+						return err
+					}
+				}
+			}
+			return nil
+		})
 }
