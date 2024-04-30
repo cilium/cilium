cilium/cilium

commit 2079aa44fa3cb5ad9eaa6cfebf715ca4c6c1d9a1

datapath/sysctl: Use generic reconciler for sysctl params
Refactor the sysctl package using the statedb table and the generic
reconciler. The sysctl specific reconciler operations are in charge of
syncing the desired state with the actual kernel sysctl parameters.

To be backward compatible with the old sysctl interface, the exported
methods are kept synchronous, so they wait for the reconciliation to
actually happen before returning to the caller.

Moreover, `NewDirectSysctl` construct a sysctl.Sysctl
implementation to change sysctl parameters without relying on the
reconciler. They are meant to be used *ONLY* in binaries that are not
based on the "hive and cells" framework, IOW where a statedb and the
reconcilers are not available (e.g: cilium-docker and cilium-cni).

The sysctl package tests are adapted to the new implementation, and the
TestSysctl is written as a unit-style integration test to verify the
sysctl reconciler operations.

All the privileged tests that don't rely on a hive but require a working
implementation of the sysctl package are temporarily left broken. The
same goes for the controlplane tests that require a fake sysctl
implementation.
They will be fixed in the subsequent commits.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

plugins/cilium-cni/cmd/cmd.go
@@ -21,6 +21,7 @@ import (
 	gops "github.com/google/gops/agent"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
 	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
 
@@ -480,6 +481,8 @@ func (cmd *Cmd) Add(args *skel.CmdArgs) (err error) {
 		return fmt.Errorf("failed to determine endpoint configuration: %w", err)
 	}
 
+	sysctl := sysctl.NewDirectSysctl(afero.NewOsFs(), "/proc")
+
 	for _, epConf := range configs {
 		if err = netns.RemoveIfFromNetNSIfExists(netNs, epConf.IfName()); err != nil {
 			return fmt.Errorf("failed removing interface %q from namespace %q: %w",
@@ -523,7 +526,7 @@ func (cmd *Cmd) Add(args *skel.CmdArgs) (err error) {
 			cniID := ep.ContainerID + ":" + ep.ContainerInterfaceName
 			veth, peer, tmpIfName, err := connector.SetupVeth(cniID, int(conf.DeviceMTU),
 				int(conf.GROMaxSize), int(conf.GSOMaxSize),
-				int(conf.GROIPV4MaxSize), int(conf.GSOIPV4MaxSize), ep)
+				int(conf.GROIPV4MaxSize), int(conf.GSOIPV4MaxSize), ep, sysctl)
 			if err != nil {
 				return fmt.Errorf("unable to set up veth on host side: %w", err)
 			}
@@ -595,8 +598,13 @@ func (cmd *Cmd) Add(args *skel.CmdArgs) (err error) {
 		var macAddrStr string
 		if err = netNs.Do(func(_ ns.NetNS) error {
 			if ipv6IsEnabled(ipam) {
-				if err := sysctl.Disable("net.ipv6.conf.all.disable_ipv6"); err != nil {
-					logger.WithError(err).Warn("unable to enable ipv6 on all interfaces")
+				param := "net.ipv6.conf.all.disable_ipv6"
+				if err != nil {
+					logger.WithError(err).WithField(logfields.SysParamName, param).Warn("invalid sysctl parameter")
+				} else {
+					if err := sysctl.Disable(param); err != nil {
+						logger.WithError(err).Warn("unable to enable ipv6 on all interfaces")
+					}
 				}
 			}
 			macAddrStr, err = configureIface(ipam, epConf.IfName(), state)

cilium-dbg/cmd/post_uninstall_cleanup.go
@@ -574,7 +574,7 @@ func removeTCFilters(linkAndFilters map[string][]*netlink.BpfFilter) error {
 
 func removeXDPAttachments(links []netlink.Link) error {
 	for _, link := range links {
-		if err := loader.NewLoader().DetachXDP(link, bpf.CiliumPath(), "cil_xdp_entry"); err != nil {
+		if err := loader.NewLoader(nil).DetachXDP(link, bpf.CiliumPath(), "cil_xdp_entry"); err != nil {
 			return err
 		}
 		fmt.Printf("removed cilium xdp of %s\n", link.Attrs().Name)

cilium-health/launch/endpoint.go
@@ -107,7 +107,7 @@ func configureHealthRouting(netns, dev string, addressing *models.NodeAddressing
 	return err
 }
 
-func configureHealthInterface(netNS ns.NetNS, ifName string, ip4Addr, ip6Addr *net.IPNet) error {
+func configureHealthInterface(netNS ns.NetNS, ifName string, ip4Addr, ip6Addr *net.IPNet, sysctl sysctl.Sysctl) error {
 	return netNS.Do(func(_ ns.NetNS) error {
 		link, err := netlink.LinkByName(ifName)
 		if err != nil {
@@ -118,7 +118,7 @@ func configureHealthInterface(netNS ns.NetNS, ifName string, ip4Addr, ip6Addr *n
 			name := fmt.Sprintf("net.ipv6.conf.%s.disable_ipv6", ifName)
 			// Ignore the error; if IPv6 is completely disabled
 			// then it's okay if we can't write the sysctl.
-			_ = sysctl.Write(name, "1")
+			_ = sysctl.Enable(name)
 		} else {
 			if err = netlink.AddrAdd(link, &netlink.Addr{IPNet: ip6Addr}); err != nil {
 				return err
@@ -231,7 +231,9 @@ func LaunchAsEndpoint(baseCtx context.Context,
 	epMgr EndpointAdder,
 	proxy endpoint.EndpointProxy,
 	allocator cache.IdentityAllocator,
-	routingConfig routingConfigurer) (*Client, error) {
+	routingConfig routingConfigurer,
+	sysctl sysctl.Sysctl,
+) (*Client, error) {
 
 	var (
 		cmd  = launcher.Launcher{}
@@ -276,7 +278,8 @@ func LaunchAsEndpoint(baseCtx context.Context,
 	case datapathOption.DatapathModeVeth:
 		_, epLink, err := connector.SetupVethWithNames(vethName, epIfaceName, mtuConfig.GetDeviceMTU(),
 			bigTCPConfig.GetGROIPv6MaxSize(), bigTCPConfig.GetGSOIPv6MaxSize(),
-			bigTCPConfig.GetGROIPv4MaxSize(), bigTCPConfig.GetGSOIPv4MaxSize(), info)
+			bigTCPConfig.GetGROIPv4MaxSize(), bigTCPConfig.GetGSOIPv4MaxSize(),
+			info, sysctl)
 		if err != nil {
 			return nil, fmt.Errorf("Error while creating veth: %s", err)
 		}
@@ -286,7 +289,7 @@ func LaunchAsEndpoint(baseCtx context.Context,
 		}
 	}
 
-	if err = configureHealthInterface(netNS, epIfaceName, ip4Address, ip6Address); err != nil {
+	if err = configureHealthInterface(netNS, epIfaceName, ip4Address, ip6Address, sysctl); err != nil {
 		return nil, fmt.Errorf("failed configure health interface %q: %s", epIfaceName, err)
 	}
 

daemon/cmd/cells.go
@@ -119,6 +119,9 @@ var (
 		// Store cell provides factory for creating watchStore/syncStore/storeManager
 		// useful for synchronizing data from/to kvstore.
 		store.Cell,
+
+		// Reconciler cell provides the shared metrics for all the reconcilers.
+		reconciler.Cell,
 	)
 
 	// ControlPlane implement the per-node control functions. These are pure

daemon/cmd/daemon.go
@@ -359,7 +359,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 	// detection, might disable BPF NodePort and friends. But this is fine, as
 	// the feature does not influence the decision which BPF maps should be
 	// created.
-	if err := initKubeProxyReplacementOptions(params.TunnelConfig); err != nil {
+	if err := initKubeProxyReplacementOptions(params.Sysctl, params.TunnelConfig); err != nil {
 		log.WithError(err).Error("unable to initialize kube-proxy replacement options")
 		return nil, nil, fmt.Errorf("unable to initialize kube-proxy replacement options: %w", err)
 	}
@@ -737,7 +737,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		d.l2announcer.DevicesChanged(devices)
 	}
 
-	if err := finishKubeProxyReplacementInit(); err != nil {
+	if err := finishKubeProxyReplacementInit(params.Sysctl); err != nil {
 		log.WithError(err).Error("failed to finalise LB initialization")
 		return nil, nil, fmt.Errorf("failed to finalise LB initialization: %w", err)
 	}

daemon/cmd/daemon_main.go
@@ -718,9 +718,6 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.Int(option.MTUName, 0, "Overwrite auto-detected MTU of underlying network")
 	option.BindEnv(vp, option.MTUName)
 
-	flags.String(option.ProcFs, "/proc", "Root's proc filesystem path")
-	option.BindEnv(vp, option.ProcFs)
-
 	flags.Int(option.RouteMetric, 0, "Overwrite the metric used by cilium when adding routes to its 'cilium_host' device")
 	option.BindEnv(vp, option.RouteMetric)
 
@@ -1182,8 +1179,6 @@ func initEnv(vp *viper.Viper) {
 	var debugDatapath bool
 
 	option.LogRegisteredOptions(vp, log)
-
-	sysctl.SetProcfs(option.Config.ProcFs)
 
 	for _, grp := range option.Config.DebugVerbose {
 		switch grp {
@@ -1665,6 +1660,7 @@ type daemonParams struct {
 	BandwidthManager    datapath.BandwidthManager
 	IPsecKeyCustodian   datapath.IPsecKeyCustodian
 	MTU                 mtu.MTU
+	Sysctl              sysctl.Sysctl
 }
 
 func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
@@ -1831,7 +1827,7 @@ func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *da
 
 	bootstrapStats.healthCheck.Start()
 	if option.Config.EnableHealthChecking {
-		d.initHealth(params.HealthAPISpec, cleaner)
+		d.initHealth(params.HealthAPISpec, cleaner, params.Sysctl)
 	}
 	bootstrapStats.healthCheck.End(true)
 

daemon/cmd/health.go
@@ -11,6 +11,7 @@ import (
 	healthApi "github.com/cilium/cilium/api/v1/health/server"
 	health "github.com/cilium/cilium/cilium-health/launch"
 	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/datapath/linux/sysctl"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/health/defaults"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -22,7 +23,7 @@ import (
 
 var healthControllerGroup = controller.NewGroup("cilium-health")
 
-func (d *Daemon) initHealth(spec *healthApi.Spec, cleaner *daemonCleanup) {
+func (d *Daemon) initHealth(spec *healthApi.Spec, cleaner *daemonCleanup, sysctl sysctl.Sysctl) {
 	// Launch cilium-health in the same process (and namespace) as cilium.
 	log.Info("Launching Cilium health daemon")
 	if ch, err := health.Launch(spec, d.datapath.Loader().HostDatapathInitialized()); err != nil {
@@ -80,6 +81,7 @@ func (d *Daemon) initHealth(spec *healthApi.Spec, cleaner *daemonCleanup) {
 						d.l7Proxy,
 						d.identityAllocator,
 						d.healthEndpointRouting,
+						sysctl,
 					)
 					if launchErr != nil {
 						if err != nil {

daemon/cmd/kube_proxy_replacement.go
@@ -41,7 +41,7 @@ import (
 //
 // if this function cannot determine the strictness an error is returned and the boolean
 // is false. If an error is returned the boolean is of no meaning.
-func initKubeProxyReplacementOptions(tunnelConfig tunnel.Config) error {
+func initKubeProxyReplacementOptions(sysctl sysctl.Sysctl, tunnelConfig tunnel.Config) error {
 	if option.Config.KubeProxyReplacement != option.KubeProxyReplacementStrict &&
 		option.Config.KubeProxyReplacement != option.KubeProxyReplacementPartial &&
 		option.Config.KubeProxyReplacement != option.KubeProxyReplacementDisabled &&
@@ -277,18 +277,18 @@ func initKubeProxyReplacementOptions(tunnelConfig tunnel.Config) error {
 		return nil
 	}
 
-	return probeKubeProxyReplacementOptions()
+	return probeKubeProxyReplacementOptions(sysctl)
 }
 
 // probeKubeProxyReplacementOptions checks whether the requested KPR options can be enabled with
 // the running kernel.
-func probeKubeProxyReplacementOptions() error {
+func probeKubeProxyReplacementOptions(sysctl sysctl.Sysctl) error {
 	if option.Config.EnableNodePort {
 		if probes.HaveProgramHelper(ebpf.SchedCLS, asm.FnFibLookup) != nil {
 			return fmt.Errorf("BPF NodePort services needs kernel 4.17.0 or newer")
 		}
 
-		if err := checkNodePortAndEphemeralPortRanges(); err != nil {
+		if err := checkNodePortAndEphemeralPortRanges(sysctl); err != nil {
 			return err
 		}
 
@@ -384,7 +384,7 @@ func probeKubeProxyReplacementOptions() error {
 
 // finishKubeProxyReplacementInit finishes initialization of kube-proxy
 // replacement after all devices are known.
-func finishKubeProxyReplacementInit() error {
+func finishKubeProxyReplacementInit(sysctl sysctl.Sysctl) error {
 	if !(option.Config.EnableNodePort || option.Config.EnableWireguard) {
 		// Make sure that NodePort dependencies are disabled
 		disableNodePort()
@@ -573,7 +573,7 @@ func markHostExtension() {
 // making cilium-agent to stop.
 // Otherwise, if EnableAutoProtectNodePortRange == true, then append the nodeport
 // range to ip_local_reserved_ports.
-func checkNodePortAndEphemeralPortRanges() error {
+func checkNodePortAndEphemeralPortRanges(sysctl sysctl.Sysctl) error {
 	ephemeralPortRangeStr, err := sysctl.Read("net.ipv4.ip_local_port_range")
 	if err != nil {
 		return fmt.Errorf("Unable to read net.ipv4.ip_local_port_range: %w", err)

commit dd3d812b9bde67d0ef834e91d0300901db6a38de

cilium-cni: Remove unnecessary error check
This removes a if statement which seems to check if the generated sysctl
parameter name was valid - but since that parameter name is constant and
the error value is populated elsewhere (independently of the parameter
name), this check was clearly broken.

Thus, this commit removes the log line and inlines the constant
parameter name directly.

Fixes: 2079aa44fa3c ("datapath/sysctl: Use generic reconciler for sysctl params")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

plugins/cilium-cni/cmd/cmd.go
@@ -604,13 +604,8 @@ func (cmd *Cmd) Add(args *skel.CmdArgs) (err error) {
 
 		if err = ns.Do(func() error {
 			if ipv6IsEnabled(ipam) {
-				param := "net.ipv6.conf.all.disable_ipv6"
-				if err != nil {
-					logger.WithError(err).WithField(logfields.SysParamName, param).Warn("invalid sysctl parameter")
-				} else {
-					if err := sysctl.Disable(param); err != nil {
-						logger.WithError(err).Warn("unable to enable ipv6 on all interfaces")
-					}
+				if err := sysctl.Disable("net.ipv6.conf.all.disable_ipv6"); err != nil {
+					logger.WithError(err).Warn("unable to enable ipv6 on all interfaces")
 				}
 			}
 			macAddrStr, err = configureIface(ipam, epConf.IfName(), state)