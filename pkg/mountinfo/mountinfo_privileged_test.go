60cde220cb51418b29713a3bb8418fcb0515c4bc
Tam Mach <tam.mach@cilium.io>
2024-04-30 21:18:43 +1000

    pkg/mountinfo: Replace gocheck with built-in go test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/mountinfo/mountinfo_privileged_test.go
+++ pkg/mountinfo/mountinfo_privileged_test.go
@@ -7,46 +7,41 @@ package mountinfo
 
 import (
 	"os"
+	"testing"
 
-	. "github.com/cilium/checkmate"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/sys/unix"
 
 	"github.com/cilium/cilium/pkg/testutils"
 )
 
-type MountInfoPrivilegedTestSuite struct{}
-
-var _ = Suite(&MountInfoPrivilegedTestSuite{})
-
-func (s *MountInfoPrivilegedTestSuite) SetUpSuite(c *C) {
-	testutils.PrivilegedTest(c)
-}
-
 // TestIsMountFSbyMount tests the public function IsMountFS by performing
 // an actual mount.
-func (s *MountInfoPrivilegedTestSuite) TestIsMountFSbyMount(c *C) {
+func TestIsMountFSbyMount(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
 	tmpDir, err := os.MkdirTemp("", "IsMountFS_")
-	c.Assert(err, IsNil)
+	require.Nil(t, err)
 	defer os.RemoveAll(tmpDir)
 
 	mounted, matched, err := IsMountFS(unix.TMPFS_MAGIC, tmpDir)
-	c.Assert(err, IsNil)
-	c.Assert(mounted, Equals, false)
-	c.Assert(matched, Equals, false)
+	require.Nil(t, err)
+	require.Equal(t, false, mounted)
+	require.Equal(t, false, matched)
 
 	err = unix.Mount("tmpfs", tmpDir, "tmpfs", 0, "")
-	c.Assert(err, IsNil)
+	require.Nil(t, err)
 	defer unix.Unmount(tmpDir, unix.MNT_DETACH)
 
 	// deliberately check with wrong fstype
 	mounted, matched, err = IsMountFS(unix.PROC_SUPER_MAGIC, tmpDir)
-	c.Assert(err, IsNil)
-	c.Assert(mounted, Equals, true)
-	c.Assert(matched, Equals, false)
+	require.Nil(t, err)
+	require.Equal(t, true, mounted)
+	require.Equal(t, false, matched)
 
 	// now check with proper fstype
 	mounted, matched, err = IsMountFS(unix.TMPFS_MAGIC, tmpDir)
-	c.Assert(err, IsNil)
-	c.Assert(mounted, Equals, true)
-	c.Assert(matched, Equals, true)
+	require.Nil(t, err)
+	require.Equal(t, true, mounted)
+	require.Equal(t, true, matched)
 }
