34539caae179066cdfc9ded8a4f945d56dbc7a4b
Tam Mach <tam.mach@cilium.io>
2024-04-30 21:24:10 +1000

    pkg/mcastmanager: Replace gocheck with built-in go test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/mcastmanager/mcastmanager_test.go
+++ pkg/mcastmanager/mcastmanager_test.go
@@ -7,23 +7,16 @@ import (
 	"net/netip"
 	"testing"
 
-	. "github.com/cilium/checkmate"
+	"github.com/stretchr/testify/require"
 	"github.com/vishvananda/netlink"
 )
 
-func Test(t *testing.T) { TestingT(t) }
-
-type McastManagerSuite struct {
-}
-
-var _ = Suite(&McastManagerSuite{})
-
-func (m *McastManagerSuite) TestAddRemoveEndpoint(c *C) {
+func TestAddRemoveEndpoint(t *testing.T) {
 	ifaces, err := netlink.LinkList()
-	c.Assert(err, IsNil)
+	require.Nil(t, err)
 
 	if len(ifaces) == 0 {
-		c.Skip("no interfaces to test")
+		t.Skip("no interfaces to test")
 	}
 
 	mgr := New(ifaces[0].Attrs().Name)
@@ -31,36 +24,36 @@ func (m *McastManagerSuite) TestAddRemoveEndpoint(c *C) {
 	// Add first endpoint
 	mgr.AddAddress(netip.MustParseAddr("f00d::1234"))
 
-	c.Assert(mgr.state, HasLen, 1)
+	require.Len(t, mgr.state, 1)
 	_, ok := mgr.state[netip.MustParseAddr("ff02::1:ff00:1234")]
-	c.Assert(ok, Equals, true)
+	require.Equal(t, true, ok)
 
 	// Add another endpoint that shares the same maddr
 	mgr.AddAddress(netip.MustParseAddr("f00d:aabb::1234"))
 
-	c.Assert(mgr.state, HasLen, 1)
+	require.Len(t, mgr.state, 1)
 
 	// Remove the first endpoint
 	mgr.RemoveAddress(netip.MustParseAddr("f00d::1234"))
 
-	c.Assert(mgr.state, HasLen, 1)
+	require.Len(t, mgr.state, 1)
 	_, ok = mgr.state[netip.MustParseAddr("ff02::1:ff00:1234")]
-	c.Assert(ok, Equals, true)
+	require.Equal(t, true, ok)
 
 	// Remove the second endpoint
 	mgr.RemoveAddress(netip.MustParseAddr("f00d:aabb::1234"))
 
-	c.Assert(mgr.state, HasLen, 0)
+	require.Len(t, mgr.state, 0)
 	_, ok = mgr.state[netip.MustParseAddr("ff02::1:ff00:1234")]
-	c.Assert(ok, Equals, false)
+	require.Equal(t, false, ok)
 }
 
-func (m *McastManagerSuite) TestAddRemoveNil(c *C) {
+func TestAddRemoveNil(t *testing.T) {
 	ifaces, err := netlink.LinkList()
-	c.Assert(err, IsNil)
+	require.Nil(t, err)
 
 	if len(ifaces) == 0 {
-		c.Skip("no interfaces to test")
+		t.Skip("no interfaces to test")
 	}
 
 	var (
@@ -69,7 +62,7 @@ func (m *McastManagerSuite) TestAddRemoveNil(c *C) {
 	)
 
 	mgr.AddAddress(netip.Addr{})
-	c.Assert(mgr.state, HasLen, 0)
+	require.Len(t, mgr.state, 0)
 	mgr.RemoveAddress(netip.Addr{})
-	c.Assert(mgr.state, HasLen, 0)
+	require.Len(t, mgr.state, 0)
 }
