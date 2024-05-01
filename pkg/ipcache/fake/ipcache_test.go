e7b7026d196ec7fc05b2130716612259e5511854
Tam Mach <tam.mach@cilium.io>
2024-05-01 20:40:06 +1000

    pkg/ipcache: Replace gocheck with built-in go test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/ipcache/fake/ipcache_test.go
+++ pkg/ipcache/fake/ipcache_test.go
@@ -8,38 +8,29 @@ import (
 	"testing"
 	"time"
 
-	check "github.com/cilium/checkmate"
+	"github.com/stretchr/testify/require"
 
-	"github.com/cilium/cilium/pkg/checker"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/source"
 )
 
-func Test(t *testing.T) {
-	check.TestingT(t)
-}
-
-type fakeIPCacheSuite struct{}
-
-var _ = check.Suite(&fakeIPCacheSuite{})
-
-func (s *fakeIPCacheSuite) TestFakeIPCache(c *check.C) {
+func TestFakeIPCache(t *testing.T) {
 	ipcacheMock := NewIPCache(true)
 
 	ipcacheMock.Upsert("1.1.1.1", net.ParseIP("2.2.2.2"), 0, nil, ipcache.Identity{ID: 1, Source: source.Local})
 	select {
 	case event := <-ipcacheMock.Events:
-		c.Assert(event, checker.DeepEquals, NodeEvent{EventUpsert, net.ParseIP("1.1.1.1")})
+		require.EqualValues(t, NodeEvent{EventUpsert, net.ParseIP("1.1.1.1")}, event)
 	case <-time.After(5 * time.Second):
-		c.Errorf("timeout while waiting for ipcache upsert for IP 1.1.1.1")
+		t.Errorf("timeout while waiting for ipcache upsert for IP 1.1.1.1")
 	}
 
 	ipcacheMock.Delete("1.1.1.1", source.Local)
 
 	select {
 	case event := <-ipcacheMock.Events:
-		c.Assert(event, checker.DeepEquals, NodeEvent{EventDelete, net.ParseIP("1.1.1.1")})
+		require.EqualValues(t, NodeEvent{EventDelete, net.ParseIP("1.1.1.1")}, event)
 	case <-time.After(5 * time.Second):
-		c.Errorf("timeout while waiting for ipcache delete for IP 1.1.1.1")
+		t.Errorf("timeout while waiting for ipcache delete for IP 1.1.1.1")
 	}
 }
