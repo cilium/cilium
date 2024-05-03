a8fcefa059ffcf40078722dd2bfadbbfe7504471
Daniel Borkmann <daniel@iogearbox.net>
2024-05-03 15:10:39 +0000

    cilium, status: Add datapath mode to status
    Dump the status for the used device mode into cilium status, so that this
    is picked up by the sysdump.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- daemon/cmd/status.go
+++ daemon/cmd/status.go
@@ -235,6 +235,17 @@ func (d *Daemon) getAttachModeStatus() models.AttachMode {
 	return mode
 }
 
+func (d *Daemon) getDatapathModeStatus() models.DatapathMode {
+	mode := models.DatapathModeVeth
+	switch option.Config.DatapathMode {
+	case datapathOption.DatapathModeNetkit:
+		mode = models.DatapathModeNetkit
+	case datapathOption.DatapathModeNetkitL2:
+		mode = models.DatapathModeNetkitDashL2
+	}
+	return mode
+}
+
 func (d *Daemon) getCNIChainingStatus() *models.CNIChainingStatus {
 	mode := d.cniConfigManager.GetChainingMode()
 	if len(mode) == 0 {
@@ -1119,6 +1130,7 @@ func (d *Daemon) startStatusCollector(cleaner *daemonCleanup) {
 	d.statusResponse.IdentityRange = d.getIdentityRange()
 	d.statusResponse.Srv6 = d.getSRv6Status()
 	d.statusResponse.AttachMode = d.getAttachModeStatus()
+	d.statusResponse.DatapathMode = d.getDatapathModeStatus()
 
 	d.statusCollector = status.NewCollector(probes, status.Config{StackdumpPath: "/run/cilium/state/agent.stack.gz"})
 
