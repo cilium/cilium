de3d4cdc53a8c2eb4102d238fae1b187cd6f32a5
Tom Hadlaw <tom.hadlaw@isovalent.com>
2024-05-10 13:41:37 -0700

    daemon: newDaemon returns promise for *option.DaemonConfig.
    This resolves in the same way that the daemon promise resolves, but
    resolves to *option.DaemonConfig notifying that newDaemon init tasks
    have completed. Providing daemon config as a resolvable promise
    allows code that is being added to the Daemon cells to wait for this
    resolution without introducing cyclic package dependencies.
    
    This is necessary as newDaemon may override fields in DaemonConfig such
    as when doing kube proxy replacement init or device detection.
    
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -1683,8 +1683,9 @@ type daemonParams struct {
 	MetalLBBgpSpeaker   speaker.MetalLBBgpSpeaker
 }
 
-func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
+func newDaemonPromise(params daemonParams) (promise.Promise[*Daemon], promise.Promise[*option.DaemonConfig]) {
 	daemonResolver, daemonPromise := promise.New[*Daemon]()
+	cfgResolver, cfgPromise := promise.New[*option.DaemonConfig]()
 
 	// daemonCtx is the daemon-wide context cancelled when stopping.
 	daemonCtx, cancelDaemonCtx := context.WithCancel(context.Background())
@@ -1714,10 +1715,12 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 					daemonResolver.Reject(err)
 					cancelDaemonCtx()
 					cleaner.Clean()
+					cfgResolver.Reject(err)
 					return err
 				}
 			}
 			daemonResolver.Resolve(daemon)
+			cfgResolver.Resolve(option.Config)
 			return nil
 		},
 		OnStop: func(cell.HookContext) error {
@@ -1727,7 +1730,7 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 			return nil
 		},
 	})
-	return daemonPromise
+	return daemonPromise, cfgPromise
 }
 
 // startDaemon starts the old unmodular part of the cilium-agent.
