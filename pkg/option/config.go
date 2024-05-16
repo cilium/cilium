1164b7aa69b1ce1b8d5464dc0e99db651cedc1e4
Marcel Zieba <marcel.zieba@isovalent.com>
2024-05-16 16:46:02 +0200

    ipsec: cache xfrm state list
    Reduces GC CPU usage and memory allocations coming from XfrmStateList.
    To ensure we have up-to-date cache, wrap all XfrmState related
    functions inside cache, which is invalidated whenever XfrmState changes.
    
    This is follow-up to #32577
    While that PR averages out CPU usage over time, in large cluster 100+
    nodes amount of allocations coming from netlink.XfrmStateList() is high
    due to backgroundSync where we usually don't change any Xfrm states.
    This becomes more and more expensive as number of nodes increases.
    
    Added CI test to make sure that we accidentally don't add calls that
    modify XFRMState without going through cache.
    
    Also, added hidden option that allows to turn of caching.
    
    Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -720,6 +720,10 @@ const (
 	// be necessary on key rotations.
 	EnableIPsecKeyWatcher = "enable-ipsec-key-watcher"
 
+	// Enable caching for XfrmState for IPSec. Significantly reduces CPU usage
+	// in large clusters.
+	EnableIPSecXfrmStateCaching = "enable-ipsec-xfrm-state-caching"
+
 	// IPSecKeyFileName is the name of the option for ipsec key file
 	IPSecKeyFileName = "ipsec-key-file"
 
@@ -1595,6 +1599,9 @@ type DaemonConfig struct {
 	// Enable watcher for IPsec key. If disabled, a restart of the agent will
 	// be necessary on key rotations.
 	EnableIPsecKeyWatcher bool
+
+	// EnableIPSecXfrmStateCaching enables IPSec XfrmState caching.
+	EnableIPSecXfrmStateCaching bool
 
 	// EnableIPSecEncryptedOverlay enables IPSec encryption for overlay traffic.
 	EnableIPSecEncryptedOverlay bool
@@ -3051,6 +3058,7 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 	c.IPSecKeyFile = vp.GetString(IPSecKeyFileName)
 	c.IPsecKeyRotationDuration = vp.GetDuration(IPsecKeyRotationDuration)
 	c.EnableIPsecKeyWatcher = vp.GetBool(EnableIPsecKeyWatcher)
+	c.EnableIPSecXfrmStateCaching = vp.GetBool(EnableIPSecXfrmStateCaching)
 	c.MonitorAggregation = vp.GetString(MonitorAggregationName)
 	c.MonitorAggregationInterval = vp.GetDuration(MonitorAggregationInterval)
 	c.MTU = vp.GetInt(MTUName)
