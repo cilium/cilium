e22431dcdc97a7b7ca57cd07043974566aba5f50
Yusuke Suzuki <yusuke.suzuki@isovalent.com>
2024-05-20 10:38:35 +0900

    egressgw: add bpf unit test to check the redirection from bpf_host
    Signed-off-by: Yusuke Suzuki <yusuke.suzuki@isovalent.com>

--- bpf/tests/lib/ipcache.h
+++ bpf/tests/lib/ipcache.h
@@ -3,10 +3,10 @@
 
 static __always_inline void
 __ipcache_v4_add_entry(__be32 addr, __u8 cluster_id, __u32 sec_identity,
-		       __u32 tunnel_ep, __u8 spi, bool flag_skip_tunnel)
+		       __u32 tunnel_ep, __u8 spi, bool flag_skip_tunnel, __u32 mask_size)
 {
 	struct ipcache_key key = {
-		.lpm_key.prefixlen = IPCACHE_PREFIX_LEN(V4_CACHE_KEY_LEN),
+		.lpm_key.prefixlen = IPCACHE_PREFIX_LEN(mask_size),
 		.cluster_id = cluster_id,
 		.family = ENDPOINT_KEY_IPV4,
 		.ip4 = addr,
@@ -25,14 +25,23 @@ static __always_inline void
 ipcache_v4_add_entry(__be32 addr, __u8 cluster_id, __u32 sec_identity,
 		     __u32 tunnel_ep, __u8 spi)
 {
-	__ipcache_v4_add_entry(addr, cluster_id, sec_identity, tunnel_ep, spi, false);
+	__ipcache_v4_add_entry(addr, cluster_id, sec_identity, tunnel_ep, spi, false,
+			       V4_CACHE_KEY_LEN);
 }
 
 static __always_inline void
 ipcache_v4_add_entry_with_flags(__be32 addr, __u8 cluster_id, __u32 sec_identity,
 				__u32 tunnel_ep, __u8 spi, bool flag_skip_tunnel)
 {
-	__ipcache_v4_add_entry(addr, cluster_id, sec_identity, tunnel_ep, spi, flag_skip_tunnel);
+	__ipcache_v4_add_entry(addr, cluster_id, sec_identity, tunnel_ep, spi, flag_skip_tunnel,
+			       V4_CACHE_KEY_LEN);
+}
+
+static __always_inline void
+ipcache_v4_add_entry_with_mask_size(__be32 addr, __u8 cluster_id, __u32 sec_identity,
+				    __u32 tunnel_ep, __u8 spi, __u32 mask_size)
+{
+	__ipcache_v4_add_entry(addr, cluster_id, sec_identity, tunnel_ep, spi, false, mask_size);
 }
 
 static __always_inline void
