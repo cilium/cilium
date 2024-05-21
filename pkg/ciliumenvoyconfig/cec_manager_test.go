fce952b97579e8c29d1269a742008d1fbcd68ba4
Tam Mach <tam.mach@cilium.io>
2024-05-21 23:05:52 +1000

    ingress: Support headless service
    Currently, we don't inject headless endpoints into envoy XDS, hence the
    response is coming with error `no healthy upstream`. This commit is to
    explicitly handle headless service in CEC controller.
    
    One point worth noting is that k8s.Endpoint watcher is used as it's a
    wrapper for both k8s Endpoint and EndpointSlice.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/ciliumenvoyconfig/cec_manager_test.go
+++ pkg/ciliumenvoyconfig/cec_manager_test.go
@@ -12,9 +12,16 @@ import (
 	envoy_config_http "github.com/cilium/proxy/go/envoy/extensions/filters/network/http_connection_manager/v3"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"sigs.k8s.io/yaml"
 
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
+	"github.com/cilium/cilium/pkg/k8s"
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	"github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/util/intstr"
+	"github.com/cilium/cilium/pkg/loadbalancer"
 )
 
 var envoySpec = []byte(`apiVersion: cilium.io/v2
@@ -246,4 +253,267 @@ spec:
 	err = json.Unmarshal(jsonBytes, ccec)
 	assert.NoError(t, err)
 	assert.True(t, useOriginalSourceAddress(&ccec.ObjectMeta))
+}
+
+func Test_convertToLBService(t *testing.T) {
+	type args struct {
+		svc *slim_corev1.Service
+		ep  *k8s.Endpoints
+	}
+	tests := []struct {
+		name string
+		args args
+		want []*loadbalancer.SVC
+	}{
+		{
+			name: "headless with one port and one address",
+			args: args{
+				svc: &slim_corev1.Service{
+					ObjectMeta: slim_metav1.ObjectMeta{
+						Name:      "headless-service",
+						Namespace: "default",
+					},
+					Spec: slim_corev1.ServiceSpec{
+						ClusterIP: "None",
+						Ports: []slim_corev1.ServicePort{
+							{
+								Name:       "http",
+								Protocol:   "TCP",
+								Port:       8080,
+								TargetPort: intstr.FromInt32(3000),
+							},
+						},
+					},
+				},
+				ep: &k8s.Endpoints{
+					Backends: map[cmtypes.AddrCluster]*k8s.Backend{
+						cmtypes.MustParseAddrCluster("10.0.0.1"): {
+							Ports: map[string]*loadbalancer.L4Addr{
+								"http": {
+									Protocol: "TCP",
+									Port:     3000,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: []*loadbalancer.SVC{
+				{
+					Name: loadbalancer.ServiceName{
+						Name:      "headless-service",
+						Namespace: "default",
+					},
+					Frontend: loadbalancer.L3n4AddrID{
+						L3n4Addr: loadbalancer.L3n4Addr{
+							L4Addr: loadbalancer.L4Addr{
+								Protocol: "TCP",
+								Port:     8080,
+							},
+						},
+					},
+					Backends: []*loadbalancer.Backend{
+						{
+							FEPortName: "http",
+							L3n4Addr: loadbalancer.L3n4Addr{
+								AddrCluster: cmtypes.MustParseAddrCluster("10.0.0.1"),
+								L4Addr: loadbalancer.L4Addr{
+									Protocol: "TCP",
+									Port:     3000,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "headless with one port and two addresses",
+			args: args{
+				svc: &slim_corev1.Service{
+					ObjectMeta: slim_metav1.ObjectMeta{
+						Name:      "headless-service",
+						Namespace: "default",
+					},
+					Spec: slim_corev1.ServiceSpec{
+						ClusterIP: "None",
+						Ports: []slim_corev1.ServicePort{
+							{
+								Name:     "http",
+								Protocol: "TCP",
+								Port:     8080,
+							},
+						},
+					},
+				},
+				ep: &k8s.Endpoints{
+					Backends: map[cmtypes.AddrCluster]*k8s.Backend{
+						cmtypes.MustParseAddrCluster("10.0.0.1"): {
+							Ports: map[string]*loadbalancer.L4Addr{
+								"http": {
+									Protocol: "TCP",
+									Port:     8080,
+								},
+							},
+						},
+						cmtypes.MustParseAddrCluster("10.0.0.2"): {
+							Ports: map[string]*loadbalancer.L4Addr{
+								"http": {
+									Protocol: "TCP",
+									Port:     8080,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: []*loadbalancer.SVC{
+				{
+					Name: loadbalancer.ServiceName{
+						Name:      "headless-service",
+						Namespace: "default",
+					},
+					Frontend: loadbalancer.L3n4AddrID{
+						L3n4Addr: loadbalancer.L3n4Addr{
+							L4Addr: loadbalancer.L4Addr{
+								Protocol: "TCP",
+								Port:     8080,
+							},
+						},
+					},
+					Backends: []*loadbalancer.Backend{
+						{
+							FEPortName: "http",
+							L3n4Addr: loadbalancer.L3n4Addr{
+								AddrCluster: cmtypes.MustParseAddrCluster("10.0.0.1"),
+								L4Addr: loadbalancer.L4Addr{
+									Protocol: "TCP",
+									Port:     8080,
+								},
+							},
+						},
+						{
+							FEPortName: "http",
+							L3n4Addr: loadbalancer.L3n4Addr{
+								AddrCluster: cmtypes.MustParseAddrCluster("10.0.0.2"),
+								L4Addr: loadbalancer.L4Addr{
+									Protocol: "TCP",
+									Port:     8080,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "headless with two ports and one address",
+			args: args{
+				svc: &slim_corev1.Service{
+					ObjectMeta: slim_metav1.ObjectMeta{
+						Name:      "headless-service",
+						Namespace: "default",
+					},
+					Spec: slim_corev1.ServiceSpec{
+						ClusterIP: "None",
+						Ports: []slim_corev1.ServicePort{
+							{
+								Name:     "http",
+								Protocol: "TCP",
+								Port:     8080,
+							},
+							{
+								Name:     "https",
+								Protocol: "TCP",
+								Port:     8443,
+							},
+						},
+					},
+				},
+				ep: &k8s.Endpoints{
+					Backends: map[cmtypes.AddrCluster]*k8s.Backend{
+						cmtypes.MustParseAddrCluster("10.0.0.1"): {
+							Ports: map[string]*loadbalancer.L4Addr{
+								"http": {
+									Protocol: "TCP",
+									Port:     8080,
+								},
+								"https": {
+									Protocol: "TCP",
+									Port:     8443,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: []*loadbalancer.SVC{
+				{
+					Name: loadbalancer.ServiceName{
+						Name:      "headless-service",
+						Namespace: "default",
+					},
+					Frontend: loadbalancer.L3n4AddrID{
+						L3n4Addr: loadbalancer.L3n4Addr{
+							L4Addr: loadbalancer.L4Addr{
+								Protocol: "TCP",
+								Port:     8080,
+							},
+						},
+					},
+					Backends: []*loadbalancer.Backend{
+						{
+							FEPortName: "http",
+							L3n4Addr: loadbalancer.L3n4Addr{
+								AddrCluster: cmtypes.MustParseAddrCluster("10.0.0.1"),
+								L4Addr: loadbalancer.L4Addr{
+									Protocol: "TCP",
+									Port:     8080,
+								},
+							},
+						},
+					},
+				},
+				{
+					Name: loadbalancer.ServiceName{
+						Name:      "headless-service",
+						Namespace: "default",
+					},
+					Frontend: loadbalancer.L3n4AddrID{
+						L3n4Addr: loadbalancer.L3n4Addr{
+							L4Addr: loadbalancer.L4Addr{
+								Protocol: "TCP",
+								Port:     8443,
+							},
+						},
+					},
+					Backends: []*loadbalancer.Backend{
+						{
+							FEPortName: "https",
+							L3n4Addr: loadbalancer.L3n4Addr{
+								AddrCluster: cmtypes.MustParseAddrCluster("10.0.0.1"),
+								L4Addr: loadbalancer.L4Addr{
+									Protocol: "TCP",
+									Port:     8443,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			svcs := convertToLBService(tt.args.svc, tt.args.ep)
+			require.Len(t, svcs, len(tt.want))
+			for i := 0; i < len(svcs); i++ {
+				require.Equal(t, tt.want[i].Name, svcs[i].Name)
+				require.Equal(t, tt.want[i].Frontend, svcs[i].Frontend)
+				require.Len(t, svcs[i].Backends, len(tt.want[i].Backends))
+				require.ElementsMatch(t, tt.want[i].Backends, svcs[i].Backends)
+			}
+		})
+	}
 }
