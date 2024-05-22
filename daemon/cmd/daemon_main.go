cilium/cilium

commit f21f303003d9026a925cf5a838ff528ea977f202

daemon: bubble up error from startDaemon instead of fataling
Return the error from startDaemon through the start hook to abort
shutdown cleanly rather than using log.Fatal.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/daemon_main.go
@@ -1688,7 +1688,10 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 			daemon = d
 
 			if !option.Config.DryMode {
-				startDaemon(daemon, restoredEndpoints, cleaner, params)
+				if err := startDaemon(daemon, restoredEndpoints, cleaner, params); err != nil {
+					daemonResolver.Reject(err)
+					return err
+				}
 			}
 			daemonResolver.Resolve(daemon)
 
@@ -1708,14 +1711,14 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 }
 
 // startDaemon starts the old unmodular part of the cilium-agent.
-func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *daemonCleanup, params daemonParams) {
+func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *daemonCleanup, params daemonParams) error {
 	log.Info("Initializing daemon")
 
 	// This validation needs to be done outside of the agent until
 	// datapath.NodeAddressing is used consistently across the code base.
 	log.Info("Validating configured node address ranges")
 	if err := node.ValidatePostInit(); err != nil {
-		log.Fatalf("postinit failed: %s", err)
+		return fmt.Errorf("postinit failed: %w", err)
 	}
 
 	bootstrapStats.enableConntrack.Start()
@@ -1744,11 +1747,11 @@ func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *da
 		d.endpointManager.InitHostEndpointLabels(d.ctx)
 	} else {
 		log.Info("Creating host endpoint")
-		if err := d.endpointManager.AddHostEndpoint(
+		err := d.endpointManager.AddHostEndpoint(
 			d.ctx, d, d, d.ipcache, d.l7Proxy, d.identityAllocator,
-			"Create host endpoint", nodeTypes.GetName(),
-		); err != nil {
-			log.Fatalf("unable to create host endpoint: %s", err)
+			"Create host endpoint", nodeTypes.GetName())
+		if err != nil {
+			return fmt.Errorf("unable to create host endpoint: %w", err)
 		}
 	}
 
@@ -1761,11 +1764,11 @@ func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *da
 				log.Warn("Ingress IPs are not available, skipping creation of the Ingress Endpoint: Policy enforcement on Cilium Ingress will not work as expected.")
 			} else {
 				log.Info("Creating ingress endpoint")
-				if err := d.endpointManager.AddIngressEndpoint(
+				err := d.endpointManager.AddIngressEndpoint(
 					d.ctx, d, d, d.ipcache, d.l7Proxy, d.identityAllocator,
-					"Create ingress endpoint",
-				); err != nil {
-					log.Fatalf("unable to create ingress endpoint: %s", err)
+					"Create ingress endpoint")
+				if err != nil {
+					return fmt.Errorf("unable to create ingress endpoint: %w", err)
 				}
 			}
 		}
@@ -1774,7 +1777,7 @@ func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *da
 	if option.Config.EnableIPMasqAgent {
 		ipmasqAgent, err := ipmasq.NewIPMasqAgent(option.Config.IPMasqAgentConfigPath)
 		if err != nil {
-			log.Fatalf("failed to create ipmasq agent: %s", err)
+			return fmt.Errorf("failed to create ipmasq agent: %w", err)
 		}
 		ipmasqAgent.Start()
 	}
@@ -1876,6 +1879,8 @@ func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *da
 	if err != nil {
 		log.WithError(err).Error("Unable to store Viper's configuration")
 	}
+
+	return nil
 }
 
 func newRestorerPromise(lc cell.Lifecycle, daemonPromise promise.Promise[*Daemon]) promise.Promise[endpointstate.Restorer] {

commit 12d1b8f24a2ad568711d65bfb6cf7c02cd647df9

daemon: Cleanup after a failed start
If the daemon start failed due to e.g. invalid configuration the cleanup was not
performed as it was only done from the stop function that was not invoked since
the daemon start had failed.

This in some situations lead to confusing crashes as hive did stop all the dependencies
of the legacy daemon, for example this crash was reported:

   level=fatal msg="failed to start: daemon creation failed: failed to detect devices: ...
   ... bunch of "stopping" messages omitted ...
   panic: runtime error: invalid memory address or nil pointer dereference
   [signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0x1dfa041]
   goroutine 460 [running]:
     github.com/cilium/cilium/pkg/node.(*LocalNodeStore).Observe(0x3f4cfb0? ...
     	<autogenerated>:1 +0x21

The crash was caused by the LocalNodeStore stopping and clearing the global variable
in the node package (to make sure for example our tests don't leak data across them
and to make sure we do cleanup correctly).

To fix this, run the cleanup functions also when "newDaemon" or "startDaemon" fails
with an error.

Fixes: f21f303003d9 ("daemon: bubble up error from startDaemon instead of fataling")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/daemon_main.go
@@ -1698,6 +1698,8 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 		OnStart: func(cell.HookContext) error {
 			d, restoredEndpoints, err := newDaemon(daemonCtx, cleaner, &params)
 			if err != nil {
+				cancelDaemonCtx()
+				cleaner.Clean()
 				return fmt.Errorf("daemon creation failed: %w", err)
 			}
 			daemon = d
@@ -1705,6 +1707,8 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 			if !option.Config.DryMode {
 				if err := startDaemon(daemon, restoredEndpoints, cleaner, params); err != nil {
 					daemonResolver.Reject(err)
+					cancelDaemonCtx()
+					cleaner.Clean()
 					return err
 				}
 			}
@@ -1713,9 +1717,6 @@ func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {
 		},
 		OnStop: func(cell.HookContext) error {
 			cancelDaemonCtx()
-			if daemon.statusCollector != nil {
-				daemon.statusCollector.Close()
-			}
 			cleaner.Clean()
 			wg.Wait()
 			return nil

daemon/cmd/status.go
@@ -1136,5 +1136,7 @@ func (d *Daemon) startStatusCollector(cleaner *daemonCleanup) {
 			}).Error("KVStore state not OK")
 
 		}
+
+		d.statusCollector.Close()
 	})
 }