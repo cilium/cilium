78f035f685eff2e6fb2496fd567b6ca7609b7f64
Chris Tarazi <chris@isovalent.com>
2024-05-22 16:12:04 -0700

    endpoint: Consolidate regeneration metrics accounting
    This commit consolidates the policy regeneration/recalculation metrics
    into the broader `regenerationStatistics` struct.
    
    The policy regeneration count metric is equivalent to the endpoint
    regeneration count, by inspecting the values on a local deployment of
    Cilium. Additionally through code inspection, it occurred to me that the
    entirety of the `policyRegenerationStatistics` struct already exists in
    `regenerationStatistics` struct, except for the policy repository span.
    Therefore, it is waste to have duplicated metrics because it
    unnecessarily increases the number of metrics Cilium sends to Prometheus
    which has costs to the user.
    
    Subsequent commits will remove this redundancy altogether and deprecate
    the corresponding metrics to reduce cardinality.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/endpoint/metrics.go
+++ pkg/endpoint/metrics.go
@@ -38,19 +38,21 @@ func sendMetrics(stats statistics, metric metric.Vec[metric.Observer]) {
 }
 
 type regenerationStatistics struct {
-	success                bool
-	endpointID             uint16
-	policyStatus           models.EndpointPolicyEnabled
-	totalTime              spanstat.SpanStat
-	waitingForLock         spanstat.SpanStat
-	waitingForCTClean      spanstat.SpanStat
-	policyCalculation      spanstat.SpanStat
-	proxyConfiguration     spanstat.SpanStat
-	proxyPolicyCalculation spanstat.SpanStat
-	proxyWaitForAck        spanstat.SpanStat
-	datapathRealization    loaderMetrics.SpanStat
-	mapSync                spanstat.SpanStat
-	prepareBuild           spanstat.SpanStat
+	success                    bool
+	endpointID                 uint16
+	policyStatus               models.EndpointPolicyEnabled
+	totalTime                  spanstat.SpanStat
+	waitingForLock             spanstat.SpanStat
+	waitingForIdentityCache    spanstat.SpanStat
+	waitingForPolicyRepository spanstat.SpanStat
+	waitingForCTClean          spanstat.SpanStat
+	policyCalculation          spanstat.SpanStat
+	proxyConfiguration         spanstat.SpanStat
+	proxyPolicyCalculation     spanstat.SpanStat
+	proxyWaitForAck            spanstat.SpanStat
+	datapathRealization        loaderMetrics.SpanStat
+	mapSync                    spanstat.SpanStat
+	prepareBuild               spanstat.SpanStat
 }
 
 // SendMetrics sends the regeneration statistics for this endpoint to
@@ -72,15 +74,17 @@ func (s *regenerationStatistics) SendMetrics() {
 // GetMap returns a map which key is the stat name and the value is the stat
 func (s *regenerationStatistics) GetMap() map[string]*spanstat.SpanStat {
 	result := map[string]*spanstat.SpanStat{
-		"waitingForLock":         &s.waitingForLock,
-		"waitingForCTClean":      &s.waitingForCTClean,
-		"policyCalculation":      &s.policyCalculation,
-		"proxyConfiguration":     &s.proxyConfiguration,
-		"proxyPolicyCalculation": &s.proxyPolicyCalculation,
-		"proxyWaitForAck":        &s.proxyWaitForAck,
-		"mapSync":                &s.mapSync,
-		"prepareBuild":           &s.prepareBuild,
-		"total":                  &s.totalTime,
+		"waitingForLock":             &s.waitingForLock,
+		"waitingForIdentityCache":    &s.waitingForIdentityCache,
+		"waitingForPolicyRepository": &s.waitingForPolicyRepository,
+		"waitingForCTClean":          &s.waitingForCTClean,
+		"policyCalculation":          &s.policyCalculation,
+		"proxyConfiguration":         &s.proxyConfiguration,
+		"proxyPolicyCalculation":     &s.proxyPolicyCalculation,
+		"proxyWaitForAck":            &s.proxyWaitForAck,
+		"mapSync":                    &s.mapSync,
+		"prepareBuild":               &s.prepareBuild,
+		"total":                      &s.totalTime,
 	}
 	for k, v := range s.datapathRealization.GetMap() {
 		result[k] = v
