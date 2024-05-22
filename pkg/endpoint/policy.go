a296b283f9f724ecfc791289d98bf989d2a5aef8
Chris Tarazi <chris@isovalent.com>
2024-05-22 16:12:04 -0700

    endpoint: Consolidate regeneration metrics accounting
    This commit consolidates the policy regeneration/recalculation metrics
    into the broader `regenerationStatistics` struct.
    
    The policy regeneration count metric is equivalent to the endpoint
    regeneration count, by inspecting the values on a local deployment of
    Cilium. Additionally through code inspection, it occurred to me that the
    entirety of the `policyRegenerationStatistics` struct already exists in
    `regenerationStatistics` struct, except for the policy repository span.
    Therefore, it is waste to have duplicated metrics because it
    unnecessarily increases the number of metrics Cilium sends to Prometheus
    which has costs to the user.
    
    Subsequent commits will remove this redundancy altogether and deprecate
    the corresponding metrics to reduce cardinality.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/endpoint/policy.go
+++ pkg/endpoint/policy.go
@@ -195,7 +195,7 @@ type policyGenerateResult struct {
 //
 // Returns a result that should be passed to setDesiredPolicy after the endpoint's
 // write lock has been acquired, or err if recomputing policy failed.
-func (e *Endpoint) regeneratePolicy() (*policyGenerateResult, error) {
+func (e *Endpoint) regeneratePolicy(stats *regenerationStatistics) (*policyGenerateResult, error) {
 	var err error
 
 	// lock the endpoint, read our values, then unlock
@@ -229,11 +229,6 @@ func (e *Endpoint) regeneratePolicy() (*policyGenerateResult, error) {
 	e.runlock()
 
 	e.getLogger().Debug("Starting policy recalculation...")
-	stats := &policyRegenerationStatistics{}
-	stats.totalTime.Start()
-	defer func() {
-		e.updatePolicyRegenerationStatistics(stats, forcePolicyCompute, err)
-	}()
 
 	stats.waitingForPolicyRepository.Start()
 	repo := e.policyGetter.GetPolicyRepository()
