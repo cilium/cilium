3de3d37bc3d0d599bfa35c7980c42e425293cf48
Ondrej Blazek <ondrej.blazek@firma.seznam.cz>
2024-05-22 11:18:13 +0200

    policy: skip validation of policies behind a flag
    Both `cilium policy import` and `cilium policy validate` command not
    only use server side validation of policies but both also use client
    side validation of policies - meaning they both run `r.Sanitize()`
    without being able to properly check cilium-agent's configuration
    flags. This can result in being unable to validate/import policy that
    uses a field (like fromNodes, toNodes) which is not available by default
    and it's configuration is false by default.
    
    In existing examples this "only" runs `r.Sanitize()` twice. The problem
    is in a situation where some of the features are not enabled by default.
    In that case client side validation fails even though server side
    validation would run properly.
    This can also be a problem when we do `./check-examples.sh` in
    documentation which runs only client side validation.
    
    Let's skip client side validation of rules that are validatable only on
    the server side.
    
    Signed-off-by: Ondrej Blazek <ondrej.blazek@firma.seznam.cz>

--- cilium-dbg/cmd/policy_import.go
+++ cilium-dbg/cmd/policy_import.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 
@@ -12,6 +13,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/policy/api"
 )
 
 var printPolicy bool
@@ -40,7 +42,12 @@ var policyImportCmd = &cobra.Command{
 
 			for _, r := range ruleList {
 				if err := r.Sanitize(); err != nil {
-					Fatalf("%s", err)
+					if errors.Is(err, api.ErrFromToNodesRequiresNodeSelectorOption) {
+						// Don't error out as this can't be validated client-side
+						fmt.Printf("Validation of policy %s has been skipped in the client, further validation will occur server-side.\n", path)
+					} else {
+						Fatalf("%s", err)
+					}
 				}
 			}
 
