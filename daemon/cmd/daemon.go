0001f4b3fcf950ba6a3bb6fb0cba14931b340ce3
Marco Iorio <marco.iorio@isovalent.com>
2024-05-27 17:50:59 +0200

    identity: parametrize AllocationIdentity range functions
    Take the clusterID as parameter, rather than retrieving it from the
    global option. This allows to reuse the same methods also for clusterIDs
    different from the one configured locally.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- daemon/cmd/daemon.go
+++ daemon/cmd/daemon.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cilium/cilium/pkg/auth"
 	"github.com/cilium/cilium/pkg/cgroups/manager"
 	"github.com/cilium/cilium/pkg/clustermesh"
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/datapath/link"
 	linuxdatapath "github.com/cilium/cilium/pkg/datapath/linux"
@@ -132,6 +133,7 @@ type Daemon struct {
 	// programs.
 	compilationLock datapath.CompilationLock
 
+	clusterInfo cmtypes.ClusterInfo
 	clustermesh *clustermesh.ClusterMesh
 
 	mtuConfig mtu.MTU
@@ -434,6 +436,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		egressGatewayManager: params.EgressGatewayManager,
 		ipamMetadata:         params.IPAMMetadataManager,
 		cniConfigManager:     params.CNIConfigManager,
+		clusterInfo:          params.ClusterInfo,
 		clustermesh:          params.ClusterMesh,
 		monitorAgent:         params.MonitorAgent,
 		svc:                  params.ServiceManager,
