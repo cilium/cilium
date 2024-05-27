a6739eec1f854b405843f2608edcc126ee498c6a
Robin Hahling <robin.hahling@gw-computing.net>
2024-05-27 17:36:41 +0200

    images/builder: let renovate update proto plugins
    [ upstream commit dc683b9ea77098d99fa18f6421664cc00319706c ]
    
    [ backporter's notes: skipped the renovate.json5 hunk, as not
      applicable. ]
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -8,8 +8,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# 8ba23be9613c672d40ae261d2a1335d639bdd59b == tag: cmd/protoc-gen-go-grpc/v1.3.0
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@8ba23be9613c672d40ae261d2a1335d639bdd59b
+# renovate: datasource=github-releases depName=grpc/grpc-go
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@1adbea267b837660726952ed6711b348dee87aa5
+# renovate: datasource=github-releases depName=protocolbuffers/protobuf-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.31.0
+# renovate: datasource=github-releases depName=mfridman/protoc-gen-go-json
 go install github.com/mfridman/protoc-gen-go-json@v1.2.0
+# renovate: datasource=github-releases depName=pseudomuto/protoc-gen-doc
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1
