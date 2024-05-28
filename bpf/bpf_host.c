66130255b10f3283359ea6d09ce0c3ee48122a4f
gray <greyschwinger@gmail.com>
2024-05-28 15:57:23 +0800

    Don't set 0x200 mark for proxy to world traffic in iptables PREROUTING
    We have an iptables rule to set 0x200 mark for transparent socket:
    
    ```
    *mangle
    -A PREROUTING -m comment --comment "cilium-feeder: CILIUM_PRE_mangle" -j CILIUM_PRE_mangle
    -A CILIUM_PRE_mangle -m socket --transparent -m mark ! --mark 0xe00/0xf00 -m comment --comment "cilium: any->pod redirect proxied traffic to host proxy" -j MARK --set-xmark 0x200/0xffffffff
    ```
    
    This rule is in the mangle PREROUTING which checks packets ingressed
    from a netdev.
    
    Let's then focus on the pod to world traffic when IPsec=on + proxy=on +
    tunnel=off.
    
    Currently, a pod-to-world packet will go through the path:
    1. from_lxc@lxc: skb->mark is set to 0x200 and returned to stack
    2. iptables: skb is hijacked by tproxy (due to 0x200), to be accepted by proxy
    3. proxy process: the old skb is consumed by proxy, an new skb is sent to upstream (world)
    4. stack routing: the new skb is routed to eth0
    5. stack iptables: the new skb is traversing OUTPUT chain and POSTROUTING chain
    6. to_netdev@eth0: the new skb is going to world
    
    Please note the new skb won't hit PREROUTING chain, where there is a
    rule setting skb->mark=0x200.
    
    To fix https://github.com/cilium/cilium/issues/31984, we are going to
    change the routing for packets from egress proxy; consequently, on the
    step 4 above, the new skb will be routed to cilium_host instead:
    
    4. stack routing: the new skb is routed to cilium_host
    5. from_host@cilium_host: the new skb is returned to stack
    6. to_host@cilium_net: the new skb is returned to stack
    7. stack: PREROUTING, routing, FORWARD, POSTROUTING
    
    Look at step 7, we are hitting PREROUTING! Because of
    https://github.com/cilium/proxy/pull/742, this to-world skb is also
    linked to a transparent socket, matching the "-m socket --transparent"
    condition, the packet will fortunately have the 0x200 mark.
    
    If we do nothing, this to-world skb marked with 0x200 will then hit
    routiong rule "from all fwmark 0x200/0xf00 lookup 2004" and be routed to
    local. It should have gone to the world.
    
    This patch fixes this future issue as a precaution (otherwise we'll
    break git-bisect).
    
    This patch provides a straightforward solution: at step 5
    from_host@cilium_host, we set a specical mark 0x800
    (MARK_MAGIC_PROXY_TO_WORLD), then iptables can exclude this mark using
    "-m mark ! --mark 0x800/0xf00".
    
    Signed-off-by: gray <gray.liang@isovalent.com>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -398,7 +398,11 @@ skip_tunnel:
 	if (from_proxy && info->tunnel_endpoint && encrypt_key)
 		return set_ipsec_encrypt(ctx, encrypt_key, info->tunnel_endpoint,
 					 info->sec_identity, true, false);
-#endif
+
+	if (from_proxy &&
+	    (!info || !identity_is_cluster(info->sec_identity)))
+		ctx->mark = MARK_MAGIC_PROXY_TO_WORLD;
+#endif /* ENABLE_IPSEC && !TUNNEL_MODE */
 
 	return CTX_ACT_OK;
 }
@@ -854,7 +858,11 @@ skip_tunnel:
 	if (from_proxy && info->tunnel_endpoint && encrypt_key)
 		return set_ipsec_encrypt(ctx, encrypt_key, info->tunnel_endpoint,
 					 info->sec_identity, true, false);
-#endif
+
+	if (from_proxy &&
+	    (!info || !identity_is_cluster(info->sec_identity)))
+		ctx->mark = MARK_MAGIC_PROXY_TO_WORLD;
+#endif /* ENABLE_IPSEC && !TUNNEL_MODE */
 
 	return CTX_ACT_OK;
 }
