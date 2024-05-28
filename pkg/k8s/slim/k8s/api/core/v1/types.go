07a4baf4c57ec3d4ec09d497dc462e1a3eb5c86f
Tam Mach <tam.mach@cilium.io>
2024-05-28 23:06:13 +1000

    k8s: Add TargetPort into Service slim struct
    There is no use case that I can think of right now, but will be handy
    to use it in Gateway API unit tests.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- pkg/k8s/slim/k8s/api/core/v1/types.go
+++ pkg/k8s/slim/k8s/api/core/v1/types.go
@@ -7,6 +7,7 @@ package v1
 
 import (
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	"github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/util/intstr"
 )
 
 const (
@@ -911,6 +912,17 @@ type ServicePort struct {
 
 	// The port that will be exposed by this service.
 	Port int32 `json:"port" protobuf:"varint,3,opt,name=port"`
+
+	// Number or name of the port to access on the pods targeted by the service.
+	// Number must be in the range 1 to 65535. Name must be an IANA_SVC_NAME.
+	// If this is a string, it will be looked up as a named port in the
+	// target Pod's container ports. If this is not specified, the value
+	// of the 'port' field is used (an identity map).
+	// This field is ignored for services with clusterIP=None, and should be
+	// omitted or set equal to the 'port' field.
+	// More info: https://kubernetes.io/docs/concepts/services-networking/service/#defining-a-service
+	// +optional
+	TargetPort intstr.IntOrString `json:"targetPort,omitempty" protobuf:"bytes,4,opt,name=targetPort"`
 
 	// The port on each node on which this service is exposed when type is
 	// NodePort or LoadBalancer.  Usually assigned by the system. If a value is
