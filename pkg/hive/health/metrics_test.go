cilium/cilium

commit 2311f3d7811c8950b7f5e1e2ac290f8216be0dc3

treewide: rebase on cilium/hive
The general idea is to have `pkg/hive` contain the cilium-specific
adaptations of `cilium/hive`. Specifically, that's providing certain
cells like statedb and jobs directly, and having module decorators
creating the right health and logging components.

It's one big commit as it has proven difficult to get intermediate
commits to compile (which our CI mandates). The health part needs a bit
of love after this, we can probably move `healthv2` into `hive/health`
as it's strongly tied to cilium's hive.

The bulk of the changes are mechanical. The categories are:

1. import path change from cilium/cilium/pkg/hive/{cell,job} to
cilium/hive/{cell,job}
2. giving hive.{Start,Stop,Run} an slog.Logger to provide to the hive,
3. changes to the health interfaces - Scope and HealthReporter are
unified under just Health
4. modules are provided with a job group scoped to their module id
directly, instead of having to create a group from a registry

In addition, a broken test is fixed in api_limits_test. The test was
inadvertently using global state of pflag, and reading the config did
_not_ actually work, but just inherited the existing state from a
previous test.

Finally, in pkg endpoint, due to the removal of cell.GetSubScope, a bit
of health logic is exposed - cell.GetSubScope used to allocate a new
shim scope if the passed parent is `nil`, which it is for EP tests.
Instead of fixing up all the tests which don't pass a health, put that
logic into pkg/endpoint where it's at least visible, and can be cleaned
up if so desired.

Signed-off-by: Jussi Maki <jussi@isovalent.com>
Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

clustermesh-apiserver/metrics/metrics.go
@@ -8,13 +8,13 @@ import (
 	"net/http"
 	"regexp"
 
+	"github.com/cilium/hive/cell"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 
-	"github.com/cilium/cilium/pkg/hive/cell"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/metrics/metric"
 	"github.com/cilium/cilium/pkg/option"

daemon/cmd/cni/cell.go
@@ -8,12 +8,12 @@ import (
 	"fmt"
 	"path"
 
+	"github.com/cilium/hive/cell"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/controller"
-	"github.com/cilium/cilium/pkg/hive/cell"
 	"github.com/cilium/cilium/pkg/option"
 	cnitypes "github.com/cilium/cilium/plugins/cilium-cni/types"
 )

daemon/restapi/cell.go
@@ -3,7 +3,7 @@
 
 package restapi
 
-import "github.com/cilium/cilium/pkg/hive/cell"
+import "github.com/cilium/hive/cell"
 
 var Cell = cell.Module(
 	"cilium-restapi",

operator/api/cell.go
@@ -4,9 +4,8 @@
 package api
 
 import (
+	"github.com/cilium/hive/cell"
 	"github.com/spf13/pflag"
-
-	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
 const (

operator/api/metrics.go
@@ -4,11 +4,11 @@
 package api
 
 import (
+	"github.com/cilium/hive/cell"
 	"github.com/go-openapi/runtime/middleware"
 
 	"github.com/cilium/cilium/api/v1/operator/server/restapi/metrics"
 	opMetrics "github.com/cilium/cilium/operator/metrics"
-	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
 var MetricsHandlerCell = cell.Module(

operator/api/metrics_test.go
@@ -12,6 +12,8 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/hivetest"
 	"github.com/go-openapi/runtime"
 	"go.uber.org/goleak"
 
@@ -19,7 +21,7 @@ import (
 	"github.com/cilium/cilium/api/v1/operator/server/restapi/metrics"
 	operatorMetrics "github.com/cilium/cilium/operator/metrics"
 	"github.com/cilium/cilium/pkg/hive"
-	"github.com/cilium/cilium/pkg/hive/cell"
+	cellMetric "github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/metrics/metric"
 	"github.com/cilium/cilium/pkg/safeio"
 )
@@ -59,7 +61,8 @@ func TestMetricsHandlerWithoutMetrics(t *testing.T) {
 		}),
 	)
 
-	if err := hive.Start(context.Background()); err != nil {
+	tlog := hivetest.Logger(t)
+	if err := hive.Start(tlog, context.Background()); err != nil {
 		t.Fatalf("failed to start: %s", err)
 	}
 
@@ -82,7 +85,7 @@ func TestMetricsHandlerWithoutMetrics(t *testing.T) {
 		t.Fatalf("no metrics expected, found %v", metrics)
 	}
 
-	if err := hive.Stop(context.Background()); err != nil {
+	if err := hive.Stop(tlog, context.Background()); err != nil {
 		t.Fatalf("failed to stop: %s", err)
 	}
 }
@@ -104,7 +107,7 @@ func TestMetricsHandlerWithMetrics(t *testing.T) {
 				EnableGatewayAPI: false,
 			}
 		}),
-		cell.Metric(newTestMetrics),
+		cellMetric.Metric(newTestMetrics),
 
 		MetricsHandlerCell,
 
@@ -138,7 +141,8 @@ func TestMetricsHandlerWithMetrics(t *testing.T) {
 	// To avoid port clashing while testing, let the kernel pick an available port.
 	hive.Viper().Set(operatorMetrics.OperatorPrometheusServeAddr, "localhost:0")
 
-	if err := hive.Start(context.Background()); err != nil {
+	tlog := hivetest.Logger(t)
+	if err := hive.Start(tlog, context.Background()); err != nil {
 		t.Fatalf("failed to start: %s", err)
 	}
 
@@ -168,7 +172,7 @@ func TestMetricsHandlerWithMetrics(t *testing.T) {
 		t.Fatalf("error while inspecting metric: %s", err)
 	}
 
-	if err := hive.Stop(context.Background()); err != nil {
+	if err := hive.Stop(tlog, context.Background()); err != nil {
 		t.Fatalf("failed to stop: %s", err)
 	}
 }

operator/auth/cell.go
@@ -4,10 +4,10 @@
 package auth
 
 import (
+	"github.com/cilium/hive/cell"
 	"github.com/spf13/pflag"
 
 	"github.com/cilium/cilium/operator/auth/spire"
-	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
 const (

operator/endpointgc/cell.go
@@ -6,7 +6,9 @@ package endpointgc
 import (
 	"time"
 
-	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/hive/cell"
+
+	"github.com/cilium/cilium/pkg/metrics"
 )
 
 // Cell is a cell that implements a periodic and one-off Cilium endpoints
@@ -21,7 +23,7 @@ var Cell = cell.Module(
 	// Invoke forces the instantiation of the endpoint gc
 	cell.Invoke(registerGC),
 
-	cell.Metric(NewMetrics),
+	metrics.Metric(NewMetrics),
 )
 
 // SharedConfig contains the configuration that is shared between

commit c04f30d4584a34660e4764eec1eb83da8623cedd

hive/health: readd module health metrics
Somewhere in the move from pkg/hive to cilium/hive we lost the module
health metrics. Add them back in the new form of a query on the status
table, and add a test.

Fixes: 2311f3d781 (treewide: rebase on cilium/hive)

Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

pkg/hive/health/cell.go
@@ -10,6 +10,7 @@ import (
 	"github.com/cilium/statedb/index"
 
 	"github.com/cilium/cilium/pkg/hive/health/types"
+	"github.com/cilium/cilium/pkg/metrics"
 )
 
 var Cell = cell.Module(
@@ -20,6 +21,10 @@ var Cell = cell.Module(
 		newHealthV2Provider,
 		statedb.RWTable[types.Status].ToTable,
 	),
+
+	// Module health metrics.
+	cell.Invoke(metricPublisher),
+	metrics.Metric(newMetrics),
 )
 
 var (

pkg/hive/health/metrics.go
@@ -1,0 +1,91 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package health
+
+import (
+	"context"
+	"strings"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+	"github.com/cilium/statedb"
+
+	"github.com/cilium/cilium/pkg/hive/health/types"
+	"github.com/cilium/cilium/pkg/metrics/metric"
+	"github.com/cilium/cilium/pkg/rate"
+	"github.com/cilium/cilium/pkg/time"
+)
+
+type Metrics struct {
+	HealthStatusGauge metric.Vec[metric.Gauge]
+}
+
+func newMetrics() *Metrics {
+	return &Metrics{
+		HealthStatusGauge: metric.NewGaugeVec(metric.GaugeOpts{
+			ConfigName: "hive_health_status_levels",
+			Namespace:  "cilium",
+			Subsystem:  "hive",
+			Name:       "status",
+			Help:       "Counts of health status levels of Hive components",
+		}, []string{"status"}),
+	}
+}
+
+type publishFunc func(map[types.Level]uint64)
+
+type metricPublisherParams struct {
+	cell.In
+
+	DB       *statedb.DB
+	Table    statedb.Table[types.Status]
+	JobGroup job.Group
+	Metrics  *Metrics
+}
+
+// metricPublisher periodically publishes the hive module health metric (hive_health_status_levels).
+func metricPublisher(p metricPublisherParams) {
+	// Performs the actual writing to the metric. Extracted to make testing easy.
+	publish := func(stats map[types.Level]uint64) {
+		for l, v := range stats {
+			p.Metrics.HealthStatusGauge.WithLabelValues(strings.ToLower(string(l))).Set(float64(v))
+		}
+	}
+
+	if p.Metrics.HealthStatusGauge.IsEnabled() {
+		p.JobGroup.Add(job.OneShot("module-status-metrics",
+			func(ctx context.Context, health cell.Health) error {
+				return publishJob(ctx, p, publish)
+			}))
+	}
+}
+
+func publishJob(ctx context.Context, p metricPublisherParams, publish publishFunc) error {
+	// Limit rate of updates to the metric. The status table is updated often, the
+	// watch channel is closed on every modification (since we're watching all) and
+	// traversing the full table is somewhat expensive, so let's limit ourselves.
+	limiter := rate.NewLimiter(15*time.Second, 3)
+	defer limiter.Stop() // Avoids leaking a goroutine.
+
+	it, watch := p.Table.All(p.DB.ReadTxn())
+	for {
+		stats := make(map[types.Level]uint64)
+		for obj, _, ok := it.Next(); ok; obj, _, ok = it.Next() {
+			stats[obj.Level]++
+		}
+		publish(stats)
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-watch:
+		}
+
+		if err := limiter.Wait(ctx); err != nil {
+			return err
+		}
+
+		it, watch = p.Table.All(p.DB.ReadTxn())
+	}
+}

pkg/hive/health/metrics_test.go
@@ -1,0 +1,115 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package health
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cilium/hive"
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/hivetest"
+	"github.com/cilium/hive/job"
+	"github.com/cilium/statedb"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/cilium/cilium/pkg/hive/health/types"
+)
+
+func Test_Metrics(t *testing.T) {
+	var db *statedb.DB
+	var table statedb.Table[types.Status]
+	resChan := make(chan map[types.Level]uint64)
+	// Using upstream hive here to avoid import cycles and be able to inject the right metric
+	// publishing func for the test.
+	h := hive.New(
+		statedb.Cell,
+		job.Cell,
+
+		cell.ProvidePrivate(newTablesPrivate),
+		cell.Provide(
+			newHealthV2Provider,
+			statedb.RWTable[types.Status].ToTable,
+		),
+		cell.Provide(func(lc cell.Lifecycle, p types.Provider, jr job.Registry) job.Group {
+			h := p.ForModule(cell.FullModuleID{"test"})
+			jg := jr.NewGroup(h)
+			lc.Append(jg)
+			return jg
+		}),
+
+		cell.Module("health-metrics-test", "hive module health metrics test",
+			cell.Provide(newMetrics),
+			cell.Invoke(func(p metricPublisherParams) {
+				// Writes the updates to a chan to synchronise with the test.
+				publish := func(stats map[types.Level]uint64) {
+					resChan <- stats
+				}
+
+				p.JobGroup.Add(job.OneShot("module-status-metrics",
+					func(ctx context.Context, health cell.Health) error {
+						return publishJob(ctx, p, publish)
+					}))
+			}),
+
+			// Generate some test module health data
+			cell.Invoke(func(db_ *statedb.DB, table_ statedb.RWTable[types.Status]) {
+				db = db_
+				table = table_
+				txn := db_.WriteTxn(table_)
+				_, _, err := table_.Insert(txn, types.Status{
+					ID: types.Identifier{
+						Module: cell.FullModuleID{"test"},
+					},
+					Level: types.LevelDegraded,
+				})
+				assert.NoError(t, err)
+				_, _, err = table_.Insert(txn, types.Status{
+					ID: types.Identifier{
+						Module: cell.FullModuleID{"test2"},
+					},
+					Level: types.LevelOK,
+				})
+				assert.NoError(t, err)
+				txn.Commit()
+			}),
+		),
+	)
+	require.NotNil(t, h)
+
+	tlog := hivetest.Logger(t)
+	err := h.Start(tlog, context.TODO())
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		err = h.Stop(tlog, context.TODO())
+		require.NoError(t, err)
+	})
+
+	// Metrics as they would be published
+	resMetrics := <-resChan
+	it, _ := table.All(db.ReadTxn())
+	ok, degraded, stopped := count(it)
+
+	assert.Equal(t, resMetrics[types.LevelOK], ok)
+	assert.Greater(t, resMetrics[types.LevelOK], uint64(1))
+	assert.Equal(t, resMetrics[types.LevelDegraded], degraded)
+	assert.Equal(t, resMetrics[types.LevelDegraded], uint64(1))
+	assert.Equal(t, resMetrics[types.LevelStopped], stopped)
+}
+
+func count(it statedb.Iterator[types.Status]) (ok uint64, degraded uint64, stopped uint64) {
+	for obj, _, hasMore := it.Next(); hasMore; obj, _, hasMore = it.Next() {
+		switch obj.Level {
+		case types.LevelOK:
+			ok++
+		case types.LevelDegraded:
+			degraded++
+		case types.LevelStopped:
+			stopped++
+		}
+	}
+	return
+}