0c77053c6f3dc9ab8ddf8bc0d6252742677ab2cf
Nate Sweet <nathanjsweet@pm.me>
2024-06-03 18:02:55 -0500

    connectivity: Add Port Range Tests
    Add port range tests for all port based
    connectivity tests.
    
    Signed-off-by: Nate Sweet <nathanjsweet@pm.me>

--- cilium-cli/connectivity/builder/client_with_service_account_egress_to_echo.go
+++ cilium-cli/connectivity/builder/client_with_service_account_egress_to_echo.go
@@ -8,17 +8,34 @@ import (
 
 	"github.com/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium-cli/connectivity/tests"
+	"github.com/cilium/cilium-cli/utils/features"
 )
 
 //go:embed manifests/client-with-service-account-egress-to-echo.yaml
 var clientWithServiceAccountEgressToEchoPolicyYAML string
 
+//go:embed manifests/client-with-service-account-egress-to-echo-port-range.yaml
+var clientWithServiceAccountEgressToEchoPolicyPortRangeYAML string
+
 type clientWithServiceAccountEgressToEcho struct{}
 
 func (t clientWithServiceAccountEgressToEcho) build(ct *check.ConnectivityTest, _ map[string]string) {
+	clientWithServiceAccountEgressToEchoTest(ct, false)
+	if ct.Features[features.PortRanges].Enabled {
+		clientWithServiceAccountEgressToEchoTest(ct, true)
+	}
+}
+
+func clientWithServiceAccountEgressToEchoTest(ct *check.ConnectivityTest, portRanges bool) {
+	testName := "client-with-service-account-egress-to-echo"
+	policyYAML := clientWithServiceAccountEgressToEchoPolicyYAML
+	if portRanges {
+		testName = "client-with-service-account-egress-to-echo-port-range"
+		policyYAML = clientWithServiceAccountEgressToEchoPolicyPortRangeYAML
+	}
 	// This policy allows port 8080 from client with service account label to echo
-	newTest("client-with-service-account-egress-to-echo", ct).
-		WithCiliumPolicy(clientWithServiceAccountEgressToEchoPolicyYAML).
+	newTest(testName, ct).
+		WithCiliumPolicy(policyYAML).
 		WithScenarios(
 			tests.PodToPod(tests.WithSourceLabelsOption(map[string]string{"kind": "client"})),
 		)
