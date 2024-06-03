f61308713bdcd9c4841e69d9369c90590e72f4ad
gray <greyschwinger@gmail.com>
2024-06-03 15:07:30 +0800

    bpf: nodeport: remove revDNAT for local backends in bpf_lxc
    Just let to-overlay/to-netdev handle it.
    
    This fixes a netfilter asymmetry, and the EDT issue.
    
    Signed-off-by: gray <gray.liang@isovalent.com>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -536,18 +536,6 @@ ct_recreate6:
 	case CT_RELATED:
 	case CT_REPLY:
 		policy_mark_skip(ctx);
-
-#ifdef ENABLE_NODEPORT
-		/* See comment in handle_ipv4_from_lxc(). */
-		if (ct_state->node_port && lb_is_svc_proto(tuple->nexthdr)) {
-			send_trace_notify(ctx, TRACE_TO_NETWORK, SECLABEL_IPV6,
-					  *dst_sec_identity, TRACE_EP_ID_UNKNOWN,
-					  TRACE_IFINDEX_UNKNOWN,
-					  trace.reason, trace.monitor);
-			return tail_call_internal(ctx, CILIUM_CALL_IPV6_NODEPORT_REVNAT,
-						  ext_err);
-		}
-#endif /* ENABLE_NODEPORT */
 		break;
 	default:
 		return DROP_UNKNOWN_CT;
@@ -991,25 +979,6 @@ ct_recreate4:
 	case CT_RELATED:
 	case CT_REPLY:
 		policy_mark_skip(ctx);
-
-#ifdef ENABLE_NODEPORT
-		/* This handles reply traffic for the case where the nodeport EP
-		 * is local to the node. We'll do the tail call to perform
-		 * the reverse DNAT.
-		 *
-		 * This codepath currently doesn't support revDNAT for ICMP,
-		 * so make sure that we only send TCP/UDP/SCTP down this way.
-		 */
-		if (ct_state->node_port && lb_is_svc_proto(tuple->nexthdr)) {
-			send_trace_notify(ctx, TRACE_TO_NETWORK, SECLABEL_IPV4,
-					  *dst_sec_identity, TRACE_EP_ID_UNKNOWN,
-					  TRACE_IFINDEX_UNKNOWN,
-					  trace.reason, trace.monitor);
-			return tail_call_internal(ctx, CILIUM_CALL_IPV4_NODEPORT_REVNAT,
-						  ext_err);
-		}
-
-#endif /* ENABLE_NODEPORT */
 
 		/* RevNAT for replies on a loopback connection: */
 		if (ct_state->rev_nat_index && ct_state->loopback) {
@@ -1602,17 +1571,6 @@ ipv6_policy(struct __ctx_buff *ctx, struct ipv6hdr *ip6, int ifindex, __u32 src_
 		return verdict;
 
 skip_policy_enforcement:
-#ifdef ENABLE_NODEPORT
-	if (ret == CT_NEW || ret == CT_REOPENED) {
-		bool node_port = ct_has_nodeport_egress_entry6(get_ct_map6(tuple),
-							       tuple, NULL, false);
-
-		ct_state_new.node_port = node_port;
-		if (ret == CT_REOPENED && ct_state->node_port != node_port)
-			ct_update_nodeport(get_ct_map6(tuple), tuple, node_port);
-	}
-#endif /* ENABLE_NODEPORT */
-
 	if (ret == CT_NEW) {
 		ct_state_new.src_sec_id = src_label;
 		ct_state_new.from_tunnel = from_tunnel;
@@ -1951,17 +1909,6 @@ ipv4_policy(struct __ctx_buff *ctx, struct iphdr *ip4, int ifindex, __u32 src_la
 		return verdict;
 
 skip_policy_enforcement:
-#ifdef ENABLE_NODEPORT
-	if (ret == CT_NEW || ret == CT_REOPENED) {
-		bool node_port = ct_has_nodeport_egress_entry4(get_ct_map4(tuple),
-							       tuple, NULL, false);
-
-		ct_state_new.node_port = node_port;
-		if (ret == CT_REOPENED && ct_state->node_port != node_port)
-			ct_update_nodeport(get_ct_map4(tuple), tuple, node_port);
-	}
-#endif /* ENABLE_NODEPORT */
-
 	if (ret == CT_NEW) {
 		ct_state_new.src_sec_id = src_label;
 		ct_state_new.from_tunnel = from_tunnel;
