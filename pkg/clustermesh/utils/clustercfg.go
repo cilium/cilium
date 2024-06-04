3058a83f507c9482ae3d5ae545e7a95954cb5f93
Marco Iorio <marco.iorio@isovalent.com>
2024-06-04 10:14:03 +0200

    clustermesh: periodically enforce cluster config presence
    Currently, we only write the cluster configuration once, when the
    clustermesh-apiserver starts and when kvstoremesh connects to a remote
    cluster. However, the cluster configuration presence is critical, as
    otherwise remote clusters cannot connect to the given cluster. Hence,
    let's introduce a periodic enforcement mechanism (every 5 minutes), to
    ensure that it gets eventually restored in case of external modifications
    or deletions (e.g., if the associated lease unexpectedly expires).
    
    No changes are performed in the cilium operator, as the configuration
    is already periodically enforced there.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/clustermesh/utils/clustercfg.go
+++ pkg/clustermesh/utils/clustercfg.go
@@ -7,10 +7,14 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"path"
 	"time"
 
+	"github.com/sirupsen/logrus"
+
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
+	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/kvstore"
 )
 
@@ -18,6 +22,12 @@ var (
 	// ErrClusterConfigNotFound is the sentinel error returned by
 	// GetClusterConfig if the cluster configuration is not found.
 	ErrClusterConfigNotFound = errors.New("not found")
+
+	// clusterConfigEnforcerGroup is the group for the controllers enforcing
+	clusterConfigEnforcerGroup = controller.NewGroup("clustermesh-cluster-config-enforcer")
+
+	// runInterval is the cluster configuration enforcement interval
+	runInterval = 5 * time.Minute
 )
 
 type ClusterConfigBackend interface {
@@ -42,6 +52,51 @@ func SetClusterConfig(ctx context.Context, clusterName string, config cmtypes.Ci
 	}
 
 	return nil
+}
+
+// EnforceClusterConfig synchronously writes the cluster configuration, and
+// additionally registers a background task to periodically enforce its presence
+// (e.g., in case the associated lease unexpectedly expired).
+func EnforceClusterConfig(
+	ctx context.Context, clusterName string, config cmtypes.CiliumClusterConfig,
+	backend ClusterConfigBackend, log logrus.FieldLogger,
+) (stopAndWait func(), err error) {
+	var (
+		mgr = controller.NewManager()
+		ch  = make(chan error, 1)
+	)
+
+	mgr.UpdateController(
+		fmt.Sprintf("clustermesh-cluster-config-enforcer-%s", clusterName),
+		controller.ControllerParams{
+			Context:     ctx,
+			Group:       clusterConfigEnforcerGroup,
+			RunInterval: runInterval,
+			DoFunc: func(ctx context.Context) error {
+				err := SetClusterConfig(ctx, clusterName, config, backend)
+				select {
+				case ch <- err:
+					if err != nil {
+						return controller.NewExitReason("initial enforcement failed")
+					}
+					return nil
+
+				default:
+					if err != nil {
+						log.WithError(err).Warning("Failed to write cluster configuration")
+					}
+
+					return err
+				}
+			},
+		},
+	)
+
+	// Wait to the initial synchronous enforcement, and then return.
+	err = <-ch
+	ch = nil
+
+	return mgr.RemoveAllAndWait, err
 }
 
 func GetClusterConfig(ctx context.Context, clusterName string, backend ClusterConfigBackend) (cmtypes.CiliumClusterConfig, error) {
