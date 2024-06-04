8f6e4f1b8c00a7d08063bf26339951d799edc15b
Marco Iorio <marco.iorio@isovalent.com>
2024-06-04 10:14:03 +0200

    clustermesh: periodically enforce cluster config presence
    Currently, we only write the cluster configuration once, when the
    clustermesh-apiserver starts and when kvstoremesh connects to a remote
    cluster. However, the cluster configuration presence is critical, as
    otherwise remote clusters cannot connect to the given cluster. Hence,
    let's introduce a periodic enforcement mechanism (every 5 minutes), to
    ensure that it gets eventually restored in case of external modifications
    or deletions (e.g., if the associated lease unexpectedly expires).
    
    No changes are performed in the cilium operator, as the configuration
    is already periodically enforced there.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/clustermesh/utils/clustercfg_test.go
+++ pkg/clustermesh/utils/clustercfg_test.go
@@ -8,6 +8,11 @@ import (
 	"errors"
 	"path"
 	"testing"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/goleak"
 
 	"github.com/stretchr/testify/require"
 
@@ -17,6 +22,12 @@ import (
 )
 
 var mockerr = errors.New("error")
+
+// Configure a generous timeout to prevent flakes when running in a noisy CI environment.
+var (
+	tick    = 10 * time.Millisecond
+	timeout = 5 * time.Second
+)
 
 type mockBackend struct {
 	data   lock.Map[string, []byte]
@@ -80,4 +91,48 @@ func TestGetSetClusterConfig(t *testing.T) {
 	mb.UpdateIfDifferent(ctx, path.Join(kvstore.ClusterConfigPrefix, "invalid"), []byte("invalid"), true)
 	_, err = GetClusterConfig(ctx, "invalid", &mb)
 	require.ErrorContains(t, err, "invalid character", "unmarshaling error not propagated correctly")
+}
+
+func TestEnforceClusterConfig(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	// Configure a short run interval for testing purposes
+	defer func(orig time.Duration) { runInterval = orig }(runInterval)
+	runInterval = 25 * time.Millisecond
+
+	ctx := context.Background()
+	mb := mockBackend{}
+	log := logrus.New()
+
+	cfg1 := cmtypes.CiliumClusterConfig{ID: 11, Capabilities: cmtypes.CiliumClusterConfigCapabilities{SyncedCanaries: true}}
+	cfg2 := cmtypes.CiliumClusterConfig{ID: 22, Capabilities: cmtypes.CiliumClusterConfigCapabilities{Cached: true}}
+
+	stopAndWait1, err := EnforceClusterConfig(ctx, "foo", cfg1, &mb, log)
+	defer stopAndWait1()
+	require.NoError(t, err, "failed to write cluster configuration")
+
+	stopAndWait2, err := EnforceClusterConfig(ctx, "bar", cfg2, &mb, log)
+	defer stopAndWait2()
+	require.NoError(t, err, "failed to write cluster configuration")
+
+	mb.withError("error")
+	stopAndWait3, err := EnforceClusterConfig(ctx, "error", cfg2, &mb, log)
+	defer stopAndWait3()
+	require.ErrorIs(t, err, mockerr, "kvstore error not propagated correctly")
+
+	got, err := GetClusterConfig(ctx, "foo", &mb)
+	require.NoError(t, err, "failed to read cluster configuration")
+	require.EqualValues(t, got, cfg1, "retrieved configuration does not match expected one")
+
+	got, err = GetClusterConfig(ctx, "bar", &mb)
+	require.NoError(t, err, "failed to read cluster configuration")
+	require.EqualValues(t, got, cfg2, "retrieved configuration does not match expected one")
+
+	// Externally mutate the cluster configuration, and assert that it gets eventually reconciled.
+	require.NoError(t, SetClusterConfig(ctx, "bar", cfg1, &mb), "failed to override cluster configuration")
+	require.EventuallyWithT(t, func(c *assert.CollectT) {
+		got, err = GetClusterConfig(ctx, "bar", &mb)
+		assert.NoError(c, err, "failed to read cluster configuration")
+		assert.EqualValues(c, got, cfg2, "retrieved configuration does not match expected one")
+	}, timeout, tick)
 }
