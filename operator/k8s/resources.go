47071d9b9c76d2a62b164bb8c16b95a916f42f30
Ovidiu Tirla <otirla@google.com>
2024-06-10 11:59:39 +0000

    pkg/k8s: Index Pod resources by namespace
    The Pod resource will be used by Operator Managing CIDs to reconcile all
    the pods in a namespace when the namespace labels are added or removed.
    
    Related #27752
    
    Signed-off-by: Ovidiu Tirla <otirla@google.com>

--- operator/k8s/resources.go
+++ operator/k8s/resources.go
@@ -43,7 +43,7 @@ var (
 			CiliumEndpointResource,
 			CiliumEndpointSliceResource,
 			CiliumNodeResource,
-			k8s.PodResource,
+			PodResource,
 			k8s.NamespaceResource,
 			k8s.CiliumNetworkPolicyResource,
 			k8s.CiliumClusterwideNetworkPolicyResource,
