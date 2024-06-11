cilium/cilium

commit 8d525fe39113ab371718ba99a2cdbe8dae1dfacf

Add node neighbor link updater controller
This controller is responsible for monitoring nodes that request a neighbor
link update.
It provides a single place in the application where all node neighbor link
are updates are performed as insertNeighbor are sprinkled in the code base.
Additionally given the central location we can provide additional
module health signals should neighbor link updates failed.

Signed-off-by: Fernand Galiana <fernand.galiana@gmail.com>

pkg/node/manager/manager.go
@@ -43,8 +43,10 @@ import (
 var (
 	randGen                    = rand.NewSafeRand(time.Now().UnixNano())
 	baseBackgroundSyncInterval = time.Minute
+	defaultNodeUpdateInterval  = 10 * time.Second
 
 	neighborTableRefreshControllerGroup = controller.NewGroup("neighbor-table-refresh")
+	neighborTableUpdateControllerGroup  = controller.NewGroup("neighbor-table-update")
 )
 
 const (
@@ -127,6 +129,24 @@ type manager struct {
 
 	// healthScope reports on the current health status of the node manager module.
 	healthScope cell.Scope
+
+	// nodeNeighborQueue tracks node neighbor link updates.
+	nodeNeighborQueue queue[nodeQueueEntry]
+}
+
+type nodeQueueEntry struct {
+	node    *nodeTypes.Node
+	refresh bool
+}
+
+// Enqueue add a node to a controller managed queue which sets up the neighbor link.
+func (m *manager) Enqueue(n *nodeTypes.Node, refresh bool) {
+	if n == nil {
+		log.WithFields(logrus.Fields{
+			logfields.LogSubsys: "enqueue",
+		}).Warn("Skipping nodeNeighbor insert: No node given")
+	}
+	m.nodeNeighborQueue.push(&nodeQueueEntry{node: n, refresh: refresh})
 }
 
 // Subscribe subscribes the given node handler to node events.
@@ -326,19 +346,21 @@ func (m *manager) backgroundSync(ctx context.Context) error {
 				m.mutex.RUnlock()
 				continue
 			}
+			m.mutex.RUnlock()
 
 			entry.mutex.Lock()
-			m.mutex.RUnlock()
-			m.Iter(func(nh datapath.NodeHandler) {
-				if err := nh.NodeValidateImplementation(entry.node); err != nil {
-					log.WithFields(logrus.Fields{
-						"handler": nh.Name(),
-						"node":    entry.node.Name,
-					}).WithError(err).
-						Error("Failed to apply node handler during background sync. Cilium may have degraded functionality. See error message for details.")
-					errs = errors.Join(errs, fmt.Errorf("failed while handling %s on node %s: %w", nh.Name(), entry.node.Name, err))
-				}
-			})
+			{
+				m.Iter(func(nh datapath.NodeHandler) {
+					if err := nh.NodeValidateImplementation(entry.node); err != nil {
+						log.WithFields(logrus.Fields{
+							"handler": nh.Name(),
+							"node":    entry.node.Name,
+						}).WithError(err).
+							Error("Failed to apply node handler during background sync. Cilium may have degraded functionality. See error message for details.")
+						errs = errors.Join(errs, fmt.Errorf("failed while handling %s on node %s: %w", nh.Name(), entry.node.Name, err))
+					}
+				})
+			}
 			entry.mutex.Unlock()
 
 			m.metrics.DatapathValidations.Inc()
@@ -833,8 +855,47 @@ func (m *manager) GetNodes() map[nodeTypes.Identity]nodeTypes.Node {
 	return nodes
 }
 
+// StartNodeNeighborLinkUpdater manages node neighbors links sync.
+// This provides a central location for all node neighbor link updates.
+// Under proper conditions, publisher enqueues the node which requires a link update.
+// This controller is agnostic of the condition under which the links must be established, thus
+// that responsibility lies on the publishers.
+// This controller also provides for module health to be reported in a single central location.
+func (m *manager) StartNodeNeighborLinkUpdater(nh datapath.NodeNeighbors) {
+	sc := cell.GetSubScope(m.healthScope, "neighbor-link-updater")
+	controller.NewManager().UpdateController(
+		"node-neighbor-link-updater",
+		controller.ControllerParams{
+			Group: neighborTableUpdateControllerGroup,
+			DoFunc: func(ctx context.Context) error {
+				if m.nodeNeighborQueue.isEmpty() {
+					return nil
+				}
+				var errs error
+				for {
+					e := m.nodeNeighborQueue.pop()
+					if e == nil || e.node == nil {
+						errs = errors.Join(errs, fmt.Errorf("invalid node spec found in queue: %#v", e))
+						break
+					}
+
+					log.Debugf("Refreshing node neighbor link for %s", e.node.Name)
+					hr := cell.GetHealthReporter(sc, e.node.Name)
+					if errs = errors.Join(errs, nh.NodeNeighborRefresh(ctx, *e.node, e.refresh)); errs != nil {
+						hr.Degraded("Failed node neighbor link update", errs)
+					} else {
+						hr.OK("Node neighbor link update successful")
+					}
+				}
+				return errs
+			},
+			RunInterval: defaultNodeUpdateInterval,
+		},
+	)
+}
+
 // StartNeighborRefresh spawns a controller which refreshes neighbor table
-// by sending arping periodically.
+// by forcing node neighbors refresh periodically based on the arping settings.
 func (m *manager) StartNeighborRefresh(nh datapath.NodeNeighbors) {
 	ctx, cancel := context.WithCancel(context.Background())
 	controller.NewManager().UpdateController(
@@ -854,14 +915,15 @@ func (m *manager) StartNeighborRefresh(nh datapath.NodeNeighbors) {
 					if entryNode.IsLocal() {
 						continue
 					}
-					go func(c context.Context, e nodeTypes.Node) {
+					go func(ctx context.Context, e *nodeTypes.Node) {
+						// TODO Should this be moved to dequeue instead?
 						// To avoid flooding network with arping requests
 						// at the same time, spread them over the
 						// [0; ARPPingRefreshPeriod/2) period.
 						n := randGen.Int63n(int64(m.conf.ARPPingRefreshPeriod / 2))
 						time.Sleep(time.Duration(n))
-						nh.NodeNeighborRefresh(c, e)
-					}(ctx, entryNode)
+						m.Enqueue(e, false)
+					}(ctx, &entryNode)
 				}
 				return nil
 			},

pkg/node/manager/queue_test.go
@@ -1,0 +1,65 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package manager
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQPush(t *testing.T) {
+	uu := map[string]struct {
+		items, e []string
+	}{
+		"empty": {},
+		"happy": {
+			items: []string{"a", "b", "c"},
+			e:     []string{"a", "b", "c"},
+		},
+	}
+
+	for k := range uu {
+		u := uu[k]
+		t.Run(k, func(t *testing.T) {
+			var q queue[string]
+
+			for i := range u.items {
+				q.push(&u.items[i])
+			}
+			for i := 0; i < len(u.items); i++ {
+				v := q.pop()
+				assert.Less(t, i, len(u.e))
+				assert.Equal(t, u.e[i], *v)
+			}
+			assert.True(t, q.isEmpty())
+		})
+	}
+}
+
+func TestQPop(t *testing.T) {
+	uu := map[string]struct {
+		items, e []string
+	}{
+		"empty": {},
+		"happy": {
+			items: []string{"a", "b", "c"},
+			e:     []string{"a", "b", "c"},
+		},
+	}
+
+	for k := range uu {
+		u := uu[k]
+		t.Run(k, func(t *testing.T) {
+			var q queue[string]
+
+			for i := range u.items {
+				q.push(&u.items[i])
+			}
+			for q.pop() != nil {
+			}
+			assert.True(t, q.isEmpty())
+		})
+	}
+}

daemon/cmd/daemon_main.go
@@ -1883,6 +1883,9 @@ func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *da
 		log.WithError(err).Warn("Failed to send agent start monitor message")
 	}
 
+	// Watches for node neighbors link updates.
+	d.nodeDiscovery.Manager.StartNodeNeighborLinkUpdater(d.datapath.NodeNeighbors())
+
 	if option.Config.DatapathMode != datapathOption.DatapathModeLBOnly {
 		if !d.datapath.NodeNeighbors().NodeNeighDiscoveryEnabled() {
 			// Remove all non-GC'ed neighbor entries that might have previously set

daemon/cmd/status.go
@@ -542,7 +542,7 @@ func (c *clusterNodesClient) NodeNeighDiscoveryEnabled() bool {
 	return false
 }
 
-func (c *clusterNodesClient) NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node) error {
+func (c *clusterNodesClient) NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node, refresh bool) error {
 	return nil
 }
 

pkg/datapath/cells.go
@@ -34,6 +34,7 @@ import (
 	"github.com/cilium/cilium/pkg/maps/nodemap"
 	monitorAgent "github.com/cilium/cilium/pkg/monitor/agent"
 	"github.com/cilium/cilium/pkg/mtu"
+	nodeManager "github.com/cilium/cilium/pkg/node/manager"
 	"github.com/cilium/cilium/pkg/option"
 	wg "github.com/cilium/cilium/pkg/wireguard/agent"
 	wgTypes "github.com/cilium/cilium/pkg/wireguard/types"
@@ -173,6 +174,7 @@ func newDatapath(params datapathParams) types.Datapath {
 		NodeAddressing: params.NodeAddressing,
 		BWManager:      params.BandwidthManager,
 		Loader:         params.Loader,
+		NodeManager:    params.NodeManager,
 	}, datapathConfig)
 
 	params.LC.Append(cell.Hook{
@@ -215,4 +217,6 @@ type datapathParams struct {
 	TunnelConfig tunnel.Config
 
 	Loader loaderTypes.Loader
+
+	NodeManager nodeManager.NodeManager
 }

pkg/datapath/fake/types/node.go
@@ -68,7 +68,7 @@ func (n *FakeNodeHandler) NodeNeighDiscoveryEnabled() bool {
 	return false
 }
 
-func (n *FakeNodeHandler) NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node) error {
+func (n *FakeNodeHandler) NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node, refresh bool) error {
 	return nil
 }
 

pkg/datapath/linux/datapath.go
@@ -8,6 +8,7 @@ import (
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
 	"github.com/cilium/cilium/pkg/maps/nodemap"
+	"github.com/cilium/cilium/pkg/node/manager"
 )
 
 // DatapathConfiguration is the static configuration of the datapath. The
@@ -41,6 +42,7 @@ type DatapathParams struct {
 	NodeAddressing datapath.NodeAddressing
 	MTU            datapath.MTUConfiguration
 	Loader         loader.Loader
+	NodeManager    manager.NodeManager
 }
 
 // NewDatapath creates a new Linux datapath
@@ -56,7 +58,7 @@ func NewDatapath(p DatapathParams, cfg DatapathConfiguration) datapath.Datapath 
 		bwmgr:           p.BWManager,
 	}
 
-	dp.node = NewNodeHandler(cfg, dp.nodeAddressing, p.NodeMap, p.MTU)
+	dp.node = NewNodeHandler(cfg, dp.nodeAddressing, p.NodeMap, p.MTU, p.NodeManager)
 	return dp
 }
 

pkg/datapath/linux/fuzz_test.go
@@ -23,7 +23,7 @@ func FuzzNodeHandler(f *testing.F) {
 		}
 		dpConfig := DatapathConfiguration{HostDevice: "veth0"}
 		fakeNodeAddressing := fakeTypes.NewNodeAddressing()
-		linuxNodeHandler := NewNodeHandler(dpConfig, fakeNodeAddressing, nil, &fakeTypes.MTU{})
+		linuxNodeHandler := NewNodeHandler(dpConfig, fakeNodeAddressing, nil, &fakeTypes.MTU{}, new(mockEnqueuer))
 		if linuxNodeHandler == nil {
 			panic("Should not be nil")
 		}
@@ -31,8 +31,20 @@ func FuzzNodeHandler(f *testing.F) {
 		if err != nil {
 			t.Skip()
 		}
-		linuxNodeHandler.NodeNeighborRefresh(context.Background(), nodev1)
+		linuxNodeHandler.NodeNeighborRefresh(context.Background(), nodev1, true)
 		linuxNodeHandler.NodeDelete(nodev1)
-		linuxNodeHandler.NodeNeighborRefresh(context.Background(), nodev1)
+		linuxNodeHandler.NodeNeighborRefresh(context.Background(), nodev1, true)
 	})
+}
+
+type mockEnqueuer struct {
+	nh *linuxNodeHandler
+}
+
+func (q *mockEnqueuer) Enqueue(n *nodeTypes.Node, refresh bool) {
+	if q.nh != nil {
+		if err := q.nh.insertNeighbor(context.Background(), n, refresh); err != nil {
+			log.Errorf("MockQ NodeNeighborRefresh failed: %s", err)
+		}
+	}
 }

commit 83b5a20fc693bbd665ca09f11627c6b8a711a94d

l2-discovery: fix health reporting for link updater
As-is, when l2 neighbor discovery is enabled, the
node-neighbor-link-updater controller fails with "invalid node spec
found in queue". This is due to a bug in the controller's DoFunc, where
an empty list is treated the same as an invalid queue entry.

When this controller fails, `cilium status` reports errors for all nodes
in the cluster similar to the following:

```
cilium             cilium-mgstt    controller node-neighbor-link-updater is failing since 21s (49x): invalid node spec found in queue: (*manager.nodeQueueEntry)(nil)
```

To differentiate between an empty queue and a nil item, the queue's
`pop` method now also returns a bool to indicate whether an element was
successfully retrieved from the queue.

Fixes: 8d525fe39113 ("Add node neighbor link updater controller")

Signed-off-by: Tim Horner <timothy.horner@isovalent.com>

pkg/node/manager/manager.go
@@ -899,13 +899,15 @@ func (m *manager) StartNodeNeighborLinkUpdater(nh datapath.NodeNeighbors) {
 		controller.ControllerParams{
 			Group: neighborTableUpdateControllerGroup,
 			DoFunc: func(ctx context.Context) error {
+				var errs error
 				if m.nodeNeighborQueue.isEmpty() {
 					return nil
 				}
-				var errs error
 				for {
-					e := m.nodeNeighborQueue.pop()
-					if e == nil || e.node == nil {
+					e, ok := m.nodeNeighborQueue.pop()
+					if !ok {
+						break
+					} else if e == nil || e.node == nil {
 						errs = errors.Join(errs, fmt.Errorf("invalid node spec found in queue: %#v", e))
 						break
 					}

pkg/node/manager/queue.go
@@ -24,15 +24,15 @@ func (q *queue[T]) isEmpty() bool {
 	return len(q.items) == 0
 }
 
-func (q *queue[T]) pop() *T {
-	if q.isEmpty() {
-		return nil
-	}
-
+func (q *queue[T]) pop() (*T, bool) {
 	q.mx.Lock()
 	defer q.mx.Unlock()
+
+	if len(q.items) == 0 {
+		return nil, false
+	}
 	t := q.items[0]
 	q.items = q.items[1:]
 
-	return t
+	return t, true
 }

pkg/node/manager/queue_test.go
@@ -29,11 +29,16 @@ func TestQPush(t *testing.T) {
 				q.push(&u.items[i])
 			}
 			for i := 0; i < len(u.items); i++ {
-				v := q.pop()
+				v, ok := q.pop()
+				assert.True(t, ok)
 				assert.Less(t, i, len(u.e))
 				assert.Equal(t, u.e[i], *v)
 			}
 			assert.True(t, q.isEmpty())
+
+			v, ok := q.pop()
+			assert.False(t, ok)
+			assert.Nil(t, v)
 		})
 	}
 }
@@ -57,7 +62,10 @@ func TestQPop(t *testing.T) {
 			for i := range u.items {
 				q.push(&u.items[i])
 			}
-			for q.pop() != nil {
+			for {
+				if _, ok := q.pop(); !ok {
+					break
+				}
 			}
 			assert.True(t, q.isEmpty())
 		})