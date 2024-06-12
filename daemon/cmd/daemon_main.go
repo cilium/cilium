8a3a3c30fd5c77df4be1309cab58a614ccfbeabc
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-06-12 21:16:06 +0200

    prefilter: move api handler from daemon to prefilter hive cell
    Currently, the daemon implements the Prefilter API handler. With it
    comes the last dependency from the daemon to the prefilter itself.
    
    Therefore, this commit moves the prefilter API handler from the daemon
    to the existing prefilter hive cell and removes the dependency from the
    daemon to the prefilter.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -1681,7 +1681,6 @@ type daemonParams struct {
 	SyncHostIPs         *syncHostIPs
 	LRPManager          *redirectpolicy.Manager
 	NodeDiscovery       *nodediscovery.NodeDiscovery
-	Prefilter           datapath.PreFilter
 	CompilationLock     datapath.CompilationLock
 	MetalLBBgpSpeaker   speaker.MetalLBBgpSpeaker
 	CGroupManager       cgroup.CGroupManager
