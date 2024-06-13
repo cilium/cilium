f27470f1a22cfff922f9237e80ea48fceab39dd3
Dylan Reimerink <dylan.reimerink@isovalent.com>
2024-06-13 16:39:38 +0200

    lbipam: Make `AllowFirstLastIP` default to `yes`
    In v1.15, we introduced this setting and defaulted to `no` for
    `AllowFirstLastIP` to preserve the previous behavior. However, in most
    cases its preferable to default to `yes`. Users should explicitly set
    this field to their desired value if they don't want the behavior of
    pools to change.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- operator/pkg/lbipam/lbipam.go
+++ operator/pkg/lbipam/lbipam.go
@@ -1304,10 +1304,8 @@ func (ipam *LBIPAM) handleNewPool(ctx context.Context, pool *cilium_api_v2alpha1
 			return fmt.Errorf("Error making LB Range for '%s': %w", ipBlock.Cidr, err)
 		}
 
-		// If AllowFirstLastIPs is no or unspecified, mark the first and last IP as allocated upon range creation.
-		if fromCidr && pool.Spec.AllowFirstLastIPs != cilium_api_v2alpha1.AllowFirstLastIPYes {
-			// TODO: in 1.16 switch from default no to default yes.
-			// https://github.com/cilium/cilium/issues/28591
+		// If AllowFirstLastIPs is no, mark the first and last IP as allocated upon range creation.
+		if fromCidr && pool.Spec.AllowFirstLastIPs == cilium_api_v2alpha1.AllowFirstLastIPNo {
 			from, to := lbRange.alloc.Range()
 
 			// If the first and last IPs are the same or adjacent, we would reserve the entire range.
@@ -1357,8 +1355,8 @@ func ipRangeFromBlock(block cilium_api_v2alpha1.CiliumLoadBalancerIPPoolIPBlock)
 func (ipam *LBIPAM) handlePoolModified(ctx context.Context, pool *cilium_api_v2alpha1.CiliumLoadBalancerIPPool) error {
 	changedAllowFirstLastIPs := false
 	if existingPool, ok := ipam.pools[pool.GetName()]; ok {
-		changedAllowFirstLastIPs = (existingPool.Spec.AllowFirstLastIPs == cilium_api_v2alpha1.AllowFirstLastIPYes) !=
-			(pool.Spec.AllowFirstLastIPs == cilium_api_v2alpha1.AllowFirstLastIPYes)
+		changedAllowFirstLastIPs = (existingPool.Spec.AllowFirstLastIPs == cilium_api_v2alpha1.AllowFirstLastIPNo) !=
+			(pool.Spec.AllowFirstLastIPs == cilium_api_v2alpha1.AllowFirstLastIPNo)
 	}
 
 	ipam.pools[pool.GetName()] = pool
@@ -1400,9 +1398,7 @@ func (ipam *LBIPAM) handlePoolModified(ctx context.Context, pool *cilium_api_v2a
 		if found {
 			// If the AllowFirstLastIPs state changed
 			if fromCidr && changedAllowFirstLastIPs {
-				// TODO in 1.16 switch from default no to default yes.
-				// https://github.com/cilium/cilium/issues/28591
-				if pool.Spec.AllowFirstLastIPs == cilium_api_v2alpha1.AllowFirstLastIPYes {
+				if pool.Spec.AllowFirstLastIPs != cilium_api_v2alpha1.AllowFirstLastIPNo {
 					// If we are allowing first and last IPs again, free them for allocation
 					from, to := extRange.alloc.Range()
 
@@ -1454,10 +1450,8 @@ func (ipam *LBIPAM) handlePoolModified(ctx context.Context, pool *cilium_api_v2a
 			return fmt.Errorf("Error while making new LB range for range '%s - %s': %w", newRange.from, newRange.to, err)
 		}
 
-		// If AllowFirstLastIPs is no or default, mark the first and last IP as allocated upon range creation.
-		if newRange.fromCidr && pool.Spec.AllowFirstLastIPs != cilium_api_v2alpha1.AllowFirstLastIPYes {
-			// TODO: in 1.16 switch from default no to default yes.
-			// https://github.com/cilium/cilium/issues/28591
+		// If AllowFirstLastIPs is no, mark the first and last IP as allocated upon range creation.
+		if newRange.fromCidr && pool.Spec.AllowFirstLastIPs == cilium_api_v2alpha1.AllowFirstLastIPNo {
 			from, to := newLBRange.alloc.Range()
 
 			// If the first and last IPs are the same or adjacent, we would reserve the entire range.
