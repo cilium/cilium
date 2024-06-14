791ff1a0200512b73f7941cce9a5f30937fb8a67
Jarno Rajahalme <jarno@isovalent.com>
2024-06-14 11:27:14 +0200

    resources: Wait for CRDs before listing Cilium resources
    Wait for CRD sync before trying to list Cilium resources. This eliminates
    error and warning logs from vendored k8s code that happen when an unknown
    resource is listed or watched.
    
    CRDSync promise is optional. This avoids hanging any hives that do not
    provide this promise.
    
    Add a once-a-minute info log on the remaining CRDs not yet
    registered. This may help debug cases when we are waiting for CRDs that
    are not going to be registered at all.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- clustermesh-apiserver/clustermesh/k8s/resource_ctors.go
+++ clustermesh-apiserver/clustermesh/k8s/resource_ctors.go
@@ -4,43 +4,41 @@
 package k8s
 
 import (
-	"github.com/cilium/hive/cell"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	"github.com/cilium/cilium/pkg/k8s"
 	cilium_api_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
-	"github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/resource"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/k8s/utils"
 )
 
-func CiliumSlimEndpointResource(lc cell.Lifecycle, cs client.Clientset, opts ...func(*metav1.ListOptions)) (resource.Resource[*types.CiliumEndpoint], error) {
-	if !cs.IsEnabled() {
+func CiliumSlimEndpointResource(params k8s.CiliumResourceParams, opts ...func(*metav1.ListOptions)) (resource.Resource[*types.CiliumEndpoint], error) {
+	if !params.ClientSet.IsEnabled() {
 		return nil, nil
 	}
 	lw := utils.ListerWatcherWithModifiers(
-		utils.ListerWatcherFromTyped[*cilium_api_v2.CiliumEndpointList](cs.CiliumV2().CiliumEndpoints(slim_corev1.NamespaceAll)),
+		utils.ListerWatcherFromTyped[*cilium_api_v2.CiliumEndpointList](params.ClientSet.CiliumV2().CiliumEndpoints(slim_corev1.NamespaceAll)),
 		opts...,
 	)
-	return resource.New[*types.CiliumEndpoint](lc, lw,
+	return resource.New[*types.CiliumEndpoint](params.Lifecycle, lw,
 		resource.WithLazyTransform(func() runtime.Object {
 			return &cilium_api_v2.CiliumEndpoint{}
-		}, k8s.TransformToCiliumEndpoint),
+		}, k8s.TransformToCiliumEndpoint), resource.WithCRDSync(params.CRDSyncPromise),
 	), nil
 }
 
-func CiliumNodeResource(lc cell.Lifecycle, cs client.Clientset, opts ...func(*metav1.ListOptions)) (resource.Resource[*cilium_api_v2.CiliumNode], error) {
-	if !cs.IsEnabled() {
+func CiliumNodeResource(params k8s.CiliumResourceParams, opts ...func(*metav1.ListOptions)) (resource.Resource[*cilium_api_v2.CiliumNode], error) {
+	if !params.ClientSet.IsEnabled() {
 		return nil, nil
 	}
 	lw := utils.ListerWatcherWithModifiers(
-		utils.ListerWatcherFromTyped[*cilium_api_v2.CiliumNodeList](cs.CiliumV2().CiliumNodes()),
+		utils.ListerWatcherFromTyped[*cilium_api_v2.CiliumNodeList](params.ClientSet.CiliumV2().CiliumNodes()),
 		opts...,
 	)
-	return resource.New[*cilium_api_v2.CiliumNode](lc, lw,
-		resource.WithMetric("CiliumNode"),
+	return resource.New[*cilium_api_v2.CiliumNode](params.Lifecycle, lw,
+		resource.WithMetric("CiliumNode"), resource.WithCRDSync(params.CRDSyncPromise),
 	), nil
 }
