cilium/cilium

commit cb6a58bef00b40084392774de0a6bc0602c6daec

clustermesh: granular etcd permissions for kvstoremesh cached data
Currently, the same etcd user (i.e., remote) is granted permissions to
read the whole content of the clustermesh-apiserver's sidecar etcd
instance, including also the data cached by kvstoremesh, when enabled.
In an effort to harden the overall clustermesh posture, let's introduce
a separate and dedicated user for local access, to ensure that remote
clusters cannot access cached data, as it may include information that
they would not normally have access to.

Specifically, the remote user is intended to have access only to the
information regarding the local cluster, while the local user can
access cached data about remote clusters only. Still, for backward
compatibility purposes, the remote user still retains access to cached
data as well in this release. The reason being that there would
otherwise be a time window upon upgrade in which Cilium Agents would
lose access to the kvstoremesh data (especially in large clusters).
Indeed, the new certificate would be mounted by the agents only upon
rollout, but the configuration would be immediately reloaded (thus
targeting the new, not yet mounted, certificate), hence breaking the
access to the information cached by kvstoremesh.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/kvstore/etcdinit/init.go
@@ -6,6 +6,7 @@ package init
 import (
 	"context"
 	"fmt"
+	"path"
 	"strings"
 
 	"github.com/sirupsen/logrus"
@@ -62,7 +63,7 @@ func ClusterMeshEtcdInit(ctx context.Context, log *logrus.Entry, client *clientv
 	}
 
 	// Admin user
-	adminUsername := adminUsernameForClusterName(ciliumClusterName)
+	adminUsername := usernameForClusterName("admin", ciliumClusterName)
 	log.WithField("etcdUsername", adminUsername).
 		Info("Configuring admin user")
 	err = ic.addNoPasswordUser(ctx, adminUsername)
@@ -104,7 +105,31 @@ func ClusterMeshEtcdInit(ctx context.Context, log *logrus.Entry, client *clientv
 		return err
 	}
 
-	// Remote user
+	// Local user (i.e., local agents accessing information cached by KVStoreMesh)
+	localUsername := usernameForClusterName("local", ciliumClusterName)
+	log.WithField("etcdUsername", localUsername).
+		Info("Configuring local user")
+	localRolename := rolename("local")
+	err = ic.addNoPasswordUser(ctx, localUsername)
+	if err != nil {
+		return err
+	}
+	err = ic.addRole(ctx, localRolename)
+	if err != nil {
+		return err
+	}
+	err = ic.grantRoleToUser(ctx, localRolename, localUsername)
+	if err != nil {
+		return err
+	}
+	for _, keyRange := range rangesForLocalRole() {
+		err = ic.grantPermissionToRole(ctx, readOnly, keyRange, localRolename)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Remote user (i.e., remote clusters accessing state information)
 	remoteUsername := username("remote")
 	log.WithField("etcdUsername", remoteUsername).
 		Info("Configuring remote user")
@@ -121,9 +146,11 @@ func ClusterMeshEtcdInit(ctx context.Context, log *logrus.Entry, client *clientv
 	if err != nil {
 		return err
 	}
-	err = ic.grantPermissionToRole(ctx, readOnly, allKeysRange, remoteRolename)
-	if err != nil {
-		return err
+	for _, keyRange := range rangesForRemoteRole(ciliumClusterName) {
+		err = ic.grantPermissionToRole(ctx, readOnly, keyRange, remoteRolename)
+		if err != nil {
+			return err
+		}
 	}
 
 	// Post setup
@@ -136,13 +163,13 @@ func ClusterMeshEtcdInit(ctx context.Context, log *logrus.Entry, client *clientv
 	return nil
 }
 
-// adminUsernameForClusterName generates the admin account username for a given clusterName. This handles the edge case
+// usernameForClusterName generates the account username for a given clusterName. This handles the edge case
 // where the clusterName is blank, ensuring we don't have a username with a trailing hyphen.
-func adminUsernameForClusterName(clusterName string) username {
+func usernameForClusterName(base, clusterName string) username {
 	if clusterName == "" {
-		return "admin"
+		return username(base)
 	}
-	return username(fmt.Sprintf("admin-%s", clusterName))
+	return username(fmt.Sprintf("%s-%s", base, clusterName))
 }
 
 // initClient is a thin wrapper around the etcd client library that provides functions with more useful error messages,
@@ -226,6 +253,11 @@ type keyRange struct {
 	end   string
 }
 
+// rangeForKey generates a keyRange for a given key. Differently from rangeForPrefix, it does not enforce a trailing /
+func rangeForKey(key string) keyRange {
+	return keyRange{key, clientv3.GetPrefixRangeEnd(key)}
+}
+
 // rangeForPrefix generates a keyRange for a given prefix. This is a wrapper around the client's GetPrefixRangeEnd
 // function.
 func rangeForPrefix(prefix string) keyRange {
@@ -235,10 +267,7 @@ func rangeForPrefix(prefix string) keyRange {
 	if !strings.HasSuffix(prefix, "/") {
 		prefix += "/"
 	}
-	return keyRange{
-		prefix,
-		clientv3.GetPrefixRangeEnd(prefix),
-	}
+	return rangeForKey(prefix)
 }
 
 // allKeysRange is the range over all keys in etcd. Granting permissions on this range is the same as granting global
@@ -283,4 +312,31 @@ func (ic initClient) enableAuth(ctx context.Context) error {
 		return fmt.Errorf("enabling authentication on etcd: %w", err)
 	}
 	return nil
+}
+
+// rangesForLocalRole returns the set of etcd key ranges allowed to be accessed by the local user.
+func rangesForLocalRole() []keyRange {
+	return []keyRange{
+		rangeForKey(kvstore.HeartbeatPath),
+		rangeForKey(kvstore.HasClusterConfigPath),
+		rangeForPrefix(kvstore.CachePrefix),
+		rangeForPrefix(kvstore.ClusterConfigPrefix),
+		rangeForPrefix(kvstore.SyncedPrefix),
+	}
+}
+
+// rangesForLocalUser returns the set of etcd key ranges allowed to be accessed by the remote user.
+func rangesForRemoteRole(clusterName string) []keyRange {
+	return []keyRange{
+		rangeForKey(kvstore.HeartbeatPath),
+		rangeForKey(kvstore.HasClusterConfigPath),
+		rangeForPrefix(kvstore.StatePrefix),
+		rangeForKey(path.Join(kvstore.ClusterConfigPrefix, clusterName)),
+		rangeForPrefix(path.Join(kvstore.SyncedPrefix, clusterName)),
+
+		// kvstoremesh-specific prefixes still allowed for backward compatibility
+		rangeForPrefix(kvstore.CachePrefix),
+		rangeForPrefix(kvstore.ClusterConfigPrefix),
+		rangeForPrefix(kvstore.SyncedPrefix),
+	}
 }

pkg/kvstore/kvstore.go
@@ -25,6 +25,13 @@ const (
 	// BaseKeyPrefix is the base prefix that should be used for all keys
 	BaseKeyPrefix = "cilium"
 
+	// StatePrefix is the kvstore prefix used to store the Cilium's state.
+	StatePrefix = BaseKeyPrefix + "/state"
+
+	// CachePrefix is the kvstore prefix used to store the information retrieved
+	// from a remote cluster and cached locally by KVStoreMesh.
+	CachePrefix = BaseKeyPrefix + "/cache"
+
 	// InitLockPath is the path to the init lock to test quorum
 	InitLockPath = BaseKeyPrefix + "/.initlock"
 
@@ -59,8 +66,8 @@ const (
 // (holding the cilium state) to the corresponding one holding cached information
 // from another kvstore (that is, "cilium/cache").
 func StateToCachePrefix(prefix string) string {
-	if strings.HasPrefix(prefix, "cilium/state") {
-		return strings.Replace(prefix, "cilium/state", "cilium/cache", 1)
+	if strings.HasPrefix(prefix, StatePrefix) {
+		return strings.Replace(prefix, StatePrefix, CachePrefix, 1)
 	}
 	return prefix
 }

commit 8b66954771205ae846e803b9152608939550e48c

kvstore: correctly assign permissions to single key, rather than prefix
Fix the recently introduced rangeForKey function leveraged by the
etcdinit logic to only grant permissions to the given key, and not
the full prefix starting with that key, when appropriate.

Fixes: cb6a58bef00b ("clustermesh: granular etcd permissions for kvstoremesh cached data")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/kvstore/etcdinit/init.go
@@ -253,9 +253,9 @@ type keyRange struct {
 	end   string
 }
 
-// rangeForKey generates a keyRange for a given key. Differently from rangeForPrefix, it does not enforce a trailing /
+// rangeForKey generates a keyRange for a single key.
 func rangeForKey(key string) keyRange {
-	return keyRange{key, clientv3.GetPrefixRangeEnd(key)}
+	return keyRange{key, ""}
 }
 
 // rangeForPrefix generates a keyRange for a given prefix. This is a wrapper around the client's GetPrefixRangeEnd
@@ -267,7 +267,7 @@ func rangeForPrefix(prefix string) keyRange {
 	if !strings.HasSuffix(prefix, "/") {
 		prefix += "/"
 	}
-	return rangeForKey(prefix)
+	return keyRange{prefix, clientv3.GetPrefixRangeEnd(prefix)}
 }
 
 // allKeysRange is the range over all keys in etcd. Granting permissions on this range is the same as granting global