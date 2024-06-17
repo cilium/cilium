60976e2ce5f79fbe30e48648ae70946e20d3e5a0
Ovidiu Tirla <otirla@google.com>
2024-06-17 11:40:38 +0000

    pkg/ciliumidentity: Add CID OP Pod listener
    Add the pod event listener and handler to enqueue pod reconciliation
    
    Signed-off-by: Ovidiu Tirla <otirla@google.com>

--- operator/pkg/ciliumidentity/pod.go
+++ operator/pkg/ciliumidentity/pod.go
@@ -3,7 +3,12 @@
 
 package ciliumidentity
 
-import "github.com/cilium/cilium/pkg/k8s/resource"
+import (
+	"context"
+
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
 
 func podResourceKey(podName, podNamespace string) resource.Key {
 	return resource.Key{Name: podName, Namespace: podNamespace}
@@ -15,4 +20,15 @@ type PodItem struct {
 
 func (p PodItem) Key() resource.Key {
 	return p.key
+}
+
+func (c *Controller) processPodEvents(ctx context.Context) error {
+	for event := range c.pod.Events(ctx) {
+		if event.Kind == resource.Upsert || event.Kind == resource.Delete {
+			c.logger.Debug("Got Pod event", logfields.Type, event.Kind, logfields.K8sPodName, event.Key.String())
+			c.enqueueReconciliation(PodItem{podResourceKey(event.Object.Name, event.Object.Namespace)}, 0)
+		}
+		event.Done(nil)
+	}
+	return nil
 }
