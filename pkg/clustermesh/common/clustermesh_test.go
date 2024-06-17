d8a5fe6334f1212a019958ae600037b087bbaff0
Marco Iorio <marco.iorio@isovalent.com>
2024-06-17 18:51:18 +0200

    clustermesh: make cluster disconnection asynchronous
    Currently, both cluster configuration upsertions and removals are
    handled synchronously. However, while upsertions are negligible as
    they only trigger the restart of a controller, removals can take
    a significant amount of time, possibly blocking any other parallel
    operations. This applies both to the agents logic, due to the
    draining of all previously known entries, and to kvstoremesh, as
    the subsequent commits will implement the removal of the cached
    data upon disconnection.
    
    Let's introduce the support for processing the configurations of
    separate clusters in parallel by running the removal logic
    asynchronously. Still, we need to ensure that events for the same
    cluster continue to be processed in order, to prevent issues in
    case the configuration of a given cluster is first removed and
    then subsequently added back again while the removal is still
    in progress. In this case, we delay the addition event and replay
    it only once the removal terminated.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

---
+++ pkg/clustermesh/common/clustermesh_test.go
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package common
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/cilium/hive/hivetest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/cilium/cilium/pkg/clustermesh/types"
+	"github.com/cilium/cilium/pkg/clustermesh/utils"
+	"github.com/cilium/cilium/pkg/kvstore"
+	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/testutils"
+)
+
+func TestClusterMeshMultipleAddRemove(t *testing.T) {
+	testutils.IntegrationTest(t)
+	kvstore.SetupDummy(t, "etcd")
+
+	baseDir := t.TempDir()
+	path := func(name string) string { return filepath.Join(baseDir, name) }
+
+	for i, cluster := range []string{"cluster1", "cluster2", "cluster3"} {
+		writeFile(t, path(cluster), fmt.Sprintf("endpoints:\n- %s\n", kvstore.EtcdDummyAddress()))
+		cfg := types.CiliumClusterConfig{ID: uint32(i + 1)}
+		require.NoError(t, utils.SetClusterConfig(context.Background(), cluster, cfg, kvstore.Client()))
+	}
+
+	var ready lock.Map[string, bool]
+	isReady := func(name string) bool {
+		rdy, _ := ready.Load(name)
+		return rdy
+	}
+
+	var blockRemoval lock.Map[string, chan struct{}]
+	blockRemoval.Store("cluster1", make(chan struct{}))
+	blockRemoval.Store("cluster2", make(chan struct{}))
+	blockRemoval.Store("cluster3", make(chan struct{}))
+
+	gcm := NewClusterMesh(Configuration{
+		Config:      Config{ClusterMeshConfig: baseDir},
+		ClusterInfo: types.ClusterInfo{ID: 255, Name: "local"},
+		NewRemoteCluster: func(name string, _ StatusFunc) RemoteCluster {
+			return &fakeRemoteCluster{
+				onRun: func() { ready.Store(name, true) },
+				onRemove: func() {
+					wait, _ := blockRemoval.Load(name)
+					<-wait
+				},
+			}
+		},
+		Metrics: MetricsProvider("clustermesh")(),
+	})
+	hivetest.Lifecycle(t).Append(gcm)
+	cm := gcm.(*clusterMesh)
+
+	// Directly call the add/remove methods, rather than creating/removing the
+	// files to prevent race conditions due to the interplay with the watcher.
+	cm.add("cluster1", path("cluster1"))
+	require.EventuallyWithT(t, func(c *assert.CollectT) { assert.True(c, isReady("cluster1")) }, timeout, tick, "Cluster1 is not ready")
+
+	// A blocked cluster removal operation should not block parallel cluster additions
+	cm.remove("cluster1")
+
+	cm.add("cluster2", path("cluster2"))
+	cm.add("cluster3", path("cluster3"))
+
+	require.EventuallyWithT(t, func(c *assert.CollectT) { assert.True(c, isReady("cluster2")) }, timeout, tick, "Cluster2 is not ready")
+	require.EventuallyWithT(t, func(c *assert.CollectT) { assert.True(c, isReady("cluster3")) }, timeout, tick, "Cluster3 is not ready")
+
+	// Unblock the cluster removal
+	block, _ := blockRemoval.Load("cluster1")
+	close(block)
+
+	// Multiple removals and additions, ending with an addition should lead to a ready cluster
+	ready.Store("cluster2", false)
+	cm.remove("cluster2")
+	cm.add("cluster2", path("cluster2"))
+	cm.remove("cluster2")
+	cm.add("cluster2", path("cluster2"))
+
+	require.False(t, isReady("cluster2"), "Cluster2 is ready, although it shouldn't")
+
+	// Unblock the cluster removal
+	block, _ = blockRemoval.Load("cluster2")
+	close(block)
+
+	require.EventuallyWithT(t, func(c *assert.CollectT) { assert.True(c, isReady("cluster2")) }, timeout, tick, "Cluster2 is not ready")
+
+	// Multiple removals and additions, ending with a removal should lead to a non-ready cluster
+	ready.Store("cluster3", false)
+	cm.remove("cluster3")
+	cm.add("cluster3", path("cluster3"))
+	cm.remove("cluster3")
+	cm.add("cluster3", path("cluster3"))
+	cm.remove("cluster3")
+
+	require.False(t, isReady("cluster3"), "Cluster3 is ready, although it shouldn't")
+
+	// Unblock the cluster removal
+	block, _ = blockRemoval.Load("cluster3")
+	close(block)
+
+	// Make sure that the deletion go routine terminated before checking
+	cm.wg.Wait()
+	require.False(t, isReady("cluster3"), "Cluster3 is ready, although it shouldn't")
+}
