68c4b51b145ab0ec559e3547ecdc4eaeb910a546
Julian Wiedmann <jwi@isovalent.com>
2024-06-17 20:41:09 +0300

    datapath: clean up unused SECLABEL_NB
    Looks like the last user was removed with
    32a921aab817 ("bpf: Remove flowlabel optimization for identity").
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- pkg/datapath/linux/config/config.go
+++ pkg/datapath/linux/config/config.go
@@ -1027,7 +1027,6 @@ func (h *HeaderfileWriter) writeStaticData(devices []string, fw io.Writer, e dat
 	fmt.Fprint(fw, defineUint32("SECLABEL", secID))
 	fmt.Fprint(fw, defineUint32("SECLABEL_IPV4", secID))
 	fmt.Fprint(fw, defineUint32("SECLABEL_IPV6", secID))
-	fmt.Fprint(fw, defineUint32("SECLABEL_NB", byteorder.HostToNetwork32(secID)))
 	fmt.Fprint(fw, defineUint32("POLICY_VERDICT_LOG_FILTER", e.GetPolicyVerdictLogFilter()))
 
 	epID := uint16(e.GetID())
