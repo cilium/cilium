cilium/cilium

commit d11e4d2612793287ef1c0213c3430c8b58ee24d0

proxy: Reuse proxy ports from datapath on restart
Previously we only reused the DNS proxy port from the datapath. Change to
reuse the old proxy ports for all proxy redirects from datapath, if
possible. This reduces Listener churn on daemonset Envoy on agent
restart.

With this change the listener update in Envoy logs after agent restart
looks like this:

  begin add/update listener: name=cilium-http-egress:13563 hash=11560876577076369351
  duplicate/locked listener 'cilium-http-egress:13563'. no add/update
  lds: add/update listener 'cilium-http-egress:13563' skipped

Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

daemon/cmd/fqdn.go
@@ -92,20 +92,15 @@ func (d *Daemon) bootstrapFQDN(possibleEndpoints map[uint16]*endpoint.Endpoint, 
 		return nil
 	}
 
-	// Once we stop returning errors from StartDNSProxy this should live in
-	// StartProxySupport
-	port, err := d.l7Proxy.GetProxyPort(proxytypes.DNSProxyName)
-	if err != nil {
-		return err
-	}
-	if option.Config.ToFQDNsProxyPort != 0 {
-		port = uint16(option.Config.ToFQDNsProxyPort)
-	} else if port == 0 {
-		// Try locate old DNS proxy port number from the datapath, and reuse it if it's not open
-		oldPort := d.datapath.GetProxyPort(proxytypes.DNSProxyName)
-		openLocalPorts := proxy.OpenLocalPorts()
-		if _, alreadyOpen := openLocalPorts[oldPort]; !alreadyOpen {
-			port = oldPort
+	// A configured proxy port takes precedence over using the previous port.
+	port := uint16(option.Config.ToFQDNsProxyPort)
+	if port == 0 {
+		// Try reuse previous DNS proxy port number
+		if oldPort, err := d.l7Proxy.GetProxyPort(proxytypes.DNSProxyName); err == nil {
+			openLocalPorts := proxy.OpenLocalPorts()
+			if _, alreadyOpen := openLocalPorts[oldPort]; !alreadyOpen {
+				port = oldPort
+			}
 		}
 	}
 	if err := re.InitRegexCompileLRU(option.Config.FQDNRegexCompileLRUSize); err != nil {

pkg/proxy/proxy.go
@@ -44,6 +44,7 @@ const (
 type DatapathUpdater interface {
 	InstallProxyRules(proxyPort uint16, name string)
 	SupportsOriginalSourceAddr() bool
+	GetProxyPorts() map[string]uint16
 }
 
 type ProxyPort struct {
@@ -67,7 +68,7 @@ type ProxyPort struct {
 	configured bool
 	// rulesPort contains the proxy port value configured to the datapath rules and
 	// is non-zero when a proxy has been successfully created and the
-	// datapath rules have been created.
+	// (new, if after restart) datapath rules have been created.
 	rulesPort uint16
 }
 
@@ -307,6 +308,29 @@ func proxyNotFoundError(name string) error {
 }
 
 // Exported API
+
+// RestoreProxyPorts tries to find earlier port numbers from datapath and use them
+// as defaults for proxy ports
+func (p *Proxy) RestoreProxyPorts() {
+	portMap := p.datapathUpdater.GetProxyPorts()
+	for name, port := range portMap {
+		pp := p.proxyPorts[name]
+		if pp != nil {
+			pp.proxyPort = port
+		} else {
+			// Only CRD type proxy ports can be dynamically allocated. Assume a port
+			// from datapath with an unknown name was for a dynamically allocated CRD
+			// proxy and pre-allocate a proxy port for it.
+			// CRD proxy ports always have 'ingress' as 'false'.
+			p.proxyPorts[name] = &ProxyPort{proxyType: types.ProxyTypeCRD, ingress: false, proxyPort: port}
+		}
+		p.allocatedPorts[port] = false
+		log.
+			WithField(fieldProxyRedirectID, name).
+			WithField("proxyPort", port).
+			Debugf("RestoreProxyPorts: preallocated proxy port")
+	}
+}
 
 // GetProxyPort() returns the fixed listen port for a proxy, if any.
 func (p *Proxy) GetProxyPort(name string) (uint16, error) {
@@ -579,12 +603,17 @@ func (p *Proxy) createNewRedirect(
 	}
 
 	for nRetry := 0; nRetry < redirectCreationAttempts; nRetry++ {
-		if !pp.configured {
-			if nRetry > 0 {
-				// Clear the proxy port on retry so that a random new port will be
-				// tried.
-				pp.proxyPort = 0
-			}
+		// Reallocate port only if not yet configured and the first try failed, or the port
+		// has not been (pre)allocated yet.
+		// For example, on restart we may have preallocated port that is not configured
+		// yet. The port may already be listening, causing allocatePort() to select another
+		// one, as the port is not available. We should make the first try with the
+		// preallocated port, as in typical case the listener we are about to configure
+		// already exists (daemonset proxy), or can be created on a new proxy with the same
+		// port (embedded Envoy).
+		if !pp.configured && (nRetry > 0 || pp.proxyPort == 0) {
+			// Clear the proxy port on retry so that a random new port will be tried.
+			pp.proxyPort = 0
 
 			// Check if pp.proxyPort is available and find another available proxy port if not.
 			proxyPort, err := p.allocatePort(pp.proxyPort, p.rangeMin, p.rangeMax)

pkg/proxy/proxy_test.go
@@ -27,6 +27,10 @@ func (m *MockDatapathUpdater) SupportsOriginalSourceAddr() bool {
 	return true
 }
 
+func (m *MockDatapathUpdater) GetProxyPorts() map[string]uint16 {
+	return nil
+}
+
 func TestPortAllocator(t *testing.T) {
 	mockDatapathUpdater := &MockDatapathUpdater{}
 

daemon/cmd/daemon.go
@@ -586,6 +586,12 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 	if err != nil {
 		log.WithError(err).Error("Unable to read existing endpoints")
 	}
+	// Restore all proxy ports from datapath, if possible
+	// Must be run before d.bootstrapFQDN(), which depends
+	// on the ports having been restored.
+	if d.l7Proxy != nil {
+		d.l7Proxy.RestoreProxyPorts()
+	}
 	bootstrapStats.restore.End(true)
 
 	bootstrapStats.fqdn.Start()

pkg/datapath/fake/types/datapath.go
@@ -88,8 +88,8 @@ func (f *FakeDatapath) SupportsOriginalSourceAddr() bool {
 	return false
 }
 
-func (m *FakeDatapath) GetProxyPort(name string) uint16 {
-	return 0
+func (m *FakeDatapath) GetProxyPorts() map[string]uint16 {
+	return nil
 }
 
 func (m *FakeDatapath) InstallNoTrackRules(ip netip.Addr, port uint16) {

pkg/datapath/iptables/iptables.go
@@ -1002,46 +1002,45 @@ func (m *Manager) doInstallProxyRules(proxyPort uint16, name string) error {
 	return nil
 }
 
-// GetProxyPort finds a proxy port used for redirect 'name' installed earlier with InstallProxyRules.
-// By convention "ingress" or "egress" is part of 'name' so it does not need to be specified explicitly.
-// Returns 0 a TPROXY entry with 'name' can not be found.
-func (m *Manager) GetProxyPort(name string) uint16 {
+// GetProxyPorts enumerates all existing TPROXY rules in the datapath installed earlier with
+// InstallProxyRules and returns all proxy ports found.
+func (m *Manager) GetProxyPorts() map[string]uint16 {
 	prog := ip4tables
 	if !m.sharedCfg.EnableIPv4 {
 		prog = ip6tables
 	}
 
-	return m.doGetProxyPort(prog, name)
+	return m.doGetProxyPorts(prog)
 }
 
-func (m *Manager) doGetProxyPort(prog iptablesInterface, name string) uint16 {
+func (m *Manager) doGetProxyPorts(prog iptablesInterface) map[string]uint16 {
+	portMap := make(map[string]uint16)
+
 	m.lock.Lock()
 	defer m.lock.Unlock()
 
 	rules, err := prog.runProgOutput([]string{"-t", "mangle", "-n", "-L", ciliumPreMangleChain})
 	if err != nil {
-		return 0
+		return portMap
 	}
 
 	re := regexp.MustCompile(
-		name + ".*TPROXY redirect " +
+		"(cilium-[^ ]*) proxy.*TPROXY redirect " +
 			"(0.0.0.0|" + ipfamily.IPv4().Localhost +
 			"|::|" + ipfamily.IPv6().Localhost + ")" +
 			":([1-9][0-9]*) mark",
 	)
 	strs := re.FindAllString(rules, -1)
-	if len(strs) == 0 {
-		return 0
+	for _, str := range strs {
+		// Pick the name and port number from each match
+		name := re.ReplaceAllString(str, "$1")
+		portStr := re.ReplaceAllString(str, "$3")
+		portUInt64, err := strconv.ParseUint(portStr, 10, 16)
+		if err == nil {
+			portMap[name] = uint16(portUInt64)
+		}
 	}
-	// Pick the port number from the last match, as rules are appended to the end (-A)
-	portStr := re.ReplaceAllString(strs[len(strs)-1], "$2")
-	portUInt64, err := strconv.ParseUint(portStr, 10, 16)
-	if err != nil {
-		log.WithError(err).Debugf("Port number cannot be parsed: %s", portStr)
-		return 0
-	}
-
-	return uint16(portUInt64)
+	return portMap
 }
 
 func (m *Manager) getDeliveryInterface(ifName string) string {

pkg/datapath/iptables/iptables_test.go
@@ -344,7 +344,7 @@ func TestAddProxyRulesv4(t *testing.T) {
 	}
 }
 
-func TestGetProxyPort(t *testing.T) {
+func TestGetProxyPorts(t *testing.T) {
 	mockIp4tables := &mockIptables{t: t, prog: "iptables"}
 	mockIp4tables.expectations = []expectation{
 		{
@@ -362,9 +362,9 @@ TPROXY     udp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd7a90
 	}
 
 	// Finds the latest port number if multiple rules for the same proxy name
-	port := mockManager.doGetProxyPort(mockIp4tables, "cilium-dns-egress")
-	if port != uint16(43479) {
-		t.Fatalf("expected port number %d, got %d", uint16(43479), port)
+	portMap := mockManager.doGetProxyPorts(mockIp4tables)
+	if len(portMap) != 1 || portMap["cilium-dns-egress"] != uint16(43479) {
+		t.Fatalf("expected port number %d, got %d, portMap: %v", uint16(43479), portMap["cilium-dns-egress"], portMap)
 	}
 	if err := mockIp4tables.checkExpectations(); err != nil {
 		t.Fatal(err)
@@ -379,17 +379,17 @@ TPROXY     udp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd7a90
 				`Chain CILIUM_PRE_mangle (1 references)
 target     prot opt source               destination
 MARK       all  --  0.0.0.0/0            0.0.0.0/0            socket --transparent /* cilium: any->pod redirect proxied traffic to host proxy */ MARK set 0x200
-TPROXY     tcp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd5a90200 /* cilium: TPROXY to host cilium-random-proxy proxy */ TPROXY redirect 0.0.0.0:43477 mark 0x200/0xffffffff
-TPROXY     udp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd5a90200 /* cilium: TPROXY to host cilium-random-proxy proxy */ TPROXY redirect 0.0.0.0:43477 mark 0x200/0xffffffff
-TPROXY     tcp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd7a90200 /* cilium: TPROXY to host cilium-random-proxy proxy */ TPROXY redirect 0.0.0.0:43479 mark 0x200/0xffffffff
-TPROXY     udp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd7a90200 /* cilium: TPROXY to host cilium-random-proxy proxy */ TPROXY redirect 0.0.0.0:43479 mark 0x200/0xffffffff
+TPROXY     tcp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd5a90200 /* cilium: TPROXY to host cilium-random-ingress proxy */ TPROXY redirect 0.0.0.0:43477 mark 0x200/0xffffffff
+TPROXY     udp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd5a90200 /* cilium: TPROXY to host cilium-random-ingress proxy */ TPROXY redirect 0.0.0.0:43477 mark 0x200/0xffffffff
+TPROXY     tcp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd7a90200 /* cilium: TPROXY to host cilium-random-ingress proxy */ TPROXY redirect 0.0.0.0:43479 mark 0x200/0xffffffff
+TPROXY     udp  --  0.0.0.0/0            0.0.0.0/0            mark match 0xd7a90200 /* cilium: TPROXY to host cilium-random-ingress proxy */ TPROXY redirect 0.0.0.0:43479 mark 0x200/0xffffffff
 `),
 		},
 	}
 
-	port = mockManager.doGetProxyPort(mockIp4tables, "cilium-random-proxy")
-	if port != uint16(43479) {
-		t.Fatalf("expected port number %d, got %d", uint16(43479), port)
+	portMap = mockManager.doGetProxyPorts(mockIp4tables)
+	if len(portMap) != 1 || portMap["cilium-random-ingress"] != uint16(43479) {
+		t.Fatalf("expected port number %d, got %d, portMap: %v", uint16(43479), portMap["cilium-random-ingress"], portMap)
 	}
 	if err := mockIp4tables.checkExpectations(); err != nil {
 		t.Fatal(err)

pkg/datapath/types/loader.go
@@ -54,9 +54,9 @@ type IptablesManager interface {
 	// connections.
 	SupportsOriginalSourceAddr() bool
 
-	// GetProxyPort fetches the existing proxy port configured for the
-	// specified listener. Used early in bootstrap to reopen proxy ports.
-	GetProxyPort(listener string) uint16
+	// GetProxyPorts fetches the existing proxy ports configured in the
+	// datapath. Used early in bootstrap to reopen proxy ports.
+	GetProxyPorts() map[string]uint16
 
 	// InstallNoTrackRules is explicitly called when a pod has valid
 	// "policy.cilium.io/no-track-port" annotation.  When

commit 47f14671453cfeb6580a3e337ad930f54a840791

fqdn: Skip "open ports" check for statically configured ports
When restoring the previous DNS proxy port, we check if the port is
already in-use. However, if the port we retrieved from `GetProxyPort`
was previously set via `SetProxyPort`, then we want to use it
unconditionally. We rely on `isStatic` for this, as static ports cannot
change and the open port may be open with `SO_REUSEPORT` (which
`proxy.OpenLocalPorts()` does not check). Restored ports never have
`isStatic` set to true, so this does retain the "open ports" check if
the port was restored.

In addition, when restoring ports we want to make sure that previous
calls to `SetProxyPort` are also not overwritten, thus this commit also
only restores the port if it wasn't explicitly set.

This is the same behavior we had previously, which did not check the
returned port of `d.l7Proxy.GetProxyPort` against the list of open
ports.

Fixes: d11e4d261279 ("proxy: Reuse proxy ports from datapath on restart")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

daemon/cmd/fqdn.go
@@ -96,10 +96,16 @@ func (d *Daemon) bootstrapFQDN(possibleEndpoints map[uint16]*endpoint.Endpoint, 
 	port := uint16(option.Config.ToFQDNsProxyPort)
 	if port == 0 {
 		// Try reuse previous DNS proxy port number
-		if oldPort, err := d.l7Proxy.GetProxyPort(proxytypes.DNSProxyName); err == nil {
-			openLocalPorts := proxy.OpenLocalPorts()
-			if _, alreadyOpen := openLocalPorts[oldPort]; !alreadyOpen {
+		if oldPort, isStatic, err := d.l7Proxy.GetProxyPort(proxytypes.DNSProxyName); err == nil {
+			if isStatic {
 				port = oldPort
+			} else {
+				openLocalPorts := proxy.OpenLocalPorts()
+				if _, alreadyOpen := openLocalPorts[oldPort]; !alreadyOpen {
+					port = oldPort
+				} else {
+					log.WithField(logfields.Port, oldPort).Info("Unable re-use old DNS proxy port as it is already in use")
+				}
 			}
 		}
 	}

pkg/proxy/proxy.go
@@ -386,6 +386,11 @@ func (p *Proxy) restoreProxyPortsFromFile() error {
 	}
 
 	for name, pp := range portsMap {
+		if existing := p.proxyPorts[name]; existing != nil {
+			if existing.ProxyPort != 0 {
+				continue // do not overwrite explicitly set port
+			}
+		}
 		p.proxyPorts[name] = pp
 		p.allocatedPorts[pp.ProxyPort] = false
 		log.
@@ -405,6 +410,9 @@ func (p *Proxy) restoreProxyPortsFromIptables() {
 	for name, port := range portsMap {
 		pp := p.proxyPorts[name]
 		if pp != nil {
+			if pp.ProxyPort != 0 {
+				continue // do not overwrite explicitly set port
+			}
 			pp.ProxyPort = port
 		} else {
 			// Only CRD type proxy ports can be dynamically allocated. Assume a port
@@ -437,15 +445,15 @@ func (p *Proxy) RestoreProxyPorts() {
 }
 
 // GetProxyPort() returns the fixed listen port for a proxy, if any.
-func (p *Proxy) GetProxyPort(name string) (uint16, error) {
+func (p *Proxy) GetProxyPort(name string) (port uint16, isStatic bool, err error) {
 	// Accessing pp.proxyPort requires the lock
 	p.mutex.RLock()
 	defer p.mutex.RUnlock()
 	pp := p.proxyPorts[name]
 	if pp != nil {
-		return pp.ProxyPort, nil
+		return pp.ProxyPort, pp.isStatic, nil
 	}
-	return 0, proxyNotFoundError(name)
+	return 0, false, proxyNotFoundError(name)
 }
 
 // AllocateCRDProxyPort() allocates a new port for listener 'name', or returns the current one if

pkg/proxy/proxy_test.go
@@ -61,7 +61,7 @@ func TestPortAllocator(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEqual(t, 0, port)
 
-	port1, err := p.GetProxyPort("listener1")
+	port1, _, err := p.GetProxyPort("listener1")
 	require.NoError(t, err)
 	require.Equal(t, port, port1)
 
@@ -84,7 +84,7 @@ func TestPortAllocator(t *testing.T) {
 	require.NoError(t, err)
 
 	// ProxyPort lingers and can still be found, but it's port is zeroed
-	port1b, err := p.GetProxyPort("listener1")
+	port1b, _, err := p.GetProxyPort("listener1")
 	require.NoError(t, err)
 	require.Equal(t, uint16(0), port1b)
 	require.Equal(t, uint16(0), pp.ProxyPort)