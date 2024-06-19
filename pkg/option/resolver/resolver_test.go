110ab296648ec3c0a39bef693009aa1a0b956f3d
Chris Tarazi <chris@isovalent.com>
2024-06-18 17:13:55 -0700

    option/resolver: Avoid falling back to v2alpha1 CNC if v2 found
    Previously, Cilium would always fetch the v2alpha1 version of the CNC
    even if the v2 version was already found. Fetching the v2alpha1 triggers
    the deprecation warning from Kubernetes as it was deprecated in
    https://github.com/cilium/cilium/pull/31721. Those warnings cause CI
    failures such as:
    
    ```
    FAIL: Found 26 k8s-app=cilium logs matching list of errors that must be investigated:
    2024-06-17T13:38:56.846200595Z time="2024-06-17T13:38:56Z" level=warning msg="cilium.io/v2alpha1 CiliumNodeConfig will be deprecated in cilium v1.16; use cilium.io/v2 CiliumNodeConfig" subsys=klog
    ```
    
    Fixes: db0d41af34 ("crd, option: Make CiliumNodeConfig available on v2 API")
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/option/resolver/resolver_test.go
+++ pkg/option/resolver/resolver_test.go
@@ -171,11 +171,9 @@ func TestResolveConfigurations(t *testing.T) {
 	g.Expect(config).To(gomega.Equal(map[string]string{
 		"cm-key":         "cm-val",
 		"anno-key":       "anno-val",
-		"cnc-key":        "cnc-val",
-		"cnc-key-2":      "cnc-val-2",
 		"cnc-key-v2":     "cnc-val-v2",
 		"cnc-key-2-v2":   "cnc-val-2-v2",
-		"config-sources": "cilium-node-config:test-ns/specific,cilium-node-config:test-ns/specific-v2,cilium-node-config:test-ns/test-1,cilium-node-config:test-ns/test-1-v2,config-map:test-ns/cm,node:nodename",
+		"config-sources": "cilium-node-config:test-ns/specific-v2,cilium-node-config:test-ns/test-1-v2,config-map:test-ns/cm,node:nodename",
 	}))
 }
 
@@ -367,7 +365,7 @@ func TestReadNodeConfigs(t *testing.T) {
 				g.Expect(err).To(gomega.BeNil())
 			}
 
-			configs, _, err := readNodeConfigsAllVersions(context.Background(), clients, tc.name, testNS, "")
+			configs, _, _, err := readNodeConfigsAllVersions(context.Background(), clients, tc.name, testNS, "")
 			g.Expect(err).To(gomega.BeNil())
 
 			g.Expect(configs).To(gomega.Equal(tc.expected))
@@ -482,7 +480,7 @@ func TestReadNodeConfigsAlpha(t *testing.T) {
 				g.Expect(err).To(gomega.BeNil())
 			}
 
-			configs, _, err := readNodeConfigsAllVersions(context.Background(), clients, tc.name, testNS, "")
+			configs, _, _, err := readNodeConfigsAllVersions(context.Background(), clients, tc.name, testNS, "")
 			g.Expect(err).To(gomega.BeNil())
 
 			g.Expect(configs).To(gomega.Equal(tc.expected))
