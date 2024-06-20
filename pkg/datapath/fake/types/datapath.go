cilium/cilium

commit 76ca09a1087d19106e665d2278017683ec577676

loader: stop linking template.o into ep.StateDir()
Compilation of an endpoint program creates a template.o
symlink in the endpoint state directory. This is problematic
since we really should be creating the file in the "next"
state directory. Overall, the loader should not fiddle
around in the endpoints state directory.

Replace the symlink with template.txt in the endpoint state
directory, which contains the hash of the used template.
Importantly, we write this file from the endpoint code,
not from the loader.

Move the only user of template.o to template.txt.

Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

pkg/datapath/loader/loader.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"net"
 	"net/netip"
-	"os"
 	"path"
 	"strings"
 	"sync"
@@ -679,30 +678,6 @@ func (l *loader) compileOrLoad(ctx context.Context, ep datapath.Endpoint, dirs *
 		return err
 	}
 	defer template.Close()
-
-	symPath := path.Join(ep.StateDir(), defaults.TemplatePath)
-	if _, err := os.Stat(symPath); err == nil {
-		if err = os.RemoveAll(symPath); err != nil {
-			return &os.PathError{
-				Op:   "Failed to remove old symlink",
-				Path: symPath,
-				Err:  err,
-			}
-		}
-	} else if !os.IsNotExist(err) {
-		return &os.PathError{
-			Op:   "Failed to locate symlink",
-			Path: symPath,
-			Err:  err,
-		}
-	}
-	if err := os.Symlink(templateFile.Name(), symPath); err != nil {
-		return &os.PathError{
-			Op:   fmt.Sprintf("Failed to create symlink to %s", templateFile.Name()),
-			Path: symPath,
-			Err:  err,
-		}
-	}
 
 	stats.BpfWriteELF.Start()
 	epObj := endpointObj

pkg/endpoint/bpf.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 	"github.com/cilium/cilium/pkg/fqdn/restore"
 	"github.com/cilium/cilium/pkg/loadinfo"
@@ -927,6 +928,10 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext, rul
 	if datapathRegenCtxt.regenerationLevel >= regeneration.RegenerateWithDatapathRewrite {
 		if err := e.writeHeaderfile(nextDir); err != nil {
 			return fmt.Errorf("unable to write header file: %w", err)
+		}
+
+		if err := os.WriteFile(filepath.Join(nextDir, defaults.TemplateIDPath), []byte(datapathRegenCtxt.bpfHeaderfilesHash+"\n"), 0644); err != nil {
+			return fmt.Errorf("unable to write template id: %w", err)
 		}
 	}
 

cilium-dbg/cmd/bpf_sha_list.go
@@ -4,6 +4,7 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -19,9 +20,6 @@ import (
 const (
 	shaTitle       = "Datapath SHA"
 	endpointsTitle = "Endpoint(s)"
-
-	noSHA     = "<No template used>"
-	brokenSHA = "<Template path invalid>"
 )
 
 var (
@@ -52,24 +50,17 @@ func isEndpointID(name string) bool {
 // getTemplateSHA returns the SHA that should be reported to the user for
 // the specified endpoint ID.
 func getTemplateSHA(epID string) string {
-	// Get symlink from endpointDir
-	templateSymlink := filepath.Join(stateDir, epID, defaults.TemplatePath)
-	f, err := os.Lstat(templateSymlink)
+	contents, err := os.ReadFile(filepath.Join(stateDir, epID, defaults.TemplateIDPath))
 	if err != nil {
-		return noSHA
-	}
-	if f.Mode()&os.ModeSymlink == 0 {
-		return brokenSHA
+		return fmt.Sprintf("<missing %s>", defaults.TemplateIDPath)
 	}
 
-	template, err := os.Readlink(templateSymlink)
-	if err != nil {
-		return brokenSHA
+	templateID := string(bytes.TrimSpace(contents))
+	if _, err := os.Stat(filepath.Join(templatesDir, templateID)); err != nil {
+		return "<template path invalid>"
 	}
-	if _, err = os.Stat(template); err != nil {
-		return brokenSHA
-	}
-	return filepath.Base(filepath.Dir(template))
+
+	return templateID
 }
 
 func dumpShaList() {

pkg/defaults/defaults.go
@@ -56,8 +56,9 @@ const (
 	// TemplatesDir is the default path for the compiled template objects relative to StateDir
 	TemplatesDir = "templates"
 
-	// TemplatePath is the default path for a symlink to a template relative to StateDir/<EPID>
-	TemplatePath = "template.o"
+	// TemplateIDPath is the name of a file which contains the ID (aka hash) of
+	// the template used by the endpoint.
+	TemplateIDPath = "template.txt"
 
 	// BpfDir is the default path for template files relative to LibDir
 	BpfDir = "bpf"

commit d8fb7e05f8c59462b2c70a21ae341cfecc3c74bd

pkg/endpoint: store template hash in template.txt
The loader uses two subtly different hashes to identify endpoint
configuration. The endpoint hash is unique for each endpoint and is
used to detect when configuration changes. The template hash is
shared between multiple endpoints and is used as a cache key of sorts.

When I rewrote the loader to not fiddle with the endpoint state directory
anymore I made the mistake of persisting the endpoint hash into template.txt.
This means that it is not possible to correlate an endpoint state directory
with a template cache entry.

Fix this by returning the template hash as a sort of ID from ReloadDatapath
and writing that into template.txt.

Fixes: 76ca09a108 ("loader: stop linking template.o into ep.StateDir()")
Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

pkg/datapath/fake/types/datapath.go
@@ -130,7 +130,7 @@ func (f *FakeLoader) CompileOrLoad(ctx context.Context, ep datapath.Endpoint, st
 	panic("implement me")
 }
 
-func (f *FakeLoader) ReloadDatapath(ctx context.Context, ep datapath.Endpoint, stats *metrics.SpanStat) error {
+func (f *FakeLoader) ReloadDatapath(ctx context.Context, ep datapath.Endpoint, stats *metrics.SpanStat) (string, error) {
 	panic("implement me")
 }
 

pkg/datapath/loader/cache.go
@@ -158,15 +158,13 @@ func (o *objectCache) build(ctx context.Context, nodeCfg *datapath.LocalNodeConf
 // threads attempt to concurrently fetchOrCompile a template binary for the
 // same set of EndpointConfiguration.
 //
-// Returns a copy of the compiled and parsed ELF and whether a compilation was
-// triggered.
-func (o *objectCache) fetchOrCompile(ctx context.Context, nodeCfg *datapath.LocalNodeConfiguration, cfg datapath.EndpointConfiguration, dir *directoryInfo, stats *metrics.SpanStat) (spec *ebpf.CollectionSpec, compiled bool, err error) {
+// Returns a copy of the compiled and parsed ELF and a hash identifying a cached entry.
+func (o *objectCache) fetchOrCompile(ctx context.Context, nodeCfg *datapath.LocalNodeConfiguration, cfg datapath.EndpointConfiguration, dir *directoryInfo, stats *metrics.SpanStat) (spec *ebpf.CollectionSpec, hash string, err error) {
 	cfg = wrap(cfg)
 
-	var hash string
 	hash, err = o.baseHash.hashTemplate(o, nodeCfg, cfg)
 	if err != nil {
-		return nil, false, err
+		return nil, "", err
 	}
 
 	// Capture the time spent waiting for the template to compile.
@@ -185,7 +183,7 @@ func (o *objectCache) fetchOrCompile(ctx context.Context, nodeCfg *datapath.Loca
 	defer obj.Unlock()
 
 	if obj.spec != nil {
-		return obj.spec.Copy(), false, nil
+		return obj.spec.Copy(), hash, nil
 	}
 
 	if stats == nil {
@@ -197,13 +195,13 @@ func (o *objectCache) fetchOrCompile(ctx context.Context, nodeCfg *datapath.Loca
 		if !errors.Is(err, context.Canceled) {
 			scopedLog.WithError(err).Error("BPF template object creation failed")
 		}
-		return nil, false, err
+		return nil, "", err
 	}
 
 	obj.spec, err = bpf.LoadCollectionSpec(path)
 	if err != nil {
-		return nil, false, fmt.Errorf("load eBPF ELF %s: %w", path, err)
+		return nil, "", fmt.Errorf("load eBPF ELF %s: %w", path, err)
 	}
 
-	return obj.spec.Copy(), true, nil
+	return obj.spec.Copy(), hash, nil
 }

pkg/datapath/loader/loader.go
@@ -576,7 +576,7 @@ func (l *loader) replaceOverlayDatapath(ctx context.Context, cArgs []string, ifa
 // CompileOrLoad with the same configuration parameters. When the first
 // goroutine completes compilation of the template, all other CompileOrLoad
 // invocations will be released.
-func (l *loader) ReloadDatapath(ctx context.Context, ep datapath.Endpoint, stats *metrics.SpanStat) (err error) {
+func (l *loader) ReloadDatapath(ctx context.Context, ep datapath.Endpoint, stats *metrics.SpanStat) (hash string, err error) {
 	dirs := directoryInfo{
 		Library: option.Config.BpfDir,
 		Runtime: option.Config.StateDir,
@@ -586,15 +586,15 @@ func (l *loader) ReloadDatapath(ctx context.Context, ep datapath.Endpoint, stats
 
 	cfg := l.nodeConfig.Load()
 
-	spec, _, err := l.templateCache.fetchOrCompile(ctx, cfg, ep, &dirs, stats)
+	spec, hash, err := l.templateCache.fetchOrCompile(ctx, cfg, ep, &dirs, stats)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	stats.BpfLoadProg.Start()
 	err = l.reloadDatapath(ep, spec)
 	stats.BpfLoadProg.End(err == nil)
-	return err
+	return hash, err
 }
 
 // Unload removes the datapath specific program aspects

pkg/datapath/types/loader.go
@@ -17,7 +17,7 @@ import (
 type Loader interface {
 	CallsMapPath(id uint16) string
 	CustomCallsMapPath(id uint16) string
-	ReloadDatapath(ctx context.Context, ep Endpoint, stats *metrics.SpanStat) error
+	ReloadDatapath(ctx context.Context, ep Endpoint, stats *metrics.SpanStat) (string, error)
 	ReinitializeXDP(ctx context.Context, extraCArgs []string) error
 	EndpointHash(cfg EndpointConfiguration) (string, error)
 	Unload(ep Endpoint)

pkg/endpoint/bpf.go
@@ -661,12 +661,16 @@ func (e *Endpoint) realizeBPFState(regenContext *regenerationContext) (err error
 		}
 
 		// Compile and install BPF programs for this endpoint
-		err = e.owner.Datapath().Loader().ReloadDatapath(datapathRegenCtxt.completionCtx, datapathRegenCtxt.epInfoCache, &stats.datapathRealization)
+		templateHash, err := e.owner.Datapath().Loader().ReloadDatapath(datapathRegenCtxt.completionCtx, datapathRegenCtxt.epInfoCache, &stats.datapathRealization)
 		if err != nil {
 			if !errors.Is(err, context.Canceled) {
 				e.getLogger().WithError(err).Error("Error while reloading endpoint BPF program")
 			}
 			return err
+		}
+
+		if err := os.WriteFile(filepath.Join(datapathRegenCtxt.nextDir, defaults.TemplateIDPath), []byte(templateHash+"\n"), 0644); err != nil {
+			return fmt.Errorf("unable to write template id: %w", err)
 		}
 
 		e.getLogger().Info("Reloaded endpoint BPF program")
@@ -916,10 +920,6 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext, rul
 	if datapathRegenCtxt.regenerationLevel >= regeneration.RegenerateWithDatapath {
 		if err := e.writeHeaderfile(nextDir); err != nil {
 			return fmt.Errorf("unable to write header file: %w", err)
-		}
-
-		if err := os.WriteFile(filepath.Join(nextDir, defaults.TemplateIDPath), []byte(datapathRegenCtxt.bpfHeaderfilesHash+"\n"), 0644); err != nil {
-			return fmt.Errorf("unable to write template id: %w", err)
 		}
 
 		datapathRegenCtxt.epInfoCache = e.createEpInfoCache(nextDir)

pkg/datapath/loader/cache_test.go
@@ -33,28 +33,28 @@ func TestObjectCache(t *testing.T) {
 	dir := getDirs(t)
 
 	// First run should compile and generate the object.
-	first, isNew, err := cache.fetchOrCompile(ctx, &localNodeConfig, &realEP, dir, nil)
+	first, hash, err := cache.fetchOrCompile(ctx, &localNodeConfig, &realEP, dir, nil)
 	require.NoError(t, err)
-	require.True(t, isNew)
+	require.NotEmpty(t, hash)
 
 	// Same EP should not be compiled twice.
-	second, isNew, err := cache.fetchOrCompile(ctx, &localNodeConfig, &realEP, dir, nil)
+	second, hash2, err := cache.fetchOrCompile(ctx, &localNodeConfig, &realEP, dir, nil)
 	require.NoError(t, err)
-	require.False(t, isNew)
+	require.Equal(t, hash, hash2)
 	require.False(t, second == first)
 
 	// Changing the ID should not generate a new object.
 	realEP.Id++
-	third, isNew, err := cache.fetchOrCompile(ctx, &localNodeConfig, &realEP, dir, nil)
+	third, hash3, err := cache.fetchOrCompile(ctx, &localNodeConfig, &realEP, dir, nil)
 	require.NoError(t, err)
-	require.False(t, isNew)
+	require.Equal(t, hash, hash3)
 	require.False(t, third == first)
 
 	// Changing a setting on the EP should generate a new object.
 	realEP.Opts.SetBool("foo", true)
-	fourth, isNew, err := cache.fetchOrCompile(ctx, &localNodeConfig, &realEP, dir, nil)
+	fourth, hash4, err := cache.fetchOrCompile(ctx, &localNodeConfig, &realEP, dir, nil)
 	require.NoError(t, err)
-	require.True(t, isNew)
+	require.NotEqual(t, hash, hash4)
 	require.False(t, fourth == first)
 }
 

pkg/datapath/loader/loader_test.go
@@ -90,7 +90,7 @@ func testReloadDatapath(t *testing.T, ep *testutils.TestEndpoint) {
 	stats := &metrics.SpanStat{}
 
 	l := newTestLoader(t)
-	err := l.ReloadDatapath(ctx, ep, stats)
+	_, err := l.ReloadDatapath(ctx, ep, stats)
 	require.NoError(t, err)
 }
 
@@ -171,7 +171,7 @@ func testCompileFailure(t *testing.T, ep *testutils.TestEndpoint) {
 	var err error
 	stats := &metrics.SpanStat{}
 	for err == nil && time.Now().Before(timeout) {
-		err = l.ReloadDatapath(ctx, ep, stats)
+		_, err = l.ReloadDatapath(ctx, ep, stats)
 	}
 	require.Error(t, err)
 }