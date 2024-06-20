558bb07e932c0ed8f11b629ea54f3ce0ca23de4a
Alexandre Perrin <alex@isovalent.com>
2024-06-20 16:52:52 +0200

    hubble: deflake TestLocalObserverServer_NodeLabels
    Before this patch, TestLocalObserverServer_NodeLabels was flaky because
    it didn't wait on the LocalNodeWatcher to have been updated at least
    once before using it.
    
    This commit makes NewLocalNodeWatcher retrieve the local node info so
    that the returned LocalNodeWatcher is usable right away. This means that
    the Hubble startup will block until the LocalNodeStore is ready with
    local node info, which in practice should not be an issue.
    
    Reported-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Alexandre Perrin <alex@isovalent.com>

--- pkg/hubble/observer/local_node_watcher.go
+++ pkg/hubble/observer/local_node_watcher.go
@@ -7,8 +7,6 @@ import (
 	"context"
 	"slices"
 
-	"github.com/cilium/stream"
-
 	flowpb "github.com/cilium/cilium/api/v1/flow"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/node"
@@ -17,9 +15,8 @@ import (
 // LocalNodeWatcher populate Hubble flows local node related fields (currently
 // only labels).
 type LocalNodeWatcher struct {
-	mu      lock.Mutex
-	updated int // update counter
-	cache   struct {
+	mu    lock.Mutex
+	cache struct {
 		// labels are represented as a Go map in node.LocalNode, but we need a
 		// key=val slice for Hubble flows.
 		labels []string
@@ -27,21 +24,29 @@ type LocalNodeWatcher struct {
 }
 
 // NewLocalNodeWatcher return a new LocalNodeWatcher. The given context control
-// whether the LocalNodeWatcher gets updated by the localNodeStream. It is safe
+// whether the LocalNodeWatcher gets updated by the localNodeStore. It is safe
 // to use the returned LocalNodeWatcher once the context is cancelled, but its
 // information might be out-of-date.
-func NewLocalNodeWatcher(ctx context.Context, localNodeStream stream.Observable[node.LocalNode]) *LocalNodeWatcher {
+func NewLocalNodeWatcher(ctx context.Context, localNodeStore *node.LocalNodeStore) (*LocalNodeWatcher, error) {
 	watcher := LocalNodeWatcher{}
-	localNodeStream.Observe(ctx, watcher.update, watcher.complete)
-	return &watcher
+	// fetch the initial local node
+	n, err := localNodeStore.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	watcher.update(n)
+	// subscribe to local node changes.
+	localNodeStore.Observe(ctx, watcher.update, watcher.complete)
+
+	return &watcher, nil
 }
 
 // OnDecodedFlow implements OnDecodedFlow for LocalNodeWatcher. The
 // LocalNodeWatcher populate the flow's node_labels field.
 func (w *LocalNodeWatcher) OnDecodedFlow(_ context.Context, flow *flowpb.Flow) (bool, error) {
 	w.mu.Lock()
+	defer w.mu.Unlock()
 	flow.NodeLabels = w.cache.labels
-	w.mu.Unlock()
 	return false, nil
 }
 
@@ -49,24 +54,15 @@ func (w *LocalNodeWatcher) OnDecodedFlow(_ context.Context, flow *flowpb.Flow) (
 func (w *LocalNodeWatcher) update(n node.LocalNode) {
 	labels := sortedLabelSlice(n.Labels)
 	w.mu.Lock()
-	w.cache.labels = labels
-	w.updated++
-	w.mu.Unlock()
-}
-
-// generation return this watcher update counter.
-func (w *LocalNodeWatcher) generation() int {
-	w.mu.Lock()
 	defer w.mu.Unlock()
-	return w.updated
+	w.cache.labels = labels
 }
 
 // complete clears the LocalNodeWatcher cache.
 func (w *LocalNodeWatcher) complete(error) {
 	w.mu.Lock()
+	defer w.mu.Unlock()
 	w.cache.labels = nil
-	w.updated++
-	w.mu.Unlock()
 }
 
 // sortedLabelSlice convert a given map of key/val labels, and return a sorted
