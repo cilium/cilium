cilium/cilium

commit 92cd032bf7b183241c0e248bd101258474f1c5c8

bpf, snat: select lru map if available otherwise fall back to htab
Same principle as in conntrack table. In case of evicting old NAT
entries from active connection via LRU mechanism, we could run into
two scenarios wrt TCP connections: i) packet coming in via ingress.
If there is no state for the given connection, the packet will be
dropped. Node might resend in which case we'll have an outgoing
connection. In case of the latter, we'll then re-create a new mapping
via snat_v{4,6}_new_mapping(). Try to retain evicted 5-tuple mapping
if possible such that there is a chance connections would keep
working before we try completely random ones. Lift the short interval
for GC cleanup when NAT is enabled to not interfere with stale NAT
entries in LRU. NAT hash-tab would normally piggy-back on GC.

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/lib/nat.h
@@ -46,9 +46,24 @@ struct nat_entry {
 #define NAT_CONTINUE_XLATE 	0
 #define NAT_PUNT_TO_STACK	1
 
-static __always_inline __be16 __snat_get_port_range(__u16 start, __u16 end)
+#ifdef HAVE_LRU_MAP_TYPE
+#define NAT_MAP_TYPE BPF_MAP_TYPE_LRU_HASH
+#else
+#define NAT_MAP_TYPE BPF_MAP_TYPE_HASH
+#endif
+
+static __always_inline __be16 __snat_clamp_port_range(__u16 start, __u16 end,
+						      __u16 val)
 {
-	return (get_prandom_u32() % (end - start) + start);
+	return (val % (end - start)) + start;
+}
+
+#define GOLDEN_RATIO_32 0x61C88647
+
+static __always_inline __be16 __snat_hash(__u16 val)
+{
+	/* High bits are more random, so use them. */
+	return ((__u32)val * GOLDEN_RATIO_32) >> 16;
 }
 
 static __always_inline void *__snat_lookup(void *map, void *tuple)
@@ -91,7 +106,7 @@ struct ipv4_nat_entry {
 
 #if defined ENABLE_IPV4 && defined ENABLE_MASQUERADE
 struct bpf_elf_map __section_maps SNAT_MAPPING_IPV4 = {
-	.type		= BPF_MAP_TYPE_HASH,
+	.type		= NAT_MAP_TYPE,
 	.size_key	= sizeof(struct ipv4_ct_tuple),
 	.size_value	= sizeof(struct ipv4_nat_entry),
 	.pinning	= PIN_GLOBAL_NS,
@@ -176,6 +191,7 @@ static __always_inline int snat_v4_new_mapping(struct __sk_buff *skb,
 	struct ipv4_nat_entry rstate;
 	struct ipv4_ct_tuple rtuple;
 	int ret, retries;
+	__be16 port;
 
 	__builtin_memset(&rstate, 0, sizeof(rstate));
 	__builtin_memset(ostate, 0, sizeof(*ostate));
@@ -204,10 +220,14 @@ static __always_inline int snat_v4_new_mapping(struct __sk_buff *skb,
 			if (!ret)
 				return 0;
 		}
-
-		rtuple.dport = ostate->to_sport = bpf_htons(
-			__snat_get_port_range(SNAT_MAPPING_MIN_PORT,
-					      SNAT_MAPPING_MAX_PORT));
+		if (NAT_MAP_TYPE == BPF_MAP_TYPE_LRU_HASH &&
+		    retries < SNAT_DETERMINISTIC_RETRIES)
+			port = __snat_hash(rtuple.dport);
+		else
+			port = get_prandom_u32();
+		port = __snat_clamp_port_range(SNAT_MAPPING_MIN_PORT,
+					       SNAT_MAPPING_MAX_PORT, port);
+		rtuple.dport = ostate->to_sport = bpf_htons(port);
 	}
 
 	return DROP_NAT_NO_MAPPING;
@@ -267,7 +287,9 @@ static __always_inline int snat_v4_handle_mapping(struct __sk_buff *skb,
 	else if (*state)
 		return NAT_CONTINUE_XLATE;
 	else if (dir == NAT_DIR_INGRESS)
-		return NAT_PUNT_TO_STACK;
+		return tuple->nexthdr != IPPROTO_ICMP &&
+		       bpf_ntohs(tuple->dport) < SNAT_MAPPING_MIN_PORT ?
+		       NAT_PUNT_TO_STACK : DROP_NAT_NO_MAPPING;
 	else
 		return snat_v4_new_mapping(skb, tuple, (*state = tmp));
 }
@@ -467,7 +489,7 @@ struct ipv6_nat_entry {
 
 #if defined ENABLE_IPV6 && defined ENABLE_MASQUERADE
 struct bpf_elf_map __section_maps SNAT_MAPPING_IPV6 = {
-	.type		= BPF_MAP_TYPE_HASH,
+	.type		= NAT_MAP_TYPE,
 	.size_key	= sizeof(struct ipv6_ct_tuple),
 	.size_value	= sizeof(struct ipv6_nat_entry),
 	.pinning	= PIN_GLOBAL_NS,
@@ -553,6 +575,7 @@ static __always_inline int snat_v6_new_mapping(struct __sk_buff *skb,
 	struct ipv6_nat_entry rstate;
 	struct ipv6_ct_tuple rtuple;
 	int ret, retries;
+	__be16 port;
 
 	__builtin_memset(&rstate, 0, sizeof(rstate));
 	__builtin_memset(ostate, 0, sizeof(*ostate));
@@ -581,10 +604,14 @@ static __always_inline int snat_v6_new_mapping(struct __sk_buff *skb,
 			if (!ret)
 				return 0;
 		}
-
-		rtuple.dport = ostate->to_sport = bpf_htons(
-			__snat_get_port_range(SNAT_MAPPING_MIN_PORT,
-					      SNAT_MAPPING_MAX_PORT));
+		if (NAT_MAP_TYPE == BPF_MAP_TYPE_LRU_HASH &&
+		    retries < SNAT_DETERMINISTIC_RETRIES)
+			port = __snat_hash(rtuple.dport);
+		else
+			port = get_prandom_u32();
+		port = __snat_clamp_port_range(SNAT_MAPPING_MIN_PORT,
+					       SNAT_MAPPING_MAX_PORT, port);
+		rtuple.dport = ostate->to_sport = bpf_htons(port);
 	}
 
 	return DROP_NAT_NO_MAPPING;
@@ -646,7 +673,9 @@ static __always_inline int snat_v6_handle_mapping(struct __sk_buff *skb,
 	else if (*state)
 		return NAT_CONTINUE_XLATE;
 	else if (dir == NAT_DIR_INGRESS)
-		return NAT_PUNT_TO_STACK;
+		return tuple->nexthdr != IPPROTO_ICMPV6 &&
+		       bpf_ntohs(tuple->dport) < SNAT_MAPPING_MIN_PORT ?
+		       NAT_PUNT_TO_STACK : DROP_NAT_NO_MAPPING;
 	else
 		return snat_v6_new_mapping(skb, tuple, (*state = tmp));
 }

bpf/node_config.h
@@ -47,6 +47,7 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 #define SNAT_MAPPING_MIN_PORT 1024
 #define SNAT_MAPPING_MAX_PORT 65535
 #define SNAT_COLLISION_RETRIES 16
+#define SNAT_DETERMINISTIC_RETRIES 6
 #endif
 
 #ifdef ENABLE_IPV4

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=8e6cf1818f20efab3344a715dd2dd1bb8a57f3fb
+GO_BINDATA_SHA1SUM=2535dba6a8464fb2f09f4fdb44f0f9be5d2552bc
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

pkg/datapath/linux/config.go
@@ -151,6 +151,7 @@ func (l *linuxDatapath) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeConf
 		fmt.Fprintf(fw, "#define SNAT_MAPPING_MIN_PORT %d\n", nat.MinPortSnatDefault)
 		fmt.Fprintf(fw, "#define SNAT_MAPPING_MAX_PORT %d\n", nat.MaxPortSnatDefault)
 		fmt.Fprintf(fw, "#define SNAT_COLLISION_RETRIES %d\n", nat.CollisionRetriesDefault)
+		fmt.Fprintf(fw, "#define SNAT_DETERMINISTIC_RETRIES %d\n", nat.DeterministicRetriesDefault)
 		// SNAT_DIRECTION is defined by init.sh
 		if option.Config.EnableIPv4 {
 			ipv4Addr := node.GetExternalIPv4()

pkg/endpointmanager/conntrack.go
@@ -105,11 +105,7 @@ func calculateInterval(mapType bpf.MapType) time.Duration {
 		return val
 	}
 
-	bpfNAT := !option.Config.InstallIptRules && option.Config.Masquerade
-
-	//  Fall-back to short interval when BPF NAT is enabled for now as the
-	//  NAT entry expiration relies on a frequent cleanup
-	if mapType == bpf.MapTypeLRUHash && !bpfNAT {
+	if mapType == bpf.MapTypeLRUHash {
 		return defaults.ConntrackGCIntervalLRU
 	}
 

pkg/maps/nat/nat.go
@@ -43,6 +43,10 @@ const (
 	// CollisionRetriesDefault defines maximum retries for resolving port collisions.
 	CollisionRetriesDefault = 16
 
+	// DeterministicRetriesDefault defines maximum deterministic retries for
+	// resolving port collisions.
+	DeterministicRetriesDefault = 6
+
 	// MaxEntries defines maximum NAT entries.
 	MaxEntries = 524288
 
@@ -109,7 +113,7 @@ func NewMap(name string, v4 bool) *Map {
 	return &Map{
 		Map: *bpf.NewMap(
 			name,
-			bpf.BPF_MAP_TYPE_HASH,
+			bpf.MapTypeLRUHash,
 			sizeKey,
 			sizeVal,
 			MaxEntries,

commit 5255799e9340fa7555d042a537286dd39f94cf30

bpf: fix __snat_clamp_port_range not using highest port
__snat_clamp_port_range has an output in the range [start, end),
but the call sites expect it to produce a value in [start, end].

Operate on the full range and fix tests which relied on the
[start, end) interpretation.

Fixes: 92cd032bf7 ("bpf, snat: select lru map if available otherwise fall back to htab")
Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

bpf/lib/nat.h
@@ -43,7 +43,10 @@ struct nat_entry {
 static __always_inline __u16 __snat_clamp_port_range(__u16 start, __u16 end,
 						     __u16 val)
 {
-	return (val % (__u16)(end - start)) + start;
+	/* Account for closed interval. */
+	__u32 n = (__u32)(end - start) + 1;
+
+	return (__u16)((__u32)(val) % n) + start;
 }
 
 static __always_inline __maybe_unused __u16

bpf/tests/bpf_nat_tests.c
@@ -184,7 +184,7 @@ int test_nat4_icmp_error_tcp(__maybe_unused struct __ctx_buff *ctx)
 	struct ipv4_nat_target target = {
 		.addr = bpf_htonl(IP_HOST),
 		.min_port = NODEPORT_PORT_MIN_NAT,
-		.max_port = NODEPORT_PORT_MIN_NAT + 1,
+		.max_port = NODEPORT_PORT_MIN_NAT,
 	};
 	struct ipv4_nat_entry state;
 	struct trace_ctx trace;
@@ -298,7 +298,7 @@ int test_nat4_icmp_error_udp(__maybe_unused struct __ctx_buff *ctx)
 	struct ipv4_nat_target target = {
 		.addr = bpf_htonl(IP_HOST),
 		.min_port = NODEPORT_PORT_MIN_NAT,
-		.max_port = NODEPORT_PORT_MIN_NAT + 1,
+		.max_port = NODEPORT_PORT_MIN_NAT,
 	};
 	struct ipv4_nat_entry state;
 	struct trace_ctx trace;
@@ -411,7 +411,7 @@ int test_nat4_icmp_error_icmp(__maybe_unused struct __ctx_buff *ctx)
 	struct ipv4_nat_target target = {
 		.addr = bpf_htonl(IP_HOST),
 		.min_port = NODEPORT_PORT_MIN_NAT,
-		.max_port = NODEPORT_PORT_MIN_NAT + 1,
+		.max_port = NODEPORT_PORT_MIN_NAT,
 	};
 	struct ipv4_nat_entry state;
 	struct trace_ctx trace;
@@ -513,7 +513,7 @@ int test_nat4_icmp_error_sctp(__maybe_unused struct __ctx_buff *ctx)
 	struct ipv4_nat_target target = {
 		.addr = bpf_htonl(IP_HOST),
 		.min_port = NODEPORT_PORT_MIN_NAT,
-		.max_port = NODEPORT_PORT_MIN_NAT + 1,
+		.max_port = NODEPORT_PORT_MIN_NAT,
 	};
 	struct ipv4_nat_entry state;
 	struct trace_ctx trace;
@@ -579,7 +579,7 @@ int test_nat4_icmp_error_tcp_egress(__maybe_unused struct __ctx_buff *ctx)
 	struct ipv4_nat_target target = {
 		.addr = bpf_htonl(IP_HOST),
 		.min_port = NODEPORT_PORT_MIN_NAT - 1,
-		.max_port = NODEPORT_PORT_MIN_NAT,
+		.max_port = NODEPORT_PORT_MIN_NAT - 1,
 	};
 	struct ipv4_nat_entry state;
 	void *map;
@@ -816,7 +816,7 @@ int test_nat4_icmp_error_icmp_egress(__maybe_unused struct __ctx_buff *ctx)
 	struct ipv4_nat_target target = {
 	    .addr = bpf_htonl(IP_HOST),
 	    .min_port = NODEPORT_PORT_MIN_NAT - 1,
-	    .max_port = NODEPORT_PORT_MIN_NAT,
+	    .max_port = NODEPORT_PORT_MIN_NAT - 1,
 	};
 	struct ipv4_nat_entry state;
 	void *map;

bpf/tests/inter_cluster_snat_clusterip_backend_overlay.c
@@ -41,7 +41,7 @@
 #undef NODEPORT_PORT_MAX_NAT
 #define NODEPORT_PORT_MAX 32767
 #define NODEPORT_PORT_MIN_NAT (NODEPORT_PORT_MAX + 1)
-#define NODEPORT_PORT_MAX_NAT (NODEPORT_PORT_MIN_NAT + 1)
+#define NODEPORT_PORT_MAX_NAT (NODEPORT_PORT_MIN_NAT)
 
 /* Overwrite (local) CLUSTER_ID defined in node_config.h */
 #undef CLUSTER_ID

bpf/tests/inter_cluster_snat_clusterip_client_overlay.c
@@ -41,7 +41,7 @@
 #undef NODEPORT_PORT_MAX_NAT
 #define NODEPORT_PORT_MAX 32767
 #define NODEPORT_PORT_MIN_NAT (NODEPORT_PORT_MAX + 1)
-#define NODEPORT_PORT_MAX_NAT (NODEPORT_PORT_MIN_NAT + 1)
+#define NODEPORT_PORT_MAX_NAT (NODEPORT_PORT_MIN_NAT)
 
 /* Overwrite (local) CLUSTER_ID defined in node_config.h */
 #undef CLUSTER_ID