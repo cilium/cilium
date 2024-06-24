cilium/cilium

commit 95886de65f7cb746e08bb585a8435fc8d3affcbc

GwAPI: externalTrafficPolicy support for GwAPI
Added externalTrafficPolicy (eTP) support for Cilium GatewayAPI.
eTP is globally configurable via `gatewayAPI.externalTrafficPolicy`
Helm flag.

Signed-off-by: Philip Schmid <phisch@cisco.com>

install/kubernetes/cilium/values.yaml
@@ -848,6 +848,9 @@ gatewayAPI:
   enableAlpn: false
   # -- The number of additional GatewayAPI proxy hops from the right side of the HTTP header to trust when determining the origin client's IP address.
   xffNumTrustedHops: 0
+  # -- Control how traffic from external sources is routed to the LoadBalancer Kubernetes Service for all Cilium GatewayAPI Gateway instances. Valid values are "Cluster" and "Local".
+  # ref: https://kubernetes.io/docs/reference/networking/virtual-ips/#external-traffic-policy
+  externalTrafficPolicy: Cluster
   # -- SecretsNamespace is the namespace in which envoy SDS will retrieve TLS secrets from.
   secretsNamespace:
     # -- Create secrets namespace for Gateway API.

install/kubernetes/cilium/values.yaml.tmpl
@@ -850,6 +850,9 @@ gatewayAPI:
   enableAlpn: false
   # -- The number of additional GatewayAPI proxy hops from the right side of the HTTP header to trust when determining the origin client's IP address.
   xffNumTrustedHops: 0
+  # -- Control how traffic from external sources is routed to the LoadBalancer Kubernetes Service for all Cilium GatewayAPI Gateway instances. Valid values are "Cluster" and "Local".
+  # ref: https://kubernetes.io/docs/reference/networking/virtual-ips/#external-traffic-policy
+  externalTrafficPolicy: Cluster
   # -- SecretsNamespace is the namespace in which envoy SDS will retrieve TLS secrets from.
   secretsNamespace:
     # -- Create secrets namespace for Gateway API.

operator/pkg/gateway-api/cell.go
@@ -11,6 +11,7 @@ import (
 	"github.com/cilium/hive/cell"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -34,12 +35,13 @@ var Cell = cell.Module(
 	"Manages the Gateway API controllers",
 
 	cell.Config(gatewayApiConfig{
-		EnableGatewayAPISecretsSync:   true,
-		EnableGatewayAPIProxyProtocol: false,
-		EnableGatewayAPIAppProtocol:   false,
-		EnableGatewayAPIAlpn:          false,
-		GatewayAPISecretsNamespace:    "cilium-secrets",
-		GatewayAPIXffNumTrustedHops:   0,
+		EnableGatewayAPISecretsSync:            true,
+		EnableGatewayAPIProxyProtocol:          false,
+		EnableGatewayAPIAppProtocol:            false,
+		EnableGatewayAPIAlpn:                   false,
+		GatewayAPIServiceExternalTrafficPolicy: "Cluster",
+		GatewayAPISecretsNamespace:             "cilium-secrets",
+		GatewayAPIXffNumTrustedHops:            0,
 
 		GatewayAPIHostnetworkEnabled:           false,
 		GatewayAPIHostnetworkNodelabelselector: "",
@@ -61,12 +63,13 @@ type gatewayApiConfig struct {
 	KubeProxyReplacement string
 	EnableNodePort       bool
 
-	EnableGatewayAPISecretsSync   bool
-	EnableGatewayAPIProxyProtocol bool
-	EnableGatewayAPIAppProtocol   bool
-	EnableGatewayAPIAlpn          bool
-	GatewayAPISecretsNamespace    string
-	GatewayAPIXffNumTrustedHops   uint32
+	EnableGatewayAPISecretsSync            bool
+	EnableGatewayAPIProxyProtocol          bool
+	EnableGatewayAPIAppProtocol            bool
+	EnableGatewayAPIAlpn                   bool
+	GatewayAPIServiceExternalTrafficPolicy string
+	GatewayAPISecretsNamespace             string
+	GatewayAPIXffNumTrustedHops            uint32
 
 	GatewayAPIHostnetworkEnabled           bool
 	GatewayAPIHostnetworkNodelabelselector string
@@ -81,6 +84,7 @@ func (r gatewayApiConfig) Flags(flags *pflag.FlagSet) {
 	flags.Bool("enable-gateway-api-app-protocol", r.EnableGatewayAPIAppProtocol, "Enables Backend Protocol selection (GEP-1911) for Gateway API via appProtocol")
 	flags.Bool("enable-gateway-api-alpn", r.EnableGatewayAPIAlpn, "Enables exposing ALPN with HTTP2 and HTTP/1.1 support for Gateway API")
 	flags.Uint32("gateway-api-xff-num-trusted-hops", r.GatewayAPIXffNumTrustedHops, "The number of additional GatewayAPI proxy hops from the right side of the HTTP header to trust when determining the origin client's IP address.")
+	flags.String("gateway-api-service-externaltrafficpolicy", r.GatewayAPIServiceExternalTrafficPolicy, "Kubernetes LoadBalancer Service externalTrafficPolicy for all Gateway instances.")
 	flags.String("gateway-api-secrets-namespace", r.GatewayAPISecretsNamespace, "Namespace having tls secrets used by CEC for Gateway API")
 	flags.Bool("gateway-api-hostnetwork-enabled", r.GatewayAPIHostnetworkEnabled, "Exposes Gateway listeners on the host network.")
 	flags.String("gateway-api-hostnetwork-nodelabelselector", r.GatewayAPIHostnetworkNodelabelselector, "Label selector that matches the nodes where the gateway listeners should be exposed. It's a list of comma-separated key-value label pairs. e.g. 'kubernetes.io/os=linux,kubernetes.io/hostname=kind-worker'")
@@ -108,6 +112,10 @@ func initGatewayAPIController(params gatewayAPIParams) error {
 		!params.GatewayApiConfig.EnableNodePort {
 		params.Logger.Warn("Gateway API support requires either kube-proxy-replacement or enable-node-port enabled")
 		return nil
+	}
+
+	if err := validateExternalTrafficPolicy(params); err != nil {
+		return err
 	}
 
 	params.Logger.WithField("requiredGVK", requiredGVK).Info("Checking for required GatewayAPI resources")
@@ -139,7 +147,11 @@ func initGatewayAPIController(params gatewayAPIParams) error {
 
 	cecTranslator.WithUseAlpn(params.GatewayApiConfig.EnableGatewayAPIAlpn)
 
-	gatewayAPITranslator := gatewayApiTranslation.NewTranslator(cecTranslator, params.GatewayApiConfig.GatewayAPIHostnetworkEnabled)
+	gatewayAPITranslator := gatewayApiTranslation.NewTranslator(
+		cecTranslator,
+		params.GatewayApiConfig.GatewayAPIHostnetworkEnabled,
+		params.GatewayApiConfig.GatewayAPIServiceExternalTrafficPolicy,
+	)
 
 	if err := registerReconcilers(
 		params.CtrlRuntimeManager,
@@ -170,6 +182,14 @@ func registerSecretSync(params gatewayAPIParams) secretsync.SecretSyncRegistrati
 			SecretsNamespace:     params.GatewayApiConfig.GatewayAPISecretsNamespace,
 		},
 	}
+}
+
+func validateExternalTrafficPolicy(params gatewayAPIParams) error {
+	if params.GatewayApiConfig.GatewayAPIServiceExternalTrafficPolicy == string(corev1.ServiceExternalTrafficPolicyCluster) ||
+		params.GatewayApiConfig.GatewayAPIServiceExternalTrafficPolicy == string(corev1.ServiceExternalTrafficPolicyLocal) {
+		return nil
+	}
+	return fmt.Errorf("invalid externalTrafficPolicy: %s", params.GatewayApiConfig.GatewayAPIServiceExternalTrafficPolicy)
 }
 
 func checkCRD(ctx context.Context, clientset k8sClient.Clientset, gvk schema.GroupVersionKind) error {

operator/pkg/model/translation/gateway-api/translator.go
@@ -26,13 +26,15 @@ const (
 type gatewayAPITranslator struct {
 	cecTranslator translation.CECTranslator
 
-	hostNetworkEnabled bool
+	hostNetworkEnabled    bool
+	externalTrafficPolicy string
 }
 
-func NewTranslator(cecTranslator translation.CECTranslator, hostNetworkEnabled bool) translation.Translator {
+func NewTranslator(cecTranslator translation.CECTranslator, hostNetworkEnabled bool, externalTrafficPolicy string) translation.Translator {
 	return &gatewayAPITranslator{
-		cecTranslator:      cecTranslator,
-		hostNetworkEnabled: hostNetworkEnabled,
+		cecTranslator:         cecTranslator,
+		hostNetworkEnabled:    hostNetworkEnabled,
+		externalTrafficPolicy: externalTrafficPolicy,
 	}
 }
 
@@ -101,7 +103,7 @@ func (t *gatewayAPITranslator) Translate(m *model.Model) (*ciliumv2.CiliumEnvoyC
 		allLabels = mergeMap(allLabels, l.GetLabels())
 	}
 
-	lbSvc := getService(source, ports, allLabels, allAnnotations)
+	lbSvc := getService(source, ports, allLabels, allAnnotations, t.externalTrafficPolicy)
 
 	if t.hostNetworkEnabled {
 		lbSvc.Spec.Type = corev1.ServiceTypeClusterIP
@@ -110,7 +112,7 @@ func (t *gatewayAPITranslator) Translate(m *model.Model) (*ciliumv2.CiliumEnvoyC
 	return cec, lbSvc, getEndpoints(*source), err
 }
 
-func getService(resource *model.FullyQualifiedResource, allPorts []uint32, labels, annotations map[string]string) *corev1.Service {
+func getService(resource *model.FullyQualifiedResource, allPorts []uint32, labels, annotations map[string]string, externalTrafficPolicy string) *corev1.Service {
 	uniquePorts := map[uint32]struct{}{}
 	for _, p := range allPorts {
 		uniquePorts[p] = struct{}{}
@@ -142,8 +144,9 @@ func getService(resource *model.FullyQualifiedResource, allPorts []uint32, label
 			},
 		},
 		Spec: corev1.ServiceSpec{
-			Type:  corev1.ServiceTypeLoadBalancer,
-			Ports: ports,
+			Type:                  corev1.ServiceTypeLoadBalancer,
+			ExternalTrafficPolicy: corev1.ServiceExternalTrafficPolicy(externalTrafficPolicy),
+			Ports:                 ports,
 		},
 	}
 }

operator/pkg/model/translation/gateway-api/translator_test.go
@@ -505,10 +505,11 @@ func Test_translator_Translate_WithXffNumTrustedHops(t *testing.T) {
 
 func Test_getService(t *testing.T) {
 	type args struct {
-		resource    *model.FullyQualifiedResource
-		allPorts    []uint32
-		labels      map[string]string
-		annotations map[string]string
+		resource              *model.FullyQualifiedResource
+		allPorts              []uint32
+		labels                map[string]string
+		annotations           map[string]string
+		externalTrafficPolicy string
 	}
 	tests := []struct {
 		name string
@@ -525,7 +526,8 @@ func Test_getService(t *testing.T) {
 					Kind:      "Gateway",
 					UID:       "57889650-380b-4c05-9a2e-3baee7fd5271",
 				},
-				allPorts: []uint32{80},
+				allPorts:              []uint32{80},
+				externalTrafficPolicy: "Cluster",
 			},
 			want: &corev1.Service{
 				ObjectMeta: metav1.ObjectMeta{
@@ -552,14 +554,58 @@ func Test_getService(t *testing.T) {
 							Protocol: corev1.ProtocolTCP,
 						},
 					},
-					Type: corev1.ServiceTypeLoadBalancer,
+					Type:                  corev1.ServiceTypeLoadBalancer,
+					ExternalTrafficPolicy: corev1.ServiceExternalTrafficPolicyCluster,
+				},
+			},
+		},
+		{
+			name: "externaltrafficpolicy set to local",
+			args: args{
+				resource: &model.FullyQualifiedResource{
+					Name:      "test-externaltrafficpolicy-local",
+					Namespace: "default",
+					Version:   "v1",
+					Kind:      "Gateway",
+					UID:       "41b82697-2d8d-4776-81b6-44d0bbac7faa",
+				},
+				allPorts:              []uint32{80},
+				externalTrafficPolicy: "Local",
+			},
+			want: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "cilium-gateway-test-externaltrafficpolicy-local",
+					Namespace: "default",
+					Labels: map[string]string{
+						owningGatewayLabel: "test-externaltrafficpolicy-local",
+					},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion: gatewayv1beta1.GroupVersion.String(),
+							Kind:       "Gateway",
+							Name:       "test-externaltrafficpolicy-local",
+							UID:        types.UID("41b82697-2d8d-4776-81b6-44d0bbac7faa"),
+							Controller: model.AddressOf(true),
+						},
+					},
+				},
+				Spec: corev1.ServiceSpec{
+					Ports: []corev1.ServicePort{
+						{
+							Name:     fmt.Sprintf("port-%d", 80),
+							Port:     80,
+							Protocol: corev1.ProtocolTCP,
+						},
+					},
+					Type:                  corev1.ServiceTypeLoadBalancer,
+					ExternalTrafficPolicy: corev1.ServiceExternalTrafficPolicyLocal,
 				},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := getService(tt.args.resource, tt.args.allPorts, tt.args.labels, tt.args.annotations)
+			got := getService(tt.args.resource, tt.args.allPorts, tt.args.labels, tt.args.annotations, tt.args.externalTrafficPolicy)
 			assert.Equalf(t, tt.want, got, "getService(%v, %v, %v, %v)", tt.args.resource, tt.args.allPorts, tt.args.labels, tt.args.annotations)
 			assert.Equal(t, true, len(got.Name) <= 63, "Service name is too long")
 		})

install/kubernetes/cilium/templates/cilium-configmap.yaml
@@ -280,6 +280,7 @@ data:
   enable-gateway-api-app-protocol: {{ or .Values.gatewayAPI.enableAppProtocol .Values.gatewayAPI.enableAlpn | quote }}
   enable-gateway-api-alpn: {{ .Values.gatewayAPI.enableAlpn | quote }}
   gateway-api-xff-num-trusted-hops: {{ .Values.gatewayAPI.xffNumTrustedHops | quote }}
+  gateway-api-service-externaltrafficpolicy: {{ .Values.gatewayAPI.externalTrafficPolicy | quote }}
   gateway-api-secrets-namespace: {{ .Values.gatewayAPI.secretsNamespace.name | quote }}
   gateway-api-hostnetwork-enabled: {{ .Values.gatewayAPI.hostNetwork.enabled | quote }}
   gateway-api-hostnetwork-nodelabelselector: {{ include "mapToString" .Values.gatewayAPI.hostNetwork.nodes.matchLabels | quote }}

install/kubernetes/cilium/templates/validate.yaml
@@ -66,6 +66,12 @@
   {{- end }}
 {{- end }}
 
+{{- if .Values.gatewayAPI.enabled }}
+  {{- if not (or (eq .Values.gatewayAPI.externalTrafficPolicy "Cluster") (eq .Values.gatewayAPI.externalTrafficPolicy "Local")) }}
+    {{ fail "Cilium GatewayAPI needs an externalTrafficPolicy set to 'Cluster' or 'Local'." }}
+  {{- end }}
+{{- end }}
+
 {{- if or .Values.envoyConfig.enabled .Values.ingressController.enabled .Values.gatewayAPI.enabled }}
   {{- if or (eq (toString .Values.kubeProxyReplacement) "disabled") (and (not (hasKey .Values "kubeProxyReplacement")) (not (semverCompare ">=1.14" (default "1.14" .Values.upgradeCompatibility)))) }}
     {{ fail "Ingress/Gateway API controller and EnvoyConfig require .Values.kubeProxyReplacement to be explicitly set to 'false' or 'true'" }}

operator/pkg/gateway-api/gateway_reconcile_test.go
@@ -304,7 +304,7 @@ func Test_gatewayReconciler_Reconcile(t *testing.T) {
 		Build()
 
 	cecTranslator := translation.NewCECTranslator("", false, false, true, 60, false, nil, false, false, 0)
-	gatewayAPITranslator := gatewayApiTranslation.NewTranslator(cecTranslator, false)
+	gatewayAPITranslator := gatewayApiTranslation.NewTranslator(cecTranslator, false, string(corev1.ServiceExternalTrafficPolicyCluster))
 
 	r := &gatewayReconciler{
 		Client:     c,

commit 6294478d40ed5d43bd55df0e032dfd976f99c15d

gateway-api: Un-set externalTrafficPolicy on LB service for host network
Support has recently been added for setting an `externalTrafficPolicy`
on the service generated for a Gateway. Unfortunately, this broke
support for the case when `hostNetworkEnabled` is enabled, as only
_externally-facing_ services (which services of type `ClusterIP` are
_not_) may have an `externalTrafficPolicy`.

This commit addresses that by explicitly removing the
`externalTrafficPolicy` when `hostNetworkEnabled == true`.

Fixes: 95886de65f7

Signed-off-by: Stefan Zwanenburg <stefan@zwanenburg.info>

install/kubernetes/cilium/values.yaml
@@ -847,6 +847,7 @@ gatewayAPI:
   # -- The number of additional GatewayAPI proxy hops from the right side of the HTTP header to trust when determining the origin client's IP address.
   xffNumTrustedHops: 0
   # -- Control how traffic from external sources is routed to the LoadBalancer Kubernetes Service for all Cilium GatewayAPI Gateway instances. Valid values are "Cluster" and "Local".
+  # Note that this value will be ignored when `hostNetwork.enabled == true`.
   # ref: https://kubernetes.io/docs/reference/networking/virtual-ips/#external-traffic-policy
   externalTrafficPolicy: Cluster
   # -- SecretsNamespace is the namespace in which envoy SDS will retrieve TLS secrets from.

install/kubernetes/cilium/values.yaml.tmpl
@@ -849,6 +849,7 @@ gatewayAPI:
   # -- The number of additional GatewayAPI proxy hops from the right side of the HTTP header to trust when determining the origin client's IP address.
   xffNumTrustedHops: 0
   # -- Control how traffic from external sources is routed to the LoadBalancer Kubernetes Service for all Cilium GatewayAPI Gateway instances. Valid values are "Cluster" and "Local".
+  # Note that this value will be ignored when `hostNetwork.enabled == true`.
   # ref: https://kubernetes.io/docs/reference/networking/virtual-ips/#external-traffic-policy
   externalTrafficPolicy: Cluster
   # -- SecretsNamespace is the namespace in which envoy SDS will retrieve TLS secrets from.

operator/pkg/gateway-api/cell.go
@@ -185,7 +185,10 @@ func registerSecretSync(params gatewayAPIParams) secretsync.SecretSyncRegistrati
 }
 
 func validateExternalTrafficPolicy(params gatewayAPIParams) error {
-	if params.GatewayApiConfig.GatewayAPIServiceExternalTrafficPolicy == string(corev1.ServiceExternalTrafficPolicyCluster) ||
+	if params.GatewayApiConfig.GatewayAPIHostnetworkEnabled && params.GatewayApiConfig.GatewayAPIServiceExternalTrafficPolicy != "" {
+		log.Warn("Gateway API host networking is enabled, externalTrafficPolicy will be ignored.")
+		return nil
+	} else if params.GatewayApiConfig.GatewayAPIServiceExternalTrafficPolicy == string(corev1.ServiceExternalTrafficPolicyCluster) ||
 		params.GatewayApiConfig.GatewayAPIServiceExternalTrafficPolicy == string(corev1.ServiceExternalTrafficPolicyLocal) {
 		return nil
 	}

operator/pkg/model/translation/gateway-api/translator.go
@@ -107,6 +107,7 @@ func (t *gatewayAPITranslator) Translate(m *model.Model) (*ciliumv2.CiliumEnvoyC
 
 	if t.hostNetworkEnabled {
 		lbSvc.Spec.Type = corev1.ServiceTypeClusterIP
+		lbSvc.Spec.ExternalTrafficPolicy = corev1.ServiceExternalTrafficPolicy("")
 	}
 
 	return cec, lbSvc, getEndpoints(*source), err

operator/pkg/model/translation/gateway-api/translator_test.go
@@ -440,23 +440,45 @@ func Test_translator_Translate_HostNetwork(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		translatorCases := []struct {
+			name                 string
+			gatewayAPITranslator *gatewayAPITranslator
+		}{
+			{
+				name: "Without externalTrafficPolicy",
+				gatewayAPITranslator: &gatewayAPITranslator{
+					cecTranslator:      translation.NewCECTranslator("cilium-secrets", false, false, true, 60, true, tt.nodeLabelSelector, tt.ipv4Enabled, tt.ipv6Enabled, 0),
+					hostNetworkEnabled: true,
+				},
+			},
+			{
+				name: "With externalTrafficPolicy",
+				gatewayAPITranslator: &gatewayAPITranslator{
+					cecTranslator:         translation.NewCECTranslator("cilium-secrets", false, false, true, 60, true, tt.nodeLabelSelector, tt.ipv4Enabled, tt.ipv6Enabled, 0),
+					hostNetworkEnabled:    true,
+					externalTrafficPolicy: "Cluster",
+				},
+			},
+		}
+
 		t.Run(tt.name, func(t *testing.T) {
-			trans := &gatewayAPITranslator{
-				cecTranslator:      translation.NewCECTranslator("cilium-secrets", false, false, true, 60, true, tt.nodeLabelSelector, tt.ipv4Enabled, tt.ipv6Enabled, 0),
-				hostNetworkEnabled: true,
+			for _, translatorCase := range translatorCases {
+				t.Run(translatorCase.name, func(t *testing.T) {
+					cec, svc, ep, err := translatorCase.gatewayAPITranslator.Translate(tt.args.m)
+					require.Equal(t, tt.wantErr, err != nil, "Error mismatch")
+
+					diffOutput := cmp.Diff(tt.want, cec, protocmp.Transform())
+					if len(diffOutput) != 0 {
+						t.Errorf("CiliumEnvoyConfigs did not match:\n%s\n", diffOutput)
+					}
+
+					require.NotNil(t, svc)
+					assert.Equal(t, corev1.ServiceTypeClusterIP, svc.Spec.Type)
+					require.Emptyf(t, svc.Spec.ExternalTrafficPolicy, "ClusterIP Services must not have an ExternalTrafficPolicy")
+
+					require.NotNil(t, ep)
+				})
 			}
-			cec, svc, ep, err := trans.Translate(tt.args.m)
-			require.Equal(t, tt.wantErr, err != nil, "Error mismatch")
-
-			diffOutput := cmp.Diff(tt.want, cec, protocmp.Transform())
-			if len(diffOutput) != 0 {
-				t.Errorf("CiliumEnvoyConfigs did not match:\n%s\n", diffOutput)
-			}
-
-			require.NotNil(t, svc)
-			assert.Equal(t, corev1.ServiceTypeClusterIP, svc.Spec.Type)
-
-			require.NotNil(t, ep)
 		})
 	}
 }