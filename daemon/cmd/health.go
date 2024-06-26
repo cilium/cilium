3e06dbf8135b779def7694ebdfd3abc8869cf50a
Robin Gögge <r.goegge@isovalent.com>
2024-06-26 16:38:34 +0200

    cilium: Remove health ep from ep manager on shutdown
    Currently on shutdown we don't remove the health endpoint from the
    endpoint manager but do remove the corresponding netlink interface.
    Note that validateEndpoint() special cases the health endpoint and
    does remove the old state directory if it existed before. But
    independent of that it would be better to just streamline cleanup
    via cleanupHealthEndpoint() for all cases.
    
    Related: #32689
    Signed-off-by: Robin Gögge <r.goegge@isovalent.com>
    Co-developed-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- daemon/cmd/health.go
+++ daemon/cmd/health.go
@@ -117,8 +117,7 @@ func (d *Daemon) initHealth(spec *healthApi.Spec, cleaner *daemonCleanup, sysctl
 	)
 
 	// Make sure to clean up the endpoint namespace when cilium-agent terminates
-	cleaner.cleanupFuncs.Add(health.KillEndpoint)
-	cleaner.cleanupFuncs.Add(health.CleanupEndpoint)
+	cleaner.cleanupFuncs.Add(d.cleanupHealthEndpoint)
 }
 
 func (d *Daemon) cleanupHealthEndpoint() {
