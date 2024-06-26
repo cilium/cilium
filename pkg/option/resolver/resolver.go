cilium/cilium

commit db0d41af348370abe6bb1ffc7071543b8bb4e1d8

crd, option: Make CiliumNodeConfig available on v2 API
This commit intens to turn CiliumNodeConfig feature to Limited. The CRD is now
available in v2 and v2alpha1 API versions. The depreciation of v2alpha1 should
be done in another commit.  The yaml definition of `CiliumNodeConfig` is in
`v2` directory but hold the two versions.

Signed-off-by: Donia Chaiehloudj <donia.cld@isovalent.com>

pkg/option/resolver/resolver.go
@@ -8,6 +8,7 @@ package resolver
 import (
 	"context"
 	"fmt"
+	"maps"
 	"os"
 	"path/filepath"
 	"sort"
@@ -16,11 +17,12 @@ import (
 	"github.com/sirupsen/logrus"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	labels "k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/sets"
 	apivalidation "k8s.io/apimachinery/pkg/util/validation"
 
 	"github.com/cilium/cilium/pkg/annotation"
+	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	ciliumv2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/logging"
@@ -167,7 +169,7 @@ func ReadConfigSource(ctx context.Context, client client.Clientset, nodeName str
 	case KindConfigMap:
 		return readConfigMap(ctx, client, source)
 	case KindNodeConfig:
-		return readNodeConfigs(ctx, client, nodeName, source.Namespace, source.Name)
+		return readNodeConfigsAllVersions(ctx, client, nodeName, source.Namespace, source.Name)
 	}
 	return nil, nil, fmt.Errorf("invalid source kind %s", source.Kind)
 }
@@ -229,12 +231,49 @@ func readConfigMap(ctx context.Context, client client.Clientset, source ConfigSo
 	return cm.Data, []string{source.String()}, nil
 }
 
-// ReadConfigOverrides reads all the CiliumNodeConfig objects and returns a flattened map
+// readNodeConfigsAllVersions read node configurations for versions v2 and v2alpha1 of CiliumNodeConfig CRD.
+// TODO depreciate CNC on v2alpha1 https://github.com/cilium/cilium/issues/31982
+func readNodeConfigsAllVersions(ctx context.Context, client client.Clientset, nodeName, namespace, name string) (map[string]string, []string, error) {
+	var errv2, errv2alpha1 error
+
+	nodeConfigv2, descv2, errv2 := readNodeConfigs(ctx, client, nodeName, namespace, name)
+	if errv2 != nil {
+		log.WithFields(logrus.Fields{logfields.Node: nodeName}).Errorf("CiliumNodeConfig v2 not found: %s", errv2)
+	}
+
+	nodeConfigv2alpha1, descv2alpha1, errv2alpha1 := readNodeConfigsv2alpha1(ctx, client, nodeName, namespace, name)
+	if errv2alpha1 != nil {
+		log.WithFields(logrus.Fields{logfields.Node: nodeName}).Errorf("CiliumNodeConfig v2alpha1 not found: %s", errv2alpha1)
+		// return the errors for the two versions
+		if errv2 != nil {
+			msg := fmt.Sprintf("CiliumNodeConfig v2 and v2alpha1 not found: %s and %s\n", errv2, errv2alpha1)
+			return nil, nil, fmt.Errorf(msg)
+		}
+		return nil, nil, errv2alpha1
+	}
+
+	// Copiying values from a map into a nil map results in a panic, please refer to https://github.com/golang/go/issues/64390
+	if nodeConfigv2alpha1 == nil {
+		nodeConfigv2alpha1 = nodeConfigv2
+	} else {
+		// overwrite nodeConfigv2alpha1 with nodeConfigv2 values
+		maps.Copy(nodeConfigv2alpha1, nodeConfigv2)
+	}
+
+	descv2 = append(descv2, descv2alpha1...)
+
+	return nodeConfigv2alpha1, descv2, nil
+}
+
+// readNodeConfigs reads all the CiliumNodeConfig in v2 objects and returns a flattened map
 // of any key overrides that apply to this node.
+// TODO remove me when CiliumNodeConfig v2alpha1 is deprecated
 func readNodeConfigs(ctx context.Context, client client.Clientset, nodeName, namespace, name string) (map[string]string, []string, error) {
-	var overrides []ciliumv2alpha1.CiliumNodeConfig
+	var overrides []ciliumv2.CiliumNodeConfig
+
+	// Retrieve CNCs if the name is not provided
 	if name == "" {
-		l, err := client.CiliumV2alpha1().CiliumNodeConfigs(namespace).List(ctx, metav1.ListOptions{})
+		l, err := client.CiliumV2().CiliumNodeConfigs(namespace).List(ctx, metav1.ListOptions{})
 		if err != nil {
 			if apierrors.IsNotFound(err) { // Tolerate the CRD not existing
 				return nil, nil, nil
@@ -243,14 +282,15 @@ func readNodeConfigs(ctx context.Context, client client.Clientset, nodeName, nam
 		}
 		overrides = l.Items
 	} else {
-		o, err := client.CiliumV2alpha1().CiliumNodeConfigs(namespace).Get(ctx, name, metav1.GetOptions{})
+		// Retrieve CNCs with the given name
+		o, err := client.CiliumV2().CiliumNodeConfigs(namespace).Get(ctx, name, metav1.GetOptions{})
 		if err != nil && !apierrors.IsNotFound(err) {
 			if apierrors.IsNotFound(err) { // Tolerate the CRD not existing
 				return nil, nil, nil
 			}
 			return nil, nil, fmt.Errorf("could not retrieve CiliumNodeConfig %s/%s: %w", namespace, name, err)
 		} else if err == nil {
-			overrides = []ciliumv2alpha1.CiliumNodeConfig{*o}
+			overrides = append(overrides, *o)
 		}
 	}
 
@@ -265,10 +305,10 @@ func readNodeConfigs(ctx context.Context, client client.Clientset, nodeName, nam
 		return nil, nil, fmt.Errorf("could not get Node %s: %w", nodeName, err)
 	}
 
-	matching := map[string]ciliumv2alpha1.CiliumNodeConfig{}
+	matching := map[string]ciliumv2.CiliumNodeConfig{}
 
 	// track names separately, since we will compute "priority" by lexicographic sort
-	matchingNames := []string{}
+	var matchingNames []string
 
 	for _, override := range overrides {
 		// ignore empty overrides
@@ -295,7 +335,7 @@ func readNodeConfigs(ctx context.Context, client client.Clientset, nodeName, nam
 	// Within overrides, lexicograpical ordering determines priority.
 	sort.Strings(matchingNames)
 
-	out := map[string]string{}
+	out := make(map[string]string)
 	for _, name := range matchingNames {
 		for k, v := range matching[name].Spec.Defaults {
 			if errs := apivalidation.IsConfigMapKey(k); len(errs) > 0 {
@@ -307,13 +347,108 @@ func readNodeConfigs(ctx context.Context, client client.Clientset, nodeName, nam
 			if _, set := out[k]; set {
 				log.WithFields(logrus.Fields{
 					logfields.ConfigKey: k,
-				}).Infof("Key %s set in multiple CiliumNodeConfigs", k)
+				}).Warnf("Key %s set in multiple CiliumNodeConfigs", k)
 			}
 			out[k] = v
 		}
 	}
 
-	sourceDescriptions := []string{}
+	var sourceDescriptions []string
+	for _, name := range matchingNames {
+		sourceDescriptions = append(sourceDescriptions, fmt.Sprintf("%s:%s/%s", KindNodeConfig, namespace, name))
+	}
+
+	return out, sourceDescriptions, nil
+}
+
+// readNodeConfigsv2alpha1 reads all the CiliumNodeConfig in v2alpha1 objects and returns a flattened map
+// of any key overrides that apply to this node.
+// TODO depreciate CNC on v2alpha1 https://github.com/cilium/cilium/issues/31982
+func readNodeConfigsv2alpha1(ctx context.Context, client client.Clientset, nodeName, namespace, name string) (map[string]string, []string, error) {
+	var overrides []ciliumv2alpha1.CiliumNodeConfig
+
+	// Retrieve CNCs if the name is not provided
+	if name == "" {
+		l, err := client.CiliumV2alpha1().CiliumNodeConfigs(namespace).List(ctx, metav1.ListOptions{})
+		if apierrors.IsNotFound(err) { // Tolerate the CRD not existing
+			return nil, nil, nil
+		}
+		if err != nil {
+			return nil, nil, fmt.Errorf("could not list CiliumNodeConfig v2alpha1 objects: %w", err)
+		}
+		overrides = l.Items
+	} else {
+		// Retrieve CNCs with the given name
+		o, err := client.CiliumV2alpha1().CiliumNodeConfigs(namespace).Get(ctx, name, metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			return nil, nil, nil
+		}
+		if err != nil {
+			return nil, nil, fmt.Errorf("could not retrieve CiliumNodeConfig v2alpha1 %s/%s: %w", namespace, name, err)
+		}
+		overrides = append(overrides, *o)
+	}
+
+	if len(overrides) == 0 {
+		return nil, nil, nil
+	}
+
+	// If there are overrides, retrieve our node.
+	// We'll need it to match label selectors
+	node, err := client.CoreV1().Nodes().Get(ctx, nodeName, metav1.GetOptions{})
+	if err != nil {
+		return nil, nil, fmt.Errorf("could not get Node %s: %w", nodeName, err)
+	}
+
+	matching := map[string]ciliumv2alpha1.CiliumNodeConfig{}
+
+	// track names separately, since we will compute "priority" by lexicographic sort
+	var matchingNames []string
+
+	for _, override := range overrides {
+		// ignore empty overrides
+		if len(override.Spec.Defaults) == 0 {
+			continue
+		}
+
+		// if we're selecting on a list, then evaluate the node selector
+		if name == "" && override.Spec.NodeSelector != nil {
+			ls, err := metav1.LabelSelectorAsSelector(override.Spec.NodeSelector)
+			if err != nil { // unreachable
+				return nil, nil, fmt.Errorf("invalid selector in CiliumNodeConfig v2alpha1 %s: %w", override.Name, err)
+			}
+			if ls.Matches(labels.Set(node.Labels)) {
+				matching[override.Name] = override
+				matchingNames = append(matchingNames, override.Name)
+			}
+		} else if name != "" {
+			matching[override.Name] = override
+			matchingNames = append(matchingNames, override.Name)
+		}
+	}
+
+	// Within overrides, lexicograpical ordering determines priority.
+	sort.Strings(matchingNames)
+
+	out := make(map[string]string)
+	for _, name := range matchingNames {
+		for k, v := range matching[name].Spec.Defaults {
+			if errs := apivalidation.IsConfigMapKey(k); len(errs) > 0 {
+				log.WithFields(logrus.Fields{
+					logfields.ConfigKey: k,
+				}).Errorf("Invalid key in CiliumNodeConfigs v2alpha1 %s/%s: %v", matching[name].Namespace, name, k)
+				continue
+			}
+			if _, set := out[k]; set {
+				log.WithFields(logrus.Fields{
+					logfields.ConfigKey: k,
+				}).Infof("Key %s set in multiple CiliumNodeConfigs v2alpha1", k)
+			}
+			out[k] = v
+		}
+	}
+
+	var sourceDescriptions []string
 	for _, name := range matchingNames {
 		sourceDescriptions = append(sourceDescriptions, fmt.Sprintf("%s:%s/%s", KindNodeConfig, namespace, name))
 	}

.github/actions/ginkgo/main-focus.yaml
@@ -44,7 +44,8 @@ include:
   # K8sAgentFQDNTest Restart Cilium validate that FQDN is still working
   # K8sAgentFQDNTest Validate that FQDN policy continues to work after being updated
   # K8sAgentFQDNTest Validate that multiple specs are working correctly
-  # K8sAgentPerNodeConfigTest Correctly computes config overrides
+  # K8sAgentPerNodeConfigTest Correctly computes config overrides with CNC v2alpha1
+  # K8sAgentPerNodeConfigTest Correctly computes config overrides with CNC v2
   - focus: "f02-agent-fqdn"
     cliFocus: "K8sAgentFQDNTest|K8sAgentPerNodeConfigTest"
 

Makefile
@@ -246,7 +246,8 @@ CRDS_CILIUM_V2 := ciliumnetworkpolicies \
                   ciliumlocalredirectpolicies \
                   ciliumegressgatewaypolicies \
                   ciliumenvoyconfigs \
-                  ciliumclusterwideenvoyconfigs
+                  ciliumclusterwideenvoyconfigs \
+                  ciliumnodeconfigs
 CRDS_CILIUM_V2ALPHA1 := ciliumendpointslices \
                         ciliumbgppeeringpolicies \
                         ciliumbgpclusterconfigs \
@@ -255,7 +256,6 @@ CRDS_CILIUM_V2ALPHA1 := ciliumendpointslices \
                         ciliumbgpnodeconfigs \
                         ciliumbgpnodeconfigoverrides \
                         ciliumloadbalancerippools \
-                        ciliumnodeconfigs \
                         ciliumcidrgroups \
                         ciliuml2announcementpolicies \
                         ciliumpodippools

pkg/k8s/apis/cilium.io/client/crds/v2/ciliumnodeconfigs.yaml
@@ -17,7 +17,7 @@ spec:
     singular: ciliumnodeconfig
   scope: Namespaced
   versions:
-  - name: v2alpha1
+  - name: v2
     schema:
       openAPIV3Schema:
         description: "CiliumNodeConfig is a list of configuration key-value pairs.
@@ -106,6 +106,98 @@ spec:
         type: object
     served: true
     storage: true
+  - deprecated: true
+    deprecationWarning: cilium.io/v2alpha1 CiliumNodeConfig will be deprecated in
+      cilium v1.16; use cilium.io/v2 CiliumNodeConfig
+    name: v2alpha1
+    schema:
+      openAPIV3Schema:
+        description: "CiliumNodeConfig is a list of configuration key-value pairs.
+          It is applied to nodes indicated by a label selector. \n If multiple overrides
+          apply to the same node, they will be ordered by name with later Overrides
+          overwriting any conflicting keys."
+        properties:
+          apiVersion:
+            description: 'APIVersion defines the versioned schema of this representation
+              of an object. Servers should convert recognized schemas to the latest
+              internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources'
+            type: string
+          kind:
+            description: 'Kind is a string value representing the REST resource this
+              object represents. Servers may infer this from the endpoint the client
+              submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds'
+            type: string
+          metadata:
+            type: object
+          spec:
+            description: Spec is the desired Cilium configuration overrides for a
+              given node
+            properties:
+              defaults:
+                additionalProperties:
+                  type: string
+                description: Defaults is treated the same as the cilium-config ConfigMap
+                  - a set of key-value pairs parsed by the agent and operator processes.
+                  Each key must be a valid config-map data field (i.e. a-z, A-Z, -,
+                  _, and .)
+                type: object
+              nodeSelector:
+                description: NodeSelector is a label selector that determines to which
+                  nodes this configuration applies. If not supplied, then this config
+                  applies to no nodes. If empty, then it applies to all nodes.
+                properties:
+                  matchExpressions:
+                    description: matchExpressions is a list of label selector requirements.
+                      The requirements are ANDed.
+                    items:
+                      description: A label selector requirement is a selector that
+                        contains values, a key, and an operator that relates the key
+                        and values.
+                      properties:
+                        key:
+                          description: key is the label key that the selector applies
+                            to.
+                          type: string
+                        operator:
+                          description: operator represents a key's relationship to
+                            a set of values. Valid operators are In, NotIn, Exists
+                            and DoesNotExist.
+                          type: string
+                        values:
+                          description: values is an array of string values. If the
+                            operator is In or NotIn, the values array must be non-empty.
+                            If the operator is Exists or DoesNotExist, the values
+                            array must be empty. This array is replaced during a strategic
+                            merge patch.
+                          items:
+                            type: string
+                          type: array
+                          x-kubernetes-list-type: atomic
+                      required:
+                      - key
+                      - operator
+                      type: object
+                    type: array
+                    x-kubernetes-list-type: atomic
+                  matchLabels:
+                    additionalProperties:
+                      type: string
+                    description: matchLabels is a map of {key,value} pairs. A single
+                      {key,value} in the matchLabels map is equivalent to an element
+                      of matchExpressions, whose key field is "key", the operator
+                      is "In", and the values array contains only "value". The requirements
+                      are ANDed.
+                    type: object
+                type: object
+            required:
+            - defaults
+            - nodeSelector
+            type: object
+        required:
+        - spec
+        type: object
+    served: true
+    storage: false
 status:
   acceptedNames:
     kind: ""

pkg/k8s/apis/cilium.io/client/register.go
@@ -62,6 +62,9 @@ const (
 	// CECCRDName is the full name of the CEC CRD.
 	CECCRDName = k8sconstv2.CECKindDefinition + "/" + k8sconstv2.CustomResourceDefinitionVersion
 
+	// CNCCRDName is the full name of the CiliumNodeConfig CRD.
+	CNCCRDName = k8sconstv2.CNCKindDefinition + "/" + k8sconstv2.CustomResourceDefinitionVersion
+
 	// BGPPCRDName is the full name of the BGPP CRD.
 	BGPPCRDName = k8sconstv2alpha1.BGPPKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
 
@@ -83,8 +86,9 @@ const (
 	// LBIPPoolCRDName is the full name of the BGPPool CRD.
 	LBIPPoolCRDName = k8sconstv2alpha1.PoolKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
 
-	// CNCCRDName is the full name of the CiliumNodeConfig CRD.
-	CNCCRDName = k8sconstv2alpha1.CNCKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
+	// CNCCRDNameAlpha is the full name of the CiliumNodeConfig CRD.
+	// TODO remove me when CNC CRD v2alpha1 will be deprecated.
+	CNCCRDNameAlpha = k8sconstv2alpha1.CNCKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
 
 	// CCGCRDName is the full name of the CiliumCIDRGroup CRD.
 	CCGCRDName = k8sconstv2alpha1.CCGKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
@@ -104,7 +108,7 @@ type CRDList struct {
 	FullName string
 }
 
-// Returns a map of CRDs
+// CustomResourceDefinitionList returns a map of CRDs
 func CustomResourceDefinitionList() map[string]*CRDList {
 	return map[string]*CRDList{
 		synced.CRDResourceName(k8sconstv2.CNPName): {
@@ -138,6 +142,10 @@ func CustomResourceDefinitionList() map[string]*CRDList {
 		synced.CRDResourceName(k8sconstv2.CEGPName): {
 			Name:     CEGPCRDName,
 			FullName: k8sconstv2.CEGPName,
+		},
+		synced.CRDResourceName(k8sconstv2.CNCName): {
+			Name:     CNCCRDName,
+			FullName: k8sconstv2.CNCName,
 		},
 		synced.CRDResourceName(k8sconstv2alpha1.CESName): {
 			Name:     CESCRDName,
@@ -179,8 +187,9 @@ func CustomResourceDefinitionList() map[string]*CRDList {
 			Name:     LBIPPoolCRDName,
 			FullName: k8sconstv2alpha1.LBIPPoolName,
 		},
+		// TODO remove me when CNC v2alpha 1 will be deprecated
 		synced.CRDResourceName(k8sconstv2alpha1.CNCName): {
-			Name:     CNCCRDName,
+			Name:     CNCCRDNameAlpha,
 			FullName: k8sconstv2alpha1.CNCName,
 		},
 		synced.CRDResourceName(k8sconstv2alpha1.CCGName): {
@@ -243,6 +252,9 @@ var (
 	//go:embed crds/v2/ciliumegressgatewaypolicies.yaml
 	crdsv2Ciliumegressgatewaypolicies []byte
 
+	//go:embed crds/v2/ciliumnodeconfigs.yaml
+	crdsv2CiliumNodeConfigs []byte
+
 	//go:embed crds/v2alpha1/ciliumendpointslices.yaml
 	crdsv2Alpha1Ciliumendpointslices []byte
 
@@ -272,9 +284,6 @@ var (
 
 	//go:embed crds/v2alpha1/ciliumloadbalancerippools.yaml
 	crdsv2Alpha1Ciliumloadbalancerippools []byte
-
-	//go:embed crds/v2alpha1/ciliumnodeconfigs.yaml
-	crdsv2Alpha1CiliumNodeConfigs []byte
 
 	//go:embed crds/v2alpha1/ciliumcidrgroups.yaml
 	crdsv2Alpha1CiliumCIDRGroups []byte
@@ -335,8 +344,10 @@ func GetPregeneratedCRD(crdName string) apiextensionsv1.CustomResourceDefinition
 		crdBytes = crdsv2Alpha1Ciliumbgpnodeconfigoverrides
 	case LBIPPoolCRDName:
 		crdBytes = crdsv2Alpha1Ciliumloadbalancerippools
+	case CNCCRDNameAlpha:
+		crdBytes = crdsv2CiliumNodeConfigs
 	case CNCCRDName:
-		crdBytes = crdsv2Alpha1CiliumNodeConfigs
+		crdBytes = crdsv2CiliumNodeConfigs
 	case CCGCRDName:
 		crdBytes = crdsv2Alpha1CiliumCIDRGroups
 	case L2AnnouncementCRDName:
@@ -391,8 +402,9 @@ func constructV1CRD(
 				ShortNames: template.Spec.Names.ShortNames,
 				Singular:   template.Spec.Names.Singular,
 			},
-			Scope:    template.Spec.Scope,
-			Versions: template.Spec.Versions,
+			Scope:      template.Spec.Scope,
+			Versions:   template.Spec.Versions,
+			Conversion: template.Spec.Conversion, // conversion strategy is needed to support several versions of a same CRD
 		},
 	}
 }

pkg/k8s/apis/cilium.io/v2/cnc_types.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package v2alpha1
+package v2
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -11,8 +11,8 @@ import (
 //+k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 //+kubebuilder:resource:categories={cilium}
 //+kubebuilder:object:root=true
-//+kubebuilder:deprecatedversion:warning="cilium.io/v2alpha1 CiliumNodeConfig will be deprecated in cilium v1.16; use cilium.io/v2 CiliumNodeConfig"
 //+deepequal-gen=false
+//+kubebuilder:storageversion
 
 // CiliumNodeConfig is a list of configuration key-value pairs. It is applied to
 // nodes indicated by a label selector.

pkg/k8s/apis/cilium.io/v2/register.go
@@ -127,6 +127,17 @@ const (
 
 	// CECName is the full name of Cilium Envoy Config
 	CECName = CECPluralName + "." + CustomResourceDefinitionGroup
+
+	// CiliumNodeConfig (CNC)
+
+	// CNCPluralName is the plural name of Cilium Node Config
+	CNCPluralName = "ciliumnodeconfigs"
+
+	// CNCKindDefinition is the kind name of Cilium Node Config
+	CNCKindDefinition = "CiliumNodeConfig"
+
+	// CNCName is the full name of Cilium Node Config
+	CNCName = CNCPluralName + "." + CustomResourceDefinitionGroup
 )
 
 // SchemeGroupVersion is group version used to register these objects
@@ -180,6 +191,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&CiliumEndpointList{},
 		&CiliumNode{},
 		&CiliumNodeList{},
+		&CiliumNodeConfig{},
+		&CiliumNodeConfigList{},
 		&CiliumExternalWorkload{},
 		&CiliumExternalWorkloadList{},
 		&CiliumIdentity{},

pkg/k8s/apis/cilium.io/v2/zz_generated.deepcopy.go
@@ -12,6 +12,7 @@ import (
 	models "github.com/cilium/cilium/api/v1/models"
 	v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	api "github.com/cilium/cilium/pkg/policy/api"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -829,6 +830,94 @@ func (in *CiliumNode) DeepCopyObject() runtime.Object {
 		return c
 	}
 	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CiliumNodeConfig) DeepCopyInto(out *CiliumNodeConfig) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CiliumNodeConfig.
+func (in *CiliumNodeConfig) DeepCopy() *CiliumNodeConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(CiliumNodeConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *CiliumNodeConfig) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CiliumNodeConfigList) DeepCopyInto(out *CiliumNodeConfigList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]CiliumNodeConfig, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CiliumNodeConfigList.
+func (in *CiliumNodeConfigList) DeepCopy() *CiliumNodeConfigList {
+	if in == nil {
+		return nil
+	}
+	out := new(CiliumNodeConfigList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *CiliumNodeConfigList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CiliumNodeConfigSpec) DeepCopyInto(out *CiliumNodeConfigSpec) {
+	*out = *in
+	if in.Defaults != nil {
+		in, out := &in.Defaults, &out.Defaults
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	if in.NodeSelector != nil {
+		in, out := &in.NodeSelector, &out.NodeSelector
+		*out = new(metav1.LabelSelector)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CiliumNodeConfigSpec.
+func (in *CiliumNodeConfigSpec) DeepCopy() *CiliumNodeConfigSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(CiliumNodeConfigSpec)
+	in.DeepCopyInto(out)
+	return out
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.

commit 9a2bf3703aad061f0a417bb97c962e6ea309a121

option/resolver: Use structured logging for errors
Fixes: db0d41af34 ("crd, option: Make CiliumNodeConfig available on v2 API")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/option/resolver/resolver.go
@@ -238,12 +238,12 @@ func readNodeConfigsAllVersions(ctx context.Context, client client.Clientset, no
 
 	nodeConfigv2, descv2, errv2 := readNodeConfigs(ctx, client, nodeName, namespace, name)
 	if errv2 != nil {
-		log.WithFields(logrus.Fields{logfields.Node: nodeName}).Errorf("CiliumNodeConfig v2 not found: %s", errv2)
+		log.WithError(errv2).WithFields(logrus.Fields{logfields.Node: nodeName}).Error("CiliumNodeConfig v2 not found when reading configuration")
 	}
 
 	nodeConfigv2alpha1, descv2alpha1, errv2alpha1 := readNodeConfigsv2alpha1(ctx, client, nodeName, namespace, name)
 	if errv2alpha1 != nil {
-		log.WithFields(logrus.Fields{logfields.Node: nodeName}).Errorf("CiliumNodeConfig v2alpha1 not found: %s", errv2alpha1)
+		log.WithError(errv2alpha1).WithFields(logrus.Fields{logfields.Node: nodeName}).Errorf("CiliumNodeConfig v2alpha1 not found when reading configuration")
 		// return the errors for the two versions
 		if errv2 != nil {
 			msg := fmt.Sprintf("CiliumNodeConfig v2 and v2alpha1 not found: %s and %s\n", errv2, errv2alpha1)