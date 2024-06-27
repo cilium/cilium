c8a4fa3fc0f1081fe45c7ce424eaffb15be8c2ae
gray <greyschwinger@gmail.com>
2024-06-27 19:11:10 +0800

    Attach to-wireguard to the tc egress of cilium_wg0
    This is to handle rev-DNAT when L7 ingress proxy is enabled with
    wireguard, nodeport, native routing, and KPR.
    
    Consider a pod-to-remote-nodeport connection, the reply packet will
    pass: from_lxc@veth -> redirected to L7 proxy -> from_host@cilium_host
    -> to_host@cilium_net -> to_netdev@eth0 -> redirected to cilium_wg0.
    This patch ensures the new attached to-wireguard@cilium_wg0 can do the
    necessary rev-DNAT.
    
    Fixes: https://github.com/cilium/cilium/issues/32899
    
    Signed-off-by: gray <gray.liang@isovalent.com>

---
+++ bpf/bpf_wireguard.c
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#define ETH_HLEN 0
+#define IS_BPF_WIREGUARD 1
+
+#include <bpf/ctx/skb.h>
+#include <bpf/api.h>
+
+#include <node_config.h>
+#include <netdev_config.h>
+
+#include "lib/trace.h"
+#include "lib/drop.h"
+#include "lib/nodeport.h"
+
+/* to-wireguard is attached as a tc egress filter to the cilium_wg0 device.
+ */
+__section_entry
+int cil_to_wireguard(struct __ctx_buff *ctx)
+{
+	int __maybe_unused ret;
+	__s8 __maybe_unused ext_err = 0;
+	__u16 __maybe_unused proto = ctx_get_protocol(ctx);
+	__u32 __maybe_unused src_sec_identity = get_identity(ctx);
+
+	struct trace_ctx __maybe_unused trace = {
+		.reason = TRACE_REASON_UNKNOWN,
+		.monitor = 0,
+	};
+
+	bpf_clear_meta(ctx);
+
+#ifdef ENABLE_NODEPORT
+	ret = handle_nat_fwd(ctx, 0, proto, &trace, &ext_err);
+	if (IS_ERR(ret))
+		return send_drop_notify_error_ext(ctx, src_sec_identity, ret, ext_err,
+						  CTX_ACT_DROP, METRIC_EGRESS);
+#endif /* ENABLE_NODEPORT */
+
+	return TC_ACT_OK;
+}
+
+BPF_LICENSE("Dual BSD/GPL");
