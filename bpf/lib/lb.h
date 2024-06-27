f8367055fd7aca1b6b95ae9b804a0e4d6f11c535
Gilberto Bertin <jibi@cilium.io>
2024-06-27 17:20:19 +0200

    service: differentiate UDP and TCP protocols
    this commit updates the service logic, from the agent all the way down
    to the datapath, to take into account the L4 protocols specified in a
    k8s service.
    
    Historically Cilium has always been ignoring, at least from a end user
    perspective, the protocol specified in a k8s service, resulting in the
    inability to expose a port only for a given protocol, or in the opposite
    case, resulting in a single protocol-agnostic service exposed whenever
    2 TCP and UDP services on the same port were configured.
    
    Co-authored-by: Nate Sweet <nathanjsweet@pm.me>
    Co-authored-by: Antonio Ojea <aojea@google.com>
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- bpf/lib/lb.h
+++ bpf/lib/lb.h
@@ -527,8 +527,7 @@ static __always_inline int lb6_rev_nat(struct __ctx_buff *ctx, int l4_off,
 static __always_inline void
 lb6_fill_key(struct lb6_key *key, struct ipv6_ct_tuple *tuple)
 {
-	/* FIXME: set after adding support for different L4 protocols in LB */
-	key->proto = 0;
+	key->proto = tuple->nexthdr;
 	ipv6_addr_copy(&key->address, &tuple->daddr);
 	key->dport = tuple->sport;
 }
@@ -1201,8 +1200,7 @@ static __always_inline int lb4_rev_nat(struct __ctx_buff *ctx, int l3_off, int l
 static __always_inline void
 lb4_fill_key(struct lb4_key *key, const struct ipv4_ct_tuple *tuple)
 {
-	/* FIXME: set after adding support for different L4 protocols in LB */
-	key->proto = 0;
+	key->proto = tuple->nexthdr;
 	key->address = tuple->daddr;
 	/* CT tuple has ports in reverse order: */
 	key->dport = tuple->sport;
