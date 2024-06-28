cilium/cilium

commit ad09e20df402127cd88fa7293c560a107ccd816c

bpf: Skip LB for local redirect svc tuples
This commit revises the socket-LB datapath to
skip load-balancing for traffic matching user
configured local redirect policies with the
skipRedirectFromBackend flag.

Old logic:
Previously, this was enforced by default for all local redirect
services with the help of bpf_sk_lookup_* helpers. The intention was
to identify cases where traffic from an LRP backend pod was getting
redirected to itself using the socket lookup helpers. However, this
logic led to cases where LB was skipped erroneously based on the
unexpected behavior of the sk_lookup API implementation. Particularly,
the logic intended to get established sockets matching passed ip/port
tuple, but the kernel API can also return listening sockets matching
just the passed port [1]. As a result, when a client pod has a process
listening on the same port as deployed LRP(s), traffic from the pod
destined to an LRP service frontend was not load-balanced, thereby
skipping the desired node-local redirection.

New logic:
Skip LB only for socket tuples matching user configured local
redirect policy tuples populated in the cilium_skip_lb{4,6} maps.
The maps are populated by the userspace based on user deployed
LRPs.
The BPF cgroup hooks don't have access to the source
IP address set in the BPF socket `ctx`, but it needs some
kind of identifier to skip LB only for traffic from source
entities selected by user configured LRPs. To that end, we
use the netns cookie retrieved from the 'ctx' as this identifier.

[1] https://elixir.bootlin.com/linux/v6.8/source/include/net/inet_hashtables.h#L425

Signed-off-by: Aditi Ghag <aditi@cilium.io>

bpf/bpf_sock.c
@@ -12,6 +12,7 @@
 
 #include "lib/common.h"
 #include "lib/lb.h"
+#include "lib/endian.h"
 #include "lib/eps.h"
 #include "lib/identity.h"
 #include "lib/metrics.h"
@@ -130,6 +131,15 @@ struct {
 	__uint(pinning, LIBBPF_PIN_BY_NAME);
 	__uint(max_entries, LB4_REVERSE_NAT_SK_MAP_SIZE);
 } LB4_REVERSE_NAT_SK_MAP __section_maps_btf;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, struct skip_lb4_key);
+	__type(value, __u8);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, CILIUM_LB_SKIP_MAP_MAX_ENTRIES);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+} LB4_SKIP_MAP __section_maps_btf;
 
 static __always_inline int sock4_update_revnat(struct bpf_sock_addr *ctx,
 					       const struct lb4_backend *backend,
@@ -256,31 +266,25 @@ sock4_wildcard_lookup_full(struct lb4_key *key __maybe_unused,
  * service translation.
  */
 static __always_inline bool
-sock4_skip_xlate_if_same_netns(struct bpf_sock_addr *ctx __maybe_unused,
-			       const struct lb4_backend *backend __maybe_unused)
+sock4_skip_xlate_from_ctx_to_svc(struct bpf_sock_addr *ctx __maybe_unused,
+				 __be32 address __maybe_unused, __be16 port __maybe_unused)
 {
-	struct bpf_sock_tuple tuple = {
-		.ipv4.daddr = backend->address,
-		.ipv4.dport = backend->port,
-	};
-	struct bpf_sock *sk = NULL;
+#ifdef HAVE_NETNS_COOKIE
+	__net_cookie cookie = get_netns_cookie(ctx);
+	struct skip_lb4_key key;
+	__u8 *val = NULL;
 
-	switch (ctx->protocol) {
-	case IPPROTO_TCP:
-		sk = sk_lookup_tcp(ctx, &tuple, sizeof(tuple.ipv4),
-				   BPF_F_CURRENT_NETNS, 0);
-		break;
-	case IPPROTO_UDP:
-		sk = sk_lookup_udp(ctx, &tuple, sizeof(tuple.ipv4),
-				   BPF_F_CURRENT_NETNS, 0);
-		break;
-	}
-
-	if (sk) {
-		sk_release(sk);
+	memset(&key, 0, sizeof(key));
+	key.netns_cookie = cookie;
+	key.address = address;
+	key.port = port;
+	val = map_lookup_elem(&LB4_SKIP_MAP, &key);
+	if (val)
 		return true;
-	}
 	return false;
+#else
+	return false;
+#endif
 }
 #endif /* ENABLE_LOCAL_REDIRECT_POLICY */
 
@@ -334,6 +338,12 @@ static __always_inline int __sock4_xlate_fwd(struct bpf_sock_addr *ctx,
 	 */
 	if (sock4_skip_xlate(svc, orig_key.address))
 		return -EPERM;
+
+#ifdef ENABLE_LOCAL_REDIRECT_POLICY
+	if (lb4_svc_is_localredirect(svc) &&
+	    sock4_skip_xlate_from_ctx_to_svc(ctx_full, orig_key.address, orig_key.dport))
+		return -ENXIO;
+#endif /* ENABLE_LOCAL_REDIRECT_POLICY */
 
 #ifdef ENABLE_L7_LB
 	/* Do not perform service translation at socker layer for
@@ -406,12 +416,6 @@ static __always_inline int __sock4_xlate_fwd(struct bpf_sock_addr *ctx,
 		update_metrics(0, METRIC_EGRESS, REASON_LB_NO_BACKEND);
 		return -EHOSTUNREACH;
 	}
-
-#ifdef ENABLE_LOCAL_REDIRECT_POLICY
-	if (lb4_svc_is_localredirect(svc) &&
-	    sock4_skip_xlate_if_same_netns(ctx_full, backend))
-		return -ENXIO;
-#endif /* ENABLE_LOCAL_REDIRECT_POLICY */
 
 	if (lb4_svc_is_affinity(svc) && !backend_from_affinity)
 		lb4_update_affinity_by_netns(svc, &id, backend_id);
@@ -654,6 +658,14 @@ struct {
 	__uint(max_entries, LB6_REVERSE_NAT_SK_MAP_SIZE);
 } LB6_REVERSE_NAT_SK_MAP __section_maps_btf;
 
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, struct skip_lb6_key);
+	__type(value, __u8);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, CILIUM_LB_SKIP_MAP_MAX_ENTRIES);
+} LB6_SKIP_MAP __section_maps_btf;
+
 static __always_inline int sock6_update_revnat(struct bpf_sock_addr *ctx,
 					       const struct lb6_backend *backend,
 					       const struct lb6_key *orig_key,
@@ -677,6 +689,33 @@ static __always_inline int sock6_update_revnat(struct bpf_sock_addr *ctx,
 				      &val, 0);
 	return ret;
 }
+
+static __always_inline void ctx_get_v6_address(const struct bpf_sock_addr *ctx,
+					       union v6addr *addr);
+
+#ifdef ENABLE_LOCAL_REDIRECT_POLICY
+static __always_inline bool
+sock6_skip_xlate_from_ctx_to_svc(struct bpf_sock_addr *ctx __maybe_unused,
+				 const __be16 port __maybe_unused)
+{
+#ifdef HAVE_NETNS_COOKIE
+	__net_cookie cookie = get_netns_cookie(ctx);
+	struct skip_lb6_key key;
+	__u8 *val = NULL;
+
+	memset(&key, 0, sizeof(key));
+	key.netns_cookie = cookie;
+	ctx_get_v6_address(ctx, &key.address);
+	key.port = port;
+	val = map_lookup_elem(&LB6_SKIP_MAP, &key);
+	if (val)
+		return true;
+	return false;
+#else
+	return false;
+#endif
+}
+#endif /* ENABLE_LOCAL_REDIRECT_POLICY */
 #endif /* ENABLE_IPV6 */
 
 static __always_inline void ctx_get_v6_address(const struct bpf_sock_addr *ctx,
@@ -1010,6 +1049,12 @@ static __always_inline int __sock6_xlate_fwd(struct bpf_sock_addr *ctx,
 
 	if (sock6_skip_xlate(svc, &orig_key.address))
 		return -EPERM;
+
+#ifdef ENABLE_LOCAL_REDIRECT_POLICY
+	if (lb6_svc_is_localredirect(svc) &&
+	    sock6_skip_xlate_from_ctx_to_svc(ctx, orig_key.dport))
+		return -ENXIO;
+#endif /* ENABLE_LOCAL_REDIRECT_POLICY */
 
 #ifdef ENABLE_L7_LB
 	/* See __sock4_xlate_fwd for commentary. */

bpf/lib/common.h
@@ -1243,6 +1243,21 @@ struct lpm_val {
 	__u8 flags;
 };
 
+struct skip_lb4_key {
+	__u64 netns_cookie;     /* Source pod netns cookie */
+	__u32 address;          /* Destination service virtual IPv4 address */
+	__u16 port;             /* Destination service virtual layer4 port */
+	__u16 pad;
+};
+
+struct skip_lb6_key {
+	__u64 netns_cookie;     /* Source pod netns cookie */
+	union v6addr address;   /* Destination service virtual IPv6 address */
+	__u32 pad;
+	__u16 port;             /* Destination service virtual layer4 port */
+	__u16 pad2;
+};
+
 /* Older kernels don't support the larger tunnel key structure and we don't
  * need it since we only want to retrieve the tunnel ID anyway.
  */

bpf/lib/lb.h
@@ -348,6 +348,12 @@ bool lb4_svc_is_localredirect(const struct lb4_service *svc)
 {
 	return svc->flags2 & SVC_FLAG_LOCALREDIRECT;
 }
+
+static __always_inline
+bool lb6_svc_is_localredirect(const struct lb6_service *svc)
+{
+	return svc->flags2 & SVC_FLAG_LOCALREDIRECT;
+}
 #endif /* ENABLE_LOCAL_REDIRECT_POLICY */
 
 static __always_inline

bpf/node_config.h
@@ -191,6 +191,7 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 #define CILIUM_LB_AFFINITY_MAP_MAX_ENTRIES	65536
 #define CILIUM_LB_REV_NAT_MAP_MAX_ENTRIES	65536
 #define CILIUM_LB_MAGLEV_MAP_MAX_ENTRIES	65536
+#define CILIUM_LB_SKIP_MAP_MAX_ENTRIES		100
 #define POLICY_MAP_SIZE 16384
 #define AUTH_MAP_SIZE 512000
 #define CONFIG_MAP_SIZE 256

commit 4624bba40fb2a17ac981784d9f03e7a271242880

bpf: Remove stale comment
The logic was revised in commit ad09e20df4, so
remove the stale comment.

Fixes: ad09e20df4 (bpf: Skip LB for local redirect svc tuples)
Signed-off-by: Aditi Ghag <aditi@cilium.io>

bpf/bpf_sock.c
@@ -261,9 +261,7 @@ sock4_wildcard_lookup_full(struct lb4_key *key __maybe_unused,
  * backend Pod. When traffic destined to the frontend originates from the kiam
  * Pod in namespace ns1 (host ns when the kiam proxy Pod is deployed in
  * hostNetwork mode or regular Pod ns) and the Pod is selected as a backend, the
- * traffic would get looped back to the proxy Pod. Identify such cases by doing
- * a socket lookup for the backend <ip, port> in its namespace, ns1, and skip
- * service translation.
+ * traffic would get looped back to the proxy Pod.
  */
 static __always_inline bool
 sock4_skip_xlate_from_ctx_to_svc(struct bpf_sock_addr *ctx __maybe_unused,