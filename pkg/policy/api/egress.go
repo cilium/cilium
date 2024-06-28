cilium/cilium

commit e97df7badd74e6a666710846a781ba78bec84a41

policy: Do not select any identity with ingress empty slices
In case of L4 ingress policies with an explicit (non-nil) empty slice in
either:

- fromEndpoints
- fromCIDR
- fromCIDRSet
- fromEntities

no identities should be selected, thus falling back to default deny for
an allow policy.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/policy/api/ingress.go
@@ -175,12 +175,21 @@ type IngressDenyRule struct {
 // FromEndpoints is not aggregated due to requirement folding in
 // GetSourceEndpointSelectorsWithRequirements()
 func (i *IngressCommonRule) SetAggregatedSelectors() {
+	// Goroutines can race setting i.aggregatedSelectors, but they will all compute the same result, so it does not matter.
+
+	// explicitly check for empty non-nil slices, it should not result in any identity being selected.
+	if (i.FromCIDR != nil && len(i.FromCIDR) == 0) ||
+		(i.FromCIDRSet != nil && len(i.FromCIDRSet) == 0) ||
+		(i.FromEntities != nil && len(i.FromEntities) == 0) {
+		i.aggregatedSelectors = nil
+		return
+	}
+
 	res := make(EndpointSelectorSlice, 0, len(i.FromEntities)+len(i.FromCIDR)+len(i.FromCIDRSet))
 	res = append(res, i.FromEntities.GetAsEndpointSelectors()...)
 	res = append(res, i.FromCIDR.GetAsEndpointSelectors()...)
 	res = append(res, i.FromCIDRSet.GetAsEndpointSelectors()...)
-	// Goroutines can race setting this, but they will all compute
-	// the same result, so it does not matter.
+
 	i.aggregatedSelectors = res
 }
 
@@ -190,6 +199,12 @@ func (i *IngressCommonRule) GetSourceEndpointSelectorsWithRequirements(requireme
 	if i.aggregatedSelectors == nil {
 		i.SetAggregatedSelectors()
 	}
+
+	// explicitly check for empty non-nil slices, it should not result in any identity being selected.
+	if i.aggregatedSelectors == nil || (i.FromEndpoints != nil && len(i.FromEndpoints) == 0) {
+		return nil
+	}
+
 	res := make(EndpointSelectorSlice, 0, len(i.FromEndpoints)+len(i.aggregatedSelectors))
 	if len(requirements) > 0 && len(i.FromEndpoints) > 0 {
 		for idx := range i.FromEndpoints {

pkg/endpoint/redirect_test.go
@@ -322,10 +322,8 @@ var (
 	ruleL4L7Allow = api.NewRule().
 			WithLabels(lblsL4L7Allow).
 			WithIngressRules([]api.IngressRule{{
-			IngressCommonRule: api.IngressCommonRule{
-				FromEndpoints: []api.EndpointSelector{},
-			},
-			ToPorts: combineL4L7(allowPort80, allowHTTPRoot),
+			IngressCommonRule: api.IngressCommonRule{},
+			ToPorts:           combineL4L7(allowPort80, allowHTTPRoot),
 		}})
 
 	AllowAnyEgressLabels = labels.LabelArray{labels.NewLabel(policy.LabelKeyPolicyDerivedFrom,

pkg/policy/l4_filter_deny_test.go
@@ -26,14 +26,14 @@ import (
 // |Case | L3 (1, 2) match | L4 match | Notes                                                |
 // +=====+=================+==========+======================================================+
 // |  1A |      *, *       |  80/TCP  | Deny all communication on the specified port         |
-// |  1B |      *, *       |  80/TCP  | Same as 1A, with implicit L3 wildcards               |
+// |  1B |      -, -       |  80/TCP  | Don't select anything                                |
 // |  2A |   "id=a", *     |  80/TCP  | Rule 2 is a superset of rule 1                       |
 // |  2B |   *, "id=a"     |  80/TCP  | Same as 2A, but import in reverse order              |
 // |  3  | "id=a", "id=c"  |  80/TCP  | Deny at L4 for two distinct labels (disjoint set)    |
 // |  4A |      *, *       |  80/TCP  | Allow all communication on the specified port        |
 // |     |                 |          | and deny one endpoint selector                       |
 // |  4B |      *, *       |  80/TCP  | Same as 4A, but import in reverse order              |
-// |  5A |      *, *       |  80/TCP  | Deny all communication on a specified endpoint        |
+// |  5A |      *, *       |  80/TCP  | Deny all communication on a specified endpoint       |
 // |     |                 |          | except while wildcarding all L7 policy.              |
 // |  5B |      *, *       |  80/TCP  | Same as 5A, but import in reverse order              |
 // +-----+-----------------+----------+------------------------------------------------------+
@@ -103,7 +103,7 @@ func (ds *PolicyTestSuite) TestMergeDenyAllL3(c *C) {
 	c.Assert(len(filter.PerSelectorPolicies), Equals, 1)
 	l4IngressDenyPolicy.Detach(repo.GetSelectorCache())
 
-	// Case1B: implicitly deny all endpoints.
+	// Case1B: an empty non-nil FromEndpoints does not select any identity.
 	repo = parseAndAddRules(c, api.Rules{&api.Rule{
 		EndpointSelector: endpointSelectorA,
 		IngressDeny: []api.IngressDenyRule{
@@ -139,17 +139,9 @@ func (ds *PolicyTestSuite) TestMergeDenyAllL3(c *C) {
 
 	c.Log(buffer)
 
-	filter, ok = l4IngressDenyPolicy["80/TCP"]
-	c.Assert(ok, Equals, true)
-	c.Assert(filter.Port, Equals, 80)
-	c.Assert(filter.Ingress, Equals, true)
+	_, ok = l4IngressDenyPolicy["80/TCP"]
+	c.Assert(ok, Equals, false)
 
-	c.Assert(filter.SelectsAllEndpoints(), Equals, true)
-	c.Assert(filter.wildcard, Not(IsNil))
-	c.Assert(filter.PerSelectorPolicies[filter.wildcard].IsDeny, Equals, true)
-
-	c.Assert(filter.L7Parser, Equals, ParserTypeNone)
-	c.Assert(len(filter.PerSelectorPolicies), Equals, 1)
 	l4IngressDenyPolicy.Detach(repo.GetSelectorCache())
 }
 

pkg/policy/l4_filter_test.go
@@ -115,7 +115,7 @@ func (ds *PolicyTestSuite) TearDownSuite(c *C) {
 // |Case | L3 (1, 2) match | L4 match | L7 match (1, 2) | Notes                                                |
 // +=====+=================+==========+=================+======================================================+
 // |  1A |      *, *       |  80/TCP  |      *, *       | Allow all communication on the specified port        |
-// |  1B |      *, *       |  80/TCP  |      *, *       | Same as 1A, with implicit L3 wildcards               |
+// |  1B |      -, -       |  80/TCP  |      *, *       | Deny all with an empty FromEndpoints slice           |
 // |  2A |      *, *       |  80/TCP  |   *, "GET /"    | Rule 1 shadows rule 2                                |
 // |  2B |      *, *       |  80/TCP  |   "GET /", *    | Same as 2A, but import in reverse order              |
 // |  3  |      *, *       |  80/TCP  | "GET /","GET /" | Exactly duplicate rules (HTTP)                       |
@@ -135,7 +135,6 @@ func (ds *PolicyTestSuite) TearDownSuite(c *C) {
 // | 12  |     "id=a",     |  80/TCP  |     "GET /"     | Configure to allow localhost traffic always          |
 // +-----+-----------------+----------+-----------------+------------------------------------------------------+
 
-// Case 1: allow all at L3 in both rules, and all at L7 (duplicate rule).
 func (ds *PolicyTestSuite) TestMergeAllowAllL3AndAllowAllL7(c *C) {
 	// Case 1A: Specify WildcardEndpointSelector explicitly.
 	repo := parseAndAddRules(c, api.Rules{&api.Rule{
@@ -184,7 +183,7 @@ func (ds *PolicyTestSuite) TestMergeAllowAllL3AndAllowAllL7(c *C) {
 	c.Assert(len(filter.PerSelectorPolicies), Equals, 1)
 	l4IngressPolicy.Detach(repo.GetSelectorCache())
 
-	// Case1B: implicitly wildcard all endpoints.
+	// Case1B: an empty non-nil FromEndpoints does not select any identity.
 	repo = parseAndAddRules(c, api.Rules{&api.Rule{
 		EndpointSelector: endpointSelectorA,
 		Ingress: []api.IngressRule{
@@ -220,15 +219,9 @@ func (ds *PolicyTestSuite) TestMergeAllowAllL3AndAllowAllL7(c *C) {
 
 	c.Log(buffer)
 
-	filter, ok = l4IngressPolicy["80/TCP"]
-	c.Assert(ok, Equals, true)
-	c.Assert(filter.Port, Equals, 80)
-	c.Assert(filter.Ingress, Equals, true)
+	_, ok = l4IngressPolicy["80/TCP"]
+	c.Assert(ok, Equals, false)
 
-	c.Assert(filter.SelectsAllEndpoints(), Equals, true)
-
-	c.Assert(filter.L7Parser, Equals, ParserTypeNone)
-	c.Assert(len(filter.PerSelectorPolicies), Equals, 1)
 	l4IngressPolicy.Detach(repo.GetSelectorCache())
 }
 

pkg/policy/rule.go
@@ -359,6 +359,11 @@ func rulePortsCoverSearchContext(ports []api.PortProtocol, ctx *SearchContext) b
 func mergeIngress(policyCtx PolicyContext, ctx *SearchContext, fromEndpoints api.EndpointSelectorSlice, auth *api.Authentication, toPorts, icmp api.PortsIterator, ruleLabels labels.LabelArray, resMap L4PolicyMap) (int, error) {
 	found := 0
 
+	// short-circuit if no endpoint is selected
+	if fromEndpoints == nil {
+		return found, nil
+	}
+
 	if ctx.From != nil && len(fromEndpoints) > 0 {
 		if ctx.TraceEnabled() {
 			traceL3(ctx, fromEndpoints, "from", policyCtx.IsDeny())
@@ -469,6 +474,7 @@ func mergeIngress(policyCtx PolicyContext, ctx *SearchContext, fromEndpoints api
 		if len(fromEndpoints) == 0 {
 			fromEndpoints = api.EndpointSelectorSlice{api.WildcardEndpointSelector}
 		}
+
 		if !policyCtx.IsDeny() {
 			ctx.PolicyTrace("      Allows ICMP type %v\n", r.GetPortProtocols())
 		} else {

commit 68e810df49ca1b72f3d8111c2e7b0be58e492efa

policy/api: Fix bug that skips other selectors if ToEndpoints is empty
As reported in GH-33432, do not skip other selectors in the case
ToEndpoints is empty. For example, if the user defined a ToCIDR with an
empty ToEndpoints, then
getDestinationEndpointSelectorsWithRequirements() /
GetSourceEndpointSelectorsWithRequirements() returns nil, causing the
ToCIDRs section to be skipped. Instead, return the aggregated selectors,
which aggregates selectors such as ToCIDRs, prior to the call to the
aforementioned functions inside SetAggregatedSelectors().

Fixes: e97df7badd ("policy: Do not select any identity with ingress empty slices")
Fixes: 6ccd044b05 ("policy: Do not select any identity with egress empty slices")
Fixes: https://github.com/cilium/cilium/pull/29608

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/policy/api/egress.go
@@ -298,7 +298,7 @@ func (e *EgressCommonRule) getDestinationEndpointSelectorsWithRequirements(
 	// explicitly check for empty non-nil slices, it should not result in any identity being selected.
 	if e.aggregatedSelectors == nil || (e.ToEndpoints != nil && len(e.ToEndpoints) == 0) ||
 		(e.ToNodes != nil && len(e.ToNodes) == 0) {
-		return nil
+		return e.aggregatedSelectors
 	}
 
 	res := make(EndpointSelectorSlice, 0, len(e.ToEndpoints)+len(e.aggregatedSelectors)+len(e.ToNodes))

pkg/policy/api/ingress.go
@@ -225,7 +225,7 @@ func (i *IngressCommonRule) GetSourceEndpointSelectorsWithRequirements(requireme
 	// explicitly check for empty non-nil slices, it should not result in any identity being selected.
 	if i.aggregatedSelectors == nil || (i.FromEndpoints != nil && len(i.FromEndpoints) == 0) ||
 		(i.FromNodes != nil && len(i.FromNodes) == 0) {
-		return nil
+		return i.aggregatedSelectors
 	}
 
 	res := make(EndpointSelectorSlice, 0, len(i.FromEndpoints)+len(i.aggregatedSelectors)+len(i.FromNodes))