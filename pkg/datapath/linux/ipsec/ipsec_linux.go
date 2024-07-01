cilium/cilium

commit c28e046d4c6216dea4fddae7b4be4823e8736764

ipsec: Compute per-node-pair IPsec keys
We need to ensure the (key used, sequence number) tuple for each
encrypted packet is always unique on the network. Today that's not the
case because the key is the same for all nodes and the sequence number
starts at 0 on node reboot.

To enable this, we will derive one key per node pair from a global key
shared across all nodes. We need it per node pair and not per node
because the first message emitted from A to B shouldn't be using the
same key as the first message emitted from B to A, to satisfy the above
requirement.

To that end, for each node pair (A, B), we compute a key as follows:

    key = sha256(global_key + ip_of_a + ip_of_b)

The sha256 sum is then truncated to the expected length.

Once computed, we install the derived keys such that the key used for
encryption on node A is the same as the key used for decryption on node
B:

    Node A               <> Node B
    XFRM IN:  key(b+a)      XFRM IN:  key(a+b)
    XFRM OUT: key(a+b)      XFRM OUT: key(b+a)

Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -8,6 +8,7 @@ package ipsec
 import (
 	"bufio"
 	"context"
+	"crypto/sha256"
 	"encoding/hex"
 	"errors"
 	"fmt"
@@ -135,7 +136,7 @@ var (
 	removeStaleIPv6XFRMOnce sync.Once
 )
 
-func getIPSecKeys(ip net.IP) *ipSecKey {
+func getGlobalIPsecKey(ip net.IP) *ipSecKey {
 	ipSecLock.RLock()
 	defer ipSecLock.RUnlock()
 
@@ -144,6 +145,65 @@ func getIPSecKeys(ip net.IP) *ipSecKey {
 		key = ipSecKeysGlobal[""]
 	}
 	return key
+}
+
+// computeNodeIPsecKey computes per-node-pair IPsec keys from the global,
+// pre-shared key. The per-node-pair keys are computed with a SHA256 hash of
+// the global key, source node IP, destination node IP appended together.
+func computeNodeIPsecKey(globalKey, srcNodeIP, dstNodeIP []byte) []byte {
+	input := append(globalKey, srcNodeIP...)
+	input = append(input, dstNodeIP...)
+	output := sha256.Sum256(input)
+	return output[:len(globalKey)]
+}
+
+// deriveNodeIPsecKey builds a per-node-pair ipSecKey object from the global
+// ipSecKey object.
+func deriveNodeIPsecKey(globalKey *ipSecKey, srcNodeIP, dstNodeIP net.IP) *ipSecKey {
+	nodeKey := &ipSecKey{
+		Spi:   globalKey.Spi,
+		ReqID: globalKey.ReqID,
+	}
+
+	if globalKey.Aead != nil {
+		nodeKey.Aead = &netlink.XfrmStateAlgo{
+			Name:   globalKey.Aead.Name,
+			Key:    computeNodeIPsecKey(globalKey.Aead.Key, srcNodeIP, dstNodeIP),
+			ICVLen: globalKey.Aead.ICVLen,
+		}
+	} else {
+		nodeKey.Auth = &netlink.XfrmStateAlgo{
+			Name: globalKey.Auth.Name,
+			Key:  computeNodeIPsecKey(globalKey.Auth.Key, srcNodeIP, dstNodeIP),
+		}
+
+		nodeKey.Crypt = &netlink.XfrmStateAlgo{
+			Name: globalKey.Crypt.Name,
+			Key:  computeNodeIPsecKey(globalKey.Crypt.Key, srcNodeIP, dstNodeIP),
+		}
+	}
+
+	return nodeKey
+}
+
+// We want one IPsec key per node pair. For a pair of nodes A and B with IP
+// addresses a and b, we will therefore install two different keys:
+// Node A               <> Node B
+// XFRM IN:  key(b+a)      XFRM IN:  key(a+b)
+// XFRM OUT: key(a+b)      XFRM OUT: key(b+a)
+// This is done such that, for each pair of nodes A, B, the key used for
+// decryption on A (XFRM IN) is the same key used for encryption on B (XFRM
+// OUT), and vice versa.
+func getNodeIPsecKey(localNodeIP, remoteNodeIP net.IP, dir netlink.Dir) *ipSecKey {
+	globalKey := getGlobalIPsecKey(localNodeIP)
+	if globalKey == nil {
+		return nil
+	}
+
+	if dir == netlink.XFRM_DIR_OUT {
+		return deriveNodeIPsecKey(globalKey, localNodeIP, remoteNodeIP)
+	}
+	return deriveNodeIPsecKey(globalKey, remoteNodeIP, localNodeIP)
 }
 
 func ipSecNewState(keys *ipSecKey) *netlink.XfrmState {
@@ -330,7 +390,7 @@ func xfrmMarkEqual(mark1, mark2 *netlink.XfrmMark) bool {
 }
 
 func ipSecReplaceStateIn(localIP, remoteIP net.IP, zeroMark bool) (uint8, error) {
-	key := getIPSecKeys(remoteIP)
+	key := getNodeIPsecKey(localIP, remoteIP, netlink.XFRM_DIR_IN)
 	if key == nil {
 		return 0, fmt.Errorf("IPSec key missing")
 	}
@@ -357,7 +417,7 @@ func ipSecReplaceStateIn(localIP, remoteIP net.IP, zeroMark bool) (uint8, error)
 }
 
 func ipSecReplaceStateOut(localIP, remoteIP net.IP, nodeID uint16) (uint8, error) {
-	key := getIPSecKeys(localIP)
+	key := getNodeIPsecKey(localIP, remoteIP, netlink.XFRM_DIR_OUT)
 	if key == nil {
 		return 0, fmt.Errorf("IPSec key missing")
 	}
@@ -374,7 +434,9 @@ func ipSecReplaceStateOut(localIP, remoteIP net.IP, nodeID uint16) (uint8, error
 
 func _ipSecReplacePolicyInFwd(src, dst *net.IPNet, tmplSrc, tmplDst net.IP, proxyMark bool, dir netlink.Dir) error {
 	optional := int(0)
-	key := getIPSecKeys(dst.IP)
+	// We can use the global IPsec key here because we are not going to
+	// actually use the secret itself.
+	key := getGlobalIPsecKey(dst.IP)
 	if key == nil {
 		return fmt.Errorf("IPSec key missing")
 	}
@@ -529,7 +591,9 @@ func generateEncryptMark(spi uint8, nodeID uint16) *netlink.XfrmMark {
 func ipSecReplacePolicyOut(src, dst *net.IPNet, tmplSrc, tmplDst net.IP, nodeID uint16, dir IPSecDir) error {
 	// TODO: Remove old policy pointing to target net
 
-	key := getIPSecKeys(dst.IP)
+	// We can use the global IPsec key here because we are not going to
+	// actually use the secret itself.
+	key := getGlobalIPsecKey(dst.IP)
 	if key == nil {
 		return fmt.Errorf("IPSec key missing")
 	}

.github/actions/ipsec-key-rotate/action.yaml
@@ -33,9 +33,10 @@ runs:
           kubectl patch secret -n kube-system cilium-ipsec-keys -p="$data" -v=1
 
           # Wait until key rotation starts
+          # We expect twice the amount of keys in use during rotation.
           while true; do
             keys_in_use=$(kubectl -n kube-system exec daemonset/cilium -c cilium-agent -- cilium-dbg encrypt status | awk '/Keys in use/ {print $NF}')
-            if [[ $keys_in_use == 2 ]]; then
+            if [[ $keys_in_use == 16 ]]; then
               break
             fi
             echo "Waiting until key rotation starts (seeing $keys_in_use keys)"
@@ -47,7 +48,7 @@ runs:
           sleep $((4*60))
           while true; do
             keys_in_use=$(kubectl -n kube-system exec daemonset/cilium -c cilium-agent -- cilium-dbg encrypt status | awk '/Keys in use/ {print $NF}')
-            if [[ $keys_in_use == 1 ]]; then
+            if [[ $keys_in_use == 8 ]]; then
               break
             fi
             echo "Waiting until key rotation completes (seeing $keys_in_use keys)"

commit 4847d5527b1a831cbe4cfb654f738ca0eda94662

Update ipsec to handle larger psk values
For psk values <= 32 bytes use SHA256 to compute the node key.  Otherwise use SHA512. This is needed to support GCM-256-AES since a PSK for this would require a 36 bytes as per RFC 4106.

Fixes: #33457
Fixes: c28e046d4c6 ("ipsec: Compute per-node-pair IPsec keys")

Signed-off-by: Jason Aliyetti <jaliyetti@gmail.com>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -10,6 +10,7 @@ import (
 	"bytes"
 	"context"
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 	"errors"
 	"fmt"
@@ -184,8 +185,16 @@ func computeNodeIPsecKey(globalKey, srcNodeIP, dstNodeIP, srcBootID, dstBootID [
 	input = append(input, dstNodeIP...)
 	input = append(input, srcBootID[:36]...)
 	input = append(input, dstBootID[:36]...)
-	output := sha256.Sum256(input)
-	return output[:len(globalKey)]
+
+	var hash []byte
+	if len(globalKey) <= 32 {
+		h := sha256.Sum256(input)
+		hash = h[:]
+	} else {
+		h := sha512.Sum512(input)
+		hash = h[:]
+	}
+	return hash[:len(globalKey)]
 }
 
 // canonicalIP returns a canonical IPv4 address (4 bytes)

pkg/datapath/linux/ipsec/ipsec_linux_test.go
@@ -42,6 +42,7 @@ var (
 	path           = "ipsec_keys_test"
 	keysDat        = []byte("1 hmac(sha256) 0123456789abcdef0123456789abcdef cbc(aes) 0123456789abcdef0123456789abcdef\n1 hmac(sha256) 0123456789abcdef0123456789abcdef cbc(aes) 0123456789abcdef0123456789abcdef foobar\n1 digest_null \"\" cipher_null \"\"\n")
 	keysAeadDat    = []byte("6 rfc4106(gcm(aes)) 44434241343332312423222114131211f4f3f2f1 128\n")
+	keysAeadDat256 = []byte("6 rfc4106(gcm(aes)) 44434241343332312423222114131211f4f3f2f144434241343332312423222114131211 128\n")
 	invalidKeysDat = []byte("1 test abcdefghijklmnopqrstuvwzyzABCDEF test abcdefghijklmnopqrstuvwzyzABCDEF\n")
 )
 
@@ -71,16 +72,14 @@ func TestInvalidLoadKeys(t *testing.T) {
 func TestLoadKeys(t *testing.T) {
 	log := setupIPSecSuitePrivileged(t)
 
-	keys := bytes.NewReader(keysDat)
-	_, spi, err := LoadIPSecKeys(log, keys)
-	require.NoError(t, err)
-	err = SetIPSecSPI(log, spi)
-	require.NoError(t, err)
-	keys = bytes.NewReader(keysAeadDat)
-	_, spi, err = LoadIPSecKeys(log, keys)
-	require.NoError(t, err)
-	err = SetIPSecSPI(log, spi)
-	require.NoError(t, err)
+	testCases := [][]byte{keysDat, keysAeadDat, keysAeadDat256}
+	for _, testCase := range testCases {
+		keys := bytes.NewReader(testCase)
+		_, spi, err := LoadIPSecKeys(log, keys)
+		require.NoError(t, err)
+		err = SetIPSecSPI(log, spi)
+		require.NoError(t, err)
+	}
 }
 
 func TestParseSPI(t *testing.T) {