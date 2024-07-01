cilium/cilium

commit 6ccd044b05090ad04d7cc3d7e4881083335244e8

policy: Do not select any identity with egress empty slices
In case of L4 egress policies with an explicit (non-nil) empty slice in
one of:

- toEndpoints
- toCIDR
- toCIDRSet
- toEntities

no identities should be selected, thus falling back to default deny for
an allow policy.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/policy/api/egress.go
@@ -219,6 +219,13 @@ type EgressDenyRule struct {
 // ToEndpoints is not aggregated due to requirement folding in
 // GetDestinationEndpointSelectorsWithRequirements()
 func (e *EgressCommonRule) getAggregatedSelectors() EndpointSelectorSlice {
+	// explicitly check for empty non-nil slices, it should not result in any identity being selected.
+	if (e.ToEntities != nil && len(e.ToEntities) == 0) ||
+		(e.ToCIDR != nil && len(e.ToCIDR) == 0) ||
+		(e.ToCIDRSet != nil && len(e.ToCIDRSet) == 0) {
+		return nil
+	}
+
 	res := make(EndpointSelectorSlice, 0, len(e.ToEntities)+len(e.ToCIDR)+len(e.ToCIDRSet))
 	res = append(res, e.ToEntities.GetAsEndpointSelectors()...)
 	res = append(res, e.ToCIDR.GetAsEndpointSelectors()...)
@@ -281,6 +288,11 @@ func (e *EgressDenyRule) GetDestinationEndpointSelectorsWithRequirements(require
 func (e *EgressCommonRule) getDestinationEndpointSelectorsWithRequirements(
 	requirements []slim_metav1.LabelSelectorRequirement,
 ) EndpointSelectorSlice {
+
+	// explicitly check for empty non-nil slices, it should not result in any identity being selected.
+	if e.aggregatedSelectors == nil || (e.ToEndpoints != nil && len(e.ToEndpoints) == 0) {
+		return nil
+	}
 
 	res := make(EndpointSelectorSlice, 0, len(e.ToEndpoints)+len(e.aggregatedSelectors))
 

pkg/policy/rule.go
@@ -603,6 +603,11 @@ func (r *rule) matches(securityIdentity *identity.Identity) bool {
 func mergeEgress(policyCtx PolicyContext, ctx *SearchContext, toEndpoints api.EndpointSelectorSlice, auth *api.Authentication, toPorts, icmp api.PortsIterator, ruleLabels labels.LabelArray, resMap L4PolicyMap, fqdns api.FQDNSelectorSlice) (int, error) {
 	found := 0
 
+	// short-circuit if no endpoint is selected
+	if toEndpoints == nil {
+		return found, nil
+	}
+
 	if ctx.To != nil && len(toEndpoints) > 0 {
 		if ctx.TraceEnabled() {
 			traceL3(ctx, toEndpoints, "to", policyCtx.IsDeny())

commit 28796eef1a2ff2067a9ad591548549df6eee9ea2

policy/api: Use a custom DeepEqual method to compare EgressCommonRule
The semantic of a nil slice in one of the EgressCommonRule fields is
different from the semantic of an empty non-nil slice. In order to
compare two EgressCommonRule instances correctly, we add a custom
DeepEqual method the explicitly checks for this case before calling the
autogenerated method.

This allows to correctly propagate CNP/CCNP updates when, for example,
the ToEndpoints selector is changed from nil to an empty non-nil
slice. In the latter case, the CNP should not select any identity,
falling back to the default behavior (e.g: default deny for an allow
policy).

Fixes: 6ccd044b05 ("policy: Do not select any identity with egress empty slices")
Fixes: https://github.com/cilium/cilium/issues/33432

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/policy/api/egress.go
@@ -12,6 +12,8 @@ import (
 // EgressCommonRule is a rule that shares some of its fields across the
 // EgressRule and EgressDenyRule. It's publicly exported so the code generators
 // can generate code for this structure.
+//
+// +deepequal-gen:private-method=true
 type EgressCommonRule struct {
 	// ToEndpoints is a list of endpoints identified by an EndpointSelector to
 	// which the endpoints subject to the rule are allowed to communicate.
@@ -109,6 +111,31 @@ type EgressCommonRule struct {
 	// TODO: Move this to the policy package
 	// (https://github.com/cilium/cilium/issues/8353)
 	aggregatedSelectors EndpointSelectorSlice `json:"-"`
+}
+
+// DeepEqual returns true if both EgressCommonRule are deep equal.
+// The semantic of a nil slice in one of its fields is different from the semantic
+// of an empty non-nil slice, thus it explicitly checks for that case before calling
+// the autogenerated method.
+func (in *EgressCommonRule) DeepEqual(other *EgressCommonRule) bool {
+	if in.ToEndpoints == nil && other.ToEndpoints != nil ||
+		in.ToEndpoints != nil && other.ToEndpoints == nil {
+		return false
+	}
+	if in.ToCIDR == nil && other.ToCIDR != nil ||
+		in.ToCIDR != nil && other.ToCIDR == nil {
+		return false
+	}
+	if in.ToCIDRSet == nil && other.ToCIDRSet != nil ||
+		in.ToCIDRSet != nil && other.ToCIDRSet == nil {
+		return false
+	}
+	if in.ToEntities == nil && other.ToEntities != nil ||
+		in.ToEntities != nil && other.ToEntities == nil {
+		return false
+	}
+
+	return in.deepEqual(other)
 }
 
 // EgressRule contains all rule types which can be applied at egress, i.e.

pkg/policy/api/zz_generated.deepequal.go
@@ -194,9 +194,9 @@ func (in *DefaultDenyConfig) DeepEqual(other *DefaultDenyConfig) bool {
 	return true
 }
 
-// DeepEqual is an autogenerated deepequal function, deeply comparing the
+// deepEqual is an autogenerated deepequal function, deeply comparing the
 // receiver with other. in must be non-nil.
-func (in *EgressCommonRule) DeepEqual(other *EgressCommonRule) bool {
+func (in *EgressCommonRule) deepEqual(other *EgressCommonRule) bool {
 	if other == nil {
 		return false
 	}

pkg/policy/api/egress_test.go
@@ -357,3 +357,119 @@ func TestIsLabelBasedEgress(t *testing.T) {
 		require.EqualValues(t, want.isLabelBased, isLabelBased, fmt.Sprintf("Test name: %q", tt.name))
 	}
 }
+
+func TestEgressCommonRuleDeepEqual(t *testing.T) {
+	testCases := []struct {
+		name      string
+		in, other *EgressCommonRule
+		expected  bool
+	}{
+		{
+			name:     "All fields are nil in both",
+			in:       &EgressCommonRule{},
+			other:    &EgressCommonRule{},
+			expected: true,
+		},
+		{
+			name: "All fields are empty in both",
+			in: &EgressCommonRule{
+				ToEndpoints: []EndpointSelector{},
+				ToCIDR:      []CIDR{},
+				ToCIDRSet:   []CIDRRule{},
+				ToEntities:  []Entity{},
+			},
+			other: &EgressCommonRule{
+				ToEndpoints: []EndpointSelector{},
+				ToCIDR:      []CIDR{},
+				ToCIDRSet:   []CIDRRule{},
+				ToEntities:  []Entity{},
+			},
+			expected: true,
+		},
+		{
+			name: "ToEndpoints is nil in left operand",
+			in: &EgressCommonRule{
+				ToEndpoints: nil,
+			},
+			other: &EgressCommonRule{
+				ToEndpoints: []EndpointSelector{},
+			},
+			expected: false,
+		},
+		{
+			name: "ToEndpoints is empty in left operand",
+			in: &EgressCommonRule{
+				ToEndpoints: []EndpointSelector{},
+			},
+			other: &EgressCommonRule{
+				ToEndpoints: nil,
+			},
+			expected: false,
+		},
+		{
+			name: "ToCIDR is nil in left operand",
+			in: &EgressCommonRule{
+				ToCIDR: nil,
+			},
+			other: &EgressCommonRule{
+				ToCIDR: []CIDR{},
+			},
+			expected: false,
+		},
+		{
+			name: "ToCIDR is empty in left operand",
+			in: &EgressCommonRule{
+				ToCIDR: []CIDR{},
+			},
+			other: &EgressCommonRule{
+				ToCIDR: nil,
+			},
+			expected: false,
+		},
+		{
+			name: "ToCIDRSet is nil in left operand",
+			in: &EgressCommonRule{
+				ToCIDRSet: nil,
+			},
+			other: &EgressCommonRule{
+				ToCIDRSet: []CIDRRule{},
+			},
+			expected: false,
+		},
+		{
+			name: "ToCIDRSet is empty in left operand",
+			in: &EgressCommonRule{
+				ToCIDRSet: []CIDRRule{},
+			},
+			other: &EgressCommonRule{
+				ToCIDRSet: nil,
+			},
+			expected: false,
+		},
+		{
+			name: "ToEntities is nil in left operand",
+			in: &EgressCommonRule{
+				ToEntities: nil,
+			},
+			other: &EgressCommonRule{
+				ToEntities: []Entity{},
+			},
+			expected: false,
+		},
+		{
+			name: "ToEntities is empty in left operand",
+			in: &EgressCommonRule{
+				ToEntities: []Entity{},
+			},
+			other: &EgressCommonRule{
+				ToEntities: nil,
+			},
+			expected: false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			require.Equal(t, tc.expected, tc.in.DeepEqual(tc.other))
+		})
+	}
+}