f123a7fa4d7979ba10f88b79f731cc0ca3b22e6c
Jarno Rajahalme <jarno@isovalent.com>
2024-07-03 23:10:33 +0300

    policy: remove unnecessary map operations
    Remove redundant map operations.
    
    If key exists in the allows map, then the entry is an allow entry, and
    updates in the denies map are not necessary, and the other wayu around.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- pkg/policy/mapstate.go
+++ pkg/policy/mapstate.go
@@ -816,14 +816,13 @@ func (ms *mapState) RemoveDependent(owner Key, dependent Key, identities Identit
 	if e, exists := ms.allows.Lookup(owner); exists {
 		changes.insertOldIfNotExists(owner, e)
 		e.RemoveDependent(dependent)
-		ms.denies.delete(owner, identities)
+		// update the value in the allows map
 		ms.allows.upsert(owner, e, identities)
 		return
 	}
 	if e, exists := ms.denies.Lookup(owner); exists {
 		changes.insertOldIfNotExists(owner, e)
 		e.RemoveDependent(dependent)
-		ms.allows.delete(owner, identities)
 		ms.denies.upsert(owner, e, identities)
 	}
 }
