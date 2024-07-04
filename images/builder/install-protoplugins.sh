cilium/cilium

commit dc683b9ea77098d99fa18f6421664cc00319706c

images/builder: let renovate update proto plugins
Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

.github/renovate.json5
@@ -421,6 +421,7 @@
       "fileMatch": [
         "images/cilium/download-hubble\\.sh",
         "images/builder/install-protoc.sh",
+        "images/builder/install-protoplugins.sh",
         "images/hubble-relay/download-grpc-health-probe.sh"
         "images/runtime/build-gops.sh",
       ],

images/builder/install-protoplugins.sh
@@ -8,8 +8,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# 8ba23be9613c672d40ae261d2a1335d639bdd59b == tag: cmd/protoc-gen-go-grpc/v1.3.0
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@8ba23be9613c672d40ae261d2a1335d639bdd59b
+# renovate: datasource=github-releases depName=grpc/grpc-go
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@1adbea267b837660726952ed6711b348dee87aa5
+# renovate: datasource=github-releases depName=protocolbuffers/protobuf-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.31.0
+# renovate: datasource=github-releases depName=mfridman/protoc-gen-go-json
 go install github.com/mfridman/protoc-gen-go-json@v1.4.0
+# renovate: datasource=github-releases depName=pseudomuto/protoc-gen-doc
 go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.1

commit 7a1fba551f7953bdf2e0733d16754d4f3c50d455

renovate: add fix grpc-go autodetection
As grpc-go has a tag specifically for each project, we need to adjust
renovate accordingly so that it detects the versioning of the grpc-go
only.

Fixes: dc683b9ea770 ("images/builder: let renovate update proto plugins")
Signed-off-by: André Martins <andre@cilium.io>

.github/renovate.json5
@@ -578,6 +578,12 @@
         "quay.io/cilium/cilium-envoy"
       ],
       "versioning": "regex:v(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)-.*$",
+    },
+    {
+      "matchDepNames": [
+        "grpc/grpc-go"
+      ],
+      "versioning": "regex:cmd/protoc-gen-go-grpc/v(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)$",
     }
   ],
   "kubernetes": {
@@ -604,7 +610,8 @@
         // Note that for GitHub release artifact digests, you likely want to use
         // github-release-attachments as the datasource here.
         "# renovate: datasource=(?<datasource>.*?) depName=(?<depName>.*?) digestVersion=(?<currentValue>.*)\\s+.+_sha256.*=\"(?<currentDigest>.*)\"",
-        "# renovate: datasource=(?<datasource>.*?) depName=(?<depName>.*?)\\s.+@(?<currentValue>.*)"
+        "# renovate: datasource=(?<datasource>.*?) depName=(?<depName>.*?)\\s.+@(?<currentValue>.*)",
+        "# renovate: datasource=(?<datasource>.*?) depName=(?<depName>.*?)\\s.+_VERSION=(?<currentValue>.*)"
       ]
     },
     {

images/builder/install-protoplugins.sh
@@ -8,8 +8,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# renovate: datasource=github-releases depName=grpc/grpc-go
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@1adbea267b837660726952ed6711b348dee87aa5
+# renovate: datasource=github-tags depName=grpc/grpc-go
+GRPC_VERSION=cmd/protoc-gen-go-grpc/v1.4.0
+
+GRPC_VERSION=${GRPC_VERSION#cmd/protoc-gen-go-grpc/}
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@${GRPC_VERSION}
+
 # renovate: datasource=github-releases depName=protocolbuffers/protobuf-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.31.0
 # renovate: datasource=github-releases depName=mfridman/protoc-gen-go-json
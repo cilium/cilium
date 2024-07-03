7a1fba551f7953bdf2e0733d16754d4f3c50d455
André Martins <andre@cilium.io>
2024-07-03 13:16:52 +0200

    renovate: add fix grpc-go autodetection
    As grpc-go has a tag specifically for each project, we need to adjust
    renovate accordingly so that it detects the versioning of the grpc-go
    only.
    
    Fixes: dc683b9ea770 ("images/builder: let renovate update proto plugins")
    Signed-off-by: André Martins <andre@cilium.io>

--- images/builder/install-protoplugins.sh
+++ images/builder/install-protoplugins.sh
@@ -8,8 +8,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# renovate: datasource=github-releases depName=grpc/grpc-go
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@1adbea267b837660726952ed6711b348dee87aa5
+# renovate: datasource=github-tags depName=grpc/grpc-go
+GRPC_VERSION=cmd/protoc-gen-go-grpc/v1.4.0
+
+GRPC_VERSION=${GRPC_VERSION#cmd/protoc-gen-go-grpc/}
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@${GRPC_VERSION}
+
 # renovate: datasource=github-releases depName=protocolbuffers/protobuf-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.31.0
 # renovate: datasource=github-releases depName=mfridman/protoc-gen-go-json
