cilium/cilium

commit 2c2c5ae08e7e6eeef106a2800caa71b2c958ea02

bpf: Send ICMP unreachable when no service backends are available
Before this commit we would always drop packets if we were unable to
find a backend for a service. This causes a client to retry multiple
times until a timeout is reached. This is not expected behavior since
kube-proxy and other CNI's will send an ICMP reply.

If enabled, we can now reply with an ICMP port unreachable message when
no backend is available for a service. This is implemented with a
separate tail call since crating the ICMP message can be quite complex.

This functionality is implemented for both IPv4 and IPv6. For IPv4
traffic the received packet is truncated to 64 bytes and  included in
the ICMP response. For IPv6 traffic up to 1232 bytes of the original
message is included in the ICMPv6 response. This matches the behavior
of kube-proxy.

Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

bpf/lib/nodeport.h
@@ -1320,6 +1320,14 @@ static __always_inline int nodeport_lb6(struct __ctx_buff *ctx,
 		ret = lb6_local(get_ct_map6(&tuple), ctx, l3_off, l4_off,
 				&key, &tuple, svc, &ct_state_new,
 				skip_l3_xlate, ext_err);
+
+#ifdef SERVICE_NO_BACKEND_RESPONSE
+		if (ret == DROP_NO_SERVICE) {
+			ep_tail_call(ctx, CILIUM_CALL_IPV6_NO_SERVICE);
+			return DROP_MISSED_TAIL_CALL;
+		}
+#endif
+
 		if (IS_ERR(ret))
 			return ret;
 
@@ -2802,6 +2810,12 @@ static __always_inline int nodeport_lb4(struct __ctx_buff *ctx,
 					&key, &tuple, svc, &ct_state_new,
 					has_l4_header, skip_l3_xlate, &cluster_id,
 					ext_err);
+#ifdef SERVICE_NO_BACKEND_RESPONSE
+			if (ret == DROP_NO_SERVICE) {
+				ep_tail_call(ctx, CILIUM_CALL_IPV4_NO_SERVICE);
+				return DROP_MISSED_TAIL_CALL;
+			}
+#endif
 		}
 		if (IS_ERR(ret))
 			return ret;
@@ -3153,6 +3167,7 @@ int tail_handle_nat_fwd_ipv4(struct __ctx_buff *ctx)
 
 	return ret;
 }
+
 #endif /* ENABLE_IPV4 */
 
 #ifdef ENABLE_HEALTH_CHECK

bpf/bpf_lxc.c
@@ -108,6 +108,14 @@ static __always_inline int __per_packet_lb_svc_xlate_4(void *ctx, struct iphdr *
 		ret = lb4_local(get_ct_map4(&tuple), ctx, ipv4_is_fragment(ip4),
 				ETH_HLEN, l4_off, &key, &tuple, svc, &ct_state_new,
 				has_l4_header, false, &cluster_id, ext_err);
+
+#ifdef SERVICE_NO_BACKEND_RESPONSE
+		if (ret == DROP_NO_SERVICE) {
+			ep_tail_call(ctx, CILIUM_CALL_IPV4_NO_SERVICE);
+			return DROP_MISSED_TAIL_CALL;
+		}
+#endif
+
 		if (IS_ERR(ret))
 			return ret;
 	}
@@ -158,6 +166,14 @@ static __always_inline int __per_packet_lb_svc_xlate_6(void *ctx, struct ipv6hdr
 #endif /* ENABLE_L7_LB */
 		ret = lb6_local(get_ct_map6(&tuple), ctx, ETH_HLEN, l4_off,
 				&key, &tuple, svc, &ct_state_new, false, ext_err);
+
+#ifdef SERVICE_NO_BACKEND_RESPONSE
+		if (ret == DROP_NO_SERVICE) {
+			ep_tail_call(ctx, CILIUM_CALL_IPV6_NO_SERVICE);
+			return DROP_MISSED_TAIL_CALL;
+		}
+#endif
+
 		if (IS_ERR(ret))
 			return ret;
 	}

bpf/lib/common.h
@@ -133,7 +133,9 @@ enum {
 #define CILIUM_CALL_IPV4_CONT_FROM_NETDEV	44
 #define CILIUM_CALL_IPV6_CONT_FROM_HOST		45
 #define CILIUM_CALL_IPV6_CONT_FROM_NETDEV	46
-#define CILIUM_CALL_SIZE			47
+#define CILIUM_CALL_IPV4_NO_SERVICE		47
+#define CILIUM_CALL_IPV6_NO_SERVICE		48
+#define CILIUM_CALL_SIZE			49
 
 typedef __u64 mac_t;
 

bpf/lib/lb.h
@@ -12,6 +12,10 @@
 #include "ids.h"
 #include "nat_46x64.h"
 
+#ifndef SKIP_CALLS_MAP
+#include "drop.h"
+#endif
+
 #ifdef ENABLE_IPV6
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
@@ -1741,7 +1745,345 @@ lb4_ctx_restore_state(struct __ctx_buff *ctx, struct ct_state *state,
 #endif
 }
 
+/* Because we use tail calls and this file is included in bpf_sock.h */
+#ifndef SKIP_CALLS_MAP
+#ifdef SERVICE_NO_BACKEND_RESPONSE
+
+#define ICMP_PACKET_MAX_SAMPLE_SIZE 64
+
+static __always_inline
+__wsum icmp_wsum_accumulate(void *data_start, void *data_end, int sample_len);
+
+static __always_inline
+int __tail_no_service_ipv4(struct __ctx_buff *ctx)
+{
+	void *data, *data_end;
+	struct ethhdr *ethhdr;
+	struct iphdr *ip4;
+	struct icmphdr *icmphdr;
+	union macaddr smac = {};
+	union macaddr dmac = {};
+	__be32	saddr;
+	__be32	daddr;
+	__u8	tos;
+	__wsum csum;
+	int sample_len;
+	int ret;
+	const int inner_offset = sizeof(struct ethhdr) + sizeof(struct iphdr) +
+		sizeof(struct icmphdr);
+
+	if (!revalidate_data(ctx, &data, &data_end, &ip4))
+		return DROP_INVALID;
+
+	/* copy the incoming src and dest IPs and mac addresses to the stack.
+	 * the pointers will not be valid after adding headroom.
+	 */
+
+	if (eth_load_saddr(ctx, smac.addr, 0) < 0)
+		return DROP_INVALID;
+
+	if (eth_load_daddr(ctx, dmac.addr, 0) < 0)
+		return DROP_INVALID;
+
+	saddr = ip4->saddr;
+	daddr = ip4->daddr;
+	tos = ip4->tos;
+
+	/* Resize to ethernet header + 64 bytes or less */
+	sample_len = ctx_full_len(ctx);
+	if (sample_len > ICMP_PACKET_MAX_SAMPLE_SIZE)
+		sample_len = ICMP_PACKET_MAX_SAMPLE_SIZE;
+	ctx_adjust_troom(ctx, sample_len + sizeof(struct ethhdr) - ctx_full_len(ctx));
+
+	data = ctx_data(ctx);
+	data_end = ctx_data_end(ctx);
+
+	/* Calculate the checksum of the ICMP sample */
+	csum = icmp_wsum_accumulate(data + sizeof(struct ethhdr), data_end, sample_len);
+
+	/* We need to insert a IPv4 and ICMP header before the original packet.
+	 * Make that room.
+	 */
+
+#if __ctx_is == __ctx_xdp
+	ret = xdp_adjust_head(ctx, 0 - (int)(sizeof(struct iphdr) + sizeof(struct icmphdr)));
+#else
+	ret = skb_adjust_room(ctx, sizeof(struct iphdr) + sizeof(struct icmphdr),
+			      BPF_ADJ_ROOM_MAC, 0);
+#endif
+
+	if (ret < 0)
+		return DROP_INVALID;
+
+	/* changing size invalidates pointers, so we need to re-fetch them. */
+	data = ctx_data(ctx);
+	data_end = ctx_data_end(ctx);
+
+	/* Bound check all 3 headers at once. */
+	if (data + inner_offset > data_end)
+		return DROP_INVALID;
+
+	/* Write reversed eth header, ready for egress */
+	ethhdr = data;
+	memcpy(ethhdr->h_dest, smac.addr, sizeof(smac.addr));
+	memcpy(ethhdr->h_source, dmac.addr, sizeof(dmac.addr));
+	ethhdr->h_proto = bpf_htons(ETH_P_IP);
+
+	/* Write reversed ip header, ready for egress */
+	ip4 = data + sizeof(struct ethhdr);
+	ip4->version = 4;
+	ip4->ihl = sizeof(struct iphdr) >> 2;
+	ip4->tos = tos;
+	ip4->tot_len = bpf_htons(sizeof(struct iphdr) + sizeof(struct icmphdr) +
+		       (__u16)sample_len);
+	ip4->id = 0;
+	ip4->frag_off = 0;
+	ip4->ttl = 64;
+	ip4->protocol = IPPROTO_ICMP;
+	ip4->check = 0;
+	ip4->daddr = saddr;
+	ip4->saddr = daddr;
+	ip4->check = csum_fold(csum_diff(ip4, 0, ip4, sizeof(struct iphdr), 0));
+
+	/* Write reversed icmp header */
+	icmphdr = data + sizeof(struct ethhdr) + sizeof(struct iphdr);
+	icmphdr->type = ICMP_DEST_UNREACH;
+	icmphdr->code = ICMP_PORT_UNREACH;
+	icmphdr->checksum = 0;
+	icmphdr->un.gateway = 0;
+
+	/* Add ICMP header checksum to sum of its body */
+	csum += csum_diff(icmphdr, 0, icmphdr, sizeof(struct icmphdr), 0);
+	icmphdr->checksum = csum_fold(csum);
+
+	/* Redirect ICMP to the interface we received it on. */
+	cilium_dbg_capture(ctx, DBG_CAPTURE_DELIVERY,
+			   ctx_get_ifindex(ctx));
+	return ctx_redirect(ctx, ctx_get_ifindex(ctx), 0);
+}
+
+__section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV4_NO_SERVICE)
+int tail_no_service_ipv4(struct __ctx_buff *ctx)
+{
+	__u32 src_sec_identity = ctx_load_meta(ctx, CB_SRC_LABEL);
+	int ret;
+
+	ret = __tail_no_service_ipv4(ctx);
+	if (IS_ERR(ret))
+		return send_drop_notify_error(ctx, src_sec_identity, ret,
+			CTX_ACT_DROP, METRIC_INGRESS);
+
+	return ret;
+}
+#endif /* SERVICE_NO_BACKEND_RESPONSE */
+#endif /* SKIP_CALLS_MAP */
+
 #endif /* ENABLE_IPV4 */
+
+#ifdef ENABLE_IPV6
+
+/* Because we use tail calls and this file is included in bpf_sock.h */
+#ifndef SKIP_CALLS_MAP
+#ifdef SERVICE_NO_BACKEND_RESPONSE
+
+#define ICMPV6_PACKET_MAX_SAMPLE_SIZE 1280 - sizeof(struct ipv6hdr) - sizeof(struct icmp6hdr)
+
+static __always_inline
+__wsum icmp_wsum_accumulate(void *data_start, void *data_end, int sample_len);
+
+/* The IPv6 pseudo-header */
+struct ipv6_pseudo_header_t {
+	union {
+		struct header {
+			struct in6_addr src_ip;
+			struct in6_addr dst_ip;
+			__be32 top_level_length;
+			__u8 zero[3];
+			__u8 next_header;
+		} __packed fields;
+		__u16 words[20];
+	};
+};
+
+static __always_inline
+int __tail_no_service_ipv6(struct __ctx_buff *ctx)
+{
+	void *data, *data_end;
+	struct ethhdr *ethhdr;
+	struct ipv6hdr *ip6;
+	struct icmp6hdr *icmphdr;
+	struct ipv6_pseudo_header_t pseudo_header;
+	union macaddr smac = {};
+	union macaddr dmac = {};
+	struct in6_addr saddr;
+	struct in6_addr daddr;
+	__wsum csum;
+	__u64 sample_len;
+	int i;
+	int ret;
+	const int inner_offset = sizeof(struct ethhdr) + sizeof(struct ipv6hdr) +
+		sizeof(struct icmp6hdr);
+
+	if (!revalidate_data(ctx, &data, &data_end, &ip6))
+		return DROP_INVALID;
+
+	/* copy the incoming src and dest IPs and mac addresses to the stack.
+	 * the pointers will not be valid after adding headroom.
+	 */
+
+	if (eth_load_saddr(ctx, smac.addr, 0) < 0)
+		return DROP_INVALID;
+
+	if (eth_load_daddr(ctx, dmac.addr, 0) < 0)
+		return DROP_INVALID;
+
+	memcpy(&saddr, &ip6->saddr, sizeof(struct in6_addr));
+	memcpy(&daddr, &ip6->daddr, sizeof(struct in6_addr));
+
+	/* Resize to min MTU - IPv6 hdr + ICMPv6 hdr */
+	sample_len = ctx_full_len(ctx);
+	if (sample_len > (__u64)ICMPV6_PACKET_MAX_SAMPLE_SIZE)
+		sample_len = ICMPV6_PACKET_MAX_SAMPLE_SIZE;
+	ctx_adjust_troom(ctx, sample_len + sizeof(struct ethhdr) - ctx_full_len(ctx));
+
+	data = ctx_data(ctx);
+	data_end = ctx_data_end(ctx);
+
+	/* Calculate the unfolded checksum of the ICMPv6 sample */
+	csum = icmp_wsum_accumulate(data + sizeof(struct ethhdr), data_end, sample_len);
+
+	/* We need to insert a IPv6 and ICMPv6 header before the original packet.
+	 * Make that room.
+	 */
+
+#if __ctx_is == __ctx_xdp
+	ret = xdp_adjust_head(ctx, 0 - (int)(sizeof(struct ipv6hdr) + sizeof(struct icmp6hdr)));
+#else
+	ret = skb_adjust_room(ctx, sizeof(struct ipv6hdr) + sizeof(struct icmp6hdr),
+			      BPF_ADJ_ROOM_MAC, 0);
+#endif
+
+	if (ret < 0)
+		return DROP_INVALID;
+
+	/* changing size invalidates pointers, so we need to re-fetch them. */
+	data = ctx_data(ctx);
+	data_end = ctx_data_end(ctx);
+
+	/* Bound check all 3 headers at once. */
+	if (data + inner_offset > data_end)
+		return DROP_INVALID;
+
+	/* Write reversed eth header, ready for egress */
+	ethhdr = data;
+	memcpy(ethhdr->h_dest, smac.addr, sizeof(smac.addr));
+	memcpy(ethhdr->h_source, dmac.addr, sizeof(dmac.addr));
+	ethhdr->h_proto = bpf_htons(ETH_P_IPV6);
+
+	/* Write reversed ip header, ready for egress */
+	ip6 = data + sizeof(struct ethhdr);
+	ip6->version = 6;
+	ip6->priority = 0;
+	ip6->flow_lbl[0] = 0;
+	ip6->flow_lbl[1] = 0;
+	ip6->flow_lbl[2] = 0;
+	ip6->payload_len = bpf_htons(sizeof(struct icmp6hdr) + (__u16)sample_len);
+	ip6->nexthdr = IPPROTO_ICMPV6;
+	ip6->hop_limit = 64;
+	memcpy(&ip6->daddr, &saddr, sizeof(struct in6_addr));
+	memcpy(&ip6->saddr, &daddr, sizeof(struct in6_addr));
+
+	/* Write reversed icmp header */
+	icmphdr = data + sizeof(struct ethhdr) + sizeof(struct ipv6hdr);
+	icmphdr->icmp6_type = ICMPV6_DEST_UNREACH;
+	icmphdr->icmp6_code = ICMPV6_PORT_UNREACH;
+	icmphdr->icmp6_cksum = 0;
+	icmphdr->icmp6_dataun.un_data32[0] = 0;
+
+	/* Add the ICMP header to the checksum (only type and code are non-zero) */
+	csum += ((__u16)icmphdr->icmp6_code) << 8 | (__u16)icmphdr->icmp6_type;
+
+	/* Fill pseudo header */
+	memcpy(&pseudo_header.fields.src_ip, &ip6->saddr, sizeof(struct in6_addr));
+	memcpy(&pseudo_header.fields.dst_ip, &ip6->daddr, sizeof(struct in6_addr));
+	pseudo_header.fields.top_level_length = bpf_htonl(sizeof(struct icmp6hdr) +
+						(__u32)sample_len);
+	__bpf_memzero(pseudo_header.fields.zero, sizeof(pseudo_header.fields.zero));
+	pseudo_header.fields.next_header = IPPROTO_ICMPV6;
+
+	#pragma unroll
+	for (i = 0; i < (int)(sizeof(pseudo_header.words) / sizeof(__u16)); i++)
+		csum += pseudo_header.words[i];
+
+	icmphdr->icmp6_cksum = csum_fold(csum);
+
+	/* Redirect ICMP to the interface we received it on. */
+	cilium_dbg_capture(ctx, DBG_CAPTURE_DELIVERY,
+			   ctx_get_ifindex(ctx));
+	return ctx_redirect(ctx, ctx_get_ifindex(ctx), 0);
+}
+
+__section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV6_NO_SERVICE)
+int tail_no_service_ipv6(struct __ctx_buff *ctx)
+{
+	__u32 src_sec_identity = ctx_load_meta(ctx, CB_SRC_LABEL);
+	int ret;
+
+	ret = __tail_no_service_ipv6(ctx);
+	if (IS_ERR(ret))
+		return send_drop_notify_error(ctx, src_sec_identity, ret,
+			CTX_ACT_DROP, METRIC_INGRESS);
+
+	return ret;
+}
+#endif /* SERVICE_NO_BACKEND_RESPONSE */
+#endif /* SKIP_CALLS_MAP */
+#endif /* ENABLE_IPV6 */
+
+#ifdef SERVICE_NO_BACKEND_RESPONSE
+
+static __always_inline
+__wsum icmp_wsum_accumulate(void *data_start, void *data_end, int sample_len)
+{
+	/* Unrolled loop to calculate the checksum of the ICMP sample
+	 * Done manually because the compiler refuses with #pragma unroll
+	 */
+	__wsum wsum = 0;
+
+	#define body(i) if ((i) > sample_len) \
+		return wsum; \
+	if (data_start + (i) + sizeof(__u16) > data_end) { \
+		if (data_start + (i) + sizeof(__u8) <= data_end)\
+			wsum += *(__u8 *)(data_start + (i)); \
+		return wsum; \
+	} \
+	wsum += *(__u16 *)(data_start + (i));
+
+	#define body4(i) body(i)\
+		body(i + 2) \
+		body(i + 4) \
+		body(i + 6)
+
+	#define body16(i) body4(i)\
+		body4(i + 8) \
+		body4(i + 16) \
+		body4(i + 24)
+
+	#define body128(i) body16(i)\
+		body16(i + 32) \
+		body16(i + 64) \
+		body16(i + 96)
+
+	body128(0)
+	body128(256)
+	body128(512)
+	body128(768)
+	body128(1024)
+
+	return wsum;
+}
+
+#endif /* SERVICE_NO_BACKEND_RESPONSE */
 
 /* sock_local_cookie retrieves the socket cookie for the
  * passed socket structure.

bpf/source_names_to_ids.h
@@ -31,6 +31,7 @@ __source_file_name_to_id(const char *const header_name)
 	_strcase_(103, "egress_policies.h");
 	_strcase_(104, "icmp6.h");
 	_strcase_(105, "nodeport.h");
+	_strcase_(106, "lb.h");
 
 	return 0;
 }

pkg/monitor/datapath_drop.go
@@ -60,6 +60,7 @@ var sourceFileNames = map[int]string{
 	103: "egress_policies.h",
 	104: "icmp6.h",
 	105: "nodeport.h",
+	106: "lb.h",
 	//end
 }
 

commit 841aa63d3d61bc6f63d30a536c84bd0455b4c6c7

bpf: nodeport: clear EDT info for "Port unreachable" ICMP reply
The EDT ID for pod-egressing traffic is stored in skb->queue_mapping, and
checked by the Bandwidth Manager code in to-netdev.

When the N/S LB responds to a service request with a "Port unreachable"
ICMP message, it rebuilds the initial request and redirects it back out
(passing through to-netdev). As the NIC driver potentially filled the
skb->queue_mapping field on RX, we need to clear it to avoid aliasing with
genuine EDT marking.

This is in line with what the nodeport code does when forwarding LB'ed
requests to a remote backend.

Fixes: 2c2c5ae08e7e ("bpf: Send ICMP unreachable when no service backends are available")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -1304,9 +1304,11 @@ static __always_inline int nodeport_svc_lb6(struct __ctx_buff *ctx,
 			skip_l3_xlate, ext_err);
 
 #ifdef SERVICE_NO_BACKEND_RESPONSE
-	if (ret == DROP_NO_SERVICE)
+	if (ret == DROP_NO_SERVICE) {
+		edt_set_aggregate(ctx, 0);
 		ret = tail_call_internal(ctx, CILIUM_CALL_IPV6_NO_SERVICE,
 					 ext_err);
+	}
 #endif
 
 	if (IS_ERR(ret))
@@ -2843,9 +2845,12 @@ static __always_inline int nodeport_svc_lb4(struct __ctx_buff *ctx,
 				has_l4_header, skip_l3_xlate, &cluster_id,
 				ext_err);
 #ifdef SERVICE_NO_BACKEND_RESPONSE
-		if (ret == DROP_NO_SERVICE)
+		if (ret == DROP_NO_SERVICE) {
+			/* Packet is TX'ed back out, avoid EDT false-positives: */
+			edt_set_aggregate(ctx, 0);
 			ret = tail_call_internal(ctx, CILIUM_CALL_IPV4_NO_SERVICE,
 						 ext_err);
+		}
 #endif
 	}
 	if (IS_ERR(ret))