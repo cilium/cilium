f062da41c36da1ea286cb3ae354e8a84239eeeef
harsimran pabla <hpabla@isovalent.com>
2024-07-09 14:17:52 +0000

    bgpv2: add jitter to status updates
    [ upstream commit af7f165f7882330f753763c78916e810dd65a5be ]
    
    Adding ticker with jitter in BGP CRD status updates. BGP changes are
    generally done simultaneously on all nodes, which will result in status
    update of CiliumBGPNodeConfig resource. Since there can be lot of nodes
    in the cluster, we want to avoid all nodes updating status at the same
    time.
    
    This commit also introduces external dependency github.com/lthibault/jitterbug
    which implements ticker with jitter.
    
    Signed-off-by: harsimran pabla <hpabla@isovalent.com>

---
+++ vendor/github.com/lthibault/jitterbug/univariate.go
@@ -0,0 +1,20 @@
+package jitterbug
+
+import "time"
+
+// Sampler can sample from any univariate distribution.  It is used in conjunction
+// with the Univariate type and is compatible with GoNum:
+// https://godoc.org/gonum.org/v1/gonum/stat/distuv.
+type Sampler interface {
+	Rand() float64
+}
+
+// Univariate distribution
+type Univariate struct {
+	Sampler
+}
+
+// Jitter the duration by drawing from a univariate distribution
+func (u Univariate) Jitter(d time.Duration) time.Duration {
+	return d + time.Duration(u.Rand())
+}
