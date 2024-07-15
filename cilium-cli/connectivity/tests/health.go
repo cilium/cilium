a20ffa554eceb4ef15ef42282cc0e42e7415c171
Michi Mutsuzaki <michi@isovalent.com>
2024-07-15 04:34:15 +0000

    Replace timer.After with inctimer.After
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Replace timer.After with inctimer.After so that custom-vet-check.sh [^1]
    doesn't complain when cilium-cli gets merged with cilium repo [^2].
    
    [^1]: https://github.com/cilium/cilium/blob/main/contrib/scripts/custom-vet-check.sh
    [^2]: https://github.com/cilium/design-cfps/pull/9
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- cilium-cli/connectivity/tests/health.go
+++ cilium-cli/connectivity/tests/health.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/cilium/cilium/pkg/inctimer"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/util/jsonpath"
@@ -44,7 +45,7 @@ func runHealthProbe(ctx context.Context, t *check.Test, pod *check.Pod) {
 
 	// Probe health status until it passes checks or timeout is reached.
 	for {
-		retryTimer := time.After(time.Second)
+		retryTimer := inctimer.After(time.Second)
 
 		if _, err := pod.K8sClient.GetPod(ctx, pod.Pod.Namespace, pod.Pod.Name, metav1.GetOptions{}); k8serrors.IsNotFound(err) {
 			t.Failf("cilium-health validation failed. Cilium Agent Pod %s/%s no longer exists", pod.Pod.Namespace, pod.Pod.Name)
