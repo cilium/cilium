ae9d9a7da110c78acacfa28786fa8a9cd63c7e95
Michi Mutsuzaki <michi@isovalent.com>
2024-07-15 04:34:15 +0000

    Replace timer.After with inctimer.After
    [ cherry-picked from cilium/cilium-cli repository ]
    
    Replace timer.After with inctimer.After so that custom-vet-check.sh [^1]
    doesn't complain when cilium-cli gets merged with cilium repo [^2].
    
    [^1]: https://github.com/cilium/cilium/blob/main/contrib/scripts/custom-vet-check.sh
    [^2]: https://github.com/cilium/design-cfps/pull/9
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- cilium-cli/connectivity/check/wait.go
+++ cilium-cli/connectivity/check/wait.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/inctimer"
 	"golang.org/x/exp/slices"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
@@ -47,7 +48,7 @@ func WaitForDeployment(ctx context.Context, log Logger, client *k8s.Client, name
 		log.Debugf("[%s] Deployment %s/%s is not yet ready: %s", client.ClusterName(), namespace, name, err)
 
 		select {
-		case <-time.After(PollInterval):
+		case <-inctimer.After(PollInterval):
 		case <-ctx.Done():
 			return fmt.Errorf("timeout reached waiting for deployment %s/%s to become ready (last error: %w)",
 				namespace, name, err)
@@ -70,7 +71,7 @@ func WaitForDaemonSet(ctx context.Context, log Logger, client *k8s.Client, names
 		log.Debugf("[%s] DaemonSet %s/%s is not yet ready: %s", client.ClusterName(), namespace, name, err)
 
 		select {
-		case <-time.After(PollInterval):
+		case <-inctimer.After(PollInterval):
 		case <-ctx.Done():
 			return fmt.Errorf("timeout reached waiting for DaemonSet %s/%s to become ready (last error: %w)",
 				namespace, name, err)
@@ -102,7 +103,7 @@ func WaitForPodDNS(ctx context.Context, log Logger, src, dst Pod) error {
 			src.K8sClient.ClusterName(), target, src.Name(), dst.Name(), err, stdout.String())
 
 		select {
-		case <-time.After(PollInterval):
+		case <-inctimer.After(PollInterval):
 		case <-ctx.Done():
 			return fmt.Errorf("timeout reached waiting for lookup for %s from pod %s to server on pod %s to succeed (last error: %w)",
 				target, src.Name(), dst.Name(), err,
@@ -130,7 +131,7 @@ func WaitForCoreDNS(ctx context.Context, log Logger, client Pod) error {
 			client.K8sClient.ClusterName(), target, client.Name(), err, stdout.String())
 
 		select {
-		case <-time.After(PollInterval):
+		case <-inctimer.After(PollInterval):
 		case <-ctx.Done():
 			return fmt.Errorf("timeout reached waiting for lookup for %s from pod %s to succeed (last error: %w)",
 				target, client.Name(), err)
@@ -153,7 +154,7 @@ func WaitForServiceRetrieval(ctx context.Context, log Logger, client *k8s.Client
 		log.Debugf("[%s] Failed to retrieve Service %s/%s: %s", client.ClusterName(), namespace, name, err)
 
 		select {
-		case <-time.After(PollInterval):
+		case <-inctimer.After(PollInterval):
 		case <-ctx.Done():
 			return Service{}, fmt.Errorf("timeout reached waiting for service %s/%s to be retrieved (last error: %w)",
 				namespace, name, err)
@@ -196,7 +197,7 @@ func WaitForService(ctx context.Context, log Logger, client Pod, service Service
 			client.K8sClient.ClusterName(), service.Name(), err, stdout.String())
 
 		select {
-		case <-time.After(PollInterval):
+		case <-inctimer.After(PollInterval):
 		case <-ctx.Done():
 			return fmt.Errorf("timeout reached waiting for service %s (last error: %w)", service.Name(), err)
 		}
@@ -227,7 +228,7 @@ func WaitForServiceEndpoints(ctx context.Context, log Logger, agent Pod, service
 			agent.K8sClient.ClusterName(), service.Name(), agent.Name(), err)
 
 		select {
-		case <-time.After(PollInterval):
+		case <-inctimer.After(PollInterval):
 		case <-ctx.Done():
 			return fmt.Errorf("timeout reached waiting for service %s to appear in Cilium pod %s (last error: %w)",
 				service.Name(), agent.Name(), err)
@@ -307,7 +308,7 @@ func WaitForNodePorts(ctx context.Context, log Logger, client Pod, nodeIP string
 				client.K8sClient.ClusterName(), nodeIP, nodePort, service.Name(), err, stdout.String())
 
 			select {
-			case <-time.After(PollInterval):
+			case <-inctimer.After(PollInterval):
 			case <-ctx.Done():
 				return fmt.Errorf("timeout reached waiting for NodePort %s:%d (%s) (last error: %w)",
 					nodeIP, nodePort, service.Name(), err)
@@ -335,7 +336,7 @@ func WaitForIPCache(ctx context.Context, log Logger, agent Pod, pods []Pod) erro
 		log.Debugf("[%s] Error checking pod IPs in IPCache: %s", agent.K8sClient.ClusterName(), err)
 
 		select {
-		case <-time.After(PollInterval):
+		case <-inctimer.After(PollInterval):
 		case <-ctx.Done():
 			return fmt.Errorf("timeout reached waiting for pod IPs to be in IPCache of Cilium pod %s (last error: %w)",
 				agent.Name(), err)
