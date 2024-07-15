9b7cbd7952f739d3a4abdfaffcd6e3b2bc0f260c
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-07-15 10:43:55 +0200

    lbipam: improve status message for incompatible services
    Currently, when requesting a specific IP from LB IPAM, the logic validates whether
    the all services requesting the same IP are compatible with each other.
    
    If they are incompatible, it always results in the following status message.
    
    ```
    message: The IP '100.64.0.102' is already allocated to another service with a different sharing key
    reason: already_allocated_different_sharing_key
    ```
    
    It's the same reason (different sharing key), even though the underlying reason might be a different one.
    
    - Different sharing key
    - Different and not permitted namespace
    - Same port
    - Different ExternalTrafficPolicy
    - Same ExternalTrafficPolicy Local but selecting different set of pods
    
    Therefore, this commit improves the message by adding the actual reason to the message.
    
    In addition, the reason changed from `already_allocated_different_sharing_key` to `already_allocated_incompatible_service`.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- operator/pkg/lbipam/service_store.go
+++ operator/pkg/lbipam/service_store.go
@@ -86,18 +86,18 @@ type ServiceView struct {
 }
 
 // isCompatible checks if two services are compatible for sharing an IP.
-func (sv *ServiceView) isCompatible(osv *ServiceView) bool {
+func (sv *ServiceView) isCompatible(osv *ServiceView) (bool, string) {
 	// They have the same sharing key.
 	if sv.SharingKey != osv.SharingKey {
-		return false
+		return false, "different sharing key"
 	}
 
 	// Services are namespaced, so services are only compatible if they are in the same namespace.
-	// This is for security reasons, otherwise a bad tennant could use a service in another namespace.
+	// This is for security reasons, otherwise a bad tenant could use a service in another namespace.
 	// We still allow cross-namespace sharing if specifically allowed on both services.
 	if sv.Namespace != osv.Namespace {
 		if !slices.Contains(sv.SharingCrossNamespace, osv.Namespace) && !slices.Contains(sv.SharingCrossNamespace, ciliumSvcLBISKCNWildward) || !slices.Contains(osv.SharingCrossNamespace, sv.Namespace) && !slices.Contains(osv.SharingCrossNamespace, ciliumSvcLBISKCNWildward) {
-			return false
+			return false, "different and not permitted namespace"
 		}
 	}
 
@@ -107,7 +107,7 @@ func (sv *ServiceView) isCompatible(osv *ServiceView) bool {
 	for _, port1 := range sv.Ports {
 		for _, port2 := range osv.Ports {
 			if port1.Port == port2.Port {
-				return false
+				return false, "same port"
 			}
 		}
 	}
@@ -116,7 +116,7 @@ func (sv *ServiceView) isCompatible(osv *ServiceView) bool {
 	// If this were not the case, then we could end up in a situation directing traffic to a node which doesn't
 	// have the pod running on it for one of the services with an `local` external traffic policy.
 	if sv.ExternalTrafficPolicy != osv.ExternalTrafficPolicy {
-		return false
+		return false, "different ExternalTrafficPolicy"
 	}
 
 	// If both services have a 'local' external traffic policy, then they must select the same set of pods.
@@ -126,17 +126,17 @@ func (sv *ServiceView) isCompatible(osv *ServiceView) bool {
 		// If any of the two service doesn't select any pods with the selector, it likely uses an endpoints object to
 		// link the service to pods. LB-IPAM isn't smart enough to handle this case (yet), so we don't allow it.
 		if len(sv.Selector) == 0 || len(osv.Selector) == 0 {
-			return false
+			return false, "compatible ExternalTrafficPolicy local but selecting different set of pods"
 		}
 
 		// If both use selectors, and they are not the same, then the services are not compatible.
 		if !maps.Equal(sv.Selector, osv.Selector) {
-			return false
+			return false, "compatible ExternalTrafficPolicy local but selecting different set of pods"
 		}
 	}
 
 	// If we can't find any reason to disqualify the services, then they are compatible.
-	return true
+	return true, ""
 }
 
 func (sv *ServiceView) isSatisfied() bool {
