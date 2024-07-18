6bbcaa8dadf46a653661269dd0745b9d475ae038
Ryan Drew <ryan.drew@isovalent.com>
2024-07-18 09:44:52 -0600

    ci, scale: Add EGW masquerade delay scale test
    This commit introduces a new scale test for Egress Gateway. At a high
    level, the scale test creates a series of Pods that attempt to
    communicate to an external target through an EGW node. Metrics are
    tracked by each client to record the client's experience in connecting
    to the external target, such as how long it took for a connection to be
    masqueraded through an EGW node, and how many connections could be
    established before masquerading took place.
    
    The GHA workflow in this commit creates an EKS cluster to host the test.
    A node is deployed without Cilium to host the external target. Nodes
    hosting client Pods are configured to have a pod limit of 100, which
    respects the upstream Kubernetes guidelines of 110 pods per node while
    adding some buffer. Cilium is installed with prefix delegation enabled
    to help limit the number of nodes that are required to be deployed. The
    workflow will run the masquerade delay test, as well as the masquerade
    delay baseline test. The latter is the exact same test as the former,
    but with no EGW policy deployed, causing client pods to connect directly
    to the external target without being routed through an EGW node.
    
    Signed-off-by: Ryan Drew <ryan.drew@isovalent.com>

---
+++ .github/actions/cl2-modules/egw/preflight.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -xeo pipefail
+
+if [ -z "${EGW_IMAGE_TAG}"]; then
+    echo "FATAL: \$EGW_IMAGE_TAG must be defined"
+    exit 1
+fi
+
+fill_template() {
+    echo $EGW_EXTERNAL_TARGET_CIDR
+    cat $1 | envsubst | tee > "${1//\.tmpl/}"
+}
+
+get_node_internal_ip() {
+    kubectl get node -l $1 -ojsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}'
+}
+
+if [ "$1" != "baseline" ]; then
+    external_target_ip=$(get_node_internal_ip "role.scaffolding/egw-node=true")
+    export EGW_ALLOWED_CIDR="${external_target_ip}/32"
+else
+    export EGW_ALLOWED_CIDR="0.0.0.0/0"
+fi
+
+egw_node_ip=$(get_node_internal_ip "cilium.io/no-schedule=true")
+export EGW_EXTERNAL_TARGET_CIDR="${egw_node_ip}/32"
+export EGW_EXTERNAL_TARGET_ADDR="${egw_node_ip}"
+
+for template in ./manifests/*.tmpl.yaml; do
+    fill_template $template
+done
+
+if [ "$1" != "baseline" ]; then
+    kubectl apply -f ./manifests/egw-policy.yaml
+else
+    kubectl delete --ignore-not-found \
+        ciliumegressgatewaypolicies.cilium.io/egw-scale-test-route-external
+fi
