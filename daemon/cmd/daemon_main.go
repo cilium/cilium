cilium/cilium

commit d26009cfea8112443f842ebcfc31b9a76ec76443

cilium: Enable load-balancer mode to connect to kube-apiserver
Allow connecting to the kube-apiserver in lb-only datapath mode. This allows
to utilize the K8s control plane for orchestration as in CNI mode and enables
persistent storage of services and backends.

Note that in this mode having local Pods in the cluster is currently not
supported and therefore the BPF code generation skipped in endpoints package.

Co-developed-by: Aditi Ghag <aditi@cilium.io>
Signed-off-by: Aditi Ghag <aditi@cilium.io>
Co-developed-by: Marco Hofstetter <marco.hofstetter@isovalent.com>
Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

daemon/cmd/daemon_main.go
@@ -1376,8 +1376,7 @@ func initEnv(vp *viper.Viper) {
 		}
 		option.Config.KubeProxyReplacement = option.KubeProxyReplacementFalse
 		option.Config.EnableSocketLB = true
-		// Socket-LB tracing relies on metadata that's retrieved from Kubernetes.
-		option.Config.EnableSocketLBTracing = false
+		option.Config.EnableSocketLBTracing = true
 		option.Config.EnableHostPort = false
 		option.Config.EnableNodePort = true
 		option.Config.EnableExternalIPs = true
@@ -1710,12 +1709,6 @@ func newDaemonPromise(params daemonParams) (promise.Promise[*Daemon], promise.Pr
 	// daemonCtx is the daemon-wide context cancelled when stopping.
 	daemonCtx, cancelDaemonCtx := context.WithCancel(context.Background())
 	cleaner := NewDaemonCleanup()
-
-	if option.Config.DatapathMode == datapathOption.DatapathModeLBOnly {
-		// In lb-only mode the k8s client is not used, even if its configuration
-		// is available, so disable it here before it starts.
-		params.Clientset.Disable()
-	}
 
 	var daemon *Daemon
 	var wg sync.WaitGroup

daemon/cmd/api_handlers.go
@@ -109,38 +109,38 @@ func ciliumAPIHandlers(dp promise.Promise[*Daemon], cfg *option.DaemonConfig, _ 
 	// /cluster/nodes
 	out.DaemonGetClusterNodesHandler = NewGetClusterNodesHandler(dp)
 
+	// /endpoint/
+	out.EndpointDeleteEndpointHandler = wrapAPIHandler(dp, deleteEndpointHandler)
+	out.EndpointGetEndpointHandler = wrapAPIHandler(dp, getEndpointHandler)
+
+	// /endpoint/{id}
+	out.EndpointGetEndpointIDHandler = wrapAPIHandler(dp, getEndpointIDHandler)
+	out.EndpointPutEndpointIDHandler = wrapAPIHandler(dp, putEndpointIDHandler)
+	out.EndpointPatchEndpointIDHandler = wrapAPIHandler(dp, patchEndpointIDHandler)
+	out.EndpointDeleteEndpointIDHandler = wrapAPIHandler(dp, deleteEndpointIDHandler)
+
+	// /endpoint/{id}config/
+	out.EndpointGetEndpointIDConfigHandler = wrapAPIHandler(dp, getEndpointIDConfigHandler)
+	out.EndpointPatchEndpointIDConfigHandler = wrapAPIHandler(dp, patchEndpointIDConfigHandler)
+
+	// /endpoint/{id}/labels/
+	out.EndpointGetEndpointIDLabelsHandler = wrapAPIHandler(dp, getEndpointIDLabelsHandler)
+	out.EndpointPatchEndpointIDLabelsHandler = wrapAPIHandler(dp, putEndpointIDLabelsHandler)
+
+	// /endpoint/{id}/log/
+	out.EndpointGetEndpointIDLogHandler = wrapAPIHandler(dp, getEndpointIDLogHandler)
+
+	// /endpoint/{id}/healthz
+	out.EndpointGetEndpointIDHealthzHandler = wrapAPIHandler(dp, getEndpointIDHealthzHandler)
+
+	// /identity/
+	out.PolicyGetIdentityHandler = wrapAPIHandler(dp, getIdentityHandler)
+	out.PolicyGetIdentityIDHandler = wrapAPIHandler(dp, getIdentityIDHandler)
+
+	// /identity/endpoints
+	out.PolicyGetIdentityEndpointsHandler = wrapAPIHandler(dp, getIdentityEndpointsHandler)
+
 	if cfg.DatapathMode != datapathOption.DatapathModeLBOnly {
-		// /endpoint/
-		out.EndpointDeleteEndpointHandler = wrapAPIHandler(dp, deleteEndpointHandler)
-		out.EndpointGetEndpointHandler = wrapAPIHandler(dp, getEndpointHandler)
-
-		// /endpoint/{id}
-		out.EndpointGetEndpointIDHandler = wrapAPIHandler(dp, getEndpointIDHandler)
-		out.EndpointPutEndpointIDHandler = wrapAPIHandler(dp, putEndpointIDHandler)
-		out.EndpointPatchEndpointIDHandler = wrapAPIHandler(dp, patchEndpointIDHandler)
-		out.EndpointDeleteEndpointIDHandler = wrapAPIHandler(dp, deleteEndpointIDHandler)
-
-		// /endpoint/{id}config/
-		out.EndpointGetEndpointIDConfigHandler = wrapAPIHandler(dp, getEndpointIDConfigHandler)
-		out.EndpointPatchEndpointIDConfigHandler = wrapAPIHandler(dp, patchEndpointIDConfigHandler)
-
-		// /endpoint/{id}/labels/
-		out.EndpointGetEndpointIDLabelsHandler = wrapAPIHandler(dp, getEndpointIDLabelsHandler)
-		out.EndpointPatchEndpointIDLabelsHandler = wrapAPIHandler(dp, putEndpointIDLabelsHandler)
-
-		// /endpoint/{id}/log/
-		out.EndpointGetEndpointIDLogHandler = wrapAPIHandler(dp, getEndpointIDLogHandler)
-
-		// /endpoint/{id}/healthz
-		out.EndpointGetEndpointIDHealthzHandler = wrapAPIHandler(dp, getEndpointIDHealthzHandler)
-
-		// /identity/
-		out.PolicyGetIdentityHandler = wrapAPIHandler(dp, getIdentityHandler)
-		out.PolicyGetIdentityIDHandler = wrapAPIHandler(dp, getIdentityIDHandler)
-
-		// /identity/endpoints
-		out.PolicyGetIdentityEndpointsHandler = wrapAPIHandler(dp, getIdentityEndpointsHandler)
-
 		// /policy/
 		out.PolicyGetPolicyHandler = wrapAPIHandler(dp, getPolicyHandler)
 		out.PolicyPutPolicyHandler = wrapAPIHandler(dp, putPolicyHandler)

daemon/cmd/daemon.go
@@ -34,7 +34,6 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/linux/bigtcp"
 	"github.com/cilium/cilium/pkg/datapath/linux/probes"
 	linuxrouting "github.com/cilium/cilium/pkg/datapath/linux/routing"
-	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	datapathTables "github.com/cilium/cilium/pkg/datapath/tables"
 	"github.com/cilium/cilium/pkg/datapath/tunnel"
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
@@ -296,8 +295,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 	bootstrapStats.daemonInit.Start()
 
 	// Validate configuration options that depend on other cells.
-	if option.Config.IdentityAllocationMode == option.IdentityAllocationModeCRD && !params.Clientset.IsEnabled() &&
-		option.Config.DatapathMode != datapathOption.DatapathModeLBOnly {
+	if option.Config.IdentityAllocationMode == option.IdentityAllocationModeCRD && !params.Clientset.IsEnabled() {
 		return nil, nil, fmt.Errorf("CRD Identity allocation mode requires k8s to be configured")
 	}
 
@@ -831,15 +829,13 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		d.ipam.IPv4Allocator.RestoreFinished()
 	}
 
-	if option.Config.DatapathMode != datapathOption.DatapathModeLBOnly {
-		// This needs to be done after the node addressing has been configured
-		// as the node address is required as suffix.
-		// well known identities have already been initialized above.
-		// Ignore the channel returned by this function, as we want the global
-		// identity allocator to run asynchronously.
-		realIdentityAllocator := d.identityAllocator
-		realIdentityAllocator.InitIdentityAllocator(params.Clientset)
-	}
+	// This needs to be done after the node addressing has been configured
+	// as the node address is required as suffix.
+	// well known identities have already been initialized above.
+	// Ignore the channel returned by this function, as we want the global
+	// identity allocator to run asynchronously.
+	realIdentityAllocator := d.identityAllocator
+	realIdentityAllocator.InitIdentityAllocator(params.Clientset)
 
 	// Must be done at least after initializing BPF LB-related maps
 	// (lbmap.Init()).

daemon/cmd/watchdogs.go
@@ -12,8 +12,10 @@ import (
 
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/datapath/loader"
+	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/promise"
 	"github.com/cilium/cilium/pkg/time"
 )
@@ -58,7 +60,12 @@ func registerEndpointBPFProgWatchdog(p epBPFProgWatchdogParams) {
 	if p.Config.EndpointBPFProgWatchdogInterval == 0 {
 		return
 	}
-
+	// The watchdog works only for tc BPF, but not when the L4LB has
+	// XDP acceleration enabled. While we could enable it for tc BPF
+	// mode, lets keep same behavior on both settings.
+	if option.Config.DatapathMode == datapathOption.DatapathModeLBOnly {
+		return
+	}
 	var (
 		ctx, cancel = context.WithCancel(context.Background())
 		mgr         = controller.NewManager()

pkg/endpoint/bpf.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
+	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 	"github.com/cilium/cilium/pkg/fqdn/restore"
@@ -547,7 +548,8 @@ func (e *Endpoint) regenerateBPF(regenContext *regenerationContext) (revnum uint
 	// reverted, and execute it in case of regeneration success.
 	defer func() {
 		// Ignore finalizing of proxy state in dry mode.
-		if !e.isProperty(PropertyFakeEndpoint) {
+		if !e.isProperty(PropertyFakeEndpoint) &&
+			option.Config.DatapathMode != datapathOption.DatapathModeLBOnly {
 			e.finalizeProxyState(regenContext, reterr)
 		}
 	}()
@@ -556,8 +558,11 @@ func (e *Endpoint) regenerateBPF(regenContext *regenerationContext) (revnum uint
 		return 0, err
 	}
 
-	// No need to compile BPF in dry mode.
-	if e.isProperty(PropertyFakeEndpoint) {
+	// No need to compile BPF in dry mode. Also, in lb-only mode we do not
+	// support local Pods on the worker node, hence endpoint BPF regeneration
+	// is skipped everywhere.
+	if e.isProperty(PropertyFakeEndpoint) ||
+		(option.Config.DatapathMode == datapathOption.DatapathModeLBOnly && !datapathRegenCtxt.epInfoCache.IsHost()) {
 		return e.nextPolicyRevision, nil
 	}
 
@@ -719,7 +724,8 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext, rul
 	// pre-existing connections using that IP are now invalid.
 	if !e.ctCleaned {
 		go func() {
-			if !e.isProperty(PropertyFakeEndpoint) {
+			if !e.isProperty(PropertyFakeEndpoint) &&
+				option.Config.DatapathMode != datapathOption.DatapathModeLBOnly {
 				ipv4 := option.Config.EnableIPv4
 				ipv6 := option.Config.EnableIPv6
 				exists := ctmap.Exists(nil, ipv4, ipv6)

pkg/endpoint/policy.go
@@ -18,6 +18,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
+	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	dptypes "github.com/cilium/cilium/pkg/datapath/types"
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
@@ -517,7 +518,8 @@ func (e *Endpoint) updateRealizedState(stats *regenerationStatistics, origDir st
 
 	// Start periodic background full reconciliation of the policy map.
 	// Does nothing if it has already been started.
-	if !e.isProperty(PropertyFakeEndpoint) {
+	if !e.isProperty(PropertyFakeEndpoint) &&
+		option.Config.DatapathMode != datapathOption.DatapathModeLBOnly {
 		e.startSyncPolicyMapController()
 	}
 

commit 5c36a7cb611058472447f89a4f6fa3e0bea3ed3c

cilium: Do not explicitly enable socket LB tracing in LB only mode
Instead, let the user drive this when needed. The value in LB only mode
for the tracing is also limited.

Fixes: d26009cfea8 ("cilium: Enable load-balancer mode to connect to kube-apiserver")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

daemon/cmd/daemon_main.go
@@ -1384,7 +1384,6 @@ func initEnv(vp *viper.Viper) {
 		}
 		option.Config.KubeProxyReplacement = option.KubeProxyReplacementFalse
 		option.Config.EnableSocketLB = true
-		option.Config.EnableSocketLBTracing = true
 		option.Config.EnableSocketLBPodConnectionTermination = true
 		option.Config.EnableHostPort = false
 		option.Config.EnableNodePort = true