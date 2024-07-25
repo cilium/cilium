01fbe9a6e01b681994f91b0ce6dde8ea086e39e2
Yusuke Suzuki <yusuke.suzuki@isovalent.com>
2024-07-26 00:27:26 +0900

    bugtool: dump cilium_skip_lb{4,6}
    Collect cilium_skip_lb{4,6} maps in sysdump
    
    fixes: #33901
    
    Signed-off-by: Yusuke Suzuki <yusuke.suzuki@isovalent.com>

--- bugtool/cmd/configuration.go
+++ bugtool/cmd/configuration.go
@@ -187,6 +187,8 @@ func defaultCommands(confDir string, cmdDir string, k8sPods []string) []string {
 		"tc/globals/cilium_l2_responder_v4",
 		"tc/globals/cilium_ratelimit",
 		"tc/globals/cilium_ratelimit_metrics",
+		"tc/globals/cilium_skip_lb4",
+		"tc/globals/cilium_skip_lb6",
 	}
 	commands = append(commands, bpfMapDumpCommands(bpfMapsPath)...)
 
