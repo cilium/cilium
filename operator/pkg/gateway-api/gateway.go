414a96b53d51ef6e6645c44426e26bc8e7c7c059
Tam Mach <tam.mach@cilium.io>
2024-07-26 12:33:36 +0000

    gateway-api: Enqueue gateway for Reference Grant changes
    [ upstream commit ed3dfa0aab8b80f7e841a6d49d2a990ac2dca053 ]
    
    [ backporter's notes: resolved conflicts due to v1.15 still using
      `logrus` for logging. ]
    
    This commit is to make sure that the reconciliation loop is kicked off
    for Gateway object if there is any change in ReferenceGrant (mainly for
    SecretObjectReference).
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- operator/pkg/gateway-api/gateway.go
+++ operator/pkg/gateway-api/gateway.go
@@ -20,6 +20,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 	gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
+	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 
 	"github.com/cilium/cilium/operator/pkg/gateway-api/helpers"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
@@ -90,6 +91,8 @@ func (r *gatewayReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		// Watch related namespace in allowed namespaces
 		Watches(&corev1.Namespace{},
 			r.enqueueRequestForAllowedNamespace()).
+		// Watch for changes to Reference Grants
+		Watches(&gatewayv1beta1.ReferenceGrant{}, r.enqueueRequestForReferenceGrant()).
 		// Watch created and owned resources
 		Owns(&ciliumv2.CiliumEnvoyConfig{}).
 		Owns(&corev1.Service{}).
@@ -287,4 +290,36 @@ func (r *gatewayReconciler) enqueueRequestForAllowedNamespace() handler.EventHan
 
 func (r *gatewayReconciler) usedInGateway(obj client.Object) bool {
 	return len(getGatewaysForSecret(context.Background(), r.Client, obj)) > 0
+}
+
+func (r *gatewayReconciler) enqueueRequestForReferenceGrant() handler.EventHandler {
+	return handler.EnqueueRequestsFromMapFunc(r.enqueueAll())
+}
+
+func (r *gatewayReconciler) enqueueAll() handler.MapFunc {
+	return func(ctx context.Context, o client.Object) []reconcile.Request {
+		scopedLog := log.WithFields(logrus.Fields{
+			logfields.Controller: "gateway",
+			logfields.Resource:   client.ObjectKeyFromObject(o),
+		})
+		list := &gatewayv1.GatewayList{}
+
+		if err := r.Client.List(ctx, list, &client.ListOptions{}); err != nil {
+			scopedLog.WithError(err).Error("Failed to list Gateway")
+			return []reconcile.Request{}
+		}
+
+		requests := make([]reconcile.Request, 0, len(list.Items))
+		for _, item := range list.Items {
+			gw := client.ObjectKey{
+				Namespace: item.GetNamespace(),
+				Name:      item.GetName(),
+			}
+			requests = append(requests, reconcile.Request{
+				NamespacedName: gw,
+			})
+			scopedLog.Info("Enqueued Gateway for resource", gateway, gw)
+		}
+		return requests
+	}
 }
