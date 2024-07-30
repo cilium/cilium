cilium/cilium

commit e33b9f9bff201007879290a711b4fbd5b63fde0c

kvstore: add support for paginated lists in etcd.ListAndWatch
This commit extends the ListAndWatch implementation of the etcd client
with the support for pagination during the initial list operation. This
allows to retrieve the initial state in batches, reducing the overall
load in case a large number of entries needs to be obtained. The batch
size can be configured through the `etcd.limit` option, which defaults
to 256.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/kvstore/etcd.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/blang/semver/v4"
 	"github.com/sirupsen/logrus"
+	"go.etcd.io/etcd/api/v3/mvccpb"
 	v3rpcErrors "go.etcd.io/etcd/api/v3/v3rpc/rpctypes"
 	"go.etcd.io/etcd/client/pkg/v3/tlsutil"
 	client "go.etcd.io/etcd/client/v3"
@@ -49,6 +50,10 @@ const (
 
 	// EtcdRateLimitOption specifies maximum kv operations per second
 	EtcdRateLimitOption = "etcd.qps"
+
+	// EtcdListLimitOption limits the number of results retrieved in one batch
+	// by ListAndWatch operations. A 0 value equals to no limit.
+	EtcdListLimitOption = "etcd.limit"
 
 	minRequiredVersionStr = ">=3.1.0"
 
@@ -128,6 +133,13 @@ func newEtcdModule() backendModule {
 					return err
 				},
 			},
+			EtcdListLimitOption: &backendOption{
+				description: "Max number of results retrieved in one batch by ListAndWatch operations (0 = no limit)",
+				validate: func(v string) error {
+					_, err := strconv.Atoi(v)
+					return err
+				},
+			},
 		},
 	}
 }
@@ -171,6 +183,7 @@ type clientOptions struct {
 	KeepAliveHeartbeat time.Duration
 	KeepAliveTimeout   time.Duration
 	RateLimit          int
+	ListBatchSize      int
 }
 
 func (e *etcdModule) newClient(ctx context.Context, opts *ExtraOptions) (BackendOperations, chan error) {
@@ -180,10 +193,15 @@ func (e *etcdModule) newClient(ctx context.Context, opts *ExtraOptions) (Backend
 		KeepAliveHeartbeat: 15 * time.Second,
 		KeepAliveTimeout:   25 * time.Second,
 		RateLimit:          defaults.KVstoreQPS,
+		ListBatchSize:      256,
 	}
 
 	if o, ok := e.opts[EtcdRateLimitOption]; ok && o.value != "" {
 		clientOptions.RateLimit, _ = strconv.Atoi(o.value)
+	}
+
+	if o, ok := e.opts[EtcdListLimitOption]; ok && o.value != "" {
+		clientOptions.ListBatchSize, _ = strconv.Atoi(o.value)
 	}
 
 	if o, ok := e.opts[etcdOptionKeepAliveTimeout]; ok && o.value != "" {
@@ -227,6 +245,7 @@ func (e *etcdModule) newClient(ctx context.Context, opts *ExtraOptions) (Backend
 		"KeepAliveHeartbeat": clientOptions.KeepAliveHeartbeat,
 		"KeepAliveTimeout":   clientOptions.KeepAliveTimeout,
 		"RateLimit":          clientOptions.RateLimit,
+		"ListLimit":          clientOptions.ListBatchSize,
 	}).Info("Creating etcd client")
 
 	for {
@@ -311,7 +330,8 @@ type etcdClient struct {
 
 	extraOptions *ExtraOptions
 
-	limiter *rate.Limiter
+	limiter       *rate.Limiter
+	listBatchSize int
 
 	lastHeartbeat time.Time
 }
@@ -705,6 +725,7 @@ func connectEtcdClient(ctx context.Context, config *client.Config, cfgPath strin
 		stopStatusChecker:    make(chan struct{}),
 		extraOptions:         opts,
 		limiter:              rate.NewLimiter(rate.Limit(clientOptions.RateLimit), clientOptions.RateLimit),
+		listBatchSize:        clientOptions.ListBatchSize,
 		statusCheckErrors:    make(chan error, 128),
 	}
 
@@ -982,8 +1003,7 @@ reList:
 		}
 
 		e.limiter.Wait(ctx)
-		res, err := e.client.Get(ctx, w.Prefix, client.WithPrefix(),
-			client.WithSerializable())
+		kvs, revision, err := e.paginatedList(ctx, scopedLog, w.Prefix)
 		if err != nil {
 			scopedLog.WithError(Hint(err)).Warn("Unable to list keys before starting watcher")
 			errLimiter.Wait(ctx)
@@ -991,33 +1011,25 @@ reList:
 		}
 		errLimiter.Reset()
 
-		nextRev := res.Header.Revision + 1
-		scopedLog.Debugf("List response from etcd len=%d: %+v", res.Count, res)
-
-		if res.Count > 0 {
-			for _, key := range res.Kvs {
-				t := EventTypeCreate
-				if localCache.Exists(key.Key) {
-					t = EventTypeModify
-				}
-
-				localCache.MarkInUse(key.Key)
-				scopedLog.Debugf("Emitting list result as %s event for %s=%s", t, key.Key, key.Value)
-
-				queueStart := spanstat.Start()
-				w.Events <- KeyValueEvent{
-					Key:   string(key.Key),
-					Value: key.Value,
-					Typ:   t,
-				}
-				trackEventQueued(string(key.Key), t, queueStart.End(true).Total())
+		for _, key := range kvs {
+			t := EventTypeCreate
+			if localCache.Exists(key.Key) {
+				t = EventTypeModify
 			}
+
+			localCache.MarkInUse(key.Key)
+			scopedLog.Debugf("Emitting list result as %s event for %s=%s", t, key.Key, key.Value)
+
+			queueStart := spanstat.Start()
+			w.Events <- KeyValueEvent{
+				Key:   string(key.Key),
+				Value: key.Value,
+				Typ:   t,
+			}
+			trackEventQueued(string(key.Key), t, queueStart.End(true).Total())
 		}
 
-		// More keys to be read, call Get() again
-		if res.More {
-			continue
-		}
+		nextRev := revision + 1
 
 		// Send out deletion events for all keys that were deleted
 		// between our last known revision and the latest revision
@@ -1108,6 +1120,39 @@ reList:
 				}
 			}
 		}
+	}
+}
+
+func (e *etcdClient) paginatedList(ctx context.Context, log *logrus.Entry, prefix string) (kvs []*mvccpb.KeyValue, revision int64, err error) {
+	start, end := prefix, client.GetPrefixRangeEnd(prefix)
+
+	for {
+		res, err := e.client.Get(ctx, start, client.WithRange(end),
+			client.WithSort(client.SortByKey, client.SortAscend),
+			client.WithRev(revision), client.WithSerializable(),
+			client.WithLimit(int64(e.listBatchSize)),
+		)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		log.WithFields(logrus.Fields{
+			fieldNumEntries:       len(res.Kvs),
+			fieldRemainingEntries: res.Count - int64(len(res.Kvs)),
+		}).Debug("Received list response from etcd")
+
+		if kvs == nil {
+			kvs = make([]*mvccpb.KeyValue, 0, res.Count)
+		}
+
+		kvs = append(kvs, res.Kvs...)
+
+		revision = res.Header.Revision
+		if !res.More || len(res.Kvs) == 0 {
+			return kvs, revision, nil
+		}
+
+		start = string(res.Kvs[len(res.Kvs)-1].Key) + "\x00"
 	}
 }
 

pkg/kvstore/etcd_test.go
@@ -2016,3 +2016,63 @@ func (e *EtcdRateLimiterSuite) TestRateLimiter(c *C) {
 		}
 	}
 }
+
+func (e *EtcdSuite) TestPaginatedList(c *C) {
+	const prefix = "list/paginated"
+	ctx := context.Background()
+
+	run := func(batch int) {
+		keys := map[string]struct{}{
+			path.Join(prefix, "immortal-finch"):   {},
+			path.Join(prefix, "rare-goshawk"):     {},
+			path.Join(prefix, "cunning-bison"):    {},
+			path.Join(prefix, "amusing-tick"):     {},
+			path.Join(prefix, "prepared-shark"):   {},
+			path.Join(prefix, "exciting-mustang"): {},
+			path.Join(prefix, "ethical-ibex"):     {},
+			path.Join(prefix, "accepted-kite"):    {},
+			path.Join(prefix, "model-javelin"):    {},
+			path.Join(prefix, "inviting-hog"):     {},
+		}
+
+		defer func(previous int) {
+			Client().(*etcdClient).listBatchSize = previous
+			c.Assert(Client().DeletePrefix(ctx, prefix), IsNil)
+		}(Client().(*etcdClient).listBatchSize)
+		Client().(*etcdClient).listBatchSize = batch
+
+		var expected int64
+		for key := range keys {
+			res, err := Client().(*etcdClient).client.Put(ctx, key, "value")
+			expected = res.Header.Revision
+			c.Assert(err, IsNil)
+		}
+
+		kvs, found, err := Client().(*etcdClient).paginatedList(ctx, log, prefix)
+		c.Assert(err, IsNil)
+
+		for _, kv := range kvs {
+			key := string(kv.Key)
+			if _, ok := keys[key]; !ok {
+				c.Fatalf("Retrieved unexpected key, key: %s", key)
+			}
+			delete(keys, key)
+		}
+
+		c.Assert(keys, HasLen, 0)
+
+		// There is no guarantee that found == expected, because new operations might have occurred in parallel.
+		if found < expected {
+			c.Fatalf("Next revision (%d) is lower than the one of the last update (%d)", found, expected)
+		}
+	}
+
+	// Batch size = 1
+	run(1)
+
+	// Batch size = 4
+	run(4)
+
+	// Batch size = 11
+	run(11)
+}

pkg/kvstore/logfields.go
@@ -38,6 +38,9 @@ const (
 	// fieldNumEntries is the number of entries in the result
 	fieldNumEntries = "numEntries"
 
+	// fieldRemainingEntries is the number of entries still to be retrieved
+	fieldRemainingEntries = "remainingEntries"
+
 	// fieldAttachLease is true if the key must be attached to a lease
 	fieldAttachLease = "attachLease"
 

commit 48266fae5e3605ecbb8ac5cdac46d20d255edb1d

etcd: fix paginated list missing events with parallel operations
Currently, the etcd paginatedList implementation is affected by a bug
that can lead to missing events of both upsertions and deletions that
are performed between the end of the first Get call and the last Get
call. Indeed, the tracked revision is incorrectly updated after every
Get call, and etcd always returns the current revision, regardless of
whether WithRev is actually requesting a specific revision. In turn,
this leads to subsequent Get calls targeting different revisions,
hence missing all the events happened in between for the already
processed chunks of the prefix, as the watch operation is eventually
started from the latest retrieved revision.

Let's address this by consistently using the same revision during the
entire paginatedList execution, to ensure that we correctly list all
entries at that revision, and subsequently start watching events from
the next one. Additionally, let's update the associated unit test
to additionally cover the parallel operations case and prevent future
regressions in this respect.

This bug affected both Cilium running in KVStore mode and clustermesh,
although the race condition window is sufficiently short to trigger
its occurrence only rarely and in high scale environments.

Fixes: e33b9f9bff20 ("kvstore: add support for paginated lists in etcd.ListAndWatch")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/kvstore/etcd.go
@@ -981,7 +981,14 @@ func (e *etcdClient) paginatedList(ctx context.Context, log *logrus.Entry, prefi
 
 		kvs = append(kvs, res.Kvs...)
 
-		revision = res.Header.Revision
+		// Do not modify the revision once set, as subsequent Get queries may
+		// return higher revisions in case other operations are performed in
+		// parallel (regardless of whether we specify WithRev), leading to
+		// possibly missing the events happened in the meantime.
+		if revision == 0 {
+			revision = res.Header.Revision
+		}
+
 		if !res.More || len(res.Kvs) == 0 {
 			return kvs, revision, nil
 		}

pkg/kvstore/etcd_test.go
@@ -14,6 +14,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 	etcdAPI "go.etcd.io/etcd/client/v3"
+	"golang.org/x/exp/maps"
+	"k8s.io/apimachinery/pkg/util/rand"
 
 	"github.com/cilium/cilium/pkg/testutils"
 )
@@ -1408,6 +1410,20 @@ func testEtcdRateLimiter(t *testing.T, qps, count int, cmp func(require.TestingT
 	}
 }
 
+type kvWrapper struct {
+	etcdAPI.KV
+	postGet func(context.Context) error
+}
+
+func (kvw *kvWrapper) Get(ctx context.Context, key string, opts ...etcdAPI.OpOption) (*etcdAPI.GetResponse, error) {
+	res, err := kvw.KV.Get(ctx, key, opts...)
+	if err != nil {
+		return res, err
+	}
+
+	return res, kvw.postGet(ctx)
+}
+
 func TestPaginatedList(t *testing.T) {
 	testutils.IntegrationTest(t)
 	SetupDummyWithConfigOpts(t, "etcd", opts("etcd"))
@@ -1415,7 +1431,8 @@ func TestPaginatedList(t *testing.T) {
 	const prefix = "list/paginated"
 	ctx := context.Background()
 
-	run := func(batch int) {
+	run := func(t *testing.T, batch int, withParallelOps bool) {
+		cl := Client().(*etcdClient)
 		keys := map[string]struct{}{
 			path.Join(prefix, "immortal-finch"):   {},
 			path.Join(prefix, "rare-goshawk"):     {},
@@ -1430,19 +1447,45 @@ func TestPaginatedList(t *testing.T) {
 		}
 
 		defer func(previous int) {
-			Client().(*etcdClient).listBatchSize = previous
-			require.Nil(t, Client().DeletePrefix(ctx, prefix))
-		}(Client().(*etcdClient).listBatchSize)
-		Client().(*etcdClient).listBatchSize = batch
+			cl.listBatchSize = previous
+			require.Nil(t, cl.DeletePrefix(ctx, prefix))
+		}(cl.listBatchSize)
+		cl.listBatchSize = batch
+
+		var next int64
+		if withParallelOps {
+			pkv := cl.client.KV
+			defer func() { cl.client.KV = pkv }()
+
+			cl.client.KV = &kvWrapper{
+				KV: pkv,
+				// paginatedList should observe neither upsertions nor deletions
+				// performed after that the initial chunk of entries was retrieved.
+				postGet: func(ctx context.Context) error {
+					key := path.Join(prefix, rand.String(10))
+					res, err := cl.client.Put(ctx, key, "value")
+					if err != nil {
+						return err
+					}
+
+					if next == 0 {
+						next = res.Header.Revision
+					}
+
+					_, err = cl.client.Delete(ctx, maps.Keys(keys)[0])
+					return err
+				},
+			}
+		}
 
 		var expected int64
 		for key := range keys {
-			res, err := Client().(*etcdClient).client.Put(ctx, key, "value")
+			res, err := cl.client.Put(ctx, key, "value")
 			expected = res.Header.Revision
 			require.NoError(t, err)
 		}
 
-		kvs, found, err := Client().(*etcdClient).paginatedList(ctx, log, prefix)
+		kvs, found, err := cl.paginatedList(ctx, log, prefix)
 		require.NoError(t, err)
 
 		for _, kv := range kvs {
@@ -1459,14 +1502,16 @@ func TestPaginatedList(t *testing.T) {
 		if found < expected {
 			t.Fatalf("Next revision (%d) is lower than the one of the last update (%d)", found, expected)
 		}
+
+		if withParallelOps && found >= next {
+			t.Fatalf("Next revision (%d) is higher than the one of subsequent updates (%d)", found, next)
+		}
 	}
 
-	// Batch size = 1
-	run(1)
-
-	// Batch size = 4
-	run(4)
-
-	// Batch size = 11
-	run(11)
+	for _, batchSize := range []int{1, 4, 11} {
+		for _, parallelOps := range []bool{false, true} {
+			t.Run(fmt.Sprintf("batch-size-%d-parallel-ops-%t", batchSize, parallelOps),
+				func(t *testing.T) { run(t, batchSize, parallelOps) })
+		}
+	}
 }