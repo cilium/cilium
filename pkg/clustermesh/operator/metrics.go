c1a6831e9a48af0c87cca9a6111d03f9b6197f00
Arthur Outhenin-Chalandre <arthur@cri.epita.fr>
2024-07-30 18:08:29 +0200

    clustermesh: add service export read path in clustermesh/operator
    This commit adds the read path support for service exports inside the
    clustermesh/operator package like there is currently for regular
    services. This is going to be used soon by the clustermesh/mcsapi package for
    the ServiceImport sync support.
    
    Signed-off-by: Arthur Outhenin-Chalandre <arthur@cri.epita.fr>

--- pkg/clustermesh/operator/metrics.go
+++ pkg/clustermesh/operator/metrics.go
@@ -11,6 +11,8 @@ import (
 type Metrics struct {
 	// TotalGlobalServices tracks the total number of global services.
 	TotalGlobalServices metric.Vec[metric.Gauge]
+	// TotalGlobalServiceExports tracks the total number of global service exports.
+	TotalGlobalServiceExports metric.Vec[metric.Gauge]
 }
 
 func NewMetrics() Metrics {
@@ -20,6 +22,12 @@ func NewMetrics() Metrics {
 			Subsystem: subsystem,
 			Name:      "global_services",
 			Help:      "The total number of global services in the cluster mesh",
+		}, []string{metrics.LabelSourceCluster}),
+		TotalGlobalServiceExports: metric.NewGaugeVec(metric.GaugeOpts{
+			Namespace: metrics.CiliumOperatorNamespace,
+			Subsystem: subsystem,
+			Name:      "global_service_exports",
+			Help:      "The total number of MCS-API global service exports in the cluster mesh",
 		}, []string{metrics.LabelSourceCluster}),
 	}
 }
