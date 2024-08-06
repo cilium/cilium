a587d650910b76a332fd7406c33d1e1da0a5a16e
Kaczyniec <kaczynska@google.com>
2024-08-06 10:10:44 +0000

    feat: fast and standard queue in controller
        In order to allow to enforce Network Policy quicker in more critical namespaces, we propose to introduce a second queue in the controller. CES and CEP updates from namespaces listed as prioritized in the controller attribute priorityNamespaces are added to fast queue and processed first. The queueing delay metric is reported separately for both queues.
    
    Signed-off-by: Paulina Kaczynska <kaczynska@google.com>

--- operator/pkg/ciliumendpointslice/endpointslice_test.go
+++ operator/pkg/ciliumendpointslice/endpointslice_test.go
@@ -5,6 +5,8 @@ package ciliumendpointslice
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"testing"
 	"time"
 
@@ -19,7 +21,9 @@ import (
 	cilium_v2a1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/testutils"
 )
 
 func TestFCFSModeSyncCESsInLocalCache(t *testing.T) {
@@ -60,7 +64,7 @@ func TestFCFSModeSyncCESsInLocalCache(t *testing.T) {
 		reconciler:          r,
 		manager:             m,
 		rateLimit:           rateLimitConfig,
-		enqueuedAt:          make(map[CESName]time.Time),
+		enqueuedAt:          make(map[CESKey]time.Time),
 	}
 	cesController.initializeQueue()
 
@@ -84,11 +88,12 @@ func TestFCFSModeSyncCESsInLocalCache(t *testing.T) {
 		for _, cep := range ces.Endpoints {
 			cesN, _ := mapping.getCESName(NewCEPName(cep.Name, "ns"))
 			// ensure that the CEP is mapped to the correct CES
-			assert.Equal(t, cesN, NewCESName(ces.Name))
+			assert.Equal(t, cesN, CESName(ces.Name))
 		}
 	}
 
-	cesController.queue.ShutDown()
+	cesController.fastQueue.ShutDown()
+	cesController.standardQueue.ShutDown()
 	hive.Stop(tlog, context.Background())
 }
 
@@ -130,7 +135,7 @@ func TestIdentityModeSyncCESsInLocalCache(t *testing.T) {
 		reconciler:          r,
 		manager:             m,
 		rateLimit:           rateLimitConfig,
-		enqueuedAt:          make(map[CESName]time.Time),
+		enqueuedAt:          make(map[CESKey]time.Time),
 	}
 	cesController.initializeQueue()
 
@@ -151,13 +156,193 @@ func TestIdentityModeSyncCESsInLocalCache(t *testing.T) {
 		for _, cep := range ces.Endpoints {
 			cesN, _ := mapping.getCESName(NewCEPName(cep.Name, "ns"))
 			// ensure that the CEP is mapped to the correct CES
-			assert.Equal(t, cesN, NewCESName(ces.Name))
+			assert.Equal(t, cesN, CESName(ces.Name))
 			// ensure that the CES to identity mappings are correct
 			assert.Equal(t, m.cesToIdentity[cesN], cep.IdentityID)
 			assert.Contains(t, m.identityToCES[cep.IdentityID], cesN)
 		}
 	}
 
-	cesController.queue.ShutDown()
+	cesController.fastQueue.ShutDown()
+	cesController.standardQueue.ShutDown()
+	hive.Stop(tlog, context.Background())
+}
+
+func TestDifferentSpeedQueues(t *testing.T) {
+
+	var r *reconciler
+	var fakeClient k8sClient.FakeClientset
+	m := newCESManagerIdentity(2, log).(*cesManagerIdentity)
+	var ciliumEndpoint resource.Resource[*cilium_v2.CiliumEndpoint]
+	var ciliumEndpointSlice resource.Resource[*cilium_v2a1.CiliumEndpointSlice]
+	var cesMetrics *Metrics
+	hive := hive.New(
+		k8sClient.FakeClientCell,
+		k8s.ResourcesCell,
+		metrics.Metric(NewMetrics),
+		cell.Invoke(func(
+			c *k8sClient.FakeClientset,
+			cep resource.Resource[*cilium_v2.CiliumEndpoint],
+			ces resource.Resource[*cilium_v2a1.CiliumEndpointSlice],
+			metrics *Metrics,
+		) error {
+			fakeClient = *c
+			ciliumEndpoint = cep
+			ciliumEndpointSlice = ces
+			cesMetrics = metrics
+			return nil
+		}),
+	)
+	tlog := hivetest.Logger(t)
+	hive.Start(tlog, context.Background())
+
+	r = newReconciler(context.Background(), fakeClient.CiliumFakeClientset.CiliumV2alpha1(), m, log, ciliumEndpoint, ciliumEndpointSlice, cesMetrics)
+
+	rateLimitConfig, err := getRateLimitConfig(params{Cfg: defaultConfig})
+	assert.NoError(t, err)
+	cesController := &Controller{
+		logger:              log,
+		clientset:           fakeClient.Clientset,
+		ciliumEndpoint:      ciliumEndpoint,
+		ciliumEndpointSlice: ciliumEndpointSlice,
+		reconciler:          r,
+		manager:             m,
+		rateLimit:           rateLimitConfig,
+		enqueuedAt:          make(map[CESKey]time.Time),
+		metrics:             cesMetrics,
+		priorityNamespaces:  make(map[string]struct{}),
+		syncDelay:           0,
+	}
+	cesController.cond = *sync.NewCond(&lock.Mutex{})
+	cesController.context, cesController.contextCancel = context.WithCancel(context.Background())
+	cesController.priorityNamespaces["FastNamespace"] = struct{}{}
+	cesController.initializeQueue()
+	var ns string = "NotSoImportant"
+	var standardQueueLen int
+	var fastQueueLen int
+
+	for i := 0; i < 10; i++ {
+		if i == 6 {
+			ns = "FastNamespace"
+		}
+		cep1 := tu.CreateManagerEndpoint("cep1", int64(2*i+1))
+		cep2 := tu.CreateManagerEndpoint("cep2", int64(2*i))
+
+		ces := tu.CreateStoreEndpointSlice(fmt.Sprintf("ces-%d", i), ns, []cilium_v2a1.CoreCiliumEndpoint{cep1, cep2})
+
+		cesController.onSliceUpdate(ces)
+		if i < 6 {
+			standardQueueLen = i + 1
+			fastQueueLen = 0
+		} else {
+			standardQueueLen = 6
+			fastQueueLen = i - 5
+		}
+		//Ensure that the lengths of the queues after adding an element are correct
+		if err := testutils.WaitUntil(func() bool {
+			return cesController.standardQueue.Len() == standardQueueLen && cesController.fastQueue.Len() == fastQueueLen
+		}, time.Second); err != nil {
+			assert.Equal(t, standardQueueLen, cesController.standardQueue.Len())
+			assert.Equal(t, fastQueueLen, cesController.fastQueue.Len())
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		cesController.processNextWorkItem()
+		if i < 4 {
+			standardQueueLen = 6
+			fastQueueLen = 3 - i
+		} else {
+			standardQueueLen = 6 - (i - 3)
+			fastQueueLen = 0
+		}
+		//Ensure that the lengths of the queues after removing an element are correct
+		if err := testutils.WaitUntil(func() bool {
+			return cesController.standardQueue.Len() == standardQueueLen && cesController.fastQueue.Len() == fastQueueLen
+		}, time.Second); err != nil {
+			assert.Equal(t, standardQueueLen, cesController.standardQueue.Len())
+			assert.Equal(t, fastQueueLen, cesController.fastQueue.Len())
+		}
+	}
+
+	cesController.fastQueue.ShutDown()
+	cesController.standardQueue.ShutDown()
+	hive.Stop(tlog, context.Background())
+}
+
+func TestCESManagement(t *testing.T) {
+
+	var r *reconciler
+	var fakeClient k8sClient.FakeClientset
+	m := newCESManagerIdentity(2, log).(*cesManagerIdentity)
+	var ciliumEndpoint resource.Resource[*cilium_v2.CiliumEndpoint]
+	var ciliumEndpointSlice resource.Resource[*cilium_v2a1.CiliumEndpointSlice]
+	var cesMetrics *Metrics
+	hive := hive.New(
+		k8sClient.FakeClientCell,
+		k8s.ResourcesCell,
+		metrics.Metric(NewMetrics),
+		cell.Invoke(func(
+			c *k8sClient.FakeClientset,
+			cep resource.Resource[*cilium_v2.CiliumEndpoint],
+			ces resource.Resource[*cilium_v2a1.CiliumEndpointSlice],
+			metrics *Metrics,
+		) error {
+			fakeClient = *c
+			ciliumEndpoint = cep
+			ciliumEndpointSlice = ces
+			cesMetrics = metrics
+			return nil
+		}),
+	)
+	tlog := hivetest.Logger(t)
+	hive.Start(tlog, context.Background())
+
+	r = newReconciler(context.Background(), fakeClient.CiliumFakeClientset.CiliumV2alpha1(), m, log, ciliumEndpoint, ciliumEndpointSlice, cesMetrics)
+
+	rateLimitConfig, err := getRateLimitConfig(params{Cfg: defaultConfig})
+	assert.NoError(t, err)
+	cesController := &Controller{
+		logger:              log,
+		clientset:           fakeClient.Clientset,
+		ciliumEndpoint:      ciliumEndpoint,
+		ciliumEndpointSlice: ciliumEndpointSlice,
+		reconciler:          r,
+		manager:             m,
+		rateLimit:           rateLimitConfig,
+		enqueuedAt:          make(map[CESKey]time.Time),
+		metrics:             cesMetrics,
+		priorityNamespaces:  make(map[string]struct{}),
+		syncDelay:           0,
+	}
+	cesController.cond = *sync.NewCond(&lock.Mutex{})
+	cesController.context, cesController.contextCancel = context.WithCancel(context.Background())
+	cesController.initializeQueue()
+	var ns string = "ns"
+
+	cep1 := tu.CreateStoreEndpoint(fmt.Sprintf("cep-%d", 0), ns, 0)
+	cesController.onEndpointUpdate(cep1)
+	if err := testutils.WaitUntil(func() bool {
+		return cesController.standardQueue.Len() == 1
+	}, time.Second); err != nil {
+		assert.Equal(t, 1, cesController.standardQueue.Len())
+	}
+	cesController.processNextWorkItem()
+	//A CEP is enqueued and processed. Then, the same CEP (and CES) is enqueued
+	//to test if the CESStore works properly and if the associated CES can be found in the store
+	cesController.onEndpointUpdate(cep1)
+
+	queue := cesController.getQueue()
+	key, _ := queue.Get()
+	if err := testutils.WaitUntil(func() bool {
+		_, exists, _ := r.cesStore.GetByKey(NewCESKey(key.Name, "").key())
+		return exists == true
+	}, time.Second); err != nil {
+		_, exists, _ := r.cesStore.GetByKey(NewCESKey(key.Name, "").key())
+		assert.Equal(t, true, exists)
+	}
+
+	cesController.fastQueue.ShutDown()
+	cesController.standardQueue.ShutDown()
 	hive.Stop(tlog, context.Background())
 }
