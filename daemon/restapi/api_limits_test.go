cilium/cilium

commit d7ac88839768ea717ba4775b0c4c8c66f49d1e96

daemon: Move rate limiter to daemon/restapi
Make a cell for rate.APILimiterSet along with its configuration.

Since we cannot use option.NewNamedMapOptions() with cell.Config, we're
implementing it using pflag's StringToString. This seems to have the exact
behavior we want. The api_limits_test.go has test cases to confirm this.

Signed-off-by: Jussi Maki <jussi@isovalent.com>
Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

daemon/restapi/api_limits.go
@@ -1,25 +1,56 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package cmd
+package restapi
 
 import (
 	"time"
 
+	"github.com/spf13/pflag"
+
+	"github.com/cilium/cilium/pkg/hive/cell"
 	"github.com/cilium/cilium/pkg/rate"
+	ratemetrics "github.com/cilium/cilium/pkg/rate/metrics"
 )
 
+var rateLimiterCell = cell.Module(
+	"rate-limiter",
+	"Rate limiting for Cilium API requests",
+
+	cell.Config(defaultRateLimiterConfig),
+	cell.Provide(newApiRateLimiter),
+)
+
+type rateLimiterConfig struct {
+	APIRateLimit map[string]string
+}
+
+func (def rateLimiterConfig) Flags(flags *pflag.FlagSet) {
+	flags.StringToString(
+		"api-rate-limit",
+		def.APIRateLimit,
+		"API rate limiting configuration (example: --api-rate-limit endpoint-create=rate-limit:10/m,rate-burst:2)")
+}
+
+var defaultRateLimiterConfig = rateLimiterConfig{
+	APIRateLimit: make(map[string]string),
+}
+
+func newApiRateLimiter(cfg rateLimiterConfig) (*rate.APILimiterSet, error) {
+	return rate.NewAPILimiterSet(cfg.APIRateLimit, apiRateLimitDefaults, ratemetrics.APILimiterObserver())
+}
+
 const (
-	apiRequestEndpointCreate = "endpoint-create"
-	apiRequestEndpointDelete = "endpoint-delete"
-	apiRequestEndpointGet    = "endpoint-get"
-	apiRequestEndpointPatch  = "endpoint-patch"
-	apiRequestEndpointList   = "endpoint-list"
+	APIRequestEndpointCreate = "endpoint-create"
+	APIRequestEndpointDelete = "endpoint-delete"
+	APIRequestEndpointGet    = "endpoint-get"
+	APIRequestEndpointPatch  = "endpoint-patch"
+	APIRequestEndpointList   = "endpoint-list"
 )
 
 var apiRateLimitDefaults = map[string]rate.APILimiterParameters{
 	// PUT /endpoint/{id}
-	apiRequestEndpointCreate: {
+	APIRequestEndpointCreate: {
 		AutoAdjust:                  true,
 		EstimatedProcessingDuration: time.Second * 2,
 		RateLimit:                   0.5,
@@ -38,7 +69,7 @@ var apiRateLimitDefaults = map[string]rate.APILimiterParameters{
 	// the maximum number of parallel requests will grow to a larger number
 	// but it will never shrink below 4. Logging is enabled for visibility
 	// as frequency should be low.
-	apiRequestEndpointDelete: {
+	APIRequestEndpointDelete: {
 		EstimatedProcessingDuration: 200 * time.Millisecond,
 		AutoAdjust:                  true,
 		ParallelRequests:            4,
@@ -52,7 +83,7 @@ var apiRateLimitDefaults = map[string]rate.APILimiterParameters{
 	//
 	// All GET calls to endpoint attributes are grouped together and rate
 	// limited.
-	apiRequestEndpointGet: {
+	APIRequestEndpointGet: {
 		AutoAdjust:                  true,
 		EstimatedProcessingDuration: time.Millisecond * 200,
 		RateLimit:                   4.0,
@@ -70,7 +101,7 @@ var apiRateLimitDefaults = map[string]rate.APILimiterParameters{
 	// group as they are less likely to be expensive. They can be expensive
 	// though if datapath regenerations are required. Logging is enabled
 	// for visibility.
-	apiRequestEndpointPatch: {
+	APIRequestEndpointPatch: {
 		AutoAdjust:                  true,
 		EstimatedProcessingDuration: time.Second,
 		RateLimit:                   0.5,
@@ -85,7 +116,7 @@ var apiRateLimitDefaults = map[string]rate.APILimiterParameters{
 	// Listing endpoints should be relatively quick, even with a large
 	// number of endpoints on a node. Always permit two parallel requests
 	// and rely on rate limiting to throttle if load becomes high.
-	apiRequestEndpointList: {
+	APIRequestEndpointList: {
 		AutoAdjust:                  true,
 		EstimatedProcessingDuration: time.Millisecond * 300,
 		RateLimit:                   1.0,

daemon/restapi/api_limits_test.go
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package restapi
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/assert"
+	timeRate "golang.org/x/time/rate"
+
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/rate"
+)
+
+// TestRateLimiterConfigFlag checks that rateLimiterConfig is properly parsed from
+// command-line flag.
+func testRateLimiterConfig(t *testing.T, setConfig func(h *hive.Hive, limiter string, limits string)) {
+	var limiterSet *rate.APILimiterSet
+	take := cell.Invoke(func(l *rate.APILimiterSet) { limiterSet = l })
+
+	h := hive.New(rateLimiterCell, take)
+
+	setConfig(h, APIRequestEndpointCreate, "rate-limit:42/m,rate-burst:1234")
+	assert.Nil(t, h.Start(context.TODO()))
+
+	l := limiterSet.Limiter(APIRequestEndpointCreate)
+	assert.NotNil(t, l)
+
+	p := l.Parameters()
+	assert.Equal(t, timeRate.Limit(42.0/60.0), p.RateLimit)
+	assert.Equal(t, 1234, p.RateBurst)
+	assert.Nil(t, h.Stop(context.TODO()))
+}
+
+// TestRateLimiterConfigFlag checks that rateLimiterConfig is properly parsed from
+// command-line flag.
+func TestRateLimiterConfigFlag(t *testing.T) {
+	testRateLimiterConfig(t,
+		func(h *hive.Hive, limiter string, limits string) {
+			flags := pflag.NewFlagSet("test", pflag.ContinueOnError)
+			h.RegisterFlags(flags)
+			err := flags.Parse([]string{"--api-rate-limit", limiter + "=" + limits})
+			assert.Nil(t, err, "failed to parse flags")
+		})
+}
+
+// TestRateLimiterConfigFile checks that rateLimiterConfig is properly parsed from
+// a config file.
+func TestRateLimiterConfigFile(t *testing.T) {
+	testRateLimiterConfig(t,
+		func(h *hive.Hive, limiter string, limits string) {
+			cfg := fmt.Sprintf("api-rate-limit:\n  %s:%s\n", limiter, limits)
+			buf := bytes.NewReader([]byte(cfg))
+			err := h.Viper().ReadConfig(buf)
+			assert.Nil(t, err, "failed to ReadConfig with Viper")
+		})
+}

daemon/cmd/cells.go
@@ -8,6 +8,7 @@ import (
 	"github.com/cilium/cilium/api/v1/server"
 	"github.com/cilium/cilium/daemon/cmd/cni"
 	agentK8s "github.com/cilium/cilium/daemon/k8s"
+	"github.com/cilium/cilium/daemon/restapi"
 	"github.com/cilium/cilium/pkg/auth"
 	"github.com/cilium/cilium/pkg/bgpv1"
 	"github.com/cilium/cilium/pkg/clustermesh"
@@ -121,6 +122,9 @@ var (
 		// makes different L7 proxies (Envoy, DNS proxy) usable to Cilium endpoints through
 		// a common Proxy 'redirect' abstraction.
 		proxy.Cell,
+
+		// Cilium REST API handlers
+		restapi.Cell,
 
 		// The BGP Control Plane which enables various BGP related interop.
 		bgpv1.Cell,

daemon/cmd/daemon.go
@@ -84,7 +84,6 @@ import (
 	policyAPI "github.com/cilium/cilium/pkg/policy/api"
 	"github.com/cilium/cilium/pkg/proxy"
 	"github.com/cilium/cilium/pkg/rate"
-	ratemetrics "github.com/cilium/cilium/pkg/rate/metrics"
 	"github.com/cilium/cilium/pkg/recorder"
 	"github.com/cilium/cilium/pkg/redirectpolicy"
 	"github.com/cilium/cilium/pkg/service"
@@ -422,12 +421,6 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		log.WithField("mtu", configuredMTU).Info("Overwriting MTU based on CNI configuration")
 	}
 
-	apiLimiterSet, err := rate.NewAPILimiterSet(option.Config.APIRateLimit, apiRateLimitDefaults, ratemetrics.APILimiterObserver())
-	if err != nil {
-		log.WithError(err).Error("unable to configure API rate limiting")
-		return nil, nil, fmt.Errorf("unable to configure API rate limiting: %w", err)
-	}
-
 	// Check the kernel if we can make use of managed neighbor entries which
 	// simplifies and fully 'offloads' L2 resolution handling to the kernel.
 	if !option.Config.DryMode {
@@ -536,7 +529,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		nodeDiscovery:     nd,
 		nodeLocalStore:    params.LocalNodeStore,
 		endpointCreations: newEndpointCreationManager(params.Clientset),
-		apiLimiterSet:     apiLimiterSet,
+		apiLimiterSet:     params.APILimiterSet,
 		controllers:       controller.NewManager(),
 		// **NOTE** The global identity allocator is not yet initialized here; that
 		// happens below via InitIdentityAllocator(). Only the local identity

daemon/cmd/daemon_main.go
@@ -85,6 +85,7 @@ import (
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/promise"
 	"github.com/cilium/cilium/pkg/proxy"
+	"github.com/cilium/cilium/pkg/rate"
 	"github.com/cilium/cilium/pkg/statedb"
 	"github.com/cilium/cilium/pkg/sysctl"
 	"github.com/cilium/cilium/pkg/version"
@@ -1027,9 +1028,6 @@ func initializeFlags() {
 	flags.String(option.K8sServiceProxyName, "", "Value of K8s service-proxy-name label for which Cilium handles the services (empty = all services without service.kubernetes.io/service-proxy-name label)")
 	option.BindEnv(Vp, option.K8sServiceProxyName)
 
-	flags.Var(option.NewNamedMapOptions(option.APIRateLimitName, &option.Config.APIRateLimit, nil), option.APIRateLimitName, "API rate limiting configuration (example: --rate-limit endpoint-create=rate-limit:10/m,rate-burst:2)")
-	option.BindEnv(Vp, option.APIRateLimitName)
-
 	flags.Var(option.NewNamedMapOptions(option.BPFMapEventBuffers, &option.Config.BPFMapEventBuffers, option.Config.BPFMapEventBuffersValidator), option.BPFMapEventBuffers, "Configuration for BPF map event buffers: (example: --bpf-map-event-buffers cilium_ipcache=true,1024,1h)")
 	flags.MarkHidden(option.BPFMapEventBuffers)
 
@@ -1629,6 +1627,7 @@ type daemonParams struct {
 	L2Announcer          *l2announcer.L2Announcer
 	L7Proxy              *proxy.Proxy
 	DB                   statedb.DB
+	APILimiterSet        *rate.APILimiterSet
 }
 
 func newDaemonPromise(params daemonParams) promise.Promise[*Daemon] {

daemon/cmd/endpoint.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
+	"github.com/cilium/cilium/daemon/restapi"
 	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/api"
 	"github.com/cilium/cilium/pkg/bandwidth"
@@ -55,7 +56,7 @@ func NewGetEndpointHandler(d *Daemon) GetEndpointHandler {
 func (h *getEndpoint) Handle(params GetEndpointParams) middleware.Responder {
 	log.WithField(logfields.Params, logfields.Repr(params)).Debug("GET /endpoint request")
 
-	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointList)
+	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointList)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -143,7 +144,7 @@ func NewGetEndpointIDHandler(d *Daemon) GetEndpointIDHandler {
 func (h *getEndpointID) Handle(params GetEndpointIDParams) middleware.Responder {
 	log.WithField(logfields.EndpointID, params.ID).Debug("GET /endpoint/{id} request")
 
-	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointGet)
+	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointGet)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -566,7 +567,7 @@ func (h *putEndpointID) Handle(params PutEndpointIDParams) (resp middleware.Resp
 	}
 	epTemplate := params.Endpoint
 
-	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointCreate)
+	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointCreate)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -611,7 +612,7 @@ func (h *patchEndpointID) Handle(params PatchEndpointIDParams) middleware.Respon
 	}
 	scopedLog.Debug("PATCH /endpoint/{id} request")
 
-	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointPatch)
+	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointPatch)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -793,7 +794,7 @@ func NewDeleteEndpointIDHandler(d *Daemon) DeleteEndpointIDHandler {
 func (h *deleteEndpointID) Handle(params DeleteEndpointIDParams) middleware.Responder {
 	log.WithField(logfields.Params, logfields.Repr(params)).Debug("DELETE /endpoint/{id} request")
 
-	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointDelete)
+	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointDelete)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -850,7 +851,7 @@ func NewPatchEndpointIDConfigHandler(d *Daemon) PatchEndpointIDConfigHandler {
 func (h *patchEndpointIDConfig) Handle(params PatchEndpointIDConfigParams) middleware.Responder {
 	log.WithField(logfields.Params, logfields.Repr(params)).Debug("PATCH /endpoint/{id}/config request")
 
-	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointPatch)
+	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointPatch)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -879,7 +880,7 @@ func NewGetEndpointIDConfigHandler(d *Daemon) GetEndpointIDConfigHandler {
 func (h *getEndpointIDConfig) Handle(params GetEndpointIDConfigParams) middleware.Responder {
 	log.WithField(logfields.Params, logfields.Repr(params)).Debug("GET /endpoint/{id}/config")
 
-	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointGet)
+	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointGet)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -910,7 +911,7 @@ func NewGetEndpointIDLabelsHandler(d *Daemon) GetEndpointIDLabelsHandler {
 func (h *getEndpointIDLabels) Handle(params GetEndpointIDLabelsParams) middleware.Responder {
 	log.WithField(logfields.Params, logfields.Repr(params)).Debug("GET /endpoint/{id}/labels")
 
-	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointGet)
+	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointGet)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -946,7 +947,7 @@ func NewGetEndpointIDLogHandler(d *Daemon) GetEndpointIDLogHandler {
 func (h *getEndpointIDLog) Handle(params GetEndpointIDLogParams) middleware.Responder {
 	log.WithField(logfields.EndpointID, params.ID).Debug("GET /endpoint/{id}/log request")
 
-	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointGet)
+	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointGet)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -976,7 +977,7 @@ func NewGetEndpointIDHealthzHandler(d *Daemon) GetEndpointIDHealthzHandler {
 func (h *getEndpointIDHealthz) Handle(params GetEndpointIDHealthzParams) middleware.Responder {
 	log.WithField(logfields.EndpointID, params.ID).Debug("GET /endpoint/{id}/log request")
 
-	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointGet)
+	r, err := h.d.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointGet)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}
@@ -1040,7 +1041,7 @@ func NewPatchEndpointIDLabelsHandler(d *Daemon) PatchEndpointIDLabelsHandler {
 func (h *putEndpointIDLabels) Handle(params PatchEndpointIDLabelsParams) middleware.Responder {
 	log.WithField(logfields.Params, logfields.Repr(params)).Debug("PATCH /endpoint/{id}/labels request")
 
-	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), apiRequestEndpointPatch)
+	r, err := h.daemon.apiLimiterSet.Wait(params.HTTPRequest.Context(), restapi.APIRequestEndpointPatch)
 	if err != nil {
 		return api.Error(http.StatusTooManyRequests, err)
 	}

daemon/restapi/cell.go
@@ -1,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package restapi
+
+import "github.com/cilium/cilium/pkg/hive/cell"
+
+var Cell = cell.Module(
+	"cilium-restapi",
+	"Cilium Agent API handlers",
+
+	rateLimiterCell, // Request rate-limiting
+)

pkg/option/config.go
@@ -1083,9 +1083,6 @@ const (
 	// service.kubernetes.io/service-proxy-name label equals the provided value.
 	K8sServiceProxyName = "k8s-service-proxy-name"
 
-	// APIRateLimitName enables configuration of the API rate limits
-	APIRateLimitName = "api-rate-limit"
-
 	// CRDWaitTimeout is the timeout in which Cilium will exit if CRDs are not
 	// available.
 	CRDWaitTimeout = "crd-wait-timeout"
@@ -2313,9 +2310,6 @@ type DaemonConfig struct {
 	// https://github.com/kubernetes/enhancements/tree/master/keps/sig-network/2447-Make-kube-proxy-service-abstraction-optional
 	K8sServiceProxyName string
 
-	// APIRateLimitName enables configuration of the API rate limits
-	APIRateLimit map[string]string
-
 	// CRDWaitTimeout is the timeout in which Cilium will exit if CRDs are not
 	// available.
 	CRDWaitTimeout time.Duration
@@ -2462,7 +2456,6 @@ var (
 		UseCiliumInternalIPForIPsec:  defaults.UseCiliumInternalIPForIPsec,
 
 		K8sEnableLeasesFallbackDiscovery: defaults.K8sEnableLeasesFallbackDiscovery,
-		APIRateLimit:                     make(map[string]string),
 
 		ExternalClusterIP:      defaults.ExternalClusterIP,
 		EnableVTEP:             defaults.EnableVTEP,
@@ -3383,12 +3376,6 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 		log.Fatalf("unable to parse %s: %s", LogOpt, err)
 	} else {
 		c.LogOpt = m
-	}
-
-	if m, err := command.GetStringMapStringE(vp, APIRateLimitName); err != nil {
-		log.Fatalf("unable to parse %s: %s", APIRateLimitName, err)
-	} else {
-		c.APIRateLimit = m
 	}
 
 	c.bpfMapEventConfigs = make(BPFEventBufferConfigs)

commit 651e4273bb73e1dbf94fb9b53c4fffcc06a68b86

daemon: Fix parsing of complex api-rate-limit arguments
In d7ac88839768 the API rate limit parsing was redone to use StringToString.
This broke parsing of api-rate-limits of form:

  "foo=rate-limit:1/s,rate-burst:1,bar=rate-limit:2/s"

The pflag StringToString failed to split this correctly leading to
it trying to parse "rate-burst:1" as a "key=value". Additionally
the change broke the support for specifying the rate limits in JSON
format.

Expand the test-cases to cover this usage and fix the issue by switching
back to parsing with the command.ToStringMapStringE method.

Fixes: #31894
Fixes: d7ac88839768 ("daemon: Move rate limiter to daemon/restapi")

Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/restapi/api_limits.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cilium/hive/cell"
 	"github.com/spf13/pflag"
 
+	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/rate"
 	ratemetrics "github.com/cilium/cilium/pkg/rate/metrics"
 	"github.com/cilium/cilium/pkg/time"
@@ -21,22 +22,26 @@ var rateLimiterCell = cell.Module(
 )
 
 type rateLimiterConfig struct {
-	APIRateLimit map[string]string
+	APIRateLimit string
 }
 
 func (def rateLimiterConfig) Flags(flags *pflag.FlagSet) {
-	flags.StringToString(
+	flags.String(
 		"api-rate-limit",
 		def.APIRateLimit,
 		"API rate limiting configuration (example: --api-rate-limit endpoint-create=rate-limit:10/m,rate-burst:2)")
 }
 
 var defaultRateLimiterConfig = rateLimiterConfig{
-	APIRateLimit: make(map[string]string),
+	APIRateLimit: "",
 }
 
 func newApiRateLimiter(cfg rateLimiterConfig) (*rate.APILimiterSet, error) {
-	return rate.NewAPILimiterSet(cfg.APIRateLimit, apiRateLimitDefaults, ratemetrics.APILimiterObserver())
+	config, err := command.ToStringMapStringE(cfg.APIRateLimit)
+	if err != nil {
+		return nil, err
+	}
+	return rate.NewAPILimiterSet(config, apiRateLimitDefaults, ratemetrics.APILimiterObserver())
 }
 
 const (

daemon/restapi/api_limits_test.go
@@ -6,7 +6,9 @@ package restapi
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/cilium/hive/cell"
@@ -19,15 +21,36 @@ import (
 	"github.com/cilium/cilium/pkg/rate"
 )
 
+var testLimits = map[string]string{
+	APIRequestEndpointCreate: "rate-limit:42/m,rate-burst:1",
+	APIRequestEndpointDelete: "rate-limit:72/s,rate-burst:2",
+}
+
+func testLimitsKVString() string {
+	var limits []string
+	for k, v := range testLimits {
+		limits = append(limits, k+"="+v)
+	}
+	return strings.Join(limits, ",")
+}
+
+func testLimitsJSONString() string {
+	bs, err := json.Marshal(testLimits)
+	if err != nil {
+		panic(err)
+	}
+	return string(bs)
+}
+
 // TestRateLimiterConfigFlag checks that rateLimiterConfig is properly parsed from
 // command-line flag.
-func testRateLimiterConfig(t *testing.T, burst int, setConfig func(h *hive.Hive, limiter string, limits string)) {
+func testRateLimiterConfig(t *testing.T, setConfig func(h *hive.Hive)) {
 	var limiterSet *rate.APILimiterSet
 	take := cell.Invoke(func(l *rate.APILimiterSet) { limiterSet = l })
 
 	h := hive.New(rateLimiterCell, take)
 
-	setConfig(h, APIRequestEndpointCreate, fmt.Sprintf("rate-limit:42/m,rate-burst:%d", burst))
+	setConfig(h)
 	tlog := hivetest.Logger(t)
 	assert.Nil(t, h.Start(tlog, context.TODO()))
 
@@ -36,18 +59,26 @@ func testRateLimiterConfig(t *testing.T, burst int, setConfig func(h *hive.Hive,
 
 	p := l.Parameters()
 	assert.Equal(t, timeRate.Limit(42.0/60.0), p.RateLimit)
-	assert.Equal(t, burst, p.RateBurst)
+	assert.Equal(t, 1, p.RateBurst)
+
+	l = limiterSet.Limiter(APIRequestEndpointDelete)
+	assert.NotNil(t, l)
+
+	p = l.Parameters()
+	assert.Equal(t, timeRate.Limit(72.0), p.RateLimit)
+	assert.Equal(t, 2, p.RateBurst)
+
 	assert.Nil(t, h.Stop(tlog, context.TODO()))
 }
 
 // TestRateLimiterConfigFlag checks that rateLimiterConfig is properly parsed from
 // command-line flag.
 func TestRateLimiterConfigFlag(t *testing.T) {
-	testRateLimiterConfig(t, 1234,
-		func(h *hive.Hive, limiter string, limits string) {
+	testRateLimiterConfig(t,
+		func(h *hive.Hive) {
 			flags := pflag.NewFlagSet("test", pflag.ContinueOnError)
 			h.RegisterFlags(flags)
-			err := flags.Parse([]string{"--api-rate-limit", limiter + "=" + limits})
+			err := flags.Parse([]string{"--api-rate-limit", testLimitsKVString()})
 			assert.Nil(t, err, "failed to parse flags")
 		})
 }
@@ -55,13 +86,42 @@ func TestRateLimiterConfigFlag(t *testing.T) {
 // TestRateLimiterConfigFile checks that rateLimiterConfig is properly parsed from
 // a config file.
 func TestRateLimiterConfigFile(t *testing.T) {
-	testRateLimiterConfig(t, 1235,
-		func(h *hive.Hive, limiter string, limits string) {
-			cfg := fmt.Sprintf("api-rate-limit:\n  %s: %s\n", limiter, limits)
+	testRateLimiterConfig(t,
+		func(h *hive.Hive) {
+			cfg := fmt.Sprintf("api-rate-limit: %s\n", testLimitsKVString())
 			buf := bytes.NewReader([]byte(cfg))
 			v := h.Viper()
 			v.SetConfigType("yaml")
 			err := v.ReadConfig(buf)
 			assert.Nil(t, err, "failed to ReadConfig with Viper")
+		})
+}
+
+// TestRateLimiterConfigDir checks that rateLimiterConfig is properly parsed from
+// a config directory.
+func TestRateLimiterConfigDir(t *testing.T) {
+	// Test with k=v string
+	testRateLimiterConfig(t,
+		func(h *hive.Hive) {
+			// option.InitConfig reads the files into a map[string]any which
+			// is then merged into viper with MergeConfig. We're simulating that
+			// here.
+			configMap := map[string]any{
+				"api-rate-limit": testLimitsKVString(),
+			}
+			v := h.Viper()
+			err := v.MergeConfigMap(configMap)
+			assert.Nil(t, err, "failed to MergeConfigMap with Viper")
+		})
+
+	// Test with JSON string
+	testRateLimiterConfig(t,
+		func(h *hive.Hive) {
+			configMap := map[string]any{
+				"api-rate-limit": testLimitsJSONString(),
+			}
+			v := h.Viper()
+			err := v.MergeConfigMap(configMap)
+			assert.Nil(t, err, "failed to MergeConfigMap with Viper")
 		})
 }
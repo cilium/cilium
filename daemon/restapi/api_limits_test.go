651e4273bb73e1dbf94fb9b53c4fffcc06a68b86
Jussi Maki <jussi@isovalent.com>
2024-08-08 16:56:03 +0200

    daemon: Fix parsing of complex api-rate-limit arguments
    In d7ac88839768 the API rate limit parsing was redone to use StringToString.
    This broke parsing of api-rate-limits of form:
    
      "foo=rate-limit:1/s,rate-burst:1,bar=rate-limit:2/s"
    
    The pflag StringToString failed to split this correctly leading to
    it trying to parse "rate-burst:1" as a "key=value". Additionally
    the change broke the support for specifying the rate limits in JSON
    format.
    
    Expand the test-cases to cover this usage and fix the issue by switching
    back to parsing with the command.ToStringMapStringE method.
    
    Fixes: #31894
    Fixes: d7ac88839768 ("daemon: Move rate limiter to daemon/restapi")
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- daemon/restapi/api_limits_test.go
+++ daemon/restapi/api_limits_test.go
@@ -6,7 +6,9 @@ package restapi
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/cilium/hive/cell"
@@ -19,15 +21,36 @@ import (
 	"github.com/cilium/cilium/pkg/rate"
 )
 
+var testLimits = map[string]string{
+	APIRequestEndpointCreate: "rate-limit:42/m,rate-burst:1",
+	APIRequestEndpointDelete: "rate-limit:72/s,rate-burst:2",
+}
+
+func testLimitsKVString() string {
+	var limits []string
+	for k, v := range testLimits {
+		limits = append(limits, k+"="+v)
+	}
+	return strings.Join(limits, ",")
+}
+
+func testLimitsJSONString() string {
+	bs, err := json.Marshal(testLimits)
+	if err != nil {
+		panic(err)
+	}
+	return string(bs)
+}
+
 // TestRateLimiterConfigFlag checks that rateLimiterConfig is properly parsed from
 // command-line flag.
-func testRateLimiterConfig(t *testing.T, burst int, setConfig func(h *hive.Hive, limiter string, limits string)) {
+func testRateLimiterConfig(t *testing.T, setConfig func(h *hive.Hive)) {
 	var limiterSet *rate.APILimiterSet
 	take := cell.Invoke(func(l *rate.APILimiterSet) { limiterSet = l })
 
 	h := hive.New(rateLimiterCell, take)
 
-	setConfig(h, APIRequestEndpointCreate, fmt.Sprintf("rate-limit:42/m,rate-burst:%d", burst))
+	setConfig(h)
 	tlog := hivetest.Logger(t)
 	assert.Nil(t, h.Start(tlog, context.TODO()))
 
@@ -36,18 +59,26 @@ func testRateLimiterConfig(t *testing.T, burst int, setConfig func(h *hive.Hive,
 
 	p := l.Parameters()
 	assert.Equal(t, timeRate.Limit(42.0/60.0), p.RateLimit)
-	assert.Equal(t, burst, p.RateBurst)
+	assert.Equal(t, 1, p.RateBurst)
+
+	l = limiterSet.Limiter(APIRequestEndpointDelete)
+	assert.NotNil(t, l)
+
+	p = l.Parameters()
+	assert.Equal(t, timeRate.Limit(72.0), p.RateLimit)
+	assert.Equal(t, 2, p.RateBurst)
+
 	assert.Nil(t, h.Stop(tlog, context.TODO()))
 }
 
 // TestRateLimiterConfigFlag checks that rateLimiterConfig is properly parsed from
 // command-line flag.
 func TestRateLimiterConfigFlag(t *testing.T) {
-	testRateLimiterConfig(t, 1234,
-		func(h *hive.Hive, limiter string, limits string) {
+	testRateLimiterConfig(t,
+		func(h *hive.Hive) {
 			flags := pflag.NewFlagSet("test", pflag.ContinueOnError)
 			h.RegisterFlags(flags)
-			err := flags.Parse([]string{"--api-rate-limit", limiter + "=" + limits})
+			err := flags.Parse([]string{"--api-rate-limit", testLimitsKVString()})
 			assert.Nil(t, err, "failed to parse flags")
 		})
 }
@@ -55,13 +86,42 @@ func TestRateLimiterConfigFlag(t *testing.T) {
 // TestRateLimiterConfigFile checks that rateLimiterConfig is properly parsed from
 // a config file.
 func TestRateLimiterConfigFile(t *testing.T) {
-	testRateLimiterConfig(t, 1235,
-		func(h *hive.Hive, limiter string, limits string) {
-			cfg := fmt.Sprintf("api-rate-limit:\n  %s: %s\n", limiter, limits)
+	testRateLimiterConfig(t,
+		func(h *hive.Hive) {
+			cfg := fmt.Sprintf("api-rate-limit: %s\n", testLimitsKVString())
 			buf := bytes.NewReader([]byte(cfg))
 			v := h.Viper()
 			v.SetConfigType("yaml")
 			err := v.ReadConfig(buf)
 			assert.Nil(t, err, "failed to ReadConfig with Viper")
+		})
+}
+
+// TestRateLimiterConfigDir checks that rateLimiterConfig is properly parsed from
+// a config directory.
+func TestRateLimiterConfigDir(t *testing.T) {
+	// Test with k=v string
+	testRateLimiterConfig(t,
+		func(h *hive.Hive) {
+			// option.InitConfig reads the files into a map[string]any which
+			// is then merged into viper with MergeConfig. We're simulating that
+			// here.
+			configMap := map[string]any{
+				"api-rate-limit": testLimitsKVString(),
+			}
+			v := h.Viper()
+			err := v.MergeConfigMap(configMap)
+			assert.Nil(t, err, "failed to MergeConfigMap with Viper")
+		})
+
+	// Test with JSON string
+	testRateLimiterConfig(t,
+		func(h *hive.Hive) {
+			configMap := map[string]any{
+				"api-rate-limit": testLimitsJSONString(),
+			}
+			v := h.Viper()
+			err := v.MergeConfigMap(configMap)
+			assert.Nil(t, err, "failed to MergeConfigMap with Viper")
 		})
 }
