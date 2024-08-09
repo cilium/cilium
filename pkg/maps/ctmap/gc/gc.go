b233e797b958666aa7ae3ff8530ddd1209ddbad0
Tom Hadlaw <tom.hadlaw@isovalent.com>
2024-08-09 15:43:22 -0700

    ctmap/gc: remove unused parameter/field for GC.
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

--- pkg/maps/ctmap/gc/gc.go
+++ pkg/maps/ctmap/gc/gc.go
@@ -65,7 +65,6 @@ type GC struct {
 	nodeAddrs statedb.Table[tables.NodeAddress]
 
 	endpointsManager EndpointManager
-	nodeAddressing   types.NodeAddressing
 	signalHandler    SignalHandler
 
 	perClusterCTMapsRetriever PerClusterCTMapsRetriever
@@ -83,7 +82,6 @@ func New(params parameters) *GC {
 		nodeAddrs: params.NodeAddrs,
 
 		endpointsManager: params.EndpointManager,
-		nodeAddressing:   params.NodeAddressing,
 		signalHandler:    params.SignalManager,
 
 		controllerManager: controller.NewManager(),
