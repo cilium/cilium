a4acded0453694c3b93f19752d01a3f317ebcb6d
gray <greyschwinger@gmail.com>
2024-08-12 17:20:30 +0800

    bpf: revDNAT reply traffic before IPsec encryption
    When IPsec is enabled with L7 ingress policy, nodeport, and KPR, the
    reply path for a pod-to-remote-hostport connection is supposed to be:
    from-container@lxc -> L7 proxy -> from-host@cilium_host ->
    to-host@cilium_net -> xfrm IPsec encryption -> to-netdev@eth0.
    
    The problem of above path is, we don't stand a chance to perform revDNAT.
    It is supposed to take place at to-netdev@eth0, but bpf code fails to do
    so because datapath sees IPsec-encrypted ESP.
    
    Another existing revDNAT position is from-container@lxc, but we intend to
    remove that in future and rely on node's egress interface only
    (to-netdev@eth0). On the other hand, revDNAT at from-container@lxc still
    doesn't work in this L7 proxy case because proxy_redirect happens prior
    to the revDNAT.
    
    This commit adds a new revDNAT hook at to-host@cilium_net for IPsec
    specifically, where handle_nat_fwd() is called with revdnat_only=1 to
    skip unnecessary fib_lookup, redirect, and snat.
    
    Fixes: https://github.com/cilium/cilium/issues/32897
    
    Signed-off-by: gray <gray.liang@isovalent.com>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -1651,7 +1651,23 @@ int cil_to_host(struct __ctx_buff *ctx)
 	 * to mark as PACKET_OTHERHOST and drop.
 	 */
 	ctx_change_type(ctx, PACKET_HOST);
-#endif
+
+# ifdef ENABLE_NODEPORT
+	if (!validate_ethertype(ctx, &proto)) {
+		ret = DROP_UNSUPPORTED_L2;
+		goto out;
+	}
+	/* handle_nat_fwd() tail calls in the majority of cases, so control
+	 * might never return to this program. Since IPsec is not compatible
+	 * iwth Host Firewall, this won't be an issue.
+	 */
+	ret = handle_nat_fwd(ctx, 0, proto, 1, &trace, &ext_err);
+	if (IS_ERR(ret))
+		goto out;
+
+# endif /* ENABLE_NODEPORT */
+
+#endif /* ENABLE_IPSEC */
 #ifdef ENABLE_HOST_FIREWALL
 	if (!validate_ethertype(ctx, &proto)) {
 		ret = DROP_UNSUPPORTED_L2;
