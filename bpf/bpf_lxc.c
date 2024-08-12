cilium/cilium

commit 043f2ea723f7d425c24e15e3f953fe4aec6a34ad

bpf: lxc: also reduce CT lookup scope after L7 service match
In the L7 service path, the from-container program observes the connection
twice:
1. before the packet has been redirected to envoy, and
2. after envoy has LBed the packet, when it gets recircled back into
   the from-container program via tail-call by bpf_host into the
   POLICY_EGRESSCALL_MAP.

The CT entries for both stages should be in forward direction, any other
matched entry is stale and should be ignored. Adjust the scope of the
CT lookup accordingly.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/bpf_lxc.c
@@ -307,6 +307,11 @@ int NAME(struct __ctx_buff *ctx)						\
 				      &cluster_id, false);			\
 		if (ct_state_new.rev_nat_index)					\
 			scope = SCOPE_FORWARD;					\
+		if (is_defined(ENABLE_L7_LB) && proxy_port)			\
+			scope = SCOPE_FORWARD;					\
+		if (is_defined(ENABLE_L7_LB) &&					\
+		    (ctx_load_meta(ctx, CB_FROM_HOST == FROM_HOST_L7_LB)))	\
+			scope = SCOPE_FORWARD;					\
 	}									\
 										\
 	ct_buffer.ret = ct_lookup4(map, tuple, ctx, ip4, ct_buffer.l4_off,	\
@@ -362,6 +367,11 @@ int NAME(struct __ctx_buff *ctx)						\
 										\
 		lb6_ctx_restore_state(ctx, &ct_state_new, &proxy_port, false);	\
 		if (ct_state_new.rev_nat_index)					\
+			scope = SCOPE_FORWARD;					\
+		if (is_defined(ENABLE_L7_LB) && proxy_port)			\
+			scope = SCOPE_FORWARD;					\
+		if (is_defined(ENABLE_L7_LB) &&					\
+		    (ctx_load_meta(ctx, CB_FROM_HOST == FROM_HOST_L7_LB)))	\
 			scope = SCOPE_FORWARD;					\
 	}									\
 										\

commit 6a269e2eba82334790a706ce835d455f9a36b874

bpf: lxc: fix up a typo in the L7 LB CT lookup
Fix up a subtle typo that was introduced by
043f2ea723f7 ("bpf: lxc: also reduce CT lookup scope after L7 service match").

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/bpf_lxc.c
@@ -310,7 +310,7 @@ int NAME(struct __ctx_buff *ctx)						\
 		if (is_defined(ENABLE_L7_LB) && proxy_port)			\
 			scope = SCOPE_FORWARD;					\
 		if (is_defined(ENABLE_L7_LB) &&					\
-		    (ctx_load_meta(ctx, CB_FROM_HOST == FROM_HOST_L7_LB)))	\
+		    (ctx_load_meta(ctx, CB_FROM_HOST) == FROM_HOST_L7_LB))	\
 			scope = SCOPE_FORWARD;					\
 	}									\
 										\
@@ -371,7 +371,7 @@ int NAME(struct __ctx_buff *ctx)						\
 		if (is_defined(ENABLE_L7_LB) && proxy_port)			\
 			scope = SCOPE_FORWARD;					\
 		if (is_defined(ENABLE_L7_LB) &&					\
-		    (ctx_load_meta(ctx, CB_FROM_HOST == FROM_HOST_L7_LB)))	\
+		    (ctx_load_meta(ctx, CB_FROM_HOST) == FROM_HOST_L7_LB))	\
 			scope = SCOPE_FORWARD;					\
 	}									\
 										\