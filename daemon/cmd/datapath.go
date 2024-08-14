55bf68c2997efa075c17b623f570caeec624d1c6
Paul Chaignon <paul.chaignon@gmail.com>
2024-08-14 18:40:56 +0000

    daemon: Create IPsec and LRP maps early on startup
    [ upstream commit 64968210015b557b19849a932dfd912090eb3eaf ]
    
    We have some sort of race popping up in CI from time to time around the
    loading of some BPF maps:
    
        loading eBPF collection into the kernel: map cilium_encrypt_state: pin map to /sys/fs/bpf/tc/globals/cilium_encrypt_state: file exists
    
    One approach to avoid this is to ensure such maps are always created
    early on agent startup instead of being created during endpoint creation
    (where concurrent program loadings can run into this race).
    
    This commit therefore adds the two maps concerned by this issue to the
    initMaps method.
    
    Suggested-by: Joe Stringer <joe@cilium.io>
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- daemon/cmd/datapath.go
+++ daemon/cmd/datapath.go
@@ -20,6 +20,7 @@ import (
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
+	"github.com/cilium/cilium/pkg/maps/encrypt"
 	"github.com/cilium/cilium/pkg/maps/fragmap"
 	ipcachemap "github.com/cilium/cilium/pkg/maps/ipcache"
 	"github.com/cilium/cilium/pkg/maps/ipmasq"
@@ -239,6 +240,12 @@ func (d *Daemon) initMaps() error {
 		}
 	}
 
+	if option.Config.EnableIPSec {
+		if err := encrypt.MapCreate(); err != nil {
+			return fmt.Errorf("initializing IPsec map: %w", err)
+		}
+	}
+
 	if !option.Config.RestoreState {
 		// If we are not restoring state, all endpoints can be
 		// deleted. Entries will be re-populated.
@@ -278,6 +285,11 @@ func (d *Daemon) initMaps() error {
 		if err := lbmap.InitMaglevMaps(option.Config.EnableIPv4, option.Config.EnableIPv6, uint32(option.Config.MaglevTableSize)); err != nil {
 			return fmt.Errorf("initializing maglev maps: %w", err)
 		}
+	}
+
+	_, err := lbmap.NewSkipLBMap()
+	if err != nil {
+		return fmt.Errorf("initializing local redirect policy maps: %w", err)
 	}
 
 	return nil
