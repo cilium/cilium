27cd07eb715826d180f26a5abde7b7abf2653a72
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-08-14 16:31:30 +0200

    cilium-dbg: add command 'envoy admin config'
    Currently, dumping the applied envoy config requires either exposing
    the envoy admin interface via IP listener and accessing it via browser
    or SSHing into the cilium-agent, installing `curl` and accessing the
    envoy admin interface via UNIX socket (`/var/run/cilium/envoy/sockets/admin.sock`).
    
    Since both approaches aren't really userfriendly (e.g. during development), this
    commit introduces a new command `envoy admin config` to the `cilium-dbg` binary.
    This way, the Envoy Config of a Cilium Node can be viewed without having to install
    curl and remember the UNIX socket path.
    
    In addition to viewing the config it's also possible to
    
    * filter by resource type by appending it to the command
      (e.g. `cilium-dbg envoy admin config listeners`)
    * filter by resource name
      (e.g. `cilium-dbg envoy admin config -n test`)
      (Can be combined with resource type filtering)
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

---
+++ cilium-dbg/cmd/envoy_adminclient.go
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
+	"path/filepath"
+
+	"github.com/cilium/cilium/pkg/safeio"
+)
+
+const envoySocketDirPath = "/var/run/cilium/envoy/sockets/"
+
+type envoyAdminClient struct {
+	adminURL string
+	unixPath string
+}
+
+func newEnvoyAdminClient() *envoyAdminClient {
+	return &envoyAdminClient{
+		// Needs to be provided to envoy (received as ':authority') - even though we Dial to a Unix domain socket.
+		adminURL: fmt.Sprintf("http://%s/", "envoy-admin"),
+		unixPath: filepath.Join(envoySocketDirPath, "admin.sock"),
+	}
+}
+
+func (a *envoyAdminClient) get(path string) (string, error) {
+	// Use a custom dialer to use a Unix domain socket for a HTTP connection.
+	var conn net.Conn
+	var err error
+	client := &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				conn, err = net.Dial("unix", a.unixPath)
+				return conn, err
+			},
+		},
+	}
+
+	u, err := url.Parse(fmt.Sprintf("%s%s", a.adminURL, path))
+	if err != nil {
+		return "", fmt.Errorf("failed to parse URL: %w", err)
+	}
+
+	resp, err := client.Get(u.String())
+	if err != nil {
+		return "", fmt.Errorf("failed to call %q endpoint: %w", path, err)
+	}
+	defer conn.Close()
+	defer resp.Body.Close()
+
+	body, err := safeio.ReadAllLimit(resp.Body, safeio.MB)
+	if err != nil {
+		return "", fmt.Errorf("failed to read %q response: %w", path, err)
+	}
+
+	return string(body), nil
+}
+
+func (a *envoyAdminClient) GetConfigDump(resourceType string, resourceName string) (string, error) {
+	path := "config_dump?include_eds"
+
+	if len(resourceType) > 0 {
+		path = path + "&resource=" + resourceType
+	}
+
+	if len(resourceName) > 0 {
+		path = path + "&name_regex=" + resourceName
+	}
+
+	return a.get(path)
+}
