be0e831da13897d06aa0c80c3eef242bb38c25b9
Joe Stringer <joe@cilium.io>
2024-08-13 18:26:22 -0700

    .nvim: Add debugging configurations for go test
    Using 'mfussenegger/nvim-dap', 'rcarriga/nvim-dap-ui', and
    'ldelossa/nvim-dap-projects' these targets can provide a way to easily
    enable a debugger inside neovim for common Cilium test patterns.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- .nvim/nvim-dap.lua
+++ .nvim/nvim-dap.lua
@@ -1,5 +1,15 @@
 local dap = require('dap')
 
+dap.adapters.cilium_delve = {
+  type = 'server',
+  port = '${port}',
+  executable = {
+    command = 'dlv',
+    args = {'dap', '-l', '127.0.0.1:${port}'},
+    -- add this if on windows, otherwise server won't open successfully
+    -- detached = false
+  }
+}
 dap.adapters.cilium_kind_control_plane_1 = {
     type = "server",
     host = "127.0.0.1",
@@ -21,6 +31,21 @@ dap.adapters.cilium_operator_kind_worker_1 = {
     port = 23511
 }
 dap.configurations.go = {
+    {
+        name = "Debug unit tests in the current file",
+        type = "cilium_delve",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
+    },
+    {
+        name = "Debug controlplane test (open test/controlplane/${test}/*.go first)",
+        type = "cilium_delve",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}/../",
+        args = { "-test.v", "-test.run", "TestControlPlane/${fileDirname}"},
+    },
     {
         type = "cilium_kind_control_plane_1",
         request = "attach",
