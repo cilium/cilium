cilium/cilium-cli

implement helm get values with sysdump

sysdump/defaults.go
@@ -4,6 +4,7 @@
 package sysdump
 
 import (
+	"github.com/cilium/cilium-cli/defaults"
 	"runtime"
 	"time"
 )
@@ -15,6 +16,7 @@ const (
 const (
 	DefaultCiliumLabelSelector               = labelPrefix + "cilium"
 	DefaultCiliumEnvoyLabelSelector          = labelPrefix + "cilium-envoy"
+	DefaultCiliumHelmChartName               = defaults.HelmReleaseName
 	DefaultCiliumOperatorLabelSelector       = "io.cilium/app=operator"
 	DefaultClustermeshApiserverLabelSelector = labelPrefix + "clustermesh-apiserver"
 	DefaultDebug                             = false

cilium/cilium

[ cherry-picked from cilium/cilium-cli repository ]
include the helm get values in the sysdump collecting, and it will try
to grab the user provide value and write to a file. when users provides
a sysdump file, we don't have to ask the user to submit a separate helm
value file.
in this implementation, we just grab the latest value and user provide
values for cilium. in the future, there can be more enhancement for
this.
we can provide more options to collect previous version or the rendered
helm values for everything.
Signed-off-by: liyi huang <liyi.huang@isovalent.com>

cilium-cli/sysdump/defaults.go
@@ -6,6 +6,8 @@ package sysdump
 import (
 	"runtime"
 	"time"
+
+	"github.com/cilium/cilium-cli/defaults"
 )
 
 const (
@@ -41,6 +43,7 @@ const (
 	DefaultTetragonCLICommand                = "tetra"
 	DefaultTetragonTracingPolicy             = "tetragontracingpolicy-<ts>.yaml"
 	DefaultTetragonTracingPolicyNamespaced   = "tetragontracingpolicynamespaced-<ts>.yaml"
+	DefaultCiliumHelmReleaseName             = defaults.HelmReleaseName
 )
 
 var (