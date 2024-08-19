66bce9bacaf6fb7cfc3c66218c55acb619887d90
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-08-19 14:02:18 +0200

    secretsync: unit test ca.crt
    This commit adds a missing unit test for the k8s -> Envoy secretsync
    for the Trusted CA use-case.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/envoy/secretsync_test.go
+++ pkg/envoy/secretsync_test.go
@@ -23,7 +23,7 @@ import (
 	"github.com/cilium/cilium/pkg/proxy/endpoint"
 )
 
-func Test_k8sToEnvoySecret(t *testing.T) {
+func Test_k8sSecretToEnvoySecretTlsCertificate(t *testing.T) {
 	envoySecret := k8sToEnvoySecret(&slim_corev1.Secret{
 		ObjectMeta: slim_metav1.ObjectMeta{
 			Name:      "dummy-secret",
@@ -33,12 +33,29 @@ func Test_k8sToEnvoySecret(t *testing.T) {
 			"tls.crt": []byte{1, 2, 3},
 			"tls.key": []byte{4, 5, 6},
 		},
-		Type: "kubernetes.io/tls",
+		Type: slim_corev1.SecretTypeTLS,
 	})
 
 	require.Equal(t, "dummy-namespace/dummy-secret", envoySecret.Name)
 	require.Equal(t, []byte{1, 2, 3}, envoySecret.GetTlsCertificate().GetCertificateChain().GetInlineBytes())
 	require.Equal(t, []byte{4, 5, 6}, envoySecret.GetTlsCertificate().GetPrivateKey().GetInlineBytes())
+}
+
+func Test_k8sSecretToEnvoySecretValidationContext(t *testing.T) {
+	envoySecret := k8sToEnvoySecret(&slim_corev1.Secret{
+		ObjectMeta: slim_metav1.ObjectMeta{
+			Name:      "dummy-secret",
+			Namespace: "dummy-namespace",
+		},
+		Data: map[string]slim_corev1.Bytes{
+			"ca.crt": []byte{1, 2, 3},
+		},
+		Type: slim_corev1.SecretTypeOpaque,
+	})
+
+	require.Equal(t, "dummy-namespace/dummy-secret", envoySecret.Name)
+	require.Equal(t, []byte{1, 2, 3}, envoySecret.GetValidationContext().TrustedCa.GetInlineBytes())
+	require.Nil(t, envoySecret.GetTlsCertificate())
 }
 
 func TestHandleSecretEvent(t *testing.T) {
