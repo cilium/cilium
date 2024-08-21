c5ed2214a25a54db44492b81265277961bda1a0f
Julian Wiedmann <jwi@isovalent.com>
2024-08-21 16:21:22 +0300

    datapath: require TCP EDT support and writeable skb queue_mapping
    These features are available as of kernel 5.1, which means we can just
    probe once at startup and bail out.
    
    Simplify the datapath and bandwidth manager accordingly.
    
    Note: from checking the RHEL8 source code (linux-4.18.0-372.32.1.el8_6) the
    skb->tstamp and skb->queue_mapping are writeable there as well.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -22,7 +22,6 @@
 #include "lib/maps.h"
 #include "lib/arp.h"
 #include "lib/edt.h"
-#include "lib/qm.h"
 #include "lib/ipv6.h"
 #include "lib/ipv4.h"
 #include "lib/icmp6.h"
@@ -1483,7 +1482,16 @@ int cil_from_container(struct __ctx_buff *ctx)
 	int ret;
 
 	bpf_clear_meta(ctx);
-	reset_queue_mapping(ctx);
+
+	/* Workaround for GH-18311 where veth driver might have recorded
+	 * veth's RX queue mapping instead of leaving it at 0. This can
+	 * cause issues on the phys device where all traffic would only
+	 * hit a single TX queue (given veth device had a single one and
+	 * mapping was left at 1). Reset so that stack picks a fresh queue.
+	 * Kernel fix is at 710ad98c363a ("veth: Do not record rx queue
+	 * hint in veth_xmit").
+	 */
+	ctx->queue_mapping = 0;
 
 	send_trace_notify(ctx, TRACE_FROM_LXC, sec_label, UNKNOWN_ID,
 			  TRACE_EP_ID_UNKNOWN, TRACE_IFINDEX_UNKNOWN,
