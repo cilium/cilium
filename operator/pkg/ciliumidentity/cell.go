cilium/cilium

commit 78463ff99305da00eb2be6e4ff2be25d579736ee

pkg/ciliumidentity: Use cell flag for EnableOperatorManageCIDs
The EnableOperatorManageCIDs flag no longer needs to be shared. Its only consumer, identitygc, was reworked as part of cilium#34058.

Signed-off-by: Ovidiu Tirla <otirla@google.com>

operator/pkg/ciliumidentity/cell.go
@@ -5,6 +5,7 @@ package ciliumidentity
 
 import (
 	"github.com/cilium/hive/cell"
+	"github.com/spf13/pflag"
 
 	"github.com/cilium/cilium/pkg/metrics"
 )
@@ -18,15 +19,19 @@ var Cell = cell.Module(
 	metrics.Metric(NewMetrics),
 )
 
+type config struct {
+	EnableOperatorManageCIDs bool
+}
+
+func (c config) Flags(flags *pflag.FlagSet) {
+	flags.Bool("operator-manages-identities", c.EnableOperatorManageCIDs, "Enables operator to manage Cilium Identities by running a Cilium Identity controller")
+}
+
 // SharedConfig contains the configuration that is shared between
 // this module and others.
 // It is a temporary solution meant to avoid polluting this module with a direct
 // dependency on global operator and daemon configurations.
 type SharedConfig struct {
-	// EnableOperatorManageCIDs enables operator to manage Cilium Identities by
-	// running a Cilium Identity controller.
-	EnableOperatorManageCIDs bool
-
 	// EnableCiliumEndpointSlice indicates if the Cilium Endpoint Slice feature is
 	// enabled.
 	EnableCiliumEndpointSlice bool

operator/pkg/ciliumidentity/controller.go
@@ -47,6 +47,7 @@ type params struct {
 	cell.In
 
 	Logger              *slog.Logger
+	Config              config
 	Lifecycle           cell.Lifecycle
 	Clientset           k8sClient.Clientset
 	SharedCfg           SharedConfig
@@ -89,7 +90,7 @@ type Controller struct {
 }
 
 func registerController(p params) {
-	if !p.Clientset.IsEnabled() || !p.SharedCfg.EnableOperatorManageCIDs {
+	if !p.Clientset.IsEnabled() || !p.Config.EnableOperatorManageCIDs {
 		return
 	}
 

operator/pkg/ciliumidentity/controller_test.go
@@ -109,9 +109,13 @@ func initHiveTest(operatorManagingCID bool) (*resource.Resource[*capi_v2.CiliumI
 		k8sClient.FakeClientCell,
 		k8s.ResourcesCell,
 		metrics.Metric(NewMetrics),
+		cell.Provide(func() config {
+			return config{
+				EnableOperatorManageCIDs: operatorManagingCID,
+			}
+		}),
 		cell.Provide(func() SharedConfig {
 			return SharedConfig{
-				EnableOperatorManageCIDs:  operatorManagingCID,
 				EnableCiliumEndpointSlice: true,
 			}
 		}),

commit 8f9c4db0cdd4e2a2a0ed267757fcc3c20d6a49bd

pkg/ciliumidentity: Fix cell config
ciliumidentity cell config struct has been introduced in a previous
commit to enable the management of Cilium Identities from the operator.
However, an instnace of the new config type has not been registered into
the cell with cell.Config. This makes the cell not usable, since when it
is added into the operator hive (to be done in a future commit), the
operator is not able to startup correctly anymore:

```
level=error msg="Invoke failed" error="missing dependencies for function \"github.com/cilium/cilium/operator/pkg/ciliumidentity\".registerController\n\t/go/src/github.com/cilium/cilium/operator/pkg/ciliumidentity/controller.go:92:\nmissing type:\n\t- ciliumidentity.config (did you mean to Provide it?)" function="ciliumidentity.registerController (pkg/ciliumidentity/controller.go:92)"
time=2024-09-03T13:53:39Z level=info msg=Stopping
```

Moreover, since the config struct field `EnableOperatorManageCIDs` does
not match exactly the related config option name
`operator-manages-identities`, a mapstructure tag should be added to the
field, so that the hive can correctly load the option value into the
struct.

Fixes: 78463ff993 ("pkg/ciliumidentity: Use cell flag for EnableOperatorManageCIDs")

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

operator/pkg/ciliumidentity/cell.go
@@ -17,14 +17,19 @@ var Cell = cell.Module(
 	"Cilium Identity Controller Operator",
 	cell.Invoke(registerController),
 	metrics.Metric(NewMetrics),
+	cell.Config(defaultConfig),
 )
 
 type config struct {
-	EnableOperatorManageCIDs bool
+	EnableOperatorManageCIDs bool `mapstructure:"operator-manages-identities"`
 }
 
 func (c config) Flags(flags *pflag.FlagSet) {
 	flags.Bool("operator-manages-identities", c.EnableOperatorManageCIDs, "Enables operator to manage Cilium Identities by running a Cilium Identity controller")
+}
+
+var defaultConfig = config{
+	EnableOperatorManageCIDs: false,
 }
 
 // SharedConfig contains the configuration that is shared between