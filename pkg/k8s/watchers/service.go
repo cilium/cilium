cilium/cilium

commit 55357a4b2c9aca18a99f859c5ce9102f599e1aa6

cilium, service: Introduce service.cilium.io/node label
Currently, a service applied to a cluster results in the same datapath exposure
on all nodes participating in the cluster. There are cases where a service should
only be applied on a subset of nodes. Therefore, add a new 'service.cilium.io/node'
label. If a service is labelled as such, the datapath is only populated on nodes
that are labelled with the label and where the values of node and service match.

Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>
Co-developed-by: Daniel Borkmann <daniel@iogearbox.net>
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/k8s/watchers/service.go
@@ -6,6 +6,7 @@ package watchers
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net"
 	"sync/atomic"
 
@@ -13,6 +14,7 @@ import (
 	"github.com/sirupsen/logrus"
 
 	agentK8s "github.com/cilium/cilium/daemon/k8s"
+	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/bgp/speaker"
 	"github.com/cilium/cilium/pkg/cidr"
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
@@ -27,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/redirectpolicy"
 	"github.com/cilium/cilium/pkg/safetime"
@@ -47,6 +50,7 @@ type k8sServiceWatcherParams struct {
 	ServiceManager    service.ServiceManager
 	LRPManager        *redirectpolicy.Manager
 	MetalLBBgpSpeaker speaker.MetalLBBgpSpeaker
+	LocalNodeStore    *node.LocalNodeStore
 }
 
 func newK8sServiceWatcher(params k8sServiceWatcherParams) *K8sServiceWatcher {
@@ -59,6 +63,7 @@ func newK8sServiceWatcher(params k8sServiceWatcherParams) *K8sServiceWatcher {
 		svcManager:            params.ServiceManager,
 		redirectPolicyManager: params.LRPManager,
 		bgpSpeakerManager:     params.MetalLBBgpSpeaker,
+		localNodeStore:        params.LocalNodeStore,
 		stop:                  make(chan struct{}),
 	}
 }
@@ -78,6 +83,7 @@ type K8sServiceWatcher struct {
 	svcManager            svcManager
 	redirectPolicyManager redirectPolicyManager
 	bgpSpeakerManager     bgpSpeakerManager
+	localNodeStore        *node.LocalNodeStore
 
 	stop chan struct{}
 }
@@ -352,7 +358,12 @@ func genCartesianProduct(
 }
 
 // datapathSVCs returns all services that should be set in the datapath.
-func datapathSVCs(svc *k8s.Service, endpoints *k8s.Endpoints) (svcs []loadbalancer.SVC) {
+func (k *K8sServiceWatcher) datapathSVCs(svc *k8s.Service, endpoints *k8s.Endpoints) ([]loadbalancer.SVC, error) {
+	svcs := []loadbalancer.SVC{}
+
+	if nodeMatches, err := k.checkServiceNodeExposure(svc); err != nil || !nodeMatches {
+		return svcs, err
+	}
 	uniqPorts := svc.UniquePorts()
 
 	clusterIPPorts := map[loadbalancer.FEPortName]*loadbalancer.L4Addr{}
@@ -409,7 +420,25 @@ func datapathSVCs(svc *k8s.Service, endpoints *k8s.Endpoints) (svcs []loadbalanc
 		svcs[i].Annotations = svc.Annotations
 	}
 
-	return svcs
+	return svcs, nil
+}
+
+// checkServiceNodeExposure returns true if the service should be installed onto the
+// local node, and false if the node should ignore and not install the service.
+func (k *K8sServiceWatcher) checkServiceNodeExposure(svc *k8s.Service) (bool, error) {
+	if serviceLabelValue, serviceLabelExists := svc.Labels[annotation.ServiceNodeExposure]; serviceLabelExists {
+		ln, err := k.localNodeStore.Get(context.Background())
+		if err != nil {
+			return false, fmt.Errorf("failed to retrieve local node: %w", err)
+		}
+
+		nodeLabelValue, nodeLabelExists := ln.Labels[annotation.ServiceNodeExposure]
+		if !nodeLabelExists || nodeLabelValue != serviceLabelValue {
+			return false, nil
+		}
+	}
+
+	return true, nil
 }
 
 // hashSVCMap returns a mapping of all frontend's hash to the its corresponded
@@ -432,15 +461,21 @@ func (k *K8sServiceWatcher) addK8sSVCs(svcID k8s.ServiceID, oldSvc, svc *k8s.Ser
 		logfields.K8sSvcName:   svcID.Name,
 		logfields.K8sNamespace: svcID.Namespace,
 	})
-
-	svcs := datapathSVCs(svc, endpoints)
+	svcs, err := k.datapathSVCs(svc, endpoints)
+	if err != nil {
+		scopedLog.WithError(err).Error("Error while evaluating datapath services")
+		return
+	}
 	svcMap := hashSVCMap(svcs)
 
 	if oldSvc != nil {
 		// If we have oldService then we need to detect which frontends
 		// are no longer in the updated service and delete them in the datapath.
-
-		oldSVCs := datapathSVCs(oldSvc, endpoints)
+		oldSVCs, err := k.datapathSVCs(oldSvc, endpoints)
+		if err != nil {
+			scopedLog.WithError(err).Error("Error while evaluating datapath services for old service")
+			return
+		}
 		oldSVCMap := hashSVCMap(oldSVCs)
 
 		for svcHash, oldSvc := range oldSVCMap {

pkg/annotation/k8s.go
@@ -117,6 +117,11 @@ const (
 	ServiceAffinity      = ServicePrefix + "/affinity"
 	ServiceAffinityAlias = Prefix + "/service-affinity"
 
+	// ServiceNodeExposure is the label name used to mark a service to only a
+	// subset of the nodes which match the same value. For all other nodes, this
+	// service is ignored and not installed into their datapath.
+	ServiceNodeExposure = ServicePrefix + "/node"
+
 	// ProxyVisibility / ProxyVisibilityAlias is the annotation name used to
 	// indicate whether proxy visibility should be enabled for a given pod (i.e.,
 	// all traffic for the pod is redirected to the proxy for the given port /

commit dac3ffaa23071cb1d96fb3c0332db0c311781733

k8s: Convert service.cilium.io/node to annotation
Instead of using as a label.

This aligns better with K8s common practice to put configuration options
within annotations instead of labels. Also, other service.cilium.io/*
options are exposed as annotations.

Fixes: 55357a4b2c ("cilium, service: Introduce service.cilium.io/node label")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/k8s/watchers/service.go
@@ -432,14 +432,14 @@ func (k *K8sServiceWatcher) datapathSVCs(svc *k8s.Service, endpoints *k8s.Endpoi
 // checkServiceNodeExposure returns true if the service should be installed onto the
 // local node, and false if the node should ignore and not install the service.
 func (k *K8sServiceWatcher) checkServiceNodeExposure(svc *k8s.Service) (bool, error) {
-	if serviceLabelValue, serviceLabelExists := svc.Labels[annotation.ServiceNodeExposure]; serviceLabelExists {
+	if serviceAnnotationValue, serviceAnnotationExists := svc.Annotations[annotation.ServiceNodeExposure]; serviceAnnotationExists {
 		ln, err := k.localNodeStore.Get(context.Background())
 		if err != nil {
 			return false, fmt.Errorf("failed to retrieve local node: %w", err)
 		}
 
 		nodeLabelValue, nodeLabelExists := ln.Labels[annotation.ServiceNodeExposure]
-		if !nodeLabelExists || nodeLabelValue != serviceLabelValue {
+		if !nodeLabelExists || nodeLabelValue != serviceAnnotationValue {
 			return false, nil
 		}
 	}

pkg/k8s/node_test.go
@@ -384,7 +384,7 @@ func TestParseNodeWithService(t *testing.T) {
 	objMeta := slim_metav1.ObjectMeta{
 		Name:      "foo",
 		Namespace: "bar",
-		Labels: map[string]string{
+		Annotations: map[string]string{
 			annotation.ServiceNodeExposure: "beefy",
 		},
 	}
@@ -406,7 +406,7 @@ func TestParseNodeWithService(t *testing.T) {
 		IntTrafficPolicy:         loadbalancer.SVCTrafficPolicyCluster,
 		FrontendIPs:              []net.IP{net.ParseIP("127.0.0.1")},
 		Selector:                 map[string]string{"foo": "bar"},
-		Labels:                   map[string]string{annotation.ServiceNodeExposure: "beefy"},
+		Annotations:              map[string]string{annotation.ServiceNodeExposure: "beefy"},
 		Ports:                    map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
 		NodePorts:                map[loadbalancer.FEPortName]NodePortToFrontend{},
 		LoadBalancerSourceRanges: map[string]*cidr.CIDR{},