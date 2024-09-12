95830b62528eb40645b3fedc70c345b2c7d6d31c
André Martins <andre@cilium.io>
2024-09-12 18:15:51 +0200

    .github: use caches for lint-build-commits
    With theses changes, the workflow run time will decreases tremendously
    up with the usage of ccache and golang caches.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- contrib/scripts/builder.sh
+++ contrib/scripts/builder.sh
@@ -12,8 +12,29 @@ if [ -n "${RUN_AS_NONROOT:-}" ]; then
     USER_OPTION="--user $(id -u):$(id -g)"
 fi
 
+MOUNT_GOCACHE_DIR=""
+
+if [ -n "${BUILDER_GOCACHE_DIR:-}" ]; then
+    MOUNT_GOCACHE_DIR="-v ${BUILDER_GOCACHE_DIR}:/root/.cache/go-build"
+fi
+
+MOUNT_GOMODCACHE_DIR=""
+
+if [ -n "${BUILDER_GOMODCACHE_DIR:-}" ]; then
+    MOUNT_GOMODCACHE_DIR="-v ${BUILDER_GOMODCACHE_DIR}:/go/pkg/mod"
+fi
+
+MOUNT_CCACHE_DIR=""
+
+if [ -n "${BUILDER_CCACHE_DIR:-}" ]; then
+    MOUNT_CCACHE_DIR="-v ${BUILDER_CCACHE_DIR}:/root/.ccache"
+fi
+
 docker run --rm \
 	$USER_OPTION \
+	$MOUNT_GOCACHE_DIR \
+	$MOUNT_GOMODCACHE_DIR \
+	$MOUNT_CCACHE_DIR \
 	-v "$PWD":/go/src/github.com/cilium/cilium \
 	-w /go/src/github.com/cilium/cilium \
 	${DOCKER_ARGS:+"$DOCKER_ARGS"} \
