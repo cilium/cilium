85c483fee642cded5410436b8bb2f5081d4f1869
Jarno Rajahalme <jarno@isovalent.com>
2024-09-13 10:49:35 +0000

    policy: Use 'insertOldIfNotExists() in addDependentOnEntry()
    [ upstream commit 3aad3f8bcbc6fda4a59236591170844a0576d712 ]
    
    [ backporter's notes: hit minor conflict due to different surrounding
      context; accepted combination. Additionally updated the
      TestMapState_AccumulateMapChangesOnVisibilityKeys test, which has
      been removed in the meanwhile on main, applying the changes
      originally part of cilium/cilium@137abf7b4b6a (from #35109). ]
    
    Keep the oldest changed value in 'addDependentOnEntry()' by using
    'insertOldIfNotExists()' rather than modifying 'changes.Old'
    directly. Changes in tests are due to the way 'insertOldIfNotExists()'
    filters out new entries (not adding an 'old' entry if the key exists in
    'changes.Adds'). Test changes are in the 'deletes' field due to the test
    internally setting a 'deletes' key for each key in 'changes.Old'.
    
    Old key values are used when reverting a failed endpoint update, prior to
    this fix that revert may have produced incorrect results.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/policy/mapstate.go
+++ pkg/policy/mapstate.go
@@ -218,9 +218,7 @@ func (keys MapState) AddDependent(owner Key, dependent Key, changes ChangeState)
 // addDependentOnEntry adds 'dependent' to the set of dependent keys of 'e'.
 func (keys MapState) addDependentOnEntry(owner Key, e MapStateEntry, dependent Key, changes ChangeState) {
 	if _, exists := e.dependents[dependent]; !exists {
-		if changes.Old != nil {
-			changes.Old[owner] = e
-		}
+		changes.insertOldIfNotExists(owner, e)
 		e.AddDependent(dependent)
 		keys[owner] = e
 	}
@@ -378,9 +376,7 @@ func (keys MapState) addKeyWithChanges(key Key, entry MapStateEntry, changes Cha
 		}
 
 		// Save old value before any changes, if desired
-		if changes.Old != nil {
-			changes.insertOldIfNotExists(key, oldEntry)
-		}
+		changes.insertOldIfNotExists(key, oldEntry)
 
 		// Compare for datapath equalness before merging, as the old entry is updated in
 		// place!
@@ -872,8 +868,11 @@ var visibilityDerivedFromLabels = labels.LabelArray{
 
 var visibilityDerivedFrom = labels.LabelArrayList{visibilityDerivedFromLabels}
 
-// insertIfNotExists only inserts `key=value` if `key` does not exist in keys already
-// returns 'true' if 'key=entry' was added to 'keys'
+// insertIfNotExists only inserts an entry in 'changes.Old' if 'key' does not exist in there already
+// and 'key' does not already exist in 'changes.Adds'. This prevents recording "old" values for
+// newly added keys. When an entry is updated, we are called before the key is added to
+// 'changes.Adds' so we'll record the old value as expected.
+// Returns 'true' if an old entry was added.
 func (changes *ChangeState) insertOldIfNotExists(key Key, entry MapStateEntry) bool {
 	if changes == nil || changes.Old == nil {
 		return false
