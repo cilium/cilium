c0bd90399966b3aad511e83eddc863548ac326c2
Marc Suñé <marc.sune@isovalent.com>
2024-09-17 15:10:21 +0200

    bpf: Add support for IPv6 L2 announcements
    This patch adds supports in bpf_host for IPv6 L2 announcements.
    
    Signed-off-by: Marc Suñé <marc.sune@isovalent.com>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -1005,14 +1005,16 @@ do_netdev_encrypt_encap(struct __ctx_buff *ctx, __be16 proto, __u32 src_id)
 #endif /* ENABLE_IPSEC && TUNNEL_MODE */
 
 #ifdef ENABLE_L2_ANNOUNCEMENTS
-static __always_inline int handle_l2_announcement(struct __ctx_buff *ctx)
+static __always_inline
+int handle_l2_announcement(struct __ctx_buff *ctx, struct ipv6hdr *ip6)
 {
-	union macaddr mac = THIS_INTERFACE_MAC;
+	union macaddr mac = CONFIG(interface_mac);
 	union macaddr smac;
-	__be32 sip;
-	__be32 tip;
-	struct l2_responder_v4_key key;
-	struct l2_responder_v4_stats *stats;
+	__be32 __maybe_unused sip;
+	__be32 __maybe_unused tip;
+	union v6addr __maybe_unused sip6;
+	union v6addr __maybe_unused tip6;
+	struct l2_responder_stats *stats;
 	int ret;
 	__u64 time;
 
@@ -1027,22 +1029,47 @@ static __always_inline int handle_l2_announcement(struct __ctx_buff *ctx)
 	if (ktime_get_ns() - (time) > L2_ANNOUNCEMENTS_MAX_LIVENESS)
 		return CTX_ACT_OK;
 
-	if (!arp_validate(ctx, &mac, &smac, &sip, &tip))
-		return CTX_ACT_OK;
+	if (!ip6) {
+		struct l2_responder_v4_key key;
 
-	key.ip4 = tip;
-	key.ifindex = ctx->ingress_ifindex;
-	stats = map_lookup_elem(&cilium_l2_responder_v4, &key);
-	if (!stats)
-		return CTX_ACT_OK;
+		if (!arp_validate(ctx, &mac, &smac, &sip, &tip))
+			return CTX_ACT_OK;
 
-	ret = arp_respond(ctx, &mac, tip, &smac, sip, 0);
+		key.ip4 = tip;
+		key.ifindex = ctx->ingress_ifindex;
+		stats = map_lookup_elem(&cilium_l2_responder_v4, &key);
+		if (!stats)
+			return CTX_ACT_OK;
+
+		ret = arp_respond(ctx, &mac, tip, &smac, sip, 0);
+	} else {
+#ifdef ENABLE_IPV6
+		struct l2_responder_v6_key key6;
+
+		if (!icmp6_ndisc_validate(ctx, ip6, &mac, &smac, &sip6, &tip6))
+			return CTX_ACT_OK;
+
+		key6.ip6 = tip6;
+		key6.ifindex = ctx->ingress_ifindex;
+		key6.pad = 0;
+		stats = map_lookup_elem(&cilium_l2_responder_v6, &key6);
+		if (!stats)
+			return CTX_ACT_OK;
+
+		int l3_off = (__u8 *)ip6 - (__u8 *)ctx_data(ctx);
+
+		ret = icmp6_send_ndisc_adv(ctx, l3_off, &mac, false);
+#else
+		return CTX_ACT_OK;
+#endif
+	}
 
 	if (ret == CTX_ACT_REDIRECT)
 		__sync_fetch_and_add(&stats->responses_sent, 1);
 
 	return ret;
-};
+}
+
 #endif
 
 static __always_inline int
@@ -1071,7 +1098,7 @@ do_netdev(struct __ctx_buff *ctx, __u16 proto, __u32 __maybe_unused identity,
 		send_trace_notify(ctx, obs_point, UNKNOWN_ID, UNKNOWN_ID, TRACE_EP_ID_UNKNOWN,
 				  ctx->ingress_ifindex, trace.reason, trace.monitor);
 		#ifdef ENABLE_L2_ANNOUNCEMENTS
-			ret = handle_l2_announcement(ctx);
+			ret = handle_l2_announcement(ctx, NULL);
 		#else
 			ret = CTX_ACT_OK;
 		#endif
@@ -1080,8 +1107,22 @@ do_netdev(struct __ctx_buff *ctx, __u16 proto, __u32 __maybe_unused identity,
 #ifdef ENABLE_IPV6
 	case bpf_htons(ETH_P_IPV6):
 		if (!revalidate_data_pull(ctx, &data, &data_end, &ip6))
-			return send_drop_notify_error(ctx, identity, DROP_INVALID,
+			return send_drop_notify_error(ctx, identity,
+						      DROP_INVALID,
 						      METRIC_INGRESS);
+#ifdef ENABLE_L2_ANNOUNCEMENTS
+		if (ip6->nexthdr == NEXTHDR_ICMP) {
+			ret = handle_l2_announcement(ctx, ip6);
+			if (ret != CTX_ACT_OK)
+				break;
+			/* Verifier invalidates ip6 for some reason.. sigh*/
+			if (!revalidate_data_pull(ctx, &data, &data_end, &ip6))
+				return send_drop_notify_error(ctx, identity,
+							      DROP_INVALID,
+							      METRIC_INGRESS);
+		}
+
+#endif /*ENABLE_L2_ANNOUNCEMENTS */
 
 		identity = resolve_srcid_ipv6(ctx, ip6, identity, &ipcache_srcid, from_host);
 		ctx_store_meta(ctx, CB_SRC_LABEL, identity);
