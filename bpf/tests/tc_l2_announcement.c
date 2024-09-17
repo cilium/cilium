35152c65c504cd8933e2aba5c178c6310b798633
Marc Suñé <marc.sune@isovalent.com>
2024-09-17 15:10:21 +0200

    bpf: Add support for IPv6 L2 announcements
    This patch adds supports in bpf_host for IPv6 L2 announcements.
    
    Signed-off-by: Marc Suñé <marc.sune@isovalent.com>

--- bpf/tests/tc_l2_announcement.c
+++ bpf/tests/tc_l2_announcement.c
@@ -151,7 +151,7 @@ SETUP("tc", "1_happy_path")
 int l2_announcement_arp_happy_path_setup(struct __ctx_buff *ctx)
 {
 	struct l2_responder_v4_key key;
-	struct l2_responder_v4_stats value = {0};
+	struct l2_responder_stats value = {0};
 
 	key.ifindex = 0;
 	key.ip4 = v4_svc_one;
