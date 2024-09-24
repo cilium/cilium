fadfc2b9306a612a2ae45027357047d42a8b3dcf
Dylan Reimerink <dylan.reimerink@isovalent.com>
2024-09-24 13:20:09 +0200

    endpoint: Add parent ifindex to endpoint and endpoint map
    This commit adds a new field to the endpoint struct called the parent
    interface index. It records the ifindex of the ENI interface to which
    the endpoint "belongs". The IP of the endpoint is assigned from this
    ENI interface and all non-masquerade traffic from the endpoint should
    route through this interface. The value is only relevant for ENI
    environments and is set to 0 for non-ENI environments.
    
    In a followup, the parent ifindex will be used by datapath changes to
    ensure that reply traffic from ENI endpoints is always routed back
    through the correct ENI interface.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- pkg/maps/lxcmap/lxcmap.go
+++ pkg/maps/lxcmap/lxcmap.go
@@ -62,6 +62,7 @@ type EndpointFrontend interface {
 	LXCMac() mac.MAC
 	GetNodeMAC() mac.MAC
 	GetIfIndex() int
+	GetParentIfIndex() int
 	GetID() uint64
 	IPv4Address() netip.Addr
 	IPv6Address() netip.Addr
@@ -102,11 +103,12 @@ func GetBPFValue(e EndpointFrontend) (*EndpointInfo, error) {
 
 	// Both lxc and node mac can be nil for the case of L3/NOARP devices.
 	info := &EndpointInfo{
-		IfIndex: uint32(e.GetIfIndex()),
-		LxcID:   uint16(e.GetID()),
-		MAC:     mac,
-		NodeMAC: nodeMAC,
-		SecID:   e.GetIdentity().Uint32(), // Host byte-order
+		IfIndex:       uint32(e.GetIfIndex()),
+		LxcID:         uint16(e.GetID()),
+		MAC:           mac,
+		NodeMAC:       nodeMAC,
+		SecID:         e.GetIdentity().Uint32(), // Host byte-order
+		ParentIfIndex: uint32(e.GetParentIfIndex()),
 	}
 
 	if e.IsAtHostNS() {
@@ -117,7 +119,7 @@ func GetBPFValue(e EndpointFrontend) (*EndpointInfo, error) {
 
 }
 
-type pad3uint32 [3]uint32
+type pad2uint32 [2]uint32
 
 // EndpointInfo represents the value of the endpoints BPF map.
 //
@@ -128,11 +130,12 @@ type EndpointInfo struct {
 	LxcID   uint16 `align:"lxc_id"`
 	Flags   uint32 `align:"flags"`
 	// go alignment
-	_       uint32
-	MAC     mac.Uint64MAC `align:"mac"`
-	NodeMAC mac.Uint64MAC `align:"node_mac"`
-	SecID   uint32        `align:"sec_id"`
-	Pad     pad3uint32    `align:"pad"`
+	_             uint32
+	MAC           mac.Uint64MAC `align:"mac"`
+	NodeMAC       mac.Uint64MAC `align:"node_mac"`
+	SecID         uint32        `align:"sec_id"`
+	ParentIfIndex uint32        `align:"parent_ifindex"`
+	Pad           pad2uint32    `align:"pad"`
 }
 
 type EndpointKey struct {
