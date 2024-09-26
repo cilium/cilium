cilium/cilium

commit 2cfeb0a2727b867414055f5d8c6c3a2769dd700f

LB-IPAM: Add flag to ignore services without LBClass set
The existing behavior of LB-IPAM was to assume LB-IPAM is responsible
for services without an LBClass set. This made sense since LB-IPAM is
aimed at environments where traditional LB controllers are not
available and there were still supported k8s deployments that did not
support LBClass.

All supported k8s deployments now support LBClass, and users have
reported situations where the default LB controller has to be assigned
to a controller other than LB-IPAM due to the other controller not
respecting LBClasses. Therefor, it is advantages to have the option to
tell LB-IPAM to ignore services without an LBClass set and only manage
services with an LBClass that we own.

This commit adds this functionality, by default we still have the old
behavior, but the `--lbipam-require-lb-class` flag can be set to true
to make LB-IPAM ignore services without an LBClass set.

Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

operator/pkg/lbipam/cell.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 
 	cilium_api_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
@@ -27,7 +28,20 @@ var Cell = cell.Module(
 	cell.Invoke(func(*LBIPAM) {}),
 	// Provide LB-IPAM related metrics
 	metrics.Metric(newMetrics),
+	// Register configuration flags
+	cell.Config(lbipamConfig{
+		LBIPAMRequireLBClass: false,
+	}),
 )
+
+type lbipamConfig struct {
+	LBIPAMRequireLBClass bool
+}
+
+func (lc lbipamConfig) Flags(flags *pflag.FlagSet) {
+	flags.BoolVar(&lc.LBIPAMRequireLBClass, "lbipam-require-lb-class", lc.LBIPAMRequireLBClass, "Require the LoadBalancerClass field to "+
+		"be set on services for LB-IPAM to start assigning IPs")
+}
 
 type lbipamCellParams struct {
 	cell.In
@@ -45,6 +59,8 @@ type lbipamCellParams struct {
 	DaemonConfig *option.DaemonConfig
 
 	Metrics *ipamMetrics
+
+	Config lbipamConfig
 }
 
 func newLBIPAMCell(params lbipamCellParams) *LBIPAM {

operator/pkg/lbipam/lbipam.go
@@ -82,6 +82,8 @@ type lbIPAMParams struct {
 	jobGroup job.Group
 
 	metrics *ipamMetrics
+
+	config lbipamConfig
 }
 
 func newLBIPAM(params lbIPAMParams) *LBIPAM {
@@ -1135,10 +1137,10 @@ func (ipam *LBIPAM) isResponsibleForSVC(svc *slim_core_v1.Service) bool {
 		return false
 	}
 
-	// We will assume that we are the default LB, LB-IPAM shouldn't be enabled clusters that don't support LBClasses
-	// and have multiple LBs.
+	// If no load balancer class is specified, we will assume that we are responsible for the service
+	// unless we have been configured to require a load balancer class.
 	if svc.Spec.LoadBalancerClass == nil {
-		return true
+		return !ipam.lbIPAMParams.config.LBIPAMRequireLBClass
 	}
 
 	if !slices.Contains(ipam.lbClasses, *svc.Spec.LoadBalancerClass) {

operator/pkg/lbipam/lbipam_test.go
@@ -1892,6 +1892,47 @@ func TestNonMatchingLBClass(t *testing.T) {
 	}
 }
 
+// TestRequiredLBClass tests that when LBIPAM is configured to only allocate IPs for services with a specific
+// LoadBalancerClass, we leave services without a LoadBalancerClass alone.
+func TestRequiredLBClass(t *testing.T) {
+	poolA := mkPool(poolAUID, "pool-a", []string{"10.0.10.0/24"})
+	fixture := mkTestFixture(true, true)
+
+	// Enable the requirement for a specific LBClass and set a class to look for
+	fixture.lbipam.config.LBIPAMRequireLBClass = true
+	fixture.lbipam.lbClasses = []string{cilium_api_v2alpha1.BGPLoadBalancerClass}
+
+	fixture.UpsertPool(t, poolA)
+
+	svcA := &slim_core_v1.Service{
+		ObjectMeta: slim_meta_v1.ObjectMeta{
+			Name:      "service-a",
+			Namespace: "default",
+			UID:       serviceAUID,
+		},
+		Spec: slim_core_v1.ServiceSpec{
+			Type: slim_core_v1.ServiceTypeLoadBalancer,
+		},
+	}
+	fixture.UpsertSvc(t, svcA)
+	svcA = fixture.GetSvc("default", "service-a")
+
+	if len(svcA.Status.LoadBalancer.Ingress) != 0 {
+		t.Error("Expected service to receive no ingress IPs")
+	}
+
+	lbClass := cilium_api_v2alpha1.BGPLoadBalancerClass
+	svcA.Spec.LoadBalancerClass = &lbClass
+
+	fixture.UpsertSvc(t, svcA)
+
+	svcA = fixture.GetSvc("default", "service-a")
+
+	if len(svcA.Status.LoadBalancer.Ingress) == 0 {
+		t.Error("Expected service to receive ingress IPs")
+	}
+}
+
 // TestChangePoolSelector tests that when the selector of a pool changes, all services which no longer match are
 // stripped of their allocations and assignments
 func TestChangePoolSelector(t *testing.T) {

commit 82afefca458a5d972a7139514194083d6f9fc791

LB-IPAM: Pass `lbipamConfig` struct to controller
Fixes: 2cfeb0a2727b ("LB-IPAM: Add flag to ignore services without LBClass set")

This commit ensures that the `--lbipam-require-lb-class` actually has an
effect on the LB-IPAM controller by assigning the parsed `lbipamConfig`
struct to field `config` of the `lbIPAMParams` struct which is used to
initialize the LB-IPAM controller.

Signed-off-by: Simon Gerber <simon.gerber@vshn.ch>

operator/pkg/lbipam/cell.go
@@ -91,6 +91,7 @@ func newLBIPAMCell(params lbipamCellParams) *LBIPAM {
 		poolClient:   params.Clientset.CiliumV2alpha1().CiliumLoadBalancerIPPools(),
 		svcClient:    params.Clientset.Slim().CoreV1(),
 		jobGroup:     params.JobGroup,
+		config:       params.Config,
 	})
 
 	lbIPAM.jobGroup.Add(