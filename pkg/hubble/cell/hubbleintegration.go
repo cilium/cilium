2cf16e52a8686b00b1e13cffa3a67466be8127ed
Alexandre Perrin <alex@isovalent.com>
2024-09-30 13:11:56 +0200

    hubble: [02/14] extract status report from the Cilium daemon
    Move the Hubble status report into the new Hubble cell.
    
    Before this patch, a pointer to the "Hubble Observer" was held in the
    Cilium daemon. This pointer was set once at the very end of
    launchHubble(), as a signal of Hubble components startup finalization.
    
    This commit moves the pointer into the Hubble struct, removing the
    Hubble status report concern away from the Cilium daemon into the Hubble
    cell. It is the first step in gradually replacing all references of the
    Cilium daemon by the Hubble cell in launchHubble(). The next commits
    will introduce similar changes to gradually remove the dependency on the
    Cilium daemon.
    
    Signed-off-by: Alexandre Perrin <alex@isovalent.com>

--- pkg/hubble/cell/hubbleintegration.go
+++ pkg/hubble/cell/hubbleintegration.go
@@ -3,13 +3,27 @@
 
 package hubblecell
 
-import "github.com/cilium/cilium/pkg/option"
+import (
+	"context"
+	"sync/atomic"
+	"time"
+
+	"github.com/go-openapi/strfmt"
+
+	"github.com/cilium/cilium/api/v1/models"
+	observerpb "github.com/cilium/cilium/api/v1/observer"
+	"github.com/cilium/cilium/pkg/hubble/observer"
+	"github.com/cilium/cilium/pkg/option"
+)
 
 // Hubble is responsible for configuration, initialization, and shutdown of
 // every Hubble components including the Hubble observer servers (TCP, UNIX
 // domain socket), the Hubble metrics server, etc.
 type Hubble struct {
 	agentConfig *option.DaemonConfig
+	// Observer will be set by the Cilium daemon once the Hubble Observer has
+	// been started.
+	Observer atomic.Pointer[observer.LocalObserverServer]
 }
 
 // new creates and return a new Hubble.
@@ -18,5 +32,49 @@ func new(
 ) *Hubble {
 	return &Hubble{
 		agentConfig: agentConfig,
+		Observer:    atomic.Pointer[observer.LocalObserverServer]{},
 	}
+}
+
+// Status report the Hubble status for the Cilium Daemon status collector
+// probe.
+func (h *Hubble) Status(ctx context.Context) *models.HubbleStatus {
+	if !h.agentConfig.EnableHubble {
+		return &models.HubbleStatus{State: models.HubbleStatusStateDisabled}
+	}
+
+	obs := h.Observer.Load()
+	if obs == nil {
+		return &models.HubbleStatus{
+			State: models.HubbleStatusStateWarning,
+			Msg:   "Server not initialized",
+		}
+	}
+
+	req := &observerpb.ServerStatusRequest{}
+	status, err := obs.ServerStatus(ctx, req)
+	if err != nil {
+		return &models.HubbleStatus{State: models.HubbleStatusStateFailure, Msg: err.Error()}
+	}
+
+	metricsState := models.HubbleStatusMetricsStateDisabled
+	if option.Config.HubbleMetricsServer != "" {
+		// TODO: The metrics package should be refactored to be able report its actual state
+		metricsState = models.HubbleStatusMetricsStateOk
+	}
+
+	hubbleStatus := &models.HubbleStatus{
+		State: models.StatusStateOk,
+		Observer: &models.HubbleStatusObserver{
+			CurrentFlows: int64(status.NumFlows),
+			MaxFlows:     int64(status.MaxFlows),
+			SeenFlows:    int64(status.SeenFlows),
+			Uptime:       strfmt.Duration(time.Duration(status.UptimeNs)),
+		},
+		Metrics: &models.HubbleStatusMetrics{
+			State: metricsState,
+		},
+	}
+
+	return hubbleStatus
 }
