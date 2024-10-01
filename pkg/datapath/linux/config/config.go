cilium/cilium

commit 68953415f03af14a1fadfd8c4638eebf0a52fd7e

cilium, connector: Add netkit connector
Add netkit support to Cilium.

netkit is a new, minimal BPF-programmable device we presented at
LSF/MM/BPF, LPC and KubeCons some time ago. The core idea is that
BPF programs are executed within the drivers xmit routine and
therefore e.g. in case of containers/Pods moving BPF processing
closer to the source.

One of the goals was that in case of Pod egress traffic, this
allows to move BPF programs from hostns tcx ingress into the
device itself, providing earlier drop or forward mechanisms, for
example, if the BPF program determines that the skb must be sent
out of the node, then a redirect to the physical device can take
place directly without going through per-CPU backlog queue. This
helps to shift processing for such traffic from softirq to process
context, leading to better scheduling decisions/performance (see
measurements in the slides).

Going forward, we plan to use netkit devices in Cilium as the main
device type for connecting Pods. They will be operated in L3 mode
in order to simplify a Pod's neighbor management and the peer will
operate in default drop mode, so that no traffic is leaving between
the time when a Pod is brought up by the CNI plugin and programs
attached by the agent. This PR supports both netkit operation modes
with the L3 mode being the main/recommended one.

Note: netkit in L3 mode has an all-zero mac, so the eth header has
dst == src == zero mac, hence the tests for mac len in the template
code as node mac is not needed/used. Also in L3 mode any ARP
responder is compiled out as the Pod with netkit does not attempt
to resolve mac addresses (it's a NOARP device).

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
Link: https://lore.kernel.org/bpf/20231024214904.29825-1-daniel@iogearbox.net
Link: https://sched.co/1R2s5

pkg/datapath/linux/config/config.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/link"
 	dpdef "github.com/cilium/cilium/pkg/datapath/linux/config/defines"
 	"github.com/cilium/cilium/pkg/datapath/linux/sysctl"
+	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/datapath/tables"
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/defaults"
@@ -1088,10 +1089,12 @@ func (h *HeaderfileWriter) writeTemplateConfig(fw *bufio.Writer, devices []strin
 
 	fmt.Fprintf(fw, "#define HOST_EP_ID %d\n", uint32(hostEndpointID))
 
-	if e.RequireARPPassthrough() {
-		fmt.Fprint(fw, "#define ENABLE_ARP_PASSTHROUGH 1\n")
-	} else {
-		fmt.Fprint(fw, "#define ENABLE_ARP_RESPONDER 1\n")
+	if option.Config.DatapathMode != datapathOption.DatapathModeNetkit {
+		if e.RequireARPPassthrough() {
+			fmt.Fprint(fw, "#define ENABLE_ARP_PASSTHROUGH 1\n")
+		} else {
+			fmt.Fprint(fw, "#define ENABLE_ARP_RESPONDER 1\n")
+		}
 	}
 
 	if e.ConntrackLocalLocked() {

cilium-health/launch/endpoint.go
@@ -46,13 +46,12 @@ const (
 	ciliumHealth = "cilium-health"
 	binaryName   = "cilium-health-responder"
 
-	// vethName is the host-side veth link device name for cilium-health EP
-	// (veth mode only).
-	vethName = "lxc_health"
+	// healthName is the host-side virtual device name for cilium-health EP
+	healthName = "lxc_health"
 
-	// legacyVethName is the host-side cilium-health EP device name used in
+	// legacyHealthName is the host-side cilium-health EP device name used in
 	// older Cilium versions. Used for removal only.
-	legacyVethName = "cilium_health"
+	legacyHealthName = "cilium_health"
 
 	// epIfaceName is the endpoint-side link device name for cilium-health.
 	epIfaceName = "cilium"
@@ -194,13 +193,14 @@ func CleanupEndpoint() {
 	// Explicit removal is performed to ensure that everything referencing the network namespace
 	// the endpoint process is executed under is disposed, so that the network namespace itself is properly disposed.
 	switch option.Config.DatapathMode {
-	case datapathOption.DatapathModeVeth:
-		for _, iface := range []string{legacyVethName, vethName} {
-			scopedLog := log.WithField(logfields.Veth, iface)
+	case datapathOption.DatapathModeVeth, datapathOption.DatapathModeNetkit, datapathOption.DatapathModeNetkitL2:
+		for _, iface := range []string{legacyHealthName, healthName} {
+			scopedLog := log.WithField(logfields.Interface, iface)
 			if link, err := netlink.LinkByName(iface); err == nil {
 				err = netlink.LinkDel(link)
 				if err != nil {
-					scopedLog.WithError(err).Info("Couldn't delete cilium-health veth device")
+					scopedLog.WithError(err).Infof("Couldn't delete cilium-health %s device",
+						option.Config.DatapathMode)
 				}
 			} else {
 				scopedLog.WithError(err).Debug("Didn't find existing device")
@@ -274,14 +274,25 @@ func LaunchAsEndpoint(baseCtx context.Context,
 
 	switch option.Config.DatapathMode {
 	case datapathOption.DatapathModeVeth:
-		_, epLink, err := connector.SetupVethWithNames(vethName, epIfaceName, mtuConfig.GetDeviceMTU(),
+		_, epLink, err := connector.SetupVethWithNames(healthName, epIfaceName, mtuConfig.GetDeviceMTU(),
 			bigTCPConfig.GetGROIPv6MaxSize(), bigTCPConfig.GetGSOIPv6MaxSize(),
 			bigTCPConfig.GetGROIPv4MaxSize(), bigTCPConfig.GetGSOIPv4MaxSize(),
 			info, sysctl)
 		if err != nil {
 			return nil, fmt.Errorf("Error while creating veth: %w", err)
 		}
-
+		if err = netlink.LinkSetNsFd(epLink, int(ns.FD())); err != nil {
+			return nil, fmt.Errorf("failed to move device %q to health namespace: %w", epIfaceName, err)
+		}
+	case datapathOption.DatapathModeNetkit, datapathOption.DatapathModeNetkitL2:
+		l2Mode := option.Config.DatapathMode == datapathOption.DatapathModeNetkitL2
+		_, epLink, err := connector.SetupNetkitWithNames(healthName, epIfaceName, mtuConfig.GetDeviceMTU(),
+			bigTCPConfig.GetGROIPv6MaxSize(), bigTCPConfig.GetGSOIPv6MaxSize(),
+			bigTCPConfig.GetGROIPv4MaxSize(), bigTCPConfig.GetGSOIPv4MaxSize(), l2Mode,
+			info, sysctl)
+		if err != nil {
+			return nil, fmt.Errorf("Error while creating netkit: %w", err)
+		}
 		if err = netlink.LinkSetNsFd(epLink, int(ns.FD())); err != nil {
 			return nil, fmt.Errorf("failed to move device %q to health namespace: %w", epIfaceName, err)
 		}

daemon/cmd/daemon_main.go
@@ -254,7 +254,10 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	option.BindEnv(vp, option.EnableRuntimeDeviceDetection)
 	flags.MarkDeprecated(option.EnableRuntimeDeviceDetection, "Runtime device detection and datapath reconfiguration is now the default and only mode of operation")
 
-	flags.String(option.DatapathMode, defaults.DatapathMode, "Datapath mode name")
+	flags.String(option.DatapathMode, defaults.DatapathMode,
+		fmt.Sprintf("Datapath mode name (%s, %s, %s, %s)",
+			datapathOption.DatapathModeVeth, datapathOption.DatapathModeNetkit,
+			datapathOption.DatapathModeNetkitL2, datapathOption.DatapathModeLBOnly))
 	option.BindEnv(vp, option.DatapathMode)
 
 	flags.Bool(option.EnableEndpointRoutes, defaults.EnableEndpointRoutes, "Use per endpoint routes instead of routing via cilium_host")
@@ -1346,6 +1349,8 @@ func initEnv(vp *viper.Viper) {
 
 	switch option.Config.DatapathMode {
 	case datapathOption.DatapathModeVeth:
+	case datapathOption.DatapathModeNetkit:
+	case datapathOption.DatapathModeNetkitL2:
 	case datapathOption.DatapathModeLBOnly:
 		log.Info("Running in LB-only mode")
 		if option.Config.NodePortAcceleration != option.NodePortAccelerationDisabled {

pkg/datapath/connector/netkit.go
@@ -1,0 +1,163 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package connector
+
+import (
+	"fmt"
+
+	"github.com/vishvananda/netlink"
+
+	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/datapath/link"
+	"github.com/cilium/cilium/pkg/datapath/linux/sysctl"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/netns"
+)
+
+// SetupNetkitRemoteNs renames the netdevice in the target namespace to the
+// provided dstIfName.
+func SetupNetkitRemoteNs(ns *netns.NetNS, srcIfName, dstIfName string) error {
+	return ns.Do(func() error {
+		err := link.Rename(srcIfName, dstIfName)
+		if err != nil {
+			return fmt.Errorf("failed to rename netkit from %q to %q: %w", srcIfName, dstIfName, err)
+		}
+		return nil
+	})
+}
+
+// SetupNetkit sets up the net interface, the temporary interface and fills up some
+// endpoint fields such as mac, NodeMac, ifIndex and ifName. Returns a pointer for the
+// created netkit, a pointer for the temporary link, the name of the temporary link
+// and error if something fails.
+func SetupNetkit(id string, mtu, groIPv6MaxSize, gsoIPv6MaxSize, groIPv4MaxSize, gsoIPv4MaxSize int, l2Mode bool, ep *models.EndpointChangeRequest, sysctl sysctl.Sysctl) (*netlink.Netkit, netlink.Link, string, error) {
+	if id == "" {
+		return nil, nil, "", fmt.Errorf("invalid: empty ID")
+	}
+
+	lxcIfName := Endpoint2IfName(id)
+	tmpIfName := Endpoint2TempIfName(id)
+
+	netkit, link, err := SetupNetkitWithNames(lxcIfName, tmpIfName, mtu,
+		groIPv6MaxSize, gsoIPv6MaxSize, groIPv4MaxSize, gsoIPv4MaxSize, l2Mode, ep, sysctl)
+	return netkit, link, tmpIfName, err
+}
+
+// SetupNetkitWithNames sets up the net interface, the peer interface and fills up some
+// endpoint fields such as mac, NodeMac, ifIndex and ifName. Returns a pointer for the
+// created netkit, a pointer for the peer link and error if something fails.
+func SetupNetkitWithNames(lxcIfName, peerIfName string, mtu, groIPv6MaxSize, gsoIPv6MaxSize, groIPv4MaxSize, gsoIPv4MaxSize int, l2Mode bool, ep *models.EndpointChangeRequest, sysctl sysctl.Sysctl) (*netlink.Netkit, netlink.Link, error) {
+	mode := netlink.NETKIT_MODE_L3
+	if l2Mode {
+		mode = netlink.NETKIT_MODE_L2
+	}
+	netkit := &netlink.Netkit{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:   lxcIfName,
+			TxQLen: 1000,
+		},
+		Mode:       mode,
+		Policy:     netlink.NETKIT_POLICY_FORWARD,
+		PeerPolicy: netlink.NETKIT_POLICY_BLACKHOLE,
+	}
+	peerAttr := &netlink.LinkAttrs{
+		Name: peerIfName,
+	}
+	netkit.SetPeerAttrs(peerAttr)
+
+	err := netlink.LinkAdd(netkit)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to create netkit pair: %w", err)
+	}
+	defer func() {
+		if err != nil {
+			if err = netlink.LinkDel(netkit); err != nil {
+				log.WithError(err).WithField(logfields.Netkit, netkit.Name).Warn("failed to clean up netkit")
+			}
+		}
+	}()
+
+	log.WithField(logfields.NetkitPair, []string{peerIfName, lxcIfName}).Debug("Created netkit pair")
+
+	// Disable reverse path filter on the host side netkit peer to allow
+	// container addresses to be used as source address when the linux
+	// stack performs routing.
+	err = DisableRpFilter(sysctl, lxcIfName)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	peer, err := netlink.LinkByName(peerIfName)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to lookup netkit peer just created: %w", err)
+	}
+
+	if err = netlink.LinkSetMTU(peer, mtu); err != nil {
+		return nil, nil, fmt.Errorf("unable to set MTU to %q: %w", peerIfName, err)
+	}
+
+	hostNetkit, err := netlink.LinkByName(lxcIfName)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to lookup netkit just created: %w", err)
+	}
+
+	if err = netlink.LinkSetMTU(hostNetkit, mtu); err != nil {
+		return nil, nil, fmt.Errorf("unable to set MTU to %q: %w", lxcIfName, err)
+	}
+
+	if err = netlink.LinkSetUp(netkit); err != nil {
+		return nil, nil, fmt.Errorf("unable to bring up netkit pair: %w", err)
+	}
+
+	if groIPv6MaxSize > 0 {
+		if err = netlink.LinkSetGROMaxSize(hostNetkit, groIPv6MaxSize); err != nil {
+			return nil, nil, fmt.Errorf("unable to set GRO max size to %q: %w",
+				lxcIfName, err)
+		}
+		if err = netlink.LinkSetGROMaxSize(peer, groIPv6MaxSize); err != nil {
+			return nil, nil, fmt.Errorf("unable to set GRO max size to %q: %w",
+				peerIfName, err)
+		}
+	}
+
+	if gsoIPv6MaxSize > 0 {
+		if err = netlink.LinkSetGSOMaxSize(hostNetkit, gsoIPv6MaxSize); err != nil {
+			return nil, nil, fmt.Errorf("unable to set GSO max size to %q: %w",
+				lxcIfName, err)
+		}
+		if err = netlink.LinkSetGSOMaxSize(peer, gsoIPv6MaxSize); err != nil {
+			return nil, nil, fmt.Errorf("unable to set GSO max size to %q: %w",
+				peerIfName, err)
+		}
+	}
+
+	if groIPv4MaxSize > 0 {
+		if err = netlink.LinkSetGROIPv4MaxSize(hostNetkit, groIPv4MaxSize); err != nil {
+			return nil, nil, fmt.Errorf("unable to set GRO max size to %q: %w",
+				lxcIfName, err)
+		}
+		if err = netlink.LinkSetGROIPv4MaxSize(peer, groIPv4MaxSize); err != nil {
+			return nil, nil, fmt.Errorf("unable to set GRO max size to %q: %w",
+				peerIfName, err)
+		}
+	}
+
+	if gsoIPv4MaxSize > 0 {
+		if err = netlink.LinkSetGSOIPv4MaxSize(hostNetkit, gsoIPv4MaxSize); err != nil {
+			return nil, nil, fmt.Errorf("unable to set GSO max size to %q: %w",
+				lxcIfName, err)
+		}
+		if err = netlink.LinkSetGSOIPv4MaxSize(peer, gsoIPv4MaxSize); err != nil {
+			return nil, nil, fmt.Errorf("unable to set GSO max size to %q: %w",
+				peerIfName, err)
+		}
+	}
+
+	ep.Mac = peer.Attrs().HardwareAddr.String()
+	ep.HostMac = hostNetkit.Attrs().HardwareAddr.String()
+	ep.InterfaceIndex = int64(hostNetkit.Attrs().Index)
+	ep.InterfaceName = lxcIfName
+
+	return netkit, peer, nil
+}

pkg/datapath/linux/bigtcp/bigtcp.go
@@ -215,8 +215,8 @@ type params struct {
 
 func validateConfig(cfg UserConfig, daemonCfg *option.DaemonConfig) error {
 	if cfg.EnableIPv6BIGTCP || cfg.EnableIPv4BIGTCP {
-		if daemonCfg.DatapathMode != datapathOption.DatapathModeVeth {
-			return errors.New("BIG TCP is supported only in veth datapath mode")
+		if daemonCfg.DatapathMode == datapathOption.DatapathModeLBOnly {
+			return errors.New("BIG TCP is supported only in veth & netkit datapath mode")
 		}
 		if daemonCfg.TunnelingEnabled() {
 			return errors.New("BIG TCP is not supported in tunneling mode")

pkg/datapath/loader/loader.go
@@ -500,7 +500,7 @@ func (l *loader) reloadDatapath(ep datapath.Endpoint, spec *ebpf.CollectionSpec)
 
 	if ep.RequireEndpointRoute() {
 		scopedLog := ep.Logger(subsystem).WithFields(logrus.Fields{
-			logfields.Veth: device,
+			logfields.Interface: device,
 		})
 		if ip := ep.IPv4Address(); ip.IsValid() {
 			if err := upsertEndpointRoute(ep, *iputil.AddrToIPNet(ip)); err != nil {

pkg/datapath/loader/template.go
@@ -227,8 +227,11 @@ func ELFVariableSubstitutions(ep datapath.Endpoint) map[string]uint64 {
 	}
 
 	mac := ep.GetNodeMAC()
-	result["THIS_INTERFACE_MAC_1"] = uint64(sliceToBe32(mac[0:4]))
-	result["THIS_INTERFACE_MAC_2"] = uint64(sliceToBe16(mac[4:6]))
+	// For L3/NOARP devices node mac is not populated.
+	if len(mac) != 0 {
+		result["THIS_INTERFACE_MAC_1"] = uint64(sliceToBe32(mac[0:4]))
+		result["THIS_INTERFACE_MAC_2"] = uint64(sliceToBe16(mac[4:6]))
+	}
 
 	if ep.IsHost() {
 		if option.Config.EnableNodePort {

pkg/datapath/option/option.go
@@ -9,6 +9,16 @@ const (
 	// attached to a network via veth pairs).
 	DatapathModeVeth = "veth"
 
+	// DatapathModeNetkit specifies netkit datapath mode (i.e. containers
+	// are attached to a network via netkit pairs). netkit is created in
+	// L3 mode.
+	DatapathModeNetkit = "netkit"
+
+	// DatapathModeNetkitL2 specifies netkit datapath mode (i.e. containers
+	// are attached to a network via netkit pairs). netkit is created in
+	// L2 mode.
+	DatapathModeNetkitL2 = "netkit-l2"
+
 	// DatapathModeLBOnly specifies lb-only datapath mode.
 	DatapathModeLBOnly = "lb-only"
 )

commit 144edb3fa1e207b812e3008f10ce2ccab2fb3da3

datapath, netkit: Allow ARP passthrough on host when using netkit
When using the `netkit` datapath mode neither `ENABLE_ARP_PASSTHROUGH` nor
`ENABLE_ARP_RESPONDER` is set for any endpoint. When host firewall is
enabled all ARP messages are blocked to and from the netdev. Make an
exception for host endpoints to ensure connectivity is maintained
between the host and the outside world. Additionally, add test coverage
for netkit combined with host firewall.

Fixes: #34230
Fixes: 68953415f03a ("cilium, connector: Add netkit connector")

Signed-off-by: Jordan Rife <jrife@google.com>

.github/workflows/tests-e2e-upgrade.yaml
@@ -372,6 +372,20 @@ jobs:
             secondary-network: 'true'
             ingress-controller: 'true'
 
+          - name: '23'
+            # renovate: datasource=docker depName=quay.io/lvh-images/kind
+            kernel: 'bpf-20240711.013133'
+            misc: 'bpf.datapathMode=netkit,bpf.masquerade=true'
+            kube-proxy: 'none'
+            kpr: 'true'
+            ipv6: 'true'
+            tunnel: 'disabled'
+            devices: '{eth0,eth1}'
+            secondary-network: 'true'
+            ingress-controller: 'true'
+            host-fw: 'true'
+            skip-upgrade: 'true'
+
           # Example of a feature that is being introduced, and we want to test
           # it without performing an upgrade, we use skip-upgrade: 'true'
           # - name: '23'

pkg/datapath/linux/config/config.go
@@ -1096,7 +1096,7 @@ func (h *HeaderfileWriter) writeTemplateConfig(fw *bufio.Writer, devices []strin
 
 	fmt.Fprintf(fw, "#define HOST_EP_ID %d\n", uint32(hostEndpointID))
 
-	if option.Config.DatapathMode != datapathOption.DatapathModeNetkit {
+	if e.IsHost() || option.Config.DatapathMode != datapathOption.DatapathModeNetkit {
 		if e.RequireARPPassthrough() {
 			fmt.Fprint(fw, "#define ENABLE_ARP_PASSTHROUGH 1\n")
 		} else {