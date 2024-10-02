ce31b782cf04ac0f846cd4e986c170e096c5bd2e
Ovidiu Tirla <otirla@google.com>
2024-10-02 21:39:13 +0000

    pkg/dynamiclifecycle: Register dynamiclifecycle
    Adds the dynamiclifecycle.Cell to agent cells and register the table as an experimental table in CLI.
    
    Signed-off-by: Ovidiu Tirla <otirla@google.com>

--- daemon/cmd/cells.go
+++ daemon/cmd/cells.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cilium/cilium/pkg/dial"
 	"github.com/cilium/cilium/pkg/driftchecker"
 	"github.com/cilium/cilium/pkg/dynamicconfig"
+	"github.com/cilium/cilium/pkg/dynamiclifecycle"
 	"github.com/cilium/cilium/pkg/egressgateway"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointcleanup"
@@ -292,6 +293,11 @@ var (
 
 		// Provides a wrapper of the cilium config that can be watched dynamically
 		dynamicconfig.Cell,
+
+		// Provides the manager for WithDynamicFeature()
+		// Which allows to group the cell lifecycles together and control the enablement
+		// by leveraging the dynamicconfig.Cell.
+		dynamiclifecycle.Cell,
 
 		// Allows agent to monitor the configuration drift and publish drift metric
 		driftchecker.Cell,
