cilium/cilium

commit 0c0800c8b3120410dc4e391d90b2dec682552301

ci: make runtime privileged tests not run in parallel
There was a significant flakiness of IPSec-related privileged tests due
to the fact that tests in different packages were modifying xfrm
states/policies concurrently.

While increasing timeout for test and making it last longer is non-ideal,
less flaky tests outweight it.

Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>

Makefile
@@ -82,7 +82,7 @@ $(SUBDIRS): force ## Execute default make target(make all) for the provided subd
 
 tests-privileged: ## Run Go tests including ones that require elevated privileges.
 	@$(ECHO_CHECK) running privileged tests...
-	PRIVILEGED_TESTS=true PATH=$(PATH):$(ROOT_DIR)/bpf $(GO_TEST) $(TEST_LDFLAGS) \
+	PRIVILEGED_TESTS=true PATH=$(PATH):$(ROOT_DIR)/bpf $(GO_TEST) -p 1 $(TEST_LDFLAGS) \
 		$(TESTPKGS) $(GOTEST_BASE) $(GOTEST_COVER_OPTS) | $(GOTEST_FORMATTER)
 	$(MAKE) generate-cov
 

.github/workflows/conformance-runtime.yaml
@@ -232,7 +232,7 @@ jobs:
           - focus: "datapath"
             cliFocus: "RuntimeDatapathConntrackInVethModeTest|RuntimeDatapathMonitorTest"
 
-    timeout-minutes: 40
+    timeout-minutes: 50
     steps:
       - name: Collect Workflow Telemetry
         uses: catchpoint/workflow-telemetry-action@94c3c3d9567a0205de6da68a76c428ce4e769af1 # v2.0.0
@@ -380,7 +380,7 @@ jobs:
 
       - name: Runtime privileged tests
         if: ${{ matrix.focus == 'privileged' }}
-        timeout-minutes: 30
+        timeout-minutes: 40
         uses: cilium/little-vm-helper@3c748d6fc9d6c44a433de85a66f70e8f7043be04 # v0.0.18
         with:
           provision: 'false'

commit 1c3f716b5cf91e6840c66567ff30255f875ac515

tests: run ipsec tests sequentially and other tests concurrently
The commit 0c0800c8b312 removed the concurrency of test execution for
all runtime privileged tests. This was unnecessary and caused the tests to
take longer time to complete. With this commit we will run all tests
concurrently except the flaky tests that were related with the ipsec
packages.

Fixes: 0c0800c8b312 ("ci: make runtime privileged tests not run in parallel")
Signed-off-by: André Martins <andre@cilium.io>

Makefile
@@ -81,7 +81,7 @@ $(SUBDIRS): force ## Execute default make target(make all) for the provided subd
 
 tests-privileged: ## Run Go tests including ones that require elevated privileges.
 	@$(ECHO_CHECK) running privileged tests...
-	PRIVILEGED_TESTS=true PATH=$(PATH):$(ROOT_DIR)/bpf $(GO_TEST) -p 1 $(TEST_LDFLAGS) \
+	PRIVILEGED_TESTS=true PATH=$(PATH):$(ROOT_DIR)/bpf $(GO_TEST) $(TEST_LDFLAGS) \
 		$(TESTPKGS) $(GOTEST_BASE) $(GOTEST_COVER_OPTS) | $(GOTEST_FORMATTER)
 	$(MAKE) generate-cov
 

pkg/datapath/linux/ipsec/ipsec_linux_test.go
@@ -16,18 +16,28 @@ import (
 	"github.com/vishvananda/netlink"
 
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/testutils"
 )
 
+var (
+	// User a mutex to force the ipsec tests to not run in parallel.
+	// If the ipsec tests run in parallel there were packages modifying xfrm
+	// states/policies concurrently which was causing flakes.
+	mutex lock.Mutex
+)
+
 func setupIPSecSuitePrivileged(tb testing.TB) *slog.Logger {
 	testutils.PrivilegedTest(tb)
+	mutex.Lock()
 	node.SetTestLocalNodeStore()
 	err := rlimit.RemoveMemlock()
 	require.NoError(tb, err)
 	log := hivetest.Logger(tb)
 
 	tb.Cleanup(func() {
+		defer mutex.Unlock()
 		UnsetTestIPSecKey()
 		node.UnsetTestLocalNodeStore()
 		err := DeleteXFRM(log, AllReqID)