f25bf2d3a3a3b52a6516fcb397636bbcbbcf6ae1
Louis DeLosSantos <louis.delos@isovalent.com>
2024-10-07 13:31:12 -0400

    ipsec: remove direction from getNodeIPsecKey
    Now that we push the parameters for XFRM state/policy creation, declaratively,
    from a higher level structure we no longer need to flip parameters
    depending on direction within getNodeIPsecKey.
    
    Remove the dir parameter from the function and always derive node
    specific keys with the declarative parameters being passed in. They are
    in the correct order for the policy/state creation which is calling
    `getNodeIPsecKey`.
    
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -300,7 +300,7 @@ func deriveNodeIPsecKey(globalKey *ipSecKey, srcNodeIP, dstNodeIP net.IP, srcBoo
 // This is done such that, for each pair of nodes A, B, the key used for
 // decryption on A (XFRM IN) is the same key used for encryption on B (XFRM
 // OUT), and vice versa. And its ESN automatically resets on each node reboot.
-func getNodeIPsecKey(localNodeIP, remoteNodeIP net.IP, localBootID, remoteBootID string, dir netlink.Dir) *ipSecKey {
+func getNodeIPsecKey(localNodeIP, remoteNodeIP net.IP, localBootID, remoteBootID string) *ipSecKey {
 	globalKey := getGlobalIPsecKey(localNodeIP)
 	if globalKey == nil {
 		return nil
@@ -309,10 +309,7 @@ func getNodeIPsecKey(localNodeIP, remoteNodeIP net.IP, localBootID, remoteBootID
 		return globalKey
 	}
 
-	if dir == netlink.XFRM_DIR_OUT {
-		return deriveNodeIPsecKey(globalKey, localNodeIP, remoteNodeIP, localBootID, remoteBootID)
-	}
-	return deriveNodeIPsecKey(globalKey, remoteNodeIP, localNodeIP, remoteBootID, localBootID)
+	return deriveNodeIPsecKey(globalKey, localNodeIP, remoteNodeIP, localBootID, remoteBootID)
 }
 
 func ipSecNewState(keys *ipSecKey) *netlink.XfrmState {
@@ -540,7 +537,7 @@ func xfrmKeyEqual(s1, s2 *netlink.XfrmState) bool {
 }
 
 func ipSecReplaceStateIn(log *slog.Logger, params *IPSecParameters) (uint8, error) {
-	key := getNodeIPsecKey(*params.SourceTunnelIP, *params.DestTunnelIP, params.LocalBootID, params.RemoteBootID, netlink.XFRM_DIR_IN)
+	key := getNodeIPsecKey(*params.SourceTunnelIP, *params.DestTunnelIP, params.LocalBootID, params.RemoteBootID)
 	if key == nil {
 		return 0, fmt.Errorf("IPSec key missing")
 	}
@@ -573,7 +570,7 @@ func ipSecReplaceStateIn(log *slog.Logger, params *IPSecParameters) (uint8, erro
 }
 
 func ipSecReplaceStateOut(log *slog.Logger, params *IPSecParameters) (uint8, error) {
-	key := getNodeIPsecKey(*params.SourceTunnelIP, *params.DestTunnelIP, params.LocalBootID, params.RemoteBootID, netlink.XFRM_DIR_OUT)
+	key := getNodeIPsecKey(*params.SourceTunnelIP, *params.DestTunnelIP, params.LocalBootID, params.RemoteBootID)
 	if key == nil {
 		return 0, fmt.Errorf("IPSec key missing")
 	}
