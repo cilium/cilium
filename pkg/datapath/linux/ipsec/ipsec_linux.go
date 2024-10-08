33964dcc6a578302edfa3692774219de51c92df6
Louis DeLosSantos <louis.delos@isovalent.com>
2024-10-08 15:38:43 -0400

    ipsec: remove IPSecDirBoth
    Remove the use of IPSecDirBoth for creating both IN and OUT policies.
    
    This option was only used in tests and had no functional use during
    Cilium's runtime.
    
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -48,7 +48,6 @@ const (
 	IPSecDirIn      IPSecDir = "IPSEC_IN"
 	IPSecDirOut     IPSecDir = "IPSEC_OUT"
 	IPSecDirFwd     IPSecDir = "IPSEC_FWD"
-	IPSecDirBoth    IPSecDir = "IPSEC_BOTH"
 	IPSecDirOutNode IPSecDir = "IPSEC_OUT_NODE"
 
 	// Constants used to decode the IPsec secret in both formats:
@@ -951,7 +950,7 @@ func UpsertIPsecEndpoint(log *slog.Logger, params *IPSecParameters) (uint8, erro
 	 * state would need to be cached in the ipcache.
 	 */
 	if !params.SourceTunnelIP.Equal(*params.DestTunnelIP) {
-		if params.Dir == IPSecDirIn || params.Dir == IPSecDirBoth {
+		if params.Dir == IPSecDirIn {
 			if spi, err = ipSecReplaceStateIn(log, params); err != nil {
 				return 0, fmt.Errorf("unable to replace local state: %w", err)
 			}
@@ -970,7 +969,7 @@ func UpsertIPsecEndpoint(log *slog.Logger, params *IPSecParameters) (uint8, erro
 			}
 		}
 
-		if params.Dir == IPSecDirOut || params.Dir == IPSecDirOutNode || params.Dir == IPSecDirBoth {
+		if params.Dir == IPSecDirOut || params.Dir == IPSecDirOutNode {
 			if spi, err = ipSecReplaceStateOut(log, params); err != nil {
 				return 0, fmt.Errorf("unable to replace remote state: %w", err)
 			}
