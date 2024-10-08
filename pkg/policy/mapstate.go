cilium/cilium

commit 02d28d9ac9afcaddd301fae6fb4d6cda8c2d0c45

policy: Fix broad deny with except bug
With the policy as shown by [1] applied, traffic to 1.1.1.2/32 would be
denied but only because of the default-deny fallback, and not have a
policy correlation.  In Hubble, this would show up as
`policy-verdict:none` in the flow instead of "Policy denied by
denylist".

In the denyPreferredInsertWithChanges() logic for v1.15 and below (v1.16
and main unaffected), the "deny-deny" [2] logic removed what the policy
engine considered a redundant deny entry that leads to the missing
policy correlation described above.

The fix is to minimally cherry-pick 317a8af ("policy: Keep deny
entries when covered by another CIDR deny") [from the v1.16 tree]. This
cherry-picked commit is built off of a2ff24f ("policy: Fix Deny
Insertion Logic"), which was found by bisecting v1.15 and v1.16 to
identify when this bug was "fixed" in the later versions of Cilium.

In addition, this commit contains another minimal cherry-pick from
317a8af ("policy: Keep deny entries when covered by another CIDR
deny") [from the v1.16 tree] for the "deny-allow" [3] and "allow-deny"
[4] case. [3] fixes a transient bug as explained by
cilium/cilium#33953 (comment).

For more context on the changes to the policy engine between versions,
tracing the commit history from the commits mentioned above would be the
best way.

[1]:

```
apiVersion: cilium.io/v2
kind: CiliumNetworkPolicy
metadata:
  name: allow
spec:
  egress:
  - toCIDR:
    - 1.1.1.1/32
    - 1.1.1.2/32
  endpointSelector: {}
---
apiVersion: cilium.io/v2
kind: CiliumNetworkPolicy
metadata:
  name: deny
spec:
  egressDeny:
  - toCIDRSet:
    - cidr: 1.0.0.0/8
      except:
      - 1.1.1.1/32
  endpointSelector: {}
```

[2]: "Deny-deny" refers to the policy engine code path inside
     denyPreferredInsertWithChanges() under

```
if newEntry.IsDeny {
  ...
  ms.ForEachDeny()
  ...
}
```

[3]: Similar to [2], "deny-allow" refers to

```
if newEntry.IsDeny {
  ...
  ms.ForEachAllow()
  ...
}
```

pkg/policy/mapstate.go
@@ -234,6 +234,21 @@ func (e *MapStateEntry) HasDependent(key Key) bool {
 	return ok
 }
 
+// HasSameOwners returns true if both MapStateEntries
+// have the same owners as one another.
+// MapStateEntries are stored by value, so we do not check for nil pointers here.
+func (e *MapStateEntry) HasSameOwners(bEntry *MapStateEntry) bool {
+	if len(e.owners) != len(bEntry.owners) {
+		return false
+	}
+	for owner := range e.owners {
+		if _, ok := bEntry.owners[owner]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
 var worldNets = map[identity.NumericIdentity][]*net.IPNet{
 	identity.ReservedIdentityWorld: {
 		{IP: net.IPv4zero, Mask: net.CIDRMask(0, net.IPv4len*8)},
@@ -778,13 +793,41 @@ func (ms *mapState) denyPreferredInsertWithChanges(newKey Key, newEntry MapState
 					// identity is removed.
 					newEntry.AddDependent(newKeyCpy)
 				}
-			} else if (newKey.Identity == k.Identity ||
-				identityIsSupersetOf(newKey.Identity, k.Identity, identities)) &&
-				(newKey.PortProtoIsBroader(k) || newKey.PortProtoIsEqual(k)) {
-				// If the new-entry is a superset (or equal) of the iterated-allow-entry and
-				// the new-entry has a broader (or equal) port-protocol then we
-				// should delete the iterated-allow-entry
-				ms.deleteKeyWithChanges(k, nil, changes)
+			} else if newKey.PortProtoIsBroader(k) || newKey.PortProtoIsEqual(k) {
+				// If newKey has a broader (or equal) port-protocol then we should
+				// either delete the iterated-allow-entry (if the identity is the
+				// same or the newKey is L3 wildcard), or change it to a deny entry
+				// if the newKey's identity is a superset of the iterated identity
+				// (e.g., newKey has a wider CIDR (say 10/8 covering the iterated
+				// identity of more specific CIDR (say 10.1.1.1). Note that the
+				// security identities assigned to these CIDRs have no numerical
+				// relation to each other (e.g, they could be any numbers X and Y)
+				// and the datapath does an exact match on them.
+				if newKey.Identity == 0 || newKey.Identity == k.Identity {
+					ms.deleteKeyWithChanges(k, nil, changes)
+				} else if identityIsSupersetOf(newKey.Identity, k.Identity, identities) {
+					// When newKey.Identity is not ANY and is different from the
+					// subset key, but still a superset (e.g., in CIDR sense) we
+					// must keep the subset key and make it a deny instead.
+					l3l4DenyEntry := NewMapStateEntry(newKey, newEntry.DerivedFromRules, 0, "", 0, true, DefaultAuthType, AuthTypeDisabled)
+					ms.addKeyWithChanges(k, l3l4DenyEntry, changes)
+					newEntry.AddDependent(k)
+				}
+			} else if identityIsSupersetOf(newKey.Identity, k.Identity, identities) {
+				// k.PortProtoIsBroader(newKey) // due to if statements above
+
+				// Deny takes precedence for the port/proto of the newKey
+				// for each allow with broader port/proto and narrower ID.
+
+				// If newKey is a superset of the iterated allow key and newKey has
+				// a less specific port-protocol than the iterated allow key then an
+				// additional deny entry with port/proto of newKey and with the
+				// identity of the iterated allow key must be added.
+				denyKeyCpy := newKey
+				denyKeyCpy.Identity = k.Identity
+				l3l4DenyEntry := NewMapStateEntry(newKey, newEntry.DerivedFromRules, 0, "", 0, true, DefaultAuthType, AuthTypeDisabled)
+				ms.addKeyWithChanges(denyKeyCpy, l3l4DenyEntry, changes)
+				newEntry.AddDependent(denyKeyCpy)
 			}
 			return true
 		})
@@ -797,32 +840,39 @@ func (ms *mapState) denyPreferredInsertWithChanges(newKey Key, newEntry MapState
 				return true
 			}
 
-			if (newKey.Identity == k.Identity ||
-				identityIsSupersetOf(k.Identity, newKey.Identity, identities)) &&
-				k.DestPort == 0 && k.Nexthdr == 0 &&
-				!v.HasDependent(newKey) {
-				// If this iterated-deny-entry is a supserset (or equal) of the new-entry and
-				// the iterated-deny-entry is an L3-only policy then we
-				// should not insert the new entry (as long as it is not one
-				// of the special L4-only denies we created to cover the special
-				// case of a superset-allow with a more specific port-protocol).
-				//
-				// NOTE: This condition could be broader to reject more deny entries,
-				// but there *may* be performance tradeoffs.
-				bailed = true
-				return false
-			} else if (newKey.Identity == k.Identity ||
-				identityIsSupersetOf(newKey.Identity, k.Identity, identities)) &&
-				newKey.DestPort == 0 && newKey.Nexthdr == 0 &&
+			// A narrower of two deny keys is redundant in the datapath only if
+			// the broader ID is 0, or the IDs are the same. This is because the
+			// ID will be assigned from the ipcache and datapath has no notion
+			// of one ID being related to another (e.g., in a CIDR sense).
+			if (k.Identity == 0 || k.Identity == newKey.Identity) &&
+				(k.PortProtoIsEqual(newKey) || k.PortProtoIsBroader(newKey)) {
+				// If this iterated-deny-entry is an deny-all-L3 or has the same ID
+				// as the new-entry and the iterated-deny-entry has a broader (or
+				// equal) port-protocol it will match all the packets the newKey
+				// would, given that we do not allow more specific allow rules to be
+				// inserted.
+
+				// Identical key needs to be added if owners are different (to merge
+				// them). This has no effect on the datapath policy map but is
+				// needed for internal bookkeeping.
+				if k != newKey || v.HasSameOwners(&newEntry) {
+					bailed = true
+					return false
+				}
+			} else if (newKey.Identity == 0 || newKey.Identity == k.Identity) &&
+				(newKey.PortProtoIsEqual(k) || newKey.PortProtoIsBroader(k)) &&
 				!newEntry.HasDependent(k) {
-				// If this iterated-deny-entry is a subset (or equal) of the new-entry and
-				// the new-entry is an L3-only policy then we
-				// should delete the iterated-deny-entry (as long as it is not one
-				// of the special L4-only denies we created to cover the special
-				// case of a superset-allow with a more specific port-protocol).
-				//
-				// NOTE: This condition could be broader to reject more deny entries,
-				// but there *may* be performance tradeoffs.
+				// If this iterated-deny-entry is a subset (or equal) of the
+				// new-entry and the new-entry has a broader (or equal)
+				// port-protocol the newKey will match all the packets the iterated
+				// key would, given that there are no more specific or L4-only allow
+				// entries. We removed the more specific allow rules in the loop
+				// above, and added more specific deny rules if there was an L4-only
+				// allow rule. We use 'HasDependant' to figure out that 'k' must
+				// remain to take precedence over the L4-only allow key.
+
+				// Identical key would have been captured in the block above, so we
+				// do not need to check for it here.
 				ms.deleteKeyWithChanges(k, nil, changes)
 			}
 			return true
@@ -834,6 +884,7 @@ func (ms *mapState) denyPreferredInsertWithChanges(newKey Key, newEntry MapState
 	} else {
 		// NOTE: We do not delete redundant allow entries.
 		bailed := false
+		changeToDeny := false
 		ms.ForEachDeny(func(k Key, v MapStateEntry) bool {
 			// Protocols and traffic directions that don't match ensure that the policies
 			// do not interact in anyway.
@@ -857,20 +908,51 @@ func (ms *mapState) denyPreferredInsertWithChanges(newKey Key, newEntry MapState
 					// identity is removed.
 					ms.addDependentOnEntry(k, v, denyKeyCpy, changes)
 				}
-			} else if (k.Identity == newKey.Identity ||
-				identityIsSupersetOf(k.Identity, newKey.Identity, identities)) &&
-				(k.PortProtoIsBroader(newKey) || k.PortProtoIsEqual(newKey)) &&
-				!v.HasDependent(newKey) {
-				// If the iterated-deny-entry is a superset (or equal) of the new-entry and has a
-				// broader (or equal) port-protocol than the new-entry then the new
-				// entry should not be inserted.
-				bailed = true
-				return false
+			} else if k.PortProtoIsBroader(newKey) || k.PortProtoIsEqual(newKey) {
+				if k.Identity == 0 || k.Identity == newKey.Identity {
+					// If the iterated-deny-entry is a datapath superset (or
+					// equal) of the new-entry and has a broader (or equal)
+					// port-protocol than the new-entry then the new entry
+					// should not be inserted.
+					bailed = true
+					return false
+				} else if identityIsSupersetOf(k.Identity, newKey.Identity, identities) {
+					// if newKey is not bailed due to being covered in the
+					// datapath by a deny entry, but is covered by a deny entry
+					// in the CIDR sense, we must change this allow entry to a
+					// deny entry so that the covering deny policy is honored
+					// also for this ID in the datapath.
+					changeToDeny = true
+				}
+			} else { // newKey.PortProtoIsBroader(k)
+				if identityIsSupersetOf(k.Identity, newKey.Identity, identities) {
+					// If the new-entry is a subset of the iterated-deny-entry
+					// and the new-entry has a less specific port-protocol than
+					// the iterated-deny-entry then an additional copy of the
+					// iterated-deny-entry with the identity of the new-entry
+					// must be added.
+					denyKeyCpy := k
+					denyKeyCpy.Identity = newKey.Identity
+					l3l4DenyEntry := NewMapStateEntry(k, v.DerivedFromRules, 0, "", 0, true, DefaultAuthType, AuthTypeDisabled)
+					ms.addKeyWithChanges(denyKeyCpy, l3l4DenyEntry, changes)
+					// L3-only entries can be deleted incrementally so we need
+					// to track their effects on other entries so that those
+					// effects can be reverted when the identity is removed.
+					ms.addDependentOnEntry(k, v, denyKeyCpy, changes)
+				}
 			}
 
 			return true
 		})
 
+		if changeToDeny {
+			newEntry.IsDeny = true
+			newEntry.ProxyPort = 0
+			newEntry.Listener = ""
+			newEntry.priority = 0
+			newEntry.hasAuthType = DefaultAuthType
+			newEntry.AuthType = AuthTypeDisabled
+		}
 		if !bailed {
 			ms.authPreferredInsert(newKey, newEntry, features, changes)
 		}

pkg/policy/mapstate_test.go
@@ -2265,10 +2265,22 @@ func (ds *PolicyTestSuite) TestMapState_AccumulateMapChangesOnVisibilityKeys(c *
 	}
 }
 
+func (e MapStateEntry) asDeny() MapStateEntry {
+	if !e.IsDeny {
+		e.IsDeny = true
+		e.ProxyPort = 0
+		e.Listener = ""
+		e.priority = 0
+		e.hasAuthType = DefaultAuthType
+		e.AuthType = AuthTypeDisabled
+	}
+	return e
+}
+
 func (ds *PolicyTestSuite) TestMapState_denyPreferredInsertWithSubnets(c *check.C) {
 	identityCache := cache.IdentityCache{
 		identity.ReservedIdentityWorld: labels.LabelWorld.LabelArray(),
-		worldIPIdentity:                lblWorldIP,                  // "192.0.2.3/32"
+		worldIPIdentity:                lblWorldIP.LabelArray(),     // "192.0.2.3/32"
 		worldSubnetIdentity:            lblWorldSubnet.LabelArray(), // "192.0.2.0/24"
 	}
 
@@ -2284,9 +2296,10 @@ func (ds *PolicyTestSuite) TestMapState_denyPreferredInsertWithSubnets(c *check.
 		insertAWithBProto
 		insertBWithAProto
 
-		insertBoth            = insertA | insertB
-		canDeleteAInsertsBoth = insertBoth
-		canDeleteBInsertsBoth = insertBoth
+		insertBWithAProtoAsDeny
+		insertAasDeny
+		insertBasDeny
+		insertBoth = insertA | insertB
 	)
 	// these tests are based on the sheet https://docs.google.com/spreadsheets/d/1WANIoZGB48nryylQjjOw6lKjI80eVgPShrdMTMalLEw#gid=2109052536
 	tests := []struct {
@@ -2300,46 +2313,48 @@ func (ds *PolicyTestSuite) TestMapState_denyPreferredInsertWithSubnets(c *check.
 		outcome              action
 	}{
 		// deny-allow insertions
-		{"deny-allow: a superset a|b L3-only", reservedWorldID, worldSubnetID, true, false, 0, 0, 0, 0, insertA},
+		{"deny-allow: a superset a|b L3-only", reservedWorldID, worldSubnetID, true, false, 0, 0, 0, 0, insertA | insertBasDeny},
 		{"deny-allow: b superset a|b L3-only", worldIPID, worldSubnetID, true, false, 0, 0, 0, 0, insertBoth},
-		{"deny-allow: a superset a L3-only, b L4", reservedWorldID, worldSubnetID, true, false, 0, 0, 0, 6, insertA},
+		{"deny-allow: a superset a L3-only, b L4", reservedWorldID, worldSubnetID, true, false, 0, 0, 0, 6, insertA | insertBasDeny},
 		{"deny-allow: b superset a L3-only, b L4", worldIPID, worldSubnetID, true, false, 0, 0, 0, 6, insertBoth | insertAWithBProto},
-		{"deny-allow: a superset a L3-only, b L3L4", reservedWorldID, worldSubnetID, true, false, 0, 0, 80, 6, insertA},
+		{"deny-allow: a superset a L3-only, b L3L4", reservedWorldID, worldSubnetID, true, false, 0, 0, 80, 6, insertA | insertBasDeny},
 		{"deny-allow: b superset a L3-only, b L3L4", worldIPID, worldSubnetID, true, false, 0, 0, 80, 6, insertBoth | insertAWithBProto},
-		{"deny-allow: a superset a L4, b L3-only", reservedWorldID, worldSubnetID, true, false, 0, 6, 0, 0, insertBoth},
+		{"deny-allow: a superset a L4, b L3-only", reservedWorldID, worldSubnetID, true, false, 0, 6, 0, 0, insertBoth | insertBWithAProtoAsDeny},
 		{"deny-allow: b superset a L4, b L3-only", worldIPID, worldSubnetID, true, false, 0, 6, 0, 0, insertBoth},
-		{"deny-allow: a superset a L4, b L4", reservedWorldID, worldSubnetID, true, false, 0, 6, 0, 6, insertA},
+		{"deny-allow: a superset a L4, b L4", reservedWorldID, worldSubnetID, true, false, 0, 6, 0, 6, insertA | insertBasDeny},
 		{"deny-allow: b superset a L4, b L4", worldIPID, worldSubnetID, true, false, 0, 6, 0, 6, insertBoth},
-		{"deny-allow: a superset a L4, b L3L4", reservedWorldID, worldSubnetID, true, false, 0, 6, 80, 6, insertA},
+		{"deny-allow: a superset a L4, b L3L4", reservedWorldID, worldSubnetID, true, false, 0, 6, 80, 6, insertA | insertBasDeny},
 		{"deny-allow: b superset a L4, b L3L4", worldIPID, worldSubnetID, true, false, 0, 6, 80, 6, insertBoth | insertAWithBProto},
-		{"deny-allow: a superset a L3L4, b L3-only", reservedWorldID, worldSubnetID, true, false, 80, 6, 0, 0, insertBoth},
+		{"deny-allow: a superset a L3L4, b L3-only", reservedWorldID, worldSubnetID, true, false, 80, 6, 0, 0, insertBoth | insertBWithAProtoAsDeny},
 		{"deny-allow: b superset a L3L4, b L3-only", worldIPID, worldSubnetID, true, false, 80, 6, 0, 0, insertBoth},
-		{"deny-allow: a superset a L3L4, b L4", reservedWorldID, worldSubnetID, true, false, 80, 6, 0, 6, insertBoth},
+		{"deny-allow: a superset a L3L4, b L4", reservedWorldID, worldSubnetID, true, false, 80, 6, 0, 6, insertBoth | insertBWithAProtoAsDeny},
 		{"deny-allow: b superset a L3L4, b L4", worldIPID, worldSubnetID, true, false, 80, 6, 0, 6, insertBoth},
-		{"deny-allow: a superset a L3L4, b L3L4", reservedWorldID, worldSubnetID, true, false, 80, 6, 80, 6, insertA},
+		{"deny-allow: a superset a L3L4, b L3L4", reservedWorldID, worldSubnetID, true, false, 80, 6, 80, 6, insertA | insertBasDeny},
 		{"deny-allow: b superset a L3L4, b L3L4", worldIPID, worldSubnetID, true, false, 80, 6, 80, 6, insertBoth},
 
-		// deny-deny insertions: Note: We do not delete all redundant deny-deny insertions that we could.
-		// We only delete entries redundant to L3-only port protocols, all other port-protocol supersets
-		// *do not* have this effect.
-		{"deny-deny: a superset a|b L3-only", worldSubnetID, worldIPID, true, true, 0, 0, 0, 0, insertA},
-		{"deny-deny: b superset a|b L3-only", worldSubnetID, reservedWorldID, true, true, 0, 0, 0, 0, insertB},
-		{"deny-deny: a superset a L3-only, b L4", worldSubnetID, worldIPID, true, true, 0, 0, 0, 6, insertA},
+		// deny-deny insertions: Note: There is no dedundancy between different non-zero security IDs on the
+		// datapath, even if one would be a CIDR subset of another. Situation would be different if we could
+		// completely remove (or not add in the first place) the redundant ID from the ipcache so that
+		// datapath could never assign that ID to a packet for policy enforcement.
+		// These test case are left here for such future improvement.
+		{"deny-deny: a superset a|b L3-only", worldSubnetID, worldIPID, true, true, 0, 0, 0, 0, insertBoth},
+		{"deny-deny: b superset a|b L3-only", worldSubnetID, reservedWorldID, true, true, 0, 0, 0, 0, insertBoth},
+		{"deny-deny: a superset a L3-only, b L4", worldSubnetID, worldIPID, true, true, 0, 0, 0, 6, insertBoth},
 		{"deny-deny: b superset a L3-only, b L4", worldSubnetID, reservedWorldID, true, true, 0, 0, 0, 6, insertBoth},
-		{"deny-deny: a superset a L3-only, b L3L4", worldSubnetID, worldIPID, true, true, 0, 0, 80, 6, insertA},
+		{"deny-deny: a superset a L3-only, b L3L4", worldSubnetID, worldIPID, true, true, 0, 0, 80, 6, insertBoth},
 		{"deny-deny: b superset a L3-only, b L3L4", worldSubnetID, reservedWorldID, true, true, 0, 0, 80, 6, insertBoth},
 		{"deny-deny: a superset a L4, b L3-only", worldSubnetID, worldIPID, true, true, 0, 6, 0, 0, insertBoth},
-		{"deny-deny: b superset a L4, b L3-only", worldSubnetID, reservedWorldID, true, true, 0, 6, 0, 0, insertB},
-		{"deny-deny: a superset a L4, b L4", worldSubnetID, worldIPID, true, true, 0, 6, 0, 6, canDeleteBInsertsBoth},
-		{"deny-deny: b superset a L4, b L4", worldSubnetID, reservedWorldID, true, true, 0, 6, 0, 6, canDeleteAInsertsBoth},
-		{"deny-deny: a superset a L4, b L3L4", worldSubnetID, worldIPID, true, true, 0, 6, 80, 6, canDeleteBInsertsBoth},
+		{"deny-deny: b superset a L4, b L3-only", worldSubnetID, reservedWorldID, true, true, 0, 6, 0, 0, insertBoth},
+		{"deny-deny: a superset a L4, b L4", worldSubnetID, worldIPID, true, true, 0, 6, 0, 6, insertBoth},
+		{"deny-deny: b superset a L4, b L4", worldSubnetID, reservedWorldID, true, true, 0, 6, 0, 6, insertBoth},
+		{"deny-deny: a superset a L4, b L3L4", worldSubnetID, worldIPID, true, true, 0, 6, 80, 6, insertBoth},
 		{"deny-deny: b superset a L4, b L3L4", worldSubnetID, reservedWorldID, true, true, 0, 6, 80, 6, insertBoth},
 		{"deny-deny: a superset a L3L4, b L3-only", worldSubnetID, worldIPID, true, true, 80, 6, 0, 0, insertBoth},
-		{"deny-deny: b superset a L3L4, b L3-only", worldSubnetID, reservedWorldID, true, true, 80, 6, 0, 0, insertB},
+		{"deny-deny: b superset a L3L4, b L3-only", worldSubnetID, reservedWorldID, true, true, 80, 6, 0, 0, insertBoth},
 		{"deny-deny: a superset a L3L4, b L4", worldSubnetID, worldIPID, true, true, 80, 6, 0, 6, insertBoth},
-		{"deny-deny: b superset a L3L4, b L4", worldSubnetID, reservedWorldID, true, true, 80, 6, 0, 6, canDeleteAInsertsBoth},
-		{"deny-deny: a superset a L3L4, b L3L4", worldSubnetID, worldIPID, true, true, 80, 6, 80, 6, canDeleteBInsertsBoth},
-		{"deny-deny: b superset a L3L4, b L3L4", worldSubnetID, reservedWorldID, true, true, 80, 6, 80, 6, canDeleteAInsertsBoth},
+		{"deny-deny: b superset a L3L4, b L4", worldSubnetID, reservedWorldID, true, true, 80, 6, 0, 6, insertBoth},
+		{"deny-deny: a superset a L3L4, b L3L4", worldSubnetID, worldIPID, true, true, 80, 6, 80, 6, insertBoth},
+		{"deny-deny: b superset a L3L4, b L3L4", worldSubnetID, reservedWorldID, true, true, 80, 6, 80, 6, insertBoth},
 		// allow-allow insertions do not need to be tests as they will all be inserted
 	}
 	for _, tt := range tests {
@@ -2355,6 +2370,9 @@ func (ds *PolicyTestSuite) TestMapState_denyPreferredInsertWithSubnets(c *check.
 				expectedKeys.allows[aKey] = aEntry
 			}
 		}
+		if tt.outcome&insertAasDeny > 0 {
+			expectedKeys.denies[aKey] = aEntry.asDeny()
+		}
 		if tt.outcome&insertB > 0 {
 			if tt.bIsDeny {
 				expectedKeys.denies[bKey] = bEntry
@@ -2362,31 +2380,39 @@ func (ds *PolicyTestSuite) TestMapState_denyPreferredInsertWithSubnets(c *check.
 				expectedKeys.allows[bKey] = bEntry
 			}
 		}
+		if tt.outcome&insertBasDeny > 0 {
+			expectedKeys.denies[bKey] = bEntry.asDeny()
+		}
 		if tt.outcome&insertAWithBProto > 0 {
 			aKeyWithBProto := Key{Identity: tt.aIdentity, DestPort: tt.bPort, Nexthdr: tt.bProto}
-			aEntryCpy := MapStateEntry{IsDeny: tt.aIsDeny}
-			aEntryCpy.owners = map[MapStateOwner]struct{}{aKey: {}}
-			aEntry.AddDependent(aKeyWithBProto)
+			aEntryCpy := aEntry.WithOwners(aKey)
+			aEntryWithDep := aEntry.WithDependents(aKeyWithBProto)
 			if tt.aIsDeny {
-				expectedKeys.denies[aKey] = aEntry
+				expectedKeys.denies[aKey] = aEntryWithDep
 				expectedKeys.denies[aKeyWithBProto] = aEntryCpy
 			} else {
-				expectedKeys.allows[aKey] = aEntry
+				expectedKeys.allows[aKey] = aEntryWithDep
 				expectedKeys.allows[aKeyWithBProto] = aEntryCpy
 			}
 		}
 		if tt.outcome&insertBWithAProto > 0 {
-			bKeyWithBProto := Key{Identity: tt.bIdentity, DestPort: tt.aPort, Nexthdr: tt.aProto}
-			bEntryCpy := MapStateEntry{IsDeny: tt.bIsDeny}
-			bEntryCpy.owners = map[MapStateOwner]struct{}{bKey: {}}
-			bEntry.AddDependent(bKeyWithBProto)
+			bKeyWithAProto := Key{tt.bIdentity, tt.aPort, tt.aProto, 0}
+			bEntryCpy := bEntry.WithOwners(bKey)
+			bEntryWithDep := bEntry.WithDependents(bKeyWithAProto)
 			if tt.bIsDeny {
-				expectedKeys.denies[bKey] = bEntry
-				expectedKeys.denies[bKeyWithBProto] = bEntryCpy
+				expectedKeys.denies[bKey] = bEntryWithDep
+				expectedKeys.denies[bKeyWithAProto] = bEntryCpy
 			} else {
-				expectedKeys.allows[bKey] = bEntry
-				expectedKeys.allows[bKeyWithBProto] = bEntryCpy
+				expectedKeys.allows[bKey] = bEntryWithDep
+				expectedKeys.allows[bKeyWithAProto] = bEntryCpy
 			}
+		}
+		if tt.outcome&insertBWithAProtoAsDeny > 0 {
+			bKeyWithAProto := Key{tt.bIdentity, tt.aPort, tt.aProto, 0}
+			bEntryAsDeny := bEntry.WithOwners(aKey).asDeny()
+			aEntryWithDep := aEntry.WithDependents(bKeyWithAProto)
+			expectedKeys.denies[aKey] = aEntryWithDep
+			expectedKeys.denies[bKeyWithAProto] = bEntryAsDeny
 		}
 		outcomeKeys := newMapState(nil)
 		outcomeKeys.denyPreferredInsert(aKey, aEntry, selectorCache, allFeatures)

commit 48c02aa2faa238338eea12b1ebd22f1e34c62fbd

policy: Fix breakages on v1.15 branch
Following the merge of 02d28d9ac9, the v1.15 branch was broken due to
the wrong function signatures and undefined struct field being used.
It's unclear why the CI did not catch this.

Fixes: 02d28d9ac9 ("policy: Fix broad deny with except bug")
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/policy/mapstate.go
@@ -809,7 +809,7 @@ func (ms *mapState) denyPreferredInsertWithChanges(newKey Key, newEntry MapState
 					// When newKey.Identity is not ANY and is different from the
 					// subset key, but still a superset (e.g., in CIDR sense) we
 					// must keep the subset key and make it a deny instead.
-					l3l4DenyEntry := NewMapStateEntry(newKey, newEntry.DerivedFromRules, 0, "", 0, true, DefaultAuthType, AuthTypeDisabled)
+					l3l4DenyEntry := NewMapStateEntry(newKey, newEntry.DerivedFromRules, false, true, DefaultAuthType, AuthTypeDisabled)
 					ms.addKeyWithChanges(k, l3l4DenyEntry, changes)
 					newEntry.AddDependent(k)
 				}
@@ -825,7 +825,7 @@ func (ms *mapState) denyPreferredInsertWithChanges(newKey Key, newEntry MapState
 				// identity of the iterated allow key must be added.
 				denyKeyCpy := newKey
 				denyKeyCpy.Identity = k.Identity
-				l3l4DenyEntry := NewMapStateEntry(newKey, newEntry.DerivedFromRules, 0, "", 0, true, DefaultAuthType, AuthTypeDisabled)
+				l3l4DenyEntry := NewMapStateEntry(newKey, newEntry.DerivedFromRules, false, true, DefaultAuthType, AuthTypeDisabled)
 				ms.addKeyWithChanges(denyKeyCpy, l3l4DenyEntry, changes)
 				newEntry.AddDependent(denyKeyCpy)
 			}
@@ -933,7 +933,7 @@ func (ms *mapState) denyPreferredInsertWithChanges(newKey Key, newEntry MapState
 					// must be added.
 					denyKeyCpy := k
 					denyKeyCpy.Identity = newKey.Identity
-					l3l4DenyEntry := NewMapStateEntry(k, v.DerivedFromRules, 0, "", 0, true, DefaultAuthType, AuthTypeDisabled)
+					l3l4DenyEntry := NewMapStateEntry(k, v.DerivedFromRules, false, true, DefaultAuthType, AuthTypeDisabled)
 					ms.addKeyWithChanges(denyKeyCpy, l3l4DenyEntry, changes)
 					// L3-only entries can be deleted incrementally so we need
 					// to track their effects on other entries so that those
@@ -948,8 +948,6 @@ func (ms *mapState) denyPreferredInsertWithChanges(newKey Key, newEntry MapState
 		if changeToDeny {
 			newEntry.IsDeny = true
 			newEntry.ProxyPort = 0
-			newEntry.Listener = ""
-			newEntry.priority = 0
 			newEntry.hasAuthType = DefaultAuthType
 			newEntry.AuthType = AuthTypeDisabled
 		}

pkg/policy/mapstate_test.go
@@ -2269,8 +2269,6 @@ func (e MapStateEntry) asDeny() MapStateEntry {
 	if !e.IsDeny {
 		e.IsDeny = true
 		e.ProxyPort = 0
-		e.Listener = ""
-		e.priority = 0
 		e.hasAuthType = DefaultAuthType
 		e.AuthType = AuthTypeDisabled
 	}