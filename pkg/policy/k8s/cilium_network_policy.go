33d4fe97cbec6ff6273b0cbefa834926ab4cbf84
Casey Callendrello <cdc@isovalent.com>
2024-10-08 15:35:28 +0200

    policy: skip resolving ToServices if no ToServices present
    We already track whether or not a given CNP / CCNP has a ToServices
    selector. We can use this bit to skip resolving ToServices entirely
    (and thus iterating over all known services) when no ToServices
    selectors are present.
    
    Signed-off-by: Casey Callendrello <cdc@isovalent.com>

--- pkg/policy/k8s/cilium_network_policy.go
+++ pkg/policy/k8s/cilium_network_policy.go
@@ -77,7 +77,14 @@ func (p *policyWatcher) onUpsert(
 	if hasToServices(cnp) {
 		p.toServicesPolicies[key] = struct{}{}
 	} else {
-		delete(p.toServicesPolicies, key)
+		if _, hadToServices := p.toServicesPolicies[key]; hadToServices {
+			// transitioning from with toServices to without toServices
+			delete(p.toServicesPolicies, key)
+			// Clear ToServices index
+			for svcID := range p.cnpByServiceID {
+				p.clearCNPForService(key, svcID)
+			}
+		}
 	}
 
 	return p.resolveCiliumNetworkPolicyRefs(cnp, key, initialRecvTime, resourceID)
@@ -134,7 +141,9 @@ func (p *policyWatcher) resolveCiliumNetworkPolicyRefs(
 	translatedCNP := cnp.DeepCopy()
 
 	// Resolve ToService references
-	p.resolveToServices(key, translatedCNP)
+	if _, exists := p.toServicesPolicies[key]; exists {
+		p.resolveToServices(key, translatedCNP)
+	}
 
 	err := p.upsertCiliumNetworkPolicyV2(translatedCNP, initialRecvTime, resourceID)
 	if err == nil {
