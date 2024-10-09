0def8524788e83787ed7d38d71eeaadf9c93091b
Hemanth Malla <hemanth.malla@datadoghq.com>
2024-10-09 18:45:14 -0400

    WIP: Add support for FIPS compliance
    Also restricts all TLS config to FIPS-approved settings.
    
    Note : We (Author of this commit, Datadog Inc. or Cilium Maintainers) are
    not making any statements or representations about the suitability of this
    code in relation to the FIPS 140 standard. Interested users will have
    to evaluate for themselves whether the code is useful for their own
    purposes.
    
    Fixes: #23919
    
    Signed-off-by: Hemanth Malla <hemanth.malla@datadoghq.com>

---
+++ cilium-health/fips.go
@@ -0,0 +1,7 @@
+//go:build boringcrypto
+
+package main
+
+// Package fipsonly restricts all TLS configuration to FIPS-approved settings.
+// See https://github.com/golang/go/blob/master/src/crypto/tls/fipsonly/fipsonly.go
+import _ "crypto/tls/fipsonly"
