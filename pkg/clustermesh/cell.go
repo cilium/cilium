12ad2279006ce5384ba7504fd76ca7981b649e67
Marco Iorio <marco.iorio@isovalent.com>
2024-10-10 16:04:07 +0000

    clustermesh: guardrails for broken ENI/aws-chaining + cluster ID combo
    [ upstream commit 1ede9d906524893135084cbf71fa54ad6197d446 ]
    
    Cilium is currently affected by a bug that causes traffic matched by network
    policies to be incorrectly dropped when running in either ENI mode (both AWS
    and AlibabaCloud) or AWS VPC CNI chaining mode, if the cluster ID is 128-255
    (and 384-511 when maxConnectedClusters=511) [1]. Let's add validation both
    at the helm level and in the agent itself to prevent users from configuring
    Cilium in a configuration which is known to be broken.
    
    [1]: cilium/cilium#21330
    
    Suggested-by: Joe Stringer <joe@cilium.io>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- pkg/clustermesh/cell.go
+++ pkg/clustermesh/cell.go
@@ -6,7 +6,9 @@ package clustermesh
 import (
 	"github.com/cilium/hive/cell"
 
+	"github.com/cilium/cilium/daemon/cmd/cni"
 	"github.com/cilium/cilium/pkg/clustermesh/common"
+	"github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/clustermesh/wait"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/k8s"
@@ -14,6 +16,7 @@ import (
 	"github.com/cilium/cilium/pkg/metrics"
 	nodemanager "github.com/cilium/cilium/pkg/node/manager"
 	nodeStore "github.com/cilium/cilium/pkg/node/store"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 var Cell = cell.Module(
@@ -36,6 +39,9 @@ var Cell = cell.Module(
 	metrics.Metric(NewMetrics),
 	metrics.Metric(common.MetricsProvider(subsystem)),
 
+	cell.Invoke(func(info types.ClusterInfo, dcfg *option.DaemonConfig, cnimgr cni.CNIConfigManager) error {
+		return info.ValidateBuggyClusterID(dcfg.IPAM, cnimgr.GetChainingMode())
+	}),
 	cell.Invoke(ipsetNotifier),
 	cell.Invoke(nodeManagerNotifier),
 )
