3f863471175e4ce9e078e6d71497f11866f0e345
Marco Iorio <marco.iorio@isovalent.com>
2024-10-15 11:57:05 +0200

    logging: consistent error attribute when emitted through logr
    Extend the log attributes mangling logic to ensure consistency in the
    attribute key used to identify an error. Indeed, the logr.Error method
    takes an error as first parameter, and the slog adapter converts it into
    an attribute identified as `err`. Let's replace that into `error` to
    ensure consistency with all the other occurrences.
    
    Related: d91e387bdbb6 ("cec: Switch to slog for CEC")
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/logging/slog.go
+++ pkg/logging/slog.go
@@ -11,7 +11,12 @@ import (
 	"time"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/cilium/cilium/pkg/logging/logfields"
 )
+
+// logrErrorKey is the key used by the logr library for the error parameter.
+const logrErrorKey = "err"
 
 // SlogNopHandler discards all logs.
 var SlogNopHandler slog.Handler = nopHandler{}
@@ -94,6 +99,12 @@ func replaceAttrFn(groups []string, a slog.Attr) slog.Attr {
 		return slog.Attr{
 			Key:   a.Key,
 			Value: slog.StringValue(strings.ToLower(a.Value.String())),
+		}
+	case logrErrorKey:
+		// Uniform the attribute identifying the error
+		return slog.Attr{
+			Key:   logfields.Error,
+			Value: a.Value,
 		}
 	}
 	return a
