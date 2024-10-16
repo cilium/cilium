86843d65a8fa879c37725dcdfc14c78b705e18fe
Tam Mach <tam.mach@cilium.io>
2024-10-16 12:56:27 +1100

    k8s: Unify k8s generation logic
    This commit is to move deepequal-gen step to the existing k8s-code-gen
    for easier maintenance. One point worth noting is the new parser used
    in deepequal-gen now requires no compilation error for input package, so
    this step should be done after client generation to avoid the below
    issue.
    
    ```
    Error: pkg/k8s/slim/k8s/apiextensions-clientset/clientset.go:18:2: cannot find module providing package github.com/cilium/cilium/pkg/k8s/slim/k8s/apiextensions-client/clientset/versioned/typed/apiextensions/v1: import lookup disabled by -mod=vendor
    	(Go version in go.mod is at least 1.14 and vendor directory exists.)
    error(s) in "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/fake":
    ...
    ```
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- contrib/scripts/k8s-code-gen.sh
+++ contrib/scripts/k8s-code-gen.sh
@@ -14,6 +14,78 @@ source "${CODEGEN_PKG}/kube_codegen.sh"
 TMPDIR=${1}
 PLURAL_EXCEPTIONS="Endpoints:Endpoints,ResourceClaimParameters:ResourceClaimParameters,ResourceClassParameters:ResourceClassParameters"
 
+# This is adapted from the kube::codegen::gen_helper function in kube_codegen.sh
+# The main reason is to accommodate custom deepequal-gen generator
+# from https://github.com/cilium/deepequal-gen
+function kube::codegen::deepequal_helpers() {
+    local in_dir=""
+    local boilerplate="${KUBE_CODEGEN_ROOT}/hack/boilerplate.go.txt"
+    local v="${KUBE_VERBOSE:-0}"
+    local output_base=""
+
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+            "--boilerplate")
+                boilerplate="$2"
+                shift 2
+                ;;
+            "--output-base")
+                output_base="$2"
+                shift 2
+                ;;
+            *)
+                if [[ "$1" =~ ^-- ]]; then
+                    echo "unknown argument: $1" >&2
+                    return 1
+                fi
+                if [ -n "$in_dir" ]; then
+                    echo "too many arguments: $1 (already have $in_dir)" >&2
+                    return 1
+                fi
+                in_dir="$1"
+                shift
+                ;;
+        esac
+    done
+
+    if [ -z "${in_dir}" ]; then
+        echo "input-dir argument is required" >&2
+        return 1
+    fi
+
+    local input_pkgs=()
+    while read -r dir; do
+        pkg="$(cd "${dir}" && GO111MODULE=on go list -find .)"
+        input_pkgs+=("${pkg}")
+    done < <(
+        ( kube::codegen::internal::grep -l --null \
+            -e '^\s*//\s*+deepequal-gen=' \
+            -r "${in_dir}" \
+            --include '*.go' \
+            || true \
+        ) | while read -r -d $'\0' F; do dirname "${F}"; done \
+          | LC_ALL=C sort -u
+    )
+
+    if [ "${#input_pkgs[@]}" != 0 ]; then
+        echo "Generating deepequal code for ${#input_pkgs[@]} targets"
+
+        kube::codegen::internal::findz \
+            "${in_dir}" \
+            -path ./vendor -prune \
+            -type f \
+            -name zz_generated.deepequal.go \
+            | xargs -0 rm -f
+
+        go run github.com/cilium/deepequal-gen \
+            -v "${v}" \
+            --output-file zz_generated.deepequal.go \
+            --go-header-file "${boilerplate}" \
+            --output-base "${output_base}" \
+            "${input_pkgs[@]}"
+    fi
+}
+
 kube::codegen::gen_client \
     "./pkg/k8s/slim/k8s/api" \
     --with-watch \
@@ -47,3 +119,10 @@ kube::codegen::gen_helpers \
 kube::codegen::gen_helpers \
     --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt" \
     "$PWD/pkg"
+
+kube::codegen::deepequal_helpers \
+    --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt" \
+    --output-base "${TMPDIR}" \
+    "$PWD"
+
+cp -r "${TMPDIR}/github.com/cilium/cilium/." ./
