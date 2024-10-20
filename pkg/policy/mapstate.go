b17b54d684aa126c72336975a68e4589b6b894ff
Jarno Rajahalme <jarno@isovalent.com>
2024-10-20 22:30:31 +0200

    policy: Update existing keys without checking for structural changes
    When an existing entry is updated without changing its IsDeny flag the
    update can be done on the 'entries' map without delete from the other
    map, or without sanity checking the key.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- pkg/policy/mapstate.go
+++ pkg/policy/mapstate.go
@@ -698,12 +698,13 @@ func (ms *mapState) AddDependent(owner Key, dependent Key, changes ChangeState) 
 	}
 }
 
-// addDependentOnEntry adds 'dependent' to the set of dependent keys of 'e'.
+// addDependentOnEntry adds 'dependent' to the set of dependent keys of 'e', where 'e' already
+// exists in 'ms'.
 func (ms *mapState) addDependentOnEntry(owner Key, e mapStateEntry, dependent Key, changes ChangeState) {
 	if _, exists := e.dependents[dependent]; !exists {
 		changes.insertOldIfNotExists(owner, e)
 		e.AddDependent(dependent)
-		ms.insert(owner, e)
+		ms.updateExisting(owner, e)
 	}
 }
 
@@ -870,7 +871,7 @@ func (ms *mapState) addKeyWithChanges(key Key, entry mapStateEntry, changes Chan
 		// place!
 		datapathEqual = oldEntry.MapStateEntry == entry.MapStateEntry
 		oldEntry.merge(&entry)
-		ms.insert(key, oldEntry)
+		ms.updateExisting(key, oldEntry)
 	} else if !exists || entry.IsDeny {
 		// Insert a new entry if one did not exist or a deny entry is overwriting an allow
 		// entry.
