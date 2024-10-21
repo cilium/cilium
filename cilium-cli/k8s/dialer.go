a6229a490387ed9cb761f84107508aa61b9a54c0
Alexandre Barone <abalexandrebarone@gmail.com>
2024-10-21 19:16:19 -0400

    cilium-cli: use native port-forwarding in hubble commands
    Replace kubectl-based port-forwarding with a native
    implementation inspired by the kubectl port-forward
    cmd introduced by the PortForwarder helper type.
    
    Signed-off-by: Alexandre Barone <abalexandrebarone@gmail.com>

--- cilium-cli/k8s/dialer.go
+++ cilium-cli/k8s/dialer.go
@@ -14,3 +14,12 @@ import (
 func (c *Client) PortForward(ctx context.Context, p k8s.PortForwardParameters) (*k8s.PortForwardResult, error) {
 	return k8s.NewPortForwarder(c.Clientset, c.Config).PortForward(ctx, p)
 }
+
+// PortForwardService executes in a goroutine a port forward command towards one of the pod behind a
+// service. If `localPort` is 0, a random port is selected. If `svcPort` is 0, uses the first port
+// configured on the service.
+//
+// To stop the port-forwarding, use the context by cancelling it.
+func (c *Client) PortForwardService(ctx context.Context, namespace, name string, localPort, svcPort int32) (*k8s.PortForwardServiceResult, error) {
+	return k8s.NewPortForwarder(c.Clientset, c.Config).PortForwardService(ctx, namespace, name, localPort, svcPort)
+}
