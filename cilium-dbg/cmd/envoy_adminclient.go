c38440c8fa889df1080b493cb4f98dfaae2be18c
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-10-23 12:21:42 +0200

    dbg: envoy: Introduce possibility to change Envoy log level
    Currently, the log level of the Envoy instance can only be configured
    (Helm) or adjusted during runtime (Rest API) globally.
    
    Envoy itself provides the possibility to configure the log levels per
    logger / Envoy component via its Admin interface
    (https://www.envoyproxy.io/docs/envoy/latest/operations/admin).
    
    This is helpful to reduce the amount of logs while investigating issues.
    
    Therefore, this commit introduces new commands to the cilium-dbg
    binary to list and change the log levels.
    
    * `cilium-dbg envoy admin logging list`
    * `cilium-dbg envoy admin logging set global <level>`
    * `cilium-dbg envoy admin logging set loggers <name>=<level>`
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- cilium-dbg/cmd/envoy_adminclient.go
+++ cilium-dbg/cmd/envoy_adminclient.go
@@ -52,6 +52,28 @@ func (a *envoyAdminClient) get(path string) (string, error) {
 	return string(body), nil
 }
 
+func (a *envoyAdminClient) post(path string) (string, error) {
+	client := a.unixHTTPClient()
+	defer client.CloseIdleConnections()
+
+	u, err := url.Parse(fmt.Sprintf("%s%s", a.adminURL, path))
+	if err != nil {
+		return "", fmt.Errorf("failed to parse URL: %w", err)
+	}
+
+	resp, err := client.Post(u.String(), "", nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to call %q endpoint: %w", path, err)
+	}
+	defer resp.Body.Close()
+
+	body, err := safeio.ReadAllLimit(resp.Body, safeio.MB)
+	if err != nil {
+		return "", fmt.Errorf("failed to read %q response: %w", path, err)
+	}
+
+	return string(body), nil
+}
 
 func (a *envoyAdminClient) unixHTTPClient() *http.Client {
 	// Use a custom dialer to use a Unix domain socket for a HTTP connection.
@@ -80,6 +102,31 @@ func (a *envoyAdminClient) GetConfigDump(resourceType string, resourceName strin
 
 func (a *envoyAdminClient) GetServerInfo() (string, error) {
 	return a.get("server_info")
+}
+
+func (a *envoyAdminClient) ListLoggingLevels() (string, error) {
+	path := "logging"
+
+	return a.post(path)
+}
+
+func (a *envoyAdminClient) SetLoggingLevelForAllLoggers(level string) (string, error) {
+	path := "logging?level=" + level
+
+	return a.post(path)
+}
+
+func (a *envoyAdminClient) SetLoggingLevelForLoggers(loggingLevels map[string]string) (string, error) {
+	path := "logging"
+
+	if len(loggingLevels) > 0 {
+		path = path + "?paths="
+		for k, v := range loggingLevels {
+			path = path + k + ":" + v + ","
+		}
+	}
+
+	return a.post(path)
 }
 
 func (a *envoyAdminClient) GetPrometheusStatistics(filterRegex string) (string, error) {
