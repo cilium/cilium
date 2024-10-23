2ed8f92549bf5bbf6282c9e6e3efc80ab67be7a1
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-10-23 12:19:30 +0200

    dbg: envoy: refactor closing of Envoy UNIX domain socket connection
    Currently, the Envoy Admin Client that is used in the cilium-dbg
    closes the underlying connection by keeping a reference to it.
    
    This isn't necessary, as the HTTP client provides the method
    `CloseIdleConnections`.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- cilium-dbg/cmd/envoy_adminclient.go
+++ cilium-dbg/cmd/envoy_adminclient.go
@@ -30,17 +30,8 @@ func newEnvoyAdminClient() *envoyAdminClient {
 }
 
 func (a *envoyAdminClient) get(path string) (string, error) {
-	// Use a custom dialer to use a Unix domain socket for a HTTP connection.
-	var conn net.Conn
-	var err error
-	client := &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
-				conn, err = net.Dial("unix", a.unixPath)
-				return conn, err
-			},
-		},
-	}
+	client := a.unixHTTPClient()
+	defer client.CloseIdleConnections()
 
 	u, err := url.Parse(fmt.Sprintf("%s%s", a.adminURL, path))
 	if err != nil {
@@ -51,7 +42,6 @@ func (a *envoyAdminClient) get(path string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("failed to call %q endpoint: %w", path, err)
 	}
-	defer conn.Close()
 	defer resp.Body.Close()
 
 	body, err := safeio.ReadAllLimit(resp.Body, safeio.MB)
@@ -60,6 +50,18 @@ func (a *envoyAdminClient) get(path string) (string, error) {
 	}
 
 	return string(body), nil
+}
+
+
+func (a *envoyAdminClient) unixHTTPClient() *http.Client {
+	// Use a custom dialer to use a Unix domain socket for a HTTP connection.
+	return &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", a.unixPath)
+			},
+		},
+	}
 }
 
 func (a *envoyAdminClient) GetConfigDump(resourceType string, resourceName string) (string, error) {
