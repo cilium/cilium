4c0c79045a19c69a4c55222bfa8164dfd4de9e8f
Nicolas Busseneau <nicolas@isovalent.com>
2024-10-24 19:32:36 +0200

    cli: move AWS chaining mode setup to AWS-specific code
    We move the logic for setting up chaining mode on AWS from the high
    level entrypoint to the dedicated AWS-specific code. This will help keep
    the entrypoint clean as we intend to enrich the high level logic for AWS
    in subsequent commits.
    
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- cilium-cli/install/aws.go
+++ cilium-cli/install/aws.go
@@ -3,9 +3,42 @@
 
 package install
 
+import (
+	"context"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+)
+
 const (
 	AwsNodeDaemonSetName              = "aws-node"
 	AwsNodeDaemonSetNamespace         = "kube-system"
 	AwsNodeDaemonSetNodeSelectorKey   = "io.cilium/aws-node-enabled"
 	AwsNodeDaemonSetNodeSelectorValue = "true"
 )
+
+func getChainingMode(values map[string]interface{}) string {
+	chainingMode, _, _ := unstructured.NestedString(values, "cni", "chainingMode")
+	return chainingMode
+}
+
+func (k *K8sInstaller) awsSetupChainingMode(ctx context.Context, values map[string]interface{}) error {
+	// detect chaining mode
+	chainingMode := getChainingMode(values)
+
+	// Do not stop AWS DS if we are running in chaining mode
+	if chainingMode != "aws-cni" && !k.params.IsDryRun() {
+		if _, err := k.client.GetDaemonSet(ctx, AwsNodeDaemonSetNamespace, AwsNodeDaemonSetName, metav1.GetOptions{}); err == nil {
+			k.Log("🔥 Patching the %q DaemonSet to evict its pods...", AwsNodeDaemonSetName)
+			patch := []byte(fmt.Sprintf(`{"spec":{"template":{"spec":{"nodeSelector":{"%s":"%s"}}}}}`, AwsNodeDaemonSetNodeSelectorKey, AwsNodeDaemonSetNodeSelectorValue))
+			if _, err := k.client.PatchDaemonSet(ctx, AwsNodeDaemonSetNamespace, AwsNodeDaemonSetName, types.StrategicMergePatchType, patch, metav1.PatchOptions{}); err != nil {
+				k.Log("❌ Unable to patch the %q DaemonSet", AwsNodeDaemonSetName)
+				return err
+			}
+		}
+	}
+
+	return nil
+}
