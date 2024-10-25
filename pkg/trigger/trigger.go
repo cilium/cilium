bf0ee34c167664b8a70d362a984e8b18f585a673
David Bimmler <david.bimmler@isovalent.com>
2024-10-25 13:19:30 +0200

    trigger: avoid allocating a new timer each second
    Each Trigger which are not triggered would allocate a new time.Timer (via
    inctimer.After) on each iteration of the waiter loop. There's no need to
    create this much garbage - we can use a ticker instead of the inctimer.
    
    The behaviour is not _exactly_ the same, but the semantics of the
    MinInterval and trigger folding are kept intact. As the stdlib docs say,
    the ticker adjusts to slow readers. Therefore if the trigger _is_
    triggered ofer, and we end up waiting for the MinInterval, the waiter loop is
    not run more times due to queued up ticks.
    
    goos: linux
    goarch: arm64
    pkg: github.com/cilium/cilium/pkg/trigger
    
                          │  before.txt  │              after.txt              │
                          │     B/op     │    B/op      vs base                │
    UntriggeredTrigger-10   11234.5 ± 1%   767.0 ± 11%  -93.17% (p=0.000 n=10)
    
                          │  before.txt  │             after.txt              │
                          │  allocs/op   │ allocs/op   vs base                │
    UntriggeredTrigger-10   144.000 ± 1%   8.000 ± 0%  -94.44% (p=0.000 n=10)
    
    Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

--- pkg/trigger/trigger.go
+++ pkg/trigger/trigger.go
@@ -6,7 +6,6 @@ package trigger
 import (
 	"fmt"
 
-	"github.com/cilium/cilium/pkg/inctimer"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/time"
 )
@@ -173,8 +172,9 @@ func (t *Trigger) Shutdown() {
 }
 
 func (t *Trigger) waiter() {
-	sleepTimer, sleepTimerDone := inctimer.New()
-	defer sleepTimerDone()
+	tk := time.NewTicker(t.params.sleepInterval)
+	defer tk.Stop()
+
 	for {
 		// keep critical section as small as possible
 		t.mutex.Lock()
@@ -208,8 +208,7 @@ func (t *Trigger) waiter() {
 
 		select {
 		case <-t.wakeupChan:
-		case <-sleepTimer.After(t.params.sleepInterval):
-
+		case <-tk.C:
 		case <-t.closeChan:
 			shutdownFunc := t.params.ShutdownFunc
 			if shutdownFunc != nil {
