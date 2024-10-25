b67716ac64bdc1d95d9b5a814f3ccfafb1d926d3
Alexandre Barone <abalexandrebarone@gmail.com>
2024-10-25 10:11:32 -0400

    k8s: add PortForwardService method to PortForwarder
    Add a new helper method that finds the best candidate
    pod for port-forwrding using a service reference.
    
    Signed-off-by: Alexandre Barone <abalexandrebarone@gmail.com>

--- pkg/k8s/portforward.go
+++ pkg/k8s/portforward.go
@@ -5,14 +5,22 @@ package k8s
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
+	"sort"
 	"strings"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/portforward"
 	"k8s.io/client-go/transport/spdy"
+	kutil "k8s.io/kubectl/pkg/util"
+	"k8s.io/kubectl/pkg/util/podutils"
+
+	"github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/labels"
 )
 
 // ForwardedPort holds the local and remote mapped ports.
@@ -114,4 +122,77 @@ func (pf *PortForwarder) PortForward(ctx context.Context, p PortForwardParameter
 	return &PortForwardResult{
 		ForwardedPorts: forwardedPorts,
 	}, nil
+}
+
+// PortForwardServiceResult are the ports that have been forwarded by PortForwardService.
+type PortForwardServiceResult struct {
+	ForwardedPort ForwardedPort
+}
+
+// PortForwardService executes in a goroutine a port forward command towards one of the pod behind a
+// service. If `localPort` is 0, a random port is selected. If `svcPort` is 0, uses the first port
+// configured on the service.
+//
+// To stop the port-forwarding, use the context by cancelling it.
+func (pf *PortForwarder) PortForwardService(ctx context.Context, namespace, name string, localPort, svcPort int32) (*PortForwardServiceResult, error) {
+	svc, err := pf.clientset.CoreV1().Services(namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get service %q: %w", name, err)
+	}
+
+	pod, err := pf.getFirstPodForService(ctx, svc)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get service %q: %w", name, err)
+	}
+
+	if svcPort == 0 {
+		svcPort = svc.Spec.Ports[0].Port
+	}
+
+	containerPort, err := kutil.LookupContainerPortNumberByServicePort(*svc, *pod, svcPort)
+	if err != nil {
+		return nil, fmt.Errorf("failed to lookup container port with service port %d: %w", svcPort, err)
+	}
+
+	p := PortForwardParameters{
+		Namespace:  pod.Namespace,
+		Pod:        pod.Name,
+		Ports:      []string{fmt.Sprintf("%d:%d", localPort, containerPort)},
+		Addresses:  nil, // default is localhost
+		OutWriters: OutWriters{Out: nil, ErrOut: nil},
+	}
+
+	res, err := pf.PortForward(ctx, p)
+	if err != nil {
+		return nil, fmt.Errorf("failed to port forward: %w", err)
+	}
+
+	return &PortForwardServiceResult{
+		ForwardedPort: res.ForwardedPorts[0],
+	}, nil
+}
+
+// getFirstPodForService returns the first pod in the list of pods matching the service selector,
+// sorted from most to less active (see `podutils.ActivePods` for more details).
+func (pf *PortForwarder) getFirstPodForService(ctx context.Context, svc *corev1.Service) (*corev1.Pod, error) {
+	selector := labels.SelectorFromSet(svc.Spec.Selector)
+	podList, err := pf.clientset.CoreV1().Pods(svc.Namespace).List(ctx, metav1.ListOptions{LabelSelector: selector.String()})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get list of pods for service %q: %w", svc.Name, err)
+	}
+	if len(podList.Items) == 0 {
+		return nil, fmt.Errorf("no pods found for service: %s", svc.Name)
+	}
+	if len(podList.Items) == 1 {
+		return &podList.Items[0], nil
+	}
+
+	pods := make([]*corev1.Pod, 0, len(podList.Items))
+	for _, pod := range podList.Items {
+		pods = append(pods, &pod)
+	}
+	sortBy := func(pods []*corev1.Pod) sort.Interface { return sort.Reverse(podutils.ActivePods(pods)) }
+	sort.Sort(sortBy(pods))
+
+	return pods[0], nil
 }
