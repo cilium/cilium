54396ed2d13a7cb8d68f43efa1f57c1f6b578620
Daniel Borkmann <daniel@iogearbox.net>
2024-10-25 15:28:11 +0000

    cilium, probes: Add netkit feature probe
    [ upstream commit 9c85a0d5fc7f45f66d6b369f5a9c77658581b807 ]
    
    Add a feature probe so that we can warn the user early if netkit is not
    supported by the underlying kernel.
    
    Fixes: #33878
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/datapath/linux/probes/probes.go
+++ pkg/datapath/linux/probes/probes.go
@@ -462,6 +462,51 @@ var HaveTCX = sync.OnceValue(func() error {
 	})
 })
 
+// HaveNetkit returns nil if the running kernel supports attaching bpf programs
+// to netkit devices.
+var HaveNetkit = sync.OnceValue(func() error {
+	prog, err := ebpf.NewProgram(&ebpf.ProgramSpec{
+		Type: ebpf.SchedCLS,
+		Instructions: asm.Instructions{
+			asm.Mov.Imm(asm.R0, 0),
+			asm.Return(),
+		},
+		License: "Apache-2.0",
+	})
+	if err != nil {
+		return err
+	}
+	defer prog.Close()
+
+	ns, err := netns.New()
+	if err != nil {
+		return fmt.Errorf("create netns: %w", err)
+	}
+	defer ns.Close()
+
+	return ns.Do(func() error {
+		l, err := link.AttachNetkit(link.NetkitOptions{
+			Program:   prog,
+			Attach:    ebpf.AttachNetkitPrimary,
+			Interface: int(^uint32(0)),
+		})
+		// We rely on this being checked during the syscall. With
+		// an otherwise correct payload we expect ENODEV here as
+		// an indication that the feature is present.
+		if errors.Is(err, unix.ENODEV) {
+			return nil
+		}
+		if err != nil {
+			return fmt.Errorf("creating link: %w", err)
+		}
+		if err := l.Close(); err != nil {
+			return fmt.Errorf("closing link: %w", err)
+		}
+
+		return fmt.Errorf("unexpected success: %w", err)
+	})
+})
+
 // HaveOuterSourceIPSupport tests whether the kernel support setting the outer
 // source IP address via the bpf_skb_set_tunnel_key BPF helper. We can't rely
 // on the verifier to reject a program using the new support because the
