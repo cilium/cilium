cilium/cilium

commit 6743d285e43ff08ba7a104baae4a8a1a0f10ed50

daemon: warn if auto direct node routes is set without a native routing CIDR
Issue a warning when these two options are selected, since we are not aware of
a use-case where this combination is deliberately chosen.

The reasoning is as follows. If you need auto-direct-node-routes, then all
nodes are directly connected via L2, you're using direct routing, and you don't
otherwise have a way to share the routes for pods. Hence, what you, presumably,
want is to automatically configure the routes for pod CIDRs between nodes and
also you would not want to masquerade/NAT such intra-cluster traffic.

In fact, if you did apply NAT for such traffic then it could even get you into
trouble because the identity of traffic from pods on one node towards pods on
another node could get lost. This is because in direct routing mode the
identity of the source of the traffic is not held in the packets between nodes,
therefore the destination must use the source address to look up the identity
when applying ingress policy. If the source masqueraded the traffic, then
instead of seeing a source pod IP, you'll see a source node IP and therefore
the identity will be considered as "remote-node" instead of having a rich
identity of the actual pod source, so the network policy will not be able to
allow traffic based on the source pod labels.

Suggested-by: Kornilios Kourtis <kornilios@isovalent.com>
Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

pkg/option/config.go
@@ -2702,6 +2702,11 @@ func (c *DaemonConfig) Populate() {
 		}
 	}
 
+	if c.EnableIPv4 && nativeRoutingCIDR == "" && ipv4NativeRoutingCIDR == "" && c.EnableAutoDirectRouting {
+		log.Warnf("If %s is enabled, then you are recommended to also configure %s. If %s is not configured, this may lead to pod to pod traffic being masqueraded, "+
+			"which can cause problems with performance, observability and policy", EnableAutoDirectRoutingName, IPv4NativeRoutingCIDR, IPv4NativeRoutingCIDR)
+	}
+
 	ipv6NativeRoutingCIDR := viper.GetString(IPv6NativeRoutingCIDR)
 
 	if ipv6NativeRoutingCIDR != "" {
@@ -2710,6 +2715,11 @@ func (c *DaemonConfig) Populate() {
 		if len(c.IPv6NativeRoutingCIDR.IP) != net.IPv6len {
 			log.Fatalf("%s must be an IPv6 CIDR", IPv6NativeRoutingCIDR)
 		}
+	}
+
+	if c.EnableIPv6 && ipv6NativeRoutingCIDR == "" && c.EnableAutoDirectRouting {
+		log.Warnf("If %s is enabled, then you are recommended to also configure %s. If %s is not configured, this may lead to pod to pod traffic being masqueraded, "+
+			"which can cause problems with performance, observability and policy", EnableAutoDirectRoutingName, IPv6NativeRoutingCIDR, IPv6NativeRoutingCIDR)
 	}
 
 	if err := c.calculateBPFMapSizes(); err != nil {

commit 6a537d0d4db078714a79d218c94282bb548dfec3

config: Remove superfluous warning on native routing CIDR
This commit removes a warning that was intended to warn the user that
running in native routing mode without a correct native routing CIDR
will lead to problems.

However, the conditions for the warning are not fully correct. There are
native routing setups where the user does not need to specify a native
routing CIDR, namely:

  1. The user is running in ENI or AlibabaCloud IPAM, where the native
     routing CIDR is auto-detected if not explicitly configured.
  2. The user is running with BPF ip-masq-agent where the native routing
     CIDR is optional (as there are other means of specifying exclusion
     CIDRs).
  3. The user is not running with Cilium-based masquerading.

The existing warning did not trigger for case 1, as the if condition
checked for `auto-direct-node-routes` rather than the `routing-mode`
flag, and `auto-direct-node-routes` is not used on ENI/AlibabaCloud IPAM
modes.

However, the warning would trigger for cases 2 and 3, which is
incorrect. Cases 2 and 3 should not emit a warning either. And indeed
already have a check on the native routing CIDR that handles all three
cases correctly, namely `checkIPv{4,6}NativeRoutingCIDR` here:

https://github.com/cilium/cilium/blob/bc26c6d3a74e3bfbeb1149e5116de51c496dd254/pkg/option/config.go#L3451-L3476

Therefore, we can safely remove the warning in this commit, as the
conditions are already covered by `checkIPv{4,6}NativeRoutingCIDR`. I
have also manually tested that the check does take effect if one sets
`routingMode=native` and `autoDirectNodeRoutes=true` without specifying
a native routing CIDR.

Fixes: 6743d285e43f ("daemon: warn if auto direct node routes is set without a native routing CIDR")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/option/config.go
@@ -3028,11 +3028,6 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 		}
 	}
 
-	if c.EnableIPv4 && ipv4NativeRoutingCIDR == "" && c.EnableAutoDirectRouting {
-		log.Warnf("If %s is enabled, then you are recommended to also configure %s. If %s is not configured, this may lead to pod to pod traffic being masqueraded, "+
-			"which can cause problems with performance, observability and policy", EnableAutoDirectRoutingName, IPv4NativeRoutingCIDR, IPv4NativeRoutingCIDR)
-	}
-
 	ipv6NativeRoutingCIDR := vp.GetString(IPv6NativeRoutingCIDR)
 
 	if ipv6NativeRoutingCIDR != "" {
@@ -3044,11 +3039,6 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 		if len(c.IPv6NativeRoutingCIDR.IP) != net.IPv6len {
 			log.Fatalf("%s must be an IPv6 CIDR", IPv6NativeRoutingCIDR)
 		}
-	}
-
-	if c.EnableIPv6 && ipv6NativeRoutingCIDR == "" && c.EnableAutoDirectRouting {
-		log.Warnf("If %s is enabled, then you are recommended to also configure %s. If %s is not configured, this may lead to pod to pod traffic being masqueraded, "+
-			"which can cause problems with performance, observability and policy", EnableAutoDirectRoutingName, IPv6NativeRoutingCIDR, IPv6NativeRoutingCIDR)
 	}
 
 	if c.DirectRoutingSkipUnreachable && !c.EnableAutoDirectRouting {