369c2e873b4da9a7876f6013de1478925e82ac8e
Rastislav Szabo <rastislav.szabo@isovalent.com>
2024-11-04 11:39:30 +0100

    cilium-cli: Run BGP tests sequentially
    BGP tests need to run sequentially since BGP CP uses cluster-scoped
    CRDs and the test inrfra uses same node-without-cilium to peer with.
    Run them sequentially and deploy the test FRR instance only when
    test concurrency is disabled.
    
    Signed-off-by: Rastislav Szabo <rastislav.szabo@isovalent.com>

--- cilium-cli/connectivity/builder/bgp_control_plane.go
+++ cilium-cli/connectivity/builder/bgp_control_plane.go
@@ -13,7 +13,8 @@ import (
 type bgpControlPlane struct{}
 
 func (t bgpControlPlane) build(ct *check.ConnectivityTest, _ map[string]string) {
-	newTest("bgp-control-plane-v1", ct).
+	// prefix the test name with `seq-` to run it sequentially
+	newTest("seq-bgp-control-plane-v1", ct).
 		WithCondition(func() bool {
 			return versioncheck.MustCompile(">=1.16.0")(ct.CiliumVersion) && ct.Params().IncludeUnsafeTests
 		}).
@@ -23,7 +24,8 @@ func (t bgpControlPlane) build(ct *check.ConnectivityTest, _ map[string]string) 
 		).
 		WithScenarios(tests.BGPAdvertisements(1))
 
-	newTest("bgp-control-plane-v2", ct).
+	// prefix the test name with `seq-` to run it sequentially
+	newTest("seq-bgp-control-plane-v2", ct).
 		WithCondition(func() bool {
 			return versioncheck.MustCompile(">=1.16.0")(ct.CiliumVersion) && ct.Params().IncludeUnsafeTests
 		}).
