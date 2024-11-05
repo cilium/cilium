31532517d9b00a87a97dc10f8427a45039d9fd92
Tom Hadlaw <tom.hadlaw@isovalent.com>
2024-11-05 14:30:45 -0800

    Revert "datapath: move policy map value prefix length to flags"
    This reverts commit 60bc8fa5f4641e9e547716ba73ff9be31c1b4e94.

--- pkg/endpoint/bpf.go
+++ pkg/endpoint/bpf.go
@@ -1138,11 +1138,13 @@ func (e *Endpoint) syncPolicyMapWith(realized policy.MapState, withDiffs bool) (
 func (e *Endpoint) dumpPolicyMapToMapState() (policy.MapState, error) {
 	currentMap, insert := policy.NewMapStateWithInsert()
 
-	cb := func(policymapKey *policymap.PolicyKey, policymapEntry *policymap.PolicyEntry) {
+	cb := func(key bpf.MapKey, value bpf.MapValue) {
+		policymapKey := key.(*policymap.PolicyKey)
 		// Convert from policymap.Key to policy.Key
 		policyKey := policy.KeyForDirection(trafficdirection.TrafficDirection(policymapKey.TrafficDirection)).
 			WithIdentity(identity.NumericIdentity(policymapKey.Identity)).
 			WithPortProtoPrefix(u8proto.U8proto(policymapKey.Nexthdr), policymapKey.GetDestPort(), policymapKey.GetPortPrefixLen())
+		policymapEntry := value.(*policymap.PolicyEntry)
 		// Convert from policymap.PolicyEntry to policy.MapStateEntry.
 		policyEntry := policy.MapStateEntry{
 			ProxyPort: policymapEntry.GetProxyPort(),
@@ -1151,7 +1153,7 @@ func (e *Endpoint) dumpPolicyMapToMapState() (policy.MapState, error) {
 		}
 		insert(policyKey, policyEntry)
 	}
-	err := e.policyMap.DumpValid(cb)
+	err := e.policyMap.DumpWithCallback(cb)
 
 	return currentMap, err
 }
