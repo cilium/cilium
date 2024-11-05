3823621519442cc1f1a0f9f7be6dd1f4754eec0f
Tom Hadlaw <tom.hadlaw@isovalent.com>
2024-11-05 14:32:09 -0800

    Revert "ctrl-runtime: lower severity of retryable reconcile errors"
    This reverts commit e2b17ece53309060572db0eafffda14be96a97f8.

--- operator/pkg/controller-runtime/cell.go
+++ operator/pkg/controller-runtime/cell.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
+	"github.com/go-logr/logr"
 	"google.golang.org/protobuf/proto"
 	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -69,7 +70,7 @@ func newManager(params managerParams) (ctrlRuntime.Manager, error) {
 		return proto.Equal(xdsResource1.Any, xdsResource2.Any)
 	})
 
-	ctrlRuntime.SetLogger(newLogrFromSlog(params.Logger))
+	ctrlRuntime.SetLogger(logr.FromSlogHandler(params.Logger.Handler()))
 
 	mgr, err := ctrlRuntime.NewManager(params.K8sClient.RestConfig(), ctrlRuntime.Options{
 		Scheme: params.Scheme,
@@ -77,6 +78,7 @@ func newManager(params managerParams) (ctrlRuntime.Manager, error) {
 		Metrics: metricsserver.Options{
 			BindAddress: "0",
 		},
+		Logger: logr.FromSlogHandler(params.Logger.Handler()),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to create new controller-runtime manager: %w", err)
