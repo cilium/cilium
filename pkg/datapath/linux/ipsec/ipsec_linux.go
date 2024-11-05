77acfbdd54961d79cff21e3afdf41beaaff1b804
Tom Hadlaw <tom.hadlaw@isovalent.com>
2024-11-05 14:32:09 -0800

    Revert "ipsec: Define IPSecDir as flags instead of strings"
    This reverts commit a8bde254162b2a597265bb136a78db6949dbae2c.

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -42,12 +42,12 @@ import (
 	"github.com/cilium/cilium/pkg/time"
 )
 
-type IPSecDir uint32
+type IPSecDir string
 
 const (
-	IPSecDirIn IPSecDir = 1 << iota
-	IPSecDirOut
-	IPSecDirFwd
+	IPSecDirIn  IPSecDir = "IPSEC_IN"
+	IPSecDirOut IPSecDir = "IPSEC_OUT"
+	IPSecDirFwd IPSecDir = "IPSEC_FWD"
 
 	// Constants used to decode the IPsec secret in both formats:
 	// 1. [spi] aead-algo aead-key icv-len
@@ -945,7 +945,7 @@ func UpsertIPsecEndpoint(log *slog.Logger, params *IPSecParameters) (uint8, erro
 	 * state would need to be cached in the ipcache.
 	 */
 	if !params.SourceTunnelIP.Equal(*params.DestTunnelIP) {
-		if params.Dir&IPSecDirIn != 0 {
+		if params.Dir == IPSecDirIn {
 			if spi, err = ipSecReplaceStateIn(log, params); err != nil {
 				return 0, fmt.Errorf("unable to replace local state: %w", err)
 			}
@@ -956,7 +956,7 @@ func UpsertIPsecEndpoint(log *slog.Logger, params *IPSecParameters) (uint8, erro
 			}
 		}
 
-		if params.Dir&IPSecDirFwd != 0 {
+		if params.Dir == IPSecDirFwd {
 			if err = IpSecReplacePolicyFwd(params); err != nil {
 				if !os.IsExist(err) {
 					return 0, fmt.Errorf("unable to replace policy fwd: %w", err)
@@ -964,7 +964,7 @@ func UpsertIPsecEndpoint(log *slog.Logger, params *IPSecParameters) (uint8, erro
 			}
 		}
 
-		if params.Dir&IPSecDirOut != 0 {
+		if params.Dir == IPSecDirOut {
 			if spi, err = ipSecReplaceStateOut(log, params); err != nil {
 				return 0, fmt.Errorf("unable to replace remote state: %w", err)
 			}
