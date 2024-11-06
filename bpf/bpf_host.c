cilium/cilium

commit b0e0b0c773da7b446052b75accff5fc41ff59be8

bpf: propagate src sec id from ingress bpf_overlay to egress bpf_host
as in a subsequent change bpf_host will need to access the source
identity that was carried over the tunnel

Signed-off-by: Gilberto Bertin <jibi@cilium.io>

bpf/bpf_host.c
@@ -1381,6 +1381,8 @@ int cil_to_netdev(struct __ctx_buff *ctx __maybe_unused)
 
 	if (magic == MARK_MAGIC_HOST || magic == MARK_MAGIC_OVERLAY)
 		src_sec_identity = HOST_ID;
+	else if (magic == MARK_MAGIC_IDENTITY)
+		src_sec_identity = get_identity(ctx);
 
 	/* Filter allowed vlan id's and pass them back to kernel.
 	 */

bpf/bpf_overlay.c
@@ -457,6 +457,9 @@ not_esp:
 	/* A packet entering the node from the tunnel and not going to a local
 	 * endpoint has to be going to the local host.
 	 */
+
+	set_identity_mark(ctx, *identity, MARK_MAGIC_IDENTITY);
+
 	return ipv4_host_delivery(ctx, ip4);
 }
 

commit 7f241bb4292a2d22a43471a6be7ec24ced22755e

bpf: host: fix HostFW egress path in to-netdev for non-host traffic
When using iptables for masquerading, the HostFW egress path needs to
differentiate between "host-originating traffic" and "pod-originating
but masqueraded with a Host IP". This uses several mechanisms:
1. for host-originating traffic, the skb is marked with MARK_MAGIC_HOST
   by an iptables rule. The to-netdev program then selects the packet's
   **real** source identity as HOST_ID.
2. for other traffic, the resolve_srcid_ipv*() call in
   handle_to_netdev_ipv*() performs an ipcache lookup to resolve the
   "apparent" source identity.

The HostFW code then takes the following decisions:
1. for a packet that is **neither* originating from Host **nor** SNATed
   with a Host IP, skip the HostFW path.
2. for a packet that is **not** originating from Host **but** SNATed with
   a Host IP, create a CT entry but skip the HostFW policy. The CT entry
   is needed so that replies can match it and skip Ingress Network policy.
3. for a packet that is genuinely originating from Host, create a CT entry
   **and** apply HostFW policy.

Unfortunately this logic broke with commit
b0e0b0c773da ("bpf: propagate src sec id from ingress bpf_overlay to egress bpf_host").
With this change the to-netdev program now also extracts the actual
identity from the skb->mark for pod-originating packets. When
resolve_srcid_ipv*() receives this identity, it skips the ipcache lookup.
And therefore ipv*_host_policy_egress_lookup() determines that the packet
is not SNATed with a Host IP, and exits the HostFW path without creating
the expected CT entry.

In short we take case (1), when we should take case (2). Thus inbound
replies can't be matched against a CT entry, fall into scope of Ingress
Network policy and are potentially dropped.

Quick-fix this problem by manually over-riding the source identity in the
HostFW path, so that we again perform the needed ipcache lookup. This

bpf/bpf_host.c
@@ -517,6 +517,14 @@ handle_to_netdev_ipv6(struct __ctx_buff *ctx, __u32 src_sec_identity,
 			return ret;
 	}
 
+	/* The code below only cares about host-originating yes/no,
+	 * and currently breaks when being passed a fine-grained pod src_sec_identity.
+	 *
+	 * Restore old behavior for now, and clean it up once we have tests.
+	 */
+	if (src_sec_identity != HOST_ID)
+		src_sec_identity = 0;
+
 	srcid = resolve_srcid_ipv6(ctx, ip6, src_sec_identity,
 				   &ipcache_srcid, true);
 
@@ -963,6 +971,14 @@ handle_to_netdev_ipv4(struct __ctx_buff *ctx, __u32 src_sec_identity,
 
 	if (!revalidate_data_pull(ctx, &data, &data_end, &ip4))
 		return DROP_INVALID;
+
+	/* The code below only cares about host-originating yes/no,
+	 * and currently breaks when being passed a fine-grained pod src_sec_identity.
+	 *
+	 * Restore old behavior for now, and clean it up once we have tests.
+	 */
+	if (src_sec_identity != HOST_ID)
+		src_sec_identity = 0;
 
 	src_id = resolve_srcid_ipv4(ctx, ip4, src_sec_identity,
 				    &ipcache_srcid, true);