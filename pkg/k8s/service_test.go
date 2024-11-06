cilium/cilium

commit ab8557c8accf85a78b2ec3a683f5808442568bd9

k8s: support for loadbalancer svc ip mode
This patch implements KEP-1860 for cilium kube-proxy replacement.
This feature has been an alpha feature since K8s 1.29 and recently
became beta feture in 1.30.

In a word, kube-proxy should only perform loadbalancing for
LoadbalancerIP when the IPMode is "VIP" or empty.

Signed-off-by: Quan Wei <quanwei.153@bytedance.com>

pkg/k8s/service.go
@@ -229,7 +229,7 @@ func ParseService(svc *slim_corev1.Service, nodePortAddrs []netip.Addr) (Service
 
 	if expType.canExpose(slim_corev1.ServiceTypeLoadBalancer) {
 		for _, ip := range svc.Status.LoadBalancer.Ingress {
-			if ip.IP != "" {
+			if ip.IP != "" && ip.IPMode == nil || *ip.IPMode == slim_corev1.LoadBalancerIPModeVIP {
 				loadBalancerIPs = append(loadBalancerIPs, ip.IP)
 			}
 		}

pkg/k8s/service_test.go
@@ -439,6 +439,7 @@ func TestParseService(t *testing.T) {
 	objMeta.Annotations = map[string]string{
 		corev1.DeprecatedAnnotationTopologyAwareHints: "auto",
 	}
+	loadbalancerIngressIP := "127.0.0.1"
 	k8sSvc = &slim_corev1.Service{
 		ObjectMeta: objMeta,
 		Spec: slim_corev1.ServiceSpec{
@@ -457,6 +458,15 @@ func TestParseService(t *testing.T) {
 					Port:     69,
 					NodePort: 0,
 					Protocol: slim_corev1.ProtocolUDP,
+				},
+			},
+		},
+		Status: slim_corev1.ServiceStatus{
+			LoadBalancer: slim_corev1.LoadBalancerStatus{
+				Ingress: []slim_corev1.LoadBalancerIngress{
+					{
+						IP: loadbalancerIngressIP,
+					},
 				},
 			},
 		},
@@ -480,6 +490,66 @@ func TestParseService(t *testing.T) {
 		ipv4NodePortAddrCluster.Addr(),
 	}
 
+	id, svc = ParseService(k8sSvc, addrs)
+	require.EqualValues(t, ServiceID{Namespace: "bar", Name: "foo"}, id)
+	require.EqualValues(t, &Service{
+		FrontendIPs: []net.IP{net.ParseIP("127.0.0.1")},
+		Labels:      map[string]string{"foo": "bar"},
+		Ports: map[loadbalancer.FEPortName]*loadbalancer.L4Addr{
+			"http": loadbalancer.NewL4Addr(loadbalancer.L4Type(slim_corev1.ProtocolTCP), uint16(80)),
+			"tftp": loadbalancer.NewL4Addr(loadbalancer.L4Type(slim_corev1.ProtocolUDP), uint16(69)),
+		},
+		ExtTrafficPolicy: loadbalancer.SVCTrafficPolicyCluster,
+		IntTrafficPolicy: loadbalancer.SVCTrafficPolicyCluster,
+		NodePorts: map[loadbalancer.FEPortName]NodePortToFrontend{
+			"http": {
+				zeroFE.String():     zeroFE,
+				internalFE.String(): internalFE,
+				nodePortFE.String(): nodePortFE,
+			},
+		},
+		LoadBalancerSourceRanges: map[string]*cidr.CIDR{},
+		K8sExternalIPs:           map[string]net.IP{},
+		LoadBalancerIPs:          map[string]net.IP{loadbalancerIngressIP: net.ParseIP(loadbalancerIngressIP)},
+		Type:                     loadbalancer.SVCTypeLoadBalancer,
+		TopologyAware:            true,
+		ForwardingMode:           loadbalancer.SVCForwardingModeSNAT,
+		Annotations:              map[string]string{"service.kubernetes.io/topology-aware-hints": "auto"},
+	}, svc)
+
+	ipMode := slim_corev1.LoadBalancerIPModeProxy
+	k8sSvc = &slim_corev1.Service{
+		ObjectMeta: objMeta,
+		Spec: slim_corev1.ServiceSpec{
+			ClusterIP: "127.0.0.1",
+			Type:      slim_corev1.ServiceTypeLoadBalancer,
+			Ports: []slim_corev1.ServicePort{
+				{
+					Name:     "http",
+					Port:     80,
+					NodePort: 31111,
+					Protocol: slim_corev1.ProtocolTCP,
+				},
+				{
+					// NodePort should not be allocated for this entry.
+					Name:     "tftp",
+					Port:     69,
+					NodePort: 0,
+					Protocol: slim_corev1.ProtocolUDP,
+				},
+			},
+		},
+		Status: slim_corev1.ServiceStatus{
+			LoadBalancer: slim_corev1.LoadBalancerStatus{
+				Ingress: []slim_corev1.LoadBalancerIngress{
+					{
+						IP:     loadbalancerIngressIP,
+						IPMode: &ipMode,
+					},
+				},
+			},
+		},
+	}
 	id, svc = ParseService(k8sSvc, addrs)
 	require.EqualValues(t, ServiceID{Namespace: "bar", Name: "foo"}, id)
 	require.EqualValues(t, &Service{

commit 3684b8ec829ef49418abec2d735cedfd496266ee

k8s: Avoid panic while checking ip mode
Update if condition to avoid panic if ip mode is nil, as OR operator is
having higher precedence than AND operator.

```
panic: runtime error: invalid memory address or nil pointer dereference
 [signal SIGSEGV: segmentation violation code=0x1 addr=0x8 pc=0x222dcb0]
 goroutine 444 [running]:
 github.com/cilium/cilium/pkg/k8s.ParseService(0x400308f0e0, {0x40030a8480, 0x3, 0x4})
     /go/src/github.com/cilium/cilium/pkg/k8s/service.go:232 +0x1320
 github.com/cilium/cilium/pkg/k8s.(*ServiceCache).UpdateService(0x4001f10c60, 0x400308f0e0, 0x400133f680)
     /go/src/github.com/cilium/cilium/pkg/k8s/service_cache.go:337 +0x1e8
 github.com/cilium/cilium/pkg/k8s/watchers.(*K8sServiceWatcher).upsertK8sServiceV1(0x40005f9400, 0x400308f0e0, 0x10?)
     /go/src/github.com/cilium/cilium/pkg/k8s/watchers/service.go:142 +0x30
 github.com/cilium/cilium/pkg/k8s/watchers.(*K8sServiceWatcher).serviceEventLoop(0x40005f9400, 0x400190be38, 0x400133f680)
     /go/src/github.com/cilium/cilium/pkg/k8s/watchers/service.go:130 +0x1f4
 created by github.com/cilium/cilium/pkg/k8s/watchers.(*K8sServiceWatcher).servicesInit in goroutine 1
     /go/src/github.com/cilium/cilium/pkg/k8s/watchers/service.go:101 +0x160
```

Fixes: ab8557c8accf85a78b2ec3a683f5808442568bd9
Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/k8s/service.go
@@ -229,7 +229,7 @@ func ParseService(svc *slim_corev1.Service, nodePortAddrs []netip.Addr) (Service
 
 	if expType.canExpose(slim_corev1.ServiceTypeLoadBalancer) {
 		for _, ip := range svc.Status.LoadBalancer.Ingress {
-			if ip.IP != "" && ip.IPMode == nil || *ip.IPMode == slim_corev1.LoadBalancerIPModeVIP {
+			if ip.IP != "" && (ip.IPMode == nil || *ip.IPMode == slim_corev1.LoadBalancerIPModeVIP) {
 				loadBalancerIPs = append(loadBalancerIPs, ip.IP)
 			}
 		}

pkg/k8s/service_test.go
@@ -576,6 +576,57 @@ func TestParseService(t *testing.T) {
 		TopologyAware:            true,
 		Annotations:              map[string]string{"service.kubernetes.io/topology-aware-hints": "auto"},
 	}, svc)
+
+	// Same as the previous test, but LB service status is empty.
+	// This is to simulate the delay while waiting for cloud provider to assign IP.
+	k8sSvc = &slim_corev1.Service{
+		ObjectMeta: objMeta,
+		Spec: slim_corev1.ServiceSpec{
+			ClusterIP: "127.0.0.1",
+			Type:      slim_corev1.ServiceTypeLoadBalancer,
+			Ports: []slim_corev1.ServicePort{
+				{
+					Name:     "http",
+					Port:     80,
+					NodePort: 31111,
+					Protocol: slim_corev1.ProtocolTCP,
+				},
+				{
+					// NodePort should not be allocated for this entry.
+					Name:     "tftp",
+					Port:     69,
+					NodePort: 0,
+					Protocol: slim_corev1.ProtocolUDP,
+				},
+			},
+		},
+	}
+	id, svc = ParseService(k8sSvc, addrs)
+	require.EqualValues(t, ServiceID{Namespace: "bar", Name: "foo"}, id)
+	require.EqualValues(t, &Service{
+		FrontendIPs: []net.IP{net.ParseIP("127.0.0.1")},
+		Labels:      map[string]string{"foo": "bar"},
+		Ports: map[loadbalancer.FEPortName]*loadbalancer.L4Addr{
+			"http": loadbalancer.NewL4Addr(loadbalancer.L4Type(slim_corev1.ProtocolTCP), uint16(80)),
+			"tftp": loadbalancer.NewL4Addr(loadbalancer.L4Type(slim_corev1.ProtocolUDP), uint16(69)),
+		},
+		ExtTrafficPolicy: loadbalancer.SVCTrafficPolicyCluster,
+		IntTrafficPolicy: loadbalancer.SVCTrafficPolicyCluster,
+		NodePorts: map[loadbalancer.FEPortName]NodePortToFrontend{
+			"http": {
+				zeroFE.String():     zeroFE,
+				internalFE.String(): internalFE,
+				nodePortFE.String(): nodePortFE,
+			},
+		},
+		LoadBalancerSourceRanges: map[string]*cidr.CIDR{},
+		K8sExternalIPs:           map[string]net.IP{},
+		LoadBalancerIPs:          map[string]net.IP{},
+		Type:                     loadbalancer.SVCTypeLoadBalancer,
+		ForwardingMode:           loadbalancer.SVCForwardingModeSNAT,
+		TopologyAware:            true,
+		Annotations:              map[string]string{"service.kubernetes.io/topology-aware-hints": "auto"},
+	}, svc)
 }
 
 func TestIsK8ServiceExternal(t *testing.T) {