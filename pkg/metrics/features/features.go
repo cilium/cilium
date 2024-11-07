7616c794ccca27675576c8da45853134619a9aae
André Martins <andre@cilium.io>
2024-11-07 11:34:34 +0100

    metrics/features: introduce features package to track enable features
    With a dedicate package to detect which features are enabled, we will be
    able to report, in form of metrics, from the agents, which features are
    enabled. Once all features are tracked, it will give us the ability to
    track down which features are being tested in our CI and the ones that
    are lacking coverage.
    
    Signed-off-by: André Martins <andre@cilium.io>

---
+++ pkg/metrics/features/features.go
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package features
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+)
+
+func updateAgentConfigMetricOnStart(jg job.Group, params featuresParams, m featureMetrics) error {
+	jg.Add(job.OneShot("update-config-metric", func(ctx context.Context, health cell.Health) error {
+		// We depend on settings modified by the Daemon startup.
+		// Once the Deamon is initialized this promise
+		// is resolved and we are guaranteed to have the correct settings.
+		health.OK("Waiting for agent config")
+		agentConfig, err := params.ConfigPromise.Await(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to get agent config: %w", err)
+		}
+		m.update(&params, agentConfig)
+		return nil
+	}))
+
+	return nil
+}
