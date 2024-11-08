75b6220c9a4d1c178468bc76926d4b32a30fa5a8
André Martins <andre@cilium.io>
2024-11-08 13:53:08 +0100

    metrics/features: enable Egress Gateway
    Track if Egress Gateway is enabled in Cilium
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/metrics/features/metrics_test.go
+++ pkg/metrics/features/metrics_test.go
@@ -890,3 +890,48 @@ func TestUpdateBGPAvertisment(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdateIPv4EgressGateway(t *testing.T) {
+	tests := []struct {
+		name      string
+		enableEGW bool
+		expected  float64
+	}{
+		{
+			name:      "Enable EGW",
+			enableEGW: true,
+			expected:  1,
+		},
+		{
+			name:      "Disable EGW",
+			enableEGW: false,
+			expected:  0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			metrics := NewMetrics(true)
+			config := &option.DaemonConfig{
+				IPAM:                    defaultIPAMModes[0],
+				EnableIPv4:              true,
+				IdentityAllocationMode:  defaultIdentityAllocationModes[0],
+				DatapathMode:            defaultDeviceModes[0],
+				NodePortMode:            defaultNodePortModes[0],
+				NodePortAlg:             defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:    defaultNodePortModeAccelerations[0],
+				EnableIPv4EgressGateway: tt.enableEGW,
+			}
+
+			params := mockFeaturesParams{
+				CNIChainingMode: defaultChainingModes[0],
+			}
+
+			metrics.update(params, config)
+
+			counterValue := metrics.ACLBEgressGatewayEnabled.Get()
+
+			assert.Equal(t, tt.expected, counterValue, "Expected value to be %.f for enabled: %t, got %.f", tt.expected, tt.enableEGW, counterValue)
+		})
+	}
+}
