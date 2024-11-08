cilium/cilium

commit 485331fbae75f0a56d1f8657685532909aba3475

experimental: Extend LBMaps implementation
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/loadbalancer/experimental/writer.go
@@ -319,6 +319,7 @@ func (w *Writer) DeleteServicesBySource(txn WriteTxn, source source.Source) erro
 	return nil
 }
 
+// UpsertBackends adds/updates backends for the given service.
 func (w *Writer) UpsertBackends(txn WriteTxn, serviceName loadbalancer.ServiceName, source source.Source, bes ...BackendParams) error {
 	refs, err := w.updateBackends(txn, serviceName, source, bes)
 	if err != nil {
@@ -330,6 +331,46 @@ func (w *Writer) UpsertBackends(txn WriteTxn, serviceName loadbalancer.ServiceNa
 			return err
 		}
 	}
+	return nil
+}
+
+// SetBackends sets the backends associated with a service. Existing backends from this source that
+// are associated with the service but are not given are released.
+func (w *Writer) SetBackends(txn WriteTxn, name loadbalancer.ServiceName, source source.Source, bes ...BackendParams) error {
+	addrs := sets.New[loadbalancer.L3n4Addr]()
+	for _, be := range bes {
+		addrs.Insert(be.L3n4Addr)
+	}
+	orphans := statedb.Filter(
+		w.bes.List(txn, BackendByServiceName(name)),
+		func(be *Backend) bool { return !addrs.Has(be.L3n4Addr) })
+
+	refs, err := w.updateBackends(txn, name, source, bes)
+	if err != nil {
+		return err
+	}
+
+	// Release orphaned backends, e.g. all backends from this source referencing this
+	// service.
+	for orphan, _, ok := orphans.Next(); ok; orphan, _, ok = orphans.Next() {
+		iter := orphan.Instances.All()
+		for _, inst, ok := iter.Next(); ok; _, inst, ok = iter.Next() {
+			if inst.Source == source {
+				if err := w.removeBackendRef(txn, name, orphan); err != nil {
+					return err
+				}
+			}
+			break
+		}
+	}
+
+	// Recompute the backends associated with each frontend.
+	for svc := range refs {
+		if err := w.refreshFrontendsOfService(txn, svc); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -473,6 +514,23 @@ func (w *Writer) ReleaseBackend(txn WriteTxn, name loadbalancer.ServiceName, add
 
 	if err := w.removeBackendRef(txn, name, be); err != nil {
 		return err
+	}
+	return w.refreshFrontendsOfService(txn, name)
+}
+
+func (w *Writer) ReleaseBackendsFromSource(txn WriteTxn, name loadbalancer.ServiceName, source source.Source) error {
+	bes := w.bes.List(txn, BackendByServiceName(name))
+	for be, _, ok := bes.Next(); ok; be, _, ok = bes.Next() {
+		iter := be.Instances.All()
+		for _, inst, ok := iter.Next(); ok; _, inst, ok = iter.Next() {
+			if inst.Source != source {
+				continue
+			}
+			if err := w.removeBackendRef(txn, name, be); err != nil {
+				return err
+			}
+			break
+		}
 	}
 	return w.refreshFrontendsOfService(txn, name)
 }

pkg/loadbalancer/experimental/bpf_reconciler.go
@@ -17,7 +17,6 @@ import (
 	"golang.org/x/sys/unix"
 	"k8s.io/apimachinery/pkg/util/sets"
 
-	"github.com/cilium/cilium/pkg/bpf"
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
@@ -69,7 +68,13 @@ type bpfOps struct {
 	backendIDAlloc     idAllocator
 	restoredBackendIDs sets.Set[loadbalancer.BackendID]
 
-	backendStates     map[loadbalancer.L3n4Addr]backendState
+	// backendStates maps from backend address to associated state.
+	// This is used to track which frontends reference a specific backend
+	// in order to delete orphaned backeds.
+	backendStates map[loadbalancer.L3n4Addr]backendState
+
+	// backendReferences maps from frontend address to the set of referenced
+	// backends.
 	backendReferences map[loadbalancer.L3n4Addr]sets.Set[loadbalancer.L3n4Addr]
 
 	// nodePortAddrs are the last used NodePort addresses for a given NodePort
@@ -107,6 +112,9 @@ func (ops *bpfOps) start(_ cell.HookContext) error {
 	// Restore the ID allocations from the BPF maps in order to reuse
 	// them and thus avoiding traffic disruptions.
 	err := ops.lbmaps.DumpService(func(key lbmap.ServiceKey, value lbmap.ServiceValue) {
+		if key.GetBackendSlot() != 0 {
+			return
+		}
 		id := loadbalancer.ID(value.GetRevNat())
 		ops.serviceIDAlloc.addID(svcKeyToAddr(key), id)
 		ops.restoredServiceIDs.Insert(id)
@@ -134,10 +142,10 @@ func svcKeyToAddr(svcKey lbmap.ServiceKey) loadbalancer.L3n4Addr {
 }
 
 func beValueToAddr(beValue lbmap.BackendValue) loadbalancer.L3n4Addr {
-	feIP := beValue.GetAddress()
-	feAddrCluster := cmtypes.MustAddrClusterFromIP(feIP)
-	feL3n4Addr := loadbalancer.NewL3n4Addr(loadbalancer.TCP /* FIXME */, feAddrCluster, beValue.GetPort(), 0)
-	return *feL3n4Addr
+	beIP := beValue.GetAddress()
+	beAddrCluster := cmtypes.MustAddrClusterFromIP(beIP)
+	beL3n4Addr := loadbalancer.NewL3n4Addr(loadbalancer.TCP /* FIXME */, beAddrCluster, beValue.GetPort(), 0)
+	return *beL3n4Addr
 }
 
 // Delete implements reconciler.Operations.
@@ -233,11 +241,10 @@ func (ops *bpfOps) deleteFrontend(fe *Frontend) error {
 
 func (ops *bpfOps) pruneServiceMaps() error {
 	toDelete := []lbmap.ServiceKey{}
-	svcCB := func(key bpf.MapKey, value bpf.MapValue) {
-		svcKey := key.(lbmap.ServiceKey).ToHost()
+	svcCB := func(svcKey lbmap.ServiceKey, _ lbmap.ServiceValue) {
 		ac, ok := cmtypes.AddrClusterFromIP(svcKey.GetAddress())
 		if !ok {
-			ops.log.Warn("Prune: bad address in service key", "key", key)
+			ops.log.Warn("Prune: bad address in service key", "key", svcKey)
 			return
 		}
 		addr := loadbalancer.L3n4Addr{
@@ -246,15 +253,16 @@ func (ops *bpfOps) pruneServiceMaps() error {
 			Scope:       svcKey.GetScope(),
 		}
 		if _, ok := ops.backendReferences[addr]; !ok {
-			toDelete = append(toDelete, svcKey)
+			toDelete = append(toDelete, svcKey.ToNetwork())
 		}
 	}
-	lbmap.Service4MapV2.DumpWithCallback(svcCB)
-	lbmap.Service6MapV2.DumpWithCallback(svcCB)
+	if err := ops.lbmaps.DumpService(svcCB); err != nil {
+		ops.log.Warn("Failed to prune service maps", "error", err)
+	}
 
 	for _, key := range toDelete {
-		if err := key.MapDelete(); err != nil {
-			ops.log.Warn("Failed to delete from service map", "error", err)
+		if err := ops.lbmaps.DeleteService(key); err != nil {
+			ops.log.Warn("Failed to delete from service map while pruning", "error", err)
 		}
 	}
 	return nil
@@ -262,18 +270,17 @@ func (ops *bpfOps) pruneServiceMaps() error {
 
 func (ops *bpfOps) pruneBackendMaps() error {
 	toDelete := []lbmap.BackendKey{}
-	beCB := func(key bpf.MapKey, value bpf.MapValue) {
-		beKey := key.(lbmap.BackendKey)
-		beValue := value.(lbmap.BackendValue).ToHost()
+	beCB := func(beKey lbmap.BackendKey, beValue lbmap.BackendValue) {
 		if _, ok := ops.backendStates[beValueToAddr(beValue)]; !ok {
 			ops.log.Info("pruneBackendMaps: deleting", "id", beKey.GetID(), "addr", beValueToAddr(beValue))
 			toDelete = append(toDelete, beKey)
 		}
 	}
-	lbmap.Backend4MapV3.DumpWithCallback(beCB)
-	lbmap.Backend6MapV3.DumpWithCallback(beCB)
+	if err := ops.lbmaps.DumpBackend(beCB); err != nil {
+		ops.log.Warn("Failed to prune backend maps", "error", err)
+	}
 	for _, key := range toDelete {
-		if err := key.Map().Delete(key); err != nil {
+		if err := ops.lbmaps.DeleteBackend(key); err != nil {
 			ops.log.Warn("Failed to delete from backend map", "error", err)
 		}
 	}
@@ -298,8 +305,8 @@ func (ops *bpfOps) pruneRestoredIDs() error {
 		}
 	}
 
-	ops.restoredServiceIDs.Clear()
-	ops.restoredBackendIDs.Clear()
+	ops.restoredServiceIDs = nil
+	ops.restoredBackendIDs = nil
 
 	return nil
 }
@@ -348,10 +355,6 @@ func (ops *bpfOps) Update(_ context.Context, _ statedb.ReadTxn, fe *Frontend) er
 		fe.Type == loadbalancer.SVCTypeHostPort && fe.Address.AddrCluster.IsUnspecified() {
 		// For NodePort create entries for each node address.
 		// For HostPort only create them if the address was not specified (HostIP is unset).
-		// TODO: HostPort loopback?
-		// TODO: When the nodeport addresses change trigger a full refresh by marking everything as
-		// pending?
-
 		old := sets.New(ops.nodePortAddrs[fe.Address.Port]...)
 		for _, addr := range fe.nodePortAddrs {
 			if fe.Address.IsIPv6() != addr.Is6() {
@@ -414,16 +417,15 @@ func (ops *bpfOps) updateFrontend(fe *Frontend) error {
 	svc := fe.Service()
 	flag := loadbalancer.NewSvcFlag(&loadbalancer.SvcFlagParam{
 		SvcType:          fe.Type,
+		SvcNatPolicy:     svc.NatPolicy,
 		SvcExtLocal:      svc.ExtTrafficPolicy == loadbalancer.SVCTrafficPolicyLocal,
 		SvcIntLocal:      svc.IntTrafficPolicy == loadbalancer.SVCTrafficPolicyLocal,
-		SvcNatPolicy:     svc.NatPolicy,
 		SessionAffinity:  svc.SessionAffinity,
 		IsRoutable:       isRoutable,
+		CheckSourceRange: len(svc.SourceRanges) > 0,
 		L7LoadBalancer:   svc.L7ProxyPort != 0,
 		LoopbackHostport: svc.LoopbackHostPort,
-
-		// TODO:
-		//CheckSourceRange: checkSourceRange,
+		Quarantined:      false,
 	})
 	svcVal.SetFlags(flag.UInt16())
 	svcVal.SetRevNat(int(feID))
@@ -568,7 +570,7 @@ func (ops *bpfOps) upsertMaster(svcKey lbmap.ServiceKey, svcVal lbmap.ServiceVal
 func (ops *bpfOps) cleanupSlots(svcKey lbmap.ServiceKey, oldCount, newCount int) error {
 	for i := newCount; i < oldCount; i++ {
 		svcKey.SetBackendSlot(i + 1)
-		_, err := svcKey.Map().SilentDelete(svcKey.ToNetwork())
+		err := ops.lbmaps.DeleteService(svcKey.ToNetwork())
 		if err != nil {
 			return fmt.Errorf("cleanup service slot %q: %w", svcKey.String(), err)
 		}
@@ -604,7 +606,7 @@ func (ops *bpfOps) deleteBackend(ipv6 bool, id loadbalancer.BackendID) error {
 	} else {
 		key = lbmap.NewBackend4KeyV3(id)
 	}
-	_, err := key.Map().SilentDelete(key)
+	err := ops.lbmaps.DeleteBackend(key)
 	if err != nil {
 		return fmt.Errorf("delete backend %d: %w", id, err)
 	}
@@ -622,7 +624,7 @@ func (ops *bpfOps) upsertAffinityMatch(id loadbalancer.ID, beID loadbalancer.Bac
 	}
 	var value lbmap.AffinityMatchValue
 	ops.log.Info("upsertAffinityMatch", "key", key)
-	return lbmap.AffinityMatchMap.Update(key.ToNetwork(), &value)
+	return ops.lbmaps.UpdateAffinityMatch(key.ToNetwork(), &value)
 }
 
 func (ops *bpfOps) deleteAffinityMatch(id loadbalancer.ID, beID loadbalancer.BackendID) error {
@@ -635,8 +637,7 @@ func (ops *bpfOps) deleteAffinityMatch(id loadbalancer.ID, beID loadbalancer.Bac
 		RevNATID:  uint16(id),
 	}
 	ops.log.Info("deleteAffinityMatch", "serviceID", id, "backendID", beID)
-	_, err := lbmap.AffinityMatchMap.SilentDelete(key.ToNetwork())
-	return err
+	return ops.lbmaps.DeleteAffinityMatch(key.ToNetwork())
 }
 
 func (ops *bpfOps) upsertRevNat(id loadbalancer.ID, svcKey lbmap.ServiceKey, svcVal lbmap.ServiceValue) error {

pkg/loadbalancer/experimental/bpf_reconciler_test.go
@@ -21,7 +21,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/logging"
@@ -821,27 +820,11 @@ func parseAddrPort(s string) loadbalancer.L3n4Addr {
 func TestBPFOps(t *testing.T) {
 	testutils.PrivilegedTest(t)
 
-	lbmap.Init(lbmap.InitParams{
-		IPv4:                     true,
-		IPv6:                     true,
-		MaxSockRevNatMapEntries:  1000,
-		ServiceMapMaxEntries:     1000,
-		BackEndMapMaxEntries:     1000,
-		RevNatMapMaxEntries:      1000,
-		AffinityMapMaxEntries:    1000,
-		SourceRangeMapMaxEntries: 1000,
-		MaglevMapMaxEntries:      1000,
-	})
-	require.NoError(t, lbmap.Service4MapV2.CreateUnpinned())
-	require.NoError(t, lbmap.Service6MapV2.CreateUnpinned())
-	require.NoError(t, lbmap.Backend4MapV3.CreateUnpinned())
-	require.NoError(t, lbmap.Backend6MapV3.CreateUnpinned())
-	require.NoError(t, lbmap.RevNat4Map.CreateUnpinned())
-	require.NoError(t, lbmap.RevNat6Map.CreateUnpinned())
-	require.NoError(t, lbmap.AffinityMatchMap.CreateUnpinned())
-
 	lc := hivetest.Lifecycle(t)
 	log := hivetest.Logger(t)
+
+	lbmaps := &realLBMaps{pinned: false}
+	lc.Append(lbmaps)
 
 	// Initialize the metrics registry. Otherwise the bpf.Map ops will incur a 1 second
 	// delay as they try to update the pressure gauge.
@@ -865,7 +848,7 @@ func TestBPFOps(t *testing.T) {
 		for _, addr := range frontendAddrs {
 			// For each set of test cases, use a fresh instance so each set gets
 			// fresh IDs.
-			ops := newBPFOps(lc, log, cfg, &realLBMaps{})
+			ops := newBPFOps(lc, log, cfg, lbmaps)
 			for _, testCase := range testCaseSet {
 				t.Run(testCase.name, func(t *testing.T) {
 					frontend := testCase.frontend
@@ -906,7 +889,7 @@ func TestBPFOps(t *testing.T) {
 						),
 						"Prune")
 
-					out := dump(addr, false)
+					out := dump(lbmaps, addr, false)
 					if !slices.Equal(out, testCase.maps) {
 						t.Fatalf("BPF map contents differ!\nexpected:\n%s\nactual:\n%s", showMaps(testCase.maps), showMaps(out))
 					}
@@ -914,7 +897,7 @@ func TestBPFOps(t *testing.T) {
 			}
 
 			// Verify that the BPF maps are empty after the test set.
-			assert.Empty(t, dump(addr, false), "BPF maps not empty")
+			assert.Empty(t, dump(lbmaps, addr, false), "BPF maps not empty")
 
 			// Verify that all internal state has been cleaned up.
 			assert.Empty(t, ops.backendIDAlloc.entities, "Backend ID allocations remain")
@@ -956,7 +939,7 @@ func sanitizeID[Num numeric](n Num, sanitize bool) string {
 }
 
 // dump the load-balancing maps into a concise format for assertions in tests.
-func dump(feAddr loadbalancer.L3n4Addr, sanitizeIDs bool) (out []mapDump) {
+func dump(lbmaps lbmaps, feAddr loadbalancer.L3n4Addr, sanitizeIDs bool) (out []mapDump) {
 	out = []string{}
 
 	replaceAddr := func(addr net.IP, port uint16) (s string) {
@@ -981,9 +964,7 @@ func dump(feAddr loadbalancer.L3n4Addr, sanitizeIDs bool) (out []mapDump) {
 		return
 	}
 
-	svcCB := func(key bpf.MapKey, value bpf.MapValue) {
-		svcKey := key.(lbmap.ServiceKey).ToHost()
-		svcValue := value.(lbmap.ServiceValue).ToHost()
+	svcCB := func(svcKey lbmap.ServiceKey, svcValue lbmap.ServiceValue) {
 		addr := svcKey.GetAddress()
 		addrS := replaceAddr(addr, svcKey.GetPort())
 		if svcKey.GetScope() == loadbalancer.ScopeInternal {
@@ -1000,12 +981,11 @@ func dump(feAddr loadbalancer.L3n4Addr, sanitizeIDs bool) (out []mapDump) {
 				", ", "+"),
 		))
 	}
-	lbmap.Service4MapV2.DumpWithCallback(svcCB)
-	lbmap.Service6MapV2.DumpWithCallback(svcCB)
+	if err := lbmaps.DumpService(svcCB); err != nil {
+		panic(err)
+	}
 
-	beCB := func(key bpf.MapKey, value bpf.MapValue) {
-		beKey := key.(lbmap.BackendKey)
-		beValue := value.(lbmap.BackendValue).ToHost()
+	beCB := func(beKey lbmap.BackendKey, beValue lbmap.BackendValue) {
 		addr := beValue.GetAddress()
 		addrS := addr.String()
 		if addr.To4() == nil {
@@ -1019,12 +999,11 @@ func dump(feAddr loadbalancer.L3n4Addr, sanitizeIDs bool) (out []mapDump) {
 			stateS,
 		))
 	}
-	lbmap.Backend4MapV3.DumpWithCallback(beCB)
-	lbmap.Backend6MapV3.DumpWithCallback(beCB)
+	if err := lbmaps.DumpBackend(beCB); err != nil {
+		panic(err)
+	}
 
-	revCB := func(key bpf.MapKey, value bpf.MapValue) {
-		revKey := key.(lbmap.RevNatKey).ToHost()
-		revValue := value.(lbmap.RevNatValue).ToHost()
+	revCB := func(revKey lbmap.RevNatKey, revValue lbmap.RevNatValue) {
 		var addr string
 
 		switch v := revValue.(type) {
@@ -1040,17 +1019,18 @@ func dump(feAddr loadbalancer.L3n4Addr, sanitizeIDs bool) (out []mapDump) {
 			addr,
 		))
 	}
-	lbmap.RevNat4Map.DumpWithCallback(revCB)
-	lbmap.RevNat6Map.DumpWithCallback(revCB)
+	if err := lbmaps.DumpRevNat(revCB); err != nil {
+		panic(err)
+	}
 
-	affCB := func(key bpf.MapKey, value bpf.MapValue) {
-		affKey := key.(*lbmap.AffinityMatchKey).ToHost()
+	affCB := func(affKey *lbmap.AffinityMatchKey, _ *lbmap.AffinityMatchValue) {
 		out = append(out, fmt.Sprintf("AFF: ID=%s BEID=%d",
 			sanitizeID(affKey.RevNATID, sanitizeIDs),
 			affKey.BackendID,
 		))
 	}
-	if err := lbmap.AffinityMatchMap.DumpWithCallback(affCB); err != nil {
+
+	if err := lbmaps.DumpAffinityMatch(affCB); err != nil {
 		panic(err)
 	}
 

pkg/loadbalancer/experimental/cell.go
@@ -35,7 +35,7 @@ var Cell = cell.Module(
 	ReconcilerCell,
 
 	// Provide [lbmaps], abstraction for the load-balancing BPF map access.
-	cell.ProvidePrivate(newLBMaps),
+	cell.ProvidePrivate(newLBMaps, newLBMapsConfig),
 )
 
 // TablesCell provides the [Writer] API for configuring load-balancing and the
@@ -64,11 +64,13 @@ var TablesCell = cell.Module(
 	),
 )
 
-func newLBMaps(w *Writer) lbmaps {
+func newLBMaps(lc cell.Lifecycle, cfg LBMapsConfig, w *Writer) lbmaps {
 	if !w.IsEnabled() {
 		return nil
 	}
-	return &realLBMaps{}
+	r := &realLBMaps{pinned: true, cfg: cfg}
+	lc.Append(r)
+	return r
 }
 
 type resourceIn struct {

pkg/loadbalancer/experimental/config.go
@@ -11,9 +11,8 @@ import (
 
 type Config struct {
 	EnableExperimentalLB bool
-
-	RetryBackoffMin time.Duration `mapstructure:"lb-retry-backoff-min"`
-	RetryBackoffMax time.Duration `mapstructure:"lb-retry-backoff-max"`
+	RetryBackoffMin      time.Duration `mapstructure:"lb-retry-backoff-min"`
+	RetryBackoffMax      time.Duration `mapstructure:"lb-retry-backoff-max"`
 }
 
 func (def Config) Flags(flags *pflag.FlagSet) {

pkg/loadbalancer/experimental/k8s_test.go
@@ -34,7 +34,6 @@ import (
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	slim_discovery_v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1"
 	slim_fake "github.com/cilium/cilium/pkg/k8s/slim/k8s/client/clientset/versioned/fake"
-	"github.com/cilium/cilium/pkg/maps/lbmap"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/testutils"
@@ -92,32 +91,10 @@ func deleteEvent[Obj k8sRuntime.Object](obj Obj) resource.Event[Obj] {
 func TestIntegrationK8s(t *testing.T) {
 	testutils.PrivilegedTest(t)
 
-	// TODO: clean up once there's a LBMap cell of sorts. Need
-	// support for unpinned maps though...?
-	lbmap.Init(lbmap.InitParams{
-		IPv4:                     true,
-		IPv6:                     true,
-		MaxSockRevNatMapEntries:  1000,
-		ServiceMapMaxEntries:     1000,
-		BackEndMapMaxEntries:     1000,
-		RevNatMapMaxEntries:      1000,
-		AffinityMapMaxEntries:    1000,
-		SourceRangeMapMaxEntries: 1000,
-		MaglevMapMaxEntries:      1000,
-	})
-
 	// TODO: Move this option somewhere sane.
 	option.Config.EnableK8sTerminatingEndpoint = true
 
 	log := hivetest.Logger(t)
-
-	require.NoError(t, lbmap.Service4MapV2.CreateUnpinned())
-	require.NoError(t, lbmap.Service6MapV2.CreateUnpinned())
-	require.NoError(t, lbmap.Backend4MapV3.CreateUnpinned())
-	require.NoError(t, lbmap.Backend6MapV3.CreateUnpinned())
-	require.NoError(t, lbmap.RevNat4Map.CreateUnpinned())
-	require.NoError(t, lbmap.RevNat6Map.CreateUnpinned())
-	require.NoError(t, lbmap.AffinityMatchMap.CreateUnpinned())
 
 	services := make(chan resource.Event[*slim_corev1.Service], 1)
 	services <- resource.Event[*slim_corev1.Service]{
@@ -141,6 +118,8 @@ func TestIntegrationK8s(t *testing.T) {
 		db     *statedb.DB
 		bo     *bpfOps
 	)
+
+	maps := &realLBMaps{pinned: false}
 
 	h := hive.New(
 		// FIXME.  Need this to avoid 1 second delay on metric operations.
@@ -171,9 +150,10 @@ func TestIntegrationK8s(t *testing.T) {
 			}),
 
 			cell.Provide(
-				func() lbmaps {
+				func(lc cell.Lifecycle) lbmaps {
+					lc.Append(maps)
 					return &faultyLBMaps{
-						impl:               &realLBMaps{},
+						impl:               maps,
 						failureProbability: 0.05, // 5% chance of failure.
 					}
 				},
@@ -298,7 +278,7 @@ func TestIntegrationK8s(t *testing.T) {
 			if !assert.Eventually(
 				t,
 				func() bool {
-					return checkTablesAndMaps(db, writer, testDataPath)
+					return checkTablesAndMaps(db, writer, maps, testDataPath)
 				},
 				5*time.Second,
 				10*time.Millisecond,
@@ -329,7 +309,7 @@ func TestIntegrationK8s(t *testing.T) {
 			if !assert.Eventually(
 				t,
 				func() bool {
-					lastDump = dump(frontendAddrs[0], true)
+					lastDump = dump(maps, frontendAddrs[0], true)
 					return len(lastDump) == 0
 				},
 				time.Minute,
@@ -364,7 +344,7 @@ func sanitizeTables(dump []byte) []byte {
 	return r.ReplaceAll(dump, []byte("(??? ago)"))
 }
 
-func checkTablesAndMaps(db *statedb.DB, writer *Writer, testDataPath string) bool {
+func checkTablesAndMaps(db *statedb.DB, writer *Writer, maps lbmaps, testDataPath string) bool {
 	var tableBuf bytes.Buffer
 	writer.DebugDump(db.ReadTxn(), &tableBuf)
 	actualTables := tableBuf.Bytes()
@@ -382,7 +362,7 @@ func checkTablesAndMaps(db *statedb.DB, writer *Writer, testDataPath string) boo
 	if expectedData, err := os.ReadFile(path.Join(testDataPath, "expected.maps")); err == nil {
 		expectedMaps = strings.Split(strings.TrimSpace(string(expectedData)), "\n")
 	}
-	actualMaps := dump(frontendAddrs[0], true)
+	actualMaps := dump(maps, frontendAddrs[0], true)
 
 	actualPath := path.Join(testDataPath, "actual.maps")
 	os.WriteFile(

commit 338dcb33dbb4c36f5998fef2f2477a39a23e2469

fix: SetBackends should always update service
When SetBackends received an empty slice [bes] of BackendParams,
it failed to refresh frontends of the service specified by [name]. This
omission is now fixed and a test for SetBackends is added which in
particular covers this issue.

Fixes: 485331fbae75f0a56d1f8657685532909aba3475
Signed-off-by: Damian Sawicki <dsawicki@google.com>

pkg/loadbalancer/experimental/writer.go
@@ -334,6 +334,7 @@ func (w *Writer) SetBackends(txn WriteTxn, name loadbalancer.ServiceName, source
 	if err != nil {
 		return err
 	}
+	refs = refs.Insert(name) // Even for empty bes, we need to refresh this service.
 
 	// Release orphaned backends, e.g. all backends from this source referencing this
 	// service.

pkg/loadbalancer/experimental/writer_test.go
@@ -444,3 +444,154 @@ func TestWriter_Initializers(t *testing.T) {
 	require.NotEmpty(t, p.BackendTable.PendingInitializers(firstTxn), "expected backends to be uninitialized")
 	require.NotEmpty(t, p.ServiceTable.PendingInitializers(firstTxn), "expected services to be uninitialized")
 }
+
+func TestSetBackends(t *testing.T) {
+	p := fixture(t)
+
+	name1 := loadbalancer.ServiceName{Namespace: "test", Name: "test1"}
+	name2 := loadbalancer.ServiceName{Namespace: "test", Name: "test2"}
+
+	feAddr1 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1231), 1231, loadbalancer.ScopeExternal)
+	feAddr2 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1232), 1232, loadbalancer.ScopeExternal)
+
+	beAddr1 := *loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(121), 4241, loadbalancer.ScopeExternal)
+	beAddr2 := *loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(122), 4242, loadbalancer.ScopeExternal)
+	beAddr3 := *loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(123), 4243, loadbalancer.ScopeExternal)
+
+	backend1 := experimental.BackendParams{L3n4Addr: beAddr1}
+	backend2 := experimental.BackendParams{L3n4Addr: beAddr2}
+	backend3 := experimental.BackendParams{L3n4Addr: beAddr3}
+
+	type testCase struct {
+		desc       string
+		action     func(*testing.T, *experimental.Writer, experimental.WriteTxn)
+		references map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool
+		existence  map[loadbalancer.L3n4Addr]bool
+	}
+	tcs := []testCase{
+		{
+			desc: "create two services and frontends",
+			action: func(t *testing.T, w *experimental.Writer, wtxn experimental.WriteTxn) {
+				_, err := w.UpsertService(wtxn, &experimental.Service{Name: name1})
+				require.NoError(t, err)
+				_, err = w.UpsertService(wtxn, &experimental.Service{Name: name2})
+				require.NoError(t, err)
+				_, err = w.UpsertFrontend(wtxn, experimental.FrontendParams{Address: *feAddr1, ServiceName: name1})
+				require.NoError(t, err)
+				_, err = w.UpsertFrontend(wtxn, experimental.FrontendParams{Address: *feAddr2, ServiceName: name2})
+				require.NoError(t, err)
+			},
+			references: map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool{
+				name1: {beAddr1: false, beAddr2: false, beAddr3: false},
+				name2: {beAddr1: false, beAddr2: false, beAddr3: false},
+			},
+			existence: map[loadbalancer.L3n4Addr]bool{beAddr1: false, beAddr2: false, beAddr3: false},
+		},
+		{
+			desc: "add all backends to first service",
+			action: func(t *testing.T, w *experimental.Writer, wtxn experimental.WriteTxn) {
+				require.NoError(t, w.SetBackends(wtxn, name1, source.Kubernetes, backend1, backend2, backend3))
+			},
+			references: map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool{
+				name1: {beAddr1: true, beAddr2: true, beAddr3: true},
+				name2: {beAddr1: false, beAddr2: false, beAddr3: false},
+			},
+			existence: map[loadbalancer.L3n4Addr]bool{beAddr1: true, beAddr2: true, beAddr3: true},
+		},
+		{
+			desc: "add all backends to second service",
+			action: func(t *testing.T, w *experimental.Writer, wtxn experimental.WriteTxn) {
+				require.NoError(t, w.SetBackends(wtxn, name2, source.Kubernetes, backend1, backend2, backend3))
+			},
+			references: map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool{
+				name1: {beAddr1: true, beAddr2: true, beAddr3: true},
+				name2: {beAddr1: true, beAddr2: true, beAddr3: true},
+			},
+			existence: map[loadbalancer.L3n4Addr]bool{beAddr1: true, beAddr2: true, beAddr3: true},
+		},
+		{
+			desc: "delete third backend from first service",
+			action: func(t *testing.T, w *experimental.Writer, wtxn experimental.WriteTxn) {
+				require.NoError(t, w.SetBackends(wtxn, name1, source.Kubernetes, backend1, backend2))
+			},
+			references: map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool{
+				name1: {beAddr1: true, beAddr2: true, beAddr3: false},
+				name2: {beAddr1: true, beAddr2: true, beAddr3: true},
+			},
+			existence: map[loadbalancer.L3n4Addr]bool{beAddr1: true, beAddr2: true, beAddr3: true},
+		},
+		{
+			desc: "delete first backend from both services",
+			action: func(t *testing.T, w *experimental.Writer, wtxn experimental.WriteTxn) {
+				require.NoError(t, w.SetBackends(wtxn, name1, source.Kubernetes, backend2))
+				require.NoError(t, w.SetBackends(wtxn, name2, source.Kubernetes, backend2, backend3))
+			},
+			references: map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool{
+				name1: {beAddr1: false, beAddr2: true, beAddr3: false},
+				name2: {beAddr1: false, beAddr2: true, beAddr3: true},
+			},
+			existence: map[loadbalancer.L3n4Addr]bool{beAddr1: false, beAddr2: true, beAddr3: true},
+		},
+		{
+			desc: "delete remaining two backends from second service",
+			action: func(t *testing.T, w *experimental.Writer, wtxn experimental.WriteTxn) {
+				require.NoError(t, w.SetBackends(wtxn, name2, source.Kubernetes))
+			},
+			references: map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool{
+				name1: {beAddr1: false, beAddr2: true, beAddr3: false},
+				name2: {beAddr1: false, beAddr2: false, beAddr3: false},
+			},
+			existence: map[loadbalancer.L3n4Addr]bool{beAddr1: false, beAddr2: true, beAddr3: false},
+		},
+		{
+			desc: "delete remaining backend from first service",
+			action: func(t *testing.T, w *experimental.Writer, wtxn experimental.WriteTxn) {
+				require.NoError(t, w.SetBackends(wtxn, name1, source.Kubernetes))
+				require.NoError(t, w.SetBackends(wtxn, name2, source.Kubernetes))
+			},
+			references: map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool{
+				name1: {beAddr1: false, beAddr2: false, beAddr3: false},
+				name2: {beAddr1: false, beAddr2: false, beAddr3: false},
+			},
+			existence: map[loadbalancer.L3n4Addr]bool{beAddr1: false, beAddr2: false, beAddr3: false},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.desc, func(t *testing.T) {
+			wtxn := p.Writer.WriteTxn()
+			tc.action(t, p.Writer, wtxn)
+			txn := wtxn.Commit()
+			for name, innerMap := range tc.references {
+				for addr, present := range innerMap {
+					fe, _, ok := p.Writer.Frontends().Get(txn, experimental.FrontendByServiceName(name)) // We assume only one frontend per service
+					require.True(t, ok)
+					if !present {
+						be, _, found := p.Writer.Backends().Get(txn, experimental.BackendByAddress(addr))
+						if found { // Backend should not exist...
+							_, foundForService := be.Instances.Get(name)
+							require.False(t, foundForService) // ...or not be associated with the service.
+						}
+						for _, b := range fe.Backends {
+							require.NotEqual(t, addr, b.L3n4Addr)
+						}
+					} else {
+						be, _, found := p.Writer.Backends().Get(txn, experimental.BackendByAddress(addr))
+						require.True(t, found)
+						_, foundForService := be.Instances.Get(name)
+						require.True(t, foundForService)
+						foundInFrontend := false
+						for _, b := range fe.Backends {
+							foundInFrontend = foundInFrontend || b.Backend.L3n4Addr == addr
+						}
+						require.True(t, foundInFrontend)
+					}
+				}
+			}
+			for addr, shouldExist := range tc.existence {
+				_, _, found := p.Writer.Backends().Get(txn, experimental.BackendByAddress(addr))
+				require.Equal(t, shouldExist, found, addr)
+			}
+		})
+	}
+}