a05fad0481cf4c5f75bdb9d4e59c574177096633
André Martins <andre@cilium.io>
2024-11-08 12:19:56 +0100

    metrics/features: enable standalone north-south LB
    Track if standalone NS LB is enabled in Cilium
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/metrics/features/metrics.go
+++ pkg/metrics/features/metrics.go
@@ -32,6 +32,7 @@ type Metrics struct {
 
 	ACLBTransparentEncryption       metric.Vec[metric.Gauge]
 	ACLBKubeProxyReplacementEnabled metric.Gauge
+	ACLBNodePortConfig              metric.Vec[metric.Gauge]
 }
 
 const (
@@ -115,6 +116,25 @@ var (
 	defaultEncryptionModes = []string{
 		advConnNetEncIPSec,
 		advConnNetEncWireGuard,
+	}
+
+	defaultNodePortModes = []string{
+		option.NodePortModeSNAT,
+		option.NodePortModeDSR,
+		option.NodePortModeAnnotation,
+		option.NodePortModeHybrid,
+	}
+
+	defaultNodePortModeAlgorithms = []string{
+		option.NodePortAlgMaglev,
+		option.NodePortAlgRandom,
+	}
+
+	defaultNodePortModeAccelerations = []string{
+		option.NodePortAccelerationDisabled,
+		option.NodePortAccelerationGeneric,
+		option.NodePortAccelerationBestEffort,
+		option.NodePortAccelerationNative,
 	}
 )
 
@@ -318,6 +338,44 @@ func NewMetrics(withDefaults bool) Metrics {
 			Subsystem: subsystemACLB,
 			Name:      "kube_proxy_replacement_enabled",
 		}),
+
+		ACLBNodePortConfig: metric.NewGaugeVecWithLabels(metric.GaugeOpts{
+			Help:      "Node Port configuration enabled on the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemACLB,
+			Name:      "node_port_configuration",
+		}, metric.Labels{
+			{
+				Name: "mode", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultNodePortModes...,
+					)
+				}(),
+			},
+			{
+				Name: "algorithm", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultNodePortModeAlgorithms...,
+					)
+				}(),
+			},
+			{
+				Name: "acceleration", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultNodePortModeAccelerations...,
+					)
+				}(),
+			},
+		}),
 	}
 }
 
@@ -402,4 +460,6 @@ func (m Metrics) update(params enabledFeatures, config *option.DaemonConfig) {
 	if config.KubeProxyReplacement == option.KubeProxyReplacementTrue {
 		m.ACLBKubeProxyReplacementEnabled.Add(1)
 	}
+
+	m.ACLBNodePortConfig.WithLabelValues(config.NodePortMode, config.NodePortAlg, config.NodePortAcceleration).Add(1)
 }
