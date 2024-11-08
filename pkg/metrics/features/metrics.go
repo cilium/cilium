592a3419ce379bb8e0813f9adbd42a5f9e5f0813
André Martins <andre@cilium.io>
2024-11-08 09:30:00 +0000

    metrics/features: enable HostFirewall
    [ upstream commit 6537393cd00ef48ed4dd5daf93dc90fc4b06c849 ]
    
    Track if HostFirewall is enabled in Cilium
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/metrics/features/metrics.go
+++ pkg/metrics/features/metrics.go
@@ -22,10 +22,13 @@ type Metrics struct {
 	DPIdentityAllocation          metric.Vec[metric.Gauge]
 	DPCiliumEndpointSlicesEnabled metric.Gauge
 	DPDeviceMode                  metric.Vec[metric.Gauge]
+
+	NPHostFirewallEnabled metric.Gauge
 }
 
 const (
 	subsystemDP = "feature_datapath"
+	subsystemNP = "feature_network_policies"
 )
 
 const (
@@ -209,6 +212,13 @@ func NewMetrics(withDefaults bool) Metrics {
 				}(),
 			},
 		}),
+
+		NPHostFirewallEnabled: metric.NewGauge(metric.GaugeOpts{
+			Help:      "Host firewall enabled on the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemNP,
+			Name:      "host_firewall_enabled",
+		}),
 	}
 }
 
@@ -254,4 +264,8 @@ func (m Metrics) update(params enabledFeatures, config *option.DaemonConfig) {
 
 	deviceMode := config.DatapathMode
 	m.DPDeviceMode.WithLabelValues(deviceMode).Add(1)
+
+	if config.EnableHostFirewall {
+		m.NPHostFirewallEnabled.Add(1)
+	}
 }
