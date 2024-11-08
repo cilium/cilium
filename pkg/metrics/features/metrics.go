da89d4aecd2e567da06b7bc071d9b15ae3dc007c
André Martins <andre@cilium.io>
2024-11-08 13:39:03 +0000

    metrics/features: enable BigTCP
    [ upstream commit a77e607215703441e80e1b478137ad947dd9c0c2 ]
    
    Track which protocol and if BigTCP is enabled in Cilium
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/metrics/features/metrics.go
+++ pkg/metrics/features/metrics.go
@@ -38,6 +38,7 @@ type Metrics struct {
 	ACLBSCTPEnabled                 metric.Gauge
 	ACLBVTEPEnabled                 metric.Gauge
 	ACLBCiliumEnvoyConfigEnabled    metric.Gauge
+	ACLBBigTCPEnabled               metric.Vec[metric.Gauge]
 }
 
 const (
@@ -64,6 +65,10 @@ const (
 
 	advConnNetEncIPSec     = "ipsec"
 	advConnNetEncWireGuard = "wireguard"
+
+	advConnBigTCPIPv4      = "ipv4-only"
+	advConnBigTCPIPv6      = "ipv6-only"
+	advConnBigTCPDualStack = "ipv4-ipv6-dual-stack"
 )
 
 var (
@@ -137,6 +142,12 @@ var (
 		option.NodePortAccelerationGeneric,
 		option.NodePortAccelerationBestEffort,
 		option.NodePortAccelerationNative,
+	}
+
+	defaultBigTCPAddressFamilies = []string{
+		advConnBigTCPIPv4,
+		advConnBigTCPIPv6,
+		advConnBigTCPDualStack,
 	}
 )
 
@@ -413,6 +424,24 @@ func NewMetrics(withDefaults bool) Metrics {
 			Subsystem: subsystemACLB,
 			Name:      "cilium_envoy_config_enabled",
 		}),
+
+		ACLBBigTCPEnabled: metric.NewGaugeVecWithLabels(metric.GaugeOpts{
+			Help:      "Big TCP enabled on the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemACLB,
+			Name:      "big_tcp_enabled",
+		}, metric.Labels{
+			{
+				Name: "address_family", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultBigTCPAddressFamilies...,
+					)
+				}(),
+			},
+		}),
 	}
 }
 
@@ -518,5 +547,19 @@ func (m Metrics) update(params enabledFeatures, config *option.DaemonConfig) {
 
 	if config.EnableEnvoyConfig {
 		m.ACLBCiliumEnvoyConfigEnabled.Add(1)
+	}
+
+	var bigTCPProto string
+	switch {
+	case params.BigTCPConfig().IsIPv4Enabled() && params.BigTCPConfig().IsIPv6Enabled():
+		bigTCPProto = advConnBigTCPDualStack
+	case params.BigTCPConfig().IsIPv4Enabled():
+		bigTCPProto = advConnBigTCPIPv4
+	case params.BigTCPConfig().IsIPv6Enabled():
+		bigTCPProto = advConnBigTCPIPv6
+	}
+
+	if bigTCPProto != "" {
+		m.ACLBBigTCPEnabled.WithLabelValues(bigTCPProto).Add(1)
 	}
 }
