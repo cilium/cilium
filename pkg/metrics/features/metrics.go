2c8e7648d8b9330c39beedeb24b6e3f41755bff9
André Martins <andre@cilium.io>
2024-11-08 13:51:50 +0000

    metrics/features: enable external EnvoyProxy
    [ upstream commit 13b7283bc47bd58ff057b26f3d57ec5e2ccbbbe0 ]
    
    Track if Envoy Proxy is enabled in Cilium
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/metrics/features/metrics.go
+++ pkg/metrics/features/metrics.go
@@ -41,6 +41,7 @@ type Metrics struct {
 	ACLBBigTCPEnabled               metric.Vec[metric.Gauge]
 	ACLBL2LBEnabled                 metric.Gauge
 	ACLBL2PodAnnouncementEnabled    metric.Gauge
+	ACLBExternalEnvoyProxyEnabled   metric.Vec[metric.Gauge]
 }
 
 const (
@@ -71,6 +72,9 @@ const (
 	advConnBigTCPIPv4      = "ipv4-only"
 	advConnBigTCPIPv6      = "ipv6-only"
 	advConnBigTCPDualStack = "ipv4-ipv6-dual-stack"
+
+	advConnExtEnvoyProxyStandalone = "standalone"
+	advConnExtEnvoyProxyEmbedded   = "embedded"
 )
 
 var (
@@ -150,6 +154,11 @@ var (
 		advConnBigTCPIPv4,
 		advConnBigTCPIPv6,
 		advConnBigTCPDualStack,
+	}
+
+	defaultExternalEnvoyProxyModes = []string{
+		advConnExtEnvoyProxyStandalone,
+		advConnExtEnvoyProxyEmbedded,
 	}
 )
 
@@ -458,6 +467,24 @@ func NewMetrics(withDefaults bool) Metrics {
 			Subsystem: subsystemACLB,
 			Name:      "l2_pod_announcement_enabled",
 		}),
+
+		ACLBExternalEnvoyProxyEnabled: metric.NewGaugeVecWithLabels(metric.GaugeOpts{
+			Help:      "Envoy Proxy mode enabled on the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemACLB,
+			Name:      "envoy_proxy_enabled",
+		}, metric.Labels{
+			{
+				Name: "mode", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultExternalEnvoyProxyModes...,
+					)
+				}(),
+			},
+		}),
 	}
 }
 
@@ -585,5 +612,11 @@ func (m Metrics) update(params enabledFeatures, config *option.DaemonConfig) {
 
 	if params.IsL2PodAnnouncementEnabled() {
 		m.ACLBL2PodAnnouncementEnabled.Add(1)
+	}
+
+	if config.ExternalEnvoyProxy {
+		m.ACLBExternalEnvoyProxyEnabled.WithLabelValues(advConnExtEnvoyProxyStandalone).Add(1)
+	} else {
+		m.ACLBExternalEnvoyProxyEnabled.WithLabelValues(advConnExtEnvoyProxyEmbedded).Add(1)
 	}
 }
