afadef138867893775125ded3fd0211c0d9e4204
André Martins <andre@cilium.io>
2024-11-12 11:14:57 +0000

    metrics/features/operator: enable Ingress Controller
    [ upstream commit dafc22887edf8cbec9a42d417d703a91c62cc80c ]
    
    Track if IngressController is enabled in Cilium Operator
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/metrics/features/operator/metrics_test.go
+++ pkg/metrics/features/operator/metrics_test.go
@@ -12,6 +12,11 @@ import (
 )
 
 type mockFeaturesParams struct {
+	IngressControllerEnabled bool
+}
+
+func (p mockFeaturesParams) IsIngressControllerEnabled() bool {
+	return p.IngressControllerEnabled
 }
 
 func TestUpdateGatewayAPI(t *testing.T) {
@@ -45,6 +50,41 @@ func TestUpdateGatewayAPI(t *testing.T) {
 
 			counterValue := metrics.ACLBGatewayAPIEnabled.Get()
 			assert.Equal(t, tt.expected, counterValue, "Expected value to be %.f for enabled: %t, got %.f", tt.expected, tt.enableGatewayAPI, counterValue)
+		})
+	}
+}
+
+func TestUpdateIngressControllerEnabled(t *testing.T) {
+	tests := []struct {
+		name                           string
+		enableIngressControllerEnabled bool
+		expected                       float64
+	}{
+		{
+			name:                           "IngressController enabled",
+			enableIngressControllerEnabled: true,
+			expected:                       1,
+		},
+		{
+			name:                           "IngressController disabled",
+			enableIngressControllerEnabled: false,
+			expected:                       0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			metrics := NewMetrics(true)
+			config := &option.OperatorConfig{}
+
+			params := mockFeaturesParams{
+				IngressControllerEnabled: tt.enableIngressControllerEnabled,
+			}
+
+			metrics.update(params, config)
+
+			counterValue := metrics.ACLBIngressControllerEnabled.Get()
+			assert.Equal(t, tt.expected, counterValue, "Expected value to be %.f for enabled: %t, got %.f", tt.expected, tt.enableIngressControllerEnabled, counterValue)
 		})
 	}
 }
