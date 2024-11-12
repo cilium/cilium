59369ee4d4ff0ae356c7a8c90d7ff3130485d405
André Martins <andre@cilium.io>
2024-11-12 12:14:57 +0100

    metrics/features/operator: enable Ingress Controller
    Track if IngressController is enabled in Cilium Operator
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/metrics/features/operator/cell.go
+++ pkg/metrics/features/operator/cell.go
@@ -10,6 +10,7 @@ import (
 	"github.com/cilium/hive/job"
 
 	operatorOption "github.com/cilium/cilium/operator/option"
+	"github.com/cilium/cilium/operator/pkg/ingress"
 	"github.com/cilium/cilium/pkg/metrics"
 )
 
@@ -41,7 +42,14 @@ type featuresParams struct {
 	Metrics   featureMetrics
 
 	OperatorConfig *operatorOption.OperatorConfig
+
+	IngressController ingress.IngressConfig
+}
+
+func (p featuresParams) IsIngressControllerEnabled() bool {
+	return p.IngressController.IsEnabled()
 }
 
 type enabledFeatures interface {
+	IsIngressControllerEnabled() bool
 }
