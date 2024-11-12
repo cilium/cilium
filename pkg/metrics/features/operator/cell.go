151da00b18ab15660bce741a0647f7d878958ee7
André Martins <andre@cilium.io>
2024-11-12 11:10:40 +0000

    metrics/features/operator: introduce features package to track enable features
    [ upstream commit 14963732ba0d0a0a2d4f47908cd5b423fe908852 ]
    
    With a dedicate package to detect which features are enabled, we will be
    able to report, in form of metrics, from the agents, which features are
    enabled. Once all features are tracked, it will give us the ability to
    track down which features are being tested in our CI and the ones that
    are lacking coverage.
    
    Signed-off-by: André Martins <andre@cilium.io>

---
+++ pkg/metrics/features/operator/cell.go
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package features
+
+import (
+	operatorOption "github.com/cilium/cilium/operator/option"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/hive/job"
+)
+
+// Cell will retrieve information from all other cells /
+// configuration to describe, in form of prometheus metrics, which
+// features are enabled on the operator.
+var Cell = cell.Module(
+	"enabled-features",
+	"Exports prometheus metrics describing which features are enabled in operator",
+
+	cell.Invoke(newOperatorConfigMetricOnStart),
+	cell.Provide(
+		func(m Metrics) featureMetrics {
+			return m
+		},
+	),
+	cell.Metric(func() Metrics {
+		return NewMetrics(true)
+	}),
+)
+
+type featuresParams struct {
+	cell.In
+
+	JobRegistry job.Registry
+	Health      cell.Health
+	Lifecycle   cell.Lifecycle
+	Metrics     featureMetrics
+
+	OperatorConfig *operatorOption.OperatorConfig
+}
+
+type enabledFeatures interface {
+}
