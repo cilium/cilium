41d496a5c85679748f627b9c97158bf750079112
André Martins <andre@cilium.io>
2024-11-12 11:10:40 +0000

    metrics/features/operator: introduce features package to track enable features
    [ upstream commit 14963732ba0d0a0a2d4f47908cd5b423fe908852 ]
    
    With a dedicate package to detect which features are enabled, we will be
    able to report, in form of metrics, from the agents, which features are
    enabled. Once all features are tracked, it will give us the ability to
    track down which features are being tested in our CI and the ones that
    are lacking coverage.
    
    Signed-off-by: André Martins <andre@cilium.io>

---
+++ pkg/metrics/features/operator/metrics.go
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package features
+
+import (
+	"github.com/cilium/cilium/operator/option"
+	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/metrics/metric"
+)
+
+type Metrics struct {
+	ACLBGatewayAPIEnabled metric.Gauge
+}
+
+const (
+	subsystemACLB = "feature_adv_connect_and_lb"
+)
+
+// NewMetrics returns all feature metrics. If 'withDefaults' is set, then
+// all metrics will have defined all of their possible values.
+func NewMetrics(withDefaults bool) Metrics {
+	return Metrics{
+		ACLBGatewayAPIEnabled: metric.NewGauge(metric.GaugeOpts{
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemACLB,
+			Help:      "GatewayAPI enabled on the operator",
+			Name:      "gateway_api_enabled",
+		}),
+	}
+}
+
+type featureMetrics interface {
+	update(params enabledFeatures, config *option.OperatorConfig)
+}
+
+func (m Metrics) update(params enabledFeatures, config *option.OperatorConfig) {
+	if config.EnableGatewayAPI {
+		m.ACLBGatewayAPIEnabled.Add(1)
+	}
+}
