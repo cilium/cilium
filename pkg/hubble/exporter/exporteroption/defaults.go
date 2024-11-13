b6145c2b256599a85fd23e125998c7e6524280b4
Alexandre Barone <abalexandrebarone@gmail.com>
2024-11-13 17:26:59 -0500

    hubble: allow providing custom encoder in flow exporter
    Signed-off-by: Alexandre Barone <abalexandrebarone@gmail.com>

--- pkg/hubble/exporter/exporteroption/defaults.go
+++ pkg/hubble/exporter/exporteroption/defaults.go
@@ -3,10 +3,21 @@
 
 package exporteroption
 
+import (
+	"encoding/json"
+	"io"
+)
+
 // Default specifies default values for Hubble exporter options.
 var Default = Options{
-	Path:       "", // An empty string disables Hubble export.
-	MaxSizeMB:  10,
-	MaxBackups: 5,
-	Compress:   false,
+	Path:           "", // An empty string disables Hubble export.
+	MaxSizeMB:      10,
+	MaxBackups:     5,
+	Compress:       false,
+	NewEncoderFunc: JsonEncoder,
+}
+
+// JsonEncoder is a NewEncoderFunc that returns a JSON encoder.
+func JsonEncoder(writer io.Writer) (Encoder, error) {
+	return json.NewEncoder(writer), nil
 }
