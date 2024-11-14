cilium/cilium

commit 897fcb7dcefc3ae8b4aca75db8a6dda0b2b60b97

daemon,cilium-dbg: Cilium shell
Cilium shell is a new simpler way for implementing debugging abilities
for the cilium-agent based on a simple script language. The same scripting
language can also be used to implement complex integration tests. By sharing
the same language for debugging and testing it becomes easier to write complex
integration tests (as the commands become familiar from debugging) and it makes
sure our debugging commands are actually tested.

Commands can be added by any cell by calling cell.Provide with hive.NewScriptCmd:

  var Cell = cell.Module(
    ...
    cell.Provide(newScriptCommand),
  )

  func newScriptCommand(t *Thing) hive.ScriptCmdOut {
    return hive.NewScriptCmd(
		"thing",
		script.Command(
			script.CmdUsage{Summary: "Inspect the thing"},
			func(s *script.State, args ...string) (script.WaitFunc, error) {
			  return func(*script.State) (stdout, stderr string, err error) {
			    stdout = fmt.Sprintf("Thing status: %s\n", t.Status())
				return
 			  }, nil
			},
		),
	)
  }

This can be now used in "cilium-dbg shell":

  $ kubectl exec -it -n kube-system ds/cilium -- cilium-dbg shell
  ...
  cilium> thing
  [stdout]
  Thing status: ...

The same command can be used in an integration test:

  func TestThing(t *testing.T) {
    ctx := context.Background()
    log := hivetest.Logger(t)
    h := hive.New(thing.Cell, <deps>...)
	engine := &script.Engine{
      Cmds: h.ScriptCommands(log),
	}
	require.NoError(t, h.Start(log, ctx), "Start")
	t.Cleanup(func() {
		assert.NoError(t, h.Stop(log, ctx), "Stop")
	})
    scripttest.Test(
	  t,
	  context.Background(),
	  func() *script.Engine { return engine },
	  []string{},
	  "testdata/*.txtar",
	)
  }

An example test script "testdata/test.txtar" count now look something like:

  thing
  grep '^Thing status: OK$'

Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/shell.go
@@ -1,0 +1,131 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cmd
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log/slog"
+	"net"
+	"os"
+	"sync"
+
+	upstreamHive "github.com/cilium/hive"
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+	"github.com/cilium/hive/script"
+
+	"github.com/cilium/cilium/pkg/defaults"
+)
+
+var shellCell = cell.Module(
+	"shell",
+	"Cilium debug shell",
+	cell.Invoke(registerShell),
+)
+
+func registerShell(in upstreamHive.ScriptCmds, log *slog.Logger, jg job.Group) {
+	cmds := in.Map()
+	for k, cmd := range script.DefaultCmds() {
+		cmds[k] = cmd
+	}
+	e := script.Engine{
+		Cmds:  cmds,
+		Conds: nil,
+	}
+	jg.Add(job.OneShot("listener", shell{jg, log, &e}.listener))
+}
+
+type shell struct {
+	jg     job.Group
+	log    *slog.Logger
+	engine *script.Engine
+}
+
+func (sh shell) listener(ctx context.Context, health cell.Health) error {
+	// Remove any old UNIX sock file from previous agent run.
+	os.Remove(defaults.ShellSockPath)
+
+	var lc net.ListenConfig
+	l, err := lc.Listen(ctx, "unix", defaults.ShellSockPath)
+	if err != nil {
+		return fmt.Errorf("failed to listen on %q: %w", defaults.ShellSockPath, err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		<-ctx.Done()
+		l.Close()
+		wg.Done()
+	}()
+	defer wg.Wait()
+
+	health.OK(fmt.Sprintf("Listening on %s", defaults.ShellSockPath))
+	connCount := 0
+	for ctx.Err() == nil {
+		conn, err := l.Accept()
+		if err != nil {
+			return fmt.Errorf("accept failed: %w", err)
+		}
+		sh.jg.Add(job.OneShot(
+			fmt.Sprintf("shell-%d", connCount),
+			func(ctx context.Context, _ cell.Health) error {
+				sh.handleConn(ctx, conn)
+				sh.log.Info("exited")
+				return nil
+			}))
+		connCount++
+	}
+	return nil
+}
+
+func (sh shell) handleConn(ctx context.Context, conn net.Conn) {
+	ctx, cancel := context.WithCancel(ctx)
+
+	// Wait for context cancellation in the background and close
+	// the connection if that happens. This allows teardown on
+	// errors or when parent context cancels.
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		<-ctx.Done()
+		conn.Close()
+		wg.Done()
+	}()
+	defer wg.Wait()
+	defer cancel()
+
+	s, err := script.NewState(ctx, "/tmp", nil)
+	if err != nil {
+		sh.log.Error("NewState", "error", err)
+		return
+	}
+
+	bio := bufio.NewReader(conn)
+	for {
+		bline, _, err := bio.ReadLine()
+		if err != nil {
+			break
+		}
+		line := string(bline)
+		switch line {
+		case "stop", "exit", "quit":
+			return
+		}
+		err = sh.engine.ExecuteLine(s, line, conn)
+		if err != nil {
+			_, err = fmt.Fprintln(conn, err)
+			if err != nil {
+				break
+			}
+		}
+		// Send the "end of command output" marker
+		_, err = fmt.Fprintln(conn, "<<end>>")
+		if err != nil {
+			break
+		}
+	}
+}

cilium-dbg/cmd/shell.go
@@ -1,0 +1,200 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cmd
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+
+	"github.com/cilium/hive/script"
+	"github.com/spf13/cobra"
+	"golang.org/x/term"
+
+	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/inctimer"
+	"github.com/cilium/cilium/pkg/version"
+)
+
+var shellCmd = &cobra.Command{
+	Use:   "shell [command] [args]...",
+	Short: "Connect to the Cilium shell",
+	Run:   shell,
+}
+
+var stdReadWriter = struct {
+	io.Reader
+	io.Writer
+}{
+	Reader: os.Stdin,
+	Writer: os.Stdout,
+}
+
+func init() {
+	RootCmd.AddCommand(shellCmd)
+}
+
+func dialShell(w io.Writer) (net.Conn, error) {
+	var conn net.Conn
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	for {
+		var err error
+		var d net.Dialer
+		conn, err = d.DialContext(ctx, "unix", defaults.ShellSockPath)
+		if err == nil {
+			break
+		}
+		// Dialing failed. Agent might not be fully up yet. Wait a bit and retry.
+		select {
+		case <-ctx.Done():
+			return nil, fmt.Errorf("dialing timed out: %w", err)
+		case <-inctimer.After(time.Second):
+			fmt.Fprintf(w, "Dialing failed: %s. Retrying...\n", err)
+		}
+	}
+	return conn, nil
+}
+
+func shellExchange(w io.Writer, format string, args ...any) error {
+	conn, err := dialShell(os.Stderr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	_, err = fmt.Fprintf(conn, format+"\nexit\n", args...)
+	if err != nil {
+		return err
+	}
+	bio := bufio.NewReader(conn)
+	for {
+		line, _, err := bio.ReadLine()
+		if err != nil {
+			return nil
+		}
+		switch string(line) {
+		case "<<end>>":
+			return nil
+		case "[stdout]":
+		default:
+			if _, err := w.Write(line); err != nil {
+				return err
+			}
+			if _, err := w.Write(newline); err != nil {
+				return err
+			}
+		}
+	}
+
+}
+
+func shell(_ *cobra.Command, args []string) {
+	if len(args) > 0 {
+		err := shellExchange(os.Stdout, "%s", strings.Join(args, " "))
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "error: %s\n", err)
+		}
+	} else {
+		interactiveShell()
+	}
+}
+
+func interactiveShell() {
+	// Try to set the terminal to raw mode (so that cursor keys work etc.)
+	restore, err := script.MakeRaw(0)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error setting terminal to raw mode: %s\n", err)
+	} else {
+		defer restore()
+	}
+
+	// Listen for SIGINT to break.
+	sigs := make(chan os.Signal, 1)
+	defer signal.Stop(sigs)
+	signal.Notify(sigs, os.Interrupt)
+
+	console := term.NewTerminal(stdReadWriter, "cilium> ")
+	printShellGreeting(console)
+
+	for {
+		// Try to dial the shell.sock. Since it takes a moment for the agent to come up and this
+		// is meant for interactive use we'll try to be helpful and retry the dialing until
+		// agent comes up.
+		conn, err := dialShell(console)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			os.Exit(1)
+		}
+
+		stop := make(chan struct{})
+
+		go func() {
+			select {
+			case <-stop:
+			case <-sigs:
+				// On interrupt close the connection.
+				conn.Close()
+			}
+		}()
+
+		bio := bufio.NewReader(conn)
+
+		// Read commands from the console and send them to the agent for execution.
+		// Stop on errors reading from the connection and redial. This allows interrupting
+		// long running commands with ctrl-c.
+		closed := false
+		for !closed {
+			line, err := console.ReadLine()
+			if err != nil {
+				close(stop)
+				return
+			}
+			if _, err = fmt.Fprintln(conn, line); err != nil {
+				close(stop)
+				return
+			}
+
+			// Read until the command finishes with <<end>>.
+			for {
+				line, _, err := bio.ReadLine()
+				if err != nil {
+					// Connection closed!
+					closed = true
+					break
+				}
+				if string(line) == "<<end>>" {
+					break
+				}
+				console.Write(line)
+				console.Write(newline)
+			}
+		}
+		close(stop)
+	}
+}
+
+func printShellGreeting(term *term.Terminal) {
+	var (
+		Red     = string(term.Escape.Red)
+		Yellow  = string(term.Escape.Yellow)
+		Blue    = string(term.Escape.Blue)
+		Green   = string(term.Escape.Green)
+		Magenta = string(term.Escape.Magenta)
+		Cyan    = string(term.Escape.Cyan)
+		Reset   = string(term.Escape.Reset)
+	)
+	fmt.Fprint(term, Yellow+"    /¯¯\\\n")
+	fmt.Fprint(term, Cyan+" /¯¯"+Yellow+"\\__/"+Green+"¯¯\\"+Reset+"\n")
+	fmt.Fprintf(term, Cyan+" \\__"+Red+"/¯¯\\"+Green+"__/"+Reset+"  Cilium %s\n", version.Version)
+	fmt.Fprint(term, Green+" /¯¯"+Red+"\\__/"+Magenta+"¯¯\\"+Reset+"  Welcome to the Cilium Shell! Type 'help' for list of commands.\n")
+	fmt.Fprint(term, Green+" \\__"+Blue+"/¯¯\\"+Magenta+"__/"+Reset+"\n")
+	fmt.Fprint(term, Blue+Blue+Blue+"    \\__/"+Reset+"\n")
+	fmt.Fprint(term, "\n")
+}

daemon/cmd/cells.go
@@ -132,6 +132,9 @@ var (
 		// Allows cells to wait for CRDs before trying to list Cilium resources.
 		// This is separate from k8sSynced.Cell as this one needs to be mocked for tests.
 		k8sSynced.CRDSyncCell,
+
+		// Shell for inspecting the agent. Listens on the 'shell.sock' UNIX socket.
+		shellCell,
 	)
 
 	// ControlPlane implement the per-node control functions. These are pure

pkg/defaults/defaults.go
@@ -72,6 +72,10 @@ const (
 	// SockPathEnv is the environment variable to overwrite SockPath
 	SockPathEnv = "CILIUM_SOCK"
 
+	// ShellSockPath is the path to the UNIX domain socket exposing the debug shell
+	// to which "cilium-dbg shell" connects to.
+	ShellSockPath = RuntimePath + "/shell.sock"
+
 	// MonitorSockPath1_2 is the path to the UNIX domain socket used to
 	// distribute BPF and agent events to listeners.
 	// This is the 1.2 protocol version.

commit bbe08f31422d86d732e8c008c27d270c2474dc2a

daemon: Catch panics in shell handler
To make sure that the shell commands cannot bring down the agent,
catch the panics in the handler. On panic log it and also send it
over the connection:

cilium> help tiuheot
PANIC: runtime error: invalid memory address or nil pointer dereference
goroutine 1684 [running]:
github.com/cilium/cilium/daemon/cmd.shell.handleConn.func2()
        .../cilium/cilium/daemon/cmd/shell.go:109 +0x85
panic({0x3cba7e0?, 0x75d9af0?})
        .../share/go/src/runtime/panic.go:785 +0x132
github.com/cilium/hive/script.(*Engine).ListCmds(0xc000d68a38, ...
        .../cilium/cilium/vendor/github.com/cilium/hive/script/engine.go:731 +0x14c
        ...

Fixes: 897fcb7dcefc ("daemon,cilium-dbg: Cilium shell")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/shell.go
@@ -10,6 +10,7 @@ import (
 	"log/slog"
 	"net"
 	"os"
+	"runtime"
 	"sync"
 
 	upstreamHive "github.com/cilium/hive"
@@ -83,6 +84,7 @@ func (sh shell) listener(ctx context.Context, health cell.Health) error {
 }
 
 func (sh shell) handleConn(ctx context.Context, conn net.Conn) {
+	const endMarker = "<<end>>"
 	ctx, cancel := context.WithCancel(ctx)
 
 	// Wait for context cancellation in the background and close
@@ -97,6 +99,18 @@ func (sh shell) handleConn(ctx context.Context, conn net.Conn) {
 	}()
 	defer wg.Wait()
 	defer cancel()
+
+	// Catch panics to make sure the script commands can't bring the agent down.
+	defer func() {
+		if err := recover(); err != nil {
+			// Log the panic and also write it to cilium-dbg. We keep processing
+			// more commands after this.
+			stack := make([]byte, 1024)
+			stack = stack[:runtime.Stack(stack, false)]
+			sh.log.Error("Panic in the shell handler", "error", err, "stack", stack)
+			fmt.Fprintf(conn, "PANIC: %s\n%s\n%s\n", err, stack, endMarker)
+		}
+	}()
 
 	s, err := script.NewState(ctx, "/tmp", nil)
 	if err != nil {
@@ -123,7 +137,7 @@ func (sh shell) handleConn(ctx context.Context, conn net.Conn) {
 			}
 		}
 		// Send the "end of command output" marker
-		_, err = fmt.Fprintln(conn, "<<end>>")
+		_, err = fmt.Fprintln(conn, endMarker)
 		if err != nil {
 			break
 		}