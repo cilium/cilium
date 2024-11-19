46843bc261f772381c6abf1ee8e9574c5d9a5cd3
Odin Ugedal <ougedal@palantir.com>
2024-11-19 11:51:53 +0100

    kvstore: Add mutex Lock and Unlock metrics
    Add metrics lock and unlock metrics in order to make it easier to
    understand how much time they take, and if there potentially is a lot of
    contention around these locks.
    
    This metric does not include the time it takes to potentially acquire a
    new session, or the time taken to grab the local kvstore lock for the
    given key.
    
    Signed-off-by: Odin Ugedal <ougedal@palantir.com>
    Signed-off-by: Odin Ugedal <odin@uged.al>

--- pkg/kvstore/etcd.go
+++ pkg/kvstore/etcd.go
@@ -377,10 +377,14 @@ type etcdClient struct {
 type etcdMutex struct {
 	mutex    *concurrency.Mutex
 	onUnlock func()
+	path     string
 }
 
-func (e *etcdMutex) Unlock(ctx context.Context) error {
+func (e *etcdMutex) Unlock(ctx context.Context) (err error) {
 	e.onUnlock()
+	defer func(duration *spanstat.SpanStat) {
+		increaseMetric(e.path, metricDelete, "Unlock", duration.EndError(err).Total(), err)
+	}(spanstat.Start())
 	return e.mutex.Unlock(ctx)
 }
 
@@ -712,7 +716,7 @@ func (e *etcdClient) sessionError() (err error) {
 	return
 }
 
-func (e *etcdClient) LockPath(ctx context.Context, path string) (KVLocker, error) {
+func (e *etcdClient) LockPath(ctx context.Context, path string) (locker KVLocker, err error) {
 	// Create the context first, so that the timeout also accounts for the time
 	// possibly required to acquire a new session (if not already established).
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
@@ -723,6 +727,9 @@ func (e *etcdClient) LockPath(ctx context.Context, path string) (KVLocker, error
 		return nil, Hint(err)
 	}
 
+	defer func(duration *spanstat.SpanStat) {
+		increaseMetric(path, metricSet, "Lock", duration.EndError(err).Total(), err)
+	}(spanstat.Start())
 	mu := concurrency.NewMutex(session, path)
 	err = mu.Lock(ctx)
 	if err != nil {
@@ -731,7 +738,7 @@ func (e *etcdClient) LockPath(ctx context.Context, path string) (KVLocker, error
 	}
 
 	release := func() { e.lockLeaseManager.Release(path) }
-	return &etcdMutex{mutex: mu, onUnlock: release}, nil
+	return &etcdMutex{mutex: mu, onUnlock: release, path: path}, nil
 }
 
 func (e *etcdClient) DeletePrefix(ctx context.Context, path string) (err error) {
