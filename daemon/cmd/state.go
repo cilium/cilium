a87facb959c65ec984c5a3c13543065c30056c54
Yusuke Suzuki <yusuke.suzuki@isovalent.com>
2024-11-19 19:34:41 +0900

    lrp: update LRP services with stale backends on agent restart
    [ upstream commit e2bd4a9141cb05eee9b6a351d3e25367d967c28d ]
    
    [ backporter's notes: hit minor conflicts due to different surrounding
      context, adapted as appropriate. ]
    
    This commit fixes the issue where stale backends are associated with
    the LRP service after the agent restarts.
    
    Cilium restores the service and backend cache from the BPF map
    and synchronizes it with the Kubernetes API server, assuming that
    UpsertService is called for each active service. During the sync period,
    Cilium keeps a list of restored backends that haven't been observed
    for each service to prevent temporary connectivity loss during agent
    restarts. (See commit 920976aeea72892057e669cd660611650be6b204.)
    
    After synchronization, an update is triggered for each service still
    associated with stale backends, allowing them to be removed. However,
    LRP services are not updated and remain associated with stale backends
    because the ServiceCache cannot update LRP services. Instead, the LRP
    manager is responsible for updating them.
    
    This issue arises if a CLRP is created during an agent restart.
    For example, consider a scenario where the following nodelocaldns CLRP
    is applied during agent startup:
    
    1) Cilium restores the kube-dns ClusterIP service and its backends (coredns)
       from the BPF map and synchronizes them with Kubernetes.
    2) If the LRP manager calls UpsertService first, it retains coredns, adds
       node-local-dns as a backend, and updates the kube-dns service to
       an LRP-type service.
    3) After synchronization, updates are triggered for all services. However,
       the LRP service is not updated, leaving stale backends associated with it.
    
    To address this issue, this commit ensures that the LRP manager calls
    EnsureService to remove stale backends.
    
    apiVersion: "cilium.io/v2"
    kind: CiliumLocalRedirectPolicy
    metadata:
      name: "nodelocaldns"
      namespace: kube-system
    spec:
      redirectFrontend:
        serviceMatcher:
          serviceName: kube-dns
          namespace: kube-system
      redirectBackend:
        localEndpointSelector:
          matchLabels:
            k8s-app: node-local-dns
        toPorts:
          - port: "53"
            name: dns
            protocol: UDP
          - port: "53"
            name: dns-tcp
            protocol: TCP
    
    Signed-off-by: Yusuke Suzuki <yusuke.suzuki@isovalent.com>

--- daemon/cmd/state.go
+++ daemon/cmd/state.go
@@ -482,6 +482,9 @@ func (d *Daemon) initRestore(restoredEndpoints *endpointRestoreState, endpointsR
 								swg := lock.NewStoppableWaitGroup()
 								for _, svc := range stale {
 									d.k8sWatcher.K8sSvcCache.EnsureService(svc, swg)
+									if option.Config.EnableLocalRedirectPolicy {
+										d.redirectPolicyManager.EnsureService(svc)
+									}
 								}
 
 								swg.Stop()
