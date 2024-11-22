c702414e5c8f26011dede3bdd97000180e0404e5
André Martins <andre@cilium.io>
2024-11-22 11:55:36 +0100

    metrics/features: enable CiliumEnvoyConfig
    Track if CiliumEnvoyConfig and CiliumClusterwideEnvoyConfig are deployed
    in Cilium
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/ciliumenvoyconfig/cec_manager.go
+++ pkg/ciliumenvoyconfig/cec_manager.go
@@ -46,6 +46,8 @@ type cecManager struct {
 
 	services  resource.Resource[*slim_corev1.Service]
 	endpoints resource.Resource[*k8s.Endpoints]
+
+	metricsManager CECMetrics
 }
 
 func newCiliumEnvoyConfigManager(logger logrus.FieldLogger,
@@ -57,6 +59,7 @@ func newCiliumEnvoyConfigManager(logger logrus.FieldLogger,
 	envoyConfigTimeout time.Duration,
 	services resource.Resource[*slim_corev1.Service],
 	endpoints resource.Resource[*k8s.Endpoints],
+	metricsManager CECMetrics,
 ) *cecManager {
 	return &cecManager{
 		logger:             logger,
@@ -68,10 +71,16 @@ func newCiliumEnvoyConfigManager(logger logrus.FieldLogger,
 		envoyConfigTimeout: envoyConfigTimeout,
 		services:           services,
 		endpoints:          endpoints,
+		metricsManager:     metricsManager,
 	}
 }
 
 func (r *cecManager) addCiliumEnvoyConfig(cecObjectMeta metav1.ObjectMeta, cecSpec *ciliumv2.CiliumEnvoyConfigSpec) error {
+	if cecObjectMeta.GetNamespace() == "" {
+		r.metricsManager.AddCCEC(cecSpec)
+	} else {
+		r.metricsManager.AddCEC(cecSpec)
+	}
 	resources, err := r.resourceParser.parseResources(
 		cecObjectMeta.GetNamespace(),
 		cecObjectMeta.GetName(),
@@ -325,6 +334,16 @@ func (r *cecManager) updateCiliumEnvoyConfig(
 	oldCECObjectMeta metav1.ObjectMeta, oldCECSpec *ciliumv2.CiliumEnvoyConfigSpec,
 	newCECObjectMeta metav1.ObjectMeta, newCECSpec *ciliumv2.CiliumEnvoyConfigSpec,
 ) error {
+	if oldCECObjectMeta.GetNamespace() == "" {
+		r.metricsManager.DelCCEC(oldCECSpec)
+	} else {
+		r.metricsManager.DelCEC(oldCECSpec)
+	}
+	if newCECObjectMeta.GetNamespace() == "" {
+		r.metricsManager.AddCCEC(newCECSpec)
+	} else {
+		r.metricsManager.AddCEC(newCECSpec)
+	}
 	oldResources, err := r.resourceParser.parseResources(
 		oldCECObjectMeta.GetNamespace(),
 		oldCECObjectMeta.GetName(),
@@ -434,6 +453,11 @@ func (r *cecManager) removeK8sServiceRedirects(resourceName service.L7LBResource
 }
 
 func (r *cecManager) deleteCiliumEnvoyConfig(cecObjectMeta metav1.ObjectMeta, cecSpec *ciliumv2.CiliumEnvoyConfigSpec) error {
+	if cecObjectMeta.GetNamespace() == "" {
+		r.metricsManager.DelCCEC(cecSpec)
+	} else {
+		r.metricsManager.DelCEC(cecSpec)
+	}
 	resources, err := r.resourceParser.parseResources(
 		cecObjectMeta.GetNamespace(),
 		cecObjectMeta.GetName(),
