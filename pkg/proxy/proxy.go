919c6d56a88e2f6e4e7597e14d9be15b2100fe73
Jarno Rajahalme <jarno@isovalent.com>
2024-11-25 16:29:23 +0100

    proxy: Do not error on static proxy port release
    [ upstream commit 4161e98b59e9edc285c2f9669c23ca874bb87b87 ]
    
    Do not return an error when releasing a static proxy port, but silently
    ignore it instead. This will remove unnecessary warning logs.
    
    Make release of a proxy port without any redirects return an error, so
    that releases must be paired with proxy redirect creation exactly, so
    that we do not accidentally release the port too soon.
    
    Move the port reuse delay to releaseProxyPort so that it can be issued
    only when needed on the last reference.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -264,29 +264,44 @@ func (p *Proxy) ackProxyPort(ctx context.Context, name string, pp *ProxyPort) er
 
 // releaseProxyPort() decreases the use count and frees the port if no users remain
 // Must be called with mutex held!
-func (p *Proxy) releaseProxyPort(name string) error {
+func (p *Proxy) releaseProxyPort(name string, portReuseWait time.Duration) error {
 	pp := p.proxyPorts[name]
 	if pp == nil {
 		return fmt.Errorf("Can't find proxy port %s", name)
 	}
 
-	pp.nRedirects--
 	if pp.nRedirects <= 0 {
-		if pp.isStatic {
-			return fmt.Errorf("Can't release proxy port: proxy %s on %d has a static listener", name, pp.ProxyPort)
-		}
-
-		log.WithField(fieldProxyRedirectID, name).Debugf("Delayed release of proxy port %d", pp.ProxyPort)
-
-		// Allow the port to be reallocated for other use if needed.
-		p.allocatedPorts[pp.ProxyPort] = false
-		pp.ProxyPort = 0
-		pp.configured = false
+		nRedirects := pp.nRedirects
 		pp.nRedirects = 0
+		return fmt.Errorf("Can't release proxy port with has non-positive reference count: %d", nRedirects)
+	}
 
-		// Leave the datapath rules behind on the hope that they get reused later.
-		// This becomes possible when we are able to keep the proxy listeners
-		// configured also when there are no redirects.
+	pp.nRedirects--
+
+	// Static proxy port is not released, dynamic proxy ports are released after a delay if
+	// still on last reference count
+	if !pp.isStatic && pp.nRedirects == 0 {
+		pp.nRedirects = 1 // keep the last reference during the delay
+		go func() {
+			time.Sleep(portReuseWait)
+
+			p.mutex.Lock()
+			defer p.mutex.Unlock()
+
+			pp.nRedirects--
+			if pp.nRedirects == 0 {
+				log.WithField(fieldProxyRedirectID, name).Debugf("Delayed release of proxy port %d", pp.ProxyPort)
+
+				// Allow the port to be reallocated for other use if needed.
+				p.allocatedPorts[pp.ProxyPort] = false
+				pp.ProxyPort = 0
+				pp.configured = false
+
+				// Leave the datapath rules behind on the hope that they get reused
+				// later.  This becomes possible when we are able to keep the proxy
+				// listeners configured also when there are no redirects.
+			}
+		}()
 	}
 
 	return nil
@@ -519,7 +534,7 @@ func (p *Proxy) ReleaseProxyPort(name string) error {
 	// Accessing pp.proxyPort requires the lock
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	return p.releaseProxyPort(name)
+	return p.releaseProxyPort(name, portReuseDelay)
 }
 
 // SetProxyPort() marks the proxy 'name' as successfully created with proxy port 'port'.
@@ -901,20 +916,16 @@ func (p *Proxy) removeRedirect(id string, wg *completion.WaitGroup) (error, reve
 		// break GC loop (implementation may point back to 'r')
 		r.implementation = nil
 
-		go func() {
-			time.Sleep(portReuseDelay)
-
-			p.mutex.Lock()
-			err := p.releaseProxyPort(listenerName)
-			p.mutex.Unlock()
-			if err != nil {
-				log.
-					WithField(fieldProxyRedirectID, id).
-					WithField("proxyPort", proxyPort).
-					WithError(err).
-					Warning("Releasing proxy port failed")
-			}
-		}()
+		p.mutex.Lock()
+		err := p.releaseProxyPort(listenerName, portReuseDelay)
+		p.mutex.Unlock()
+		if err != nil {
+			log.
+				WithField(fieldProxyRedirectID, id).
+				WithField("proxyPort", proxyPort).
+				WithError(err).
+				Warning("Releasing proxy port failed")
+		}
 	})
 
 	revertStack.Push(func() error {
