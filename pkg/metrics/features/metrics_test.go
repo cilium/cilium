57a0fe6e88a045f05329b6cb9818822df2a441f3
André Martins <andre@cilium.io>
2024-11-28 11:05:19 +0100

    metrics/features: fix NodePortConfig test case
    Even if NodePortConfig acceleration is disabled it doesn't mean that all
    other configurations are not set. Thus, we can remove this test case.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/metrics/features/metrics_test.go
+++ pkg/metrics/features/metrics_test.go
@@ -789,7 +789,7 @@ func TestUpdateKubeProxyReplacement(t *testing.T) {
 	}
 }
 
-func TestUpdateStandaloneNSLB(t *testing.T) {
+func TestUpdateNodePortConfig(t *testing.T) {
 	type testCase struct {
 		name             string
 		portMode         string
@@ -805,7 +805,7 @@ func TestUpdateStandaloneNSLB(t *testing.T) {
 		for _, algoMode := range defaultNodePortModeAlgorithms {
 			for _, aclMode := range defaultNodePortModeAccelerations {
 				tests = append(tests, testCase{
-					name:             fmt.Sprintf("NSLB %s - %s - %s", portMode, algoMode, aclMode),
+					name:             fmt.Sprintf("NodePortConfig %s - %s - %s", portMode, algoMode, aclMode),
 					portMode:         portMode,
 					algoMode:         algoMode,
 					accelerationMode: aclMode,
@@ -817,10 +817,6 @@ func TestUpdateStandaloneNSLB(t *testing.T) {
 			}
 		}
 	}
-	tests = append(tests, testCase{
-		name:             "NSLB disabled",
-		accelerationMode: option.NodePortAccelerationDisabled,
-	})
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
