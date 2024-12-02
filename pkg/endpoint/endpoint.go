cilium/cilium

commit 40130c7a208db8588040cba540ef0abc1563722a

endpoint: silence metadata resolver not found errors during restoration
During the agent bootstrap phase, it is possible to attempt restoring an
endpoint for a pod that has just been deleted asynchronously, but the
endpoint deletion event has not been received (as it has been queued
through the deletion file). If this race condition occurs, the metadata
resolution tasks would fail, because the pod does no longer exist, in
turn leading to error logs such as:

    level=error msg="EndpointNoTrackEvent event failed"

Let's lower the severity of these messages to info level, given that the
endpoint is going to be deleted anyways. Indeed, we are restoring an
existing endpoint, whose corresponding pod had existed in the past, and
the cache is guaranteed to be synced. The pod must have been deleted in
the meanwhile in case we are not seeing it anymore.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/endpoint/endpoint.go
@@ -20,6 +20,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
 	"go4.org/netipx"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	k8sTypes "k8s.io/apimachinery/pkg/types"
 
 	"github.com/cilium/cilium/api/v1/models"
@@ -1723,6 +1724,15 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 		return false, nil
 	}
 
+	filterResolveMetadataError := func(err error) error {
+		if restoredEndpoint && k8sErrors.IsNotFound(err) {
+			e.getLogger().WithError(err).Info("Unable to resolve metadata during endpoint restoration. Is the pod still running?")
+			return nil
+		}
+
+		return err
+	}
+
 	// copy the base labels into this local variable
 	// so that we don't override 'baseLabels'.
 	controllerBaseLabels := labels.NewFrom(baseLabels)
@@ -1732,6 +1742,10 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 	pod, k8sMetadata, err := resolveMetadata(ns, podName)
 	switch {
 	case err != nil:
+		if filterResolveMetadataError(err) == nil {
+			break
+		}
+
 		e.Logger(resolveLabels).WithError(err).Warning("Unable to fetch kubernetes labels")
 		fallthrough
 	case e.K8sUID != "" && e.K8sUID != string(pod.GetUID()):
@@ -1763,7 +1777,7 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 	e.UpdateNoTrackRules(func(_, _ string) (noTrackPort string, err error) {
 		po, _, err := resolveMetadata(ns, podName)
 		if err != nil {
-			return "", err
+			return "", filterResolveMetadataError(err)
 		}
 		value, _ := annotation.Get(po, annotation.NoTrack, annotation.NoTrackAlias)
 		return value, nil
@@ -1771,7 +1785,7 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 	e.UpdateBandwidthPolicy(bwm, func(ns, podName string) (bandwidthEgress string, err error) {
 		_, k8sMetadata, err := resolveMetadata(ns, podName)
 		if err != nil {
-			return "", err
+			return "", filterResolveMetadataError(err)
 		}
 		return k8sMetadata.Annotations[bandwidth.EgressBandwidth], nil
 	})

commit f7d59508aed93df8b72d897a3bf28be0e092b5c8

endpoint: fix panic if restored endpoint does no longer exist in k8s
The blamed commit was intended to silence not found errors during
restoration, which are expected if the corresponding pod has been
deleted asynchronously while the agent was down. However, it mistakenly
modified the execution flow, breaking out of the switch (rather than
returning) and eventually leading to accessing a nil pointer, with
the obvious ill-fated consequences.

Let's fix this by restoring the original execution flow, and add
a dedicated unit test to prevent regressions.

Fixes: 40130c7a208d ("endpoint: silence metadata resolver not found errors during restoration")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/endpoint/endpoint.go
@@ -1753,11 +1753,10 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 	pod, k8sMetadata, err := resolveMetadata(ns, podName)
 	switch {
 	case err != nil:
-		if filterResolveMetadataError(err) == nil {
-			break
+		if filterResolveMetadataError(err) != nil {
+			e.Logger(resolveLabels).WithError(err).Warning("Unable to fetch kubernetes labels")
 		}
 
-		e.Logger(resolveLabels).WithError(err).Warning("Unable to fetch kubernetes labels")
 		fallthrough
 	case e.K8sUID != "" && e.K8sUID != string(pod.GetUID()):
 		if err == nil {

pkg/endpoint/endpoint_test.go
@@ -5,12 +5,16 @@ package endpoint
 
 import (
 	"context"
+	"fmt"
 	"reflect"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	"github.com/cilium/cilium/api/v1/models"
 	fakeTypes "github.com/cilium/cilium/pkg/datapath/fake/types"
@@ -20,6 +24,8 @@ import (
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	ciliumio "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/labelsfilter"
@@ -799,4 +805,56 @@ func (e *Endpoint) getK8sPodLabels() labels.Labels {
 		}
 	}
 	return k8sEPPodLabels
+}
+
+func TestMetadataResolver(t *testing.T) {
+	s := setupEndpointSuite(t)
+
+	tests := []struct {
+		name            string
+		resolveMetadata MetadataResolverCB
+		assert          assert.ErrorAssertionFunc
+	}{
+		{
+			name: "pod not found",
+			resolveMetadata: func(ns, podName string) (pod *corev1.Pod, k8sMetadata *K8sMetadata, err error) {
+				return nil, nil, k8sErrors.NewNotFound(schema.GroupResource{Group: "core", Resource: "pod"}, "foo")
+			},
+			assert: assert.Error,
+		},
+		{
+			name: "pod uid mismatch",
+			resolveMetadata: func(ns, podName string) (pod *corev1.Pod, k8sMetadata *K8sMetadata, err error) {
+				return &corev1.Pod{ObjectMeta: metav1.ObjectMeta{
+					Namespace: "bar", Name: "foo", UID: "other",
+				}}, &K8sMetadata{IdentityLabels: labels.NewLabelsFromSortedList("k8s:foo=bar;k8s:qux=fred;")}, nil
+			},
+			assert: assert.Error,
+		},
+		{
+			name: "pod uid match",
+			resolveMetadata: func(ns, podName string) (pod *corev1.Pod, k8sMetadata *K8sMetadata, err error) {
+				return &corev1.Pod{ObjectMeta: metav1.ObjectMeta{
+					Namespace: "bar", Name: "foo", UID: "uid",
+				}}, &K8sMetadata{IdentityLabels: labels.NewLabelsFromSortedList("k8s:foo=bar;k8s:qux=fred;")}, nil
+			},
+			assert: assert.NoError,
+		},
+	}
+
+	for _, restored := range []bool{false, true} {
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%s (restored=%t)", tt.name, restored), func(t *testing.T) {
+				ctx, cancel := context.WithCancel(context.Background())
+				defer cancel()
+
+				ep := NewTestEndpointWithState(s, s, testipcache.NewMockIPCache(), &FakeEndpointProxy{},
+					testidentity.NewMockIdentityAllocator(nil), ctmap.NewFakeGCRunner(), 123, StateWaitingForIdentity)
+				ep.K8sNamespace, ep.K8sPodName, ep.K8sUID = "bar", "foo", "uid"
+
+				_, err := ep.metadataResolver(ctx, restored, true, labels.Labels{}, &fakeTypes.BandwidthManager{}, tt.resolveMetadata)
+				tt.assert(t, err)
+			})
+		}
+	}
 }