555907beb45250cbad5072e6ec928e96993e5b88
Casey Callendrello <cdc@isovalent.com>
2024-12-02 13:58:16 +0100

    cilium-cli: add cidrgroup-by-label test
    This is identical to the cidrgroup-by-name test, but instead references
    CIDRGroup by labels.
    
    Signed-off-by: Casey Callendrello <cdc@isovalent.com>

--- cilium-cli/connectivity/builder/client_egress_to_cidrgroup_deny.go
+++ cilium-cli/connectivity/builder/client_egress_to_cidrgroup_deny.go
@@ -30,3 +30,27 @@ func (t clientEgressToCidrgroupDeny) build(ct *check.ConnectivityTest, templates
 			return check.ResultDrop, check.ResultDrop
 		})
 }
+
+type clientEgressToCidrgroupDenyByLabel struct{}
+
+// same as above, but references CIDRGroups by label, rather than name
+func (t clientEgressToCidrgroupDenyByLabel) build(ct *check.ConnectivityTest, templates map[string]string) {
+	// This policy denies L3 traffic to ExternalCIDR except ExternalIP/32
+	// It does so using a CiliumCIDRGroup
+	newTest("client-egress-to-cidrgroup-deny-by-label", ct).
+		WithCiliumVersion(">=1.17.0").
+		WithCiliumPolicy(allowAllEgressPolicyYAML). // Allow all egress traffic
+		WithCiliumPolicy(templates["clientEgressToCIDRGroupExternalDenyLabelPolicyYAML"]).
+		WithScenarios(
+			tests.PodToCIDR(tests.WithRetryDestIP(ct.Params().ExternalIP)), // Denies all traffic to ExternalOtherIP, but allow ExternalIP
+		).
+		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
+			if a.Destination().Address(features.GetIPFamily(ct.Params().ExternalOtherIP)) == ct.Params().ExternalOtherIP {
+				return check.ResultPolicyDenyEgressDrop, check.ResultNone
+			}
+			if a.Destination().Address(features.GetIPFamily(ct.Params().ExternalIP)) == ct.Params().ExternalIP {
+				return check.ResultOK, check.ResultNone
+			}
+			return check.ResultDrop, check.ResultDrop
+		})
+}
