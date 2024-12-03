2135b095d4779441aaaa20f5bf7d763419fffc83
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-12-03 10:21:11 +0100

    node: remove refresh parameter from NodeNeighborRefresh
    This commit removes the unused parameter `refresh` and logic
    from the method `NodeNeighborRefresh`. Except for tests, it's
    always passed as `false`.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/datapath/types/node.go
+++ pkg/datapath/types/node.go
@@ -239,7 +239,7 @@ type NodeNeighbors interface {
 	NodeNeighDiscoveryEnabled() bool
 
 	// NodeNeighborRefresh is called to refresh node neighbor table
-	NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node, refresh bool) error
+	NodeNeighborRefresh(ctx context.Context, node nodeTypes.Node) error
 
 	// NodeCleanNeighbors cleans all neighbor entries for the direct routing device
 	// and the encrypt interface.
@@ -250,7 +250,7 @@ type NodeNeighbors interface {
 	// be added, for example, for external service backends.
 	InsertMiscNeighbor(newNode *nodeTypes.Node)
 
-	// DeleteMiscNeighbor delets a eighbor entry for the address passed via oldNode.
+	// DeleteMiscNeighbor deletes a neighbor entry for the address passed via oldNode.
 	// This is needed to delete the entries which have been inserted at an earlier
 	// point in time through InsertMiscNeighbor.
 	DeleteMiscNeighbor(oldNode *nodeTypes.Node)
