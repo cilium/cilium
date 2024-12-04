89211badc0602a20c17eb9e78befd0f48d4c1c51
Fabio Falzoi <fabio.falzoi@isovalent.com>
2024-12-04 17:27:09 +0100

    daemon: Remove checks for multi-pool IPAM and tunneling
    As a consequence of using the ipcache map to support tunneling,
    multi-pool IPAM is now compatible with it, thus lift the startup checks
    that stops the agent when both the features are enabled.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -1328,10 +1328,6 @@ func initEnv(vp *viper.Viper) {
 		option.Config.EncryptInterface = append(option.Config.EncryptInterface, link)
 	}
 
-	if option.Config.TunnelingEnabled() && option.Config.EnableAutoDirectRouting {
-		log.Fatalf("%s cannot be used with tunneling. Packets must be routed through the tunnel device.", option.EnableAutoDirectRoutingName)
-	}
-
 	initClockSourceOption()
 
 	if option.Config.EnableSRv6 {
@@ -1397,9 +1393,6 @@ func initEnv(vp *viper.Viper) {
 	}
 
 	if option.Config.IPAM == ipamOption.IPAMMultiPool {
-		if option.Config.TunnelingEnabled() {
-			log.Fatalf("Cannot specify IPAM mode %s in tunnel mode.", option.Config.IPAM)
-		}
 		if option.Config.EnableIPSec {
 			log.Fatalf("Cannot specify IPAM mode %s with %s.", option.Config.IPAM, option.EnableIPSecName)
 		}
