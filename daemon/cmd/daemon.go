cilium/cilium

commit f6606c6ccdb80c187243ec8359ddfb037aa75969

daemon,endpoint,cni: Pass pod UID through CNI ADD
Building off of the previous commit, This commit modifies if the CNI ADD
handling logic to pass the Kubernetes Pod UID in the ADD request to the
Agent. In addition, a extra condition checks whether the UID of the pod
corresponding to the CNI ADD request is the same UID which Cilium is
aware of (within its pod informer store) during endpoint creation. This
is key for handling StatefulSets properly, see [1].

If the UID is outdated, this means that Cilium has not yet received the
updated pod event that corresponds with the CNI ADD. In this case,
Cilium will attempt to query the pod store up for to 2 seconds. If the
pod store does not have the altest pod reference after 2 seconds, Cilium
will trigger a direct fetch from the apiserver, as to avoid a
potentially large window of time where an endpoint cannot be created. If
the direct fetch fails, the endpoint will be created with the 'init'
identity until the metadata resolver (controller) is able to fetch the
latest pod metadata.

The reason for these changes is due to the way StatefulSets are
implemented in Kubernetes. One significant property of StatefulSets is
that they have a fixed name. When a StatefulSet is restarted, Cilium is
unable to properly handle it because Cilium indexes based on
namespace/name[1]. Therefore, we must pass a UID through the CNI ADD in
order to ensure that when Cilium creates a new endpoint, it is created
with the most up-to-date corresponding Kubernetes metadata.

[1]: https://github.com/cilium/cilium/issues/30409, see case 1.

Signed-off-by: Chris Tarazi <chris@isovalent.com>

daemon/cmd/endpoint.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
@@ -40,6 +41,7 @@ import (
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/proxy"
+	"github.com/cilium/cilium/pkg/resiliency"
 	"github.com/cilium/cilium/pkg/time"
 )
 
@@ -361,6 +363,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, e
 		"datapathConfiguration":      epTemplate.DatapathConfiguration,
 		logfields.Interface:          epTemplate.InterfaceName,
 		logfields.K8sPodName:         epTemplate.K8sNamespace + "/" + epTemplate.K8sPodName,
+		logfields.K8sUID:             epTemplate.K8sUID,
 		logfields.Labels:             epTemplate.Labels,
 		"sync-build":                 epTemplate.SyncBuildEndpoint,
 	}).Info("Create endpoint request")
@@ -434,7 +437,30 @@ func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, e
 	identityLbls := maps.Clone(apiLabels)
 
 	if ep.K8sNamespaceAndPodNameIsSet() && d.clientset.IsEnabled() {
-		pod, k8sMetadata, err := d.fetchK8sMetadataForEndpoint(ep.K8sNamespace, ep.K8sPodName)
+		pod, k8sMetadata, err := d.handleOutdatedPodInformer(ctx, ep)
+		if errors.Is(err, podStoreOutdatedErr) {
+			log.WithFields(logrus.Fields{
+				logfields.K8sPodName: ep.K8sNamespace + "/" + ep.K8sPodName,
+				logfields.K8sUID:     ep.K8sUID,
+			}).Warn("Timeout occurred waiting for Pod store, fetching latest Pod via the apiserver.")
+
+			// Fetch the latest instance of the pod because
+			// fetchK8sMetadataForEndpoint() returned a stale pod from the
+			// store. If there's a mismatch in UIDs, this is an indication of a
+			// StatefulSet workload that was restarted on the local node and we
+			// must handle it as a special case. See GH-30409.
+			if newPod, err2 := d.clientset.Slim().CoreV1().Pods(ep.K8sNamespace).Get(
+				ctx, ep.K8sPodName, metav1.GetOptions{},
+			); err2 != nil {
+				ep.Logger("api").WithError(err2).Warn(
+					"Failed to fetch Kubernetes Pod during detection of an outdated Pod UID. Endpoint will be created with the 'init' identity. " +
+						"The Endpoint will be updated with a real identity once the Kubernetes can be fetched.")
+				err = errors.Join(err, err2)
+			} else {
+				pod = newPod
+			}
+		}
+
 		if err != nil {
 			ep.Logger("api").WithError(err).Warning("Unable to fetch kubernetes labels")
 		} else {
@@ -563,6 +589,40 @@ func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, e
 
 	return ep, 0, nil
 }
+
+func (d *Daemon) handleOutdatedPodInformer(
+	ctx context.Context,
+	ep *endpoint.Endpoint,
+) (pod *slim_corev1.Pod, k8sMetadata *endpoint.K8sMetadata, err error) {
+	var once sync.Once
+
+	// Average attempt is every 100ms.
+	err = resiliency.Retry(ctx, 2*time.Second, 20, func(_ context.Context, _ int) (bool, error) {
+		var err2 error
+		pod, k8sMetadata, err2 = d.fetchK8sMetadataForEndpoint(ep.K8sNamespace, ep.K8sPodName)
+		if ep.K8sUID == "" {
+			// If the CNI did not set the UID, then don't retry and just exit
+			// out of the loop to proceed as normal.
+			return true, nil
+		}
+
+		if pod != nil && ep.K8sUID != string(pod.GetUID()) {
+			once.Do(func() {
+				log.WithFields(logrus.Fields{
+					logfields.K8sPodName: ep.K8sNamespace + "/" + ep.K8sPodName,
+					logfields.K8sUID:     ep.K8sUID,
+				}).Warn("Detected outdated Pod UID during Endpoint creation. Endpoint creation cannot proceed with an outdated Pod store. Attempting to fetch latest Pod.")
+			})
+
+			return false, podStoreOutdatedErr
+		}
+		return true, err2
+	})
+
+	return pod, k8sMetadata, err
+}
+
+var podStoreOutdatedErr = errors.New("pod store outdated")
 
 func putEndpointIDHandler(d *Daemon, params PutEndpointIDParams) (resp middleware.Responder) {
 	if ep := params.Endpoint; ep != nil {

pkg/endpoint/endpoint.go
@@ -291,6 +291,10 @@ type Endpoint struct {
 	// Immutable after Endpoint creation.
 	K8sNamespace string
 
+	// K8sUID is the Kubernetes UID of the pod. Passed directly from the CNI.
+	// Immutable after Endpoint creation.
+	K8sUID string
+
 	// pod
 	pod atomic.Pointer[slim_corev1.Pod]
 
@@ -1264,6 +1268,14 @@ func (e *Endpoint) GetK8sNamespace() string {
 	return ns
 }
 
+// GetK8sUID returns the UID of the pod if the endpoint represents a Kubernetes
+// pod.
+func (e *Endpoint) GetK8sUID() string {
+	// const after creation
+	uid := e.K8sUID
+	return uid
+}
+
 // SetPod sets the pod related to this endpoint.
 func (e *Endpoint) SetPod(pod *slim_corev1.Pod) {
 	e.pod.Store(pod)
@@ -1722,8 +1734,14 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 	ns, podName := e.GetK8sNamespace(), e.GetK8sPodName()
 
 	pod, k8sMetadata, err := resolveMetadata(ns, podName)
-	if err != nil {
+	switch {
+	case err != nil:
 		e.Logger(resolveLabels).WithError(err).Warning("Unable to fetch kubernetes labels")
+		fallthrough
+	case e.K8sUID != "" && e.K8sUID != string(pod.GetUID()):
+		if err == nil {
+			err = errors.New("metadata resolver: pod store out-of-date")
+		}
 		// If we were unable to fetch the k8s endpoints then
 		// we will mark the endpoint with the init identity.
 		if !restoredEndpoint {

plugins/cilium-cni/cmd/endpoint.go
@@ -78,6 +78,7 @@ func (c *defaultEndpointConfiguration) PrepareEndpoint(ipam *models.IPAMResponse
 		Addressing:             &models.AddressPair{},
 		K8sPodName:             string(c.CniArgs.K8S_POD_NAME),
 		K8sNamespace:           string(c.CniArgs.K8S_POD_NAMESPACE),
+		K8sUID:                 string(c.CniArgs.K8S_POD_UID),
 		ContainerInterfaceName: c.Args.IfName,
 		DatapathConfiguration:  &models.EndpointDatapathConfiguration{},
 	}

api/v1/models/endpoint_change_request.go
@@ -67,6 +67,9 @@ type EndpointChangeRequest struct {
 	// Kubernetes pod name
 	K8sPodName string `json:"k8s-pod-name,omitempty"`
 
+	// Kubernetes pod UID
+	K8sUID string `json:"k8s-uid,omitempty"`
+
 	// Labels describing the identity
 	Labels Labels `json:"labels,omitempty"`
 

api/v1/openapi.yaml
@@ -1479,6 +1479,9 @@ definitions:
       k8s-namespace:
         description: Kubernetes namespace name
         type: string
+      k8s-uid:
+        description: Kubernetes pod UID
+        type: string
       datapath-map-id:
         description: ID of datapath tail call map
         type: integer

api/v1/server/embedded_spec.go
@@ -2925,6 +2925,10 @@ func init() {
           "description": "Kubernetes pod name",
           "type": "string"
         },
+        "k8s-uid": {
+          "description": "Kubernetes pod UID",
+          "type": "string"
+        },
         "labels": {
           "description": "Labels describing the identity",
           "$ref": "#/definitions/Labels"
@@ -8766,6 +8770,10 @@ func init() {
         },
         "k8s-pod-name": {
           "description": "Kubernetes pod name",
+          "type": "string"
+        },
+        "k8s-uid": {
+          "description": "Kubernetes pod UID",
           "type": "string"
         },
         "labels": {

pkg/endpoint/api.go
@@ -79,6 +79,7 @@ func NewEndpointFromChangeModel(ctx context.Context, owner regeneration.Owner, p
 	ep.dockerEndpointID = base.DockerEndpointID
 	ep.K8sPodName = base.K8sPodName
 	ep.K8sNamespace = base.K8sNamespace
+	ep.K8sUID = base.K8sUID
 	ep.disableLegacyIdentifiers = base.DisableLegacyIdentifiers
 
 	if base.Mac != "" {

pkg/endpoint/restore.go
@@ -410,6 +410,7 @@ func (e *Endpoint) toSerializedEndpoint() *serializableEndpoint {
 		DNSZombies:               e.DNSZombies,
 		K8sPodName:               e.K8sPodName,
 		K8sNamespace:             e.K8sNamespace,
+		K8sUID:                   e.K8sUID,
 		DatapathConfiguration:    e.DatapathConfiguration,
 		CiliumEndpointUID:        e.ciliumEndpointUID,
 		Properties:               e.properties,
@@ -512,6 +513,9 @@ type serializableEndpoint struct {
 	// K8sNamespace is the Kubernetes namespace of the endpoint
 	K8sNamespace string
 
+	// K8sUID is the Kubernetes pod UID of the endpoint
+	K8sUID string
+
 	// DatapathConfiguration is the endpoint's datapath configuration as
 	// passed in via the plugin that created the endpoint, e.g. the CNI
 	// plugin which performed the plumbing will enable certain datapath
@@ -579,6 +583,7 @@ func (ep *Endpoint) fromSerializedEndpoint(r *serializableEndpoint) {
 	ep.DNSZombies = r.DNSZombies
 	ep.K8sPodName = r.K8sPodName
 	ep.K8sNamespace = r.K8sNamespace
+	ep.K8sUID = r.K8sUID
 	ep.DatapathConfiguration = r.DatapathConfiguration
 	ep.Options = r.Options
 	ep.ciliumEndpointUID = r.CiliumEndpointUID

commit 8d82f8ff1046fc795e379719db84be0506d30caf

endpoint: compute updated metadata on pod UID mismatch
The blamed commit introduced a fallback logic to retrieve the pod
corresponding to a given endpoint directly from k8s if the store
is out of date, and contains an entry with a mismatching UID. However,
the metadata is not recomputed based on the newly retrieved pod,
hence potentially configuring incorrect values. Let's get this fixed.

Fixes: f6606c6ccdb8 ("daemon,endpoint,cni: Pass pod UID through CNI ADD")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

daemon/cmd/daemon.go
@@ -902,5 +902,6 @@ func (d *Daemon) SendNotification(notification monitorAPI.AgentNotifyMessage) er
 }
 
 type endpointMetadataFetcher interface {
-	Fetch(nsName, podName string) (*slim_corev1.Namespace, *slim_corev1.Pod, error)
+	FetchNamespace(nsName string) (*slim_corev1.Namespace, error)
+	FetchPod(nsName, podName string) (*slim_corev1.Pod, error)
 }

daemon/cmd/endpoint.go
@@ -178,15 +178,25 @@ func getEndpointIDHandler(d *Daemon, params GetEndpointIDParams) middleware.Resp
 // The returned pod is deepcopied which means the its fields can be written
 // into.
 func (d *Daemon) fetchK8sMetadataForEndpoint(nsName, podName string) (*slim_corev1.Pod, *endpoint.K8sMetadata, error) {
-	ns, p, err := d.endpointMetadataFetcher.Fetch(nsName, podName)
+	p, err := d.endpointMetadataFetcher.FetchPod(nsName, podName)
 	if err != nil {
 		return nil, nil, err
+	}
+
+	metadata, err := d.fetchK8sMetadataForEndpointFromPod(p)
+	return p, metadata, err
+}
+
+func (d *Daemon) fetchK8sMetadataForEndpointFromPod(p *slim_corev1.Pod) (*endpoint.K8sMetadata, error) {
+	ns, err := d.endpointMetadataFetcher.FetchNamespace(p.Namespace)
+	if err != nil {
+		return nil, err
 	}
 
 	containerPorts, lbls, annotations := k8s.GetPodMetadata(ns, p)
 	k8sLbls := labels.Map2Labels(lbls, labels.LabelSourceK8s)
 	identityLabels, infoLabels := labelsfilter.Filter(k8sLbls)
-	return p, &endpoint.K8sMetadata{
+	return &endpoint.K8sMetadata{
 		ContainerPorts: containerPorts,
 		IdentityLabels: identityLabels,
 		InfoLabels:     infoLabels,
@@ -198,16 +208,12 @@ type cachedEndpointMetadataFetcher struct {
 	k8sWatcher *watchers.K8sWatcher
 }
 
-func (cemf *cachedEndpointMetadataFetcher) Fetch(nsName, podName string) (*slim_corev1.Namespace, *slim_corev1.Pod, error) {
-	p, err := cemf.k8sWatcher.GetCachedPod(nsName, podName)
-	if err != nil {
-		return nil, nil, err
-	}
-	ns, err := cemf.k8sWatcher.GetCachedNamespace(nsName)
-	if err != nil {
-		return nil, nil, err
-	}
-	return ns, p, err
+func (cemf *cachedEndpointMetadataFetcher) FetchNamespace(nsName string) (*slim_corev1.Namespace, error) {
+	return cemf.k8sWatcher.GetCachedNamespace(nsName)
+}
+
+func (cemf *cachedEndpointMetadataFetcher) FetchPod(nsName, podName string) (*slim_corev1.Pod, error) {
+	return cemf.k8sWatcher.GetCachedPod(nsName, podName)
 }
 
 func invalidDataError(ep *endpoint.Endpoint, err error) (*endpoint.Endpoint, int, error) {
@@ -454,9 +460,9 @@ func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, e
 				err = errors.Join(err, err2)
 			} else {
 				pod = newPod
-				// Clear the error so the code can proceed below as we've
-				// succeeded here.
-				err = nil
+				// Clear the error so the code can proceed below, if the metadata
+				// retrieval succeeds correctly.
+				k8sMetadata, err = d.fetchK8sMetadataForEndpointFromPod(pod)
 			}
 		}
 