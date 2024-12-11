cilium/cilium

commit 849809274b8b0e84d2e9da9833a696fd3efed8c8

connectivity: Add secondary network NodePort tests
[ cherry-picked from cilium/cilium-cli repository ]

First, this commit introduces "--secondary-network-iface" flag. The flag
is used to determine a secondary network IP addr of each node.

Second, the commit extends the NodePort from outside tests. When the
aforementioned flag is set, in addition to the regular curl request, the
test case sends HTTP requests to an IP addr on each Cilium node which is
set to the "--secondary-network-face".

Signed-off-by: Martynas Pumputis <m@lambda.lt>

cilium-cli/connectivity/check/deployment.go
@@ -6,6 +6,7 @@ package check
 import (
 	"context"
 	"fmt"
+	"strings"
 	"sync"
 	"time"
 
@@ -1319,6 +1320,25 @@ func (ct *ConnectivityTest) validateDeployment(ctx context.Context) error {
 			Outside:   ok,
 		}
 		ct.hostNetNSPodsByNode[pod.Spec.NodeName] = p
+
+		if iface := ct.params.SecondaryNetworkIface; iface != "" {
+			if ct.Features[FeatureIPv4].Enabled {
+				cmd := []string{"/bin/sh", "-c", fmt.Sprintf("ip -family inet -oneline address show dev %s scope global | awk '{print $4}' | cut -d/ -f1", iface)}
+				addr, err := ct.client.ExecInPod(ctx, pod.Namespace, pod.Name, "", cmd)
+				if err != nil {
+					return fmt.Errorf("failed to fetch secondary network ip addr: %w", err)
+				}
+				ct.secondaryNetworkNodeIPv4[pod.Spec.NodeName] = strings.TrimSuffix(addr.String(), "\n")
+			}
+			if ct.Features[FeatureIPv4].Enabled {
+				cmd := []string{"/bin/sh", "-c", fmt.Sprintf("ip -family inet6 -oneline address show dev %s scope global | awk '{print $4}' | cut -d/ -f1", iface)}
+				addr, err := ct.client.ExecInPod(ctx, pod.Namespace, pod.Name, "", cmd)
+				if err != nil {
+					return fmt.Errorf("failed to fetch secondary network ip addr: %w", err)
+				}
+				ct.secondaryNetworkNodeIPv6[pod.Spec.NodeName] = strings.TrimSuffix(addr.String(), "\n")
+			}
+		}
 	}
 
 	var logOnce sync.Once

cilium-cli/connectivity/check/check.go
@@ -69,6 +69,7 @@ type Parameters struct {
 	ConnDisruptTestRestartsPath   string
 	ConnDisruptTestXfrmErrorsPath string
 	FlushCT                       bool
+	SecondaryNetworkIface         string
 
 	K8sVersion           string
 	HelmChartDirectory   string

cilium-cli/connectivity/check/context.go
@@ -66,7 +66,9 @@ type ConnectivityTest struct {
 	ingressService    map[string]Service
 	externalWorkloads map[string]ExternalWorkload
 
-	hostNetNSPodsByNode map[string]Pod
+	hostNetNSPodsByNode      map[string]Pod
+	secondaryNetworkNodeIPv4 map[string]string // node name => secondary ip
+	secondaryNetworkNodeIPv6 map[string]string // node name => secondary ip
 
 	tests     []*Test
 	testNames map[string]struct{}
@@ -182,24 +184,26 @@ func NewConnectivityTest(client *k8s.Client, p Parameters, version string) (*Con
 	}
 
 	k := &ConnectivityTest{
-		client:              client,
-		params:              p,
-		version:             version,
-		ciliumPods:          make(map[string]Pod),
-		echoPods:            make(map[string]Pod),
-		echoExternalPods:    make(map[string]Pod),
-		clientPods:          make(map[string]Pod),
-		perfClientPods:      make(map[string]Pod),
-		perfServerPod:       make(map[string]Pod),
-		PerfResults:         make(map[PerfTests]PerfResult),
-		echoServices:        make(map[string]Service),
-		ingressService:      make(map[string]Service),
-		externalWorkloads:   make(map[string]ExternalWorkload),
-		hostNetNSPodsByNode: make(map[string]Pod),
-		nodes:               make(map[string]*corev1.Node),
-		tests:               []*Test{},
-		testNames:           make(map[string]struct{}),
-		lastFlowTimestamps:  make(map[string]time.Time),
+		client:                   client,
+		params:                   p,
+		version:                  version,
+		ciliumPods:               make(map[string]Pod),
+		echoPods:                 make(map[string]Pod),
+		echoExternalPods:         make(map[string]Pod),
+		clientPods:               make(map[string]Pod),
+		perfClientPods:           make(map[string]Pod),
+		perfServerPod:            make(map[string]Pod),
+		PerfResults:              make(map[PerfTests]PerfResult),
+		echoServices:             make(map[string]Service),
+		ingressService:           make(map[string]Service),
+		externalWorkloads:        make(map[string]ExternalWorkload),
+		hostNetNSPodsByNode:      make(map[string]Pod),
+		secondaryNetworkNodeIPv4: make(map[string]string),
+		secondaryNetworkNodeIPv6: make(map[string]string),
+		nodes:                    make(map[string]*corev1.Node),
+		tests:                    []*Test{},
+		testNames:                make(map[string]struct{}),
+		lastFlowTimestamps:       make(map[string]time.Time),
 	}
 
 	return k, nil
@@ -876,6 +880,14 @@ func (ct *ConnectivityTest) ClientPods() map[string]Pod {
 
 func (ct *ConnectivityTest) HostNetNSPodsByNode() map[string]Pod {
 	return ct.hostNetNSPodsByNode
+}
+
+func (ct *ConnectivityTest) SecondaryNetworkNodeIPv4() map[string]string {
+	return ct.secondaryNetworkNodeIPv4
+}
+
+func (ct *ConnectivityTest) SecondaryNetworkNodeIPv6() map[string]string {
+	return ct.secondaryNetworkNodeIPv6
 }
 
 func (ct *ConnectivityTest) PerfServerPod() map[string]Pod {

cilium-cli/connectivity/tests/service.go
@@ -152,7 +152,7 @@ func (s *podToRemoteNodePort) Run(ctx context.Context, t *check.Test) {
 
 				// If src and dst pod are running on different nodes,
 				// call the Cilium Pod's host IP on the service's NodePort.
-				curlNodePort(ctx, s, t, fmt.Sprintf("curl-%d", i), &pod, svc, node, true)
+				curlNodePort(ctx, s, t, fmt.Sprintf("curl-%d", i), &pod, svc, node, true, false)
 
 				i++
 			}
@@ -188,7 +188,7 @@ func (s *podToLocalNodePort) Run(ctx context.Context, t *check.Test) {
 					if pod.Pod.Status.HostIP == addr.Address {
 						// If src and dst pod are running on the same node,
 						// call the Cilium Pod's host IP on the service's NodePort.
-						curlNodePort(ctx, s, t, fmt.Sprintf("curl-%d", i), &pod, svc, node, true)
+						curlNodePort(ctx, s, t, fmt.Sprintf("curl-%d", i), &pod, svc, node, true, false)
 
 						i++
 					}
@@ -200,14 +200,32 @@ func (s *podToLocalNodePort) Run(ctx context.Context, t *check.Test) {
 
 func curlNodePort(ctx context.Context, s check.Scenario, t *check.Test,
 	name string, pod *check.Pod, svc check.Service, node *corev1.Node,
-	validateFlows bool) {
+	validateFlows bool, secondaryNetwork bool) {
 
 	// Get the NodePort allocated to the Service.
 	np := uint32(svc.Service.Spec.Ports[0].NodePort)
 
+	addrs := make([]corev1.NodeAddress, len(node.Status.Addresses))
+	copy(addrs, node.Status.Addresses)
+
+	if secondaryNetwork {
+		if t.Context().Features[check.FeatureIPv4].Enabled {
+			addrs = append(addrs, corev1.NodeAddress{
+				Type:    "SecondaryNetworkIPv4",
+				Address: t.Context().SecondaryNetworkNodeIPv4()[node.Name],
+			})
+		}
+		if t.Context().Features[check.FeatureIPv6].Enabled {
+			addrs = append(addrs, corev1.NodeAddress{
+				Type:    "SecondaryNetworkIPv6",
+				Address: t.Context().SecondaryNetworkNodeIPv6()[node.Name],
+			})
+		}
+	}
+
 	t.ForEachIPFamily(func(ipFam check.IPFamily) {
 
-		for _, addr := range node.Status.Addresses {
+		for _, addr := range addrs {
 			if check.GetIPFamily(addr.Address) != ipFam {
 				continue
 			}
@@ -273,7 +291,7 @@ func (s *outsideToNodePort) Run(ctx context.Context, t *check.Test) {
 		for _, node := range t.Context().Nodes() {
 			node := node // copy to avoid memory aliasing when using reference
 
-			curlNodePort(ctx, s, t, fmt.Sprintf("curl-%d", i), &clientPod, svc, node, validateFlows)
+			curlNodePort(ctx, s, t, fmt.Sprintf("curl-%d", i), &clientPod, svc, node, validateFlows, t.Context().Params().SecondaryNetworkIface != "")
 			i++
 		}
 	}

cilium-cli/internal/cli/cmd/connectivity.go
@@ -180,6 +180,7 @@ func newCmdConnectivityTest(hooks Hooks) *cobra.Command {
 	cmd.Flags().StringVar(&params.ConnDisruptTestXfrmErrorsPath, "conn-disrupt-test-xfrm-errors-path", "/tmp/cilium-conn-disrupt-xfrm-errors", "Conn disrupt test temporary result file (used internally)")
 	cmd.Flags().BoolVar(&params.FlushCT, "flush-ct", false, "Flush conntrack of Cilium on each node")
 	cmd.Flags().MarkHidden("flush-ct")
+	cmd.Flags().StringVar(&params.SecondaryNetworkIface, "secondary-network-iface", "", "Secondary network iface name (e.g., to test NodePort BPF on multiple networks)")
 
 	hooks.AddConnectivityTestFlags(cmd.Flags())
 

commit 868903bb01d14589f15758360fd68d96bf65fbce

cilium-cli/connectivity: fix IPv6 feature check for 2ndary node IPv6
Currently, the feature check is for IPv4 to be enabled, even though the
secondary node IPv6 address is configured. This was likely copy-pasted
from the section above which does the corresponding configuration of the
secondary node IPv4 address. Fix the feature check accordingly to check
for IPv6 to be enabled.

Fixes: #35125
Fixes: 849809274b8b ("connectivity: Add secondary network NodePort tests")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

cilium-cli/connectivity/check/deployment.go
@@ -1751,7 +1751,7 @@ func (ct *ConnectivityTest) validateDeployment(ctx context.Context) error {
 					}
 					ct.secondaryNetworkNodeIPv4[pod.Spec.NodeName] = strings.TrimSuffix(addr.String(), "\n")
 				}
-				if ct.Features[features.IPv4].Enabled {
+				if ct.Features[features.IPv6].Enabled {
 					cmd := []string{"/bin/sh", "-c", fmt.Sprintf("ip -family inet6 -oneline address show dev %s scope global | awk '{print $4}' | cut -d/ -f1", iface)}
 					addr, err := client.ExecInPod(ctx, pod.Namespace, pod.Name, "", cmd)
 					if err != nil {