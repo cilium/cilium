2e933bff508492f8af12444ce08cc9ba6dcf4740
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2024-12-11 07:50:10 +0000

    masquerade: type change reverted
    [ upstream commit b8da5070737d4623fb13bdfe99dfbbae1d793d43 ]
    
    [ backporter's notes: skipped change in cilium-cli/install/helm.go
      as the cilium-cli directory does not exist in the v1.15 branch. ]
    
    MasqueradeInterfaces type reverted to string slice.
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -3090,9 +3090,7 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 	c.EnableWellKnownIdentities = vp.GetBool(EnableWellKnownIdentities)
 	c.EnableXDPPrefilter = vp.GetBool(EnableXDPPrefilter)
 	c.DisableCiliumEndpointCRD = vp.GetBool(DisableCiliumEndpointCRDName)
-	if masqInfs := vp.GetString(MasqueradeInterfaces); masqInfs != "" {
-		c.MasqueradeInterfaces = strings.Split(masqInfs, ",")
-	}
+	c.MasqueradeInterfaces = vp.GetStringSlice(MasqueradeInterfaces)
 	c.BPFSocketLBHostnsOnly = vp.GetBool(BPFSocketLBHostnsOnly)
 	c.EnableSocketLB = vp.GetBool(EnableSocketLB)
 	c.EnableSocketLBTracing = vp.GetBool(EnableSocketLBTracing)
