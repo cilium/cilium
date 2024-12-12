cilium/cilium

commit 52d61cb11424636fc89af027e48c8c07f63cc22e

policy: gather policy calculation in the repository
This simplifies the policy calculation flow by centralizing calculations
entirely within the policy package. Endpoints no longer need to lock the
repository manually; policy calculation manages this itself.

Some care is needed to preserve the logic to skip policy revisisions;
endpoints are informed when they can skip policy calculations and thus
must pass this back down to the repository when calculating policy.

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

pkg/endpoint/metrics.go
@@ -90,6 +90,16 @@ func (s *regenerationStatistics) GetMap() map[string]*spanstat.SpanStat {
 	return result
 }
 
+// used by PolicyRepository.GetSelectorPolicy
+func (s *regenerationStatistics) WaitingForPolicyRepository() *spanstat.SpanStat {
+	return &s.waitingForPolicyRepository
+}
+
+// used by PolicyRepository.GetSelectorPolicy
+func (s *regenerationStatistics) PolicyCalculation() *spanstat.SpanStat {
+	return &s.policyCalculation
+}
+
 // endpointPolicyStatusMap is a map to store the endpoint id and the policy
 // enforcement status. It is used only to send metrics to prometheus.
 type endpointPolicyStatusMap struct {

pkg/endpoint/policy.go
@@ -190,48 +190,28 @@ func (e *Endpoint) regeneratePolicy(stats *regenerationStatistics, datapathRegen
 	e.unlock()
 
 	e.getLogger().Debug("Starting policy recalculation...")
-
-	stats.waitingForPolicyRepository.Start()
-	repo := e.policyGetter.GetPolicyRepository()
-	repo.RLock() // Be sure to release this lock!
-	stats.waitingForPolicyRepository.End(true)
-
-	result.policyRevision = repo.GetRevision()
-
-	// Recompute policy for this endpoint only if not already done for this revision
-	// and identity.
-	if e.nextPolicyRevision >= result.policyRevision && e.desiredPolicy != nil {
-
-		if !forcePolicyCompute {
-			if logger := e.getLogger(); logging.CanLogAt(logger.Logger, logrus.DebugLevel) {
-				e.getLogger().WithFields(logrus.Fields{
-					"policyRevision.next": e.nextPolicyRevision,
-					"policyRevision.repo": result.policyRevision,
-					"policyChanged":       e.nextPolicyRevision > e.policyRevision,
-				}).Debug("Skipping unnecessary endpoint policy recalculation")
-			}
-			repo.RUnlock()
-			return result, nil
-		} else {
-			e.getLogger().Debug("Forced policy recalculation")
-		}
+	skipPolicyRevision := e.nextPolicyRevision
+	if forcePolicyCompute || e.desiredPolicy == nil {
+		e.getLogger().Debug("Forced policy recalculation")
+		skipPolicyRevision = 0
 	}
 
-	stats.policyCalculation.Start()
-	defer func() { stats.policyCalculation.End(err == nil) }()
-
-	// UpdatePolicy ensures the SelectorPolicy is fully resolved.
-	// Endpoint lock must not be held!
-	// TODO: GH-7515: Consider ways to compute policy outside of the
-	// endpoint regeneration process, ideally as part of the policy change
-	// handler.
-	selectorPolicy, err := repo.GetPolicyCache().UpdatePolicy(securityIdentity)
+	var selectorPolicy policy.SelectorPolicy
+	selectorPolicy, result.policyRevision, err = e.policyGetter.GetPolicyRepository().GetSelectorPolicy(securityIdentity, skipPolicyRevision, stats)
 	if err != nil {
-		e.getLogger().WithError(err).Warning("Failed to update policy")
-		repo.RUnlock()
+		e.getLogger().WithError(err).Warning("Failed to calculate SelectorPolicy")
 		return nil, err
 	}
-	repo.RUnlock() // Done with policy repository; release this now as Consume() can be slow
+
+	// selectorPolicy is nil if skipRevision was matched.
+	if selectorPolicy == nil {
+		e.getLogger().WithFields(logrus.Fields{
+			"policyRevision.next": e.nextPolicyRevision,
+			"policyRevision.repo": result.policyRevision,
+			"policyChanged":       e.nextPolicyRevision > e.policyRevision,
+		}).Debug("Skipping unnecessary endpoint policy recalculation")
+		return result, err
+	}
 
 	// Add new redirects before Consume() so that all required proxy ports are available for it.
 	var desiredRedirects map[string]uint16

pkg/policy/repository.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/cilium/cilium/pkg/spanstat"
 )
 
 // PolicyContext is an interface policy resolution functions use to access the Repository.
@@ -125,6 +126,15 @@ type PolicyRepository interface {
 	GetAuthTypes(localID identity.NumericIdentity, remoteID identity.NumericIdentity) AuthTypes
 	GetEnvoyHTTPRules(l7Rules *api.L7Rules, ns string) (*cilium.HttpNetworkPolicyRules, bool)
 	GetPolicyCache() *PolicyCache
+
+	// GetSelectorPolicy computes the SelectorPolicy for a given identity.
+	//
+	// It returns nil if skipRevision is >= than the already calculated version.
+	// This is used to skip policy calculation when a certain revision delta is
+	// known to not affect the given identity. Pass a skipRevision of 0 to force
+	// calculation.
+	GetSelectorPolicy(id *identity.Identity, skipRevision uint64, stats GetPolicyStatistics) (SelectorPolicy, uint64, error)
+
 	GetRevision() uint64
 	GetRulesList() *models.Policy
 	GetSelectorCache() *SelectorCache
@@ -136,6 +146,11 @@ type PolicyRepository interface {
 	SearchRLocked(lbls labels.LabelArray) api.Rules
 	SetEnvoyRulesFunc(f func(certificatemanager.SecretManager, *api.L7Rules, string, string) (*cilium.HttpNetworkPolicyRules, bool))
 	Start()
+}
+
+type GetPolicyStatistics interface {
+	WaitingForPolicyRepository() *spanstat.SpanStat
+	PolicyCalculation() *spanstat.SpanStat
 }
 
 // Repository is a list of policy rules which in combination form the security
@@ -929,4 +944,38 @@ func wildcardRule(lbls labels.LabelArray, ingress bool) *rule {
 	_ = r.Sanitize()
 
 	return r
+}
+
+// GetSelectorPolicy computes the SelectorPolicy for a given identity.
+//
+// It returns nil if skipRevision is >= than the already calculated version.
+// This is used to skip policy calculation when a certain revision delta is
+// known to not affect the given identity. Pass a skipRevision of 0 to force
+// calculation.
+func (r *Repository) GetSelectorPolicy(id *identity.Identity, skipRevision uint64, stats GetPolicyStatistics) (SelectorPolicy, uint64, error) {
+	stats.WaitingForPolicyRepository().Start()
+	r.RLock()
+	defer r.RUnlock()
+	stats.WaitingForPolicyRepository().End(true)
+
+	rev := r.GetRevision()
+
+	// Do we already have a given revision?
+	// If so, skip calculation.
+	if skipRevision >= rev {
+		return nil, rev, nil
+	}
+
+	stats.PolicyCalculation().Start()
+	// This may call back in to the (locked) repository to generate the
+	// selector policy
+	sp, updated, err := r.policyCache.updateSelectorPolicy(id)
+	stats.PolicyCalculation().EndError(err)
+
+	// If we hit cache, reset the statistics.
+	if !updated {
+		stats.PolicyCalculation().Reset()
+	}
+
+	return sp, rev, nil
 }

pkg/policy/distillery.go
@@ -112,16 +112,6 @@ func (cache *PolicyCache) LocalEndpointIdentityRemoved(identity *identityPkg.Ide
 	cache.delete(identity)
 }
 
-// UpdatePolicy resolves the policy for the security identity of the specified
-// endpoint and caches it for future use.
-//
-// The caller must provide threadsafety for iteration over the policy
-// repository.
-func (cache *PolicyCache) UpdatePolicy(identity *identityPkg.Identity) (SelectorPolicy, error) {
-	sp, _, err := cache.updateSelectorPolicy(identity)
-	return sp, err
-}
-
 // GetAuthTypes returns the AuthTypes required by the policy between the localID and remoteID, if
 // any, otherwise returns nil.
 func (cache *PolicyCache) GetAuthTypes(localID, remoteID identityPkg.NumericIdentity) AuthTypes {

commit adc15d4265c5b2bbde73bf382623e32829ff3da8

regeneration statistics: split policy calculation stats
The PolicyCalculation span statistic could use some sub-spans. It is
interesting to know the total time taken, but this can be affected by
short-cutting and caching. So, add two additional spans for
SelectorPolicy and EndpointPolicy calculation, and only update these
when their calculations are not short-cutted.

This also fixes a bug (never shipped) that accidentally updated the
PolicyCalculation span statistic twice during endpoint regeneration.

Fixes: 52d61cb11

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

pkg/endpoint/metrics.go
@@ -46,6 +46,8 @@ type regenerationStatistics struct {
 	waitingForPolicyRepository spanstat.SpanStat
 	waitingForCTClean          spanstat.SpanStat
 	policyCalculation          spanstat.SpanStat
+	selectorPolicyCalculation  spanstat.SpanStat
+	endpointPolicyCalculation  spanstat.SpanStat
 	proxyConfiguration         spanstat.SpanStat
 	proxyPolicyCalculation     spanstat.SpanStat
 	proxyWaitForAck            spanstat.SpanStat
@@ -78,6 +80,8 @@ func (s *regenerationStatistics) GetMap() map[string]*spanstat.SpanStat {
 		"waitingForCTClean":          &s.waitingForCTClean,
 		"policyCalculation":          &s.policyCalculation,
 		"proxyConfiguration":         &s.proxyConfiguration,
+		"selectorPolicyCalculation":  &s.selectorPolicyCalculation,
+		"endpointPolicyCalculation":  &s.endpointPolicyCalculation,
 		"proxyPolicyCalculation":     &s.proxyPolicyCalculation,
 		"proxyWaitForAck":            &s.proxyWaitForAck,
 		"mapSync":                    &s.mapSync,
@@ -96,8 +100,8 @@ func (s *regenerationStatistics) WaitingForPolicyRepository() *spanstat.SpanStat
 }
 
 // used by PolicyRepository.GetSelectorPolicy
-func (s *regenerationStatistics) PolicyCalculation() *spanstat.SpanStat {
-	return &s.policyCalculation
+func (s *regenerationStatistics) SelectorPolicyCalculation() *spanstat.SpanStat {
+	return &s.selectorPolicyCalculation
 }
 
 // endpointPolicyStatusMap is a map to store the endpoint id and the policy

pkg/endpoint/policy.go
@@ -245,7 +245,9 @@ func (e *Endpoint) regeneratePolicy(stats *regenerationStatistics, datapathRegen
 	e.runlock()
 
 	// DistillPolicy converts a SelectorPolicy in to an EndpointPolicy
+	stats.endpointPolicyCalculation.Start()
 	result.endpointPolicy = selectorPolicy.DistillPolicy(e, desiredRedirects)
+	stats.endpointPolicyCalculation.End(true)
 
 	return result, nil
 }

pkg/policy/repository.go
@@ -133,7 +133,7 @@ type PolicyRepository interface {
 
 type GetPolicyStatistics interface {
 	WaitingForPolicyRepository() *spanstat.SpanStat
-	PolicyCalculation() *spanstat.SpanStat
+	SelectorPolicyCalculation() *spanstat.SpanStat
 }
 
 // Repository is a list of policy rules which in combination form the security
@@ -840,15 +840,15 @@ func (r *Repository) GetSelectorPolicy(id *identity.Identity, skipRevision uint6
 		return nil, rev, nil
 	}
 
-	stats.PolicyCalculation().Start()
+	stats.SelectorPolicyCalculation().Start()
 	// This may call back in to the (locked) repository to generate the
 	// selector policy
 	sp, updated, err := r.policyCache.updateSelectorPolicy(id)
-	stats.PolicyCalculation().EndError(err)
+	stats.SelectorPolicyCalculation().EndError(err)
 
 	// If we hit cache, reset the statistics.
 	if !updated {
-		stats.PolicyCalculation().Reset()
+		stats.SelectorPolicyCalculation().Reset()
 	}
 
 	return sp, rev, nil

pkg/policy/distillery_test.go
@@ -2049,6 +2049,6 @@ func (s *dummyPolicyStats) WaitingForPolicyRepository() *spanstat.SpanStat {
 	return &s.waitingForPolicyRepository
 }
 
-func (s *dummyPolicyStats) PolicyCalculation() *spanstat.SpanStat {
+func (s *dummyPolicyStats) SelectorPolicyCalculation() *spanstat.SpanStat {
 	return &s.policyCalculation
 }