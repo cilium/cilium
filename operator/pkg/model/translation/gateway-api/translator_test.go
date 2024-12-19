c647239c92e5cd07bdaa915d6f7758873ee55bcc
Tam Mach <tam.mach@cilium.io>
2024-12-19 14:14:28 +0000

    gateway-api: Convert translation test to file based
    [ upstream commit 50e742126592f3d36e5fca6429aa8a42446993fe ]
    
    This will reduce the effort on writing/debugging tests compared to go
    structs. Additionally, these test data files will be referenced for any
    potential issue.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/model/translation/gateway-api/translator_test.go
+++ operator/pkg/model/translation/gateway-api/translator_test.go
@@ -5,330 +5,99 @@ package gateway_api
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"testing"
 
-	envoy_config_route_v3 "github.com/cilium/proxy/go/envoy/config/route/v3"
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/testing/protocmp"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1"
+	k8syaml "sigs.k8s.io/yaml"
 
 	"github.com/cilium/cilium/operator/pkg/model"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 )
 
 func Test_translator_Translate(t *testing.T) {
-	type args struct {
-		m *model.Model
-	}
 	tests := []struct {
 		name    string
-		args    args
-		want    *ciliumv2.CiliumEnvoyConfig
 		wantErr bool
 	}{
-		{
-			name: "Basic HTTP Listener",
-			args: args{
-				m: &model.Model{
-					HTTP: basicHTTPListeners,
-				},
-			},
-			want: basicHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Basic TLS SNI Listener",
-			args: args{
-				m: &model.Model{
-					TLS: basicTLSListeners,
-				},
-			},
-			want: basicTLSListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteSimpleSameNamespace",
-			args: args{
-				m: &model.Model{
-					HTTP: simpleSameNamespaceHTTPListeners,
-				},
-			},
-			want: simpleSameNamespaceHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteCrossNamespace",
-			args: args{
-				m: &model.Model{
-					HTTP: crossNamespaceHTTPListeners,
-				},
-			},
-			want: crossNamespaceHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPExactPathMatching",
-			args: args{
-				m: &model.Model{
-					HTTP: exactPathMatchingHTTPListeners,
-				},
-			},
-			want: exactPathMatchingHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteHeaderMatching",
-			args: args{
-				m: &model.Model{
-					HTTP: headerMatchingHTTPListeners,
-				},
-			},
-			want: headerMatchingHTTPCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteHostnameIntersection",
-			args: args{
-				m: &model.Model{
-					HTTP: hostnameIntersectionHTTPListeners,
-				},
-			},
-			want: hostnameIntersectionHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteListenerHostnameMatching",
-			args: args{
-				m: &model.Model{
-					HTTP: listenerHostnameMatchingHTTPListeners,
-				},
-			},
-			want: listenerHostNameMatchingCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteMatchingAcrossRoutes",
-			args: args{
-				m: &model.Model{
-					HTTP: matchingAcrossHTTPListeners,
-				},
-			},
-			want: matchingAcrossHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteMatching",
-			args: args{
-				m: &model.Model{
-					HTTP: matchingHTTPListeners,
-				},
-			},
-			want: matchingHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteMethodMatching",
-			args: args{
-				m: &model.Model{
-					HTTP: methodMatchingHTTPListeners,
-				},
-			},
-			want: methodMatchingHTTPListenersHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteQueryParamMatching",
-			args: args{
-				m: &model.Model{
-					HTTP: queryParamMatchingHTTPListeners,
-				},
-			},
-			want: queryParamMatchingHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteRequestHeaderModifier",
-			args: args{
-				m: &model.Model{
-					HTTP: requestHeaderModifierHTTPListeners,
-				},
-			},
-			want: requestHeaderModifierHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteBackendRefsRequestHeaderModifier",
-			args: args{
-				m: &model.Model{
-					HTTP: backendRefsRequestHeaderModifierHTTPListeners,
-				},
-			},
-			want: backendRefsRequestHeaderModifierHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteRequestRedirect",
-			args: args{
-				m: &model.Model{
-					HTTP: requestRedirectHTTPListeners,
-				},
-			},
-			want: requestRedirectHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteResponseHeaderModifier",
-			args: args{
-				m: &model.Model{
-					HTTP: responseHeaderModifierHTTPListeners,
-				},
-			},
-			want: responseHeaderModifierHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteBackendRefsResponseHeaderModifier",
-			args: args{
-				m: &model.Model{
-					HTTP: backendRefsResponseHeaderModifierHTTPListeners,
-				},
-			},
-			want: backendRefsResponseHeaderModifierHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteRewriteHost",
-			args: args{
-				m: &model.Model{
-					HTTP: rewriteHostHTTPListeners,
-				},
-			},
-			want: rewriteHostHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteRewritePath",
-			args: args{
-				m: &model.Model{
-					HTTP: rewritePathHTTPListeners,
-				},
-			},
-			want: rewritePathHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteRequestMirror",
-			args: args{
-				m: &model.Model{
-					HTTP: mirrorHTTPListeners,
-				},
-			},
-			want: mirrorHTTPListenersCiliumEnvoyConfig,
-		},
-		{
-			name: "Conformance/HTTPRouteRequestRedirectWithMultiHTTPListeners",
-			args: args{
-				m: &model.Model{
-					HTTP: requestRedirectWithMultiHTTPListeners,
-				},
-			},
-			want: requestRedirectWithMultiHTTPListenersCiliumEnvoyConfig,
-		},
+		{name: "basic_http_listener"},
+		{name: "basic_tls_sni_listener"},
+		{name: "conformance/httproute_simple_same_namespace"},
+		{name: "conformance/httproute_backend_protocol_h_2_c"},
+		{name: "conformance/httproute_cross_namespace"},
+		{name: "conformance/httpexact_path_matching"},
+		{name: "conformance/httproute_header_matching"},
+		{name: "conformance/httproute_hostname_intersection"},
+		{name: "conformance/httproute_listener_hostname_matching"},
+		{name: "conformance/httproute_matching_across_routes"},
+		{name: "conformance/httproute_matching"},
+		{name: "conformance/httproute_method_matching"},
+		{name: "conformance/httproute_query_param_matching"},
+		{name: "conformance/httproute_request_header_modifier"},
+		{name: "conformance/httproute_backend_refs_request_header_modifier"},
+		{name: "conformance/httproute_request_redirect"},
+		{name: "conformance/httproute_response_header_modifier"},
+		{name: "conformance/httproute_backend_refs_response_header_modifier"},
+		{name: "conformance/httproute_rewrite_host"},
+		{name: "conformance/httproute_rewrite_path"},
+		{name: "conformance/httproute_request_mirror"},
+		{name: "conformance/httproute_request_redirect_with_multi_httplisteners"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			trans := &translator{
 				idleTimeoutSeconds: 60,
+				secretsNamespace:   "cilium-secrets",
 			}
-			cec, _, _, err := trans.Translate(tt.args.m)
+
+			input := &model.Model{}
+			readInput(t, fmt.Sprintf("testdata/%s/input.yaml", tt.name), input)
+			output := &ciliumv2.CiliumEnvoyConfig{}
+			readOutput(t, fmt.Sprintf("testdata/%s/cec-output.yaml", tt.name), output)
+
+			cec, _, _, err := trans.Translate(input)
+
 			require.Equal(t, tt.wantErr, err != nil, "Error mismatch")
-			require.Equal(t, tt.want, cec, "CiliumEnvoyConfig did not match")
+			diffOutput := cmp.Diff(output, cec, protocmp.Transform())
+			if len(diffOutput) != 0 {
+				t.Errorf("CiliumEnvoyConfigs did not match:\n%s\n", diffOutput)
+			}
 		})
 	}
 }
 
-func Test_translator_TranslateResource(t *testing.T) {
-	type args struct {
-		m *model.Model
-	}
+func Test_translator_Translate_AppProtocol(t *testing.T) {
 	tests := []struct {
-		name          string
-		args          args
-		wantErr       bool
-		validateFuncs []func(config *ciliumv2.CiliumEnvoyConfig) bool
+		name    string
+		wantErr bool
 	}{
-		{
-			name: "MultipleListenerGateway",
-			args: args{
-				m: &model.Model{
-					HTTP: multipleListenerGatewayListeners,
-				},
-			},
-			validateFuncs: []func(cec *ciliumv2.CiliumEnvoyConfig) bool{
-				func(cec *ciliumv2.CiliumEnvoyConfig) bool {
-					resource := ciliumv2.XDSResource{
-						Any: toAny(&envoy_config_route_v3.RouteConfiguration{
-							Name: "listener-insecure",
-							VirtualHosts: []*envoy_config_route_v3.VirtualHost{
-								{
-									Name: "example.com",
-									Domains: []string{
-										"example.com",
-										"example.com:*",
-									},
-									Routes: []*envoy_config_route_v3.Route{
-										{
-											Match: &envoy_config_route_v3.RouteMatch{
-												PathSpecifier: &envoy_config_route_v3.RouteMatch_Prefix{
-													Prefix: "/",
-												},
-											},
-											Action: &envoy_config_route_v3.Route_Redirect{
-												Redirect: &envoy_config_route_v3.RedirectAction{
-													SchemeRewriteSpecifier: &envoy_config_route_v3.RedirectAction_SchemeRedirect{
-														SchemeRedirect: "https",
-													},
-													PortRedirect: 443,
-												},
-											},
-										},
-									},
-								},
-							},
-						}),
-					}
-
-					expected, _ := resource.MarshalJSON()
-					got, _ := cec.Spec.Resources[1].MarshalJSON()
-					return assert.Equal(t, string(expected), string(got), "Route Configuration mismatch")
-				},
-				func(cec *ciliumv2.CiliumEnvoyConfig) bool {
-					resource := ciliumv2.XDSResource{
-						Any: toAny(&envoy_config_route_v3.RouteConfiguration{
-							Name: "listener-secure",
-							VirtualHosts: []*envoy_config_route_v3.VirtualHost{
-								{
-									Name: "example.com",
-									Domains: []string{
-										"example.com",
-										"example.com:*",
-									},
-									Routes: []*envoy_config_route_v3.Route{
-										{
-											Match: &envoy_config_route_v3.RouteMatch{
-												PathSpecifier: &envoy_config_route_v3.RouteMatch_Prefix{
-													Prefix: "/",
-												},
-											},
-											Action: toRouteAction("default", "my-service", "8080"),
-										},
-									},
-								},
-							},
-						}),
-					}
-
-					expected, _ := resource.MarshalJSON()
-					got, _ := cec.Spec.Resources[2].MarshalJSON()
-					return assert.Equal(t, string(expected), string(got), "Route Configuration mismatch")
-				},
-			},
-		},
+		{name: "conformance/httproute_backend_protocol_h_2_c_app_protocol"},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			trans := &translator{
 				idleTimeoutSeconds: 60,
 			}
-			cec, _, _, err := trans.Translate(tt.args.m)
+
+			input := &model.Model{}
+			readInput(t, fmt.Sprintf("testdata/%s/input.yaml", tt.name), input)
+			output := &ciliumv2.CiliumEnvoyConfig{}
+			readOutput(t, fmt.Sprintf("testdata/%s/cec-output.yaml", tt.name), output)
+
+			cec, _, _, err := trans.Translate(input)
+
 			require.Equal(t, tt.wantErr, err != nil, "Error mismatch")
-			for _, fn := range tt.validateFuncs {
-				require.True(t, fn(cec), "Validation failed")
+			diffOutput := cmp.Diff(output, cec, protocmp.Transform())
+			if len(diffOutput) != 0 {
+				t.Errorf("CiliumEnvoyConfigs did not match:\n%s\n", diffOutput)
 			}
 		})
 	}
@@ -395,4 +164,34 @@ func Test_getService(t *testing.T) {
 			assert.Equal(t, true, len(got.Name) <= 63, "Service name is too long")
 		})
 	}
+}
+
+func readInput(t *testing.T, file string, obj any) {
+	inputYaml, err := os.ReadFile(file)
+	require.NoError(t, err)
+
+	require.NoError(t, k8syaml.Unmarshal(inputYaml, obj))
+}
+
+func readOutput(t *testing.T, file string, obj any) string {
+	// unmarshal and marshal to prevent formatting diffs
+	outputYaml, err := os.ReadFile(file)
+	require.NoError(t, err)
+
+	if strings.TrimSpace(string(outputYaml)) == "" {
+		return strings.TrimSpace(string(outputYaml))
+	}
+
+	require.NoError(t, k8syaml.Unmarshal(outputYaml, obj))
+
+	yamlText := toYaml(t, obj)
+
+	return strings.TrimSpace(yamlText)
+}
+
+func toYaml(t *testing.T, obj any) string {
+	yamlText, err := k8syaml.Marshal(obj)
+	require.NoError(t, err)
+
+	return strings.TrimSpace(string(yamlText))
 }
