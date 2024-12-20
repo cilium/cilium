e9ef7e21ed7dd921d6f570bf7519820b9fb3f0e9
Jarno Rajahalme <jarno@isovalent.com>
2024-12-21 00:14:34 +0100

    Envoy: Wait for initial endpoint policy computation instead of restoration
    Only pre-compute endpoint policy on first regeneration. This avoids extra
    work if the endpoint policy gets updated while it is waiting to be
    regenerated.
    
    Start serving xDS resources to Envoy as soon as the policies of all
    restored endopoints have been computed, rather than waiting for them to
    have been fully restored. This allows Envoy to get policy updates sooner
    on restart.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- pkg/endpointstate/restorer.go
+++ pkg/endpointstate/restorer.go
@@ -10,4 +10,8 @@ type Restorer interface {
 	// WaitForEndpointRestore blocks the caller until either the context is
 	// cancelled or all the endpoints have been restored from a previous run.
 	WaitForEndpointRestore(ctx context.Context) error
+
+	// WaitForInitialEnvoyPolicy blocks the caller until either the context is
+	// cancelled or policies of all the endpoints have been computed.
+	WaitForInitialEnvoyPolicy(ctx context.Context) error
 }
