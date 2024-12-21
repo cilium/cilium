f1549078953bb473e53017a582bfcb35d3ef1aae
wszqkzqk <wszqkzqk@qq.com>
2024-12-21 14:25:30 +0800

    pkg/byteorder: add loong64 platform support
    Signed-off-by: wszqkzqk <wszqkzqk@qq.com>

--- pkg/byteorder/byteorder_littleendian.go
+++ pkg/byteorder/byteorder_littleendian.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-//go:build 386 || amd64 || arm || arm64 || mips64le || ppc64le || riscv64 || wasm
+//go:build 386 || amd64 || arm || arm64 || mips64le || ppc64le || riscv64 || wasm || loong64
 
 package byteorder
 
