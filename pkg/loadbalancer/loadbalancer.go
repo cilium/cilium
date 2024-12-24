4158fcb2436900940d21f1782736b966856002a3
Damian Sawicki <dsawicki@google.com>
2024-12-24 09:14:34 +0000

    experimental: Add LBALG to DumpLBMaps
    This adds LBALG to DumpLBMaps in preparation for adding support for
    Per-service Load Balancing Algorithm Selection (CFP-34577) to
    StateDB-based load balancing.
    
    Signed-off-by: Damian Sawicki <dsawicki@google.com>

--- pkg/loadbalancer/loadbalancer.go
+++ pkg/loadbalancer/loadbalancer.go
@@ -74,10 +74,21 @@ func ToSVCForwardingMode(s string) SVCForwardingMode {
 type SVCLoadBalancingAlgorithm uint8
 
 const (
-	SVCLoadBalancingAlgorithmUndef  = 0
-	SVCLoadBalancingAlgorithmRandom = 1
-	SVCLoadBalancingAlgorithmMaglev = 2
+	SVCLoadBalancingAlgorithmUndef  SVCLoadBalancingAlgorithm = 0
+	SVCLoadBalancingAlgorithmRandom SVCLoadBalancingAlgorithm = 1
+	SVCLoadBalancingAlgorithmMaglev SVCLoadBalancingAlgorithm = 2
 )
+
+func (d SVCLoadBalancingAlgorithm) String() string {
+	switch d {
+	case SVCLoadBalancingAlgorithmRandom:
+		return "random"
+	case SVCLoadBalancingAlgorithmMaglev:
+		return "maglev"
+	default:
+		return "undef"
+	}
+}
 
 func ToSVCLoadBalancingAlgorithm(s string) SVCLoadBalancingAlgorithm {
 	if s == option.NodePortAlgMaglev {
