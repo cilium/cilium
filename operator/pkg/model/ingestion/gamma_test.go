1c322500534181cb280e5337852a023b10225e0d
Tam Mach <tam.mach@cilium.io>
2024-12-30 14:57:34 +1100

    gateway/gamma: Convert ingestion test to file based
    This will reduce the effort on writing/debugging tests compared to go
    structs. Additionally, these test data files will be referenced for any
    potential issue.
    
    Relates: #36732
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/model/ingestion/gamma_test.go
+++ operator/pkg/model/ingestion/gamma_test.go
@@ -4,299 +4,45 @@
 package ingestion
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/utils/ptr"
-	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
+	"github.com/stretchr/testify/require"
 
 	"github.com/cilium/cilium/operator/pkg/model"
 )
 
-var meshSplitInput = GammaInput{
-	Services: []corev1.Service{
-		gammaEchoService,
-		gammaEchoV1Service,
-		gammaEchoV2Service,
-	},
-	HTTPRoutes: []gatewayv1.HTTPRoute{
-		meshSplitHTTPRoute,
-	},
-}
-
-var meshSplitListeners = []model.HTTPListener{
-	{
-		Name: "gateway-conformance-mesh-echo-80",
-		Sources: []model.FullyQualifiedResource{
-			{
-				Name:      "echo",
-				Namespace: "gateway-conformance-mesh",
-				Version:   "v1",
-				Kind:      "Service",
-			},
-			{
-				Name:      "mesh-split",
-				Namespace: "gateway-conformance-mesh",
-				Version:   "v1",
-				Group:     "gateway.networking.k8s.io",
-				Kind:      "HTTPRoute",
-			},
-		},
-		Port:     80,
-		Hostname: "*",
-		Routes: []model.HTTPRoute{
-			{
-				Hostnames: []string{
-					"*",
-				},
-				PathMatch: model.StringMatch{
-					Exact: "/v1",
-				},
-				Backends: []model.Backend{
-					{
-						Name:      "echo-v1",
-						Namespace: "gateway-conformance-mesh",
-						Port: &model.BackendPort{
-							Port: 80,
-						},
-						AppProtocol: ptr.To("http"),
-					},
-				},
-			},
-			{
-				Hostnames: []string{
-					"*",
-				},
-				PathMatch: model.StringMatch{
-					Exact: "/v2",
-				},
-				Backends: []model.Backend{
-					{
-						Name:      "echo-v2",
-						Namespace: "gateway-conformance-mesh",
-						Port: &model.BackendPort{
-							Port: 80,
-						},
-						AppProtocol: ptr.To("http"),
-					},
-				},
-			},
-		},
-		Service: &model.Service{
-			Type: "ClusterIP",
-		},
-	},
-	{
-		Name: "gateway-conformance-mesh-echo-8080",
-		Sources: []model.FullyQualifiedResource{
-			{
-				Name:      "echo",
-				Namespace: "gateway-conformance-mesh",
-				Version:   "v1",
-				Kind:      "Service",
-			},
-			{
-				Name:      "mesh-split",
-				Namespace: "gateway-conformance-mesh",
-				Version:   "v1",
-				Group:     "gateway.networking.k8s.io",
-				Kind:      "HTTPRoute",
-			},
-		},
-		Port:     8080,
-		Hostname: "*",
-		Routes: []model.HTTPRoute{
-			{
-				Hostnames: []string{
-					"*",
-				},
-				PathMatch: model.StringMatch{
-					Exact: "/v1",
-				},
-				Backends: []model.Backend{
-					{
-						Name:      "echo-v1",
-						Namespace: "gateway-conformance-mesh",
-						Port: &model.BackendPort{
-							Port: 80,
-						},
-						AppProtocol: ptr.To("http"),
-					},
-				},
-			},
-			{
-				Hostnames: []string{
-					"*",
-				},
-				PathMatch: model.StringMatch{
-					Exact: "/v2",
-				},
-				Backends: []model.Backend{
-					{
-						Name:      "echo-v2",
-						Namespace: "gateway-conformance-mesh",
-						Port: &model.BackendPort{
-							Port: 80,
-						},
-						AppProtocol: ptr.To("http"),
-					},
-				},
-			},
-		},
-		Service: &model.Service{
-			Type: "ClusterIP",
-		},
-	},
-}
-
-var meshPortsInput = GammaInput{
-	Services: []corev1.Service{
-		gammaEchoService,
-		gammaEchoV1Service,
-		gammaEchoV2Service,
-	},
-	HTTPRoutes: []gatewayv1.HTTPRoute{
-		meshPortsHTTPRoute,
-	},
-}
-
-var meshPortsListeners = []model.HTTPListener{
-	{
-		Name: "gateway-conformance-mesh-echo-v1-80",
-		Sources: []model.FullyQualifiedResource{
-			{
-				Name:      "echo-v1",
-				Namespace: "gateway-conformance-mesh",
-				Version:   "v1",
-				Kind:      "Service",
-			},
-			{
-				Name:      "mesh-ports",
-				Namespace: "gateway-conformance-mesh",
-				Version:   "v1",
-				Group:     "gateway.networking.k8s.io",
-				Kind:      "HTTPRoute",
-			},
-		},
-		Port:     80,
-		Hostname: "*",
-		Routes: []model.HTTPRoute{
-			{
-				Hostnames: []string{
-					"*",
-				},
-				Backends: []model.Backend{
-					{
-						Name:      "echo-v1",
-						Namespace: "gateway-conformance-mesh",
-						Port: &model.BackendPort{
-							Port: 80,
-						},
-						AppProtocol: ptr.To("http"),
-					},
-				},
-				ResponseHeaderModifier: &model.HTTPHeaderFilter{
-					HeadersToSet: []model.Header{
-						{
-							Name:  "X-Header-Set",
-							Value: "v1",
-						},
-					},
-				},
-			},
-		},
-		Service: &model.Service{
-			Type: "ClusterIP",
-		},
-	},
-}
-
-var meshFrontendInput = GammaInput{
-	Services: []corev1.Service{
-		gammaEchoService,
-		gammaEchoV1Service,
-		gammaEchoV2Service,
-	},
-	HTTPRoutes: []gatewayv1.HTTPRoute{
-		meshFrontendHTTPRoute,
-	},
-}
-
-var meshFrontendListeners = []model.HTTPListener{
-	{
-		Name: "gateway-conformance-mesh-echo-v2-80",
-		Sources: []model.FullyQualifiedResource{
-			{
-				Name:      "echo-v2",
-				Namespace: "gateway-conformance-mesh",
-				Version:   "v1",
-				Kind:      "Service",
-			},
-			{
-				Name:      "mesh-split-v1",
-				Namespace: "gateway-conformance-mesh",
-				Version:   "v1",
-				Group:     "gateway.networking.k8s.io",
-				Kind:      "HTTPRoute",
-			},
-		},
-		Port:     80,
-		Hostname: "*",
-		Routes: []model.HTTPRoute{
-			{
-				Hostnames: []string{
-					"*",
-				},
-				Backends: []model.Backend{
-					{
-						Name:      "echo-v2",
-						Namespace: "gateway-conformance-mesh",
-						Port: &model.BackendPort{
-							Port: 80,
-						},
-						AppProtocol: ptr.To("http"),
-					},
-				},
-				ResponseHeaderModifier: &model.HTTPHeaderFilter{
-					HeadersToSet: []model.Header{
-						{
-							Name:  "X-Header-Set",
-							Value: "set",
-						},
-					},
-				},
-			},
-		},
-		Service: &model.Service{
-			Type: "ClusterIP",
-		},
-	},
-}
+const (
+	basedGammaTestdataDir = "testdata/gamma"
+)
 
 func TestGammaConformance(t *testing.T) {
 	tests := map[string]struct {
-		input GammaInput
-		want  []model.HTTPListener
 	}{
-		"Mesh Split": {
-			input: meshSplitInput,
-			want:  meshSplitListeners,
-		},
-		"Mesh Ports": {
-			input: meshPortsInput,
-			want:  meshPortsListeners,
-		},
-		"Mesh Frontend": {
-			input: meshFrontendInput,
-			want:  meshFrontendListeners,
-		},
+		"Mesh Split":    {},
+		"Mesh Ports":    {},
+		"Mesh Frontend": {},
 	}
 
-	for name, tc := range tests {
+	for name := range tests {
 		t.Run(name, func(t *testing.T) {
-			listeners := GammaHTTPRoutes(tc.input)
-			assert.Equal(t, tc.want, listeners, "Listeners did not match")
+			input := readGammaInput(t, name)
+			listeners := GammaHTTPRoutes(input)
+
+			expected := []model.HTTPListener{}
+			readOutput(t, fmt.Sprintf("%s/%s/%s", basedGammaTestdataDir, rewriteTestName(name), "output-listeners.yaml"), &expected)
+
+			require.Equal(t, expected, listeners, "Listeners did not match")
 		})
 	}
+}
+
+func readGammaInput(t *testing.T, testName string) GammaInput {
+	input := GammaInput{}
+
+	readInput(t, fmt.Sprintf("%s/%s/%s", basedGammaTestdataDir, rewriteTestName(testName), "input-httproute.yaml"), &input.HTTPRoutes)
+	readInput(t, fmt.Sprintf("%s/%s/%s", basedGammaTestdataDir, rewriteTestName(testName), "input-service.yaml"), &input.Services)
+	readInput(t, fmt.Sprintf("%s/%s/%s", basedGammaTestdataDir, rewriteTestName(testName), "input-referencegrant.yaml"), &input.ReferenceGrants)
+
+	return input
 }
