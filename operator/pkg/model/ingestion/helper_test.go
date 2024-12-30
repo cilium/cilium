a2d290a84bdeffec7e1b78b374e2004dfa5501de
Tam Mach <tam.mach@cilium.io>
2024-12-30 13:12:32 +1100

    ingress: Convert ingestion test to file based
    This will reduce the effort on writing/debugging tests compared to go
    structs. Additionally, these test data files will be referenced for any
    potential issue.
    
    Relates: #36732
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/model/ingestion/helper_test.go
+++ operator/pkg/model/ingestion/helper_test.go
@@ -46,12 +46,40 @@ func toYaml(t *testing.T, obj any) string {
 	return strings.TrimSpace(string(yamlText))
 }
 
-func writeOutput(t *testing.T, file string, obj any) {
-	yamlText := toYaml(t, obj)
-
-	require.NoError(t, os.WriteFile(file, []byte(yamlText), 0644))
+// rewriteTestName rewrites a subname to having only printable characters and no white
+// space.
+// Copied from standard library testing package.
+func rewriteTestName(testName string) string {
+	b := []byte{}
+	for _, r := range testName {
+		switch {
+		case isSpace(r):
+			b = append(b, '_')
+		case !strconv.IsPrint(r):
+			s := strconv.QuoteRune(r)
+			b = append(b, s[1:len(s)-1]...)
+		default:
+			b = append(b, string(r)...)
+		}
+	}
+	return string(b)
 }
 
-func toTestDataDir(testName string) string {
-	return strings.Replace(strings.Replace(testName, " ", "_", -1), "-", "_", -1)
+func isSpace(r rune) bool {
+	if r < 0x2000 {
+		switch r {
+		// Note: not the same as Unicode Z class.
+		case '\t', '\n', '\v', '\f', '\r', ' ', 0x85, 0xA0, 0x1680:
+			return true
+		}
+	} else {
+		if r <= 0x200a {
+			return true
+		}
+		switch r {
+		case 0x2028, 0x2029, 0x202f, 0x205f, 0x3000:
+			return true
+		}
+	}
+	return false
 }
