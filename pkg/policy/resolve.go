67004d01a197b1dcfe1c3c1abbc57f9ffdb79dce
Jarno Rajahalme <jarno@isovalent.com>
2025-01-06 10:44:47 +0000

    policy: Rename newMapState() as emptyMapState()
    [ upstream commit 09ea9ee59e382a9e450197f8a877dabd8ea578d1 ]
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>
    Signed-off-by: Rastislav Szabo <rastislav.szabo@isovalent.com>

--- pkg/policy/resolve.go
+++ pkg/policy/resolve.go
@@ -166,7 +166,7 @@ func (p *selectorPolicy) DistillPolicy(policyOwner PolicyOwner, redirects map[st
 		calculatedPolicy = &EndpointPolicy{
 			selectorPolicy: p,
 			VersionHandle:  version,
-			policyMapState: newMapState(),
+			policyMapState: emptyMapState(),
 			policyMapChanges: MapChanges{
 				firstVersion: version.Version(),
 			},
@@ -421,6 +421,6 @@ func (p *EndpointPolicy) ConsumeMapChanges() (closer func(), changes ChangeState
 func NewEndpointPolicy(repo PolicyRepository) *EndpointPolicy {
 	return &EndpointPolicy{
 		selectorPolicy: newSelectorPolicy(repo.GetSelectorCache()),
-		policyMapState: newMapState(),
+		policyMapState: emptyMapState(),
 	}
 }
