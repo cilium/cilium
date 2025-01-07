cilium/cilium

commit 253a05bef38306624eb6675c16a4e90cb3e5c729

policy: Add config for enabling Cilium NetworkPolicy
Signed-off-by: Dorde Lapcevic <dordel@google.com>

pkg/policy/k8s/cell.go
@@ -139,13 +139,15 @@ func startK8sPolicyWatcher(params PolicyWatcherParams) {
 			return p.knpSynced.Load()
 		})
 	}
-	p.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumNetworkPolicyV2, func() bool {
-		return p.cnpSynced.Load() && p.cidrGroupSynced.Load()
-	})
-	p.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumClusterwideNetworkPolicyV2, func() bool {
-		return p.ccnpSynced.Load() && p.cidrGroupSynced.Load()
-	})
-	p.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumCIDRGroupV2Alpha1, func() bool {
-		return p.cidrGroupSynced.Load()
-	})
+	if params.Config.EnableCiliumNetworkPolicy {
+		p.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumNetworkPolicyV2, func() bool {
+			return p.cnpSynced.Load() && p.cidrGroupSynced.Load()
+		})
+		p.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumClusterwideNetworkPolicyV2, func() bool {
+			return p.ccnpSynced.Load() && p.cidrGroupSynced.Load()
+		})
+		p.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumCIDRGroupV2Alpha1, func() bool {
+			return p.cidrGroupSynced.Load()
+		})
+	}
 }

operator/pkg/networkpolicy/cell.go
@@ -72,6 +72,11 @@ func registerPolicyValidator(params PolicyParams) {
 		return
 	}
 
+	if !option.Config.EnableCiliumNetworkPolicy {
+		params.Logger.Infof("CNP / CCNP validator doesn't run when CNP / CCNP are disabled (%s=false)", option.EnableCiliumNetworkPolicy)
+		return
+	}
+
 	pv := &policyValidator{
 		params: &params,
 	}

daemon/cmd/daemon_main.go
@@ -1018,6 +1018,10 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.MarkHidden(option.EnableK8sNetworkPolicy)
 	option.BindEnv(vp, option.EnableK8sNetworkPolicy)
 
+	flags.Bool(option.EnableCiliumNetworkPolicy, defaults.EnableCiliumNetworkPolicy, "Enable support for Cilium Network Policy and Cilium Clusterwide Network Policy")
+	flags.MarkHidden(option.EnableCiliumNetworkPolicy)
+	option.BindEnv(vp, option.EnableCiliumNetworkPolicy)
+
 	flags.StringSlice(option.PolicyCIDRMatchMode, defaults.PolicyCIDRMatchMode, "The entities that can be selected by CIDR policy. Supported values: 'nodes'")
 	option.BindEnv(vp, option.PolicyCIDRMatchMode)
 

operator/cmd/flags.go
@@ -271,6 +271,10 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.MarkHidden(option.KVstoreLeaseTTL)
 	option.BindEnv(vp, option.KVstoreLeaseTTL)
 
+	flags.Bool(option.EnableCiliumNetworkPolicy, defaults.EnableCiliumNetworkPolicy, "Enable support for Cilium Network Policy and Cilium Clusterwide Network Policy")
+	flags.MarkHidden(option.EnableCiliumNetworkPolicy)
+	option.BindEnv(vp, option.EnableCiliumNetworkPolicy)
+
 	vp.BindPFlags(flags)
 }
 

operator/cmd/root.go
@@ -730,7 +730,7 @@ func (legacy *legacyOnLeader) onStart(_ cell.HookContext) error {
 		}
 	}
 
-	if legacy.clientset.IsEnabled() {
+	if legacy.clientset.IsEnabled() && option.Config.EnableCiliumNetworkPolicy {
 		err = enableCNPWatcher(legacy.ctx, &legacy.wg, legacy.clientset)
 		if err != nil {
 			log.WithError(err).WithField(logfields.LogSubsys, "CNPWatcher").Fatal(

pkg/defaults/defaults.go
@@ -533,6 +533,10 @@ const (
 	// EnableK8sNetworkPolicy enables support for K8s NetworkPolicy.
 	EnableK8sNetworkPolicy = true
 
+	// EnableCiliumNetworkPolicy enables support for Cilium Network Policy and
+	// Cilium Clusterwide Network Policy.
+	EnableCiliumNetworkPolicy = true
+
 	// MaxConnectedClusters sets the maximum number of clusters that can be
 	// connected in a clustermesh.
 	// The value is used to determine the bit allocation for cluster ID and

pkg/k8s/synced/crd.go
@@ -37,11 +37,8 @@ func CRDResourceName(crd string) string {
 
 func agentCRDResourceNames() []string {
 	result := []string{
-		CRDResourceName(v2.CNPName),
-		CRDResourceName(v2.CCNPName),
 		CRDResourceName(v2.CNName),
 		CRDResourceName(v2.CIDName),
-		CRDResourceName(v2alpha1.CCGName),
 		CRDResourceName(v2alpha1.CPIPName),
 	}
 
@@ -50,6 +47,12 @@ func agentCRDResourceNames() []string {
 		if option.Config.EnableCiliumEndpointSlice {
 			result = append(result, CRDResourceName(v2alpha1.CESName))
 		}
+	}
+
+	if option.Config.EnableCiliumNetworkPolicy {
+		result = append(result, CRDResourceName(v2.CNPName))
+		result = append(result, CRDResourceName(v2.CCNPName))
+		result = append(result, CRDResourceName(v2alpha1.CCGName))
 	}
 
 	if option.Config.EnableIPv4EgressGateway {

pkg/option/config.go
@@ -1099,6 +1099,10 @@ const (
 	// EnableK8sNetworkPolicy enables support for K8s NetworkPolicy.
 	EnableK8sNetworkPolicy = "enable-k8s-networkpolicy"
 
+	// EnableCiliumNetworkPolicy enables support for Cilium Network Policy and
+	// Cilium Clusterwide Network Policy.
+	EnableCiliumNetworkPolicy = "enable-cilium-network-policy"
+
 	// PolicyCIDRMatchMode defines the entities that CIDR selectors can reach
 	PolicyCIDRMatchMode = "policy-cidr-match-mode"
 
@@ -2177,6 +2181,10 @@ type DaemonConfig struct {
 	// EnableK8sNetworkPolicy enables support for K8s NetworkPolicy.
 	EnableK8sNetworkPolicy bool
 
+	// EnableCiliumNetworkPolicy Enable support for Cilium Network Policy and
+	// Cilium Clusterwide Network Policy.
+	EnableCiliumNetworkPolicy bool
+
 	// PolicyCIDRMatchMode is the list of entities that can be selected by CIDR policy.
 	// Currently supported values:
 	// - world
@@ -2255,12 +2263,13 @@ var (
 
 		K8sEnableLeasesFallbackDiscovery: defaults.K8sEnableLeasesFallbackDiscovery,
 
-		ExternalClusterIP:      defaults.ExternalClusterIP,
-		EnableVTEP:             defaults.EnableVTEP,
-		EnableBGPControlPlane:  defaults.EnableBGPControlPlane,
-		EnableK8sNetworkPolicy: defaults.EnableK8sNetworkPolicy,
-		PolicyCIDRMatchMode:    defaults.PolicyCIDRMatchMode,
-		MaxConnectedClusters:   defaults.MaxConnectedClusters,
+		ExternalClusterIP:         defaults.ExternalClusterIP,
+		EnableVTEP:                defaults.EnableVTEP,
+		EnableBGPControlPlane:     defaults.EnableBGPControlPlane,
+		EnableK8sNetworkPolicy:    defaults.EnableK8sNetworkPolicy,
+		EnableCiliumNetworkPolicy: defaults.EnableCiliumNetworkPolicy,
+		PolicyCIDRMatchMode:       defaults.PolicyCIDRMatchMode,
+		MaxConnectedClusters:      defaults.MaxConnectedClusters,
 
 		BPFEventsDropEnabled:          defaults.BPFEventsDropEnabled,
 		BPFEventsPolicyVerdictEnabled: defaults.BPFEventsPolicyVerdictEnabled,
@@ -3220,6 +3229,8 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 	c.PolicyCIDRMatchMode = vp.GetStringSlice(PolicyCIDRMatchMode)
 	c.EnableNodeSelectorLabels = vp.GetBool(EnableNodeSelectorLabels)
 	c.NodeLabels = vp.GetStringSlice(NodeLabels)
+
+	c.EnableCiliumNetworkPolicy = vp.GetBool(EnableCiliumNetworkPolicy)
 
 	// Parse node label patterns
 	nodeLabelPatterns := vp.GetStringSlice(ExcludeNodeLabelPatterns)

commit 624d4390ffab66024e0fdde21627bfce433065e8

policy: don't subscribe to service events if CNPs/CCNPs are disabled
Service events are not used when CNPs and CCNPs are disabled. However,
we currently subscribe to the service cache, and enqueue all events in
a local cache, in turn causing a memory leak in that case given that
the events would never be dequeued. Let's get this fixed.

Fixes: 253a05bef383 ("policy: Add config for enabling Cilium NetworkPolicy")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/policy/k8s/cell.go
@@ -93,7 +93,6 @@ func startK8sPolicyWatcher(params PolicyWatcherParams) {
 	// We want to subscribe before the start hook is invoked in order to not miss
 	// any events
 	ctx, cancel := context.WithCancel(context.Background())
-	svcCacheNotifications := serviceNotificationsQueue(ctx, params.ServiceCache.Notifications())
 
 	p := &policyWatcher{
 		log:                              params.Logger,
@@ -102,7 +101,6 @@ func startK8sPolicyWatcher(params PolicyWatcherParams) {
 		k8sResourceSynced:                params.K8sResourceSynced,
 		k8sAPIGroups:                     params.K8sAPIGroups,
 		svcCache:                         params.ServiceCache,
-		svcCacheNotifications:            svcCacheNotifications,
 		ipCache:                          params.IPCache,
 		ciliumNetworkPolicies:            params.CiliumNetworkPolicies,
 		ciliumClusterwideNetworkPolicies: params.CiliumClusterwideNetworkPolicies,
@@ -116,6 +114,11 @@ func startK8sPolicyWatcher(params PolicyWatcherParams) {
 		toServicesPolicies: make(map[resource.Key]struct{}),
 		cnpByServiceID:     make(map[k8s.ServiceID]map[resource.Key]struct{}),
 		metricsManager:     params.MetricsManager,
+	}
+
+	// Service notifications are not used if CNPs/CCNPs are disabled.
+	if params.Config.EnableCiliumNetworkPolicy || params.Config.EnableCiliumClusterwideNetworkPolicy {
+		p.svcCacheNotifications = serviceNotificationsQueue(ctx, params.ServiceCache.Notifications())
 	}
 
 	params.Lifecycle.Append(cell.Hook{