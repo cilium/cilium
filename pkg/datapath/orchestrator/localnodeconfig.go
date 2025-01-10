d427ce38e6feeaca8305e5970f162e00f8fee68c
Julian Wiedmann <jwi@isovalent.com>
2025-01-10 13:32:40 +0000

    localnodeconfig: dedup cluster routing mode
    [ upstream commit 6f137bd6274811877198df6eb3087e60baf53774 ]
    
    `RoutingMode` and `EnableEncapsulation` have the same semantics. Slim it
    down to a single config variable.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Rastislav Szabo <rastislav.szabo@isovalent.com>

--- pkg/datapath/orchestrator/localnodeconfig.go
+++ pkg/datapath/orchestrator/localnodeconfig.go
@@ -101,6 +101,5 @@ func newLocalNodeConfig(
 		IPv4PodSubnets:               cidr.NewCIDRSlice(config.IPv4PodSubnets),
 		IPv6PodSubnets:               cidr.NewCIDRSlice(config.IPv6PodSubnets),
 		XDPConfig:                    xdpConfig,
-		RoutingMode:                  config.RoutingMode,
 	}, common.MergeChannels(devsWatch, addrsWatch, directRoutingDevWatch, mtuWatch), nil
 }
