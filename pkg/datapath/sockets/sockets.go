fd57f71dee6d03835dce99a73cdd5207bf5c3ce9
Fabio Falzoi <fabio.falzoi@isovalent.com>
2025-01-10 12:12:30 +0100

    pkg/datapath/sockets: Avoid manual buffer management
    Avoid manual buffer management in socket request struct serialization
    and socket struct deserialization.
    
    The previous implementation relied on two helper structures, readBuffer
    and writeBuffer, to manually manage the underlying deserialization and
    serialization buffer in order to avoid allocations.
    
    The new implementation relies on standard library facilities only, is
    more concise and guarantees the same amount of memory allocations and
    comparable performance in terms of CPU cycles.
    
    At the core of the new implementation is the bytes.Buffer type. On
    the deserialization side, it is practically a 1:1 translation from the
    previous approach, since the bytes.Buffer offers an equivalent Next method.
    On the
    serialization side, the bytes.Buffer can be used in place of the
    readBuffer thanks to the AvailableBuffer method, that allows to expose
    the underlying buffer for append-like operations (like AppendUint16,
    AppendUint32 and so on). This, in combination with binary.NativeEndian
    and binary.BigEndian that implement the binary.AppendByteOrder interface,
    allows for an equivalent implementation of the previous serialization
    that minimize memory allocations.
    
    The only downside of the new approach is a slight performance
    degradation in terms of CPU cycles for the socket request serialization.
    This is due to the bytes.Buffer not having a method to advance the
    internal buffer offset without writing anything. To overcome this
    limitation and avoid allocations, we are forced to write zero bytes in a
    loop.  Given the number of calls to the serialization logic, this should
    not be of any concern.
    
    goos: linux
    goarch: amd64
    pkg: github.com/cilium/cilium/pkg/datapath/sockets
    cpu: AMD Ryzen 9 3900X 12-Core Processor
                          │   old.txt   │               new.txt                │
                          │   sec/op    │   sec/op     vs base                 │
    SocketReqSerialize-24   209.0n ± 2%   497.1n ± 4%  +137.85% (p=0.000 n=10)
    SocketDeserialize-24    291.6n ± 4%   266.5n ± 5%    -8.62% (p=0.000 n=10)
    geomean                 246.9n        364.0n        +47.42%
    
                          │  old.txt   │               new.txt               │
                          │    B/op    │    B/op     vs base                 │
    SocketReqSerialize-24   128.0 ± 0%   128.0 ± 0%       ~ (p=1.000 n=10) ¹
    SocketDeserialize-24    64.00 ± 0%   64.00 ± 0%       ~ (p=1.000 n=10) ¹
    geomean                 90.51        90.51       +0.00%
    ¹ all samples are equal
    
                          │  old.txt   │               new.txt               │
                          │ allocs/op  │ allocs/op   vs base                 │
    SocketReqSerialize-24   2.000 ± 0%   2.000 ± 0%       ~ (p=1.000 n=10) ¹
    SocketDeserialize-24    4.000 ± 0%   4.000 ± 0%       ~ (p=1.000 n=10) ¹
    geomean                 2.828        2.828       +0.00%
    ¹ all samples are equal
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/datapath/sockets/sockets.go
+++ pkg/datapath/sockets/sockets.go
@@ -4,6 +4,7 @@
 package sockets
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"fmt"
@@ -28,7 +29,7 @@ const (
 
 var (
 	log          = logging.DefaultLogger.WithField(logfields.LogSubsys, "datapath-sockets")
-	native       = nl.NativeEndian()
+	native       = binary.NativeEndian
 	networkOrder = binary.BigEndian
 )
 
@@ -125,8 +126,6 @@ func filterAndDestroyUDPSockets(family uint8, socketCB func(socket netlink.Socke
 	return nil
 }
 
-// Below handlers are adapted from netlink/socket_linux.go to avoid memory allocations.
-
 type socketRequest struct {
 	Family   uint8
 	Protocol uint8
@@ -136,95 +135,82 @@ type socketRequest struct {
 	ID       netlink.SocketID
 }
 
-type writeBuffer struct {
-	Bytes []byte
-	pos   int
-}
+func (r socketRequest) Serialize() []byte {
+	var bb bytes.Buffer
 
-func (b *writeBuffer) write(c byte) {
-	b.Bytes[b.pos] = c
-	b.pos++
-}
+	bb.Grow(sizeofSocketRequest)
 
-func (b *writeBuffer) next(n int) []byte {
-	s := b.Bytes[b.pos : b.pos+n]
-	b.pos += n
-	return s
-}
+	bb.WriteByte(r.Family)
+	bb.WriteByte(r.Protocol)
+	bb.WriteByte(r.Ext)
+	bb.WriteByte(r.pad)
+	b := bb.AvailableBuffer()
+	b = native.AppendUint32(b, r.States)
+	b = networkOrder.AppendUint16(b, r.ID.SourcePort)
+	b = networkOrder.AppendUint16(b, r.ID.DestinationPort)
+	bb.Write(b)
+	serializeAddr(&bb, r.Family, r.ID.Source)
+	serializeAddr(&bb, r.Family, r.ID.Destination)
+	b = bb.AvailableBuffer()
+	b = native.AppendUint32(b, r.ID.Interface)
+	b = native.AppendUint32(b, r.ID.Cookie[0])
+	b = native.AppendUint32(b, r.ID.Cookie[1])
+	bb.Write(b)
 
-func (r *socketRequest) Serialize() []byte {
-	b := writeBuffer{Bytes: make([]byte, sizeofSocketRequest)}
-	b.write(r.Family)
-	b.write(r.Protocol)
-	b.write(r.Ext)
-	b.write(r.pad)
-	native.PutUint32(b.next(4), r.States)
-	networkOrder.PutUint16(b.next(2), r.ID.SourcePort)
-	networkOrder.PutUint16(b.next(2), r.ID.DestinationPort)
-	if r.Family == unix.AF_INET6 {
-		copy(b.next(16), r.ID.Source)
-		copy(b.next(16), r.ID.Destination)
-	} else {
-		copy(b.next(4), r.ID.Source.To4())
-		b.next(12)
-		copy(b.next(4), r.ID.Destination.To4())
-		b.next(12)
-	}
-	native.PutUint32(b.next(4), r.ID.Interface)
-	native.PutUint32(b.next(4), r.ID.Cookie[0])
-	native.PutUint32(b.next(4), r.ID.Cookie[1])
-	return b.Bytes
+	return bb.Bytes()
 }
 
 func (r *socketRequest) Len() int { return sizeofSocketRequest }
 
-type readBuffer struct {
-	Bytes []byte
-	pos   int
-}
-
-func (b *readBuffer) Read() byte {
-	c := b.Bytes[b.pos]
-	b.pos++
-	return c
-}
-
-func (b *readBuffer) Next(n int) []byte {
-	s := b.Bytes[b.pos : b.pos+n]
-	b.pos += n
-	return s
+func serializeAddr(bb *bytes.Buffer, family uint8, addr net.IP) {
+	if addr == nil {
+		for range net.IPv6len {
+			bb.WriteByte(0)
+		}
+		return
+	}
+	if family == unix.AF_INET6 {
+		bb.Write(addr)
+	} else {
+		bb.Write(addr.To4())
+		for range net.IPv6len - net.IPv4len {
+			bb.WriteByte(0)
+		}
+	}
 }
 
 type socket netlink.Socket
 
 func (s *socket) deserialize(b []byte) error {
+	// early size check to guarantee safety of reads below
 	if len(b) < sizeofSocket {
 		return fmt.Errorf("socket data short read (%d); want %d", len(b), sizeofSocket)
 	}
-	rb := readBuffer{Bytes: b}
-	s.Family = rb.Read()
-	s.State = rb.Read()
-	s.Timer = rb.Read()
-	s.Retrans = rb.Read()
-	s.ID.SourcePort = networkOrder.Uint16(rb.Next(2))
-	s.ID.DestinationPort = networkOrder.Uint16(rb.Next(2))
+
+	bb := bytes.NewBuffer(b)
+	s.Family, _ = bb.ReadByte()
+	s.State, _ = bb.ReadByte()
+	s.Timer, _ = bb.ReadByte()
+	s.Retrans, _ = bb.ReadByte()
+	s.ID.SourcePort = networkOrder.Uint16(bb.Next(2))
+	s.ID.DestinationPort = networkOrder.Uint16(bb.Next(2))
 	if s.Family == unix.AF_INET6 {
-		s.ID.Source = net.IP(rb.Next(16))
-		s.ID.Destination = net.IP(rb.Next(16))
+		s.ID.Source = net.IP(bb.Next(net.IPv6len))
+		s.ID.Destination = net.IP(bb.Next(net.IPv6len))
 	} else {
-		s.ID.Source = net.IPv4(rb.Read(), rb.Read(), rb.Read(), rb.Read())
-		rb.Next(12)
-		s.ID.Destination = net.IPv4(rb.Read(), rb.Read(), rb.Read(), rb.Read())
-		rb.Next(12)
+		src := bb.Next(net.IPv6len)
+		s.ID.Source = net.IPv4(src[0], src[1], src[2], src[3])
+		dst := bb.Next(net.IPv6len)
+		s.ID.Destination = net.IPv4(dst[0], dst[1], dst[2], dst[3])
 	}
-	s.ID.Interface = native.Uint32(rb.Next(4))
-	s.ID.Cookie[0] = native.Uint32(rb.Next(4))
-	s.ID.Cookie[1] = native.Uint32(rb.Next(4))
-	s.Expires = native.Uint32(rb.Next(4))
-	s.RQueue = native.Uint32(rb.Next(4))
-	s.WQueue = native.Uint32(rb.Next(4))
-	s.UID = native.Uint32(rb.Next(4))
-	s.INode = native.Uint32(rb.Next(4))
+	s.ID.Interface = native.Uint32(bb.Next(4))
+	s.ID.Cookie[0] = native.Uint32(bb.Next(4))
+	s.ID.Cookie[1] = native.Uint32(bb.Next(4))
+	s.Expires = native.Uint32(bb.Next(4))
+	s.RQueue = native.Uint32(bb.Next(4))
+	s.WQueue = native.Uint32(bb.Next(4))
+	s.UID = native.Uint32(bb.Next(4))
+	s.INode = native.Uint32(bb.Next(4))
 	return nil
 }
 
