6a4bb33a2ad59dcb9699e0caf4f81002a83375f7
Marco Iorio <marco.iorio@isovalent.com>
2025-01-10 15:13:56 +0100

    cilium-cli sysdump: output yaml lists one element at a time
    The yaml marshaler appears to consume a significant amount of memory
    when processing large objects (mostly lists of pods/nodes in the context
    of sysdumps) and does not seem to support streaming processing out of
    the box. In an effort to reduce the RAM requirements during sysdump
    collection, let's leverage a manual approach to independently marshal
    each list item independently, immediately writing the output into the
    target file. Let's also introduce a test to validate that we respect
    the expected format, to prevent breaking changes.
    
    Overall, processing one list item at a time reduced by approx. 5 times
    the memory requirements both considering the retrieval of 1K nodes
    (150MB/625MB RSS) and the full sysdump collection against a 1K
    nodes cluster (2.5GB/14GB RSS).
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- cilium-cli/sysdump/writers.go
+++ cilium-cli/sysdump/writers.go
@@ -8,16 +8,19 @@ import (
 	_ "embed"
 	"fmt"
 	"html/template"
+	"io"
 	"os"
 	"sort"
 	"strings"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/cli-runtime/pkg/printers"
 	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/yaml"
 )
 
 //go:embed eventSummary.html
@@ -42,17 +45,50 @@ func writeTable(p string, o *metav1.Table) error {
 	return writeString(p, b.String())
 }
 
-func writeYaml(p string, o runtime.Object) error {
-	var j printers.YAMLPrinter
-	w, err := printers.NewTypeSetter(scheme.Scheme).WrapToPrinter(&j, nil)
-	if err != nil {
-		return err
+func writeYAML(obj runtime.Object, out io.Writer) error {
+	delegate := printers.ResourcePrinter(&printers.YAMLPrinter{})
+	if meta.IsListType(obj) && meta.LenList(obj) > 0 {
+		// Print lists one item at a time, to reduce the amount of memory used by the printer.
+		delegate = printers.ResourcePrinterFunc(func(obj runtime.Object, out io.Writer) error {
+			_, err := fmt.Fprintf(out, "apiVersion: %s\nitems:\n", obj.GetObjectKind().GroupVersionKind().GroupVersion())
+			if err != nil {
+				return err
+			}
+
+			if err := meta.EachListItem(obj, func(item runtime.Object) error {
+				data, err := yaml.Marshal(item)
+				if err != nil {
+					return err
+				}
+
+				_, err = indentAsListItem(data, out)
+				return err
+			}); err != nil {
+				return err
+			}
+
+			_, err = fmt.Fprintf(out, "kind: %s\n", obj.GetObjectKind().GroupVersionKind().Kind)
+			if err != nil {
+				return err
+			}
+
+			if listMeta, err := meta.ListAccessor(obj); err == nil {
+				if rv := listMeta.GetResourceVersion(); rv != "" {
+					_, err = fmt.Fprintf(out, "metadata:\n  resourceVersion: %q\n", rv)
+				} else {
+					_, err = fmt.Fprint(out, "metadata: {}\n", rv)
+				}
+
+				return err
+			}
+
+			return nil
+		})
 	}
-	var b bytes.Buffer
-	if err := w.PrintObj(o, &b); err != nil {
-		return err
-	}
-	return writeString(p, b.String())
+
+	// WrapToPrinter never returns an error, since we don't provide one.
+	printer, _ := printers.NewTypeSetter(scheme.Scheme).WrapToPrinter(delegate, nil)
+	return printer.PrintObj(obj, out)
 }
 
 // writeEventTable writes a html summary of cluster events.
@@ -99,4 +135,32 @@ func makeEventTable(events []corev1.Event) []byte {
 		panic(err) // unreachable
 	}
 	return out.Bytes()
+}
+
+func indentAsListItem(in []byte, out io.Writer) (cnt int, err error) {
+	handle := func(n int, err error) error {
+		cnt += n
+		return err
+	}
+
+	indent := []byte("- ")
+	for len(in) > 0 {
+		to := bytes.IndexAny(in, "\n")
+		if to == -1 {
+			to = len(in)
+		} else {
+			to++
+		}
+
+		if err = handle(out.Write(indent)); err != nil {
+			return cnt, err
+		}
+		if err = handle(out.Write(in[:to])); err != nil {
+			return cnt, err
+		}
+
+		in, indent = in[to:], []byte("  ")
+	}
+
+	return cnt, nil
 }
