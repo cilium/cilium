d7b4766faf08cad25173c8d21909b498adcd1282
Fabio Falzoi <fabio.falzoi@isovalent.com>
2025-01-10 12:09:24 +0100

    pkg/datapath/sockets: Add test and benchmark for socket deserialize
    Add a unit test and a micro benchmark for socket.deserialize.  This
    is useful in a subsequent commit, where an alternative implementation
    with an equal number of allocations will be added.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/datapath/sockets/sockets_test.go
+++ pkg/datapath/sockets/sockets_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-func TestSerialize(t *testing.T) {
+func TestSocketReqSerialize(t *testing.T) {
 	testCases := []struct {
 		name     string
 		req      socketRequest
@@ -63,7 +63,70 @@ func TestSerialize(t *testing.T) {
 	}
 }
 
-func BenchmarkSerialize(b *testing.B) {
+func TestSocketDeserialize(t *testing.T) {
+	testCases := []struct {
+		name     string
+		buf      []byte
+		expected socket
+	}{
+		{
+			name: "default route addresses",
+			buf:  []byte{2, 7, 0, 0, 170, 213, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 108, 0, 0, 0, 89, 101, 0, 0, 5, 0, 8, 0, 0, 0, 0, 0, 8, 0, 15, 0, 0, 0, 0, 0, 12, 0, 21, 0, 157, 14, 0, 0, 0, 0, 0, 0, 6, 0, 22, 0, 80, 0, 0, 0},
+			expected: socket{
+				Family:  2,
+				State:   7,
+				Timer:   0,
+				Retrans: 0,
+				ID: netlink.SocketID{
+					SourcePort:      43733,
+					DestinationPort: 0,
+					Source:          net.ParseIP("0.0.0.0"),
+					Destination:     net.ParseIP("0.0.0.0"),
+					Interface:       0,
+					Cookie:          [2]uint32{8201, 0},
+				},
+				Expires: 0,
+				RQueue:  0,
+				WQueue:  0,
+				UID:     108,
+				INode:   25945,
+			},
+		},
+		{
+			name: "non default route addresses",
+			buf:  []byte{2, 1, 0, 0, 189, 137, 1, 187, 192, 168, 50, 194, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 151, 99, 52, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 19, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 232, 3, 0, 0, 146, 138, 10, 0, 5, 0, 8, 0, 0, 0, 0, 0, 8, 0, 15, 0, 0, 0, 0, 0, 12, 0, 21, 0, 1, 42, 0, 0, 0, 0, 0, 0, 6, 0, 22, 0, 80, 0, 0, 0},
+			expected: socket{
+				Family:  2,
+				State:   1,
+				Timer:   0,
+				Retrans: 0,
+				ID: netlink.SocketID{
+					SourcePort:      48521,
+					DestinationPort: 443,
+					Source:          net.ParseIP("192.168.50.194"),
+					Destination:     net.ParseIP("151.99.52.13"),
+					Interface:       0,
+					Cookie:          [2]uint32{8211, 0},
+				},
+				Expires: 0,
+				RQueue:  0,
+				WQueue:  0,
+				UID:     1000,
+				INode:   690834,
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var sock socket
+			err := sock.deserialize(tc.buf)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, sock)
+		})
+	}
+}
+
+func BenchmarkSocketReqSerialize(b *testing.B) {
 	requests := [...]socketRequest{
 		{
 			Family:   2,
@@ -100,6 +163,20 @@ func BenchmarkSerialize(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, req := range requests {
 			req.Serialize()
+		}
+	}
+}
+
+func BenchmarkSocketDeserialize(b *testing.B) {
+	buffers := [...][]byte{
+		{2, 7, 0, 0, 170, 213, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 108, 0, 0, 0, 89, 101, 0, 0, 5, 0, 8, 0, 0, 0, 0, 0, 8, 0, 15, 0, 0, 0, 0, 0, 12, 0, 21, 0, 157, 14, 0, 0, 0, 0, 0, 0, 6, 0, 22, 0, 80, 0, 0, 0},
+		{2, 1, 0, 0, 189, 137, 1, 187, 192, 168, 50, 194, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 151, 99, 52, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 19, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 232, 3, 0, 0, 146, 138, 10, 0, 5, 0, 8, 0, 0, 0, 0, 0, 8, 0, 15, 0, 0, 0, 0, 0, 12, 0, 21, 0, 1, 42, 0, 0, 0, 0, 0, 0, 6, 0, 22, 0, 80, 0, 0, 0},
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, buf := range buffers {
+			var sock socket
+			sock.deserialize(buf)
 		}
 	}
 }
