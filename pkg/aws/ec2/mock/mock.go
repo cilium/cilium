e024e1e38cfcb3e96b8c08c4c70c7f3ad6a7a374
Liyi Huang <pdshly@gmail.com>
2025-01-11 13:56:01 -0500

    ipam/aws: enhance findSuitableSubnet logic
    - Implement struct to get route table from ec2 api
    - Implement new logic and function to enhance the findSuitableSubnet logic
    - Updates the unit test and mock ec2 api
    - Update the doc
    
    Signed-off-by: Liyi Huang  <liyi.huang@isovalent.com>

--- pkg/aws/ec2/mock/mock.go
+++ pkg/aws/ec2/mock/mock.go
@@ -53,6 +53,7 @@ type API struct {
 	enis           map[string]ENIMap
 	subnets        map[string]*ipamTypes.Subnet
 	vpcs           map[string]*ipamTypes.VirtualNetwork
+	routeTables    map[string]*ipamTypes.RouteTable
 	securityGroups map[string]*types.SecurityGroup
 	instanceTypes  []ec2_types.InstanceTypeInfo
 	errors         map[Operation]error
@@ -64,7 +65,7 @@ type API struct {
 }
 
 // NewAPI returns a new mocked EC2 API
-func NewAPI(subnets []*ipamTypes.Subnet, vpcs []*ipamTypes.VirtualNetwork, securityGroups []*types.SecurityGroup) *API {
+func NewAPI(subnets []*ipamTypes.Subnet, vpcs []*ipamTypes.VirtualNetwork, securityGroups []*types.SecurityGroup, routeTables []*ipamTypes.RouteTable) *API {
 
 	// Start with base CIDR 10.0.0.0/16
 	_, baseCidr, _ := net.ParseCIDR("10.10.0.0/16")
@@ -86,6 +87,7 @@ func NewAPI(subnets []*ipamTypes.Subnet, vpcs []*ipamTypes.VirtualNetwork, secur
 		enis:           map[string]ENIMap{},
 		subnets:        map[string]*ipamTypes.Subnet{},
 		vpcs:           map[string]*ipamTypes.VirtualNetwork{},
+		routeTables:    map[string]*ipamTypes.RouteTable{},
 		securityGroups: map[string]*types.SecurityGroup{},
 		instanceTypes:  []ec2_types.InstanceTypeInfo{},
 		allocator:      podCidrRange,
@@ -97,7 +99,7 @@ func NewAPI(subnets []*ipamTypes.Subnet, vpcs []*ipamTypes.VirtualNetwork, secur
 
 	api.UpdateSubnets(subnets)
 	api.UpdateSecurityGroups(securityGroups)
-
+	api.UpdateRouteTables(routeTables)
 	for _, v := range vpcs {
 		api.vpcs[v.ID] = v
 	}
@@ -111,6 +113,16 @@ func (e *API) UpdateSubnets(subnets []*ipamTypes.Subnet) {
 	e.subnets = map[string]*ipamTypes.Subnet{}
 	for _, s := range subnets {
 		e.subnets[s.ID] = s.DeepCopy()
+	}
+	e.mutex.Unlock()
+}
+
+// UpdateRouteTables replaces the route tables which the mock API will return
+func (e *API) UpdateRouteTables(routeTables []*ipamTypes.RouteTable) {
+	e.mutex.Lock()
+	e.routeTables = map[string]*ipamTypes.RouteTable{}
+	for _, rt := range routeTables {
+		e.routeTables[rt.ID] = rt.DeepCopy()
 	}
 	e.mutex.Unlock()
 }
@@ -651,6 +663,18 @@ func (e *API) GetSubnets(ctx context.Context) (ipamTypes.SubnetMap, error) {
 		subnets[s.ID] = s.DeepCopy()
 	}
 	return subnets, nil
+}
+
+func (e *API) GetRouteTables(ctx context.Context) (ipamTypes.RouteTableMap, error) {
+	routeTables := ipamTypes.RouteTableMap{}
+
+	e.mutex.RLock()
+	defer e.mutex.RUnlock()
+
+	for _, rt := range e.routeTables {
+		routeTables[rt.ID] = rt.DeepCopy()
+	}
+	return routeTables, nil
 }
 
 func (e *API) TagENI(ctx context.Context, eniID string, eniTags map[string]string) error {
