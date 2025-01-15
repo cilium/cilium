8405df0eeb70adc413621ae324c7fa8932d7495d
Joe Stringer <joe@cilium.io>
2025-01-14 16:43:08 -0800

    helm: Support extraConfig.allowUnsafePolicySKBUsage
    Let the user configure the new quirks.allowUnsafePolicySKBUsage flag and
    have that also bypass the helm-level check for the same issue. See
    GH-21330 for more information about the underlying limitation.
    
    Tested by force-enabling the IPAM options in the Helm template plus
    applying this configuration YAML:
    
    cluster:
      id: 166
    extraConfig:
      allow-unsafe-policy-skb-usage: "true"
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/clustermesh/cell.go
+++ pkg/clustermesh/cell.go
@@ -5,6 +5,7 @@ package clustermesh
 
 import (
 	"github.com/cilium/hive/cell"
+	"github.com/sirupsen/logrus"
 
 	"github.com/cilium/cilium/daemon/cmd/cni"
 	"github.com/cilium/cilium/pkg/clustermesh/common"
@@ -40,10 +41,10 @@ var Cell = cell.Module(
 	metrics.Metric(common.MetricsProvider(subsystem)),
 
 	cell.Config(types.DefaultQuirks),
-	cell.Invoke(func(info types.ClusterInfo, dcfg *option.DaemonConfig, cnimgr cni.CNIConfigManager, cfg Configuration, quirks types.QuirksConfig) error {
+	cell.Invoke(func(info types.ClusterInfo, dcfg *option.DaemonConfig, cnimgr cni.CNIConfigManager, log logrus.FieldLogger, quirks types.QuirksConfig) error {
 		err := info.ValidateBuggyClusterID(dcfg.IPAM, cnimgr.GetChainingMode())
 		if err != nil && quirks.AllowUnsafePolicySKBUsage {
-			cfg.Logger.WithError(err).Error("Detected clustermesh ID configuration that may cause connection impact")
+			log.WithError(err).Error("Detected clustermesh ID configuration that may cause connection impact")
 			return nil
 		}
 		return err
