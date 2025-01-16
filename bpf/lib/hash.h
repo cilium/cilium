10c4f53b5ce6802fb5b5caeff09c08c96a5e1f59
Daniel Borkmann <daniel@iogearbox.net>
2025-01-16 08:50:00 +0000

    bpf: Refactor hashing functions to take in sport as argument
    No functional change, refactor the helper so that custom sport can be
    passed into the hashing function.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- bpf/lib/hash.h
+++ bpf/lib/hash.h
@@ -9,14 +9,21 @@
 /* The daddr is explicitly excluded from the hash here in order to allow for
  * backend selection to choose the same backend even on different service VIPs.
  */
-static __always_inline __u32 hash_from_tuple_v4(const struct ipv4_ct_tuple *tuple)
+static __always_inline __u32
+__hash_from_tuple_v4(const struct ipv4_ct_tuple *tuple, __be16 sport)
 {
 	return jhash_3words(tuple->saddr,
-			    ((__u32)tuple->dport << 16) | tuple->sport,
+			    ((__u32)tuple->dport << 16) | sport,
 			    tuple->nexthdr, HASH_INIT4_SEED);
 }
 
-static __always_inline __u32 hash_from_tuple_v6(const struct ipv6_ct_tuple *tuple)
+static __always_inline __u32 hash_from_tuple_v4(const struct ipv4_ct_tuple *tuple)
+{
+	return __hash_from_tuple_v4(tuple, tuple->sport);
+}
+
+static __always_inline __u32
+__hash_from_tuple_v6(const struct ipv6_ct_tuple *tuple, __be16 sport)
 {
 	__u32 a, b, c;
 
@@ -25,10 +32,15 @@ static __always_inline __u32 hash_from_tuple_v6(const struct ipv6_ct_tuple *tupl
 	c = tuple->saddr.p3;
 	__jhash_mix(a, b, c);
 	a += tuple->saddr.p4;
-	b += ((__u32)tuple->dport << 16) | tuple->sport;
+	b += ((__u32)tuple->dport << 16) | sport;
 	c += tuple->nexthdr;
 	__jhash_mix(a, b, c);
 	a += HASH_INIT6_SEED;
 	__jhash_final(a, b, c);
 	return c;
+}
+
+static __always_inline __u32 hash_from_tuple_v6(const struct ipv6_ct_tuple *tuple)
+{
+	return __hash_from_tuple_v6(tuple, tuple->sport);
 }
