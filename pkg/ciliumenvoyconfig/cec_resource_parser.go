cilium/cilium

commit 70a4979ebb5993cd043d937413419804887de750

envoy: Increase per-cluster concurrent retry limit
Envoy default limit on concurrent retries on a cluster is 3. This has
been seen hit on CI when tests use multiple parallel requests from
curl. Increase the limit to 128 via a new helm value
'envoy.maxConcurrentRetries' and Cilium Agent command line argument
'--proxy-max-concurrent-retries'.

Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

pkg/ciliumenvoyconfig/cec_resource_parser.go
@@ -56,6 +56,8 @@ type cecResourceParser struct {
 
 	ingressIPv4 net.IP
 	ingressIPv6 net.IP
+
+	defaultMaxConcurrentRetries uint32
 }
 
 type parserParams struct {
@@ -66,12 +68,15 @@ type parserParams struct {
 
 	PortAllocator  PortAllocator
 	LocalNodeStore *node.LocalNodeStore
+
+	CecConfig cecConfig
 }
 
 func newCECResourceParser(params parserParams) *cecResourceParser {
 	parser := &cecResourceParser{
-		logger:        params.Logger,
-		portAllocator: params.PortAllocator,
+		logger:                      params.Logger,
+		portAllocator:               params.PortAllocator,
+		defaultMaxConcurrentRetries: params.CecConfig.ProxyMaxConcurrentRetries,
 	}
 
 	// Retrieve Ingress IPs from local Node.
@@ -312,6 +317,8 @@ func (r *cecResourceParser) parseResources(cecNamespace string, cecName string, 
 			}
 
 			fillInTransportSocketXDS(cecNamespace, cecName, cluster.TransportSocket)
+
+			fillInCircuitBreakers(cluster, r.defaultMaxConcurrentRetries)
 
 			// Fill in EDS config source if unset
 			if enum := cluster.GetType(); enum == envoy_config_cluster.Cluster_EDS {
@@ -902,6 +909,16 @@ func fillInTransportSocketXDS(cecNamespace string, cecName string, ts *envoy_con
 					TypedConfig: updated,
 				}
 			}
+		}
+	}
+}
+
+func fillInCircuitBreakers(cluster *envoy_config_cluster.Cluster, defaultConcurrentRetries uint32) {
+	if cluster.CircuitBreakers == nil {
+		cluster.CircuitBreakers = &envoy_config_cluster.CircuitBreakers{
+			Thresholds: []*envoy_config_cluster.CircuitBreakers_Thresholds{{
+				MaxRetries: &wrapperspb.UInt32Value{Value: defaultConcurrentRetries},
+			}},
 		}
 	}
 }

pkg/ciliumenvoyconfig/cell.go
@@ -41,6 +41,7 @@ var Cell = cell.Module(
 	"ciliumenvoyconfig",
 	"CiliumEnvoyConfig",
 
+	cell.Config(cecConfig{}),
 	cell.Invoke(registerCECK8sReconciler),
 	cell.ProvidePrivate(newCECManager),
 	cell.ProvidePrivate(newCECResourceParser),
@@ -54,19 +55,20 @@ var Cell = cell.Module(
 		},
 	),
 	cell.ProvidePrivate(newPortAllocator),
-	cell.Config(cecConfig{}),
 
 	experimentalCell,
 )
 
 type cecConfig struct {
-	EnvoyConfigRetryInterval time.Duration
-	EnvoyConfigTimeout       time.Duration
+	EnvoyConfigRetryInterval  time.Duration
+	EnvoyConfigTimeout        time.Duration
+	ProxyMaxConcurrentRetries uint32
 }
 
 func (r cecConfig) Flags(flags *pflag.FlagSet) {
 	flags.Duration("envoy-config-retry-interval", 15*time.Second, "Interval in which an attempt is made to reconcile failed EnvoyConfigs. If the duration is zero, the retry is deactivated.")
 	flags.Duration("envoy-config-timeout", 2*time.Minute, "Timeout that determines how long to wait for Envoy to N/ACK CiliumEnvoyConfig resources")
+	flags.Uint32("proxy-max-concurrent-retries", 128, "Maximum number of concurrent retries on Envoy clusters")
 }
 
 type reconcilerParams struct {
@@ -176,7 +178,7 @@ type managerParams struct {
 
 func newCECManager(params managerParams) ciliumEnvoyConfigManager {
 	return newCiliumEnvoyConfigManager(params.Logger, params.PolicyUpdater, params.ServiceManager, params.XdsServer,
-		params.BackendSyncer, params.ResourceParser, params.Config.EnvoyConfigTimeout, params.Services, params.Endpoints, params.MetricsManager)
+		params.BackendSyncer, params.ResourceParser, params.Config.EnvoyConfigTimeout, params.Config.ProxyMaxConcurrentRetries, params.Services, params.Endpoints, params.MetricsManager)
 }
 
 func newPortAllocator(proxy *proxy.Proxy) PortAllocator {

pkg/envoy/cell.go
@@ -60,6 +60,7 @@ type envoyProxyConfig struct {
 	ProxyMaxRequestsPerConnection     int
 	ProxyMaxConnectionDurationSeconds int
 	ProxyIdleTimeoutSeconds           int
+	ProxyMaxConcurrentRetries         uint32
 	HTTPNormalizePath                 bool
 	HTTPRequestTimeout                uint
 	HTTPIdleTimeout                   uint
@@ -85,6 +86,7 @@ func (r envoyProxyConfig) Flags(flags *pflag.FlagSet) {
 	flags.Int("proxy-max-requests-per-connection", 0, "Set Envoy HTTP option max_requests_per_connection. Default 0 (disable)")
 	flags.Int("proxy-max-connection-duration-seconds", 0, "Set Envoy HTTP option max_connection_duration seconds. Default 0 (disable)")
 	flags.Int("proxy-idle-timeout-seconds", 60, "Set Envoy upstream HTTP idle connection timeout seconds. Does not apply to connections with pending requests. Default 60s")
+	flags.Uint32("proxy-max-concurrent-retries", 128, "Maximum number of concurrent retries on Envoy clusters")
 	flags.Bool("http-normalize-path", true, "Use Envoy HTTP path normalization options, which currently includes RFC 3986 path normalization, Envoy merge slashes option, and unescaping and redirecting for paths that contain escaped slashes. These are necessary to keep path based access control functional, and should not interfere with normal operation. Set this to false only with caution.")
 	flags.Uint("http-request-timeout", 60*60, "Time after which a forwarded HTTP request is considered failed unless completed (in seconds); Use 0 for unlimited")
 	flags.Uint("http-idle-timeout", 0, "Time after which a non-gRPC HTTP stream is considered failed unless traffic in the stream has been processed (in seconds); defaults to 0 (unlimited)")
@@ -196,6 +198,7 @@ func newEnvoyXDSServer(params xdsServerParams) (XDSServer, error) {
 			maxRequestsPerConnection: uint32(params.EnvoyProxyConfig.ProxyMaxRequestsPerConnection),
 			maxConnectionDuration:    time.Duration(params.EnvoyProxyConfig.ProxyMaxConnectionDurationSeconds) * time.Second,
 			idleTimeout:              time.Duration(params.EnvoyProxyConfig.ProxyIdleTimeoutSeconds) * time.Second,
+			maxConcurrentRetries:     params.EnvoyProxyConfig.ProxyMaxConcurrentRetries,
 		}, nil
 	}
 

install/kubernetes/cilium/templates/cilium-configmap.yaml
@@ -1324,6 +1324,7 @@ data:
   proxy-max-requests-per-connection: {{ .Values.envoy.maxRequestsPerConnection | quote }}
   proxy-max-connection-duration-seconds: {{ .Values.envoy.maxConnectionDurationSeconds | quote }}
   proxy-idle-timeout-seconds: {{ .Values.envoy.idleTimeoutDurationSeconds | quote }}
+  proxy-max-concurrent-retries: {{ .Values.envoy.maxConcurrentRetries | quote }}
 
   external-envoy-proxy: {{ include "envoyDaemonSetEnabled" . | quote }}
   envoy-base-id: {{ .Values.envoy.baseID | quote }}

install/kubernetes/cilium/values.yaml
@@ -2312,6 +2312,8 @@ envoy:
   connectTimeoutSeconds: 2
   # -- Time in seconds after which the initial fetch on an xDS stream is considered timed out
   initialFetchTimeoutSeconds: 30
+  # -- Maximum number of concurrent retries on Envoy clusters
+  maxConcurrentRetries: 128
   # -- ProxyMaxRequestsPerConnection specifies the max_requests_per_connection setting for Envoy
   maxRequestsPerConnection: 0
   # -- Set Envoy HTTP option max_connection_duration seconds. Default 0 (disable)

install/kubernetes/cilium/values.yaml.tmpl
@@ -2327,6 +2327,8 @@ envoy:
   connectTimeoutSeconds: 2
   # -- Time in seconds after which the initial fetch on an xDS stream is considered timed out
   initialFetchTimeoutSeconds: 30
+  # -- Maximum number of concurrent retries on Envoy clusters
+  maxConcurrentRetries: 128
   # -- ProxyMaxRequestsPerConnection specifies the max_requests_per_connection setting for Envoy
   maxRequestsPerConnection: 0
   # -- Set Envoy HTTP option max_connection_duration seconds. Default 0 (disable)

pkg/ciliumenvoyconfig/cec_manager.go
@@ -42,7 +42,8 @@ type cecManager struct {
 	backendSyncer  *envoyServiceBackendSyncer
 	resourceParser *cecResourceParser
 
-	envoyConfigTimeout time.Duration
+	envoyConfigTimeout   time.Duration
+	maxConcurrentRetries uint32
 
 	services  resource.Resource[*slim_corev1.Service]
 	endpoints resource.Resource[*k8s.Endpoints]
@@ -57,21 +58,23 @@ func newCiliumEnvoyConfigManager(logger logrus.FieldLogger,
 	backendSyncer *envoyServiceBackendSyncer,
 	resourceParser *cecResourceParser,
 	envoyConfigTimeout time.Duration,
+	maxConcurrentRetries uint32,
 	services resource.Resource[*slim_corev1.Service],
 	endpoints resource.Resource[*k8s.Endpoints],
 	metricsManager CECMetrics,
 ) *cecManager {
 	return &cecManager{
-		logger:             logger,
-		policyUpdater:      policyUpdater,
-		serviceManager:     serviceManager,
-		xdsServer:          xdsServer,
-		backendSyncer:      backendSyncer,
-		resourceParser:     resourceParser,
-		envoyConfigTimeout: envoyConfigTimeout,
-		services:           services,
-		endpoints:          endpoints,
-		metricsManager:     metricsManager,
+		logger:               logger,
+		policyUpdater:        policyUpdater,
+		serviceManager:       serviceManager,
+		xdsServer:            xdsServer,
+		backendSyncer:        backendSyncer,
+		resourceParser:       resourceParser,
+		envoyConfigTimeout:   envoyConfigTimeout,
+		maxConcurrentRetries: maxConcurrentRetries,
+		services:             services,
+		endpoints:            endpoints,
+		metricsManager:       metricsManager,
 	}
 }
 

pkg/ciliumenvoyconfig/cec_resource_parser_test.go
@@ -604,8 +604,9 @@ func TestCiliumEnvoyConfigMulti(t *testing.T) {
 	logger := logrus.New()
 	logger.SetOutput(io.Discard)
 	parser := cecResourceParser{
-		logger:        logger,
-		portAllocator: NewMockPortAllocator(),
+		logger:                      logger,
+		portAllocator:               NewMockPortAllocator(),
+		defaultMaxConcurrentRetries: 128,
 	}
 
 	jsonBytes, err := yaml.YAMLToJSON([]byte(ciliumEnvoyConfigMulti))
@@ -672,6 +673,13 @@ func TestCiliumEnvoyConfigMulti(t *testing.T) {
 	assert.Equal(t, int32(250000000), resources.Clusters[0].ConnectTimeout.Nanos)
 	assert.Equal(t, envoy_config_cluster.Cluster_ROUND_ROBIN, resources.Clusters[0].LbPolicy)
 	assert.Equal(t, envoy_config_cluster.Cluster_EDS, resources.Clusters[0].GetType())
+	//
+	// Check that missing CircuitBreakers is automatically filled in
+	//
+	cb := resources.Clusters[0].CircuitBreakers
+	assert.NotNil(t, cb)
+	assert.Len(t, cb.Thresholds, 1)
+	assert.Equal(t, uint32(128), cb.Thresholds[0].MaxRetries.Value)
 	//
 	// Check that missing EDS config source is automatically filled in
 	//
@@ -1102,6 +1110,7 @@ func TestCiliumEnvoyConfigTCPProxyTermination(t *testing.T) {
 func checkCiliumXDS(t *testing.T, cs *envoy_config_core.ConfigSource) {
 	require.NotNil(t, cs)
 	assert.Equal(t, envoy_config_core.ApiVersion_V3, cs.ResourceApiVersion)
+	assert.Equal(t, int64(30), cs.InitialFetchTimeout.Seconds)
 	acs := cs.GetApiConfigSource()
 	assert.NotNil(t, acs)
 	assert.Equal(t, envoy_config_core.ApiConfigSource_GRPC, acs.ApiType)

commit b15c0b9378a8805816f38bc3cbfcaca2e9c534f4

cec: Re-use flag for proxy-max-concurrent-retries
Hive/Cell does not have defined behavior when the same flag is
registered twice. Avoid registering the flag twice by depending on the
envoy package configuration that has the flag, and remove the second
declaration.

Fixes: 70a4979ebb59 ("envoy: Increase per-cluster concurrent retry limit")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/ciliumenvoyconfig/cec_resource_parser.go
@@ -69,14 +69,15 @@ type parserParams struct {
 	PortAllocator  PortAllocator
 	LocalNodeStore *node.LocalNodeStore
 
-	CecConfig cecConfig
+	CecConfig   cecConfig
+	EnvoyConfig envoy.ProxyConfig
 }
 
 func newCECResourceParser(params parserParams) *cecResourceParser {
 	parser := &cecResourceParser{
 		logger:                      params.Logger,
 		portAllocator:               params.PortAllocator,
-		defaultMaxConcurrentRetries: params.CecConfig.ProxyMaxConcurrentRetries,
+		defaultMaxConcurrentRetries: params.EnvoyConfig.ProxyMaxConcurrentRetries,
 	}
 
 	// Retrieve Ingress IPs from local Node.

pkg/ciliumenvoyconfig/cell.go
@@ -60,15 +60,13 @@ var Cell = cell.Module(
 )
 
 type cecConfig struct {
-	EnvoyConfigRetryInterval  time.Duration
-	EnvoyConfigTimeout        time.Duration
-	ProxyMaxConcurrentRetries uint32
+	EnvoyConfigRetryInterval time.Duration
+	EnvoyConfigTimeout       time.Duration
 }
 
 func (r cecConfig) Flags(flags *pflag.FlagSet) {
 	flags.Duration("envoy-config-retry-interval", 15*time.Second, "Interval in which an attempt is made to reconcile failed EnvoyConfigs. If the duration is zero, the retry is deactivated.")
 	flags.Duration("envoy-config-timeout", 2*time.Minute, "Timeout that determines how long to wait for Envoy to N/ACK CiliumEnvoyConfig resources")
-	flags.Uint32("proxy-max-concurrent-retries", 128, "Maximum number of concurrent retries on Envoy clusters")
 }
 
 type reconcilerParams struct {
@@ -161,7 +159,8 @@ type managerParams struct {
 
 	Logger logrus.FieldLogger
 
-	Config cecConfig
+	Config      cecConfig
+	EnvoyConfig envoy.ProxyConfig
 
 	PolicyUpdater  *policy.Updater
 	ServiceManager service.ServiceManager
@@ -178,7 +177,7 @@ type managerParams struct {
 
 func newCECManager(params managerParams) ciliumEnvoyConfigManager {
 	return newCiliumEnvoyConfigManager(params.Logger, params.PolicyUpdater, params.ServiceManager, params.XdsServer,
-		params.BackendSyncer, params.ResourceParser, params.Config.EnvoyConfigTimeout, params.Config.ProxyMaxConcurrentRetries, params.Services, params.Endpoints, params.MetricsManager)
+		params.BackendSyncer, params.ResourceParser, params.Config.EnvoyConfigTimeout, params.EnvoyConfig.ProxyMaxConcurrentRetries, params.Services, params.Endpoints, params.MetricsManager)
 }
 
 func newPortAllocator(proxy *proxy.Proxy) PortAllocator {