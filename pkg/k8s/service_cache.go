eb156004824db78ddd5c9833bc4ccde50fecaf51
Tim Horner <timothy.horner@isovalent.com>
2025-01-21 09:47:12 -0500

    Revert "k8s: Use Old{Service,Endpoints} in delete events"
    This reverts commit 10d3eb523c1a8181ce3a9079539ae4446d9b9681.
    
    Signed-off-by: Tim Horner <timothy.horner@isovalent.com>

--- pkg/k8s/service_cache.go
+++ pkg/k8s/service_cache.go
@@ -415,11 +415,11 @@ func (s *ServiceCache) DeleteService(k8sSvc *slim_corev1.Service, swg *lock.Stop
 		s.metrics.DelService(oldService)
 		swg.Add()
 		s.emitEvent(ServiceEvent{
-			Action:       DeleteService,
-			ID:           svcID,
-			OldService:   oldService,
-			OldEndpoints: endpoints,
-			SWG:          swg,
+			Action:    DeleteService,
+			ID:        svcID,
+			Service:   oldService,
+			Endpoints: endpoints,
+			SWG:       swg,
 		})
 	}
 }
@@ -813,7 +813,6 @@ func (s *ServiceCache) mergeExternalServiceDeleteLocked(service *serviceStore.Cl
 				Action:       UpdateService,
 				ID:           id,
 				Service:      svc,
-				OldService:   svc,
 				Endpoints:    endpoints,
 				OldEndpoints: oldEPs,
 				SWG:          swg,
@@ -822,8 +821,6 @@ func (s *ServiceCache) mergeExternalServiceDeleteLocked(service *serviceStore.Cl
 			if !serviceReady {
 				delete(s.services, id)
 				event.Action = DeleteService
-				event.Service = nil
-				event.Endpoints = nil
 			}
 
 			s.emitEvent(event)
@@ -880,11 +877,11 @@ func (s *ServiceCache) MergeClusterServiceDelete(service *serviceStore.ClusterSe
 	if ok {
 		swg.Add()
 		s.emitEvent(ServiceEvent{
-			Action:       DeleteService,
-			ID:           id,
-			OldService:   svc,
-			OldEndpoints: endpoints,
-			SWG:          swg,
+			Action:    DeleteService,
+			ID:        id,
+			Service:   svc,
+			Endpoints: endpoints,
+			SWG:       swg,
 		})
 	}
 }
