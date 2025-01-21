e9feecb4e64fdfd3ed9efe2e09d4a3c7f1909900
Damian Sawicki <dsawicki@google.com>
2025-01-21 13:53:31 +0000

    lbmap: Add GetL7LBProxyPort to ServiceValue
    This is not to avoid using GetBackendID for L7 Proxy in order to
    handle endianness in the same way as in the setter.
    
    Signed-off-by: Damian Sawicki <dsawicki@google.com>

--- pkg/maps/lbmap/ipv4.go
+++ pkg/maps/lbmap/ipv4.go
@@ -352,6 +352,10 @@ func (s *Service4Value) SetL7LBProxyPort(port uint16) {
 	s.BackendID = uint32(byteorder.HostToNetwork16(port))
 }
 
+func (s *Service4Value) GetL7LBProxyPort() uint16 {
+	return byteorder.HostToNetwork16(uint16(s.BackendID))
+}
+
 func (s *Service4Value) SetBackendID(id loadbalancer.BackendID) {
 	s.BackendID = uint32(id)
 }
