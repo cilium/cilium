770ffb99543b7e19745c6e32e0880a9f5feee241
Julian Wiedmann <jwi@isovalent.com>
2025-01-21 12:07:12 +0000

    devices controller: spell out list of required netlink families
    [ upstream commit 4e50ea8d683593db19d4d23d9d272472e2b0999b ]
    
    Spell out the list of netlink families that we use. Otherwise the netlink
    library will pull in a default list (`SupportedNlFamilies`) which
    includes NETLINK_XFRM. This might not be available on the host kernel.
    
    See https://github.com/cilium/cilium/issues/36600#issuecomment-2546387246.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Rastislav Szabo <rastislav.szabo@isovalent.com>

--- pkg/datapath/linux/devices_controller.go
+++ pkg/datapath/linux/devices_controller.go
@@ -714,7 +714,7 @@ type netlinkFuncs struct {
 // makeNetlinkFuncs returns a *netlinkFuncs containing netlink accessors to the
 // network namespace of the calling goroutine's OS thread.
 func makeNetlinkFuncs() (*netlinkFuncs, error) {
-	netlinkHandle, err := netlink.NewHandle()
+	netlinkHandle, err := netlink.NewHandle(unix.NETLINK_ROUTE, unix.NETLINK_NETFILTER)
 	if err != nil {
 		return nil, fmt.Errorf("creating netlink handle: %w", err)
 	}
