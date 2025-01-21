e4e5f91397c02f741f39264e0e029c9b39dd7b9b
Tam Mach <tam.mach@cilium.io>
2025-01-21 22:51:03 +1100

    test: Add negative test case for TLS SNI
    This commit is to add the negative test case for TLS SNI feature with
    another external target (e.g. cilium.io). There should be no policy
    dropped but SSL error from curl command.
    
    ```
    $ kex -n cilium-test-1 client-645b68dcf7-n4xx7 -- curl -v https://cilium.io
    * Host cilium.io:443 was resolved.
    * IPv6: (none)
    * IPv4: 104.198.14.52
    *   Trying 104.198.14.52:443...
    * Connected to cilium.io (104.198.14.52) port 443
    * ALPN: curl offers h2,http/1.1
    * TLSv1.3 (OUT), TLS handshake, Client hello (1):
    *  CAfile: /etc/ssl/certs/ca-certificates.crt
    *  CApath: /etc/ssl/certs
    * Recv failure: Connection reset by peer
    * OpenSSL SSL_connect: Connection reset by peer in connection to cilium.io:443
    * Closing connection
    curl: (35) Recv failure: Connection reset by peer
    command terminated with exit code 35
    ```
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- cilium-cli/connectivity/builder/client_egress_tls_sni.go
+++ cilium-cli/connectivity/builder/client_egress_tls_sni.go
@@ -4,6 +4,8 @@
 package builder
 
 import (
+	"fmt"
+
 	"github.com/cilium/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium/cilium-cli/connectivity/tests"
 	"github.com/cilium/cilium/cilium-cli/utils/features"
@@ -23,12 +25,26 @@ func clientEgressTlsSniTest(ct *check.ConnectivityTest, templates map[string]str
 	newTest(testName, ct).
 		WithCiliumVersion("!1.14.15 !1.14.16 !1.15.9 !1.15.10 !1.16.2 !1.16.3").
 		WithFeatureRequirements(features.RequireEnabled(features.L7Proxy)).
-		WithCiliumPolicy(yamlFile).                                   // L7 allow policy TLS SNI enforcement
+		WithCiliumPolicy(yamlFile).                                   // L7 allow policy TLS SNI enforcement for external target
 		WithCiliumPolicy(templates["clientEgressOnlyDNSPolicyYAML"]). // DNS resolution only
 		WithScenarios(tests.PodToWorld()).
 		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
 			if a.Destination().Port() == 443 {
 				return check.ResultOK, check.ResultNone
+			}
+			return check.ResultDefaultDenyEgressDrop, check.ResultNone
+		})
+
+	newTest(fmt.Sprintf("%s-denied", testName), ct).
+		WithCiliumVersion("!1.14.15 !1.14.16 !1.15.9 !1.15.10 !1.16.2 !1.16.3").
+		WithFeatureRequirements(features.RequireEnabled(features.L7Proxy)).
+		WithCiliumPolicy(yamlFile).                                   // L7 allow policy TLS SNI enforcement for external target
+		WithCiliumPolicy(templates["clientEgressOnlyDNSPolicyYAML"]). // DNS resolution only
+		WithScenarios(tests.PodToWorld2()).                           // Another External Target is not allowed
+		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
+			if a.Destination().Port() == 443 {
+				// SSL error as another external target (e.g. cilium.io) SNI is not allowed
+				return check.ResultCurlSSLError, check.ResultNone
 			}
 			return check.ResultDefaultDenyEgressDrop, check.ResultNone
 		})
