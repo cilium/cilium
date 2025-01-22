abc4748f19bf72aa3610b05ef47c9a3a8e65468e
Chris Tarazi <chris@isovalent.com>
2025-01-21 16:57:12 -0800

    test/fuzzing: Fix fuzzing test failure
    ```
    pkg/policy/rule_test_fuzz.go:57:10: td.repo.mustAdd undefined (type *Repository has no field or method mustAdd)
    ...
    pkg/policy/repository_test_fuzz.go:59:84: undefined: lbls
    pkg/policy/repository_test_fuzz.go:2374:18: undefined: fooIdentity
    pkg/policy/repository_test_fuzz.go:2382:68: undefined: fooIdentity
    pkg/policy/repository_test_fuzz.go:2390:18: undefined: fooIdentity
    pkg/policy/repository_test_fuzz.go:2398:68: undefined: fooIdentity
    pkg/policy/repository_test_fuzz.go:2408:19: undefined: fooIdentity
    pkg/policy/repository_test_fuzz.go:2421:69: undefined: fooIdentity
    ...
    pkg/policy/resolve_test_fuzz.go:130:39: undefined: generateCIDREgressRule
    pkg/policy/resolve_test_fuzz.go:136:16: undefined: generateCIDRIdentities
    pkg/policy/resolve_test_fuzz.go:521:35: emptyMapState().withState undefined (type mapState has no field or method withState)
    pkg/policy/resolve_test_fuzz.go:694:35: emptyMapState().withState undefined (type mapState has no field or method withState)
    pkg/policy/resolve_test_fuzz.go:699:102: rule1MapStateEntry.withExplicitAuth undefined (type mapStateEntry has no field or method withExplicitAuth)
    pkg/policy/resolve_test_fuzz.go:699:119: undefined: AuthTypeDisabled
    pkg/policy/resolve_test_fuzz.go:700:102: rule1MapStateEntry.withExplicitAuth undefined (type mapStateEntry has no field or method withExplicitAuth)
    pkg/policy/resolve_test_fuzz.go:700:119: undefined: AuthTypeDisabled
    pkg/policy/resolve_test_fuzz.go:718:3: undefined: ingressKey
    pkg/policy/resolve_test_fuzz.go:719:3: undefined: ingressKey
    ```
    
    Fix by including the path with the policy repository `mustAdd()`
    function in the fuzzing script and where the above variables are
    defined, so that it's in scope when the fuzzer is run.
    
    Ref: https://github.com/cilium/cilium/actions/runs/12862705459/workflow
    Fixes: d036ef2021 ("pkg/policy: convert rule_test.go to use Lookup")
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- test/fuzzing/oss-fuzz-build.sh
+++ test/fuzzing/oss-fuzz-build.sh
@@ -13,8 +13,13 @@ set -eu
 printf "package policy\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/cilium/pkg/policy/registerfuzzdep.go
 
 go mod tidy && go mod vendor
+mv $SRC/cilium/pkg/policy/distillery_test.go $SRC/cilium/pkg/policy/distillery_test_fuzz.go
 mv $SRC/cilium/pkg/policy/l4_filter_test.go $SRC/cilium/pkg/policy/l4_filter_test_fuzz.go
 mv $SRC/cilium/pkg/policy/policy_test.go $SRC/cilium/pkg/policy/policy_test_fuzz.go
+mv $SRC/cilium/pkg/policy/mapstate_test.go $SRC/cilium/pkg/policy/mapstate_test_fuzz.go
+mv $SRC/cilium/pkg/policy/repository_test.go $SRC/cilium/pkg/policy/repository_test_fuzz.go
+mv $SRC/cilium/pkg/policy/resolve_test.go $SRC/cilium/pkg/policy/resolve_test_fuzz.go
+mv $SRC/cilium/pkg/policy/resolve_deny_test.go $SRC/cilium/pkg/policy/resolve_deny_test_fuzz.go
 mv $SRC/cilium/pkg/policy/rule_test.go $SRC/cilium/pkg/policy/rule_test_fuzz.go
 mv $SRC/cilium/pkg/policy/selectorcache_test.go $SRC/cilium/pkg/policy/selectorcache_test_fuzz.go
 
