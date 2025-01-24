cilium/cilium

commit ef23827692fd9dbe142f36a2f6f00fdcba272416

hubble: provide exporters using the FlowLogExporter interface
Signed-off-by: Alexandre Barone <abalexandrebarone@gmail.com>

pkg/hubble/exporter/cell/cell.go
@@ -15,7 +15,6 @@ import (
 
 	flowpb "github.com/cilium/cilium/api/v1/flow"
 	"github.com/cilium/cilium/pkg/hubble/exporter"
-	"github.com/cilium/cilium/pkg/hubble/observer/observeroption"
 )
 
 type config struct {
@@ -72,12 +71,6 @@ func (cfg config) validate() error {
 	return nil
 }
 
-type hubbleExportersOut struct {
-	cell.Out
-
-	ObserverOptions []observeroption.Option `group:"hubble-observer-options,flatten"`
-}
-
 type hubbleExportersParams struct {
 	cell.In
 
@@ -87,6 +80,12 @@ type hubbleExportersParams struct {
 
 	// TODO: replace by slog
 	Logger logrus.FieldLogger
+}
+
+type hubbleExportersOut struct {
+	cell.Out
+
+	Exporters []exporter.FlowLogExporter `group:"hubble-flow-log-exporters,flatten"`
 }
 
 // validatedConfig is a config that is known to be valid.
@@ -118,7 +117,7 @@ func NewHubbleStaticExporter(params hubbleExportersParams) (hubbleExportersOut, 
 			Compress:   params.Config.ExportFileCompress,
 		})))
 	}
-	exporter, err := exporter.NewExporter(params.Logger, exporterOpts...)
+	staticExporter, err := exporter.NewExporter(params.Logger, exporterOpts...)
 	if err != nil {
 		// non-fatal failure, log and continue
 		params.Logger.WithError(err).Error("Failed to configure Hubble static exporter")
@@ -127,12 +126,10 @@ func NewHubbleStaticExporter(params hubbleExportersParams) (hubbleExportersOut, 
 
 	params.Lifecycle.Append(cell.Hook{
 		OnStop: func(hc cell.HookContext) error {
-			return exporter.Stop()
+			return staticExporter.Stop()
 		},
 	})
-	return hubbleExportersOut{
-		ObserverOptions: []observeroption.Option{observeroption.WithOnDecodedEvent(exporter)},
-	}, nil
+	return hubbleExportersOut{Exporters: []exporter.FlowLogExporter{staticExporter}}, nil
 }
 
 func NewHubbleDynamicExporter(params hubbleExportersParams) (hubbleExportersOut, error) {
@@ -141,16 +138,14 @@ func NewHubbleDynamicExporter(params hubbleExportersParams) (hubbleExportersOut,
 		return hubbleExportersOut{}, nil
 	}
 
-	exporter := exporter.NewDynamicExporter(params.Logger, params.Config.FlowlogsConfigFilePath, params.Config.ExportFileMaxSizeMB, params.Config.ExportFileMaxBackups)
+	dynamicExporter := exporter.NewDynamicExporter(params.Logger, params.Config.FlowlogsConfigFilePath, params.Config.ExportFileMaxSizeMB, params.Config.ExportFileMaxBackups)
 	params.JobGroup.Add(job.OneShot("hubble-dynamic-exporter", func(ctx context.Context, health cell.Health) error {
-		return exporter.Watch(ctx)
+		return dynamicExporter.Watch(ctx)
 	}))
 	params.Lifecycle.Append(cell.Hook{
 		OnStop: func(hc cell.HookContext) error {
-			return exporter.Stop()
+			return dynamicExporter.Stop()
 		},
 	})
-	return hubbleExportersOut{
-		ObserverOptions: []observeroption.Option{observeroption.WithOnDecodedEvent(exporter)},
-	}, nil
+	return hubbleExportersOut{Exporters: []exporter.FlowLogExporter{dynamicExporter}}, nil
 }

pkg/hubble/cell/cell.go
@@ -15,6 +15,7 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/cgroups/manager"
 	"github.com/cilium/cilium/pkg/endpointmanager"
+	"github.com/cilium/cilium/pkg/hubble/exporter"
 	exportercell "github.com/cilium/cilium/pkg/hubble/exporter/cell"
 	"github.com/cilium/cilium/pkg/hubble/observer/observeroption"
 	identitycell "github.com/cilium/cilium/pkg/identity/cache/cell"
@@ -39,7 +40,7 @@ var Cell = cell.Module(
 	cell.Provide(newHubbleIntegration),
 	cell.Config(defaultConfig),
 
-	// Provide Hubble flow log exporters as observer options
+	// Provide Hubble flow log exporters
 	cell.ProvidePrivate(exportercell.NewValidatedConfig),
 	cell.ProvidePrivate(exportercell.NewHubbleStaticExporter),
 	cell.ProvidePrivate(exportercell.NewHubbleDynamicExporter),
@@ -63,9 +64,9 @@ type hubbleParams struct {
 	MonitorAgent      monitorAgent.Agent
 	Recorder          *recorder.Recorder
 
-	// Observer Server options
 	// NOTE: ordering is not guaranteed, do not rely on it.
-	ObserverOptions []observeroption.Option `group:"hubble-observer-options"`
+	ObserverOptions []observeroption.Option    `group:"hubble-observer-options"`
+	Exporters       []exporter.FlowLogExporter `group:"hubble-flow-log-exporters"`
 
 	// NOTE: we still need DaemonConfig for the shared EnableRecorder flag.
 	AgentConfig *option.DaemonConfig
@@ -93,6 +94,7 @@ func newHubbleIntegration(params hubbleParams) (HubbleIntegration, error) {
 		params.MonitorAgent,
 		params.Recorder,
 		params.ObserverOptions,
+		params.Exporters,
 		params.AgentConfig,
 		params.Config,
 		params.Logger,

pkg/hubble/cell/hubbleintegration.go
@@ -26,8 +26,10 @@ import (
 	"github.com/cilium/cilium/pkg/crypto/certloader"
 	"github.com/cilium/cilium/pkg/datapath/link"
 	"github.com/cilium/cilium/pkg/endpointmanager"
+	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
 	"github.com/cilium/cilium/pkg/hubble/container"
 	"github.com/cilium/cilium/pkg/hubble/dropeventemitter"
+	"github.com/cilium/cilium/pkg/hubble/exporter"
 	"github.com/cilium/cilium/pkg/hubble/metrics"
 	"github.com/cilium/cilium/pkg/hubble/metrics/api"
 	"github.com/cilium/cilium/pkg/hubble/monitor"
@@ -77,6 +79,7 @@ type hubbleIntegration struct {
 	nodeLocalStore    *node.LocalNodeStore
 	monitorAgent      monitorAgent.Agent
 	recorder          *recorder.Recorder
+	exporters         []exporter.FlowLogExporter
 
 	// NOTE: we still need DaemonConfig for the shared EnableRecorder flag.
 	agentConfig *option.DaemonConfig
@@ -100,6 +103,7 @@ func new(
 	monitorAgent monitorAgent.Agent,
 	recorder *recorder.Recorder,
 	observerOptions []observeroption.Option,
+	exporters []exporter.FlowLogExporter,
 	agentConfig *option.DaemonConfig,
 	config config,
 	log logrus.FieldLogger,
@@ -123,6 +127,7 @@ func new(
 		monitorAgent:      monitorAgent,
 		recorder:          recorder,
 		observerOptions:   observerOptions,
+		exporters:         exporters,
 		agentConfig:       agentConfig,
 		config:            config,
 		log:               log,
@@ -407,6 +412,13 @@ func (h *hubbleIntegration) launch(ctx context.Context) {
 		observeroption.WithMaxFlows(maxFlows),
 		observeroption.WithMonitorBuffer(h.config.EventQueueSize),
 	)
+
+	// register exporters
+	for _, exporter := range h.exporters {
+		observerOpts = append(observerOpts, observeroption.WithOnDecodedEventFunc(func(ctx context.Context, e *v1.Event) (bool, error) {
+			return false, exporter.Export(ctx, e)
+		}))
+	}
 
 	// register injected observer options last to allow
 	// for explicit ordering of known dependencies

pkg/hubble/exporter/api.go
@@ -4,17 +4,20 @@
 package exporter
 
 import (
+	"context"
 	"reflect"
 	"slices"
 
 	flowpb "github.com/cilium/cilium/api/v1/flow"
-	"github.com/cilium/cilium/pkg/hubble/observer/observeroption"
+	v1 "github.com/cilium/cilium/pkg/hubble/api/v1"
 	"github.com/cilium/cilium/pkg/time"
 )
 
-// FlowLogExporter exports hubble events to configured target file.
+// FlowLogExporter is an interface that defines exporters for hubble events.
 type FlowLogExporter interface {
-	observeroption.OnDecodedEvent
+	// Export exports the received event.
+	Export(ctx context.Context, ev *v1.Event) error
+
 	// Stop stops this exporter instance from further events processing.
 	Stop() error
 }

pkg/hubble/exporter/dynamic_exporter.go
@@ -14,13 +14,13 @@ import (
 	"github.com/cilium/cilium/pkg/time"
 )
 
+var _ FlowLogExporter = (*DynamicExporter)(nil)
+
 var reloadInterval = 5 * time.Second
 
 // DynamicExporter is a wrapper of the hubble exporter that supports dynamic configuration reload
 // for a set of exporters.
 type DynamicExporter struct {
-	FlowLogExporter
-
 	logger        logrus.FieldLogger
 	watcher       *configWatcher
 	maxFileSizeMB int
@@ -58,12 +58,13 @@ func (d *DynamicExporter) Watch(ctx context.Context) error {
 	return d.watcher.watch(ctx, reloadInterval)
 }
 
-// OnDecodedEvent implemebnts FlowLogExporter.
+// Export implements the FlowLogExporter interface.
+//
 // It distributes events across all managed exporters.
-func (d *DynamicExporter) OnDecodedEvent(ctx context.Context, event *v1.Event) (bool, error) {
+func (d *DynamicExporter) Export(ctx context.Context, event *v1.Event) error {
 	select {
 	case <-ctx.Done():
-		return false, nil
+		return ctx.Err()
 	default:
 	}
 
@@ -73,14 +74,15 @@ func (d *DynamicExporter) OnDecodedEvent(ctx context.Context, event *v1.Event) (
 	var errs error
 	for _, me := range d.managedExporters {
 		if me.config.End == nil || me.config.End.After(time.Now()) {
-			_, err := me.exporter.OnDecodedEvent(ctx, event)
+			err := me.exporter.Export(ctx, event)
 			errs = errors.Join(errs, err)
 		}
 	}
-	return false, errs
+	return errs
 }
 
-// Stop implements FlowLogExporter.
+// Stop implements the FlowLogExporter interface.
+//
 // It stops all managed flow log exporters.
 func (d *DynamicExporter) Stop() error {
 	d.mutex.Lock()

pkg/hubble/exporter/dynamic_exporter_test.go
@@ -207,7 +207,7 @@ func TestEventPropagation(t *testing.T) {
 	}
 
 	// when
-	sut.OnDecodedEvent(context.TODO(), &v1.Event{})
+	sut.Export(context.TODO(), &v1.Event{})
 
 	// then
 	assert.Equal(t, 1, mockExporter0.events)
@@ -509,17 +509,19 @@ func createEmptyLogFile(t *testing.T) *os.File {
 	return file
 }
 
+var _ FlowLogExporter = (*mockExporter)(nil)
+
 type mockExporter struct {
 	events  int
 	stopped bool
 }
 
-func (m *mockExporter) Stop() error {
-	m.stopped = true
+func (m *mockExporter) Export(_ context.Context, _ *v1.Event) error {
+	m.events++
 	return nil
 }
 
-func (m *mockExporter) OnDecodedEvent(_ context.Context, _ *v1.Event) (bool, error) {
-	m.events++
-	return false, nil
+func (m *mockExporter) Stop() error {
+	m.stopped = true
+	return nil
 }

pkg/hubble/exporter/exporter.go
@@ -17,6 +17,8 @@ import (
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 )
 
+var _ FlowLogExporter = (*exporter)(nil)
+
 // OnExportEvent is a hook that can be registered on an exporter and is invoked for each event.
 //
 // Returning false will stop the export pipeline for the current event, meaning the default export
@@ -35,8 +37,6 @@ func (f OnExportEventFunc) OnExportEvent(ctx context.Context, ev *v1.Event, enco
 
 // exporter is an implementation of OnDecodedEvent interface that writes Hubble events to a file.
 type exporter struct {
-	FlowLogExporter
-
 	logger  logrus.FieldLogger
 	encoder Encoder
 	writer  io.WriteCloser
@@ -82,6 +82,53 @@ func newExporter(logger logrus.FieldLogger, opts Options) (*exporter, error) {
 	}, nil
 }
 
+// Export implements the FlowLogExporter interface.
+//
+// It takes care of applying filters on the received event, and if allowed, proceeds to invoke the
+// registered OnExportEvent hooks. If none of the hooks return true (abort signal) the event is then
+// wrapped in observerpb.ExportEvent before being encoded and written to its underlying writer.
+func (e *exporter) Export(ctx context.Context, ev *v1.Event) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+	if !filters.Apply(e.opts.AllowFilters(), e.opts.DenyFilters(), ev) {
+		return nil
+	}
+
+	// Process OnExportEvent hooks
+	for _, f := range e.opts.OnExportEvent {
+		stop, err := f.OnExportEvent(ctx, ev, e.encoder)
+		if err != nil {
+			e.logger.WithError(err).Warn("OnExportEvent failed")
+		}
+		if stop {
+			// abort exporter pipeline by returning early but do not prevent
+			// other OnDecodedEvent hooks from firing
+			return nil
+		}
+	}
+
+	res := e.eventToExportEvent(ev)
+	if res == nil {
+		return nil
+	}
+	return e.encoder.Encode(res)
+}
+
+// Stop implements the FlowLogExporter interface.
+func (e *exporter) Stop() error {
+	e.logger.Debug("hubble flow exporter stopping")
+	if e.writer == nil {
+		// Already stoppped
+		return nil
+	}
+	err := e.writer.Close()
+	e.writer = nil
+	return err
+}
+
 // eventToExportEvent converts Event to ExportEvent.
 func (e *exporter) eventToExportEvent(event *v1.Event) *observerpb.ExportEvent {
 	switch ev := event.Event.(type) {
@@ -124,50 +171,4 @@ func (e *exporter) eventToExportEvent(event *v1.Event) *observerpb.ExportEvent {
 	default:
 		return nil
 	}
-}
-
-func (e *exporter) Stop() error {
-	e.logger.Debug("hubble flow exporter stopping")
-	if e.writer == nil {
-		// Already stoppped
-		return nil
-	}
-	err := e.writer.Close()
-	e.writer = nil
-	return err
-}
-
-// OnDecodedEvent implements the observeroption.OnDecodedEvent interface.
-//
-// It takes care of applying filters on the received event, and if allowed, proceeds to invoke the
-// registered OnExportEvent hooks. If none of the hooks return true (abort signal) the event is then
-// wrapped in observerpb.ExportEvent before being json-encoded and written to its underlying writer.
-func (e *exporter) OnDecodedEvent(ctx context.Context, ev *v1.Event) (bool, error) {
-	select {
-	case <-ctx.Done():
-		return false, nil
-	default:
-	}
-	if !filters.Apply(e.opts.AllowFilters(), e.opts.DenyFilters(), ev) {
-		return false, nil
-	}
-
-	// Process OnExportEvent hooks
-	for _, f := range e.opts.OnExportEvent {
-		stop, err := f.OnExportEvent(ctx, ev, e.encoder)
-		if err != nil {
-			e.logger.WithError(err).Warn("OnExportEvent failed")
-		}
-		if stop {
-			// abort exporter pipeline by returning early but do not prevent
-			// other OnDecodedEvent hooks from firing
-			return false, nil
-		}
-	}
-
-	res := e.eventToExportEvent(ev)
-	if res == nil {
-		return false, nil
-	}
-	return false, e.encoder.Encode(res)
 }

pkg/hubble/exporter/exporter_test.go
@@ -61,8 +61,7 @@ func TestExporter(t *testing.T) {
 
 	ctx := context.Background()
 	for _, ev := range events {
-		stop, err := exporter.OnDecodedEvent(ctx, ev)
-		assert.False(t, stop)
+		err := exporter.Export(ctx, ev)
 		assert.NoError(t, err)
 
 	}
@@ -142,15 +141,19 @@ func TestExporterWithFilters(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	cancelled := false
 	for i, ev := range events {
 		// Check if processing stops (shouldn't write the last event)
 		if i == len(events)-1 {
 			cancel()
+			cancelled = true
 		}
-		stop, err := exporter.OnDecodedEvent(ctx, ev)
-		assert.False(t, stop)
-		assert.NoError(t, err)
-
+		err := exporter.Export(ctx, ev)
+		if cancelled {
+			assert.ErrorIs(t, err, context.Canceled)
+		} else {
+			assert.NoError(t, err)
+		}
 	}
 	assert.Equal(t,
 		`{"flow":{"time":"1970-01-01T00:00:13Z","source":{"namespace":"namespace-a","pod_name":"x"}},"time":"1970-01-01T00:00:13Z"}
@@ -271,8 +274,7 @@ func TestExporterWithFieldMask(t *testing.T) {
 	defer cancel()
 
 	for _, ev := range events {
-		stop, err := exporter.OnDecodedEvent(ctx, ev)
-		assert.False(t, stop)
+		err := exporter.Export(ctx, ev)
 		assert.NoError(t, err)
 	}
 
@@ -354,8 +356,7 @@ func TestExporterOnExportEvent(t *testing.T) {
 
 	ctx := context.Background()
 	for _, ev := range events {
-		stop, err := exporter.OnDecodedEvent(ctx, ev)
-		assert.False(t, stop)
+		err := exporter.Export(ctx, ev)
 		assert.NoError(t, err)
 	}
 
@@ -473,8 +474,7 @@ func BenchmarkExporter(b *testing.B) {
 		if i%10 == 1 { // 10% matches deny filter
 			event = &denyEvent
 		}
-		stop, err := exporter.OnDecodedEvent(ctx, event)
-		assert.False(b, stop)
+		err := exporter.Export(ctx, event)
 		assert.NoError(b, err)
 	}
 }

commit e5322db2906a320fe4844c0a52c01a9476d9128e

hubble-export: fix allowlist and denylist flow-filters parsing
Hubble export allow/deny list parsing regressed in https://github.com/cilium/cilium/pull/35596
when moving export to a hive cell due to how hive cells handle flags.StringSlices.
Similar to https://github.com/cilium/cilium/issues/35619.

Redefine the flag as a string to avoid going through the implicit hive decoder hooks
that convert strings to slices by splitting on comma. Update hubble.ParseFlowFilters
to take in a string and decode all filters until EOF.

Also added tests for the hubble.ParseFlowFilters helper to make it clear which
formats we support and which we don't.

Signed-off-by: Alexandre Barone <abalexandrebarone@gmail.com>

pkg/hive/hive.go
@@ -16,11 +16,9 @@ import (
 	"github.com/cilium/statedb"
 	"github.com/sirupsen/logrus"
 
-	flowpb "github.com/cilium/cilium/api/v1/flow"
 	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/hive/health"
 	"github.com/cilium/cilium/pkg/hive/health/types"
-	"github.com/cilium/cilium/pkg/hubble"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
@@ -113,20 +111,6 @@ var decodeHooks = cell.DecodeHooks{
 			return data, nil
 		}
 		return cidr.ParseCIDR(s)
-	},
-	// Decode JSON encoded *flowpb.FlowFilter fields
-	func(from reflect.Type, to reflect.Type, data interface{}) (interface{}, error) {
-		if from.Kind() != reflect.Slice {
-			return data, nil
-		}
-		xs, ok := data.([]string)
-		if !ok {
-			return data, nil
-		}
-		if to != reflect.TypeOf(([]*flowpb.FlowFilter)(nil)) {
-			return data, nil
-		}
-		return hubble.ParseFlowFilters(xs...)
 	},
 }
 

pkg/hubble/exporter/cell/cell.go
@@ -14,6 +14,7 @@ import (
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 
 	flowpb "github.com/cilium/cilium/api/v1/flow"
+	"github.com/cilium/cilium/pkg/hubble"
 	"github.com/cilium/cilium/pkg/hubble/exporter"
 )
 
@@ -32,9 +33,9 @@ type config struct {
 	// ExportFileCompress specifies whether rotated files are compressed.
 	ExportFileCompress bool `mapstructure:"hubble-export-file-compress"`
 	// ExportAllowlist specifies allow list filter use by exporter.
-	ExportAllowlist []*flowpb.FlowFilter `mapstructure:"hubble-export-allowlist"`
+	ExportAllowlist string `mapstructure:"hubble-export-allowlist"`
 	// ExportDenylist specifies deny list filter use by exporter.
-	ExportDenylist []*flowpb.FlowFilter `mapstructure:"hubble-export-denylist"`
+	ExportDenylist string `mapstructure:"hubble-export-denylist"`
 	// ExportFieldmask specifies list of fields to log in exporter.
 	ExportFieldmask []string `mapstructure:"hubble-export-fieldmask"`
 }
@@ -45,8 +46,8 @@ var DefaultConfig = config{
 	ExportFileMaxSizeMB:    exporter.DefaultFileMaxSizeMB,
 	ExportFileMaxBackups:   exporter.DefaultFileMaxBackups,
 	ExportFileCompress:     false,
-	ExportAllowlist:        []*flowpb.FlowFilter{},
-	ExportDenylist:         []*flowpb.FlowFilter{},
+	ExportAllowlist:        "",
+	ExportDenylist:         "",
 	ExportFieldmask:        []string{},
 }
 
@@ -56,8 +57,8 @@ func (def config) Flags(flags *pflag.FlagSet) {
 	flags.Int("hubble-export-file-max-size-mb", def.ExportFileMaxSizeMB, "Size in MB at which to rotate Hubble export file.")
 	flags.Int("hubble-export-file-max-backups", def.ExportFileMaxBackups, "Number of rotated Hubble export files to keep.")
 	flags.Bool("hubble-export-file-compress", def.ExportFileCompress, "Compress rotated Hubble export files.")
-	flags.StringSlice("hubble-export-allowlist", []string{}, "Specify allowlist as JSON encoded FlowFilters to Hubble exporter.")
-	flags.StringSlice("hubble-export-denylist", []string{}, "Specify denylist as JSON encoded FlowFilters to Hubble exporter.")
+	flags.String("hubble-export-allowlist", "", "Specify allowlist as JSON encoded FlowFilters to Hubble exporter.")
+	flags.String("hubble-export-denylist", "", "Specify denylist as JSON encoded FlowFilters to Hubble exporter.")
 	flags.StringSlice("hubble-export-fieldmask", def.ExportFieldmask, "Specify list of fields to use for field mask in Hubble exporter.")
 }
 
@@ -104,9 +105,18 @@ func NewHubbleStaticExporter(params hubbleExportersParams) (hubbleExportersOut, 
 		return hubbleExportersOut{}, nil
 	}
 
+	allowList, err := hubble.ParseFlowFilters(params.Config.ExportAllowlist)
+	if err != nil {
+		return hubbleExportersOut{}, fmt.Errorf("failed to parse allowlist: %w", err)
+	}
+	denyList, err := hubble.ParseFlowFilters(params.Config.ExportDenylist)
+	if err != nil {
+		return hubbleExportersOut{}, fmt.Errorf("failed to parse denylist: %w", err)
+	}
+
 	exporterOpts := []exporter.Option{
-		exporter.WithAllowList(params.Logger, params.Config.ExportAllowlist),
-		exporter.WithDenyList(params.Logger, params.Config.ExportDenylist),
+		exporter.WithAllowList(params.Logger, allowList),
+		exporter.WithDenyList(params.Logger, denyList),
 		exporter.WithFieldMask(params.Config.ExportFieldmask),
 	}
 	if params.Config.ExportFilePath != "stdout" {

pkg/hubble/helpers.go
@@ -5,19 +5,26 @@ package hubble
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"strings"
 
 	flowpb "github.com/cilium/cilium/api/v1/flow"
 )
 
-func ParseFlowFilters(args ...string) ([]*flowpb.FlowFilter, error) {
-	filters := make([]*flowpb.FlowFilter, 0, len(args))
-	for _, enc := range args {
-		dec := json.NewDecoder(strings.NewReader(enc))
+// ParseFlowFilters parses a whitespace-delimited list of JSON-encoded flow filters.
+func ParseFlowFilters(arg string) ([]*flowpb.FlowFilter, error) {
+	var filters []*flowpb.FlowFilter
+	dec := json.NewDecoder(strings.NewReader(arg))
+	for {
 		var filter flowpb.FlowFilter
-		if err := dec.Decode(&filter); err != nil {
-			return nil, fmt.Errorf("failed to decode flow filter '%v': %w", enc, err)
+		err := dec.Decode(&filter)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode flow filters %q: %w", arg, err)
 		}
 		filters = append(filters, &filter)
 	}

pkg/hubble/helpers_test.go
@@ -1,0 +1,73 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package hubble
+
+import (
+	"fmt"
+	"testing"
+
+	flowpb "github.com/cilium/cilium/api/v1/flow"
+	"github.com/cilium/cilium/test/helpers"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseFlowFilters(t *testing.T) {
+	testCases := []struct {
+		name    string
+		arg     string
+		want    []*flowpb.FlowFilter
+		wantErr bool
+	}{
+		// valid
+		{name: "empty"},
+		{name: "whitespaces", arg: "   "},
+		{name: "one json", arg: "{}", want: []*flowpb.FlowFilter{{}}},
+		{name: "one json whitespaces", arg: "{}  ", want: []*flowpb.FlowFilter{{}}},
+		{
+			name: "one json whitespaces",
+			arg:  ` { "source_ip": ["1.2.3.4",  "2.3.4.5"]  } `,
+			want: []*flowpb.FlowFilter{{
+				SourceIp: []string{"1.2.3.4", "2.3.4.5"},
+			}},
+		},
+		{name: "two jsons", arg: "{}{}", want: []*flowpb.FlowFilter{{}, {}}},
+		{name: "two jsons whitespaces", arg: "{}  {}", want: []*flowpb.FlowFilter{{}, {}}},
+		{name: "two jsons whitespaces", arg: "{}  {}   ", want: []*flowpb.FlowFilter{{}, {}}},
+		{
+			name: "two json whitespaces",
+			arg: `
+                { "source_ip": ["1.2.3.4",  "2.3.4.5"],
+                  "destination_ip": ["2.3.4.5", "1.2.3.4"] }
+                { "destination_pod": ["mypod1",  "mypod2"]  }
+            `,
+			want: []*flowpb.FlowFilter{
+				{SourceIp: []string{"1.2.3.4", "2.3.4.5"}, DestinationIp: []string{"2.3.4.5", "1.2.3.4"}},
+				{DestinationPod: []string{"mypod1", "mypod2"}},
+			},
+		},
+		// invalid
+		{name: "quoted json", arg: "'{}'", wantErr: true},
+		{name: "quoted jsons", arg: "'{}''{}'", wantErr: true},
+		{name: "comma delimited", arg: "{},{}", wantErr: true},
+		{name: "json array", arg: "[]", wantErr: true},
+		{name: "json array", arg: "[{}]", wantErr: true},
+		{name: "json array", arg: "[{},{}]", wantErr: true},
+		{name: "invalid json", arg: "{", wantErr: true},
+		{name: "invalid json", arg: "{}}", wantErr: true},
+		{name: "invalid json", arg: "}}", wantErr: true},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d-%s", i, tc.name), func(t *testing.T) {
+			got, err := ParseFlowFilters(tc.arg)
+			if tc.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			helpers.AssertProtoEqual(t, tc.want, got)
+		})
+	}
+}