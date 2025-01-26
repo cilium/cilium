cilium/cilium

commit 1ff9be2d77e23f5c4fc1b5a7bd8752874738492b

policy/k8s: Move ToServices translation to policy package
This commit moves the logic which translates `ToServices` rules to
`ToCIDRSet` rules into `pkg/policy/k8s`. In doing so, we also change how
translation is performed:

Previously, translation was split over multiple packages: Initial policy
ingestion (in `pkg/policy`) invoked `k8s.PreprocessRules`, which
populated the initial version of the policy with `ToCIDRSet` rules.
Service updates were handled incrementally by the service watcher, which
invoked a `k8s.RuleTranslator` for each update and allocating CIDR
identities (via IPCache) accordingly.

The new approach centralizes all the logic into `pkg/policy/k8s`. We
mirror the approach from the existing `CiliumCIDRGroup` logic which also
translates references to `CiliumCIDRGroups` to `ToSericeSet`s. Because
the new approach modifies the CNP/CCNP before it is added to the policy
repository, we no longer perform incremental updates and no longer need
manage CIDR identities ourselves. This simplifies the code and allows us
to remove one of the last users of the "synchronous IPCache API".

As a side-effect of this change, `ToServices` rules for policies
ingested via the non-persistent API (i.e. `cilium-dbg policy import`)
are no longer supported. We assume that this was not a commonly used
feature for two reasons: First, importing non-K8s policies that refer
K8s resources (i.e. services) is likely rather rare. And secondly, the
previously implementation was slightly broken: Because policies imported
via API were not preprocessed, the policy was defunct until one of the
matched services generated an update event.

Note that this commit only removes the conflicting parts the old
infrastructure to keep this commit reviewable. The remaining code of the
old infrastructure is removed in a subsequent commit.

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/policy/k8s/cell.go
@@ -6,6 +6,7 @@ package k8s
 import (
 	"context"
 
+	"github.com/cilium/stream"
 	"github.com/sirupsen/logrus"
 
 	"github.com/cilium/cilium/pkg/hive/cell"
@@ -49,6 +50,10 @@ type PolicyManager interface {
 	PolicyDelete(labels labels.LabelArray, opts *policy.DeleteOptions) (newRev uint64, err error)
 }
 
+type serviceCache interface {
+	ForEachService(func(svcID k8s.ServiceID, svc *k8s.Service, eps *k8s.Endpoints) bool)
+}
+
 type PolicyWatcherParams struct {
 	cell.In
 
@@ -75,10 +80,12 @@ func startK8sPolicyWatcher(p PolicyWatcherParams) {
 		return // skip watcher if K8s is not enabled
 	}
 
-	var (
-		ctx    context.Context
-		cancel context.CancelFunc
-	)
+	// We want to subscribe before the start hook is invoked in order to not miss
+	// any events
+	ctx, cancel := context.WithCancel(context.Background())
+	svcCacheNotifications := stream.ToChannel(ctx, p.ServiceCache.Notifications(),
+		stream.WithBufferSize(int(p.Config.K8sServiceCacheSize)))
+
 	p.Lifecycle.Append(cell.Hook{
 		OnStart: func(startCtx cell.HookContext) error {
 			policyManager, err := p.PolicyManager.Await(startCtx)
@@ -86,13 +93,13 @@ func startK8sPolicyWatcher(p PolicyWatcherParams) {
 				return err
 			}
 
-			ctx, cancel = context.WithCancel(context.Background())
 			w := &PolicyWatcher{
 				log:                              p.Logger,
 				config:                           p.Config,
 				k8sResourceSynced:                p.K8sResourceSynced,
 				k8sAPIGroups:                     p.K8sAPIGroups,
-				K8sSvcCache:                      p.ServiceCache,
+				svcCache:                         p.ServiceCache,
+				svcCacheNotifications:            svcCacheNotifications,
 				policyManager:                    policyManager,
 				CiliumNetworkPolicies:            p.CiliumNetworkPolicies,
 				CiliumClusterwideNetworkPolicies: p.CiliumClusterwideNetworkPolicies,
@@ -102,6 +109,9 @@ func startK8sPolicyWatcher(p PolicyWatcherParams) {
 				cnpCache:          make(map[resource.Key]*types.SlimCNP),
 				cidrGroupCache:    make(map[string]*cilium_v2_alpha1.CiliumCIDRGroup),
 				cidrGroupPolicies: make(map[resource.Key]struct{}),
+
+				toServicesPolicies: make(map[resource.Key]struct{}),
+				cnpByServiceID:     make(map[k8s.ServiceID]map[resource.Key]struct{}),
 			}
 
 			w.watchResources(ctx)

pkg/policy/k8s/service.go
@@ -1,0 +1,322 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package k8s
+
+import (
+	"errors"
+
+	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/labels"
+
+	"github.com/cilium/cilium/pkg/k8s"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/k8s/types"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/cilium/cilium/pkg/time"
+)
+
+// isSelectableService returns true if the service svc can be selected by a ToServices rule.
+// Normally, only services without a label selector (i.e. empty services)
+// are allowed as targets of a toServices rule.
+// This is to minimize the chances of a pod IP being selected by this rule, which might
+// cause conflicting entries in the ipcache.
+//
+// This requirement, however, is dropped for HighScale IPCache mode, because pod IPs are
+// normally excluded from the ipcache regardless. Therefore, in HighScale IPCache mode,
+// all services can be selected by ToServices.
+func (p *PolicyWatcher) isSelectableService(svc *k8s.Service) bool {
+	if svc == nil {
+		return false
+	}
+	return p.config.EnableHighScaleIPcache || svc.IsExternal()
+}
+
+// onServiceEvent processes a ServiceNotification and (if necessary)
+// recalculates all policies affected by this change.
+func (p *PolicyWatcher) onServiceEvent(event k8s.ServiceNotification) {
+	err := p.updateToServicesPolicies(event.ID, event.Service, event.OldService)
+	if err != nil {
+		p.log.WithError(err).WithFields(logrus.Fields{
+			logfields.Event:     event.Action,
+			logfields.ServiceID: event.ID,
+		}).Warning("Failed to recalculate CiliumNetworkPolicy rules after service event")
+	}
+}
+
+// updateToServicesPolicies is to be invoked when a service has changed (i.e. it was
+// added, removed, its endpoints have changed, or its labels have changed).
+// This function then checks if any of the known CNP/CCNPs are affected by this
+// change, and recomputes them by calling resolveCiliumNetworkPolicyRefs.
+func (p *PolicyWatcher) updateToServicesPolicies(svcID k8s.ServiceID, newSVC, oldSVC *k8s.Service) error {
+	var errs []error
+
+	// Bail out early if updated service is not selectable
+	if !(p.isSelectableService(newSVC) || p.isSelectableService(oldSVC)) {
+		return nil
+	}
+
+	// newService is true if this is the first time we observe this service
+	newService := oldSVC == nil
+	// changedService is true if the service label or selector has changed
+	changedService := !newSVC.DeepEqual(oldSVC)
+
+	// candidatePolicyKeys contains the set of policy names we need to process
+	// for this service update. By default, we consider all policies with
+	// a ToServices selector as candidates.
+	candidatePolicyKeys := p.toServicesPolicies
+	if !(newService || changedService) {
+		// If the service definition itself has not changed, and it's not the
+		// first time we process this service, we only need to check the
+		// policies which are known to select the old version of the service
+		candidatePolicyKeys = p.cnpByServiceID[svcID]
+	}
+
+	// Iterate over all policies potentially affected by this service update,
+	// and re-resolve the policy refs for each.
+	for key := range candidatePolicyKeys {
+		cnp, ok := p.cnpCache[key]
+		if !ok {
+			p.log.WithFields(logrus.Fields{
+				logfields.Key:       key,
+				logfields.ServiceID: svcID,
+			}).Error("BUG: Candidate policy for service update not found. Please report this bug to Cilium developers.")
+			continue
+		}
+
+		// Skip policies which are not affected by this service update
+		if !(p.cnpMatchesService(cnp, svcID, newSVC) ||
+			(!newService && changedService && p.cnpMatchesService(cnp, svcID, oldSVC))) {
+			continue
+		}
+
+		if p.config.Debug {
+			p.log.WithFields(logrus.Fields{
+				logfields.CiliumNetworkPolicyName: cnp.Name,
+				logfields.K8sAPIVersion:           cnp.APIVersion,
+				logfields.K8sNamespace:            cnp.Namespace,
+				logfields.ServiceID:               svcID,
+			}).Debug("Service updated or deleted, recalculating CiliumNetworkPolicy rules")
+		}
+		initialRecvTime := time.Now()
+
+		resourceID := resourceIDForCiliumNetworkPolicy(key, cnp)
+
+		errs = append(errs, p.resolveCiliumNetworkPolicyRefs(cnp, key, initialRecvTime, resourceID))
+	}
+	return errors.Join(errs...)
+}
+
+// resolveToServices translates all ToServices rules found in the provided CNP
+// and to corresponding ToCIDRSet rules. Mutates the passed in cnp in place.
+func (p *PolicyWatcher) resolveToServices(key resource.Key, cnp *types.SlimCNP) {
+	// We consult the service cache to obtain the service endpoints
+	// which are selected by the ToServices selectors found in the CNP.
+	p.svcCache.ForEachService(func(svcID k8s.ServiceID, svc *k8s.Service, eps *k8s.Endpoints) bool {
+		if !p.isSelectableService(svc) {
+			return true // continue
+		}
+
+		// svcEndpoints caches the selected endpoints in case they are
+		// referenced more than once by this CNP
+		svcEndpoints := newServiceEndpoints(svcID, svc, eps)
+
+		// This extracts the selected service endpoints from the rule
+		// and translates it to a ToCIDRSet
+		numMatches := svcEndpoints.processRule(cnp.Spec)
+		for _, spec := range cnp.Specs {
+			numMatches += svcEndpoints.processRule(spec)
+		}
+
+		// Mark the policy as selecting the service svcID. This allows us to
+		// reduce the number of policy candidates in updateToServicesPolicies
+		if numMatches > 0 {
+			p.markCNPForService(key, svcID)
+		} else {
+			p.clearCNPForService(key, svcID)
+		}
+
+		return true
+	})
+}
+
+// cnpMatchesService returns true if the cnp contains a ToServices rule which
+// matches the provided service svcID/svc
+func (p *PolicyWatcher) cnpMatchesService(cnp *types.SlimCNP, svcID k8s.ServiceID, svc *k8s.Service) bool {
+	if !p.isSelectableService(svc) {
+		return false
+	}
+
+	if hasMatchingToServices(cnp.Spec, svcID, svc) {
+		return true
+	}
+
+	for _, spec := range cnp.Specs {
+		if hasMatchingToServices(spec, svcID, svc) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// markCNPForService marks that a policy (referred to by 'key') contains a
+// ToServices selector selecting the service svcID
+func (p *PolicyWatcher) markCNPForService(key resource.Key, svcID k8s.ServiceID) {
+	svcMap, ok := p.cnpByServiceID[svcID]
+	if !ok {
+		svcMap = make(map[resource.Key]struct{}, 1)
+		p.cnpByServiceID[svcID] = svcMap
+	}
+
+	svcMap[key] = struct{}{}
+}
+
+// clearCNPForService indicates that a policy (referred to by 'key') no longer
+// selects the service svcID via a ToServices rule
+func (p *PolicyWatcher) clearCNPForService(key resource.Key, svcID k8s.ServiceID) {
+	delete(p.cnpByServiceID[svcID], key)
+	if len(p.cnpByServiceID[svcID]) == 0 {
+		delete(p.cnpByServiceID, svcID)
+	}
+}
+
+// specHasMatchingToServices returns true if the rule contains a ToServices rule which
+// matches the provided service svcID/svc
+func hasMatchingToServices(spec *api.Rule, svcID k8s.ServiceID, svc *k8s.Service) bool {
+	if spec == nil {
+		return false
+	}
+	for _, egress := range spec.Egress {
+		for _, toService := range egress.ToServices {
+			if sel := toService.K8sServiceSelector; sel != nil {
+				if serviceSelectorMatches(sel, svcID, svc) {
+					return true
+				}
+			} else if ref := toService.K8sService; ref != nil {
+				if serviceRefMatches(ref, svcID) {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+// hasToServices returns true if the CNP contains a ToServices rule
+func hasToServices(cnp *types.SlimCNP) bool {
+	if specHasToServices(cnp.Spec) {
+		return true
+	}
+	for _, spec := range cnp.Specs {
+		if specHasToServices(spec) {
+			return true
+		}
+	}
+	return false
+}
+
+// specHasToServices returns true if the rule contains a ToServices rule
+func specHasToServices(spec *api.Rule) bool {
+	if spec == nil {
+		return false
+	}
+	for _, egress := range spec.Egress {
+		if len(egress.ToServices) > 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+// serviceSelectorMatches returns true if the ToServices k8sServiceSelector
+// matches the labels of the provided service svc
+func serviceSelectorMatches(sel *api.K8sServiceSelectorNamespace, svcID k8s.ServiceID, svc *k8s.Service) bool {
+	if !(sel.Namespace == svcID.Namespace || sel.Namespace == "") {
+		return false
+	}
+
+	es := api.EndpointSelector(sel.Selector)
+	es.SyncRequirementsWithLabelSelector()
+	return es.Matches(labels.Set(svc.Labels))
+}
+
+// serviceRefMatches returns true if the ToServices k8sService reference
+// matches the name/namespace of the provided service svc
+func serviceRefMatches(ref *api.K8sServiceNamespace, svcID k8s.ServiceID) bool {
+	return (ref.Namespace == svcID.Namespace || ref.Namespace == "") &&
+		ref.ServiceName == svcID.Name
+}
+
+// serviceEndpoints stores the endpoints associated with a service
+type serviceEndpoints struct {
+	svcID k8s.ServiceID
+	svc   *k8s.Service
+	eps   *k8s.Endpoints
+
+	valid  bool
+	cached []api.CIDR
+}
+
+// newServiceEndpoints returns an initialized serviceEndpoints struct
+func newServiceEndpoints(svcID k8s.ServiceID, svc *k8s.Service, eps *k8s.Endpoints) *serviceEndpoints {
+	return &serviceEndpoints{
+		svcID: svcID,
+		svc:   svc,
+		eps:   eps,
+	}
+}
+
+// endpoints returns the service's endpoints as an []api.CIDR slice.
+// It caches the result such that repeat invocations do not allocate.
+func (s *serviceEndpoints) endpoints() []api.CIDR {
+	if s.valid {
+		return s.cached
+	}
+
+	prefixes := s.eps.Prefixes()
+	s.cached = make([]api.CIDR, 0, len(prefixes))
+	for _, prefix := range prefixes {
+		s.cached = append(s.cached, api.CIDR(prefix.String()))
+	}
+
+	s.valid = true
+	return s.cached
+}
+
+// appendEndpoints appends all the endpoint as generated CIDRRules into the toCIDRSet
+func appendEndpoints(toCIDRSet *api.CIDRRuleSlice, endpoints []api.CIDR) {
+	for _, cidr := range endpoints {
+		*toCIDRSet = append(*toCIDRSet, api.CIDRRule{
+			Cidr:      cidr,
+			Generated: true,
+		})
+	}
+}
+
+// processRule parses the ToServices selectors in the provided rule and translates
+// it to ToCIDRSet entries
+func (s *serviceEndpoints) processRule(rule *api.Rule) (numMatches int) {
+	if rule == nil {
+		return
+	}
+	for i, egress := range rule.Egress {
+		for _, toService := range egress.ToServices {
+			if sel := toService.K8sServiceSelector; sel != nil {
+				if serviceSelectorMatches(sel, s.svcID, s.svc) {
+					appendEndpoints(&rule.Egress[i].ToCIDRSet, s.endpoints())
+					numMatches++
+				}
+			} else if ref := toService.K8sService; ref != nil {
+				if serviceRefMatches(ref, s.svcID) {
+					appendEndpoints(&rule.Egress[i].ToCIDRSet, s.endpoints())
+					numMatches++
+				}
+			}
+		}
+	}
+	return numMatches
+}

pkg/policy/k8s/watcher.go
@@ -27,8 +27,9 @@ type PolicyWatcher struct {
 	k8sResourceSynced *k8sSynced.Resources
 	k8sAPIGroups      *k8sSynced.APIGroups
 
-	policyManager PolicyManager
-	K8sSvcCache   *k8s.ServiceCache
+	policyManager         PolicyManager
+	svcCache              serviceCache
+	svcCacheNotifications <-chan k8s.ServiceNotification
 
 	CiliumNetworkPolicies            resource.Resource[*cilium_v2.CiliumNetworkPolicy]
 	CiliumClusterwideNetworkPolicies resource.Resource[*cilium_v2.CiliumClusterwideNetworkPolicy]
@@ -45,6 +46,9 @@ type PolicyWatcher struct {
 	cidrGroupCache map[string]*cilium_api_v2alpha1.CiliumCIDRGroup
 	// cidrGroupPolicies is the set of policies that are referencing CiliumCIDRGroup objects.
 	cidrGroupPolicies map[resource.Key]struct{}
+	// cidrGroupPolicies is the set of policies that contain ToServices references
+	toServicesPolicies map[resource.Key]struct{}
+	cnpByServiceID     map[k8s.ServiceID]map[resource.Key]struct{}
 }
 
 func (p *PolicyWatcher) watchResources(ctx context.Context) {
@@ -57,6 +61,7 @@ func (p *PolicyWatcher) watchResources(ctx context.Context) {
 		cnpEvents := p.CiliumNetworkPolicies.Events(ctx)
 		ccnpEvents := p.CiliumClusterwideNetworkPolicies.Events(ctx)
 		cidrGroupEvents := p.CiliumCIDRGroups.Events(ctx)
+		serviceEvents := p.svcCacheNotifications
 
 		for {
 			select {
@@ -170,8 +175,18 @@ func (p *PolicyWatcher) watchResources(ctx context.Context) {
 					err = p.onDeleteCIDRGroup(event.Object.Name, k8sAPIGroupCiliumCIDRGroupV2Alpha1)
 				}
 				event.Done(err)
+			case event, ok := <-serviceEvents:
+				if !ok {
+					serviceEvents = nil
+					break
+				}
+
+				switch event.Action {
+				case k8s.UpdateService, k8s.DeleteService:
+					p.onServiceEvent(event)
+				}
 			}
-			if knpEvents == nil && cnpEvents == nil && ccnpEvents == nil && cidrGroupEvents == nil {
+			if knpEvents == nil && cnpEvents == nil && ccnpEvents == nil && cidrGroupEvents == nil && serviceEvents == nil {
 				return
 			}
 		}

pkg/k8s/watchers/watcher.go
@@ -563,59 +563,9 @@ func (k *K8sWatcher) k8sServiceHandler() {
 			if err := k.addK8sSVCs(event.ID, event.OldService, svc, event.Endpoints); err != nil {
 				scopedLog.WithError(err).Error("Unable to add/update service to implement k8s event")
 			}
-
-			// Normally, only services without a label selector (i.e. "bottomless" or empty services)
-			// are allowed as targets of a toServices rule.
-			// This is to minimize the chances of a pod IP being selected by this rule, which might
-			// cause conflicting entries in the ipcache.
-			//
-			// This requirement, however, is dropped for HighScale IPCache mode, because pod IPs are
-			// normally excluded from the ipcache regardless.
-			if !option.Config.EnableHighScaleIPcache && !svc.IsExternal() {
-				return
-			}
-
-			var oldEP k8s.Endpoints
-			if event.OldEndpoints != nil {
-				oldEP = *event.OldEndpoints
-			}
-			translator := k8s.NewK8sTranslator(event.ID, oldEP, *event.Endpoints, svc.Labels)
-			result, err := k.policyRepository.TranslateRules(translator)
-			if err != nil {
-				log.WithError(err).Error("Unable to repopulate egress policies from ToService rules")
-				break
-			} else if result.NumToServicesRules > 0 {
-				// Only trigger policy updates if ToServices rules are in effect
-				k.ipcache.ReleaseCIDRIdentitiesByCIDR(result.PrefixesToRelease)
-				_, err := k.ipcache.AllocateCIDRs(result.PrefixesToAdd, nil)
-				if err != nil {
-					scopedLog.WithError(err).
-						Error("Unabled to allocate ipcache CIDR for toService rule")
-					break
-				}
-				k.policyManager.TriggerPolicyUpdates(true, "Kubernetes service endpoint added")
-			}
-
 		case k8s.DeleteService:
 			if err := k.delK8sSVCs(event.ID, event.Service, event.Endpoints); err != nil {
 				scopedLog.WithError(err).Error("Unable to delete service to implement k8s event")
-			}
-
-			if !option.Config.EnableHighScaleIPcache && !svc.IsExternal() {
-				return
-			}
-
-			// Use the current Endpoints object as the "old" object and an empty
-			// Endpoints as "new" object.
-			translator := k8s.NewK8sTranslator(event.ID, *event.Endpoints, k8s.Endpoints{}, svc.Labels)
-			result, err := k.policyRepository.TranslateRules(translator)
-			if err != nil {
-				log.WithError(err).Error("Unable to depopulate egress policies from ToService rules")
-				break
-			} else if result.NumToServicesRules > 0 {
-				// Only trigger policy updates if ToServices rules are in effect
-				k.ipcache.ReleaseCIDRIdentitiesByCIDR(result.PrefixesToRelease)
-				k.policyManager.TriggerPolicyUpdates(true, "Kubernetes service endpoint deleted")
 			}
 		}
 	}

pkg/policy/k8s/cilium_network_policy.go
@@ -9,7 +9,6 @@ import (
 	"github.com/sirupsen/logrus"
 
 	ipcacheTypes "github.com/cilium/cilium/pkg/ipcache/types"
-	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/k8s/resource"
 	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -61,6 +60,13 @@ func (p *PolicyWatcher) onUpsert(
 	}
 	metrics.CIDRGroupsReferenced.Set(float64(len(p.cidrGroupPolicies)))
 
+	// check if this cnp was referencing or is now referencing at least one ToServices rule
+	if hasToServices(cnp) {
+		p.toServicesPolicies[key] = struct{}{}
+	} else {
+		delete(p.toServicesPolicies, key)
+	}
+
 	return p.resolveCiliumNetworkPolicyRefs(cnp, key, initialRecvTime, resourceID)
 }
 
@@ -73,8 +79,15 @@ func (p *PolicyWatcher) onDelete(
 	err := p.deleteCiliumNetworkPolicyV2(cnp, resourceID)
 	delete(p.cnpCache, key)
 
+	// Clear CIDRGroupRef index
 	delete(p.cidrGroupPolicies, key)
 	metrics.CIDRGroupsReferenced.Set(float64(len(p.cidrGroupPolicies)))
+
+	// Clear ToServices index
+	for svcID := range p.cnpByServiceID {
+		p.clearCNPForService(key, svcID)
+	}
+	delete(p.toServicesPolicies, key)
 
 	p.k8sResourceSynced.SetEventTimestamp(apiGroup)
 
@@ -102,6 +115,9 @@ func (p *PolicyWatcher) resolveCiliumNetworkPolicyRefs(
 	p.resolveCIDRGroupRef(translatedCNP)
 	metrics.CIDRGroupTranslationTimeStats.Observe(time.Since(translationStart).Seconds())
 
+	// Resolve ToService references
+	p.resolveToServices(key, translatedCNP)
+
 	err := p.upsertCiliumNetworkPolicyV2(translatedCNP, initialRecvTime, resourceID)
 	if err == nil {
 		p.cnpCache[key] = cnp
@@ -121,17 +137,12 @@ func (p *PolicyWatcher) upsertCiliumNetworkPolicyV2(cnp *types.SlimCNP, initialR
 
 	rules, policyImportErr := cnp.Parse()
 	if policyImportErr == nil {
-		policyImportErr = k8s.PreprocessRules(rules, p.K8sSvcCache)
-		// Replace all rules with the same name, namespace and
-		// resourceTypeCiliumNetworkPolicy
-		if policyImportErr == nil {
-			_, policyImportErr = p.policyManager.PolicyAdd(rules, &policy.AddOptions{
-				ReplaceWithLabels:   cnp.GetIdentityLabels(),
-				Source:              source.CustomResource,
-				ProcessingStartTime: initialRecvTime,
-				Resource:            resourceID,
-			})
-		}
+		_, policyImportErr = p.policyManager.PolicyAdd(rules, &policy.AddOptions{
+			ReplaceWithLabels:   cnp.GetIdentityLabels(),
+			Source:              source.CustomResource,
+			ProcessingStartTime: initialRecvTime,
+			Resource:            resourceID,
+		})
 	}
 
 	if policyImportErr != nil {

commit 9d373f8ec8a06c973d8617faaaa31c8cc2d2624b

policy/k8s: Do not rely on OldService==nil as proxy for new service
We cannot rely on the OldService field being nil for checking whether
this service has been processed or not as in the scenario where service
is seen first and endpoint after the OldService would be set even though
it had not been processed yet as ServiceCache delayed the event until
EndpointSlice was seen.

This worked accidentally before when on [ServiceCache.UpdateEndpoints] the
OldService was always left nil. This caused the service handling in policy
to always reprocess the service even though only its endpoints had changed.

With the introduction of the coalescing of ServiceEvents we now need to fill
the OldService in order to correctly coalesce deletion events. This broke
the policy service handling.

Fix this by keeping a record of seen service ids to understand whether or not
this services has been processed already.

Fixes: 1ff9be2d77e2 ("policy/k8s: Move ToServices translation to policy package")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/policy/k8s/cell.go
@@ -113,6 +113,7 @@ func startK8sPolicyWatcher(params PolicyWatcherParams) {
 
 		toServicesPolicies: make(map[resource.Key]struct{}),
 		cnpByServiceID:     make(map[k8s.ServiceID]map[resource.Key]struct{}),
+		seenServices:       sets.New[k8s.ServiceID](),
 		metricsManager:     params.MetricsManager,
 	}
 

pkg/policy/k8s/service.go
@@ -42,9 +42,15 @@ func (p *policyWatcher) onServiceEvent(event k8s.ServiceNotification) {
 func (p *policyWatcher) updateToServicesPolicies(svcID k8s.ServiceID, newSVC, oldSVC *k8s.Service) error {
 	var errs []error
 	// newService is true if this is the first time we observe this service
-	newService := oldSVC == nil
+	newService := !p.seenServices.Has(svcID)
 	// changedService is true if the service label or selector has changed
 	changedService := !newSVC.DeepEqual(oldSVC)
+
+	if newSVC != nil {
+		p.seenServices.Insert(svcID)
+	} else {
+		p.seenServices.Delete(svcID)
+	}
 
 	// candidatePolicyKeys contains the set of policy names we need to process
 	// for this service update. By default, we consider all policies with

pkg/policy/k8s/watcher.go
@@ -64,6 +64,7 @@ type policyWatcher struct {
 	// toServicesPolicies is the set of policies that contain ToServices references
 	toServicesPolicies map[resource.Key]struct{}
 	cnpByServiceID     map[k8s.ServiceID]map[resource.Key]struct{}
+	seenServices       sets.Set[k8s.ServiceID]
 
 	metricsManager CNPMetrics
 }

pkg/policy/k8s/service_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/k8s"
@@ -259,6 +260,7 @@ func TestPolicyWatcher_updateToServicesPolicies(t *testing.T) {
 		cnpCache:           map[resource.Key]*types.SlimCNP{},
 		toServicesPolicies: map[resource.Key]struct{}{},
 		cnpByServiceID:     map[k8s.ServiceID]map[resource.Key]struct{}{},
+		seenServices:       sets.New[k8s.ServiceID](),
 		metricsManager:     NewCNPMetricsNoop(),
 	}
 
@@ -539,6 +541,7 @@ func TestPolicyWatcher_updateToServicesPoliciesTransformToEndpoint(t *testing.T)
 		cnpCache:           map[resource.Key]*types.SlimCNP{},
 		toServicesPolicies: map[resource.Key]struct{}{},
 		cnpByServiceID:     map[k8s.ServiceID]map[resource.Key]struct{}{},
+		seenServices:       sets.New[k8s.ServiceID](),
 		metricsManager:     NewCNPMetricsNoop(),
 	}
 