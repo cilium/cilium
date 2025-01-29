cilium/cilium

commit 5f0e3e21ae40b7f8537e4949652aa99b98b264ab

operator: add troubleshoot subcommands
Make the same troubleshoot commands already introduced as part of the
cilium-dbg binary also available for the operator, to simplify
troubleshooting kvstore and remote clusters connectivity issues.

The troubleshoot commands can be run through something along the lines of:

$ kubectl exec -it -n kube-system deployments/cilium-operator -- \
      cilium-operator-generic troubleshoot kvstore
$ kubectl exec -it -n kube-system deployments/cilium-operator -- \
      cilium-operator-generic troubleshoot clustermesh

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

operator/cmd/root.go
@@ -27,6 +27,7 @@ import (
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 
 	operatorApi "github.com/cilium/cilium/api/v1/operator/server"
+	ciliumdbg "github.com/cilium/cilium/cilium-dbg/cmd"
 	"github.com/cilium/cilium/operator/api"
 	"github.com/cilium/cilium/operator/auth"
 	"github.com/cilium/cilium/operator/endpointgc"
@@ -281,6 +282,7 @@ func NewOperatorCmd(h *hive.Hive) *cobra.Command {
 	cmd.AddCommand(
 		MetricsCmd,
 		StatusCmd,
+		ciliumdbg.TroubleshootCmd,
 		h.Command(),
 	)
 

cilium-dbg/cmd/troubleshoot.go
@@ -21,7 +21,8 @@ import (
 	"github.com/cilium/cilium/pkg/loadbalancer"
 )
 
-// TroubleshootCmd represents the troubleshoot command
+// TroubleshootCmd represents the troubleshoot command. Note that this command
+// is additionally registered as a subcommand of the Cilium operator.
 var TroubleshootCmd = &cobra.Command{
 	Use:   "troubleshoot",
 	Short: "Run troubleshooting utilities to check control-plane connectivity",

commit 662557138621c322e1cd715f158d582d8b9f0089

cilium-dbg/troubleshoot: do not import cilium-dbg from operator
Since we are importing cilium-dbg from the operator, this is causing all
of its init() functions to be initialized causing some potential
concurrency issues as cilium-dbg is initializing functions assuming it's
the only binary using them, same as with the operator. To avoid this
problem, we will move the troubleshoot as an independent package that
can be imported by the operator, the clustermesh-apiserver and the
cilium-dbg tool.

Fixes: 5f0e3e21ae40 ("operator: add troubleshoot subcommands")
Signed-off-by: André Martins <andre@cilium.io>

cilium-dbg/cmd/root.go
@@ -12,6 +12,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
+	"github.com/cilium/cilium/cilium-dbg/cmd/troubleshoot"
 	clientPkg "github.com/cilium/cilium/pkg/client"
 	"github.com/cilium/cilium/pkg/cmdref"
 	"github.com/cilium/cilium/pkg/components"
@@ -55,6 +56,7 @@ func init() {
 	vp.BindPFlags(flags)
 	RootCmd.AddCommand(cmdref.NewCmd(RootCmd))
 	RootCmd.AddCommand(newCmdCompletion(os.Stdout))
+	RootCmd.AddCommand(troubleshoot.Cmd)
 	RootCmd.SetOut(os.Stdout)
 	RootCmd.SetErr(os.Stderr)
 }

cilium-dbg/cmd/troubleshoot/troubleshoot.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package cmd
+package troubleshoot
 
 import (
 	"context"
@@ -21,15 +21,11 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore"
 )
 
-// TroubleshootCmd represents the troubleshoot command. Note that this command
+// Cmd represents the troubleshoot command. Note that this command
 // is additionally registered as a subcommand of the Cilium operator.
-var TroubleshootCmd = &cobra.Command{
+var Cmd = &cobra.Command{
 	Use:   "troubleshoot",
 	Short: "Run troubleshooting utilities to check control-plane connectivity",
-}
-
-func init() {
-	RootCmd.AddCommand(TroubleshootCmd)
 }
 
 var _ kvstore.EtcdDbgDialer = (*troubleshootDialer)(nil)

cilium-dbg/cmd/troubleshoot/troubleshoot_clustermesh.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package cmd
+package troubleshoot
 
 import (
 	"context"
@@ -15,6 +15,7 @@ import (
 
 	"github.com/spf13/cobra"
 
+	clientPkg "github.com/cilium/cilium/pkg/client"
 	"github.com/cilium/cilium/pkg/clustermesh/common"
 	"github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/kvstore"
@@ -28,6 +29,9 @@ var troubleshootClusterMeshCmd = func() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "clustermesh [clusters...]",
 		Short: "Troubleshoot connectivity towards remote clusters",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			initConfig()
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			local := getLocalClusterName(cmd.ErrOrStderr())
 			TroubleshootClusterMesh(
@@ -41,12 +45,28 @@ var troubleshootClusterMeshCmd = func() *cobra.Command {
 	flags.StringVar(&cfg, "clustermesh-config", "/var/lib/cilium/clustermesh/", "Path to the ClusterMesh configuration directory")
 	flags.DurationVar(&timeout, "timeout", 5*time.Second, "Timeout when checking connectivity to a given cluster")
 	flags.BoolVar(&disableDialer, "without-service-resolution", false, "Disable k8s service to IP resolution through the k8s client")
+	flags.StringVar(&clientHost, "H", "", "URI to server-side API")
 
 	return cmd
 }()
 
 func init() {
-	TroubleshootCmd.AddCommand(troubleshootClusterMeshCmd)
+	Cmd.AddCommand(troubleshootClusterMeshCmd)
+}
+
+var (
+	clientHost string
+	client     *clientPkg.Client
+)
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cl, err := clientPkg.NewClient(clientHost); err != nil {
+		fmt.Fprintf(os.Stderr, "Error while creating client: %s\n", err)
+		os.Exit(1)
+	} else {
+		client = cl
+	}
 }
 
 func TroubleshootClusterMesh(

cilium-dbg/cmd/troubleshoot/troubleshoot_kvstore.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package cmd
+package troubleshoot
 
 import (
 	"context"
@@ -51,5 +51,5 @@ var troubleshootKVStoreCmd = func() *cobra.Command {
 }()
 
 func init() {
-	TroubleshootCmd.AddCommand(troubleshootKVStoreCmd)
+	Cmd.AddCommand(troubleshootKVStoreCmd)
 }

clustermesh-apiserver/kvstoremesh-dbg/troubleshoot.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/spf13/cobra"
 
-	ciliumdbg "github.com/cilium/cilium/cilium-dbg/cmd"
+	ciliumdbg "github.com/cilium/cilium/cilium-dbg/cmd/troubleshoot"
 	"github.com/cilium/cilium/pkg/kvstore"
 )
 

operator/cmd/root.go
@@ -25,7 +25,7 @@ import (
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 
 	operatorApi "github.com/cilium/cilium/api/v1/operator/server"
-	ciliumdbg "github.com/cilium/cilium/cilium-dbg/cmd"
+	troubleshoot "github.com/cilium/cilium/cilium-dbg/cmd/troubleshoot"
 	"github.com/cilium/cilium/operator/api"
 	"github.com/cilium/cilium/operator/auth"
 	"github.com/cilium/cilium/operator/doublewrite"
@@ -324,7 +324,7 @@ func NewOperatorCmd(h *hive.Hive) *cobra.Command {
 		cmdref.NewCmd(cmd),
 		MetricsCmd,
 		StatusCmd,
-		ciliumdbg.TroubleshootCmd,
+		troubleshoot.Cmd,
 		h.Command(),
 	)
 