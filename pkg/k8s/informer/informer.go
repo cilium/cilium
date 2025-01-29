cilium/cilium

commit 113a383e171c124f24ba48da0610422d27723871

resource: Use the cache package to dedup strings
Reduce the agent memory usage by deduplicating the strings and string maps
in the k8s object metadata.

Co-authored-by: David Bimmler <david.bimmler@isovalent.com>
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/k8s/resource/resource.go
@@ -22,6 +22,7 @@ import (
 	"k8s.io/client-go/util/workqueue"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 
+	metaCache "github.com/cilium/cilium/pkg/container/cache"
 	k8smetrics "github.com/cilium/cilium/pkg/k8s/metrics"
 	"github.com/cilium/cilium/pkg/k8s/synced"
 	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
@@ -856,6 +857,9 @@ func (r *resource[T]) newInformer() (cache.Indexer, cache.Controller) {
 				// this is a no-op in production environments.
 				cacheMutationDetector.AddObject(obj)
 
+				// Deduplicate the strings in the object metadata to reduce memory consumption.
+				dedupMetadata(obj)
+
 				key := NewKey(obj)
 
 				switch d.Type {
@@ -898,6 +902,18 @@ func (r *resource[T]) newInformer() (cache.Indexer, cache.Controller) {
 		Controller:            cache.New(cfg),
 		cacheMutationDetector: cacheMutationDetector,
 	}
+}
+
+// dedupMetadata deduplicates the allocated strings in the metadata using the container/cache package.
+func dedupMetadata(obj any) {
+	meta, err := meta.Accessor(obj)
+	if err != nil {
+		return
+	}
+	meta.SetName(metaCache.Strings.Get(meta.GetName()))
+	meta.SetNamespace(metaCache.Strings.Get(meta.GetNamespace()))
+	meta.SetLabels(metaCache.StringMaps.Get(meta.GetLabels()))
+	meta.SetAnnotations(metaCache.StringMaps.Get(meta.GetAnnotations()))
 }
 
 func getUID(obj k8sRuntime.Object) types.UID {

commit 38d7f9e6014b2e2c81717c3fdb5f423996c3eb62

pkg/k8s: add the DedupMetadata to the other informer
As we have two informer functions, we need to make sure they are in
sync, this commit also adds the missing DedupMetadata function that got
added in 113a383e171c.

Fixes: 113a383e171c ("resource: Use the cache package to dedup strings")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/informer/informer.go
@@ -13,6 +13,7 @@ import (
 	utilRuntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/tools/cache"
 
+	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/time"
@@ -114,6 +115,9 @@ func NewInformerWithStore(
 				} else {
 					obj = d.Object
 				}
+
+				// Deduplicate the strings in the object metadata to reduce memory consumption.
+				resources.DedupMetadata(obj)
 
 				// In CI we detect if the objects were modified and panic
 				// this is a no-op in production environments.

pkg/k8s/resource/resource.go
@@ -22,7 +22,6 @@ import (
 	"k8s.io/client-go/util/workqueue"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 
-	metaCache "github.com/cilium/cilium/pkg/container/cache"
 	k8smetrics "github.com/cilium/cilium/pkg/k8s/metrics"
 	"github.com/cilium/cilium/pkg/k8s/synced"
 	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
@@ -853,7 +852,7 @@ func (r *resource[T]) newInformer() (cache.Indexer, cache.Controller) {
 				}
 
 				// Deduplicate the strings in the object metadata to reduce memory consumption.
-				dedupMetadata(obj)
+				resources.DedupMetadata(obj)
 
 				// In CI we detect if the objects were modified and panic
 				// (e.g. when KUBE_CACHE_MUTATION_DETECTOR is set)
@@ -902,18 +901,6 @@ func (r *resource[T]) newInformer() (cache.Indexer, cache.Controller) {
 		Controller:            cache.New(cfg),
 		cacheMutationDetector: cacheMutationDetector,
 	}
-}
-
-// dedupMetadata deduplicates the allocated strings in the metadata using the container/cache package.
-func dedupMetadata(obj any) {
-	meta, err := meta.Accessor(obj)
-	if err != nil {
-		return
-	}
-	meta.SetName(metaCache.Strings.Get(meta.GetName()))
-	meta.SetNamespace(metaCache.Strings.Get(meta.GetNamespace()))
-	meta.SetLabels(metaCache.StringMaps.Get(meta.GetLabels()))
-	meta.SetAnnotations(metaCache.StringMaps.Get(meta.GetAnnotations()))
 }
 
 func getUID(obj k8sRuntime.Object) types.UID {

pkg/k8s/watchers/resources/resources.go
@@ -5,6 +5,12 @@
 // K8s watchers.
 package resources
 
+import (
+	"k8s.io/apimachinery/pkg/api/meta"
+
+	"github.com/cilium/cilium/pkg/container/cache"
+)
+
 const (
 	// K8sAPIGroupServiceV1Core is the identifier for K8s resources of type core/v1/Service.
 	K8sAPIGroupServiceV1Core = "core/v1::Service"
@@ -34,3 +40,15 @@ const (
 	// MetricDelete the label for watcher metrics related to delete events.
 	MetricDelete = "delete"
 )
+
+// dedupMetadata deduplicates the allocated strings in the metadata using the container/cache package.
+func DedupMetadata(obj any) {
+	meta, err := meta.Accessor(obj)
+	if err != nil {
+		return
+	}
+	meta.SetName(cache.Strings.Get(meta.GetName()))
+	meta.SetNamespace(cache.Strings.Get(meta.GetNamespace()))
+	meta.SetLabels(cache.StringMaps.Get(meta.GetLabels()))
+	meta.SetAnnotations(cache.StringMaps.Get(meta.GetAnnotations()))
+}