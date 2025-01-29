cilium/cilium

commit e33b36edcd70cbe6ccc1ce8382960a72bb4a37a9

cilium, ipam: Use clientset in newIPAMAPIHandler
The IPAM hooks really depend on whether k8s client is enabled or not.
Thus convert to the latter rather than using the LB switch to set up
ipamAPIHandler.

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/ipam/cell/cell.go
@@ -58,6 +58,7 @@ type ipamAPIHandlerParams struct {
 	cell.In
 
 	IPAM            *ipam.IPAM
+	Clientset       k8sClient.Clientset
 	EndpointManager endpointmanager.EndpointManager
 }
 
@@ -70,7 +71,7 @@ type ipamAPIHandlerOut struct {
 }
 
 func newIPAMAPIHandler(params ipamAPIHandlerParams) ipamAPIHandlerOut {
-	if option.Config.LoadBalancerOnly {
+	if !params.Clientset.IsEnabled() {
 		return ipamAPIHandlerOut{}
 	}
 

commit e3351b223045da2a488d379e6c0428ec53d7b69e

cilium, ipam: Remove clientset conditional in newIPAMAPIHandler
Remove the clientset conditional in newIPAMAPIHandler altogether. Sebastian
mentioned that IPAM API handlers should not be disabled. They are just
forwarding requests from the outside. If k8s is disabled, then cluster-pool
IPAM should still keep working. Normally, the range is derived from k8s,
but in this case it needs to be specified manually via --ipv4-range (see
below). Also, the docker plugin is relying on this and it has k8s client
disabled too. Thus, just remove the conditional altogether to fix it.

Before:

  # ./daemon/cilium-agent --enable-ipv4=true --enable-ipv6=false --datapath-mode=veth \
    --bpf-lb-only=true --bpf-lb-mode=snat --bpf-lb-acceleration=native --devices=enp5s0 \
    --enable-k8s=false
  [...]
  time="2025-01-29T12:55:59.875831476Z" level=info msg="Delete endpoint by containerID request" containerID=8064e0378f25c1e8842cc331fd836516dbf6b0fd4c70798ba03a89302488baed endpointID=793 k8sNamespace=kube-system k8sPodName=coredns-7c65d6cfc9-7glgh subsys=daemon
  time="2025-01-29T12:55:59.875925553Z" level=warning msg="Failed while waiting for initial global identities" endpointID=793 error="initial global identity sync was cancelled: context canceled" subsys=endpoint
  time="2025-01-29T12:55:59.906867027Z" level=info msg="Removed endpoint" ciliumEndpointName=kube-system/coredns-7c65d6cfc9-7glgh containerID=8064e0378f containerInterface=eth0 datapathPolicyRevision=0 desiredPolicyRevision=0 endpointID=793 identity=5 ipv4=10.128.205.58 ipv6= k8sPodName=kube-system/coredns-7c65d6cfc9-7glgh subsys=endpoint
  [...]

After:

  # ./daemon/cilium-agent --enable-ipv4=true --enable-ipv6=false --datapath-mode=veth \
    --bpf-lb-only=true --bpf-lb-mode=snat --bpf-lb-acceleration=native --devices=enp5s0 \
    --enable-k8s=false --ipv4-range="10.16.0.0/16"
  [...]
  time="2025-01-29T13:17:13.47010098Z" level=info msg="Delete endpoint by containerID request" containerID=b0adcadf2cd13dd6da8d157e9d0e91fd397ab59e1086f63896c2d156275e0e69 endpointID=2361 k8sNamespace=kube-system k8sPodName=coredns-7c65d6cfc9-7glgh subsys=daemon
  time="2025-01-29T13:17:13.504341657Z" level=info msg="Removed endpoint" ciliumEndpointName=kube-system/coredns-7c65d6cfc9-7glgh containerID=b0adcadf2c containerInterface= datapathPolicyRevision=1 desiredPolicyRevision=1 endpointID=2361 identity=5 ipv4=10.16.124.198 ipv6= k8sPodName=kube-system/coredns-7c65d6cfc9-7glgh subsys=endpoint
  [...]

Keep this here as a separate commit for future reference.

Fixes: e33b36edcd70 ("cilium, ipam: Use clientset in newIPAMAPIHandler")

pkg/ipam/cell/cell.go
@@ -58,7 +58,6 @@ type ipamAPIHandlerParams struct {
 	cell.In
 
 	IPAM            *ipam.IPAM
-	Clientset       k8sClient.Clientset
 	EndpointManager endpointmanager.EndpointManager
 }
 
@@ -71,10 +70,6 @@ type ipamAPIHandlerOut struct {
 }
 
 func newIPAMAPIHandler(params ipamAPIHandlerParams) ipamAPIHandlerOut {
-	if !params.Clientset.IsEnabled() {
-		return ipamAPIHandlerOut{}
-	}
-
 	return ipamAPIHandlerOut{
 		IpamDeleteIpamIPHandler: &ipamapi.IpamDeleteIpamIPHandler{
 			IPAM:            params.IPAM,