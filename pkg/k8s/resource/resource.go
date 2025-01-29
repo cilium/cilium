cilium/cilium

commit 113a383e171c124f24ba48da0610422d27723871

resource: Use the cache package to dedup strings
Reduce the agent memory usage by deduplicating the strings and string maps
in the k8s object metadata.

Co-authored-by: David Bimmler <david.bimmler@isovalent.com>
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/k8s/resource/resource.go
@@ -22,6 +22,7 @@ import (
 	"k8s.io/client-go/util/workqueue"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 
+	metaCache "github.com/cilium/cilium/pkg/container/cache"
 	k8smetrics "github.com/cilium/cilium/pkg/k8s/metrics"
 	"github.com/cilium/cilium/pkg/k8s/synced"
 	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
@@ -856,6 +857,9 @@ func (r *resource[T]) newInformer() (cache.Indexer, cache.Controller) {
 				// this is a no-op in production environments.
 				cacheMutationDetector.AddObject(obj)
 
+				// Deduplicate the strings in the object metadata to reduce memory consumption.
+				dedupMetadata(obj)
+
 				key := NewKey(obj)
 
 				switch d.Type {
@@ -898,6 +902,18 @@ func (r *resource[T]) newInformer() (cache.Indexer, cache.Controller) {
 		Controller:            cache.New(cfg),
 		cacheMutationDetector: cacheMutationDetector,
 	}
+}
+
+// dedupMetadata deduplicates the allocated strings in the metadata using the container/cache package.
+func dedupMetadata(obj any) {
+	meta, err := meta.Accessor(obj)
+	if err != nil {
+		return
+	}
+	meta.SetName(metaCache.Strings.Get(meta.GetName()))
+	meta.SetNamespace(metaCache.Strings.Get(meta.GetNamespace()))
+	meta.SetLabels(metaCache.StringMaps.Get(meta.GetLabels()))
+	meta.SetAnnotations(metaCache.StringMaps.Get(meta.GetAnnotations()))
 }
 
 func getUID(obj k8sRuntime.Object) types.UID {

commit 44d9a920383b29c3814fde64134188c55455b87b

pkg/k8s: fix cache mutation detection issue
The cache mutation detector flagged objects as modified because it assumes
they remain unchanged after being added. This fix ensures objects are not
mutated after caching to prevent false detections.

Fixes: 113a383e171c ("resource: Use the cache package to dedup strings")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/resource/resource.go
@@ -852,13 +852,13 @@ func (r *resource[T]) newInformer() (cache.Indexer, cache.Controller) {
 					obj = d.Object
 				}
 
+				// Deduplicate the strings in the object metadata to reduce memory consumption.
+				dedupMetadata(obj)
+
 				// In CI we detect if the objects were modified and panic
 				// (e.g. when KUBE_CACHE_MUTATION_DETECTOR is set)
 				// this is a no-op in production environments.
 				cacheMutationDetector.AddObject(obj)
-
-				// Deduplicate the strings in the object metadata to reduce memory consumption.
-				dedupMetadata(obj)
 
 				key := NewKey(obj)
 