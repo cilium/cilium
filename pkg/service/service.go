d4c2e297cc6d9293d61e5d32332e35872b39f81b
Daniel Borkmann <daniel@iogearbox.net>
2025-01-30 12:02:05 +0000

    cilium: Unconditionally upsert neighbor entries
    Starting with Cilium 1.18, we unconditionally upsert backend neighbor
    entries. This means that if backends are in a different L2 domain, we
    always keep the gateway entry "fresh" as managed neighbor, and if
    backends are in the same L2 domain, the kernel always has up to date
    hw address. Unconditionally given even with 'services with externalIPs'
    type, the external IPs could be in the same L2. We already keep track
    of in-cluster nodes to have them as managed neighbors, so this small
    change additionally keeps track of external endpoints which is not
    expected to add a lot to the total set of managed neighbors.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -861,10 +861,10 @@ func (s *Service) upsertService(params *lb.SVC) (bool, lb.ID, error) {
 		return false, lb.ID(0), err
 	}
 
-	// Update managed neighbor entries of the LB
-	if option.Config.LoadBalancerOnly {
-		s.upsertBackendNeighbors(newBackends, obsoleteBackends)
-	}
+	// Update managed neighbor entries of the LB, this is needed so that
+	// neighbor entries for the backends are always up to date if they
+	// reside in the same L2. In particular XDP cannot resolve on-demand.
+	s.upsertBackendNeighbors(newBackends, obsoleteBackends)
 
 	// Only add a HealthCheckNodePort server if this is a service which may
 	// only contain local backends (i.e. it has externalTrafficPolicy=Local)
@@ -1994,9 +1994,7 @@ func (s *Service) deleteServiceLocked(svc *svcInfo) error {
 	}
 
 	// Delete managed neighbor entries of the LB
-	if option.Config.LoadBalancerOnly {
-		s.deleteBackendNeighbors(obsoleteBackends)
-	}
+	s.deleteBackendNeighbors(obsoleteBackends)
 
 	if svc.healthcheckFrontendHash != "" {
 		healthSvc := s.svcByHash[svc.healthcheckFrontendHash]
