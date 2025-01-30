3579296146bc30eb6f8c74073dbffc4a9f879f4d
Jared Ledvina <jared.ledvina@datadoghq.com>
2025-01-30 14:22:12 -0500

    [metrics] Increase available api_duration_seconds metric buckets
    Signed-off-by: Jared Ledvina <jared.ledvina@datadoghq.com>

--- pkg/api/metrics/metrics.go
+++ pkg/api/metrics/metrics.go
@@ -29,6 +29,8 @@ func NewPrometheusMetrics(namespace, subsystem string, registry metrics.Register
 		Subsystem: subsystem,
 		Name:      "api_duration_seconds",
 		Help:      "Duration of interactions with API",
+		Buckets: []float64{0.005, 0.025, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 1.25, 1.5, 2, 3,
+			4, 5, 6, 8, 10, 15, 20, 30, 45, 60},
 	}, []string{"operation", "response_code"})
 
 	m.RateLimit = prometheus.NewHistogramVec(prometheus.HistogramOpts{
