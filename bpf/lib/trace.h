d265bda9705fd642f837d0ea2a22b9ac83e3df04
Simone Magnani <simone.magnani@isovalent.com>
2025-01-31 11:57:38 +0100

    bpf:monitor:hubble: decode pkt support for trace from L3 device
    [ upstream commit 978311f077cca8f351da162e9366fceffbaff78c ]
    
    This patch enabled the correct decoding of trace notifications when
    emitted from an L3 device (ex. Wireguard). Prior to this patch, our
    Monitor/Hubble logic always attempted to decode the raw bytes starting
    from the Ethernet Layer. This of course doesn't hold true for L3 devices,
    where we never see the L2 layer.
    
    Since we already have a flag field in the trace notification with some
    unused padding, let's borrow a bit to indicate whether the packet is being
    logged from an L3 device or not. This could have been also achieved from
    Monitor/Hubble directly, since the notification even has the ifindex, but
    it would have required to add logic to keep ifindexes related to L3
    interfaces up-to-date in Hubble/Monitor. IMHO, (1) this might not scale
    well, and (2) it is much clearer to have all info forwarded from the
    datapath. I'm not concerned about performance implication here since
    no new data is being forwarded in the trace event.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

--- bpf/lib/trace.h
+++ bpf/lib/trace.h
@@ -171,7 +171,8 @@ struct trace_notify {
 	__u16		dst_id;
 	__u8		reason;
 	__u8		ipv6:1;
-	__u8		pad:7;
+	__u8		l3_dev:1;
+	__u8		pad:6;
 	__u32		ifindex;
 	union {
 		struct {
@@ -223,6 +224,7 @@ _send_trace_notify(struct __ctx_buff *ctx, enum trace_point obs_point,
 		   __u32 src, __u32 dst, __u16 dst_id, __u32 ifindex,
 		   enum trace_reason reason, __u32 monitor, __u16 line, __u8 file)
 {
+	bool l3_dev = false, ipv6 = false;
 	__u64 ctx_len = ctx_full_len(ctx);
 	__u64 cap_len = min_t(__u64, monitor ? : TRACE_PAYLOAD_LEN,
 			      ctx_len);
@@ -246,6 +248,13 @@ _send_trace_notify(struct __ctx_buff *ctx, enum trace_point obs_point,
 			return;
 	}
 
+#if defined(ENABLE_WIREGUARD) && (defined(IS_BPF_HOST) || defined(IS_BPF_WIREGUARD))
+	if (THIS_INTERFACE_IFINDEX == WG_IFINDEX) {
+		l3_dev = true;
+		ipv6 = ctx->protocol == bpf_htons(ETH_P_IPV6);
+	}
+#endif
+
 	msg = (typeof(msg)) {
 		__notify_common_hdr(CILIUM_NOTIFY_TRACE, obs_point),
 		__notify_pktcap_hdr((__u32)ctx_len, (__u16)cap_len, NOTIFY_CAPTURE_VER),
@@ -253,6 +262,8 @@ _send_trace_notify(struct __ctx_buff *ctx, enum trace_point obs_point,
 		.dst_label	= dst,
 		.dst_id		= dst_id,
 		.reason		= reason,
+		.ipv6		= ipv6,
+		.l3_dev		= l3_dev,
 		.ifindex	= ifindex,
 	};
 	memset(&msg.orig_ip6, 0, sizeof(union v6addr));
@@ -267,6 +278,7 @@ send_trace_notify4(struct __ctx_buff *ctx, enum trace_point obs_point,
 		   __u32 src, __u32 dst, __be32 orig_addr, __u16 dst_id,
 		   __u32 ifindex, enum trace_reason reason, __u32 monitor)
 {
+	bool l3_dev = false;
 	__u64 ctx_len = ctx_full_len(ctx);
 	__u64 cap_len = min_t(__u64, monitor ? : TRACE_PAYLOAD_LEN,
 			      ctx_len);
@@ -290,6 +302,11 @@ send_trace_notify4(struct __ctx_buff *ctx, enum trace_point obs_point,
 			return;
 	}
 
+#if defined(ENABLE_WIREGUARD) && (defined(IS_BPF_HOST) || defined(IS_BPF_WIREGUARD))
+	if (THIS_INTERFACE_IFINDEX == WG_IFINDEX)
+		l3_dev = true;
+#endif
+
 	msg = (typeof(msg)) {
 		__notify_common_hdr(CILIUM_NOTIFY_TRACE, obs_point),
 		__notify_pktcap_hdr((__u32)ctx_len, (__u16)cap_len, NOTIFY_CAPTURE_VER),
@@ -298,7 +315,8 @@ send_trace_notify4(struct __ctx_buff *ctx, enum trace_point obs_point,
 		.dst_id		= dst_id,
 		.reason		= reason,
 		.ifindex	= ifindex,
-		.ipv6		= 0,
+		.ipv6		= false,
+		.l3_dev		= l3_dev,
 		.orig_ip4	= orig_addr,
 	};
 
@@ -313,6 +331,7 @@ send_trace_notify6(struct __ctx_buff *ctx, enum trace_point obs_point,
 		   __u16 dst_id, __u32 ifindex, enum trace_reason reason,
 		   __u32 monitor)
 {
+	bool l3_dev = false;
 	__u64 ctx_len = ctx_full_len(ctx);
 	__u64 cap_len = min_t(__u64, monitor ? : TRACE_PAYLOAD_LEN,
 			      ctx_len);
@@ -336,6 +355,11 @@ send_trace_notify6(struct __ctx_buff *ctx, enum trace_point obs_point,
 			return;
 	}
 
+#if defined(ENABLE_WIREGUARD) && (defined(IS_BPF_HOST) || defined(IS_BPF_WIREGUARD))
+	if (THIS_INTERFACE_IFINDEX == WG_IFINDEX)
+		l3_dev = true;
+#endif
+
 	msg = (typeof(msg)) {
 		__notify_common_hdr(CILIUM_NOTIFY_TRACE, obs_point),
 		__notify_pktcap_hdr((__u32)ctx_len, (__u16)cap_len, NOTIFY_CAPTURE_VER),
@@ -344,7 +368,8 @@ send_trace_notify6(struct __ctx_buff *ctx, enum trace_point obs_point,
 		.dst_id		= dst_id,
 		.reason		= reason,
 		.ifindex	= ifindex,
-		.ipv6		= 1,
+		.ipv6		= true,
+		.l3_dev		= l3_dev,
 	};
 
 	ipv6_addr_copy(&msg.orig_ip6, orig_addr);
