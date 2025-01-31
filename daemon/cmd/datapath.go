cilium/cilium

commit 60f074d63f9f8b3e6cfccf62d99f7c58b4d6c11d

bpf: Metrics for SNAT allocation retries
Collect a histogram of the number of attempts taken before a successful
(or an unsuccessful) port allocation for SNAT occurs. These data will be
useful for troubleshooting and for evaluating SNAT port allocation
algorithms. For example, collecting these statistics at various ratios
of allocated ports will show the distribution of how quickly the
algorithm can find an unused port (and whether at all).

New cilium-dbg commands are added:

cilium-dbg bpf nat retries list - to print the histogram.
cilium-dbg bpf nat retries flush - to reset the counters to zero.

Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

pkg/maps/nat/nat.go
@@ -33,6 +33,14 @@ const (
 	MinPortSnatDefault = 1024
 	// MaxPortSnatDefault represents default max port from range.
 	MaxPortSnatDefault = 65535
+
+	// MapNameSnat4AllocRetries represents the histogram of IPv4 NAT port allocation retries.
+	MapNameSnat4AllocRetries = "cilium_snat_v4_alloc_retries"
+	// MapNameSnat6AllocRetries represents the histogram of IPv6 NAT port allocation retries.
+	MapNameSnat6AllocRetries = "cilium_snat_v6_alloc_retries"
+
+	// SnatCollisionRetries represents the maximum number of port allocation retries.
+	SnatCollisionRetries = 128
 )
 
 // Map represents a NAT map.
@@ -61,14 +69,24 @@ type NatMapRecord struct {
 	Value NatEntry
 }
 
-// NatMap interface represents a NAT map, and can be reused to implement mock
-// maps for unit tests.
-type NatMap interface {
+type commonMap interface {
 	Open() error
 	Close() error
 	Path() (string, error)
+}
+
+// NatMap interface represents a NAT map, and can be reused to implement mock
+// maps for unit tests.
+type NatMap interface {
+	commonMap
 	DumpEntries() (string, error)
 	DumpWithCallback(bpf.DumpCallback) error
+}
+
+type RetriesMap interface {
+	commonMap
+	DumpPerCPUWithCallback(bpf.DumpPerCPUCallback) error
+	ClearAll() error
 }
 
 // NewMap instantiates a Map.
@@ -97,6 +115,39 @@ func NewMap(name string, family IPFamily, entries int) *Map {
 			WithPressureMetric(),
 		family: family,
 	}
+}
+
+type RetriesKey struct {
+	Key uint32
+}
+
+func (k *RetriesKey) String() string  { return fmt.Sprintf("%d", k.Key) }
+func (k *RetriesKey) New() bpf.MapKey { return &RetriesKey{} }
+
+type RetriesValue struct {
+	Value uint32
+}
+
+type RetriesValues []RetriesValue
+
+func (k *RetriesValue) String() string    { return fmt.Sprintf("%d", k.Value) }
+func (k *RetriesValue) New() bpf.MapValue { return &RetriesValue{} }
+func (k *RetriesValue) NewSlice() any     { return &RetriesValues{} }
+
+type RetriesMapRecord struct {
+	Key   *RetriesKey
+	Value *RetriesValue
+}
+
+func NewRetriesMap(name string) *bpf.Map {
+	return bpf.NewMap(
+		name,
+		ebpf.PerCPUArray,
+		&RetriesKey{},
+		&RetriesValue{},
+		SnatCollisionRetries+1,
+		0,
+	)
 }
 
 // DumpBatch4 uses batch iteration to walk the map and applies fn for each batch of entries.
@@ -370,4 +421,18 @@ func maxEntries() int {
 		return option.Config.NATMapEntriesGlobal
 	}
 	return option.LimitTableMax
+}
+
+// RetriesMaps returns the maps that contain the histograms of the number of retries.
+func RetriesMaps(ipv4, ipv6, nodeport bool) (ipv4RetriesMap, ipv6RetriesMap RetriesMap) {
+	if !nodeport {
+		return
+	}
+	if ipv4 {
+		ipv4RetriesMap = NewRetriesMap(MapNameSnat4AllocRetries)
+	}
+	if ipv6 {
+		ipv6RetriesMap = NewRetriesMap(MapNameSnat6AllocRetries)
+	}
+	return
 }

bpf/lib/nat.h
@@ -35,8 +35,7 @@ struct nat_entry {
 	__u64 pad2;		/* Future use. */
 };
 
-#define SNAT_COLLISION_RETRIES		128
-#define SNAT_SIGNAL_THRES		64
+#define SNAT_SIGNAL_THRES		(SNAT_COLLISION_RETRIES / 2)
 
 #define snat_v4_needs_masquerade_hook(ctx, target) 0
 
@@ -120,6 +119,14 @@ struct {
 	__uint(max_entries, SNAT_MAPPING_IPV4_SIZE);
 } SNAT_MAPPING_IPV4 __section_maps_btf;
 
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__type(key, __u32);
+	__type(value, __u32);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, SNAT_COLLISION_RETRIES + 1);
+} SNAT_ALLOC_RETRIES_IPV4 __section_maps_btf;
+
 #ifdef ENABLE_CLUSTER_AWARE_ADDRESSING
 struct per_cluster_snat_mapping_ipv4_inner_map {
 	__uint(type, BPF_MAP_TYPE_LRU_HASH);
@@ -200,7 +207,9 @@ static __always_inline int snat_v4_new_mapping(struct __ctx_buff *ctx, void *map
 {
 	struct ipv4_ct_tuple rtuple = {};
 	struct ipv4_nat_entry rstate;
-	int ret, retries;
+	__u32 *retries_hist;
+	__u32 retries;
+	int ret;
 	__u16 port;
 
 	memset(&rstate, 0, sizeof(rstate));
@@ -238,11 +247,19 @@ static __always_inline int snat_v4_new_mapping(struct __ctx_buff *ctx, void *map
 					       (__u16)get_prandom_u32());
 	}
 
+	retries_hist = map_lookup_elem(&SNAT_ALLOC_RETRIES_IPV4, &(__u32){retries});
+	if (retries_hist)
+		++*retries_hist;
+
 	/* Loop completed without finding a free port: */
 	ret = DROP_NAT_NO_MAPPING;
 	goto out;
 
 create_nat_entry:
+	retries_hist = map_lookup_elem(&SNAT_ALLOC_RETRIES_IPV4, &(__u32){retries});
+	if (retries_hist)
+		++*retries_hist;
+
 	ostate->to_sport = rtuple.dport;
 	ostate->common.created = rstate.common.created;
 
@@ -1044,7 +1061,7 @@ rewrite:
 				       tuple.daddr, state->to_daddr, IPV4_DADDR_OFF,
 				       tuple.dport, to_dport, port_off);
 }
-#else
+#else /* defined(ENABLE_IPV4) && defined(ENABLE_NODEPORT) */
 static __always_inline __maybe_unused
 int snat_v4_nat(struct __ctx_buff *ctx __maybe_unused,
 		const struct ipv4_nat_target *target __maybe_unused)
@@ -1058,7 +1075,7 @@ int snat_v4_rev_nat(struct __ctx_buff *ctx __maybe_unused,
 {
 	return CTX_ACT_OK;
 }
-#endif
+#endif /* defined(ENABLE_IPV4) && defined(ENABLE_NODEPORT) */
 
 struct ipv6_nat_entry {
 	struct nat_entry common;
@@ -1091,6 +1108,13 @@ struct {
 	__uint(pinning, LIBBPF_PIN_BY_NAME);
 	__uint(max_entries, SNAT_MAPPING_IPV6_SIZE);
 } SNAT_MAPPING_IPV6 __section_maps_btf;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__type(key, __u32);
+	__type(value, __u32);
+	__uint(max_entries, SNAT_COLLISION_RETRIES + 1);
+} SNAT_ALLOC_RETRIES_IPV6 __section_maps_btf;
 
 #ifdef ENABLE_CLUSTER_AWARE_ADDRESSING
 struct {
@@ -1156,7 +1180,9 @@ static __always_inline int snat_v6_new_mapping(struct __ctx_buff *ctx,
 {
 	struct ipv6_ct_tuple rtuple = {};
 	struct ipv6_nat_entry rstate;
-	int ret, retries;
+	__u32 *retries_hist;
+	__u32 retries;
+	int ret;
 	__u16 port;
 
 	memset(&rstate, 0, sizeof(rstate));
@@ -1192,10 +1218,18 @@ static __always_inline int snat_v6_new_mapping(struct __ctx_buff *ctx,
 					       (__u16)get_prandom_u32());
 	}
 
+	retries_hist = map_lookup_elem(&SNAT_ALLOC_RETRIES_IPV6, &(__u32){retries});
+	if (retries_hist)
+		++*retries_hist;
+
 	ret = DROP_NAT_NO_MAPPING;
 	goto out;
 
 create_nat_entry:
+	retries_hist = map_lookup_elem(&SNAT_ALLOC_RETRIES_IPV6, &(__u32){retries});
+	if (retries_hist)
+		++*retries_hist;
+
 	ostate->to_sport = rtuple.dport;
 	ostate->common.created = rstate.common.created;
 
@@ -1783,7 +1817,7 @@ rewrite:
 				       &tuple.daddr, &state->to_daddr, IPV6_DADDR_OFF,
 				       tuple.dport, to_dport, port_off);
 }
-#else
+#else /* defined(ENABLE_IPV6) && defined(ENABLE_NODEPORT) */
 static __always_inline __maybe_unused
 int snat_v6_nat(struct __ctx_buff *ctx __maybe_unused,
 		const struct ipv6_nat_target *target __maybe_unused)
@@ -1797,7 +1831,7 @@ int snat_v6_rev_nat(struct __ctx_buff *ctx __maybe_unused,
 {
 	return CTX_ACT_OK;
 }
-#endif
+#endif /* defined(ENABLE_IPV6) && defined(ENABLE_NODEPORT) */
 
 #if defined(ENABLE_IPV6) && defined(ENABLE_NODEPORT)
 static __always_inline int

bpf/node_config.h
@@ -139,6 +139,10 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 #define CAPTURE6_SIZE 16384
 #endif /* ENABLE_IPV6 */
 
+#ifdef ENABLE_NODEPORT
+#define SNAT_COLLISION_RETRIES 128
+#endif
+
 #define EGRESS_POLICY_MAP test_cilium_egress_gw_policy_v4
 #define SRV6_VRF_MAP4 test_cilium_srv6_vrf_v4
 #define SRV6_VRF_MAP6 test_cilium_srv6_vrf_v6

cilium-dbg/cmd/bpf_nat_retries.go
@@ -1,0 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// BPFNatRetriesCmd represents the bpf_nat_retries command
+var BPFNatRetriesCmd = &cobra.Command{
+	Use:   "retries",
+	Short: "Histogram of retries",
+}
+
+func init() {
+	BPFNatCmd.AddCommand(BPFNatRetriesCmd)
+}

cilium-dbg/cmd/bpf_nat_retries_flush.go
@@ -1,0 +1,54 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cilium/cilium/pkg/common"
+	"github.com/cilium/cilium/pkg/maps/nat"
+)
+
+// bpfNatRetriesFlushCmd represents the bpf_nat_flush command
+var bpfNatRetriesFlushCmd = &cobra.Command{
+	Use:   "flush",
+	Short: "Reset the NAT retries histogram",
+	Run: func(cmd *cobra.Command, args []string) {
+		common.RequireRootPrivilege("cilium bpf nat retries flush")
+		flushRetries()
+	},
+}
+
+func init() {
+	BPFNatRetriesCmd.AddCommand(bpfNatRetriesFlushCmd)
+}
+
+func flushRetries() {
+	ipv4, ipv6 := nat.RetriesMaps(true, getIpv6EnableStatus(), true)
+
+	for _, m := range []nat.RetriesMap{ipv4, ipv6} {
+		if m == nil {
+			continue
+		}
+		path, err := m.Path()
+		if err == nil {
+			err = m.Open()
+		}
+		if err != nil {
+			if os.IsNotExist(err) {
+				fmt.Fprintf(os.Stderr, "Unable to open %s: %s. Skipping.\n", path, err)
+				continue
+			}
+			Fatalf("Unable to open %s: %s", path, err)
+		}
+		defer m.Close()
+
+		if err = m.ClearAll(); err != nil {
+			Fatalf("Error while clearing BPF map entries: %s", err)
+		}
+	}
+}

cilium-dbg/cmd/bpf_nat_retries_list.go
@@ -1,0 +1,83 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/command"
+	"github.com/cilium/cilium/pkg/common"
+	"github.com/cilium/cilium/pkg/maps/nat"
+)
+
+// bpfNatRetriesListCmd represents the bpf_nat_retries_list command
+var bpfNatRetriesListCmd = &cobra.Command{
+	Use:     "list",
+	Aliases: []string{"ls"},
+	Short:   "Show the NAT retries histogram",
+	Run: func(cmd *cobra.Command, args []string) {
+		common.RequireRootPrivilege("cilium bpf nat retries list")
+		if len(args) != 0 {
+			cmd.PrintErr("Invalid argument")
+			return
+		}
+
+		ipv4, ipv6 := nat.RetriesMaps(true, getIpv6EnableStatus(), true)
+		if ipv4 != nil {
+			dumpRetries(ipv4)
+		}
+		if ipv6 != nil {
+			dumpRetries(ipv6)
+		}
+	},
+}
+
+func init() {
+	BPFNatRetriesCmd.AddCommand(bpfNatRetriesListCmd)
+	command.AddOutputOption(bpfNatRetriesListCmd)
+}
+
+func dumpRetries(m nat.RetriesMap) {
+	path, err := m.Path()
+	if err == nil {
+		err = m.Open()
+	}
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Fprintf(os.Stderr, "Unable to open %s: %s. Skipping.\n", path, err)
+			return
+		}
+		Fatalf("Unable to open %s: %s", path, err)
+	}
+	defer m.Close()
+
+	var entries []nat.RetriesMapRecord
+	var histogram = make([]uint32, nat.SnatCollisionRetries+1)
+	callback := func(key bpf.MapKey, values any) {
+		var sum uint32
+		for _, v := range *values.(*nat.RetriesValues) {
+			sum += v.Value
+		}
+		record := nat.RetriesMapRecord{Key: key.(*nat.RetriesKey), Value: &nat.RetriesValue{Value: sum}}
+		entries = append(entries, record)
+		histogram[key.(*nat.RetriesKey).Key] = sum
+	}
+	if err = m.DumpPerCPUWithCallback(callback); err != nil {
+		Fatalf("Error while collecting BPF map entries: %s", err)
+	}
+
+	if command.OutputOption() {
+		if err := command.PrintOutput(entries); err != nil {
+			os.Exit(1)
+		}
+	} else {
+		for k, v := range histogram {
+			fmt.Fprintf(os.Stdout, "%d:\t%d\n", k, v)
+		}
+	}
+}

pkg/bpf/map_linux.go
@@ -54,6 +54,15 @@ type MapValue interface {
 	New() MapValue
 }
 
+// MapPerCPUValue is the same as MapValue, but for per-CPU maps. Implement to be
+// able to fetch map values from all CPUs.
+type MapPerCPUValue interface {
+	MapValue
+
+	// NewSlice must return a pointer to a slice of structs that implement MapValue.
+	NewSlice() any
+}
+
 type cacheEntry struct {
 	Key   MapKey
 	Value MapValue
@@ -161,6 +170,11 @@ func (m *Map) Flags() uint32 {
 		return m.spec.Flags
 	}
 	return 0
+}
+
+func (m *Map) hasPerCPUValue() bool {
+	mt := m.Type()
+	return mt == ebpf.PerCPUHash || mt == ebpf.PerCPUArray || mt == ebpf.LRUCPUHash || mt == ebpf.PerCPUCGroupStorage
 }
 
 func (m *Map) updateMetrics() {
@@ -604,8 +618,7 @@ type DumpCallback func(key MapKey, value MapValue)
 // each map entry. With the current implementation, it is safe for callbacks to
 // retain the values received, as they are guaranteed to be new instances.
 //
-// TODO(tb): This package currently doesn't support dumping per-cpu maps, as
-// ReadValueSize is always set to the size of a single value.
+// To dump per-cpu maps, use DumpPerCPUWithCallback.
 func (m *Map) DumpWithCallback(cb DumpCallback) error {
 	if cb == nil {
 		return errors.New("empty callback")
@@ -628,6 +641,50 @@ func (m *Map) DumpWithCallback(cb DumpCallback) error {
 
 		mk = m.key.New()
 		mv = m.value.New()
+	}
+
+	return i.Err()
+}
+
+// DumpPerCPUCallback is called by DumpPerCPUWithCallback with the map key and
+// the slice of all values from all CPUs.
+type DumpPerCPUCallback func(key MapKey, values any)
+
+// DumpPerCPUWithCallback iterates over the Map and calls the given
+// DumpPerCPUCallback for each map entry, passing the slice with all values
+// from all CPUs. With the current implementation, it is safe for callbacks
+// to retain the values received, as they are guaranteed to be new instances.
+func (m *Map) DumpPerCPUWithCallback(cb DumpPerCPUCallback) error {
+	if cb == nil {
+		return errors.New("empty callback")
+	}
+
+	if !m.hasPerCPUValue() {
+		return fmt.Errorf("map %s is not a per-CPU map", m.name)
+	}
+
+	v, ok := m.value.(MapPerCPUValue)
+	if !ok {
+		return fmt.Errorf("map %s value type does not implement MapPerCPUValue", m.name)
+	}
+
+	if err := m.Open(); err != nil {
+		return err
+	}
+
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	// Don't need deep copies here, only fresh pointers.
+	mk := m.key.New()
+	mv := v.NewSlice()
+
+	i := m.m.Iterate()
+	for i.Next(mk, mv) {
+		cb(mk, mv)
+
+		mk = m.key.New()
+		mv = v.NewSlice()
 	}
 
 	return i.Err()
@@ -1276,6 +1333,44 @@ func (m *Map) DeleteAll() error {
 	}
 
 	return err
+}
+
+func (m *Map) ClearAll() error {
+	if m.eventsBufferEnabled || m.withValueCache {
+		return fmt.Errorf("clear map: events buffer and value cache are not supported")
+	}
+
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	defer m.updatePressureMetric()
+
+	if err := m.open(); err != nil {
+		return err
+	}
+
+	mk := m.key.New()
+	var mv any
+	if m.hasPerCPUValue() {
+		mv = m.value.(MapPerCPUValue).NewSlice()
+	} else {
+		mv = m.value.New()
+	}
+	empty := reflect.Indirect(reflect.ValueOf(mv)).Interface()
+
+	i := m.m.Iterate()
+	for i.Next(mk, mv) {
+		err := m.m.Update(mk, empty, ebpf.UpdateAny)
+
+		if metrics.BPFMapOps.IsEnabled() {
+			metrics.BPFMapOps.WithLabelValues(m.commonName(), metricOpUpdate, metrics.Error2Outcome(err)).Inc()
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return i.Err()
 }
 
 // GetModel returns a BPF map in the representation served via the API

pkg/bpf/map_linux_test.go
@@ -40,6 +40,7 @@ type TestLPMKey struct {
 type TestValue struct {
 	Value uint32
 }
+type TestValues []TestValue
 
 func (k *TestKey) String() string { return fmt.Sprintf("key=%d", k.Key) }
 func (k *TestKey) New() MapKey    { return &TestKey{} }
@@ -49,6 +50,7 @@ func (k *TestLPMKey) New() MapKey    { return &TestLPMKey{} }
 
 func (v *TestValue) String() string { return fmt.Sprintf("value=%d", v.Value) }
 func (v *TestValue) New() MapValue  { return &TestValue{} }
+func (k *TestValue) NewSlice() any  { return &TestValues{} }
 
 func setup(tb testing.TB) *Map {
 	testutils.PrivilegedTest(tb)
@@ -65,6 +67,32 @@ func setup(tb testing.TB) *Map {
 		maxEntries,
 		BPF_F_NO_PREALLOC,
 	).WithCache()
+
+	err = testMap.OpenOrCreate()
+	require.NoError(tb, err, "Failed to create map")
+
+	tb.Cleanup(func() {
+		require.NoError(tb, testMap.Close())
+	})
+
+	return testMap
+}
+
+func setupPerCPU(tb testing.TB) *Map {
+	testutils.PrivilegedTest(tb)
+
+	CheckOrMountFS("")
+
+	err := rlimit.RemoveMemlock()
+	require.NoError(tb, err)
+
+	testMap := NewMap("cilium_test_percpu",
+		ebpf.PerCPUArray,
+		&TestKey{},
+		&TestValue{},
+		3,
+		0,
+	)
 
 	err = testMap.OpenOrCreate()
 	require.NoError(tb, err, "Failed to create map")
@@ -506,6 +534,55 @@ func TestDump(t *testing.T) {
 		"key=105": {"value=205"},
 		"key=106": {"value=206"},
 	}, dump5)
+}
+
+func TestDumpPerCPU(t *testing.T) {
+	testMap := setupPerCPU(t)
+
+	key1 := &TestKey{Key: 0}
+	value1 := &TestValue{Value: 205}
+	key2 := &TestKey{Key: 2}
+	value2 := &TestValue{Value: 206}
+
+	func() {
+		testMap.lock.Lock()
+		defer testMap.lock.Unlock()
+		err := testMap.m.Update(key1, []any{value1}, ebpf.UpdateAny)
+		require.NoError(t, err)
+		err = testMap.m.Update(key2, []any{value1}, ebpf.UpdateAny)
+		require.NoError(t, err)
+		err = testMap.m.Update(key2, []any{value2}, ebpf.UpdateAny)
+		require.NoError(t, err)
+	}()
+
+	dump := map[string][]uint32{}
+	customCb := func(key MapKey, values any) {
+		var value uint32
+		for _, v := range *values.(*TestValues) {
+			if value == 0 && v.Value != 0 {
+				value = v.Value
+			} else if value != 0 {
+				require.Equal(t, uint32(0), v.Value)
+			}
+		}
+		dump[key.String()] = append(dump[key.String()], value)
+	}
+	testMap.DumpPerCPUWithCallback(customCb)
+	require.Equal(t, map[string][]uint32{
+		"key=0": {205},
+		"key=1": {0},
+		"key=2": {206},
+	}, dump)
+
+	require.NoError(t, testMap.ClearAll())
+
+	dump = map[string][]uint32{}
+	testMap.DumpPerCPUWithCallback(customCb)
+	require.Equal(t, map[string][]uint32{
+		"key=0": {0},
+		"key=1": {0},
+		"key=2": {0},
+	}, dump)
 }
 
 // TestDumpReliablyWithCallbackOverlapping attempts to test that DumpReliablyWithCallback

commit ff19b1256a0887b3f5212e6691763ee8cd20f43d

daemon: Create NAT retries maps early on startup
We have some sort of race popping up in CI from time to time around the
loading of some BPF maps:

    loading eBPF collection into the kernel: map
      cilium_snat_v4_alloc_retries: pin map to
      /sys/fs/bpf/tc/globals/cilium_snat_v4_alloc_retries: file exists

One approach to avoid this is to ensure such maps are always created
early on agent startup instead of being created during endpoint creation
(where concurrent program loadings can run into this race).

This commit therefore adds the two maps concerned by this issue to the
initMaps method.

Fixes: 60f074d63f9 ("bpf: Metrics for SNAT allocation retries")
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

daemon/cmd/datapath.go
@@ -226,6 +226,10 @@ func (d *Daemon) initMaps() error {
 			option.Config.EnableIPv6); err != nil {
 			return fmt.Errorf("initializing neighbors map: %w", err)
 		}
+		if err := nat.CreateRetriesMaps(option.Config.EnableIPv4,
+			option.Config.EnableIPv6); err != nil {
+			return fmt.Errorf("initializing NAT retries map: %w", err)
+		}
 	}
 
 	if option.Config.EnableIPv4FragmentsTracking {

pkg/maps/nat/nat.go
@@ -436,3 +436,19 @@ func RetriesMaps(ipv4, ipv6, nodeport bool) (ipv4RetriesMap, ipv6RetriesMap Retr
 	}
 	return
 }
+
+func CreateRetriesMaps(ipv4, ipv6 bool) error {
+	if ipv4 {
+		ipv4Map := NewRetriesMap(MapNameSnat4AllocRetries)
+		if err := ipv4Map.OpenOrCreate(); err != nil {
+			return err
+		}
+	}
+	if ipv6 {
+		ipv6Map := NewRetriesMap(MapNameSnat6AllocRetries)
+		if err := ipv6Map.OpenOrCreate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}