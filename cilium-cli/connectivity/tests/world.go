d45445d47a51031902937d81a9fe21947b87dc6f
Maxim Mikityanskiy <maxim@isovalent.com>
2025-02-04 23:03:12 +0200

    cilium-cli: Explicitly test IPv4/IPv6 in PodToWorld and PodToWorld2 tests
    Test both IPv4 and IPv6, instead of one arbitrary address family
    selected automatically.
    
    Fixes: https://github.com/cilium/cilium/issues/37139
    Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

--- cilium-cli/connectivity/tests/world.go
+++ cilium-cli/connectivity/tests/world.go
@@ -62,25 +62,27 @@ func (s *podToWorld) Run(ctx context.Context, t *check.Test) {
 	ct := t.Context()
 
 	for _, client := range ct.ClientPods() {
-		// With http, over port 80.
-		httpOpts := s.rc.CurlOptions(http, features.IPFamilyAny, client, ct.Params())
-		t.NewAction(s, fmt.Sprintf("http-to-%s-%d", extTarget, i), &client, http, features.IPFamilyAny).Run(func(a *check.Action) {
-			a.ExecInPod(ctx, a.CurlCommand(http, httpOpts...))
-			a.ValidateFlows(ctx, client, a.GetEgressRequirements(fp))
-		})
+		t.ForEachIPFamily(func(ipFam features.IPFamily) {
+			// With http, over port 80.
+			httpOpts := s.rc.CurlOptions(http, ipFam, client, ct.Params())
+			t.NewAction(s, fmt.Sprintf("http-to-%s-%s-%d", extTarget, ipFam, i), &client, http, ipFam).Run(func(a *check.Action) {
+				a.ExecInPod(ctx, a.CurlCommand(http, httpOpts...))
+				a.ValidateFlows(ctx, client, a.GetEgressRequirements(fp))
+			})
 
-		// With https, over port 443.
-		httpsOpts := s.rc.CurlOptions(https, features.IPFamilyAny, client, ct.Params())
-		t.NewAction(s, fmt.Sprintf("https-to-%s-%d", extTarget, i), &client, https, features.IPFamilyAny).Run(func(a *check.Action) {
-			a.ExecInPod(ctx, a.CurlCommand(https, httpsOpts...))
-			a.ValidateFlows(ctx, client, a.GetEgressRequirements(fp))
-		})
+			// With https, over port 443.
+			httpsOpts := s.rc.CurlOptions(https, ipFam, client, ct.Params())
+			t.NewAction(s, fmt.Sprintf("https-to-%s-%s-%d", extTarget, ipFam, i), &client, https, ipFam).Run(func(a *check.Action) {
+				a.ExecInPod(ctx, a.CurlCommand(https, httpsOpts...))
+				a.ValidateFlows(ctx, client, a.GetEgressRequirements(fp))
+			})
 
-		// With https, over port 443, index.html.
-		httpsindexOpts := s.rc.CurlOptions(httpsindex, features.IPFamilyAny, client, ct.Params())
-		t.NewAction(s, fmt.Sprintf("https-to-%s-index-%d", extTarget, i), &client, httpsindex, features.IPFamilyAny).Run(func(a *check.Action) {
-			a.ExecInPod(ctx, a.CurlCommand(httpsindex, httpsindexOpts...))
-			a.ValidateFlows(ctx, client, a.GetEgressRequirements(fp))
+			// With https, over port 443, index.html.
+			httpsindexOpts := s.rc.CurlOptions(httpsindex, ipFam, client, ct.Params())
+			t.NewAction(s, fmt.Sprintf("https-to-%s-index-%s-%d", extTarget, ipFam, i), &client, httpsindex, ipFam).Run(func(a *check.Action) {
+				a.ExecInPod(ctx, a.CurlCommand(httpsindex, httpsindexOpts...))
+				a.ValidateFlows(ctx, client, a.GetEgressRequirements(fp))
+			})
 		})
 
 		i++
@@ -117,11 +119,13 @@ func (s *podToWorld2) Run(ctx context.Context, t *check.Test) {
 	ct := t.Context()
 
 	for _, client := range ct.ClientPods() {
-		// With https, over port 443.
-		t.NewAction(s, fmt.Sprintf("https-%s-%d", extTarget, i), &client, https, features.IPFamilyAny).Run(func(a *check.Action) {
-			a.ExecInPod(ctx, a.CurlCommand(https))
-			a.ValidateFlows(ctx, client, a.GetEgressRequirements(fp))
-			a.ValidateMetrics(ctx, client, a.GetEgressMetricsRequirements())
+		t.ForEachIPFamily(func(ipFam features.IPFamily) {
+			// With https, over port 443.
+			t.NewAction(s, fmt.Sprintf("https-%s-%s-%d", extTarget, ipFam, i), &client, https, ipFam).Run(func(a *check.Action) {
+				a.ExecInPod(ctx, a.CurlCommand(https))
+				a.ValidateFlows(ctx, client, a.GetEgressRequirements(fp))
+				a.ValidateMetrics(ctx, client, a.GetEgressMetricsRequirements())
+			})
 		})
 
 		i++
