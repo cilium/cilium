77442decf804bc23edd911b665d2eaa4043c1423
Martynas Pumputis <martynas@isovalent.com>
2025-02-04 16:20:12 +0100

    clustermesh: Make id/name validation strict
    0/"default" were valid only for CEW. Since the entire feature was
    removed in the previous commit, we can validate for the != 0/"default".
    
    Signed-off-by: Martynas Pumputis <martynas@isovalent.com>

--- pkg/clustermesh/types/types.go
+++ pkg/clustermesh/types/types.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"regexp"
 
+	"github.com/cilium/hive/cell"
+
 	"github.com/cilium/cilium/pkg/defaults"
 )
 
@@ -75,6 +77,20 @@ func ValidateClusterName(name string) error {
 	}
 
 	return nil
+}
+
+func RegisterClusterInfoValidator(lc cell.Lifecycle, cinfo ClusterInfo) {
+	lc.Append(cell.Hook{
+		OnStart: func(cell.HookContext) error {
+			if err := cinfo.InitClusterIDMax(); err != nil {
+				return err
+			}
+			if err := cinfo.ValidateStrict(); err != nil {
+				return err
+			}
+			return nil
+		},
+	})
 }
 
 type CiliumClusterConfig struct {
