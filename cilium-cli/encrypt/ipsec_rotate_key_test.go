22aa4ae47c6d859fe5de5acd400381881eb95a71
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2025-02-10 18:43:31 +0100

    cli,ipsec: remove key-per-node param
    The commit removes `key-per-node` parameter as newer Cilium versions
    support only IPsec key per node.
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- cilium-cli/encrypt/ipsec_rotate_key_test.go
+++ cilium-cli/encrypt/ipsec_rotate_key_test.go
@@ -15,42 +15,18 @@ func Test_ipsecKeyFromString(t *testing.T) {
 		expected ipsecKey
 	}{
 		{
-			have: "3 rfc4106(gcm(aes)) 41049390e1e2b5d6543901daab6435f4042155fe 128",
-			expected: ipsecKey{
-				spi:       3,
-				spiSuffix: false,
-				algo:      "rfc4106(gcm(aes))",
-				key:       "41049390e1e2b5d6543901daab6435f4042155fe",
-				size:      128,
-			},
-		},
-		{
 			have: "3+ rfc4106(gcm(aes)) 41049390e1e2b5d6543901daab6435f4042155fe 128",
 			expected: ipsecKey{
-				spi:       3,
-				spiSuffix: true,
-				algo:      "rfc4106(gcm(aes))",
-				key:       "41049390e1e2b5d6543901daab6435f4042155fe",
-				size:      128,
-			},
-		},
-		{
-			have: "3 hmac(sha256) e6b4bab427cd37bb64b39cd66a8476a62963174b78bc544fb525f4c2f548342b cbc(aes) 0f12337d9ee75095ff21402dc98476f5f9107261073b70bb37747237d2691d3e",
-			expected: ipsecKey{
-				spi:        3,
-				spiSuffix:  false,
-				algo:       "hmac(sha256)",
-				key:        "e6b4bab427cd37bb64b39cd66a8476a62963174b78bc544fb525f4c2f548342b",
-				size:       0,
-				cipherMode: "cbc(aes)",
-				cipherKey:  "0f12337d9ee75095ff21402dc98476f5f9107261073b70bb37747237d2691d3e",
+				spi:  3,
+				algo: "rfc4106(gcm(aes))",
+				key:  "41049390e1e2b5d6543901daab6435f4042155fe",
+				size: 128,
 			},
 		},
 		{
 			have: "3+ hmac(sha256) e6b4bab427cd37bb64b39cd66a8476a62963174b78bc544fb525f4c2f548342b cbc(aes) 0f12337d9ee75095ff21402dc98476f5f9107261073b70bb37747237d2691d3e",
 			expected: ipsecKey{
 				spi:        3,
-				spiSuffix:  true,
 				algo:       "hmac(sha256)",
 				key:        "e6b4bab427cd37bb64b39cd66a8476a62963174b78bc544fb525f4c2f548342b",
 				size:       0,
@@ -76,40 +52,16 @@ func Test_ipsecKey_String(t *testing.T) {
 	}{
 		{
 			have: ipsecKey{
-				spi:       3,
-				spiSuffix: false,
-				algo:      "rfc4106(gcm(aes))",
-				key:       "41049390e1e2b5d6543901daab6435f4042155fe",
-				size:      128,
-			},
-			expected: "3 rfc4106(gcm(aes)) 41049390e1e2b5d6543901daab6435f4042155fe 128",
-		},
-		{
-			have: ipsecKey{
-				spi:       3,
-				spiSuffix: true,
-				algo:      "rfc4106(gcm(aes))",
-				key:       "41049390e1e2b5d6543901daab6435f4042155fe",
-				size:      128,
+				spi:  3,
+				algo: "rfc4106(gcm(aes))",
+				key:  "41049390e1e2b5d6543901daab6435f4042155fe",
+				size: 128,
 			},
 			expected: "3+ rfc4106(gcm(aes)) 41049390e1e2b5d6543901daab6435f4042155fe 128",
 		},
 		{
 			have: ipsecKey{
 				spi:        3,
-				spiSuffix:  false,
-				algo:       "hmac(sha256)",
-				key:        "e6b4bab427cd37bb64b39cd66a8476a62963174b78bc544fb525f4c2f548342b",
-				size:       0,
-				cipherMode: "cbc(aes)",
-				cipherKey:  "0f12337d9ee75095ff21402dc98476f5f9107261073b70bb37747237d2691d3e",
-			},
-			expected: "3 hmac(sha256) e6b4bab427cd37bb64b39cd66a8476a62963174b78bc544fb525f4c2f548342b cbc(aes) 0f12337d9ee75095ff21402dc98476f5f9107261073b70bb37747237d2691d3e",
-		},
-		{
-			have: ipsecKey{
-				spi:        3,
-				spiSuffix:  true,
 				algo:       "hmac(sha256)",
 				key:        "e6b4bab427cd37bb64b39cd66a8476a62963174b78bc544fb525f4c2f548342b",
 				size:       0,
