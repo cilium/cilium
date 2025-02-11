f44c320db76fc6a2b301bb21d6ec1c3daebd43e2
Joe Stringer <joe@cilium.io>
2025-02-11 12:53:59 -0800

    .github: Log codeowners for unit test failures
    [ upstream commit edae668e8bc786ce512026e5f0ab902238e777ca ]
    
    [ Backporter's notes:
      - Commit 5a7ea3a6ffee ("ci: run privileged tests in parallel except for
        IPSec") removed go-junit-report in favor of tparse, do the same here
        as this will be supplemented with a Cilium fork of go-junit-report
        in an upcoming backport commit.
      - Integration tests on this branch use the build.sh script, so add the
        parameters for codeowner logging there rather than directly in the
        workflow.
    ]
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- .github/actions/unit-tests/build.sh
+++ .github/actions/unit-tests/build.sh
@@ -15,6 +15,6 @@ make clean
 # Run with default verbosity here since this builds all Go code by running
 # 'go vet' and all integration tests. At least one line of output is generated
 # after each Go package is built and tested.
-make integration-tests
+make integration-tests LOG_CODEOWNERS=1
 
 exit 0
