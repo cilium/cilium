9c1bb5f91afbeb03b9e0956b237e4c060b51a35b
Jarno Rajahalme <jarno@isovalent.com>
2025-02-12 17:58:54 +0100

    daemon: Fix policy map text in daemon status
    Policy maps are per endpoint, not global. Fix the label text for policy
    map (max) size to "Endpoint policy" from "Global policy".
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- daemon/cmd/status.go
+++ daemon/cmd/status.go
@@ -456,7 +456,7 @@ func (d *Daemon) getBPFMapStatus() *models.BPFMapStatus {
 				Size: int64(option.Config.NeighMapEntriesGlobal),
 			},
 			{
-				Name: "Global policy",
+				Name: "Endpoint policy",
 				Size: int64(option.Config.PolicyMapEntries),
 			},
 			{
