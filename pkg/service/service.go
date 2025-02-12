7a5d8b2b0506b364ef7805b0b29dfbf60e81f023
Tom Hadlaw <tom.hadlaw@isovalent.com>
2025-02-12 15:12:02 -0800

    service: make service testable.
    This abstracts concern related to iterating netns and
    uses the new netns.All() function to replace the current
    default functionality parity.
    
    This will allow for providing an iterator that doesn't
    rely on pinned netns (i.e. just ones we create at runtime
    for the test process) which will make it easier to test.
    
    As well, switch global use of *option.Config to a field
    now provided via hive. This will avoiding global options
    mutation for tests.
    
    Finally, we now return errors from the terminate function,
    although we do not handle it as errors are already logged.
    The purpose is to return errors while testing in following
    commits.
    
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -6,6 +6,7 @@ package service
 import (
 	"errors"
 	"fmt"
+	"iter"
 	"net"
 	"net/netip"
 	"sync/atomic"
@@ -28,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/metrics"
 	monitorAgent "github.com/cilium/cilium/pkg/monitor/agent"
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
+	"github.com/cilium/cilium/pkg/netns"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/node/addressing"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
@@ -299,13 +301,17 @@ type Service struct {
 	l7lbSvcs map[lb.ServiceName]*L7LBInfo
 
 	backendConnectionHandler sockets.SocketDestroyer
+	nsIterator               func() (iter.Seq2[string, *netns.NetNS], <-chan error)
 
 	backendDiscovery       datapathTypes.NodeNeighbors
 	k8sControlplaneEnabled bool
+
+	config *option.DaemonConfig
 }
 
 // newService creates a new instance of the service handler.
-func newService(monitorAgent monitorAgent.Agent, lbmap datapathTypes.LBMap, backendDiscoveryHandler datapathTypes.NodeNeighbors, healthCheckers []HealthChecker, k8sControlplaneEnabled bool) *Service {
+func newService(monitorAgent monitorAgent.Agent, lbmap datapathTypes.LBMap, backendDiscoveryHandler datapathTypes.NodeNeighbors, healthCheckers []HealthChecker, k8sControlplaneEnabled bool,
+	config *option.DaemonConfig) *Service {
 	var localHealthServer healthServer
 	if option.Config.EnableHealthCheckNodePort {
 		localHealthServer = healthserver.New()
@@ -325,6 +331,8 @@ func newService(monitorAgent monitorAgent.Agent, lbmap datapathTypes.LBMap, back
 		backendDiscovery:         backendDiscoveryHandler,
 		healthCheckers:           healthCheckers,
 		k8sControlplaneEnabled:   k8sControlplaneEnabled,
+		config:                   config,
+		nsIterator:               netns.All,
 	}
 	svc.lastUpdatedTs.Store(time.Now())
 
@@ -1748,6 +1756,9 @@ func (s *Service) upsertServiceIntoLBMaps(svc *svcInfo, isExtLocal, isIntLocal b
 				Debug("Removing obsolete backend")
 		}
 		s.lbmap.DeleteBackendByID(id)
+		// Note: TerminateUDPConnectionsToBackend returns an error but we do not need to handle it here.
+		// errors are already logged inside the function and we do not want to return early in case of
+		// termination failures - these are always best effort.
 		s.TerminateUDPConnectionsToBackend(&be.L3n4Addr)
 	}
 
