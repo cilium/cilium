14bf3eb9aa333a8952265dec18f0c9d8e44eff54
Daniel Borkmann <daniel@iogearbox.net>
2025-02-17 21:20:56 +0000

    cilium: Remove standalone l4lb test suite
    Remove the test suite now that we deprecated and removed the docker
    only mode.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- test/bpf/xdp.c
+++ test/bpf/xdp.c
@@ -2,13 +2,13 @@
 
 #ifndef __section
 # define __section(NAME)                  \
-	   __attribute__((section(NAME), used))
+	 __attribute__((section(NAME), used))
 #endif
 
 __section("prog")
 int xdp_foo(struct xdp_md *ctx)
 {
-	    return XDP_PASS;
+	return XDP_PASS;
 }
 
 char __license[] __section("license") = "GPL";
