cilium/cilium

commit d877fe3675159de559c1145f0f7d30c0229ed740

cilium-cli: Autodetect owners for GitHub workflows
When reporting owners of test files as part of the --log-codeowners
parameter, autodetect the workflow file from the environment variable
GITHUB_WORKFLOW_REF which GitHub sets in the environment in order to
list the owners for the workflow itself while listing owners for a
failed test.

Signed-off-by: Joe Stringer <joe@cilium.io>

cilium-cli/connectivity/check/logging.go
@@ -11,8 +11,11 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
+	"strings"
 	"syscall"
 	"time"
+
+	"github.com/hmarr/codeowners"
 )
 
 const (
@@ -121,9 +124,32 @@ func (ct *ConnectivityTest) LogOwners(scenario ownedScenario) {
 			"\n\t\tname=%s path=%s err=%s", scenario.Name(), scenario.FilePath(), err)
 		return
 	}
+
+	var workflowOwners []codeowners.Owner
+	// Example: cilium/cilium/.github/workflows/conformance-kind-proxy-embedded.yaml@refs/pull/37593/merge
+	ghWorkflow := os.Getenv("GITHUB_WORKFLOW_REF")
+	ghWorkflow = strings.TrimPrefix(ghWorkflow, "cilium/cilium/")
+	ghWorkflow, _, _ = strings.Cut(ghWorkflow, "@")
+	if ghWorkflow != "" {
+		workflowRule, err := ct.CodeOwners.Match(ghWorkflow)
+		if err != nil || workflowRule == nil || workflowRule.Owners == nil {
+			ct.Warnf("Failed to find CODEOWNERS for workflow %s: %s", ghWorkflow, err)
+		}
+		workflowOwners = workflowRule.Owners
+	}
+
 	ct.Log("    ⛑️ The following owners are responsible for reliability of this test: ")
 	for _, o := range rule.Owners {
 		ct.Log("        - " + o.String() + " (" + scenario.Name() + ")")
+	}
+	for _, o := range workflowOwners {
+		owner := o.String()
+		switch owner {
+		case "@cilium/github-sec":
+			// Skip
+		default:
+			ct.Log("        - " + owner + " (" + ghWorkflow + ")")
+		}
 	}
 }
 

commit 27a039798490ebe86f3a42b19c66644829aef553

cilium-cli: Fix GITHUB_WORKFLOW_REF parsing
Always trim the name/repo from GITHUB_WORKFLOW_REF, regardless of which
target GitHub repo location it is. This should fix attribution when
using this tooling outside of cilium/cilium.

Fixes: d877fe367515 ("cilium-cli: Autodetect owners for GitHub workflows")
Signed-off-by: Joe Stringer <joe@cilium.io>

cilium-cli/connectivity/check/logging.go
@@ -128,8 +128,9 @@ func (ct *ConnectivityTest) LogOwners(scenario ownedScenario) {
 	var workflowOwners []codeowners.Owner
 	// Example: cilium/cilium/.github/workflows/conformance-kind-proxy-embedded.yaml@refs/pull/37593/merge
 	ghWorkflow := os.Getenv("GITHUB_WORKFLOW_REF")
-	ghWorkflow = strings.TrimPrefix(ghWorkflow, "cilium/cilium/")
 	ghWorkflow, _, _ = strings.Cut(ghWorkflow, "@")
+	segments := strings.Split(ghWorkflow, "/")
+	ghWorkflow = strings.Join(segments[2:], "/")
 	if ghWorkflow != "" {
 		workflowRule, err := ct.CodeOwners.Match(ghWorkflow)
 		if err != nil || workflowRule == nil || workflowRule.Owners == nil {