cilium/cilium

commit af9ac194453338217af6bde1b52dc81e0aad93fd

cilium-cli: Fix GITHUB_WORKFLOW_REF parsing
Always trim the name/repo from GITHUB_WORKFLOW_REF, regardless of which
target GitHub repo location it is. This should fix attribution when
using this tooling outside of cilium/cilium.

Fixes: d877fe367515 ("cilium-cli: Autodetect owners for GitHub workflows")
Signed-off-by: Joe Stringer <joe@cilium.io>

cilium-cli/connectivity/check/logging.go
@@ -128,8 +128,9 @@ func (ct *ConnectivityTest) LogOwners(scenario ownedScenario) {
 	var workflowOwners []codeowners.Owner
 	// Example: cilium/cilium/.github/workflows/conformance-kind-proxy-embedded.yaml@refs/pull/37593/merge
 	ghWorkflow := os.Getenv("GITHUB_WORKFLOW_REF")
-	ghWorkflow = strings.TrimPrefix(ghWorkflow, "cilium/cilium/")
 	ghWorkflow, _, _ = strings.Cut(ghWorkflow, "@")
+	segments := strings.Split(ghWorkflow, "/")
+	ghWorkflow = strings.Join(segments[2:], "/")
 	if ghWorkflow != "" {
 		workflowRule, err := ct.CodeOwners.Match(ghWorkflow)
 		if err != nil || workflowRule == nil || workflowRule.Owners == nil {

commit 5d03faba7a8cf5c44243c4c99ee89c8d1053e791

cilium-cli: re-fix GITHUB_WORKFLOW_REF parsing
af9ac194453338217af6bde1b52dc81e0aad93fd introduced an issue where the
cilium-cli connectivity check could panic when GITHUB_WORKFLOW_REF was
not set as expected.

This commit changes the GITHUB_WORKFLOW_REF parsing logic using a regexp
capture to extract the workflow name from GITHUB_WORKFLOW_REF and check
whether the match was successful.

Reported-by: Martynas Pumputis <martynas@isovalent.com>
Signed-off-by: Alexandre Perrin <alex@isovalent.com>

cilium-cli/connectivity/check/logging.go
@@ -10,8 +10,8 @@ import (
 	"io"
 	"os"
 	"os/signal"
+	"regexp"
 	"runtime"
-	"strings"
 	"syscall"
 	"time"
 
@@ -113,6 +113,8 @@ func (s defaultScenario) Name() string {
 	return "cli-test-framework"
 }
 
+var ghWorkflowRegexp = regexp.MustCompile("^(?:.+?)/(?:.+?)/(.+?)@.*$")
+
 func (ct *ConnectivityTest) LogOwners(scenario ownedScenario) {
 	if !ct.params.LogCodeOwners {
 		return
@@ -126,11 +128,15 @@ func (ct *ConnectivityTest) LogOwners(scenario ownedScenario) {
 	}
 
 	var workflowOwners []codeowners.Owner
+	var ghWorkflow string
 	// Example: cilium/cilium/.github/workflows/conformance-kind-proxy-embedded.yaml@refs/pull/37593/merge
-	ghWorkflow := os.Getenv("GITHUB_WORKFLOW_REF")
-	ghWorkflow, _, _ = strings.Cut(ghWorkflow, "@")
-	segments := strings.Split(ghWorkflow, "/")
-	ghWorkflow = strings.Join(segments[2:], "/")
+	ghWorkflowRef := os.Getenv("GITHUB_WORKFLOW_REF")
+	matches := ghWorkflowRegexp.FindStringSubmatch(ghWorkflowRef)
+	// here matches should either be nil (no match) or a slice with two values:
+	// the full match and the capture.
+	if len(matches) == 2 {
+		ghWorkflow = matches[1]
+	}
 	if ghWorkflow != "" {
 		workflowRule, err := ct.CodeOwners.Match(ghWorkflow)
 		if err != nil || workflowRule == nil || workflowRule.Owners == nil {