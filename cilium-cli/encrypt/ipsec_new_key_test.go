7bbcbb2e9695857a572bbb67fbf9cf3ca930a2ad
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2025-02-18 15:06:24 +0100

    cli,ipsec: create key command
    The commit adds IPsec key create sub-command in Cilium CLI.
    The intention is to use this command for encryption secret provisioning
    before the Cilium installation.
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

---
+++ cilium-cli/encrypt/ipsec_new_key_test.go
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package encrypt
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_createIPsecKey(t *testing.T) {
+	testCases := []struct {
+		algo        string
+		expectedKey ipsecKey
+	}{
+		{
+			algo: "gcm-aes",
+			expectedKey: ipsecKey{
+				spi:  1,
+				algo: "rfc4106(gcm(aes))",
+				key:  "1234567890123456789012345678901234567890",
+				size: 128,
+			},
+		},
+		{
+			algo: "hmac-sha256",
+			expectedKey: ipsecKey{
+				spi:        1,
+				algo:       "hmac(sha256)",
+				key:        "12345678901234567890123456789012",
+				cipherKey:  "12345678901234567890123456789012",
+				cipherMode: "cbc(aes)",
+			},
+		},
+		{
+			algo: "hmac-sha512",
+			expectedKey: ipsecKey{
+				spi:        1,
+				algo:       "hmac(sha512)",
+				key:        "1234567890123456789012345678901234567890123456789012345678901234",
+				cipherKey:  "12345678901234567890123456789012",
+				cipherMode: "cbc(aes)",
+			},
+		},
+	}
+
+	for _, tt := range testCases {
+		actualKey, err := createIPsecKey(tt.algo)
+
+		require.NoError(t, err)
+		require.Equal(t, tt.expectedKey.spi, actualKey.spi)
+		require.Equal(t, tt.expectedKey.algo, actualKey.algo)
+		require.Len(t, tt.expectedKey.key, len(actualKey.key))
+		require.Len(t, tt.expectedKey.cipherKey, len(actualKey.cipherKey))
+		require.Equal(t, tt.expectedKey.cipherMode, actualKey.cipherMode)
+		require.Equal(t, tt.expectedKey.size, actualKey.size)
+	}
+
+}
