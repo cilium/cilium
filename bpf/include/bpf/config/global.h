noironetworks/cilium-net

bpf: single source of truth for runtime-subbed node configs
Moved the following variables:

- THIS_INTERFACE_MAC: node_config.h -> config/global.h
- THIS_INTERFACE_IFINDEX: node_config.h -> config/global.h
- IPV4_MASQUERADE: lib/stubs.h -> lib/nat.h
- IPV6_MASQUERADE: lib/stubs.h -> lib/nat.h
- ROUTER_IP: temporary hack to prevent it from showing up in Go codegen, still rendered into node_config.h by the agent
- HOST_IP: only used in tests, moved out of node_config.h and renamed to NODE_IPV6 to avoid confusion

Signed-off-by: Timo Beckers <timo@isovalent.com>

bpf/include/bpf/config/global.h
@@ -9,3 +9,10 @@
 #pragma once
 
 #include <lib/static_data.h>
+
+DECLARE_CONFIG(__u32, interface_mac_1, "First 32 bytes of the MAC address of the interface the bpf program is attached to")
+DECLARE_CONFIG(__u16, interface_mac_2, "Second 16 bytes of the MAC address of the interface the bpf program is attached to")
+#define THIS_INTERFACE_MAC fetch_mac(interface_mac) /* Backwards compatibility */
+
+DECLARE_CONFIG(__u32, interface_ifindex, "ifindex of the interface the bpf program is attached to")
+#define THIS_INTERFACE_IFINDEX CONFIG(interface_ifindex) /* Backwards compatibility */

cilium/cilium

bpf/include/bpf/config/global.h
@@ -9,3 +9,10 @@
 #pragma once
 
 #include <lib/static_data.h>
+
+DECLARE_CONFIG(__u32, interface_mac_1, "First 32 bits of the MAC address of the interface the bpf program is attached to")
+DECLARE_CONFIG(__u16, interface_mac_2, "Latter 16 bits of the MAC address of the interface the bpf program is attached to")
+#define THIS_INTERFACE_MAC fetch_mac(interface_mac) /* Backwards compatibility */
+
+DECLARE_CONFIG(__u32, interface_ifindex, "ifindex of the interface the bpf program is attached to")
+#define THIS_INTERFACE_IFINDEX CONFIG(interface_ifindex) /* Backwards compatibility */