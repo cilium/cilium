cilium/cilium

commit 218649f9764207d083e7d8ece891157f23e2e1f5

egressgw: support egress gw handling at bpf_host
Currently packets from Pods selected by an egress gateway policy will
not be forwarded to a gateway if there's an L7 policy applied to those
packets. That's because we apply the egress gateway policy at bpf_lxc
right after the L7 policy redirection. Therefore the Egress Gateway
logic will be skipped if packets are redirected to the L7 proxy.

This commit adds the egress gateway handling code at to-netdev@bpf_host
so that packets from the L7 proxy can be properly redirected to an egress gw.

We will keep the egress gw code in from-container@bpf_lxc around
until v1.17 to avoid disruption of traffic to egress gateway.
It’s possible that the datapath becomes an incomplete state where bpf_lxc
has been upgraded while bpf_host hasn't during the upgrade. If that situation
were to occur, traffic destined for egress gateway would be broken for that
period of time. So we will keep the egress gateway code at both bpf_lxc
and bpf_host to avoid this scenario.

Fixes: cilium/cilium#19642

Signed-off-by: Yusuke Suzuki <yusuke.suzuki@isovalent.com>

bpf/bpf_host.c
@@ -1351,7 +1351,7 @@ int cil_to_netdev(struct __ctx_buff *ctx __maybe_unused)
 	__u32 src_sec_identity = 0;
 	int ret = CTX_ACT_OK;
 	__s8 ext_err = 0;
-#ifdef ENABLE_HOST_FIREWALL
+#if defined(ENABLE_HOST_FIREWALL) || defined(ENABLE_EGRESS_GATEWAY_COMMON)
 	__u16 proto = 0;
 #endif
 
@@ -1495,6 +1495,75 @@ skip_host_firewall:
 	ret = lb_handle_health(ctx);
 	if (ret != CTX_ACT_OK)
 		goto exit;
+#endif
+
+#ifdef ENABLE_EGRESS_GATEWAY_COMMON
+	{
+		void *data, *data_end;
+		struct iphdr *ip4;
+		struct ipv4_ct_tuple tuple = {};
+		struct ct_state ct_state = {};
+		enum ct_status ct_status;
+		bool has_l4_header = true;
+		int l4_off;
+		struct remote_endpoint_info *info;
+		struct endpoint_info *src_ep;
+		__u32 dst_sec_identity = UNKNOWN_ID;
+
+		if (!validate_ethertype(ctx, &proto) || proto != bpf_htons(ETH_P_IP))
+			goto skip_egress_gateway;
+
+		if (ctx_egw_done(ctx))
+			goto skip_egress_gateway;
+
+		if (!revalidate_data(ctx, &data, &data_end, &ip4)) {
+			ret = DROP_INVALID;
+			goto drop_err;
+		}
+
+		tuple.nexthdr = ip4->protocol;
+		tuple.daddr = ip4->daddr;
+		tuple.saddr = ip4->saddr;
+
+		l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
+		ret = ct_extract_ports4(ctx, ip4, l4_off, CT_EGRESS, &tuple,
+					&has_l4_header);
+		if (IS_ERR(ret)) {
+			if (ret == DROP_CT_UNKNOWN_PROTO)
+				goto skip_egress_gateway;
+
+			goto drop_err;
+		}
+
+		__ipv4_ct_tuple_reverse(&tuple);
+		ret = ct_lazy_lookup4(get_ct_map4(&tuple),
+				      &tuple, ctx, ipv4_is_fragment(ip4),
+				      l4_off, has_l4_header, CT_EGRESS,
+				      SCOPE_FORWARD, CT_ENTRY_ANY,
+				      &ct_state, &trace.monitor);
+		if (IS_ERR(ret))
+			goto drop_err;
+
+		ct_status = (enum ct_status)ret;
+
+		src_ep = __lookup_ip4_endpoint(ip4->saddr);
+		if (src_ep)
+			src_sec_identity = src_ep->sec_id;
+
+		info = lookup_ip4_remote_endpoint(ip4->daddr, 0);
+		if (info && info->sec_identity)
+			dst_sec_identity = info->sec_identity;
+
+		ret = egress_gw_handle_packet(ctx, &tuple, ct_status,
+					      src_sec_identity, dst_sec_identity,
+					      &trace);
+		if (IS_ERR(ret))
+			goto drop_err;
+
+		if (ret != CTX_ACT_OK)
+			return ret;
+	}
+skip_egress_gateway:
 #endif
 
 #ifdef ENABLE_NODEPORT

bpf/bpf_lxc.c
@@ -1133,39 +1133,15 @@ ct_recreate4:
 	}
 
 #ifdef ENABLE_EGRESS_GATEWAY_COMMON
-	{
-		struct endpoint_info *gateway_node_ep;
-		__be32 gateway_ip = 0;
-
-		/* If the packet is destined to an entity inside the cluster,
-		 * either EP or node, it should not be forwarded to an egress
-		 * gateway since only traffic leaving the cluster is supposed to
-		 * be masqueraded with an egress IP.
-		 */
-		if (identity_is_cluster(*dst_sec_identity))
-			goto skip_egress_gateway;
-
-		ret = egress_gw_request_needs_redirect_hook(tuple, ct_status, &gateway_ip);
-		if (IS_ERR(ret))
-			return ret;
-
-		if (ret == CTX_ACT_OK)
-			goto skip_egress_gateway;
-
-		/* If the gateway node is the local node, then just let the
-		 * packet go through, as it will be SNATed later on by
-		 * handle_nat_fwd().
-		 */
-		gateway_node_ep = __lookup_ip4_endpoint(gateway_ip);
-		if (gateway_node_ep && (gateway_node_ep->flags & ENDPOINT_F_HOST))
-			goto skip_egress_gateway;
-
-		/* Send the packet to egress gateway node through a tunnel. */
-		return __encap_and_redirect_lxc(ctx, gateway_ip, 0,
-						SECLABEL_IPV4,
-						*dst_sec_identity, &trace);
-	}
-skip_egress_gateway:
+	/* We handle traffic to Egress GW that is not redirected to L7 proxy here.
+	 * The traffic that has been redirected is processed by to-netdev@bpf_host.
+	 * This code is retained in v1.16 to prevent disruptions during the upgrade.
+	 * It will be removed in v1.17.
+	 */
+	ret = egress_gw_handle_packet(ctx, tuple, ct_status, SECLABEL_IPV4,
+				      *dst_sec_identity, &trace);
+	if (ret != CTX_ACT_OK)
+		return ret;
 #endif
 
 	/* L7 proxy result in VTEP redirection in bpf_host, but when L7 proxy disabled

bpf/bpf_overlay.c
@@ -429,6 +429,8 @@ not_esp:
 			if (unlikely(ret != CTX_ACT_OK))
 				return ret;
 
+			ctx_egw_done_set(ctx);
+
 			/* to-netdev@bpf_host handles SNAT, so no need to do it here. */
 			ret = egress_gw_fib_lookup_and_redirect(ctx, snat_addr,
 								daddr, ext_err);

bpf/lib/common.h
@@ -719,6 +719,7 @@ enum metric_dir {
 #define MARK_MAGIC_TO_PROXY		0x0200
 #define MARK_MAGIC_SNAT_DONE		0x0300
 #define MARK_MAGIC_OVERLAY		0x0400
+#define MARK_MAGIC_EGW_DONE		0x0500
 
 #define MARK_MAGIC_KEY_MASK		0xFF00
 

bpf/lib/egress_gateway.h
@@ -8,6 +8,7 @@
 #include "lib/identity.h"
 #include "lib/overloadable.h"
 
+#include "encap.h"
 #include "maps.h"
 
 #ifdef ENABLE_EGRESS_GATEWAY_COMMON
@@ -155,6 +156,7 @@ egress_gw_request_needs_redirect_hook(struct ipv4_ct_tuple *rtuple,
 				      enum ct_status ct_status,
 				      __be32 *gateway_ip)
 {
+#if defined(IS_BPF_LXC)
 	/* If the packet is a reply or is related, it means that outside
 	 * has initiated the connection, and so we should skip egress
 	 * gateway, since an egress policy is only matching connections
@@ -162,6 +164,20 @@ egress_gw_request_needs_redirect_hook(struct ipv4_ct_tuple *rtuple,
 	 */
 	if (ct_status == CT_REPLY || ct_status == CT_RELATED)
 		return CTX_ACT_OK;
+#else
+	/* We lookup CT in forward direction at to-netdev and expect to
+	 * get CT_ESTABLISHED or CT_REOPENED for outbound connection as
+	 * from_container should have already created a CT entry.
+	 * If we get CT_NEW here, it's an indication that it's a reply
+	 * for inbound connection or host-level outbound connection.
+	 * We don't expect to receive any other ct_status here.
+	 *
+	 * (We never get CT_REOPENED here for now since from_container
+	 * should have reopened the close connections)
+	 */
+	if (ct_status != CT_ESTABLISHED && ct_status != CT_REOPENED)
+		return CTX_ACT_OK;
+#endif
 
 	return egress_gw_request_needs_redirect(rtuple, gateway_ip);
 }
@@ -201,6 +217,46 @@ bool egress_gw_reply_needs_redirect_hook(struct iphdr *ip4, __u32 *tunnel_endpoi
 	}
 
 	return false;
+}
+
+static __always_inline
+int egress_gw_handle_packet(struct __ctx_buff *ctx,
+			    struct ipv4_ct_tuple *tuple,
+			    enum ct_status ct_status,
+			    __u32 src_sec_identity, __u32 dst_sec_identity,
+			    const struct trace_ctx *trace)
+{
+	struct endpoint_info *gateway_node_ep;
+	__be32 gateway_ip = 0;
+	int ret;
+
+	/* If the packet is destined to an entity inside the cluster,
+	 * either EP or node, it should not be forwarded to an egress
+	 * gateway since only traffic leaving the cluster is supposed to
+	 * be masqueraded with an egress IP.
+	 */
+	if (identity_is_cluster(dst_sec_identity))
+		return CTX_ACT_OK;
+
+	ret = egress_gw_request_needs_redirect_hook(tuple, ct_status, &gateway_ip);
+	if (IS_ERR(ret))
+		return ret;
+
+	if (ret == CTX_ACT_OK)
+		return ret;
+
+	/* If the gateway node is the local node, then just let the
+	 * packet go through, as it will be SNATed later on by
+	 * handle_nat_fwd().
+	 */
+	gateway_node_ep = __lookup_ip4_endpoint(gateway_ip);
+	if (gateway_node_ep && (gateway_node_ep->flags & ENDPOINT_F_HOST))
+		return CTX_ACT_OK;
+
+	/* Send the packet to egress gateway node through a tunnel. */
+	return __encap_and_redirect_with_nodeid(ctx, 0, gateway_ip,
+						src_sec_identity, dst_sec_identity,
+						NOT_VTEP_DST, trace);
 }
 
 #endif /* ENABLE_EGRESS_GATEWAY_COMMON */

bpf/lib/encap.h
@@ -6,6 +6,7 @@
 
 #include "common.h"
 #include "dbg.h"
+#include "hash.h"
 #include "trace.h"
 #include "l3.h"
 

bpf/lib/overloadable_skb.h
@@ -251,6 +251,19 @@ static __always_inline bool ctx_is_overlay(const struct __sk_buff *ctx)
 	return (ctx->mark & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_OVERLAY;
 }
 
+#ifdef ENABLE_EGRESS_GATEWAY_COMMON
+static __always_inline void ctx_egw_done_set(struct __sk_buff *ctx)
+{
+	ctx->mark &= ~MARK_MAGIC_HOST_MASK;
+	ctx->mark |= MARK_MAGIC_EGW_DONE;
+}
+
+static __always_inline bool ctx_egw_done(const struct __sk_buff *ctx)
+{
+	return (ctx->mark & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_EGW_DONE;
+}
+#endif /* ENABLE_EGRESS_GATEWAY_COMMON */
+
 #ifdef HAVE_ENCAP
 static __always_inline __maybe_unused int
 ctx_set_encap_info(struct __sk_buff *ctx, __u32 src_ip,

bpf/tests/tc_egressgw_snat.c
@@ -90,6 +90,7 @@ int egressgw_snat1_setup(struct __ctx_buff *ctx)
 				  GATEWAY_NODE_IP, EGRESS_IP);
 
 	/* Jump into the entrypoint */
+	ctx_egw_done_set(ctx);
 	tail_call_static(ctx, entry_call_map, TO_NETDEV);
 	/* Fail if we didn't jump */
 	return TEST_ERROR;
@@ -152,6 +153,7 @@ SETUP("tc", "tc_egressgw_snat2")
 int egressgw_snat2_setup(struct __ctx_buff *ctx)
 {
 	/* Jump into the entrypoint */
+	ctx_egw_done_set(ctx);
 	tail_call_static(ctx, entry_call_map, TO_NETDEV);
 	/* Fail if we didn't jump */
 	return TEST_ERROR;
@@ -190,6 +192,7 @@ int egressgw_skip_excluded_cidr_snat_setup(struct __ctx_buff *ctx)
 	add_egressgw_policy_entry(CLIENT_IP, EXTERNAL_SVC_IP, 32, EGRESS_GATEWAY_EXCLUDED_CIDR, 0);
 
 	/* Jump into the entrypoint */
+	ctx_egw_done_set(ctx);
 	tail_call_static(ctx, entry_call_map, TO_NETDEV);
 	/* Fail if we didn't jump */
 	return TEST_ERROR;
@@ -266,6 +269,7 @@ int egressgw_fib_redirect_setup(struct __ctx_buff *ctx)
 				  GATEWAY_NODE_IP, EGRESS_IP2);
 
 	/* Jump into the entrypoint */
+	ctx_egw_done_set(ctx);
 	tail_call_static(ctx, entry_call_map, TO_NETDEV);
 	/* Fail if we didn't jump */
 	return TEST_ERROR;

commit 7d01b05a6a9fd774b75f56f8065be595780942f4

bpf: host: don't apply Bandwidth-Manager twice on EGW traffic
For EGW traffic on a worker node that is redirected to some gateway node,
we're currently applying BWM policy twice - first in to-netdev, then in
to-overlay. Lift the EGW codepath in to-netdev up, so that it can redirect
to the overlay interface *before* applying BWM policy.

Fixes: 218649f97642 ("egressgw: support egress gw handling at bpf_host")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/bpf_host.c
@@ -1481,6 +1481,71 @@ skip_host_firewall:
 	if (IS_ERR(ret))
 		goto drop_err;
 
+#ifdef ENABLE_EGRESS_GATEWAY_COMMON
+	{
+		void *data, *data_end;
+		struct iphdr *ip4;
+		struct ipv4_ct_tuple tuple = {};
+		int l4_off;
+		struct remote_endpoint_info *info;
+		struct endpoint_info *src_ep;
+		bool is_reply;
+
+		if (src_sec_identity == HOST_ID)
+			goto skip_egress_gateway;
+
+		if (proto != bpf_htons(ETH_P_IP))
+			goto skip_egress_gateway;
+
+		if (ctx_egw_done(ctx))
+			goto skip_egress_gateway;
+
+		if (!revalidate_data(ctx, &data, &data_end, &ip4)) {
+			ret = DROP_INVALID;
+			goto drop_err;
+		}
+
+		tuple.nexthdr = ip4->protocol;
+		tuple.daddr = ip4->daddr;
+		tuple.saddr = ip4->saddr;
+
+		l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
+		ret = ct_extract_ports4(ctx, ip4, l4_off, CT_EGRESS, &tuple,
+					NULL);
+		if (IS_ERR(ret)) {
+			if (ret == DROP_CT_UNKNOWN_PROTO)
+				goto skip_egress_gateway;
+
+			goto drop_err;
+		}
+
+		/* Only handle outbound connections: */
+		is_reply = ct_is_reply4(get_ct_map4(&tuple), &tuple);
+		if (is_reply)
+			goto skip_egress_gateway;
+
+		src_ep = __lookup_ip4_endpoint(ip4->saddr);
+		if (src_ep)
+			src_sec_identity = src_ep->sec_id;
+
+		info = lookup_ip4_remote_endpoint(ip4->daddr, 0);
+		if (info && info->sec_identity)
+			dst_sec_identity = info->sec_identity;
+
+		/* lower-level code expects CT tuple to be flipped: */
+		__ipv4_ct_tuple_reverse(&tuple);
+		ret = egress_gw_handle_packet(ctx, &tuple,
+					      src_sec_identity, dst_sec_identity,
+					      &trace);
+		if (IS_ERR(ret))
+			goto drop_err;
+
+		if (ret != CTX_ACT_OK)
+			return ret;
+	}
+skip_egress_gateway:
+#endif
+
 #if defined(ENABLE_BANDWIDTH_MANAGER)
 	ret = edt_sched_departure(ctx, proto);
 	/* No send_drop_notify_error() here given we're rate-limiting. */
@@ -1553,71 +1618,6 @@ skip_host_firewall:
 	ret = lb_handle_health(ctx, proto);
 	if (ret != CTX_ACT_OK)
 		goto exit;
-#endif
-
-#ifdef ENABLE_EGRESS_GATEWAY_COMMON
-	{
-		void *data, *data_end;
-		struct iphdr *ip4;
-		struct ipv4_ct_tuple tuple = {};
-		int l4_off;
-		struct remote_endpoint_info *info;
-		struct endpoint_info *src_ep;
-		bool is_reply;
-
-		if (src_sec_identity == HOST_ID)
-			goto skip_egress_gateway;
-
-		if (proto != bpf_htons(ETH_P_IP))
-			goto skip_egress_gateway;
-
-		if (ctx_egw_done(ctx))
-			goto skip_egress_gateway;
-
-		if (!revalidate_data(ctx, &data, &data_end, &ip4)) {
-			ret = DROP_INVALID;
-			goto drop_err;
-		}
-
-		tuple.nexthdr = ip4->protocol;
-		tuple.daddr = ip4->daddr;
-		tuple.saddr = ip4->saddr;
-
-		l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
-		ret = ct_extract_ports4(ctx, ip4, l4_off, CT_EGRESS, &tuple,
-					NULL);
-		if (IS_ERR(ret)) {
-			if (ret == DROP_CT_UNKNOWN_PROTO)
-				goto skip_egress_gateway;
-
-			goto drop_err;
-		}
-
-		/* Only handle outbound connections: */
-		is_reply = ct_is_reply4(get_ct_map4(&tuple), &tuple);
-		if (is_reply)
-			goto skip_egress_gateway;
-
-		src_ep = __lookup_ip4_endpoint(ip4->saddr);
-		if (src_ep)
-			src_sec_identity = src_ep->sec_id;
-
-		info = lookup_ip4_remote_endpoint(ip4->daddr, 0);
-		if (info && info->sec_identity)
-			dst_sec_identity = info->sec_identity;
-
-		/* lower-level code expects CT tuple to be flipped: */
-		__ipv4_ct_tuple_reverse(&tuple);
-		ret = egress_gw_handle_packet(ctx, &tuple,
-					      src_sec_identity, dst_sec_identity,
-					      &trace);
-		if (IS_ERR(ret))
-			goto drop_err;
-
-		if (ret != CTX_ACT_OK)
-			return ret;
-	}
-skip_egress_gateway:
 #endif
 
 #ifdef ENABLE_NODEPORT