cilium/cilium

commit 77942781f28f725a3cef89fa898c4f44dbfb8de9

api: Add new CiliumGatewayClassConfig CRD
This new CRD will be used as GatewayClass.Spec.ParameterRef later.

Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/k8s/synced/crd.go
@@ -18,7 +18,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
-	v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
@@ -113,6 +113,7 @@ func AllCiliumCRDResourceNames() []string {
 		AgentCRDResourceNames(),
 		CRDResourceName(v2.CNCName),
 		CRDResourceName(v2alpha1.CNCName), // TODO depreciate CNC on v2alpha1 https://github.com/cilium/cilium/issues/31982
+		CRDResourceName(v2alpha1.CGCCName),
 	)
 }
 

contrib/scripts/k8s-manifests-gen.sh
@@ -39,7 +39,8 @@ CRDS_CILIUM_V2ALPHA1="ciliumendpointslices \
                       ciliumloadbalancerippools \
                       ciliumcidrgroups \
                       ciliuml2announcementpolicies \
-                      ciliumpodippools"
+                      ciliumpodippools \
+                      ciliumgatewayclassconfigs"
 
 TMPDIR=$(mktemp -d -t cilium.tmpXXXXXXXX)
 go run sigs.k8s.io/controller-tools/cmd/controller-gen ${CRD_OPTIONS} paths="${CRD_PATHS}" output:crd:artifacts:config="${TMPDIR}"

install/kubernetes/cilium/Chart.yaml
@@ -170,3 +170,9 @@ annotations:
       displayName: Cilium Pod IP Pool
       description: |
         CiliumPodIPPool defines an IP pool that can be used for pooled IPAM (i.e. the multi-pool IPAM mode).
+    - kind: CiliumGatewayClassConfig
+      version: v2alpha1
+      name: ciliumgatewayclassconfigs.cilium.io
+      displayName: Cilium Gateway Class Config
+      description: |
+        CiliumGatewayClassConfig defines a configuration for Gateway API GatewayClass.

pkg/k8s/apis/cilium.io/client/crds/v2alpha1/ciliumgatewayclassconfigs.yaml
@@ -1,0 +1,184 @@
+---
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  annotations:
+    controller-gen.kubebuilder.io/version: v0.16.5
+  name: ciliumgatewayclassconfigs.cilium.io
+spec:
+  group: cilium.io
+  names:
+    categories:
+    - cilium
+    kind: CiliumGatewayClassConfig
+    listKind: CiliumGatewayClassConfigList
+    plural: ciliumgatewayclassconfigs
+    shortNames:
+    - cgcc
+    singular: ciliumgatewayclassconfig
+  scope: Namespaced
+  versions:
+  - additionalPrinterColumns:
+    - jsonPath: .status.conditions[?(@.type=="Accepted")].status
+      name: Accepted
+      type: string
+    - jsonPath: .metadata.creationTimestamp
+      name: Age
+      type: date
+    - jsonPath: .spec.description
+      name: Description
+      priority: 1
+      type: string
+    name: v2alpha1
+    schema:
+      openAPIV3Schema:
+        description: |-
+          CiliumGatewayClassConfig is a Kubernetes third-party resource which
+          is used to configure Gateways owned by GatewayClass.
+        properties:
+          apiVersion:
+            description: |-
+              APIVersion defines the versioned schema of this representation of an object.
+              Servers should convert recognized schemas to the latest internal value, and
+              may reject unrecognized values.
+              More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+            type: string
+          kind:
+            description: |-
+              Kind is a string value representing the REST resource this object represents.
+              Servers may infer this from the endpoint the client submits requests to.
+              Cannot be updated.
+              In CamelCase.
+              More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+            type: string
+          metadata:
+            type: object
+          spec:
+            description: Spec is a human-readable of a GatewayClass configuration.
+            properties:
+              description:
+                description: Description helps describe a GatewayClass configuration
+                  with more details.
+                maxLength: 64
+                type: string
+              service:
+                description: |-
+                  Service specifies the configuration for the generated Service.
+                  Note that not all fields from upstream Service.Spec are supported
+                properties:
+                  allocateLoadBalancerNodePorts:
+                    description: Sets the Service.Spec.AllocateLoadBalancerNodePorts
+                      in generated Service objects to the given value.
+                    type: boolean
+                  externalTrafficPolicy:
+                    default: Cluster
+                    description: Sets the Service.Spec.ExternalTrafficPolicy in generated
+                      Service objects to the given value.
+                    type: string
+                  ipFamilies:
+                    description: Sets the Service.Spec.IPFamilies in generated Service
+                      objects to the given value.
+                    items:
+                      description: |-
+                        IPFamily represents the IP Family (IPv4 or IPv6). This type is used
+                        to express the family of an IP expressed by a type (e.g. service.spec.ipFamilies).
+                      type: string
+                    type: array
+                    x-kubernetes-list-type: atomic
+                  ipFamilyPolicy:
+                    description: Sets the Service.Spec.IPFamilyPolicy in generated
+                      Service objects to the given value.
+                    type: string
+                  loadBalancerClass:
+                    description: Sets the Service.Spec.LoadBalancerClass in generated
+                      Service objects to the given value.
+                    type: string
+                  loadBalancerSourceRanges:
+                    description: Sets the Service.Spec.LoadBalancerSourceRanges in
+                      generated Service objects to the given value.
+                    items:
+                      type: string
+                    type: array
+                    x-kubernetes-list-type: atomic
+                  trafficDistribution:
+                    description: Sets the Service.Spec.TrafficDistribution in generated
+                      Service objects to the given value.
+                    type: string
+                  type:
+                    default: LoadBalancer
+                    description: Sets the Service.Spec.Type in generated Service objects
+                      to the given value.
+                    type: string
+                type: object
+            type: object
+          status:
+            description: Status is the status of the policy.
+            properties:
+              conditions:
+                description: Current service state
+                items:
+                  description: Condition contains details for one aspect of the current
+                    state of this API Resource.
+                  properties:
+                    lastTransitionTime:
+                      description: |-
+                        lastTransitionTime is the last time the condition transitioned from one status to another.
+                        This should be when the underlying condition changed.  If that is not known, then using the time when the API field changed is acceptable.
+                      format: date-time
+                      type: string
+                    message:
+                      description: |-
+                        message is a human readable message indicating details about the transition.
+                        This may be an empty string.
+                      maxLength: 32768
+                      type: string
+                    observedGeneration:
+                      description: |-
+                        observedGeneration represents the .metadata.generation that the condition was set based upon.
+                        For instance, if .metadata.generation is currently 12, but the .status.conditions[x].observedGeneration is 9, the condition is out of date
+                        with respect to the current state of the instance.
+                      format: int64
+                      minimum: 0
+                      type: integer
+                    reason:
+                      description: |-
+                        reason contains a programmatic identifier indicating the reason for the condition's last transition.
+                        Producers of specific condition types may define expected values and meanings for this field,
+                        and whether the values are considered a guaranteed API.
+                        The value should be a CamelCase string.
+                        This field may not be empty.
+                      maxLength: 1024
+                      minLength: 1
+                      pattern: ^[A-Za-z]([A-Za-z0-9_,:]*[A-Za-z0-9_])?$
+                      type: string
+                    status:
+                      description: status of the condition, one of True, False, Unknown.
+                      enum:
+                      - "True"
+                      - "False"
+                      - Unknown
+                      type: string
+                    type:
+                      description: type of condition in CamelCase or in foo.example.com/CamelCase.
+                      maxLength: 316
+                      pattern: ^([a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*/)?(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])$
+                      type: string
+                  required:
+                  - lastTransitionTime
+                  - message
+                  - reason
+                  - status
+                  - type
+                  type: object
+                type: array
+                x-kubernetes-list-map-keys:
+                - type
+                x-kubernetes-list-type: map
+            type: object
+        required:
+        - metadata
+        type: object
+    served: true
+    storage: true
+    subresources:
+      status: {}

pkg/k8s/apis/cilium.io/client/register.go
@@ -95,6 +95,8 @@ const (
 
 	// CPIPCRDName is the full name of the CiliumPodIPPool CRD.
 	CPIPCRDName = k8sconstv2alpha1.CPIPKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
+	// CGCCCRDName is the full name of the CiliumGatewayClassConfig CRD.
+	CGCCCRDName = k8sconstv2alpha1.CGCCKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
 )
 
 // log is the k8s package logger object.
@@ -197,6 +199,10 @@ func CustomResourceDefinitionList() map[string]*CRDList {
 			Name:     CPIPCRDName,
 			FullName: k8sconstv2alpha1.CPIPName,
 		},
+		synced.CRDResourceName(k8sconstv2alpha1.CGCCName): {
+			Name:     CGCCCRDName,
+			FullName: k8sconstv2alpha1.CGCCName,
+		},
 	}
 }
 
@@ -283,6 +289,9 @@ var (
 
 	//go:embed crds/v2alpha1/ciliumpodippools.yaml
 	crdsv2Alpha1CiliumPodIPPools []byte
+
+	//go:embed crds/v2alpha1/ciliumgatewayclassconfigs.yaml
+	crdsv2Alpha1CiliumGatewayClassConfigs []byte
 )
 
 // GetPregeneratedCRD returns the pregenerated CRD based on the requested CRD
@@ -342,6 +351,8 @@ func GetPregeneratedCRD(crdName string) apiextensionsv1.CustomResourceDefinition
 		crdBytes = crdsv2Alpha1CiliumL2AnnouncementPolicies
 	case CPIPCRDName:
 		crdBytes = crdsv2Alpha1CiliumPodIPPools
+	case CGCCCRDName:
+		crdBytes = crdsv2Alpha1CiliumGatewayClassConfigs
 	default:
 		scopedLog.Fatal("Pregenerated CRD does not exist")
 	}

pkg/k8s/apis/cilium.io/v2alpha1/gatewayclassconfig_types.go
@@ -1,0 +1,129 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package v2alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:categories={cilium},singular="ciliumgatewayclassconfig",path="ciliumgatewayclassconfigs",scope="Namespaced",shortName={cgcc}
+// +kubebuilder:printcolumn:name="Accepted",type=string,JSONPath=`.status.conditions[?(@.type=="Accepted")].status`
+// +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
+// +kubebuilder:printcolumn:name="Description",type=string,JSONPath=`.spec.description`,priority=1
+// +kubebuilder:subresource:status
+// +kubebuilder:storageversion
+
+// CiliumGatewayClassConfig is a Kubernetes third-party resource which
+// is used to configure Gateways owned by GatewayClass.
+type CiliumGatewayClassConfig struct {
+	// +deepequal-gen=false
+	metav1.TypeMeta `json:",inline"`
+	// +deepequal-gen=false
+	metav1.ObjectMeta `json:"metadata"`
+
+	// Spec is a human-readable of a GatewayClass configuration.
+	//
+	// +kubebuilder:validation:Optional
+	Spec CiliumGatewayClassConfigSpec `json:"spec,omitempty"`
+
+	// Status is the status of the policy.
+	//
+	// +deepequal-gen=false
+	// +kubebuilder:validation:Optional
+	Status CiliumGatewayClassConfigStatus `json:"status"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:openapi-gen=false
+// +deepequal-gen=false
+
+// CiliumGatewayClassConfigList is a list of
+// CiliumGatewayClassConfig objects.
+type CiliumGatewayClassConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	// Items is a list of CiliumGatewayClassConfigs.
+	Items []CiliumGatewayClassConfig `json:"items"`
+}
+
+// +deepequal-gen=true
+
+type ServiceConfig struct {
+	// Sets the Service.Spec.Type in generated Service objects to the given value.
+	//
+	// +kubebuilder:default="LoadBalancer"
+	Type corev1.ServiceType `json:"type,omitempty"`
+
+	// Sets the Service.Spec.ExternalTrafficPolicy in generated Service objects to the given value.
+	//
+	// +optional
+	// +kubebuilder:default="Cluster"
+	ExternalTrafficPolicy corev1.ServiceExternalTrafficPolicy `json:"externalTrafficPolicy,omitempty"`
+
+	// Sets the Service.Spec.LoadBalancerClass in generated Service objects to the given value.
+	//
+	// +optional
+	LoadBalancerClass *string `json:"loadBalancerClass,omitempty"`
+
+	// Sets the Service.Spec.IPFamilies in generated Service objects to the given value.
+	//
+	// +listType=atomic
+	// +optional
+	IPFamilies []corev1.IPFamily `json:"ipFamilies,omitempty"`
+
+	// Sets the Service.Spec.IPFamilyPolicy in generated Service objects to the given value.
+	//
+	// +optional
+	IPFamilyPolicy *corev1.IPFamilyPolicy `json:"ipFamilyPolicy,omitempty"`
+
+	// Sets the Service.Spec.AllocateLoadBalancerNodePorts in generated Service objects to the given value.
+	//
+	// +optional
+	AllocateLoadBalancerNodePorts *bool `json:"allocateLoadBalancerNodePorts,omitempty"`
+
+	// Sets the Service.Spec.LoadBalancerSourceRanges in generated Service objects to the given value.
+	//
+	// +optional
+	// +listType=atomic
+	LoadBalancerSourceRanges []string `json:"loadBalancerSourceRanges,omitempty"`
+
+	// Sets the Service.Spec.TrafficDistribution in generated Service objects to the given value.
+	//
+	// +optional
+	TrafficDistribution *string `json:"trafficDistribution,omitempty"`
+}
+
+// CiliumGatewayClassConfigSpec specifies all the configuration options for a
+// Cilium managed GatewayClass.
+type CiliumGatewayClassConfigSpec struct {
+	// Description helps describe a GatewayClass configuration with more details.
+	//
+	// +kubebuilder:validation:MaxLength=64
+	// +kubebuilder:validation:Optional
+	Description *string `json:"description,omitempty"`
+
+	// Service specifies the configuration for the generated Service.
+	// Note that not all fields from upstream Service.Spec are supported.
+	//
+	// +kubebuilder:validation:Optional
+	Service *ServiceConfig `json:"service,omitempty"`
+}
+
+// +deepequal-gen=false
+
+// CiliumGatewayClassConfigStatus contains the status of a CiliumGatewayClassConfig.
+type CiliumGatewayClassConfigStatus struct {
+	// Current service state
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	// +listType=map
+	// +listMapKey=type
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}

pkg/k8s/apis/cilium.io/v2alpha1/register.go
@@ -110,6 +110,12 @@ const (
 	CPIPPluralName     = "ciliumpodippools"
 	CPIPKindDefinition = "CiliumPodIPPool"
 	CPIPName           = CPIPPluralName + "." + CustomResourceDefinitionGroup
+
+	// CiliumGatewayClassConfig (CGCC)
+	CGCCPluralName     = "ciliumgatewayclassconfigs"
+	CGCCListName       = "ciliumgatewayclassconfiglists"
+	CGCCKindDefinition = "CiliumGatewayClassConfig"
+	CGCCName           = CGCCPluralName + "." + CustomResourceDefinitionGroup
 )
 
 // SchemeGroupVersion is group version used to register these objects
@@ -179,6 +185,10 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&CiliumBGPNodeConfigList{},
 		&CiliumBGPNodeConfigOverride{},
 		&CiliumBGPNodeConfigOverrideList{},
+
+		// new Gateway API types
+		&CiliumGatewayClassConfig{},
+		&CiliumGatewayClassConfigList{},
 	)
 
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)

pkg/k8s/apis/cilium.io/v2alpha1/zz_generated.deepcopy.go
@@ -11,6 +11,7 @@ package v2alpha1
 import (
 	models "github.com/cilium/cilium/api/v1/models"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	api "github.com/cilium/cilium/pkg/policy/api"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -1457,6 +1458,116 @@ func (in *CiliumEndpointSliceList) DeepCopyObject() runtime.Object {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CiliumGatewayClassConfig) DeepCopyInto(out *CiliumGatewayClassConfig) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CiliumGatewayClassConfig.
+func (in *CiliumGatewayClassConfig) DeepCopy() *CiliumGatewayClassConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(CiliumGatewayClassConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *CiliumGatewayClassConfig) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CiliumGatewayClassConfigList) DeepCopyInto(out *CiliumGatewayClassConfigList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]CiliumGatewayClassConfig, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CiliumGatewayClassConfigList.
+func (in *CiliumGatewayClassConfigList) DeepCopy() *CiliumGatewayClassConfigList {
+	if in == nil {
+		return nil
+	}
+	out := new(CiliumGatewayClassConfigList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *CiliumGatewayClassConfigList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CiliumGatewayClassConfigSpec) DeepCopyInto(out *CiliumGatewayClassConfigSpec) {
+	*out = *in
+	if in.Description != nil {
+		in, out := &in.Description, &out.Description
+		*out = new(string)
+		**out = **in
+	}
+	if in.Service != nil {
+		in, out := &in.Service, &out.Service
+		*out = new(ServiceConfig)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CiliumGatewayClassConfigSpec.
+func (in *CiliumGatewayClassConfigSpec) DeepCopy() *CiliumGatewayClassConfigSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(CiliumGatewayClassConfigSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CiliumGatewayClassConfigStatus) DeepCopyInto(out *CiliumGatewayClassConfigStatus) {
+	*out = *in
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]metav1.Condition, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CiliumGatewayClassConfigStatus.
+func (in *CiliumGatewayClassConfigStatus) DeepCopy() *CiliumGatewayClassConfigStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(CiliumGatewayClassConfigStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *CiliumL2AnnouncementPolicy) DeepCopyInto(out *CiliumL2AnnouncementPolicy) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
@@ -1984,6 +2095,52 @@ func (in *PeerConfigReference) DeepCopy() *PeerConfigReference {
 		return nil
 	}
 	out := new(PeerConfigReference)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ServiceConfig) DeepCopyInto(out *ServiceConfig) {
+	*out = *in
+	if in.LoadBalancerClass != nil {
+		in, out := &in.LoadBalancerClass, &out.LoadBalancerClass
+		*out = new(string)
+		**out = **in
+	}
+	if in.IPFamilies != nil {
+		in, out := &in.IPFamilies, &out.IPFamilies
+		*out = make([]corev1.IPFamily, len(*in))
+		copy(*out, *in)
+	}
+	if in.IPFamilyPolicy != nil {
+		in, out := &in.IPFamilyPolicy, &out.IPFamilyPolicy
+		*out = new(corev1.IPFamilyPolicy)
+		**out = **in
+	}
+	if in.AllocateLoadBalancerNodePorts != nil {
+		in, out := &in.AllocateLoadBalancerNodePorts, &out.AllocateLoadBalancerNodePorts
+		*out = new(bool)
+		**out = **in
+	}
+	if in.LoadBalancerSourceRanges != nil {
+		in, out := &in.LoadBalancerSourceRanges, &out.LoadBalancerSourceRanges
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.TrafficDistribution != nil {
+		in, out := &in.TrafficDistribution, &out.TrafficDistribution
+		*out = new(string)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ServiceConfig.
+func (in *ServiceConfig) DeepCopy() *ServiceConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(ServiceConfig)
 	in.DeepCopyInto(out)
 	return out
 }

commit 79b698dc878e0dd6702779d49408568c2e99d611

api: Conditionally register GatewayAPI related CRDs
Fixes: 77942781f28f725a3cef89fa898c4f44dbfb8de9
Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/k8s/synced/crd.go
@@ -17,6 +17,7 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 
+	operatorOption "github.com/cilium/cilium/operator/option"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/k8s/client"
@@ -106,15 +107,24 @@ func ClusterMeshAPIServerResourceNames() []string {
 	}
 }
 
+func GatewayAPIResourceNames() []string {
+	if !operatorOption.Config.EnableGatewayAPI {
+		return nil
+	}
+	return []string{
+		CRDResourceName(v2alpha1.CGCCName),
+	}
+}
+
 // AllCiliumCRDResourceNames returns a list of all Cilium CRD resource names
 // that the cilium operator or testsuite may register.
 func AllCiliumCRDResourceNames() []string {
-	return append(
-		AgentCRDResourceNames(),
+	res := append(AgentCRDResourceNames(), GatewayAPIResourceNames()...)
+	res = append(res,
 		CRDResourceName(v2.CNCName),
 		CRDResourceName(v2alpha1.CNCName), // TODO depreciate CNC on v2alpha1 https://github.com/cilium/cilium/issues/31982
-		CRDResourceName(v2alpha1.CGCCName),
 	)
+	return res
 }
 
 // SyncCRDs will sync Cilium CRDs to ensure that they have all been