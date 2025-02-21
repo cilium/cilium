0ee3d3a5f8da18e317c50448fc8ddd8d4492b52a
André Martins <andre@cilium.io>
2025-02-21 12:23:40 +0100

    pkg/logging: add slog helper functions
    Add Fatal and Panic helper functions for slog migration.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/logging/slog.go
+++ pkg/logging/slog.go
@@ -119,3 +119,30 @@ func replaceAttrFnWithoutTimestamp(groups []string, a slog.Attr) slog.Attr {
 		return replaceAttrFn(groups, a)
 	}
 }
+
+type FieldLogger interface {
+	Handler() slog.Handler
+	With(args ...any) *slog.Logger
+	WithGroup(name string) *slog.Logger
+	Enabled(ctx context.Context, level slog.Level) bool
+	Log(ctx context.Context, level slog.Level, msg string, args ...any)
+	LogAttrs(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr)
+	Debug(msg string, args ...any)
+	DebugContext(ctx context.Context, msg string, args ...any)
+	Info(msg string, args ...any)
+	InfoContext(ctx context.Context, msg string, args ...any)
+	Warn(msg string, args ...any)
+	WarnContext(ctx context.Context, msg string, args ...any)
+	Error(msg string, args ...any)
+	ErrorContext(ctx context.Context, msg string, args ...any)
+}
+
+func Fatal(logger FieldLogger, msg string, args ...any) {
+	logger.Error(msg, args...)
+	os.Exit(-1)
+}
+
+func Panic(logger FieldLogger, msg string, args ...any) {
+	logger.Error(msg, args...)
+	panic(msg)
+}
