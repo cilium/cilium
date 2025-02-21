86e5e23328ccb8cbc20b339bddd15b6eeb768010
Jarno Rajahalme <jarno@isovalent.com>
2025-02-21 09:55:51 +0100

    cilium-cli: remove unused CurlCommandParallelWithOutput
    Remove unused function CurlCommandParallelWithOutput.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- cilium-cli/connectivity/check/context.go
+++ cilium-cli/connectivity/check/context.go
@@ -1010,32 +1010,6 @@ func (ct *ConnectivityTest) CurlCommandWithOutput(peer TestPeer, ipFam features.
 	return cmd
 }
 
-func (ct *ConnectivityTest) CurlCommandParallelWithOutput(peer TestPeer, ipFam features.IPFamily, parallel int, opts ...string) []string {
-	cmd := []string{
-		"curl", "--silent", "--fail", "--show-error",
-		"--parallel", "--parallel-immediate", "--parallel-max", fmt.Sprint(parallel),
-	}
-
-	if connectTimeout := ct.params.ConnectTimeout.Seconds(); connectTimeout > 0.0 {
-		cmd = append(cmd, "--connect-timeout", strconv.FormatFloat(connectTimeout, 'f', -1, 64))
-	}
-	if requestTimeout := ct.params.RequestTimeout.Seconds(); requestTimeout > 0.0 {
-		cmd = append(cmd, "--max-time", strconv.FormatFloat(requestTimeout, 'f', -1, 64))
-	}
-
-	cmd = append(cmd, opts...)
-	url := fmt.Sprintf("%s://%s%s",
-		peer.Scheme(),
-		net.JoinHostPort(peer.Address(ipFam), fmt.Sprint(peer.Port())),
-		peer.Path())
-
-	for i := 0; i < parallel; i++ {
-		cmd = append(cmd, url)
-	}
-
-	return cmd
-}
-
 func (ct *ConnectivityTest) PingCommand(peer TestPeer, ipFam features.IPFamily, extraArgs ...string) []string {
 	cmd := []string{"ping", "-c", "1"}
 
