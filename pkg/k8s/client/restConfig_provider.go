8a338267c1541416dbf0366e8d446b54a64dc0b6
Aditi Ghag <aditi@cilium.io>
2025-02-25 10:42:42 -0800

    pkg/k8s: Support setting multiple kubeapi-servers
    This commit implements the logic to configure the
    agent Kubernetes client module to connect to one
    of the active kube-apiservers when users configure
    multiple kube-apiserver URLs. This is a first step
    to support high availability for the Kubernetes
    API server. Follow-up commits switch over to the
    `kubernetes` service with api server endpoints
    at runtime.
    
    The k8s-client cell provides a set of Kubernetes clients
    for other modules to connect to the API server. More specifically,
    the client provides Kubernetes client interfaces that are wired
    into other modules during agent initialization via the hive/cell
    framework. In order to fail over to an active kube-apiserver instance
    at runtime, the commit wraps k8s client rest configuration with an
    HTTP round tripper. The internal round tripper updates rest
    configuration host whenever there are connectivity failures
    to the api server.
    
    Co-developed-by: Deepesh Pathak <deepeshpathak09@gmail.com>
    Signed-off-by: Deepesh Pathak <deepeshpathak09@gmail.com>
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

--- pkg/k8s/client/restConfig_provider.go
+++ pkg/k8s/client/restConfig_provider.go
@@ -5,6 +5,9 @@ package client
 
 import (
 	"fmt"
+	"math/rand/v2"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,25 +16,43 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/version"
 )
 
 type restConfigManager struct {
-	restConfig *rest.Config
-	log        logrus.FieldLogger
+	restConfig    *rest.Config
+	apiServerURLs []*url.URL
+	lock.RWMutex
+	log logrus.FieldLogger
+	rt  *rotatingHttpRoundTripper
 }
 
 type restConfig interface {
 	getConfig() *rest.Config
+	canRotateAPIServerURL() bool
+	rotateAPIServerURL()
 }
 
 func (r *restConfigManager) getConfig() *rest.Config {
+	r.RLock()
+	defer r.RUnlock()
 	return rest.CopyConfig(r.restConfig)
+}
+
+func (r *restConfigManager) canRotateAPIServerURL() bool {
+	return len(r.apiServerURLs) > 1
 }
 
 func restConfigManagerInit(cfg Config, name string, log logrus.FieldLogger) (restConfig, error) {
 	var err error
-	manager := restConfigManager{log: log}
+	manager := restConfigManager{
+		log: log,
+		rt: &rotatingHttpRoundTripper{
+			log: log,
+		}}
+
+	manager.parseConfig(cfg)
 
 	cmdName := "cilium"
 	if len(os.Args[0]) != 0 {
@@ -43,7 +64,13 @@ func restConfigManagerInit(cfg Config, name string, log logrus.FieldLogger) (res
 		userAgent = fmt.Sprintf("%s %s", userAgent, name)
 	}
 
-	manager.restConfig, err = createConfig(cfg.K8sAPIServer, cfg.K8sKubeConfigPath, cfg.K8sClientQPS, cfg.K8sClientBurst, userAgent)
+	if manager.restConfig, err = manager.createConfig(cfg, userAgent); err != nil {
+		return nil, err
+	}
+	if manager.canRotateAPIServerURL() {
+		// Pick an API server at random.
+		manager.rotateAPIServerURL()
+	}
 
 	return &manager, err
 }
@@ -52,13 +79,22 @@ func restConfigManagerInit(cfg Config, name string, log logrus.FieldLogger) (res
 //
 // The precedence of the configuration selection is the following:
 // 1. kubeCfgPath
-// 2. apiServerURL (https if specified)
+// 2. apiServerURL(s) (https if specified)
 // 3. rest.InClusterConfig().
-func createConfig(apiServerURL, kubeCfgPath string, qps float32, burst int, userAgent string) (*rest.Config, error) {
+func (r *restConfigManager) createConfig(cfg Config, userAgent string) (*rest.Config, error) {
 	var (
-		config *rest.Config
-		err    error
+		config       *rest.Config
+		err          error
+		apiServerURL string
 	)
+	if cfg.K8sAPIServer != "" {
+		apiServerURL = cfg.K8sAPIServer
+	} else if len(r.apiServerURLs) > 0 {
+		apiServerURL = r.apiServerURLs[0].String()
+	}
+	kubeCfgPath := cfg.K8sKubeConfigPath
+	qps := cfg.K8sClientQPS
+	burst := cfg.K8sClientBurst
 
 	switch {
 	// If the apiServerURL and the kubeCfgPath are empty then we can try getting
@@ -81,8 +117,50 @@ func createConfig(apiServerURL, kubeCfgPath string, qps float32, burst int, user
 		config = &rest.Config{Host: apiServerURL, UserAgent: userAgent}
 	}
 
+	// The HTTP round tripper rotates API server URLs in case of connectivity failures.
+	if len(r.apiServerURLs) > 1 {
+		config.Wrap(r.WrapRoundTripper)
+	}
+
 	setConfig(config, userAgent, qps, burst)
 	return config, nil
+}
+
+func (r *restConfigManager) parseConfig(cfg Config) {
+	if cfg.K8sAPIServer != "" {
+		var (
+			serverURL *url.URL
+			err       error
+		)
+		s := cfg.K8sAPIServer
+		if !strings.HasPrefix(s, "http") {
+			s = fmt.Sprintf("http://%s", s) // default to HTTP
+		}
+		serverURL, err = url.Parse(s)
+		if err != nil {
+			r.log.WithError(err).Errorf("Failed to parse APIServerURL %s, skipping", serverURL)
+			return
+		}
+		r.apiServerURLs = append(r.apiServerURLs, serverURL)
+		return
+	}
+	for _, apiServerURL := range cfg.K8sAPIServerURLs {
+		if apiServerURL == "" {
+			continue
+		}
+
+		if !strings.HasPrefix(apiServerURL, "http") && !strings.HasPrefix(apiServerURL, "https") {
+			apiServerURL = fmt.Sprintf("https://%s", apiServerURL)
+		}
+
+		serverURL, err := url.Parse(apiServerURL)
+		if err != nil {
+			r.log.WithError(err).Errorf("Failed to parse APIServerURL %s, skipping", apiServerURL)
+			continue
+		}
+
+		r.apiServerURLs = append(r.apiServerURLs, serverURL)
+	}
 }
 
 func setConfig(config *rest.Config, userAgent string, qps float32, burst int) {
@@ -95,4 +173,46 @@ func setConfig(config *rest.Config, userAgent string, qps float32, burst int) {
 	if burst != 0 {
 		config.Burst = burst
 	}
+}
+
+func (r *restConfigManager) rotateAPIServerURL() {
+	if len(r.apiServerURLs) <= 1 {
+		return
+	}
+
+	r.rt.Lock()
+	defer r.rt.Unlock()
+	for {
+		idx := rand.IntN(len(r.apiServerURLs))
+		if r.rt.apiServerURL != r.apiServerURLs[idx] {
+			r.rt.apiServerURL = r.apiServerURLs[idx]
+			break
+		}
+	}
+	r.Lock()
+	r.restConfig.Host = r.rt.apiServerURL.String()
+	r.Unlock()
+	r.log.WithField("url", r.rt.apiServerURL).Info("Rotated api server")
+}
+
+// rotatingHttpRoundTripper sets the remote host in the rest configuration used to make API requests to the API server.
+type rotatingHttpRoundTripper struct {
+	delegate     http.RoundTripper
+	log          logrus.FieldLogger
+	apiServerURL *url.URL
+	lock.RWMutex // Synchronizes access to apiServerURL
+}
+
+func (rt *rotatingHttpRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	rt.RLock()
+	defer rt.RUnlock()
+
+	rt.log.WithField("host", rt.apiServerURL).Debug("Kubernetes api server host")
+	req.URL.Host = rt.apiServerURL.Host
+	return rt.delegate.RoundTrip(req)
+}
+
+func (r *restConfigManager) WrapRoundTripper(rt http.RoundTripper) http.RoundTripper {
+	r.rt.delegate = rt
+	return r.rt
 }
