b02e50ab786341c186eebf86e81ed3a9fd810995
Jussi Maki <jussi@isovalent.com>
2025-02-25 09:11:25 +0100

    daemon/k8s: Add Table[Namespace]
    Add the Table[Namespace] for exposing the k8s namespaces via StateDB table.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- daemon/k8s/script_test.go
+++ daemon/k8s/script_test.go
@@ -31,7 +31,14 @@ func TestScript(t *testing.T) {
 	time.Now = func() time.Time {
 		return time.Date(2000, 1, 1, 10, 30, 0, 0, time.UTC)
 	}
-	t.Cleanup(func() { time.Now = now })
+	since := time.Since
+	time.Since = func(t time.Time) time.Duration {
+		return time.Minute
+	}
+	t.Cleanup(func() {
+		time.Now = now
+		time.Since = since
+	})
 	t.Setenv("TZ", "")
 	nodeTypes.SetName("testnode")
 
@@ -50,6 +57,7 @@ func TestScript(t *testing.T) {
 				// would depend on them).
 				cell.Invoke(
 					func(statedb.Table[LocalPod]) {},
+					func(statedb.Table[Namespace]) {},
 				),
 			)
 
