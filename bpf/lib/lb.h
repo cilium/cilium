90c559da139eeeebfea20934aefdddf0ac249e01
Julian Wiedmann <jwi@isovalent.com>
2025-02-26 12:17:19 +0200

    bpf: lb: use redirect_self() in tail_no_service_ipv*()
    Use the intended helper to be a bit more self-documenting.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/lb.h
+++ bpf/lib/lb.h
@@ -2039,7 +2039,7 @@ int tail_no_service_ipv4(struct __ctx_buff *ctx)
 		/* Redirect ICMP to the interface we received it on. */
 		cilium_dbg_capture(ctx, DBG_CAPTURE_DELIVERY,
 				   ctx_get_ifindex(ctx));
-		ret = ctx_redirect(ctx, ctx_get_ifindex(ctx), 0);
+		ret = redirect_self(ctx);
 	}
 
 	if (IS_ERR(ret))
@@ -2219,7 +2219,7 @@ int tail_no_service_ipv6(struct __ctx_buff *ctx)
 		/* Redirect ICMP to the interface we received it on. */
 		cilium_dbg_capture(ctx, DBG_CAPTURE_DELIVERY,
 				   ctx_get_ifindex(ctx));
-		ret = ctx_redirect(ctx, ctx_get_ifindex(ctx), 0);
+		ret = redirect_self(ctx);
 	}
 
 drop_err:
