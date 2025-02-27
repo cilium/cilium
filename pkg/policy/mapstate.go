11f5422f02c71888827b88c3f9882064e168e7b9
Jarno Rajahalme <jarno@isovalent.com>
2025-02-27 09:30:26 +0100

    policy: Keep all explicit auth entries
    Do not delete covered explicit auth entries even if the covering entry
    has the same auth requirement, as it is possible for a more specific
    covering key with a different auth requirement to be added later, after
    which the covered key (now with different auth requirement than the
    most specific covering key) is needed after all.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- pkg/policy/mapstate.go
+++ pkg/policy/mapstate.go
@@ -732,7 +732,7 @@ func (ms *mapState) insertWithChanges(newKey Key, newEntry mapStateEntry, featur
 // old entry in 'changes'.
 // Returns 'true' if changes were made.
 func (ms *mapState) overrideProxyPortForAuth(newEntry mapStateEntry, k Key, v mapStateEntry, changes ChangeState) bool {
-	if v.AuthRequirement != newEntry.AuthRequirement && v.AuthRequirement.IsExplicit() {
+	if v.AuthRequirement.IsExplicit() {
 		// Save the old value first
 		changes.insertOldIfNotExists(k, v)
 
@@ -776,9 +776,9 @@ func (ms *mapState) authPreferredInsert(newKey Key, newEntry mapStateEntry, feat
 			return // bail if covered by deny
 		}
 		if v.ProxyPortPriority > newEntry.ProxyPortPriority {
-			if !newEntryHasExplicitAuth || v.AuthRequirement == newEntry.AuthRequirement {
+			if !newEntryHasExplicitAuth {
 				// Covering entry has higher proxy port priority and newEntry has a
-				// default auth type or the same auth requirement => can bail out
+				// default auth type => can bail out
 				return
 			}
 
