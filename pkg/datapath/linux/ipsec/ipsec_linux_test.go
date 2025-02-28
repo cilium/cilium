c78e2c26a974e0427fbd2d284cd077853153e5b0
Simone Magnani <simone.magnani@isovalent.com>
2025-02-28 18:26:09 +0100

    ipsec:doc: reject key rotations with different auth key length
    [ upstream commit d16124fbb2e84349b239111f2a3a043b6c4de3bd ]
    
    [ backporter notes:
      - solved merge conflicts due to non-backported IPSec SPI parse logic.
      - borrowed UnsetTestIPSecKey from non-backported commit
        "detecting IPSEC SPI unchanged" to fix test logic. ]
    
    With this commit we explicitly reject key rotations when the newly provided
    algorithm has a different keyLen than the previous one. This is to prevent
    potential connectivity disruptions: while accounting for the IPSec overhead
    for the MTU, we consider the default 16B for keyLen, and we correctly adjust
    it depending on the algorithm provided in the IPSec secret. However, this
    adjustment is not performed at runtime upon providing a new key during key rotation.
    
    * For IPv4, we never saw the issue, since during the MTU computation we
      account for the worst case, meanining IPv6, therefore having 20B additional
      that can be "borrowed" for a higher keyLen during key rotation.
    * For IPv6 where the MTU computation is just right, there are no bits to
      borrow. This means that, when providing a new key with greater keyLen,
      the MTU is not dynamically lowered, therefore we see conn disrupt.
    
    This commit also includes the minimal doc change in our IPSec key
    rotation section to state this behavior.
    
    NB: this patch fixes https://github.com/cilium/cilium/issues/29480 by
        preventing this behavior. If we ever plan to support it, we should
        subscribe to MTU discovery and update it accordingly.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

--- pkg/datapath/linux/ipsec/ipsec_linux_test.go
+++ pkg/datapath/linux/ipsec/ipsec_linux_test.go
@@ -40,7 +40,8 @@ func setupIPSecSuitePrivileged(tb testing.TB) *slog.Logger {
 
 var (
 	path           = "ipsec_keys_test"
-	keysDat        = []byte("1 hmac(sha256) 0123456789abcdef0123456789abcdef cbc(aes) 0123456789abcdef0123456789abcdef\n1 hmac(sha256) 0123456789abcdef0123456789abcdef cbc(aes) 0123456789abcdef0123456789abcdef foobar\n1 digest_null \"\" cipher_null \"\"\n")
+	keysDat        = []byte("1 hmac(sha256) 0123456789abcdef0123456789abcdef cbc(aes) 0123456789abcdef0123456789abcdef\n1 hmac(sha256) 0123456789abcdef0123456789abcdef cbc(aes) 0123456789abcdef0123456789abcdef foobar\n")
+	keysNullDat    = []byte("1 digest_null \"\" cipher_null \"\"\n")
 	keysAeadDat    = []byte("6 rfc4106(gcm(aes)) 44434241343332312423222114131211f4f3f2f1 128\n")
 	keysAeadDat256 = []byte("6 rfc4106(gcm(aes)) 44434241343332312423222114131211f4f3f2f144434241343332312423222114131211 128\n")
 	invalidKeysDat = []byte("1 test abcdefghijklmnopqrstuvwzyzABCDEF test abcdefghijklmnopqrstuvwzyzABCDEF\n")
@@ -72,14 +73,23 @@ func TestInvalidLoadKeys(t *testing.T) {
 func TestLoadKeys(t *testing.T) {
 	log := setupIPSecSuitePrivileged(t)
 
-	testCases := [][]byte{keysDat, keysAeadDat, keysAeadDat256}
+	testCases := [][]byte{keysDat, keysNullDat, keysAeadDat, keysAeadDat256}
 	for _, testCase := range testCases {
 		keys := bytes.NewReader(testCase)
 		_, spi, err := LoadIPSecKeys(log, keys)
 		require.NoError(t, err)
 		err = SetIPSecSPI(log, spi)
 		require.NoError(t, err)
+		UnsetTestIPSecKey()
 	}
+}
+
+func TestLoadKeysLenChange(t *testing.T) {
+	log := setupIPSecSuitePrivileged(t)
+
+	keys := bytes.NewReader(append(keysDat, keysNullDat...))
+	_, _, err := LoadIPSecKeys(log, keys)
+	require.ErrorContains(t, err, "invalid key rotation: key length must not change")
 }
 
 func TestParseSPI(t *testing.T) {
