cilium/cilium

commit 2fc52eb444635c83843c45735766db0e338b581c

fix: Adding ipv6 check on the node init function
Cilium pods on a single stack cluster were failing when ipv6 was enabled.
The change would make sure that if the underlying node does not have ipv6
configuration enabled it would fail even if KPR is disabled.Earlier, if KPR
is enabled, the node would not see a fatal error.

Signed-off-by: Vipul Singh <singhvipul@microsoft.com>

daemon/k8s/init.go
@@ -128,6 +128,16 @@ func WaitForNodeInformation(ctx context.Context, log logrus.FieldLogger, localNo
 			logfields.K8sNodeIP:        k8sNodeIP,
 		}).Info("Received own node information from API server")
 
+		// If the host does not have an IPv6 address, return an error
+		if option.Config.EnableIPv6 && nodeIP6 == nil {
+			log.WithFields(logrus.Fields{
+				logfields.NodeName:         n.Name,
+				logfields.IPAddr + ".ipv4": nodeIP4,
+				logfields.IPAddr + ".ipv6": nodeIP6,
+			}).Error("No IPv6 support on node as ipv6 address is nil")
+			return fmt.Errorf("node %s does not have an IPv6 address", n.Name)
+		}
+
 		useNodeCIDR(n)
 	} else {
 		// if node resource could not be received, fail if

commit 9003e85ccf2f3593b0d36c03d68158ca5836583e

fix: allow single-stack IPv6 without address set on the node
In certain IPv6-only setups it is possible for nodes to have working
IPv6 connectivity without having an address set on the node early during
node bootstrap.

The check introduced in 2fc52eb attempts to eliminate inconsistent
behaviour during migrations from single-stack IPv4 to dual-stack network
environments, but inadvertently breaks cluster bootstrapping in some
single-stack IPv6 environments. This commit remedies that by making both
IPv4 and IPv6 single-stack clusters behave the same way and not validating
that an address is set on the node resource.

Fixes: #34861
Fixes: 2fc52eb ("fix: Adding ipv6 check on the node init function")

Signed-off-by: Hex Sattler <hex.sattler@wobcom.de>

daemon/k8s/init.go
@@ -126,8 +126,8 @@ func WaitForNodeInformation(ctx context.Context, log logrus.FieldLogger, localNo
 			logfields.K8sNodeIP:        k8sNodeIP,
 		}).Info("Received own node information from API server")
 
-		// If the host does not have an IPv6 address, return an error
-		if option.Config.EnableIPv6 && nodeIP6 == nil {
+		// If the host does not have an IPv6 address and IPv4 is enabled, return an error
+		if option.Config.EnableIPv4 && option.Config.EnableIPv6 && nodeIP6 == nil {
 			log.WithFields(logrus.Fields{
 				logfields.NodeName:         n.Name,
 				logfields.IPAddr + ".ipv4": nodeIP4,