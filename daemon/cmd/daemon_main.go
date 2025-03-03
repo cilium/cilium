4d8d90726d25421a24a87b6a814578d810db345b
Dylan Reimerink <dylan.reimerink@isovalent.com>
2025-03-03 14:16:26 +0100

    linux/routing: Modernize egress route reconciliation
    In the previous commit we added a crude form of route reconciliation
    that attempts to re-add routes every 30 seconds. The previous commit
    was made to be easily backportable but we can do better in newer
    Cilium versions.
    
    This commit makes a number of improvements. It changes the controller to
    a job with health reporting. Instead of doing periodic reconciliation,
    we only attempt to add routes when a change in the route table occurred.
    We still imit the rate of reconciliation to once every 30 seconds at
    most, but we should not be doing anything if the route table is stable.
    Lastly, we switched from "Replacing" the routes to "Adding" routes. This
    is done so we do not cause update events when the desired routes were
    already in place. Since an update event would re-trigger the controller
    this avoids a feedback loop.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -19,6 +19,7 @@ import (
 	"github.com/cilium/ebpf/asm"
 	"github.com/cilium/ebpf/rlimit"
 	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
 	"github.com/cilium/statedb"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -1533,6 +1534,7 @@ type daemonParams struct {
 	CertManager         certificatemanager.CertificateManager
 	SecretManager       certificatemanager.SecretManager
 	IdentityAllocator   identitycell.CachingIdentityAllocator
+	JobGroup            job.Group
 	Policy              policy.PolicyRepository
 	IPCache             *ipcache.IPCache
 	DirReadStatus       policyDirectory.DirectoryWatcherReadStatus
@@ -1550,6 +1552,7 @@ type daemonParams struct {
 	APILimiterSet       *rate.APILimiterSet
 	AuthManager         *auth.AuthManager
 	Settings            cellSettings
+	Routes              statedb.Table[*datapathTables.Route]
 	Devices             statedb.Table[*datapathTables.Device]
 	NodeAddrs           statedb.Table[datapathTables.NodeAddress]
 	DirectRoutingDevice datapathTables.DirectRoutingDevice
