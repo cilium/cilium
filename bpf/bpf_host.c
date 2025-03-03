798ee391e62f3658227bf77b16a5492895a1a0a7
Julian Wiedmann <jwi@isovalent.com>
2025-03-03 13:08:52 +0200

    bpf: host: identify Cilium's Wireguard traffic as from HOST
    [ upstream commit a321ba46ebf46558d18d943072b8486dfe2f65e7 ]
    
    This improves the information in the trace notifications, and allows us to
    skip the EgressGW policy handling. It also matches the expected security
    identity when performing an ipcache lookup for the source IP.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -1404,7 +1404,7 @@ int cil_to_netdev(struct __ctx_buff *ctx __maybe_unused)
 
 	bpf_clear_meta(ctx);
 
-	if (magic == MARK_MAGIC_HOST || magic == MARK_MAGIC_OVERLAY)
+	if (magic == MARK_MAGIC_HOST || magic == MARK_MAGIC_OVERLAY || ctx_mark_is_wireguard(ctx))
 		src_sec_identity = HOST_ID;
 	else if (magic == MARK_MAGIC_IDENTITY)
 		src_sec_identity = get_identity(ctx);
