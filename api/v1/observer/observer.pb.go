32aaaadea9fb2a007bf3c25078b7b1083a5e3224
Daniel Borkmann <daniel@iogearbox.net>
2025-03-05 14:23:06 +0000

    cilium: Regen hubble api wrt drop codes
    Add DROP_PUNT_PROXY to the flow.proto file and run generate-hubble-api
    to auto-regenerate the drop code table.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- api/v1/observer/observer.pb.go
+++ api/v1/observer/observer.pb.go
@@ -197,6 +197,7 @@ const DropReason_MULTICAST_HANDLED = flow.DropReason_MULTICAST_HANDLED
 const DropReason_DROP_HOST_NOT_READY = flow.DropReason_DROP_HOST_NOT_READY
 const DropReason_DROP_EP_NOT_READY = flow.DropReason_DROP_EP_NOT_READY
 const DropReason_DROP_NO_EGRESS_IP = flow.DropReason_DROP_NO_EGRESS_IP
+const DropReason_DROP_PUNT_PROXY = flow.DropReason_DROP_PUNT_PROXY
 
 var DropReason_name = flow.DropReason_name
 var DropReason_value = flow.DropReason_value
