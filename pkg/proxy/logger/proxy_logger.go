1d23490b16a0e77417c56b6e7eb8ff0893fb33a6
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-03-05 09:57:53 +0100

    proxy: use new access logger instead of calling Log on record
    This commit is moving the method `Log` from the proxy access log record
    to the `ProxyAccessLogger`. This way the global variables `notifier`
    and `metadata` and the corresponding locks can be removed.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/proxy/logger/proxy_logger.go
+++ pkg/proxy/logger/proxy_logger.go
@@ -6,10 +6,17 @@ package logger
 import (
 	"log/slog"
 
+	"github.com/sirupsen/logrus"
+
+	"github.com/cilium/cilium/pkg/flowdebug"
 	"github.com/cilium/cilium/pkg/option"
 )
 
-type ProxyAccessLogger interface{}
+type ProxyAccessLogger interface {
+	// Log logs the given log record to the flow log (if flow debug logging is enabled)
+	// and sends it of to the monitor agent via notifier.
+	Log(lr *LogRecord)
+}
 
 type proxyAccessLogger struct {
 	logger *slog.Logger
@@ -18,10 +25,30 @@ type proxyAccessLogger struct {
 	metadata []string
 }
 
-func newProcyAccessLogger(logger *slog.Logger, config proxyAccessLoggerConfig, notifier LogRecordNotifier) ProxyAccessLogger {
+// LogRecordNotifier is the interface to implement LogRecord notifications.
+// Each type that wants to implement this interface must support concurrent calls
+// to the interface methods.
+// Besides, the number of concurrent calls may be very high, so long critical sections
+// should be avoided (i.e.: avoid using a single lock for slow logging operations).
+type LogRecordNotifier interface {
+	// NewProxyLogRecord is called for each new log record
+	NewProxyLogRecord(l *LogRecord) error
+}
+
+func NewProcyAccessLogger(logger *slog.Logger, config ProxyAccessLoggerConfig, notifier LogRecordNotifier) ProxyAccessLogger {
 	return &proxyAccessLogger{
 		logger:   logger,
 		notifier: notifier,
 		metadata: option.Config.AgentLabels, // TODO: use value from config struct
 	}
+}
+
+func (r *proxyAccessLogger) Log(lr *LogRecord) {
+	flowdebug.Log(func() (*logrus.Entry, string) {
+		return lr.getLogFields(), "Logging flow record"
+	})
+
+	lr.Metadata = r.metadata
+
+	r.notifier.NewProxyLogRecord(lr)
 }
