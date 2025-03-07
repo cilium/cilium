56ffda78b055a7a7ec70adf90fa1ee263773c114
Tam Mach <tam.mach@cilium.io>
2025-03-07 14:47:50 +1100

    connectivity: Compile all pod to ingress tests
    It's easier to maintain all related tests if they are in the same file.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- cilium-cli/connectivity/builder/pod_to_ingress_service.go
+++ cilium-cli/connectivity/builder/pod_to_ingress_service.go
@@ -4,9 +4,22 @@
 package builder
 
 import (
+	_ "embed"
+
 	"github.com/cilium/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium/cilium-cli/connectivity/tests"
 	"github.com/cilium/cilium/cilium-cli/utils/features"
+)
+
+var (
+	//go:embed manifests/allow-ingress-identity.yaml
+	allowIngressIdentityPolicyYAML string
+
+	//go:embed manifests/deny-ingress-backend.yaml
+	denyIngressBackendPolicyYAML string
+
+	//go:embed manifests/deny-ingress-entity.yaml
+	denyIngressIdentityPolicyYAML string
 )
 
 type podToIngressService struct{}
@@ -16,4 +29,34 @@ func (t podToIngressService) build(ct *check.ConnectivityTest, _ map[string]stri
 	newTest("pod-to-ingress-service", ct).
 		WithFeatureRequirements(features.RequireEnabled(features.IngressController)).
 		WithScenarios(tests.PodToIngress())
+
+	newTest("pod-to-ingress-service-allow-ingress-identity", ct).
+		WithFeatureRequirements(features.RequireEnabled(features.IngressController)).
+		WithCiliumPolicy(denyAllIngressPolicyYAML).
+		WithCiliumPolicy(allowIngressIdentityPolicyYAML).
+		WithScenarios(tests.PodToIngress())
+
+	newTest("pod-to-ingress-service-deny-all", ct).
+		WithFeatureRequirements(features.RequireEnabled(features.IngressController)).
+		WithCiliumPolicy(denyAllIngressPolicyYAML).
+		WithScenarios(tests.PodToIngress()).
+		WithExpectations(func(_ *check.Action) (egress check.Result, ingress check.Result) {
+			return check.ResultDefaultDenyEgressDrop, check.ResultNone
+		})
+
+	newTest("pod-to-ingress-service-deny-backend-service", ct).
+		WithFeatureRequirements(features.RequireEnabled(features.IngressController)).
+		WithCiliumPolicy(denyIngressBackendPolicyYAML).
+		WithScenarios(tests.PodToIngress()).
+		WithExpectations(func(_ *check.Action) (egress check.Result, ingress check.Result) {
+			return check.ResultDefaultDenyEgressDrop, check.ResultNone
+		})
+
+	newTest("pod-to-ingress-service-deny-ingress-identity", ct).
+		WithFeatureRequirements(features.RequireEnabled(features.IngressController)).
+		WithCiliumPolicy(denyIngressIdentityPolicyYAML).
+		WithScenarios(tests.PodToIngress()).
+		WithExpectations(func(_ *check.Action) (egress check.Result, ingress check.Result) {
+			return check.ResultDefaultDenyEgressDrop, check.ResultNone
+		})
 }
