e86c65af5af74cb02ddad6092904f9e2600a7625
Julian Wiedmann <jwi@isovalent.com>
2025-03-09 16:00:23 +0100

    Revert "Fix NodePort traffic to host netns backends with Geneve+DSR"
    This reverts commit 614aaaf14c8be8e6c6b1d292868d6a9b9edd9058.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- pkg/node/manager/manager.go
+++ pkg/node/manager/manager.go
@@ -564,7 +564,7 @@ func (m *manager) checkpoint() error {
 	return f.CloseAtomicallyReplace()
 }
 
-func (m *manager) nodeAddressShouldUseTunnel(address nodeTypes.Address) bool {
+func (m *manager) nodeAddressHasTunnelIP(address nodeTypes.Address) bool {
 	// If the host firewall is enabled, all traffic to remote nodes must go
 	// through the tunnel to preserve the source identity as part of the
 	// encapsulation. In encryption case we also want to use vxlan device
@@ -674,13 +674,9 @@ func (m *manager) NodeUpdated(n nodeTypes.Node) {
 			ipsetEntries = append(ipsetEntries, prefix)
 		}
 
-		// Always set the tunnelIP so it can be used for metadata like DSR info
-		var tunnelIP netip.Addr = nodeIP
-
-		// Inform datapath not to use tunnelling for directly reachable endpoints
-		endpointFlags := ipcacheTypes.EndpointFlags{}
-		if !m.nodeAddressShouldUseTunnel(address) {
-			endpointFlags.SetSkipTunnel(true)
+		var tunnelIP netip.Addr
+		if m.nodeAddressHasTunnelIP(address) {
+			tunnelIP = nodeIP
 		}
 
 		var key uint8
@@ -719,8 +715,7 @@ func (m *manager) NodeUpdated(n nodeTypes.Node) {
 		m.ipcache.UpsertMetadata(prefix, n.Source, resource,
 			lbls,
 			ipcacheTypes.TunnelPeer{Addr: tunnelIP},
-			ipcacheTypes.EncryptKey(key),
-			endpointFlags)
+			ipcacheTypes.EncryptKey(key))
 		if nodeIdentityOverride {
 			m.ipcache.OverrideIdentity(prefix, nodeLabels, n.Source, resource)
 		}
@@ -885,11 +880,9 @@ func (m *manager) removeNodeFromIPCache(oldNode nodeTypes.Node, resource ipcache
 			}
 		}
 
-		var oldTunnelIP netip.Addr = oldNodeIP
-
-		oldEndpointFlags := ipcacheTypes.EndpointFlags{}
-		if !m.nodeAddressShouldUseTunnel(address) {
-			oldEndpointFlags.SetSkipTunnel(true)
+		var oldTunnelIP netip.Addr
+		if m.nodeAddressHasTunnelIP(address) {
+			oldTunnelIP = oldNodeIP
 		}
 
 		var oldKey uint8
@@ -900,8 +893,7 @@ func (m *manager) removeNodeFromIPCache(oldNode nodeTypes.Node, resource ipcache
 		m.ipcache.RemoveMetadata(oldPrefix, resource,
 			oldNodeLabels,
 			ipcacheTypes.TunnelPeer{Addr: oldTunnelIP},
-			ipcacheTypes.EncryptKey(oldKey),
-			oldEndpointFlags)
+			ipcacheTypes.EncryptKey(oldKey))
 		if oldNodeIdentityOverride {
 			m.ipcache.RemoveIdentityOverride(oldPrefix, oldNodeLabels, resource)
 		}
