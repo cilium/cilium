d6db3efdf5d27ca3ee044e20590a9240c75a781b
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-03-11 13:19:13 +0100

    proxy: move pkg/proxy/logger into pkg/proxy/accesslog
    This commit moves the content of package `pkg/proxy/logger` into
    `pkg/proxy/accesslog`. These two packages belong together.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/proxy/accesslog/monitoragent_lognotifier.go
+++ pkg/proxy/accesslog/monitoragent_lognotifier.go
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package logger
+package accesslog
 
 import (
 	"fmt"
 
 	monitoragent "github.com/cilium/cilium/pkg/monitor/agent"
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
-	"github.com/cilium/cilium/pkg/proxy/accesslog"
 )
 
 type monitorAgentLogRecordNotifier struct {
@@ -19,7 +18,7 @@ func newMonitorAgentLogRecordNotifier(monitorAgent monitoragent.Agent) LogRecord
 	return &monitorAgentLogRecordNotifier{monitorAgent: monitorAgent}
 }
 
-func (m *monitorAgentLogRecordNotifier) NewProxyLogRecord(l *accesslog.LogRecord) error {
+func (m *monitorAgentLogRecordNotifier) NewProxyLogRecord(l *LogRecord) error {
 	// Note: important to pass the event as value
 	if err := m.monitorAgent.SendEvent(monitorAPI.MessageTypeAccessLog, *l); err != nil {
 		return fmt.Errorf("failed to send log record to monitor agent: %w", err)
