cilium/cilium

commit a7f4ba11026c9b9a5fd11a37e275413aea5b4778

cli: Load code owners dynamically via --code-owners
Embedding the code owners into the CLI from the top of the tree was a
nice trick, but it's a bit restrictive. Looking forward, we would like
to have the review code owners diverge from test owners in some narrow
use cases such as on stable branches or on the cilium-cli repository
where the paths differ from the main cilium tree. As such, it would be
useful to be able to override the existing code owners rule sets by
specifying the path to one or more CODEOWNERS-style files on the
command-line when running these tests.

Subsequent commits will introduce the GitHub workflow changes to
leverage these changes.

Signed-off-by: Joe Stringer <joe@cilium.io>

cilium-cli/cli/connectivity.go
@@ -17,12 +17,12 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 
-	assets "github.com/cilium/cilium"
 	"github.com/cilium/cilium/cilium-cli/api"
 	"github.com/cilium/cilium/cilium-cli/connectivity"
 	"github.com/cilium/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium/cilium-cli/defaults"
 	"github.com/cilium/cilium/cilium-cli/sysdump"
+	owners_util "github.com/cilium/cilium/cilium-cli/utils/codeowners"
 	"github.com/cilium/cilium/cilium-cli/utils/features"
 	"github.com/cilium/cilium/pkg/option"
 )
@@ -95,12 +95,12 @@ func RunE(hooks api.Hooks) func(cmd *cobra.Command, args []string) error {
 			return nil
 		}
 
-		logger := check.NewConcurrentLogger(params.Writer, params.TestConcurrency)
-		owners, err := codeowners.ParseFile(strings.NewReader(assets.CodeOwnersRaw))
+		owners, err := owners_util.Load(params.CodeOwners)
 		if err != nil {
-			return fmt.Errorf("🐛 Failed to parse CODEOWNERS. Developer BUG? %w", err)
+			return fmt.Errorf("❗ Failed to load code owners: %w", err)
 		}
 
+		logger := check.NewConcurrentLogger(params.Writer, params.TestConcurrency)
 		connTests, err := newConnectivityTests(params, hooks, logger, owners)
 		if err != nil {
 			return err
@@ -207,6 +207,8 @@ func newCmdConnectivityTest(hooks api.Hooks) *cobra.Command {
 	cmd.Flags().StringSliceVar(&params.ExpectedXFRMErrors, "expected-xfrm-errors", defaults.ExpectedXFRMErrors, "List of expected XFRM errors")
 	cmd.Flags().MarkHidden("expected-xfrm-errors")
 
+	cmd.Flags().StringSliceVar(&params.CodeOwners, "code-owners", []string{}, "Use the code owners defined in these files for --log-code-owners")
+	cmd.Flags().MarkHidden("code-owners")
 	cmd.Flags().BoolVar(&params.LogCodeOwners, "log-code-owners", defaults.LogCodeOwners, "Log code owners for tests that fail")
 	cmd.Flags().MarkHidden("log-code-owners")
 	cmd.Flags().StringSliceVar(&params.ExcludeCodeOwners, "exclude-code-owners", []string{}, "Exclude specific code owners from --log-code-owners")

CODEOWNERS
@@ -272,7 +272,6 @@
 /api/v1/peer/ @cilium/sig-hubble-api
 /api/v1/recorder/ @cilium/sig-hubble-api
 /api/v1/relay/ @cilium/sig-hubble-api
-/assets.go @cilium/sig-agent
 /bpf/ @cilium/sig-datapath
 /bpf/lib/egress_gateway.h @cilium/egress-gateway
 Makefile* @cilium/build

assets.go
@@ -1,12 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-// Cilium provides access to top-level files in the tree for Cilium development.
-package cilium
-
-import (
-	_ "embed"
-)
-
-//go:embed CODEOWNERS
-var CodeOwnersRaw string

cilium-cli/cli/connectivity_test.go
@@ -5,16 +5,14 @@ package cli
 
 import (
 	"bytes"
-	"strings"
 	"testing"
 
-	"github.com/hmarr/codeowners"
 	"github.com/stretchr/testify/require"
 
-	assets "github.com/cilium/cilium"
 	"github.com/cilium/cilium/cilium-cli/api"
 	"github.com/cilium/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium/cilium-cli/defaults"
+	"github.com/cilium/cilium/cilium-cli/utils/codeowners"
 )
 
 func TestNewConnectivityTests(t *testing.T) {
@@ -79,7 +77,7 @@ func TestNewConnectivityTests(t *testing.T) {
 		},
 	}
 	for _, tt := range testCases {
-		owners, err := codeowners.ParseFile(strings.NewReader(assets.CodeOwnersRaw))
+		owners, err := codeowners.Load([]string{})
 		if err != nil {
 			t.Fatalf("🐛 Failed to parse CODEOWNERS. Developer BUG? %s", err)
 		}

cilium-cli/connectivity/check/check.go
@@ -121,6 +121,7 @@ type Parameters struct {
 	ExpectedDropReasons []string
 	ExpectedXFRMErrors  []string
 
+	CodeOwners        []string
 	LogCodeOwners     bool
 	ExcludeCodeOwners []string
 	LogCheckLevels    []string

commit edf6f31728ae79faf0efa78980fdf8de78e8c92b

cilium-cli: Allow running outside the Cilium tree
The last change in this area baked in a hard expectation that the CLI is
run inside the Cilium tree, but this doesn't make sense. Relax it so the
code owners are only loaded when --log-code-owners is passed on the
command line.

Fixes: a7f4ba11026c ("cli: Load code owners dynamically via --code-owners")
Reported-by: Marco Iorio <marco.iorio@isovalent.com>
Signed-off-by: Joe Stringer <joe@cilium.io>

cilium-cli/cli/connectivity.go
@@ -95,9 +95,14 @@ func RunE(hooks api.Hooks) func(cmd *cobra.Command, args []string) error {
 			return nil
 		}
 
-		owners, err := owners_util.Load(params.CodeOwners)
-		if err != nil {
-			return fmt.Errorf("❗ Failed to load code owners: %w", err)
+		var owners codeowners.Ruleset
+		if params.LogCodeOwners {
+			var err error
+
+			owners, err = owners_util.Load(params.CodeOwners)
+			if err != nil {
+				return fmt.Errorf("❗ Failed to load code owners: %w", err)
+			}
 		}
 
 		logger := check.NewConcurrentLogger(params.Writer, params.TestConcurrency)