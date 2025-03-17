1a393fd262118edd4455af120290d40ef8befea3
Tam Mach <tam.mach@cilium.io>
2025-03-17 23:46:57 +1100

    endpoint: Add policy manager callback
    This commit is to extend existing EndpointManager component to provide
    the callback when the policy update happens:
    
    - All Endpoint Regeneration
    - UpdatePolicyMaps function call for regen IDs
    - ApplyPolicyMaps changes
    
    The callback is non-blocking call, so that we don't hold the main
    processing flow.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

---
+++ pkg/endpointmanager/callback_test.go
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package endpointmanager
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cilium/cilium/pkg/container/set"
+	"github.com/cilium/cilium/pkg/identity"
+)
+
+const numberOfEndpointPolicies = 10
+
+func Test_PolicyUpdateCallback(t *testing.T) {
+	mgr := New(&dummyEpSyncher{}, nil, nil, nil, nil)
+	called := int32(0)
+	updateFunc := func(idsRegen *set.Set[identity.NumericIdentity], incremental bool) error {
+		atomic.AddInt32(&called, 1)
+		return nil
+	}
+
+	for i := range numberOfEndpointPolicies {
+		mgr.RegisterPolicyUpdateCallback(fmt.Sprintf("callback-%d", i), updateFunc)
+	}
+	wg := sync.WaitGroup{}
+	mgr.policyUpdateCallback(&wg, nil, false)
+	wg.Wait()
+	require.Equal(t, int32(10), called)
+
+	for i := range numberOfEndpointPolicies {
+		mgr.DeregisterPolicyUpdateCallback(fmt.Sprintf("callback-%d", i))
+	}
+	wg = sync.WaitGroup{}
+	mgr.policyUpdateCallback(&wg, nil, false)
+	wg.Wait()
+	require.Equal(t, int32(10), called)
+}
