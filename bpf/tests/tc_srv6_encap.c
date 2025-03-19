d4cdac41b1099ddb545c8d39a242d7b422733f83
Jarno Rajahalme <jarno@isovalent.com>
2025-03-19 10:38:18 +0100

    bpf/tests: Fix misaligned stack access
    bpf verifier complained about misaligned stack access:
    
    --- FAIL: TestBPF (10.77s)
        --- FAIL: TestBPF/tc_srv6_encap.o (0.16s)
            bpf_test.go:166: verifier error: load program: permission denied:
                    0: R1=ctx() R10=fp0
                    ; int srv6_encap_from_pod_ipv6_setup(struct __ctx_buff *ctx __maybe_unused) @ tc_srv6_encap.c:235
                    0: (bf) r6 = r1                       ; R1=ctx() R6_w=ctx()
                    1: (b4) w1 = 128                      ; R1_w=128
                    ; struct srv6_vrf_key6 vrf_key = { @ tc_srv6_encap.c:237
                    2: (63) *(u32 *)(r10 -64) = r1        ; R1_w=128 R10=fp0 fp-64=????128
                    3: (b4) w1 = 1                        ; R1_w=1
                    ; __u32 vrf_id = 1; @ tc_srv6_encap.c:240
                    4: (63) *(u32 *)(r10 -68) = r1        ; R1_w=1 R10=fp0 fp-72=mmmm????
                    5: (18) r1 = 0x1000000a0              ; R1_w=0x1000000a0
                    ; struct srv6_policy_key6 policy_key = { @ tc_srv6_encap.c:241
                    7: (7b) *(u64 *)(r10 -96) = r1        ; R1_w=0x1000000a0 R10=fp0 fp-96_w=0x1000000a0
                    8: (b7) r1 = 1277                     ; R1_w=1277
                    ; case  8: jmp_8:  __it_mob(d, s, 64); @ builtins.h:177
                    9: (7b) *(u64 *)(r10 -60) = r1
                    misaligned stack access off 0+0+-60 size 8
                    processed 9 insns (limit 1000000) max_states_per_insn 0 total_states 0 peak_states 0 mark_read 0
    
    This happens since `union v6addr` is packed and thus misaligned in
    'struct srv6_vrf_key6', so the memcpy to it, when placed in stack,
    becomes misaligned.
    
    This is fixes in the test code by defining an array of two
    srv6_vrf_key6's, and using the second one that is now always 8-byte
    aligned since the array has the __align_stack_8 attribute.
    
    In my local testing using the 1st array element reliably reproduces the
    misaligned stack access error from the verifier, while using the 2nd one
    never does.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- bpf/tests/tc_srv6_encap.c
+++ bpf/tests/tc_srv6_encap.c
@@ -234,19 +234,27 @@ int srv6_encap_from_pod_ipv6_pktgen(struct __ctx_buff *ctx)
 SETUP("tc", "tc_srv6_encap_from_pod_ipv6")
 int srv6_encap_from_pod_ipv6_setup(struct __ctx_buff *ctx __maybe_unused)
 {
-	struct srv6_vrf_key6 vrf_key = {
-		.lpm = {SRV6_VRF_STATIC_PREFIX6, {} },
+	/*
+	 * struct srv6_vrf_key6 has misaligned packed v6addr fields as its ast members, that can not
+	 * be memcpy'd into on the stack. Force 8-byte alignment on the v6addr fields by using two
+	 * of keys so the 2nd one has 8-byte aligned v6addr fields.
+	 */
+	struct srv6_vrf_key6 vrf_key[2] __align_stack_8 = {
+		{},
+		{
+			.lpm = {SRV6_VRF_STATIC_PREFIX6, {} }
+		},
 	};
-	struct srv6_policy_key6 policy_key = {
+	struct srv6_policy_key6 policy_key __align_stack_8 = {
 		.lpm = {SRV6_POLICY_STATIC_PREFIX6 + 128, {} },
 		.vrf_id = 1,
 	};
 	union v6addr sid;
 	__u32 vrf_id = 1;
 
-	memcpy(&vrf_key.src_ip, (const void *)POD_IPV6, sizeof(union v6addr));
-	memset(&vrf_key.dst_cidr, 0, sizeof(union v6addr));
-	map_update_elem(&cilium_srv6_vrf_v6, &vrf_key, &vrf_id, 0);
+	memcpy(&vrf_key[1].src_ip, (const void *)POD_IPV6, sizeof(union v6addr));
+	memset(&vrf_key[1].dst_cidr, 0, sizeof(union v6addr));
+	map_update_elem(&cilium_srv6_vrf_v6, &vrf_key[1], &vrf_id, 0);
 
 	memcpy(&policy_key.dst_cidr, (const void *)EXT_IPV6, sizeof(union v6addr));
 	memcpy(&sid, (const void *)SID, sizeof(sid));
