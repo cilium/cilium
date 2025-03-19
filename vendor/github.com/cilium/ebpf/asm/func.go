03a2d94b4ada323c639512a817a3e78fa5f2833d
Joe Stringer <joe@cilium.io>
2025-03-19 09:48:33 -0700

    vendor: Revert cilium/ebpf to v0.17.1
    The Cilium eBPF library v0.17.2 broke CLI compilation on the main branch:
    
        $ make -C cilium-cli local-release
        ...
        Building release binary for darwin/amd64...
        # github.com/cilium/cilium/pkg/datapath/linux/probes
        Error: ../pkg/datapath/linux/probes/attach_type.go:28:21: undefined: features.HaveProgramType
        Error: ../pkg/datapath/linux/probes/probes.go:351:18: undefined: features.HaveProgramHelper
        Error: ../pkg/datapath/linux/probes/probes.go:365:18: undefined: features.HaveLargeInstructions
        Error: ../pkg/datapath/linux/probes/probes.go:379:18: undefined: features.HaveBoundedLoops
        Error: ../pkg/datapath/linux/probes/probes.go:393:18: undefined: features.HaveProgramHelper
        Error: ../pkg/datapath/linux/probes/probes.go:400:18: undefined: features.HaveProgramHelper
        Error: ../pkg/datapath/linux/probes/probes.go:407:18: undefined: features.HaveV2ISA
        Error: ../pkg/datapath/linux/probes/probes.go:421:18: undefined: features.HaveV3ISA
    
    Revert to the older version until it is fixed.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- vendor/github.com/cilium/ebpf/asm/func.go
+++ vendor/github.com/cilium/ebpf/asm/func.go
@@ -5,6 +5,10 @@ package asm
 // BuiltinFunc is a built-in eBPF function.
 type BuiltinFunc int32
 
+func (_ BuiltinFunc) Max() BuiltinFunc {
+	return maxBuiltinFunc - 1
+}
+
 // eBPF built-in functions
 //
 // You can regenerate this list using the following gawk script:
@@ -233,6 +237,8 @@ const (
 	FnUserRingbufDrain
 	FnCgrpStorageGet
 	FnCgrpStorageDelete
+
+	maxBuiltinFunc
 )
 
 // Call emits a function call.
