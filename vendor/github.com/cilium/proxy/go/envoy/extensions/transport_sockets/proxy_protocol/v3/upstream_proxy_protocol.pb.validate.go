299dbde8456a26fe3bc145cb71ff6f4e16316ff7
Jarno Rajahalme <jarno@isovalent.com>
2025-03-25 20:26:46 +0100

    vendor: Update API for BpfMetadata filter update
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- vendor/github.com/cilium/proxy/go/envoy/extensions/transport_sockets/proxy_protocol/v3/upstream_proxy_protocol.pb.validate.go
+++ vendor/github.com/cilium/proxy/go/envoy/extensions/transport_sockets/proxy_protocol/v3/upstream_proxy_protocol.pb.validate.go
@@ -126,6 +126,10 @@ func (m *ProxyProtocolUpstreamTransport) validate(all bool) error {
 		}
 	}
 
+	// no validation rules for AllowUnspecifiedAddress
+
+	// no validation rules for TlvAsPoolKey
+
 	if len(errors) > 0 {
 		return ProxyProtocolUpstreamTransportMultiError(errors)
 	}
