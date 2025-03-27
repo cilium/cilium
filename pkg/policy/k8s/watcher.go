2acb38564062f1998459a2dbb5111f00d8b0b93c
Casey Callendrello <cdc@isovalent.com>
2025-03-27 14:21:59 +0100

    policy: don't drop policy update notifications on full channel
    Out of paranoia, we were dropping policy update notifications if the
    channel was full. However, this caused a bug where agents failed to
    start when the cluster contained large numbers of policies. This was
    because we didn't get the feedback that all policies had been ingested,
    thus never marked k8s resources as synced.
    
    Since the policy importer doesn't hold any locks, this paranoia was
    misplaced. Dropping a notification was, in fact, a serious correctness
    issue.
    
    Fixes: #38070
    
    Signed-off-by: Casey Callendrello <cdc@isovalent.com>

--- pkg/policy/k8s/watcher.go
+++ pkg/policy/k8s/watcher.go
@@ -73,13 +73,13 @@ func (p *policyWatcher) watchResources(ctx context.Context) {
 	// Only used during initialization
 	var knpDone, cnpDone, ccnpDone chan uint64
 	if p.config.EnableK8sNetworkPolicy {
-		knpDone = make(chan uint64, 100)
+		knpDone = make(chan uint64, 1024)
 	}
 	if p.config.EnableCiliumNetworkPolicy {
-		cnpDone = make(chan uint64, 100)
+		cnpDone = make(chan uint64, 1024)
 	}
 	if p.config.EnableCiliumClusterwideNetworkPolicy {
-		ccnpDone = make(chan uint64, 100)
+		ccnpDone = make(chan uint64, 1024)
 	}
 
 	// Consume result channels, decrement outstanding work counter.
