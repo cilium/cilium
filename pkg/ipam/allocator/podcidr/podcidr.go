cilium/cilium

commit 1fb88a1572f927c4c2a77981aa9e6bff5cbc5e39

pkg/{alibabacloud,aws,azure,ipam}: migrate to slog
Migrate this package to use slog instead of logrus.

Signed-off-by: André Martins <andre@cilium.io>

pkg/ipam/allocator/podcidr/podcidr.go
@@ -6,10 +6,11 @@ package podcidr
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"net"
 	"strings"
 
-	"github.com/sirupsen/logrus"
+	"github.com/cloudflare/cfssl/log"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/cilium/cilium/pkg/cidr"
@@ -19,7 +20,6 @@ import (
 	"github.com/cilium/cilium/pkg/ipam/allocator/clusterpool/cidralloc"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/lock"
-	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/revert"
 	"github.com/cilium/cilium/pkg/time"
@@ -34,8 +34,6 @@ const (
 )
 
 var (
-	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "pod-cidr")
-
 	ciliumNodesPodCidrControllerGroup = controller.NewGroup("update-cilium-nodes-pod-cidr")
 )
 
@@ -151,6 +149,7 @@ var updateK8sInterval = 15 * time.Second
 // NodesPodCIDRManager will be used to manage podCIDRs for the nodes in the
 // cluster.
 type NodesPodCIDRManager struct {
+	logger              *slog.Logger
 	k8sReSyncController *controller.Manager
 	k8sReSync           *trigger.Trigger
 
@@ -185,11 +184,13 @@ type NodesPodCIDRManager struct {
 // nodeGetter will be used to populate synced node status / spec with
 // kubernetes.
 func NewNodesPodCIDRManager(
+	logger *slog.Logger,
 	v4Allocators, v6Allocators []cidralloc.CIDRAllocator,
 	nodeGetter ipam.CiliumNodeGetterUpdater,
 	triggerMetrics trigger.MetricsObserver) *NodesPodCIDRManager {
 
 	n := &NodesPodCIDRManager{
+		logger:              logger,
 		nodesToAllocate:     map[string]*v2.CiliumNode{},
 		v4CIDRAllocators:    v4Allocators,
 		v6CIDRAllocators:    v6Allocators,
@@ -212,7 +213,7 @@ func NewNodesPodCIDRManager(
 					DoFunc: func(context.Context) error {
 						n.Mutex.Lock()
 						defer n.Mutex.Unlock()
-						return syncToK8s(nodeGetter, n.ciliumNodesToK8s)
+						return syncToK8s(n.logger, nodeGetter, n.ciliumNodesToK8s)
 					},
 					RunInterval: updateK8sInterval,
 				},
@@ -236,14 +237,11 @@ func NewNodesPodCIDRManager(
 // In case any of the nodes failed to be synced with kubernetes the returned
 // error is for one of those nodes. Remaining nodes will still be synced with
 // kubernetes.
-func syncToK8s(nodeGetterUpdater ipam.CiliumNodeGetterUpdater, ciliumNodesToK8s map[string]*ciliumNodeK8sOp) (retErr error) {
+func syncToK8s(logger *slog.Logger, nodeGetterUpdater ipam.CiliumNodeGetterUpdater, ciliumNodesToK8s map[string]*ciliumNodeK8sOp) (retErr error) {
 	for nodeName, nodeToK8s := range ciliumNodesToK8s {
 		var (
 			err, err2     error
 			newCiliumNode *v2.CiliumNode
-			log           = log.WithFields(logrus.Fields{
-				"node-name": nodeName,
-			})
 		)
 		switch nodeToK8s.op {
 		case k8sOpCreate:
@@ -252,7 +250,7 @@ func syncToK8s(nodeGetterUpdater ipam.CiliumNodeGetterUpdater, ciliumNodesToK8s 
 		case k8sOpUpdate:
 			var updatedNode *v2.CiliumNode
 			updatedNode, err = nodeGetterUpdater.Update(nil, nodeToK8s.ciliumNode)
-			log.WithError(err).Debug("Updated Node")
+			log.Debug("Updated Node", logfields.Error, err, logfields.NodeName, nodeName)
 			if err != nil {
 				if k8sErrors.IsNotFound(err) {
 					// In case the node was not found we should not try to re-create
@@ -270,7 +268,7 @@ func syncToK8s(nodeGetterUpdater ipam.CiliumNodeGetterUpdater, ciliumNodesToK8s 
 			fallthrough
 		case k8sOpUpdateStatus:
 			_, err = nodeGetterUpdater.UpdateStatus(nil, nodeToK8s.ciliumNode)
-			log.WithError(err).Debug("UpdatedStatus Node")
+			log.Debug("UpdatedStatus Node", logfields.Error, err, logfields.NodeName, nodeName)
 			switch {
 			case k8sErrors.IsNotFound(err):
 				// In case the node was not found we should not try to re-create
@@ -289,7 +287,11 @@ func syncToK8s(nodeGetterUpdater ipam.CiliumNodeGetterUpdater, ciliumNodesToK8s 
 				// already be deleted from k8s.
 				err = nil
 			} else {
-				log.WithError(err).Warn("Received a CiliumNode delete event, but the resource may not have been deleted (see error).")
+				logger.Warn(
+					"Received a CiliumNode delete event, but the resource may not have been deleted (see error).",
+					logfields.Error, err,
+					logfields.NodeName, nodeName,
+				)
 			}
 		}
 		switch {
@@ -423,10 +425,6 @@ func (n *NodesPodCIDRManager) Resync(context.Context, time.Time) {
 func (n *NodesPodCIDRManager) allocateNode(node *v2.CiliumNode) (cn *v2.CiliumNode, allocated, updateStatus bool, err error) {
 	var cidrs *nodeCIDRs
 
-	log = log.WithFields(logrus.Fields{
-		"node-name": node.Name,
-	})
-
 	defer func() {
 		// Overwrite err value if we want to update the status of the
 		// cilium node into kubernetes.
@@ -442,7 +440,7 @@ func (n *NodesPodCIDRManager) allocateNode(node *v2.CiliumNode) (cn *v2.CiliumNo
 		// If we can't allocate podCIDRs for now we should store the node
 		// temporarily until n.reSync is called.
 		if !n.canAllocatePodCIDRs {
-			log.Debug("Postponing CIDR allocation")
+			n.logger.Debug("Postponing CIDR allocation", logfields.NodeName, node.Name)
 			n.nodesToAllocate[node.GetName()] = node
 			return nil, false, false, nil
 		}
@@ -455,10 +453,12 @@ func (n *NodesPodCIDRManager) allocateNode(node *v2.CiliumNode) (cn *v2.CiliumNo
 			return
 		}
 
-		log.WithFields(logrus.Fields{
-			"cidrs":     cidrs.String(),
-			"allocated": allocated,
-		}).Debug("Allocated new CIDRs")
+		n.logger.Debug(
+			"Allocated new CIDRs",
+			logfields.NodeName, node.Name,
+			logfields.CIDRs, cidrs,
+			logfields.Allocated, allocated,
+		)
 	} else {
 		cidrs, err = parsePodCIDRs(node.Spec.IPAM.PodCIDRs)
 		if err != nil {
@@ -475,11 +475,13 @@ func (n *NodesPodCIDRManager) allocateNode(node *v2.CiliumNode) (cn *v2.CiliumNo
 			updateStatus = true
 			return
 		}
-		log.WithFields(logrus.Fields{
-			"cidrs":                 cidrs.String(),
-			"allocated":             allocated,
-			"n.canAllocatePodCIDRs": n.canAllocatePodCIDRs,
-		}).Debug("Allocated existing CIDRs")
+		n.logger.Debug(
+			"Allocated existing CIDRs",
+			logfields.NodeName, node.Name,
+			logfields.CIDRs, cidrs,
+			logfields.Allocated, allocated,
+			logfields.CanAllocatePodCIDRs, n.canAllocatePodCIDRs,
+		)
 		if !allocated {
 			// If we can't allocate podCIDRs for now we should store the node
 			// temporarily until n.reSync is called.
@@ -540,17 +542,13 @@ func (n *NodesPodCIDRManager) releaseIPNets(nodeName string) bool {
 
 	delete(n.nodes, nodeName)
 
-	log = log.WithFields(logrus.Fields{
-		"node-name": nodeName,
-	})
-
-	releaseCIDRs(n.v4CIDRAllocators, cidrs.v4PodCIDRs)
-	releaseCIDRs(n.v6CIDRAllocators, cidrs.v6PodCIDRs)
+	n.releaseCIDRs(n.v4CIDRAllocators, cidrs.v4PodCIDRs)
+	n.releaseCIDRs(n.v6CIDRAllocators, cidrs.v6PodCIDRs)
 
 	return true
 }
 
-func releaseCIDRs(cidrAllocators []cidralloc.CIDRAllocator, cidrsToRelease []*net.IPNet) {
+func (n *NodesPodCIDRManager) releaseCIDRs(cidrAllocators []cidralloc.CIDRAllocator, cidrsToRelease []*net.IPNet) {
 	if len(cidrAllocators) == 0 {
 		return
 	}
@@ -560,14 +558,14 @@ func releaseCIDRs(cidrAllocators []cidralloc.CIDRAllocator, cidrsToRelease []*ne
 				continue
 			}
 			err := clusterCIDR.Release(ipNet)
-			log = log.WithFields(logrus.Fields{
-				"cidr": ipNet.String(),
-			})
 			if err != nil {
-				log.WithError(err).Error("failed to release cidr")
+				n.logger.Error("failed to release cidr",
+					logfields.Error, err,
+					logfields.CIDR, ipNet,
+				)
 				continue
 			}
-			log.Info("node released cidrs")
+			n.logger.Info("node released CIDRs", logfields.CIDR, ipNet)
 			break
 		}
 	}
@@ -584,9 +582,6 @@ func (n *NodesPodCIDRManager) reuseIPNets(
 ) (
 	newNodeCIDRs *nodeCIDRs, allocated bool, err error,
 ) {
-	log = log.WithFields(logrus.Fields{
-		"node-name": nodeName,
-	})
 	if len(n.v4CIDRAllocators) == 0 && len(v4CIDR) != 0 {
 		return nil, false, &ErrAllocatorNotFound{
 			cidr:          v4CIDR,
@@ -678,7 +673,10 @@ func (n *NodesPodCIDRManager) reuseIPNets(
 		}
 		revertStack.Push(revertFunc)
 		oldNodeCIDRs.v4PodCIDRs = v4CIDR
-		log.Debugf("Allocated v4CIDR %s", v4CIDR)
+		n.logger.Debug(
+			"Allocated v4CIDR",
+			logfields.NodeName, nodeName,
+		)
 		allocated = true
 	}
 
@@ -702,7 +700,10 @@ func (n *NodesPodCIDRManager) reuseIPNets(
 		}
 		revertStack.Push(revertFunc)
 		oldNodeCIDRs.v6PodCIDRs = v6CIDR
-		log.Debugf("Allocated v6CIDR %s", v6CIDR)
+		n.logger.Debug(
+			"Allocated v6CIDR",
+			logfields.NodeName, nodeName,
+		)
 		allocated = true
 	}
 
@@ -828,7 +829,7 @@ func (n *NodesPodCIDRManager) allocateNext(nodeName string) (*nodeCIDRs, bool, e
 			return nil, false, err
 		}
 
-		log.WithField("CIDR", v4CIDR).Debug("v4 allocated CIDR")
+		n.logger.Debug("v4 allocated CIDR", logfields.CIDR, v4CIDR)
 		cidrs.v4PodCIDRs = []*net.IPNet{v4CIDR}
 
 		revertStack.Push(revertFunc)
@@ -839,7 +840,7 @@ func (n *NodesPodCIDRManager) allocateNext(nodeName string) (*nodeCIDRs, bool, e
 			return nil, false, err
 		}
 
-		log.WithField("CIDR", v6CIDR).Debug("v6 allocated CIDR")
+		n.logger.Debug("v6 allocated CIDR", logfields.CIDR, v6CIDR)
 		cidrs.v6PodCIDRs = []*net.IPNet{v6CIDR}
 
 		revertStack.Push(revertFunc)

operator/cmd/allocator_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cilium/hive/hivetest"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
@@ -86,7 +87,7 @@ func podCIDRAllocatorOverlapTestRun(t *testing.T) {
 	}
 
 	// Create a new pod manager with only our IPv4 allocator and fake client set.
-	podCidrManager := podcidr.NewNodesPodCIDRManager([]cidralloc.CIDRAllocator{
+	podCidrManager := podcidr.NewNodesPodCIDRManager(hivetest.Logger(t), []cidralloc.CIDRAllocator{
 		set,
 	}, nil, &ciliumNodeUpdateImplementation{clientset: fakeSet}, nil)
 

operator/cmd/root.go
@@ -25,7 +25,7 @@ import (
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 
 	operatorApi "github.com/cilium/cilium/api/v1/operator/server"
-	troubleshoot "github.com/cilium/cilium/cilium-dbg/cmd/troubleshoot"
+	"github.com/cilium/cilium/cilium-dbg/cmd/troubleshoot"
 	"github.com/cilium/cilium/operator/api"
 	"github.com/cilium/cilium/operator/auth"
 	"github.com/cilium/cilium/operator/doublewrite"
@@ -580,7 +580,7 @@ func (legacy *legacyOnLeader) onStart(_ cell.HookContext) error {
 			log.Fatalf("%s allocator is not supported by this version of %s", ipamMode, binaryName)
 		}
 
-		if err := alloc.Init(legacy.ctx); err != nil {
+		if err := alloc.Init(legacy.ctx, logging.DefaultSlogLogger); err != nil {
 			log.WithError(err).Fatalf("Unable to init %s allocator", ipamMode)
 		}
 

pkg/alibabacloud/eni/instances.go
@@ -5,8 +5,7 @@ package eni
 
 import (
 	"context"
-
-	"github.com/sirupsen/logrus"
+	"log/slog"
 
 	eniTypes "github.com/cilium/cilium/pkg/alibabacloud/eni/types"
 	"github.com/cilium/cilium/pkg/alibabacloud/types"
@@ -14,6 +13,7 @@ import (
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/time"
 )
 
@@ -36,6 +36,8 @@ type AlibabaCloudAPI interface {
 // InstancesManager maintains the list of instances. It must be kept up to date
 // by calling resync() regularly.
 type InstancesManager struct {
+	logger *slog.Logger
+
 	// resyncLock ensures instance incremental resync do not run at the same time as a full API resync
 	resyncLock lock.RWMutex
 
@@ -49,8 +51,9 @@ type InstancesManager struct {
 }
 
 // NewInstancesManager returns a new instances manager
-func NewInstancesManager(api AlibabaCloudAPI) *InstancesManager {
+func NewInstancesManager(logger *slog.Logger, api AlibabaCloudAPI) *InstancesManager {
 	return &InstancesManager{
+		logger:    logger.With(subsysLogAttr...),
 		instances: ipamTypes.NewInstanceMap(),
 		api:       api,
 	}
@@ -58,7 +61,7 @@ func NewInstancesManager(api AlibabaCloudAPI) *InstancesManager {
 
 // CreateNode
 func (m *InstancesManager) CreateNode(obj *v2.CiliumNode, node *ipam.Node) ipam.NodeOperations {
-	return &Node{k8sObj: obj, manager: m, node: node, instanceID: node.InstanceID()}
+	return &Node{logger: m.logger, k8sObj: obj, manager: m, node: node, instanceID: node.InstanceID()}
 }
 
 // HasInstance returns whether the instance is in instances
@@ -107,19 +110,19 @@ func (m *InstancesManager) resync(ctx context.Context, instanceID string) time.T
 
 	vpcs, err := m.api.GetVPCs(ctx)
 	if err != nil {
-		log.WithError(err).Warning("Unable to synchronize VPC list")
+		m.logger.Warn("Unable to synchronize VPC list", logfields.Error, err)
 		return time.Time{}
 	}
 
 	vSwitches, err := m.api.GetVSwitches(ctx)
 	if err != nil {
-		log.WithError(err).Warning("Unable to retrieve VPC vSwitches list")
+		m.logger.Warn("Unable to retrieve VPC vSwitches list", logfields.Error, err)
 		return time.Time{}
 	}
 
 	securityGroups, err := m.api.GetSecurityGroups(ctx)
 	if err != nil {
-		log.WithError(err).Warning("Unable to retrieve ECS security group list")
+		m.logger.Warn("Unable to retrieve ECS security group list", logfields.Error, err)
 		return time.Time{}
 	}
 
@@ -129,16 +132,17 @@ func (m *InstancesManager) resync(ctx context.Context, instanceID string) time.T
 	if instanceID == "" {
 		instances, err := m.api.GetInstances(ctx, vpcs, vSwitches)
 		if err != nil {
-			log.WithError(err).Warning("Unable to synchronize ECS interface list")
+			m.logger.Warn("Unable to synchronize ECS interface list", logfields.Error, err)
 			return time.Time{}
 		}
 
-		log.WithFields(logrus.Fields{
-			"numInstances":      instances.NumInstances(),
-			"numVPCs":           len(vpcs),
-			"numVSwitches":      len(vSwitches),
-			"numSecurityGroups": len(securityGroups),
-		}).Info("Synchronized ENI information")
+		m.logger.Info(
+			"Synchronized ENI information",
+			logfields.NumInstances, instances.NumInstances(),
+			logfields.NumVPCs, len(vpcs),
+			logfields.NumVSwitches, len(vSwitches),
+			logfields.NumSecurityGroups, len(securityGroups),
+		)
 
 		m.mutex.Lock()
 		defer m.mutex.Unlock()
@@ -146,16 +150,17 @@ func (m *InstancesManager) resync(ctx context.Context, instanceID string) time.T
 	} else {
 		instance, err := m.api.GetInstance(ctx, vpcs, vSwitches, instanceID)
 		if err != nil {
-			log.WithError(err).Warning("Unable to synchronize ECS interface list")
+			m.logger.Warn("Unable to synchronize ECS interface list", logfields.Error, err)
 			return time.Time{}
 		}
 
-		log.WithFields(logrus.Fields{
-			"instance":          instanceID,
-			"numVPCs":           len(vpcs),
-			"numVSwitches":      len(vSwitches),
-			"numSecurityGroups": len(securityGroups),
-		}).Info("Synchronized ENI information for the corresponding instance")
+		m.logger.Info(
+			"Synchronized ENI information for the corresponding instance",
+			logfields.InstanceID, instanceID,
+			logfields.NumVPCs, len(vpcs),
+			logfields.NumVSwitches, len(vSwitches),
+			logfields.NumSecurityGroups, len(securityGroups),
+		)
 
 		m.mutex.Lock()
 		defer m.mutex.Unlock()

pkg/alibabacloud/eni/log.go
@@ -4,12 +4,11 @@
 package eni
 
 import (
-	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
 var (
-	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "eni")
+	subsysLogAttr = []any{logfields.LogSubsys, "eni"}
 )
 
 const (

pkg/alibabacloud/eni/node.go
@@ -7,8 +7,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-
-	"github.com/sirupsen/logrus"
+	"log/slog"
 
 	"github.com/cilium/cilium/pkg/alibabacloud/eni/limits"
 	eniTypes "github.com/cilium/cilium/pkg/alibabacloud/eni/types"
@@ -19,6 +18,7 @@ import (
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
 // The following error constants represent the error conditions for
@@ -47,6 +47,7 @@ type ipamNodeActions interface {
 }
 
 type Node struct {
+	logger *slog.Logger
 	// node contains the general purpose fields of a node
 	node ipamNodeActions
 
@@ -93,7 +94,7 @@ func (n *Node) PopulateStatusFields(resource *v2.CiliumNode) {
 // attaches it to the instance as specified by the CiliumNode. neededAddresses
 // of secondary IPs are assigned to the interface up to the maximum number of
 // addresses as allowed by the instance.
-func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationAction, scopedLog *logrus.Entry) (int, string, error) {
+func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationAction, scopedLog *slog.Logger) (int, string, error) {
 	l, limitsAvailable := n.getLimits()
 	if !limitsAvailable {
 		return 0, unableToDetermineLimits, errors.New(errUnableToDetermineLimits)
@@ -131,19 +132,22 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 			fmt.Errorf("%s: %w", errUnableToGetSecurityGroups, err)
 	}
 
-	scopedLog = scopedLog.WithFields(logrus.Fields{
-		"securityGroupIDs": securityGroupIDs,
-		"vSwitchID":        bestSubnet.ID,
-		"toAllocate":       toAllocate,
-	})
-	scopedLog.Info("No more IPs available, creating new ENI")
+	scopedLog = scopedLog.With(
+		logfields.SecurityGroupIDs, securityGroupIDs,
+		logfields.VSwitchID, bestSubnet.ID,
+		logfields.ToAllocate, toAllocate,
+	)
+
+	scopedLog.Info(
+		"No more IPs available, creating new ENI",
+	)
 
 	instanceID := n.node.InstanceID()
 	n.mutex.Lock()
 	defer n.mutex.Unlock()
 	index, err := n.allocENIIndex()
 	if err != nil {
-		scopedLog.WithField("instanceID", instanceID).Error(err)
+		scopedLog.Error(err.Error(), logfields.InstanceID, instanceID)
 		return 0, "", err
 	}
 	eniID, eni, err := n.manager.api.CreateNetworkInterface(ctx, toAllocate-1, bestSubnet.ID, securityGroupIDs,
@@ -152,8 +156,7 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 		return 0, unableToCreateENI, fmt.Errorf("%s: %w", errUnableToCreateENI, err)
 	}
 
-	scopedLog = scopedLog.WithField(fieldENIID, eniID)
-	scopedLog.Info("Created new ENI")
+	scopedLog.Info("Created new ENI", fieldENIID, eniID)
 
 	if bestSubnet.CIDR != nil {
 		eni.VSwitch.CIDRBlock = bestSubnet.CIDR.String()
@@ -163,7 +166,11 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 	if err != nil {
 		err2 := n.manager.api.DeleteNetworkInterface(ctx, eniID)
 		if err2 != nil {
-			scopedLog.Errorf("Failed to release ENI after failure to attach, %s", err2.Error())
+			scopedLog.Error(
+				"Failed to release ENI after failure to attach",
+				fieldENIID, eniID,
+				logfields.Error, err2,
+			)
 		}
 		return 0, unableToAttachENI, fmt.Errorf("%s: %w", errUnableToAttachENI, err)
 	}
@@ -171,7 +178,11 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 	if err != nil {
 		err2 := n.manager.api.DeleteNetworkInterface(ctx, eniID)
 		if err2 != nil {
-			scopedLog.Errorf("Failed to release ENI after failure to attach, %s", err2.Error())
+			scopedLog.Error(
+				"Failed to release ENI after failure to attach",
+				fieldENIID, eniID,
+				logfields.Error, err2,
+			)
 		}
 		return 0, unableToAttachENI, fmt.Errorf("%s: %w", errUnableToAttachENI, err)
 	}
@@ -186,7 +197,7 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 
 // ResyncInterfacesAndIPs is called to retrieve and ENIs and IPs as known to
 // the AlibabaCloud API and return them
-func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Entry) (available ipamTypes.AllocationMap, stats stats.InterfaceStats, err error) {
+func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *slog.Logger) (available ipamTypes.AllocationMap, stats stats.InterfaceStats, err error) {
 	limits, limitsAvailable := n.getLimits()
 	if !limitsAvailable {
 		return nil, stats, ipam.LimitsNotFound{}
@@ -238,7 +249,7 @@ func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Ent
 
 	// An ECS instance has at least one ENI attached, no ENI found implies instance not found.
 	if enis == 0 {
-		scopedLog.Warning("Instance not found! Please delete corresponding ciliumnode if instance has already been deleted.")
+		scopedLog.Warn("Instance not found! Please delete corresponding ciliumnode if instance has already been deleted.")
 		return nil, stats, errors.New("unable to retrieve ENIs")
 	}
 
@@ -248,7 +259,7 @@ func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Ent
 
 // PrepareIPAllocation returns the number of ENI IPs and interfaces that can be
 // allocated/created.
-func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (*ipam.AllocationAction, error) {
+func (n *Node) PrepareIPAllocation(scopedLog *slog.Logger) (*ipam.AllocationAction, error) {
 	l, limitsAvailable := n.getLimits()
 	if !limitsAvailable {
 		return nil, errors.New(errUnableToDetermineLimits)
@@ -262,11 +273,12 @@ func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (*ipam.AllocationAct
 		if e.Type != eniTypes.ENITypeSecondary {
 			continue
 		}
-		scopedLog.WithFields(logrus.Fields{
-			fieldENIID:  e.NetworkInterfaceID,
-			"ipv4Limit": l.IPv4,
-			"allocated": len(e.PrivateIPSets),
-		}).Debug("Considering ENI for allocation")
+		scopedLog.Debug(
+			"Considering ENI for allocation",
+			fieldENIID, e.NetworkInterfaceID,
+			logfields.IPv4Limit, l.IPv4,
+			logfields.Allocated, len(e.PrivateIPSets),
+		)
 
 		// limit
 		availableOnENI := max(l.IPv4-len(e.PrivateIPSets), 0)
@@ -276,17 +288,19 @@ func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (*ipam.AllocationAct
 			a.IPv4.InterfaceCandidates++
 		}
 
-		scopedLog.WithFields(logrus.Fields{
-			fieldENIID:       e.NetworkInterfaceID,
-			"availableOnENI": availableOnENI,
-		}).Debug("ENI has IPs available")
+		scopedLog.Debug(
+			"ENI has IPs available",
+			fieldENIID, e.NetworkInterfaceID,
+			logfields.AvailableOnENI, availableOnENI,
+		)
 
 		if subnet := n.manager.GetVSwitch(e.VSwitch.VSwitchID); subnet != nil {
 			if subnet.AvailableAddresses > 0 && a.InterfaceID == "" {
-				scopedLog.WithFields(logrus.Fields{
-					"vSwitchID":          e.VSwitch.VSwitchID,
-					"availableAddresses": subnet.AvailableAddresses,
-				}).Debug("Subnet has IPs available")
+				scopedLog.Debug(
+					"Subnet has IPs available",
+					logfields.VSwitchID, e.VSwitch.VSwitchID,
+					logfields.AvailableAddresses, subnet.AvailableAddresses,
+				)
 
 				a.InterfaceID = key
 				a.PoolID = ipamTypes.PoolID(subnet.ID)
@@ -310,7 +324,7 @@ func (n *Node) AllocateStaticIP(ctx context.Context, staticIPTags ipamTypes.Tags
 }
 
 // PrepareIPRelease prepares the release of ENI IPs.
-func (n *Node) PrepareIPRelease(excessIPs int, scopedLog *logrus.Entry) *ipam.ReleaseAction {
+func (n *Node) PrepareIPRelease(excessIPs int, scopedLog *slog.Logger) *ipam.ReleaseAction {
 	r := &ipam.ReleaseAction{}
 
 	n.mutex.Lock()
@@ -322,10 +336,11 @@ func (n *Node) PrepareIPRelease(excessIPs int, scopedLog *logrus.Entry) *ipam.Re
 		if e.Type != eniTypes.ENITypeSecondary {
 			continue
 		}
-		scopedLog.WithFields(logrus.Fields{
-			fieldENIID:     e.NetworkInterfaceID,
-			"numAddresses": len(e.PrivateIPSets),
-		}).Debug("Considering ENI for IP release")
+		scopedLog.Debug(
+			"Considering ENI for IP release",
+			fieldENIID, e.NetworkInterfaceID,
+			logfields.NumAddresses, len(e.PrivateIPSets),
+		)
 
 		// Count free IP addresses on this ENI
 		ipsOnENI := n.k8sObj.Status.AlibabaCloud.ENIs[e.NetworkInterfaceID].PrivateIPSets
@@ -346,11 +361,12 @@ func (n *Node) PrepareIPRelease(excessIPs int, scopedLog *logrus.Entry) *ipam.Re
 			continue
 		}
 
-		scopedLog.WithFields(logrus.Fields{
-			fieldENIID:       e.NetworkInterfaceID,
-			"excessIPs":      excessIPs,
-			"freeOnENICount": freeOnENICount,
-		}).Debug("ENI has unused IPs that can be released")
+		scopedLog.Debug(
+			"ENI has unused IPs that can be released",
+			fieldENIID, e.NetworkInterfaceID,
+			logfields.ExcessIPs, excessIPs,
+			logfields.FreeOnENICount, freeOnENICount,
+		)
 		maxReleaseOnENI := min(freeOnENICount, excessIPs)
 
 		r.InterfaceID = key
@@ -389,12 +405,12 @@ func (n *Node) GetMinimumAllocatableIPv4() int {
 	return defaults.IPAMPreAllocation
 }
 
-func (n *Node) loggerLocked() *logrus.Entry {
+func (n *Node) loggerLocked() *slog.Logger {
 	if n == nil || n.instanceID == "" {
-		return log
+		return n.logger
 	}
 
-	return log.WithField("instanceID", n.instanceID)
+	return n.logger.With(logfields.InstanceID, n.instanceID)
 }
 
 func (n *Node) IsPrefixDelegated() bool {
@@ -434,10 +450,11 @@ func (n *Node) getSecurityGroupIDs(ctx context.Context, eniSpec eniTypes.Spec) (
 	if len(eniSpec.SecurityGroupTags) > 0 {
 		securityGroups := n.manager.FindSecurityGroupByTags(eniSpec.VPCID, eniSpec.SecurityGroupTags)
 		if len(securityGroups) == 0 {
-			n.loggerLocked().WithFields(logrus.Fields{
-				"vpcID": eniSpec.VPCID,
-				"tags":  eniSpec.SecurityGroupTags,
-			}).Warn("No security groups match required VPC ID and tags, using primary ENI's security groups")
+			n.loggerLocked().Warn(
+				"No security groups match required VPC ID and tags, using primary ENI's security groups",
+				logfields.VPCID, eniSpec.VPCID,
+				logfields.Tags, eniSpec.SecurityGroupTags,
+			)
 		} else {
 			groups := make([]string, 0, len(securityGroups))
 			for _, secGroup := range securityGroups {
@@ -471,7 +488,7 @@ func (n *Node) allocENIIndex() (int, error) {
 	// alloc index for each created ENI
 	used := make([]bool, maxENIPerNode)
 	for _, v := range n.enis {
-		index := utils.GetENIIndexFromTags(v.Tags)
+		index := utils.GetENIIndexFromTags(n.logger, v.Tags)
 		if index > maxENIPerNode || index < 0 {
 			return 0, fmt.Errorf("ENI index(%d) is out of range", index)
 		}

pkg/alibabacloud/eni/node_stat_test.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/cilium/hive/hivetest"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/cilium/cilium/pkg/alibabacloud/eni/limits"
@@ -37,7 +38,8 @@ func TestENIIPAMCapacityAccounting(t *testing.T) {
 	})
 
 	n := &Node{
-		node: mockIPAMNode("vm1"),
+		logger: hivetest.Logger(t),
+		node:   mockIPAMNode("vm1"),
 		manager: &InstancesManager{
 			instances: m,
 		},
@@ -49,7 +51,7 @@ func TestENIIPAMCapacityAccounting(t *testing.T) {
 			},
 		},
 	}
-	_, stats, err := n.ResyncInterfacesAndIPs(context.Background(), log)
+	_, stats, err := n.ResyncInterfacesAndIPs(context.Background(), n.logger)
 	assert.NoError(err)
 	// 3 ENIs, 10 IPs per ENI, 1 primary IP and one ENI is primary.
 	assert.Equal(19, stats.NodeCapacity)

commit 9dbbe8657254127e5931c6ee7ca4f2beef49e0e2

pkg/ipam: use the right slog.Logger
Due some automatic imports, some log usages were using the log from
cloudflare instead of slog.

Fixes: 1fb88a1572f9 ("pkg/{alibabacloud,aws,azure,ipam}: migrate to slog")
Signed-off-by: André Martins <andre@cilium.io>

pkg/ipam/allocator/podcidr/podcidr.go
@@ -10,7 +10,6 @@ import (
 	"net"
 	"strings"
 
-	"github.com/cloudflare/cfssl/log"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/cilium/cilium/pkg/cidr"
@@ -250,7 +249,10 @@ func syncToK8s(logger *slog.Logger, nodeGetterUpdater ipam.CiliumNodeGetterUpdat
 		case k8sOpUpdate:
 			var updatedNode *v2.CiliumNode
 			updatedNode, err = nodeGetterUpdater.Update(nil, nodeToK8s.ciliumNode)
-			log.Debug("Updated Node", logfields.Error, err, logfields.NodeName, nodeName)
+			logger.Debug("Updated Node",
+				logfields.Error, err,
+				logfields.NodeName, nodeName,
+			)
 			if err != nil {
 				if k8sErrors.IsNotFound(err) {
 					// In case the node was not found we should not try to re-create
@@ -268,7 +270,10 @@ func syncToK8s(logger *slog.Logger, nodeGetterUpdater ipam.CiliumNodeGetterUpdat
 			fallthrough
 		case k8sOpUpdateStatus:
 			_, err = nodeGetterUpdater.UpdateStatus(nil, nodeToK8s.ciliumNode)
-			log.Debug("UpdatedStatus Node", logfields.Error, err, logfields.NodeName, nodeName)
+			logger.Debug("UpdatedStatus Node",
+				logfields.Error, err,
+				logfields.NodeName, nodeName,
+			)
 			switch {
 			case k8sErrors.IsNotFound(err):
 				// In case the node was not found we should not try to re-create