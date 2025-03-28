cilium/cilium

commit afcb0c986df5b45d0037cc768c7da4f5ecc367ab

pkg/k8s: migrate to slog
Migrate this package to use slog instead of logrus.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/identitybackend/identity.go
@@ -6,13 +6,14 @@ package identitybackend
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"reflect"
 	"sort"
 	"strconv"
 	"strings"
 	"sync/atomic"
 
-	"github.com/sirupsen/logrus"
+	"github.com/cloudflare/cfssl/log"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
 
@@ -26,13 +27,8 @@ import (
 	k8sUtils "github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labels"
-	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/rate"
-)
-
-var (
-	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "crd-allocator")
 )
 
 const (
@@ -47,8 +43,8 @@ const (
 	byKeyIndex = "by-key-index"
 )
 
-func NewCRDBackend(c CRDBackendConfiguration) (allocator.Backend, error) {
-	return &crdBackend{CRDBackendConfiguration: c}, nil
+func NewCRDBackend(logger *slog.Logger, c CRDBackendConfiguration) (allocator.Backend, error) {
+	return &crdBackend{logger: logger, CRDBackendConfiguration: c}, nil
 }
 
 type CRDBackendConfiguration struct {
@@ -59,6 +55,7 @@ type CRDBackendConfiguration struct {
 }
 
 type crdBackend struct {
+	logger *slog.Logger
 	CRDBackendConfiguration
 }
 
@@ -98,7 +95,10 @@ func SanitizeK8sLabels(old map[string]string) (selected, skipped map[string]stri
 // Returns an allocator key with the cilium identity stored in it.
 func (c *crdBackend) AllocateID(ctx context.Context, id idpool.ID, key allocator.AllocatorKey) (allocator.AllocatorKey, error) {
 	selectedLabels, skippedLabels := SanitizeK8sLabels(key.GetAsMap())
-	log.WithField(logfields.Labels, skippedLabels).Info("Skipped non-kubernetes labels when labelling ciliumidentity. All labels will still be used in identity determination")
+	c.logger.Info(
+		"Skipped non-kubernetes labels when labelling ciliumidentity. All labels will still be used in identity determination",
+		logfields.Labels, skippedLabels,
+	)
 
 	identity := &v2.CiliumIdentity{
 		ObjectMeta: metav1.ObjectMeta{
@@ -157,7 +157,11 @@ func (c *crdBackend) AcquireReference(ctx context.Context, id idpool.ID, key all
 
 	ts, ok = ci.Annotations[HeartBeatAnnotation]
 	if ok {
-		log.WithField(logfields.Identity, ci).Infof("Identity marked for deletion (at %s); attempting to unmark it", ts)
+		c.logger.Info(
+			"Identity marked for deletion; attempting to unmark it",
+			logfields.Timeout, ts,
+			logfields.Identity, ci,
+		)
 		ci = ci.DeepCopy()
 		delete(ci.Annotations, HeartBeatAnnotation)
 		_, err = c.Client.CiliumV2().CiliumIdentities().Update(ctx, ci, metav1.UpdateOptions{})
@@ -183,19 +187,22 @@ func (c *crdBackend) RunGC(context.Context, *rate.Limiter, map[string]uint64, id
 func (c *crdBackend) UpdateKey(ctx context.Context, id idpool.ID, key allocator.AllocatorKey, reliablyMissing bool) error {
 	err := c.AcquireReference(ctx, id, key, nil)
 	if err == nil {
-		log.WithFields(logrus.Fields{
-			logfields.Identity: id,
-			logfields.Labels:   key,
-		}).Debug("Acquired reference for identity")
+		c.logger.Debug(
+			"Acquired reference for identity",
+			logfields.Identity, id,
+			logfields.Labels, key,
+		)
 		return nil
 	}
 
 	// The CRD (aka the master key) is missing. Try to recover by recreating it
 	// if reliablyMissing is set.
-	log.WithError(err).WithFields(logrus.Fields{
-		logfields.Identity: id,
-		logfields.Labels:   key,
-	}).Warning("Unable update CRD identity information with a reference for this node")
+	c.logger.Warn(
+		"Unable update CRD identity information with a reference for this node",
+		logfields.Error, err,
+		logfields.Identity, id,
+		logfields.Labels, key,
+	)
 
 	if reliablyMissing {
 		// Recreate a missing master key
@@ -364,7 +371,10 @@ func (c *crdBackend) ListIDs(ctx context.Context) (identityIDs []idpool.ID, err 
 	for _, identity := range c.Store.List() {
 		idParsed, err := strconv.ParseUint(identity.(*v2.CiliumIdentity).Name, 10, 64)
 		if err != nil {
-			log.WithField(logfields.Identity, identity.(*v2.CiliumIdentity).Name).Warn("Cannot parse identity ID")
+			c.logger.Warn(
+				"Cannot parse identity ID",
+				logfields.Identity, identity.(*v2.CiliumIdentity).Name,
+			)
 			continue
 		}
 		identityIDs = append(identityIDs, idpool.ID(idParsed))
@@ -412,7 +422,10 @@ func (c *crdBackend) ListAndWatch(ctx context.Context, handler allocator.CacheMu
 						handler.OnDelete(idpool.ID(id), c.KeyFunc(identity.SecurityLabels))
 					}
 				} else {
-					log.Debugf("Ignoring unknown delete event %#v", obj)
+					log.Debug(
+						"Ignoring unknown delete event",
+						logfields.Object, obj,
+					)
 				}
 			},
 		},

pkg/k8s/resource_ctors.go
@@ -5,18 +5,19 @@ package k8s
 
 import (
 	"fmt"
+	"log/slog"
 	"sync"
 
-	"github.com/cilium/cilium/pkg/allocator"
-	"github.com/cilium/cilium/pkg/identity/key"
-
 	"github.com/cilium/hive/cell"
+	"github.com/cloudflare/cfssl/log"
 	"github.com/spf13/pflag"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sRuntime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/tools/cache"
 
+	"github.com/cilium/cilium/pkg/allocator"
+	"github.com/cilium/cilium/pkg/identity/key"
 	cilium_api_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	cilium_api_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/k8s/client"
@@ -294,7 +295,7 @@ func CiliumBGPPeerConfigResource(params CiliumResourceParams, opts ...func(*meta
 	return resource.New[*cilium_api_v2.CiliumBGPPeerConfig](params.Lifecycle, lw, resource.WithMetric("CiliumBGPPeerConfig"), resource.WithCRDSync(params.CRDSyncPromise)), nil
 }
 
-func EndpointsResource(lc cell.Lifecycle, cfg Config, cs client.Clientset, opts ...func(*metav1.ListOptions)) (resource.Resource[*Endpoints], error) {
+func EndpointsResource(logger *slog.Logger, lc cell.Lifecycle, cfg Config, cs client.Clientset, opts ...func(*metav1.ListOptions)) (resource.Resource[*Endpoints], error) {
 	if !cs.IsEnabled() {
 		return nil, nil
 	}
@@ -317,7 +318,9 @@ func EndpointsResource(lc cell.Lifecycle, cfg Config, cs client.Clientset, opts 
 	return resource.New[*Endpoints](
 		lc,
 		lw,
-		resource.WithLazyTransform(lw.getSourceObj, transformEndpoint),
+		resource.WithLazyTransform(lw.getSourceObj, func(i any) (any, error) {
+			return transformEndpoint(logger, i)
+		}),
 		resource.WithMetric("Endpoint"),
 		resource.WithName("endpoints"),
 	), nil
@@ -379,12 +382,12 @@ func (lw *endpointsListerWatcher) Watch(opts metav1.ListOptions) (watch.Interfac
 	return lw.getListerWatcher().Watch(opts)
 }
 
-func transformEndpoint(obj any) (any, error) {
+func transformEndpoint(logger *slog.Logger, obj any) (any, error) {
 	switch obj := obj.(type) {
 	case *slim_corev1.Endpoints:
 		return ParseEndpoints(obj), nil
 	case *slim_discoveryv1.EndpointSlice:
-		return ParseEndpointSliceV1(obj), nil
+		return ParseEndpointSliceV1(logger, obj), nil
 	case *slim_discoveryv1beta1.EndpointSlice:
 		return ParseEndpointSliceV1Beta1(obj), nil
 	default:
@@ -427,8 +430,10 @@ func ciliumEndpointLocalPodIndexFunc(obj any) ([]string, error) {
 	}
 	indices := []string{}
 	if cep.Networking == nil {
-		log.WithField("ciliumendpoint", cep.GetNamespace()+"/"+cep.GetName()).
-			Debug("cannot index CiliumEndpoint by node without network status")
+		log.Debug(
+			"cannot index CiliumEndpoint by node without network status",
+			slog.Any("ciliumendpoint", cep.GetNamespace()+"/"+cep.GetName()),
+		)
 		return nil, nil
 	}
 	if cep.Networking.NodeIP == node.GetCiliumEndpointNodeIP() {

pkg/k8s/synced/crd.go
@@ -8,7 +8,10 @@ package synced
 import (
 	"context"
 	"errors"
+	"fmt"
+	"log/slog"
 
+	"github.com/cloudflare/cfssl/log"
 	apiextclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -24,6 +27,8 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/time"
 )
@@ -131,8 +136,8 @@ func AllCiliumCRDResourceNames() []string {
 // installed inside the K8s cluster. These CRDs are added by the
 // Cilium Operator. This function will block until it finds all the
 // CRDs or if a timeout occurs.
-func SyncCRDs(ctx context.Context, clientset client.Clientset, crdNames []string, rs *Resources, ag *APIGroups, cfg CRDSyncConfig) error {
-	crds := newCRDState(crdNames)
+func SyncCRDs(ctx context.Context, logger *slog.Logger, clientset client.Clientset, crdNames []string, rs *Resources, ag *APIGroups, cfg CRDSyncConfig) error {
+	crds := newCRDState(logger, crdNames)
 
 	listerWatcher := newListWatchFromClient(
 		newCRDGetter(clientset),
@@ -194,7 +199,7 @@ func SyncCRDs(ctx context.Context, clientset client.Clientset, crdNames []string
 	// controller will exit after this function.
 	defer ag.RemoveAPI(k8sAPIGroupCRD)
 
-	log.Info("Waiting until all Cilium CRDs are available")
+	logger.Info("Waiting until all Cilium CRDs are available")
 
 	ticker := time.NewTicker(50 * time.Millisecond)
 	count := 0
@@ -203,12 +208,15 @@ func SyncCRDs(ctx context.Context, clientset client.Clientset, crdNames []string
 		case <-ctx.Done():
 			err := ctx.Err()
 			if err != nil && !errors.Is(err, context.Canceled) {
-				log.WithError(err).
-					Fatalf("Unable to find all Cilium CRDs necessary within "+
+				logging.Fatal(
+					logger,
+					fmt.Sprintf("Unable to find all Cilium CRDs necessary within "+
 						"%v timeout. Please ensure that Cilium Operator is "+
 						"running, as it's responsible for registering all "+
 						"the Cilium CRDs. The following CRDs were not found: %v",
-						cfg.CRDWaitTimeout, crds.unSynced())
+						cfg.CRDWaitTimeout, crds.unSynced()),
+					logfields.Error, err,
+				)
 			}
 			// If the context was canceled it means the daemon is being stopped
 			// so we can return the context's error.
@@ -222,14 +230,17 @@ func SyncCRDs(ctx context.Context, clientset client.Clientset, crdNames []string
 			count++
 			if count == 20 {
 				count = 0
-				log.Infof("Still waiting for Cilium Operator to register the following CRDs: %v", crds.unSynced())
+				log.Info(
+					"Still waiting for Cilium Operator to register CRDs",
+					logfields.CRDs, crds.unSynced(),
+				)
 			}
 		}
 	}
 }
 
 func (s *crdState) add(obj interface{}) {
-	if pom := informer.CastInformerEvent[slim_metav1.PartialObjectMetadata](obj); pom != nil {
+	if pom := informer.CastInformerEvent[slim_metav1.PartialObjectMetadata](s.logger, obj); pom != nil {
 		s.Lock()
 		s.m[CRDResourceName(pom.GetName())] = true
 		s.Unlock()
@@ -237,7 +248,7 @@ func (s *crdState) add(obj interface{}) {
 }
 
 func (s *crdState) remove(obj interface{}) {
-	if pom := informer.CastInformerEvent[slim_metav1.PartialObjectMetadata](obj); pom != nil {
+	if pom := informer.CastInformerEvent[slim_metav1.PartialObjectMetadata](s.logger, obj); pom != nil {
 		s.Lock()
 		s.m[CRDResourceName(pom.GetName())] = false
 		s.Unlock()
@@ -273,6 +284,7 @@ func (s *crdState) unSynced() []string {
 
 // crdState contains the state of the CRDs inside the cluster.
 type crdState struct {
+	logger *slog.Logger
 	lock.Mutex
 
 	// m is a map which maps the CRD name to its synced state in the cluster.
@@ -280,13 +292,14 @@ type crdState struct {
 	m map[string]bool
 }
 
-func newCRDState(crds []string) crdState {
+func newCRDState(logger *slog.Logger, crds []string) crdState {
 	m := make(map[string]bool, len(crds))
 	for _, name := range crds {
 		m[name] = false
 	}
 	return crdState{
-		m: m,
+		logger: logger,
+		m:      m,
 	}
 }
 

pkg/k8s/watchers/service.go
@@ -7,11 +7,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log/slog"
 	"net"
 	"sync/atomic"
 
 	"github.com/cilium/hive/cell"
-	"github.com/sirupsen/logrus"
+	"github.com/cloudflare/cfssl/log"
 	"k8s.io/apimachinery/pkg/labels"
 
 	agentK8s "github.com/cilium/cilium/daemon/k8s"
@@ -27,7 +28,6 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/lock"
-	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/node"
@@ -40,6 +40,8 @@ import (
 
 type k8sServiceWatcherParams struct {
 	cell.In
+
+	Logger *slog.Logger
 
 	K8sEventReporter *K8sEventReporter
 
@@ -55,6 +57,7 @@ type k8sServiceWatcherParams struct {
 
 func newK8sServiceWatcher(params k8sServiceWatcherParams) *K8sServiceWatcher {
 	return &K8sServiceWatcher{
+		logger:                params.Logger,
 		k8sEventReporter:      params.K8sEventReporter,
 		k8sResourceSynced:     params.K8sResourceSynced,
 		k8sAPIGroups:          params.K8sAPIGroups,
@@ -68,6 +71,7 @@ func newK8sServiceWatcher(params k8sServiceWatcherParams) *K8sServiceWatcher {
 }
 
 type K8sServiceWatcher struct {
+	logger           *slog.Logger
 	k8sEventReporter *K8sEventReporter
 	// k8sResourceSynced maps a resource name to a channel. Once the given
 	// resource name is synchronized with k8s, the channel for which that
@@ -166,19 +170,17 @@ func (k *K8sServiceWatcher) k8sServiceHandler() {
 
 		svc := event.Service
 
-		scopedLog := log.WithFields(logrus.Fields{
-			logfields.K8sSvcName:   event.ID.Name,
-			logfields.K8sNamespace: event.ID.Namespace,
-		})
-
-		if logging.CanLogAt(scopedLog.Logger, logrus.DebugLevel) {
-			scopedLog.WithFields(logrus.Fields{
-				"action":        event.Action.String(),
-				"service":       event.Service.String(),
-				"old-service":   event.OldService.String(),
-				"endpoints":     event.Endpoints.String(),
-				"old-endpoints": event.OldEndpoints.String(),
-			}).Debug("Kubernetes service definition changed")
+		if k.logger.Enabled(context.Background(), slog.LevelDebug) {
+			k.logger.Debug(
+				"Kubernetes service definition changed",
+				logfields.Action, event.Action,
+				logfields.Service, event.Service,
+				logfields.OldService, event.OldService,
+				logfields.Endpoints, event.Endpoints,
+				logfields.OldEndpoints, event.OldEndpoints,
+				logfields.K8sSvcName, event.ID.Name,
+				logfields.K8sNamespace, event.ID.Namespace,
+			)
 		}
 
 		switch event.Action {
@@ -211,10 +213,10 @@ func (k *K8sServiceWatcher) delK8sSVCs(svc k8s.ServiceID, svcInfo *k8s.Service) 
 	if svcInfo.IsHeadless && len(svcInfo.FrontendIPs) == 0 {
 		return
 	}
-	scopedLog := log.WithFields(logrus.Fields{
-		logfields.K8sSvcName:   svc.Name,
-		logfields.K8sNamespace: svc.Namespace,
-	})
+	scopedLog := k.logger.With(
+		logfields.K8sSvcName, svc.Name,
+		logfields.K8sNamespace, svc.Namespace,
+	)
 
 	for _, checkProtocol := range []bool{true, false} {
 		if !checkProtocol {
@@ -260,12 +262,22 @@ func (k *K8sServiceWatcher) delK8sSVCs(svc k8s.ServiceID, svcInfo *k8s.Service) 
 
 		for _, fe := range frontends {
 			if found, err := k.svcManager.DeleteService(*fe); err != nil {
-				scopedLog.WithError(err).WithField(logfields.Object, logfields.Repr(fe)).
-					Warn("Error deleting service by frontend")
+				scopedLog.Warn(
+					"Error deleting service by frontend",
+					logfields.Error, err,
+					logfields.Object, fe,
+				)
 			} else if !found {
-				scopedLog.WithField(logfields.Object, logfields.Repr(fe)).Warn("service not found")
+				scopedLog.Warn(
+					"service not found",
+					logfields.Object, fe,
+				)
 			} else {
-				scopedLog.Debugf("# cilium lb delete-service %s %d 0", fe.AddrCluster.String(), fe.Port)
+				if option.Config.Debug {
+					scopedLog.Debug(
+						fmt.Sprintf("# cilium lb delete-service %s %d 0", fe.AddrCluster.String(), fe.Port),
+					)
+				}
 			}
 		}
 	}
@@ -529,10 +541,10 @@ func (k *K8sServiceWatcher) addK8sSVCs(svcID k8s.ServiceID, oldSvc, svc *k8s.Ser
 		return
 	}
 
-	scopedLog := log.WithFields(logrus.Fields{
-		logfields.K8sSvcName:   svcID.Name,
-		logfields.K8sNamespace: svcID.Namespace,
-	})
+	scopedLog := k.logger.With(
+		logfields.K8sSvcName, svcID.Name,
+		logfields.K8sNamespace, svcID.Namespace,
+	)
 
 	if !option.Config.LoadBalancerProtocolDifferentiation {
 		oldSvc = stripServiceProtocol(oldSvc)
@@ -542,7 +554,7 @@ func (k *K8sServiceWatcher) addK8sSVCs(svcID k8s.ServiceID, oldSvc, svc *k8s.Ser
 
 	svcs, err := k.datapathSVCs(svc, endpoints)
 	if err != nil {
-		scopedLog.WithError(err).Error("Error while evaluating datapath services")
+		scopedLog.Error("Error while evaluating datapath services", logfields.Error, err)
 		return
 	}
 	svcMap := hashSVCMap(svcs)
@@ -552,7 +564,7 @@ func (k *K8sServiceWatcher) addK8sSVCs(svcID k8s.ServiceID, oldSvc, svc *k8s.Ser
 		// are no longer in the updated service and delete them in the datapath.
 		oldSVCs, err := k.datapathSVCs(oldSvc, endpoints)
 		if err != nil {
-			scopedLog.WithError(err).Error("Error while evaluating datapath services for old service")
+			scopedLog.Error("Error while evaluating datapath services for old service", logfields.Error, err)
 			return
 		}
 		oldSVCMap := hashSVCMap(oldSVCs)
@@ -560,12 +572,20 @@ func (k *K8sServiceWatcher) addK8sSVCs(svcID k8s.ServiceID, oldSvc, svc *k8s.Ser
 		for svcHash, oldSvc := range oldSVCMap {
 			if _, ok := svcMap[svcHash]; !ok {
 				if found, err := k.svcManager.DeleteService(oldSvc); err != nil {
-					scopedLog.WithError(err).WithField(logfields.Object, logfields.Repr(oldSvc)).
-						Warn("Error deleting service by frontend")
+					scopedLog.Warn(
+						"Error deleting service by frontend",
+						logfields.Error, err,
+						logfields.Object, oldSvc,
+					)
 				} else if !found {
-					scopedLog.WithField(logfields.Object, logfields.Repr(oldSvc)).Warn("service not found")
+					scopedLog.Warn(
+						"service not found",
+						logfields.Object, oldSvc,
+					)
 				} else {
-					scopedLog.Debugf("# cilium lb delete-service %s %d 0", oldSvc.AddrCluster.String(), oldSvc.Port)
+					if option.Config.Debug {
+						scopedLog.Debug(fmt.Sprintf("# cilium lb delete-service %s %d 0", oldSvc.AddrCluster.String(), oldSvc.Port))
+					}
 				}
 			}
 		}
@@ -595,9 +615,9 @@ func (k *K8sServiceWatcher) addK8sSVCs(svcID k8s.ServiceID, oldSvc, svc *k8s.Ser
 		}
 		if _, _, err := k.svcManager.UpsertService(p); err != nil {
 			if errors.Is(err, service.NewErrLocalRedirectServiceExists(p.Frontend, p.Name)) {
-				scopedLog.WithError(err).Debug("Error while inserting service in LB map")
+				scopedLog.Debug("Error while inserting service in LB map", logfields.Error, err)
 			} else {
-				scopedLog.WithError(err).Error("Error while inserting service in LB map")
+				scopedLog.Error("Error while inserting service in LB map", logfields.Error, err)
 			}
 		}
 		k.updateK8sAPIServiceMappings(p)
@@ -617,12 +637,14 @@ func (k *K8sServiceWatcher) updateK8sAPIServiceMappings(svc *loadbalancer.SVC) {
 	for i := range mapping.Endpoints {
 		mapping.Endpoints[i] = svc.Backends[i].AddrString()
 	}
-	log.WithField("entry", mapping).Info("writing kubernetes service mapping")
-	client.UpdateK8sAPIServerEntry(mapping)
+	log.Info("writing kubernetes service mapping",
+		logfields.Entry, mapping,
+	)
+	client.UpdateK8sAPIServerEntry(k.logger, mapping)
 }
 
 // k8sServiceEventProcessed is called to do metrics accounting the duration to program the service.
 func (k *K8sServiceWatcher) k8sServiceEventProcessed(action string, startTime time.Time) {
-	duration, _ := safetime.TimeSinceSafe(startTime, log)
+	duration, _ := safetime.TimeSinceSafe(startTime, k.logger)
 	metrics.ServiceImplementationDelay.WithLabelValues(action).Observe(duration.Seconds())
 }

commit 0a85df320601ac1fadffef52184edadad2426b7e

pkg/k8s: use the right slog.Logger
Due some automatic imports, some log usages were using the log from
cloudflare instead of slog.

Fixes: afcb0c986df5 ("pkg/k8s: migrate to slog")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/identitybackend/identity.go
@@ -13,7 +13,6 @@ import (
 	"strings"
 	"sync/atomic"
 
-	"github.com/cloudflare/cfssl/log"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
 
@@ -422,7 +421,7 @@ func (c *crdBackend) ListAndWatch(ctx context.Context, handler allocator.CacheMu
 						handler.OnDelete(idpool.ID(id), c.KeyFunc(identity.SecurityLabels))
 					}
 				} else {
-					log.Debug(
+					c.logger.Debug(
 						"Ignoring unknown delete event",
 						logfields.Object, obj,
 					)

pkg/k8s/resource_ctors.go
@@ -9,7 +9,6 @@ import (
 	"sync"
 
 	"github.com/cilium/hive/cell"
-	"github.com/cloudflare/cfssl/log"
 	"github.com/spf13/pflag"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sRuntime "k8s.io/apimachinery/pkg/runtime"
@@ -30,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/k8s/version"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/promise"
 )
@@ -90,6 +90,7 @@ func GetIdentitiesByKeyFunc(keyFunc func(map[string]string) allocator.AllocatorK
 type CiliumResourceParams struct {
 	cell.In
 
+	Logger         *slog.Logger
 	Lifecycle      cell.Lifecycle
 	ClientSet      client.Clientset
 	CRDSyncPromise promise.Promise[synced.CRDSync] `optional:"true"`
@@ -310,6 +311,7 @@ func EndpointsResource(logger *slog.Logger, lc cell.Lifecycle, cfg Config, cs cl
 	}
 
 	lw := &endpointsListerWatcher{
+		logger:                      logger,
 		cs:                          cs,
 		enableK8sEndpointSlice:      cfg.EnableK8sEndpointSlice,
 		endpointsOptsModifiers:      append(opts, endpointsOptsModifier),
@@ -330,6 +332,7 @@ func EndpointsResource(logger *slog.Logger, lc cell.Lifecycle, cfg Config, cs cl
 // performs the capability check on first call to List/Watch. This allows constructing
 // the resource before the client has been started and capabilities have been probed.
 type endpointsListerWatcher struct {
+	logger                      *slog.Logger
 	cs                          client.Clientset
 	enableK8sEndpointSlice      bool
 	endpointsOptsModifiers      []func(*metav1.ListOptions)
@@ -349,13 +352,13 @@ func (lw *endpointsListerWatcher) getListerWatcher() cache.ListerWatcher {
 	lw.once.Do(func() {
 		if lw.enableK8sEndpointSlice && version.Capabilities().EndpointSlice {
 			if version.Capabilities().EndpointSliceV1 {
-				log.Info("Using discoveryv1.EndpointSlice")
+				lw.logger.Info("Using discoveryv1.EndpointSlice")
 				lw.cachedListerWatcher = utils.ListerWatcherFromTyped[*slim_discoveryv1.EndpointSliceList](
 					lw.cs.Slim().DiscoveryV1().EndpointSlices(""),
 				)
 				lw.sourceObj = &slim_discoveryv1.EndpointSlice{}
 			} else {
-				log.Info("Using discoveryv1beta1.EndpointSlice")
+				lw.logger.Info("Using discoveryv1beta1.EndpointSlice")
 				lw.cachedListerWatcher = utils.ListerWatcherFromTyped[*slim_discoveryv1beta1.EndpointSliceList](
 					lw.cs.Slim().DiscoveryV1beta1().EndpointSlices(""),
 				)
@@ -363,7 +366,7 @@ func (lw *endpointsListerWatcher) getListerWatcher() cache.ListerWatcher {
 			}
 			lw.cachedListerWatcher = utils.ListerWatcherWithModifiers(lw.cachedListerWatcher, lw.endpointSlicesOptsModifiers...)
 		} else {
-			log.Info("Using v1.Endpoints")
+			lw.logger.Info("Using v1.Endpoints")
 			lw.cachedListerWatcher = utils.ListerWatcherFromTyped[*slim_corev1.EndpointsList](
 				lw.cs.Slim().CoreV1().Endpoints(""),
 			)
@@ -409,7 +412,9 @@ func CiliumSlimEndpointResource(params CiliumResourceParams, _ *node.LocalNodeSt
 		opts...,
 	)
 	indexers := cache.Indexers{
-		"localNode": ciliumEndpointLocalPodIndexFunc,
+		"localNode": func(obj any) ([]string, error) {
+			return ciliumEndpointLocalPodIndexFunc(params.Logger, obj)
+		},
 	}
 	return resource.New[*types.CiliumEndpoint](params.Lifecycle, lw,
 		resource.WithLazyTransform(func() k8sRuntime.Object {
@@ -423,16 +428,16 @@ func CiliumSlimEndpointResource(params CiliumResourceParams, _ *node.LocalNodeSt
 
 // ciliumEndpointLocalPodIndexFunc is an IndexFunc that indexes only local
 // CiliumEndpoints, by their local Node IP.
-func ciliumEndpointLocalPodIndexFunc(obj any) ([]string, error) {
+func ciliumEndpointLocalPodIndexFunc(logger *slog.Logger, obj any) ([]string, error) {
 	cep, ok := obj.(*types.CiliumEndpoint)
 	if !ok {
 		return nil, fmt.Errorf("unexpected object type: %T", obj)
 	}
 	indices := []string{}
 	if cep.Networking == nil {
-		log.Debug(
+		logger.Debug(
 			"cannot index CiliumEndpoint by node without network status",
-			slog.Any("ciliumendpoint", cep.GetNamespace()+"/"+cep.GetName()),
+			logfields.Name, cep.GetNamespace()+"/"+cep.GetName(),
 		)
 		return nil, nil
 	}

pkg/k8s/synced/crd.go
@@ -11,7 +11,6 @@ import (
 	"fmt"
 	"log/slog"
 
-	"github.com/cloudflare/cfssl/log"
 	apiextclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -224,13 +223,13 @@ func SyncCRDs(ctx context.Context, logger *slog.Logger, clientset client.Clients
 		case <-ticker.C:
 			if crds.isSynced() {
 				ticker.Stop()
-				log.Info("All Cilium CRDs have been found and are available")
+				logger.Info("All Cilium CRDs have been found and are available")
 				return nil
 			}
 			count++
 			if count == 20 {
 				count = 0
-				log.Info(
+				logger.Info(
 					"Still waiting for Cilium Operator to register CRDs",
 					logfields.CRDs, crds.unSynced(),
 				)

pkg/k8s/watchers/service.go
@@ -12,7 +12,6 @@ import (
 	"sync/atomic"
 
 	"github.com/cilium/hive/cell"
-	"github.com/cloudflare/cfssl/log"
 	"k8s.io/apimachinery/pkg/labels"
 
 	agentK8s "github.com/cilium/cilium/daemon/k8s"
@@ -637,7 +636,7 @@ func (k *K8sServiceWatcher) updateK8sAPIServiceMappings(svc *loadbalancer.SVC) {
 	for i := range mapping.Endpoints {
 		mapping.Endpoints[i] = svc.Backends[i].AddrString()
 	}
-	log.Info("writing kubernetes service mapping",
+	k.logger.Info("writing kubernetes service mapping",
 		logfields.Entry, mapping,
 	)
 	client.UpdateK8sAPIServerEntry(k.logger, mapping)