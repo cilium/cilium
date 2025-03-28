cilium/cilium

commit 93f1619cb4727ecf580b426532b487e0255a6725

bpf: Add ratelimiting feature and use it for ICMPv6 responses
This commit adds token bucket ratelimiting to the datapath. It is
implemented purely in BPF. A new map is added to keep track of the
buckets. A bucket can be keyed on anything, though since ICMPv6 is
currently the only user it is keyed on ifindex. The value is the current
amounts of tokens in the bucket and the last time we added tokens to the
bucket.

For every event we check if there is at least 1 token left in the bucket
if so, we decrement the token count and continue, if not we execute the
rate limiting action. Typically a timer would add new tokens into the
bucket, in our case we keep track of the last time we added tokens and
calculate how many tokens we should have added since then before we
do the token check.

This implements a burstable rate limiting mechanism. The burst size
and token refil is configurable. For ICMPv6 it is currently set to
100 replies per second with a burst size of 1000.

Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

bpf/lib/ratelimit.h
@@ -1,0 +1,76 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright Authors of Cilium */
+
+#ifndef __RATELIMIT_H_
+#define __RATELIMIT_H_
+
+#include "common.h"
+#include "bpf/helpers.h"
+
+struct ratelimit_key {
+	__u32 netdev_idx;
+};
+
+struct ratelimit_value {
+	__u64 last_topup;
+	__u64 tokens;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_LRU_HASH);
+	__type(key, struct ratelimit_key);
+	__type(value, struct ratelimit_value);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, 1024);
+} RATELIMIT_MAP __section_maps_btf;
+
+struct ratelimit_settings {
+	/* A bucket will never have more than X amount of tokens, limits burt size */
+	__u64 bucket_size;
+	/* The amount of tokens added to a bucket for every topup */
+	__u64 tokens_per_topup;
+	/* The interval at which the topups happen */
+	__u64 topup_interval_ns;
+};
+
+static inline bool ratelimit_check_and_take(struct ratelimit_key *key,
+					    const struct ratelimit_settings *settings)
+{
+	struct ratelimit_value *value;
+	struct ratelimit_value new_value;
+	__u64 since_last_topup;
+	int ret;
+
+	/* Create a new bucket if we do not yet have one for the key */
+	value = map_lookup_elem(&RATELIMIT_MAP, key);
+	if (!value) {
+		new_value.last_topup = ktime_get_ns();
+		new_value.tokens = settings->tokens_per_topup - 1;
+		ret = map_update_elem(&RATELIMIT_MAP, key, &new_value, BPF_ANY);
+		if (ret < 0)
+			return false;
+		return true;
+	}
+
+	/* Topup the bucket if it has been at least more than 1 interval since we have done so */
+	since_last_topup = ktime_get_ns() - value->last_topup;
+	if (since_last_topup > settings->topup_interval_ns) {
+		/* Add tokens of every missed interval */
+		value->tokens += (since_last_topup / settings->topup_interval_ns) *
+						  settings->tokens_per_topup;
+		value->last_topup = ktime_get_ns();
+		/* Make sure to not overflow the bucket */
+		if (value->tokens > settings->bucket_size)
+			value->tokens = settings->bucket_size;
+	}
+
+	/* Take a token if there is at least one */
+	if (value->tokens > 0) {
+		value->tokens--;
+		return true;
+	}
+
+	return false;
+}
+
+#endif

api/v1/flow/flow.proto
@@ -402,6 +402,7 @@ enum DropReason {
     UNENCRYPTED_TRAFFIC = 195;
     TTL_EXCEEDED = 196;
     NO_NODE_ID = 197;
+    DROP_RATE_LIMITED = 198;
 }
 
 enum TrafficDirection {

api/v1/observer/observer.pb.go
@@ -172,6 +172,7 @@ const DropReason_NO_EGRESS_GATEWAY = flow.DropReason_NO_EGRESS_GATEWAY
 const DropReason_UNENCRYPTED_TRAFFIC = flow.DropReason_UNENCRYPTED_TRAFFIC
 const DropReason_TTL_EXCEEDED = flow.DropReason_TTL_EXCEEDED
 const DropReason_NO_NODE_ID = flow.DropReason_NO_NODE_ID
+const DropReason_DROP_RATE_LIMITED = flow.DropReason_DROP_RATE_LIMITED
 
 var DropReason_name = flow.DropReason_name
 var DropReason_value = flow.DropReason_value

bpf/lib/common.h
@@ -638,6 +638,7 @@ enum {
 #define DROP_UNENCRYPTED_TRAFFIC	-195
 #define DROP_TTL_EXCEEDED	-196
 #define DROP_NO_NODE_ID		-197
+#define DROP_RATE_LIMITED	-198
 
 #define NAT_PUNT_TO_STACK	DROP_NAT_NOT_NEEDED
 #define NAT_NEEDED		CTX_ACT_OK

bpf/lib/lb.h
@@ -11,6 +11,7 @@
 #include "hash.h"
 #include "ids.h"
 #include "nat_46x64.h"
+#include "ratelimit.h"
 
 #ifndef SKIP_CALLS_MAP
 #include "drop.h"
@@ -1917,12 +1918,23 @@ int __tail_no_service_ipv6(struct __ctx_buff *ctx)
 	union macaddr dmac = {};
 	struct in6_addr saddr;
 	struct in6_addr daddr;
+	struct ratelimit_key rkey = {};
+	/* Rate limit to 100 ICMPv6 replies per second, burstable to 1000 responses/s */
+	struct ratelimit_settings settings = {
+		.bucket_size = 1000,
+		.tokens_per_topup = 100,
+		.topup_interval_ns = NSEC_PER_SEC,
+	};
 	__wsum csum;
 	__u64 sample_len;
 	int i;
 	int ret;
 	const int inner_offset = sizeof(struct ethhdr) + sizeof(struct ipv6hdr) +
 		sizeof(struct icmp6hdr);
+
+	rkey.netdev_idx = ctx_get_ifindex(ctx);
+	if (!ratelimit_check_and_take(&rkey, &settings))
+		return DROP_RATE_LIMITED;
 
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
 		return DROP_INVALID;

bpf/node_config.h
@@ -151,6 +151,7 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 #define NODE_MAP test_cilium_node_map
 #define ENCRYPT_MAP test_cilium_encrypt_state
 #define L2_RESPONDER_MAP4 test_cilium_l2_responder_v4
+#define RATELIMIT_MAP test_cilium_ratelimit
 #define TUNNEL_MAP test_cilium_tunnel_map
 #define VTEP_MAP test_cilium_vtep_map
 #define LB6_REVERSE_NAT_MAP test_cilium_lb6_reverse_nat

bpf/tests/ratelimit.c
@@ -1,0 +1,85 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#include "bpf/ctx/xdp.h"
+#include "node_config.h"
+#include "common.h"
+#include "lib/maps.h"
+#include "lib/ratelimit.h"
+
+CHECK("xdp", "ratelimit") int test_ratelimit(void)
+{
+	struct ratelimit_settings settings = {
+		.bucket_size = 1000,
+		.tokens_per_topup = 100,
+		.topup_interval_ns = NSEC_PER_SEC,
+	};
+	struct ratelimit_key key = {
+		.netdev_idx = 1,
+	};
+	struct ratelimit_value *value;
+
+	test_init();
+
+	TEST("bucket-created-when-missing", {
+		value = map_lookup_elem(&RATELIMIT_MAP, &key);
+		if (value)
+			test_fatal("Bucket already exits");
+
+		ratelimit_check_and_take(&key, &settings);
+
+		value = map_lookup_elem(&RATELIMIT_MAP, &key);
+		if (!value)
+			test_fatal("Bucket not created");
+	})
+
+	TEST("block-on-bucket-empty", {
+		value = map_lookup_elem(&RATELIMIT_MAP, &key);
+		if (!value)
+			test_fatal("Bucket not created");
+
+		value->tokens = 1;
+		if (!ratelimit_check_and_take(&key, &settings))
+			test_fatal("Rate limit not allowed when bucket not empty");
+
+		if (value->tokens != 0)
+			test_fatal("Bucket not empty");
+
+		if (ratelimit_check_and_take(&key, &settings))
+			test_fatal("Rate limit allowed when bucket empty");
+	})
+
+	TEST("topup-after-interval", {
+		value = map_lookup_elem(&RATELIMIT_MAP, &key);
+		if (!value)
+			test_fatal("Bucket not created");
+
+		/* Set last topup to 1 interval ago */
+		value->tokens = 0;
+		value->last_topup = ktime_get_ns() - settings.topup_interval_ns;
+
+		if (!ratelimit_check_and_take(&key, &settings))
+			test_fatal("Rate limit not allowed after topup");
+
+		if (value->tokens != settings.tokens_per_topup - 1)
+			test_fatal("Unexpected token amount after topup");
+	})
+
+	TEST("do-not-go-over-bucket-size", {
+		value = map_lookup_elem(&RATELIMIT_MAP, &key);
+		if (!value)
+			test_fatal("Bucket not created");
+
+		/* Set last topup to 100 intervals ago */
+		value->tokens = 0;
+		value->last_topup = ktime_get_ns() - (100 * settings.topup_interval_ns);
+
+		if (!ratelimit_check_and_take(&key, &settings))
+			test_fatal("Rate limit not allowed after topup");
+
+		if (value->tokens != settings.bucket_size - 1)
+			test_fatal("Unexpected token amount after topup");
+	})
+
+	test_finish();
+}

commit 3b3ebecc2975e91733005e1aa3bdd785f5e177a4

bpf: Always inline ratelimit_check_and_take
After enabling coverage for EVENTS_MAP_RATE_LIMIT, clang sometimes
uninlines ratelimit_check_and_take (marked as inline, but not
__always_inline), which produces this verification error in bpf_host
and bpf_lxc on kernel 5.4:

Error: program tail_ipv4_to_endpoint: load program: invalid argument: tail_calls are not allowed in programs with bpf-to-bpf calls

Mark ratelimit_check_and_take as __always_inline to suppress this
behavior.

Fixes: 93f1619cb472 ("bpf: Add ratelimiting feature and use it for ICMPv6 responses")
Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

bpf/lib/ratelimit.h
@@ -57,8 +57,8 @@ struct ratelimit_settings {
 	__u64 topup_interval_ns;
 };
 
-static inline bool ratelimit_check_and_take(struct ratelimit_key *key,
-					    const struct ratelimit_settings *settings)
+static __always_inline bool ratelimit_check_and_take(struct ratelimit_key *key,
+						     const struct ratelimit_settings *settings)
 {
 	struct ratelimit_value *value;
 	struct ratelimit_value new_value;