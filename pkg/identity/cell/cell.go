f1850710cf63e7feccd3415b06454ca5806108fc
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-03-31 17:43:15 +0200

    identity: provide identity cell
    Currently, multiple identity cells (manager, cache, api) are individually
    registered in the agent cell.
    
    This commit creates a new identity cell that combines the subcells. This way
    only one cell needs to be registered in the agent.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

---
+++ pkg/identity/cell/cell.go
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package identityapi
+
+import (
+	"github.com/cilium/hive/cell"
+
+	identityapi "github.com/cilium/cilium/pkg/identity/api"
+	identitycache "github.com/cilium/cilium/pkg/identity/cache/cell"
+	"github.com/cilium/cilium/pkg/identity/identitymanager"
+)
+
+// Cell provides the identity controlplane that is responsible to allocate and manage security identities
+var Cell = cell.Module(
+	"identity",
+	"Identity ControlPlane",
+
+	// IdentityManager maintains the set of identities and a count of its users.
+	identitymanager.Cell,
+
+	// Provides IdentityAllocators (Responsible for allocating security identities)
+	identitycache.Cell,
+
+	// IdentityApiHandler provides the Identity Cilium API
+	identityapi.Cell,
+)
