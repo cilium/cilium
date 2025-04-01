dcbf6d14c29d8b01da1c14bf535499b31884dc81
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-04-01 21:14:55 +0200

    fqdn: extract dnsrequesthandler
    This commit extracts the DNS request handling from the
    FQDN boostrapper into its own Hive component.
    
    Logically it's not part of the bootstrap process. By extracting,
    it can also be easier reused in other components (e.g. upcoming
    standalone dns proxy)
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/fqdn/bootstrap/cell.go
+++ pkg/fqdn/bootstrap/cell.go
@@ -24,6 +24,7 @@ var Cell = cell.Module(
 	"Bootstraps the FQDN policy subsystem",
 
 	cell.Provide(newFQDNProxyBootstrapper),
+	cell.Provide(newDNSRequestHandler),
 )
 
 type fqdnProxyBootstrapperParams struct {
@@ -34,10 +35,10 @@ type fqdnProxyBootstrapperParams struct {
 	NameManager       namemanager.NameManager
 	ProxyInstance     defaultdns.Proxy
 	ProxyPorts        *proxy.Proxy
-	ProxyAccessLogger accesslog.ProxyAccessLogger
 	PolicyRepo        policy.PolicyRepository
 	IPCache           *ipcache.IPCache
 	EndpointManager   endpointmanager.EndpointManager
+	DNSRequestHandler DNSRequestHandler
 }
 
 func newFQDNProxyBootstrapper(params fqdnProxyBootstrapperParams) FQDNProxyBootstrapper {
@@ -49,10 +50,10 @@ func newFQDNProxyBootstrapper(params fqdnProxyBootstrapperParams) FQDNProxyBoots
 		nameManager:       params.NameManager,
 		proxyInstance:     params.ProxyInstance,
 		proxyPorts:        params.ProxyPorts,
-		proxyAccessLogger: params.ProxyAccessLogger,
 		policyRepo:        params.PolicyRepo,
 		ipcache:           params.IPCache,
 		endpointManager:   params.EndpointManager,
+		dnsRequestHandler: params.DNSRequestHandler,
 	}
 
 	params.Lifecycle.Append(cell.Hook{
@@ -63,4 +64,35 @@ func newFQDNProxyBootstrapper(params fqdnProxyBootstrapperParams) FQDNProxyBoots
 	})
 
 	return bootstrapper
+}
+
+type dnsRequestHandlerParams struct {
+	cell.In
+
+	Lifecycle         cell.Lifecycle
+	Logger            *slog.Logger
+	NameManager       namemanager.NameManager
+	ProxyInstance     defaultdns.Proxy
+	ProxyAccessLogger accesslog.ProxyAccessLogger
+}
+
+func newDNSRequestHandler(params dnsRequestHandlerParams) DNSRequestHandler {
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
+	handler := &dnsRequestHandler{
+		ctx:               ctx,
+		logger:            params.Logger,
+		nameManager:       params.NameManager,
+		proxyInstance:     params.ProxyInstance,
+		proxyAccessLogger: params.ProxyAccessLogger,
+	}
+
+	params.Lifecycle.Append(cell.Hook{
+		OnStop: func(hookContext cell.HookContext) error {
+			cancelCtx()
+			return nil
+		},
+	})
+
+	return handler
 }
