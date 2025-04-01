95533d30c7185bca508943323decaa0e12b8d1ba
Jordan Rife <jrife@google.com>
2025-04-01 20:36:36 +0000

    bpf: Implement socket iterator program and tests
    Implement bpf_sock_term.c, the program that handles UDP socket
    destruction, and its unit tests. cil_sock_udp_destroy is meant to be
    used in conjunction with BPF socket iterators to iterate through all
    sockets in a network namespace and destroy any matching the configured
    filter. cilium_sock_term_filter is a single-element array containing the
    filter, a value consisting of a backend's address family, address, and
    port.
    
    The program combines the current socket's cookie and the configured
    filter into a cilium_lb(4|6)_reverse_sk map key and checks for the
    existence of that key to see if this socket was connected to the now
    obsolete backend. If so, it destroys it with the bpf_sock_destroy()
    kfunc.
    
    An earlier prototype of the program simply used DECLARE_CONFIG for each
    of the filter fields, requiring a new program to be configured and
    loaded for each new backend filter. This was convenient since there was
    no need to coordinate access to the map in userspace between parallel
    calls to TerminateUDPConnectionsToBackend(), but was ultimately too slow
    (about 100x slower according to a simple benchmark). So, userspace needs
    to synchronize access to the map and this program using a mutex. Each
    "iterator instance" needs to follow this sequence:
    
    1) Acquire some lock.
    2) Set the value of the filter in cilium_sock_term_filter.
    3) Create a new socket iterator.
    4) Read all items in the iterator.
    5) Release the lock.
    
    It's OK for different calls to TerminateUDPConnectionsToBackend() to
    intertwine as long as these "critical sections" are sequential between
    threads.
    
    Fixes: #37907
    
    Signed-off-by: Jordan Rife <jrife@google.com>

--- bpf/lib/sock.h
+++ bpf/lib/sock.h
@@ -16,6 +16,27 @@ sock_local_cookie(struct bpf_sock_addr *ctx)
 #endif
 }
 
+struct sock_term_filter {
+	union {
+		union v6addr addr6;
+		struct {
+			char pad[12];
+			__be32 addr4;
+		};
+	} address __packed;
+	__be16 port;
+	__u8 address_family;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__type(key, __u32);
+	__type(value, struct sock_term_filter);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, 1);
+	__uint(map_flags, LRU_MEM_FLAVOR);
+} cilium_sock_term_filter __section_maps_btf;
+
 #ifdef ENABLE_IPV4
 struct {
 	__uint(type, BPF_MAP_TYPE_LRU_HASH);
