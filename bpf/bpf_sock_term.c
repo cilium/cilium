1964f47130d8ea794ab6ba7d31be48c63d2ab5df
Jordan Rife <jrife@google.com>
2025-04-01 20:36:36 +0000

    bpf: Implement socket iterator program and tests
    Implement bpf_sock_term.c, the program that handles UDP socket
    destruction, and its unit tests. cil_sock_udp_destroy is meant to be
    used in conjunction with BPF socket iterators to iterate through all
    sockets in a network namespace and destroy any matching the configured
    filter. cilium_sock_term_filter is a single-element array containing the
    filter, a value consisting of a backend's address family, address, and
    port.
    
    The program combines the current socket's cookie and the configured
    filter into a cilium_lb(4|6)_reverse_sk map key and checks for the
    existence of that key to see if this socket was connected to the now
    obsolete backend. If so, it destroys it with the bpf_sock_destroy()
    kfunc.
    
    An earlier prototype of the program simply used DECLARE_CONFIG for each
    of the filter fields, requiring a new program to be configured and
    loaded for each new backend filter. This was convenient since there was
    no need to coordinate access to the map in userspace between parallel
    calls to TerminateUDPConnectionsToBackend(), but was ultimately too slow
    (about 100x slower according to a simple benchmark). So, userspace needs
    to synchronize access to the map and this program using a mutex. Each
    "iterator instance" needs to follow this sequence:
    
    1) Acquire some lock.
    2) Set the value of the filter in cilium_sock_term_filter.
    3) Create a new socket iterator.
    4) Read all items in the iterator.
    5) Release the lock.
    
    It's OK for different calls to TerminateUDPConnectionsToBackend() to
    intertwine as long as these "critical sections" are sequential between
    threads.
    
    Fixes: #37907
    
    Signed-off-by: Jordan Rife <jrife@google.com>

---
+++ bpf/bpf_sock_term.c
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#include <bpf/ctx/unspec.h>
+#include <bpf/api.h>
+
+#include <node_config.h>
+#include <lib/static_data.h>
+
+#include "bpf/compiler.h"
+#include "lib/common.h"
+#include "lib/sock.h"
+#include "lib/sock_term.h"
+
+/* Stub out types that would normally be found in vmlinux.h to satisfy BTF type
+ * checks
+ */
+struct seq_file {};
+
+struct bpf_iter_meta {
+	struct seq_file *seq;
+};
+
+struct bpf_iter__udp {
+	struct bpf_iter_meta *meta;
+	void *udp_sk;
+};
+
+struct sock_common {};
+
+#ifndef TEST_BPF_SOCK_TERM
+int bpf_sock_destroy(struct sock_common *sk) __section(".ksyms");
+static int BPF_FUNC(seq_write, struct seq_file *m, const void *data,
+		    __u32 len);
+#endif
+
+static __always_inline
+bool matches_v4(const struct sock_term_filter *filter, __sock_cookie cookie)
+{
+	struct ipv4_revnat_tuple key = {};
+
+	key.address = filter->address.addr4;
+	key.port    = filter->port;
+	key.cookie  = cookie;
+
+	return map_lookup_elem(&cilium_lb4_reverse_sk, &key);
+}
+
+static __always_inline
+bool matches_v6(const struct sock_term_filter *filter, __sock_cookie cookie)
+{
+	struct ipv6_revnat_tuple key = {};
+
+	key.address = filter->address.addr6;
+	key.port    = filter->port;
+	key.cookie  = cookie;
+
+	return map_lookup_elem(&cilium_lb6_reverse_sk, &key);
+}
+
+static __always_inline
+int sock_udp_destroy(struct bpf_iter__udp *ctx)
+{
+	struct sock_term_filter *filter;
+	void *sk = ctx->udp_sk;
+	bool matches = false;
+	__sock_cookie cookie;
+	__u32 zero = 0;
+
+	if (!sk)
+		return 0;
+
+	filter = map_lookup_elem(&cilium_sock_term_filter, &zero);
+	if (!filter)
+		return 0;
+
+	cookie = get_socket_cookie(sk);
+	switch (filter->address_family) {
+	case AF_INET:
+		matches = matches_v4(filter, cookie);
+		break;
+	case AF_INET6:
+		matches = matches_v6(filter, cookie);
+		break;
+	}
+
+	if (!matches)
+		return 0;
+
+	if (!bpf_sock_destroy(sk))
+		seq_write(ctx->meta->seq, &cookie, sizeof(cookie));
+
+	return 0;
+}
+
+__section("iter/udp")
+int cil_sock_udp_destroy(struct bpf_iter__udp *ctx)
+{
+	return sock_udp_destroy(ctx);
+}
+
+BPF_LICENSE("Dual BSD/GPL");
