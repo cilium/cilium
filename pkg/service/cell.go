37ea501dc6828e9c3bbcc2c57252f66712d651c8
Jordan Rife <jrife@google.com>
2025-04-01 21:21:48 +0000

    service: Initialize and plumb in filter maps
    Implement SockTermFilterMap to allow the agent to initialize the filter
    map and set the filter value. Ensure the filter map is initialized and
    passed in as one of the serviceManagerParams; later commits need this to
    be ready so it can be passed to backendConnectionHandler.
    
    Signed-off-by: Jordan Rife <jrife@google.com>

--- pkg/service/cell.go
+++ pkg/service/cell.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/datapath/types"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/maps/filter"
 	monitorAgent "github.com/cilium/cilium/pkg/monitor/agent"
 )
 
@@ -26,6 +27,8 @@ var Cell = cell.Module(
 
 	cell.ProvidePrivate(func(sm ServiceManager) syncNodePort { return sm }),
 	cell.Invoke(registerServiceReconciler),
+
+	filter.Cell,
 )
 
 type serviceManagerParams struct {
@@ -40,6 +43,8 @@ type serviceManagerParams struct {
 	HealthCheckers []HealthChecker `group:"healthCheckers"`
 	Clientset      k8sClient.Clientset
 	NodeNeighbors  types.NodeNeighbors
+
+	SockTermFilter *filter.SockTermFilterMap
 }
 
 func newServiceInternal(params serviceManagerParams) *Service {
