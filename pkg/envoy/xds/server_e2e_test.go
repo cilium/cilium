7afd21e7c373c472d286548786c7dd49e6134b6a
Marcel Zieba <marcel.zieba@isovalent.com>
2025-04-02 14:38:18 +0200

    xds: fix test TestRequestHighVersionFromTheStart
    First request on new stream always has empty nonce.
    
    Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>

--- pkg/envoy/xds/server_e2e_test.go
+++ pkg/envoy/xds/server_e2e_test.go
@@ -1066,7 +1066,7 @@ func TestRequestHighVersionFromTheStart(t *testing.T) {
 		VersionInfo:   "64",
 		Node:          nodes[node0],
 		ResourceNames: nil,
-		ResponseNonce: "64",
+		ResponseNonce: "",
 	}
 	err = stream.SendRequest(req)
 	require.NoError(t, err)
