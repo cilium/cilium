cilium/cilium

commit fa94009d44b528352763ea7b3a552fa24452964b

init.sh: move ipip device creation to Go
This refactors the creation of the cilium_ipip{4,6} devices to a
Go-based implementation and adds a simple device creation test.

One behavioural change was made: no longer try to repurpose
tunl0 by renaming it and enabling collect_md, as that dance is
rather tricky and fragile due to the kernel module needing to be
loaded first.

With this change, we still prefix the fallback devices with cilium_,
meaning cilium_tunl0 for v4 and cilium_ip6tnl0 for v6, but we no
longer try to use or reconfigure these devices. cilium_ipip4/6 are
always created with the correct parameters from the start, reducing
the amount of moving parts.

Signed-off-by: Robin Gögge <r.goegge@isovalent.com>
Co-authored-by: Timo Beckers <timo@isovalent.com>

pkg/datapath/loader/base.go
@@ -319,9 +319,6 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		encapProto = option.Config.TunnelProtocol
 	case option.Config.EnableHealthDatapath:
 		mode = option.DSRDispatchIPIP
-		sysSettings = append(sysSettings,
-			sysctl.Setting{Name: "net.core.fb_tunnels_only_for_init_net",
-				Val: "2", IgnoreErr: true})
 	default:
 		mode = directMode
 	}
@@ -351,6 +348,18 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 	}
 	args[initArgHostDev1] = hostDev1.Attrs().Name
 	args[initArgHostDev2] = hostDev2.Attrs().Name
+
+	if option.Config.EnableHealthDatapath {
+		sysSettings = append(
+			sysSettings,
+			sysctl.Setting{
+				Name: "net.core.fb_tunnels_only_for_init_net", Val: "2", IgnoreErr: true,
+			},
+		)
+		if err := setupIPIPDevices(option.Config.IPv4Enabled(), option.Config.IPv6Enabled()); err != nil {
+			return fmt.Errorf("unable to create ipip encapsulation devices for health datapath")
+		}
+	}
 
 	if option.Config.IPAM == ipamOption.IPAMENI {
 		var err error

pkg/datapath/loader/netlink.go
@@ -226,7 +226,8 @@ func RemoveTCFilters(ifName string, tcDir uint32) error {
 	return nil
 }
 
-func setupDev(link netlink.Link) error {
+// enableForwarding puts the given link into the up state and enables IP forwarding.
+func enableForwarding(link netlink.Link) error {
 	ifName := link.Attrs().Name
 
 	if err := netlink.LinkSetUp(link); err != nil {
@@ -284,14 +285,14 @@ func setupVethPair(name, peerName string) error {
 	if err != nil {
 		return err
 	}
-	if err := setupDev(veth); err != nil {
+	if err := enableForwarding(veth); err != nil {
 		return err
 	}
 	peer, err := netlink.LinkByName(peerName)
 	if err != nil {
 		return err
 	}
-	if err := setupDev(peer); err != nil {
+	if err := enableForwarding(peer); err != nil {
 		return err
 	}
 
@@ -442,5 +443,145 @@ func addHostDeviceAddr(hostDev netlink.Link, ipv4, ipv6 net.IP) error {
 		}
 
 	}
+	return nil
+}
+
+// setupIPIPDevices ensures the specified v4 and/or v6 devices are created and
+// configured with their respective sysctls.
+//
+// Calling this function may result in tunl0 (v4) or ip6tnl0 (v6) fallback
+// interfaces being created as a result of loading the ipip and ip6_tunnel
+// kernel modules by creating cilium_ tunnel interfaces. These are catch-all
+// interfaces for the ipip decapsulation stack. By default, these interfaces
+// will be created in new network namespaces, but Cilium disables this behaviour
+// by setting net.core.fb_tunnels_only_for_init_net = 2.
+//
+// In versions of Cilium prior to 1.15, the behaviour was as follows:
+//   - Repurpose the default tunl0 by setting it into collect_md mode and renaming
+//     it to cilium_ipip4. Use the interface for production traffic.
+//   - The same cannot be done for ip6tunl0, as collect_md cannot be enabled on
+//     this interface. Leave it unused.
+//   - Rename sit0 to cilium_sit, if present. This was potentially a mistake,
+//     as the sit module is not involved with ip6tnl interfaces.
+//
+// As of Cilium 1.15, if present, tunl0 is renamed to cilium_tunl and ip6tnl0 is
+// renamed to cilium_ip6tnl. This is to communicate to the user that Cilium has
+// taken control of the encapsulation stack on the node, as it currently doesn't
+// explicitly support sharing it with other tools/CNIs. Fallback devices are left
+// unused for production traffic. Only devices that were explicitly created are used.
+func setupIPIPDevices(ipv4, ipv6 bool) error {
+	// FlowBased sets IFLA_IPTUN_COLLECT_METADATA, the equivalent of 'ip link add
+	// ... type ipip/ip6tnl external'. This is needed so bpf programs can use
+	// bpf_skb_[gs]et_tunnel_key() on packets flowing through tunnels.
+
+	if ipv4 {
+		// Set up IPv4 tunnel device if requested.
+		if _, err := ensureDevice(&netlink.Iptun{
+			LinkAttrs: netlink.LinkAttrs{Name: defaults.IPIPv4Device},
+			FlowBased: true,
+		}); err != nil {
+			return fmt.Errorf("creating %s: %w", defaults.IPIPv4Device, err)
+		}
+
+		// Rename fallback device created by potential kernel module load after
+		// creating tunnel interface.
+		if err := renameDevice("tunl0", "cilium_tunl"); err != nil {
+			return fmt.Errorf("renaming fallback device %s: %w", "tunl0", err)
+		}
+	} else {
+		if err := removeDevice(defaults.IPIPv4Device); err != nil {
+			return fmt.Errorf("removing %s: %w", defaults.IPIPv4Device, err)
+		}
+	}
+
+	if ipv6 {
+		// Set up IPv6 tunnel device if requested.
+		if _, err := ensureDevice(&netlink.Ip6tnl{
+			LinkAttrs: netlink.LinkAttrs{Name: defaults.IPIPv6Device},
+			FlowBased: true,
+		}); err != nil {
+			return fmt.Errorf("creating %s: %w", defaults.IPIPv6Device, err)
+		}
+
+		// Rename fallback device created by potential kernel module load after
+		// creating tunnel interface.
+		if err := renameDevice("ip6tnl0", "cilium_ip6tnl"); err != nil {
+			return fmt.Errorf("renaming fallback device %s: %w", "tunl0", err)
+		}
+	} else {
+		if err := removeDevice(defaults.IPIPv6Device); err != nil {
+			return fmt.Errorf("removing %s: %w", defaults.IPIPv6Device, err)
+		}
+	}
+
+	return nil
+}
+
+// ensureDevice ensures a device with the given attrs is present on the system.
+// If a device with the given name already exists, device creation is skipped and
+// the existing device will be used as-is for the subsequent configuration steps.
+// The device is never recreated.
+//
+// The device's state is set to 'up', L3 forwarding sysctls are applied, and MTU
+// is set.
+func ensureDevice(attrs netlink.Link) (netlink.Link, error) {
+	name := attrs.Attrs().Name
+
+	// Reuse existing tunnel interface created by previous runs.
+	l, err := netlink.LinkByName(name)
+	if err != nil {
+		if err := netlink.LinkAdd(attrs); err != nil {
+			return nil, fmt.Errorf("creating device %s: %w", name, err)
+		}
+
+		// Fetch the link we've just created.
+		l, err = netlink.LinkByName(name)
+		if err != nil {
+			return nil, fmt.Errorf("retrieving created device %s: %w", name, err)
+		}
+	}
+
+	if err := enableForwarding(l); err != nil {
+		return nil, fmt.Errorf("setting up device %s: %w", name, err)
+	}
+
+	// Update MTU on the link if necessary.
+	wantMTU, gotMTU := attrs.Attrs().MTU, l.Attrs().MTU
+	if wantMTU != 0 && wantMTU != gotMTU {
+		if err := netlink.LinkSetMTU(l, wantMTU); err != nil {
+			return nil, fmt.Errorf("setting MTU on %s: %w", name, err)
+		}
+	}
+
+	return l, nil
+}
+
+// removeDevice removes the device with the given name. Returns error if the
+// device exists but was unable to be removed.
+func removeDevice(name string) error {
+	link, err := netlink.LinkByName(name)
+	if err != nil {
+		return nil
+	}
+
+	if err := netlink.LinkDel(link); err != nil {
+		return fmt.Errorf("removing device %s: %w", name, err)
+	}
+
+	return nil
+}
+
+// renameDevice renames a network device from and to a given value. Returns nil
+// if the device does not exist.
+func renameDevice(from, to string) error {
+	link, err := netlink.LinkByName(from)
+	if err != nil {
+		return nil
+	}
+
+	if err := netlink.LinkSetName(link, to); err != nil {
+		return fmt.Errorf("renaming device %s to %s: %w", from, to, err)
+	}
+
 	return nil
 }

bpf/init.sh
@@ -190,54 +190,6 @@ move_local_rules
 setup_proxy_rules
 fi
 
-if [ "$MODE" = "ipip" ]; then
-	if [ "$IP4_HOST" != "<nil>" ]; then
-		ENCAP_DEV="cilium_ipip4"
-		ip link show $ENCAP_DEV || {
-			# Upon module load it will create a non-removable tunl0
-			# device. Instead of creating an additional useless one,
-			# rename tunl0 with cilium prefix in a second step. If
-			# we to do 'ip link add name $ENCAP_DEV [...]' it would
-			# create two devices. :/
-			ip link add name tunl0 type ipip external || true
-			ip link set tunl0 name $ENCAP_DEV
-		}
-		setup_dev $ENCAP_DEV || encap_fail
-
-		ENCAP_IDX=$(cat "${SYSCLASSNETDIR}/${ENCAP_DEV}/ifindex")
-		sed -i '/^#.*ENCAP4_IFINDEX.*$/d' $RUNDIR/globals/node_config.h
-		echo "#define ENCAP4_IFINDEX $ENCAP_IDX" >> $RUNDIR/globals/node_config.h
-	else
-		ip link del cilium_ipip4 2> /dev/null || true
-	fi
-	if [ "$IP6_HOST" != "<nil>" ]; then
-		ENCAP_DEV="cilium_ipip6"
-		ip link show $ENCAP_DEV || {
-			# For cilium_ipip6 device, we unfortunately cannot use the
-			# same workaround as cilium_ipip4. While the latter allows
-			# to set an existing tunl0 into collect_md mode, the default
-			# ip6tnl0 if present cannot. It's quite annoying, but if v6
-			# was built into the kernel, we might just need to live with
-			# it. Default device creation can still be worked around
-			# via boot param if the sysctl from agent won't do it.
-			ip link add name $ENCAP_DEV type ip6tnl external || true
-			ip link set sit0 name cilium_sit || true
-		}
-		setup_dev $ENCAP_DEV || encap_fail
-
-		ENCAP_IDX=$(cat "${SYSCLASSNETDIR}/${ENCAP_DEV}/ifindex")
-		sed -i '/^#.*ENCAP6_IFINDEX.*$/d' $RUNDIR/globals/node_config.h
-		echo "#define ENCAP6_IFINDEX $ENCAP_IDX" >> $RUNDIR/globals/node_config.h
-	else
-		ip link del cilium_ipip6 2> /dev/null || true
-		ip link del cilium_sit   2> /dev/null || true
-	fi
-else
-	ip link del cilium_ipip4 2> /dev/null || true
-	ip link del cilium_ipip6 2> /dev/null || true
-	ip link del cilium_sit   2> /dev/null || true
-fi
-
 # Remove eventual existing encapsulation device from previous run
 case "${TUNNEL_PROTOCOL}" in
   "<nil>")

pkg/datapath/linux/config/config.go
@@ -738,6 +738,23 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		}
 	}
 
+	if option.Config.EnableHealthDatapath {
+		if option.Config.IPv4Enabled() {
+			ipip4, err := netlink.LinkByName(defaults.IPIPv4Device)
+			if err != nil {
+				return err
+			}
+			cDefinesMap["ENCAP4_IFINDEX"] = fmt.Sprintf("%d", ipip4.Attrs().Index)
+		}
+		if option.Config.IPv6Enabled() {
+			ipip6, err := netlink.LinkByName(defaults.IPIPv6Device)
+			if err != nil {
+				return err
+			}
+			cDefinesMap["ENCAP6_IFINDEX"] = fmt.Sprintf("%d", ipip6.Attrs().Index)
+		}
+	}
+
 	// Since golang maps are unordered, we sort the keys in the map
 	// to get a consistent written format to the writer. This maintains
 	// the consistency when we try to calculate hash for a datapath after

pkg/datapath/loader/netlink_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/vishvananda/netlink"
 
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/netns"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/sysctl"
@@ -83,7 +84,7 @@ func TestSetupDev(t *testing.T) {
 		err := netlink.LinkAdd(dummy)
 		require.NoError(t, err)
 
-		err = setupDev(dummy)
+		err = enableForwarding(dummy)
 		require.NoError(t, err)
 
 		enabledSettings := []string{
@@ -269,6 +270,53 @@ func TestRemoveTCPrograms(t *testing.T) {
 
 		err = netlink.LinkDel(dummy)
 		require.NoError(t, err)
+
+		return nil
+	})
+}
+
+func TestSetupIPIPDevices(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	netnsName := "test-setup-ipip-devs"
+	netns0, err := netns.ReplaceNetNSWithName(netnsName)
+	require.NoError(t, err)
+	require.NotNil(t, netns0)
+	t.Cleanup(func() {
+		netns0.Close()
+		netns.RemoveNetNSWithName(netnsName)
+	})
+
+	netns0.Do(func(_ ns.NetNS) error {
+		err := setupIPIPDevices(true, true)
+		require.NoError(t, err)
+
+		_, err = netlink.LinkByName(defaults.IPIPv4Device)
+		require.NoError(t, err)
+
+		_, err = netlink.LinkByName(defaults.IPIPv6Device)
+		require.NoError(t, err)
+
+		_, err = netlink.LinkByName("cilium_tunl")
+		require.NoError(t, err)
+
+		_, err = netlink.LinkByName("cilium_ip6tnl")
+		require.NoError(t, err)
+
+		_, err = netlink.LinkByName("tunl0")
+		require.Error(t, err)
+
+		_, err = netlink.LinkByName("ip6tnl0")
+		require.Error(t, err)
+
+		err = setupIPIPDevices(false, false)
+		require.NoError(t, err)
+
+		_, err = netlink.LinkByName(defaults.IPIPv4Device)
+		require.Error(t, err)
+
+		_, err = netlink.LinkByName(defaults.IPIPv6Device)
+		require.Error(t, err)
 
 		return nil
 	})

pkg/defaults/node.go
@@ -23,6 +23,12 @@ const (
 	// SecondHostDevice is the name of the second interface of the host veth pair.
 	SecondHostDevice = "cilium_net"
 
+	// IPIPv4Device is a device of type 'ipip', created by the agent.
+	IPIPv4Device = "cilium_ipip4"
+
+	// IPIPv6Device is a device of type 'ip6tnl', created by the agent.
+	IPIPv6Device = "cilium_ipip6"
+
 	// CiliumK8sAnnotationPrefix is the prefix key for the annotations used in kubernetes.
 	CiliumK8sAnnotationPrefix = "cilium.io/"
 

commit 5213490d7a5925e0d60d7996ab982e32999fd987

cilium, ipip: Apply fb_tunnels_only_for_init_net before creating devs
This sysctl needs to be applied before we attempt to create the IPIP
devices since it prevents the kernel IPIP driver to create the legacy
device:

  Controls if fallback tunnels (like tunl0, gre0, gretap0, erspan0, sit0,
  ip6tnl0, ip6gre0) are automatically created when a new network namespace
  is created, if corresponding tunnel is present in initial network
  namespace. [...]

For more details, see the comment in setupIPIPDevices().

Fixes: fa94009d44b5 ("init.sh: move ipip device creation to Go")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/datapath/loader/base.go
@@ -384,12 +384,13 @@ func (l *loader) Reinitialize(ctx context.Context, lnc *datapath.LocalNodeConfig
 	}
 
 	if option.Config.EnableHealthDatapath || option.Config.EnableIPIPTermination {
-		sysSettings = append(
-			sysSettings,
-			tables.Sysctl{
-				Name: []string{"net", "core", "fb_tunnels_only_for_init_net"}, Val: "2", IgnoreErr: true,
-			},
-		)
+		// This setting needs to be applied before creating the IPIP devices.
+		sysIPIP := []tables.Sysctl{
+			{Name: []string{"net", "core", "fb_tunnels_only_for_init_net"}, Val: "2", IgnoreErr: true},
+		}
+		if err := l.sysctl.ApplySettings(sysIPIP); err != nil {
+			return err
+		}
 		if err := setupIPIPDevices(l.sysctl, option.Config.IPv4Enabled(), option.Config.IPv6Enabled(), lnc.DeviceMTU); err != nil {
 			return fmt.Errorf("unable to create ipip devices: %w", err)
 		}

pkg/datapath/loader/netlink.go
@@ -315,7 +315,8 @@ func setupVxlanDevice(sysctl sysctl.Sysctl, port, srcPortLow, srcPortHigh uint16
 // renamed to cilium_ip6tnl. This is to communicate to the user that Cilium has
 // taken control of the encapsulation stack on the node, as it currently doesn't
 // explicitly support sharing it with other tools/CNIs. Fallback devices are left
-// unused for production traffic. Only devices that were explicitly created are used.
+// unused for production traffic. Only devices that were explicitly created are
+// used. As of Cilium 1.18, cilium_tunl and cilium_ip6tnl are not created anymore.
 func setupIPIPDevices(sysctl sysctl.Sysctl, ipv4, ipv6 bool, mtu int) error {
 	// FlowBased sets IFLA_IPTUN_COLLECT_METADATA, the equivalent of 'ip link add
 	// ... type ipip/ip6tnl external'. This is needed so bpf programs can use