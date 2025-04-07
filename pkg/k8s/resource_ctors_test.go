a05c0137ebf7b45a63637d9c7c57ef9c3a76bed9
Marcel Zieba <marcel.zieba@isovalent.com>
2025-04-07 11:50:57 +0000

    k8s: fix missing service endpoints delete events
    [ upstream commit d82ee4049771fb831b816c2bb9f96f542db34d42 ]
    
    In case of problem with k8s connectivity, Reflector performs relist
    operation and DeltaFIFO generates Sync events for already existing elements and
    Delete events for removed elements. However, we were not handling
    events DeletedFinalStateUnknown which resulted in stale cache for
    services.
    
    Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

---
+++ pkg/k8s/resource_ctors_test.go
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package k8s
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"k8s.io/client-go/tools/cache"
+)
+
+func TestEndpointTransformPreservesThombstoneKey(t *testing.T) {
+	unknownObj := 100
+	endpoint := cache.DeletedFinalStateUnknown{
+		Key: "default/some-service",
+		Obj: unknownObj,
+	}
+
+	result, err := transformEndpoint(endpoint)
+	require.NoError(t, err)
+	tombstone, ok := result.(cache.DeletedFinalStateUnknown)
+	require.True(t, ok)
+	require.Equal(t, endpoint.Key, tombstone.Key)
+}
