cilium/cilium

commit 212c22832589e3d3bdd973a04e9e872572cb8f53

connectivity: Add test for source egress in Ingress
This test will cover both positive and negative cases:

- Request from any client pods to cilium-ingress-same-node will be
  allowed.
- Request from any client pods to cilium-ingress-other-node will be
  denied.

Relates: https://github.com/cilium/proxy/pull/1172
Signed-off-by: Tam Mach <tam.mach@cilium.io>

cilium-cli/connectivity/builder/pod_to_ingress_service.go
@@ -5,6 +5,7 @@ package builder
 
 import (
 	_ "embed"
+	"strings"
 
 	"github.com/cilium/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium/cilium-cli/connectivity/tests"
@@ -20,11 +21,14 @@ var (
 
 	//go:embed manifests/deny-ingress-entity.yaml
 	denyIngressIdentityPolicyYAML string
+
+	//go:embed manifests/deny-ingress-source-egress-other-node.yaml
+	denyIngressSourceEgressOtherNodePolicyYML string
 )
 
 type podToIngressService struct{}
 
-func (t podToIngressService) build(ct *check.ConnectivityTest, _ map[string]string) {
+func (t podToIngressService) build(ct *check.ConnectivityTest, templates map[string]string) {
 	// Test Ingress controller
 	newTest("pod-to-ingress-service", ct).
 		WithFeatureRequirements(features.RequireEnabled(features.IngressController)).
@@ -57,6 +61,19 @@ func (t podToIngressService) build(ct *check.ConnectivityTest, _ map[string]stri
 		WithCiliumPolicy(denyIngressIdentityPolicyYAML).
 		WithScenarios(tests.PodToIngress()).
 		WithExpectations(func(_ *check.Action) (egress check.Result, ingress check.Result) {
+			return check.ResultDefaultDenyEgressDrop, check.ResultNone
+		})
+
+	newTest("pod-to-ingress-service-deny-source-egress-other-node", ct).
+		WithCiliumVersion(">1.17.1 >1.16.7 >1.15.14").
+		WithFeatureRequirements(features.RequireEnabled(features.IngressController)).
+		WithCiliumPolicy(denyIngressSourceEgressOtherNodePolicyYML).
+		WithCiliumPolicy(templates["clientEgressOnlyDNSPolicyYAML"]). // DNS resolution only
+		WithScenarios(tests.PodToIngress()).
+		WithExpectations(func(a *check.Action) (egress check.Result, ingress check.Result) {
+			if strings.Contains(a.Destination().Name(), "cilium-ingress-same-node") {
+				return check.ResultOK, check.ResultOK
+			}
 			return check.ResultDefaultDenyEgressDrop, check.ResultNone
 		})
 }

cilium-cli/connectivity/builder/manifests/deny-ingress-source-egress-other-node.yaml
@@ -1,0 +1,12 @@
+apiVersion: "cilium.io/v2"
+kind: CiliumNetworkPolicy
+metadata:
+  name: "ingress-source-egress-deny-other-node"
+spec:
+  endpointSelector:
+    matchLabels:
+      kind: client
+  egress:
+  - toEndpoints:
+    - matchLabels:
+        name: echo-same-node

commit c47c712a1804056c38b1896672edbd0b63c4ad90

connectivity: Correct version check for Ingress test
The conditions should be including per minor version with OR operation.
Otherwise, the test will be skipped like below

```
Skipping test [pod-to-ingress-service-deny-source-egress-other-node] [20/22] (requires Cilium version >1.17.1 >1.16.7 >1.15.14 but running 1.16.8)
```

Fixes: 212c22832589e3d3bdd973a04e9e872572cb8f53
Reported-by: Julian Wiedmann <jwi@isovalent.com>
Suggested-by: Simone Magnani <simone.magnani@isovalent.com>
Signed-off-by: Tam Mach <tam.mach@cilium.io>

cilium-cli/connectivity/builder/pod_to_ingress_service.go
@@ -65,7 +65,7 @@ func (t podToIngressService) build(ct *check.ConnectivityTest, templates map[str
 		})
 
 	newTest("pod-to-ingress-service-deny-source-egress-other-node", ct).
-		WithCiliumVersion(">1.17.1 >1.16.7 >1.15.14").
+		WithCiliumVersion(">1.17.1 || >1.16.7 <1.17.0 || >1.15.14 <1.16.0").
 		WithFeatureRequirements(features.RequireEnabled(features.IngressController)).
 		WithCiliumPolicy(denyIngressSourceEgressOtherNodePolicyYML).
 		WithCiliumPolicy(templates["clientEgressOnlyDNSPolicyYAML"]). // DNS resolution only