cilium/cilium

commit c7760bdab8694de997069ce126e44b6e4e1cb6f1

sockets: use general purpose iterateNetlinkSockets function.
Instead of having a custom function for iterating UDP connections
rename and change this function to: iterateNetlinkSockets.

iterateNetlinkSockets will be able iterate any type of socket via
the netlink api.

In subsequent commits, this will be used to make the datapath/sockets
library a general purpose sockets library that allows for separately
iterating and destroying sockets via netlink.

Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

pkg/datapath/sockets/sockets.go
@@ -114,16 +114,10 @@ func (f *SocketFilter) MatchSocket(socket netlink.SocketID) bool {
 }
 
 func filterAndDestroyUDPSockets(family uint8, socketCB func(socket netlink.SocketID, err error)) error {
-	err := socketDiagUDPExecutor(family, func(m syscall.NetlinkMessage) error {
-		sockInfo := &Socket{}
-		err := sockInfo.Deserialize(m.Data)
+	return iterateNetlinkSockets(unix.IPPROTO_UDP, syscall.AF_INET, 0xffff, func(sockInfo *Socket, err error) error {
 		socketCB(sockInfo.ID, err)
 		return nil
 	})
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 // SocketRequest implements netlink.NetlinkRequestData to be used
@@ -241,8 +235,13 @@ func destroySocket(sockId netlink.SocketID, family uint8, protocol uint8) error 
 	return err
 }
 
-func socketDiagUDPExecutor(family uint8, receiver func(message syscall.NetlinkMessage) error) error {
-	s, err := nl.Subscribe(unix.NETLINK_INET_DIAG)
+// openSubscribeHandle opens a netlink socket sub.
+func openSubscribeHandle() (*nl.NetlinkSocket, error) {
+	return nl.Subscribe(unix.NETLINK_INET_DIAG)
+}
+
+func iterateNetlinkSockets(proto uint8, family uint8, stateFilter uint32, fn func(*Socket, error) error) error {
+	s, err := openSubscribeHandle()
 	if err != nil {
 		return err
 	}
@@ -251,22 +250,27 @@ func socketDiagUDPExecutor(family uint8, receiver func(message syscall.NetlinkMe
 	req := nl.NewNetlinkRequest(nl.SOCK_DIAG_BY_FAMILY, unix.NLM_F_DUMP)
 	req.AddData(&SocketRequest{
 		Family:   family,
-		Protocol: unix.IPPROTO_UDP,
-		States:   uint32(0xfff),
+		Protocol: uint8(proto),
+		States:   stateFilter,
 	})
-	s.Send(req)
+	if err := s.Send(req); err != nil {
+		return fmt.Errorf("failed to send netlink list request: %w", err)
+	}
 
 loop:
 	for {
 		msgs, from, err := s.Receive()
 		if err != nil {
-			return err
+			fn(nil, err)
+			continue loop
 		}
 		if from.Pid != nl.PidKernel {
-			return fmt.Errorf("Wrong sender portid %d, expected %d", from.Pid, nl.PidKernel)
+			fn(nil, fmt.Errorf("Wrong sender portid %d, expected %d", from.Pid, nl.PidKernel))
+			continue loop
 		}
 		if len(msgs) == 0 {
-			return errors.New("no message nor error from netlink")
+			fn(nil, errors.New("no message nor error from netlink"))
+			continue loop
 		}
 
 		for _, m := range msgs {
@@ -275,9 +279,12 @@ loop:
 				break loop
 			case unix.NLMSG_ERROR:
 				error := int32(native.Uint32(m.Data[0:4]))
-				return syscall.Errno(-error)
+				fn(nil, syscall.Errno(-error))
+				continue loop
 			}
-			if err := receiver(m); err != nil {
+			sockInfo := &Socket{}
+			err := sockInfo.Deserialize(m.Data)
+			if err := fn(sockInfo, err); err != nil {
 				return err
 			}
 		}

commit 2d53d5169dcbf9ea1644126d57fa60e1e2b0f657

sockets: Pass family through to iterateNetlinkSockets
Propagate family from filterAndDestroyUDPSockets to
iterateNetlinkSockets so that it works for AF_INET6 sockets as well.

Fixes: c7760bdab869 ("sockets: use general purpose iterateNetlinkSockets function.")

Signed-off-by: Jordan Rife <jrife@google.com>

pkg/datapath/sockets/sockets.go
@@ -137,7 +137,7 @@ func (f *SocketFilter) MatchSocket(socket netlink.SocketID) bool {
 }
 
 func filterAndDestroyUDPSockets(family uint8, socketCB func(socket netlink.SocketID, err error)) error {
-	return iterateNetlinkSockets(unix.IPPROTO_UDP, syscall.AF_INET, 0xffff, func(sockInfo *Socket, err error) error {
+	return iterateNetlinkSockets(unix.IPPROTO_UDP, family, 0xffff, func(sockInfo *Socket, err error) error {
 		socketCB(sockInfo.ID, err)
 		return nil
 	})