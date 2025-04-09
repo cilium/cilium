923bf94913a86c322db8e85fe25d44f6aff50ed0
Jussi Maki <jussi@isovalent.com>
2025-04-09 15:24:05 +0200

    ciliumenvoyconfig: Process backends separately from listeners
    As with the prior implementation, keep the "cec" resources separate from
    the "backendsync" resources since there can be multiple listeners referring to the
    same clusters ("services") and these should be able to be changed separately,
    e.g. listeners shouldn't need to be recomputed if backends change.
    
    Switch to using prototext for rendering the envoy resources so that we're
    checking for the whole message. Since prototext is on purpose not deterministic
    don't compare the marshalled format but instead compare the unmarshalled messages.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/ciliumenvoyconfig/exp_reconciler.go
+++ pkg/ciliumenvoyconfig/exp_reconciler.go
@@ -7,10 +7,10 @@ import (
 	"context"
 	"iter"
 	"log/slog"
-	"maps"
 
 	"github.com/cilium/statedb"
 	"github.com/cilium/statedb/reconciler"
+	"k8s.io/apimachinery/pkg/util/sets"
 
 	"github.com/cilium/cilium/pkg/envoy"
 	"github.com/cilium/cilium/pkg/loadbalancer/experimental"
@@ -23,15 +23,16 @@ type envoyOps struct {
 	xds           resourceMutator
 	policyTrigger policyTrigger
 	writer        *experimental.Writer
+	portAllocator PortAllocator
 }
 
 // Delete implements reconciler.Operations.
 func (ops *envoyOps) Delete(ctx context.Context, _ statedb.ReadTxn, res *EnvoyResource) error {
-	if len(res.Redirects) > 0 {
+	if res.Redirects.Len() > 0 {
 		// Remove redirects from services no longer selected by the CEC
 		wtxn := ops.writer.WriteTxn()
 		defer wtxn.Abort()
-		for name := range res.ReconciledRedirects {
+		for name := range res.ReconciledRedirects.All() {
 			svc, _, found := ops.writer.Services().Get(wtxn, experimental.ServiceByName(name))
 			if found {
 				svc = svc.Clone()
@@ -42,13 +43,31 @@ func (ops *envoyOps) Delete(ctx context.Context, _ statedb.ReadTxn, res *EnvoyRe
 		wtxn.Commit()
 	}
 
+	releasedListeners := sets.New[string]()
+
 	var err error
 	if prev := res.ReconciledResources; prev != nil {
 		// Perform the deletion with the resources that were last successfully reconciled
 		// instead of whatever the latest one is (which would have not been pushed to Envoy).
 		err = ops.xds.DeleteEnvoyResources(ctx, *prev)
+
+		for _, listener := range prev.Listeners {
+			ops.portAllocator.ReleaseProxyPort(listener.Name)
+			releasedListeners.Insert(listener.Name)
+		}
 	}
-	ops.policyTrigger.TriggerPolicyUpdates()
+
+	// Release the proxy ports of any unreconciled resources
+	for _, listener := range res.Resources.Listeners {
+		if !releasedListeners.Has(listener.Name) {
+			ops.portAllocator.ReleaseProxyPort(listener.Name)
+			releasedListeners.Insert(listener.Name)
+		}
+	}
+
+	if len(releasedListeners) > 0 {
+		ops.policyTrigger.TriggerPolicyUpdates()
+	}
 	return err
 }
 
@@ -68,26 +87,32 @@ func (ops *envoyOps) Update(ctx context.Context, txn statedb.ReadTxn, res *Envoy
 	if res.ReconciledResources != nil {
 		prevResources = *res.ReconciledResources
 	}
+
 	err := ops.xds.UpdateEnvoyResources(ctx, prevResources, resources)
 	if err == nil {
 		if prevResources.ListenersAddedOrDeleted(&resources) {
 			ops.policyTrigger.TriggerPolicyUpdates()
 		}
 
+		res.ReconciledResources = &resources
+		res.ReconciledResources.PortAllocationCallbacks = nil
+
 		// With the envoy resources successfully pushed to Envoy, set the proxy redirections
 		// for the associated services.
-		if len(res.Redirects) > 0 || len(res.ReconciledRedirects) > 0 {
+		if res.Redirects.Len() > 0 || res.ReconciledRedirects.Len() > 0 {
 			wtxn := ops.writer.WriteTxn()
-			for name, redirect := range res.Redirects {
+			orphanRedirects := res.ReconciledRedirects
+			for name, redirect := range res.Redirects.All() {
 				svc, _, found := ops.writer.Services().Get(wtxn, experimental.ServiceByName(name))
 				if found && !svc.ProxyRedirect.Equal(redirect) {
 					svc = svc.Clone()
 					svc.ProxyRedirect = redirect
 					ops.writer.UpsertService(wtxn, svc)
 				}
+				orphanRedirects = orphanRedirects.Delete(name)
 			}
-			for name := range res.ReconciledRedirects {
-				if _, found := res.Redirects[name]; !found {
+			for name := range orphanRedirects.All() {
+				if _, found := res.Redirects.Get(name); !found {
 					svc, _, found := ops.writer.Services().Get(wtxn, experimental.ServiceByName(name))
 					if found {
 						svc = svc.Clone()
@@ -97,10 +122,8 @@ func (ops *envoyOps) Update(ctx context.Context, txn statedb.ReadTxn, res *Envoy
 				}
 			}
 			wtxn.Commit()
-			res.ReconciledRedirects = maps.Clone(res.Redirects)
+			res.ReconciledRedirects = res.Redirects
 		}
-
-		res.ReconciledResources = &resources
 	}
 	return err
 }
@@ -115,6 +138,7 @@ func registerEnvoyReconciler(
 	params reconciler.Params,
 	writer *experimental.Writer,
 	envoyResources statedb.RWTable[*EnvoyResource],
+	portAllocator PortAllocator,
 ) error {
 	ops := &envoyOps{
 		config:        config,
@@ -122,6 +146,7 @@ func registerEnvoyReconciler(
 		xds:           xds,
 		writer:        writer,
 		policyTrigger: pt,
+		portAllocator: portAllocator,
 	}
 	_, err := reconciler.Register(
 		params,
