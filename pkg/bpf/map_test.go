67b0a1244550b179816e1eccf31e882a8cde368f
Florian Bütler <florian.buetler@proton.me>
2025-04-11 21:08:43 +0200

    bpf/maps: cleanup old proxymaps unconditionally
    Fixes: #7522
    
    Signed-off-by: Florian Bütler <florian.buetler@proton.me>

--- pkg/bpf/map_test.go
+++ pkg/bpf/map_test.go
@@ -27,6 +27,5 @@ func TestExtractCommonName(t *testing.T) {
 	assert.Equal(t, "policy", extractCommonName("cilium_policy_reserved_1"))
 	assert.Equal(t, "policy", extractCommonName("cilium_policy_v2_1157"))
 	assert.Equal(t, "policy", extractCommonName("cilium_policy_v2_reserved_1"))
-	assert.Equal(t, "proxy4", extractCommonName("cilium_proxy4"))
 	assert.Equal(t, "tunnel_map", extractCommonName("cilium_tunnel_map"))
 }
