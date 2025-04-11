8c369928346c5ba4f677e475b1301651d361d2a4
Dylan Reimerink <dylan.reimerink@isovalent.com>
2025-04-11 13:39:27 +0200

    cilium-cli/connectivity: Add `external-target-ipv6-capable` flag
    This commit adds a flag which can be used to tell the connectivity tests
    if the external targets we are running with are IPv6 capable. If
    targets are not IPv6 capable, or the environment we run in does
    not provide connectivity to IPv6, we will skip IPv6 variants for the
    `PodToWorld` and `PodToWorld2` tests.
    
    Since IPv6 traffic to external targets via L7 proxy is broken, we
    disable IPv6 for any tests that use the L7 proxy. To be switched to
    using this same flag in the future.
    
    Lastly, FQDN is currently tested together with L7 proxy, this is
    important to validate that these features work together. However,
    this means that we cannot run the FQDN with IPv6 at the moment.
    So we add a new tests which tests FQDN on its own where we can enable
    IPv6, and we disable the IPv6 variant of the FQDN test when using L7
    proxy.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- cilium-cli/connectivity/builder/to_fqdns.go
+++ cilium-cli/connectivity/builder/to_fqdns.go
@@ -19,10 +19,53 @@ func (t toFqdns) build(ct *check.ConnectivityTest, templates map[string]string) 
 	newTest("to-fqdns", ct).
 		WithCiliumPolicy(templates["clientEgressToFQDNsPolicyYAML"]).
 		WithCiliumPolicy(templates["clientEgressOnlyDNSPolicyYAML"]).
+		WithScenarios(
+			tests.PodToWorld(ct.Params().ExternalTargetIPv6Capable, tests.WithRetryDestPort(80)),
+			tests.PodToWorld2(ct.Params().ExternalTargetIPv6Capable), // resolves to ExternalOtherTarget
+		).
+		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
+			if a.Destination().Address(features.IPFamilyAny) == ct.Params().ExternalOtherTarget {
+				if a.Destination().Path() == "/" || a.Destination().Path() == "" {
+					// Expect packets to other external target to be dropped.
+					return check.ResultDropCurlTimeout, check.ResultNone
+				}
+				// Else expect HTTP drop by proxy
+				return check.ResultDNSOKDropCurlHTTPError, check.ResultNone
+			}
+
+			extTarget := ct.Params().ExternalTarget
+			if a.Destination().Port() == 80 && a.Destination().Address(features.GetIPFamily(extTarget)) == extTarget {
+				if a.Destination().Path() == "/" || a.Destination().Path() == "" {
+					egress = check.ResultDNSOK
+					egress.HTTP = check.HTTP{
+						Method: "GET",
+						// Trim the trailing dot, if any, to match the behavior of the curl
+						// action and make sure that flow validation can succeed.
+						URL: fmt.Sprintf("http://%s/", strings.TrimSuffix(extTarget, ".")),
+					}
+					return egress, check.ResultNone
+				}
+				// Else expect HTTP drop by proxy
+				return check.ResultDNSOKDropCurlHTTPError, check.ResultNone
+			}
+			// No HTTP proxy on other ports
+			return check.ResultDNSOKDropCurlTimeout, check.ResultNone
+		})
+}
+
+type toFqdnsWithProxy struct{}
+
+func (t toFqdnsWithProxy) build(ct *check.ConnectivityTest, templates map[string]string) {
+	// This policy only allows port 80 to domain-name, default one.one.one.one., DNS proxy enabled.
+	newTest("to-fqdns-with-proxy", ct).
+		WithCiliumPolicy(templates["clientEgressToFQDNsAndHTTPGetPolicyYAML"]).
+		WithCiliumPolicy(templates["clientEgressOnlyDNSPolicyYAML"]).
 		WithFeatureRequirements(features.RequireEnabled(features.L7Proxy)).
 		WithScenarios(
-			tests.PodToWorld(tests.WithRetryDestPort(80)),
-			tests.PodToWorld2(), // resolves to ExternalOtherTarget
+			// TODO: Reenable IPv6 for this test once the kernel with the bugfix is released:
+			// https://patchwork.kernel.org/project/netdevbpf/patch/20250318161516.3791383-1-maxim@isovalent.com/
+			tests.PodToWorld(false, tests.WithRetryDestPort(80)),
+			tests.PodToWorld2(false), // resolves to ExternalOtherTarget
 		).
 		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
 			if a.Destination().Address(features.IPFamilyAny) == ct.Params().ExternalOtherTarget {
