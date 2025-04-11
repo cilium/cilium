cilium/cilium

commit 7b80f94e5e0f47e68934e31f9987388791ce0f32

iptables/ipset: Use batch operations
Performing an ipset list operation at each reconciler update can become
expensive in terms of memory, because it requires the allocation of a
set of addresses each time it is called. In a very large cluster or in a
large multi-cluster setup, the cardinality of this list can be pretty
high. When the cluster is experiencing node churn, and the
ipset.RemoveFromIPSet is called many times, this can lead to a very high
GC pressure, because we rely on `ipset list` in the reconciler Delete
operation to be sure that the IP set already exists.

To avoid that, reconcile the ipset changes using "ipset restore" in
order to add multiple IPs in one go and avoid the overhead of executing
ipset many times.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/datapath/iptables/ipset/ops.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/datapath/tables"
 	"github.com/cilium/cilium/pkg/statedb"
+	"github.com/cilium/cilium/pkg/statedb/reconciler"
 )
 
 func newOps(logger logrus.FieldLogger, ipset *ipset, cfg config) *ops {
@@ -30,37 +31,61 @@ type ops struct {
 	ipset   *ipset
 }
 
-func (ops *ops) Update(ctx context.Context, _ statedb.ReadTxn, entry *tables.IPSetEntry, changed *bool) error {
+// UpdateBatch implements reconciler.BatchOperations.
+func (ops *ops) UpdateBatch(ctx context.Context, txn statedb.ReadTxn, batch []reconciler.BatchEntry[*tables.IPSetEntry]) {
 	if !ops.enabled {
-		return nil
+		return
 	}
 
-	// create the set if does not exist
-	if err := ops.ipset.create(ctx, entry.Name, string(entry.Family)); err != nil {
-		return err
+	addrsByName := map[string][]netip.Addr{}
+	for _, entry := range batch {
+		addrsByName[entry.Object.Name] = append(addrsByName[entry.Object.Name], entry.Object.Addr)
+	}
+	err := ops.ipset.addBatch(ctx, addrsByName)
+	if err != nil {
+		// Fail the whole batch.
+		for i := range batch {
+			batch[i].Result = err
+		}
+	}
+}
+
+// DeleteBatch implements reconciler.BatchOperations.
+func (ops *ops) DeleteBatch(ctx context.Context, txn statedb.ReadTxn, batch []reconciler.BatchEntry[*tables.IPSetEntry]) {
+	if !ops.enabled {
+		return
 	}
 
-	if err := ops.ipset.add(ctx, entry.Name, entry.Addr); err != nil {
-		return err
+	addrsByName := map[string][]netip.Addr{}
+	for _, entry := range batch {
+		addrsByName[entry.Object.Name] = append(addrsByName[entry.Object.Name], entry.Object.Addr)
 	}
-
-	if changed != nil {
-		*changed = true
+	err := ops.ipset.delBatch(ctx, addrsByName)
+	if err != nil {
+		// Fail the whole batch.
+		for i := range batch {
+			batch[i].Result = err
+		}
 	}
+}
 
+var _ reconciler.Operations[*tables.IPSetEntry] = &ops{}
+var _ reconciler.BatchOperations[*tables.IPSetEntry] = &ops{}
+
+func (ops *ops) Update(ctx context.Context, _ statedb.ReadTxn, entry *tables.IPSetEntry, changed *bool) error {
+	// Since we're using batch operations Update is only called for full reconciliation.
+	// As we're doing full synchronization in Prune() we don't need to do anything here.
+	// The reconciler will be changed from Update+Prune to Sync in the future after which
+	// this hack can be removed.
+	if changed == nil {
+		// Panic here in case the assumptions change.
+		panic("Unexpectedly Update() called from incremental reconciliation")
+	}
 	return nil
 }
 
 func (ops *ops) Delete(ctx context.Context, _ statedb.ReadTxn, entry *tables.IPSetEntry) error {
-	if !ops.enabled {
-		return nil
-	}
-
-	// check that the set exists
-	if _, err := ops.ipset.list(ctx, entry.Name); err != nil {
-		return nil
-	}
-	return ops.ipset.del(ctx, entry.Name, entry.Addr)
+	panic("Unexpectedly Delete() called from incremental reconciliation")
 }
 
 func (ops *ops) Prune(ctx context.Context, _ statedb.ReadTxn, iter statedb.Iterator[*tables.IPSetEntry]) error {
@@ -102,19 +127,16 @@ func reconcile(
 	}
 
 	toDel := curSet.Difference(desired)
-	for addr := range toDel {
-		if err := ipset.del(ctx, name, addr); err != nil {
-			return fmt.Errorf("unable to delete addr %s from ipset %s: %w", name, addr, err)
-		}
+	delBatch := map[string][]netip.Addr{name: toDel.UnsortedList()}
+	if err := ipset.delBatch(ctx, delBatch); err != nil {
+		return fmt.Errorf("unable to delete from ipset: %w", err)
 	}
 
 	toAdd := desired.Difference(curSet)
-	for addr := range toAdd {
-		if err := ipset.add(ctx, name, addr); err != nil {
-			return fmt.Errorf("unable to add addr %s from ipset %s: %w", name, addr, err)
-		}
+	addBatch := map[string][]netip.Addr{name: toAdd.UnsortedList()}
+	if err := ipset.addBatch(ctx, addBatch); err != nil {
+		return fmt.Errorf("unable to delete from ipset: %w", err)
 	}
-
 	return nil
 }
 

daemon/cmd/cells_test.go
@@ -23,6 +23,8 @@ var goleakOptions = []goleak.Option{
 	// Ignore goroutines started by the policy trifecta, see [newPolicyTrifecta].
 	goleak.IgnoreTopFunction("github.com/cilium/cilium/pkg/identity/cache.(*identityWatcher).watch.func1"),
 	goleak.IgnoreTopFunction("github.com/cilium/cilium/pkg/trigger.(*Trigger).waiter"),
+	// Ignore goroutine started by the ipset reconciler rate limiter
+	goleak.IgnoreTopFunction("github.com/cilium/cilium/pkg/rate.NewLimiter.func1"),
 }
 
 // TestAgentCell verifies that the Agent hive can be instantiated with

pkg/datapath/iptables/ipset/cell.go
@@ -6,12 +6,14 @@ package ipset
 import (
 	"context"
 	"os/exec"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 
 	"github.com/cilium/cilium/pkg/datapath/tables"
 	"github.com/cilium/cilium/pkg/hive/cell"
 	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/rate"
 	"github.com/cilium/cilium/pkg/statedb/reconciler"
 	"github.com/cilium/cilium/pkg/time"
 )
@@ -38,14 +40,13 @@ var Cell = cell.Module(
 		reconciler.New[*tables.IPSetEntry],
 		newReconcilerConfig,
 		newOps,
-		func(ops *ops) reconciler.Operations[*tables.IPSetEntry] {
-			return ops
-		},
 	),
 	cell.ProvidePrivate(func(logger logrus.FieldLogger) *ipset {
 		return &ipset{
-			executable: funcExecutable(func(ctx context.Context, name string, arg ...string) ([]byte, error) {
-				return exec.CommandContext(ctx, name, arg...).Output()
+			executable: funcExecutable(func(ctx context.Context, name string, stdin string, arg ...string) ([]byte, error) {
+				cmd := exec.CommandContext(ctx, name, arg...)
+				cmd.Stdin = strings.NewReader(stdin)
+				return cmd.Output()
 			}),
 			log: logger,
 		}
@@ -59,16 +60,22 @@ type config struct {
 	NodeIPSetNeeded bool
 }
 
-func newReconcilerConfig(
-	ops reconciler.Operations[*tables.IPSetEntry],
-) reconciler.Config[*tables.IPSetEntry] {
+func newReconcilerConfig(ops *ops) reconciler.Config[*tables.IPSetEntry] {
 	return reconciler.Config[*tables.IPSetEntry]{
 		FullReconcilationInterval: 30 * time.Minute,
 		RetryBackoffMinDuration:   100 * time.Millisecond,
 		RetryBackoffMaxDuration:   5 * time.Second,
-		IncrementalRoundSize:      100,
 		GetObjectStatus:           (*tables.IPSetEntry).GetStatus,
 		WithObjectStatus:          (*tables.IPSetEntry).WithStatus,
 		Operations:                ops,
+		BatchOperations:           ops,
+
+		// Set the maximum batch size to 100, and limit the incremental
+		// reconciliation to once every 10ms, giving us maximum throughput
+		// of 1000/10 * 100 = 10000 per second.
+		IncrementalRoundSize: 100,
+
+		// Set the rate limiter to accumulate a batch of entries to reconcile.
+		RateLimiter: rate.NewLimiter(10*time.Millisecond, 1),
 	}
 }

pkg/datapath/iptables/ipset/ipset.go
@@ -150,7 +150,6 @@ func newIPSetManager(
 	reconciler reconciler.Reconciler[*tables.IPSetEntry],
 	ops *ops,
 ) Manager {
-	db.RegisterTable(table)
 	mgr := &manager{
 		logger:     logger,
 		enabled:    cfg.NodeIPSetNeeded,
@@ -267,23 +266,31 @@ func (i *ipset) list(ctx context.Context, name string) (AddrSet, error) {
 	return addrs, nil
 }
 
-func (i *ipset) add(ctx context.Context, name string, addr netip.Addr) error {
-	if _, err := i.run(ctx, "add", name, addr.String(), "-exist"); err != nil {
-		return fmt.Errorf("failed to add %s to ipset %s: %w", addr, name, err)
+func (i *ipset) addBatch(ctx context.Context, batch map[string][]netip.Addr) error {
+	b := strings.Builder{}
+	for name, addrs := range batch {
+		for _, addr := range addrs {
+			fmt.Fprintf(&b, "add %s %s -exist\n", name, addr)
+		}
 	}
-	return nil
+	_, err := i.exec(ctx, "ipset", b.String(), "restore")
+	return err
 }
 
-func (i *ipset) del(ctx context.Context, name string, addr netip.Addr) error {
-	if _, err := i.run(ctx, "del", name, addr.String(), "-exist"); err != nil {
-		return fmt.Errorf("failed to del %s to ipset %s: %w", addr, name, err)
+func (i *ipset) delBatch(ctx context.Context, batch map[string][]netip.Addr) error {
+	b := strings.Builder{}
+	for name, addrs := range batch {
+		for _, addr := range addrs {
+			fmt.Fprintf(&b, "del %s %s -exist\n", name, addr)
+		}
 	}
-	return nil
+	_, err := i.exec(ctx, "ipset", b.String(), "restore")
+	return err
 }
 
 func (i *ipset) run(ctx context.Context, args ...string) ([]byte, error) {
 	i.log.Debugf("Running command %s", i.fullCommand(args...))
-	return i.exec(ctx, "ipset", args...)
+	return i.exec(ctx, "ipset", "", args...)
 }
 
 func (i *ipset) fullCommand(args ...string) string {
@@ -292,11 +299,11 @@ func (i *ipset) fullCommand(args ...string) string {
 
 // useful to ease the creation of a mock ipset command for testing purposes
 type executable interface {
-	exec(ctx context.Context, name string, arg ...string) ([]byte, error)
+	exec(ctx context.Context, name string, stdin string, arg ...string) ([]byte, error)
 }
 
-type funcExecutable func(ctx context.Context, name string, arg ...string) ([]byte, error)
+type funcExecutable func(ctx context.Context, name string, stdin string, arg ...string) ([]byte, error)
 
-func (f funcExecutable) exec(ctx context.Context, name string, arg ...string) ([]byte, error) {
-	return f(ctx, name, arg...)
+func (f funcExecutable) exec(ctx context.Context, name string, stdin string, arg ...string) ([]byte, error) {
+	return f(ctx, name, stdin, arg...)
 }

pkg/datapath/iptables/ipset/ipset_test.go
@@ -6,6 +6,7 @@ package ipset
 import (
 	"bytes"
 	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"html/template"
@@ -80,57 +81,67 @@ func TestManager(t *testing.T) {
 			cell.Provide(func(logger logrus.FieldLogger) *ipset {
 				return &ipset{
 					executable: funcExecutable(
-						func(ctx context.Context, command string, arg ...string) ([]byte, error) {
+						func(ctx context.Context, command string, stdin string, arg ...string) ([]byte, error) {
 							mu.Lock()
 							defer mu.Unlock()
 
-							t.Logf("%s %s", command, strings.Join(arg, " "))
-
-							subCommand := arg[0]
-							name := arg[1]
-
-							switch subCommand {
-							case "create":
-								if _, found := ipsets[name]; !found {
-									ipsets[name] = AddrSet{}
+							var commands [][]string
+							if arg[0] == "restore" {
+								lines := strings.Split(stdin, "\n")
+								for _, line := range lines {
+									if len(line) > 0 {
+										commands = append(commands, strings.Split(line, " "))
+									}
 								}
-								return nil, nil
-							case "destroy":
-								if _, found := ipsets[name]; !found {
-									return nil, fmt.Errorf("ipset %s not found", name)
-								}
-								delete(ipsets, name)
-								return nil, nil
-							case "list":
-								if _, found := ipsets[name]; !found {
-									return nil, fmt.Errorf("ipset %s not found", name)
-								}
-								var bb bytes.Buffer
-								if err := tmpl.Execute(&bb, map[string]AddrSet{name: ipsets[name]}); err != nil {
-									return nil, err
-								}
-								b := bb.Bytes()
-								return b, nil
-							case "add":
-								if _, found := ipsets[name]; !found {
-									return nil, fmt.Errorf("ipset %s not found", name)
-								}
-								addr := netip.MustParseAddr(arg[len(arg)-2])
-								ipsets[name] = ipsets[name].Insert(addr)
-								return nil, nil
-							case "del":
-								if _, found := ipsets[name]; !found {
-									return nil, fmt.Errorf("ipset %s not found", name)
-								}
-								addr := netip.MustParseAddr(arg[len(arg)-2])
-								if !ipsets[name].Has(addr) {
-									return nil, nil
-								}
-								ipsets[name] = ipsets[name].Delete(addr)
-								return nil, nil
-							default:
-								return nil, fmt.Errorf("unexpected ipset subcommand %s", arg[1])
+							} else {
+								commands = [][]string{arg}
 							}
+
+							for _, arg := range commands {
+								subCommand := arg[0]
+								name := arg[1]
+								t.Logf("%s %s", subCommand, strings.Join(arg[1:], " "))
+
+								switch subCommand {
+								case "create":
+									if _, found := ipsets[name]; !found {
+										ipsets[name] = AddrSet{}
+									}
+								case "destroy":
+									if _, found := ipsets[name]; !found {
+										return nil, fmt.Errorf("ipset %s not found", name)
+									}
+									delete(ipsets, name)
+								case "list":
+									if _, found := ipsets[name]; !found {
+										return nil, fmt.Errorf("ipset %s not found", name)
+									}
+									var bb bytes.Buffer
+									if err := tmpl.Execute(&bb, map[string]AddrSet{name: ipsets[name]}); err != nil {
+										return nil, err
+									}
+									b := bb.Bytes()
+									return b, nil
+								case "add":
+									if _, found := ipsets[name]; !found {
+										return nil, fmt.Errorf("ipset %s not found", name)
+									}
+									addr := netip.MustParseAddr(arg[len(arg)-2])
+									ipsets[name] = ipsets[name].Insert(addr)
+								case "del":
+									if _, found := ipsets[name]; !found {
+										return nil, fmt.Errorf("ipset %s not found", name)
+									}
+									addr := netip.MustParseAddr(arg[len(arg)-2])
+									if !ipsets[name].Has(addr) {
+										return nil, nil
+									}
+									ipsets[name] = ipsets[name].Delete(addr)
+								default:
+									return nil, fmt.Errorf("unexpected ipset subcommand %s", arg[1])
+								}
+							}
+							return nil, nil
 						},
 					),
 					log: logger,
@@ -307,7 +318,7 @@ func TestManagerNodeIpsetNotNeeded(t *testing.T) {
 			}),
 			cell.Provide(func(logger logrus.FieldLogger) *ipset {
 				return &ipset{
-					executable: funcExecutable(func(ctx context.Context, command string, arg ...string) ([]byte, error) {
+					executable: funcExecutable(func(ctx context.Context, command string, stdin string, arg ...string) ([]byte, error) {
 						mu.Lock()
 						defer mu.Unlock()
 
@@ -325,7 +336,6 @@ func TestManagerNodeIpsetNotNeeded(t *testing.T) {
 					log: logger,
 				}
 			}),
-
 			// force manager instantiation
 			cell.Invoke(func(_ Manager) {}),
 		),
@@ -415,7 +425,7 @@ func TestOpsPruneEnabled(t *testing.T) {
 	var nCalled atomic.Bool // true if the ipset utility has been called
 
 	ipset := &ipset{
-		executable: funcExecutable(func(ctx context.Context, command string, arg ...string) ([]byte, error) {
+		executable: funcExecutable(func(ctx context.Context, command string, stdin string, arg ...string) ([]byte, error) {
 			nCalled.Store(true)
 			t.Logf("%s %s", command, strings.Join(arg, " "))
 			return nil, nil
@@ -515,6 +525,118 @@ type mockExec struct {
 	err error
 }
 
-func (e *mockExec) exec(ctx context.Context, name string, arg ...string) ([]byte, error) {
+func (e *mockExec) exec(ctx context.Context, name string, stdin string, arg ...string) ([]byte, error) {
 	return e.out, e.err
+}
+
+func BenchmarkManager(b *testing.B) {
+
+	var (
+		mgr         Manager
+		initializer Initializer
+		addCount    atomic.Int32
+		deleteCount atomic.Int32
+	)
+
+	hive := hive.New(
+		statedb.Cell,
+		job.Cell,
+		reconciler.Cell,
+
+		cell.Module(
+			"ipset-manager-test",
+			"ipset-manager-test",
+
+			cell.Provide(func() config {
+				return config{NodeIPSetNeeded: true}
+			}),
+
+			cell.Provide(
+				newIPSetManager,
+				tables.NewIPSetTable,
+				reconciler.New[*tables.IPSetEntry],
+				newReconcilerConfig,
+				newOps,
+			),
+			cell.Provide(func(ops *ops) reconciler.Operations[*tables.IPSetEntry] {
+				return ops
+			}),
+
+			cell.Provide(func(logger logrus.FieldLogger) *ipset {
+				return &ipset{
+					executable: funcExecutable(
+						func(ctx context.Context, command string, stdin string, arg ...string) ([]byte, error) {
+							// exec of ipset add takes about ~0.51ms
+							time.Sleep(time.Millisecond)
+							if arg[0] == "add" {
+								addCount.Add(1)
+							} else if arg[0] == "del" {
+								deleteCount.Add(1)
+							}
+
+							if arg[0] == "restore" {
+								count := strings.Count(stdin, "\n")
+								if strings.HasPrefix(stdin, "add") {
+									addCount.Add(int32(count))
+								} else {
+									deleteCount.Add(int32(count))
+								}
+							}
+							return nil, nil
+						}),
+					log: logger,
+				}
+			}),
+		),
+
+		cell.Invoke(func(m Manager) {
+			// Add an initializer to stop the pruning
+			initializer = m.NewInitializer()
+			mgr = m
+		}),
+	)
+
+	assert.NoError(b, hive.Start(context.Background()))
+
+	b.ResetTimer()
+
+	numEntries := 1000
+
+	toNetIP := func(i int) netip.Addr {
+		var addr1 [4]byte
+		binary.BigEndian.PutUint32(addr1[:], 0x02000000+uint32(i))
+		return netip.AddrFrom4(addr1)
+	}
+
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < numEntries; i++ {
+			ip := toNetIP(i)
+			mgr.AddToIPSet(CiliumNodeIPSetV4, INetFamily, ip)
+		}
+
+		// Wait for all ops to be done
+		for addCount.Load() != int32(numEntries) {
+			time.Sleep(time.Millisecond)
+
+		}
+		for i := 0; i < numEntries; i++ {
+			ip := toNetIP(i)
+			mgr.RemoveFromIPSet(CiliumNodeIPSetV4, ip)
+		}
+
+		for deleteCount.Load() != int32(numEntries) {
+			time.Sleep(time.Millisecond)
+		}
+
+		addCount.Store(0)
+		deleteCount.Store(0)
+	}
+
+	b.StopTimer()
+
+	b.ReportMetric(float64(2 /*add&delete*/ *b.N*numEntries)/b.Elapsed().Seconds(), "ops/sec")
+
+	initializer.InitDone()
+
+	assert.NoError(b, hive.Stop(context.Background()))
 }

pkg/datapath/tables/ipset.go
@@ -36,7 +36,10 @@ func NewIPSetTable(db *statedb.DB) (statedb.RWTable[*IPSetEntry], error) {
 		IPSetsTableName,
 		IPSetEntryIndex,
 	)
-	return tbl, err
+	if err != nil {
+		return nil, err
+	}
+	return tbl, db.RegisterTable(tbl)
 }
 
 func (s *IPSetEntry) TableHeader() []string {

commit 754625e2db3d61eb18e81754959412e4f849adc6

iptables/ipset: Define non-batched reconciler operations
When a cilium/statedb reconciler relies on batched operations and a
failure happens, the subsequent retry calls the non-batched operations
(e.g: `Update` instead of `UpdateBatch`). Therefore, to avoid a panic in
case of a retry, we need to define the non-batched operations too for
the ipset reconciler.

Fixes: 7b80f94e5e ("iptables/ipset: Use batch operations")

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/datapath/iptables/ipset/ops.go
@@ -74,11 +74,21 @@ var _ reconciler.Operations[*tables.IPSetEntry] = &ops{}
 var _ reconciler.BatchOperations[*tables.IPSetEntry] = &ops{}
 
 func (ops *ops) Update(ctx context.Context, _ statedb.ReadTxn, entry *tables.IPSetEntry) error {
-	panic("Unexpectedly Update() called for reconciliation")
+	if !ops.enabled {
+		return nil
+	}
+
+	addrsByName := map[string][]netip.Addr{entry.Name: {entry.Addr}}
+	return ops.ipset.addBatch(ctx, addrsByName)
 }
 
 func (ops *ops) Delete(ctx context.Context, _ statedb.ReadTxn, entry *tables.IPSetEntry) error {
-	panic("Unexpectedly Delete() called for reconciliation")
+	if !ops.enabled {
+		return nil
+	}
+
+	addrsByName := map[string][]netip.Addr{entry.Name: {entry.Addr}}
+	return ops.ipset.delBatch(ctx, addrsByName)
 }
 
 func (ops *ops) Prune(ctx context.Context, _ statedb.ReadTxn, objs iter.Seq2[*tables.IPSetEntry, statedb.Revision]) error {