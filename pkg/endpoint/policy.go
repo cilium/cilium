6e1aaf1d55d85f62f0af1d02d3d4d16ddb1e65fa
Emily Shepherd <emily@redcoat.dev>
2025-04-14 22:32:26 +0100

    endpoint: Unlock on IPIdentitySync error
    Previously, if this job failed due to the host IP not being available,
    we would return an error but never unlock the endpoint. This would leave
    the system deadlocked and would be unrecoverable, even if a valid host
    IP was later added.
    
    Fixes #37791
    
    Signed-off-by: Emily Shepherd <emily@redcoat.dev>

--- pkg/endpoint/policy.go
+++ pkg/endpoint/policy.go
@@ -982,6 +982,7 @@ func (e *Endpoint) runIPIdentitySync(endpointIP netip.Addr) {
 				ID := e.SecurityIdentity.ID
 				hostIP, ok := netipx.FromStdIP(node.GetIPv4())
 				if !ok {
+					e.runlock()
 					return controller.NewExitReason("Failed to convert node IPv4 address")
 				}
 				key := node.GetEndpointEncryptKeyIndex()
