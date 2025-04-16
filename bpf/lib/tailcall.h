2d634922d77aca7d6daa083c2bc2f3dabb70b02f
Julian Wiedmann <jwi@isovalent.com>
2025-04-16 10:59:58 +0300

    bpf: don't guard internal tailcall map with SKIP_CALLS_MAP
    This macro is only used by bpf_sock.c, and we're intentionally not
    including tailcall.h there.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/tailcall.h
+++ bpf/lib/tailcall.h
@@ -97,7 +97,6 @@
 #define CILIUM_CALL_MULTICAST_EP_DELIVERY	46
 #define CILIUM_CALL_SIZE			47
 
-#ifndef SKIP_CALLS_MAP
 /* Private per-EP map for internal tail calls. Its bpffs pin is replaced every
  * time the BPF object is loaded. An existing pinned map is never reused.
  */
@@ -109,9 +108,7 @@ struct bpf_elf_map __section_maps cilium_calls = {
 	.pinning	= CILIUM_PIN_REPLACE,
 	.max_elem	= CILIUM_CALL_SIZE,
 };
-#endif /* SKIP_CALLS_MAP */
 
-#ifndef SKIP_CALLS_MAP
 static __always_inline __must_check int
 tail_call_internal(struct __ctx_buff *ctx, const __u32 index, __s8 *ext_err)
 {
@@ -121,7 +118,6 @@ tail_call_internal(struct __ctx_buff *ctx, const __u32 index, __s8 *ext_err)
 		*ext_err = (__s8)index;
 	return DROP_MISSED_TAIL_CALL;
 }
-#endif /* SKIP_CALLS_MAP */
 
 /* invoke_tailcall_if() is a helper which based on COND either selects to emit
  * a tail call for the underlying function when true or emits it as inlined
