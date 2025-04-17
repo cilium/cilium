1b6b9313303d1537305bad0161bff917eeac1487
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-04-17 17:46:06 +0200

    endpoint: move endpoint deletion queue to endpoint api cell
    The Endpoint `DeletionQueue` is part of the Endpoint API as it
    is responsible to handle the Endpoint deletion requests from the CNI
    that happened during the time the Endpoint API wasn't available.
    
    Therefore, this commit moves the `DeletionQueue` from the daemon cells
    to the Hive module `endpoint-api`, where it is provided as private cell.
    
    Note: Instead of depending on the `DaemonPromise`, the deletion queue now
    depends on the `EndpointRestorationPromise`. Under the hood this is the same,
    because the later is resolved once the first is resolved. It's simply to workaround
    cyclic dependencies - and it's more expressive.
    
    Note2: The deletion queue no longer has access to the lifecycle context of
    the daemon (`d.ctx`). Therefore, this commit temporarily uses a `context.TODO()`.
    This will be fixed in a follow up commit that refactors the deletion queue
    to use Hive Jobs instead of raw Hive lifecycle hooks.
    
    Note3: Looks like PR #32981 introduced a regression in the deletion queue
    lock/unlocking behaviour (premature unlocking). This will be fixed in a following commit.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- daemon/cmd/daemon_test.go
+++ daemon/cmd/daemon_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/api/v1/server"
 	cnicell "github.com/cilium/cilium/daemon/cmd/cni"
 	fakecni "github.com/cilium/cilium/daemon/cmd/cni/fake"
 	"github.com/cilium/cilium/pkg/controller"
@@ -120,6 +121,7 @@ func setupDaemonSuite(tb testing.TB) *DaemonSuite {
 			func() *loadbalancer.TestConfig {
 				return &loadbalancer.TestConfig{}
 			},
+			func() *server.Server { return nil },
 		),
 		fakeDatapath.Cell,
 		prefilter.Cell,
