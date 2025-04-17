ee07553508f49427645bdcd3cd6af7241b3d19ee
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-04-17 17:20:57 +0200

    endpoint: provide EndpointAPIManager via hive cell
    Currently, some methods on the `Daemon` are used by the Endpoint API
    and DeletionQueue.
    
    In preparation for extracting the Endpoint API, this commit extracts
    these methods from the `Daemon` and introduces a new Hive component
    `EndpointAPIManager` that provides this functionality. It is provided
    by the Hive module `endpoint-api`.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -27,7 +27,6 @@ import (
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 
-	"github.com/cilium/cilium/daemon/cmd/cni"
 	agentK8s "github.com/cilium/cilium/daemon/k8s"
 	"github.com/cilium/cilium/pkg/aws/eni"
 	"github.com/cilium/cilium/pkg/bpf"
@@ -1478,7 +1477,7 @@ type daemonParams struct {
 	EndpointCreator     endpointcreator.EndpointCreator
 	EndpointManager     endpointmanager.EndpointManager
 	EndpointMetadata    endpointmetadata.EndpointMetadataFetcher
-	EndpointCreations   endpointapi.EndpointCreationManager
+	EndpointAPIManager  endpointapi.EndpointAPIManager
 	CertManager         certificatemanager.CertificateManager
 	SecretManager       certificatemanager.SecretManager
 	IdentityAllocator   identitycell.CachingIdentityAllocator
@@ -1486,7 +1485,6 @@ type daemonParams struct {
 	Policy              policy.PolicyRepository
 	IPCache             *ipcache.IPCache
 	DirReadStatus       policyDirectory.DirectoryWatcherReadStatus
-	CNIConfigManager    cni.CNIConfigManager
 	CiliumHealth        health.CiliumHealthManager
 	ClusterMesh         *clustermesh.ClusterMesh
 	MonitorAgent        monitorAgent.Agent
