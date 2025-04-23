d5d1911d623635f2c47d11fe505a27ff6ba942a2
Liyi Huang <pdshly@gmail.com>
2025-04-23 21:55:42 +0000

    operator/pkg/gateway-api: Update ServiceImport and TLSRoute support checks
    [ upstream commit 9ac6c4c3e40daac9cfac5462009e88358b08829b ]
    
    -Currently, if we dont have TLSRoute CRD installed, the operator will still try to list TLSRoute CRD
    that's because we validate the TLSRoute through IsGroupRegistered.
    
    - This commit refactors the HasServiceImportSupport and HasTLSRouteSupport functions to check for
    the registration of the ServiceImport and TLSRoute CRDs instead of IsGroupRegistered.
    so the optinal CRD will not just pass when the Group is registered.
    
    - Update the UT to test the TLSRoute properly
    
    Signed-off-by: Liyi Huang <liyi.huang@isovalent.com>
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/gateway-api/helpers/tlsroute.go
+++ operator/pkg/gateway-api/helpers/tlsroute.go
@@ -9,9 +9,9 @@ import (
 )
 
 // HasTLSRouteSupport returns if the TLSRoute CRD is supported.
-// This checks if the Gateway API v1alpha2 group is registered in the client scheme
+// This checks if the Gateway API v1alpha2 TLSRoute CRD is registered in the client scheme
 // and it is expected that it is registered only if the TLSRoute
 // CRD has been installed prior to the client setup.
 func HasTLSRouteSupport(scheme *runtime.Scheme) bool {
-	return scheme.IsGroupRegistered(gatewayv1alpha2.GroupVersion.Group)
+	return scheme.Recognizes(gatewayv1alpha2.SchemeGroupVersion.WithKind("TLSRoute"))
 }
