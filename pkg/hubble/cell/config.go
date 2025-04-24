cilium/cilium

commit 5df6f520eb8fcdc79c8586e3ba6f96b2a17c8a37

feat(hubble): decouple the payloadparser from hubble control plane.
This pull request introduces important changes to integrate a payload parser for Hubble. Currently, the parser (or Decoder) is tightly coupled with the launch code for Hubble, making it impossible to run Hubble without the Cilium dataplane. While we expose Hubble as a cell, we do not explicitly expose the parser as a dependency.

In this update, I have extracted the parser initiation and exposed it as a cell. This change allows Hubble to operate as a standalone component, fully decoupled from the Cilium dataplane.

Signed-off-by: Ritwik Ranjan <ritwikranjan@microsoft.com>

pkg/hubble/cell/config.go
@@ -26,9 +26,6 @@ type config struct {
 	// EventQueueSize specifies the buffer size of the channel to receive
 	// monitor events.
 	EventQueueSize int `mapstructure:"hubble-event-queue-size"`
-	// SkipUnknownCGroupIDs specifies if events with unknown cgroup ids should
-	// be skipped.
-	SkipUnknownCGroupIDs bool `mapstructure:"hubble-skip-unknown-cgroup-ids"`
 	// MonitorEvents specifies Cilium monitor events for Hubble to observe. By
 	// default, Hubble observes all monitor events.
 	MonitorEvents []string `mapstructure:"hubble-monitor-events"`
@@ -89,22 +86,6 @@ type config struct {
 	// RecorderSinkQueueSize is the queue size for each recorder sink.
 	RecorderSinkQueueSize int `mapstructure:"hubble-recorder-sink-queue-size"`
 
-	// EnableRedact controls if sensitive information will be redacted from L7
-	// flows.
-	EnableRedact bool `mapstructure:"hubble-redact-enabled"`
-	// RedactHttpURLQuery controls if the URL query will be redacted from flows.
-	RedactHttpURLQuery bool `mapstructure:"hubble-redact-http-urlquery"`
-	// RedactHttpUserInfo controls if the user info will be redacted from flows.
-	RedactHttpUserInfo bool `mapstructure:"hubble-redact-http-userinfo"`
-	// RedactHttpHeadersAllow controls which http headers will not be redacted
-	// from flows.
-	RedactHttpHeadersAllow []string `mapstructure:"hubble-redact-http-headers-allow"`
-	// RedactHttpHeadersDeny controls which http headers will be redacted from
-	// flows.
-	RedactHttpHeadersDeny []string `mapstructure:"hubble-redact-http-headers-deny"`
-	// RedactKafkaAPIKey controls if Kafka API key will be redacted from flows.
-	RedactKafkaAPIKey bool `mapstructure:"hubble-redact-kafka-apikey"`
-
 	// EnableK8sDropEvents controls whether Hubble should create v1.Events for
 	// packet drops related to pods.
 	EnableK8sDropEvents bool `mapstructure:"hubble-drop-events"`
@@ -120,10 +101,9 @@ type config struct {
 var defaultConfig = config{
 	EnableHubble: false,
 	// Hubble internals (parser, ringbuffer) configuration
-	EventBufferCapacity:  observeroption.Default.MaxFlows.AsInt(),
-	EventQueueSize:       0, // see getDefaultMonitorQueueSize()
-	SkipUnknownCGroupIDs: true,
-	MonitorEvents:        []string{},
+	EventBufferCapacity: observeroption.Default.MaxFlows.AsInt(),
+	EventQueueSize:      0, // see getDefaultMonitorQueueSize()
+	MonitorEvents:       []string{},
 	// Hubble local server configuration
 	SocketPath: hubbleDefaults.SocketPath,
 	// Hubble TCP server configuration
@@ -148,13 +128,6 @@ var defaultConfig = config{
 	EnableRecorderAPI:     true,
 	RecorderStoragePath:   hubbleDefaults.RecorderStoragePath,
 	RecorderSinkQueueSize: 1024,
-	// Hubble field redaction configuration
-	EnableRedact:           false,
-	RedactHttpURLQuery:     false,
-	RedactHttpUserInfo:     true,
-	RedactHttpHeadersAllow: []string{},
-	RedactHttpHeadersDeny:  []string{},
-	RedactKafkaAPIKey:      false,
 	// Hubble k8s v1.Events integration configuration.
 	EnableK8sDropEvents:            false,
 	K8sDropEventsInterval:          2 * time.Minute,
@@ -167,7 +140,6 @@ func (def config) Flags(flags *pflag.FlagSet) {
 	// Hubble internals (parser, ringbuffer) configuration
 	flags.Int("hubble-event-buffer-capacity", def.EventBufferCapacity, "Capacity of Hubble events buffer. The provided value must be one less than an integer power of two and no larger than 65535 (ie: 1, 3, ..., 2047, 4095, ..., 65535)")
 	flags.Int("hubble-event-queue-size", def.EventQueueSize, "Buffer size of the channel to receive monitor events.")
-	flags.Bool("hubble-skip-unknown-cgroup-ids", def.SkipUnknownCGroupIDs, "Skip Hubble events with unknown cgroup ids")
 	flags.StringSlice("hubble-monitor-events", def.MonitorEvents,
 		fmt.Sprintf(
 			"Cilium monitor events for Hubble to observe: [%s]. By default, Hubble observes all monitor events.",
@@ -197,13 +169,6 @@ func (def config) Flags(flags *pflag.FlagSet) {
 	flags.Bool("enable-hubble-recorder-api", def.EnableRecorderAPI, "Enable the Hubble recorder API")
 	flags.String("hubble-recorder-storage-path", def.RecorderStoragePath, "Directory in which pcap files created via the Hubble Recorder API are stored")
 	flags.Int("hubble-recorder-sink-queue-size", def.RecorderSinkQueueSize, "Queue size of each Hubble recorder sink")
-	// Hubble field redaction configuration
-	flags.Bool("hubble-redact-enabled", def.EnableRedact, "Hubble redact sensitive information from flows")
-	flags.Bool("hubble-redact-http-urlquery", def.RedactHttpURLQuery, "Hubble redact http URL query from flows")
-	flags.Bool("hubble-redact-http-userinfo", def.RedactHttpUserInfo, "Hubble redact http user info from flows")
-	flags.StringSlice("hubble-redact-http-headers-allow", def.RedactHttpHeadersAllow, "HTTP headers to keep visible in flows")
-	flags.StringSlice("hubble-redact-http-headers-deny", def.RedactHttpHeadersDeny, "HTTP headers to redact from flows")
-	flags.Bool("hubble-redact-kafka-apikey", def.RedactKafkaAPIKey, "Hubble redact Kafka API key from flows")
 	// Hubble k8s v1.Events integration configuration.
 	flags.Bool("hubble-drop-events", def.EnableK8sDropEvents, "Emit packet drop Events related to pods (alpha)")
 	flags.Duration("hubble-drop-events-interval", def.K8sDropEventsInterval, "Minimum time between emitting same events")
@@ -233,13 +198,6 @@ func (cfg *config) normalize() {
 	if len(cfg.K8sDropEventsReasons) == 1 {
 		cfg.K8sDropEventsReasons = strings.Fields(cfg.K8sDropEventsReasons[0])
 	}
-}
-
-func (cfg config) validate() error {
-	if len(cfg.RedactHttpHeadersAllow) > 0 && len(cfg.RedactHttpHeadersDeny) > 0 {
-		return fmt.Errorf("Only one of --hubble-redact-http-headers-allow and --hubble-redact-http-headers-deny can be specified, not both")
-	}
-	return nil
 }
 
 func getDefaultMonitorQueueSize(numCPU int) int {

pkg/hubble/parser/cell/cell.go
@@ -1,0 +1,168 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Hubble
+
+package cell
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/netip"
+	"strings"
+
+	"github.com/cilium/hive/cell"
+
+	flowpb "github.com/cilium/cilium/api/v1/flow"
+	"github.com/cilium/cilium/pkg/cgroups/manager"
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
+	"github.com/cilium/cilium/pkg/datapath/link"
+	"github.com/cilium/cilium/pkg/endpointmanager"
+	"github.com/cilium/cilium/pkg/hubble/parser"
+	hubbleGetters "github.com/cilium/cilium/pkg/hubble/parser/getters"
+	parserOptions "github.com/cilium/cilium/pkg/hubble/parser/options"
+	"github.com/cilium/cilium/pkg/identity"
+	identitycell "github.com/cilium/cilium/pkg/identity/cache/cell"
+	"github.com/cilium/cilium/pkg/ipcache"
+	"github.com/cilium/cilium/pkg/loadbalancer"
+	"github.com/cilium/cilium/pkg/service"
+)
+
+var Cell = cell.Module(
+	"payload-parser",
+	"Provides a payload parser for Hubble",
+
+	link.Cell,
+
+	cell.Provide(newPayloadParser),
+	cell.Config(defaultConfig),
+)
+
+func newPayloadParser(params payloadParserParams) (parser.Decoder, error) {
+	if err := params.Config.validate(); err != nil {
+		return nil, fmt.Errorf("failed to validate configuration: %w", err)
+	}
+	g := &payloadGetters{
+		log:               params.Log,
+		identityAllocator: params.IdentityAllocator,
+		endpointManager:   params.EndpointManager,
+		ipcache:           params.Ipcache,
+		serviceManager:    params.ServiceManager,
+	}
+	var parserOpts []parserOptions.Option
+	if params.Config.EnableRedact {
+		parserOpts = append(
+			parserOpts,
+			parserOptions.WithRedact(
+				params.Log,
+				params.Config.RedactHttpURLQuery,
+				params.Config.RedactHttpUserInfo,
+				params.Config.RedactKafkaAPIKey,
+				params.Config.RedactHttpHeadersAllow,
+				params.Config.RedactHttpHeadersDeny,
+			),
+		)
+	}
+	return parser.New(params.Log, g, g, g, params.Ipcache, g, params.LinkCache, params.CGroupManager, params.Config.SkipUnknownCGroupIDs, parserOpts...)
+}
+
+type payloadParserParams struct {
+	cell.In
+
+	Log *slog.Logger
+
+	IdentityAllocator identitycell.CachingIdentityAllocator
+	EndpointManager   endpointmanager.EndpointManager
+	Ipcache           *ipcache.IPCache
+	ServiceManager    service.ServiceManager
+	CGroupManager     manager.CGroupManager
+	LinkCache         *link.LinkCache
+
+	Config config
+}
+
+type payloadGetters struct {
+	log *slog.Logger
+
+	identityAllocator identitycell.CachingIdentityAllocator
+	endpointManager   endpointmanager.EndpointManager
+	ipcache           *ipcache.IPCache
+	serviceManager    service.ServiceManager
+}
+
+// GetIdentity implements IdentityGetter. It looks up identity by ID from
+// Cilium's identity cache. Hubble uses the identity info to populate flow
+// source and destination labels.
+func (p *payloadGetters) GetIdentity(securityIdentity uint32) (*identity.Identity, error) {
+	ident := p.identityAllocator.LookupIdentityByID(context.Background(), identity.NumericIdentity(securityIdentity))
+	if ident == nil {
+		return nil, fmt.Errorf("identity %d not found", securityIdentity)
+	}
+	return ident, nil
+}
+
+// GetEndpointInfo implements EndpointGetter. It returns endpoint info for a
+// given IP address. Hubble uses this function to populate fields like
+// namespace and pod name for local endpoints.
+func (h *payloadGetters) GetEndpointInfo(ip netip.Addr) (endpoint hubbleGetters.EndpointInfo, ok bool) {
+	if !ip.IsValid() {
+		return nil, false
+	}
+	ep := h.endpointManager.LookupIP(ip)
+	if ep == nil {
+		return nil, false
+	}
+	return ep, true
+}
+
+// GetEndpointInfoByID implements EndpointGetter. It returns endpoint info for
+// a given Cilium endpoint id. Used by Hubble.
+func (h *payloadGetters) GetEndpointInfoByID(id uint16) (endpoint hubbleGetters.EndpointInfo, ok bool) {
+	ep := h.endpointManager.LookupCiliumID(id)
+	if ep == nil {
+		return nil, false
+	}
+	return ep, true
+}
+
+// GetNamesOf implements DNSGetter.GetNamesOf. It looks up DNS names of a given
+// IP from the FQDN cache of an endpoint specified by sourceEpID.
+func (h *payloadGetters) GetNamesOf(sourceEpID uint32, ip netip.Addr) []string {
+	ep := h.endpointManager.LookupCiliumID(uint16(sourceEpID))
+	if ep == nil {
+		return nil
+	}
+
+	if !ip.IsValid() {
+		return nil
+	}
+	names := ep.DNSHistory.LookupIP(ip)
+
+	for i := range names {
+		names[i] = strings.TrimSuffix(names[i], ".")
+	}
+
+	return names
+}
+
+// GetServiceByAddr implements ServiceGetter. It looks up service by IP/port.
+// Hubble uses this function to annotate flows with service information.
+func (h *payloadGetters) GetServiceByAddr(ip netip.Addr, port uint16) *flowpb.Service {
+	if !ip.IsValid() {
+		return nil
+	}
+	addrCluster := cmtypes.AddrClusterFrom(ip, 0)
+	addr := loadbalancer.L3n4Addr{
+		AddrCluster: addrCluster,
+		L4Addr: loadbalancer.L4Addr{
+			Port: port,
+		},
+	}
+	namespace, name, ok := h.serviceManager.GetServiceNameByAddr(addr)
+	if !ok {
+		return nil
+	}
+	return &flowpb.Service{
+		Namespace: namespace,
+		Name:      name,
+	}
+}

pkg/hubble/parser/cell/config.go
@@ -1,0 +1,60 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Hubble
+
+package cell
+
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+)
+
+type config struct {
+	// SkipUnknownCGroupIDs specifies if events with unknown cgroup ids should
+	// be skipped.
+	SkipUnknownCGroupIDs bool `mapstructure:"hubble-skip-unknown-cgroup-ids"`
+
+	// EnableRedact controls if sensitive information will be redacted from L7
+	// flows.
+	EnableRedact bool `mapstructure:"hubble-redact-enabled"`
+	// RedactHttpURLQuery controls if the URL query will be redacted from flows.
+	RedactHttpURLQuery bool `mapstructure:"hubble-redact-http-urlquery"`
+	// RedactHttpUserInfo controls if the user info will be redacted from flows.
+	RedactHttpUserInfo bool `mapstructure:"hubble-redact-http-userinfo"`
+	// RedactHttpHeadersAllow controls which http headers will not be redacted
+	// from flows.
+	RedactHttpHeadersAllow []string `mapstructure:"hubble-redact-http-headers-allow"`
+	// RedactHttpHeadersDeny controls which http headers will be redacted from
+	// flows.
+	RedactHttpHeadersDeny []string `mapstructure:"hubble-redact-http-headers-deny"`
+	// RedactKafkaAPIKey controls if Kafka API key will be redacted from flows.
+	RedactKafkaAPIKey bool `mapstructure:"hubble-redact-kafka-apikey"`
+}
+
+var defaultConfig = config{
+	SkipUnknownCGroupIDs:   true,
+	EnableRedact:           false,
+	RedactHttpURLQuery:     false,
+	RedactHttpUserInfo:     true,
+	RedactHttpHeadersAllow: []string{},
+	RedactHttpHeadersDeny:  []string{},
+	RedactKafkaAPIKey:      false,
+}
+
+func (cfg config) validate() error {
+	if len(cfg.RedactHttpHeadersAllow) > 0 && len(cfg.RedactHttpHeadersDeny) > 0 {
+		return fmt.Errorf("Only one of --hubble-redact-http-headers-allow and --hubble-redact-http-headers-deny can be specified, not both")
+	}
+	return nil
+}
+
+func (def config) Flags(flags *pflag.FlagSet) {
+	flags.Bool("hubble-skip-unknown-cgroup-ids", def.SkipUnknownCGroupIDs, "Skip Hubble events with unknown cgroup ids")
+	// Hubble field redaction configuration
+	flags.Bool("hubble-redact-enabled", def.EnableRedact, "Hubble redact sensitive information from flows")
+	flags.Bool("hubble-redact-http-urlquery", def.RedactHttpURLQuery, "Hubble redact http URL query from flows")
+	flags.Bool("hubble-redact-http-userinfo", def.RedactHttpUserInfo, "Hubble redact http user info from flows")
+	flags.StringSlice("hubble-redact-http-headers-allow", def.RedactHttpHeadersAllow, "HTTP headers to keep visible in flows")
+	flags.StringSlice("hubble-redact-http-headers-deny", def.RedactHttpHeadersDeny, "HTTP headers to redact from flows")
+	flags.Bool("hubble-redact-kafka-apikey", def.RedactKafkaAPIKey, "Hubble redact Kafka API key from flows")
+}

pkg/hubble/cell/cell.go
@@ -16,6 +16,8 @@ import (
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	exportercell "github.com/cilium/cilium/pkg/hubble/exporter/cell"
 	"github.com/cilium/cilium/pkg/hubble/observer/observeroption"
+	"github.com/cilium/cilium/pkg/hubble/parser"
+	parsercell "github.com/cilium/cilium/pkg/hubble/parser/cell"
 	identitycell "github.com/cilium/cilium/pkg/identity/cache/cell"
 	"github.com/cilium/cilium/pkg/ipcache"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
@@ -35,11 +37,21 @@ var Cell = cell.Module(
 	"hubble",
 	"Exposes the Observer gRPC API and Hubble metrics",
 
-	cell.Provide(newHubbleIntegration),
-	cell.Config(defaultConfig),
+	Core,
 
 	// Hubble flow log exporters
 	exportercell.Cell,
+
+	// Parser for Hubble flows
+	parsercell.Cell,
+)
+
+// The core cell group, which contains the Hubble integration and the
+// Hubble integration configuration isolated from the dependency graph
+// will enable us to run hubble with a different dataplane
+var Core = cell.Group(
+	cell.Provide(newHubbleIntegration),
+	cell.Config(defaultConfig),
 )
 
 type hubbleParams struct {
@@ -64,6 +76,8 @@ type hubbleParams struct {
 	// NOTE: ordering is not guaranteed, do not rely on it.
 	ObserverOptions  []observeroption.Option                `group:"hubble-observer-options"`
 	ExporterBuilders []*exportercell.FlowLogExporterBuilder `group:"hubble-exporter-builders"`
+
+	PayloadParser parser.Decoder
 
 	// NOTE: we still need DaemonConfig for the shared EnableRecorder flag.
 	AgentConfig *option.DaemonConfig
@@ -90,6 +104,7 @@ func newHubbleIntegration(params hubbleParams) (HubbleIntegration, error) {
 		params.Recorder,
 		params.ObserverOptions,
 		params.ExporterBuilders,
+		params.PayloadParser,
 		params.AgentConfig,
 		params.Config,
 		params.Logger,

pkg/datapath/link/link.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"sync"
 
+	"github.com/cilium/hive/cell"
 	"github.com/vishvananda/netlink"
 
 	"github.com/cilium/cilium/pkg/controller"
@@ -78,25 +79,74 @@ func GetIfIndex(ifName string) (uint32, error) {
 type LinkCache struct {
 	mu          lock.RWMutex
 	indexToName map[int]string
+	manager     *controller.Manager
 }
 
-// NewLinkCache begins monitoring local interfaces for changes in order to
-// track local link information.
-func NewLinkCache() *LinkCache {
-	once.Do(func() {
-		linkCache = LinkCache{}
-		controller.NewManager().UpdateController("link-cache",
-			controller.ControllerParams{
-				Group:       linkCacheControllerGroup,
-				RunInterval: 15 * time.Second,
-				DoFunc: func(ctx context.Context) error {
-					return linkCache.syncCache()
+var Cell = cell.Module(
+	"link-cache",
+	"Provides a cache of link names to ifindex mappings",
+
+	cell.Provide(newLinkCache),
+	cell.Invoke(registerLinkCacheHooks),
+)
+
+type linkCacheParams struct {
+	cell.In
+	Lifecycle cell.Lifecycle
+}
+
+func registerLinkCacheHooks(params linkCacheParams, cache *LinkCache) {
+	params.Lifecycle.Append(cell.Hook{
+		OnStart: func(ctx cell.HookContext) error {
+			// Start the controller when the application starts
+			cache.manager.UpdateController("link-cache",
+				controller.ControllerParams{
+					Group:       linkCacheControllerGroup,
+					RunInterval: 15 * time.Second,
+					DoFunc: func(ctx context.Context) error {
+						return cache.syncCache()
+					},
 				},
-			},
-		)
+			)
+			return nil
+		},
+		OnStop: func(ctx cell.HookContext) error {
+			cache.Stop()
+			return nil
+		},
+	})
+}
+
+func newLinkCache() *LinkCache {
+	once.Do(func() {
+		linkCache = LinkCache{
+			indexToName: make(map[int]string),
+			manager:     controller.NewManager(),
+		}
 	})
 
 	return &linkCache
+}
+
+func NewLinkCache() *LinkCache {
+	lc := newLinkCache()
+
+	lc.manager.UpdateController("link-cache",
+		controller.ControllerParams{
+			Group:       linkCacheControllerGroup,
+			RunInterval: 15 * time.Second,
+			DoFunc: func(ctx context.Context) error {
+				return linkCache.syncCache()
+			},
+		},
+	)
+
+	return lc
+}
+
+// Stop terminates the link cache controller
+func (c *LinkCache) Stop() {
+	c.manager.RemoveController("link-cache")
 }
 
 func (c *LinkCache) syncCache() error {

commit 87d6f2092ba36f65b5dd49891177b7cdfd93e8bc

hubble: allow to enable/disable network policy correlation option again
The referenced commit caused the Hubble network policy correlation to be
always enabled, i.e. setting --hubble-network-policy-correlation-enabled
no longer had any effect.

Fix this by correctly wiring up the config again and translating it into
a parser option in newPayloadParser.

Fixes: 5df6f520eb8f ("feat(hubble): decouple the payloadparser from hubble control plane.")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/hubble/cell/config.go
@@ -94,8 +94,6 @@ type config struct {
 	K8sDropEventsInterval time.Duration `mapstructure:"hubble-drop-events-interval"`
 	// K8sDropEventsReasons controls which drop reasons to emit events for.
 	K8sDropEventsReasons []string `mapstructure:"hubble-drop-events-reasons"`
-	// EnableNetworkPolicyCorrelation controls whether to enable network policy correlation of Hubble flows
-	EnableNetworkPolicyCorrelation bool `mapstructure:"hubble-network-policy-correlation-enabled"`
 }
 
 var defaultConfig = config{
@@ -129,10 +127,9 @@ var defaultConfig = config{
 	RecorderStoragePath:   hubbleDefaults.RecorderStoragePath,
 	RecorderSinkQueueSize: 1024,
 	// Hubble k8s v1.Events integration configuration.
-	EnableK8sDropEvents:            false,
-	K8sDropEventsInterval:          2 * time.Minute,
-	K8sDropEventsReasons:           []string{"auth_required", "policy_denied"},
-	EnableNetworkPolicyCorrelation: true,
+	EnableK8sDropEvents:   false,
+	K8sDropEventsInterval: 2 * time.Minute,
+	K8sDropEventsReasons:  []string{"auth_required", "policy_denied"},
 }
 
 func (def config) Flags(flags *pflag.FlagSet) {
@@ -173,7 +170,6 @@ func (def config) Flags(flags *pflag.FlagSet) {
 	flags.Bool("hubble-drop-events", def.EnableK8sDropEvents, "Emit packet drop Events related to pods (alpha)")
 	flags.Duration("hubble-drop-events-interval", def.K8sDropEventsInterval, "Minimum time between emitting same events")
 	flags.StringSlice("hubble-drop-events-reasons", def.K8sDropEventsReasons, "Drop reasons to emit events for")
-	flags.Bool("hubble-network-policy-correlation-enabled", def.EnableNetworkPolicyCorrelation, "Enable network policy correlation of Hubble flows")
 }
 
 func (cfg *config) normalize() {

pkg/hubble/parser/cell/cell.go
@@ -60,6 +60,12 @@ func newPayloadParser(params payloadParserParams) (parser.Decoder, error) {
 			),
 		)
 	}
+	parserOpts = append(
+		parserOpts,
+		parserOptions.WithNetworkPolicyCorrelation(
+			params.Log,
+			params.Config.EnableNetworkPolicyCorrelation,
+		))
 	return parser.New(params.Log, g, g, g, params.Ipcache, g, params.LinkCache, params.CGroupManager, params.Config.SkipUnknownCGroupIDs, parserOpts...)
 }
 

pkg/hubble/parser/cell/config.go
@@ -14,6 +14,9 @@ type config struct {
 	// be skipped.
 	SkipUnknownCGroupIDs bool `mapstructure:"hubble-skip-unknown-cgroup-ids"`
 
+	// EnableNetworkPolicyCorrelation controls whether to enable network policy correlation of Hubble flows
+	EnableNetworkPolicyCorrelation bool `mapstructure:"hubble-network-policy-correlation-enabled"`
+
 	// EnableRedact controls if sensitive information will be redacted from L7
 	// flows.
 	EnableRedact bool `mapstructure:"hubble-redact-enabled"`
@@ -32,13 +35,14 @@ type config struct {
 }
 
 var defaultConfig = config{
-	SkipUnknownCGroupIDs:   true,
-	EnableRedact:           false,
-	RedactHttpURLQuery:     false,
-	RedactHttpUserInfo:     true,
-	RedactHttpHeadersAllow: []string{},
-	RedactHttpHeadersDeny:  []string{},
-	RedactKafkaAPIKey:      false,
+	SkipUnknownCGroupIDs:           true,
+	EnableNetworkPolicyCorrelation: true,
+	EnableRedact:                   false,
+	RedactHttpURLQuery:             false,
+	RedactHttpUserInfo:             true,
+	RedactHttpHeadersAllow:         []string{},
+	RedactHttpHeadersDeny:          []string{},
+	RedactKafkaAPIKey:              false,
 }
 
 func (cfg config) validate() error {
@@ -57,4 +61,5 @@ func (def config) Flags(flags *pflag.FlagSet) {
 	flags.StringSlice("hubble-redact-http-headers-allow", def.RedactHttpHeadersAllow, "HTTP headers to keep visible in flows")
 	flags.StringSlice("hubble-redact-http-headers-deny", def.RedactHttpHeadersDeny, "HTTP headers to redact from flows")
 	flags.Bool("hubble-redact-kafka-apikey", def.RedactKafkaAPIKey, "Hubble redact Kafka API key from flows")
+	flags.Bool("hubble-network-policy-correlation-enabled", def.EnableNetworkPolicyCorrelation, "Enable network policy correlation of Hubble flows")
 }

pkg/hubble/parser/options/options.go
@@ -62,7 +62,7 @@ func WithRedact(logger *slog.Logger, httpQuery, httpUserInfo, kafkaApiKey bool, 
 	}
 }
 
-// EnableL3L4PolicyCorrelation configures the Network Policy correlation of Hubble Flows.
+// WithNetworkPolicyCorrelation configures the Network Policy correlation of Hubble Flows.
 func WithNetworkPolicyCorrelation(logger *slog.Logger, enabled bool) Option {
 	return func(opt *Options) {
 		opt.EnableNetworkPolicyCorrelation = enabled

pkg/hubble/parser/options/options_test.go
@@ -50,8 +50,8 @@ func TestEnableNetworkPolicyCorrelation(t *testing.T) {
 		),
 	)
 	opt := WithNetworkPolicyCorrelation(logger, true)
-	opt(&Options{
-		EnableNetworkPolicyCorrelation: true,
-	})
+	opts := Options{EnableNetworkPolicyCorrelation: false}
+	opt(&opts)
+	assert.True(t, opts.EnableNetworkPolicyCorrelation)
 	assert.Equal(t, want, buf.String())
 }