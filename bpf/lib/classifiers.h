ba6f15c8eccf93ff86f39c5c3a3d3c84f18f32f7
Simone Magnani <simone.magnani@isovalent.com>
2025-04-24 12:13:16 +0200

    bpf: refactor trace/drop notification flags into common classifiers
    This commit is a prep work for the upcoming commits. We refactor the trace
    flags currently in use (ipv6, l3_dev) into an apposite file and data type
    `cls_flags_t` now defined as a `__u8`. We noticed that, while handling
    trace and drop notification from the userspace in monitor/hubble, the
    datapath need to report common flags for both trace/drop notification.
    Therefore, we here refactor that into a dedicated file with shared methods:
    
    1. ctx_device_classifiers(ctx):  base function to report whether the packet
                                  is from an L3 device (CLS_FLAG_L3_DEV), and
                                  in case set CLS_FLAG_IPV6 if ctx->protocol
                                  is ETH_P_IPV6.
    2. ctx_device_classifiers4(ctx): same as (1), added for completeness and
                                  future extensions.
    3. ctx_device_classifiers6(ctx): same as (1) but always sets CLS_FLAG_IPV6.
    
    In `struct trace_notify` and `struct drop_notify`, we leave the flags as
    a `__u8` fields. This is useful for when in future the cls_flags_t data type
    grows from __u8 into, for instance, __u16 because we need to handle more
    and more flags. In that case, the compiler would report an error, and we
    will know that we need to hack the implementation in the structures (ex.
    split flags into multiple fields, as flags_lower/flags_upper). Trace and
    Drop structures can grow independently, therefore they have to handle
    this changes on their own, and cannot rely on a cls_flags_t field.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

---
+++ bpf/lib/classifiers.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright Authors of Cilium */
+
+#pragma once
+
+#include "lib/common.h"
+
+/* Layer 3 packets are observed from the WireGuard device cilium_wg0 */
+#if defined(IS_BPF_WIREGUARD)
+# define CLASSIFIERS_DEVICE
+#endif
+
+typedef __u8 cls_flags_t;
+
+enum {
+	CLS_FLAG_IPV6	= (1 << 0),
+	CLS_FLAG_L3_DEV = (1 << 1),
+};
+
+#define EMPTY_CLASSIFIERS ((cls_flags_t)0)
+
+#ifdef CLASSIFIERS_DEVICE
+/* Compute packet layer classifiers
+ * - CLS_FLAG_L3_DEV: packet from a L3 device
+ * - CLS_FLAG_IPV6:   IPv6 packet when CLS_FLAG_L3_DEV is set. When already knowing a
+ *                    packet is IPv6, use ctx_device_classifiers6() instead.
+ */
+static __always_inline cls_flags_t
+ctx_device_classifiers(const struct __ctx_buff *ctx __maybe_unused)
+{
+	if (ctx->protocol == bpf_htons(ETH_P_IPV6))
+		return CLS_FLAG_L3_DEV | CLS_FLAG_IPV6;
+
+	return CLS_FLAG_L3_DEV;
+}
+
+#define ctx_device_classifiers4(ctx) (ctx_device_classifiers(ctx))
+#define ctx_device_classifiers6(ctx) (ctx_device_classifiers(ctx) | CLS_FLAG_IPV6)
+#else
+#define ctx_device_classifiers(ctx)  ((cls_flags_t)0)
+#define ctx_device_classifiers4(ctx) ((cls_flags_t)0)
+#define ctx_device_classifiers6(ctx) ((cls_flags_t)CLS_FLAG_IPV6)
+#endif /* CLASSIFIERS_DEVICE */
