6d1be0b09f72c70fe50039ea587e0cddec11ee65
Julian Wiedmann <jwi@isovalent.com>
2025-04-24 08:23:14 +0300

    bpf: encrypt: move node-map related helpers to node.h
    While most usage of the node_map is related to IPSec, it still makes sense
    to consolidate all references to cilium_node_map_v2 in a single header
    file.
    
    Add simple lookup wrappers and uses them for the existing helpers. Also
    add the needed include in bpf_overlay.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/node.h
+++ bpf/lib/node.h
@@ -15,3 +15,53 @@ struct {
 	__uint(max_entries, NODE_MAP_SIZE);
 	__uint(map_flags, BPF_F_NO_PREALLOC);
 } cilium_node_map_v2 __section_maps_btf;
+
+static __always_inline struct node_value *
+lookup_ip4_node(__be32 ip4)
+{
+	struct node_key key = {};
+
+	key.family = ENDPOINT_KEY_IPV4;
+	key.ip4 = ip4;
+
+	return map_lookup_elem(&cilium_node_map_v2, &key);
+}
+
+static __always_inline __u16
+lookup_ip4_node_id(__be32 ip4)
+{
+	struct node_value *node_value;
+
+	node_value = lookup_ip4_node(ip4);
+	if (!node_value)
+		return 0;
+	if (!node_value->id)
+		return 0;
+	return node_value->id;
+}
+
+# ifdef ENABLE_IPV6
+static __always_inline struct node_value *
+lookup_ip6_node(const union v6addr *ip6)
+{
+	struct node_key key = {};
+
+	key.family = ENDPOINT_KEY_IPV6;
+	key.ip6 = *ip6;
+
+	return map_lookup_elem(&cilium_node_map_v2, &key);
+}
+
+static __always_inline __u16
+lookup_ip6_node_id(const union v6addr *ip6)
+{
+	struct node_value *node_value;
+
+	node_value = lookup_ip6_node(ip6);
+	if (!node_value)
+		return 0;
+	if (!node_value->id)
+		return 0;
+	return node_value->id;
+}
+# endif /* ENABLE_IPV6 */
