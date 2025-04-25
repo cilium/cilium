ab712204a499598751bfd656aad3d3b104fa7691
André Martins <andre@cilium.io>
2025-04-25 10:33:59 +0200

    pkg/loadbalancer/benchmark/cmd: migrate to slog
    Migrate this package to use slog instead of logrus.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/loadbalancer/benchmark/cmd/main.go
+++ pkg/loadbalancer/benchmark/cmd/main.go
@@ -5,12 +5,12 @@ package main
 
 import (
 	"flag"
-	"log"
 	"log/slog"
 	"os"
 	"runtime/pprof"
 
 	"github.com/cilium/cilium/pkg/loadbalancer/benchmark"
+	"github.com/cilium/cilium/pkg/logging"
 )
 
 // Test size is the number of services. For each service, there is a single endpointslice with a single endpoint with a single port.
@@ -26,11 +26,11 @@ func main() {
 	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
 		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
+			logging.Fatal(slog.Default(), "could not create CPU profile: ", err)
 		}
 		defer f.Close()
 		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
+			logging.Fatal(slog.Default(), "could not start CPU profile: ", err)
 		}
 		defer pprof.StopCPUProfile()
 	}
