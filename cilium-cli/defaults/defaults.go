e8c698d056fdd259750cc1255277213c7819dff9
darox <maderdario@gmail.com>
2025-04-28 12:43:22 +0200

    CLI: bump ConnectivityTestConnDisruptImage
    This commit bumps the
    ConnectivityTestConnDisruptImage to the
    latest version.
    
    Signed-off-by: darox <maderdario@gmail.com>

--- cilium-cli/defaults/defaults.go
+++ cilium-cli/defaults/defaults.go
@@ -173,7 +173,7 @@ var (
 		// renovate: datasource=docker
 		"ConnectivityDNSTestServerImage": "docker.io/coredns/coredns:1.12.0@sha256:40384aa1f5ea6bfdc77997d243aec73da05f27aed0c5e9d65bfa98933c519d97",
 		// renovate: datasource=docker
-		"ConnectivityTestConnDisruptImage": "quay.io/cilium/test-connection-disruption:v0.0.14@sha256:c3fd56e326ae16f6cb63dbb2e26b4e47ec07a123040623e11399a7fe1196baa0",
+		"ConnectivityTestConnDisruptImage": "quay.io/cilium/test-connection-disruption:v0.0.16@sha256:e8e3257b2c89543dc49a2d820f2d2d69c1fe60eaf1036fc1f1f7375bad8e6232",
 		// renovate: datasource=docker
 		"ConnectivityTestFRRImage": "quay.io/frrouting/frr:10.2.1@sha256:c8543d3e0a1348cc0f2b19154fd8b0300e237773dbec65d9d6d6570c1d088deb",
 		// renovate: datasource=docker
