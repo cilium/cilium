87a895e87b1c76c993e364f3532181e0850bb535
Carlos Abad <carlosab@google.com>
2025-04-29 23:31:12 +0000

    pkg/egressgateway: add multigateway to CEGP API
    Signed-off-by: Carlos Abad <carlosab@google.com>

--- pkg/k8s/apis/cilium.io/v2/cegp_types.go
+++ pkg/k8s/apis/cilium.io/v2/cegp_types.go
@@ -63,7 +63,18 @@ type CiliumEgressGatewayPolicySpec struct {
 	ExcludedCIDRs []CIDR `json:"excludedCIDRs"`
 
 	// EgressGateway is the gateway node responsible for SNATing traffic.
+	// In case multiple nodes are a match for the given set of labels, the first node
+	// in lexical ordering based on their name will be selected.
 	EgressGateway *EgressGateway `json:"egressGateway"`
+
+	// Optional list of gateway nodes responsible for SNATing traffic.
+	// If this field has any entries the contents of the egressGateway field will be ignored.
+	// In case multiple nodes are a match for the given set of labels in each entry,
+	// the first node in lexical ordering based on their name will be selected for each entry.
+	//
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default={}
+	EgressGateways []EgressGateway `json:"egressGateways,omitempty"`
 }
 
 // EgressGateway identifies the node that should act as egress gateway for a
