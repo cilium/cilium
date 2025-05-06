48a753b9dffb5a2cbb912f0b686ee042d9ee8fbf
pasteley <ceasebeing@gmail.com>
2025-05-06 10:51:07 +0000

    cilium, bpf: Perform 2nd lookup also on local backends for lb-proto-diff
    [ upstream commit 8cd635f6620a86ff07c04b0c5aedadea48e77c8c ]
    
    [ backporter's note: Adjusted map names for LB6_SERVICES_MAP_V2 and
      LB4_SERVICES_MAP_V2 ]
    
    When performing load balancer service lookups for internal scope
    (LB_LOOKUP_SCOPE_INT), ensure protocol differentiation is considered
    just like it is for external scope lookups.
    
    Fixes: https://github.com/cilium/cilium/issues/39358
    Signed-off-by: pasteley <ceasebeing@gmail.com>

--- bpf/lib/lb.h
+++ bpf/lib/lb.h
@@ -631,6 +631,7 @@ struct lb6_service *lb6_lookup_service(struct lb6_key *key,
 				       const bool scope_switch)
 {
 	struct lb6_service *svc;
+	__u8 orig_proto = key->proto;
 
 	key->scope = LB_LOOKUP_SCOPE_EXT;
 	key->backend_slot = 0;
@@ -648,7 +649,16 @@ struct lb6_service *lb6_lookup_service(struct lb6_key *key,
 		if (!scope_switch || !lb6_svc_is_two_scopes(svc))
 			return svc;
 		key->scope = LB_LOOKUP_SCOPE_INT;
+		key->proto = orig_proto;
 		svc = map_lookup_elem(&LB6_SERVICES_MAP_V2, key);
+
+#if defined(ENABLE_SERVICE_PROTOCOL_DIFFERENTIATION)
+		/* Also check for ANY protocol for internal scope lookups */
+		if (!svc && key->proto != 0) {
+			key->proto = 0;
+			svc = map_lookup_elem(&LB6_SERVICES_MAP_V2, key);
+		}
+#endif
 	}
 
 	return svc;
@@ -1351,6 +1361,7 @@ struct lb4_service *lb4_lookup_service(struct lb4_key *key,
 				       const bool scope_switch)
 {
 	struct lb4_service *svc;
+	__u8 orig_proto = key->proto;
 
 	key->scope = LB_LOOKUP_SCOPE_EXT;
 	key->backend_slot = 0;
@@ -1368,7 +1379,16 @@ struct lb4_service *lb4_lookup_service(struct lb4_key *key,
 		if (!scope_switch || !lb4_svc_is_two_scopes(svc))
 			return svc;
 		key->scope = LB_LOOKUP_SCOPE_INT;
+		key->proto = orig_proto;
 		svc = map_lookup_elem(&LB4_SERVICES_MAP_V2, key);
+
+#if defined(ENABLE_SERVICE_PROTOCOL_DIFFERENTIATION)
+		/* Also check for ANY protocol for internal scope lookups */
+		if (!svc && key->proto != 0) {
+			key->proto = 0;
+			svc = map_lookup_elem(&LB4_SERVICES_MAP_V2, key);
+		}
+#endif
 	}
 
 	return svc;
