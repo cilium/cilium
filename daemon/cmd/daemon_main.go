f069e49111f0488a3db6a2a4c9ecc0c9ecbe4379
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-05-07 15:55:28 +0200

    endpoint: extract endpoint restoration logic from daemon
    Currently, the logic and state of the Cilium Agent endpoint restoration
    logic is part of the `Daemon`.
    
    To further reduce the scope of the `Daemon` - this commit extracts this
    logic into it's own Go struct and Hive cell `endpointRestorer`.
    
    The lifecycle of the restoration logic is still managed by the
    Daemon lifecycle logic.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -51,7 +51,6 @@ import (
 	"github.com/cilium/cilium/pkg/dial"
 	"github.com/cilium/cilium/pkg/endpoint"
 	endpointcreator "github.com/cilium/cilium/pkg/endpoint/creator"
-	endpointmetadata "github.com/cilium/cilium/pkg/endpoint/metadata"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/endpointstate"
 	"github.com/cilium/cilium/pkg/envoy"
@@ -74,7 +73,6 @@ import (
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/labelsfilter"
 	"github.com/cilium/cilium/pkg/loadbalancer"
-	"github.com/cilium/cilium/pkg/loadbalancer/legacy/redirectpolicy"
 	"github.com/cilium/cilium/pkg/loadbalancer/legacy/service"
 	"github.com/cilium/cilium/pkg/loadinfo"
 	"github.com/cilium/cilium/pkg/logging"
@@ -1439,9 +1437,12 @@ var daemonCell = cell.Module(
 
 	cell.Provide(
 		newDaemonPromise,
-		promise.New[endpointstate.Restorer],
 		promise.New[*option.DaemonConfig],
 		newSyncHostIPs,
+	),
+	cell.Provide(
+		promise.New[endpointstate.Restorer],
+		newEndpointRestorer,
 	),
 	cell.Invoke(registerEndpointStateResolver),
 	cell.Invoke(func(promise.Promise[*Daemon]) {}), // Force instantiation.
@@ -1471,7 +1472,7 @@ type daemonParams struct {
 	NodeAddressing      datapath.NodeAddressing
 	EndpointCreator     endpointcreator.EndpointCreator
 	EndpointManager     endpointmanager.EndpointManager
-	EndpointMetadata    endpointmetadata.EndpointMetadataFetcher
+	EndpointRestorer    *endpointRestorer
 	CertManager         certificatemanager.CertificateManager
 	SecretManager       certificatemanager.SecretManager
 	IdentityAllocator   identitycell.CachingIdentityAllocator
@@ -1507,7 +1508,6 @@ type daemonParams struct {
 	IPAM                *ipam.IPAM
 	CRDSyncPromise      promise.Promise[k8sSynced.CRDSync]
 	IdentityManager     identitymanager.IDManager
-	LRPManager          *redirectpolicy.Manager
 	MaglevConfig        maglev.Config
 	LBConfig            loadbalancer.Config
 	DNSProxy            bootstrap.FQDNProxyBootstrapper
@@ -1620,7 +1620,7 @@ func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *da
 		log.WithError(err).Error("Failed to wait for initial IPCache revision")
 	}
 
-	d.initRestore(restoredEndpoints, params.EndpointRegenerator)
+	params.EndpointRestorer.initRestore(d.ctx, restoredEndpoints, params.EndpointRegenerator)
 
 	bootstrapStats.enableConntrack.Start()
 	log.Info("Starting connection tracking garbage collector")
@@ -1690,13 +1690,10 @@ func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *da
 	}
 
 	go func() {
-		if d.endpointRestoreComplete != nil {
-			select {
-			case <-d.endpointRestoreComplete:
-			case <-d.ctx.Done():
-				return
-			}
+		if err := params.EndpointRestorer.WaitForEndpointRestore(d.ctx); err != nil {
+			return
 		}
+
 		params.DNSProxy.CompleteBootstrap()
 
 		ms := maps.NewMapSweeper(params.Logger, &EndpointMapManager{
@@ -1791,7 +1788,7 @@ func startDaemon(d *Daemon, restoredEndpoints *endpointRestoreState, cleaner *da
 	return nil
 }
 
-func registerEndpointStateResolver(lc cell.Lifecycle, daemonPromise promise.Promise[*Daemon], resolver promise.Resolver[endpointstate.Restorer]) {
+func registerEndpointStateResolver(lc cell.Lifecycle, daemonPromise promise.Promise[*Daemon], endpointRestorer *endpointRestorer, resolver promise.Resolver[endpointstate.Restorer]) {
 	var wg sync.WaitGroup
 
 	lc.Append(cell.Hook{
@@ -1799,11 +1796,10 @@ func registerEndpointStateResolver(lc cell.Lifecycle, daemonPromise promise.Prom
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				daemon, err := daemonPromise.Await(context.Background())
-				if err != nil {
+				if _, err := daemonPromise.Await(context.Background()); err != nil {
 					resolver.Reject(err)
 				} else {
-					resolver.Resolve(daemon)
+					resolver.Resolve(endpointRestorer)
 				}
 			}()
 			return nil
