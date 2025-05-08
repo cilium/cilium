282a6ad3316404e60ab4060fd91355e3b730ff25
Dylan Reimerink <dylan.reimerink@isovalent.com>
2025-05-08 15:45:56 +0200

    pkg/bpf: Do not load unused maps
    At present we use `ifdef`s to manually determine when a map definition
    should be emitted into the object file. The object file gets parsed
    as collection spec, then when loading every map is created and its
    file descriptor linked into the bytecode before loading.
    
    Given the quest for clang-freedom, we wish to be able to pre-compile
    our programs and only load maps for features that are enabled at
    load time via global variables.
    
    This commit introduces this capability. It works by static analysis of
    the BPF program. By figuring out the basic blocks and finding global
    data accesses we can find branching instructions which are always
    or never taken. By excluding the "dead" parts of the program we can
    see which maps will and will not be in use after the verifier did
    dead code elimination as part of the loading process.
    
    We then remove the unused maps from the spec before loading.
    
    This technique works for both `ifdef`s as well as load time variables.
    Even for the current situation it is an improvement since we only
    need the `ifdef`s around code using the map. All maps can now be
    unconditionally defined, and developers no longer have to think about
    when maps are or are not loaded.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- pkg/datapath/loader/cell.go
+++ pkg/datapath/loader/cell.go
@@ -5,6 +5,7 @@ package loader
 
 import (
 	"github.com/cilium/hive/cell"
+	"github.com/spf13/pflag"
 
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
 )
@@ -15,7 +16,23 @@ var Cell = cell.Module(
 
 	cell.Provide(NewLoader),
 	cell.Provide(NewCompilationLock),
+	cell.Config(&Config{
+		ErrorOnUnusedMaps: false,
+	}),
 )
+
+type Config struct {
+	// We have logic to remove unused maps from collection specs before they are loaded.
+	// This is an optimization to avoid using memory for maps which are not used.
+	// Normally this causes a warning, but during CI we want to throw an error
+	// to make visible that something is going wrong.
+	ErrorOnUnusedMaps bool
+}
+
+func (c *Config) Flags(flags *pflag.FlagSet) {
+	flags.BoolVar(&c.ErrorOnUnusedMaps, "error-on-unused-maps", false, "Throw an error if after loading a collection contains maps that are not used")
+	_ = flags.MarkHidden("error-on-unused-maps")
+}
 
 // NewLoader returns a new loader.
 func NewLoader(p Params) datapath.Loader {
