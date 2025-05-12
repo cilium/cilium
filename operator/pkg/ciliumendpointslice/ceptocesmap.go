efeef6d2ee96e89beac21e4ecb335c5371acb33e
jshr-w <shjayaraman@microsoft.com>
2025-05-12 21:13:15 +0000

    CES: refactor ceptocesmap mappings
    Refactor maps for CES->CEPs and CES->ns into a single map. This change
    is intended to reduce complexity in the local state mappings, and is
    made in preparation for adding more mappings and logic in future commits
    that will create CES directly from pods.
    
    Signed-off-by: jshr-w <shjayaraman@microsoft.com>

--- operator/pkg/ciliumendpointslice/ceptocesmap.go
+++ operator/pkg/ciliumendpointslice/ceptocesmap.go
@@ -15,6 +15,14 @@ type CEPName resource.Key
 type CESKey resource.Key
 type CESName string
 
+// CESData contains all CES data, including endpoints.
+// CES is reconciled to have endpoints equal to CEPs mapped to it
+// and other fields set from the CESData.
+type CESData struct {
+	ceps sets.Set[CEPName]
+	ns   string
+}
+
 // CESToCEPMapping is used to map Cilium Endpoints to CiliumEndpointSlices and
 // retrieving all the Cilium Endpoints mapped to the given CiliumEndpointSlice.
 // This map is protected by lock for consistent and concurrent access.
@@ -22,24 +30,16 @@ type CESToCEPMapping struct {
 	mutex lock.RWMutex
 	// cepNameToCESName is used to map CiliumEndpoint name to CiliumEndpointSlice name.
 	cepNameToCESName map[CEPName]CESName
-	// cesNameToCEPNameSet is used to map CiliumEndpointSlice name to all CiliumEndpoints names it contains.
-	cesNameToCEPNameSet map[CESName]sets.Set[CEPName]
-	cesData             map[CESName]CESData
-}
-
-// CESData contains all CES data except endpoints.
-// CES is reconicled to have endpoints equal to CEPs mapped to it
-// and other fields set from the CESData.
-type CESData struct {
-	ns string
+	// cesNameToData is used to map CiliumEndpointSlice name to all CiliumEndpoints names it contains
+	// and the namespace associated with it
+	cesNameToData map[CESName]*CESData
 }
 
 // Creates and intializes the new CESToCEPMapping
 func newCESToCEPMapping() *CESToCEPMapping {
 	return &CESToCEPMapping{
-		cepNameToCESName:    make(map[CEPName]CESName),
-		cesNameToCEPNameSet: make(map[CESName]sets.Set[CEPName]),
-		cesData:             make(map[CESName]CESData),
+		cepNameToCESName: make(map[CEPName]CESName),
+		cesNameToData:    make(map[CESName]*CESData),
 	}
 }
 
@@ -48,14 +48,14 @@ func (c *CESToCEPMapping) insertCEP(cepName CEPName, cesName CESName) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 	c.cepNameToCESName[cepName] = cesName
-	c.cesNameToCEPNameSet[cesName].Insert(cepName)
+	c.cesNameToData[cesName].ceps.Insert(cepName)
 }
 
 // Remove the CEP entry from map
 func (c *CESToCEPMapping) deleteCEP(cepName CEPName) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
-	c.cesNameToCEPNameSet[c.cepNameToCESName[cepName]].Delete(cepName)
+	c.cesNameToData[c.cepNameToCESName[cepName]].ceps.Delete(cepName)
 	delete(c.cepNameToCESName, cepName)
 }
 
@@ -85,23 +85,27 @@ func (c *CESToCEPMapping) countCEPs() int {
 func (c *CESToCEPMapping) countCEPsInCES(ces CESName) int {
 	c.mutex.RLock()
 	defer c.mutex.RUnlock()
-	return c.cesNameToCEPNameSet[ces].Len()
+
+	if cesData, ok := c.cesNameToData[ces]; ok {
+		return cesData.ceps.Len()
+	}
+	return 0
 }
 
 // Return CEP Names mapped to the given CES
 func (c *CESToCEPMapping) getCEPsInCES(ces CESName) []CEPName {
 	c.mutex.RLock()
 	defer c.mutex.RUnlock()
-	return c.cesNameToCEPNameSet[ces].UnsortedList()
+	return c.cesNameToData[ces].ceps.UnsortedList()
 }
 
 // Initializes mapping structure for CES
 func (c *CESToCEPMapping) insertCES(cesName CESName, ns string) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
-	c.cesNameToCEPNameSet[cesName] = sets.New[CEPName]()
-	c.cesData[cesName] = CESData{
-		ns: ns,
+	c.cesNameToData[cesName] = &CESData{
+		ceps: sets.New[CEPName](),
+		ns:   ns,
 	}
 }
 
@@ -109,14 +113,13 @@ func (c *CESToCEPMapping) insertCES(cesName CESName, ns string) {
 func (c *CESToCEPMapping) deleteCES(cesName CESName) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
-	delete(c.cesNameToCEPNameSet, cesName)
-	delete(c.cesData, cesName)
+	delete(c.cesNameToData, cesName)
 }
 
 func (c *CESToCEPMapping) hasCESName(cesName CESName) bool {
 	c.mutex.RLock()
 	defer c.mutex.RUnlock()
-	_, ok := c.cesNameToCEPNameSet[cesName]
+	_, ok := c.cesNameToData[cesName]
 	return ok
 }
 
@@ -124,26 +127,28 @@ func (c *CESToCEPMapping) hasCESName(cesName CESName) bool {
 func (c *CESToCEPMapping) getCESCount() int {
 	c.mutex.RLock()
 	defer c.mutex.RUnlock()
-	return len(c.cesNameToCEPNameSet)
+	return len(c.cesNameToData)
 }
 
 // Return names of all CESs.
 func (c *CESToCEPMapping) getAllCESs() []CESName {
 	c.mutex.RLock()
 	defer c.mutex.RUnlock()
-	cess := make([]CESName, 0, len(c.cesNameToCEPNameSet))
-	for ces := range c.cesNameToCEPNameSet {
+	cess := make([]CESName, 0, len(c.cesNameToData))
+	for ces := range c.cesNameToData {
 		cess = append(cess, ces)
 	}
 	return cess
 }
 
-// Return the CES data
-func (c *CESToCEPMapping) getCESData(name CESName) CESData {
+// Return the namespace of the given CES
+func (c *CESToCEPMapping) getCESNamespace(name CESName) string {
 	c.mutex.RLock()
 	defer c.mutex.RUnlock()
-	data := c.cesData[name]
-	return data
+	if cesData, ok := c.cesNameToData[name]; ok {
+		return cesData.ns
+	}
+	return ""
 }
 
 func (ces CESKey) key() resource.Key {
