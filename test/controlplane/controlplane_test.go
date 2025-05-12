f45a5e87285dc8be36a12d6449811cf9a1da5e8c
Jussi Maki <jussi@isovalent.com>
2025-05-12 12:06:59 +0200

    test/controlplane: Remove load-balancing test cases
    The pkg/loadbalancer/tests does the same tests, but in a much more maintainable
    way.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- test/controlplane/controlplane_test.go
+++ test/controlplane/controlplane_test.go
@@ -8,10 +8,6 @@ import (
 
 	_ "github.com/cilium/cilium/test/controlplane/node"
 	_ "github.com/cilium/cilium/test/controlplane/node/ciliumnodes"
-	_ "github.com/cilium/cilium/test/controlplane/pod/hostport"
-	_ "github.com/cilium/cilium/test/controlplane/services/dualstack"
-	_ "github.com/cilium/cilium/test/controlplane/services/graceful-termination"
-	_ "github.com/cilium/cilium/test/controlplane/services/nodeport"
 	"github.com/cilium/cilium/test/controlplane/suite"
 )
 
