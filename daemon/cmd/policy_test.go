3fb8618f00d084b4b3fd8170331a2593e28f3da5
Jarno Rajahalme <jarno@isovalent.com>
2025-05-13 14:01:33 +0200

    envoy: Remove single-selector wildcard assumption
    We can no longer treat single-selector policies as wildcarding L3, as we
    no longer have bpf datapath always performing policy enforcement before
    cilium-envoy.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- daemon/cmd/policy_test.go
+++ daemon/cmd/policy_test.go
@@ -379,7 +379,8 @@ func (ds *DaemonSuite) testUpdateConsumerMap(t *testing.T) {
 				Protocol: envoy_config_core.SocketAddress_TCP,
 				Rules: []*cilium.PortNetworkPolicyRule{
 					{
-						L7: &PNPAllowGETbar,
+						RemotePolicies: expectedRemotePolicies,
+						L7:             &PNPAllowGETbar,
 					},
 				},
 			},
@@ -421,7 +422,8 @@ func (ds *DaemonSuite) testUpdateConsumerMap(t *testing.T) {
 				Protocol: envoy_config_core.SocketAddress_TCP,
 				Rules: []*cilium.PortNetworkPolicyRule{
 					{
-						L7: &PNPAllowGETbar,
+						RemotePolicies: expectedRemotePolicies,
+						L7:             &PNPAllowGETbar,
 					},
 				},
 			},
@@ -697,7 +699,8 @@ func (ds *DaemonSuite) testL3DependentL7(t *testing.T) {
 				Protocol: envoy_config_core.SocketAddress_TCP,
 				Rules: []*cilium.PortNetworkPolicyRule{
 					{
-						L7: &PNPAllowGETbar,
+						RemotePolicies: []uint32{uint32(qaFooSecLblsCtx.ID)},
+						L7:             &PNPAllowGETbar,
 					},
 				},
 			},
@@ -934,21 +937,12 @@ func (ds *DaemonSuite) testIncrementalPolicy(t *testing.T) {
 	// proxies.
 	networkPolicies := ds.getXDSNetworkPolicies(t, nil)
 	require.Len(t, networkPolicies, 2)
+
 	qaBarNetworkPolicy := networkPolicies[QAIPv4Addr.String()]
 	require.NotNil(t, qaBarNetworkPolicy)
 
-	require.Len(t, qaBarNetworkPolicy.IngressPerPortPolicies, 1)
-
-	expectedIngressPolicy := &cilium.PortNetworkPolicy{
-		Port:     80,
-		Protocol: envoy_config_core.SocketAddress_TCP,
-		Rules: []*cilium.PortNetworkPolicyRule{
-			{
-				L7: &PNPAllowGETbar,
-			},
-		},
-	}
-	require.EqualExportedValues(t, expectedIngressPolicy, qaBarNetworkPolicy.IngressPerPortPolicies[0])
+	// "foo" identity does not exist yet, so there are no ingress policies
+	require.Empty(t, qaBarNetworkPolicy.IngressPerPortPolicies)
 
 	// Allocate identities needed for this test
 	qaFooLbls := labels.Labels{lblFoo.Key: lblFoo, lblQA.Key: lblQA}
@@ -991,7 +985,8 @@ func (ds *DaemonSuite) testIncrementalPolicy(t *testing.T) {
 				Protocol: envoy_config_core.SocketAddress_TCP,
 				Rules: []*cilium.PortNetworkPolicyRule{
 					{
-						L7: &PNPAllowGETbar,
+						RemotePolicies: []uint32{uint32(qaFooID.ID)},
+						L7:             &PNPAllowGETbar,
 					},
 				},
 			},
