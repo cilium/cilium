bb2b1a28af8608e7a6a15d1ca17f56999454f0e1
Timo Beckers <timo@isovalent.com>
2025-05-15 14:53:45 +0200

    bpf: consistently name datapath entry points and tail calls
    Due to a prior commit, all Cilium programs now need to either have the cil_ or
    tail_ prefixes for entry points and tail calls respectively. This enables
    straightforward metrics collection.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/datapath/loader/objects.go
+++ pkg/datapath/loader/objects.go
@@ -25,10 +25,10 @@ type lxcObjects struct {
 	ToContainer   *ebpf.Program `ebpf:"cil_to_container"`
 	FromContainer *ebpf.Program `ebpf:"cil_from_container"`
 
-	PolicyProg *ebpf.Program `ebpf:"handle_policy"`
+	PolicyProg *ebpf.Program `ebpf:"cil_lxc_policy"`
 	PolicyMap  *ebpf.Map     `ebpf:"cilium_call_policy"`
 
-	EgressPolicyProg *ebpf.Program `ebpf:"handle_policy_egress"`
+	EgressPolicyProg *ebpf.Program `ebpf:"cil_lxc_policy_egress"`
 	EgressPolicyMap  *ebpf.Map     `ebpf:"cilium_egresscall_policy"`
 }
 
@@ -42,7 +42,7 @@ type hostObjects struct {
 	ToHost   *ebpf.Program `ebpf:"cil_to_host"`
 	FromHost *ebpf.Program `ebpf:"cil_from_host"`
 
-	PolicyProg *ebpf.Program `ebpf:"handle_lxc_traffic"`
+	PolicyProg *ebpf.Program `ebpf:"cil_host_policy"`
 	PolicyMap  *ebpf.Map     `ebpf:"cilium_call_policy"`
 }
 
