a2f34f0f2cc377c550b94147cda17e8f61003cae
Alexandre Barone <abalexandrebarone@gmail.com>
2025-05-15 12:20:30 -0400

    crypto/certloader: Add reusable cell provider for WatchedServerConfig
    Add a new constructor that can be used in a cell provide call that
    provides a Promise over a WatchedServerConfig. The promise resolves
    when the certificate files become available. When disabled, the
    promise will be nil, allowing to detect whether certloader was enabled
    or not. On hive stop, the cell will take care of stopping the config
    watcher if it exists.
    
    Signed-off-by: Alexandre Barone <abalexandrebarone@gmail.com>
    (cherry picked from commit 0a6b63c961c67d14b86a3948fa3a0409a9243773)

---
+++ pkg/crypto/certloader/cell_test.go
@@ -0,0 +1,193 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package certloader
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/hivetest"
+	"github.com/cilium/hive/job"
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/health/types"
+	"github.com/cilium/cilium/pkg/promise"
+)
+
+func TestCell(t *testing.T) {
+	t.Cleanup(func() {
+		goleak.VerifyNone(t)
+	})
+
+	// create directory structure and config inputs
+	dir, hubble, relay := directories(t)
+
+	// init hive
+	ctx := t.Context()
+	var err error
+	var serverConfig *WatchedServerConfig
+
+	config := testConfig{
+		TLS:              true,
+		TLSCertFile:      hubble.certFile,
+		TLSKeyFile:       hubble.privkeyFile,
+		TLSClientCAFiles: hubble.caFiles,
+	}
+
+	hive := hive.New(
+		cell.Provide(func(lc cell.Lifecycle, p types.Provider, jr job.Registry) job.Group {
+			h := p.ForModule(cell.FullModuleID{"test"})
+			return jr.NewGroup(h, lc)
+		}),
+		cell.ProvidePrivate(func(cfg testConfig) Config {
+			return Config(cfg)
+		}),
+		cell.Provide(NewWatchedServerConfigPromise),
+		cell.Config(config),
+		cell.Invoke(func(p promise.Promise[*WatchedServerConfig]) {
+			if p != nil {
+				go func() {
+					serverConfig, err = p.Await(ctx)
+				}()
+			}
+		}),
+	)
+
+	// start hive
+	tlog := hivetest.Logger(t)
+	if err := hive.Start(tlog, ctx); err != nil {
+		t.Fatalf("failed to start: %s", err)
+	}
+
+	// create tmp certs after a delay
+	go func() {
+		select {
+		case <-time.After(time.Second):
+		case <-ctx.Done():
+			return
+		}
+		setup(t, hubble, relay)
+		t.Cleanup(func() {
+			cleanup(dir)
+		})
+	}()
+
+	require.Eventually(t, func() bool {
+		return err == nil && serverConfig != nil && serverConfig.certFile == config.TLSCertFile
+	}, 5*time.Second, 100*time.Millisecond, "TLS server config promise should resolve after a delay")
+
+	if err := hive.Stop(tlog, ctx); err != nil && !errors.Is(err, context.Canceled) {
+		t.Fatalf("failed to stop: %s", err)
+	}
+}
+
+func TestCellConfigError(t *testing.T) {
+	t.Cleanup(func() {
+		goleak.VerifyNone(t)
+	})
+
+	// init hive
+	ctx := t.Context()
+	var err error
+	var serverConfig *WatchedServerConfig
+
+	hive := hive.New(
+		cell.Provide(func(lc cell.Lifecycle, p types.Provider, jr job.Registry) job.Group {
+			h := p.ForModule(cell.FullModuleID{"test"})
+			return jr.NewGroup(h, lc)
+		}),
+		cell.ProvidePrivate(func(cfg testConfig) Config {
+			return Config(cfg)
+		}),
+		cell.Provide(NewWatchedServerConfigPromise),
+		//exhaustruct:ignore
+		cell.Config(testConfig{
+			TLS: true,
+		}),
+		cell.Invoke(func(p promise.Promise[*WatchedServerConfig]) {
+			if p != nil {
+				go func() {
+					serverConfig, err = p.Await(ctx)
+				}()
+			}
+		}),
+	)
+
+	// start hive
+	tlog := hivetest.Logger(t)
+	if err := hive.Start(tlog, ctx); err != nil {
+		t.Fatalf("failed to start: %s", err)
+	}
+
+	// add a small delay
+	time.Sleep(time.Second)
+
+	require.Eventually(t, func() bool {
+		return err != nil && serverConfig == nil
+	}, 5*time.Second, 100*time.Millisecond, "TLS server config promise should resolve with an error after a delay")
+
+	if err := hive.Stop(tlog, ctx); err != nil && !errors.Is(err, context.Canceled) {
+		t.Fatalf("failed to stop: %s", err)
+	}
+}
+
+func TestCellDisabled(t *testing.T) {
+	t.Cleanup(func() {
+		goleak.VerifyNone(t)
+	})
+
+	ctx := t.Context()
+	var cfgPromise promise.Promise[*WatchedServerConfig]
+
+	hive := hive.New(
+		cell.Provide(func(lc cell.Lifecycle, p types.Provider, jr job.Registry) job.Group {
+			h := p.ForModule(cell.FullModuleID{"test"})
+			return jr.NewGroup(h, lc)
+		}),
+		cell.ProvidePrivate(func(cfg testConfig) Config {
+			return Config(cfg)
+		}),
+		cell.Provide(NewWatchedServerConfigPromise),
+		//exhaustruct:ignore
+		cell.Config(testConfig{
+			TLS: false,
+		}),
+		cell.Invoke(func(p promise.Promise[*WatchedServerConfig]) {
+			cfgPromise = p
+		}),
+	)
+
+	tlog := hivetest.Logger(t)
+	if err := hive.Start(tlog, ctx); err != nil {
+		t.Fatalf("failed to start: %s", err)
+	}
+
+	if cfgPromise != nil {
+		t.Fatalf("certloader unexpectedly enabled")
+	}
+
+	if err := hive.Stop(tlog, ctx); err != nil && !errors.Is(err, context.Canceled) {
+		t.Fatalf("failed to stop: %s", err)
+	}
+}
+
+type testConfig struct {
+	TLS              bool     `mapstructure:"tls"`
+	TLSCertFile      string   `mapstructure:"tls-cert-file"`
+	TLSKeyFile       string   `mapstructure:"tls-key-file"`
+	TLSClientCAFiles []string `mapstructure:"tls-client-ca-files"`
+}
+
+func (def testConfig) Flags(flags *pflag.FlagSet) {
+	flags.Bool("tls", def.TLS, "")
+	flags.String("tls-cert-file", def.TLSCertFile, "")
+	flags.String("tls-key-file", def.TLSKeyFile, "")
+	flags.StringSlice("tls-client-ca-files", def.TLSClientCAFiles, "")
+}
