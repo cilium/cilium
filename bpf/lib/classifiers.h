7bba6ac7fb0209e998db0e2ac838a6d080ae752f
Simone Magnani <simone.magnani@isovalent.com>
2025-05-15 16:48:57 +0200

    bpf: refactor L2/L3 device check for tracing into classifiers
    This commit refactors the current logic to signal packets coming from a
    L3 device and IPv6 packets into a unique file `bpf/lib/classifiers.h` to
    be used from within `bpf/lib/{trace,drop}.h`. Right now these two files
    share the logic to detect such packets and set the apposite bit into their
    message structures. With this commit we move from individual bits to a
    unique `__u8 flags` field in the struct trace/drop. This helps with
    upcoming patches where we'll add additional classifiers to also tag
    Encrypted/Decrypted WireGuard/IPSec packets and overlay. This also aligns
    to the userspace Hubble/Monitor, where we have a unique Flag field.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

---
+++ bpf/lib/classifiers.h
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright Authors of Cilium */
+
+#pragma once
+
+#include "lib/common.h"
+
+typedef __u8 cls_flags_t;
+
+/* Classification flags used to enrich trace/drop notifications events. */
+enum {
+	/* Packet uses IPv6. This flag is only needed/set in trace event:
+	 * - carrying the orig_ip IPv6 info from send_trace_notify6, or
+	 * - with L3 IPv6 packets, to instruct Hubble to use the right decoder.
+	 */
+	CLS_FLAG_IPV6	   = (1 << 0),
+	/* Packet originates from a L3 device (no ethernet header). */
+	CLS_FLAG_L3_DEV    = (1 << 1),
+};
+
+/* Wrapper for specifying empty flags during the trace/drop event. */
+#define CLS_FLAG_NONE ((cls_flags_t)0)
+
+/**
+ * ctx_classify
+ * @ctx: socket buffer
+ *
+ * Compute classifiers (CLS_FLAG_*) for the given packet to be used during
+ * trace/drop notification events.
+ */
+static __always_inline cls_flags_t
+ctx_classify(const struct __ctx_buff *ctx __maybe_unused)
+{
+#if defined(IS_BPF_WIREGUARD)
+	if (ctx_get_protocol(ctx) == bpf_htons(ETH_P_IPV6))
+		return CLS_FLAG_L3_DEV | CLS_FLAG_IPV6;
+
+	return CLS_FLAG_L3_DEV;
+#else
+	return CLS_FLAG_NONE;
+#endif
+}
