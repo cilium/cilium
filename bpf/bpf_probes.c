923fb3c8806610a0f21b0653fb44297371891b86
Jordan Rife <jrife@google.com>
2025-05-17 19:15:40 -0700

    bpf: fib: Add probe for BPF_FIB_LOOKUP_SKIP_NEIGH support
    Prepare for the next patch which passes BPF_FIB_LOOKUP_SKIP_NEIGH to
    bpf_fib_lookup in cases where we want to defer to bpf_redirect_neigh for
    neighbor resolution by adding a probe to see if the current system
    supports it.
    
    Fixes: cilium#34503
    Signed-off-by: Jordan Rife <jrife@google.com>

---
+++ bpf/bpf_probes.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#include <bpf/ctx/unspec.h>
+#include <bpf/api.h>
+
+#include <node_config.h>
+#include <lib/static_data.h>
+
+#include "lib/common.h"
+
+__section_entry
+int probe_fib_lookup_skip_neigh(struct __ctx_buff *ctx)
+{
+	struct bpf_fib_lookup fib_params = {
+		.family		= AF_INET,
+		.ifindex	= ctx_get_ifindex(ctx),
+		.ipv4_src	= 0,
+		.ipv4_dst	= 0,
+	};
+
+	/* Returns < 0 if flags are invalid. */
+	return fib_lookup(ctx, &fib_params, sizeof(fib_params),
+			  BPF_FIB_LOOKUP_SKIP_NEIGH) < 0;
+}
+
+BPF_LICENSE("Dual BSD/GPL");
