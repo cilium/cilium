eec9882f4e067cafb85886591ed90060c7ad8584
Marc Suñé <marc.sune@isovalent.com>
2025-05-20 12:00:43 +0200

    pkg: add v6 L2 responder map
    This commit adds v6 L2 responder map.
    
    Signed-off-by: Marc Suñé <marc.sune@isovalent.com>

--- pkg/maps/l2v6respondermap/l2_responder_map6.go
+++ pkg/maps/l2v6respondermap/l2_responder_map6.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package l2respondermap
+package l2v6respondermap
 
 import (
 	"fmt"
@@ -14,11 +14,12 @@ import (
 	"golang.org/x/sys/unix"
 
 	"github.com/cilium/cilium/pkg/ebpf"
+	"github.com/cilium/cilium/pkg/maps/l2respondermap"
 	"github.com/cilium/cilium/pkg/types"
 )
 
 const (
-	MapName           = "cilium_l2_responder_v4"
+	MapName           = "cilium_l2_responder_v6"
 	DefaultMaxEntries = 4096
 )
 
@@ -26,7 +27,7 @@ var Cell = cell.Provide(NewMap)
 
 type Map interface {
 	Create(ip netip.Addr, ifIndex uint32) error
-	Lookup(ip netip.Addr, ifIndex uint32) (*L2ResponderStats, error)
+	Lookup(ip netip.Addr, ifIndex uint32) (*l2respondermap.L2ResponderStats, error)
 	Delete(ip netip.Addr, ifIndex uint32) error
 	IterateWithCallback(cb IterateCallback) error
 }
@@ -35,12 +36,12 @@ func NewMap(lifecycle cell.Lifecycle, logger *slog.Logger) Map {
 	return newMap(lifecycle, DefaultMaxEntries, logger)
 }
 
-type l2ResponderMap struct {
+type l2V6ResponderMap struct {
 	*ebpf.Map
 }
 
-func newMap(lifecycle cell.Lifecycle, maxEntries int, logger *slog.Logger) *l2ResponderMap {
-	outerMap := &l2ResponderMap{}
+func newMap(lifecycle cell.Lifecycle, maxEntries int, logger *slog.Logger) *l2V6ResponderMap {
+	outerMap := &l2V6ResponderMap{}
 
 	lifecycle.Append(cell.Hook{
 		OnStart: func(hc cell.HookContext) error {
@@ -53,8 +54,8 @@ func newMap(lifecycle cell.Lifecycle, maxEntries int, logger *slog.Logger) *l2Re
 				m = ebpf.NewMap(logger, &ebpf.MapSpec{
 					Name:       MapName,
 					Type:       ebpf.Hash,
-					KeySize:    uint32(unsafe.Sizeof(L2ResponderKey{})),
-					ValueSize:  uint32(unsafe.Sizeof(L2ResponderStats{})),
+					KeySize:    uint32(unsafe.Sizeof(L2V6ResponderKey{})),
+					ValueSize:  uint32(unsafe.Sizeof(l2respondermap.L2ResponderStats{})),
 					MaxEntries: uint32(maxEntries),
 					Flags:      unix.BPF_F_NO_PREALLOC,
 					Pinning:    ebpf.PinByName,
@@ -74,21 +75,21 @@ func newMap(lifecycle cell.Lifecycle, maxEntries int, logger *slog.Logger) *l2Re
 }
 
 // Create creates a new entry for the given IP and IfIndex tuple.
-func (m *l2ResponderMap) Create(ip netip.Addr, ifIndex uint32) error {
-	key := newL2ResponderKey(ip, ifIndex)
-	return m.Map.Put(key, L2ResponderStats{})
+func (m *l2V6ResponderMap) Create(ip netip.Addr, ifIndex uint32) error {
+	key := newL2V6ResponderKey(ip, ifIndex)
+	return m.Map.Put(key, l2respondermap.L2ResponderStats{})
 }
 
 // Delete deletes the entry associated with the provided IP and IfIndex tuple.
-func (m *l2ResponderMap) Delete(ip netip.Addr, ifIndex uint32) error {
-	key := newL2ResponderKey(ip, ifIndex)
+func (m *l2V6ResponderMap) Delete(ip netip.Addr, ifIndex uint32) error {
+	key := newL2V6ResponderKey(ip, ifIndex)
 	return m.Map.Delete(key)
 }
 
 // Lookup returns the stats object associated with the provided IP and IfIndex tuple.
-func (m *l2ResponderMap) Lookup(ip netip.Addr, ifIndex uint32) (*L2ResponderStats, error) {
-	key := newL2ResponderKey(ip, ifIndex)
-	val := L2ResponderStats{}
+func (m *l2V6ResponderMap) Lookup(ip netip.Addr, ifIndex uint32) (*l2respondermap.L2ResponderStats, error) {
+	key := newL2V6ResponderKey(ip, ifIndex)
+	val := l2respondermap.L2ResponderStats{}
 
 	err := m.Map.Lookup(&key, &val)
 
@@ -98,46 +99,35 @@ func (m *l2ResponderMap) Lookup(ip netip.Addr, ifIndex uint32) (*L2ResponderStat
 // IterateCallback represents the signature of the callback function
 // expected by the IterateWithCallback method, which in turn is used to iterate
 // all the keys/values of a L2 responder map.
-type IterateCallback func(*L2ResponderKey, *L2ResponderStats)
+type IterateCallback func(*L2V6ResponderKey, *l2respondermap.L2ResponderStats)
 
 // IterateWithCallback iterates through all the keys/values of a L2 responder map,
 // passing each key/value pair to the cb callback.
-func (m *l2ResponderMap) IterateWithCallback(cb IterateCallback) error {
-	return m.Map.IterateWithCallback(&L2ResponderKey{}, &L2ResponderStats{},
+func (m *l2V6ResponderMap) IterateWithCallback(cb IterateCallback) error {
+	return m.Map.IterateWithCallback(&L2V6ResponderKey{}, &l2respondermap.L2ResponderStats{},
 		func(k, v any) {
-			key := k.(*L2ResponderKey)
-			value := v.(*L2ResponderStats)
+			key := k.(*L2V6ResponderKey)
+			value := v.(*l2respondermap.L2ResponderStats)
 			cb(key, value)
 		},
 	)
 }
 
-// L2ResponderKey implements the bpf.MapKey interface.
+// L2V6ResponderKey implements the bpf.MapKey interface.
 //
-// Must be in sync with struct l2_responder_v4_key in <bpf/lib/maps.h>
-type L2ResponderKey struct {
-	IP      types.IPv4 `align:"ip4"`
+// Must be in sync with struct l2_responder_v6_key in <bpf/lib/l2_responder.h>
+type L2V6ResponderKey struct {
+	IP      types.IPv6 `align:"ip6"`
 	IfIndex uint32     `align:"ifindex"`
 }
 
-func (k *L2ResponderKey) String() string {
+func (k *L2V6ResponderKey) String() string {
 	return fmt.Sprintf("ip=%s, ifIndex=%d", net.IP(k.IP[:]), k.IfIndex)
 }
 
-func newL2ResponderKey(ip netip.Addr, ifIndex uint32) L2ResponderKey {
-	return L2ResponderKey{
-		IP:      types.IPv4(ip.As4()),
+func newL2V6ResponderKey(ip netip.Addr, ifIndex uint32) L2V6ResponderKey {
+	return L2V6ResponderKey{
+		IP:      types.IPv6(ip.As16()),
 		IfIndex: ifIndex,
 	}
-}
-
-// L2ResponderStats implements the bpf.MapValue interface.
-//
-// Must be in sync with struct l2_responder_stats in <bpf/lib/l2_responder.h>
-type L2ResponderStats struct {
-	ResponsesSent uint64 `align:"responses_sent"`
-}
-
-func (s *L2ResponderStats) String() string {
-	return fmt.Sprintf("responses_sent=%q", s.ResponsesSent)
 }
