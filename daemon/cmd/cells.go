4688eb47350816145c9c582041347e0f4fe9da04
Stanislav Zaikin <zstaseg@gmail.com>
2025-05-23 13:56:19 +0200

    bpf, dbg: add vtep policy for flexible routing from/to outside world
    Co-developed-by: Jarrod Baumann <j@rrod.io>
    Signed-off-by: Jarrod Baumann <j@rrod.io>
    Signed-off-by: Stanislav Zaikin <zstaseg@gmail.com>

--- daemon/cmd/cells.go
+++ daemon/cmd/cells.go
@@ -78,6 +78,7 @@ import (
 	"github.com/cilium/cilium/pkg/signal"
 	"github.com/cilium/cilium/pkg/source"
 	"github.com/cilium/cilium/pkg/status"
+	"github.com/cilium/cilium/pkg/vteppolicy"
 )
 
 var (
@@ -337,6 +338,9 @@ var (
 
 		// Cilium Debuginfo API
 		debugapi.Cell,
+
+		// VTEP Policy allows two-way communication with an external VXLAN gateway
+		vteppolicy.Cell,
 	)
 )
 
