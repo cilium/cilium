2ee43a1be51acd4313870665474fa6a665ff17d4
Marcel Zieba <marcel.zieba@isovalent.com>
2025-05-28 15:00:48 +0200

    policy: add test for duplicated PortRuleHTTP
    This test fails with current implementation as we blindly copy
    PortRuleHTTP for the first rule skipping removal of duplicates.
    
    Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>

--- pkg/policy/rule_test.go
+++ pkg/policy/rule_test.go
@@ -2433,6 +2433,52 @@ func TestMergeL7PolicyEgressWithMultipleSelectors(t *testing.T) {
 	td.policyMapEquals(t, nil, expected, &rule1)
 }
 
+func TestMergeL7DuplicatedRules(t *testing.T) {
+	td := newTestData(hivetest.Logger(t))
+
+	rule1 := api.Rule{
+		EndpointSelector: endpointSelectorA,
+		Egress: []api.EgressRule{
+			{
+				EgressCommonRule: api.EgressCommonRule{
+					ToEndpoints: []api.EndpointSelector{endpointSelectorC},
+				},
+				ToPorts: []api.PortRule{{
+					Ports: []api.PortProtocol{
+						{Port: "80", Protocol: api.ProtoTCP},
+					},
+					Rules: &api.L7Rules{
+						HTTP: []api.PortRuleHTTP{
+							{Method: "GET"},
+							{Method: "GET"},
+						},
+					},
+				}},
+			},
+		},
+	}
+
+	expected := NewL4PolicyMapWithValues(map[string]*L4Filter{"80/TCP": {
+		Port: 80, Protocol: api.ProtoTCP, U8Proto: 6,
+		PerSelectorPolicies: L7DataMap{
+			td.cachedSelectorC: &PerSelectorPolicy{
+				L7Parser: ParserTypeHTTP,
+				Priority: ListenerPriorityHTTP,
+				L7Rules: api.L7Rules{
+					HTTP: []api.PortRuleHTTP{{Method: "GET"}},
+				},
+			},
+		},
+		Ingress: false,
+		RuleOrigin: OriginForTest(map[CachedSelector]labels.LabelArrayList{
+			td.cachedSelectorB: {nil},
+			td.cachedSelectorC: {nil},
+		}),
+	}})
+
+	td.policyMapEquals(t, nil, expected, &rule1)
+}
+
 func TestMergeListenerReference(t *testing.T) {
 	// No listener remains a no listener
 	ps := &PerSelectorPolicy{}
