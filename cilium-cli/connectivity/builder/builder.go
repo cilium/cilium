4b518c3f3e31d941270151b40beddad17755349d
Chris Tarazi <chris@isovalent.com>
2025-05-28 11:38:36 -0700

    cilium-cli: Use v2alpha1 version of CCG for Cilium versions below v1.18
    This was causing issues in CI[1].
    
    [1]: https://github.com/cilium/cilium-cli/actions/runs/15278380967/job/42970668457?pr=3039
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- cilium-cli/connectivity/builder/builder.go
+++ cilium-cli/connectivity/builder/builder.go
@@ -61,8 +61,14 @@ var (
 	//go:embed manifests/client-egress-to-cidrgroup-external-deny.yaml
 	clientEgressToCIDRGroupExternalDenyPolicyYAML string
 
+	//go:embed manifests/client-egress-to-cidrgroup-external-deny-v2alpha1.yaml
+	clientEgressToCIDRGroupExternalDenyPolicyV2Alpha1YAML string
+
 	//go:embed manifests/client-egress-to-cidrgroup-external-deny-label.yaml
 	clientEgressToCIDRGroupExternalDenyLabelPolicyYAML string
+
+	//go:embed manifests/client-egress-to-cidrgroup-external-deny-label-v2alpha1.yaml
+	clientEgressToCIDRGroupExternalDenyLabelPolicyV2Alpha1YAML string
 
 	//go:embed manifests/client-egress-l7-http.yaml
 	clientEgressL7HTTPPolicyYAML string
@@ -327,32 +333,34 @@ func finalTests(ct *check.ConnectivityTest) error {
 
 func renderTemplates(clusterName string, param check.Parameters) (map[string]string, error) {
 	templates := map[string]string{
-		"clientEgressToCIDRExternalPolicyYAML":               clientEgressToCIDRExternalPolicyYAML,
-		"clientEgressToCIDRExternalPolicyKNPYAML":            clientEgressToCIDRExternalPolicyKNPYAML,
-		"clientEgressToCIDRNodeKNPYAML":                      clientEgressToCIDRNodeKNPYAML,
-		"clientEgressToCIDRExternalDenyPolicyYAML":           clientEgressToCIDRExternalDenyPolicyYAML,
-		"clientEgressToCIDRGroupExternalDenyPolicyYAML":      clientEgressToCIDRGroupExternalDenyPolicyYAML,
-		"clientEgressToCIDRGroupExternalDenyLabelPolicyYAML": clientEgressToCIDRGroupExternalDenyLabelPolicyYAML,
-		"clientEgressL7HTTPPolicyYAML":                       clientEgressL7HTTPPolicyYAML,
-		"clientEgressL7HTTPPolicyPortRangeYAML":              clientEgressL7HTTPPolicyPortRangeYAML,
-		"clientEgressL7HTTPNamedPortPolicyYAML":              clientEgressL7HTTPNamedPortPolicyYAML,
-		"clientEgressToFQDNsPolicyYAML":                      clientEgressToFQDNsPolicyYAML,
-		"clientEgressToFQDNsAndHTTPGetPolicyYAML":            clientEgressToFQDNsAndHTTPGetPolicyYAML,
-		"clientEgressTLSSNIPolicyYAML":                       clientEgressTLSSNIPolicyYAML,
-		"clientEgressTLSSNIWildcardPolicyYAML":               clientEgressTLSSNIWildcardPolicyYAML,
-		"clientEgressTLSSNIDoubleWildcardPolicyYAML":         clientEgressTLSSNIDoubleWildcardPolicyYAML,
-		"clientEgressTLSSNIOtherPolicyYAML":                  clientEgressTLSSNIOtherPolicyYAML,
-		"clientEgressL7TLSSNIPolicyYAML":                     clientEgressL7TLSSNIPolicyYAML,
-		"clientEgressL7TLSOtherSNIPolicyYAML":                clientEgressL7TLSOtherSNIPolicyYAML,
-		"clientEgressL7TLSPolicyYAML":                        clientEgressL7TLSPolicyYAML,
-		"clientEgressL7TLSPolicyPortRangeYAML":               clientEgressL7TLSPolicyPortRangeYAML,
-		"clientEgressL7HTTPMatchheaderSecretYAML":            clientEgressL7HTTPMatchheaderSecretYAML,
-		"clientEgressL7HTTPMatchheaderSecretPortRangeYAML":   clientEgressL7HTTPMatchheaderSecretPortRangeYAML,
-		"clientEgressL7HTTPExternalYAML":                     clientEgressL7HTTPExternalYAML,
-		"clientEgressNodeLocalDNSYAML":                       clientEgressNodeLocalDNSYAML,
-		"clientEgressOnlyDNSPolicyYAML":                      clientEgressOnlyDNSPolicyYAML,
-		"echoIngressFromCIDRYAML":                            echoIngressFromCIDRYAML,
-		"denyCIDRPolicyYAML":                                 denyCIDRPolicyYAML,
+		"clientEgressToCIDRExternalPolicyYAML":                       clientEgressToCIDRExternalPolicyYAML,
+		"clientEgressToCIDRExternalPolicyKNPYAML":                    clientEgressToCIDRExternalPolicyKNPYAML,
+		"clientEgressToCIDRNodeKNPYAML":                              clientEgressToCIDRNodeKNPYAML,
+		"clientEgressToCIDRExternalDenyPolicyYAML":                   clientEgressToCIDRExternalDenyPolicyYAML,
+		"clientEgressToCIDRGroupExternalDenyPolicyYAML":              clientEgressToCIDRGroupExternalDenyPolicyYAML,
+		"clientEgressToCIDRGroupExternalDenyPolicyV2Alpha1YAML":      clientEgressToCIDRGroupExternalDenyPolicyV2Alpha1YAML,
+		"clientEgressToCIDRGroupExternalDenyLabelPolicyYAML":         clientEgressToCIDRGroupExternalDenyLabelPolicyYAML,
+		"clientEgressToCIDRGroupExternalDenyLabelPolicyV2Alpha1YAML": clientEgressToCIDRGroupExternalDenyLabelPolicyV2Alpha1YAML,
+		"clientEgressL7HTTPPolicyYAML":                               clientEgressL7HTTPPolicyYAML,
+		"clientEgressL7HTTPPolicyPortRangeYAML":                      clientEgressL7HTTPPolicyPortRangeYAML,
+		"clientEgressL7HTTPNamedPortPolicyYAML":                      clientEgressL7HTTPNamedPortPolicyYAML,
+		"clientEgressToFQDNsPolicyYAML":                              clientEgressToFQDNsPolicyYAML,
+		"clientEgressToFQDNsAndHTTPGetPolicyYAML":                    clientEgressToFQDNsAndHTTPGetPolicyYAML,
+		"clientEgressTLSSNIPolicyYAML":                               clientEgressTLSSNIPolicyYAML,
+		"clientEgressTLSSNIWildcardPolicyYAML":                       clientEgressTLSSNIWildcardPolicyYAML,
+		"clientEgressTLSSNIDoubleWildcardPolicyYAML":                 clientEgressTLSSNIDoubleWildcardPolicyYAML,
+		"clientEgressTLSSNIOtherPolicyYAML":                          clientEgressTLSSNIOtherPolicyYAML,
+		"clientEgressL7TLSSNIPolicyYAML":                             clientEgressL7TLSSNIPolicyYAML,
+		"clientEgressL7TLSOtherSNIPolicyYAML":                        clientEgressL7TLSOtherSNIPolicyYAML,
+		"clientEgressL7TLSPolicyYAML":                                clientEgressL7TLSPolicyYAML,
+		"clientEgressL7TLSPolicyPortRangeYAML":                       clientEgressL7TLSPolicyPortRangeYAML,
+		"clientEgressL7HTTPMatchheaderSecretYAML":                    clientEgressL7HTTPMatchheaderSecretYAML,
+		"clientEgressL7HTTPMatchheaderSecretPortRangeYAML":           clientEgressL7HTTPMatchheaderSecretPortRangeYAML,
+		"clientEgressL7HTTPExternalYAML":                             clientEgressL7HTTPExternalYAML,
+		"clientEgressNodeLocalDNSYAML":                               clientEgressNodeLocalDNSYAML,
+		"clientEgressOnlyDNSPolicyYAML":                              clientEgressOnlyDNSPolicyYAML,
+		"echoIngressFromCIDRYAML":                                    echoIngressFromCIDRYAML,
+		"denyCIDRPolicyYAML":                                         denyCIDRPolicyYAML,
 	}
 	if param.K8sLocalHostTest {
 		templates["clientEgressToCIDRCPHostPolicyYAML"] = clientEgressToCIDRCPHostPolicyYAML
--- cilium-cli/connectivity/builder/client_egress_to_cidrgroup_deny.go
+++ cilium-cli/connectivity/builder/client_egress_to_cidrgroup_deny.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cilium/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium/cilium-cli/connectivity/tests"
 	"github.com/cilium/cilium/cilium-cli/utils/features"
+	"github.com/cilium/cilium/pkg/versioncheck"
 )
 
 type clientEgressToCidrgroupDeny struct{}
@@ -14,10 +15,14 @@ type clientEgressToCidrgroupDeny struct{}
 func (t clientEgressToCidrgroupDeny) build(ct *check.ConnectivityTest, templates map[string]string) {
 	// This policy denies L3 traffic to ExternalCIDR except ExternalIP/32
 	// It does so using a CiliumCIDRGroup
+	policy := templates["clientEgressToCIDRGroupExternalDenyPolicyYAML"]
+	if !versioncheck.MustCompile("<=1.17.0")(ct.CiliumVersion) {
+		policy = templates["clientEgressToCIDRGroupExternalDenyPolicyV2Alpha1YAML"]
+	}
 	newTest("client-egress-to-cidrgroup-deny", ct).
 		WithCiliumVersion(">=1.17.0").
 		WithCiliumPolicy(allowAllEgressPolicyYAML). // Allow all egress traffic
-		WithCiliumPolicy(templates["clientEgressToCIDRGroupExternalDenyPolicyYAML"]).
+		WithCiliumPolicy(policy).
 		WithScenarios(
 			tests.PodToCIDR(tests.WithRetryDestIP(ct.Params().ExternalIPv4)), // Denies all traffic to ExternalOtherIP, but allow ExternalIP
 		).
@@ -40,10 +45,14 @@ type clientEgressToCidrgroupDenyByLabel struct{}
 func (t clientEgressToCidrgroupDenyByLabel) build(ct *check.ConnectivityTest, templates map[string]string) {
 	// This policy denies L3 traffic to ExternalCIDR except ExternalIP/32
 	// It does so using a CiliumCIDRGroup
+	policy := templates["clientEgressToCIDRGroupExternalDenyLabelPolicyYAML"]
+	if !versioncheck.MustCompile("<=1.17.0")(ct.CiliumVersion) {
+		policy = templates["clientEgressToCIDRGroupExternalDenyLabelPolicyV2Alpha1YAML"]
+	}
 	newTest("client-egress-to-cidrgroup-deny-by-label", ct).
 		WithCiliumVersion(">=1.17.0").
 		WithCiliumPolicy(allowAllEgressPolicyYAML). // Allow all egress traffic
-		WithCiliumPolicy(templates["clientEgressToCIDRGroupExternalDenyLabelPolicyYAML"]).
+		WithCiliumPolicy(policy).
 		WithScenarios(
 			tests.PodToCIDR(tests.WithRetryDestIP(ct.Params().ExternalIPv4)), // Denies all traffic to ExternalOtherIP, but allow ExternalIP
 		).
