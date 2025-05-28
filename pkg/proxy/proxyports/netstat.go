2a77563bd8be8e9cd36517d01eb132bc1b942e66
Mikhail Gorozhin <m.gorozhin@gmail.com>
2025-05-28 12:11:13 +0500

    Fix: L7 policy not working when kernel disable ipv6
    Prevents proxy from reporting an error while reading /proc/net/ files for the disabled ipvX version.
    
    Fixes: #14865
    
    Signed-off-by: Mikhail Gorozhin <m.gorozhin@gmail.com>

--- pkg/proxy/proxyports/netstat.go
+++ pkg/proxy/proxyports/netstat.go
@@ -10,21 +10,18 @@ import (
 	"strconv"
 
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 var (
-	// procNetTCPFiles is the constant list of /proc/net files to read to get
-	// the same information about open TCP connections as output by netstat.
-	procNetTCPFiles = []string{
-		"/proc/net/tcp",
-		"/proc/net/tcp6",
-	}
-
-	// procNetUDPFiles is the constant list of /proc/net files to read to get
-	// the same information about open UDP connections as output by netstat.
-	procNetUDPFiles = []string{
-		"/proc/net/udp",
-		"/proc/net/udp6",
+	// procNetFiles is the constant map showing the correspondance of /proc/net files
+	// to the bool flag of the "do we expect them to be present based on the config"
+	// /proc/net files may be used to get the information about open connections as output by netstat.
+	procNetFiles = map[string]bool{
+		"/proc/net/tcp":  option.Config.EnableIPv4,
+		"/proc/net/udp":  option.Config.EnableIPv4,
+		"/proc/net/tcp6": option.Config.EnableIPv6,
+		"/proc/net/udp6": option.Config.EnableIPv6,
 	}
 
 	// procNetFileRegexp matches the first two columns of /proc/net/{tcp,udp}*
@@ -36,13 +33,18 @@ var (
 func (p *ProxyPorts) GetOpenLocalPorts() map[uint16]struct{} {
 	openLocalPorts := make(map[uint16]struct{}, 128)
 
-	for _, file := range append(procNetTCPFiles, procNetUDPFiles...) {
+	for file, enabled := range procNetFiles {
 		b, err := os.ReadFile(file)
 		if err != nil {
-			p.logger.Error("cannot read proc file",
-				logfields.Path, file,
-				logfields.Error, err,
-			)
+			// we only need to report this as unexpected behaviour
+			// when the ipvX is enabled in the config, but not present as a file
+			if enabled {
+				p.logger.Error("cannot read proc file",
+					logfields.Path, file,
+					logfields.Error, err,
+				)
+			}
+
 			continue
 		}
 
