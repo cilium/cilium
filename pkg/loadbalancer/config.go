d9d63346db6076f4f4cfa3c0167eb7df5e315d9c
Martynas Pumputis <martynas@isovalent.com>
2025-05-28 13:31:08 +0200

    pkg/lb: Add support for EnableHostPort
    Do not provision HostPort services if !EnableHostPort.
    
    The check will go away in v1.19 -
    https://github.com/cilium/cilium/issues/39582.
    
    Signed-off-by: Martynas Pumputis <martynas@isovalent.com>

--- pkg/loadbalancer/config.go
+++ pkg/loadbalancer/config.go
@@ -498,6 +498,9 @@ type ExternalConfig struct {
 	BPFSocketLBHostnsOnly                  bool
 	EnableSocketLB                         bool
 	EnableSocketLBPodConnectionTermination bool
+
+	// The following options will be removed in v1.19
+	EnableHostPort bool
 }
 
 // NewExternalConfig maps the daemon config to [ExternalConfig].
@@ -510,6 +513,7 @@ func NewExternalConfig(cfg *option.DaemonConfig) ExternalConfig {
 		BPFSocketLBHostnsOnly:                  cfg.BPFSocketLBHostnsOnly,
 		EnableSocketLB:                         cfg.EnableSocketLB,
 		EnableSocketLBPodConnectionTermination: cfg.EnableSocketLBPodConnectionTermination,
+		EnableHostPort:                         cfg.EnableHostPort,
 	}
 }
 
