f166b96b4179f89b9798374d130389263eb79859
Marcel Zieba <marcel.zieba@isovalent.com>
2025-05-30 12:59:26 +0200

    operator: remove deprecated CES options
    In #32523 multiple flags have been deprecated and their values have been
    ignored since 1.16.
    
    Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>

--- operator/pkg/ciliumendpointslice/controller.go
+++ operator/pkg/ciliumendpointslice/controller.go
@@ -5,7 +5,6 @@ package ciliumendpointslice
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
 	"sync"
 	"time"
@@ -111,8 +110,6 @@ func registerController(p params) error {
 		return err
 	}
 
-	checkDeprecatedOpts(p.Cfg, p.Logger)
-
 	cesController := &Controller{
 		logger:              p.Logger,
 		clientset:           clientset,
@@ -131,21 +128,4 @@ func registerController(p params) error {
 	}
 	p.Lifecycle.Append(cesController)
 	return nil
-}
-
-// checkDeprecatedOpts will log an error if the user has supplied any of the
-// no-op, deprecated rate limit options.
-// TODO: Remove this function when the deprecated options are removed.
-func checkDeprecatedOpts(cfg Config, logger *slog.Logger) {
-	switch {
-	case cfg.CESWriteQPSLimit > 0:
-	case cfg.CESWriteQPSBurst > 0:
-	case cfg.CESEnableDynamicRateLimit:
-	case len(cfg.CESDynamicRateLimitNodes) > 0:
-	case len(cfg.CESDynamicRateLimitQPSLimit) > 0:
-	case len(cfg.CESDynamicRateLimitQPSBurst) > 0:
-	default:
-		return
-	}
-	logger.Error(fmt.Sprintf("You are using deprecated rate limit option(s) that have no effect. To configure custom rate limits please use --%s", CESRateLimits))
 }
