d7a4a1676a4d63f728d0f23a4d3980ac53d03033
Marcel Zieba <marcel.zieba@isovalent.com>
2025-05-30 12:59:26 +0200

    operator: remove deprecated CES options
    In #32523 multiple flags have been deprecated and their values have been
    ignored since 1.16.
    
    Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>

--- operator/pkg/ciliumendpointslice/cell.go
+++ operator/pkg/ciliumendpointslice/cell.go
@@ -4,8 +4,6 @@
 package ciliumendpointslice
 
 import (
-	"fmt"
-
 	"github.com/cilium/hive/cell"
 	"github.com/spf13/pflag"
 
@@ -19,32 +17,6 @@ const (
 
 	// CESSlicingMode instructs how CEPs are grouped in a CES.
 	CESSlicingMode = "ces-slice-mode"
-
-	// CESWriteQPSLimit is the rate limit per second for the CES work queue to
-	// process  CES events that result in CES write (Create, Update, Delete)
-	// requests to the kube-apiserver.
-	CESWriteQPSLimit = "ces-write-qps-limit"
-
-	// CESWriteQPSBurst is the burst rate per second used with CESWriteQPSLimit
-	// for the CES work queue to process CES events that result in CES write
-	// (Create, Update, Delete) requests to the kube-apiserver.
-	CESWriteQPSBurst = "ces-write-qps-burst"
-
-	// CESEnableDynamicRateLimit is used to ignore static QPS Limit and Burst
-	// and use dynamic limit, burst and nodes instead.
-	CESEnableDynamicRateLimit = "ces-enable-dynamic-rate-limit"
-
-	// CESDynamicRateLimitNodes is used to specify the list of nodes used for the
-	// dynamic rate limit steps.
-	CESDynamicRateLimitNodes = "ces-dynamic-rate-limit-nodes"
-
-	// CESDynamicRateLimitQPSLimit is used to specify the list of qps limits for the
-	// dynamic rate limit steps.
-	CESDynamicRateLimitQPSLimit = "ces-dynamic-rate-limit-qps-limit"
-
-	// CESDynamicRateLimitQPSBurst is used to specify the list of qps bursts for the
-	// dynamic rate limit steps.
-	CESDynamicRateLimitQPSBurst = "ces-dynamic-rate-limit-qps-burst"
 
 	// CESRateLimits can be used to configure a custom, stepped dynamic rate limit based on cluster size.
 	CESRateLimits = "ces-rate-limits"
@@ -62,15 +34,9 @@ var Cell = cell.Module(
 )
 
 type Config struct {
-	CESMaxCEPsInCES             int      `mapstructure:"ces-max-ciliumendpoints-per-ces"`
-	CESSlicingMode              string   `mapstructure:"ces-slice-mode"`
-	CESWriteQPSLimit            float64  `mapstructure:"ces-write-qps-limit" exhaustruct:"optional"`
-	CESWriteQPSBurst            int      `mapstructure:"ces-write-qps-burst" exhaustruct:"optional"`
-	CESEnableDynamicRateLimit   bool     `mapstructure:"ces-enable-dynamic-rate-limit" exhaustruct:"optional"`
-	CESDynamicRateLimitNodes    []string `mapstructure:"ces-dynamic-rate-limit-nodes" exhaustruct:"optional"`
-	CESDynamicRateLimitQPSLimit []string `mapstructure:"ces-dynamic-rate-limit-qps-limit" exhaustruct:"optional"`
-	CESDynamicRateLimitQPSBurst []string `mapstructure:"ces-dynamic-rate-limit-qps-burst" exhaustruct:"optional"`
-	CESDynamicRateLimitConfig   string   `mapstructure:"ces-rate-limits"`
+	CESMaxCEPsInCES           int    `mapstructure:"ces-max-ciliumendpoints-per-ces"`
+	CESSlicingMode            string `mapstructure:"ces-slice-mode"`
+	CESDynamicRateLimitConfig string `mapstructure:"ces-rate-limits"`
 }
 
 var defaultConfig = Config{
@@ -80,23 +46,9 @@ var defaultConfig = Config{
 }
 
 func (def Config) Flags(flags *pflag.FlagSet) {
-	depUseDLR := fmt.Sprintf("dynamic rate limiting is now configured by default. Please use --%s to supply a custom config", CESRateLimits)
 	flags.Int(CESMaxCEPsInCES, def.CESMaxCEPsInCES, "Maximum number of CiliumEndpoints allowed in a CES")
 	flags.String(CESSlicingMode, def.CESSlicingMode, "Slicing mode defines how CiliumEndpoints are grouped into CES: either batched by their Identity (\"identity\") or batched on a \"First Come, First Served\" basis (\"fcfs\")")
 	flags.MarkDeprecated(CESSlicingMode, "Slicing mode defaults to the FCFS mode and is now deprecated option. It does not have a functional effect")
-	flags.Float64(CESWriteQPSLimit, def.CESWriteQPSLimit, "CES work queue rate limit. Ignored when "+CESEnableDynamicRateLimit+" is set")
-	flags.MarkDeprecated(CESWriteQPSLimit, depUseDLR)
-	flags.Int(CESWriteQPSBurst, def.CESWriteQPSBurst, "CES work queue burst rate. Ignored when "+CESEnableDynamicRateLimit+" is set")
-	flags.MarkDeprecated(CESWriteQPSBurst, depUseDLR)
-
-	flags.Bool(CESEnableDynamicRateLimit, def.CESEnableDynamicRateLimit, "Flag to enable dynamic rate limit specified in separate fields instead of the static one")
-	flags.MarkDeprecated(CESEnableDynamicRateLimit, depUseDLR)
-	flags.StringSlice(CESDynamicRateLimitNodes, def.CESDynamicRateLimitNodes, "List of nodes used for the dynamic rate limit steps")
-	flags.MarkDeprecated(CESDynamicRateLimitNodes, depUseDLR)
-	flags.StringSlice(CESDynamicRateLimitQPSLimit, def.CESDynamicRateLimitQPSLimit, "List of qps limits used for the dynamic rate limit steps")
-	flags.MarkDeprecated(CESDynamicRateLimitQPSLimit, depUseDLR)
-	flags.StringSlice(CESDynamicRateLimitQPSBurst, def.CESDynamicRateLimitQPSBurst, "List of qps burst used for the dynamic rate limit steps")
-	flags.MarkDeprecated(CESDynamicRateLimitQPSBurst, depUseDLR)
 
 	flags.String(CESRateLimits, def.CESDynamicRateLimitConfig, "Configure rate limits for the CES controller. Accepts a list of rate limit configurations, must be a JSON formatted string.")
 }
