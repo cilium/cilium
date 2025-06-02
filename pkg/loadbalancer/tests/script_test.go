ef72d80166cc6bfc9b46d5da2738871f1045e806
Martynas Pumputis <martynas@isovalent.com>
2025-06-02 16:51:34 +0200

    pkg/lb: Add support for EnableSVCSourceRangeCheck
    The flag is most likely to be removed in v1.19.
    
    Signed-off-by: Martynas Pumputis <martynas@isovalent.com>

--- pkg/loadbalancer/tests/script_test.go
+++ pkg/loadbalancer/tests/script_test.go
@@ -101,10 +101,11 @@ func TestScript(t *testing.T) {
 					},
 					func() kpr.KPRConfig {
 						return kpr.KPRConfig{
-							KubeProxyReplacement:  option.KubeProxyReplacementTrue,
-							EnableNodePort:        true,
-							EnableHostPort:        true,
-							EnableSessionAffinity: true,
+							KubeProxyReplacement:      option.KubeProxyReplacementTrue,
+							EnableNodePort:            true,
+							EnableHostPort:            true,
+							EnableSessionAffinity:     true,
+							EnableSVCSourceRangeCheck: true,
 						}
 					},
 					func(ops *lbreconciler.BPFOps, lns *node.LocalNodeStore, w *writer.Writer, waitFn loadbalancer.InitWaitFunc) uhive.ScriptCmdsOut {
