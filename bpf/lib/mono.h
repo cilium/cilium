efc4e394a7bc47d94f42703fea59cfc98997eab1
Vipul Singh <singhvipul@microsoft.com>
2025-06-02 23:07:46 +0000

    fix: Fix implicit conversion warnings in bpf_sec_to_mono macro
    The bpf_sec_to_mono macro was returning a __u64 value, which led to implicit conversion
    warnings when assigned to __u32 variables. This change adds an explicit cast to __u32 to
    ensure the result fits the expected data type.
    
    Signed-off-by: Vipul Singh <singhvipul@microsoft.com>

--- bpf/lib/mono.h
+++ bpf/lib/mono.h
@@ -7,7 +7,7 @@
 #if defined(ENABLE_JIFFIES) && KERNEL_HZ != 1
 # define BPF_MONO_SCALER	8
 # define bpf_mono_now()		(jiffies >> BPF_MONO_SCALER)
-# define bpf_sec_to_mono(s)	(bpf_sec_to_jiffies(s) >> BPF_MONO_SCALER)
+# define bpf_sec_to_mono(s)	((__u32)bpf_sec_to_jiffies(s) >> BPF_MONO_SCALER)
 #else
 # define bpf_mono_now()		bpf_ktime_get_sec()
 # define bpf_sec_to_mono(s)	(s)
