629e7cca943b40a4a447a27c0637e5e52a308ecf
Dylan Reimerink <dylan.reimerink@isovalent.com>
2025-06-03 16:35:30 +0200

    pkg/datapath/linux: make backend neighbor sync work with forwardable IPs
    This commit hooks up the neighbor subsystem to the new loadbalancer
    logic. There currently exists a `syncBackendNeighbors` job which
    maps the backends table to `linuxNodeHandler` calls. We simply change
    that out of calls to the forwardable IP manager.
    
    Since we have access to the backends table, we can simply watch when
    that table is initialized and forward that to the forwardable IP
    initializer.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- pkg/datapath/linux/backend_neighbors.go
+++ pkg/datapath/linux/backend_neighbors.go
@@ -5,22 +5,21 @@ package linux
 
 import (
 	"context"
-	"fmt"
+	"log/slog"
 
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
 	"github.com/cilium/statedb"
 
-	"github.com/cilium/cilium/pkg/datapath/types"
+	"github.com/cilium/cilium/pkg/datapath/neighbor"
 	"github.com/cilium/cilium/pkg/loadbalancer"
-	"github.com/cilium/cilium/pkg/node/addressing"
-	nodeTypes "github.com/cilium/cilium/pkg/node/types"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/rate"
 	"github.com/cilium/cilium/pkg/time"
 )
 
 // BackendNeighborSyncCell watches Table[*loadbalancer.Backend] and inserts/deletes
-// the neighbor table entries for each backend address.
+// the forwardable IP entries for each backend address.
 //
 // This is needed by XDP program to be able to resolve the hardware address of
 // the backend as it cannot use the neighbor sub-system to resolve it on-demand.
@@ -34,23 +33,26 @@ var BackendNeighborSyncCell = cell.Module(
 type backendNeighborSyncParams struct {
 	cell.In
 
-	JobGroup      job.Group
-	DB            *statedb.DB
-	NodeNeighbors types.NodeNeighbors
-	Backends      statedb.Table[*loadbalancer.Backend]
+	JobGroup             job.Group
+	Logger               *slog.Logger
+	DB                   *statedb.DB
+	ForwardableIPManager *neighbor.ForwardableIPManager
+	Backends             statedb.Table[*loadbalancer.Backend]
 }
 
 func registerBackendNeighborSync(p backendNeighborSyncParams) {
+	initializer := p.ForwardableIPManager.RegisterInitializer("service-backend-neighbor-sync")
+
 	p.JobGroup.Add(
 		job.OneShot(
 			"backend-neighbor-sync",
 			func(ctx context.Context, _ cell.Health) error {
-				return syncBackendNeighbors(p, ctx)
+				return syncBackendNeighbors(p, ctx, initializer)
 			},
 		))
 }
 
-func syncBackendNeighbors(p backendNeighborSyncParams, ctx context.Context) error {
+func syncBackendNeighbors(p backendNeighborSyncParams, ctx context.Context, initializer neighbor.ForwardableIPInitializer) error {
 	wtxn := p.DB.WriteTxn(p.Backends)
 	changeIter, err := p.Backends.Changes(wtxn)
 	wtxn.Commit()
@@ -62,22 +64,39 @@ func syncBackendNeighbors(p backendNeighborSyncParams, ctx context.Context) erro
 	limiter := rate.NewLimiter(50*time.Millisecond, 1)
 	defer limiter.Stop()
 
-	addedNeighbors := map[loadbalancer.L3n4Addr]*nodeTypes.Node{}
-
 	for {
-		changes, watch := changeIter.Next(p.DB.ReadTxn())
-		for change := range changes {
-			n, found := addedNeighbors[change.Object.Address]
-			switch {
-			case change.Deleted && found:
-				delete(addedNeighbors, change.Object.Address)
-				p.NodeNeighbors.DeleteMiscNeighbor(n)
+		rx := p.DB.ReadTxn()
 
-			case !change.Deleted && !found:
-				n := backendToNode(change.Object)
-				addedNeighbors[change.Object.Address] = n
-				p.NodeNeighbors.InsertMiscNeighbor(n)
+		changes, watch := changeIter.Next(rx)
+		for change := range changes {
+			owner := neighbor.ForwardableIPOwner{
+				Type: neighbor.ForwardableIPOwnerService,
+				ID:   change.Object.Address.StringID(),
 			}
+
+			if change.Deleted {
+				err := p.ForwardableIPManager.Delete(
+					change.Object.Address.AddrCluster.Addr(),
+					owner,
+				)
+				if err != nil {
+					p.Logger.Error("Failed to delete forwardable IP", logfields.Error, err)
+				}
+			} else {
+				err := p.ForwardableIPManager.Insert(
+					change.Object.Address.AddrCluster.Addr(),
+					owner,
+				)
+				if err != nil {
+					p.Logger.Error("Failed to insert forwardable IP", logfields.Error, err)
+				}
+			}
+		}
+
+		// If the service backends have been initialized, we can finish the
+		// initializer of the ForwardableIP table.
+		if init, _ := p.Backends.Initialized(rx); init {
+			p.ForwardableIPManager.FinishInitializer(initializer)
 		}
 
 		select {
@@ -88,15 +107,5 @@ func syncBackendNeighbors(p backendNeighborSyncParams, ctx context.Context) erro
 		if err := limiter.Wait(ctx); err != nil {
 			return err
 		}
-	}
-}
-
-func backendToNode(b *loadbalancer.Backend) *nodeTypes.Node {
-	return &nodeTypes.Node{
-		Name: fmt.Sprintf("backend-%s", b.Address.AddrCluster.AsNetIP()),
-		IPAddresses: []nodeTypes.Address{{
-			Type: addressing.NodeInternalIP,
-			IP:   b.Address.AddrCluster.AsNetIP(),
-		}},
 	}
 }
