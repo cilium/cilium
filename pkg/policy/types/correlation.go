6f3f84a967d2490b9a777a9f8e4f4738eb139811
Casey Callendrello <cdc@isovalent.com>
2025-06-04 21:46:27 +0200

    hubble: add Log to hubble rule metadata
    In addition to returning the set of labels, we now add the set of log
    lines to the correlation info.
    
    Signed-off-by: Casey Callendrello <cdc@isovalent.com>

--- pkg/policy/types/correlation.go
+++ pkg/policy/types/correlation.go
@@ -9,6 +9,11 @@ import "github.com/cilium/cilium/pkg/labels"
 type PolicyCorrelationInfo struct {
 	// RuleLabels are the rule labels.
 	RuleLabels labels.LabelArrayListString
+
+	// Log is the set of custom Log strings. Policies without a Spec.Log.Value will have
+	// no entry here. Duplicate strings are coalesced.
+	Log []string
+
 	// Revision is the policy revision.
 	Revision uint64
 }
