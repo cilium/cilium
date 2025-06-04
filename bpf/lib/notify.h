397ebe043daae1bf763f28eb2712057f7b6dbb09
Simone Magnani <simone.magnani@isovalent.com>
2025-06-04 15:41:07 +0200

    bpf: move trace_point definition
    Simply moving the definition of available `enum trace_point` into a
    different file to avoid circular dependencies in subsequent commits.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

--- bpf/lib/notify.h
+++ bpf/lib/notify.h
@@ -44,3 +44,20 @@ enum {
 	.len_cap	= (c),		\
 	.version	= (v)
 
+/* Available observation points. */
+enum trace_point {
+	TRACE_TO_LXC,
+	TRACE_TO_PROXY,
+	TRACE_TO_HOST,
+	TRACE_TO_STACK,
+	TRACE_TO_OVERLAY,
+	TRACE_FROM_LXC,
+	TRACE_FROM_PROXY,
+	TRACE_FROM_HOST,
+	TRACE_FROM_STACK,
+	TRACE_FROM_OVERLAY,
+	TRACE_FROM_NETWORK,
+	TRACE_TO_NETWORK,
+	TRACE_FROM_CRYPTO,
+	TRACE_TO_CRYPTO,
+} __packed;
