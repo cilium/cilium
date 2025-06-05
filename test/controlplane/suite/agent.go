a8cc9abe47c8d151a8f711880ac196ff107d3a7c
Jussi Maki <jussi@isovalent.com>
2025-06-05 15:00:55 +0200

    test/controlplane: Remove EnableExperimentalLB override
    The LB tests are no longer part of test/controlplane so no need
    to override and use the old control-plane.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- test/controlplane/suite/agent.go
+++ test/controlplane/suite/agent.go
@@ -80,6 +80,7 @@ func (h *agentHandle) setupCiliumAgentHive(clientset k8sClient.Clientset, extraC
 			func() ctmap.GCRunner { return ctmap.NewFakeGCRunner() },
 			func() policymap.Factory { return nil },
 			func() *server.Server { return nil },
+			func() *loadbalancer.TestConfig { return &loadbalancer.TestConfig{} },
 			k8sSynced.RejectedCRDSyncPromise,
 		),
 		fakeDatapath.Cell,
@@ -102,13 +103,6 @@ func (h *agentHandle) setupCiliumAgentHive(clientset k8sClient.Clientset, extraC
 
 	hive.AddConfigOverride(h.hive, func(c *datapathTables.DirectRoutingDeviceConfig) {
 		c.DirectRoutingDevice = "test0"
-	})
-
-	// Disable the experimental LB control-plane. The tests here use the "LBMockMap" which is not used
-	// by the new implementation. Once we switch implementations we can remove the LB related tests from
-	// here as they're already covered by the LB test suite.
-	hive.AddConfigOverride(h.hive, func(c *loadbalancer.UserConfig) {
-		c.EnableExperimentalLB = false
 	})
 }
 
