bfa4d7c19f9b1a56bc699b50b302288c8b992e0f
Jussi Maki <jussi@isovalent.com>
2025-06-05 15:06:45 +0200

    redirectpolicy: Add LRPMetrics
    Implement the LRPMetrics and move pkg/metrics/features to use it instead
    of the legacy one.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/loadbalancer/redirectpolicy/controller.go
+++ pkg/loadbalancer/redirectpolicy/controller.go
@@ -63,6 +63,7 @@ type lrpControllerParams struct {
 	Writer             *writer.Writer
 	NetNSCookieSupport lbmaps.HaveNetNSCookieSupport
 	Metrics            controllerMetrics
+	LRPMetrics         LRPMetrics `optional:"true"`
 }
 
 type lrpController struct {
@@ -123,6 +124,10 @@ func (c *lrpController) run(ctx context.Context, health cell.Health) error {
 
 		existing := sets.New[k8s.ServiceID]()
 		for lrp := range lrps {
+			if c.p.LRPMetrics != nil && !existing.Has(lrp.ID) {
+				c.p.LRPMetrics.AddLRPConfig(lrp.ID)
+			}
+
 			existing.Insert(lrp.ID)
 			orphans.Delete(lrp.ID)
 
@@ -148,6 +153,9 @@ func (c *lrpController) run(ctx context.Context, health cell.Health) error {
 				cleanup(wtxn)
 			}
 			delete(cleanupFuncs, lrpID)
+			if c.p.LRPMetrics != nil {
+				c.p.LRPMetrics.DelLRPConfig(lrpID)
+			}
 		}
 
 		if initWatches != nil {
