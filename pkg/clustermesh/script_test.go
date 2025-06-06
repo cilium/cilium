c8c9a9f6960f636aa5dc89a5f4f0974cb633ec33
Marco Iorio <marco.iorio@isovalent.com>
2025-06-06 14:28:09 +0200

    kvstore: move script commands out of clustermesh tests
    The kvstore script commands are potentially useful for multiple tests.
    Let's move them into the kvstore package to favor reuse. While being
    there, let's also extend the kvstore/list one to allow selecting the
    output format, given that certain values are json encoded, while others
    are not. Finally, let's ensure that the listed entries are sorted, to
    guarantee consistent ordering in tests.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/clustermesh/script_test.go
+++ pkg/clustermesh/script_test.go
@@ -6,7 +6,6 @@ package clustermesh_test
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log/slog"
 	"maps"
 	"os"
@@ -136,7 +135,7 @@ func TestScript(t *testing.T) {
 			cell.Provide(func(db *statedb.DB) (kvstore.Client, uhive.ScriptCmdsOut) {
 				kvstore.SetupInMemory(db)
 				client := kvstore.SetupDummy(t, "in-memory")
-				return client, uhive.NewScriptCmds(kvstoreCommands{client}.cmds())
+				return client, uhive.NewScriptCmds(kvstore.Commands(client))
 			}),
 
 			cell.DecorateAll(func(client kvstore.Client) common.RemoteClientFactoryFn {
@@ -285,73 +284,3 @@ func (d dummyRemoteIdentityWatcher) WatchRemoteIdentities(remoteName string, rem
 }
 
 var _ clustermesh.RemoteIdentityWatcher = dummyRemoteIdentityWatcher{}
-
-type kvstoreCommands struct {
-	client kvstore.BackendOperations
-}
-
-func (e kvstoreCommands) cmds() map[string]script.Cmd {
-	return map[string]script.Cmd{
-		"kvstore/update": e.update(),
-		"kvstore/delete": e.delete(),
-		"kvstore/list":   e.list(),
-	}
-}
-
-func (e kvstoreCommands) update() script.Cmd {
-	return script.Command(
-		script.CmdUsage{
-			Summary: "update kvstore key-value",
-			Args:    "key value-file",
-		},
-		func(s *script.State, args ...string) (script.WaitFunc, error) {
-			if len(args) != 2 {
-				return nil, fmt.Errorf("%w: expected key and value file", script.ErrUsage)
-			}
-			b, err := os.ReadFile(s.Path(args[1]))
-			if err != nil {
-				return nil, err
-			}
-
-			return nil, e.client.Update(s.Context(), args[0], b, false)
-		},
-	)
-}
-
-func (e kvstoreCommands) delete() script.Cmd {
-	return script.Command(
-		script.CmdUsage{
-			Summary: "delete kvstore key-value",
-			Args:    "key",
-		},
-		func(s *script.State, args ...string) (script.WaitFunc, error) {
-			if len(args) != 1 {
-				return nil, fmt.Errorf("%w: expected key", script.ErrUsage)
-			}
-			return nil, e.client.Delete(s.Context(), args[0])
-		},
-	)
-}
-
-func (e kvstoreCommands) list() script.Cmd {
-	return script.Command(
-		script.CmdUsage{
-			Summary: "list kvstore key-value pairs",
-			Args:    "(prefix)",
-		},
-		func(s *script.State, args ...string) (script.WaitFunc, error) {
-			prefix := ""
-			if len(args) > 0 {
-				prefix = args[0]
-			}
-			kvs, err := e.client.ListPrefix(s.Context(), prefix)
-			if err != nil {
-				return nil, err
-			}
-			for k, v := range kvs {
-				s.Logf("%s: %s\n", k, v.Data)
-			}
-			return nil, nil
-		},
-	)
-}
