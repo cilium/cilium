84db599a429e5caed58be9702464b21c3d4f0ca0
André Martins <andre@cilium.io>
2025-06-06 17:11:42 +0200

    pkg/datapath: migrate all logging.DefaultSlogLogger to a local logger
    Instead of use logging.DefaultSlogLogger, we should always use the
    logger from the caller that has been properly setup after initializing
    the hive framework
    
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/datapath/prefilter/prefilter.go
+++ pkg/datapath/prefilter/prefilter.go
@@ -14,7 +14,6 @@ import (
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/lock"
-	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/maps/cidrmap"
 	"github.com/cilium/cilium/pkg/option"
 )
@@ -222,7 +221,7 @@ func (p *PreFilter) initOneMap(which preFilterMapType) error {
 		path = bpf.MapPath(p.logger, cidrmap.MapName+"v6_fix")
 	}
 
-	p.maps[which], err = cidrmap.OpenMapElems(logging.DefaultSlogLogger, path, prefixlen, prefixdyn, maxelems)
+	p.maps[which], err = cidrmap.OpenMapElems(p.logger, path, prefixlen, prefixdyn, maxelems)
 	if err != nil {
 		return err
 	}
