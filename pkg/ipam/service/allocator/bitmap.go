8a6bd322427088007eae6bc2b06c6a53e350c839
Tobias Klauser <tobias@cilium.io>
2025-06-06 13:36:54 +0200

    ipam/service/allocator: remove always-nil Allocate{,Next} return value
    (*AllocationBitmap).Allocate{,Next} always returns a nil error. Remove
    it and avoid some superfluous error handling in the call sites.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- pkg/ipam/service/allocator/bitmap.go
+++ pkg/ipam/service/allocator/bitmap.go
@@ -72,31 +72,31 @@ func NewContiguousAllocationMap(max int, rangeSpec string) *AllocationBitmap {
 
 // Allocate attempts to reserve the provided item.
 // Returns true if it was allocated, false if it was already in use
-func (r *AllocationBitmap) Allocate(offset int) (bool, error) {
+func (r *AllocationBitmap) Allocate(offset int) bool {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 
 	if r.allocated.Bit(offset) == 1 {
-		return false, nil
+		return false
 	}
 	r.allocated = r.allocated.SetBit(r.allocated, offset, 1)
 	r.count++
-	return true, nil
+	return true
 }
 
 // AllocateNext reserves one of the items from the pool.
 // (0, false, nil) may be returned if there are no items left.
-func (r *AllocationBitmap) AllocateNext() (int, bool, error) {
+func (r *AllocationBitmap) AllocateNext() (int, bool) {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 
 	next, ok := r.strategy.AllocateBit(r.allocated, r.max, r.count)
 	if !ok {
-		return 0, false, nil
+		return 0, false
 	}
 	r.count++
 	r.allocated = r.allocated.SetBit(r.allocated, next, 1)
-	return next, true, nil
+	return next, true
 }
 
 // Release releases the item back to the pool. Releasing an
