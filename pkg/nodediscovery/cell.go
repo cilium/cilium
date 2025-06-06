0a0906cf985dc817fa1af7a42c16864ecb2b0529
Marco Iorio <marco.iorio@isovalent.com>
2025-06-06 09:14:06 +0200

    nodediscovery: directly register the kvstore-nodes fence
    Directly register the kvstore-nodes fence from the nodediscovery cell,
    rather than having it done indirectly by the regenerator one. This also
    allows to drop the reference to the to-be-removed global kvstore option
    from there.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/nodediscovery/cell.go
+++ pkg/nodediscovery/cell.go
@@ -6,7 +6,7 @@ package nodediscovery
 import (
 	"github.com/cilium/hive/cell"
 
-	"github.com/cilium/cilium/pkg/endpoint"
+	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 )
 
 // The node discovery cell provides the local node configuration and node discovery
@@ -18,10 +18,11 @@ var Cell = cell.Module(
 	// Node discovery communicates changes in local node information to the API server or KVStore
 	cell.Provide(NewNodeDiscovery),
 
-	// Provide the function used to wait for completion of node synchronization from
-	// the kvstore. This is provided as a separate type (rather than having the
-	// consumer depend on the whole NodeDiscovery object) to break an import loop.
-	cell.Provide(func(nd *NodeDiscovery) endpoint.KVStoreNodesWaitFn {
-		return nd.WaitForKVStoreSync
+	// Register node discovery to the fence to ensure that we wait for node
+	// synchronization from the kvstore (when enabled) before endpoint regen,
+	// as nodes also contribute entries to the ipcache map, most notably about
+	// the remote node IPs.
+	cell.Invoke(func(nd *NodeDiscovery, fence regeneration.Fence) {
+		fence.Add("kvstore-nodes", nd.WaitForKVStoreSync)
 	}),
 )
