0f9891e5e88be3cb67a58005832e30d46825ade6
Jussi Maki <jussi@isovalent.com>
2025-06-12 16:03:29 +0200

    k8s: Remove ServiceCache and Service
    These are now unused.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/loadbalancer/writer/writer_test.go
+++ pkg/loadbalancer/writer/writer_test.go
@@ -173,7 +173,7 @@ func TestWriter_Service_UpsertDelete(t *testing.T) {
 		wtxn := p.Writer.WriteTxn()
 		assert.Equal(t, 1, p.ServiceTable.NumObjects(wtxn))
 
-		err := p.Writer.DeleteServiceAndFrontends(wtxn, name)
+		_, err := p.Writer.DeleteServiceAndFrontends(wtxn, name)
 		assert.NoError(t, err, "DeleteService failed")
 
 		_, _, found := p.ServiceTable.Get(wtxn, loadbalancer.ServiceByName(name))
