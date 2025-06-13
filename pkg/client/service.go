7a78ffec584bdad18408405ff27f011f552acdb6
Jussi Maki <jussi@isovalent.com>
2025-06-13 09:47:48 +0200

    api,cilium-dbg: Drop "service get" support
    The ID-based lookup of services has been deprecated since v1.14, so finally remove it.
    
    We have now better ways of looking up services, e.g. via "db/get frontends" etc.
    To find by ID one can use "service list" (maybe with -o json) or via "db/show frontends -f json"
    and search with 'jq'.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/client/service.go
+++ pkg/client/service.go
@@ -4,24 +4,12 @@
 package client
 
 import (
-	"github.com/cilium/cilium/api/v1/client/service"
 	"github.com/cilium/cilium/api/v1/models"
-	"github.com/cilium/cilium/pkg/api"
 )
 
 // GetServices returns a list of all services.
 func (c *Client) GetServices() ([]*models.Service, error) {
 	resp, err := c.Service.GetService(nil)
-	if err != nil {
-		return nil, Hint(err)
-	}
-	return resp.Payload, nil
-}
-
-// GetServiceID returns a service by ID.
-func (c *Client) GetServiceID(id int64) (*models.Service, error) {
-	params := service.NewGetServiceIDParams().WithID(id).WithTimeout(api.ClientTimeout)
-	resp, err := c.Service.GetServiceID(params)
 	if err != nil {
 		return nil, Hint(err)
 	}
