7fd0cc0940d8dbcffe8b9bc86f00d891ab1e99ec
Marco Iorio <marco.iorio@isovalent.com>
2025-06-13 17:02:31 +0200

    test/cmapisrv: fix TestUsersManagement race
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- clustermesh-apiserver/clustermesh/users_mgmt_test.go
+++ clustermesh-apiserver/clustermesh/users_mgmt_test.go
@@ -19,6 +19,7 @@ import (
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/kvstore"
+	"github.com/cilium/cilium/pkg/lock"
 )
 
 const (
@@ -27,6 +28,7 @@ const (
 )
 
 type fakeUserMgmtClient struct {
+	mu      lock.RWMutex
 	created map[string]string
 	deleted map[string]int
 }
@@ -37,12 +39,18 @@ func (f *fakeUserMgmtClient) init() {
 }
 
 func (f *fakeUserMgmtClient) UserEnforcePresence(_ context.Context, name string, roles []string) error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
 	// The existing value (if any) is concatenated, to detect if this is called twice for the same name
 	f.created[name] = f.created[name] + strings.Join(roles, "|")
 	return nil
 }
 
 func (f *fakeUserMgmtClient) UserEnforceAbsence(_ context.Context, name string) error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
 	f.deleted[name]++
 	return nil
 }
@@ -95,6 +103,9 @@ func TestUsersManagement(t *testing.T) {
 	}()
 
 	require.Eventuallyf(t, func() bool {
+		client.mu.Lock()
+		defer client.mu.Unlock()
+
 		return len(client.created) == 3 && len(client.deleted) == 0
 	}, time.Second, 10*time.Millisecond,
 		"Failed waiting for events to be triggered: created: %v, deleted: %v",
@@ -115,6 +126,9 @@ func TestUsersManagement(t *testing.T) {
 	require.NoError(t, os.Rename(cfgPath2, cfgPath))
 
 	require.Eventuallyf(t, func() bool {
+		client.mu.Lock()
+		defer client.mu.Unlock()
+
 		return len(client.created) == 2 && len(client.deleted) == 1
 	}, time.Second, 10*time.Millisecond,
 		"Failed waiting for events to be triggered: created: %v, deleted: %v",
