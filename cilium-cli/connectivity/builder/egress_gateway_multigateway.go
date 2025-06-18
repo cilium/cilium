0f9392dbe29910bf91536538d34e6a656b340ea5
André Martins <andre@cilium.io>
2025-06-18 15:45:22 +0200

    cilium-cli: remove sequential tests
    The sequential tests were introduced as they were causing the CI to be
    flaky. Since they have been removed, they are no longer flaky and we can
    run them in parallel again.
    
    Signed-off-by: André Martins <andre@cilium.io>

--- cilium-cli/connectivity/builder/egress_gateway_multigateway.go
+++ cilium-cli/connectivity/builder/egress_gateway_multigateway.go
@@ -14,8 +14,7 @@ import (
 type egressGatewayMultigateway struct{}
 
 func (t egressGatewayMultigateway) build(ct *check.ConnectivityTest, _ map[string]string) {
-	// Prefix the test name with `seq-` to run it sequentially.
-	newTest("seq-egress-gateway-multigateway", ct).
+	newTest("egress-gateway-multigateway", ct).
 		WithCiliumVersion(">=1.18.0").
 		WithCondition(func() bool { return ct.Params().IncludeUnsafeTests }).
 		WithCiliumEgressGatewayPolicy(check.CiliumEgressGatewayPolicyParams{
