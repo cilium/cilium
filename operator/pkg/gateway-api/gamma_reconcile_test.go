da5ad0a0f4e55c4bd865fc1f32e2590dc70b26a6
Tam Mach <tam.mach@cilium.io>
2025-06-19 13:43:42 +1000

    gateway-api: Add file based test for mesh-matching
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/gateway-api/gamma_reconcile_test.go
+++ operator/pkg/gateway-api/gamma_reconcile_test.go
@@ -62,6 +62,7 @@ func Test_gammaReconciler_Reconcile(t *testing.T) {
 	}{
 		{name: "mesh-split", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 		{name: "mesh-frontend", serviceKey: []types.NamespacedName{serviceKeyEchoV2}},
+		{name: "mesh-matching", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 		{name: "mesh-ports", serviceKey: []types.NamespacedName{serviceKeyEchoV1, serviceKeyEchoV2}},
 		{name: "mesh-query-param-matching", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 		{name: "mesh-redirect-host-and-status", serviceKey: []types.NamespacedName{serviceKeyEcho}},
