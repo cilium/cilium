cilium/cilium

commit 2296784f69faa964c9929d73310a13e9e6a5ea42

cilium, socklb: Fix socket termination error handling
In my local testing I ran Cilium as a standalone process rather than
container. As a result of this, /var/run/cilium/netns was not mounted.

From testing socket termination, it turned out that for a given service
the first time a backend which had client connections was deleted was
terminated properly, that is, client sockets got destroyed fine.

However, readding the backend with new client connections, and then
terminating it again turned out /not/ to work. After the grace period
the client socket was still alive:

  c3-small-x86-02-bpf-fix> db/watch backends
  Address                  Instances                        Shadows   NodeName
  147.28.227.99:6443/TCP   default/kubernetes (https)
  10.244.0.128:80/TCP      default/nginx-service                      c3-small-x86-02-bpf-fix
  10.244.0.58:80/TCP       default/nginx-service                      c3-small-x86-02-bpf-fix
  10.244.0.231:9153/TCP    kube-system/kube-dns (metrics)             c3-small-x86-02-bpf-fix
  10.244.0.231:53/UDP      kube-system/kube-dns (dns)                 c3-small-x86-02-bpf-fix
  10.244.0.231:53/TCP      kube-system/kube-dns (dns-tcp)             c3-small-x86-02-bpf-fix
  10.244.0.213:9153/TCP    kube-system/kube-dns (metrics)             c3-small-x86-02-bpf-fix
  10.244.0.213:53/UDP      kube-system/kube-dns (dns)                 c3-small-x86-02-bpf-fix
  10.244.0.213:53/TCP      kube-system/kube-dns (dns-tcp)             c3-small-x86-02-bpf-fix
  10.244.0.99:9090/UDP     default/udp-service                        c3-small-x86-02-bpf-fix
  10.244.0.99:9090/UDP     default/udp-service [terminating]             c3-small-x86-02-bpf-fix
  10.244.0.99:9090/UDP     default/udp-service [terminating]             c3-small-x86-02-bpf-fix (deleted) <-- termination worked
  10.244.0.153:9090/UDP    default/udp-service                           c3-small-x86-02-bpf-fix

pkg/loadbalancer/reconciler/termination.go
@@ -239,12 +239,14 @@ func terminateUDPConnectionsToBackend(p socketTerminationParams, l3n4Addr lb.L3n
 	// Iterate over all pod network namespaces, and terminate any stale connections.
 	if p.ExtConfig.EnableSocketLBPodConnectionTermination && !p.ExtConfig.BPFSocketLBHostnsOnly {
 		iter, errs := p.NetNSOps.all()
-		for name, ns := range iter {
-			destroy(name, ns)
-		}
 		for err := range errs {
 			p.Log.Debug("Error opening netns, skipping",
 				logfields.Error, err)
+		}
+		if iter != nil {
+			for name, ns := range iter {
+				destroy(name, ns)
+			}
 		}
 	}
 

pkg/netns/netns_linux.go
@@ -256,9 +256,8 @@ func getFromThread(pid, tid int) (*os.File, error) {
 // The *NetNS handle is provided for to allow for cases where the ns must
 // be used explicitly*inside* the loop.
 func All() (iter.Seq2[string, *NetNS], <-chan error) {
-	errCh := make(chan error)
+	errCh := make(chan error, 1)
 	files, err := os.ReadDir(defaults.NetNsPath)
-
 	if err != nil {
 		errCh <- err
 		close(errCh)
@@ -291,5 +290,4 @@ func All() (iter.Seq2[string, *NetNS], <-chan error) {
 			}
 		}
 	}, errCh
-
 }

commit c73b7fbf805dfaf5cd54eb6f8048bfd9765cf71a

loadbalancer: Fix socket termination of non-host namespaces
2296784f69fa iterated over the error channel before the namespaces
which deadlocks. Switch the order back and change the test case
to match how netns.All() iterator works.

Fixes: 2296784f69fa ("cilium, socklb: Fix socket termination error handling")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/loadbalancer/reconciler/termination.go
@@ -239,14 +239,14 @@ func terminateUDPConnectionsToBackend(p socketTerminationParams, l3n4Addr lb.L3n
 	// Iterate over all pod network namespaces, and terminate any stale connections.
 	if p.ExtConfig.EnableSocketLBPodConnectionTermination && !p.ExtConfig.BPFSocketLBHostnsOnly {
 		iter, errs := p.NetNSOps.all()
-		for err := range errs {
-			p.Log.Debug("Error opening netns, skipping",
-				logfields.Error, err)
-		}
 		if iter != nil {
 			for name, ns := range iter {
 				destroy(name, ns)
 			}
+		}
+		for err := range errs {
+			p.Log.Debug("Error opening netns, skipping",
+				logfields.Error, err)
 		}
 	}
 

pkg/loadbalancer/reconciler/termination_test.go
@@ -292,6 +292,12 @@ func TestSocketTermination_Datapath(t *testing.T) {
 		goleak.VerifyNone(t)
 	})
 
+	namespaces := map[string]*netns.NetNS{
+		"cni-0000": ns1,
+		"cni-0001": ns2,
+		"cni-0002": ns3,
+	}
+
 	// Set up the parameters that [terminateUDPConnectionsToBackend] needs.
 	params := socketTerminationParams{
 		JobGroup:        nil,
@@ -307,12 +313,14 @@ func TestSocketTermination_Datapath(t *testing.T) {
 			do:      (*netns.NetNS).Do,
 			all: func() (iter.Seq2[string, *netns.NetNS], <-chan error) {
 				errs := make(chan error)
-				close(errs)
-				return maps.All(map[string]*netns.NetNS{
-					"cni-0000": ns1,
-					"cni-0001": ns2,
-					"cni-0002": ns3,
-				}), errs
+				return func(yield func(string, *netns.NetNS) bool) {
+					defer close(errs)
+					for n, ns := range namespaces {
+						if !yield(n, ns) {
+							return
+						}
+					}
+				}, errs
 			},
 		},
 	}