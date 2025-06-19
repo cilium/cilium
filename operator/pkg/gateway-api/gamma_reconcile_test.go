95468d3fcd58a746b3d841477ec2a6602cb87996
Tam Mach <tam.mach@cilium.io>
2025-06-19 13:31:33 +1000

    gateway-api: Add file based test for mesh-request-header-modifier
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/gateway-api/gamma_reconcile_test.go
+++ operator/pkg/gateway-api/gamma_reconcile_test.go
@@ -67,6 +67,7 @@ func Test_gammaReconciler_Reconcile(t *testing.T) {
 		{name: "mesh-redirect-path", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 		{name: "mesh-redirect-port", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 		{name: "mesh-redirect-scheme", serviceKey: []types.NamespacedName{serviceKeyEcho}},
+		{name: "mesh-request-header-modifier", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 	}
 
 	for _, tt := range tests {
