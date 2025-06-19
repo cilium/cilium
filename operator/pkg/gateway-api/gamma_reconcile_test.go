7315e0be195f5535bc648ddd35b9ace0a63ee860
Tam Mach <tam.mach@cilium.io>
2025-06-19 13:33:05 +1000

    gateway-api: Add file based test for mesh-rewrite-path
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/gateway-api/gamma_reconcile_test.go
+++ operator/pkg/gateway-api/gamma_reconcile_test.go
@@ -68,6 +68,7 @@ func Test_gammaReconciler_Reconcile(t *testing.T) {
 		{name: "mesh-redirect-port", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 		{name: "mesh-redirect-scheme", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 		{name: "mesh-request-header-modifier", serviceKey: []types.NamespacedName{serviceKeyEcho}},
+		{name: "mesh-rewrite-path", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 	}
 
 	for _, tt := range tests {
