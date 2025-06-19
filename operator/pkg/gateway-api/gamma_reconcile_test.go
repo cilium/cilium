e50147e3737150061f286d1c7560247a02bb3b38
Tam Mach <tam.mach@cilium.io>
2025-06-19 13:24:21 +1000

    gateway-api: Add file based test for mesh-redirect-host-and-status
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- operator/pkg/gateway-api/gamma_reconcile_test.go
+++ operator/pkg/gateway-api/gamma_reconcile_test.go
@@ -63,6 +63,7 @@ func Test_gammaReconciler_Reconcile(t *testing.T) {
 		{name: "mesh-split", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 		{name: "mesh-frontend", serviceKey: []types.NamespacedName{serviceKeyEchoV2}},
 		{name: "mesh-ports", serviceKey: []types.NamespacedName{serviceKeyEchoV1, serviceKeyEchoV2}},
+		{name: "mesh-redirect-host-and-status", serviceKey: []types.NamespacedName{serviceKeyEcho}},
 	}
 
 	for _, tt := range tests {
