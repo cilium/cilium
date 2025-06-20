cilium/cilium

commit 0a5d2f779f37f8b2c3939201e665c5c48a4b5ebb

policy: wrong cidrGroupRefs select nothing not all
When current code translates a CNP with _only_ a dangling cidrGroupRef,
it fails distinguish between nil (meaning an unset selector, selecting
everything) and the empty list (selecting nothing). This leads to
bizarre situations, such as the following. With "dangling" pointing to a
non-existant cidrGroup, the CNP below allowed egress traffic to port 80
instead of denying it.

  egress:
  - toCIDRSet: [{cidrGroupRef: dangling}]
    toPorts: ( ... 80 ... )

To fix this, transform the nil to an empty list in an overly explicit
manner to make clear why this is necessary. It's somewhat subtle as in
Go, most of the time, nil and empty list are equivalent, hence be
explicit to avoid an easy refactor breaking this again. In addition, of
course, adapt the tests so that they would catch it too.

Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

pkg/policy/k8s/cilium_cidr_group.go
@@ -222,16 +222,34 @@ func translateCIDRGroupRefs(cnp *types.SlimCNP, cidrsSets map[string][]api.CIDR)
 
 func translateSpec(spec *api.Rule, cidrsSets map[string][]api.CIDR) {
 	for i := range spec.Ingress {
-		spec.Ingress[i].FromCIDRSet = translateCIDRRuleSlice(spec.Ingress[i].FromCIDRSet, cidrsSets)
+		cidrSet := translateCIDRRuleSlice(spec.Ingress[i].FromCIDRSet, cidrsSets)
+		// Careful to distinguish between nil (unset, selecting everything) and
+		// empty list (selecting nothing), hence this overly explicit code.
+		if cidrSet == nil {
+			cidrSet = make([]api.CIDRRule, 0)
+		}
+		spec.Ingress[i].FromCIDRSet = cidrSet
 	}
 	for i := range spec.IngressDeny {
-		spec.IngressDeny[i].FromCIDRSet = translateCIDRRuleSlice(spec.IngressDeny[i].FromCIDRSet, cidrsSets)
+		cidrSet := translateCIDRRuleSlice(spec.IngressDeny[i].FromCIDRSet, cidrsSets)
+		if cidrSet == nil {
+			cidrSet = make([]api.CIDRRule, 0)
+		}
+		spec.IngressDeny[i].FromCIDRSet = cidrSet
 	}
 	for i := range spec.Egress {
-		spec.Egress[i].ToCIDRSet = translateCIDRRuleSlice(spec.Egress[i].ToCIDRSet, cidrsSets)
+		cidrSet := translateCIDRRuleSlice(spec.Egress[i].ToCIDRSet, cidrsSets)
+		if cidrSet == nil {
+			cidrSet = make([]api.CIDRRule, 0)
+		}
+		spec.Egress[i].ToCIDRSet = cidrSet
 	}
 	for i := range spec.EgressDeny {
-		spec.EgressDeny[i].ToCIDRSet = translateCIDRRuleSlice(spec.EgressDeny[i].ToCIDRSet, cidrsSets)
+		cidrSet := translateCIDRRuleSlice(spec.EgressDeny[i].ToCIDRSet, cidrsSets)
+		if cidrSet == nil {
+			cidrSet = make([]api.CIDRRule, 0)
+		}
+		spec.EgressDeny[i].ToCIDRSet = cidrSet
 	}
 }
 

pkg/policy/k8s/cilium_cidr_group_test.go
@@ -1538,7 +1538,7 @@ func TestCIDRGroupRefsTranslate(t *testing.T) {
 								},
 								{
 									IngressCommonRule: api.IngressCommonRule{
-										FromCIDRSet: nil,
+										FromCIDRSet: []api.CIDRRule{}, // Empty list, not nil!
 									},
 								},
 							},
@@ -1560,7 +1560,7 @@ func TestCIDRGroupRefsTranslate(t *testing.T) {
 								},
 								{
 									EgressCommonRule: api.EgressCommonRule{
-										ToCIDRSet: nil,
+										ToCIDRSet: []api.CIDRRule{}, // Empty list, not nil!
 									},
 								},
 							},
@@ -1671,7 +1671,7 @@ func TestCIDRGroupRefsTranslate(t *testing.T) {
 								},
 								{
 									IngressCommonRule: api.IngressCommonRule{
-										FromCIDRSet: nil,
+										FromCIDRSet: []api.CIDRRule{}, // Empty list, not nil!
 									},
 								},
 							},
@@ -1782,7 +1782,7 @@ func TestCIDRGroupRefsTranslate(t *testing.T) {
 								},
 								{
 									EgressCommonRule: api.EgressCommonRule{
-										ToCIDRSet: nil,
+										ToCIDRSet: []api.CIDRRule{}, // Empty list, not nil!
 									},
 								},
 							},

commit 65d8b9da3171f48bfac4500d7f34ede3001656f8

policy: fix CIDRGroup ref handling in cilium NetworkPolicy rule spec
This commit fixes an issue introduced as part of another bugfix related
to CIDRGroupRef in cilium policy rule spec (mentioned below).
In the current CIDR group reference handling, if *any* rule in the network
policy spec contains a CIDR group reference, we explicitly set
`FromCIDRSet` as an empty slice even if that specific rule doesn't
contain the reference.
https://github.com/cilium/cilium/blob/v1.16.11/pkg/policy/k8s/cilium_cidr_group.go#L228C3-L230C4

This causes a conflict in policy resolution if there is another mutually
exclusive selector present in that rule(`fromEntities`, `fromCIDR`),
resulting in the rule not processing the intended selectors.
https://github.com/cilium/cilium/blob/v1.16/pkg/policy/api/ingress.go#L223

For Example the first rule in the below spec skips processing the
`fromEntities` selector thinking there is another empty selector
`fromCIDRSet`.

```
spec:
  ingress:
  - fromEntities:
    - cluster
  - fromCIDRSet:
    - cidrGroupRef: cidr-group
```

This fix processes CIDR Group reference in the spec rule only if that
rule specifically has a reference.

This issue only effects v1.16. For v1.17 and above the handling of
CiliumCIDRGroup has been refactored and decoupled from CNP processing.
Now we don't transform the CNP object by resolving references against
CIDRGroup object. Instead an identity is created for this object
and CNP directly creates the selector from CIDR Group reference that
selects it.
https://github.com/cilium/cilium/blob/v1.17/pkg/policy/api/cidr.go#L201-L206

Fixes #36393
Fixes: 0a5d2f7 ("policy: wrong cidrGroupRefs select nothing not all")

Signed-off-by: fristonio <deepeshpathak09@gmail.com>

pkg/policy/k8s/cilium_cidr_group.go
@@ -222,8 +222,16 @@ func translateCIDRGroupRefs(cnp *types.SlimCNP, cidrsSets map[string][]api.CIDR)
 
 func translateSpec(spec *api.Rule, cidrsSets map[string][]api.CIDR) {
 	for i := range spec.Ingress {
+		// CIDRSet is mutually exclusive with other types of selectors like `Entities`.
+		// See comment for policy/api.{Ingress,Egress}Rule
+		// Don't process the selector if {To,From}CIDRSet is not set in the rule, so
+		// as to avoid any unintended mutation on rule selectors.
+		if spec.Ingress[i].FromCIDRSet == nil {
+			continue
+		}
+
 		cidrSet := translateCIDRRuleSlice(spec.Ingress[i].FromCIDRSet, cidrsSets)
-		// Careful to distinguish between nil (unset, selecting everything) and
+		// Careful to distinguish between nil (unset) and
 		// empty list (selecting nothing), hence this overly explicit code.
 		if cidrSet == nil {
 			cidrSet = make([]api.CIDRRule, 0)
@@ -231,6 +239,10 @@ func translateSpec(spec *api.Rule, cidrsSets map[string][]api.CIDR) {
 		spec.Ingress[i].FromCIDRSet = cidrSet
 	}
 	for i := range spec.IngressDeny {
+		if spec.IngressDeny[i].FromCIDRSet == nil {
+			continue
+		}
+
 		cidrSet := translateCIDRRuleSlice(spec.IngressDeny[i].FromCIDRSet, cidrsSets)
 		if cidrSet == nil {
 			cidrSet = make([]api.CIDRRule, 0)
@@ -238,6 +250,10 @@ func translateSpec(spec *api.Rule, cidrsSets map[string][]api.CIDR) {
 		spec.IngressDeny[i].FromCIDRSet = cidrSet
 	}
 	for i := range spec.Egress {
+		if spec.Egress[i].ToCIDRSet == nil {
+			continue
+		}
+
 		cidrSet := translateCIDRRuleSlice(spec.Egress[i].ToCIDRSet, cidrsSets)
 		if cidrSet == nil {
 			cidrSet = make([]api.CIDRRule, 0)
@@ -245,6 +261,10 @@ func translateSpec(spec *api.Rule, cidrsSets map[string][]api.CIDR) {
 		spec.Egress[i].ToCIDRSet = cidrSet
 	}
 	for i := range spec.EgressDeny {
+		if spec.EgressDeny[i].ToCIDRSet == nil {
+			continue
+		}
+
 		cidrSet := translateCIDRRuleSlice(spec.EgressDeny[i].ToCIDRSet, cidrsSets)
 		if cidrSet == nil {
 			cidrSet = make([]api.CIDRRule, 0)

pkg/policy/k8s/cilium_cidr_group_test.go
@@ -13,6 +13,7 @@ import (
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	cilium_v2_alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/k8s/types"
+	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/policy/api"
 )
 
@@ -1174,6 +1175,22 @@ func TestCIDRGroupRefsToCIDRsSets(t *testing.T) {
 }
 
 func TestCIDRGroupRefsTranslate(t *testing.T) {
+	testEntitySelector := api.IngressRule{
+		IngressCommonRule: api.IngressCommonRule{
+			FromEntities: api.EntitySlice{
+				api.EntityHost,
+				api.EntityHealth,
+			},
+		},
+	}
+	testEndpointSelector := api.EgressRule{
+		EgressCommonRule: api.EgressCommonRule{
+			ToEndpoints: api.EndpointSelectorSlice{
+				api.NewESFromLabels(labels.NewLabel("app", "test", labels.LabelSourceK8s)),
+			},
+		},
+	}
+
 	testCases := [...]struct {
 		name      string
 		cnp       *types.SlimCNP
@@ -2314,6 +2331,93 @@ func TestCIDRGroupRefsTranslate(t *testing.T) {
 							},
 						},
 						Egress: []api.EgressRule{
+							{
+								EgressCommonRule: api.EgressCommonRule{
+									ToCIDRSet: api.CIDRRuleSlice{
+										{
+											Cidr:        "110.0.0.0/8",
+											ExceptCIDRs: []api.CIDR{"110.96.0.0/12", "110.112.0.0/12"},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "with Ingress/Egress rules and multiple selectors",
+			cnp: &types.SlimCNP{
+				CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "cilium.io/v2",
+						Kind:       "CiliumNetworkPolicy",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-policy",
+						Namespace: "test-namespace",
+					},
+					Spec: &api.Rule{
+						Ingress: []api.IngressRule{
+							testEntitySelector,
+							{
+								IngressCommonRule: api.IngressCommonRule{
+									FromCIDRSet: api.CIDRRuleSlice{
+										{
+											CIDRGroupRef: "cidr-group-1",
+											ExceptCIDRs:  []api.CIDR{"10.96.0.0/12", "10.112.0.0/12"},
+										},
+									},
+								},
+							},
+						},
+						Egress: []api.EgressRule{
+							testEndpointSelector,
+							{
+								EgressCommonRule: api.EgressCommonRule{
+									ToCIDRSet: api.CIDRRuleSlice{
+										{
+											CIDRGroupRef: "cidr-group-1-e",
+											ExceptCIDRs:  []api.CIDR{"110.96.0.0/12", "110.112.0.0/12"},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			cidrsSets: map[string][]api.CIDR{
+				"cidr-group-1":   {"10.0.0.0/8"},
+				"cidr-group-1-e": {"110.0.0.0/8"},
+			},
+			expected: &types.SlimCNP{
+				CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "cilium.io/v2",
+						Kind:       "CiliumNetworkPolicy",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-policy",
+						Namespace: "test-namespace",
+					},
+					Spec: &api.Rule{
+						Ingress: []api.IngressRule{
+							testEntitySelector,
+							{
+								IngressCommonRule: api.IngressCommonRule{
+									FromCIDRSet: api.CIDRRuleSlice{
+										{
+											Cidr:        "10.0.0.0/8",
+											ExceptCIDRs: []api.CIDR{"10.96.0.0/12", "10.112.0.0/12"},
+										},
+									},
+								},
+							},
+						},
+						Egress: []api.EgressRule{
+							testEndpointSelector,
 							{
 								EgressCommonRule: api.EgressCommonRule{
 									ToCIDRSet: api.CIDRRuleSlice{