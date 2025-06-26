cilium/cilium

commit a951c39c8ea4ce1d382c52df28bf552ccc5e9e55

make: build and install cilium-cni binary in fast targets
Build and install the CNI plugin cilium-cni binary in the
kind-image-fast-agent target. This maks CNI changes faster to iterate
on during development.

Signed-off-by: Tobias Klauser <tobias@cilium.io>

Makefile.kind
@@ -255,6 +255,10 @@ kind-install-cilium-fast: check_deps kind-ready ## "Fast" Install a local Cilium
 build-cli: ## Build cilium cli binary
 	$(QUIET)$(MAKE) -C $(CILIUM_BUILD_DIR)/cilium-dbg GOOS=linux
 
+.PHONY: build-cni
+build-cni: ## Build cilium-cni binary
+	$(QUIET)$(MAKE) -C $(CILIUM_BUILD_DIR)/plugins/cilium-cni GOOS=linux
+
 .PHONY: build-agent
 build-agent: ## Build cilium daemon binary
 	$(QUIET)$(MAKE) -C $(CILIUM_BUILD_DIR)/daemon GOOS=linux
@@ -276,7 +280,7 @@ build-bugtool: ## Build bugtool binary
 	$(QUIET)$(MAKE) -C $(CILIUM_BUILD_DIR)/bugtool GOOS=linux
 
 .PHONY: kind-image-fast-agent
-kind-image-fast-agent: kind-ready build-cli build-agent build-hubble-cli build-bugtool ## Build cilium cli, daemon binaries, and hubble cli. Copy the bins and bpf files to kind nodes.
+kind-image-fast-agent: kind-ready build-cli build-cni build-agent build-hubble-cli build-bugtool ## Build agent and node CLI binaries. Copy them and bpf files to kind nodes.
 	$(eval dst:=/cilium-binaries)
 	for cluster_name in $${KIND_CLUSTERS:-$(shell kind get clusters)}; do \
 		for node_name in $$(kind get nodes -n "$$cluster_name"); do \
@@ -290,6 +294,10 @@ kind-image-fast-agent: kind-ready build-cli build-agent build-hubble-cli build-b
 			docker exec $${node_name} rm -f "${dst}/cilium-dbg"; \
 			docker cp "$(CILIUM_BUILD_DIR)/cilium-dbg/cilium-dbg" $${node_name}:"${dst}"; \
 			docker exec $${node_name} chmod +x "${dst}/cilium-dbg"; \
+			\
+			docker exec $${node_name} rm -f "${dst}/cilium-cni"; \
+			docker cp "$(CILIUM_BUILD_DIR)/plugins/cilium-cni/cilium-cni" $${node_name}:"${dst}"; \
+			docker exec $${node_name} chmod +x "${dst}/cilium-cni"; \
 			\
 			docker exec $${node_name} rm -f "${dst}/cilium-agent"; \
 			docker cp "$(CILIUM_BUILD_DIR)/daemon/cilium-agent" $${node_name}:"${dst}"; \

commit f91e32a45a8bdba17d512411957a4df336fa4a6e

make: fix cilium-cni binary installation in fast targets
The blamed commit introduced the support for building and installing the
cilium-cni binary in the kind-image-fast-agent target. However, while the
binary is copied into the kind nodes, it is not installed in the expected
path, and does not get actually invoked.

Let's get this fixed by directly copying the binary into the expected
directory, and configuring a dummy cni.binPath setting so that it does
not get overwritten by the Cilium's init container with the previous one.
We don't completely disable that init container because the same setting
(`cni.install`) also controls the creation of the CNI configuration, which
we still want to be automatically performed.

Fixes: a951c39c8ea4 ("make: build and install cilium-cni binary in fast targets")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

Makefile.kind
@@ -295,9 +295,9 @@ kind-image-fast-agent: kind-ready build-cli build-cni build-agent build-hubble-c
 			docker cp "$(CILIUM_BUILD_DIR)/cilium-dbg/cilium-dbg" $${node_name}:"${dst}"; \
 			docker exec $${node_name} chmod +x "${dst}/cilium-dbg"; \
 			\
-			docker exec $${node_name} rm -f "${dst}/cilium-cni"; \
-			docker cp "$(CILIUM_BUILD_DIR)/plugins/cilium-cni/cilium-cni" $${node_name}:"${dst}"; \
-			docker exec $${node_name} chmod +x "${dst}/cilium-cni"; \
+			docker exec $${node_name} rm -f "/opt/cni/bin/cilium-cni"; \
+			docker cp "$(CILIUM_BUILD_DIR)/plugins/cilium-cni/cilium-cni" $${node_name}:"/opt/cni/bin/"; \
+			docker exec $${node_name} chmod +x "/opt/cni/bin/cilium-cni"; \
 			\
 			docker exec $${node_name} rm -f "${dst}/cilium-agent"; \
 			docker cp "$(CILIUM_BUILD_DIR)/daemon/cilium-agent" $${node_name}:"${dst}"; \

contrib/testing/kind-fast.yaml
@@ -66,3 +66,9 @@ clustermesh:
       pullPolicy: IfNotPresent
 image:
   pullPolicy: IfNotPresent
+
+cni:
+  # Set a dummy path, as we don't want the init-container to override the
+  # binary we directly copied to the node. But we still want the configuration
+  # to be created, which is also controlled by `cni.install`.
+  binPath: /opt/dummy/