34c91fe0cc41d318428d9e4791d02226503be148
Marc Suñé <marc.sune@isovalent.com>
2025-07-01 00:04:36 +0200

    bpf/tests: add scapy support
    This commit adds some rudimentary scapy support in BPF unit tests
    to simplify the process of writing and debugging unit tests.
    
    Note: this commit does NOT replace pktgen.h, and tests written with
    pktgen.h and scapy can coexist. In fact, scapy-based tests _still_ rely
    on some pktgen.h infra.
    
    The mechanics as of today are a bit hacky, and can be broken down in
    two distinct stages:
    
    * Compilation: before clang is invoked, `scapy/parser.py` goes
      through the C BPF unit test code looking for `BUF_DECL(NAME, ...)`
      invocations. As of today, `BUF_DECL()` only accepts a single
      argument which is the scapy packet definition, e.g.:
    
    ```
      BUF_DECL(ARP_REQ, arp_req)
    ```
    
    And in `bpf/tests/scapy/pkt_defs.py`:
    
    ```
    arp_req = Ether(dst=mac_bcast, src=mac_one)/ARP(op="who-has", psrc=v4_ext_one, pdst=v4_svc_one, hwsrc=mac_one, hwdst=mac_bcast));
    ```
      (but is kept as __VA_ARGS__ as it could accept more parameters to
      be passed to the parser, see TODO2)
    
      `scapy/parser.py` then creates a scapy packet, and generates
      a header file `scapy/output/gen_pkts.h` with the pkt byte array. This autogen
      header is then included via `scapy.h`. As of today, the buffer is allocated
      in stack. One can reference the scapy buffer - if reachable, in scope - using
      `SCAPY_BUF(NAME)`.
    
      At the same time, `scapy.h` defines a MACRO to compare a pkt (ctx)
      to a scapy buffer, to simplify writing pkt unit test assertions:
      `ASSERT_CTX_BUF_OFF(...)`.
    
      `scapy/pkt_defs.py` contains the packet definitions to be used in the tests,
      along with global constants (e.g. `mac_one`) reused in packet definitions.
      Packet definitions should be prepended with the test name or alike.
      Symbols in there are imported (via eval!) by `parser.py`, so they are
      available when `BUF_DECL()` is parsed, hugely simplifying buffer definitions.
    
    * Output post-processing: BPF assertions are essentially the result of
      `memcmp()`, so while writing whole pkt checks is easier, debugging
      errors is difficult. To solve this, when `ASSERT_CTX_BUF_OFF()` fails,
      it uses `hexdump()` to dump both the expected and the actual packet.
      `scapy/trace_diff_pkts.py` in combination with the MAkefile then reads
      the packets from the `trace_pipe` and outputs the 'expected' and the 'got'
      using scapy's built-in dumper (`show()`).
    
    TODO2: support no csum computation (don't invoke show2())
    TODO3: _maybe_ scope buf names to the unit test itself (right now they are global in pkt_defs.py).
    TODO4: don't use `trace_pipe` and post-processing to enrich assertions.
           It might be easier to use a map to store failed assertions, and
           post-process from there.
    
    Signed-off-by: Marc Suñé <marc.sune@isovalent.com>

---
+++ bpf/tests/scapy/trace_diff_pkts.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import sys
+
+from scapy.all import *
+
+def parse_asserts(filename: str) -> List[Dict]:
+    """
+    Parses asserts in the log file.
+
+    Expected assert log format:
+       '... bpf_trace_printk: (file):(linenum) assert '(assert_name)' FAILED! (Got|Expected) ... : pkt_hex \((first_layer)\)[(bytes)]'
+    Example:
+       'bpftest.test-1515316 [001] b..11 102260.946507: bpf_trace_printk: tc_l2_announcement.c:164 assert 'arp_rep_only_ok' FAILED! Got (ctx): pkt_hex ARP[0001080006040002133713371337ac100a01deadbeefdeef6e000b01]'
+
+        file: tc_l2_announcement.c
+        linenum: 164
+        assert_name: arp_rep_only_ok
+        Got|Expected: Got
+        first_layer: Ether
+        bytes: 0001080006040002133713371337ac100a01deadbeefdeef6e000b01
+    """
+    pattern = re.compile(r"\s*.*bpf_trace_printk: (\S+):(\d+)\s+assert\s*'([^']+)'\s+FAILED!\s+(Got|Expected)[^:]*:\s*pkt_hex\s*(\w+)\[(.*?)\]")
+
+    asserts = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            match = pattern.match(line)
+            if not match:
+                continue
+
+            name = match.group(3)
+            kind = match.group(4)
+
+            if name not in asserts:
+                asserts[name] = {}
+            asserts[name]["file"] = match.group(1)
+            asserts[name]["linenum"] = match.group(2)
+
+            s = f"{match.group(5)}({bytes.fromhex(match.group(6))})"
+            asserts[name][kind.lower().strip()] = eval(s)
+    return asserts
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse failed assertions from a trace_pipe log file.")
+    parser.add_argument('filename', help="Log file to parse")
+
+    args = parser.parse_args()
+    asserts = parse_asserts(args.filename)
+
+    for name, data in asserts.items():
+        print(f"=== Start {data['file']}:{data['linenum']} '{name}' ===")
+        print(f"--- Expected ---")
+        asserts[name]["expected"].show()
+        print(f"--- Got ---")
+        asserts[name]["got"].show()
+        print(f"===  End  {data['file']}:{data['linenum']} '{name}' ===")
+
+        #TODO add a more specific diff (patch-style diff?)
+
+if __name__ == "__main__":
+    main()
