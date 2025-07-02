cilium/cilium

commit 7cc1d5395466a4e6bb70397341929245ab7569ed

mtu: Support tunneling over IPv6 in calculations
Our MTU calculations in the agent currently assume that tunneling is
always over IPv4. That won't always be the case anymore, so the
calculations need to be updated.

Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

pkg/mtu/cell.go
@@ -91,11 +91,13 @@ func newForCell(lc cell.Lifecycle, p mtuParams, cc Config) (MTU, error) {
 	lc.Append(group)
 	lc.Append(cell.Hook{
 		OnStart: func(ctx cell.HookContext) error {
+			tunnelOverIPv6 := !option.Config.EnableIPv4 && option.Config.RoutingMode == option.RoutingModeTunnel
 			*c = NewConfiguration(
 				p.IPsec.AuthKeySize(),
 				option.Config.EnableIPSec,
 				p.TunnelConfig.ShouldAdaptMTU(),
 				option.Config.EnableWireguard,
+				tunnelOverIPv6,
 			)
 
 			configuredMTU := option.Config.MTU

daemon/cmd/datapath.go
@@ -350,7 +350,7 @@ func setupRouteToVtepCidr() error {
 	}
 
 	addedVtepRoutes, removedVtepRoutes := cidr.DiffCIDRLists(routeCidrs, option.Config.VtepCIDRs)
-	vtepMTU := mtu.EthernetMTU - mtu.TunnelOverhead
+	vtepMTU := mtu.EthernetMTU - mtu.TunnelOverheadIPv4
 
 	if option.Config.EnableL7Proxy {
 		for _, prefix := range addedVtepRoutes {

pkg/datapath/linux/node_linux_test.go
@@ -101,7 +101,7 @@ func setupLinuxPrivilegedBaseTestSuite(tb testing.TB, addressing datapath.NodeAd
 	s.sysctl = sysctl.NewDirectSysctl(afero.NewOsFs(), "/proc")
 
 	rlimit.RemoveMemlock()
-	mtuConfig := mtu.NewConfiguration(0, false, false, false)
+	mtuConfig := mtu.NewConfiguration(0, false, false, false, false)
 	s.mtuCalc = mtuConfig.Calculate(1500)
 	s.enableIPv6 = enableIPv6
 	s.enableIPv4 = enableIPv4

pkg/datapath/linux/node_test.go
@@ -34,7 +34,7 @@ var (
 		RouteMTU:            calcMtu.RouteMTU,
 		RoutePostEncryptMTU: calcMtu.RoutePostEncryptMTU,
 	}
-	mtuConfig = mtu.NewConfiguration(0, false, false, false)
+	mtuConfig = mtu.NewConfiguration(0, false, false, false, false)
 	calcMtu   = mtuConfig.Calculate(100)
 	nh        = linuxNodeHandler{
 		nodeConfig: nodeConfig,

pkg/mtu/mtu.go
@@ -17,16 +17,18 @@ const (
 	// as the MTU for container devices when running direct routing mode.
 	EthernetMTU = 1500
 
-	// TunnelOverhead is an approximation for bytes used for tunnel
+	// TunnelOverheadIPv{4,6} is an approximation for bytes used for tunnel
 	// encapsulation. It accounts for:
+	//                       IPv4  IPv6
 	//    (Outer ethernet is not accounted against MTU size)
-	//    Outer IPv4 header:  20B
-	//    Outer UDP header:    8B
-	//    Outer VXLAN header:  8B
-	//    Original Ethernet:  14B
-	//                        ---
-	//    Total extra bytes:  50B
-	TunnelOverhead = 50
+	//    Outer IP header:    20B  40B
+	//    Outer UDP header:    8B   8B
+	//    Outer VXLAN header:  8B   8B
+	//    Original Ethernet:  14B  14B
+	//                        ---  ---
+	//    Total extra bytes:  50B  70B
+	TunnelOverheadIPv4 = 50
+	TunnelOverheadIPv6 = 70
 
 	// DsrTunnelOverhead is about the GENEVE DSR option that gets inserted
 	// by the LB, when addressing a Service in hs-ipcache mode
@@ -81,16 +83,22 @@ type Configuration struct {
 	encapEnabled     bool
 	encryptEnabled   bool
 	wireguardEnabled bool
+	tunnelOverhead   int
 }
 
 // NewConfiguration returns a new MTU configuration which is used to calculate
 // MTU values from a base MTU based on the config.
-func NewConfiguration(authKeySize int, encryptEnabled bool, encapEnabled bool, wireguardEnabled bool) Configuration {
+func NewConfiguration(authKeySize int, encryptEnabled, encapEnabled, wireguardEnabled, tunnelOverIPv6 bool) Configuration {
+	tunnelOverhead := TunnelOverheadIPv4
+	if tunnelOverIPv6 {
+		tunnelOverhead = TunnelOverheadIPv6
+	}
 	return Configuration{
 		authKeySize:      authKeySize,
 		encapEnabled:     encapEnabled,
 		encryptEnabled:   encryptEnabled,
 		wireguardEnabled: wireguardEnabled,
+		tunnelOverhead:   tunnelOverhead,
 	}
 }
 
@@ -107,9 +115,9 @@ func (c Configuration) Calculate(baseMTU int) RouteMTU {
 // case accounts for the tunnel overhead.
 func (c *Configuration) getRoutePostEncryptMTU(baseMTU int) int {
 	if c.encapEnabled {
-		postEncryptMTU := baseMTU - TunnelOverhead
+		postEncryptMTU := baseMTU - c.tunnelOverhead
 		if postEncryptMTU == 0 {
-			return EthernetMTU - TunnelOverhead
+			return EthernetMTU - c.tunnelOverhead
 		}
 		return postEncryptMTU
 
@@ -126,7 +134,7 @@ func (c *Configuration) getRoutePostEncryptMTU(baseMTU int) int {
 func (c *Configuration) getRouteMTU(baseMTU int) int {
 	if c.wireguardEnabled {
 		if c.encapEnabled {
-			return c.getDeviceMTU(baseMTU) - (WireguardOverhead + TunnelOverhead)
+			return c.getDeviceMTU(baseMTU) - (WireguardOverhead + c.tunnelOverhead)
 		}
 		return c.getDeviceMTU(baseMTU) - WireguardOverhead
 	}
@@ -151,12 +159,12 @@ func (c *Configuration) getRouteMTU(baseMTU int) int {
 		return preEncryptMTU
 	}
 
-	tunnelMTU := baseMTU - (TunnelOverhead + encryptOverhead)
+	tunnelMTU := baseMTU - (c.tunnelOverhead + encryptOverhead)
 	if tunnelMTU <= 0 {
 		if c.encryptEnabled {
-			return EthernetMTU - (TunnelOverhead + EncryptionIPsecOverhead)
+			return EthernetMTU - (c.tunnelOverhead + EncryptionIPsecOverhead)
 		}
-		return EthernetMTU - TunnelOverhead
+		return EthernetMTU - c.tunnelOverhead
 	}
 
 	return tunnelMTU

pkg/mtu/mtu_test.go
@@ -11,57 +11,62 @@ import (
 
 func TestNewConfiguration(t *testing.T) {
 	// Add routes with no encryption or tunnel
-	conf := NewConfiguration(0, false, false, false)
+	conf := NewConfiguration(0, false, false, false, false)
 	require.NotEqual(t, 0, conf.getDeviceMTU(0))
 	require.Equal(t, conf.getDeviceMTU(0), conf.getRouteMTU(0))
 
 	// Add routes with no encryption or tunnel and set MTU
-	conf = NewConfiguration(0, false, false, false)
+	conf = NewConfiguration(0, false, false, false, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
 	require.Equal(t, conf.getDeviceMTU(1400), conf.getRouteMTU(1400))
 
 	// Add routes with tunnel
-	conf = NewConfiguration(0, false, true, false)
+	conf = NewConfiguration(0, false, true, false, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
-	require.Equal(t, conf.getDeviceMTU(1400)-TunnelOverhead, conf.getRouteMTU(1400))
+	require.Equal(t, conf.getDeviceMTU(1400)-TunnelOverheadIPv4, conf.getRouteMTU(1400))
+
+	// Add routes with tunnel over IPv6
+	conf = NewConfiguration(0, false, true, false, true)
+	require.Equal(t, 1400, conf.getDeviceMTU(1400))
+	require.Equal(t, conf.getDeviceMTU(1400)-TunnelOverheadIPv6, conf.getRouteMTU(1400))
 
 	// Add routes with tunnel and set MTU
-	conf = NewConfiguration(0, false, true, false)
+	conf = NewConfiguration(0, false, true, false, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
-	require.Equal(t, conf.getDeviceMTU(1400)-TunnelOverhead, conf.getRouteMTU(1400))
+	require.Equal(t, conf.getDeviceMTU(1400)-TunnelOverheadIPv4, conf.getRouteMTU(1400))
 
 	// Add routes with encryption and set MTU using standard 128bit, larger 256bit and smaller 96bit ICVlen keys
-	conf = NewConfiguration(16, true, false, false)
+	conf = NewConfiguration(16, true, false, false, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
 	require.Equal(t, conf.getDeviceMTU(1400)-EncryptionIPsecOverhead, conf.getRouteMTU(1400))
 
-	conf = NewConfiguration(32, true, false, false)
+	conf = NewConfiguration(32, true, false, false, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
 	require.Equal(t, conf.getDeviceMTU(1400)-(EncryptionIPsecOverhead+16), conf.getRouteMTU(1400))
 
-	conf = NewConfiguration(12, true, false, false)
+	conf = NewConfiguration(12, true, false, false, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
 	require.Equal(t, conf.getDeviceMTU(1400)-(EncryptionIPsecOverhead-4), conf.getRouteMTU(1400))
 
 	// Add routes with encryption and tunnels using standard 128bit, larger 256bit and smaller 96bit ICVlen keys
-	conf = NewConfiguration(16, true, true, false)
+	conf = NewConfiguration(16, true, true, false, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
-	require.Equal(t, conf.getDeviceMTU(1400)-(TunnelOverhead+EncryptionIPsecOverhead), conf.getRouteMTU(1400))
+	require.Equal(t, conf.getDeviceMTU(1400)-(TunnelOverheadIPv4+EncryptionIPsecOverhead), conf.getRouteMTU(1400))
 
-	conf = NewConfiguration(32, true, true, false)
+	conf = NewConfiguration(32, true, true, false, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
-	require.Equal(t, conf.getDeviceMTU(1400)-(TunnelOverhead+EncryptionIPsecOverhead+16), conf.getRouteMTU(1400))
+	require.Equal(t, conf.getDeviceMTU(1400)-(TunnelOverheadIPv4+EncryptionIPsecOverhead+16), conf.getRouteMTU(1400))
 
-	conf = NewConfiguration(32, true, true, false)
+	conf = NewConfiguration(32, true, true, false, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
-	require.Equal(t, conf.getDeviceMTU(1400)-(TunnelOverhead+EncryptionIPsecOverhead+16), conf.getRouteMTU(1400))
+	require.Equal(t, conf.getDeviceMTU(1400)-(TunnelOverheadIPv4+EncryptionIPsecOverhead+16), conf.getRouteMTU(1400))
 
 	// Add routes with WireGuard enabled
-	conf = NewConfiguration(32, false, false, true)
+	conf = NewConfiguration(32, false, false, true, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
 	require.Equal(t, conf.getDeviceMTU(1400)-WireguardOverhead, conf.getRouteMTU(1400))
 
-	conf = NewConfiguration(32, false, true, true)
+	conf = NewConfiguration(32, false, true, true, false)
 	require.Equal(t, 1400, conf.getDeviceMTU(1400))
-	require.Equal(t, conf.getDeviceMTU(1400)-(WireguardOverhead+TunnelOverhead), conf.getRouteMTU(1400))
+	require.Equal(t, conf.getDeviceMTU(1400)-(WireguardOverhead+TunnelOverheadIPv4), conf.getRouteMTU(1400))
 }

commit 84e38c5a1fdca5f082a0d6c085bbe9830eae8735

mtu: Support IPv6 underlay protocol in dual-stack
Commit 7cc1d5395 ("mtu: Support tunneling over IPv6 in calculations")
adjusted the MTU calculation to take IPv6 underlays into account. But
because this commit was written before the underlay-protocol flag was
introduced, it simply assumed that we're in IPv6 underlay mode if
tunneling is enabled but IPv4 isn't.

That however doesn't work for dual-stack clusters with IPv6 underlay.
The present commit fixes it to use the underlay-protocol flag.

Fixes: 7cc1d5395 ("mtu: Support tunneling over IPv6 in calculations")
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

pkg/mtu/cell.go
@@ -90,7 +90,8 @@ func newForCell(lc cell.Lifecycle, p mtuParams, cc Config) (MTU, error) {
 	group := p.JobRegistry.NewGroup(p.Health, lc)
 	lc.Append(cell.Hook{
 		OnStart: func(ctx cell.HookContext) error {
-			tunnelOverIPv6 := !option.Config.EnableIPv4 && option.Config.RoutingMode == option.RoutingModeTunnel
+			tunnelOverIPv6 := option.Config.RoutingMode == option.RoutingModeTunnel &&
+				p.TunnelConfig.UnderlayProtocol() == tunnel.IPv6
 			*c = NewConfiguration(
 				p.IPsec.AuthKeySize(),
 				option.Config.EnableIPSec,