cilium/cilium

commit b7d58c11ed61c6c25e048849c3c25e75427520f3

service-cache: cleanup external endpoints and services on delete
Previously, upon detecting the deletion of a global service in a remote
cluster, we removed the corresponding external endpoints. Still, we did
not delete the map associated with that service when no remote endpoints
were left. This commit fixes this, and also ensures that the service
entry is deleted if no longer ready.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/k8s/service_cache.go
@@ -608,6 +608,9 @@ func (s *ServiceCache) MergeExternalServiceDelete(service *serviceStore.ClusterS
 		scopedLog.Debug("Deleting external endpoints")
 
 		delete(externalEndpoints.endpoints, service.Cluster)
+		if len(externalEndpoints.endpoints) == 0 {
+			delete(s.externalEndpoints, id)
+		}
 
 		svc, ok := s.services[id]
 
@@ -625,6 +628,7 @@ func (s *ServiceCache) MergeExternalServiceDelete(service *serviceStore.ClusterS
 			}
 
 			if !serviceReady {
+				delete(s.services, id)
 				event.Action = DeleteService
 			}
 
@@ -670,6 +674,9 @@ func (s *ServiceCache) MergeClusterServiceDelete(service *serviceStore.ClusterSe
 	if ok {
 		scopedLog.Debug("Deleting cluster endpoints")
 		delete(externalEndpoints.endpoints, service.Cluster)
+		if len(externalEndpoints.endpoints) == 0 {
+			delete(s.externalEndpoints, id)
+		}
 	}
 
 	svc, ok := s.services[id]

commit f5558a1e0a0b789ab561c3845f091f40a27dba8c

service-cache: fix incorrect service deletion on remote backends removal
The blamed commit introduced the removal of a service entry from the
internal service cache data structure upon reception of a deletion
event for remote endpoints from a remote cluster, if the service is
no longer associated with either a local endpointslice or remote
endpoints from any other cluster.

However, this is not correct, and can lead to an unrecoverable situation
(until a service update is performed, or Cilium agents are restarted),
because subsequent endpoint updates get then discarded due to not finding
the corresponding service anymore. Most notably, this bug is triggered if
the service in the local cluster has no selector, as in turn it is not
associated with any endpointslice, and the service in the remote cluster,
initially marked as global, gets deleted (or no longer shared). At that
point, even if the remote service gets recreated (or marked global again),
the remote endpoints are not merged anymore.

Let's get this fixed by not deleting the service entry from the service
cache in this case, as it is always removed upon actual deletion of the
local service object. Let's also add a unit test to cover this specific
scenario, to validate that the fix works as expected (the test does fail
without the fix).

This bug can be also reproduced with:

  make kind-clustermesh && make kind-clustermesh-images && make kind-install-cilium-clustermesh

  kubectl --context kind-clustermesh1 create deploy podinfo --image=stefanprodan/podinfo --replicas=1
  kubectl --context kind-clustermesh1 expose deploy podinfo --port 80 --target-port 9898
  kubectl --context kind-clustermesh1 annotate svc podinfo service.cilium.io/global=true

  # Create a service without a selector
  cat <<EOF | kubectl --context kind-clustermesh2 create -f -
  apiVersion: v1
  kind: Service
  metadata:
    annotations:
      service.cilium.io/global: "true"
    name: podinfo
    namespace: default
  spec:
    ports:
    - port: 80
      targetPort: 9898
  EOF

  # Marking the remote service as not global triggers the deletion of
  # the service entry, and marking it as global again does not add back
  # the remote endpoint entries.
  kubectl --context kind-clustermesh1 annotate svc podinfo service.cilium.io/global-
  kubectl --context kind-clustermesh1 annotate svc podinfo service.cilium.io/global=true

Fixes: b7d58c11ed61 ("service-cache: cleanup external endpoints and services on delete")

pkg/k8s/service_cache.go
@@ -814,7 +814,6 @@ func (s *ServiceCache) mergeExternalServiceDeleteLocked(service *serviceStore.Cl
 			}
 
 			if !serviceReady {
-				delete(s.services, id)
 				event.Action = DeleteService
 			}
 

pkg/k8s/service_cache_test.go
@@ -851,6 +851,76 @@ func TestExternalServiceMerging(t *testing.T) {
 	}, 2*time.Second))
 }
 
+func TestExternalServiceMergingNoLocalEndpoints(t *testing.T) {
+	db, nodeAddrs := newDB(t)
+	svcCache := NewServiceCache(db, nodeAddrs, NewSVCMetricsNoop())
+	swg := lock.NewStoppableWaitGroup()
+
+	svcID := svcCache.UpdateService(&slim_corev1.Service{
+		ObjectMeta: slim_metav1.ObjectMeta{
+			Namespace: "bar", Name: "foo",
+			Annotations: map[string]string{
+				"service.cilium.io/global": "true",
+			},
+		},
+		Spec: slim_corev1.ServiceSpec{
+			ClusterIP: "127.0.0.1",
+			Type:      slim_corev1.ServiceTypeClusterIP,
+			Ports: []slim_corev1.ServicePort{
+				{
+					Name:     "foo",
+					Protocol: slim_corev1.ProtocolTCP,
+					Port:     80,
+				},
+			},
+		},
+	}, swg)
+
+	clusterService := serviceStore.ClusterService{
+		Cluster: "cluster1", Namespace: "bar", Name: "foo",
+		Backends: map[string]serviceStore.PortConfiguration{
+			"10.0.0.1": map[string]*loadbalancer.L4Addr{
+				"http": {Protocol: loadbalancer.TCP, Port: 80},
+			},
+		},
+		IncludeExternal: true, Shared: true,
+	}
+
+	recv := func(t *testing.T, ch <-chan ServiceEvent) ServiceEvent {
+		t.Helper()
+		select {
+		case ev := <-ch:
+			return ev
+		case <-time.After(2 * time.Second):
+			t.Fatal("Timeout out waiting to receive the expected event")
+			return ServiceEvent{}
+		}
+	}
+
+	// Merge the backends from a remote cluster
+	svcCache.MergeExternalServiceUpdate(&clusterService, swg)
+
+	event := recv(t, svcCache.Events)
+	require.Equal(t, UpdateService, event.Action)
+	require.Equal(t, svcID, event.ID)
+	require.Len(t, event.Endpoints.Backends, 1)
+
+	// Delete the backends from a remote cluster
+	svcCache.MergeExternalServiceDelete(&clusterService, swg)
+
+	event = recv(t, svcCache.Events)
+	require.Equal(t, DeleteService, event.Action)
+	require.Equal(t, svcID, event.ID)
+
+	// Merge again the backends from a remote cluster
+	svcCache.MergeExternalServiceUpdate(&clusterService, swg)
+
+	event = recv(t, svcCache.Events)
+	require.Equal(t, UpdateService, event.Action)
+	require.Equal(t, svcID, event.ID)
+	require.Len(t, event.Endpoints.Backends, 1)
+}
+
 func TestExternalServiceDeletion(t *testing.T) {
 	const cluster = "cluster"
 
@@ -880,7 +950,7 @@ func TestExternalServiceDeletion(t *testing.T) {
 
 	svcCache.MergeExternalServiceDelete(&clsvc, swg)
 	_, ok := svcCache.services[id1]
-	require.False(t, ok)
+	require.True(t, ok) // Must be deleted only upon service deletion.
 	_, ok = svcCache.externalEndpoints[id1]
 	require.False(t, ok)
 
@@ -918,7 +988,7 @@ func TestExternalServiceDeletion(t *testing.T) {
 
 	svcCache.MergeExternalServiceDelete(&clsvc, swg)
 	_, ok = svcCache.services[id2]
-	require.False(t, ok)
+	require.True(t, ok) // Must be deleted only upon service deletion.
 	_, ok = svcCache.externalEndpoints[id2]
 	require.False(t, ok)
 