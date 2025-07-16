e28576ce4d002484dd62dc14b0aeb35394f308d3
Marcel Zieba <marcel.zieba@isovalent.com>
2025-07-16 14:12:07 +0200

    k8s: remove EndpointSliceV1 / EndpointSlice capabilities
    EndpointSlices are stable since 1.21
    
    Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>

--- pkg/k8s/version/version.go
+++ pkg/k8s/version/version.go
@@ -6,17 +6,13 @@
 package version
 
 import (
-	"context"
 	"fmt"
 	"log/slog"
 
 	"github.com/blang/semver/v4"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/cilium/cilium/pkg/lock"
-	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/versioncheck"
 )
 
@@ -27,13 +23,6 @@ type ServerCapabilities struct {
 	// MinimalVersionMet is true when the minimal version of Kubernetes
 	// required to run Cilium has been met
 	MinimalVersionMet bool
-
-	// EndpointSlice is the ability of k8s server to support endpoint slices
-	EndpointSlice bool
-
-	// EndpointSliceV1 is the ability of k8s server to support endpoint slices
-	// v1. This version was introduced in K8s v1.21.0.
-	EndpointSliceV1 bool
 }
 
 type cachedVersion struct {
@@ -50,18 +39,6 @@ const (
 
 var (
 	cached = cachedVersion{}
-
-	discoveryAPIGroupV1beta1 = "discovery.k8s.io/v1beta1"
-	discoveryAPIGroupV1      = "discovery.k8s.io/v1"
-	endpointSliceKind        = "EndpointSlice"
-
-	// Constraint to check support for discovery/v1 types. Support for v1
-	// discovery was introduced in K8s version 1.21.
-	isGEThanAPIDiscoveryV1 = versioncheck.MustCompile(">=1.21.0")
-
-	// Constraint to check support for discovery/v1beta1 types. Support for
-	// v1beta1 discovery was introduced in K8s version 1.17.
-	isGEThanAPIDiscoveryV1Beta1 = versioncheck.MustCompile(">=1.17.0")
 
 	// isGEThanMinimalVersionConstraint is the minimal version required to run
 	// Cilium
@@ -91,35 +68,6 @@ func updateVersion(version semver.Version) {
 	cached.version = version
 
 	cached.capabilities.MinimalVersionMet = isGEThanMinimalVersionConstraint(version)
-	cached.capabilities.EndpointSliceV1 = isGEThanAPIDiscoveryV1(version)
-	cached.capabilities.EndpointSlice = isGEThanAPIDiscoveryV1Beta1(version)
-}
-
-func updateServerGroupsAndResources(apiResourceLists []*metav1.APIResourceList) {
-	cached.mutex.Lock()
-	defer cached.mutex.Unlock()
-
-	cached.capabilities.EndpointSlice = false
-	cached.capabilities.EndpointSliceV1 = false
-	for _, rscList := range apiResourceLists {
-		if rscList.GroupVersion == discoveryAPIGroupV1beta1 {
-			for _, rsc := range rscList.APIResources {
-				if rsc.Kind == endpointSliceKind {
-					cached.capabilities.EndpointSlice = true
-					break
-				}
-			}
-		}
-		if rscList.GroupVersion == discoveryAPIGroupV1 {
-			for _, rsc := range rscList.APIResources {
-				if rsc.Kind == endpointSliceKind {
-					cached.capabilities.EndpointSlice = true
-					cached.capabilities.EndpointSliceV1 = true
-					break
-				}
-			}
-		}
-	}
 }
 
 // Force forces the use of a specific version
@@ -130,45 +78,6 @@ func Force(version string) error {
 	}
 	updateVersion(ver)
 	return nil
-}
-
-func endpointSlicesFallbackDiscovery(logger *slog.Logger, client kubernetes.Interface) error {
-	// If a k8s version with discovery v1 is used, then do not even bother
-	// checking for v1beta1
-	cached.mutex.Lock()
-	if cached.capabilities.EndpointSliceV1 {
-		cached.capabilities.EndpointSlice = true
-		cached.mutex.Unlock()
-		return nil
-	}
-	cached.mutex.Unlock()
-
-	// Discovery of API groups requires the API services of the apiserver to be
-	// healthy. Such API services can depend on the readiness of regular pods
-	// which require Cilium to function correctly. By treating failure to
-	// discover API groups as fatal, a critial loop can be entered in which
-	// Cilium cannot start because the API groups can't be discovered.
-	//
-	// Here we acknowledge the lack of discovery ability as non Fatal and fall back to probing
-	// the API directly.
-	_, err := client.DiscoveryV1beta1().EndpointSlices("default").Get(context.TODO(), "kubernetes", metav1.GetOptions{})
-	if err == nil {
-		cached.mutex.Lock()
-		cached.capabilities.EndpointSlice = true
-		cached.mutex.Unlock()
-		return nil
-	}
-
-	if errors.IsNotFound(err) {
-		logger.Info("Unable to retrieve EndpointSlices for default/kubernetes. Disabling EndpointSlices", logfields.Error, err)
-		// StatusNotFound is a safe error, EndpointSlices are
-		// disabled and the agent can continue.
-		return nil
-	}
-
-	// Unknown error, we can't derive whether to enable or disable
-	// EndpointSlices and need to error out.
-	return fmt.Errorf("unable to validate EndpointSlices support: %w", err)
 }
 
 func updateK8sServerVersion(client kubernetes.Interface) error {
@@ -211,29 +120,6 @@ func Update(logger *slog.Logger, client kubernetes.Interface, apiDiscoveryEnable
 	err := updateK8sServerVersion(client)
 	if err != nil {
 		return err
-	}
-
-	if apiDiscoveryEnabled {
-		// Discovery of API groups requires the API services of the
-		// apiserver to be healthy. Such API services can depend on the
-		// readiness of regular pods which require Cilium to function
-		// correctly. By treating failure to discover API groups as
-		// fatal, a critical loop can be entered in which Cilium cannot
-		// start because the API groups can't be discovered and th API
-		// groups will only become discoverable once Cilium is up.
-		_, apiResourceLists, err := client.Discovery().ServerGroupsAndResources()
-		if err != nil {
-			// It doesn't make sense to retry the retrieval of this
-			// information at a later point because the capabilities are
-			// primiarly used while the agent is starting up. Instead, fall
-			// back to probing API endpoints directly.
-			logger.Warn("Unable to discover API groups and resources", logfields.Error, err)
-			return endpointSlicesFallbackDiscovery(logger, client)
-		}
-
-		updateServerGroupsAndResources(apiResourceLists)
-	} else {
-		return endpointSlicesFallbackDiscovery(logger, client)
 	}
 
 	return nil
