c9f817078f14cd2bcf2948293822b0960fed539a
Tobias Klauser <tobias@cilium.io>
2025-07-16 09:43:00 +0200

    tools/dev-doctor: remove vagrant dev VM specific checks
    The NFS config and iptables rules checks in dev-doctor were only
    relevant for the deprecated vagrant dev VM setup which was removed in
    commit bafb49b7379f ("cleanup: Remove vagrant related file").
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- tools/dev-doctor/common.go
+++ tools/dev-doctor/common.go
@@ -6,19 +6,7 @@ package main
 import (
 	"go/build"
 	"os"
-	"os/exec"
 )
-
-// sudo returns cmd run with sudo. cmd is modified in place.
-func sudo(cmd *exec.Cmd) (*exec.Cmd, error) {
-	sudoPath, err := exec.LookPath("sudo")
-	if err != nil {
-		return nil, err
-	}
-	cmd.Args = append([]string{cmd.Path}, cmd.Args...)
-	cmd.Path = sudoPath
-	return cmd, nil
-}
 
 // goPath returns the environment $GOPATH, or the default when empty or unset.
 func goPath() string {
