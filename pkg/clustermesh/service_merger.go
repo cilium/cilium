cilium/cilium

commit 49a8edf7da2dbfca1a0d246520820e3922752417

experimental: Include ClusterID when manipulating backends
For ClusterMesh we may get backends for the same service from multiple clusters
and thus it is not enough to just use the Source for figuring out what backends
are orphans in SetBackends(). Add the ClusterID as an argument so we can
differentiate between the clusters.

Remove the unused ReleaseBackendsFromSource and ReleaseBackendsForService.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/clustermesh/service_merger.go
@@ -52,10 +52,11 @@ func (sm *expServiceMerger) MergeExternalServiceDelete(service *serviceStore.Clu
 	}
 	txn := sm.writer.WriteTxn()
 	defer txn.Commit()
-	sm.writer.DeleteBackendsOfService(
+	sm.writer.DeleteBackendsOfServiceFromCluster(
 		txn,
 		name,
 		source.ClusterMesh,
+		service.ClusterID,
 	)
 }
 
@@ -69,27 +70,18 @@ func (sm *expServiceMerger) MergeExternalServiceUpdate(service *serviceStore.Clu
 	txn := sm.writer.WriteTxn()
 	defer txn.Commit()
 
-	if !service.Shared {
-		// The service potentially became unshared with other clusters, remove any potential
-		// prior backends.
-		sm.writer.DeleteBackendsOfService(txn, name, source.ClusterMesh)
-		return
-	}
-
-	sm.writer.SetBackends(
+	sm.writer.SetBackendsOfCluster(
 		txn,
 		name,
 		source.ClusterMesh,
+		service.ClusterID,
 		ClusterServiceToBackendParams(service)...,
 	)
 }
 
 func ClusterServiceToBackendParams(service *serviceStore.ClusterService) (beps []experimental.BackendParams) {
 	for ipString, portConfig := range service.Backends {
-		addrCluster, err := cmtypes.ParseAddrCluster(ipString)
-		if err != nil {
-			continue
-		}
+		addrCluster := cmtypes.MustParseAddrCluster(ipString)
 		for name, l4 := range portConfig {
 			portNames := []string(nil)
 			if name != "" {
@@ -103,6 +95,7 @@ func ClusterServiceToBackendParams(service *serviceStore.ClusterService) (beps [
 				PortNames: portNames,
 				Weight:    loadbalancer.DefaultBackendWeight,
 				NodeName:  "",
+				ClusterID: service.ClusterID,
 				State:     loadbalancer.BackendStateActive,
 			}
 			beps = append(beps, bep)

pkg/loadbalancer/experimental/backend.go
@@ -43,6 +43,9 @@ type BackendParams struct {
 	// ForZones where this backend should be consumed in
 	ForZones []string
 
+	// ClusterID of the cluster in which the backend is located. 0 for local cluster.
+	ClusterID uint32
+
 	// Source of the backend.
 	Source source.Source
 
@@ -259,10 +262,10 @@ func (be *Backend) release(name loadbalancer.ServiceName) (*Backend, bool) {
 	return &beCopy, beCopy.Instances.Len() == 0
 }
 
-func (be *Backend) releasePerSource(name loadbalancer.ServiceName, source source.Source) (*Backend, bool) {
+func (be *Backend) releasePerSource(name loadbalancer.ServiceName, source source.Source, clusterID uint32) (*Backend, bool) {
 	var keyToDelete *BackendInstanceKey
 	for k, inst := range be.instancesOfService(name) {
-		if inst.Source == source {
+		if inst.Source == source && inst.ClusterID == clusterID {
 			keyToDelete = &k
 			break
 		}

pkg/loadbalancer/experimental/testdata/marshalling.txtar
@@ -114,6 +114,7 @@ Address              Instances
       "NodeName": "nodeport-worker",
       "Zone": "",
       "ForZones": null,
+      "ClusterID": 0,
       "Source": "k8s",
       "State": 0,
       "Unhealthy": false,
@@ -158,6 +159,7 @@ Address              Instances
       "NodeName": "nodeport-worker",
       "Zone": "",
       "ForZones": null,
+      "ClusterID": 0,
       "Source": "k8s",
       "State": 0,
       "Unhealthy": false,
@@ -199,6 +201,7 @@ Address              Instances
         "NodeName": "nodeport-worker",
         "Zone": "",
         "ForZones": null,
+        "ClusterID": 0,
         "Source": "k8s",
         "State": 0,
         "Unhealthy": false,
@@ -238,6 +241,7 @@ Address              Instances
         "NodeName": "nodeport-worker",
         "Zone": "",
         "ForZones": null,
+        "ClusterID": 0,
         "Source": "k8s",
         "State": 0,
         "Unhealthy": false,
@@ -291,6 +295,7 @@ backends:
       nodename: nodeport-worker
       zone: ""
       forzones: []
+      clusterid: 0
       source: k8s
       state: 0
       unhealthy: false
@@ -319,6 +324,7 @@ backends:
       nodename: nodeport-worker
       zone: ""
       forzones: []
+      clusterid: 0
       source: k8s
       state: 0
       unhealthy: false
@@ -342,6 +348,7 @@ instances:
         nodename: nodeport-worker
         zone: ""
         forzones: []
+        clusterid: 0
         source: k8s
         state: 0
         unhealthy: false
@@ -363,6 +370,7 @@ instances:
         nodename: nodeport-worker
         zone: ""
         forzones: []
+        clusterid: 0
         source: k8s
         state: 0
         unhealthy: false

pkg/loadbalancer/experimental/writer.go
@@ -44,6 +44,9 @@ type Writer struct {
 
 type SelectBackendsFunc = func(statedb.ReadTxn, statedb.Table[*Backend], *Frontend) iter.Seq2[BackendParams, statedb.Revision]
 
+// Backends for the local cluster are associated with ID 0, regardless of the real cluster id.
+const LocalClusterID = 0
+
 type writerParams struct {
 	cell.In
 
@@ -422,7 +425,7 @@ func (w *Writer) DeleteServicesBySource(txn WriteTxn, source source.Source) erro
 
 // UpsertBackends adds/updates backends for the given service.
 func (w *Writer) UpsertBackends(txn WriteTxn, serviceName loadbalancer.ServiceName, source source.Source, bes ...BackendParams) error {
-	refs, err := w.updateBackends(txn, serviceName, source, bes)
+	refs, err := w.updateBackends(txn, serviceName, source, LocalClusterID, bes)
 	if err != nil {
 		return err
 	}
@@ -438,18 +441,25 @@ func (w *Writer) UpsertBackends(txn WriteTxn, serviceName loadbalancer.ServiceNa
 // SetBackends sets the backends associated with a service. Existing backends from this source that
 // are associated with the service but are not given are released.
 func (w *Writer) SetBackends(txn WriteTxn, name loadbalancer.ServiceName, source source.Source, bes ...BackendParams) error {
+	return w.SetBackendsOfCluster(txn, name, source, LocalClusterID, bes...)
+}
+
+// SetBackendsOfCluster sets the backends associated with a service from the specified cluster. It will
+// not affect the backends from other clusters associated with the service.
+func (w *Writer) SetBackendsOfCluster(txn WriteTxn, name loadbalancer.ServiceName, source source.Source, clusterID uint32, bes ...BackendParams) error {
 	addrs := sets.New[loadbalancer.L3n4Addr]()
 	for _, be := range bes {
 		addrs.Insert(be.Address)
 	}
-	perSourceOrphans := statedb.Filter(
+
+	orphans := statedb.Filter(
 		w.bes.List(txn, BackendByServiceName(name)),
 		func(be *Backend) bool {
 			inst := be.GetInstanceFromSource(name, source)
-			return inst != nil && !addrs.Has(be.Address)
+			return inst != nil && inst.ClusterID == clusterID && !addrs.Has(be.Address)
 		})
 
-	refs, err := w.updateBackends(txn, name, source, bes)
+	refs, err := w.updateBackends(txn, name, source, clusterID, bes)
 	if err != nil {
 		return err
 	}
@@ -457,8 +467,8 @@ func (w *Writer) SetBackends(txn WriteTxn, name loadbalancer.ServiceName, source
 
 	// Release orphaned backends, e.g. all backends from this source referencing this
 	// service.
-	for be := range perSourceOrphans {
-		if _, err := w.removeBackendRefPerSource(txn, name, be, source); err != nil {
+	for be := range orphans {
+		if _, err := w.removeBackendRefPerSource(txn, name, be, source, clusterID); err != nil {
 			return err
 		}
 	}
@@ -473,7 +483,7 @@ func (w *Writer) SetBackends(txn WriteTxn, name loadbalancer.ServiceName, source
 	return nil
 }
 
-func (w *Writer) updateBackends(txn WriteTxn, serviceName loadbalancer.ServiceName, source source.Source, bes []BackendParams) (sets.Set[loadbalancer.ServiceName], error) {
+func (w *Writer) updateBackends(txn WriteTxn, serviceName loadbalancer.ServiceName, source source.Source, clusterID uint32, bes []BackendParams) (sets.Set[loadbalancer.ServiceName], error) {
 	// Collect all the service names linked with the updated backends in order to bump the
 	// associated frontends for reconciliation.
 	referencedServices := sets.New[loadbalancer.ServiceName]()
@@ -493,6 +503,7 @@ func (w *Writer) updateBackends(txn WriteTxn, serviceName loadbalancer.ServiceNa
 		}
 
 		bep.Source = source
+		bep.ClusterID = clusterID
 		be.Instances = be.Instances.Set(
 			BackendInstanceKey{ServiceName: serviceName, SourcePriority: w.sourcePriority(bep.Source)},
 			bep,
@@ -510,14 +521,18 @@ func (w *Writer) updateBackends(txn WriteTxn, serviceName loadbalancer.ServiceNa
 }
 
 func (w *Writer) DeleteBackendsOfService(txn WriteTxn, name loadbalancer.ServiceName, src source.Source) error {
+	return w.DeleteBackendsOfServiceFromCluster(txn, name, src, LocalClusterID)
+}
+
+func (w *Writer) DeleteBackendsOfServiceFromCluster(txn WriteTxn, name loadbalancer.ServiceName, src source.Source, clusterID uint32) error {
 	for be := range w.bes.List(txn, BackendByServiceName(name)) {
 		if inst := be.GetInstanceFromSource(name, src); inst != nil {
-			be, orphaned := be.releasePerSource(name, src)
+			beNew, orphaned := be.releasePerSource(name, src, clusterID)
 			var err error
 			if orphaned {
-				_, _, err = w.bes.Delete(txn, be)
-			} else {
-				_, _, err = w.bes.Insert(txn, be)
+				_, _, err = w.bes.Delete(txn, beNew)
+			} else if beNew != be {
+				_, _, err = w.bes.Insert(txn, beNew)
 			}
 			if err != nil {
 				return err
@@ -525,43 +540,6 @@ func (w *Writer) DeleteBackendsOfService(txn WriteTxn, name loadbalancer.Service
 		}
 	}
 	return w.RefreshFrontends(txn, name)
-}
-
-func (w *Writer) DeleteBackendsBySource(txn WriteTxn, src source.Source) error {
-	// Iterating over all as this is a rare operation so we can afford it.
-	names := sets.New[loadbalancer.ServiceName]()
-	for be := range w.bes.All(txn) {
-		orphaned, matched := false, false
-		for k, inst := range be.Instances.All() {
-			if inst.Source == src {
-				names.Insert(k.ServiceName)
-				be, orphaned = be.releasePerSource(k.ServiceName, src)
-				matched = true
-			}
-		}
-		if !matched {
-			continue
-		}
-		var err error
-		if orphaned {
-			_, _, err = w.bes.Delete(txn, be)
-		} else {
-			_, _, err = w.bes.Insert(txn, be)
-		}
-		if err != nil {
-			return err
-		}
-	}
-
-	// Mark the frontends of all referenced services as pending to reconcile the
-	// deleted backends. We need to reconcile every frontend to update the references
-	// to the backends in the services and maglev BPF maps.
-	for name := range names {
-		if err := w.RefreshFrontends(txn, name); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 func (w *Writer) removeBackendRef(txn WriteTxn, name loadbalancer.ServiceName, be *Backend) (err error) {
@@ -574,8 +552,8 @@ func (w *Writer) removeBackendRef(txn WriteTxn, name loadbalancer.ServiceName, b
 	return err
 }
 
-func (w *Writer) removeBackendRefPerSource(txn WriteTxn, name loadbalancer.ServiceName, be *Backend, src source.Source) (backend *Backend, err error) {
-	be, orphan := be.releasePerSource(name, src)
+func (w *Writer) removeBackendRefPerSource(txn WriteTxn, name loadbalancer.ServiceName, be *Backend, src source.Source, clusterID uint32) (backend *Backend, err error) {
+	be, orphan := be.releasePerSource(name, src, clusterID)
 	if orphan {
 		_, _, err = w.bes.Delete(txn, be)
 	} else {
@@ -601,18 +579,6 @@ func (w *Writer) ReleaseBackends(txn WriteTxn, name loadbalancer.ServiceName, ad
 	return w.RefreshFrontends(txn, name)
 }
 
-func (w *Writer) ReleaseBackendsFromSource(txn WriteTxn, name loadbalancer.ServiceName, source source.Source) error {
-	for be := range w.bes.List(txn, BackendByServiceName(name)) {
-		if inst := be.GetInstanceFromSource(name, source); inst == nil {
-			continue
-		}
-		if _, err := w.removeBackendRefPerSource(txn, name, be, source); err != nil {
-			return err
-		}
-	}
-	return w.RefreshFrontends(txn, name)
-}
-
 func (w *Writer) SetRedirectTo(txn WriteTxn, fe *Frontend, to *loadbalancer.ServiceName) {
 	if to == nil && fe.RedirectTo == nil {
 		return
@@ -626,17 +592,6 @@ func (w *Writer) SetRedirectTo(txn WriteTxn, fe *Frontend, to *loadbalancer.Serv
 	fe.RedirectTo = to
 	w.refreshFrontend(txn, fe)
 	w.fes.Insert(txn, fe)
-}
-
-func (w *Writer) ReleaseBackendsForService(txn WriteTxn, name loadbalancer.ServiceName) error {
-	be, _, ok := w.bes.Get(txn, BackendByServiceName(name))
-	if !ok {
-		return statedb.ErrObjectNotFound
-	}
-	if err := w.removeBackendRef(txn, name, be); err != nil {
-		return err
-	}
-	return w.RefreshFrontends(txn, name)
 }
 
 func (w *Writer) DebugDump(txn statedb.ReadTxn, to io.Writer) {

commit 12fdcbe2cab01890d0548b55edde92e8e6075e6b

loadbalancer: Remove BackendParams.ClusterID
The ClusterID is already part of AddrCluster (in L3n4Addr) and thus we
shouldn't duplicate it.

Remove BackendParams.ClusterID and use Address.AddrCluster.ClusterID()
instead.

Fixes: 49a8edf7da2 ("experimental: Include ClusterID when manipulating backends")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/clustermesh/service_merger.go
@@ -5,6 +5,7 @@ package clustermesh
 
 import (
 	"context"
+	"net/netip"
 
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
@@ -91,7 +92,11 @@ func (sm *serviceMerger) MergeExternalServiceUpdate(service *serviceStore.Cluste
 
 func ClusterServiceToBackendParams(service *serviceStore.ClusterService) (beps []loadbalancer.BackendParams) {
 	for ipString, portConfig := range service.Backends {
-		addrCluster := cmtypes.MustParseAddrCluster(ipString)
+		addr, err := netip.ParseAddr(ipString)
+		if err != nil {
+			continue
+		}
+		addrCluster := cmtypes.AddrClusterFrom(addr, service.ClusterID)
 		for name, l4 := range portConfig {
 			portNames := []string(nil)
 			if name != "" {
@@ -105,7 +110,6 @@ func ClusterServiceToBackendParams(service *serviceStore.ClusterService) (beps [
 				PortNames: portNames,
 				Weight:    loadbalancer.DefaultBackendWeight,
 				NodeName:  "",
-				ClusterID: service.ClusterID,
 				State:     loadbalancer.BackendStateActive,
 			}
 			beps = append(beps, bep)

pkg/loadbalancer/backend.go
@@ -41,9 +41,6 @@ type BackendParams struct {
 	// ForZones where this backend should be consumed in
 	ForZones []string
 
-	// ClusterID of the cluster in which the backend is located. 0 for local cluster.
-	ClusterID uint32
-
 	// Source of the backend.
 	Source source.Source
 
@@ -59,6 +56,25 @@ type BackendParams struct {
 	// UnhealthyUpdatedAt is the timestamp for when [Unhealthy] was last updated. Zero
 	// value if never updated.
 	UnhealthyUpdatedAt time.Time
+}
+
+func (bep *BackendParams) ClusterID() uint32 {
+	return bep.Address.AddrCluster.ClusterID()
+}
+
+type BackendInstanceKey struct {
+	ServiceName    ServiceName
+	SourcePriority uint8
+}
+
+func (k BackendInstanceKey) Key() []byte {
+	if k.SourcePriority == 0 {
+		return k.ServiceName.Key()
+	}
+	sk := k.ServiceName.Key()
+	buf := make([]byte, 0, 2+len(sk))
+	buf = append(buf, sk...)
+	return append(buf, ' ', k.SourcePriority)
 }
 
 // Backend is a composite of the per-service backend instances that share the same
@@ -77,19 +93,8 @@ type Backend struct {
 	Instances part.Map[BackendInstanceKey, BackendParams]
 }
 
-type BackendInstanceKey struct {
-	ServiceName    ServiceName
-	SourcePriority uint8
-}
-
-func (k BackendInstanceKey) Key() []byte {
-	if k.SourcePriority == 0 {
-		return k.ServiceName.Key()
-	}
-	sk := k.ServiceName.Key()
-	buf := make([]byte, 0, 2+len(sk))
-	buf = append(buf, sk...)
-	return append(buf, ' ', k.SourcePriority)
+func (be *Backend) ClusterID() uint32 {
+	return be.Address.AddrCluster.ClusterID()
 }
 
 func (be *Backend) GetInstance(name ServiceName) *BackendParams {

pkg/loadbalancer/writer/writer.go
@@ -560,7 +560,7 @@ func (w *Writer) SetBackendsOfCluster(txn WriteTxn, name loadbalancer.ServiceNam
 			continue
 		}
 		inst := be.GetInstanceFromSource(name, source)
-		if inst == nil || inst.ClusterID != clusterID {
+		if inst == nil || inst.ClusterID() != clusterID {
 			continue
 		}
 		if err := w.removeBackendRefPerSource(txn, name, be, source, clusterID); err != nil {
@@ -598,7 +598,6 @@ func (w *Writer) updateBackends(txn WriteTxn, serviceName loadbalancer.ServiceNa
 		}
 
 		bep.Source = source
-		bep.ClusterID = clusterID
 		be.Instances = be.Instances.Set(
 			loadbalancer.BackendInstanceKey{ServiceName: serviceName, SourcePriority: w.sourcePriority(bep.Source)},
 			bep,
@@ -776,7 +775,7 @@ func backendRelease(be *loadbalancer.Backend, name loadbalancer.ServiceName) (*l
 
 func backendReleasePerSource(be *loadbalancer.Backend, name loadbalancer.ServiceName, source source.Source, clusterID uint32) (*loadbalancer.Backend, bool) {
 	for k, inst := range be.GetInstancesOfService(name) {
-		if inst.Source == source && inst.ClusterID == clusterID {
+		if inst.Source == source && inst.ClusterID() == clusterID {
 			if be.Instances.Len() == 1 {
 				// This was the last instance.
 				return nil, true

pkg/clustermesh/testdata/clusterservice-without-local-eps.txtar
@@ -27,12 +27,12 @@ db/cmp backends backends_v2.table
 
 -- backends.table --
 Address              Instances         NodeName
-20.0.0.2:9090/TCP    test/echo (tcp)
+20.0.0.2@2:9090/TCP  test/echo (tcp)
 
 -- backends_v2.table --
 Address              Instances         NodeName
-20.0.0.2:9090/TCP    test/echo (tcp)
-20.0.0.3:9090/TCP    test/echo (tcp)
+20.0.0.2@2:9090/TCP  test/echo (tcp)
+20.0.0.3@2:9090/TCP  test/echo (tcp)
 
 -- frontends.table --
 Address            Type       ServiceName   Status  Backends
@@ -40,11 +40,11 @@ Address            Type       ServiceName   Status  Backends
 
 -- frontends-with-clusterservice.table --
 Address            Type       ServiceName   Status  Backends
-10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    20.0.0.2:9090/TCP
+10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    20.0.0.2@2:9090/TCP
 
 -- frontends-with-clusterservice_v2.table --
 Address            Type       ServiceName   Status  Backends
-10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    20.0.0.2:9090/TCP, 20.0.0.3:9090/TCP
+10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    20.0.0.2@2:9090/TCP, 20.0.0.3@2:9090/TCP
 
 
 -- clusterservice2.json --

pkg/clustermesh/testdata/clusterservice.txtar
@@ -42,20 +42,20 @@ db/cmp frontends frontends.table
 -- backends-2+3.table --
 Address              Instances         NodeName
 10.1.0.1:8080/TCP    test/echo (tcp)   nodeport-worker
-20.0.0.2:9090/TCP    test/echo (tcp)
-30.0.0.3:9090/TCP    test/echo (tcp)
+20.0.0.2@2:9090/TCP  test/echo (tcp)
+30.0.0.3@3:9090/TCP  test/echo (tcp)
 
 -- backends-2+3_v2.table --
 Address              Instances         NodeName
 10.1.0.1:8080/TCP    test/echo (tcp)   nodeport-worker
-20.0.0.2:9090/TCP    test/echo (tcp)
-20.0.0.3:9090/TCP    test/echo (tcp)
-30.0.0.3:9090/TCP    test/echo (tcp)
+20.0.0.2@2:9090/TCP  test/echo (tcp)
+20.0.0.3@2:9090/TCP  test/echo (tcp)
+30.0.0.3@3:9090/TCP  test/echo (tcp)
 
 -- backends-3.table --
 Address              Instances         NodeName
 10.1.0.1:8080/TCP    test/echo (tcp)   nodeport-worker
-30.0.0.3:9090/TCP    test/echo (tcp)
+30.0.0.3@3:9090/TCP  test/echo (tcp)
 
 -- frontends.table --
 Address            Type       ServiceName   Status  Backends
@@ -63,11 +63,11 @@ Address            Type       ServiceName   Status  Backends
 
 -- frontends-with-clusterservice-3.table --
 Address            Type       ServiceName   Status  Backends
-10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    10.1.0.1:8080/TCP, 30.0.0.3:9090/TCP
+10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    10.1.0.1:8080/TCP, 30.0.0.3@3:9090/TCP
 
 -- frontends-with-clusterservice-2+3.table --
 Address            Type       ServiceName   Status  Backends
-10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    10.1.0.1:8080/TCP, 20.0.0.2:9090/TCP, 30.0.0.3:9090/TCP
+10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    10.1.0.1:8080/TCP, 20.0.0.2@2:9090/TCP, 30.0.0.3@3:9090/TCP
 
 
 

pkg/clustermesh/testdata/service-affinity.txtar
@@ -45,7 +45,7 @@ db/cmp frontends frontends-all.table
 -- backends.table --
 Address              Instances         NodeName
 10.1.0.1:8080/TCP    test/echo (tcp)   nodeport-worker
-20.0.0.2:9090/TCP    test/echo (tcp)
+20.0.0.2@2:9090/TCP  test/echo (tcp)
 
 -- frontends-local.table --
 Address            Type       ServiceName   Status  Backends
@@ -53,11 +53,11 @@ Address            Type       ServiceName   Status  Backends
 
 -- frontends-remote.table --
 Address            Type       ServiceName   Status  Backends
-10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    20.0.0.2:9090/TCP
+10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    20.0.0.2@2:9090/TCP
 
 -- frontends-all.table --
 Address            Type       ServiceName   Status  Backends
-10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    10.1.0.1:8080/TCP, 20.0.0.2:9090/TCP
+10.0.0.1:8080/TCP  ClusterIP  test/echo     Done    10.1.0.1:8080/TCP, 20.0.0.2@2:9090/TCP
 
 -- clusterservice2.json --
 {

pkg/loadbalancer/tests/testdata/marshalling.txtar
@@ -106,7 +106,6 @@ Address              Instances
       "NodeName": "nodeport-worker",
       "Zone": "",
       "ForZones": null,
-      "ClusterID": 0,
       "Source": "k8s",
       "State": 0,
       "Unhealthy": false,
@@ -147,7 +146,6 @@ Address              Instances
       "NodeName": "nodeport-worker",
       "Zone": "",
       "ForZones": null,
-      "ClusterID": 0,
       "Source": "k8s",
       "State": 0,
       "Unhealthy": false,
@@ -185,7 +183,6 @@ Address              Instances
         "NodeName": "nodeport-worker",
         "Zone": "",
         "ForZones": null,
-        "ClusterID": 0,
         "Source": "k8s",
         "State": 0,
         "Unhealthy": false,
@@ -221,7 +218,6 @@ Address              Instances
         "NodeName": "nodeport-worker",
         "Zone": "",
         "ForZones": null,
-        "ClusterID": 0,
         "Source": "k8s",
         "State": 0,
         "Unhealthy": false,
@@ -270,7 +266,6 @@ backends:
       nodename: nodeport-worker
       zone: ""
       forzones: []
-      clusterid: 0
       source: k8s
       state: 0
       unhealthy: false
@@ -296,7 +291,6 @@ backends:
       nodename: nodeport-worker
       zone: ""
       forzones: []
-      clusterid: 0
       source: k8s
       state: 0
       unhealthy: false
@@ -317,7 +311,6 @@ instances:
         nodename: nodeport-worker
         zone: ""
         forzones: []
-        clusterid: 0
         source: k8s
         state: 0
         unhealthy: false
@@ -336,7 +329,6 @@ instances:
         nodename: nodeport-worker
         zone: ""
         forzones: []
-        clusterid: 0
         source: k8s
         state: 0
         unhealthy: false

pkg/loadbalancer/writer/writer_test.go
@@ -8,6 +8,7 @@ import (
 	"encoding/binary"
 	"iter"
 	"log/slog"
+	"net/netip"
 	"os"
 	"slices"
 	"testing"
@@ -67,17 +68,16 @@ func fixture(t testing.TB) (p testParams) {
 	return p
 }
 
-func intToAddr(i int) types.AddrCluster {
+func intToAddr(i int, clusterID uint32) types.AddrCluster {
 	var addr [4]byte
 	binary.BigEndian.PutUint32(addr[:], 0x0100_0000+uint32(i))
-	addrCluster, _ := types.AddrClusterFromIP(addr[:])
-	return addrCluster
+	return types.AddrClusterFrom(netip.AddrFrom4(addr), clusterID)
 }
 
 func TestWriter_Service_UpsertDelete(t *testing.T) {
 	p := fixture(t)
 	name := loadbalancer.NewServiceName("test", "test1")
-	addrCluster := intToAddr(1)
+	addrCluster := intToAddr(1, LocalClusterID)
 	frontend := loadbalancer.NewL3n4Addr(loadbalancer.TCP, addrCluster, 12345, loadbalancer.ScopeExternal)
 
 	// Add a dump of the state if the test fails. Note that we abort
@@ -194,7 +194,7 @@ func TestWriter_Backend_UpsertDelete(t *testing.T) {
 	nextAddr := 0
 	mkAddr := func(port uint16) loadbalancer.L3n4Addr {
 		nextAddr++
-		addrCluster := intToAddr(nextAddr)
+		addrCluster := intToAddr(nextAddr, LocalClusterID)
 		return loadbalancer.NewL3n4Addr(loadbalancer.TCP, addrCluster, port, loadbalancer.ScopeExternal)
 	}
 	frontend := mkAddr(3000)
@@ -303,7 +303,7 @@ func TestWriter_Initializers(t *testing.T) {
 	complete2 := p.Writer.RegisterInitializer("test2")
 
 	wtxn := p.Writer.WriteTxn()
-	addr := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(123), 12345, loadbalancer.ScopeExternal)
+	addr := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(123, LocalClusterID), 12345, loadbalancer.ScopeExternal)
 	name := loadbalancer.NewServiceName("test-ns", "test-name")
 	err := p.Writer.UpsertServiceAndFrontends(
 		wtxn,
@@ -360,20 +360,24 @@ func TestWriter_SetBackends(t *testing.T) {
 	name1 := loadbalancer.NewServiceName("test", "test1")
 	name2 := loadbalancer.NewServiceName("test", "test2")
 
-	feAddr1 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1231), 1231, loadbalancer.ScopeExternal)
-	feAddr2 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1232), 1232, loadbalancer.ScopeExternal)
+	feAddr1 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1231, LocalClusterID), 1231, loadbalancer.ScopeExternal)
+	feAddr2 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1232, LocalClusterID), 1232, loadbalancer.ScopeExternal)
 
-	beAddr1 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(121), 4241, loadbalancer.ScopeExternal)
-	beAddr2 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(122), 4242, loadbalancer.ScopeExternal)
-	beAddr3 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(123), 4243, loadbalancer.ScopeExternal)
+	beAddr1 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(121, LocalClusterID), 4241, loadbalancer.ScopeExternal)
+	beAddr2 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(122, LocalClusterID), 4242, loadbalancer.ScopeExternal)
+	beAddr3 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(123, LocalClusterID), 4243, loadbalancer.ScopeExternal)
 
 	backend1 := loadbalancer.BackendParams{Address: beAddr1}
 	backend2 := loadbalancer.BackendParams{Address: beAddr2}
 	backend3 := loadbalancer.BackendParams{Address: beAddr3}
 
-	backend1_cluster1 := loadbalancer.BackendParams{Address: beAddr1, ClusterID: 1}
-	backend2_cluster1 := loadbalancer.BackendParams{Address: beAddr2, ClusterID: 1}
-	backend3_cluster2 := loadbalancer.BackendParams{Address: beAddr3, ClusterID: 2}
+	beAddr1_cluster1 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(121, 1), 4241, loadbalancer.ScopeExternal)
+	beAddr2_cluster1 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(122, 1), 4242, loadbalancer.ScopeExternal)
+	beAddr3_cluster2 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(123, 2), 4243, loadbalancer.ScopeExternal)
+
+	backend1_cluster1 := loadbalancer.BackendParams{Address: beAddr1_cluster1}
+	backend2_cluster1 := loadbalancer.BackendParams{Address: beAddr2_cluster1}
+	backend3_cluster2 := loadbalancer.BackendParams{Address: beAddr3_cluster2}
 
 	type testCase struct {
 		desc   string
@@ -472,9 +476,9 @@ func TestWriter_SetBackends(t *testing.T) {
 				require.NoError(t, w.SetBackendsOfCluster(wtxn, name1, source.ClusterMesh, 2, backend3_cluster2))
 			},
 			references: map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool{
-				name1: {beAddr1: true, beAddr2: true, beAddr3: true},
+				name1: {beAddr1_cluster1: true, beAddr2_cluster1: true, beAddr3_cluster2: true},
 			},
-			existence: map[loadbalancer.L3n4Addr]bool{beAddr1: true, beAddr2: true, beAddr3: true},
+			existence: map[loadbalancer.L3n4Addr]bool{beAddr1_cluster1: true, beAddr2_cluster1: true, beAddr3_cluster2: true},
 		},
 		{
 			desc: "delete backend2 from first cluster",
@@ -483,9 +487,9 @@ func TestWriter_SetBackends(t *testing.T) {
 				require.NoError(t, w.SetBackendsOfCluster(wtxn, name1, source.ClusterMesh, 2, backend3_cluster2))
 			},
 			references: map[loadbalancer.ServiceName]map[loadbalancer.L3n4Addr]bool{
-				name1: {beAddr1: true, beAddr2: false, beAddr3: true},
+				name1: {beAddr1_cluster1: true, beAddr3_cluster2: true},
 			},
-			existence: map[loadbalancer.L3n4Addr]bool{beAddr1: true, beAddr2: false, beAddr3: true},
+			existence: map[loadbalancer.L3n4Addr]bool{beAddr1_cluster1: true, beAddr3_cluster2: true},
 		},
 	}
 
@@ -534,10 +538,10 @@ func TestWriter_WithConflictingSources(t *testing.T) {
 	name1 := loadbalancer.NewServiceName("test", "test1")
 	name2 := loadbalancer.NewServiceName("test", "test2")
 
-	feAddr1 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1234), 1234, loadbalancer.ScopeExternal)
-	feAddr2 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1235), 1235, loadbalancer.ScopeExternal)
+	feAddr1 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1234, LocalClusterID), 1234, loadbalancer.ScopeExternal)
+	feAddr2 := loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(1235, LocalClusterID), 1235, loadbalancer.ScopeExternal)
 
-	backendTemplate := loadbalancer.BackendParams{Address: loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(123), 4242, loadbalancer.ScopeExternal)}
+	backendTemplate := loadbalancer.BackendParams{Address: loadbalancer.NewL3n4Addr(loadbalancer.TCP, intToAddr(123, 0), 4242, loadbalancer.ScopeExternal)}
 	backend10 := backendTemplate
 	backend10.Weight = 10
 	backend11 := backendTemplate