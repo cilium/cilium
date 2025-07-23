1a621ca785a66a3e2ad8a4a37f5a23ffca7ae12b
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2025-07-23 11:15:23 +0100

    bpf: Remove redundant map key fixups in lbX_lookup_service() failure paths.
    Previously, lbX_lookup_service() would zeroise the key protocol field,
    such that callers would	need to	reinstate the key protocol field if their
    logic re-used the key.
    
    Following the previous commit, the lbX_lookup_service()	routines will now
    only zeroise the key protocol (and dport) fields if an external	wild-card
    entry is returned. Thus, the restoration of key values can be removed.
    
    This commit also removes lbX_key_set_protocol() helpers as they became
    broadly unnecessary.
    
    Signed-off-by: Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>

--- bpf/lib/lb.h
+++ bpf/lib/lb.h
@@ -595,16 +595,9 @@ static __always_inline int lb6_rev_nat(struct __ctx_buff *ctx, int l4_off, __u16
 }
 
 static __always_inline void
-lb6_key_set_protocol(struct lb6_key *key __maybe_unused,
-		     __u8 protocol __maybe_unused)
-{
-	key->proto = protocol;
-}
-
-static __always_inline void
 lb6_fill_key(struct lb6_key *key, struct ipv6_ct_tuple *tuple)
 {
-	lb6_key_set_protocol(key, tuple->nexthdr);
+	key->proto = tuple->nexthdr;
 	ipv6_addr_copy(&key->address, &tuple->daddr);
 	key->dport = tuple->sport;
 }
@@ -1219,12 +1212,6 @@ struct lb6_service *lb6_lookup_service(struct lb6_key *key __maybe_unused,
 	return NULL;
 }
 
-static __always_inline void
-lb6_key_set_protocol(struct lb6_key *key __maybe_unused,
-		     __u8 protocol __maybe_unused)
-{
-}
-
 static __always_inline
 struct lb6_service *__lb6_lookup_backend_slot(struct lb6_key *key __maybe_unused)
 {
@@ -1343,16 +1330,9 @@ static __always_inline int lb4_rev_nat(struct __ctx_buff *ctx, int l3_off, int l
 }
 
 static __always_inline void
-lb4_key_set_protocol(struct lb4_key *key __maybe_unused,
-		     __u8 protocol __maybe_unused)
-{
-	key->proto = protocol;
-}
-
-static __always_inline void
 lb4_fill_key(struct lb4_key *key, const struct ipv4_ct_tuple *tuple)
 {
-	lb4_key_set_protocol(key, tuple->nexthdr);
+	key->proto = tuple->nexthdr;
 	key->address = tuple->daddr;
 	/* CT tuple has ports in reverse order: */
 	key->dport = tuple->sport;
