93e011d8d0961e5a2cbb5bd294c5cf4959d9c7a0
Dylan Reimerink <dylan.reimerink@isovalent.com>
2025-07-23 14:13:18 +0200

    datapath: Add route reconciler
    TODO: make a nice description
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

---
+++ pkg/datapath/linux/route/reconciler/reconciler_test.go
@@ -0,0 +1,189 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package reconciler
+
+import (
+	"fmt"
+	"maps"
+	"net/netip"
+	"os"
+	"strconv"
+	"testing"
+
+	"go.uber.org/goleak"
+	"go.yaml.in/yaml/v3"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/hivetest"
+	"github.com/cilium/hive/script"
+	"github.com/cilium/hive/script/scripttest"
+	"github.com/cilium/statedb"
+	"github.com/stretchr/testify/require"
+
+	"github.com/cilium/cilium/pkg/datapath/linux"
+	"github.com/cilium/cilium/pkg/datapath/tables"
+	ciliumhive "github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/testutils"
+	"github.com/cilium/cilium/pkg/testutils/scriptnet"
+)
+
+func TestPrivilegedScript(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	defer goleak.VerifyNone(t)
+
+	scripttest.Test(t,
+		t.Context(),
+		func(t testing.TB, args []string) *script.Engine {
+			nsManager, err := scriptnet.NewNSManager(t)
+			require.NoError(t, err, "NewNSManager")
+
+			err = nsManager.LockThreadAndInitialize(t, true)
+			require.NoError(t, err, "LockThreadAndInitialize")
+
+			var (
+				drm    *DesiredRouteManager
+				devTbl statedb.Table[*tables.Device]
+			)
+			h := ciliumhive.New(
+				Cell,
+				linux.DevicesControllerCell,
+				cell.Provide(func() *option.DaemonConfig { return &option.DaemonConfig{} }),
+				cell.Invoke(func(m *DesiredRouteManager, devices statedb.Table[*tables.Device]) {
+					drm = m
+					devTbl = devices
+				}),
+			)
+
+			log := hivetest.Logger(t)
+			cmds, err := h.ScriptCommands(log)
+			require.NoError(t, err, "ScriptCommands")
+
+			maps.Insert(cmds, maps.All(nsManager.Commands()))
+			maps.Insert(cmds, maps.All(script.DefaultCmds()))
+			maps.Insert(cmds, maps.All(testDesiredRouteCmds(drm, devTbl)))
+
+			e := &script.Engine{
+				Cmds: cmds,
+			}
+			return e
+		}, []string{"PATH=" + os.Getenv("PATH")}, "testdata/*.txtar")
+}
+
+func testDesiredRouteCmds(drm *DesiredRouteManager, devTbl statedb.Table[*tables.Device]) map[string]script.Cmd {
+	return map[string]script.Cmd{
+		"add-owner": script.Command(script.CmdUsage{
+			Summary: "Adds a new route owner",
+			Args:    "name adminDistance",
+		}, func(state *script.State, args ...string) (script.WaitFunc, error) {
+			if len(args) != 2 {
+				return nil, script.ErrUsage
+			}
+
+			ad, err := strconv.Atoi(args[1])
+			if err != nil {
+				return nil, fmt.Errorf("invalid admin distance %q: %w", args[1], err)
+			}
+
+			if _, err := drm.RegisterOwner(args[0], AdminDistance(ad)); err != nil {
+				return nil, fmt.Errorf("failed to register owner %q: %w", args, err)
+			}
+			return nil, nil
+		}),
+		"add-route": script.Command(script.CmdUsage{
+			Summary: "Adds a new route",
+			Args:    "owner route-file",
+		}, func(state *script.State, args ...string) (script.WaitFunc, error) {
+			if len(args) != 2 {
+				return nil, script.ErrUsage
+			}
+
+			owner, err := drm.GetOwner(args[0])
+			if err != nil {
+				return nil, fmt.Errorf("failed to get owner %q: %w", args[0], err)
+			}
+
+			routeFile, err := os.ReadFile(state.Path(args[1]))
+			if err != nil {
+				return nil, fmt.Errorf("failed to read route file %q: %w", args[1], err)
+			}
+
+			type desiredRoute struct {
+				Owner         *RouteOwner
+				Table         TableID
+				Prefix        netip.Prefix
+				Priority      uint32
+				Nexthop       netip.Addr
+				Src           netip.Addr
+				Device        string
+				DeviceIfIndex int `yaml:"deviceIfIndex"`
+				MTU           uint32
+				Scope         Scope
+				Type          Type
+			}
+
+			var route desiredRoute
+			if err := yaml.Unmarshal(routeFile, &route); err != nil {
+				return nil, fmt.Errorf("failed to unmarshal route file %q: %w", args[1], err)
+			}
+
+			var dev *tables.Device
+			if route.Device != "" || route.DeviceIfIndex != 0 {
+				var q statedb.Query[*tables.Device]
+				if route.Device != "" {
+					q = tables.DeviceNameIndex.Query(route.Device)
+				} else if route.DeviceIfIndex != 0 {
+					q = tables.DeviceIDIndex.Query(route.DeviceIfIndex)
+				}
+
+				var found bool
+				dev, _, found = devTbl.Get(drm.db.ReadTxn(), q)
+				if !found {
+					if route.Device != "" {
+						return nil, fmt.Errorf("device %q not found", route.Device)
+					} else if route.DeviceIfIndex != 0 {
+						return nil, fmt.Errorf("device with index %d not found", route.DeviceIfIndex)
+					}
+				}
+			}
+
+			if err := drm.UpsertRoutes(DesiredRoute{
+				Owner:    owner,
+				Table:    route.Table,
+				Prefix:   route.Prefix,
+				Priority: route.Priority,
+				Nexthop:  route.Nexthop,
+				Device:   dev,
+				Src:      route.Src,
+				MTU:      route.MTU,
+				Scope:    route.Scope,
+				Type:     route.Type,
+			}); err != nil {
+				return nil, fmt.Errorf("failed to upsert routes for owner %q: %w", args[0], err)
+			}
+
+			return nil, nil
+		}),
+		"remove-owner": script.Command(script.CmdUsage{
+			Summary: "Removes a route owner",
+			Args:    "name",
+		}, func(state *script.State, args ...string) (script.WaitFunc, error) {
+			if len(args) != 1 {
+				return nil, script.ErrUsage
+			}
+
+			owner, err := drm.GetOwner(args[0])
+			if err != nil {
+				return nil, fmt.Errorf("failed to get owner %q: %w", args[0], err)
+			}
+
+			if err := drm.RemoveOwner(owner); err != nil {
+				return nil, fmt.Errorf("failed to remove owner %q: %w", args[0], err)
+			}
+
+			return nil, nil
+		}),
+	}
+}
---
+++ pkg/datapath/linux/route/reconciler/table.go
@@ -0,0 +1,276 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package reconciler
+
+import (
+	"fmt"
+	"net/netip"
+	"strconv"
+	"strings"
+
+	"github.com/cilium/statedb"
+	"github.com/cilium/statedb/index"
+	"github.com/cilium/statedb/reconciler"
+
+	"github.com/cilium/cilium/pkg/datapath/tables"
+)
+
+var _ statedb.TableWritable = &DesiredRoute{}
+
+type DesiredRouteKey struct {
+	Owner    *RouteOwner
+	Table    TableID
+	Prefix   netip.Prefix
+	Priority uint32
+}
+
+func (k DesiredRouteKey) Key() index.Key {
+	var key []byte
+	if k.Owner != nil {
+		key = index.String(k.Owner.name)
+		// Owner name is variable, insert a 0x00 byte to separate it from the rest of the key.
+		key = append(key, 0x00)
+	}
+
+	if k.Table != 0 {
+		key = append(key, index.Uint32(uint32(k.Table))...)
+	}
+
+	if k.Prefix.IsValid() {
+		key = append(key, index.NetIPAddr(k.Prefix.Addr())...)
+		key = append(key, uint8(k.Prefix.Bits()))
+	}
+
+	if k.Priority != 0 {
+		key = append(key, index.Uint32(k.Priority)...)
+	}
+
+	return key
+}
+
+func (k DesiredRouteKey) String() string {
+	parts := []string{k.Owner.String(), k.Table.String(), k.Prefix.String()}
+	if k.Priority != 0 {
+		parts = append(parts, strconv.FormatUint(uint64(k.Priority), 10))
+	}
+	return strings.Join(parts, ":")
+}
+
+type DesiredRoute struct {
+	// Composite primary key for the route.
+	Owner    *RouteOwner
+	Table    TableID
+	Prefix   netip.Prefix
+	Priority uint32
+
+	// If true, the route is selected for installation, a calculated property.
+	selected bool
+
+	// Optional, if [netip.Addr.IsValid] then nexthop is specified.
+	Nexthop netip.Addr
+	// Optional, if [netip.Addr.IsValid] then source address is specified.
+	Src netip.Addr
+	// Optional, non-nil if device is specified.
+	Device *tables.Device
+	// Optional, if 0 no MTU is specified.
+	MTU uint32
+	// Optional, if 0 no scope is specified.
+	Scope Scope
+	// Optional, if 0 no type is specified.
+	Type Type
+
+	status reconciler.Status
+}
+
+func (dr *DesiredRoute) GetFullKey() DesiredRouteKey {
+	return DesiredRouteKey{
+		Owner:    dr.Owner,
+		Table:    dr.Table,
+		Prefix:   dr.Prefix,
+		Priority: dr.Priority,
+	}
+}
+
+func (dr *DesiredRoute) GetOwnerlessKey() DesiredRouteKey {
+	return DesiredRouteKey{
+		Table:    dr.Table,
+		Prefix:   dr.Prefix,
+		Priority: dr.Priority,
+	}
+}
+
+func (dr *DesiredRoute) TableHeader() []string {
+	return []string{
+		"Owner",
+		"Table",
+		"Prefix",
+		"Priority",
+		"Selected",
+		"Nexthop",
+		"Src",
+		"Device",
+		"MTU",
+		"Scope",
+		"Type",
+		"Status",
+	}
+}
+
+func (dr *DesiredRoute) TableRow() []string {
+	row := []string{
+		dr.Owner.String(),
+		dr.Table.String(),
+		dr.Prefix.String(),
+	}
+
+	if dr.Priority == 0 {
+		row = append(row, "none")
+	} else {
+		row = append(row, strconv.FormatUint(uint64(dr.Priority), 10))
+	}
+
+	row = append(row, strconv.FormatBool(dr.selected))
+
+	if !dr.Nexthop.IsValid() {
+		row = append(row, "none")
+	} else {
+		row = append(row, dr.Nexthop.String())
+	}
+
+	if !dr.Src.IsValid() {
+		row = append(row, "none")
+	} else {
+		row = append(row, dr.Src.String())
+	}
+
+	if dr.Device == nil {
+		row = append(row, "none")
+	} else {
+		row = append(row, dr.Device.Name+" ("+strconv.Itoa(dr.Device.Index)+")")
+	}
+
+	if dr.MTU == 0 {
+		row = append(row, "none")
+	} else {
+		row = append(row, strconv.FormatUint(uint64(dr.MTU), 10))
+	}
+
+	row = append(row, dr.Scope.String())
+	row = append(row, dr.Type.String())
+	row = append(row, dr.status.String())
+
+	return row
+}
+
+func (dr *DesiredRoute) GetStatus() reconciler.Status {
+	return dr.status
+}
+
+func (dr *DesiredRoute) SetStatus(s reconciler.Status) *DesiredRoute {
+	dr.status = s
+	return dr
+}
+
+func (dr *DesiredRoute) Clone() *DesiredRoute {
+	dr2 := *dr
+	return &dr2
+}
+
+var (
+	DesiredRouteIndex = statedb.Index[*DesiredRoute, DesiredRouteKey]{
+		Name: "id",
+		FromObject: func(d *DesiredRoute) index.KeySet {
+			return index.NewKeySet(d.GetFullKey().Key())
+		},
+		FromKey: DesiredRouteKey.Key,
+		FromString: func(s string) (index.Key, error) {
+			parts := strings.Split(s, ":")
+			if len(parts) > 3 {
+				return nil, fmt.Errorf("bad key, expected \"owner[:table[:prefix[:priority]]]\", got %s", s)
+			}
+
+			var key DesiredRouteKey
+			if len(parts) >= 2 {
+				if err := key.Table.FromString(parts[1]); err != nil {
+					return nil, fmt.Errorf("bad key, expected \"owner[:table[:prefix[:priority]]]\", got %s: %w", s, err)
+				}
+			}
+
+			if len(parts) >= 3 {
+				var err error
+				key.Prefix, err = netip.ParsePrefix(parts[2])
+				if err != nil {
+					return nil, fmt.Errorf("bad key, expected \"owner[:table[:prefix[:priority]]]\", got %s: %w", s, err)
+				}
+			}
+
+			if len(parts) >= 4 {
+				prio, err := strconv.ParseUint(parts[3], 10, 32)
+				if err != nil {
+					return nil, fmt.Errorf("bad key, expected \"owner[:table[:prefix[:priority]]]\", got %s: %w", s, err)
+				}
+				key.Priority = uint32(prio)
+			}
+
+			return key.Key(), nil
+		},
+		Unique: true,
+	}
+
+	DesiredRouteTablePrefixIndex = statedb.Index[*DesiredRoute, DesiredRouteKey]{
+		Name: "table-prefix",
+		FromObject: func(d *DesiredRoute) index.KeySet {
+			return index.NewKeySet(d.GetOwnerlessKey().Key())
+		},
+		FromKey: func(key DesiredRouteKey) index.Key {
+			key.Owner = nil // Owner is not part of the key for this index
+			return key.Key()
+		},
+		FromString: func(s string) (index.Key, error) {
+			// The string is expected to be in the format "table:prefix[:priority]"
+			parts := strings.Split(s, ":")
+
+			var key DesiredRouteKey
+			if err := key.Table.FromString(parts[0]); err != nil {
+				return nil, fmt.Errorf("bad key, expected \"table:prefix:priority\", got %s: %w", s, err)
+			}
+
+			if len(parts) < 2 {
+				return key.Key(), nil
+			}
+
+			var err error
+			key.Prefix, err = netip.ParsePrefix(parts[1])
+			if err != nil {
+				return nil, err
+			}
+
+			if len(parts) < 3 {
+				return key.Key(), nil
+			}
+
+			prio, err := strconv.ParseUint(parts[2], 10, 32)
+			if err != nil {
+				return nil, fmt.Errorf("bad key, expected \"table:prefix:priority\", got %s: %w", s, err)
+			}
+			key.Priority = uint32(prio)
+
+			return key.Key(), nil
+		},
+		Unique: false,
+	}
+)
+
+func newDesiredRouteTable(db *statedb.DB) (statedb.RWTable[*DesiredRoute], error) {
+	tbl, err := statedb.NewTable(
+		"desired-routes",
+		DesiredRouteIndex,
+		DesiredRouteTablePrefixIndex,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return tbl, db.RegisterTable(tbl)
+}
