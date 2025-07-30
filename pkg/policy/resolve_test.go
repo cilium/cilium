cilium/cilium

commit fe165aaa43d3eb81192c3c70e0636f4fed157eff

endpoint, policy: Don't accidentally clear out endpoint policy maps
Transient errors inside regenerateBPF can lead to an endpoint's policy
map either not being initialized or being accidentally cleared out,
resulting in broken connectivity.

Scenario
========
In this scenario, a transient error causes the first call to
regenerateBPF to fail for an endpoint. The second regeneration attempt
succeeds, but the final policy map state is empty.

initial state:
    e.desiredPolicy == e.realizedPolicy == <empty EndpointPolicy>

0:  e.ComputeInitialPolicy(...)
1:      e.regeneratePolicy(...)
2:          result := &policyGenerateResult{
                endpointPolicy:   e.desiredPolicy,
                identityRevision: e.identityRevision,
            }
3:          selectorPolicy, ... = e.policyRepo.GetSelectorPolicy(...)
            ...
4:          result.endpointPolicy = selectorPolicy.DistillPolicy(...)
            ...
5:      e.setDesiredPolicy(...)
6:          e.setNextPolicyRevision(res.policyRevision)
7:              e.nextPolicyRevision = revision
8:          if ... && res.endpointPolicy != e.desiredPolicy {
                ...
9:              e.desiredPolicy = res.endpointPolicy
10:             datapathRegenCtx.revertStack.Push(func() error {
                    ...
                    e.desiredPolicy = e.realizedPolicy
                    ...
                    _, _, err = e.syncPolicyMapWith(currentMap, false);
                    ...
                })
            }
12:         res.endpointPolicy = nil

current state:
    e.desiredPolicy = <computed policy>
    e.realizedPolicy = <empty policy>

13: e.regenerate(...)
14:     revision, err := e.regenerateBPF(...)
            e.runPreCompilationSteps(regenContext)
	        e.regeneratePolicy(...)
		    ...
		    selectorPolicy, ... = e.policyRepo.GetSelectorPolicy(...)
		    <no-op, since selectorPolicy == nil>
                e.setDesiredPolicy(...) <no-op - e.desiredPolicy matches

pkg/policy/resolve.go
@@ -249,6 +249,15 @@ func (p *EndpointPolicy) Lookup(key Key) (MapStateEntry, RuleMeta, bool) {
 	return entry.MapStateEntry, entry.derivedFromRules.Value(), found
 }
 
+// CopyMapStateFrom copies the policy map entries from m.
+func (p *EndpointPolicy) CopyMapStateFrom(m MapStateMap) {
+	for key, entry := range m {
+		p.policyMapState.entries[key] = mapStateEntry{
+			MapStateEntry: entry,
+		}
+	}
+}
+
 // PolicyOwner is anything which consumes a EndpointPolicy.
 type PolicyOwner interface {
 	GetID() uint64

pkg/endpoint/bpf.go
@@ -801,6 +801,14 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (pr
 			if err != nil {
 				return fmt.Errorf("policymap synchronization failed: %w", err)
 			}
+		} else {
+			// Ensure that e.realizedPolicy actually represents the
+			// current policy map state in case rollback is
+			// necessary, so we don't try to "roll back" to an empty
+			// map and delete all the entries, even momentarily.
+			// This may be the case if the agent just restarted,
+			// for example. See GH-38998.
+			e.realizedPolicy.CopyMapStateFrom(datapathRegenCtxt.policyMapDump)
 		}
 		datapathRegenCtxt.policyMapSyncDone = true
 	}

pkg/endpoint/policy.go
@@ -309,6 +309,7 @@ func (e *Endpoint) setDesiredPolicy(datapathRegenCtxt *datapathRegenerationConte
 		return fmt.Errorf("endpoint %d SecurityIdentity changed during policy regeneration", e.ID)
 	}
 
+	oldNextPolicyRevision := e.nextPolicyRevision
 	// Set the revision of this endpoint to the current revision of the policy
 	// repository.
 	e.setNextPolicyRevision(res.policyRevision)
@@ -332,6 +333,12 @@ func (e *Endpoint) setDesiredPolicy(datapathRegenCtxt *datapathRegenerationConte
 		datapathRegenCtxt.revertStack.Push(func() error {
 			// Do nothing if e.policyMap was not initialized already
 			if e.policyMap != nil && e.desiredPolicy != e.realizedPolicy {
+				// Revert nextPolicyRevision; otherwise,
+				// res.endpointPolicy will not be recalculated
+				// on the next regeneration attempt, and we
+				// won't advance to the true desired policy map
+				// state. See GH-38998.
+				e.setNextPolicyRevision(oldNextPolicyRevision)
 				e.desiredPolicy.Detach(e.getLogger())
 				e.desiredPolicy = e.realizedPolicy
 

commit dd9903188c3c540f7800ab21e96b9b8ecf3e43fd

policy / hubble: fix segfault on startup when hubble enabled
In the event that a regeneration failed, we re-populated the MapState
but forgot to set the rule origin. Unfortunately, the zero value for
Handle is not equivalent to nil; we must always set it explicitly. Use
the constructor instead.

Fixes: fe165aaa43

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

pkg/policy/resolve.go
@@ -252,9 +252,7 @@ func (p *EndpointPolicy) Lookup(key Key) (MapStateEntry, RuleMeta, bool) {
 // CopyMapStateFrom copies the policy map entries from m.
 func (p *EndpointPolicy) CopyMapStateFrom(m MapStateMap) {
 	for key, entry := range m {
-		p.policyMapState.entries[key] = mapStateEntry{
-			MapStateEntry: entry,
-		}
+		p.policyMapState.entries[key] = NewMapStateEntry(entry)
 	}
 }
 

pkg/policy/resolve_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/cilium/cilium/pkg/policy/types"
 	"github.com/cilium/cilium/pkg/u8proto"
 )
 
@@ -953,4 +954,50 @@ func TestEndpointPolicy_AllowsIdentity(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestEndpointPolicy_GetRuleMeta(t *testing.T) {
+	log := hivetest.Logger(t)
+
+	key1 := ingressKey(192, 6, 80, 0)
+	key2 := ingressKey(193, 6, 80, 0)
+
+	lbls := labels.ParseLabelArray("k8s:k=v")
+	lblss := labels.LabelArrayList{lbls}
+	logstr := "log"
+	logstrs := []string{logstr}
+
+	// test empty map state
+	p := &EndpointPolicy{
+		policyMapState: emptyMapState(log),
+	}
+	_, err := p.GetRuleMeta(key1)
+	require.Error(t, err)
+
+	// test non-empty mapstate
+	p.policyMapState = emptyMapState(log).withState(mapStateMap{
+		key1: newMapStateEntry(makeSingleRuleOrigin(lbls, logstr), 0, 0, false, NoAuthRequirement),
+	})
+
+	rm, err := p.GetRuleMeta(key1)
+	require.NoError(t, err)
+	require.Equal(t, rm.LabelArray(), lblss)
+	require.Equal(t, rm.Log(), logstrs)
+
+	_, err = p.GetRuleMeta(key2)
+	require.Error(t, err)
+
+	// test mapstate from dump
+	msDump := MapStateMap{
+		key1: types.NewMapStateEntry(false, 0, 0, NoAuthRequirement),
+	}
+
+	p = &EndpointPolicy{
+		policyMapState: emptyMapState(log),
+	}
+
+	p.CopyMapStateFrom(msDump)
+	rm, err = p.GetRuleMeta(key1)
+	require.NoError(t, err)
+	require.Equal(t, NilRuleOrigin.Value(), rm)
 }