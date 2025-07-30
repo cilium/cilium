cilium/cilium

commit 226a978e50c2174f17ecb89816ecf913b4c1991c

identity: Allow local identity for ingress label
The ingress label is for L7 LB with cilium proxy, which is running on
every node. So it's not necessary to be global identity, but local
identity instead.

Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/identity/identity.go
@@ -202,6 +202,13 @@ func ScopeForLabels(lbls labels.Labels) NumericIdentity {
 		return IdentityScopeRemoteNode
 	}
 
+	// The ingress label is for L7 LB with cilium proxy, which is running on
+	// every node. So it's not necessary to be global identity, but local
+	// identity instead.
+	if lbls.HasIngressLabel() {
+		return IdentityScopeLocal
+	}
+
 	for _, label := range lbls {
 		switch label.Source {
 		case labels.LabelSourceCIDR, labels.LabelSourceFQDN, labels.LabelSourceReserved, labels.LabelSourceCIDRGroup:

pkg/identity/cache/allocation_test.go
@@ -632,9 +632,11 @@ func testAllocateLocally(t *testing.T, testConfig testConfig) {
 
 	cidrLbls := labels.NewLabelsFromSortedList("cidr:1.2.3.4/32")
 	podLbls := labels.NewLabelsFromSortedList("k8s:foo=bar")
+	ingressLbls := labels.NewLabelsFromSortedList("reserved:ingress;k8s:foo=bar")
 
 	assert.False(t, needsGlobalIdentity(cidrLbls))
 	assert.True(t, needsGlobalIdentity(podLbls))
+	assert.False(t, needsGlobalIdentity(ingressLbls))
 
 	id, allocated, err := mgr.AllocateLocalIdentity(cidrLbls, false, identity.IdentityScopeLocal+50)
 	assert.NoError(t, err)
@@ -645,6 +647,12 @@ func testAllocateLocally(t *testing.T, testConfig testConfig) {
 	id, _, err = mgr.AllocateLocalIdentity(podLbls, false, 0)
 	assert.ErrorIs(t, err, ErrNonLocalIdentity)
 	assert.Nil(t, id)
+
+	id, _, err = mgr.AllocateLocalIdentity(ingressLbls, false, 0)
+	assert.NoError(t, err)
+	assert.True(t, allocated)
+	assert.Equal(t, identity.IdentityScopeLocal, id.ID.Scope())
+	assert.Equal(t, identity.IdentityScopeLocal+1, id.ID)
 }
 
 func TestCheckpointRestore(t *testing.T) {

commit 9e3f98117fb95ddd97c961738d4eedb7eed73726

identity: Ensure ingress label is reserved label to be node-local
If this check is not present, then Cilium consider global identity to be
node-local if there is a label called "ingress" in the label set.

For example, a workload pod could have the label set:

```
k8s:foo=bar
k8s:ingress=allowed
```

and Cilium will incorrectly assign it a node-local identity because the
"ingress" label is present, without checking the source. Hence why we
need to add a check for the reserved source.

A unit test is added to validate this behavior.

Fixes: 226a978e50 ("identity: Allow local identity for ingress label")
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/identity/identity.go
@@ -205,7 +205,7 @@ func ScopeForLabels(lbls labels.Labels) NumericIdentity {
 	// The ingress label is for L7 LB with cilium proxy, which is running on
 	// every node. So it's not necessary to be global identity, but local
 	// identity instead.
-	if lbls.HasIngressLabel() {
+	if lbls.IsReserved() && lbls.HasIngressLabel() {
 		return IdentityScopeLocal
 	}
 

pkg/identity/identity_test.go
@@ -116,6 +116,10 @@ func TestScopeForLabels(t *testing.T) {
 			lbls:  labels.NewLabelsFromModel([]string{"reserved:remote-node", "reserved:kube-apiserver"}),
 			scope: IdentityScopeRemoteNode,
 		},
+		{
+			lbls:  labels.NewLabelsFromModel([]string{"k8s:ingress=allowed"}),
+			scope: IdentityScopeGlobal,
+		},
 	}
 
 	for i, test := range tests {