01f97b7efc9e13c8f0ac04e9bfd96163ab52282b
Chris Tarazi <chris@isovalent.com>
2025-07-29 22:18:22 -0700

    policy: Trigger policy computation on policy import
    This commit triggers policy computation asynchronously which will insert
    or update the SelectorPolicy for every identity on the node into the
    policy-computations table.
    
    Then after this is kicked off, endpoint regeneration is triggered. In
    the subsequent commit, endpoint regeneration will read from the
    policy-computations table that was just written to.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/policy/compute/cell.go
+++ pkg/policy/compute/cell.go
@@ -5,12 +5,14 @@ package compute
 
 import (
 	"log/slog"
+	"testing"
 
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
 	"github.com/cilium/statedb"
 	"github.com/cilium/stream"
 
+	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/identity/identitymanager"
 	"github.com/cilium/cilium/pkg/policy"
 )
@@ -85,4 +87,36 @@ type Params struct {
 	IDManager identitymanager.IDManager
 
 	PolicyCacheEvents stream.Observable[policy.PolicyCacheChange]
+}
+
+func InstantiateCellForTesting(tb testing.TB, logger *slog.Logger, id, desc string, repo policy.PolicyRepository, idmgr identitymanager.IDManager) PolicyRecomputer {
+	tb.Helper()
+
+	var pc PolicyRecomputer
+
+	hive.New(
+		cell.Module(id, desc,
+			cell.Invoke(
+				func(c_ PolicyRecomputer) error {
+					pc = c_
+					return nil
+				},
+			),
+
+			cell.ProvidePrivate(func() (policy.PolicyRepository, stream.Observable[policy.PolicyCacheChange]) {
+				return repo, repo.PolicyCacheObservable()
+			}),
+			cell.ProvidePrivate(func() identitymanager.IDManager { return idmgr }),
+
+			cell.Provide(
+				func(params Params) PolicyRecomputer {
+					return NewIdentityPolicyComputer(params)
+				},
+			),
+			cell.Provide(NewPolicyComputationTable),
+			cell.Provide(statedb.RWTable[Result].ToTable),
+		),
+	).Populate(logger)
+
+	return pc
 }
