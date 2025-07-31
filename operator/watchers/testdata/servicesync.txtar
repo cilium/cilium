cilium/cilium

commit 12ae78bbb2abbb5b7b7b9305e333c16b91e7fbf2

k8s,loadbalancer: Optimize 'k8s.Backend.Ports' to reduce allocations
Store the ports key'd by the L4 address instead of by the port name to avoid
the shuffling in pkg/loadbalancer/reflector. This gets rid of the temporary
data structures.

With this, merge UpsertBackends and ReleaseBackends into UpsertAndReleaseBackends for
fewer RefreshFrontend() calls

Before:
Memory statistics from N=10 iterations:
Min: Allocated 743006kB in total, 2187723 objects / 157600kB still reachable (per service:  43 objs, 15216B alloc,  3227B in-use)
Avg: Allocated 760234kB in total, 2774655 objects / 218753kB still reachable (per service:  55 objs, 15569B alloc,  4480B in-use)
Max: Allocated 797163kB in total, 3439048 objects / 300599kB still reachable (per service:  68 objs, 16325B alloc,  6156B in-use)

After:
Memory statistics from N=10 iterations:
Min: Allocated 658130kB in total, 2087920 objects / 125124kB still reachable (per service:  41 objs, 13478B alloc,  2562B in-use)
Avg: Allocated 679615kB in total, 2651179 objects / 185077kB still reachable (per service:  53 objs, 13918B alloc,  3790B in-use)
Max: Allocated 721163kB in total, 3289507 objects / 267704kB still reachable (per service:  65 objs, 14769B alloc,  5482B in-use)

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/k8s/endpoints.go
@@ -6,6 +6,7 @@ package k8s
 import (
 	"fmt"
 	"log/slog"
+	"maps"
 	"net"
 	"net/netip"
 	"slices"
@@ -100,15 +101,35 @@ func (in *Endpoints) DeepCopy() *Endpoints {
 // Backend contains all ports, terminating state, and the node name of a given backend
 //
 // +k8s:deepcopy-gen=true
-// +deepequal-gen=true
+// +deepequal-gen=false
 type Backend struct {
-	Ports         serviceStore.PortConfiguration
+	Ports         map[loadbalancer.L4Addr][]string
 	NodeName      string
 	Hostname      string
 	Terminating   bool
 	HintsForZones []string
 	Preferred     bool
 	Zone          string
+}
+
+func (b *Backend) DeepEqual(other *Backend) bool {
+	return maps.EqualFunc(b.Ports, other.Ports, slices.Equal) &&
+		b.NodeName == other.NodeName &&
+		b.Hostname == other.Hostname &&
+		b.Terminating == other.Terminating &&
+		slices.Equal(b.HintsForZones, other.HintsForZones) &&
+		b.Preferred == other.Preferred &&
+		b.Zone == other.Zone
+}
+
+func (b *Backend) ToPortConfiguration() serviceStore.PortConfiguration {
+	pc := serviceStore.PortConfiguration{}
+	for addr, names := range b.Ports {
+		for _, name := range names {
+			pc[name] = &addr
+		}
+	}
+	return pc
 }
 
 // String returns the string representation of an endpoints resource, with
@@ -120,7 +141,7 @@ func (e *Endpoints) String() string {
 
 	backends := []string{}
 	for addrCluster, be := range e.Backends {
-		for _, port := range be.Ports {
+		for port := range be.Ports {
 			if be.Zone != "" {
 				backends = append(backends, fmt.Sprintf("%s/%s[%s]", net.JoinHostPort(addrCluster.Addr().String(), strconv.Itoa(int(port.Port))), port.Protocol, be.Zone))
 			} else {
@@ -176,7 +197,7 @@ func ParseEndpoints(ep *slim_corev1.Endpoints) *Endpoints {
 
 			backend, ok := endpoints.Backends[addrCluster]
 			if !ok {
-				backend = &Backend{Ports: serviceStore.PortConfiguration{}}
+				backend = &Backend{Ports: map[loadbalancer.L4Addr][]string{}}
 				endpoints.Backends[addrCluster] = backend
 			}
 
@@ -186,8 +207,12 @@ func ParseEndpoints(ep *slim_corev1.Endpoints) *Endpoints {
 			backend.Hostname = addr.Hostname
 
 			for _, port := range sub.Ports {
-				lbPort := loadbalancer.NewL4Addr(loadbalancer.L4Type(port.Protocol), uint16(port.Port))
-				backend.Ports[port.Name] = lbPort
+				lbPort := *loadbalancer.NewL4Addr(loadbalancer.L4Type(port.Protocol), uint16(port.Port))
+				if port.Name != "" {
+					backend.Ports[lbPort] = append(backend.Ports[lbPort], port.Name)
+				} else {
+					backend.Ports[lbPort] = nil
+				}
 			}
 		}
 	}
@@ -257,7 +282,7 @@ func ParseEndpointSliceV1Beta1(ep *slim_discovery_v1beta1.EndpointSlice) *Endpoi
 
 			backend, ok := endpoints.Backends[addrCluster]
 			if !ok {
-				backend = &Backend{Ports: serviceStore.PortConfiguration{}}
+				backend = &Backend{Ports: map[loadbalancer.L4Addr][]string{}}
 				endpoints.Backends[addrCluster] = backend
 				if nodeName, ok := sub.Topology[corev1.LabelHostname]; ok {
 					backend.NodeName = nodeName
@@ -277,7 +302,11 @@ func ParseEndpointSliceV1Beta1(ep *slim_discovery_v1beta1.EndpointSlice) *Endpoi
 			for _, port := range ep.Ports {
 				name, lbPort := parseEndpointPortV1Beta1(port)
 				if lbPort != nil {
-					backend.Ports[name] = lbPort
+					if name != "" {
+						backend.Ports[*lbPort] = append(backend.Ports[*lbPort], name)
+					} else {
+						backend.Ports[*lbPort] = nil
+					}
 				}
 			}
 		}
@@ -377,7 +406,7 @@ func ParseEndpointSliceV1(logger *slog.Logger, ep *slim_discovery_v1.EndpointSli
 
 			backend, ok := endpoints.Backends[addrCluster]
 			if !ok {
-				backend = &Backend{Ports: serviceStore.PortConfiguration{}}
+				backend = &Backend{Ports: map[loadbalancer.L4Addr][]string{}}
 				endpoints.Backends[addrCluster] = backend
 				if sub.NodeName != nil {
 					backend.NodeName = *sub.NodeName
@@ -410,7 +439,11 @@ func ParseEndpointSliceV1(logger *slog.Logger, ep *slim_discovery_v1.EndpointSli
 			for _, port := range ep.Ports {
 				name, lbPort := parseEndpointPortV1(port)
 				if lbPort != nil {
-					backend.Ports[name] = lbPort
+					if name != "" {
+						backend.Ports[*lbPort] = append(backend.Ports[*lbPort], name)
+					} else {
+						backend.Ports[*lbPort] = nil
+					}
 				}
 			}
 			if sub.Hints != nil && (*sub.Hints).ForZones != nil {
@@ -494,7 +527,7 @@ func (es *EndpointSlices) GetEndpoints() *Endpoints {
 				allEps.Backends[backend] = ep.DeepCopy()
 			} else {
 				for k, v := range ep.Ports {
-					b.Ports[k] = v.DeepCopy()
+					b.Ports[k] = slices.Clone(v)
 				}
 			}
 		}

pkg/loadbalancer/reflectors/conversions.go
@@ -397,69 +397,54 @@ func getIPFamilies(svc *slim_corev1.Service) []slim_corev1.IPFamily {
 	return svc.Spec.IPFamilies
 }
 
-func convertEndpoints(rawlog *slog.Logger, cfg loadbalancer.ExternalConfig, svcName loadbalancer.ServiceName, bes iter.Seq2[cmtypes.AddrCluster, *k8s.Backend]) (out []loadbalancer.BackendParams) {
-	// Lazily construct the augmented logger as we very rarely log here.
-	log := sync.OnceValue(func() *slog.Logger {
-		return rawlog.With(
-			logfields.Service, svcName.Name,
-			logfields.K8sNamespace, svcName.Namespace,
-		)
-	})
-
-	// k8s.Endpoints may have the same backend address multiple times
-	// with a different port name. Collapse them down into single
-	// entry.
-	type entry struct {
-		portNames []string
-		backend   *k8s.Backend
-	}
-	entries := map[loadbalancer.L3n4Addr]entry{}
-
-	for addrCluster, be := range bes {
-		if (!cfg.EnableIPv6 && addrCluster.Is6()) || (!cfg.EnableIPv4 && addrCluster.Is4()) {
-			log().Debug(
-				"Skipping Backend due to disabled IP family",
-				logfields.IPv4, cfg.EnableIPv4,
-				logfields.IPv6, cfg.EnableIPv6,
-				logfields.Address, addrCluster,
+func convertEndpoints(rawlog *slog.Logger, cfg loadbalancer.ExternalConfig, svcName loadbalancer.ServiceName, bes iter.Seq2[cmtypes.AddrCluster, *k8s.Backend]) iter.Seq[loadbalancer.BackendParams] {
+	return func(yield func(be loadbalancer.BackendParams) bool) {
+		// Lazily construct the augmented logger as we very rarely log here.
+		log := sync.OnceValue(func() *slog.Logger {
+			return rawlog.With(
+				logfields.Service, svcName.Name,
+				logfields.K8sNamespace, svcName.Namespace,
 			)
-			continue
-		}
-		for portName, l4Addr := range be.Ports {
-			l3n4Addr := loadbalancer.L3n4Addr{
-				AddrCluster: addrCluster,
-				L4Addr:      *l4Addr,
-			}
-			if isIngressDummyEndpoint(l3n4Addr) {
+		})
+
+		for addrCluster, be := range bes {
+			if (!cfg.EnableIPv6 && addrCluster.Is6()) || (!cfg.EnableIPv4 && addrCluster.Is4()) {
+				log().Debug(
+					"Skipping Backend due to disabled IP family",
+					logfields.IPv4, cfg.EnableIPv4,
+					logfields.IPv6, cfg.EnableIPv6,
+					logfields.Address, addrCluster,
+				)
 				continue
 			}
-			portNames := entries[l3n4Addr].portNames
-			if portName != "" {
-				portNames = append(portNames, portName)
-			}
-			entries[l3n4Addr] = entry{
-				portNames: portNames,
-				backend:   be,
+			for l4Addr, portNames := range be.Ports {
+				l3n4Addr := loadbalancer.L3n4Addr{
+					AddrCluster: addrCluster,
+					L4Addr:      l4Addr,
+				}
+				if isIngressDummyEndpoint(l3n4Addr) {
+					continue
+				}
+
+				state := loadbalancer.BackendStateActive
+				if be.Terminating {
+					state = loadbalancer.BackendStateTerminating
+				}
+				be := loadbalancer.BackendParams{
+					Address:   l3n4Addr,
+					NodeName:  be.NodeName,
+					PortNames: portNames,
+					Weight:    loadbalancer.DefaultBackendWeight,
+					Zone:      be.Zone,
+					ForZones:  be.HintsForZones,
+					State:     state,
+				}
+				if !yield(be) {
+					break
+				}
 			}
 		}
 	}
-	for l3n4Addr, entry := range entries {
-		state := loadbalancer.BackendStateActive
-		if entry.backend.Terminating {
-			state = loadbalancer.BackendStateTerminating
-		}
-		be := loadbalancer.BackendParams{
-			Address:   l3n4Addr,
-			NodeName:  entry.backend.NodeName,
-			PortNames: entry.portNames,
-			Weight:    loadbalancer.DefaultBackendWeight,
-			Zone:      entry.backend.Zone,
-			ForZones:  entry.backend.HintsForZones,
-			State:     state,
-		}
-		out = append(out, be)
-	}
-	return
 }
 
 func isTopologyAware(svc *slim_corev1.Service) bool {

operator/watchers/service_sync.go
@@ -237,7 +237,7 @@ func (d DefaultClusterServiceConverter) Convert(k8sService *slim_corev1.Service,
 	for _, ep := range getEndpoints(svc.Namespace, svc.Name) {
 		for addrCluster, backend := range ep.Backends {
 			addrString := addrCluster.Addr().String()
-			svc.Backends[addrString] = backend.Ports
+			svc.Backends[addrString] = backend.ToPortConfiguration()
 			if backend.Hostname != "" {
 				svc.Hostnames[addrString] = backend.Hostname
 			}

pkg/k8s/zz_generated.deepcopy.go
@@ -9,7 +9,6 @@
 package k8s
 
 import (
-	store "github.com/cilium/cilium/pkg/clustermesh/store"
 	loadbalancer "github.com/cilium/cilium/pkg/loadbalancer"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 )
@@ -19,15 +18,15 @@ func (in *Backend) DeepCopyInto(out *Backend) {
 	*out = *in
 	if in.Ports != nil {
 		in, out := &in.Ports, &out.Ports
-		*out = make(store.PortConfiguration, len(*in))
+		*out = make(map[loadbalancer.L4Addr][]string, len(*in))
 		for key, val := range *in {
-			var outVal *loadbalancer.L4Addr
+			var outVal []string
 			if val == nil {
 				(*out)[key] = nil
 			} else {
 				in, out := &val, &outVal
-				*out = new(loadbalancer.L4Addr)
-				**out = **in
+				*out = make([]string, len(*in))
+				copy(*out, *in)
 			}
 			(*out)[key] = outVal
 		}

pkg/k8s/zz_generated.deepequal.go
@@ -10,56 +10,6 @@ package k8s
 
 // DeepEqual is an autogenerated deepequal function, deeply comparing the
 // receiver with other. in must be non-nil.
-func (in *Backend) DeepEqual(other *Backend) bool {
-	if other == nil {
-		return false
-	}
-
-	if ((in.Ports != nil) && (other.Ports != nil)) || ((in.Ports == nil) != (other.Ports == nil)) {
-		in, other := &in.Ports, &other.Ports
-		if other == nil || !in.DeepEqual(other) {
-			return false
-		}
-	}
-
-	if in.NodeName != other.NodeName {
-		return false
-	}
-	if in.Hostname != other.Hostname {
-		return false
-	}
-	if in.Terminating != other.Terminating {
-		return false
-	}
-	if ((in.HintsForZones != nil) && (other.HintsForZones != nil)) || ((in.HintsForZones == nil) != (other.HintsForZones == nil)) {
-		in, other := &in.HintsForZones, &other.HintsForZones
-		if other == nil {
-			return false
-		}
-
-		if len(*in) != len(*other) {
-			return false
-		} else {
-			for i, inElement := range *in {
-				if inElement != (*other)[i] {
-					return false
-				}
-			}
-		}
-	}
-
-	if in.Preferred != other.Preferred {
-		return false
-	}
-	if in.Zone != other.Zone {
-		return false
-	}
-
-	return true
-}
-
-// DeepEqual is an autogenerated deepequal function, deeply comparing the
-// receiver with other. in must be non-nil.
 func (in *EndpointSlices) DeepEqual(other *EndpointSlices) bool {
 	if other == nil {
 		return false

pkg/loadbalancer/benchmark/benchmark.go
@@ -477,7 +477,7 @@ func checkTables(db *statedb.DB, writer *writer.Writer, svcs []*slim_corev1.Serv
 					if be.Address.AddrCluster != wantAddr {
 						err = errors.Join(err, fmt.Errorf("Incorrect address for backend #%06d, got %v, want %v", i, be.Address.AddrCluster, wantAddr))
 					}
-					for _, wantPort := range wantBe.Ports { // There is only one element in this map.
+					for wantPort := range wantBe.Ports { // There is only one element in this map.
 						if be.Address.Port != wantPort.Port {
 							err = errors.Join(err, fmt.Errorf("Incorrect port for backend #%06d, got %v, want %v", i, be.Address.Port, wantPort.Port))
 						}

pkg/loadbalancer/reflectors/file.go
@@ -228,7 +228,7 @@ func (s *fileReflector) synchronize(txn writer.WriteTxn, state *StateFile) (numS
 	for i := range state.Endpoints {
 		eps := k8s.ParseEndpointSliceV1(s.log, &state.Endpoints[i])
 		bes := convertEndpoints(s.log, s.extConfig, eps.ServiceName, maps.All(eps.Backends))
-		if err := s.w.UpsertBackends(txn, eps.ServiceName, source.LocalAPI, bes...); err != nil {
+		if err := s.w.UpsertBackends(txn, eps.ServiceName, source.LocalAPI, bes); err != nil {
 			return 0, 0, 0, fmt.Errorf("failed to upsert backends: %w", err)
 		}
 		numBackends++

commit 47edbf1a36d419392693db100a885ae51d9a8332

clustermesh: fix propagation of services with unnamed ports
The blamed commit reworked the storage of port information as part of
the k8s.Backends structure, with the goal of reducing allocations.
However, unnamed ports are handled differently, which down the line
broke the propagation of services via clustermesh, when associated
with unnamed ports, as incorrectly ignored.

Let's get this fixed by uniforming the handling, and adding appropriate
filtering to remove the unnamed port entry where appropriate, to
preserve the existing behavior. Additionally, let's extend the service
synchronization script test, to prevent future regressions.

Fixes: 12ae78bbb2ab ("k8s,loadbalancer: Optimize 'k8s.Backend.Ports' to reduce allocations")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/k8s/endpoints.go
@@ -287,11 +287,7 @@ func ParseEndpointSliceV1(logger *slog.Logger, ep *slim_discovery_v1.EndpointSli
 			for _, port := range ep.Ports {
 				name, lbPort, ok := parseEndpointPortV1(port)
 				if ok {
-					if name != "" {
-						backend.Ports[lbPort] = append(backend.Ports[lbPort], name)
-					} else {
-						backend.Ports[lbPort] = nil
-					}
+					backend.Ports[lbPort] = append(backend.Ports[lbPort], name)
 				}
 			}
 			if sub.Hints != nil && (*sub.Hints).ForZones != nil {

pkg/loadbalancer/reflectors/conversions.go
@@ -421,6 +421,15 @@ func convertEndpoints(rawlog *slog.Logger, cfg loadbalancer.ExternalConfig, svcN
 					continue
 				}
 
+				// Filter out the unnamed port, if present
+				if idx := slices.Index(portNames, ""); idx != -1 {
+					if len(portNames) == 1 {
+						portNames = nil
+					} else {
+						portNames = slices.Concat(portNames[:idx], portNames[idx+1:])
+					}
+				}
+
 				state := loadbalancer.BackendStateActive
 				if be.Terminating {
 					state = loadbalancer.BackendStateTerminating

operator/watchers/testdata/servicesync.txtar
@@ -73,7 +73,7 @@ k8s/update service.yaml
 kvstore/list -o json cilium/state/services services.actual
 * empty services.actual
 
-# 10. Toggle the global annotation back. The shared annotation is not neeeded.
+# 10. Toggle the global annotation back. The shared annotation is not needed.
 # Service should be added again.
 sed 'service.cilium.io/shared: "true"' 'placeholder: "true"' service.yaml
 sed 'service.cilium.io/global: "false"' 'service.cilium.io/global: "true"' service.yaml
@@ -90,6 +90,21 @@ k8s/update service.yaml
 # Wait for synchronization
 kvstore/list -o json cilium/state/services services.actual
 * empty services.actual
+
+# 12. Validate the export of services with unnamed ports.
+sed 'service.cilium.io/shared: "false"' 'service.cilium.io/shared: "true"' service.yaml
+sed http "" service.yaml
+sed http "" endpointslice.yaml
+
+k8s/update service.yaml
+k8s/update endpointslice.yaml
+
+cp services-4.expected services-5.expected
+sed http '' services-5.expected
+
+# Wait for synchronization
+kvstore/list -o json cilium/state/services services.actual
+* cmp services.actual services-5.expected
 
 # ----
 