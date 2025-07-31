f63f51ae93543b6bab4949f411c959af671f8d74
Jussi Maki <jussi@isovalent.com>
2025-07-31 16:15:46 +0200

    vendor: Bump to StateDB v0.4.5
    Bump to v0.4.5 which includes fixes to watch channels:
    https://github.com/cilium/statedb/pull/80, https://github.com/cilium/statedb/pull/90
    
    Full changelog: https://github.com/cilium/statedb/compare/v0.4.0...v0.4.5
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- vendor/github.com/cilium/statedb/part/txn.go
+++ vendor/github.com/cilium/statedb/part/txn.go
@@ -10,14 +10,15 @@ import (
 // Txn is a transaction against a tree. It allows doing efficient
 // modifications to a tree by caching and reusing cloned nodes.
 type Txn[T any] struct {
-	// tree is the tree being modified
-	Tree[T]
+	root *header[T]
+	opts options
+	size int // the number of objects in the tree
 
 	// mutated is the set of nodes mutated in this transaction
 	// that we can keep mutating without cloning them again.
 	// It is cleared if the transaction is cloned or iterated
 	// upon.
-	mutated nodeMutated[T]
+	mutated *nodeMutated
 
 	// watches contains the channels of cloned nodes that should be closed
 	// when transaction is committed.
@@ -39,8 +40,12 @@ func (txn *Txn[T]) Clone() Ops[T] {
 	// Clear the mutated nodes so that the returned clone won't be changed by
 	// further modifications in this transaction.
 	txn.mutated.clear()
-	treeCopy := txn.Tree
-	return &treeCopy
+	return &Tree[T]{
+		opts: txn.opts,
+		root: txn.root,
+		size: txn.size,
+		txn:  nil,
+	}
 }
 
 // Insert or update the tree with the given key and value.
@@ -57,6 +62,9 @@ func (txn *Txn[T]) InsertWatch(key []byte, value T) (old T, hadOld bool, watch <
 	old, hadOld, watch, txn.root = txn.insert(txn.root, key, value)
 	if !hadOld {
 		txn.size++
+	}
+	if txn.opts.rootOnlyWatch() {
+		watch = txn.root.watch
 	}
 	return
 }
@@ -79,6 +87,9 @@ func (txn *Txn[T]) ModifyWatch(key []byte, mod func(T) T) (old T, hadOld bool, w
 	old, hadOld, watch, txn.root = txn.modify(txn.root, key, mod)
 	if !hadOld {
 		txn.size++
+	}
+	if txn.opts.rootOnlyWatch() {
+		watch = txn.root.watch
 	}
 	return
 }
@@ -106,7 +117,7 @@ func (txn *Txn[T]) RootWatch() <-chan struct{} {
 // value was found.
 func (txn *Txn[T]) Get(key []byte) (T, <-chan struct{}, bool) {
 	value, watch, ok := search(txn.root, key)
-	if txn.opts.rootOnlyWatch {
+	if txn.opts.rootOnlyWatch() {
 		watch = txn.root.watch
 	}
 	return value, watch, ok
@@ -118,7 +129,7 @@ func (txn *Txn[T]) Get(key []byte) (T, <-chan struct{}, bool) {
 func (txn *Txn[T]) Prefix(key []byte) (*Iterator[T], <-chan struct{}) {
 	txn.mutated.clear()
 	iter, watch := prefixSearch(txn.root, key)
-	if txn.opts.rootOnlyWatch {
+	if txn.opts.rootOnlyWatch() {
 		watch = txn.root.watch
 	}
 	return iter, watch
@@ -134,34 +145,35 @@ func (txn *Txn[T]) LowerBound(key []byte) *Iterator[T] {
 // Iterator returns an iterator for all objects.
 func (txn *Txn[T]) Iterator() *Iterator[T] {
 	txn.mutated.clear()
-	return newIterator[T](txn.root)
+	return newIterator(txn.root)
 }
 
 // Commit the transaction and produce the new tree.
 func (txn *Txn[T]) Commit() *Tree[T] {
-	txn.mutated.clear()
-	for ch := range txn.watches {
-		close(ch)
-	}
-	txn.watches = nil
-	return &Tree[T]{txn.opts, txn.root, txn.size}
+	txn.Notify()
+	return txn.CommitOnly()
 }
 
 // CommitOnly the transaction, but do not close the
 // watch channels. Returns the new tree.
-// To close the watch channels call Notify().
+// To close the watch channels call Notify(). You must call Notify() before
+// Tree.Txn().
 func (txn *Txn[T]) CommitOnly() *Tree[T] {
-	txn.mutated.clear()
-	return &Tree[T]{txn.opts, txn.root, txn.size}
+	t := &Tree[T]{opts: txn.opts, root: txn.root, size: txn.size}
+	if !txn.opts.noCache() {
+		t.txn = txn
+	}
+	return t
 }
 
 // Notify closes the watch channels of nodes that were
-// mutated as part of this transaction.
+// mutated as part of this transaction. Must be called before
+// Tree.Txn() is used again.
 func (txn *Txn[T]) Notify() {
 	for ch := range txn.watches {
 		close(ch)
 	}
-	txn.watches = nil
+	clear(txn.watches)
 }
 
 // PrintTree to the standard output. For debugging.
@@ -170,14 +182,14 @@ func (txn *Txn[T]) PrintTree() {
 }
 
 func (txn *Txn[T]) cloneNode(n *header[T]) *header[T] {
-	if txn.mutated.exists(n) {
+	if nodeMutatedExists(txn.mutated, n) {
 		return n
 	}
 	if n.watch != nil {
 		txn.watches[n.watch] = struct{}{}
 	}
-	n = n.clone(!txn.opts.rootOnlyWatch || n == txn.root)
-	txn.mutated.put(n)
+	n = n.clone(!txn.opts.rootOnlyWatch() || n == txn.root)
+	nodeMutatedSet(txn.mutated, n)
 	return n
 }
 
@@ -188,6 +200,8 @@ func (txn *Txn[T]) insert(root *header[T], key []byte, value T) (oldValue T, had
 func (txn *Txn[T]) modify(root *header[T], key []byte, mod func(T) T) (oldValue T, hadOld bool, watch <-chan struct{}, newRoot *header[T]) {
 	fullKey := key
 
+	// Start recursing from the root to find the insertion point.
+	// Point [thisp] to the root we're returning. It'll be replaced by a clone of the root when we recurse into it.
 	this := root
 	thisp := &newRoot
 
@@ -195,20 +209,11 @@ func (txn *Txn[T]) modify(root *header[T], key []byte, mod func(T) T) (oldValue 
 	// it, we do it and return. If an existing node exists where the key should go, then
 	// we stop. 'this' points to that node, and 'thisp' to its memory location. It has
 	// not been cloned.
-	for {
-		if this.isLeaf() {
-			// We've reached a leaf node, cannot go further.
-			break
-		}
-
-		if !bytes.HasPrefix(key, this.prefix) {
-			break
-		}
-
+	for !this.isLeaf() && bytes.HasPrefix(key, this.prefix()) {
 		// Prefix matched. Consume it and go further.
-		key = key[len(this.prefix):]
+		key = key[this.prefixLen:]
 		if len(key) == 0 {
-			// Our key matches this node.
+			// Our key matches this node or we reached a leaf node.
 			break
 		}
 
@@ -220,8 +225,8 @@ func (txn *Txn[T]) modify(root *header[T], key []byte, mod func(T) T) (oldValue 
 				if this.watch != nil {
 					txn.watches[this.watch] = struct{}{}
 				}
-				this = this.promote(!txn.opts.rootOnlyWatch || this == newRoot)
-				txn.mutated.put(this)
+				this = this.promote(!txn.opts.rootOnlyWatch() || this == root)
+				nodeMutatedSet(txn.mutated, this)
 			} else {
 				// Node is big enough, clone it so we can mutate it
 				this = txn.cloneNode(this)
@@ -242,132 +247,93 @@ func (txn *Txn[T]) modify(root *header[T], key []byte, mod func(T) T) (oldValue 
 		this = *thisp
 	}
 
-	// A node exists where we wanted to insert the key.
+	common := commonPrefix(key, this.prefix())
+
+	// A node already exists where we wanted to insert the key.
 	// 'this' points to it, and 'thisp' is its memory location. The parents
 	// have been cloned.
-	switch {
-	case this.isLeaf():
-		common := commonPrefix(key, this.prefix)
-		if len(common) == len(this.prefix) && len(common) == len(key) {
-			// Exact match, clone and update the value.
-			oldValue = this.getLeaf().value
-			hadOld = true
-			this = txn.cloneNode(this)
-			*thisp = this
-			leaf := this.getLeaf()
-			leaf.value = mod(oldValue)
-			watch = leaf.watch
-		} else {
-			// Partially matching prefix.
-			newNode := &node4[T]{
-				header: header[T]{prefix: common},
-			}
-			newNode.setKind(nodeKind4)
-
-			// Make a shallow copy of the leaf. But keep its watch channel
-			// intact since we're only manipulating its prefix.
-			oldLeafCopy := *this.getLeaf()
-			oldLeaf := &oldLeafCopy
-			oldLeaf.prefix = oldLeaf.prefix[len(common):]
-			key = key[len(common):]
-			var zero T
-			newLeaf := newLeaf(txn.opts, key, fullKey, mod(zero))
-			watch = newLeaf.watch
-
-			// Insert the two leaves into the node we created. If one has
-			// a key that is a subset of the other, then we can insert them
-			// as a leaf of the node4, otherwise they become children.
-			switch {
-			case len(oldLeaf.prefix) == 0:
-				oldLeaf.prefix = common
-				newNode.setLeaf(oldLeaf)
-				newNode.children[0] = newLeaf.self()
-				newNode.keys[0] = newLeaf.prefix[0]
-				newNode.setSize(1)
-
-			case len(key) == 0:
-				newLeaf.prefix = common
-				newNode.setLeaf(newLeaf)
-				newNode.children[0] = oldLeaf.self()
-				newNode.keys[0] = oldLeaf.prefix[0]
-				newNode.setSize(1)
-
-			case oldLeaf.prefix[0] < key[0]:
-				newNode.children[0] = oldLeaf.self()
-				newNode.keys[0] = oldLeaf.prefix[0]
-				newNode.children[1] = newLeaf.self()
-				newNode.keys[1] = key[0]
-				newNode.setSize(2)
-
-			default:
-				newNode.children[0] = newLeaf.self()
-				newNode.keys[0] = key[0]
-				newNode.children[1] = oldLeaf.self()
-				newNode.keys[1] = oldLeaf.prefix[0]
-				newNode.setSize(2)
-			}
-			*thisp = newNode.self()
-		}
-	case len(key) == 0:
-		// Exact match, but not a leaf node
+	//
+	// Check first if it's an exact match.
+	if len(key) == 0 || len(key) == len(common) && len(key) == int(this.prefixLen) {
 		this = txn.cloneNode(this)
 		*thisp = this
 		if leaf := this.getLeaf(); leaf != nil {
-			// Replace the existing leaf
 			oldValue = leaf.value
 			hadOld = true
-			leaf = txn.cloneNode(leaf.self()).getLeaf()
+			if !this.isLeaf() {
+				// [this] is a non-leaf node, clone its leaf so we can update it.
+				leaf = txn.cloneNode(leaf.self()).getLeaf()
+				this.setLeaf(leaf)
+			}
 			leaf.value = mod(oldValue)
 			watch = leaf.watch
-			this.setLeaf(leaf)
 		} else {
 			// Set the leaf
 			var zero T
-			leaf := newLeaf(txn.opts, this.prefix, fullKey, mod(zero))
+			leaf := newLeaf(txn.opts, this.prefix(), fullKey, mod(zero))
 			watch = leaf.watch
 			this.setLeaf(leaf)
 		}
-
-	default:
-		// Partially matching prefix, non-leaf node.
-		common := commonPrefix(key, this.prefix)
-
-		this = txn.cloneNode(this)
-		*thisp = this
-		this.prefix = this.prefix[len(common):]
-		key = key[len(common):]
-
-		var zero T
-		newLeaf := newLeaf(txn.opts, key, fullKey, mod(zero))
-		watch = newLeaf.watch
-		newNode := &node4[T]{
-			header: header[T]{prefix: common},
-		}
-		newNode.setKind(nodeKind4)
-
-		switch {
-		case len(key) == 0:
-			newLeaf.prefix = common
-			newNode.setLeaf(newLeaf)
-			newNode.children[0] = this
-			newNode.keys[0] = this.prefix[0]
-			newNode.setSize(1)
-
-		case this.prefix[0] < key[0]:
-			newNode.children[0] = this
-			newNode.keys[0] = this.prefix[0]
-			newNode.children[1] = newLeaf.self()
-			newNode.keys[1] = key[0]
-			newNode.setSize(2)
-		default:
-			newNode.children[0] = newLeaf.self()
-			newNode.keys[0] = key[0]
-			newNode.children[1] = this
-			newNode.keys[1] = this.prefix[0]
-			newNode.setSize(2)
-		}
-		*thisp = newNode.self()
+		return
 	}
+
+	// The target node into which we want to insert has only a partially matching prefix.
+	// We'll replace target with a new [node4] and insert the target and new node into it
+	// (either as children or as leaf, depending on the prefixes).
+	if this.isLeaf() {
+		// We're replacing a leaf node, make a shallow copy to retain
+		// its watch channel since we're just manipulating prefix.
+		leafCopy := *this.getLeaf()
+		this = &leafCopy.header
+	} else {
+		this = txn.cloneNode(this)
+	}
+	*thisp = this
+	this.setPrefix(this.prefix()[len(common):])
+	key = key[len(common):]
+
+	var zero T
+	newLeaf := newLeaf(txn.opts, key, fullKey, mod(zero))
+	watch = newLeaf.watch
+	newNode := &node4[T]{}
+	newNode.setPrefix(common)
+	newNode.setKind(nodeKind4)
+	if !txn.opts.rootOnlyWatch() {
+		newNode.watch = make(chan struct{})
+	}
+
+	switch {
+	case this.prefixLen == 0:
+		// target has shorter key than new leaf
+		newNode.setLeaf(this.getLeaf())
+		newNode.children[0] = newLeaf.self()
+		newNode.keys[0] = key[0]
+		newNode.setSize(1)
+
+	case len(key) == 0:
+		// new leaf has shorter key than target
+		newNode.setLeaf(newLeaf)
+		newNode.children[0] = this
+		newNode.keys[0] = this.key()
+		newNode.setSize(1)
+
+	case this.key() < key[0]:
+		// target node has smaller key then new leaf
+		newNode.children[0] = this
+		newNode.keys[0] = this.key()
+		newNode.children[1] = newLeaf.self()
+		newNode.keys[1] = key[0]
+		newNode.setSize(2)
+	default:
+		// new leaf has smaller key then target node
+		newNode.children[0] = newLeaf.self()
+		newNode.keys[0] = key[0]
+		newNode.children[1] = this
+		newNode.keys[1] = this.key()
+		newNode.setSize(2)
+	}
+	*thisp = newNode.self()
+
 	return
 }
 
@@ -393,8 +359,8 @@ func (txn *Txn[T]) delete(root *header[T], key []byte) (oldValue T, hadOld bool,
 	// Find the target node and record the path to it.
 	var leaf *leaf[T]
 	for {
-		if bytes.HasPrefix(key, this.prefix) {
-			key = key[len(this.prefix):]
+		if bytes.HasPrefix(key, this.prefix()) {
+			key = key[this.prefixLen:]
 			if len(key) == 0 {
 				leaf = this.getLeaf()
 				if leaf == nil {
@@ -486,8 +452,8 @@ func (txn *Txn[T]) delete(root *header[T], key []byte) (oldValue T, hadOld bool,
 				n16 := newNode.node16()
 				n16.leaf = parent.node.getLeaf()
 				size := n16.size()
-				for i := 0; i < size; i++ {
-					n16.keys[i] = n16.children[i].prefix[0]
+				for i := range size {
+					n16.keys[i] = n16.children[i].key()
 				}
 			case parent.node.kind() == nodeKind16 && parent.node.size() <= 3:
 				newNode = (&node4[T]{header: *parent.node}).self()
