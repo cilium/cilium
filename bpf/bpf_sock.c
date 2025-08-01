362d44fd3f9dc9cabe271ef4c539d2f0daee4166
Ondrej Blazek <ondrej.blazek@firma.seznam.cz>
2025-08-01 12:46:29 +0200

    bgp: withdraw a service when there are no endpoints
    Signed-off-by: Ondrej Blazek <ondrej.blazek@firma.seznam.cz>

--- bpf/bpf_sock.c
+++ bpf/bpf_sock.c
@@ -295,8 +295,17 @@ static __always_inline int __sock4_xlate_fwd(struct bpf_sock_addr *ctx,
 	}
 	if (!svc)
 		return -ENXIO;
-	if (svc->count == 0 && !lb4_svc_is_l7_loadbalancer(svc))
-		return -EHOSTUNREACH;
+	if (svc->count == 0 && !lb4_svc_is_l7_loadbalancer(svc)) {
+#ifdef NO_ENDPOINTS_ROUTABLE
+                return -EHOSTUNREACH;
+#else
+		if ((lb4_svc_is_routable(svc) && (svc->flags & SVC_FLAG_EXT_LOCAL_SCOPE)) ||
+		    (!lb4_svc_is_routable(svc) && (svc->flags2 & SVC_FLAG_INT_LOCAL_SCOPE))) {
+			return -EHOSTUNREACH;
+		}
+		return 0;
+#endif
+	}
 
 	send_trace_sock_notify4(ctx_full, XLATE_PRE_DIRECTION_FWD, dst_ip,
 				bpf_ntohs(dst_port));
@@ -1027,8 +1036,17 @@ static __always_inline int __sock6_xlate_fwd(struct bpf_sock_addr *ctx,
 	}
 	if (!svc)
 		return sock6_xlate_v4_in_v6(ctx, udp_only);
-	if (svc->count == 0 && !lb6_svc_is_l7_loadbalancer(svc))
-		return -EHOSTUNREACH;
+	if (svc->count == 0 && !lb6_svc_is_l7_loadbalancer(svc)) {
+#ifdef NO_ENDPOINTS_ROUTABLE
+                return -EHOSTUNREACH;
+#else
+		if ((lb6_svc_is_routable(svc) && (svc->flags & SVC_FLAG_EXT_LOCAL_SCOPE)) ||
+		    (!lb6_svc_is_routable(svc) && (svc->flags2 & SVC_FLAG_INT_LOCAL_SCOPE))) {
+			return -EHOSTUNREACH;
+		}
+		return 0;
+#endif
+	}
 
 	send_trace_sock_notify6(ctx, XLATE_PRE_DIRECTION_FWD, &key.address,
 				bpf_ntohs(dst_port));
