3da230a02c01ffd45b74c5d4d3fd922f52c94773
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2025-08-01 11:20:34 +0100

    bpf: lb: Remove unnecessary service map lookups in __lbXX_lookup_service().
    Previously, if the lookup of a service derived from ingress 5-tuple results
    in no match, we undertake an additional lookup with IPPROTO_ANY. This was
    necessary to maintain backwards compatibility with earlier versions of Cilium
    that did not support protocol differentiation when programming the data path.
    
    Cilium 1.18 introduced logic to migrate IPPROTO_ANY service/backend BPF map
    entries to an appropriate protocol automatically, such that the additional
    lookups are no longer required.
    
    Appropriate tests updated to include explicit lookups for IPPROTO_TCP,
    IPPROTO_UDP, and mismatched protocol checks.
    
    Signed-off-by: Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>

--- bpf/tests/lb_tests.c
+++ bpf/tests/lb_tests.c
@@ -19,7 +19,7 @@
 
 #include "lib/lb.h"
 
-/* Service was created with L4 proto differentiation: */
+/* TCP Service, single-scope */
 CHECK("tc", "lb4_tcp_single_scope")
 int test_lb4_tcp_single_scope(__maybe_unused struct __ctx_buff *ctx)
 {
@@ -44,7 +44,7 @@ int test_lb4_tcp_single_scope(__maybe_unused struct __ctx_buff *ctx)
 	test_finish();
 }
 
-/* Dual-scope Service was created with L4 proto differentiation: */
+/* TCP Service, dual-scope */
 CHECK("tc", "lb4_tcp_dual_scope")
 int test_lb4_tcp_dual_scope(__maybe_unused struct __ctx_buff *ctx)
 {
@@ -70,83 +70,103 @@ int test_lb4_tcp_dual_scope(__maybe_unused struct __ctx_buff *ctx)
 	test_finish();
 }
 
-/* Service was created without L4 proto differentiation: */
-CHECK("tc", "lb4_any_proto_single_scope")
-int test_lb4_any_proto_single_scope(__maybe_unused struct __ctx_buff *ctx)
+/* UDP Service, single-scope */
+CHECK("tc", "lb4_udp_single_scope")
+int test_lb4_udp_single_scope(__maybe_unused struct __ctx_buff *ctx)
 {
 	struct lb4_service *service;
 	struct lb4_key key = {
 		.address = FRONTEND_IP,
 		.dport = FRONTEND_PORT,
-		.proto = IPPROTO_TCP,
+		.proto = IPPROTO_UDP,
 	};
 
 	test_init();
 
-	lb_v4_add_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_ANY,
+	lb_v4_add_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_UDP,
 			  BACKEND_COUNT, REVNAT_INDEX);
 
 	service = lb4_lookup_service(&key, true);
 	assert(service);
-	assert(key.proto == IPPROTO_ANY);
+	assert(key.proto == IPPROTO_UDP);
 
-	lb_v4_delete_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_ANY);
+	lb_v4_delete_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_UDP);
 
 	test_finish();
 }
 
-/* Dual-scope Service was created without L4 proto differentiation: */
-CHECK("tc", "lb4_any_proto_dual_scope")
-int test_lb4_any_proto_dual_scope(__maybe_unused struct __ctx_buff *ctx)
+/* UDP Service, dual-scope */
+CHECK("tc", "lb4_udp_dual_scope")
+int test_lb4_udp_dual_scope(__maybe_unused struct __ctx_buff *ctx)
 {
 	struct lb4_service *service;
 	struct lb4_key key = {
 		.address = FRONTEND_IP,
 		.dport = FRONTEND_PORT,
-		.proto = IPPROTO_TCP,
+		.proto = IPPROTO_UDP,
 	};
 
 	test_init();
 
-	lb_v4_add_service_with_flags(FRONTEND_IP, FRONTEND_PORT,
-				     IPPROTO_ANY, BACKEND_COUNT,
-				     REVNAT_INDEX, 0, SVC_FLAG_TWO_SCOPES);
+	lb_v4_add_service_with_flags(FRONTEND_IP, FRONTEND_PORT, IPPROTO_UDP,
+				     BACKEND_COUNT, REVNAT_INDEX,
+				     0, SVC_FLAG_TWO_SCOPES);
 
 	service = lb4_lookup_service(&key, true);
 	assert(service);
-	assert(key.proto == IPPROTO_ANY);
+	assert(key.proto == IPPROTO_UDP);
 
-	lb_v4_delete_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_ANY);
+	lb_v4_delete_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_UDP);
 
 	test_finish();
 }
 
-/* Dual-scope Service was created with different L4 proto values
- * (due to eg. upgrade from a legacy Cilium version without proto differentiation,
- *  and then changing iTP / eTP from 'Cluster' to 'Local').
- */
-CHECK("tc", "lb4_mixed_proto_dual_scope")
-int test_lb4_mixed_proto_dual_scope(__maybe_unused struct __ctx_buff *ctx)
+/* Protocol mismatch, single scope */
+CHECK("tc", "lb4_proto_mismatch_single_scope")
+int test_lb4_proto_mismatch_single_scope(__maybe_unused struct __ctx_buff *ctx)
 {
 	struct lb4_service *service;
 	struct lb4_key key = {
 		.address = FRONTEND_IP,
 		.dport = FRONTEND_PORT,
-		.proto = IPPROTO_TCP,
+		.proto = IPPROTO_UDP,
 	};
 
 	test_init();
 
-	lb_v4_add_mixed_proto_service_with_flags(FRONTEND_IP, FRONTEND_PORT,
-						 IPPROTO_ANY, IPPROTO_TCP,
-						 BACKEND_COUNT, REVNAT_INDEX,
-						 0, SVC_FLAG_TWO_SCOPES);
+	lb_v4_add_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_TCP,
+			  BACKEND_COUNT, REVNAT_INDEX);
 
 	service = lb4_lookup_service(&key, true);
-	assert(service);
-	assert(key.proto == IPPROTO_TCP);
+	assert(!service);
+	assert(key.proto == IPPROTO_UDP);
 
-	lb_v4_delete_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_ANY);
+	lb_v4_delete_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_TCP);
+
+	test_finish();
+}
+
+/* Protocol mismatch, dual scope */
+CHECK("tc", "lb4_proto_mismatch_dual_scope")
+int test_lb4_proto_mismatch_dual_scope(__maybe_unused struct __ctx_buff *ctx)
+{
+	struct lb4_service *service;
+	struct lb4_key key = {
+		.address = FRONTEND_IP,
+		.dport = FRONTEND_PORT,
+		.proto = IPPROTO_UDP,
+	};
+
+	test_init();
+
+	lb_v4_add_service_with_flags(FRONTEND_IP, FRONTEND_PORT, IPPROTO_TCP,
+				     BACKEND_COUNT, REVNAT_INDEX,
+				     0, SVC_FLAG_TWO_SCOPES);
+
+	service = lb4_lookup_service(&key, true);
+	assert(!service);
+	assert(key.proto == IPPROTO_UDP);
+
 	lb_v4_delete_service(FRONTEND_IP, FRONTEND_PORT, IPPROTO_TCP);
 
 	test_finish();
