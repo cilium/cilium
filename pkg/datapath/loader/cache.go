cilium/cilium

commit 70f6608b646002576e0386ca291fc3fb487852c0

loader: evict object cache when datapath config changes
The object cache currently does no invalidation, which means that
we accumulate cachedObject in memory and template ELF on disk.
Use update of the base datapath hash as an opportunity to evict
some of that cache.

In practice this is probably not a big issue: datapath config changes
rarely if every, and we only have templates for endpoints and host endpoint.

Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

pkg/datapath/loader/cache.go
@@ -4,6 +4,7 @@
 package loader
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -20,34 +21,17 @@ import (
 	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
-// RestoreTemplates populates the object cache from templates on the filesystem
-// at the specified path.
-func (l *loader) RestoreTemplates(stateDir string) error {
-	// Simplest implementation: Just garbage-collect everything.
-	// In future we should make this smarter.
-	path := filepath.Join(stateDir, defaults.TemplatesDir)
-	err := os.RemoveAll(path)
-	if err == nil || os.IsNotExist(err) {
-		return nil
-	}
-	return &os.PathError{
-		Op:   "failed to remove old BPF templates",
-		Path: path,
-		Err:  err,
-	}
-}
-
-// objectCache is a map from a hash of the datapath to the path on the
-// filesystem where its corresponding BPF object file exists.
+// objectCache amortises the cost of BPF compilation for endpoints.
 type objectCache struct {
 	lock.Mutex
 	datapath.ConfigWriter
 
+	// The directory used for caching. Must not be accessed by another process.
 	workingDirectory string
-	baseHash         datapathHash
 
-	// objects maps a hash to a queue which ensures that only one
-	// attempt is made concurrently to compile the corresponding template.
+	baseHash datapathHash
+
+	// The cached objects.
 	objects map[string]*cachedObject
 }
 
@@ -76,15 +60,36 @@ func (o *objectCache) UpdateDatapathHash(nodeCfg *datapath.LocalNodeConfiguratio
 		return fmt.Errorf("hash datapath config: %w", err)
 	}
 
+	// Prevent new compilation from starting.
 	o.Lock()
 	defer o.Unlock()
+
+	// Don't invalidate if the hash is the same.
+	if bytes.Equal(newHash, o.baseHash) {
+		return nil
+	}
+
+	// Wait until all concurrent compilation has finished.
+	for _, obj := range o.objects {
+		obj.Lock()
+	}
+
+	if err := os.RemoveAll(o.workingDirectory); err != nil {
+		for _, obj := range o.objects {
+			obj.Unlock()
+		}
+
+		return err
+	}
+
 	o.baseHash = newHash
+	o.objects = make(map[string]*cachedObject)
 	return nil
 }
 
 // serialize access to an abitrary key.
 //
-// Lock the returned object to ensure mutual exclusion.
+// The caller must call Unlock on the returned cachedObject.
 func (o *objectCache) serialize(key string) *cachedObject {
 	o.Lock()
 	defer o.Unlock()
@@ -94,6 +99,8 @@ func (o *objectCache) serialize(key string) *cachedObject {
 		obj = new(cachedObject)
 		o.objects[key] = obj
 	}
+
+	obj.Lock()
 	return obj
 }
 
@@ -101,7 +108,7 @@ func (o *objectCache) serialize(key string) *cachedObject {
 // corresponding to the specified endpoint configuration.
 func (o *objectCache) build(ctx context.Context, nodeCfg *datapath.LocalNodeConfiguration, cfg datapath.EndpointConfiguration, stats *metrics.SpanStat, dir *directoryInfo, hash string) (string, error) {
 	isHost := cfg.IsHost()
-	templatePath := filepath.Join(o.workingDirectory, defaults.TemplatesDir, hash)
+	templatePath := filepath.Join(o.workingDirectory, hash)
 	dir = &directoryInfo{
 		Library: dir.Library,
 		Runtime: dir.Runtime,
@@ -172,21 +179,14 @@ func (o *objectCache) fetchOrCompile(ctx context.Context, nodeCfg *datapath.Loca
 
 	scopedLog := log.WithField(logfields.BPFHeaderfileHash, hash)
 
+	// Only allow a single concurrent compilation per hash.
 	obj := o.serialize(hash)
-
-	// Only allow a single concurrent compilation.
-	obj.Lock()
 	defer obj.Unlock()
 
-	if obj.path != "" {
-		// Only attempt to use a cached object if we previously built this object.
-		// Otherwise we risk reusing a previous process' output since we're not
-		// guaranteed an empty working directory.
-		if cached, err := os.Open(obj.path); err == nil {
-			return cached, false, nil
-		} else if !errors.Is(err, os.ErrNotExist) {
-			return nil, false, err
-		}
+	if cached, err := os.Open(obj.path); err == nil {
+		return cached, false, nil
+	} else if !errors.Is(err, os.ErrNotExist) {
+		return nil, false, err
 	}
 
 	if stats == nil {

daemon/cmd/daemon.go
@@ -914,10 +914,6 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		return nil, nil, err
 	}
 
-	if err := d.datapath.Loader().RestoreTemplates(option.Config.StateDir); err != nil {
-		log.WithError(err).Error("Unable to restore previous BPF templates")
-	}
-
 	// Start watcher for endpoint IP --> identity mappings in key-value store.
 	// this needs to be done *after* init() for the daemon in that function,
 	// we populate the IPCache with the host's IP(s).

pkg/datapath/fake/types/datapath.go
@@ -162,10 +162,6 @@ func (f *FakeLoader) HostDatapathInitialized() <-chan struct{} {
 	return nil
 }
 
-func (f *FakeLoader) RestoreTemplates(stateDir string) error {
-	return nil
-}
-
 func (f *FakeLoader) DetachXDP(ifaceName string, bpffsBase, progName string) error {
 	return nil
 }

pkg/datapath/loader/loader.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"net"
 	"net/netip"
+	"path/filepath"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -104,7 +105,7 @@ type Params struct {
 func newLoader(p Params) *loader {
 	return &loader{
 		cfg:               p.Config,
-		templateCache:     newObjectCache(p.ConfigWriter, option.Config.StateDir),
+		templateCache:     newObjectCache(p.ConfigWriter, filepath.Join(option.Config.StateDir, defaults.TemplatesDir)),
 		sysctl:            p.Sysctl,
 		hostDpInitialized: make(chan struct{}),
 		prefilter:         p.Prefilter,

pkg/datapath/types/loader.go
@@ -23,7 +23,6 @@ type Loader interface {
 	Unload(ep Endpoint)
 	Reinitialize(ctx context.Context, cfg LocalNodeConfiguration, tunnelConfig tunnel.Config, iptMgr IptablesManager, p Proxy) error
 	HostDatapathInitialized() <-chan struct{}
-	RestoreTemplates(stateDir string) error
 	DetachXDP(iface string, bpffsBase, progName string) error
 
 	WriteEndpointConfig(w io.Writer, cfg EndpointConfiguration) error

commit c6d666766da0ce910987784fcc40929db07525df

pkg/datapath: unlock objects to avoid deadlock detection
If we don't unlock these locked objects, to avoid deadlock detection,
the race detector considers this as a race condition.

Fixes: 70f6608b6460 ("loader: evict object cache when datapath config changes")
Signed-off-by: André Martins <andre@cilium.io>

pkg/datapath/loader/cache.go
@@ -85,6 +85,11 @@ func (o *objectCache) UpdateDatapathHash(nodeCfg *datapath.LocalNodeConfiguratio
 
 		return err
 	}
+	// Unlock all objects so that race detector doesn't complain about potential
+	// deadlocks.
+	for _, obj := range o.objects {
+		obj.Unlock()
+	}
 
 	o.baseHash = newHash
 	o.objects = make(map[string]*cachedSpec)