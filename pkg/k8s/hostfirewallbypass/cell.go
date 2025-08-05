9ef97e8bf76f220900f08cc69ec22eaa1d8a3bf2
Anton Tykhyy <atykhyy@gmail.com>
2025-08-05 11:17:51 +0300

    Don't enable host firewall bypass unless host firewall is enabled
    Signed-off-by: Anton Tykhyy <atykhyy@gmail.com>

--- pkg/k8s/hostfirewallbypass/cell.go
+++ pkg/k8s/hostfirewallbypass/cell.go
@@ -6,6 +6,8 @@ package hostfirewallbypass
 import (
 	"github.com/cilium/hive/cell"
 	"github.com/spf13/pflag"
+
+	"github.com/cilium/cilium/pkg/option"
 )
 
 // Provides a host firewall bypass for k8s Clientset
@@ -14,17 +16,24 @@ var Cell = cell.Module(
 	"k8s-host-firewall-bypaass",
 	"Kubernetes host firewall bypass",
 
-	cell.Config(Params{
-		EnableK8sHostFirewallBypass: false,
+	cell.Config(config{
+		EnableK8sHostFirewallBypass: true,
 	}),
 	cell.Provide(NewK8sHostFirewallBypass),
 )
 
 type Params struct {
+	cell.In
+
+	DaemonConfig *option.DaemonConfig `optional:"true"`
+	LocalConfig  config
+}
+
+type config struct {
 	EnableK8sHostFirewallBypass bool
 }
 
-func (p Params) Flags(flags *pflag.FlagSet) {
+func (p config) Flags(flags *pflag.FlagSet) {
 	enable := "enable-k8s-host-firewall-bypass"
 	flags.Bool(enable, p.EnableK8sHostFirewallBypass, "Enable bypassing host firewall for Kubernetes API server access.")
 	flags.MarkHidden(enable)
