be39000896ad9b23a8adfc4167251fb9843d7100
Tom Hadlaw <tom.hadlaw@isovalent.com>
2025-08-06 13:23:27 -0700

    cilium-cli: fix egress gw policy clobbering on concurrent tests.
    Egress GW policies are global, thus we cannot depend on separate
    namespaces to distinguish them.
    
    Going to concurrent tests this means that each test namespace instance
    is clobbering the last one.
    This makes the naming of policies unique to each namespace fixing this
    issue.
    
    The policy pod selection should mean that all of our egw policies don't
    overlap and produce distinct policy maps otherwise.
    
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

--- cilium-cli/connectivity/builder/egress_gateway_excluded_cidrs.go
+++ cilium-cli/connectivity/builder/egress_gateway_excluded_cidrs.go
@@ -4,6 +4,8 @@
 package builder
 
 import (
+	"fmt"
+
 	"github.com/cilium/cilium/cilium-cli/connectivity/check"
 	"github.com/cilium/cilium/cilium-cli/connectivity/tests"
 	"github.com/cilium/cilium/cilium-cli/utils/features"
@@ -14,7 +16,7 @@ type egressGatewayExcludedCidrs struct{}
 func (t egressGatewayExcludedCidrs) build(ct *check.ConnectivityTest, _ map[string]string) {
 	newTest("egress-gateway-excluded-cidrs", ct).
 		WithCiliumEgressGatewayPolicy(check.CiliumEgressGatewayPolicyParams{
-			Name:              "cegp-sample-client",
+			Name:              fmt.Sprintf("cegp-sample-client-%d", ct.Params().TestNamespaceIndex),
 			PodSelectorKind:   "client",
 			ExcludedCIDRsConf: check.ExternalNodeExcludedCIDRs,
 		}).
