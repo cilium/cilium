cilium/cilium

commit e35c099d60bf35ac7e1e76b4fd450c8caed0169e

experimental: Implement support for topology-aware routing
Add support for topology-aware routing:

* Add [Service.TrafficDistribution] for enabling topology-aware routing.
  This is set either when Spec.TrafficDistribution is set or the
  service.kubernetes.io/topology-mode annotation is set.

* Add [BackendParams.ForZones] to carry the zones for which the backend is intended
  for.

* Change [BackendParams.ZoneID] to [BackendParams.Zone]. Convert to ZoneID only when
  reconciling.

* Skip backend when traffic distribution is set to PreferClose and the
  local zone is not part of ForZones.

* Watch changes to node's zone label and refresh backend selection of affected
  frontends on changes.

Topology-aware routing is always supported and we can remove the
[k8s.ServiceCacheConfig] once we've switched over.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/loadbalancer/experimental/testdata/topology-aware.txtar
@@ -1,0 +1,141 @@
+#! --enable-experimental-lb --lb-test-fault-probability=0.0
+#
+# Test topology-aware hints, e.g. backends meant for other zones are ignored.
+#
+
+# Set the node zone label before starting so it's read by the zone watcher.
+test/set-node-labels topology.kubernetes.io/zone=foo
+
+# Start and wait for watchers
+hive start
+db/initialized
+
+# Add the service (with topology-mode=auto) and endpoints
+# 10.244.1.1 (for zones foo & bar), 10.244.2.1 (for zone quux).
+k8s/add service.yaml
+db/cmp services services.table
+k8s/add endpointslice.yaml endpointslice2.yaml
+
+# With zone set to 'foo' and topology-awareness enabled, we should
+# only select backen 10.244.1.1.
+db/cmp backends backends.table 
+db/cmp frontends frontends.table
+
+# Move the node to a different zone which won't match ForZones.
+test/set-node-labels topology.kubernetes.io/zone=baz
+db/cmp frontends frontends-no-backends.table
+
+# Move to another zone which isn't backend's own zone but mentioned in
+# ForZones.
+test/set-node-labels topology.kubernetes.io/zone=bar
+db/cmp frontends frontends.table
+
+# Remove the topology-mode annotation. Both backends should now be selected.
+sed 'topology-mode' 'topology-nope' service.yaml
+k8s/update service.yaml
+db/cmp frontends frontends-no-topo.table
+
+# Set 'trafficDistribution' and check that this results in identical behavior
+# to topology-mode.
+sed '#trafficD' 'trafficD' service.yaml
+k8s/update service.yaml
+db/cmp frontends frontends.table
+
+#####
+
+-- services.table --
+Name        Source   PortNames  TrafficPolicy  Flags
+test/echo   k8s      http=80    Cluster        TrafficDistribution=PreferClose
+
+-- frontends.table --
+Address               Type        ServiceName   PortName   Status  Backends
+10.96.50.104:80/TCP   ClusterIP   test/echo     http       Done    10.244.1.1:80/TCP
+
+-- frontends-no-backends.table --
+Address               Type        ServiceName   PortName   Status   Backends
+10.96.50.104:80/TCP   ClusterIP   test/echo     http       Done
+
+-- frontends-no-topo.table --
+Address               Type        ServiceName   PortName   Status  Backends
+10.96.50.104:80/TCP   ClusterIP   test/echo     http       Done    10.244.1.1:80/TCP, 10.244.2.1:80/TCP
+
+-- backends.table --
+Address             Instances
+10.244.1.1:80/TCP   test/echo (http)
+10.244.2.1:80/TCP   test/echo (http)
+
+-- service.yaml --
+apiVersion: v1
+kind: Service
+metadata:
+  name: echo
+  namespace: test
+  annotations:
+    service.kubernetes.io/topology-mode: auto
+spec:
+  clusterIP: 10.96.50.104
+  clusterIPs:
+  - 10.96.50.104
+  ports:
+  - name: http
+    port: 80
+    protocol: TCP
+    targetPort: 80
+  selector:
+    name: echo
+  type: ClusterIP
+  #trafficDistribution: PreferClose
+
+-- endpointslice.yaml --
+apiVersion: discovery.k8s.io/v1
+kind: EndpointSlice
+metadata:
+  labels:
+    kubernetes.io/service-name: echo
+  name: echo-eps1
+  namespace: test
+addressType: IPv4
+endpoints:
+- addresses:
+  - 10.244.1.1
+  conditions:
+    ready: true
+    serving: true
+    terminating: false
+  nodeName: nodeport-worker
+  zone: "foo"
+  hints:
+    forZones:
+    - name: foo
+    - name: bar
+ports:
+- name: http
+  port: 80
+  protocol: TCP
+
+-- endpointslice2.yaml --
+apiVersion: discovery.k8s.io/v1
+kind: EndpointSlice
+metadata:
+  labels:
+    kubernetes.io/service-name: echo
+  name: echo-eps2
+  namespace: test
+addressType: IPv4
+endpoints:
+- addresses:
+  - 10.244.2.1
+  conditions:
+    ready: true
+    serving: true
+    terminating: false
+  nodeName: nodeport-worker
+  zone: "quux"
+  hints:
+    forZones:
+    - name: quux
+ports:
+- name: http
+  port: 80
+  protocol: TCP
+

pkg/loadbalancer/experimental/writer.go
@@ -9,6 +9,7 @@ import (
 	"iter"
 	"slices"
 	"strings"
+	"sync/atomic"
 	"text/tabwriter"
 
 	"github.com/cilium/hive/cell"
@@ -26,7 +27,9 @@ import (
 
 // Writer provides validated write access to the service load-balancing state.
 type Writer struct {
-	nodeName  string
+	nodeName string
+	nodeZone atomic.Pointer[string]
+
 	db        *statedb.DB
 	nodeAddrs statedb.Table[tables.NodeAddress]
 	svcs      statedb.RWTable[*Service]
@@ -102,10 +105,6 @@ func (w *Writer) IsEnabled() bool {
 	return w != nil
 }
 
-type WriteTxn struct {
-	statedb.WriteTxn
-}
-
 // RegisterInitializer registers a component as an initializer to the load-balancing
 // tables. This blocks pruning of data until this and all other registered initializers
 // have called the returned 'complete' function.
@@ -146,12 +145,45 @@ func (w *Writer) ReadTxn() statedb.ReadTxn {
 	return w.db.ReadTxn()
 }
 
+type WriteTxn struct {
+	statedb.WriteTxn
+}
+
 // WriteTxn returns a write transaction against services & backends and other additional
 // tables to be used with the methods of [Writer]. The returned transaction MUST be
 // Abort()'ed or Commit()'ed.
 func (w *Writer) WriteTxn(extraTables ...statedb.TableMeta) WriteTxn {
 	return WriteTxn{
 		w.db.WriteTxn(w.svcs, append(extraTables, w.bes, w.fes)...),
+	}
+}
+
+func (w *Writer) updateZone(zone string) {
+	// Grab a write transaction before updating [w.nodeZone]
+	// to make sure there's no changes to the tables while we
+	// refresh the frontends.
+	txn := w.WriteTxn()
+	defer txn.Abort()
+
+	if zone == "" {
+		w.nodeZone.Store(nil)
+	} else {
+		w.nodeZone.Store(&zone)
+	}
+
+	// Refresh all frontends associated with topology-aware services
+	// as the backend selection might change.
+	updated := false
+	for fe := range w.fes.All(txn) {
+		if fe.service.TrafficDistribution == TrafficDistributionPreferClose {
+			fe = fe.Clone()
+			w.refreshFrontend(txn, fe)
+			w.fes.Insert(txn, fe)
+			updated = true
+		}
+	}
+	if updated {
+		txn.Commit()
 	}
 }
 
@@ -327,6 +359,15 @@ func (w *Writer) DefaultSelectBackends(txn statedb.ReadTxn, tbl statedb.Table[*B
 			instance := be.GetInstance(serviceName)
 			if onlyLocal && len(instance.NodeName) != 0 && instance.NodeName != w.nodeName {
 				continue
+			}
+			if fe.RedirectTo == nil && fe.service.TrafficDistribution == TrafficDistributionPreferClose {
+				thisZone := w.nodeZone.Load()
+				if len(instance.ForZones) > 0 && thisZone != nil {
+					// Topology-aware routing is enabled. Only use this backend if it is selected for this zone.
+					if !slices.Contains(instance.ForZones, *thisZone) {
+						continue
+					}
+				}
 			}
 			if fe.PortName != "" {
 				// A backend with specific port name requested. Look up what this backend

pkg/loadbalancer/experimental/adapters.go
@@ -260,7 +260,6 @@ func clusterServiceToBackendParams(service *store.ClusterService) (beps []Backen
 				PortNames: portNames,
 				Weight:    0,
 				NodeName:  "",
-				ZoneID:    0,
 				State:     loadbalancer.BackendStateActive,
 			}
 			beps = append(beps, bep)
@@ -542,7 +541,7 @@ func (s *serviceManagerAdapter) GetDeepCopyServices() (svcs []*loadbalancer.SVC)
 				ID:         0,
 				Weight:     be.Weight,
 				NodeName:   be.NodeName,
-				ZoneID:     be.ZoneID,
+				ZoneID:     option.Config.GetZoneID(be.Zone),
 				L3n4Addr:   be.Address,
 				State:      be.State,
 				Preferred:  true,

pkg/loadbalancer/experimental/backend.go
@@ -38,7 +38,10 @@ type BackendParams struct {
 	NodeName string
 
 	// Zone where backend is located.
-	ZoneID uint8
+	Zone string
+
+	// ForZones where this backend should be consumed in
+	ForZones []string
 
 	// Source of the backend.
 	Source source.Source

pkg/loadbalancer/experimental/benchmark/benchmark.go
@@ -73,6 +73,7 @@ func RunBenchmark(testSize int, iterations int, loglevel slog.Level, validate bo
 			Log:    log,
 			Pinned: false,
 			Cfg: experimental.ExternalConfig{
+				ZoneMapper: &option.DaemonConfig{},
 				LBMapsConfig: experimental.LBMapsConfig{
 					MaxSockRevNatMapEntries:  3 * testSize,
 					ServiceMapMaxEntries:     3 * testSize,
@@ -522,6 +523,7 @@ func testHive(maps experimental.LBMaps,
 	bo **experimental.BPFOps,
 ) *hive.Hive {
 	extConfig := experimental.ExternalConfig{
+		ZoneMapper:        &option.DaemonConfig{},
 		EnableIPv4:        true,
 		EnableIPv6:        true,
 		ExternalClusterIP: false,

pkg/loadbalancer/experimental/bpf_reconciler.go
@@ -1044,13 +1044,13 @@ func (ops *BPFOps) upsertBackend(id loadbalancer.BackendID, be *BackendParams) (
 
 	if be.Address.AddrCluster.Is6() {
 		lbbe, err = lbmap.NewBackend6V3(id, be.Address.AddrCluster, be.Address.Port, proto,
-			be.State, be.ZoneID)
+			be.State, ops.cfg.GetZoneID(be.Zone))
 		if err != nil {
 			return err
 		}
 	} else {
 		lbbe, err = lbmap.NewBackend4V3(id, be.Address.AddrCluster, be.Address.Port, proto,
-			be.State, be.ZoneID)
+			be.State, ops.cfg.GetZoneID(be.Zone))
 		if err != nil {
 			return err
 		}

pkg/loadbalancer/experimental/bpf_reconciler_test.go
@@ -101,7 +101,6 @@ func newTestBackend(addr loadbalancer.L3n4Addr, state loadbalancer.BackendState)
 			BackendParams{
 				Address:   addr,
 				NodeName:  "",
-				ZoneID:    0,
 				PortNames: nil,
 				Weight:    0,
 				State:     state,
@@ -951,6 +950,7 @@ func TestBPFOps(t *testing.T) {
 
 	// Enable features.
 	extCfg := ExternalConfig{
+		ZoneMapper: &option.DaemonConfig{},
 		LBMapsConfig: LBMapsConfig{
 			MaxSockRevNatMapEntries:  1000,
 			ServiceMapMaxEntries:     1000,

pkg/loadbalancer/experimental/cell.go
@@ -48,6 +48,9 @@ var Cell = cell.Module(
 	// the node addresses change.
 	cell.Invoke(registerNodePortAddressReconciler),
 
+	// Register a background job to watch for node zone label changes.
+	cell.Invoke(registerNodeZoneWatcher),
+
 	// Replace the [k8s.ServiceCacheReader] and [service.ServiceReader] if this
 	// implementation is enabled.
 	cell.Provide(newAdapters),

commit a5819f1f9de6eec624704f8fd9e7148b5e577dec

loadbalancer: Fix topology-aware routing safe-guards
Cilium's topology-aware implementation did not correctly implement
the following safe guards [1]:

4. If no fitting backend found for current zone us backends from all zones
5. Use all backends if one or more endpoints did not have zone hints

It did correctly implement:
(1. insufficient endpoints (endpointslice controller responsibility)
(2. unbalanced allocation (endpointslice controller responsibility)
3. Use all backends if local node missing zone label

This is fixed by doing an additional iteration over the backends to check
4. and 5. before trying to apply the zone hints.

[1]: https://kubernetes.io/docs/concepts/services-networking/topology-aware-routing/#enabling-topology-aware-routing.

Fixes: e35c099d60bf ("experimental: Implement support for topology-aware routing")
Fixes: #41022
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/loadbalancer/writer/writer.go
@@ -389,6 +389,31 @@ func (w *Writer) DefaultSelectBackends(bes iter.Seq2[loadbalancer.BackendParams,
 		}
 	}
 
+	// Check whether the [BackendParams.ForZones] hints should be consulted when
+	// selecting a backend.
+	checkZoneHints := false
+	thisZone := w.nodeZone.Load()
+	if w.config.EnableServiceTopology &&
+		thisZone != nil &&
+		fe != nil && fe.RedirectTo == nil &&
+		fe.Service.TrafficDistribution == loadbalancer.TrafficDistributionPreferClose {
+		// Topology-aware routing enabled. See if we can find any backends fitting
+		// for our zone. If we don't find any we fall back to default behaviour.
+		// https://kubernetes.io/docs/concepts/services-networking/topology-aware-routing/#safeguards
+		candidatesFound, missingHints := false, false
+		for be := range bes {
+			if be.Zone != nil && len(be.Zone.ForZones) > 0 {
+				if !candidatesFound && slices.Contains(be.Zone.ForZones, *thisZone) {
+					candidatesFound = true
+				}
+			} else {
+				missingHints = true
+				break
+			}
+		}
+		checkZoneHints = candidatesFound && !missingHints
+	}
+
 	return func(yield func(loadbalancer.BackendParams, statedb.Revision) bool) {
 		for be, rev := range bes {
 			if fe != nil && fe.Address.Protocol() != be.Address.Protocol() {
@@ -409,18 +434,10 @@ func (w *Writer) DefaultSelectBackends(bes iter.Seq2[loadbalancer.BackendParams,
 					continue
 				}
 			}
+			if checkZoneHints && !slices.Contains(be.Zone.ForZones, *thisZone) {
+				continue
+			}
 			if fe != nil {
-				if w.config.EnableServiceTopology &&
-					fe.RedirectTo == nil &&
-					fe.Service.TrafficDistribution == loadbalancer.TrafficDistributionPreferClose {
-					thisZone := w.nodeZone.Load()
-					if be.Zone != nil && len(be.Zone.ForZones) > 0 && thisZone != nil {
-						// Topology-aware routing is enabled. Only use this backend if it is selected for this zone.
-						if !slices.Contains(be.Zone.ForZones, *thisZone) {
-							continue
-						}
-					}
-				}
 				if fe.PortName != "" {
 					// A backend with specific port name requested. Look up what this backend
 					// is called for this service.

pkg/loadbalancer/tests/testdata/topology-aware.txtar
@@ -16,29 +16,43 @@ db/cmp services services.table
 k8s/add endpointslice.yaml endpointslice2.yaml
 
 # With zone set to 'foo' and topology-awareness enabled, we should
-# only select backen 10.244.1.1.
+# only select backend 10.244.1.1 which has 'foo' in its ForZones.
 db/cmp backends backends.table 
-db/cmp frontends frontends.table
+db/cmp frontends frontends-foo.table
 
 # Move the node to a different zone which won't match ForZones.
+# Now we'll fall back to selecting all backends.
+# (safeguard #5 at https://kubernetes.io/docs/concepts/services-networking/topology-aware-routing/#safeguards)
 test/set-node-labels topology.kubernetes.io/zone=baz
-db/cmp frontends frontends-no-backends.table
+db/cmp frontends frontends-all.table
 
 # Move to another zone which isn't backend's own zone but mentioned in
 # ForZones.
 test/set-node-labels topology.kubernetes.io/zone=bar
-db/cmp frontends frontends.table
+db/cmp frontends frontends-foo.table
 
-# Remove the topology-mode annotation. Both backends should now be selected.
+# Remove the zone hints from one of the endpointslices. We'll again
+# fall back to selecting all backends.
+# (safeguard #4 at https://kubernetes.io/docs/concepts/services-networking/topology-aware-routing/#safeguards)
+cp endpointslice2.yaml endpointslice2-no-hints.yaml
+sed 'forZones:' 'notForZones:' endpointslice2-no-hints.yaml
+k8s/update endpointslice2-no-hints.yaml
+db/cmp frontends frontends-all.table
+
+# Revert back to selecting backends with foo zone.
+k8s/update endpointslice2.yaml
+db/cmp frontends frontends-foo.table
+
+# Remove the topology-mode annotation. All backends should now be selected.
 sed 'topology-mode' 'topology-nope' service.yaml
 k8s/update service.yaml
-db/cmp frontends frontends-no-topo.table
+db/cmp frontends frontends-all.table
 
 # Set 'trafficDistribution' and check that this results in identical behavior
 # to topology-mode.
 sed '#trafficD' 'trafficD' service.yaml
 k8s/update service.yaml
-db/cmp frontends frontends.table
+db/cmp frontends frontends-foo.table
 
 #####
 
@@ -46,15 +60,15 @@ db/cmp frontends frontends.table
 Name        Source   PortNames  TrafficPolicy  Flags
 test/echo   k8s      http=80    Cluster        TrafficDistribution=PreferClose
 
--- frontends.table --
+-- frontends-foo.table --
 Address               Type        ServiceName   PortName   Status  Backends
 10.96.50.104:80/TCP   ClusterIP   test/echo     http       Done    10.244.1.1:80/TCP
 
--- frontends-no-backends.table --
+-- frontends-none.table --
 Address               Type        ServiceName   PortName   Status   Backends
 10.96.50.104:80/TCP   ClusterIP   test/echo     http       Done
 
--- frontends-no-topo.table --
+-- frontends-all.table --
 Address               Type        ServiceName   PortName   Status  Backends
 10.96.50.104:80/TCP   ClusterIP   test/echo     http       Done    10.244.1.1:80/TCP, 10.244.2.1:80/TCP
 