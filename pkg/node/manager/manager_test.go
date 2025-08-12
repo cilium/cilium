d2dfc6008a60bec0747d05ac5e1848999106dfc7
Charlie Kenney <charles.kenney@isovalent.com>
2025-08-11 22:32:16 -0400

    node/manager: only decrement num_nodes_all for nodes we've added
    DeleteNodes is called with synthetic delete events for nodes that have
    been deleted between NodeManager restarts. Now, we check that the given
    node is not a stale node being pruned (that we haven't accounted for)
    before we decrement num_nodes_all.
    
    Signed-off-by: Charlie Kenney <charles.kenney@isovalent.com>

--- pkg/node/manager/manager_test.go
+++ pkg/node/manager/manager_test.go
@@ -1481,6 +1481,9 @@ func TestNodesStartupPruning(t *testing.T) {
 		}
 
 		checkNodeFileMatches(t, stateDir, c1Node1, c1Node2, c2Node1)
+
+		assert.Equal(t, float64(2), mngr.metrics.EventsReceived.WithLabelValues("delete", string(source.Restored)).Get())
+		assert.Equal(t, float64(3), mngr.metrics.NumNodes.Get())
 	})
 
 	t.Run("meshed nodes synced first", func(t *testing.T) {
@@ -1524,5 +1527,8 @@ func TestNodesStartupPruning(t *testing.T) {
 		}
 
 		checkNodeFileMatches(t, stateDir, c1Node1, c1Node2, c2Node1)
+
+		assert.Equal(t, float64(2), mngr.metrics.EventsReceived.WithLabelValues("delete", string(source.Restored)).Get())
+		assert.Equal(t, float64(3), mngr.metrics.NumNodes.Get())
 	})
 }
