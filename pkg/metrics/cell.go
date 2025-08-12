cilium/cilium

commit 0b3672fa1c01a0a295e89e1d6b7a8c5610fef5c6

pkg/metrics: prepare *metrics.Registry for use by operator.
1) Add ctor for *metrics.Registry type without agent metrics.
2) Expose underlying registry gather functionality needed by
	operator metrics command.
3) Add OperatorCell which provides minimal metrics infra for
	Operator to use:
	i) Allow operator to initialize lifecycle hooks manually
		to allow us to manually gather and register operator
		metrics.

*metrics.Registry is used by the agent hive to register metrics.
This does a lot of agent specific stuff when initializing.
In order to make this more generic, we create a agent specific
constructor and a general one that does not handle any initialization.

This allows us to create a OperatorCell that provides the registry
and allows the operator metrics code to initialize its metrics
manually.

We will use this to replace the operator metric init such
that we're basing it on the *Registry type.

The benefits of this are that this will allow us to include
the metrics commands types which will be very useful in conjunction
with recent commits to add the `shell` command to the operator binary.

Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

pkg/metrics/cell.go
@@ -17,7 +17,8 @@ import (
 // Cell provides metrics registry and the 'metrics*' shell commands.
 var Cell = cell.Module("metrics", "Metrics",
 	// Provide registry to hive, but also invoke if case no cells decide to use as dependency
-	cell.Provide(NewRegistry),
+	cell.Provide(NewAgentRegistry),
+	Metric(NewLegacyMetrics),
 	cell.Config(defaultRegistryConfig),
 	cell.Config(defaultSamplerConfig),
 	cell.Provide(
@@ -31,7 +32,6 @@ var Cell = cell.Module("metrics", "Metrics",
 // and without pulling in the legacy metrics.
 var AgentCell = cell.Group(
 	Cell,
-	Metric(NewLegacyMetrics),
 	cell.Invoke(
 		func(logger *slog.Logger, reg *Registry) {
 			// Register the agent status and BPF metrics.
@@ -49,6 +49,12 @@ var AgentCell = cell.Group(
 
 		},
 	),
+)
+
+var OperatorCell = cell.Module("operator-metrics", "Operator Metrics",
+	cell.Config(defaultSamplerConfig),
+	cell.Provide(NewRegistry),
+	cell.Provide(metricsCommands, newSampler),
 )
 
 // Metric constructs a new metric cell.

pkg/metrics/metrics_test.go
@@ -17,7 +17,7 @@ func TestGaugeWithThreshold(t *testing.T) {
 	threshold := 1.0
 	underThreshold := threshold - 0.5
 	overThreshold := threshold + 0.5
-	reg := NewRegistry(RegistryParams{
+	reg := NewAgentRegistry(RegistryParams{
 		Logger:       logger,
 		DaemonConfig: &option.DaemonConfig{},
 	})

pkg/metrics/registry.go
@@ -16,6 +16,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	dto "github.com/prometheus/client_model/go"
 	"github.com/spf13/pflag"
 
 	"github.com/cilium/cilium/pkg/lock"
@@ -70,31 +71,29 @@ type Registry struct {
 	params RegistryParams
 }
 
-func NewRegistry(params RegistryParams) *Registry {
-	reg := &Registry{
-		inner:  prometheus.NewPedanticRegistry(),
-		params: params,
-	}
+// Gather exposes metrics gather functionality, used by operator metrics command.
+func (reg *Registry) Gather() ([]*dto.MetricFamily, error) {
+	return reg.inner.Gather()
+}
 
-	reg.registerMetrics()
-
-	if params.Config.PrometheusServeAddr != "" {
+func (reg *Registry) AddServerRuntimeHooks() {
+	if reg.params.Config.PrometheusServeAddr != "" {
 		// The Handler function provides a default handler to expose metrics
 		// via an HTTP server. "/metrics" is the usual endpoint for that.
 		mux := http.NewServeMux()
-		mux.Handle("/metrics", promhttp.HandlerFor(reg.inner, promhttp.HandlerOpts{}))
+		mux.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
 		srv := http.Server{
-			Addr:    params.Config.PrometheusServeAddr,
+			Addr:    reg.params.Config.PrometheusServeAddr,
 			Handler: mux,
 		}
 
-		params.Lifecycle.Append(cell.Hook{
+		reg.params.Lifecycle.Append(cell.Hook{
 			OnStart: func(hc cell.HookContext) error {
 				go func() {
-					params.Logger.Info("Serving prometheus metrics", logfields.Address, params.Config.PrometheusServeAddr)
+					reg.params.Logger.Info("Serving prometheus metrics", logfields.Address, reg.params.Config.PrometheusServeAddr)
 					err := srv.ListenAndServe()
 					if err != nil && !errors.Is(err, http.ErrServerClosed) {
-						params.Shutdowner.Shutdown(hive.ShutdownWithError(err))
+						reg.params.Shutdowner.Shutdown(hive.ShutdownWithError(err))
 					}
 				}()
 				return nil
@@ -104,6 +103,29 @@ func NewRegistry(params RegistryParams) *Registry {
 			},
 		})
 	}
+}
+
+// NewRegistry constructs a new registry that is not initialized with
+// hive/legacy metrics and has registered its runtime hooks yet.
+func NewRegistry(params RegistryParams) *Registry {
+	reg := &Registry{
+		params: params,
+		inner:  prometheus.NewPedanticRegistry(),
+	}
+	return reg
+}
+
+func NewAgentRegistry(params RegistryParams) *Registry {
+	reg := &Registry{
+		params: params,
+	}
+
+	reg.registerMetrics()
+
+	// Resolve the global registry variable for as long as we still have global functions
+	registryResolver.Resolve(reg)
+
+	reg.AddServerRuntimeHooks()
 
 	return reg
 }
@@ -125,6 +147,7 @@ var goCustomCollectorsRX = regexp.MustCompile(`^/sched/latencies:seconds`)
 
 // Reinitialize creates a new internal registry and re-registers metrics to it.
 func (r *Registry) registerMetrics() {
+	r.inner = prometheus.NewPedanticRegistry()
 	// Default metrics which can't be disabled.
 	r.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{Namespace: Namespace}))
 	r.MustRegister(collectors.NewGoCollector(

commit 7013bf22e1248b1f2fc93944f3983a3782f72cee

metrics: Move NewLegacyMetrics into AgentCell
This should have never moved into 'Cell' as the whole point was to keep the legacy
metrics and global variables out of 'Cell' so tests can use it.

Fixes: 0b3672fa1c01 ("pkg/metrics: prepare *metrics.Registry for use by operator.")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/metrics/cell.go
@@ -18,7 +18,6 @@ import (
 var Cell = cell.Module("metrics", "Metrics",
 	// Provide registry to hive, but also invoke if case no cells decide to use as dependency
 	cell.Provide(NewAgentRegistry),
-	Metric(NewLegacyMetrics),
 	cell.Config(defaultRegistryConfig),
 	cell.Config(defaultSamplerConfig),
 	cell.Provide(
@@ -32,6 +31,7 @@ var Cell = cell.Module("metrics", "Metrics",
 // and without pulling in the legacy metrics.
 var AgentCell = cell.Group(
 	Cell,
+	Metric(NewLegacyMetrics),
 	cell.Invoke(
 		func(logger *slog.Logger, reg *Registry) {
 			// Register the agent status and BPF metrics.