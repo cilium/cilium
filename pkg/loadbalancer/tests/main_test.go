6f2545bcd1ef40415bdf2550f7b10b4838246a21
Jussi Maki <jussi@isovalent.com>
2025-08-12 13:48:06 +0200

    loadbalancer: Update goleak ignores for client-go workqueue
    This had changed when client-go was updated and this was causing
    false positive goroutine leak failures.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/loadbalancer/tests/main_test.go
+++ pkg/loadbalancer/tests/main_test.go
@@ -17,8 +17,8 @@ func TestMain(m *testing.M) {
 		goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
 		goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
 
-		// Unfortunately we don't have a way for waiting for the DelayingWorkQueue's background goroutine
+		// Unfortunately we don't have a way for waiting for the workqueue's background goroutine
 		// to exit (used by pkg/k8s/resource), so we'll just need to ignore it.
-		goleak.IgnoreTopFunction("k8s.io/client-go/util/workqueue.(*delayingType[...]).waitingLoop"),
+		goleak.IgnoreTopFunction("k8s.io/client-go/util/workqueue.(*Typed[...]).updateUnfinishedWorkLoop"),
 	)
 }
