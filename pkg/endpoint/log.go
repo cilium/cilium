cilium/cilium

commit baeb61facde9c05f6365c360a23cfad52d4454b8

endpoint: Add DebugPolicy option
Add endpoint DebugPolicy option that, if enabled, logs endpoint policy
map update details to /var/run/cilium/state/endpoint-policy.log.

The new DebugPolicy option is enabled if the new flag
--debug-verbose=policy is set, but can be enabled also independently
via:

  cilium endpoint config <EPID> DebugPolicy=true

Signed-off-by: Martynas Pumputis <m@lambda.lt>
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/endpoint/log.go
@@ -15,17 +15,27 @@
 package endpoint
 
 import (
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
 	"sync/atomic"
 	"unsafe"
 
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-
 	"github.com/cilium/cilium/pkg/option"
+
 	"github.com/sirupsen/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-var log = logging.DefaultLogger.WithField(logfields.LogSubsys, subsystem)
+var (
+	log       = logging.DefaultLogger.WithField(logfields.LogSubsys, subsystem)
+	policyLog = logrus.New()
+
+	policyLogOnce sync.Once
+)
 
 const (
 	subsystem = "endpoint"
@@ -38,6 +48,20 @@ const (
 func (e *Endpoint) getLogger() *logrus.Entry {
 	v := atomic.LoadPointer(&e.logger)
 	return (*logrus.Entry)(v)
+}
+
+// getPolicyLogger returns a logger to be used for policy update debugging, or nil,
+// if not configured.
+func (e *Endpoint) getPolicyLogger() *logrus.Entry {
+	v := atomic.LoadPointer(&e.policyLogger)
+	return (*logrus.Entry)(v)
+}
+
+// policyDebug logs the 'msg' with 'fields' if policy debug logging is enabled.
+func (e *Endpoint) policyDebug(fields logrus.Fields, msg string) {
+	if dbgLog := e.getPolicyLogger(); dbgLog != nil {
+		dbgLog.WithFields(fields).Debug(msg)
+	}
 }
 
 // Logger returns a logrus object with EndpointID, containerID and the Endpoint
@@ -54,8 +78,12 @@ func (e *Endpoint) Logger(subsystem string) *logrus.Entry {
 // create a custom Debug logger for this endpoint when the option on it is set.
 // If fields is not nil only the those specific fields will be updated in the
 // endpoint's logger, otherwise a full update of those fields is executed.
-// Note: You must hold Endpoint.Mutex for reading if fields is nil.
+//
+// Note: You must hold Endpoint.mutex.Lock() to synchronize logger pointer
+// updates if the endpoint is already exposed. Callers that create new
+// endopoints do not need locks to call this.
 func (e *Endpoint) UpdateLogger(fields map[string]interface{}) {
+	e.updatePolicyLogger(fields)
 	v := atomic.LoadPointer(&e.logger)
 	epLogger := (*logrus.Entry)(v)
 	if fields != nil && epLogger != nil {
@@ -113,4 +141,63 @@ func (e *Endpoint) UpdateLogger(fields map[string]interface{}) {
 	}
 
 	atomic.StorePointer(&e.logger, unsafe.Pointer(l))
+}
+
+// Only to be called from UpdateLogger() above
+func (e *Endpoint) updatePolicyLogger(fields map[string]interface{}) {
+	pv := atomic.LoadPointer(&e.policyLogger)
+	policyLogger := (*logrus.Entry)(pv)
+	// e.Options check needed for unit testing.
+	if policyLogger == nil && e.Options != nil && e.Options.IsEnabled(option.DebugPolicy) {
+		policyLogOnce.Do(func() {
+			maxSize := 10 // 10 MB
+			if ms := os.Getenv("CILIUM_DBG_POLICY_LOG_MAX_SIZE"); ms != "" {
+				if ms, err := strconv.Atoi(ms); err == nil {
+					maxSize = ms
+				}
+			}
+			maxBackups := 3
+			if mb := os.Getenv("CILIUM_DBG_POLICY_LOG_MAX_BACKUPS"); mb != "" {
+				if mb, err := strconv.Atoi(mb); err == nil {
+					maxBackups = mb
+				}
+			}
+			lumberjackLogger := &lumberjack.Logger{
+				Filename:   filepath.Join(option.Config.StateDir, "endpoint-policy.log"),
+				MaxSize:    maxSize,
+				MaxBackups: maxBackups,
+				MaxAge:     28, // days
+				LocalTime:  true,
+				Compress:   true,
+			}
+			policyLog.SetOutput(lumberjackLogger)
+			policyLog.SetLevel(logrus.DebugLevel)
+		})
+		policyLogger = logrus.NewEntry(policyLog)
+	}
+	if policyLogger == nil || e.Options == nil {
+		return
+	}
+
+	if !e.Options.IsEnabled(option.DebugPolicy) {
+		policyLogger = nil
+	} else if fields != nil {
+		policyLogger = policyLogger.WithFields(fields)
+	} else {
+		policyLogger = policyLogger.WithFields(logrus.Fields{
+			logfields.LogSubsys:              subsystem,
+			logfields.EndpointID:             e.ID,
+			logfields.ContainerID:            e.getShortContainerID(),
+			logfields.DatapathPolicyRevision: e.policyRevision,
+			logfields.DesiredPolicyRevision:  e.nextPolicyRevision,
+			logfields.IPv4:                   e.IPv4.String(),
+			logfields.IPv6:                   e.IPv6.String(),
+			logfields.K8sPodName:             e.getK8sNamespaceAndPodName(),
+		})
+
+		if e.SecurityIdentity != nil {
+			policyLogger = policyLogger.WithField(logfields.Identity, e.SecurityIdentity.ID.StringID())
+		}
+	}
+	atomic.StorePointer(&e.policyLogger, unsafe.Pointer(policyLogger))
 }

cilium/cmd/helpers_test.go
@@ -101,6 +101,7 @@ func (s *CMDHelpersSuite) TestExpandNestedJSON(c *C) {
         "ConntrackLocal": "Disabled",
         "Debug": "Enabled",
         "DebugLB": "Enabled",
+        "DebugPolicy": "Enabled",
         "DropNotification": "Enabled",
         "MonitorAggregationLevel": "None",
         "NAT46": "Disabled",
@@ -290,6 +291,7 @@ func (s *CMDHelpersSuite) TestExpandNestedJSON(c *C) {
           "ConntrackLocal": "Disabled",
           "Debug": "Enabled",
           "DebugLB": "Enabled",
+          "DebugPolicy": "Enabled",
           "DropNotification": "Enabled",
           "MonitorAggregationLevel": "None",
           "NAT46": "Disabled",
@@ -313,6 +315,7 @@ func (s *CMDHelpersSuite) TestExpandNestedJSON(c *C) {
         "ConntrackLocal": "Disabled",
         "Debug": "Enabled",
         "DebugLB": "Enabled",
+        "DebugPolicy": "Enabled",
         "DropNotification": "Enabled",
         "MonitorAggregationLevel": "None",
         "NAT46": "Disabled",
@@ -532,6 +535,7 @@ func (s *CMDHelpersSuite) TestExpandNestedJSON(c *C) {
           "ConntrackLocal": "Disabled",
           "Debug": "Enabled",
           "DebugLB": "Enabled",
+          "DebugPolicy": "Enabled",
           "DropNotification": "Enabled",
           "MonitorAggregationLevel": "None",
           "NAT46": "Disabled",

daemon/cmd/daemon_main.go
@@ -88,6 +88,7 @@ const (
 	argDebugVerboseKvstore  = "kvstore"
 	argDebugVerboseEnvoy    = "envoy"
 	argDebugVerboseDatapath = "datapath"
+	argDebugVerbosePolicy   = "policy"
 
 	apiTimeout   = 60 * time.Second
 	daemonSubsys = "daemon"
@@ -962,6 +963,8 @@ func initEnv(cmd *cobra.Command) {
 		case argDebugVerboseDatapath:
 			log.Debugf("Enabling datapath debug messages")
 			debugDatapath = true
+		case argDebugVerbosePolicy:
+			option.Config.Opts.SetBool(option.DebugPolicy, true)
 		default:
 			log.Warningf("Unknown verbose debug group: %s", grp)
 		}

pkg/endpoint/bpf.go
@@ -808,6 +808,7 @@ func (e *Endpoint) runPreCompilationSteps(regenContext *regenerationContext) (he
 		}
 		// Clean up map contents
 		e.getLogger().Debug("flushing old PolicyMap")
+		e.policyDebug(nil, "runPreCompilationSteps flushing old PolicyMap")
 		err = e.policyMap.DeleteAll()
 		if err != nil {
 			return false, err
@@ -1096,10 +1097,10 @@ func (e *Endpoint) deletePolicyKey(keyToDelete policy.Key, incremental bool, had
 		return false
 	}
 
-	if hadProxy != nil {
-		if entry, ok := e.realizedPolicy.PolicyMapState[keyToDelete]; ok && entry.ProxyPort != 0 {
-			*hadProxy = true
-		}
+	var entry policy.MapStateEntry
+	var ok bool
+	if entry, ok = e.realizedPolicy.PolicyMapState[keyToDelete]; ok && entry.ProxyPort != 0 && hadProxy != nil {
+		*hadProxy = true
 	}
 
 	// Operation was successful, remove from realized state.
@@ -1109,6 +1110,12 @@ func (e *Endpoint) deletePolicyKey(keyToDelete policy.Key, incremental bool, had
 	if incremental && e.desiredPolicy != e.realizedPolicy {
 		delete(e.desiredPolicy.PolicyMapState, keyToDelete)
 	}
+
+	e.policyDebug(logrus.Fields{
+		logfields.BPFMapKey:   keyToDelete,
+		logfields.BPFMapValue: entry,
+		"incremental":         incremental,
+	}, "deletePolicyKey")
 
 	return true
 }
@@ -1144,6 +1151,11 @@ func (e *Endpoint) addPolicyKey(keyToAdd policy.Key, entry policy.MapStateEntry,
 		e.desiredPolicy.PolicyMapState[keyToAdd] = entry
 	}
 
+	e.policyDebug(logrus.Fields{
+		logfields.BPFMapKey:   keyToAdd,
+		logfields.BPFMapValue: entry,
+		"incremental":         incremental,
+	}, "addPolicyKey")
 	return true
 }
 
@@ -1156,6 +1168,8 @@ func (e *Endpoint) ApplyPolicyMapChanges(proxyWaitGroup *completion.WaitGroup) e
 		return err
 	}
 	defer e.unlock()
+
+	e.policyDebug(nil, "ApplyPolicyMapChanges")
 
 	proxyChanges, err := e.applyPolicyMapChanges()
 	if err != nil {
@@ -1177,6 +1191,8 @@ func (e *Endpoint) ApplyPolicyMapChanges(proxyWaitGroup *completion.WaitGroup) e
 // collected on the desired policy.
 func (e *Endpoint) applyPolicyMapChanges() (proxyChanges bool, err error) {
 	errors := 0
+
+	e.policyDebug(nil, "applyPolicyMapChanges")
 
 	//  Note that after successful endpoint regeneration the
 	//  desired and realized policies are the same pointer. During
@@ -1222,6 +1238,10 @@ func (e *Endpoint) applyPolicyMapChanges() (proxyChanges bool, err error) {
 // difference between the realized and desired policy state without
 // dumping the bpf policy map.
 func (e *Endpoint) syncPolicyMap() error {
+	e.policyDebug(logrus.Fields{
+		"policyRealized": e.realizedPolicy == e.desiredPolicy,
+	}, "syncPolicyMap")
+
 	// Nothing to do if the desired policy is already fully realized.
 	if e.realizedPolicy != e.desiredPolicy {
 		errors := 0
@@ -1243,7 +1263,7 @@ func (e *Endpoint) syncPolicyMap() error {
 		}
 
 		if errors > 0 {
-			return fmt.Errorf("syncPolicyMapDelta failed")
+			return fmt.Errorf("syncPolicyMap failed")
 		}
 	}
 
@@ -1330,6 +1350,11 @@ func (e *Endpoint) syncPolicyMapWithDump() error {
 	}
 
 	errors := 0
+
+	// Log full policy map for every dump
+	e.policyDebug(logrus.Fields{
+		"dumpedPolicyMap": currentMapContents,
+	}, "syncPolicyMapWithDump")
 
 	for _, entry := range currentMapContents {
 		// Convert key to host-byte order for lookup in the desiredMapState.

pkg/endpoint/endpoint.go
@@ -126,8 +126,7 @@ type Endpoint struct {
 	// recalculated on endpoint restore.
 	createdAt time.Time
 
-	// mutex protects write operations to this endpoint structure except
-	// for the logger field which has its own mutex
+	// mutex protects write operations to this endpoint structure
 	mutex lock.RWMutex
 
 	// containerName is the name given to the endpoint by the container runtime
@@ -280,8 +279,16 @@ type Endpoint struct {
 	buildMutex lock.Mutex
 
 	// logger is a logrus object with fields set to report an endpoints information.
-	// You must hold Endpoint.Mutex to read or write it (but not to log with it).
+	// This must only be accessed with atomic.LoadPointer/StorePointer.
+	// 'mutex' must be Lock()ed to synchronize stores. No lock needs to be held
+	// when loading this pointer.
 	logger unsafe.Pointer
+
+	// policyLogger is a logrus object with fields set to report an endpoints information.
+	// This must only be accessed with atomic LoadPointer/StorePointer.
+	// 'mutex' must be Lock()ed to synchronize stores. No lock needs to be held
+	// when loading this pointer.
+	policyLogger unsafe.Pointer
 
 	// controllers is the list of async controllers syncing the endpoint to
 	// other resources

pkg/endpoint/log_test.go
@@ -1,0 +1,72 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package endpoint
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/policy"
+	"github.com/cilium/cilium/pkg/testutils/allocator"
+
+	"github.com/sirupsen/logrus"
+	. "gopkg.in/check.v1"
+)
+
+func (s *EndpointSuite) TestPolicyLog(c *C) {
+	ep := NewEndpointWithState(&DummyOwner{repo: policy.NewPolicyRepository(nil, nil)}, nil, &allocator.FakeIdentityAllocator{}, 12345, StateReady)
+
+	// Initially nil
+	policyLogger := ep.getPolicyLogger()
+	c.Assert(policyLogger, IsNil)
+
+	// Enable DebugPolicy option
+	ep.Options.SetValidated(option.DebugPolicy, option.OptionEnabled)
+	c.Assert(ep.Options.IsEnabled(option.DebugPolicy), Equals, true)
+	ep.UpdateLogger(nil)
+	policyLogger = ep.getPolicyLogger()
+	c.Assert(policyLogger, Not(IsNil))
+	defer func() {
+		// remote created log file when we are done.
+		err := os.Remove(filepath.Join(option.Config.StateDir, "endpoint-policy.log"))
+		c.Assert(err, IsNil)
+	}()
+
+	// Test logging, policyLogger must not be nil
+	policyLogger.Info("testing policy logging")
+
+	// Test logging with integrated nil check, no fields
+	ep.policyDebug(nil, "testing policyDebug")
+	ep.policyDebug(logrus.Fields{"testField": "Test Value"}, "policyDebug with fields")
+
+	// Disable option
+	ep.Options.SetValidated(option.DebugPolicy, option.OptionDisabled)
+	c.Assert(ep.Options.IsEnabled(option.DebugPolicy), Equals, false)
+	ep.UpdateLogger(nil)
+	policyLogger = ep.getPolicyLogger()
+	c.Assert(policyLogger, IsNil)
+
+	// Verify file exists and contains the logged message
+	buf, err := ioutil.ReadFile(filepath.Join(option.Config.StateDir, "endpoint-policy.log"))
+	c.Assert(err, IsNil)
+	c.Assert(bytes.Contains(buf, []byte("testing policy logging")), Equals, true)
+	c.Assert(bytes.Contains(buf, []byte("testing policyDebug")), Equals, true)
+	c.Assert(bytes.Contains(buf, []byte("Test Value")), Equals, true)
+}

pkg/maps/policymap/policymap.go
@@ -195,6 +195,16 @@ type PolicyEntryDump struct {
 // PolicyEntriesDump is a wrapper for a slice of PolicyEntryDump
 type PolicyEntriesDump []PolicyEntryDump
 
+// String returns a string representation of PolicyEntriesDump
+func (p PolicyEntriesDump) String() string {
+	var sb strings.Builder
+	for _, entry := range p {
+		sb.WriteString(fmt.Sprintf("%20s: %s\n",
+			entry.Key.String(), entry.PolicyEntry.String()))
+	}
+	return sb.String()
+}
+
 // Less is a function used to sort PolicyEntriesDump by Policy Type
 // (Deny / Allow), TrafficDirection (Ingress / Egress) and Identity
 // (ascending order).
@@ -343,16 +353,11 @@ func (pm *PolicyMap) String() string {
 }
 
 func (pm *PolicyMap) Dump() (string, error) {
-	var sb strings.Builder
 	entries, err := pm.DumpToSlice()
 	if err != nil {
 		return "", err
 	}
-	for _, entry := range entries {
-		sb.WriteString(fmt.Sprintf("%20s: %s\n",
-			entry.Key.String(), entry.PolicyEntry.String()))
-	}
-	return sb.String(), nil
+	return entries.String(), nil
 }
 
 func (pm *PolicyMap) DumpToSlice() (PolicyEntriesDump, error) {

pkg/option/daemon.go
@@ -31,6 +31,7 @@ var (
 		Conntrack:           &specConntrack,
 		Debug:               &specDebug,
 		DebugLB:             &specDebugLB,
+		DebugPolicy:         &specDebugPolicy,
 		DropNotify:          &specDropNotify,
 		TraceNotify:         &specTraceNotify,
 		PolicyVerdictNotify: &specPolicyVerdictNotify,

commit 70e1d375cf0c7ff07f7e63a04b4d8213a630ff88

endpoint: fix file descriptor leak in policy debug logging
When multiple endpoints have policy debug logging enabled, they all create
separate lumberjack.Logger instances that write to the same file path
(endpoint-policy.log). This causes file descriptor leaks because:

1. Each endpoint creates its own lumberjack logger with the same filename

2. When one logger rotates the file, other loggers still hold open file
   descriptors to the old (renamed) file

3. The old file appears as 'deleted' in lsof but continues consuming
   file descriptors

Fix this by making each endpoint use a unique log filename that includes
the endpoint ID (endpoint-{id}-policy.log). This ensures each endpoint
manages its own log file independently without interfering with others.

Fixes: baeb61facde9 ("endpoint: Add DebugPolicy option")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/log.go
@@ -5,6 +5,7 @@ package endpoint
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -156,7 +157,7 @@ func (e *Endpoint) updatePolicyLogger(fields map[string]any) {
 			}
 		}
 		lumberjackLogger := &lumberjack.Logger{
-			Filename:   filepath.Join(option.Config.StateDir, "endpoint-policy.log"),
+			Filename:   filepath.Join(option.Config.StateDir, fmt.Sprintf("endpoint-%d-policy.log", e.GetID())),
 			MaxSize:    maxSize,
 			MaxBackups: maxBackups,
 			MaxAge:     28, // days