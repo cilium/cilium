cilium/cilium

commit 8f02631f230cb6fdd38315259689a0ec87f3827a

contrib: Reuse local go cache in builder
The builder script here was ignoring local go build caches even when
available. The result is that attempting to update the command reference
in the docs via 'make -C Documentation update-cmdref' could take dozens
of minutes because it would recreate all Go caches each time you run the
command.

CI should already be specifying the cache directories, but for all other
cases (such as running the script locally), attempt to reuse locally
available caches.

Signed-off-by: Joe Stringer <joe@cilium.io>

contrib/scripts/builder.sh
@@ -8,18 +8,20 @@ CILIUM_BUILDER_IMAGE=$(cat images/cilium/Dockerfile | grep '^ARG CILIUM_BUILDER_
 RUN_AS_NONROOT="${RUN_AS_NONROOT:-1}"
 
 USER_OPTION=""
+USER_PATH="/root"
 
 if [ -n "${RUN_AS_NONROOT:-}" ]; then
     USER_OPTION="--user $(id -u):$(id -g)"
+    USER_PATH="$HOME"
 fi
 
-MOUNT_GOCACHE_DIR=""
+MOUNT_GOCACHE_DIR="-v $(go env GOCACHE):$(go env GOCACHE)"
 
 if [ -n "${BUILDER_GOCACHE_DIR:-}" ]; then
-    MOUNT_GOCACHE_DIR="-v ${BUILDER_GOCACHE_DIR}:/root/.cache/go-build"
+    MOUNT_GOCACHE_DIR="-v ${BUILDER_GOCACHE_DIR}:${USER_PATH}/.cache/go-build"
 fi
 
-MOUNT_GOMODCACHE_DIR=""
+MOUNT_GOMODCACHE_DIR="-v $(go env GOMODCACHE):$(go env GOMODCACHE)"
 
 if [ -n "${BUILDER_GOMODCACHE_DIR:-}" ]; then
     MOUNT_GOMODCACHE_DIR="-v ${BUILDER_GOMODCACHE_DIR}:/go/pkg/mod"
@@ -27,8 +29,10 @@ fi
 
 MOUNT_CCACHE_DIR=""
 
-if [ -n "${BUILDER_CCACHE_DIR:-}" ]; then
-    MOUNT_CCACHE_DIR="-v ${BUILDER_CCACHE_DIR}:/root/.ccache"
+if [ -z "${BUILDER_CCACHE_DIR:-}" ]; then
+    MOUNT_CCACHE_DIR="-v ${USER_PATH}/.ccache:${USER_PATH}/.ccache"
+else
+    MOUNT_CCACHE_DIR="-v ${BUILDER_CCACHE_DIR}:${USER_PATH}/.ccache"
 fi
 
 docker run --rm \

commit 1db37b1d6297baab604886b1a3ad43713b0c7de3

contrib: Fix wrong mount paths in builder.sh
Volume mounts for local runs of builder.sh used wrong paths, both inside
and outside the container, which effectively invalidated the idea of
sharing the caches with the host.

1. The mountpoints inside the container should use the paths configured
for tools inside the container. Instead, the host caches were mounted
at the same paths inside the container, where the container tools
couldn't find them.

2. The ccache directory ~/.ccache is a non-standard location. Ccache
uses $CCACHE_DIR, or the one set in its config file, or falls back to
$XDG_CACHE_DIR/ccache, with some more shenanigans on macOS. Two things
to make it work: a) query the host ccache (if installed) for the actual
cache dir; b) use the correct path inside the container.'

3. The home directory inside the container is either /root or
/home/ubuntu. Set $USER_PATH correctly, instead of using the host $HOME
and mounting the caches to a wrong location in the container. It also
obsoletes the macOS workaround that didn't have a chance to work,
because /tmp/.ccache didn't exist on the host and wasn't used by the
container.

Fixes: 8f02631f230c ("contrib: Reuse local go cache in builder")
Fixes: d582b6a08e56 ("contrib: Use tmp directory for macOS builder")
Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

contrib/scripts/builder.sh
@@ -11,18 +11,14 @@ GO="$(which go 2> /dev/null || :)"
 USER_OPTION=()
 USER_PATH="/root"
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    USER_PATH="/tmp"
-fi
-
 if [ -z "${RUN_AS_ROOT:-}" ]; then
 	USER_OPTION=(--user "$(id -u):$(id -g)")
-    USER_PATH="$HOME"
+    USER_PATH="/home/ubuntu"
 fi
 
 MOUNT_GOCACHE_DIR=()
 if [ -n "${GO}" ]; then
-	MOUNT_GOCACHE_DIR=(-v "$(go env GOCACHE):$(go env GOCACHE)")
+	MOUNT_GOCACHE_DIR=(-v "$(go env GOCACHE):$USER_PATH/.cache/go-build")
 fi
 
 if [ -n "${BUILDER_GOCACHE_DIR:-}" ]; then
@@ -31,7 +27,7 @@ fi
 
 MOUNT_GOMODCACHE_DIR=()
 if [ -n "${GO}" ]; then
-	MOUNT_GOMODCACHE_DIR=(-v "$(go env GOMODCACHE):$(go env GOMODCACHE)")
+	MOUNT_GOMODCACHE_DIR=(-v "$(go env GOMODCACHE):/go/pkg/mod")
 fi
 
 if [ -n "${BUILDER_GOMODCACHE_DIR:-}" ]; then
@@ -39,10 +35,11 @@ if [ -n "${BUILDER_GOMODCACHE_DIR:-}" ]; then
 fi
 
 MOUNT_CCACHE_DIR=()
-if [ -z "${BUILDER_CCACHE_DIR:-}" ]; then
-	MOUNT_CCACHE_DIR=(-v "${USER_PATH}/.ccache:${USER_PATH}/.ccache")
-else
-	MOUNT_CCACHE_DIR=(-v "${BUILDER_CCACHE_DIR}:${USER_PATH}/.ccache")
+LOCAL_CCACHE_DIR=$(ccache -k cache_dir 2> /dev/null || :)
+if [ -n "${BUILDER_CCACHE_DIR:-}" ]; then
+	MOUNT_CCACHE_DIR=(-v "$BUILDER_CCACHE_DIR:$USER_PATH/.cache/ccache")
+elif [ -d "$LOCAL_CCACHE_DIR" ]; then
+	MOUNT_CCACHE_DIR=(-v "$LOCAL_CCACHE_DIR:$USER_PATH/.cache/ccache")
 fi
 
 docker run --rm \