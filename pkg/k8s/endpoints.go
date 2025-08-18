cilium/cilium

commit 6f41c986bbfceae31fde143b0e029970d343b83c

loadbalancer: Keep non-serving terminating backends
A terminating pod that no longer signals readiness will be seen as an
endpoint with {ready: false, serving: false, terminating: true}. These
were skipped leading to the backend being removed from the backends map
which causes connection disruptions.

Fix this by keeping the non-serving terminating backends in the backends BPF
map, but not using it as a fallback backend if only terminating backends exists.

This is implemented by:
- Including all conditions in [k8s.Backend], not just Terminating.
- Adding a new backend state (TerminatingNotServing) used with Terminating&!Serving conditions.
- Not considering TerminatingNotServing backends when selecting the backends.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/k8s/endpoints.go
@@ -97,7 +97,58 @@ func (in *Endpoints) DeepCopy() *Endpoints {
 	return out
 }
 
-// Backend contains all ports, terminating state, and the node name of a given backend
+// BackendCondition is a flag mirroring the endpoint Conditions.
+type BackendCondition uint8
+
+const (
+	// BackendConditionReady indicates that this endpoint is prepared to receive traffic,
+	// according to whatever system is managing the endpoint.
+	// More info: vendor/k8s.io/api/discovery/v1/types.go
+	// See also https://github.com/kubernetes/kubernetes/issues/108523
+	BackendConditionReady = 1 << iota
+
+	// BackendConditionServing indicates that this endpoint can serve new connections.
+	// It is meaningful to Cilium when the backend is terminating. If this condition is false
+	// then a terminating backend will not be used for new connections even as fallback
+	// when no active backends exist.
+	// More info: vendor/k8s.io/api/discovery/v1/types.go
+	// See also https://github.com/kubernetes/kubernetes/issues/108523 and
+	// https://github.com/kubernetes/kubernetes/blob/790393ae92e97262827d4f1fba24e8ae65bbada0/pkg/proxy/topology.go#L76
+	BackendConditionServing
+
+	// Terminating indicates that the endpoint is getting terminated.
+	// It will not be used for new conditions unless 1) no active backends exist
+	// and 2) this backend is serving.
+	//
+	// If [publishNotReadyAddresses] is set on a service then a backend may be
+	// both terminating and ready in which case the terminating state is ignored.
+	//
+	// More info: vendor/k8s.io/api/discovery/v1/types.go
+	// See also https://github.com/kubernetes/kubernetes/issues/108523
+	BackendConditionTerminating
+)
+
+var backendConditions = [...]string{
+	BackendConditionReady:       "ready",
+	BackendConditionServing:     "serving",
+	BackendConditionTerminating: "terminating",
+}
+
+func (bc BackendCondition) String() string {
+	var flags []string
+	for mask, str := range backendConditions {
+		if str != "" && bc&BackendCondition(mask) != 0 {
+			flags = append(flags, str)
+		}
+	}
+	return strings.Join(flags, "+")
+}
+
+func (bc BackendCondition) IsReady() bool       { return bc&BackendConditionReady != 0 }
+func (bc BackendCondition) IsServing() bool     { return bc&BackendConditionServing != 0 }
+func (bc BackendCondition) IsTerminating() bool { return bc&BackendConditionTerminating != 0 }
+
+// Backend contains all ports, conditions, and the node name of a given backend
 //
 // +k8s:deepcopy-gen=true
 // +deepequal-gen=false
@@ -105,7 +156,7 @@ type Backend struct {
 	Ports         map[loadbalancer.L4Addr][]string
 	NodeName      string
 	Hostname      string
-	Terminating   bool
+	Conditions    BackendCondition
 	HintsForZones []string
 	Preferred     bool
 	Zone          string
@@ -115,7 +166,7 @@ func (b *Backend) DeepEqual(other *Backend) bool {
 	return maps.EqualFunc(b.Ports, other.Ports, slices.Equal) &&
 		b.NodeName == other.NodeName &&
 		b.Hostname == other.Hostname &&
-		b.Terminating == other.Terminating &&
+		b.Conditions == other.Conditions &&
 		slices.Equal(b.HintsForZones, other.HintsForZones) &&
 		b.Preferred == other.Preferred &&
 		b.Zone == other.Zone
@@ -191,6 +242,19 @@ func ParseEndpointSliceID(es endpointSlice) EndpointSliceID {
 
 const logfieldTerminating = "terminating"
 
+func ParseEndpointConditionsV1(conditions slim_discovery_v1.EndpointConditions) (bc BackendCondition) {
+	if conditions.Ready == nil || *conditions.Ready {
+		bc |= BackendConditionReady
+	}
+	if conditions.Serving == nil || conditions.Serving != nil && *conditions.Serving {
+		bc |= BackendConditionServing
+	}
+	if conditions.Terminating != nil && *conditions.Terminating {
+		bc |= BackendConditionTerminating
+	}
+	return
+}
+
 // ParseEndpointSliceV1 parses a Kubernetes EndpointSlice resource.
 // It reads ready and terminating state of endpoints in the EndpointSlice to
 // return an EndpointSlice ID and a filtered list of Endpoints for service load-balancing.
@@ -224,41 +288,10 @@ func ParseEndpointSliceV1(logger *slog.Logger, ep *slim_discovery_v1.EndpointSli
 	}
 
 	for i, sub := range ep.Endpoints {
-		// ready indicates that this endpoint is prepared to receive traffic,
-		// according to whatever system is managing the endpoint. A nil value
-		// indicates an unknown state. In most cases consumers should interpret this
-		// unknown state as ready.
-		// More info: vendor/k8s.io/api/discovery/v1/types.go
-		isReady := sub.Conditions.Ready == nil || *sub.Conditions.Ready
-		// serving is identical to ready except that it is set regardless of the
-		// terminating state of endpoints. This condition should be set to true for
-		// a ready endpoint that is terminating. If nil, consumers should defer to
-		// the ready condition.
-		// More info: vendor/k8s.io/api/discovery/v1/types.go
-		isServing := (sub.Conditions.Serving == nil && isReady) || (sub.Conditions.Serving != nil && *sub.Conditions.Serving)
-		// Terminating indicates that the endpoint is getting terminated. A
-		// nil values indicates an unknown state. Ready is never true when
-		// an endpoint is terminating. Propagate the terminating endpoint
-		// state so that we can gracefully remove those endpoints.
-		// More info: vendor/k8s.io/api/discovery/v1/types.go
-		isTerminating := sub.Conditions.Terminating != nil && *sub.Conditions.Terminating
-
-		// if is not Ready allow endpoints that are Serving and Terminating
-		if !isReady {
-			// filter not Serving endpoints since those can not receive traffic
-			if !isServing {
-				logger.Debug(
-					"discarding Endpoint on EndpointSlice: not Serving",
-					logfields.Name, ep.Name,
-				)
-				continue
-			}
-		}
-
 		// Construct the backend configuration shared by all the addresses in this slice.
 		backend := &Backend{
-			Ports:       ports,
-			Terminating: !isReady && isServing && isTerminating,
+			Conditions: ParseEndpointConditionsV1(sub.Conditions),
+			Ports:      ports,
 		}
 
 		if sub.NodeName != nil {
@@ -301,7 +334,7 @@ func ParseEndpointSliceV1(logger *slog.Logger, ep *slim_discovery_v1.EndpointSli
 			endpoints.Backends[addrCluster] = backend
 		}
 
-		if backend.Terminating {
+		if backend.Conditions.IsTerminating() {
 			metrics.TerminatingEndpointsEvents.Inc()
 		}
 
@@ -310,7 +343,7 @@ func ParseEndpointSliceV1(logger *slog.Logger, ep *slim_discovery_v1.EndpointSli
 			logfields.Name, ep.Name,
 			logfields.Addresses, sub.Addresses,
 			logfields.Backend, backend,
-			logfieldTerminating, backend.Terminating,
+			logfieldTerminating, backend.Conditions.IsTerminating(),
 		)
 	}
 

pkg/loadbalancer/tests/testdata/graceful-termination.txtar
@@ -8,10 +8,12 @@ hive start
 
 # Start with two backends that are both active.
 cp endpointslice.yaml.tmpl endpointslice.yaml
-replace '$TERM1' 'false' endpointslice.yaml
 replace '$READY1' 'true' endpointslice.yaml
-replace '$TERM2' 'false' endpointslice.yaml
+replace '$SERVING1' 'true' endpointslice.yaml
+replace '$TERM1' 'false' endpointslice.yaml
 replace '$READY2' 'true' endpointslice.yaml
+replace '$SERVING2' 'true' endpointslice.yaml
+replace '$TERM2' 'false' endpointslice.yaml
 
 k8s/add service.yaml endpointslice.yaml
 db/cmp services services.table
@@ -25,10 +27,12 @@ lb/maps-dump lbmaps.actual
 # Set the first backend as terminating. The second backend
 # will now serve the traffic.
 cp endpointslice.yaml.tmpl endpointslice.yaml
-replace '$TERM1' 'true' endpointslice.yaml
 replace '$READY1' 'false' endpointslice.yaml
-replace '$TERM2' 'false' endpointslice.yaml
+replace '$SERVING1' 'true' endpointslice.yaml
+replace '$TERM1' 'true' endpointslice.yaml
 replace '$READY2' 'true' endpointslice.yaml
+replace '$SERVING2' 'true' endpointslice.yaml
+replace '$TERM2' 'false' endpointslice.yaml
 k8s/update endpointslice.yaml
 db/cmp backends backends-terminating1.table
 
@@ -40,10 +44,12 @@ lb/maps-dump lbmaps.actual
 # are now active backends the terminating backends are used
 # instead.
 cp endpointslice.yaml.tmpl endpointslice.yaml
-replace '$TERM1' 'true' endpointslice.yaml
 replace '$READY1' 'false' endpointslice.yaml
-replace '$TERM2' 'true' endpointslice.yaml
+replace '$TERM1' 'true' endpointslice.yaml
+replace '$SERVING1' 'true' endpointslice.yaml
 replace '$READY2' 'false' endpointslice.yaml
+replace '$TERM2' 'true' endpointslice.yaml
+replace '$SERVING2' 'true' endpointslice.yaml
 k8s/update endpointslice.yaml
 db/cmp frontends frontends-terminating2.table
 db/cmp backends backends-terminating2.table
@@ -51,6 +57,43 @@ db/cmp backends backends-terminating2.table
 # Check BPF maps
 lb/maps-dump lbmaps.actual
 * cmp lbmaps-terminating2.expected lbmaps.actual
+
+# Set the second backend as terminating AND not serving.
+# This now won't be used as a fallback.
+cp endpointslice.yaml.tmpl endpointslice.yaml
+replace '$READY1' 'false' endpointslice.yaml
+replace '$SERVING1' 'true' endpointslice.yaml
+replace '$TERM1' 'true' endpointslice.yaml
+replace '$READY2' 'false' endpointslice.yaml
+replace '$SERVING2' 'false' endpointslice.yaml
+replace '$TERM2' 'true' endpointslice.yaml
+k8s/update endpointslice.yaml
+db/cmp frontends frontends-terminating3.table
+db/cmp backends backends-terminating3.table
+
+# Check BPF maps
+lb/maps-dump lbmaps.actual
+* cmp lbmaps-terminating3.expected lbmaps.actual
+
+# Set the first backend as not serving and second as ready+terminating .
+# First backend will be quarantined. Existing connections to
+# it won't be affected as it's still in the backends BPF map.
+# The second backend will be just considered ready and its terminating
+# condition is ignored ("ready" overrides everything).
+cp endpointslice.yaml.tmpl endpointslice.yaml
+replace '$READY1' 'false' endpointslice.yaml
+replace '$SERVING1' 'false' endpointslice.yaml
+replace '$TERM1' 'false' endpointslice.yaml
+replace '$READY2' 'true' endpointslice.yaml
+replace '$SERVING2' 'false' endpointslice.yaml
+replace '$TERM2' 'true' endpointslice.yaml
+k8s/update endpointslice.yaml
+db/cmp frontends frontends-terminating4.table
+db/cmp backends backends-terminating4.table
+
+# Check BPF maps
+lb/maps-dump lbmaps.actual
+* cmp lbmaps-terminating4.expected lbmaps.actual
 
 # Cleanup
 k8s/delete service.yaml endpointslice.yaml
@@ -73,6 +116,14 @@ Address                 Type        ServiceName              Status  Backends
 Address                 Type        ServiceName              Status  Backends
 10.96.116.33:8081/TCP   ClusterIP   test/graceful-term-svc   Done    10.244.0.112:8081/TCP, 10.244.0.113:8081/TCP
 
+-- frontends-terminating3.table --
+Address                 Type        ServiceName              Status  Backends
+10.96.116.33:8081/TCP   ClusterIP   test/graceful-term-svc   Done    10.244.0.112:8081/TCP, 10.244.0.113:8081/TCP
+
+-- frontends-terminating4.table --
+Address                 Type        ServiceName              Status  Backends
+10.96.116.33:8081/TCP   ClusterIP   test/graceful-term-svc   Done    10.244.0.112:8081/TCP, 10.244.0.113:8081/TCP
+
 -- backends.table --
 Address                 Instances                NodeName
 10.244.0.112:8081/TCP   test/graceful-term-svc   graceful-term-control-plane
@@ -87,6 +138,16 @@ Address                 Instances                              NodeName
 Address                 Instances                             NodeName
 10.244.0.112:8081/TCP   test/graceful-term-svc [terminating]  graceful-term-control-plane
 10.244.0.113:8081/TCP   test/graceful-term-svc [terminating]  graceful-term-control-plane
+
+-- backends-terminating3.table --
+Address                 Instances                                         NodeName
+10.244.0.112:8081/TCP   test/graceful-term-svc [terminating]              graceful-term-control-plane
+10.244.0.113:8081/TCP   test/graceful-term-svc [terminating-not-serving]  graceful-term-control-plane
+
+-- backends-terminating4.table --
+Address                 Instances                                         NodeName
+10.244.0.112:8081/TCP   test/graceful-term-svc [quarantined]              graceful-term-control-plane
+10.244.0.113:8081/TCP   test/graceful-term-svc                            graceful-term-control-plane
 
 -- service.yaml --
 apiVersion: v1
@@ -146,7 +207,7 @@ endpoints:
   - 10.244.0.112
   conditions:
     ready: $READY1
-    serving: true
+    serving: $SERVING1
     terminating: $TERM1
   nodeName: graceful-term-control-plane
   targetRef:
@@ -159,7 +220,7 @@ endpoints:
   - 10.244.0.113
   conditions:
     ready: $READY2
-    serving: true
+    serving: $SERVING2
     terminating: $TERM2
   nodeName: graceful-term-control-plane
   targetRef:
@@ -197,3 +258,19 @@ REV: ID=1 ADDR=10.96.116.33:8081
 SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=0 LBALG=undef AFFTimeout=0 COUNT=2 QCOUNT=0 FLAGS=ClusterIP
 SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=1 BEID=1 COUNT=0 QCOUNT=0 FLAGS=ClusterIP
 SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=2 BEID=2 COUNT=0 QCOUNT=0 FLAGS=ClusterIP
+-- lbmaps-terminating3.expected --
+BE: ID=1 ADDR=10.244.0.112:8081/TCP STATE=terminating
+BE: ID=2 ADDR=10.244.0.113:8081/TCP STATE=terminating
+MAGLEV: ID=1 INNER=[1(1021)]
+REV: ID=1 ADDR=10.96.116.33:8081
+SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=0 LBALG=undef AFFTimeout=0 COUNT=1 QCOUNT=1 FLAGS=ClusterIP
+SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=1 BEID=1 COUNT=0 QCOUNT=0 FLAGS=ClusterIP
+SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=2 BEID=2 COUNT=0 QCOUNT=0 FLAGS=ClusterIP
+-- lbmaps-terminating4.expected --
+BE: ID=1 ADDR=10.244.0.112:8081/TCP STATE=quarantined
+BE: ID=2 ADDR=10.244.0.113:8081/TCP STATE=active
+MAGLEV: ID=1 INNER=[2(1021)]
+REV: ID=1 ADDR=10.96.116.33:8081
+SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=0 LBALG=undef AFFTimeout=0 COUNT=1 QCOUNT=1 FLAGS=ClusterIP
+SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=1 BEID=2 COUNT=0 QCOUNT=0 FLAGS=ClusterIP
+SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=2 BEID=1 COUNT=0 QCOUNT=0 FLAGS=ClusterIP

api/v1/models/backend_address.go
@@ -41,7 +41,7 @@ type BackendAddress struct {
 	Protocol string `json:"protocol,omitempty"`
 
 	// State of the backend for load-balancing service traffic
-	// Enum: ["active","terminating","quarantined","maintenance"]
+	// Enum: ["active","terminating","terminating-not-serving","quarantined","maintenance"]
 	State string `json:"state,omitempty"`
 
 	// Backend weight
@@ -82,7 +82,7 @@ var backendAddressTypeStatePropEnum []interface{}
 
 func init() {
 	var res []string
-	if err := json.Unmarshal([]byte(`["active","terminating","quarantined","maintenance"]`), &res); err != nil {
+	if err := json.Unmarshal([]byte(`["active","terminating","terminating-not-serving","quarantined","maintenance"]`), &res); err != nil {
 		panic(err)
 	}
 	for _, v := range res {
@@ -97,6 +97,9 @@ const (
 
 	// BackendAddressStateTerminating captures enum value "terminating"
 	BackendAddressStateTerminating string = "terminating"
+
+	// BackendAddressStateTerminatingDashNotDashServing captures enum value "terminating-not-serving"
+	BackendAddressStateTerminatingDashNotDashServing string = "terminating-not-serving"
 
 	// BackendAddressStateQuarantined captures enum value "quarantined"
 	BackendAddressStateQuarantined string = "quarantined"

api/v1/openapi.yaml
@@ -2924,6 +2924,7 @@ definitions:
         enum:
           - active
           - terminating
+          - terminating-not-serving
           - quarantined
           - maintenance
       preferred:

api/v1/server/embedded_spec.go
@@ -1824,6 +1824,7 @@ func init() {
           "enum": [
             "active",
             "terminating",
+            "terminating-not-serving",
             "quarantined",
             "maintenance"
           ]
@@ -7456,6 +7457,7 @@ func init() {
           "enum": [
             "active",
             "terminating",
+            "terminating-not-serving",
             "quarantined",
             "maintenance"
           ]

pkg/bgpv1/manager/reconciler/service.go
@@ -158,7 +158,7 @@ endpointsLoop:
 		svcID := eps.ServiceName
 
 		for _, be := range eps.Backends {
-			if !be.Terminating && be.NodeName == localNodeName {
+			if !be.Conditions.IsTerminating() && be.NodeName == localNodeName {
 				// At least one endpoint is available on this node. We
 				// can make unavailable to available.
 				if _, found := ls[svcID]; !found {

pkg/bgpv1/manager/reconciler/service_test.go
@@ -122,7 +122,8 @@ func TestServiceReconcilerWithLoadBalancer(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -138,8 +139,8 @@ func TestServiceReconcilerWithLoadBalancer(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.1"): {
-				NodeName:    "node1",
-				Terminating: true,
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionTerminating,
 			},
 		},
 	}
@@ -155,7 +156,8 @@ func TestServiceReconcilerWithLoadBalancer(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -171,10 +173,12 @@ func TestServiceReconcilerWithLoadBalancer(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 			cmtypes.MustParseAddrCluster("10.0.0.2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -190,7 +194,8 @@ func TestServiceReconcilerWithLoadBalancer(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("fd00:10::1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -206,7 +211,8 @@ func TestServiceReconcilerWithLoadBalancer(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("fd00:10::2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -222,10 +228,12 @@ func TestServiceReconcilerWithLoadBalancer(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("fd00:10::1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 			cmtypes.MustParseAddrCluster("fd00:10::2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -825,7 +833,8 @@ func TestServiceReconcilerWithClusterIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -841,7 +850,8 @@ func TestServiceReconcilerWithClusterIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -857,10 +867,12 @@ func TestServiceReconcilerWithClusterIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 			cmtypes.MustParseAddrCluster("10.0.0.2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -876,7 +888,8 @@ func TestServiceReconcilerWithClusterIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("fd00:10::1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -892,7 +905,8 @@ func TestServiceReconcilerWithClusterIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("fd00:10::2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -908,7 +922,8 @@ func TestServiceReconcilerWithClusterIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("fd00:10::1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 			cmtypes.MustParseAddrCluster("fd00:10::2"): {
 				NodeName: "node2",
@@ -1468,7 +1483,8 @@ func TestServiceReconcilerWithExternalIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -1484,7 +1500,8 @@ func TestServiceReconcilerWithExternalIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -1500,10 +1517,12 @@ func TestServiceReconcilerWithExternalIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 			cmtypes.MustParseAddrCluster("10.0.0.2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -1519,7 +1538,8 @@ func TestServiceReconcilerWithExternalIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("fd00:10::1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -1535,7 +1555,8 @@ func TestServiceReconcilerWithExternalIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("fd00:10::2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -1551,10 +1572,12 @@ func TestServiceReconcilerWithExternalIP(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("fd00:10::1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 			cmtypes.MustParseAddrCluster("fd00:10::2"): {
-				NodeName: "node2",
+				NodeName:   "node2",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
@@ -2063,14 +2086,16 @@ func TestEPUpdateOnly(t *testing.T) {
 		},
 		Backends: map[cmtypes.AddrCluster]*k8s.Backend{
 			cmtypes.MustParseAddrCluster("10.0.0.1"): {
-				NodeName: "node1",
+				NodeName:   "node1",
+				Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 			},
 		},
 	}
 	eps1IPv4LocalUpdated := eps1IPv4Local.DeepCopy()
 	eps1IPv4LocalUpdated.Backends = map[cmtypes.AddrCluster]*k8s.Backend{
 		cmtypes.MustParseAddrCluster("10.0.0.1"): {
-			NodeName: "node2",
+			NodeName:   "node2",
+			Conditions: k8s.BackendConditionReady | k8s.BackendConditionServing,
 		},
 	}
 

pkg/bgpv1/manager/reconcilerv2/service.go
@@ -361,7 +361,7 @@ endpointsLoop:
 		}
 
 		for _, be := range eps.Backends {
-			if !be.Terminating && be.NodeName == localNodeName {
+			if !be.Conditions.IsTerminating() && be.NodeName == localNodeName {
 				// At least one endpoint is available on this node. We
 				// can add service to the local services set.
 				ls.Insert(svcKey)

commit ddfc4990ba2128efe085be3d87f72a89245bc9a7

k8s: Skip endpoints without conditions
Skip endpoints in endpoint slices that have no conditions set as those
are not yet ready to serve traffic. Before this they were marked quarantined
which is unnecessary and it caused issues as the quarantined state was restored
and not cleared until health checked (and we usually don't have a health checker).

Fixes: #41194
Fixes: 6f41c986bbfce ("loadbalancer: Keep non-serving terminating backends")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/k8s/endpoints.go
@@ -288,9 +288,16 @@ func ParseEndpointSliceV1(logger *slog.Logger, ep *slim_discovery_v1.EndpointSli
 	}
 
 	for i, sub := range ep.Endpoints {
+		conditions := ParseEndpointConditionsV1(sub.Conditions)
+		if conditions == 0 {
+			// Skip backends that have no conditions set as these are not ready to
+			// serve traffic.
+			continue
+		}
+
 		// Construct the backend configuration shared by all the addresses in this slice.
 		backend := &Backend{
-			Conditions: ParseEndpointConditionsV1(sub.Conditions),
+			Conditions: conditions,
 			Ports:      ports,
 		}
 

pkg/loadbalancer/tests/testdata/graceful-termination.txtar
@@ -76,8 +76,7 @@ lb/maps-dump lbmaps.actual
 * cmp lbmaps-terminating3.expected lbmaps.actual
 
 # Set the first backend as not serving and second as ready+terminating .
-# First backend will be quarantined. Existing connections to
-# it won't be affected as it's still in the backends BPF map.
+# First backend will be removed.
 # The second backend will be just considered ready and its terminating
 # condition is ignored ("ready" overrides everything).
 cp endpointslice.yaml.tmpl endpointslice.yaml
@@ -146,7 +145,6 @@ Address                 Instances                                         NodeNa
 
 -- backends-terminating4.table --
 Address                 Instances                                         NodeName
-10.244.0.112:8081/TCP   test/graceful-term-svc [quarantined]              graceful-term-control-plane
 10.244.0.113:8081/TCP   test/graceful-term-svc                            graceful-term-control-plane
 
 -- service.yaml --
@@ -267,10 +265,8 @@ SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=0 LBALG=undef AFFTimeout=0 COUNT=1 QCO
 SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=1 BEID=1 COUNT=0 QCOUNT=0 FLAGS=ClusterIP
 SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=2 BEID=2 COUNT=0 QCOUNT=0 FLAGS=ClusterIP
 -- lbmaps-terminating4.expected --
-BE: ID=1 ADDR=10.244.0.112:8081/TCP STATE=quarantined
 BE: ID=2 ADDR=10.244.0.113:8081/TCP STATE=active
 MAGLEV: ID=1 INNER=[2(1021)]
 REV: ID=1 ADDR=10.96.116.33:8081
-SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=0 LBALG=undef AFFTimeout=0 COUNT=1 QCOUNT=1 FLAGS=ClusterIP
+SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=0 LBALG=undef AFFTimeout=0 COUNT=1 QCOUNT=0 FLAGS=ClusterIP
 SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=1 BEID=2 COUNT=0 QCOUNT=0 FLAGS=ClusterIP
-SVC: ID=1 ADDR=10.96.116.33:8081/TCP SLOT=2 BEID=1 COUNT=0 QCOUNT=0 FLAGS=ClusterIP