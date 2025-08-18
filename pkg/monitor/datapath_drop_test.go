043a8a578c3e7681837bd67e7612c73981440d53
Ben Bigdelle <bigdelle@google.com>
2025-08-18 19:06:02 +0000

    feat: expose IP trace in drop notify struct
    This change extends the `DropNotify` struct to include the IP trace 
    ID.
    
    The following changes are included:
    - The `DropNotify` struct in the control plane is updated to include
    the `IPTraceID` field.
    - The BPF code is updated to check to see if there is a stored trace ID
    in the BPF map and, if so, populating it.
    - The `cilium-monitor` output is updated to display the IP trace ID when
    present in a drop notify message.
    
    Signed-off-by: Ben Bigdelle <bigdelle@google.com>

--- pkg/monitor/datapath_drop_test.go
+++ pkg/monitor/datapath_drop_test.go
@@ -8,76 +8,205 @@ import (
 	"encoding/binary"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 
 	"github.com/cilium/cilium/pkg/byteorder"
 )
 
-func TestDecodeDropNotify(t *testing.T) {
+func TestDropNotifyV1_Decode(t *testing.T) {
+	// This check on the struct length constant is there to ensure that this
+	// test is updated when the struct changes.
+	require.Equal(t, 36, dropNotifyV1Len)
+
+	testCases := []struct {
+		name  string
+		input DropNotify
+	}{
+		{
+			name: "empty",
+		},
+		{
+			name: "arbitrary",
+			input: DropNotify{
+				Type:     0x00,
+				SubType:  0x01,
+				Source:   0x02_03,
+				Hash:     0x04_05_06_07,
+				OrigLen:  0x08_09_0a_0b,
+				CapLen:   0x0e_10,
+				Version:  0x00_01,
+				SrcLabel: 0x11_12_13_14,
+				DstLabel: 0x15_16_17_18,
+				DstID:    0x19_1a_1b_1c,
+				Line:     0x1d_1e,
+				File:     0x20,
+				ExtError: 0x21,
+				Ifindex:  0x22_23_24_25,
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			if err := binary.Write(buf, byteorder.Native, tc.input); err != nil {
+				t.Fatalf("Unexpected error from Write(...); got: %v", err)
+			}
+
+			output := DropNotify{}
+			if err := output.Decode(buf.Bytes()); err != nil {
+				t.Fatalf("Unexpected error from Decode(<bytes>); got: %v", err)
+			}
+
+			if diff := cmp.Diff(tc.input, output); diff != "" {
+				t.Errorf("Unexpected diff (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestDropNotify_Decode(t *testing.T) {
 	// This check on the struct length constant is there to ensure that this
 	// test is updated when the struct changes.
 	require.Equal(t, 40, dropNotifyV2Len)
+	require.Equal(t, 48, dropNotifyV3Len)
 
-	input := DropNotify{
-		Type:     0x00,
-		SubType:  0x01,
-		Source:   0x02_03,
-		Hash:     0x04_05_06_07,
-		OrigLen:  0x08_09_0a_0b,
-		CapLen:   0x0c_0d,
-		Version:  0x02,
-		SrcLabel: 0x11_12_13_14,
-		DstLabel: 0x15_16_17_18,
-		DstID:    0x19_1a_1b_1c,
-		Line:     0x1d_1e,
-		File:     0x20,
-		ExtError: 0x21,
-		Ifindex:  0x22_23_24_25,
-		Flags:    0x0f,
+	testCases := []struct {
+		name  string
+		input DropNotify
+	}{
+		{
+			name: "empty",
+		},
+		{
+			name: "arbitrary",
+			input: DropNotify{
+				Type:      0x00,
+				SubType:   0x01,
+				Source:    0x02_03,
+				Hash:      0x04_05_06_07,
+				OrigLen:   0x08_09_0a_0b,
+				CapLen:    0x0e_10,
+				Version:   0x00_03,
+				SrcLabel:  0x11_12_13_14,
+				DstLabel:  0x15_16_17_18,
+				DstID:     0x19_1a_1b_1c,
+				Line:      0x1d_1e,
+				File:      0x20,
+				ExtError:  0x21,
+				Ifindex:   0x22_23_24_25,
+				Flags:     0x0f,
+				IPTraceID: 0x99,
+			},
+		},
 	}
-	buf := bytes.NewBuffer(nil)
-	err := binary.Write(buf, byteorder.Native, input)
-	require.NoError(t, err)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			if err := binary.Write(buf, byteorder.Native, tc.input); err != nil {
+				t.Fatalf("Unexpected error from Write(...); got: %v", err)
+			}
 
-	output := &DropNotify{}
-	err = output.Decode(buf.Bytes())
-	require.NoError(t, err)
+			output := DropNotify{}
+			if err := output.Decode(buf.Bytes()); err != nil {
+				t.Fatalf("Unexpected error from Decode(<bytes>); got: %v", err)
+			}
 
-	require.Equal(t, input.Type, output.Type)
-	require.Equal(t, input.SubType, output.SubType)
-	require.Equal(t, input.Source, output.Source)
-	require.Equal(t, input.Hash, output.Hash)
-	require.Equal(t, input.OrigLen, output.OrigLen)
-	require.Equal(t, input.CapLen, output.CapLen)
-	require.Equal(t, input.SrcLabel, output.SrcLabel)
-	require.Equal(t, input.DstLabel, output.DstLabel)
-	require.Equal(t, input.DstID, output.DstID)
-	require.Equal(t, input.Line, output.Line)
-	require.Equal(t, input.File, output.File)
-	require.Equal(t, input.ExtError, output.ExtError)
-	require.Equal(t, input.Ifindex, output.Ifindex)
-	require.Equal(t, input.Flags, output.Flags)
-	require.True(t, output.IsL3Device())
-	require.True(t, output.IsIPv6())
-	require.True(t, output.IsVXLAN())
-	require.True(t, output.IsGeneve())
+			if diff := cmp.Diff(tc.input, output); diff != "" {
+				t.Errorf("Unexpected diff (-want +got):\n%s", diff)
+			}
+		})
+	}
 }
 
-func TestDecodeDropNotifyErrors(t *testing.T) {
-	n := DropNotify{}
-	err := n.Decode([]byte{})
-	require.Error(t, err)
-	require.Equal(t, "unexpected DropNotify data length, expected at least 36 but got 0", err.Error())
+func TestDecodeDropNotify(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input any
+		want  uint
+	}{
+		{
+			name: "v1",
+			input: DropNotify{
+				Type:     0x00,
+				SubType:  0x01,
+				Source:   0x02_03,
+				Hash:     0x04_05_06_07,
+				OrigLen:  0x08_09_0a_0b,
+				CapLen:   0x0e_10,
+				Version:  0x00_01,
+				SrcLabel: 0x11_12_13_14,
+				DstLabel: 0x15_16_17_18,
+				DstID:    0x19_1a_1b_1c,
+				Line:     0x1d_1e,
+				File:     0x20,
+				ExtError: 0x21,
+				Ifindex:  0x22_23_24_25,
+			},
+			want: dropNotifyV1Len,
+		},
+		{
+			name: "v2",
+			input: DropNotify{
+				Type:     0x00,
+				SubType:  0x01,
+				Source:   0x02_03,
+				Hash:     0x04_05_06_07,
+				OrigLen:  0x08_09_0a_0b,
+				CapLen:   0x0e_10,
+				Version:  0x00_02,
+				SrcLabel: 0x11_12_13_14,
+				DstLabel: 0x15_16_17_18,
+				DstID:    0x19_1a_1b_1c,
+				Line:     0x1d_1e,
+				File:     0x20,
+				ExtError: 0x21,
+				Ifindex:  0x22_23_24_25,
+			},
+			want: dropNotifyV2Len,
+		},
+		{
+			name: "with_iptrace",
+			input: DropNotify{
+				Type:      0x00,
+				SubType:   0x01,
+				Source:    0x02_03,
+				Hash:      0x04_05_06_07,
+				OrigLen:   0x08_09_0a_0b,
+				CapLen:    0x0e_10,
+				Version:   0x00_03,
+				SrcLabel:  0x11_12_13_14,
+				DstLabel:  0x15_16_17_18,
+				DstID:     0x19_1a_1b_1c,
+				Line:      0x1d_1e,
+				File:      0x20,
+				ExtError:  0x21,
+				Ifindex:   0x22_23_24_25,
+				IPTraceID: 0x999,
+			},
+			want: dropNotifyV3Len,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			if err := binary.Write(buf, byteorder.Native, tc.input); err != nil {
+				t.Fatalf("Unexpected error from Write(...); got: %v", err)
+			}
 
-	// invalid version
-	ev := make([]byte, dropNotifyV2Len)
-	ev[14] = 0xff
-	err = n.Decode(ev)
-	require.Error(t, err)
-	require.Equal(t, "Unrecognized drop event (version 255)", err.Error())
+			output := DropNotify{}
+			if err := output.Decode(buf.Bytes()); err != nil {
+				t.Fatalf("Unexpected error from Decode(<bytes>); got: %v", err)
+			}
+
+			if got := output.DataOffset(); got != tc.want {
+				t.Fatalf("Unexpected DataOffset(); want %d, got %d", tc.want, got)
+			}
+		})
+	}
 }
 
-func BenchmarkDecodeDropNotifyV1(b *testing.B) {
+func BenchmarkNewDropNotifyV1_Decode(b *testing.B) {
 	input := DropNotify{}
 	buf := bytes.NewBuffer(nil)
 
@@ -95,8 +224,8 @@ func BenchmarkDecodeDropNotifyV1(b *testing.B) {
 	}
 }
 
-func BenchmarkDecodeDropNotifyV2(b *testing.B) {
-	input := DropNotify{Version: DropNotifyVersion2}
+func BenchmarkOldDropNotifyV1_Decode(b *testing.B) {
+	input := DropNotify{}
 	buf := bytes.NewBuffer(nil)
 
 	if err := binary.Write(buf, byteorder.Native, input); err != nil {
@@ -107,7 +236,7 @@ func BenchmarkDecodeDropNotifyV2(b *testing.B) {
 
 	for b.Loop() {
 		dn := &DropNotify{}
-		if err := dn.Decode(buf.Bytes()); err != nil {
+		if err := binary.Read(bytes.NewReader(buf.Bytes()), byteorder.Native, dn); err != nil {
 			b.Fatal(err)
 		}
 	}
