0022941864ea45cb5338a6f6d3569beae6ed6ca3
Tom Hadlaw <tom.hadlaw@isovalent.com>
2025-08-27 00:40:54 +0000

    ctmap/gc: continue interval time on partial GC pass.
    [ upstream commit 529cdc9a96c7f0dbe3b4a54c3ea12d319d46da33 ]
    
    When {ipv4,ipv6} specific signals are sent from the datapath
    to indicate conditions such as high ctmap or snat pressure
    we may invoke a ipv4 or ipv6 *only* gc pass.
    Currently this will also reset the gc interval by recomputing
    the next interval.
    
    Under a situation where a lot of ipv4/ipv6 specific signals
    are being sent this may a starvation condition such that the
    other family never has a chance to be run since before the
    interval time can finish the loop is woken up by another signal.
    
    To improve this, if the GC was a "partial" pass (i.e. not including
    all *enabled* ip families), then we will continue waiting from where
    the previous timer left off.  Following a full GC pass we will just
    recompute the interval as normal (doesn't matter if the full pass
    was caused by the timer or a signal).
    
    Fixes: #40820
    
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- pkg/maps/ctmap/gc/gc.go
+++ pkg/maps/ctmap/gc/gc.go
@@ -110,6 +110,14 @@ func New(params parameters) *GC {
 	return gc
 }
 
+// A full GC pass is when we perform GC on any and all
+// enabled IP families.
+// Partial passes can happen as a result of datapath
+// signals on particular families (i.e. ct or nat).
+func (gc *GC) isFullGC(ipv4, ipv6 bool) bool {
+	return ipv4 == gc.ipv4 && ipv6 == gc.ipv6
+}
+
 // Enable enables the connection tracking garbage collection.
 func (gc *GC) Enable() {
 	gc.enable(gc.runGC, true)
@@ -129,6 +137,7 @@ func (gc *GC) enable(
 		ipv6 := gc.ipv6
 		triggeredBySignal := false
 		var gcPrev time.Time
+		var forceFullGCTTL time.Time
 		var cachedGCInterval time.Duration
 		for {
 			var (
@@ -189,14 +198,33 @@ func (gc *GC) enable(
 				maxDeleteRatio, success = runGC(ipv4, ipv6, triggeredBySignal, gcFilter)
 			}
 
-			// Mark the CT GC as over in each EP DNSZombies instance, if we did a *full* GC run
 			interval := ctmap.GetInterval(gc.logger, gcInterval, cachedGCInterval, maxDeleteRatio)
-			if success && ipv4 == gc.ipv4 && ipv6 == gc.ipv6 {
+			if success && gc.isFullGC(ipv4, ipv6) {
+				// Mark the CT GC as over in each EP DNSZombies instance, if we did a *full* GC run
+				nextGCTime := time.Now().Add(interval)
 				for _, e := range eps {
-					e.MarkCTGCTime(gcStart, time.Now().Add(interval))
+					e.MarkCTGCTime(gcStart, nextGCTime)
+				}
+
+				forceFullGCTTL = time.Now().Add(interval)
+				// full pass so we reset our cached GC interval.
+				cachedGCInterval = interval
+			} else if !initialScan {
+				// If we did not succeed, or it wasn't a full pass then we take the
+				// minimum of the new interval and any remaining time on the last interval
+				// clock - effectively running out the clock of the previous interval.
+				// This is because in a partial GC pass one of the IP families has not been
+				// tended to, so to avoid potentially starving GC on one of the IP families
+				// we do this to ensure it is eventually run.
+				forceInterval := max(0, time.Until(forceFullGCTTL))
+				if forceInterval < interval {
+					interval = forceInterval
+				} else {
+					// partial pass, but the new interval is less than any leftover ttl so
+					// we cache this as well.
+					cachedGCInterval = interval
 				}
 			}
-			cachedGCInterval = interval
 
 			if initialScan {
 				close(initialScanComplete)
