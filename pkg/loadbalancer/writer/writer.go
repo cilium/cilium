62fd7eeda42919d22e06e104c7670a4e1f10b8bd
Liyi Huang <pdshly@gmail.com>
2025-08-31 16:39:29 -0400

    pkg/lrp: remove the port name check in writer
    We have the frontend and backend matching port name logic introduced
    from 1.18. This could cause the issue while people upgrade from 1.17 to
    1.18 since we dont have such logic in 1.17.
    
    Consider the following example:
    apiVersion: v1
    kind: Service
    metadata:
      name: echo-server
      labels:
        app: echo-server
    spec:
      selector:
        app: echo-server
      ports:
        - name: http
          port: 8080
          targetPort: 8080
    
    ---
    apiVersion: apps/v1
    kind: Deployment
    metadata:
      name: echo-server
      labels:
        app: echo-server
    spec:
      replicas: 2
      selector:
        matchLabels:
          app: echo-server
      template:
        metadata:
          labels:
            app: echo-server
        spec:
          containers:
            - name: echo-server
              image: kicbase/echo-server:1.0
              ports:
                - containerPort: 8080 --- no name port http here
    ---
    apiVersion: "cilium.io/v2"
    kind: CiliumLocalRedirectPolicy
    metadata:
      name: "echo-server-lrp"
    spec:
      redirectFrontend:
        serviceMatcher:
          serviceName: echo-server
          namespace: default
      redirectBackend:
        localEndpointSelector:
          matchLabels:
            app: echo-server
        toPorts:
          - port: "8080"
            protocol: TCP
    
    With the above LPR config, cilium doesn't add the backend for LRP from
    1.18 because pod doesn't have http as port name but this is allowed
    and working in 1.17.
    
    Signed-off-by: Liyi Huang <liyi.huang@isovalent.com>

--- pkg/loadbalancer/writer/writer.go
+++ pkg/loadbalancer/writer/writer.go
@@ -470,15 +470,6 @@ func (w *Writer) DefaultSelectBackends(bes iter.Seq2[loadbalancer.BackendParams,
 			if checkZoneHints && !slices.Contains(be.Zone.ForZones, *thisZone) {
 				continue
 			}
-			if fe != nil {
-				if fe.PortName != "" {
-					// A backend with specific port name requested. Look up what this backend
-					// is called for this service.
-					if !slices.Contains(be.PortNames, string(fe.PortName)) {
-						continue
-					}
-				}
-			}
 			if !yield(be, rev) {
 				return
 			}
