313934e3bf12ba79e78bd6bb4f2e9505aedadd87
David Ackroyd <dackroyd@nine.com.au>
2025-09-03 12:23:47 +0000

    datapath: iptables: fix proxy removal by prefix
    [ upstream commit b2be22498ec51fd90bf18512a10669cbb4cad2db ]
    
    Preventing removal of existing tproxy iptables rules for other services
    when they share a name prefix.
    
    When rules are initially applied at startup, the full set of iptables
    rules is successfully added, and L7 proxying works as expected across
    all services. With particular combinations of namespaces/services
    however, 30m later some of those services become unroutable, seemingly
    at random. This behaviour also varies from node to node.
    
    Investigation revealed that the impacted services were those in the same
    namespace, where another L7 service's name was a prefix of the impacted
    service's name, e.g. services "api" and "api-gateway". This *does not*
    impact cases across different namespaces, or where service names share
    a prefix like "api-v1" and "api-v2".
    
    The 30m trigger for this was identified as the reconcile process, which
    attempts to sync the state, and remove any stale rules. As this
    re-applies the rules for each service, it removes any existing rules
    that match the prefix even though they belong to a different service.
    This situation could also have been triggered by new services being
    added after cilium starts, if they hit that same prefix scenario.
    
    The fix is simple: the condition to test if the rule matches the same
    service comment must include a suffix after the name, so that only exact
    matches are removed. This is consistent with the other conditions used
    to filter out mismatches.
    
    The bpf management of tproxy rules (beta) is not impacted by this
    problem, as it replaces the iptables usage.
    
    Co-authored-by: Michael Lorant <michael.lorant@nine.com.au>
    Co-authored-by: Matt Hope <matt.hope@nine.com.au>
    Co-authored-by: Marc de Beer <marcdebeer@nine.com.au>
    Fixes: #41413
    Signed-off-by: David Ackroyd <dackroyd@nine.com.au>
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- pkg/datapath/iptables/iptables_test.go
+++ pkg/datapath/iptables/iptables_test.go
@@ -344,6 +344,45 @@ func TestAddProxyRulesv4(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	mockIp4tables.expectations = []expectation{
+		{
+			args: "-t mangle -S",
+			out: []byte(
+				`-P PREROUTING ACCEPT
+-P INPUT ACCEPT
+-P FORWARD ACCEPT
+-P OUTPUT ACCEPT
+-P POSTROUTING ACCEPT
+-N OLD_CILIUM_POST_mangle
+-N OLD_CILIUM_PRE_mangle
+-N CILIUM_POST_mangle
+-N CILIUM_PRE_mangle
+-N KUBE-KUBELET-CANARY
+-N KUBE-PROXY-CANARY
+-A PREROUTING -m comment --comment "cilium-feeder: CILIUM_PRE_mangle" -j OLD_CILIUM_PRE_mangle
+-A POSTROUTING -m comment --comment "cilium-feeder: CILIUM_POST_mangle" -j OLD_CILIUM_POST_mangle
+-A PREROUTING -m comment --comment "cilium-feeder: CILIUM_PRE_mangle" -j CILIUM_PRE_mangle
+-A POSTROUTING -m comment --comment "cilium-feeder: CILIUM_POST_mangle" -j CILIUM_POST_mangle
+-A OLD_CILIUM_PRE_mangle -m socket --transparent -m comment --comment "cilium: any->pod redirect proxied traffic to host proxy" -j MARK --set-xmark 0x200/0xffffffff
+-A OLD_CILIUM_PRE_mangle -p tcp -m mark --mark 0x3920200 -m comment --comment "cilium: TPROXY to host cilium-dns-egress proxy" -j TPROXY --on-port 37379 --on-ip 127.0.0.1 --tproxy-mark 0x200/0xffffffff
+-A OLD_CILIUM_PRE_mangle -p udp -m mark --mark 0x3920200 -m comment --comment "cilium: TPROXY to host cilium-dns-egress proxy" -j TPROXY --on-port 37379 --on-ip 127.0.0.1 --tproxy-mark 0x200/0xffffffff
+-A CILIUM_PRE_mangle -p tcp -m mark --mark 0x3920200 -m comment --comment "cilium: TPROXY to host cilium-dns-egress proxy" -j TPROXY --on-port 37379 --on-ip 127.0.0.1 --tproxy-mark 0x200/0xffffffff
+-A CILIUM_PRE_mangle -p udp -m mark --mark 0x3920200 -m comment --comment "cilium: TPROXY to host cilium-dns-egress proxy" -j TPROXY --on-port 37379 --on-ip 127.0.0.1 --tproxy-mark 0x200/0xffffffff
+`),
+		}, {
+			args: "-t mangle -A CILIUM_PRE_mangle -p tcp -m mark --mark 0x4920200 -m comment --comment cilium: TPROXY to host cilium-dns proxy -j TPROXY --tproxy-mark 0x200 --on-ip 127.0.0.1 --on-port 37380",
+		}, {
+			args: "-t mangle -A CILIUM_PRE_mangle -p udp -m mark --mark 0x4920200 -m comment --comment cilium: TPROXY to host cilium-dns proxy -j TPROXY --tproxy-mark 0x200 --on-ip 127.0.0.1 --on-port 37380",
+		},
+	}
+
+	// Adds new proxy rules for different service, whose name is a prefix of an existing one
+	mockManager.addProxyRules(mockIp4tables, "127.0.0.1", 37380, "cilium-dns")
+	err = mockIp4tables.checkExpectations()
+	if err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestGetProxyPorts(t *testing.T) {
@@ -510,6 +549,45 @@ func TestAddProxyRulesv6(t *testing.T) {
 
 	// New port number, adds new ones, deletes stale rules. Does not touch OLD_ chains
 	mockManager.addProxyRules(mockIp6tables, "::1", 43479, "cilium-dns-egress")
+	err = mockIp6tables.checkExpectations()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mockIp6tables.expectations = []expectation{
+		{
+			args: "-t mangle -S",
+			out: []byte(
+				`-P PREROUTING ACCEPT
+-P INPUT ACCEPT
+-P FORWARD ACCEPT
+-P OUTPUT ACCEPT
+-P POSTROUTING ACCEPT
+-N OLD_CILIUM_POST_mangle
+-N OLD_CILIUM_PRE_mangle
+-N CILIUM_POST_mangle
+-N CILIUM_PRE_mangle
+-N KUBE-KUBELET-CANARY
+-N KUBE-PROXY-CANARY
+-A PREROUTING -m comment --comment "cilium-feeder: CILIUM_PRE_mangle" -j OLD_CILIUM_PRE_mangle
+-A POSTROUTING -m comment --comment "cilium-feeder: CILIUM_POST_mangle" -j OLD_CILIUM_POST_mangle
+-A PREROUTING -m comment --comment "cilium-feeder: CILIUM_PRE_mangle" -j CILIUM_PRE_mangle
+-A POSTROUTING -m comment --comment "cilium-feeder: CILIUM_POST_mangle" -j CILIUM_POST_mangle
+-A OLD_CILIUM_PRE_mangle -m socket --transparent -m comment --comment "cilium: any->pod redirect proxied traffic to host proxy" -j MARK --set-xmark 0x200/0xffffffff
+-A OLD_CILIUM_PRE_mangle -p tcp -m mark --mark 0xd5a90200 -m comment --comment "cilium: TPROXY to host cilium-dns-egress proxy" -j TPROXY --on-port 43477 --on-ip ::1 --tproxy-mark 0x200/0xffffffff
+-A OLD_CILIUM_PRE_mangle -p udp -m mark --mark 0xd5a90200 -m comment --comment "cilium: TPROXY to host cilium-dns-egress proxy" -j TPROXY --on-port 43477 --on-ip ::1 --tproxy-mark 0x200/0xffffffff
+-A CILIUM_PRE_mangle -p tcp -m mark --mark 0xd5a90200 -m comment --comment "cilium: TPROXY to host cilium-dns-egress proxy" -j TPROXY --on-port 43477 --on-ip ::1 --tproxy-mark 0x200/0xffffffff
+-A CILIUM_PRE_mangle -p udp -m mark --mark 0xd5a90200 -m comment --comment "cilium: TPROXY to host cilium-dns-egress proxy" -j TPROXY --on-port 43477 --on-ip ::1 --tproxy-mark 0x200/0xffffffff
+`),
+		}, {
+			args: "-t mangle -A CILIUM_PRE_mangle -p tcp -m mark --mark 0xd8a90200 -m comment --comment cilium: TPROXY to host cilium-dns proxy -j TPROXY --tproxy-mark 0x200 --on-ip ::1 --on-port 43480",
+		}, {
+			args: "-t mangle -A CILIUM_PRE_mangle -p udp -m mark --mark 0xd8a90200 -m comment --comment cilium: TPROXY to host cilium-dns proxy -j TPROXY --tproxy-mark 0x200 --on-ip ::1 --on-port 43480",
+		},
+	}
+
+	// Adds new proxy rules for different service, whose name is a prefix of an existing one
+	mockManager.addProxyRules(mockIp6tables, "::1", 43480, "cilium-dns")
 	err = mockIp6tables.checkExpectations()
 	if err != nil {
 		t.Fatal(err)
