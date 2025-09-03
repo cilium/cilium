f7fc5a2b067f0467966505e90f6f1e43e4bdb917
André Martins <andre@cilium.io>
2025-09-03 10:25:58 +0000

    metrics/features: remove aws-vpc-cni
    [ upstream commit 37f19f665af2bee10f4455b3954d9c9f97324238 ]
    
    This metric is duplicated since it was meant to be aws-cni, which
    already exists. Thus, we can remove it.
    
    Signed-off-by: André Martins <andre@cilium.io>
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- pkg/metrics/features/metrics.go
+++ pkg/metrics/features/metrics.go
@@ -90,7 +90,6 @@ const (
 
 	networkChainingModeNone        = "none"
 	networkChainingModeAWSCNI      = "aws-cni"
-	networkChainingModeAWSVPCCNI   = "aws-vpc-cni"
 	networkChainingModeCalico      = "calico"
 	networkChainingModeFlannel     = "flannel"
 	networkChainingModeGenericVeth = "generic-veth"
@@ -145,7 +144,6 @@ var (
 	defaultChainingModes = []string{
 		networkChainingModeNone,
 		networkChainingModeAWSCNI,
-		networkChainingModeAWSVPCCNI,
 		networkChainingModeCalico,
 		networkChainingModeFlannel,
 		networkChainingModeGenericVeth,
