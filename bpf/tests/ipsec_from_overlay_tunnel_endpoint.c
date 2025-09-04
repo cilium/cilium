5fce3da500e1e5624b4062b2d0eda3aaeda0a577
Julian Wiedmann <jwi@isovalent.com>
2025-09-04 18:25:46 +0300

    datapath: remove inbound ESP-in-overlay support
    This was only needed for the upgrade from v1.17, and can now safely go away
    in the v1.19 release.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/tests/ipsec_from_overlay_tunnel_endpoint.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
-/* Copyright Authors of Cilium */
-
-#define ENABLE_ENDPOINT_ROUTES 1
-
-#define EXPECTED_STATUS_CODE_FOR_DECRYPTED TC_ACT_OK
-
-#include "ipsec_from_overlay_generic.h"
