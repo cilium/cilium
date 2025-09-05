ce761116b1eac06733997bc68751ec97810d40f6
Tam Mach <tam.mach@cilium.io>
2025-09-05 19:54:09 +1000

    test: Extend L7LB test for ipv6
    Relates: https://github.com/cilium/cilium/pull/39577#discussion_r2313336617
    
    Suggested-by: Julian Wiedmann <jwi@isovalent.com>
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- cilium-cli/connectivity/tests/service.go
+++ cilium-cli/connectivity/tests/service.go
@@ -381,12 +381,14 @@ func (s *podToL7Service) Run(ctx context.Context, t *check.Test) {
 			if !hasAllLabels(svc, s.destinationLabels) {
 				continue
 			}
-			t.NewAction(s, fmt.Sprintf("curl-%d", i), &pod, svc, features.IPFamilyAny).Run(func(a *check.Action) {
-				a.ExecInPod(ctx, a.CurlCommand(svc))
-				a.ValidateFlows(ctx, pod, a.GetEgressRequirements(check.FlowParameters{
-					DNSRequired: true,
-					AltDstPort:  svc.Port(),
-				}))
+			t.ForEachIPFamily(func(ipFamily features.IPFamily) {
+				t.NewAction(s, fmt.Sprintf("curl-%s-%d", ipFamily, i), &pod, svc, ipFamily).Run(func(a *check.Action) {
+					a.ExecInPod(ctx, a.CurlCommand(svc))
+					a.ValidateFlows(ctx, pod, a.GetEgressRequirements(check.FlowParameters{
+						DNSRequired: true,
+						AltDstPort:  svc.Port(),
+					}))
+				})
 			})
 			i++
 		}
