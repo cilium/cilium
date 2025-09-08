cilium/cilium

commit 93018d28b4197fc556e26c24c3ed369b8cc812b6

envoy: add stream idle timeout configuration option
Signed-off-by: joey <zchengjoey@gmail.com>
Signed-off-by: Tam Mach <tam.mach@cilium.io>

operator/option/config.go
@@ -29,6 +29,9 @@ const (
 
 	// DefaultProxyIdleTimeoutSeconds is the default value for the proxy idle timeout
 	DefaultProxyIdleTimeoutSeconds = 60
+
+	// DefaultProxyStreamIdleTimeoutSeconds is the default value for the proxy stream idle timeout
+	DefaultProxyStreamIdleTimeoutSeconds = 300
 )
 
 const (
@@ -178,6 +181,9 @@ const (
 
 	// ProxyIdleTimeoutSeconds is the idle timeout for proxy connections to upstream clusters
 	ProxyIdleTimeoutSeconds = "proxy-idle-timeout-seconds"
+
+	// ProxyStreamIdleTimeoutSeconds is the stream timeout for proxy connections to upstream clusters
+	ProxyStreamIdleTimeoutSeconds = "proxy-stream-idle-timeout-seconds"
 
 	// EnableGatewayAPI enables support of Gateway API
 	// This must be enabled along with enable-envoy-config in cilium agent.
@@ -377,6 +383,9 @@ type OperatorConfig struct {
 	// ProxyIdleTimeoutSeconds is the idle timeout for the proxy to upstream cluster
 	ProxyIdleTimeoutSeconds int
 
+	// ProxyStreamIdleTimeoutSeconds is the stream idle timeout for the proxy to upstream cluster
+	ProxyStreamIdleTimeoutSeconds int
+
 	// CiliumK8sNamespace is the namespace where Cilium pods are running.
 	CiliumK8sNamespace string
 
@@ -423,6 +432,10 @@ func (c *OperatorConfig) Populate(vp *viper.Viper) {
 	c.ProxyIdleTimeoutSeconds = vp.GetInt(ProxyIdleTimeoutSeconds)
 	if c.ProxyIdleTimeoutSeconds == 0 {
 		c.ProxyIdleTimeoutSeconds = DefaultProxyIdleTimeoutSeconds
+	}
+	c.ProxyStreamIdleTimeoutSeconds = vp.GetInt(ProxyStreamIdleTimeoutSeconds)
+	if c.ProxyStreamIdleTimeoutSeconds == 0 {
+		c.ProxyStreamIdleTimeoutSeconds = DefaultProxyStreamIdleTimeoutSeconds
 	}
 	c.CiliumPodLabels = vp.GetString(CiliumPodLabels)
 	c.TaintSyncWorkers = vp.GetInt(TaintSyncWorkers)

operator/pkg/ciliumenvoyconfig/ciliumenvoyconfig.go
@@ -18,16 +18,17 @@ type ciliumEnvoyConfigReconciler struct {
 	client client.Client
 	logger *slog.Logger
 
-	algorithm          string
-	ports              []string
-	maxRetries         int
-	idleTimeoutSeconds int
-	enableIpv4         bool
-	enableIpv6         bool
+	algorithm                string
+	ports                    []string
+	maxRetries               int
+	idleTimeoutSeconds       int
+	streamIdleTimeoutSeconds int
+	enableIpv4               bool
+	enableIpv6               bool
 }
 
 func newCiliumEnvoyConfigReconciler(c client.Client, logger *slog.Logger, defaultAlgorithm string, ports []string,
-	maxRetries int, idleTimeoutSeconds int, enableIpv4 bool, enableIpv6 bool,
+	maxRetries int, idleTimeoutSeconds int, streamIdleTimeoutSeconds int, enableIpv4 bool, enableIpv6 bool,
 ) *ciliumEnvoyConfigReconciler {
 	return &ciliumEnvoyConfigReconciler{
 		client: c,

operator/pkg/model/translation/envoy_listener.go
@@ -19,6 +19,7 @@ import (
 	envoy_extensions_transport_sockets_tls_v3 "github.com/cilium/proxy/go/envoy/extensions/transport_sockets/tls/v3"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/durationpb"
 
 	"github.com/cilium/cilium/operator/pkg/model"
 	"github.com/cilium/cilium/pkg/envoy"
@@ -106,6 +107,39 @@ func withXffNumTrustedHops(xff uint32) ListenerMutator {
 							continue
 						}
 						hcmConfig.XffNumTrustedHops = xff
+						filter.ConfigType = &envoy_config_listener.Filter_TypedConfig{
+							TypedConfig: toAny(hcmConfig),
+						}
+					}
+				}
+			}
+		}
+		return listener
+	}
+}
+
+func WithStreamIdleTimeout(streamIdleTimeoutSeconds int) ListenerMutator {
+	return func(listener *envoy_config_listener.Listener) *envoy_config_listener.Listener {
+		if streamIdleTimeoutSeconds == 0 {
+			return listener
+		}
+		for _, filterChain := range listener.FilterChains {
+			for _, filter := range filterChain.Filters {
+				if filter.Name == httpConnectionManagerType {
+					tc := filter.GetTypedConfig()
+					switch tc.GetTypeUrl() {
+					case envoy.HttpConnectionManagerTypeURL:
+						hcm, err := tc.UnmarshalNew()
+						if err != nil {
+							continue
+						}
+						hcmConfig, ok := hcm.(*httpConnectionManagerv3.HttpConnectionManager)
+						if !ok {
+							continue
+						}
+						hcmConfig.StreamIdleTimeout = &durationpb.Duration{
+							Seconds: int64(streamIdleTimeoutSeconds),
+						}
 						filter.ConfigType = &envoy_config_listener.Filter_TypedConfig{
 							TypedConfig: toAny(hcmConfig),
 						}
@@ -249,6 +283,10 @@ func (i *cecTranslator) listenerMutators(m *model.Model) []ListenerMutator {
 
 	if i.Config.HostNetworkConfig.Enabled {
 		res = append(res, withHostNetworkPort(m, i.Config.IPConfig.IPv4Enabled, i.Config.IPConfig.IPv6Enabled))
+	}
+
+	if i.Config.ListenerConfig.StreamIdleTimeoutSeconds > 0 {
+		res = append(res, WithStreamIdleTimeout(i.Config.ListenerConfig.StreamIdleTimeoutSeconds))
 	}
 
 	if i.Config.OriginalIPDetectionConfig.XFFNumTrustedHops > 0 {

commit 1bb21ac81f753313bea9b9f22dd6e0d6f237ef55

envoy: Allow stream timeout to be disabled
This commit is to allow to have stream timeout to be disabled (i.e. the
value of zero). Additionally, this commit is to pass the value for L7
LB service, which is missed in original PR.

Fixes: 93018d28b4197fc556e26c24c3ed369b8cc812b6
Signed-off-by: Tam Mach <tam.mach@cilium.io>

operator/option/config.go
@@ -363,6 +363,8 @@ type OperatorConfig struct {
 	ProxyIdleTimeoutSeconds int
 
 	// ProxyStreamIdleTimeoutSeconds is the stream idle timeout for the proxy to upstream cluster
+	// A value of 0 will completely disable the connection manager stream idle timeout.
+	// A value of -1 will ignore the stream idle timeout and use whatever is default in Envoy (currently 300s).
 	ProxyStreamIdleTimeoutSeconds int
 
 	// CiliumK8sNamespace is the namespace where Cilium pods are running.
@@ -416,9 +418,6 @@ func (c *OperatorConfig) Populate(logger *slog.Logger, vp *viper.Viper) {
 		c.ProxyIdleTimeoutSeconds = DefaultProxyIdleTimeoutSeconds
 	}
 	c.ProxyStreamIdleTimeoutSeconds = vp.GetInt(ProxyStreamIdleTimeoutSeconds)
-	if c.ProxyStreamIdleTimeoutSeconds == 0 {
-		c.ProxyStreamIdleTimeoutSeconds = DefaultProxyStreamIdleTimeoutSeconds
-	}
 	c.CiliumPodLabels = vp.GetString(CiliumPodLabels)
 	c.TaintSyncWorkers = vp.GetInt(TaintSyncWorkers)
 	c.RemoveCiliumNodeTaints = vp.GetBool(RemoveCiliumNodeTaints)

operator/pkg/ciliumenvoyconfig/ciliumenvoyconfig.go
@@ -34,12 +34,13 @@ func newCiliumEnvoyConfigReconciler(c client.Client, logger *slog.Logger, defaul
 		client: c,
 		logger: logger,
 
-		algorithm:          defaultAlgorithm,
-		ports:              ports,
-		maxRetries:         maxRetries,
-		idleTimeoutSeconds: idleTimeoutSeconds,
-		enableIpv4:         enableIpv4,
-		enableIpv6:         enableIpv6,
+		algorithm:                defaultAlgorithm,
+		ports:                    ports,
+		maxRetries:               maxRetries,
+		idleTimeoutSeconds:       idleTimeoutSeconds,
+		streamIdleTimeoutSeconds: streamIdleTimeoutSeconds,
+		enableIpv4:               enableIpv4,
+		enableIpv6:               enableIpv6,
 	}
 }
 

operator/pkg/model/translation/envoy_listener.go
@@ -285,7 +285,7 @@ func (i *cecTranslator) listenerMutators(m *model.Model) []ListenerMutator {
 		res = append(res, withHostNetworkPort(m, i.Config.IPConfig.IPv4Enabled, i.Config.IPConfig.IPv6Enabled))
 	}
 
-	if i.Config.ListenerConfig.StreamIdleTimeoutSeconds > 0 {
+	if i.Config.ListenerConfig.StreamIdleTimeoutSeconds > -1 {
 		res = append(res, WithStreamIdleTimeout(i.Config.ListenerConfig.StreamIdleTimeoutSeconds))
 	}
 