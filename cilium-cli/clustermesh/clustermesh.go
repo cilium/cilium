cilium/cilium

commit 3ab6ebf3f5500c3b8c1f68c334ce4051bd2caa88

clustermesh: remove GlobalServiceCache in cilium-agent
This remove the usage of GlobalServiceCache in the agent which was only useful
to count the number of global Service. This count didn't accounted the local
cluster and thus is misleading. While performance impact was not tested this
removes managing two level of nested maps and a global lock on each
remote endpoints updates which should certainly be valuable.

The global services count reported through cilium-dbg and the CLI
is no longer supported/exposed. Users with an older version of the CLI would
always see a count of 0 reported. Global Service counts will continue to be
reported per cluster along the count with other resources though.

Signed-off-by: Arthur Outhenin-Chalandre <git@mrfreezeex.fr>

cilium-cli/clustermesh/clustermesh.go
@@ -682,15 +682,6 @@ func remoteClusterStatusToError(status *models.RemoteCluster) error {
 }
 
 func (c *ConnectivityStatus) parseAgentStatus(name string, expected []string, s *status.ClusterMeshAgentConnectivityStatus) {
-	if c.GlobalServices.Min < 0 || c.GlobalServices.Min > s.GlobalServices {
-		c.GlobalServices.Min = s.GlobalServices
-	}
-
-	if c.GlobalServices.Max < s.GlobalServices {
-		c.GlobalServices.Max = s.GlobalServices
-	}
-
-	c.GlobalServices.Avg += float64(s.GlobalServices)
 	c.Total++
 
 	ready := int64(0)

pkg/clustermesh/clustermesh.go
@@ -110,10 +110,6 @@ type ClusterMesh struct {
 	// common implements the common logic to connect to remote clusters.
 	common common.ClusterMesh
 
-	// globalServices is a list of all global services. The datastructure
-	// is protected by its own mutex inside the structure.
-	globalServices *common.GlobalServiceCache
-
 	// nodeName is the name of the local node. This is used for logging and metrics
 	nodeName string
 
@@ -134,12 +130,8 @@ func NewClusterMesh(lifecycle cell.Lifecycle, c Configuration) *ClusterMesh {
 
 	nodeName := nodeTypes.GetName()
 	cm := &ClusterMesh{
-		conf:     c,
-		nodeName: nodeName,
-		globalServices: common.NewGlobalServiceCache(
-			c.Logger,
-			c.Metrics.TotalGlobalServices.WithLabelValues(c.ClusterInfo.Name, nodeName),
-		),
+		conf:           c,
+		nodeName:       nodeName,
 		FeatureMetrics: c.FeatureMetrics,
 	}
 
@@ -168,6 +160,20 @@ func NewClusterMesh(lifecycle cell.Lifecycle, c Configuration) *ClusterMesh {
 
 	lifecycle.Append(cm.common)
 	return cm
+}
+
+type clusterServiceObserver struct {
+	serviceMerger ServiceMerger
+}
+
+func (obs *clusterServiceObserver) OnUpdate(key store.Key) {
+	svc := &(key.(*serviceStore.ValidatingClusterService).ClusterService)
+	obs.serviceMerger.MergeExternalServiceUpdate(svc)
+}
+
+func (obs *clusterServiceObserver) OnDelete(key store.NamedKey) {
+	svc := &(key.(*serviceStore.ValidatingClusterService).ClusterService)
+	obs.serviceMerger.MergeExternalServiceDelete(svc)
 }
 
 func (cm *ClusterMesh) NewRemoteCluster(name string, status common.StatusFunc) common.RemoteCluster {
@@ -203,12 +209,7 @@ func (cm *ClusterMesh) NewRemoteCluster(name string, status common.StatusFunc) c
 			serviceStore.NamespacedNameValidator(),
 			serviceStore.ClusterIDValidator(&rc.clusterID),
 		),
-		common.NewSharedServicesObserver(
-			rc.log,
-			cm.globalServices,
-			cm.conf.ServiceMerger.MergeExternalServiceUpdate,
-			cm.conf.ServiceMerger.MergeExternalServiceDelete,
-		),
+		&clusterServiceObserver{serviceMerger: cm.conf.ServiceMerger},
 		store.RWSWithOnSyncCallback(func(ctx context.Context) { close(rc.synced.services) }),
 	)
 
@@ -281,9 +282,7 @@ func (cm *ClusterMesh) synced(ctx context.Context, toWaitFn func(*remoteCluster)
 
 // Status returns the status of the ClusterMesh subsystem
 func (cm *ClusterMesh) Status() (status *models.ClusterMeshStatus) {
-	status = &models.ClusterMeshStatus{
-		NumGlobalServices: int64(cm.globalServices.Size()),
-	}
+	status = &models.ClusterMeshStatus{}
 
 	cm.common.ForEachRemoteCluster(func(rci common.RemoteCluster) error {
 		rc := rci.(*remoteCluster)

api/v1/models/cluster_mesh_status.go
@@ -26,9 +26,6 @@ type ClusterMeshStatus struct {
 
 	// List of remote clusters
 	Clusters []*RemoteCluster `json:"clusters"`
-
-	// Number of global services
-	NumGlobalServices int64 `json:"num-global-services,omitempty"`
 }
 
 // Validate validates this cluster mesh status

api/v1/openapi.yaml
@@ -2339,9 +2339,6 @@ definitions:
         type: array
         items:
           "$ref": "#/definitions/RemoteCluster"
-      num-global-services:
-        description: Number of global services
-        type: integer
   RemoteCluster:
     description: |-
       Status of remote cluster

api/v1/server/embedded_spec.go
@@ -2200,10 +2200,6 @@ func init() {
           "items": {
             "$ref": "#/definitions/RemoteCluster"
           }
-        },
-        "num-global-services": {
-          "description": "Number of global services",
-          "type": "integer"
         }
       }
     },
@@ -7569,10 +7565,6 @@ func init() {
           "items": {
             "$ref": "#/definitions/RemoteCluster"
           }
-        },
-        "num-global-services": {
-          "description": "Number of global services",
-          "type": "integer"
         }
       }
     },

cilium-cli/status/k8s.go
@@ -92,9 +92,8 @@ func NewK8sStatusCollector(client k8sImplementation, params K8sStatusParameters)
 }
 
 type ClusterMeshAgentConnectivityStatus struct {
-	GlobalServices int64
-	Clusters       map[string]*models.RemoteCluster
-	Errors         ErrorCountMap
+	Clusters map[string]*models.RemoteCluster
+	Errors   ErrorCountMap
 }
 
 // ErrClusterMeshStatusNotAvailable is a sentinel.
@@ -117,7 +116,6 @@ func (k *K8sStatusCollector) ClusterMeshConnectivity(ctx context.Context, cilium
 		return nil, ErrClusterMeshStatusNotAvailable
 	}
 
-	c.GlobalServices = status.ClusterMesh.NumGlobalServices
 	for _, cluster := range status.ClusterMesh.Clusters {
 		c.Clusters[cluster.Name] = cluster
 	}

pkg/client/client.go
@@ -436,8 +436,8 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 	}
 
 	if sr.ClusterMesh != nil {
-		fmt.Fprintf(w, "ClusterMesh:\t%d/%d remote clusters ready, %d global-services\n",
-			NumReadyClusters(sr.ClusterMesh.Clusters), len(sr.ClusterMesh.Clusters), sr.ClusterMesh.NumGlobalServices)
+		fmt.Fprintf(w, "ClusterMesh:\t%d/%d remote clusters ready\n",
+			NumReadyClusters(sr.ClusterMesh.Clusters), len(sr.ClusterMesh.Clusters))
 
 		verbosity := RemoteClustersStatusNotReadyOnly
 		if sd.AllClusters {

pkg/clustermesh/remote_cluster_test.go
@@ -16,7 +16,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/cilium/cilium/pkg/clustermesh/common"
 	serviceStore "github.com/cilium/cilium/pkg/clustermesh/store"
 	"github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/identity"
@@ -24,7 +23,6 @@ import (
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/kvstore/store"
-	"github.com/cilium/cilium/pkg/metrics"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/source"
 	"github.com/cilium/cilium/pkg/testutils"
@@ -166,7 +164,6 @@ func TestRemoteClusterRun(t *testing.T) {
 					FeatureMetrics:        NewClusterMeshMetricsNoop(),
 					Logger:                logger,
 				},
-				globalServices: common.NewGlobalServiceCache(logger, metrics.NoOpGauge),
 				FeatureMetrics: NewClusterMeshMetricsNoop(),
 			}
 			rc := cm.NewRemoteCluster("foo", nil).(*remoteCluster)
@@ -297,7 +294,6 @@ func TestRemoteClusterClusterIDChange(t *testing.T) {
 			Logger:                logger,
 		},
 		FeatureMetrics: NewClusterMeshMetricsNoop(),
-		globalServices: common.NewGlobalServiceCache(logger, metrics.NoOpGauge),
 	}
 	rc := cm.NewRemoteCluster("foo", nil).(*remoteCluster)
 

commit 0192c871333c1cce36c6dcfb17c7d4620cd601a7

cli/clustermesh: remove leftover global services status info
The support for exposing and displaying the count of global services got
dropped in the blamed commit. Let's also clean up the leftover global
services status info, which otherwise always displays incorrect information.

Fixes: 3ab6ebf3f550 ("clustermesh: remove GlobalServiceCache in cilium-agent")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

cilium-cli/clustermesh/clustermesh.go
@@ -629,12 +629,11 @@ type ClusterStats struct {
 }
 
 type ConnectivityStatus struct {
-	GlobalServices StatisticalStatus        `json:"global_services,omitempty"`
-	Connected      StatisticalStatus        `json:"connected,omitempty"`
-	Clusters       map[string]*ClusterStats `json:"clusters,omitempty"`
-	Total          int64                    `json:"total,omitempty"`
-	NotReady       int64                    `json:"not_ready,omitempty"`
-	Errors         status.ErrorCountMapMap  `json:"errors,omitempty"`
+	Connected StatisticalStatus        `json:"connected,omitempty"`
+	Clusters  map[string]*ClusterStats `json:"clusters,omitempty"`
+	Total     int64                    `json:"total,omitempty"`
+	NotReady  int64                    `json:"not_ready,omitempty"`
+	Errors    status.ErrorCountMapMap  `json:"errors,omitempty"`
 }
 
 func (c *ConnectivityStatus) addError(pod, cluster string, err error) {
@@ -788,10 +787,9 @@ func (k *K8sClusterMesh) determineStatusConnectivity(ctx context.Context, secret
 	collector func(ctx context.Context, ciliumPod string) (*status.ClusterMeshAgentConnectivityStatus, error),
 ) (*ConnectivityStatus, error) {
 	stats := &ConnectivityStatus{
-		GlobalServices: StatisticalStatus{Min: -1},
-		Connected:      StatisticalStatus{Min: -1},
-		Errors:         status.ErrorCountMapMap{},
-		Clusters:       map[string]*ClusterStats{},
+		Connected: StatisticalStatus{Min: -1},
+		Errors:    status.ErrorCountMapMap{},
+		Clusters:  map[string]*ClusterStats{},
 	}
 
 	// Retrieve the remote clusters to connect to from the clustermesh configuration,
@@ -831,7 +829,6 @@ func (k *K8sClusterMesh) determineStatusConnectivity(ctx context.Context, secret
 	}
 
 	if len(pods.Items) > 0 {
-		stats.GlobalServices.Avg /= float64(len(pods.Items))
 		stats.Connected.Avg /= float64(len(pods.Items))
 	}
 
@@ -960,12 +957,6 @@ func (k *K8sClusterMesh) outputConnectivityStatus(agents, kvstoremesh *Connectiv
 	} else {
 		k.Log("🔌 No cluster connected")
 	}
-
-	k.Log("")
-	k.Log("🔀 Global services: [ min:%d / avg:%.1f / max:%d ]",
-		agents.GlobalServices.Min,
-		agents.GlobalServices.Avg,
-		agents.GlobalServices.Max)
 
 	k.Log("")
 	errCount := len(agents.Errors)