e169a715cbe1a173cb7a270f2ea6652d76545670
Marc Suñé <marc.sune@isovalent.com>
2025-09-17 12:39:02 +0200

    bpf/tests: add flowtracer unit test
    This commit adds a unit test for the new flowtracer library.
    
    Signed-off-by: Marc Suñé <marc.sune@isovalent.com>

---
+++ bpf/tests/scapy/tc_flowtracer_pkt_defs.py
@@ -0,0 +1,46 @@
+from pkt_defs import *
+from flowtracer_hdr import *
+
+ee_tc_ft_sentinel = (
+    Ether(dst=mac_one, src=mac_two) /
+    IP(src=v4_ext_one, dst=v4_svc_one) /
+    TCP(dport=80) /
+    FlowTracer(cmds = FT_CMDS_ALL, l4_sport=64000) /
+    Raw(b'\00'*512)
+)
+
+ee_tc_ft_sentinel_intercepted = ee_tc_ft_sentinel.copy()
+ee_tc_ft_sentinel_intercepted["TCP"].sport = 64000
+ee_tc_ft_sentinel_intercepted["FlowTracer"].l4_sport = 896
+
+assert(ee_tc_ft_sentinel["TCP"].sport == 896)
+assert(ee_tc_ft_sentinel_intercepted["TCP"].sport == 64000)
+
+ee_tc_ft_traces32 = (
+    Ether(dst=mac_one, src=mac_two) /
+    IP(src=v4_ext_one, dst=v4_svc_one) /
+    TCP(dport=80, sport=64000) /
+    FlowTracer(cmds = FT_CMDS_ALL, l4_sport=896, tlvs= [
+        FlowTracerTLVIngIfindex(tracepoint=0x16E55, ifindex=1),
+        FlowTracerTLVEgrIfindex(tracepoint=0xE6E55, ifindex=2),
+        FlowTracerTLVCpu(tracepoint=0xC06E, cpu=2)
+    ]) /
+    Raw(b'\00'*464)
+)
+
+ee_tc_ft_traces3264 = (
+    Ether(dst=mac_one, src=mac_two) /
+    IP(src=v4_ext_one, dst=v4_svc_one) /
+    TCP(dport=80, sport=64000) /
+    FlowTracer(cmds = FT_CMDS_ALL, l4_sport=896, tlvs= [
+        FlowTracerTLVIngIfindex(tracepoint=0x16E55, ifindex=1),
+        FlowTracerTLVEgrIfindex(tracepoint=0xE6E55, ifindex=2),
+        FlowTracerTLVCpu(tracepoint=0xC06E, cpu=2),
+        FlowTracerTLVIngTs(tracepoint=0x16E55, ts=0x1234),
+        FlowTracerTLVEgrTs(tracepoint=0xE6E55, ts=0x4321),
+        FlowTracerTLVNode(tracepoint=0xE6E55, node=0x1B00F1),
+        FlowTracerTLVLBNode(tracepoint=0xE6E55, node=0x1B00F2),
+        FlowTracerTLVLBBackend(tracepoint=0xE6E55, backend=0x1B00BE)
+    ]) /
+    Raw(b'\00'*364)
+)
