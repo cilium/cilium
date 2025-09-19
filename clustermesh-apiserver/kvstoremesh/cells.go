cilium/cilium

commit f3a76e9799fd66fdbaf2d46801c4deccd5ecdcb8

kvstoremesh: leader election with etcd lock Fixes: #37127
Create lock in destination etcd so that multiple instances
of kvstoremesh can be used to sync data.

Signed-off-by: Petr Baloun <petr.baloun@firma.seznam.cz>

clustermesh-apiserver/kvstoremesh/cells.go
@@ -4,7 +4,6 @@
 package kvstoremesh
 
 import (
-	"github.com/cilium/cilium/clustermesh-apiserver/syncstate"
 	"github.com/cilium/hive/cell"
 
 	"github.com/cilium/cilium/clustermesh-apiserver/option"
@@ -28,32 +27,29 @@ var Cell = cell.Module(
 
 	APIServerCell,
 
-	kvstoremesh.Cell,
+	WithLeaderLifecycle(
+		kvstoremesh.Cell,
 
-	cell.Provide(func(kmConfig kvstoremesh.Config) heartbeat.Config {
-		return heartbeat.Config{
-			EnableHeartBeat: kmConfig.EnableHeartBeat,
-		}
-	}),
-	heartbeat.Cell,
+		cell.Provide(func(kmConfig kvstoremesh.Config) heartbeat.Config {
+			return heartbeat.Config{
+				EnableHeartBeat: kmConfig.EnableHeartBeat,
+			}
+		}),
+		heartbeat.Cell,
 
-	cell.Invoke(kvstoremesh.RegisterSyncWaiter),
+		cell.Invoke(func(*kvstoremesh.KVStoreMesh) {}),
+	),
 
-	cell.Invoke(func(*kvstoremesh.KVStoreMesh) {}),
-
-	cell.Invoke(registerSyncStateStop),
+	// This needs to be the last in the list, so that the start hook responsible
+	// for leader election is guaranteed to be executed last, when
+	// all the previous ones have already completed. Otherwise, cells within
+	// the "WithLeaderLifecycle" scope may be incorrectly started too early,
+	// given that "registerLeaderElectionHooks" does not depend on all of their
+	// individual dependencies outside of that scope.
+	cell.Invoke(
+		registerLeaderElectionHooks,
+	),
 )
-
-func registerSyncStateStop(lc cell.Lifecycle, ss syncstate.SyncState) {
-	lc.Append(
-		cell.Hook{
-			OnStart: func(cell.HookContext) error {
-				ss.Stop()
-				return nil
-			},
-		},
-	)
-}
 
 var pprofConfig = pprof.Config{
 	Pprof:        false,

clustermesh-apiserver/kvstoremesh/root.go
@@ -4,14 +4,30 @@
 package kvstoremesh
 
 import (
+	"context"
+	"errors"
+	"log/slog"
+	"sync"
+	"time"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/spf13/cobra"
 
+	"github.com/cilium/cilium/clustermesh-apiserver/syncstate"
+	"github.com/cilium/cilium/pkg/clustermesh/kvstoremesh"
 	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/version"
+)
+
+var (
+	backendLockName = kvstore.BaseKeyPrefix + "/kvstoremesh-lock"
 )
 
 func NewCmd(h *hive.Hive) *cobra.Command {
@@ -43,4 +59,110 @@ func NewCmd(h *hive.Hive) *cobra.Command {
 	rootCmd.AddCommand(h.Command())
 
 	return rootCmd
+}
+
+type params struct {
+	cell.In
+
+	// Client is the client targeting the local cluster
+	Client kvstore.Client
+
+	Metrics     kvstoremesh.Metrics
+	Shutdowner  hive.Shutdowner
+	Log         *slog.Logger
+	SyncState   syncstate.SyncState
+	JobGroup    job.Group
+	KVStoreMesh *kvstoremesh.KVStoreMesh
+	Health      cell.Health
+}
+
+func registerLeaderElectionHooks(lc cell.Lifecycle, llc *LeaderLifecycle, params params) {
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+
+	lc.Append(cell.Hook{
+		OnStart: func(cell.HookContext) error {
+			wg.Add(1)
+			go func() {
+				runLeaderElection(ctx, llc, params)
+				wg.Done()
+			}()
+			return nil
+		},
+		OnStop: func(hctx cell.HookContext) error {
+			if err := llc.Stop(params.Log, hctx); err != nil {
+				return err
+			}
+			cancel()
+			wg.Wait()
+			return nil
+		},
+	})
+}
+
+func runLeaderElection(ctx context.Context, lc *LeaderLifecycle, params params) {
+
+	params.Client.RegisterLockLeaseExpiredObserver(backendLockName, func(string) {
+		params.Shutdowner.Shutdown(hive.ShutdownWithError(errors.New("Leader election lost")))
+	})
+
+	params.Metrics.LeaderElectionStatus.With(prometheus.Labels{metrics.LabelLeaderElectionName: "kvstoremesh"}).Set(float64(0))
+
+	// Try to win leader election with short timeout to verify if we can
+	// be immediately promoted as leader (e.g., we are the only replica).
+	// If the timeout expires, just signal readiness and try again
+	// with infinite timeout.
+	params.Log.Info("Acquiring leader election lock")
+	toCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+
+	lock, err := params.Client.LockPath(toCtx, backendLockName)
+	cancel()
+
+	if err != nil && errors.Is(err, kvstore.ErrEtcdTimeout) {
+		// signal readiness
+		params.SyncState.Stop()
+
+		// try again with infinite timeout
+		params.Log.Info("Reattempting to acquire leader election lock")
+		lock, err = params.Client.LockPath(ctx, backendLockName)
+	}
+
+	if err != nil {
+		params.Log.Error("Failed to acquire backend lock", logfields.Error, err)
+		// no need to shutdown here as it is triggered by etcd lock lease expired hook
+		return
+	}
+
+	defer func() {
+		params.Log.Info("Releasing leader election lock")
+
+		// unlock with timeout for case the etcd sidecar has already terminated
+		toCtx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+		err = lock.Unlock(toCtx)
+		if err != nil {
+			params.Log.Error("Failed to release leader election lock", logfields.Error, err)
+		}
+	}()
+
+	params.Log.Info("Leader election lock acquired")
+	params.Metrics.LeaderElectionStatus.With(prometheus.Labels{metrics.LabelLeaderElectionName: "kvstoremesh"}).Set(float64(1))
+
+	kvstoremesh.RegisterSyncWaiter(kvstoremesh.SyncWaiterParams{
+		KVStoreMesh: params.KVStoreMesh,
+		SyncState:   params.SyncState,
+		Lifecycle:   lc,
+		JobGroup:    params.JobGroup,
+		Health:      params.Health,
+	})
+	params.SyncState.Stop()
+
+	err = lc.Start(params.Log, ctx)
+	if err != nil {
+		params.Log.Error("Failed to run KvStoreMesh", logfields.Error, err)
+		params.Shutdowner.Shutdown(hive.ShutdownWithError(err))
+		return
+	}
+
+	<-ctx.Done()
 }

pkg/clustermesh/kvstoremesh/kvstoremesh.go
@@ -113,8 +113,6 @@ func newKVStoreMesh(lc cell.Lifecycle, params params) *KVStoreMesh {
 }
 
 type SyncWaiterParams struct {
-	cell.In
-
 	KVStoreMesh *KVStoreMesh
 	SyncState   syncstate.SyncState
 	Lifecycle   cell.Lifecycle

clustermesh-apiserver/kvstoremesh/lifecycle.go
@@ -1,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package kvstoremesh
+
+import (
+	"log/slog"
+	"runtime/pprof"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+)
+
+// LeaderLifecycle is the inner lifecycle of the kvstoremesh that is started when this
+// instance is elected leader. It implements cell.Lifecycle allowing cells to use it.
+type LeaderLifecycle struct {
+	cell.DefaultLifecycle
+}
+
+func WithLeaderLifecycle(cells ...cell.Cell) cell.Cell {
+	return cell.Module(
+		"leader-lifecycle",
+		"KVStoreMesh Leader Lifecycle",
+
+		cell.Provide(
+			func() *LeaderLifecycle { return &LeaderLifecycle{} },
+		),
+		cell.Decorate(
+			func(reg job.Registry, h cell.Health, logger *slog.Logger, llc *LeaderLifecycle, mid cell.ModuleID) (cell.Lifecycle, job.Group) {
+				return llc, reg.NewGroup(h, llc,
+					job.WithLogger(logger),
+					job.WithPprofLabels(pprof.Labels("cell", string(mid))))
+			},
+			cells...,
+		),
+	)
+}

pkg/clustermesh/kvstoremesh/cell.go
@@ -23,4 +23,5 @@ var Cell = cell.Module(
 	cell.Config(common.DefaultConfig),
 
 	metrics.Metric(common.MetricsProvider("")),
+	metrics.Metric(MetricsProvider),
 )

pkg/clustermesh/kvstoremesh/metrics.go
@@ -1,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package kvstoremesh
+
+import (
+	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/metrics/metric"
+)
+
+type Metrics struct {
+	// LeaderElectionStatus indicates state of leader election
+	LeaderElectionStatus metric.Vec[metric.Gauge]
+}
+
+func MetricsProvider() Metrics {
+	return Metrics{
+		LeaderElectionStatus: metric.NewGaugeVec(metric.GaugeOpts{
+			Namespace: metrics.Namespace,
+			Subsystem: "",
+			Name:      "leader_election_master_status",
+			Help:      "leader election status",
+		}, []string{metrics.LabelLeaderElectionName}),
+	}
+}

pkg/metrics/metrics.go
@@ -223,6 +223,9 @@ const (
 	// LabelTargetCluster is the label for target cluster name
 	LabelTargetCluster = "target_cluster"
 
+	// LabelLeaderElectionName is the name of leader election
+	LabelLeaderElectionName = "name"
+
 	// Rule label is a label for a L7 rule name.
 	LabelL7Rule = "rule"
 

commit 870571b7396be9fdd85279e122fd704f66d25689

kvstoremesh: fix regression in determining end of bootstrap phase
The KVStoreMesh bootstrap phase takes care of populating the initial
state into etcd for all the target clusters, and operates under the
assumption that no watchers have been established yet (the container
is not marked as ready) so that higher rate limiting settings can
be used to speed up the process.

However, the blamed commit broke this logic due to some shuffling,
and KVStoreMesh could end up exiting the bootstrap phase too early,
when synchronization did not actually complete. That's because it
was no longer guaranteed that the job waiting for synchronization
started after the KVStoreMesh start hook, which retrieves the list
of clusters to connect to.

Let's get this fixed, moving that logic back to the leader election
cell, so that it uses the appropriate job group, that depends on
KVStoreMesh, and gets started only after its start hook terminated.
While not being strictly necessary, let's also add an explicit check
so that [*KVStoreMesh.synced] blocks until the start hook actually
completed, to prevent surprises in the future.

Fixes: f3a76e9799fd ("kvstoremesh: leader election with etcd lock")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

clustermesh-apiserver/kvstoremesh/cells.go
@@ -37,6 +37,7 @@ var Cell = cell.Module(
 		}),
 		heartbeat.Cell,
 
+		cell.Provide(kvstoremesh.NewSyncWaiter),
 		cell.Invoke(func(*kvstoremesh.KVStoreMesh) {}),
 	),
 

clustermesh-apiserver/kvstoremesh/root.go
@@ -11,11 +11,9 @@ import (
 	"time"
 
 	"github.com/cilium/hive/cell"
-	"github.com/cilium/hive/job"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/spf13/cobra"
 
-	"github.com/cilium/cilium/clustermesh-apiserver/syncstate"
 	"github.com/cilium/cilium/pkg/clustermesh/kvstoremesh"
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/kvstore"
@@ -70,10 +68,8 @@ type params struct {
 	Metrics     kvstoremesh.Metrics
 	Shutdowner  hive.Shutdowner
 	Log         *slog.Logger
-	SyncState   syncstate.SyncState
-	JobGroup    job.Group
+	SyncWaiter  kvstoremesh.SyncWaiter
 	KVStoreMesh *kvstoremesh.KVStoreMesh
-	Health      cell.Health
 }
 
 func registerLeaderElectionHooks(lc cell.Lifecycle, llc *LeaderLifecycle, params params) {
@@ -120,7 +116,7 @@ func runLeaderElection(ctx context.Context, lc *LeaderLifecycle, params params) 
 
 	if err != nil && errors.Is(err, kvstore.ErrEtcdTimeout) {
 		// signal readiness
-		params.SyncState.Stop()
+		params.SyncWaiter.ForceReady()
 
 		// try again with infinite timeout
 		params.Log.Info("Reattempting to acquire leader election lock")
@@ -147,15 +143,6 @@ func runLeaderElection(ctx context.Context, lc *LeaderLifecycle, params params) 
 
 	params.Log.Info("Leader election lock acquired")
 	params.Metrics.LeaderElectionStatus.With(prometheus.Labels{metrics.LabelLeaderElectionName: "kvstoremesh"}).Set(float64(1))
-
-	kvstoremesh.RegisterSyncWaiter(kvstoremesh.SyncWaiterParams{
-		KVStoreMesh: params.KVStoreMesh,
-		SyncState:   params.SyncState,
-		Lifecycle:   lc,
-		JobGroup:    params.JobGroup,
-		Health:      params.Health,
-	})
-	params.SyncState.Stop()
 
 	err = lc.Start(params.Log, ctx)
 	if err != nil {

pkg/clustermesh/kvstoremesh/kvstoremesh.go
@@ -68,6 +68,8 @@ type KVStoreMesh struct {
 
 	// clock allows to override the clock for testing purposes
 	clock clock.Clock
+
+	started chan struct{}
 }
 
 type params struct {
@@ -97,6 +99,7 @@ func newKVStoreMesh(lc cell.Lifecycle, params params) *KVStoreMesh {
 		storeFactory: params.StoreFactory,
 		logger:       params.Logger,
 		clock:        clock.RealClock{},
+		started:      make(chan struct{}),
 	}
 	km.common = common.NewClusterMesh(common.Configuration{
 		Logger:              params.Logger,
@@ -109,26 +112,38 @@ func newKVStoreMesh(lc cell.Lifecycle, params params) *KVStoreMesh {
 
 	lc.Append(km.common)
 
+	// Needs to run after the "common" start hook, to signal that initialization
+	// successfully completed.
+	lc.Append(cell.Hook{
+		OnStart: func(hc cell.HookContext) error {
+			close(km.started)
+			return nil
+		},
+	})
+
 	return &km
 }
 
-type SyncWaiterParams struct {
-	KVStoreMesh *KVStoreMesh
-	SyncState   syncstate.SyncState
-	Lifecycle   cell.Lifecycle
-	JobGroup    job.Group
-	Health      cell.Health
-}
+// SyncWaiter wraps a SyncState to wait for KVStoreMesh synchronization, while
+// allowing to force marking it as ready when necessary.
+type SyncWaiter func()
 
-func RegisterSyncWaiter(p SyncWaiterParams) {
-	syncedCallback := p.SyncState.WaitForResource()
+func NewSyncWaiter(jg job.Group, km *KVStoreMesh, ss syncstate.SyncState) SyncWaiter {
+	done := ss.WaitForResource()
 
-	p.JobGroup.Add(
+	jg.Add(
 		job.OneShot("kvstoremesh-sync-waiter", func(ctx context.Context, health cell.Health) error {
-			return p.KVStoreMesh.synced(ctx, syncedCallback)
+			return km.synced(ctx, func(ctx context.Context) {
+				done(ctx)
+				ss.Stop()
+			})
 		}),
 	)
+
+	return func() { ss.Stop(); done(context.Background()) }
 }
+
+func (sw SyncWaiter) ForceReady() { sw() }
 
 func (km *KVStoreMesh) newRemoteCluster(name string, status common.StatusFunc) common.RemoteCluster {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -182,6 +197,12 @@ func (km *KVStoreMesh) newRemoteCluster(name string, status common.StatusFunc) c
 // timeout has been reached. The given syncCallback is always executed before
 // the function returns.
 func (km *KVStoreMesh) synced(ctx context.Context, syncCallback func(context.Context)) error {
+	select {
+	case <-km.started:
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+
 	ctx, cancel := context.WithTimeout(ctx, km.config.GlobalReadyTimeout)
 	defer func() {
 		syncCallback(ctx)

pkg/clustermesh/kvstoremesh/kvstoremesh_test.go
@@ -748,10 +748,12 @@ func TestRemoteClusterSync(t *testing.T) {
 				clusters: make(map[string]*remoteCluster),
 			}
 			km := KVStoreMesh{
-				config: tt.config,
-				common: mockClusterMesh,
-				logger: hivetest.Logger(t),
+				config:  tt.config,
+				common:  mockClusterMesh,
+				logger:  hivetest.Logger(t),
+				started: make(chan struct{}),
 			}
+			close(km.started)
 
 			rc := &remoteCluster{
 				name:         "foo",