a1a78d986cd2d7fe742db1b156e8aa8e3f8ecb7a
Martynas Pumputis <martynas@isovalent.com>
2025-09-19 15:27:00 +0200

    Revert "cilium-cli: Remove KPR subflags"
    This reverts commit 21572bb18a8ec9a6842b66c4c23748be99c8a3f0.

--- cilium-cli/connectivity/builder/echo_ingress_l7.go
+++ cilium-cli/connectivity/builder/echo_ingress_l7.go
@@ -66,7 +66,7 @@ func (t echoIngressL7) build(ct *check.ConnectivityTest, _ map[string]string) {
 				}
 				ok, _ = ct.Features.MatchRequirements(
 					features.RequireEnabled(features.Tunnel),
-					features.RequireEnabled(features.KPR),
+					features.RequireEnabled(features.KPRMode),
 				)
 				return !ok
 			}
