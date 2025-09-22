cilium/cilium

commit 21572bb18a8ec9a6842b66c4c23748be99c8a3f0

cilium-cli: Remove KPR subflags
Signed-off-by: Martynas Pumputis <martynas@isovalent.com>

cilium-cli/connectivity/check/features.go
@@ -166,31 +166,14 @@ func (ct *ConnectivityTest) extractFeaturesFromCiliumStatus(ctx context.Context,
 	if kpr := st.KubeProxyReplacement; kpr != nil {
 		mode = strings.ToLower(kpr.Mode)
 		if f := kpr.Features; f != nil {
-			if f.ExternalIPs != nil {
-				result[features.KPRExternalIPs] = features.Status{Enabled: f.ExternalIPs.Enabled}
-			}
-			if f.HostPort != nil {
-				result[features.KPRHostPort] = features.Status{Enabled: f.HostPort.Enabled}
-			}
-			if f.NodePort != nil {
-				result[features.KPRNodePort] = features.Status{Enabled: f.NodePort.Enabled}
-				acceleration := strings.ToLower(f.NodePort.Acceleration)
-				result[features.KPRNodePortAcceleration] = features.Status{
-					Enabled: mode != "false" && acceleration != "disabled",
-					Mode:    mode,
-				}
-			}
-			if f.SessionAffinity != nil {
-				result[features.KPRSessionAffinity] = features.Status{Enabled: f.SessionAffinity.Enabled}
-			}
 			if f.SocketLB != nil {
 				result[features.KPRSocketLB] = features.Status{Enabled: f.SocketLB.Enabled}
 				result[features.KPRSocketLBHostnsOnly] = features.Status{Enabled: f.BpfSocketLBHostnsOnly}
 			}
 		}
 	}
-	result[features.KPRMode] = features.Status{
-		Enabled: mode != "false" && mode != "disabled",
+	result[features.KPR] = features.Status{
+		Enabled: mode == "true" || mode == "strict",
 		Mode:    mode,
 	}
 

cilium-cli/utils/features/features.go
@@ -28,14 +28,10 @@ const (
 	TunnelPort         Feature = "tunnel-port"
 	EndpointRoutes     Feature = "endpoint-routes"
 
-	KPRMode                 Feature = "kpr-mode"
-	KPRExternalIPs          Feature = "kpr-external-ips"
-	KPRHostPort             Feature = "kpr-hostport"
+	KPR                     Feature = "kpr"
 	KPRSocketLB             Feature = "kpr-socket-lb"
 	KPRSocketLBHostnsOnly   Feature = "kpr-socket-lb-hostns-only"
-	KPRNodePort             Feature = "kpr-nodeport"
 	KPRNodePortAcceleration Feature = "kpr-nodeport-acceleration"
-	KPRSessionAffinity      Feature = "kpr-session-affinity"
 
 	BPFLBExternalClusterIP Feature = "bpf-lb-external-clusterip"
 
@@ -197,7 +193,7 @@ func (fs Set) DeriveFeatures() error {
 		Enabled: (fs[CNIChaining].Enabled && fs[CNIChaining].Mode == "portmap" &&
 			// cilium/cilium#12541: Host firewall doesn't work with portmap CNI chaining
 			!fs[HostFirewall].Enabled) ||
-			fs[KPRHostPort].Enabled,
+			fs[KPR].Enabled,
 	}
 
 	return nil

cilium-cli/connectivity/builder/builder.go
@@ -422,7 +422,7 @@ func withKPRReqForMultiCluster(ct *check.ConnectivityTest, reqs ...features.Requ
 	// Skip the nodeport-related tests in the multicluster scenario if KPR is not
 	// enabled, since global nodeport services are not supported in that case.
 	if ct.Params().MultiCluster != "" {
-		reqs = append(reqs, features.RequireEnabled(features.KPRNodePort))
+		reqs = append(reqs, features.RequireEnabled(features.KPR))
 	}
 	return reqs
 }

cilium-cli/connectivity/builder/echo_ingress_l7.go
@@ -66,7 +66,7 @@ func (t echoIngressL7) build(ct *check.ConnectivityTest, _ map[string]string) {
 				}
 				ok, _ = ct.Features.MatchRequirements(
 					features.RequireEnabled(features.Tunnel),
-					features.RequireEnabled(features.KPRMode),
+					features.RequireEnabled(features.KPR),
 				)
 				return !ok
 			}

cilium-cli/connectivity/check/context.go
@@ -1337,7 +1337,7 @@ func (ct *ConnectivityTest) ForEachIPFamily(hasNetworkPolicies bool, do func(fea
 func (ct *ConnectivityTest) ShouldRunConnDisruptNSTraffic() bool {
 	return ct.params.IncludeConnDisruptTestNSTraffic &&
 		ct.Features[features.NodeWithoutCilium].Enabled &&
-		(ct.Params().MultiCluster == "" || ct.Features[features.KPRNodePort].Enabled) &&
+		(ct.Params().MultiCluster == "" || ct.Features[features.KPR].Enabled) &&
 		!ct.Features[features.KPRNodePortAcceleration].Enabled
 }
 

cilium-cli/connectivity/tests/service.go
@@ -289,7 +289,7 @@ func (s *outsideToNodePort) Run(ctx context.Context, t *check.Test) {
 	// With kube-proxy doing N/S LB it is not possible to see the original client
 	// IP, as iptables rules do the LB SNAT/DNAT before the packet hits any
 	// of Cilium's datapath BPF progs. So, skip the flow validation in that case.
-	status, ok := t.Context().Feature(features.KPRNodePort)
+	status, ok := t.Context().Feature(features.KPR)
 	validateFlows := ok && status.Enabled
 
 	for _, svc := range t.Context().EchoServices() {

commit 53737d8b1fc859bf873262681884b42b45d5744f

cilium-cli: Bring back NodePort Acceleration feature detection
This made to run the EGW/NS connectivity disruption tests which most
likely resulted in the "FIB lookup failed" reported in [1].

Also, while here, change the check for [features.KPR] to check only for
"true". The "strict" / "disabled" are no longer available since Cilium
v1.14.

[1]: https://github.com/cilium/cilium/issues/41520

Fixes: 21572bb18a ("cilium-cli: Remove KPR subflags")
Signed-off-by: Martynas Pumputis <martynas@isovalent.com>

cilium-cli/connectivity/check/features.go
@@ -172,10 +172,15 @@ func (ct *ConnectivityTest) extractFeaturesFromCiliumStatus(ctx context.Context,
 				result[features.KPRSocketLB] = features.Status{Enabled: f.SocketLB.Enabled}
 				result[features.KPRSocketLBHostnsOnly] = features.Status{Enabled: f.BpfSocketLBHostnsOnly}
 			}
+			acceleration := strings.ToLower(f.NodePort.Acceleration)
+			result[features.KPRNodePortAcceleration] = features.Status{
+				Enabled: mode == "true" && acceleration != "disabled",
+				Mode:    acceleration,
+			}
 		}
 	}
 	result[features.KPR] = features.Status{
-		Enabled: mode == "true" || mode == "strict",
+		Enabled: mode == "true",
 		Mode:    mode,
 	}
 