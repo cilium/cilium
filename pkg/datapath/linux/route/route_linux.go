cilium/cilium

commit 35ca979f447139eedb915d94a3d5bb7ba73fd511

datapath/linux/route: Fix Delete
Since the deletion of routes with the Nexthop or the Local field set
fails (see the current comment in the code), the Delete function cannot
call the getNetlinkRoute helper.  But since it is not using that helper,
Delete lacks the copy of several important fields in the route to be
deleted, like the source, the priority, the protocol and so on. This can
lead to errors while deleting stale route inserted with the Upsert
function. As an example, let's consider the deletion of the routes added
in the IPSec specific routing table in multi-pool IPAM mode (to be added
soon in a subsequent PR). In that case, with the previous version of the
Delete function, the following error is returned:

`msg="Unable to delete the IPsec route IN from the host routing table" error="no such process"`

and the stale route is not deleted.

The commit fixes the Delete implementation to copy additional fields
from the route.Route internal representation.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/datapath/linux/route/route_linux.go
@@ -302,13 +302,23 @@ func Delete(route Route) error {
 	// Therefore do not use getNetlinkRoute().
 	routeSpec := netlink.Route{
 		Dst:       &route.Prefix,
+		Src:       route.Local,
 		LinkIndex: link.Attrs().Index,
+		Priority:  route.Priority,
 		Table:     route.Table,
+		Type:      route.Type,
+		Protocol:  netlink.RouteProtocol(route.Proto),
+		MTU:       route.MTU,
 	}
 
 	// Scope can only be specified for IPv4
 	if route.Prefix.IP.To4() != nil {
 		routeSpec.Scope = route.Scope
+		if route.Scope == netlink.SCOPE_UNIVERSE && route.Type == RTN_LOCAL {
+			routeSpec.Scope = netlink.SCOPE_HOST
+		} else {
+			routeSpec.Scope = route.Scope
+		}
 	}
 
 	if err := netlink.RouteDel(&routeSpec); err != nil {

commit 93383f7f96dfffef890ba6ac7b00f2e85fc5f476

datapath/linux/route: Disregard MTU value when deleting routes
When Cilium starts in tunnel mode, a route for each remote node pod
CIDRs is added to the current node. For these routes, the MTU is set to
1450, to include the tunnel overhead.

If the routing mode is then changed to native and Cilium is restarted,
the stale routes should be deleted at startup. Unfortunately, this is
currently not happening because the MTU is set to 1500 in the deletion
request, resulting in the folowing error from netlink:

msg="Unable to delete route" ... error="no such process"

In other words, the route cannot be found because the MTU value set in
the deletion request is not matching the one of the installed route.

To solve this, just disregard the MTU value while deleting a route. This
should still allow to correctly remove stale IPsec related routes as
intended in commit 35ca979f44

Fixes: 35ca979f44 ("datapath/linux/route: Fix Delete")
Fixes: https://github.com/cilium/cilium/issues/41811

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/datapath/linux/route/route_linux.go
@@ -308,7 +308,6 @@ func Delete(route Route) error {
 		Table:     route.Table,
 		Type:      route.Type,
 		Protocol:  netlink.RouteProtocol(route.Proto),
-		MTU:       route.MTU,
 	}
 
 	// Scope can only be specified for IPv4