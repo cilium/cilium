2ac09bdc9225de731fda889d4c472929dab278af
Julian Wiedmann <jwi@isovalent.com>
2025-10-01 09:59:53 +0300

    bpf: hostfw: share code for CT entry creation in egress policy path
    The egress policy path needs to create CT entries in two cases:
    1. for genuine host-originating connections,
    2. for pod-originating connections that use a HOST_ID-type source IP
       (because they were iptables-masqueraded, or have been replaced by a
       non-transparent proxy connection). Having a CT entry prevents us from
       applying ingress network policy for such a connection.
    
    Share the code to create a CT entry amongst both cases.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/host_firewall.h
+++ bpf/lib/host_firewall.h
@@ -56,13 +56,11 @@ __ipv6_host_policy_egress(struct __ctx_buff *ctx, bool is_host_id __maybe_unused
 			  struct trace_ctx *trace, __s8 *ext_err)
 {
 	struct ipv6_ct_tuple *tuple = &ct_buffer->tuple;
-	__u32 tunnel_endpoint = 0;
 	int ret = ct_buffer->ret;
-	int verdict;
+	int verdict = CTX_ACT_OK;
 	__u8 policy_match_type = POLICY_MATCH_NONE;
 	__u8 audited = 0;
 	__u8 auth_type = 0;
-	struct remote_endpoint_info *info;
 	__u32 dst_sec_identity = 0;
 	__u16 proxy_port = 0;
 
@@ -73,40 +71,35 @@ __ipv6_host_policy_egress(struct __ctx_buff *ctx, bool is_host_id __maybe_unused
 	if (ret == CT_REPLY || ret == CT_RELATED)
 		return CTX_ACT_OK;
 
-	if (!is_host_id) {
-		if (ret == CT_NEW) {
-			ret = ct_create6(get_ct_map6(tuple), &cilium_ct_any6_global,
-					 tuple, ctx, CT_EGRESS, NULL, ext_err);
-			if (unlikely(ret < 0))
-				return ret;
+	if (is_host_id) {
+		struct remote_endpoint_info *info;
+		__u32 tunnel_endpoint = 0;
+
+		/* Retrieve destination identity. */
+		info = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr, 0);
+		if (info) {
+			dst_sec_identity = info->sec_identity;
+			tunnel_endpoint = info->tunnel_endpoint.ip4;
 		}
+		cilium_dbg(ctx, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
+			   ip6->daddr.s6_addr32[3], dst_sec_identity);
 
-		return CTX_ACT_OK;
-	}
-
-	/* Retrieve destination identity. */
-	info = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr, 0);
-	if (info) {
-		dst_sec_identity = info->sec_identity;
-		tunnel_endpoint = info->tunnel_endpoint.ip4;
-	}
-	cilium_dbg(ctx, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
-		   ip6->daddr.s6_addr32[3], dst_sec_identity);
-
-	/* Perform policy lookup. */
-	verdict = policy_can_egress6(ctx, tuple, ct_buffer->l4_off, HOST_ID,
-				     dst_sec_identity, &policy_match_type, &audited, ext_err,
-				     &proxy_port);
-	if (verdict == DROP_POLICY_AUTH_REQUIRED) {
-		auth_type = (__u8)*ext_err;
-		verdict = auth_lookup(ctx, HOST_ID, dst_sec_identity, tunnel_endpoint, auth_type);
+		/* Perform policy lookup. */
+		verdict = policy_can_egress6(ctx, tuple, ct_buffer->l4_off, HOST_ID,
+					     dst_sec_identity, &policy_match_type,
+					     &audited, ext_err, &proxy_port);
+		if (verdict == DROP_POLICY_AUTH_REQUIRED) {
+			auth_type = (__u8)*ext_err;
+			verdict = auth_lookup(ctx, HOST_ID, dst_sec_identity,
+					      tunnel_endpoint, auth_type);
+		}
 	}
 
 	/* Only create CT entry for accepted connections */
 	if (ret == CT_NEW && verdict == CTX_ACT_OK) {
 		struct ct_state ct_state_new = {};
 
-		ct_state_new.src_sec_id = HOST_ID;
+		ct_state_new.src_sec_id = is_host_id ? HOST_ID : 0;
 		ct_state_new.proxy_redirect = proxy_port > 0;
 
 		/* ext_err may contain a value from __policy_can_access, and
@@ -121,19 +114,21 @@ __ipv6_host_policy_egress(struct __ctx_buff *ctx, bool is_host_id __maybe_unused
 			return ret;
 	}
 
-	/* Emit verdict if drop or if allow for CT_NEW. */
-	if (verdict != CTX_ACT_OK || ret != CT_ESTABLISHED)
-		send_policy_verdict_notify(ctx, dst_sec_identity, tuple->dport,
-					   tuple->nexthdr, POLICY_EGRESS, 1,
-					   verdict, proxy_port, policy_match_type, audited,
-					   auth_type);
+	if (is_host_id) {
+		/* Emit verdict if drop or if allow for CT_NEW. */
+		if (verdict != CTX_ACT_OK || ret != CT_ESTABLISHED)
+			send_policy_verdict_notify(ctx, dst_sec_identity, tuple->dport,
+						   tuple->nexthdr, POLICY_EGRESS, 1,
+						   verdict, proxy_port, policy_match_type, audited,
+						   auth_type);
 
-	if (proxy_port > 0 && (ret == CT_NEW || ret == CT_ESTABLISHED)) {
-		/* Trace the packet before it is forwarded to proxy */
-		send_trace_notify(ctx, TRACE_TO_PROXY, SECLABEL_IPV6, UNKNOWN_ID,
-				  bpf_ntohs(proxy_port), TRACE_IFINDEX_UNKNOWN,
-				  trace->reason, trace->monitor, bpf_htons(ETH_P_IPV6));
-		return ctx_redirect_to_proxy_host_egress(ctx, proxy_port);
+		if (proxy_port > 0 && (ret == CT_NEW || ret == CT_ESTABLISHED)) {
+			/* Trace the packet before it is forwarded to proxy */
+			send_trace_notify(ctx, TRACE_TO_PROXY, SECLABEL_IPV6, UNKNOWN_ID,
+					  bpf_ntohs(proxy_port), TRACE_IFINDEX_UNKNOWN,
+					  trace->reason, trace->monitor, bpf_htons(ETH_P_IPV6));
+			return ctx_redirect_to_proxy_host_egress(ctx, proxy_port);
+		}
 	}
 
 	return verdict;
@@ -330,13 +325,11 @@ __ipv4_host_policy_egress(struct __ctx_buff *ctx, bool is_host_id __maybe_unused
 			  struct trace_ctx *trace, __s8 *ext_err)
 {
 	struct ipv4_ct_tuple *tuple = &ct_buffer->tuple;
-	__u32 tunnel_endpoint = 0;
 	int ret = ct_buffer->ret;
-	int verdict;
+	int verdict = CTX_ACT_OK;
 	__u8 policy_match_type = POLICY_MATCH_NONE;
 	__u8 audited = 0;
 	__u8 auth_type = 0;
-	struct remote_endpoint_info *info;
 	__u32 dst_sec_identity = 0;
 	__u16 proxy_port = 0;
 
@@ -351,50 +344,47 @@ __ipv4_host_policy_egress(struct __ctx_buff *ctx, bool is_host_id __maybe_unused
 	 * (eg. non-transparent proxy connection, or when using iptables masquerading).
 	 * The response packet will therefore have a host IP as the destination IP.
 	 *
-	 * To avoid enforcing host policies for response packets to pods, we
+	 * We don't want to apply egress policy for such packets. But
+	 * to avoid enforcing host policies for response packets to pods, we
 	 * need to create a CT entry for the forward, SNATed packet from the
 	 * pod. Response packets will thus match this CT entry and bypass host
 	 * policies.
 	 * We know the packet is a SNATed packet if the srcid from ipcache is
 	 * HOST_ID, but the actual srcid (derived from the packet mark) isn't.
 	 */
-	if (!is_host_id) {
-		if (ret == CT_NEW) {
-			ret = ct_create4(get_ct_map4(tuple), &cilium_ct_any4_global,
-					 tuple, ctx, CT_EGRESS, NULL, ext_err);
-			if (unlikely(ret < 0))
-				return ret;
+
+	if (is_host_id) {
+		struct remote_endpoint_info *info;
+		__u32 tunnel_endpoint = 0;
+
+		/* Retrieve destination identity. */
+		info = lookup_ip4_remote_endpoint(ip4->daddr, 0);
+		if (info) {
+			dst_sec_identity = info->sec_identity;
+			tunnel_endpoint = info->tunnel_endpoint.ip4;
 		}
+		cilium_dbg(ctx, info ? DBG_IP_ID_MAP_SUCCEED4 : DBG_IP_ID_MAP_FAILED4,
+			   ip4->daddr, dst_sec_identity);
 
-		return CTX_ACT_OK;
-	}
-
-	/* Retrieve destination identity. */
-	info = lookup_ip4_remote_endpoint(ip4->daddr, 0);
-	if (info) {
-		dst_sec_identity = info->sec_identity;
-		tunnel_endpoint = info->tunnel_endpoint.ip4;
-	}
-	cilium_dbg(ctx, info ? DBG_IP_ID_MAP_SUCCEED4 : DBG_IP_ID_MAP_FAILED4,
-		   ip4->daddr, dst_sec_identity);
-
-	/* Perform policy lookup. */
-	verdict = policy_can_egress4(ctx, tuple, ct_buffer->l4_off, HOST_ID,
-				     dst_sec_identity, &policy_match_type,
-				     &audited, ext_err, &proxy_port);
-	if (verdict == DROP_POLICY_AUTH_REQUIRED) {
-		auth_type = (__u8)*ext_err;
-		verdict = auth_lookup(ctx, HOST_ID, dst_sec_identity, tunnel_endpoint, auth_type);
+		/* Perform policy lookup. */
+		verdict = policy_can_egress4(ctx, tuple, ct_buffer->l4_off, HOST_ID,
+					     dst_sec_identity, &policy_match_type,
+					     &audited, ext_err, &proxy_port);
+		if (verdict == DROP_POLICY_AUTH_REQUIRED) {
+			auth_type = (__u8)*ext_err;
+			verdict = auth_lookup(ctx, HOST_ID, dst_sec_identity,
+					      tunnel_endpoint, auth_type);
+		}
 	}
 
 	/* Only create CT entry for accepted connections */
 	if (ret == CT_NEW && verdict == CTX_ACT_OK) {
 		struct ct_state ct_state_new = {};
 
-		ct_state_new.src_sec_id = HOST_ID;
+		ct_state_new.src_sec_id = is_host_id ? HOST_ID : 0;
 		ct_state_new.proxy_redirect = proxy_port > 0;
 
-		/* ext_err may contain a value from __eolicy_can_access, and
+		/* ext_err may contain a value from __policy_can_access, and
 		 * ct_create4 overwrites it only if it returns an error itself.
 		 * As the error from __policy_can_access is dropped in that
 		 * case, it's OK to return ext_err from ct_create4 along with
@@ -406,19 +396,21 @@ __ipv4_host_policy_egress(struct __ctx_buff *ctx, bool is_host_id __maybe_unused
 			return ret;
 	}
 
-	/* Emit verdict if drop or if allow for CT_NEW. */
-	if (verdict != CTX_ACT_OK || ret != CT_ESTABLISHED)
-		send_policy_verdict_notify(ctx, dst_sec_identity, tuple->dport,
-					   tuple->nexthdr, POLICY_EGRESS, 0,
-					   verdict, proxy_port, policy_match_type, audited,
-					   auth_type);
+	if (is_host_id) {
+		/* Emit verdict if drop or if allow for CT_NEW. */
+		if (verdict != CTX_ACT_OK || ret != CT_ESTABLISHED)
+			send_policy_verdict_notify(ctx, dst_sec_identity, tuple->dport,
+						   tuple->nexthdr, POLICY_EGRESS, 0,
+						   verdict, proxy_port, policy_match_type, audited,
+						   auth_type);
 
-	if (proxy_port > 0 && (ret == CT_NEW || ret == CT_ESTABLISHED)) {
-		/* Trace the packet before it is forwarded to proxy */
-		send_trace_notify(ctx, TRACE_TO_PROXY, SECLABEL_IPV4, UNKNOWN_ID,
-				  bpf_ntohs(proxy_port), TRACE_IFINDEX_UNKNOWN,
-				  trace->reason, trace->monitor, bpf_htons(ETH_P_IP));
-		return ctx_redirect_to_proxy_host_egress(ctx, proxy_port);
+		if (proxy_port > 0 && (ret == CT_NEW || ret == CT_ESTABLISHED)) {
+			/* Trace the packet before it is forwarded to proxy */
+			send_trace_notify(ctx, TRACE_TO_PROXY, SECLABEL_IPV4, UNKNOWN_ID,
+					  bpf_ntohs(proxy_port), TRACE_IFINDEX_UNKNOWN,
+					  trace->reason, trace->monitor, bpf_htons(ETH_P_IP));
+			return ctx_redirect_to_proxy_host_egress(ctx, proxy_port);
+		}
 	}
 
 	return verdict;
