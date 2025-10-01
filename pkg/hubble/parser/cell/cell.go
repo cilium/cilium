cilium/cilium

commit 676d346a1eadb481a3d3dac4019868613fbe1347

loadbalancer: Use unique for L3n4Addr
The L3n4Addr is used in a lot of places and is compared and hashed a lot.
Reduce memory usage and speed up comparisons by making L3n4Addr a unique.Handle.

pkg/loadbalancer/benchmark before:
Min: Allocated 563043kB in total, 1837855 objects / 118158kB still reachable (per service:  36 objs, 11531B alloc,  2419B in-use)
Avg: Allocated 583710kB in total, 2174642 objects / 154810kB still reachable (per service:  43 objs, 11954B alloc,  3170B in-use)
Max: Allocated 643000kB in total, 3089878 objects / 263505kB still reachable (per service:  61 objs, 13168B alloc,  5396B in-use)

After:
Min: Allocated 510398kB in total, 1734286 objects /  83716kB still reachable (per service:  34 objs, 10452B alloc,  1714B in-use)
Avg: Allocated 523617kB in total, 2188965 objects / 125627kB still reachable (per service:  43 objs, 10723B alloc,  2572B in-use)
Max: Allocated 564751kB in total, 3478097 objects / 229608kB still reachable (per service:  69 objs, 11566B alloc,  4702B in-use)

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hubble/parser/cell/cell.go
@@ -162,14 +162,12 @@ func (h *payloadGetters) GetServiceByAddr(ip netip.Addr, port uint16) *flowpb.Se
 		return nil
 	}
 	addrCluster := cmtypes.AddrClusterFrom(ip, 0)
-	addr := loadbalancer.L3n4Addr{
-		AddrCluster: addrCluster,
-		L4Addr: loadbalancer.L4Addr{
-			Port: port,
-		},
-	}
-
+	addr := loadbalancer.NewL3n4Addr(loadbalancer.TCP, addrCluster, port, loadbalancer.ScopeExternal)
 	fe, _, found := h.frontends.Get(h.db.ReadTxn(), loadbalancer.FrontendByAddress(addr))
+	if !found {
+		addr := loadbalancer.NewL3n4Addr(loadbalancer.UDP, addrCluster, port, loadbalancer.ScopeExternal)
+		fe, _, found = h.frontends.Get(h.db.ReadTxn(), loadbalancer.FrontendByAddress(addr))
+	}
 	if !found {
 		return nil
 	}

pkg/ciliumenvoyconfig/controller.go
@@ -493,7 +493,7 @@ func computeLoadAssignments(
 			// The below is to make sure that UDP and SCTP are not allowed instead of comparing with lb.TCP
 			// The reason is to avoid extra dependencies with ongoing work to differentiate protocols in datapath,
 			// which might add more values such as lb.Any, lb.None, etc.
-			if be.Address.Protocol == loadbalancer.UDP || be.Address.Protocol == loadbalancer.SCTP {
+			if be.Address.Protocol() == loadbalancer.UDP || be.Address.Protocol() == loadbalancer.SCTP {
 				continue
 			}
 
@@ -503,9 +503,9 @@ func computeLoadAssignments(
 						Address: &envoy_config_core.Address{
 							Address: &envoy_config_core.Address_SocketAddress{
 								SocketAddress: &envoy_config_core.SocketAddress{
-									Address: be.Address.AddrCluster.String(),
+									Address: be.Address.AddrCluster().String(),
 									PortSpecifier: &envoy_config_core.SocketAddress_PortValue{
-										PortValue: uint32(be.Address.Port),
+										PortValue: uint32(be.Address.Port()),
 									},
 								},
 							},

pkg/clustermesh/service_merger.go
@@ -98,10 +98,12 @@ func ClusterServiceToBackendParams(service *serviceStore.ClusterService) (beps [
 				portNames = []string{name}
 			}
 			bep := loadbalancer.BackendParams{
-				Address: loadbalancer.L3n4Addr{
-					AddrCluster: addrCluster,
-					L4Addr:      *l4,
-				},
+				Address: loadbalancer.NewL3n4Addr(
+					l4.Protocol,
+					addrCluster,
+					l4.Port,
+					loadbalancer.ScopeExternal,
+				),
 				PortNames: portNames,
 				Weight:    loadbalancer.DefaultBackendWeight,
 				NodeName:  "",

pkg/clustermesh/types/addressing.go
@@ -187,7 +187,7 @@ func (ac AddrCluster) ClusterID() uint32 {
 
 // Equal returns true when given AddrCluster has a same IP address and ClusterID
 func (ac0 AddrCluster) Equal(ac1 AddrCluster) bool {
-	return ac0.addr == ac1.addr && ac0.clusterID == ac1.clusterID
+	return ac0 == ac1
 }
 
 // Less compares ac0 and ac1 and returns true if ac0 is lesser than ac1

pkg/datapath/linux/backend_neighbors.go
@@ -82,7 +82,7 @@ func syncBackendNeighbors(p backendNeighborSyncParams, ctx context.Context, init
 
 			if change.Deleted {
 				err := p.ForwardableIPManager.Delete(
-					change.Object.Address.AddrCluster.Addr(),
+					change.Object.Address.Addr(),
 					owner,
 				)
 				if err != nil {
@@ -90,7 +90,7 @@ func syncBackendNeighbors(p backendNeighborSyncParams, ctx context.Context, init
 				}
 			} else {
 				err := p.ForwardableIPManager.Insert(
-					change.Object.Address.AddrCluster.Addr(),
+					change.Object.Address.Addr(),
 					owner,
 				)
 				if err != nil {

pkg/datapath/linux/backend_neighbors_test.go
@@ -65,7 +65,7 @@ func TestBackendNeighborSync(t *testing.T) {
 		return func() bool {
 			rx := db.ReadTxn()
 			for fip := range forwardableIPs.All(rx) {
-				if fip.IP == addr.AddrCluster.Addr() {
+				if fip.IP == addr.Addr() {
 					return !invert
 				}
 			}

pkg/loadbalancer/benchmark/benchmark.go
@@ -442,8 +442,8 @@ func checkTables(db *statedb.DB, writer *writer.Writer, svcs []*slim_corev1.Serv
 					err = errors.Join(err, fmt.Errorf("Incorrect name for frontend #%06d, got %q, want %q", i, fe.ServiceName.Name(), want.Name))
 				}
 				wantIP, _ := netip.ParseAddr(want.Spec.ClusterIP)
-				if fe.Address.AddrCluster.Addr() != wantIP {
-					err = errors.Join(err, fmt.Errorf("Incorrect address for frontend #%06d, got %v, want %v", i, fe.Address.AddrCluster.Addr(), wantIP))
+				if fe.Address.Addr() != wantIP {
+					err = errors.Join(err, fmt.Errorf("Incorrect address for frontend #%06d, got %v, want %v", i, fe.Address.Addr(), wantIP))
 				}
 				if fe.Type != loadbalancer.SVCType(want.Spec.Type) {
 					err = errors.Join(err, fmt.Errorf("Incorrect service type for frontend #%06d, got %v, want %v", i, fe.Type, loadbalancer.SVCType(want.Spec.Type)))
@@ -456,8 +456,8 @@ func checkTables(db *statedb.DB, writer *writer.Writer, svcs []*slim_corev1.Serv
 				}
 				backends := slices.Collect(statedb.ToSeq(iter.Seq2[loadbalancer.BackendParams, statedb.Revision](fe.Backends)))
 				for wantAddr := range epSlices[i].Backends { // There is only one element in this map.
-					if backends[0].Address.AddrCluster != wantAddr {
-						err = errors.Join(err, fmt.Errorf("Incorrect backend address for frontend #%06d, got %v, want %v", i, backends[0].Address.AddrCluster, wantAddr))
+					if backends[0].Address.AddrCluster() != wantAddr {
+						err = errors.Join(err, fmt.Errorf("Incorrect backend address for frontend #%06d, got %v, want %v", i, backends[0].Address.AddrCluster(), wantAddr))
 					}
 				}
 
@@ -474,15 +474,15 @@ func checkTables(db *statedb.DB, writer *writer.Writer, svcs []*slim_corev1.Serv
 			for be := range writer.Backends().All(txn) {
 				want := epSlices[i]
 				for wantAddr, wantBe := range want.Backends { // There is only one element in this map.
-					if be.Address.AddrCluster != wantAddr {
-						err = errors.Join(err, fmt.Errorf("Incorrect address for backend #%06d, got %v, want %v", i, be.Address.AddrCluster, wantAddr))
+					if be.Address.AddrCluster() != wantAddr {
+						err = errors.Join(err, fmt.Errorf("Incorrect address for backend #%06d, got %v, want %v", i, be.Address.AddrCluster(), wantAddr))
 					}
 					for wantPort := range wantBe.Ports { // There is only one element in this map.
-						if be.Address.Port != wantPort.Port {
-							err = errors.Join(err, fmt.Errorf("Incorrect port for backend #%06d, got %v, want %v", i, be.Address.Port, wantPort.Port))
+						if be.Address.Port() != wantPort.Port {
+							err = errors.Join(err, fmt.Errorf("Incorrect port for backend #%06d, got %v, want %v", i, be.Address.Port(), wantPort.Port))
 						}
-						if be.Address.Protocol != wantPort.Protocol {
-							err = errors.Join(err, fmt.Errorf("Incorrect protocol for backend #%06d, got %v, want %v", i, be.Address.Protocol, wantPort.Protocol))
+						if be.Address.Protocol() != wantPort.Protocol {
+							err = errors.Join(err, fmt.Errorf("Incorrect protocol for backend #%06d, got %v, want %v", i, be.Address.Protocol(), wantPort.Protocol))
 						}
 					}
 				}

pkg/loadbalancer/frontend.go
@@ -156,18 +156,16 @@ func (fe *Frontend) ToModel() *models.Service {
 	}
 
 	backendModel := func(be BackendParams) *models.BackendAddress {
-		addrClusterStr := be.Address.AddrCluster.String()
-
+		addrClusterStr := be.Address.AddrCluster().String()
 		state := be.State
 		if be.Unhealthy {
 			state = BackendStateQuarantined
 		}
 		stateStr, _ := state.String()
-
 		return &models.BackendAddress{
 			IP:        &addrClusterStr,
-			Protocol:  be.Address.Protocol,
-			Port:      be.Address.Port,
+			Protocol:  be.Address.Protocol(),
+			Port:      be.Address.Port(),
 			NodeName:  be.NodeName,
 			Zone:      be.Zone,
 			State:     stateStr,

commit 281bc30e59d0415fa62012d9f4f5122dff5fb46a

hubble: Use lb.LookupFrontendByTuple to reduce allocations
The hubble parser does frontend lookups for every address it encounters
to fill in service info into the flow. The unique package in Go v1.24 does
not deal well with this access pattern and can cause memory usage to go
higher and higher if the rate of unique address is consistently high.

Fix this by using the [LookupFrontendByTuple] function introduced by
the previous commit that avoids making a unique.Handle for the address.

Benchmark results (GOMAXPROCS=4 benchtime=5s):

Go v1.24.0 no fix:
BenchmarkGetServiceByAddr-4···3206036····2062·ns/op·····1269·HeapSys+Released/MB·······503·B/op·····10·allocs/op⏎

Go v1.25.0 no fix:
BenchmarkGetServiceByAddr-4···2083958····3425·ns/op·····73.45·HeapSys+Released/MB······549·B/op·····16·allocs/op⏎

Go v1.24.0 with LookupFrontendByTuple:
BenchmarkGetServiceByAddr-4   18820899   315.4 ns/op    27.78 HeapSys+Released/MB      176 B/op     4 allocs/op

Go v1.25.0 with LookupFrontendByTuple:
BenchmarkGetServiceByAddr-4   18694239   317.0 ns/op    28.14 HeapSys+Released/MB      176 B/op     4 allocs/op

Fixes: #41623
Fixes: 676d346a1ead ("loadbalancer: Use unique for L3n4Addr")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hubble/parser/cell/cell.go
@@ -162,11 +162,10 @@ func (h *payloadGetters) GetServiceByAddr(ip netip.Addr, port uint16) *flowpb.Se
 		return nil
 	}
 	addrCluster := cmtypes.AddrClusterFrom(ip, 0)
-	addr := loadbalancer.NewL3n4Addr(loadbalancer.TCP, addrCluster, port, loadbalancer.ScopeExternal)
-	fe, _, found := h.frontends.Get(h.db.ReadTxn(), loadbalancer.FrontendByAddress(addr))
+	txn := h.db.ReadTxn()
+	fe, found := loadbalancer.LookupFrontendByTuple(txn, h.frontends, addrCluster, loadbalancer.TCP, port, loadbalancer.ScopeExternal)
 	if !found {
-		addr := loadbalancer.NewL3n4Addr(loadbalancer.UDP, addrCluster, port, loadbalancer.ScopeExternal)
-		fe, _, found = h.frontends.Get(h.db.ReadTxn(), loadbalancer.FrontendByAddress(addr))
+		fe, found = loadbalancer.LookupFrontendByTuple(txn, h.frontends, addrCluster, loadbalancer.UDP, port, loadbalancer.ScopeExternal)
 	}
 	if !found {
 		return nil