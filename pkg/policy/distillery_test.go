e0ba054446f9bd9308d9e9ec825917b7bb4c6988
Chris Tarazi <chris@isovalent.com>
2025-10-01 14:24:17 -0700

    policy: Return old policy from policy cache
    When swapping in the new policy object inside the policyCache, return
    the old policy. This is useful in subsequent commits when the endpoint
    will fetch the new policy and will need access to the old policy in
    order to call Detach().
    
    It's worth noting that this is also important for performing the detach
    operation at the same time as when the endpoint policy code was before
    introducing the policy computation cell that exists outside the endpoint
    package. In other words, this commit is in attempt to do the detach
    immediately after applying the new policy to the endpoint.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/policy/distillery_test.go
+++ pkg/policy/distillery_test.go
@@ -68,10 +68,10 @@ func TestCacheManagement(t *testing.T) {
 	require.True(t, removed)
 
 	// Insert identity twice. Should be the same policy.
-	policy1, updated, err := cache.updateSelectorPolicy(identity, ep1.Id)
+	policy1, _, updated, err := cache.updateSelectorPolicy(identity, ep1.Id)
 	require.NoError(t, err)
 	require.True(t, updated)
-	policy2, updated, err := cache.updateSelectorPolicy(identity, ep1.Id)
+	policy2, _, updated, err := cache.updateSelectorPolicy(identity, ep1.Id)
 	require.NoError(t, err)
 	require.False(t, updated)
 	// must be same pointer
@@ -90,13 +90,13 @@ func TestCacheManagement(t *testing.T) {
 	ep3.SetIdentity(1234, true)
 	identity3 := ep3.GetSecurityIdentity()
 	require.NotEqual(t, identity, identity3)
-	policy1, _, _ = cache.updateSelectorPolicy(identity, ep1.Id)
+	policy1, _, _, _ = cache.updateSelectorPolicy(identity, ep1.Id)
 	require.NotNil(t, policy1)
-	policy3, _, _ := cache.updateSelectorPolicy(identity3, ep3.Id)
+	policy3, _, _, _ := cache.updateSelectorPolicy(identity3, ep3.Id)
 	require.NotNil(t, policy3)
 	require.NotSame(t, policy3, policy1)
 	_ = cache.delete(identity)
-	_, updated, _ = cache.updateSelectorPolicy(identity3, ep3.Id)
+	_, _, updated, _ = cache.updateSelectorPolicy(identity3, ep3.Id)
 	require.False(t, updated)
 }
 
@@ -109,26 +109,26 @@ func TestCachePopulation(t *testing.T) {
 	require.Equal(t, identity1, ep2.GetSecurityIdentity())
 
 	// Calculate the policy and observe that it's cached
-	policy1, updated, err := cache.updateSelectorPolicy(identity1, ep1.Id)
+	policy1, _, updated, err := cache.updateSelectorPolicy(identity1, ep1.Id)
 	require.NoError(t, err)
 	require.True(t, updated)
-	_, updated, err = cache.updateSelectorPolicy(identity1, ep1.Id)
+	_, _, updated, err = cache.updateSelectorPolicy(identity1, ep1.Id)
 	require.NoError(t, err)
 	require.False(t, updated)
-	policy2, _, _ := cache.updateSelectorPolicy(identity1, ep1.Id)
+	policy2, _, _, _ := cache.updateSelectorPolicy(identity1, ep1.Id)
 	require.NotNil(t, policy2)
 	require.Same(t, policy1, policy2)
 
 	// Remove the identity and observe that it is no longer available
 	cacheCleared := cache.delete(identity1)
 	require.True(t, cacheCleared)
-	_, updated, _ = cache.updateSelectorPolicy(identity1, ep1.Id)
+	_, _, updated, _ = cache.updateSelectorPolicy(identity1, ep1.Id)
 	require.True(t, updated)
 
 	// Attempt to update policy for non-cached endpoint and observe failure
 	ep3 := testutils.NewTestEndpoint(t)
 	ep3.SetIdentity(1234, true)
-	policy3, updated, err := cache.updateSelectorPolicy(ep3.GetSecurityIdentity(), ep3.Id)
+	policy3, _, updated, err := cache.updateSelectorPolicy(ep3.GetSecurityIdentity(), ep3.Id)
 	require.NoError(t, err)
 	require.True(t, updated)
 
