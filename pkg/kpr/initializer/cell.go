63ea8a96cf432d595d016ebb683cbba730e5716b
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-10-02 12:12:19 +0200

    kpr: introduce initializer cell & component
    This commit introduces a Hive Cell for the KPR initialization
    logic. The legacy daemon logic is still responsible to handle the
    lifecycle aspects by triggering the respective functionality.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

---
+++ pkg/kpr/initializer/cell.go
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package initializer
+
+import (
+	"log/slog"
+
+	"github.com/cilium/hive/cell"
+
+	"github.com/cilium/cilium/pkg/datapath/linux/sysctl"
+	"github.com/cilium/cilium/pkg/datapath/tables"
+	"github.com/cilium/cilium/pkg/datapath/tunnel"
+	datapath "github.com/cilium/cilium/pkg/datapath/types"
+	"github.com/cilium/cilium/pkg/kpr"
+	"github.com/cilium/cilium/pkg/loadbalancer"
+	wgTypes "github.com/cilium/cilium/pkg/wireguard/types"
+)
+
+var Cell = cell.Module(
+	"kube-proxy-replacement-init",
+	"Provides KPR initialization logic",
+	kpr.Cell,
+	cell.Provide(newKPRInitializer),
+)
+
+type KPRInitializer interface {
+	// InitKubeProxyReplacementOptions will configure the loadbalancer config options according
+	// to the KPRConfig.
+	InitKubeProxyReplacementOptions() error
+
+	// FinishKubeProxyReplacementInit finishes initialization of kube-proxy
+	// replacement after all devices are known.
+	FinishKubeProxyReplacementInit(devices []*tables.Device, directRoutingDevice string) error
+}
+
+type kprInitializerParams struct {
+	cell.In
+
+	Logger       *slog.Logger
+	Sysctl       sysctl.Sysctl
+	TunnelConfig tunnel.Config
+	LBConfig     loadbalancer.Config
+	KPRCfg       kpr.KPRConfig
+	WireguardCfg wgTypes.WireguardConfig
+	IPSecAgent   datapath.IPsecAgent
+}
+
+func newKPRInitializer(params kprInitializerParams) KPRInitializer {
+	return &kprInitializer{
+		logger:       params.Logger,
+		sysctl:       params.Sysctl,
+		tunnelConfig: params.TunnelConfig,
+		lbConfig:     params.LBConfig,
+		kprCfg:       params.KPRCfg,
+		wgCfg:        params.WireguardCfg,
+		ipsecAgent:   params.IPSecAgent,
+	}
+}
