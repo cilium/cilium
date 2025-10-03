8d5f1e6e6aae0f61df8d474e67756dbd7b0100e2
Julian Wiedmann <jwi@isovalent.com>
2025-10-03 12:37:03 +0300

    bpf: overlay: fix up include order for mcast.h
    mcast.h by accident pulls in common.h, but before the various file-wide
    defines like IS_BPF_OVERLAY are set. This makes them unusable in places
    like overloadable_skb.h.
    
    Fix things up making common.h the first regular header that gets included,
    as done in all other programs. For this also shuffle the tailcall.h further
    down, even though it's currently harmless.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/bpf_overlay.c
+++ bpf/bpf_overlay.c
@@ -7,7 +7,6 @@
 #include <bpf/config/node.h>
 #include <bpf/config/global.h>
 #include <netdev_config.h>
-#include "lib/mcast.h"
 
 #define IS_BPF_OVERLAY 1
 
@@ -26,7 +25,6 @@
  */
 #define SKIP_SRV6_HANDLING
 
-#include "lib/tailcall.h"
 #include "lib/common.h"
 #include "lib/edt.h"
 #include "lib/eps.h"
@@ -38,10 +36,12 @@
 #include "lib/local_delivery.h"
 #include "lib/drop.h"
 #include "lib/identity.h"
+#include "lib/mcast.h"
 #include "lib/nodeport.h"
 #include "lib/nodeport_egress.h"
 #include "lib/clustermesh.h"
 #include "lib/egress_gateway.h"
+#include "lib/tailcall.h"
 
 #ifdef ENABLE_VTEP
 #include "lib/vtep.h"
