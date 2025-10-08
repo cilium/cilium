cilium/cilium

commit 0821c15c2cc222d68dd78b3583d01c17ac47e518

pkg/aws: filter aws resources through vpc id
Make the aws operator to figure out its own vpcID to filter common AWS
resources to reduce the aws calls and operator's CPU and memory
pressure.

Signed-off-by: Liyi Huang <liyi.huang@isovalent.com>

pkg/aws/eni/instances.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/aws/eni/limits"
 	eniTypes "github.com/cilium/cilium/pkg/aws/eni/types"
+	"github.com/cilium/cilium/pkg/aws/metadata"
 	"github.com/cilium/cilium/pkg/aws/types"
 	"github.com/cilium/cilium/pkg/ipam"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
@@ -28,10 +29,11 @@ import (
 type EC2API interface {
 	GetInstance(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap, subnets ipamTypes.SubnetMap, instanceID string) (*ipamTypes.Instance, error)
 	GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap, subnets ipamTypes.SubnetMap) (*ipamTypes.InstanceMap, error)
-	GetSubnets(ctx context.Context) (ipamTypes.SubnetMap, error)
-	GetVpcs(ctx context.Context) (ipamTypes.VirtualNetworkMap, error)
-	GetRouteTables(ctx context.Context) (ipamTypes.RouteTableMap, error)
-	GetSecurityGroups(ctx context.Context) (types.SecurityGroupMap, error)
+	GetSubnets(ctx context.Context, vpcID string) (ipamTypes.SubnetMap, error)
+	GetVpcs(ctx context.Context, vpcID string) (ipamTypes.VirtualNetworkMap, error)
+	GetRouteTables(ctx context.Context, vpcID string) (ipamTypes.RouteTableMap, error)
+	GetSecurityGroups(ctx context.Context, vpcID string) (types.SecurityGroupMap, error)
+
 	GetDetachedNetworkInterfaces(ctx context.Context, tags ipamTypes.Tags, maxResults int32) ([]string, error)
 	CreateNetworkInterface(ctx context.Context, toAllocate int32, subnetID, desc string, groups []string, allocatePrefixes bool) (string, *eniTypes.ENI, error)
 	AttachNetworkInterface(ctx context.Context, index int32, instanceID, eniID string) (string, error)
@@ -51,7 +53,8 @@ type InstancesManager struct {
 	logger *slog.Logger
 	// resyncLock ensures instance incremental resync do not run at the same time as a full API resync
 	resyncLock lock.RWMutex
-
+	// vpcID is the VPC ID current operator running on, we will use it to filter other AWS resources only within this VPC
+	vpcID string
 	// mutex protects the fields below
 	mutex          lock.RWMutex
 	instances      *ipamTypes.InstanceMap
@@ -202,24 +205,31 @@ func (m *InstancesManager) Resync(ctx context.Context) time.Time {
 func (m *InstancesManager) resync(ctx context.Context, instanceID string) time.Time {
 	resyncStart := time.Now()
 
-	vpcs, err := m.api.GetVpcs(ctx)
+	_, _, _, currentVpcID, _, err := metadata.GetInstanceMetadata()
+	if err != nil {
+		m.logger.Warn("Unable to retrieve AWS instance metadata and will use cached VPC ID", logfields.Error, err)
+		// when we can't retrieve the VPC ID from AWS metadata, we use the cached VPC ID
+		currentVpcID = m.vpcID
+	}
+
+	vpcs, err := m.api.GetVpcs(ctx, currentVpcID)
 	if err != nil {
 		m.logger.Warn("Unable to synchronize EC2 VPC list", logfields.Error, err)
 		return time.Time{}
 	}
 
-	subnets, err := m.api.GetSubnets(ctx)
+	subnets, err := m.api.GetSubnets(ctx, currentVpcID)
 	if err != nil {
 		m.logger.Warn("Unable to retrieve EC2 subnets list", logfields.Error, err)
 		return time.Time{}
 	}
 
-	securityGroups, err := m.api.GetSecurityGroups(ctx)
+	securityGroups, err := m.api.GetSecurityGroups(ctx, currentVpcID)
 	if err != nil {
 		m.logger.Warn("Unable to retrieve EC2 security group list", logfields.Error, err)
 		return time.Time{}
 	}
-	routeTables, err := m.api.GetRouteTables(ctx)
+	routeTables, err := m.api.GetRouteTables(ctx, currentVpcID)
 	if err != nil {
 		m.logger.Warn("Unable to retrieve EC2 route table list", logfields.Error, err)
 		return time.Time{}
@@ -268,6 +278,7 @@ func (m *InstancesManager) resync(ctx context.Context, instanceID string) time.T
 		m.instances.UpdateInstance(instanceID, instance)
 	}
 
+	m.vpcID = currentVpcID
 	m.subnets = subnets
 	m.vpcs = vpcs
 	m.securityGroups = securityGroups

pkg/aws/ec2/ec2.go
@@ -533,10 +533,14 @@ func (c *Client) GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetwork
 	return instances, nil
 }
 
-// describeVpcs lists all VPCs
-func (c *Client) describeVpcs(ctx context.Context) ([]ec2_types.Vpc, error) {
+// describeVpcs lists the specific VPC based on the vpcID, or all VPCs if the vpcID is empty
+func (c *Client) describeVpcs(ctx context.Context, vpcID string) ([]ec2_types.Vpc, error) {
 	var result []ec2_types.Vpc
-	paginator := ec2.NewDescribeVpcsPaginator(c.ec2Client, &ec2.DescribeVpcsInput{})
+	input := &ec2.DescribeVpcsInput{}
+	if vpcID != "" {
+		input.VpcIds = []string{vpcID}
+	}
+	paginator := ec2.NewDescribeVpcsPaginator(c.ec2Client, input)
 	for paginator.HasMorePages() {
 		c.limiter.Limit(ctx, DescribeVpcs)
 		sinceStart := spanstat.Start()
@@ -550,11 +554,11 @@ func (c *Client) describeVpcs(ctx context.Context) ([]ec2_types.Vpc, error) {
 	return result, nil
 }
 
-// GetVpcs retrieves and returns all Vpcs
-func (c *Client) GetVpcs(ctx context.Context) (ipamTypes.VirtualNetworkMap, error) {
+// GetVpcs retrieves and returns the VPC based on vpcID, or all VPCs if the vpcID is empty.
+func (c *Client) GetVpcs(ctx context.Context, vpcID string) (ipamTypes.VirtualNetworkMap, error) {
 	vpcs := ipamTypes.VirtualNetworkMap{}
 
-	vpcList, err := c.describeVpcs(ctx)
+	vpcList, err := c.describeVpcs(ctx, vpcID)
 	if err != nil {
 		return nil, err
 	}
@@ -578,13 +582,14 @@ func (c *Client) GetVpcs(ctx context.Context) (ipamTypes.VirtualNetworkMap, erro
 	return vpcs, nil
 }
 
-// describeSubnets lists all subnets
-func (c *Client) describeSubnets(ctx context.Context) ([]ec2_types.Subnet, error) {
+// describeSubnets lists the subnets in the vpcID, or all subnets if the vpcID is empty
+func (c *Client) describeSubnets(ctx context.Context, vpcID string) ([]ec2_types.Subnet, error) {
 	var result []ec2_types.Subnet
 	input := &ec2.DescribeSubnetsInput{}
 	if len(c.subnetsFilters) > 0 {
 		input.Filters = c.subnetsFilters
 	}
+	input.Filters = addVPCFilterIfPresent(input.Filters, vpcID)
 	paginator := ec2.NewDescribeSubnetsPaginator(c.ec2Client, input)
 	for paginator.HasMorePages() {
 		c.limiter.Limit(ctx, DescribeSubnets)
@@ -600,11 +605,11 @@ func (c *Client) describeSubnets(ctx context.Context) ([]ec2_types.Subnet, error
 	return result, nil
 }
 
-// GetSubnets returns all EC2 subnets as a subnetMap
-func (c *Client) GetSubnets(ctx context.Context) (ipamTypes.SubnetMap, error) {
+// GetSubnets returns the subnets in the vpcID, or all subnets if the vpcID is empty.
+func (c *Client) GetSubnets(ctx context.Context, vpcID string) (ipamTypes.SubnetMap, error) {
 	subnets := ipamTypes.SubnetMap{}
 
-	subnetList, err := c.describeSubnets(ctx)
+	subnetList, err := c.describeSubnets(ctx, vpcID)
 	if err != nil {
 		return nil, err
 	}
@@ -643,13 +648,14 @@ func (c *Client) GetSubnets(ctx context.Context) (ipamTypes.SubnetMap, error) {
 	return subnets, nil
 }
 
-// describeRouteTables lists all route tables
-func (c *Client) describeRouteTables(ctx context.Context) ([]ec2_types.RouteTable, error) {
+// describeRouteTables lists the route tables in the vpcID, or all route tables if the vpcID is empty.
+func (c *Client) describeRouteTables(ctx context.Context, vpcID string) ([]ec2_types.RouteTable, error) {
 	var result []ec2_types.RouteTable
 	input := &ec2.DescribeRouteTablesInput{}
 	if len(c.routeTableFilters) > 0 {
 		input.Filters = c.routeTableFilters
 	}
+	input.Filters = addVPCFilterIfPresent(input.Filters, vpcID)
 	paginator := ec2.NewDescribeRouteTablesPaginator(c.ec2Client, input)
 	for paginator.HasMorePages() {
 		c.limiter.Limit(ctx, DescribeRouteTables)
@@ -664,11 +670,11 @@ func (c *Client) describeRouteTables(ctx context.Context) ([]ec2_types.RouteTabl
 	return result, nil
 }
 
-// GetRouteTables returns all EC2 route tables as a RouteTableMap
-func (c *Client) GetRouteTables(ctx context.Context) (ipamTypes.RouteTableMap, error) {
+// GetRouteTables returns the route tables in the vpcID, or all route tables if the vpcID is empty.
+func (c *Client) GetRouteTables(ctx context.Context, vpcID string) (ipamTypes.RouteTableMap, error) {
 	routeTables := ipamTypes.RouteTableMap{}
 
-	routeTableList, err := c.describeRouteTables(ctx)
+	routeTableList, err := c.describeRouteTables(ctx, vpcID)
 	if err != nil {
 		return nil, err
 	}
@@ -922,12 +928,15 @@ func createAWSTagSlice(tags map[string]string) []ec2_types.Tag {
 	return awsTags
 }
 
-func (c *Client) describeSecurityGroups(ctx context.Context) ([]ec2_types.SecurityGroup, error) {
+// describeSecurityGroups lists the security groups in the vpcID, or all security groups if the vpcID is empty.
+func (c *Client) describeSecurityGroups(ctx context.Context, vpcID string) ([]ec2_types.SecurityGroup, error) {
 	var result []ec2_types.SecurityGroup
 	input := &ec2.DescribeSecurityGroupsInput{}
 	if operatorOption.Config.AWSPaginationEnabled {
 		input.MaxResults = aws.Int32(defaults.AWSResultsPerApiCall)
 	}
+	filters := addVPCFilterIfPresent([]ec2_types.Filter{}, vpcID)
+	input.Filters = filters
 	paginator := ec2.NewDescribeSecurityGroupsPaginator(c.ec2Client, input)
 	for paginator.HasMorePages() {
 		c.limiter.Limit(ctx, DescribeSecurityGroups)
@@ -942,11 +951,11 @@ func (c *Client) describeSecurityGroups(ctx context.Context) ([]ec2_types.Securi
 	return result, nil
 }
 
-// GetSecurityGroups returns all EC2 security groups as a SecurityGroupMap
-func (c *Client) GetSecurityGroups(ctx context.Context) (types.SecurityGroupMap, error) {
+// GetSecurityGroups returns the security groups in the vpcID, or all security groups if the vpcID is empty.
+func (c *Client) GetSecurityGroups(ctx context.Context, vpcID string) (types.SecurityGroupMap, error) {
 	securityGroups := types.SecurityGroupMap{}
 
-	secGroupList, err := c.describeSecurityGroups(ctx)
+	secGroupList, err := c.describeSecurityGroups(ctx, vpcID)
 	if err != nil {
 		return securityGroups, err
 	}
@@ -986,4 +995,16 @@ func (c *Client) GetInstanceTypes(ctx context.Context) ([]ec2_types.InstanceType
 		result = append(result, output.InstanceTypes...)
 	}
 	return result, nil
+}
+
+// addVPCFilterIfPresent checks if a vpcID is provided and, if so, adds a
+// vpc-id filter to the given list of filters.
+func addVPCFilterIfPresent(filters []ec2_types.Filter, vpcID string) []ec2_types.Filter {
+	if vpcID == "" {
+		return filters
+	}
+	return append(filters, ec2_types.Filter{
+		Name:   aws.String("vpc-id"),
+		Values: []string{vpcID},
+	})
 }

pkg/aws/ec2/mock/mock.go
@@ -722,7 +722,7 @@ func (e *API) GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap
 	return instances, nil
 }
 
-func (e *API) GetVpcs(ctx context.Context) (ipamTypes.VirtualNetworkMap, error) {
+func (e *API) GetVpcs(ctx context.Context, vpcID string) (ipamTypes.VirtualNetworkMap, error) {
 	vpcs := ipamTypes.VirtualNetworkMap{}
 
 	e.mutex.RLock()
@@ -734,7 +734,7 @@ func (e *API) GetVpcs(ctx context.Context) (ipamTypes.VirtualNetworkMap, error) 
 	return vpcs, nil
 }
 
-func (e *API) GetSubnets(ctx context.Context) (ipamTypes.SubnetMap, error) {
+func (e *API) GetSubnets(ctx context.Context, vpcID string) (ipamTypes.SubnetMap, error) {
 	subnets := ipamTypes.SubnetMap{}
 
 	e.mutex.RLock()
@@ -746,7 +746,7 @@ func (e *API) GetSubnets(ctx context.Context) (ipamTypes.SubnetMap, error) {
 	return subnets, nil
 }
 
-func (e *API) GetRouteTables(ctx context.Context) (ipamTypes.RouteTableMap, error) {
+func (e *API) GetRouteTables(ctx context.Context, vpcID string) (ipamTypes.RouteTableMap, error) {
 	routeTables := ipamTypes.RouteTableMap{}
 
 	e.mutex.RLock()
@@ -783,7 +783,7 @@ func (e *API) TagENI(ctx context.Context, eniID string, eniTags map[string]strin
 	return fmt.Errorf("Unable to find ENI with ID %s", eniID)
 }
 
-func (e *API) GetSecurityGroups(ctx context.Context) (types.SecurityGroupMap, error) {
+func (e *API) GetSecurityGroups(ctx context.Context, vpcID string) (types.SecurityGroupMap, error) {
 	securityGroups := types.SecurityGroupMap{}
 
 	e.mutex.RLock()

pkg/aws/ec2/mock/mock_test.go
@@ -77,7 +77,7 @@ func TestMock(t *testing.T) {
 	}
 	api.UpdateSecurityGroups([]*types.SecurityGroup{sg1, sg2})
 
-	sgMap, err := api.GetSecurityGroups(t.Context())
+	sgMap, err := api.GetSecurityGroups(t.Context(), "")
 	require.NoError(t, err)
 	require.Equal(t, types.SecurityGroupMap{"sg1": sg1, "sg2": sg2}, sgMap)
 }
@@ -179,7 +179,7 @@ func TestGetRouteTables(t *testing.T) {
 		routeTables,
 	)
 
-	tables, err := api.GetRouteTables(t.Context())
+	tables, err := api.GetRouteTables(t.Context(), "")
 	require.NoError(t, err)
 	require.Len(t, tables, 2)
 

commit bcd22587fdec9e7fe026ee4f671b42bb823d6baa

eni: Lower severity of IMDS failure
When fetching the instance metadata in the operator, it seems like this
can sometimes fail. Since this causes CI flakes and there is nothing the
user can do about it, lower the severity of the log message. The
operator will either use the last observed VPCID or none if this happens
the first time. In either case, the operator can continue its
operations.

Additionally, also log the cached VPC ID as it will help in
troubleshooting.

Fixes: 0821c15c2cc2 ("pkg/aws: filter aws resources through vpc id")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/aws/eni/instances.go
@@ -214,8 +214,10 @@ func (m *InstancesManager) resync(ctx context.Context, instanceID string) time.T
 	var currentVpcID string
 	metadataInfo, err := m.metadataapi.GetInstanceMetadata()
 	if err != nil {
-		m.logger.Warn("Unable to retrieve AWS instance metadata and will use cached VPC ID", logfields.Error, err)
 		// when we can't retrieve the VPC ID from AWS metadata, we use the cached VPC ID
+		m.logger.Info("Unable to retrieve AWS instance metadata and will use cached VPC ID",
+			logfields.VPCID, m.vpcID,
+			logfields.Error, err)
 		currentVpcID = m.vpcID
 	} else {
 		currentVpcID = metadataInfo.VPCID