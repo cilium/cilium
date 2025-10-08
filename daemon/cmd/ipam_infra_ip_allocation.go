d17c5c5afd3b5dd689914dda7664793400c20c55
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-10-08 11:28:21 +0200

    daemon: split ipam.go
    After introducing the `infraIPAllocator`, the file `daemon/cmd/ipam.go`
    contains two aspects: configure&start IPAM and allocating infra IPs.
    
    To better understand this, this commit splits the existing file `ipam.go`
    into two `ipam_init.go` and `ipam_infra_ip_allocation.go`.
    
    Note: in addition the two methods `configureIPAM` & `startIPAM` are merged.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- daemon/cmd/ipam_infra_ip_allocation.go
+++ daemon/cmd/ipam_infra_ip_allocation.go
@@ -17,7 +17,6 @@ import (
 	"golang.org/x/sys/unix"
 	"k8s.io/apimachinery/pkg/util/wait"
 
-	"github.com/cilium/cilium/pkg/cidr"
 	linuxrouting "github.com/cilium/cilium/pkg/datapath/linux/routing"
 	"github.com/cilium/cilium/pkg/datapath/linux/safenetlink"
 	"github.com/cilium/cilium/pkg/datapath/tables"
@@ -27,7 +26,6 @@ import (
 	iputil "github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/ipam"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
-	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/mtu"
 	"github.com/cilium/cilium/pkg/node"
@@ -632,64 +630,6 @@ func (r *infraIPAllocator) AllocateIPs(ctx context.Context, router restoredIPs) 
 	}
 
 	return r.allocateHealthIPs()
-}
-
-func (d *Daemon) configureIPAM(ctx context.Context) {
-	// If the device has been specified, the IPv4AllocPrefix and the
-	// IPv6AllocPrefix were already allocated before the k8s.Init().
-	//
-	// If the device hasn't been specified, k8s.Init() allocated the
-	// IPv4AllocPrefix and the IPv6AllocPrefix from k8s node annotations.
-	//
-	// If k8s.Init() failed to retrieve the IPv4AllocPrefix we can try to derive
-	// it from an existing node_config.h file or from previous cilium_host
-	// interfaces.
-	//
-	// Then, we will calculate the IPv4 or IPv6 alloc prefix based on the IPv6
-	// or IPv4 alloc prefix, respectively, retrieved by k8s node annotations.
-	if option.Config.IPv4Range != AutoCIDR {
-		allocCIDR, err := cidr.ParseCIDR(option.Config.IPv4Range)
-		if err != nil {
-			logging.Fatal(
-				d.params.Logger,
-				"Invalid IPv4 allocation prefix",
-				logfields.Error, err,
-				logfields.V4Prefix, option.Config.IPv4Range,
-			)
-		}
-		node.SetIPv4AllocRange(allocCIDR)
-	}
-
-	if option.Config.IPv6Range != AutoCIDR {
-		allocCIDR, err := cidr.ParseCIDR(option.Config.IPv6Range)
-		if err != nil {
-			logging.Fatal(
-				d.params.Logger,
-				"Invalid IPv6 allocation prefix",
-				logfields.Error, err,
-				logfields.V6Prefix, option.Config.IPv6Range,
-			)
-		}
-
-		node.SetIPv6NodeRange(allocCIDR)
-	}
-
-	device := ""
-	drd, _ := d.params.DirectRoutingDevice.Get(ctx, d.params.DB.ReadTxn())
-	if drd != nil {
-		device = drd.Name
-	}
-	if err := node.AutoComplete(d.params.Logger, device); err != nil {
-		logging.Fatal(d.params.Logger, "Cannot autocomplete node addresses", logfields.Error, err)
-	}
-}
-
-func (d *Daemon) startIPAM() {
-	bootstrapStats.ipam.Start()
-	d.params.Logger.Info("Initializing node addressing")
-	// Set up ipam conf after init() because we might be running d.conf.KVStoreIPv4Registration
-	d.params.IPAM.ConfigureAllocator()
-	bootstrapStats.ipam.End(true)
 }
 
 func (r *infraIPAllocator) parseRoutingInfo(result *ipam.AllocationResult) (*linuxrouting.RoutingInfo, error) {
