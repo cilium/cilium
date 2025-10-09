cilium/cilium

commit 4ce357a9573d28e40b4cc4589a917ef8cb2a752a

pkg/maps: migrate to slog
Migrate this package to use slog instead of logrus.

Signed-off-by: André Martins <andre@cilium.io>

pkg/maps/multicast/subscribermap.go
@@ -6,13 +6,13 @@ package multicast
 import (
 	"errors"
 	"fmt"
+	"log/slog"
 	"net/netip"
 	"unsafe"
 
 	ciliumebpf "github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/asm"
 	"github.com/cilium/hive/cell"
-	"github.com/sirupsen/logrus"
 
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/datapath/linux/config/defines"
@@ -52,11 +52,12 @@ type GroupV4OuterMap struct {
 
 	// batchLookupSupported indicates if the kernel supports batch lookup.
 	batchLookupSupported bool
+	logger               *slog.Logger
 }
 
-func NewGroupV4OuterMap(name string) *GroupV4OuterMap {
+func NewGroupV4OuterMap(logger *slog.Logger, name string) *GroupV4OuterMap {
 	innerMap := newSubscriberV4InnerMapSpec()
-	m := ebpf.NewMap(&ebpf.MapSpec{
+	m := ebpf.NewMap(logger, &ebpf.MapSpec{
 		Name:       name,
 		Type:       ebpf.HashOfMaps,
 		KeySize:    uint32(unsafe.Sizeof(GroupV4Key{})),
@@ -66,7 +67,7 @@ func NewGroupV4OuterMap(name string) *GroupV4OuterMap {
 		Pinning:    ebpf.PinByName,
 	})
 
-	return &GroupV4OuterMap{Map: m}
+	return &GroupV4OuterMap{logger: logger, Map: m}
 }
 
 // ParamsIn are parameters provided by the Hive and is the argument for
@@ -74,7 +75,7 @@ func NewGroupV4OuterMap(name string) *GroupV4OuterMap {
 type ParamsIn struct {
 	cell.In
 	Lifecycle cell.Lifecycle
-	Logger    logrus.FieldLogger
+	Logger    *slog.Logger
 	Config
 }
 
@@ -108,7 +109,7 @@ func NewGroupV4Map(in ParamsIn) ParamsOut {
 
 	out.NodeDefines["ENABLE_MULTICAST"] = "1"
 
-	groupMap := NewGroupV4OuterMap(GroupOuter4MapName)
+	groupMap := NewGroupV4OuterMap(in.Logger, GroupOuter4MapName)
 
 	out.MapOut = bpf.NewMapOut((GroupV4Map(groupMap)))
 
@@ -135,7 +136,7 @@ func (m GroupV4OuterMap) Insert(group netip.Addr) error {
 		return err
 	}
 
-	subMap, err := newSubscriberV4InnerMap()
+	subMap, err := newSubscriberV4InnerMap(m.logger)
 	if err != nil {
 		return fmt.Errorf("failed to create SubscriberV4InnerMap: %w", err)
 	}
@@ -170,7 +171,7 @@ func (m GroupV4OuterMap) Lookup(group netip.Addr) (SubscriberV4Map, error) {
 	}
 
 	var subMap *ebpf.Map
-	subMap, err = ebpf.MapFromID(int(val.FD))
+	subMap, err = ebpf.MapFromID(m.logger, int(val.FD))
 	if err != nil {
 		return nil, fmt.Errorf("failed to convert SubscriberV4InnerMap FD to *ebpf.Map: %w", err)
 	}
@@ -271,8 +272,8 @@ type GroupV4Val struct {
 	FD uint32
 }
 
-func OpenGroupV4OuterMap(name string) (*GroupV4OuterMap, error) {
-	m, err := ebpf.LoadRegisterMap(name)
+func OpenGroupV4OuterMap(logger *slog.Logger, name string) (*GroupV4OuterMap, error) {
+	m, err := ebpf.LoadRegisterMap(logger, name)
 	if err != nil {
 		return nil, err
 	}
@@ -313,10 +314,10 @@ type SubscriberV4InnerMap struct {
 	*ebpf.Map
 }
 
-func newSubscriberV4InnerMap() (*SubscriberV4InnerMap, error) {
+func newSubscriberV4InnerMap(logger *slog.Logger) (*SubscriberV4InnerMap, error) {
 	spec := newSubscriberV4InnerMapSpec()
 
-	m := ebpf.NewMap(spec)
+	m := ebpf.NewMap(logger, spec)
 	if err := m.OpenOrCreate(); err != nil {
 		return nil, err
 	}

cilium-dbg/cmd/bpf_auth_flush.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
+	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/maps/authmap"
 )
 
@@ -24,7 +25,7 @@ var bpfAuthFlushCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		common.RequireRootPrivilege("cilium bpf auth flush")
 
-		authMap, err := authmap.LoadAuthMap()
+		authMap, err := authmap.LoadAuthMap(logging.DefaultSlogLogger)
 		if err != nil {
 			if errors.Is(err, fs.ErrNotExist) {
 				fmt.Fprintln(os.Stderr, "Cannot find auth bpf map")

cilium-dbg/cmd/bpf_auth_list.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
+	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/maps/authmap"
 	"github.com/cilium/cilium/pkg/policy"
 )
@@ -35,7 +36,7 @@ var bpfAuthListCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		common.RequireRootPrivilege("cilium bpf auth list")
 
-		authMap, err := authmap.LoadAuthMap()
+		authMap, err := authmap.LoadAuthMap(logging.DefaultSlogLogger)
 		if err != nil {
 			if errors.Is(err, fs.ErrNotExist) {
 				fmt.Fprintln(os.Stderr, "Cannot find auth bpf map")

cilium-dbg/cmd/bpf_lb_maglev_get.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/ebpf"
+	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
 )
 
@@ -80,7 +81,7 @@ func getMaglevServiceBackends(svcID uint16) (map[string][]string, error) {
 // dumpMaglevServiceBackends looks up the given service ID in the Maglev map
 // with the given name.
 func dumpMaglevServiceBackends(mapName string, svcID uint16) (string, error) {
-	m, err := lbmap.OpenMaglevOuterMap(mapName)
+	m, err := lbmap.OpenMaglevOuterMap(logging.DefaultSlogLogger, mapName)
 	if err != nil {
 		return "", err
 	}

cilium-dbg/cmd/bpf_lb_maglev_list.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
+	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
 )
 
@@ -62,7 +63,7 @@ func dumpMaglevTables() (map[string][]string, error) {
 // dumps the backend tables of all services. Returns an empty initialized
 // map if the given eBPF map does not exist.
 func dumpMaglevTable(name string, ipv6 bool) (map[string][]string, error) {
-	m, err := lbmap.OpenMaglevOuterMap(name)
+	m, err := lbmap.OpenMaglevOuterMap(logging.DefaultSlogLogger, name)
 	if errors.Is(err, os.ErrNotExist) {
 		// Map not existing is not an error.
 		// Skip dumping it and return an empty allocated map.

cilium-dbg/cmd/bpf_metrics_list.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
+	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/maps/metricsmap"
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 )
@@ -53,6 +54,7 @@ var bpfMetricsListCmd = &cobra.Command{
 	Short: "List BPF datapath traffic metrics",
 	Run: func(cmd *cobra.Command, args []string) {
 		common.RequireRootPrivilege("cilium bpf metrics list")
+		metricsmap.InitMap(logging.DefaultSlogLogger)
 		listMetrics(&metricsmap.Metrics)
 	},
 }

cilium-dbg/cmd/bpf_multicast_groups.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"log/slog"
 	"net/netip"
 	"os"
 	"sort"
@@ -16,6 +17,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
+	"github.com/cilium/cilium/pkg/logging"
 	maps_multicast "github.com/cilium/cilium/pkg/maps/multicast"
 )
 
@@ -39,7 +41,7 @@ var MulticastGroupListCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		common.RequireRootPrivilege("cilium bpf multicast group list")
 
-		groupV4Map, err := getMulticastGroupMap()
+		groupV4Map, err := getMulticastGroupMap(logging.DefaultSlogLogger)
 		if err != nil {
 			Fatalf("failed to get multicast bpf map: %s", err)
 		}
@@ -88,7 +90,7 @@ cilium-dbg bpf multicast group add 229.0.0.1`,
 			Fatalf("Invalid arguments: %s", err)
 		}
 
-		groupV4Map, err := getMulticastGroupMap()
+		groupV4Map, err := getMulticastGroupMap(logging.DefaultSlogLogger)
 		if err != nil {
 			Fatalf("failed to get multicast bpf map: %s", err)
 		}
@@ -116,7 +118,7 @@ cilium-dbg bpf multicast group delete 229.0.0.1`,
 			Fatalf("Invalid arguments: %s", err)
 		}
 
-		groupV4Map, err := getMulticastGroupMap()
+		groupV4Map, err := getMulticastGroupMap(logging.DefaultSlogLogger)
 		if err != nil {
 			Fatalf("failed to get multicast bpf map: %s", err)
 		}
@@ -128,8 +130,8 @@ cilium-dbg bpf multicast group delete 229.0.0.1`,
 	},
 }
 
-func getMulticastGroupMap() (*maps_multicast.GroupV4OuterMap, error) {
-	groupV4Map, err := maps_multicast.OpenGroupV4OuterMap(maps_multicast.GroupOuter4MapName)
+func getMulticastGroupMap(logger *slog.Logger) (*maps_multicast.GroupV4OuterMap, error) {
+	groupV4Map, err := maps_multicast.OpenGroupV4OuterMap(logger, maps_multicast.GroupOuter4MapName)
 	if err != nil {
 		if errors.Is(err, fs.ErrNotExist) {
 			return nil, fmt.Errorf("multicast not enabled")

cilium-dbg/cmd/bpf_multicast_subscribers.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/datapath/linux/safenetlink"
 	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/logging"
 	maps_multicast "github.com/cilium/cilium/pkg/maps/multicast"
 )
 
@@ -52,7 +53,7 @@ var MulticastGroupSubscriberListCmd = &cobra.Command{
 			Fatalf("invalid argument: %s", err)
 		}
 
-		groupV4Map, err := getMulticastGroupMap()
+		groupV4Map, err := getMulticastGroupMap(logging.DefaultSlogLogger)
 		if err != nil {
 			Fatalf("Failed to get multicast bpf map: %s", err)
 		}
@@ -220,7 +221,7 @@ func printSubscriberList(subscribers []SubscriberData) {
 }
 
 func getMulticastSubscriberMap(groupAddr netip.Addr) (maps_multicast.SubscriberV4Map, error) {
-	groupV4Map, err := getMulticastGroupMap()
+	groupV4Map, err := getMulticastGroupMap(logging.DefaultSlogLogger)
 	if err != nil {
 		return nil, err
 	}

commit 402506eaa6b7275555a77325ade22a0eaca2cd70

maps/multicast: set GroupV4OuterMap.logger in OpenGroupV4OuterMap
Otherwise multicast map access from cilium-dbg will cause a panic due to
nil-pointer dereference:

[signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0x8a8018]

goroutine 1 [running]:
log/slog.(*Logger).Handler(...)
	/usr/local/go/src/log/slog/logger.go:121
log/slog.(*Logger).Enabled(0x49fce0?, {0x3cb4410?, 0x6454960?}, 0x735f747361636d5f?)
	/usr/local/go/src/log/slog/logger.go:168 +0x18
log/slog.(*Logger).log(0x0, {0x3cb4410?, 0x6454960?}, 0xfffffffffffffffc, {0x3792070, 0x12}, {0xc0008eb918, 0x2, 0x2})
	/usr/local/go/src/log/slog/logger.go:244 +0x99
log/slog.(*Logger).Debug(...)
	/usr/local/go/src/log/slog/logger.go:199
github.com/cilium/cilium/pkg/ebpf.registerMap(0xc000823700)
	/go/src/github.com/cilium/cilium/pkg/ebpf/map_register.go:22 +0x114
github.com/cilium/cilium/pkg/ebpf.(*Map).OpenOrCreate(0xc000823700)
	/go/src/github.com/cilium/cilium/pkg/ebpf/map.go:187 +0x68a
github.com/cilium/cilium/pkg/maps/multicast.newSubscriberV4InnerMap(0x0)
	/go/src/github.com/cilium/cilium/pkg/maps/multicast/subscribermap.go:320 +0xb2
github.com/cilium/cilium/pkg/maps/multicast.GroupV4OuterMap.Insert({0xc0008236c0?, 0x22?, 0x0?}, {{0x0?, 0x0?}, {0xc00000f680?}})
	/go/src/github.com/cilium/cilium/pkg/maps/multicast/subscribermap.go:138 +0x65
github.com/cilium/cilium/cilium-dbg/cmd.init.func74(0xc000cded00?, {0xc000d95ca0, 0x1, 0x1})
	/go/src/github.com/cilium/cilium/cilium-dbg/cmd/bpf_multicast_groups.go:97 +0x10c
github.com/spf13/cobra.(*Command).execute(0x5ed4120, {0xc000d95c60, 0x1, 0x1})
	/go/src/github.com/cilium/cilium/vendor/github.com/spf13/cobra/command.go:1019 +0xae7
github.com/spf13/cobra.(*Command).ExecuteC(0x5ed91a0)
	/go/src/github.com/cilium/cilium/vendor/github.com/spf13/cobra/command.go:1148 +0x465
github.com/spf13/cobra.(*Command).Execute(...)
	/go/src/github.com/cilium/cilium/vendor/github.com/spf13/cobra/command.go:1071
github.com/cilium/cilium/cilium-dbg/cmd.Execute()
	/go/src/github.com/cilium/cilium/cilium-dbg/cmd/root.go:45 +0x1a
main.main()
	/go/src/github.com/cilium/cilium/cilium-dbg/main.go:9 +0xf

Fixes: 4ce357a9573d ("pkg/maps: migrate to slog")

pkg/maps/multicast/subscribermap.go
@@ -280,6 +280,7 @@ func OpenGroupV4OuterMap(logger *slog.Logger, name string) (*GroupV4OuterMap, er
 	return &GroupV4OuterMap{
 		Map:                  m,
 		batchLookupSupported: haveBatchLookupSupport[GroupV4Key, GroupV4Val](m),
+		logger:               logger,
 	}, nil
 }
 