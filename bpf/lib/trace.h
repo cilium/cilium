cilium/cilium

commit a860170c107dcb1c295491b1bd638d7d392945b1

feat: add IPv4 packet tracing via IP options
This change introduces a new feature that allows for tracing IPv4 packets with an
embedded a trace ID in its IP option. Code changes include creating the
feature flag, parsing, and the BPF map to store the trace ID.

The following changes are included:

- A new feature-gate, `ip-tracing-option-type`, is added to enable and configure
the IP option type to be used for tracing.
- Helper functions are implemented to parse IPv4 options and extract the trace ID.
- Logic is added to save the parsed IPv4 options into a per-CPU array map, which
can then be used by other parts of the system. 

Signed-off-by: Ben Bigdelle <bigdelle@google.com>

bpf/lib/trace_helpers.h
@@ -1,0 +1,63 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright Authors of Cilium */
+
+#pragma once
+
+#include <bpf/ctx/ctx.h>
+#include <bpf/api.h>
+#include "common.h"
+#include "ip_options.h"
+
+/* Define the ip trace ID map with __u64 trace_id */
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, __u32); /* only one key */
+	__type(value, __u64); /* trace_id type */
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+} cilium_percpu_trace_id __section_maps_btf;
+
+/* bpf_trace_id_set sets the trace_id in the map. */
+static __always_inline void bpf_trace_id_set(__u64 trace_id)
+{
+	__u32 zero = 0;
+	__u64 *value = map_lookup_elem(&cilium_percpu_trace_id, &zero);
+
+	if (value)
+		*value = trace_id;
+}
+
+/* bpf_trace_id_get retrieves the trace_id from the map. */
+static __always_inline __u64 bpf_trace_id_get(void)
+{
+	__u32 zero = 0;
+	__u64 *value = map_lookup_elem(&cilium_percpu_trace_id, &zero);
+
+	if (value)
+		return *value;
+	return 0;
+}
+
+/* Function to parse and store the trace_id if the feature is enabled. */
+static __always_inline void
+check_and_store_ip_trace_id(struct __ctx_buff *ctx)
+{
+	__s64 trace_id = 0;
+	int ret;
+
+	if (CONFIG(tracing_ip_option_type) == 0) {
+		bpf_trace_id_set(0);
+		return;
+	}
+
+	ret = trace_id_from_ctx(ctx, &trace_id, CONFIG(tracing_ip_option_type));
+	if (IS_ERR(ret))
+		bpf_trace_id_set(0);
+	else
+		bpf_trace_id_set(trace_id);
+}
+
+static __always_inline __u64 load_ip_trace_id(void)
+{
+	return bpf_trace_id_get();
+}

bpf/include/bpf/config/node.h
@@ -29,3 +29,5 @@ NODE_CONFIG(__u32, direct_routing_dev_ifindex, "Index of the interface used to c
 
 NODE_CONFIG(bool, supports_fib_lookup_skip_neigh,
 	    "Whether or not BPF_FIB_LOOKUP_SKIP_NEIGH is supported.")
+
+NODE_CONFIG(__u8, tracing_ip_option_type, "The IP option type to use for packet tracing")

bpf/lib/ip_options.h
@@ -1,0 +1,190 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright Authors of Cilium */
+
+#include "common.h"
+
+/* Length of the initial supported IPv4 trace_id option (4 bytes).
+ * IPv4 IP options consist of 2 fixed bytes for the type and length,
+ * followed by a variable-length data field. An option length of 4 bytes
+ * indicates 2 fixed bytes for the type and length fields, and 2 bytes of
+ * ip_trace_id data.
+ */
+#define OPT16_LEN 4
+
+/* Length of the second supported IPv4 trace_id option (6 bytes).
+ * Indicates 4 bytes of ip_trace_id data.
+ */
+#define OPT32_LEN 6
+
+/* Length of the third supported IPv4 trace_id option (10 bytes).
+ * Indicates 8 bytes of ip_trace_id data.
+ */
+#define OPT64_LEN 10
+
+/* Maximum number of IPv4 options to process. */
+#define MAX_IPV4_OPTS 3
+
+/* The minimum value for IHL which corresponds to a packet with no options.
+ *
+ * A standard IP packet header has 20 bytes and the IHL is the number of 32 byte
+ * words.
+ */
+#define IHL_WITH_NO_OPTS 5
+
+/* Signifies that options were parsed correctly but no trace ID was found. */
+#define TRACE_ID_NOT_FOUND 0
+
+/* Signifies a failure to determine the trace ID based on an unspecified error. */
+#define TRACE_ID_ERROR -1
+
+/* Signifies that the trace ID was found but it was invalid. */
+#define TRACE_ID_INVALID -2
+
+/* Signifies a failure to determine trace ID because the IP family was not found. */
+#define TRACE_ID_NO_FAMILY -3
+
+/* Signifies a failure to determine trace ID because the IP option length was
+ * not supported.
+ */
+#define TRACE_ID_UNSUPPORTED_LENGTH_ERROR -4
+
+/* Signifies trace points which are being ignored because they're in IPv6
+ * code and not supported yet.
+ */
+#define TRACE_ID_SKIP_IPV6 -100
+
+/* trace_id_from_ip4 parses the IP options and returns the trace ID.
+ *
+ * See trace_id_from_ctx for more info.
+ */
+static __always_inline int
+trace_id_from_ip4(struct __ctx_buff *ctx, __s64 *value,
+		  const struct iphdr *ip4,
+		  __u8 trace_ip_opt_type)
+{
+	__u8 opt_type;
+	__u32 offset;
+	__u8 optlen;
+	__u32 end;
+	int i;
+
+	if (ip4->ihl <= IHL_WITH_NO_OPTS)
+		return TRACE_ID_NOT_FOUND;
+
+	offset = ETH_HLEN + sizeof(struct iphdr);
+	end = offset + (ip4->ihl << 2);
+
+#pragma unroll(MAX_IPV4_OPTS)
+	for (i = 0; i < MAX_IPV4_OPTS && offset < end; i++) {
+		/* We load the option header 1 field at a time since different types
+		 * have different formats.
+		 *
+		 * "Options 0 and 1 are exactly one octet which is their type field. All
+		 * other options have their one octet type field, followed by a one
+		 * octet length field, followed by length-2 octets of option data."
+		 *
+		 * Ref: https://www.iana.org/assignments/ip-parameters/ip-parameters.xhtml
+		 */
+
+		if (ctx_load_bytes(ctx, offset, &opt_type, 1) < 0)
+			return TRACE_ID_ERROR;
+
+		if (opt_type == IPOPT_END)
+			break;
+
+		if (opt_type == IPOPT_NOOP) {
+			offset++;
+			continue;
+		}
+
+		if (ctx_load_bytes(ctx, offset + 1, &optlen, 1) < 0)
+			return TRACE_ID_ERROR;
+
+		if (opt_type != trace_ip_opt_type) {
+			offset += optlen;
+			continue;
+		}
+
+		if (optlen != OPT16_LEN && optlen != OPT32_LEN && optlen != OPT64_LEN)
+			return TRACE_ID_INVALID;
+
+		switch (optlen) {
+			case OPT16_LEN: {
+				__s16 temp;
+
+				if (ctx_load_bytes(ctx, offset + 2, &temp, sizeof(temp)) < 0)
+					return TRACE_ID_ERROR;
+				*value = bpf_ntohs(temp);
+				return 0;
+			}
+			case OPT32_LEN: {
+				__s32 temp;
+
+				if (ctx_load_bytes(ctx, offset + 2, &temp, sizeof(temp)) < 0)
+					return TRACE_ID_ERROR;
+				*value = bpf_ntohl(temp);
+				return 0;
+			}
+			case OPT64_LEN: {
+				__s64 temp;
+
+				if (ctx_load_bytes(ctx, offset + 2, &temp, sizeof(temp)) < 0)
+					return TRACE_ID_ERROR;
+				*value = __bpf_be64_to_cpu(temp);
+				return 0;
+			}
+		default:
+			return TRACE_ID_UNSUPPORTED_LENGTH_ERROR;
+		}
+	}
+	return TRACE_ID_NOT_FOUND;
+}
+
+/*
+ * Parses the context to extract the trace ID from the IP options.
+ *
+ * Arguments:
+ * - ctx: The context buffer from which the IP options will be read.
+ * - value: A pointer to an __s64 where the resulting trace ID will be stored.
+ * - ip_opt_type_value: The type value of the IP option that contains the trace ID.
+ *
+ * Prerequisites:
+ * - Supports reading a trace ID embedded in IP options with lengths of 2, 4, or 8 bytes.
+ * - No support for trace_ids that are not 2, 4, or 8 bytes.
+ *
+ * Outputs:
+ * - Returns 0 if the trace ID is found.
+ * - Returns TRACE_ID_NOT_FOUND if no trace ID is found in the options.
+ * - Returns TRACE_ID_INVALID if the found trace ID is invalid (e.g., non-positive).
+ * - Returns TRACE_ID_ERROR if there is an error during parsing.
+ * - Returns TRACE_ID_NO_FAMILY if the packet is not IPv4.
+ * - Returns TRACE_ID_SKIP_IPV6 if the packet is IPv6.
+ */
+static __always_inline int
+trace_id_from_ctx(struct __ctx_buff *ctx, __s64 *value, __u8 ip_opt_type_value)
+{
+	void *data, *data_end;
+	__s64 trace_id = 0;
+	struct iphdr *ip4;
+	__u16 proto;
+	int ret;
+
+	if (!validate_ethertype(ctx, &proto))
+		return TRACE_ID_ERROR;
+
+	if (proto == bpf_htons(ETH_P_IPV6))
+		return TRACE_ID_SKIP_IPV6;
+
+	if (proto != bpf_htons(ETH_P_IP))
+		return TRACE_ID_NO_FAMILY;
+
+	if (!revalidate_data(ctx, &data, &data_end, &ip4))
+		return TRACE_ID_ERROR;
+
+	ret = trace_id_from_ip4(ctx, &trace_id, ip4, ip_opt_type_value);
+	if (IS_ERR(ret))
+		return ret;
+
+	*value = trace_id;
+	return 0;
+}

daemon/cmd/daemon_main.go
@@ -871,6 +871,9 @@ func InitGlobalFlags(logger *slog.Logger, cmd *cobra.Command, vp *viper.Viper) {
 	flags.Bool(option.EnableExtendedIPProtocols, defaults.EnableExtendedIPProtocols, "Enable traffic with extended IP protocols in datapath")
 	option.BindEnv(vp, option.EnableExtendedIPProtocols)
 
+	flags.Uint8(option.IPTracingOptionType, 0, "Specifies what IPv4 option type should be used to extract trace information from a packet; a value of 0 (default) disables IP tracing.")
+	option.BindEnv(vp, option.IPTracingOptionType)
+
 	if err := vp.BindPFlags(flags); err != nil {
 		logging.Fatal(logger, "BindPFlags failed", logfields.Error, err)
 	}

pkg/datapath/config/node_config.go
@@ -23,6 +23,8 @@ type Node struct {
 	TracePayloadLen uint32 `config:"trace_payload_len"`
 	// Length of payload to capture when tracing overlay packets.
 	TracePayloadLenOverlay uint32 `config:"trace_payload_len_overlay"`
+	// The IP option type to use for packet tracing.
+	TracingIPOptionType uint8 `config:"tracing_ip_option_type"`
 }
 
 func NewNode() *Node {
@@ -30,5 +32,5 @@ func NewNode() *Node {
 		[16]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
 		[4]byte{0x0, 0x0, 0x0, 0x0},
 		[16]byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
-		false, 0x0, 0x0}
+		false, 0x0, 0x0, 0x0}
 }

pkg/datapath/loader/config.go
@@ -34,5 +34,7 @@ func nodeConfig(lnc *datapath.LocalNodeConfiguration) config.Node {
 
 	node.SupportsFibLookupSkipNeigh = probes.HaveFibLookupSkipNeigh() == nil
 
+	node.TracingIPOptionType = uint8(option.Config.IPTracingOptionType)
+
 	return node
 }

pkg/datapath/loader/verifier_load_test.go
@@ -13,6 +13,7 @@ func lxcLoadPermutations() iter.Seq[*config.BPFLXC] {
 	return func(yield func(*config.BPFLXC) bool) {
 		for permutation := range permute(1) {
 			cfg := config.NewBPFLXC(*config.NewNode())
+			cfg.Node.TracingIPOptionType = 1
 			cfg.SecctxFromIPCache = permutation[0]
 			if !yield(cfg) {
 				return
@@ -25,6 +26,7 @@ func hostLoadPermutations() iter.Seq[*config.BPFHost] {
 	return func(yield func(*config.BPFHost) bool) {
 		for permutation := range permute(2) {
 			cfg := config.NewBPFHost(*config.NewNode())
+			cfg.Node.TracingIPOptionType = 1
 			cfg.SecctxFromIPCache = permutation[0]
 			cfg.EnableRemoteNodeMasquerade = permutation[1]
 			if !yield(cfg) {
@@ -37,6 +39,7 @@ func hostLoadPermutations() iter.Seq[*config.BPFHost] {
 func networkLoadPermutations() iter.Seq[*config.BPFNetwork] {
 	return func(yield func(*config.BPFNetwork) bool) {
 		cfg := config.NewBPFNetwork(*config.NewNode())
+		cfg.Node.TracingIPOptionType = 1
 		if !yield(cfg) {
 			return
 		}
@@ -47,6 +50,7 @@ func overlayLoadPermutations() iter.Seq[*config.BPFOverlay] {
 	return func(yield func(*config.BPFOverlay) bool) {
 		for permutation := range permute(1) {
 			cfg := config.NewBPFOverlay(*config.NewNode())
+			cfg.Node.TracingIPOptionType = 1
 			cfg.SecctxFromIPCache = permutation[0]
 			if !yield(cfg) {
 				return
@@ -68,6 +72,7 @@ func wireguardLoadPermutations() iter.Seq[*config.BPFWireguard] {
 	return func(yield func(*config.BPFWireguard) bool) {
 		for permutation := range permute(1) {
 			cfg := config.NewBPFWireguard(*config.NewNode())
+			cfg.Node.TracingIPOptionType = 1
 			cfg.SecctxFromIPCache = permutation[0]
 			if !yield(cfg) {
 				return
@@ -80,6 +85,7 @@ func xdpLoadPermutations() iter.Seq[*config.BPFXDP] {
 	return func(yield func(*config.BPFXDP) bool) {
 		for permutation := range permute(1) {
 			cfg := config.NewBPFXDP(*config.NewNode())
+			cfg.Node.TracingIPOptionType = 1
 			cfg.SecctxFromIPCache = permutation[0]
 			if !yield(cfg) {
 				return

commit 925f0b34f0eb73c8f4626b592a867d73493dc4c5

bpf: don't look up trace ID in map if tracing via IP options is disabled
Currently, check_and_store_ip_trace_id and load_ip_trace_id both incur a
map lookup on every call, even if packet tracing via IP options is
disabled (--ip-tracing-option-type=0). Avoid these unnecessary lookups
in case the feature is disabled.

Fixes: a860170c107d ("feat: add IPv4 packet tracing via IP options")
Signed-off-by: Tobias Klauser <tobias@cilium.io>

bpf/lib/trace.h
@@ -247,7 +247,7 @@ _send_trace_notify(struct __ctx_buff *ctx, enum trace_point obs_point,
 		.reason		= reason,
 		.flags		= flags,
 		.ifindex	= ifindex,
-		.ip_trace_id = ip_trace_id,
+		.ip_trace_id	= ip_trace_id,
 	};
 	memset(&msg.orig_ip6, 0, sizeof(union v6addr));
 
@@ -299,7 +299,7 @@ _send_trace_notify4(struct __ctx_buff *ctx, enum trace_point obs_point,
 		.ifindex	= ifindex,
 		.flags		= flags,
 		.orig_ip4	= orig_addr,
-		.ip_trace_id = ip_trace_id,
+		.ip_trace_id	= ip_trace_id,
 	};
 
 	ctx_event_output(ctx, &cilium_events,
@@ -349,7 +349,7 @@ _send_trace_notify6(struct __ctx_buff *ctx, enum trace_point obs_point,
 		.reason		= reason,
 		.ifindex	= ifindex,
 		.flags		= flags,
-		.ip_trace_id = ip_trace_id,
+		.ip_trace_id	= ip_trace_id,
 	};
 
 	ipv6_addr_copy(&msg.orig_ip6, orig_addr);

bpf/lib/trace_helpers.h
@@ -45,10 +45,8 @@ check_and_store_ip_trace_id(struct __ctx_buff *ctx)
 	__s64 trace_id = 0;
 	int ret;
 
-	if (CONFIG(tracing_ip_option_type) == 0) {
-		bpf_trace_id_set(0);
+	if (CONFIG(tracing_ip_option_type) == 0)
 		return;
-	}
 
 	ret = trace_id_from_ctx(ctx, &trace_id, CONFIG(tracing_ip_option_type));
 	if (IS_ERR(ret))
@@ -59,5 +57,7 @@ check_and_store_ip_trace_id(struct __ctx_buff *ctx)
 
 static __always_inline __u64 load_ip_trace_id(void)
 {
+	if (CONFIG(tracing_ip_option_type) == 0)
+		return 0;
 	return bpf_trace_id_get();
 }