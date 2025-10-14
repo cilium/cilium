f8df778171dae42ce86db8420d29a897b1e06f59
Jarno Rajahalme <jarno@isovalent.com>
2025-10-14 10:54:49 +0200

    policy: Pre-parse labelIdentitySelector
    Use the cached string and requirements with pre-parsed labels instead of
    api.EndpointSelector in labelIdentitySelector.
    
    labels.LabelArray is changed to add HasLabel(), GetLabel() and
    LookupLabel() functions that take a pre-parsed Label as an argument
    instead of a string.
    
    This makes CIDR policy resolution in BenchmarkResolveCIDRPolicyRules 70%
    faster, with 90% less memory used and 95% less allocations.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

---
+++ pkg/k8s/selector/requirements.go
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package selector
+
+import (
+	"log/slog"
+	"strconv"
+
+	"github.com/cilium/cilium/pkg/container/set"
+	k8sLbls "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/labels"
+	"github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/selection"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
+
+// Version of k8sLbls.Requirement where the key is pre-parsed and values are stored as a Set optimal
+// for single-valued values.
+type Requirement struct {
+	key      labels.Label       // pre-parsed Label
+	operator selection.Operator // requirement operator
+	values   set.Set[string]    // usually only one value
+}
+
+// Requirements is AND of all requirements.
+type Requirements []Requirement
+
+func (r *Requirement) HasValue(value string) bool {
+	return r.values.Has(value)
+}
+
+func FromK8sRequirements(k8sReqs k8sLbls.Requirements) Requirements {
+	reqs := make(Requirements, 0, len(k8sReqs))
+	for _, req := range k8sReqs {
+		reqs = append(reqs, Requirement{
+			key:      labels.ParseSelectDotLabel(req.Key()),
+			operator: req.Operator(),
+			values:   set.NewSet[string](req.ShallowValues()...),
+		})
+	}
+	return reqs
+}
+
+// Matches returns true if the Requirement matches the input Labels.
+// This is structurally the same as k8sLbls.Requirement.Matches(),
+// but takes LabelArray as an argument instead of a string.
+func (r *Requirement) Matches(logger *slog.Logger, ls labels.LabelArray) bool {
+	switch r.operator {
+	case selection.In, selection.Equals, selection.DoubleEquals:
+		val, exists := ls.LookupLabel(r.key)
+		if !exists {
+			return false
+		}
+		return r.HasValue(val)
+	case selection.NotIn, selection.NotEquals:
+		val, exists := ls.LookupLabel(r.key)
+		if !exists {
+			return true
+		}
+		return !r.HasValue(val)
+	case selection.Exists:
+		return ls.HasLabel(r.key)
+	case selection.DoesNotExist:
+		return !ls.HasLabel(r.key)
+	case selection.GreaterThan, selection.LessThan:
+		val, exists := ls.LookupLabel(r.key)
+		if !exists {
+			return false
+		}
+		lsValue, err := strconv.ParseInt(val, 10, 64)
+		if err != nil {
+			logger.Info(
+				"ParseInt failed",
+				logfields.Error, err,
+				logfields.EndpointLabelSelector, ls,
+				logfields.Value, val,
+			)
+			return false
+		}
+
+		// There should be only one string value in r.values, and can be converted to an
+		// integer.
+		if r.values.Len() != 1 {
+			logger.Info(
+				"Invalid values count for 'Gt', 'Lt' operators, exactly one value is required",
+				logfields.Label, r.key,
+				logfields.Value, r.values,
+				logfields.LenEntries, r.values.Len())
+			return false
+		}
+
+		var rValue int64
+		for val := range r.values.Members() {
+			rValue, err = strconv.ParseInt(val, 10, 64)
+			if err != nil {
+				logger.Info(
+					"ParseInt failed, for 'Gt', 'Lt' operators, the value must be an integer",
+					logfields.Error, err,
+					logfields.Label, r.key,
+					logfields.Value, val,
+				)
+				return false
+			}
+		}
+		return (r.operator == selection.GreaterThan && lsValue > rValue) || (r.operator == selection.LessThan && lsValue < rValue)
+	default:
+		return false
+	}
+}
+
+func (reqs Requirements) Matches(logger *slog.Logger, labels labels.LabelArray) bool {
+	for i := range reqs {
+		if !reqs[i].Matches(logger, labels) {
+			return false
+		}
+	}
+	return true
+}
