89b618b9f693fbe1a03a00df2658d5132778bf88
Robin Gögge <r.goegge@isovalent.com>
2025-10-14 11:34:12 +0200

    pkg/ztunnel: add scaffolding for ztunnel Cell
    Introduce basic controlplane structure for ztunnel integration using
    a new Cell in the Hive framework. This provides the foundation for
    implementing ztunnel control logic. It also sets new adds new codeowners
    for the ztunnel package.
    
    Signed-off-by: Robin Gögge <r.goegge@isovalent.com>

---
+++ pkg/ztunnel/cell.go
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package ztunnel
+
+import (
+	"fmt"
+
+	"github.com/cilium/hive/cell"
+
+	"github.com/cilium/cilium/pkg/clustermesh"
+	"github.com/cilium/cilium/pkg/ztunnel/config"
+)
+
+// Cell starts ztunnel related control-plane components.
+var Cell = cell.Module(
+	"ztunnel",
+	"ztunnel related control-plane components",
+	cell.Config(config.DefaultConfig),
+	cell.Invoke(validateConfig),
+)
+
+type ztunnelParams struct {
+	cell.In
+
+	Config      config.Config
+	ClusterMesh *clustermesh.ClusterMesh
+}
+
+func validateConfig(params ztunnelParams) error {
+	if !params.Config.EnableZTunnel {
+		return nil
+	}
+
+	if params.ClusterMesh != nil {
+		return fmt.Errorf("ztunnel is not compatible with clustermesh")
+	}
+
+	return nil
+}
