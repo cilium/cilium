f32e40a7890692f37dfcd6645ce8f38ed7b42440
Arthur Outhenin-Chalandre <git@mrfreezeex.fr>
2025-10-17 01:17:29 +0200

    k8s: allow to customise CRD version checking during CRD install
    Wrap existing version check in a function call so that callers would be
    able to use those helpers with a different need upgrade logic.
    
    Also improve a bit the code to clarify the CRD variable name (currentCRD
    and targetCRD instead of crd and targetCRD) and add crdName in
    logfields.
    
    Signed-off-by: Arthur Outhenin-Chalandre <git@mrfreezeex.fr>

--- pkg/k8s/apis/crdhelpers/register.go
+++ pkg/k8s/apis/crdhelpers/register.go
@@ -22,30 +22,31 @@ import (
 	"github.com/cilium/cilium/pkg/versioncheck"
 )
 
+type NeedUpdateCRDFunc func(currentCRD, targetCRD *apiextensionsv1.CustomResourceDefinition) (bool, error)
+
 // CreateUpdateCRD ensures the CRD object is installed into the K8s cluster. It
 // will create or update the CRD and its validation schema as necessary. This
 // function only accepts v1 CRD objects.
 func CreateUpdateCRD(
 	logger *slog.Logger,
 	clientset apiextensionsclient.Interface,
-	crd *apiextensionsv1.CustomResourceDefinition,
+	targetCRD *apiextensionsv1.CustomResourceDefinition,
 	poller poller,
-	crdSchemaVersionLabelKey string,
-	minCRDSchemaVersion semver.Version,
+	needsUpdateCRDFunc NeedUpdateCRDFunc,
 ) error {
-	scopedLog := logger.With(logfields.Name, crd.Name)
+	scopedLog := logger.With(logfields.Name, targetCRD.Name)
 
 	v1CRDClient := clientset.ApiextensionsV1()
-	clusterCRD, err := v1CRDClient.CustomResourceDefinitions().Get(
+	currentCRD, err := v1CRDClient.CustomResourceDefinitions().Get(
 		context.TODO(),
-		crd.ObjectMeta.Name,
+		targetCRD.ObjectMeta.Name,
 		metav1.GetOptions{})
 	if errors.IsNotFound(err) {
 		scopedLog.Info("Creating CRD (CustomResourceDefinition)...")
 
-		clusterCRD, err = v1CRDClient.CustomResourceDefinitions().Create(
+		currentCRD, err = v1CRDClient.CustomResourceDefinitions().Create(
 			context.TODO(),
-			crd,
+			targetCRD,
 			metav1.CreateOptions{})
 		// This occurs when multiple agents race to create the CRD. Since another has
 		// created it, it will also update it, hence the non-error return.
@@ -57,10 +58,10 @@ func CreateUpdateCRD(
 		return err
 	}
 
-	if err := updateV1CRD(scopedLog, crd, clusterCRD, v1CRDClient, poller, crdSchemaVersionLabelKey, minCRDSchemaVersion); err != nil {
+	if err := updateV1CRD(scopedLog, targetCRD, currentCRD, v1CRDClient, poller, needsUpdateCRDFunc); err != nil {
 		return err
 	}
-	if err := waitForV1CRD(scopedLog, clusterCRD, v1CRDClient, poller); err != nil {
+	if err := waitForV1CRD(scopedLog, currentCRD, v1CRDClient, poller); err != nil {
 		return err
 	}
 
@@ -69,49 +70,53 @@ func CreateUpdateCRD(
 	return nil
 }
 
-func needsUpdateV1(
-	clusterCRD *apiextensionsv1.CustomResourceDefinition,
+func NeedsUpdateV1Factory(
 	crdSchemaVersionLabelKey string,
 	minCRDSchemaVersion semver.Version,
-) bool {
-	if clusterCRD.Spec.Versions[0].Schema == nil {
-		// no validation detected
-		return true
-	}
-	v, ok := clusterCRD.Labels[crdSchemaVersionLabelKey]
-	if !ok {
-		// no schema version detected
-		return true
-	}
+) NeedUpdateCRDFunc {
+	return func(_, currentCRD *apiextensionsv1.CustomResourceDefinition) (bool, error) {
+		if currentCRD.Spec.Versions[0].Schema == nil {
+			// no validation detected
+			return true, nil
+		}
+		v, ok := currentCRD.Labels[crdSchemaVersionLabelKey]
+		if !ok {
+			// no schema version detected
+			return true, nil
+		}
 
-	clusterVersion, err := versioncheck.Version(v)
-	if err != nil || clusterVersion.LT(minCRDSchemaVersion) {
-		// version in cluster is either unparsable or smaller than current version
-		return true
-	}
+		currentVersion, err := versioncheck.Version(v)
+		if err != nil || currentVersion.LT(minCRDSchemaVersion) {
+			// version in cluster is either unparsable or smaller than current version
+			return true, nil
+		}
 
-	return false
+		return false, nil
+	}
 }
 
 func updateV1CRD(
 	scopedLog *slog.Logger,
-	crd, clusterCRD *apiextensionsv1.CustomResourceDefinition,
+	targetCRD, currentCRD *apiextensionsv1.CustomResourceDefinition,
 	client v1client.CustomResourceDefinitionsGetter,
 	poller poller,
-	crdSchemaVersionLabelKey string,
-	minCRDSchemaVersion semver.Version,
+	needsUpdateCRDFunc NeedUpdateCRDFunc,
 ) error {
 	scopedLog.Debug("Checking if CRD (CustomResourceDefinition) needs update...")
+	needsUpdate, err := needsUpdateCRDFunc(targetCRD, currentCRD)
+	if err != nil {
+		return err
+	}
 
-	if crd.Spec.Versions[0].Schema != nil && needsUpdateV1(clusterCRD, crdSchemaVersionLabelKey, minCRDSchemaVersion) {
+	if targetCRD.Spec.Versions[0].Schema != nil && needsUpdate {
 		scopedLog.Info("Updating CRD (CustomResourceDefinition)...")
 
 		// Update the CRD with the validation schema.
 		err := poller.Poll(500*time.Millisecond, 60*time.Second, func() (bool, error) {
 			var err error
-			clusterCRD, err = client.CustomResourceDefinitions().Get(
+			currentCRD, err = client.CustomResourceDefinitions().Get(
 				context.TODO(),
-				crd.ObjectMeta.Name,
+				targetCRD.ObjectMeta.Name,
 				metav1.GetOptions{})
 			if err != nil {
 				return false, err
@@ -120,21 +125,25 @@ func updateV1CRD(
 			// This seems too permissive but we only get here if the version is
 			// different per needsUpdate above. If so, we want to update on any
 			// validation change including adding or removing validation.
-			if needsUpdateV1(clusterCRD, crdSchemaVersionLabelKey, minCRDSchemaVersion) {
+			needsUpdate, err := needsUpdateCRDFunc(targetCRD, currentCRD)
+			if err != nil {
+				return false, err
+			}
+			if needsUpdate {
 				scopedLog.Debug("CRD validation is different, updating it...")
 
-				clusterCRD.ObjectMeta.Labels = crd.ObjectMeta.Labels
-				clusterCRD.Spec = crd.Spec
+				currentCRD.ObjectMeta.Labels = targetCRD.ObjectMeta.Labels
+				currentCRD.Spec = targetCRD.Spec
 
 				// Even though v1 CRDs omit this field by default (which also
 				// means it's false) it is still carried over from the previous
 				// CRD. Therefore, we must set this to false explicitly because
 				// the apiserver will carry over the old value (true).
-				clusterCRD.Spec.PreserveUnknownFields = false
+				currentCRD.Spec.PreserveUnknownFields = false
 
 				_, err := client.CustomResourceDefinitions().Update(
 					context.TODO(),
-					clusterCRD,
+					currentCRD,
 					metav1.UpdateOptions{})
 				switch {
 				case errors.IsConflict(err): // Occurs as Operators race to update CRDs.
