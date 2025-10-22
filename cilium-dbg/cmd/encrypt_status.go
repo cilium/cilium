ed81a8f76679ad5c3f4c889487e406f1f1eb6781
Bernardo Soares <bersoare@cisco.com>
2025-10-22 09:17:46 +0000

    encrypt status: also check tcx attachment on interfaces
    [ upstream commit ae5d15c3eb20a9bc01b5c3bee632641c1ae223f2 ]
    
    right now, encrypt status doesn't show any interfaces
    when encryption is enabled and tcx attachment is used.
    in this commit, in addition to tc filter attach,
    look for tcx attachments to determine the decryption
    interface
    
    Signed-off-by: Bernardo Soares <bersoare@cisco.com>
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- cilium-dbg/cmd/encrypt_status.go
+++ cilium-dbg/cmd/encrypt_status.go
@@ -15,6 +15,9 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/vishvananda/netlink"
 
+	"github.com/cilium/ebpf"
+	ebpf_link "github.com/cilium/ebpf/link"
+
 	"github.com/cilium/cilium/api/v1/client/daemon"
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/command"
@@ -201,6 +204,32 @@ func isDecryptionInterface(link netlink.Link) (bool, error) {
 			}
 		}
 	}
+
+	progs, err := ebpf_link.QueryPrograms(
+		ebpf_link.QueryOptions{
+			Target: link.Attrs().Index,
+			Attach: ebpf.AttachTCXIngress,
+		},
+	)
+	if err != nil {
+		// probably not supported
+		return false, nil
+	}
+
+	for _, p := range progs.Programs {
+		prog, err := ebpf.NewProgramFromID(p.ID)
+		if err != nil {
+			return false, fmt.Errorf("failed to find program: %w for %d", err, p.ID)
+		}
+
+		if progInfo, err := prog.Info(); err == nil {
+			if strings.Contains(progInfo.Name, "cil_from_network") ||
+				strings.Contains(progInfo.Name, "cil_from_netdev") {
+				return true, nil
+			}
+		}
+	}
+
 	return false, nil
 }
 
