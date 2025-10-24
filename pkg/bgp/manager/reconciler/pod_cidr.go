99678b4f5238ea7d7e4ca34379d8b039fdecee3a
Rastislav Szabo <rastislav.szabo@isovalent.com>
2025-10-24 16:23:47 +0200

    bgp: Rename reconciler groups from reconciler-v2 to reconciler
    Signed-off-by: Rastislav Szabo <rastislav.szabo@isovalent.com>

--- pkg/bgp/manager/reconciler/pod_cidr.go
+++ pkg/bgp/manager/reconciler/pod_cidr.go
@@ -20,7 +20,7 @@ import (
 type PodCIDRReconcilerOut struct {
 	cell.Out
 
-	Reconciler ConfigReconciler `group:"bgp-config-reconciler-v2"`
+	Reconciler ConfigReconciler `group:"bgp-config-reconciler"`
 }
 
 type PodCIDRReconcilerIn struct {
