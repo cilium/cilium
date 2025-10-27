c07acb8ad33cbb4c980f1dedf299f79effe9f371
Louis DeLosSantos <louis.delos@isovalent.com>
2025-10-27 12:22:11 -0400

    pkg/ztunnel: certificate authority server
    Add the necessary scaffolding and xDS certificate authority server for
    usage with ztunnel.
    
    Following the pattern of IPSec's key injection, we update the Cilium
    daemonset to wait on a secret-backed volume mount named
    "cilium-ztunnel-secrets".
    
    The secret includes 4 items, all of which are PEM encoded.
        1. Bootstrap Certificate (bootstrap-root.crt)
        2. Bootstrap Private Key (bootstrap-private.key)
        3. CA Certificate (ca-root.crt)
        4. CA Private Key (ca-private.key)
    
    The bootstrap items are used to boostrap a TLS connection between
    ztunnel and the CA server introduced into Cilium.
    
    The CA items are used to create and sign certificates given
    a certificate signing request from ztunnel.
    
    The CA server implements the necessary gRPC server and methods expected
    by ZTunnel. See the `github.com/cilium/cilium/ztunnel/pb` package for more
    details.
    
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>
    Signed-off-by: Robin Gögge <r.goegge@isovalent.com>
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- pkg/ztunnel/cell.go
+++ pkg/ztunnel/cell.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cilium/hive/cell"
 
 	"github.com/cilium/cilium/pkg/ztunnel/config"
+	"github.com/cilium/cilium/pkg/ztunnel/xds"
 )
 
 // Cell starts ztunnel related control-plane components.
@@ -14,4 +15,7 @@ var Cell = cell.Module(
 	"ztunnel",
 	"ztunnel related control-plane components",
 	cell.Config(config.DefaultConfig),
+
+	// XDS control plane for ztunnel
+	xds.Cell,
 )
