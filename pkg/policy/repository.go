cilium/cilium

commit df42a7d819da710547078f004848369a170fed24

policy: track rules by resource, not by labels
When upserting a CNP or KNP, we identify existing rules in the
repository by a set of labels. However, evaluating this set of labels is
expensive, especially as we must check against all label selectors every
time we want to add or remove a policy.

Rather than using label selectors internally, track policies by
owning resource, much the way that prefixes are tracked in the ipcache.
Then, when upserting policies, the set of existing rules attached to a
given resource can be easily retrieved.

The existing behavior is preserved, as it is also exposed via the local
gRPC API. However, the k8s handlers no longer use it.

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

pkg/policy/repository.go
@@ -4,9 +4,11 @@
 package policy
 
 import (
+	"cmp"
 	"context"
 	"encoding/json"
 	"fmt"
+	"slices"
 	"sync"
 	"sync/atomic"
 
@@ -17,6 +19,7 @@ import (
 	"github.com/cilium/cilium/pkg/eventqueue"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
+	ipcachetypes "github.com/cilium/cilium/pkg/ipcache/types"
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
@@ -108,7 +111,14 @@ func (p *policyContext) SetDeny(deny bool) bool {
 type Repository struct {
 	// Mutex protects the whole policy tree
 	Mutex lock.RWMutex
-	rules ruleSlice
+
+	rules           map[ruleKey]*rule
+	rulesByResource map[ipcachetypes.ResourceID]map[ruleKey]*rule
+
+	// We will need a way to synthesize a rule key for rules without a resource;
+	// these are - in practice - very rare, as they only come from the local API,
+	// never via k8s.
+	nextID uint
 
 	// revision is the revision of the policy repository. It will be
 	// incremented whenever the policy repository is changed.
@@ -189,9 +199,11 @@ func NewStoppedPolicyRepository(
 ) *Repository {
 	selectorCache := NewSelectorCache(idAllocator, idCache)
 	repo := &Repository{
-		selectorCache: selectorCache,
-		certManager:   certManager,
-		secretManager: secretManager,
+		rules:           make(map[ruleKey]*rule),
+		rulesByResource: make(map[ipcachetypes.ResourceID]map[ruleKey]*rule),
+		selectorCache:   selectorCache,
+		certManager:     certManager,
+		secretManager:   secretManager,
 	}
 	repo.revision.Store(1)
 	repo.policyCache = NewPolicyCache(repo, true)
@@ -263,7 +275,15 @@ func (p *Repository) ResolveL4IngressPolicy(ctx *SearchContext) (L4PolicyMap, er
 		repo: p,
 		ns:   ctx.To.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PodNamespaceLabel),
 	}
-	result, err := p.rules.resolveL4IngressPolicy(&policyCtx, ctx)
+	rules := make(ruleSlice, 0, len(p.rules))
+	for _, rule := range p.rules {
+		rules = append(rules, rule)
+	}
+	// Sort for unit tests
+	slices.SortFunc[ruleSlice](rules, func(a, b *rule) int {
+		return cmp.Compare(a.key.idx, b.key.idx)
+	})
+	result, err := rules.resolveL4IngressPolicy(&policyCtx, ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -286,7 +306,14 @@ func (p *Repository) ResolveL4EgressPolicy(ctx *SearchContext) (L4PolicyMap, err
 		repo: p,
 		ns:   ctx.From.Get(labels.LabelSourceK8sKeyPrefix + k8sConst.PodNamespaceLabel),
 	}
-	result, err := p.rules.resolveL4EgressPolicy(&policyCtx, ctx)
+	rules := make(ruleSlice, 0, len(p.rules))
+	for _, rule := range p.rules {
+		rules = append(rules, rule)
+	}
+	slices.SortFunc[ruleSlice](rules, func(a, b *rule) int {
+		return cmp.Compare(a.key.idx, b.key.idx)
+	})
+	result, err := rules.resolveL4EgressPolicy(&policyCtx, ctx)
 
 	if err != nil {
 		return nil, err
@@ -397,20 +424,86 @@ func (p *Repository) Add(r api.Rule) (uint64, map[uint16]struct{}, error) {
 // AddListLocked inserts a rule into the policy repository with the repository already locked
 // Expects that the entire rule list has already been sanitized.
 func (p *Repository) AddListLocked(rules api.Rules) (ruleSlice, uint64) {
-
-	newList := make(ruleSlice, len(rules))
-	for i := range rules {
+	newRules := make(ruleSlice, 0, len(rules))
+	for _, r := range rules {
 		newRule := &rule{
-			Rule:     *rules[i],
+			Rule:     *r,
+			key:      ruleKey{idx: p.nextID},
 			metadata: newRuleMetadata(),
 		}
-		newList[i] = newRule
+		newRules = append(newRules, newRule)
+		p.insert(newRule)
+		p.nextID++
 	}
 
-	p.rules = append(p.rules, newList...)
-	p.BumpRevision()
-	metrics.Policy.Add(float64(len(newList)))
-	return newList, p.GetRevision()
+	return newRules, p.BumpRevision()
+}
+
+// ReplaceByResourceLocked replaces all rules that belong to a given resource with a
+// new set. The set of rules added and removed is returned, along with the new revision number.
+// Resource must not be empty
+func (p *Repository) ReplaceByResourceLocked(rules api.Rules, resource ipcachetypes.ResourceID) (newRules ruleSlice, oldRules ruleSlice, revision uint64) {
+	if len(resource) == 0 {
+		// This should never ever be hit, as the caller should have already validated the resource.
+		// However, if it does happen, it means something very wrong has happened and we are at risk
+		// of removing all network policies. So, we must panic rather than risk disabling network security.
+		panic("may not replace API rules with an empty resource")
+	}
+
+	if old, ok := p.rulesByResource[resource]; ok {
+		oldRules = make(ruleSlice, 0, len(old))
+		for key, oldRule := range old {
+			oldRules = append(oldRules, oldRule)
+			p.del(key)
+		}
+	}
+
+	newRules = make(ruleSlice, 0, len(rules))
+
+	if len(rules) > 0 {
+		p.rulesByResource[resource] = make(map[ruleKey]*rule, len(rules))
+
+		for i, r := range rules {
+			newRule := &rule{
+				Rule:     *r,
+				key:      ruleKey{resource: resource, idx: uint(i)},
+				metadata: newRuleMetadata(),
+			}
+			newRules = append(newRules, newRule)
+			p.insert(newRule)
+		}
+	}
+
+	return newRules, oldRules, p.BumpRevision()
+}
+
+func (p *Repository) insert(r *rule) {
+	p.rules[r.key] = r
+	rid := r.key.resource
+	if len(rid) > 0 {
+		if p.rulesByResource[rid] == nil {
+			p.rulesByResource[rid] = map[ruleKey]*rule{}
+		}
+		p.rulesByResource[rid][r.key] = r
+	}
+
+	metrics.Policy.Inc()
+}
+
+func (p *Repository) del(key ruleKey) {
+	if p.rules[key] == nil {
+		return
+	}
+	delete(p.rules, key)
+
+	rid := key.resource
+	if len(rid) > 0 && p.rulesByResource[rid] != nil {
+		delete(p.rulesByResource[rid], key)
+		if len(p.rulesByResource[rid]) == 0 {
+			delete(p.rulesByResource, rid)
+		}
+	}
+	metrics.Policy.Dec()
 }
 
 // removeIdentityFromRuleCaches removes the identity from the selector cache
@@ -502,27 +595,37 @@ func (r ruleSlice) UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpoints
 // contain the specified labels. Returns the revision of the policy repository
 // after deleting the rules, as well as now many rules were deleted.
 func (p *Repository) DeleteByLabelsLocked(lbls labels.LabelArray) (ruleSlice, uint64, int) {
-
-	deleted := 0
-	new := p.rules[:0]
 	deletedRules := ruleSlice{}
 
-	for _, r := range p.rules {
-		if !r.Labels.Contains(lbls) {
-			new = append(new, r)
-		} else {
+	for key, r := range p.rules {
+		if r.Labels.Contains(lbls) {
 			deletedRules = append(deletedRules, r)
-			deleted++
+			p.del(key)
 		}
 	}
+	l := len(deletedRules)
 
-	if deleted > 0 {
+	if l > 0 {
 		p.BumpRevision()
-		p.rules = new
-		metrics.Policy.Sub(float64(deleted))
 	}
 
-	return deletedRules, p.GetRevision(), deleted
+	return deletedRules, p.GetRevision(), l
+}
+
+func (p *Repository) DeleteByResourceLocked(rid ipcachetypes.ResourceID) (ruleSlice, uint64) {
+	rules := p.rulesByResource[rid]
+	if len(rules) == 0 {
+		delete(p.rulesByResource, rid)
+		return nil, p.GetRevision()
+	}
+
+	deletedRules := make(ruleSlice, 0, len(rules))
+	for key, rule := range rules {
+		p.del(key)
+		deletedRules = append(deletedRules, rule)
+	}
+
+	return deletedRules, p.BumpRevision()
 }
 
 // DeleteByLabels deletes all rules in the policy repository which contain the
@@ -611,9 +714,9 @@ func (p *Repository) Empty() bool {
 }
 
 // BumpRevision allows forcing policy regeneration
-func (p *Repository) BumpRevision() {
+func (p *Repository) BumpRevision() uint64 {
 	metrics.PolicyRevision.Inc()
-	p.revision.Add(1)
+	return p.revision.Add(1)
 }
 
 // GetRulesList returns the current policy

daemon/cmd/policy.go
@@ -279,6 +279,9 @@ func (d *Daemon) policyAdd(sourceRules policyAPI.Rules, opts *policy.AddOptions,
 	// updated.
 	var policySelectionWG sync.WaitGroup
 
+	// newRev is the new policy revision after rule updates
+	var newRev uint64
+
 	// Get all endpoints at the time rules were added / updated so we can figure
 	// out which endpoints to regenerate / bump policy revision.
 	allEndpoints := d.endpointManager.GetPolicyEndpoints()
@@ -289,37 +292,69 @@ func (d *Daemon) policyAdd(sourceRules policyAPI.Rules, opts *policy.AddOptions,
 
 	endpointsToRegen := policy.NewEndpointSet(nil)
 
-	if opts != nil {
-		if opts.Replace {
-			for _, r := range sourceRules {
-				oldRules := d.policy.SearchRLocked(r.Labels)
+	// Policies can be upserted one of two ways: by labels or by resource.
+	// Here we replace by resource if specified.
+	// This block of code is, sadly, copy-pasty because DeleteByLabels / AddList return an unexported type.
+	if opts != nil && opts.ReplaceByResource && len(opts.Resource) > 0 {
+		// Update the policy repository with the new rules
+		addedRules, deletedRules, rev := d.policy.ReplaceByResourceLocked(sourceRules, opts.Resource)
+		newRev = rev
+
+		if len(deletedRules) > 0 {
+			// Record any prefix allocations that should be deleted
+			removedPrefixes = append(removedPrefixes, policy.GetCIDRPrefixes(deletedRules.AsPolicyRules())...)
+
+			// Determine which endpoints, if any, need to be regenerated due to removing these rules
+			deletedRules.UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpointsToRegen, &policySelectionWG)
+		}
+
+		// The information needed by the caller is available at this point, signal
+		// accordingly.
+		resChan <- &PolicyAddResult{
+			newRev: newRev,
+			err:    nil,
+		}
+
+		// Determine which endpoints, if any, need to be regenerated due to being selected by a new rule
+		addedRules.UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpointsToRegen, &policySelectionWG)
+
+	} else {
+		// Replacing by labels
+		// This only happens if a policy is specified via the gRPC API. It is much less efficient
+		// due to needing to scan the entire repository to find matching labels.
+		if opts != nil {
+			if opts.Replace {
+				for _, r := range sourceRules {
+					oldRules := d.policy.SearchRLocked(r.Labels)
+					removedPrefixes = append(removedPrefixes, policy.GetCIDRPrefixes(oldRules)...)
+					if len(oldRules) > 0 {
+						deletedRules, _, _ := d.policy.DeleteByLabelsLocked(r.Labels)
+						deletedRules.UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpointsToRegen, &policySelectionWG)
+					}
+				}
+			}
+			if len(opts.ReplaceWithLabels) > 0 {
+				oldRules := d.policy.SearchRLocked(opts.ReplaceWithLabels)
 				removedPrefixes = append(removedPrefixes, policy.GetCIDRPrefixes(oldRules)...)
 				if len(oldRules) > 0 {
-					deletedRules, _, _ := d.policy.DeleteByLabelsLocked(r.Labels)
+					deletedRules, _, _ := d.policy.DeleteByLabelsLocked(opts.ReplaceWithLabels)
 					deletedRules.UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpointsToRegen, &policySelectionWG)
 				}
 			}
 		}
-		if len(opts.ReplaceWithLabels) > 0 {
-			oldRules := d.policy.SearchRLocked(opts.ReplaceWithLabels)
-			removedPrefixes = append(removedPrefixes, policy.GetCIDRPrefixes(oldRules)...)
-			if len(oldRules) > 0 {
-				deletedRules, _, _ := d.policy.DeleteByLabelsLocked(opts.ReplaceWithLabels)
-				deletedRules.UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpointsToRegen, &policySelectionWG)
-			}
+
+		addedRules, rev := d.policy.AddListLocked(sourceRules)
+		newRev = rev
+
+		// The information needed by the caller is available at this point, signal
+		// accordingly.
+		resChan <- &PolicyAddResult{
+			newRev: newRev,
+			err:    nil,
 		}
+
+		addedRules.UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpointsToRegen, &policySelectionWG)
 	}
-
-	addedRules, newRev := d.policy.AddListLocked(sourceRules)
-
-	// The information needed by the caller is available at this point, signal
-	// accordingly.
-	resChan <- &PolicyAddResult{
-		newRev: newRev,
-		err:    nil,
-	}
-
-	addedRules.UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpointsToRegen, &policySelectionWG)
 
 	d.policy.Mutex.Unlock()
 
@@ -536,24 +571,42 @@ func (d *Daemon) policyDelete(labels labels.LabelArray, opts *policy.DeleteOptio
 
 	endpointsToRegen := policy.NewEndpointSet(nil)
 
-	deletedRules, rev, deleted := d.policy.DeleteByLabelsLocked(labels)
+	var deleted int
+	var rev uint64
+	var prefixes []netip.Prefix
 
-	// Return an error if a label filter was provided and there are no
-	// rules matching it. A deletion request for all policy entries should
-	// not fail if no policies are loaded.
-	if len(deletedRules) == 0 && len(labels) != 0 {
-		rev := d.policy.GetRevision()
-		d.policy.Mutex.Unlock()
+	if opts.DeleteByResource && len(opts.Resource) > 0 {
+		deletedRules, newRev := d.policy.DeleteByResourceLocked(opts.Resource)
+		rev = newRev
+		deleted = len(deletedRules)
 
-		err := api.New(DeletePolicyNotFoundCode, "policy not found")
+		deletedRules.UpdateRulesEndpointsCaches(epsToBumpRevision, endpointsToRegen, &policySelectionWG)
+		prefixes = policy.GetCIDRPrefixes(deletedRules.AsPolicyRules())
+	} else {
 
-		res <- &PolicyDeleteResult{
-			newRev: rev,
-			err:    err,
+		deletedRules, newRev, _ := d.policy.DeleteByLabelsLocked(labels)
+		rev = newRev
+		deleted = len(deletedRules)
+
+		// Return an error if a label filter was provided and there are no
+		// rules matching it. A deletion request for all policy entries should
+		// not fail if no policies are loaded.
+		if len(deletedRules) == 0 && len(labels) != 0 {
+			rev := d.policy.GetRevision()
+			d.policy.Mutex.Unlock()
+
+			err := api.New(DeletePolicyNotFoundCode, "policy not found")
+
+			res <- &PolicyDeleteResult{
+				newRev: rev,
+				err:    err,
+			}
+			return
 		}
-		return
+
+		deletedRules.UpdateRulesEndpointsCaches(epsToBumpRevision, endpointsToRegen, &policySelectionWG)
+		prefixes = policy.GetCIDRPrefixes(deletedRules.AsPolicyRules())
 	}
-	deletedRules.UpdateRulesEndpointsCaches(epsToBumpRevision, endpointsToRegen, &policySelectionWG)
 
 	res <- &PolicyDeleteResult{
 		newRev: rev,
@@ -568,7 +621,6 @@ func (d *Daemon) policyDelete(labels labels.LabelArray, opts *policy.DeleteOptio
 	// We don't treat failures to clean up identities as API failures,
 	// because the policy can still successfully be updated. We're just
 	// not appropriately performing garbage collection.
-	prefixes := policy.GetCIDRPrefixes(deletedRules.AsPolicyRules())
 	log.WithField("prefixes", prefixes).Debug("Policy deleted via API, found prefixes...")
 
 	// Updates to the datapath are serialized via the policy reaction queue.

pkg/policy/config.go
@@ -44,6 +44,7 @@ type AddOptions struct {
 	// ReplaceWithLabels if present indicates that existing rules with the
 	// given LabelArray should be deleted.
 	ReplaceWithLabels labels.LabelArray
+
 	// Generated should be set as true to signalize a the policy being inserted
 	// was generated by cilium-agent, e.g. dns poller.
 	Generated bool
@@ -58,6 +59,10 @@ type AddOptions struct {
 	// Resource provides the object ID for the underlying object that backs
 	// this information from 'source'.
 	Resource ipcacheTypes.ResourceID
+
+	// ReplaceByResource indicates the policy repository should replace any
+	// rules owned by the given Resource with the new set of rules
+	ReplaceByResource bool
 }
 
 // DeleteOptions are options which can be passed to PolicyDelete
@@ -68,4 +73,8 @@ type DeleteOptions struct {
 	// Resource provides the object ID for the underlying object that backs
 	// this information from 'source'.
 	Resource ipcacheTypes.ResourceID
+
+	// DeleteByResource should be true if the resource should be used to identify
+	// which rules should be deleted.
+	DeleteByResource bool
 }

pkg/policy/k8s/cilium_network_policy.go
@@ -138,10 +138,10 @@ func (p *policyWatcher) upsertCiliumNetworkPolicyV2(cnp *types.SlimCNP, initialR
 	rules, policyImportErr := cnp.Parse()
 	if policyImportErr == nil {
 		_, policyImportErr = p.policyManager.PolicyAdd(rules, &policy.AddOptions{
-			ReplaceWithLabels:   cnp.GetIdentityLabels(),
 			Source:              source.CustomResource,
 			ProcessingStartTime: initialRecvTime,
 			Resource:            resourceID,
+			ReplaceByResource:   true,
 		})
 	}
 
@@ -163,9 +163,10 @@ func (p *policyWatcher) deleteCiliumNetworkPolicyV2(cnp *types.SlimCNP, resource
 
 	scopedLog.Debug("Deleting CiliumNetworkPolicy")
 
-	_, err := p.policyManager.PolicyDelete(cnp.GetIdentityLabels(), &policy.DeleteOptions{
-		Source:   source.CustomResource,
-		Resource: resourceID,
+	_, err := p.policyManager.PolicyDelete(nil, &policy.DeleteOptions{
+		Source:           source.CustomResource,
+		Resource:         resourceID,
+		DeleteByResource: true,
 	})
 	if err == nil {
 		scopedLog.Info("Deleted CiliumNetworkPolicy")

pkg/policy/k8s/network_policy.go
@@ -36,13 +36,13 @@ func (p *policyWatcher) addK8sNetworkPolicyV1(k8sNP *slim_networkingv1.NetworkPo
 	}
 
 	opts := policy.AddOptions{
-		Replace: true,
-		Source:  source.Kubernetes,
+		Source: source.Kubernetes,
 		Resource: ipcacheTypes.NewResourceID(
 			ipcacheTypes.ResourceKindNetpol,
 			k8sNP.ObjectMeta.Namespace,
 			k8sNP.ObjectMeta.Name,
 		),
+		ReplaceByResource: true,
 	}
 	if _, err := p.policyManager.PolicyAdd(rules, &opts); err != nil {
 		metrics.PolicyChangeTotal.WithLabelValues(metrics.LabelValueOutcomeFail).Inc()
@@ -74,8 +74,9 @@ func (p *policyWatcher) deleteK8sNetworkPolicyV1(k8sNP *slim_networkingv1.Networ
 		logfields.K8sAPIVersion:        k8sNP.TypeMeta.APIVersion,
 		logfields.Labels:               logfields.Repr(labels),
 	})
-	if _, err := p.policyManager.PolicyDelete(labels, &policy.DeleteOptions{
-		Source: source.Kubernetes,
+	if _, err := p.policyManager.PolicyDelete(nil, &policy.DeleteOptions{
+		Source:           source.Kubernetes,
+		DeleteByResource: true,
 		Resource: ipcacheTypes.NewResourceID(
 			ipcacheTypes.ResourceKindNetpol,
 			k8sNP.ObjectMeta.Namespace,

pkg/policy/repository_deny_test.go
@@ -1577,7 +1577,7 @@ func TestRemoveIdentityFromRuleDenyCaches(t *testing.T) {
 		},
 	}})
 
-	addedRule := td.repo.rules[0]
+	addedRule := td.repo.rules[ruleKey{idx: 0}]
 
 	selectedEpLabels := labels.ParseSelectLabel("id=a")
 	selectedIdentity := identity.NewIdentity(54321, labels.Labels{selectedEpLabels.Key: selectedEpLabels})

pkg/policy/repository_test.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/identity"
+	ipcachetypes "github.com/cilium/cilium/pkg/ipcache/types"
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	"github.com/cilium/cilium/pkg/labels"
@@ -156,8 +157,7 @@ func TestComputePolicyEnforcementAndRules(t *testing.T) {
 	ing, egr, matchingRules = repo.computePolicyEnforcementAndRules(fooIdentity)
 	require.Equal(t, true, ing, "ingress policy enforcement should apply since ingress rule selects")
 	require.Equal(t, false, egr, "egress policy enforcement should not apply since no egress rules select")
-	require.EqualValues(t, fooIngressRule1, matchingRules[0].Rule, "returned matching rules did not match")
-	require.EqualValues(t, fooIngressRule2, matchingRules[1].Rule, "returned matching rules did not match")
+	require.ElementsMatch(t, matchingRules.AsPolicyRules(), api.Rules{&fooIngressRule1, &fooIngressRule2})
 
 	_, _, numDeleted := repo.DeleteByLabelsLocked(labels.LabelArray{fooIngressRule1Label})
 	require.Equal(t, 1, numDeleted)
@@ -300,8 +300,8 @@ func TestAddSearchDelete(t *testing.T) {
 
 	// search rule1,rule2
 	repo.Mutex.RLock()
-	require.EqualValues(t, api.Rules{&rule1, &rule2}, repo.SearchRLocked(lbls1))
-	require.EqualValues(t, api.Rules{&rule3}, repo.SearchRLocked(lbls2))
+	require.ElementsMatch(t, api.Rules{&rule1, &rule2}, repo.SearchRLocked(lbls1))
+	require.ElementsMatch(t, api.Rules{&rule3}, repo.SearchRLocked(lbls2))
 	repo.Mutex.RUnlock()
 
 	// delete rule1, rule2
@@ -2282,7 +2282,7 @@ func TestRemoveIdentityFromRuleCaches(t *testing.T) {
 		},
 	}})
 
-	addedRule := td.repo.rules[0]
+	addedRule := td.repo.rules[ruleKey{idx: 0}]
 
 	selectedEpLabels := labels.ParseSelectLabel("id=a")
 	selectedIdentity := identity.NewIdentity(54321, labels.Labels{selectedEpLabels.Key: selectedEpLabels})
@@ -2531,4 +2531,99 @@ func TestDefaultAllow(t *testing.T) {
 			require.Equal(t, itc.ruleC+etc.ruleC, len(matchingRules), "case ingress %d + egress %d: rule count should match", i, e)
 		}
 	}
+}
+
+func TestReplaceByResource(t *testing.T) {
+	repo := NewPolicyRepository(nil, nil, nil, nil)
+
+	numRules := 10
+	rules := make(api.Rules, 0, numRules)
+	lbls := make([]labels.Label, numRules)
+	for i := 0; i < numRules; i++ {
+		it := fmt.Sprintf("baz%d", i)
+		epSelector := api.NewESFromLabels(
+			labels.NewLabel(
+				"foo",
+				it,
+				labels.LabelSourceK8s,
+			),
+		)
+		lbls[i] = labels.NewLabel("tag3", it, labels.LabelSourceK8s)
+		rules = append(rules, &api.Rule{
+			EndpointSelector: epSelector,
+			Labels:           labels.LabelArray{lbls[i]},
+			Egress: []api.EgressRule{
+				{
+					EgressCommonRule: api.EgressCommonRule{
+						ToEndpoints: []api.EndpointSelector{
+							epSelector,
+						},
+					},
+				},
+			},
+		})
+	}
+
+	rulesMatch := func(s ruleSlice, rs api.Rules) {
+		t.Helper()
+		ss := make(api.Rules, 0, len(s))
+		for _, rule := range s {
+			ss = append(ss, &rule.Rule)
+		}
+		assert.ElementsMatch(t, ss, rs)
+	}
+	toSlice := func(m map[ruleKey]*rule) ruleSlice {
+		out := ruleSlice{}
+		for _, v := range m {
+			out = append(out, v)
+		}
+		return out
+	}
+
+	rID1 := ipcachetypes.ResourceID("res1")
+	rID2 := ipcachetypes.ResourceID("res2")
+
+	new, old, rev := repo.ReplaceByResourceLocked(rules[0:1], rID1)
+	assert.Len(t, new, 1)
+	assert.Len(t, old, 0)
+	assert.EqualValues(t, rev, 2)
+
+	// check basic bookkeeping
+	assert.Len(t, repo.rules, 1)
+	assert.Len(t, repo.rulesByResource, 1)
+	assert.Len(t, repo.rulesByResource[rID1], 1)
+	rulesMatch(toSlice(repo.rulesByResource[rID1]), rules[0:1])
+
+	// add second resource
+	new, old, rev = repo.ReplaceByResourceLocked(rules[1:3], rID2)
+
+	assert.Len(t, new, 2)
+	assert.Len(t, old, 0)
+	assert.EqualValues(t, rev, 3)
+
+	// check basic bookkeeping
+	assert.Len(t, repo.rules, 3)
+	assert.Len(t, repo.rulesByResource, 2)
+	assert.Len(t, repo.rulesByResource[rID1], 1)
+	assert.Len(t, repo.rulesByResource[rID2], 2)
+
+	// replace rid1 with new rules
+	new, old, _ = repo.ReplaceByResourceLocked(rules[3:5], rID1)
+	assert.Len(t, new, 2)
+	assert.Len(t, old, 1)
+
+	// check basic bookkeeping
+	assert.Len(t, repo.rules, 4)
+	assert.Len(t, repo.rulesByResource, 2)
+	assert.Len(t, repo.rulesByResource[rID1], 2)
+	assert.Len(t, repo.rulesByResource[rID2], 2)
+
+	rulesMatch(old, rules[0:1])
+	rulesMatch(new, rules[3:5])
+	rulesMatch(toSlice(repo.rulesByResource[rID1]), rules[3:5])
+	assert.Equal(t, repo.rules[ruleKey{
+		resource: rID1,
+		idx:      0,
+	}].Rule, *rules[3])
+
 }

pkg/policy/rule.go
@@ -11,6 +11,7 @@ import (
 	"github.com/cilium/proxy/pkg/policy/api/kafka"
 
 	"github.com/cilium/cilium/pkg/identity"
+	ipcachetypes "github.com/cilium/cilium/pkg/ipcache/types"
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
@@ -18,9 +19,14 @@ import (
 	"github.com/cilium/cilium/pkg/policy/api"
 )
 
+type ruleKey struct {
+	resource ipcachetypes.ResourceID
+	idx      uint
+}
+
 type rule struct {
 	api.Rule
-
+	key      ruleKey
 	metadata *ruleMetadata
 }
 

commit 3be1716d4b248429e4faf9c818127057c5b6fc68

policy: ensure matched policy order is deterministic
This ensures that the selected policies always keep the same order local
to the agent. This was the old behavior before cilium v1.16, and some
subsystems partially depend on that fact.

In v1.16, policies are indexed in a map instead of a list, in order to
boost policy upsert and deletion performance. This is great for most
use-cases, but we have seen that the side effect of the random policy order is
very negative. Due to the random order when iterating through a map in
golang, policies end up being unsorted, with a new order
on every regeneration. This is especially bad for the dnsproxy that is
using the order of FQDNs in the distilled policy to generate a reference
counted regex.

We see that prior to v1.16, a pod with a stable policy set always reused
the same regex in the dnsproxy. In v1.16+, the order of FQDNs is
highly likely to be different for every regeneration, causing a lot of
additional regex compilation and thrash. The regexes are often also
reused by different pods, resulting in less allocations and cpu
chrun, as well as a big reduction in heap memory being used - since
large regexes take up a lot of memory. In some cases, we see the
reduction in memory by this patch to be more than 50%, and a similar number
for cpu usage on the agent when comparing to v1.15.

We assume that its cheaper, at least for now, to do the sorting here
rather than inside the dnsproxy. If a distilled policy has say 1000
FQDNs coming from 10 policies, its much cheaper to sort the slice with
10 elements than it is sorting the one with 1000.

Its also not very important to get a perfect sort order, just that its
always the same across two regenerations with the same set of policies.

Fixes: df42a7d ("policy: track rules by resource, not by labels")
Signed-off-by: Odin Ugedal <odin@uged.al>
Signed-off-by: Odin Ugedal <ougedal@palantir.com>

pkg/policy/repository.go
@@ -4,6 +4,7 @@
 package policy
 
 import (
+	"cmp"
 	"encoding/json"
 	"log/slog"
 	"maps"
@@ -390,6 +391,18 @@ func (p *Repository) computePolicyEnforcementAndRules(securityIdentity *identity
 			}
 		}
 	}
+
+	// Always sort matched rules to get a stable policy order.
+	// It's not the order per se that is important, just that it's always in the same order when the
+	// elements are the same. In most cases this should be a small list so the overhead should be pretty minimal.
+	// This is very useful for subsystems like the dnsproxy that can reuse the same regex during recompilation
+	// if the list of FQDNs is the same.
+	slices.SortFunc(matchingRules, func(a, b *rule) int {
+		if sign := cmp.Compare(a.key.resource, b.key.resource); sign != 0 {
+			return sign
+		}
+		return cmp.Compare(a.key.idx, b.key.idx)
+	})
 
 	// If policy enforcement is enabled for the daemon, then it has to be
 	// enabled for the endpoint.