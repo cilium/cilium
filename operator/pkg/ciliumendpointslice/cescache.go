a5d9e25969590180520c46b20d29e4d8cd1a05c0
jshr-w <shjayaraman@microsoft.com>
2025-10-29 16:58:24 +0000

    ces: update cescache cid handling
    We extend the CESCache to handle updates to CIDs. We will need to
    associate pods with their identities for creating CES.
    
    The SecIds data structure contains a selected ID, which is used if there
    are multiple ids associated with a label to minimize churn.
    
    Signed-off-by: jshr-w <shjayaraman@microsoft.com>

--- operator/pkg/ciliumendpointslice/cescache.go
+++ operator/pkg/ciliumendpointslice/cescache.go
@@ -4,12 +4,16 @@
 package ciliumendpointslice
 
 import (
+	"cmp"
+
 	"github.com/cilium/cilium/pkg/lock"
 	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 type NodeName string
 type EncryptionKey int
+type CID string
+type Label string
 
 // NodeData contains information about the node; the set of coreceps on
 // the node and the known encryption key associated with the node.
@@ -24,18 +28,41 @@ func NewNodeData() *NodeData {
 	}
 }
 
+// SecIDs contains the selected CID, a set of CIDs and a set of CEPs.
+// One CID from the set is selected to maintain compatibility with duplicate
+// identities.
+type SecIDs struct {
+	selectedID CID
+	ids        sets.Set[CID]
+	ceps       sets.Set[CEPName]
+}
+
+func NewSecIDs() *SecIDs {
+	return &SecIDs{
+		ids:  sets.New[CID](),
+		ceps: sets.New[CEPName](),
+	}
+}
+
 type CESCache struct {
 	mutex lock.RWMutex
 
 	// nodeData is used to map node name to all CiliumEndpoints on the node
 	// and the known encryption key associated with it
 	nodeData map[NodeName]*NodeData
+	// globalIdLabelsToCIDSet maps a set of labels to the CEPs and CIDs associated with it.
+	// Compatible with Agent's CID management which can cause duplicate CIDs.
+	globalIdLabelsToCIDSet map[Label]*SecIDs
+	// cidToGidLabels maps CID to the GID labels associated with it.
+	cidToGidLabels map[CID]Label
 }
 
 // Creates and intializes the new CESCache
 func newCESCache() *CESCache {
 	return &CESCache{
-		nodeData: make(map[NodeName]*NodeData),
+		nodeData:               make(map[NodeName]*NodeData),
+		globalIdLabelsToCIDSet: make(map[Label]*SecIDs),
+		cidToGidLabels:         make(map[CID]Label),
 	}
 }
 
@@ -83,4 +110,51 @@ func (c *CESCache) hasNode(nodeName NodeName) bool {
 func (c *CESCache) getCESForCEPs(ceps sets.Set[CEPName]) []CESKey {
 	// TODO: Implement when CEP/CES state is tracked in cache
 	return nil
+}
+
+// Return if given CID is present in cache
+func (c *CESCache) hasCID(cid CID, gidLabels Label) bool {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	_, ok := c.cidToGidLabels[cid]
+	return ok
+}
+
+// Return the selected CID for the given GID labels
+func (c *CESCache) GetSelectedId(gid Label) (CID, bool) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+
+	if gidData, ok := c.globalIdLabelsToCIDSet[gid]; ok {
+		return gidData.selectedID, true
+	}
+	return "", false
+}
+
+// Clean up globalIdLabelsToCIDSet map, if no label state exists.
+// Caller should hold cache lock.
+func (c *CESCache) cleanLabelsMap(gidLabels Label) {
+	if secId, ok := c.globalIdLabelsToCIDSet[gidLabels]; ok {
+		if secId.ceps.Len() == 0 && secId.ids.Len() == 0 {
+			delete(c.globalIdLabelsToCIDSet, gidLabels)
+		}
+	}
+}
+
+func (cid CID) String() string {
+	return string(cid)
+}
+
+// SetSelectedID will update the selectedID to the given CID if not set.
+// If findNextCID is true, it will find the next available CID
+// and update selectedID to it, or set to empty if no CIDs are available.
+// Caller should hold cache lock.
+func (s *SecIDs) setSelectedID(newCID CID, findNextCID bool) {
+	if findNextCID {
+		for nextID := range s.ids {
+			s.selectedID = nextID
+			return
+		}
+	}
+	s.selectedID = cmp.Or(s.selectedID, newCID)
 }
