cilium/cilium

commit 93018d28b4197fc556e26c24c3ed369b8cc812b6

envoy: add stream idle timeout configuration option
Signed-off-by: joey <zchengjoey@gmail.com>
Signed-off-by: Tam Mach <tam.mach@cilium.io>

operator/pkg/ciliumenvoyconfig/ciliumenvoyconfig.go
@@ -18,16 +18,17 @@ type ciliumEnvoyConfigReconciler struct {
 	client client.Client
 	logger *slog.Logger
 
-	algorithm          string
-	ports              []string
-	maxRetries         int
-	idleTimeoutSeconds int
-	enableIpv4         bool
-	enableIpv6         bool
+	algorithm                string
+	ports                    []string
+	maxRetries               int
+	idleTimeoutSeconds       int
+	streamIdleTimeoutSeconds int
+	enableIpv4               bool
+	enableIpv6               bool
 }
 
 func newCiliumEnvoyConfigReconciler(c client.Client, logger *slog.Logger, defaultAlgorithm string, ports []string,
-	maxRetries int, idleTimeoutSeconds int, enableIpv4 bool, enableIpv6 bool,
+	maxRetries int, idleTimeoutSeconds int, streamIdleTimeoutSeconds int, enableIpv4 bool, enableIpv6 bool,
 ) *ciliumEnvoyConfigReconciler {
 	return &ciliumEnvoyConfigReconciler{
 		client: c,

commit 85503ffa8142b2dd4cdd5449c56b2bad76b84bde

operator/ciliumenvoyconfig: set streamIdleTimeoutSeconds in newCiliumEnvoyConfigReconciler
The referenced commit missed to set the value passed to
newCiliumEnvoyConfigReconciler (from the --http-stream-idle-timeout
config option) in the returned *ciliumEnvoyConfigReconciler.

Found when running unparam against the Cilium code base.

Fixes: 93018d28b419 ("envoy: add stream idle timeout configuration option")
Signed-off-by: Tobias Klauser <tobias@cilium.io>

operator/pkg/ciliumenvoyconfig/ciliumenvoyconfig.go
@@ -34,12 +34,13 @@ func newCiliumEnvoyConfigReconciler(c client.Client, logger *slog.Logger, defaul
 		client: c,
 		logger: logger,
 
-		algorithm:          defaultAlgorithm,
-		ports:              ports,
-		maxRetries:         maxRetries,
-		idleTimeoutSeconds: idleTimeoutSeconds,
-		enableIpv4:         enableIpv4,
-		enableIpv6:         enableIpv6,
+		algorithm:                defaultAlgorithm,
+		ports:                    ports,
+		maxRetries:               maxRetries,
+		idleTimeoutSeconds:       idleTimeoutSeconds,
+		streamIdleTimeoutSeconds: streamIdleTimeoutSeconds,
+		enableIpv4:               enableIpv4,
+		enableIpv6:               enableIpv6,
 	}
 }
 