cilium/cilium

commit 2e2c6c5acc0265a7d3b88fd8b0efb7b7f81f0b0c

policy: determine subject identities via SelectorCache
In order to determine applicable identities to which a policy applies, we
need to evaluate label selectors. Given that we already have an
efficient mechanism for caching label selectors (the SelectorCache), we
should use that for subject endpoints as well.

This refactors the PolicyRepository to use the SelectorCache when
determining subject identities. It removes yet another static cache of
matched identities and a corresponding event bus. It also saves memory
in the case of reused selectors, which is common.

An important consideration is that any new identities must be in the
selectorcache *before* that endpoint is regenerated, or else it will not
get the correct set of policies. Indeed this is safe, because identity
allocation updates the SelectorCache synchronously, and endpoints must
have their security identity allocated before they can use it.

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

pkg/policy/repository.go
@@ -405,11 +405,7 @@ func (p *Repository) SearchRLocked(lbls labels.LabelArray) api.Rules {
 func (p *Repository) AddListLocked(rules api.Rules) (ruleSlice, uint64) {
 	newRules := make(ruleSlice, 0, len(rules))
 	for _, r := range rules {
-		newRule := &rule{
-			Rule:     *r,
-			key:      ruleKey{idx: p.nextID},
-			metadata: newRuleMetadata(),
-		}
+		newRule := p.newRule(*r, ruleKey{idx: p.nextID})
 		newRules = append(newRules, newRule)
 		p.insert(newRule)
 		p.nextID++
@@ -438,16 +434,10 @@ func (p *Repository) ReplaceByResourceLocked(rules api.Rules, resource ipcachety
 	}
 
 	newRules = make(ruleSlice, 0, len(rules))
-
 	if len(rules) > 0 {
 		p.rulesByResource[resource] = make(map[ruleKey]*rule, len(rules))
-
 		for i, r := range rules {
-			newRule := &rule{
-				Rule:     *r,
-				key:      ruleKey{resource: resource, idx: uint(i)},
-				metadata: newRuleMetadata(),
-			}
+			newRule := p.newRule(*r, ruleKey{resource: resource, idx: uint(i)})
 			newRules = append(newRules, newRule)
 			p.insert(newRule)
 		}
@@ -485,40 +475,25 @@ func (p *Repository) del(key ruleKey) {
 	metrics.Policy.Dec()
 }
 
-// removeIdentityFromRuleCaches removes the identity from the selector cache
-// in each rule in the repository.
-//
-// Returns a sync.WaitGroup that blocks until the policy operation is complete.
-// The repository read lock must be held until the waitgroup is complete.
-func (p *Repository) removeIdentityFromRuleCaches(identity *identity.Identity) *sync.WaitGroup {
-	var wg sync.WaitGroup
-	wg.Add(len(p.rules))
-	for _, r := range p.rules {
-		go func(rr *rule, wgg *sync.WaitGroup) {
-			rr.metadata.delete(identity)
-			wgg.Done()
-		}(r, &wg)
+// newRule allocates a CachedSelector for a given rule.
+func (p *Repository) newRule(apiRule api.Rule, key ruleKey) *rule {
+	r := &rule{
+		Rule: apiRule,
+		key:  key,
 	}
-	return &wg
+	r.subjectSelector, _ = p.selectorCache.AddIdentitySelector(r, r.Labels, *r.getSelector())
+	return r
 }
 
-// LocalEndpointIdentityAdded handles local identity add events.
-func (p *Repository) LocalEndpointIdentityAdded(*identity.Identity) {
-	// no-op for now.
-}
-
-// LocalEndpointIdentityRemoved handles local identity removal events to
-// remove references from rules in the repository to the specified identity.
-func (p *Repository) LocalEndpointIdentityRemoved(identity *identity.Identity) {
-	go func() {
-		scopedLog := log.WithField(logfields.Identity, identity)
-		scopedLog.Debug("Removing identity references from policy cache")
-		p.Mutex.RLock()
-		wg := p.removeIdentityFromRuleCaches(identity)
-		wg.Wait()
-		p.Mutex.RUnlock()
-		scopedLog.Debug("Finished cleaning policy cache")
-	}()
+// Release releases resources owned by a given rule slice.
+// This is needed because we need to evaluate deleted rules after they
+// are removed from the repository, so we must allow for a specific lifecycle
+func (p *Repository) Release(rs ruleSlice) {
+	for _, r := range rs {
+		if r.subjectSelector != nil {
+			p.selectorCache.RemoveSelector(r.subjectSelector, r)
+		}
+	}
 }
 
 // MustAddList inserts a rule into the policy repository. It is used for
@@ -546,28 +521,21 @@ func (p *Repository) Iterate(f func(rule *api.Rule)) {
 	}
 }
 
-// UpdateRulesEndpointsCaches updates the caches within each rule in r that
-// specify whether the rule selects the endpoints in eps. If any rule matches
-// the endpoints, it is added to the provided IDSet, and removed from the
-// provided EndpointSet. The provided WaitGroup is signaled for a given endpoint
-// when it is finished being processed.
-func (r ruleSlice) UpdateRulesEndpointsCaches(endpointsToBumpRevision, endpointsToRegenerate *EndpointSet, policySelectionWG *sync.WaitGroup) {
+// FindSelectedEndpoints finds all endpoints selected by a given ruleSlice.
+// All endpoints that are selected will be added to endpointsToRegenerate; all
+// endpoints that are *not* selected (but still valid) remain in endpointsToBumpRevision.
+// policySelectionWG is done when all endpoints have been considered.
+func (r ruleSlice) FindSelectedEndpoints(endpointsToBumpRevision, endpointsToRegenerate *EndpointSet, policySelectionWG *sync.WaitGroup) {
 	endpointsToBumpRevision.ForEachGo(policySelectionWG, func(epp Endpoint) {
-		endpointSelected, err := r.updateEndpointsCaches(epp)
-		if endpointSelected {
-			endpointsToRegenerate.Insert(epp)
+		securityIdentity, err := epp.GetSecurityIdentity()
+		if err != nil || securityIdentity == nil {
+			// The endpoint is no longer alive, or it does not have a security identity.
+			// We should remove it from the set of endpoints that will be bumped
+			endpointsToBumpRevision.Delete(epp)
+			return
 		}
-		// If we could not evaluate the rules against the current endpoint, or
-		// the endpoint is selected by the rules, remove it from the set of
-		// endpoints to bump the revision. If the error is non-nil, the
-		// endpoint is no longer in either set (endpointsToBumpRevision or
-		// endpointsToRegenerate, as we could not determine what to do for the
-		// endpoint). This is usually the case when the endpoint is no longer
-		// alive (i.e., it has been marked to be deleted).
-		if endpointSelected || err != nil {
-			if err != nil {
-				log.WithError(err).Debug("could not determine whether endpoint was selected by rule")
-			}
+		if r.matchesSubject(securityIdentity) {
+			endpointsToRegenerate.Insert(epp)
 			endpointsToBumpRevision.Delete(epp)
 		}
 	})
@@ -807,7 +775,7 @@ func (p *Repository) computePolicyEnforcementAndRules(securityIdentity *identity
 
 	matchingRules = []*rule{}
 	for _, r := range p.rules {
-		if r.matches(securityIdentity) {
+		if r.matchesSubject(securityIdentity) {
 			matchingRules = append(matchingRules, r)
 		}
 	}
@@ -878,9 +846,7 @@ func (p *Repository) computePolicyEnforcementAndRules(securityIdentity *identity
 
 // wildcardRule generates a wildcard rule that only selects the given identity.
 func wildcardRule(lbls labels.LabelArray, ingress bool) *rule {
-	r := &rule{
-		metadata: newRuleMetadata(),
-	}
+	r := &rule{}
 
 	if ingress {
 		r.Ingress = []api.IngressRule{

pkg/policy/l4_filter_test.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"fmt"
 	stdlog "log"
+	"sync"
 	"testing"
 
 	cilium "github.com/cilium/proxy/go/cilium/api"
@@ -15,6 +16,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/fqdn/re"
+	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy/api"
@@ -82,6 +84,15 @@ func (td *testData) resetRepo() *Repository {
 	td.repo = NewPolicyRepository(nil, nil, nil)
 	td.repo.selectorCache = td.sc
 	return td.repo
+}
+
+func (td *testData) addIdentity(id *identity.Identity) {
+	wg := &sync.WaitGroup{}
+	td.sc.UpdateIdentities(
+		identity.IdentityMap{
+			id.ID: id.LabelArray,
+		}, nil, wg)
+	wg.Wait()
 }
 
 // testPolicyContexttype is a dummy context used when evaluating rules.

pkg/policy/repository_test.go
@@ -56,6 +56,7 @@ func TestComputePolicyEnforcementAndRules(t *testing.T) {
 	fooSelectLabel := labels.ParseSelectLabel("foo")
 	fooNumericIdentity := 9001
 	fooIdentity := identity.NewIdentity(identity.NumericIdentity(fooNumericIdentity), lbls)
+	td.addIdentity(fooIdentity)
 	fooIngressRule1Label := labels.NewLabel(k8sConst.PolicyLabelName, "fooIngressRule1", labels.LabelSourceAny)
 	fooIngressRule2Label := labels.NewLabel(k8sConst.PolicyLabelName, "fooIngressRule2", labels.LabelSourceAny)
 	fooEgressRule1Label := labels.NewLabel(k8sConst.PolicyLabelName, "fooEgressRule1", labels.LabelSourceAny)
@@ -2280,47 +2281,6 @@ Ingress verdict: denied
 	require.Equal(t, api.Allowed, verdict)
 }
 
-func TestRemoveIdentityFromRuleCaches(t *testing.T) {
-	td := newTestData()
-
-	td.repo.MustAddList(api.Rules{&api.Rule{
-		EndpointSelector: endpointSelectorA,
-		Ingress: []api.IngressRule{
-			{
-				IngressCommonRule: api.IngressCommonRule{
-					FromEndpoints: []api.EndpointSelector{endpointSelectorC},
-				},
-			},
-		},
-	}})
-
-	addedRule := td.repo.rules[ruleKey{idx: 0}]
-
-	selectedEpLabels := labels.ParseSelectLabel("id=a")
-	selectedIdentity := identity.NewIdentity(54321, labels.Labels{selectedEpLabels.Key: selectedEpLabels})
-
-	notSelectedEpLabels := labels.ParseSelectLabel("id=b")
-	notSelectedIdentity := identity.NewIdentity(9876, labels.Labels{notSelectedEpLabels.Key: notSelectedEpLabels})
-
-	// selectedEndpoint is selected by rule, so we it should be added to
-	// EndpointsSelected.
-	require.Equal(t, true, addedRule.matches(selectedIdentity))
-	require.EqualValues(t, map[identity.NumericIdentity]bool{selectedIdentity.ID: true}, addedRule.metadata.IdentitySelected)
-
-	wg := td.repo.removeIdentityFromRuleCaches(selectedIdentity)
-	wg.Wait()
-
-	require.EqualValues(t, map[identity.NumericIdentity]bool{}, addedRule.metadata.IdentitySelected)
-
-	require.Equal(t, false, addedRule.matches(notSelectedIdentity))
-	require.EqualValues(t, map[identity.NumericIdentity]bool{notSelectedIdentity.ID: false}, addedRule.metadata.IdentitySelected)
-
-	wg = td.repo.removeIdentityFromRuleCaches(notSelectedIdentity)
-	wg.Wait()
-
-	require.EqualValues(t, map[identity.NumericIdentity]bool{}, addedRule.metadata.IdentitySelected)
-}
-
 func TestIterate(t *testing.T) {
 	td := newTestData()
 	repo := td.repo
@@ -2408,8 +2368,6 @@ func TestDefaultAllow(t *testing.T) {
 	SetPolicyEnabled(option.DefaultEnforcement)
 
 	fooSelectLabel := labels.ParseSelectLabel("foo")
-	fooNumericIdentity := 9001
-	fooIdentity := identity.NewIdentity(identity.NumericIdentity(fooNumericIdentity), lbls)
 
 	genRule := func(ingress, defaultDeny bool) api.Rule {
 		name := fmt.Sprintf("%v_%v", ingress, defaultDeny)
@@ -2493,6 +2451,7 @@ func TestDefaultAllow(t *testing.T) {
 	// three test runs: ingress, egress, and ingress + egress cartesian
 	for i, tc := range ingressCases {
 		td := newTestData()
+		td.addIdentity(fooIdentity)
 		repo := td.repo
 
 		for _, rule := range tc.rules {
@@ -2508,6 +2467,7 @@ func TestDefaultAllow(t *testing.T) {
 
 	for i, tc := range egressCases {
 		td := newTestData()
+		td.addIdentity(fooIdentity)
 		repo := td.repo
 
 		for _, rule := range tc.rules {
@@ -2525,6 +2485,7 @@ func TestDefaultAllow(t *testing.T) {
 	for e, etc := range egressCases {
 		for i, itc := range ingressCases {
 			td := newTestData()
+			td.addIdentity(fooIdentity)
 			repo := td.repo
 
 			for _, rule := range etc.rules {
@@ -2546,34 +2507,42 @@ func TestDefaultAllow(t *testing.T) {
 }
 
 func TestReplaceByResource(t *testing.T) {
+	// don't use the full testdata() here, since we want to watch
+	// selectorcache changes carefully
 	repo := NewPolicyRepository(nil, nil, nil)
+	sc := testNewSelectorCache(nil)
+	repo.selectorCache = sc
+	assert.Len(t, sc.selectors, 0)
 
 	numRules := 10
 	rules := make(api.Rules, 0, numRules)
-	lbls := make([]labels.Label, numRules)
+	// share the dest selector
+	destSelector := api.NewESFromLabels(labels.NewLabel("peer", "pod", "k8s"))
 	for i := 0; i < numRules; i++ {
-		it := fmt.Sprintf("baz%d", i)
+		it := fmt.Sprintf("num-%d", i)
 		epSelector := api.NewESFromLabels(
 			labels.NewLabel(
-				"foo",
+				"subject-pod",
 				it,
 				labels.LabelSourceK8s,
 			),
 		)
-		lbls[i] = labels.NewLabel("tag3", it, labels.LabelSourceK8s)
-		rules = append(rules, &api.Rule{
+		lbl := labels.NewLabel("policy-label", it, labels.LabelSourceK8s)
+		rule := &api.Rule{
 			EndpointSelector: epSelector,
-			Labels:           labels.LabelArray{lbls[i]},
+			Labels:           labels.LabelArray{lbl},
 			Egress: []api.EgressRule{
 				{
 					EgressCommonRule: api.EgressCommonRule{
 						ToEndpoints: []api.EndpointSelector{
-							epSelector,
+							destSelector,
 						},
 					},
 				},
 			},
-		})
+		}
+		require.Nil(t, rule.Sanitize())
+		rules = append(rules, rule)
 	}
 
 	rulesMatch := func(s ruleSlice, rs api.Rules) {
@@ -2606,6 +2575,10 @@ func TestReplaceByResource(t *testing.T) {
 	assert.Len(t, repo.rulesByResource[rID1], 1)
 	rulesMatch(toSlice(repo.rulesByResource[rID1]), rules[0:1])
 
+	// Check that the selectorcache is sane
+	// It should have one selector: the subject pod for rule 0
+	assert.Len(t, sc.selectors, 1)
+
 	// add second resource
 	new, old, rev = repo.ReplaceByResourceLocked(rules[1:3], rID2)
 
@@ -2618,17 +2591,20 @@ func TestReplaceByResource(t *testing.T) {
 	assert.Len(t, repo.rulesByResource, 2)
 	assert.Len(t, repo.rulesByResource[rID1], 1)
 	assert.Len(t, repo.rulesByResource[rID2], 2)
+	assert.Len(t, sc.selectors, 3)
 
 	// replace rid1 with new rules
 	new, old, _ = repo.ReplaceByResourceLocked(rules[3:5], rID1)
 	assert.Len(t, new, 2)
 	assert.Len(t, old, 1)
+	repo.Release(old)
 
 	// check basic bookkeeping
 	assert.Len(t, repo.rules, 4)
 	assert.Len(t, repo.rulesByResource, 2)
 	assert.Len(t, repo.rulesByResource[rID1], 2)
 	assert.Len(t, repo.rulesByResource[rID2], 2)
+	assert.Len(t, sc.selectors, 4)
 
 	rulesMatch(old, rules[0:1])
 	rulesMatch(new, rules[3:5])
@@ -2638,4 +2614,31 @@ func TestReplaceByResource(t *testing.T) {
 		idx:      0,
 	}].Rule, *rules[3])
 
+	// delete rid1
+	old, _ = repo.DeleteByResourceLocked(rID1)
+	assert.Len(t, old, 2)
+	repo.Release(old)
+
+	assert.Len(t, repo.rules, 2)
+	assert.Len(t, repo.rulesByResource, 1)
+	assert.Len(t, repo.rulesByResource[rID2], 2)
+	assert.Len(t, sc.selectors, 2)
+
+	// delete rid1 again (noop)
+	old, _ = repo.DeleteByResourceLocked(rID1)
+	assert.Len(t, old, 0)
+
+	assert.Len(t, repo.rules, 2)
+	assert.Len(t, repo.rulesByResource, 1)
+	assert.Len(t, repo.rulesByResource[rID2], 2)
+	assert.Len(t, sc.selectors, 2)
+
+	// delete rid2
+	old, _ = repo.DeleteByResourceLocked(rID2)
+	assert.Len(t, old, 2)
+	repo.Release(old)
+
+	assert.Len(t, repo.rules, 0)
+	assert.Len(t, repo.rulesByResource, 0)
+	assert.Len(t, sc.selectors, 0)
 }

pkg/policy/resolve_test.go
@@ -206,7 +206,9 @@ func (td *testData) bootstrapRepo(ruleGenFunc func(int) api.Rules, numRules int,
 	SetPolicyEnabled(option.DefaultEnforcement)
 	wg := &sync.WaitGroup{}
 	// load in standard reserved identities
-	c := identity.IdentityMap{}
+	c := identity.IdentityMap{
+		fooIdentity.ID: fooIdentity.LabelArray,
+	}
 	identity.IterateReservedIdentities(func(ni identity.NumericIdentity, id *identity.Identity) {
 		c[ni] = id.Labels.LabelArray()
 	})
@@ -225,7 +227,7 @@ func (td *testData) bootstrapRepo(ruleGenFunc func(int) api.Rules, numRules int,
 
 	epsToRegen := NewEndpointSet(nil)
 	wg = &sync.WaitGroup{}
-	rulez.UpdateRulesEndpointsCaches(epSet, epsToRegen, wg)
+	rulez.FindSelectedEndpoints(epSet, epsToRegen, wg)
 	wg.Wait()
 
 	require.Equal(tb, 0, epSet.Len())
@@ -278,6 +280,7 @@ func TestL7WithIngressWildcard(t *testing.T) {
 		idFooSelectLabels[lbl.Key] = lbl
 	}
 	fooIdentity := identity.NewIdentity(12345, idFooSelectLabels)
+	td.addIdentity(fooIdentity)
 
 	selFoo := api.NewESFromLabels(labels.ParseSelectLabel("id=foo"))
 	rule1 := api.Rule{
@@ -376,6 +379,7 @@ func TestL7WithLocalHostWildcard(t *testing.T) {
 	}
 
 	fooIdentity := identity.NewIdentity(12345, idFooSelectLabels)
+	td.addIdentity(fooIdentity)
 
 	// Emulate Kubernetes mode with allow from localhost
 	oldLocalhostOpt := option.Config.AllowLocalhost
@@ -485,6 +489,7 @@ func TestMapStateWithIngressWildcard(t *testing.T) {
 		idFooSelectLabels[lbl.Key] = lbl
 	}
 	fooIdentity := identity.NewIdentity(12345, idFooSelectLabels)
+	td.addIdentity(fooIdentity)
 
 	selFoo := api.NewESFromLabels(labels.ParseSelectLabel("id=foo"))
 	rule1 := api.Rule{
@@ -587,6 +592,7 @@ func TestMapStateWithIngress(t *testing.T) {
 		idFooSelectLabels[lbl.Key] = lbl
 	}
 	fooIdentity := identity.NewIdentity(12345, idFooSelectLabels)
+	td.addIdentity(fooIdentity)
 
 	lblTest := labels.ParseLabel("id=resolve_test_1")
 

daemon/cmd/daemon.go
@@ -922,7 +922,6 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 	// this needs to be done *after* init() for the daemon in that function,
 	// we populate the IPCache with the host's IP(s).
 	d.ipcache.InitIPIdentityWatcher(d.ctx, params.StoreFactory)
-	identitymanager.Subscribe(d.policy)
 
 	if err := params.IPsecKeyCustodian.StartBackgroundJobs(d.Datapath().Node()); err != nil {
 		log.WithError(err).Error("Unable to start IPsec key watcher")

daemon/cmd/daemon_test.go
@@ -42,6 +42,7 @@ import (
 	"github.com/cilium/cilium/pkg/promise"
 	"github.com/cilium/cilium/pkg/proxy"
 	"github.com/cilium/cilium/pkg/testutils"
+	testidentity "github.com/cilium/cilium/pkg/testutils/identity"
 	"github.com/cilium/cilium/pkg/types"
 )
 
@@ -155,6 +156,8 @@ func setupDaemonSuite(tb testing.TB) *DaemonSuite {
 
 	kvstore.Client().DeletePrefix(ctx, kvstore.OperationalPath)
 	kvstore.Client().DeletePrefix(ctx, kvstore.BaseKeyPrefix)
+
+	ds.d.policy.GetSelectorCache().SetLocalIdentityNotifier(testidentity.NewDummyIdentityNotifier())
 
 	ds.OnGetPolicyRepository = ds.d.GetPolicyRepository
 	ds.OnQueueEndpointBuild = nil

commit 36d5a2f51ca1e9114b50a02fc7eb213ffc96644d

policy: split selectorCache in two
This creates two unique selector caches inside the policy repository. We
do this since the performance after changes in v1.16 caused the selector cache
to become a lot slower since more data is injected.

This split takes advantage of the fact that in a cluster where pods,
identities and unique policies are churned consistently at a given rate r - we
know that not all aspects are equally expensive. If we then assume these
will have the cardinality c, we can do some calculations.

We know that identities and the policies are cluster level and will
churn at the same rate r, and has the same cardinality c.
However, we know that on a given node, the churn of identities used by
local endpoints is pretty small, most likely much smaller than r. The
same with the cardinality of those identities, that can be at most the
same number as there are endpoints on a node. We then also know, based
on the assumption that not all policies match all pods, that not all l3/l4
{to,from}Endpoints are used by all endpoints. We can then assume that on
a given node, not all c l3/l4 policies are being used. The same we can
assume for the churn then, that the change of l3/l4 policies in use at a
given time on the node, is much smaller than r. This also includes
policies like FQDN and toCIDR policies, and other types of policies that
end up in the selector cache today - but the concept is exactly the
same.

Based on this - we can split the selector cache in two, where;

One is used to index:
- policies from their endpointSelector (high churn rate, high cardinality)
- identities used by local endpoints (low churn rate, low cardinality)

And the other one to index:
- internal l3/l4 policies used by local endpoints (low churn rate, low
  cardinality)
- all identities (high churn rate, high cardinality)

All this together, means that we go from a single selector cache that
indexes a lot of different high churn and high cardinality items - to
two distinct selector caches that index one high cardinality high churn
aspect, and one that is low cardinality low churn.

Fixes: 2e2c6c5a ("policy: determine subject identities via SelectorCache")

pkg/policy/distillery.go
@@ -45,6 +45,7 @@ func (cache *policyCache) lookupOrCreate(identity *identityPkg.Identity) *cached
 	if !ok {
 		cip = newCachedSelectorPolicy(identity)
 		cache.policies[identity.ID] = cip
+		cache.repo.policySelectorCache.UpdateIdentities(identityPkg.IdentityMap{identity.ID: identity.LabelArray}, nil, nil)
 	}
 	return cip
 }
@@ -77,6 +78,7 @@ func (cache *policyCache) delete(identity *identityPkg.Identity) bool {
 		if selPolicy != nil {
 			selPolicy.detach(true, 0)
 		}
+		cache.repo.policySelectorCache.UpdateIdentities(nil, identityPkg.IdentityMap{identity.ID: identity.LabelArray}, nil)
 	}
 	return ok
 }

pkg/policy/repository.go
@@ -50,6 +50,7 @@ type PolicyRepository interface {
 	GetRevision() uint64
 	GetRulesList() *models.Policy
 	GetSelectorCache() *SelectorCache
+	GetPolicySelectorCache() *SelectorCache
 	Iterate(f func(rule *types.PolicyEntry))
 	ReplaceByResource(rules types.PolicyEntries, resource ipcachetypes.ResourceID) (affectedIDs *set.Set[identity.NumericIdentity], rev uint64, oldRevCnt int)
 	ReplaceByLabels(rules types.PolicyEntries, searchLabelsList []labels.LabelArray) (affectedIDs *set.Set[identity.NumericIdentity], rev uint64, oldRevCnt int)
@@ -83,8 +84,12 @@ type Repository struct {
 	revision atomic.Uint64
 
 	// selectorCache tracks the selectors used in the policies
-	// resolved from the repository.
+	// resolved from the repository by alive endpoints.
 	selectorCache *SelectorCache
+
+	// policySelectorCache tracks the selectors used by policies
+	// to select what endpoints they apply to
+	policySelectorCache *SelectorCache
 
 	// policyCache tracks the selector policies created from this repo
 	policyCache *policyCache
@@ -104,6 +109,11 @@ func (p *Repository) GetSelectorCache() *SelectorCache {
 	return p.selectorCache
 }
 
+// GetPolicySelectorCache() returns the selector cache used by the Repository for indexing policies
+func (p *Repository) GetPolicySelectorCache() *SelectorCache {
+	return p.policySelectorCache
+}
+
 // GetAuthTypes returns the AuthTypes required by the policy between the localID and remoteID
 func (p *Repository) GetAuthTypes(localID, remoteID identity.NumericIdentity) AuthTypes {
 	return p.policyCache.getAuthTypes(localID, remoteID)
@@ -119,15 +129,17 @@ func NewPolicyRepository(
 	metricsManager types.PolicyMetrics,
 ) *Repository {
 	selectorCache := NewSelectorCache(logger, initialIDs)
+	policySelectorCache := NewSelectorCache(logger, nil)
 	repo := &Repository{
-		logger:            logger,
-		rules:             make(map[ruleKey]*rule),
-		rulesByNamespace:  make(map[string]sets.Set[ruleKey]),
-		rulesByResource:   make(map[ipcachetypes.ResourceID]map[ruleKey]*rule),
-		selectorCache:     selectorCache,
-		certManager:       certManager,
-		metricsManager:    metricsManager,
-		l7RulesTranslator: l7RulesTranslator,
+		logger:              logger,
+		rules:               make(map[ruleKey]*rule),
+		rulesByNamespace:    make(map[string]sets.Set[ruleKey]),
+		rulesByResource:     make(map[ipcachetypes.ResourceID]map[ruleKey]*rule),
+		selectorCache:       selectorCache,
+		policySelectorCache: policySelectorCache,
+		certManager:         certManager,
+		metricsManager:      metricsManager,
+		l7RulesTranslator:   l7RulesTranslator,
 	}
 	repo.revision.Store(1)
 	repo.policyCache = newPolicyCache(repo, idmgr)
@@ -218,14 +230,14 @@ func (p *Repository) newRule(policyEntry types.PolicyEntry, key ruleKey) *rule {
 		PolicyEntry: policyEntry,
 		key:         key,
 	}
-	r.subjectSelector, _ = p.selectorCache.AddIdentitySelector(r, makeStringLabels(r.Labels), r.Subject)
+	r.subjectSelector, _ = p.policySelectorCache.AddIdentitySelector(r, makeStringLabels(r.Labels), r.Subject)
 	return r
 }
 
 // releaseRule releases the cached selector for a given rul
 func (p *Repository) releaseRule(r *rule) {
 	if r.subjectSelector != nil {
-		p.selectorCache.RemoveSelector(r.subjectSelector, r)
+		p.policySelectorCache.RemoveSelector(r.subjectSelector, r)
 	}
 }
 

pkg/policy/selectorcache.go
@@ -616,17 +616,26 @@ func (sc *SelectorCache) UpdateIdentities(added, deleted identity.IdentityMap, w
 				updated = true
 				sc.selectorUpdates = sc.selectorUpdates.Append(idSel, txn)
 				idSel.updateSelections(txn)
-				idSel.notifyUsers(sc, adds, dels, wg)
+				// just for proof-of-concept
+				if wg != nil {
+					idSel.notifyUsers(sc, adds, dels, wg)
+				}
 			}
 		}
 	}
 
 	if updated {
-		// Launch a waiter that holds the new version as long as needed for users to have grabbed it
-		sc.queueNotifiedUsersCommit(txn, wg)
+		// just for proof-of-concept
+		if wg != nil {
+			// Launch a waiter that holds the new version as long as needed for users to have grabbed it
+			sc.queueNotifiedUsersCommit(txn, wg)
+		}
 
 		go func(version *versioned.VersionHandle) {
-			wg.Wait()
+			// just for proof-of-concept
+			if wg != nil {
+				wg.Wait()
+			}
 			sc.logger.Debug(
 				"UpdateIdentities: Waited for incremental updates to have committed, closing handle on the new version.",
 				logfields.NewVersion, txn,

pkg/policy/cell/policy_importer_test.go
@@ -111,6 +111,7 @@ func TestAddReplaceRemoveRule(t *testing.T) {
 
 		prefixesByResource: map[ipcachetypes.ResourceID][]netip.Prefix{},
 	}
+	pi.repo.GetPolicySelectorCache().UpdateIdentities(ids, nil, nil)
 	pi.repo.GetSelectorCache().SetLocalIdentityNotifier(testidentity.NewDummyIdentityNotifier())
 
 	writeRule := func(r *policyapi.Rule) uint64 {

pkg/policy/l4_filter_test.go
@@ -44,8 +44,9 @@ var (
 )
 
 type testData struct {
-	sc   *SelectorCache
-	repo *Repository
+	sc       *SelectorCache
+	policySc *SelectorCache
+	repo     *Repository
 
 	idSet set.Set[identity.NumericIdentity]
 
@@ -72,14 +73,17 @@ func newTestData(logger *slog.Logger) *testData {
 
 	td := &testData{
 		sc:                testNewSelectorCache(logger, nil),
+		policySc:          testNewSelectorCache(logger, nil),
 		repo:              NewPolicyRepository(logger, nil, &fakeCertificateManager{}, envoypolicy.NewEnvoyL7RulesTranslator(logger, certificatemanager.NewMockSecretManagerInline()), nil, testpolicy.NewPolicyMetricsNoop()),
 		idSet:             set.NewSet[identity.NumericIdentity](),
 		testPolicyContext: &testPolicyContextType{logger: logger},
 	}
 	td.testPolicyContext.sc = td.sc
 	td.repo.selectorCache = td.sc
+	td.repo.policySelectorCache = td.policySc
 
 	td.wildcardCachedSelector, _ = td.sc.AddIdentitySelector(dummySelectorCacheUser, EmptyStringLabels, api.WildcardEndpointSelector)
+	td.policySc.AddIdentitySelector(dummySelectorCacheUser, EmptyStringLabels, api.WildcardEndpointSelector)
 
 	td.cachedSelectorA = td.getCachedSelectorForTest(endpointSelectorA, idA.ID)
 	td.cachedSelectorB = td.getCachedSelectorForTest(endpointSelectorB, idB.ID)
@@ -122,6 +126,7 @@ func (td *testData) withIDs(initIDs ...identity.IdentityMap) *testData {
 		maps.Copy(initial, im)
 	}
 	wg := &sync.WaitGroup{}
+	td.policySc.UpdateIdentities(initial, nil, nil)
 	td.sc.UpdateIdentities(initial, nil, wg)
 	wg.Wait()
 
@@ -133,6 +138,10 @@ func (td *testData) withIDs(initIDs ...identity.IdentityMap) *testData {
 
 func (td *testData) addIdentity(id *identity.Identity) {
 	wg := &sync.WaitGroup{}
+	td.policySc.UpdateIdentities(
+		identity.IdentityMap{
+			id.ID: id.LabelArray,
+		}, nil, nil)
 	td.sc.UpdateIdentities(
 		identity.IdentityMap{
 			id.ID: id.LabelArray,
@@ -143,6 +152,11 @@ func (td *testData) addIdentity(id *identity.Identity) {
 
 func (td *testData) removeIdentity(id *identity.Identity) {
 	wg := &sync.WaitGroup{}
+	td.policySc.UpdateIdentities(
+		nil,
+		identity.IdentityMap{
+			id.ID: id.LabelArray,
+		}, nil)
 	td.sc.UpdateIdentities(
 		nil,
 		identity.IdentityMap{

pkg/policy/repository_test.go
@@ -1621,7 +1621,7 @@ func TestReplaceByResource(t *testing.T) {
 	// selectorcache changes carefully
 	repo := NewPolicyRepository(hivetest.Logger(t), nil, nil, nil, nil, testpolicy.NewPolicyMetricsNoop())
 	sc := testNewSelectorCache(hivetest.Logger(t), nil)
-	repo.selectorCache = sc
+	repo.policySelectorCache = sc
 	assert.Empty(t, sc.selectors)
 
 	// create 10 rules, each with a subject selector that selects one identity.
@@ -1762,7 +1762,7 @@ func TestReplaceByLabels(t *testing.T) {
 	// selectorcache changes carefully
 	repo := NewPolicyRepository(hivetest.Logger(t), nil, nil, nil, nil, testpolicy.NewPolicyMetricsNoop())
 	sc := testNewSelectorCache(hivetest.Logger(t), nil)
-	repo.selectorCache = sc
+	repo.policySelectorCache = sc
 	assert.Empty(t, sc.selectors)
 
 	// create 10 rules, each with a subject selector that selects one identity.

pkg/policy/resolve_test.go
@@ -186,9 +186,11 @@ func (td *testData) bootstrapRepo(ruleGenFunc func(int) (api.Rules, identity.Ide
 		c[ni] = id.Labels.LabelArray()
 	})
 	td.sc.UpdateIdentities(c, nil, wg)
+	td.policySc.UpdateIdentities(c, nil, nil)
 
 	apiRules, ids := ruleGenFunc(numRules)
 	td.sc.UpdateIdentities(ids, nil, wg)
+	td.policySc.UpdateIdentities(ids, nil, nil)
 	wg.Wait()
 	td.repo.MustAddList(apiRules)
 }