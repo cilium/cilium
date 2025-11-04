cilium/cilium

commit 66a4e8e45187f9e25235113690e0ebf6275a81f4

plugins/cilium-cni: Busy wait for a bootstrapping agent
Before processing the deletion queue, the bootstrapping agent has to
take an exclusive lock on the directory, to avoid racing with the cilium
CNI handling a concurrent DEL request. In environments with high pod
churn, where the agent is racing with the CNI handling multiple DEL
requests, this can lead to lock starvation, since the CNI takes the lock
in non-exclusive mode.  In that case, the agent might expire all the
time allotted by the hive to complete the start hooks, set by default to
5 minutes, ultimately leading to an agent restart.

To avoid this, let the CNI checks if the agent is bootstrapping before
taking the lock. If so, avoid taking the lock to reduce contention and
try to wait 5 seconds (up to 3 times) for the agent to be ready to
handle the DEL request.

To understand that the agent is bootstrapping, we check that the
directory "/var/run/cilium" exists but the socket has not yet been
created. This works because at the very early stages of its startup
phase the agent creates the directory and removes the socket file
created by previous instances. Then it proceeds executing all its start
hooks, among which there is the deletion queue handling and soon after
the creation of the socket file. Note that checking the directory helps
detect the case when the agent has never started on the node.

The unit test exercising the fallback client has been updated to take
into account the additional retries due to the busy wait logic.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

plugins/cilium-cni/lib/deletion_queue.go
@@ -8,6 +8,7 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"io/fs"
 	"log/slog"
 	"net/http"
 	"os"
@@ -37,14 +38,21 @@ type DeletionFallbackClient struct {
 	deleteQueueLockfile string
 
 	newCiliumClientFn newCiliumClientFn
+
+	connectionBackoff time.Duration
 }
 
-// the timeout for connecting and obtaining the lock
-// the default of 30 seconds is too long; kubelet will time us out before then
-const timeoutDuration = 1500 * time.Millisecond
+const (
+	// the timeout for connecting and obtaining the lock
+	// the default of 30 seconds is too long; kubelet will time us out before then
+	timeoutDuration = 1500 * time.Millisecond
 
-// the maximum number of queued deletions allowed, to protect against kubelet insanity
-const maxDeletionFiles = 256
+	// default backoff interval between two subsequent connection attempts to the agent
+	connectionBackoffDefault = 5 * time.Second
+
+	// the maximum number of queued deletions allowed, to protect against kubelet insanity
+	maxDeletionFiles = 256
+)
 
 var (
 	// Indicates a non-recoverable error for DeletionFallbackClient.
@@ -64,6 +72,8 @@ func NewDeletionFallbackClient(logger *slog.Logger) *DeletionFallbackClient {
 		deleteQueueLockfile: defaults.DeleteQueueLockfile,
 
 		newCiliumClientFn: newCiliumClient,
+
+		connectionBackoff: connectionBackoffDefault,
 	}
 }
 
@@ -164,8 +174,35 @@ func (dc *DeletionFallbackClient) EndpointDeleteMany(req *models.EndpointBatchDe
 // indicating the caller to attempt fallback logic.
 func (dc *DeletionFallbackClient) deleteEndpointsBatch(req *models.EndpointBatchDeleteRequest) (bool, error) {
 	if err := dc.tryConnect(); err != nil {
-		// Failed to setup cilium client.
-		return true, err
+		// Check if agent is starting up. If so, it is about to handle the
+		// deletion queue, thus we should avoid taking the lock in order to
+		// reduce contention.
+		// Instead, we wait for it to complete its bootstrap and retry to
+		// connect again later.
+		var dirNotExists, socketNotExists bool
+		if _, err := os.Stat(filepath.Dir(client.DefaultSockPath())); errors.Is(err, fs.ErrNotExist) {
+			dirNotExists = true
+		}
+		if _, err := os.Stat(client.DefaultSockPath()); errors.Is(err, fs.ErrNotExist) {
+			socketNotExists = true
+		}
+		if !dirNotExists || !socketNotExists {
+			// Agent is not bootstrapping
+			return true, err
+		}
+
+		for range 3 {
+			dc.logger.Info("Agent is starting up, will retry to connect to the agent socket again in 5 seconds")
+			time.Sleep(dc.connectionBackoff)
+			if err := dc.tryConnect(); err == nil {
+				dc.logger.Info("Successfully connected to the API after waiting for the agent to start")
+				break
+			}
+		}
+		if dc.cli == nil {
+			// Failed to setup cilium client.
+			return true, err
+		}
 	}
 
 	err := dc.cli.EndpointDeleteMany(req)

plugins/cilium-cni/lib/deletion_queue_test.go
@@ -25,6 +25,7 @@ type errorCase int
 const (
 	errorCaseNever errorCase = iota
 	errorCaseOnce
+	errorCaseTwice
 	errorCaseAlways
 )
 
@@ -43,6 +44,11 @@ func (e *errorMock) call() error {
 		return nil
 	case errorCaseOnce:
 		if e.callCount == 1 {
+			return e.err
+		}
+		return nil
+	case errorCaseTwice:
+		if e.callCount <= 2 {
 			return e.err
 		}
 		return nil
@@ -68,7 +74,7 @@ type fakeCiliumClientCreator struct {
 }
 
 func (f *fakeCiliumClientCreator) ToString() string {
-	errMockStrs := []string{"Never", "Once", "Always"}
+	errMockStrs := []string{"Never", "Once", "Twice", "Always"}
 
 	return fmt.Sprintf("NewClient: %s, EndpointDelete: %s",
 		errMockStrs[f.errorMock.errorCase],
@@ -96,6 +102,7 @@ func TestDeletionFallbackClient(t *testing.T) {
 			deleteQueueLockfile: deleteQueueLockfile,
 
 			newCiliumClientFn: newClientFn,
+			connectionBackoff: time.Millisecond,
 		}
 	}
 
@@ -118,6 +125,10 @@ func TestDeletionFallbackClient(t *testing.T) {
 	}
 	deletionClientErrorOnce := errorMock{
 		errorCase: errorCaseOnce,
+		err:       deletionClientErr,
+	}
+	deletionClientErrorTwice := errorMock{
+		errorCase: errorCaseTwice,
 		err:       deletionClientErr,
 	}
 	deletionClientErrorAlways := errorMock{
@@ -161,6 +172,13 @@ func TestDeletionFallbackClient(t *testing.T) {
 			newClientCreator: fakeCiliumClientCreator{
 				errorMock:       newClientErrorOnce,
 				clientErrorMock: deletionClientErrorOnce,
+			},
+			shouldQueueDeletion: false,
+		},
+		{
+			newClientCreator: fakeCiliumClientCreator{
+				errorMock:       newClientErrorOnce,
+				clientErrorMock: deletionClientErrorTwice,
 			},
 			shouldQueueDeletion: true,
 		},

pkg/client/client.go
@@ -38,7 +38,11 @@ func DefaultSockPath() string {
 		// If unset, fall back to default value
 		e = defaults.SockPath
 	}
-	return "unix://" + e
+	return e
+}
+
+func DefaultSockPathProtocol() string {
+	return "unix://" + DefaultSockPath()
 }
 
 func configureTransport(tr *http.Transport, proto, addr string) *http.Transport {
@@ -129,7 +133,7 @@ func WithBasePath(basePath string) func(options *runtimeOptions) {
 
 func NewTransport(host string) (*http.Transport, error) {
 	if host == "" {
-		host = DefaultSockPath()
+		host = DefaultSockPathProtocol()
 	}
 	schema, host, found := strings.Cut(host, "://")
 	if !found {
@@ -159,7 +163,7 @@ func NewRuntime(opts ...func(options *runtimeOptions)) (*runtime_client.Runtime,
 
 	host := r.host
 	if host == "" {
-		host = DefaultSockPath()
+		host = DefaultSockPathProtocol()
 	}
 
 	_, hostHeader, found := strings.Cut(host, "://")

pkg/health/health_connectivity_node.go
@@ -48,7 +48,7 @@ func (h *ciliumHealthManager) launchCiliumNodeHealth(spec *healthApi.Spec, initi
 	)
 
 	config := server.Config{
-		CiliumURI:     ciliumPkg.DefaultSockPath(),
+		CiliumURI:     ciliumPkg.DefaultSockPathProtocol(),
 		Debug:         option.Config.Opts.IsEnabled(option.Debug),
 		ICMPReqsCount: option.Config.HealthCheckICMPFailureThreshold,
 		ProbeDeadline: serverProbeDeadline,

plugins/cilium-docker/driver/driver.go
@@ -80,7 +80,7 @@ func newLibnetworkRoute(route route.Route) api.StaticRoute {
 func NewDriver(logger *slog.Logger, ciliumSockPath, dockerHostPath string) (Driver, error) {
 
 	if ciliumSockPath == "" {
-		ciliumSockPath = client.DefaultSockPath()
+		ciliumSockPath = client.DefaultSockPathProtocol()
 	}
 
 	scopedLog := logger.With(

commit 50cc02555fdfb43ac9d728a3c3d1d8de8062cbe2

cilium-cni: Fix bootstrapping agent detection check
An agent that is bootstrapping after a restart can be detected by:

- the presence of the runtime directory (e.g: "/var/run/cilium")
- the absence of the Cilium UNIX domain socket in the directory above
  (e.g: "/var/run/cilium/cilium.sock")

Fix the check introduced in the previous commit using the conditions
above. Specifically, the previous commit erroneously checked for the
absence of the runtime directory, while that directory should already be
present in case of a restart.

Fixes: 66a4e8e451 ("plugins/cilium-cni: Busy wait for a bootstrapping agent")

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

plugins/cilium-cni/lib/deletion_queue.go
@@ -179,14 +179,14 @@ func (dc *DeletionFallbackClient) deleteEndpointsBatch(req *models.EndpointBatch
 		// reduce contention.
 		// Instead, we wait for it to complete its bootstrap and retry to
 		// connect again later.
-		var dirNotExists, socketNotExists bool
-		if _, err := os.Stat(filepath.Dir(client.DefaultSockPath())); errors.Is(err, fs.ErrNotExist) {
-			dirNotExists = true
+		var dirExists, socketNotExists bool
+		if _, err := os.Stat(filepath.Dir(client.DefaultSockPath())); err == nil {
+			dirExists = true
 		}
 		if _, err := os.Stat(client.DefaultSockPath()); errors.Is(err, fs.ErrNotExist) {
 			socketNotExists = true
 		}
-		if !dirNotExists || !socketNotExists {
+		if !dirExists || !socketNotExists {
 			// Agent is not bootstrapping
 			return true, err
 		}

plugins/cilium-cni/lib/deletion_queue_test.go
@@ -173,7 +173,7 @@ func TestDeletionFallbackClient(t *testing.T) {
 				errorMock:       newClientErrorOnce,
 				clientErrorMock: deletionClientErrorOnce,
 			},
-			shouldQueueDeletion: false,
+			shouldQueueDeletion: true,
 		},
 		{
 			newClientCreator: fakeCiliumClientCreator{