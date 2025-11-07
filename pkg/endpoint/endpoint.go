cilium/cilium

commit 36c9bba3a9424fbeb89c1b1127790e55df909764

policy/cache: simplify cache entry creation flow
This change removes the need for local identities to have a stub entry
first added to the policy cache. Now, we create the entry when we first
try and compute policy.

No functional change, just cleans up the lifecycle somewhat.

Signed-off-by: Casey Callendrello <cdc@isovalent.com>

pkg/endpoint/policy.go
@@ -220,25 +220,12 @@ func (e *Endpoint) regeneratePolicy(stats *regenerationStatistics, datapathRegen
 	stats.policyCalculation.Start()
 	defer func() { stats.policyCalculation.End(err == nil) }()
 
-	// Upon initial insertion or restore, there's currently no good
-	// trigger point to ensure that the security Identity is
-	// assigned after the endpoint is added to the endpointmanager
-	// (and hence also the identitymanager). In that case, detect
-	// that the selectorPolicy is not set and find it.
-	selectorPolicy := repo.GetPolicyCache().Lookup(securityIdentity)
-	if selectorPolicy == nil {
-		err := fmt.Errorf("no cached selectorPolicy found")
-		e.getLogger().WithError(err).Warning("Failed to regenerate from cached policy")
-		repo.RUnlock()
-		return result, err
-	}
-
 	// UpdatePolicy ensures the SelectorPolicy is fully resolved.
 	// Endpoint lock must not be held!
 	// TODO: GH-7515: Consider ways to compute policy outside of the
 	// endpoint regeneration process, ideally as part of the policy change
 	// handler.
-	err = repo.GetPolicyCache().UpdatePolicy(securityIdentity)
+	selectorPolicy, err := repo.GetPolicyCache().UpdatePolicy(securityIdentity)
 	if err != nil {
 		e.getLogger().WithError(err).Warning("Failed to update policy")
 		repo.RUnlock()

pkg/endpoint/policy_test.go
@@ -74,7 +74,6 @@ func TestIncrementalUpdatesDuringPolicyGeneration(t *testing.T) {
 	}
 
 	podID := addIdentity("pod")
-	repo.GetPolicyCache().LocalEndpointIdentityAdded(podID)
 
 	ep := Endpoint{
 		SecurityIdentity: podID,

pkg/endpoint/restore_test.go
@@ -57,9 +57,6 @@ func (s *EndpointSuite) endpointCreator(id uint16, secID identity.NumericIdentit
 	}
 	identity.Sanitize()
 
-	repo := s.GetPolicyRepository()
-	repo.GetPolicyCache().LocalEndpointIdentityAdded(identity)
-
 	ep := NewTestEndpointWithState(s, s, testipcache.NewMockIPCache(), &FakeEndpointProxy{}, testidentity.NewMockIdentityAllocator(nil), id, StateReady)
 	// Random network ID and docker endpoint ID with 59 hex chars + 5 strID = 64 hex chars
 	ep.dockerNetworkID = "603e047d2268a57f5a5f93f7f9e1263e9207e348a06654bf64948def001" + strID

pkg/policy/distillery.go
@@ -4,7 +4,6 @@
 package policy
 
 import (
-	"fmt"
 	"iter"
 	"sync/atomic"
 
@@ -53,24 +52,15 @@ func NewPolicyCache(repo *Repository, idmgr identitymanager.IDManager) *PolicyCa
 
 // lookupOrCreate adds the specified Identity to the policy cache, with a reference
 // from the specified Endpoint, then returns the threadsafe copy of the policy.
-func (cache *PolicyCache) lookupOrCreate(identity *identityPkg.Identity, create bool) SelectorPolicy {
+func (cache *PolicyCache) lookupOrCreate(identity *identityPkg.Identity) *cachedSelectorPolicy {
 	cache.Lock()
 	defer cache.Unlock()
 	cip, ok := cache.policies[identity.ID]
 	if !ok {
-		if !create {
-			return nil
-		}
-		cip = newCachedSelectorPolicy(identity, cache.repo.GetSelectorCache())
+		cip = newCachedSelectorPolicy(identity)
 		cache.policies[identity.ID] = cip
 	}
 	return cip
-}
-
-// insert adds the specified Identity to the policy cache, with a reference
-// from the specified Endpoint, then returns the threadsafe copy of the policy.
-func (cache *PolicyCache) insert(identity *identityPkg.Identity) SelectorPolicy {
-	return cache.lookupOrCreate(identity, true)
 }
 
 // delete forgets about any cached SelectorPolicy that this endpoint uses.
@@ -94,13 +84,8 @@ func (cache *PolicyCache) delete(identity *identityPkg.Identity) bool {
 // Returns whether the cache was updated, or an error.
 //
 // Must be called with repo.Mutex held for reading.
-func (cache *PolicyCache) updateSelectorPolicy(identity *identityPkg.Identity) (bool, error) {
-	cache.Lock()
-	cip, ok := cache.policies[identity.ID]
-	cache.Unlock()
-	if !ok {
-		return false, fmt.Errorf("SelectorPolicy not found in cache for ID %d", identity.ID)
-	}
+func (cache *PolicyCache) updateSelectorPolicy(identity *identityPkg.Identity) (*cachedSelectorPolicy, bool, error) {
+	cip := cache.lookupOrCreate(identity)
 
 	// As long as UpdatePolicy() is triggered from endpoint
 	// regeneration, it's possible for two endpoints with the
@@ -115,25 +100,24 @@ func (cache *PolicyCache) updateSelectorPolicy(identity *identityPkg.Identity) (
 	defer cip.Unlock()
 
 	// Don't resolve policy if it was already done for this or later revision.
-	if cip.getPolicy().Revision >= cache.repo.GetRevision() {
-		return false, nil
+	if policy := cip.getPolicy(); policy != nil && policy.Revision >= cache.repo.GetRevision() {
+		return cip, false, nil
 	}
 
 	// Resolve the policies, which could fail
 	selPolicy, err := cache.repo.resolvePolicyLocked(identity)
 	if err != nil {
-		return false, err
+		return nil, false, err
 	}
 
 	cip.setPolicy(selPolicy)
 
-	return true, nil
+	return cip, true, nil
 }
 
-// LocalEndpointIdentityAdded creates a SelectorPolicy cache entry for the
-// specified Identity, without calculating any policy for it.
+// LocalEndpointIdentityAdded is not needed; we only care about local endpoint
+// deletion
 func (cache *PolicyCache) LocalEndpointIdentityAdded(identity *identityPkg.Identity) {
-	cache.insert(identity)
 }
 
 // LocalEndpointIdentityRemoved deletes the cached SelectorPolicy for the
@@ -142,20 +126,14 @@ func (cache *PolicyCache) LocalEndpointIdentityRemoved(identity *identityPkg.Ide
 	cache.delete(identity)
 }
 
-// Lookup attempts to locate the SelectorPolicy corresponding to the specified
-// identity. If policy is not cached for the identity, it returns nil.
-func (cache *PolicyCache) Lookup(identity *identityPkg.Identity) SelectorPolicy {
-	return cache.lookupOrCreate(identity, false)
-}
-
 // UpdatePolicy resolves the policy for the security identity of the specified
 // endpoint and caches it for future use.
 //
 // The caller must provide threadsafety for iteration over the policy
 // repository.
-func (cache *PolicyCache) UpdatePolicy(identity *identityPkg.Identity) error {
-	_, err := cache.updateSelectorPolicy(identity)
-	return err
+func (cache *PolicyCache) UpdatePolicy(identity *identityPkg.Identity) (SelectorPolicy, error) {
+	cip, _, err := cache.updateSelectorPolicy(identity)
+	return cip, err
 }
 
 // GetAuthTypes returns the AuthTypes required by the policy between the localID and remoteID, if
@@ -204,11 +182,10 @@ type cachedSelectorPolicy struct {
 	policy   atomic.Pointer[selectorPolicy]
 }
 
-func newCachedSelectorPolicy(identity *identityPkg.Identity, selectorCache *SelectorCache) *cachedSelectorPolicy {
+func newCachedSelectorPolicy(identity *identityPkg.Identity) *cachedSelectorPolicy {
 	cip := &cachedSelectorPolicy{
 		identity: identity,
 	}
-	cip.setPolicy(newSelectorPolicy(selectorCache))
 	return cip
 }
 

pkg/policy/distillery_test.go
@@ -35,8 +35,8 @@ var (
 
 func localIdentity(n uint32) identity.NumericIdentity {
 	return identity.NumericIdentity(n) | identity.IdentityScopeLocal
-
 }
+
 func TestCacheManagement(t *testing.T) {
 	repo := NewStoppedPolicyRepository(nil, nil, nil, nil)
 	cache := repo.policyCache
@@ -48,9 +48,14 @@ func TestCacheManagement(t *testing.T) {
 	require.False(t, deleted)
 
 	// Insert identity twice. Should be the same policy.
-	policy1 := cache.insert(identity)
-	policy2 := cache.insert(identity)
-	require.Equal(t, policy2, policy1)
+	policy1, updated, err := cache.updateSelectorPolicy(identity)
+	require.NoError(t, err)
+	require.True(t, updated)
+	policy2, updated, err := cache.updateSelectorPolicy(identity)
+	require.NoError(t, err)
+	require.False(t, updated)
+	// must be same pointer
+	require.Same(t, policy2, policy1)
 
 	// Despite two insert calls, there is no reference tracking; any delete
 	// will clear the cache.
@@ -65,12 +70,14 @@ func TestCacheManagement(t *testing.T) {
 	ep3.SetIdentity(1234, true)
 	identity3 := ep3.GetSecurityIdentity()
 	require.NotEqual(t, identity, identity3)
-	policy1 = cache.insert(identity)
-	policy3 := cache.insert(identity3)
-	require.NotEqual(t, policy3, policy1)
-	_ = cache.delete(identity)
-	policy3 = cache.lookupOrCreate(identity3, false)
+	policy1, _, _ = cache.updateSelectorPolicy(identity)
+	require.NotNil(t, policy1)
+	policy3, _, _ := cache.updateSelectorPolicy(identity3)
 	require.NotNil(t, policy3)
+	require.NotSame(t, policy3, policy1)
+	_ = cache.delete(identity)
+	_, updated, _ = cache.updateSelectorPolicy(identity3)
+	require.False(t, updated)
 }
 
 func TestCachePopulation(t *testing.T) {
@@ -80,52 +87,37 @@ func TestCachePopulation(t *testing.T) {
 
 	identity1 := ep1.GetSecurityIdentity()
 	require.Equal(t, identity1, ep2.GetSecurityIdentity())
-	policy1 := cache.insert(identity1)
 
 	// Calculate the policy and observe that it's cached
-	updated, err := cache.updateSelectorPolicy(identity1)
+	policy1, updated, err := cache.updateSelectorPolicy(identity1)
 	require.NoError(t, err)
 	require.True(t, updated)
-	updated, err = cache.updateSelectorPolicy(identity1)
+	_, updated, err = cache.updateSelectorPolicy(identity1)
 	require.NoError(t, err)
 	require.False(t, updated)
-	policy2 := cache.insert(identity1)
-	idp1 := policy1.(*cachedSelectorPolicy).getPolicy()
-	idp2 := policy2.(*cachedSelectorPolicy).getPolicy()
+	policy2, _, _ := cache.updateSelectorPolicy(identity1)
+	require.NotNil(t, policy2)
+	idp1 := policy1.getPolicy()
+	idp2 := policy2.getPolicy()
 	require.Equal(t, idp2, idp1)
 
 	// Remove the identity and observe that it is no longer available
 	cacheCleared := cache.delete(identity1)
 	require.True(t, cacheCleared)
-	updated, err = cache.updateSelectorPolicy(identity1)
-	require.Error(t, err)
+	_, updated, err = cache.updateSelectorPolicy(identity1)
+	require.True(t, updated)
 
 	// Attempt to update policy for non-cached endpoint and observe failure
 	ep3 := testutils.NewTestEndpoint()
 	ep3.SetIdentity(1234, true)
-	_, err = cache.updateSelectorPolicy(ep3.GetSecurityIdentity())
-	require.Error(t, err)
-	require.False(t, updated)
-
-	// Insert endpoint with different identity and observe that the cache
-	// is different from ep1, ep2
-	policy1 = cache.insert(identity1)
-	idp1 = policy1.(*cachedSelectorPolicy).getPolicy()
-	require.NotNil(t, idp1)
-	identity3 := ep3.GetSecurityIdentity()
-	policy3 := cache.insert(identity3)
-	require.NotEqual(t, policy1, policy3)
-	updated, err = cache.updateSelectorPolicy(identity3)
+	policy3, updated, err := cache.updateSelectorPolicy(ep3.GetSecurityIdentity())
 	require.NoError(t, err)
 	require.True(t, updated)
-	idp3 := policy3.(*cachedSelectorPolicy).getPolicy()
-	require.NotEqual(t, idp1, idp3)
 
-	// If there's an error during policy resolution, update should fail
-	//repo.err = fmt.Errorf("not implemented!")
-	//repo.revision++
-	//_, err = cache.updateSelectorPolicy(identity3)
-	//require.Error(t, err)
+	// policy3 must be different from ep1, ep2
+	require.NoError(t, err)
+	idp3 := policy3.getPolicy()
+	require.NotEqual(t, idp1, idp3)
 }
 
 // Distillery integration tests
@@ -405,9 +397,11 @@ func (d *policyDistillery) WithLogBuffer(w io.Writer) *policyDistillery {
 // distillPolicy distills the policy repository into a set of bpf map state
 // entries for an endpoint with the specified labels.
 func (d *policyDistillery) distillPolicy(owner PolicyOwner, epLabels labels.LabelArray, identity *identity.Identity) (*mapState, error) {
-	sp := d.Repository.GetPolicyCache().insert(identity)
-	d.Repository.GetPolicyCache().UpdatePolicy(identity)
-	epp := sp.Consume(DummyOwner{}, testRedirects)
+	sp, err := d.Repository.GetPolicyCache().UpdatePolicy(identity)
+	if err != nil {
+		return nil, fmt.Errorf("failed to calculate policy: %w", err)
+	}
+	epp := sp.Consume(owner, testRedirects)
 	if epp == nil {
 		return nil, errors.New("policy distillation failure")
 	}

commit 7e06da25bc97603593a70f8847099ce95d3af6cd

endpoint: fix identity manager refCount issues
This fixes a refCount issue in the identity manager in the case where an
agent is restoring endpoints that are in the process of being stopped
and cleaned up. In that case, the identity might never be added to the
identity manger, but it will unconditionally be removed from it when its
being cleaned up.

This is seemingly fine in the base case with a single
endpoint, but if there are two or more sharing the same identity, we are
approaching muddy water. In that case the identity will be
upserted to the identity manager by the endpoint that is running, and
then removed by the one that is not running. That results in the
selector policy being removed _and_ detached when the endpoint is
running. This has the potential to cause a policy deadlock on the
running endpoint, since it is running with a detached selector policy -
not getting new updates. Also, prior to
36c9bba ("policy/cache: simplify cache entry creation flow"), introduced
in v1.17, this also caused a permanently broken endpoint that is unable
to regenerate. In v1.17 and newer, the selector policy is instead leaked
when the endpoint then is deleted. This effectively caused that
mentioned patch to hide this bug, making it harder to notice.

We have seen this cause endpoints to be stuck for hours until they are
deleted, without being able to get new policy updates.

Signed-off-by: Odin Ugedal <ougedal@palantir.com>

pkg/endpoint/endpoint.go
@@ -266,6 +266,12 @@ type Endpoint struct {
 	// the endpoint's labels.
 	SecurityIdentity *identity.Identity `json:"SecLabel"`
 
+	// identitySet is used to keep track of whether the SecurityIdentity of an endpoint
+	// has been added to the identity manager. This is only false for new endpoints without an identity or
+	// on endpoints that have been restored. We do this to ensure we only remove it from
+	// the identity manager if we are sure it was added.
+	identitySet bool
+
 	// policyMapFactory is used to create endpoint policy maps
 	policyMapFactory policymap.Factory
 
@@ -1275,16 +1281,20 @@ func (e *Endpoint) leaveLocked(conf DeleteConfig) []error {
 	}
 
 	if !conf.NoIdentityRelease && e.SecurityIdentity != nil {
-		// Restored endpoint may be created with a reserved identity of 5
-		// (init), which is not registered in the identity manager and
-		// therefore doesn't need to be removed.
-		if e.SecurityIdentity.ID != identity.ReservedIdentityInit {
-			e.identityManager.Remove(e.SecurityIdentity)
-		}
+		if e.identitySet {
+			// Restored endpoint may be created with a reserved identity of 5
+			// (init), which is not registered in the identity manager and
+			// therefore doesn't need to be removed.
+			// If identity was never set, eg. if the endpoint was disconnecting when it was restored
+			// we have not yet added its identity - hence we should not remove it.
+			if e.SecurityIdentity.ID != identity.ReservedIdentityInit {
+				e.identityManager.Remove(e.SecurityIdentity)
+			}
 
-		_, err := e.allocator.Release(context.Background(), e.SecurityIdentity, false)
-		if err != nil {
-			errs = append(errs, fmt.Errorf("unable to release identity: %w", err))
+			_, err := e.allocator.Release(context.Background(), e.SecurityIdentity, false)
+			if err != nil {
+				errs = append(errs, fmt.Errorf("unable to release identity: %w", err))
+			}
 		}
 		e.removeNetworkPolicy()
 		e.SecurityIdentity = nil

pkg/endpoint/policy.go
@@ -1107,14 +1107,16 @@ func (e *Endpoint) SetIdentity(identity *identityPkg.Identity, newEndpoint bool)
 	}
 
 	// Current security identity for endpoint is its old identity - delete its
-	// reference from global identity manager, add add a reference to the new
-	// identity for the endpoint.
-	if newEndpoint {
+	// reference from global identity manager, add a reference to the new
+	// identity for the endpoint. If the endpoint has never had its identitySet,
+	// we should note remove the old identity from the identity manager
+	if newEndpoint || !e.identitySet {
 		// TODO - GH-9354.
 		e.identityManager.Add(identity)
 	} else {
 		e.identityManager.RemoveOldAddNew(e.SecurityIdentity, identity)
 	}
+	e.identitySet = true
 	e.SecurityIdentity = identity
 	e.replaceIdentityLabels(labels.LabelSourceAny, identity.Labels)
 

pkg/endpoint/restore.go
@@ -288,6 +288,17 @@ func (e *Endpoint) restoreIdentity(regenerator *Regenerator) error {
 	case <-allocatedIdentity:
 	}
 
+	releaseNewlyAllocatedIdentity := func() {
+		_, err := e.allocator.Release(context.Background(), id, false)
+		if err != nil {
+			e.getLogger().Warn(
+				"Unable to release newly allocated identity again",
+				logfields.Error, err,
+				logfields.IdentityNew, id.ID,
+			)
+		}
+	}
+
 	// Wait for initial identities and ipcache from the
 	// kvstore before doing any policy calculation for
 	// endpoints that don't have a fixed identity or are
@@ -320,6 +331,7 @@ func (e *Endpoint) restoreIdentity(regenerator *Regenerator) error {
 		// is deleted.
 		select {
 		case <-e.aliveCtx.Done():
+			releaseNewlyAllocatedIdentity()
 			return ErrNotAlive
 		case <-gotInitialGlobalIdentities:
 		}
@@ -327,10 +339,12 @@ func (e *Endpoint) restoreIdentity(regenerator *Regenerator) error {
 
 	// Wait for registered initializers to complete before allowing endpoint regeneration.
 	if err := regenerator.WaitForFence(e.aliveCtx); err != nil {
+		releaseNewlyAllocatedIdentity()
 		return err
 	}
 
 	if err := e.lockAlive(); err != nil {
+		releaseNewlyAllocatedIdentity()
 		e.getLogger().Warn("Endpoint to restore has been deleted")
 		return err
 	}