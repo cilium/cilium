9e873653bbe397107631be222536ed228218d32f
Paul Chaignon <paul.chaignon@gmail.com>
2025-11-07 23:43:35 +0000

    ipsec: Fix detection logic for conflicting XFRM states
    [ upstream commit 0bebe48ac789d77edc1969bfe1500eab009d9f15 ]
    
    Function xfrmDeleteConflictingState aims to delete any XFRM states that
    conflict with the state we're trying to add. This is needed because the
    kernel will reject our XFRM state insertion if an existing state
    conflicts with it. XFRM states don't need to match 1:1 to conflict;
    unfortunately, the logic to reject conflicting states is a bit more
    complicated.
    
    In the kernel, for our specific case (ESP), that logic is implemented
    in function __xfrm_state_lookup [1]. The Cilium agent thus needs to
    replicate that logic to identify conflicting states and delete them
    before re-trying the insertion.
    
    That logic was however not correctly replicated in the agent: in the
    kernel, __xfrm_state_lookup doesn't compare the source IP address to
    detect conflicts, whereas in the agent, we would. This commit fixes it
    by removing the comparison of source IP addresses.
    
    1 - https://elixir.bootlin.com/linux/v6.16/source/net/xfrm/xfrm_state.c#L1174
    Reported-by: Marcel Zięba <marcel.zieba@isovalent.com>
    Reported-by: Steven Johnson <sjohnson@palantir.com>
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -473,7 +473,7 @@ func xfrmDeleteConflictingState(log *slog.Logger, states []netlink.XfrmState, ne
 	for _, s := range states {
 		if new.Spi == s.Spi && (new.Mark == nil) == (s.Mark == nil) &&
 			(new.Mark == nil || new.Mark.Value&new.Mark.Mask&s.Mark.Mask == s.Mark.Value) &&
-			xfrmIPEqual(new.Src, s.Src) && xfrmIPEqual(new.Dst, s.Dst) {
+			xfrmIPEqual(new.Dst, s.Dst) {
 			if err := xfrmStateCache.XfrmStateDel(&s); err != nil {
 				errs.Add(err)
 				continue
