cilium/cilium

commit e80eea9f06aa5f08f233a5897d3acf31f130edf5

clustermesh: policy: add --policy-default-local-cluster
Add a new option to select endpoint rules from the local cluster by default
(unless the rule select clusters explicitly) for CNP, CCNP and k8s NP. It works
in a similar way as namespace for CNP. Enabling this new option will make
sure that users do not add too wide network rules in ClusterMesh
environments.

This new option is for now opt-in but meant to be a future new default once
ready and after giving Cilium users a transition period.

Signed-off-by: Arthur Outhenin-Chalandre <arthur@cri.epita.fr>

pkg/k8s/network_policy.go
@@ -10,6 +10,7 @@ import (
 	"slices"
 
 	"github.com/cilium/cilium/pkg/annotation"
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	k8sCiliumUtils "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/utils"
 	slim_networkingv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/networking/v1"
@@ -56,12 +57,39 @@ func GetPolicyLabelsv1(logger *slog.Logger, np *slim_networkingv1.NetworkPolicy)
 	return k8sCiliumUtils.GetPolicyLabels(ns, policyName, policyUID, resourceTypeNetworkPolicy)
 }
 
-func parseNetworkPolicyPeer(namespace string, peer *slim_networkingv1.NetworkPolicyPeer) *api.EndpointSelector {
+func isPodSelectorSelectingCluster(podSelector *slim_metav1.LabelSelector) bool {
+	if podSelector == nil {
+		return false
+	}
+	if podSelector.MatchLabels[k8sConst.PolicyLabelCluster] != "" {
+		return true
+	}
+	for _, expr := range podSelector.MatchExpressions {
+		if expr.Key == k8sConst.PolicyLabelCluster {
+			return true
+		}
+	}
+
+	return false
+}
+
+func parseNetworkPolicyPeer(clusterName, namespace string, peer *slim_networkingv1.NetworkPolicyPeer) *api.EndpointSelector {
 	if peer == nil {
 		return nil
 	}
 
 	var retSel *api.EndpointSelector
+	// The PodSelector should only reflect to the configured cluster unless the selector
+	// explicitly targets another cluster already.
+	if clusterName != cmtypes.PolicyAnyCluster && !isPodSelectorSelectingCluster(peer.PodSelector) {
+		if peer.PodSelector == nil {
+			peer.PodSelector = &slim_metav1.LabelSelector{}
+		}
+		if peer.PodSelector.MatchLabels == nil {
+			peer.PodSelector.MatchLabels = map[string]slim_metav1.MatchLabelsValue{}
+		}
+		peer.PodSelector.MatchLabels[k8sConst.PolicyLabelCluster] = clusterName
+	}
 
 	if peer.NamespaceSelector != nil {
 		namespaceSelector := &slim_metav1.LabelSelector{
@@ -112,8 +140,7 @@ func hasV1PolicyType(pTypes []slim_networkingv1.PolicyType, typ slim_networkingv
 // ParseNetworkPolicy parses a k8s NetworkPolicy. Returns a list of
 // Cilium policy rules that can be added, along with an error if there was an
 // error sanitizing the rules.
-func ParseNetworkPolicy(logger *slog.Logger, np *slim_networkingv1.NetworkPolicy) (api.Rules, error) {
-
+func ParseNetworkPolicy(logger *slog.Logger, clusterName string, np *slim_networkingv1.NetworkPolicy) (api.Rules, error) {
 	if np == nil {
 		return nil, fmt.Errorf("cannot parse NetworkPolicy because it is nil")
 	}
@@ -130,7 +157,7 @@ func ParseNetworkPolicy(logger *slog.Logger, np *slim_networkingv1.NetworkPolicy
 		if len(iRule.From) > 0 {
 			for _, rule := range iRule.From {
 				ingress := api.IngressRule{}
-				endpointSelector := parseNetworkPolicyPeer(namespace, &rule)
+				endpointSelector := parseNetworkPolicyPeer(clusterName, namespace, &rule)
 
 				if endpointSelector != nil {
 					ingress.FromEndpoints = append(ingress.FromEndpoints, *endpointSelector)
@@ -175,7 +202,7 @@ func ParseNetworkPolicy(logger *slog.Logger, np *slim_networkingv1.NetworkPolicy
 			for _, rule := range eRule.To {
 				egress := api.EgressRule{}
 				if rule.NamespaceSelector != nil || rule.PodSelector != nil {
-					endpointSelector := parseNetworkPolicyPeer(namespace, &rule)
+					endpointSelector := parseNetworkPolicyPeer(clusterName, namespace, &rule)
 
 					if endpointSelector != nil {
 						egress.ToEndpoints = append(egress.ToEndpoints, *endpointSelector)

pkg/policy/k8s/network_policy.go
@@ -14,12 +14,12 @@ import (
 	"github.com/cilium/cilium/pkg/source"
 )
 
-func (p *policyWatcher) addK8sNetworkPolicyV1(k8sNP *slim_networkingv1.NetworkPolicy, apiGroup string, dc chan uint64) error {
+func (p *policyWatcher) addK8sNetworkPolicyV1(k8sNP *slim_networkingv1.NetworkPolicy, apiGroup string, dc chan uint64, clusterName string) error {
 	defer func() {
 		p.k8sResourceSynced.SetEventTimestamp(apiGroup)
 	}()
 
-	rules, err := k8s.ParseNetworkPolicy(p.log, k8sNP)
+	rules, err := k8s.ParseNetworkPolicy(p.log, clusterName, k8sNP)
 	if err != nil {
 		metrics.PolicyChangeTotal.WithLabelValues(metrics.LabelValueOutcomeFail).Inc()
 		p.log.Error(

daemon/cmd/cells.go
@@ -260,6 +260,7 @@ var (
 
 		// ClusterMesh is the Cilium's multicluster implementation.
 		cell.Config(cmtypes.DefaultClusterInfo),
+		cell.Config(cmtypes.DefaultPolicyConfig),
 		clustermesh.Cell,
 
 		// L2announcer resolves l2announcement policies, services, node labels and devices into a list of IPs+netdevs

install/kubernetes/cilium/templates/cilium-configmap.yaml
@@ -1401,6 +1401,7 @@ data:
 {{- end }}
   clustermesh-enable-endpoint-sync: {{ .Values.clustermesh.enableEndpointSliceSynchronization | quote }}
   clustermesh-enable-mcs-api: {{ .Values.clustermesh.enableMCSAPISupport | quote }}
+  policy-default-local-cluster: {{ .Values.clustermesh.policyDefaultLocalCluster | quote }}
 
   nat-map-stats-entries: {{ .Values.nat.mapStatsEntries | quote }}
   nat-map-stats-interval: {{ .Values.nat.mapStatsInterval | quote }}

install/kubernetes/cilium/values.yaml
@@ -3250,6 +3250,8 @@ clustermesh:
   enableEndpointSliceSynchronization: false
   # -- Enable Multi-Cluster Services API support
   enableMCSAPISupport: false
+  # -- Control whether policy rules assume by default the local cluster if not explicitly selected
+  policyDefaultLocalCluster: false
   # -- Annotations to be added to all top-level clustermesh objects (resources under templates/clustermesh-apiserver and templates/clustermesh-config)
   annotations: {}
   # -- Clustermesh explicit configuration.

install/kubernetes/cilium/values.yaml.tmpl
@@ -3271,6 +3271,8 @@ clustermesh:
   enableEndpointSliceSynchronization: false
   # -- Enable Multi-Cluster Services API support
   enableMCSAPISupport: false
+  # -- Control whether policy rules assume by default the local cluster if not explicitly selected
+  policyDefaultLocalCluster: false
 
   # -- Annotations to be added to all top-level clustermesh objects (resources under templates/clustermesh-apiserver and templates/clustermesh-config)
   annotations: {}

operator/cmd/ccnp_event.go
@@ -32,8 +32,8 @@ func k8sEventMetric(scope, action string) {
 // enableCCNPWatcher is similar to enableCNPWatcher but handles the watch events for
 // clusterwide policies. Since, internally Clusterwide policies are implemented
 // using CiliumNetworkPolicy itself, the entire implementation uses the methods
-// associcated with CiliumNetworkPolicy.
-func enableCCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGroup, clientset k8sClient.Clientset) {
+// associated with CiliumNetworkPolicy.
+func enableCCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGroup, clientset k8sClient.Clientset, clusterName string) {
 	logger.Info("Starting CCNP derivative handler")
 
 	ccnpStore := cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
@@ -51,7 +51,7 @@ func enableCCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGr
 					// See https://github.com/cilium/cilium/blob/27fee207f5422c95479422162e9ea0d2f2b6c770/pkg/policy/api/ingress.go#L112-L134
 					cnpCpy := cnp.DeepCopy()
 
-					groups.AddDerivativePolicyIfNeeded(logger, clientset, cnpCpy.CiliumNetworkPolicy, true)
+					groups.AddDerivativePolicyIfNeeded(logger, clientset, clusterName, cnpCpy.CiliumNetworkPolicy, true)
 				}
 			},
 			UpdateFunc: func(oldObj, newObj any) {
@@ -68,7 +68,7 @@ func enableCCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGr
 						newCNPCpy := newCNP.DeepCopy()
 						oldCNPCpy := oldCNP.DeepCopy()
 
-						groups.UpdateDerivativePolicyIfNeeded(logger, clientset, newCNPCpy.CiliumNetworkPolicy, oldCNPCpy.CiliumNetworkPolicy, true)
+						groups.UpdateDerivativePolicyIfNeeded(logger, clientset, clusterName, newCNPCpy.CiliumNetworkPolicy, oldCNPCpy.CiliumNetworkPolicy, true)
 					}
 				}
 			},
@@ -100,7 +100,7 @@ func enableCCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGr
 		controller.ControllerParams{
 			Group: ccnpToGroupsControllerGroup,
 			DoFunc: func(ctx context.Context) error {
-				groups.UpdateCNPInformation(logger, clientset)
+				groups.UpdateCNPInformation(logger, clientset, clusterName)
 				return nil
 			},
 			RunInterval: 5 * time.Minute,

operator/cmd/cnp_event.go
@@ -35,7 +35,7 @@ func init() {
 
 // enableCNPWatcher waits for the CiliumNetworkPolicy CRD availability and then
 // garbage collects stale CiliumNetworkPolicy status field entries.
-func enableCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGroup, clientset k8sClient.Clientset) {
+func enableCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGroup, clientset k8sClient.Clientset, clusterName string) {
 	logger.Info("Starting CNP derivative handler")
 	cnpStore := cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
 
@@ -52,7 +52,7 @@ func enableCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGro
 					// See https://github.com/cilium/cilium/blob/27fee207f5422c95479422162e9ea0d2f2b6c770/pkg/policy/api/ingress.go#L112-L134
 					cnpCpy := cnp.DeepCopy()
 
-					groups.AddDerivativePolicyIfNeeded(logger, clientset, cnpCpy.CiliumNetworkPolicy, false)
+					groups.AddDerivativePolicyIfNeeded(logger, clientset, clusterName, cnpCpy.CiliumNetworkPolicy, false)
 				}
 			},
 			UpdateFunc: func(oldObj, newObj any) {
@@ -69,7 +69,7 @@ func enableCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGro
 						newCNPCpy := newCNP.DeepCopy()
 						oldCNPCpy := oldCNP.DeepCopy()
 
-						groups.UpdateDerivativePolicyIfNeeded(logger, clientset, newCNPCpy.CiliumNetworkPolicy, oldCNPCpy.CiliumNetworkPolicy, false)
+						groups.UpdateDerivativePolicyIfNeeded(logger, clientset, clusterName, newCNPCpy.CiliumNetworkPolicy, oldCNPCpy.CiliumNetworkPolicy, false)
 					}
 				}
 			},
@@ -101,7 +101,7 @@ func enableCNPWatcher(ctx context.Context, logger *slog.Logger, wg *sync.WaitGro
 		controller.ControllerParams{
 			Group: cnpToGroupsControllerGroup,
 			DoFunc: func(ctx context.Context) error {
-				groups.UpdateCNPInformation(logger, clientset)
+				groups.UpdateCNPInformation(logger, clientset, clusterName)
 				return nil
 			},
 			RunInterval: 5 * time.Minute,

commit 0ea25c0dcd412548120b7998904747c3c26a538c

k8s: remove input mutation in parseNetworkPolicyPeer
parseNetworkPolicyPeer should not mutate the peer argument. The addition for
the option `policy-default-local-cluster` mistakenly added a such
mutation and this commit fixes that.

Fixes: e80eea9 ("clustermesh: policy: add --policy-default-local-cluster")

Signed-off-by: Arthur Outhenin-Chalandre <git@mrfreezeex.fr>

pkg/k8s/network_policy.go
@@ -79,17 +79,21 @@ func parseNetworkPolicyPeer(clusterName, namespace string, peer *slim_networking
 		return nil
 	}
 
+	// peer should not be nutated in this function
+
 	var retSel *api.EndpointSelector
+	podSelector := peer.PodSelector.DeepCopy()
+
 	// The PodSelector should only reflect to the configured cluster unless the selector
 	// explicitly targets another cluster already.
-	if clusterName != cmtypes.PolicyAnyCluster && !isPodSelectorSelectingCluster(peer.PodSelector) {
-		if peer.PodSelector == nil {
-			peer.PodSelector = &slim_metav1.LabelSelector{}
+	if clusterName != cmtypes.PolicyAnyCluster && !isPodSelectorSelectingCluster(podSelector) {
+		if podSelector == nil {
+			podSelector = &slim_metav1.LabelSelector{}
 		}
-		if peer.PodSelector.MatchLabels == nil {
-			peer.PodSelector.MatchLabels = map[string]slim_metav1.MatchLabelsValue{}
+		if podSelector.MatchLabels == nil {
+			podSelector.MatchLabels = map[string]slim_metav1.MatchLabelsValue{}
 		}
-		peer.PodSelector.MatchLabels[k8sConst.PolicyLabelCluster] = clusterName
+		podSelector.MatchLabels[k8sConst.PolicyLabelCluster] = clusterName
 	}
 
 	if peer.NamespaceSelector != nil {
@@ -123,10 +127,10 @@ func parseNetworkPolicyPeer(clusterName, namespace string, peer *slim_networking
 			namespaceSelector.MatchExpressions = []slim_metav1.LabelSelectorRequirement{allowAllNamespacesRequirement}
 		}
 
-		selector := api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, namespaceSelector, peer.PodSelector)
+		selector := api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, namespaceSelector, podSelector)
 		retSel = &selector
-	} else if peer.PodSelector != nil {
-		podSelector := parsePodSelector(peer.PodSelector, namespace)
+	} else if podSelector != nil {
+		podSelector = parsePodSelector(podSelector, namespace)
 		selector := api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, podSelector)
 		retSel = &selector
 	}