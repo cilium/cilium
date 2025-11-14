d29fc980c510398ecea02ebee77f146c7fdffd47
Odin Ugedal <ougedal@palantir.com>
2025-11-14 12:56:10 +0000

    fqdn: change function returns to be easier to read
    [ upstream commit 3c1e0db6dae92118be2fbd879f5b5bebe3ced05e ]
    
    This is a noop that switches from returning two bools with almost the
    same name, to instead returning a struct.
    
    Signed-off-by: Odin Ugedal <odin@uged.al>
    Signed-off-by: Odin Ugedal <ougedal@palantir.com>
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- pkg/fqdn/namemanager/manager_test.go
+++ pkg/fqdn/namemanager/manager_test.go
@@ -66,9 +66,8 @@ func TestMapIPsToSelectors(t *testing.T) {
 
 	// Just one IP.
 	ciliumIOName := prepareMatchName(ciliumIOSel.MatchName)
-	updated, upserted := cache.Update(now, ciliumIOName, []netip.Addr{ciliumIP1}, 100)
-	require.True(t, updated)
-	require.True(t, upserted)
+	res := cache.Update(now, ciliumIOName, []netip.Addr{ciliumIP1}, 100)
+	require.Equal(t, fqdn.UpdateStatus{Updated: true, Upserted: true}, res)
 	nameIPMapping = nameManager.mapSelectorsToNamesLocked(ciliumIOSel)
 	require.Len(t, nameIPMapping, 1)
 	println(ciliumIOSel.MatchName)
@@ -78,9 +77,8 @@ func TestMapIPsToSelectors(t *testing.T) {
 	require.Equal(t, ciliumIP1, ciliumIPs[0])
 
 	// Two IPs now.
-	updated, upserted = cache.Update(now, ciliumIOName, []netip.Addr{ciliumIP1, ciliumIP2}, 100)
-	require.True(t, updated)
-	require.True(t, upserted)
+	res = cache.Update(now, ciliumIOName, []netip.Addr{ciliumIP1, ciliumIP2}, 100)
+	require.Equal(t, fqdn.UpdateStatus{Updated: true, Upserted: true}, res)
 	nameIPMapping = nameManager.mapSelectorsToNamesLocked(ciliumIOSel)
 	require.Len(t, nameIPMapping, 1)
 	ciliumIPs, ok = nameIPMapping[ciliumIOName]
@@ -91,9 +89,8 @@ func TestMapIPsToSelectors(t *testing.T) {
 	require.Equal(t, ciliumIP2, ciliumIPs[1])
 
 	// Two IPs again with long ttl.
-	updated, upserted = cache.Update(now, ciliumIOName, []netip.Addr{ciliumIP1, ciliumIP2}, 101)
-	require.True(t, updated)
-	require.False(t, upserted)
+	res = cache.Update(now, ciliumIOName, []netip.Addr{ciliumIP1, ciliumIP2}, 101)
+	require.Equal(t, fqdn.UpdateStatus{Updated: true, Upserted: false}, res)
 	nameIPMapping = nameManager.mapSelectorsToNamesLocked(ciliumIOSel)
 	require.Len(t, nameIPMapping, 1)
 	ciliumIPs, ok = nameIPMapping[ciliumIOName]
@@ -104,9 +101,8 @@ func TestMapIPsToSelectors(t *testing.T) {
 	require.Equal(t, ciliumIP2, ciliumIPs[1])
 
 	// Two IPs again with short ttl.
-	updated, upserted = cache.Update(now, ciliumIOName, []netip.Addr{ciliumIP1, ciliumIP2}, 1)
-	require.False(t, updated)
-	require.False(t, upserted)
+	res = cache.Update(now, ciliumIOName, []netip.Addr{ciliumIP1, ciliumIP2}, 1)
+	require.Equal(t, fqdn.UpdateStatus{Updated: false, Upserted: false}, res)
 	nameIPMapping = nameManager.mapSelectorsToNamesLocked(ciliumIOSel)
 	require.Len(t, nameIPMapping, 1)
 	ciliumIPs, ok = nameIPMapping[ciliumIOName]
