e0b896767302be8da30d0783fa51b94074ce3c31
Timo Beckers <timo@isovalent.com>
2025-11-20 10:37:27 +0100

    bpf/analyze: add BenchmarkReachabilityBPF for benching real Cilium progs
    Also adds two helpers to pkg/testutils: Glob and BenchmarkFiles. Does what it
    says on the tin.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/testutils/file.go
+++ pkg/testutils/file.go
@@ -6,6 +6,7 @@ package testutils
 import (
 	"errors"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -16,5 +17,34 @@ func SkipIfFileMissing(t testing.TB, file string) {
 	}
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+// Glob returns the list of files matching pattern or fails the test if an error
+// occurs.
+func Glob(tb testing.TB, pattern string) []string {
+	tb.Helper()
+
+	files, err := filepath.Glob(pattern)
+	if err != nil {
+		tb.Fatal("Can't glob files:", err)
+	}
+
+	return files
+}
+
+// BenchmarkFiles runs sub-benchmarks for each file in files, calling fn with
+// the testing.B and the file path.
+func BenchmarkFiles(b *testing.B, files []string, fn func(*testing.B, string)) {
+	b.Helper()
+
+	if len(files) == 0 {
+		b.Skip("No files given")
+	}
+
+	for _, f := range files {
+		b.Run(filepath.Base(f), func(b *testing.B) {
+			fn(b, f)
+		})
 	}
 }
