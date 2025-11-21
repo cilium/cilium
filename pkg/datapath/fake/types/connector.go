6c40152aabf5a7eedb7377e1d93cddeb2348a157
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2025-11-21 12:18:18 +0000

    pkg/datapath: Expand ConnectorConfig to include ConnectorModes.
    This commit introduces the concept of a ConnectorMode, which will be
    used to carry the Datapath Mode of Cilium moving forward.
    
    The ConnectorConfig structure is expanded to carry two additional
    members:
    
    - configuredMode:  articulates the desired datapath mode as set by the
                       daemon configuration.
    
    - operationalMode: articulates the operational or effective mode the
                       datapath is operating in once the agent has started.
    
    This lays the groundwork to support an "auto" datapath mode, which will
    probe the host system for its underlying capabilities, and automatically
    enable the netkit connector if approproate features are present in the
    kernel.
    
    This commit mirrors the configuredMode to the operationalMode for now,
    but this will be modified in the following commits.
    
    This commit also adds the default.DatapathMode into the DaemonConfig
    at startup, because configuration validation in the connector cell
    will be activated during test invocations.
    
    Signed-off-by: Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>

---
+++ pkg/datapath/fake/types/connector.go
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package types
+
+import "github.com/cilium/cilium/pkg/datapath/types"
+
+type fakeConnectorConfig struct {
+	configuredMode  types.ConnectorMode
+	operationalMode types.ConnectorMode
+}
+
+func NewFakeConnectorConfig(configuredMode types.ConnectorMode, operationalMode types.ConnectorMode) types.ConnectorConfig {
+	return &fakeConnectorConfig{
+		configuredMode:  configuredMode,
+		operationalMode: operationalMode,
+	}
+}
+func NewFakeConnectorVeth() types.ConnectorConfig {
+	return NewFakeConnectorConfig(types.ConnectorModeVeth, types.ConnectorModeVeth)
+}
+func NewFakeConnectorNetkit() types.ConnectorConfig {
+	return NewFakeConnectorConfig(types.ConnectorModeNetkit, types.ConnectorModeNetkit)
+}
+func NewFakeConnectorNetkitL2() types.ConnectorConfig {
+	return NewFakeConnectorConfig(types.ConnectorModeNetkitL2, types.ConnectorModeNetkitL2)
+}
+
+func (fcc fakeConnectorConfig) GetPodDeviceHeadroom() uint16 {
+	return 0
+}
+
+func (fcc fakeConnectorConfig) GetPodDeviceTailroom() uint16 {
+	return 0
+}
+
+func (fcc fakeConnectorConfig) GetConfiguredMode() types.ConnectorMode {
+	return fcc.configuredMode
+}
+
+func (fcc fakeConnectorConfig) GetOperationalMode() types.ConnectorMode {
+	return fcc.operationalMode
+}
