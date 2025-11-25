cilium/cilium

commit 08d891a289a4198f24a1a4b45c2995b2afb586dd

Add leader requirement to watch from Etcd.
In case of network partition of etcd, it's possible that watch will be hanging without any updates, which can lead to hard to debug issues.
This adds leader requirement. In this case if leader is not elected within 3 election cycles, watch will return error.

Signed-off-by: Marcel Zieba <marcel.zieba@isovalent.com>

pkg/kvstore/etcd.go
@@ -1018,7 +1018,7 @@ reList:
 		scopedLog.WithField(fieldRev, nextRev).Debug("Starting to watch a prefix")
 
 		e.limiter.Wait(ctx)
-		etcdWatch := e.client.Watch(ctx, w.Prefix,
+		etcdWatch := e.client.Watch(client.WithRequireLeader(ctx), w.Prefix,
 			client.WithPrefix(), client.WithRev(nextRev))
 		for {
 			select {

commit d36d09c0aaacc675cf7c0dbe28e3edcab347a3d6

etcd: require quorum on etcd range requests
This removes the only client.WithSerializable request present in the
agent. This can in theory result in slightly slower reads, but it
ensures we always get data from a functioning etcd cluster - and not a
partitioned server.

The main reason for doing this is handling the case when the etcd
cluster is in a non functioning state. This can happen for various
reasons, but when a cluster is lacking a leader - something is off.
Prior to 08d891a ("Add leader requirement to watch from Etcd.") we would
just hold onto a watch to the server when it had no quorum, but after
that patch we ensure to terminate watchers in such a condition. That did
however have the side-effect of resetting the list+watch setup. Then,
the list/range request with client.WithSerializable would succeed -
before the watch again failed. This would all work fine from a single
client perspective, but it would use a lot of cpu and memory on
unmarshalling the requests.

On a cluster scale, treating etcd quorum losses by running a set of
hotloops listing all data, across all cilium-agents, is pretty sub
optimal. In production this can lead to network starvation on the alive
etcd instance, causing instability across various components in the
system. On the same time, it could lead to memory spikes that again
leads to OOMs. When this happens, it could result in all cilium
instances constantly OOMing as they are being constantly spammed by
cilium-agents.

The new behavior is that the etcd client will do the retries inside the
Get call. Hence it would still call etcd, but it won't get any data back
while the cluster has no leader. This ensures that the overhead is low.
A further improvement could be to limit the length of a Get call by
specifying a context, but overall the etcd client should already handle
it.

A cilium-agent that cannot do quorum operations on the kvstore will
still report unhealthy and exit after waiting a while - so that is not
treated differently.

Another solution is to continue with client.WithSerializable here and do
proper backoff waits when watches fail, and we should probably do that
anyways, but that would still dump data from etcd when its in a non
quorum situation - potentially making things worse.

Fixes: 08d891a ("Add leader requirement to watch from Etcd.")

pkg/kvstore/etcd.go
@@ -891,8 +891,7 @@ func (e *etcdClient) paginatedList(ctx context.Context, log *slog.Logger, prefix
 	for {
 		res, err := e.client.Get(ctx, start, client.WithRange(end),
 			client.WithSort(client.SortByKey, client.SortAscend),
-			client.WithRev(revision), client.WithSerializable(),
-			client.WithLimit(int64(e.listBatchSize)),
+			client.WithRev(revision), client.WithLimit(int64(e.listBatchSize)),
 		)
 		if err != nil {
 			return nil, 0, err