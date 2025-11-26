ded729fca14138590933e3e11815b9ef651c9a58
Julian Wiedmann <jwi@isovalent.com>
2025-11-26 09:19:31 +0200

    bpf: lb: remove `saddr` variable in lb*_local()
    All usage has essentially moved to lb*_dnat_request() now, and for the IPv6
    function we were already relying on the __maybe_unused to suppress the
    compile error.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/lb.h
+++ bpf/lib/lb.h
@@ -1276,7 +1276,6 @@ static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
 				     __s8 *ext_err)
 {
 	__u32 monitor; /* Deliberately ignored; regular CT will determine monitoring. */
-	union v6addr saddr __maybe_unused = tuple->saddr;
 	__u8 flags = tuple->flags;
 	const struct lb6_backend *backend;
 	__u32 backend_id = 0;
@@ -1988,13 +1987,12 @@ static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
 				     __s8 *ext_err)
 {
 	__u32 monitor; /* Deliberately ignored; regular CT will determine monitoring. */
-	__be32 saddr __maybe_unused = tuple->saddr;
 	__u8 flags = tuple->flags;
 	const struct lb4_backend *backend;
 	__u32 backend_id = 0;
 	int ret;
 	union lb4_affinity_client_id client_id = {
-		.client_ip = saddr,
+		.client_ip = tuple->saddr,
 	};
 
 	state->rev_nat_index = svc->rev_nat_index;
