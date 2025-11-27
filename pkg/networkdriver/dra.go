04fc4404a9c7f2d3dd11db43b5a7d66babc73590
Bernardo Soares <20172413+bersoare@users.noreply.github.com>
2025-11-27 15:36:22 +0000

    network-driver: add new network-driver feature pkg
    network-driver is a new feature introduced that enables
    exposing unmanaged network interfaces to pods.
    
    adding sr-iov interface support and basic netdev configuration
    inside the pod
    
    Signed-off-by: Bernardo Soares <20172413+bersoare@users.noreply.github.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

---
+++ pkg/networkdriver/dra.go
@@ -0,0 +1,208 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package networkdriver
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"path"
+
+	resourceapi "k8s.io/api/resource/v1"
+	kube_types "k8s.io/apimachinery/pkg/types"
+	"k8s.io/dynamic-resource-allocation/kubeletplugin"
+
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/networkdriver/types"
+)
+
+// HandleError logs out error messages from kubelet.
+func (d *Driver) HandleError(ctx context.Context, err error, msg string) {
+	d.logger.ErrorContext(
+		ctx, "HandleError called:",
+		logfields.Error, err,
+		logfields.Message, msg,
+	)
+}
+
+// unprepareResourceClaim removes an allocation and frees up the device.
+func (d *Driver) unprepareResourceClaim(ctx context.Context, claim kubeletplugin.NamespacedObject) error {
+	var errs error
+	var found bool
+
+	for pod, alloc := range d.allocations {
+		devices, ok := alloc[claim.UID]
+		if ok {
+			found = true
+			for _, dev := range devices {
+				if err := dev.Device.Free(dev.Config); err != nil {
+					errors.Join(errs, err)
+				}
+			}
+		}
+
+		if found {
+			delete(alloc, claim.UID)
+			// see if pod ended up without any allocations.
+			// clean it up if we just removed the last one.
+			if len(alloc) == 0 {
+				delete(d.allocations, pod)
+			}
+
+			break
+		}
+	}
+
+	if !found {
+		d.logger.DebugContext(
+			ctx, "no allocation found for claim",
+			logfields.UID, claim.UID,
+			logfields.K8sNamespace, claim.Namespace,
+			logfields.Name, claim.Name,
+		)
+	}
+
+	return errs
+}
+
+// UnprepareResourceClaims gets called whenever we have a request to deallocate a resource claim. ex: pod goes away.
+func (driver *Driver) UnprepareResourceClaims(ctx context.Context, claims []kubeletplugin.NamespacedObject) (result map[kube_types.UID]error, err error) {
+	driver.logger.DebugContext(ctx, fmt.Sprintf("UnprepareResourceClaims called with %d claims", len(claims)))
+
+	result = make(map[kube_types.UID]error, len(claims))
+
+	err = driver.withLock(func() error {
+		for _, c := range claims {
+			result[c.UID] = driver.unprepareResourceClaim(ctx, c)
+			driver.logger.DebugContext(
+				ctx, "freeing resources for claim",
+				logfields.Name, c.Name,
+				logfields.K8sNamespace, c.Namespace,
+				logfields.UID, string(c.UID),
+				logfields.Error, result[c.UID],
+			)
+		}
+
+		return nil
+	})
+
+	return result, err
+}
+
+func (driver *Driver) prepareResourceClaim(ctx context.Context, claim *resourceapi.ResourceClaim) kubeletplugin.PrepareResult {
+	if len(claim.Status.ReservedFor) != 1 {
+		return kubeletplugin.PrepareResult{
+			Err: fmt.Errorf("%w: Status.ReservedFor field has more than one entry", errUnexpectedInput),
+		}
+	}
+
+	pod := claim.Status.ReservedFor[0]
+
+	if _, ok := driver.allocations[pod.UID]; ok {
+		return kubeletplugin.PrepareResult{
+			Err: fmt.Errorf("%w: name: %s, resource: %s, uid: %s", errAllocationAlreadyExistsForPod, pod.Name, pod.Resource, pod.UID),
+		}
+	}
+
+	var alloc []allocation
+
+	for _, result := range claim.Status.Allocation.Devices.Results {
+		var thisAlloc allocation
+
+		for _, cfg := range claim.Status.Allocation.Devices.Config {
+			for _, reqName := range cfg.Requests {
+				var foundConfig bool
+
+				if reqName == result.Request && cfg.Opaque.Parameters.Raw != nil {
+					c := types.DeviceConfig{}
+					err := json.Unmarshal(cfg.Opaque.Parameters.Raw, &c)
+					if err != nil {
+						driver.logger.ErrorContext(
+							ctx, "failed to parse config",
+							logfields.Request, reqName,
+							logfields.Params, cfg.Opaque.Parameters,
+							logfields.Error, err,
+						)
+
+						return kubeletplugin.PrepareResult{
+							Err: fmt.Errorf("failed to unmarshal config for %s: %w", path.Join(claim.Namespace, claim.Name), err),
+						}
+					}
+
+					thisAlloc.Config = c
+					foundConfig = true
+					break
+				}
+
+				if foundConfig {
+					// we found a config for this, no need to look further
+					break
+				}
+			}
+		}
+
+		var found bool
+
+		for _, device := range driver.devices {
+			if device.IfName() == result.Device {
+				thisAlloc.Device = device
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return kubeletplugin.PrepareResult{
+				Err: fmt.Errorf("%w with ifname %s for %s", errDeviceNotFound, result.Device, path.Join(claim.Namespace, claim.Name)),
+			}
+		}
+
+		if err := thisAlloc.Device.Setup(thisAlloc.Config); err != nil {
+			driver.logger.ErrorContext(ctx, "failed to set up device",
+				logfields.Device, thisAlloc.Device.IfName(),
+				logfields.Config, thisAlloc.Config,
+				logfields.Error, err,
+			)
+
+			return kubeletplugin.PrepareResult{
+				Err: fmt.Errorf("%w for ifname %s on %s", err, thisAlloc.Device.IfName(), path.Join(claim.Namespace, claim.Name)),
+			}
+		}
+
+		alloc = append(alloc, thisAlloc)
+	}
+	driver.allocations[pod.UID] = make(map[kube_types.UID][]allocation)
+	driver.allocations[pod.UID][claim.UID] = alloc
+
+	// we dont need to return anything here.
+	return kubeletplugin.PrepareResult{}
+}
+
+// PrepareResourceClaims gets called when we have a request to allocate a resource claim. we also need to have a way to remember
+// the allocations elsewhere so allocation state persist across restarts in the plugin.
+func (driver *Driver) PrepareResourceClaims(ctx context.Context, claims []*resourceapi.ResourceClaim) (result map[kube_types.UID]kubeletplugin.PrepareResult, err error) {
+	driver.logger.DebugContext(ctx, fmt.Sprintf("PrepareResourceClaims called with %d claims", len(claims)))
+
+	result = make(map[kube_types.UID]kubeletplugin.PrepareResult)
+
+	err = driver.withLock(func() error {
+		for _, c := range claims {
+			l := driver.logger.With(
+				logfields.K8sNamespace, c.Namespace,
+				logfields.UID, c.UID,
+				logfields.Name, c.Name,
+			)
+			result[c.UID] = driver.prepareResourceClaim(ctx, c)
+
+			l.DebugContext(ctx, "allocation for claim",
+				logfields.Result, result[c.UID],
+			)
+		}
+
+		return nil
+	})
+
+	return result, err
+}
