cilium/cilium

commit 243c6c20e58fb606e88f910f78cc698ba36d020c

bpf: nodeport: stash DSR RevDNAT info in CT entry
There's sufficient space in the CT entry to store the NAT info. Start
making use of it for DSR, so we can eventually remove its usage of a
single entry in the NAT map.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/conntrack.h
@@ -39,6 +39,8 @@ enum ct_entry_type {
 };
 
 struct ct_state {
+	union v6addr nat_addr;
+	__be16 nat_port;
 	__u16 rev_nat_index;
 #ifdef USE_LOOPBACK_LB
 	__u16 loopback:1,
@@ -85,8 +87,15 @@ struct ct_buffer6 {
 };
 
 struct ct_entry {
-	__u64 reserved0;	/* unused since v1.16 */
-	__u64 backend_id;
+	union {
+		/* For CT_EGRESS entry: */
+		union v6addr nat_addr;
+		/* For CT_SERVICE entry: */
+		struct {
+			__u64 reserved0;	/* unused since v1.16 */
+			__u64 backend_id;
+		};
+	};
 	__u64 packets;
 	__u64 bytes;
 	__u32 lifetime;
@@ -103,7 +112,7 @@ struct ct_entry {
 	      from_tunnel:1,	/* Connection is over tunnel */
 	      reserved3:5;
 	__u16 rev_nat_index;
-	__u16 reserved4;	/* unused since v1.18 */
+	__be16 nat_port;	/* For CT_EGRESS entry. */
 
 	/* *x_flags_seen represents the OR of all TCP flags seen for the
 	 * transmit/receive direction of this entry.
@@ -1046,6 +1055,8 @@ ct_create_fill_entry(struct ct_entry *entry, const struct ct_state *state,
 #ifdef USE_LOOPBACK_LB
 		entry->lb_loopback = state->loopback;
 #endif
+		ipv6_addr_copy(&entry->nat_addr, &state->nat_addr);
+		entry->nat_port = state->nat_port;
 		entry->node_port = state->node_port;
 		entry->dsr_internal = state->dsr_internal;
 		entry->from_tunnel = state->from_tunnel;

bpf/lib/nodeport.h
@@ -810,6 +810,8 @@ create_ct:
 		if (port == 0)
 			return DROP_INVALID;
 
+		ipv6_addr_copy(&ct_state_new.nat_addr, addr);
+		ct_state_new.nat_port = port;
 		ct_state_new.src_sec_id = WORLD_IPV6_ID;
 		ct_state_new.dsr_internal = 1;
 
@@ -2196,6 +2198,8 @@ create_ct:
 			 */
 			return DROP_INVALID;
 
+		ct_state_new.nat_addr.p4 = addr;
+		ct_state_new.nat_port = port;
 		ct_state_new.src_sec_id = WORLD_IPV4_ID;
 		ct_state_new.dsr_internal = 1;
 
@@ -2210,12 +2214,15 @@ create_ct:
 	case CT_ESTABLISHED:
 		/* For TCP we only expect DSR info on the SYN, so CT_ESTABLISHED
 		 * is unexpected and we need to refresh the CT entry.
-		 *
-		 * Otherwise we tolerate DSR info on an established connection.
-		 * TODO: how do we know if we need to refresh the SNAT entry?
 		 */
 		if (tuple->nexthdr == IPPROTO_TCP && port)
 			goto create_ct;
+
+		 /*
+		 * Otherwise we tolerate DSR info on an established connection.
+		 * TODO: obtain NAT info from CT lookup, and compare it against
+		 *	 DSR info. Re-create on mismatch.
+		 */
 		break;
 	default:
 		return DROP_UNKNOWN_CT;

pkg/maps/ctmap/ctmap.go
@@ -388,7 +388,7 @@ func purgeCtEntry(m *Map, key CtKey, entry *CtEntry, natMap *nat.Map, next func(
 	tupleType := t.GetFlags()
 
 	if tupleType == tuple.TUPLE_F_SERVICE && ACT != nil {
-		actCountFailed(entry.RevNAT, uint32(entry.BackendID))
+		actCountFailed(entry.RevNAT, uint32(entry.Union0[1]))
 	}
 
 	next(GCEvent{

pkg/maps/ctmap/types.go
@@ -525,15 +525,14 @@ func (k *CtKey6Global) GetTupleKey() tuple.TupleKey {
 
 // CtEntry represents an entry in the connection tracking table.
 type CtEntry struct {
-	Reserved0 uint64 `align:"reserved0"`
-	BackendID uint64 `align:"backend_id"`
-	Packets   uint64 `align:"packets"`
-	Bytes     uint64 `align:"bytes"`
-	Lifetime  uint32 `align:"lifetime"`
-	Flags     uint16 `align:"rx_closing"`
+	Union0   [2]uint64 `align:"$union0"`
+	Packets  uint64    `align:"packets"`
+	Bytes    uint64    `align:"bytes"`
+	Lifetime uint32    `align:"lifetime"`
+	Flags    uint16    `align:"rx_closing"`
 	// RevNAT is in network byte order
 	RevNAT           uint16 `align:"rev_nat_index"`
-	Reserved4        uint16 `align:"reserved4"`
+	NatPort          uint16 `align:"nat_port"`
 	TxFlagsSeen      uint8  `align:"tx_flags_seen"`
 	RxFlagsSeen      uint8  `align:"rx_flags_seen"`
 	SourceSecurityID uint32 `align:"src_sec_id"`
@@ -614,7 +613,7 @@ func (c *CtEntry) StringWithTimeDiff(toRemSecs func(uint32) string) string {
 		timeDiff = ""
 	}
 
-	return fmt.Sprintf("expires=%d%s Packets=%d Bytes=%d RxFlagsSeen=%#02x LastRxReport=%d TxFlagsSeen=%#02x LastTxReport=%d %s RevNAT=%d SourceSecurityID=%d BackendID=%d \n",
+	return fmt.Sprintf("expires=%d%s Packets=%d Bytes=%d RxFlagsSeen=%#02x LastRxReport=%d TxFlagsSeen=%#02x LastTxReport=%d %s RevNAT=%d SourceSecurityID=%d BackendID=%d NatPort=%d \n",
 		c.Lifetime,
 		timeDiff,
 		c.Packets,
@@ -626,7 +625,9 @@ func (c *CtEntry) StringWithTimeDiff(toRemSecs func(uint32) string) string {
 		c.flagsString(),
 		byteorder.NetworkToHost16(c.RevNAT),
 		c.SourceSecurityID,
-		c.BackendID)
+		c.Union0[1],
+		// TODO NatAddr, either IPv4 or IPv6
+		c.NatPort)
 }
 
 // String returns the readable format

commit 2d2efa9c6d920a230fed07cc23106b7a640ffefc

bpf: conntrack: populate nat info in ct_lookup_fill_state
In commit 243c6c20e58f ("bpf: nodeport: stash DSR RevDNAT info in CT
entry"), nat_addr and nat_port fields were added to the CT entry but
ct_lookup_fill_state() was not updated to copy these values into the
ct_state structure.
Fix by copying nat_addr and nat_port from the CT entry to ct_state.

Fixes: 243c6c20e58f ("bpf: nodeport: stash DSR RevDNAT info in CT entry")

Signed-off-by: Gyutae Bae <gyutae.bae@navercorp.com>

bpf/lib/conntrack.h
@@ -275,6 +275,8 @@ ct_lookup_fill_state(struct ct_state *state, const struct ct_entry *entry,
 #ifdef USE_LOOPBACK_LB
 		state->loopback = entry->lb_loopback;
 #endif
+		ipv6_addr_copy(&state->nat_addr, &entry->nat_addr);
+		state->nat_port = entry->nat_port;
 		state->node_port = entry->node_port;
 		state->dsr_internal = entry->dsr_internal;
 		state->proxy_redirect = entry->proxy_redirect;