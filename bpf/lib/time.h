382f548e2e5107b317bd60a97362925746af440d
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2025-12-03 20:57:34 +0100

    bpf, time: remove mono header
    We have just a few time-helper macros that are spread across time and
    mono headers. The commit moves all macros from the mono header into the
    time header and removes the mono header. The motivation here is to keep
    all time helper code in a single header file.
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- bpf/lib/time.h
+++ bpf/lib/time.h
@@ -15,3 +15,12 @@
 /* Jiffies */
 #define bpf_sec_to_jiffies(s)	\
 	({ __u64 __x = (s) * KERNEL_HZ; __x; })
+
+#if defined(ENABLE_JIFFIES) && KERNEL_HZ != 1
+# define BPF_MONO_SCALER	8
+# define bpf_mono_now()		(jiffies >> BPF_MONO_SCALER)
+# define bpf_sec_to_mono(s)	((__u32)bpf_sec_to_jiffies(s) >> BPF_MONO_SCALER)
+#else
+# define bpf_mono_now()		bpf_ktime_get_sec()
+# define bpf_sec_to_mono(s)	(s)
+#endif /* ENABLE_JIFFIES */
