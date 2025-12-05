cilium/cilium

commit c926ac3051b698afb03ae36ad79ba880f1b49e0d

maps: retrieve per-cluster maps for GC through Hive
Currently, the per-cluster maps are accessed by the GC logic through a
global variable. Let's instead propagate a function to retrieve them
through the Hive framework, to prepare for the subsequent removal of
the global variable and better align with the overall dependency
injection approach.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/maps/ctmap/gc/gc.go
@@ -32,6 +32,8 @@ type EndpointManager interface {
 	GetEndpoints() []*endpoint.Endpoint
 }
 
+type PerClusterCTMapsRetriever func() []*ctmap.Map
+
 type parameters struct {
 	cell.In
 
@@ -41,6 +43,8 @@ type parameters struct {
 	EndpointManager EndpointManager
 	Datapath        types.Datapath
 	SignalManager   SignalHandler
+
+	PerClusterCTMapsRetriever PerClusterCTMapsRetriever `optional:"true"`
 }
 
 type GC struct {
@@ -52,6 +56,8 @@ type GC struct {
 	endpointsManager EndpointManager
 	nodeAddressing   types.NodeAddressing
 	signalHandler    SignalHandler
+
+	perClusterCTMapsRetriever PerClusterCTMapsRetriever
 }
 
 func New(params parameters) *GC {
@@ -201,16 +207,10 @@ func (gc *GC) runGC(e *endpoint.Endpoint, ipv4, ipv6, triggeredBySignal bool, fi
 	if e == nil {
 		maps = ctmap.GlobalMaps(ipv4, ipv6)
 
-		// We treat per-cluster CT Maps as global map. When we don't enable
-		// cluster-aware addressing, ctmap.PerClusterCTMaps is nil (this is
-		// the default).
-		if ctmap.PerClusterCTMaps != nil {
-			perClusterMaps, err := ctmap.PerClusterCTMaps.GetAllClusterCTMaps()
-			if err != nil {
-				gc.logger.Error("Failed to get per-cluster CT maps. Continue without them.")
-			} else {
-				maps = append(maps, perClusterMaps...)
-			}
+		// We treat per-cluster CT Maps as global maps. When we don't enable
+		// cluster-aware addressing, perClusterCTMapsRetriever is nil (default).
+		if gc.perClusterCTMapsRetriever != nil {
+			maps = append(maps, gc.perClusterCTMapsRetriever()...)
 		}
 	} else {
 		maps = ctmap.LocalMaps(e, ipv4, ipv6)

commit fb05621e90dda9ad52f67586c6a7cd05c1cb3cdf

ctmaps: correctly hook GC of per-cluster ctmaps
The blamed commit refactored the retrieval of the per-cluster ctmaps
as part of the corresponding GC logic, but missed actually hooking
the new parameter into place; hence, the optional retriever method is
never invoked. Let's get this fixed, so that per-cluster maps can be
also GCed correctly.

Fixes: c926ac3051b6 ("maps: retrieve per-cluster maps for GC through Hive")
Reported-by: Marco Hofstetter <marco.hofstetter@isovalent.com>
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/maps/ctmap/gc/gc.go
@@ -100,6 +100,8 @@ func New(params parameters) *GC {
 		signalHandler:    params.SignalManager,
 
 		controllerManager: controller.NewManager(),
+
+		perClusterCTMapsRetriever: params.PerClusterCTMapsRetriever,
 	}
 
 	gc.observable4, gc.next4, gc.complete4 = stream.Multicast[ctmap.GCEvent]()