cilium/cilium

commit a3bd1a69b3b262209bfa6a614ab9c86eca4f7d0f

ipcache: Consolidate prefixes from various resources
When many CIDRs are inserted into the ipcache via the metadata API (i.e.
UpsertMetadataBatch() / UpsertMetadata()) and many of those CIDRs are
present across many various resources such as CNPs (aka "duplicated"
CIDRs across many policies), the ipcache ends up performing many
redundant label merging operations via ToLabels() inside
resolveIdentity(). See BenchmarkManyCIDREntries() for more context.

To optimize the ipcache metadata structure and avoid storing duplicate
instances of metadata (particularly CIDR prefixes from policies), we
handle CIDR prefixes outside the cluster as a special case. Instead of
tracking each CIDR by its originating policy, we consolidate them by
treating each CIDR as originating from its own dedicated resource. This
approach reduces duplication.

The above is done by a refcount of each CIDR prefix that is destined for
outside of the cluster.

Benchmark results:

```
$ go test -v ./pkg/ipcache -test.run '^$' -test.bench 'BenchmarkManyCIDREntries' -count 10 > old
$ go test -v ./pkg/ipcache -test.run '^$' -test.bench 'BenchmarkManyCIDREntries' -count 10 > new
$ benchstat old new
name                old time/op    new time/op    delta
ManyCIDREntries-16    35.4ms ± 4%     0.2ms ± 1%  -99.33%  (p=0.000 n=10+10)

name                old alloc/op   new alloc/op   delta
ManyCIDREntries-16    4.94MB ± 1%    0.22MB ± 0%  -95.52%  (p=0.000 n=10+10)

name                old allocs/op  new allocs/op  delta
ManyCIDREntries-16     33.4k ± 1%      4.0k ± 0%  -88.11%  (p=0.000 n=10+10)
```

Suggested-by: Casey Callendrello <cdc@isovalent.com>
Suggested-by: Sebastian Wicki <sebastian@isovalent.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipcache/ipcache.go
@@ -510,6 +510,7 @@ type MU struct {
 	Source   source.Source
 	Resource ipcacheTypes.ResourceID
 	Metadata []IPMetadata
+	IsCIDR   bool
 }
 
 // UpsertMetadata upserts a given IP and some corresponding information into
@@ -529,7 +530,9 @@ func (ipc *IPCache) UpsertMetadataBatch(updates ...MU) (revision uint64) {
 	prefixes := make([]cmtypes.PrefixCluster, 0, len(updates))
 	ipc.metadata.Lock()
 	for _, upd := range updates {
-		prefixes = append(prefixes, ipc.metadata.upsertLocked(upd.Prefix, upd.Source, upd.Resource, upd.Metadata...)...)
+		if !upd.IsCIDR || ipc.metadata.prefixRefCounter.Add(upd.Prefix) {
+			prefixes = append(prefixes, ipc.metadata.upsertLocked(upd.Prefix, upd.Source, upd.Resource, upd.Metadata...)...)
+		}
 	}
 	ipc.metadata.Unlock()
 	revision = ipc.metadata.enqueuePrefixUpdates(prefixes...)
@@ -558,7 +561,9 @@ func (ipc *IPCache) RemoveMetadataBatch(updates ...MU) (revision uint64) {
 	prefixes := make([]cmtypes.PrefixCluster, 0, len(updates))
 	ipc.metadata.Lock()
 	for _, upd := range updates {
-		prefixes = append(prefixes, ipc.metadata.remove(upd.Prefix, upd.Resource, upd.Metadata...)...)
+		if !upd.IsCIDR || ipc.metadata.prefixRefCounter.Delete(upd.Prefix) {
+			prefixes = append(prefixes, ipc.metadata.remove(upd.Prefix, upd.Resource, upd.Metadata...)...)
+		}
 	}
 	ipc.metadata.Unlock()
 	revision = ipc.metadata.enqueuePrefixUpdates(prefixes...)

pkg/ipcache/metadata_test.go
@@ -1475,6 +1475,7 @@ func BenchmarkManyCIDREntries(b *testing.B) {
 				Source:   source.Generated,
 				Resource: types.NewResourceID(types.ResourceKindCNP, fmt.Sprintf("namespace_%d", i), "my-policy"),
 				Metadata: []IPMetadata{lbls},
+				IsCIDR:   true,
 			})
 		}
 		revs = append(revs, IPIdentityCache.UpsertMetadataBatch(mu...))
@@ -1495,6 +1496,7 @@ func BenchmarkManyCIDREntries(b *testing.B) {
 				Source:   source.Generated,
 				Resource: types.NewResourceID(types.ResourceKindCNP, fmt.Sprintf("namespace_%d", i), "my-policy"),
 				Metadata: []IPMetadata{labels.Labels{}},
+				IsCIDR:   true,
 			})
 		}
 		revs = append(revs, IPIdentityCache.RemoveMetadataBatch(mu...))
@@ -1515,6 +1517,7 @@ func BenchmarkManyCIDREntries(b *testing.B) {
 				Source:   source.Generated,
 				Resource: types.NewResourceID(types.ResourceKindCNP, fmt.Sprintf("namespace_%d", i), "my-policy"),
 				Metadata: []IPMetadata{cidrLabels[cidr]},
+				IsCIDR:   true,
 			})
 		}
 		revs = append(revs, IPIdentityCache.UpsertMetadataBatch(mu...))

pkg/ipcache/metadata.go
@@ -18,6 +18,7 @@ import (
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/container/bitlpm"
 	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/counter"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipcache/types"
 	"github.com/cilium/cilium/pkg/labels"
@@ -84,6 +85,11 @@ type metadata struct {
 	// clusterID, in order to find descendants efficiently.
 	prefixes *bitlpm.CIDRTrieMap[clusterID, struct{}]
 
+	// prefixRefCounter keeps a reference count of all prefixes that come from
+	// policy resources, as an optimization, in order to avoid redundantly
+	// storing all prefixes from policies.
+	prefixRefCounter counter.Counter[cmtypes.PrefixCluster]
+
 	// queued* handle updates into the IPCache. Whenever a label is added
 	// or removed from a specific IP prefix, that prefix is added into
 	// 'queuedPrefixes'. Each time label injection is triggered, it will
@@ -117,10 +123,11 @@ type metadata struct {
 
 func newMetadata() *metadata {
 	return &metadata{
-		m:              make(map[cmtypes.PrefixCluster]prefixInfo),
-		prefixes:       bitlpm.NewCIDRTrieMap[clusterID, struct{}](),
-		queuedPrefixes: make(map[cmtypes.PrefixCluster]struct{}),
-		queuedRevision: 1,
+		m:                make(map[cmtypes.PrefixCluster]prefixInfo),
+		prefixes:         bitlpm.NewCIDRTrieMap[clusterID, struct{}](),
+		prefixRefCounter: make(counter.Counter[cmtypes.PrefixCluster]),
+		queuedPrefixes:   make(map[cmtypes.PrefixCluster]struct{}),
+		queuedRevision:   1,
 
 		injectedRevisionCond: sync.NewCond(&lock.Mutex{}),
 

pkg/policy/cell/policy_importer.go
@@ -170,6 +170,7 @@ func (i *policyImporter) updatePrefixes(ctx context.Context, updates []*policyty
 					Source:   prefixSource[resource],
 					Resource: resource,
 					Metadata: []ipcache.IPMetadata{labels.GetCIDRLabels(prefix)},
+					IsCIDR:   true,
 				})
 			}
 			continue
@@ -202,6 +203,7 @@ func (i *policyImporter) updatePrefixes(ctx context.Context, updates []*policyty
 				Source:   prefixSource[resource],
 				Resource: resource,
 				Metadata: []ipcache.IPMetadata{labels.GetCIDRLabels(prefix)},
+				IsCIDR:   true,
 			})
 		}
 		if oldSet.Len() > 0 {
@@ -245,6 +247,7 @@ func (i *policyImporter) prunePrefixes(prunePrefixes map[ipcachetypes.ResourceID
 				Prefix:   cmtypes.NewLocalPrefixCluster(oldPrefix),
 				Resource: resource,
 				Metadata: []ipcache.IPMetadata{labels.Labels{}},
+				IsCIDR:   true,
 			})
 		}
 	}

commit be129741a43444ae78ab4ce3c3db8ee7730f7017

ipcache: Fix leak in CIDR metadata consolidation logic
It is possible to leak CIDR entries in the ipcache metadata layer due to
mishandling the ipcache resource for CIDR entries. The CIDR entries were
inserted into the ipcache but in a way where we attempted to optimize
the entries by consolidating all CIDRs that appear in policies into a
single resource per CIDR. See added test for more details on the leak.

Also, the logic checks that it is only a CIDR entry (only
from CIDR statements in policies) before consolidating. This way, we
don't break the FQDN name manager when there is overlap in the CIDRs
because we don't want to consolidate the ipcache metadata resources when
they are of different types.

Fixes: https://github.com/cilium/cilium/pull/39176
Fixes: a3bd1a69b3b262209bfa6a614ab9c86eca4f7d0f ("ipcache: Consolidate prefixes from various resources")
Reported-by: Sebastian Wicki <sebastian@isovalent.com>
Co-authored-by: Odin Ugedal <odin@uged.al>
Co-authored-by: Odin Ugedal <ougedal@palantir.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipcache/ipcache.go
@@ -504,6 +504,11 @@ type MU struct {
 	IsCIDR   bool
 }
 
+// cidrResourceID is a fixed resource type to represent CIDR metadata entries
+// from policies. This resource type represents entries from the CIDR
+// consolidation optimization logic.
+var cidrResourceID = ipcacheTypes.NewResourceID(ipcacheTypes.ResourceKindDaemon, "", "consolidated-prefix")
+
 // UpsertMetadata upserts a given IP and some corresponding information into
 // the ipcache metadata map. See IPMetadata for a list of types that are valid
 // to pass into this function. This will trigger asynchronous calculation of
@@ -522,7 +527,11 @@ func (ipc *IPCache) UpsertMetadataBatch(updates ...MU) (revision uint64) {
 	ipc.metadata.Lock()
 	for _, upd := range updates {
 		if !upd.IsCIDR || ipc.metadata.prefixRefCounter.Add(upd.Prefix) {
-			prefixes = append(prefixes, ipc.metadata.upsertLocked(upd.Prefix, upd.Source, upd.Resource, upd.Metadata...)...)
+			resource := upd.Resource
+			if upd.IsCIDR {
+				resource = cidrResourceID
+			}
+			prefixes = append(prefixes, ipc.metadata.upsertLocked(upd.Prefix, upd.Source, resource, upd.Metadata...)...)
 		}
 	}
 	ipc.metadata.Unlock()
@@ -553,7 +562,11 @@ func (ipc *IPCache) RemoveMetadataBatch(updates ...MU) (revision uint64) {
 	ipc.metadata.Lock()
 	for _, upd := range updates {
 		if !upd.IsCIDR || ipc.metadata.prefixRefCounter.Delete(upd.Prefix) {
-			prefixes = append(prefixes, ipc.metadata.remove(upd.Prefix, upd.Resource, upd.Metadata...)...)
+			resource := upd.Resource
+			if upd.IsCIDR {
+				resource = cidrResourceID
+			}
+			prefixes = append(prefixes, ipc.metadata.remove(upd.Prefix, resource, upd.Metadata...)...)
 		}
 	}
 	ipc.metadata.Unlock()

pkg/ipcache/metadata_test.go
@@ -1406,6 +1406,122 @@ func TestIPCachePodCIDREntries(t *testing.T) {
 	assert.Equal(t, identity.ReservedIdentityWorldIPv4, nid.ID)
 }
 
+// TestIPCacheCIDRResourceConsolidation tests for a leak in the ipcache
+// metadata layer regarding the CIDR resource consolidation.
+func TestIPCacheCIDRResourceConsolidation(t *testing.T) {
+	s := setupIPCacheTestSuite(t)
+
+	prefix := cmtypes.NewLocalPrefixCluster(netip.MustParsePrefix("10.0.0.0/8"))
+	lbls := labels.GetCIDRLabels(prefix.AsPrefix())
+
+	//
+	// Test 1: Regression test of CIDR leak:
+	//
+
+	// CIDR 10.0.0.0/8 is added with resource owner policy-foo. Refcount for
+	// the CIDR is bumped to 1, which means this if statement is true and we
+	// upsert 10.0.0.0/8 with owner policy-foo.
+	assert.NoError(t, s.IPIdentityCache.WaitForRevision(
+		context.TODO(), s.IPIdentityCache.UpsertMetadataBatch(MU{
+			Prefix:   prefix,
+			Source:   source.Generated,
+			Resource: types.NewResourceID(types.ResourceKindCNP, "default", "policy-foo"),
+			Metadata: []IPMetadata{lbls},
+			IsCIDR:   true,
+		}),
+	))
+	// CIDR 10.0.0.0/8 is added with resource owner policy-bar. The above if
+	// statement is false, the CIDR is not forwarded to the metadata tracker
+	// (as it should).
+	assert.NoError(t, s.IPIdentityCache.WaitForRevision(
+		context.TODO(), s.IPIdentityCache.UpsertMetadataBatch(MU{
+			Prefix:   prefix,
+			Source:   source.Generated,
+			Resource: types.NewResourceID(types.ResourceKindCNP, "default", "policy-bar"),
+			Metadata: []IPMetadata{lbls},
+			IsCIDR:   true,
+		}),
+	))
+	// Policy policy-foo is deleted. This decreases refcount to 1, which means
+	// the deletion would not be forwarded to the metadata tracker (as it
+	// should).
+	assert.NoError(t, s.IPIdentityCache.WaitForRevision(
+		context.TODO(), s.IPIdentityCache.RemoveMetadataBatch(MU{
+			Prefix:   prefix,
+			Source:   source.Generated,
+			Resource: types.NewResourceID(types.ResourceKindCNP, "default", "policy-foo"),
+			Metadata: []IPMetadata{lbls},
+			IsCIDR:   true,
+		}),
+	))
+	// Policy policy-bar is deleted. This decreases the refcount to 0. This if
+	// statement is now true, but the deletion event is forwarded with resource
+	// owner policy-bar to the metadata tracker. The metadata tracker never
+	// heard about policy-bar (it still has -foo stored) and therefore ignores
+	// this deletion event. The CIDR is leaked.
+	assert.NoError(t, s.IPIdentityCache.WaitForRevision(
+		context.TODO(), s.IPIdentityCache.RemoveMetadataBatch(MU{
+			Prefix:   prefix,
+			Source:   source.Generated,
+			Resource: types.NewResourceID(types.ResourceKindCNP, "default", "policy-bar"),
+			Metadata: []IPMetadata{lbls},
+			IsCIDR:   true,
+		}),
+	))
+	assert.Nil(t, s.IPIdentityCache.metadata.get(prefix))
+
+	//
+	// Test 2: Mix of CIDRs and FQDN
+	//
+	cidr := cmtypes.NewLocalPrefixCluster(netip.MustParsePrefix("10.0.0.1/32"))
+	cidrOverlapResource := types.NewResourceID(types.ResourceKindCNP, "default", "policy-fqdn-overlap")
+	fqdnNameManagerResource := types.NewResourceID(types.ResourceKindDaemon, "", "fqdn-name-manager")
+	assert.NoError(t, s.IPIdentityCache.WaitForRevision(
+		context.TODO(), s.IPIdentityCache.UpsertMetadataBatch(MU{
+			Prefix:   cidr,
+			Source:   source.Generated,
+			Resource: cidrOverlapResource,
+			Metadata: []IPMetadata{labels.GetCIDRLabels(cidr.AsPrefix())},
+			IsCIDR:   true,
+		}),
+	))
+	assert.NoError(t, s.IPIdentityCache.WaitForRevision(
+		context.TODO(), s.IPIdentityCache.UpsertMetadataBatch(MU{
+			Prefix:   cidr,
+			Source:   source.Generated,
+			Resource: fqdnNameManagerResource,
+			Metadata: []IPMetadata{labels.NewLabelsFromSortedList("fqdn:foo.com")},
+			IsCIDR:   false,
+		}),
+	))
+	assert.Len(t, s.IPIdentityCache.metadata.m[cidr].byResource, 2)
+	// UpsertMetadataBatch() will change the resource by detecting that it is a
+	// CIDR.
+	assert.NotNil(t, s.IPIdentityCache.metadata.m[cidr].byResource[cidrResourceID])
+	assert.NotNil(t, s.IPIdentityCache.metadata.m[cidr].byResource[fqdnNameManagerResource])
+	assert.NoError(t, s.IPIdentityCache.WaitForRevision(
+		context.TODO(), s.IPIdentityCache.RemoveMetadataBatch(MU{
+			Prefix:   cidr,
+			Source:   source.Generated,
+			Resource: fqdnNameManagerResource,
+			Metadata: []IPMetadata{labels.NewLabelsFromSortedList("fqdn:foo.com")},
+			IsCIDR:   false,
+		}),
+	))
+	assert.Len(t, s.IPIdentityCache.metadata.m[cidr].byResource, 1)
+	assert.NotNil(t, s.IPIdentityCache.metadata.m[cidr].byResource[cidrResourceID])
+	assert.NoError(t, s.IPIdentityCache.WaitForRevision(
+		context.TODO(), s.IPIdentityCache.RemoveMetadataBatch(MU{
+			Prefix:   cidr,
+			Source:   source.Generated,
+			Resource: cidrOverlapResource,
+			Metadata: []IPMetadata{labels.GetCIDRLabels(cidr.AsPrefix())},
+			IsCIDR:   true,
+		}),
+	))
+	assert.Nil(t, s.IPIdentityCache.metadata.get(cidr))
+}
+
 func BenchmarkManyResources(b *testing.B) {
 	logger := hivetest.Logger(b)
 	m := newMetadata(logger)