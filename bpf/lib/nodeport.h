cilium/cilium

commit c171f22725ee75f27583e1912695e0244819f315

bpf: lb: split up lb*_local()
This function is shared amongst bpf_lxc and nodeport.h, and implements the
whole sequence of backend selection and DNAT for a service request.

But there's a bunch of code in it that actually only applies to *one* of
the callers. Let's make it easier to refactor by splitting this massive
function into two pieces - one that selects the backend, the other that
takes the backend as input and applies the DNAT. With room in-between to
run the caller-specific pieces (LRP + loopback for bpf_lxc, all sorts of
skip/punt cases for nodeport.h).

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -1340,6 +1340,7 @@ static __always_inline int nodeport_svc_lb6(struct __ctx_buff *ctx,
 					    __s8 *ext_err)
 {
 	struct ct_state ct_state_svc = {};
+	const struct lb6_backend *backend;
 	bool backend_local;
 	__u32 monitor = 0;
 	int ret;
@@ -1384,9 +1385,8 @@ static __always_inline int nodeport_svc_lb6(struct __ctx_buff *ctx,
 		return CTX_ACT_OK;
 	}
 #endif
-	ret = lb6_local(get_ct_map6(tuple), ctx, l3_off, fraginfo, l4_off,
-			key, tuple, svc, &ct_state_svc,
-			nodeport_xlate6(svc, tuple), ext_err, 0);
+	ret = lb6_local(get_ct_map6(tuple), ctx, fraginfo, l4_off,
+			key, tuple, svc, &ct_state_svc, &backend, ext_err);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_NO_SERVICE) {
 			if (!CONFIG(enable_no_service_endpoints_routable))
@@ -1398,12 +1398,24 @@ static __always_inline int nodeport_svc_lb6(struct __ctx_buff *ctx,
 			return ret;
 #endif
 		}
-		if (ret == LB_PUNT_TO_STACK) {
-			*punt_to_stack = true;
-			return CTX_ACT_OK;
-		}
+
 		return ret;
 	}
+
+	if (lb6_svc_is_l7_punt_proxy(svc) &&
+	    __lookup_ip6_endpoint(&backend->address)) {
+		ctx_skip_nodeport_set(ctx);
+		*punt_to_stack = true;
+		return CTX_ACT_OK;
+	}
+
+	if (nodeport_xlate6(svc, tuple))
+		return CTX_ACT_OK;
+
+	ret = lb6_dnat_request(ctx, backend, l3_off, fraginfo, l4_off,
+			       key, tuple, &ct_state_svc);
+	if (IS_ERR(ret))
+		return ret;
 
 	backend_local = __lookup_ip6_endpoint(&tuple->daddr);
 	if (!backend_local && lb6_svc_is_hostport(svc))
@@ -2744,9 +2756,11 @@ static __always_inline int nodeport_svc_lb4(struct __ctx_buff *ctx,
 		if (!ret)
 			return NAT_46X64_RECIRC;
 	} else {
-		ret = lb4_local(get_ct_map4(tuple), ctx, l3_off, fraginfo, l4_off,
-				key, tuple, svc, &ct_state_svc,
-				nodeport_xlate4(svc, tuple), &cluster_id, ext_err, 0);
+		const struct lb4_backend *backend;
+
+		ret = lb4_local(get_ct_map4(tuple), ctx, fraginfo, l4_off,
+				key, tuple, svc, &ct_state_svc, &backend,
+				&cluster_id, ext_err);
 		if (IS_ERR(ret)) {
 			if (ret == DROP_NO_SERVICE) {
 				if (!CONFIG(enable_no_service_endpoints_routable))
@@ -2761,13 +2775,21 @@ static __always_inline int nodeport_svc_lb4(struct __ctx_buff *ctx,
 #endif
 			}
 
-			if (ret == LB_PUNT_TO_STACK) {
-				*punt_to_stack = true;
-				return CTX_ACT_OK;
-			}
-
 			return ret;
 		}
+
+		if (lb4_svc_is_l7_punt_proxy(svc) &&
+		    __lookup_ip4_endpoint(backend->address)) {
+			ctx_skip_nodeport_set(ctx);
+			*punt_to_stack = true;
+			return CTX_ACT_OK;
+		}
+
+		if (nodeport_xlate4(svc, tuple))
+			return CTX_ACT_OK;
+
+		ret = lb4_dnat_request(ctx, backend, l3_off, fraginfo, l4_off,
+				       key, tuple, &ct_state_svc);
 	}
 
 	if (IS_ERR(ret))

bpf/bpf_lxc.c
@@ -34,6 +34,7 @@
 #include "lib/l3.h"
 #include "lib/local_delivery.h"
 #include "lib/lxc.h"
+#include "lib/lrp.h"
 #include "lib/identity.h"
 #include "lib/policy.h"
 #include "lib/mcast.h"
@@ -134,6 +135,8 @@ static __always_inline int __per_packet_lb_svc_xlate_4(void *ctx, struct iphdr *
 
 	svc = lb4_lookup_service(&key, is_defined(ENABLE_NODEPORT));
 	if (svc) {
+		const struct lb4_backend *backend;
+
 #if defined(ENABLE_L7_LB)
 		if (lb4_svc_is_l7_loadbalancer(svc)) {
 			proxy_port = (__u16)svc->l7_lb_proxy_port;
@@ -156,9 +159,9 @@ static __always_inline int __per_packet_lb_svc_xlate_4(void *ctx, struct iphdr *
 		if (unlikely(lb4_svc_is_localredirect(svc)))
 			goto skip_service_lookup;
 #endif /* ENABLE_LOCAL_REDIRECT_POLICY && ENABLE_SOCKET_LB_FULL */
-		ret = lb4_local(get_ct_map4(&tuple), ctx, ETH_HLEN, fraginfo,
+		ret = lb4_local(get_ct_map4(&tuple), ctx, fraginfo,
 				l4_off, &key, &tuple, svc, &ct_state_new,
-				false, &cluster_id, ext_err, CONFIG(endpoint_netns_cookie));
+				&backend, &cluster_id, ext_err);
 
 		if (IS_ERR(ret)) {
 			if (ret == DROP_NO_SERVICE) {
@@ -171,6 +174,32 @@ static __always_inline int __per_packet_lb_svc_xlate_4(void *ctx, struct iphdr *
 			}
 			return ret;
 		}
+
+		if (tuple.saddr == backend->address) {
+#if defined(ENABLE_LOCAL_REDIRECT_POLICY)
+			__net_cookie netns_cookie = CONFIG(endpoint_netns_cookie);
+
+			if (netns_cookie > 0 && unlikely(lb4_svc_is_localredirect(svc)) &&
+			    lrp_v4_skip_xlate_from_ctx_to_svc(netns_cookie, tuple.daddr,
+							      tuple.sport))
+				goto skip_service_lookup;
+#endif /* ENABLE_LOCAL_REDIRECT_POLICY */
+
+			/* Special loopback case: The origin endpoint has transmitted to a
+			 * service which is being translated back to the source. This would
+			 * result in a packet with identical source and destination address.
+			 * Linux considers such packets as martian source and will drop unless
+			 * received on a loopback device. Perform NAT on the source address
+			 * to make it appear from an outside address.
+			 */
+			ct_state_new.loopback = 1;
+		}
+
+		ret = lb4_dnat_request(ctx, backend, ETH_HLEN, fraginfo,
+				       l4_off, &key, &tuple, &ct_state_new);
+		if (IS_ERR(ret))
+			return ret;
+
 	}
 skip_service_lookup:
 	/* Store state to be picked up on the continuation tail call. */
@@ -218,6 +247,8 @@ static __always_inline int __per_packet_lb_svc_xlate_6(void *ctx, struct ipv6hdr
 	 */
 	svc = lb6_lookup_service(&key, is_defined(ENABLE_NODEPORT));
 	if (svc) {
+		const struct lb6_backend *backend;
+
 #if defined(ENABLE_L7_LB)
 		if (lb6_svc_is_l7_loadbalancer(svc)) {
 			proxy_port = (__u16)svc->l7_lb_proxy_port;
@@ -232,9 +263,9 @@ static __always_inline int __per_packet_lb_svc_xlate_6(void *ctx, struct ipv6hdr
 		if (unlikely(lb6_svc_is_localredirect(svc)))
 			goto skip_service_lookup;
 #endif /* ENABLE_LOCAL_REDIRECT_POLICY && ENABLE_SOCKET_LB_FULL */
-		ret = lb6_local(get_ct_map6(&tuple), ctx, ETH_HLEN, fraginfo,
+		ret = lb6_local(get_ct_map6(&tuple), ctx, fraginfo,
 				l4_off, &key, &tuple, svc, &ct_state_new,
-				false, ext_err, CONFIG(endpoint_netns_cookie));
+				&backend, ext_err);
 
 		if (IS_ERR(ret)) {
 			if (ret == DROP_NO_SERVICE) {
@@ -247,6 +278,23 @@ static __always_inline int __per_packet_lb_svc_xlate_6(void *ctx, struct ipv6hdr
 			}
 			return ret;
 		}
+
+		if (ipv6_addr_equals(&tuple.saddr, &backend->address)) {
+#if defined(ENABLE_LOCAL_REDIRECT_POLICY)
+			__net_cookie netns_cookie = CONFIG(endpoint_netns_cookie);
+
+			if (netns_cookie > 0 && unlikely(lb6_svc_is_localredirect(svc)) &&
+			    lrp_v6_skip_xlate_from_ctx_to_svc(netns_cookie, tuple.daddr,
+							      tuple.sport))
+				goto skip_service_lookup;
+#endif
+			ct_state_new.loopback = 1;
+		}
+
+		ret = lb6_dnat_request(ctx, backend, ETH_HLEN, fraginfo,
+				       l4_off, &key, &tuple, &ct_state_new);
+		if (IS_ERR(ret))
+			return ret;
 	}
 
 skip_service_lookup:

bpf/lib/common.h
@@ -234,7 +234,6 @@ struct srv6_policy_key6 {
 #define POLICY_ACT_PROXY_REDIRECT (1 << 16)
 
 #define NAT_PUNT_TO_STACK	DROP_NAT_NOT_NEEDED
-#define LB_PUNT_TO_STACK	DROP_PUNT_PROXY
 
 #define NAT_NEEDED		CTX_ACT_OK
 #define NAT_46X64_RECIRC	100

bpf/lib/lb.h
@@ -8,7 +8,6 @@
 #include "conntrack.h"
 #include "ipv4.h"
 #include "hash.h"
-#include "lrp.h"
 #include "eps.h"
 #include "nat_46x64.h"
 #include "ratelimit.h"
@@ -1268,21 +1267,19 @@ lb6_to_lb4(struct __ctx_buff *ctx __maybe_unused,
 }
 
 static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
-				     int l3_off, fraginfo_t fraginfo, int l4_off,
+				     fraginfo_t fraginfo, int l4_off,
 				     struct lb6_key *key,
 				     struct ipv6_ct_tuple *tuple,
 				     const struct lb6_service *svc,
 				     struct ct_state *state,
-				     const bool skip_xlate,
-				     __s8 *ext_err,
-				     __net_cookie netns_cookie __maybe_unused)
+				     const struct lb6_backend **selected_backend,
+				     __s8 *ext_err)
 {
 	__u32 monitor; /* Deliberately ignored; regular CT will determine monitoring. */
-	union v6addr saddr = tuple->saddr;
+	union v6addr saddr __maybe_unused = tuple->saddr;
 	__u8 flags = tuple->flags;
 	const struct lb6_backend *backend;
 	__u32 backend_id = 0;
-	union v6addr new_saddr = {};
 	int ret;
 	union lb6_affinity_client_id client_id;
 
@@ -1378,30 +1375,33 @@ static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
 	if (lb6_svc_is_affinity(svc))
 		lb6_update_affinity_by_addr(svc, &client_id, backend_id);
 
-#if defined(USE_LOOPBACK_LB) || defined(ENABLE_LOCAL_REDIRECT_POLICY)
-	if (ipv6_addr_equals(&saddr, &backend->address)) {
-	#if defined(ENABLE_LOCAL_REDIRECT_POLICY)
-		if (netns_cookie > 0 && unlikely(lb6_svc_is_localredirect(svc)) &&
-		    lrp_v6_skip_xlate_from_ctx_to_svc(netns_cookie, tuple->daddr, tuple->sport))
-			return CTX_ACT_OK;
-	#endif
+	*selected_backend = backend;
 
-	#ifdef USE_LOOPBACK_LB
+	return CTX_ACT_OK;
+
+no_service:
+	ret = DROP_NO_SERVICE;
+drop_err:
+	tuple->flags = flags;
+	return ret;
+}
+
+static __always_inline int
+lb6_dnat_request(struct __ctx_buff *ctx, const struct lb6_backend *backend,
+		 int l3_off, fraginfo_t fraginfo, int l4_off,
+		 struct lb6_key *key, struct ipv6_ct_tuple *tuple,
+		 const struct ct_state *state __maybe_unused)
+{
+	union v6addr saddr = tuple->saddr;
+	union v6addr new_saddr = {};
+
+#ifdef USE_LOOPBACK_LB
+	if (state->loopback) {
 		union v6addr loopback_addr = CONFIG(service_loopback_ipv6);
 
 		ipv6_addr_copy(&new_saddr, &loopback_addr);
-		state->loopback = 1;
-	#endif
 	}
-#endif /* ENABLE_LOCAL_REDIRECT_POLICY */
-
-	if (lb6_svc_is_l7_punt_proxy(svc) &&
-	    __lookup_ip6_endpoint(&backend->address)) {
-		ctx_skip_nodeport_set(ctx);
-		return LB_PUNT_TO_STACK;
-	}
-	if (skip_xlate)
-		return CTX_ACT_OK;
+#endif
 
 #ifdef USE_LOOPBACK_LB
 	if (!state->loopback)
@@ -1413,11 +1413,6 @@ static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
 
 	return lb6_xlate(ctx, &new_saddr, &saddr, tuple->nexthdr, l3_off, l4_off, key,
 			 backend, ipfrag_has_l4_header(fraginfo));
-no_service:
-	ret = DROP_NO_SERVICE;
-drop_err:
-	tuple->flags = flags;
-	return ret;
 }
 
 /* lb6_ctx_store_state() stores per packet load balancing state to be picked
@@ -2031,22 +2026,20 @@ lb4_to_lb6(struct __ctx_buff *ctx __maybe_unused,
 }
 
 static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
-				     int l3_off, fraginfo_t fraginfo, int l4_off,
+				     fraginfo_t fraginfo, int l4_off,
 				     struct lb4_key *key,
 				     struct ipv4_ct_tuple *tuple,
 				     const struct lb4_service *svc,
 				     struct ct_state *state,
-				     const bool skip_xlate,
+				     const struct lb4_backend **selected_backend,
 				     __u32 *cluster_id __maybe_unused,
-				     __s8 *ext_err,
-				     __net_cookie netns_cookie __maybe_unused)
+				     __s8 *ext_err)
 {
 	__u32 monitor; /* Deliberately ignored; regular CT will determine monitoring. */
-	__be32 saddr = tuple->saddr;
+	__be32 saddr __maybe_unused = tuple->saddr;
 	__u8 flags = tuple->flags;
 	const struct lb4_backend *backend;
 	__u32 backend_id = 0;
-	__be32 new_saddr = 0;
 	int ret;
 	union lb4_affinity_client_id client_id = {
 		.client_ip = saddr,
@@ -2146,35 +2139,30 @@ static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
 	if (lb4_svc_is_affinity(svc))
 		lb4_update_affinity_by_addr(svc, &client_id, backend_id);
 
-#if defined(USE_LOOPBACK_LB) || defined(ENABLE_LOCAL_REDIRECT_POLICY)
-	if (saddr == backend->address) {
-	#if defined(ENABLE_LOCAL_REDIRECT_POLICY)
-		if (netns_cookie > 0 && unlikely(lb4_svc_is_localredirect(svc)) &&
-		    lrp_v4_skip_xlate_from_ctx_to_svc(netns_cookie, tuple->daddr, tuple->sport))
-			return CTX_ACT_OK;
-	#endif /* ENABLE_LOCAL_REDIRECT_POLICY */
+	*selected_backend = backend;
 
-		/* Special loopback case: The origin endpoint has transmitted to a
-		 * service which is being translated back to the source. This would
-		 * result in a packet with identical source and destination address.
-		 * Linux considers such packets as martian source and will drop unless
-		 * received on a loopback device. Perform NAT on the source address
-		 * to make it appear from an outside address.
-		 */
-	#ifdef USE_LOOPBACK_LB
+	return CTX_ACT_OK;
+
+no_service:
+	ret = DROP_NO_SERVICE;
+drop_err:
+	tuple->flags = flags;
+	return ret;
+}
+
+static __always_inline int
+lb4_dnat_request(struct __ctx_buff *ctx, const struct lb4_backend *backend,
+		 int l3_off, fraginfo_t fraginfo, int l4_off,
+		 struct lb4_key *key,  struct ipv4_ct_tuple *tuple,
+		 const struct ct_state *state __maybe_unused)
+{
+	__be32 saddr = tuple->saddr;
+	__be32 new_saddr = 0;
+
+#ifdef USE_LOOPBACK_LB
+	if (state->loopback)
 		new_saddr = CONFIG(service_loopback_ipv4).be32;
-		state->loopback = 1;
-	#endif
-	}
 #endif
-
-	if (lb4_svc_is_l7_punt_proxy(svc) &&
-	    __lookup_ip4_endpoint(backend->address)) {
-		ctx_skip_nodeport_set(ctx);
-		return LB_PUNT_TO_STACK;
-	}
-	if (skip_xlate)
-		return CTX_ACT_OK;
 
 #ifdef USE_LOOPBACK_LB
 	if (!state->loopback)
@@ -2188,11 +2176,6 @@ static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
 	return lb4_xlate(ctx, &new_saddr, &saddr,
 			 tuple->nexthdr, l3_off, l4_off, key,
 			 backend, ipfrag_has_l4_header(fraginfo));
-no_service:
-	ret = DROP_NO_SERVICE;
-drop_err:
-	tuple->flags = flags;
-	return ret;
 }
 
 /* lb4_ctx_store_state() stores per packet load balancing state to be picked

commit 5bcc16701edae6ad06bfefe198fac3119072eb14

bpf: nodeport: fix up IPIP forward path
While the IPIP-style service processing typically skips the DNAT, we still
want to continue in the forwarding path (and not just punt the packet up
to the stack). c171f22725ee broke this when lifting the nodeport_xlate*()
check into the caller.

While at it also fix up the naming of the helper.

Fixes: c171f22725ee ("bpf: lb: split up lb*_local()")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -236,7 +236,7 @@ static __always_inline bool nodeport_uses_dsr6(const struct lb6_service *svc)
 	return nodeport_uses_dsr(svc->flags2 & SVC_FLAG_FWD_MODE_DSR);
 }
 
-static __always_inline bool nodeport_xlate6(const struct lb6_service *svc)
+static __always_inline bool nodeport_skip_xlate6(const struct lb6_service *svc)
 {
 	bool skip_xlate = DSR_ENCAP_MODE == DSR_ENCAP_IPIP;
 
@@ -1399,13 +1399,12 @@ static __always_inline int nodeport_svc_lb6(struct __ctx_buff *ctx,
 		return CTX_ACT_OK;
 	}
 
-	if (nodeport_xlate6(svc))
-		return CTX_ACT_OK;
-
-	ret = lb6_dnat_request(ctx, backend, l3_off, fraginfo, l4_off,
-			       key, tuple, false);
-	if (IS_ERR(ret))
-		return ret;
+	if (!nodeport_skip_xlate6(svc)) {
+		ret = lb6_dnat_request(ctx, backend, l3_off, fraginfo,
+				       l4_off, key, tuple, false);
+		if (IS_ERR(ret))
+			return ret;
+	}
 
 	backend_local = __lookup_ip6_endpoint(&backend->address);
 	if (!backend_local && lb6_svc_is_hostport(svc))
@@ -1576,7 +1575,7 @@ static __always_inline bool nodeport_uses_dsr4(const struct lb4_service *svc)
 	return nodeport_uses_dsr(svc->flags2 & SVC_FLAG_FWD_MODE_DSR);
 }
 
-static __always_inline bool nodeport_xlate4(const struct lb4_service *svc)
+static __always_inline bool nodeport_skip_xlate4(const struct lb4_service *svc)
 {
 	bool skip_xlate = DSR_ENCAP_MODE == DSR_ENCAP_IPIP;
 
@@ -2771,15 +2770,13 @@ static __always_inline int nodeport_svc_lb4(struct __ctx_buff *ctx,
 			return CTX_ACT_OK;
 		}
 
-		if (nodeport_xlate4(svc))
-			return CTX_ACT_OK;
-
 #ifdef ENABLE_CLUSTER_AWARE_ADDRESSING
 		cluster_id = backend->cluster_id;
 #endif
 
-		ret = lb4_dnat_request(ctx, backend, l3_off, fraginfo, l4_off,
-				       key, tuple, false);
+		if (!nodeport_skip_xlate4(svc))
+			ret = lb4_dnat_request(ctx, backend, l3_off, fraginfo,
+					       l4_off, key, tuple, false);
 	}
 
 	if (IS_ERR(ret))