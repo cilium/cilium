c8050221ecf09f79135624929f755937914890ae
Marc Suñé <marc.sune@isovalent.com>
2025-12-10 12:08:34 +0100

    bpf/tests/scapy: warn ctx LEN < expected len scapy
    We are unable to easily write in the Scapy assert MAP when the ctx
    len < than the expected length, as in order to unroll loops we are
    expecting a const value.
    
    Therefore, store the expected len and the actual ctx len, so that
    bpftest can issue a warning and a useful tip to further debug the
    issue in absence of the packet diff.
    
    Added test coverage.
    
    Signed-off-by: Marc Suñé <marc.sune@isovalent.com>

--- bpf/tests/_scapy_selftest.c
+++ bpf/tests/_scapy_selftest.c
@@ -18,9 +18,11 @@
 
 #define ASSERT1_FAIL "assert1_fail"
 #define ASSERT2_FAIL "assert2_fail"
+#define ASSERT3_FAIL "assert3_fail"
 
 #define LEN_SST_EXP sizeof(BUF(SST_EXP))
 #define LEN_SST_NOT_EXP sizeof(BUF(SST_NOT_EXP))
+#define LEN_SST_NOT_EXP_PAD sizeof(BUF(SST_NOT_EXP_PAD))
 
 /**
  * These are here so that test_fail_now() that returns is captured
@@ -56,7 +58,20 @@ int force_assert_fail_off2(struct __ctx_buff *ctx)
 	return TEST_PASS;
 }
 
-static struct scapy_assert null_entry = {0};
+static __always_inline
+int force_assert_fail_ctx_smaller_exp(struct __ctx_buff *ctx)
+{
+	test_init();
+
+	BUF_DECL(SST_NOT_EXP_PAD, sst_rep_pad);
+
+	ASSERT_CTX_BUF_OFF2(ASSERT3_FAIL, "Ether", ctx, 0, SST_NOT_EXP_PAD,
+			    BUF(SST_NOT_EXP_PAD), LEN_SST_NOT_EXP_PAD,
+			    LEN_SST_NOT_EXP_PAD);
+	fake_test_end();
+
+	return TEST_PASS;
+}
 
 PKTGEN("tc", "1_basic_test")
 int pktgen_scapy_basic_test(struct __ctx_buff *ctx)
@@ -83,6 +98,7 @@ int check_scapy_basic_test(struct __ctx_buff *ctx)
 
 	BUF_DECL(SST_EXP, sst_req);
 	BUF_DECL(SST_NOT_EXP, sst_rep);
+	BUF_DECL(SST_NOT_EXP_PAD, sst_rep_pad);
 
 	ASSERT_CTX_BUF_OFF(ASSERT1, "Ether", ctx, 0, SST_EXP,
 			   LEN_SST_EXP);
@@ -94,8 +110,10 @@ int check_scapy_basic_test(struct __ctx_buff *ctx)
 	assert(rc == TEST_FAIL);
 	rc = force_assert_fail_off2(ctx);
 	assert(rc == TEST_FAIL);
+	rc = force_assert_fail_ctx_smaller_exp(ctx);
+	assert(rc == TEST_FAIL);
 
-	assert(scapy_assert_map_cnt == 2);
+	assert(scapy_assert_map_cnt == 3);
 
 	{
 		/* ASSERT_CTX_BUF_OFF */
@@ -108,9 +126,12 @@ int check_scapy_basic_test(struct __ctx_buff *ctx)
 				    LEN_SST_EXP) == 0);
 		assert(memcmp(entry->name, ASSERT1_FAIL,
 			      sizeof(ASSERT1_FAIL)) == 0);
-		assert(entry->len == LEN_SST_NOT_EXP);
-		rc = map_update_elem(&scapy_assert_map, &id, &null_entry,
-				     BPF_ANY);
+		assert(entry->exp_len == LEN_SST_NOT_EXP);
+		assert(entry->got_len == LEN_SST_EXP);
+		assert(entry->exp_len == entry->got_len);
+
+		rc = map_update_elem(&scapy_assert_map, &id,
+				     &__scapy_null_assert, BPF_ANY);
 		assert(rc == 0);
 	}
 	{
@@ -124,8 +145,28 @@ int check_scapy_basic_test(struct __ctx_buff *ctx)
 				    LEN_SST_EXP) == 0);
 		assert(memcmp(entry->name, ASSERT2_FAIL,
 			      sizeof(ASSERT2_FAIL)) == 0);
-		rc = map_update_elem(&scapy_assert_map, &id, &null_entry,
-				     BPF_ANY);
+		assert(entry->exp_len == LEN_SST_NOT_EXP);
+		assert(entry->got_len == LEN_SST_EXP);
+		assert(entry->exp_len == entry->got_len);
+		rc = map_update_elem(&scapy_assert_map, &id,
+				     &__scapy_null_assert, BPF_ANY);
+		assert(rc == 0);
+	}
+	{
+		/* len(CTX) < len(EXP) */
+		id = 2;
+		entry = map_lookup_elem(&scapy_assert_map, &id);
+		assert(entry);
+		assert(scapy_memcmp(entry->exp_buf, BUF(SST_NOT_EXP),
+				    LEN_SST_NOT_EXP) == 0);
+		assert(scapy_memcmp(entry->got_buf, __scapy_null_assert.got_buf,
+				    LEN_SST_EXP) == 0);
+		assert(memcmp(entry->name, ASSERT3_FAIL,
+			      sizeof(ASSERT3_FAIL)) == 0);
+		assert(entry->exp_len == LEN_SST_NOT_EXP_PAD);
+		assert(entry->got_len == LEN_SST_EXP);
+		rc = map_update_elem(&scapy_assert_map, &id,
+				     &__scapy_null_assert, BPF_ANY);
 		assert(rc == 0);
 	}
 
--- bpf/tests/scapy/selftest_pkt_defs.py
+++ bpf/tests/scapy/selftest_pkt_defs.py
@@ -17,3 +17,13 @@ sst_rep = (
     ARP(op="is-at", psrc=v4_svc_one, pdst=v4_ext_one, \
         hwsrc=mac_two, hwdst=mac_one)
 )
+
+# Padded reply to test
+sst_rep_pad = (
+    Ether(dst=mac_one, src=mac_two) /
+    ARP(op="is-at", psrc=v4_svc_one, pdst=v4_ext_one, \
+        hwsrc=mac_two, hwdst=mac_one) /
+    Raw("A"*8)
+)
+
+assert len(bytes(sst_rep_pad)) == (len(bytes(sst_rep)) + 8)
