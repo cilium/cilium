cilium/cilium

commit 9ef6388d9ae120a89e0f303a03a9dd6f089dcbfb

bpf: Add helper functions to transfer ClusterID with mark
Introduce set_cluster_id_mark and ctx_get_cluster_id_from_mark which
set/get ClusterID to/from skb->mark. For XDP, they are nop. Also reserve
a new mark magic number for ClusterID.

Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

bpf/lib/overloadable_skb.h
@@ -4,6 +4,7 @@
 #ifndef __LIB_OVERLOADABLE_SKB_H_
 #define __LIB_OVERLOADABLE_SKB_H_
 
+#include "lib/common.h"
 #include "linux/ip.h"
 
 static __always_inline __maybe_unused void
@@ -65,6 +66,29 @@ static __always_inline __maybe_unused void
 set_encrypt_key_meta(struct __sk_buff *ctx, __u8 key, __u32 node_id)
 {
 	ctx->cb[CB_ENCRYPT_MAGIC] = or_encrypt_key(key) | node_id << 16;
+}
+
+/**
+ * set_cluster_id_mark - sets the cluster_id mark.
+ */
+static __always_inline __maybe_unused void
+ctx_set_cluster_id_mark(struct __sk_buff *ctx, __u32 cluster_id)
+{
+	ctx->mark |= cluster_id | MARK_MAGIC_CLUSTER_ID;
+}
+
+static __always_inline __maybe_unused __u32
+ctx_get_cluster_id_mark(struct __sk_buff *ctx)
+{
+	__u32 ret = 0;
+
+	if ((ctx->mark & MARK_MAGIC_CLUSTER_ID) != MARK_MAGIC_CLUSTER_ID)
+		return ret;
+
+	ret = ctx->mark & MARK_MAGIC_CLUSTER_ID_MASK;
+	ctx->mark &= ~(__u32)(MARK_MAGIC_CLUSTER_ID | MARK_MAGIC_CLUSTER_ID_MASK);
+
+	return ret;
 }
 
 static __always_inline __maybe_unused int

bpf/lib/common.h
@@ -640,6 +640,10 @@ enum metric_dir {
  */
 #define MARK_MAGIC_HEALTH		MARK_MAGIC_DECRYPT
 
+/* Shouldn't interfere with MARK_MAGIC_TO_PROXY. Lower 8bits carries cluster_id */
+#define MARK_MAGIC_CLUSTER_ID		MARK_MAGIC_TO_PROXY
+#define MARK_MAGIC_CLUSTER_ID_MASK	0x00FF
+
 /* IPv4 option used to carry service addr and port for DSR.
  *
  * Copy = 1 (option is copied to each fragment)

bpf/lib/overloadable_xdp.h
@@ -44,6 +44,17 @@ set_encrypt_key_meta(struct xdp_md *ctx __maybe_unused, __u8 key __maybe_unused,
 {
 }
 
+static __always_inline __maybe_unused void
+ctx_set_cluster_id_mark(struct xdp_md *ctx __maybe_unused, __u32 cluster_id __maybe_unused)
+{
+}
+
+static __always_inline __maybe_unused __u32
+ctx_get_cluster_id_mark(struct __sk_buff *ctx __maybe_unused)
+{
+	return 0;
+}
+
 static __always_inline __maybe_unused int
 redirect_self(struct xdp_md *ctx __maybe_unused)
 {

commit 53391531760abb9b26a6edaf0078eccf16aa3d8a

bpf: fix magic mark check before retrieving cluster ID from skb->mark
The `ctx_get_cluster_id_mark` helper function allows to retrieve the
cluster ID stored inside of `skb->mark`, and it does so after having
checked the magic part, to prevent reading it from unrelated data.
However, the check incorrectly uses `MARK_MAGIC_CLUSTER_ID` as mask,
hence potentially misinterpreting other partially overlapping magic
values (e.g., `MARK_MAGIC_IDENTITY`) as matching as well.

Let's get this fixed by checking that the magic part exactly matches
`MARK_MAGIC_CLUSTER_ID`, and update the corresponding setter function
to override the entire mark content, similarly to what `set_identity_mark`
also does.

Fixes: 9ef6388d9ae1 ("bpf: Add helper functions to transfer ClusterID with mark")
Fixes: 5313b0d9f086 ("bpf: clear mark content before storing the cluster ID")
Suggested-by: Julian Wiedmann <jwi@isovalent.com>
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

bpf/lib/overloadable_skb.h
@@ -122,8 +122,7 @@ ctx_set_cluster_id_mark(struct __sk_buff *ctx, __u32 cluster_id)
 	__u32 cluster_id_lower = (cluster_id & 0xFF);
 	__u32 cluster_id_upper = ((cluster_id & 0xFFFFFF00) << (8 + IDENTITY_LEN));
 
-	ctx->mark &= MARK_MAGIC_KEY_MASK;
-	ctx->mark |=  cluster_id_lower | cluster_id_upper | MARK_MAGIC_CLUSTER_ID;
+	ctx->mark = cluster_id_lower | cluster_id_upper | MARK_MAGIC_CLUSTER_ID;
 }
 
 static __always_inline __maybe_unused __u32
@@ -133,7 +132,7 @@ ctx_get_cluster_id_mark(struct __sk_buff *ctx)
 	__u32 cluster_id_lower = ctx->mark & CLUSTER_ID_LOWER_MASK;
 	__u32 cluster_id_upper = (ctx->mark & get_cluster_id_upper_mask()) >> (8 + IDENTITY_LEN);
 
-	if ((ctx->mark & MARK_MAGIC_CLUSTER_ID) != MARK_MAGIC_CLUSTER_ID)
+	if ((ctx->mark & MARK_MAGIC_HOST_MASK) != MARK_MAGIC_CLUSTER_ID)
 		return ret;
 
 	ret = (cluster_id_upper | cluster_id_lower) & CLUSTER_ID_MAX;