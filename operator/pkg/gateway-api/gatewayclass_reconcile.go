8af3b35a85e612662fb307d165b65c8263dd1037
Nick Young <nick@isovalent.com>
2025-12-11 16:35:52 +1100

    Revert "gatewayAPI: fix reference to cluster-scoped CGCC resource"
    This reverts commit 433db70380f72dd2bb19e132bcee42abea93ce57.
    
    Signed-off-by: Nick Young <nick@isovalent.com>

--- operator/pkg/gateway-api/gatewayclass_reconcile.go
+++ operator/pkg/gateway-api/gatewayclass_reconcile.go
@@ -61,9 +61,20 @@ func (r *gatewayClassReconciler) Reconcile(ctx context.Context, req ctrl.Request
 			return controllerruntime.Fail(nil)
 		}
 
+		if ref.Namespace == nil || ref.Name == "" {
+			scopedLog.ErrorContext(ctx, "ParametersRef must specify namespace and name")
+			setGatewayClassAccepted(gwc, false)
+			if err := r.ensureStatus(ctx, gwc, original); err != nil {
+				scopedLog.ErrorContext(ctx, "Failed to update GatewayClass status", logfields.Error, err)
+				return controllerruntime.Fail(err)
+			}
+			return controllerruntime.Fail(nil)
+		}
+
 		cgcc := &v2alpha1.CiliumGatewayClassConfig{}
 		key := client.ObjectKey{
-			Name: ref.Name,
+			Namespace: string(*ref.Namespace),
+			Name:      ref.Name,
 		}
 		if err := r.Client.Get(ctx, key, cgcc); err != nil {
 			setGatewayClassAccepted(gwc, false)
