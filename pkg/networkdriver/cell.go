39afd2b88f4bb3b8e74672fc91eb221844436930
Bernardo Soares <20172413+bersoare@users.noreply.github.com>
2025-12-11 15:45:18 +0000

    network driver: fetch configuration from CRD
    get and use configuration from the CRD
    
    Signed-off-by: Bernardo Soares <20172413+bersoare@users.noreply.github.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/networkdriver/cell.go
+++ pkg/networkdriver/cell.go
@@ -9,11 +9,13 @@ import (
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
 	kube_types "k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
 
 	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/lock"
-	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/networkdriver/types"
 )
 
@@ -23,6 +25,7 @@ var Cell = cell.Module(
 	"network-driver",
 	"Cilium Network Driver",
 
+	cell.ProvidePrivate(ciliumNetworkDriverConfigResource),
 	cell.Invoke(registerNetworkDriver),
 )
 
@@ -33,62 +36,30 @@ type networkDriverParams struct {
 	Lifecycle cell.Lifecycle
 	ClientSet k8sClient.Clientset
 	JobGroup  job.Group
+	Configs   resource.Resource[*v2alpha1.CiliumNetworkDriverConfig]
 }
 
-// getNetworkDriverConfig returns the network driver configuration.
-// This will be deprecated in favor a configuration passed via a Custom Resource.
-//
-// An example configuration to manage dummy devices is:
-//
-//	{
-//		DraRegistrationRetry:   time.Second,
-//		DraRegistrationTimeout: 30 * time.Second,
-//		PublishInterval:        3 * time.Second,
-//		DriverName:             "dummy.cilium.k8s.io",
-//		DeviceManagerConfigs: map[types.DeviceManagerType]types.DeviceManagerConfig{
-//			types.DeviceManagerTypeDummy: dummy.DummyConfig{Enabled: true},
-//		},
-//		Pools: []PoolConfig{
-//			{
-//				Name: "dummy-devices",
-//				Filter: types.DeviceFilter{
-//					DriverTypes: []types.DeviceManagerType{types.DeviceManagerTypeDummy},
-//					IfNames:     []string{"dummy"},
-//				},
-//			},
-//		},
-//	}
-func getNetworkDriverConfig(_ k8sClient.Clientset) (*v2alpha1.CiliumNetworkDriverConfigSpec, error) {
-	return nil, nil
+func ciliumNetworkDriverConfigResource(cs k8sClient.Clientset, lc cell.Lifecycle, mp workqueue.MetricsProvider) resource.Resource[*v2alpha1.CiliumNetworkDriverConfig] {
+	if !cs.IsEnabled() {
+		return nil
+	}
+
+	return resource.New[*v2alpha1.CiliumNetworkDriverConfig](
+		lc,
+		utils.ListerWatcherFromTyped(cs.CiliumV2alpha1().CiliumNetworkDriverConfigs()),
+		mp,
+		resource.WithMetric("CiliumNetworkDriverConfig"),
+	)
 }
 
 func registerNetworkDriver(params networkDriverParams) *Driver {
-	cfg, err := getNetworkDriverConfig(params.ClientSet)
-	if err != nil {
-		params.Log.Error(
-			"failed to retrieve network driver configuration",
-			logfields.Error, err,
-		)
-
-		return nil
-	}
-
-	if cfg == nil {
-		params.Log.Debug(
-			"network driver configuration not found, skipping",
-		)
-
-		return nil
-	}
-
 	driver := &Driver{
-		driverName:     cfg.DriverName,
 		logger:         params.Log,
 		lock:           lock.Mutex{},
 		jg:             params.JobGroup,
 		kubeClient:     params.ClientSet,
 		deviceManagers: make(map[types.DeviceManagerType]types.DeviceManager),
-		config:         *cfg,
+		configCRD:      params.Configs,
 		allocations:    make(map[kube_types.UID]map[kube_types.UID][]allocation),
 	}
 
