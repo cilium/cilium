6588d74985daaac63c0fabde4aa1d711fca5a680
Bernardo Soares <20172413+bersoare@users.noreply.github.com>
2025-12-18 15:22:37 +0000

    network driver: collect CiliumNetworkDriverConfigs in sysdump
    collect the CRD for the network driver configuration
    with the sysdump command
    
    Signed-off-by: Bernardo Soares <20172413+bersoare@users.noreply.github.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- cilium-cli/sysdump/constants.go
+++ cilium-cli/sysdump/constants.go
@@ -124,6 +124,7 @@ const (
 	referenceGrantsFileName                  = "gatewayapi-referencegrants-<ts>.yaml"
 	ingressClassesFileName                   = "ingressclasses-<ts>.yaml"
 	k8sResourceFileName                      = "%s-<ts>.yaml"
+	ciliumNetworkDriverConfigFileName        = "cilium-network-driver-configs-<ts>.yaml"
 )
 
 const (
