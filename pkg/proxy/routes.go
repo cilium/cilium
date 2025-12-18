97d7b80884aa77677d6d4ff106872dce82b09c21
Simone Magnani <simone.magnani@isovalent.com>
2025-12-18 15:45:19 +0100

    proxy: refactor requireFromProxyRoutes to accept mtu parameter
    [ upstream commit 116ae1c7 ]
    
    [ backporter's notes: fixed conflicts due to IPSec modularization Cell not
    being backported ]
    
    The method now returns the correct mtu to be used for proxy routes.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

--- pkg/proxy/routes.go
+++ pkg/proxy/routes.go
@@ -55,12 +55,7 @@ var (
 // ReinstallRoutingRules ensures the presence of routing rules and tables needed
 // to route packets to and from the L7 proxy.
 func ReinstallRoutingRules(logger *slog.Logger, localNode node.LocalNode, mtu int) error {
-	fromIngressProxy, fromEgressProxy := requireFromProxyRoutes()
-
-	// Use the provided mtu (RouteMTU) only with both ingress and egress proxy.
-	if !fromIngressProxy || !fromEgressProxy {
-		mtu = 0
-	}
+	fromIngressProxy, fromEgressProxy, mtu := requireFromProxyRoutes(mtu)
 
 	if option.Config.EnableIPv4 {
 		if err := installToProxyRoutesIPv4(logger); err != nil {
@@ -115,9 +110,14 @@ func ReinstallRoutingRules(logger *slog.Logger, localNode node.LocalNode, mtu in
 	return nil
 }
 
-func requireFromProxyRoutes() (fromIngressProxy, fromEgressProxy bool) {
+func requireFromProxyRoutes(mtuIn int) (fromIngressProxy, fromEgressProxy bool, mtu int) {
 	fromIngressProxy = (option.Config.EnableEnvoyConfig || option.Config.EnableIPSec) && !option.Config.TunnelingEnabled()
 	fromEgressProxy = option.Config.EnableIPSec && !option.Config.TunnelingEnabled()
+
+	// Use the provided mtu (RouteMTU) only with both ingress and egress proxy.
+	if fromIngressProxy && fromEgressProxy {
+		mtu = mtuIn
+	}
 	return
 }
 
