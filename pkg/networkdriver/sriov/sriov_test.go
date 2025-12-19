5e257a3339bd9c84ad02243dd33843bd17b0d0e7
Bernardo Soares <20172413+bersoare@users.noreply.github.com>
2025-12-19 11:22:29 +0000

    network driver: add tests for sriov device attributes
    added some more test coverage, now ensuring that
    the attributes match the expectation
    
    Signed-off-by: Bernardo Soares <20172413+bersoare@users.noreply.github.com>

--- pkg/networkdriver/sriov/sriov_test.go
+++ pkg/networkdriver/sriov/sriov_test.go
@@ -2,13 +2,31 @@ package sriov
 
 import (
 	"log/slog"
+	"maps"
+	"slices"
 	"testing"
 
 	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/stretchr/testify/require"
 	"github.com/vishvananda/netlink"
+	resourceapi "k8s.io/api/resource/v1"
 )
 
+func compareAttrs(t *testing.T, one, two map[resourceapi.QualifiedName]resourceapi.DeviceAttribute) {
+	require.NotEmpty(t, one)
+	require.ElementsMatch(t, slices.Collect(maps.Keys(one)), slices.Collect(maps.Keys(two)))
+
+	for k, v := range one {
+		require.NotEmpty(t, v.String())
+		other := two[k]
+		require.Equal(t, v.String(), other.String())
+	}
+}
+
+// TODO:
+// - test sr-iov setup
+// - test listdevices
+// - filter matching logic
 func TestSriov(t *testing.T) {
 	t.Run("test device parsing", func(t *testing.T) {
 		listLinkFunc := func() ([]netlink.Link, error) {
@@ -44,9 +62,21 @@ func TestSriov(t *testing.T) {
 		device, err := mgr.parseDevice("0000:02:00.1", byPCI)
 		require.NoError(t, err)
 		require.NotNil(t, device)
-		require.Equal(t, "mypf", device.pfName)
-		require.Equal(t, "myvf", device.kernelIfName)
-		require.Equal(t, "mydriver", device.driver)
-		require.NotZero(t, device.vfID)
+
+		expectedDevice := PciDevice{
+			addr:         "0000:02:00.1",
+			pfName:       "mypf",
+			driver:       "mydriver",
+			vfID:         1,
+			kernelIfName: "myvf",
+			deviceID:     "mydeviceid",
+			vendor:       "myvendor",
+		}
+
+		require.Equal(t, expectedDevice.pfName, device.pfName)
+		require.Equal(t, expectedDevice.kernelIfName, device.kernelIfName)
+		require.Equal(t, expectedDevice.driver, device.driver)
+		require.Equal(t, expectedDevice.vfID, device.vfID)
+		compareAttrs(t, device.GetAttrs(), expectedDevice.GetAttrs())
 	})
 }
---
+++ pkg/networkdriver/types/attributes.go
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package types
+
+const (
+	KernelIfNameLabel  = "kernelIfName"
+	IfNameLabel        = "ifName"
+	PCIAddrLabel       = "pciAddr"
+	PFNameLabel        = "pfName"
+	VendorLabel        = "vendor"
+	DeviceIDLabel      = "deviceID"
+	DriverLabel        = "driver"
+	DeviceManagerLabel = "deviceManager"
+	PoolNameLabel      = "pool"
+)
