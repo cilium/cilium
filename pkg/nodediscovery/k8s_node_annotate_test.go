ca8babeec45367d6fe6430b663edef201c4ff916
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-12-22 11:50:50 +0100

    k8s/annotate: remove need for WithTestLocalNodeStore
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/nodediscovery/k8s_node_annotate_test.go
+++ pkg/nodediscovery/k8s_node_annotate_test.go
@@ -22,129 +22,116 @@ import (
 	"github.com/cilium/cilium/pkg/k8s"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
-	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/source"
 )
 
 func TestPatchingCIDRAnnotation(t *testing.T) {
 	logger := hivetest.Logger(t)
-	node.WithTestLocalNodeStore(func() {
-		prevAnnotateK8sNode := option.Config.AnnotateK8sNode
-		option.Config.AnnotateK8sNode = true
-		defer func() {
-			option.Config.AnnotateK8sNode = prevAnnotateK8sNode
-		}()
 
-		// Test IPv4
-		node1 := v1.Node{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "node1",
-				Annotations: map[string]string{
-					annotation.V4CIDRName:   "10.254.0.0/16",
-					annotation.CiliumHostIP: "10.254.0.1",
-				},
+	prevAnnotateK8sNode := option.Config.AnnotateK8sNode
+	option.Config.AnnotateK8sNode = true
+	defer func() {
+		option.Config.AnnotateK8sNode = prevAnnotateK8sNode
+	}()
+
+	// Test IPv4
+	node1 := v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node1",
+			Annotations: map[string]string{
+				annotation.V4CIDRName:   "10.254.0.0/16",
+				annotation.CiliumHostIP: "10.254.0.1",
 			},
-			Spec: v1.NodeSpec{
-				PodCIDR: "10.2.0.0/16",
+		},
+		Spec: v1.NodeSpec{
+			PodCIDR: "10.2.0.0/16",
+		},
+	}
+
+	// set buffer to 2 to prevent blocking when calling UseNodeCIDR
+	// and we need to wait for the response of the channel.
+	patchChan := make(chan bool, 2)
+	fakeK8sClient := &fake.Clientset{}
+	fakeK8sClient.AddReactor("patch", "nodes",
+		func(action k8stesting.Action) (bool, runtime.Object, error) {
+			n1copy := node1.DeepCopy()
+			n1copy.Annotations[annotation.V4CIDRName] = "10.2.0.0/16"
+			raw, err := json.Marshal(n1copy.Annotations)
+			if err != nil {
+				require.NoError(t, err)
+			}
+			patchWanted := fmt.Appendf(nil, `{"metadata":{"annotations":%s}}`, raw)
+
+			patchReceived := action.(k8stesting.PatchAction).GetPatch()
+			require.Equal(t, string(patchWanted), string(patchReceived))
+			patchChan <- true
+			return true, n1copy, nil
+		})
+
+	node1Cilium := k8s.ParseNode(logger, toSlimNode(node1.DeepCopy()), source.Unspec)
+	node1Cilium.SetCiliumInternalIP(net.ParseIP("10.254.0.1"))
+
+	n := &NodeDiscovery{logger: logger}
+	n.annotateK8sNode(t.Context(), fakeK8sClient, *node1Cilium)
+
+	select {
+	case <-patchChan:
+	case <-time.Tick(10 * time.Second):
+		t.Errorf("d.fakeK8sClient.CoreV1().Nodes().Update() was not called")
+		t.FailNow()
+	}
+
+	// Test IPv6
+	node2 := v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node2",
+			Annotations: map[string]string{
+				annotation.V4CIDRName:   "10.254.0.0/16",
+				annotation.CiliumHostIP: "10.254.0.1",
 			},
-		}
+		},
+		Spec: v1.NodeSpec{
+			PodCIDR: "aaaa:aaaa:aaaa:aaaa:beef:beef::/96",
+		},
+	}
 
-		// set buffer to 2 to prevent blocking when calling UseNodeCIDR
-		// and we need to wait for the response of the channel.
-		patchChan := make(chan bool, 2)
-		fakeK8sClient := &fake.Clientset{}
-		fakeK8sClient.AddReactor("patch", "nodes",
-			func(action k8stesting.Action) (bool, runtime.Object, error) {
-				n1copy := node1.DeepCopy()
-				n1copy.Annotations[annotation.V4CIDRName] = "10.2.0.0/16"
-				raw, err := json.Marshal(n1copy.Annotations)
-				if err != nil {
-					require.NoError(t, err)
-				}
-				patchWanted := fmt.Appendf(nil, `{"metadata":{"annotations":%s}}`, raw)
+	failAttempts := 0
 
-				patchReceived := action.(k8stesting.PatchAction).GetPatch()
-				require.Equal(t, string(patchWanted), string(patchReceived))
-				patchChan <- true
-				return true, n1copy, nil
-			})
+	fakeK8sClient = &fake.Clientset{}
+	fakeK8sClient.AddReactor("patch", "nodes",
+		func(action k8stesting.Action) (bool, runtime.Object, error) {
+			// first call will be a patch for annotations
+			if failAttempts == 0 {
+				failAttempts++
+				return true, nil, fmt.Errorf("failing on purpose")
+			}
+			n2Copy := node2.DeepCopy()
+			n2Copy.Annotations[annotation.V4CIDRName] = "10.254.0.0/16"
+			n2Copy.Annotations[annotation.V6CIDRName] = "aaaa:aaaa:aaaa:aaaa:beef:beef::/96"
+			raw, err := json.Marshal(n2Copy.Annotations)
+			if err != nil {
+				require.NoError(t, err)
+			}
+			patchWanted := fmt.Appendf(nil, `{"metadata":{"annotations":%s}}`, raw)
 
-		node1Cilium := k8s.ParseNode(logger, toSlimNode(node1.DeepCopy()), source.Unspec)
-		node1Cilium.SetCiliumInternalIP(net.ParseIP("10.254.0.1"))
-		node.SetIPv4AllocRange(node1Cilium.IPv4AllocCIDR)
+			patchReceived := action.(k8stesting.PatchAction).GetPatch()
+			require.Equal(t, string(patchWanted), string(patchReceived))
+			patchChan <- true
+			return true, n2Copy, nil
+		})
 
-		require.Equal(t, "10.2.0.0/16", node.GetIPv4AllocRange(logger).String())
-		// IPv6 Node range is not checked because it shouldn't be changed.
+	node2Cilium := k8s.ParseNode(hivetest.Logger(t), toSlimNode(node2.DeepCopy()), source.Unspec)
+	node2Cilium.SetCiliumInternalIP(net.ParseIP("10.254.0.1"))
 
-		n := &NodeDiscovery{logger: logger}
-		n.annotateK8sNode(t.Context(), fakeK8sClient, *node1Cilium)
+	n.annotateK8sNode(t.Context(), fakeK8sClient, *node2Cilium)
 
-		select {
-		case <-patchChan:
-		case <-time.Tick(10 * time.Second):
-			t.Errorf("d.fakeK8sClient.CoreV1().Nodes().Update() was not called")
-			t.FailNow()
-		}
-
-		// Test IPv6
-		node2 := v1.Node{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "node2",
-				Annotations: map[string]string{
-					annotation.V4CIDRName:   "10.254.0.0/16",
-					annotation.CiliumHostIP: "10.254.0.1",
-				},
-			},
-			Spec: v1.NodeSpec{
-				PodCIDR: "aaaa:aaaa:aaaa:aaaa:beef:beef::/96",
-			},
-		}
-
-		failAttempts := 0
-
-		fakeK8sClient = &fake.Clientset{}
-		fakeK8sClient.AddReactor("patch", "nodes",
-			func(action k8stesting.Action) (bool, runtime.Object, error) {
-				// first call will be a patch for annotations
-				if failAttempts == 0 {
-					failAttempts++
-					return true, nil, fmt.Errorf("failing on purpose")
-				}
-				n2Copy := node2.DeepCopy()
-				n2Copy.Annotations[annotation.V4CIDRName] = "10.254.0.0/16"
-				n2Copy.Annotations[annotation.V6CIDRName] = "aaaa:aaaa:aaaa:aaaa:beef:beef::/96"
-				raw, err := json.Marshal(n2Copy.Annotations)
-				if err != nil {
-					require.NoError(t, err)
-				}
-				patchWanted := fmt.Appendf(nil, `{"metadata":{"annotations":%s}}`, raw)
-
-				patchReceived := action.(k8stesting.PatchAction).GetPatch()
-				require.Equal(t, string(patchWanted), string(patchReceived))
-				patchChan <- true
-				return true, n2Copy, nil
-			})
-
-		node2Cilium := k8s.ParseNode(hivetest.Logger(t), toSlimNode(node2.DeepCopy()), source.Unspec)
-		node2Cilium.SetCiliumInternalIP(net.ParseIP("10.254.0.1"))
-		node.SetIPv4AllocRange(node2Cilium.IPv4AllocCIDR)
-		node.SetIPv6NodeRange(node2Cilium.IPv6AllocCIDR)
-
-		// We use the node's annotation for the IPv4 and the PodCIDR for the
-		// IPv6.
-		require.Equal(t, "10.254.0.0/16", node.GetIPv4AllocRange(logger).String())
-		require.Equal(t, "aaaa:aaaa:aaaa:aaaa:beef:beef::/96", node.GetIPv6AllocRange(logger).String())
-
-		n.annotateK8sNode(t.Context(), fakeK8sClient, *node2Cilium)
-
-		select {
-		case <-patchChan:
-		case <-time.Tick(10 * time.Second):
-			t.Errorf("d.fakeK8sClient.CoreV1().Nodes().Update() was not called")
-			t.FailNow()
-		}
-	})
+	select {
+	case <-patchChan:
+	case <-time.Tick(10 * time.Second):
+		t.Errorf("d.fakeK8sClient.CoreV1().Nodes().Update() was not called")
+		t.FailNow()
+	}
 }
 
 func convertToAddress(v1Addrs []v1.NodeAddress) []slim_corev1.NodeAddress {
