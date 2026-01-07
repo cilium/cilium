a151b5453ed37bb31639842d5a6016afc8c80d28
Marco Hofstetter <marco.hofstetter@isovalent.com>
2026-01-07 11:49:13 +0100

    k8s/podwatcher: use localnodestore
    This commit refactors the k8s podwatcher to use the injected
    `LocalNodeStore` to retrieve the local node instead of
    using the global node function `node.GetEndpointEncryptKeyIndex`
    that uses the globally held `LocalNodeStore`. It provides a new global
    function that expects the local node as parameter, this way we can
    eventually get rid of that global variable.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/node/address.go
+++ pkg/node/address.go
@@ -332,9 +332,20 @@ func GetIngressIPv6(logger *slog.Logger) net.IP {
 // of the local node. If you want to obtain the key index of the local node itself,
 // access the `EncryptionKey` field via the LocalNodeStore.
 func GetEndpointEncryptKeyIndex(logger *slog.Logger, wgEnabled, ipsecEnabled bool) uint8 {
+	return GetEndpointEncryptKeyIndexWithNode(getLocalNode(logger), wgEnabled, ipsecEnabled)
+}
+
+// GetEndpointEncryptKeyIndexWithNode returns the encryption key value for an endpoint
+// owned by the given local node.
+// With IPSec encryption, this is the ID of the currently loaded key.
+// With WireGuard, this returns a non-zero static value.
+// Note that the key index returned by this function is only valid for _endpoints_
+// of the local node. If you want to obtain the key index of the local node itself,
+// access the `EncryptionKey` field via the LocalNodeStore.
+func GetEndpointEncryptKeyIndexWithNode(localNode LocalNode, wgEnabled, ipsecEnabled bool) uint8 {
 	switch {
 	case ipsecEnabled:
-		return getLocalNode(logger).EncryptionKey
+		return localNode.EncryptionKey
 	case wgEnabled:
 		return wgTypes.StaticEncryptKey
 
