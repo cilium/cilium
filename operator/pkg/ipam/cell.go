241bd498bdc0bc361ac34c929f7bf31aeec76d65
Fabio Falzoi <fabio.falzoi@isovalent.com>
2026-01-09 18:51:20 +0100

    operator/ipam: Add allocators common config
    ParallelAllocWorkers is a configuration option used in both AWS and
    Azure allocators, therefore it is declared as a cell.Config in the
    operator IP allocator provide cell.
    
    Azure and AWS allocators will be added in a subsequent commit and will
    relay on this shared config.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- operator/pkg/ipam/cell.go
+++ operator/pkg/ipam/cell.go
@@ -6,7 +6,9 @@ package ipam
 import (
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
+	"github.com/spf13/pflag"
 
+	"github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/ipam/allocator"
 )
 
@@ -14,6 +16,7 @@ var Cell = cell.Module(
 	"ip-allocator-provider",
 	"Operator IP allocator provider",
 
+	cell.Config(defaultConfig),
 	cell.ProvidePrivate(newNodeWatcherJobFactory),
 
 	cell.Group(
@@ -21,5 +24,17 @@ var Cell = cell.Module(
 		multiPoolCell,
 	),
 )
+
+type Config struct {
+	ParallelAllocWorkers int64
+}
+
+var defaultConfig = Config{
+	ParallelAllocWorkers: 50,
+}
+
+func (cfg Config) Flags(flags *pflag.FlagSet) {
+	flags.Int64(option.ParallelAllocWorkers, defaultConfig.ParallelAllocWorkers, "Maximum number of parallel IPAM workers")
+}
 
 type nodeWatcherJobFactory func(nm allocator.NodeEventHandler) job.Job
