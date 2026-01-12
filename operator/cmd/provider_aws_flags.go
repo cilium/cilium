da140cbfdd356ba7def747829d79310da8133122
Fabio Falzoi <fabio.falzoi@isovalent.com>
2026-01-12 15:14:47 +0100

    operator/ipam: Remove AWS deprecated option
    Remove deprecated AWS AWSPaginationEnabled option. Also, move
    AWSMaxResultsPerCall into the AWS IPAM cell. AWSMaxResultsPerCall was
    kept as a global operator option to keep the backward compatibility with
    the AWSPaginationEnabled logic, but since that has been removed
    MaxResultsPerCall can be treated just like any other AWS option.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- operator/cmd/provider_aws_flags.go
+++ operator/cmd/provider_aws_flags.go
@@ -8,9 +8,6 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-
-	operatorOption "github.com/cilium/cilium/operator/option"
-	"github.com/cilium/cilium/pkg/option"
 )
 
 func init() {
@@ -21,15 +18,6 @@ type awsFlagsHooks struct{}
 
 func (hook *awsFlagsHooks) RegisterProviderFlag(cmd *cobra.Command, vp *viper.Viper) {
 	flags := cmd.Flags()
-
-	flags.Int32(operatorOption.AWSMaxResultsPerCall, 0, "Maximum results per AWS API call for DescribeNetworkInterfaces and DescribeSecurityGroups. Set to 0 to let AWS determine optimal page size (default). If set to 0 and AWS returns OperationNotPermitted errors, automatically switches to 1000 for all future requests")
-	option.BindEnv(vp, operatorOption.AWSMaxResultsPerCall)
-
-	// Deprecated: aws-pagination-enabled is deprecated in v1.19 and will be removed in v1.20.
-	// Use --aws-max-results-per-call instead (true maps to 1000, false maps to 0).
-	flags.Bool(operatorOption.AWSPaginationEnabled, true, "Deprecated: Use --aws-max-results-per-call instead")
-	flags.MarkHidden(operatorOption.AWSPaginationEnabled)
-	option.BindEnv(vp, operatorOption.AWSPaginationEnabled)
 
 	vp.BindPFlags(flags)
 }
