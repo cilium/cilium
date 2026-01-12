32ab84ba4e2d328d7ecbff450e637b5e71ab0636
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2026-01-12 17:34:06 +0000

    datapath/connector: add additional linkpair testing
    Add additional testing for link pairs, which also moves existing
    test logic to use temporary namespaces.
    
    Signed-off-by: Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>

--- pkg/datapath/connector/link_test.go
+++ pkg/datapath/connector/link_test.go
@@ -4,20 +4,22 @@
 package connector
 
 import (
-	"errors"
-	"log/slog"
+	"fmt"
+	"net"
 	"testing"
 
 	"github.com/cilium/hive/hivetest"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/vishvananda/netlink"
 
 	"github.com/cilium/cilium/pkg/datapath/linux/safenetlink"
 	"github.com/cilium/cilium/pkg/datapath/linux/sysctl"
 	"github.com/cilium/cilium/pkg/datapath/types"
-	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/mac"
 	"github.com/cilium/cilium/pkg/testutils"
+	"github.com/cilium/cilium/pkg/testutils/netns"
 )
 
 var (
@@ -26,38 +28,75 @@ var (
 	TestEndpointID          = string("d34db33fc4f3:linkpeertest0")
 	TestEndpointTmpIfName   = string("tmpd34db")
 	TestEndpointFinalIfName = string("linkpeertest0")
-	TestMTU                 = int(1500)
+
+	// MTU values
+	TestStandardMTU = int(1500)
+	TestSmallMTU    = int(1200)
+
+	// GRO values
+	TestGROMaxSize = int(16384)
+	TestGSOMaxSize = int(8192)
 
 	NamedLinkConfig = types.LinkConfig{
 		HostIfName: TestHostIfName,
 		PeerIfName: TestPeerIfName,
-		DeviceMTU:  TestMTU,
+		DeviceMTU:  TestStandardMTU,
 	}
 	EndpointLinkConfig = types.LinkConfig{
 		EndpointID: TestEndpointID,
-		DeviceMTU:  TestMTU,
+		DeviceMTU:  TestStandardMTU,
 	}
 )
 
-func deleteInterfaces(log *slog.Logger, ifNames []string) {
-	for _, ifName := range ifNames {
-		iface, err := safenetlink.LinkByName(ifName)
-		if err != nil {
-			if !errors.As(err, &netlink.LinkNotFoundError{}) {
-				log.Error("Failed to lookup test interface",
-					logfields.Interface, ifName,
-					logfields.Error, err,
-				)
-			}
-		} else if err = netlink.LinkDel(iface); err != nil {
-			if !errors.As(err, &netlink.LinkNotFoundError{}) {
-				log.Error("Failed to delete test interface",
-					logfields.Interface, ifName,
-					logfields.Error, err,
-				)
-			}
-		}
+func createFakePair(tb testing.TB, h *netlink.Handle, hostIfName string, peerIfName string) {
+	tb.Helper()
+
+	if h == nil {
+		tb.Fatalf("bad netlink handle")
 	}
+	if hostIfName == "" || peerIfName == "" {
+		tb.Fatalf("no ifNames supplied")
+	}
+
+	hostMacAddr, err := mac.GenerateRandMAC()
+	if err != nil {
+		tb.Fatalf("error generatign host mac addr: %v", err)
+	}
+
+	peerMacAddr, err := mac.GenerateRandMAC()
+	if err != nil {
+		tb.Fatalf("error generating fake peer mac addr: %v", err)
+	}
+
+	hostVeth := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:         hostIfName,
+			HardwareAddr: net.HardwareAddr(hostMacAddr),
+			TxQLen:       1000,
+		},
+		PeerName:         peerIfName,
+		PeerHardwareAddr: net.HardwareAddr(peerMacAddr),
+	}
+	if err := h.LinkAdd(hostVeth); err != nil {
+		tb.Fatalf("error creating fake veth pair: %v", err)
+	}
+}
+
+func queryLinkSafe(tb testing.TB, h *netlink.Handle, ifName string) netlink.Link {
+	tb.Helper()
+
+	if h == nil {
+		tb.Fatalf("bad netlink handle")
+	}
+
+	link, err := safenetlink.WithRetryResult(func() (netlink.Link, error) {
+		//nolint:forbidigo
+		return h.LinkByName(ifName)
+	})
+	if err != nil {
+		tb.Fatalf("LinkByName failed: %v", err)
+	}
+	return link
 }
 
 func TestPrivilegedNewLinkPair(t *testing.T) {
@@ -92,7 +131,7 @@ func TestPrivilegedNewLinkPair(t *testing.T) {
 			name: "mode-veth+no-names-and-endpoint",
 			mode: types.ConnectorModeVeth,
 			cfg: types.LinkConfig{
-				DeviceMTU: TestMTU,
+				DeviceMTU: TestStandardMTU,
 			},
 			shouldSkip:  false,
 			shouldError: true,
@@ -104,7 +143,7 @@ func TestPrivilegedNewLinkPair(t *testing.T) {
 			mode: types.ConnectorModeVeth,
 			cfg: types.LinkConfig{
 				HostIfName: "hostifnameonly",
-				DeviceMTU:  TestMTU,
+				DeviceMTU:  TestStandardMTU,
 			},
 			shouldSkip:  false,
 			shouldError: true,
@@ -116,7 +155,7 @@ func TestPrivilegedNewLinkPair(t *testing.T) {
 			mode: types.ConnectorModeVeth,
 			cfg: types.LinkConfig{
 				PeerIfName: "peerifnameonly",
-				DeviceMTU:  TestMTU,
+				DeviceMTU:  TestStandardMTU,
 			},
 			shouldSkip:  false,
 			shouldError: true,
@@ -171,37 +210,91 @@ func TestPrivilegedNewLinkPair(t *testing.T) {
 		},
 	}
 
-	testInterfaces := []string{
-		TestHostIfName,
-		TestEndpointTmpIfName,
-		TestEndpointFinalIfName,
-	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if tt.shouldSkip {
 				t.Skip()
 			}
 
-			ctl := sysctl.NewDirectSysctl(afero.NewOsFs(), "/proc")
-			linkPair, err := NewLinkPair(log, tt.mode, tt.cfg, ctl)
+			var linkPair *LinkPair
 
-			switch tt.shouldError {
-			case true:
-				assert.Error(t, err)
-				assert.Nil(t, linkPair)
+			ns := netns.NewNetNS(t)
+			require.NoError(t, ns.Do(func() error {
+				ctl := sysctl.NewDirectSysctl(afero.NewOsFs(), "/proc")
 
-			case false:
-				// Ensure we cleanup before we assert
-				t.Cleanup(func() { deleteInterfaces(log, testInterfaces) })
+				var err error
+				linkPair, err = NewLinkPair(log, tt.mode, tt.cfg, ctl)
 
-				assert.NoError(t, err)
-				if assert.NotNil(t, linkPair) {
-					assert.Equal(t, linkPair.mode, tt.mode)
+				if tt.shouldError && err != nil {
+					return nil
 				}
+				return err
+			}))
+
+			if tt.shouldError {
+				require.Nil(t, linkPair)
+			} else {
+				require.NotNil(t, linkPair)
+				assert.NotNil(t, linkPair.hostLink)
+				assert.NotNil(t, linkPair.peerLink)
+				assert.Equal(t, linkPair.mode, tt.mode)
 			}
 		})
 	}
+}
+
+func TestPrivilegedConfigureLinkPair(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	cfg := types.LinkConfig{
+		DeviceMTU:      TestSmallMTU,
+		GROIPv4MaxSize: TestGROMaxSize,
+		GROIPv6MaxSize: TestGROMaxSize,
+		GSOIPv4MaxSize: TestGSOMaxSize,
+		GSOIPv6MaxSize: TestGSOMaxSize,
+	}
+
+	var h *netlink.Handle
+	ns := netns.NewNetNS(t)
+
+	require.NoError(t, ns.Do(func() error {
+		var err error
+
+		h, err = safenetlink.NewHandle(nil)
+		if err != nil {
+			return fmt.Errorf("bad netlink handle: %w", err)
+		}
+
+		// For the purposes of this test, we will operate on a dummy veth pair.
+		createFakePair(t, h, TestHostIfName, TestPeerIfName)
+
+		hostLink := queryLinkSafe(t, h, TestHostIfName)
+		peerLink := queryLinkSafe(t, h, TestPeerIfName)
+
+		if err := configureLinkPair(hostLink, peerLink, cfg); err != nil {
+			return err
+		}
+		return nil
+	}))
+
+	assertLinkConfig := func(attrs *netlink.LinkAttrs, cfg *types.LinkConfig) {
+		// MTU
+		assert.Equal(t, attrs.MTU, cfg.DeviceMTU)
+
+		// GRO
+		assert.Equal(t, attrs.GROIPv4MaxSize, uint32(cfg.GROIPv4MaxSize))
+		assert.Equal(t, attrs.GROMaxSize, uint32(cfg.GROIPv6MaxSize))
+
+		// GSO
+		assert.Equal(t, attrs.GSOIPv4MaxSize, uint32(cfg.GSOIPv4MaxSize))
+		assert.Equal(t, attrs.GSOMaxSize, uint32(cfg.GSOIPv6MaxSize))
+	}
+
+	newHostLink := queryLinkSafe(t, h, TestHostIfName)
+	assertLinkConfig(newHostLink.Attrs(), &cfg)
+
+	newPeerLink := queryLinkSafe(t, h, TestPeerIfName)
+	assertLinkConfig(newPeerLink.Attrs(), &cfg)
 }
 
 func TestPrivilegedLinkPairDelete(t *testing.T) {
@@ -231,19 +324,31 @@ func TestPrivilegedLinkPairDelete(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ctl := sysctl.NewDirectSysctl(afero.NewOsFs(), "/proc")
-			linkPair, err := NewLinkPair(log, tt.mode, tt.cfg, ctl)
+			var linkPair *LinkPair
 
-			assert.NoError(t, err)
-			if assert.NotNil(t, linkPair) {
-				// Check we can delete this interface pair
-				err := linkPair.Delete()
-				if assert.NoError(t, err) {
-					// Check we can't delete it twice
-					err := linkPair.Delete()
-					assert.NoError(t, err)
+			ns := netns.NewNetNS(t)
+			require.NoError(t, ns.Do(func() error {
+				var err error
+
+				ctl := sysctl.NewDirectSysctl(afero.NewOsFs(), "/proc")
+				linkPair, err = NewLinkPair(log, tt.mode, tt.cfg, ctl)
+
+				if tt.shouldError && err != nil {
+					return nil
 				}
-			}
+				return err
+			}))
+
+			require.NotNil(t, linkPair)
+			assert.NotNil(t, linkPair.hostLink)
+			assert.NotNil(t, linkPair.peerLink)
+
+			require.NoError(t, ns.Do(func() error {
+				return linkPair.Delete()
+			}))
+
+			assert.Nil(t, linkPair.hostLink)
+			assert.Nil(t, linkPair.peerLink)
 		})
 	}
 }
