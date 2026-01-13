cilium/cilium

commit c4d6072fa2a4b67a02f44106ee2902fb6d8e03ee

loadbalancer: do not create wildcard entries for Node-IPAM VIPs
Currently, the loadbalancer's BPF reconciler will create "wildcard"
service entries in the Cilium datapath for any externally facing VIP,
to ensure the datapath does not punt IP traffic towards VIPs on ports
that are not actually mapped to a service.

This works as intended with LB-IPAM, but if VIPs are allocated with
Node-IPAM, the result is that Node IPs are used instead. This leads to
wildcard entries being programmed with Node IPs, which disrupts control
traffic within a cluster.

This commit introduces additional logic in the loadbalancer's BPF
reconciler to only program wildcard service entries if either of
the following cases are true.

1. There is no loadBalancerClass defined on the service, and the
   default LB service IPAM is LB-IPAM.

2. There is a defined loadBalancerClass defined on the service, and
   it is set to either of the following strings:
   - BGPLoadBalancerClass ("io.cilium/bgp-control-plane")
   - L2AnnounceLoadBalancerClass ("io.cilium/l2-announcer")

This ensures the loadbalancer does not program wildcard service entries
for Node-IPAM allocated VIPs.

Example with service allocated via LB-IPAM:

  $ kubectl get svc netshoot-service -o json | jq '{loadBalancerClass: .spec.loadBalancerClass, ingress: .status.loadBalancer.ingress}'
  {
    "loadBalancerClass": null,
    "ingress": [
      {
        "ip": "10.244.255.200",
        "ipMode": "VIP"
      },
      {
        "ip": "fd00:10:244:ff:ffff:ffff:ffff:ff00",
        "ipMode": "VIP"
      }
    ]
  }

Note presence of wildcard "ANY" entry in datapath:

  # cilium bpf lb list |  grep -E '10.244.255.200.*LoadBalancer'
  10.244.255.200:8000/TCP (0)                         0.0.0.0:0 (19) (0) [LoadBalancer]
  10.244.255.200:8000/UDP (0)                         0.0.0.0:0 (20) (0) [LoadBalancer]
  10.244.255.200:0/ANY (0)                            0.0.0.0:0 (0) (0) [LoadBalancer, non-routable]

pkg/loadbalancer/reconciler/bpf_reconciler.go
@@ -25,12 +25,14 @@ import (
 	"github.com/cilium/cilium/pkg/byteorder"
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/datapath/tables"
+	"github.com/cilium/cilium/pkg/lbipamconfig"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/loadbalancer/maps"
 	"github.com/cilium/cilium/pkg/loadbalancer/writer"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maglev"
+	"github.com/cilium/cilium/pkg/nodeipamconfig"
 	"github.com/cilium/cilium/pkg/promise"
 	"github.com/cilium/cilium/pkg/time"
 	"github.com/cilium/cilium/pkg/u8proto"
@@ -1145,8 +1147,23 @@ func (ops *BPFOps) useWildcard(fe *loadbalancer.Frontend) bool {
 	switch fe.Type {
 	case loadbalancer.SVCTypeLoadBalancer,
 		loadbalancer.SVCTypeClusterIP:
+
 		// Only external scoped entries can parent wildcard entries
-		return fe.Address.Scope() == loadbalancer.ScopeExternal
+		if fe.Address.Scope() != loadbalancer.ScopeExternal {
+			return false
+		}
+
+		// We only want to program wildcard entries if LB-IPAM has allocated
+		// the VIP, otherwise we risk programming Node internal IPs into the
+		// datapath and causing connectivity faults.
+		lbClass := fe.Service.LoadBalancerClass
+		if lbClass == nil {
+			return ops.extCfg.DefaultLBServiceIPAM == lbipamconfig.DefaultLBClassLBIPAM
+		}
+
+		// The service has a loadBalancerClass, so we expicitly exclude the
+		// nodeipam annotation here.
+		return *lbClass != nodeipamconfig.NodeSvcLBClass
 	}
 	return false
 }

commit 3112aef4595b01f63cd107824f65f0a321e0818b

pkg/loadbalancer: further restrict use of wildcard svc entries
Following the introduction of wildcard service entries in the
loadbalancer in commit b3b0fc849f, it became possible to disrupt
internal cluster traffic, where a VIP was allocated via NodeIPAM.

An initial fix was applied for this scenario in commit c4d6072fa2,
which updated the loadbalancer's BPF reconciler to restrict the use
of wildcard service entries if using NodeIPAM, either by default or
via loadBalancerClass.

Unfortunately the revised logic still permits wildcard service
entries if non-Cilium IPAM is used, which can still cause disruption
to cluster traffic [0].

This commit reverses the loadBalancerClass check to instead only use
wildcard service entries if the loadBalancerClass is defined and is
either Cilium's BGPLoadBalancerClass or L2AnnounceLoadBalancerClass.

This change has required modification of unit tests that set a dummy
loadBalancerClass. However, additional tests will be added in the next
commit to verify this change in behaviour.

[0] https://github.com/cilium/cilium/issues/43206#issuecomment-3701608269

Fixes: c4d6072fa2 ("loadbalancer: do not create wildcard entries for Node-IPAM VIPs")
Signed-off-by: Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@ledger.fr>
Signed-off-by: Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>

pkg/loadbalancer/reconciler/bpf_reconciler.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cilium/cilium/pkg/byteorder"
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/datapath/tables"
+	cilium_api_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/lbipamconfig"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/loadbalancer/maps"
@@ -32,7 +33,6 @@ import (
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maglev"
-	"github.com/cilium/cilium/pkg/nodeipamconfig"
 	"github.com/cilium/cilium/pkg/promise"
 	"github.com/cilium/cilium/pkg/time"
 	"github.com/cilium/cilium/pkg/u8proto"
@@ -1161,9 +1161,14 @@ func (ops *BPFOps) useWildcard(fe *loadbalancer.Frontend) bool {
 			return ops.extCfg.DefaultLBServiceIPAM == lbipamconfig.DefaultLBClassLBIPAM
 		}
 
-		// The service has a loadBalancerClass, so we expicitly exclude the
-		// nodeipam annotation here.
-		return *lbClass != nodeipamconfig.NodeSvcLBClass
+		// The service has a loadBalancerClass, so we only include a wildcard
+		// service entry if it's a class Cilium actively manages, again to avoid
+		// programming wildcard entries for IP addresses we don't manage.
+		//
+		// TODO: improve this in future, perhaps by matching against known
+		// CiliumInternalIPs.
+		return *lbClass == cilium_api_v2alpha1.BGPLoadBalancerClass ||
+			*lbClass == cilium_api_v2alpha1.L2AnnounceLoadBalancerClass
 	}
 	return false
 }

pkg/loadbalancer/tests/testdata/loadbalancer-multiport.txtar
@@ -44,7 +44,7 @@ Address NodePort Primary DeviceName
 
 -- services.table --
 Name        Source   PortNames            TrafficPolicy   Flags
-test/echo   k8s      http=80, https=443   Cluster         LoadBalancerClass=test
+test/echo   k8s      http=80, https=443   Cluster
 
 -- frontends.table --
 Address                Type           ServiceName   PortName   Backends                                 RedirectTo   Status
@@ -142,7 +142,6 @@ spec:
   selector:
     name: echo
   sessionAffinity: None
-  loadBalancerClass: "test"
   type: LoadBalancer
 status:
   loadBalancer:

pkg/loadbalancer/tests/testdata/loadbalancer-multiprotocol.txtar
@@ -44,7 +44,7 @@ Address NodePort Primary DeviceName
 
 -- services.table --
 Name        Source   PortNames                      TrafficPolicy   Flags
-test/echo   k8s      dtls=443, http=80, https=443   Cluster         LoadBalancerClass=test
+test/echo   k8s      dtls=443, http=80, https=443   Cluster         
 
 -- frontends.table --
 Address                Type           ServiceName   PortName   Backends                                 RedirectTo   Status
@@ -173,7 +173,6 @@ spec:
   selector:
     name: echo
   sessionAffinity: None
-  loadBalancerClass: "test"
   type: LoadBalancer
 status:
   loadBalancer:

pkg/loadbalancer/tests/testdata/loadbalancer.txtar
@@ -50,7 +50,7 @@ Address NodePort Primary DeviceName
 
 -- services.table --
 Name         Source   PortNames  TrafficPolicy   Flags
-test/echo    k8s      http=80    Cluster         LoadBalancerClass=test
+test/echo    k8s      http=80    Cluster         
 
 -- frontends.table --
 Address               Type         ServiceName   PortName   Status  Backends
@@ -122,7 +122,6 @@ spec:
   selector:
     name: echo
   sessionAffinity: None
-  loadBalancerClass: "test"
   type: LoadBalancer
 status:
   loadBalancer: