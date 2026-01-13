483224187c92c3dc1879bc7790530559f6e4d693
Julian Wiedmann <jwi@isovalent.com>
2026-01-13 07:27:43 +0200

    bpf: nodeport: dsr: don't create NAT entries for RevDNAT
    The infrastructure for CT entry-based RevDNAT
    (https://github.com/cilium/cilium/pull/43017) landed in v1.19. And thus
    v1.20 can rely on it for downgrade purposes, and stop creating NAT entries.
    
    Fixes: #43339
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/tests/tc_geneve_dsr_v4_legacy.c
+++ bpf/tests/tc_geneve_dsr_v4_legacy.c
@@ -94,21 +94,11 @@ int tc_geneve_dsr_v4_legacy_check(struct __ctx_buff *ctx)
 	void *data, *data_end;
 	__u32 *status_code;
 	struct ct_entry *ct_entry;
-	struct ipv4_nat_entry *nat_entry;
 	struct ipv4_ct_tuple expected_tuple_for_ct = {
 		.saddr   = BACKEND_IP,
 		.daddr   = CLIENT_IP,
 		.sport   = CLIENT_PORT,
 		.dport   = BACKEND_PORT,
-		.nexthdr = IPPROTO_TCP,
-		.flags   = TUPLE_F_OUT,
-	};
-
-	struct ipv4_ct_tuple expected_tuple_for_nat = {
-		.saddr   = BACKEND_IP,
-		.daddr   = CLIENT_IP,
-		.sport   = BACKEND_PORT,
-		.dport   = CLIENT_PORT,
 		.nexthdr = IPPROTO_TCP,
 		.flags   = TUPLE_F_OUT,
 	};
@@ -129,11 +119,6 @@ int tc_geneve_dsr_v4_legacy_check(struct __ctx_buff *ctx)
 	ct_entry = map_lookup_elem(&cilium_ct4_global, &expected_tuple_for_ct);
 	if (!ct_entry)
 		test_fatal("No entry in conntrack map");
-
-	/* Verify that the datapath inserted the SNAT entry */
-	nat_entry = snat_v4_lookup(&expected_tuple_for_nat);
-	if (!nat_entry)
-		test_fatal("No entry in SNAT map");
 
 	test_finish();
 }
