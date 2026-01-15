d28e9af733f6c67c5126f4c66639f65e2591f3f9
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2026-01-15 20:45:47 +0100

    bpf, nat46x64: move RFC6052 prefix into node config
    This commit moves NAT 46x64 RFC6052 prefix bytes into the node
    configuration so BPF programs consume CONFIG(nat_46x64_prefix) value
    instead of C header defines.
    The Go datapath now populates this field from the NAT46x64 config, and
    the headerfile writer no longer emits NAT_46X64_PREFIX_ defines.
    
    Updates included:
    
    - BPF nat_46x64 helpers switched to CONFIG(nat_46x64_prefix).
    - Node config population moved to runtime config and legacy defines
      dropped.
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- bpf/node_config.h
+++ bpf/node_config.h
@@ -180,13 +180,6 @@ return false;
 #define CIDR_IDENTITY_RANGE_START ((1 << 24) + 1)
 #define CIDR_IDENTITY_RANGE_END   ((1 << 24) + (1<<16) - 1)
 
-#ifndef NAT_46X64_PREFIX_0
-# define NAT_46X64_PREFIX_0 0
-# define NAT_46X64_PREFIX_1 0
-# define NAT_46X64_PREFIX_2 0
-# define NAT_46X64_PREFIX_3 0
-#endif
-
 /*
  *   **** WARNING, THIS FILE IS DEPRECATED, SEE COMMENT AT THE TOP ****
  */
