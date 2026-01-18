701a487674efe9bea0c37d5ba87c0b8db78c1195
Liyi Huang <pdshly@gmail.com>
2026-01-18 15:07:10 -0500

    bpf: correct comments in cil_from_netdev function
    Removed conditions from the comment block describing the cil_from_netdev function since the logic has been changed
    
    Signed-off-by: Liyi Huang <liyi.huang@isovalent.com>

--- bpf/bpf_host.c
+++ bpf/bpf_host.c
@@ -1172,10 +1172,7 @@ drop_err_ingress: __maybe_unused
 
 /*
  * from-netdev is attached as a tc ingress filter to one or more physical devices
- * managed by Cilium (e.g., eth0). This program is only attached when:
- * - the host firewall is enabled, or
- * - BPF NodePort is enabled, or
- * - L2 announcements are enabled
+ * managed by Cilium (e.g., eth0).
  */
 __section_entry
 int cil_from_netdev(struct __ctx_buff *ctx)
