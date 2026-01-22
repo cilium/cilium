f85d72c265189ad8ae9c203d85166472ca3b090f
Julian Wiedmann <jwi@isovalent.com>
2026-01-22 10:51:00 +0200

    Revert "bpf: migrate CILIUM_NET_MAC to runtime config"
    This reverts commit 9bdf851b7d99e2371ec7891b50a340a350e8f46e.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- pkg/datapath/orchestrator/localnodeconfig.go
+++ pkg/datapath/orchestrator/localnodeconfig.go
@@ -127,10 +127,6 @@ func newLocalNodeConfig(
 	if !ok {
 		return datapath.LocalNodeConfiguration{}, nil, fmt.Errorf("failed to look up link '%s'", defaults.SecondHostDevice)
 	}
-	ciliumNetMAC, err := mac.ParseMAC(ciliumNetDevice.HardwareAddr.String())
-	if err != nil {
-		return datapath.LocalNodeConfiguration{}, nil, fmt.Errorf("failed to parse hardware address of '%s': %w", defaults.SecondHostDevice, err)
-	}
 
 	return datapath.LocalNodeConfiguration{
 		NodeIPv4:                     localNode.GetNodeIP(false),
@@ -138,7 +134,6 @@ func newLocalNodeConfig(
 		CiliumInternalIPv4:           localNode.GetCiliumInternalIP(false),
 		CiliumInternalIPv6:           localNode.GetCiliumInternalIP(true),
 		CiliumNetIfIndex:             uint32(ciliumNetDevice.Index),
-		CiliumNetMAC:                 ciliumNetMAC,
 		CiliumHostIfIndex:            uint32(ciliumHostDevice.Index),
 		CiliumHostMAC:                ciliumHostMAC,
 		AllocCIDRIPv4:                localNode.IPv4AllocCIDR,
