5e0d4bb568ee3b60ddc40800c22d9531fbbcc6c3
Fabio Falzoi <fabio.falzoi@isovalent.com>
2026-01-22 17:13:34 +0100

    operator/multi-pool: Move autoCreateIPPools into multi pool cell
    Just like the multi pool IPAM implementation, the auto creation of
    CiliumPodIPPool feature can be split into two parts:
    
    - a reusable set of functions to auto create pools based on a generic
      description
    - a set of functions that finalize the creation of the pools in a
      specific kind of k8s object
    
    While the first part could be reused for auto creation of multi pool
    Resource IPAM pools too (that is, for CiliumResourceIPPool objects), the
    second one implements the creation of pools specific for multi pool Pod
    IPAM, namely CiliumPodIPPool objects.
    
    The commit move the pod specific part into the multi pool cell for Pods
    IPAM.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- Documentation/cmdref/cilium-operator-generic.md
+++ Documentation/cmdref/cilium-operator-generic.md
@@ -11,7 +11,7 @@ cilium-operator-generic [flags]
 ### Options
 
 ```
-      --auto-create-cilium-pod-ip-pools map                  Automatically create CiliumPodIPPool resources on startup. Specify pools in the form of <pool>=ipv4-cidrs:<cidr>,[<cidr>...];ipv4-mask-size:<size> (multiple pools can also be passed by repeating the CLI flag)
+      --auto-create-cilium-pod-ip-pools stringToString       Automatically create CiliumPodIPPool resources on startup. Specify pools in the form of <pool>=ipv4-cidrs:<cidr>,[<cidr>...];ipv4-mask-size:<size> (multiple pools can also be passed by repeating the CLI flag) (default [])
       --ces-max-ciliumendpoints-per-ces int                  Maximum number of CiliumEndpoints allowed in a CES (default 100)
       --ces-rate-limits string                               Configure rate limits for the CES controller. Accepts a list of rate limit configurations, must be a JSON formatted string. (default "[{\"nodes\":0,\"limit\":10,\"burst\":20}]")
       --cilium-endpoint-gc-interval duration                 GC interval for cilium endpoints (default 5m0s)
--- operator/pkg/ipam/multipool.go
+++ operator/pkg/ipam/multipool.go
@@ -6,16 +6,24 @@
 package ipam
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
 
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
+	"github.com/spf13/pflag"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/operator/pkg/multipool"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	"github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	cilium_api_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	cilium_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/client"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/resource"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -28,8 +36,23 @@ func init() {
 		"multipool-ipam-allocator",
 		"Multi Pool IP Allocator",
 
+		cell.Config(multiPoolDefaultConfig),
 		cell.Invoke(startMultiPoolAllocator),
 	))
+}
+
+type MultiPoolConfig struct {
+	AutoCreatePools map[string]string `mapstructure:"auto-create-cilium-pod-ip-pools"`
+}
+
+var multiPoolDefaultConfig = MultiPoolConfig{
+	AutoCreatePools: nil,
+}
+
+func (cfg MultiPoolConfig) Flags(flags *pflag.FlagSet) {
+	flags.StringToString(operatorOption.IPAMAutoCreateCiliumPodIPPools, multiPoolDefaultConfig.AutoCreatePools,
+		"Automatically create CiliumPodIPPool resources on startup. "+
+			"Specify pools in the form of <pool>=ipv4-cidrs:<cidr>,[<cidr>...];ipv4-mask-size:<size> (multiple pools can also be passed by repeating the CLI flag)")
 }
 
 type multiPoolParams struct {
@@ -43,6 +66,8 @@ type multiPoolParams struct {
 	DaemonCfg          *option.DaemonConfig
 	CiliumPodIPPools   resource.Resource[*cilium_api_v2alpha1.CiliumPodIPPool]
 	NodeWatcherFactory nodeWatcherJobFactory
+
+	MultiPoolCfg MultiPoolConfig
 }
 
 func startMultiPoolAllocator(p multiPoolParams) {
@@ -57,10 +82,14 @@ func startMultiPoolAllocator(p multiPoolParams) {
 	p.Lifecycle.Append(
 		cell.Hook{
 			OnStart: func(ctx cell.HookContext) error {
+				if err := multiPoolAutoCreatePools(ctx, p.Clientset, p.MultiPoolCfg.AutoCreatePools, logger); err != nil {
+					return err
+				}
+
 				// The following operation will block until all pools are restored, thus it
 				// is safe to continue starting node allocation right after return.
 				multipool.StartIPPoolAllocator(
-					ctx, p.Clientset, allocator, p.CiliumPodIPPools,
+					ctx, allocator, p.CiliumPodIPPools,
 					p.Logger.With(logfields.LogSubsys, "ip-pool-watcher"),
 				)
 
@@ -78,4 +107,49 @@ func startMultiPoolAllocator(p multiPoolParams) {
 			},
 		},
 	)
+}
+
+func multiPoolAutoCreatePools(ctx context.Context, clientset client.Clientset, poolMap map[string]string, logger *slog.Logger) error {
+	for poolName, poolSpecStr := range poolMap {
+		v4PoolSpec, v6PoolSpec, err := multipool.ParsePoolSpec(poolSpecStr)
+		if err != nil {
+			logger.ErrorContext(ctx,
+				fmt.Sprintf("Failed to parse IP pool spec in %q flag", operatorOption.IPAMAutoCreateCiliumPodIPPools),
+				logfields.PoolName, poolName,
+				logfields.PoolSpec, poolSpecStr,
+				logfields.Error, err)
+			return err
+		}
+
+		pool := &cilium_v2alpha1.CiliumPodIPPool{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: poolName,
+			},
+			Spec: cilium_v2alpha1.IPPoolSpec{
+				IPv4: v4PoolSpec,
+				IPv6: v6PoolSpec,
+			},
+		}
+
+		_, err = clientset.CiliumV2alpha1().CiliumPodIPPools().Create(ctx, pool, metav1.CreateOptions{})
+		if err != nil {
+			if k8sErrors.IsAlreadyExists(err) {
+				// Nothing to do, we will not try to update an existing resource
+				logger.InfoContext(ctx,
+					"Found existing CiliumPodIPPool resource. Skipping creation",
+					logfields.PoolName, poolName)
+			} else {
+				logger.ErrorContext(ctx,
+					"Failed to create CiliumPodIPPool resource",
+					logfields.PoolName, poolName,
+					logfields.Object, pool,
+					logfields.Error, err)
+			}
+			continue
+		}
+
+		logger.InfoContext(ctx, "Created CiliumPodIPPool resource", logfields.PoolName, poolName)
+	}
+
+	return nil
 }
