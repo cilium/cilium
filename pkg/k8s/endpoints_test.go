ec5d2c006ddbbff8cb951578788fed1de7074275
Emily Shepherd <emily@redcoat.dev>
2026-01-25 15:43:40 +0000

    k8s: include namespace in EndpointSliceName
    [ upstream commit 5c146c3f9f08f8aa41a26ae3889458933d52e690 ]
    
    We previously did not include the Namespace in EndpointSliceNames,
    relying on the fact that EndpointSlices that use generateName- are
    unlikely to have name collisions, even across namespaces. While this is
    usually the case, there is no requirement for EndpointSlice managers to
    use generateName, and there are examples of controllers that do not (for
    example the master kubernetes service's EndpointSlice is always called
    "kubernetes").
    
    Including the namespace in EndpointSliceNames guarantees collisions
    cannot occur.
    
    See https://github.com/cilium/cilium/pull/43999 for further discussion
    of this bug.
    
    Signed-off-by: Emily Shepherd <emily@redcoat.dev>

--- pkg/k8s/endpoints_test.go
+++ pkg/k8s/endpoints_test.go
@@ -262,7 +262,7 @@ func Test_parseK8sEPv1(t *testing.T) {
 	}
 	sliceID := EndpointSliceID{
 		ServiceName:       loadbalancer.NewServiceName("bar", "foo"),
-		EndpointSliceName: "foo",
+		EndpointSliceName: "bar/foo",
 	}
 	newEmptyEndpoints := func() *Endpoints {
 		eps := newEndpoints(1)
@@ -577,7 +577,7 @@ func Test_parseK8sEPSlicev1Beta1(t *testing.T) {
 	}
 	sliceID := EndpointSliceID{
 		ServiceName:       loadbalancer.NewServiceName("bar", "quux"),
-		EndpointSliceName: "foo",
+		EndpointSliceName: "bar/foo",
 	}
 
 	newEmptyEndpoints := func() *Endpoints {
@@ -1161,7 +1161,7 @@ func Test_parseK8sEPSlicev1(t *testing.T) {
 	}
 	sliceID := EndpointSliceID{
 		ServiceName:       loadbalancer.NewServiceName("bar", "quux"),
-		EndpointSliceName: "foo",
+		EndpointSliceName: "bar/foo",
 	}
 
 	newEmptyEndpoints := func() *Endpoints {
