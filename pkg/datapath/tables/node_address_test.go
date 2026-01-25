cilium/cilium

commit f8eba9a48fdcc6f0fd1964d6bd8c54815f13109f

datapath: Prefer node IPs for NodePort and Primary
The K8s Node IP address should be preferred when choosing the NodePort and
Primary (BPF masquerade) addresses.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/datapath/tables/node_address.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cilium/hive/job"
 	"github.com/cilium/statedb"
 	"github.com/cilium/statedb/index"
+	"github.com/cilium/stream"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"k8s.io/apimachinery/pkg/util/sets"
@@ -24,6 +25,7 @@ import (
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/rate"
 	"github.com/cilium/cilium/pkg/time"
@@ -206,13 +208,14 @@ type nodeAddressControllerParams struct {
 	Devices         statedb.Table[*Device]
 	NodeAddresses   statedb.RWTable[NodeAddress]
 	AddressScopeMax AddressScopeMax
+	LocalNode       *node.LocalNodeStore
 }
 
 type nodeAddressController struct {
 	nodeAddressControllerParams
 
-	deviceChanges statedb.ChangeIterator[*Device]
-
+	deviceChanges     statedb.ChangeIterator[*Device]
+	k8sIPv4, k8sIPv6  netip.Addr
 	fallbackAddresses fallbackAddresses
 }
 
@@ -248,6 +251,10 @@ func (n *nodeAddressController) register() {
 					return fmt.Errorf("DeleteTracker: %w", err)
 				}
 
+				if node, err := n.LocalNode.Get(ctx); err == nil {
+					n.updateK8sNodeIPs(node)
+				}
+
 				// Do an immediate update to populate the table before it is read from.
 				devices := n.Devices.All(txn)
 				for dev, _, ok := devices.Next(); ok; dev, _, ok = devices.Next() {
@@ -265,8 +272,31 @@ func (n *nodeAddressController) register() {
 
 }
 
+func (n *nodeAddressController) updateK8sNodeIPs(node node.LocalNode) (updated bool) {
+	if ip := node.GetNodeIP(true); ip != nil {
+		if newIP, ok := netip.AddrFromSlice(ip); ok {
+			if newIP != n.k8sIPv6 {
+				n.k8sIPv6 = newIP
+				updated = true
+			}
+		}
+	}
+	if ip := node.GetNodeIP(false); ip != nil {
+		if newIP, ok := netip.AddrFromSlice(ip); ok {
+			if newIP != n.k8sIPv4 {
+				n.k8sIPv4 = newIP
+				updated = true
+			}
+		}
+	}
+	return
+}
+
 func (n *nodeAddressController) run(ctx context.Context, reporter cell.Health) error {
 	defer n.deviceChanges.Close()
+
+	localNodeChanges := stream.ToChannel(ctx, n.LocalNode)
+	n.updateK8sNodeIPs(<-localNodeChanges)
 
 	limiter := rate.NewLimiter(nodeAddressControllerMinInterval, 1)
 	for {
@@ -287,6 +317,22 @@ func (n *nodeAddressController) run(ctx context.Context, reporter cell.Health) e
 		case <-ctx.Done():
 			return nil
 		case <-n.deviceChanges.Watch(n.DB.ReadTxn()):
+		case localNode, ok := <-localNodeChanges:
+			if !ok {
+				localNodeChanges = nil
+				break
+			}
+			if n.updateK8sNodeIPs(localNode) {
+				// Recompute the node addresses as the k8s node IP has changed, which
+				// affects the prioritization.
+				txn := n.DB.WriteTxn(n.NodeAddresses)
+				devices := n.Devices.All(txn)
+				for dev, _, ok := devices.Next(); ok; dev, _, ok = devices.Next() {
+					n.update(txn, n.getAddressesFromDevice(dev), nil, dev.Name)
+					n.updateWildcardDevice(txn, dev, false)
+				}
+				txn.Commit()
+			}
 		}
 		if err := limiter.Wait(ctx); err != nil {
 			return err
@@ -405,10 +451,6 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device) []NodeAddres
 
 	addrs := make([]NodeAddress, 0, len(dev.Addrs))
 
-	// ipv4Found and ipv6Found are set to true when the primary address is picked
-	// (used for the Primary flag)
-	ipv4Found, ipv6Found := false, false
-
 	// The indexes for the first public and private addresses for picking NodePort
 	// addresses.
 	ipv4PublicIndex, ipv4PrivateIndex := -1, -1
@@ -432,12 +474,13 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device) []NodeAddres
 		index := len(addrs)
 
 		isPublic := ip.IsPublicAddr(addr.Addr.AsSlice())
-		primary := false
 		if addr.Addr.Is4() {
-			if !ipv4Found {
-				ipv4Found = true
-				primary = true
+			if addr.Addr.Unmap() == n.k8sIPv4.Unmap() {
+				// Address matches the K8s Node IP. Force this to be picked.
+				ipv4PublicIndex = index
+				ipv4PrivateIndex = index
 			}
+
 			if ipv4PublicIndex < 0 && isPublic {
 				ipv4PublicIndex = index
 			}
@@ -447,9 +490,10 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device) []NodeAddres
 		}
 
 		if addr.Addr.Is6() {
-			if !ipv6Found {
-				ipv6Found = true
-				primary = true
+			if addr.Addr == n.k8sIPv6 {
+				// Address matches the K8s Node IP. Force this to be picked.
+				ipv6PublicIndex = index
+				ipv6PrivateIndex = index
 			}
 
 			if ipv6PublicIndex < 0 && isPublic {
@@ -470,7 +514,6 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device) []NodeAddres
 		addrs = append(addrs,
 			NodeAddress{
 				Addr:       addr.Addr,
-				Primary:    primary,
 				NodePort:   nodePort,
 				DeviceName: dev.Name,
 			})
@@ -483,12 +526,23 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device) []NodeAddres
 		} else if ipv4PublicIndex >= 0 {
 			addrs[ipv4PublicIndex].NodePort = true
 		}
-
 		if ipv6PrivateIndex >= 0 {
 			addrs[ipv6PrivateIndex].NodePort = true
 		} else if ipv6PublicIndex >= 0 {
 			addrs[ipv6PublicIndex].NodePort = true
 		}
+	}
+
+	// Pick the primary address. Prefer public over private.
+	if ipv4PublicIndex >= 0 {
+		addrs[ipv4PublicIndex].Primary = true
+	} else if ipv4PrivateIndex >= 0 {
+		addrs[ipv4PrivateIndex].Primary = true
+	}
+	if ipv6PublicIndex >= 0 {
+		addrs[ipv6PublicIndex].Primary = true
+	} else if ipv6PrivateIndex >= 0 {
+		addrs[ipv6PrivateIndex].Primary = true
 	}
 
 	return addrs

pkg/datapath/tables/node_address_test.go
@@ -23,7 +23,7 @@ import (
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/ip"
-	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 )
 
@@ -57,8 +57,12 @@ func TestNodeAddressConfig(t *testing.T) {
 	}
 }
 
-var ciliumHostIP = net.ParseIP("9.9.9.9")
-var ciliumHostIPLinkScoped = net.ParseIP("9.9.9.8")
+var (
+	testNodeIPv4           = netip.MustParseAddr("172.16.0.1")
+	testNodeIPv6           = netip.MustParseAddr("2222::1")
+	ciliumHostIP           = net.ParseIP("9.9.9.9")
+	ciliumHostIPLinkScoped = net.ParseIP("9.9.9.8")
+)
 
 var nodeAddressTests = []struct {
 	name         string
@@ -245,6 +249,53 @@ var nodeAddressTests = []struct {
 			net.ParseIP("2001:db8::1"),
 		},
 	},
+
+	{
+		name: "node IP preferred",
+		addrs: []DeviceAddress{
+			{
+				Addr:  netip.MustParseAddr("10.0.0.1"),
+				Scope: RT_SCOPE_UNIVERSE,
+			},
+			{
+				Addr:  netip.MustParseAddr("1.1.1.1"),
+				Scope: RT_SCOPE_UNIVERSE,
+			},
+			{
+				Addr:  testNodeIPv4,
+				Scope: RT_SCOPE_UNIVERSE,
+			},
+			{
+				Addr:  netip.MustParseAddr("2001:db8::1"),
+				Scope: RT_SCOPE_UNIVERSE,
+			},
+			{
+				Addr:  testNodeIPv6,
+				Scope: RT_SCOPE_UNIVERSE,
+			},
+		},
+
+		wantAddrs: []net.IP{
+			ciliumHostIP,
+			ciliumHostIPLinkScoped,
+			net.ParseIP("10.0.0.1"),
+			net.ParseIP("1.1.1.1"),
+			net.ParseIP("2001:db8::1"),
+			testNodeIPv4.AsSlice(),
+			testNodeIPv6.AsSlice(),
+		},
+
+		wantPrimary: []net.IP{
+			ciliumHostIP,
+			testNodeIPv4.AsSlice(),
+			testNodeIPv6.AsSlice(),
+		},
+
+		wantNodePort: []net.IP{
+			testNodeIPv4.AsSlice(),
+			testNodeIPv6.AsSlice(),
+		},
+	},
 }
 
 func TestNodeAddress(t *testing.T) {
@@ -252,7 +303,7 @@ func TestNodeAddress(t *testing.T) {
 
 	// Use a shared fixture so that we're dealing with an evolving set of addresses
 	// for the device.
-	db, devices, nodeAddrs := fixture(t, defaults.AddressScopeMax, nil)
+	db, devices, nodeAddrs, _ := fixture(t, defaults.AddressScopeMax, nil)
 
 	_, watch := nodeAddrs.AllWatch(db.ReadTxn())
 	txn := db.WriteTxn(devices)
@@ -339,8 +390,7 @@ func TestNodeAddress(t *testing.T) {
 	txn.Commit()
 	<-watch // wait for propagation
 
-	assert.Equal(t, nodeAddrs.NumObjects(db.ReadTxn()), 0, "expected no NodeAddresses after device deletion")
-
+	assert.Equal(t, 0, nodeAddrs.NumObjects(db.ReadTxn()), "expected no NodeAddresses after device deletion")
 }
 
 // TestNodeAddressHostDevice checks that the for cilium_host the link scope'd
@@ -349,7 +399,7 @@ func TestNodeAddress(t *testing.T) {
 func TestNodeAddressHostDevice(t *testing.T) {
 	t.Parallel()
 
-	db, devices, nodeAddrs := fixture(t, int(RT_SCOPE_SITE), nil)
+	db, devices, nodeAddrs, _ := fixture(t, int(RT_SCOPE_SITE), nil)
 
 	txn := db.WriteTxn(devices)
 	_, watch := nodeAddrs.AllWatch(txn)
@@ -490,7 +540,7 @@ func TestNodeAddressWhitelist(t *testing.T) {
 
 	for _, tt := range nodeAddressWhitelistTests {
 		t.Run(tt.name, func(t *testing.T) {
-			db, devices, nodeAddrs := fixture(t, defaults.AddressScopeMax,
+			db, devices, nodeAddrs, _ := fixture(t, defaults.AddressScopeMax,
 				func(h *hive.Hive) {
 					h.Viper().Set("nodeport-addresses", tt.cidrs)
 				})
@@ -545,7 +595,7 @@ func TestNodeAddressWhitelist(t *testing.T) {
 
 // TestNodeAddressUpdate tests incremental updates to the node addresses.
 func TestNodeAddressUpdate(t *testing.T) {
-	db, devices, nodeAddrs := fixture(t, defaults.AddressScopeMax, func(*hive.Hive) {})
+	db, devices, nodeAddrs, _ := fixture(t, defaults.AddressScopeMax, func(*hive.Hive) {})
 
 	// Insert 10.0.0.1
 	txn := db.WriteTxn(devices)
@@ -644,22 +694,67 @@ func TestNodeAddressUpdate(t *testing.T) {
 	assert.Zero(t, nodeAddrs.NumObjects(db.ReadTxn()))
 }
 
-func fixture(t *testing.T, addressScopeMax int, beforeStart func(*hive.Hive)) (*statedb.DB, statedb.RWTable[*Device], statedb.Table[NodeAddress]) {
+func TestNodeAddressNodeIPChange(t *testing.T) {
+	db, devices, nodeAddrs, localNodeStore := fixture(t, defaults.AddressScopeMax, func(*hive.Hive) {})
+
+	// Insert 10.0.0.1 and the current node IP
+	txn := db.WriteTxn(devices)
+	_, watch := nodeAddrs.AllWatch(txn)
+	devices.Insert(txn, &Device{
+		Index: 1,
+		Name:  "test",
+		Flags: net.FlagUp,
+		Addrs: []DeviceAddress{
+			{Addr: netip.MustParseAddr("10.0.0.1"), Scope: RT_SCOPE_UNIVERSE},
+			{Addr: testNodeIPv4, Scope: RT_SCOPE_UNIVERSE},
+		},
+		Selected: true,
+	})
+	txn.Commit()
+	<-watch // wait for propagation
+
+	iter, watch := nodeAddrs.ListWatch(db.ReadTxn(), NodeAddressNodePortIndex.Query(true))
+	addrs := statedb.Collect(iter)
+	if assert.Len(t, addrs, 1) {
+		assert.Equal(t, testNodeIPv4, addrs[0].Addr)
+		assert.Equal(t, "test", addrs[0].DeviceName)
+	}
+
+	// Make the 10.0.0.1 the new NodeIP.
+	localNodeStore.Update(func(n *node.LocalNode) {
+		n.SetNodeExternalIP(net.ParseIP("10.0.0.1"))
+	})
+	<-watch
+
+	// The new node IP should now be preferred for NodePort.
+	iter = nodeAddrs.List(db.ReadTxn(), NodeAddressNodePortIndex.Query(true))
+	addrs = statedb.Collect(iter)
+	if assert.Len(t, addrs, 1) {
+		assert.Equal(t, "10.0.0.1", addrs[0].Addr.String())
+		assert.Equal(t, "test", addrs[0].DeviceName)
+	}
+}
+
+func fixture(t *testing.T, addressScopeMax int, beforeStart func(*hive.Hive)) (*statedb.DB, statedb.RWTable[*Device], statedb.Table[NodeAddress], *node.LocalNodeStore) {
 	var (
-		db        *statedb.DB
-		devices   statedb.RWTable[*Device]
-		nodeAddrs statedb.Table[NodeAddress]
+		db             *statedb.DB
+		devices        statedb.RWTable[*Device]
+		nodeAddrs      statedb.Table[NodeAddress]
+		localNodeStore *node.LocalNodeStore
 	)
 	h := hive.New(
 		NodeAddressCell,
+		node.LocalNodeStoreCell,
 		cell.Provide(
 			NewDeviceTable,
 			statedb.RWTable[*Device].ToTable,
 		),
-		cell.Invoke(func(db_ *statedb.DB, d statedb.RWTable[*Device], na statedb.Table[NodeAddress]) {
+		cell.Provide(func() node.LocalNodeSynchronizer { return testLocalNodeSync{} }),
+		cell.Invoke(func(db_ *statedb.DB, d statedb.RWTable[*Device], na statedb.Table[NodeAddress], lns *node.LocalNodeStore) {
 			db = db_
 			devices = d
 			nodeAddrs = na
+			localNodeStore = lns
 			db.RegisterTable(d)
 		}),
 
@@ -667,8 +762,7 @@ func fixture(t *testing.T, addressScopeMax int, beforeStart func(*hive.Hive)) (*
 		// in a follow-up PR.
 		cell.Provide(func() *option.DaemonConfig {
 			return &option.DaemonConfig{
-				ConfigPatchMutex: new(lock.RWMutex),
-				AddressScopeMax:  addressScopeMax,
+				AddressScopeMax: addressScopeMax,
 			}
 		}),
 	)
@@ -678,11 +772,28 @@ func fixture(t *testing.T, addressScopeMax int, beforeStart func(*hive.Hive)) (*
 
 	tlog := hivetest.Logger(t)
 	require.NoError(t, h.Start(tlog, context.TODO()), "Start")
+
 	t.Cleanup(func() {
 		assert.NoError(t, h.Stop(tlog, context.TODO()), "Stop")
 	})
-	return db, devices, nodeAddrs
+	return db, devices, nodeAddrs, localNodeStore
 }
+
+type testLocalNodeSync struct {
+}
+
+// InitLocalNode implements node.LocalNodeSynchronizer.
+func (t testLocalNodeSync) InitLocalNode(_ context.Context, n *node.LocalNode) error {
+	n.SetNodeExternalIP(testNodeIPv4.AsSlice())
+	n.SetNodeExternalIP(testNodeIPv6.AsSlice())
+	return nil
+}
+
+// SyncLocalNode implements node.LocalNodeSynchronizer.
+func (t testLocalNodeSync) SyncLocalNode(context.Context, *node.LocalNodeStore) {
+}
+
+var _ node.LocalNodeSynchronizer = testLocalNodeSync{}
 
 // ipStrings converts net.IP to a string. Used to assert equalence without having to deal
 // with e.g. IPv4-mapped IPv6 presentation etc.

commit 0dddd39e496a86fa12268b09dafeef9480f16725

datapath: Fix BPF masquerade IP selection with multiple IPs per interface
When a network device has multiple IP addresses (both public and
private), BPF masquerading was incorrectly selecting the Kubernetes
Node IP even when it was a private address and a public address was
available on the same interface.

The issue was introduced in PR #33629 which added K8s Node IP
prioritization. The code was setting both ipv4PublicIndex and
ipv4PrivateIndex to the K8s Node IP index, effectively forcing
it to be selected regardless of public/private status. This broke
the documented "prefer public over private" logic for Primary
address selection used by BPF masquerading.

The fix ensures that K8s Node IP prioritization only applies within
its own category (public or private):
- If K8s Node IP is public, it takes precedence over other public IPs
- If K8s Node IP is private, it takes precedence over other private IPs
- But public IPs still take precedence over private IPs for masquerading

This restores the correct behavior where egress traffic is masqueraded
using the public IP address when available, which is required for
proper routing in environments with both public and private IPs on
the same interface.

Fixes: #41866
Signed-off-by: Jonathan Siegel <248302+usiegj00@users.noreply.github.com>

pkg/datapath/tables/node_address.go
@@ -493,9 +493,17 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device, k8sIPv4, k8s
 		isPublic := ip.IsPublicAddr(addr.Addr.AsSlice())
 		if addr.Addr.Is4() {
 			if addr.Addr.Unmap() == k8sIPv4.Unmap() {
-				// Address matches the K8s Node IP. Force this to be picked.
-				ipv4PublicIndex = index
-				ipv4PrivateIndex = index
+				// Address matches the K8s Node IP. Prioritize it within its
+				// category (public or private) for NodePort address selection.
+				// We don't force it to both categories, as that would break
+				// the "prefer public over private" logic for Primary address
+				// selection used by BPF masquerading.
+				// See: https://github.com/cilium/cilium/issues/41866
+				if isPublic {
+					ipv4PublicIndex = index
+				} else {
+					ipv4PrivateIndex = index
+				}
 			}
 			if ipv4PublicIndex < 0 && isPublic {
 				ipv4PublicIndex = index
@@ -507,9 +515,17 @@ func (n *nodeAddressController) getAddressesFromDevice(dev *Device, k8sIPv4, k8s
 
 		if addr.Addr.Is6() {
 			if addr.Addr == k8sIPv6 {
-				// Address matches the K8s Node IP. Force this to be picked.
-				ipv6PublicIndex = index
-				ipv6PrivateIndex = index
+				// Address matches the K8s Node IP. Prioritize it within its
+				// category (public or private) for NodePort address selection.
+				// We don't force it to both categories, as that would break
+				// the "prefer public over private" logic for Primary address
+				// selection used by BPF masquerading.
+				// See: https://github.com/cilium/cilium/issues/41866
+				if isPublic {
+					ipv6PublicIndex = index
+				} else {
+					ipv6PrivateIndex = index
+				}
 			}
 			if ipv6PublicIndex < 0 && isPublic {
 				ipv6PublicIndex = index

pkg/datapath/tables/node_address_test.go
@@ -215,26 +215,36 @@ var nodeAddressTests = []struct {
 	},
 
 	{
-		name: "node IP preferred",
+		// Test that K8s Node IP is prioritized within its category (public/private)
+		// but doesn't override the public/private preference itself.
+		// - testNodeIPv4 (172.16.0.1) is private, should be prioritized among private IPs
+		// - testNodeIPv6 (2222::1) is public, should be prioritized among public IPs
+		name: "k8s node IP prioritized within category",
 		addrs: []DeviceAddress{
+			// IPv4: multiple private IPs + one public
 			{
-				Addr:  netip.MustParseAddr("10.0.0.1"),
+				Addr:  netip.MustParseAddr("10.0.0.1"), // private, but not K8s IP
 				Scope: RT_SCOPE_UNIVERSE,
 			},
 			{
-				Addr:  netip.MustParseAddr("1.1.1.1"),
+				Addr:  netip.MustParseAddr("1.1.1.1"), // public
 				Scope: RT_SCOPE_UNIVERSE,
 			},
 			{
-				Addr:  testNodeIPv4,
+				Addr:  testNodeIPv4, // private K8s Node IP (172.16.0.1)
+				Scope: RT_SCOPE_UNIVERSE,
+			},
+			// IPv6: multiple public IPs + one private
+			{
+				Addr:  netip.MustParseAddr("2001:db8::1"), // private (documentation prefix)
 				Scope: RT_SCOPE_UNIVERSE,
 			},
 			{
-				Addr:  netip.MustParseAddr("2001:db8::1"),
+				Addr:  netip.MustParseAddr("2600:beef::1"), // public, but not K8s IP
 				Scope: RT_SCOPE_UNIVERSE,
 			},
 			{
-				Addr:  testNodeIPv6,
+				Addr:  testNodeIPv6, // public K8s Node IP (2222::1)
 				Scope: RT_SCOPE_UNIVERSE,
 			},
 		},
@@ -244,20 +254,23 @@ var nodeAddressTests = []struct {
 			ciliumHostIPLinkScoped,
 			netip.MustParseAddr("10.0.0.1"),
 			netip.MustParseAddr("1.1.1.1"),
-			netip.MustParseAddr("2001:db8::1"),
 			testNodeIPv4,
+			netip.MustParseAddr("2001:db8::1"),
+			netip.MustParseAddr("2600:beef::1"),
 			testNodeIPv6,
 		},
 
+		// Primary prefers public; among public IPs, K8s Node IP is prioritized
 		wantPrimary: []netip.Addr{
 			ciliumHostIP,
-			testNodeIPv4,
-			testNodeIPv6,
+			netip.MustParseAddr("1.1.1.1"), // IPv4: only public IP
+			testNodeIPv6,                   // IPv6: K8s Node IP prioritized among public
 		},
 
+		// NodePort prefers private; among private IPs, K8s Node IP is prioritized
 		wantNodePort: []netip.Addr{
-			testNodeIPv4,
-			testNodeIPv6,
+			testNodeIPv4,                       // IPv4: K8s Node IP prioritized among private
+			netip.MustParseAddr("2001:db8::1"), // IPv6: only private IP
 		},
 	},
 }