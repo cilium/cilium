703677862549bdaa832a80b8ef934ee54f37c40a
Alex Melhem <alex.melhem@datadoghq.com>
2026-01-26 11:55:35 +0100

    pkg/nodediscovery: Fix global static IP tags with flags
    In a previous PR #43524 setting static IP tags was made global to all
    IPAM modes. This makes sure the behavior also applies when setting the
    tags through the IPAM flag.
    
    Signed-off-by: Alex Melhem <alex.melhem@datadoghq.com>

--- pkg/nodediscovery/nodediscovery.go
+++ pkg/nodediscovery/nodediscovery.go
@@ -388,6 +388,7 @@ func (n *NodeDiscovery) mutateNodeResource(ctx context.Context, nodeResource *ci
 
 	nodeResource.Spec.BootID = ln.BootID
 
+	nodeResource.Spec.IPAM.StaticIPTags = n.config.IPAMStaticIPTags
 	if c := n.cniConfigManager.GetCustomNetConf(); c != nil {
 		if len(c.IPAM.StaticIPTags) > 0 {
 			nodeResource.Spec.IPAM.StaticIPTags = c.IPAM.StaticIPTags
@@ -433,7 +434,6 @@ func (n *NodeDiscovery) mutateNodeResource(ctx context.Context, nodeResource *ci
 		nodeResource.Spec.IPAM.MinAllocate = n.config.IPAMMinAllocate
 		nodeResource.Spec.IPAM.PreAllocate = n.config.IPAMPreAllocate
 		nodeResource.Spec.IPAM.MaxAllocate = n.config.IPAMMaxAllocate
-		nodeResource.Spec.IPAM.StaticIPTags = n.config.IPAMStaticIPTags
 
 		if c := n.cniConfigManager.GetCustomNetConf(); c != nil {
 			if c.IPAM.MinAllocate != 0 {
@@ -505,7 +505,6 @@ func (n *NodeDiscovery) mutateNodeResource(ctx context.Context, nodeResource *ci
 		nodeResource.Spec.IPAM.MinAllocate = n.config.IPAMMinAllocate
 		nodeResource.Spec.IPAM.PreAllocate = n.config.IPAMPreAllocate
 		nodeResource.Spec.IPAM.MaxAllocate = n.config.IPAMMaxAllocate
-		nodeResource.Spec.IPAM.StaticIPTags = n.config.IPAMStaticIPTags
 		nodeResource.Spec.Azure.InterfaceName = n.config.AzureInterfaceName
 
 		if c := n.cniConfigManager.GetCustomNetConf(); c != nil {
@@ -557,7 +556,6 @@ func (n *NodeDiscovery) mutateNodeResource(ctx context.Context, nodeResource *ci
 		nodeResource.Spec.IPAM.PreAllocate = n.config.IPAMPreAllocate
 		nodeResource.Spec.IPAM.MinAllocate = n.config.IPAMMinAllocate
 		nodeResource.Spec.IPAM.MaxAllocate = n.config.IPAMMaxAllocate
-		nodeResource.Spec.IPAM.StaticIPTags = n.config.IPAMStaticIPTags
 		nodeResource.Spec.AlibabaCloud.VSwitches = n.config.AlibabaCloudVSwitches
 		nodeResource.Spec.AlibabaCloud.VSwitchTags = n.config.AlibabaCloudVSwitchTags
 		nodeResource.Spec.AlibabaCloud.SecurityGroups = n.config.AlibabaCloudSecurityGroups
