cilium/cilium

commit cf367c56a84e0827483e719497dffd60b4b816db

datapath: iptables: remove matchFromDNSProxy exception
commit 94f6553f5b79 ("dnsproxy: Use original source address in connections
to dns servers") switched the DNS proxy from using `MagicMarkIdentity` to
the generic `MagicMarkEgress` mark. And this value is already covered by
the `matchFromProxy` rule.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

pkg/datapath/iptables/iptables.go
@@ -1605,7 +1605,6 @@ func (m *Manager) installHostTrafficMarkRule(prog runnable) error {
 	matchOverlay := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkOverlay, linux_defaults.MagicMarkHostMask)
 	matchFromProxy := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIsProxy, linux_defaults.MagicMarkProxyMask)
 	matchFromProxyEPID := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIsProxyEPID, linux_defaults.MagicMarkProxyMask)
-	matchFromDNSProxy := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIdentity, linux_defaults.MagicMarkHostMask)
 	markAsFromHost := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkHost, linux_defaults.MagicMarkHostMask)
 
 	return prog.runProg([]string{
@@ -1616,7 +1615,6 @@ func (m *Manager) installHostTrafficMarkRule(prog runnable) error {
 		"-m", "mark", "!", "--mark", matchOverlay, // Don't match Cilium's overlay traffic
 		"-m", "mark", "!", "--mark", matchFromProxy, // Don't match proxy traffic
 		"-m", "mark", "!", "--mark", matchFromProxyEPID, // Don't match proxy traffic
-		"-m", "mark", "!", "--mark", matchFromDNSProxy, // Don't match DNS proxy egress traffic
 		"-m", "comment", "--comment", "cilium: host->any mark as from host",
 		"-j", "MARK", "--set-xmark", markAsFromHost})
 }

commit 5ba0017b8e7960e510845bfb62233cb5e8712da2

bpf: Fix regression from removing iptables matchFromDNSProxy rule
Revert cf367c56a84e ("datapath: iptables: remove matchFromDNSProxy exception")
and rework it into a health check IPIP related rule to make it more clear.

What can be seen is that cf367c56a84e caused a loop where the BPF code did
not match MARK_MAGIC_HEALTH_IPIP_DONE anymore causing traffic for IPIP to
be encapsulated over and over again:

  # tcpdump -i cilium_ipip4 -n
  [...]
  16:02:58.351159 IP 172.18.0.4.43285 > 100.64.0.0.81: Flags [S], seq 1754297322, win 42340, options [mss 1460,sackOK,TS val 3216258162 ecr 0,nop,wscale 8], length 0
  16:02:58.351171 IP 172.18.0.4 > 172.18.0.2: IP 172.18.0.4.43285 > 100.64.0.0.81: Flags [S], seq 1754297322, win 42340, options [mss 1460,sackOK,TS val 3216258162 ecr 0,nop,wscale 8], length 0
  16:02:58.351174 IP 172.18.0.4 > 172.18.0.2: IP 172.18.0.4 > 172.18.0.2: IP 172.18.0.4.43285 > 100.64.0.0.81: Flags [S], seq 1754297322, win 42340, options [mss 1460,sackOK,TS val 3216258162 ecr 0,nop,wscale 8], length 0
  16:02:58.351178 IP 172.18.0.4 > 172.18.0.2: IP 172.18.0.4 > 172.18.0.2: IP 172.18.0.4 > 172.18.0.2: IP 172.18.0.4.43285 > 100.64.0.0.81: Flags [S], seq 1754297322, win 42340, options [mss 1460,sackOK,TS val 3216258162 ecr 0,nop,wscale 8], length 0
  [...]

Fixes: cf367c56a84e ("datapath: iptables: remove matchFromDNSProxy exception")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/lib/nodeport_egress.h
@@ -594,6 +594,18 @@ lb_handle_health(struct __ctx_buff *ctx __maybe_unused, __be16 proto)
 	    MARK_MAGIC_HEALTH_IPIP_DONE)
 		return CTX_ACT_OK;
 
+	/* See installHostTrafficMarkRule where we install
+	 * an iptables match clause (not matchIPIPHealthCheck
+	 * in HostTrafficMarkRule) to avoid marking host-
+	 * originating traffic. The IPIP encap goes through
+	 * the stack twice, so once at the phys device we must
+	 * skip setting 0xf00 to 0xc00 otherwise we bypass
+	 * above MARK_MAGIC_HEALTH_IPIP_DONE check and keep
+	 * looping traffic to the IPIP device.
+	 */
+	build_bug_on((MARK_MAGIC_HEALTH_IPIP_DONE |
+		      MARK_MAGIC_HEALTH) != 0xf00);
+
 	switch (proto) {
 #if defined(ENABLE_IPV4) && DSR_ENCAP_MODE == DSR_ENCAP_IPIP
 	case bpf_htons(ETH_P_IP): {

pkg/datapath/iptables/iptables.go
@@ -1700,6 +1700,7 @@ func (m *Manager) installHostTrafficMarkRule(prog runnable) error {
 	matchOverlay := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkOverlay, linux_defaults.MagicMarkHostMask)
 	matchFromProxy := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIsProxy, linux_defaults.MagicMarkProxyMask)
 	matchFromProxyEPID := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIsProxyEPID, linux_defaults.MagicMarkProxyMask)
+	matchIPIPHealthCheck := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIPIPHealthCheck, linux_defaults.MagicMarkHostMask)
 	markAsFromHost := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkHost, linux_defaults.MagicMarkHostMask)
 
 	return prog.runProg([]string{
@@ -1710,6 +1711,7 @@ func (m *Manager) installHostTrafficMarkRule(prog runnable) error {
 		"-m", "mark", "!", "--mark", matchOverlay, // Don't match Cilium's overlay traffic
 		"-m", "mark", "!", "--mark", matchFromProxy, // Don't match proxy traffic
 		"-m", "mark", "!", "--mark", matchFromProxyEPID, // Don't match proxy traffic
+		"-m", "mark", "!", "--mark", matchIPIPHealthCheck, // Don't match IPIP health check traffic
 		"-m", "comment", "--comment", "cilium: host->any mark as from host",
 		"-j", "MARK", "--set-xmark", markAsFromHost})
 }

pkg/datapath/linux/linux_defaults/mark.go
@@ -120,6 +120,18 @@ const (
 	// interfering with that bit.
 	MagicMarkDecryptedOverlay = 0x1D00
 
+	// MagicMarkIPIPHealthCheck indicates to the datapath that the packet
+	// came from the host namespace where a socket set MARK_MAGIC_HEALTH
+	// when connecting to a service.
+	//
+	// That packet is then pushed to an IPIP device. So it ends up twice
+	// on the phys device, and going through iptables.
+	//
+	// The second time marked as MARK_MAGIC_HEALTH (0x0d00) |
+	// MARK_MAGIC_HEALTH_IPIP_DONE (0x0300) resulting in 0x0f00.
+	// See also lb_handle_health() in the BPF code.
+	MagicMarkIPIPHealthCheck = 0x0F00
+
 	// MagicMarkEncrypt is the packet mark to use to indicate datapath
 	// needs to encrypt a packet.
 	MagicMarkEncrypt = 0x0E00