cbe64eb965c3e1ea4e3e85d5ce2dbaf4ed133524
Tobias Klauser <tobias@cilium.io>
2026-01-28 10:35:47 +0100

    byteorder: test NetIPAddrToHost32
    Add test coverage for the NetIPAddrToHost32 helper added in commit
    5da588275456 ("pod2pod strict mode for WireGuard").
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- pkg/byteorder/byteorder_test.go
+++ pkg/byteorder/byteorder_test.go
@@ -5,6 +5,7 @@ package byteorder
 
 import (
 	"net"
+	"net/netip"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -28,5 +29,15 @@ func TestNetIPv4ToHost32(t *testing.T) {
 	} else {
 		require.Equal(t, uint32(0x5b810b0a), NetIPv4ToHost32(net.ParseIP("10.11.129.91")))
 		require.Equal(t, uint32(0xd68a0b0a), NetIPv4ToHost32(net.ParseIP("10.11.138.214")))
+	}
+}
+
+func TestNetiIPAddrToHost32(t *testing.T) {
+	if cpu.IsBigEndian {
+		require.Equal(t, uint32(0x0a0b815b), NetIPAddrToHost32(netip.MustParseAddr("10.11.129.91")))
+		require.Equal(t, uint32(0x0a0b8ad6), NetIPAddrToHost32(netip.MustParseAddr("10.11.138.214")))
+	} else {
+		require.Equal(t, uint32(0x5b810b0a), NetIPAddrToHost32(netip.MustParseAddr("10.11.129.91")))
+		require.Equal(t, uint32(0xd68a0b0a), NetIPAddrToHost32(netip.MustParseAddr("10.11.138.214")))
 	}
 }
