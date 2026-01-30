e0c14da3f19e3d9bd80f9b5074c0262d58b281fe
Fabio Falzoi <fabio.falzoi@isovalent.com>
2026-01-30 18:42:15 +0100

    network-driver/ipam: Add multi-pool Resource IPAM operator cell
    Add the operator cell that manages the multi-pool controller for
    DRA resource IPAM. The implementation works in the same way of the pod
    IPAM counterpart, but it is specialized for DRA resources and isolated
    from the existing one.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- operator/cmd/root.go
+++ operator/cmd/root.go
@@ -46,6 +46,7 @@ import (
 	"github.com/cilium/cilium/operator/pkg/kvstore/locksweeper"
 	"github.com/cilium/cilium/operator/pkg/kvstore/nodesgc"
 	"github.com/cilium/cilium/operator/pkg/lbipam"
+	networkdriveripam "github.com/cilium/cilium/operator/pkg/networkdriver/ipam"
 	"github.com/cilium/cilium/operator/pkg/networkpolicy"
 	"github.com/cilium/cilium/operator/pkg/nodeipam"
 	"github.com/cilium/cilium/operator/pkg/secretsync"
@@ -340,6 +341,9 @@ var (
 		// Provides the ztunnel daemonset controller if ztunnel encryption
 		// is specified.
 		ztunnel.Cell,
+
+		// Provide Multi Pool IPAM for DRA resources managed by the Cilium Network Driver
+		networkdriveripam.Cell,
 	}
 
 	binaryName = filepath.Base(os.Args[0])
