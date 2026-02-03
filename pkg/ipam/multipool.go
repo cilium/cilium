cilium/cilium

commit 788d1aa82b2db6d3c0994ff431ce2732e4b51d8b

ipam, metrics: Add new capacity metric
This commit implements a new metric which tracks the total IPAM
capacity, which to be clear is not the current capacity, but rather size
of the IPAM pool.

Users can now track the currently allocated count of IPs across all IPAM
modes by taking this metric and subtracting by number of IPs allocated.

Previously, this information was only visible from `cilium status`.

In Prometheus, the query would be:

```
cilium_ipam_capacity{family=~"ipv4"} - cilium_ip_addresses{family=~"ipv4"}
```

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipam/multipool.go
@@ -718,6 +718,24 @@ func (c *multiPoolAllocator) Dump() (map[string]string, string) {
 	return c.manager.dump(c.family)
 }
 
+func (c *multiPoolAllocator) Capacity() uint64 {
+	var capacity uint64
+	for _, pool := range c.manager.pools {
+		var p *podCIDRPool
+		switch c.family {
+		case IPv4:
+			p = pool.v4
+		case IPv6:
+			p = pool.v6
+		}
+		if p == nil {
+			continue
+		}
+		capacity += uint64(p.capacity())
+	}
+	return uint64(capacity)
+}
+
 func (c *multiPoolAllocator) RestoreFinished() {
 	c.manager.restoreFinished(c.family)
 }

pkg/ipam/allocator.go
@@ -94,6 +94,7 @@ func (ipam *IPAM) allocateIP(ip net.IP, owner string, pool Pool, needSyncUpstrea
 				return
 			}
 		}
+		metrics.IPAMCapacity.WithLabelValues(string(family)).Set(float64(ipam.IPv4Allocator.Capacity()))
 	} else {
 		family = IPv6
 		if ipam.IPv6Allocator == nil {
@@ -110,6 +111,7 @@ func (ipam *IPAM) allocateIP(ip net.IP, owner string, pool Pool, needSyncUpstrea
 				return
 			}
 		}
+		metrics.IPAMCapacity.WithLabelValues(string(family)).Set(float64(ipam.IPv6Allocator.Capacity()))
 	}
 
 	// If the allocator did not populate the pool, we assume it does not
@@ -134,8 +136,10 @@ func (ipam *IPAM) allocateNextFamily(family Family, owner string, pool Pool, nee
 	switch family {
 	case IPv6:
 		allocator = ipam.IPv6Allocator
+		metrics.IPAMCapacity.WithLabelValues(string(family)).Set(float64(ipam.IPv6Allocator.Capacity()))
 	case IPv4:
 		allocator = ipam.IPv4Allocator
+		metrics.IPAMCapacity.WithLabelValues(string(family)).Set(float64(ipam.IPv4Allocator.Capacity()))
 
 	default:
 		err = fmt.Errorf("unknown address \"%s\" family requested", family)
@@ -276,6 +280,7 @@ func (ipam *IPAM) releaseIPLocked(ip net.IP, pool Pool) error {
 		}
 
 		ipam.IPv4Allocator.Release(ip, pool)
+		metrics.IPAMCapacity.WithLabelValues(string(family)).Set(float64(ipam.IPv4Allocator.Capacity()))
 	} else {
 		family = IPv6
 		if ipam.IPv6Allocator == nil {
@@ -283,6 +288,7 @@ func (ipam *IPAM) releaseIPLocked(ip net.IP, pool Pool) error {
 		}
 
 		ipam.IPv6Allocator.Release(ip, pool)
+		metrics.IPAMCapacity.WithLabelValues(string(family)).Set(float64(ipam.IPv6Allocator.Capacity()))
 	}
 
 	owner := ipam.releaseIPOwner(ip, pool)
@@ -292,6 +298,7 @@ func (ipam *IPAM) releaseIPLocked(ip net.IP, pool Pool) error {
 	}).Debugf("Released IP")
 	delete(ipam.expirationTimers, ip.String())
 
+	metrics.IpamEvent.WithLabelValues(metricRelease, string(family)).Inc()
 	metrics.IpamEvent.WithLabelValues(metricRelease, string(family)).Inc()
 	return nil
 }

pkg/ipam/clusterpool.go
@@ -419,6 +419,11 @@ func (c *clusterPoolAllocator) Dump() (map[string]string, string) {
 	return ipToOwner, fmt.Sprintf("%d/%d allocated from %d pod CIDRs", usedIPs, availableIPs, numPodCIDRs)
 }
 
+func (c *clusterPoolAllocator) Capacity() uint64 {
+	_, _, availableIPs, _, _ := c.pool.dump()
+	return uint64(availableIPs)
+}
+
 func (c *clusterPoolAllocator) RestoreFinished() {
 	sharedCRDWatcher.restoreFinished()
 }

pkg/ipam/crd.go
@@ -890,6 +890,12 @@ func (a *crdAllocator) Dump() (map[string]string, string) {
 	return allocs, status
 }
 
+func (a *crdAllocator) Capacity() uint64 {
+	a.mutex.RLock()
+	defer a.mutex.RUnlock()
+	return uint64(a.store.totalPoolSize(a.family))
+}
+
 // RestoreFinished marks the status of restoration as done
 func (a *crdAllocator) RestoreFinished() {
 	a.store.restoreCloseOnce.Do(func() {

pkg/ipam/hostscope.go
@@ -89,5 +89,9 @@ func (h *hostScopeAllocator) Dump() (map[string]string, string) {
 	return alloc, status
 }
 
+func (h *hostScopeAllocator) Capacity() uint64 {
+	return ip.CountIPsInCIDR(h.allocCIDR).Uint64()
+}
+
 // RestoreFinished marks the status of restoration as done
 func (h *hostScopeAllocator) RestoreFinished() {}

pkg/ipam/ipam_test.go
@@ -120,6 +120,10 @@ func (f fakePoolAllocator) Dump() (map[string]string, string) {
 	return result, fmt.Sprintf("%d pools", len(f.pools))
 }
 
+func (f fakePoolAllocator) Capacity() uint64 {
+	return uint64(0)
+}
+
 func (f fakePoolAllocator) RestoreFinished() {}
 
 func (s *IPAMSuite) TestLock(c *C) {

pkg/ipam/noop_allocator.go
@@ -39,5 +39,9 @@ func (n *noOpAllocator) Dump() (map[string]string, string) {
 	return nil, "delegated to plugin"
 }
 
+func (n *noOpAllocator) Capacity() uint64 {
+	return uint64(0)
+}
+
 func (n *noOpAllocator) RestoreFinished() {
 }

pkg/ipam/pool.go
@@ -168,6 +168,22 @@ func (p *podCIDRPool) dump() (ipToOwner map[string]string, usedIPs, freeIPs, num
 	return
 }
 
+func (p *podCIDRPool) capacity() (freeIPs int) {
+	// TODO(gandro): Use the Snapshot interface to avoid locking during dump
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	for _, ipAllocator := range p.ipAllocators {
+		cidrNet := ipAllocator.CIDR()
+		cidrStr := cidrNet.String()
+		if _, removed := p.removed[cidrStr]; !removed {
+			freeIPs += ipAllocator.Free()
+		}
+	}
+
+	return
+}
+
 func (p *podCIDRPool) calculateIPsLocked() (totalUsed, totalFree int) {
 	// Compute the total number of free and used IPs for all non-released pod
 	// CIDRs.

commit b7da4d3b4be64853e386a2e75400ef7aaeeb5c08

ipam: Fix concurrent map access to multipool map
For some reason, I forgot to acquire the mutex when accessing the
multipool map in 788d1aa82b2d ("ipam, metrics: Add new capacity
metric"). Fix it now to prevent the concurrent map access panic as seen
in https://github.com/cilium/cilium/issues/44107.

Fixes: 788d1aa82b2d ("ipam, metrics: Add new capacity
metric")
Fixes: https://github.com/cilium/cilium/issues/44107

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipam/multipool.go
@@ -116,6 +116,9 @@ func (c *multiPoolAllocator) Dump() (map[Pool]map[string]string, string) {
 }
 
 func (c *multiPoolAllocator) Capacity() uint64 {
+	c.manager.mutex.Lock()
+	defer c.manager.mutex.Unlock()
+
 	var capacity uint64
 	for _, pool := range c.manager.pools {
 		var p *cidrPool