61c895315831f06d254dab0f0efc07aa9f4bd4b9
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2026-02-03 16:46:50 +0000

    pkg/metrics: update tests to cover version metrics
    This commit updates existing logic that tests the handling of Kubernetes
    version in the operator, and also adds the same tests for the Linux
    kernel version in the agent.
    
    Signed-off-by: Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>

--- pkg/metrics/features/operator/metrics_test.go
+++ pkg/metrics/features/operator/metrics_test.go
@@ -216,18 +216,28 @@ func TestUpdateNodeIPAMEnabled(t *testing.T) {
 
 func TestUpdateKubernetesVersion(t *testing.T) {
 	tests := []struct {
-		name     string
-		expected float64
+		name           string
+		withEnvVersion bool
+		enabled        bool
+		expected       float64
 	}{
 		{
-			name:     "Kubernetes version metric",
-			expected: 1,
+			name:           "Kubernetes version metric withEnvVersion=true",
+			withEnvVersion: true,
+			enabled:        true,
+			expected:       1,
+		},
+		{
+			name:           "Kubernetes version metric withEnvVersion=false",
+			withEnvVersion: false,
+			enabled:        false,
+			expected:       0,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			metrics := NewMetrics(true, true)
+			metrics := NewMetrics(true, tt.withEnvVersion)
 			config := &option.OperatorConfig{}
 
 			params := mockFeaturesParams{
@@ -236,11 +246,12 @@ func TestUpdateKubernetesVersion(t *testing.T) {
 
 			metrics.update(params, config)
 
-			counter, err := metrics.CPKubernetesVersion.GetMetricWithLabelValues("1.31.0")
+			counter, err := metrics.CPKubernetesVersion.GetMetricWithLabelValues(params.K8sVersion())
 			assert.NoError(t, err)
+			assert.Equal(t, tt.enabled, counter.IsEnabled())
 
 			counterValue := counter.Get()
-			assert.Equal(t, float64(1), counterValue, "Expected version %s to be incremented", "1.31.0")
+			assert.Equal(t, tt.expected, counterValue, "Expected version %s to be %f", params.K8sVersion(), tt.expected)
 		})
 	}
 }
