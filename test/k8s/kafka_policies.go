674aea4655b6f0eee66974763de76f2ef65d8c85
Shivam Pal <shivampal60076@gmail.com>
2026-02-05 00:05:47 +0530

    test: remove deprecated Kafka policy test file
    Signed-off-by: Shivam Pal <shivampal60076@gmail.com>

--- test/k8s/kafka_policies.go
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-package k8sTest
-
-// Deprecated: Kafka policy tests removed as Kafka support is deprecated.
-// This file is kept for historical reference and should not contain any Ginkgo tests.
-
