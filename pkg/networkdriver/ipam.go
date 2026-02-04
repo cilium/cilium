29e40c13eba1b4ac955db08ec71801353ab63e34
Fabio Falzoi <fabio.falzoi@isovalent.com>
2026-02-04 17:14:36 +0100

    network-driver/ipam: Add support for NodeSelector in IP pools
    Add a NodeSelector in the CiliumResourceIPPool object. The NodeSelector
    allows to restrict the usage of the pool only to the matching set of
    nodes. An empty NodeSelector matches all nodes.
    
    Moreover, if a ResourceClaim does not reference any pool, the Network
    Driver automatically looks for a suitable CiliumResourceIPPool to use
    (that is, a pool with a selector matching the current node) and assigns
    an IP address from that pool. Having more than one pool with a non-nil
    NodeSelector matching the same node is considered an error.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/networkdriver/ipam.go
+++ pkg/networkdriver/ipam.go
@@ -6,19 +6,31 @@ package networkdriver
 import (
 	"fmt"
 	"log/slog"
+	"strings"
 
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
+	"github.com/cilium/statedb"
+	"github.com/cilium/statedb/index"
 	"github.com/spf13/pflag"
+	"k8s.io/apimachinery/pkg/util/duration"
 
-	"github.com/cilium/cilium/daemon/k8s"
+	daemonK8s "github.com/cilium/cilium/daemon/k8s"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/ipam"
 	ipamtypes "github.com/cilium/cilium/pkg/ipam/types"
+	"github.com/cilium/cilium/pkg/k8s"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/client"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/labels"
+	slimv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	"github.com/cilium/cilium/pkg/k8s/synced"
+	"github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/promise"
 	"github.com/cilium/cilium/pkg/time"
 )
 
@@ -48,7 +60,10 @@ const (
 var resourceIPAM = cell.Group(
 	cell.Config(defaultIPAMConfig),
 
-	cell.ProvidePrivate(newMultiPoolManager),
+	cell.ProvidePrivate(
+		newResourceIPPoolTableAndReflector,
+		newMultiPoolManager,
+	),
 )
 
 type IPAMConfig struct {
@@ -65,7 +80,7 @@ func (cfg IPAMConfig) Flags(flags *pflag.FlagSet) {
 func newMultiPoolManager(
 	Logger *slog.Logger,
 	DaemonCfg *option.DaemonConfig,
-	LocalNode k8s.LocalCiliumNodeResource,
+	LocalNode daemonK8s.LocalCiliumNodeResource,
 	ClientSet k8sClient.Clientset,
 	JobGroup job.Group,
 	Cfg IPAMConfig,
@@ -95,4 +110,141 @@ func newMultiPoolManager(
 			return &cn.Spec.IPAM.ResourcePools
 		},
 	})
+}
+
+type resourceIPPool struct {
+	Name         string                 `json:"name" yaml:"name"`
+	IPv4         *v2alpha1.IPv4PoolSpec `json:"ipv4" yaml:"ipv4"`
+	IPv6         *v2alpha1.IPv6PoolSpec `json:"ipv6" yaml:"ipv6"`
+	NodeSelector labels.Selector        `json:"nodeSelector,omitempty" yaml:"nodeSelector,omitempty"`
+
+	// UpdatedAt is the time when [resourceIPPool] was last updated, e.g. it
+	// shows when last change was received from the api-server.
+	UpdatedAt time.Time `json:"updatedAt" yaml:"updatedAt"`
+}
+
+func (p resourceIPPool) GetName() string      { return p.Name }
+func (p resourceIPPool) GetNamespace() string { return "" }
+
+func (p resourceIPPool) TableHeader() []string {
+	return []string{
+		"Name",
+		"IPv4 CIDRs",
+		"IPv4 MaskSize",
+		"IPv6 CIDRs",
+		"IPv6 MaskSize",
+		"Node Selector",
+		"Age",
+	}
+}
+
+func (p resourceIPPool) TableRow() []string {
+	showCIDRs := func(cidrs []v2alpha1.PoolCIDR) string {
+		var b strings.Builder
+		for i := 0; i < len(cidrs); i++ {
+			b.WriteString(string(cidrs[i]))
+			if i != len(cidrs)-1 {
+				b.WriteString(", ")
+			}
+		}
+		return b.String()
+	}
+
+	var (
+		v4CIDRs, v6CIDRs   string
+		v4MaskSz, v6MaskSz string
+	)
+	if p.IPv4 != nil {
+		v4CIDRs = showCIDRs(p.IPv4.CIDRs)
+	}
+	if p.IPv6 != nil {
+		v6CIDRs = showCIDRs(p.IPv6.CIDRs)
+	}
+
+	return []string{
+		p.Name,
+		v4CIDRs, v4MaskSz,
+		v6CIDRs, v6MaskSz,
+		p.NodeSelector.String(),
+		duration.HumanDuration(time.Since(p.UpdatedAt)),
+	}
+}
+
+const ResourceIPPoolTableName = "k8s-cilium-resource-ip-pools"
+
+var (
+	ResourceIPPoolIndex = statedb.Index[resourceIPPool, string]{
+		Name: "name",
+		FromObject: func(obj resourceIPPool) index.KeySet {
+			return index.NewKeySet(index.String(obj.GetName()))
+		},
+		FromKey: index.String,
+		FromString: func(key string) (index.Key, error) {
+			return index.String(key), nil
+		},
+		Unique: true,
+	}
+
+	ResourceIPPoolByName = ResourceIPPoolIndex.Query
+)
+
+func newResourceIPPoolTableAndReflector(jg job.Group, db *statedb.DB, cs client.Clientset, crdSync promise.Promise[synced.CRDSync], daemonCfg *option.DaemonConfig) (statedb.Table[resourceIPPool], error) {
+	if !daemonCfg.EnableCiliumNetworkDriver {
+		return nil, nil
+	}
+
+	resourceIPPools, err := NewResourceIPPoolTable(db)
+	if err != nil {
+		return nil, err
+	}
+
+	if !cs.IsEnabled() {
+		return resourceIPPools, nil
+	}
+
+	cfg := resourceIPPoolReflectorConfig(cs, crdSync, resourceIPPools)
+	err = k8s.RegisterReflector(jg, db, cfg)
+	return resourceIPPools, err
+}
+
+func NewResourceIPPoolTable(db *statedb.DB) (statedb.RWTable[resourceIPPool], error) {
+	return statedb.NewTable(
+		db,
+		ResourceIPPoolTableName,
+		ResourceIPPoolIndex,
+	)
+}
+
+func resourceIPPoolReflectorConfig(cs client.Clientset, crdSync promise.Promise[synced.CRDSync], pools statedb.RWTable[resourceIPPool]) k8s.ReflectorConfig[resourceIPPool] {
+	lw := utils.ListerWatcherFromTyped(cs.CiliumV2alpha1().CiliumResourceIPPools())
+	return k8s.ReflectorConfig[resourceIPPool]{
+		Name:          "cilium-resource-ip-pool-k8s-reflector",
+		Table:         pools,
+		ListerWatcher: lw,
+		MetricScope:   "CiliumResourceIPPool",
+		Transform: func(_ statedb.ReadTxn, obj any) (resourceIPPool, bool) {
+			pool, ok := obj.(*v2alpha1.CiliumResourceIPPool)
+			if !ok {
+				return resourceIPPool{}, false
+			}
+			var nodeSel labels.Selector
+			if pool.Spec.NodeSelector == nil {
+				nodeSel = labels.Everything()
+			} else {
+				sel, err := slimv1.LabelSelectorAsSelector(pool.Spec.NodeSelector)
+				if err != nil {
+					return resourceIPPool{}, false
+				}
+				nodeSel = sel
+			}
+			return resourceIPPool{
+				Name:         pool.Name,
+				IPv4:         pool.Spec.IPv4,
+				IPv6:         pool.Spec.IPv6,
+				NodeSelector: nodeSel,
+				UpdatedAt:    time.Now(),
+			}, true
+		},
+		CRDSync: crdSync,
+	}
 }
