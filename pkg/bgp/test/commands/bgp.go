051c289cc7472074d9c4dc978e595cb17ae8d21b
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2026-02-05 23:57:31 +0000

    bgp: add routes hive command
    [ upstream commit 436d64a5f0b08645649a6c2f9b5a7d0d593af444 ]
    
    Introduce bgp/routes to query BGPRouterManager.GetRoutes and render a
    structured table for loc-rib and adj-rib tables.
    
    Example output:
    
    loc-rib
    
    ```
    Instance      Prefix          NextHop    Best   Age
    instance0     10.0.0.0/32     0.0.0.0    true   10s
                  10.0.0.1/32     0.0.0.0    true   10s
    instance1     10.0.0.2/32     0.0.0.0    true   10s
    ```
    
    adj-rib
    
    ```
    Instance    Peer    Prefix            NextHop       Age
    instance0   peer0   10.244.0.0/24     10.99.0.110   10s
                        10.96.50.104/32   10.99.0.110   10s
                peer1   10.244.0.0/24     10.99.0.110   10s
                        10.96.50.104/32   10.99.0.110   10s
    instance1   peer2   10.244.0.0/24     10.99.0.110   10s
                        10.96.50.104/32   10.99.0.110   10s
    ```
    
    It has an optional flags -o (output to file) and --no-age (disable Age
    output to make the command output predictable).
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

--- pkg/bgp/test/commands/bgp.go
+++ pkg/bgp/test/commands/bgp.go
@@ -13,89 +13,14 @@ import (
 )
 
 const (
-	peerFlag      = "peer"
-	peerFlagShort = "p"
-
 	routerASNFlag      = "router-asn"
 	routerASNFlagShort = "r"
 )
 
 func BGPScriptCmds(bgpMgr agent.BGPRouterManager) map[string]script.Cmd {
 	return map[string]script.Cmd{
-		"bgp/routes":         BGPPRoutesCmd(bgpMgr),
 		"bgp/route-policies": BGPPRoutePolicies(bgpMgr),
 	}
-}
-
-func BGPPRoutesCmd(bgpMgr agent.BGPRouterManager) script.Cmd {
-	return script.Command(
-		script.CmdUsage{
-			Summary: "List BGP routes on Cilium",
-			Args:    "[available|advertised] [afi] [safi]",
-			Flags: func(fs *pflag.FlagSet) {
-				fs.StringP(peerFlag, peerFlagShort, "", "IP address of the peer. If provided, routes advertised to the specified peer are listed.")
-				fs.Uint32P(routerASNFlag, routerASNFlagShort, 0, "ASN number of the Cilium router instance. Lists routes of all instances if omitted.")
-				addOutFileFlag(fs)
-			},
-			Detail: []string{
-				"List routes in the BGP Control Plane's RIBs",
-				"",
-				"'available' lists routes from the local RIB, 'advertised' lists routes from the RIB-OUT of BGP peer(s).",
-				"When none of them is provided, lists 'available' routes",
-				"",
-				"'afi' is Address Family Indicator, defaults to 'ipv4'.",
-				"'safi' is Subsequent Address Family Identifier, defaults to 'unicast'.",
-			},
-		},
-		func(s *script.State, args ...string) (script.WaitFunc, error) {
-			peer, err := s.Flags.GetString(peerFlag)
-			if err != nil {
-				return nil, err
-			}
-			asn, err := s.Flags.GetUint32(routerASNFlag)
-			if err != nil {
-				return nil, err
-			}
-			return func(*script.State) (stdout, stderr string, err error) {
-				tw, buf, f, err := getCmdTabWriter(s)
-				if err != nil {
-					return "", "", err
-				}
-				if f != nil {
-					defer f.Close()
-				}
-
-				params := restapi.GetBgpRoutesParams{
-					TableType: "loc-rib",
-					Afi:       "ipv4",
-					Safi:      "unicast",
-				}
-				if len(args) > 0 && args[0] == "advertised" {
-					params.TableType = "adj-rib-out"
-				}
-				if len(args) > 1 && args[1] != "" {
-					params.Afi = args[1]
-				}
-				if len(args) > 2 && args[2] != "" {
-					params.Safi = args[2]
-				}
-				if peer != "" {
-					params.Neighbor = &peer
-				}
-				if asn != 0 {
-					asn64 := int64(asn)
-					params.RouterAsn = &asn64
-				}
-				routes, err := bgpMgr.GetRoutesLegacy(s.Context(), params)
-				if err != nil {
-					return "", "", err
-				}
-				err = api.PrintBGPRoutesTable(tw, routes, params.TableType == "adj-rib-out", false)
-
-				return buf.String(), "", err
-			}, nil
-		},
-	)
 }
 
 func BGPPRoutePolicies(bgpMgr agent.BGPRouterManager) script.Cmd {
