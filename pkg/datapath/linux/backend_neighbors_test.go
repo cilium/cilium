20e4549cce942cc044d28f85a678e92e7a7eefe3
Marco Iorio <marco.iorio@isovalent.com>
2026-02-06 12:30:54 +0100

    test: fix goleak check in combination with script tests
    Currently, multiple script tests are intended to validate that no
    goroutines are leaked once the tests end, deferring the invocation
    of the dedicated [testutils.GoleakVerifyNone] function. However,
    the underlying [goleak.VerifyNone] utility is incompatible with
    t.Parallel [1], which is set by default by script tests, and no
    check is actually performed.
    
    Let's get this fixed by using [goleak.VerifyTestMain] instead, as
    also suggested by goleak documentation itself. This commit fixes all
    occurrences spotted via:
    
    $ git grep -l GoleakVerifyNone | xargs grep -l testdata
    
    It is worth additionally mentioning that:
    
    * GoleakVerifyTestMain was already invoked in the redirectpolicy
      package, and is thus not added;
    * The functions previously ignored in the devices_controller tests
      do not appear to be necessary anymore, and have been omitted; yet,
      we need to additionally ignore one metrics related goroutine that
      is otherwise flagged when IPSec is enabled;
    * One of the script tests in the route/reconciler package did not
      correctly stop the hive, causing a few goroutines to be leaked.
    
    Ideally we should have a linter to catch this problem directly
    in CI, but that's deferred for the future.
    
    [1]: https://pkg.go.dev/go.uber.org/goleak#VerifyNone
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/datapath/linux/backend_neighbors_test.go
+++ pkg/datapath/linux/backend_neighbors_test.go
@@ -16,16 +16,10 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/neighbor"
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/loadbalancer"
-	"github.com/cilium/cilium/pkg/testutils"
 	"github.com/cilium/cilium/pkg/time"
 )
 
 func TestBackendNeighborSync(t *testing.T) {
-	// Ignore all the currently running goroutines spawned
-	// by prior tests or by package init() functions.
-	goleakOpt := testutils.GoleakIgnoreCurrent()
-	t.Cleanup(func() { testutils.GoleakVerifyNone(t, goleakOpt) })
-
 	var (
 		db             *statedb.DB
 		backends       statedb.RWTable[*loadbalancer.Backend]
