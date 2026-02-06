48dbff33b072286f9f3d2b681c20020b4ca0de2b
Marco Iorio <marco.iorio@isovalent.com>
2026-02-06 11:30:54 +0000

    test: fix goleak check in combination with script tests
    [ upstream commit 8c6252ffe151d50e0f7bf69b5d15287846e2084f ]
    
    Currently, multiple script tests are intended to validate that no
    goroutines are leaked once the tests end, deferring the invocation
    of the dedicated [testutils.GoleakVerifyNone] function. However,
    the underlying [goleak.VerifyNone] utility is incompatible with
    t.Parallel [1], which is set by default by script tests, and no
    check is actually performed.
    
    Let's get this fixed by using [goleak.VerifyTestMain] instead, as
    also suggested by goleak documentation itself. This commit fixes all
    occurrences spotted via:
    
    $ git grep -l GoleakVerifyNone | xargs grep -l testdata
    
    It is worth additionally mentioning that:
    
    * GoleakVerifyTestMain was already invoked in the redirectpolicy
      package, and is thus not added;
    * The functions previously ignored in the devices_controller tests
      do not appear to be necessary anymore, and have been omitted; yet,
      we need to additionally ignore one metrics related goroutine that
      is otherwise flagged when IPSec is enabled;
    * One of the script tests in the route/reconciler package did not
      correctly stop the hive, causing a few goroutines to be leaked.
    
    Ideally we should have a linter to catch this problem directly
    in CI, but that's deferred for the future.
    
    [1]: https://pkg.go.dev/go.uber.org/goleak#VerifyNone
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>
    Signed-off-by: Fabian Fischer <fabian.fischer@isovalent.com>

--- operator/watchers/script_test.go
+++ operator/watchers/script_test.go
@@ -31,21 +31,12 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging"
-	"github.com/cilium/cilium/pkg/testutils"
 	"github.com/cilium/cilium/pkg/time"
 )
 
 var debug = flag.Bool("debug", false, "Enable debug logging")
 
 func TestScript(t *testing.T) {
-	// Catch any leaked goroutines.
-	t.Cleanup(func() {
-		testutils.GoleakVerifyNone(t,
-			// Ignore goroutines possibly left by other tests.
-			testutils.GoleakIgnoreCurrent(),
-		)
-	})
-
 	version.Force(k8sTestutils.DefaultVersion)
 
 	var opts []hivetest.LogOption
