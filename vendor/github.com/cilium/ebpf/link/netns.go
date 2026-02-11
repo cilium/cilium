7c633a7d232e2b654073cca316cbedd7babb22db
Dylan Reimerink <dylan.reimerink@isovalent.com>
2026-02-11 15:36:06 +0100

    vendor: bump ebpf-go to unreleased v0.21
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- vendor/github.com/cilium/ebpf/link/netns.go
+++ vendor/github.com/cilium/ebpf/link/netns.go
@@ -44,7 +44,7 @@ func (ns *NetNsLink) Info() (*Info, error) {
 		return nil, fmt.Errorf("netns link info: %s", err)
 	}
 	extra := &NetNsInfo{
-		NetnsIno:   info.NetnsIno,
+		NetnsInode: info.NetnsIno,
 		AttachType: info.AttachType,
 	}
 
