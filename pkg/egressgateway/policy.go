cilium/cilium

commit 7823a5f85cc8fddad3baf1eaa55b496b188a9329

egressgw: simplify error handling when selecting egressIP
Instead of setting `EgressIPNotFoundIPv*` from every error path, just set
it by default. And later override it with the actual egressIP, once we
were able to derive the gateway node's network configuration.

This helps to ensure that if we were not able to derive a network config
on the gateway node, the policy entry always ends up with the
EgressIPNotFoundIPv* value - and thus explicitly drops any EGW traffic that
is forwarded to the gateway.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

pkg/egressgateway/policy.go
@@ -157,32 +157,38 @@ func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 // data (such as egress IP or interface) given a policy egress gateway config
 func (gwc *gatewayConfig) deriveFromPolicyGatewayConfig(logger *slog.Logger, gc *policyGatewayConfig) error {
 	var err error
+	var egressIP4, egressIP6 netip.Addr
 
 	gwc.localNodeConfiguredAsGateway = false
+	gwc.egressIP4 = EgressIPNotFoundIPv4
+	gwc.egressIP6 = EgressIPNotFoundIPv6
 
 	switch {
 	case gc.iface != "":
 		// If the gateway config specifies an interface, use the first IPv4/v6 assigned to that
 		// interface as egress IPs
 		gwc.ifaceName = gc.iface
-		gwc.egressIP4, err = netdevice.GetIfaceFirstIPv4Address(gc.iface)
+		egressIP4, err = netdevice.GetIfaceFirstIPv4Address(gc.iface)
 		if err != nil {
-			gwc.egressIP4 = EgressIPNotFoundIPv4
 			return fmt.Errorf("failed to retrieve IPv4 address for egress interface: %w", err)
 		}
 
 		if gc.v6needed {
-			gwc.egressIP6, err = netdevice.GetIfaceFirstIPv6Address(gc.iface)
+			egressIP6, err = netdevice.GetIfaceFirstIPv6Address(gc.iface)
 			if err != nil {
-				gwc.egressIP6 = EgressIPNotFoundIPv6
 				return fmt.Errorf("failed to retrieve IPv6 address for egress interface: %w", err)
 			}
 		}
 	case gc.egressIP.IsValid():
 		// If the gateway config specifies an egress IP, use the interface with that IP as egress
 		// interface.
+		egressIP4 = gc.egressIP
+
 		// TODO: add ipv6 support for specifying an egress IP, currently only ipv4 is supported.
-		gwc.egressIP4 = gc.egressIP
+		if gc.v6needed {
+			egressIP6 = EgressIPNotFoundIPv6
+		}
+
 		gwc.ifaceName, err = netdevice.GetIfaceWithIPv4Address(gc.egressIP)
 		if err != nil {
 			return fmt.Errorf("failed to retrieve interface with egress IP: %w", err)
@@ -193,21 +199,18 @@ func (gwc *gatewayConfig) deriveFromPolicyGatewayConfig(logger *slog.Logger, gc 
 		// interface with the IPv4 default route
 		iface, err := route.NodeDeviceWithDefaultRoute(logger, true, false)
 		if err != nil {
-			gwc.egressIP4 = EgressIPNotFoundIPv4
 			return fmt.Errorf("failed to find interface with IPv4 default route: %w", err)
 		}
 
 		gwc.ifaceName = iface.Attrs().Name
-		gwc.egressIP4, err = netdevice.GetIfaceFirstIPv4Address(gwc.ifaceName)
+		egressIP4, err = netdevice.GetIfaceFirstIPv4Address(gwc.ifaceName)
 		if err != nil {
-			gwc.egressIP4 = EgressIPNotFoundIPv4
 			return fmt.Errorf("failed to retrieve IPv4 address for egress interface: %w", err)
 		}
 
 		if gc.v6needed {
 			iface, err := route.NodeDeviceWithDefaultRoute(logger, false, true)
 			if err != nil {
-				gwc.egressIP6 = EgressIPNotFoundIPv6
 				return fmt.Errorf("failed to find interface with IPv6 default route: %w", err)
 			}
 
@@ -216,14 +219,17 @@ func (gwc *gatewayConfig) deriveFromPolicyGatewayConfig(logger *slog.Logger, gc 
 			}
 
 			gwc.ifaceName = iface.Attrs().Name
-			gwc.egressIP6, err = netdevice.GetIfaceFirstIPv6Address(gwc.ifaceName)
+			egressIP6, err = netdevice.GetIfaceFirstIPv6Address(gwc.ifaceName)
 			if err != nil {
-				gwc.egressIP6 = EgressIPNotFoundIPv6
 				return fmt.Errorf("failed to retrieve IPv6 address for egress interface: %w", err)
 			}
 		}
 	}
 
+	gwc.egressIP4 = egressIP4
+	if gc.v6needed {
+		gwc.egressIP6 = egressIP6
+	}
 	gwc.localNodeConfiguredAsGateway = true
 
 	return nil

commit ebfa000810f844690a14f2d37f7d2e31d1e9d690

Set egress IP even if IP not detected in any node interface
This is because, IP not being attached to any interface is an expected
scenario in cloud environments(eg GKE) when the egress IP is managed by
the provider.

This semantically reverts the change in https://github.com/cilium/cilium/commit/7823a5f85c,
which broke the egress gateway in cloud environments.

Signed-off-by: sudeephb <sudeephb19@gmail.com>

pkg/egressgateway/policy.go
@@ -205,8 +205,16 @@ func (gwc *gatewayConfig) deriveFromPolicyGatewayConfig(logger *slog.Logger, gc 
 		}
 
 		gwc.ifaceName, err = netdevice.GetIfaceWithIPv4Address(gc.egressIP)
+		// Try to find the interface with this IP. This is best-effort: in cloud environments
+		// (e.g. GKE, EKS, AKS), the egress IP may be managed by the cloud provider and not
+		// directly visible on a local interface. In such cases, we still use the egress IP for
+		// SNAT, just without interface metadata.
 		if err != nil {
-			return fmt.Errorf("failed to retrieve interface with egress IP: %w", err)
+			logger.Warn(
+				"Failed to find interface with egress IP, continuing without interface metadata",
+				logfields.Error, err,
+				logfields.EgressIP, gc.egressIP,
+			)
 		}
 
 	default:
@@ -247,7 +255,10 @@ func (gwc *gatewayConfig) deriveFromPolicyGatewayConfig(logger *slog.Logger, gc 
 	if v6Needed {
 		gwc.egressIP6 = egressIP6
 	}
-	gwc.localNodeConfiguredAsGateway = true
+
+	if gwc.ifaceName != "" {
+		gwc.localNodeConfiguredAsGateway = true
+	}
 
 	return nil
 }