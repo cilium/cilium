f382bdd9b174b75356f7ae303ce4554ea38a0208
Simone Magnani <simone.magnani@isovalent.com>
2026-02-11 23:42:27 +0100

    bpf:test: add IPv6 N/S loadbalancer nodeport with fragments test
    This commit adds a BPF unit test for loadbalancing of fragmented packets
    destined to a NodePort service.
    This aims at replacing the Ginkgo "Supports IPv4 Fragments" test.
    
    We declare the two fragments as follows:
    
    1. Eth(14) + IPv6(40) + IPv6ExtHeader (8B) + TCP(20) + Raw(60) = 142B
    2. Eth(14) + IPv6(40) + IPv6ExtHeader (8B) + Raw(1)            = 63B
    
    The BPF tests are executed sequentially, therefore we don't need to manually
    replicate the setup for each test, nor simulate the 1st fragment reception
    during the test of the 2nd fragment. If the 1st test passed, we expect the
    underlying datapath structures to be correctly populated (ip frags,
    conntrack, etc.). The test ensure that both the 1st and 2nd fragments are
    correctly handled, bpf metrics are updated as expected, and that the
    conntrack map accounts for both the two fragments while computing statistics.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

--- bpf/tests/scapy/lb_pkt_defs.py
+++ bpf/tests/scapy/lb_pkt_defs.py
@@ -65,3 +65,39 @@ lb4_nodeport_fragment2_post_dnat = (
     IP(src=v4_ext_one, dst=v4_pod_one, flags='', frag=(20+60)//8, id=256, proto=6) /
     Raw(load="S" * 1)
 )
+
+# Create two TCP fragments over IPv6:
+# 1. Eth(14) + IPv6(40) + IPv6ExtHeader (8B) + TCP(20) + Raw(1) = 83B
+# 2. Eth(14) + IPv6(40) + IPv6ExtHeader (8B) + Raw(1)           = 63B
+#
+# nh=44 indicates Fragment Header
+# m=1 is More Fragments (MF)
+lb6_nodeport_fragment1 = (
+    Ether(src=mac_one, dst=mac_two) /
+    IPv6(src=v6_ext_node_one, dst=v6_svc_one, nh=44) /
+    IPv6ExtHdrFragment(offset=0, m=1, id=256, nh=6) /
+    TCP(sport=tcp_src_one, dport=tcp_svc_one) /
+    Raw(load="S" * 60)
+)
+
+lb6_nodeport_fragment1_post_dnat = (
+    Ether(src=mac_one, dst=mac_two) /
+    IPv6(src=v6_ext_node_one, dst=v6_pod_one, nh=44) /
+    IPv6ExtHdrFragment(offset=0, m=1, id=256, nh=6) /
+    TCP(sport=tcp_src_one, dport=tcp_dst_one) /
+    Raw(load="S" * 60)
+)
+
+lb6_nodeport_fragment2 = (
+    Ether(src=mac_one, dst=mac_two) /
+    IPv6(src=v6_ext_node_one, dst=v6_svc_one, nh=44) /
+    IPv6ExtHdrFragment(offset=(20+60)//8, m=0, id=256, nh=6) /
+    Raw(load="S" * 1)
+)
+
+lb6_nodeport_fragment2_post_dnat = (
+    Ether(src=mac_one, dst=mac_two) /
+    IPv6(src=v6_ext_node_one, dst=v6_pod_one, nh=44) /
+    IPv6ExtHdrFragment(offset=(20+60)//8, m=0, id=256, nh=6) /
+    Raw(load="S" * 1)
+)
