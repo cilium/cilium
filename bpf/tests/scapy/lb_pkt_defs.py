49387e07c968c10fa59c239f8de0d95c662a6bd2
Simone Magnani <simone.magnani@isovalent.com>
2026-02-11 23:29:03 +0100

    bpf:test: add IPv4 N/S loadbalancer nodeport with fragments test
    This commit adds a BPF unit test for loadbalancing of fragmented packets
    destined to a NodePort service.
    This aims at replacing the Ginkgo "Supports IPv4 Fragments" test.
    
    We declare the two fragments as follows:
    
    1. Ether(14) + IP(20) + TCP(20) + Raw(1) = 55B
    2. Ether(14) + IP(20) + Raw(1)           = 35B.
    
    The BPF tests are executed sequentially, therefore we don't need to manually
    replicate the setup for each test, nor simulate the 1st fragment reception
    during the test of the 2nd fragment. If the 1st test passed, we expect the
    underlying datapath structures to be correctly populated (ip frags,
    conntrack, etc.). The test ensure that both the 1st and 2nd fragments are
    correctly handled, bpf metrics are updated as expected, and that the
    conntrack map accounts for both the two fragments while computing statistics.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

--- bpf/tests/scapy/lb_pkt_defs.py
+++ bpf/tests/scapy/lb_pkt_defs.py
@@ -32,3 +32,36 @@ lb6_clusterip_post_dnat = (
     TCP(sport=tcp_src_one, dport=tcp_dst_one) /
     Raw("S"*1)
 )
+
+# Create two TCP fragments over IPv4:
+# 1. Ether(14) + IP(20) + TCP(20) + Raw(1) = 55B
+# 2. Ether(14) + IP(20) + Raw(1)           = 35B.
+#
+# Dev Notes:
+# - `frag` is in 8-byte units.
+# - `id` is in big endian.
+lb4_nodeport_fragment1 = (
+    Ether(src=mac_one, dst=mac_two) /
+    IP(src=v4_ext_one, dst=v4_svc_one, flags='MF', frag=0, id=256, proto=6) /
+    TCP(sport=tcp_src_one, dport=tcp_svc_one) /
+    Raw(load="S" * 1)
+)
+
+lb4_nodeport_fragment1_post_dnat = (
+    Ether(src=mac_one, dst=mac_two) /
+    IP(src=v4_ext_one, dst=v4_pod_one, flags='MF', frag=0, id=256, proto=6) /
+    TCP(sport=tcp_src_one, dport=tcp_dst_one) /
+    Raw(load="S" * 1)
+)
+
+lb4_nodeport_fragment2 = (
+    Ether(src=mac_one, dst=mac_two) /
+    IP(src=v4_ext_one, dst=v4_svc_one, flags='', frag=(20+60)//8, id=256, proto=6) /
+    Raw(load="S" * 1)
+)
+
+lb4_nodeport_fragment2_post_dnat = (
+    Ether(src=mac_one, dst=mac_two) /
+    IP(src=v4_ext_one, dst=v4_pod_one, flags='', frag=(20+60)//8, id=256, proto=6) /
+    Raw(load="S" * 1)
+)
