f6a2eab0ac1f4ca4bfd4e824c90095682533ecb2
Alice Mikityanska <alice@isovalent.com>
2026-02-16 21:34:05 +0200

    [WIP] .github/workflows: Fake external target for GKE workflow
    Signed-off-by: Alice Mikityanska <alice@isovalent.com>

---
+++ .github/actions/generic-external-targets/action.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+TARGETNAME=nginx-external.default.svc
+OTHERTARGETNAME=nginx-external-other.default.svc
+
+# Create a private key for the self signed CA
+openssl genrsa 2048 > ca-key.pem
+
+# Create a self signed CA certificate
+openssl req -new -x509 -nodes -days 365 \
+    -key ca-key.pem \
+    -subj "/O=Cilium/CN=Cilium CA" \
+    -out ca-cert.pem
+
+# Create a secret with the CA certificate, will be used by L7 tests as trused CA for
+# connections between Envoy and our external services
+kubectl create ns external-target-secrets
+kubectl -n external-target-secrets create secret generic custom-ca --from-file=ca.crt=ca-cert.pem
+
+# Create a cerificate signing request and private key for external services
+# Note only the primary external target is in the common name.
+openssl req -newkey rsa:2048 -nodes \
+    -keyout external-service.cilium.key \
+    -subj "/CN=$TARGETNAME" \
+    -out external-service.cilium.req.pem
+
+# Create a config file to tell openssl how to turn the signing request into a certificate
+# Make sure the key usage is such that we can use the cert for HTTPS traffic.
+# Also make sure both domain names are in the subjectAltName so the certificate works for
+# both external services and the IP addresses without domain name.
+cat > v3.ext << EOF
+subjectKeyIdentifier   = hash
+authorityKeyIdentifier = keyid:always,issuer:always
+keyUsage               = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign
+subjectAltName         = DNS:$OTHERTARGETNAME, DNS:$TARGETNAME
+EOF
+
+# Turn the certificate signing request into a certificate, signed by our CA
+openssl x509 -req -days 365 -set_serial 01 \
+    -in external-service.cilium.req.pem \
+    -out external-service.cilium.crt \
+    -extfile v3.ext \
+    -CA ca-cert.pem \
+    -CAkey ca-key.pem
+
+# Start the external targets.
+mapfile -t NODES_WITHOUT_CILIUM < <(kubectl get nodes -l cilium.io/no-schedule=true -o name | sed 's@^node/@@')
+NGINX_CERT_BASE64="$(base64 -w0 external-service.cilium.crt)" \
+NGINX_KEY_BASE64="$(base64 -w0 external-service.cilium.key)" \
+EXTERNAL_NODE1="${NODES_WITHOUT_CILIUM[0]}" \
+EXTERNAL_NODE2="${NODES_WITHOUT_CILIUM[1]}" \
+envsubst '$NGINX_CERT_BASE64 $NGINX_KEY_BASE64 $EXTERNAL_NODE1 $EXTERNAL_NODE2' < "$(dirname "$0")/nginx.yaml" | kubectl apply -f -
+kubectl rollout status daemonset nginx-external --timeout 60s
+
+# Save nginx ConfigMap versions to track when they are updated.
+NGINX1_CONFIGMAP_VERSION="$(kubectl exec "$(kubectl get pods -l app=nginx-external -o name)" -- readlink /etc/nginx/ssl/..data)"
+NGINX2_CONFIGMAP_VERSION="$(kubectl exec "$(kubectl get pods -l app=nginx-external-other -o name)" -- readlink /etc/nginx/ssl/..data)"
+
+# Figure out the addresses of the newly deployed pods.
+IP4TARGET="$(kubectl get pods -l app=nginx-external -o json | jq -r '.items[].status.hostIP')"
+IP4OTHERTARGET="$(kubectl get pods -l app=nginx-external-other -o json | jq -r '.items[].status.hostIP')"
+echo "ipv4_external_target=$IP4TARGET" >> $GITHUB_OUTPUT
+echo "ipv4_other_external_target=$IP4OTHERTARGET" >> $GITHUB_OUTPUT
+echo "external_target_name=$TARGETNAME" >> $GITHUB_OUTPUT
+echo "other_external_target_name=$OTHERTARGETNAME" >> $GITHUB_OUTPUT
+
+# Add IP addresses to subjectAltName in the certificate.
+cat > v3.ext << EOF
+subjectKeyIdentifier   = hash
+authorityKeyIdentifier = keyid:always,issuer:always
+keyUsage               = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign
+subjectAltName         = DNS:$OTHERTARGETNAME, DNS:$TARGETNAME, IP:$IP4TARGET, IP:$IP4OTHERTARGET
+EOF
+
+# Turn the certificate signing request into a certificate, signed by our CA
+openssl x509 -req -days 365 -set_serial 01 \
+    -in external-service.cilium.req.pem \
+    -out external-service.cilium.crt \
+    -extfile v3.ext \
+    -CA ca-cert.pem \
+    -CAkey ca-key.pem
+
+# Update the ConfigMap with the new certificate.
+NGINX_CERT_BASE64="$(base64 -w0 external-service.cilium.crt)" \
+NGINX_KEY_BASE64="$(base64 -w0 external-service.cilium.key)" \
+EXTERNAL_NODE1="${NODES_WITHOUT_CILIUM[0]}" \
+EXTERNAL_NODE2="${NODES_WITHOUT_CILIUM[1]}" \
+envsubst '$NGINX_CERT_BASE64 $NGINX_KEY_BASE64 $EXTERNAL_NODE1 $EXTERNAL_NODE2' < "$(dirname "$0")/nginx.yaml" | kubectl apply -f -
+
+# Wait until the ConfigMap gets updated. Timeout after 10 minutes.
+CONFIGMAP_UPDATED=
+for i in $(seq 1 60); do
+	NGINX1_CONFIGMAP_VERSION_NEW="$(kubectl exec "$(kubectl get pods -l app=nginx-external -o name)" -- readlink /etc/nginx/ssl/..data)"
+	NGINX2_CONFIGMAP_VERSION_NEW="$(kubectl exec "$(kubectl get pods -l app=nginx-external-other -o name)" -- readlink /etc/nginx/ssl/..data)"
+	if [ "$NGINX1_CONFIGMAP_VERSION" != "$NGINX1_CONFIGMAP_VERSION_NEW" ] && [ "$NGINX2_CONFIGMAP_VERSION" != "$NGINX2_CONFIGMAP_VERSION_NEW" ]; then
+		CONFIGMAP_UPDATED=1
+		break
+	fi
+	echo 'Waiting until nginx ConfigMap is updated...'
+	sleep 10
+done
+if [ -z "$CONFIGMAP_UPDATED" ]; then
+	echo 'Timeout waiting for nginx ConfigMap update.'
+	exit 1
+fi
+
+# Reload nginx after updating the ConfigMap.
+kubectl exec "$(kubectl get pods -l app=nginx-external -o name)" -- /usr/sbin/nginx -s reload
+kubectl exec "$(kubectl get pods -l app=nginx-external-other -o name)" -- /usr/sbin/nginx -s reload
