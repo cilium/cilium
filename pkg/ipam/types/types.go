d61bc12dd5ac90a0b5d2cd5129649f8892af0ee3
Hadrien Patte <hadrien.patte@datadoghq.com>
2026-02-18 00:56:18 +0100

    ipam: Use private `deepEqual` for `Subnet`
    This is a followup to #39335 which migrated the `ipam.Subnet` struct
    fields from `net.IPNet` to `netip.Prefix`. When I did that PR, I
    disabled the autogeneration of the `Subnet.DeepEqual()` method as the
    generator does not support `netip.Prefix` fields.
    
    But I now learned that we can still rely on the generator to generate a
    private `deepEqual()` method and only exclude the unsupported fields and
    then only have to maintain a public `DeepEqual()` method that manually
    compares the unsupported fields and then calls the private method.
    
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- pkg/ipam/types/types.go
+++ pkg/ipam/types/types.go
@@ -306,7 +306,8 @@ func (t Tags) Match(required Tags) bool {
 
 // Subnet is a representation of a subnet
 // +k8s:deepcopy-gen=false
-// +deepequal-gen=false
+// +deepequal-gen=true
+// +deepequal-gen:private-method=true
 type Subnet struct {
 	// ID is the subnet ID
 	ID string
@@ -315,9 +316,11 @@ type Subnet struct {
 	Name string
 
 	// CIDR is the IPv4 CIDR associated with the subnet
+	// +deepequal-gen=false
 	CIDR netip.Prefix
 
 	// IPv6CIDR is the IPv6 CIDR associated with the subnet
+	// +deepequal-gen=false
 	IPv6CIDR netip.Prefix
 
 	// AvailabilityZone is the availability zone of the subnet
@@ -338,47 +341,20 @@ type Subnet struct {
 	Tags Tags
 }
 
-// DeepEqual is a deepequal function, deeply comparing the
-// receiver with other. in must be non-nil.
+// DeepEqual compares two Subnet structs for equality.
 func (in *Subnet) DeepEqual(other *Subnet) bool {
 	if other == nil {
 		return false
 	}
-
-	if in.ID != other.ID {
-		return false
-	}
-	if in.Name != other.Name {
-		return false
-	}
+	// Manually compare netip.Prefix fields
 	if in.CIDR != other.CIDR {
 		return false
 	}
-
 	if in.IPv6CIDR != other.IPv6CIDR {
 		return false
 	}
-
-	if in.AvailabilityZone != other.AvailabilityZone {
-		return false
-	}
-	if in.VirtualNetworkID != other.VirtualNetworkID {
-		return false
-	}
-	if in.AvailableAddresses != other.AvailableAddresses {
-		return false
-	}
-	if in.AvailableIPv6Addresses != other.AvailableIPv6Addresses {
-		return false
-	}
-	if ((in.Tags != nil) && (other.Tags != nil)) || ((in.Tags == nil) != (other.Tags == nil)) {
-		in, other := &in.Tags, &other.Tags
-		if !in.DeepEqual(other) {
-			return false
-		}
-	}
-
-	return true
+	// Call generated private method for other fields
+	return in.deepEqual(other)
 }
 
 // DeepCopyInto is a deepcopy function, copying the receiver, writing into out. in must be non-nil.
