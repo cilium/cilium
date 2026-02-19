c4c2c6424d9de32d8f4afdf09df4bba7fbc34bd2
Simone Magnani <simone.magnani@isovalent.com>
2026-02-19 19:57:56 +0100

    bpf:test: add IPv4/6 N/S loadbalancer tests with ClusterIP
    This commit adds a BPF unit test for loadbalancing of external packets
    destined to ClusterIP service. This aims at replacing the Ginkgo
    "ClusterIP can be accessed when external access is enabled" test.
    
    Differently than testing a non-routable ClusterIP service, in this BPF
    test we enable the SVC_FLAG_ROUTABLE flag when creating the SVC in the
    bpf map. Therefore, we expect the packet to be correctly handled, DNATed,
    and conntrack to track the connections.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

--- bpf/tests/scapy/lb_pkt_defs.py
+++ bpf/tests/scapy/lb_pkt_defs.py
@@ -12,9 +12,23 @@ lb4_clusterip = (
     Raw("S"*1)
 )
 
+lb4_clusterip_post_dnat = (
+    Ether(src=mac_one, dst=mac_two) /
+    IP(src=v4_ext_one, dst=v4_pod_one) /
+    TCP(sport=tcp_src_one, dport=tcp_dst_one) /
+    Raw("S"*1)
+)
+
 lb6_clusterip = (
     Ether(src=mac_one, dst=mac_two) /
     IPv6(src=v6_ext_node_one, dst=v6_svc_one) /
     TCP(sport=tcp_src_one, dport=tcp_svc_one) /
+    Raw("S"*1)
+)
+
+lb6_clusterip_post_dnat = (
+    Ether(src=mac_one, dst=mac_two) /
+    IPv6(src=v6_ext_node_one, dst=v6_pod_one) /
+    TCP(sport=tcp_src_one, dport=tcp_dst_one) /
     Raw("S"*1)
 )
