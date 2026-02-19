d08d1b8ff9abff3baca8ad0e1f6b2c93aae94f6f
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2026-02-19 17:15:34 +0000

    bpf/tests: add tests for bpf_host redirecting with config_netkit
    This commit introduces BPF testing to verify the appropriate redirect
    helpers are called via bpf_host in both veth and netkit modes. This
    focuses on Host-to-Pod scenarios.
    
    Signed-off-by: Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>

--- bpf/tests/scapy/tc_redirect_pkt_defs.py
+++ bpf/tests/scapy/tc_redirect_pkt_defs.py
@@ -32,3 +32,31 @@ tc_redirect_lxc_ipv6_post = (
 	TCP(sport=tcp_src_one, dport=tcp_dst_one, seq=tcp_default_seq, window=tcp_default_win) /
 	Raw(default_data)
 )
+
+# tc_redirect_host IPv4
+tc_redirect_host_ipv4_pre = (
+	Ether(src=mac_one, dst=mac_two) /
+	IP(src=v4_ext_one, dst=v4_pod_one, id=0, ttl=64) /
+	TCP(sport=tcp_src_one, dport=tcp_svc_one, seq=tcp_default_seq, window=tcp_default_win) /
+	Raw(default_data)
+)
+tc_redirect_host_ipv4_post = (
+	Ether(src=mac_three, dst=mac_four) /
+	IP(src=v4_ext_one, dst=v4_pod_one, id=0, ttl=63) /
+	TCP(sport=tcp_src_one, dport=tcp_svc_one, seq=tcp_default_seq, window=tcp_default_win) /
+	Raw(default_data)
+)
+
+# tc_redirect_host IPv6
+tc_redirect_host_ipv6_pre = (
+	Ether(src=mac_one, dst=mac_two) /
+	IPv6(src=v6_ext_node_one, dst=v6_pod_one, hlim=64) /
+	TCP(sport=tcp_src_one, dport=tcp_svc_one, seq=tcp_default_seq, window=tcp_default_win) /
+	Raw(default_data)
+)
+tc_redirect_host_ipv6_post = (
+	Ether(src=mac_three, dst=mac_four) /
+	IPv6(src=v6_ext_node_one, dst=v6_pod_one, hlim=63) /
+	TCP(sport=tcp_src_one, dport=tcp_svc_one, seq=tcp_default_seq, window=tcp_default_win) /
+	Raw(default_data)
+)
