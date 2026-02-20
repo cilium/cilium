8593d365a13266d74054570869e3d77434e8066b
Arthur Outhenin-Chalandre <git@mrfreezeex.fr>
2026-02-20 20:10:36 +0100

    wip: fix conformance
    Signed-off-by: Arthur Outhenin-Chalandre <git@mrfreezeex.fr>

--- vendor/sigs.k8s.io/mcs-api/conformance/framework.go
+++ vendor/sigs.k8s.io/mcs-api/conformance/framework.go
@@ -45,7 +45,7 @@ func execCmd(k8s kubernetes.Interface, config *rest.Config, podName string, podN
 
 	var stdout, stderr bytes.Buffer
 
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	err = exec.StreamWithContext(ctx, remotecommand.StreamOptions{
