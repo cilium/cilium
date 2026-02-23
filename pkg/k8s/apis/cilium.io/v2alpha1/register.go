cilium/cilium

commit 6ba13948d434ebcd48eb8b3a662776b378ffe3df

network driver: add CRDs for cluster and node configs
adding the CRD types for both cilium operator
and cilium agent, in preparation
to add the configuration selection logic to the
cilium operator

make the `vfCount` field immutable

Signed-off-by: Bernardo Soares <20172413+bersoare@users.noreply.github.com>
Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/k8s/apis/cilium.io/v2alpha1/register.go
@@ -101,12 +101,19 @@ const (
 	CGCCKindDefinition = "CiliumGatewayClassConfig"
 	CGCCName           = CGCCPluralName + "." + CustomResourceDefinitionGroup
 
-	// CiliumNetworkDriverConfig
-	CiliumNetworkDriverConfigSingularName   = "ciliumnetworkdriverconfig"
-	CiliumNetworkDriverConfigPluralName     = "ciliumnetworkdriverconfigs"
-	CiliumNetworkDriverConfigKindDefinition = "CiliumNetworkDriverConfig"
+	// CiliumNetworkDriverClusterConfig
+	CiliumNetworkDriverClusterConfigSingularName   = "ciliumnetworkdriverclusterconfig"
+	CiliumNetworkDriverClusterConfigPluralName     = "ciliumnetworkdriverclusterconfigs"
+	CiliumNetworkDriverClusterConfigKindDefinition = "CiliumNetworkDriverClusterConfig"
 
-	CiliumNetworkDriverConfigName = CiliumNetworkDriverConfigPluralName + "." + CustomResourceDefinitionGroup
+	CiliumNetworkDriverClusterConfigName = CiliumNetworkDriverClusterConfigPluralName + "." + CustomResourceDefinitionGroup
+
+	// CiliumNetworkDriverNodeConfig
+	CiliumNetworkDriverNodeConfigSingularName   = "ciliumnetworkdrivernodeconfig"
+	CiliumNetworkDriverNodeConfigPluralName     = "ciliumnetworkdrivernodeconfigs"
+	CiliumNetworkDriverNodeConfigKindDefinition = "CiliumNetworkDriverNodeConfig"
+
+	CiliumNetworkDriverNodeConfigName = CiliumNetworkDriverNodeConfigPluralName + "." + CustomResourceDefinitionGroup
 )
 
 // SchemeGroupVersion is group version used to register these objects
@@ -177,8 +184,9 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&CiliumGatewayClassConfig{},
 		&CiliumGatewayClassConfigList{},
 
-		&CiliumNetworkDriverConfig{},
-		&CiliumNetworkDriverConfigList{},
+		&CiliumNetworkDriverClusterConfig{},
+		&CiliumNetworkDriverClusterConfigList{},
+		&CiliumNetworkDriverNodeConfig{},
 	)
 
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)

pkg/k8s/apis/cilium.io/client/register.go
@@ -91,8 +91,11 @@ const (
 	// CGCCCRDName is the full name of the CiliumGatewayClassConfig CRD.
 	CGCCCRDName = k8sconstv2alpha1.CGCCKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
 
-	// CiliumNetworkDriverConfigCRDName is the full name of the CiliumNetworkDriverConfig CRD.
-	CiliumNetworkDriverConfigCRDName = k8sconstv2alpha1.CiliumNetworkDriverConfigKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
+	// CiliumNetworkDriverClusterConfigCRDName is the full name of the CiliumNetworkDriverClusterConfig CRD.
+	CiliumNetworkDriverClusterConfigCRDName = k8sconstv2alpha1.CiliumNetworkDriverClusterConfigKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
+
+	// CiliumNetworkDriverNodeConfigCRDName is the full name of the CiliumNetworkDriverNodeConfig CRD.
+	CiliumNetworkDriverNodeConfigCRDName = k8sconstv2alpha1.CiliumNetworkDriverNodeConfigKindDefinition + "/" + k8sconstv2alpha1.CustomResourceDefinitionVersion
 )
 
 type CRDList struct {
@@ -187,9 +190,13 @@ func CustomResourceDefinitionList() map[string]*CRDList {
 			Name:     CGCCCRDName,
 			FullName: k8sconstv2alpha1.CGCCName,
 		},
-		synced.CRDResourceName(k8sconstv2alpha1.CiliumNetworkDriverConfigName): {
-			Name:     CiliumNetworkDriverConfigCRDName,
-			FullName: k8sconstv2alpha1.CiliumNetworkDriverConfigName,
+		synced.CRDResourceName(k8sconstv2alpha1.CiliumNetworkDriverClusterConfigName): {
+			Name:     CiliumNetworkDriverClusterConfigCRDName,
+			FullName: k8sconstv2alpha1.CiliumNetworkDriverClusterConfigName,
+		},
+		synced.CRDResourceName(k8sconstv2alpha1.CiliumNetworkDriverNodeConfigName): {
+			Name:     CiliumNetworkDriverNodeConfigCRDName,
+			FullName: k8sconstv2alpha1.CiliumNetworkDriverNodeConfigName,
 		},
 	}
 }
@@ -276,8 +283,11 @@ var (
 	//go:embed crds/v2alpha1/ciliumgatewayclassconfigs.yaml
 	crdsv2Alpha1CiliumGatewayClassConfigs []byte
 
-	//go:embed crds/v2alpha1/ciliumnetworkdriverconfigs.yaml
-	crdsv2Alpha1CiliumNetworkDriverConfigs []byte
+	//go:embed crds/v2alpha1/ciliumnetworkdriverclusterconfigs.yaml
+	crdsv2Alpha1CiliumNetworkDriverClusterConfigs []byte
+
+	//go:embed crds/v2alpha1/ciliumnetworkdrivernodeconfigs.yaml
+	crdsv2Alpha1CiliumNetworkDriverNodeConfigs []byte
 )
 
 // GetPregeneratedCRD returns the pregenerated CRD based on the requested CRD
@@ -334,8 +344,10 @@ func GetPregeneratedCRD(logger *slog.Logger, crdName string) apiextensionsv1.Cus
 		crdBytes = crdsv2Alpha1CiliumPodIPPools
 	case CGCCCRDName:
 		crdBytes = crdsv2Alpha1CiliumGatewayClassConfigs
-	case CiliumNetworkDriverConfigCRDName:
-		crdBytes = crdsv2Alpha1CiliumNetworkDriverConfigs
+	case CiliumNetworkDriverClusterConfigCRDName:
+		crdBytes = crdsv2Alpha1CiliumNetworkDriverClusterConfigs
+	case CiliumNetworkDriverNodeConfigCRDName:
+		crdBytes = crdsv2Alpha1CiliumNetworkDriverNodeConfigs
 	default:
 		logging.Fatal(logger, "Pregenerated CRD does not exist", logfields.CRDName, crdName)
 	}

pkg/k8s/apis/cilium.io/register.go
@@ -15,5 +15,5 @@ const (
 	//
 	// Maintainers: Run ./Documentation/check-crd-compat-table.sh for each release
 	// Developers: Bump patch for each change in the CRD schema.
-	CustomResourceDefinitionSchemaVersion = "1.33.1"
+	CustomResourceDefinitionSchemaVersion = "1.33.3"
 )

contrib/scripts/k8s-manifests-gen.sh
@@ -40,7 +40,8 @@ CRDS_CILIUM_V2ALPHA1="ciliumendpointslices \
                       ciliuml2announcementpolicies \
                       ciliumpodippools \
                       ciliumgatewayclassconfigs \
-                      ciliumnetworkdriverconfigs"
+                      ciliumnetworkdriverclusterconfigs \
+                      ciliumnetworkdrivernodeconfigs"
 
 TMPDIR=$(mktemp -d -t cilium.tmpXXXXXXXX)
 go tool sigs.k8s.io/controller-tools/cmd/controller-gen ${CRD_OPTIONS} paths="${CRD_PATHS}" output:crd:artifacts:config="${TMPDIR}"

install/kubernetes/cilium/Chart.yaml
@@ -169,9 +169,15 @@ annotations:
       displayName: Cilium Gateway Class Config
       description: |
         CiliumGatewayClassConfig defines a configuration for Gateway API GatewayClass.
-    - kind: CiliumNetworkDriverConfig
+    - kind: CiliumNetworkDriverNodeConfig
       version: v2alpha1
-      name: ciliumnetworkdriverconfigs.cilium.io
-      displayName: Cilium Network Driver Config
+      name: ciliumnetworkdrivernodeconfigs.cilium.io
+      displayName: Cilium Network Driver Node Config
       description: |
-        CiliumNetworkDriverConfig defines a configuration for the Cilium Network Driver module.
+        CiliumNetworkDriverNodeConfig defines a configuration for the Cilium Network Driver module on a single node.
+    - kind: CiliumNetworkDriverClusterConfig
+      version: v2alpha1
+      name: ciliumnetworkdriverclusterconfigs.cilium.io
+      displayName: Cilium Network Driver Cluster Config
+      description: |
+        CiliumNetworkDriverClusterConfig defines a configuration for the Cilium Network Driver module.

install/kubernetes/cilium/Chart.yaml-e
@@ -1,177 +1,0 @@
-apiVersion: v2
-name: cilium
-displayName: Cilium
-home: https://cilium.io/
-version: 1.19.0-dev
-appVersion: 1.19.0-dev
-kubeVersion: ">= 1.21.0-0"
-icon: https://cdn.jsdelivr.net/gh/cilium/cilium@main/Documentation/images/logo-solo.svg
-description: eBPF-based Networking, Security, and Observability
-keywords:
-  - BPF
-  - eBPF
-  - Kubernetes
-  - Networking
-  - Security
-  - Observability
-  - Troubleshooting
-sources:
-  - https://github.com/cilium/cilium
-links:
-  - name: eBPF.io
-    url: https://ebpf.io/
-annotations:
-  artifacthub.io/crds: |
-    - kind: CiliumNetworkPolicy
-      version: v2
-      name: ciliumnetworkpolicies.cilium.io
-      displayName: Cilium Network Policy
-      description: |
-        Cilium Network Policies provide additional functionality beyond what
-        is provided by standard Kubernetes NetworkPolicy such as the ability
-        to allow traffic based on FQDNs, or to filter at Layer 7.
-    - kind: CiliumClusterwideNetworkPolicy
-      version: v2
-      name: ciliumclusterwidenetworkpolicies.cilium.io
-      displayName: Cilium Clusterwide Network Policy
-      description: |
-        Cilium Clusterwide Network Policies support configuring network traffic
-        policiies across the entire cluster, including applying node firewalls.
-    - kind: CiliumLocalRedirectPolicy
-      version: v2
-      name: ciliumlocalredirectpolicies.cilium.io
-      displayName: Cilium Local Redirect Policy
-      description: |
-        Cilium Local Redirect Policy allows local redirects to be configured
-        within a node to support use cases like Node-Local DNS.
-    - kind: CiliumNode
-      version: v2
-      name: ciliumnodes.cilium.io
-      displayName: Cilium Node
-      description: |
-        Cilium Node represents a node managed by Cilium. It contains a
-        specification to control various node specific configuration aspects
-        and a status section to represent the status of the node.
-    - kind: CiliumIdentity
-      version: v2
-      name: ciliumidentities.cilium.io
-      displayName: Cilium Identity
-      description: |
-        Cilium Identity allows introspection into security identities that
-        Cilium allocates which identify sets of labels that are assigned to
-        individual endpoints in the cluster.
-    - kind: CiliumEndpoint
-      version: v2
-      name: ciliumendpoints.cilium.io
-      displayName: Cilium Endpoint
-      description: |
-        Cilium Endpoint represents the status of individual pods or nodes in
-        the cluster which are managed by Cilium, including enforcement status,
-        IP addressing and whether the networking is successfully operational.
-    - kind: CiliumEndpointSlice
-      version: v2alpha1
-      name: ciliumendpointslices.cilium.io
-      displayName: Cilium Endpoint Slice
-      description: |
-        Cilium Endpoint Slice represents the status of groups of pods or nodes
-        in the cluster which are managed by Cilium, including enforcement status,
-        IP addressing and whether the networking is successfully operational.
-    - kind: CiliumEgressGatewayPolicy
-      version: v2
-      name: ciliumegressgatewaypolicies.cilium.io
-      displayName: Cilium Egress Gateway Policy
-      description: |
-        Cilium Egress Gateway Policy provides control over the way that traffic
-        leaves the cluster and which source addresses to use for that traffic.
-    - kind: CiliumClusterwideEnvoyConfig
-      version: v2
-      name: ciliumclusterwideenvoyconfigs.cilium.io
-      displayName: Cilium Clusterwide Envoy Config
-      description: |
-        Cilium Clusterwide Envoy Config specifies Envoy resources and K8s service mappings
-        to be provisioned into Cilium host proxy instances in cluster context.
-    - kind: CiliumEnvoyConfig
-      version: v2
-      name: ciliumenvoyconfigs.cilium.io
-      displayName: Cilium Envoy Config
-      description: |
-        Cilium Envoy Config specifies Envoy resources and K8s service mappings
-        to be provisioned into Cilium host proxy instances in namespace context.
-    - kind: CiliumNodeConfig
-      version: v2
-      name: ciliumnodeconfigs.cilium.io
-      displayName: Cilium Node Configuration
-      description: |
-        CiliumNodeConfig is a list of configuration key-value pairs. It is applied to
-        nodes indicated by a label selector.
-    - kind: CiliumBGPClusterConfig
-      version: v2alpha1
-      name: ciliumbgpclusterconfigs.cilium.io
-      displayName: Cilium BGP Cluster Config
-      description: |
-        Cilium BGP Cluster Config instructs Cilium operator to create specific BGP cluster
-        configurations.
-    - kind: CiliumBGPPeerConfig
-      version: v2alpha1
-      name: ciliumbgppeerconfigs.cilium.io
-      displayName: Cilium BGP Peer Config
-      description: |
-        CiliumBGPPeerConfig is a common set of BGP peer configurations. It can be referenced 
-        by multiple peers from CiliumBGPClusterConfig.
-    - kind: CiliumBGPAdvertisement
-      version: v2alpha1
-      name: ciliumbgpadvertisements.cilium.io
-      displayName: Cilium BGP Advertisement
-      description: |
-        CiliumBGPAdvertisement is used to define source of BGP advertisement as well as BGP attributes 
-        to be advertised with those prefixes.
-    - kind: CiliumBGPNodeConfig
-      version: v2alpha1
-      name: ciliumbgpnodeconfigs.cilium.io
-      displayName: Cilium BGP Node Config
-      description: |
-        CiliumBGPNodeConfig is read only node specific BGP configuration. It is constructed by Cilium operator.
-        It will also contain node local BGP state information.
-    - kind: CiliumBGPNodeConfigOverride
-      version: v2alpha1
-      name: ciliumbgpnodeconfigoverrides.cilium.io
-      displayName: Cilium BGP Node Config Override
-      description: |
-        CiliumBGPNodeConfigOverride can be used to override node specific BGP configuration.
-    - kind: CiliumLoadBalancerIPPool
-      version: v2
-      name: ciliumloadbalancerippools.cilium.io
-      displayName: Cilium Load Balancer IP Pool
-      description: |
-        Defining a Cilium Load Balancer IP Pool instructs Cilium to assign IPs to LoadBalancer Services.
-    - kind: CiliumCIDRGroup
-      version: v2alpha1
-      name: ciliumcidrgroups.cilium.io
-      displayName: Cilium CIDR Group
-      description: |
-        CiliumCIDRGroup is a list of CIDRs that can be referenced as a single entity from CiliumNetworkPolicies.
-    - kind: CiliumL2AnnouncementPolicy
-      version: v2alpha1
-      name: ciliuml2announcementpolicies.cilium.io
-      displayName: Cilium L2 Announcement Policy
-      description: |
-        CiliumL2AnnouncementPolicy is a policy which determines which service IPs will be announced to
-        the local area network, by which nodes, and via which interfaces.
-    - kind: CiliumPodIPPool
-      version: v2alpha1
-      name: ciliumpodippools.cilium.io
-      displayName: Cilium Pod IP Pool
-      description: |
-        CiliumPodIPPool defines an IP pool that can be used for pooled IPAM (i.e. the multi-pool IPAM mode).
-    - kind: CiliumGatewayClassConfig
-      version: v2alpha1
-      name: ciliumgatewayclassconfigs.cilium.io
-      displayName: Cilium Gateway Class Config
-      description: |
-        CiliumGatewayClassConfig defines a configuration for Gateway API GatewayClass.
-    - kind: CiliumNetworkDriverConfig
-      version: v2alpha1
-      name: ciliumnetworkdriverconfigs.cilium.io
-      displayName: Cilium Network Driver Config
-      description: |
-        CiliumNetworkDriverConfig defines a configuration for the Cilium Network Driver module.

install/kubernetes/cilium/Chart.yaml-e-e
@@ -1,177 +1,0 @@
-apiVersion: v2
-name: cilium
-displayName: Cilium
-home: https://cilium.io/
-version: 1.19.0-dev
-appVersion: 1.19.0-dev
-kubeVersion: ">= 1.21.0-0"
-icon: https://cdn.jsdelivr.net/gh/cilium/cilium@main/Documentation/images/logo-solo.svg
-description: eBPF-based Networking, Security, and Observability
-keywords:
-  - BPF
-  - eBPF
-  - Kubernetes
-  - Networking
-  - Security
-  - Observability
-  - Troubleshooting
-sources:
-  - https://github.com/cilium/cilium
-links:
-  - name: eBPF.io
-    url: https://ebpf.io/
-annotations:
-  artifacthub.io/crds: |
-    - kind: CiliumNetworkPolicy
-      version: v2
-      name: ciliumnetworkpolicies.cilium.io
-      displayName: Cilium Network Policy
-      description: |
-        Cilium Network Policies provide additional functionality beyond what
-        is provided by standard Kubernetes NetworkPolicy such as the ability
-        to allow traffic based on FQDNs, or to filter at Layer 7.
-    - kind: CiliumClusterwideNetworkPolicy
-      version: v2
-      name: ciliumclusterwidenetworkpolicies.cilium.io
-      displayName: Cilium Clusterwide Network Policy
-      description: |
-        Cilium Clusterwide Network Policies support configuring network traffic
-        policiies across the entire cluster, including applying node firewalls.
-    - kind: CiliumLocalRedirectPolicy
-      version: v2
-      name: ciliumlocalredirectpolicies.cilium.io
-      displayName: Cilium Local Redirect Policy
-      description: |
-        Cilium Local Redirect Policy allows local redirects to be configured
-        within a node to support use cases like Node-Local DNS.
-    - kind: CiliumNode
-      version: v2
-      name: ciliumnodes.cilium.io
-      displayName: Cilium Node
-      description: |
-        Cilium Node represents a node managed by Cilium. It contains a
-        specification to control various node specific configuration aspects
-        and a status section to represent the status of the node.
-    - kind: CiliumIdentity
-      version: v2
-      name: ciliumidentities.cilium.io
-      displayName: Cilium Identity
-      description: |
-        Cilium Identity allows introspection into security identities that
-        Cilium allocates which identify sets of labels that are assigned to
-        individual endpoints in the cluster.
-    - kind: CiliumEndpoint
-      version: v2
-      name: ciliumendpoints.cilium.io
-      displayName: Cilium Endpoint
-      description: |
-        Cilium Endpoint represents the status of individual pods or nodes in
-        the cluster which are managed by Cilium, including enforcement status,
-        IP addressing and whether the networking is successfully operational.
-    - kind: CiliumEndpointSlice
-      version: v2alpha1
-      name: ciliumendpointslices.cilium.io
-      displayName: Cilium Endpoint Slice
-      description: |
-        Cilium Endpoint Slice represents the status of groups of pods or nodes
-        in the cluster which are managed by Cilium, including enforcement status,
-        IP addressing and whether the networking is successfully operational.
-    - kind: CiliumEgressGatewayPolicy
-      version: v2
-      name: ciliumegressgatewaypolicies.cilium.io
-      displayName: Cilium Egress Gateway Policy
-      description: |
-        Cilium Egress Gateway Policy provides control over the way that traffic
-        leaves the cluster and which source addresses to use for that traffic.
-    - kind: CiliumClusterwideEnvoyConfig
-      version: v2
-      name: ciliumclusterwideenvoyconfigs.cilium.io
-      displayName: Cilium Clusterwide Envoy Config
-      description: |
-        Cilium Clusterwide Envoy Config specifies Envoy resources and K8s service mappings
-        to be provisioned into Cilium host proxy instances in cluster context.
-    - kind: CiliumEnvoyConfig
-      version: v2
-      name: ciliumenvoyconfigs.cilium.io
-      displayName: Cilium Envoy Config
-      description: |
-        Cilium Envoy Config specifies Envoy resources and K8s service mappings
-        to be provisioned into Cilium host proxy instances in namespace context.
-    - kind: CiliumNodeConfig
-      version: v2
-      name: ciliumnodeconfigs.cilium.io
-      displayName: Cilium Node Configuration
-      description: |
-        CiliumNodeConfig is a list of configuration key-value pairs. It is applied to
-        nodes indicated by a label selector.
-    - kind: CiliumBGPClusterConfig
-      version: v2alpha1
-      name: ciliumbgpclusterconfigs.cilium.io
-      displayName: Cilium BGP Cluster Config
-      description: |
-        Cilium BGP Cluster Config instructs Cilium operator to create specific BGP cluster
-        configurations.
-    - kind: CiliumBGPPeerConfig
-      version: v2alpha1
-      name: ciliumbgppeerconfigs.cilium.io
-      displayName: Cilium BGP Peer Config
-      description: |
-        CiliumBGPPeerConfig is a common set of BGP peer configurations. It can be referenced 
-        by multiple peers from CiliumBGPClusterConfig.
-    - kind: CiliumBGPAdvertisement
-      version: v2alpha1
-      name: ciliumbgpadvertisements.cilium.io
-      displayName: Cilium BGP Advertisement
-      description: |
-        CiliumBGPAdvertisement is used to define source of BGP advertisement as well as BGP attributes 
-        to be advertised with those prefixes.
-    - kind: CiliumBGPNodeConfig
-      version: v2alpha1
-      name: ciliumbgpnodeconfigs.cilium.io
-      displayName: Cilium BGP Node Config
-      description: |
-        CiliumBGPNodeConfig is read only node specific BGP configuration. It is constructed by Cilium operator.
-        It will also contain node local BGP state information.
-    - kind: CiliumBGPNodeConfigOverride
-      version: v2alpha1
-      name: ciliumbgpnodeconfigoverrides.cilium.io
-      displayName: Cilium BGP Node Config Override
-      description: |
-        CiliumBGPNodeConfigOverride can be used to override node specific BGP configuration.
-    - kind: CiliumLoadBalancerIPPool
-      version: v2
-      name: ciliumloadbalancerippools.cilium.io
-      displayName: Cilium Load Balancer IP Pool
-      description: |
-        Defining a Cilium Load Balancer IP Pool instructs Cilium to assign IPs to LoadBalancer Services.
-    - kind: CiliumCIDRGroup
-      version: v2alpha1
-      name: ciliumcidrgroups.cilium.io
-      displayName: Cilium CIDR Group
-      description: |
-        CiliumCIDRGroup is a list of CIDRs that can be referenced as a single entity from CiliumNetworkPolicies.
-    - kind: CiliumL2AnnouncementPolicy
-      version: v2alpha1
-      name: ciliuml2announcementpolicies.cilium.io
-      displayName: Cilium L2 Announcement Policy
-      description: |
-        CiliumL2AnnouncementPolicy is a policy which determines which service IPs will be announced to
-        the local area network, by which nodes, and via which interfaces.
-    - kind: CiliumPodIPPool
-      version: v2alpha1
-      name: ciliumpodippools.cilium.io
-      displayName: Cilium Pod IP Pool
-      description: |
-        CiliumPodIPPool defines an IP pool that can be used for pooled IPAM (i.e. the multi-pool IPAM mode).
-    - kind: CiliumGatewayClassConfig
-      version: v2alpha1
-      name: ciliumgatewayclassconfigs.cilium.io
-      displayName: Cilium Gateway Class Config
-      description: |
-        CiliumGatewayClassConfig defines a configuration for Gateway API GatewayClass.
-    - kind: CiliumNetworkDriverConfig
-      version: v2alpha1
-      name: ciliumnetworkdriverconfigs.cilium.io
-      displayName: Cilium Network Driver Config
-      description: |
-        CiliumNetworkDriverConfig defines a configuration for the Cilium Network Driver module.

install/kubernetes/cilium/templates/cilium-agent/clusterrole.yaml
@@ -110,7 +110,7 @@ rules:
   - ciliumcidrgroups
   - ciliuml2announcementpolicies
   - ciliumpodippools
-  - ciliumnetworkdriverconfigs
+  - ciliumnetworkdrivernodeconfigs
   verbs:
   - list
   - watch

commit 80d3c882b9cd05ece91e2bca04ea84b9890b0eed

k8s: Add registration of CiliumNetworkDriverNodeConfigList
CiliumNetworkDriverNodeConfigList was not listed among the known types
and therefore it was not registered by the operator.

Fixes: 6ba13948d4 ("network driver: add CRDs for cluster and node configs")

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

pkg/k8s/apis/cilium.io/v2alpha1/register.go
@@ -194,6 +194,7 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&CiliumNetworkDriverClusterConfig{},
 		&CiliumNetworkDriverClusterConfigList{},
 		&CiliumNetworkDriverNodeConfig{},
+		&CiliumNetworkDriverNodeConfigList{},
 	)
 
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)