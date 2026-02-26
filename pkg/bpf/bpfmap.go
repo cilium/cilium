3bbd1a5c0d8a57904543d90d676eaf5b0fab60bc
Timo Beckers <timo@isovalent.com>
2026-02-26 09:25:15 +0100

    bpf: simplify, document and test MapOut
    The BpfMap type is not strictly necessary, the type of the provided value is
    not relevant as long as it's assignable to the receiving []any (which all types
    are).
    
    Also add the bpf.MapGroup type to implement map plumbing in a single place,
    along with a test that makes sure the value group behaves as expected.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/bpf/bpfmap.go
+++ pkg/bpf/bpfmap.go
@@ -5,20 +5,32 @@ package bpf
 
 import "github.com/cilium/hive/cell"
 
-// BpfMap defines the base interface every BPF map needs to implement.
-//
-// Its main purpose is to register a BPF map via value group `bpf-maps`. See [MapOut].
-type BpfMap any
-
-// MapOut ensures that maps are created before the datapath loader
-// is invoked.
+// MapOut ensures that BPF maps are created before the datapath loader Cell is
+// invoked.
 type MapOut[T any] struct {
 	cell.Out
 
-	Map    T
-	BpfMap BpfMap `group:"bpf-maps"`
+	Map T
+
+	// loader.Cell depends on the bpf-maps value group to ensure it is started
+	// after all maps have been started. Values provided to a value group cannot
+	// be received as regular Cell arguments, so we need to output it twice.
+	Group any `group:"bpf-maps"`
 }
 
+// NewMapOut returns a MapOut containing the provided map. Use this in the
+// Provide function of a Cell to make a BPF map available to other Cells.
 func NewMapOut[T any](m T) MapOut[T] {
-	return MapOut[T]{Map: m, BpfMap: m}
+	return MapOut[T]{Map: m, Group: m}
+}
+
+// MapGroup is used as a Cell argument to depend on all BPF maps created by the
+// agent.
+//
+// Depend on this type if you want to ensure that your Cell is started after all
+// BPF maps have been created.
+type MapGroup struct {
+	cell.In
+
+	Group []any `group:"bpf-maps"`
 }
