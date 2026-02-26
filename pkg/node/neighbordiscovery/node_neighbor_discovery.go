cilium/cilium

commit ccb7bd1ece9abc7dbcbab88d9c54606e19c20069

pkg/node/neighbordiscovery: Add node handler for neighbor discovery
This commit hooks the new neighbor subsystem up to the node manager.
We do this by create a new `NodeHandler` called `nodeNeighborHandler`.
This handler gets registered with the node manager so we get informed
when nodes are added or removed. This is similar to the existing
mechanism but no longer part of the overloaded `linuxNodeHandler`.

Node add/remove events are translated into forwardable IPs. The
`nodeNeighborHandler` is also a `NodeConfigChangeHandler` and is
registered at the `NodeConfigNotifier`. The `NodeConfigurationChanged`
method is called when the loader reinitialized the datapath. And we
use this as a proxy to know that all "nodes" have been added.
This isn't ideal, but the same mechanism currently used by
`linuxNodeHandler` because there is no better signal available at
the moment.

Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

pkg/node/neighbordiscovery/node_neighbor_discovery.go
@@ -1,0 +1,208 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package neighbordiscovery
+
+import (
+	"fmt"
+	"net/netip"
+
+	"github.com/cilium/hive/cell"
+
+	"github.com/cilium/cilium/pkg/datapath/neighbor"
+	datapath "github.com/cilium/cilium/pkg/datapath/types"
+	"github.com/cilium/cilium/pkg/node/manager"
+	nodeTypes "github.com/cilium/cilium/pkg/node/types"
+	"github.com/cilium/cilium/pkg/option"
+)
+
+var Cell = cell.Module(
+	"node-neighbor-discovery",
+	"A node handler that manages forwardable IPs for nodes in the cluster",
+	cell.Invoke(NewNodeNeighborHandler),
+)
+
+var (
+	_ datapath.NodeHandler             = (*nodeNeighborHandler)(nil)
+	_ datapath.NodeConfigChangeHandler = (*nodeNeighborHandler)(nil)
+)
+
+// NewNodeNeighborHandler initializes the node neighbor handler and
+// subscribes it to the node manager if L2 neighbor discovery is enabled.
+// It will add forwardable IPs for all nodes in the cluster to the
+func NewNodeNeighborHandler(
+	nodeManger manager.NodeManager,
+	forwardableIPManager *neighbor.ForwardableIPManager,
+	deamonConfig *option.DaemonConfig,
+	nodeConfigNotifier *manager.NodeConfigNotifier,
+) {
+	// If the forwardable IP manager is not enabled, then there is no point to
+	// doing any work here.
+	if !forwardableIPManager.Enabled() {
+		return
+	}
+
+	nnh := &nodeNeighborHandler{
+		forwardableIPManager: forwardableIPManager,
+		initializer:          forwardableIPManager.RegisterInitializer("node-neighbor-discovery"),
+	}
+
+	if deamonConfig.EnableL2NeighDiscovery {
+		nodeManger.Subscribe(nnh)
+		nodeConfigNotifier.Subscribe(nnh)
+	}
+}
+
+type nodeNeighborHandler struct {
+	forwardableIPManager *neighbor.ForwardableIPManager
+	initializer          neighbor.ForwardableIPInitializer
+}
+
+// Name identifies the handler, this is used in logging/reporting handler
+// reconciliation errors.
+func (nnh *nodeNeighborHandler) Name() string {
+	return "node-neighbor-handler"
+}
+
+// NodeAdd is called when a node is discovered for the first time.
+func (nnh *nodeNeighborHandler) NodeAdd(newNode nodeTypes.Node) error {
+	// We only want to add forwardable IPs for nodes that are not local.
+	if newNode.IsLocal() {
+		return nil
+	}
+
+	if newNode.GetNodeIP(false).To4() != nil {
+		ipv4, ok := netip.AddrFromSlice(newNode.GetNodeIP(false).To4())
+		if ok {
+			err := nnh.forwardableIPManager.Insert(
+				ipv4,
+				neighbor.ForwardableIPOwner{
+					Type: neighbor.ForwardableIPOwnerNode,
+					ID:   newNode.Identity().String(),
+				},
+			)
+			if err != nil {
+				return fmt.Errorf("failed to insert forwardable IP for node %s: %w", newNode.Name, err)
+			}
+		}
+	}
+
+	if newNode.GetNodeIP(true).To16() != nil {
+		ipv4, ok := netip.AddrFromSlice(newNode.GetNodeIP(true).To16())
+		if ok {
+			err := nnh.forwardableIPManager.Insert(
+				ipv4,
+				neighbor.ForwardableIPOwner{
+					Type: neighbor.ForwardableIPOwnerNode,
+					ID:   newNode.Identity().String(),
+				},
+			)
+			if err != nil {
+				return fmt.Errorf("failed to insert forwardable IP for node %s: %w", newNode.Name, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+// NodeUpdate is called when a node definition changes. Both the old
+// and new node definition is provided. NodeUpdate() is never called
+// before NodeAdd() is called for a particular node.
+func (nnh *nodeNeighborHandler) NodeUpdate(oldNode, newNode nodeTypes.Node) error {
+	// We only want to add forwardable IPs for nodes that are not local.
+	if oldNode.IsLocal() {
+		return nil
+	}
+
+	// We only care if the node name or IP address has changed.
+	if oldNode.Identity().String() != newNode.Identity().String() ||
+		!oldNode.GetNodeIP(false).To4().Equal(newNode.GetNodeIP(false).To4()) ||
+		!oldNode.GetNodeIP(false).To16().Equal(newNode.GetNodeIP(false).To16()) {
+
+		if err := nnh.NodeDelete(oldNode); err != nil {
+			return fmt.Errorf("failed to delete old node %s: %w", oldNode.Name, err)
+		}
+
+		if err := nnh.NodeAdd(newNode); err != nil {
+			return fmt.Errorf("failed to add new node %s: %w", newNode.Name, err)
+		}
+	}
+
+	return nil
+}
+
+// NodeDelete is called after a node has been deleted
+func (nnh *nodeNeighborHandler) NodeDelete(node nodeTypes.Node) error {
+	// We only want to add forwardable IPs for nodes that are not local.
+	if node.IsLocal() {
+		return nil
+	}
+
+	if node.GetNodeIP(false).To4() != nil {
+		ipv4, ok := netip.AddrFromSlice(node.GetNodeIP(false).To4())
+		if ok {
+			err := nnh.forwardableIPManager.Delete(
+				ipv4,
+				neighbor.ForwardableIPOwner{
+					Type: neighbor.ForwardableIPOwnerNode,
+					ID:   node.Name,
+				},
+			)
+			if err != nil {
+				return fmt.Errorf("failed to delete forwardable IP for node %s: %w", node.Name, err)
+			}
+		}
+	}
+
+	if node.GetNodeIP(false).To16() != nil {
+		ipv4, ok := netip.AddrFromSlice(node.GetNodeIP(false).To16())
+		if ok {
+			err := nnh.forwardableIPManager.Insert(
+				ipv4,
+				neighbor.ForwardableIPOwner{
+					Type: neighbor.ForwardableIPOwnerNode,
+					ID:   node.Name,
+				},
+			)
+			if err != nil {
+				return fmt.Errorf("failed to delete forwardable IP for node %s: %w", node.Name, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+// AllNodeValidateImplementation is called to validate the implementation
+// of all nodes in the node cache.
+func (nnh *nodeNeighborHandler) AllNodeValidateImplementation() {
+	// This is a no-op for the node neighbor handler.
+}
+
+// NodeValidateImplementation is called to validate the implementation of
+// the node in the datapath. This function is intended to be run on an
+// interval to ensure that the datapath is consistently converged.
+func (nnh *nodeNeighborHandler) NodeValidateImplementation(node nodeTypes.Node) error {
+	// We only want to add forwardable IPs for nodes that are not local.
+	if node.IsLocal() {
+		return nil
+	}
+
+	// [nodeNeighborHandler.NodeAdd] is idempotent, so a forwardable IP already exists
+	// this is a no-op. If the forwardable IP does not exist, it will be created.
+	return nnh.NodeAdd(node)
+}
+
+// NodeConfigurationChanged is called when the local node configuration
+// has changed
+func (nnh *nodeNeighborHandler) NodeConfigurationChanged(config datapath.LocalNodeConfiguration) error {
+
+	// `NodeConfigurationChanged` is called by the loader when the datapath is initialized.
+	// We use this event as a signal that `NodeAdd` should have been called for all nodes
+	// we should know about and that we can consider our entries in the forwardable IP manager
+	// as initialized.
+	nnh.forwardableIPManager.FinishInitializer(nnh.initializer)
+
+	return nil
+}

CODEOWNERS
@@ -597,6 +597,7 @@ Makefile* @cilium/build
 /pkg/murmur3/ @cilium/sig-datapath
 /pkg/netns/ @cilium/sig-datapath @cilium/sig-k8s
 /pkg/node @cilium/sig-agent
+/pkg/node/neighbordiscovery @cilium/sig-agent @cilium/sig-datapath
 /pkg/nodediscovery/ @cilium/sig-agent
 /pkg/option @cilium/sig-agent @cilium/cli
 /pkg/pidfile @cilium/sig-agent

daemon/cmd/cells.go
@@ -62,6 +62,7 @@ import (
 	"github.com/cilium/cilium/pkg/metrics/features"
 	"github.com/cilium/cilium/pkg/node"
 	nodeManager "github.com/cilium/cilium/pkg/node/manager"
+	"github.com/cilium/cilium/pkg/node/neighbordiscovery"
 	"github.com/cilium/cilium/pkg/nodediscovery"
 	"github.com/cilium/cilium/pkg/option"
 	policy "github.com/cilium/cilium/pkg/policy/cell"
@@ -190,6 +191,11 @@ var (
 
 		// NodeManager maintains a collection of other nodes in the cluster.
 		nodeManager.Cell,
+
+		// NodeNeighborDiscovery is a node handler that subscribes to the NodeManager
+		// and ensures node IPs are "forwardable" by adding them to the forwardable IP table.
+		// The neighbor subsystem will create neighbor entries for these forwardable IPs.
+		neighbordiscovery.Cell,
 
 		// Certificate manager provides an API for retrieving secrets and certificate in the form of TLS contexts.
 		certificatemanager.Cell,

daemon/cmd/daemon_test.go
@@ -22,6 +22,7 @@ import (
 	fakecni "github.com/cilium/cilium/daemon/cmd/cni/fake"
 	"github.com/cilium/cilium/pkg/controller"
 	fakeDatapath "github.com/cilium/cilium/pkg/datapath/fake"
+	"github.com/cilium/cilium/pkg/datapath/neighbor"
 	"github.com/cilium/cilium/pkg/datapath/prefilter"
 	"github.com/cilium/cilium/pkg/dial"
 	endpointapi "github.com/cilium/cilium/pkg/endpoint/api"
@@ -129,6 +130,8 @@ func setupDaemonEtcdSuite(tb testing.TB) *DaemonSuite {
 			func() *server.Server { return nil },
 		),
 		fakeDatapath.Cell,
+		neighbor.ForwardableIPCell,
+		cell.Provide(neighbor.NewCommonTestConfig(true, false)),
 		prefilter.Cell,
 		monitorAgent.Cell,
 		dial.ServiceResolverCell,

test/controlplane/suite/agent.go
@@ -19,6 +19,7 @@ import (
 	fakecni "github.com/cilium/cilium/daemon/cmd/cni/fake"
 	fakeDatapath "github.com/cilium/cilium/pkg/datapath/fake"
 	fakeTypes "github.com/cilium/cilium/pkg/datapath/fake/types"
+	"github.com/cilium/cilium/pkg/datapath/neighbor"
 	"github.com/cilium/cilium/pkg/datapath/prefilter"
 	datapathTables "github.com/cilium/cilium/pkg/datapath/tables"
 	"github.com/cilium/cilium/pkg/dial"
@@ -85,6 +86,8 @@ func (h *agentHandle) setupCiliumAgentHive(clientset k8sClient.Clientset, extraC
 		),
 		kvstore.Cell(kvstore.DisabledBackendName),
 		fakeDatapath.Cell,
+		neighbor.ForwardableIPCell,
+		cell.Provide(neighbor.NewCommonTestConfig(true, false)),
 		prefilter.Cell,
 		monitorAgent.Cell,
 		metrics.Cell,

commit 9d1768bd06511bdd341d74e4f681c7868a51a9a6

node/neighbordiscovery: correctly update and delete node IPv6 addresses
The logic in (*nodeNeighborHandler).NodeUpdate and
(*nodeNeighborHandler).NodeDelete treats the node IPv4 address
retrieved using node.GetNodeIP(false) (where false means ipv6=false) as
an IPv6 address. This will lead to the address not correctly compared
and propagated to the forwardableIPManager.

Correct this by retrieving the node IPv6 address consistently for the
IPv6 paths.

Also rename an incorrectly named local variable in
(*nodeNeighborHandler).NodeAdd while at it.

Fixes: ccb7bd1ece9a ("pkg/node/neighbordiscovery: Add node handler for neighbor discovery")
Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/node/neighbordiscovery/node_neighbor_discovery.go
@@ -84,10 +84,10 @@ func (nnh *nodeNeighborHandler) NodeAdd(newNode nodeTypes.Node) error {
 	}
 
 	if newNode.GetNodeIP(true).To16() != nil {
-		ipv4, ok := netip.AddrFromSlice(newNode.GetNodeIP(true).To16())
+		ipv6, ok := netip.AddrFromSlice(newNode.GetNodeIP(true).To16())
 		if ok {
 			err := nnh.forwardableIPManager.Insert(
-				ipv4,
+				ipv6,
 				neighbor.ForwardableIPOwner{
 					Type: neighbor.ForwardableIPOwnerNode,
 					ID:   newNode.Identity().String(),
@@ -114,7 +114,7 @@ func (nnh *nodeNeighborHandler) NodeUpdate(oldNode, newNode nodeTypes.Node) erro
 	// We only care if the node name or IP address has changed.
 	if oldNode.Identity().String() != newNode.Identity().String() ||
 		!oldNode.GetNodeIP(false).To4().Equal(newNode.GetNodeIP(false).To4()) ||
-		!oldNode.GetNodeIP(false).To16().Equal(newNode.GetNodeIP(false).To16()) {
+		!oldNode.GetNodeIP(true).To16().Equal(newNode.GetNodeIP(true).To16()) {
 
 		if err := nnh.NodeDelete(oldNode); err != nil {
 			return fmt.Errorf("failed to delete old node %s: %w", oldNode.Name, err)
@@ -151,11 +151,11 @@ func (nnh *nodeNeighborHandler) NodeDelete(node nodeTypes.Node) error {
 		}
 	}
 
-	if node.GetNodeIP(false).To16() != nil {
-		ipv4, ok := netip.AddrFromSlice(node.GetNodeIP(false).To16())
+	if node.GetNodeIP(true).To16() != nil {
+		ipv6, ok := netip.AddrFromSlice(node.GetNodeIP(true).To16())
 		if ok {
-			err := nnh.forwardableIPManager.Insert(
-				ipv4,
+			err := nnh.forwardableIPManager.Delete(
+				ipv6,
 				neighbor.ForwardableIPOwner{
 					Type: neighbor.ForwardableIPOwnerNode,
 					ID:   node.Name,