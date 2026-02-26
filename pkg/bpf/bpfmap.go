8a630a0cac583ec8fa2d8bb58b0d7208a798a77d
Timo Beckers <timo@isovalent.com>
2026-02-26 09:37:09 +0100

    bpf,hive: hive.Optional and bpf.MaybeMapOut types for optional maps
    While porting the bwmap cell to use the new MapSpec registry, a few challenges
    became apparent:
    
    1. There is no longer a MapSpec available in the constructor/provider
    2. The map does not get created when a certain cfg is false.
    3. When the map is not provided, its reconciler doesn't need to be started
    4. When the map is not provided, table pressure metrics don't need to be collected
    
    With the addition of point 1., a bpf.Map can only be instantiated in an OnStart
    hook, since the map registry is write-only during Hive creation and only becomes
    readable during OnStart. Unfortunately, the constructor still needs to return a
    map wrapper during Provide (a *throttleMap in this case), which will contain a
    nil *bpf.Map until the Hive has been started.
    
    Due to the above factors, the programmer has to be _very_ careful accessing
    pointers, since *throttleMap can be nil, as well as its enclosed *bpf.Map, but
    only before OnStart(). This commit won't fix all hazards, but it will prevent
    one class of bugs due to misunderstanding provided values: optionality can be made
    explicit.
    
    This allows common infrastructure cells (reconcilers and e.g. pressure metrics)
    to disable themselves if certain arguments were not populated at runtime, as well
    as making for much more straightforward code within modules if e.g. certain invoke
    functions depend on a map being enabled.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/bpf/bpfmap.go
+++ pkg/bpf/bpfmap.go
@@ -3,7 +3,11 @@
 
 package bpf
 
-import "github.com/cilium/hive/cell"
+import (
+	"github.com/cilium/hive/cell"
+
+	"github.com/cilium/cilium/pkg/hive"
+)
 
 // MapOut ensures that BPF maps are created before the datapath loader Cell is
 // invoked.
@@ -33,4 +37,51 @@ type MapGroup struct {
 	cell.In
 
 	Group []any `group:"bpf-maps"`
+}
+
+// MaybeMapOut is returned by a Provide function when a map is only created when
+// certain features are enabled. Use [MaybeMap] to receive an optional map in
+// another Cell.
+type MaybeMapOut[T any] struct {
+	cell.Out
+
+	Map hive.Optional[T]
+
+	Group any `group:"bpf-maps"`
+}
+
+// SomeMap returns a [MaybeMapOut] containing the provided map. Use this in the
+// Provide function of a Cell to make a BPF map available to other Cells.
+//
+// This is used when a map is only created when certain features are enabled. If
+// the map is not enabled, return [NoneMap] instead.
+func SomeMap[T any](m T) MaybeMapOut[T] {
+	om := hive.Some(m)
+	return MaybeMapOut[T]{Map: om, Group: om}
+}
+
+// NoneMap returns an empty [MaybeMapOut]. Return this instead of nil from the
+// Provide function of a Cell to indicate that a BPF map was disabled by runtime
+// configuration.
+func NoneMap[T any]() MaybeMapOut[T] {
+	n := hive.None[T]()
+	return MaybeMapOut[T]{Map: n, Group: n}
+}
+
+// MaybeMap is used to receive BPF maps provided by other Cells using
+// [MaybeMapOut]. Some maps are only provided when certain features are enabled.
+//
+// Call [MaybeMap.Get] and check if the returned boolean value is true before
+// accessing the first return value.
+type MaybeMap[T any] struct {
+	cell.In
+
+	// Do not access this field directly, use [MaybeMap.Get].
+	hive.Optional[T]
+}
+
+// Get returns the map provided by another Cell and a boolean value indicating
+// whether the map is enabled or not.
+func (mi MaybeMap[T]) Get() (T, bool) {
+	return mi.Optional.Get()
 }
