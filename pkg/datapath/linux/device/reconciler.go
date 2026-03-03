f18d44c7de6866925757af58e38bac7f825c6768
Timo Beckers <timo@isovalent.com>
2026-03-03 15:22:18 +0100

    safenetlink: replace package with package inl
    This commit removes package safenetlink, and adds a small wrapper package for
    netlink.NewHandle that takes care of setting defaults. The forbidigo config
    is update to enforce usage of inl.NewHandle() over netlink.NewHandle().
    
    Lastly, add a testutils/inl subpackage with a NetNSHandle function for easy
    Handle creation in tests.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/datapath/linux/device/reconciler.go
+++ pkg/datapath/linux/device/reconciler.go
@@ -18,6 +18,8 @@ import (
 	"github.com/vishvananda/netlink"
 
 	"github.com/cilium/cilium/pkg/container/set"
+
+	"github.com/cilium/cilium/pkg/datapath/inl"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/time"
@@ -76,7 +78,7 @@ func newOps(
 
 func (ops *ops) Start(_ cell.HookContext) error {
 	var err error
-	ops.handle, err = netlink.NewHandle()
+	ops.handle, err = inl.NewHandle()
 	if err != nil {
 		return err
 	}
