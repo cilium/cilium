cilium/cilium

commit e9ede604490b6d63f327fb1dc28c101bd4eb63a3

bpf: implement 'dpgen maps' command and MapSpec registry
This commit introduces a MapSpec registry, populated with all maps
defined in the datapath at compile time. During agent startup, any Cell
can modify any MapSpec, typically to change MaxEntries based on
configuration. Any time a Map gets instantiated, the central MapSpec is
used.

In the current approach, Cell authors have to manually generate MapSpecs
based on runtime changes. This introduces inconsistencies between the
C definition and the one created in Go by the agent.

This new approach uses generated Go code to mirror the C MapSpec,
including BTF type information. Once the registry has finished its
initialization at runtime, changes can no longer be made, so all Cells,
the datapath and the loader have the same view of the world.

This introduces a single source of truth for map definitions, providing
a single mechanism of making runtime changes, as well as opening up
modifications to any Cell, not just the one responsible for creating the
map.

Signed-off-by: Timo Beckers <timo@isovalent.com>
Co-authored-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

tools/dpgen/main.go
@@ -4,83 +4,101 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"strings"
 
-	"github.com/cilium/ebpf"
+	"github.com/spf13/cobra"
 )
 
-var path, kind, name, embed, out string
-var embeds []string
-
-func init() {
-	flag.StringVar(&path, "path", "", "path to the eBPF collection")
-	flag.StringVar(&out, "out", "", "output Go file for the generated config struct")
-	flag.StringVar(&kind, "kind", "", "include variables with a decl tag matching 'kind:$kind' (typically object or node)")
-	flag.StringVar(&name, "name", "", "name of the generated Go struct")
-	flag.StringVar(&embed, "embed", "", "comma-separated list of structs to embed")
-
-	flag.Parse()
-
-	if embed != "" {
-		embeds = strings.Split(embed, ",")
-	}
-
-	validate()
-}
+var (
+	goPkg string
+)
 
 func main() {
-	spec, err := ebpf.LoadCollectionSpec(path)
-	if err != nil {
-		errexit(fmt.Errorf("loading spec: %w", err))
+	var rootCmd = &cobra.Command{
+		Use:   "dpgen",
+		Short: "dpgen generates Go code from eBPF datapath objects",
 	}
 
-	comment := fmt.Sprintf("%s is a configuration struct for a Cilium datapath object. "+
-		"Warning: do not instantiate directly! Always use [New%s] to ensure the default "+
-		"values configured in the ELF are honored.", name, name)
-	s, err := varsToStruct(spec, name, kind, comment, embeds)
-	if err != nil {
-		errexit(fmt.Errorf("generating config struct: %w", err))
-	}
+	rootCmd.AddCommand(configCmd())
+	rootCmd.AddCommand(mapsCmd())
 
-	var b strings.Builder
-	b.WriteString("// Code generated by dpgen. DO NOT EDIT.\n")
-	b.WriteString("\n")
-	b.WriteString("// SPDX-License-Identifier: Apache-2.0\n")
-	b.WriteString("// Copyright Authors of Cilium\n")
-	b.WriteString("\n")
-	b.WriteString("package config\n")
-	b.WriteString("\n")
-	b.WriteString(s)
+	flags := rootCmd.Flags()
+	flags.StringVarP(&goPkg, "package", "p", os.Getenv("GOPACKAGE"), "name of the Go package dpgen was invoked for/from")
 
-	os.WriteFile(out, []byte(b.String()), 0644)
-}
-
-func validate() {
-	if path == "" {
-		exit("path cannot be empty")
-	}
-
-	if name == "" {
-		exit("name cannot be empty")
-	}
-
-	if out == "" {
-		exit("out cannot be empty")
-	}
-
-	if kind == "" {
-		exit("kind cannot be empty")
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
 	}
 }
 
-func exit(s string) {
-	fmt.Fprintln(os.Stderr, s)
-	os.Exit(1)
+var (
+	inPath, kind, name, embed, out string
+	embeds                         []string
+)
+
+func configCmd() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "config",
+		Short: "Generates a configuration struct from an eBPF datapath object",
+		RunE:  runConfig,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if embed != "" {
+				embeds = strings.Split(embed, ",")
+			}
+
+			if inPath == "" {
+				return fmt.Errorf("path cannot be empty")
+			}
+
+			if name == "" {
+				return fmt.Errorf("name cannot be empty")
+			}
+
+			if out == "" {
+				return fmt.Errorf("out cannot be empty")
+			}
+
+			if kind != "object" && kind != "node" {
+				return fmt.Errorf("kind needs to be 'object' or 'node'")
+			}
+			return nil
+		},
+	}
+
+	flags := c.Flags()
+	flags.StringVar(&inPath, "path", "", "path to the eBPF collection")
+	flags.StringVar(&out, "out", "", "output Go file for the generated config struct")
+	flags.StringVar(&kind, "kind", "object", "kind of the eBPF collection (object or node)")
+	flags.StringVar(&name, "name", "", "name of the generated Go struct")
+	flags.StringVar(&embed, "embed", "", "comma-separated list of structs to embed")
+
+	return c
 }
 
-func errexit(err error) {
-	exit(err.Error())
+func mapsCmd() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "maps [pattern(s) ...]",
+		Short: "Generates Go MapSpecs from one or more datapath objects",
+		Long: `Generates Go MapSpecs from one or more datapath objects.
+
+Patterns are interpreted as glob patterns to match eBPF object files.
+
+Use with go:generate in a Go source file (e.g. gen.go) to automatically detect
+the current package name:
+
+    //go:generate go run github.com/cilium/cilium/tools/dpgen maps ../../../bpf/bpf_*.o
+
+If running outside of go:generate, the package name must be provided with -p/--package.`,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if goPkg == "" {
+				return fmt.Errorf("package name cannot be empty, set with -p/--package or GOPACKAGE")
+			}
+			return nil
+		},
+		RunE: runMaps,
+		Args: cobra.MinimumNArgs(1),
+	}
+
+	return c
 }

bpf/Makefile
@@ -175,6 +175,7 @@ testdata:
 
 generate: $(BPF)
 	$(GO) generate ../pkg/datapath/config
+	$(GO) generate ../pkg/datapath/maps
 	BPF2GO_CC="$(CLANG)" BPF2GO_CFLAGS="$(MAX_OVERLAY_OPTIONS) $(CLANG_FLAGS)" $(GO) generate ../pkg/datapath/bpf
 
 $(BPF_SIMPLE): %.o: %.c $(LIB)

pkg/bpf/map_linux.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/maps/registry"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/spanstat"
@@ -269,6 +270,29 @@ func NewMap(name string, mapType ebpf.MapType, mapKey MapKey, mapValue MapValue,
 		value: mapValue,
 		group: name,
 	}
+}
+
+// NewMapFromRegistry pulls an [ebpf.MapSpec] from the given registry and
+// returns a new [Map] based on the resulting spec.
+func NewMapFromRegistry(reg *registry.MapRegistry, name string, mapKey MapKey, mapValue MapValue) (*Map, error) {
+	// slogloggercheck: it's safe to use the default logger here as it has been initialized by the program up to this point.
+	defaultSlogLogger := logging.DefaultSlogLogger
+
+	spec, err := reg.Get(name)
+	if err != nil {
+		return nil, fmt.Errorf("get map from registry: %w", err)
+	}
+
+	return &Map{
+		Logger: defaultSlogLogger.With(
+			logfields.BPFMapName, spec.Name,
+		),
+		spec:  spec,
+		name:  spec.Name,
+		key:   mapKey,
+		value: mapValue,
+		group: spec.Name,
+	}, nil
 }
 
 // NewMapWithInnerSpec creates a new Map instance - object representing a BPF map with an inner map specification

pkg/datapath/config/gen.go
@@ -5,11 +5,11 @@ package config
 
 // Node configuration is present in all objects and doesn't have a dedicated
 // ELF, so pull it out of bpf_lxc.
-//go:generate go run github.com/cilium/cilium/tools/dpgen -path ../../../bpf/bpf_lxc.o -kind node -name Node -out node_config.go
+//go:generate go run github.com/cilium/cilium/tools/dpgen config --path ../../../bpf/bpf_lxc.o --kind node --name Node --out node_config.go
 
-//go:generate go run github.com/cilium/cilium/tools/dpgen -path ../../../bpf/bpf_lxc.o -embed Node -kind object -name BPFLXC -out lxc_config.go
-//go:generate go run github.com/cilium/cilium/tools/dpgen -path ../../../bpf/bpf_xdp.o -embed Node -kind object -name BPFXDP -out xdp_config.go
-//go:generate go run github.com/cilium/cilium/tools/dpgen -path ../../../bpf/bpf_host.o -embed Node -kind object -name BPFHost -out host_config.go
-//go:generate go run github.com/cilium/cilium/tools/dpgen -path ../../../bpf/bpf_overlay.o -embed Node -kind object -name BPFOverlay -out overlay_config.go
-//go:generate go run github.com/cilium/cilium/tools/dpgen -path ../../../bpf/bpf_wireguard.o -embed Node -kind object -name BPFWireguard -out wireguard_config.go
-//go:generate go run github.com/cilium/cilium/tools/dpgen -path ../../../bpf/bpf_sock.o -embed Node -kind object -name BPFSock -out sock_config.go
+//go:generate go run github.com/cilium/cilium/tools/dpgen config --path ../../../bpf/bpf_lxc.o --embed Node --kind object --name BPFLXC --out lxc_config.go
+//go:generate go run github.com/cilium/cilium/tools/dpgen config --path ../../../bpf/bpf_xdp.o --embed Node --kind object --name BPFXDP --out xdp_config.go
+//go:generate go run github.com/cilium/cilium/tools/dpgen config --path ../../../bpf/bpf_host.o --embed Node --kind object --name BPFHost --out host_config.go
+//go:generate go run github.com/cilium/cilium/tools/dpgen config --path ../../../bpf/bpf_overlay.o --embed Node --kind object --name BPFOverlay --out overlay_config.go
+//go:generate go run github.com/cilium/cilium/tools/dpgen config --path ../../../bpf/bpf_wireguard.o --embed Node --kind object --name BPFWireguard --out wireguard_config.go
+//go:generate go run github.com/cilium/cilium/tools/dpgen config --path ../../../bpf/bpf_sock.o --embed Node --kind object --name BPFSock --out sock_config.go

pkg/datapath/maps/gen.go
@@ -1,0 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package maps
+
+//go:generate go run github.com/cilium/cilium/tools/dpgen maps ../../../bpf/bpf_*.o

pkg/datapath/maps/maps_generated_test.go
@@ -1,0 +1,21 @@
+// Code generated by dpgen. DO NOT EDIT.
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package maps
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Test if BTF was correctly deduplicated, since LoadMapSpecs will panic if
+// there are multiple candidates for any named k/v type.
+func TestLoadMapSpecs(t *testing.T) {
+	specs, err := LoadMapSpecs()
+	require.NoError(t, err)
+	assert.NotEmpty(t, specs)
+}

commit c0bbff97ad855db55aca84e3690fc396f35a7de8

dpgen: relax validation of kind flag of dpgen config
Historically, dpgen used to support any value for the kind flag. The
blamed commit introduced stricter validation, only allowing explicitly
defined kinds. However, this introduces an unnecessary limitation in
overall flexibility, preventing downstream projects from leveraging
different kinds for specific purposes (e.g., separation). Hence, let's
relax the validation again, to allow different kinds as well.

Fixes: e9ede604490b ("bpf: implement 'dpgen maps' command and MapSpec registry")

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

tools/dpgen/main.go
@@ -59,8 +59,8 @@ func configCmd() *cobra.Command {
 				return fmt.Errorf("out cannot be empty")
 			}
 
-			if kind != "object" && kind != "node" {
-				return fmt.Errorf("kind needs to be 'object' or 'node'")
+			if kind == "" {
+				return fmt.Errorf("kind cannot be empty")
 			}
 			return nil
 		},