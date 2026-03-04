cilium/cilium

commit 07f78934c9bd03637c945f41ee765a1cd117561b

datapath/linux/device: add DesiredVLANDeviceSpec
Move the already existing type used for tests to a dedicated
DesiredVLANDeviceSpec type so it can be used outside of the package.
Also implement the JSON/YAML marshalling method in terms of struct tags
rather than having to construct a map where all fields have to be
repeated.

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/datapath/linux/device/vlan.go
@@ -1,0 +1,45 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package device
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/vishvananda/netlink"
+	"go.yaml.in/yaml/v3"
+)
+
+type DesiredVLANDeviceSpec struct {
+	Name        string `json:"name" yaml:"name"`
+	VLANID      int    `json:"vlanID" yaml:"vlanID"`
+	MTU         int    `json:"mtu" yaml:"mtu"`
+	ParentName  string `json:"parentName" yaml:"parentName"`
+	ParentIndex int
+}
+
+var _ DesiredDeviceSpec = (*DesiredVLANDeviceSpec)(nil)
+
+func (d *DesiredVLANDeviceSpec) ToNetlink() (netlink.Link, error) {
+	return &netlink.Vlan{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:        d.Name,
+			MTU:         d.MTU,
+			ParentIndex: d.ParentIndex,
+		},
+		VlanId: d.VLANID,
+	}, nil
+}
+
+func (d *DesiredVLANDeviceSpec) Properties() string {
+	return fmt.Sprintf("Type=vlan, ParentDevice=%s, VLAN=%d", d.ParentName, d.VLANID)
+}
+
+func (d *DesiredVLANDeviceSpec) MarshalYAML() (any, error) {
+	return yaml.Marshal(*d)
+}
+
+func (d *DesiredVLANDeviceSpec) MarshalJSON() ([]byte, error) {
+	return json.Marshal(*d)
+}

pkg/datapath/linux/device/scripttest/device_test.go
@@ -5,7 +5,6 @@ package scripttest
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -24,9 +23,9 @@ import (
 	"github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/vishvananda/netlink"
 
 	"github.com/cilium/cilium/pkg/datapath/linux"
+	"github.com/cilium/cilium/pkg/datapath/linux/device"
 	linuxdevice "github.com/cilium/cilium/pkg/datapath/linux/device"
 	"github.com/cilium/cilium/pkg/datapath/linux/sysctl"
 	"github.com/cilium/cilium/pkg/datapath/tables"
@@ -39,48 +38,6 @@ import (
 )
 
 var debug = flag.Bool("debug", false, "Enable debug logging")
-
-type desiredVlanDevice struct {
-	Name         string `yaml:"name"`
-	ParentDevice string `yaml:"parentDevice"`
-	VlanID       int    `yaml:"vlanID"`
-	MTU          int    `yaml:"mtu"`
-
-	parentIdx int
-}
-
-func (d desiredVlanDevice) ToNetlink() (netlink.Link, error) {
-	return &netlink.Vlan{
-		LinkAttrs: netlink.LinkAttrs{
-			Name:        d.Name,
-			MTU:         d.MTU,
-			ParentIndex: d.parentIdx,
-		},
-		VlanId: d.VlanID,
-	}, nil
-}
-
-func (d desiredVlanDevice) Properties() string {
-	return fmt.Sprintf("Type=vlan, ParentDevice=%s, VlanID=%d", d.ParentDevice, d.VlanID)
-}
-
-func (d desiredVlanDevice) MarshalYAML() (any, error) {
-	return map[string]any{
-		"name":         d.Name,
-		"parentDevice": d.ParentDevice,
-		"vlanID":       d.VlanID,
-		"mtu":          d.MTU,
-	}, nil
-}
-
-func (d desiredVlanDevice) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]any{
-		"name":         d.Name,
-		"parentDevice": d.ParentDevice,
-		"vlanID":       d.VlanID,
-		"mtu":          d.MTU,
-	})
-}
 
 func TestMain(m *testing.M) {
 	testutils.GoleakVerifyTestMain(m)
@@ -211,16 +168,16 @@ func testDesiredDevicesCmds(db *statedb.DB, dm linuxdevice.ManagerOperations, de
 				return nil, fmt.Errorf("failed to read device file %q: %w", args[1], err)
 			}
 
-			var device desiredVlanDevice
+			var device *device.DesiredVLANDeviceSpec
 			if err := yaml.Unmarshal(deviceFile, &device); err != nil {
 				return nil, fmt.Errorf("failed to unmarshal device file %q: %w", args[1], err)
 			}
 
-			dev, _, found := devTbl.Get(db.ReadTxn(), tables.DeviceNameIndex.Query(device.ParentDevice))
+			dev, _, found := devTbl.Get(db.ReadTxn(), tables.DeviceNameIndex.Query(device.ParentName))
 			if !found {
-				return nil, fmt.Errorf("parent device %q not found for VLAN device %q", device.ParentDevice, device.Name)
+				return nil, fmt.Errorf("parent device %q not found for VLAN device %q", device.ParentName, device.Name)
 			}
-			device.parentIdx = dev.Index
+			device.ParentIndex = dev.Index
 
 			if err := dm.UpsertDevice(linuxdevice.DesiredDevice{
 				Owner:      owner,

pkg/datapath/linux/device/scripttest/testdata/owners.txtar
@@ -27,5 +27,5 @@ eth0.10   dummy    down
 
 -- eth0.10.yaml --
 name: eth0.10
-parentDevice: eth0
+parentName: eth0
 vlanID: 10

pkg/datapath/linux/device/scripttest/testdata/persistance.txtar
@@ -35,18 +35,18 @@ hive/stop
 
 -- eth0.10.yaml --
 name: eth0.10
-parentDevice: eth0
+parentName: eth0
 vlanID: 10
 
 -- eth0.20.yaml --
 name: eth0.20
-parentDevice: eth0
+parentName: eth0
 vlanID: 20
 
 -- desired-device.1.table --
 Owner  Name      Properties
-owner1 eth0.10   Type=vlan, ParentDevice=eth0, VlanID=10
-owner1 eth0.20   Type=vlan, ParentDevice=eth0, VlanID=20
+owner1 eth0.10   Type=vlan, ParentDevice=eth0, VLAN=10
+owner1 eth0.20   Type=vlan, ParentDevice=eth0, VLAN=20
 
 -- devices.1.table --
 Name      Type     MTU     OperStatus
@@ -57,7 +57,7 @@ eth0.20   vlan     1500    up
 
 -- desired-device.2.table --
 Owner  Name      Properties
-owner1 eth0.10   Type=vlan, ParentDevice=eth0, VlanID=10
+owner1 eth0.10   Type=vlan, ParentDevice=eth0, VLAN=10
 
 -- devices.2.table --
 Name      Type     MTU     OperStatus

pkg/datapath/linux/device/scripttest/testdata/vlan.txtar
@@ -30,24 +30,24 @@ hive/stop
 
 -- eth0.10.yaml --
 name: eth0.10
-parentDevice: eth0
+parentName: eth0
 vlanID: 10
 
 -- eth0.20.yaml --
 name: eth0.20
-parentDevice: eth0
+parentName: eth0
 vlanID: 20
 
 -- eth0.20-updated.yaml --
 name: eth0.20
-parentDevice: eth0
+parentName: eth0
 vlanID: 20
 mtu: 1400
 
 -- desired-device.1.table --
 Owner  Name      Properties
-owner1 eth0.10   Type=vlan, ParentDevice=eth0, VlanID=10
-owner2 eth0.20   Type=vlan, ParentDevice=eth0, VlanID=20
+owner1 eth0.10   Type=vlan, ParentDevice=eth0, VLAN=10
+owner2 eth0.20   Type=vlan, ParentDevice=eth0, VLAN=20
 
 -- devices.1.table --
 Name      Type     MTU     OperStatus
@@ -58,7 +58,7 @@ eth0.20   vlan     1500    up
 
 -- desired-device.2.table --
 Owner  Name      Properties
-owner2 eth0.20   Type=vlan, ParentDevice=eth0, VlanID=20
+owner2 eth0.20   Type=vlan, ParentDevice=eth0, VLAN=20
 
 -- devices.2.table --
 Name      Type     MTU     OperStatus
@@ -68,7 +68,7 @@ eth0.20   vlan     1500    up
 
 -- desired-device.3.table --
 Owner  Name      Properties
-owner2 eth0.20   Type=vlan, ParentDevice=eth0, VlanID=20
+owner2 eth0.20   Type=vlan, ParentDevice=eth0, VLAN=20
 
 -- devices.3.table --
 Name      Type     MTU     OperStatus

commit 889006ef199b2e28a584aac512ea50b7bf76ccb8

datapath/linux/device: add desired VLAN device parent index JSON/YAML tags
Make sure the parent index field is also marshaled in the same way as
the other field (i.e. with lowercase field name). This was missed in the
original change introducing the DesiredVLANDeviceSpec type.

Fixes: 07f78934c9bd ("datapath/linux/device: add DesiredVLANDeviceSpec")

Suggested-by: Marco Iorio <marco.iorio@isovalent.com>
Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/datapath/linux/device/vlan.go
@@ -16,7 +16,7 @@ type DesiredVLANDeviceSpec struct {
 	VLANID      int    `json:"vlanID" yaml:"vlanID"`
 	MTU         int    `json:"mtu" yaml:"mtu"`
 	ParentName  string `json:"parentName" yaml:"parentName"`
-	ParentIndex int
+	ParentIndex int    `json:"parentIndex" yaml:"parentIndex"`
 }
 
 var _ DesiredDeviceSpec = (*DesiredVLANDeviceSpec)(nil)