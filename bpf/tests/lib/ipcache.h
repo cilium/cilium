23a64794be90339a722e1880e233bd8182918d5f
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2026-03-05 13:17:30 +0000

    bpf/tests: refactor ipcache helpers to expose remote endpoint flags
    This commit performs a minor refactor to the ipcache test helpers to
    better expose the full set of remote_endpoint_info flags. This will
    allow us to add additional helpers for testing of the new
    flag_unroutable field.
    
    Signed-off-by: Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>

--- bpf/tests/lib/ipcache.h
+++ bpf/tests/lib/ipcache.h
@@ -1,15 +1,27 @@
 /* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
 /* Copyright Authors of Cilium */
 
+struct remote_ep_flags {
+	/* This must be kept in sync with remote_endpoint_info in bpf/lib/eps.h. Perhaps these
+	 * could be split out of the remote_endpoint_info in future, for reuse here.
+	 */
+	__u8	flag_skip_tunnel:1,
+		flag_has_tunnel_ep:1,
+		flag_ipv6_tunnel_ep:1,
+		flag_remote_cluster:1,
+		flag_unroutable:1,
+		pad2:3;
+};
+
 static __always_inline void
-ipcache_add_entry(struct ipcache_key *key, __u32 sec_identity,
-		  const union v6addr *tunnel_ep, __u8 spi, bool flag_skip_tunnel,
-		  bool ipv6_underlay)
+__ipcache_add_entry(struct ipcache_key *key, __u32 sec_identity,
+		    const union v6addr *tunnel_ep, __u8 spi,
+		    struct remote_ep_flags flags)
 {
 	struct remote_endpoint_info value = {};
 
 	value.sec_identity = sec_identity;
-	if (ipv6_underlay) {
+	if (flags.flag_ipv6_tunnel_ep) {
 		/* The tunnel endpoint is misaligned on the stack so we need to use the builtin. */
 		__bpf_memcpy_builtin(&value.tunnel_endpoint.ip6, tunnel_ep, sizeof(*tunnel_ep));
 		value.flag_ipv6_tunnel_ep = true;
@@ -17,9 +29,11 @@ ipcache_add_entry(struct ipcache_key *key, __u32 sec_identity,
 		value.tunnel_endpoint.ip4 = tunnel_ep->p1;
 	}
 	value.key = spi;
-	value.flag_skip_tunnel = flag_skip_tunnel;
+	value.flag_skip_tunnel = flags.flag_skip_tunnel;
 	if (tunnel_ep->p1)
 		value.flag_has_tunnel_ep = true;
+
+	value.flag_unroutable = flags.flag_unroutable;
 
 	map_update_elem(&cilium_ipcache_v2, key, &value, BPF_ANY);
 }
@@ -27,7 +41,7 @@ ipcache_add_entry(struct ipcache_key *key, __u32 sec_identity,
 static __always_inline void
 __ipcache_v4_add_entry(__be32 addr, __u8 cluster_id, __u32 sec_identity,
 		       const union v6addr *tunnel_ep, __u8 spi, bool flag_skip_tunnel,
-		       bool ipv6_underlay, __u32 mask_size)
+		       bool ipv6_underlay, bool flag_unroutable, __u32 mask_size)
 {
 	struct ipcache_key key = {
 		.lpm_key.prefixlen = IPCACHE_PREFIX_LEN(mask_size),
@@ -35,9 +49,13 @@ __ipcache_v4_add_entry(__be32 addr, __u8 cluster_id, __u32 sec_identity,
 		.family = ENDPOINT_KEY_IPV4,
 		.ip4 = addr,
 	};
+	struct remote_ep_flags flags = {
+		.flag_skip_tunnel = flag_skip_tunnel,
+		.flag_ipv6_tunnel_ep = ipv6_underlay,
+		.flag_unroutable = flag_unroutable,
+	};
 
-	ipcache_add_entry(&key, sec_identity, tunnel_ep, spi,
-			  flag_skip_tunnel, ipv6_underlay);
+	__ipcache_add_entry(&key, sec_identity, tunnel_ep, spi, flags);
 }
 
 static __always_inline void
@@ -45,7 +63,7 @@ ipcache_v4_add_world_entry()
 {
 	union v6addr tunnel_ep = {0};
 
-	__ipcache_v4_add_entry(v4_all, 0, WORLD_IPV4_ID, &tunnel_ep, 0, false, false, 0);
+	__ipcache_v4_add_entry(v4_all, 0, WORLD_IPV4_ID, &tunnel_ep, 0, false, false, false, 0);
 }
 
 static __always_inline void
@@ -53,7 +71,7 @@ ipcache_v4_add_entry(__be32 addr, __u8 cluster_id, __u32 sec_identity,
 		     __u32 tunnel_ep, __u8 spi)
 {
 	__ipcache_v4_add_entry(addr, cluster_id, sec_identity, (union v6addr *)&tunnel_ep, spi,
-			       false, false, V4_CACHE_KEY_LEN);
+			       false, false, false, V4_CACHE_KEY_LEN);
 }
 
 static __always_inline void
@@ -61,7 +79,7 @@ ipcache_v4_add_entry_ipv6_underlay(__be32 addr, __u8 cluster_id, __u32 sec_ident
 				   const union v6addr *tunnel_ep, __u8 spi)
 {
 	__ipcache_v4_add_entry(addr, cluster_id, sec_identity, tunnel_ep, spi, false, true,
-			       V4_CACHE_KEY_LEN);
+			       false, V4_CACHE_KEY_LEN);
 }
 
 static __always_inline void
@@ -69,7 +87,7 @@ ipcache_v4_add_entry_with_flags(__be32 addr, __u8 cluster_id, __u32 sec_identity
 				__u32 tunnel_ep, __u8 spi, bool flag_skip_tunnel)
 {
 	__ipcache_v4_add_entry(addr, cluster_id, sec_identity, (union v6addr *)&tunnel_ep, spi,
-			       flag_skip_tunnel, false, V4_CACHE_KEY_LEN);
+			       flag_skip_tunnel, false, false, V4_CACHE_KEY_LEN);
 }
 
 static __always_inline void
@@ -77,24 +95,28 @@ ipcache_v4_add_entry_with_mask_size(__be32 addr, __u8 cluster_id, __u32 sec_iden
 				    __u32 tunnel_ep, __u8 spi, __u32 mask_size)
 {
 	__ipcache_v4_add_entry(addr, cluster_id, sec_identity, (union v6addr *)&tunnel_ep, spi,
-			       false, false, mask_size);
+			       false, false, false, mask_size);
 }
 
 static __always_inline void
 __ipcache_v6_add_entry(const union v6addr *addr, __u8 cluster_id, __u32 sec_identity,
 		       const union v6addr *tunnel_ep, __u8 spi, bool flag_skip_tunnel,
-		       bool ipv6_underlay, __u32 mask_size)
+		       bool ipv6_underlay, bool flag_unroutable, __u32 mask_size)
 {
 	struct ipcache_key key __align_stack_8 = {
 		.lpm_key.prefixlen = IPCACHE_PREFIX_LEN(mask_size),
 		.cluster_id = cluster_id,
 		.family = ENDPOINT_KEY_IPV6,
 	};
+	struct remote_ep_flags flags = {
+		.flag_skip_tunnel = flag_skip_tunnel,
+		.flag_ipv6_tunnel_ep = ipv6_underlay,
+		.flag_unroutable = flag_unroutable,
+	};
 
 	memcpy(&key.ip6, addr, sizeof(*addr));
 
-	ipcache_add_entry(&key, sec_identity, tunnel_ep, spi,
-			  flag_skip_tunnel, ipv6_underlay);
+	__ipcache_add_entry(&key, sec_identity, tunnel_ep, spi, flags);
 }
 
 static __always_inline void
@@ -103,7 +125,7 @@ ipcache_v6_add_world_entry()
 	union v6addr tunnel_ep = {0};
 
 	__ipcache_v6_add_entry((union v6addr *)v6_all, 0, WORLD_IPV6_ID,
-			       &tunnel_ep, 0, false, false, 0);
+			       &tunnel_ep, 0, false, false, false, 0);
 }
 
 static __always_inline void
@@ -111,7 +133,7 @@ ipcache_v6_add_entry(const union v6addr *addr, __u8 cluster_id, __u32 sec_identi
 		     __u32 tunnel_ep, __u8 spi)
 {
 	__ipcache_v6_add_entry(addr, cluster_id, sec_identity, (union v6addr *)&tunnel_ep, spi,
-			       false, false, V6_CACHE_KEY_LEN);
+			       false, false, false, V6_CACHE_KEY_LEN);
 }
 
 static __always_inline void
@@ -119,7 +141,7 @@ ipcache_v6_add_entry_ipv6_underlay(const union v6addr *addr, __u8 cluster_id, __
 				   const union v6addr *tunnel_ep, __u8 spi)
 {
 	__ipcache_v6_add_entry(addr, cluster_id, sec_identity, tunnel_ep, spi, false, true,
-			       V6_CACHE_KEY_LEN);
+			       false, V6_CACHE_KEY_LEN);
 }
 
 static __always_inline void
@@ -127,5 +149,5 @@ ipcache_v6_add_entry_with_flags(const union v6addr *addr, __u8 cluster_id, __u32
 				__u32 tunnel_ep, __u8 spi, bool flag_skip_tunnel)
 {
 	__ipcache_v6_add_entry(addr, cluster_id, sec_identity, (union v6addr *)&tunnel_ep, spi,
-			       flag_skip_tunnel, false, V6_CACHE_KEY_LEN);
+			       flag_skip_tunnel, false, false, V6_CACHE_KEY_LEN);
 }
