724e1e4d59a50388275bed7a49e12e7d8b742e48
Zihong Zheng <zihongz@google.com>
2026-03-05 18:45:16 +0000

    Documentation: fix CI to catch untracked generated files
    When new commands are added or new documentation is generated, the
    scripts in Documentation/ check for diffs using 'git diff --quiet'.
    However, this does not catch new, untracked files. This can lead to CI
    passing even if required documentation files are missing from the PR
    (but generated during the CI run).
    
    This PR adds 'git add --intent-to-add' before the diff check to ensure
    new files are also considered.
    
    Signed-off-by: Zihong Zheng <zihongz@google.com>

--- Documentation/check-cmdref.sh
+++ Documentation/check-cmdref.sh
@@ -7,6 +7,9 @@ set -o pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cmdref_dir="${script_dir}/cmdref"
 
+# Ensure new files are also considered in the diff
+git add --intent-to-add -- "${cmdref_dir}"
+
 if ! git diff --quiet -- "${cmdref_dir}" ; then
     git --no-pager diff "${cmdref_dir}"
     echo "HINT: to fix this, run 'make -C Documentation update-cmdref'"
