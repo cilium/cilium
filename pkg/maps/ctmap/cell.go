cilium/cilium

commit 3872944a6b7dcdade1fb2d8f3b782ac94a970ece

ct/map: introduce hive cell
This commit introduces a hive cell for the connection tracking map.

This includes the following things.

* provide CT BPF maps via hive (interface `CTMaps` to retrieve `ActiveMaps`)
  (depending on enabled IP families - no need for the caller to pass this info)
  -> For the time being there's no attempt in cleaning up the API of the CTMap
  -> This has been the last map that has been initialized by the legacy daemon init
     logic - we can get rid of the function `initMaps`.
* handle lifecycle of map (open & close) via Hive lifecycle start/stop hooks
* initialize legacy mapsinfo as part of ctmap hive cell (no longer by daemon)
  -> the dependency from the CT to the NAT map should be temporarily
  -> we should be able to eventually get rid of this once have the NAT hive cell too
* CT/NAT garbage collector depends on ctmaps

Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

pkg/maps/ctmap/cell.go
@@ -1,0 +1,90 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package ctmap
+
+import (
+	"fmt"
+	"slices"
+
+	"github.com/cilium/hive/cell"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/maps/nat"
+	"github.com/cilium/cilium/pkg/option"
+)
+
+// Cell provides the ctmap.Map which contains the connection tracking state.
+var Cell = cell.Module(
+	"ct-map",
+	"eBPF map which manages connection tracking",
+
+	cell.Provide(newCTMaps),
+)
+
+func newCTMaps(lifecycle cell.Lifecycle, daemonConfig *option.DaemonConfig, natMap4 nat.NatMap4, natMap6 nat.NatMap6) bpf.MapOut[CTMaps] {
+	InitMapInfo(natMap4, natMap6)
+
+	ctMaps := &ctMaps{}
+
+	if daemonConfig.IPv4Enabled() {
+		ctMaps.v4AnyMap = newMap(MapNameAny4Global, mapTypeIPv4AnyGlobal)
+		ctMaps.v4TCPMap = newMap(MapNameTCP4Global, mapTypeIPv4TCPGlobal)
+	}
+
+	if daemonConfig.IPv6Enabled() {
+		ctMaps.v6AnyMap = newMap(MapNameAny6Global, mapTypeIPv6AnyGlobal)
+		ctMaps.v6TCPMap = newMap(MapNameTCP6Global, mapTypeIPv6TCPGlobal)
+	}
+
+	lifecycle.Append(cell.Hook{
+		OnStart: func(context cell.HookContext) error {
+			return ctMaps.init()
+		},
+		OnStop: func(context cell.HookContext) error {
+			return ctMaps.close()
+		},
+	})
+
+	return bpf.NewMapOut(CTMaps(ctMaps))
+}
+
+// CTMaps provides access to the active connection tracking BPF maps.
+type CTMaps interface {
+	// ActiveMaps returns a slice of global CT maps that are used, depending
+	// on whether IPv4 and/or IPv6 is configured.
+	ActiveMaps() []*Map
+}
+
+type ctMaps struct {
+	v4AnyMap *Map
+	v4TCPMap *Map
+	v6AnyMap *Map
+	v6TCPMap *Map
+}
+
+var _ CTMaps = (*ctMaps)(nil)
+
+func (r *ctMaps) ActiveMaps() []*Map {
+	return slices.DeleteFunc([]*Map{r.v4AnyMap, r.v4TCPMap, r.v6AnyMap, r.v6TCPMap}, func(m *Map) bool { return m == nil })
+}
+
+func (r *ctMaps) init() error {
+	for _, m := range r.ActiveMaps() {
+		if err := m.OpenOrCreate(); err != nil {
+			return fmt.Errorf("failed to open and create %s map: %w", m.Name(), err)
+		}
+	}
+
+	return nil
+}
+
+func (r *ctMaps) close() error {
+	for _, m := range r.ActiveMaps() {
+		if err := m.Close(); err != nil {
+			return fmt.Errorf("failed to close %s map: %w", m.Name(), err)
+		}
+	}
+
+	return nil
+}

cilium-dbg/cmd/bpf_ct_flush.go
@@ -48,7 +48,7 @@ func flushCt() {
 			defer func() { nat6.Close() }()
 		}
 	}
-	ctmap.InitMapInfo(nil, ipv4, ipv6, nat4, nat6)
+	ctmap.InitMapInfo(nat4, nat6)
 
 	maps := ctmap.Maps(ipv4, ipv6)
 

daemon/cmd/bootstrap_statistics.go
@@ -10,18 +10,15 @@ import (
 )
 
 type bootstrapStatistics struct {
-	overall     spanstat.SpanStat
-	earlyInit   spanstat.SpanStat
-	k8sInit     spanstat.SpanStat
-	restore     spanstat.SpanStat
-	cleanup     spanstat.SpanStat
-	bpfBase     spanstat.SpanStat
-	ipam        spanstat.SpanStat
-	daemonInit  spanstat.SpanStat
-	mapsInit    spanstat.SpanStat
-	fqdn        spanstat.SpanStat
-	kvstore     spanstat.SpanStat
-	deleteQueue spanstat.SpanStat
+	overall   spanstat.SpanStat
+	earlyInit spanstat.SpanStat
+	k8sInit   spanstat.SpanStat
+	restore   spanstat.SpanStat
+	cleanup   spanstat.SpanStat
+	bpfBase   spanstat.SpanStat
+	ipam      spanstat.SpanStat
+	fqdn      spanstat.SpanStat
+	kvstore   spanstat.SpanStat
 }
 
 func (b *bootstrapStatistics) updateMetrics() {
@@ -41,17 +38,14 @@ func (b *bootstrapStatistics) updateMetrics() {
 
 func (b *bootstrapStatistics) getMap() map[string]*spanstat.SpanStat {
 	return map[string]*spanstat.SpanStat{
-		"overall":     &b.overall,
-		"earlyInit":   &b.earlyInit,
-		"k8sInit":     &b.k8sInit,
-		"restore":     &b.restore,
-		"cleanup":     &b.cleanup,
-		"bpfBase":     &b.bpfBase,
-		"ipam":        &b.ipam,
-		"daemonInit":  &b.daemonInit,
-		"mapsInit":    &b.mapsInit,
-		"fqdn":        &b.fqdn,
-		"kvstore":     &b.kvstore,
-		"deleteQueue": &b.deleteQueue,
+		"overall":   &b.overall,
+		"earlyInit": &b.earlyInit,
+		"k8sInit":   &b.k8sInit,
+		"restore":   &b.restore,
+		"cleanup":   &b.cleanup,
+		"bpfBase":   &b.bpfBase,
+		"ipam":      &b.ipam,
+		"fqdn":      &b.fqdn,
+		"kvstore":   &b.kvstore,
 	}
 }

daemon/cmd/daemon.go
@@ -17,7 +17,6 @@ import (
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/maps/ctmap"
 	"github.com/cilium/cilium/pkg/node"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
@@ -142,20 +141,6 @@ func initAndValidateDaemonConfig(params daemonConfigParams) error {
 
 func configureDaemon(ctx context.Context, params daemonParams) error {
 	var err error
-
-	bootstrapStats.daemonInit.Start()
-
-	ctmap.InitMapInfo(params.MetricsRegistry, params.DaemonConfig.EnableIPv4, params.DaemonConfig.EnableIPv6, params.NatMap4, params.NatMap6)
-
-	bootstrapStats.daemonInit.End(true)
-
-	// Open or create BPF maps.
-	bootstrapStats.mapsInit.Start()
-	err = initMaps(params)
-	bootstrapStats.mapsInit.EndError(err)
-	if err != nil {
-		return fmt.Errorf("error while opening/creating BPF maps: %w", err)
-	}
 
 	bootstrapStats.restore.Start()
 	// fetch old endpoints before k8s is configured.

daemon/cmd/daemon_main.go
@@ -1218,7 +1218,6 @@ type daemonParams struct {
 	Logger    *slog.Logger
 	Lifecycle cell.Lifecycle
 
-	MetricsRegistry     *metrics.Registry
 	Clientset           k8sClient.Clientset
 	KVStoreClient       kvstore.Client
 	WGAgent             wgTypes.WireguardAgent
@@ -1244,8 +1243,6 @@ type daemonParams struct {
 	KPRConfig           kpr.KPRConfig
 	KPRInitializer      kprinitializer.KPRInitializer
 	InfraIPAllocator    infraendpoints.InfraIPAllocator
-	NatMap4             nat.NatMap4
-	NatMap6             nat.NatMap6
 }
 
 func daemonConfigInitialization(params daemonConfigParams) legacy.DaemonConfigInitialization {

daemon/cmd/datapath.go
@@ -12,8 +12,6 @@ import (
 
 	"github.com/cilium/cilium/pkg/datapath/linux/safenetlink"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/maps/ctmap"
-	"github.com/cilium/cilium/pkg/option"
 )
 
 // listFilterIfs returns a map of interfaces based on the given filter.
@@ -73,23 +71,5 @@ func clearCiliumVeths(logger *slog.Logger) error {
 			}
 		}
 	}
-	return nil
-}
-
-// initMaps opens all BPF maps (and creates them if they do not exist). This
-// must be done *before* any operations which read BPF maps, especially
-// restoring endpoints and services.
-func initMaps(params daemonParams) error {
-	if option.Config.DryMode {
-		return nil
-	}
-
-	for _, m := range ctmap.Maps(option.Config.EnableIPv4,
-		option.Config.EnableIPv6) {
-		if err := m.Create(); err != nil {
-			return fmt.Errorf("initializing conntrack map %s: %w", m.Name(), err)
-		}
-	}
-
 	return nil
 }

pkg/maps/cells.go
@@ -13,6 +13,7 @@ import (
 	"github.com/cilium/cilium/pkg/maps/authmap"
 	"github.com/cilium/cilium/pkg/maps/bwmap"
 	"github.com/cilium/cilium/pkg/maps/configmap"
+	"github.com/cilium/cilium/pkg/maps/ctmap"
 	"github.com/cilium/cilium/pkg/maps/ctmap/gc"
 	"github.com/cilium/cilium/pkg/maps/egressmap"
 	"github.com/cilium/cilium/pkg/maps/encrypt"
@@ -46,6 +47,9 @@ var Cell = cell.Module(
 	// Receives datapath signals for GC fill-up events
 	// Note that we can't import this from ctmap package, as gc needs to import ctmap.
 	gc.Cell,
+
+	// Provides the ctmap.GCMps which contains the connection tracking state maps.
+	ctmap.Cell,
 
 	// Provides access to egressgateway specific maps.
 	egressmap.Cell,

pkg/maps/ctmap/ctmap.go
@@ -118,7 +118,7 @@ type CtMapRecord struct {
 
 // InitMapInfo builds the information about different CT maps for the
 // combination of L3/L4 protocols.
-func InitMapInfo(registry *metrics.Registry, v4, v6 bool, nat4 nat.NatMap4, nat6 nat.NatMap6) {
+func InitMapInfo(nat4 nat.NatMap4, nat6 nat.NatMap6) {
 	var global4Map, global6Map *nat.Map
 	global4MapLock := &lock.Mutex{}
 	global6MapLock := &lock.Mutex{}
@@ -542,7 +542,7 @@ func PurgeOrphanNATEntries(ctMapTCP, ctMapAny *Map) *NatGCStats {
 		natMap = ctMap.natMap
 	} else {
 		// per-cluster map handling
-		var family = nat.IPv4
+		family := nat.IPv4
 		if ctMapTCP.mapType.isIPv6() {
 			family = nat.IPv6
 		}
@@ -690,6 +690,8 @@ func DeleteIfUpgradeNeeded() {
 // If ipv4 or ipv6 are false, the maps for that protocol will not be returned.
 //
 // The returned maps are not yet opened.
+//
+// This should only be used from components which aren't capable of using hive - mainly the Cilium CLI.
 func Maps(ipv4, ipv6 bool) []*Map {
 	result := make([]*Map, 0, mapCount)
 	if ipv4 {

commit 9942e786e9e954f324c4ca2303e00e89a5f00e46

ctmap: Change order of active maps
The ActiveMaps() method returns list of open CT maps. The list is used
by the GC routine. The routine passes relevant CT maps to the
PurgeOrphanNATEntries. The latter expects the TCP CT map to be the first
one:

    ctMapTCP, ctMapAny := maps[i], maps[i+1]

However, the 3872944a6b7 commit changed the order of the maps in the
list. Bring it back, otherwise the PurgeOrphanNATEntries keeps deleting
valid NAT entries (as it cannot find in the CT map).

Fixes: 3872944a6b7 ("ct/map: introduce hive cell")
Signed-off-by: Martynas Pumputis <martynas@isovalent.com>

pkg/maps/ctmap/cell.go
@@ -67,7 +67,7 @@ type ctMaps struct {
 var _ CTMaps = (*ctMaps)(nil)
 
 func (r *ctMaps) ActiveMaps() []*Map {
-	return slices.DeleteFunc([]*Map{r.v4AnyMap, r.v4TCPMap, r.v6AnyMap, r.v6TCPMap}, func(m *Map) bool { return m == nil })
+	return slices.DeleteFunc([]*Map{r.v4TCPMap, r.v4AnyMap, r.v6TCPMap, r.v6AnyMap}, func(m *Map) bool { return m == nil })
 }
 
 func (r *ctMaps) init() error {