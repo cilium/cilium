d0a9317789a7eef84962b1b7367386c5ae37af3a
Timo Beckers <timo@isovalent.com>
2026-03-12 13:55:25 +0100

    dpgen,datapath: embed generated types in runtime config structs
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- pkg/datapath/config/config.go
+++ pkg/datapath/config/config.go
@@ -15,20 +15,20 @@ func NodeConfig(lnc *datapath.LocalNodeConfiguration) Node {
 	node.ClusterIDBits = identity.GetClusterIDBits()
 
 	node.CiliumHostIfIndex = lnc.CiliumHostIfIndex
-	node.CiliumHostMAC = lnc.CiliumHostMAC.As8()
+	node.CiliumHostMAC.Addr = lnc.CiliumHostMAC.As6()
 	node.CiliumNetIfIndex = lnc.CiliumNetIfIndex
-	node.CiliumNetMAC = lnc.CiliumNetMAC.As8()
+	node.CiliumNetMAC.Addr = lnc.CiliumNetMAC.As6()
 
 	if lnc.ServiceLoopbackIPv4.IsValid() {
-		node.ServiceLoopbackIPv4 = lnc.ServiceLoopbackIPv4.As4()
+		node.ServiceLoopbackIPv4.Addr = lnc.ServiceLoopbackIPv4.As4()
 	}
 
 	if lnc.ServiceLoopbackIPv6.IsValid() {
-		node.ServiceLoopbackIPv6 = lnc.ServiceLoopbackIPv6.As16()
+		node.ServiceLoopbackIPv6.Addr = lnc.ServiceLoopbackIPv6.As16()
 	}
 
 	if lnc.CiliumInternalIPv6.IsValid() {
-		node.RouterIPv6 = lnc.CiliumInternalIPv6.As16()
+		node.RouterIPv6.Addr = lnc.CiliumInternalIPv6.As16()
 	}
 
 	node.ClusterID = option.Config.ClusterID
@@ -53,7 +53,7 @@ func NodeConfig(lnc *datapath.LocalNodeConfiguration) Node {
 	node.NodeportPortMax = lnc.LBConfig.NodePortMax
 
 	if option.Config.EnableNat46X64Gateway {
-		node.NAT46X64Prefix = option.Config.IPv6NAT46x64CIDRBase.As4()
+		node.NAT46X64Prefix.Addr = option.Config.IPv6NAT46x64CIDRBase.As4()
 	}
 
 	node.EnableJiffies = option.Config.ClockSource == option.ClockSourceJiffies
