15248236d0774a6cef89406a26585655c5861225
Jordan Rife <jrife@google.com>
2026-03-12 12:15:24 -0700

    plugins: Implement bpfCollectionLoader
    bpfCollectionLoader coordinates between datapath plugins when loading a
    BPF collection. It provides an interface similar to the usual bpf.Load
    and bpf.LoadAndAssign functions.
    
    Signed-off-by: Jordan Rife <jrife@google.com>

--- pkg/datapath/loader/plugins_test.go
+++ pkg/datapath/loader/plugins_test.go
@@ -4,20 +4,30 @@
 package loader
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
 
+	"github.com/cilium/hive/hivetest"
+
 	"github.com/cilium/cilium/api/v1/datapathplugins"
+	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/datapath/bpf/testprogs"
+	"github.com/cilium/cilium/pkg/datapath/config"
+	plugin "github.com/cilium/cilium/pkg/datapath/plugins/types"
+	api_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/testutils"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 	"google.golang.org/protobuf/testing/protocmp"
 )
 
@@ -612,6 +622,572 @@ func TestPrivilegedHooksSpec(t *testing.T) {
 				cmp.AllowUnexported(outputValues{}),
 			); diff != "" {
 				t.Fatalf("outputs did not match what was expected (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+type fakePluginRegistry map[string]*fakePlugin
+
+func (r fakePluginRegistry) Register(dpp *api_v2alpha1.CiliumDatapathPlugin) error {
+	return nil
+}
+
+func (r fakePluginRegistry) Unregister(dpp *api_v2alpha1.CiliumDatapathPlugin) error {
+	return nil
+}
+
+func (r fakePluginRegistry) Plugins() plugin.Plugins {
+	result := plugin.Plugins{}
+
+	for _, p := range r {
+		result[p.name] = p
+	}
+
+	return result
+}
+
+func (r fakePluginRegistry) closeAll() {
+	for _, p := range r {
+		if p.coll != nil {
+			p.coll.Close()
+		}
+	}
+}
+
+type transaction struct {
+	prepareHooksReq  *datapathplugins.PrepareCollectionRequest
+	prepareHooksResp *datapathplugins.PrepareCollectionResponse
+	prepareHooksErr  error
+	loadHooksReq     *datapathplugins.InstrumentCollectionRequest
+	loadHooksResp    *datapathplugins.InstrumentCollectionResponse
+	loadHooksErr     error
+}
+
+var _ plugin.Plugin = &fakePlugin{}
+
+type fakePlugin struct {
+	name             string
+	attachmentPolicy api_v2alpha1.CiliumDatapathPluginAttachmentPolicy
+	transaction      *transaction
+
+	coll *ebpf.Collection
+}
+
+func (p *fakePlugin) Name() string {
+	return p.name
+}
+
+func (p *fakePlugin) AttachmentPolicy() api_v2alpha1.CiliumDatapathPluginAttachmentPolicy {
+	return p.attachmentPolicy
+}
+
+func (p *fakePlugin) DeepEqual(o plugin.Plugin) bool {
+	return false
+}
+
+func (p *fakePlugin) PrepareCollection(ctx context.Context, in *datapathplugins.PrepareCollectionRequest, opts ...grpc.CallOption) (*datapathplugins.PrepareCollectionResponse, error) {
+	p.transaction.prepareHooksReq = in
+
+	if p.transaction.prepareHooksErr != nil {
+		return nil, p.transaction.prepareHooksErr
+	}
+
+	return p.transaction.prepareHooksResp, nil
+}
+
+func (p *fakePlugin) InstrumentCollection(ctx context.Context, in *datapathplugins.InstrumentCollectionRequest, opts ...grpc.CallOption) (*datapathplugins.InstrumentCollectionResponse, error) {
+	p.transaction.loadHooksReq = in
+
+	if p.transaction.loadHooksErr != nil {
+		return nil, p.transaction.loadHooksErr
+	}
+
+	spec, err := testprogs.LoadPluginsHooks()
+	if err != nil {
+		return nil, fmt.Errorf("loading plugin hooks spec: %w", err)
+	}
+
+	targetProgs := map[ebpf.ProgramID]*ebpf.Program{}
+	usedHookPrograms := map[string]bool{}
+
+	for _, h := range in.Hooks {
+		progName := chooseHookProgram(h)
+		progSpec := spec.Programs[progName]
+		if progSpec == nil {
+			return nil, fmt.Errorf("unknown program: %s", progName)
+		}
+
+		usedHookPrograms[progName] = true
+		id := ebpf.ProgramID(h.AttachTarget.ProgramId)
+		if targetProgs[id] == nil {
+			targetProg, err := ebpf.NewProgramFromID(id)
+			if err != nil {
+				return nil, fmt.Errorf("loading target program %d: %w", h.AttachTarget.ProgramId, err)
+			}
+			defer targetProg.Close()
+			targetProgs[id] = targetProg
+		}
+
+		progSpec.AttachTarget = targetProgs[id]
+		progSpec.AttachTo = h.AttachTarget.SubprogName
+	}
+
+	// prune any unused hook programs so they aren't loaded
+	for name := range spec.Programs {
+		if !usedHookPrograms[name] {
+			delete(spec.Programs, name)
+		}
+	}
+
+	seqID := in.Collection.Maps["seq"].Id
+	seq, err := ebpf.NewMapFromID(ebpf.MapID(seqID))
+	if err != nil {
+		return nil, fmt.Errorf("loading seq map: %w", err)
+	}
+	defer seq.Close()
+
+	coll, err := ebpf.NewCollectionWithOptions(spec, ebpf.CollectionOptions{
+		MapReplacements: map[string]*ebpf.Map{
+			"seq": seq,
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("loading plugin hooks: %w", err)
+	}
+
+	for _, h := range in.Hooks {
+		progName := chooseHookProgram(h)
+		prog := coll.Programs[progName]
+
+		if err := prog.Pin(h.PinPath); err != nil {
+			coll.Close()
+			return nil, fmt.Errorf("pinning program %s to %s: %w", progName, h.PinPath, err)
+		}
+	}
+
+	p.coll = coll
+
+	return p.transaction.loadHooksResp, nil
+}
+
+func TestPrivilegedLoadAndAssignWithPlugins(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	baseSpec, err := testprogs.LoadPluginsBase()
+	require.NoError(t, err)
+
+	baseCollSpec := &datapathplugins.PrepareCollectionRequest_CollectionSpec{
+		Programs: map[string]*datapathplugins.PrepareCollectionRequest_CollectionSpec_ProgramSpec{},
+		Maps:     map[string]*datapathplugins.PrepareCollectionRequest_CollectionSpec_MapSpec{},
+	}
+
+	baseColl := &datapathplugins.InstrumentCollectionRequest_Collection{
+		Programs: map[string]*datapathplugins.InstrumentCollectionRequest_Collection_Program{},
+		Maps:     map[string]*datapathplugins.InstrumentCollectionRequest_Collection_Map{},
+	}
+
+	for name, p := range baseSpec.Programs {
+		baseCollSpec.Programs[name] = &datapathplugins.PrepareCollectionRequest_CollectionSpec_ProgramSpec{
+			License:     p.License,
+			SectionName: p.SectionName,
+			Type:        uint32(p.Type),
+			AttachType:  uint32(p.AttachType),
+		}
+		baseColl.Programs[name] = &datapathplugins.InstrumentCollectionRequest_Collection_Program{}
+	}
+	for name, m := range baseSpec.Maps {
+		baseCollSpec.Maps[name] = &datapathplugins.PrepareCollectionRequest_CollectionSpec_MapSpec{
+			KeySize:    m.KeySize,
+			MaxEntries: m.MaxEntries,
+			Type:       uint32(m.Type),
+			ValueSize:  m.ValueSize,
+		}
+		baseColl.Maps[name] = &datapathplugins.InstrumentCollectionRequest_Collection_Map{}
+	}
+
+	prepareHooksReq := &datapathplugins.PrepareCollectionRequest{
+		Collection:        baseCollSpec,
+		AttachmentContext: &datapathplugins.AttachmentContext{},
+	}
+
+	hookSpec := func(typ datapathplugins.HookType,
+		target string,
+		constraints []*datapathplugins.PrepareCollectionResponse_HookSpec_OrderingConstraint) *datapathplugins.PrepareCollectionResponse_HookSpec {
+		return &datapathplugins.PrepareCollectionResponse_HookSpec{
+			Type:        typ,
+			Target:      target,
+			Constraints: constraints,
+		}
+	}
+
+	pre := func(target string, constraints ...*datapathplugins.PrepareCollectionResponse_HookSpec_OrderingConstraint) *datapathplugins.PrepareCollectionResponse_HookSpec {
+		return hookSpec(datapathplugins.HookType_PRE, target, constraints)
+	}
+
+	post := func(target string, constraints ...*datapathplugins.PrepareCollectionResponse_HookSpec_OrderingConstraint) *datapathplugins.PrepareCollectionResponse_HookSpec {
+		return hookSpec(datapathplugins.HookType_POST, target, constraints)
+	}
+
+	before := func(plugin string) *datapathplugins.PrepareCollectionResponse_HookSpec_OrderingConstraint {
+		return &datapathplugins.PrepareCollectionResponse_HookSpec_OrderingConstraint{
+			Plugin: plugin,
+			Order:  datapathplugins.PrepareCollectionResponse_HookSpec_OrderingConstraint_BEFORE,
+		}
+	}
+
+	pluginAPrepareHooksResp := &datapathplugins.PrepareCollectionResponse{
+		Hooks: []*datapathplugins.PrepareCollectionResponse_HookSpec{
+			pre("program_tc", before("plugin_b")),
+			post("program_tc", before("plugin_b")),
+			pre("program_xdp", before("plugin_b")),
+			post("program_xdp", before("plugin_b")),
+		},
+		Cookie: "plugin_a_cookie",
+	}
+
+	pluginBPrepareHooksResp := &datapathplugins.PrepareCollectionResponse{
+		Hooks: []*datapathplugins.PrepareCollectionResponse_HookSpec{
+			pre("program_tc"),
+			post("program_tc"),
+			pre("program_xdp"),
+			post("program_xdp"),
+		},
+		Cookie: "plugin_b_cookie",
+	}
+
+	loadHooksReq := func(plugin string) *datapathplugins.InstrumentCollectionRequest {
+		return &datapathplugins.InstrumentCollectionRequest{
+			Collection:        baseColl,
+			AttachmentContext: &datapathplugins.AttachmentContext{},
+			Hooks: []*datapathplugins.InstrumentCollectionRequest_Hook{
+				{
+					Type:   datapathplugins.HookType_PRE,
+					Target: "program_tc",
+					AttachTarget: &datapathplugins.InstrumentCollectionRequest_Hook_AttachTarget{
+						SubprogName: preHookSubprogName(plugin),
+					},
+				},
+				{
+					Type:   datapathplugins.HookType_POST,
+					Target: "program_tc",
+					AttachTarget: &datapathplugins.InstrumentCollectionRequest_Hook_AttachTarget{
+						SubprogName: postHookSubprogName(plugin),
+					},
+				},
+				{
+					Type:   datapathplugins.HookType_PRE,
+					Target: "program_xdp",
+					AttachTarget: &datapathplugins.InstrumentCollectionRequest_Hook_AttachTarget{
+						SubprogName: preHookSubprogName(plugin),
+					},
+				},
+				{
+					Type:   datapathplugins.HookType_POST,
+					Target: "program_xdp",
+					AttachTarget: &datapathplugins.InstrumentCollectionRequest_Hook_AttachTarget{
+						SubprogName: postHookSubprogName(plugin),
+					},
+				},
+			},
+			Cookie: fmt.Sprintf("%s_cookie", plugin),
+		}
+	}
+
+	testCases := []struct {
+		name                 string
+		inputValues          inputValues
+		attachmentPolicies   map[string]api_v2alpha1.CiliumDatapathPluginAttachmentPolicy
+		transactions         map[string]transaction
+		expectedOutputValues outputValues
+		expectedErr          error
+	}{
+		{
+			name: "pre and post hooks basic",
+			transactions: map[string]transaction{
+				"plugin_a": {
+					prepareHooksReq:  prepareHooksReq,
+					prepareHooksResp: pluginAPrepareHooksResp,
+					loadHooksReq:     loadHooksReq("plugin_a"),
+					loadHooksResp:    &datapathplugins.InstrumentCollectionResponse{},
+				},
+				"plugin_b": {
+					prepareHooksReq:  prepareHooksReq,
+					prepareHooksResp: pluginBPrepareHooksResp,
+					loadHooksReq:     loadHooksReq("plugin_b"),
+					loadHooksResp:    &datapathplugins.InstrumentCollectionResponse{},
+				},
+			},
+			attachmentPolicies: map[string]api_v2alpha1.CiliumDatapathPluginAttachmentPolicy{
+				"plugin_a": api_v2alpha1.AttachmentPolicyAlways,
+				"plugin_b": api_v2alpha1.AttachmentPolicyAlways,
+			},
+			inputValues: inputValues{
+				hook: map[string]map[string]int32{
+					"plugin_a": {
+						"before_program_tc_ret":  -1,
+						"after_program_tc_ret":   -1,
+						"before_program_xdp_ret": -1,
+						"after_program_xdp_ret":  -1,
+					},
+					"plugin_b": {
+						"before_program_tc_ret":  -1,
+						"after_program_tc_ret":   -1,
+						"before_program_xdp_ret": -1,
+						"after_program_xdp_ret":  -1,
+					},
+				},
+			},
+			expectedOutputValues: outputValues{
+				base: map[string]int32{
+					"program_tc_seq":  3,
+					"program_xdp_seq": 3,
+				},
+				returns: map[string]uint32{
+					"program_tc":  1,
+					"program_xdp": 1,
+				},
+				hook: map[string]map[string]int32{
+					"plugin_a": {
+						"before_program_tc_seq":       1,
+						"before_program_tc_ret":       -1,
+						"after_program_tc_seq":        4,
+						"after_program_tc_ret":        -1,
+						"after_program_tc_ret_param":  1,
+						"before_program_xdp_seq":      1,
+						"before_program_xdp_ret":      -1,
+						"after_program_xdp_seq":       4,
+						"after_program_xdp_ret":       -1,
+						"after_program_xdp_ret_param": 1,
+					},
+					"plugin_b": {
+						"before_program_tc_seq":       2,
+						"before_program_tc_ret":       -1,
+						"after_program_tc_seq":        5,
+						"after_program_tc_ret":        -1,
+						"after_program_tc_ret_param":  1,
+						"before_program_xdp_seq":      2,
+						"before_program_xdp_ret":      -1,
+						"after_program_xdp_seq":       5,
+						"after_program_xdp_ret":       -1,
+						"after_program_xdp_ret_param": 1,
+					},
+				},
+			},
+		},
+		{
+			name: "plugin_b returns an error in PrepareCollection() with AttachmentPolicyAlways",
+			transactions: map[string]transaction{
+				"plugin_a": {
+					prepareHooksReq:  prepareHooksReq,
+					prepareHooksResp: pluginAPrepareHooksResp,
+					loadHooksReq:     loadHooksReq("plugin_a"),
+					loadHooksResp:    &datapathplugins.InstrumentCollectionResponse{},
+				},
+				"plugin_b": {
+					prepareHooksReq: prepareHooksReq,
+					prepareHooksErr: errors.New("some error"),
+				},
+			},
+			attachmentPolicies: map[string]api_v2alpha1.CiliumDatapathPluginAttachmentPolicy{
+				"plugin_a": api_v2alpha1.AttachmentPolicyAlways,
+				"plugin_b": api_v2alpha1.AttachmentPolicyAlways,
+			},
+			expectedErr: errors.New("some error"),
+		},
+		{
+			name: "plugin_b returns an error in PrepareCollection() with AttachmentPolicyBestEffort",
+			transactions: map[string]transaction{
+				"plugin_a": {
+					prepareHooksReq:  prepareHooksReq,
+					prepareHooksResp: pluginAPrepareHooksResp,
+					loadHooksReq:     loadHooksReq("plugin_a"),
+					loadHooksResp:    &datapathplugins.InstrumentCollectionResponse{},
+				},
+				"plugin_b": {
+					prepareHooksReq: prepareHooksReq,
+					prepareHooksErr: errors.New("some error"),
+				},
+			},
+			attachmentPolicies: map[string]api_v2alpha1.CiliumDatapathPluginAttachmentPolicy{
+				"plugin_a": api_v2alpha1.AttachmentPolicyAlways,
+				"plugin_b": api_v2alpha1.AttachmentPolicyBestEffort,
+			},
+			inputValues: inputValues{
+				hook: map[string]map[string]int32{
+					"plugin_a": {
+						"before_program_tc_ret":  -1,
+						"after_program_tc_ret":   -1,
+						"before_program_xdp_ret": -1,
+						"after_program_xdp_ret":  -1,
+					},
+				},
+			},
+			expectedOutputValues: outputValues{
+				base: map[string]int32{
+					"program_tc_seq":  2,
+					"program_xdp_seq": 2,
+				},
+				returns: map[string]uint32{
+					"program_tc":  1,
+					"program_xdp": 1,
+				},
+				hook: map[string]map[string]int32{
+					"plugin_a": {
+						"before_program_tc_seq":       1,
+						"before_program_tc_ret":       -1,
+						"after_program_tc_seq":        3,
+						"after_program_tc_ret":        -1,
+						"after_program_tc_ret_param":  1,
+						"before_program_xdp_seq":      1,
+						"before_program_xdp_ret":      -1,
+						"after_program_xdp_seq":       3,
+						"after_program_xdp_ret":       -1,
+						"after_program_xdp_ret_param": 1,
+					},
+				},
+			},
+		},
+		{
+			name: "plugin_b returns an error in InstrumentCollection() with AttachmentPolicyAlways",
+			transactions: map[string]transaction{
+				"plugin_a": {
+					prepareHooksReq:  prepareHooksReq,
+					prepareHooksResp: pluginAPrepareHooksResp,
+					loadHooksReq:     loadHooksReq("plugin_a"),
+					loadHooksResp:    &datapathplugins.InstrumentCollectionResponse{},
+				},
+				"plugin_b": {
+					prepareHooksReq:  prepareHooksReq,
+					prepareHooksResp: pluginBPrepareHooksResp,
+					loadHooksReq:     loadHooksReq("plugin_b"),
+					loadHooksErr:     errors.New("some error"),
+				},
+			},
+			attachmentPolicies: map[string]api_v2alpha1.CiliumDatapathPluginAttachmentPolicy{
+				"plugin_a": api_v2alpha1.AttachmentPolicyAlways,
+				"plugin_b": api_v2alpha1.AttachmentPolicyAlways,
+			},
+			expectedErr: errors.New("some error"),
+		},
+		{
+			name: "plugin_b returns an error in InstrumentCollection() with AttachmentPolicyBestEffort",
+			transactions: map[string]transaction{
+				"plugin_a": {
+					prepareHooksReq:  prepareHooksReq,
+					prepareHooksResp: pluginAPrepareHooksResp,
+					loadHooksReq:     loadHooksReq("plugin_a"),
+					loadHooksResp:    &datapathplugins.InstrumentCollectionResponse{},
+				},
+				"plugin_b": {
+					prepareHooksReq:  prepareHooksReq,
+					prepareHooksResp: pluginBPrepareHooksResp,
+					loadHooksReq:     loadHooksReq("plugin_b"),
+					loadHooksErr:     errors.New("some error"),
+				},
+			},
+			attachmentPolicies: map[string]api_v2alpha1.CiliumDatapathPluginAttachmentPolicy{
+				"plugin_a": api_v2alpha1.AttachmentPolicyAlways,
+				"plugin_b": api_v2alpha1.AttachmentPolicyBestEffort,
+			},
+			expectedErr: errors.New("some error"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			logger := hivetest.Logger(t)
+			tmp := testutils.TempBPFFS(t)
+			plugins := fakePluginRegistry{}
+			defer plugins.closeAll()
+
+			for p, t := range tc.transactions {
+				plugins[p] = &fakePlugin{
+					name:             p,
+					attachmentPolicy: tc.attachmentPolicies[p],
+					transaction: &transaction{
+						prepareHooksResp: t.prepareHooksResp,
+						prepareHooksErr:  t.prepareHooksErr,
+						loadHooksResp:    t.loadHooksResp,
+						loadHooksErr:     t.loadHooksErr,
+					},
+				}
+			}
+
+			l := newBPFCollectionLoader(true, bpffsPluginsOperationsDir(tmp))
+
+			pinsDir := filepath.Join(tmp, "pins_dir")
+			baseColl, commit, cleanup, err := l.Load(
+				t.Context(),
+				logger,
+				baseSpec,
+				&bpf.CollectionOptions{},
+				&config.Config{
+					Plugins: plugins.Plugins(),
+				},
+				&datapathplugins.AttachmentContext{},
+				pinsDir,
+			)
+			if tc.expectedErr != nil {
+				require.ErrorContains(t, err, tc.expectedErr.Error())
+				return
+			} else {
+				require.NoError(t, err)
+			}
+
+			require.NoError(t, commit())
+			cleanup()
+
+			for name, plugin := range plugins {
+				if diff := cmp.Diff(
+					tc.transactions[name],
+					*plugin.transaction,
+					cmp.AllowUnexported(transaction{}),
+					cmpopts.EquateErrors(),
+					protocmp.Transform(),
+					protocmp.SortRepeated(func(a, b *datapathplugins.InstrumentCollectionRequest_Hook) bool {
+						if a.Target == b.Target {
+							return a.Type < b.Type
+						}
+
+						return a.Target < b.Target
+					}),
+					protocmp.IgnoreFields(&datapathplugins.InstrumentCollectionRequest{}, "pins"),
+					protocmp.IgnoreFields(&datapathplugins.InstrumentCollectionRequest_Collection_Map{}, "id"),
+					protocmp.IgnoreFields(&datapathplugins.InstrumentCollectionRequest_Collection_Program{}, "id"),
+					protocmp.IgnoreFields(&datapathplugins.InstrumentCollectionRequest_Hook{}, "pin_path"),
+					protocmp.IgnoreFields(&datapathplugins.InstrumentCollectionRequest_Hook_AttachTarget{}, "program_id"),
+				); diff != "" {
+					t.Fatalf("transaction for plugin %s did not match what was expected (-want +got):\n%s", name, diff)
+				}
+			}
+
+			// Fill in the blanks
+			pluginHooksObjs := map[string]*ebpf.Collection{}
+			for name, plugin := range plugins {
+				pluginHooksObjs[name] = plugin.coll
+
+				if tc.expectedOutputValues.hook[name] != nil {
+					for varName := range plugin.coll.Variables {
+						if _, ok := tc.expectedOutputValues.hook[name][varName]; !ok {
+							tc.expectedOutputValues.hook[name][varName] = 0
+						}
+					}
+				}
+			}
+			for name := range baseColl.Variables {
+				if _, ok := tc.expectedOutputValues.base[name]; !ok {
+					tc.expectedOutputValues.base[name] = 0
+				}
+			}
+
+			outputs := runTestProgs(t, tc.inputValues, baseColl, pluginHooksObjs)
+			if diff := cmp.Diff(
+				tc.expectedOutputValues,
+				outputs,
+				cmp.AllowUnexported(outputValues{}),
+			); diff != "" {
+				t.Errorf("outputs did not match what was expected (-want +got):\n%s", diff)
 			}
 		})
 	}
