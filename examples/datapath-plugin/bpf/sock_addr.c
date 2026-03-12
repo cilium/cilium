143edb2058fcbad05bb8de499c2e3a562718c07b
Jordan Rife <jrife@google.com>
2026-03-12 13:53:49 -0700

    plugins, examples: Add datapath plugin example
    Create an example plugin that instruments every kind of instrumentable
    program with a before and after hook that prints that current attachment
    context.
    
    `make kind-image-datapath-plugin` builds the example plugin and loads
    the image into a kind cluster.
    
    `make kind-install-cilium-datapath-plugin` installs the datapath plugin
    in a kind cluster.
    
    Signed-off-by: Jordan Rife <jrife@google.com>

---
+++ examples/datapath-plugin/bpf/sock_addr.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#include <bpf/ctx/unspec.h>
+#include "common.h"
+
+__section("freplace")
+int before(struct bpf_sock_addr *ctx __maybe_unused)
+{
+	printk("before %s\n", attachment_context);
+
+	return SYS_PROCEED;
+}
+
+__section("freplace")
+int after(struct bpf_sock_addr *ctx __maybe_unused, int ret)
+{
+	printk("after %s (ret=%d)\n", attachment_context, ret);
+
+	return SYS_PROCEED;
+}
+
+BPF_LICENSE("Dual BSD/GPL");
+
