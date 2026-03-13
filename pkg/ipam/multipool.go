cilium/cilium

commit db32834c543f48dfdd19a9c93f850cee25bc3782

ipam: Fix concurrent map access to multipool map
[ upstream commit 45523ba54c2c026afe5af8dbd9a4755021ff7473 ]

[ backporter's note: pkg/ipam/multipool_manager.go doesn't exist in this
  branch. Move all upstream logic to pkg/ipam/multipool.go.
]

For some reason, I forgot to acquire the mutex when accessing the
multipool map in 788d1aa82b2d ("ipam, metrics: Add new capacity
metric"). Fix it now to prevent the concurrent map access panic as seen
in https://github.com/cilium/cilium/issues/44107.

Fixes: 788d1aa82b2d ("ipam, metrics: Add new capacity
metric")
Fixes: https://github.com/cilium/cilium/issues/44107

Co-authored-by: Fabio Falzoi <fabio.falzoi@isovalent.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipam/multipool.go
@@ -251,6 +251,7 @@ type multiPoolManager struct {
 	preallocatedIPsPerPool preAllocatePerPool
 	pendingIPsPerPool      *pendingAllocationsPerPool
 
+	poolsMutex   lock.Mutex
 	pools        map[Pool]*poolPair
 	poolsUpdated chan struct{}
 
@@ -823,6 +824,27 @@ func (m *multiPoolManager) Allocator(family Family) Allocator {
 	}
 }
 
+func (m *multiPoolManager) capacity(family Family) uint64 {
+	m.poolsMutex.Lock()
+	defer m.poolsMutex.Unlock()
+
+	var cap uint64
+	for _, pool := range m.pools {
+		var p *podCIDRPool
+		switch family {
+		case IPv4:
+			p = pool.v4
+		case IPv6:
+			p = pool.v6
+		}
+		if p == nil {
+			continue
+		}
+		cap += uint64(p.capacity())
+	}
+	return uint64(cap)
+}
+
 type multiPoolAllocator struct {
 	manager *multiPoolManager
 	family  Family
@@ -853,21 +875,7 @@ func (c *multiPoolAllocator) Dump() (map[Pool]map[string]string, string) {
 }
 
 func (c *multiPoolAllocator) Capacity() uint64 {
-	var capacity uint64
-	for _, pool := range c.manager.pools {
-		var p *podCIDRPool
-		switch c.family {
-		case IPv4:
-			p = pool.v4
-		case IPv6:
-			p = pool.v6
-		}
-		if p == nil {
-			continue
-		}
-		capacity += uint64(p.capacity())
-	}
-	return uint64(capacity)
+	return c.manager.capacity(c.family)
 }
 
 func (c *multiPoolAllocator) RestoreFinished() {

commit fd0c8a50e1879b4936613cb88f801d81a964a4f9

ipam: Use existing mutex for multipool capacity synchronization
The backport conflict resolution incorrectly introduced a new
poolsMutex that isn't used anywhere else, hence providing no actual
synchronization. Use the existing m.mutex instead, matching the
upstream fix.

Fixes: db32834c543f ("ipam: Fix concurrent map access to multipool map")
Fixes: https://github.com/cilium/cilium/pull/44517

Reported-by: Marco Iorio <marco.iorio@isovalent.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipam/multipool.go
@@ -251,7 +251,6 @@ type multiPoolManager struct {
 	preallocatedIPsPerPool preAllocatePerPool
 	pendingIPsPerPool      *pendingAllocationsPerPool
 
-	poolsMutex   lock.Mutex
 	pools        map[Pool]*poolPair
 	poolsUpdated chan struct{}
 
@@ -825,8 +824,8 @@ func (m *multiPoolManager) Allocator(family Family) Allocator {
 }
 
 func (m *multiPoolManager) capacity(family Family) uint64 {
-	m.poolsMutex.Lock()
-	defer m.poolsMutex.Unlock()
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 
 	var cap uint64
 	for _, pool := range m.pools {