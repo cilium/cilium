8967a8f3e646aeba3703c43ae199a66085155978
Simone Magnani <simone.magnani@isovalent.com>
2026-03-16 11:21:23 +0100

    l7lb: fix bypassing ingress policies for local backends
    [ upstream commit ea17ed8729 ]
    
    [ backporter's notes:
      * using macro CILIUM_HOST_IFINDEX instead of CONFIG(cilium_host_ifindex) as it was not backported.
      * fixed conflicts in bpf_lxc.c, as #43226, #42926 and others haven't been backported.
    ]
    
    In commit d1d8e7a35b35 ("datapath: Add support for re-entering LXC egress path after L7 LB"),
    we enabled support for re-entering the LXC egress path after a packet is
    processed by a L7 LB. This allows us to correctly apply egress policies to
    packets sent by a L7 LB to local backends.
    
    Without per-endpoint routes, the pod policies are checked from the tail
    call in bpf_host. We reach this code from cilium_host (ctx->ifindex).
    
    With per-endpoint routes, the packet is handled directly in the
    local backend pod `cil_to_container` path, completely skipping ingress
    policies. Returning CTX_ACT_OK at this point would bypass policies.
    To fix this, we need to hairpin the packet back to cil_to_container
    (the ctx->mark is already cleared at the beginning of the codepath).
    This would allow us to correctly apply ingress policies on the local
    backend ingress path, restoring the expected behavior.
    
    Fixes: d1d8e7a35b35 ("datapath: Add support for re-entering LXC egress path after L7 LB")
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

--- bpf/tests/l7_lb_local_backend.h
+++ bpf/tests/l7_lb_local_backend.h
@@ -70,6 +70,9 @@ mock_tail_call_dynamic(struct __ctx_buff *ctx __maybe_unused,
 #ifdef ENABLE_ROUTING
 /* We are tail calling from cilium_host */
 # define CILIUM_HOST_IFINDEX 1
+#else
+/* We are tail calling from bpf_lxc, let's change cilium_host ifindex */
+# define CILIUM_HOST_IFINDEX 2
 #endif
 
 #define CILIUM_NET_IFINDEX 10
@@ -82,6 +85,8 @@ ASSIGN_CONFIG(__u32, interface_ifindex, 12)
 /* Test that a packet received from a L7LB for a local backend gets correctly handled:
  * - in case of per-endpoint routes disabled, packet tail calls from
      cil_from_host, and it will return to stack
+ * - in case of per-endpoint routes, packet tail calls from
+     cil_to_container, and it will be hairpinned back to handle ingress policies.
  * Given we cannot import both bpf_host and bpf_lxc, in our SETUP functions
  * we will simulate hitting the `tail_call_egress_policy(ctx, lxc_id)` codepath.
  */
@@ -132,6 +137,10 @@ int l7_lb_local_backend_v4_check(const struct __ctx_buff *ctx)
 
 #ifdef ENABLE_ROUTING
 	assert(*status_code == CTX_ACT_OK);
+#else
+	assert(*status_code == CTX_ACT_REDIRECT);
+
+	assert(redirect_ifindex == ctx_get_ifindex(ctx));
 #endif
 
 	test_finish();
@@ -185,6 +194,10 @@ int l7_lb_local_backend_v6_check(const struct __ctx_buff *ctx)
 
 #ifdef ENABLE_ROUTING
 	assert(*status_code == CTX_ACT_OK);
+#else
+	assert(*status_code == CTX_ACT_REDIRECT);
+
+	assert(redirect_ifindex == ctx_get_ifindex(ctx));
 #endif
 
 	test_finish();
