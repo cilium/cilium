b9a38c337c6a1425bf26e3d28cfef3037a8f84ea
Deepesh Pathak <deepeshpathak09@gmail.com>
2026-03-15 22:47:54 -0700

    envoy: fix npds listeners accounting in xds server
    When a network policy is updated, the decision to wait on ACK
    from envoy depends on weather or not there is any listener filter that
    requires NPDS(presence of `cilium.bpf_metadata` filter). Before this
    change, only internal proxy listeners were accounted. This caused
    NetworkPolicy update wait to be skipped if **only** the listeners configured
    through CiliumEnvoyConfig were present.
    
    Given that all CEC listeners get injected with `cilium.bpf_metadata`
    filter for L3 policy enforcement, incorrect accounting leads to
    a race condition where BPF policy map redirects can be configured before
    envoy receives the up-to-date policy information. This can cause traffic
    interruption due to policy denials in envoy.
    
    This commit fixes this issue by keeping track of `cilium.bpf_metadata`
    filter across all envoy listeners to determine the need of NPDS
    subscription.
    
    Signed-off-by: Deepesh Pathak <deepeshpathak09@gmail.com>

--- pkg/envoy/utils.go
+++ pkg/envoy/utils.go
@@ -6,6 +6,15 @@ package envoy
 import (
 	"regexp"
 	"strings"
+
+	cilium "github.com/cilium/proxy/go/cilium/api"
+	envoy_config_listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+
+	"github.com/cilium/cilium/pkg/bpf"
+)
+
+const (
+	ciliumBPFMetadataListenerFilterName = "cilium.bpf_metadata"
 )
 
 var (
@@ -57,4 +66,27 @@ func sanitizeServerNamePattern(pattern string) string {
 		return subdomainWildcardSpecifierPrefix + pattern
 	}
 	return pattern
+}
+
+// listenerRequiresNPDS returns true if the listener carries a cilium.bpf_metadata
+// listener filter that will start an NPDS (Network Policy Discovery Service) client.
+//
+// For the listener to start the NPDS client it must have a correctly configured
+// bpf_root (matching the system's BPF filesystem root)
+//
+// Corresponding proxy logic: https://github.com/cilium/proxy/blob/4054ac22c6338f0372aac9f23a7cda99e8ebf6da/cilium/bpf_metadata.cc#L273
+func listenerRequiresNPDS(listener *envoy_config_listener.Listener) bool {
+	for _, lf := range listener.GetListenerFilters() {
+		if lf.GetName() != ciliumBPFMetadataListenerFilterName || lf.GetTypedConfig() == nil {
+			continue
+		}
+
+		var bpfMeta cilium.BpfMetadata
+		err := lf.GetTypedConfig().UnmarshalTo(&bpfMeta)
+
+		if err == nil && bpfMeta.GetBpfRoot() == bpf.BPFFSRoot() {
+			return true
+		}
+	}
+	return false
 }
