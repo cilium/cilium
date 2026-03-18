1d14b0694658e9a98a1f0a35cb1eb14dc171423b
Alice Mikityanska <alice@isovalent.com>
2026-03-18 22:19:50 +0200

    contrib: Fix UID != 1000 in builder.sh
    If the host user is not 1000, it will cause permission errors in the
    container, where /home/ubuntu is owned by 1000:1000. Change UID:GID of
    the ubuntu user and ownership of the home directory, according to the
    host user ID to allow seamless interaction with host-mounted paths.
    
    Host directories for volumes mounted in Docker must be created as the
    needed user if they don't exist. Otherwise, Docker would create them as
    root and make them unwritable inside the container as a normal user.
    This issue existed before, but was masked in GitHub runners (user 1001):
    as the target user 1001 didn't exist inside the container, HOME was
    detected as /, and GOCACHE used /.cache/go-build, with /.cache
    precreated with mode 0777 while building the container. It "worked", but
    it didn't actually reuse the host GOCACHE mounted at
    /home/ubuntu/.cache/go-build (before this patch) or even at the host
    GOCACHE path (before this series).
    
    Signed-off-by: Alice Mikityanska <alice@isovalent.com>

--- contrib/scripts/builder.sh
+++ contrib/scripts/builder.sh
@@ -12,43 +12,61 @@ USER_OPTION=()
 USER_PATH="/root"
 
 if [ -z "${RUN_AS_ROOT:-}" ]; then
-	USER_OPTION=(--user "$(id -u):$(id -g)")
+	USERID=$(id -u)
+	GROUPID=$(id -g)
+	USER_OPTION=(--user "$USERID:$GROUPID")
 	USER_PATH="/home/ubuntu"
 fi
 
-MOUNT_GOCACHE_DIR=()
-if [ -n "${GO}" ]; then
-	MOUNT_GOCACHE_DIR=(-v "$(go env GOCACHE):$USER_PATH/.cache/go-build")
-fi
+# Ensure that the GOCACHE and GOMODCACHE directories exist, create as the
+# current user if not: Docker would create it as root if they don't exist, and
+# it wouldn't be writable by the user inside the container.
 
+MOUNT_GOCACHE_DIR=()
 if [ -n "${BUILDER_GOCACHE_DIR:-}" ]; then
 	MOUNT_GOCACHE_DIR=(-v "${BUILDER_GOCACHE_DIR}:${USER_PATH}/.cache/go-build")
+	mkdir -p "$BUILDER_GOCACHE_DIR"
+elif [ -n "${GO}" ]; then
+	GOCACHE=$("$GO" env GOCACHE)
+	MOUNT_GOCACHE_DIR=(-v "$GOCACHE:$USER_PATH/.cache/go-build")
+	mkdir -p "$GOCACHE"
 fi
 
 MOUNT_GOMODCACHE_DIR=()
-if [ -n "${GO}" ]; then
-	MOUNT_GOMODCACHE_DIR=(-v "$(go env GOMODCACHE):/go/pkg/mod")
-fi
-
 if [ -n "${BUILDER_GOMODCACHE_DIR:-}" ]; then
 	MOUNT_GOMODCACHE_DIR=(-v "${BUILDER_GOMODCACHE_DIR}:/go/pkg/mod")
+	mkdir -p "$BUILDER_GOMODCACHE_DIR"
+elif [ -n "${GO}" ]; then
+	GOMODCACHE=$("$GO" env GOMODCACHE)
+	MOUNT_GOMODCACHE_DIR=(-v "$GOMODCACHE:/go/pkg/mod")
+	mkdir -p "$GOMODCACHE"
 fi
 
 MOUNT_CCACHE_DIR=()
 LOCAL_CCACHE_DIR=$(ccache -k cache_dir 2> /dev/null || :)
 if [ -n "${BUILDER_CCACHE_DIR:-}" ]; then
 	MOUNT_CCACHE_DIR=(-v "$BUILDER_CCACHE_DIR:$USER_PATH/.cache/ccache")
+	mkdir -p "$BUILDER_CCACHE_DIR"
 elif [ -d "$LOCAL_CCACHE_DIR" ]; then
 	MOUNT_CCACHE_DIR=(-v "$LOCAL_CCACHE_DIR:$USER_PATH/.cache/ccache")
 fi
 
-docker run --rm \
-	"${USER_OPTION[@]}" \
+echo "Docker params: ${MOUNT_GOCACHE_DIR[@]} ${MOUNT_GOMODCACHE_DIR[@]} ${MOUNT_CCACHE_DIR[@]}"
+CONTAINER=$(docker create \
 	"${MOUNT_GOCACHE_DIR[@]}" \
 	"${MOUNT_GOMODCACHE_DIR[@]}" \
 	"${MOUNT_CCACHE_DIR[@]}" \
 	-v "$PWD":/go/src/github.com/cilium/cilium \
 	-w /go/src/github.com/cilium/cilium \
-	${DOCKER_ARGS:+$DOCKER_ARGS} \
 	"$CILIUM_BUILDER_IMAGE" \
-	"$@"
+	sleep infinity
+)
+trap 'docker rm -f "$CONTAINER"' EXIT
+docker start "$CONTAINER"
+if [ -z "${RUN_AS_ROOT:-}" ]; then
+	docker exec "$CONTAINER" groupmod -g "$GROUPID" ubuntu
+	# usermod fixes UIDs, but GIDs need to be fixed manually.
+	docker exec "$CONTAINER" chown -Rhc --from=:1000 :ubuntu /home/ubuntu
+	docker exec "$CONTAINER" usermod -u "$USERID" ubuntu
+fi
+docker exec "${USER_OPTION[@]}" ${DOCKER_ARGS:+$DOCKER_ARGS} "$CONTAINER" "$@"
