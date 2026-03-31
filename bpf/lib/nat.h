699ad58a46a25913e8b9427a50a98b747ef14f89
Dylan Reimerink <dylan.reimerink@isovalent.com>
2026-03-31 16:54:55 +0200

    bpf: Reduce stack usage and complexity of `tail_handle_snat_fwd_ipv6`
    The goal of this commit it to reduce the stack usage of the
    `tail_handle_snat_fwd_ipv6` tailcall. To do so we move a number of
    larger variables such as `struct ipv6_ct_tuple` and
    `struct ipv6_nat_target` to per-CPU map values.
    
    Moving variables to aux data on its own would reduce stack usage at the
    cost of complexity. Likely due to the verifier not being able to track
    the state of these variables anymore, requiring more states to be
    explored. A big reason for the complexity in  `nodeport_snat_fwd_ipv6`
    is due to the number of different permutations of code paths before we
    get to `snat_v6_nat`.
    
    By moving `snat_v6_nat` to a global function we reduce the number of
    state permutations to 1 upon entering the function, leading to a
    combined reduction in both stack usage and complexity.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- bpf/lib/nat.h
+++ bpf/lib/nat.h
@@ -1813,6 +1813,12 @@ struct {
 	__type(key, int);
 	__type(value, struct ipv6_nat_target);
 } nat_target_storage __section_maps_btf;
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 1);
+	__type(key, int);
+	__type(value, struct trace_ctx);
+} trace_ctx_storage __section_maps_btf;
 
 __noinline __weak int
 snat_v6_needs_masquerade(struct __ctx_buff *ctx __maybe_unused,
@@ -1952,17 +1958,33 @@ __snat_v6_nat(struct __ctx_buff *ctx, struct ipv6_ct_tuple *tuple,
 	return ret;
 }
 
-static __always_inline __maybe_unused int
-snat_v6_nat(struct __ctx_buff *ctx, struct ipv6_ct_tuple *tuple,
-	    struct ipv6hdr *ip6, fraginfo_t fraginfo,
-	    int off, struct ipv6_nat_target *target,
-	    struct trace_ctx *trace, __s8 *ext_err)
+__noinline __weak int
+snat_v6_nat(struct __ctx_buff *ctx, fraginfo_t fraginfo, int off, __s8 *ext_err)
 {
 	struct ipv6_nat_entry *state = NULL;
+	struct ipv6_nat_target *target;
+	struct ipv6_ct_tuple *tuple;
+	struct trace_ctx *trace;
+	void *data, *data_end;
+	struct ipv6hdr *ip6;
 	__u16 port_off = 0;
+	int zero = 0;
 	int ret;
 
+	tuple = map_lookup_elem(&ct_tuple_storage, &zero);
+	if (!tuple)
+		return DROP_INVALID;
+	target = map_lookup_elem(&nat_target_storage, &zero);
+	if (!target)
+		return DROP_INVALID;
+	trace = map_lookup_elem(&trace_ctx_storage, &zero);
+	if (!trace)
+		return DROP_INVALID;
+
 	build_bug_on(sizeof(struct ipv6_nat_entry) > 64);
+
+	if (!revalidate_data(ctx, &data, &data_end, &ip6))
+		return DROP_INVALID;
 
 	switch (tuple->nexthdr) {
 	case IPPROTO_TCP:
