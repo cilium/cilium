39858f2751331108bb9dddd8e0a8a951be639dda
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2026-03-31 23:11:16 -0700

    bgp: Consolidate fakeRouterWithReset to FakeRouter
    There's an adhoc implementation of the fake router in the
    policies_test.go. What it actually does is implementing the
    ResetNeighbor and ResetAllNeighbors call which is anyways a part of the
    types.Router interface. We can consolidate this into the FakeRouter.
    
    Since the FakeRouter and the test are in the different package, we
    cannot access to the `resets` field directly anymore. Add a
    FakeRouter-specific getter method GetResets which returns a copy of the
    internal reset status.
    
    Also introduce a RWMutex to the FakeRouter to make it thread safe.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

--- pkg/bgp/fake/router.go
+++ pkg/bgp/fake/router.go
@@ -5,19 +5,25 @@ package fake
 
 import (
 	"context"
+	"maps"
+	"net/netip"
 
 	"github.com/cilium/cilium/pkg/bgp/types"
+	"github.com/cilium/cilium/pkg/lock"
 )
 
 type FakeRouter struct {
+	mu       lock.RWMutex
 	paths    map[string]*types.Path
 	policies map[string]*types.RoutePolicy
+	resets   map[netip.Addr]types.SoftResetDirection
 }
 
 func NewFakeRouter() *FakeRouter {
 	return &FakeRouter{
 		paths:    make(map[string]*types.Path),
 		policies: make(map[string]*types.RoutePolicy),
+		resets:   make(map[netip.Addr]types.SoftResetDirection),
 	}
 }
 
@@ -36,31 +42,56 @@ func (f *FakeRouter) RemoveNeighbor(ctx context.Context, n *types.Neighbor) erro
 }
 
 func (f *FakeRouter) ResetNeighbor(ctx context.Context, r types.ResetNeighborRequest) error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	if r.Soft {
+		f.resets[r.PeerAddress] = r.SoftResetDirection
+	}
 	return nil
 }
 
 func (f *FakeRouter) ResetAllNeighbors(ctx context.Context, r types.ResetAllNeighborsRequest) error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	if r.Soft {
+		// Use an invalid address to indicate the all reset
+		f.resets[netip.Addr{}] = r.SoftResetDirection
+	}
 	return nil
 }
 
+func (f *FakeRouter) GetResets() map[netip.Addr]types.SoftResetDirection {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	return maps.Clone(f.resets)
+}
+
 func (f *FakeRouter) AdvertisePath(ctx context.Context, p types.PathRequest) (types.PathResponse, error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
 	path := p.Path
 	f.paths[path.NLRI.String()] = path
 	return types.PathResponse{Path: path}, nil
 }
 
 func (f *FakeRouter) WithdrawPath(ctx context.Context, p types.PathRequest) error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
 	path := p.Path
 	delete(f.paths, path.NLRI.String())
 	return nil
 }
 
 func (f *FakeRouter) AddRoutePolicy(ctx context.Context, p types.RoutePolicyRequest) error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
 	f.policies[p.Policy.Name] = p.Policy
 	return nil
 }
 
 func (f *FakeRouter) RemoveRoutePolicy(ctx context.Context, p types.RoutePolicyRequest) error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
 	delete(f.policies, p.Policy.Name)
 	return nil
 }
@@ -74,6 +105,8 @@ func (f *FakeRouter) GetPeerStateLegacy(ctx context.Context) (types.GetPeerState
 }
 
 func (f *FakeRouter) GetRoutes(ctx context.Context, r *types.GetRoutesRequest) (*types.GetRoutesResponse, error) {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
 	var routes []*types.Route
 	for _, path := range f.paths {
 		routes = append(routes, &types.Route{
@@ -85,6 +118,8 @@ func (f *FakeRouter) GetRoutes(ctx context.Context, r *types.GetRoutesRequest) (
 }
 
 func (f *FakeRouter) GetRoutePolicies(ctx context.Context) (*types.GetRoutePoliciesResponse, error) {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
 	var policies []*types.RoutePolicy
 	for _, policy := range f.policies {
 		policies = append(policies, policy)
