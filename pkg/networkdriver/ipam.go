91cb44dccb8cd47baa0f91f24595cc9d30543d42
Fabio Falzoi <fabio.falzoi@isovalent.com>
2026-04-02 19:44:38 +0200

    network-driver: Configure static routes for device
    Configure additional static routes from the ResourceClaim Status when
    creating or removing the Pod sandbox.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/networkdriver/ipam.go
+++ pkg/networkdriver/ipam.go
@@ -38,11 +38,11 @@ const (
 	ResourceIPAMMultiPoolPreAllocation = "resource-ipam-multi-pool-pre-allocation"
 )
 
-type ipamAction string
+type action string
 
 const (
-	addrAdd ipamAction = "add"
-	addrDel ipamAction = "del"
+	add action = "add"
+	del action = "del"
 )
 
 var resourceIPAM = cell.Group(
