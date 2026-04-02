1d2652e558937ae587aa64e1b3b48a777b9a7430
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2026-04-01 21:36:04 -0700

    bgp: Make BGP Hive Shell command overridable
    Instead of directly provide the Hive Shell command from BGPCommands(),
    put one level of indirection and provide the map of command name =>
    Cmd object to the Hive. So that we can later override the command
    instance using cell.DecolateAll which is useful for overriding command
    with some extension.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

--- pkg/bgp/commands/cell.go
+++ pkg/bgp/commands/cell.go
@@ -11,11 +11,22 @@ import (
 	"github.com/cilium/cilium/pkg/bgp/agent"
 )
 
-var Cell = cell.Provide(BGPCommands)
+var Cell = cell.Provide(
+	// Provide command map then provide it as a a script.CmdsOut. This
+	// indirection gives us the ability to override the bgp/ commands with
+	// DecorateAll. This is useful for switching to a different command
+	// implementation with some custom logic.
+	NewBGPCommands,
+	func(cmds BGPCommands) hive.ScriptCmdsOut {
+		return hive.NewScriptCmds(cmds)
+	},
+)
 
-func BGPCommands(bgpMgr agent.BGPRouterManager) hive.ScriptCmdsOut {
-	return hive.NewScriptCmds(map[string]script.Cmd{
+type BGPCommands map[string]script.Cmd
+
+func NewBGPCommands(bgpMgr agent.BGPRouterManager) BGPCommands {
+	return map[string]script.Cmd{
 		"bgp/peers":  BGPPeersCmd(bgpMgr),
 		"bgp/routes": BGPRoutesCmd(bgpMgr),
-	})
+	}
 }
