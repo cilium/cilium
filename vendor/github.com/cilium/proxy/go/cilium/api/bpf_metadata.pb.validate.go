0425447c6e5db713adbf52f0eaf784d8cfae95e3
Jarno Rajahalme <jarno@isovalent.com>
2026-04-07 10:44:07 +0200

    envoy: Update API
    Update Envoy Go API for the renamed CiliumConfigSource.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- vendor/github.com/cilium/proxy/go/cilium/api/bpf_metadata.pb.validate.go
+++ vendor/github.com/cilium/proxy/go/cilium/api/bpf_metadata.pb.validate.go
@@ -176,11 +176,11 @@ func (m *BpfMetadata) validate(all bool) error {
 	}
 
 	if all {
-		switch v := interface{}(m.GetNpdsConfig()).(type) {
+		switch v := interface{}(m.GetCiliumConfigSource()).(type) {
 		case interface{ ValidateAll() error }:
 			if err := v.ValidateAll(); err != nil {
 				errors = append(errors, BpfMetadataValidationError{
-					field:  "NpdsConfig",
+					field:  "CiliumConfigSource",
 					reason: "embedded message failed validation",
 					cause:  err,
 				})
@@ -188,16 +188,16 @@ func (m *BpfMetadata) validate(all bool) error {
 		case interface{ Validate() error }:
 			if err := v.Validate(); err != nil {
 				errors = append(errors, BpfMetadataValidationError{
-					field:  "NpdsConfig",
+					field:  "CiliumConfigSource",
 					reason: "embedded message failed validation",
 					cause:  err,
 				})
 			}
 		}
-	} else if v, ok := interface{}(m.GetNpdsConfig()).(interface{ Validate() error }); ok {
+	} else if v, ok := interface{}(m.GetCiliumConfigSource()).(interface{ Validate() error }); ok {
 		if err := v.Validate(); err != nil {
 			return BpfMetadataValidationError{
-				field:  "NpdsConfig",
+				field:  "CiliumConfigSource",
 				reason: "embedded message failed validation",
 				cause:  err,
 			}
