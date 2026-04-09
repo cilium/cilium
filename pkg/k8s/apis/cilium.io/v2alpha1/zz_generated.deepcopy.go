1bf4942b8d0f53b4ca99f80626081bd7253222d0
Bernardo Soares <bersoare@isovalent.com>
2026-04-09 11:24:32 +0100

    network driver: allow macvlan filtering by parent name
    allow filtering macvlan interfaces by parent name
    
    Signed-off-by: Bernardo Soares <bersoare@isovalent.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- pkg/k8s/apis/cilium.io/v2alpha1/zz_generated.deepcopy.go
+++ pkg/k8s/apis/cilium.io/v2alpha1/zz_generated.deepcopy.go
@@ -1801,6 +1801,11 @@ func (in *CiliumNetworkDriverDeviceFilter) DeepCopyInto(out *CiliumNetworkDriver
 		*out = make([]string, len(*in))
 		copy(*out, *in)
 	}
+	if in.ParentIfNames != nil {
+		in, out := &in.ParentIfNames, &out.ParentIfNames
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
