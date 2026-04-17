cilium/cilium

commit fffae2c8a1f165bf7be9791ab3056d05784d468b

maps/netdev: switch cilium_devices from array to hash map
BPF_MAP_TYPE_ARRAY uses the key directly as an index into the backing
array. For cilium_devices, the index is the network interface index
(ifindex) which is allocated monotonically per network namespace and
only wraps around at INT_MAX (xa_limit_31b) [^1]. On nodes that have
hosted many pods over their lifetime, the host netns ifindex could thus
potentially exceed 4096. This would cause silently dropped map updates
in the control plane and either incorrect L3 device detection or packet
drops in the datapath.

Also, currently, if a network device goes away, the device_mac lookup
for its ifindex in the datapath will still return a non-NULL entry with
zero MAC and thus the if (!smac) check in fib_do_redirect[^2] would
never evalulate as true and the packet would be rewritten and redirected
with a zero source MAC.

To address these two problems, switch cilium_devices to map type
BPF_MAP_TYPE_HASH. For this map type max_entries limits the number of
active entries rather than the key range. Any ifindex is a valid key as
long as the map is not full, which eliminates the out-of-range failure
mode mentioned in the first section.

Since the map only holds currently selected Cilium devices, reduce
max_entries to 512. Even on large nodes with many pods, the number of
simultaneously active selected devices should be well within this limit.

The only downside of switching to BPF_MAP_TYPE_HASH is the slightly
higher overhead per lookup. This is likely acceptable given how
infrequently this map is accessed relative to other hot-path maps.

[^1]: https://elixir.bootlin.com/linux/v6.19.10/source/net/core/dev.c#L10795-L10796
[^2]: https://github.com/cilium/cilium/blob/86ed05736395ad91d66e3f9bae7f3e645214f187/bpf/lib/fib.h#L131-L135

Fixes: 585e63f7e965 ("datapath: move device metadata to cilium_devices map")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

bpf/lib/network_device.h
@@ -24,11 +24,11 @@ struct device_state {
  * verifier.
  */
 struct {
-	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(type, BPF_MAP_TYPE_HASH);
 	__type(key, __u32);
 	__type(value, struct device_state);
 	__uint(pinning, LIBBPF_PIN_BY_NAME);
-	__uint(max_entries, 4096);
+	__uint(max_entries, 512);
 } cilium_devices __section_maps_btf;
 
 static __always_inline const struct device_state *

pkg/datapath/maps/maps_generated.go
@@ -279,12 +279,12 @@ func newCiliumCTAny6GlobalSpec(btf *btf.Spec) *ebpf.MapSpec {
 func newCiliumDevicesSpec(btf *btf.Spec) *ebpf.MapSpec {
 	return &ebpf.MapSpec{
 		Name:       CiliumDevices,
-		Type:       ebpf.Array,
+		Type:       ebpf.Hash,
 		KeySize:    4,
 		Key:        anyTypeByName(btf, "__u32"),
 		ValueSize:  16,
 		Value:      anyTypeByName(btf, "device_state"),
-		MaxEntries: 4096,
+		MaxEntries: 512,
 		Flags:      0,
 		Pinning:    ebpf.PinByName,
 	}

pkg/maps/netdev/netdev.go
@@ -20,7 +20,7 @@ type Map interface {
 
 	IterateWithCallback(cb IterateCallback) error
 
-	Clear(ifindex uint32) error
+	Delete(ifindex uint32) error
 }
 
 type netDevMap struct {
@@ -32,10 +32,10 @@ func newNetDevMap() *netDevMap {
 	return &netDevMap{
 		Map: bpf.NewMap(
 			"cilium_devices",
-			ebpf.Array,
+			ebpf.Hash,
 			&index,
 			&DeviceState{},
-			4096,
+			512,
 			0,
 		),
 	}
@@ -64,12 +64,10 @@ func (m *netDevMap) IterateWithCallback(cb IterateCallback) error {
 	})
 }
 
-var zeroDeviceState = DeviceState{}
-
-// Clear resets an entry to the zero value.
-func (m *netDevMap) Clear(ifindex uint32) error {
+// Delete removes an entry from the map.
+func (m *netDevMap) Delete(ifindex uint32) error {
 	key := Index(ifindex)
-	return m.Map.Update(&key, &zeroDeviceState)
+	return m.Map.Delete(&key)
 }
 
 func (m *netDevMap) init() error {

pkg/maps/netdev/netdev_sync.go
@@ -94,8 +94,8 @@ func pruneStaleDevices(p netDevMapSyncParams, desired map[uint32]DeviceState) {
 		p.Logger.Warn("Failed to iterate network devices map", logfields.Error, err)
 	}
 	for _, ifindex := range stale {
-		if err := p.DeviceMap.Clear(ifindex); err != nil {
-			p.Logger.Warn("Failed to clear stale network devices map entry",
+		if err := p.DeviceMap.Delete(ifindex); err != nil {
+			p.Logger.Warn("Failed to delete stale network devices map entry",
 				logfields.Error, err,
 				logfields.Interface, ifindex,
 			)

pkg/maps/netdev/netdev_test.go
@@ -77,8 +77,7 @@ func TestPrivilegedNetDevMap(t *testing.T) {
 	require.Equal(t, state1, seen[1])
 	require.Equal(t, state2, seen[2])
 
-	require.NoError(t, dm.Clear(2))
-	got2, err = dm.Lookup(2)
-	require.NoError(t, err)
-	require.Equal(t, DeviceState{}, *got2)
+	require.NoError(t, dm.Delete(2))
+	_, err = dm.Lookup(2)
+	require.Error(t, err)
 }

commit fd57bf6d9b34fa85480b00be5c6714d4c5d1b804

bpf: let device_mac return zero MAC in case of missing device
After the switch of the cilium_devices map from array to hash map in
commit fffae2c8a1f1 ("maps/netdev: switch cilium_devices from array to
hash map"), device_mac will return NULL if the device is not present in
the map whereas before, it would always return a zero MAC and thus the
NULL check in fib_do_redirect would never hit. This is also consistent
with the behavior before commit 585e63f7e965 ("datapath: move device
metadata to cilium_devices map") switched  device metadata from macros
to the cilium_devices map. There, NATIVE_DEV_MAC_BY_IFINDEX would also
return a zero MAC if no device was found for a given ifindex lookup.

Restore that behavior by letting device_mac return a zero MAC in case
the ifindex isn't present in cilium_devices. This also allows to drop
the now unused DROP_NO_DEVICE drop reason. Since it was never part of a
released version (just on main branch), this can be done without
breaking API.

Fixes: fffae2c8a1f1 ("maps/netdev: switch cilium_devices from array to hash map")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

api/v1/flow/flow.pb.go
@@ -652,8 +652,6 @@ const (
 	DropReason_DROP_NO_EGRESS_IP DropReason = 204
 	// Punt packet to a user space proxy.
 	DropReason_DROP_PUNT_PROXY DropReason = 205
-	// A BPF program failed to look up information for a network device.
-	DropReason_DROP_NO_DEVICE DropReason = 206
 )
 
 // Enum value maps for DropReason.
@@ -735,7 +733,6 @@ var (
 		203: "DROP_EP_NOT_READY",
 		204: "DROP_NO_EGRESS_IP",
 		205: "DROP_PUNT_PROXY",
-		206: "DROP_NO_DEVICE",
 	}
 	DropReason_value = map[string]int32{
 		"DROP_REASON_UNKNOWN":                                   0,
@@ -814,7 +811,6 @@ var (
 		"DROP_EP_NOT_READY":                                     203,
 		"DROP_NO_EGRESS_IP":                                     204,
 		"DROP_PUNT_PROXY":                                       205,
-		"DROP_NO_DEVICE":                                        206,
 	}
 )
 
@@ -5920,7 +5916,7 @@ const file_flow_flow_proto_rawDesc = "" +
 	"\n" +
 	"\x06TRACED\x10\x06\x12\x0e\n" +
 	"\n" +
-	"TRANSLATED\x10\a*\xda\x11\n" +
+	"TRANSLATED\x10\a*\xc5\x11\n" +
 	"\n" +
 	"DropReason\x12\x17\n" +
 	"\x13DROP_REASON_UNKNOWN\x10\x00\x12\x1b\n" +
@@ -6001,8 +5997,7 @@ const file_flow_flow_proto_rawDesc = "" +
 	"\x13DROP_HOST_NOT_READY\x10\xca\x01\x12\x16\n" +
 	"\x11DROP_EP_NOT_READY\x10\xcb\x01\x12\x16\n" +
 	"\x11DROP_NO_EGRESS_IP\x10\xcc\x01\x12\x14\n" +
-	"\x0fDROP_PUNT_PROXY\x10\xcd\x01\x12\x13\n" +
-	"\x0eDROP_NO_DEVICE\x10\xce\x01*J\n" +
+	"\x0fDROP_PUNT_PROXY\x10\xcd\x01*J\n" +
 	"\x10TrafficDirection\x12\x1d\n" +
 	"\x19TRAFFIC_DIRECTION_UNKNOWN\x10\x00\x12\v\n" +
 	"\aINGRESS\x10\x01\x12\n" +

api/v1/flow/flow.proto
@@ -524,8 +524,6 @@ enum DropReason {
     DROP_NO_EGRESS_IP = 204;
     // Punt packet to a user space proxy.
     DROP_PUNT_PROXY = 205;
-    // A BPF program failed to look up information for a network device.
-    DROP_NO_DEVICE = 206;
 }
 
 enum TrafficDirection {

api/v1/observer/observer.pb.go
@@ -211,7 +211,6 @@ const DropReason_DROP_HOST_NOT_READY = flow.DropReason_DROP_HOST_NOT_READY
 const DropReason_DROP_EP_NOT_READY = flow.DropReason_DROP_EP_NOT_READY
 const DropReason_DROP_NO_EGRESS_IP = flow.DropReason_DROP_NO_EGRESS_IP
 const DropReason_DROP_PUNT_PROXY = flow.DropReason_DROP_PUNT_PROXY
-const DropReason_DROP_NO_DEVICE = flow.DropReason_DROP_NO_DEVICE
 
 var DropReason_name = flow.DropReason_name
 var DropReason_value = flow.DropReason_value

bpf/lib/drop_reasons.h
@@ -86,4 +86,3 @@
 #define DROP_EP_NOT_READY	-203
 #define DROP_NO_EGRESS_IP	-204
 #define DROP_PUNT_PROXY		-205 /* Mapped as drop code, though drop not necessary. */
-#define DROP_NO_DEVICE		-206

bpf/lib/fib.h
@@ -66,7 +66,6 @@ static __always_inline bool fib_ok(int ret)
   *
   * Returns:
   *   - result of BPF redirect
-  *   - DROP_NO_DEVICE when SMAC couldn't be resolved
   *   - DROP_NO_FIB when DMAC couldn't be resolved
   *   - other DROP reasons
   *
@@ -128,9 +127,6 @@ fib_do_redirect(struct __ctx_buff *ctx, const bool needs_l2_check,
 	} else {
 		const union macaddr *smac = device_mac(oif);
 		const union macaddr *dmac = NULL;
-
-		if (!smac)
-			return DROP_NO_DEVICE;
 
 		if (allow_neigh_map) {
 			/* The neigh_record_ip{4,6} locations are mainly from

bpf/lib/network_device.h
@@ -29,6 +29,7 @@ struct {
 	__type(value, struct device_state);
 	__uint(pinning, LIBBPF_PIN_BY_NAME);
 	__uint(max_entries, 512);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
 } cilium_devices __section_maps_btf;
 
 static __always_inline const struct device_state *
@@ -49,6 +50,7 @@ static __always_inline const union macaddr *
 device_mac(__u32 ifindex)
 {
 	const struct device_state *state = device_state_lookup(ifindex);
+	static union macaddr __zero_mac = {};
 
-	return state ? &state->mac : NULL;
+	return state ? &state->mac : &__zero_mac;
 }

pkg/datapath/maps/maps_generated.go
@@ -285,7 +285,7 @@ func newCiliumDevicesSpec(btf *btf.Spec) *ebpf.MapSpec {
 		ValueSize:  16,
 		Value:      anyTypeByName(btf, "device_state"),
 		MaxEntries: 512,
-		Flags:      0,
+		Flags:      unix.BPF_F_NO_PREALLOC,
 		Pinning:    ebpf.PinByName,
 	}
 }