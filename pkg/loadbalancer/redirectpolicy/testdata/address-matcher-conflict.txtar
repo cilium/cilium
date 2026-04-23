cilium/cilium

commit c6b2c2a053210c90b804257b83deaa1a012a1991

redirectpolicy: Only set redirection when target pods available
The local redirection should only be in effect when local target pods
are available. Fix the issue by only setting the redirect when the
LRP service has associated backends.

Fixes: #41450
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/loadbalancer/redirectpolicy/controller.go
@@ -256,64 +256,6 @@ func (c *lrpController) processRedirectPolicy(wtxn writer.WriteTxn, lrpID lb.Ser
 		}
 	}
 
-	switch lrp.LRPType {
-	case lrpConfigTypeSvc:
-		// Find frontends associated with the target service that match the redirection criteria and
-		// redirect them to the LRP "pseudo-service".
-		targetName := lrp.ServiceID
-		fes, watch := c.p.Writer.Frontends().ListWatch(wtxn, lb.FrontendByServiceName(targetName))
-		ws.Add(watch)
-		for fe := range fes {
-			// Only ClusterIP services can be redirected.
-			if fe.Type != lb.SVCTypeClusterIP {
-				continue
-			}
-			if shouldRedirectFrontend(c.p.Log, lrp, fe) {
-				c.p.Log.Debug("Redirecting frontend",
-					logfields.Frontend, fe,
-					logfields.ServiceName, targetName,
-					logfields.Target, &lrpServiceName)
-				c.p.Writer.SetRedirectTo(wtxn, fe, &lrpServiceName)
-			} else {
-				c.p.Writer.SetRedirectTo(wtxn, fe, nil)
-			}
-		}
-
-	case lrpConfigTypeAddr:
-		// In address-based mode there is no existing service/frontend to match against and
-		// instead the frontend is created here.
-		for _, feM := range lrp.FrontendMappings {
-			fe, _, found := c.p.Writer.Frontends().Get(wtxn, lb.FrontendByAddress(feM.feAddr))
-			if found {
-				if fe.Type != lb.SVCTypeLocalRedirect {
-					c.p.Log.Error("LocalRedirectPolicy matches an address owned by an existing service => refusing to override",
-						logfields.Address, feM.feAddr,
-						logfields.ServiceName, fe.ServiceName)
-				}
-				continue
-			}
-			_, err := c.p.Writer.UpsertFrontend(
-				wtxn,
-				lb.FrontendParams{
-					Address:     feM.feAddr,
-					Type:        lb.SVCTypeLocalRedirect,
-					ServiceName: lrpServiceName,
-					ServicePort: feM.feAddr.Port(),
-				},
-			)
-			if err != nil {
-				// Not expecting any errors here as the address conflict already handled above.
-				c.p.Log.Error("Failed to upsert frontend for LocalRedirectPolicy",
-					logfields.LRPName, lrp.ID,
-					logfields.Address, feM.feAddr,
-					logfields.Error, err)
-			}
-		}
-	case lrpConfigTypeNone:
-		cleanup(wtxn)
-		return ws, func(writer.WriteTxn) {}
-	}
-
 	// For each matching pod create a backend and associate it with the LocalRedirect
 	// service we just created above. We find pods by doing a prefix search with the
 	// namespace (more efficient than having a separate namespace index for pods).
@@ -332,7 +274,77 @@ func (c *lrpController) processRedirectPolicy(wtxn writer.WriteTxn, lrpID lb.Ser
 	}
 	c.updateRedirectBackends(wtxn, ws, lrp, matchingPods)
 	c.updateSkipLB(wtxn, ws, lrp, matchingPods)
+	c.updateRedirects(wtxn, ws, cleanup, lrp, matchingPods)
+
 	return ws, cleanup
+}
+
+func (c *lrpController) updateRedirects(wtxn writer.WriteTxn, ws *statedb.WatchSet, cleanup func(writer.WriteTxn), lrp *LocalRedirectPolicy, pods []podInfo) func(writer.WriteTxn) {
+	lrpServiceName := lrp.RedirectServiceName()
+	switch lrp.LRPType {
+	case lrpConfigTypeSvc:
+		// Find frontends associated with the target service that match the redirection criteria and
+		// redirect them to the LRP "pseudo-service".
+		targetName := lrp.ServiceID
+		fes, watch := c.p.Writer.Frontends().ListWatch(wtxn, lb.FrontendByServiceName(targetName))
+		ws.Add(watch)
+		for fe := range fes {
+			// Only ClusterIP services can be redirected.
+			if fe.Type != lb.SVCTypeClusterIP {
+				continue
+			}
+			if shouldRedirectFrontend(c.p.Log, lrp, fe, pods) {
+				c.p.Log.Debug("Redirecting frontend",
+					logfields.Frontend, fe,
+					logfields.ServiceName, targetName,
+					logfields.Target, &lrpServiceName)
+				c.p.Writer.SetRedirectTo(wtxn, fe, &lrpServiceName)
+			} else {
+				c.p.Writer.SetRedirectTo(wtxn, fe, nil)
+			}
+		}
+
+	case lrpConfigTypeAddr:
+		// In address-based mode there is no existing service/frontend to match against and
+		// instead the frontend is created here.
+		for _, feM := range lrp.FrontendMappings {
+			if len(pods) == 0 {
+				// No pods exist to redirect the traffic to. Remove the frontend to let the traffic
+				// be handled normally.
+				c.p.Writer.DeleteFrontend(wtxn, feM.feAddr)
+			} else {
+				fe, _, found := c.p.Writer.Frontends().Get(wtxn, lb.FrontendByAddress(feM.feAddr))
+				if found {
+					if fe.Type != lb.SVCTypeLocalRedirect {
+						c.p.Log.Error("LocalRedirectPolicy matches an address owned by an existing service => refusing to override",
+							logfields.Address, feM.feAddr,
+							logfields.ServiceName, fe.ServiceName)
+					}
+					continue
+				}
+				_, err := c.p.Writer.UpsertFrontend(
+					wtxn,
+					lb.FrontendParams{
+						Address:     feM.feAddr,
+						Type:        lb.SVCTypeLocalRedirect,
+						ServiceName: lrpServiceName,
+						ServicePort: feM.feAddr.Port(),
+					},
+				)
+				if err != nil {
+					// Not expecting any errors here as the address conflict already handled above.
+					c.p.Log.Error("Failed to upsert frontend for LocalRedirectPolicy",
+						logfields.LRPName, lrp.ID,
+						logfields.Address, feM.feAddr,
+						logfields.Error, err)
+				}
+			}
+		}
+	case lrpConfigTypeNone:
+		cleanup(wtxn)
+		return func(writer.WriteTxn) {}
+	}
+	return cleanup
 }
 
 func (c *lrpController) updateRedirectBackends(wtxn writer.WriteTxn, ws *statedb.WatchSet, lrp *LocalRedirectPolicy, pods []podInfo) {
@@ -411,7 +423,12 @@ func (c *lrpController) updateRedirectBackends(wtxn writer.WriteTxn, ws *statedb
 	}
 }
 
-func shouldRedirectFrontend(log *slog.Logger, lrp *LocalRedirectPolicy, fe *lb.Frontend) bool {
+func shouldRedirectFrontend(log *slog.Logger, lrp *LocalRedirectPolicy, fe *lb.Frontend, pods []podInfo) bool {
+	// 0. Don't redirect if we have no matching target pods.
+	if len(pods) == 0 {
+		return false
+	}
+
 	// 1. First match the frontend based on "RedirectFrontend.ToPorts"
 	// 1.1. All frontends match if no ports are given
 	match := len(lrp.FrontendMappings) == 0

pkg/loadbalancer/redirectpolicy/testdata/no-target-pods.txtar
@@ -9,9 +9,9 @@ k8s/add service.yaml endpointslice.yaml
 db/cmp services services-before.table
 db/cmp frontends frontends-before.table
 
-# Add service-name based redirect. The existing frontends should not be affected
+# Add the redirects. The existing frontends should not be affected
 # by this since no local pods exist.
-k8s/add lrp-svc.yaml
+k8s/add lrp-svc.yaml lrp-addr.yaml
 db/cmp localredirectpolicies lrp.table
 db/cmp services services.table
 db/cmp frontends frontends-before.table
@@ -20,7 +20,8 @@ db/cmp frontends frontends-before.table
 k8s/add pod.yaml
 db/cmp frontends frontends.table
 
-# Removing the pod will remove the redirection.
+# Removing the pod will remove the redirections and revert
+# back to the earlier state.
 k8s/delete pod.yaml
 db/cmp frontends frontends-before.table
  
@@ -45,9 +46,27 @@ spec:
       - port: "80"
         name: "tcp"
         protocol: TCP
-      - port: "70"
-        name: "udp"
-        protocol: UDP
+
+-- lrp-addr.yaml --
+apiVersion: "cilium.io/v2"
+kind: CiliumLocalRedirectPolicy
+metadata:
+  name: "lrp-addr"
+  namespace: "test"
+spec:
+  redirectFrontend:
+    addressMatcher:
+      ip: "169.254.169.254"
+      toPorts:
+        - port: "8080"
+          protocol: TCP
+  redirectBackend:
+    localEndpointSelector:
+      matchLabels:
+        app: proxy
+    toPorts:
+      - port: "80"
+        protocol: TCP
 
 -- pod.yaml --
 apiVersion: v1
@@ -65,9 +84,6 @@ spec:
         - containerPort: 80
           name: tcp
           protocol: TCP
-        - containerPort: 70
-          name: udp
-          protocol: UDP
   nodeName: testnode
 status:
   hostIP: 172.19.0.3
@@ -80,11 +96,6 @@ status:
   - ip: 2001::1
   qosClass: BestEffort
   startTime: "2024-07-10T16:20:42Z"
-  conditions:
-  - lastProbeTime: null
-    lastTransitionTime: '2019-07-08T09:41:59Z'
-    status: 'True'
-    type: Ready
 
 -- service.yaml --
 apiVersion: v1
@@ -93,9 +104,9 @@ metadata:
   name: echo
   namespace: test
 spec:
-  clusterIP: 169.254.169.254
+  clusterIP: 1.1.1.1
   clusterIPs:
-  - 169.254.169.254
+  - 1.1.1.1
   - 1001::1
   externalTrafficPolicy: Cluster
   internalTrafficPolicy: Cluster
@@ -108,10 +119,6 @@ spec:
     port: 8080
     protocol: TCP
     targetPort: 8080
-  - name: udp
-    port: 7070
-    protocol: UDP
-    targetPort: 7070
   selector:
     name: echo
   sessionAffinity: None
@@ -138,12 +145,10 @@ ports:
 - name: tcp
   port: 8080
   protocol: TCP
-- name: udp
-  port: 7070
-  protocol: UDP
 
 -- lrp.table --
 Name           Type     FrontendType                Frontends
+test/lrp-addr  address  addr-single-port            169.254.169.254:8080/TCP
 test/lrp-svc   service  all
 
 -- services-before.table --
@@ -152,19 +157,17 @@ test/echo                     k8s
 
 -- services.table --
 Name                          Source
-test/echo                     k8s   
+test/echo                     k8s
+test/lrp-addr:local-redirect  k8s
 test/lrp-svc:local-redirect   k8s   
 
 -- frontends-before.table --
 Address                    Type        ServiceName   PortName   Backends              RedirectTo                    Status
-169.254.169.254:7070/UDP   ClusterIP   test/echo     udp        10.244.1.1:7070/UDP                                 Done
-169.254.169.254:8080/TCP   ClusterIP   test/echo     tcp        10.244.1.1:8080/TCP                                 Done
-[1001::1]:7070/UDP         ClusterIP   test/echo     udp                                                            Done
+1.1.1.1:8080/TCP           ClusterIP   test/echo     tcp        10.244.1.1:8080/TCP                                 Done
 [1001::1]:8080/TCP         ClusterIP   test/echo     tcp                                                            Done
 
 -- frontends.table --
-Address                    Type        ServiceName   PortName   Backends              RedirectTo                    Status
-169.254.169.254:7070/UDP   ClusterIP   test/echo     udp        10.244.2.1:70/UDP     test/lrp-svc:local-redirect   Done
-169.254.169.254:8080/TCP   ClusterIP   test/echo     tcp        10.244.2.1:80/TCP     test/lrp-svc:local-redirect   Done
-[1001::1]:7070/UDP         ClusterIP   test/echo     udp        [2001::1]:70/UDP      test/lrp-svc:local-redirect   Done
-[1001::1]:8080/TCP         ClusterIP   test/echo     tcp        [2001::1]:80/TCP      test/lrp-svc:local-redirect   Done
+Address                  Type          ServiceName                  PortName   Backends              RedirectTo                    Status
+1.1.1.1:8080/TCP         ClusterIP     test/echo                    tcp        10.244.2.1:80/TCP     test/lrp-svc:local-redirect   Done
+169.254.169.254:8080/TCP LocalRedirect test/lrp-addr:local-redirect            10.244.2.1:80/TCP                                   Done
+[1001::1]:8080/TCP       ClusterIP     test/echo                    tcp        [2001::1]:80/TCP      test/lrp-svc:local-redirect   Done

pkg/loadbalancer/writer/writer.go
@@ -237,6 +237,13 @@ func (w *Writer) UpsertFrontend(txn WriteTxn, params loadbalancer.FrontendParams
 	return w.upsertFrontendParams(txn, params, svc)
 }
 
+func (w *Writer) DeleteFrontend(txn WriteTxn, addr loadbalancer.L3n4Addr) {
+	fe, _, found := w.fes.Get(txn, loadbalancer.FrontendByAddress(addr))
+	if found {
+		w.fes.Delete(txn, fe)
+	}
+}
+
 func (w *Writer) UpdateBackendHealth(txn WriteTxn, serviceName loadbalancer.ServiceName, backend loadbalancer.L3n4Addr, healthy bool) (bool, error) {
 	be, _, ok := w.bes.Get(txn, loadbalancer.BackendByAddress(backend))
 	if !ok {

commit 031954b15ee8cb5abd695083b2c191acc78b2834

redirectpolicy: restore addressMatcher refuse-override guard
The LRP controller has a guard that prevents a namespaced
addressMatcher LRP from hijacking a frontend owned by another Service.
However, the guard was effectively dead: when the LRP's backing pods
are not yet Ready, the len(pods)==0 path runs an unconditional
DeleteFrontend that wipes the frontend before the guard ever
inspects it. By the time pods come up, the address slot is empty and
the override silently succeeds.

Fix this by scoping the delete to frontends the LRP itself installed.
This restores the refuse-override guarantee while preserving the
intent of #41450 (drop our own LocalRedirect when no targets remain).

Regression introduced by c6b2c2a053

Signed-off-by: Yusuke Suzuki <yusuke.suzuki@isovalent.com>

pkg/loadbalancer/redirectpolicy/controller.go
@@ -320,12 +320,15 @@ func (c *lrpController) updateRedirects(wtxn writer.WriteTxn, ws *statedb.WatchS
 		// In address-based mode there is no existing service/frontend to match against and
 		// instead the frontend is created here.
 		for _, feM := range lrp.FrontendMappings {
+			fe, _, found := c.p.Writer.Frontends().Get(wtxn, lb.FrontendByAddress(feM.feAddr))
 			if len(pods) == 0 {
-				// No pods exist to redirect the traffic to. Remove the frontend to let the traffic
-				// be handled normally.
-				c.p.Writer.DeleteFrontend(wtxn, feM.feAddr)
+				// No pods exist to redirect the traffic to. If we previously installed a
+				// LocalRedirect frontend for this LRP, remove it so traffic falls back to
+				// the original service. Never touch a frontend owned by another service.
+				if found && fe.Type == lb.SVCTypeLocalRedirect && fe.ServiceName.Equal(lrpServiceName) {
+					c.p.Writer.DeleteFrontend(wtxn, feM.feAddr)
+				}
 			} else {
-				fe, _, found := c.p.Writer.Frontends().Get(wtxn, lb.FrontendByAddress(feM.feAddr))
 				if found {
 					if fe.Type != lb.SVCTypeLocalRedirect {
 						c.p.Log.Error("LocalRedirectPolicy matches an address owned by an existing service => refusing to override",

pkg/loadbalancer/redirectpolicy/testdata/address-matcher-conflict.txtar
@@ -1,0 +1,163 @@
+# Verifies that a LocalRedirectPolicy using an addressMatcher does NOT
+# hijack an existing Service's frontend.
+
+hive start
+
+# kube-system/kube-dns already exists and owns 10.96.0.10:53/UDP.
+k8s/add kube-dns-service.yaml kube-dns-endpointslice.yaml
+db/cmp services services-before.table
+db/cmp frontends frontends-before.table
+
+# Apply the LRP in lrp-test together with its backend pod that is not
+# yet Ready.
+k8s/add lrp-addr.yaml lrp-pod-not-ready.yaml
+db/cmp localredirectpolicies lrp.table
+db/cmp services services-with-lrp.table
+db/cmp frontends frontends-before.table
+
+# The pod becomes Ready. The frontend at 10.96.0.10:53/UDP is still
+# owned by kube-system/kube-dns, so the refuse-override path must kick
+# in and leave the frontend as a ClusterIP pointing to the kube-dns
+# endpoints.
+cp lrp-pod-not-ready.yaml lrp-pod-ready.yaml
+sed 'status: "False"' 'status: "True"' lrp-pod-ready.yaml
+k8s/update lrp-pod-ready.yaml
+db/cmp backends backends.table
+db/cmp frontends frontends-before.table
+
+# Removing the LRP tears down the pseudo-service and its backend, and
+# leaves kube-dns untouched.
+k8s/delete lrp-addr.yaml
+* db/empty localredirectpolicies
+db/cmp services services-before.table
+db/cmp frontends frontends-before.table
+db/cmp backends backends-after.table
+
+# ---
+
+-- lrp-addr.yaml --
+apiVersion: "cilium.io/v2"
+kind: CiliumLocalRedirectPolicy
+metadata:
+  name: "lrp-addr"
+  namespace: "lrp-test"
+spec:
+  redirectFrontend:
+    addressMatcher:
+      ip: "10.96.0.10"
+      toPorts:
+        - port: "53"
+          protocol: UDP
+  redirectBackend:
+    localEndpointSelector:
+      matchLabels:
+        app.kubernetes.io/name: lrp-test
+    toPorts:
+      - name: dns
+        port: "53"
+        protocol: UDP
+
+-- kube-dns-service.yaml --
+apiVersion: v1
+kind: Service
+metadata:
+  name: kube-dns
+  namespace: kube-system
+spec:
+  clusterIP: 10.96.0.10
+  clusterIPs:
+  - 10.96.0.10
+  externalTrafficPolicy: Cluster
+  internalTrafficPolicy: Cluster
+  ipFamilies:
+  - IPv4
+  ipFamilyPolicy: SingleStack
+  ports:
+  - name: dns
+    port: 53
+    protocol: UDP
+    targetPort: 53
+  selector:
+    k8s-app: kube-dns
+  sessionAffinity: None
+  type: ClusterIP
+
+-- kube-dns-endpointslice.yaml --
+apiVersion: discovery.k8s.io/v1
+kind: EndpointSlice
+metadata:
+  labels:
+    kubernetes.io/service-name: kube-dns
+  name: kube-dns-abcde
+  namespace: kube-system
+addressType: IPv4
+endpoints:
+- addresses:
+  - 10.244.0.108
+  conditions:
+    ready: true
+    serving: true
+    terminating: false
+  nodeName: worker-0
+ports:
+- name: dns
+  port: 53
+  protocol: UDP
+
+-- lrp-pod-not-ready.yaml --
+apiVersion: v1
+kind: Pod
+metadata:
+  name: lrp-test-pod
+  namespace: lrp-test
+  labels:
+    app.kubernetes.io/name: lrp-test
+spec:
+  containers:
+    - name: lrp-test
+      image: example/dns
+      ports:
+        - containerPort: 53
+          name: dns
+          protocol: UDP
+  nodeName: testnode
+status:
+  hostIP: 172.19.0.3
+  hostIPs:
+  - ip: 172.19.0.3
+  phase: Pending
+  podIP: 10.244.2.222
+  podIPs:
+  - ip: 10.244.2.222
+  qosClass: BestEffort
+  startTime: "2024-07-10T16:20:42Z"
+  conditions:
+  - lastProbeTime: null
+    type: Ready
+    status: "False"
+
+-- lrp.table --
+Name               Type     FrontendType      Frontends
+lrp-test/lrp-addr  address  addr-single-port  10.96.0.10:53/UDP
+
+-- services-before.table --
+Name                              Source
+kube-system/kube-dns              k8s
+
+-- services-with-lrp.table --
+Name                              Source
+kube-system/kube-dns              k8s
+lrp-test/lrp-addr:local-redirect  k8s
+
+-- frontends-before.table --
+Address             Type        ServiceName           PortName  Backends              RedirectTo  Status
+10.96.0.10:53/UDP   ClusterIP   kube-system/kube-dns  dns       10.244.0.108:53/UDP               Done
+
+-- backends.table --
+Service                           Address
+kube-system/kube-dns              10.244.0.108:53/UDP
+lrp-test/lrp-addr:local-redirect  10.244.2.222:53/UDP
+
+-- backends-after.table --
+Service               Address
+kube-system/kube-dns  10.244.0.108:53/UDP