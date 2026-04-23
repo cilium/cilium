5e801fd1858f3518e3363f431f8aa922a88486e1
vchammas <vvanessachammas@gmail.com>
2026-04-23 16:41:13 +0000

    routing: add hybrid routing mode support
    adds config RequiresNativeRouting and option RoutingModeHybrid. includes tests against routingMode
    
    Signed-off-by: vchammas <vvanessachammas@gmail.com>

--- pkg/maps/subnet/table.go
+++ pkg/maps/subnet/table.go
@@ -4,16 +4,12 @@
 package subnet
 
 import (
-	"encoding"
 	"fmt"
 	"net/netip"
 
 	"github.com/cilium/statedb"
 	"github.com/cilium/statedb/index"
 	"github.com/cilium/statedb/reconciler"
-
-	"github.com/cilium/cilium/pkg/bpf"
-	"github.com/cilium/cilium/pkg/types"
 )
 
 const TableName = "subnet-identities"
@@ -64,43 +60,6 @@ func (s SubnetTableEntry) setStatus(status reconciler.Status) SubnetTableEntry {
 // getStatus returns the current reconciliation status.
 func (s SubnetTableEntry) getStatus() reconciler.Status {
 	return s.Status
-}
-
-// BinaryKey returns the binary representation of the subnet prefix for the eBPF map key.
-func (s SubnetTableEntry) BinaryKey() encoding.BinaryMarshaler {
-	var ip types.IPv6
-	var family uint8
-
-	// Convert netip.Prefix to SubnetMapKey
-	addr := s.Key.Addr()
-
-	// Copy the IP address bytes into the IPv6 array
-	if addr.Is4() {
-		// For IPv4, copy to the last 4 bytes (IPv4-mapped IPv6 format)
-		ipv4 := addr.As4()
-		copy(ip[:], ipv4[:])
-		family = bpf.EndpointKeyIPv4
-	} else {
-		// For IPv6, copy all 16 bytes
-		ipv6 := addr.As16()
-		copy(ip[:], ipv6[:])
-		family = bpf.EndpointKeyIPv6
-	}
-
-	k := SubnetMapKey{
-		Prefixlen: getStaticPrefixBits() + uint32(s.Key.Bits()),
-		Family:    family,
-		IP:        ip,
-	}
-	return bpf.StructBinaryMarshaler{Target: &k}
-}
-
-// BinaryValue returns the binary representation of the identity for the eBPF map value.
-func (s SubnetTableEntry) BinaryValue() encoding.BinaryMarshaler {
-	v := SubnetMapValue{
-		Identity: s.Value,
-	}
-	return bpf.StructBinaryMarshaler{Target: &v}
 }
 
 // SubnetIndex is the primary index for SubnetEntry, indexing by Prefix.
