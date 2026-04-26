cilium/cilium

commit 6292f7d7195355dbbd017d8814d79a227ea2898f

gateway-api: Add TLSRoute hostname intersection test
This commit adds the new TLSRoute hostname intersection test
added to Gateway API in v1.5, which tests behavior that Cilium
has had subtly incorrect for some time.

This adds a custom sorter for hostnames, which enables the
hostname intersection calculation to be correct.

This also required changing some details in how the model
lists backends, in `model.TLSBackends()`.

Signed-off-by: Nick Young <nick@isovalent.com>

operator/pkg/model/translation/envoy_listener.go
@@ -24,7 +24,6 @@ import (
 	"github.com/cilium/cilium/operator/pkg/model"
 	"github.com/cilium/cilium/pkg/envoy"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
-	"github.com/cilium/cilium/pkg/slices"
 )
 
 const (
@@ -400,27 +399,24 @@ func getHostNetworkListenerAddresses(ports []uint32, ipv4Enabled, ipv6Enabled bo
 }
 
 func tlsPassthroughFilterChains(m *model.Model) []*envoy_config_listener.FilterChain {
-	ptBackendsToHostnames := m.TLSBackendsToHostnames()
+	ptBackendsToHostnames := m.TLSBackends()
 	if len(ptBackendsToHostnames) == 0 {
 		return nil
 	}
 
 	var filterChains []*envoy_config_listener.FilterChain
 
-	orderedBackends := goslices.Sorted(maps.Keys(ptBackendsToHostnames))
-
-	for _, backend := range orderedBackends {
-		hostNames := ptBackendsToHostnames[backend]
+	for _, backend := range ptBackendsToHostnames {
 		filterChains = append(filterChains, &envoy_config_listener.FilterChain{
-			FilterChainMatch: toFilterChainMatch(hostNames),
+			FilterChainMatch: toFilterChainMatch(backend.Hostnames),
 			Filters: []*envoy_config_listener.Filter{
 				{
 					Name: tcpProxyType,
 					ConfigType: &envoy_config_listener.Filter_TypedConfig{
 						TypedConfig: toAny(&envoy_extensions_filters_network_tcp_v3.TcpProxy{
-							StatPrefix: backend,
+							StatPrefix: backend.BackendKey,
 							ClusterSpecifier: &envoy_extensions_filters_network_tcp_v3.TcpProxy_Cluster{
-								Cluster: backend,
+								Cluster: backend.BackendKey,
 							},
 						}),
 					},
@@ -467,11 +463,10 @@ func toFilterChainMatch(hostNames []string) *envoy_config_listener.FilterChainMa
 	res := &envoy_config_listener.FilterChainMatch{
 		TransportProtocol: tlsTransportProtocol,
 	}
-	// ServerNames must be sorted and unique, however, envoy don't support "*" as a server name
-	serverNames := slices.SortedUnique(hostNames)
-	if goslices.Contains(serverNames, "*") {
+	// The hostNames slice cannot have duplicates, so we do not need to check for uniqueness.
+	if goslices.Contains(hostNames, "*") {
 		return res
 	}
-	res.ServerNames = serverNames
+	res.ServerNames = hostNames
 	return res
 }

operator/pkg/gateway-api/gateway_reconcile_test.go
@@ -270,6 +270,12 @@ func Test_Conformance(t *testing.T) {
 		{name: "httproute-backendtlspolicy-invalid-kind", gateway: []gwDetails{gatewaySameNamespace}},
 		{name: "tlsroute-invalid-reference-grant", gateway: []gwDetails{{FullName: types.NamespacedName{Name: "gateway-tlsroute-referencegrant", Namespace: "gateway-conformance-infra"}}}},
 		{name: "tlsroute-simple-same-namespace", gateway: []gwDetails{{FullName: types.NamespacedName{Name: "gateway-tlsroute", Namespace: "gateway-conformance-infra"}}}},
+		{name: "tlsroute-hostname-intersection", gateway: []gwDetails{
+			{FullName: types.NamespacedName{Name: "gw-tlsroute-empty-hostname-x-4", Namespace: "gateway-conformance-infra"}},
+			{FullName: types.NamespacedName{Name: "gw-tlsroute-exact-hostname-x-1", Namespace: "gateway-conformance-infra"}},
+			{FullName: types.NamespacedName{Name: "gw-tlsroute-less-specific-wc-hostname-x-3", Namespace: "gateway-conformance-infra"}},
+			{FullName: types.NamespacedName{Name: "gw-tlsroute-more-specific-wc-hostname-x-2", Namespace: "gateway-conformance-infra"}},
+		}},
 	}
 
 	for _, tt := range tests {

operator/pkg/gateway-api/testdata/gateway/base/manifests.yaml
@@ -61,6 +61,32 @@ spec:
             kind: Secret
             name: tls-validity-checks-certificate
             namespace: gateway-conformance-infra
+    - name: https-with-wildcard-hostname
+      port: 443
+      hostname: "*.wildcard.org"
+      protocol: HTTPS
+      allowedRoutes:
+        namespaces:
+          from: Same
+      tls:
+        certificateRefs:
+          - group: ""
+            kind: Secret
+            name: tls-validity-checks-certificate
+            namespace: gateway-conformance-infra
+    - name: https-with-hostname-matching-wildcard
+      port: 443
+      hostname: "fourth-example.wildcard.org"
+      protocol: HTTPS
+      allowedRoutes:
+        namespaces:
+          from: Same
+      tls:
+        certificateRefs:
+          - group: ""
+            kind: Secret
+            name: tls-validity-checks-certificate
+            namespace: gateway-conformance-infra
 ---
 apiVersion: gateway.networking.k8s.io/v1
 kind: Gateway
@@ -139,7 +165,7 @@ spec:
       containers:
         - name: infra-backend-v1
           # Originally from https://github.com/kubernetes-sigs/ingress-controller-conformance/tree/master/images/echoserver
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           env:
             - name: POD_NAME
               valueFrom:
@@ -185,7 +211,7 @@ spec:
     spec:
       containers:
         - name: infra-backend-v2
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           env:
             - name: POD_NAME
               valueFrom:
@@ -231,7 +257,7 @@ spec:
     spec:
       containers:
         - name: infra-backend-v3
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           env:
             - name: POD_NAME
               valueFrom:
@@ -277,7 +303,69 @@ spec:
     spec:
       containers:
         - name: tls-backend
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
+          volumeMounts:
+            - name: secret-volume
+              mountPath: /etc/secret-volume
+          env:
+            - name: POD_NAME
+              valueFrom:
+                fieldRef:
+                  fieldPath: metadata.name
+            - name: NAMESPACE
+              valueFrom:
+                fieldRef:
+                  fieldPath: metadata.namespace
+            - name: TLS_SERVER_CERT
+              value: /etc/secret-volume/crt
+            - name: TLS_SERVER_PRIVKEY
+              value: /etc/secret-volume/key
+          resources:
+            requests:
+              cpu: 10m
+      volumes:
+        - name: secret-volume
+          secret:
+            secretName: tls-checks-certificate
+            items:
+              - key: tls.crt
+                path: crt
+              - key: tls.key
+                path: key
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: tls-backend-2
+  namespace: gateway-conformance-infra
+spec:
+  selector:
+    app: tls-backend-2
+  ports:
+    - protocol: TCP
+      port: 443
+      targetPort: 8443
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: tls-backend-2
+  namespace: gateway-conformance-infra
+  labels:
+    app: tls-backend-2
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: tls-backend-2
+  template:
+    metadata:
+      labels:
+        app: tls-backend-2
+    spec:
+      containers:
+        - name: tls-backend-2
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           volumeMounts:
             - name: secret-volume
               mountPath: /etc/secret-volume
@@ -346,7 +434,7 @@ spec:
     spec:
       containers:
         - name: tls-backend
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           volumeMounts:
             - name: secret-volume
               mountPath: /etc/secret-volume
@@ -408,7 +496,7 @@ spec:
     spec:
       containers:
         - name: app-backend-v1
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           env:
             - name: POD_NAME
               valueFrom:
@@ -454,7 +542,7 @@ spec:
     spec:
       containers:
         - name: app-backend-v2
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           env:
             - name: POD_NAME
               valueFrom:
@@ -507,7 +595,7 @@ spec:
     spec:
       containers:
         - name: web-backend
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           env:
             - name: POD_NAME
               valueFrom:
@@ -554,7 +642,7 @@ spec:
     spec:
       containers:
         - name: grpc-infra-backend-v1
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           env:
             - name: POD_NAME
               valueFrom:
@@ -603,7 +691,7 @@ spec:
     spec:
       containers:
         - name: grpc-infra-backend-v2
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           env:
             - name: POD_NAME
               valueFrom:
@@ -652,7 +740,7 @@ spec:
     spec:
       containers:
         - name: grpc-infra-backend-v3
-          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20240412-v1.0.0-394-g40c666fd
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
           env:
             - name: POD_NAME
               valueFrom:
@@ -738,3 +826,75 @@ data:
     foo.bar.com:53 {
       whoami
     }
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: tcp-backend
+  namespace: gateway-conformance-infra
+spec:
+  selector:
+    app: tcp-backend
+  ports:
+    - name: echo-tcp-plain
+      protocol: TCP
+      port: 3000
+      targetPort: 3000
+    - name: echo-tcp-tls
+      protocol: TCP
+      port: 8443
+      targetPort: 8443
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: tcp-backend
+  namespace: gateway-conformance-infra
+  labels:
+    app: tcp-backend
+spec:
+  replicas: 2
+  selector:
+    matchLabels:
+      app: tcp-backend
+  template:
+    metadata:
+      labels:
+        app: tcp-backend
+    spec:
+      containers:
+        - name: tcp-backend
+          image: gcr.io/k8s-staging-gateway-api/echo-basic:v20260204-monthly-2026.01-60-g28382302
+          resources:
+            requests:
+              cpu: 10m
+          volumeMounts:
+            - name: secret-volume
+              mountPath: /etc/secret-volume
+          env:
+            - name: POD_NAME
+              valueFrom:
+                fieldRef:
+                  fieldPath: metadata.name
+            - name: NAMESPACE
+              valueFrom:
+                fieldRef:
+                  fieldPath: metadata.namespace
+            - name: TLS_SERVER_CERT
+              value: /etc/secret-volume/crt
+            - name: TLS_SERVER_PRIV_KEY
+              value: /etc/secret-volume/key
+            - name: TCP_ECHO_SERVER
+              value: "1"
+      volumes:
+        - name: secret-volume
+          secret:
+            # certificate issued with hostname abc.example.com
+            secretName: tls-passthrough-checks-certificate
+            items:
+              - key: tls.crt
+                path: crt
+              - key: tls.key
+                path: key
+---
+

operator/pkg/gateway-api/testdata/gateway/httproute-backendtlspolicy-multiparent/output/same-namespace-with-https-listener.yaml
@@ -32,6 +32,32 @@ spec:
             kind: Secret
             name: tls-validity-checks-certificate
             namespace: gateway-conformance-infra
+    - name: https-with-wildcard-hostname
+      port: 443
+      hostname: "*.wildcard.org"
+      protocol: HTTPS
+      allowedRoutes:
+        namespaces:
+          from: Same
+      tls:
+        certificateRefs:
+          - group: ""
+            kind: Secret
+            name: tls-validity-checks-certificate
+            namespace: gateway-conformance-infra
+    - name: https-with-hostname-matching-wildcard
+      port: 443
+      hostname: "fourth-example.wildcard.org"
+      protocol: HTTPS
+      allowedRoutes:
+        namespaces:
+          from: Same
+      tls:
+        certificateRefs:
+          - group: ""
+            kind: Secret
+            name: tls-validity-checks-certificate
+            namespace: gateway-conformance-infra
 status:
   addresses:
     - type: IPAddress
@@ -89,6 +115,52 @@ status:
           status: "True"
           type: Programmed
       name: https-with-hostname
+      supportedKinds:
+        - group: gateway.networking.k8s.io
+          kind: HTTPRoute
+        - group: gateway.networking.k8s.io
+          kind: GRPCRoute
+    - attachedRoutes: 0
+      conditions:
+        - lastTransitionTime: "2025-07-01T05:06:15Z"
+          message: Resolved Refs
+          reason: ResolvedRefs
+          status: "True"
+          type: ResolvedRefs
+        - lastTransitionTime: "2025-07-01T14:19:43Z"
+          message: Listener Accepted
+          reason: Accepted
+          status: "True"
+          type: Accepted
+        - lastTransitionTime: "2025-08-18T04:35:05Z"
+          message: Listener Programmed
+          reason: Programmed
+          status: "True"
+          type: Programmed
+      name: https-with-wildcard-hostname
+      supportedKinds:
+        - group: gateway.networking.k8s.io
+          kind: HTTPRoute
+        - group: gateway.networking.k8s.io
+          kind: GRPCRoute
+    - attachedRoutes: 0
+      conditions:
+        - lastTransitionTime: "2025-07-01T05:06:15Z"
+          message: Resolved Refs
+          reason: ResolvedRefs
+          status: "True"
+          type: ResolvedRefs
+        - lastTransitionTime: "2025-07-01T14:19:43Z"
+          message: Listener Accepted
+          reason: Accepted
+          status: "True"
+          type: Accepted
+        - lastTransitionTime: "2025-08-18T04:35:05Z"
+          message: Listener Programmed
+          reason: Programmed
+          status: "True"
+          type: Programmed
+      name: https-with-hostname-matching-wildcard
       supportedKinds:
         - group: gateway.networking.k8s.io
           kind: HTTPRoute

operator/pkg/gateway-api/testdata/gateway/httproute-backendtlspolicy-reencrypt/output/same-namespace-with-https-listener.yaml
@@ -32,6 +32,32 @@ spec:
             kind: Secret
             name: tls-validity-checks-certificate
             namespace: gateway-conformance-infra
+    - name: https-with-wildcard-hostname
+      port: 443
+      hostname: "*.wildcard.org"
+      protocol: HTTPS
+      allowedRoutes:
+        namespaces:
+          from: Same
+      tls:
+        certificateRefs:
+          - group: ""
+            kind: Secret
+            name: tls-validity-checks-certificate
+            namespace: gateway-conformance-infra
+    - name: https-with-hostname-matching-wildcard
+      port: 443
+      hostname: "fourth-example.wildcard.org"
+      protocol: HTTPS
+      allowedRoutes:
+        namespaces:
+          from: Same
+      tls:
+        certificateRefs:
+          - group: ""
+            kind: Secret
+            name: tls-validity-checks-certificate
+            namespace: gateway-conformance-infra
 status:
   addresses:
     - type: IPAddress
@@ -89,6 +115,52 @@ status:
           status: "True"
           type: Programmed
       name: https-with-hostname
+      supportedKinds:
+        - group: gateway.networking.k8s.io
+          kind: HTTPRoute
+        - group: gateway.networking.k8s.io
+          kind: GRPCRoute
+    - attachedRoutes: 0
+      conditions:
+        - lastTransitionTime: "2025-07-01T05:06:15Z"
+          message: Resolved Refs
+          reason: ResolvedRefs
+          status: "True"
+          type: ResolvedRefs
+        - lastTransitionTime: "2025-07-01T14:19:43Z"
+          message: Listener Accepted
+          reason: Accepted
+          status: "True"
+          type: Accepted
+        - lastTransitionTime: "2025-08-18T04:35:05Z"
+          message: Listener Programmed
+          reason: Programmed
+          status: "True"
+          type: Programmed
+      name: https-with-wildcard-hostname
+      supportedKinds:
+        - group: gateway.networking.k8s.io
+          kind: HTTPRoute
+        - group: gateway.networking.k8s.io
+          kind: GRPCRoute
+    - attachedRoutes: 0
+      conditions:
+        - lastTransitionTime: "2025-07-01T05:06:15Z"
+          message: Resolved Refs
+          reason: ResolvedRefs
+          status: "True"
+          type: ResolvedRefs
+        - lastTransitionTime: "2025-07-01T14:19:43Z"
+          message: Listener Accepted
+          reason: Accepted
+          status: "True"
+          type: Accepted
+        - lastTransitionTime: "2025-08-18T04:35:05Z"
+          message: Listener Programmed
+          reason: Programmed
+          status: "True"
+          type: Programmed
+      name: https-with-hostname-matching-wildcard
       supportedKinds:
         - group: gateway.networking.k8s.io
           kind: HTTPRoute

operator/pkg/gateway-api/testdata/gateway/httproute-https-listener/output/same-namespace-with-https-listener.yaml
@@ -1,32 +1,56 @@
 apiVersion: gateway.networking.k8s.io/v1
 kind: Gateway
 metadata:
-  creationTimestamp: null
   name: same-namespace-with-https-listener
   namespace: gateway-conformance-infra
-  resourceVersion: "1000"
 spec:
   gatewayClassName: cilium
   listeners:
-    - allowedRoutes:
-        namespaces:
-          from: Same
-      name: https
+    - name: https
       port: 443
       protocol: HTTPS
+      allowedRoutes:
+        namespaces:
+          from: Same
       tls:
         certificateRefs:
           - group: ""
             kind: Secret
             name: tls-validity-checks-certificate
             namespace: gateway-conformance-infra
-    - allowedRoutes:
+    - name: https-with-hostname
+      port: 443
+      hostname: second-example.org
+      protocol: HTTPS
+      allowedRoutes:
         namespaces:
           from: Same
-      hostname: second-example.org
-      name: https-with-hostname
+      tls:
+        certificateRefs:
+          - group: ""
+            kind: Secret
+            name: tls-validity-checks-certificate
+            namespace: gateway-conformance-infra
+    - name: https-with-wildcard-hostname
       port: 443
+      hostname: "*.wildcard.org"
       protocol: HTTPS
+      allowedRoutes:
+        namespaces:
+          from: Same
+      tls:
+        certificateRefs:
+          - group: ""
+            kind: Secret
+            name: tls-validity-checks-certificate
+            namespace: gateway-conformance-infra
+    - name: https-with-hostname-matching-wildcard
+      port: 443
+      hostname: "fourth-example.wildcard.org"
+      protocol: HTTPS
+      allowedRoutes:
+        namespaces:
+          from: Same
       tls:
         certificateRefs:
           - group: ""
@@ -87,6 +111,52 @@ status:
           status: "False"
           type: Programmed
       name: https-with-hostname
+      supportedKinds:
+        - group: gateway.networking.k8s.io
+          kind: HTTPRoute
+        - group: gateway.networking.k8s.io
+          kind: GRPCRoute
+    - attachedRoutes: 0
+      conditions:
+        - lastTransitionTime: "2025-07-01T05:06:15Z"
+          message: Resolved Refs
+          reason: ResolvedRefs
+          status: "True"
+          type: ResolvedRefs
+        - lastTransitionTime: "2025-07-01T14:19:43Z"
+          message: Listener Accepted
+          reason: Accepted
+          status: "True"
+          type: Accepted
+        - lastTransitionTime: "2025-07-01T14:29:32Z"
+          message: Address not ready yet
+          reason: Pending
+          status: "False"
+          type: Programmed
+      name: https-with-wildcard-hostname
+      supportedKinds:
+        - group: gateway.networking.k8s.io
+          kind: HTTPRoute
+        - group: gateway.networking.k8s.io
+          kind: GRPCRoute
+    - attachedRoutes: 0
+      conditions:
+        - lastTransitionTime: "2025-07-01T05:06:15Z"
+          message: Resolved Refs
+          reason: ResolvedRefs
+          status: "True"
+          type: ResolvedRefs
+        - lastTransitionTime: "2025-07-01T14:19:43Z"
+          message: Listener Accepted
+          reason: Accepted
+          status: "True"
+          type: Accepted
+        - lastTransitionTime: "2025-07-01T14:29:32Z"
+          message: Address not ready yet
+          reason: Pending
+          status: "False"
+          type: Programmed
+      name: https-with-hostname-matching-wildcard
       supportedKinds:
         - group: gateway.networking.k8s.io
           kind: HTTPRoute

commit 2e9fa33d86f7ac97b716f0b44f0a46d86caa7dc6

gateway-api: deduplicate server names in filter chain match
The Gateway API spec allows two HTTPS listeners to share the same
hostname as long as they differ on port. When this happens,
TLSSecretsToHostnames groups both listeners under the same TLS secret,
producing a hostname slice with duplicates. Writing that slice directly
into FilterChainMatch.ServerNames causes Envoy to reject the entire listener config.

Restore the slices.SortedUnique call in toFilterChainMatch so that
duplicate server names are eliminated before the filter chain match is
built.

Fixes: 6292f7d71953 ("gateway-api: Add TLSRoute hostname intersection test")
Fixes: #45623

Signed-off-by: Martin Lindberg <203184076+eufriction@users.noreply.github.com>

operator/pkg/model/translation/envoy_listener.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cilium/cilium/operator/pkg/model"
 	"github.com/cilium/cilium/pkg/envoy"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/slices"
 )
 
 const (
@@ -463,10 +464,13 @@ func toFilterChainMatch(hostNames []string) *envoy_config_listener.FilterChainMa
 	res := &envoy_config_listener.FilterChainMatch{
 		TransportProtocol: tlsTransportProtocol,
 	}
-	// The hostNames slice cannot have duplicates, so we do not need to check for uniqueness.
-	if goslices.Contains(hostNames, "*") {
+	// ServerNames must be sorted and deduplicated. Duplicates arise when
+	// multiple HTTPS listeners share a TLS secret but differ on port.
+	// Envoy rejects "*" as a server name; omit ServerNames to match all SNI.
+	serverNames := slices.SortedUnique(goslices.Clone(hostNames))
+	if goslices.Contains(serverNames, "*") {
 		return res
 	}
-	res.ServerNames = hostNames
+	res.ServerNames = serverNames
 	return res
 }

operator/pkg/model/translation/envoy_listener_test.go
@@ -10,10 +10,65 @@ import (
 	envoy_config_listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/testing/protocmp"
 
 	"github.com/cilium/cilium/operator/pkg/model"
 )
+
+func Test_toFilterChainMatch(t *testing.T) {
+	tests := []struct {
+		name                string
+		hostNames           []string
+		expectedServerNames []string
+	}{
+		{
+			name:                "nil input",
+			hostNames:           nil,
+			expectedServerNames: nil,
+		},
+		{
+			name:                "single hostname",
+			hostNames:           []string{"foo.example.com"},
+			expectedServerNames: []string{"foo.example.com"},
+		},
+		{
+			name:                "multiple distinct hostnames",
+			hostNames:           []string{"foo.example.com", "bar.example.com"},
+			expectedServerNames: []string{"bar.example.com", "foo.example.com"},
+		},
+		{
+			name:                "wildcard hostname returns empty serverNames",
+			hostNames:           []string{"*"},
+			expectedServerNames: nil,
+		},
+		{
+			name:                "wildcard among others returns empty serverNames",
+			hostNames:           []string{"foo.example.com", "*"},
+			expectedServerNames: nil,
+		},
+		{
+			name:                "duplicate hostnames from multi-port listeners are deduplicated",
+			hostNames:           []string{"*.example.test", "*.example.test"},
+			expectedServerNames: []string{"*.example.test"},
+		},
+		{
+			name:                "triple duplicates are deduplicated",
+			hostNames:           []string{"a.example.com", "b.example.com", "a.example.com"},
+			expectedServerNames: []string{"a.example.com", "b.example.com"},
+		},
+	}
+
+	for _, tC := range tests {
+		t.Run(tC.name, func(t *testing.T) {
+			got := toFilterChainMatch(tC.hostNames)
+			require.NotNil(t, got)
+			assert.Equal(t, "tls", got.TransportProtocol)
+			assert.Equal(t, tC.expectedServerNames, got.ServerNames,
+				"serverNames must be sorted and deduplicated")
+		})
+	}
+}
 
 func Test_getHostNetworkListenerAddresses(t *testing.T) {
 	testCases := []struct {