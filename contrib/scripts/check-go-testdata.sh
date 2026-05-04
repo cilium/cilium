ba000565e314ea49cd2f7bacb63fbda26a5eb973
Dylan Reimerink <dylan.reimerink@isovalent.com>
2026-05-04 16:03:42 +0200

    Revert "tools/stackwhere: Add a tool to analyze BPF stack usage"
    This reverts commit 307c7de846fcb1ecb1f950507984fad1349e0b7b.
    
    Reverting this commit, undoing the addition of the stackwhere tool as it
    has been moved to a separate repository.
    It now lives at https://github.com/cilium/stackwhere
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- contrib/scripts/check-go-testdata.sh
+++ contrib/scripts/check-go-testdata.sh
@@ -5,6 +5,4 @@
 set -e
 
 make -C pkg/bpf/testdata docker
-make -C tools/stackwhere/testdata docker
 test -z "$(git status pkg/bpf/testdata --porcelain)" || (echo "please run 'make -C pkg/bpf/testdata docker' and submit your changes"; exit 1)
-test -z "$(git status tools/stackwhere/testdata --porcelain)" || (echo "please run 'make -C tools/stackwhere/testdata docker' and submit your changes"; exit 1)
