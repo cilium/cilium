0511887f3a0c44506fc936b3a476a4ad125d4ce9
Andrew Sauber <andrew.sauber@isovalent.com>
2026-05-07 17:33:17 +0900

    gatewayapi: update IsParentAttachable for ListenerSet routes
    Signed-off-by: Andrew Sauber <andrew.sauber@isovalent.com>

--- operator/pkg/gateway-api/helpers/routes.go
+++ operator/pkg/gateway-api/helpers/routes.go
@@ -10,21 +10,46 @@ import (
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
-func IsParentAttachable(_ context.Context, reconcileParent metav1.Object, route metav1.Object, parents []gatewayv1.RouteParentStatus) bool {
+func IsParentAttachable(
+	_ context.Context,
+	reconcileParent metav1.Object,
+	route metav1.Object,
+	parents []gatewayv1.RouteParentStatus,
+	attachedListenerSets []gatewayv1.ListenerSet,
+) bool {
 	for _, rps := range parents {
-		if NamespaceDerefOr(rps.ParentRef.Namespace, route.GetNamespace()) != reconcileParent.GetNamespace() ||
-			string(rps.ParentRef.Name) != reconcileParent.GetName() {
+		parentNS := NamespaceDerefOr(rps.ParentRef.Namespace, route.GetNamespace())
+		parentName := string(rps.ParentRef.Name)
+
+		matched := false
+		if parentNS == reconcileParent.GetNamespace() && parentName == reconcileParent.GetName() {
+			matched = true
+		} else if IsListenerSet(rps.ParentRef) {
+			for _, ls := range attachedListenerSets {
+				if parentNS == ls.GetNamespace() && parentName == ls.GetName() {
+					matched = true
+					break
+				}
+			}
+		}
+
+		if !matched {
 			continue
 		}
 
-		acceptedValid := false
+		// (ajs) Note well that this predicate depends upon looping over a list
+		// of conditions that are expected to already be populated. In the
+		// future, let's have this type of condition be an explicit mark on an
+		// augmented Route type.
+		//
+		// Also note well, we are first checking the parentRef relationship
+		// here, and then checking if the route was accepted. These states
+		// should be more than implicitly related, such that this type of
+		// function is not needed.
 		for _, cond := range rps.Conditions {
 			if cond.Type == string(gatewayv1.RouteConditionAccepted) && cond.Status == metav1.ConditionTrue {
-				acceptedValid = true
+				return true
 			}
-		}
-		if acceptedValid {
-			return true
 		}
 	}
 	return false
