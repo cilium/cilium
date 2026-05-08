d096f0b65036990cf40d050b403d543fa2884274
Marco Iorio <marco.iorio@isovalent.com>
2026-05-08 09:20:48 +0200

    lb: move tunnel configuration for DSR geneve to the lb config cell
    Move the logic enabling the tunnel configuration when DSR Geneve is
    enabled directly to the loadbalancer config cell, now that the config
    has been fully modularized. This prevents the datapath/tunnel module
    from unnecessarily depending on the entire loadbalancer package.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/datapath/tunnel/cell.go
+++ pkg/datapath/tunnel/cell.go
@@ -6,8 +6,6 @@ package tunnel
 import (
 	"github.com/cilium/hive/cell"
 
-	"github.com/cilium/cilium/pkg/kpr"
-	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/option"
 )
 
@@ -28,20 +26,6 @@ var Cell = cell.Module(
 		// Enable tunnel configuration when it is the primary routing mode.
 		func(dcfg *option.DaemonConfig) EnablerOut {
 			return NewEnabler(dcfg.TunnelingEnabled())
-		},
-
-		// Enable tunnel configuration when DSR Geneve is enabled (this is currently
-		// handled here, as the corresponding logic has not yet been modularized).
-		func(kpr kpr.KPRConfig, lbcfg loadbalancer.Config) EnablerOut {
-			return NewEnabler(
-				kpr.KubeProxyReplacement &&
-					lbcfg.LoadBalancerUsesDSR() &&
-					lbcfg.DSRDispatch == loadbalancer.DSRDispatchGeneve,
-				// The datapath logic takes care of the MTU overhead. So no need to
-				// take it into account here.
-				// See encap_geneve_dsr_opt[4,6] in nodeport.h
-				WithoutMTUAdaptation(),
-			)
 		},
 	),
 )
