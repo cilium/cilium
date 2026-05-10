ecc8a9850c616eb9ec114afa56632b18d6153473
weizhoublue <weizhou.lan@daocloud.io>
2026-05-10 21:53:41 +0800

    gateway-api: allow TLS 1.3 backend origination
    Set tlsMaximumProtocolVersion to TLSv1_3 in UpstreamTlsContext to
    allow connections to backends that only support TLS 1.3. Update
    golden CEC test fixtures accordingly.
    
    Fixes: https://github.com/cilium/cilium/issues/45864
    Signed-off-by: weizhoublue <weizhou.lan@daocloud.io>

--- operator/pkg/model/translation/envoy_cluster_test.go
+++ operator/pkg/model/translation/envoy_cluster_test.go
@@ -7,8 +7,12 @@ import (
 	"testing"
 
 	envoy_config_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
+	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	envoy_config_tls "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/proto"
+
+	"github.com/cilium/cilium/operator/pkg/model"
 )
 
 func Test_withClusterLbPolicy(t *testing.T) {
@@ -80,6 +84,30 @@ func Test_httpCluster(t *testing.T) {
 	require.Equal(t, &envoy_config_cluster_v3.Cluster_Type{
 		Type: envoy_config_cluster_v3.Cluster_EDS,
 	}, cluster.ClusterDiscoveryType)
+}
+
+func Test_httpClusterWithTLSOriginationAllowsTLS13(t *testing.T) {
+	c := &cecTranslator{}
+	res, err := c.httpCluster("dummy-name", "dummy-name", false, "", &model.BackendTLSOrigination{
+		SNI: "backend.example.com",
+		CACertRef: &model.FullyQualifiedResource{
+			Name:      "ca-cert",
+			Namespace: "default",
+		},
+	})
+	require.NoError(t, err)
+
+	cluster := &envoy_config_cluster_v3.Cluster{}
+	err = proto.Unmarshal(res.Value, cluster)
+	require.NoError(t, err)
+	require.Equal(t, "envoy.transport_sockets.tls", cluster.TransportSocket.Name)
+
+	typedConfig := cluster.TransportSocket.ConfigType.(*envoy_config_core_v3.TransportSocket_TypedConfig)
+	tlsContext := &envoy_config_tls.UpstreamTlsContext{}
+	err = proto.Unmarshal(typedConfig.TypedConfig.Value, tlsContext)
+	require.NoError(t, err)
+	require.NotNil(t, tlsContext.CommonTlsContext.TlsParams)
+	require.Equal(t, envoy_config_tls.TlsParameters_TLSv1_3, tlsContext.CommonTlsContext.TlsParams.TlsMaximumProtocolVersion)
 }
 
 func Test_tcpCluster(t *testing.T) {
