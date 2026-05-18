d5271fdf83c0fb9bfaef2243f984a18c0f318e94
Fabio Falzoi <fabio.falzoi@isovalent.com>
2026-05-18 17:43:22 +0200

    operator/multi-pool: Add a script based test
    Add a script based test to validate the correctness of the allocator for
    the multi-pool IPAM mode. The test simulates a scenario with two
    separate pools and multiple nodes requesting addresses from each pool.
    
    This test is useful to also verify that the allocator is able to
    correctly handle CiliumPodIPPools that are added when the operator is
    already running, not at startup via the
    "auto-create-cilium-pod-ip-pools" option.
    
    Related: https://github.com/cilium/cilium/pull/46034
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

---
+++ operator/pkg/ipam/multipool_script_test.go
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package ipam
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"maps"
+	"testing"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/hivetest"
+	"github.com/cilium/hive/script"
+	"github.com/cilium/hive/script/scripttest"
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/cilium/cilium/operator/k8s"
+	"github.com/cilium/cilium/operator/pkg/ipam/allocator/multipool"
+	"github.com/cilium/cilium/pkg/hive"
+	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client/testutils"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/option"
+)
+
+// Register the multi-pool IPAM allocator directly to test it without the specific build tag.
+func init() {
+	allocators = append(allocators, cell.Module(
+		"multipool-ipam-allocator",
+		"Multi Pool IP Allocator",
+
+		cell.Config(multipool.DefaultConfig),
+		cell.Invoke(multipool.StartAllocator),
+	))
+}
+
+var debug = flag.Bool("debug", false, "Enable debug logging")
+
+func TestScriptMultiPool(t *testing.T) {
+	setup := func(t testing.TB, args []string) *script.Engine {
+		h := hive.New(
+			k8sClient.FakeClientCell(),
+			k8s.ResourcesCell,
+			cell.Provide(func() *option.DaemonConfig {
+				return &option.DaemonConfig{
+					EnableIPv4: true,
+					EnableIPv6: true,
+					IPAM:       ipamOption.IPAMMultiPool,
+				}
+			}),
+
+			Cell(),
+		)
+
+		flags := pflag.NewFlagSet("", pflag.ContinueOnError)
+		h.RegisterFlags(flags)
+
+		var opts []hivetest.LogOption
+		if *debug {
+			opts = append(opts, hivetest.LogLevel(slog.LevelDebug))
+			logging.SetLogLevel(slog.LevelDebug)
+		}
+		log := hivetest.Logger(t, opts...)
+
+		t.Cleanup(func() {
+			assert.NoError(t, h.Stop(log, context.TODO()))
+		})
+
+		cmds, err := h.ScriptCommands(log)
+		require.NoError(t, err, "ScriptCommands")
+		maps.Insert(cmds, maps.All(script.DefaultCmds()))
+
+		return &script.Engine{Cmds: cmds}
+	}
+
+	scripttest.Test(t,
+		t.Context(),
+		setup,
+		[]string{},
+		"testdata/multipool/*.txtar",
+	)
+}
