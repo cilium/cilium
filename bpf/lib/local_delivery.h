0b9b606b24502b297ce2c73288216a75d5e74ee9
Julian Wiedmann <jwi@isovalent.com>
2026-05-25 12:25:46 +0300

    bpf: local_delivery: clean up always-false use_redirect_peer
    We previously checked that `use_redirect_peer` is false. Apply this
    knowledge in subsequent code.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/local_delivery.h
+++ bpf/lib/local_delivery.h
@@ -193,7 +193,7 @@ local_delivery(struct __ctx_buff *ctx, __u32 seclabel, __u32 magic,
 		}
 # endif /* !ENABLE_NODEPORT */
 
-		return redirect_ep(ctx, ep->ifindex, use_redirect_peer, from_tunnel);
+		return redirect_ep(ctx, ep->ifindex, false, from_tunnel);
 	}
 
 	/* Jumps to destination pod's BPF program to enforce ingress policies. */
