c67a2874320e9c6b4f36185df08dc54b918f4b38
Julian Wiedmann <jwi@isovalent.com>
2026-05-29 11:28:30 +0300

    bpf: nat: constify snat_v*_nat_*_mapping()
    I wanted to convince myself that these functions don't touch the CT tuple
    or the packet data.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/signal.h
+++ bpf/lib/signal.h
@@ -49,7 +49,7 @@ struct signal_msg {
 			 sizeof(msg.signal_nr) + sizeof(msg.MEMBER));	\
   }
 
-static __always_inline void send_signal_nat_fill_up(struct __ctx_buff *ctx,
+static __always_inline void send_signal_nat_fill_up(const struct __ctx_buff *ctx,
 						    __u32 proto)
 {
 	SEND_SIGNAL(ctx, SIGNAL_NAT_FILL_UP, proto, proto);
