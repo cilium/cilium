cilium/cilium

commit 963c5310f22895c7774d02496dc2f8f06d336668

ces: add slim mode CES controller initialization
On initialization of the operator and CES controller, we sync CES from
the store to our local cache before the controller runs. To do this, we
read the existing store state and create the existing CES in our local
cache.

Signed-off-by: jshr-w <shjayaraman@microsoft.com>

operator/pkg/ciliumendpointslice/endpointslice.go
@@ -5,6 +5,7 @@ package ciliumendpointslice
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/cilium/hive/cell"
@@ -15,6 +16,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/util/workqueue"
 
+	op_k8s "github.com/cilium/cilium/operator/k8s"
 	"github.com/cilium/cilium/pkg/k8s"
 	cilium_api_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	capi_v2a1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
@@ -385,6 +387,63 @@ func (c *DefaultController) syncCESsInLocalCache(ctx context.Context) error {
 			c.manager.initializeMappingCEPtoCES(&cep, ces.Namespace, cesName)
 		}
 	}
+	c.logger.DebugContext(ctx, "Successfully synced all CESs locally")
+	return nil
+}
+
+// Sync all CESs from cesStore to manager cache.
+// Note: CESs are synced locally before CES controller running and this is required.
+func (c *SlimController) syncCESsInLocalCache(ctx context.Context) error {
+	cesStore, err := c.ciliumEndpointSlice.Store(ctx)
+	if err != nil {
+		c.logger.WarnContext(ctx, "Error getting CES Store", logfields.Error, err)
+		return err
+	}
+
+	cidStore, err := c.ciliumIdentity.Store(ctx)
+	if err != nil {
+		c.logger.WarnContext(ctx, "Error getting CID Store", logfields.Error, err)
+		return err
+	}
+
+	cnodeStore, err := c.ciliumNodes.Store(ctx)
+	if err != nil {
+		c.logger.WarnContext(ctx, "Error getting CiliumNode Store", logfields.Error, err)
+		return err
+	}
+
+	cidToLabels := make(map[CID]Labels)
+	for _, cid := range cidStore.List() {
+		cidName, gidLabels := cidToGidLabels(cid)
+		cidToLabels[cidName] = gidLabels
+	}
+
+	for _, ces := range cesStore.List() {
+		c.manager.initializeMappingForCES(ces)
+		for _, cep := range ces.Endpoints {
+			identityid := strconv.FormatInt(cep.IdentityID, 10)
+			labels, ok := cidToLabels[CID(identityid)]
+			// If the CID is not found (e.g., deleted during operator restart), we skip restoring the state of this CEP on startup.
+			// We will get the CEP & CID add events through the resource stores and update the latest state in the local cache.
+			if !ok {
+				c.logger.DebugContext(ctx, "CID not found in Store for CEP",
+					logfields.CIDName, identityid,
+					logfields.CEPName, cep.Name)
+				continue
+			}
+
+			nodeObj, err := cnodeStore.ByIndex(op_k8s.CiliumNodeIPIndex, cep.Networking.NodeIP)
+			// If the CiliumNode is not found (e.g., deleted during operator restart), we skip restoring the state of this CEP on startup.
+			// We will get the CEP & CiliumNode add events through the resource stores and update the latest state in the local cache.
+			if err != nil {
+				c.logger.DebugContext(ctx, "Error getting CiliumNode by IP",
+					logfields.Error, err)
+				continue
+			}
+			c.manager.initializeMappingPodToNode(NewCEPName(cep.Name, ces.Namespace), NodeName(nodeObj[0].Name), CESName(ces.Name), CID(identityid), Labels(labels), EncryptionKey(cep.Encryption.Key))
+		}
+	}
+
 	c.logger.DebugContext(ctx, "Successfully synced all CESs locally")
 	return nil
 }

operator/pkg/ciliumendpointslice/controller.go
@@ -130,6 +130,7 @@ type SlimController struct {
 	wgEnabled    bool
 
 	ciliumIdentity resource.Resource[*v2.CiliumIdentity]
+	pods           resource.Resource[*slim_corev1.Pod]
 }
 
 // registerController creates and initializes the CES controller

operator/pkg/ciliumendpointslice/manager.go
@@ -188,9 +188,25 @@ func (c *defaultManager) initializeMappingForCES(ces *cilium_v2a1.CiliumEndpoint
 	return c.createCES(ces.Name, ces.Namespace)
 }
 
+func (c *slimManager) initializeMappingForCES(ces *cilium_v2a1.CiliumEndpointSlice) CESName {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	return c.createCESLocked(ces.Name, ces.Namespace)
+}
+
 func (c *defaultManager) initializeMappingCEPtoCES(cep *cilium_v2a1.CoreCiliumEndpoint, ns string, ces CESName) {
 	cepName := GetCEPNameFromCCEP(cep, ns)
 	c.mapping.insertCEP(cepName, ces)
+}
+
+func (c *slimManager) initializeMappingPodToNode(cepName CEPName, nodeName NodeName, ces CESName, cid CID, gidLabels Labels, encryptionKey EncryptionKey) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.mapping.addCEP(cepName, ces, nodeName, gidLabels)
+	c.mapping.insertCID(cid, gidLabels)
+	c.mapping.insertNode(nodeName, encryptionKey)
 }
 
 func (c *defaultManager) getCEPCountInCES(ces CESName) int {

commit 7214cc6b1078471a8b3d3ea7ad983d98d81bbf49

ces: avoid panic on empty CiliumNode lookup result
When restoring CES state on startup, cnodeStore.ByIndex() may return an
empty slice without an error if no CiliumNode matches the endpoint's
node IP (e.g. the node was deleted during an operator restart).

Accessing nodeObj[0] further down would cause a slice out-of-bounds
access and panic. Fix this by adding check and skipping the endpoint in
that case.

Fixes: 963c5310f228 ("ces: add slim mode CES controller initialization")
Signed-off-by: Tobias Klauser <tobias@cilium.io>

operator/pkg/ciliumendpointslice/endpointslice.go
@@ -517,7 +517,7 @@ func (c *SlimController) syncCESsInLocalCache(ctx context.Context) error {
 			nodeObj, err := cnodeStore.ByIndex(op_k8s.CiliumNodeIPIndex, cep.Networking.NodeIP)
 			// If the CiliumNode is not found (e.g., deleted during operator restart), we skip restoring the state of this CEP on startup.
 			// We will get the CEP & CiliumNode add events through the resource stores and update the latest state in the local cache.
-			if err != nil {
+			if err != nil || len(nodeObj) == 0 {
 				c.logger.DebugContext(ctx, "Error getting CiliumNode by IP",
 					logfields.Error, err)
 				continue