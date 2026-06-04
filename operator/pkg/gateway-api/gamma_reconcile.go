a70f5ecfad4648a91e460e76bdf27ad1105e6b59
Marco Hofstetter <marco.hofstetter@isovalent.com>
2026-06-04 13:31:15 +0200

    gateway-api: reconcilers and status prune should not use const
    This commit changes the reconcilers and status prune functionality to
    not directly depend on the const and instead either use the struct field
    or have it passed to the function.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- operator/pkg/gateway-api/gamma_reconcile.go
+++ operator/pkg/gateway-api/gamma_reconcile.go
@@ -167,7 +167,7 @@ func (r *gammaReconciler) setHTTPRouteStatuses(gammaLogger *slog.Logger, ctx con
 	for httpRouteIndex, original := range httpRoutes.Items {
 
 		hr := original.DeepCopy()
-		hr.Status.Parents = pruneRouteParentStatuses(hr.Status.Parents, hr.Spec.ParentRefs)
+		hr.Status.Parents = pruneRouteParentStatuses(hr.Status.Parents, hr.Spec.ParentRefs, r.controllerName)
 
 		hrName := types.NamespacedName{
 			Name:      hr.Name,
@@ -271,7 +271,7 @@ func (r *gammaReconciler) setGRPCRouteStatuses(gammaLogger *slog.Logger, ctx con
 	for grpcRouteIndex, original := range grpcRoutes.Items {
 
 		grpc := original.DeepCopy()
-		grpc.Status.Parents = pruneRouteParentStatuses(grpc.Status.Parents, grpc.Spec.ParentRefs)
+		grpc.Status.Parents = pruneRouteParentStatuses(grpc.Status.Parents, grpc.Spec.ParentRefs, r.controllerName)
 
 		grpcName := types.NamespacedName{
 			Name:      grpc.Name,
--- operator/pkg/gateway-api/status_route.go
+++ operator/pkg/gateway-api/status_route.go
@@ -8,15 +8,13 @@ import (
 	"slices"
 
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
-
-	"github.com/cilium/cilium/operator/pkg/gateway-api/helpers"
 )
 
-func pruneRouteParentStatuses(parents []gatewayv1.RouteParentStatus, currentParentRefs []gatewayv1.ParentReference) []gatewayv1.RouteParentStatus {
+func pruneRouteParentStatuses(parents []gatewayv1.RouteParentStatus, currentParentRefs []gatewayv1.ParentReference, controllerName string) []gatewayv1.RouteParentStatus {
 	filtered := parents[:0]
 
 	for _, parentStatus := range parents {
-		if parentStatus.ControllerName != helpers.CiliumDefaultControllerName || slices.ContainsFunc(currentParentRefs, func(ref gatewayv1.ParentReference) bool {
+		if string(parentStatus.ControllerName) != controllerName || slices.ContainsFunc(currentParentRefs, func(ref gatewayv1.ParentReference) bool {
 			return reflect.DeepEqual(ref, parentStatus.ParentRef)
 		}) {
 			filtered = append(filtered, parentStatus)
