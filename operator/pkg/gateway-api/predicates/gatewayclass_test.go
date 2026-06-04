da4da57ad6b98ea60f31636460135dcfb5393457
Marco Hofstetter <marco.hofstetter@isovalent.com>
2026-06-04 14:01:43 +0200

    gateway-api: move default cilium controller name
    This commit removes the default cilium controllername const
    from the helpers. now that the only production code that
    accesses it is the gateway api cell, it's worth to have it there.
    
    Testcases that used that constant are refactored to use a test
    controllername. This also verifies that nothing is hardcoded.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- operator/pkg/gateway-api/predicates/gatewayclass_test.go
+++ operator/pkg/gateway-api/predicates/gatewayclass_test.go
@@ -9,17 +9,17 @@ import (
 	"github.com/stretchr/testify/require"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
-
-	"github.com/cilium/cilium/operator/pkg/gateway-api/helpers"
 )
 
+const testGatewayClassControllerName = "example.com/test-gateway-controller"
+
 func Test_gatewayClassReconcilePredicate(t *testing.T) {
-	predicate := GatewayClassOwnedByController(helpers.CiliumDefaultControllerName)
+	predicate := GatewayClassOwnedByController(testGatewayClassControllerName)
 
 	t.Run("update keeps handoff reconcile when old gatewayclass matched", func(t *testing.T) {
 		require.True(t, predicate.Update(event.UpdateEvent{
 			ObjectOld: &gatewayv1.GatewayClass{
-				Spec: gatewayv1.GatewayClassSpec{ControllerName: helpers.CiliumDefaultControllerName},
+				Spec: gatewayv1.GatewayClassSpec{ControllerName: testGatewayClassControllerName},
 			},
 			ObjectNew: &gatewayv1.GatewayClass{
 				Spec: gatewayv1.GatewayClassSpec{ControllerName: "example.com/other-controller"},
