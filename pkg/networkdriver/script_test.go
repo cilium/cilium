2cd015d643b72e0d36f918c29c57b90abc1d0eec
Fabio Falzoi <fabio.falzoi@isovalent.com>
2026-06-11 18:41:41 +0200

    network-driver: Add support for driver script based testing
    Add script based testing for DRA APIs. PrepareResourceClaims and
    UnprepareResourceClaims, called by the kubelet in a production
    environments, are simulated here via custom commands. This also allows
    to remove the old hive-based tests for the Cilium Network Driver in
    dra_test.go.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

---
+++ pkg/networkdriver/script_test.go
@@ -0,0 +1,206 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package networkdriver
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"maps"
+	"strings"
+	"testing"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/hivetest"
+	"github.com/cilium/hive/script"
+	"github.com/cilium/hive/script/scripttest"
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	resourcev1 "k8s.io/api/resource/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubetypes "k8s.io/apimachinery/pkg/types"
+	typedv1 "k8s.io/client-go/kubernetes/typed/resource/v1"
+	"k8s.io/dynamic-resource-allocation/kubeletplugin"
+
+	"github.com/cilium/cilium/daemon/k8s"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/ipam"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client/testutils"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/networkdriver/dummy"
+	"github.com/cilium/cilium/pkg/networkdriver/types"
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/testutils"
+)
+
+func TestScript(t *testing.T) {
+	defer testutils.GoleakVerifyNone(t)
+
+	const (
+		testDevice = "test-device"
+		testDriver = "test.cilium.k8s.io"
+	)
+
+	var (
+		daemonCfg = &option.DaemonConfig{
+			EnableCiliumNetworkDriver: true,
+			EnableIPv4:                true,
+			EnableIPv6:                true,
+		}
+
+		mgr  *ipam.MultiPoolManager
+		cs   *k8sClient.FakeClientset
+		pods resource.Resource[*corev1.Pod]
+	)
+
+	scripttest.Test(t,
+		t.Context(),
+		func(t testing.TB, args []string) *script.Engine {
+			h := hive.New(
+				k8sClient.FakeClientCell(),
+				k8s.ResourcesCell,
+				cell.Provide(
+					podResource,
+					func() *option.DaemonConfig {
+						return daemonCfg
+					},
+				),
+				resourceIPAM,
+
+				cell.Invoke(func(m *ipam.MultiPoolManager, c *k8sClient.FakeClientset, p resource.Resource[*corev1.Pod]) {
+					mgr = m
+					cs = c
+					pods = p
+				}),
+			)
+
+			flags := pflag.NewFlagSet("", pflag.ContinueOnError)
+			h.RegisterFlags(flags)
+
+			log := hivetest.Logger(t, hivetest.LogLevel(slog.LevelError))
+			t.Cleanup(func() {
+				assert.NoError(t, h.Stop(log, context.Background()))
+			})
+
+			// Must be executed before creating the driver, so that
+			// the Invoke above is run and mgr, cs and pods are not nil.
+			cmds, err := h.ScriptCommands(log)
+			require.NoError(t, err, "ScriptCommands")
+
+			driver := &Driver{
+				logger:     log,
+				kubeClient: cs,
+				pods:       pods,
+				config: &v2alpha1.CiliumNetworkDriverNodeConfigSpec{
+					DriverName: testDriver,
+				},
+				devices: map[types.DeviceManagerType][]types.Device{
+					types.DeviceManagerTypeDummy: {
+						&dummy.DummyDevice{
+							Name: testDevice,
+						},
+					},
+				},
+				allocations:  make(map[kubetypes.UID]map[kubetypes.UID][]allocation),
+				multiPoolMgr: mgr,
+				ipv4Enabled:  daemonCfg.IPv4Enabled(),
+				ipv6Enabled:  daemonCfg.IPv6Enabled(),
+			}
+
+			maps.Insert(cmds, maps.All(script.DefaultCmds()))
+			maps.Insert(cmds, maps.All(commands(t.Context(), driver, cs.ResourceV1())))
+
+			return &script.Engine{Cmds: cmds}
+		}, []string{}, "testdata/*.txtar")
+}
+
+func commands(
+	ctx context.Context,
+	driver *Driver,
+	client typedv1.ResourceV1Interface,
+) map[string]script.Cmd {
+	return map[string]script.Cmd{
+		"driver/prepare-resource-claims": script.Command(
+			script.CmdUsage{
+				Summary: "Call PrepareResourceClaims to prepare resources",
+				Args:    "claims",
+			},
+			func(_ *script.State, args ...string) (script.WaitFunc, error) {
+				if len(args) == 0 {
+					return nil, script.ErrUsage
+				}
+
+				var claims []*resourcev1.ResourceClaim
+				for _, arg := range args {
+					obj, err := toNamespacedName(arg)
+					if err != nil {
+						return nil, err
+					}
+
+					claim, err := client.ResourceClaims(obj.Namespace).Get(ctx, obj.Name, metav1.GetOptions{})
+					if err != nil {
+						return nil, err
+					}
+					claims = append(claims, claim)
+				}
+
+				_, err := driver.PrepareResourceClaims(ctx, claims)
+
+				return nil, err
+			},
+		),
+		"driver/unprepare-resource-claims": script.Command(
+			script.CmdUsage{
+				Summary: "Call UprepareResourceClaims to release resources",
+				Args:    "claims",
+			},
+			func(_ *script.State, args ...string) (script.WaitFunc, error) {
+				if len(args) == 0 {
+					return nil, script.ErrUsage
+				}
+
+				var claims []kubeletplugin.NamespacedObject
+
+				for _, arg := range args {
+					obj, err := toNamespacedName(arg)
+					if err != nil {
+						return nil, err
+					}
+
+					claim, err := client.ResourceClaims(obj.Namespace).Get(ctx, obj.Name, metav1.GetOptions{})
+					if err != nil {
+						return nil, err
+					}
+
+					claims = append(claims, kubeletplugin.NamespacedObject{
+						NamespacedName: kubetypes.NamespacedName{
+							Namespace: claim.Namespace,
+							Name:      claim.Name,
+						},
+						UID: claim.UID,
+					})
+				}
+
+				_, err := driver.UnprepareResourceClaims(ctx, claims)
+
+				return nil, err
+			},
+		),
+	}
+}
+
+func toNamespacedName(s string) (kubetypes.NamespacedName, error) {
+	tokens := strings.Split(s, "/")
+	switch len(tokens) {
+	case 2:
+		return kubetypes.NamespacedName{Namespace: tokens[0], Name: tokens[1]}, nil
+	case 1:
+		return kubetypes.NamespacedName{Namespace: "default", Name: tokens[0]}, nil
+	default:
+		return kubetypes.NamespacedName{}, fmt.Errorf("invalid claim name: %s", s)
+	}
+}
