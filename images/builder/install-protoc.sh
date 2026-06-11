a9ce6dc2c1068cead30a7c4011391073f3ed81a1
Kirk Crane <krcrane@ca.ibm.com>
2026-06-11 14:17:27 -0700

    fix up merge conflict

--- images/builder/install-protoc.sh
+++ images/builder/install-protoc.sh
@@ -9,11 +9,7 @@ set -o pipefail
 set -o nounset
 
 # renovate: datasource=github-release-attachments depName=protocolbuffers/protobuf
-<<<<<<< HEAD
 protoc_version="v35.0"
-=======
-protoc_version="v34.2"
->>>>>>> 4635150dbb (images,bpf: Add s390x build support)
 protoc_ersion="${protoc_version//v/}"
 arch=$(arch)
 if [[ "${arch}" == "aarch64" ]]; then
