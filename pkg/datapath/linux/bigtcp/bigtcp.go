cilium/cilium

commit f8762a6bd9f17610af1098995a0b36d5e3a7dc3a

cilium, bigtcp: BIG TCP for tunnels
Add a command line option to enable BIG TCP for tunnels. Unfortunately,
it's problematic to probe the kernel support, because older kernels
allow setting gro_max_size and gso_max_size > 65536 on tunnel netdevs,
it just fails in the kernel datapath to aggregate such big packets. So,
it's controlled by the admin, and, if enabled, cilium_vxlan or
cilium_geneve device will get an increased aggregation size.

Note that settings are applied when the tunnel netdev is created. It
doesn't exist yet at the startBIGTCP stage.

Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

pkg/datapath/linux/bigtcp/bigtcp.go
@@ -29,8 +29,9 @@ const (
 )
 
 var defaultUserConfig = types.BigTCPUserConfig{
-	EnableIPv6BIGTCP: false,
-	EnableIPv4BIGTCP: false,
+	EnableIPv6BIGTCP:   false,
+	EnableIPv4BIGTCP:   false,
+	EnableTunnelBIGTCP: false,
 }
 
 var Cell = cell.Module(
@@ -46,10 +47,10 @@ var Cell = cell.Module(
 func newDefaultConfiguration(userConfig types.BigTCPUserConfig) *Configuration {
 	return &Configuration{
 		BigTCPUserConfig: userConfig,
-		groIPv4MaxSize:   defaultGROMaxSize,
-		gsoIPv4MaxSize:   defaultGSOMaxSize,
-		groIPv6MaxSize:   defaultGROMaxSize,
-		gsoIPv6MaxSize:   defaultGSOMaxSize,
+		groIPv4MaxSize:   0,
+		gsoIPv4MaxSize:   0,
+		groIPv6MaxSize:   0,
+		gsoIPv6MaxSize:   0,
 	}
 }
 
@@ -97,7 +98,7 @@ func (c *Configuration) GetGSOIPv4MaxSize() int {
 
 // If an error is returned the caller is responsible for rolling back
 // any partial changes.
-func setGROGSOIPv6MaxSize(log *slog.Logger, device string, GROMaxSize, GSOMaxSize int) error {
+func SetGROGSOIPv6MaxSize(log *slog.Logger, device string, GROMaxSize, GSOMaxSize int) error {
 	link, err := safenetlink.LinkByName(device)
 	if err != nil {
 		log.Warn("Link does not exist",
@@ -124,7 +125,7 @@ func setGROGSOIPv6MaxSize(log *slog.Logger, device string, GROMaxSize, GSOMaxSiz
 
 // If an error is returned the caller is responsible for rolling back
 // any partial changes.
-func setGROGSOIPv4MaxSize(log *slog.Logger, device string, GROMaxSize, GSOMaxSize int) error {
+func SetGROGSOIPv4MaxSize(log *slog.Logger, device string, GROMaxSize, GSOMaxSize int) error {
 	link, err := safenetlink.LinkByName(device)
 	if err != nil {
 		log.Warn("Link does not exist",
@@ -204,8 +205,8 @@ type params struct {
 
 func validateConfig(cfg types.BigTCPUserConfig, daemonCfg *option.DaemonConfig, ipsecCfg types.IPsecConfig) error {
 	if cfg.EnableIPv6BIGTCP || cfg.EnableIPv4BIGTCP {
-		if daemonCfg.TunnelingEnabled() {
-			return errors.New("BIG TCP is not supported in tunneling mode")
+		if daemonCfg.TunnelingEnabled() && !cfg.EnableTunnelBIGTCP {
+			return errors.New("BIG TCP in tunneling mode requires pending kernel support and needs to be enabled by enable-tunnel-big-tcp")
 		}
 		if ipsecCfg.Enabled() {
 			return errors.New("BIG TCP is not supported with encryption enabled")
@@ -302,7 +303,7 @@ func startBIGTCP(p params, cfg *Configuration) error {
 		// isn't greater than 64KB. So it needs to set the IPv6 one first
 		// as otherwise the IPv4 BIG TCP value will be reset.
 		if haveIPv6 {
-			err = setGROGSOIPv6MaxSize(p.Log, device,
+			err = SetGROGSOIPv6MaxSize(p.Log, device,
 				cfg.groIPv6MaxSize, cfg.gsoIPv6MaxSize)
 			if err != nil {
 				p.Log.Warn("Could not modify IPv6 gro_max_size and gso_max_size"+disableMsg,
@@ -319,7 +320,7 @@ func startBIGTCP(p params, cfg *Configuration) error {
 			)
 		}
 		if haveIPv4 {
-			err = setGROGSOIPv4MaxSize(p.Log, device,
+			err = SetGROGSOIPv4MaxSize(p.Log, device,
 				cfg.groIPv4MaxSize, cfg.gsoIPv4MaxSize)
 			if err != nil {
 				msg := "Could not modify IPv4 gro_max_size and gso_max_size" + disableMsg
@@ -350,7 +351,7 @@ func startBIGTCP(p params, cfg *Configuration) error {
 		}
 		for _, device := range modifiedDevices {
 			if bigv4 {
-				err = setGROGSOIPv4MaxSize(p.Log, device,
+				err = SetGROGSOIPv4MaxSize(p.Log, device,
 					defaultGROMaxSize, defaultGSOMaxSize)
 				if err != nil {
 					p.Log.Warn("Could not reset IPv4 gro_max_size and gso_max_size",
@@ -362,7 +363,7 @@ func startBIGTCP(p params, cfg *Configuration) error {
 				p.Log.Info("Resetting IPv4 gso_max_size and gro_max_size", logfields.Device, device)
 			}
 			if bigv6 {
-				err = setGROGSOIPv6MaxSize(p.Log, device,
+				err = SetGROGSOIPv6MaxSize(p.Log, device,
 					defaultGROMaxSize, defaultGSOMaxSize)
 				if err != nil {
 					p.Log.Warn("Could not reset IPv6 gro_max_size and gso_max_size",

pkg/datapath/fake/types/bigtcp.go
@@ -10,6 +10,9 @@ type BigTCPUserConfig struct {
 
 	// EnableIPv4BIGTCP enables IPv4 BIG TCP (larger GSO/GRO limits) for the node including pods.
 	EnableIPv4BIGTCP bool
+
+	// EnableTunnelBIGTCP enables BIG TCP (larger GSO/GRO limits) in tunneling mode for VXLAN and GENEVE tunnels.
+	EnableTunnelBIGTCP bool
 }
 
 func (def BigTCPUserConfig) IsIPv4Enabled() bool {
@@ -18,4 +21,8 @@ func (def BigTCPUserConfig) IsIPv4Enabled() bool {
 
 func (def BigTCPUserConfig) IsIPv6Enabled() bool {
 	return def.EnableIPv6BIGTCP
+}
+
+func (def BigTCPUserConfig) IsTunnelEnabled() bool {
+	return def.EnableTunnelBIGTCP
 }

pkg/datapath/types/bigtcp.go
@@ -8,8 +8,9 @@ import (
 )
 
 const (
-	EnableIPv4BIGTCPFlag = "enable-ipv4-big-tcp"
-	EnableIPv6BIGTCPFlag = "enable-ipv6-big-tcp"
+	EnableIPv4BIGTCPFlag   = "enable-ipv4-big-tcp"
+	EnableIPv6BIGTCPFlag   = "enable-ipv6-big-tcp"
+	EnableTunnelBIGTCPFlag = "enable-tunnel-big-tcp"
 )
 
 // BigTCPUserConfig are the configuration flags that the user can modify.
@@ -19,11 +20,15 @@ type BigTCPUserConfig struct {
 
 	// EnableIPv4BIGTCP enables IPv4 BIG TCP (larger GSO/GRO limits) for the node including pods.
 	EnableIPv4BIGTCP bool
+
+	// EnableTunnelBIGTCP enables BIG TCP (larger GSO/GRO limits) in tunneling mode for VXLAN and GENEVE tunnels.
+	EnableTunnelBIGTCP bool
 }
 
 func (def BigTCPUserConfig) Flags(flags *pflag.FlagSet) {
 	flags.Bool(EnableIPv4BIGTCPFlag, def.EnableIPv4BIGTCP, "Enable IPv4 BIG TCP option which increases device's maximum GRO/GSO limits for IPv4")
 	flags.Bool(EnableIPv6BIGTCPFlag, def.EnableIPv6BIGTCP, "Enable IPv6 BIG TCP option which increases device's maximum GRO/GSO limits for IPv6")
+	flags.Bool(EnableTunnelBIGTCPFlag, def.EnableTunnelBIGTCP, "Enable BIG TCP in tunneling mode and increase maximum GRO/GSO limits for VXLAN/GENEVE tunnels")
 }
 
 func (def BigTCPUserConfig) IsIPv4Enabled() bool {
@@ -34,7 +39,19 @@ func (def BigTCPUserConfig) IsIPv6Enabled() bool {
 	return def.EnableIPv6BIGTCP
 }
 
+func (def BigTCPUserConfig) IsTunnelEnabled() bool {
+	return def.EnableTunnelBIGTCP
+}
+
 type BigTCPConfig interface {
 	IsIPv4Enabled() bool
 	IsIPv6Enabled() bool
+	IsTunnelEnabled() bool
+}
+
+type BigTCPConfiguration interface {
+	GetGROIPv6MaxSize() int
+	GetGSOIPv6MaxSize() int
+	GetGROIPv4MaxSize() int
+	GetGSOIPv4MaxSize() int
 }

install/kubernetes/cilium/templates/cilium-configmap.yaml
@@ -706,6 +706,7 @@ data:
   enable-ipv4-big-tcp: {{ .Values.enableIPv4BIGTCP | quote }}
   enable-ipv6-big-tcp: {{ .Values.enableIPv6BIGTCP | quote }}
   enable-ipv6-masquerade: {{ .Values.enableIPv6Masquerade | quote }}
+  enable-tunnel-big-tcp: {{ .Values.enableTunnelBIGTCP | quote }}
 
 {{- if hasKey .Values.bpf "enableTCX" }}
   enable-tcx: {{ .Values.bpf.enableTCX | quote }}

install/kubernetes/cilium/values.yaml
@@ -2294,6 +2294,8 @@ enableMasqueradeRouteSource: false
 enableIPv4BIGTCP: false
 # -- Enables IPv6 BIG TCP support which increases maximum IPv6 GSO/GRO limits for nodes and pods
 enableIPv6BIGTCP: false
+# -- Enable BIG TCP in tunneling mode and increase maximum GRO/GSO limits for VXLAN/GENEVE tunnels
+enableTunnelBIGTCP: false
 nat:
   # -- Number of the top-k SNAT map connections to track in Cilium statedb.
   mapStatsEntries: 32

install/kubernetes/cilium/values.yaml.tmpl
@@ -2316,6 +2316,8 @@ enableMasqueradeRouteSource: false
 enableIPv4BIGTCP: false
 # -- Enables IPv6 BIG TCP support which increases maximum IPv6 GSO/GRO limits for nodes and pods
 enableIPv6BIGTCP: false
+# -- Enable BIG TCP in tunneling mode and increase maximum GRO/GSO limits for VXLAN/GENEVE tunnels
+enableTunnelBIGTCP: false
 
 nat:
   # -- Number of the top-k SNAT map connections to track in Cilium statedb.

pkg/datapath/fake/types/loader.go
@@ -39,7 +39,7 @@ func (f *FakeLoader) ReinitializeHostDev(ctx context.Context, mtu int) error {
 }
 
 // Reinitialize does nothing.
-func (f *FakeLoader) Reinitialize(ctx context.Context, lnc *datapath.LocalNodeConfiguration, tunnelConfig tunnel.Config, iptMgr datapath.IptablesManager, p datapath.Proxy) error {
+func (f *FakeLoader) Reinitialize(ctx context.Context, lnc *datapath.LocalNodeConfiguration, tunnelConfig tunnel.Config, iptMgr datapath.IptablesManager, p datapath.Proxy, bigtcp datapath.BigTCPConfiguration) error {
 	return nil
 }
 

pkg/datapath/loader/base.go
@@ -329,7 +329,7 @@ func (l *loader) ReinitializeHostDev(ctx context.Context, mtu int) error {
 // BPF programs, netfilter rule configuration and reserving routes in IPAM for
 // locally detected prefixes. It may be run upon initial Cilium startup, after
 // restore from a previous Cilium run, or during regular Cilium operation.
-func (l *loader) Reinitialize(ctx context.Context, lnc *datapath.LocalNodeConfiguration, tunnelConfig tunnel.Config, iptMgr datapath.IptablesManager, p datapath.Proxy) error {
+func (l *loader) Reinitialize(ctx context.Context, lnc *datapath.LocalNodeConfiguration, tunnelConfig tunnel.Config, iptMgr datapath.IptablesManager, p datapath.Proxy, bigtcp datapath.BigTCPConfiguration) error {
 	sysSettings := []tables.Sysctl{
 		{Name: []string{"net", "core", "bpf_jit_enable"}, Val: "1", IgnoreErr: true, Warn: "Unable to ensure that BPF JIT compilation is enabled. This can be ignored when Cilium is running inside non-host network namespace (e.g. with kind or minikube)"},
 		{Name: []string{"net", "ipv4", "conf", "all", "rp_filter"}, Val: "0", IgnoreErr: false},
@@ -384,7 +384,7 @@ func (l *loader) Reinitialize(ctx context.Context, lnc *datapath.LocalNodeConfig
 	}
 
 	if err := setupTunnelDevice(l.logger, l.sysctl, tunnelConfig.EncapProtocol(), tunnelConfig.Port(),
-		tunnelConfig.SrcPortLow(), tunnelConfig.SrcPortHigh(), lnc.DeviceMTU); err != nil {
+		tunnelConfig.SrcPortLow(), tunnelConfig.SrcPortHigh(), lnc.DeviceMTU, bigtcp); err != nil {
 		return fmt.Errorf("failed to setup %s tunnel device: %w", tunnelConfig.EncapProtocol(), err)
 	}
 

commit 07e9d6b8250a1656758fc15048f7e3cae3d2cfef

cilium, bigtcp: Fix the UDP tunnel check
It's not enough to check daemonCfg.TunnelingEnabled() to detect all
cases when VXLAN or GENEVE tunnels are created. Many other features
request tunnel creation with tunnel.NewEnabler(true).

Replace the check with the correct one:

tunnelConfig.EncapProtocol() != tunnel.Disabled

Fixes: f8762a6bd9f1 ("cilium, bigtcp: BIG TCP for tunnels")
Fixes: #46337
Signed-off-by: Alice Mikityanska <alice@isovalent.com>

pkg/datapath/linux/bigtcp/bigtcp.go
@@ -256,7 +256,7 @@ type params struct {
 	Devices      statedb.Table[*tables.Device]
 }
 
-func validateConfig(cfg UserConfig, daemonCfg *option.DaemonConfig, ipsecCfg ipsec.Config, dsrDispatch string, bigtcpTunnel bool) error {
+func validateConfig(cfg UserConfig, daemonCfg *option.DaemonConfig, ipsecCfg ipsec.Config, tunnelConfig tunnel.Config, dsrDispatch string, bigtcpTunnel bool) error {
 	if cfg.EnableIPv6BIGTCP || cfg.EnableIPv4BIGTCP {
 		// Check all configurations where Cilium creates tunnel devices
 		// that don't support BIG TCP.
@@ -264,7 +264,7 @@ func validateConfig(cfg UserConfig, daemonCfg *option.DaemonConfig, ipsecCfg ips
 			return errors.New("bpf-lb-dsr-dispatch ipip creates IPIP tunnels that aren't compatible with BIG TCP")
 		}
 		if !bigtcpTunnel {
-			if daemonCfg.TunnelingEnabled() {
+			if tunnelConfig.EncapProtocol() != tunnel.Disabled {
 				return errors.New("BIG TCP in tunneling mode requires pending kernel support")
 			}
 			if dsrDispatch != loadbalancer.DSRDispatchOption {
@@ -288,7 +288,7 @@ func newBIGTCP(lc cell.Lifecycle, p params) (Config, error) {
 		logfields.State, bigtcpTunnel,
 	)
 
-	if err := validateConfig(p.UserConfig, p.DaemonConfig, p.IPsecConfig, p.LBConfig.DSRDispatch, bigtcpTunnel); err != nil {
+	if err := validateConfig(p.UserConfig, p.DaemonConfig, p.IPsecConfig, p.TunnelConfig, p.LBConfig.DSRDispatch, bigtcpTunnel); err != nil {
 		return nil, err
 	}
 	cfg := newDefaultConfiguration(p.UserConfig)