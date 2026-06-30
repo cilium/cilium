9004cc14a24588bed8efb6585e3d787c0fc8de23
Blaz Zupan <blaz@google.com>
2026-06-30 12:57:30 -0700

    fqdn: add helper to get dns selectors from config
    Add a helper that translates the new CLI flags into pod/namespace/CIDR selectors and L4 rules. This will let us auto-configure policy rules that route DNS traffic to the proxy for FQDN inspection.
    
    Signed-off-by: Blaz Zupan <blaz@google.com>

--- pkg/fqdn/config/config.go
+++ pkg/fqdn/config/config.go
@@ -4,7 +4,15 @@
 package config
 
 import (
+	"strconv"
+
 	"github.com/spf13/pflag"
+
+	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/cilium/cilium/pkg/policy/types"
 )
 
 type FQDNPolicyDNSLookupConfig struct {
@@ -32,4 +40,50 @@ func (def FQDNPolicyDNSLookupConfig) Flags(flags *pflag.FlagSet) {
 	flags.String("fqdn-policy-dns-lookup-namespace", def.FQDNPolicyDNSLookupNamespace, "Namespace for DNS pod lookups in FQDN policies")
 	flags.StringToString("fqdn-policy-dns-lookup-pod-labels", def.FQDNPolicyDNSLookupPodLabels, "Pod labels for DNS lookups in FQDN policies")
 	flags.Int("fqdn-policy-dns-lookup-port", def.FQDNPolicyDNSLookupPort, "Port for DNS lookups in FQDN policies")
+}
+
+// GetFQDNPolicyDNSSelectors creates and returns the pod or IP selectors targeting allowed DNS servers
+// for FQDN policies, along with the fully constructed L4 PortRules containing the allowed FQDN rules.
+func GetFQDNPolicyDNSSelectors(dnsRules api.PortRulesDNS) (types.Selectors, api.PortRules) {
+	var dnsSelectors types.Selectors
+
+	cfg := GlobalFQDNPolicyDNSLookupConfig
+
+	// Add Pod Selector based on config.
+	if len(cfg.FQDNPolicyDNSLookupPodLabels) > 0 {
+		matchLabels := make(map[string]slim_metav1.MatchLabelsValue, len(cfg.FQDNPolicyDNSLookupPodLabels))
+		for k, v := range cfg.FQDNPolicyDNSLookupPodLabels {
+			matchLabels[k] = slim_metav1.MatchLabelsValue(v)
+		}
+
+		if cfg.FQDNPolicyDNSLookupNamespace != "" {
+			matchLabels[k8sConst.PodNamespaceLabel] = slim_metav1.MatchLabelsValue(cfg.FQDNPolicyDNSLookupNamespace)
+		}
+
+		podSelector := api.NewESFromK8sLabelSelector(
+			labels.LabelSourceK8sKeyPrefix,
+			&slim_metav1.LabelSelector{
+				MatchLabels: matchLabels,
+			},
+		)
+		dnsSelectors = append(dnsSelectors, types.ToSelector(podSelector))
+	}
+
+	// Add IP Selectors if IPs are provided
+	if len(cfg.FQDNPolicyDNSLookupIPs) > 0 {
+		for _, ip := range cfg.FQDNPolicyDNSLookupIPs {
+			dnsSelectors = append(dnsSelectors, types.ToSelector(api.CIDR(ip)))
+		}
+	}
+
+	portString := strconv.Itoa(cfg.FQDNPolicyDNSLookupPort)
+	portRules := api.PortRules{{
+		Ports: []api.PortProtocol{
+			{Port: portString, Protocol: api.ProtoUDP},
+			{Port: portString, Protocol: api.ProtoTCP},
+		},
+		Rules: &api.L7Rules{DNS: dnsRules},
+	}}
+
+	return dnsSelectors, portRules
 }
