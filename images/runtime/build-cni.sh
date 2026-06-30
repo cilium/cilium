f8ed44eb3549eddf8d8e8ebf3e73af1924d55052
Sekhar Sankaramanchi <sekhar@isovalent.com>
2026-06-30 00:26:27 +0000

    chore: build cni/loopback from release archive
    [ upstream commit d49792a9cc1ec1689d0309f3162f03f382fb5696 ]
    
    added `-ldflags -s -w` to ensure that debug symbols are stripped
    added CGO_ENABLED=0 to statically compile the binary
    
    Signed-off-by: Sekhar Sankaramanchi <sekhar@isovalent.com>
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

--- images/runtime/build-cni.sh
+++ images/runtime/build-cni.sh
@@ -14,18 +14,12 @@ cni_version="v1.9.1"
 mkdir -p /go/src/github.com/containernetworking
 cd /go/src/github.com/containernetworking
 
-git clone https://github.com/containernetworking/plugins.git
-cd plugins
-
-git checkout -b "${cni_version}" "${cni_version}"
-git --no-pager remote -v
-git --no-pager log -1
+curl -sSL https://github.com/containernetworking/plugins/archive/refs/tags/${cni_version}.tar.gz | tar xz
+mv plugins-${cni_version#v} cni
+cd cni
 
 mkdir -p "/out/cni"
+GOARCH="${TARGETARCH}" CGO_ENABLED=0 go build -o "/out/cni/loopback" \
+    -ldflags "-s -w -X github.com/containernetworking/plugins/pkg/utils/buildversion.BuildVersion=${cni_version}" ./plugins/main/loopback
 
-export CGO_ENABLED=0
-
-GOARCH="${TARGETARCH}" ./build_linux.sh -ldflags "-extldflags -static -X github.com/containernetworking/plugins/pkg/utils/buildversion.BuildVersion=${cni_version}"
-
-cp bin/loopback /out/cni/loopback
-strip /out/cni/loopback
+go version -m /out/cni/loopback | grep -q "GOARCH=$TARGETARCH" || (echo "Architecture mismatch: binary GOARCH does not match $TARGETARCH" && exit 1)
