12b687454e113870e5ec7d77bf90f0365546af7f
Julian Wiedmann <jwi@isovalent.com>
2026-06-30 09:16:39 +0300

    bpf: tests: fix up definition for ENABLE_ROUTING
    Define ENABLE_ROUTING so that usage of is_defined() in the datapath works
    as expected.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/tests/l7_lb_local_backend_host.c
+++ bpf/tests/l7_lb_local_backend_host.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
 /* Copyright Authors of Cilium */
 
-#define ENABLE_ROUTING
+#define ENABLE_ROUTING		1
 
 #include "l7_lb_local_backend.h"
