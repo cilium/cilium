8727bee84d7246306e339c8e828435fbf6f9be5b
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2026-07-08 13:46:45 +0200

    envoy: fix cluster locality handling
    Make /cilium-locality-cluster use the selected xDS mode instead of always
    using EDS.
    
    For external Envoy, render the locality cluster with ADS config when
    envoy.xdsMode is ads or strict-ads, and keep the existing gRPC
    apiConfigSource for split mode.
    
    For embedded Envoy, reuse GetXDSConfigSource() when building the locality
    cluster so EDS references follow the configured xDS mode consistently.
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- pkg/envoy/locality_test.go
+++ pkg/envoy/locality_test.go
@@ -8,22 +8,68 @@ import (
 
 	envoy_config_bootstrap "github.com/envoyproxy/go-control-plane/envoy/config/bootstrap/v3"
 	envoy_config_cluster "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
+	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	"github.com/stretchr/testify/require"
+
+	"github.com/cilium/cilium/pkg/envoy/config"
 )
 
 func TestAppendEmbeddedLocalityBootstrap(t *testing.T) {
-	bs := &envoy_config_bootstrap.Bootstrap{
-		StaticResources: &envoy_config_bootstrap.Bootstrap_StaticResources{},
+	tests := []struct {
+		name      string
+		xdsMode   string
+		assertEDS func(t *testing.T, edsConfig *corev3.ConfigSource)
+	}{
+		{
+			name:    "split",
+			xdsMode: config.EnvoyXDSModeSplit,
+			assertEDS: func(t *testing.T, edsConfig *corev3.ConfigSource) {
+				apiConfigSource := edsConfig.GetApiConfigSource()
+				require.NotNil(t, apiConfigSource)
+				require.NotEmpty(t, apiConfigSource.GetGrpcServices())
+				require.Equal(t, CiliumXDSClusterName, apiConfigSource.GetGrpcServices()[0].GetEnvoyGrpc().GetClusterName())
+				require.Nil(t, edsConfig.GetAds())
+			},
+		},
+		{
+			name:    "ads",
+			xdsMode: config.EnvoyXDSModeADS,
+			assertEDS: func(t *testing.T, edsConfig *corev3.ConfigSource) {
+				require.NotNil(t, edsConfig.GetAds())
+				require.Nil(t, edsConfig.GetApiConfigSource())
+			},
+		},
+		{
+			name:    "strict-ads",
+			xdsMode: config.EnvoyXDSModeStrictADS,
+			assertEDS: func(t *testing.T, edsConfig *corev3.ConfigSource) {
+				require.NotNil(t, edsConfig.GetAds())
+				require.Nil(t, edsConfig.GetApiConfigSource())
+			},
+		},
 	}
 
-	appendEmbeddedLocalityBootstrap(bs, 7, "zone-a")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SetXDSMode(tt.xdsMode)
+			t.Cleanup(func() { SetXDSMode("") })
 
-	require.Equal(t, LocalityClusterName, bs.GetClusterManager().GetLocalClusterName())
-	require.Equal(t, "zone-a", bs.GetNode().GetLocality().GetZone())
-	require.Len(t, bs.GetStaticResources().GetClusters(), 1)
+			bs := &envoy_config_bootstrap.Bootstrap{
+				StaticResources: &envoy_config_bootstrap.Bootstrap_StaticResources{},
+			}
 
-	cluster := bs.GetStaticResources().GetClusters()[0]
-	require.Equal(t, LocalityClusterName, cluster.GetName())
-	require.Equal(t, envoy_config_cluster.Cluster_EDS, cluster.GetType())
-	require.Equal(t, CiliumXDSClusterName, cluster.GetEdsClusterConfig().GetEdsConfig().GetApiConfigSource().GetGrpcServices()[0].GetEnvoyGrpc().GetClusterName())
+			appendEmbeddedLocalityBootstrap(bs, 7, "zone-a")
+
+			require.Equal(t, LocalityClusterName, bs.GetClusterManager().GetLocalClusterName())
+			require.Equal(t, "zone-a", bs.GetNode().GetLocality().GetZone())
+			require.Len(t, bs.GetStaticResources().GetClusters(), 1)
+
+			cluster := bs.GetStaticResources().GetClusters()[0]
+			require.Equal(t, LocalityClusterName, cluster.GetName())
+			require.Equal(t, envoy_config_cluster.Cluster_EDS, cluster.GetType())
+			require.Equal(t, LocalityClusterName, cluster.GetEdsClusterConfig().GetServiceName())
+
+			tt.assertEDS(t, cluster.GetEdsClusterConfig().GetEdsConfig())
+		})
+	}
 }
