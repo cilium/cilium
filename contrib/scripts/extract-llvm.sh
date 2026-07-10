c461cbfc1b29153bab4111135d7a52eb7a39b9fc
Hadrien Patte <hadrien.patte@datadoghq.com>
2026-07-10 14:32:30 +0200

    images/builder: Base on Ubuntu directly instead of the runtime image
    Currently, both the cilium-agent and the cilium-builder images are based on the same shared cilium-runtime base. This is causing challenges because those two images have different destinations:
    * The cilium-agent image is meant to be running in production, hence we want it to be as slim and light as possible (see #47065)
    * The cilium-builder image is used in CI and development environments and as such is not optimized to be light but rather to be quite full, including exhaustive cross-arch compiler toolchains and various utils tools
    
    This situation makes the common cilium-runtime base awkward: when we want to optimize the cilium-agent image, the leverage is at the source, but attempts to shrink the cilium-runtime base to achieve gains on the cilium-agent image cause problems for the cilium-builder image that needs what we want to remove from the cilium-agent image.
    
    A concrete example of that situation can be observed in PR #46446: That PR aims to remove the package manager suite from the cilium-agent image. In order to achieve that, it removes it at the source in the cilium-runtime image. But doing so breaks the cilium-builder image that does need a package manager.
    
    In order to unblock this situation, this PR changes the base of the cilium-builder image from the cilium-runtime image to a regular ubuntu base image. That way the cilium-runtime becomes only the base for the cilium-agent image and we can start applying slim-down optimizations there.
    
    This change is possible because the cilium-builder did not actually require most of what the cilium-runtime image adds on top of its ubuntu base. The only additional needed components is the `clang` binary that is transferred from the pre-built LLVM image.
    
    With this PR, the cilium-builder image no longer contains the following components:
    * iptables
    * iproute2
    * ipset
    * kmod
    * gops
    * the CNI loopback plugin
    * the iptables-wrapper
    * libatomic1
    * bash-completion
    * llc
    
    I combed through the various CI steps that use the builder image and local development make targets and I don't think any of those need any of the above.
    
    Along with that change, the `extract-llvm.sh` script used in CI to extract the LLVM toolchain gets updated to directly get it out of the cilium-llvm image instead of going through the cilium-builder one as a proxy.
    
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- contrib/scripts/extract-llvm.sh
+++ contrib/scripts/extract-llvm.sh
@@ -6,7 +6,7 @@ OUT=${1:-"$PWD"}
 
 cd "$(dirname "$0")/../.."
 
-IMAGE=$(cat images/cilium/Dockerfile | grep '^ARG CILIUM_BUILDER_IMAGE=' | cut -d '=' -f 2)
+IMAGE=$(cat images/runtime/Dockerfile | grep '^ARG CILIUM_LLVM_IMAGE=' | cut -d '=' -f 2)
 NAME=$(docker create "$IMAGE" /bin/true)
 trap 'docker rm "$NAME" > /dev/null' EXIT
 mkdir -p "$OUT"
