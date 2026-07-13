ad7633779952f26ebfdfee31d0067e9e3c10288f
Deepesh Pathak <deepeshpathak09@gmail.com>
2026-07-13 16:52:42 -0700

    policy: validate and sanitize toServices selectors in rules
    This commit adds the missing logic to validate and sanitize `toServices`
    egress policy rule selectors.
    
    Signed-off-by: Deepesh Pathak <deepeshpathak09@gmail.com>

--- pkg/policy/api/service.go
+++ pkg/policy/api/service.go
@@ -6,6 +6,16 @@ package api
 // ServiceSelector is a label selector for k8s services
 type ServiceSelector EndpointSelector
 
+// Validate the underlying LabelSelector.
+func (s *ServiceSelector) Validate() error {
+	return (*EndpointSelector)(s).Validate()
+}
+
+// Sanitize the EndpointSelector.
+func (s *ServiceSelector) Sanitize() {
+	(*EndpointSelector)(s).Sanitize()
+}
+
 // Service selects policy targets that are bundled as part of a
 // logical load-balanced service.
 //
@@ -15,6 +25,21 @@ type Service struct {
 	K8sServiceSelector *K8sServiceSelectorNamespace `json:"k8sServiceSelector,omitempty"`
 	// K8sService selects service by name and namespace pair
 	K8sService *K8sServiceNamespace `json:"k8sService,omitempty"`
+}
+
+func (s *Service) Validate() error {
+	if s.K8sServiceSelector != nil {
+		if err := s.K8sServiceSelector.Selector.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *Service) Sanitize() {
+	if s.K8sServiceSelector != nil {
+		s.K8sServiceSelector.Selector.Sanitize()
+	}
 }
 
 // K8sServiceNamespace selects services by name and, optionally, namespace.
