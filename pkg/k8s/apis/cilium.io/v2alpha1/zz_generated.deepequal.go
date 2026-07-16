ad3b0e4f03559acb310238deab2dba84696c5e5c
Bernardo Soares <bersoare@isovalent.com>
2026-07-16 13:50:54 +0100

    network driver: dummy device manager takes a count value
    missed changes from feature/dra-driver branch
    commit 3c7ad3fc2dc97b912543daf31cf832df2123ad07
    
    Signed-off-by: Bernardo Soares <bersoare@isovalent.com>

--- pkg/k8s/apis/cilium.io/v2alpha1/zz_generated.deepequal.go
+++ pkg/k8s/apis/cilium.io/v2alpha1/zz_generated.deepequal.go
@@ -1773,6 +1773,9 @@ func (in *DummyDeviceManagerConfig) DeepEqual(other *DummyDeviceManagerConfig) b
 	if in.Enabled != other.Enabled {
 		return false
 	}
+	if in.Count != other.Count {
+		return false
+	}
 
 	return true
 }
