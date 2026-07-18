2ff5ea9f02d04db73d8ee3f7a8020929ae930348
Venkat Krishnan <venkatkr@google.com>
2026-07-18 00:03:45 +0000

    bpf, go: define and register socket-local storage maps for reverse NAT
    Define the BPF_MAP_TYPE_SK_STORAGE maps `cilium_lb4_reverse_sk_st` and
    `cilium_lb6_reverse_sk_st` in the BPF datapath to store reverse NAT translation
    state for TCP and connected UDP sockets.
    
    Add `SockRevNat4St` and `SockRevNat6St` maps to the Go load balancer map
    definitions and register them in the central BPF map registry.
    
    Regenerate the BPF skeletons and update the alignment checks to cover the
    new maps. Also register the new maps in all test suites and mock registries
    (clustermesh, healthserver, envoyconfig, reconciler, etc.) and wire up `registry.Cell`
    at startup in the REPL Hive to prevent panics during test execution.
    
    Signed-off-by: Venkat Krishnan <venkatkr@google.com>

--- pkg/datapath/types/types_generated.go
+++ pkg/datapath/types/types_generated.go
@@ -341,6 +341,17 @@ type IPv4RevNATTuple struct {
 	Pad     uint16
 }
 
+// IPv4SkStorageEntry is generated from the BPF C type ipv4_sk_storage_entry.
+type IPv4SkStorageEntry struct {
+	_              structs.HostLayout
+	Address        uint32
+	Port           uint16
+	RevNATIndex    uint16
+	BackendAddress uint32
+	BackendPort    uint16
+	Pad            uint16
+}
+
 // IPv6CTTuple is generated from the BPF C type ipv6_ct_tuple.
 type IPv6CTTuple struct {
 	_     structs.HostLayout
@@ -424,6 +435,23 @@ type IPv6RevNATTuple struct {
 	Port uint16
 	Pad  uint16
 	_    [4]byte
+}
+
+// IPv6SkStorageEntry is generated from the BPF C type ipv6_sk_storage_entry.
+type IPv6SkStorageEntry struct {
+	_       structs.HostLayout
+	Address struct {
+		_    structs.HostLayout
+		Addr [16]uint8
+	}
+	Port           uint16
+	RevNATIndex    uint16
+	BackendAddress struct {
+		_    structs.HostLayout
+		Addr [16]uint8
+	}
+	BackendPort uint16
+	Pad         uint16
 }
 
 // L2ResponderStats is generated from the BPF C type l2_responder_stats.
