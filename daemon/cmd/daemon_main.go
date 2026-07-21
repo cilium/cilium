dc5eaeea187a61627fef2aa544a2f4713dfa9e2c
vchammas <vvanessachammas@gmail.com>
2026-07-21 17:51:22 +0000

    cleanup: remove stale TODO
    This TODO was removed in commit 7d5ff0e9054d, but accidentally brought back. Removing again.
    
    Signed-off-by: vchammas <vvanessachammas@gmail.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -1088,7 +1088,6 @@ func initEnv(logger *slog.Logger, vp *viper.Viper) {
 	}
 
 	if option.Config.LocalRouterIPv4 != "" || option.Config.LocalRouterIPv6 != "" {
-		// TODO(weil0ng): add a proper check for ipam in PR# 15429.
 		if !option.Config.RequiresNativeRouting() {
 			logging.Fatal(logger, fmt.Sprintf("%s and %s require native or hybrid routing mode.", option.LocalRouterIPv4, option.LocalRouterIPv6))
 		}
