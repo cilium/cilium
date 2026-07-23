cilium/cilium

commit 112e08d4b37b891e9d16f8f60d71a519253f53d4

clustermesh: fix hardcode at service type
[ cherry-picked from cilium/cilium-cli repository ]

Signed-off-by: ii2day <ii2day.zoro@gmail.com>

cilium-cli/clustermesh/clustermesh.go
@@ -127,7 +127,7 @@ func (k *K8sClusterMesh) generateService() (*corev1.Service, error) {
 	}
 
 	if k.params.ServiceType != "" {
-		if k.params.ServiceType == "NodePort" {
+		if corev1.ServiceType(k.params.ServiceType) == corev1.ServiceTypeNodePort {
 			k.Log("⚠️  Using service type NodePort may fail when nodes are removed from the cluster!")
 		}
 		svc.Spec.Type = corev1.ServiceType(k.params.ServiceType)
@@ -1977,7 +1977,7 @@ func generateEnableHelmValues(params Parameters, flavor k8s.Flavor) (map[string]
 			log("🔮 Auto-exposing service within GCP VPC (cloud.google.com/load-balancer-type=Internal)")
 			helmVals["clustermesh"].(map[string]interface{})["apiserver"] = map[string]interface{}{
 				"service": map[string]interface{}{
-					"type": "LoadBalancer",
+					"type": corev1.ServiceTypeLoadBalancer,
 					"annotations": map[string]interface{}{
 						"cloud.google.com/load-balancer-type": "Internal",
 						// Allows cross-region access
@@ -1989,7 +1989,7 @@ func generateEnableHelmValues(params Parameters, flavor k8s.Flavor) (map[string]
 			log("🔮 Auto-exposing service within Azure VPC (service.beta.kubernetes.io/azure-load-balancer-internal)")
 			helmVals["clustermesh"].(map[string]interface{})["apiserver"] = map[string]interface{}{
 				"service": map[string]interface{}{
-					"type": "LoadBalancer",
+					"type": corev1.ServiceTypeLoadBalancer,
 					"annotations": map[string]interface{}{
 						"service.beta.kubernetes.io/azure-load-balancer-internal": "true",
 					},
@@ -1999,7 +1999,7 @@ func generateEnableHelmValues(params Parameters, flavor k8s.Flavor) (map[string]
 			log("🔮 Auto-exposing service within AWS VPC (service.beta.kubernetes.io/aws-load-balancer-internal: 0.0.0.0/0")
 			helmVals["clustermesh"].(map[string]interface{})["apiserver"] = map[string]interface{}{
 				"service": map[string]interface{}{
-					"type": "LoadBalancer",
+					"type": corev1.ServiceTypeLoadBalancer,
 					"annotations": map[string]interface{}{
 						"service.beta.kubernetes.io/aws-load-balancer-internal": "0.0.0.0/0",
 					},
@@ -2009,7 +2009,7 @@ func generateEnableHelmValues(params Parameters, flavor k8s.Flavor) (map[string]
 			return nil, fmt.Errorf("cannot auto-detect service type, please specify using '--service-type' option")
 		}
 	} else {
-		if params.ServiceType == "NodePort" {
+		if corev1.ServiceType(params.ServiceType) == corev1.ServiceTypeNodePort {
 			log("⚠️  Using service type NodePort may fail when nodes are removed from the cluster!")
 		}
 		helmVals["clustermesh"].(map[string]interface{})["apiserver"] = map[string]interface{}{

commit ad256e99056a96477aa1c233adb369b5b73f3998

cilium-cli/clustermesh: Fix bad type for service type
Helm expects /clustermesh/apiserver/service/type to be a string
according to install/kubernetes/cilium/values.schema.json:1404, but
it was passed as a v1.ServiceType causing the following error:

```
cilium clustermesh enable --context cluster1
🔮 Auto-exposing service within Azure VPC (service.beta.kubernetes.io/azure-load-balancer-internal)

Error: Unable to enable ClusterMesh: values don't meet the specifications of the schema(s) in the following chart(s):
cilium:
- at '/clustermesh/apiserver/service/type': invalid jsonType v1.ServiceType
```

This is fixed by casting it to a string.

Fixes: 112e08d4b37b ("clustermesh: fix hardcode at service type")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

cilium-cli/clustermesh/clustermesh.go
@@ -1005,7 +1005,7 @@ func generateEnableHelmValues(params Parameters, flavor k8s.Flavor) (map[string]
 			log("🔮 Auto-exposing service within GCP VPC (networking.gke.io/load-balancer-type=Internal)")
 			helmVals["clustermesh"].(map[string]any)["apiserver"] = map[string]any{
 				"service": map[string]any{
-					"type": corev1.ServiceTypeLoadBalancer,
+					"type": string(corev1.ServiceTypeLoadBalancer),
 					"annotations": map[string]any{
 						"networking.gke.io/load-balancer-type": "Internal",
 						// Allows cross-region access
@@ -1017,7 +1017,7 @@ func generateEnableHelmValues(params Parameters, flavor k8s.Flavor) (map[string]
 			log("🔮 Auto-exposing service within Azure VPC (service.beta.kubernetes.io/azure-load-balancer-internal)")
 			helmVals["clustermesh"].(map[string]any)["apiserver"] = map[string]any{
 				"service": map[string]any{
-					"type": corev1.ServiceTypeLoadBalancer,
+					"type": string(corev1.ServiceTypeLoadBalancer),
 					"annotations": map[string]any{
 						"service.beta.kubernetes.io/azure-load-balancer-internal": "true",
 					},
@@ -1027,7 +1027,7 @@ func generateEnableHelmValues(params Parameters, flavor k8s.Flavor) (map[string]
 			log("🔮 Auto-exposing service within AWS VPC (service.beta.kubernetes.io/aws-load-balancer-scheme: internal")
 			helmVals["clustermesh"].(map[string]any)["apiserver"] = map[string]any{
 				"service": map[string]any{
-					"type": corev1.ServiceTypeLoadBalancer,
+					"type": string(corev1.ServiceTypeLoadBalancer),
 					"annotations": map[string]any{
 						"service.beta.kubernetes.io/aws-load-balancer-scheme": "internal",
 					},