fe5e04ad31a78a9b9a99d35edf07ab7a9678b851
Deepesh Pathak <deepeshpathak09@gmail.com>
2026-07-27 14:11:02 -0700

    k8s: add k8s statedb reflector option to wait for dependencies
    [ upstream commit 2a587dc5f4ead5255b24dc93ba6358eedd302eef ]
    
    This commit adds a new optional configuration option to statedb k8s
    reflector that blocks resource event processing until provided
    dependencies are ready. This will be used in future commits to wait for
    port allocator restore before starting CEC event processing.
    
    Signed-off-by: Deepesh Pathak <deepeshpathak09@gmail.com>

--- pkg/k8s/statedb.go
+++ pkg/k8s/statedb.go
@@ -296,6 +296,10 @@ type ReflectorConfig[Obj any] struct {
 	// ClearTableOnStop if true will cause all inserted objects to be deleted (using QueryAll)
 	// when the reflector is stopped.
 	ClearTableOnStop bool
+
+	// InitWait is an optional channel reflector waits for during setup phase.
+	// This blocks starting the processing loop to wait on any dependencies to be initialized.
+	InitWait <-chan struct{}
 }
 
 // JobName returns the name of the background reflector job.
@@ -388,6 +392,15 @@ func (r *k8sReflector[Obj]) run(ctx context.Context, health cell.Health) error {
 		health.OK("Waiting for CRD registration")
 		if _, err := r.CRDSync.Await(ctx); err != nil {
 			return err
+		}
+	}
+
+	if r.InitWait != nil {
+		health.OK("Waiting for reflector initializer")
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-r.InitWait:
 		}
 	}
 
@@ -571,6 +584,7 @@ func (r *k8sReflector[Obj]) run(ctx context.Context, health cell.Health) error {
 		health.OK(fmt.Sprintf("%d upserted, %d deleted, %d total objects", numUpserted, numDeleted, numTotal))
 	}
 
+	health.OK("Starting resource events stream observer")
 	errs := make(chan error)
 	src.Observe(
 		ctx,
