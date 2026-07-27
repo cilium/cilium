fd909f67d0769462975a670be6f1e138f005b244
Alex Melhem <alex.melhem@datadoghq.com>
2026-07-27 18:25:45 +0200

    cli: Autodetect EKS flavor from IPv6 server URL
    For IPv6 EKS clusters, the format of the server URL is different than that of IPv4 clusters. For IPv6, the URL is suffixed with eks-cluster.<region>.api.aws (See https://docs.aws.amazon.com/eks/latest/userguide/cluster-endpoint.html#cluster-endpoint-ipv6).
    
    This change adds detection of this pattern and adds unit tests to makes sure detection of EKS flavor works as expected.
    
    Signed-off-by: Alex Melhem <alex.melhem@datadoghq.com>

--- cilium-cli/k8s/client_test.go
+++ cilium-cli/k8s/client_test.go
@@ -60,3 +60,66 @@ func TestAutodetectFlavorGKE(t *testing.T) {
 		assert.Equal(t, KindGKE, flavor.Kind, "Should detect GKE via node label")
 	})
 }
+
+func TestAutodetectFlavorEKS(t *testing.T) {
+	t.Run("by-cluster-name", func(t *testing.T) {
+		c := &Client{
+			Clientset: fake.NewSimpleClientset(),
+			RawConfig: clientcmdapi.Config{
+				Contexts: map[string]*clientcmdapi.Context{
+					"foo.us-west-2.eksctl.io": {
+						Cluster: "foo.us-west-2.eksctl.io",
+					},
+				},
+				CurrentContext: "foo.us-west-2.eksctl.io",
+			},
+			contextName: "foo.us-west-2.eksctl.io",
+		}
+		flavor := c.AutodetectFlavor(context.Background())
+		assert.Equal(t, KindEKS, flavor.Kind)
+	})
+
+	t.Run("by-cluster-server", func(t *testing.T) {
+		c := &Client{
+			Clientset: fake.NewSimpleClientset(),
+			RawConfig: clientcmdapi.Config{
+				Contexts: map[string]*clientcmdapi.Context{
+					"my-cluster": {
+						Cluster: "my-cluster",
+					},
+				},
+				Clusters: map[string]*clientcmdapi.Cluster{
+					"my-cluster": {
+						Server: "https://ABCDEF1234567890.gr7.us-west-2.eks.amazonaws.com",
+					},
+				},
+				CurrentContext: "my-cluster",
+			},
+			contextName: "my-cluster",
+		}
+		flavor := c.AutodetectFlavor(context.Background())
+		assert.Equal(t, KindEKS, flavor.Kind, "Should detect EKS via cluster server URL")
+	})
+
+	t.Run("by-cluster-server-ipv6", func(t *testing.T) {
+		c := &Client{
+			Clientset: fake.NewSimpleClientset(),
+			RawConfig: clientcmdapi.Config{
+				Contexts: map[string]*clientcmdapi.Context{
+					"my-cluster": {
+						Cluster: "my-cluster",
+					},
+				},
+				Clusters: map[string]*clientcmdapi.Cluster{
+					"my-cluster": {
+						Server: "https://ABCDEF1234567890.gr7.eks-cluster.us-west-2.api.aws",
+					},
+				},
+				CurrentContext: "my-cluster",
+			},
+			contextName: "my-cluster",
+		}
+		flavor := c.AutodetectFlavor(context.Background())
+		assert.Equal(t, KindEKS, flavor.Kind, "Should detect EKS via IPv6 cluster server URL")
+	})
+}
