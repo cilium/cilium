f8a19620370d53a53e953e761fa465d86e24797b
Deepesh Pathak <deepeshpathak09@gmail.com>
2026-07-27 15:28:41 -0700

    cli: extend L7 traffic disruption tests to include egress rules in CNP
    Extend L7 traffic disruption tests to validate egress L7 policy on
    client endpoints.
    
    Signed-off-by: Deepesh Pathak <deepeshpathak09@gmail.com>

--- cilium-cli/connectivity/check/deployment.go
+++ cilium-cli/connectivity/check/deployment.go
@@ -553,7 +553,7 @@ func newConnDisruptCNPForL7Traffic(ns string) *ciliumv2.CiliumNetworkPolicy {
 		},
 	}
 
-	ports := []policyapi.PortRule{{
+	httpPortRule := policyapi.PortRule{
 		Ports: []policyapi.PortProtocol{{
 			Protocol: policyapi.ProtoTCP,
 			Port:     "8000",
@@ -564,7 +564,15 @@ func newConnDisruptCNPForL7Traffic(ns string) *ciliumv2.CiliumNetworkPolicy {
 				Method: "GET",
 			}},
 		},
-	}}
+	}
+
+	// Required in egress rule for DNS lookups(eg. for service names).
+	dnsPortRule := policyapi.PortRule{
+		Ports: []policyapi.PortProtocol{
+			{Protocol: policyapi.ProtoUDP, Port: "53"},
+			{Protocol: policyapi.ProtoTCP, Port: "53"},
+		},
+	}
 
 	return &ciliumv2.CiliumNetworkPolicy{
 		TypeMeta: metav1.TypeMeta{
@@ -580,7 +588,15 @@ func newConnDisruptCNPForL7Traffic(ns string) *ciliumv2.CiliumNetworkPolicy {
 						policyapi.EntityCluster,
 					},
 				},
-				ToPorts: ports,
+				ToPorts: []policyapi.PortRule{httpPortRule},
+			}},
+			Egress: []policyapi.EgressRule{{
+				EgressCommonRule: policyapi.EgressCommonRule{
+					ToEntities: policyapi.EntitySlice{
+						policyapi.EntityCluster,
+					},
+				},
+				ToPorts: []policyapi.PortRule{httpPortRule, dnsPortRule},
 			}},
 		},
 	}
