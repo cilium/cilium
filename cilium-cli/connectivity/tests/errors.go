c1ba125811baf8f50ae7c625f285d17d0272cdf1
Alex Melhem <alex.melhem@datadoghq.com>
2026-07-27 10:38:37 +0200

    cli: ignore ENI IPv6 beta warning
    The ENI IPv6 implementation is in beta for the v1.20 release of Cilium. This adds the beta warning log to the list of expected warnings in the CLI. Otherwise, the check no error logs test fails when running connectivity tests with ENI+IPv6 enabled.
    
    Signed-off-by: Alex Melhem <alex.melhem@datadoghq.com>

--- cilium-cli/connectivity/tests/errors.go
+++ cilium-cli/connectivity/tests/errors.go
@@ -119,7 +119,7 @@ func NoErrorsInLogs(ciliumVersion semver.Version, checkLevels []string, extraExc
 		envoyExternalTargetTLSWarning, envoyExternalOtherTargetTLSWarning,
 		hubbleUIEnvVarFallback, k8sClientNetworkStatusError, bgpAlphaResourceDeprecation, ccgAlphaResourceDeprecation,
 		k8sEndpointDeprecatedWarn, proxylibDeprecatedWarn, certloaderInitialLoadWarn, localKeyAlreadyAllocated,
-		getSecurityGroupsForVpcUnauthorized}
+		getSecurityGroupsForVpcUnauthorized, eniIPv6BetaWarn}
 
 	warningThresholdExceptions := thresholdExceptions{
 		// Benign for one node at ENI capacity, a real IP-starvation signal for
@@ -558,6 +558,7 @@ const (
 	hubbleUIEnvVarFallback           stringMatcher = "using fallback value for env var"                                      // cf. https://github.com/cilium/hubble-ui/pull/940
 	k8sClientNetworkStatusError      stringMatcher = "Network status error received, restarting client connections"          // cf. https://github.com/cilium/cilium/issues/37712
 	localKeyAlreadyAllocated         stringMatcher = "local key already allocated with different value"                      // cf. https://github.com/cilium/cilium/issues/41280
+	eniIPv6BetaWarn                  stringMatcher = "(ipam.mode=eni, ipv6.enabled=true) is a beta feature"                  // Expected when running with IPv6 enabled in ENI IPAM mode.
 
 	k8sEndpointDeprecatedWarn stringMatcher = "v1 Endpoints is deprecated in v1.33+; use discovery.k8s.io/v1 EndpointSlice" // cf. https://github.com/cilium/cilium/issues/39105
 	proxylibDeprecatedWarn    stringMatcher = "The support for Envoy Go Extensions (proxylib) has been deprecated"          // cf. https://github.com/cilium/cilium/issues/38224
