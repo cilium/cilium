4502156f2ef9767742a9efa194ea2bb8915dfdd5
Deepesh Pathak <deepeshpathak09@gmail.com>
2026-07-27 13:59:03 -0700

    endpoint/restore: skip validation for ingress endpoint
    Currently, Ingress endpoint restoration fails in the "link check"
    because the Ingress endpoint doesn't have an `ifName` configured.
    
    ```
    msg="Unable to restore endpoint, ignoring" module=agent.controlplane.daemon.endpoint-restore endpointID=791 ciliumEndpointName=/ error="interface  could not be found"
    ```
    
    Even though the Ingress endpoint is re-created shortly after this with
    the same IP(s), this leads us with a short amount of time where the endpoint
    is deleted - which can lead to issues.
    
    Therefore, with this commit, the validation is skipped for the ingress endpoint
    - similar to what we are already doing for the health endpoint.
    
    Reported-by: Sebastian Wicki <sebastian@isovalent.com>
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>
    Signed-off-by: Deepesh Pathak <deepeshpathak09@gmail.com>

--- cilium-cli/connectivity/tests/errors.go
+++ cilium-cli/connectivity/tests/errors.go
@@ -108,7 +108,7 @@ func NoErrorsInLogs(ciliumVersion semver.Version, checkLevels []string, extraExc
 	envoyExternalOtherTargetTLSWarning := regexMatcher{regexp.MustCompile(fmt.Sprintf(envoyTLSWarningTemplate, externalOtherTarget))}
 	warningLogExceptions := []logMatcher{cantEnableJIT, podCIDRUnavailable,
 		unableGetNode, sessionAffinitySocketLB, objectHasBeenModified, noBackendResponse,
-		legacyBGPFeature, etcdTimeout, endpointRestoreFailed, unableRestoreRouterIP,
+		legacyBGPFeature, etcdTimeout, unableRestoreRouterIP,
 		routerIPReallocated, cantFindIdentityInCache, keyAllocFailedFoundMaster,
 		cantRecreateMasterKey, cantUpdateCRDIdentity, cantDeleteFromPolicyMap, failedToListCRDs,
 		hubbleQueueFull, reflectPanic, svcNotFound, gobgpv3Warnings, gobgpNotification, gobgpNoMatchingWithdrawPath,
@@ -134,6 +134,10 @@ func NoErrorsInLogs(ciliumVersion semver.Version, checkLevels []string, extraExc
 
 	if ciliumVersion.LT(semver.MustParse("1.19.0")) {
 		warningLogExceptions = append(warningLogExceptions, kvstoreNodesGCWarn)
+	}
+
+	if ciliumVersion.LT(semver.MustParse("1.21.0")) {
+		warningLogExceptions = append(warningLogExceptions, endpointRestoreFailed)
 	}
 
 	for _, exception := range extraExceptions {
