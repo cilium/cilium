cce3c7cfe8f8622f09f044b5f7c3fc2ab06e3832
Simone Magnani <simone.magnani@isovalent.com>
2026-07-30 22:32:06 +0200

    bpf: hostfw: tolerate unknown CT protocols and rely on egress policies
    [ upstream commit 0a74d74987 ]
    
    Prior to this commit, the HostFW egress codepath was dropping packets
    either (a) due to an unknown CT protocol leading to a CT lookup failure,
    or (b) due to a CT lookup success but policies in place.
    
    While the latter is expected, the former is a bit controversial, as it
    leads to dropping packets that could've been allowed by egress policies,
    such as a default allow-all policy. The missing conntrack support for
    other L4 protocols should not prevent us from evaluating egress policies.
    We definitively cannot conntrack the packet, so in that case ingress
    policies would not be skipped either for the reply traffic, but at the
    same time we should honour the policies in place.
    
    The missing conntrack support for other L4 protocols was added in v1.19
    behind the `--enable-extended-ip-protocols` agent flag, to allow conntrack
    protocols such as IGMP or VRRP. In the same patch, we added support to
    extend HostFW policies to these protocols that do not have a L4 port.
    While being more granular, I believe even w/o the agent flag set we should
    stick to the policy evaluation.
    
    Signed-off-by: Simone Magnani <simone.magnani@isovalent.com>

--- bpf/tests/hostfw_igmp.h
+++ bpf/tests/hostfw_igmp.h
@@ -85,6 +85,8 @@ int hostfw_igmp_egress_check(const struct __ctx_buff *ctx)
 
 	status_code = data;
 
+	assert(*status_code == CTX_ACT_OK);
+
 	/* Check for egress CT entry */
 	struct ipv4_ct_tuple tuple = {
 		.daddr   = NODE_IP,
@@ -97,15 +99,11 @@ int hostfw_igmp_egress_check(const struct __ctx_buff *ctx)
 	struct ct_entry *ct_entry = map_lookup_elem(get_ct_map4(&tuple), &tuple);
 
 #ifdef TEST_EXTENDED_PROTOCOLS
-	assert(*status_code == CTX_ACT_OK);
-
 	if (!ct_entry)
 		test_fatal("no CT entry found");
 
 	assert(ct_entry->packets == 1);
 #else
-	assert(*status_code == CTX_ACT_DROP);
-
 	if (ct_entry)
 		test_fatal("CT entry found");
 
@@ -113,7 +111,7 @@ int hostfw_igmp_egress_check(const struct __ctx_buff *ctx)
 		.reason = (__u8)-DROP_CT_UNKNOWN_PROTO,
 		.dir = METRIC_EGRESS,
 	};
-	__u64 count = 1;
+	__u64 count = 0;
 
 	assert_metrics_count(key, count);
 #endif
@@ -152,6 +150,8 @@ int hostfw_igmp_ingress_pktgen(struct __ctx_buff *ctx)
 SETUP("tc", "hostfw_igmp_2_ingress")
 int hostfw_igmp_ingress_setup(struct __ctx_buff *ctx)
 {
+	policy_add_ingress_allow_all_entry();
+
 	return netdev_receive_packet(ctx);
 }
 
@@ -203,6 +203,8 @@ int hostfw_igmp_ingress_check(const struct __ctx_buff *ctx)
 
 	assert_metrics_count(key, count);
 #endif
+
+	policy_delete_ingress_all_entry();
 
 	test_finish();
 }
@@ -262,6 +264,8 @@ int hostfw_igmp_egress_policy_check(const struct __ctx_buff *ctx)
 
 	status_code = data;
 
+	assert(*status_code == CTX_ACT_OK);
+
 	/* Check for egress CT entry */
 	struct ipv4_ct_tuple tuple = {
 		.daddr   = NODE_IP2,
@@ -274,13 +278,9 @@ int hostfw_igmp_egress_policy_check(const struct __ctx_buff *ctx)
 	struct ct_entry *ct_entry = map_lookup_elem(get_ct_map4(&tuple), &tuple);
 
 #ifdef TEST_EXTENDED_PROTOCOLS
-	assert(*status_code == CTX_ACT_OK);
-
 	if (!ct_entry)
 		test_fatal("no CT entry found");
 #else
-	assert(*status_code == CTX_ACT_DROP);
-
 	if (ct_entry)
 		test_fatal("CT entry found");
 
@@ -288,7 +288,7 @@ int hostfw_igmp_egress_policy_check(const struct __ctx_buff *ctx)
 		.reason = (__u8)-DROP_CT_UNKNOWN_PROTO,
 		.dir = METRIC_EGRESS,
 	};
-	__u64 count = 2;
+	__u64 count = 0;
 
 	assert_metrics_count(key, count);
 #endif
@@ -328,6 +328,7 @@ SETUP("tc", "hostfw_igmp_4_ingress_policy")
 int hostfw_igmp_ingress_policy_setup(struct __ctx_buff *ctx)
 {
 	policy_add_ingress_deny_l4_entry(IPPROTO_IGMP, 0, 0);
+	policy_add_ingress_allow_all_entry();
 
 	return netdev_receive_packet(ctx);
 }
@@ -363,6 +364,8 @@ int hostfw_igmp_ingress_policy_check(const struct __ctx_buff *ctx)
 #endif
 
 	assert_metrics_count(key, count);
+
+	policy_delete_ingress_all_entry();
 
 	test_finish();
 }
