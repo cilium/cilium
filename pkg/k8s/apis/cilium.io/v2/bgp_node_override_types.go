cilium/cilium

commit ce024450db0aea444fd448bba8318cae1ef427b4

k8s: introducing BGPv2 CRDs
Introducing new BGP CRDs to enable more flexible way to configure BGP in
Cilium cluster.

- CiliumBGPClusterConfig
- CiliumBGPAdvertisement
- CiliumBGPPeerConfig
- CiliumBGPNodeConfig
- CiliumBGPNodeOverrideConfig

Pleasa refer to CFP https://isogo.to/bgpv2-crds for more details.

Signed-off-by: harsimran pabla <hpabla@isovalent.com>

pkg/k8s/apis/cilium.io/v2alpha1/bgp_node_override_types.go
@@ -1,0 +1,104 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package v2alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:categories={cilium,ciliumbgp},singular="ciliumbgpnodeconfigoverride",path="ciliumbgpnodeconfigoverrides",scope="Cluster",shortName={cbgpnodeoverride}
+// +kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",name="Age",type=date
+// +kubebuilder:storageversion
+
+// CiliumBGPNodeConfigOverride is used to overrides some of the BGP configurations which are node local.
+// Users can user this resource to override auto-generated BGP settings for the node.
+type CiliumBGPNodeConfigOverride struct {
+	// +deepequal-gen=false
+	metav1.TypeMeta `json:",inline"`
+	// +deepequal-gen=false
+	metav1.ObjectMeta `json:"metadata"`
+
+	// Spec is the specification of the desired behavior of the CiliumBGPNodeConfigOverride.
+	Spec CiliumBGPNodeConfigOverrideSpec `json:"spec"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:openapi-gen=false
+// +deepequal-gen=false
+
+// CiliumBGPNodeConfigOverrideList is a list of CiliumBGPNodeConfigOverride objects.
+type CiliumBGPNodeConfigOverrideList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	// Items is a list of CiliumBGPNodeConfigOverride.
+	Items []CiliumBGPNodeConfigOverride `json:"items"`
+}
+
+type CiliumBGPNodeConfigOverrideSpec struct {
+	// NodeRef is the name of the node for which the BGP configuration is overridden.
+	//
+	// +kubebuilder:validation:Required
+	NodeRef string `json:"nodeRef"`
+
+	// BGPInstances is a list of BGP instances to override.
+	//
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MinItems=1
+	// +listType=map
+	// +listMapKey=name
+	BGPInstances []CiliumBGPNodeConfigInstanceOverride `json:"bgpInstances"`
+}
+
+// CiliumBGPNodeConfigInstanceOverride defines configuration options which can be overridden for a specific BGP instance.
+type CiliumBGPNodeConfigInstanceOverride struct {
+	// Name is the name of the BGP instance for which the configuration is overridden.
+	//
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=255
+	Name string `json:"name"`
+
+	// RouterID is BGP router id to use for this instance. It must be unique across all BGP instances.
+	//
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Format=ipv4
+	RouterID *string `json:"routerID,omitempty"`
+
+	// LocalPort is port to use for this BGP instance.
+	//
+	// +kubebuilder:validation:Optional
+	LocalPort *int32 `json:"localPort,omitempty"`
+
+	// Peers is a list of peer configurations to override.
+	//
+	// +kubebuilder:validation:Optional
+	// +listType=map
+	// +listMapKey=name
+	Peers []CiliumBGPNodeConfigPeerOverride `json:"peers,omitempty"`
+}
+
+// CiliumBGPNodeConfigPeerOverride defines configuration options which can be overridden for a specific peer.
+type CiliumBGPNodeConfigPeerOverride struct {
+	// Name is the name of the peer for which the configuration is overridden.
+	//
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=255
+	Name string `json:"name"`
+
+	// LocalAddress is the IP address to use for connecting to this peer.
+	//
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Pattern=`((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))`
+	LocalAddress *string `json:"localAddress,omitempty"`
+
+	// LocalPort is source port to use for connecting to this peer.
+	//
+	// +kubebuilder:validation:Optional
+	LocalPort *int32 `json:"localPort,omitempty"`
+}

pkg/k8s/apis/cilium.io/client/crds/v2alpha1/ciliumbgpnodeconfigoverrides.yaml
@@ -1,0 +1,128 @@
+---
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  annotations:
+    controller-gen.kubebuilder.io/version: v0.13.0
+  creationTimestamp: null
+  name: ciliumbgpnodeconfigoverrides.cilium.io
+spec:
+  group: cilium.io
+  names:
+    categories:
+    - cilium
+    - ciliumbgp
+    kind: CiliumBGPNodeConfigOverride
+    listKind: CiliumBGPNodeConfigOverrideList
+    plural: ciliumbgpnodeconfigoverrides
+    shortNames:
+    - cbgpnodeoverride
+    singular: ciliumbgpnodeconfigoverride
+  scope: Cluster
+  versions:
+  - additionalPrinterColumns:
+    - jsonPath: .metadata.creationTimestamp
+      name: Age
+      type: date
+    name: v2alpha1
+    schema:
+      openAPIV3Schema:
+        description: CiliumBGPNodeConfigOverride is used to overrides some of the
+          BGP configurations which are node local. Users can user this resource to
+          override auto-generated BGP settings for the node.
+        properties:
+          apiVersion:
+            description: 'APIVersion defines the versioned schema of this representation
+              of an object. Servers should convert recognized schemas to the latest
+              internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources'
+            type: string
+          kind:
+            description: 'Kind is a string value representing the REST resource this
+              object represents. Servers may infer this from the endpoint the client
+              submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds'
+            type: string
+          metadata:
+            type: object
+          spec:
+            description: Spec is the specification of the desired behavior of the
+              CiliumBGPNodeConfigOverride.
+            properties:
+              bgpInstances:
+                description: BGPInstances is a list of BGP instances to override.
+                items:
+                  description: CiliumBGPNodeConfigInstanceOverride defines configuration
+                    options which can be overridden for a specific BGP instance.
+                  properties:
+                    localPort:
+                      description: LocalPort is port to use for this BGP instance.
+                      format: int32
+                      type: integer
+                    name:
+                      description: Name is the name of the BGP instance for which
+                        the configuration is overridden.
+                      maxLength: 255
+                      minLength: 1
+                      type: string
+                    peers:
+                      description: Peers is a list of peer configurations to override.
+                      items:
+                        description: CiliumBGPNodeConfigPeerOverride defines configuration
+                          options which can be overridden for a specific peer.
+                        properties:
+                          localAddress:
+                            description: LocalAddress is the IP address to use for
+                              connecting to this peer.
+                            pattern: ((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))
+                            type: string
+                          localPort:
+                            description: LocalPort is source port to use for connecting
+                              to this peer.
+                            format: int32
+                            type: integer
+                          name:
+                            description: Name is the name of the peer for which the
+                              configuration is overridden.
+                            maxLength: 255
+                            minLength: 1
+                            type: string
+                        required:
+                        - name
+                        type: object
+                      type: array
+                      x-kubernetes-list-map-keys:
+                      - name
+                      x-kubernetes-list-type: map
+                    routerID:
+                      description: RouterID is BGP router id to use for this instance.
+                        It must be unique across all BGP instances.
+                      format: ipv4
+                      type: string
+                  required:
+                  - name
+                  type: object
+                minItems: 1
+                type: array
+                x-kubernetes-list-map-keys:
+                - name
+                x-kubernetes-list-type: map
+              nodeRef:
+                description: NodeRef is the name of the node for which the BGP configuration
+                  is overridden.
+                type: string
+            required:
+            - bgpInstances
+            - nodeRef
+            type: object
+        required:
+        - metadata
+        - spec
+        type: object
+    served: true
+    storage: true
+    subresources: {}
+status:
+  acceptedNames:
+    kind: ""
+    plural: ""
+  conditions: []
+  storedVersions: []

Makefile
@@ -233,6 +233,11 @@ CRDS_CILIUM_V2 := ciliumnetworkpolicies \
                   ciliumclusterwideenvoyconfigs
 CRDS_CILIUM_V2ALPHA1 := ciliumendpointslices \
                         ciliumbgppeeringpolicies \
+                        ciliumbgpclusterconfigs \
+                        ciliumbgppeerconfigs \
+                        ciliumbgpadvertisements \
+                        ciliumbgpnodeconfigs \
+                        ciliumbgpnodeconfigoverrides \
                         ciliumloadbalancerippools \
                         ciliumnodeconfigs \
                         ciliumcidrgroups \

install/kubernetes/cilium/Chart.yaml
@@ -111,6 +111,40 @@ annotations:
       description: |
         Cilium BGP Peering Policy instructs Cilium to create specific BGP peering
         configurations.
+    - kind: CiliumBGPClusterConfig
+      version: v2alpha1
+      name: ciliumbgpclusterconfigs.cilium.io
+      displayName: Cilium BGP Cluster Config
+      description: |
+        Cilium BGP Cluster Config instructs Cilium operator to create specific BGP cluster
+        configurations.
+    - kind: CiliumBGPPeerConfig
+      version: v2alpha1
+      name: ciliumbgppeerconfigs.cilium.io
+      displayName: Cilium BGP Peer Config
+      description: |
+        CiliumBGPPeerConfig is a common set of BGP peer configurations. It can be referenced 
+        by multiple peers from CiliumBGPClusterConfig.
+    - kind: CiliumBGPAdvertisement
+      version: v2alpha1
+      name: ciliumbgpadvertisements.cilium.io
+      displayName: Cilium BGP Advertisement
+      description: |
+        CiliumBGPAdvertisement is used to define source of BGP advertisement as well as BGP attributes 
+        to be advertised with those prefixes.
+    - kind: CiliumBGPNodeConfig
+      version: v2alpha1
+      name: ciliumbgpnodeconfigs.cilium.io
+      displayName: Cilium BGP Node Config
+      description: |
+        CiliumBGPNodeConfig is read only node specific BGP configuration. It is constructed by Cilium operator.
+        It will also contain node local BGP state information.
+    - kind: CiliumBGPNodeConfigOverride
+      version: v2alpha1
+      name: ciliumbgpnodeconfigoverrides.cilium.io
+      displayName: Cilium BGP Node Config Override
+      description: |
+        CiliumBGPNodeConfigOverride can be used to override node specific BGP configuration.
     - kind: CiliumLoadBalancerIPPool
       version: v2alpha1
       name: ciliumloadbalancerippools.cilium.io

install/kubernetes/cilium/templates/cilium-agent/clusterrole.yaml
@@ -86,6 +86,9 @@ rules:
   resources:
   - ciliumloadbalancerippools
   - ciliumbgppeeringpolicies
+  - ciliumbgpnodeconfigs
+  - ciliumbgpadvertisements
+  - ciliumbgppeerconfigs
   - ciliumclusterwideenvoyconfigs
   - ciliumclusterwidenetworkpolicies
   - ciliumegressgatewaypolicies
@@ -141,6 +144,7 @@ rules:
   - ciliumendpoints/status
   - ciliumendpoints
   - ciliuml2announcementpolicies/status
+  - ciliumbgpnodeconfigs/status
   verbs:
   - patch
 {{- end }}

install/kubernetes/cilium/templates/cilium-operator/clusterrole.yaml
@@ -161,6 +161,9 @@ rules:
   resources:
   - ciliumendpointslices
   - ciliumenvoyconfigs
+  - ciliumbgppeerconfigs
+  - ciliumbgpadvertisements
+  - ciliumbgpnodeconfigs
   verbs:
   - create
   - update
@@ -187,6 +190,11 @@ rules:
   resourceNames:
   - ciliumloadbalancerippools.cilium.io
   - ciliumbgppeeringpolicies.cilium.io
+  - ciliumbgpclusterconfigs.cilium.io
+  - ciliumbgppeerconfigs.cilium.io
+  - ciliumbgpadvertisements.cilium.io
+  - ciliumbgpnodeconfigs.cilium.io
+  - ciliumbgpnodeconfigoverrides.cilium.io
   - ciliumclusterwideenvoyconfigs.cilium.io
   - ciliumclusterwidenetworkpolicies.cilium.io
   - ciliumegressgatewaypolicies.cilium.io
@@ -207,6 +215,8 @@ rules:
   resources:
   - ciliumloadbalancerippools
   - ciliumpodippools
+  - ciliumbgpclusterconfigs
+  - ciliumbgpnodeconfigoverrides
   verbs:
   - get
   - list

install/kubernetes/cilium/templates/cilium-preflight/clusterrole.yaml
@@ -86,6 +86,9 @@ rules:
   resources:
   - ciliumloadbalancerippools
   - ciliumbgppeeringpolicies
+  - ciliumbgpnodeconfigs
+  - ciliumbgpadvertisements
+  - ciliumbgppeerconfigs
   - ciliumclusterwideenvoyconfigs
   - ciliumclusterwidenetworkpolicies
   - ciliumegressgatewaypolicies
@@ -141,6 +144,7 @@ rules:
   - ciliumendpoints/status
   - ciliumendpoints
   - ciliuml2announcementpolicies/status
+  - ciliumbgpnodeconfigs/status
   verbs:
   - patch
 {{- end }}

commit f2b4b42f37fa8bf4f896d743a3710c45a1057fd7

bgp: mark CiliumBGPNodeConfigOverride peers localPort as having no effect
The peer level localPort on CiliumBGPNodeConfigOverride is documented as
the source port to use for connecting to a peer, is optional, carries no
deprecation marker and is surfaced by kubectl explain. Nothing consumes
it: the operator's peer override loop copies only LocalAddress, and
CiliumBGPNodePeer has no LocalPort field for the value to land in, so an
applied value is persisted, echoed back and dropped. A user pinning the
source port for an upstream firewall rule gets an ephemeral one.

Wiring it would resurrect a knob that was deliberately retired. The
equivalent transport localPort on CiliumBGPPeerConfig was deprecated as
unnecessary, on the grounds that the source port should be an ephemeral
port picked by the kernel, and was dropped when the CRDs were promoted to
v2. This field is the leftover of that same setting on the override CRD.

Mark it deprecated with the same wording used for its sibling, and say in
the description that it has no effect, so kubectl explain reports it.
Removing the field outright would break a served API version.

Fixes: ce024450db ("k8s: introducing BGPv2 CRDs")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/apis/cilium.io/client/crds/v2/ciliumbgpnodeconfigoverrides.yaml
@@ -87,8 +87,10 @@ spec:
                             pattern: ((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))
                             type: string
                           localPort:
-                            description: LocalPort is source port to use for connecting
-                              to this peer.
+                            description: |-
+                              Deprecated
+                              LocalPort has no effect. The source port is an ephemeral port picked by
+                              the kernel when establishing the TCP connection.
                             format: int32
                             type: integer
                           name:
@@ -196,8 +198,10 @@ spec:
                             pattern: ((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))
                             type: string
                           localPort:
-                            description: LocalPort is source port to use for connecting
-                              to this peer.
+                            description: |-
+                              Deprecated
+                              LocalPort has no effect. The source port is an ephemeral port picked by
+                              the kernel when establishing the TCP connection.
                             format: int32
                             type: integer
                           name:

pkg/k8s/apis/cilium.io/v2/bgp_node_override_types.go
@@ -104,7 +104,9 @@ type CiliumBGPNodeConfigPeerOverride struct {
 	// +kubebuilder:validation:Pattern=`((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))`
 	LocalAddress *string `json:"localAddress,omitempty"`
 
-	// LocalPort is source port to use for connecting to this peer.
+	// Deprecated
+	// LocalPort has no effect. The source port is an ephemeral port picked by
+	// the kernel when establishing the TCP connection.
 	//
 	// +kubebuilder:validation:Optional
 	LocalPort *int32 `json:"localPort,omitempty"`

pkg/k8s/apis/cilium.io/v2alpha1/bgp_node_override_types.go
@@ -104,7 +104,9 @@ type CiliumBGPNodeConfigPeerOverride struct {
 	// +kubebuilder:validation:Pattern=`((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))`
 	LocalAddress *string `json:"localAddress,omitempty"`
 
-	// LocalPort is source port to use for connecting to this peer.
+	// Deprecated
+	// LocalPort has no effect. The source port is an ephemeral port picked by
+	// the kernel when establishing the TCP connection.
 	//
 	// +kubebuilder:validation:Optional
 	LocalPort *int32 `json:"localPort,omitempty"`