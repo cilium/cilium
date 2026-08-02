102d3acdee366b65cbf846926942842380a6275e
Julian Wiedmann <jwi@isovalent.com>
2026-08-02 07:08:39 +0300

    bpf: nodeport: scope SVC-lookup related code together
    Improve readability, and potentially also help with stack allocation.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/nodeport.h
+++ bpf/lib/nodeport.h
@@ -1566,9 +1566,7 @@ static __always_inline int nodeport_lb6(struct __ctx_buff *ctx,
 	bool is_svc_proto __maybe_unused = true;
 	int ret, l3_off = ETH_HLEN, l4_off;
 	struct ipv6_ct_tuple tuple __align_stack_8 = {};
-	const struct lb6_service *svc;
 	fraginfo_t fraginfo = 0;
-	struct lb6_key key = {};
 
 	tuple.nexthdr = ip6->nexthdr;
 	ret = ipv6_hdrlen_with_fraginfo(ctx, &tuple.nexthdr, &fraginfo);
@@ -1590,13 +1588,18 @@ static __always_inline int nodeport_lb6(struct __ctx_buff *ctx,
 		return ret;
 	}
 
-	lb6_fill_key(&key, &tuple);
+	{
+		const struct lb6_service *svc;
+		struct lb6_key key = {};
 
-	svc = lb6_lookup_service(&key, false);
-	if (svc)
-		return nodeport_svc_lb6(ctx, &tuple, svc, &key, ip6, l3_off,
-					fraginfo, l4_off, src_sec_identity,
-					punt_to_stack, ext_err);
+		lb6_fill_key(&key, &tuple);
+
+		svc = lb6_lookup_service(&key, false);
+		if (svc)
+			return nodeport_svc_lb6(ctx, &tuple, svc, &key, ip6, l3_off,
+						fraginfo, l4_off, src_sec_identity,
+						punt_to_stack, ext_err);
+	}
 
 skip_service_lookup:
 #ifdef ENABLE_NAT_46X64_GATEWAY
@@ -2934,8 +2937,6 @@ static __always_inline int nodeport_lb4(struct __ctx_buff *ctx,
 	fraginfo_t fraginfo;
 	struct ipv4_ct_tuple tuple = {};
 	bool is_svc_proto = true;
-	const struct lb4_service *svc;
-	struct lb4_key key = {};
 	int l3_off = ETH_HLEN;
 	int ret, l4_off;
 
@@ -2955,13 +2956,18 @@ static __always_inline int nodeport_lb4(struct __ctx_buff *ctx,
 		return ret;
 	}
 
-	lb4_fill_key(&key, &tuple);
+	{
+		const struct lb4_service *svc;
+		struct lb4_key key = {};
 
-	svc = lb4_lookup_service(&key, false);
-	if (svc)
-		return nodeport_svc_lb4(ctx, &tuple, svc, &key, ip4, l3_off,
-					fraginfo, l4_off, src_sec_identity,
-					punt_to_stack, ext_err);
+		lb4_fill_key(&key, &tuple);
+
+		svc = lb4_lookup_service(&key, false);
+		if (svc)
+			return nodeport_svc_lb4(ctx, &tuple, svc, &key, ip4, l3_off,
+						fraginfo, l4_off, src_sec_identity,
+						punt_to_stack, ext_err);
+	}
 
 skip_service_lookup:
 #ifdef ENABLE_NAT_46X64_GATEWAY
