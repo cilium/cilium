c2d603d9dd8b7df56b298a29d7cf9924a9630110
Andrew Sauber <andrew.sauber@isovalent.com>
2026-08-03 14:56:21 +0900

    gateway-api: remove pre-filtered namespaces concept
    Move all listener-related namespace logic into the building of
    ListenerWithContext, regardless of listener source (Gateway or
    ListenerSet).
    
    This allowed us to remove the special casing in each of the ingestion
    codepaths that deal with different types of routes (HTTPRoute, TLSRoute,
    TCPRoute etc.)
    
    This also allowed us to remove the fallback construction of
    MergedListeners within the ingestion codepath.
    
    This sets up a future refactor where the related route filtering
    functions are methods of the ListenerWithContext type.
    
    Signed-off-by: Andrew Sauber <andrew.sauber@isovalent.com>

--- operator/pkg/gateway-api/gateway_reconcile.go
+++ operator/pkg/gateway-api/gateway_reconcile.go
@@ -357,7 +357,6 @@ func (r *gatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		GRPCRoutes:          grpcRoutes,
 		TCPRoutes:           tcpRoutes,
 		UDPRoutes:           udpRoutes,
-		Namespaces:          namespaces,
 		Services:            servicesList.Items,
 		ServiceImports:      serviceImportsList.Items,
 		ReferenceGrants:     grants.Items,
@@ -814,9 +813,10 @@ func (r *gatewayReconciler) mergeListeners(
 	var merged []ingestion.ListenerWithContext
 	for _, listener := range gw.Spec.Listeners {
 		merged = append(merged, ingestion.ListenerWithContext{
-			Listener:         listener,
-			Source:           gwSource,
-			SourceGeneration: gw.Generation,
+			Listener:          listener,
+			Source:            gwSource,
+			SourceGeneration:  gw.Generation,
+			AllowedNamespaces: resolveAllowedNamespaces(ctx, r.Client, gw.GetNamespace(), listener, scopedLog),
 		})
 	}
 
