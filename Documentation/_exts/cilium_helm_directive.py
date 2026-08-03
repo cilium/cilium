cilium/cilium

commit 917034aaee5357c469209fa0c1fd1ebf7c0ba8a5

docs: Remove not-stable installation instructions
Developers don't use these, and they only cause confusion for users who
stumble on the '/latest' docs without realizing they're focused at
developers. Remove these special cases for installing the development
version directly from source. Developers can find the relevant
instructions under the development section of the docs.

Signed-off-by: Joe Stringer <joe@cilium.io>

Documentation/_exts/cilium_helm_directive.py
@@ -22,27 +22,20 @@ from textwrap import dedent
 
 
 RST_TEMPLATE = """\
-.. only:: stable
+.. tabs::
 
-   .. tabs::
+   .. group-tab:: Helm Repository
 
-      .. group-tab:: Helm Repository
-
-         .. parsed-literal::
+      .. parsed-literal::
 
 {helm_repo_cmd}
 
-      .. group-tab:: OCI Registry
+   .. group-tab:: OCI Registry
 
-         .. parsed-literal::
+      .. parsed-literal::
 
 {oci_cmd}
 
-.. only:: not stable
-
-   .. parsed-literal::
-
-{not_stable_cmd}
 """
 
 
@@ -159,16 +152,11 @@ class CiliumHelmInstallDirective(SphinxDirective):
             oci_base,
             opts_list, '            ', post_helm_commands, post_commands
         )
-        not_stable_cmd = self._format_command(
-            helm_repo_base,
-            opts_list, '      ', post_helm_commands, post_commands
-        )
 
         # Generate RST from template
         rst_content = RST_TEMPLATE.format(
             helm_repo_cmd=helm_repo_cmd,
             oci_cmd=oci_cmd,
-            not_stable_cmd=not_stable_cmd,
         )
 
         # Parse and return nodes

Documentation/conf.py
@@ -114,20 +114,15 @@ else:
 branch = os.environ.get('READTHEDOCS_VERSION')
 if not branch or branch == 'latest':
     branch = 'HEAD'
-    archive_name = 'main'
-    chart_release = './cilium'
-    chart_version = "--chart-directory ./install/kubernetes/cilium"
+    chart_version = prev_release
 elif branch == 'stable':
     branch = release
-    archive_name = release
-    chart_version = '--version ' + release
-    chart_release = 'cilium/cilium ' + chart_version
+    chart_version = release
     tags.add('stable')
 else:
-    archive_name = branch
-    chart_version = '--version ' + release
-    chart_release = 'cilium/cilium ' + chart_version
+    chart_version = release
     tags.add('stable')
+chart_release = 'cilium/cilium --version ' + chart_version
 
 if relinfo.prerelease == 'dev':
     # There's no dev snapshot yet. Use the last release.
@@ -138,9 +133,6 @@ else:
 githubusercontent = 'https://raw.githubusercontent.com/cilium/cilium/'
 scm_web = githubusercontent + branch
 github_repo = 'https://github.com/cilium/cilium/'
-archive_filename = archive_name + '.tar.gz'
-archive_link = github_repo + 'archive/' + archive_filename
-archive_name = 'cilium-' + archive_name.strip('v')
 backport_format = github_repo + \
     'pulls?q=is:open+is:pr+-label:backport/author+label:%s/' + current_release
 
@@ -148,9 +140,6 @@ backport_format = github_repo + \
 rst_epilog = """
 .. |SCM_WEB| replace:: \\{s}
 .. |SCM_BRANCH| replace:: \\{b}
-.. |SCM_ARCHIVE_NAME| replace:: \\{a}
-.. |SCM_ARCHIVE_FILENAME| replace:: \\{f}
-.. |SCM_ARCHIVE_LINK| replace:: \\{l}
 .. |CURRENT_RELEASE| replace:: \\{c}
 .. |NEXT_RELEASE| replace:: \\{n}
 .. |CHART_RELEASE| replace:: \\{h}
@@ -162,9 +151,6 @@ rst_epilog = """
 """.format(
     s=scm_web,
     b=branch,
-    a=archive_name,
-    f=archive_filename,
-    l=archive_link,
     c=current_release,
     n=next_release,
     h=chart_release,

commit e28accb869e732e2ab17d3685e9bc25319516013

docs: Fix chart --version flag
Commit 917034aaee53 ("docs: Remove not-stable installation instructions")
modified the chart version variable in the python script to strip the
Helm '--version' flag. Unfortunately most of the docs were relying on this
script injecting the flag in order to properly render install
instructions. Fix it by updating all the relevant docs to explicitly
include the flag so that the variable can be a pure version number.

Fixes: 917034aaee53 ("docs: Remove not-stable installation instructions")
Signed-off-by: Joe Stringer <joe@cilium.io>

Documentation/_exts/cilium_helm_directive.py
@@ -138,10 +138,10 @@ class CiliumHelmInstallDirective(SphinxDirective):
         # For install/upgrade, format is "helm install name |CHART_RELEASE|"
         if command == 'template':
             helm_repo_base = f'helm {command} |CHART_RELEASE|'
-            oci_base = f'helm {command} oci://quay.io/cilium/charts/cilium |CHART_VERSION|'
+            oci_base = f'helm {command} oci://quay.io/cilium/charts/cilium --version |CHART_VERSION|'
         else:
             helm_repo_base = f'helm {command} {name} |CHART_RELEASE|'
-            oci_base = f'helm {command} {name} oci://quay.io/cilium/charts/cilium |CHART_VERSION|'
+            oci_base = f'helm {command} {name} oci://quay.io/cilium/charts/cilium --version |CHART_VERSION|'
 
         # Build formatted commands for each section
         helm_repo_cmd = self._format_command(