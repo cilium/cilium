cilium/cilium

commit de7d7c4c35fdddd881b33e6ffb55cae317f6657d

mispelling fixes detected by codespell.
Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

Documentation/check-build.sh
@@ -72,7 +72,7 @@ describe_redirect_errors() {
 }
 
 build_with_spellchecker() {
-    # The spell checker runs some Git commands to retreive the name of authors
+    # The spell checker runs some Git commands to retrieve the name of authors
     # and consider them as acceptable words.
     #
     # Recent Git versions refuse to work by default if the repository owner is
@@ -170,7 +170,7 @@ run_linter() {
         --ignore-roles "${CONF_PY_ROLES},spelling:ignore,spelling:word" \
         --ignore-substitutions "${CONF_PY_SUBSTITUTIONS}" \
        -r . ../README.rst 2>&1 | \
-       grep -v 'WARNING:rstcheck_core.checker:An `AttributeError` error occured. This is most probably due to a code block directive (code/code-block/sourcecode) without a specified language.'
+       grep -v 'WARNING:rstcheck_core.checker:An `AttributeError` error occurred. This is most probably due to a code block directive (code/code-block/sourcecode) without a specified language.'
 }
 
 read_all_opt=""

pkg/envoy/config/config.go
@@ -66,7 +66,7 @@ func (r ProxyConfig) Flags(flags *pflag.FlagSet) {
 	flags.Bool("use-full-tls-context", false, "If enabled, persist ca.crt keys into the Envoy config even in a terminatingTLS block on an L7 Cilium Policy. This is to enable compatibility with previously buggy behaviour. This flag is deprecated and will be removed in a future release.")
 	flags.Uint32("proxy-xff-num-trusted-hops-ingress", 0, "Number of trusted hops regarding the x-forwarded-for and related HTTP headers for the ingress L7 policy enforcement Envoy listeners.")
 	flags.Uint32("proxy-xff-num-trusted-hops-egress", 0, "Number of trusted hops regarding the x-forwarded-for and related HTTP headers for the egress L7 policy enforcement Envoy listeners.")
-	flags.Duration("envoy-policy-restore-timeout", 3*time.Minute, "Maxiumum time to wait for enpoint policy restoration before starting serving resources to Envoy")
+	flags.Duration("envoy-policy-restore-timeout", 3*time.Minute, "Maximum time to wait for endpoint policy restoration before starting serving resources to Envoy")
 	flags.Int("envoy-http-upstream-linger-timeout", -1, "Time in seconds to block Envoy worker thread while an upstream HTTP connection is closing. "+
 		"If set to 0, the connection is closed immediately (with TCP RST). If set to -1, the connection is closed asynchronously in the background.")
 }

commit 9348a1263e2f5f681c70d708eba3331e48f4042c

docs: Fix spurious AttributeError reports
Commit de7d7c4c35fd ("mispelling fixes detected by codespell.")
attempted to fix spelling errors across the tree, but it unintentionally
broke the AttributeError filtering in the docs build checks.
It's entirely possible another well-meaning contributor fixes the
spelling upstream, so let's just replace the word with a general string
match in the middle of the message so we can account for both spellings
and also avoid having a misspelled word in the codebase.

Fixes: de7d7c4c35fd ("mispelling fixes detected by codespell.")
Signed-off-by: Joe Stringer <joe@cilium.io>

Documentation/check-build.sh
@@ -170,7 +170,7 @@ run_linter() {
         --ignore-roles "${CONF_PY_ROLES},spelling:ignore,spelling:word" \
         --ignore-substitutions "${CONF_PY_SUBSTITUTIONS}" \
        -r . ../README.rst 2>&1 | \
-       grep -v 'WARNING:rstcheck_core.checker:An `AttributeError` error occurred. This is most probably due to a code block directive (code/code-block/sourcecode) without a specified language.'
+       grep -v 'WARNING:rstcheck_core.checker:An `AttributeError` error .* This is most probably due to a code block directive (code/code-block/sourcecode) without a specified language.'
 }
 
 read_all_opt=""