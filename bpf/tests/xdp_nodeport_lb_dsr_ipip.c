f701c4f57cbe65b644f150f630a5883f4fd9a5af
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2026-08-10 09:50:06 +0200

    bpf,datapath: default RSS source prefix lengths
    Set the default IPv4 and IPv6 RSS source prefix lengths to 32 and 128,
    respectively. This preserves the existing fallback behavior while
    allowing the XDP configuration to override the values for custom CIDRs.
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- bpf/tests/xdp_nodeport_lb_dsr_ipip.c
+++ bpf/tests/xdp_nodeport_lb_dsr_ipip.c
@@ -44,9 +44,7 @@ mock_fib_lookup(__maybe_unused void *ctx, struct bpf_fib_lookup *params,
 ASSIGN_CONFIG(bool, enable_endpoint_routes, true)
 
 ASSIGN_CONFIG(union v4addr, ipv4_rss_prefix, { .be32 = LB_IP })
-ASSIGN_CONFIG(__u8, ipv4_rss_prefix_bits, 32)
 ASSIGN_CONFIG(union v6addr, ipv6_rss_prefix, LB_IPV6)
-ASSIGN_CONFIG(__u8, ipv6_rss_prefix_bits, 128)
 
 ASSIGN_CONFIG(union v4addr, ipv4_direct_routing, { .be32 = LB_IP })
 ASSIGN_CONFIG(union v6addr, ipv6_direct_routing, LB_IPV6)
