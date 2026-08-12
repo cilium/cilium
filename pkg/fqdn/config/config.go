0b85b66f9a9c4fd5e6c6e31adb1bb94b029884dd
Blaz Zupan <blaz@google.com>
2026-08-12 13:27:46 -0700

    fqdn: add helper to get dns selectors from config
    Add a helper that translates the new CLI flags into pod/namespace/CIDR selectors and L4 rules. This will let us auto-configure policy rules that route DNS traffic to the proxy for FQDN inspection.
    
    Signed-off-by: Blaz Zupan <blaz@google.com>

--- pkg/fqdn/config/config.go
+++ pkg/fqdn/config/config.go
@@ -4,10 +4,20 @@
 package config
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/spf13/pflag"
+
+	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/cilium/cilium/pkg/policy/types"
 )
+
+// ServiceResolver retrieves pod labels or backend IPs for a given Kubernetes service.
+type ServiceResolver func(namespace, name string) (podSelector map[string]string, ips []string)
 
 type FQDNPolicyDNSServerConfig struct {
 	FQDNPolicyDNSServerIPs     []string `mapstructure:"fqdn-policy-dns-server-ips"`
@@ -23,6 +33,57 @@ func (cfg FQDNPolicyDNSServerConfig) Flags(flags *pflag.FlagSet) {
 	flags.StringSlice("fqdn-policy-dns-server-ips", cfg.FQDNPolicyDNSServerIPs, "Comma-separated list of IP addresses of DNS servers for FQDN policy resolution")
 	flags.String("fqdn-policy-dns-server-service", cfg.FQDNPolicyDNSServerService, "K8s service (namespace/name) of DNS servers for FQDN policy resolution")
 	flags.Int("fqdn-policy-dns-server-port", cfg.FQDNPolicyDNSServerPort, "Port of DNS server pods for FQDN policy resolution")
+}
+
+// DNSSelectors creates and returns the pod or IP selectors targeting allowed DNS servers
+// for FQDN policies, along with the fully constructed L4 PortRules containing the allowed FQDN rules.
+func (cfg FQDNPolicyDNSServerConfig) DNSSelectors(resolver ServiceResolver, dnsRules api.PortRulesDNS) (types.Selectors, api.PortRules) {
+	var dnsSelectors types.Selectors
+
+	// Add Service Selector based on config if resolver is provided
+	if cfg.FQDNPolicyDNSServerService != "" && resolver != nil {
+		ns, name := ParseServiceRef(cfg.FQDNPolicyDNSServerService)
+		podSelector, ips := resolver(ns, name)
+
+		if len(podSelector) > 0 {
+			matchLabels := make(map[string]slim_metav1.MatchLabelsValue, len(podSelector))
+			for k, v := range podSelector {
+				matchLabels[k] = slim_metav1.MatchLabelsValue(v)
+			}
+			if ns != "" {
+				matchLabels[k8sConst.PodNamespaceLabel] = slim_metav1.MatchLabelsValue(ns)
+			}
+			podSel := api.NewESFromK8sLabelSelector(
+				labels.LabelSourceK8sKeyPrefix,
+				&slim_metav1.LabelSelector{
+					MatchLabels: matchLabels,
+				},
+			)
+			dnsSelectors = append(dnsSelectors, types.NewLabelSelector(podSel))
+		}
+
+		for _, ip := range ips {
+			dnsSelectors = append(dnsSelectors, types.ToSelector(api.CIDR(ip)))
+		}
+	}
+
+	// Add IP Selectors if IPs are provided
+	if len(cfg.FQDNPolicyDNSServerIPs) > 0 {
+		for _, ip := range cfg.FQDNPolicyDNSServerIPs {
+			dnsSelectors = append(dnsSelectors, types.ToSelector(api.CIDR(ip)))
+		}
+	}
+
+	portString := strconv.Itoa(cfg.FQDNPolicyDNSServerPort)
+	portRules := api.PortRules{{
+		Ports: []api.PortProtocol{
+			{Port: portString, Protocol: api.ProtoUDP},
+			{Port: portString, Protocol: api.ProtoTCP},
+		},
+		Rules: &api.L7Rules{DNS: dnsRules},
+	}}
+
+	return dnsSelectors, portRules
 }
 
 // ParseServiceRef splits a service reference string ("namespace/name" or "name")
