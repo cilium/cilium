c14a2c37c8ac5c01a56eea30e500bdf68e6ad9dc
Casey Callendrello <cdc@isovalent.com>
2026-08-24 15:23:00 +0200

    operator: perform CRD storage version migration
    If the operator detects that a given CRD's Status has more storage
    versions than the current version, kick off a Job that performs storage
    migration.
    
    To trigger a storage migration, we only need to issue a no-op PATCH
    request to the apiserver. This will trigger the apiserver to do an
    in-place upgrade.
    
    Once all the objects are PATCH'ed, then we can safely remove the
    outgoing storage version from the CRD's Status.
    
    Signed-off-by: Casey Callendrello <cdc@isovalent.com>

--- pkg/clustermesh/mcsapi/cell.go
+++ pkg/clustermesh/mcsapi/cell.go
@@ -144,13 +144,10 @@ func registerMCSAPIController(params mcsAPIParams) error {
 }
 
 func newMCSAPICRDs(cfg mcsapitypes.MCSAPIConfig) apis.RegisterCRDsFuncOut {
+	if !cfg.ShouldInstallMCSAPICrds() {
+		return apis.RegisterCRDsFuncOut{}
+	}
 	return apis.RegisterCRDsFuncOut{
-		Func: func(ctx context.Context, logger *slog.Logger, client k8sClient.Clientset) error {
-			if !cfg.ShouldInstallMCSAPICrds() {
-				return nil
-			}
-
-			return createCustomResourceDefinitions(ctx, logger, client)
-		},
+		Func: createCustomResourceDefinitions,
 	}
 }
