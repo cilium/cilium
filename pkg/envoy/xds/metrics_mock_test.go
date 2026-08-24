5d3b374a418cce0945af4adf0302940ea59012cc
Marco Hofstetter <marco.hofstetter@isovalent.com>
2026-08-24 10:09:37 +0200

    xds: make mock metrics concurrency-safe
    Protect the test-only mockMetrics helper with a mutex so concurrent ACK,
    NACK, and cancel updates do not write to shared maps unsafely.
    
    This fixes a panic in xDS tests where multiple request streams can share
    the same mockMetrics instance and trigger "fatal error: concurrent map
    writes" while updating counters.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- pkg/envoy/xds/metrics_mock_test.go
+++ pkg/envoy/xds/metrics_mock_test.go
@@ -3,21 +3,30 @@
 
 package xds
 
+import "github.com/cilium/cilium/pkg/lock"
+
 type mockMetrics struct {
+	mutex  lock.Mutex
 	ack    map[string]int
 	nack   map[string]int
 	cancel map[string]int
 }
 
 func (m *mockMetrics) IncreaseACK(typeURL string) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	m.ack[typeURL]++
 }
 
 func (m *mockMetrics) IncreaseNACK(typeURL string) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	m.nack[typeURL]++
 }
 
 func (m *mockMetrics) IncreaseCancel(typeURL string) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	m.cancel[typeURL]++
 }
 
