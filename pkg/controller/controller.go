bf34d259fa70b7a7d77580a7d2126f457e8cbbd1
Marco Iorio <marco.iorio@isovalent.com>
2026-08-27 11:53:04 +0200

    controller: let TriggerController respect CancelDoFuncOnUpdate
    Extend [Manager.TriggerController] to additionally cancel the DoFunc
    context when [CancelDoFuncOnUpdate] is set, to mimic the same behavior
    of [Manager.UpdateController].
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -252,24 +252,41 @@ func (c *controller) SetParams(params ControllerParams) {
 		params.RunInterval = maxInterval
 	}
 
+	old := c.params
+	c.params = params
+	c.updateContextLocked(old.Context, params.Context, old.CancelDoFuncOnUpdate)
+}
+
+// MaybeResetContext cancels the controller context if [CancelDoFuncOnUpdate] is
+// set, and replaces it with a new background context. It is a no-op otherwise.
+//
+// Manager's mutex must be held; controller.mutex must not be held
+func (c *controller) MaybeResetContext() {
+	c.paramMutex.Lock()
+	defer c.paramMutex.Unlock()
+
+	c.updateContextLocked(c.params.Context, nil, c.params.CancelDoFuncOnUpdate)
+}
+
+func (c *controller) updateContextLocked(curr, next context.Context, shouldCancel bool) {
 	// Save current context on update if not canceling
-	ctx := c.params.Context
+	ctx := curr
+
 	// Check if the current context needs to be cancelled
-	if c.params.CancelDoFuncOnUpdate && c.cancelDoFunc != nil {
+	if shouldCancel && c.cancelDoFunc != nil {
 		c.cancelDoFunc()
-		c.params.Context = nil
+		ctx = nil
 	}
 
 	// (re)set the context as the previous might have been cancelled
-	if c.params.Context == nil {
-		if params.Context == nil {
+	if ctx == nil {
+		if next == nil {
 			ctx, c.cancelDoFunc = context.WithCancel(context.Background())
 		} else {
-			ctx, c.cancelDoFunc = context.WithCancel(params.Context)
+			ctx, c.cancelDoFunc = context.WithCancel(next)
 		}
 	}
 
-	c.params = params
 	c.params.Context = ctx
 }
 
