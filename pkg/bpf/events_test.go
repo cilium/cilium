04101b2d44408de9104aa8eb49b199484355302f
Joe Stringer <joe@cilium.io>
2026-08-27 15:12:30 -0700

    bpf: Add test for map events ringbuffer GC
    Validate the garbage collection functionality, with a particular focus
    on race conditions in executing the underlying single-threaded ringbuffer
    implementation. To validate this without unreliable sleeps, this test
    runs two goroutines and coordinates them to give a high probability that
    the garbage collection and dump routines are executing the same
    underyling unsafe code at the same time.
    
    This was used to validate the fix in the previous commit which addresses
    a bug that could cause the cilium-agent to crash when a user is actively
    monitoring BPF map events and the cilium-agent concurrently writes map
    values to one of the BPF maps.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/bpf/events_test.go
+++ pkg/bpf/events_test.go
@@ -4,6 +4,7 @@
 package bpf
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
@@ -36,3 +37,59 @@ type IntTestKey uint32
 
 func (k IntTestKey) String() string { return fmt.Sprintf("key=%d", k) }
 func (k IntTestKey) New() MapKey    { return new(IntTestKey) }
+
+func TestEventsGC(t *testing.T) {
+	// Rather than setting a timeout, this testcase will avoid the time
+	// dependencies by using two blocking channels to *ensure* the garbage
+	// collection occurs in the critical section.
+	eb := newEventsBuffer(hivetest.Logger(t), "test", 32, 0)
+	dumpStarted := make(chan struct{})
+	doneChan := make(chan struct{})
+
+	// Add a bunch of keys. Garbage collection should clean these up.
+	keys := []int{}
+	for i := range 32 {
+		eb.add(&Event{cacheEntry: cacheEntry{Key: IntTestKey(i)}})
+	}
+
+	// Garbage collect the ringbuffer, but wait until the main test
+	// goroutine begins iterating the objects below before executing the
+	// garbage collection. This should make the failure highly reliable.
+	go func(dumpStarted, doneChan chan struct{}) {
+		<-dumpStarted
+		_ = eb.garbageCollect(context.Background())
+		close(doneChan)
+	}(dumpStarted, doneChan)
+
+	lastDump := false
+loop:
+	// Tight loop to touch the same internal data structures as the GC
+	// goroutine above to try to trigger a data race.
+	for {
+		if dumpStarted != nil {
+			close(dumpStarted)
+			dumpStarted = nil
+		}
+		keys = []int{}
+		eb.dumpAndSubscribe(t.Context(), func(e Event) {
+			k := e.Key.(IntTestKey)
+			keys = append(keys, int(k))
+		}, false)
+		if lastDump {
+			break
+		}
+		select {
+		case <-doneChan:
+			// Do one last full dumpAndSubscribe to ensure the
+			// complete final (garbage-collected) map is dumped,
+			// so the contents comparison can pass below.
+			lastDump = true
+			continue loop
+		default:
+		}
+	}
+
+	// Ensure that garbage collection completed its cleanup successfully.
+	assert.Empty(t, keys)
+	assert.Equal(t, []int{}, keys)
+}
