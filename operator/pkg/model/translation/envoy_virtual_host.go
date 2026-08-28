cilium/cilium

commit 3cf560f9fd78b168aecf20e2743ae08c624444aa

gateway-api: Add support for rewrite path filter
For prefix rewrite to work, we need to change the prefix match based on
regex to PathSeparatedPrefix route match, so that the prefix match is
recognized correctly.

Signed-off-by: Tam Mach <tam.mach@cilium.io>

operator/pkg/model/translation/envoy_virtual_host.go
@@ -16,6 +16,7 @@ import (
 	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/cilium/cilium/operator/pkg/model"
+	"github.com/cilium/cilium/pkg/math"
 )
 
 const (
@@ -76,6 +77,15 @@ func (s SortableRoute) Less(i, j int) bool {
 	}
 
 	// Make sure longest Prefix match always comes first
+	prefixMatch1 := math.IntMax(len(s[i].Match.GetPathSeparatedPrefix()), len(s[i].Match.GetPrefix()))
+	prefixMatch2 := math.IntMax(len(s[j].Match.GetPathSeparatedPrefix()), len(s[j].Match.GetPrefix()))
+	if prefixMatch1 > prefixMatch2 {
+		return true
+	} else if prefixMatch1 < prefixMatch2 {
+		return false
+	}
+
+	// Make sure longest Regex match always comes first
 	regexMatch1 := len(s[i].Match.GetSafeRegex().String())
 	regexMatch2 := len(s[j].Match.GetSafeRegex().String())
 	if regexMatch1 > regexMatch2 {
@@ -250,11 +260,39 @@ func hostRewriteMutation(rewrite *model.HTTPURLRewriteFilter) routeActionMutatio
 	}
 }
 
+func pathPrefixMutation(rewrite *model.HTTPURLRewriteFilter) routeActionMutation {
+	return func(route *envoy_config_route_v3.Route_Route) *envoy_config_route_v3.Route_Route {
+		if rewrite == nil || rewrite.Path == nil || len(rewrite.Path.Prefix) == 0 {
+			return route
+		}
+		route.Route.PrefixRewrite = rewrite.Path.Prefix
+		return route
+	}
+}
+
+func pathFullReplaceMutation(rewrite *model.HTTPURLRewriteFilter) routeActionMutation {
+	return func(route *envoy_config_route_v3.Route_Route) *envoy_config_route_v3.Route_Route {
+		if rewrite == nil || rewrite.Path == nil || len(rewrite.Path.Exact) == 0 {
+			return route
+		}
+		route.Route.RegexRewrite = &envoy_type_matcher_v3.RegexMatchAndSubstitute{
+			Pattern: &envoy_type_matcher_v3.RegexMatcher{
+				EngineType: &envoy_type_matcher_v3.RegexMatcher_GoogleRe2{},
+				Regex:      "^/.*$",
+			},
+			Substitution: rewrite.Path.Exact,
+		}
+		return route
+	}
+}
+
 func getRouteAction(backends []model.Backend, rewrite *model.HTTPURLRewriteFilter) *envoy_config_route_v3.Route_Route {
 	var routeAction *envoy_config_route_v3.Route_Route
 
 	var mutators = []routeActionMutation{
 		hostRewriteMutation(rewrite),
+		pathPrefixMutation(rewrite),
+		pathFullReplaceMutation(rewrite),
 	}
 
 	if len(backends) == 1 {
@@ -365,7 +403,9 @@ func envoyHTTPRouteNoBackend(route model.HTTPRoute, hostnames []string, hostName
 func getRouteMatch(hostnames []string, hostNameSuffixMatch bool, pathMatch model.StringMatch, headers []model.KeyValueMatch, query []model.KeyValueMatch, method *string) *envoy_config_route_v3.RouteMatch {
 	headerMatchers := getHeaderMatchers(hostnames, hostNameSuffixMatch, headers, method)
 	queryMatchers := getQueryMatchers(query)
-	if pathMatch.Exact != "" {
+
+	switch {
+	case pathMatch.Exact != "":
 		return &envoy_config_route_v3.RouteMatch{
 			PathSpecifier: &envoy_config_route_v3.RouteMatch_Path{
 				Path: pathMatch.Exact,
@@ -373,19 +413,23 @@ func getRouteMatch(hostnames []string, hostNameSuffixMatch bool, pathMatch model
 			Headers:         headerMatchers,
 			QueryParameters: queryMatchers,
 		}
-	}
-	if pathMatch.Prefix != "" {
+	case pathMatch.Prefix == "/":
 		return &envoy_config_route_v3.RouteMatch{
-			PathSpecifier: &envoy_config_route_v3.RouteMatch_SafeRegex{
-				SafeRegex: &envoy_type_matcher_v3.RegexMatcher{
-					Regex: getMatchingPrefixRegex(pathMatch.Prefix),
-				},
+			PathSpecifier: &envoy_config_route_v3.RouteMatch_Prefix{
+				Prefix: pathMatch.Prefix,
 			},
 			Headers:         headerMatchers,
 			QueryParameters: queryMatchers,
 		}
-	}
-	if pathMatch.Regex != "" {
+	case pathMatch.Prefix != "":
+		return &envoy_config_route_v3.RouteMatch{
+			PathSpecifier: &envoy_config_route_v3.RouteMatch_PathSeparatedPrefix{
+				PathSeparatedPrefix: strings.TrimSuffix(pathMatch.Prefix, "/"),
+			},
+			Headers:         headerMatchers,
+			QueryParameters: queryMatchers,
+		}
+	case pathMatch.Regex != "":
 		return &envoy_config_route_v3.RouteMatch{
 			PathSpecifier: &envoy_config_route_v3.RouteMatch_SafeRegex{
 				SafeRegex: &envoy_type_matcher_v3.RegexMatcher{
@@ -395,13 +439,14 @@ func getRouteMatch(hostnames []string, hostNameSuffixMatch bool, pathMatch model
 			Headers:         headerMatchers,
 			QueryParameters: queryMatchers,
 		}
-	}
-	return &envoy_config_route_v3.RouteMatch{
-		PathSpecifier: &envoy_config_route_v3.RouteMatch_Prefix{
-			Prefix: "/",
-		},
-		Headers:         headerMatchers,
-		QueryParameters: queryMatchers,
+	default:
+		return &envoy_config_route_v3.RouteMatch{
+			PathSpecifier: &envoy_config_route_v3.RouteMatch_Prefix{
+				Prefix: "/",
+			},
+			Headers:         headerMatchers,
+			QueryParameters: queryMatchers,
+		}
 	}
 }
 
@@ -465,23 +510,6 @@ func getHeaderMatchers(hostnames []string, hostNameSuffixMatch bool, headers []m
 	}
 
 	return result
-}
-
-// getMatchingPrefixRegex returns safe regex used by envoy to match the
-// prefix. By default, prefix matching in envoy will not reject /foobar
-// if the original path is only /foo. Hence, conversion with safe regex
-// is required.
-//
-// If the original path is /foo, the returned regex will be /foo(/.*)?$
-// - /foo -> matched
-// - /foo/ -> matched
-// - /foobar -> not matched
-func getMatchingPrefixRegex(path string) string {
-	removedTrailingSlash := path
-	if strings.HasSuffix(path, slash) {
-		removedTrailingSlash = removedTrailingSlash[:len(removedTrailingSlash)-1]
-	}
-	return fmt.Sprintf("%s(/.*)?$", removedTrailingSlash)
 }
 
 // getMatchingHeaderRegex is to make sure that one and only one single

operator/pkg/model/translation/envoy_virtual_host_test.go
@@ -147,3 +147,27 @@ func Test_hostRewriteMutation(t *testing.T) {
 		})
 	})
 }
+
+func Test_pathPrefixMutation(t *testing.T) {
+	t.Run("no prefix rewrite", func(t *testing.T) {
+		route := &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{},
+		}
+		res := pathPrefixMutation(nil)(route)
+		require.Equal(t, route, res)
+	})
+
+	t.Run("with prefix rewrite", func(t *testing.T) {
+		route := &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{},
+		}
+		rewrite := &model.HTTPURLRewriteFilter{
+			Path: &model.StringMatch{
+				Prefix: "/prefix",
+			},
+		}
+
+		res := pathPrefixMutation(rewrite)(route)
+		require.Equal(t, res.Route.PrefixRewrite, "/prefix")
+	})
+}

.github/workflows/conformance-gateway-api.yaml
@@ -111,7 +111,7 @@ jobs:
           fi
           echo sha=${SHA} >> $GITHUB_OUTPUT
           
-          SUPPORTED_FEATURES="ReferenceGrant,HTTPRoute,TLSRoute,HTTPRouteQueryParamMatching,HTTPRouteMethodMatching,GatewayClassObservedGenerationBump,HTTPRouteHostRewrite"
+          SUPPORTED_FEATURES="ReferenceGrant,HTTPRoute,TLSRoute,HTTPRouteQueryParamMatching,HTTPRouteMethodMatching,GatewayClassObservedGenerationBump,HTTPRouteHostRewrite,HTTPRoutePathRewrite"
           if [ ${{ matrix.crd-channel }} == "experimental" ]; then
             SUPPORTED_FEATURES+=",HTTPResponseHeaderModification,RouteDestinationPortMatching"
           fi

operator/pkg/model/ingestion/gateway.go
@@ -258,8 +258,26 @@ func toHTTPRewriteFilter(rewrite *gatewayv1beta1.HTTPURLRewriteFilter) *model.HT
 	if rewrite == nil {
 		return nil
 	}
+	var path *model.StringMatch
+	if rewrite.Path != nil {
+		switch rewrite.Path.Type {
+		case gatewayv1beta1.FullPathHTTPPathModifier:
+			if rewrite.Path.ReplaceFullPath != nil {
+				path = &model.StringMatch{
+					Exact: *rewrite.Path.ReplaceFullPath,
+				}
+			}
+		case gatewayv1beta1.PrefixMatchHTTPPathModifier:
+			if rewrite.Path.ReplacePrefixMatch != nil {
+				path = &model.StringMatch{
+					Prefix: *rewrite.Path.ReplacePrefixMatch,
+				}
+			}
+		}
+	}
 	return &model.HTTPURLRewriteFilter{
 		HostName: (*string)(rewrite.Hostname),
+		Path:     path,
 	}
 }
 

operator/pkg/model/ingestion/gateway_fixture_test.go
@@ -1933,3 +1933,192 @@ var rewriteHostHTTPRoutes = []gatewayv1beta1.HTTPRoute{
 		},
 	},
 }
+
+// https://github.com/kubernetes-sigs/gateway-api/blob/v0.7.1/conformance/tests/httproute-rewrite-path.yaml
+var rewritePathHTTPRoutes = []gatewayv1beta1.HTTPRoute{
+	{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "rewrite-path",
+			Namespace: "gateway-conformance-infra",
+		},
+		Spec: gatewayv1beta1.HTTPRouteSpec{
+			CommonRouteSpec: gatewayv1beta1.CommonRouteSpec{
+				ParentRefs: []gatewayv1beta1.ParentReference{
+					{
+						Name: "same-namespace",
+					},
+				},
+			},
+			Rules: []gatewayv1beta1.HTTPRouteRule{
+				{
+					Matches: []gatewayv1beta1.HTTPRouteMatch{
+						{
+							Path: &gatewayv1beta1.HTTPPathMatch{
+								Type:  model.AddressOf[gatewayv1beta1.PathMatchType](gatewayv1beta1.PathMatchPathPrefix),
+								Value: model.AddressOf("/prefix/one"),
+							},
+						},
+					},
+					Filters: []gatewayv1beta1.HTTPRouteFilter{
+						{
+							Type: "URLRewrite",
+							URLRewrite: &gatewayv1beta1.HTTPURLRewriteFilter{
+								Path: &gatewayv1beta1.HTTPPathModifier{
+									Type:               "ReplacePrefixMatch",
+									ReplacePrefixMatch: model.AddressOf("/one"),
+								},
+							},
+						},
+					},
+					BackendRefs: []gatewayv1beta1.HTTPBackendRef{
+						{
+							BackendRef: gatewayv1beta1.BackendRef{
+								BackendObjectReference: gatewayv1beta1.BackendObjectReference{
+									Name: "infra-backend-v1",
+									Port: model.AddressOf[gatewayv1beta1.PortNumber](8080),
+								},
+							},
+						},
+					},
+				},
+				{
+					Matches: []gatewayv1beta1.HTTPRouteMatch{
+						{
+							Path: &gatewayv1beta1.HTTPPathMatch{
+								Type:  model.AddressOf[gatewayv1beta1.PathMatchType](gatewayv1beta1.PathMatchPathPrefix),
+								Value: model.AddressOf("/full/one"),
+							},
+						},
+					},
+					Filters: []gatewayv1beta1.HTTPRouteFilter{
+						{
+							Type: "URLRewrite",
+							URLRewrite: &gatewayv1beta1.HTTPURLRewriteFilter{
+								Path: &gatewayv1beta1.HTTPPathModifier{
+									Type:            "ReplaceFullPath",
+									ReplaceFullPath: model.AddressOf("/one"),
+								},
+							},
+						},
+					},
+					BackendRefs: []gatewayv1beta1.HTTPBackendRef{
+						{
+							BackendRef: gatewayv1beta1.BackendRef{
+								BackendObjectReference: gatewayv1beta1.BackendObjectReference{
+									Name: "infra-backend-v1",
+									Port: model.AddressOf[gatewayv1beta1.PortNumber](8080),
+								},
+							},
+						},
+					},
+				},
+				{
+					Matches: []gatewayv1beta1.HTTPRouteMatch{
+						{
+							Path: &gatewayv1beta1.HTTPPathMatch{
+								Type:  model.AddressOf[gatewayv1beta1.PathMatchType](gatewayv1beta1.PathMatchPathPrefix),
+								Value: model.AddressOf("/full/rewrite-path-and-modify-headers"),
+							},
+						},
+					},
+					Filters: []gatewayv1beta1.HTTPRouteFilter{
+						{
+							Type: "URLRewrite",
+							URLRewrite: &gatewayv1beta1.HTTPURLRewriteFilter{
+								Path: &gatewayv1beta1.HTTPPathModifier{
+									Type:            "ReplaceFullPath",
+									ReplaceFullPath: model.AddressOf("/test"),
+								},
+							},
+						},
+						{
+							Type: "RequestHeaderModifier",
+							RequestHeaderModifier: &gatewayv1beta1.HTTPHeaderFilter{
+								Set: []gatewayv1beta1.HTTPHeader{
+									{
+										Name:  "X-Header-Set",
+										Value: "set-overwrites-values",
+									},
+								},
+								Add: []gatewayv1beta1.HTTPHeader{
+									{
+										Name:  "X-Header-Add",
+										Value: "header-val-1",
+									},
+									{
+										Name:  "X-Header-Add-Append",
+										Value: "header-val-2",
+									},
+								},
+								Remove: []string{"X-Header-Remove"},
+							},
+						},
+					},
+					BackendRefs: []gatewayv1beta1.HTTPBackendRef{
+						{
+							BackendRef: gatewayv1beta1.BackendRef{
+								BackendObjectReference: gatewayv1beta1.BackendObjectReference{
+									Name: "infra-backend-v1",
+									Port: model.AddressOf[gatewayv1beta1.PortNumber](8080),
+								},
+							},
+						},
+					},
+				},
+				{
+					Matches: []gatewayv1beta1.HTTPRouteMatch{
+						{
+							Path: &gatewayv1beta1.HTTPPathMatch{
+								Type:  model.AddressOf[gatewayv1beta1.PathMatchType](gatewayv1beta1.PathMatchPathPrefix),
+								Value: model.AddressOf("/prefix/rewrite-path-and-modify-headers"),
+							},
+						},
+					},
+					Filters: []gatewayv1beta1.HTTPRouteFilter{
+						{
+							Type: "URLRewrite",
+							URLRewrite: &gatewayv1beta1.HTTPURLRewriteFilter{
+								Path: &gatewayv1beta1.HTTPPathModifier{
+									Type:               "ReplacePrefixMatch",
+									ReplacePrefixMatch: model.AddressOf("/prefix"),
+								},
+							},
+						},
+						{
+							Type: "RequestHeaderModifier",
+							RequestHeaderModifier: &gatewayv1beta1.HTTPHeaderFilter{
+								Set: []gatewayv1beta1.HTTPHeader{
+									{
+										Name:  "X-Header-Set",
+										Value: "set-overwrites-values",
+									},
+								},
+								Add: []gatewayv1beta1.HTTPHeader{
+									{
+										Name:  "X-Header-Add",
+										Value: "header-val-1",
+									},
+									{
+										Name:  "X-Header-Add-Append",
+										Value: "header-val-2",
+									},
+								},
+								Remove: []string{"X-Header-Remove"},
+							},
+						},
+					},
+					BackendRefs: []gatewayv1beta1.HTTPBackendRef{
+						{
+							BackendRef: gatewayv1beta1.BackendRef{
+								BackendObjectReference: gatewayv1beta1.BackendObjectReference{
+									Name: "infra-backend-v1",
+									Port: model.AddressOf[gatewayv1beta1.PortNumber](8080),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}

operator/pkg/model/ingestion/gateway_test.go
@@ -1404,6 +1404,135 @@ var rewriteHostHTTPListeners = []model.HTTPListener{
 	},
 }
 
+var rewritePathHTTPInput = Input{
+	GatewayClass: gatewayv1beta1.GatewayClass{},
+	Gateway:      sameNamespaceGateway,
+	HTTPRoutes:   rewritePathHTTPRoutes,
+	Services:     allServices,
+}
+
+var rewritePathHTTPListeners = []model.HTTPListener{
+	{
+		Name: "http",
+		Sources: []model.FullyQualifiedResource{
+			{
+				Name:      "same-namespace",
+				Namespace: "gateway-conformance-infra",
+			},
+		},
+		Port:     80,
+		Hostname: "*",
+		Routes: []model.HTTPRoute{
+			{
+				PathMatch: model.StringMatch{Prefix: "/prefix/one"},
+				Backends: []model.Backend{
+					{
+						Name:      "infra-backend-v1",
+						Namespace: "gateway-conformance-infra",
+						Port: &model.BackendPort{
+							Port: 8080,
+						},
+					},
+				},
+				Rewrite: &model.HTTPURLRewriteFilter{
+					Path: &model.StringMatch{
+						Prefix: "/one",
+					},
+				},
+			},
+			{
+				PathMatch: model.StringMatch{Prefix: "/full/one"},
+				Backends: []model.Backend{
+					{
+						Name:      "infra-backend-v1",
+						Namespace: "gateway-conformance-infra",
+						Port: &model.BackendPort{
+							Port: 8080,
+						},
+					},
+				},
+				Rewrite: &model.HTTPURLRewriteFilter{
+					Path: &model.StringMatch{
+						Exact: "/one",
+					},
+				},
+			},
+			{
+				PathMatch: model.StringMatch{Prefix: "/full/rewrite-path-and-modify-headers"},
+				Backends: []model.Backend{
+					{
+						Name:      "infra-backend-v1",
+						Namespace: "gateway-conformance-infra",
+						Port: &model.BackendPort{
+							Port: 8080,
+						},
+					},
+				},
+				Rewrite: &model.HTTPURLRewriteFilter{
+					Path: &model.StringMatch{
+						Exact: "/test",
+					},
+				},
+				RequestHeaderFilter: &model.HTTPHeaderFilter{
+					HeadersToAdd: []model.Header{
+						{
+							Name:  "X-Header-Add",
+							Value: "header-val-1",
+						},
+						{
+							Name:  "X-Header-Add-Append",
+							Value: "header-val-2",
+						},
+					},
+					HeadersToSet: []model.Header{
+						{
+							Name:  "X-Header-Set",
+							Value: "set-overwrites-values",
+						},
+					},
+					HeadersToRemove: []string{"X-Header-Remove"},
+				},
+			},
+			{
+				PathMatch: model.StringMatch{Prefix: "/prefix/rewrite-path-and-modify-headers"},
+				Backends: []model.Backend{
+					{
+						Name:      "infra-backend-v1",
+						Namespace: "gateway-conformance-infra",
+						Port: &model.BackendPort{
+							Port: 8080,
+						},
+					},
+				},
+				Rewrite: &model.HTTPURLRewriteFilter{
+					Path: &model.StringMatch{
+						Prefix: "/prefix",
+					},
+				},
+				RequestHeaderFilter: &model.HTTPHeaderFilter{
+					HeadersToAdd: []model.Header{
+						{
+							Name:  "X-Header-Add",
+							Value: "header-val-1",
+						},
+						{
+							Name:  "X-Header-Add-Append",
+							Value: "header-val-2",
+						},
+					},
+					HeadersToSet: []model.Header{
+						{
+							Name:  "X-Header-Set",
+							Value: "set-overwrites-values",
+						},
+					},
+					HeadersToRemove: []string{"X-Header-Remove"},
+				},
+			},
+		},
+	},
+}
+
 func TestHTTPGatewayAPI(t *testing.T) {
 	tests := map[string]struct {
 		input Input
@@ -1468,6 +1597,10 @@ func TestHTTPGatewayAPI(t *testing.T) {
 		"Conformance/HTTPRouteRewriteHost": {
 			input: rewriteHostHTTPInput,
 			want:  rewriteHostHTTPListeners,
+		},
+		"Conformance/HTTPRouteRewritePath": {
+			input: rewritePathHTTPInput,
+			want:  rewritePathHTTPListeners,
 		},
 	}
 

operator/pkg/model/model.go
@@ -200,6 +200,9 @@ type HTTPURLRewriteFilter struct {
 	// Hostname is the value to be used to replace the Host header value during
 	// forwarding.
 	HostName *string `json:"hostname,omitempty"`
+
+	// Path is the values to be used to replace the path
+	Path *StringMatch `json:"path,omitempty"`
 }
 
 // HTTPRoute holds all the details needed to route HTTP traffic to a backend.

commit 1f400cbe6e43e9d87092deae5672a3e0d6dd136d

gateway-api: escape backreferences in URLRewrite ReplaceFullPath
When translating a Gateway API HTTPRoute URLRewrite filter with
ReplaceFullPath, the exact path was previously passed unescaped to
Envoy's RegexMatchAndSubstitute rule.

Because Envoy uses Google RE2 for regex substitution and the matching
pattern contains no capturing groups, unescaped backslashes and capture
group references (e.g. \1) in user-defined paths cause RE2 to fail internally
at runtime. Envoy safely catches this failure, aborts the rewrite filter without
crashing, and falls back to proxying downstream requests with the original,
unmodified path.

Escape literal backslashes during translation so that Envoy
evaluates them as literal characters. Also add defense-in-depth escaping
for ReplacePrefixMatch and unit tests covering edge cases.

```release-note
Fix an issue where Gateway API URLRewrite ReplaceFullPath filters containing unescaped backreferences were silently bypassed by Envoy out of safety.
```

Fixes: 3cf560f9fd78 ("gateway-api: add support for URL rewrite")
Signed-off-by: Mateusz Masiarz <mslo@google.com>

operator/pkg/model/translation/envoy_virtual_host.go
@@ -409,7 +409,7 @@ func pathPrefixMutation(rewrite *model.HTTPURLRewriteFilter, httpRoute *model.HT
 				Pattern: &envoy_type_matcher_v3.RegexMatcher{
 					Regex: `^/(.*)`,
 				},
-				Substitution: strings.TrimSuffix(rewrite.Path.Prefix, "/") + `/\1`,
+				Substitution: strings.ReplaceAll(strings.TrimSuffix(rewrite.Path.Prefix, "/"), `\`, `\\`) + `/\1`,
 			}
 		} else {
 			route.Route.PrefixRewrite = rewrite.Path.Prefix
@@ -427,7 +427,7 @@ func pathFullReplaceMutation(rewrite *model.HTTPURLRewriteFilter) routeActionMut
 			Pattern: &envoy_type_matcher_v3.RegexMatcher{
 				Regex: "^/.*$",
 			},
-			Substitution: rewrite.Path.Exact,
+			Substitution: strings.ReplaceAll(rewrite.Path.Exact, `\`, `\\`),
 		}
 		return route
 	}

operator/pkg/model/translation/envoy_virtual_host_test.go
@@ -573,6 +573,136 @@ func Test_pathPrefixMutation(t *testing.T) {
 	})
 }
 
+func Test_pathFullReplaceMutation(t *testing.T) {
+	t.Run("no full path replace", func(t *testing.T) {
+		route := &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{},
+		}
+		res := pathFullReplaceMutation(nil)(route)
+		require.Equal(t, route, res)
+
+		res = pathFullReplaceMutation(&model.HTTPURLRewriteFilter{})(route)
+		require.Equal(t, route, res)
+
+		res = pathFullReplaceMutation(&model.HTTPURLRewriteFilter{Path: &model.StringMatch{}})(route)
+		require.Equal(t, route, res)
+	})
+
+	t.Run("with simple full path replace", func(t *testing.T) {
+		route := &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{},
+		}
+		rewrite := &model.HTTPURLRewriteFilter{
+			Path: &model.StringMatch{
+				Exact: "/new-path",
+			},
+		}
+
+		res := pathFullReplaceMutation(rewrite)(route)
+		require.Equal(t, &envoy_type_matcher_v3.RegexMatchAndSubstitute{
+			Pattern: &envoy_type_matcher_v3.RegexMatcher{
+				Regex: "^/.*$",
+			},
+			Substitution: "/new-path",
+		}, res.Route.RegexRewrite)
+	})
+
+	t.Run("with full path replace containing backreferences", func(t *testing.T) {
+		route := &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{},
+		}
+		rewrite := &model.HTTPURLRewriteFilter{
+			Path: &model.StringMatch{
+				Exact: `/foo/\1`,
+			},
+		}
+
+		res := pathFullReplaceMutation(rewrite)(route)
+		require.Equal(t, &envoy_type_matcher_v3.RegexMatchAndSubstitute{
+			Pattern: &envoy_type_matcher_v3.RegexMatcher{
+				Regex: "^/.*$",
+			},
+			Substitution: `/foo/\\1`,
+		}, res.Route.RegexRewrite)
+	})
+
+	t.Run("with full path replace containing whole match backreference 0", func(t *testing.T) {
+		route := &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{},
+		}
+		rewrite := &model.HTTPURLRewriteFilter{
+			Path: &model.StringMatch{
+				Exact: `/prefix/\0/suffix`,
+			},
+		}
+
+		res := pathFullReplaceMutation(rewrite)(route)
+		require.Equal(t, &envoy_type_matcher_v3.RegexMatchAndSubstitute{
+			Pattern: &envoy_type_matcher_v3.RegexMatcher{
+				Regex: "^/.*$",
+			},
+			Substitution: `/prefix/\\0/suffix`,
+		}, res.Route.RegexRewrite)
+	})
+
+	t.Run("with full path replace containing trailing backslash", func(t *testing.T) {
+		route := &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{},
+		}
+		rewrite := &model.HTTPURLRewriteFilter{
+			Path: &model.StringMatch{
+				Exact: `/foo\`,
+			},
+		}
+
+		res := pathFullReplaceMutation(rewrite)(route)
+		require.Equal(t, &envoy_type_matcher_v3.RegexMatchAndSubstitute{
+			Pattern: &envoy_type_matcher_v3.RegexMatcher{
+				Regex: "^/.*$",
+			},
+			Substitution: `/foo\\`,
+		}, res.Route.RegexRewrite)
+	})
+
+	t.Run("with full path replace containing consecutive backslashes", func(t *testing.T) {
+		route := &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{},
+		}
+		rewrite := &model.HTTPURLRewriteFilter{
+			Path: &model.StringMatch{
+				Exact: `/foo\\bar`,
+			},
+		}
+
+		res := pathFullReplaceMutation(rewrite)(route)
+		require.Equal(t, &envoy_type_matcher_v3.RegexMatchAndSubstitute{
+			Pattern: &envoy_type_matcher_v3.RegexMatcher{
+				Regex: "^/.*$",
+			},
+			Substitution: `/foo\\\\bar`,
+		}, res.Route.RegexRewrite)
+	})
+
+	t.Run("with full path replace containing multiple backslashes", func(t *testing.T) {
+		route := &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{},
+		}
+		rewrite := &model.HTTPURLRewriteFilter{
+			Path: &model.StringMatch{
+				Exact: `\1\2\3\g\k`,
+			},
+		}
+
+		res := pathFullReplaceMutation(rewrite)(route)
+		require.Equal(t, &envoy_type_matcher_v3.RegexMatchAndSubstitute{
+			Pattern: &envoy_type_matcher_v3.RegexMatcher{
+				Regex: "^/.*$",
+			},
+			Substitution: `\\1\\2\\3\\g\\k`,
+		}, res.Route.RegexRewrite)
+	})
+}
+
 func Test_requestMirrorMutation(t *testing.T) {
 	t.Run("no mirror", func(t *testing.T) {
 		route := &envoy_config_route_v3.Route_Route{