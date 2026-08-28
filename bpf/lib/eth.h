4a566527363d5b31f8ab33481f9d2dd1370b571b
Tobias Klauser <tobias@cilium.io>
2026-08-28 12:02:15 +0200

    bpf: constify __ctx_buff in eth_load_{d,s}addr
    This allows to use these helper functions in places where __ctx_buff is
    const. {ctx,skb}_load_bytes already takes a const __ctx_buff pointer.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- bpf/lib/eth.h
+++ bpf/lib/eth.h
@@ -48,8 +48,8 @@ static __always_inline bool eth_is_supported_ethertype(__be16 proto)
 	return proto >= bpf_htons(ETH_P_802_3_MIN);
 }
 
-static __always_inline int eth_load_saddr(struct __ctx_buff *ctx, __u8 *mac,
-					  int off)
+static __always_inline int eth_load_saddr(const struct __ctx_buff *ctx,
+					  __u8 *mac, int off)
 {
 	return ctx_load_bytes(ctx, off + ETH_ALEN, mac, ETH_ALEN);
 }
@@ -79,8 +79,8 @@ static __always_inline int eth_store_saddr(struct __ctx_buff *ctx,
 #endif
 }
 
-static __always_inline int eth_load_daddr(struct __ctx_buff *ctx, __u8 *mac,
-					  int off)
+static __always_inline int eth_load_daddr(const struct __ctx_buff *ctx,
+					  __u8 *mac, int off)
 {
 	return ctx_load_bytes(ctx, off, mac, ETH_ALEN);
 }
