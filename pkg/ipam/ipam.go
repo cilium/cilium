31e373f54726c05139b59186ff2dfbd65cba14a3
Jared Ledvina <jared.ledvina@datadoghq.com>
2026-08-31 15:23:42 -0400

    ipam: remove Azure CRD allocator handling
    Signed-off-by: Jared Ledvina <jared.ledvina@datadoghq.com>

--- pkg/ipam/ipam.go
+++ pkg/ipam/ipam.go
@@ -209,7 +209,7 @@ func (ipam *IPAM) ConfigureAllocator(ctx context.Context) error {
 		if ipam.config.IPv4Enabled() {
 			ipam.ipv4Allocator = v4Allocator
 		}
-	case ipamOption.IPAMCRD, ipamOption.IPAMAzure, ipamOption.IPAMAlibabaCloud:
+	case ipamOption.IPAMCRD, ipamOption.IPAMAlibabaCloud:
 		ipam.logger.Info("Initializing CRD-based IPAM")
 		if ipam.config.IPv6Enabled() {
 			ipam.ipv6Allocator = newCRDAllocator(ipam.logger, IPv6, ipam.config, ipam.nodeDiscovery, ipam.localNodeStore, ipam.clientset, ipam.k8sEventReg, ipam.mtuConfig, ipam.sysctl, ipam.ipMasqAgent)
