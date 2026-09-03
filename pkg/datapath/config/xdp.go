9c5aeeeac8ab2d8fa7c272f1ca6dfd12a3c93983
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2026-09-03 14:01:25 +0200

    datapath: move RSS prefix resolution out of XDP config
    Introduce loadbalancer.RSSConfig to resolve configured RSS prefixes and
    populate the resolved configuration while constructing the local node
    configuration. The XDP configuration responsible only for converting
    the prefixes to datapath types.
    
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- pkg/datapath/config/xdp.go
+++ pkg/datapath/config/xdp.go
@@ -6,7 +6,6 @@ package config
 import (
 	"github.com/vishvananda/netlink"
 
-	"github.com/cilium/cilium/pkg/datapath/tables"
 	"github.com/cilium/cilium/pkg/option"
 )
 
@@ -29,31 +28,13 @@ func XDP(lnc *Config, link netlink.Link) any {
 	cfg.EnableIPv4Fragments = option.Config.EnableIPv4FragmentsTracking
 	cfg.EnableIPv6Fragments = option.Config.EnableIPv6FragmentsTracking
 
-	if option.Config.EnableIPv4 {
-		rssPrefix := option.Config.UnsafeDaemonConfigOption.LoadBalancerRSSv4
-		if rssPrefix.IsValid() {
-			cfg.IPv4RSSPrefix.Addr = rssPrefix.Addr().As4()
-			cfg.IPv4RSSPrefixBits = uint8(rssPrefix.Bits())
-		} else if lnc.DirectRoutingDevice != nil {
-			addr := tables.PreferredIPv4Address(lnc.DirectRoutingDevice.Addrs)
-			if addr.IsValid() {
-				cfg.IPv4RSSPrefix.Addr = addr.As4()
-			}
-		}
-	}
-
-	if option.Config.EnableIPv6 {
-		rssPrefix := option.Config.UnsafeDaemonConfigOption.LoadBalancerRSSv6
-		if rssPrefix.IsValid() {
-			cfg.IPv6RSSPrefix.Addr = rssPrefix.Addr().As16()
-			cfg.IPv6RSSPrefixBits = uint8(rssPrefix.Bits())
-		} else if lnc.DirectRoutingDevice != nil {
-			addr := tables.PreferredIPv6Address(lnc.DirectRoutingDevice.Addrs)
-			if addr.IsValid() {
-				cfg.IPv6RSSPrefix.Addr = addr.As16()
-			}
-		}
-	}
+	lbRSSCfg := lnc.LoadBalancerRSS
+	ipv4Prefix := lbRSSCfg.IPv4Prefix()
+	cfg.IPv4RSSPrefix.Addr = ipv4Prefix.Addr().As4()
+	cfg.IPv4RSSPrefixBits = uint8(ipv4Prefix.Bits())
+	ipv6Prefix := lbRSSCfg.IPv6Prefix()
+	cfg.IPv6RSSPrefix.Addr = ipv6Prefix.Addr().As16()
+	cfg.IPv6RSSPrefixBits = uint8(ipv6Prefix.Bits())
 
 	return cfg
 }
