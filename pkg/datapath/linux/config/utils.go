ce0d2a574f304e1c21d5c725b3f02afc1a5a21cf
Tobias Klauser <tobias@cilium.io>
2026-09-03 11:21:41 +0200

    datapath/linux/config: remove unused FmtDefineAddress
    The utility function is unused since commit 7f8096edf59e ("bpf,
    datapath: move IPv6 SNAT exclusion to runtime config").
    
    AIL: 0
    Suggested-by: Timo Beckers <timo@isovalent.com>
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- pkg/datapath/linux/config/utils.go
+++ pkg/datapath/linux/config/utils.go
@@ -9,13 +9,6 @@ import (
 	"github.com/cilium/cilium/pkg/common"
 )
 
-// FmtDefineAddress returns the a define string from the given name and addr.
-// Example:
-// fmt.Print(FmtDefineAddress("foo", []byte{1, 2, 3})) // "#define foo { .addr = { 0x1, 0x2, 0x3 } }\n"
-func FmtDefineAddress(name string, addr []byte) string {
-	return fmt.Sprintf("#define %s { .addr = { %s } }\n", name, common.GoArray2C(addr))
-}
-
 func dumpRaw(name string, addr []byte) string {
 	return fmt.Sprintf(" %s%s\n", name, common.GoArray2C(addr))
 }
