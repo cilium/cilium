cilium/cilium

commit 64e171e9e52b801179cf7b09dfd535a38267591c

bpf: lb: move lb*_ctx_store_state() helpers to bpf_lxc
While these helpers are related to LB state, they are really managing how
bpf_lxc's per-packet path is propagating its internal state across a
program-internal tailcall.

So let's maintain them closer to where they are being used, there is
no shared logic here.

Also add a bit of build test coverage for the per-packet LB.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/bpf_lxc.c
@@ -104,9 +104,48 @@ lxc_redirect_to_host(struct __ctx_buff *ctx, __u32 src_sec_identity,
 # define ENABLE_PER_PACKET_LB 1
 #endif
 
-#ifdef ENABLE_PER_PACKET_LB
-
 #ifdef ENABLE_IPV4
+static __always_inline void
+lb4_ctx_restore_state(struct __ctx_buff *ctx, struct ct_state *state,
+		      __u16 *proxy_port, __u32 *cluster_id __maybe_unused,
+		      bool clear)
+{
+	__u32 meta = clear ? ctx_load_and_clear_meta(ctx, CB_CT_STATE) :
+			     ctx_load_meta(ctx, CB_CT_STATE);
+
+	if (meta & 1)
+		state->loopback = 1;
+
+	state->rev_nat_index = meta >> 16;
+
+	*proxy_port = clear ? (ctx_load_and_clear_meta(ctx, CB_PROXY_MAGIC) >> 16) :
+			      (ctx_load_meta(ctx, CB_PROXY_MAGIC) >> 16);
+
+#ifdef ENABLE_CLUSTER_AWARE_ADDRESSING
+	*cluster_id = clear ? ctx_load_and_clear_meta(ctx, CB_CLUSTER_ID_EGRESS) :
+			      ctx_load_meta(ctx, CB_CLUSTER_ID_EGRESS);
+#endif
+}
+
+#ifdef ENABLE_PER_PACKET_LB
+/* lb4_ctx_store_state() stores per packet load balancing state to be picked
+ * up on the continuation tail call.
+ */
+static __always_inline void
+lb4_ctx_store_state(struct __ctx_buff *ctx, const struct ct_state *state,
+		    __u16 proxy_port, __u32 cluster_id)
+{
+	ctx_store_meta(ctx, CB_PROXY_MAGIC, (__u32)proxy_port << 16);
+	ctx_store_meta(ctx, CB_CT_STATE, (__u32)state->rev_nat_index << 16 |
+		       state->loopback);
+	ctx_store_meta(ctx, CB_CLUSTER_ID_EGRESS, cluster_id);
+}
+
+/* lb4_ctx_restore_state() restores per packet load balancing state from the
+ * previous tail call.
+ * tuple->flags does not need to be restored, as it will be reinitialized from
+ * the packet.
+ */
 static __always_inline int __per_packet_lb_svc_xlate_4(void *ctx, struct iphdr *ip4,
 						       __s8 *ext_err)
 {
@@ -206,9 +245,44 @@ skip_service_lookup:
 	lb4_ctx_store_state(ctx, &ct_state_new, proxy_port, cluster_id);
 	return tail_call_internal(ctx, CILIUM_CALL_IPV4_CT_EGRESS, ext_err);
 }
+#endif /* ENABLE_PER_PACKET_LB */
 #endif /* ENABLE_IPV4 */
 
 #ifdef ENABLE_IPV6
+/* lb6_ctx_restore_state() restores per packet load balancing state from the
+ * previous tail call.
+ * tuple->flags does not need to be restored, as it will be reinitialized from
+ * the packet.
+ */
+static __always_inline void
+lb6_ctx_restore_state(struct __ctx_buff *ctx, struct ct_state *state,
+		      __u16 *proxy_port,  bool clear)
+{
+	__u32 meta = clear ? ctx_load_and_clear_meta(ctx, CB_CT_STATE) :
+				       ctx_load_meta(ctx, CB_CT_STATE);
+
+	if (meta & 1)
+		state->loopback = 1;
+
+	state->rev_nat_index = meta >> 16;
+
+	*proxy_port = clear ? (ctx_load_and_clear_meta(ctx, CB_PROXY_MAGIC) >> 16) :
+			      (ctx_load_meta(ctx, CB_PROXY_MAGIC) >> 16);
+}
+
+#ifdef ENABLE_PER_PACKET_LB
+/* lb6_ctx_store_state() stores per packet load balancing state to be picked
+ * up on the continuation tail call.
+ */
+static __always_inline void
+lb6_ctx_store_state(struct __ctx_buff *ctx, const struct ct_state *state,
+		    __u16 proxy_port)
+{
+	ctx_store_meta(ctx, CB_PROXY_MAGIC, (__u32)proxy_port << 16);
+	ctx_store_meta(ctx, CB_CT_STATE, (__u32)state->rev_nat_index << 16 |
+		       state->loopback);
+}
+
 static __always_inline int __per_packet_lb_svc_xlate_6(void *ctx, struct ipv6hdr *ip6,
 						       __s8 *ext_err)
 {
@@ -302,9 +376,8 @@ skip_service_lookup:
 	lb6_ctx_store_state(ctx, &ct_state_new, proxy_port);
 	return tail_call_internal(ctx, CILIUM_CALL_IPV6_CT_EGRESS, ext_err);
 }
+#endif /* ENABLE_PER_PACKET_LB */
 #endif /* ENABLE_IPV6 */
-
-#endif
 
 #if defined(ENABLE_ARP_PASSTHROUGH) && defined(ENABLE_ARP_RESPONDER)
 #error "Either ENABLE_ARP_PASSTHROUGH or ENABLE_ARP_RESPONDER can be defined"

bpf/Makefile
@@ -122,11 +122,14 @@ endif
 LXC_OPTIONS = \
 	 -DSKIP_DEBUG: \
 	 -DENABLE_IPV4: \
+	 -DENABLE_IPV4:-DENABLE_SOCKET_LB_FULL: \
 	 -DENABLE_IPV6: \
+	 -DENABLE_IPV6:-DENABLE_SOCKET_LB_FULL: \
 	 -DENABLE_IPV4:-DENABLE_IPV6:-DPOLICY_VERDICT_NOTIFY: \
 	 -DENABLE_IPV4:-DENABLE_IPV6:-DPOLICY_VERDICT_NOTIFY:-DENABLE_HOST_FIREWALL: \
 	 -DENABLE_IPV4:-DENABLE_IPV6:-DPOLICY_VERDICT_NOTIFY:-DENABLE_ROUTING: \
 	 -DENABLE_IPV4:-DENABLE_IPV6:-DPOLICY_VERDICT_NOTIFY:-DENABLE_HOST_FIREWALL:-DENABLE_ROUTING: \
+	 -DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_SOCKET_LB_FULL: \
 	 -DENABLE_IPV4:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY: \
 	 -DENABLE_IPV4:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY:-DENABLE_IPV4_FRAGMENTS: \
 	 -DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DPOLICY_VERDICT_NOTIFY: \

bpf/lib/lb.h
@@ -1415,49 +1415,6 @@ lb6_dnat_request(struct __ctx_buff *ctx, const struct lb6_backend *backend,
 			 backend, ipfrag_has_l4_header(fraginfo));
 }
 
-/* lb6_ctx_store_state() stores per packet load balancing state to be picked
- * up on the continuation tail call.
- * Note that the IP headers are already xlated and the tuple is re-initialized
- * from the xlated headers before restoring state.
- * NOTE: if lb_skip_l4_dnat() this is not the case as xlate is skipped. We
- * lose the updated tuple daddr in that case.
- */
-static __always_inline void lb6_ctx_store_state(struct __ctx_buff *ctx,
-						const struct ct_state *state,
-					       __u16 proxy_port)
-{
-	ctx_store_meta(ctx, CB_PROXY_MAGIC, (__u32)proxy_port << 16);
-	ctx_store_meta(ctx, CB_CT_STATE, (__u32)state->rev_nat_index << 16 |
-#ifdef USE_LOOPBACK_LB
-		state->loopback);
-#else
-		0);
-#endif /* USE_LOOPBACK_LB */
-}
-
-/* lb6_ctx_restore_state() restores per packet load balancing state from the
- * previous tail call.
- * tuple->flags does not need to be restored, as it will be reinitialized from
- * the packet.
- */
-static __always_inline void lb6_ctx_restore_state(struct __ctx_buff *ctx,
-						  struct ct_state *state,
-						 __u16 *proxy_port,
-						 bool clear)
-{
-	__u32 meta = clear ? ctx_load_and_clear_meta(ctx, CB_CT_STATE) :
-				       ctx_load_meta(ctx, CB_CT_STATE);
-
-#ifdef USE_LOOPBACK_LB
-	if (meta & 1)
-		state->loopback = 1;
-#endif
-	state->rev_nat_index = meta >> 16;
-
-	*proxy_port = clear ? (ctx_load_and_clear_meta(ctx, CB_PROXY_MAGIC) >> 16) :
-			      (ctx_load_meta(ctx, CB_PROXY_MAGIC) >> 16);
-}
-
 #else
 
 /* Stubs for v4-in-v6 socket cgroup hook case when only v4 is enabled to avoid
@@ -2176,54 +2133,6 @@ lb4_dnat_request(struct __ctx_buff *ctx, const struct lb4_backend *backend,
 	return lb4_xlate(ctx, &new_saddr, &saddr,
 			 tuple->nexthdr, l3_off, l4_off, key,
 			 backend, ipfrag_has_l4_header(fraginfo));
-}
-
-/* lb4_ctx_store_state() stores per packet load balancing state to be picked
- * up on the continuation tail call.
- * Note that the IP headers are already xlated and the tuple is re-initialized
- * from the xlated headers before restoring state.
- * NOTE: if lb_skip_l4_dnat() this is not the case as xlate is skipped. We
- * lose the updated tuple daddr in that case.
- */
-static __always_inline void lb4_ctx_store_state(struct __ctx_buff *ctx,
-						const struct ct_state *state,
-					       __u16 proxy_port, __u32 cluster_id)
-{
-	ctx_store_meta(ctx, CB_PROXY_MAGIC, (__u32)proxy_port << 16);
-	ctx_store_meta(ctx, CB_CT_STATE, (__u32)state->rev_nat_index << 16 |
-#ifdef USE_LOOPBACK_LB
-		       state->loopback);
-#else
-		       0);
-#endif
-	ctx_store_meta(ctx, CB_CLUSTER_ID_EGRESS, cluster_id);
-}
-
-/* lb4_ctx_restore_state() restores per packet load balancing state from the
- * previous tail call.
- * tuple->flags does not need to be restored, as it will be reinitialized from
- * the packet.
- */
-static __always_inline void
-lb4_ctx_restore_state(struct __ctx_buff *ctx, struct ct_state *state,
-		       __u16 *proxy_port, __u32 *cluster_id __maybe_unused,
-		       bool clear)
-{
-	__u32 meta = clear ? ctx_load_and_clear_meta(ctx, CB_CT_STATE) :
-			     ctx_load_meta(ctx, CB_CT_STATE);
-#ifdef USE_LOOPBACK_LB
-	if (meta & 1)
-		state->loopback = 1;
-#endif
-	state->rev_nat_index = meta >> 16;
-
-	*proxy_port = clear ? (ctx_load_and_clear_meta(ctx, CB_PROXY_MAGIC) >> 16) :
-			      (ctx_load_meta(ctx, CB_PROXY_MAGIC) >> 16);
-
-#ifdef ENABLE_CLUSTER_AWARE_ADDRESSING
-	*cluster_id = clear ? ctx_load_and_clear_meta(ctx, CB_CLUSTER_ID_EGRESS) :
-			      ctx_load_meta(ctx, CB_CLUSTER_ID_EGRESS);
-#endif
 }
 
 /* Because we use tail calls and this file is included in bpf_sock.h */

commit 0ca1bc8f5e5f9bce3863867d6ed42f557cc60044

bpf: lxc: move lb4_ctx_restore_state() comment to the correct place
When the function was moved in the referenced commit its comment ended
up in the wrong place above __per_packet_lb_svc_xlate_4(). Move it above
lb4_ctx_restore_state().

AIL: 0
Fixes: 64e171e9e52b ("bpf: lb: move lb*_ctx_store_state() helpers to bpf_lxc")
Signed-off-by: Tobias Klauser <tobias@cilium.io>

bpf/bpf_lxc.c
@@ -108,6 +108,11 @@ struct {
 } cilium_nodeport_nat_buffer __section_maps_btf;
 
 #ifdef ENABLE_IPV4
+/* lb4_ctx_restore_state() restores per packet load balancing state from the
+ * previous tail call.
+ * tuple->flags does not need to be restored, as it will be reinitialized from
+ * the packet.
+ */
 static __always_inline void
 lb4_ctx_restore_state(struct __ctx_buff *ctx, struct ct_state *state,
 		      __u16 *proxy_port, __u32 *cluster_id __maybe_unused,
@@ -144,11 +149,6 @@ lb4_ctx_store_state(struct __ctx_buff *ctx, const struct ct_state *state,
 	ctx_store_meta(ctx, CB_CLUSTER_ID_EGRESS, cluster_id);
 }
 
-/* lb4_ctx_restore_state() restores per packet load balancing state from the
- * previous tail call.
- * tuple->flags does not need to be restored, as it will be reinitialized from
- * the packet.
- */
 static __always_inline int __per_packet_lb_svc_xlate_4(void *ctx, struct iphdr *ip4,
 						       __s8 *ext_err)
 {