9c9c8d0c8004cf0757c974a2481355f6a5018a21
Arthur Outhenin-Chalandre <git@mrfreezeex.fr>
2026-09-06 23:12:11 +0200

    shortener: add multiple kubernetes shortener functions
    Most Kubernetes object name are actually restricted to 253 characters
    and not 63 (see [1]). Let's change add a new shortener function and rename
    the existing function to reflect that.
    
    [1]: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/
    
    Signed-off-by: Arthur Outhenin-Chalandre <git@mrfreezeex.fr>

--- operator/pkg/gateway-api/status_gateway.go
+++ operator/pkg/gateway-api/status_gateway.go
@@ -93,7 +93,7 @@ func (m *GatewayStatusManager) SetAddressStatus(ctx context.Context, gw *gateway
 
 	svcList := &corev1.ServiceList{}
 	if err := m.client.List(ctx, svcList, client.MatchingLabels{
-		owningGatewayLabel: shortener.ShortenK8sResourceName(gw.GetName()),
+		owningGatewayLabel: shortener.ShortenDNSLabelK8sName(gw.GetName()),
 	}, client.InNamespace(gw.GetNamespace())); err != nil {
 		setGatewayProgrammed(gw, metav1.ConditionFalse, "Address is not ready, failed to load services", gatewayv1.GatewayReasonAddressNotAssigned)
 		return fmt.Errorf("failed to load services: %w", err)
@@ -220,7 +220,7 @@ func (m *GatewayStatusManager) SetStaticAddressStatus(ctx context.Context, gw *g
 	}
 	svcList := &corev1.ServiceList{}
 	if err := m.client.List(ctx, svcList, client.MatchingLabels{
-		owningGatewayLabel: shortener.ShortenK8sResourceName(gw.GetName()),
+		owningGatewayLabel: shortener.ShortenDNSLabelK8sName(gw.GetName()),
 	}, client.InNamespace(gw.GetNamespace())); err != nil {
 		return fmt.Errorf("failed to load services: %w", err)
 	}
