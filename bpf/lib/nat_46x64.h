cilium/cilium

commit 4c56e10ba0697b8db4957e8601215241b5471fdb

bpf: ipv4: add L3 csum wrappers
l3_csum_replace() can be used in two ways:
a) updating the packet's checksum after modifying a specific header field
   to a new value.
b) updating the packet's checksum with an opaque diff, eg. after updating
   multiple fields.

Add corresponding wrappers, so we don't have to worry about setting up all
the parameter correctly.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nat_46x64.h
@@ -346,7 +346,7 @@ static __always_inline int ipv6_to_ipv4(struct __ctx_buff *ctx,
 	if (ctx_store_bytes(ctx, nh_off, &v4, sizeof(v4), 0) < 0 ||
 	    ctx_store_bytes(ctx, nh_off - 2, &protocol, 2, 0) < 0)
 		return DROP_WRITE_ERROR;
-	if (l3_csum_replace(ctx, nh_off + csum_off, 0, csum, 0) < 0)
+	if (ipv4_csum_update_by_diff(ctx, nh_off, csum) < 0)
 		return DROP_CSUM_L3;
 	if (v6.nexthdr == IPPROTO_ICMPV6) {
 		__be32 csum1 = 0;

bpf/bpf_host.c
@@ -712,8 +712,7 @@ do_netdev_encrypt_pools(struct __ctx_buff *ctx __maybe_unused)
 		ret = DROP_WRITE_ERROR;
 		goto drop_err;
 	}
-	if (l3_csum_replace(ctx, ETH_HLEN + offsetof(struct iphdr, check),
-	    0, sum, 0) < 0) {
+	if (ipv4_csum_update_by_diff(ctx, ETH_HLEN, sum) < 0) {
 		ret = DROP_CSUM_L3;
 		goto drop_err;
 	}
@@ -730,8 +729,7 @@ do_netdev_encrypt_pools(struct __ctx_buff *ctx __maybe_unused)
 		ret = DROP_WRITE_ERROR;
 		goto drop_err;
 	}
-	if (l3_csum_replace(ctx, ETH_HLEN + offsetof(struct iphdr, check),
-	    0, sum, 0) < 0) {
+	if (ipv4_csum_update_by_diff(ctx, ETH_HLEN, sum) < 0) {
 		ret = DROP_CSUM_L3;
 		goto drop_err;
 	}

bpf/lib/ipv4.h
@@ -32,6 +32,21 @@ struct {
 } IPV4_FRAG_DATAGRAMS_MAP __section_maps_btf;
 #endif
 
+static __always_inline int
+ipv4_csum_update_by_value(struct __ctx_buff *ctx, int l3_off, __u64 old_val,
+			  __u64 new_val, __u32 len)
+{
+	return l3_csum_replace(ctx, l3_off + offsetof(struct iphdr, check),
+			       old_val, new_val, len);
+}
+
+static __always_inline int
+ipv4_csum_update_by_diff(struct __ctx_buff *ctx, int l3_off, __u64 diff)
+{
+	return l3_csum_replace(ctx, l3_off + offsetof(struct iphdr, check),
+			       0, diff, 0);
+}
+
 static __always_inline int ipv4_load_daddr(struct __ctx_buff *ctx, int off,
 					   __u32 *dst)
 {
@@ -48,7 +63,7 @@ static __always_inline int ipv4_dec_ttl(struct __ctx_buff *ctx, int off,
 
 	new_ttl = ttl - 1;
 	/* l3_csum_replace() takes at min 2 bytes, zero extended. */
-	l3_csum_replace(ctx, off + offsetof(struct iphdr, check), ttl, new_ttl, 2);
+	ipv4_csum_update_by_value(ctx, off, ttl, new_ttl, 2);
 	ctx_store_bytes(ctx, off + offsetof(struct iphdr, ttl), &new_ttl, sizeof(new_ttl), 0);
 
 	return 0;

bpf/lib/lb.h
@@ -1093,7 +1093,7 @@ static __always_inline int __lb4_rev_nat(struct __ctx_buff *ctx, int l3_off, int
 		return DROP_WRITE_ERROR;
 
 	sum = csum_diff(&old_sip, 4, &new_sip, 4, sum);
-	if (l3_csum_replace(ctx, l3_off + offsetof(struct iphdr, check), 0, sum, 0) < 0)
+	if (ipv4_csum_update_by_diff(ctx, l3_off, sum) < 0)
 		return DROP_CSUM_L3;
 
 	if (csum_off->offset &&
@@ -1388,8 +1388,7 @@ lb4_xlate(struct __ctx_buff *ctx, __be32 *new_daddr, __be32 *new_saddr __maybe_u
 		sum = csum_diff(old_saddr, 4, new_saddr, 4, sum);
 	}
 #endif /* DISABLE_LOOPBACK_LB */
-	if (l3_csum_replace(ctx, l3_off + offsetof(struct iphdr, check),
-			    0, sum, 0) < 0)
+	if (ipv4_csum_update_by_diff(ctx, l3_off, sum) < 0)
 		return DROP_CSUM_L3;
 	if (csum_off->offset) {
 		if (csum_l4_replace(ctx, l4_off, csum_off, 0, sum,

bpf/lib/nat.h
@@ -423,8 +423,7 @@ static __always_inline int snat_v4_rewrite_egress(struct __ctx_buff *ctx,
 	if (ctx_store_bytes(ctx, ETH_HLEN + offsetof(struct iphdr, saddr),
 			    &state->to_saddr, 4, 0) < 0)
 		return DROP_WRITE_ERROR;
-	if (l3_csum_replace(ctx, ETH_HLEN + offsetof(struct iphdr, check),
-			    0, sum, 0) < 0)
+	if (ipv4_csum_update_by_diff(ctx, ETH_HLEN, sum) < 0)
 		return DROP_CSUM_L3;
 	if (tuple->nexthdr == IPPROTO_ICMP)
 		sum = sum_l4;
@@ -489,8 +488,7 @@ static __always_inline int snat_v4_rewrite_ingress(struct __ctx_buff *ctx,
 	if (ctx_store_bytes(ctx, ETH_HLEN + offsetof(struct iphdr, daddr),
 			    &state->to_daddr, 4, 0) < 0)
 		return DROP_WRITE_ERROR;
-	if (l3_csum_replace(ctx, ETH_HLEN + offsetof(struct iphdr, check),
-			    0, sum, 0) < 0)
+	if (ipv4_csum_update_by_diff(ctx, ETH_HLEN, sum) < 0)
 		return DROP_CSUM_L3;
 	if (tuple->nexthdr == IPPROTO_ICMP)
 		sum = sum_l4;

bpf/lib/nodeport.h
@@ -1399,8 +1399,7 @@ static __always_inline int dsr_set_ipip4(struct __ctx_buff *ctx,
 	if (ctx_store_bytes(ctx, l3_off + offsetof(struct iphdr, saddr),
 			    &tp_new.saddr, 8, 0) < 0)
 		return DROP_WRITE_ERROR;
-	if (l3_csum_replace(ctx, l3_off + offsetof(struct iphdr, check),
-			    0, sum, 0) < 0)
+	if (ipv4_csum_update_by_diff(ctx, l3_off, sum) < 0)
 		return DROP_CSUM_L3;
 	return 0;
 }
@@ -1457,8 +1456,7 @@ static __always_inline int dsr_set_opt4(struct __ctx_buff *ctx,
 	if (ctx_store_bytes(ctx, ETH_HLEN + sizeof(*ip4),
 			    &opt, sizeof(opt), 0) < 0)
 		return DROP_INVALID;
-	if (l3_csum_replace(ctx, ETH_HLEN + offsetof(struct iphdr, check),
-			    0, sum, 0) < 0)
+	if (ipv4_csum_update_by_diff(ctx, ETH_HLEN, sum) < 0)
 		return DROP_CSUM_L3;
 
 	return 0;

commit 69c1166aa8c9f30ccc704e1f0957cb21839e3e23

bpf: nat46: remove dead csum_off assignment
csum_off is assigned at nat_46x64.h but immediately overwritten
by get_csum_offset(v4.protocol) before being read. It has been
dead since 4c56e10 that refactored the code to use
ipv4_csum_update_by_diff, making the previous value of csum_off
unused.

AIL:2

Fixes: 4c56e10ba069 ("bpf: ipv4: add L3 csum wrappers")
Signed-off-by: fds7280 <ayushmanrout27@gmail.com>

bpf/lib/nat_46x64.h
@@ -341,7 +341,6 @@ static __always_inline int ipv6_to_ipv4(struct __ctx_buff *ctx,
 		v4.protocol = v6.nexthdr;
 	v4.ttl = v6.hop_limit;
 	v4.tot_len = bpf_htons(bpf_ntohs(v6.payload_len) + sizeof(v4));
-	csum_off = offsetof(struct iphdr, check);
 	csum = csum_diff(NULL, 0, &v4, sizeof(v4), csum);
 	if (ctx_change_proto(ctx, bpf_htons(ETH_P_IP), 0) < 0)
 		return DROP_WRITE_ERROR;