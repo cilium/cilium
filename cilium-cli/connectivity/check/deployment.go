cilium/cilium

commit 9ae977cec56c879b627f84b90220fd2372ec4bd6

ci: Add global namespace support for connectivity tests
Add support for testing clustermesh with defaultGlobalNamespace=false:
- Add defaultGlobalNamespace to test non-global namespace behavior in
  conformance-clustermesh.yaml
- Annotate namespaces in deployment.yaml when
  defaultGlobalNamespace=false

Signed-off-by: Anubhab Majumdar <anmajumdar@microsoft.com>

cilium-cli/connectivity/check/deployment.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cilium/cilium/cilium-cli/defaults"
 	"github.com/cilium/cilium/cilium-cli/k8s"
 	"github.com/cilium/cilium/cilium-cli/utils/features"
+	"github.com/cilium/cilium/pkg/annotation"
 	k8sconst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	slimcorev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
@@ -706,23 +707,38 @@ func (ct *ConnectivityTest) deployNamespace(ctx context.Context) error {
 			}
 		}
 
-		_, err := client.GetNamespace(ctx, ct.params.TestNamespace, metav1.GetOptions{})
+		namespace, err := client.GetNamespace(ctx, ct.params.TestNamespace, metav1.GetOptions{})
 		if err != nil {
 			ct.Logf("✨ [%s] Creating namespace %s for connectivity check...", client.ClusterName(), ct.params.TestNamespace)
-			namespace := &corev1.Namespace{
+			namespace = &corev1.Namespace{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:        ct.params.TestNamespace,
 					Annotations: ct.params.NamespaceAnnotations,
 					Labels:      labels.Merge(ct.params.NamespaceLabels, appLabels),
 				},
 			}
+		}
+		if !ct.Features[features.DefaultGlobalNamespace].Enabled {
+			// Mark the namespace as global to ensure resources under this
+			// namespace are treated as global. This is required for
+			// multi-cluster tests.
+			if namespace.Annotations == nil {
+				namespace.Annotations = make(map[string]string)
+			}
+			namespace.Annotations[annotation.GlobalNamespace] = "true"
+		}
+		if err == nil { // Namespace already exists.
+			_, err = client.UpdateNamespace(ctx, namespace, metav1.UpdateOptions{})
+			if err != nil {
+				return fmt.Errorf("unable to update namespace %s: %w", ct.params.TestNamespace, err)
+			}
+		} else {
 			_, err = client.CreateNamespace(ctx, namespace, metav1.CreateOptions{})
 			if err != nil {
 				return fmt.Errorf("unable to create namespace %s: %w", ct.params.TestNamespace, err)
 			}
 		}
 	}
-
 	return nil
 }
 

.github/workflows/conformance-clustermesh.yaml
@@ -130,6 +130,7 @@ jobs:
             ciliumEndpointSlice: 'enabled'
             policy-default-local-cluster: true
             multipool-ipam: 'enabled'
+            defaultGlobalNamespace: false
 
           - name: '2'
             tunnel: 'disabled'
@@ -144,6 +145,7 @@ jobs:
             ciliumEndpointSlice: 'disabled'
             policy-default-local-cluster: false
             multipool-ipam: 'disabled'
+            defaultGlobalNamespace: true
 
           - name: '3'
             tunnel: 'disabled'
@@ -158,6 +160,7 @@ jobs:
             ciliumEndpointSlice: 'disabled'
             policy-default-local-cluster: true
             multipool-ipam: 'disabled'
+            defaultGlobalNamespace: false
 
           # IPsec encryption is currently not supported in case of ipv6-only clusters (#23553)
           # Wireguard encryption is currently affected by a bug in case of ipv6-only clusters (#23917)
@@ -174,6 +177,7 @@ jobs:
             ciliumEndpointSlice: 'disabled'
             policy-default-local-cluster: false
             multipool-ipam: 'disabled'
+            defaultGlobalNamespace: true
 
           - name: '5'
             tunnel: 'disabled'
@@ -186,6 +190,7 @@ jobs:
             ciliumEndpointSlice: 'disabled'
             policy-default-local-cluster: true
             multipool-ipam: 'disabled'
+            defaultGlobalNamespace: false
 
           - name: '6'
             tunnel: 'vxlan'
@@ -200,6 +205,7 @@ jobs:
             ciliumEndpointSlice: 'enabled'
             policy-default-local-cluster: false
             multipool-ipam: 'enabled'
+            defaultGlobalNamespace: true
 
           - name: '7'
             tunnel: 'geneve'
@@ -214,6 +220,7 @@ jobs:
             ciliumEndpointSlice: 'disabled'
             policy-default-local-cluster: true
             multipool-ipam: 'disabled'
+            defaultGlobalNamespace: false
 
           - name: '8'
             tunnel: 'vxlan'
@@ -228,6 +235,7 @@ jobs:
             ciliumEndpointSlice: 'disabled'
             policy-default-local-cluster: false
             multipool-ipam: 'disabled'
+            defaultGlobalNamespace: true
 
           - name: '9'
             tunnel: 'vxlan'
@@ -242,6 +250,7 @@ jobs:
             ciliumEndpointSlice: 'enabled'
             policy-default-local-cluster: true
             multipool-ipam: 'disabled'
+            defaultGlobalNamespace: false
 
           - name: '10'
             tunnel: 'vxlan'
@@ -254,6 +263,7 @@ jobs:
             ciliumEndpointSlice: 'disabled'
             policy-default-local-cluster: true
             multipool-ipam: 'disabled'
+            defaultGlobalNamespace: true
 
     steps:
       - name: Set commit status to pending
@@ -320,6 +330,7 @@ jobs:
             --helm-set=ciliumEndpointSlice.enabled=${{ matrix.ciliumEndpointSlice == 'enabled'}} \
             --helm-set=extraConfig.clustermesh-sync-timeout=5m \
             --helm-set=extraConfig.lb-init-wait-timeout=4m \
+            --helm-set=clustermesh.defaultGlobalNamespace=${{ matrix.defaultGlobalNamespace }} \
             "
 
           CILIUM_INSTALL_TUNNEL=" \

cilium-cli/k8s/client.go
@@ -304,6 +304,10 @@ func (c *Client) CreateNamespace(ctx context.Context, namespace *corev1.Namespac
 	return c.Clientset.CoreV1().Namespaces().Create(ctx, namespace, opts)
 }
 
+func (c *Client) UpdateNamespace(ctx context.Context, namespace *corev1.Namespace, opts metav1.UpdateOptions) (*corev1.Namespace, error) {
+	return c.Clientset.CoreV1().Namespaces().Update(ctx, namespace, opts)
+}
+
 func (c *Client) GetNamespace(ctx context.Context, namespace string, options metav1.GetOptions) (*corev1.Namespace, error) {
 	return c.Clientset.CoreV1().Namespaces().Get(ctx, namespace, options)
 }

cilium-cli/utils/features/features.go
@@ -125,6 +125,8 @@ const (
 	ExternalEnvoyProxy Feature = "external-envoy-proxy"
 
 	Ztunnel Feature = "enable-ztunnel"
+
+	DefaultGlobalNamespace Feature = "clustermesh-default-global-namespace"
 )
 
 // Feature is the name of a Cilium Feature (e.g. l7-proxy, cni chaining mode etc)
@@ -421,6 +423,10 @@ func (fs Set) ExtractFromConfigMap(cm *v1.ConfigMap) {
 
 	fs[Ztunnel] = Status{
 		Enabled: cm.Data["enable-ztunnel"] == "true",
+	}
+
+	fs[DefaultGlobalNamespace] = Status{
+		Enabled: cm.Data[string(DefaultGlobalNamespace)] == "true",
 	}
 }
 

commit b858baaddcc7580a16877d2ca6fe6c0349812b33

fix(cilium-cli/connectivity): fix race on test params map
In `(*ConnectivityTest).deployNamespace` the `ct.params.NamespaceAnnotations` map is shared among test runners, but was mutated during namespace setup when `clustermesh.defaultGlobalNamespace` is `false`. Fix by cloning the map.

Fixes: 9ae977cec56c ("ci: Add global namespace support for connectivity tests")

Signed-off-by: Andrea Lattuada <alattuad@isovalent.com>

cilium-cli/connectivity/check/deployment.go
@@ -752,7 +752,7 @@ func (ct *ConnectivityTest) deployNamespace(ctx context.Context, client *k8s.Cli
 		namespace = &corev1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:        namespaceName,
-				Annotations: ct.params.NamespaceAnnotations,
+				Annotations: maps.Clone(ct.params.NamespaceAnnotations),
 				Labels:      labels.Merge(ct.params.NamespaceLabels, appLabels),
 			},
 		}