b43286df27d65ffeba1e7f98356dd1821d49f609
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-09-18 09:59:12 +0200

    datapath: turn `ENABLE_DSR_BYUSER` into runtime config
    Migrate `ENABLE_DSR_BYUSER` config macro to runtime config.
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- pkg/datapath/loader/verifier_load_test.go
+++ pkg/datapath/loader/verifier_load_test.go
@@ -34,6 +34,7 @@ func setBasePermutations(t *config.Node) {
 	t.DebugLB = true
 	t.EventsMapRateLimit = 1000
 	t.EnableIdentityMark = true
+	t.EnableDSRByuser = true
 }
 
 func baseLXCPermutations() *loadPermutationBuilder {
