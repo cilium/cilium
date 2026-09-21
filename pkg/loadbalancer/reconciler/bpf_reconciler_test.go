cilium/cilium

commit 7874a618fd0e5cb06cd93ca78031c412638e4454

experimental: Add support for health checking
Add [Unhealthy] and [UnhealthyUpdatedAt] fields to [BackendParams] and allow
manipulating them via [Writer.UpdateBackendHealth]. With this we can
implement an active health checker that updates backend health and
marks them reconciled in the services BPF maps.

With active health checking we will need to restore the quarantined
state on restart. Restore it from the services map and keep the backend
quarantined until active health checker has updated it
([BackendParams.UnhealthyUpdatedAt] is non-zero).

Add testdata/quarantined.txtar to verify updating health and the restoration.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/loadbalancer/experimental/bpf_reconciler.go
@@ -10,7 +10,9 @@ import (
 	"iter"
 	"log/slog"
 	"net/netip"
+	"slices"
 	"sort"
+	"strings"
 
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
@@ -129,6 +131,12 @@ type BPFOps struct {
 	backendIDAlloc     idAllocator
 	restoredBackendIDs sets.Set[loadbalancer.BackendID]
 
+	// restoredQuarantinedBackends are backends that were quarantined for
+	// a specific frontend. This comes into play when we have active health checker.
+	// On restart we restore this information and use this until we get an update
+	// from a health checker ([Backend.UnhealthyUpdatedAt] is non-zero).
+	restoredQuarantinedBackends map[loadbalancer.L3n4Addr]sets.Set[loadbalancer.L3n4Addr]
+
 	// backendStates maps from backend address to associated state.
 	// This is used to track which frontends reference a specific backend
 	// in order to delete orphaned backeds.
@@ -180,42 +188,32 @@ func newBPFOps(p bpfOpsParams) *BPFOps {
 		return nil
 	}
 	ops := &BPFOps{
-		cfg:                p.ExtCfg,
-		maglev:             p.Maglev,
-		serviceIDAlloc:     newIDAllocator(firstFreeServiceID, maxSetOfServiceID),
-		restoredServiceIDs: sets.New[loadbalancer.ID](),
-		backendIDAlloc:     newIDAllocator(firstFreeBackendID, maxSetOfBackendID),
-		restoredBackendIDs: sets.New[loadbalancer.BackendID](),
-		log:                p.Log,
-		backendStates:      map[loadbalancer.L3n4Addr]backendState{},
-		backendReferences:  map[loadbalancer.L3n4Addr]sets.Set[loadbalancer.L3n4Addr]{},
-		nodePortAddrByPort: map[nodePortAddrKey][]netip.Addr{},
-		prevSourceRanges:   map[loadbalancer.L3n4Addr]sets.Set[netip.Prefix]{},
-		LBMaps:             p.LBMaps,
-		db:                 p.DB,
-		nodeAddrs:          p.NodeAddresses,
+		cfg:       p.ExtCfg,
+		maglev:    p.Maglev,
+		log:       p.Log,
+		LBMaps:    p.LBMaps,
+		db:        p.DB,
+		nodeAddrs: p.NodeAddresses,
 	}
 	p.Lifecycle.Append(cell.Hook{OnStart: ops.start})
 	return ops
 }
 
-func (ops *BPFOps) start(ctx cell.HookContext) (err error) {
-	// Restore the ID allocations from the BPF maps in order to reuse
-	// them and thus avoiding traffic disruptions.
-	err = ops.LBMaps.DumpService(func(key lbmap.ServiceKey, value lbmap.ServiceValue) {
-		key = key.ToHost()
-		value = value.ToHost()
-		if key.GetBackendSlot() != 0 {
-			return
-		}
-		id := loadbalancer.ID(value.GetRevNat())
-		ops.serviceIDAlloc.addID(svcKeyToAddr(key), id)
-		ops.restoredServiceIDs.Insert(id)
-	})
-	if err != nil {
-		return fmt.Errorf("restore service ids: %w", err)
-	}
+func (ops *BPFOps) start(cell.HookContext) (err error) {
+	return ops.resetAndRestore()
+}
 
+func (ops *BPFOps) resetAndRestore() (err error) {
+	ops.serviceIDAlloc = newIDAllocator(firstFreeServiceID, maxSetOfServiceID)
+	ops.restoredServiceIDs = sets.New[loadbalancer.ID]()
+	ops.backendIDAlloc = newIDAllocator(firstFreeBackendID, maxSetOfBackendID)
+	ops.restoredBackendIDs = sets.New[loadbalancer.BackendID]()
+	ops.backendStates = map[loadbalancer.L3n4Addr]backendState{}
+	ops.backendReferences = map[loadbalancer.L3n4Addr]sets.Set[loadbalancer.L3n4Addr]{}
+	ops.nodePortAddrByPort = map[nodePortAddrKey][]netip.Addr{}
+	ops.prevSourceRanges = map[loadbalancer.L3n4Addr]sets.Set[netip.Prefix]{}
+
+	// Restore backend IDs
 	err = ops.LBMaps.DumpBackend(func(key lbmap.BackendKey, value lbmap.BackendValue) {
 		value = value.ToHost()
 		ops.backendIDAlloc.addID(beValueToAddr(value), loadbalancer.ID(key.GetID()))
@@ -225,6 +223,49 @@ func (ops *BPFOps) start(ctx cell.HookContext) (err error) {
 		return fmt.Errorf("restore backend ids: %w", err)
 	}
 
+	// Gather all services key'd by address.
+	serviceSlots := map[loadbalancer.L3n4Addr][]lbmap.ServiceValue{}
+	err = ops.LBMaps.DumpService(func(key lbmap.ServiceKey, value lbmap.ServiceValue) {
+		key = key.ToHost()
+		value = value.ToHost()
+		addr := svcKeyToAddr(key)
+		s := slices.Grow(serviceSlots[addr], key.GetBackendSlot()+1)
+		s = s[:max(len(s), key.GetBackendSlot()+1)]
+		s[key.GetBackendSlot()] = value
+		serviceSlots[addr] = s
+	})
+	if err != nil {
+		return fmt.Errorf("restore service ids: %w", err)
+	}
+
+	for addr, slots := range serviceSlots {
+		// Restore the ID allocations from the BPF maps in order to reuse
+		// them and thus avoiding traffic disruptions.
+		master := slots[0]
+		if master == nil {
+			continue
+		}
+
+		id := loadbalancer.ID(master.GetRevNat())
+		ops.serviceIDAlloc.addID(addr, id)
+		ops.restoredServiceIDs.Insert(id)
+
+		if master.GetQCount() > 0 && len(slots) == 1+master.GetCount()+master.GetQCount() {
+			if ops.restoredQuarantinedBackends == nil {
+				ops.restoredQuarantinedBackends = make(map[loadbalancer.L3n4Addr]sets.Set[loadbalancer.L3n4Addr])
+			}
+			backends := ops.restoredQuarantinedBackends[addr]
+			if backends == nil {
+				backends = sets.New[loadbalancer.L3n4Addr]()
+				ops.restoredQuarantinedBackends[addr] = backends
+			}
+			for _, slot := range slots[1+master.GetCount():] {
+				if addr := ops.backendIDAlloc.entitiesID[loadbalancer.ID(slot.GetBackendID())]; addr != nil {
+					backends.Insert(addr.L3n4Addr)
+				}
+			}
+		}
+	}
 	return nil
 }
 
@@ -274,7 +315,28 @@ func (ops *BPFOps) Delete(_ context.Context, _ statedb.ReadTxn, fe *Frontend) er
 			ops.log.Warn("no nodePortAddrs", logfields.Port, fe.Address.Port)
 		}
 	}
+
 	return nil
+}
+
+func (ops *BPFOps) deleteRestoredQuarantinedBackends(fe loadbalancer.L3n4Addr, bes ...loadbalancer.L3n4Addr) {
+	if ops.restoredQuarantinedBackends == nil {
+		return
+	}
+	if len(bes) == 0 {
+		delete(ops.restoredQuarantinedBackends, fe)
+	} else {
+		backends := ops.restoredQuarantinedBackends[fe]
+		if len(backends) > 0 {
+			backends.Delete(bes...)
+		}
+		if len(backends) == 0 {
+			delete(ops.restoredQuarantinedBackends, fe)
+		}
+	}
+	if len(ops.restoredQuarantinedBackends) == 0 {
+		ops.restoredQuarantinedBackends = nil
+	}
 }
 
 func (ops *BPFOps) deleteFrontend(fe *Frontend) error {
@@ -290,6 +352,9 @@ func (ops *BPFOps) deleteFrontend(fe *Frontend) error {
 		logfields.ID, feID,
 		logfields.Address, fe.Address,
 	)
+
+	// Drop any restored quarantine state
+	ops.deleteRestoredQuarantinedBackends(fe.Address)
 
 	// Delete Maglev.
 	if ops.useMaglev(fe) {
@@ -398,6 +463,13 @@ func (ops *BPFOps) pruneServiceMaps() error {
 				logfields.ID, svcValue.GetRevNat(),
 				logfields.Address, addr)
 			toDelete = append(toDelete, svcKey.ToNetwork())
+
+			// Drop restored quarantined state
+			if svcKey.GetBackendSlot() > 0 {
+				if beAddr, found := ops.backendIDAlloc.entitiesID[loadbalancer.ID(svcValue.GetBackendID())]; found {
+					ops.deleteRestoredQuarantinedBackends(addr, beAddr.L3n4Addr)
+				}
+			}
 		}
 	}
 	if err := ops.LBMaps.DumpService(svcCB); err != nil {
@@ -674,7 +746,7 @@ func (ops *BPFOps) updateFrontend(fe *Frontend) error {
 	svcVal.SetRevNat(int(feID))
 
 	// Gather backends for the service
-	orderedBackends := sortedBackends(fe)
+	orderedBackends := ops.sortedBackends(fe)
 
 	// Clean up any orphan backends to make room for new backends
 	backendAddrs := sets.New[loadbalancer.L3n4Addr]()
@@ -684,6 +756,7 @@ func (ops *BPFOps) updateFrontend(fe *Frontend) error {
 
 	for _, orphanState := range ops.orphanBackends(fe.Address, backendAddrs) {
 		ops.log.Debug("Delete orphan backend", logfields.Address, orphanState.addr)
+		ops.deleteRestoredQuarantinedBackends(fe.Address, orphanState.addr)
 		if err := ops.deleteBackend(orphanState.addr.IsIPv6(), orphanState.id); err != nil {
 			return fmt.Errorf("delete backend: %w", err)
 		}
@@ -758,7 +831,19 @@ func (ops *BPFOps) updateFrontend(fe *Frontend) error {
 			}
 		}
 
-		switch be.State {
+		if !be.UnhealthyUpdatedAt.IsZero() {
+			ops.deleteRestoredQuarantinedBackends(fe.Address, be.L3n4Addr)
+		}
+
+		state := be.State
+		if be.Unhealthy {
+			// We only care about [be.Unhealthy] for the Count/QCount and not for
+			// the state in the backend maps as the backend might be healthy for some
+			// service and unhealthy for another.
+			state = loadbalancer.BackendStateQuarantined
+		}
+
+		switch state {
 		case loadbalancer.BackendStateActive:
 			activeCount++
 		case loadbalancer.BackendStateTerminating:
@@ -1140,14 +1225,23 @@ func (ops *BPFOps) computeMaglevTable(bes []backendWithRevision) ([]loadbalancer
 //
 // Backends are sorted to deterministically to keep the order stable in BPF maps
 // when updating.
-func sortedBackends(fe *Frontend) []backendWithRevision {
+func (ops *BPFOps) sortedBackends(fe *Frontend) []backendWithRevision {
+	quarantined := ops.restoredQuarantinedBackends[fe.Address]
+
 	bes := []backendWithRevision{}
 	for be, rev := range fe.Backends {
+		if be.UnhealthyUpdatedAt.IsZero() && quarantined.Has(be.L3n4Addr) {
+			be.Unhealthy = true
+		}
 		bes = append(bes, backendWithRevision{&be, rev})
 	}
 	sort.Slice(bes, func(i, j int) bool {
 		a, b := bes[i], bes[j]
 		switch {
+		case !a.Unhealthy && b.Unhealthy:
+			return true
+		case a.Unhealthy && !b.Unhealthy:
+			return false
 		case a.State < b.State:
 			return true
 		case a.State > b.State:
@@ -1164,6 +1258,23 @@ func sortedBackends(fe *Frontend) []backendWithRevision {
 		}
 	})
 	return bes
+}
+
+// stateSummary returns a multi-line summary of the internal state.
+// Used in tests.
+func (ops *BPFOps) stateSummary() string {
+	var b strings.Builder
+
+	fmt.Fprintf(&b, "serviceIDs: %d\n", len(ops.serviceIDAlloc.entities))
+	fmt.Fprintf(&b, "restoredServiceIDs: %d\n", len(ops.restoredServiceIDs))
+	fmt.Fprintf(&b, "backendIDs: %d\n", len(ops.backendIDAlloc.entities))
+	fmt.Fprintf(&b, "restoredBackendIDs: %d\n", len(ops.restoredBackendIDs))
+	fmt.Fprintf(&b, "backendStates: %d\n", len(ops.backendStates))
+	fmt.Fprintf(&b, "backendReferences: %d\n", len(ops.backendReferences))
+	fmt.Fprintf(&b, "nodePortAddrByPort: %d\n", len(ops.nodePortAddrByPort))
+	fmt.Fprintf(&b, "prevSourceRanges: %d\n", len(ops.prevSourceRanges))
+	fmt.Fprintf(&b, "restoredQuarantines: %d\n", len(ops.restoredQuarantinedBackends))
+	return b.String()
 }
 
 // idAllocator contains an internal state of the ID allocator.

pkg/loadbalancer/experimental/backend.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/source"
+	"github.com/cilium/cilium/pkg/time"
 )
 
 const (
@@ -44,6 +45,16 @@ type BackendParams struct {
 
 	// State of the backend, e.g. active, quarantined or terminating.
 	State loadbalancer.BackendState
+
+	// Unhealthy marks a backend as unhealthy and overrides [State] to mark the backend
+	// as quarantined. We require a separate field for active health checking to merge
+	// with the original source of this backend. Negative is used here to allow the
+	// zero value to mean that the backend is healthy.
+	Unhealthy bool
+
+	// UnhealthyUpdatedAt is the timestamp for when [Unhealthy] was last updated. Zero
+	// value if never updated.
+	UnhealthyUpdatedAt time.Time
 }
 
 // Backend is a composite of the per-service backend instances that share the same
@@ -147,10 +158,14 @@ func showInstances(be *Backend) string {
 	for k, inst := range be.PreferredInstances() {
 		b.WriteString(k.ServiceName.String())
 
-		if inst.State != loadbalancer.BackendStateActive {
+		if inst.State != loadbalancer.BackendStateActive || inst.Unhealthy {
 			b.WriteString(" [")
-			s, _ := inst.State.String()
-			b.WriteString(s)
+			if inst.Unhealthy {
+				b.WriteString("unhealthy")
+			} else {
+				s, _ := inst.State.String()
+				b.WriteString(s)
+			}
 			b.WriteRune(']')
 		}
 		if len(inst.PortNames) > 0 {

pkg/loadbalancer/experimental/script_test.go
@@ -14,9 +14,11 @@ import (
 	"os"
 	"slices"
 	"sort"
+	"strconv"
 	"strings"
 	"testing"
 
+	uhive "github.com/cilium/hive"
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/hivetest"
 	"github.com/cilium/hive/script"
@@ -33,6 +35,7 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/testutils"
 	"github.com/cilium/cilium/pkg/k8s/version"
+	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/maglev"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
@@ -100,7 +103,11 @@ func TestScript(t *testing.T) {
 							LoadBalancerAlgorithmAnnotation: cfg.LoadBalancerAlgorithmAnnotation,
 						}
 					},
+					func(ops *BPFOps, w *Writer) uhive.ScriptCmdsOut {
+						return uhive.NewScriptCmds(testCommands{w, ops}.cmds())
+					},
 				),
+
 				cell.Invoke(statedb.RegisterTable[tables.NodeAddress]),
 			)
 
@@ -171,3 +178,70 @@ var httpGetCmd = script.Command(
 		return nil, err
 	},
 )
+
+type testCommands struct {
+	w   *Writer
+	ops *BPFOps
+}
+
+func (tc testCommands) cmds() map[string]script.Cmd {
+	return map[string]script.Cmd{
+		"test/update-backend-health": tc.updateHealth(),
+		"test/bpfops-reset":          tc.opsReset(),
+		"test/bpfops-summary":        tc.opsSummary(),
+	}
+}
+
+func (tc testCommands) updateHealth() script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "Update backend healthyness",
+			Args:    "service-name backend-addr healthy",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			if len(args) != 3 {
+				return nil, fmt.Errorf("%w: expected service name, backend address and health", script.ErrUsage)
+			}
+			ns, name, _ := strings.Cut(args[0], "/")
+			svc := loadbalancer.ServiceName{Namespace: ns, Name: name}
+
+			var beAddr loadbalancer.L3n4Addr
+			if err := beAddr.ParseFromString(args[1]); err != nil {
+				return nil, err
+			}
+
+			healthy, err := strconv.ParseBool(args[2])
+			if err != nil {
+				return nil, err
+			}
+
+			txn := tc.w.WriteTxn()
+			defer txn.Commit()
+
+			_, err = tc.w.UpdateBackendHealth(txn, svc, beAddr, healthy)
+			return nil, err
+		})
+}
+
+func (tc testCommands) opsReset() script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "Reset and restart BPF ops",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			return nil, tc.ops.resetAndRestore()
+		})
+}
+
+func (tc testCommands) opsSummary() script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "Write out summary of BPFOps state",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			return func(s *script.State) (stdout string, stderr string, err error) {
+				stdout = tc.ops.stateSummary()
+				return
+			}, nil
+		})
+}

pkg/loadbalancer/experimental/testdata/quarantined.txtar
@@ -1,0 +1,200 @@
+#! --enable-experimental-lb --lb-test-fault-probability=0.0
+#
+# Test the infra for implementing active backend health checking and the restoration
+# of quarantined backends.
+
+hive/start
+db/initialized
+
+# Add our test service and backends.
+k8s/add service.yaml endpointslice.yaml
+db/cmp services services.table
+db/cmp backends backends_healthy1.table
+
+# Mark the backend as unhealthy (for this service)
+test/update-backend-health test/echo 10.244.1.2:80/TCP false
+db/cmp backends backends_unhealthy.table
+
+# Check BPF maps
+lb/maps-dump maps.actual
+* grep 'SLOT=0.*COUNT=1 QCOUNT=1' maps1.expected
+* cmp maps.actual maps1.expected
+
+# Take a snapshot of the BPF maps and clean up
+lb/maps-snapshot
+k8s/delete service.yaml endpointslice.yaml
+
+# Check that everything empty
+* db/empty frontends services backends
+lb/maps-dump maps.actual
+* cmp maps.actual maps.empty
+
+# Restore previous BPF map state and reset BPF ops to restore state from maps.
+lb/maps-restore
+test/bpfops-reset
+lb/maps-dump
+
+# We should see a quarantined restored backend.
+test/bpfops-summary
+stdout 'restoredQuarantines: 1'
+
+# Restore service and backends. Add an additional backend to ensure we've
+# actually updated the maps.
+k8s/add service.yaml endpointslice2.yaml
+
+# Wait for reconciliation.
+db/show frontends --columns=Address,Status -o frontends.actual
+* grep '10.96.*Done' frontends.actual
+
+# Check BPF maps. We should get the same contents as before but
+# with an extra backend.
+lb/maps-dump maps.actual
+* grep 'SLOT=0.*COUNT=1 QCOUNT=1' maps2.expected
+* cmp maps.actual maps2.expected
+
+# Mark the quarantined backend as healthy again
+test/update-backend-health test/echo 10.244.1.2:80/TCP true
+db/cmp backends backends_healthy2.table
+
+# Check BPF maps
+lb/maps-dump maps.actual
+* grep 'SLOT=0.*COUNT=2 QCOUNT=0' maps3.expected
+* cmp maps.actual maps3.expected
+
+# The quarantined restored state is clean now.
+test/bpfops-summary
+stdout 'restoredQuarantines: 0'
+stdout 'restoredServiceIDs: 1'
+stdout 'restoredBackendIDs: 2'
+
+# Since we now have some restored data, check that forced
+# pruning will now clean up the state.
+lb/prune
+
+# Wait until prune is done by checking that internal state
+# is clean.
+test/bpfops-summary
+* stdout 'restoredServiceIDs: 0'
+stdout 'restoredBackendIDs: 0'
+
+# Pruning should have removed the leftover 10.244.1.1:80.
+lb/maps-dump maps.actual
+* cmp maps.actual maps4.expected
+
+-- maps1.expected --
+BE: ID=1 ADDR=10.244.1.1:80/TCP STATE=active
+BE: ID=2 ADDR=10.244.1.2:80/TCP STATE=active
+REV: ID=1 ADDR=10.96.50.104:80
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=0 LBALG=random AFFTimeout=0 COUNT=1 QCOUNT=1 FLAGS=ClusterIP+non-routable
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=1 BEID=1 COUNT=0 QCOUNT=0 FLAGS=ClusterIP+non-routable
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=2 BEID=2 COUNT=0 QCOUNT=0 FLAGS=ClusterIP+non-routable
+-- maps2.expected --
+BE: ID=1 ADDR=10.244.1.1:80/TCP STATE=active
+BE: ID=2 ADDR=10.244.1.2:80/TCP STATE=active
+BE: ID=3 ADDR=10.244.1.3:80/TCP STATE=active
+REV: ID=1 ADDR=10.96.50.104:80
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=0 LBALG=random AFFTimeout=0 COUNT=1 QCOUNT=1 FLAGS=ClusterIP+non-routable
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=1 BEID=3 COUNT=0 QCOUNT=0 FLAGS=ClusterIP+non-routable
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=2 BEID=2 COUNT=0 QCOUNT=0 FLAGS=ClusterIP+non-routable
+-- maps3.expected --
+BE: ID=1 ADDR=10.244.1.1:80/TCP STATE=active
+BE: ID=2 ADDR=10.244.1.2:80/TCP STATE=active
+BE: ID=3 ADDR=10.244.1.3:80/TCP STATE=active
+REV: ID=1 ADDR=10.96.50.104:80
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=0 LBALG=random AFFTimeout=0 COUNT=2 QCOUNT=0 FLAGS=ClusterIP+non-routable
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=1 BEID=2 COUNT=0 QCOUNT=0 FLAGS=ClusterIP+non-routable
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=2 BEID=3 COUNT=0 QCOUNT=0 FLAGS=ClusterIP+non-routable
+-- maps4.expected --
+BE: ID=2 ADDR=10.244.1.2:80/TCP STATE=active
+BE: ID=3 ADDR=10.244.1.3:80/TCP STATE=active
+REV: ID=1 ADDR=10.96.50.104:80
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=0 LBALG=random AFFTimeout=0 COUNT=2 QCOUNT=0 FLAGS=ClusterIP+non-routable
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=1 BEID=2 COUNT=0 QCOUNT=0 FLAGS=ClusterIP+non-routable
+SVC: ID=1 ADDR=10.96.50.104:80/TCP SLOT=2 BEID=3 COUNT=0 QCOUNT=0 FLAGS=ClusterIP+non-routable
+-- maps.empty --
+
+-- services.table --
+Name
+test/echo
+
+-- backends_unhealthy.table --
+Address             Instances
+10.244.1.1:80/TCP   test/echo (http)
+10.244.1.2:80/TCP   test/echo [unhealthy] (http)
+
+
+-- backends_healthy1.table --
+Address             Instances
+10.244.1.1:80/TCP   test/echo (http)
+10.244.1.2:80/TCP   test/echo (http)
+
+-- backends_healthy2.table --
+Address             Instances
+10.244.1.2:80/TCP   test/echo (http)
+10.244.1.3:80/TCP   test/echo (http)
+
+-- service.yaml --
+apiVersion: v1
+kind: Service
+metadata:
+  name: echo
+  namespace: test
+spec:
+  clusterIP: 10.96.50.104
+  clusterIPs:
+  - 10.96.50.104
+  ports:
+  - name: http
+    port: 80
+    protocol: TCP
+    targetPort: 80
+  selector:
+    name: echo
+  type: ClusterIP
+
+-- endpointslice.yaml --
+apiVersion: discovery.k8s.io/v1
+kind: EndpointSlice
+metadata:
+  labels:
+    kubernetes.io/service-name: echo
+  name: echo-kvlm2
+  namespace: test
+addressType: IPv4
+endpoints:
+- addresses:
+  - 10.244.1.1
+  - 10.244.1.2
+  conditions:
+    ready: true
+    serving: true
+    terminating: false
+  nodeName: nodeport-worker
+ports:
+- name: http
+  port: 80
+  protocol: TCP
+
+
+-- endpointslice2.yaml --
+apiVersion: discovery.k8s.io/v1
+kind: EndpointSlice
+metadata:
+  labels:
+    kubernetes.io/service-name: echo
+  name: echo-kvlm2
+  namespace: test
+addressType: IPv4
+endpoints:
+- addresses:
+  - 10.244.1.2
+  - 10.244.1.3
+  conditions:
+    ready: true
+    serving: true
+    terminating: false
+  nodeName: nodeport-worker
+ports:
+- name: http
+  port: 80
+  protocol: TCP

pkg/loadbalancer/experimental/writer.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/source"
+	"github.com/cilium/cilium/pkg/time"
 )
 
 // Writer provides validated write access to the service load-balancing state.
@@ -183,6 +184,27 @@ func (w *Writer) UpsertFrontend(txn WriteTxn, params FrontendParams) (old *Front
 		return nil, ErrServiceNotFound
 	}
 	return w.upsertFrontendParams(txn, params, svc)
+}
+
+func (w *Writer) UpdateBackendHealth(txn WriteTxn, serviceName loadbalancer.ServiceName, backend loadbalancer.L3n4Addr, healthy bool) (bool, error) {
+	be, _, ok := w.bes.Get(txn, BackendByAddress(backend))
+	if !ok {
+		return false, ErrServiceNotFound
+	}
+	inst := be.GetInstance(serviceName)
+	if inst == nil {
+		return false, ErrServiceNotFound
+	}
+	if inst.Unhealthy == !healthy && !inst.UnhealthyUpdatedAt.IsZero() {
+		return false, nil
+	}
+
+	be = be.Clone()
+	inst.Unhealthy = !healthy
+	inst.UnhealthyUpdatedAt = time.Now()
+	be.Instances = be.Instances.Set(BackendInstanceKey{serviceName, w.sourcePriority(inst.Source)}, *inst)
+	w.bes.Insert(txn, be)
+	return true, w.RefreshFrontends(txn, serviceName)
 }
 
 func (w *Writer) upsertFrontendParams(txn WriteTxn, params FrontendParams, svc *Service) (old *Frontend, err error) {
@@ -421,6 +443,12 @@ func (w *Writer) updateBackends(txn WriteTxn, serviceName loadbalancer.ServiceNa
 
 		if old, _, ok := w.bes.Get(txn, BackendByAddress(bep.L3n4Addr)); ok {
 			be = *old
+		}
+
+		if inst := be.GetInstanceFromSource(serviceName, source); inst != nil {
+			// Previous instance exists, keep the health information.
+			bep.Unhealthy = inst.Unhealthy
+			bep.UnhealthyUpdatedAt = inst.UnhealthyUpdatedAt
 		}
 
 		bep.Source = source

commit 47e283d12f39e7babaf1f85cef691e8a23558e85

loadbalancer: discard stale restored quarantines
Drop restored quarantine entries for backends omitted by a reconciled frontend
and remove restore-only state while pruning an unobserved frontend.

AIL:3
Fixes: 7874a618fd0e ("experimental: Add support for health checking")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/loadbalancer/reconciler/bpf_reconciler.go
@@ -481,8 +481,9 @@ func (ops *BPFOps) deleteRestoredQuarantinedBackends(fe loadbalancer.L3n4Addr, b
 	}
 
 	// A state created only while restoring quarantine information has no
-	// backendReferences set. Remove it once that information has been consumed.
-	if state.backendReferences == nil {
+	// backendReferences set. Remove it once all quarantine information has been
+	// consumed.
+	if state.backendReferences == nil && state.restoredQuarantinedBackends == nil {
 		delete(ops.frontendStates, fe)
 	}
 }
@@ -657,8 +658,9 @@ func (ops *BPFOps) pruneServiceMaps() error {
 			port,
 			svcKey.GetScope(),
 		)
+		state := ops.frontendStates[addr]
 		expectedSlots := 0
-		if state := ops.frontendStates[addr]; state != nil && state.backendReferences != nil {
+		if state != nil && state.backendReferences != nil {
 			expectedSlots = 1 + state.slotCount
 		}
 		if svcKey.GetBackendSlot()+1 > expectedSlots {
@@ -667,8 +669,11 @@ func (ops *BPFOps) pruneServiceMaps() error {
 				logfields.Address, addr)
 			toDelete = append(toDelete, svcKey.ToNetwork())
 
-			// Drop restored quarantined state
-			if svcKey.GetBackendSlot() > 0 {
+			// A restore-only state belongs to a frontend that was not observed
+			// during reconciliation, so none of its quarantines remain relevant.
+			if state != nil && state.backendReferences == nil {
+				ops.deleteRestoredQuarantinedBackends(addr)
+			} else if svcKey.GetBackendSlot() > 0 {
 				if beAddr, found := ops.backendIDAlloc.idToAddr[svcValue.GetBackendID()]; found {
 					ops.deleteRestoredQuarantinedBackends(addr, beAddr)
 				}
@@ -1093,6 +1098,13 @@ func (ops *BPFOps) updateFrontend(fe *loadbalancer.Frontend, isLocalAddr func(ne
 	backendAddrs := sets.New[loadbalancer.L3n4Addr]()
 	for _, be := range orderedBackends {
 		backendAddrs.Insert(be.Address)
+	}
+	if state := ops.frontendStates[fe.Address]; state != nil {
+		for addr := range state.restoredQuarantinedBackends {
+			if !backendAddrs.Has(addr) {
+				ops.deleteRestoredQuarantinedBackends(fe.Address, addr)
+			}
+		}
 	}
 
 	feState := ops.getOrCreateFrontendState(fe.Address)

pkg/loadbalancer/reconciler/bpf_reconciler_test.go
@@ -1823,6 +1823,36 @@ func TestBPFOpsLeakRegressions(t *testing.T) {
 			"restored quarantine state for an omitted backend remains")
 	})
 
+	t.Run("restored quarantine retains referenced backends", func(t *testing.T) {
+		lbmaps := maps.NewFakeLBMaps()
+		fixture := newBPFOpsLeakTestFixture(t, lbmaps, nil, nil)
+		firstBackend := newTestBackend(backend1, loadbalancer.BackendStateActive)
+		firstBackend.Unhealthy = true
+		secondBackend := newTestBackend(backend2, loadbalancer.BackendStateActive)
+		secondBackend.Unhealthy = true
+		frontend := newLeakTestFrontend(extraFrontend, ClusterIP, firstBackend, secondBackend)
+		require.NoError(t, fixture.ops.Update(context.TODO(), fixture.db.ReadTxn(), 0, &frontend), "quarantined Update")
+
+		fixture = newBPFOpsLeakTestFixture(t, lbmaps, nil, nil)
+		state := fixture.ops.frontendStates[frontend.Address]
+		require.NotNil(t, state, "restored frontend state")
+		require.Contains(t, state.restoredQuarantinedBackends, backend1,
+			"first quarantined backend was not restored")
+		require.Contains(t, state.restoredQuarantinedBackends, backend2,
+			"second quarantined backend was not restored")
+
+		frontend = newLeakTestFrontend(extraFrontend, ClusterIP,
+			newTestBackend(backend2, loadbalancer.BackendStateActive))
+		require.NoError(t, fixture.ops.Update(context.TODO(), fixture.db.ReadTxn(), 0, &frontend), "Update")
+
+		state = fixture.ops.frontendStates[frontend.Address]
+		require.NotNil(t, state, "updated frontend state")
+		require.NotContains(t, state.restoredQuarantinedBackends, backend1,
+			"restored quarantine state for an omitted backend remains")
+		require.Contains(t, state.restoredQuarantinedBackends, backend2,
+			"restored quarantine state for a referenced backend was dropped")
+	})
+
 	t.Run("startup prune removes restore-only quarantine state", func(t *testing.T) {
 		lbmaps := maps.NewFakeLBMaps()
 		frontendAddr := programQuarantinedFrontend(t, lbmaps)