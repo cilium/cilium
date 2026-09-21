f785e97f47a7e08d9844a7f8ea8b405fbc449f12
xtine <xtineskim@gmail.com>
2026-09-21 16:26:47 -0400

    gateway-api: add GIE conformance tests in conformance_test.go
    Similar to Gateway API's conformance tests, adding in way to run tests.
    Added TestGIEConformance to conformance_test.go.
    
    Signed-off-by: xtine <xtineskim@gmail.com>

--- pkg/testutils/privileged.go
+++ pkg/testutils/privileged.go
@@ -13,6 +13,7 @@ const (
 	privilegedEnv            = "PRIVILEGED_TESTS"
 	integrationEnv           = "INTEGRATION_TESTS"
 	gatewayAPIConformanceEnv = "GATEWAY_API_CONFORMANCE_TESTS"
+	gieConformanceEnv        = "GIE_CONFORMANCE_TESTS"
 	mcsAPIConformanceEnv     = "MCS_API_CONFORMANCE_TESTS"
 
 	requiredTestPrefix      = "TestPrivileged"
@@ -83,6 +84,13 @@ func GatewayAPIConformanceTest(tb testing.TB) {
 
 	if os.Getenv(gatewayAPIConformanceEnv) == "" {
 		tb.Skipf("Set %s to run this test", gatewayAPIConformanceEnv)
+	}
+}
+func GIEConformanceTest(tb testing.TB) {
+	tb.Helper()
+
+	if os.Getenv(gieConformanceEnv) == "" {
+		tb.Skipf("Set %s to run this test", gieConformanceEnv)
 	}
 }
 
