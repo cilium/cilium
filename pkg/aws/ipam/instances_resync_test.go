cilium/cilium

commit 45326efbe51cbe3e4198d81be3acb6de9c092fb8

ipam/aws: update the assigned IPs from aws to instance map
- Extracting and returning the assigned IP addresses
- Adding new methods to update ENI IP addresses in the instances manager
- Update the IP to ENI after getting the IP from aws

Signed-off-by: Liyi Huang <liyi.huang@isovalent.com>

pkg/aws/eni/instances.go
@@ -7,6 +7,7 @@ package eni
 
 import (
 	"context"
+	"slices"
 
 	ec2_types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/sirupsen/logrus"
@@ -34,7 +35,7 @@ type EC2API interface {
 	AttachNetworkInterface(ctx context.Context, index int32, instanceID, eniID string) (string, error)
 	DeleteNetworkInterface(ctx context.Context, eniID string) error
 	ModifyNetworkInterface(ctx context.Context, eniID, attachmentID string, deleteOnTermination bool) error
-	AssignPrivateIpAddresses(ctx context.Context, eniID string, addresses int32) error
+	AssignPrivateIpAddresses(ctx context.Context, eniID string, addresses int32) ([]string, error)
 	UnassignPrivateIpAddresses(ctx context.Context, eniID string, addresses []string) error
 	AssignENIPrefixes(ctx context.Context, eniID string, prefixes int32) error
 	UnassignENIPrefixes(ctx context.Context, eniID string, prefixes []string) error
@@ -276,6 +277,51 @@ func (m *InstancesManager) UpdateENI(instanceID string, eni *eniTypes.ENI) {
 	eniRevision := ipamTypes.InterfaceRevision{Resource: eni}
 	m.instances.Update(instanceID, eniRevision)
 	m.mutex.Unlock()
+}
+
+func (m *InstancesManager) AddIPsToENI(instanceID string, eniID string, ips []string) {
+	m.modifyIPsToENI(instanceID, eniID, ips, true)
+}
+
+func (m *InstancesManager) RemoveIPsFromENI(instanceID string, eniID string, ips []string) {
+	m.modifyIPsToENI(instanceID, eniID, ips, false)
+}
+
+func (m *InstancesManager) modifyIPsToENI(instanceID string, eniID string, ips []string, isAdd bool) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	ifaces, ok := m.instances.GetInterface(instanceID, eniID)
+	if !ok {
+		log.WithFields(logrus.Fields{
+			"instance": instanceID,
+			"eni":      eniID,
+		}).Warning("ENI not found")
+		return
+	}
+
+	eniIntf := ifaces.Resource.DeepCopyInterface()
+	eni, ok := eniIntf.(*eniTypes.ENI)
+	if !ok {
+		log.WithFields(logrus.Fields{
+			"instance": instanceID,
+			"eni":      eniID,
+		}).Warning("Unexpected resource type, expected *eniTypes.ENI")
+		return
+	}
+	if isAdd {
+		for _, ip := range ips {
+			if !slices.Contains(eni.Addresses, ip) {
+				eni.Addresses = append(eni.Addresses, ip)
+			}
+		}
+	} else {
+		for _, ip := range ips {
+			eni.Addresses = slices.DeleteFunc(eni.Addresses, func(addr string) bool {
+				return addr == ip
+			})
+		}
+	}
+	m.instances.Update(instanceID, ipamTypes.InterfaceRevision{Resource: eni})
 }
 
 // ForeachInstance will iterate over each interface for a particular instance inside `instances`

pkg/aws/ec2/ec2.go
@@ -710,7 +710,7 @@ func (c *Client) ModifyNetworkInterface(ctx context.Context, eniID, attachmentID
 
 // AssignPrivateIpAddresses assigns the specified number of secondary IP
 // addresses
-func (c *Client) AssignPrivateIpAddresses(ctx context.Context, eniID string, addresses int32) error {
+func (c *Client) AssignPrivateIpAddresses(ctx context.Context, eniID string, addresses int32) ([]string, error) {
 	input := &ec2.AssignPrivateIpAddressesInput{
 		NetworkInterfaceId:             aws.String(eniID),
 		SecondaryPrivateIpAddressCount: aws.Int32(addresses),
@@ -718,9 +718,16 @@ func (c *Client) AssignPrivateIpAddresses(ctx context.Context, eniID string, add
 
 	c.limiter.Limit(ctx, AssignPrivateIpAddresses)
 	sinceStart := spanstat.Start()
-	_, err := c.ec2Client.AssignPrivateIpAddresses(ctx, input)
+	output, err := c.ec2Client.AssignPrivateIpAddresses(ctx, input)
 	c.metricsAPI.ObserveAPICall(AssignPrivateIpAddresses, deriveStatus(err), sinceStart.Seconds())
-	return err
+	if err != nil {
+		return nil, err
+	}
+	assignedIPs := make([]string, addresses)
+	for i, ip := range output.AssignedPrivateIpAddresses {
+		assignedIPs[i] = aws.ToString(ip.PrivateIpAddress)
+	}
+	return assignedIPs, nil
 }
 
 // UnassignPrivateIpAddresses unassigns specified IP addresses from ENI

pkg/aws/ec2/mock/mock.go
@@ -344,7 +344,7 @@ func (e *API) GetDetachedNetworkInterfaces(ctx context.Context, tags ipamTypes.T
 	return result, nil
 }
 
-func (e *API) AssignPrivateIpAddresses(ctx context.Context, eniID string, addresses int32) error {
+func (e *API) AssignPrivateIpAddresses(ctx context.Context, eniID string, addresses int32) ([]string, error) {
 	e.rateLimit()
 	e.delaySim.Delay(AssignPrivateIpAddresses)
 
@@ -352,18 +352,18 @@ func (e *API) AssignPrivateIpAddresses(ctx context.Context, eniID string, addres
 	defer e.mutex.Unlock()
 
 	if err, ok := e.errors[AssignPrivateIpAddresses]; ok {
-		return err
+		return nil, err
 	}
 
 	for _, enis := range e.enis {
 		if eni, ok := enis[eniID]; ok {
 			subnet, ok := e.subnets[eni.Subnet.ID]
 			if !ok {
-				return fmt.Errorf("subnet %s not found", eni.Subnet.ID)
+				return nil, fmt.Errorf("subnet %s not found", eni.Subnet.ID)
 			}
 
 			if int(addresses) > subnet.AvailableAddresses {
-				return fmt.Errorf("subnet %s has not enough addresses available", eni.Subnet.ID)
+				return nil, fmt.Errorf("subnet %s has not enough addresses available", eni.Subnet.ID)
 			}
 
 			for i := int32(0); i < addresses; i++ {
@@ -374,10 +374,10 @@ func (e *API) AssignPrivateIpAddresses(ctx context.Context, eniID string, addres
 				eni.Addresses = append(eni.Addresses, ip.String())
 			}
 			subnet.AvailableAddresses -= int(addresses)
-			return nil
+			return eni.Addresses, nil
 		}
 	}
-	return fmt.Errorf("Unable to find ENI with ID %s", eniID)
+	return nil, fmt.Errorf("Unable to find ENI with ID %s", eniID)
 }
 
 func (e *API) UnassignPrivateIpAddresses(ctx context.Context, eniID string, addresses []string) error {

pkg/aws/ec2/mock/mock_test.go
@@ -102,7 +102,7 @@ func TestSetMockError(t *testing.T) {
 	require.Equal(t, mockError, err)
 
 	api.SetMockError(AssignPrivateIpAddresses, mockError)
-	err = api.AssignPrivateIpAddresses(context.TODO(), "e-1", 10)
+	_, err = api.AssignPrivateIpAddresses(context.TODO(), "e-1", 10)
 	require.Equal(t, mockError, err)
 
 	api.SetMockError(UnassignPrivateIpAddresses, mockError)

pkg/aws/eni/node.go
@@ -198,7 +198,13 @@ func (n *Node) PrepareIPRelease(excessIPs int, scopedLog *logrus.Entry) *ipam.Re
 
 // ReleaseIPs performs the ENI IP release operation
 func (n *Node) ReleaseIPs(ctx context.Context, r *ipam.ReleaseAction) error {
-	return n.manager.api.UnassignPrivateIpAddresses(ctx, r.InterfaceID, r.IPsToRelease)
+	if err := n.manager.api.UnassignPrivateIpAddresses(ctx, r.InterfaceID, r.IPsToRelease); err != nil {
+		return err
+	}
+
+	n.manager.RemoveIPsFromENI(n.node.InstanceID(), r.InterfaceID, r.IPsToRelease)
+	return nil
+
 }
 
 // PrepareIPAllocation returns the number of ENI IPs and interfaces that can be
@@ -289,7 +295,12 @@ func (n *Node) AllocateIPs(ctx context.Context, a *ipam.AllocationAction) error 
 			logfields.Node: n.k8sObj.Name,
 		}).Warning("Subnet might be out of prefixes, Cilium will not allocate prefixes on this node anymore")
 	}
-	return n.manager.api.AssignPrivateIpAddresses(ctx, a.InterfaceID, int32(a.IPv4.AvailableForAllocation))
+	assignedIPs, err := n.manager.api.AssignPrivateIpAddresses(ctx, a.InterfaceID, int32(a.IPv4.AvailableForAllocation))
+	if err != nil {
+		return err
+	}
+	n.manager.AddIPsToENI(n.node.InstanceID(), a.InterfaceID, assignedIPs)
+	return nil
 }
 
 func (n *Node) AllocateStaticIP(ctx context.Context, staticIPTags ipamTypes.Tags) (string, error) {

commit 75ff1f6c650887dd269177577bd588041467b24c

aws/ipam: prevent full resyncs from overwriting ENI updates
InstancesManager.Resync fetches EC2 state without holding m.mutex,
then takes the lock to replace the cache. If an IP release updates
the cache after the EC2 read, the resync can put the released IPs back
into the cache. The mutex prevents concurrent writes, but not an older
snapshot overwriting a newer update. In run [1], this made
TestNodeManagerReleaseAddress report 18 available IPs instead of 13.

Take a read lock on resyncLock in UpdateENI and modifyIPsToENI before
taking m.mutex. Full resyncs hold the write lock across both the EC2
fetch and cache replacement, so these updates now wait and apply to
the new cache. This follows the existing InstanceSync lock order
without holding m.mutex during EC2 calls. It also prevents a resync
from dropping a newly attached ENI from the cache.

Add regression tests that pause a full resync after its EC2 read and
check that a concurrent IP release or ENI attachment survives it.

[1] https://github.com/cilium/cilium/actions/runs/35536949428

This commit was prepared with AIL:3. I personally checked the diff.

Fixes: 45326efbe51c ("ipam/aws: update the assigned IPs from aws to instance map")

Signed-off-by: André Martins <andre@cilium.io>

pkg/aws/ipam/instances.go
@@ -58,7 +58,7 @@ type MetadataAPI interface {
 // by calling resync() regularly.
 type InstancesManager struct {
 	logger *slog.Logger
-	// resyncLock ensures instance incremental resync do not run at the same time as a full API resync
+	// resyncLock keeps incremental resync and cache updates out of a full API resync
 	resyncLock lock.RWMutex
 	// vpcID is the VPC ID current operator running on, we will use it to filter other AWS resources only within this VPC
 	vpcID string
@@ -290,6 +290,9 @@ func (m *InstancesManager) InstanceSync(ctx context.Context, instanceID string) 
 // the ENI is already known, the definition is updated, otherwise the ENI is
 // added to the instance.
 func (m *InstancesManager) UpdateENI(instanceID string, eni *types.ENI) {
+	m.resyncLock.RLock()
+	defer m.resyncLock.RUnlock()
+
 	m.mutex.Lock()
 	eniRevision := eni
 	m.instances.Update(instanceID, eniRevision)
@@ -305,6 +308,9 @@ func (m *InstancesManager) RemoveIPsFromENI(instanceID string, eniID string, ips
 }
 
 func (m *InstancesManager) modifyIPsToENI(instanceID string, eniID string, ips []string, isAdd bool) {
+	m.resyncLock.RLock()
+	defer m.resyncLock.RUnlock()
+
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 	iface, ok := m.instances.GetInterface(instanceID, eniID)

pkg/aws/ipam/instances_resync_test.go
@@ -1,0 +1,203 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package ipam
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/cilium/hive/hivetest"
+	"github.com/stretchr/testify/require"
+
+	apiMock "github.com/cilium/cilium/pkg/aws/api/mock"
+	"github.com/cilium/cilium/pkg/aws/types"
+	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
+)
+
+// hookedEC2API calls afterGetInstances while a full resync holds its EC2 snapshot.
+type hookedEC2API struct {
+	EC2API
+	afterGetInstances func()
+}
+
+func (h *hookedEC2API) GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap, subnets ipamTypes.SubnetMap) (*ipamTypes.InstanceMap, error) {
+	instances, err := h.EC2API.GetInstances(ctx, vpcs, subnets)
+	if h.afterGetInstances != nil {
+		h.afterGetInstances()
+	}
+	return instances, err
+}
+
+func cachedENIAddresses(m *InstancesManager, instanceID string) (addrs []string) {
+	m.ForeachInstance(instanceID, func(_, _ string, iface ipamTypes.Interface) error {
+		eni, ok := iface.(*types.ENI)
+		if !ok {
+			return nil
+		}
+		for _, addr := range eni.Addresses {
+			addrs = append(addrs, addr.String())
+		}
+		return nil
+	})
+	return addrs
+}
+
+func cachedENIIDs(m *InstancesManager, instanceID string) (ids []string) {
+	m.ForeachInstance(instanceID, func(_, ifaceID string, _ ipamTypes.Interface) error {
+		ids = append(ids, ifaceID)
+		return nil
+	})
+	return ids
+}
+
+// runDuringFullResync runs ec2Setup and then cacheUpdate inside the EC2 fetch window of a full resync.
+func runDuringFullResync(t *testing.T, hooked *hookedEC2API, instances *InstancesManager, ec2Setup func() error, cacheUpdate func()) {
+	t.Helper()
+
+	// An unguarded cacheUpdate must land within this much of the fetch window for the resync to drop it.
+	const detectionBudget = 2 * time.Second
+	// Only a locking regression keeps ec2Setup or cacheUpdate waiting this long.
+	const progressTimeout = 10 * time.Second
+
+	var setupErr error
+	var windows int
+	var setupStalled bool
+	atCacheUpdate := make(chan struct{})
+	done := make(chan struct{})
+	hookReturned := make(chan struct{})
+
+	// The resync goroutine runs this hook, so it records what it saw and never calls t itself.
+	hooked.afterGetInstances = func() {
+		defer close(hookReturned)
+		windows++
+		go func() {
+			defer close(done)
+			if setupErr = ec2Setup(); setupErr != nil {
+				return
+			}
+			close(atCacheUpdate)
+			cacheUpdate()
+		}()
+
+		// Keep the EC2 calls out of the budget below, and never wait on a setup that gave up.
+		select {
+		case <-atCacheUpdate:
+		case <-done:
+			return
+		case <-time.After(progressTimeout):
+			setupStalled = true
+			return
+		}
+
+		// A guarded cacheUpdate waits for resyncLock, so this select always spends the whole budget.
+		select {
+		case <-done:
+		case <-time.After(detectionBudget):
+		}
+	}
+
+	resynced := make(chan error, 1)
+	go func() {
+		_, err := instances.Resync(t.Context())
+		resynced <- err
+	}()
+
+	// The hook owns progressTimeout plus the budget, so wait that out before blaming the resync.
+	select {
+	case <-hookReturned:
+	case <-time.After(progressTimeout + detectionBudget + time.Second):
+		t.Fatalf("the full resync did not reach its EC2 fetch hook within %v", progressTimeout)
+	}
+	require.False(t, setupStalled, "EC2 setup neither reached the cache update nor failed within %v", progressTimeout)
+
+	// A cacheUpdate that takes the locks in the wrong order deadlocks the publish after the hook.
+	select {
+	case err := <-resynced:
+		require.NoError(t, err)
+	case <-time.After(progressTimeout):
+		t.Fatalf("the full resync did not publish within %v of its fetch window; cacheUpdate holds a lock it needs", progressTimeout)
+	}
+
+	select {
+	case <-done:
+	case <-time.After(progressTimeout):
+		t.Fatalf("cache update still blocked %v after the full resync released resyncLock", progressTimeout)
+	}
+
+	hooked.afterGetInstances = nil
+	require.NoError(t, setupErr)
+	// Guard against the hook silently never running.
+	require.Equal(t, 1, windows)
+}
+
+// TestFullResyncKeepsConcurrentIPRelease releases an IP inside a full resync's fetch window.
+func TestFullResyncKeepsConcurrentIPRelease(t *testing.T) {
+	const instanceID = "i-testFullResyncKeepsConcurrentIPRelease"
+
+	ec2api := apiMock.NewAPI([]*ipamTypes.Subnet{testSubnet}, []*ipamTypes.VirtualNetwork{testVpc}, testSecurityGroups, testRouteTables)
+	hooked := &hookedEC2API{EC2API: ec2api}
+	instances, err := NewInstancesManager(t.Context(), hivetest.Logger(t), hooked, metadataMockapi)
+	require.NoError(t, err)
+
+	eniID, _, err := ec2api.CreateNetworkInterface(t.Context(), 4, testSubnet.ID, "desc", []string{"sg-1"}, false, false)
+	require.NoError(t, err)
+	_, err = ec2api.AttachNetworkInterface(t.Context(), 0, instanceID, eniID)
+	require.NoError(t, err)
+	_, err = instances.Resync(t.Context())
+	require.NoError(t, err)
+
+	addrs := cachedENIAddresses(instances, instanceID)
+	require.NotEmpty(t, addrs)
+	released := addrs[0]
+
+	runDuringFullResync(t, hooked, instances,
+		func() error {
+			return ec2api.UnassignPrivateIpAddresses(context.Background(), eniID, []string{released})
+		},
+		func() {
+			instances.RemoveIPsFromENI(instanceID, eniID, []string{released})
+		})
+
+	require.NotContains(t, cachedENIAddresses(instances, instanceID), released)
+	require.Len(t, cachedENIAddresses(instances, instanceID), len(addrs)-1)
+}
+
+// TestFullResyncKeepsConcurrentENIUpdate attaches an ENI inside a full resync's fetch window.
+func TestFullResyncKeepsConcurrentENIUpdate(t *testing.T) {
+	const instanceID = "i-testFullResyncKeepsConcurrentENIUpdate"
+
+	ec2api := apiMock.NewAPI([]*ipamTypes.Subnet{testSubnet}, []*ipamTypes.VirtualNetwork{testVpc}, testSecurityGroups, testRouteTables)
+	hooked := &hookedEC2API{EC2API: ec2api}
+	instances, err := NewInstancesManager(t.Context(), hivetest.Logger(t), hooked, metadataMockapi)
+	require.NoError(t, err)
+
+	attachedENI, _, err := ec2api.CreateNetworkInterface(t.Context(), 4, testSubnet.ID, "desc", []string{"sg-1"}, false, false)
+	require.NoError(t, err)
+	_, err = ec2api.AttachNetworkInterface(t.Context(), 0, instanceID, attachedENI)
+	require.NoError(t, err)
+	_, err = instances.Resync(t.Context())
+	require.NoError(t, err)
+	require.Equal(t, []string{attachedENI}, cachedENIIDs(instances, instanceID))
+
+	var createdENI string
+	var createdIface *types.ENI
+	runDuringFullResync(t, hooked, instances,
+		func() (err error) {
+			createdENI, createdIface, err = ec2api.CreateNetworkInterface(context.Background(), 4, testSubnet.ID, "desc", []string{"sg-1"}, false, false)
+			if err != nil {
+				return err
+			}
+			if _, err = ec2api.AttachNetworkInterface(context.Background(), 1, instanceID, createdENI); err != nil {
+				return err
+			}
+			createdIface.Number = 1
+			return nil
+		},
+		func() {
+			instances.UpdateENI(instanceID, createdIface)
+		})
+
+	require.ElementsMatch(t, []string{attachedENI, createdENI}, cachedENIIDs(instances, instanceID))
+}